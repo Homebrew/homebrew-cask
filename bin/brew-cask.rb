aca9beece156b90f3361930e44a7eb84cac60ef7
phinze <paul.t.hinze@gmail.com>
2012-03-10 01:01:38 -0600

    back to .rb for bin, so homebrew `require`s us

--- bin/brew-cask.rb
+++ bin/brew-cask.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+# this file expects to be required from within homebrew's ruby environment
 
 $LOAD_PATH << File.expand_path('../../lib', Pathname.new(__FILE__).realpath)
 require 'cask'
