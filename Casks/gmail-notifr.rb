e2158003be25249be8858e0f62e05374c7fc0cd9
phinze <paul.t.hinze@gmail.com>
2012-03-10 16:20:23 -0600

    GmailNotifr 0.6.4

---
+++ Casks/gmail-notifr.rb
@@ -0,0 +1,5 @@
+class GmailNotifr < Cask
+  url 'https://github.com/downloads/ashchan/gmail-notifr/GmailNotifr-0.6.4.zip'
+  homepage 'http://ashchan.com/projects/gmail-notifr'
+  version '0.6.4'
+end
