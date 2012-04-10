3995d05dea4968bbf0dbfaf4b1572df649646f3a
Brian Cosgrove <cosgroveb@gmail.com>
2012-04-10 17:47:00 -0500

    Adds firefox 11.0

---
+++ Casks/firefox.rb
@@ -0,0 +1,5 @@
+class Firefox < Cask
+  url 'http://releases.mozilla.org/pub/mozilla.org/firefox/releases/11.0/mac/en-US/Firefox%2011.0.dmg'
+  homepage 'http://www.mozilla.org/en-US/firefox/'
+  version '11.0'
+end
