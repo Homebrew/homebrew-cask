b84326203bdb3f364c3bd3ddb88edd7ccc34b90d
phinze <paul.t.hinze@gmail.com>
2012-08-25 22:21:47 -0500

    update Alfred to 1.5.3

--- Casks/adium.rb
+++ Casks/adium.rb
@@ -1,5 +1,5 @@
 class Adium < Cask
-  url 'http://download.adium.im/Adium_1.4.4.dmg'
+  url 'http://download.adium.im/Adium_1.5.3.dmg'
   homepage 'http://www.adium.im/'
-  version '1.4.4'
+  version '1.5.3'
 end
