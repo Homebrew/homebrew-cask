5405cef0019fbdc45ba72e3b4bb1c0589c3db7ab
phinze <paul.t.hinze@gmail.com>
2012-09-01 23:18:53 -0500

    google-notifier 1.10.7

---
+++ Casks/google-notifier.rb
@@ -0,0 +1,5 @@
+class GoogleNotifier < Cask
+  url 'http://dl.google.com/mac/download/GoogleNotifier_1.10.7.dmg'
+  homepage 'http://toolbar.google.com/gmail-helper/notifier_mac.html'
+  version '1.10.7'
+end
