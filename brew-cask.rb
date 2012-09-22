ce9c568d185cfcb1938e4038ca7dd1652b1ca97d
phinze <paul.t.hinze@gmail.com>
2012-09-21 23:37:16 -0500

    update to 0.2.0; fix skip_clean deprecation

--- brew-cask.rb
+++ brew-cask.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class BrewCask < Formula
   homepage 'https://github.com/phinze/homebrew-cask/'
-  url 'https://github.com/phinze/homebrew-cask.git', :tag => 'v0.1.0'
-  version '0.1.0'
+  url 'https://github.com/phinze/homebrew-cask.git', :tag => 'v0.2.0'
+  version '0.2.0'
 
-  skip_clean :all
+  skip_clean 'bin'
 
   def install
     prefix.install_p 'lib', 'rubylib'
