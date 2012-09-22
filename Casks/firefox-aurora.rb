781afeaaae22aa981b69e642a54f51d414c2544a
Félix Saparelli <me@passcod.net>
2012-09-22 12:42:32 +1200

    Firefox Aurora 17.0a2 (210912)

---
+++ Casks/firefox-aurora.rb
@@ -0,0 +1,5 @@
+class FirefoxAurora < Cask
+  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-17.0a2.en-US.mac.dmg'
+  homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
+  version '17.0a2 (210912)'
+end
