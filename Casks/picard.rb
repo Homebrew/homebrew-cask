34a64d26190e8d06071256f6b7c8dd6b02df3688
phinze <paul.t.hinze@gmail.com>
2012-09-21 22:45:58 -0500

    picard 1.1

---
+++ Casks/picard.rb
@@ -0,0 +1,5 @@
+class Picard < Cask
+  url 'ftp://ftp.musicbrainz.org/pub/musicbrainz/picard/MusicBrainz-Picard-1.1.dmg'
+  homepage 'http://musicbrainz.org/doc/MusicBrainz_Picard'
+  version '1.1'
+end
