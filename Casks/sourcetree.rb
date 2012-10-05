81086363a686d03827b92cb0d64d38b576d2461b
phinze <paul.t.hinze@gmail.com>
2012-10-05 08:41:32 -0500

    format Sourcetree constant like homebrew expects
    fixes #27

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,4 +1,4 @@
-class SourceTree < Cask
+class Sourcetree < Cask
   url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.4.dmg'
   homepage 'http://www.sourcetreeapp.com/'
   version '1.5.4'
