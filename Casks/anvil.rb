aff3777759dfced2b58d0e14c6f1a071f48c5648
phinze <paul.t.hinze@gmail.com>
2012-10-06 11:57:23 -0500

    new cask: anvil

---
+++ Casks/anvil.rb
@@ -0,0 +1,5 @@
+class Anvil < Cask
+  url 'http://s3.amazonaws.com/anvilformac/versions/binaries/000/000/012/original/Anvil.zip?1349511953'
+  homepage 'http://anvilformac.com/'
+  version '0.32'
+end
