5ef4f6267275e34104c62a9f8cf68db0e9b35773
phinze <paul.t.hinze@gmail.com>
2012-10-06 13:52:43 -0500

    change anvil url/version to point to latest
    per @elliottkember's request on
    aff3777759dfced2b58d0e14c6f1a071f48c5648, which was incidentally also a
    good idea

--- Casks/anvil.rb
+++ Casks/anvil.rb
@@ -1,5 +1,5 @@
 class Anvil < Cask
-  url 'http://s3.amazonaws.com/anvilformac/versions/binaries/000/000/012/original/Anvil.zip?1349511953'
+  url 'http://anvilformac.com/download'
   homepage 'http://anvilformac.com/'
-  version '0.32'
+  version 'latest'
 end
