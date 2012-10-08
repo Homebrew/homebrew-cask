67b88226f963f529690efe91a5d09196d59eae39
Ryan Aghdam <raghdam@paypal.com>
2012-10-08 12:16:51 -0400

    Added cask for Thunderbird

---
+++ Casks/thunderbird.rb
@@ -0,0 +1,5 @@
+class Thunderbird < Cask
+    homepage 'http://www.mozilla.org/thunderbird/'
+    url 'http://download.mozilla.org/?product=thunderbird-15.0.1&os=osx&lang=en-US'
+    version '15.0.1'
+end
