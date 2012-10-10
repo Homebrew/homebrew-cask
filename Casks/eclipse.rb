0d0070a2f6a7a1b6f7afdbff0bd655b3fd195656
Ryan Aghdam <ryanaghdam@Ryans-MacBook-Air.local>
2012-10-10 17:54:54 -0400

    added cask for eclipse

---
+++ Casks/eclipse.rb
@@ -0,0 +1,5 @@
+class Eclipse < Cask
+  url 'http://www.gtlib.gatech.edu/pub/eclipse/eclipse/downloads/drops4/R-4.2.1-201209141800/eclipse-SDK-4.2.1-macosx-cocoa-x86_64.tar.gz'
+  homepage 'http://www.eclipse.org'
+  version '4.2.1'
+end
