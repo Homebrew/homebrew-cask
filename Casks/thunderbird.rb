c589e5771f7806ace357248e640c589ec542d2e6
Ben Bailey <bennettbailey@gmail.com>
2012-10-13 20:28:08 -0500

    removed broken thunderbird cask

--- Casks/thunderbird.rb
+++
@@ -1,5 +0,0 @@
-class Thunderbird < Cask
-  url 'http://download.mozilla.org/?product=thunderbird-15.0.1&os=osx&lang=en-US'
-  homepage 'http://www.mozilla.org/en-US/thunderbird/'
-  version '15.0.1'
-end
