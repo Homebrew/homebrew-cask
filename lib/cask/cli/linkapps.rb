9b2f2c7ecd229e5aef974384b72b7a260cd4783c
phinze <paul.t.hinze@gmail.com>
2012-10-15 18:45:55 -0500

    allow `brew cask linkapps` to accept arguments
    arguments scope linkapps command to just those casks
    
    also added unit tests around the CLI code - whee!

--- lib/cask/cli/linkapps.rb
+++ lib/cask/cli/linkapps.rb
@@ -1,8 +1,9 @@
 class Cask::CLI::Linkapps
   def self.run(*arguments)
-    Cask.installed.map { |app|
+    casks_to_link = arguments.empty? ? Cask.installed : arguments
+    casks_to_link.each do |app|
       Cask.load(app).linkapps
-    }
+    end
   end
 
   def self.help
