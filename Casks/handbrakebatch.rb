b1a229eec1828b2445954495c0343df2f435fd52
autodidakto <autodidakto@gmail.com>
2012-10-15 15:38:54 -0500

    Add handBrakeBatch latest

---
+++ Casks/handbrakebatch.rb
@@ -0,0 +1,5 @@
+class Handbrakebatch < Cask
+  url 'http://www.osomac.com/appcasts/handbrakebatch/HandBrakeBatch.zip'
+  homepage 'http://www.osomac.com/apps/osx/handbrake-batch/'
+  version 'latest'
+end
