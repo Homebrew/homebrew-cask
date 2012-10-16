f1290f53a54aab993840ce5f54910c5d4fcd604e
phinze <paul.t.hinze@gmail.com>
2012-10-15 19:04:40 -0500

    fix constant name for plex media center

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,4 +1,4 @@
-class Plex Media Center < Cask
+class Plex < Cask
   url 'http://plex-downloads.s3.amazonaws.com/plex-laika/0.9.5.4/Plex-0.9.5.4-973998f.zip'
   homepage 'http://plexapp.com'
   version '0.9.5.4'
