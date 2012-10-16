0a22c18a56797e4b329f97bcdf9a31a7faf30cd5
Sean T Allen <sean@monkeysnatchbanana.com>
2012-10-15 22:34:34 -0400

    Add cask for iReadFast.

---
+++ Casks/ireadfast.rb
@@ -0,0 +1,6 @@
+class Ireadfast < Cask
+  url 'http://s5.histats.com/stats/r.php?130163&100&1&urlr=&gengis.110mb.com/downloads/iReadFast%202.0.dmg'
+  homepage 'http://gengis.110mb.com/en/index.php'
+  version '2.0'
+end
+
