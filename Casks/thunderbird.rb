71b0b17c29d66a683db3eb7e384bbf30414ede16
Ben Bailey <bennettbailey@gmail.com>
2012-10-15 19:03:03 -0500

    merged thudnerbird correctly

---
+++ Casks/thunderbird.rb
@@ -0,0 +1,5 @@
+class Thunderbird < Cask
+  url 'http://download.mozilla.org/?product=thunderbird-15.0.1&os=osx&lang=en-US'
+  homepage 'http://www.mozilla.org/en-US/thunderbird/'
+  version '15.0.1'
+end
