4f03ae927435bfa722f05e889229d9438ccb5d57
Dustan Kasten <dustan.kasten@gmail.com>
2012-10-19 10:41:28 -0400

    Add virtualbox for osx

---
+++ Casks/virtualbox.rb
@@ -0,0 +1,6 @@
+class VirtualBox < Cask
+    homepage 'https://www.virtualbox.org/'
+    version '4.2.2'
+    url 'http://download.virtualbox.org/virtualbox/4.2.2/VirtualBox-4.2.2-81494-OSX.dmg'
+end
+
