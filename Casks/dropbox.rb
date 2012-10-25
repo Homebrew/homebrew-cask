5313a71888faaf4677843a74f55f543a2bb2da1c
Sean T Allen <sean@monkeysnatchbanana.com>
2012-10-25 05:35:37 -0400

    Update to latest version of dropbox.

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -1,5 +1,5 @@
 class Dropbox < Cask
-  url 'https://ddr3luum8vl5r.cloudfront.net/Dropbox%201.4.12.dmg'
+  url 'https://ddr3luum8vl5r.cloudfront.net/Dropbox%201.4.20.dmg'
   homepage 'http://www.dropbox.com/'
-  version '1.4.12'
+  version '1.4.20'
 end
