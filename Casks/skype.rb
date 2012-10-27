d5f33c0e052fd12b873d858175c874f65fc19c4e
Shelby Munsch <smunsch@gmail.com>
2012-10-27 07:53:11 -0700

    Updated Skype
    Previous version was giving a 404

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,5 +1,5 @@
 class Skype < Cask
     homepage 'http://www.skype.com'
-    version '5.6.0.143'
-    url 'http://download.skype.com/macosx/Skype_5.6.0.143.dmg'
+    version '6.0.60.2946'
+    url 'http://download.skype.com/macosx/Skype_6.0.60.2946.dmg'
 end
