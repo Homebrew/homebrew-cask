1066c91ddf2f8b0afd7c8de542032955860abe06
Nolan Nichols <nolan.nichols@gmail.com>
2012-11-12 14:34:16 -0800

    fixed camelcase naming of cask class

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,4 +1,4 @@
-class VirtualBox < Cask
+class Virtualbox < Cask
     homepage 'http://www.virtualbox.org'
     version '4.2.4.81684'
     url 'http://download.virtualbox.org/virtualbox/4.2.4/VirtualBox-4.2.4-81684-OSX.dmg'
