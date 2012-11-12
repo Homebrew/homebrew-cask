6904c3de6814d99051ddb1f8618cba74bd0aac5e
Nolan Nichols <nolan.nichols@gmail.com>
2012-11-11 23:25:37 -0800

    add support for VirtualBox

---
+++ Casks/virtualbox.rb
@@ -0,0 +1,5 @@
+class VirtualBox < Cask
+    homepage 'http://www.virtualbox.org'
+    version '4.2.4.81684'
+    url 'http://download.virtualbox.org/virtualbox/4.2.4/VirtualBox-4.2.4-81684-OSX.dmg'
+end
