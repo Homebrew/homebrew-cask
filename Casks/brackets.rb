ea0fd673619217a4bc2d1921c71754f51a3d606d
Kevin Ridgway <kridgway@gmail.com>
2012-11-15 10:58:33 -0500

    Update Casks/brackets.rb version number as well.
    Update Casks/brackets.rb version number as well.

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,5 +1,5 @@
 class Brackets < Cask
   url 'https://github.com/downloads/adobe/brackets/brackets-sprint-16-MAC.dmg'
   homepage 'http://brackets.io'
-  version 'sprint-15'
+  version 'sprint-16'
 end
