b2b4125c494ec46d02622004699616cc622f8cff
phinze <paul.t.hinze@gmail.com>
2012-11-18 23:19:06 -0600

    add content_length for menu-bar-filter

--- Casks/menu-bar-filter.rb
+++ Casks/menu-bar-filter.rb
@@ -2,4 +2,5 @@ class MenuBarFilter < Cask
   url 'https://github.com/downloads/wez/MenuBarFilter/MenuBarFilter_1.2.2.zip'
   homepage 'http://wez.github.com/MenuBarFilter/'
   version '1.2.2'
+  content_length '501304'
 end
