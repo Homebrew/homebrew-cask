631ae86b42aed6f0415febf562323d1bfb487eee
phinze <paul.t.hinze@gmail.com>
2012-11-19 16:37:04 -0600

    add content_length to intellij casks
    now they pass a brew cask audit

--- Casks/intellij-community.rb
+++ Casks/intellij-community.rb
@@ -2,4 +2,5 @@ class IntellijCommunity < Cask
   url 'http://download.jetbrains.com/idea/ideaIC-11.1.4.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
   version '11.1.4'
+  content_length '113789795'
 end
--- Casks/intellij-ultimate.rb
+++ Casks/intellij-ultimate.rb
@@ -2,4 +2,5 @@ class IntellijUltimate < Cask
   url 'http://download.jetbrains.com/idea/ideaIU-11.1.4.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
   version '11.1.4'
+  content_length '198099319'
 end
