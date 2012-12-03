55e6b8acf9c5e8fa0fd4b480dab2a00283501ccc
Phil Tang <tangphillip@gmail.com>
2012-12-02 19:08:02 -0500

    Add a Cask for Disk Inventory X

---
+++ Casks/disk-inventory-x.rb
@@ -0,0 +1,6 @@
+class DiskInventoryX < Cask
+  url 'http://www.derlien.com/download.php?file=DiskInventoryX'
+  homepage 'http://www.derlien.com/'
+  version 'latest'
+  content_length '2969560'
+end
