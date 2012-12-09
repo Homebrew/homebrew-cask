efcc84692213ef6eacec103203a2e185a45b6f6e
phinze <paul.t.hinze@gmail.com>
2012-12-09 13:40:41 -0600

    add mu-commander 0.9.0

---
+++ Casks/mu-commander.rb
@@ -0,0 +1,6 @@
+class MuCommander < Cask
+  url 'http://www.mucommander.com/download/mucommander-0_9_0.dmg'
+  version '0.9.0'
+  homepage 'http://www.mucommander.com/index.php'
+  content_length '4673174'
+end
