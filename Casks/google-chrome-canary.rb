2d965082975be36f46c1481cefe08e4ceaa20974
Félix Saparelli <me@passcod.net>
2012-12-11 13:55:09 +1300

    Add a few install statements

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,6 +1,8 @@
 class GoogleChromeCanary < Cask
   homepage 'https://tools.google.com/dlpage/chromesxs'
+
   edge do
     url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
+    install :app => 'Google Chrome Canary.app'
   end
 end
--- Casks/vlc.rb
+++ Casks/vlc.rb
@@ -2,4 +2,5 @@ class Vlc < Cask
   homepage 'http://www.videolan.org/vlc/'
   url 'http://downloads.sourceforge.net/project/vlc/2.0.3/macosx/vlc-2.0.3.dmg'
   sha1 'fb49e0da2584eb87abf7f8a93961cb86aa85c579'
+  install :app => 'VLC.app'
 end
