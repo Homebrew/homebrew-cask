3b444febced247cee297530f38d4e936edcfc2e0
phinze <paul.t.hinze@gmail.com>
2012-12-17 17:01:58 -0700

    update firefox and firefox-aurora
    thanks @passcod for this

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,6 +1,6 @@
 class FirefoxAurora < Cask
-  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-18.0a2.en-US.mac.dmg'
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-19.0a2.en-US.mac.dmg'
   homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
-  version '18.0a2'
   content_length '49254741'
+  version '19.0a2'
 end
--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,6 +1,6 @@
 class Firefox < Cask
-  url 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/16.0.1/mac/en-US/Firefox%2016.0.1.dmg'
+  url 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.1/mac/en-US/Firefox%2017.0.1.dmg'
   homepage 'http://www.mozilla.org/en-US/firefox/'
-  version '16.0.1'
   content_length '34958026'
+  version '17.0.1'
 end
