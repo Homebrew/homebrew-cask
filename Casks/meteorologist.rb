bf0fa682ce201e689886c66dbe8205a770cb5cf5
Matt Behrens <askedrelic@gmail.com>
2012-12-22 23:11:45 -0800

    add Meteorologist 1.6.1 (menu/dock based weather checking program)

---
+++ Casks/meteorologist.rb
@@ -0,0 +1,6 @@
+class Meteorologist < Cask
+  url 'http://downloads.sourceforge.net/heat-meteo/Meteorologist-1.6.1.dmg'
+  homepage 'http://heat-meteo.sourceforge.net/'
+  version '1.6.1'
+  sha1 '780761a786355ed540ae01fca25c956b029f37bc'
+end
