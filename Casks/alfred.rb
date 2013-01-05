3b5b2f21d800c7bfacfcbc99e4eab3990d84505b
Mikko Kouhia <mikko.kouhia@iki.fi>
2013-01-05 22:21:35 +0200

    Alfred 1.3.2
    Closes #127.
    
    Signed-off-by: Félix Saparelli <me@passcod.net>

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,7 +1,7 @@
 class Alfred < Cask
-  url 'http://cachefly.alfredapp.com/alfred_1.3.1_261.dmg'
+  url 'http://cachefly.alfredapp.com/alfred_1.3.2_265.zip'
   homepage 'http://www.alfredapp.com/'
-  version '1.3.1_261'
-  content_length '2706582'
-  sha1 '92bc216c0b0e1e49d4b98c33971274848ccbb4e1'
+  version '1.3.2_265'
+  content_length '2385318'
+  sha1 'e10f15a0ae758cae2a22557f14890fa5989260ce'
 end
