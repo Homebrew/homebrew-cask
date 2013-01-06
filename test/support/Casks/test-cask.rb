d36b9ffdda9a5e511f2b2cb3c3052261f71b2d9a
Mikko Kouhia <mikko.kouhia@iki.fi>
2013-01-06 13:46:04 +0200

    Update test cask
    Test cask previously contained only attributes `url`, `homepage` and
    `version`.  Add also `content_length` and `sha1`.

--- test/support/Casks/test-cask.rb
+++ test/support/Casks/test-cask.rb
@@ -2,4 +2,6 @@ class TestCask < Cask
   url 'http://example.com/TestCask.dmg'
   homepage 'http://example.com/'
   version '1.2.3'
+  content_length '123456'
+  sha1 '0123456789012345678901234567890123456789'
 end
