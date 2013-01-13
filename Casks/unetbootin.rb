9566d85a16abb28755cc2c23d86d61a9c8d72d0d
phinze <paul.t.hinze@gmail.com>
2013-01-13 12:51:30 -0600

    add cask - unetbootin

---
+++ Casks/unetbootin.rb
@@ -0,0 +1,6 @@
+class Unetbootin < Cask
+  url 'http://unetbootin.sourceforge.net/unetbootin-mac-latest.zip'
+  homepage 'http://unetbootin.sourceforge.net/'
+  version 'latest'
+  sha1 'afdf677dc7fa6668a4772c25ce279c91900df86a'
+end
