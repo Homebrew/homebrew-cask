a6b5a7bcea75df9196f4578b71a991af43291a81
phinze <paul.t.hinze@gmail.com>
2013-01-19 14:08:01 -0600

    add cask for Paparazzi screenshot app

---
+++ Casks/paparazzi.rb
@@ -0,0 +1,7 @@
+class Paparazzi < Cask
+  url 'http://derailer.org/paparazzi/Paparazzi!%200.6.5.dmg'
+  homepage 'http://derailer.org/paparazzi/'
+  version '0.6.5'
+  content_length '3920031'
+  sha1 '3f7e7f0cd6fc498c04a0122f75e8874b7c69bbf7'
+end
