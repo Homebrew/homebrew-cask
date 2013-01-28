4480926b5908281a74cafdf9815e8eca1a83f44b
Vítor Galvão <info@vitorgalvao.com>
2013-01-28 13:44:37 +0000

    Keka 1.0.3
    Signed-off-by: Felix Saparelli <me@passcod.net>

---
+++ Casks/keka.rb
@@ -0,0 +1,7 @@
+class Keka < Cask
+  url 'http://www.kekaosx.com/release/Keka-1.0.3-intel.dmg'
+  homepage 'http://kekaosx.com/'
+  version '1.0.3'
+  content_length '16001364'
+  sha1 '80ef6d5cd65a8c5e8ec8e29478ee1013b79a9aa0'
+end
