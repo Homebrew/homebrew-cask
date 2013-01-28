c611687723a737dddeaccf6771d6c9d1a42a2ea1
Vítor Galvão <info@vitorgalvao.com>
2013-01-28 15:05:23 +0000

    Revert "corrected github app cask"
    This reverts commit 6af0da862db16fd02786d7287ec8158fca6a340c.

--- Casks/github.rb
+++ Casks/github.rb
@@ -1,7 +1,7 @@
 class Github < Cask
-  url 'https://github-central.s3.amazonaws.com/mac%2FGitHub%20for%20Mac%20103.zip'
+  url 'https://central.github.com/mac/latest'
   homepage 'http://mac.github.com'
-  version 'Base Camp - January 11th 2013'
-  content_length '10343558'
-  sha1 'b7695581f4412884c1c06e5e7616c856f194bd10'
+  version 'latest'
+  content_length '10061271'
+  no_checksum
 end
