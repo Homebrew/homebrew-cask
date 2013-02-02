30417c1963ff9aa5765638283bf2f16e0c662725
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 22:02:53 +0000

    Added cfxr cask

---
+++ Casks/cfxr.rb
@@ -0,0 +1,7 @@
+class cfxr < Cask
+  url 'http://github.com/downloads/nevyn/cfxr/cfxr%200.2.1.zip'
+  homepage 'http://thirdcog.eu/apps/cfxr'
+  version '0.2.1'
+  content_length '178'
+  sha1 '81ffbd1712afe8cdf138b570c0fc9934742c33c1'
+end
