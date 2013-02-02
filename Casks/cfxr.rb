64535dcf43b92573145bb22ff6a339c0b4bfdfb7
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 22:02:53 +0000

    cfxr 0.2.1
    Signed-off-by: Felix Saparelli <me@passcod.name>

---
+++ Casks/cfxr.rb
@@ -0,0 +1,7 @@
+class Cfxr < Cask
+  url 'http://github.com/downloads/nevyn/cfxr/cfxr 0.2.1.zip'
+  homepage 'http://thirdcog.eu/apps/cfxr'
+  version '0.2.1'
+  content_length '756001'
+  sha1 'faf9b03f17f7c331b136cd49202d4a0c9c6bb23a'
+end
