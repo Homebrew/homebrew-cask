63f8ed1b2e648adc5f7709cd5853306e011a2b57
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 22:11:20 +0000

    Added WavTap cask

---
+++ Casks/wavtap.rb
@@ -0,0 +1,7 @@
+class WavTap < Cask
+  url 'https://github.com/downloads/pje/WavTap/WavTap%200.2.0.pkg'
+  homepage 'https://github.com/pje/wavtap'
+  version '0.2.0'
+  content_length '127'
+  sha1 'a22b0756b403b94ce890069c8a6317d1691beaf4'
+end
