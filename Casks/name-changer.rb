b0928e0315eb6fab4d8c5b17c307751493c98659
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 22:19:02 +0000

    NameChanger 2.3.3
    Signed-off-by: Felix Saparelli <me@passcod.name>

---
+++ Casks/name-changer.rb
@@ -0,0 +1,7 @@
+class NameChanger < Cask
+  url 'http://www.mrrsoftware.com/Downloads/NameChanger/Updates/NameChanger-2_3_3.zip'
+  homepage 'http://www.mrrsoftware.com/MRRSoftware/NameChanger.html'
+  version '2.3.3'
+  content_length '2065341'
+  sha1 'b9af8add259a0055a3748aeddfd56984a82cb6f2'
+end
