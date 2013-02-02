98281919ad943cf138cf4ec5db46731f225f65e8
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 21:44:25 +0000

    Dashlane 1.7.5.28074
    Signed-off-by: Felix Saparelli <me@passcod.name>

---
+++ Casks/dashlane.rb
@@ -0,0 +1,7 @@
+class Dashlane < Cask
+  url 'https://d3mfqat9ni8wb5.cloudfront.net/releases/1.7.5/1.7.5.28074/Dashlane.dmg'
+  homepage 'https://www.dashlane.com/'
+  version '1.7.5.28074'
+  content_length '38517522'
+  sha1 'a79c49e7776c4b5da15f36cbbdc979dd21f24779'
+end
