e668b9c60f432720408a5aea144459a2d853cc5b
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 21:52:46 +0000

    XtraFinder
    Version at time of commit: 0.14.3
    
    Signed-off-by: Felix Saparelli <me@passcod.name>

---
+++ Casks/xtra-finder.rb
@@ -0,0 +1,7 @@
+class XtraFinder < Cask
+  url 'http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg'
+  homepage 'http://www.trankynam.com/xtrafinder/'
+  version 'latest'
+  no_content_length
+  no_checksum
+end
