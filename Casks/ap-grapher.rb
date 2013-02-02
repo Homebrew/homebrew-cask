ea5d41297ea785808109615eaab1da1176d6edf0
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 22:16:26 +0000

    AP Grapher
    Version at time of commit: 1.2.1
    
    Signed-off-by: Felix Saparelli <me@passcod.name>

---
+++ Casks/ap-grapher.rb
@@ -0,0 +1,7 @@
+class ApGrapher < Cask
+  url 'http://www.chimoosoft.com/downloads/current/APGrapher.dmg'
+  homepage 'http://www.chimoosoft.com/products/apgrapher/'
+  version 'latest'
+  no_content_length
+  no_checksum
+end
