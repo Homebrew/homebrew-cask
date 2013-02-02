0693a93435f29b71fc56eb2a25d84c3e8a31e02d
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 22:11:20 +0000

    WavTap 0.2.0
    Signed-off-by: Felix Saparelli <me@passcod.name>

---
+++ Casks/wav-tap.rb
@@ -0,0 +1,7 @@
+class WavTap < Cask
+  url 'https://github.com/downloads/pje/WavTap/WavTap 0.2.0.pkg'
+  homepage 'https://github.com/pje/wavtap'
+  version '0.2.0'
+  content_length '1139070'
+  sha1 '9d71dcf769dd546d66278dc9cbf675df6b006fe6'
+end
