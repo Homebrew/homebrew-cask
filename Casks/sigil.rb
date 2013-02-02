8d841f72fbb60f0bd96e9fed5bba4c0d6e3755b2
Vítor Galvão <info@vitorgalvao.com>
2013-02-02 22:05:46 +0000

    Sigil 0.6.2
    Signed-off-by: Felix Saparelli <me@passcod.name>

---
+++ Casks/sigil.rb
@@ -0,0 +1,7 @@
+class Sigil < Cask
+  url 'http://sigil.googlecode.com/files/Sigil-0.6.2-Mac-Package.dmg'
+  homepage 'http://code.google.com/p/sigil/'
+  version '0.6.2'
+  content_length '28322867'
+  sha1 '4fe9d60e6a5f2809f2311494e906c72d6fef0755'
+end
