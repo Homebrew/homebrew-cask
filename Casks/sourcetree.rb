5429a3edff6496066186ad5a9fe3109f0ebe7076
Vítor Galvão <info@vitorgalvao.com>
2013-02-03 03:29:42 +0000

    SourceTree 1.5.7.1
    Signed-off-by: Felix Saparelli <me@passcod.name>

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,7 +1,7 @@
 class Sourcetree < Cask
-  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.4.dmg'
+  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.7.1.dmg'
   homepage 'http://www.sourcetreeapp.com/'
-  version '1.5.4'
-  content_length '23812125'
-  sha1 'ea656b78130e05d3edcc01aa805309082be07c97'
+  version '1.5.7.1'
+  content_length '23871155'
+  sha1 'f3cda927b68beb86015b01ad9a85842139287559'
 end
