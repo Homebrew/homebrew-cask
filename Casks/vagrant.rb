f6eb66ecba999fc1ee20b866e743d73daa5a0f03
Jean-Charles Sisk <jasisk@gmail.com>
2013-02-06 16:36:17 -0500

    Vagrant 1.0.6

---
+++ Casks/vagrant.rb
@@ -0,0 +1,7 @@
+class Vagrant < Cask
+  url 'http://files.vagrantup.com/packages/476b19a9e5f499b5d0b9d4aba5c0b16ebe434311/Vagrant.dmg'
+  homepage 'http://www.vagrantup.com'
+  version '1.0.6'
+  content_length '25674666'
+  sha256 '30d3cc92c2376ddf940a30f20ae3fa6d8248b83bd4f810a3050217385b03ead0'
+end
