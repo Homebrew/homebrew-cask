cf9f1d6a8139ea8f12e760fde259377c1321aee1
Félix Saparelli <me@passcod.net>
2013-02-11 22:31:15 +1300

    Brackets sprint-19
    Closes #157.
    
    Signed-off-by: Félix Saparelli <me@passcod.net>

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,7 +1,7 @@
 class Brackets < Cask
-  url 'https://github.com/downloads/adobe/brackets/brackets-sprint-16-MAC.dmg'
+  url 'http://download.brackets.io/file.cfm?platform=OSX&build=19'
   homepage 'http://brackets.io'
-  version 'sprint-16'
-  content_length '25020067'
-  sha1 '94322762ecb00baab857e324a98e543d548bf961'
+  version 'sprint-19'
+  content_length '26145505'
+  sha1 'e08c6ea3b4951a3111c768ee9ac522126190b4d1'
 end
