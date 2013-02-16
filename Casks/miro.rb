e71ab23844bdb877ac4654f3853dc627a01a4918
Vítor Galvão <info@vitorgalvao.com>
2013-02-16 18:59:02 +0000

    Added Miro cask

---
+++ Casks/miro.rb
@@ -0,0 +1,7 @@
+class Miro < Cask
+  url 'http://ftp.osuosl.org/pub/pculture.org/miro/osx/Miro.dmg'
+  homepage 'http://www.getmiro.com/'
+  version '5.0.4'
+  content_length '48854473'
+  sha1 'e752e6ca859d469ff5f4de88b33ebb0d44329b71'
+end
