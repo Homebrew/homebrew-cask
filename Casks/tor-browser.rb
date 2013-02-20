577fb1ed0f8cef413a58afc7fb9eff54fe226f19
Jacob Appelbaum <jacob@appelbaum.net>
2013-02-19 20:35:26 -0800

    TorBrowser 2.3.25-2 and 2.4.9-alpha-1
    Closes #162.
    
    Signed-off-by: Félix Saparelli <me@passcod.name>

---
+++ Casks/tor-browser.rb
@@ -0,0 +1,7 @@
+class TorBrowser < Cask
+  homepage 'https://www.torproject.org/projects/torbrowser.html'
+  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.3.25-2-osx-x86_64-en-US.zip'
+  version '2.3.25-2'
+  content_length '35405646'
+  sha1 'bb36926c46ede3d1f841df42ed288abed365f3d3'
+end
