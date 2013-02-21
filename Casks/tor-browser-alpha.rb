d40f463718e4581c87017efd69ba3f0ef6161943
noe111 <noe111@physics.anu.edu.au>
2013-02-21 16:58:02 +1100

    Fix missing apostrophe

--- Casks/tor-browser-alpha.rb
+++ Casks/tor-browser-alpha.rb
@@ -1,6 +1,6 @@
 class TorBrowserAlpha < Cask
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.4.9-alpha-1-osx-x86_64-en-US.zip
+  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.4.9-alpha-1-osx-x86_64-en-US.zip'
   version '2.4.9-alpha-1'
   content_length '38613261'
   sha1 'f593127548f8ea9b900fb6ed680c5b5c5ace8cef'
