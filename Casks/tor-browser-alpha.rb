d772af39bfac208b574169ed225abb9352204405
Jacob Appelbaum <jacob@appelbaum.net>
2013-02-22 15:19:25 -0800

    TorBrowser Alpha 2.4.10-alpha-2
    Closes #168 (1/2).
    
    Signed-off-by: Félix Saparelli <me@passcod.name>

--- Casks/tor-browser-alpha.rb
+++ Casks/tor-browser-alpha.rb
@@ -1,7 +1,7 @@
 class TorBrowserAlpha < Cask
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.4.9-alpha-1-osx-x86_64-en-US.zip'
-  version '2.4.9-alpha-1'
-  content_length '38613261'
-  sha1 'f593127548f8ea9b900fb6ed680c5b5c5ace8cef'
+  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.4.10-alpha-2-osx-x86_64-en-US.zip'
+  version '2.4.10-alpha-2'
+  content_length '35469516'
+  sha1 'ca8f1a4c68559214c50138a19d7e337ffdc1d331'
 end
