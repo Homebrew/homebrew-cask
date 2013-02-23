679a7043598ded5e69baa582f1d551375168d046
Jacob Appelbaum <jacob@appelbaum.net>
2013-02-22 17:17:20 -0800

    Update Thunderbird from 16.0.1 to 17.0.3esr

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,7 +1,8 @@
 class Thunderbird < Cask
-  url 'http://download.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/16.0.1/mac/en-US/Thunderbird%2016.0.1.dmg'
+  url 'http://download.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/17.0.3esr/mac/en-US/Thunderbird%2017.0.3esr.dmg'
   homepage 'http://www.mozilla.org/en-US/thunderbird/'
-  version '16.0.1'
-  content_length '38435412'
-  sha1 '50ed989c51c171fd6913883b481e00d5a4cff036'
+  version '17.0.3esr'
+  content_length '37475872'
+  # http://download.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/17.0.3esr/SHA1SUMS
+  sha1 '67805f04011db557f3df967e6d3afdcfdb9cd839'
 end
