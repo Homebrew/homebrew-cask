41f462e35072b89957d43090bb9bfcd1fd796b2f
Mikko Kouhia <mikko.kouhia@iki.fi>
2013-03-03 08:01:19 +0200

    New cask: FreeCAD
    Free Computer Assisted Design program

---
+++ Casks/free-cad.rb
@@ -0,0 +1,7 @@
+class FreeCad < Cask
+  url 'http://sourceforge.net/projects/free-cad/files/FreeCAD%20MacOSX/FreeCAD_0.12_MacOSX10.7_20120110_x64.dmg'
+  homepage 'http://sourceforge.net/projects/free-cad/'
+  version '0.12'
+  content_length '391216142'
+  sha1 'f2629f5d113e7bd59b27364d9940d857b0433359'
+end
