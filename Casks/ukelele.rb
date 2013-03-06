ca0e1d33c5eb804050c305e34b942093d27fe3ec
Vítor Galvão <info@vitorgalvao.com>
2013-03-06 15:46:43 +0000

    Added ukelele cask

---
+++ Casks/ukelele.rb
@@ -0,0 +1,7 @@
+class Ukelele < Cask
+  url 'http://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Ukelele_2.2.4&filename=Ukelele_2.2.4.dmg'
+  homepage 'http://scripts.sil.org/ukelele'
+  version '2.2.4'
+  content_length '9788023'
+  sha1 'f322ccfbc5b2efb24d0d5cf902dfd0ae552322e5'
+end
