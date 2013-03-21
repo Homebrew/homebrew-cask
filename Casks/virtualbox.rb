7446e1776639c2bcf1be2647b1e7ad3901b3f868
Stefan Schmidt <stefan.schmidt@gmx.net>
2013-03-21 16:21:19 +0100

    Updated VirtualBox to 4.2.10-84104

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,7 +1,7 @@
 class Virtualbox < Cask
   homepage 'http://www.virtualbox.org'
-  version '4.2.6.82870'
-  url 'http://download.virtualbox.org/virtualbox/4.2.6/VirtualBox-4.2.6-82870-OSX.dmg'
-  content_length '109050921'
-  sha1 '7053bba27409cd9fbb661962bc9605bf3ce7002a'
+  version '4.2.10-84104'
+  url 'http://download.virtualbox.org/virtualbox/4.2.10/VirtualBox-4.2.10-84104-OSX.dmg'
+  content_length '109022269'
+  sha1 'e4d016587426c1ef041bd460905b5385e566fe77'
 end
