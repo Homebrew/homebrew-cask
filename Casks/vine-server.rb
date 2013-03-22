13b38a7bbefdd6a547f31a9524daff1803c9942c
phinze <paul.t.hinze@gmail.com>
2013-03-22 10:40:48 -0500

    add cask for vine-server
    an OS X VNC server

---
+++ Casks/vine-server.rb
@@ -0,0 +1,6 @@
+class VineServer < Cask
+  homepage 'http://www.testplant.com/products/vine/vine-server/'
+  url 'http://www.testplant.com/downloads/Vine/VineServer4.01.dmg'
+  version '4.01'
+  sha1 '6d2d3c441147d1eab2ff76d327d5a5e55aa56ba1'
+end
