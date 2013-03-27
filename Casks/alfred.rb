f7d9ad09dc8479fad20c7d9ff60496e1798cda96
Vítor Galvão <info@vitorgalvao.com>
2013-03-27 00:42:45 +0000

    removed content_length

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -2,6 +2,5 @@ class Alfred < Cask
   url 'http://cachefly.alfredapp.com/Alfred_2.0.1_173.zip'
   homepage 'http://www.alfredapp.com/'
   version '2.0.1_173'
-  content_length '2869071'
   sha1 '3b5e59e799d7f42bc73fbd425e85b6154608a98b'
 end
