c7e5fadff6b6387a4dde213c2c40549fb0953722
Félix Saparelli <me@passcod.name>
2013-03-29 12:18:29 +1300

    Brackets sprint-22

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,6 +1,6 @@
 class Brackets < Cask
-  url 'http://download.brackets.io/file.cfm?platform=OSX&build=21'
+  url 'http://download.brackets.io/file.cfm?platform=OSX&build=22'
   homepage 'http://brackets.io'
-  version 'sprint-21'
-  sha1 '1849a894e18596d142e117f83b8a100c636ecc8a'
+  version 'sprint-22'
+  sha1 '415213e18b299fadd7300ff4829d16c8ac83c2c8'
 end
