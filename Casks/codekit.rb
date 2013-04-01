feda71a325ff82e07b11241096dd779e6b939a9c
Vítor Galvão <info@vitorgalvao.com>
2013-04-01 13:45:36 +0100

    Added codekit cask

---
+++ Casks/codekit.rb
@@ -0,0 +1,6 @@
+class Codekit < Cask
+  url 'http://incident57.com/codekit/files/codekit-8283.zip'
+  homepage 'http://incident57.com/codekit/'
+  version '1.6.1 (8283)'
+  sha1 'c0cd90916473aba28eed288f6dd8eaf57d0953a9'
+end
