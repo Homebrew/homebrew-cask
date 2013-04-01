d6ba5953da36663d4b87f64a924fb7a9169863dd
Vítor Galvão <info@vitorgalvao.com>
2013-04-01 13:30:38 +0100

    Added instashare cask

---
+++ Casks/instashare.rb
@@ -0,0 +1,6 @@
+class Instashare < Cask
+  url 'http://instashareapp.com/files/InstashareB6.zip'
+  homepage 'http://instashareapp.com/'
+  version 'Beta 6'
+  sha1 '2d59eae196539ec02f2a870469c2239bf27362aa'
+end
