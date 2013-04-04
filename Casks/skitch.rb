de503ec198c3b973d85217fc619562d23809978c
phinze <paul.t.hinze@gmail.com>
2013-04-04 17:36:35 -0600

    add skitch cask

---
+++ Casks/skitch.rb
@@ -0,0 +1,6 @@
+class Skitch < Cask
+  url 'http://cdn1.evernote.com/skitch/mac/release/Skitch-2.0.5.zip'
+  homepage 'http://evernote.com/skitch/'
+  version '2.0.5'
+  sha1 '64d61e76eb8bd2540f84b5d21ee1ac5f93c74a61'
+end
