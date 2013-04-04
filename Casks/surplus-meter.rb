b382826157b94e6d6398778f58126a1e5cd22d90
phinze <paul.t.hinze@gmail.com>
2013-04-04 16:34:10 -0600

    add surplus-meter cask

---
+++ Casks/surplus-meter.rb
@@ -0,0 +1,6 @@
+class SurplusMeter < Cask
+  url 'http://www.skoobysoft.com/downloads/SurplusMeterv203.dmg'
+  homepage 'http://www.skoobysoft.com/utilities/utilities.html#surplusmeter'
+  version '2.0.3'
+  sha1 '737fea8d51f1879f85ba65d4081808e023234445'
+end
