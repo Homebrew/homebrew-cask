c348ac1ddda78fdfc7d32f1329807126fcef6108
Mike Fiedler <miketheman@gmail.com>
2013-04-06 07:45:32 -0400

    removed md5, sha256

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -2,7 +2,5 @@ class Dropbox < Cask
   url 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.0.5.dmg'
   homepage 'http://www.dropbox.com/'
   version '2.0.5'
-  md5 '161bf3d427b5cf0c6297653f09eed7be'
   sha1 '6e88e76e3787de6c4367da0f0a1b1427c6984bd6'
-  sha256 '1169d604ed3db8b15449753f7cf9c5af2b86afefcf598231d44dc29551c4a489'
 end
