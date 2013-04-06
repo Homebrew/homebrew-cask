77aa07daf78a46ca8fe5ddef74b63bad850f019b
Mike Fiedler <miketheman@gmail.com>
2013-04-06 07:01:51 -0400

    updated Dropbox to version 2.0.5
    Added md5, sha256 to cask

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -1,6 +1,8 @@
 class Dropbox < Cask
-  url 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.0.0.dmg'
+  url 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.0.5.dmg'
   homepage 'http://www.dropbox.com/'
-  version '2.0'
-  sha1 '80ee4ea6b34ffe1220430aea5e7ed035013850aa'
+  version '2.0.5'
+  md5 '161bf3d427b5cf0c6297653f09eed7be'
+  sha1 '6e88e76e3787de6c4367da0f0a1b1427c6984bd6'
+  sha256 '1169d604ed3db8b15449753f7cf9c5af2b86afefcf598231d44dc29551c4a489'
 end
