24bcf494543bdd9ad3db3c3f1cf7300d807f6584
Miha Filej <miha@filej.net>
2013-04-07 20:54:11 +0200

    Add AirServer cask

---
+++ Casks/air-server.rb
@@ -0,0 +1,6 @@
+class AirServer < Cask
+  url 'http://dl.airserver.com/mac/AirServer-4.6.4.dmg'
+  homepage 'http://www.airserver.com'
+  version '4.6.4'
+  sha1 '3c80e2110bcdf4e46ec9809c141b5367a80d6365'
+end
