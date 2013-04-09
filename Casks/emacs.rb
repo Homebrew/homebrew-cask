2efd99374dd26c30c07861ea0519d05ef19647aa
Eduardo Kasa <eduardokasa@gmail.com>
2013-04-09 20:02:25 -0300

    emacs.rb: Update to version 24.3

--- Casks/emacs.rb
+++ Casks/emacs.rb
@@ -1,6 +1,6 @@
 class Emacs < Cask
-  url 'http://bandwidth.porkrind.org/emacs-builds/Emacs-24.2-universal-10.6.8.dmg'
+  url 'http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg'
   homepage 'http://emacsformacosx.com/'
-  version '24.2'
-  sha1 '0f5d368420d410ab395d7aedcf63f5cc6e6aaf85'
+  version '24.3'
+  sha1 '1ee96baa50806b255d2114549acf2e6710949213'
 end
