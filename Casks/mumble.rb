e6f51e74986738621000027ba1e5b94421eac8b8
Miha Filej <miha@filej.net>
2013-04-10 10:51:27 +0200

    Add Mumble cask

---
+++ Casks/mumble.rb
@@ -0,0 +1,6 @@
+class Mumble < Cask
+  url 'http://ignum.dl.sourceforge.net/project/mumble/Mumble/1.2.3a/Mumble-1.2.3a.dmg'
+  homepage 'http://mumble.sourceforge.net'
+  version '1.2.3a'
+  sha1 '5831f6bcd630c338a898cf67b93ab6d083d0dc41'
+end
