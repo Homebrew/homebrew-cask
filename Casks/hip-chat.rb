f4392ea289d0a874f82f9c41517ed700df3188ed
Michael Warkentin <mwarkentin@gmail.com>
2013-04-14 14:46:30 -0400

    Add cask for HipChat for Mac beta

---
+++ Casks/hip-chat.rb
@@ -0,0 +1,6 @@
+class HipChat < Cask
+  url 'http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.30-01uennzomg01wmg.zip'
+  homepage 'https://www.hipchat.com/'
+  version '0.30'
+  sha1 'df00f998e1b534d2ff6f1e9e36419bb5c2a4f2c6'
+end
