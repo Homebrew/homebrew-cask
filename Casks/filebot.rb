d856f5fe37ce2a80a1cf77520b2b02040278b0c0
Vince Cima <contact@vincecima.com>
2013-04-15 00:48:53 -0500

    Add Filebot starting with version 3.6

---
+++ Casks/filebot.rb
@@ -0,0 +1,6 @@
+class Filebot < Cask
+  url 'http://downloads.sourceforge.net/project/filebot/filebot/FileBot_3.6/FileBot_3.6.app.tar.gz'
+  homepage 'http://www.filebot.net/'
+  version '3.6'
+  sha1 'ad492132226c0f6eb019a5dc62e59449cd5d98cd'
+end
