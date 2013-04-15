f79b7efc3e5590392a6383d8c3920fb79a1cb4f7
Michael Warkentin <mwarkentin@gmail.com>
2013-04-15 00:48:44 -0400

    Add cask for Sublime Text 3 beta

---
+++ Casks/sublime-text-3.rb
@@ -0,0 +1,6 @@
+class SublimeText3 < Cask
+  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203021.dmg'
+  homepage 'http://www.sublimetext.com/3'
+  version '3021'
+  sha1 '3abe7fb609d3553a688ddc3ebd8d891a3f2ba189'
+end
