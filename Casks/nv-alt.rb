94c1fcc3c6e94e7ac4de3410c8384065c077324e
Vítor Galvão <info@vitorgalvao.com>
2013-04-16 16:12:14 +0100

    Bumped nv-alt to 2.2b105

--- Casks/nv-alt.rb
+++ Casks/nv-alt.rb
@@ -1,6 +1,6 @@
 class NvAlt < Cask
-  url 'http://abyss.designheresy.com/nvaltb/files/nvalt2.2b101.zip'
+  url 'http://abyss.designheresy.com/nvalt/files/nvalt2.2b105.zip'
   homepage 'http://brettterpstra.com/project/nvalt/'
-  version '2.2b101'
-  sha1 'f31fa6ea90a6d551a905d5dcd9ee65461d788c38'
+  version '2.2b105'
+  sha1 '2b70957942c233240338eaf679ebbd5d6089f5d3'
 end
