3a8ec24806c54bf4987a62b5381a112f0751c893
Hok Shun Poon <hok@hokshunpoon.me>
2013-04-16 15:43:38 +0100

    Add p4merge cask.

---
+++ Casks/p4merge.rb
@@ -0,0 +1,6 @@
+class P4merge < Cask
+  url 'http://www.perforce.com/downloads/perforce/r13.1/bin.macosx106x86_64/P4V.dmg'
+  homepage 'http://www.perforce.com/product/components/perforce-visual-merge-and-diff-tools'
+  version 'latest'
+  no_checksum
+end
