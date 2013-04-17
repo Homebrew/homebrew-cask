e51fa1179a027ad72ee0f31f01c6352dbee44d6c
Matthew Finlayson <matthew.finlayson@jivesoftware.com>
2013-04-17 11:09:31 -0700

    Add cask for CoRD

---
+++ Casks/cord.rb
@@ -0,0 +1,6 @@
+class Cord < Cask
+  url 'http://sourceforge.net/projects/cord/files/cord/0.5.7/CoRD_0.5.7.zip/download'
+  homepage 'http://cord.sourceforge.net/'
+  version '0.5.7'
+  sha1 '8a25f6efa9d3d16dcd71a7d47fac5c50c6df8796'
+end
