7fbf5ca3ecfa0aacacf2cbf09776abb3b6579e77
Matthew Finlayson <matthew.finlayson@jivesoftware.com>
2013-04-17 13:55:41 -0700

    Correcting sourceforge download url

--- Casks/cord.rb
+++ Casks/cord.rb
@@ -1,5 +1,5 @@
 class Cord < Cask
-  url 'http://sourceforge.net/projects/cord/files/cord/0.5.7/CoRD_0.5.7.zip/download'
+  url 'http://surfnet.dl.sourceforge.net/project/cord/cord/0.5.7/CoRD_0.5.7.zip'
   homepage 'http://cord.sourceforge.net/'
   version '0.5.7'
   sha1 '8a25f6efa9d3d16dcd71a7d47fac5c50c6df8796'
