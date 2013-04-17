ca57c42a6fe2d3c1248e60699ce2df5fad0ff218
Matthew Finlayson <matthew.finlayson@jivesoftware.com>
2013-04-17 14:20:35 -0700

    Fixing chicken sf link and version
    The version number was off, corrected download link, version,
    and sha.

--- Casks/chicken.rb
+++ Casks/chicken.rb
@@ -1,6 +1,6 @@
 class Chicken < Cask
-  url 'http://sourceforge.net/projects/chicken/files/Chicken-2.1.1/Chicken-2.1.1.dmg/download'
+  url 'http://downloads.sourceforge.net/project/chicken/Chicken-2.2b2.dmg'
   homepage 'http://sourceforge.net/projects/chicken/'
-  version '2.1.1'
-  sha1 '5de2812a2701d799af818a1982b3176cb8c90ae1'
+  version '2.2b2'
+  sha1 'a56b2bdeba9dce7a225f886b401bb7405cd91948'
 end
