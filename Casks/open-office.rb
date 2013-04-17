1171efe2162ba55d9284fc8b6af67dda6b7a5d0f
Matthew Finlayson <matthew.finlayson@jivesoftware.com>
2013-04-17 16:19:26 -0700

    Fixing sf download links
    Chromium, free-cad, mumble, open-office, seashore, and unetbootin
    were all using url's that ended in download rather than the
    filename. This has been changed, sha's are preserved.
    
    Mumble does not have a 'latest' download alias in the new url
    scheme. It does however specifiy a version so the Cask was updated
    to reflect this.
    
    Closes #229
    
    Signed-off-by: phinze <paul.t.hinze@gmail.com>

--- Casks/open-office.rb
+++ Casks/open-office.rb
@@ -1,5 +1,5 @@
 class OpenOffice < Cask
-  url 'http://sourceforge.net/projects/openofficeorg.mirror/files/stable/3.4.1/Apache_OpenOffice_incubating_3.4.1_MacOS_x86_install_en-US.dmg/download'
+  url 'http://downloads.sourceforge.net/sourceforge/openofficeorg.mirror/Apache_OpenOffice_incubating_3.4.1_MacOS_x86_install_en-US.dmg'
   homepage 'http://www.openoffice.org/'
   version '3.4.1'
   sha1 '63623a1cc77bfc3c0fe207c317ecd087eded3016'
