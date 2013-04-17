713f66eab6e13e6ddb5226b555b7c663d6da1bf9
Vítor Galvão <info@vitorgalvao.com>
2013-04-17 03:52:22 +0100

    updated macvim to 7.3-66

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,6 +1,6 @@
 class Macvim < Cask
-  url 'https://github.com/downloads/b4winckler/macvim/MacVim-snapshot-65.tbz'
+  url 'https://macvim.googlecode.com/files/MacVim-snapshot-66.tbz'
   homepage 'http://code.google.com/p/macvim/'
-  version '7.3-65'
-  sha1 '30f58b03127a7b5f9e2c23fd31d3749e8f3e2197'
+  version '7.3-66'
+  sha1 'b2d28f7a33ac8eb4bbf899252b0d30669bd21406'
 end
