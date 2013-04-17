b152cc2a9d9b5c443c1b08fc47efd0124a41a7b2
Vítor Galvão <info@vitorgalvao.com>
2013-04-17 14:13:08 +0100

    Added ImageAlpha

---
+++ Casks/imagealpha.rb
@@ -0,0 +1,6 @@
+class Imagealpha < Cask
+  url 'http://pngmini.com/ImageAlpha1.2.5.1.tar.bz2'
+  homepage 'http://pngmini.com/'
+  version '1.2.5.1'
+  sha1 '46672dac88acf5c91d35b74757b1d636048da7a6'
+end
