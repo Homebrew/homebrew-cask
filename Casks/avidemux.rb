4bf6d3d63500aa82baa39412e4bde99920d7e238
Vítor Galvão <info@vitorgalvao.com>
2013-04-17 14:31:58 +0100

    Added avidemux cask

---
+++ Casks/avidemux.rb
@@ -0,0 +1,6 @@
+class Avidemux < Cask
+  url 'http://downloads.sourceforge.net/project/avidemux/avidemux/2.6.3/Avidemux_2.6.3_ml_64bits.dmg'
+  homepage 'http://www.avidemux.org/'
+  version '2.6.3'
+  sha1 'e6b1d24d7f4f98a9bfa7079164eb9ca51fce6899'
+end
