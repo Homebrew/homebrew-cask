36d7a4af87dda56c2c58370cd5a02d0385e21502
Matthew Finlayson <matthew.finlayson@jivesoftware.com>
2013-04-17 16:56:34 -0700

    Fixing sf link for CoRD

--- Casks/cord.rb
+++ Casks/cord.rb
@@ -1,5 +1,5 @@
 class Cord < Cask
-  url 'http://surfnet.dl.sourceforge.net/project/cord/cord/0.5.7/CoRD_0.5.7.zip'
+  url 'http://downloads.sourceforge.net/sourceforge/cord/CoRD_0.5.7.zip'
   homepage 'http://cord.sourceforge.net/'
   version '0.5.7'
   sha1 '8a25f6efa9d3d16dcd71a7d47fac5c50c6df8796'
