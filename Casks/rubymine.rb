4a8487e189f66bf14ecbc875990e2a78a8cc4ebd
Cody Cutrer <cody@cutrer.us>
2013-04-19 10:48:24 -0600

    Update rubymine.rb

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -1,6 +1,6 @@
 class Rubymine < Cask
-  url 'http://download.jetbrains.com/ruby/RubyMine-5.4.dmg'
+  url 'http://download.jetbrains.com/ruby/RubyMine-5.4.1.dmg'
   homepage 'http://www.jetbrains.com/ruby/'
-  version '5.4'
-  sha1 'a0a53589d78f4c962f8d1eee9b3f069c0c815975'
+  version '5.4.1'
+  sha1 '10f8e8250d040bf173859dbfa24da068795d8fb2'
 end
