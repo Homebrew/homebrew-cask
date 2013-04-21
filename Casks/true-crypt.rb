3786070666d2f2534d41947977c71689859a64f1
Vítor Galvão <info@vitorgalvao.com>
2013-04-21 14:30:18 +0100

    Added TrueCrypt 7.1a

---
+++ Casks/true-crypt.rb
@@ -0,0 +1,6 @@
+class TrueCrypt < Cask
+  url 'http://www.truecrypt.org/download/TrueCrypt%207.1a%20Mac%20OS%20X.dmg'
+  homepage 'http://truecrypt.org/'
+  version '7.1a'
+  sha1 '16e6d7675d63fba9bb75a9983397e3fb610459a1'
+end
