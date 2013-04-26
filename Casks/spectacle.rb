683dd87f60811f29b57b31480adc65d08fdb8865
Vítor Galvão <info@vitorgalvao.com>
2013-04-26 14:40:20 +0100

    bumped to version 0.8.2

--- Casks/spectacle.rb
+++ Casks/spectacle.rb
@@ -1,6 +1,6 @@
 class Spectacle < Cask
-  url 'https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.1.zip'
+  url 'https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.2.zip'
   homepage 'http://www.spectacleapp.com/'
-  version '0.8.1'
-  sha1 'ef0084cf13e127106957d8a26a4e50d608d0f257'
+  version '0.8.2'
+  sha1 'e0f6b3869357694264213a014a6d270d5ab55579'
 end
