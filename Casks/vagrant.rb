ad64a20cc030de760a584221df76034883506682
Paul Gross <pgross@gmail.com>
2013-04-27 22:03:16 -0700

    Upgrade vagrant to 1.2.2

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,6 +1,6 @@
 class Vagrant < Cask
-  url 'http://files.vagrantup.com/packages/a7853fe7b7f08dbedbc934eb9230d33be6bf746f/Vagrant-1.2.1.dmg'
+  url 'http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/Vagrant-1.2.2.dmg'
   homepage 'http://www.vagrantup.com'
-  version '1.2.1'
-  sha256 '27fe439c2e0521751e96dc2a45979a85272b5fc528ba012a0c9c0d527e7ac117'
+  version '1.2.2'
+  sha256 '1581552841e076043308f330a5b1130b455c604846116c54b5330bb17240c7ee'
 end
