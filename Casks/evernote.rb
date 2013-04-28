157f5844ab465dff0ac0bbce0962ecc9c1b5f979
phinze <paul.t.hinze@gmail.com>
2013-04-28 12:34:27 -0500

    forgot to update evernote version to latest

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -1,7 +1,7 @@
 class Evernote < Cask
   url 'http://www.evernote.com/about/download/get.php?file=EvernoteMac'
   homepage 'http://evernote.com/'
-  version '400808'
+  version 'latest'
   no_checksum
 
   link :app, 'Evernote.app'
