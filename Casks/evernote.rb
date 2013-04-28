d44a217900f6b63f74adfcc9c525533450bb58ca
phinze <paul.t.hinze@gmail.com>
2013-04-28 12:24:01 -0500

    change evernote to no_checksum
    this makes sense since the download link is not version-specific
    
    also specify app to link
    
    refs #209

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -2,5 +2,7 @@ class Evernote < Cask
   url 'http://www.evernote.com/about/download/get.php?file=EvernoteMac'
   homepage 'http://evernote.com/'
   version '400808'
-  sha1 '80c8f94098d4755ff1dcd5a03d314738d2161ca4'
+  no_checksum
+
+  link :app, 'Evernote.app'
 end
