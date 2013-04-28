2b7b530bf5a3a75b509878f173b23b9cfb6f7c36
phinze <paul.t.hinze@gmail.com>
2013-04-28 11:13:11 -0500

    add p4merge caveats as recommended in #217

--- Casks/p4merge.rb
+++ Casks/p4merge.rb
@@ -5,4 +5,10 @@ class P4merge < Cask
   sha1 'b082af5840282bcff5c2445c586316f72cf71551'
 
   link :app, 'p4merge.app'
+
+  def caveats; <<-EOS.undent
+    You can set up git to use p4merge as a merge tool by following the instructions available here:
+      https://gist.github.com/henrik242/1510148
+    EOS
+  end
 end
