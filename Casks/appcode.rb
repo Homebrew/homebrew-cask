aaf300224cb0725c995813f521e3c9748a12ff00
Vítor Galvão <info@vitorgalvao.com>
2013-04-29 19:00:30 +0100

    Added AppCode

---
+++ Casks/appcode.rb
@@ -0,0 +1,6 @@
+class Appcode < Cask
+  url 'http://download.jetbrains.com/objc/AppCode-2.0.3.dmg'
+  homepage 'http://www.jetbrains.com/objc/'
+  version '2.0.3'
+  sha1 '5564abb9bebcff335e02cd227130585873a21c12'
+end
