d466345086706241a22f6271e27e2a2e267f5028
Vince Cima <contact@vincecima.com>
2013-04-30 21:32:52 -0500

    Download link does not specify version, so set to latest and skip checksum

--- Casks/menu-meters.rb
+++ Casks/menu-meters.rb
@@ -1,6 +1,6 @@
 class MenuMeters < Cask
   url 'http://www.ragingmenace.com/software/download/MenuMeters.dmg'
   homepage 'http://www.ragingmenace.com/software/menumeters/'
-  version '1.6'
-  sha1 '7854b55c1980fbb9466a5dc4316e077e4899a6cc'
+  version 'latest'
+  no_checksum
 end
