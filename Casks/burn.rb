b00c653e654ed9f5b8d38eaedf9b8cd30aaf5f84
Tim Steinbach <steinbach.tim@googlemail.com>
2013-05-01 19:39:19 -0400

    Added Burn.app
    Burn.app 2.5.1 - Simple but advanced burning for OSX

---
+++ Casks/burn.rb
@@ -0,0 +1,8 @@
+class Burn < Cask
+  url 'http://downloads.sourceforge.net/project/burn-osx/Burn/2.5.1/burn251.zip'
+  homepage 'http://burn-osx.sourceforge.net/'
+  version '2.5.1'
+  sha1 '2a71f7b7e837cf5358d20228da51b4f3fc247844'
+
+  link :app, 'Burn.app'
+end
