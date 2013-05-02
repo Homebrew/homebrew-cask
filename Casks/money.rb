d284eefeb3f6fce821f44d1dc1e16e09ace196d2
Vítor Galvão <info@vitorgalvao.com>
2013-05-02 16:09:06 +0100

    Added money 4.4.4

---
+++ Casks/money.rb
@@ -0,0 +1,6 @@
+class Money < Cask
+  url 'http://download.jumsoft.com/Applications/Money/Money_4.4.4.zip'
+  homepage 'http://www.jumsoft.com/money/'
+  version '4.4.4'
+  sha1 'c68ceadd03b3ee57bd1e71918ce7a2d4d3b8d025'
+end
