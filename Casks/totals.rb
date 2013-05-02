14f078728df574fcd829940764ab1950bf06d28f
Vítor Galvão <info@vitorgalvao.com>
2013-05-02 16:09:18 +0100

    Added totals 2.2.4

---
+++ Casks/totals.rb
@@ -0,0 +1,6 @@
+class Totals < Cask
+  url 'http://www.kedisoft.com/dl/totals/Totals-2.2.4.dmg'
+  homepage 'http://www.kedisoft.com/totals/'
+  version '2.2.4'
+  sha1 '605574387e9b86a9296f4933b8217440992ecc6a'
+end
