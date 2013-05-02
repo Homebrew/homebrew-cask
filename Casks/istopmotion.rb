2b5039591a29c28d672293851868b79f389694fd
Vítor Galvão <info@vitorgalvao.com>
2013-05-02 16:08:50 +0100

    Added istopmotion 3.0.2

---
+++ Casks/istopmotion.rb
@@ -0,0 +1,6 @@
+class Istopmotion < Cask
+  url 'https://cdn.boinx.com/software/istopmotion/Boinx_iStopMotion_3.0.2-15241.zip'
+  homepage 'http://www.boinx.com/istopmotion/mac/'
+  version '3.0.2'
+  sha1 'ca99b6562d235c9baec678ae87ff941e4847816d'
+end
