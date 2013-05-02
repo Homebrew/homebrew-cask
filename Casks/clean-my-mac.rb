0dd23c593cfa260319ab2a659c14c3b9842a7570
Vítor Galvão <info@vitorgalvao.com>
2013-05-02 16:08:26 +0100

    Added clean-my-mac 2.0.3

---
+++ Casks/clean-my-mac.rb
@@ -0,0 +1,6 @@
+class CleanMyMac < Cask
+  url 'http://dl.devmate.com/com.macpaw.CleanMyMac2/CleanMyMac2.dmg'
+  homepage 'http://macpaw.com/cleanmymac'
+  version '2.0.3'
+  sha1 '3bd47a6c1849f42389daa92309e24b395d6eb1f2'
+end
