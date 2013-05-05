eadeb5964be34c2b1cb89e2c202fc53f520019cc
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-04 19:08:13 -0700

    Add iCHM cask

---
+++ Casks/ichm.rb
@@ -0,0 +1,6 @@
+class Ichm < Cask
+  url 'https://ichm.googlecode.com/files/iChm.1.4.3.zip'
+  homepage 'https://code.google.com/p/ichm/'
+  version '1.4.3'
+  sha1 '0223be4712930d6487c0c83883b28ca1d6a08a00'
+end
