ce66229b20443f6bcc4ffe6a6e8e7b49f28aef6e
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-04 18:22:39 -0700

    Add Slicy cask

---
+++ Casks/slicy.rb
@@ -0,0 +1,6 @@
+class Slicy < Cask
+  url 'https://s3.amazonaws.com/macrabbit/downloads/Slicy+1.1.4.zip'
+  homepage 'http://macrabbit.com/slicy/'
+  version '1.1.4'
+  sha1 '04dae551319422cb9c5ecb1c65aab9427ddaeb42'
+end
