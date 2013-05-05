5f8fa7648bc5eb8a42e2753b1bb7881ac0ff56c1
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-04 18:29:11 -0700

    Add Things2 cask

---
+++ Casks/things.rb
@@ -0,0 +1,6 @@
+class Things < Cask
+  url 'http://culturedcode.cachefly.net/things/Things_2.2_1.zip'
+  homepage 'http://culturedcode.com/things/'
+  version '2.2.1'
+  sha1 'cce95e6b29c5b9ac247549055189d723821bb70f'
+end
