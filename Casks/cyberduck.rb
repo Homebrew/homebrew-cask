012d36c94a8ec90abeffb3f6e66ebe375e40474e
Vince Cima <contact@vincecima.com>
2013-05-05 14:59:08 -0500

    Add explicit link in preparation of it begin required

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -3,4 +3,5 @@ class Cyberduck < Cask
   homepage 'http://cyberduck.ch'
   version '4.2.1'
   sha1 '6f1e220830f324ba136b4301b12711f6037176c7'
+  link :app, 'Cyberduck.app'
 end
--- Casks/daisy-disk.rb
+++ Casks/daisy-disk.rb
@@ -3,4 +3,5 @@ class DaisyDisk < Cask
   homepage 'http://www.daisydiskapp.com'
   version '2.1.2'
   sha1 '584eb479c41e2e73142fd69ce77bc57470d718de'
+  link :app, 'DaisyDisk.app'
 end
