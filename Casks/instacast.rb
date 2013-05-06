8608bcbf909cae2349cdae0c73471794df83a97b
Mo Lawson <moklawson@gmail.com>
2013-05-06 09:36:51 +0100

    Add Instacast beta cask

---
+++ Casks/instacast.rb
@@ -0,0 +1,8 @@
+class Instacast < Cask
+  url 'http://assets.vemedio.com/software/snowtape/Instacast-0.7.1b-1623.zip'
+  homepage 'http://vemedio.com/products/instacast-mac'
+  version '0.7.1b-1623'
+  sha1 '48d3e47e16633e90dfff78f8155a1603bdf7feb0'
+
+  link :app, 'Instacast.app'
+end
