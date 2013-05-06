4fcc5890e70630113a995323fb8126cc796618d4
Martin Polden <martin.polden@gmail.com>
2013-05-06 20:58:20 +0200

    Add Sonos cask

---
+++ Casks/sonos.rb
@@ -0,0 +1,7 @@
+class Sonos < Cask
+  url 'http://update.sonos.com/software/mac/mdcr/SonosDesktopController40.dmg'
+  homepage 'http://www.sonos.com/'
+  version '4.0'
+  sha1 'd7db3eeab8810e749360968e5f909dea2d384056'
+  link :app, 'Sonos.app'
+end
