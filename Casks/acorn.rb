21a3238f9d87d51aceb7c4cb29480f00047ea0fb
Vítor Galvão <info@vitorgalvao.com>
2013-05-06 22:02:41 +0100

    Added Acorn cask

---
+++ Casks/acorn.rb
@@ -0,0 +1,7 @@
+class Acorn < Cask
+  url 'http://flyingmeat.com/download/Acorn.zip'
+  homepage 'http://flyingmeat.com/acorn/'
+  version '4.0.2'
+  sha1 'c971741f194c520c2f7c33b1f7367bd6b5252998'
+  link :app, 'Acorn.app'
+end
