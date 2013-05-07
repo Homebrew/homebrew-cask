353bac1d31c9ab7f885817fcf72c68400a22fc96
Crazor <crazor@gmail.com>
2013-05-07 23:18:20 +0200

    Add Pacifist cask.

---
+++ Casks/pacifist.rb
@@ -0,0 +1,8 @@
+class Pacifist < Cask
+	url 'http://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=zip'
+	homepage 'http://www.charlessoft.com/'
+	version '3.0.10'
+	sha1 'f233f70d66f71bb4c0fc90a49a95aff685fcd81a'
+
+	link :app, 'Pacifist.app'
+end
