75ad5dee350c7af99115846c271cd72b911792f3
Vítor Galvão <info@vitorgalvao.com>
2013-05-07 18:04:43 +0100

    Added Calibre cask, version 0.9.29

---
+++ Casks/calibre.rb
@@ -0,0 +1,7 @@
+class Calibre < Cask
+  url 'http://calibre-ebook.googlecode.com/files/calibre-0.9.29.dmg'
+  homepage 'http://calibre-ebook.com/'
+  version '0.9.29'
+  sha1 '800eb31748ee31ee71a4fb46b70950680cbb1033'
+  link :app, 'calibre.app'
+end
