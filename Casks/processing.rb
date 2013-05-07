d7bd44b56a9974f441ae1184dacae2eba3202801
Vítor Galvão <info@vitorgalvao.com>
2013-05-07 21:53:40 +0100

    Added processing 2.0b8

---
+++ Casks/processing.rb
@@ -0,0 +1,7 @@
+class Processing < Cask
+  url 'http://processing.googlecode.com/files/processing-2.0b8-macosx.zip'
+  homepage 'http://processing.org/'
+  version '2.0b8'
+  sha1 '1367fa8eb3f9692b8bbbfa1c4c3d6d437ee3a86d'
+  link :app, 'Processing.app'
+end
