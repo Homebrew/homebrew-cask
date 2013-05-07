a797ecee1b53053f25b16e83ffae458857bb32fe
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-07 15:25:46 -0700

    Add Ghostlab cask.

---
+++ Casks/ghostlab.rb
@@ -0,0 +1,8 @@
+class Ghostlab < Cask
+  url 'http://awesome.vanamco.com/downloads/ghostlab/latest/Ghostlab.dmg'
+  homepage 'http://vanamco.com/ghostlab/'
+  version 'latest'
+  no_checksum
+
+  link :app, 'Ghostlab.app'
+end
