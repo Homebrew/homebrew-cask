5b963ef3b88724f1296deeff94fb9987eef99ad6
Martin Polden <martin.polden@gmail.com>
2013-05-08 19:14:11 +0200

    Update IntelliJ casks to 12.1.2

--- Casks/intellij-ultimate.rb
+++ Casks/intellij-ultimate.rb
@@ -1,6 +1,7 @@
 class IntellijUltimate < Cask
-  url 'http://download.jetbrains.com/idea/ideaIU-12.0.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIU-12.1.2.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '12.0'
-  sha1 '5d3150ac7e5b2a4754f646f8560918bec3adcdeb'
+  version '12.1.2'
+  sha1 'd4ebf826f754085ec5467c3d7d0647f9f6796c6a'
+  link :app, 'IntelliJ IDEA 12.app'
 end
