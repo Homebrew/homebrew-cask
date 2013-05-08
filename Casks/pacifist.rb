bc3517265361fc956f9d845b73ade29bdfda391c
Crazor <crazor@gmail.com>
2013-05-08 11:52:22 +0200

    Update pacifist cask to verbose URL.

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -1,8 +1,7 @@
 class Pacifist < Cask
-	url 'http://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=zip'
+	url 'http://www.charlessoft.com/pacifist_download/Pacifist_3.0.10.zip'
 	homepage 'http://www.charlessoft.com/'
 	version '3.0.10'
 	sha1 'f233f70d66f71bb4c0fc90a49a95aff685fcd81a'
-
 	link :app, 'Pacifist.app'
 end
