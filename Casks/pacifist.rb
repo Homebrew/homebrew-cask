9f12d36cf4ab99f0ae83ad1d2e8d3df34d6dd87d
Vítor Galvão <info@vitorgalvao.com>
2013-05-08 12:43:17 +0100

    changed version to 'latest'

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -1,7 +1,7 @@
 class Pacifist < Cask
-	url 'http://www.charlessoft.com/pacifist_download/Pacifist_3.0.10.zip'
+	url 'http://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=dmg'
 	homepage 'http://www.charlessoft.com/'
-	version '3.0.10'
-	sha1 'f233f70d66f71bb4c0fc90a49a95aff685fcd81a'
+	version 'latest'
+	no_checksum
 	link :app, 'Pacifist.app'
 end
