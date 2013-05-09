87ec5ccf6d187004fc303931501a32a9209492cd
Martin Polden <martin.polden@gmail.com>
2013-05-09 16:05:16 +0200

    Update Libre Office cask to 4.0.3

--- Casks/libre-office.rb
+++ Casks/libre-office.rb
@@ -1,6 +1,7 @@
 class LibreOffice < Cask
-  url 'http://download.documentfoundation.org/libreoffice/stable/4.0.2/mac/x86/LibreOffice_4.0.2_MacOS_x86.dmg'
+  url 'http://download.documentfoundation.org/libreoffice/stable/4.0.3/mac/x86/LibreOffice_4.0.3_MacOS_x86.dmg'
   homepage 'http://www.libreoffice.org/'
-  version '4.0.2'
-  sha1 '7d1997d3831b3ff17642a14ea891ad2d4d899eb2'
+  version '4.0.3'
+  sha1 '8a275083892ee38f5829795eae5e7e870229efe8'
+  link :app, 'LibreOffice.app'
 end
