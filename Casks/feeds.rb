ba847ba8ddd603a6ad7537ff6c1b217f9bc3956d
Mo Lawson <moklawson@gmail.com>
2013-05-09 08:23:53 +0100

    Add Feeds cask

---
+++ Casks/feeds.rb
@@ -0,0 +1,8 @@
+class Feeds < Cask
+  url 'https://feedswww.appspot.com/releases/Feeds-2.0.5.1.zip'
+  homepage 'http://www.feedsapp.com/'
+  version '2.0.5.1'
+  sha1 '7ea1ac6556812a19d417b38bd3a8b261bfb7f501'
+
+  link :app, 'Feeds.app'
+end
