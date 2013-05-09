5626565fe4e2cdf22607579d618d5456b6515770
Martin Polden <martin.polden@gmail.com>
2013-05-09 15:55:32 +0200

    Update VLC cask to 2.0.6

--- Casks/vlc.rb
+++ Casks/vlc.rb
@@ -1,8 +1,7 @@
 class Vlc < Cask
   homepage 'http://www.videolan.org/vlc/'
-  url 'http://downloads.sourceforge.net/project/vlc/2.0.5/macosx/vlc-2.0.5.dmg'
-  version '2.0.5'
-  sha1 'f2d3b240d012f9de6b5f1042a43dccca2c90a0e4'
-
+  url 'http://downloads.sourceforge.net/project/vlc/2.0.6/macosx/vlc-2.0.6.dmg'
+  version '2.0.6'
+  sha1 '65742a2194185790925a4dcd6105ca27eb3e386a'
   link :app, 'VLC.app'
 end
