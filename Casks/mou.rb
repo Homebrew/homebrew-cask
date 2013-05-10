2ea50571f3f80e9a0dabb0e7c1fbe88f0120ded3
John Schulz <jschulz@twitter.com>
2013-05-09 17:30:39 -0700

    Use `no_checksum` instead of a sha1 when getting `latest` version.

--- Casks/mou.rb
+++ Casks/mou.rb
@@ -2,5 +2,5 @@ class Mou < Cask
   url 'http://mouapp.com/download/Mou.zip'
   version 'latest'
   homepage 'http://mouapp.com/'
-  sha1 'e69bc2bbdadea6a2929032506085b6e9501a7fb9'
+  no_checksum
 end
