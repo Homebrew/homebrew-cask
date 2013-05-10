f6ee09280887641c36833a9bc542c0993394b739
Vítor Galvão <info@vitorgalvao.com>
2013-05-10 17:22:26 +0100

    since flux (not f-lux) gets updated, but the url stays the same even though it contains 'v4', changed to have no checksum

--- Casks/flux.rb
+++ Casks/flux.rb
@@ -2,5 +2,6 @@ class Flux < Cask
   url 'http://instruktion.net/theescapers/downloads/FluxV4.zip'
   homepage 'http://www.theescapers.com/flux/'
   version '4'
-  sha1 '4c5ef6573bd6b55915ec26bf6ba16c10f83b30d1'
+  no_checksum
+  link :app, 'Flux.app'
 end
