c2a076787ea5848a3ce1917a27cb9e2a5d5441f0
Vítor Galvão <info@vitorgalvao.com>
2013-05-10 15:26:40 +0100

    replaced tabs with spaces

--- Casks/frizzix.rb
+++ Casks/frizzix.rb
@@ -1,7 +1,7 @@
 class Frizzix < Cask
-	url 'http://mac.frizzix.de/downloads/currentVersion.dmg'
-	homepage 'http://mac.frizzix.de/'
-	version 'latest'
-	no_checksum
-	link :app, 'Frizzix.app'
+  url 'http://mac.frizzix.de/downloads/currentVersion.dmg'
+  homepage 'http://mac.frizzix.de/'
+  version 'latest'
+  no_checksum
+  link :app, 'Frizzix.app'
 end
--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -1,7 +1,7 @@
 class Pacifist < Cask
-	url 'http://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=dmg'
-	homepage 'http://www.charlessoft.com/'
-	version 'latest'
-	no_checksum
-	link :app, 'Pacifist.app'
+  url 'http://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=dmg'
+  homepage 'http://www.charlessoft.com/'
+  version 'latest'
+  no_checksum
+  link :app, 'Pacifist.app'
 end
