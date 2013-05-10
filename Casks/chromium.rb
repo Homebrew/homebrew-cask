19a68e26286972f493e8ce40391e6ce03257c706
Vítor Galvão <info@vitorgalvao.com>
2013-05-10 13:57:54 +0100

    updated erroneous sourceforge link

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,5 +1,5 @@
 class Chromium < Cask
-  url 'http://netcologne.dl.sourceforge.net/project/osxportableapps/Chromium/ChromiumOSX_26.0.1410.65.dmg'
+  url 'http://downloads.sourceforge.net/project/osxportableapps/Chromium/ChromiumOSX_26.0.1410.65.dmg'
   homepage 'http://www.freesmug.org/chromium'
   version '26.0.1410.65'
   sha1 'afb9ae77e4aa2a6e3321ffa314869698105bb22f'
