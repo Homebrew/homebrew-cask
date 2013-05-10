0dabd8727f679fb0e5f9aeb9a9f4081c25d76da9
Vítor Galvão <info@vitorgalvao.com>
2013-05-10 13:52:11 +0100

    updated chromium

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,7 @@
 class Chromium < Cask
+  url 'http://netcologne.dl.sourceforge.net/project/osxportableapps/Chromium/ChromiumOSX_26.0.1410.65.dmg'
   homepage 'http://www.freesmug.org/chromium'
-  url 'http://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_22.0.1229.92.dmg'
-  version '22.0.1229.92'
+  version '26.0.1410.65'
   sha1 'afb9ae77e4aa2a6e3321ffa314869698105bb22f'
+  link :app, 'Chromium.app'
 end
