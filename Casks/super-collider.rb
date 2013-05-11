797a6a3942b5b6027e4fac7b31906bdd4e12af03
phinze <paul.t.hinze@gmail.com>
2013-05-11 14:03:28 -0500

    add super-collider cask
    who wants to code us up some music? :)

---
+++ Casks/super-collider.rb
@@ -0,0 +1,7 @@
+class SuperCollider < Cask
+  url 'http://downloads.sourceforge.net/project/supercollider/Mac%20OS%20X/3.6/SuperCollider-3.6.5-OSX-universal.dmg'
+  homepage 'http://supercollider.sourceforge.net/'
+  version '3.6.5'
+  sha1 'f778f6d3c42f8662001b241287836bbe5c532604'
+  link :app, 'SuperCollider.app'
+end
