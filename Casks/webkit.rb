a157fb7f17a0d6f90a8568e6d86761b4f9a3da1a
John Schulz <jschulz@twitter.com>
2013-05-11 17:20:40 -0700

    Add a cask for WebKit Nightly.

---
+++ Casks/webkit.rb
@@ -0,0 +1,7 @@
+class Webkit < Cask
+  url 'http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-r149932.dmg'
+  homepage 'http://nightly.webkit.org/'
+  version 'latest'
+  sha1 'a3623506e9fbcb6162741d2804ccacc579ffd33f'
+  link :app, 'WebKit.app'
+end
