633275ec6de6a07a3119a092c2dbac2c5769beaf
phinze <paul.t.hinze@gmail.com>
2013-05-11 20:20:26 -0500

    bump version to 0.11.0

--- brew-cask.rb
+++ brew-cask.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class BrewCask < Formula
   homepage 'https://github.com/phinze/homebrew-cask/'
-  url 'https://github.com/phinze/homebrew-cask.git', :tag => 'v0.10.1'
-  version '0.10.1'
+  url 'https://github.com/phinze/homebrew-cask.git', :tag => 'v0.11.0'
+  version '0.11.0'
 
   head 'https://github.com/phinze/homebrew-cask.git', :branch => 'master'
 
