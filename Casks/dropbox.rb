245a5fb33d072e25931c40571e081a7b577d2f8e
Vítor Galvão <info@vitorgalvao.com>
2013-05-12 19:18:18 +0100

    Since dropbox updates behind the scenes, it now always gets the latest version

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -1,7 +1,7 @@
 class Dropbox < Cask
-  url 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.0.10.dmg'
+  url 'https://www.dropbox.com/download?plat=mac'
   homepage 'http://www.dropbox.com/'
-  version '2.0.10'
-  sha1 '75c77b3361d78683fc4bd456922e20ae84d95b60'
+  version 'latest'
+  no_checksum
   link :app, 'Dropbox.app'
 end
