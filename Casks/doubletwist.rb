a8e10ff3ac37fa922e272da0a152863e64a4d591
Jeff Jewiss <jeffjewiss@gmail.com>
2013-05-13 18:17:05 -0400

    Add doubleTwist cask

---
+++ Casks/doubletwist.rb
@@ -0,0 +1,7 @@
+class Doubletwist < Cask
+  url 'http://download.doubletwist.com/doubleTwist.dmg'
+  homepage 'http://www.doubletwist.com/'
+  version 'latest'
+  no_checksum
+  link :app, 'doubleTwist.app'
+end
