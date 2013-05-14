0f7eaac3983541fd853fe48a28c31515087ebc14
Manfred Touron <m@42.am>
2013-05-14 13:51:04 +0200

    Add cask for Team Viewer

---
+++ Casks/team-viewer.rb
@@ -0,0 +1,7 @@
+class TeamViewer < Cask
+  url 'http://download.teamviewer.com/download/TeamViewer.dmg'
+  homepage 'http://www.teamviewer.com/'
+  version 'latest'
+  no_checksum
+  install 'Install TeamViewer.pkg'
+end
