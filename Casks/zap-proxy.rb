98cd51a58464a6ccd4119577b10b7d90c1832f37
Manfred Touron <m@42.am>
2013-05-14 12:07:18 +0200

    Added Zap Proxy cask

---
+++ Casks/zap-proxy.rb
@@ -0,0 +1,7 @@
+class ZapProxy < Cask
+  url 'https://zaproxy.googlecode.com/files/OWASP_ZAP_2.1.0_OSX.zip'
+  homepage 'https://www.owasp.org/index.php/OWASP_Zed_Attack_Proxy_Project'
+  version '2.1.0'
+  sha1 '86ebdb90e56a25ce49cd3421252a6c4c147d7d27'
+  link :app, 'OWASP ZAP.app'
+end
