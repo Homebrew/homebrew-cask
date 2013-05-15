3b2e0e58591f9b609f1c50f7434d221026edb473
Jeff Jewiss <jeffjewiss@gmail.com>
2013-05-15 15:31:01 -0400

    Adds Screenhero

---
+++ Casks/screenhero.rb
@@ -0,0 +1,7 @@
+class Screenhero < Cask
+  url 'http://dl.screenhero.com/update/screenhero/Screenhero.dmg'
+  homepage 'http://screenhero.com'
+  version 'latest'
+  no_checksum
+  link :app, 'Screenhero.app'
+end
