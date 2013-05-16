d173eb2590a5632fb5ac6662da75162ba69bb925
Roger Ragulan Rajaratnam <roger@ragusource.com>
2013-05-16 18:06:07 +0100

    Cask for poedit 1.5.5
    Added new cask for poedit 1.5.5

---
+++ Casks/poedit.rb
@@ -0,0 +1,7 @@
+class Poedit < Cask
+  url 'http://downloads.sourceforge.net/project/poedit/poedit/1.5/poedit-1.5.5.dmg'
+  homepage 'http://www.poedit.net'
+  version '1.5.5'
+  sha1 'ea5ec89695c302c781579794ab8f75ca1678bb72'
+  link :app, 'Poedit.app'
+end
