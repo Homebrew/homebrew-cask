c16924cc76b8b8b00fb4346fe7c6c36615f01d0e
Roger Ragulan Rajaratnam <roger@ragusource.com>
2013-05-16 18:32:03 +0100

    Add cask for Tagr
    Add cask for Tagr 3.1.2 (http://www.entwicklungsfreu.de/tagr.html)

---
+++ Casks/tagr.rb
@@ -0,0 +1,7 @@
+class Tagr < Cask
+  url 'http://www.harald-schubert.net/data/Tagr.zip'
+  homepage 'http://www.entwicklungsfreu.de/tagr.html'
+  version '3.1.2'
+  sha1 '37018000c06a33a0590991361fbae444e464ecfd'
+  link :app, 'Tagr.app'
+end
