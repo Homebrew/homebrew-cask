a02a688209922749226f60360e800e4968801608
Roger Ragulan Rajaratnam <roger@ragusource.com>
2013-05-16 17:44:59 +0100

    New cask file for MesaSqlite
    Created new cask file for MesaSQLite 4.0.6

---
+++ Casks/mesa-sqlite.rb
@@ -0,0 +1,7 @@
+class MesaSqlite < Cask
+  url 'http://www.desertsandsoftware.com/DEMOS/MesaSQLite406.zip'
+  homepage 'http://www.desertsandsoftware.com'
+  version '4.0.6'
+  sha1 'b435c9b9505ed9efe683c9468d44c74b615e3cd1'
+  link :app, 'MesaSQLite.app'
+end
