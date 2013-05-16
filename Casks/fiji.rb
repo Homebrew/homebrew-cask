c82b93d865f582141b6af9d7d6dd75ac444bb307
punintended <josh@turse.org>
2013-05-16 16:32:50 -0600

    Update fiji.rb
    Add version, no checksum, as per Paul's suggestions.

--- Casks/fiji.rb
+++ Casks/fiji.rb
@@ -1,7 +1,7 @@
 class Fiji < Cask
   url 'http://jenkins.imagej.net/job/Stable-Fiji-MacOSX/lastSuccessfulBuild/artifact/fiji-macosx.dmg'
   homepage 'http://fiji.sc'
-  version ''
-  sha1 'cb2442f035a03488dfa6d225f4e262ce0b206652'
+  version 'latest'
+  no_checksum
   link :app, 'Fiji.app'
 end
