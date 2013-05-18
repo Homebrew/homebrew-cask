f285060a940b29ecf92c3919bb74e77fd5878ece
Vítor Galvão <info@vitorgalvao.com>
2013-05-18 23:58:21 +0100

    added textexpander 4.0.5

---
+++ Casks/textexpander.rb
@@ -0,0 +1,7 @@
+class Textexpander < Cask
+  url 'http://cdn.smilesoftware.com/TextExpander_4.0.5.zip'
+  homepage 'http://www.smilesoftware.com/TextExpander/index.html'
+  version '4.0.5'
+  sha1 '0433fbd988346d5feb0932843e97256cf7bc0a6a'
+  link :app, 'TextExpander.app'
+end
