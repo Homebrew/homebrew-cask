31a2f2c2fc419545031561e3ab40b3b399dca8c6
Ben Lauwens <ben.lauwens@rma.ac.be>
2013-05-20 21:18:09 +0200

    Add aqua-term

---
+++ Casks/aqua-term.rb
@@ -0,0 +1,6 @@
+class AquaTerm < Cask
+  url 'http://downloads.sourceforge.net/project/aquaterm/AquaTerm/AquaTerm1.1.0.dmg'
+  homepage 'http://aquaterm.sourceforge.net/'
+  version '1.1.0'
+  sha1 'a1a6d958f0eb6c9b496496bae81d6dcd9781a5bc'
+end
