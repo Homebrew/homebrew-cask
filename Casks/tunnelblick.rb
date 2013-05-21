b9d1a213d0407143f9cb9ab60258275d28a9bebf
Ben Lauwens <ben.lauwens@rma.ac.be>
2013-05-21 20:47:24 +0200

    Add tunnelblick.rb

---
+++ Casks/tunnelblick.rb
@@ -0,0 +1,7 @@
+class Tunnelblick < Cask
+  url 'https://tunnelblick.googlecode.com/files/Tunnelblick_3.2.9.dmg'
+  homepage 'https://code.google.com/p/tunnelblick/'
+  version '3.2.9'
+  sha1 '85985883a519c0e8801a44420b4c490861ad4e03'
+  link :app, 'Tunnelblick.app'
+end
