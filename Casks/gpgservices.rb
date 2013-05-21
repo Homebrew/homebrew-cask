08a296f80999d2f7e3833d1afd2b9dc7cb4bd6dc
John Schulz <jschulz@twitter.com>
2013-05-21 16:57:59 -0700

    Broken. Installation via dmg installer works. Does not via cask.

---
+++ Casks/gpgservices.rb
@@ -0,0 +1,6 @@
+class Gpgservices < Cask
+  url 'https://s3.amazonaws.com/gpgtools/GPGServices-1.8.dmg'
+  homepage 'https://gpgtools.org/gpgservices/index.html'
+  version '1.8'
+  sha1 '5836bf9585b89db2f65f08673a7110453f32e5d0'
+end
