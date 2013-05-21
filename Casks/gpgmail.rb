4c18c9773ba7143f7d96ec182ec34d2b3a3897c6
John Schulz <jschulz@twitter.com>
2013-05-21 16:15:56 -0700

    Add cask for GPGMail. No `link` line b/c it's a plugin for Mail.app.

---
+++ Casks/gpgmail.rb
@@ -0,0 +1,6 @@
+class Gpgmail < Cask
+  url 'https://s3.amazonaws.com/gpgtools/GPGMail-2.0b6.dmg'
+  homepage 'https://gpgtools.org/gpgmail/index.html'
+  version '2.0b6'
+  sha1 'f9384b9e941684daae71184e6141e00c5c9549cd'
+end
