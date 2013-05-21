6e442db687ba668724d7fdf89bc23c6b58455b26
John Schulz <jschulz@twitter.com>
2013-05-21 16:25:29 -0700

    Broken. Not sure why.
    ```
    ± /opt/twitter/Library/Taps/phinze-cask [feature-add-gpg-keychain-access @ 5956b19] ✗ ‣ brew cask install --force gpg-keychain-access
    ==> Downloading https://s3.amazonaws.com/gpgtools/GPG+Keychain+Access-1.0.dmg
    Already downloaded: /Library/Caches/Homebrew/gpg-keychain-access-1.0.dmg
    ==> Success! gpg-keychain-access installed to /opt/homebrew-cask/Caskroom/gpg-keychain-access/1.0
    Error: undefined method `basename' for nil:NilClass
    Please report this bug:
        https://github.com/mxcl/homebrew/wiki/troubleshooting
    /opt/twitter/Cellar/brew-cask/0.12.2/rubylib/cask/app_linker.rb:24:in `link_app'
    /opt/twitter/Cellar/brew-cask/0.12.2/rubylib/cask/app_linker.rb:8:in `link'
    /opt/twitter/Cellar/brew-cask/0.12.2/rubylib/cask/app_linker.rb:8:in `each'
    /opt/twitter/Cellar/brew-cask/0.12.2/rubylib/cask/app_linker.rb:8:in `link'
    /opt/twitter/Cellar/brew-cask/0.12.2/rubylib/cask/cli/install.rb:9:in `run'
    /opt/twitter/Cellar/brew-cask/0.12.2/rubylib/cask/cli/install.rb:5:in `each'
    /opt/twitter/Cellar/brew-cask/0.12.2/rubylib/cask/cli/install.rb:5:in `run'
    /opt/twitter/Cellar/brew-cask/0.12.2/rubylib/cask/cli.rb:21:in `process'
    /opt/twitter/bin/brew-cask.rb:6
    /opt/twitter/Library/brew.rb:51:in `require'
    /opt/twitter/Library/brew.rb:51:in `require?'
    /opt/twitter/Library/brew.rb:93
    ```

---
+++ Casks/gpg-keychain-access.rb
@@ -0,0 +1,7 @@
+class GpgKeychainAccess < Cask
+  url 'https://s3.amazonaws.com/gpgtools/GPG+Keychain+Access-1.0.dmg'
+  homepage 'https://gpgtools.org/keychain/index.html'
+  version '1.0'
+  sha1 '8fd8268094865ec278ac60f2bfdd01a23be180d0'
+  link :app, 'GPG Keychain Access.app'
+end
