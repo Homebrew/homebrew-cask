dd19d1b15f3c4584e246eb51422510124661be37
John Schulz <jschulz@twitter.com>
2013-05-21 17:22:19 -0700

    Use `install` and `uninstall` as specified in phinze/homebrew-cask#14

--- Casks/gpgtools.rb
+++ Casks/gpgtools.rb
@@ -3,4 +3,6 @@ class Gpgtools < Cask
   homepage 'https://gpgtools.org/index.html'
   version '2013.05.20'
   sha1 '9f9fea935b3ce90d8d04542a754b8778f82a8b1b'
+  install 'GPGTools.pkg'
+  uninstall 'Uninstall.app'
 end
