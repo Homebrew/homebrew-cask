2d72b0b80b9ca1fb4f1a27e9a659079ccc0f4ac2
John Schulz <jschulz@twitter.com>
2013-05-21 17:19:38 -0700

    Use `install` and `uninstall` as specified in phinze/homebrew-cask#14

--- Casks/macgpg2.rb
+++ Casks/macgpg2.rb
@@ -3,4 +3,6 @@ class Macgpg2 < Cask
   homepage 'https://gpgtools.org/macgpg2/index.html'
   version '2.0.19'
   sha1 '3fda54bbb5df5a8c00b5857a7c8ee9f0b29408f9'
+  install 'MacGPG2.pkg'
+  uninstall 'Uninstall.app'
 end
