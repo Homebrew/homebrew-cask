ef08f66f13b4b283def5fb35cb08a99f86f11bb8
John Schulz <jschulz@twitter.com>
2013-05-21 17:26:08 -0700

    Use `install` and `uninstall` as specified in phinze/homebrew-cask#14.
    Bonus: fixes the "doesn't actually work" issue mentioned in the previous commit.

--- Casks/gpgservices.rb
+++ Casks/gpgservices.rb
@@ -3,4 +3,6 @@ class Gpgservices < Cask
   homepage 'https://gpgtools.org/gpgservices/index.html'
   version '1.8'
   sha1 '5836bf9585b89db2f65f08673a7110453f32e5d0'
+  install 'GPGServices.pkg'
+  uninstall 'Uninstall.app'
 end
