0b39c2bdd3fa0fe7deced7f8927ea684baa367ef
Ben Lauwens <ben.lauwens@rma.ac.be>
2013-05-22 14:30:58 +0200

    Add unison.rb

---
+++ Casks/unison.rb
@@ -0,0 +1,7 @@
+class Unison < Cask
+  url 'http://alan.petitepomme.net/unison/assets/2011.01.28-Esup-Unison-2.40.61-mac-intel.dmg'
+  homepage 'http://www.cis.upenn.edu/~bcpierce/unison/'
+  version '2.40.61'
+  sha1 '7b11e4a8a27a414e227ec2fcf825a2180faa1c30'
+  link :app, 'Unison.app'
+end
