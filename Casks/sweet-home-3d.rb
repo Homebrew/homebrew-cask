75dc4d78cd307f8857e862bbdf359a766f53205b
Alexandre COLLIGNON <alexandre@collign.net>
2013-05-23 22:02:38 +0200

    Add Sweet Home 3D (Version 4.0) Cask

---
+++ Casks/sweet-home-3d.rb
@@ -0,0 +1,7 @@
+class SweetHome3d < Cask
+  url 'http://downloads.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-4.0/SweetHome3D-4.0-macosx.dmg'
+  homepage 'http://www.sweethome3d.com/'
+  version '4.0'
+  sha1 '15b0ec1d8b70267707b00637ac7289cf8267f15d'
+  link :app, 'Sweet Home 3D.app'
+end
