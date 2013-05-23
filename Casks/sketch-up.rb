06743dcbdbf6e89a66a91973475dfd0340fa8da3
Alexandre COLLIGNON <alexandre@collign.net>
2013-05-23 20:55:33 +0200

    Add Sketch Up (version 8) Cask

---
+++ Casks/sketch-up.rb
@@ -0,0 +1,7 @@
+class SketchUp < Cask
+  url 'http://dl.trimble.com/sketchup/SketchUpMEN.dmg'
+  homepage 'http://www.sketchup.com/intl/en/'
+  version '8'
+  sha1 '0dbfc19d64119fee86b38bc757b0d2431eef01e0'
+  install 'SketchUp 8.0 Installer.pkg'
+end
