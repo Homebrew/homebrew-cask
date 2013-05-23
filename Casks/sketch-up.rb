619564cb18a26dbe6e8bad4471abfbc66056a4aa
Vítor Galvão <info@vitorgalvao.com>
2013-05-23 20:52:09 +0100

    removed sketchup, pending further changes

--- Casks/sketch-up.rb
+++
@@ -1,7 +0,0 @@
-class SketchUp < Cask
-  url 'http://dl.trimble.com/sketchup/SketchUpMEN.dmg'
-  homepage 'http://www.sketchup.com/intl/en/'
-  version '8'
-  sha1 '0dbfc19d64119fee86b38bc757b0d2431eef01e0'
-  link :app, 'SketchUp.app'
-end
