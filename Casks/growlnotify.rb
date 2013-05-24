d41bbece0eab02f6fadda31de20cbc31922c67bf
phinze <paul.t.hinze@gmail.com>
2013-05-23 20:48:20 -0500

    add cask for growlnotify

---
+++ Casks/growlnotify.rb
@@ -0,0 +1,7 @@
+class Growlnotify < Cask
+  url 'http://growl.cachefly.net/GrowlNotify-2.0.zip'
+  homepage 'http://growl.info/downloads'
+  version '2.0'
+  sha1 'efd54dec2623f57fcbbba54050206d70bc7746dd'
+  install 'GrowlNotify.pkg'
+end
