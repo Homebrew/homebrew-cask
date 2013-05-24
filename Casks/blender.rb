7780e69f6e9505a0827f32e6f986333fc9c78082
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2013-05-24 19:02:30 +0200

    Blender 2.67a

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,7 +1,7 @@
 class Blender < Cask
-  url 'http://download.blender.org/release/Blender2.67/blender-2.67-OSX_10.6-x86_64.zip'
+  url 'http://download.blender.org/release/Blender2.67/blender-2.67a-OSX_10.6-x86_64.zip'
   homepage 'http://www.blender.org/'
-  version '2.67'
-  sha1 'de0039778749c6cbd0ddc49c5ae29f597b731ea2'
+  version '2.67a'
+  sha1 '3693654763817cb40bd30e7885c62c5973b8840a'
   link :app, 'blender.app', 'blenderplayer.app'
 end
