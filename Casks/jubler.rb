d294ee6c0fae25bc3a87001d9385f1ac840244de
Alexandre COLLIGNON <alexandre@collign.net>
2013-05-26 17:41:51 +0200

    Add Jubler (Version 4.6.1) Cask

---
+++ Casks/jubler.rb
@@ -0,0 +1,7 @@
+class Jubler < Cask
+  url 'http://jubler.googlecode.com/files/Jubler-4.6.1.dmg'
+  homepage 'http://www.jubler.org/'
+  version '4.6.1'
+  sha1 'ad61a8e19c05f5491be7cc192cf9d6e43386ff49'
+  link :app, 'Jubler.app'
+end
