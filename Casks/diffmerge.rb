cbfdadc94dcb39d9af6f79c0a75b148666ea4fd6
Roger Ragulan Rajaratnam <roger@ragusource.com>
2013-05-28 00:41:39 +0100

    Update Diffmerge cask to use new link syntax
    Update Diffmerge cask to use new link syntax

--- Casks/diffmerge.rb
+++ Casks/diffmerge.rb
@@ -3,5 +3,5 @@ class Diffmerge < Cask
   homepage 'http://www.sourcegear.com/diffmerge'
   version '3.3.2'
   sha1 '134ca0c88a63c01657e1d50cd05aa006c3a1529f'
-  link :app, 'DiffMerge.app'
+  link 'DiffMerge.app'
 end
