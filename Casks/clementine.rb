537e8860bc53ca13c74dfddabf667b53e9f7fd12
Alexandre COLLIGNON <alexandre@collign.net>
2013-05-27 20:28:03 +0200

    Add Clementine (Version 1.1.0) Cask

---
+++ Casks/clementine.rb
@@ -0,0 +1,7 @@
+class Clementine < Cask
+  url 'http://clementine-player.googlecode.com/files/clementine-1.1.0.dmg'
+  homepage 'http://www.clementine-player.org/'
+  version '1.1.0'
+  sha1 'dda3baeda071f6b46e8bb168eb5f856280807ba8'
+  link :app, 'clementine.app'
+end
