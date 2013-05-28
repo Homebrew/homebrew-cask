3445fcb71e02e38219d8072cc905392c03de89b4
Mo Lawson <moklawson@gmail.com>
2013-05-28 10:39:10 +0100

    Add CrashPlan cask

---
+++ Casks/crashplan.rb
@@ -0,0 +1,8 @@
+class Crashplan < Cask
+  url 'http://download.crashplan.com/installs/mac/install/CrashPlan/CrashPlan_3.5.3_Mac.dmg'
+  homepage 'http://www.crashplan.com/'
+  version '3.5.3'
+  sha1 'b5c9b304c7191f2dd5773e0c373c495726097d0d'
+  install 'Install CrashPlan.pkg'
+  uninstall 'Uninstall.app'
+end
