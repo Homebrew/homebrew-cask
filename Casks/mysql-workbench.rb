8c576b4806132f55b8d2e9e180c0ab4529659307
Roger Ragulan Rajaratnam <roger@ragusource.com>
2013-05-29 10:46:58 +0100

    Add cask for MySQL Workbench
    Add cask for MySQL Workbench 5.2.47 (http://www.mysql.com/products/workbench)

---
+++ Casks/mysql-workbench.rb
@@ -0,0 +1,7 @@
+class MysqlWorkbench < Cask
+  url 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-gpl-5.2.47-osx-i686.dmg'
+  homepage 'http://www.mysql.com/products/workbench'
+  version '5.2.47'
+  sha1 '54b814b418ce8513efdf2a1ac48f4deace867dc3'
+  link 'MySQLWorkbench.app'
+end
