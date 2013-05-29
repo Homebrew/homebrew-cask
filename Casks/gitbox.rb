cdfad2b68c375867dbc6995f5fdb9d49ed7e8360
James Anderson <me@jamesaanderson.com>
2013-05-28 22:05:51 -0700

    Add Gitbox cask

---
+++ Casks/gitbox.rb
@@ -0,0 +1,7 @@
+class Gitbox < Cask
+  url 'http://d1oa71y4zxyi0a.cloudfront.net/gitbox-1.6.2-ml.zip'
+  homepage 'http://gitboxapp.com/'
+  version '1.6.2'
+  sha1 '20fdea01bf3b26f6ba23fa2b909a7d56cddb6250'
+  link :app, 'Gitbox.app'
+end
