a7dd92fc768cdb8e33f9d9e9b8296efdefb437dc
Vítor Galvão <info@vitorgalvao.com>
2013-05-30 02:20:34 +0100

    Updated uTorrent to latest-beta
    Since uTorrent stable now uses a `.pkg` file to [suggest a search
    extenstion](http://forum.utorrent.com/viewtopic.php?pid=721314#p721314),
    it was changed to use the beta

--- Casks/u-torrent.rb
+++ Casks/u-torrent.rb
@@ -1,6 +1,7 @@
 class UTorrent < Cask
   homepage 'http://www.utorrent.com/'
-  version '1.8.3'
-  url 'http://download.utorrent.com/1.8.3/uTorrent.dmg'
-  sha1 '903a097ab89e15f1207f286276670b6a7cde9398'
+  version 'latest-beta'
+  url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/beta/'
+  no_checksum
+  link 'uTorrent.app'
 end
