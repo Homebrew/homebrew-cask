82fdc3a2b748d6f33f3d2024b029f6b6a5b431e8
Michael Ford <fanquake@gmail.com>
2013-06-01 02:27:48 +0800

    TorBrowserAlpha - Correct URL and sha1

--- Casks/tor-browser-alpha.rb
+++ Casks/tor-browser-alpha.rb
@@ -1,7 +1,7 @@
 class TorBrowserAlpha < Cask
+  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.4.12-alpha-2-osx-x86_64-en-US.zip'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.4.12-alpha-1-osx-x86_64-en-US.zip'
   version '2.4.12-alpha-2'
-  sha1 '3fd1540bff837b1e012483575e64559b24bee5b4'
-  link :app, 'TorBrowser_en-US.app'
+  sha1 'd4767e3c568ca9bb240488adc811c7fbffad0254'
+  link 'TorBrowser_en-US.app'
 end
