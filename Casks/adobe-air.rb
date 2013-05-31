f5e9f85a63f6411dd661014448e4b69f7ea34a64
Vítor Galvão <info@vitorgalvao.com>
2013-06-01 00:50:39 +0100

    Adobe AIR now uses 'install'

--- Casks/adobe-air.rb
+++ Casks/adobe-air.rb
@@ -3,11 +3,5 @@ class AdobeAir < Cask
   homepage 'https://get.adobe.com/air/'
   version '3.5'
   sha1 '8fbd2dffc20442903d8b51e7362a3191f39752b4'
-
-  link :app, :none
-
-  def caveats; <<-EOS.undent
-    You need to run #{destination_path/'AdobeAIRInstaller.app'} to actually install Adobe AIR
-    EOS
-  end
+  install 'Adobe AIR Installer.app'
 end
