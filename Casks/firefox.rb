2eeb90e364fa1100ad506104d3e251f9e0fae2db
Michael Ford <fanquake@gmail.com>
2013-06-01 00:33:58 +0800

    Firefox 21.0

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,7 +1,7 @@
 class Firefox < Cask
-  url 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/20.0.1/mac/en-US/Firefox%2020.0.1.dmg'
-  homepage 'http://www.mozilla.org/en-US/firefox/'
-  version '20.0.1'
-  sha1 '510b7e53be4b120ca2e277dec3a64a09f64c8ed8'
-  link :app, 'Firefox.app'
+  url 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/21.0/mac/en-US/Firefox%2021.0.dmg'
+  homepage 'https://www.mozilla.org/en-US/firefox/'
+  version '21.0'
+  sha1 'ce5a9083a38ea580811b3408566a5194486dc360'
+  link 'Firefox.app'
 end
