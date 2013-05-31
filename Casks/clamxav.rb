67eaad94f2f24830096fa30307e678be2dd984cb
Alexandre COLLIGNON <alexandre@collign.net>
2013-05-31 21:51:22 +0200

    Switch to single parameter link. Relates to #304

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -3,5 +3,5 @@ class Clamxav < Cask
   homepage 'http://www.clamxav.com/'
   version '2.3.6'
   sha1 '4e59947bc049109c375613979fb6092ffe67aa55'
-  link :app, 'ClamXav.app'
+  link 'ClamXav.app'
 end
