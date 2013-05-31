fa89b4e0080e78d610e46aa81c89c52a3778197a
Michael Ford <fanquake@gmail.com>
2013-06-01 04:19:22 +0800

    Update LittleSnitch to include install/uninstall fields

--- Casks/little-snitch.rb
+++ Casks/little-snitch.rb
@@ -3,4 +3,6 @@ class LittleSnitch < Cask
   homepage 'http://www.obdev.at/products/littlesnitch/index.html'
   version '3.1'
   sha1 'c0e0b523976962f25588b59b1a57c68091ba947b'
+  install 'Little Snitch Installer.app'
+  uninstall 'Little Snitch Uninstaller.app'
 end
