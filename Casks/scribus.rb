bdd99f681c0b70e2607d9d157927f903b6a060bf
ouyang <im@yangzhiping.com>
2013-05-31 17:49:30 +0800

    add scribus cask

---
+++ Casks/scribus.rb
@@ -0,0 +1,7 @@
+class Scribus < Cask
+  url 'http://downloads.sourceforge.net/project/scribus/scribus/1.4.2/scribus-1.4.2.dmg'
+  homepage 'http://www.scribus.net/canvas/Scribus'
+  version '1.4.2'
+  sha1 '482be9e18f3a54e7cbf4472969a56f5a30f1a3d1'
+  link 'Scribus.app'
+end 
