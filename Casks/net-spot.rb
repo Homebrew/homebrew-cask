fbac72c5f231942db17754391e0fb1f6376efacb
Michael Ford <fanquake@gmail.com>
2013-06-02 00:50:05 +0800

    NetSpot Cask - use no_checksum, and set version to "latest"

--- Casks/net-spot.rb
+++ Casks/net-spot.rb
@@ -1,7 +1,7 @@
 class NetSpot < Cask
   url 'http://www.netspotapp.com/download/NetSpot.dmg'
   homepage 'http://www.netspotapp.com'
-  version '2.1'
-  sha1 '47b51e72fdef4679d42ad13fa73b6ee961c75651'
+  version 'latest'
+  no_checksum
   link 'NetSpot.app'
 end
