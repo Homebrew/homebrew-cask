bcf8afa4a2ee3730da7f13b457b1285a9498b4a6
Michael Ford <fanquake@gmail.com>
2013-06-01 14:00:34 +0800

    AirServer 4.6.5

--- Casks/air-server.rb
+++ Casks/air-server.rb
@@ -1,7 +1,7 @@
 class AirServer < Cask
-  url 'http://dl.airserver.com/mac/AirServer-4.6.4.dmg'
+  url 'http://dl.airserver.com/mac/AirServer-4.6.5.dmg'
   homepage 'http://www.airserver.com'
-  version '4.6.4'
-  sha1 '3c80e2110bcdf4e46ec9809c141b5367a80d6365'
+  version '4.6.5'
+  sha1 'd84f25d4bd7a4f645ac9736cf76cc4097f11d102'
   link 'AirServer.app'
 end
