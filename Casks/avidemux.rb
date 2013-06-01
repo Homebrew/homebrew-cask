fe1aef2ad418498e20f1ab2df12493f0ee409ab5
Vítor Galvão <info@vitorgalvao.com>
2013-06-01 17:24:41 +0100

    corrected shasum

--- Casks/avidemux.rb
+++ Casks/avidemux.rb
@@ -2,6 +2,6 @@ class Avidemux < Cask
   url 'http://downloads.sourceforge.net/avidemux/avidemux_2.6.4_64bits_ml.dmg'
   homepage 'http://www.avidemux.org/'
   version '2.6.4'
-  sha1 'ff70d323ab8a0c72f4d733e28338bd16f8bda761'
+  sha1 'fddb2db7469999090abf1ba347991cecd2b6976a'
   link 'Avidemux2.6.app'
 end
