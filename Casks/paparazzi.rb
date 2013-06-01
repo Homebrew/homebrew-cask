ac914036311eeb0ea965bce451f4aca2b2dbe290
Michael Ford <fanquake@gmail.com>
2013-06-02 01:25:56 +0800

    Paparazzi 0.6.7

--- Casks/paparazzi.rb
+++ Casks/paparazzi.rb
@@ -1,6 +1,7 @@
 class Paparazzi < Cask
-  url 'http://derailer.org/paparazzi/Paparazzi!%200.6.5.dmg'
+  url 'http://derailer.org/paparazzi/Paparazzi!%200.6.7.dmg'
   homepage 'http://derailer.org/paparazzi/'
-  version '0.6.5'
-  sha1 '3f7e7f0cd6fc498c04a0122f75e8874b7c69bbf7'
+  version '0.6.7'
+  sha1 '1c8d097ae34a43c8222322d2353bc68fb93803bc'
+  link 'Paparazzi!.app'
 end
