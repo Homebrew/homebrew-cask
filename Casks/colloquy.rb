bab0262cb70e64eec401742a62254e88a651aa87
Manfred Touron <m@42.am>
2013-06-02 19:19:27 +0300

    Fixed url

--- Casks/colloquy.rb
+++ Casks/colloquy.rb
@@ -1,5 +1,5 @@
 class Colloquy < Cask
-  url 'http://colloquy.info/downloads/colloquy-2.4.2zip'
+  url 'http://colloquy.info/downloads/colloquy-2.4.2.zip'
   homepage 'http://colloquy.info/'
   version '2.4.2'
   sha1 '19892476af32f83685a37af16676b0098ad3bacc'
