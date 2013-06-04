05921c1eb0b45beef56a8f8a87c31df18650065d
Martin Broder <mbroder@3pc.de>
2013-06-04 15:41:10 +0200

    removed versioning of boxcryptor app since its not needed

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,7 +1,7 @@
 class Boxcryptor < Cask
   url 'https://www.boxcryptor.com/download/BoxCryptor_Installer.dmg'
   homepage 'https://www.boxcryptor.com/'
-  version '1.2.3'
-  sha1 'a460d7045b855ba17d271b9fe370e39612860cf3'
+  version 'latest'
+  no_checksum
   link ''
 end
