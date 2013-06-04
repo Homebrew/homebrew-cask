259fd45c179b819fe2f902ac79167e7f89b73d2c
Jason Karns <jason@karns.name>
2013-06-04 16:42:33 -0400

    New Cask for pgAdmin3 (PostgreSQL GUI)
    Add a cask for the latest version (1.8.4) of pgAdmin3, which is a GUI
    client for PostgreSQL

---
+++ Casks/pgadmin3.rb
@@ -0,0 +1,7 @@
+class Pgadmin3 < Cask
+  url 'http://ftp.postgresql.org/pub/pgadmin3/release/v1.8.4/osx/pgadmin3-1.8.4.dmg'
+  homepage 'http://pgadmin.org'
+  version '1.8.4'
+  sha1 '6f916c133d4456b9f777b8ad0368b0f986cf7272'
+  link 'pgAdmin3.app'
+end
