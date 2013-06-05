34cb8e884b51c00eb311be1bdb7fa807c5059a1a
Michael Ford <fanquake@gmail.com>
2013-06-05 17:30:00 +0800

    AirVideoServer 2.4.6-beta3u2

--- Casks/air-video-server.rb
+++ Casks/air-video-server.rb
@@ -1,7 +1,7 @@
 class AirVideoServer < Cask
-  url 'http://s3.amazonaws.com/AirVideo/Air+Video+Server+2.4.6-beta3u1.dmg'
+  url 'http://s3.amazonaws.com/AirVideo/Air+Video+Server+2.4.6-beta3u2.dmg'
   homepage 'http://www.inmethod.com/air-video/'
-  version '2.4.6-beta3'
-  sha1 '2293b653fea6a939ab863a7bf918d601a504c9c8'
+  version '2.4.6-beta3u2'
+  sha1 'ff2e87cedad71c393c5c8bfad964b8b1fd7c7b85'
   link 'Air Video Server.app'
 end
