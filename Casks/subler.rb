1a56da4b01d8aa7ed8b0282ec8bef035959e79c3
Pete Schaffner <pjschaffner@gmail.com>
2013-06-05 13:09:17 -0400

    Adding Subler Cask

---
+++ Casks/subler.rb
@@ -0,0 +1,7 @@
+class Subler < Cask
+  url 'https://subler.googlecode.com/files/Subler-0.19.zip'
+  homepage 'https://code.google.com/p/subler/'
+  version '0.19'
+  sha1 'acc77fc2da638a4138f68627fb68cd0e6af9da39'
+  link 'Subler.app'
+end
