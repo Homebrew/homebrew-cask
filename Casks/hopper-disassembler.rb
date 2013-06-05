fc8170bd9639e8d4e2ba5905fc84410248ced356
Alexander <alyxander@gmail.com>
2013-06-05 10:38:31 +0200

    cleanup

--- Casks/hopper-disassembler.rb
+++
@@ -1,7 +0,0 @@
-class HopperDisassembler < Cask
-  url 'http://www.hopperapp.com/HopperWeb/download_last.php'
-  homepage 'http://www.hopperapp.com/'
-  version '2.7.15'
-  sha1 '6f2d78b0f1eff86a3da1ef25736b01500ae700c5'
-  link 'Hopper Disassembler.app'
-end
