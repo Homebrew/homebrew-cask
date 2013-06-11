71c07df709bd77c1a21a5993d5f81ebe5723fd43
Michael Ford <fanquake@gmail.com>
2013-06-12 01:12:21 +0800

    Alfred 2.0.5_202

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,7 +1,7 @@
 class Alfred < Cask
-  url 'http://cachefly.alfredapp.com/Alfred_2.0.4_199.zip'
+  url 'http://cachefly.alfredapp.com/Alfred_2.0.5_202.zip'
   homepage 'http://www.alfredapp.com/'
-  version '2.0.4_199'
-  sha1 '8d7ea8cf5b228a92ef715787b49345dd3abdf83f'
+  version '2.0.5_202'
+  sha1 'ced434267c45a0728332a7063652b60e01694b27'
   link 'Alfred 2.app', 'Alfred Preferences.app'
 end
