ac93c213af90abcb5774e67b1f4045ef1a8046ed
Michael Ford <fanquake@gmail.com>
2013-06-12 01:25:18 +0800

    Correct Gantt version field

--- Casks/gantt-project.rb
+++ Casks/gantt-project.rb
@@ -1,7 +1,7 @@
 class GanttProject < Cask
   url 'https://ganttproject.googlecode.com/files/ganttproject-2.6.1-r1499.dmg'
   homepage 'http://www.ganttproject.biz'
-  version '2.6'
+  version '2.6.1'
   sha1 'e59f787256a47a90b4baf13daca7f129860d3dd7'
   link 'GanttProject 2.6.1.app'
 end
