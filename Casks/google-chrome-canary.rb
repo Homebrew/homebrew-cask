e4c9c966beba16ada904366e878099ee6657ebad
Tyll Weiß <inkdpixels@googlemail.com>
2013-06-12 15:51:50 +0200

    Remove the deprecated ':app' argument

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -3,5 +3,5 @@ class GoogleChromeCanary < Cask
   homepage 'https://tools.google.com/dlpage/chromesxs'
   version 'latest'
   no_checksum
-  link :app, 'Google Chrome Canary.app'
+  link 'Google Chrome Canary.app'
 end
