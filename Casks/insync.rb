aaa48376bc2d6275bc6123896cf7b7e75b054c02
Inkdpixels <inkdpixels@googlemail.com>
2013-06-12 10:42:17 +0200

    Add a cask-file for the InSync.app

---
+++ Casks/insync.rb
@@ -0,0 +1,7 @@
+class Insync < Cask
+  url 'http://s.insynchq.com/builds/Insync-1.0.11.31522.dmg'
+  homepage 'http://insynchq.com/'
+  version '1.0.11.31522'
+  sha1 'fe311fc627bcba5ee6516a25298ce3514eaae745'
+  link 'Insync.app'
+end
