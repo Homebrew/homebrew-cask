d1be337c1be312871f970abbf7ab727eeee12cc9
yaroot <yaroot@gmail.com>
2013-06-14 20:29:17 +0800

    nomacs 1.0.0

---
+++ Casks/nomacs.rb
@@ -0,0 +1,7 @@
+class Nomacs < Cask
+  url 'http://downloads.sourceforge.net/nomacs/nomacs-1.0.0-x86_64.dmg'
+  homepage 'http://www.nomacs.org/'
+  version '1.0.0'
+  sha1 'b8caef809158e799ec3a032b2707327418139f90'
+  link 'nomacs.app'
+end
