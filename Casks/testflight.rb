af570a59b5c54d543ee15fe2cf0c04c0181d9c1e
John Stallings <jdstallings@gmail.com>
2013-06-14 21:11:17 -0700

    Add a cask-file for TestFlight 1.0_beta306

---
+++ Casks/testflight.rb
@@ -0,0 +1,7 @@
+class Testflight < Cask
+  url 'https://d193ln56du8muy.cloudfront.net/desktop_app/1370377714/TestFlight-Desktop-1.0-Beta(306).zip'
+  homepage 'http://testflightapp.com'
+  version '1.0_beta306'
+  sha1 '86180feb6e48c30501ec2a0428bb45f7b4fc3396'
+  link 'TestFlight.app'
+end
