f83b51fb2c7408f0857587b9d1703c0a8349e7c9
Michael Ford <fanquake@gmail.com>
2013-06-15 17:13:19 +0800

    TotalSpaces 1.2.9

--- Casks/totalspaces.rb
+++ Casks/totalspaces.rb
@@ -1,7 +1,7 @@
 class Totalspaces < Cask
-  url 'http://downloads.binaryage.com/TotalSpaces-1.2.2.zip'
+  url 'http://downloads.binaryage.com/TotalSpaces-1.2.9.zip'
   homepage 'http://totalspaces.binaryage.com/'
-  version '1.2.2'
-  sha1 '38151268a6c03ca5daf2213b7ce9a74cf2ff3904'
+  version '1.2.9'
+  sha1 '2d068e693cae3fb8e3a1d650ccd2d4c7418d0be9'
   install 'TotalSpaces.pkg'
 end
