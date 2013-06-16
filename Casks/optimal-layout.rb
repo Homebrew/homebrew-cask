7db9e107fe122ca1fc7d73483988163007f271f7
Michael Ford <fanquake@gmail.com>
2013-06-16 12:06:16 +0800

    Add link field to Optimal Layout Cask

--- Casks/optimal-layout.rb
+++ Casks/optimal-layout.rb
@@ -3,4 +3,5 @@ class OptimalLayout < Cask
   homepage 'http://most-advantageous.com/optimal-layout/'
   version '2.2.3'
   sha256 '333593deeab0612a0e8b14c06f2bb2b5468c9b1137b8448b85cba0abfd9df350'
+  link 'Optimal Layout.app'
 end
