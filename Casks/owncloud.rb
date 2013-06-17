7d649bc46f4d97fa626130057f7a7e6ba40b96bb
Andrew Oke <andrew@Andrews-MacBook-Pro.local>
2013-06-16 21:43:46 -0400

    add more

---
+++ Casks/owncloud.rb
@@ -0,0 +1,7 @@
+class Owncloud < Cask
+  url 'http://download.owncloud.com/download/ownCloud-1.2.5.dmg'
+  homepage 'http://owncloud.com'
+  version '1.2.5'
+  sha1 'dbc1e9880caa25535861aee351bd39c1942ded62'
+  link 'owncloud.app'
+end
