170ebc0bb4f2293cfe21f717669ada6f84ae9b23
Vítor Galvão <info@vitorgalvao.com>
2013-06-17 23:42:12 +0100

    changed to use 'version latest' and 'no_checksum'

--- Casks/hopper-disassembler.rb
+++ Casks/hopper-disassembler.rb
@@ -1,7 +1,7 @@
 class HopperDisassembler < Cask
   url 'http://www.hopperapp.com/HopperWeb/download_last.php'
   homepage 'http://www.hopperapp.com/'
-  version '2.7.15'
-  sha1 '6f2d78b0f1eff86a3da1ef25736b01500ae700c5'
+  version 'latest'
+  no_checksum
   link 'Hopper Disassembler.app'
 end
