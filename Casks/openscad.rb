7597bd71210b93a1b9d3a7e1440ae19c15249713
Andrew Oke <andrew@Andrews-MacBook-Pro.local>
2013-06-16 21:19:35 -0400

    add openscad

---
+++ Casks/openscad.rb
@@ -0,0 +1,7 @@
+class Openscad < Cask
+  url 'https://openscad.googlecode.com/files/OpenSCAD-2013.01.dmg'
+  homepage 'http://www.openscad.org/'
+  version '2013.01'
+  sha1 '075e8609886212ddb18155d122960cf6120c69b3'
+  link 'OpenSCAD.app'
+end
