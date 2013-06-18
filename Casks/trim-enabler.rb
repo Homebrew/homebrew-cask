2ed8902e872e733362d45332edf334c0f125810b
Vítor Galvão <info@vitorgalvao.com>
2013-06-18 15:34:11 +0100

    Added trim-enabler

---
+++ Casks/trim-enabler.rb
@@ -0,0 +1,7 @@
+class TrimEnabler < Cask
+  url 'http://groths.org/trimenabler/TrimEnabler.dmg'
+  homepage 'http://www.groths.org/trim-enabler/'
+  version 'latest'
+  no_checksum
+  link 'Trim Enabler.app'
+end
