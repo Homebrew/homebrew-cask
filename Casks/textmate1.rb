a1b110f1c0cfa05f30dd72438162131f536bc7e9
Vítor Galvão <info@vitorgalvao.com>
2013-06-19 22:57:53 +0100

    removed textmate1, textmate2 is now textmate

--- Casks/textmate1.rb
+++
@@ -1,6 +0,0 @@
-class Textmate1 < Cask
-  url 'http://download.macromates.com/TextMate_1.5.11_r1635.zip'
-  homepage 'http://macromates.com/'
-  version '1.5.11_r1635'
-  sha1 '41e09022f7f9bd0962e3d7032699064c60d3ae19'
-end
