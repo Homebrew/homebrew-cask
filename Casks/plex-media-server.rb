9e30ece7a2b1609bbdef29421baa9d00e07c4230
Vítor Galvão <info@vitorgalvao.com>
2013-06-19 22:45:11 +0100

    removed plex-media-server (duplicate)

--- Casks/plex-media-server.rb
+++
@@ -1,6 +0,0 @@
-class PlexMediaServer < Cask
-  url 'http://plex.r.worldssl.net/PlexMediaServer/0.9.7.9.375-d056f10/PlexMediaServer-0.9.7.9.375-d056f10-OSX.dmg'
-  homepage 'http://plexapp.com'
-  version '0.9.7.9.375'
-  sha1 'c62657773f71203e64d92cb951f4d5825286d360'
-end
