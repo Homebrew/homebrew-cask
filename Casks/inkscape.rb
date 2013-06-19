9068315bd32a65ae3942b0e5316aba8db2b37d8c
Vítor Galvão <info@vitorgalvao.com>
2013-06-19 20:41:45 +0100

    added link

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -1,6 +1,7 @@
 class Inkscape < Cask
-  homepage 'http://inkscape.org'
   url 'http://downloads.sourceforge.net/inkscape/Inkscape-0.48.2-1-SNOWLEOPARD.dmg'
+  homepage 'http://inkscape.org'
   version '0.48.2-1'
   sha1 '20073844e26dc787da04d951e3a24338758e5e8d'
+  link 'Inkscape.app'
 end
