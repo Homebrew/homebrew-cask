51111e873942bc25da49eb033779fb001611aeaa
Crazor <crazor@gmail.com>
2013-06-19 12:01:15 +0200

    Update f-lux cask to suppress the annoying "Move to /Applications?" prompt.

--- Casks/f-lux.rb
+++ Casks/f-lux.rb
@@ -4,4 +4,7 @@ class FLux < Cask
   version 'latest'
   no_checksum
   link 'Flux.app'
+
+  #Prevent f.lux from asking the user whether he wants the app bundle moved to /Applications
+  system 'defaults write org.herf.Flux moveToApplicationsFolderAlertSuppress -int 1'
 end
