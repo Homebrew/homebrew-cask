bba37f3063b89c52cb05931c8950c213a2ab9308
Vítor Galvão <info@vitorgalvao.com>
2013-06-19 21:26:47 +0100

    added link

--- Casks/mplayer-osx-extended.rb
+++ Casks/mplayer-osx-extended.rb
@@ -3,4 +3,5 @@ class MplayerOsxExtended < Cask
   homepage 'http://www.mplayerosx.ch/'
   version 'rev15-test2'
   sha1 'ef13167d15ca93a8f9b31b597d4d6b6314a26425'
+  link 'MPlayer OSX Extended.app'
 end
