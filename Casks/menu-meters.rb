cc0bae1163788867da874badabe7a82b6a2c29d6
Vítor Galvão <info@vitorgalvao.com>
2013-06-20 13:39:22 +0100

    menu-meters can now use 'install'

--- Casks/menu-meters.rb
+++ Casks/menu-meters.rb
@@ -3,10 +3,5 @@ class MenuMeters < Cask
   homepage 'http://www.ragingmenace.com/software/menumeters/'
   version 'latest'
   no_checksum
-  link 'MenuMeters Installer.app'
-
-  def caveats; <<-EOS.undent
-    You need to run MenuMeters Installer.app to actually install MenuMeters
-    EOS
-  end
+  install 'MenuMeters Installer.app'
 end
