b0627df2db30d362e8d58663c83b40599465b853
Andrew Oke <andrew.oke@gmail.com>
2013-06-20 18:36:46 -0400

    add eyetv

--- Casks/eye-tv.rb
+++ Casks/eye-tv.rb
@@ -2,6 +2,6 @@ class EyeTv < Cask
   url 'https://www.macupdate.com/download/8536/eyetv_361_7120.dmg'
   homepage 'http://www.elgato.com/'
   version '3.6.1'
-  sha1 ''
+  sha1 'da9eed821cf8fb1f5ddb27c155c3514230907b1e'
   link 'EyeTV.app'
 end
