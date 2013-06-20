ff6562c78d9f8893779a1682ab4eaded9586e22a
Andrew Oke <andrew.oke@gmail.com>
2013-06-20 19:29:36 -0400

    add xquartz

---
+++ Casks/xquartz.rb
@@ -0,0 +1,7 @@
+class Xquartz < Cask
+  url 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.4.dmg'
+  homepage 'http://xquartz.macosforge.org/trac/wiki/X112.7.4'
+  version '1.13.0'
+  sha1 '98b2ca8580046d5b91250c5a244c8182437dc9d7'
+end
+
