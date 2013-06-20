2e4773b599f908978373c34bcf4fde945be51c18
shawjia <shawjia@gmail.com>
2013-06-20 19:52:39 +0800

    update version

--- Casks/rtx.rb
+++ Casks/rtx.rb
@@ -1,7 +1,7 @@
 class Rtx < Cask
   url 'http://www.rtxapp.com/download'
   homepage 'http://www.rtxapp.com/mac/'
-  version '1.1.0.18'
-  sha1 '9d03db26669d3a44c77b243ff2de2e169042c7e6'
+  version 'latest'
+  no_checksum
   link 'RTX.app'
 end
