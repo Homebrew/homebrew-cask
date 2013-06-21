2a0f5e4766f8efece8af98457c17b9c3d770e8db
Crazor <crazor@gmail.com>
2013-06-21 12:39:00 +0200

    Add Tuxguitar cask

---
+++ Casks/tuxguitar.rb
@@ -0,0 +1,7 @@
+class Tuxguitar < Cask
+  url 'http://downloads.sourceforge.net/project/tuxguitar/TuxGuitar/TuxGuitar-1.2/tuxguitar-1.2-macosx10.5-cocoa-64.dmg'
+  homepage 'http://www.tuxguitar.com.ar/'
+  version '1.2'
+  sha1 'f7e0803006a803c66c7f32d87c5c2f5b9b1af8dd'
+  link 'Tuxguitar.app'
+end
