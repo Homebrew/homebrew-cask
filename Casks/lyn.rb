c3789b45c1359b46e09f0cf332dd20d198223204
Fernando Paredes <nanoid.xd@gmail.com>
2013-06-20 18:06:45 -0700

    Add Lyn cask

---
+++ Casks/lyn.rb
@@ -0,0 +1,7 @@
+class Lyn < Cask
+  url 'http://www.lynapp.com/downloads/Lyn-1.3.dmg'
+  homepage 'http://www.lynapp.com/'
+  version '1.3'
+  sha1 '1756d33fb9e53de579177522d93c900fd5208ff9'
+  link 'Lyn.app'
+end
