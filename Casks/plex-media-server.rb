ead0ca9f3c1478cd2518e6e1b66a4e794bedfe0e
Andrew Oke <andrew.oke@gmail.com>
2013-06-21 13:21:03 -0400

    rename plex -> plex-media-server, add plex frontend

---
+++ Casks/plex-media-server.rb
@@ -0,0 +1,7 @@
+class PlexMediaServer < Cask
+  url 'http://plex.r.worldssl.net/plex-media-server/0.9.7.28.33-f80a4a2/PlexMediaServer-0.9.7.28.33-f80a4a2-OSX.dmg'
+  homepage 'http://plexapp.com'
+  version '0.9.7.28.33'
+  sha1 '22295a1fd40c877475d72059f120025ef7a9b462'
+  link 'Plex Media Server.app'
+end
