53c219b2d88a3efe86dcd89aac191247b6c0ebdb
Andrew Oke <andrew.oke@gmail.com>
2013-06-21 12:59:40 -0400

    remove xquartz, handbrakecli

--- Casks/handbrakecli.rb
+++
@@ -1,7 +0,0 @@
-class Handbrakecli < Cask
-  url 'http://superb-dca2.dl.sourceforge.net/project/handbrake/0.9.9/HandBrake-0.9.9-MacOSX.6_CLI_x86_64.dmg'
-  homepage 'http://handbrake.fr'
-  version '0.9.9'
-  sha1 '8c175265b18bea37a9072ea90406192462600141'
-  link 'HandBrakeCLI'
-end
--- Casks/origin.rb
+++ Casks/origin.rb
@@ -1,7 +1,7 @@
 class Origin < Cask
   url 'https://eaassets-a.akamaihd.net/Origin-Client-Download/origin/mac/Origin.dmg'
   homepage 'http://origin.com'
-  version '1'
-  sha1 'd9860274d153759eb17c85aebb551c41dbea745f'
+  version 'latest'
+  no_checksum
   link 'Origin.app'
 end
