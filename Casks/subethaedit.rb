6e4c153e65ad4d2bfa4befe0f7411d09c90b7ec0
Ali Samii <ali.samii@ergonomiq.net>
2013-06-22 19:34:54 +0200

    Adding SubEthaEdit.app

---
+++ Casks/subethaedit.rb
@@ -0,0 +1,7 @@
+class Subethaedit < Cask
+  url 'http://www.codingmonkeys.de/subethaedit/SubEthaEdit.zip'
+  homepage 'http://www.codingmonkeys.de/subethaedit/'
+  version '3.5.4'
+  sha1 '59047fee393588778ff651da93a6d3ecc3567ebb'
+  link 'SubEthaEdit.app'
+end
