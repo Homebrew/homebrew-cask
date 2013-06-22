babc437b88812693b915361948e2fac925150a98
Michael Ford <fanquake@gmail.com>
2013-06-22 15:46:23 +0800

    Cura 13.06

--- Casks/cura.rb
+++ Casks/cura.rb
@@ -1,7 +1,7 @@
 class Cura < Cask
+  url 'http://software.ultimaker.com/current/Cura-13.06.3-MacOS.dmg'
   homepage 'http://daid.github.com/Cura/'
-  url 'http://software.ultimaker.com/current/Cura-13.04-MacOS.dmg'
-  version '13.04'
-  sha1 'ad308a7dbce034ae582f62db82895121c7b153fd'
+  version '13.06.3'
+  sha1 'db5764ecb2678b0a7e0cae4476eb2ac98053136d'
   link 'Cura.app'
 end
