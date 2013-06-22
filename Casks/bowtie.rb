ecb950a4f9e3ed73325c58653e8a6ae9791a894a
Ali Samii <ali.samii@ergonomiq.net>
2013-06-22 18:32:47 +0200

    Adding bowtie.app

---
+++ Casks/bowtie.rb
@@ -0,0 +1,7 @@
+class Bowtie < Cask
+  url 'http://bowtieapp.com/bowtie-1.5.zip'
+  homepage 'http://bowtieapp.com/'
+  version '1.5'
+  sha1 '6db52cf49d806856477f666a0032041471435d8f'
+  link 'Bowtie.app'
+end
--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -2,6 +2,6 @@ class Chromium < Cask
   url 'http://downloads.sourceforge.net/project/osxportableapps/Chromium/ChromiumOSX_27.0.1453.116.dmg'
   homepage 'http://www.freesmug.org/chromium'
   version '27.0.1453.116'
-  sha1 'dfcedfe05a87739430bb8d71cf66b05e2bde84f6'
+  sha1 '17a294ad64f9969189d0d3faa06d963bf72be5b3'
   link 'Chromium.app'
 end
