9b8d5c4e74216a740b6a2540893cb3462b1c985e
shawjia <shawjia@gmail.com>
2013-06-22 18:19:19 +0800

    Add Light Table App

---
+++ Casks/light-table.rb
@@ -0,0 +1,7 @@
+class LightTable < Cask
+  url 'http://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.4.11/LightTableMac.zip'
+  homepage 'http://www.lighttable.com/'
+  version '0.4.11'
+  sha1 'c6d23b1eb09a6d82dd669e8c10929d4e203e9c27'
+  link 'LightTable.app'
+end
