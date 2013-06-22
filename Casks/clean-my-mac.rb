be4342035b2c3a0d71f031413a06344bb8e62bb4
Rodolphe Stoclin <srodolphe@gmail.com>
2013-06-22 13:26:46 +0300

    CleanMyMac 2.0.4

--- Casks/clean-my-mac.rb
+++ Casks/clean-my-mac.rb
@@ -1,7 +1,7 @@
 class CleanMyMac < Cask
   url 'http://dl.devmate.com/com.macpaw.CleanMyMac2/CleanMyMac2.dmg'
   homepage 'http://macpaw.com/cleanmymac'
-  version '2.0.3'
-  sha1 '3bd47a6c1849f42389daa92309e24b395d6eb1f2'
+  version '2.0.4'
+  sha1 '08b2d57cb349d5113c4b8222de6bbbc6f97f6e9c'
   link 'CleanMyMac 2.app'
 end
