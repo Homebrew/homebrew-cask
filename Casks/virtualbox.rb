974465237ea4655eda3600d2dcaf949d42f2fee5
Zachary M. Jones <zmj@zmjones.com>
2013-06-23 14:37:45 -0400

    virtualbox sha1 fix
    virtualbox's listed sha1 is incorrect, and I neglected to test it :(

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -2,6 +2,6 @@ class Virtualbox < Cask
   url 'http://download.virtualbox.org/virtualbox/4.2.14/VirtualBox-4.2.14-86644-OSX.dmg'
   homepage 'http://www.virtualbox.org'
   version '4.2.14-86644'
-  sha1 'c7f02ce0f60a0db45c060a99856fc4e139006616eb799b5fb60dc2bebf2a5a75'
+  sha1 '93c36db799d53acaed7bb18c1402c6f11671f40a'
   install 'VirtualBox.pkg'
 end
