c4688ad5a289eb3d1d2ab29c4aefc6958fe388fd
Zachary M. Jones <zmj@zmjones.com>
2013-06-23 14:21:46 -0400

    Virtualbox cask outdated

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,7 +1,7 @@
 class Virtualbox < Cask
-  url 'http://download.virtualbox.org/virtualbox/4.2.12/VirtualBox-4.2.12-84980-OSX.dmg'
+  url 'http://download.virtualbox.org/virtualbox/4.2.14/VirtualBox-4.2.14-86644-OSX.dmg'
   homepage 'http://www.virtualbox.org'
-  version '4.2.12-84980'
-  sha1 '9a5913f0b25c69a67b554caa9bebd1a4d72d3d00'
+  version '4.2.14-86644'
+  sha1 'c7f02ce0f60a0db45c060a99856fc4e139006616eb799b5fb60dc2bebf2a5a75'
   install 'VirtualBox.pkg'
 end
