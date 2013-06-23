7f6d1d7e8ed646d004b9262663618d048bb70c33
Robert Curth <robert@rocu.de>
2013-06-23 07:17:10 +0200

    Adds cask for Soulver

---
+++ Casks/soulver.rb
@@ -0,0 +1,7 @@
+class Soulver < Cask
+  url 'http://www.acqualia.com/files/download.php?product=soulver'
+  homepage 'http://www.acqualia.com/soulver/'
+  version 'latest'
+  no_checksum
+  link 'Soulver.app'
+end
