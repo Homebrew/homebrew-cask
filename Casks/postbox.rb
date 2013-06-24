0d7f1c82850ac81cdd3a7437652ae55887795126
Robert Curth <robert@rocu.de>
2013-06-24 18:32:21 +0200

    Adds cask for Postbox

---
+++ Casks/postbox.rb
@@ -0,0 +1,7 @@
+class Postbox < Cask
+  url 'http://www.postbox-inc.com/php/download.php?a=308m'
+  homepage 'http://www.postbox-inc.com/'
+  version '3.0.8'
+  sha1 '78613f2111a6345ff1aeba63682a33888e2c256b'
+  link 'Postbox.app'
+end
