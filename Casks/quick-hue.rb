50ab68314ba084b7f1e7ba20b793b69c382ef191
Nick Rowe <nixterrimus@dcxn.com>
2013-06-24 16:49:59 -0700

    In QuickHue cask use latest as version

--- Casks/quick-hue.rb
+++ Casks/quick-hue.rb
@@ -1,7 +1,7 @@
 class QuickHue < Cask
   url 'https://github.com/danparsons/QuickHue/raw/master/QuickHue.zip'
   homepage 'https://github.com/danparsons/QuickHue'
-  version 'head'
+  version 'latest'
   no_checksum
   link 'QuickHue.app'
 end
