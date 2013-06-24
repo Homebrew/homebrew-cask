89022f9396da2527bc7f9086b5e07fe5b500d082
Fernando Paredes <nanoid.xd@gmail.com>
2013-06-23 17:25:28 -0700

    Add Shimo cask

---
+++ Casks/shimo.rb
@@ -0,0 +1,7 @@
+class Shimo < Cask
+  url 'http://www.chungwasoft.com/files/Shimo_3.2.3_2587.zip'
+  homepage 'http://www.chungwasoft.com/shimo/'
+  version '3.2.3_2587'
+  sha1 '34414b64fd1d138dc05350f56c15ceea310b4fb2'
+  link 'Shimo.app'
+end
