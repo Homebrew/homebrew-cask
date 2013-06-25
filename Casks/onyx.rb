7f80764f6ee59ad61b632ac00de88d0b234b90d5
Rodolphe Stoclin <srodolphe@gmail.com>
2013-06-25 17:12:21 +0200

    Update Onyx

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -1,4 +1,4 @@
-class Onyx108 < Cask
+class Onyx < Cask
   url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
   homepage 'http://www.titanium.free.fr/downloadonyx.php'
   version 'latest'
