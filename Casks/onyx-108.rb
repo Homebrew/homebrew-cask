5455e30541679a521b905d3012b935d40775278f
Rodolphe Stoclin <srodolphe@gmail.com>
2013-06-25 16:53:54 +0200

    Add Onyx 10.8

---
+++ Casks/onyx-108.rb
@@ -0,0 +1,7 @@
+class Onyx108 < Cask
+  url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
+  homepage 'http://www.titanium.free.fr/downloadonyx.php'
+  version 'latest'
+  no_checksum
+  link 'OnyX.app'
+end
