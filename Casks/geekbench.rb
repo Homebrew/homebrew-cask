4339f81dbb6f879544c836c763eacbe5ffc7bf0c
Robert Curth <robert@rocu.de>
2013-06-26 07:59:30 +0200

    Adds cask for Geekbench

---
+++ Casks/geekbench.rb
@@ -0,0 +1,7 @@
+class Geekbench < Cask
+  url 'http://d34wv75roto0rl.cloudfront.net/Geekbench-2.4.3-Mac.zip'
+  homepage 'http://www.primatelabs.com/geekbench/'
+  version '2.4.3'
+  sha1 '79ae4af73a1d697c6b6357a36cbba44ab6198ca6'
+  link 'Geekbench.app'
+end
