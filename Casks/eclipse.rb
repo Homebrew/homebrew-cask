b87533d99398ff339afe418589a547139e5f7622
Vítor Galvão <info@vitorgalvao.com>
2013-06-26 18:39:03 +0100

    moved url line to the top, for consistency

--- Casks/eclipse.rb
+++ Casks/eclipse.rb
@@ -1,6 +1,6 @@
 class Eclipse < Cask
-  homepage 'http://eclipse.org'
   url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/R/eclipse-standard-kepler-R-macosx-cocoa-x86_64.tar.gz'
+  homepage 'http://eclipse.org'
   version '4.3'
   sha1 'c9aeb9ba9db87d8d5c81b6abf9c11283f15a0cd2'
   link 'Eclipse.app'
