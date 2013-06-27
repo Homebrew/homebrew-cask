2454028779c82d151277de3ee37e2fb754694bbb
Vítor Galvão <info@vitorgalvao.com>
2013-06-27 03:08:38 +0100

    version latest and no_checksum

--- Casks/clarify.rb
+++ Casks/clarify.rb
@@ -1,7 +1,7 @@
 class Clarify < Cask
   url 'http://www.bluemangolearning.com/download/clarify/1_0/release/clarify.dmg'
   homepage 'http://www.clarify-it.com/'
-  version '1.2.1'
-  sha1 '41528c4715876247f824acb99c8c66d2ace19b4a'
+  version 'latest'
+  no_checksum
   link 'Clarify.app'
 end
