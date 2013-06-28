98752c33a555010e312081e8517b059f57a2cd6d
Robert Curth <robert@rocu.de>
2013-06-28 21:13:34 +0200

    Changes version of Cura

--- Casks/cura.rb
+++ Casks/cura.rb
@@ -1,7 +1,7 @@
 class Cura < Cask
-  url 'http://software.ultimaker.com/current/Cura-13.06.3-MacOS.dmg'
+  url 'http://software.ultimaker.com/current/Cura-13.06.5-MacOS.dmg'
   homepage 'http://daid.github.com/Cura/'
-  version '13.06.3'
-  sha1 'db5764ecb2678b0a7e0cae4476eb2ac98053136d'
+  version '13.06.5'
+  sha1 'e040e0dc6387d3e9b2ae941fdfc86c39bf4b2db7'
   link 'Cura.app'
 end
