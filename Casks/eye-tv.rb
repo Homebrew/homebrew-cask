ddabcaa798b7952dce157a12bdc3981fa52de41f
Michael Ford <fanquake@gmail.com>
2013-06-28 22:53:21 +0800

    Use Elgato URL for EyeTV download

--- Casks/eye-tv.rb
+++ Casks/eye-tv.rb
@@ -1,5 +1,5 @@
 class EyeTv < Cask
-  url 'https://www.macupdate.com/download/8536/eyetv_361_7120.dmg'
+  url 'http://edgedownloads.elgato.com/eyetvdownloads/support/eyetv_361_7120.dmg'
   homepage 'http://www.elgato.com/'
   version '3.6.1'
   sha1 'da9eed821cf8fb1f5ddb27c155c3514230907b1e'
