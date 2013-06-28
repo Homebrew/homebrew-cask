0d44e5eb3f5469a2c9bdfa93cd00e533a52ab00c
Robert Curth <robert@rocu.de>
2013-06-28 21:25:01 +0200

    Removes Eggscellent cask
    The Eggscellent is not accessible on Amazon any more . The App is AppStore only now

--- Casks/eggscellent.rb
+++
@@ -1,7 +0,0 @@
-class Eggscellent < Cask
-  url 'http://eggscellent.s3.amazonaws.com/betas/Eggscellent_Beta2.zip'
-  homepage 'http://www.eggscellentapp.com/'
-  version 'beta2'
-  sha1 '0fee4fb819fc51541322d671c4a6ca2829137f9f'
-  link 'Eggscellent.app'
-end
