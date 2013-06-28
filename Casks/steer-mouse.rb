1fd6d084045098b0b45cbb6f827e1823feb05658
Chris Chernoff <chris@burnedpixel.com>
2013-06-28 00:26:52 -0700

    Add SteerMouse Cask

---
+++ Casks/steer-mouse.rb
@@ -0,0 +1,7 @@
+class SteerMouse < Cask
+  url 'http://plentycom.jp/ctrl/files_sm/SteerMouse4.1.6.dmg'
+  homepage 'http://plentycom.jp/en/steermouse/'
+  version '4.1.6'
+  sha1 '7cd5569964de3ef10e042cb9048345d9c30f9af1'
+  install 'SteerMouse Installer.app/Contents/Resources/SteerMouse.pkg'
+end
