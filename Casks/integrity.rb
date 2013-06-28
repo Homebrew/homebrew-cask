674dab32c1a83b236cdca9ffa2a7df8845c62b27
Michael Ford <fanquake@gmail.com>
2013-06-28 22:12:16 +0800

    Integrity should be using 'latest' and no_checksum

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -1,7 +1,7 @@
 class Integrity < Cask
   url 'http://peacockmedia.co.uk/integrity/integrity.dmg'
   homepage 'http://peacockmedia.co.uk/integrity/'
-  version '4.1'
-  sha1 'e3c08645d38cc72545202b351e5c243da7d89721'
+  version 'latest'
+  no_checksum
   link 'Integrity.app'
 end
