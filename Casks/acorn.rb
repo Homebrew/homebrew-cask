94c6b036703f421069e32cbdc737800079efebb0
Robert Curth <robert@rocu.de>
2013-06-28 16:30:46 +0200

    Fixes hardcoded version in Acorn cask

--- Casks/acorn.rb
+++ Casks/acorn.rb
@@ -1,7 +1,7 @@
 class Acorn < Cask
   url 'http://flyingmeat.com/download/Acorn.zip'
   homepage 'http://flyingmeat.com/acorn/'
-  version '4.0.4'
-  sha1 'c3c307377bbf11a6782a02c5eeac501f96761b29'
+  version 'latest'
+  no_checksum
   link 'Acorn.app'
 end
