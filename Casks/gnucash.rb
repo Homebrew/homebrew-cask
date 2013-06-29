254fa98451709ee57c5e651b58988b781a60f2b5
Robert Curth <robert@rocu.de>
2013-06-29 09:36:17 +0200

    Fixes changed hash in GnuCash

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -2,6 +2,6 @@ class Gnucash < Cask
   url 'http://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-2.4.13.dmg'
   homepage 'http://www.gnucash.org'
   version '2.4.13'
-  sha1 'ba13690e0e3d9ca0052069fba9a3a8e53d041d99'
+  sha1 '3c17ee79d45c30e7bbc9f1ff9fd8bd72b654e1d1'
   link 'Gnucash.app'
 end
