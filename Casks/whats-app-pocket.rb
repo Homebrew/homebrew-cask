e6f8df6b342d9181a4681ec844b353e331f66692
Fernando Paredes <nanoid.xd@gmail.com>
2013-06-29 10:53:43 -0700

    Add WhatsApp Pocket cask

---
+++ Casks/whats-app-pocket.rb
@@ -0,0 +1,7 @@
+class WhatsAppPocket < Cask
+  url 'http://www.fireebok.com/product/WhatsApp_Pocket_Mac.dmg'
+  homepage 'http://www.fireebok.com/whatsapp-pocket.html'
+  version 'latest'
+  no_checksum
+  link 'WhatsApp Pocket.app'
+end
