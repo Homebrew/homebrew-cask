5ba6ce408aa61c6abce8088c7c9ccb4a5ead0ec6
Fernando Paredes <nanoid.xd@gmail.com>
2013-06-29 10:46:16 -0700

    Add DiskWave cask

---
+++ Casks/disk-wave.rb
@@ -0,0 +1,7 @@
+class DiskWave < Cask
+  url 'http://diskwave.barthe.ph/download/DiskWave_0.4.dmg'
+  homepage 'http://diskwave.barthe.ph/'
+  version '0.4.0'
+  sha1 '68f1413ddd83d047cd801850d011b0469106b3b4'
+  link 'DiskWave.app'
+end
