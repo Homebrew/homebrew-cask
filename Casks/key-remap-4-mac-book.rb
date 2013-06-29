1d80882a6bd70598eaf3b2e35d5d655efa913423
Steffen Becker <webgefrickel@gmail.com>
2013-06-29 20:13:24 +0200

    Add KeyRemap4MacBook in version 8.3.0
    A very nice and free tool for custom keyboard-mappings
    Use install instead of link / pkg file in dmg

---
+++ Casks/key-remap-4-mac-book.rb
@@ -0,0 +1,7 @@
+class KeyRemap4MacBook < Cask
+  url 'https://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.3.0.dmg'
+  homepage 'https://pqrs.org/macosx/keyremap4macbook/'
+  version '8.3.0'
+  sha1 '9b9412d69a7509ae0df7aac672e08e6363fe0ca3'
+  install 'KeyRemap4MacBook.pkg'
+end
