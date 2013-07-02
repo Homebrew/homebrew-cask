c112eb0b9947cdb6464da58186aaf190fd5c8ea2
Vítor Galvão <info@vitorgalvao.com>
2013-07-02 01:27:03 +0100

    added dbeaver cask

---
+++ Casks/dbeaver.rb
@@ -0,0 +1,7 @@
+class Dbeaver < Cask
+  url 'http://dbeaver.jkiss.org/files/dbeaver-2.2.3-macosx.cocoa.x86_64.zip'
+  homepage 'http://dbeaver.jkiss.org/'
+  version '2.2.3'
+  sha1 '4222543b15c335415128fd14987970a6eb694900'
+  link 'dbeaver.app'
+end
