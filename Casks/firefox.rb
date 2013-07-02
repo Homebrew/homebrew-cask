17426123c1546feebfcb263539c31a5ee8cbc91e
John Hsing <douglarek@outlook.com>
2013-07-02 23:55:17 +0800

    Update firefox.rb
    Fix `Error: SHA1 mismatch`

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -2,6 +2,6 @@ class Firefox < Cask
   url 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/22.0/mac/en-US/Firefox%2022.0.dmg'
   homepage 'https://www.mozilla.org/en-US/firefox/'
   version '22.0'
-  sha1 '45ef69d4254c725114c3fa8ffc5e5bef331e46bd'
+  sha1 '710f6aa6e6808b0043223306e7c4433e35254206'
   link 'Firefox.app'
 end
