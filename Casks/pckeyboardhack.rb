165a31009d8ee3c766e53fb31eb0c6705ce048f1
Joseph Pintozzi <joe@Josephs-iMac.local>
2013-07-03 16:12:48 -0500

    Adding in PCKeyboardHack

---
+++ Casks/pckeyboardhack.rb
@@ -0,0 +1,7 @@
+class PCKeyboardHack < Cask
+  url 'https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-9.0.0.dmg'
+  homepage 'https://pqrs.org/macosx/keyremap4macbook/pckeyboardhack.html.en'
+  version '9.0.0'
+  sha1 '7d7c0de605eb542b5529ac11058b5147b285c04c'
+  install 'PCKeyboardHack.pkg'
+end
