2de9ece0064cfd59e45bb5d56bfc2b829820b25a
Vítor Galvão <info@vitorgalvao.com>
2013-07-04 15:39:18 +0100

    simplified url to not include mirror information

--- Casks/paintbrush.rb
+++ Casks/paintbrush.rb
@@ -1,5 +1,5 @@
 class Paintbrush < Cask
-  url 'http://downloads.sourceforge.net/project/paintbrush/Paintbrush%202.x/Paintbrush%202.1.1/Paintbrush-2.1.1.zip?r=http%3A%2F%2Fpaintbrush.sourceforge.net%2Fdownloads%2F&ts=1351154031&use_mirror=superb-dca2'
+  url 'http://downloads.sourceforge.net/project/paintbrush/Paintbrush%202.x/Paintbrush%202.1.1/Paintbrush-2.1.1.zip'
   homepage 'http://paintbrush.sourceforge.net/'
   version '2.1.1'
   sha1 'f8dedc7b22ed017dea43d76b5686b226ebe13ef2'
