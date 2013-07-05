ddedb273477c34de2e38dbefe6afd36f726be46a
taojonesin <taojonesin@nowhere.com>
2013-07-05 01:15:46 -0700

    Added new cask Totalfinder

---
+++ Casks/totalfinder.rb
@@ -0,0 +1,7 @@
+class Totalfinder < Cask
+  url 'http://downloads.binaryage.com/TotalFinder-1.4.16.dmg'
+  homepage 'http://totalfinder.binaryage.com/'
+  version '1.4.16'
+  sha1 '751911b93b70ed9c7da66d405166606c771e3486'
+  install 'TotalFinder.pkg'
+end
