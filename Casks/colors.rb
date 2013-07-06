ca975a9cd478ecbad4c048e530d0a6df548e522f
Robert Curth <robert@rocu.de>
2013-07-06 13:03:34 +0200

    Fixes typo in homepage for colors

--- Casks/colors.rb
+++ Casks/colors.rb
@@ -1,6 +1,6 @@
 class Colors < Cask
   url 'http://mattpatenaude.com/software/colors-1.9.zip'
-  homepage 'ihttp://mattpatenaude.com/'
+  homepage 'http://mattpatenaude.com/'
   version '1.9'
   sha1 '3be1d63a349013cfd9d3aac55d37b7f57a1d7948'
   link 'Colors.app'
