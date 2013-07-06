97c727884b855227d23cb446e57c3bed3bc7deb8
phinze <paul.t.hinze@gmail.com>
2013-07-05 21:55:43 -0500

    add cask for Colors

---
+++ Casks/colors.rb
@@ -0,0 +1,7 @@
+class Colors < Cask
+  url 'http://mattpatenaude.com/software/colors-1.9.zip'
+  homepage 'ihttp://mattpatenaude.com/'
+  version '1.9'
+  sha1 '3be1d63a349013cfd9d3aac55d37b7f57a1d7948'
+  link 'Colors.app'
+end
