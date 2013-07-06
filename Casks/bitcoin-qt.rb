f7b5f8dd8a65c83623f0d2b838e821c77a1ee04f
Dalton Hubble <dghubble@gmail.com>
2013-07-06 03:23:57 -0700

    bitcoin-qt 0.8.3
    closes #669
    
    Signed-off-by: phinze <paul.t.hinze@gmail.com>

---
+++ Casks/bitcoin-qt.rb
@@ -0,0 +1,7 @@
+class BitcoinQt < Cask
+  url 'http://downloads.sourceforge.net/project/bitcoin/Bitcoin/bitcoin-0.8.3/bitcoin-0.8.3-macosx.dmg'
+  homepage 'http://sourceforge.net/projects/bitcoin/'
+  version '0.8.3'
+  sha1 'b898389f119ec30c374afef2e2d5706bf4105bc3'
+  link 'Bitcoin-Qt.app'
+end
