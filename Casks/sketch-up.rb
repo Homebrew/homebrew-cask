8a3a069897d653a318c839897633b274f9834e05
Alexandre COLLIGNON <alexandre@collign.net>
2013-07-07 15:17:50 +0200

    Update Sketch Up to 2013

--- Casks/sketch-up.rb
+++ Casks/sketch-up.rb
@@ -1,7 +1,7 @@
 class SketchUp < Cask
   url 'http://dl.trimble.com/sketchup/SketchUpMEN.dmg'
   homepage 'http://www.sketchup.com/intl/en/'
-  version '8'
-  sha1 '0dbfc19d64119fee86b38bc757b0d2431eef01e0'
-  install 'SketchUp 8.0 Installer.pkg'
+  version '2013'
+  sha1 '839461e9e65181330da8656bd92ae03951a53a9d'
+  link 'SketchUp 2013'
 end
