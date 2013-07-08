0dff1dea85a3c46d5738aa59b86fdd1606fd754d
Gene Locklin <GL@Genes-iMac.local>
2013-07-08 17:10:00 -0500

    Add Copy Cask

---
+++ Casks/copy.rb
@@ -0,0 +1,7 @@
+class Copy < Cask
+  url 'https://copy.com/install/mac/Copy.dmg'
+  homepage 'https://www.copy.com/'
+  version 'Latest'
+  sha1 '1ebb044e5c3e424701b933ca550923724b809b6a'
+  link 'Copy.app'
+end
