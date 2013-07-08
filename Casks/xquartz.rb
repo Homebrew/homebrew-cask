a5ee7113905d47ec569e30031447991b7ea93c8b
Kale Worsley <kale.worsley@trineo.co.nz>
2013-07-08 20:49:15 +1200

    Add XQuartz 2.7.4 cask.

---
+++ Casks/xquartz.rb
@@ -0,0 +1,7 @@
+class Xquartz < Cask
+  url 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.4.dmg'
+  homepage 'http://xquartz.macosforge.org/landing/'
+  version '2.7.4'
+  sha1 '98b2ca8580046d5b91250c5a244c8182437dc9d7'
+  install 'XQuartz.pkg'
+end
