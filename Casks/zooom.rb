eb5157882a94fa9eeb9b0b7b5814912b92075cfb
Jonathan Chu <jonathan.chu@me.com>
2013-07-08 13:22:34 -0400

    Minor fix to incorrect homepage URL.

--- Casks/zooom.rb
+++ Casks/zooom.rb
@@ -1,6 +1,6 @@
 class Zooom < Cask
   url 'http://software.coderage-software.com/zooom/Zooom_2.5.0.dmg'
-  homepage 'http://http://coderage-software.com/zooom'
+  homepage 'http://coderage-software.com/zooom'
   version '2.5.0'
   sha1 '1fa66ef73e7005b368bc4c21e37ae676ed7bc1f5'
   install 'Zooom2.pkg'
