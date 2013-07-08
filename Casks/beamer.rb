fe4690670a8db90978754655bd4a2a4d44b653ba
Wolfgang Lutz <wlutz@dopanic.com>
2013-07-09 01:18:45 +0200

    Beamer.app - Removed version and disabled checksum

--- Casks/beamer.rb
+++ Casks/beamer.rb
@@ -1,7 +1,7 @@
 class Beamer < Cask
   url 'http://beamer-app.com/download'
   homepage 'http://beamer-app.com'
-  version '1.0'
-  sha1 '8b3d991dfc67a7e6583e6558e0e302c73725bde1'
+  version 'latest'
+  no_checksum
   link 'Beamer.app'
 end
