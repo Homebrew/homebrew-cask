3fa3f18899cb1f368be449a68528fcb7ec931c89
Sla <alejandro.giacometti@gmail.com>
2013-07-09 12:30:06 +0100

    Update SublimeText 2 to 2.0.2

--- Casks/sublime-text.rb
+++ Casks/sublime-text.rb
@@ -1,7 +1,7 @@
 class SublimeText < Cask
-  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg'
+  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
   homepage 'http://www.sublimetext.com/2'
-  version '2.0.1'
-  sha1 'b5f91ee4f62d36c0490c021d5fb134b9e7cb3936'
+  version '2.0.2'
+  sha1 '14fba173566d4415f1881a38ad98cf22144f1f63'
   link 'Sublime Text 2.app'
 end
