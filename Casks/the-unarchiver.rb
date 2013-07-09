edc8d3e69b75adf5247e44d455143ada7efa890a
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-09 11:04:10 -0700

    Update The Unarchiver to v. 3.8

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -1,7 +1,7 @@
 class TheUnarchiver < Cask
-  url 'http://theunarchiver.googlecode.com/files/TheUnarchiver3.7.zip'
+  url 'http://theunarchiver.googlecode.com/files/TheUnarchiver3.8.zip'
   homepage 'http://unarchiver.c3.cx/'
-  version '3.7'
-  sha1 'da29b9f591d299cf1f869b7c77aac38908748ad5'
+  version '3.8'
+  sha1 'd4fca3f358df80dff6cccb67de2f5766a6d51d9f'
   link 'The Unarchiver.app'
 end
