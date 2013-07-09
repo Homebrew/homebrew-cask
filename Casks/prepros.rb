d426ae5f6fa8e9a45451a0dfe5e53e69e7bd7d8d
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2013-07-09 21:34:59 +0100

    Prepros application added

---
+++ Casks/prepros.rb
@@ -0,0 +1,7 @@
+class Prepros < Cask
+  url 'http://www.mattclements.co.uk/prepros/Prepros-2.2.0.zip'
+  homepage 'http://alphapixels.com/prepros/'
+  version '2.2.0'
+  sha1 'aa1cabf5d0581cab2e6334dd64270f92fc2caae0'
+  link 'Prepros.app'
+end
