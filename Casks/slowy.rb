42189e85e4b0943f5d609c518737685959ac4ca4
Robert Curth <robert@rocu.de>
2013-07-10 19:48:43 +0200

    Adds cask for Slowy

---
+++ Casks/slowy.rb
@@ -0,0 +1,7 @@
+class Slowy < Cask
+  url 'http://slowyapp.com/downloads/Slowyapp_v1.3.dmg'
+  homepage 'http://slowyapp.com/'
+  version '1.3'
+  sha1 '32273bfca947f116ad68370256efecfb17cb20d3'
+  link 'Slowy.app'
+end
