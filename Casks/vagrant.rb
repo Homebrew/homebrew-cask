8f79f1ee8ec755147b5591b3c599c9fd07d540b6
phinze <paul.t.hinze@gmail.com>
2013-07-10 16:27:35 -0500

    update vagrant to 1.2.3

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,7 +1,7 @@
 class Vagrant < Cask
-  url 'http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/Vagrant-1.2.2.dmg'
+  url 'http://files.vagrantup.com/packages/95d308caaecd139b8f62e41e7add0ec3f8ae3bd1/Vagrant-1.2.3.dmg'
   homepage 'http://www.vagrantup.com'
-  version '1.2.2'
-  sha1 'f8ab15ddc3e38df47778748c2eae8e811ef1bed0'
+  version '1.2.3'
+  sha1 'a1ff84d4f7e3cd91ba065a099cb3fc25085a2862'
   install 'Vagrant.pkg'
 end
