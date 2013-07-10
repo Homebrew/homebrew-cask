8d7291e5cee91a49ffc470b9d566aaba7caf45d0
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-10 16:42:14 -0700

    Correct Poedit url
    I corrected the Poedit url to match specs.

--- Casks/poedit.rb
+++ Casks/poedit.rb
@@ -1,5 +1,5 @@
 class Poedit < Cask
-  url 'http://iweb.dl.sourceforge.net/project/poedit/poedit/1.5/poedit-1.5.7.dmg'
+  url 'http://downloads.sourceforge.net/project/poedit/poedit/1.5/poedit-1.5.7.dmg'
   homepage 'http://www.poedit.net'
   version '1.5.7'
   sha1 'f3b55eba126d482b966ab115e492217f632e5146'
