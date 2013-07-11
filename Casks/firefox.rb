b62e1478ab03b26510ea20e9ba6cb56761ae2ab1
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-10 17:31:26 -0700

    Change to latest version of Firefox

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,7 +1,7 @@
 class Firefox < Cask
-  url 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/22.0/mac/en-US/Firefox%2022.0.dmg'
+  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '22.0'
-  sha1 '45ef69d4254c725114c3fa8ffc5e5bef331e46bd'
+  version 'latest'
+  no_checksum
   link 'Firefox.app'
 end
