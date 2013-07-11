eb6620d3ccd76953b5dfeeff1d3910dba787d0ce
Jonathan Chu <jonathan.chu@me.com>
2013-07-11 11:55:08 -0400

    Add new Cask for GitX-dev application.
    GitX-dev is another fork of GitX.

---
+++ Casks/gitx-dev.rb
@@ -0,0 +1,7 @@
+class GitxDev < Cask
+  url 'http://builds.phere.net/GitX/development/GitX-dev.dmg'
+  homepage 'http://rowanj.github.io/gitx/'
+  version '0.14.95'
+  sha1 'cf94fdb6410673afd1a87f60c0fd59019050ba7a'
+  link 'GitX.app'
+end
