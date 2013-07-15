ec2a637bf4858b060c129d10f79181509b590569
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-15 13:17:57 -0700

    Update Instacast to 1.0.2

--- Casks/instacast.rb
+++ Casks/instacast.rb
@@ -1,7 +1,7 @@
 class Instacast < Cask
-  url 'http://assets.vemedio.com/software/instacast/Instacast-1.0.1-1741.dmg'
+  url 'http://assets.vemedio.com/software/instacast/Instacast-1.0.2-1789.dmg'
   homepage 'http://vemedio.com/products/instacast-mac'
-  version '1.0.1-1741'
-  sha1 '1331855a801e80cd46efff6a8d7a2800adbd8cc6'
+  version '1.0.2 (1789)'
+  sha1 '3dacaab8116af10c8e9cd9692cfc3e97d8b7cb6a'
   link 'Instacast.app'
 end
