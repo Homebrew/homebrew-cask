d0de9f5bdceee13c908bbcc925683090e909fba4
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-15 13:05:33 -0700

    Add 4K Video Downloader cask

---
+++ Casks/fourk-video-downloader.rb
@@ -0,0 +1,7 @@
+class FourkVideoDownloader < Cask
+  url 'http://4kdownload.googlecode.com/files/4kvideodownloader_2.8.dmg'
+  homepage 'http://www.4kdownload.com/products/product-videodownloader'
+  version '2.8'
+  sha1 'f0e78400574b9291ed8ad271d51d231d2d9f3e11'
+  link '4K Video Downloader.app'
+end
