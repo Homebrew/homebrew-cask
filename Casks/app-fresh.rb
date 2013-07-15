c4a8d1de2b750565835f76c7d05fe64be5789c76
Robert Curth <robert@rocu.de>
2013-07-15 19:00:47 +0200

    Adds new cask for AppFresh

---
+++ Casks/app-fresh.rb
@@ -0,0 +1,7 @@
+class AppFresh < Cask
+  url 'http://backend.metaquark.de/download/appfresh'
+  homepage 'http://metaquark.de/appfresh/mac'
+  version 'latest'
+  no_checksum 
+  link 'AppFresh.app'
+end
