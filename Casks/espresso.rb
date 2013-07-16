bee962316cdff59665d011e548825d4e6f3087c0
Pete Schaffner <pjschaffner@gmail.com>
2013-07-16 10:44:27 -0400

    Update version to 'latest'

--- Casks/espresso.rb
+++ Casks/espresso.rb
@@ -1,7 +1,7 @@
 class Espresso < Cask
   url 'http://macrabbit.com/espresso/get/'
   homepage 'http://macrabbit.com/espresso/'
-  version '2.1'
-  sha1 '7574ddd9b543902d02121164e345f62df8e248a3'
+  version 'latest'
+  no_checksum
   link 'Espresso.app'
 end
