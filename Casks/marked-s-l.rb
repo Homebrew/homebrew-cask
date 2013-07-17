c8c3ec0685958c80c33df352ddb2a0ebcd023c86
Enrico Spinielli <enrico.spinielli@gmail.com>
2013-07-18 00:07:11 +0200

    Cask for Marked for Snow Leopard
    Install Marked for Snow Leopard, unfortunately the postfix, SL, cannot be removed.

---
+++ Casks/marked-s-l.rb
@@ -0,0 +1,7 @@
+class MarkedSL < Cask
+  url 'http://assets.markedapp.com/MarkedSL.zip'
+  homepage 'http://support.markedapp.com/kb/documentation/frequently-asked-questions'
+  version 'latest'
+  no_checksum
+  link 'MarkedSL.app'
+end
