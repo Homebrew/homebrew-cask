04ad99cff12570092e89d519e55ad6bbe2ec1e64
Josa Gesell <josa.gesell@ligadigital.com>
2013-07-17 10:18:02 +0200

    Add CocoaPacketAnalyzer cask

---
+++ Casks/cocoa-packet-analyzer.rb
@@ -0,0 +1,7 @@
+class CocoaPacketAnalyzer < Cask
+  url 'http://tastycocoabytes.com/_downloads/CPA_121.dmg'
+  homepage 'http://www.tastycocoabytes.com/cpa/'
+  version '1.21'
+  sha1 '53cdcfa288b0088d97f747eba38574e02ceb03d8'
+  link 'CocoaPacketAnalyzer.app'
+end
