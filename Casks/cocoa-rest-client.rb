d3545fa58b93f1be270ede9017175ea27fbe3a1e
Josa Gesell <josa.gesell@ligadigital.com>
2013-07-17 10:07:57 +0200

    Add CocoaRestClien cask

---
+++ Casks/cocoa-rest-client.rb
@@ -0,0 +1,7 @@
+class CocoaRestClient < Cask
+  url 'https://cocoa-rest-client.googlecode.com/files/CocoaRestClient-1.3.6.dmg'
+  homepage 'https://code.google.com/p/cocoa-rest-client/'
+  version '1.3.6'
+  sha1 '8c8df2c5a9a279b8e4eb03f2d0069e044f69265f'
+  link 'CocoaRestClient.app'
+end
