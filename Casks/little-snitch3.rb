c822390dd23f1c4a020419be7c261ec93562774c
James Pearson <james.pearson@thinkingincode.com>
2013-07-18 13:16:37 +0100

    Added Install and Uninstall parameters
    Signed-off-by: James Pearson <james.pearson@thinkingincode.com>

--- Casks/little-snitch3.rb
+++ Casks/little-snitch3.rb
@@ -3,4 +3,6 @@ class LittleSnitch3 < Cask
   homepage 'http://www.obdev.at/products/littlesnitch/'
   version '3.1.1'
   sha1 '8d42c4aa3164a754e07aeacdccf22ebb9be601a2'
+  install 'Little Snitch Installer.app'
+  uninstall 'Little Snitch Uninstaller.app'
 end
