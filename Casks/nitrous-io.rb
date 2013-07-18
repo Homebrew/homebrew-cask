0afc21e95b223519e9587f570adc5774b2858f09
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-18 15:26:19 -0700

    Add Nitrous.IO cask

---
+++ Casks/nitrous-io.rb
@@ -0,0 +1,7 @@
+class NitrousIo < Cask
+  url 'https://www.nitrous.io/mac/Nitrous-Mac-Latest.zip'
+  homepage 'https://www.nitrous.io/mac'
+  version 'latest'
+  no_checksum
+  link 'Nitrous.app'
+end
