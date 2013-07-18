abf0dc3b3cf78dc26bcfd26e956defdce3c31a44
James Pearson <james.pearson@thinkingincode.com>
2013-07-18 13:05:38 +0100

    Cask for the latest version of Little Snitch 3
    As Little Snitch 3 was a major (paid) upgrade I've created it as a separate cask, to allow people currently using v2 to remain at that version.
    
    Signed-off-by: James Pearson <james.pearson@thinkingincode.com>

---
+++ Casks/little-snitch3.rb
@@ -0,0 +1,6 @@
+class LittleSnitch3 < Cask
+  url 'http://www.obdev.at/downloads/LittleSnitch/LittleSnitch-3.1.1.dmg'
+  homepage 'http://www.obdev.at/products/littlesnitch/'
+  version '3.1.1'
+  sha1 '8d42c4aa3164a754e07aeacdccf22ebb9be601a2'
+end
