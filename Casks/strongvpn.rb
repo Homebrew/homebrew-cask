c14362de156f473fd626e305eab9097660bce00c
James Pearson <james.pearson@thinkingincode.com>
2013-07-18 17:09:08 +0100

    Cask for StrongVPN Client 1.0.1
    Signed-off-by: James Pearson <james.pearson@thinkingincode.com>

---
+++ Casks/strongvpn.rb
@@ -0,0 +1,8 @@
+class Strongvpn < Cask
+  url 'https://mirror2.reliablehosting.com/mac/StrongVPN_Mac_1.0.1.dmg'
+  homepage 'http://strongvpn.com/vpnclient.shtml'
+  version '1.0.1'
+  sha1 '1dd8e9e288578bda664a88e7034af9c995b2a8e2'
+  link 'StrongVPN Client.app'
+end
+
