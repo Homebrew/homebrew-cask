6a110cc13b65ce82779def3ca47a9b8334d8ae60
James Pearson <james@fearmediocrity.co.uk>
2013-07-19 10:24:45 +0100

    Cask for Querious - a MySQL GUI
    Signed-off-by: James Pearson <james@fearmediocrity.co.uk>

---
+++ Casks/querious.rb
@@ -0,0 +1,7 @@
+class Querious < Cask
+  url 'http://www.araelium.com/querious/downloads/Querious.dmg'
+  homepage 'http://www.araelium.com/querious/'
+  version '1.1.4'
+  sha1 'f902aa44a2b535ba4c49b7cfff0f30be8510c746'
+  link 'Querious.app'
+end
