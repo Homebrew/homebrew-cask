4664d2a49edc57f8f71a1446f40849af25f5172c
Robert Curth <robert@rocu.de>
2013-07-21 11:28:17 +0200

    Fixing homepage for Adium

--- Casks/adium.rb
+++ Casks/adium.rb
@@ -1,6 +1,6 @@
 class Adium < Cask
   url 'http://download.adium.im/Adium_1.5.7.dmg'
-  homepage 'http://www.adium.im/'
+  homepage 'https://www.adium.im/'
   version '1.5.7'
   sha1 '93643438f8c5c56ec015dcf252af690ab6ac3ffe'
   link 'Adium.app'
