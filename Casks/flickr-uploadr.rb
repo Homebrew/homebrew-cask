b26925eb2974618603ec8b0d0b45ecb445383196
Jason Garber <jason@sixtwothree.org>
2013-07-21 17:56:43 -0400

    Add Flickr Uploadr Cask

---
+++ Casks/flickr-uploadr.rb
@@ -0,0 +1,7 @@
+class FlickrUploadr < Cask
+  url 'http://h.yimg.com/ce/flickr/FlickrUploadr-3.2/FlickrUploadr-3.2.1-2009.06.10.01-en.dmg'
+  homepage 'http://www.flickr.com/tools/'
+  version '3.2.1'
+  sha1 'ec689291b1275a7b0183ce5e2197363cf50d33c3'
+  link 'Flickr Uploadr.app'
+end
