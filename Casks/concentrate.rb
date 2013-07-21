c93055ebfeb89afc22f5e5b60a69169186715284
Robert Curth <robert@rocu.de>
2013-07-21 11:25:39 +0200

    Adds new cask for Concentrate

---
+++ Casks/concentrate.rb
@@ -0,0 +1,7 @@
+class Concentrate < Cask
+  url 'http://www.getconcentrating.com/builds/concentrate.1.2.2.zip'
+  homepage 'http://www.getconcentrating.com/'
+  version '1.2.2'
+  sha1 '6d5aaafca68398c388034728662e15a5807149aa'
+  link 'Concentrate.app'
+end
