290c0e1ea0de187e6a1ba5a11c6565ed401ac328
Robert Curth <robert@rocu.de>
2013-07-22 18:44:20 +0200

    Adds cask for IPSecuritas

---
+++ Casks/ip-securitas.rb
@@ -0,0 +1,7 @@
+class IpSecuritas < Cask
+  url 'http://www.lobotomo.com/products/downloads/IPSecuritas.dmg'
+  homepage 'http://www.lobotomo.com/products/IPSecuritas/'
+  version 'latest'
+  no_checksum
+  link 'IPSecuritas.app'
+end
