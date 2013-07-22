d7c252983bf3e3faec08836eece33f25477d6490
Gil Desmarais <gil.desmarais@gmail.com>
2013-07-22 23:57:37 +0200

    Add KeyFinder 1.22

---
+++ Casks/keyfinder.rb
@@ -0,0 +1,7 @@
+class Keyfinder < Cask
+  url 'http://www.ibrahimshaath.co.uk/keyfinder/KeyFinder-OSX.zip'
+  homepage 'http://www.ibrahimshaath.co.uk/keyfinder/'
+  version '1.22'
+  sha1 '9e49554c36bda6ae7e4f248fb72ee95951103895'
+  link 'KeyFinder.app'
+end
