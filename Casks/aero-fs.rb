a75ea9c235b92fe76892d3dac10d998504b947ea
Fernando Paredes <nanoid.xd@gmail.com>
2013-07-23 16:49:56 -0700

    Add AeroFS cask

---
+++ Casks/aero-fs.rb
@@ -0,0 +1,7 @@
+class AeroFs < Cask
+  url 'https://dsy5cjk52fz4a.cloudfront.net/AeroFSInstall.dmg'
+  homepage 'https://www.aerofs.com/'
+  version 'latest'
+  no_checksum
+  link 'AeroFS.app'
+end
