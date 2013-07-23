622cc567ca405e209f51cbbaa7b8bc567b3f732c
Roger Ragulan Rajaratnam <roger@ragusource.com>
2013-07-23 13:21:11 +0100

    Add cask for MongoHub (https://github.com/fotonauts/MongoHub-Mac)
    Add cask for MongoHub (https://github.com/fotonauts/MongoHub-Mac)

--- Casks/mongohub.rb
+++ Casks/mongohub.rb
@@ -1,7 +1,7 @@
 class Mongohub < Cask
   url 'https://mongohub.s3.amazonaws.com/MongoHub.zip'
   homepage 'https://github.com/fotonauts/MongoHub-Mac'
-  version '2.5.9'
-  sha1 'b6ef674e3eb0b9fe0a9d743610a0b623a713a3fa'
-  link :app, 'MongoHub.app'
+  version 'latest'
+  no_checksum
+  link 'MongoHub.app'
 end
