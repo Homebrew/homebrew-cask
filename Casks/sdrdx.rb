01a324632e9cd66f645ff0eebff02bfdb86dc39a
xyb <xieyanbo@gmail.com>
2013-07-25 23:58:13 +0800

    no version but latest

--- Casks/sdrdx.rb
+++ Casks/sdrdx.rb
@@ -1,7 +1,7 @@
 class Sdrdx < Cask
   url 'http://fyngyrz.com/SdrDx-AA7AS-Light.zip'
   homepage 'http://fyngyrz.com/?p=915'
-  version '2.12q'
-  sha1 '4131202f03da899ac96ef45480ae2b69d0a8aab0'
+  version 'latest'
+  no_checksum
   link 'SdrDx.app'
 end
