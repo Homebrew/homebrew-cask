dc4b3f3ed75b1d1b363e9ebb353ea5d355fe7736
xyb <xieyanbo@gmail.com>
2013-07-25 23:52:31 +0800

    OSX port of the awesome gqrx SDR software

---
+++ Casks/gqrx.rb
@@ -0,0 +1,7 @@
+class Gqrx < Cask
+  url 'http://dekar.wc3edit.net/wp-content/uploads/2012/09/gqrx_8.dmg'
+  homepage 'http://dekar.wc3edit.net/2012/09/30/osx-port-of-the-awesome-gqrx-sdr-software/'
+  version '8'
+  sha1 '66b9f6952aff5cb0bc995556c633b3cc7f5d990e'
+  link 'gqrx.app'
+end
