04905d03907a8c92158a7ef37ff72703ff0a4580
John Schulz <jschulz@twitter.com>
2013-07-25 12:01:15 -0700

    Add Shuttle (SSH shortcut menu for OS X)

---
+++ Casks/shuttle.rb
@@ -0,0 +1,7 @@
+class Shuttle < Cask
+  url 'https://github.com/fitztrev/shuttle/releases/download/v1.1.2/Shuttle.dmg'
+  homepage 'http://fitztrev.github.io/shuttle/'
+  version '1.1.2'
+  sha1 '70ef2c86f6af51621b165bb642c49d0fecf90d90'
+  link 'Shuttle.app'
+end
