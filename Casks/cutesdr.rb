49bb2f22957b07c6e9ab901241ce281cb1047961
xyb <xieyanbo@gmail.com>
2013-07-25 22:41:13 +0800

    CuteSdr, the QT based SDR project from RF-Space.

---
+++ Casks/cutesdr.rb
@@ -0,0 +1,7 @@
+class Cutesdr < Cask
+  url 'http://downloads.sourceforge.net/project/cutesdr/CuteSdr110.dmg'
+  homepage 'http://sourceforge.net/projects/cutesdr'
+  version '1.10'
+  sha1 'e6153abd1186b3e0fad0111d339a8e41a6c51418'
+  link 'CuteSdr.app'
+end
