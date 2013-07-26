c62c3bbba6edf20ef1fb0da546bad405b04376af
xyb <xieyanbo@gmail.com>
2013-07-26 11:00:17 +0800

    Serial Tools is a set of serial port tools for Mac OS X

---
+++ Casks/serial-tools.rb
@@ -0,0 +1,7 @@
+class SerialTools < Cask
+  url 'http://www.w7ay.net/site/Downloads/Serial%20Tools/Serial%20Tools%20app.dmg'
+  homepage 'http://www.w7ay.net/site/Applications/Serial%20Tools/'
+  version 'latest'
+  no_checksum
+  link 'Serial Tools.app'
+end
