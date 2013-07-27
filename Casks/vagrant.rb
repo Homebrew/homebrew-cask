64fd73e5c329747c1e3af0d8c536ff848dfbd76a
Linh Tran <linh.mtran168@live.com>
2013-07-27 13:51:44 +0700

    Update vagrant to version 1.2.6

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,6 +1,5 @@
 class Vagrant < Cask
-    url 'http://files.vagrantup.com/packages/22b76517d6ccd4ef232a4b4ecbaa276aff8037b8/Vagrant-1.2.6.dmg
-'
+  url 'http://files.vagrantup.com/packages/22b76517d6ccd4ef232a4b4ecbaa276aff8037b8/Vagrant-1.2.6.dmg'
   homepage 'http://www.vagrantup.com'
   version '1.2.6'
   sha1 '5f3e1bc5761b41e476bc8035f5ba03d42c0e12f0'
