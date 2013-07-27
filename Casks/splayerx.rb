00a4e13df7fece534293a3332562b9de02e26ffc
xyb <xieyanbo@gmail.com>
2013-07-27 12:42:01 +0800

    SplayerX for Mac

---
+++ Casks/splayerx.rb
@@ -0,0 +1,7 @@
+class Splayerx < Cask
+  url 'http://cdn.bitbucket.org/Tomasen/splayerx/downloads/SPlayerX-1097.zip'
+  homepage 'https://bitbucket.org/Tomasen/splayerx/wiki/Home'
+  version '1097'
+  sha1 '26e9879532d0b61588050e156d517134eb6edad9'
+  link 'SPlayerX.app'
+end
