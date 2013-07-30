f8ffc832a372a4e04741f81225eb7250f2a31931
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-30 23:39:44 +0200

    New cask: Audio Hijack Pro
    New cask: Audio Hijack Pro, an audio software for recording
    applications.

---
+++ Casks/audio-hijack-pro.rb
@@ -0,0 +1,7 @@
+class AudioHijackPro < Cask
+  url 'http://www.rogueamoeba.com/audiohijackpro/download.php'
+  homepage 'http://www.rogueamoeba.com/audiohijackpro/'
+  version 'latest' 
+  no_checksum
+  link 'Audio Hijack Pro.app'
+end
