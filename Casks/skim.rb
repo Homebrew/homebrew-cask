543f4bce313c09df4dcc3cb7a0586bc9ee72b994
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 17:51:31 +0200

    Shortened url of skim
    Shortened url of skim

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,5 +1,5 @@
 class Skim < Cask
-  url 'http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.4.4/Skim-1.4.4.dmg?r=&ts=1375285627&use_mirror=garr'
+  url 'http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.4.4/Skim-1.4.4.dmg'
   homepage 'http://skim-app.sourceforge.net/'
   version '1.4.4'
   sha1 '686232d5d5e1d906f468db7ecee8f8fe337d6272'
