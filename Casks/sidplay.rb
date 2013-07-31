494fe2280eb301f9edac3e157406a7d6eaef7b55
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 17:45:05 +0200

    New cask: sidplay
    New cask: sidplay, a player/tracker for SID-files,
    an old tracker format for C64 files

---
+++ Casks/sidplay.rb
@@ -0,0 +1,7 @@
+class Sidplay < Cask
+  url 'http://www.twinbirds.com/sidplay/SIDPLAY4.zip'
+  homepage 'http://www.sidmusic.org/sidplay/mac/'
+  version '4.1.4'
+  sha1 'ced8fc73c8cdbe07b3c0776322ad6fba940c24cb'
+  link 'SIDPLAY.app'
+end
