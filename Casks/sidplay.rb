3a6ef078423a3d0098fc5d2c38fe11f1c49b30ca
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 18:06:05 +0200

    changed version/checksum for sidplay
    changed version/checksum for sidplay as suggestion from Fernando Paredes

--- Casks/sidplay.rb
+++ Casks/sidplay.rb
@@ -1,7 +1,7 @@
 class Sidplay < Cask
   url 'http://www.twinbirds.com/sidplay/SIDPLAY4.zip'
   homepage 'http://www.sidmusic.org/sidplay/mac/'
-  version '4.1.4'
-  sha1 'ced8fc73c8cdbe07b3c0776322ad6fba940c24cb'
+  version 'latest'
+  no_checksum
   link 'SIDPLAY.app'
 end
