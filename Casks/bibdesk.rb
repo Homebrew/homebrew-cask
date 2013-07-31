553235793cf162da30e69972bcfeb3fa89aea245
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 19:11:08 +0200

    Fixed url of bibdesk
    Fixed url of bibdesk

--- Casks/bibdesk.rb
+++ Casks/bibdesk.rb
@@ -1,5 +1,5 @@
 class Bibdesk < Cask
-  url 'http://downloads.sourceforge.net/project/bibdesk/BibDesk/BibDesk-1.6.1/BibDesk-1.6.1.dmg?r=http%3A%2F%2Fbibdesk.sourceforge.net'
+  url 'http://downloads.sourceforge.net/project/bibdesk/BibDesk/BibDesk-1.6.1/BibDesk-1.6.1.dmg'
   homepage 'http://bibdesk.sourceforge.net/'
   version '1.6.1'
   sha1 '438c4ec8ce4ff95983157fc8508515d54b494568'
