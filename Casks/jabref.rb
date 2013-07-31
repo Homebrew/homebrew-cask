c8c903fe80db278d62e8d5dd128330e552d7bd6a
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 18:54:51 +0200

    new cask: jabref
    new cask: jabref, an open source bibliography reference manager

---
+++ Casks/jabref.rb
@@ -0,0 +1,7 @@
+class Jabref < Cask
+  url 'http://downloads.sourceforge.net/project/jabref/jabref/2.9.2/JabRef-2.9.2-OSX.zip'
+  homepage 'http://jabref.sourceforge.net/'
+  version '2.9.2'
+  sha1 '2932cfd21a2fb44f024ef865e6c90c21f7e444e4'
+  link 'JabRef.app'
+end
