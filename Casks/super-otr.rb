b3d6251efce156ecb16c99e4e19d88f4558485ca
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 18:08:44 +0200

    new cask: super-otr
    new cask: super-otr, a decorder for the tv-recording
    site http://www.onlinetvrecorder.de

---
+++ Casks/super-otr.rb
@@ -0,0 +1,7 @@
+class SuperOtr < Cask
+  url 'http://apfel-a.macbay.de/wordpress/wp-content/plugins/download-monitor/download.php?id=Super-OTR-0.9.6.0b79.zip'
+  homepage 'http://apfel-a.macbay.de/super-otr/'
+  version '0.9.6.0b76'
+  sha1 'e7140a44087ba7419308d0698ef40c3aa7a426b5'
+  link 'Super OTR.app'
+end
