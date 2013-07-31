82b701bf3796225b361a951a7542d7ee101fe5a0
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 18:28:19 +0200

    new cask: teamspeak-client
    new cask: teamspeak-client, a voice software to
    communicate, popular with gamers

---
+++ Casks/teamspeak-client.rb
@@ -0,0 +1,7 @@
+class TeamspeakClient < Cask
+  url 'http://ftp.4players.de/pub/hosted/ts3/releases/3.0.11/TeamSpeak3-Client-macosx-3.0.11.dmg'
+  homepage 'http://www.teamspeak.com/'
+  version '3.0.11'
+  sha1 '7b4ec12215ad00fca804daf7a91593835768d0b9'
+  link 'TeamSpeak 3 Client.app'
+end
