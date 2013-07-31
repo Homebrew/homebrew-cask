10f877f87421a037a6bfa15038e900eccb052602
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 15:02:02 +0200

    New cask: pi-filler
    New cask: pi-filler, a tool to write SD cards for the
    raspberry pi project

---
+++ Casks/pi-filler.rb
@@ -0,0 +1,7 @@
+class PiFiller < Cask
+  url 'http://ivanx.com/raspberrypi/files/PiFiller.zip'
+  homepage 'http://ivanx.com/raspberrypi'
+  version '1.1.1'
+  sha1 '88bf9376f312ecea84a430123a300d60923e55ef'
+  link 'Pi Filler.app'
+end
