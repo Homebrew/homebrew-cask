a59391e790794271acc6b9e3b43b0dd0e5acfb50
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 18:14:21 +0200

    new cask: synthesia
    new cask: synthesia, a midi-instrument learning software that
    is inspired by inspired by Guitar Hero

---
+++ Casks/synthesia.rb
@@ -0,0 +1,7 @@
+class Synthesia < Cask
+  url 'http://cdn.synthesiagame.com/files/Synthesia-8.5.dmg'
+  homepage 'http://www.synthesiagame.com'
+  version '8.5'
+  sha1 '42aa31e9f26eef3dccbeebf9fdbcd3c910e187aa'
+  link 'Synthesia.app', 'SynthesiaConfig.app'
+end
