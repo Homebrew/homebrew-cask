9a86174e169d74fb56ccb52d731d4bbcd498562b
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 16:53:09 +0200

    New cask: jDownloader
    New cask: jDownloader, a downloading utility

---
+++ Casks/j-downloader.rb
@@ -0,0 +1,7 @@
+class JDownloader < Cask
+  url 'http://94.23.204.158/jDownloader_Java_1_5.dmg'
+  homepage 'http://jdownloader.org/'
+  version 'latest'
+  no_checksum
+  link 'jDownloader.app'
+end
