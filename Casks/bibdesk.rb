4b4d6091b354ef96fe891e171cf81848729c27f1
Philipp Weißmann <git@philipp-weissmann.de>
2013-07-31 18:51:22 +0200

    new cask: bibdesk
    new cask: bibdesk, a BibTeX editor

---
+++ Casks/bibdesk.rb
@@ -0,0 +1,7 @@
+class Bibdesk < Cask
+  url 'http://downloads.sourceforge.net/project/bibdesk/BibDesk/BibDesk-1.6.1/BibDesk-1.6.1.dmg?r=http%3A%2F%2Fbibdesk.sourceforge.net'
+  homepage 'http://bibdesk.sourceforge.net/'
+  version '1.6.1'
+  sha1 '438c4ec8ce4ff95983157fc8508515d54b494568'
+  link 'BibDesk.app'
+end
