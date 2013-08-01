f40578e200e523b429da682f4aaa9347ac70e4e4
Ma <ma@amp.fritz.box>
2013-08-01 11:15:25 +0200

    fourk-video-downloader sha1 changed

--- Casks/fourk-video-downloader.rb
+++ Casks/fourk-video-downloader.rb
@@ -2,6 +2,6 @@ class FourkVideoDownloader < Cask
   url 'http://4kdownload.googlecode.com/files/4kvideodownloader_2.8.dmg'
   homepage 'http://www.4kdownload.com/products/product-videodownloader'
   version '2.8.2'
-  sha1 'bf4475a6eaef018303bd1b984ae3e29bcb35f5d5'
+  sha1 '599b0e1c349ca7e0f439ade689a0318357a898e5'
   link '4K Video Downloader.app'
 end
