6eac396ceec14fbe5a6ddba0b47e34a4e9f009bd
Michael Ford <fanquake@gmail.com>
2013-08-01 13:28:38 +0800

    Update GPGTools to 2013.07.31

--- Casks/gpgtools.rb
+++ Casks/gpgtools.rb
@@ -1,8 +1,8 @@
 class Gpgtools < Cask
-  url 'https://s3.amazonaws.com/gpgtools/GPG%20Suite%20-%202013.07.27.dmg'
+  url 'https://s3.amazonaws.com/gpgtools/GPG%20Suite%20-%202013.07.31.dmg'
   homepage 'https://gpgtools.org/index.html'
-  version '2013.07.27'
-  sha1 '45976a228ca13447a4a366f1b9742e44807f9a6e'
+  version '2013.07.31'
+  sha1 '5390440f35b9ab1f7430d40b4906f9f0e47f9779'
   install 'Install.pkg'
   uninstall(
     :pkgutil => 'org.gpgtools.*'
