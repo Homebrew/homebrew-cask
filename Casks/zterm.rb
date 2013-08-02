53f4da8e4ba8f6d4e0f1024249fc7224641859cf
xyb <xieyanbo@gmail.com>
2013-08-03 00:09:35 +0800

    ZTerm is a terminal emulation program for the Macintosh

---
+++ Casks/zterm.rb
@@ -0,0 +1,7 @@
+class Zterm < Cask
+  url 'http://www.dalverson.com/zterm/ZTerm1.2.dmg'
+  homepage 'http://www.dalverson.com/zterm/'
+  version '1.2'
+  sha1 '7d350dac27eaa7f39d676f933e64515d55bc3844'
+  link 'ZTerm.app'
+end
