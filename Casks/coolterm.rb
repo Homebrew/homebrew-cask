8c97295b8d71127998a0f2ea533fcf4469c6bea6
xyb <xieyanbo@gmail.com>
2013-08-03 00:22:21 +0800

    CoolTerm is a simple serial port terminal application

---
+++ Casks/coolterm.rb
@@ -0,0 +1,7 @@
+class Coolterm < Cask
+  url 'http://freeware.the-meiers.org/CoolTermMac.zip'
+  homepage 'http://freeware.the-meiers.org/'
+  version 'latest'
+  sha1 'd89e2620cf2b496e7b378c5a982599c50347ec85'
+  link 'CoolTermMac/CoolTerm.app'
+end
