eee8ce26594182bda4bb3f437b82e0859b38ba16
xyb <xieyanbo@gmail.com>
2013-08-03 11:38:49 +0800

    fix checksum

--- Casks/coolterm.rb
+++ Casks/coolterm.rb
@@ -2,6 +2,6 @@ class Coolterm < Cask
   url 'http://freeware.the-meiers.org/CoolTermMac.zip'
   homepage 'http://freeware.the-meiers.org/'
   version 'latest'
-  sha1 'd89e2620cf2b496e7b378c5a982599c50347ec85'
+  no_checksum
   link 'CoolTermMac/CoolTerm.app'
 end
