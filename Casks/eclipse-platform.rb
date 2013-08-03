90931d5443ce808b156f5db2996ad464fb4cac44
bmcustodio <bruno@brunomcustodio.com>
2013-08-03 16:02:57 +0100

    Adding Eclipse Platform 4.3.
    Adds support for installing Eclipse Platform 4.3 via Cask.

---
+++ Casks/eclipse-platform.rb
@@ -0,0 +1,7 @@
+class EclipsePlatform < Cask
+  url 'http://download.eclipse.org/eclipse/downloads/drops4/R-4.3-201306052000/eclipse-platform-4.3-macosx-cocoa-x86_64.tar.gz'
+  homepage 'http://eclipse.org'
+  version '4.3'
+  sha1 '539aa1dd327478c56c1cbca9145c138d27e6a1e0'
+  link 'Eclipse.app'
+end
