47ce930b9cf3492b4bdcb70bee2c5bec42998668
Fernando Paredes <nano@fdp.io>
2013-08-02 17:53:47 -0700

    Add QuickCast cask

---
+++ Casks/quick-cast.rb
@@ -0,0 +1,7 @@
+class QuickCast < Cask
+  url 'https://github.com/petetak/QuickCast/releases/download/0.75/QuickCast.zip'
+  homepage 'http://quickcast.io/'
+  version '0.75'
+  sha1 '9b8f7f8e82c23520ef8268e339b67d43c7dd706d'
+  link 'QuickCast.app'
+end
