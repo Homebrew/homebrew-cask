6c03f831734d5db612e5efb0284ca6eafc4cf80b
Steven Hsu <admin@stevenhsu.ca>
2013-08-02 21:08:40 -0700

    Added Komodo Edit 8

---
+++ Casks/komodo-edit.rb
@@ -0,0 +1,7 @@
+class KomodoEdit < Cask
+  url 'http://downloads.activestate.com/Komodo/releases/8.0.2/Komodo-Edit-8.0.2-12590-macosx-x86_64.dmg'
+  homepage 'http://www.activestate.com/komodo-edit'
+  version '8.0.2'
+  sha1 '2ee60ac9ad4519b322a5b9ec49208ff0ae6ee6ad'
+  link 'Komodo Edit 8.app'
+end
