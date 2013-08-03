dd6184038d1998b8bf319ee87f10b22f34c46c3a
bmcustodio <bruno@brunomcustodio.com>
2013-08-03 11:24:40 +0100

    Adding 'Cinch'.
    Adds support for installing Cinch via Cask.

---
+++ Casks/cinch.rb
@@ -0,0 +1,7 @@
+class Cinch < Cask
+  url 'http://www.irradiatedsoftware.com/download/Cinch.zip'
+  homepage 'http://www.irradiatedsoftware.com/cinch/'
+  version 'latest'
+  no_checksum
+  link 'Cinch.app'
+end
