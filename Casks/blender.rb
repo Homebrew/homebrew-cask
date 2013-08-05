8f38000dfd03fe58b197adbb1ca288051c6e5376
Michael Ford <fanquake@gmail.com>
2013-08-05 11:33:27 +0800

    Blender 2.68a

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,7 +1,7 @@
 class Blender < Cask
-  url 'http://download.blender.org/release/Blender2.67/blender-2.67b-OSX_10.6-x86_64.zip'
+  url 'http://download.blender.org/release/Blender2.68/blender-2.68a-OSX_10.6-x86_64.zip'
   homepage 'http://www.blender.org/'
-  version '2.67b'
-  sha1 'd662936d26ec9c8b6a49358486da96cb215f2c3d'
+  version '2.68a'
+  sha1 '0ba47d7cfa740c5cb6b9b9d6a5bd96c49c35176c'
   link 'blender.app', 'blenderplayer.app'
 end
