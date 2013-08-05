76263a7723f960f4823d32bacb50f9a2c90321e1
Michael Ford <fanquake@gmail.com>
2013-08-05 22:34:07 +0800

    HandsOff 2.0.8

--- Casks/hands-off.rb
+++ Casks/hands-off.rb
@@ -1,7 +1,7 @@
 class HandsOff < Cask
-  url 'https://www.metakine.com/files/Hands%20Off!%20v2.0.7.dmg'
+  url 'https://www.metakine.com/files/Hands%20Off!%20v2.0.8.dmg'
   homepage 'http://www.metakine.com/products/handsoff/'
-  version '2.0.7'
-  sha1 'd2d7e1086204d0b3d8a9c1ee9e7e4ad8b069260a'
+  version '2.0.8'
+  sha1 '8c41cf964ce5b949c4912d322b0bbe5a34d2a5b6'
   link 'Hands Off!.app'
 end
