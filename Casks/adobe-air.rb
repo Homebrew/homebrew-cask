4133495ed3f0320fd33a9f646ceff652146ecf84
Michael Ford <fanquake@gmail.com>
2013-08-05 09:55:24 +0800

    Adobe Air 3.8

--- Casks/adobe-air.rb
+++ Casks/adobe-air.rb
@@ -1,7 +1,7 @@
 class AdobeAir < Cask
-  url 'http://airdownload.adobe.com/air/mac/download/3.7/AdobeAIR.dmg'
+  url 'http://airdownload.adobe.com/air/mac/download/3.8/AdobeAIR.dmg'
   homepage 'https://get.adobe.com/air/'
-  version '3.7'
-  sha1 '1cff6f31be6de547acc6f5cd59c59c1645322a71'
+  version '3.8'
+  sha1 '7d4c785d02f6fde6845549260574cef4eeba00b2'
   install 'Adobe AIR Installer.app'
 end
