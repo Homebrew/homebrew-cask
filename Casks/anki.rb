5a8aa001c5503be70be8654d8bc9197b1e74620e
Michael Ford <fanquake@gmail.com>
2013-08-05 10:00:18 +0800

    Anki 2.0.12

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,7 +1,7 @@
 class Anki < Cask
-  url 'https://anki.googlecode.com/files/anki-2.0.11.dmg'
+  url 'https://anki.googlecode.com/files/anki-2.0.12.dmg'
   homepage 'http://ankisrs.net/'
-  version '2.0.11'
-  sha1 '1d5b3618499228df4fa7f4c5d8e35931981dff35'
+  version '2.0.12'
+  sha1 '443c5b50cf13286dc021802c6ffef746fbf54a00'
   link 'Anki.app'
 end
