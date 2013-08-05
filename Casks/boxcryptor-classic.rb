524e2e18520768e9a30e322f2cd4ef25f908bda8
gregstallings <gregstallings@gmail.com>
2013-08-05 00:38:09 -0700

    Add BoxcryptorClassic cask
    Add BoxcryptorClassic cask, a common secure directory solution for
    dropbox and other cloud services.

---
+++ Casks/boxcryptor-classic.rb
@@ -0,0 +1,7 @@
+class BoxcryptorClassic < Cask
+  url 'https://www.boxcryptor.com/download/Boxcryptor_Installer.dmg'
+  homepage 'https://www.boxcryptor.com/en/boxcryptor-classic'
+  version 'latest'
+  no_checksum
+  link 'Boxcryptor Classic.app'
+end
