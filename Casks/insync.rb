6e5894f8da3c5c7ec195411086fbec2437f0c9dd
Michael Ford <fanquake@gmail.com>
2013-08-05 09:54:02 +0800

    Insync 1.0.20.31600

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,7 +1,7 @@
 class Insync < Cask
-  url 'http://s.insynchq.com/builds/Insync-1.0.15.31570.dmg'
-  homepage 'http://insynchq.com/'
-  version '1.0.15.31570'
-  sha1 'a4b594b165caa3f8df093880a4d0228e98d38264'
+  url 'http://s.insynchq.com/builds/Insync-1.0.20.31600.dmg'
+  homepage 'https://insynchq.com/'
+  version '1.0.20.31600'
+  sha1 '799aafba7d5d04075ce935aef67aa541aeaf1b0e'
   link 'Insync.app'
 end
