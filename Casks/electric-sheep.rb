6e4f6602f3c64a4e82638d5b490503736349eb30
Yuval Adam <yuv.adm@gmail.com>
2013-08-06 19:25:21 +0300

    Add formula for Electric Sheep
    Version 2.7b34

---
+++ Casks/electric-sheep.rb
@@ -0,0 +1,7 @@
+class ElectricSheep < Cask
+  url 'http://electricsheep.googlecode.com/files/electricsheep-2.7b34.dmg'
+  homepage 'http://www.electricsheep.org'
+  version '2.7b34'
+  sha1 'baeaf679882b4d4793f014d356f017420c42b806'
+  install 'Electric Sheep.mpkg'
+end
