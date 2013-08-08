b9618c3bc5042d496d11241100e7d882813dba79
Michael Ford <fanquake@gmail.com>
2013-08-08 15:25:26 +0800

    Snippets.me 0.8

--- Casks/snippets-me.rb
+++ Casks/snippets-me.rb
@@ -1,7 +1,7 @@
 class SnippetsMe < Cask
-  url 'http://snippets.me/download/mac/Snippets-426.zip'
+  url 'http://snippets.me/download/mac/Snippets-439.zip'
   homepage 'http://snippets.me/'
-  version '0.7.3'
-  sha1 '834153d3937038005a2ef07156825219545afc38'
+  version '0.8'
+  sha1 '504f8bda760131ab6b0d454f000bc1fe7fd5710d'
   link 'Snippets.app'
 end
