dbc41df12d766493e2339622c41297f398e64877
Michael Ford <fanquake@gmail.com>
2013-08-08 11:38:20 +0800

    Correct ViMediaManager version

--- Casks/vi-media-manager.rb
+++ Casks/vi-media-manager.rb
@@ -1,7 +1,7 @@
 class ViMediaManager < Cask
   url 'http://mariusth.channelwood.org/vimediamanager/files/vimediamanager_v0.7a11c.dmg'
   homepage 'http://mariusth.channelwood.org/vimediamanager/'
-  version '0.7a11'
+  version '0.7a11c'
   sha1 '85aad6a58e2f6d1007b7ed6a3bf4601f3291283a'
   link 'ViMediaManager.app'
 end
