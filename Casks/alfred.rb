1fa7ce40be9c133e20bf437eaff535b602f1df0f
Fernando Paredes <nano@fdp.io>
2013-08-09 07:21:13 -0700

    Update Alfred version number
    Change the dot following 7 to _

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,7 +1,7 @@
 class Alfred < Cask
   url 'http://cachefly.alfredapp.com/Alfred_2.0.7_205.zip'
   homepage 'http://www.alfredapp.com/'
-  version '2.0.7.205'
+  version '2.0.7_205'
   sha1 '004c9950bb831c670273f1fdc763e184607e8a43'
   link 'Alfred 2.app', 'Alfred Preferences.app'
 end
