d87483b476050a48ebba8b148614bc9a73b4bc27
Michael Walter <michael.walter@gmail.com>
2013-08-09 23:07:46 +0200

    Added cask for GitX-dev (rowanj's fork).

---
+++ Casks/gitx-dev.rb
@@ -0,0 +1,7 @@
+class GitxDev < Cask
+  url 'https://github.com/rowanj/gitx/releases/download/builds/0.14/95/GitX-dev-95.dmg'
+  homepage 'https://github.com/rowanj/gitx'
+  version '0.14.95'
+  sha1 'cf94fdb6410673afd1a87f60c0fd59019050ba7a'
+  link 'GitX.app'
+end
