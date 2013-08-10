52689448c2ea9cc1f5fdddaefdecdfabe83f8aac
Michael Ford <fanquake@users.noreply.github.com>
2013-08-10 15:23:41 +0800

    Dia 0.97.2-7

--- Casks/dia.rb
+++ Casks/dia.rb
@@ -1,7 +1,7 @@
 class Dia < Cask
-  url 'http://downloads.sourceforge.net/dia-installer/dia/0.97.2/Dia-0.97.2-5.dmg'
+  url 'http://downloads.sourceforge.net/dia-installer/dia/0.97.2/Dia-0.97.2-7.dmg'
   homepage 'http://dia-installer.de/'
-  version '0.97.2-5'
-  sha1 '20bcc65b63f44aaa2788a7183c4a254f6a57d57d'
+  version '0.97.2-7'
+  sha1 '1af9561c4e0382e537c7f622e657ced14300a1d4'
   link 'Dia.app'
 end
