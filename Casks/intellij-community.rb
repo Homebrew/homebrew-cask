1bb032f280688f6d190e6d1761b98c18e8beb443
Michael Ford <fanquake@users.noreply.github.com>
2013-08-11 16:14:51 +0800

    IntelliJ Community 12.1.4

--- Casks/intellij-community.rb
+++ Casks/intellij-community.rb
@@ -1,7 +1,7 @@
 class IntellijCommunity < Cask
-  url 'http://download.jetbrains.com/idea/ideaIC-12.1.3.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIC-12.1.4.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '12.1.3'
-  sha1 'f2af9c06b13620004c03317e46484d5ab275eb02'
+  version '12.1.4'
+  sha1 'fb0eae00b1e1f05712f512820c73e4636e8207a1'
   link 'IntelliJ IDEA 12 CE.app'
 end
