bc34f6d996ef0673a46c2b68dc64e8a31ffd72a3
Gene Locklin <GeneLocklin@Genes-iMac.local>
2013-08-11 15:15:58 -0500

    Add Seafile

---
+++ Casks/seafile.rb
@@ -0,0 +1,7 @@
+class Seafile < Cask
+  url 'http://seafile.googlecode.com/files/seafile-1.8.0.dmg'
+  homepage 'http://seafile.com/'
+  version '1.8.0'
+  sha1 '3bdb9b8fda81ce241dd6409f120f691327f5b2b3'
+  link 'seafile.app'
+end
