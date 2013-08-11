5f5537c782df7caad4270db3fb3c95c42936b6dd
Michael Ford <fanquake@users.noreply.github.com>
2013-08-11 15:59:00 +0800

    Cloak 1.2.3

--- Casks/cloak.rb
+++ Casks/cloak.rb
@@ -1,7 +1,7 @@
 class Cloak < Cask
-  url 'https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-1.2.dmg'
+  url 'https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-1.2.3.dmg'
   homepage 'https://www.getcloak.com'
-  version '1.2'
-  sha1 '4fce763c135f571cb42daf0e4c05822ecb9fa94e'
+  version '1.2.3'
+  sha1 '3761f0b73442307aabbc0eaacc7779f2fff69da6'
   link 'Cloak.app'
 end
