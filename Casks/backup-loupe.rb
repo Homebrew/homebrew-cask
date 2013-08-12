8c275ee8d31068de3f9565223f4bed2f1db74e34
Robert Curth <robert@rocu.de>
2013-08-12 07:09:47 +0200

    Adds cask for BackupLoupe

---
+++ Casks/backup-loupe.rb
@@ -0,0 +1,7 @@
+class BackupLoupe < Cask
+  url 'http://www.soma-zone.com/download/files/BackupLoupe_2.4.1.tar.bz2'
+  homepage 'http://www.soma-zone.com/BackupLoupe/'
+  version '2.4.1'
+  sha1 '833684f01d66a6be91571b5300eabbe42082101a'
+  link 'BackupLoupe.app'
+end
