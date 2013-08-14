be2c30f0698ad76636aaa4d77861cfef9023b83b
Jamie <jamie@stoic.net>
2013-08-14 12:06:00 -0700

    Standardize order of fields.
    Per https://github.com/phinze/homebrew-cask/pull/920#issuecomment-22657589

--- Casks/couchbase.rb
+++ Casks/couchbase.rb
@@ -1,6 +1,6 @@
 class Couchbase < Cask
-  homepage 'http://www.couchbase.com/'
   url 'http://packages.couchbase.com/releases/2.0.1/couchbase-server-community_x86_64_2.0.1.zip'
+  homepage 'http://www.couchbase.com/'
   version '2.0.1'
   sha1 'de8c239be020a50117dd53bc1aedb095a805cd80'
   link 'Couchbase Server.app'
