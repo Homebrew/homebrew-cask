ac9f7205ebdb88605415deda355e9a45631c2abb
Fernando Paredes <nano@fdp.io>
2013-08-14 08:19:48 -0700

    Update SourceTree to 1.6.3.1

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,7 +1,7 @@
 class Sourcetree < Cask
-  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.2.2.dmg'
+  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.3.1.dmg'
   homepage 'http://www.sourcetreeapp.com/'
-  version '1.6.2.2'
-  sha1 '6f3bf5d3ec339df5224f528e5f604f2cf6fe26b1'
+  version '1.6.3.1'
+  sha1 '2761cd3e61e02f1988872cd11b647a27bf9f2f04'
   link 'SourceTree.app'
 end
