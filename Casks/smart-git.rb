696ae605d6426e277492c95d4f6506feac3bea54
Steve Rozzo <steve.rozzo@gmail.com>
2013-08-14 08:36:49 -0700

    added cask for smart git

---
+++ Casks/smart-git.rb
@@ -0,0 +1,7 @@
+class SmartGit < Cask
+  url 'http://www.syntevo.com/download/smartgithg/smartgithg-macosx-4_6_2.dmg'
+  homepage 'http://www.syntevo.com/'
+  version '4.6.2'
+  sha1 'd63c9c378958146f7398d228d1ee072263386e46'
+  link 'SmartGitHg 4.6.app'
+end
