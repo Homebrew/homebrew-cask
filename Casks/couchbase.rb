8ca29197c41d4c62b4d04d1e58c48e802cc880a3
Jamie Alessio <jamie@stoic.net>
2013-08-14 11:14:40 -0700

    Added Cask for Couchbase Server installation.

---
+++ Casks/couchbase.rb
@@ -0,0 +1,7 @@
+class Couchbase < Cask
+  homepage 'http://www.couchbase.com/'
+  url 'http://packages.couchbase.com/releases/2.0.1/couchbase-server-community_x86_64_2.0.1.zip'
+  version '2.0.1'
+  sha1 'de8c239be020a50117dd53bc1aedb095a805cd80'
+  link 'Couchbase Server.app'
+end
