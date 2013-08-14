153d8239995c6f1aac978653c9e214bb0a3be441
hzlzh <hzlzh.dev@gmail.com>
2013-08-15 00:49:44 +0800

    add casks for QQ & Snip

---
+++ Casks/qq.rb
@@ -0,0 +1,7 @@
+class QQ < Cask
+  url 'http://dl_dir.qq.com/qqfile/qq/QQforMac/QQ_V2.4.2.dmg'
+  homepage 'http://im.qq.com/macqq/index.shtml'
+  version 'latest'
+  no_checksum
+  link 'QQ.app'
+end
