29aff2ee96a3fa775cbb58795b8786875e67260f
Steve Rozzo <steve.rozzo@gmail.com>
2013-08-14 14:07:32 -0700

    added Cask for Wireshark

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -5,5 +5,8 @@ class Wireshark < Cask
   sha1 '428f974cc5c71cced647d42a1d7997ba5cb49c3a'
   link :none
   install 'Wireshark 1.10.1 Intel 64.pkg'
-  uninstall :pkgutil => 'org.wireshark.*'
+  uninstall :pkgutil => 'org.wireshark.*', 
+            :files => %w(capinfos dftest dumpcap editcap mergecap randpkt
+                         rawshark text2pcap tshark 
+                         wireshark).map { |file| '/usr/local/bin/' << file}
 end
