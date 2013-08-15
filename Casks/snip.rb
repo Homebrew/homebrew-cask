2d927d05e9d91b9783fe733de4edbafd08f61aa4
hzlzh <hzlzh.dev@gmail.com>
2013-08-15 22:07:59 +1200

    Add Snip 2.0b5771
    Closes #923.
    
    Signed-off-by: Félix Saparelli <felix@passcod.name>

---
+++ Casks/snip.rb
@@ -0,0 +1,8 @@
+class Snip < Cask
+  url 'http://snip.qq.com/resources/Snip_V2.0_5771.dmg'
+  homepage 'http://snip.qq.com/'
+  version '2.0b5771'
+  sha1 'cfe020db9bee8a7393620141b306c69f668d1e97'
+  install 'Snip_V2.0.pkg'
+  link 'Snip.app'
+end
