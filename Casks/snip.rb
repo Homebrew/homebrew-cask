5d9a21449d39b5dfebbe55b515655a65e5f90db3
hzlzh <hzlzh.dev@gmail.com>
2013-08-15 11:13:59 +0800

    add version number

--- Casks/snip.rb
+++ Casks/snip.rb
@@ -1,7 +1,7 @@
 class Snip < Cask
   url 'http://snip.qq.com/resources/Snip_V2.0_5771.dmg'
   homepage 'http://snip.qq.com/'
-  version 'latest'
+  version '2.0b5771'
   no_checksum
   link 'Snip.app'
 end
