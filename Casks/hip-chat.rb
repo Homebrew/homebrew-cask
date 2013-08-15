19d23a860d295c7e6d54c4028cacc601ef04eb46
Marcos Ojeda <subliminal@gmail.com>
2013-08-15 11:37:20 -0700

    Upgrade Hipchat to 2.3

--- Casks/hip-chat.rb
+++ Casks/hip-chat.rb
@@ -1,7 +1,7 @@
 class HipChat < Cask
-  url 'http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.0.zip'
+  url 'http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.3.zip'
   homepage 'https://www.hipchat.com/'
-  version '2.0'
-  sha1 '1552c87662d6d31161fba447c28e9f9c3ace1377'
+  version '2.3'
+  sha1 '75e33a259306bdfdf36414be16f3132e23b6450d'
   link 'HipChat.app'
 end
