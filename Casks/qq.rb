84fe5310a1db86e2116cbc45365e964a3f8c94f7
hzlzh <hzlzh.dev@gmail.com>
2013-08-15 22:06:08 +1200

    Add QQ 2.4.2
    Signed-off-by: Félix Saparelli <felix@passcod.name>

---
+++ Casks/qq.rb
@@ -0,0 +1,7 @@
+class Qq < Cask
+  url 'https://dl.dropboxusercontent.com/u/13642414/disk/dl/QQ_V2.4.2.dmg'
+  homepage 'http://im.qq.com/macqq/index.shtml'
+  version '2.4.2'
+  sha1 '7e001f3969213ae1f31d1a7e5bf0ac50e6c20bee'
+  link 'QQ.app'
+end
