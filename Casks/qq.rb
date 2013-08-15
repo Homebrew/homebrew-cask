a31ac26eadecbe9e1980967d76b459d965d481ad
hzlzh <hzlzh.dev@gmail.com>
2013-08-15 12:57:36 +0800

    add sha1

--- Casks/qq.rb
+++ Casks/qq.rb
@@ -2,6 +2,6 @@ class QQ < Cask
   url 'http://dl_dir.qq.com/qqfile/qq/QQforMac/QQ_V2.4.2.dmg'
   homepage 'http://im.qq.com/macqq/index.shtml'
   version '2.4.2'
-  no_checksum
+  sha1 '7e001f3969213ae1f31d1a7e5bf0ac50e6c20bee'
   link 'QQ.app'
 end
--- Casks/snip.rb
+++ Casks/snip.rb
@@ -2,6 +2,6 @@ class Snip < Cask
   url 'http://snip.qq.com/resources/Snip_V2.0_5771.dmg'
   homepage 'http://snip.qq.com/'
   version '2.0b5771'
-  no_checksum
+  sha1 'cfe020db9bee8a7393620141b306c69f668d1e97'
   link 'Snip.app'
 end
