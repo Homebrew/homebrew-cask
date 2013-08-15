d2c0d156a8ffe6ffc09db36634563413c169ad63
Marcos Ojeda <subliminal@gmail.com>
2013-08-15 14:33:22 -0700

    Adds Pester 1.1b15
    From the webpage: Pester is designed to let you set, dismiss or snooze
    an alarm or timer as quickly and painlessly as possible, entirely from
    the keyboard if you wish... Use Pester to remind you to catch the bus or
    attend an upcoming meeting.

---
+++ Casks/pester.rb
@@ -0,0 +1,7 @@
+class Pester < Cask
+  url 'http://sabi.net/nriley/software/Pester-1.1b15.dmg'
+  homepage 'http://sabi.net/nriley/software/index.html#pester'
+  version '1.1b15'
+  sha1 '06aa24ddf39c866e9852c9e70b4c92d8b9a7861d'
+  link 'Pester.app'
+end
