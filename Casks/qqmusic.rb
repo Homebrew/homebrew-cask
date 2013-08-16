700d6a99625e4ec214af6e81279a902ca0380bb1
hzlzh <hzlzh.dev@gmail.com>
2013-08-17 00:18:36 +0800

    add cask qqpinyin.app

---
+++ Casks/qqmusic.rb
@@ -0,0 +1,8 @@
+class Qqmusic < Cask
+  url 'http://dl_dir.qq.com/music/clntupate/QQMusicForMacV1.2.0.dmg'
+  homepage 'http://y.qq.com/y/static/down/mac.html'
+  version '1.2.0'
+  sha1 '8a6d5a1a700061627bb1fa366121dd410c9c2b11'
+  install 'QQMusicForMacV1.2.0.dmg'
+  link 'QQ音乐.app'
+end
