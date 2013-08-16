43b79026db52850969710c6d9fbd57588b290239
hzlzh <hzlzh.dev@gmail.com>
2013-08-16 23:59:59 +0800

    add cask Foxmail.app

---
+++ Casks/foxmail.rb
@@ -0,0 +1,8 @@
+class Foxmail < Cask
+  url 'http://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_V1.1.0.dmg'
+  homepage 'http://mac.qq.com/'
+  version '1.1.0'
+  sha1 '1daea3e9788499c0e30f1b1dca3cc03a0939b7c0'
+  install 'Foxmail_for_Mac_V1.1.0.dmg'
+  link 'Foxmail.app'
+end
