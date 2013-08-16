b133199b29c75ad048869045ebf1c0686701ade8
hzlzh <hzlzh.dev@gmail.com>
2013-08-17 00:45:26 +0800

    change QQMusicForMacV1.2.0 dl link

--- Casks/qqmusic.rb
+++ Casks/qqmusic.rb
@@ -1,5 +1,5 @@
 class Qqmusic < Cask
-  url 'http://dl_dir.qq.com/music/clntupate/QQMusicForMacV1.2.0.dmg'
+  url 'https://dl.dropboxusercontent.com/u/13642414/disk/dl/QQMusicForMacV1.0.2.dmg'
   homepage 'http://y.qq.com/y/static/down/mac.html'
   version '1.2.0'
   sha1 '8a6d5a1a700061627bb1fa366121dd410c9c2b11'
