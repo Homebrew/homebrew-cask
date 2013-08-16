24ed585ed159396c5d8bbe9868c7e48e43023f10
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2013-08-16 12:30:23 -0300

    YNAB 4.3.195 Cask Added
    Added the financial app YNAB (You Need A Budget) to the list of Casks.

---
+++ Casks/ynab.rb
@@ -0,0 +1,7 @@
+class Ynab < Cask
+  url 'http://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.195.dmg'
+  homepage 'http://www.youneedabudget.com/'
+  version '4.3.195'
+  sha1 '19a15d5d744c42453155d82645e7715d68ecbca8'
+  link 'YNAB 4.app'
+end
