66e64ec2d28ce682a70958ea851d02ab17cb8d5a
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2013-08-16 13:29:59 -0300

    PhoneClean Cask Added
    Added the app PhoneClean, an app to clean space in your iPhone, iPad and
    iPod Touch.

---
+++ Casks/phone-clean.rb
@@ -0,0 +1,7 @@
+class PhoneClean < Cask
+  url 'http://www.imobie.com/product/phoneclean-mac.dmg'
+  homepage 'http://www.imobie.com/phoneclean/'
+  version 'latest'
+  no_checksum
+  link 'PhoneClean.app'
+end
