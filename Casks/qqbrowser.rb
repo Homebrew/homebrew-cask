339cc8d1d10f4faf7341b6849dc5a634ab662da0
hzlzh <hzlzh.dev@gmail.com>
2013-08-17 00:08:55 +0800

    add cask QQBrowser.app

---
+++ Casks/qqbrowser.rb
@@ -0,0 +1,8 @@
+class Qqbrowser < Cask
+  url 'http://dl_dir.qq.com/invc/tt/QQBrowser_1.5.0.2311.dmg'
+  homepage 'http://browser.qq.com/mac/'
+  version '1.5.0.2311'
+  sha1 '1daea3e9788499c0e30f1b1dca3cc03a0939b7c0'
+  install 'QQBrowser_1.5.0.2311.dmg'
+  link 'QQBrowser.app'
+end
