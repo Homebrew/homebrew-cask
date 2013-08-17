e1cf2d84c65bf83586cc2d6e8e99d510f121cff7
hzlzh <hzlzh.dev@gmail.com>
2013-08-17 22:39:40 +0800

    update Branches

--- Casks/qqbrowser.rb
+++
@@ -1,8 +0,0 @@
-class Qqbrowser < Cask
-  url 'http://url.cn/0Q7Ev1'
-  homepage 'http://browser.qq.com/mac/'
-  version '1.5.0.2311'
-  sha1 '0b390037a045e0ea6b0105d2ace1c40f854106ac'
-  install 'QQBrowser_1.5.0.2311.dmg'
-  link 'QQBrowser.app'
-end
