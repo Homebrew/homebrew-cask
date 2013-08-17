20df7de13b66b13cd24c21b5def1440e012feb5b
hzlzh <hzlzh.dev@gmail.com>
2013-08-17 22:50:19 +0800

    fix script

--- Casks/foxmail.rb
+++ Casks/foxmail.rb
@@ -3,6 +3,5 @@ class Foxmail < Cask
   homepage 'http://foxmail.com.cn/mac/'
   version '1.1.0'
   sha1 '1daea3e9788499c0e30f1b1dca3cc03a0939b7c0'
-  install 'Foxmail_for_Mac_V1.1.0.dmg'
   link 'Foxmail.app'
 end
