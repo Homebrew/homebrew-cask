3d6ef3f5afb235c56028cb878e9b0aa609f3c91a
hzlzh <hzlzh.dev@gmail.com>
2013-08-17 22:48:33 +0800

    fix script

--- Casks/qqbrowser.rb
+++ Casks/qqbrowser.rb
@@ -3,6 +3,5 @@ class Qqbrowser < Cask
   homepage 'http://browser.qq.com/mac/'
   version '1.5.0.2311'
   sha1 '0b390037a045e0ea6b0105d2ace1c40f854106ac'
-  install 'QQBrowser_1.5.0.2311.dmg'
   link 'QQBrowser.app'
 end
