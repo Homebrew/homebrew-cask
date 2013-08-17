ed1963ff9d0e84ddc62e48f8b43a937c5662a939
hzlzh <hzlzh.dev@gmail.com>
2013-08-17 22:48:33 +0800

    QQBrowser 1.5.0.2311
    The short URL is used due to its long version being
    considered invalid by Ruby's stdlib. See discussion
    on the pull request.
    
    Closes #933.
    
    Signed-off-by: Félix Saparelli <felix@passcod.name>

---
+++ Casks/qqbrowser.rb
@@ -0,0 +1,7 @@
+class Qqbrowser < Cask
+  url 'http://url.cn/0Q7Ev1' # Refer to #933 regarding usage of shortener
+  homepage 'http://browser.qq.com/mac/'
+  version '1.5.0.2311'
+  sha1 '0b390037a045e0ea6b0105d2ace1c40f854106ac'
+  link 'QQBrowser.app'
+end
