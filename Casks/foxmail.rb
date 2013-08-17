abfaa4d3dcb26116a510fe033d02dedddfe9bfe0
hzlzh <hzlzh.dev@gmail.com>
2013-08-18 00:13:15 +0800

    change the download link into official one.

--- Casks/foxmail.rb
+++ Casks/foxmail.rb
@@ -1,5 +1,5 @@
 class Foxmail < Cask
-  url 'http://url.cn/HPiZqy'
+  url 'http://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_V1.1.0.dmg'
   homepage 'http://foxmail.com.cn/mac/'
   version '1.1.0'
   sha1 '1daea3e9788499c0e30f1b1dca3cc03a0939b7c0'
