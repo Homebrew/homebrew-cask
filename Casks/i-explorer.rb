bd6cc2d264c317bde124f38e7009d09dbd32e923
Fernando Paredes <nano@fdp.io>
2013-08-17 08:26:45 -0700

    Correct version number on iExplorer

--- Casks/i-explorer.rb
+++ Casks/i-explorer.rb
@@ -1,7 +1,7 @@
 class IExplorer < Cask
   url 'http://downloads.macroplant.com/iExplorer-3.2.2.0.dmg'
   homepage 'http://www.macroplant.com/'
-  version '3.2.2.2'
+  version '3.2.2.0'
   sha1 '12c27e47611b1c34276eb53baa68a1e6bcf77a1d'
   link 'iExplorer.app'
 end
