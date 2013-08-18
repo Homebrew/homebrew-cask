3df41a76e7071c095a9b2826f1441767785f8cfa
Luke Petre <lpetre@gmail.com>
2013-08-18 17:20:49 +0100

    Adding a cask for retroshare
    RetroShare is a Open Source cross-platform, Friend-2-Friend and secure decentralised communication platform.

---
+++ Casks/retroshare.rb
@@ -0,0 +1,7 @@
+class Retroshare < Cask
+  url 'http://downloads.sourceforge.net/project/retroshare/RetroShare/0.5.4e/Retroshare-V0.5.4e-svn6266_OSX10.6u.dmg'
+  homepage 'http://retroshare.sourceforge.net/'
+  version '0.5.4e'
+  sha1 'de8f95fc803a2c7ccd8c2c6c3428a9ca8ac77f1b'
+  link 'Retroshare.app'
+end
