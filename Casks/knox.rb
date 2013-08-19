3bc951c7b9500d7f29d145d8d64f4d2c5ca9a3e1
Paul Love <paul@anglepoised.com>
2013-08-19 16:13:24 +0100

    Add Knox Cask
    Knox lets you easily create, access, and backup encrypted file vaults.
    These vaults are great for storing large number of files of any size:
    confidential documents, images, source code, application data files,
    customer information.

---
+++ Casks/knox.rb
@@ -0,0 +1,7 @@
+class Knox < Cask
+  url 'https://d13itkw33a7sus.cloudfront.net/dist/K/Knox-2.2.0.zip'
+  homepage 'https://agilebits.com/knox'
+  version '2.2.0'
+  sha1 '227e8f3fb2279ca1199e72a961d0f54c8ca8a479'
+  link 'Knox.app'
+end
