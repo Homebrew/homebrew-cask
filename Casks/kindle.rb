3231b7d945c24632f6eb8a1010a8eead030cae44
Paul Love <paul@anglepoised.com>
2013-08-20 07:05:51 +0100

    Rename KindleForMac Cask class to Kindle
    Closes #943
    
    Signed-off-by: Fernando Paredes <nano@fdp.io>
    
    Conflicts:
    	Casks/kindle.rb

---
+++ Casks/kindle.rb
@@ -0,0 +1,7 @@
+class Kindle < Cask
+  url 'http://kindleformac.amazon.com/40499/KindleForMac.dmg'
+  homepage 'http://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000464931'
+  version '1.10.6 (40499)'
+  sha1 'a086df6a67e97460fb09b6d115f71d952487ac38'
+  link 'Kindle.app'
+end
