ffb98ba08bd18bf5f3727ca2ef9ab1a8e9d244db
Fernando Paredes <nano@fdp.io>
2013-08-21 20:49:29 -0700

    Update Totals to 2.2.9

--- Casks/totals.rb
+++ Casks/totals.rb
@@ -1,7 +1,7 @@
 class Totals < Cask
-  url 'http://www.kedisoft.com/dl/totals/Totals-2.2.7.dmg'
+  url 'http://www.kedisoft.com/dl/totals/Totals-2.2.9.dmg'
   homepage 'http://www.kedisoft.com/totals/'
-  version '2.2.7'
-  sha1 'ae66f0b071f35ebb11ef1548ab4608e5ce05eb15'
+  version '2.2.9'
+  sha1 '73ad4d6294c4af3bd4bf37cae9365b9ea5dc9b6a'
   link 'Totals.app'
 end
