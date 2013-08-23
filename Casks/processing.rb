5c943d5504dd936576938231d2a6dbcbf5fbf0ff
Michael Ford <fanquake@users.noreply.github.com>
2013-08-23 22:13:33 +0800

    Processing 2.0.2

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -1,7 +1,7 @@
 class Processing < Cask
-  url 'https://processing.googlecode.com/files/processing-2.0.1-macosx.zip'
+  url 'http://download.processing.org/processing-2.0.2-macosx.zip'
   homepage 'http://processing.org/'
-  version '2.0.1'
-  sha1 'be5313dac0b85387ed0ee6e652fd26bbfb749a85'
+  version '2.0.2'
+  sha1 '70d10d423fbaf5f2fd9aea35d6a56544bfa6dbe3'
   link 'Processing.app'
 end
