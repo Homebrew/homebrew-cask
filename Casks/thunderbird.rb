f2bf060d88728180e107963e91bf2c60f803c53f
Michael Ford <fanquake@users.noreply.github.com>
2013-08-24 14:39:16 +0800

    Thunderbird ESR 17.0.8

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,7 +1,7 @@
 class Thunderbird < Cask
-  url 'http://download.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/17.0.7esr/mac/en-US/Thunderbird%2017.0.7esr.dmg'
+  url 'http://download.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/17.0.8esr/mac/en-US/Thunderbird%2017.0.8esr.dmg'
   homepage 'https://www.mozilla.org/en-US/thunderbird/'
-  version '17.0.7esr'
-  sha1 '5db65c59894101feb4a6acacc47ada0e1e1c5f8b'
+  version '17.0.8esr'
+  sha1 '6bc65b7d9e76932e24f6f69f21c1b0bd4636a99b'
   link 'Thunderbird.app'
 end
