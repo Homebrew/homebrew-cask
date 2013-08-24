43197b68d1d84187452785e7cf6ef145fe294305
Michael Ford <fanquake@users.noreply.github.com>
2013-08-24 17:49:14 +0800

    Royal TSX 1.1.0.0

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -1,7 +1,7 @@
 class RoyalTsx < Cask
-  url 'http://app.royaltsx.com/updates/royaltsx_1.0.0.0.dmg'
+  url 'http://app.royaltsx.com/updates/royaltsx_1.1.0.0.dmg'
   homepage 'http://www.royaltsx.com'
-  version '1.0.0.0'
-  sha1 'cbfac46ec8109ed9c2f0dc5f5b2270239447ddf5'
+  version '1.1.0.0'
+  sha1 'bd0a3d59238c02a0b3a5c04eac4794b00c151809'
   link 'Royal TSX.app'
 end
