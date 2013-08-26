97e4d521818430063b5e29e7b8ce72713a20ecbb
Alexandre COLLIGNON <alexandre@collign.net>
2013-08-26 15:57:31 +0200

    Add Pdfsam (2.2.2) Cask

---
+++ Casks/pdfsam.rb
@@ -0,0 +1,7 @@
+class Pdfsam < Cask
+  url 'http://downloads.sourceforge.net/project/pdfsam/pdfsam/2.2.2/pdfsam-2.2.2.dmg'
+  homepage 'http://www.pdfsam.org/'
+  version '2.2.2'
+  sha1 '19cce2d4a16813ac65fb2de50416d1a9da645f28'
+  link 'pdfsam.app'
+end
