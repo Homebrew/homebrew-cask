75e3c4df6ccd3bd6191e340e3900452a48375781
Michael Ford <fanquake@users.noreply.github.com>
2013-08-27 16:33:01 +0800

    NetNewsWire 4.0.0-117

--- Casks/net-news-wire.rb
+++ Casks/net-news-wire.rb
@@ -1,7 +1,7 @@
 class NetNewsWire < Cask
-  url 'http://cdn.netnewswireapp.com/releases/NetNewsWire-4.0.0-113.zip'
+  url 'http://cdn.netnewswireapp.com/releases/NetNewsWire-4.0.0-117.zip'
   homepage 'http://netnewswireapp.com/'
-  version '4.0.0-113'
-  sha1 'f8a562887b419dd3764073ec3f6bab542d08ccba'
+  version '4.0.0-117'
+  sha1 '0e7a93fe1ab6a71036d8c5d4932d4dd02c637772'
   link 'NetNewsWire.app'
 end
