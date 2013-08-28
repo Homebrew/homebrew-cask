7ceefc62c484fd14fd46c88f80ff1d349bc66f96
xyb <xieyanbo@gmail.com>
2013-08-28 10:23:57 +0800

    fritzing 0.8.3b

---
+++ Casks/fritzing.rb
@@ -0,0 +1,7 @@
+class Fritzing < Cask
+  url 'http://fritzing.org/media/downloads/fritzing.2013.07.27.cocoa.dmg'
+  homepage 'http://fritzing.org/'
+  version '0.8.3b'
+  sha1 'dd32bcfaf40ccd18f2a541215136d0334d98122a'
+  link 'Fritzing.app'
+end
