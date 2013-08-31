f2e8084f3d45a66656146cf4d060ad5266fcebe6
Michael Ford <fanquake@users.noreply.github.com>
2013-08-31 11:27:34 +0800

    FileZilla 3.7.3

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,7 +1,7 @@
 class Filezilla < Cask
-  url 'http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.7.2/FileZilla_3.7.2_i686-apple-darwin9.app.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.7.3/FileZilla_3.7.3_i686-apple-darwin9.app.tar.bz2'
   homepage 'https://filezilla-project.org/'
-  version '3.7.2'
-  sha1 '011fd4c2dac5baa07bf3db9a15e491659bc9e8d1'
+  version '3.7.3'
+  sha1 'c604d4faa7a7bcbf9bad02ce6f80eba2e35b3488'
   link 'FileZilla.app'
 end
