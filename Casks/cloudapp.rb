51e5acf0f3ffc774940708bb14336b872e3244a8
Michael Ford <fanquake@users.noreply.github.com>
2013-08-31 10:20:33 +0800

    CloudApp 1.5.6

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,7 +1,7 @@
 class Cloudapp < Cask
-  url 'https://s3.amazonaws.com/f.cl.ly/builds/CloudApp1.5.5.zip'
+  url 'https://s3.amazonaws.com/f.cl.ly/builds/CloudApp1.5.6.zip'
   homepage 'http://getcloudapp.com/'
-  version '1.5.5'
-  sha1 '6f370036adace7f00d0b8ce1524ddc78616ca945'
+  version '1.5.6'
+  sha1 '68a2b66522edd0267b6136dba6bcf887d2c7ebf4'
   link 'Cloud.app'
 end
