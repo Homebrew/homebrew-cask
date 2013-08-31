e9ab8a8fb202f6f1a6a550c3f5a241c90a7ed2a5
Michael Ford <fanquake@users.noreply.github.com>
2013-08-31 20:55:07 +0800

    Avidemux 2.6.5

--- Casks/avidemux.rb
+++ Casks/avidemux.rb
@@ -1,7 +1,7 @@
 class Avidemux < Cask
-  url 'http://downloads.sourceforge.net/avidemux/avidemux_2.6.4_64bits_ml.dmg'
+  url 'http://downloads.sourceforge.net/project/avidemux/avidemux/2.6.5/Avidemux_2.6.5.dmg'
   homepage 'http://www.avidemux.org/'
-  version '2.6.4'
-  sha1 'fddb2db7469999090abf1ba347991cecd2b6976a'
+  version '2.6.5'
+  sha1 '17dcef0f85804359d9ab9b4677cde30ce47bf7f4'
   link 'Avidemux2.6.app'
 end
