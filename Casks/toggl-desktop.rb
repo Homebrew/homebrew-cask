c891653d249d8b8fd628ee48d00d526810957291
Hok Shun Poon <hok@hokshunpoon.me>
2013-09-01 18:10:00 +0100

    Remove version directive.

--- Casks/toggl-desktop.rb
+++ Casks/toggl-desktop.rb
@@ -1,7 +1,7 @@
 class TogglDesktop < Cask
   url 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
   homepage 'https://www.toggl.com'
-  version '6.18'
-  sha1 '84b3a495c5addb36171f610d280fcb7aef8db3f1'
+  version 'latest'
+  no_checksum
   link 'TogglDesktop.app'
 end
