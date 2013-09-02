2f45eb3fc1b5cf396debe79907b0d716756fdbf9
xyb <xieyanbo@gmail.com>
2013-09-02 13:23:27 +0800

    add bitmessagemac

---
+++ Casks/bitmessage-mac.rb
@@ -0,0 +1,7 @@
+class BitmessageMac < Cask
+  url 'http://downloads.sourceforge.net/sourceforge/bitmessagemac/bitmessage-v0.3.5.dmg'
+  homepage 'http://sourceforge.net/projects/bitmessagemac/'
+  version '0.3.5'
+  sha1 'c3e814a9d4db19d6d65417433c5ea4641ff1fe06'
+  link 'Bitmessage.app'
+end
