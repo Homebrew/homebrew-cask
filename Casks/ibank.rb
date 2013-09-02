b7292aa3988f702eb75a03f4d36d997c0b2ab724
Barney Hanlon <bhanlon@inviqa.com>
2013-09-02 15:42:33 +0100

    Updated as per suggestion

--- Casks/ibank.rb
+++ Casks/ibank.rb
@@ -1,7 +1,7 @@
 class Ibank < Cask
   url 'http://www.iggsoft.com/ibank/iBank4_Web.dmg'
   homepage 'http://www.iggsoftware.com/ibank'
-  version '4.7.3'
-  sha1 'f0c8eb2bc4115f602e1ef7baefdd4c3730838125'
+  version 'latest'
+  no_checksum
   link 'iBank.app'
 end
