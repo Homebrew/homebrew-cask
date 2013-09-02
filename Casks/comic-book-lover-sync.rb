a9cc3d771f851f79284ac15ae7dfe94be6275d24
Sam Bao <sambao21@gmail.com>
2013-09-02 10:43:20 -0500

    Add cask for ComicBookLoverSync
    A companion app to ComicBookLover for iOS, ComicBookLoversync allows
    you to transfer comics from your Mac to an iOS device.

---
+++ Casks/comic-book-lover-sync.rb
@@ -0,0 +1,7 @@
+class ComicBookLoverSync < Cask
+  url 'http://www.bitcartel.com/downloads/comicbookloversync.zip'
+  homepage 'http://www.bitcartel.com/comicbooklover'
+  version 'latest'
+  no_checksum
+  link 'ComicBookLoverSync.app'
+end
