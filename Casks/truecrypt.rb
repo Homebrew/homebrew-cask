a93a28717f6a7f7178319658df84edea4d98f2a0
Barney Hanlon <bhanlon@inviqa.com>
2013-09-02 10:05:14 +0100

    Added support for truecrypt

---
+++ Casks/truecrypt.rb
@@ -0,0 +1,7 @@
+class Truecrypt < Cask
+  url 'http://www.truecrypt.org/download/TrueCrypt%207.1a%20Mac%20OS%20X.dmg'
+  homepage 'http://www.truecrypt.org/'
+  version '7.1a'
+  sha1 '16e6d7675d63fba9bb75a9983397e3fb610459a1'
+  install 'TrueCrypt 7.1a.mpkg'
+end
