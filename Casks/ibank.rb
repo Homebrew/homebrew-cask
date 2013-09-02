4bacb4d5c6cbc1cc3216cf0f474e72e1e7951c54
Barney Hanlon <bhanlon@inviqa.com>
2013-09-02 09:35:06 +0100

    Added sha1 for iBank

--- Casks/ibank.rb
+++ Casks/ibank.rb
@@ -1,7 +1,7 @@
 class Ibank < Cask
   url 'http://www.iggsoft.com/ibank/iBank4_Web.dmg'
-  homepage 'http://www.iggsoftware.com/ibank/'
+  homepage 'http://www.iggsoftware.com/ibank'
   version '4.7.3'
-  sha1 ''
+  sha1 'f0c8eb2bc4115f602e1ef7baefdd4c3730838125'
   link 'iBank.app'
 end
