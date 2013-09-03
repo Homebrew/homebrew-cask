7dbce495ab3634fcd232c686de1c2ed1575e1c66
Kevin Suttle <kevin@kevinsuttle.com>
2013-09-02 22:39:21 -0400

    Add Cask for LastPass Universal Mac OS X
    This patch adds support for the LastPass Universal
    Installer for Mac, which is a .dmg containing
    all of the LastPass browser extensions.

---
+++ Casks/lastpass-universal.rb
@@ -0,0 +1,8 @@
+class LastpassUniversal < Cask
+  url 'https://lastpass.com/lpmacosx.dmg'
+  homepage 'https://lastpass.com/'
+  version '2.5.0'
+  sha1 '0efa71f9b5efb9b4bed2574025eb9c0bedc1eada'
+  install 'lpmacosx.pkg'
+  uninstall 'LastPass Uninstall.app'
+end
