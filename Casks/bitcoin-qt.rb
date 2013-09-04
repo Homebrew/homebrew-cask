703343495d251525b5a59b84500c19497cd35176
Michael Ford <fanquake@users.noreply.github.com>
2013-09-04 17:52:40 +0800

    Bitcoin-Qt 0.8.4

--- Casks/bitcoin-qt.rb
+++ Casks/bitcoin-qt.rb
@@ -1,7 +1,7 @@
 class BitcoinQt < Cask
-  url 'http://downloads.sourceforge.net/project/bitcoin/Bitcoin/bitcoin-0.8.3/bitcoin-0.8.3-macosx.dmg'
+  url 'http://downloads.sourceforge.net/project/bitcoin/Bitcoin/bitcoin-0.8.4/bitcoin-0.8.4-macosx.dmg'
   homepage 'http://sourceforge.net/projects/bitcoin/'
-  version '0.8.3'
-  sha1 'b898389f119ec30c374afef2e2d5706bf4105bc3'
+  version '0.8.4'
+  sha1 '55e9dc295ad1264816ad65ff2e1853878984d6bd'
   link 'Bitcoin-Qt.app'
 end
