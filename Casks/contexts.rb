49fcc8ba5d642076689a93973e339992a9395427
Tal Atlas <me@tal.by>
2013-09-05 14:08:07 -0400

    Add contexts for mac

---
+++ Casks/contexts.rb
@@ -0,0 +1,7 @@
+class Contexts < Cask
+  url 'http://contextsformac.com/releases/Contexts.zip'
+  homepage 'http://contextsformac.com/'
+  version '0.9.20'
+  sha1 '1c28551057d9a02109784ded0d7d99eb4a68295b'
+  link 'Contexts.app'
+end
