a9e72417530811b025e03bb771e581fbf0ce9799
Tal Atlas <me@tal.by>
2013-09-05 14:12:59 -0400

    Changed to no checksum

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -2,6 +2,6 @@ class Contexts < Cask
   url 'http://contextsformac.com/releases/Contexts.zip'
   homepage 'http://contextsformac.com/'
   version '0.9.20'
-  sha1 '1c28551057d9a02109784ded0d7d99eb4a68295b'
+  no_checksum
   link 'Contexts.app'
 end
