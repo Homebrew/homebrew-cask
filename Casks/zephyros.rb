af7dee15df8cd0c1ca2f968d2040c4d63b3072fc
Corey Burmeister <burmeister.corey@gmail.com>
2013-09-07 12:16:06 -0700

    adds zephyros cask
    "The OS X window manager for hackers"

---
+++ Casks/zephyros.rb
@@ -0,0 +1,7 @@
+class Zephyros < Cask
+  url 'https://raw.github.com/sdegutis/zephyros/master/Builds/Zephyros-LATEST.app.tar.gz'
+  homepage 'https://github.com/sdegutis/zephyros'
+  version '5.0'
+  sha1 '89773faa038a263603c5b1a16950079e308d77c8'
+  link 'Zephyros.app'
+end
