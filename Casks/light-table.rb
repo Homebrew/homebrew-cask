4ecb4949761a62c8829561eac712119c523878d2
Michael Ford <fanquake@users.noreply.github.com>
2013-09-07 20:51:37 +0800

    LightTable 0.5.3

--- Casks/light-table.rb
+++ Casks/light-table.rb
@@ -1,7 +1,7 @@
 class LightTable < Cask
-  url 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.5.1/LightTableMac.zip'
+  url 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.5.3/LightTableMac.zip'
   homepage 'http://www.lighttable.com/'
-  version '0.5.1'
-  sha1 '6788aa05459a53f216e2517a38c23f57afab10ab'
+  version '0.5.3'
+  sha1 'cfd18be0d0fec7c7b3adf1d80efc26adcc2fb08f'
   link 'LightTable.app'
 end
