e13dd06d21ef292ca2a6e9cc5b10266823e74c16
Radosław Benkel <radoslaw.benkel@gmail.com>
2013-09-08 09:34:32 +0200

    Added cask for LineIn app
    This patch adds LineIn app from Rogue Amoeba, which enables soft playthru of audio from input devices.

---
+++ Casks/linein.rb
@@ -0,0 +1,7 @@
+class Linein < Cask
+  url 'http://www.rogueamoeba.com/freebies/download/LineIn.zip'
+  homepage 'http://www.rogueamoeba.com/freebies/'
+  version 'latest'
+  no_checksum
+  link 'LineIn.app'
+end
