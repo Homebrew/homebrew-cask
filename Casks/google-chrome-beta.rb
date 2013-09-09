4a769d12687a6164657086d3620e548b77bcbb61
Paweł Waleczek <pawel@thisismyasterisk.org>
2013-09-09 16:30:37 +0200

    removing chrome beta as requested <https://github.com/phinze/homebrew-cask/pull/995#issuecomment-24075754>

--- Casks/google-chrome-beta.rb
+++
@@ -1,7 +0,0 @@
-class GoogleChromeBeta < Cask
-  url 'https://dl.google.com/chrome/mac/beta/GoogleChrome.dmg'
-  homepage 'https://www.google.com/chrome/'
-  version 'beta-channel'
-  no_checksum
-  link 'Google Chrome.app'
-end
