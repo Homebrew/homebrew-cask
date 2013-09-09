4db0ae2695f54abc108b41f87e19f19e28d4c037
Adam Robertson <oldskool73@gmail.com>
2013-09-09 23:23:06 +1000

    upgrade sourcetree to 1.6.4.1

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,7 +1,7 @@
 class Sourcetree < Cask
-  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.3.1.dmg'
+  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.4.1.dmg'
   homepage 'http://www.sourcetreeapp.com/'
-  version '1.6.3.1'
-  sha1 '2761cd3e61e02f1988872cd11b647a27bf9f2f04'
+  version '1.6.4.1'
+  sha1 '64c2e02b4efc9049cf1aa783ecdf3f9f16478e7d'
   link 'SourceTree.app'
 end
