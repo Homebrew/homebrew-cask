f60f6391cf56f9b9a436b4aeb302d42ba20407b8
Adam Robertson <oldskool73@gmail.com>
2013-09-09 23:29:43 +1000

    changed version to 'latest' and removed checksum

--- Casks/macpardeluxe.rb
+++ Casks/macpardeluxe.rb
@@ -1,7 +1,7 @@
 class Macpardeluxe < Cask
   url 'http://www.xs4all.nl/~gp/MacPAR_deLuxe/MacPARdeLuxe.dmg'
   homepage 'http://gp.home.xs4all.nl/Site/MacPAR_deLuxe.html'
-  version '4.4.1'
-  sha1 'e5981e54e1a37c1d2c2e193e84506786d7bd2518'
+  version 'latest'
+  no_checksum
   link 'MacPAR deLuxe.app'
 end
