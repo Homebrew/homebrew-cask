8f3d825e76b056f320068a9e41ac996a8a38cc02
Paweł Waleczek <pawel@thisismyasterisk.org>
2013-09-09 13:12:17 +0200

    added Google Chrome Beta and updated VirtualBox to 4.2.18

---
+++ Casks/google-chrome-beta.rb
@@ -0,0 +1,7 @@
+class GoogleChromeBeta < Cask
+  url 'https://dl.google.com/chrome/mac/beta/GoogleChrome.dmg'
+  homepage 'https://www.google.com/chrome/'
+  version 'beta-channel'
+  no_checksum
+  link 'Google Chrome.app'
+end
--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,8 +1,8 @@
 class Virtualbox < Cask
-  url 'http://download.virtualbox.org/virtualbox/4.2.16/VirtualBox-4.2.16-86992-OSX.dmg'
+  url 'http://download.virtualbox.org/virtualbox/4.2.18/VirtualBox-4.2.18-88780-OSX.dmg'
   homepage 'http://www.virtualbox.org'
-  version '4.2.16-86992'
-  sha1 '94a2f1ab48d2daa0b0a0640efd79ecdd5556c4d7'
+  version '4.2.18-88780'
+  sha1 '2a0f6e48783ec85cccd8027684ddc4ed2da23392'
   install 'VirtualBox.pkg'
   uninstall :script => 'VirtualBox_Uninstall.tool', :args => %w[--unattended]
 end
