846a92214e47acf00c3922f48cfde20e3c5ad880
billwanjohi <kaguru@gmail.com>
2013-09-13 13:22:01 -0500

    add parallels-8 cask
    this is commercial software, and not even the latest version
    so I'd understand if this doesn't fit the brew-cask model

---
+++ Casks/parallels-8.rb
@@ -0,0 +1,7 @@
+class Parallels8 < Cask
+  url 'http://www.parallels.com/directdownload/pd8'
+  homepage 'http://www.parallels.com/products/desktop/'
+  version 'latest'
+  sha1 'f59af76bf7a71fc6816233b1b642b9d04f8d96be'
+  install 'Install.mpkg'
+end
