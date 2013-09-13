b713aa53b0301299cca97632b90ab7a720e32c1f
billwanjohi <kaguru@gmail.com>
2013-09-13 13:43:29 -0500

    remove checksum
    since this is a non-specific version url

--- Casks/parallels-8.rb
+++ Casks/parallels-8.rb
@@ -2,6 +2,6 @@ class Parallels8 < Cask
   url 'http://www.parallels.com/directdownload/pd8'
   homepage 'http://www.parallels.com/products/desktop/'
   version 'latest'
-  sha1 'f59af76bf7a71fc6816233b1b642b9d04f8d96be'
+  no_checksum
   install 'Install.mpkg'
 end
