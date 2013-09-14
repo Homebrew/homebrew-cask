b4f7d4bc96213a8186004cb0022af9e6e397cbf8
bolasblack <bolasblack@gmail.com>
2013-09-14 22:04:26 +0800

    add ccleaner cask

---
+++ Casks/ccleaner.rb
@@ -0,0 +1,7 @@
+class Ccleaner < Cask
+  url 'http://download.piriform.com/mac/CCMacSetup107.dmg'
+  homepage 'http://www.piriform.com/ccleaner'
+  version '1.07.233'
+  sha1 'b6a718aa965cf5a6d21415708cc869c80c9adbff'
+  link 'CCleaner.app', 'CCleaner.app'
+end
