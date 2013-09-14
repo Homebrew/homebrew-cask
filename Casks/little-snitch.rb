073615c6f5e9112cd32af22b72799b14dac32ad7
gregstallings <gregstallings@gmail.com>
2013-09-13 20:24:15 -0700

    Upgrade little-snitch to 3.1.1

--- Casks/little-snitch.rb
+++ Casks/little-snitch.rb
@@ -1,6 +1,6 @@
 class LittleSnitch < Cask
-  url 'http://www.obdev.at/downloads/LittleSnitch/LittleSnitch-2.5.4.dmg'
+  url 'http://www.obdev.at/downloads/LittleSnitch/LittleSnitch-3.1.1.dmg'
   homepage 'http://www.obdev.at/products/littlesnitch/index.html'
-  version '2.5.4'
-  sha1 '42ae2ffc404845b1bc4611f6574e281cd52041ac'
+  version '3.1.1'
+  sha1 '8d42c4aa3164a754e07aeacdccf22ebb9be601a2'
 end
