add207c76895821eb46768286acc72094d03b86b
Fernando Paredes <nano@fdp.io>
2013-09-15 10:11:55 -0700

    Update BackInTime to 3.0.0

--- Casks/back-in-time.rb
+++ Casks/back-in-time.rb
@@ -1,7 +1,7 @@
 class BackInTime < Cask
-  url 'http://www.tri-edre.com/pub/files/backintime2.dmg'
+  url 'http://www.tri-edre.fr/pub/files/backintime3.dmg'
   homepage 'http://www.tri-edre.fr/english/backintime.html'
-  version '2.3.0'
-  sha1 'c2ce8ec9c869474dc10c5299033515252c8a4f41'
-  link 'Back-In-Time 2.app'
+  version 'latest'
+  no_checksum
+  link 'Back-In-Time 3.app'
 end
