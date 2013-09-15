a500e6b2520dd2c0d4ed2c6b77c316b5f60b0e99
Fernando Paredes <nano@fdp.io>
2013-09-15 10:22:51 -0700

    Add OmniPlan cask
    Project management made painless

---
+++ Casks/omni-plan.rb
@@ -0,0 +1,7 @@
+class OmniPlan < Cask
+  url 'http://www.omnigroup.com/download/latest/omniplan'
+  homepage 'http://www.omnigroup.com/products/omniplan/'
+  version 'latest'
+  no_checksum
+  link 'OmniPlan.app'
+end
