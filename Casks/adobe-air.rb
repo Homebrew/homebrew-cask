204af8b9af3fb5f8817520c781e30877a4854dd8
Fernando Paredes <nano@fdp.io>
2013-09-15 10:45:12 -0700

    Update checksum for Adobe Air

--- Casks/adobe-air.rb
+++ Casks/adobe-air.rb
@@ -2,6 +2,6 @@ class AdobeAir < Cask
   url 'http://airdownload.adobe.com/air/mac/download/3.8/AdobeAIR.dmg'
   homepage 'https://get.adobe.com/air/'
   version '3.8'
-  sha1 '7d4c785d02f6fde6845549260574cef4eeba00b2'
+  sha1 '7ecc603e8481bbe37866c53a305cf654ed534e45'
   install 'Adobe AIR Installer.app'
 end
