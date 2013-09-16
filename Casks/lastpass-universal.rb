6069def73b16951008aed0a2d25e8db5687812b4
Kevin Suttle <kevin@kevinsuttle.com>
2013-09-16 15:55:29 -0400

    Remove version number and SHA
    Change to 'latest'

--- Casks/lastpass-universal.rb
+++ Casks/lastpass-universal.rb
@@ -1,7 +1,6 @@
 class LastpassUniversal < Cask
   url 'https://lastpass.com/lpmacosx.dmg'
   homepage 'https://lastpass.com/'
-  version '2.5.0'
-  sha1 '0efa71f9b5efb9b4bed2574025eb9c0bedc1eada'
+  version 'latest'
   install 'lpmacosx.pkg'
 end
