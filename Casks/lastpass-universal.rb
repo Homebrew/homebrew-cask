75d1ee9b7d2fb1a8634171241f319fbc99d8a3e0
Kevin Suttle <kevin@kevinsuttle.com>
2013-09-16 15:13:53 -0400

    Remove uninstall line from Lastpass Universal Cask

---
+++ Casks/lastpass-universal.rb
@@ -0,0 +1,7 @@
+class LastpassUniversal < Cask
+  url 'https://lastpass.com/lpmacosx.dmg'
+  homepage 'https://lastpass.com/'
+  version '2.5.0'
+  sha1 '0efa71f9b5efb9b4bed2574025eb9c0bedc1eada'
+  install 'lpmacosx.pkg'
+end
