8bec7c100bcb9dde4dacdf7c169b4b1efbc96f60
Michael Ford <fanquake@users.noreply.github.com>
2013-09-16 08:45:51 +0800

    Multibit 0.5.14

--- Casks/multibit.rb
+++ Casks/multibit.rb
@@ -1,7 +1,7 @@
 class Multibit < Cask
-  url 'https://multibit.org/releases/multibit-0.5.13/multibit-0.5.13.dmg'
+  url 'https://multibit.org/releases/multibit-0.5.14/multibit-0.5.14.dmg'
   homepage 'https://multibit.org/'
-  version '0.5.13'
-  sha1 'ed92c3cd4b095fce42d6954e9a97ddb7fc7672c2'
+  version '0.5.14'
+  sha1 '1a26137861b45014363454296bd8e1b3cc7d316d'
   link 'MultiBit.app'
 end
