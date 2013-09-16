6c759b013fbd603d1ddfdc9c56c7879b643d9ac7
Vítor Galvão <info@vitorgalvao.com>
2013-09-16 20:36:42 +0100

    removed instashare, since it's now an AppStore-only app

--- Casks/instashare.rb
+++
@@ -1,7 +0,0 @@
-class Instashare < Cask
-  url 'http://instashareapp.com/files/InstashareB13.zip'
-  homepage 'http://instashareapp.com/'
-  version 'Beta 13'
-  sha1 '93f67131daaf6664d9d76a61b5115c7252fcc9ab'
-  link 'Instashare.app'
-end
