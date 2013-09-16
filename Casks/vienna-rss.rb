c58a01b538b68af702b117b7109e17c182c8d540
Michael Ford <fanquake@users.noreply.github.com>
2013-09-16 08:39:10 +0800

    Vienna RSS 3.0.0_beta18

--- Casks/vienna-rss.rb
+++ Casks/vienna-rss.rb
@@ -1,7 +1,7 @@
 class ViennaRss < Cask
-  url 'http://downloads.sourceforge.net/vienna-rss/Vienna3.0.0_beta14.tgz'
+  url 'http://downloads.sourceforge.net/vienna-rss/Vienna3.0.0_beta18.tgz'
   homepage 'http://www.vienna-rss.org'
-  version '3.0.0_beta14'
-  sha1 '73bc5434094c611c6ed5e01e271df4fd2511cd6b'
+  version '3.0.0_beta18'
+  sha1 'd896b947470126e1d99a5ab9370452c2e7cb8033'
   link 'Vienna.app'
 end
