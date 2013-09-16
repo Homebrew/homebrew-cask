90606bbd6d84068ee4665ad70b299332d7973141
Niels <niels.lohmann@gmail.com>
2013-09-16 14:37:11 +0200

    Tapaal 2.4 - a timed Petri net modeler/verifier
    TAPAAL is a tool for modelling, simulation and verification of
    Timed-Arc Petri nets, developed at Department of Computer Science at
    AALborg University in Denmark. Timed-Arc Petri Net (TAPN) is a time
    extension of the classical Petri net model (a commonly used graphical
    model of distributed computations introduced by Carl Adam Petri in his
    disseration in 1962). The time extension we consider allows for
    explicit treatment of real-time, which is associated with the tokens in
    the net (each tokens has its own age) and arcs from places to
    transitions are labelled by time intervals that restrict the age of
    tokens that can be used in order to fire the respective transition. In
    TAPAAL tool a furter extension of this model with age invariants with
    transport arcs (which are more expressive than for example previously
    considered read-arcs) and with inhibitor arcs is implemented.

---
+++ Casks/tapaal.rb
@@ -0,0 +1,7 @@
+class Tapaal < Cask
+  url 'http://www.tapaal.net/fileadmin/download/tapaal-2.4/tapaal-2.4.0-mac.dmg'
+  homepage 'http://www.tapaal.net'
+  version '2.4.0'
+  sha1 'e02f47e3a8ae83bb951b0d6f167437e19f1a6ba7'
+  link 'Tapaal.app'
+end
