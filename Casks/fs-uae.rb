d3c4cd29fa4a6188c770546cd7d521201b2fc337
tditlu <todi@tulou.org>
2013-09-17 09:56:35 +0200

    FS-UAE 2.2.3
    FS-UAE is an Amiga emulator for Windows, Linux and Mac OS X based on UAE/WinUAE, with a focus on emulating games.

---
+++ Casks/fs-uae.rb
@@ -0,0 +1,7 @@
+class FsUae < Cask
+  url 'http://fs-uae.net/fs-uae/stable/2.2.3/fs-uae-plus-2.2.3-macosx.tar.gz'
+  homepage 'http://fs-uae.net/'
+  version '2.2.3'
+  sha1 '9120d97c2a2e0d330f53a640b95e30ffce9ef138'
+  link 'FS-UAE.app'
+end
