648682e97692fa877f08fd85d3a515d1f5d8f1d8
Brent Langston <brent@spotify.com>
2013-09-16 23:02:37 -0400

    Update sabnzbd to 0.7.16

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -1,7 +1,7 @@
 class Sabnzbd < Cask
-  url 'http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/0.7.14/SABnzbd-0.7.14-osx.dmg'
+  url 'http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/0.7.16/SABnzbd-0.7.16-osx.dmg'
   homepage 'http://sabnzbd.org/'
-  version '0.7.14'
-  sha1 '06b2cc099e78b8c70ff606867064f3fd60b22dc7'
+  version '0.7.16'
+  sha1 '27c957bb2b68a84c8ba8aa2de4a761b4851873f0'
   link 'OS X 10.8 (Mountain Lion)/SABnzbd.app'
 end
