2ba0bac635f5fb14dc0ab466424545519018803b
Vítor Galvão <info@vitorgalvao.com>
2013-09-18 17:47:45 +0100

    added shotcut (video editor)

---
+++ Casks/shotcut.rb
@@ -0,0 +1,7 @@
+class Shotcut < Cask
+  url 'http://d1av856udzjaks.cloudfront.net/shotcut/shotcut-osx-x86_64-130917.dmg'
+  homepage 'http://www.shotcut.org/'
+  version '130918'
+  sha1 '67aa3081311ff20f5cd2dece44c20b7e6b8201b7'
+  link 'Shotcut.app'
+end
