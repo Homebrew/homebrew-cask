c8a0adec2d457f6076cf6522d2ed6ee0358794b4
Michael Ford <fanquake@users.noreply.github.com>
2013-09-18 16:50:27 +0800

    Correct FirefoxAurora SHA1
    Thanks @nightire for pointing it out

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -2,6 +2,6 @@ class FirefoxAurora < Cask
   url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-25.0a2.en-US.mac.dmg'
   homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
   version '25.0a2'
-  sha1 '93bd6158f2755227fabdf087d14a06d0e1d69991'
+  sha1 'ea9a343ee322ae2e243df2bc0b23ce25005cb46f'
   link 'FirefoxAurora.app'
 end
