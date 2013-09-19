d58b11d713e3e9c603f2bd17634e183292de6b1f
Fernando Paredes <nano@fdp.io>
2013-09-19 08:02:06 -0700

    Update Sourcetree to 1.7.1

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,7 +1,7 @@
 class Sourcetree < Cask
-  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.4.1.dmg'
+  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.7.1.dmg'
   homepage 'http://www.sourcetreeapp.com/'
-  version '1.6.4.1'
-  sha1 '64c2e02b4efc9049cf1aa783ecdf3f9f16478e7d'
+  version '1.7.1'
+  sha1 '88043743b32500fa393ca65a50474d6c6a0fa4bd'
   link 'SourceTree.app'
 end
