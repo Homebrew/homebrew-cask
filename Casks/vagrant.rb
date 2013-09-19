56a8f7c512019d6e6e1e21b66e1f95549f755260
Timothy Sutton <tim@synthist.net>
2013-09-19 12:26:19 -0400

    Vagrant 1.3.3

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,8 +1,8 @@
 class Vagrant < Cask
-  url 'http://files.vagrantup.com/packages/9a394588a6dcf97e8f916da9564088fcf242c4b3/Vagrant-1.3.2.dmg'
+  url 'http://files.vagrantup.com/packages/db8e7a9c79b23264da129f55cf8569167fc22415/Vagrant-1.3.3.dmg'
   homepage 'http://www.vagrantup.com'
-  version '1.3.2'
-  sha1 '3d0dcd4d25fefb25f8914428eeb4299b7cecf7b0'
+  version '1.3.3'
+  sha1 '4787ce0786558b7b2b21577dbe7da7694b3caa4b'
   install 'Vagrant.pkg'
   uninstall :script => 'uninstall.tool', :input => %w[Yes]
 end
