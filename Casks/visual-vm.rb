f4af848493470a327215058fab5fc79aa6b979a9
Alexandre COLLIGNON <alexandre@collign.net>
2013-09-19 13:21:55 +0200

    Add VisualVM (1.3.6)

---
+++ Casks/visual-vm.rb
@@ -0,0 +1,7 @@
+class VisualVm < Cask
+  url 'https://java.net/projects/visualvm/downloads/download/release136/VisualVM_136.dmg'
+  homepage 'http://visualvm.java.net'
+  version '1.3.6'
+  sha1 '5d97e4dff9684490fd7ebced78c850e7e4319a10'
+  link 'VisualVM.app'
+end
