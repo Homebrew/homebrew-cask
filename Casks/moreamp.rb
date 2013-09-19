7861e2cd680fbeba0f6fc39eca627cab2f2f601e
Alexandre COLLIGNON <alexandre@collign.net>
2013-09-19 13:26:53 +0200

    Add MoreAmp (0.1.28)

---
+++ Casks/moreamp.rb
@@ -0,0 +1,7 @@
+class Moreamp < Cask
+  url 'http://downloads.sourceforge.net/project/moreamp/moreamp/MoreAmp-0.1.28/MoreAmp-0.1.28-binOSXintel.dmg'
+  homepage 'http://sourceforge.net/projects/moreamp/'
+  version '0.1.28'
+  sha1 'cfde9ed841b9f066dc108338f459d305540ee0bc'
+  link 'MoreAmp.app'
+end
