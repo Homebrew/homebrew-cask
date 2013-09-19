0180e92fb8ea23c094bd6967ca1b7b8e4aae78ab
Brent Langston <brent@spotify.com>
2013-09-18 20:33:05 -0400

    Adding LogMeIn Hamachi client
    This links the latest client installer for the
    LogMeIn Hamachi service.

---
+++ Casks/hamachi.rb
@@ -0,0 +1,7 @@
+class Hamachi < Cask
+  url 'https://secure.logmein.com/LogMeInHamachi.zip'
+  homepage 'http://vpn.net'
+  version 'latest'
+  no_checksum
+  link 'LogMeInHamachiInstaller.app'
+end
