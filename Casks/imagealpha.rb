150b9dde6bce691f990af7abb9b08b55a79b321f
Michael Ford <fanquake@users.noreply.github.com>
2013-09-22 01:08:49 +0800

    ImageAlpha 1.3.5

--- Casks/imagealpha.rb
+++ Casks/imagealpha.rb
@@ -1,7 +1,7 @@
 class Imagealpha < Cask
-  url 'http://pngmini.com/ImageAlpha1.3.1.tar.bz2'
+  url 'http://pngmini.com/ImageAlpha1.3.5.tar.bz2'
   homepage 'http://pngmini.com/'
-  version '1.3.1'
-  sha1 '61462ed89ad2048fe7b6fe1fc209baec01751401'
+  version '1.3.5'
+  sha1 '11160e2a18709dc959d3b97dde56ac08a2f8ff13'
   link 'ImageAlpha.app'
 end
