4360fba7f367b91a7b707937af6da032af8948d2
Michael Ford <fanquake@users.noreply.github.com>
2013-09-21 10:15:04 +0800

    Mailplane 3.0.2 (801)

--- Casks/mailplane.rb
+++ Casks/mailplane.rb
@@ -1,7 +1,7 @@
 class Mailplane < Cask
-  url 'http://dist.mailplaneapp.com/builds/Mailplane_3_755.tbz'
+  url 'http://dist.mailplaneapp.com/builds/Mailplane_3_801.tbz'
   homepage 'http://mailplaneapp.com/'
-  version '3.0_755'
-  sha1 '8da7abf402a4ef7ea174abc9ccd1f634577a07be'
+  version '3.0.2 (801)'
+  sha1 'f5bedf1790a15279dbc49f643c186e4c1943c28d'
   link 'Mailplane 3.app'
 end
