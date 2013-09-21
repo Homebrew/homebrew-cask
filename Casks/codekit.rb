78935fe3165a4243295434b60768b88e09053b17
Michael Ford <fanquake@users.noreply.github.com>
2013-09-21 10:04:29 +0800

    CodeKit 1.8.1

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -1,7 +1,7 @@
 class Codekit < Cask
-  url 'http://incident57.com/codekit/files/codekit-8322.zip'
+  url 'http://incident57.com/codekit/files/codekit-8326.zip'
   homepage 'http://incident57.com/codekit/'
-  version '1.8 (8322)'
-  sha1 '70e093cf417ef9c99a4180d71a9b318e1145828a'
+  version '1.8.1 (8326)'
+  sha1 '315f6bfa39ab9a6de435193a48870301fc4ae578'
   link 'CodeKit.app'
 end
