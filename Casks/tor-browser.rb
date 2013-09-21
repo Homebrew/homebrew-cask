ca1f12b5b8e20232c5d83dbf6438a3e2aa33a07e
Michael Ford <fanquake@users.noreply.github.com>
2013-09-22 01:12:06 +0800

    Tor Browser Bundle 2.3.25-13

--- Casks/tor-browser.rb
+++ Casks/tor-browser.rb
@@ -1,7 +1,7 @@
 class TorBrowser < Cask
-  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.3.25-12-osx-x86_64-en-US.zip'
+  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.3.25-13-osx-x86_64-en-US.zip'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  version '2.3.25-12'
-  sha1 '1a65da8d45052f29823964b0c1bdff8bcb6c8bf6'
+  version '2.3.25-13'
+  sha1 '6a32bdab9eb2e031a1ac23d1614fb31c4c251722'
   link 'TorBrowser_en-US.app'
 end
