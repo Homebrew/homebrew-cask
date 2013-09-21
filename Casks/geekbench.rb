032df5164f06a36d91405ec5756e623e19c11eb2
Michael Ford <fanquake@users.noreply.github.com>
2013-09-22 01:06:00 +0800

    Geekbench 3.1.2

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -1,7 +1,7 @@
 class Geekbench < Cask
-  url 'http://geekbench.s3.amazonaws.com/Geekbench-3.0.2-Mac.zip'
+  url 'http://geekbench.s3.amazonaws.com/Geekbench-3.1.2-Mac.zip'
   homepage 'http://www.primatelabs.com/geekbench/'
-  version '3.0.2'
-  sha1 'd2f618e6b8469786f2fc4f26045e9fa6f78442b6'
+  version '3.1.2'
+  sha1 '80e66f41367387f6727d8c22071f50824d5e2951'
   link 'Geekbench 3.app'
 end
