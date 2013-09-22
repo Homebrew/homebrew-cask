8a05cebba4be35273fac2ff114ab5af0e10235aa
Paul Hinze <paulh@instructure.com>
2013-09-22 13:03:18 -0500

    add pixel-check; a dead pixel tester

---
+++ Casks/pixel-check.rb
@@ -0,0 +1,7 @@
+class PixelCheck < Cask
+  url 'http://macguitar.me/apps/pixelcheck/PXC1.3.zip'
+  homepage 'http://macguitar.me/apps/pixelcheck/'
+  version '1.3'
+  sha1 'b7b879458dd1e6225b820bbced085d9202688a05'
+  link 'PiXel Check 1.3/PiXel Check.app'
+end
