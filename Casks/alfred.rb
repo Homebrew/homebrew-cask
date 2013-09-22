18052d415d7128ad5012eebbbe0742b76849d0ff
Julio Napurí <julionc@gmail.com>
2013-09-21 23:40:09 -0500

    Update Alfred to 2.0.8

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,7 +1,7 @@
 class Alfred < Cask
-  url 'http://cachefly.alfredapp.com/Alfred_2.0.7_205.zip'
+  url 'http://cachefly.alfredapp.com/Alfred_2.0.8_212.zip'
   homepage 'http://www.alfredapp.com/'
-  version '2.0.7_205'
-  sha1 '004c9950bb831c670273f1fdc763e184607e8a43'
+  version '2.0.8_212'
+  sha1 '72612308f0f0315340ee6ad45399bd91abffbe90'
   link 'Alfred 2.app', 'Alfred Preferences.app'
 end
