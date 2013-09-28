fcc8b25905da94678fd1c20274f2c236d7d740c9
Steven Hsu <admin@stevenhsu.ca>
2013-09-27 22:16:14 -0700

    Updated Komodo-Edit Cask to 8.5

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -1,7 +1,7 @@
 class KomodoEdit < Cask
-  url 'http://downloads.activestate.com/Komodo/releases/8.0.2/Komodo-Edit-8.0.2-12590-macosx-x86_64.dmg'
+  url 'http://downloads.activestate.com/Komodo/releases/8.5.0/Komodo-Edit-8.5.0-13638-macosx-x86_64.dmg'
   homepage 'http://www.activestate.com/komodo-edit'
-  version '8.0.2'
-  sha1 '2ee60ac9ad4519b322a5b9ec49208ff0ae6ee6ad'
+  version '8.5'
+  no_checksum
   link 'Komodo Edit 8.app'
 end
