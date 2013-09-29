41e4c9c2e9479d0355e5e4caf9b63e28a7defab3
Steven Hsu <admin@stevenhsu.ca>
2013-09-28 20:28:10 -0700

    Added checksum for latest version.

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -2,6 +2,6 @@ class KomodoEdit < Cask
   url 'http://downloads.activestate.com/Komodo/releases/8.5.0/Komodo-Edit-8.5.0-13638-macosx-x86_64.dmg'
   homepage 'http://www.activestate.com/komodo-edit'
   version '8.5'
-  no_checksum
+  sha1 'b92a670b711e59f1d17772ae7b3f67c946cb50cc'
   link 'Komodo Edit 8.app'
 end
