db67e7ce6491bd5a81e1fa69ffb2238633a82006
Jonathan Neal <jonathantneal@hotmail.com>
2013-10-01 00:47:49 -0700

    Parallels 9

---
+++ Casks/parallels-9.rb
@@ -0,0 +1,7 @@
+class Parallels9 < Cask
+  url 'http://www.parallels.com/directdownload/pd9'
+  homepage 'http://www.parallels.com/products/desktop/'
+  version 'latest'
+  no_checksum
+  install 'Install.mpkg'
+end
