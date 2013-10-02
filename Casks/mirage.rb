5dcdab284e5b9ec09e504e82adaaf215c7983da1
phinze <paul.t.hinze@gmail.com>
2013-10-01 20:46:29 -0500

    update mirage to working url
    not ideal (i.e. developer site or something) but I couldn't find
    anything else at the moment
    
    refs #1110

--- Casks/mirage.rb
+++ Casks/mirage.rb
@@ -1,5 +1,5 @@
 class Mirage < Cask
-  url 'http://download2us.softpedia.com/dl/a52298e7716cfbb6a9949a91cadc8ccd/51ec53d1/400074508/mac/Desktop/Mirage.zip'
+  url 'http://cl.ly/1agC/download/Mirage%20101.zip'
   homepage 'http://mac.softpedia.com/get/Wallpapers/Ferber-Mirage.shtml'
   version 'latest'
   no_checksum
