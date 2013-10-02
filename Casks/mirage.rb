23dd329ce23de464444e33baf3d4c275940531c9
Fernando Paredes <nano@fdp.io>
2013-10-02 15:08:40 -0700

    Remove Mirage
    The developer of the application has stated that it will not be updated and possibly doesn't work on anything since OS X 10.7

--- Casks/mirage.rb
+++
@@ -1,7 +0,0 @@
-class Mirage < Cask
-  url 'http://cl.ly/1agC/download/Mirage%20101.zip'
-  homepage 'http://mac.softpedia.com/get/Wallpapers/Ferber-Mirage.shtml'
-  version 'latest'
-  no_checksum
-  link 'Mirage.app'
-end
