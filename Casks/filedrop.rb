35da65f74f116816fb750a1e5f1e3fd3e95920f6
Fernando Paredes <nano@fdp.io>
2013-10-02 10:52:43 -0700

    Filedrop
    Filedrop is a tiny and cute app for sharing stuff via Wi-Fi.

---
+++ Casks/filedrop.rb
@@ -0,0 +1,7 @@
+class Filedrop < Cask
+  url 'http://commondatastorage.googleapis.com/filedropme/Filedrop.dmg'
+  homepage 'http://www.filedropme.com/'
+  version 'latest'
+  no_checksum
+  link 'Filedrop.app'
+end
