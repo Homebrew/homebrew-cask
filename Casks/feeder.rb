2ce4014ecb0c47628f2e41b70f514de6fabcf7ce
Fernando Paredes <nano@fdp.io>
2013-10-02 10:57:38 -0700

    Feeder 2.4
    Create and publish RSS and iTunes podcast feeds.

---
+++ Casks/feeder.rb
@@ -0,0 +1,7 @@
+class Feeder < Cask
+  url 'http://reinventedsoftware.com/feeder/downloads/Feeder_2.4.dmg'
+  homepage 'http://reinventedsoftware.com/feeder/'
+  version '2.4'
+  sha1 'fa7c00f4454b207f5553c3e9c58c4db4d60ff51a'
+  link 'Feeder.app'
+end
