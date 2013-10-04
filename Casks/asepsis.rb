76174126a80795586d3c3e661d8a125ecb5a7b4a
Rory Powis <rory@hikkendry.com>
2013-10-04 17:39:37 +0100

    Add Asepsis cask
    Asepsis prevents .DS_Store hell by redirecting their creation into a special folder.
    http://asepsis.binaryage.com/

---
+++ Casks/asepsis.rb
@@ -0,0 +1,9 @@
+class Asepsis < Cask
+  url 'http://downloads.binaryage.com/Asepsis-1.3.dmg'
+  homepage 'http://asepsis.binaryage.com/'
+  version '1.3'
+  sha1 '8eefcb13f738fce91a101eebab4314e31c4fa857'
+  link :none
+  install 'Asepsis.mpkg'
+  uninstall :pkgutil => 'com.binaryage.pkg.asepsis'
+end
