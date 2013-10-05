6403a0fd6143cadce9b675359c3aef221153cc27
elseym <elseym@me.com>
2013-10-05 19:33:37 +0200

    adding Little Snitch nightly build cask

---
+++ Casks/little-snitch-nightly.rb
@@ -0,0 +1,6 @@
+class LittleSnitchNightly < Cask
+  url 'http://www.obdev.at/downloads/LittleSnitch/nightly/LittleSnitch-3.2-nightly-(4044).dmg'
+  homepage 'http://www.obdev.at/products/littlesnitch'
+  version '3.2-nightly-(4044)'
+  sha1 '64ce89474a456dc2f49cf8a1957851a2000ad5d3'
+end
