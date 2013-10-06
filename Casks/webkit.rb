07d99a4bb1ddaa1e3361e53f2b5eea65051b8976
Newton <wreck@me.com>
2013-10-06 12:45:20 -0700

    Remove old webkit cask
    Remove the original cask as it conflicts with the node-webkit
    cask.

--- Casks/webkit.rb
+++
@@ -1,7 +0,0 @@
-class Webkit < Cask
-  url 'http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-r156962.dmg'
-  homepage 'http://www.webkit.org'
-  version 'r156962'
-  sha1 ''
-  link 'WebKit.app'
-end
