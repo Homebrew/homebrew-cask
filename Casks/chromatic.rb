e6f09c9f1bbf224041bb9561bc1cbd146d7220f6
elseym <elseym@me.com>
2013-10-06 13:02:00 +0200

    Version bump for Cask: Chromatic (0.2.3 to latest)

--- Casks/chromatic.rb
+++ Casks/chromatic.rb
@@ -1,7 +1,7 @@
 class Chromatic < Cask
   url 'http://download.mrgeckosmedia.com/Chromatic.zip'
   homepage 'https://mrgeckosmedia.com/applications/info/Chromatic'
-  version '0.2.3'
-  sha1 '5b28e43f89480d339a5f323c2650a2d4a2b5d487'
+  version 'latest'
+  no_checksum
   link 'Chromatic.app'
 end
