f4a142f400144ba08a438fc5fcd147ed497f800c
Roy Xiang <roy.x.work@gmail.com>
2013-10-06 18:39:56 +0800

    skype 6.9.59.517
    updated skype from 6.8.60.351 to 6.9.59.517

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,7 +1,7 @@
 class Skype < Cask
-  url 'http://download.skype.com/macosx/Skype_6.8.60.351.dmg'
+  url 'http://download.skype.com/macosx/Skype_6.9.59.517.dmg'
   homepage 'http://www.skype.com'
-  version '6.8.60.351'
-  sha1 '7ccb27d8de542adf176db69e6e27b520b66dd91f'
+  version '6.9.59.517'
+  sha1 '9849b43d1ae955a1e49dc2ebf86652f3e19a0662'
   link 'Skype.app'
 end
