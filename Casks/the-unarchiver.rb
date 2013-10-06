1e9d59b97901d9f256a8648cf42f08eaee3e5b66
Roy Xiang <roy.x.work@gmail.com>
2013-10-06 22:05:47 +0800

    the-unarchiver 3.9.1
    updated the-unarchiver from 3.8 to 3.9.1

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -1,7 +1,7 @@
 class TheUnarchiver < Cask
-  url 'http://theunarchiver.googlecode.com/files/TheUnarchiver3.8.zip'
+  url 'http://theunarchiver.googlecode.com/files/TheUnarchiver3.9.1.zip'
   homepage 'http://unarchiver.c3.cx/'
-  version '3.8'
-  sha1 'd4fca3f358df80dff6cccb67de2f5766a6d51d9f'
+  version '3.9.1'
+  sha1 'fc68b4b2a11560535ad055a464b48859186ba2d7'
   link 'The Unarchiver.app'
 end
