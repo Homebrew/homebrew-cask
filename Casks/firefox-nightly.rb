57ea8d54149cc9654003cb5c228dd1de381b1a94
Newton <wreck@me.com>
2013-10-06 12:53:05 -0700

    Add firefox nightly 27.0-alpha1

---
+++ Casks/firefox-nightly.rb
@@ -0,0 +1,7 @@
+class FirefoxNightly < Cask
+  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-27.0a1.en-US.mac.dmg'
+  homepage 'http://nightly.mozilla.org'
+  version '27.0-alpha1'
+  sha1 '4d321522354e9f5717fa63210f25cdb492accf91'
+  link 'FirefoxNightly.app'
+end
