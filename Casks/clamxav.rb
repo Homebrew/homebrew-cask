2f0543c85e6bf87e228139b20aa21e8f03072c5c
Fernando Paredes <nano@fdp.io>
2013-10-08 08:54:47 -0700

    ClamXav 2.6.1

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -1,7 +1,7 @@
 class Clamxav < Cask
-  url 'http://www.clamxav.com/downloads/ClamXav_2.5.dmg'
+  url 'http://www.clamxav.com/downloads/ClamXav_2.6.1.dmg'
   homepage 'http://www.clamxav.com/'
-  version '2.5'
-  sha1 '85f0d2b716b7300f6ed586032b3d88f359738971'
+  version '2.6.1'
+  sha1 '96f87257b466b2ac1eb6f5b9e32b83ca92664788'
   link 'ClamXav.app'
 end
