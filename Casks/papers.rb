48041c83fa3e1366d001ec5c74d78fc8122d6a33
Fernando Paredes <nano@fdp.io>
2013-10-08 09:02:38 -0700

    Fix link for Papers 2

--- Casks/papers.rb
+++ Casks/papers.rb
@@ -1,7 +1,7 @@
 class Papers < Cask
-  url 'http://www.papersapp.com/papers/download/mac'
+  url 'http://www.papersapp.com/papers2/download'
   homepage 'http://www.papersapp.com/papers/'
-  version 'latest'
+  version '2'
   no_checksum
   link 'Papers2.app'
 end
