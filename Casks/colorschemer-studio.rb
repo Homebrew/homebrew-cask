d9f59184972623acdf4c0f31eb96fcb15c09e157
Michael Moretti <mike.moretti@mac.com>
2013-10-07 17:44:53 -0700

    adding ColorSchemer Studio 2 cask

---
+++ Casks/colorschemer-studio.rb
@@ -0,0 +1,7 @@
+class ColorschemerStudio < Cask
+  url 'http://www.colorschemer.com/colorschemerstudio.dmg'
+  homepage 'http://www.colorschemer.com'
+  version '2.01'
+  no_checksum
+  link 'ColorSchemer Studio 2.app'
+end
