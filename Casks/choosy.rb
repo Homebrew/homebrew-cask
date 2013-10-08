b274ee3250377c39ce099c3d3dca16a118b16436
Josh Butts <josh@joshbutts.com>
2013-10-08 13:38:49 -0700

    Add Choosy

---
+++ Casks/choosy.rb
@@ -0,0 +1,7 @@
+class Choosy < Cask
+  url 'http://downloads.choosyosx.com/choosy_1.0.3.zip'
+  homepage 'http://www.choosyosx.com/'
+  version '1.0.3'
+  sha1 '738c2e14766f50922bf2d42368bd18e7f8b9d101'
+  prefpane 'Choosy.prefPane'
+end
