8669507a2a87bc2727dad49e0f164eaa49027366
Michael Moretti <mike.moretti@mac.com>
2013-10-11 16:42:28 -0700

    Added version and sha1

--- Casks/pycharm-community.rb
+++ Casks/pycharm-community.rb
@@ -1,7 +1,7 @@
 class PycharmCommunity < Cask
   url 'http://download.jetbrains.com/python/pycharm-community-3.0.dmg'
   homepage 'http://www.jetbrains.com/pycharm'
-  version 'latest'
-  no_checksum
+  version '3.0.0'
+  sha1 'c7fec41e4ba9c8d4ddfb78bde7d50029bdf8fd82'
   link 'PyCharm CE.app'
 end
