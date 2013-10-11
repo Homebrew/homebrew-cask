2519fcde4b4c1635468329b30adb689535fcece8
Sean T Allen <sean@monkeysnatchbanana.com>
2013-10-11 07:32:35 -0400

    Update to light table 0.5.6

--- Casks/light-table.rb
+++ Casks/light-table.rb
@@ -1,5 +1,5 @@
 class LightTable < Cask
-  url 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.5.4/LightTableMac.zip'
+  url 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.5.6/LightTableMac.zip'
   homepage 'http://www.lighttable.com/'
   version '0.5.6'
   sha1 '02bcb3a0794d55635d673712a4eb8c9f5efa34fa'
