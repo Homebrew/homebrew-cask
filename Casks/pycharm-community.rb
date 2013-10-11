9ca441c7f70d9001dece76e122f944f8e6e4cf6b
Michael Moretti <mike.moretti@mac.com>
2013-10-11 15:07:16 -0700

    Adding Pycharm Community version

---
+++ Casks/pycharm-community.rb
@@ -0,0 +1,7 @@
+class PycharmCommunity < Cask
+  url 'http://download.jetbrains.com/python/pycharm-community-3.0.dmg'
+  homepage 'http://www.jetbrains.com/pycharm'
+  version 'latest'
+  no_checksum
+  link 'PyCharm CE.app'
+end
