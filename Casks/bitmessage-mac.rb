6bf7d78cd66ddc3ce38a804ec729250210d0d3f0
Michael Ford <fanquake@users.noreply.github.com>
2013-10-12 12:25:08 +0800

    Update Bitmessage Homepage

--- Casks/bitmessage-mac.rb
+++ Casks/bitmessage-mac.rb
@@ -1,6 +1,6 @@
 class BitmessageMac < Cask
   url 'https://bitmessage.org/download/osx/bitmessage-v0.4.0.dmg'
-  homepage 'http://sourceforge.net/projects/bitmessagemac/'
+  homepage 'https://bitmessage.org/'
   version '0.4.0'
   sha1 '44cb0b818030bcddb577c773a053f80efcf199cd'
   link 'Bitmessage.app'
