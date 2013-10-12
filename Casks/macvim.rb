84c337e8f9bd2935c77791d00d41cbce75a149f0
gongqijian <gongqijian@gmail.com>
2013-10-12 12:25:17 +0800

    Macvim: Snow Leopard compatible

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,7 +1,14 @@
 class Macvim < Cask
-  url 'https://github.com/eee19/macvim/releases/download/snapshot-71/MacVim-snapshot-71.tbz'
   homepage 'http://code.google.com/p/macvim/'
   version '7.4-71'
-  sha1 '99c9d04b1a20fd53231bead9d10362070d0174e6'
+
+  if MacOS.version == :snow_leopard
+    url 'https://github.com/b4winckler/macvim/releases/download/snapshot-71/MacVim-snapshot-71-Snow-Leopard.tbz'
+    sha1 '104d5468544f008d84a22662798bf9bfae128993'
+  elsif MacOS.version >= :lion
+    url 'https://github.com/eee19/macvim/releases/download/snapshot-71/MacVim-snapshot-71.tbz'
+    sha1 '99c9d04b1a20fd53231bead9d10362070d0174e6'
+  end
+
   link 'MacVim-snapshot-71/MacVim.app'
 end
