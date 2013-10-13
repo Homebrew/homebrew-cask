b54ad8875212d4ad2aa967a557154f0d4bd6d929
Michael Ford <fanquake@users.noreply.github.com>
2013-10-13 16:04:41 +0800

    Update Android Studio version

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,7 +1,7 @@
 class AndroidStudio < Cask
   url 'https://dl-ssl.google.com/android/studio/ide-zips/0.2.13/android-studio-ide-132.863010-mac.zip'
   homepage 'https://developer.android.com/sdk/installing/studio.html'
-  version 'build-132.863010'
+  version '0.2.13 build-132.863010'
   sha1 '7bd5394f9cdb86755ae4763ad2b215da7f63694c'
   link 'Android Studio.app'
 end
