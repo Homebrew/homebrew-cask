2ba342fddd4e538dd8e29d277449d02077fd5037
Fernando Paredes <nano@fdp.io>
2013-10-13 22:09:29 -0700

    Add KeyboardCleanTool

---
+++ Casks/keyboard-clean-tool.rb
@@ -0,0 +1,7 @@
+class KeyboardCleanTool < Cask
+  url 'http://bettertouchtool.net/KeyboardCleanTool.zip'
+  homepage 'http://bettertouchtool.net'
+  version 'latest'
+  no_checksum
+  link 'KeyboardCleanTool.app'
+end
