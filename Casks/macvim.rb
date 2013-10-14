eda5edf3b777743aaa8b3e2499f7601901b06ebf
Kyle Huang <kyle@yellowaxe.com>
2013-10-13 23:12:00 -0500

    updated macvim link

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,7 +1,7 @@
 class Macvim < Cask
-  url 'https://github.com/eee19/macvim/releases/download/snapshot-71/MacVim-snapshot-71.tbz'
+  url 'https://github.com/eee19/macvim/releases/download/snapshot-71/MacVim-snapshot-71-Mountain-Lion.tbz'
   homepage 'http://code.google.com/p/macvim/'
   version '7.4-71'
-  sha1 '99c9d04b1a20fd53231bead9d10362070d0174e6'
+  sha1 '495a29e92769034e25779a66ee7fb120ada08792'
   link 'MacVim-snapshot-71/MacVim.app'
 end
