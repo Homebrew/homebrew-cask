4779bd224f9f4280e2827aea7414f83ff1407d74
Tom Vincent <git@tlvince.com>
2013-10-17 12:43:58 +0100

    Add MacTeX

---
+++ Casks/mactex.rb
@@ -0,0 +1,8 @@
+class Mactex < Cask
+  url 'http://mirror.ctan.org/systems/mac/mactex/MacTeX.pkg'
+  homepage 'http://www.tug.org/mactex/'
+  version 'latest'
+  sha1 'b36c1b7f16e4df3fb51bc8706a55ac62bd70b7f1'
+  install 'MacTex.pkg'
+  uninstall :pkgutil => 'org.tug.mactex.texlive2013'
+end
