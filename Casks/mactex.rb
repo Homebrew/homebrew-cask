756f81d5b3af545930bf9fe1700ad3831f79eb8e
Tom Vincent <git@tlvince.com>
2013-10-17 13:47:44 +0100

    Fix install pkg name

--- Casks/mactex.rb
+++ Casks/mactex.rb
@@ -3,6 +3,6 @@ class Mactex < Cask
   homepage 'http://www.tug.org/mactex/'
   version 'latest'
   sha1 'b36c1b7f16e4df3fb51bc8706a55ac62bd70b7f1'
-  install 'MacTex.pkg'
+  install 'MacTeX.pkg'
   uninstall :pkgutil => 'org.tug.mactex.texlive2013'
 end
