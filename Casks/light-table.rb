c44778b7e067e66be59321de46fdaa2330119dcc
Johan Hammar <johan.hammar@gmail.com>
2013-10-17 20:33:33 +0200

    Updated Light Table to 0.5.10 and corrected the link to the application.

--- Casks/light-table.rb
+++ Casks/light-table.rb
@@ -1,7 +1,7 @@
 class LightTable < Cask
-  url 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.5.6/LightTableMac.zip'
+  url 'https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.5.10/LightTableMac.zip'
   homepage 'http://www.lighttable.com/'
-  version '0.5.6'
-  sha1 '02bcb3a0794d55635d673712a4eb8c9f5efa34fa'
-  link 'LightTable.app'
+  version '0.5.10'
+  sha1 '3e09d84f2fba65d648801ac640c1ada2df98a255'
+  link 'LightTable/LightTable.app'
 end
