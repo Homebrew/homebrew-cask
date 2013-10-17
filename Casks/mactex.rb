727ba6b8bc3dba141796e80c897d479a362824b8
Tom Vincent <git@tlvince.com>
2013-10-17 14:06:35 +0100

    sha1 -> no_checksum: formula uses latest scheme

--- Casks/mactex.rb
+++ Casks/mactex.rb
@@ -2,7 +2,7 @@ class Mactex < Cask
   url 'http://mirror.ctan.org/systems/mac/mactex/MacTeX.pkg'
   homepage 'http://www.tug.org/mactex/'
   version 'latest'
-  sha1 'b36c1b7f16e4df3fb51bc8706a55ac62bd70b7f1'
+  no_checksum
   install 'MacTeX.pkg'
   uninstall :pkgutil => 'org.tug.mactex.texlive2013'
 end
