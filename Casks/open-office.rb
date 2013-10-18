bbfdbed318950384e473327b08c4769b80fd2bdb
Michael Ford <fanquake@users.noreply.github.com>
2013-10-18 11:20:52 +0800

    Open Office 4.0.1

--- Casks/open-office.rb
+++ Casks/open-office.rb
@@ -1,8 +1,8 @@
 class OpenOffice < Cask
-  url 'http://downloads.sourceforge.net/project/openofficeorg.mirror/4.0.0/binaries/en-US/Apache_OpenOffice_4.0.0_MacOS_x86_install_en-US.dmg'
+  url 'http://downloads.sourceforge.net/project/openofficeorg.mirror/4.0.1/binaries/en-US/Apache_OpenOffice_4.0.1_MacOS_x86_install_en-US.dmg'
   homepage 'http://www.openoffice.org/'
-  version '4.0.0'
-  sha1 '0184893223701aead7d627316cb2dceb92dd31cc'
+  version '4.0.1'
+  sha1 '6aed6c0a7fb6226ecbd4b6a2b371dc713ac58cd8'
   link 'OpenOffice.app'
 end
 
