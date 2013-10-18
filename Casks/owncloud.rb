c12d8c89766b2bd2aab4a7c2db2e999aa7b5575e
Michael Ford <fanquake@users.noreply.github.com>
2013-10-18 11:30:18 +0800

    ownCloud 1.4.1

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,7 +1,7 @@
 class Owncloud < Cask
-  url 'http://download.owncloud.com/download/ownCloud-1.4.0.dmg'
+  url 'http://download.owncloud.com/download/ownCloud-1.4.1.dmg'
   homepage 'http://owncloud.com'
-  version '1.4.0'
-  sha1 '6246d806509a71233944c0107c720666c3a16799'
+  version '1.4.1'
+  sha1 '291dd9babe2a16668c5f2890156c2539179f1530'
   link 'owncloud.app'
 end
