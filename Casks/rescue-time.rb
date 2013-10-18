133d1db151e0135078dafbb0c21b1e8249cbeb0f
billwanjohi <kaguru@gmail.com>
2013-10-18 05:40:13 +0000

    add rescue-time

---
+++ Casks/rescue-time.rb
@@ -0,0 +1,7 @@
+class RescueTime < Cask
+  url 'https://www.rescuetime.com/installers/RescueTimeInstaller.dmg'
+  homepage 'https://www.rescuetime.com'
+  version 'latest'
+  no_checksum
+  link 'RescueTime'
+end
