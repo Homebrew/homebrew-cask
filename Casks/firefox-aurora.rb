8bdd3ec2e463979ab9748d06520b2e1c8df17b32
Michael Ford <fanquake@users.noreply.github.com>
2013-10-18 14:03:15 +0800

    FireFox Aurora 26.0a2

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,7 +1,7 @@
 class FirefoxAurora < Cask
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-25.0a2.en-US.mac.dmg'
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-26.0a2.en-US.mac.dmg'
   homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
-  version '25.0a2'
-  sha1 'ea9a343ee322ae2e243df2bc0b23ce25005cb46f'
+  version '26.0a2'
+  sha1 'e86568eb1ccc4483d15d7c8660b4724fa7197dcd'
   link 'FirefoxAurora.app'
 end
