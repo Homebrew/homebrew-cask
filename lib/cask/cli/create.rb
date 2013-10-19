b93325eeb96d22584318c0b6aa766aa4c5eee312
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-19 23:15:38 +0800

    Revert "BROKEN: Added template to generate url, SHA and link automatically based on a new Cask's URL, but not working yet"
    This reverts commit 0a26fcd7d66672135e908c57c0e0a245ce5325f5.

--- lib/cask/cli/create.rb
+++ lib/cask/cli/create.rb
@@ -2,37 +2,13 @@ module Cask::CLI::Create
   def self.run(*arguments)
     cask_name, *_ = *arguments
     cask_path = Cask.path(cask_name)
-    url, version, sha, link
 
     if cask_path.exist?
       raise CaskAlreadyCreatedError.new cask_name
     end
 
     File.open(cask_path, 'w') do |f|
-
-    if ARGV.include "--url"
-      url = ARGV.named.first
-      version = ARGV.next if ARGV.include? '--set-version'
-      name = ARGV.next if ARGV.include? '--set-name'
-
-      # download
-      download = Cask::Download.new(@cask)
-      @downloaded_path = download.perform
-
-      # extract
-      FileUtils.mkdir_p @cask.destination_path
-      container = Cask::Container.for_path(@downloaded_path, @command)
-      unless container
-        raise "uh oh, could not identify container for #{@downloaded_path}"
-      end
-      container.new(@cask, @downloaded_path, @command).extract
-
-      # TODO: Calculate the Sha, find .app for link
-    end
- 
-    f.write template(cask_name)
-    ohai "Success! #{@cask_name} template generated in #{@f.destination_path}"
-
+      f.write template(cask_name)
     end
 
     exec_editor cask_path
@@ -42,11 +18,11 @@ module Cask::CLI::Create
     cask_class = cask_name.split('-').map(&:capitalize).join
     <<-EOS.undent
       class #{cask_class} < Cask
-        url '#{url}'
+        url ''
         homepage ''
-        version '#{version}'
-        sha1 '#{sha}'
-        link '#{link}'
+        version ''
+        sha1 ''
+        link ''
       end
     EOS
   end
