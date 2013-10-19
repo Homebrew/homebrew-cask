b7991b195f87b8bb2f501b4eed64adfcfb123609
Michael Ford <fanquake@users.noreply.github.com>
2013-10-19 10:49:18 +0800

    Chromium 30.0.1599.101

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,7 +1,7 @@
 class Chromium < Cask
-  url 'http://downloads.sourceforge.net/project/osxportableapps/Chromium/ChromiumOSX_30.0.1599.69.dmg'
+  url 'http://downloads.sourceforge.net/project/osxportableapps/Chromium/ChromiumOSX_30.0.1599.101.dmg'
   homepage 'http://www.freesmug.org/chromium'
-  version '30.0.1599.69'
-  sha1 'ed6f7f94c53e724d99dcf40e9a9b9b65760f215b'
+  version '30.0.1599.101'
+  sha1 '329b75b7a616ad55ac4cc84cd4206a0b33ac097c'
   link 'Chromium.app'
 end
