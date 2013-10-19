0b8f1897b2d432a69acc7f68708356802717e990
Darin Morrison <darinmorrison+git@gmail.com>
2013-10-18 23:15:29 -0600

    uber-network-fuser: add cask

---
+++ Casks/uber-network-fuser.rb
@@ -0,0 +1,7 @@
+class UberNetworkFuser < Cask
+  url 'https://dl.dropbox.com/s/uytv8p2eljk6fez/ubernetfuser_1.701.dmg'
+  homepage 'http://nickapedia.com/2012/01/10/breaking-new-ground-an-uber-tool-for-the-mac/'
+  version '1.701'
+  sha1 'b7ab539b71056704c2462a27b18f79a2d3d652f5'
+  link 'UBER Network Fuser 1.701.app'
+end
