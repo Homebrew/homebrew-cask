c588d2dd4921933445e34bb4285560db7a3883f3
Ken Sato <ksato9700@gmail.com>
2013-10-19 20:35:10 +0900

    add new cask for Freemind

---
+++ Casks/freemind.rb
@@ -0,0 +1,7 @@
+class Freemind < Cask
+  url 'http://downloads.sourceforge.net/project/freemind/freemind/1.0.0/FreeMind_1.0.0.dmg'
+  homepage 'freemind.sourceforge.net'
+  version '1.0.0'
+  sha1 'eb24852195b434b1c72e5b5759b3e54d5a352111'
+  link 'FreeMind.app'
+end
