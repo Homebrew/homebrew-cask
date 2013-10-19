7561af89de395b18433ab9ae4d627f029fa06ab7
Shane da Silva <shane@dasilva.io>
2013-10-19 00:48:24 -0700

    Fix broken alfred cask
    The ability to link any `.app` file without specifying the full path was
    removed by c244385e. This means as of version 0.18.0 of `brew-cask` the
    `alfred` cask has been broken, as it raises an error due to being unable
    to find `Alfred Preferences.app` (since it is not located in the root
    app directory).

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -3,5 +3,5 @@ class Alfred < Cask
   homepage 'http://www.alfredapp.com/'
   version '2.0.9_214'
   sha1 '550cba6dc44250ae5940c3d302cf06e199eb6958'
-  link 'Alfred 2.app', 'Alfred Preferences.app'
+  link 'Alfred 2.app', 'Alfred 2.app/Contents/Preferences/Alfred Preferences.app'
 end
