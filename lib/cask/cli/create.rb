0a26fcd7d66672135e908c57c0e0a245ce5325f5
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-19 22:27:27 +0800

    BROKEN: Added template to generate url, SHA and link automatically based on a new Cask's URL, but not working yet

--- lib/cask/cli/create.rb
+++ lib/cask/cli/create.rb
@@ -2,13 +2,37 @@ module Cask::CLI::Create
   def self.run(*arguments)
     cask_name, *_ = *arguments
     cask_path = Cask.path(cask_name)
+    url, version, sha, link
 
     if cask_path.exist?
       raise CaskAlreadyCreatedError.new cask_name
     end
 
     File.open(cask_path, 'w') do |f|
-      f.write template(cask_name)
+
+    if ARGV.include "--url"
+      url = ARGV.named.first
+      version = ARGV.next if ARGV.include? '--set-version'
+      name = ARGV.next if ARGV.include? '--set-name'
+
+      # download
+      download = Cask::Download.new(@cask)
+      @downloaded_path = download.perform
+
+      # extract
+      FileUtils.mkdir_p @cask.destination_path
+      container = Cask::Container.for_path(@downloaded_path, @command)
+      unless container
+        raise "uh oh, could not identify container for #{@downloaded_path}"
+      end
+      container.new(@cask, @downloaded_path, @command).extract
+
+      # TODO: Calculate the Sha, find .app for link
+    end
+ 
+    f.write template(cask_name)
+    ohai "Success! #{@cask_name} template generated in #{@f.destination_path}"
+
     end
 
     exec_editor cask_path
@@ -18,11 +42,11 @@ module Cask::CLI::Create
     cask_class = cask_name.split('-').map(&:capitalize).join
     <<-EOS.undent
       class #{cask_class} < Cask
-        url ''
+        url '#{url}'
         homepage ''
-        version ''
-        sha1 ''
-        link ''
+        version '#{version}'
+        sha1 '#{sha}'
+        link '#{link}'
       end
     EOS
   end
