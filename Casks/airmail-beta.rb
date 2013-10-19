5cd1c34d2564a1f9de25678238ea9a345a2131ef
Fernando Paredes <nano@fdp.io>
2013-10-18 18:50:01 -0700

    Airmail Beta 210

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,7 +1,7 @@
 class AirmailBeta < Cask
-  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/622/767/original/0d82401796550e70da74e4389bdc51b8/AirMail_Beta.zip'
+  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/630/583/original/629661dc14a4b417234703b0dfafd847/AirMail_Beta.zip'
   homepage 'http://airmailapp.com/'
-  version '1.2(209)'
-  sha1 '04f21290496ff47515ce77d16d91e94ec892bbf1'
+  version '1.2(210)'
+  sha1 '08bece7a7729cb9e53fd2f7aaea1320461aecd96'
   link 'AirMail Beta.app'
 end
