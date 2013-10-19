d97fae18d68a896e7401be8a85b1883278c39890
Gilles Fabio <gilles@gillesfabio.com>
2013-10-19 21:53:21 +0200

    Add SHA1 to Lion DiskMaker.

--- Casks/lion-disk-maker.rb
+++ Casks/lion-disk-maker.rb
@@ -2,6 +2,6 @@ class LionDiskMaker < Cask
   url 'http://liondiskmaker.com/downloads/LionDiskMaker.zip'
   homepage 'http://liondiskmaker.com/'
   version '2.0.2'
-  no_checksum
+  sha1 '1e0c898f48b1eb1f45fe8e33cc01cf40d237e378'
   link "Lion DiskMaker #{version}/Lion DiskMaker 2.app"
 end
