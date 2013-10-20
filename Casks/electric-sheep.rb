3269c92d304e2e02ffc254fa95d1e1f221d37bf6
Michael Ford <fanquake@users.noreply.github.com>
2013-10-20 12:04:57 +0800

    Update Electric Sheep SHA1

--- Casks/electric-sheep.rb
+++ Casks/electric-sheep.rb
@@ -2,6 +2,6 @@ class ElectricSheep < Cask
   url 'http://electricsheep.googlecode.com/files/electricsheep-2.7b35b.dmg'
   homepage 'http://www.electricsheep.org'
   version '2.7b35b'
-  sha1 'baeaf679882b4d4793f014d356f017420c42b806'
+  sha1 'fed402ee86bd91de8166c2e297f4cf3a06c99b70'
   install 'Electric Sheep.mpkg'
 end
