e4876881cc760ab34fa3a00d0f18df5418bc4f86
Michael Ford <fanquake@users.noreply.github.com>
2013-10-20 20:03:33 +0800

    DSP Radio 1.4.1

--- Casks/dsp-radio.rb
+++ Casks/dsp-radio.rb
@@ -1,7 +1,7 @@
 class DspRadio < Cask
-  url 'http://dl2sdr.homepage.t-online.de/files/DSP_Radio_140.zip'
+  url 'http://dl2sdr.homepage.t-online.de/files/DSP_Radio_141.zip'
   homepage 'http://dl2sdr.homepage.t-online.de/'
-  version '1.4.0'
-  sha1 'eb54eaba94648c9cd5127e9dd792cbf89c221bb9'
-  link 'DSP Radio 1.4.0.app'
+  version '1.4.1'
+  sha1 '695bd2ae64be507fb85471c487832661b025c6d7'
+  link 'DSP Radio 1.4.1.app'
 end
