1c668b4c7da09874b856a649ea588ab43ee8689d
Martin Fernandez <fmartin91@gmail.com>
2013-10-21 16:28:39 -0200

    Added new cask for Tunnel Bear app.
    Tunnel bear is a VPN service with an incredible simple UI. More information here: https://www.tunnelbear.com/about/

---
+++ Casks/TunnelBear.rb
@@ -0,0 +1,7 @@
+class Tunnelbear < Cask
+  url 'https://tunnelbear.s3.amazonaws.com/downloads/mac/TunnelBear-2.1.1.zip'
+  homepage 'https://www.tunnelbear.com/'
+  version '2.1.1'
+  sha1 '4047eca8def0caed815df12ddb088b9cce7a4b25'
+  link 'TunnelBear.app'
+end
