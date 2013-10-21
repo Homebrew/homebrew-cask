8c1c3fddcb2ad91f572f0b61dbd6a2287bfd5e59
phinze <paul.t.hinze@gmail.com>
2013-10-21 07:27:52 -0500

    fix audacity link line
    audacity ships in a nested directory
    
    refs #1249

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -3,5 +3,5 @@ class Audacity < Cask
   homepage 'http://audacity.sourceforge.net/'
   version '2.0.4'
   sha1 '76fdff5fece1f588d2a5d0bea703bfd309b4d619'
-  link 'Audacity.app'
+  link 'Audacity/Audacity.app'
 end
