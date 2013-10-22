d01c5684d491b34f28699cd9778d76ae2ac08f60
Jay Harris <jay@aranasoft.com>
2013-10-22 17:17:53 -0400

    PyCharm v3.0.1
    Updated PyCharm Community and Pro casks to v3.0.1

--- Casks/pycharm-pro.rb
+++ Casks/pycharm-pro.rb
@@ -1,7 +1,7 @@
 class PycharmPro < Cask
-  url 'http://download-ln.jetbrains.com/python/pycharm-professional-3.0.dmg'
+  url 'http://download-ln.jetbrains.com/python/pycharm-professional-3.0.1.dmg'
   homepage 'http://www.jetbrains.com/pycharm/'
-  version '3.0.0'
-  sha1 'd8a03ada2e6400d4b4b1c1eb20d850816307125e'
+  version '3.0.1'
+  sha1 '9109148b106b8541ec080022691b1f018845780a'
   link 'PyCharm.app'
 end
