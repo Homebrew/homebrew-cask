9c05bc02bdf04bf05fe6c70621fa8023c7226b8d
Michael Ford <fanquake@users.noreply.github.com>
2013-10-22 21:07:29 +0800

    PhpStorm 7.0

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -1,7 +1,7 @@
 class Phpstorm < Cask
-  url 'http://download.jetbrains.com/webide/PhpStorm-6.0.3.dmg'
+  url 'http://download.jetbrains.com/webide/PhpStorm-7.0.dmg'
   homepage 'http://www.jetbrains.com/phpstorm/'
-  version '6.0.3'
-  sha1 'ea98d8579e8248b953ec2f8915500dcdab834fcc'
+  version '7.0'
+  sha1 '8a9012622f5761382983877f5450456a151415f2'
   link 'PhpStorm.app'
 end
