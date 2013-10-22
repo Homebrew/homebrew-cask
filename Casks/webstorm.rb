e8309a9fa3e359d6c44cf839c709cb71021abf7e
Jay Harris <jay@aranasoft.com>
2013-10-22 17:03:11 -0400

    WebStorm 7.0.1
    Updating WebStorm Cask to v7.0.1

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -1,7 +1,7 @@
 class Webstorm < Cask
-  url 'http://download.jetbrains.com/webstorm/WebStorm-7.0.dmg'
+  url 'http://download.jetbrains.com/webstorm/WebStorm-7.0.1.dmg'
   homepage 'http://www.jetbrains.com/webstorm/'
-  version '7.0'
-  sha1 'fcebee6fc15226e2d73eb92a980860fc19766e62'
+  version '7.0.1'
+  sha1 '77cd9189a332071ebee172f169941bf4ab856da7'
   link 'WebStorm.app'
 end
