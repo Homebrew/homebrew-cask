5287ec9e4ad63d0921cce852805eab3fc6a3ca2e
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-23 21:08:25 +0100

    Update 1Password

--- Casks/one-password.rb
+++ Casks/one-password.rb
@@ -1,7 +1,7 @@
 class OnePassword < Cask
-  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.0.1.zip'
+  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.0.2.zip'
   homepage 'https://agilebits.com/onepassword'
-  version '4.0.1'
+  version '4.0.2'
   sha1 'd0ef1662459791b423bd86fbc3026a12b7d2e97b'
   link '1Password 4.app'
 end
