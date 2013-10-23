1b73765237820176b97bc8dc540239b8cdcfaed0
Haralan Dobrev <hkdobrev@gmail.com>
2013-10-23 22:13:06 +0300

    Update MPlayerX to 1.0.18

--- Casks/mplayerx.rb
+++ Casks/mplayerx.rb
@@ -1,7 +1,7 @@
 class Mplayerx < Cask
-  url 'http://mplayerx.googlecode.com/files/MPlayerX-1.0.17.zip'
+  url 'http://mplayerx.googlecode.com/files/MPlayerX-1.0.18.zip'
   homepage 'http://mplayerx.org/'
-  version '1.0.17'
-  sha1 'e26e9c54dca2404eaab881586f04d73de7d9bdc9'
+  version '1.0.18'
+  sha1 '544736401af40ad4c3ef918213a9a675426c9183'
   link 'MPlayerX.app'
 end
