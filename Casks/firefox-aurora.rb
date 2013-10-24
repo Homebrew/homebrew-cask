f068a57ed18c2133fcb4f5e5e2a479a5369b8a4f
Erwann Mest <erwann.mest@gmail.com>
2013-10-24 22:58:39 +0200

    Fix SHA1 for Aurora.

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -2,6 +2,6 @@ class FirefoxAurora < Cask
   url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-26.0a2.en-US.mac.dmg'
   homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
   version '26.0a2'
-  sha1 '9ecf3da45382284e97762345b59dcc9c5865ea4d'
+  sha1 '9504d9b313442610881b45b000d2b17af08ae48e'
   link 'FirefoxAurora.app'
 end
