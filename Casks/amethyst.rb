10969c93395a02798bd6112b90df5faf8093f1fc
Leonard Truong <leonardtruong@Leonards-MacBook-Pro.local>
2013-10-23 21:28:17 -0700

    Updating Amethyst from 0.8.2 to 0.8.3

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -1,7 +1,7 @@
 class Amethyst < Cask
-  url 'http://ianyh.com/amethyst/versions/Amethyst-0.8.2.zip'
+  url 'http://ianyh.com/amethyst/versions/Amethyst-0.8.3.zip'
   homepage 'http://ianyh.com/amethyst'
-  version '0.8.2'
-  sha1 '4f52fad5c05ea33bef862f5e8017b1d4a0456d7b'
+  version '0.8.3'
+  sha1 '441d26016128d8f4eede1d2d1b5f9b5779ee08b6'
   link 'Amethyst.app'
 end
