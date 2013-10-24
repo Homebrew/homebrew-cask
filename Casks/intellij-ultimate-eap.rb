ce854aeac4faf2ec382ecc47c511195b3b3b814c
CJ Fleck <cj.fleck@gmail.com>
2013-10-24 10:32:15 -0400

    Intellij IDEA 13 EAP
    Installs the latest Intellij Ultimate EAP build.

---
+++ Casks/intellij-ultimate-eap.rb
@@ -0,0 +1,7 @@
+class IntellijUltimateEap < Cask
+  url 'http://download.jetbrains.com/idea/ideaIU-132.719.dmg'
+  homepage 'http://confluence.jetbrains.com/display/IDEADEV/IDEA+13+EAP'
+  version '13 132.719'
+  sha1 'a0e3b53806fce63c3e6b3af5be865658b9d41956'
+  link 'Intellij IDEA 13 EAP.app'
+end
