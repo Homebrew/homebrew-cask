2b32adeb573e560d8409bb705b5c12630eb0e07e
gitolite tester <tester@example.com>
2013-10-25 14:38:27 +0800

    Add KDiff3 v0.9.97

---
+++ Casks/kdiff3.rb
@@ -0,0 +1,7 @@
+class Kdiff3 < Cask
+  url 'http://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.97/kdiff3_0.9.97_MacOS_64bit.dmg'
+#http://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.97/kdiff3_0.9.97_MacOS_64bit.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fkdiff3%2Ffiles%2F&ts=1382682668&use_mirror=jaist  homepage 'http://bibdesk.sourceforge.net/'
+  version '0.9.97'
+  sha1 '24ebaec55523808d176e4280451ed37d9295a64a'
+  link 'KDiff3.app'
+end
