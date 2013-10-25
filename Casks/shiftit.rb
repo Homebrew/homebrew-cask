1f9d77618bf77c96811a58e6320a3dccc0429af1
Fernando Paredes <nano@fdp.io>
2013-10-25 11:29:38 -0700

    Update ShiftIt Homepage

--- Casks/shiftit.rb
+++ Casks/shiftit.rb
@@ -1,6 +1,6 @@
 class Shiftit < Cask
   url 'https://github.com/downloads/fikovnik/ShiftIt/ShiftIt-develop-1.6.zip'
-  homepage 'https://github.com/fikovnik/ShiftIt/downloads'
+  homepage 'https://github.com/fikovnik/ShiftIt'
   version '1.6'
   sha1 'ee7ce2fa15f211116835490b4692eba14dbd47ab'
   link 'ShiftIt.app'
