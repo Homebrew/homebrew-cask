f7492d848407e0a56e47238047c105dd6f7bcac5
bmcustodio <bruno@brunomcustodio.com>
2013-10-25 13:35:38 +0100

    Add cask for Google Music Manager.
    Upload music from your computer to Google Play with the Music Manager desktop
    application and listen to the music you love anywhere you have Internet access.

---
+++ Casks/google-music-manager.rb
@@ -0,0 +1,7 @@
+class GoogleMusicManager < Cask
+  url 'https://dl.google.com/dl/androidjumper/mac/718015/musicmanager.dmg'
+  homepage 'https://music.google.com'
+  version '1.0.71.8015'
+  sha1 '1f695cb8b08606476a3a12acd86a0a2d36da81a5'
+  link 'MusicManager.app'
+end
