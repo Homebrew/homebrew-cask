1dd220f5c4213f44b3962b6409cae02ca73ca6b1
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-25 21:00:52 +0800

    Update diffmerge v4.2.0.697

--- Casks/diffmerge.rb
+++ Casks/diffmerge.rb
@@ -1,7 +1,7 @@
 class Diffmerge < Cask
-  url 'http://download-us.sourcegear.com/DiffMerge/4.1.0/DiffMerge.4.1.0.534.intel.stable.dmg'
+  url 'http://download-us.sourcegear.com/DiffMerge/4.2.0/DiffMerge.4.2.0.697.intel.stable.dmg'
   homepage 'http://www.sourcegear.com/diffmerge'
-  version '4.1.0.534'
-  sha1 '41f3b7c12fd5ecca09348c3d857cb84e004c0a1b'
+  version '4.2.0.697'
+  sha1 '15301ee927fd74f2c0be5efa57673c6391399ee2'
   link 'DiffMerge.app'
 end
