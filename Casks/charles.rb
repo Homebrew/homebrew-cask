05156a8b667775ce0760ce746a861252d63ddb96
Leonard van Driel <leonardvandriel@gmail.com>
2013-10-25 11:58:33 +0200

    charles 3.8.3

--- Casks/charles.rb
+++ Casks/charles.rb
@@ -1,7 +1,7 @@
 class Charles < Cask
-  url 'http://www.charlesproxy.com/assets/release/3.8.1/charles-proxy-3.8.1-applejava.dmg'
+  url 'http://www.charlesproxy.com/assets/release/3.8.3/charles-proxy-3.8.3-applejava.dmg'
   homepage 'http://www.charlesproxy.com/'
-  version '3.8.1'
-  sha1 '1a291614893955fa02e573ce27a5d40c1dffe287'
+  version '3.8.3'
+  sha1 'fb587455e7566c6d507a58231d3a3ebb68f1349f'
   link 'Charles.app'
 end
