ecc6680e5974bcfbd9b5f3f5bf211778f44f8616
Fernando Paredes <nano@fdp.io>
2013-10-25 14:35:40 -0700

    Inboard 182

---
+++ Casks/inboard.rb
@@ -0,0 +1,7 @@
+class Inboard < Cask
+  url 'http://dl.devmate.com/com.ideabits.Inboard/182/1382731593/Inboard-182.dmg'
+  homepage 'http://inboardapp.com/beta'
+  version '182'
+  sha1 '2deb28e3688abd23ef9ecd4f3467c65cefef4de1'
+  link 'Inboard.app'
+end
