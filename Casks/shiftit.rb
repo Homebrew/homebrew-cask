d74d32d76bb9038d8e016d581f2b29a7f8a3bb0a
capen <cbrinkley@gmail.com>
2013-10-25 01:01:45 -0700

    adding shiftit
    ShiftIt: Managing window size and position in OSX. BOOM!

---
+++ Casks/shiftit.rb
@@ -0,0 +1,7 @@
+class Shiftit < Cask
+  url 'https://github.com/downloads/fikovnik/ShiftIt/ShiftIt-develop-1.6.zip'
+  homepage 'https://github.com/downloads/fikovnik/ShiftIt'
+  version '1.6'
+  sha1 'ee7ce2fa15f211116835490b4692eba14dbd47ab'
+  link 'ShiftIt.app'
+end
