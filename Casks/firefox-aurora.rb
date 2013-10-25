76ebfccc154f13f9e22b56e0e8aada088f76a3c0
Erwann Mest <erwann.mest@gmail.com>
2013-10-25 22:23:37 +0200

    Remove aurora checksum. Useless for a nightly.

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -2,6 +2,6 @@ class FirefoxAurora < Cask
   url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-26.0a2.en-US.mac.dmg'
   homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
   version '26.0a2'
-  sha1 '9ecf3da45382284e97762345b59dcc9c5865ea4d'
+  no_checksum
   link 'FirefoxAurora.app'
 end
