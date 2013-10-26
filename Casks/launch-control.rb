7260163328b2355859174a4f75ef534f0bb693f1
Michael Ford <fanquake@users.noreply.github.com>
2013-10-26 11:46:26 +0800

    Launch Control 1.10

--- Casks/launch-control.rb
+++ Casks/launch-control.rb
@@ -1,7 +1,7 @@
 class LaunchControl < Cask
-  url 'http://www.soma-zone.com/download/files/LaunchControl_1.7.tar.bz2'
+  url 'http://www.soma-zone.com/download/files/LaunchControl_1.10.tar.bz2'
   homepage 'http://www.soma-zone.com/LaunchControl/'
-  version '1.7'
-  sha1 'd179beb36ee1b62d63557131e1212617c596c7d8'
+  version '1.10'
+  sha1 'ce17ecd9a418263ad41e0765dea83510649b89b9'
   link 'LaunchControl.app'
 end
