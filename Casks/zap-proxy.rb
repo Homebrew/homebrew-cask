c7c5836713c9ace434c309c11da3b896af0d4aec
Michael Ford <fanquake@users.noreply.github.com>
2013-10-26 12:24:06 +0800

    Zaproxy 2.2.2

--- Casks/zap-proxy.rb
+++ Casks/zap-proxy.rb
@@ -1,7 +1,7 @@
 class ZapProxy < Cask
-  url 'https://zaproxy.googlecode.com/files/ZAP_2.2.1_Mac_OS_X.zip'
+  url 'https://zaproxy.googlecode.com/files/ZAP_2.2.2_Mac_OS_X.zip'
   homepage 'https://www.owasp.org/index.php/OWASP_Zed_Attack_Proxy_Project'
-  version '2.2.1'
-  sha1 'a948e4b03cb095d5b097c824268e756d92205096'
+  version '2.2.2'
+  sha1 '42a21c3f1db558fcb582f09898d161dfa73646e4'
   link 'OWASP ZAP.app'
 end
