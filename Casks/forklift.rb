0408f3ea64524d3f627239f53464287486256af5
Steffen Becker <webgefrickel@gmail.com>
2013-10-26 14:56:04 +0200

    Adding Forklift File Manager
    Cask for the Forklift-Filemanager in the most current version.

---
+++ Casks/forklift.rb
@@ -0,0 +1,7 @@
+class Forklift < Cask
+  url 'http://download.binarynights.com/ForkLift2.5.6.zip'
+  homepage 'http://www.binarynights.com/'
+  version '2.5.6'
+  sha1 '95e118dc4ca5bd2a0f4fe3e3aacecc60c1aa6bbd'
+  link 'ForkLift.app'
+end
