2114fb3aa7b8f21c2a602f739c524bbcf261b353
Michael Ford <fanquake@users.noreply.github.com>
2013-10-26 12:03:43 +0800

    Opera 17.0.1241.53

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,7 +1,7 @@
 class Opera < Cask
-  url 'http://get.geo.opera.com/pub/opera/desktop/17.0.1241.45/mac/Opera_17.0.1241.45_Setup.dmg'
+  url 'http://get.geo.opera.com/pub/opera/desktop/17.0.1241.53/mac/Opera_17.0.1241.53_Setup.dmg'
   homepage 'http://www.opera.com/'
-  version '17.0.1241.45'
-  sha1 'e2c64935a0f4f8ef641540efde9a93ff2627083b'
+  version '17.0.1241.53'
+  sha1 '648550cd184c51bb46d265bf60b0a48ba341d1f9'
   link 'Opera.app'
 end
