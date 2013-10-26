43fce30c0acee0d1fc87e29bb43e139dae5b9df3
Sergey Bronnikov <estetus@gmail.com>
2013-10-26 14:47:10 +0400

    Parallels Desktop

---
+++ Casks/parallels-desktop.rb
@@ -0,0 +1,7 @@
+class ParallelsDesktop < Cask
+  url 'http://download.parallels.com/restricted/download/desktop/v9/update1/ParallelsDesktop-9.0.23136.932290.dmg?2bfb2aa2bcfacd8dd0d2caf8ff5b16ebcf2c7a3dc7549f325e09c02c458ce64531f2'
+  homepage 'http://www.parallels.com/products/desktop/'
+  version '9.0.23136'
+  sha1 '601606d709554ab1e3138742c8cf23554286b55c'
+  link 'Parallels Desktop.app'
+end
