eda95805aee0fcf51760edfd6569dac4ae1054fe
Huang Yangqi <i@hyq.me>
2013-10-26 23:34:42 +0800

    create dRadio
    dRadio is a douban FM client, for Chinese users

---
+++ Casks/dradio.rb
@@ -0,0 +1,7 @@
+class Dradio < Cask
+  url 'http://dradio.me/download'
+  homepage 'http://dradio.me'
+  version '1.5.12'
+  sha1 '1d0570679e439eb74a7ee5f3b73ed64bc8f6ac2b'
+  link 'dRadio.app'
+end
