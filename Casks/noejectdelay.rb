de6b21ff73b9d5ebebc388c0d3cd2c6a26962d6f
gregstallings <gregstallings@gmail.com>
2013-10-26 16:24:42 -0700

    Add noejectdelay cask
    NoEjectDelay eliminates the eject key delay and disables the F12 eject
    feature.

---
+++ Casks/noejectdelay.rb
@@ -0,0 +1,7 @@
+class Noejectdelay < Cask
+  url 'https://pqrs.org/macosx/keyremap4macbook/files/NoEjectDelay-5.0.0.dmg'
+  homepage 'https://pqrs.org/macosx/keyremap4macbook/noejectdelay.html.en'
+  version '5.0.0'
+  sha1 '1e1797c9d12b103a34d7b8ade58d7f3b85634336'
+  install 'NoEjectDelay.pkg'
+end
