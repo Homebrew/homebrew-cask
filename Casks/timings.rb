4757fa3647a6f170b61efb22e897b6ff42870fb7
Steffen Becker <webgefrickel@gmail.com>
2013-10-26 15:39:44 +0200

    Add Timings time-tracking app
    The awesome companion to Grand Total for time tracking.

---
+++ Casks/timings.rb
@@ -0,0 +1,7 @@
+class Timings < Cask
+  url 'http://mediaatelier.com/Timings/Timings_1.0.4.zip'
+  homepage 'http://mediaatelier.com/Timings'
+  version '1.0.4'
+  sha1 '6a757d032a9395436468bd0801f80aa6cfc4a6dd'
+  link 'Timings.app'
+end
