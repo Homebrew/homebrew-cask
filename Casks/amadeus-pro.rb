70e217419e9f673ff4b07150a0d72062b51b3739
Steffen Becker <webgefrickel@gmail.com>
2013-10-26 15:01:13 +0200

    Amadeus Pro 2
    Add a cask for the audio-editor Amadeus Pro, a nice
    and beautiful little tool for audio-editing and converting.
    
    Signed-off-by: Fernando Paredes <nano@fdp.io>
    
    Closes # 1309

---
+++ Casks/amadeus-pro.rb
@@ -0,0 +1,7 @@
+class AmadeusPro < Cask
+  url 'http://s3.amazonaws.com/AmadeusPro2/AmadeusPro.dmg'
+  homepage 'http://www.hairersoft.com/pro.html'
+  version '2'
+  no_checksum
+  link 'Amadeus Pro.app'
+end
