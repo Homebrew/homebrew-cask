4700e0a523b6d786b56c9edd7c4a217846ef334e
Michael Ford <fanquake@users.noreply.github.com>
2013-10-26 11:29:26 +0800

    Fantastical 1.3.10

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,7 +1,7 @@
 class Fantastical < Cask
-  url 'http://cdn.flexibits.com/Fantastical_1.3.9.zip'
+  url 'http://cdn.flexibits.com/Fantastical_1.3.10.zip'
   homepage 'http://flexibits.com/fantastical'
-  version '1.3.9'
-  sha1 '12d49a7ee47b67d3f0bc81deeaed4e91ef346794'
+  version '1.3.10'
+  sha1 '62443ccd47b8bd8bfb5e88d1bc2efbaeeb37691a'
   link 'Fantastical.app'
 end
