43fc8b20a274460cc4511c701c7bb649dc314cf8
Michael Ford <fanquake@users.noreply.github.com>
2013-10-26 11:36:02 +0800

    Skim 1.4.6

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,7 +1,7 @@
 class Skim < Cask
-  url 'http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.4.5/Skim-1.4.5.dmg'
+  url 'http://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.4.6/Skim-1.4.6.dmg'
   homepage 'http://skim-app.sourceforge.net/'
-  version '1.4.5'
-  sha1 '22cfaa81a68dfa857790307346b98fb65fee8a7b'
+  version '1.4.6'
+  sha1 '8d5e091bb931106a66b1ecdd07b50f56aff80475'
   link 'Skim.app'
 end
