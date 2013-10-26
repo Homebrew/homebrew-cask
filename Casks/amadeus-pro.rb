98716cb81242f06e9fd9262bc912b97804ce8003
Steffen Becker <webgefrickel@gmail.com>
2013-10-26 15:01:13 +0200

    Adding Amadeus Pro
    Add a cask for the audio-editor Amadeus Pro, a nice
    and beautiful little tool for audio-editing and converting.

---
+++ Casks/amadeus-pro.rb
@@ -0,0 +1,7 @@
+class AmadeusPro < Cask
+  url 'http://s3.amazonaws.com/AmadeusPro2/AmadeusPro.dmg'
+  homepage 'http://www.hairersoft.com/pro.html'
+  version '2.1.3'
+  sha1 'c46bd04b98e72850eaa48dd3c0957275433a8a03'
+  link 'Amadeus Pro.app'
+end
