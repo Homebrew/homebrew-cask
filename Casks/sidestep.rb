d9437629586c14f02038916851c7c48425f17392
Slawek Kolodziej <hfrntt@gmail.com>
2013-10-27 16:24:28 +0100

    Add cask for Sidestep 1.4.1

---
+++ Casks/sidestep.rb
@@ -0,0 +1,7 @@
+class Sidestep < Cask
+  url 'https://github.com/chetan51/sidestep/releases/download/1.4.1/Sidestep.zip'
+  homepage 'http://chetansurpur.com/projects/sidestep'
+  version '1.4.1'
+  sha1 '60f6c908a04e15d16c2e0e43b0694d84593ac43e'
+  link 'Sidestep.app'
+end
