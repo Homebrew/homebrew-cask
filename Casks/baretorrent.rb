c09c6329253855ea7b87aa7d956f1d8003695b52
Tadej Murovec <tadej.murovec@gmail.com>
2013-10-27 11:41:50 +0000

    Add cask for v0.4.3
    Simple torrent client.

---
+++ Casks/baretorrent.rb
@@ -0,0 +1,7 @@
+class Baretorrent < Cask
+  url 'https://launchpad.net/baretorrent/trunk/0.4.3/+download/baretorrent-0.4.3-osx-x64.dmg'
+  homepage 'http://baretorrent.org'
+  version '0.4.3'
+  sha1 '3f748c19a6b0ff535f7fa1fc9da542ebf5e86334'
+  link 'baretorrent.app'
+end
