e5c95f0b3976d8649a9a038a5bb15de856c31536
Olivier Parent <olivier.parent@arteveldehs.be>
2013-10-28 16:37:14 +0100

    Version bump to 6.0.7.1

--- Casks/mysql-workbench.rb
+++ Casks/mysql-workbench.rb
@@ -1,8 +1,7 @@
 class MysqlWorkbench < Cask
-  url 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-gpl-community-6.0.7.1-osx-i686.dmg'
+  url 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.0.7.1-osx-i686.dmg'
   homepage 'http://www.mysql.com/products/workbench'
   version '6.0.7.1'
-  md5 'd164b70e65c5cddfeb0168a355685ce7'
+  sha1 'no_checksum'
   link 'MySQLWorkbench.app'
 end
-Select
