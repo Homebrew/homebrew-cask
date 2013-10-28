2b1a41d31f529620a70b3f5a71672a28bf61f48e
Fernando Paredes <nano@fdp.io>
2013-10-28 12:45:16 -0700

    Update PS3 Media Server URL
    Update from non-conformative Sourceforge url to downloads.sourceforge.net

--- Casks/ps3-media-server.rb
+++ Casks/ps3-media-server.rb
@@ -1,5 +1,5 @@
 class Ps3MediaServer < Cask
-  url 'http://sourceforge.net/projects/ps3mediaserver/files/pms-1.90.1-setup-macosx.tar.gz'
+  url 'http://downloads.sourceforge.net/project/ps3mediaserver/pms-1.90.1-setup-macosx.tar.gz'
   homepage 'http://www.ps3mediaserver.org/'
   version '1.90.1'
   sha1 '2cba80b54b8c3bcd48676d6718c7eed5f6300a24'
