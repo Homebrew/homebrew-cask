989b4e09655818b8359e11b273073d93397756ca
Fernando Paredes <nano@fdp.io>
2013-10-28 13:23:03 -0700

    Update Evom
    Use latest scheme for Evom.

--- Casks/evom.rb
+++ Casks/evom.rb
@@ -1,7 +1,7 @@
 class Evom < Cask
   url 'http://files.thelittleappfactory.com/evom/Evom.zip'
   homepage 'http://thelittleappfactory.com/evom/'
-  version '1.0.1'
-  sha1 '1a576bc1317154fd2960165e06d51eb31ecd5862'
+  version 'latest'
+  no_checksum
   link 'Evom.app'
 end
