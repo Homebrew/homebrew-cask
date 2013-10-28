608c767e555f6971cee310f74e4b0dc8321267ec
Joseph Schmitt <me@joe.sh>
2013-10-28 09:55:18 -0700

    Simpholders 1.2.0
    Simpholders is a small utility for fast access to your iPhone Simulator
    apps. Opens folder in Finder, resets library and documents, and deletes
    the selected app.
    
    Signed-off-by: Fernando Paredes <nano@fdp.io>
    
    Closes #1332

---
+++ Casks/simpholders.rb
@@ -0,0 +1,7 @@
+class Simpholders < Cask
+  url 'http://simpholders.com/site/assets/files/1016/simpholders-1_2_0.dmg'
+  homepage 'http://simpholders.com'
+  version '1.2.0'
+  sha1 '07a573f89133195e2adcd150259c552aa869e544'
+  link 'Simpholders.app'
+end
