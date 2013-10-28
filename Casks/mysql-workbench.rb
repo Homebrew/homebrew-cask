43cf2aeaaac9de75821dfe41a9073829c0a714ca
Olivier Parent <olivier.parent@arteveldehs.be>
2013-10-28 16:52:12 +0100

    MySQL Workbench 6.0.7.1

--- Casks/mysql-workbench.rb
+++ Casks/mysql-workbench.rb
@@ -2,6 +2,6 @@ class MysqlWorkbench < Cask
   url 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.0.7.1-osx-i686.dmg'
   homepage 'http://www.mysql.com/products/workbench'
   version '6.0.7.1'
-  sha1 ''
+  sha1 '9989e1ece9459419b965eeb3912be0394b5570df'
   link 'MySQLWorkbench.app'
 end
