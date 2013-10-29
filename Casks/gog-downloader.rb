92059309b43c1781da4ac61826a240457450d9f3
Sascha Depold <sascha@depold.com>
2013-10-29 19:07:19 +0100

    gog-downloader

---
+++ Casks/gog-downloader.rb
@@ -0,0 +1,7 @@
+class GogDownloader < Cask
+  url 'http://static.gog.com/download/d3/mac-stable/GOG_Downloader_1.2_512.zip'
+  homepage 'http://www.gog.com/downloader'
+  version '1.2.512'
+  sha1 'ef9773c523fffbd5638b469ae4ade3817f67f0dd'
+  link 'GOG Downloader.app'
+end
