9126f63c96fa53eabdcd9c7c53af1d2c72ee5af3
Jim Smitley <jsmitley+github@gmail.com>
2013-10-29 10:24:45 -0500

    Added cask for Menuola

---
+++ Casks/menuola.rb
@@ -0,0 +1,8 @@
+class Menuola < Cask
+  url 'http://geocom.co.nz/downloads/Menuolav2.dmg.zip'
+  homepage 'http://www.geocom.co.nz'
+  version '2.0'
+  sha1 'e3b513a7a4102cfc59d29b699031b4be98edd12b'
+  nested_container 'Menuola.dmg'
+  link 'Menuola.app'
+end
