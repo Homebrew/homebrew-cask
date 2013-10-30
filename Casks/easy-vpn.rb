821817f7fca5312a3da45da5e981c4587bda85ff
Ben Spaulding <ben@spaulding.im>
2013-10-30 11:57:33 -0600

    Add easy-vpn cask for installing EasyVPN

---
+++ Casks/easy-vpn.rb
@@ -0,0 +1,7 @@
+class EasyVpn < Cask
+  url 'http://www.squashedsoftware.com/downloads/easyVPN_1.5.4.zip'
+  homepage 'http://www.squashedsoftware.com/products-easyvpn.php'
+  version '1.5.4'
+  sha1 '0a22682ead9414f66df1e0e4dc5164254df8b3ff'
+  link 'EasyVPN.app'
+end
