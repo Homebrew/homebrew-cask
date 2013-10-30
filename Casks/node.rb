8163f23adf4c50521a05c33d06f373e6338d06df
Jamie Hoover ⚔ <uipoet@users.noreply.github.com>
2013-10-29 18:34:26 -0700

    Node.js official package
    Installs Node.js from the official package as an alternative to the manual building that happens with vanilla homebrew.

---
+++ Casks/node.rb
@@ -0,0 +1,6 @@
+class Node < Cask
+  url 'http://nodejs.org/dist/v0.10.21/node-v0.10.21.pkg'
+  homepage 'http://nodejs.org'
+  version '0.10.21'
+  sha1 'de2bd0e858f99098ef24f99f972b8088c1f0405c'
+end
