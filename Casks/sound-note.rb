b5a06a608a1626ccbbb6cb68cf0449d11be6b399
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 11:38:11 -0500

    SoundNote latest

---
+++ Casks/sound-note.rb
@@ -0,0 +1,7 @@
+class SoundNote < Cask
+  url 'http://download.mrgeckosmedia.com/SoundNote.zip'
+  homepage 'https://mrgeckosmedia.com/applications/info/SoundNote'
+  version 'latest'
+  no_checksum
+  link 'SoundNote.app'
+end
