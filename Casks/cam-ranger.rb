69ed0a0d6c501cef6588ac509d91c389a92e345b
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 08:34:18 -0500

    CamRanger 1.3.1

---
+++ Casks/cam-ranger.rb
@@ -0,0 +1,7 @@
+class CamRanger < Cask
+  url 'http://www.camranger.com/downloadFiles/CamRanger_1_3_1.zip'
+  homepage 'http://www.camranger.com/downloads/#fbox_5594'
+  version '1.3.1'
+  sha1 '39ce7da4e08fa1778d3004c0507011c864c9f94d'
+  link '1.3.1/CamRanger.app'
+end
