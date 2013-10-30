cb76540d7d473c080ed8dc61b2c227b4bca63821
Ben Spaulding <ben@projectevolution.com>
2013-10-30 09:14:30 -0600

    Add bb-edit cask for installing BBEdit

---
+++ Casks/bb-edit.rb
@@ -0,0 +1,7 @@
+class BbEdit < Cask
+  url 'http://pine.barebones.com/files/BBEdit_10.5.5.dmg'
+  homepage 'http://www.barebones.com/products/bbedit/'
+  version '10.5.5'
+  sha1 'a181466e1044ba5360232722d2f610dd3015579a'
+  link 'BBEdit.app'
+end
