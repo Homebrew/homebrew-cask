37888cd2fa4aa4175a845c00fff07be3062e4aeb
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-30 20:17:42 +0800

    Add version and checksum for onyx

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -1,7 +1,7 @@
 class Onyx < Cask
   url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
   homepage 'http://www.titanium.free.fr/downloadonyx.php'
-  version 'latest'
-  no_checksum
+  version '2.7.4'
+  sha1 'dadd7f82fd972e73a7a8e360583b8d4d7963482c'
   link 'OnyX.app'
 end
