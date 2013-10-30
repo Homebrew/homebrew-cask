8c2968b1a07b32d5ae0e16f544a38ee9fea214e6
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-30 20:11:16 +0800

    Deeper v1.7.1 (Mountain Lion)
    Signed-off-by: Fernando Paredes <nano@fdp.io>
    
    Closes #1360

---
+++ Casks/deeper.rb
@@ -0,0 +1,7 @@
+class Deeper < Cask
+  url 'http://joel.barriere.pagesperso-orange.fr/dl/108/Deeper.dmg'
+  homepage 'http://www.titanium.free.fr/downloaddeeper.php'
+  version 'latest'
+  no_checksum
+  link 'Deeper.app'
+end
