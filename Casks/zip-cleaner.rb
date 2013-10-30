50adb35407b5d1652edb49f79db6b678e3175762
Ben Spaulding <ben@spaulding.im>
2013-10-30 13:23:51 -0600

    Use `latest` version scheme per @NanoXD’s feedback

--- Casks/zip-cleaner.rb
+++ Casks/zip-cleaner.rb
@@ -1,7 +1,7 @@
 class ZipCleaner < Cask
   url 'http://roger-jolly.nl/software/downloads/zipcleaner/ZipCleaner.zip'
   homepage 'http://roger-jolly.nl/software/#zipcleaner'
-  version '1.0'
-  sha1 'cd182d7464bbefec6695647258fd69d38e4a2211'
+  version 'latest'
+  no_checksum
   link 'ZipCleaner.app'
 end
