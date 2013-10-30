f0b2074318be6365f64b21f6218223a5e63e9014
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 14:20:06 -0500

    PixelPeeper latest

---
+++ Casks/pixel-peeper.rb
@@ -0,0 +1,7 @@
+class PixelPeeper < Cask
+  url 'http://www.irradiatedsoftware.com/download/PixelPeeper.zip'
+  homepage 'http://www.irradiatedsoftware.com/labs'
+  version 'latest'
+  no_checksum
+  link 'PixelPeeper.app'
+end
