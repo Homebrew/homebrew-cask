1a53164543f8f84b17e347c3a81c282113adaf75
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 14:29:32 -0500

    SlideMode 0.0.5

---
+++ Casks/slidemode.rb
@@ -0,0 +1,7 @@
+class Slidemode < Cask
+  url 'http://www.teaksoftware.com/workspace/files/slidemode005.dmg'
+  homepage 'http://teaksoftware.com/app/slidemode'
+  version '0.0.5'
+  sha1 'e9448c853e74b8103203be17478f5a5ae9f92d0c'
+  link 'SlideMode.app'
+end
