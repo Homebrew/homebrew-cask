debd3d9ddb8bc1b70fc868305e352fe60dfa62f2
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 12:01:52 -0500

    GrabBox 0.4.2

---
+++ Casks/grab-box.rb
@@ -0,0 +1,7 @@
+class GrabBox < Cask
+  url 'http://grabbox.devsoft.no/updates/GrabBox%20Beta%20v0.4.2.zip'
+  homepage 'http://grabbox.devsoft.no/'
+  version '0.4.2'
+  sha1 '1ac7c9b713b0bdb0d150b0f638e37f2a7c93ea09'
+  link 'GrabBox.app'
+end
