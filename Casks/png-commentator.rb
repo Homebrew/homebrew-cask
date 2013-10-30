e54ac1758efdc79f1e9204ca357afb57d7f84abc
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 13:56:47 -0500

    PNGCommentator 1.3.2

---
+++ Casks/png-commentator.rb
@@ -0,0 +1,7 @@
+class PngCommentator < Cask
+  url 'http://www.echomist.co.uk/software/downloads/PNGCommentator_1.3.2.dmg'
+  homepage 'http://www.echomist.co.uk/software/PNGCommentator.html'
+  version '1.3.2'
+  sha1 'd69215b12e684fbdfb3ec54b14a436705c89b085'
+  link 'PNGCommentator.app'
+end
