168bfeaeea93716a5669cb94f66bc87e86c040cb
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-30 20:32:09 +0800

    Add iTweaX v4.0.2 Note: this release is meant for Mountain Lion

---
+++ Casks/itweax.rb
@@ -0,0 +1,7 @@
+class Itweax < Cask
+  url 'http://www.itweax.net/itweax.dmg'
+  homepage 'http://www.itweax.net/'
+  version '4.0.2'
+  sha1 '4ec7f96655bb2307cf5567dd9cc483ddb136cc7d'
+  link 'iTweaX.app'
+end
