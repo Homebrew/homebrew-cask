4c39586c376d0619309e818c1009f6b2c62950f7
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 13:53:03 -0500

    PreferenceCleaner 1.5

---
+++ Casks/preference-cleaner.rb
@@ -0,0 +1,7 @@
+class PreferenceCleaner < Cask
+  url 'http://www.echomist.co.uk/software/downloads/PreferenceCleaner.dmg'
+  homepage 'http://www.echomist.co.uk/software/PreferenceCleaner.shtml'
+  version '1.5'
+  sha1 '33c6ed462d54b5920745a8acaec1389fadc54fc6'
+  link 'PreferenceCleaner.app'
+end
