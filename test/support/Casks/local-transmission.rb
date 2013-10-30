8345bfc3118f84114cf82e7be4f82e0b0645e145
Dave Williams <dave@dave.io>
2013-10-29 22:19:41 -0700

    Un-'fix' checksum, mirror install.rb for coverage, all tests passing.

--- test/support/Casks/local-transmission.rb
+++ test/support/Casks/local-transmission.rb
@@ -2,6 +2,6 @@ class LocalTransmission < TestCask
   url TestHelper.local_binary('transmission-2.61.dmg')
   homepage 'http://example.com/local-transmission'
   version '2.61'
-  sha1 '1f8ac10f23c5b5bc1167bda84b833e5c057a77d2'
+  sha1 'ac6cf624c3a4a3b1a692e95fa8de672b8be43391'
   link 'Transmission.app'
 end
