52bed9847eafdf0df3ce679b0fdeef3e96e0c3e1
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 13:41:20 -0500

    OmniDazzle 1.2

---
+++ Casks/omni-dazzle.rb
@@ -0,0 +1,7 @@
+class OmniDazzle < Cask
+  url 'http://downloads2.omnigroup.com/software/MacOSX/10.6/OmniDazzle-1.2.dmg'
+  homepage 'http://www.omnigroup.com/more'
+  version '1.2'
+  sha1 '8aa041f7b3737314ad4bdd418510ad548cbe6607'
+  link 'OmniDazzle.app'
+end
