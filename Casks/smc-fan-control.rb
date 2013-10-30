9659b494871bea28586b10c3111a572c80afac07
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 14:34:34 -0500

    smcFanControl 2.4

---
+++ Casks/smc-fan-control.rb
@@ -0,0 +1,7 @@
+class SmcFanControl < Cask
+  url 'http://www.eidac.de/smcfancontrol/smcfancontrol_2_4.zip'
+  homepage 'http://www.eidac.de/?p=243'
+  version '2.4'
+  sha1 '242c02d9c03a7eaa6446c82a97a588a25e3fd396'
+  link 'smcFanControl.app'
+end
