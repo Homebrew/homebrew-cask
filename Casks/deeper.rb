617b04bf8da805205ca62b92575d5ca754396500
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-30 20:11:16 +0800

    Add Deeper v1.7.1 (Mountain Lion)

---
+++ Casks/deeper.rb
@@ -0,0 +1,7 @@
+class Deeper < Cask
+  url 'http://joel.barriere.pagesperso-orange.fr/dl/108/Deeper.dmg'
+  homepage 'http://www.titanium.free.fr/downloaddeeper.php'
+  version '1.7.1'
+  sha1 '10011a13182b5bae339a50a00479c43485ddbd0e'
+  link 'Deeper.app'
+end
