2d71ad74ed4e92013a2f1577fc1d33861fcba30c
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 11:45:01 -0500

    Freezer latest

---
+++ Casks/freezer.rb
@@ -0,0 +1,7 @@
+class Freezer < Cask
+  url 'http://download.mrgeckosmedia.com/Freezer.zip'
+  homepage 'https://mrgeckosmedia.com/applications/info/Freezer'
+  version 'latest'
+  no_checksum
+  link 'Freezer/Freezer.app'
+end
