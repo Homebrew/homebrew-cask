95ceb8062a60bb03d1ccdc918f2090bf3760312a
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 12:30:00 -0500

    iCursor latest

---
+++ Casks/i-cursor.rb
@@ -0,0 +1,7 @@
+class ICursor < Cask
+  url 'https://www.macupdate.com/download/17059/iCursor.zip'
+  homepage 'https://www.macupdate.com/app/mac/17059/icursor'
+  version 'latest'
+  no_checksum
+  link 'iCursor.app'
+end
