b333b18a588b2a20acb30cc19612354e8a26782b
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 14:57:45 -0500

    WhiteClock 2.0

---
+++ Casks/white-clock.rb
@@ -0,0 +1,7 @@
+class WhiteClock < Cask
+  url 'http://www.taimila.com/downloads/WhiteClock2.zip'
+  homepage 'http://www.taimila.com/?p=1221'
+  version '2.0'
+  sha1 '2dbeccc4b2bd947ef067bf6fe1d1d7cf9350928f'
+  link 'WhiteClock.app'
+end
