dda529caa08cade96767f37b2743b879602bb345
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 15:09:51 -0500

    Wraparound 2.0b2

---
+++ Casks/wraparound.rb
@@ -0,0 +1,7 @@
+class Wraparound < Cask
+  url 'http://www.digicowsoftware.com/downloads/Wraparound2.0b2-2010.zip'
+  homepage 'http://www.digicowsoftware.com/detail?_app=Wraparound'
+  version '2.0b2'
+  sha1 '9a4b85f1e03cb44ec15383e7cb462d196730b86a'
+  link 'Wraparound.app'
+end
