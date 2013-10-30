25623cfba7f2392059349b6d6c6fbbba57b65ba4
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 09:13:10 -0500

    FFmpegX latest

---
+++ Casks/ffmpeg-x.rb
@@ -0,0 +1,7 @@
+class FfmpegX < Cask
+  url 'ftp://ftp2.ffmpegx.com/ffmpegx/ffmpegX.dmg'
+  homepage 'http://www.ffmpegx.com/download.html'
+  version 'latest'
+  no_checksum
+  link 'ffmpegX.app'
+end
