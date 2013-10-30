de3d8f7b9634e30c1f98e97e4391e986b9ddd738
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 11:51:21 -0500

    VoiceMac latest

---
+++ Casks/voice-mac.rb
@@ -0,0 +1,7 @@
+class VoiceMac < Cask
+  url 'http://download.mrgeckosmedia.com/VoiceMac.zip'
+  homepage 'https://mrgeckosmedia.com/applications/info/VoiceMac'
+  version 'latest'
+  no_checksum
+  link 'VoiceMac/VoiceMac.app'
+end
