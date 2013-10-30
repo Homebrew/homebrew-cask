7b122c6589269bdcd8a327c0bc2d883e5b5ac10d
Lee Hanxue <hanxue.developer@gmail.com>
2013-10-30 18:32:21 +0800

    Add Cocktail 7.0.1 (Mavericks)

---
+++ Casks/cocktail-mavericks.rb
@@ -0,0 +1,7 @@
+class CocktailMavericks < Cask
+  url 'http://usa.maintain.se/CocktailME.dmg'
+  homepage 'http://maintain.se/cocktail'
+  version '7.0.1'
+  sha1 'd882fe315762466720b68042445d8829e0c2f4ea'
+  link 'Cocktail.app'
+end
