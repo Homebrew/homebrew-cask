48535d9ed43f2e271efbf09da4f4458c40574113
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 12:18:26 -0500

    WaterRoof 3.8

---
+++ Casks/water-roof.rb
@@ -0,0 +1,8 @@
+class WaterRoof < Cask
+  url 'http://www.hanynet.com/waterroof-3.8.zip'
+  homepage 'http://www.hanynet.com/waterroof/index.html'
+  version '3.8'
+  sha1 '70c36773f51f257697e34b1a7b23d5a00be21761'
+  nested_container 'WaterRoof 3.8.dmg'
+  link 'WaterRoof.app'
+end
