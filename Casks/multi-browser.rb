58a05d92105502f2745f59ad06548b40c2752ed2
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 13:35:10 -0500

    MultiBrowser latest

---
+++ Casks/multi-browser.rb
@@ -0,0 +1,7 @@
+class MultiBrowser < Cask
+  url 'https://www.macupdate.com/download/33861/MultiBrowser.dmg'
+  homepage 'https://sites.google.com/site/tesseractsoftware/multibrowser'
+  version 'latest'
+  no_checksum
+  link 'MultiBrowser.app'
+end
