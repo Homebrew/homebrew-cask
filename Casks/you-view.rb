45b1ec84f22bf57f12425d562ccb6eff1e0ae10e
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 11:55:50 -0500

    YouView latest

---
+++ Casks/you-view.rb
@@ -0,0 +1,7 @@
+class YouView < Cask
+  url 'http://download.mrgeckosmedia.com/YouView.zip'
+  homepage 'https://mrgeckosmedia.com/applications/info/YouView'
+  version 'latest'
+  no_checksum
+  link 'YouView/YouView.app'
+end
