bf06be963da83653ecab1047190acf1a6dcbf8b5
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 11:02:19 -0500

    PasswordPractice latest

---
+++ Casks/password-practice.rb
@@ -0,0 +1,7 @@
+class PasswordPractice < Cask
+  url 'https://mrgeckosmedia.com/applications/download/PasswordPractice'
+  homepage 'https://mrgeckosmedia.com/applications/info/PasswordPractice'
+  version 'latest'
+  no_checksum
+  link 'Password Practice.app'
+end
