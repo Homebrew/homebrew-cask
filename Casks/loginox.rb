67a1c779cba8ea02c742809cf6806f8830c609a7
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 12:55:01 -0500

    Loginox 1.0.7b1

---
+++ Casks/loginox.rb
@@ -0,0 +1,7 @@
+class Loginox < Cask
+  url 'http://www.loginox.branox.com/DownloadFree/Loginox_107_b1.pkg'
+  homepage 'http://www.loginox.branox.com/'
+  version '1.0.7b1'
+  sha1 '073cecfd48616bcf093828381a57cf8a9e755080'
+  install 'Loginox_107_b1.pkg'
+end
