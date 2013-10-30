8a7b26d1b087d3f086d2b9875e7b74a0a2975ab4
Joel Kuzmarski <leoj3n@gmail.com>
2013-10-30 08:07:19 -0500

    AntiRSI 1.4njr4

---
+++ Casks/anti-rsi.rb
@@ -0,0 +1,7 @@
+class AntiRsi < Cask
+  url 'http://sabi.net/nriley/software/AntiRSI-1.4njr4.zip'
+  homepage 'http://sabi.net/nriley/software/#antirsi'
+  version '1.4njr4'
+  sha1 '36222614a84edb3bb785c192154d28989f3a9c93'
+  link 'AntiRSI.app'
+end
