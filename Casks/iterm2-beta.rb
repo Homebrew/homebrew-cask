e236b4e36b8074e8cc575470f5cfedc706a2bdae
Erwann Mest <erwann.mest@gmail.com>
2013-10-31 23:44:59 +0100

    Add iterm2 beta.

---
+++ Casks/iterm2-beta.rb
@@ -0,0 +1,7 @@
+class Iterm2Beta < Cask
+  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20130811.zip'
+  homepage 'http://www.iterm2.com/'
+  version 'latest'
+  no_checksum
+  link 'iTerm.app'
+end
