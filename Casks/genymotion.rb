b5c3bd8f7575aff3592297eea9815c7be41a5778
Michael Herb <exherb@4leaf.me>
2013-10-31 14:45:08 +0800

    genymotion 1.3.1
    the fastest Android emulator for app testing and presentation

---
+++ Casks/genymotion.rb
@@ -0,0 +1,7 @@
+class Genymotion < Cask
+  url 'https://ssl-files.genymotion.com/genymotion/genymotion-1.3.1/genymotion-1.3.1.dmg'
+  homepage 'http://www.genymotion.com/'
+  version '1.3.1'
+  sha1 '92a4f3b25acd6fc777d9cf42da3efdd2d4d589fa'
+  link 'Genymotion.app', 'Genymotion Shell.app'
+end
