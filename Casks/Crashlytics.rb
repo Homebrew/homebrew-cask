adf46d31c4a4c53ce8a6a2eec29a6bd7c73073ae
Martín Fernández <fmartin91@gmail.com>
2013-10-31 01:43:49 -0200

    Added no_checksum

--- Casks/Crashlytics.rb
+++ Casks/Crashlytics.rb
@@ -2,6 +2,6 @@ class Crashlytics < Cask
   url 'http://crashlytics.com/download/mac'
   homepage 'http://crashlytics.com'
   version 'latest'
-  sha1 '108ac5d547464a1f60871d7c03250ae55d206e10'
+  no_checksum
   link 'Crashlytics.app'
 end
