c1c9af9dc567129f235b4feea5b6b90638038daf
Rodolphe Stoclin <srodolphe@gmail.com>
2013-10-31 10:43:07 +0100

    Update Transmit

--- Casks/transmit.rb
+++ Casks/transmit.rb
@@ -1,7 +1,7 @@
 class Transmit < Cask
-  url 'http://www.panic.com/transmit/d/Transmit%204.4.3.zip'
+  url 'http://www.panic.com/transmit/d/Transmit%204.4.4.zip'
   homepage 'http://panic.com/transmit'
-  version '4.4.3'
-  sha1 'c66c2e0dcf7ebd7e5b354152f6d9cd57246835e4'
+  version '4.4.4'
+  sha1 '4e0d3b54c900eed493504ddd2e7748ba1d4d5ab3'
   link 'Transmit.app'
 end
