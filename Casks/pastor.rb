2bf15cb997a46e30dfdd08ede9e218fa978f6c4e
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-01 11:10:48 -0500

    Pastor 1.8.2

---
+++ Casks/pastor.rb
@@ -0,0 +1,7 @@
+class Pastor < Cask
+  url 'http://mehlau.net/pastor/Pastor182-signed.dmg'
+  homepage 'http://mehlau.net/pastor'
+  version '1.8.2'
+  sha1 '6a5df34ceb2266b5e61f49b39ac0a059c100ca37'
+  link 'Pastor.app'
+end
