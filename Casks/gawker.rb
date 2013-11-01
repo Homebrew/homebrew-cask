49b8903625bcd024b364f574481508fd41a0edc8
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-01 12:18:48 -0500

    Gawker latest
    closes #1436
    
    Signed-off-by: phinze <paul.t.hinze@gmail.com>

---
+++ Casks/gawker.rb
@@ -0,0 +1,7 @@
+class Gawker < Cask
+  url 'http://sourceforge.net/projects/gawker/files/latest/download'
+  homepage 'http://gawker.sourceforge.net/Gawker.html'
+  version 'latest'
+  no_checksum
+  link 'Gawker.app'
+end
