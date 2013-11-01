460f6f2c27a784f5c789c424bdfe5345da8983ea
Timothy Sutton <tim@synthist.net>
2013-11-01 15:55:23 -0400

    Processing 2.1

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -1,7 +1,7 @@
 class Processing < Cask
-  url 'http://download.processing.org/processing-2.0.3-macosx.zip'
+  url 'http://download.processing.org/processing-2.1-macosx.zip'
   homepage 'http://processing.org/'
-  version '2.0.3'
-  sha1 'f0126451da435f90b820a783c78bbde16739c5d2'
+  version '2.1'
+  sha1 '4e51f26c8bd0d13a28cf70f367d4966c396d6079'
   link 'Processing.app'
 end
