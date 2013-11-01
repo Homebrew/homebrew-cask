754be5cb8f0d871996c4bd123ca949bc3070c9f3
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-01 17:26:35 -0500

    Time Tracker for Mac 1.3.13

---
+++ Casks/time-tracker-mac.rb
@@ -0,0 +1,7 @@
+class TimeTrackerMac < Cask
+  url 'https://time-tracker-mac.googlecode.com/files/Time%20Tracker-1.3.13.zip'
+  homepage 'https://code.google.com/p/time-tracker-mac'
+  version '1.3.13'
+  sha1 '1d3fb3f824658856e18eb285d0ed92d81a1e9784'
+  link 'Time Tracker.app'
+end
