6c1f4db290860b2a3e80875b634576739cdd1680
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-01 11:51:30 -0500

    iPhoney 1.2

---
+++ Casks/iphoney.rb
@@ -0,0 +1,7 @@
+class Iphoney < Cask
+  url 'http://download.marketcircle.com/s3/iPhoney_1.2.zip'
+  homepage 'https://www.marketcircle.com/iphoney'
+  version '1.2'
+  sha1 '6804b11b4fe5a7d29b18f6d1cae194248e6903bf'
+  link 'iPhoney.app'
+end
