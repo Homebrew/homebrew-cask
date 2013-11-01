faf1d2a03ae1cce0485465bd730457e057f93f8b
Larry Boltovskoi <larrybolt@Larrys-MacBook-Pro.local>
2013-11-02 00:27:44 +0100

    Added Chameleon SSD optimizer, a free tool to enable trim for SSDs and a few other settings

---
+++ Casks/chameleon-ssd-optimizer.rb
@@ -0,0 +1,7 @@
+class ChameleonSsdOptimizer < Cask
+  url 'http://chameleon.alessandroboschini.it/download.php'
+  homepage 'http://chameleon.alessandroboschini.it/'
+  version '0.9.4'
+  sha1 'c137ee0206a59512cd22c1bb56b40d83eef568b1'
+  link 'Chameleon SSD Optimizer.app'
+end
