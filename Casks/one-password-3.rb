d5f14937d713c26ad14eb7865cf9610f642b3d1f
Lincoln Lee <lincolnlee@Lincolns-MacBook-Air.local>
2013-11-01 18:47:16 +0800

    Add cask for 1Password 3
    Create a separate cask from one-password history for version 3

---
+++ Casks/one-password-3.rb
@@ -0,0 +1,7 @@
+class OnePassword3 < Cask
+  url 'https://d13itkw33a7sus.cloudfront.net/dist/1P/mac/1Password-3.8.21.zip'
+  homepage 'https://agilebits.com/onepassword'
+  version '3.8.21'
+  sha1 '5ead1fcfeaca615c8b7ed3ca5c5ff681edb9ef56'
+  link '1Password.app'
+end
