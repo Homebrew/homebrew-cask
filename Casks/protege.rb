0465e26ae1717f2a1eacdd3a5f005c311269bc18
Nolan Nichols <nolan.nichols@gmail.com>
2013-11-01 15:06:28 -0700

    add: protege 4.3 ontology editor

---
+++ Casks/protege.rb
@@ -0,0 +1,7 @@
+class Protege < Cask
+  url 'http://protege.stanford.edu/download/protege/4.3/osx/protege-4.3.0-304.zip'
+  homepage 'http://protege.stanford.edu/'
+  version '4.3.0'
+  sha1 '58cfdd6d0ce40564b4c6deb93f72535fa0746f67'
+  link 'protege-4.3.app'
+end
