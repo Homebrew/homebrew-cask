2588fb5d4a1c61bb02905990e36f352be5cb8249
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-01 18:10:31 -0500

    CLIX latest

---
+++ Casks/clix.rb
@@ -0,0 +1,7 @@
+class Clix < Cask
+  url 'https://www.macupdate.com/download/13983/CLIX.zip'
+  homepage 'http://rixstep.com/4/0/clix/index.shtml'
+  version 'latest'
+  no_checksum
+  link 'CLIX/CLIX-64/CLIX.app'
+end
