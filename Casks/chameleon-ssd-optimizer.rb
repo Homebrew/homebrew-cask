93d01e866ee923797dd027fa6b8b6fa013e7c16d
Larry Boltovskoi <larry.bolt@gmail.com>
2013-11-02 00:45:33 +0100

    fixed dumb copy/paste mistake

--- Casks/chameleon-ssd-optimizer.rb
+++ Casks/chameleon-ssd-optimizer.rb
@@ -2,6 +2,6 @@ class ChameleonSsdOptimizer < Cask
   url 'http://chameleon.alessandroboschini.it/download.php'
   homepage 'http://chameleon.alessandroboschini.it/'
   version 'latest'
-  no_checkum
+  no_checksum
   link 'Chameleon SSD Optimizer.app'
 end
