4cc77248faa9441436d06b1e79626baabce9ba79
Fernando Paredes <nano@fdp.io>
2013-11-01 08:01:47 -0700

    Update OmniDiskSweeper to use latest scheme

--- Casks/omni-disk-sweeper.rb
+++ Casks/omni-disk-sweeper.rb
@@ -1,7 +1,7 @@
 class OmniDiskSweeper < Cask
   url 'http://www.omnigroup.com/download/latest/omnidisksweeper'
   homepage 'http://www.omnigroup.com/products/omnidisksweeper/'
-  version '1.8'
-  sha1 '8061c4d7ee18549f976119146f28e67180141069'
+  version 'latest'
+  no_checksum
   link 'OmniDiskSweeper.app'
 end
