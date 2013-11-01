d6e398201e760f33bd3fe1842435fbdc62630420
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-01 17:48:22 -0500

    Port Map 1.3.1-r46

---
+++ Casks/port-map.rb
@@ -0,0 +1,7 @@
+class PortMap < Cask
+  url 'http://tcmportmapper.googlecode.com/files/PortMap-1.3.1-r46.zip'
+  homepage 'http://www.codingmonkeys.de/portmap'
+  version '1.3.1-r46'
+  sha1 'bc294028f665a2e42cc32a6dc69493fd34542d8c'
+  link 'Port Map.app'
+end
