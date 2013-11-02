b4a3f4fe0b47840ab377fad48141411922fc1ed7
Vítor Galvão <info@vitorgalvao.com>
2013-11-02 16:21:55 +0000

    added jumpshare

---
+++ Casks/jumpshare.rb
@@ -0,0 +1,7 @@
+class Jumpshare < Cask
+  url 'https://jumpshare.com/desktop/mac/Jumpshare_1.0.19.dmg'
+  homepage 'https://jumpshare.com/'
+  version '1.0.19'
+  sha1 'a015bafd6e8d8cbbdab3def3e86256d38489ee76'
+  link 'Jumpshare.app'
+end
