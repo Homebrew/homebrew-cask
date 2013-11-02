74fb85a43c06fcbc1cfc73239085fb8ee1b0c5c3
Michael Ford <fanquake@users.noreply.github.com>
2013-11-02 12:35:05 +0800

    dbeaver 2.3.1

--- Casks/dbeaver.rb
+++ Casks/dbeaver.rb
@@ -1,7 +1,7 @@
 class Dbeaver < Cask
-  url 'http://dbeaver.jkiss.org/files/dbeaver-2.3.0-macosx.cocoa.x86_64.zip'
+  url 'http://dbeaver.jkiss.org/files/dbeaver-2.3.1-macosx.cocoa.x86_64.zip'
   homepage 'http://dbeaver.jkiss.org/'
-  version '2.3.0'
-  sha1 '16d5973d7c9ce093964468ffee93a66672ef1e28'
+  version '2.3.1'
+  sha1 'b62d59cc42ed566fc1e2c7c3049226ef327e08a0'
   link 'dbeaver.app'
 end
