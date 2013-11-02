26982e7bab4f0ee88a4173552bfabcc2ba32fd2e
Alexander Goldstein <alexg@alexland.org>
2013-11-01 23:28:18 -0500

    LogMeIn Ignition

---
+++ Casks/logmein-ignition-beta.rb
@@ -0,0 +1,7 @@
+class LogmeinIgnitionBeta < Cask
+  url 'https://secure.logmein.com/welcome/labs/LogMeInIgnition.dmg'
+  homepage 'https://secure.logmein.com/LABS/#IgnitionforMac'
+  version 'latest'
+  no_checksum
+  link 'LogMeIn Ignition.app'
+end
