52601b9a0a4f03fc3d5875dc9e9235d6855f16b7
Roy Xiang <roy.x.work@gmail.com>
2013-11-02 16:52:29 +0800

    virtualbox 4.3.2-90405
    updated virtualbox from 4.3.0-89960 to 4.3.2-90405

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,8 +1,8 @@
 class Virtualbox < Cask
-  url 'http://download.virtualbox.org/virtualbox/4.3.0/VirtualBox-4.3.0-89960-OSX.dmg'
+  url 'http://download.virtualbox.org/virtualbox/4.3.2/VirtualBox-4.3.2-90405-OSX.dmg'
   homepage 'http://www.virtualbox.org'
-  version '4.3.0-89960'
-  sha1 '3cc3b714775c971a09478cdd5419c6eb6fdaebb0'
+  version '4.3.2-90405'
+  sha1 '0c5816dada6a106a1eae4bde834f3e1863d2611d'
   install 'VirtualBox.pkg'
   uninstall :script => 'VirtualBox_Uninstall.tool', :args => %w[--unattended]
 end
