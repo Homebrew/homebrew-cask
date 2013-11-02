f0d2eb32b94ee81e5298be907d7d0457beb8962d
Michael Ford <fanquake@users.noreply.github.com>
2013-11-02 11:48:54 +0800

    Asepsis 1.3.2

--- Casks/asepsis.rb
+++ Casks/asepsis.rb
@@ -1,8 +1,8 @@
 class Asepsis < Cask
-  url 'http://downloads.binaryage.com/Asepsis-1.3.1.dmg'
+  url 'http://downloads.binaryage.com/Asepsis-1.3.2.dmg'
   homepage 'http://asepsis.binaryage.com/'
-  version '1.3.1'
-  sha1 '8570b24e025b701ce7144a92cdfdbf629657c395'
+  version '1.3.2'
+  sha1 'fbb97a5b91c3020e5bdb1a5088afa912512f02b0'
   install 'Asepsis.mpkg'
   uninstall :pkgutil => 'com.binaryage.pkg.asepsis'
 end
