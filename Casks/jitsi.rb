218632eb04053e159bf84b1e1e97d9050887e8e0
Alexander Goldstein <alexg@alexland.org>
2013-11-01 23:40:12 -0500

    Jitsi (SIP Comminicator)

---
+++ Casks/jitsi.rb
@@ -0,0 +1,7 @@
+class Jitsi < Cask
+  url 'https://download.jitsi.org/jitsi/macosx/jitsi-2.2-latest.dmg'
+  homepage 'https://jitsi.org/'
+  version '2.2'
+  sha1 '6cb04097b0ce9f1cabc3e04bd4203f4269f7c8b9'
+  link 'Jitsi.app'
+end
