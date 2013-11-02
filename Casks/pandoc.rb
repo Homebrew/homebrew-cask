074f44317fd95132189e53ba1f54594f29232031
Michael Ford <fanquake@users.noreply.github.com>
2013-11-02 12:15:54 +0800

    Pandoc 1.12.1-1

--- Casks/pandoc.rb
+++ Casks/pandoc.rb
@@ -1,7 +1,7 @@
 class Pandoc < Cask
   homepage 'http://johnmacfarlane.net/pandoc'
-  url "https://pandoc.googlecode.com/files/pandoc-1.12.0.2.dmg"
+  url "https://pandoc.googlecode.com/files/pandoc-1.12.1-1.dmg"
   sha1 '428ab4e784aa91b30d0bf26a49a67d688ea5fe42'
-  version '1.12.0.2'
-  install 'pandoc-1.12.0.2.pkg'
+  version '1.12.1-1'
+  install 'pandoc-1.12.1.pkg'
 end
