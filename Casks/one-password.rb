177826dc0d9867171c0e1be8eb87754eb94b0aa3
Michael Ford <fanquake@users.noreply.github.com>
2013-11-02 23:08:04 +0800

    1Password 4.0.5

--- Casks/one-password.rb
+++ Casks/one-password.rb
@@ -1,7 +1,7 @@
 class OnePassword < Cask
-  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.0.2.zip'
+  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.0.5.zip'
   homepage 'https://agilebits.com/onepassword'
-  version '4.0.2'
-  sha1 '48646cbb0d17c7e69338096bb29a4aa437ea8488'
+  version '4.0.5'
+  sha1 'a9f6feb4808d5138e02f408ba400678f22b2ad8b'
   link '1Password 4.app'
 end
