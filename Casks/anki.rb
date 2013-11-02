f6e69fc0ab56bf63006763f9a87343d6406df672
Michael Ford <fanquake@users.noreply.github.com>
2013-11-02 11:40:34 +0800

    Akni 2.0.16

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,7 +1,7 @@
 class Anki < Cask
-  url 'http://ankisrs.net/download/mirror/anki-2.0.13.dmg'
+  url 'http://ankisrs.net/download/mirror/anki-2.0.16.dmg'
   homepage 'http://ankisrs.net/'
-  version '2.0.13'
-  sha1 '40a05ee6c44a6a12e354514e2b424a68eab5969f'
+  version '2.0.16'
+  sha1 'c4f046f6c6bd10741054f5e469d36b22d2def858'
   link 'Anki.app'
 end
