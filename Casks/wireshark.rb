abec0d58dfa6e04f8d84cb529b2ef0d81ecd2a5c
Michael Ford <fanquake@users.noreply.github.com>
2013-11-03 10:22:33 +0800

    Wireshark 1.10.3

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -1,10 +1,10 @@
 class Wireshark < Cask
-  url 'http://wiresharkdownloads.riverbed.com/wireshark/osx/Wireshark%201.10.2%20Intel%2064.dmg'
+  url 'http://wiresharkdownloads.riverbed.com/wireshark/osx/Wireshark%201.10.3%20Intel%2064.dmg'
   homepage 'http://www.wireshark.org'
-  version '1.10.2'
-  sha1 'f3af6443f7c25c27824014fd88e8cb1460ecbb08'
+  version '1.10.3'
+  sha1 '5fa924b3c8c3e5bd80662b95047cea51b12ec0e8'
   link :none
-  install 'Wireshark 1.10.2 Intel 64.pkg'
+  install 'Wireshark 1.10.3 Intel 64.pkg'
   uninstall :pkgutil => 'org.wireshark.*', 
             :files => %w(capinfos dftest dumpcap editcap mergecap randpkt
                          rawshark text2pcap tshark 
