2adad6280bc2dd2c9e3b624ee5c891031b6df8d6
Michael Ford <fanquake@users.noreply.github.com>
2013-11-03 20:10:09 +0800

    Keka 1.0.4

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -1,7 +1,7 @@
 class Keka < Cask
-  url 'http://www.kekaosx.com/release/Keka-1.0.3-intel.dmg'
+  url 'http://www.kekaosx.com/release/Keka-1.0.4-intel.dmg'
   homepage 'http://kekaosx.com/'
-  version '1.0.3'
-  sha1 '80ef6d5cd65a8c5e8ec8e29478ee1013b79a9aa0'
+  version '1.0.4'
+  sha1 'dad32add9d4c040006df71d769c9694664b749a5'
   link 'Keka.app'
 end
