dc9ab9d21db563594bdbca29fdb2e47317ab8409
phinze <paul.t.hinze@gmail.com>
2013-11-03 12:23:08 -0600

    brew cask list, when given args, list installed files
    this mimics (and indeed steals code from) homebrew's behavior
    
    refs #1417

--- lib/cask/cli/list.rb
+++ lib/cask/cli/list.rb
@@ -1,9 +1,32 @@
 class Cask::CLI::List
   def self.run(*arguments)
+    if arguments.any?
+      list_files(*arguments)
+    else
+      list_installed
+    end
+  end
+
+  def self.list_files(*cask_names)
+    cask_names.each do |cask_name|
+      begin
+        cask = Cask.load(cask_name)
+        if cask.installed?
+          Cask::PrettyListing.new(cask).print
+        else
+          opoo "#{cask} is not installed"
+        end
+      rescue CaskError => e
+        onoe e
+      end
+    end
+  end
+
+  def self.list_installed
     puts_columns Cask::CLI.nice_listing(Cask.installed)
   end
 
   def self.help
-    "lists installed casks"
+    "with no args, lists installed casks; given installed casks, lists installed files"
   end
 end
