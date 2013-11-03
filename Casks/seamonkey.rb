256d6527f685a75e766cf982a964541819cdb175
Michael Ford <fanquake@users.noreply.github.com>
2013-11-03 20:06:47 +0800

    SeaMonkey 2.22

--- Casks/seamonkey.rb
+++ Casks/seamonkey.rb
@@ -1,7 +1,7 @@
 class Seamonkey < Cask
-  url 'https://download.mozilla.org/?product=seamonkey-2.21&os=osx&lang=en-US'
+  url 'https://download.mozilla.org/?product=seamonkey-2.22&os=osx&lang=en-US'
   homepage 'http://www.seamonkey-project.org/'
-  version '2.21'
-  sha1 '471b4db4cf88772f73ce9148ad9f3229cb7b120b'
+  version '2.22'
+  sha1 'a4fc6f6180a8c52c75e8730e44de08a8b52d22db'
   link 'SeaMonkey.app'
 end
