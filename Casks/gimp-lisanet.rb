0f2aa57ec12ead38d464cb64d13e7c8a0cf6d1fc
Leonard van Driel <leonardvandriel@gmail.com>
2013-11-03 11:45:31 +0100

    add cask for gimp lisanet

---
+++ Casks/gimp-lisanet.rb
@@ -0,0 +1,7 @@
+class GimpLisanet < Cask
+  url 'http://downloads.sourceforge.net/gimponosx/Gimp-2.8.6p1-MountainLion.dmg'
+  homepage 'http://gimp.lisanet.de'
+  version '2.8.6'
+  sha1 '0b93fc41a5c7705c403453452b91c6c034829d4f'
+  link 'GIMP.app'
+end
