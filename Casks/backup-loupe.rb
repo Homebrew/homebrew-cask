cc9c9a7f0346efd54d90e28f0e2b55eee63285d6
Michael Ford <fanquake@users.noreply.github.com>
2013-11-04 18:48:45 +0800

    Backup Loupe 2.4.2

--- Casks/backup-loupe.rb
+++ Casks/backup-loupe.rb
@@ -1,7 +1,7 @@
 class BackupLoupe < Cask
-  url 'http://www.soma-zone.com/download/files/BackupLoupe_2.4.1.tar.bz2'
+  url 'http://www.soma-zone.com/download/files/BackupLoupe_2.4.2.tar.bz2'
   homepage 'http://www.soma-zone.com/BackupLoupe/'
-  version '2.4.1'
-  sha1 '833684f01d66a6be91571b5300eabbe42082101a'
+  version '2.4.2'
+  sha1 'fb2321b585dbe8827e437ab715186e1cf49c1cd5'
   link 'BackupLoupe.app'
 end
