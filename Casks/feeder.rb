485fe72cd7a1d09e7226482650c9ef10df61aa7b
Michael Ford <fanquake@users.noreply.github.com>
2013-11-04 11:22:11 +0800

    Feeder 2.4.3

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -1,7 +1,7 @@
 class Feeder < Cask
-  url 'http://reinventedsoftware.com/feeder/downloads/Feeder_2.4.1.dmg'
+  url 'http://reinventedsoftware.com/feeder/downloads/Feeder_2.4.3.dmg'
   homepage 'http://reinventedsoftware.com/feeder/'
-  version '2.4.1'
-  sha1 'cc7df00b98c8827e55d54174ce37024b5ddd2c6c'
+  version '2.4.3'
+  sha1 'b4fad55676b5eac59e109d10ef34392f4b79bff4'
   link 'Feeder.app'
 end
