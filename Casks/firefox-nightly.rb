a444ae81442a6b83abf6c5b088051f7f2553b5e1
Fernando Paredes <nano@fdp.io>
2013-11-03 19:16:23 -0800

    Firefox Nightly
    Credit: @apermanentwreck
    Closes: #1471 #1144

---
+++ Casks/firefox-nightly.rb
@@ -0,0 +1,7 @@
+class FirefoxNightly < Cask
+  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-28.0a1.en-US.mac.dmg'
+  homepage 'http://nightly.mozilla.org'
+  version '28.0-alpha1'
+  sha1 'no_checksum'
+  link 'FirefoxNightly.app'
+end
