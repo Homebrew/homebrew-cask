8103b7afb74a816e0c03da2f33d013d681769977
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-03 22:17:32 -0600

    ThumbsUp 4.5

---
+++ Casks/thumbs-up.rb
@@ -0,0 +1,8 @@
+class ThumbsUp < Cask
+  url 'https://s3.amazonaws.com/DTWebsiteSupport/download/freeware/thumbsup/4.5/ThumbsUp.dmg.zip'
+  homepage 'http://www.devontechnologies.com/products/freeware.html#c966'
+  version '4.5'
+  sha1 '335334d56c98741a864ca7a2ffb9dc85a585bb97'
+  nested_container 'ThumbsUp.dmg'
+  link 'ThumbsUp.app'
+end
