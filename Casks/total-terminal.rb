b4d58268828bf826a7d8d4045961a5c2508bbd04
Jim Smitley <jsmitley+github@gmail.com>
2013-11-04 12:31:14 -0600

    Upgraded TotalTerminal to 1.4.5

--- Casks/total-terminal.rb
+++ Casks/total-terminal.rb
@@ -1,8 +1,8 @@
 class TotalTerminal < Cask
-  url 'http://downloads.binaryage.com/TotalTerminal-1.4.4.dmg'
+  url 'http://downloads.binaryage.com/TotalTerminal-1.4.5.dmg'
   homepage 'http://totalterminal.binaryage.com'
-  version '1.4.4'
-  sha1 '5cc8286b598ab4c85eff0256671c2ff6d98a0262'
+  version '1.4.5'
+  sha1 '0a0bd7dbe0cac44b69aa408ab21645885317405e'
   install 'TotalTerminal.pkg'
   uninstall :script => 'TotalTerminal Uninstaller.app/Contents/MacOS/TotalTerminal Uninstaller',
             :args => %w[--headless]
