66c660ae61eb75c9477eef15bb4b7dc855762b75
Fernando Paredes <nano@fdp.io>
2013-11-03 21:58:38 -0800

    MacVim Snapshot-72
    Closes #1455

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,7 +1,7 @@
 class Macvim < Cask
-  url 'https://github.com/eee19/macvim/releases/download/snapshot-71/MacVim-snapshot-71-Mountain-Lion.tbz'
+  url 'https://github.com/eee19/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Mountain-Lion.tbz'
   homepage 'http://code.google.com/p/macvim/'
-  version '7.4-71'
-  sha1 '495a29e92769034e25779a66ee7fb120ada08792'
-  link 'MacVim-snapshot-71/MacVim.app'
+  version '7.4-72'
+  sha1 'bc3b899634d73908ddba5afd9b9a74778988aec3'
+  link 'MacVim-snapshot-72/MacVim.app'
 end
