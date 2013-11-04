8874d73f2915d4a7eb0d62fc66e4b56926b5205a
Eric Phetteplace <phette23@gmail.com>
2013-11-03 20:44:16 -0500

    Update firefox-aurora.rb to point to latest download link
    The link ending in "firefox-26.0a2.en-US.mac.dmg" is 404ing, should be "firefox-27"

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,7 +1,7 @@
 class FirefoxAurora < Cask
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-26.0a2.en-US.mac.dmg'
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-27.0a2.en-US.mac.dmg'
   homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
-  version '26.0a2'
+  version '27.0a2'
   no_checksum
   link 'FirefoxAurora.app'
 end
