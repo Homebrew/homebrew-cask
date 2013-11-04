a18a959a026a1c272d0ce77ea9e7d9c82ea73b85
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-04 15:45:32 -0600

    Phoenix Slides 1.2.8

---
+++ Casks/phoenix-slides.rb
@@ -0,0 +1,7 @@
+class PhoenixSlides < Cask
+  url 'http://blyt.net/phxslides/phoenix-slides-128.zip'
+  homepage 'http://blyt.net/phxslides'
+  version '1.2.8'
+  sha1 'd8ae98648ac043820ceb4ec19089ed5e2cb7337c'
+  link 'Phoenix Slides.app'
+end
