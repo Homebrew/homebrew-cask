4cdc06afb8d72ee79cb7cc700c9f339311c20f88
Michael Herb <exherb@4leaf.me>
2013-11-05 09:53:47 +0800

    added google chrome dev channel
    more info http://www.google.com/chrome/intl/en/eula_dev.html?dl=mac

---
+++ Casks/google-chrome-dev.rb
@@ -0,0 +1,7 @@
+class GoogleChromeDev < Cask
+  url 'http://dl.google.com/chrome/mac/dev/googlechrome.dmg'
+  homepage 'https://www.google.com/chrome/'
+  version 'dev-channel'
+  no_checksum
+  link 'Google Chrome.app'
+end
