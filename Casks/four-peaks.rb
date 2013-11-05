4ce75ce20c67588de1b3c8b4e3882f00514cb93b
Ron Rock <rrock@uchicago.edu>
2013-11-05 17:16:48 -0600

    4Peaks.app version 1.7
    A program to read DNA sequencing chromatograms

---
+++ Casks/four-peaks.rb
@@ -0,0 +1,7 @@
+class FourPeaks < Cask
+  url 'http://nucleobytes.com/4peaks/download'
+  homepage 'http://nucleobytes.com/index.php/4peaks'
+  version '1.7'
+  sha1 'f682087ad1f4ef2c503239d1e9a1143d801cf9f9'
+  link '4Peaks.app'
+end
