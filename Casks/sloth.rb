4c5a373104228cb0ca08fbb8178a68fd22dec80b
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-05 16:51:16 -0600

    Sloth 1.5

---
+++ Casks/sloth.rb
@@ -0,0 +1,7 @@
+class Sloth < Cask
+  url 'http://sveinbjorn.org/files/software/sloth.zip'
+  homepage 'http://sveinbjorn.org/sloth'
+  version '1.5'
+  sha1 'd11213dc1ad49556539f2f5173c7b6cd8feafad3'
+  link 'Sloth-1.5/Sloth.app'
+end
