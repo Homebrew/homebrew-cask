9d03ea9dc5dbce62e91c649405803bca4e881592
Joshua Guy <jglenes@gmail.com>
2013-11-04 19:56:04 -0500

    Adding Cask for RStudio Preview v0.98.456
    RStudio Preview includes new debugging tools and
    support for authoring R Presentations. Installing
    this cask will overwrite the symlink for the 
    RStudio cask, if it is installed.

---
+++ Casks/rstudio-preview.rb
@@ -0,0 +1,7 @@
+class RstudioPreview < Cask
+  url 'https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-0.98.456.dmg'
+  homepage 'http://www.rstudio.com/ide/download/preview'
+  version '0.98.456'
+  sha1 '2b07e15aeff833d67970b86b66f656824cdf2811'
+  link 'RStudio.app'
+end
