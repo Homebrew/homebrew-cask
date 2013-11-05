54c4782609ec35c1f772318abcdc1934d87cb103
Lincoln Lee <git@lincoln.hk>
2013-11-05 21:48:49 +0800

    Add cask for Battery Time Remaining

---
+++ Casks/battery-time-remaining.rb
@@ -0,0 +1,7 @@
+class BatteryTimeRemaining < Cask
+  url 'http://yap.nu/battery-time-remaining/download/Battery%20Time%20Remaining%202-2.0.2.zip'
+  homepage 'http://yap.nu/battery-time-remaining/'
+  version '2.0.2'
+  sha1 '351b0dd2c1251681cc5a0fcf8c0af784393e98d3'
+  link 'Battery Time Remaining 2.app'
+end
