b8bf7e9fcf34ecda0913c8a130e1dc6780074a20
Chulki Lee <chulki@asperasoft.com>
2013-11-05 20:17:02 -0800

    silverlight 5.1.20913.0

--- Casks/silverlight.rb
+++ Casks/silverlight.rb
@@ -1,7 +1,7 @@
 class Silverlight < Cask
-  url 'http://silverlight.dlservice.microsoft.com/download/B/3/C/B3CF6815-40B1-4E36-8746-C4A0381AD260/20513.00/runtime/Silverlight.dmg'
+  url 'http://silverlight.dlservice.microsoft.com/download/6/B/8/6B885B0B-73D4-4BCE-BFFF-154B4A402D22/20913.00/Silverlight.dmg'
   homepage 'http://www.microsoft.com/silverlight/'
-  version '5.1.20513.0'
-  sha1 'd2b8d3a47e1c81c7f2acac5f05dd6743fa2585cf'
+  version '5.1.20913.0'
+  sha1 '87181a49ec802e8eaa00bdd0b8e7dbb8c9201812'
   install 'Silverlight.pkg'
 end
