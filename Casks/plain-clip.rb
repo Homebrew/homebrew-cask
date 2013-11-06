a8a1102bf04bb7de4a36540166ae21e9c40f3810
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-06 13:07:50 -0600

    Plain Clip latest

---
+++ Casks/plain-clip.rb
@@ -0,0 +1,7 @@
+class PlainClip < Cask
+  url 'http://www.bluem.net/files/Plain-Clip.dmg'
+  homepage 'http://www.bluem.net/en/mac/plain-clip'
+  version 'latest'
+  no_checksum
+  link 'Plain Clip.app'
+end
