f37544e98b402924b2f8b48ce16fc22976a2b150
Chulki Lee <chulki@asperasoft.com>
2013-11-05 16:20:52 -0800

    pgadmin3 1.18.1

--- Casks/pgadmin3.rb
+++ Casks/pgadmin3.rb
@@ -1,7 +1,7 @@
 class Pgadmin3 < Cask
-  url 'http://ftp.postgresql.org/pub/pgadmin3/release/v1.18.0/osx/pgadmin3-1.18.0.dmg'
+  url 'http://ftp.postgresql.org/pub/pgadmin3/release/v1.18.1/osx/pgadmin3-1.18.1.dmg'
   homepage 'http://pgadmin.org'
-  version '1.18.0'
-  sha1 '6161e63dbed424078fef337ab1bfc2f1db9b6375'
+  version '1.18.1'
+  sha1 'b05c374bb3821847db5a2bd1ce32e98794d119c8'
   link 'pgAdmin3.app'
 end
