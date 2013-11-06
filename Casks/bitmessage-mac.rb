e0b53f1393f4f6c973a0e68426ad398378d99fbc
ryanrankin <hey@ryanrankin.me>
2013-11-06 14:16:29 -0600

    Updated broken url

--- Casks/bitmessage-mac.rb
+++ Casks/bitmessage-mac.rb
@@ -1,5 +1,5 @@
 class BitmessageMac < Cask
-  url 'https://bitmessage.org/download/osx/bitmessage-v0.4.0.dmg'
+  url 'https://bitmessage.org/download/osx/Bitmessage.dmg'
   homepage 'https://bitmessage.org/'
   version '0.4.0'
   sha1 '44cb0b818030bcddb577c773a053f80efcf199cd'
