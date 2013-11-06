ac0ef742ce642e3f1890594fd6a1198638d303a6
Fernando Paredes <nano@fdp.io>
2013-11-05 19:56:42 -0800

    Move tunnelblick to homebrew-versions

--- Casks/tunnelblick-beta.rb
+++
@@ -1,7 +0,0 @@
-class TunnelblickBeta < Cask
-  url 'http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_3.4beta14.dmg'
-  homepage 'https://code.google.com/p/tunnelblick/'
-  version '3.4beta14'
-  sha1 'de32db3d94bf0a3b77247892643cbeb984e2106f'
-  link 'Tunnelblick.app'
-end
