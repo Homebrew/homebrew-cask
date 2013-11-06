036ef14b7487c5be725f287b1886324458f42397
ryanrankin <hey@ryanrankin.me>
2013-11-06 14:16:29 -0600

    Bitmessage latest
    File was renamed to remove the redundant Mac designation
    
    Signed-off-by: Fernando Paredes <nano@fdp.io>
    
    Closes #1517

--- Casks/bitmessage-mac.rb
+++
@@ -1,7 +0,0 @@
-class BitmessageMac < Cask
-  url 'https://bitmessage.org/download/osx/bitmessage-v0.4.0.dmg'
-  homepage 'https://bitmessage.org/'
-  version '0.4.0'
-  sha1 '44cb0b818030bcddb577c773a053f80efcf199cd'
-  link 'Bitmessage.app'
-end
---
+++ Casks/bitmessage.rb
@@ -0,0 +1,7 @@
+class Bitmessage < Cask
+  url 'https://bitmessage.org/download/osx/Bitmessage.dmg'
+  homepage 'https://bitmessage.org/'
+  version 'latest'
+  no_checksum
+  link 'Bitmessage.app'
+end
