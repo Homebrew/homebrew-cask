b4fcf7d051cad91bef42b84fd0f6a1775478566b
Fernando Paredes <nano@fdp.io>
2013-11-05 19:54:54 -0800

    Move Google Chrome variants to caskroom/homebrew-versions

--- Casks/google-chrome-beta.rb
+++
@@ -1,7 +0,0 @@
-class GoogleChromeBeta < Cask
-  url 'http://dl.google.com/chrome/mac/beta/googlechrome.dmg'
-  homepage 'https://www.google.com/intl/en/chrome/browser/beta.html'
-  version 'beta-channel'
-  no_checksum
-  link 'Google Chrome.app'
-end
