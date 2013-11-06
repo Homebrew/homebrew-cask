7ec673a468833e0c169876a47997bcf9168d9efa
Fernando Paredes <nano@fdp.io>
2013-11-05 19:59:05 -0800

    Move TorBrowserAlpha to homebrew-versions

--- Casks/tor-browser-alpha.rb
+++
@@ -1,7 +0,0 @@
-class TorBrowserAlpha < Cask
-  url 'https://archive.torproject.org/tor-package-archive/torbrowser/3.0a4/TorBrowserBundle-3.0-alpha-4-osx32_en-US.zip'
-  homepage 'https://www.torproject.org/projects/torbrowser.html'
-  version '3.0alpha4'
-  sha1 '7e034adc31ec1c52294444245a32e724038b5409'
-  link 'TorBrowser_en-US.app'
-end
