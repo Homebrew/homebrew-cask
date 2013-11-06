7521c676cbefba643ed72ef9f93a60c6a00c9f79
Fernando Paredes <nano@fdp.io>
2013-11-05 19:54:54 -0800

    Move Google Chrome variants to caskroom/homebrew-versions

--- Casks/google-chrome-canary.rb
+++
@@ -1,7 +0,0 @@
-class GoogleChromeCanary < Cask
-  url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
-  homepage 'https://tools.google.com/dlpage/chromesxs'
-  version 'latest'
-  no_checksum
-  link 'Google Chrome Canary.app'
-end
--- Casks/google-chrome-dev.rb
+++
@@ -1,7 +0,0 @@
-class GoogleChromeDev < Cask
-  url 'http://dl.google.com/chrome/mac/dev/googlechrome.dmg'
-  homepage 'https://www.google.com/chrome/'
-  version 'dev-channel'
-  no_checksum
-  link 'Google Chrome.app'
-end
