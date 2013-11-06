514c98b68fb3eb18d49b365f97abfe0cb8099580
Chulki Lee <chulki@asperasoft.com>
2013-11-05 16:11:48 -0800

    pg-commander 1.1.1

--- Casks/pg-commander.rb
+++ Casks/pg-commander.rb
@@ -1,7 +1,7 @@
 class PgCommander < Cask
-  url 'https://eggerapps.at/pgcommander/download/pgcommander-1.1.0.zip'
+  url 'https://eggerapps.at/pgcommander/download/pgcommander-1.1.1.zip'
   homepage 'http://eggerapps.at/pgcommander/'
-  version '1.1.0'
-  sha1 '9423315055bde9ac57215d18666381164929752b'
+  version '1.1.1'
+  sha1 '378b8cc358f2a8d7daefc8d66dd4974a47dcef4d'
   link 'PG Commander.app'
 end
