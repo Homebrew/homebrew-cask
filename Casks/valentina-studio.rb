30adb2c2d69ff91bd555c884e73868ecd3b3769f
Alexander Goldstein <alexg@alexland.org>
2013-11-06 00:27:34 -0600

    Valentina Studio

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,7 +1,7 @@
 class ValentinaStudio < Cask
   url 'http://www.valentina-db.com/download/release/mac_32/vstudio_5_mac.dmg'
   homepage 'http://www.valentina-db.com/'
-  version 'latest'
+  version '5'
   no_checksum
   link 'Valentina Studio.app'
 end
