2570e011f02e92c179da992a4e0711b3f82e95ee
Fernando Paredes <nano@fdp.io>
2013-11-06 19:29:32 -0800

    Change MacVim to Mavericks release

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,7 +1,7 @@
 class Macvim < Cask
-  url 'https://github.com/eee19/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Mountain-Lion.tbz'
+  url 'https://github.com/b4winckler/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Mavericks.tbz'
   homepage 'http://code.google.com/p/macvim/'
   version '7.4-72'
-  sha1 'bc3b899634d73908ddba5afd9b9a74778988aec3'
+  sha1 'dc983ae1e3ffae1c80f06eea9eacee49019a0c8a'
   link 'MacVim-snapshot-72/MacVim.app'
 end
