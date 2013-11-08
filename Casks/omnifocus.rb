41d31f7f995a96934b39dc7fe0429fab83b8a894
Fernando Paredes <nano@fdp.io>
2013-11-07 17:54:27 -0800

    Update and rename OmniFocus cask
    omni-focus -> omnifocus

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -1,4 +1,4 @@
-class OmniFocus < Cask
+class Omnifocus < Cask
   url 'http://www.omnigroup.com/download/latest/omnifocus'
   homepage 'http://www.omnigroup.com/products/omnifocus/'
   version 'latest'
