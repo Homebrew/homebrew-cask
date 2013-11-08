273dfead66a48535ccc9a600a3f8618c78a851dd
Fernando Paredes <nano@fdp.io>
2013-11-07 18:09:18 -0800

    Rename AppCleaner to style guide specs

--- Casks/appcleaner.rb
+++ Casks/appcleaner.rb
@@ -1,4 +1,4 @@
-class AppCleaner < Cask
+class Appcleaner < Cask
   url 'http://www.freemacsoft.net/downloads/AppCleaner_2.2.1.zip'
   homepage 'http://www.freemacsoft.net/appcleaner/'
   version '2.2.1'
