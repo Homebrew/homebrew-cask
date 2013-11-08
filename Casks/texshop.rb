0607a12dc8cdc96fe1be015b303c933c5a1e7069
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-11-08 09:41:03 +0100

    Adding TexShop, the LaTeX editor for MacOsX
    TexShop is a stable, simple and complete LaTeX (and friends) editor for
    Mac OSX. It comes normally prebundled with mactex but also exists as an
    independent package.

---
+++ Casks/texshop.rb
@@ -0,0 +1,7 @@
+class Texshop < Cask
+  url 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshop.zip'
+  homepage 'http://pages.uoregon.edu/koch/texshope'
+  version '3.2.6'
+  sha1 'b130b5b990e22387dab8144f3ae4c7440ff498fe'
+  link 'TexShop.app'
+end
