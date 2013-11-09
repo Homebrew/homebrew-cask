40ac5705aec3e52bf3942818ad16f07e1e40db91
Scott Reeves <cottser@mailworks.org>
2013-11-09 17:55:57 -0500

    Update MenuMeters for new naming conventions

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,4 +1,4 @@
-class MenuMeters < Cask
+class Menumeters < Cask
   url 'http://www.ragingmenace.com/software/download/MenuMeters.dmg'
   homepage 'http://www.ragingmenace.com/software/menumeters/'
   version 'latest'
