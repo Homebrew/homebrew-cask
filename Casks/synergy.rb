07c9f7327b6f78e0cc197cd551cf5aee61c3f770
Lincoln Lee <git@lincoln.hk>
2013-11-09 18:02:00 +0800

    Add caveats to synergy

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -4,4 +4,11 @@ class Synergy < Cask
   version '1.4.15'
   sha1 '90d8ef8e0fc2840437b9f966898b12cb1e779b23'
   link 'Synergy.app'
+
+  # Should be fixed in the next version
+  def caveats; <<-EOS.undent
+    After installation please follow the instruction here to make it work on Mavericks
+      http://synergy-foss.org/blog/mac-os-x-10-9-mavericks-support/
+    EOS
+  end
 end
