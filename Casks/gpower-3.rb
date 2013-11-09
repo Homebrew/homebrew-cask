a07beb3758296e812102964beb8677004a8ae9e0
Joshua Guy <jglenes@gmail.com>
2013-11-09 08:40:52 -0500

    Adding GPower 3 Cask
    A handy tool for statistical power analysis.

---
+++ Casks/gpower-3.rb
@@ -0,0 +1,7 @@
+class Gpower3 < Cask
+  url 'http://www.psycho.uni-duesseldorf.de/abteilungen/aap/gpower3/download-and-register/Dokumente/GPower3.1.7Mac.zip'
+  homepage 'http://www.psycho.uni-duesseldorf.de/abteilungen/aap/gpower3'
+  version '3.1.7'
+  sha1 '2fd5ba47a3d943fe35505a9a8e781f7b4c6231c6'
+  link 'G*Power 3.app'
+end
