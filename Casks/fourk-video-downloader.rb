6e7f2b9ae6a05935dac02ad6302a618b3b596bc9
Michael Ford <fanquake@users.noreply.github.com>
2013-11-09 15:55:15 +0800

    4K Video Downloader 3.0

--- Casks/fourk-video-downloader.rb
+++ Casks/fourk-video-downloader.rb
@@ -1,7 +1,7 @@
 class FourkVideoDownloader < Cask
-  url 'https://4kdownload.googlecode.com/files/4kvideodownloader_2.8.dmg'
+  url 'https://4kdownload.googlecode.com/files/4kvideodownloader_3.0.dmg'
   homepage 'http://www.4kdownload.com/products/product-videodownloader'
-  version '2.8.2'
-  sha1 '599b0e1c349ca7e0f439ade689a0318357a898e5'
+  version '3.0'
+  sha1 'e29f262f21baccd9fb9b9fa8742f3caf698e0187'
   link '4K Video Downloader.app'
 end
