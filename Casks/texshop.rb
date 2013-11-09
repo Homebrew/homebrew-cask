6205e80f4403b2c99ad55ddb2b2c92402b3a883c
Laurent Perrinet <laurent.perrinet@gmail.com>
2013-11-09 11:40:48 +0100

    adding the URL with the  version of TexShop

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,6 +1,6 @@
 class Texshop < Cask
-  url 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshop.zip'
-  homepage 'http://pages.uoregon.edu/koch/texshope'
+  url 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshop326.zip'
+  homepage 'http://pages.uoregon.edu/koch/texshop'
   version '3.2.6'
   sha1 'b130b5b990e22387dab8144f3ae4c7440ff498fe'
   link 'TexShop.app'
