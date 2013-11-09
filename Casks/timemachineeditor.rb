b02b700b080135ebc9724312f097556b396fe38d
Scott Reeves <cottser@mailworks.org>
2013-11-09 18:40:57 -0500

    Update TimeMachineEditor for new naming conventions

--- Casks/timemachineeditor.rb
+++ Casks/timemachineeditor.rb
@@ -1,4 +1,4 @@
-class TimeMachineEditor < Cask
+class Timemachineeditor < Cask
   url 'http://timesoftware.free.fr/timemachineeditor/TimeMachineEditor.zip'
   homepage 'http://timesoftware.free.fr/timemachineeditor/'
   version 'latest'
