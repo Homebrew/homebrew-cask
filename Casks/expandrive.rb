a33015e7fa700769f290997e97580485ed11587d
Scott Reeves <cottser@mailworks.org>
2013-11-09 16:17:54 -0500

    Update ExpanDrive for new naming conventions

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,4 +1,4 @@
-class ExpanDrive < Cask
+class Expandrive < Cask
   url 'http://updates.expandrive.com/apps/expandrive/download_latest'
   homepage 'http://www.expandrive.com/expandrive'
   version 'latest'
