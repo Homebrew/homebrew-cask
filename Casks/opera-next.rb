90223163b14680dfc644d4364407d5a0bd6ea04a
Michael Ford <fanquake@users.noreply.github.com>
2013-11-09 16:31:36 +0800

    Opera Next 18.0.1284.26

--- Casks/opera-next.rb
+++ Casks/opera-next.rb
@@ -1,7 +1,7 @@
 class OperaNext < Cask
-  url 'http://get.geo.opera.com/pub/opera-next/18.0.1284.11/mac/Opera_Next_18.0.1284.11_Setup.dmg'
+  url 'http://get.geo.opera.com/pub/opera-next/18.0.1284.26/mac/Opera_Next_18.0.1284.26_Setup.dmg'
   homepage 'http://www.opera.com/developer/next'
-  version '18.0.1284.11'
-  sha1 'faf85904021149b26c2446f4a3ecbccab59f70a7'
+  version '18.0.1284.26'
+  sha1 'e61e4e0960a2cb54dd9736b21720eea2b119af46'
   link 'Opera Next.app'
 end
