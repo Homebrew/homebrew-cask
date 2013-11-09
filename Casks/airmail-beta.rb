795d79ef365211a89f5dfb84bff82e31cf401c0c
Fernando Paredes <nano@fdp.io>
2013-11-09 09:04:54 -0800

    Airmail Beta 1.3.0 (214)
    Change checksum to follow MD5 on download page

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,7 +1,7 @@
 class AirmailBeta < Cask
-  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/654/561/original/4fb8e021b4f6308a8399c956b56980d9/AirMail_Beta.zip'
+  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/682/111/original/3a25588dd6b0340d5094dcbfd1f3d9c1/AirMail_Beta.zip'
   homepage 'http://airmailapp.com/'
-  version '1.2.1(213)'
-  sha1 'd984271a4ee72ddc5808682634d0bc617dc847bc'
+  version '1.3.0 (214)'
+  md5 '8E7F8BE143CFC93E5C727FEDDBBF5D29'
   link 'AirMail Beta.app'
 end
