094dae2119f83265cf7c99bc6e8d62061923cb75
Michael Ford <fanquake@users.noreply.github.com>
2013-11-09 16:04:07 +0800

    Google App Engine Launcher 1.8.7

--- Casks/google-app-engine-launcher.rb
+++ Casks/google-app-engine-launcher.rb
@@ -1,7 +1,7 @@
 class GoogleAppEngineLauncher < Cask
-  url 'https://googleappengine.googlecode.com/files/GoogleAppEngineLauncher-1.8.6.dmg'
+  url 'https://googleappengine.googlecode.com/files/GoogleAppEngineLauncher-1.8.7.dmg'
   homepage 'https://developers.google.com/appengine/'
-  version '1.8.6'
-  sha1 'badbde72fe79d20baee793341bc4d8ec863439b2'
+  version '1.8.7'
+  sha1 '513b0df1d777099c388b9f7cf6fdf13bbdd82f16'
   link 'GoogleAppEngineLauncher.app'
 end
