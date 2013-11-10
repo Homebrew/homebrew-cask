e19a80b5a4253721698fb070bbd9ec276677a84f
Lucas Mendelowski <lucas@mendelowski.com>
2013-11-10 21:52:42 +0800

    Add Jing

---
+++ Casks/jing.rb
@@ -0,0 +1,7 @@
+class Jing < Cask
+  url 'http://download.techsmith.com/jing/mac/jing.dmg'
+  homepage 'http://www.techsmith.com/jing.html'
+  version 'latest'
+  sha1 '2800da21d78294b64a53d6f03d371b7b768d40e7'
+  link 'Jing.app'
+end
