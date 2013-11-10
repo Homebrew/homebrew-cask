038446be64c85471b019c460955541c8ee2b31c8
Scott Reeves <cottser@mailworks.org>
2013-11-10 17:04:53 -0500

    Kaleidoscope 2.0.2

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -1,7 +1,7 @@
 class Kaleidoscope < Cask
-  url 'http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-2.0.1-114.zip'
+  url 'http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-2.0.2-116.zip'
   homepage 'http://www.kaleidoscopeapp.com/'
-  version '2.0.1_114'
-  sha1 'ad4d7819868e4fe3ba8ea63a7341748d57d21222'
+  version '2.0.2_116'
+  sha1 '4a35f6e826f0553e6d7ed02486617589f262f7fc'
   link 'Kaleidoscope.app'
 end
