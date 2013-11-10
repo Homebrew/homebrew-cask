b2c7e7dd291554b92c0def54bfdcdab8883fd82e
Joshua Guy <jglenes@gmail.com>
2013-11-10 10:02:31 -0500

    Fixing airmail beta checksum (again).

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -2,6 +2,6 @@ class AirmailBeta < Cask
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   homepage 'http://airmailapp.com/'
   version '1.3.0 (214)'
-  sha1 '17c5659506f621c31cc5c345f69f66118ca55a85'
+  sha1 '5af895fcf05ec937378064a1f77a6f36acc2052e'
   link 'AirMail Beta.app'
 end
