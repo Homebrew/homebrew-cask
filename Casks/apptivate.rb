4debc9e4c987e6c731e6fae3b0939db2c1f727ca
Gursimran Singh <g@kilotau.com>
2013-11-10 17:38:04 -0600

    Add a cask for Apptivate.app, 2.1 (12)
    This diff creates a new cask for Homebrew Cask for the app called
    Apptivate, which can be found at http://www.apptivateapp.com. I followed
    the instructions on contributing found on Homebrew Cask to create
    this.
    Test plan: install using `brew cask install apptivate`, launch
    Apptivate using `open -a "Apptivate"`, and check the version and if
    everything works.

---
+++ Casks/apptivate.rb
@@ -0,0 +1,7 @@
+class Apptivate < Cask
+  url 'http://www.apptivateapp.com/resources/Apptivate.app.zip'
+  homepage 'http://www.apptivateapp.com'
+  version '2.1_12'
+  sha1 '6135ba88e2aa3b6141c6a2a76cd4142518ba35fc'
+  link 'Apptivate.app'
+end
