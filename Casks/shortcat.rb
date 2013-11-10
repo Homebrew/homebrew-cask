a794e156ccb8f1ab56e578c973d9ca94ee3ad6f3
Scott Reeves <cottser@mailworks.org>
2013-11-10 17:27:56 -0500

    Shortcat 0.6.2

--- Casks/shortcat.rb
+++ Casks/shortcat.rb
@@ -1,7 +1,7 @@
 class Shortcat < Cask
-  url 'https://files.shortcatapp.com/v0.6.1/Shortcat.zip'
+  url 'https://files.shortcatapp.com/v0.6.2/Shortcat.zip'
   homepage 'http://shortcatapp.com/'
-  version '0.6.1'
-  sha1 'd99bf7374d37d04333e9dc288ff3deea2e7621c2'
+  version '0.6.2'
+  sha1 '1db2d40d36d1866ca4fc8f595219389be84c071f'
   link 'Shortcat.app'
 end
