a03d2202c5f2e3326194992a63db85a7d7f4b100
Scott Reeves <cottser@mailworks.org>
2013-11-10 09:48:28 -0500

    LaunchBar 5.6.1

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -1,7 +1,7 @@
 class Launchbar < Cask
-  url 'http://www.obdev.at/downloads/launchbar/LaunchBar-5.5.2.dmg'
+  url 'http://www.obdev.at/downloads/launchbar/LaunchBar-5.6.1.dmg'
   homepage 'http://www.obdev.at/products/launchbar/'
-  version '5.5.2'
-  sha1 'a28d80efeede584602666c6efcb849586fe88bb4'
+  version '5.6.1'
+  sha1 'cc832d5708611ee7b9fb59f947dc4de4bee08e5d'
   link 'LaunchBar.app'
 end
