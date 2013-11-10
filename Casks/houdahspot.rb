2f1ca71162413524cef2cb4fd6710a534e1c923c
Scott Reeves <cottser@mailworks.org>
2013-11-10 16:49:09 -0500

    Add HoudahSpot
    Spotlight UI.

---
+++ Casks/houdahspot.rb
@@ -0,0 +1,7 @@
+class Houdahspot < Cask
+  url 'http://www.houdah.com/houdahSpot/download_assets/HoudahSpot_latest.zip'
+  homepage 'http://www.houdah.com/houdahSpot/'
+  version 'latest'
+  no_checksum
+  link 'HoudahSpot.app'
+end
