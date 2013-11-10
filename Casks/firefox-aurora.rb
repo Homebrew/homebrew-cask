93580cc1924c96dd836e0770d9ba4f743ff817df
Scott Reeves <cottser@mailworks.org>
2013-11-10 08:12:04 -0500

    Remove Firefox Aurora and Nightly versions
    Moving to caskroom/versions

--- Casks/firefox-aurora.rb
+++
@@ -1,7 +0,0 @@
-class FirefoxAurora < Cask
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-27.0a2.en-US.mac.dmg'
-  homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
-  version '27.0a2'
-  no_checksum
-  link 'FirefoxAurora.app'
-end
