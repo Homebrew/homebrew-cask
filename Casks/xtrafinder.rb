bbd022d22f773429a484680270d0c1663c2e316a
Scott Reeves <cottser@mailworks.org>
2013-11-09 21:00:20 -0500

    Update XtraFinder for new naming conventions

--- Casks/xtrafinder.rb
+++ Casks/xtrafinder.rb
@@ -1,4 +1,4 @@
-class XtraFinder < Cask
+class Xtrafinder < Cask
   url 'http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg'
   homepage 'http://www.trankynam.com/xtrafinder/'
   version 'latest'
