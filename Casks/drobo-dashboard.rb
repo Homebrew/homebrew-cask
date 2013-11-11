0a2f033f50159ed60f295b54b128e91ed0362712
nettolt <nettolt@users.noreply.github.com>
2013-11-10 22:15:00 -0700

    Delete drobo-dashboard.rb

--- Casks/drobo-dashboard.rb
+++
@@ -1,8 +0,0 @@
-class DroboDashboard < Cask
-  url 'http://www.drobo.com/start/download/dashboard/Drobo-Dashboard-installer-2.5.2.dmg'
-  homepage 'http://wwww.drobo.com'
-  version '2.5.2'
-  sha1 'a7b81536c94674d3442ba6178f98d1acfdbe0d3e'
-  install 'Install.app/Contents/Drobo_Dashboard_Installer_2.5.2.pkg'
-  uninstall :script => 'Uninstall.app/Contents/Resources/Scripts/Drobo_Dashboard_uninstall.sh'
-end
