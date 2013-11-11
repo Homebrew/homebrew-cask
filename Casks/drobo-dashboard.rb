c0d8d4420817e887e17f5b4523eeb564736f2636
nettolt <nettolt@users.noreply.github.com>
2013-11-10 21:47:58 -0700

    Update drobo-dashboard.rb

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -1,5 +1,5 @@
 class DroboDashboard < Cask
-  url = 'http://www.drobo.com/start/download/dashboard/Drobo-Dashboard-installer-2.5.2.dmg'
+  url 'http://www.drobo.com/start/download/dashboard/Drobo-Dashboard-installer-2.5.2.dmg'
   homepage 'http://wwww.drobo.com'
   version '2.5.2'
   sha1 'a7b81536c94674d3442ba6178f98d1acfdbe0d3e'
