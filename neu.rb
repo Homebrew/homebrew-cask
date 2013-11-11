b93ef8f196945f01e7574eb80a0d5533fe232204
nettolt <nettolt@users.noreply.github.com>
2013-11-10 23:01:39 -0700

    Create neu.rb

---
+++ neu.rb
@@ -0,0 +1,7 @@
+class Neu < Cask
+  url 'http://downloads.elegantchaos.com/neu/neu-v1.2.1.zip'
+  homepage 'http://www.elegantchaos.com/neu/'
+  version '1.2.1'
+  sha1 '5b03ca0a78fcabb287cc43f3902f583b547a008d'
+  link 'neu-v1.2.1/Neu.app'
+end
