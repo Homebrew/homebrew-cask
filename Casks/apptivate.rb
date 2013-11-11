0c3f3543bf7840ea8f119c24517b4529936da23f
Gursimran Singh <g@kilotau.com>
2013-11-10 18:08:24 -0600

    Update to latest versioning scheme
    As per @NanoXD's suggestion, I've changed the version and the SHA sum to
    `latest_version` and `no_checksum`.

--- Casks/apptivate.rb
+++ Casks/apptivate.rb
@@ -1,7 +1,7 @@
 class Apptivate < Cask
   url 'http://www.apptivateapp.com/resources/Apptivate.app.zip'
   homepage 'http://www.apptivateapp.com'
-  version '2.1_12'
-  sha1 '6135ba88e2aa3b6141c6a2a76cd4142518ba35fc'
+  version 'latest'
+  no_checksum
   link 'Apptivate.app'
 end
