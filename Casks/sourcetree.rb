04e4aa47150583b3fdc257e017f88cfd36b4c66b
Paspartout <paspartout@fogglabs.de>
2013-11-11 19:06:25 +0100

    Updated SourceTree to the latest version

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,7 +1,7 @@
 class Sourcetree < Cask
-  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.7.3.dmg'
+  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.7.4.1.dmg'
   homepage 'http://www.sourcetreeapp.com/'
-  version '1.7.3'
-  sha1 '426a438aaab1fa100671c3674dee83b72cd4e841'
+  version '1.7.4.1'
+  sha1 'e99b7638ab6b2d8a47b6ae6c197a3ab4f4548739'
   link 'SourceTree.app'
 end
