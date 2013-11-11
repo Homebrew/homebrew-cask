44a2a709c3a0aaae5968d144d68ac48bcb3adff3
nettolt <nettolt@users.noreply.github.com>
2013-11-10 21:46:34 -0700

    Update and rename drobo-dashboard to drobo-dashboard.rb

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -4,3 +4,4 @@ class DroboDashboard < Cask
   version '2.5.2'
   sha1 'a7b81536c94674d3442ba6178f98d1acfdbe0d3e'
   install 'Install.app/Contents/Install.app/Drobo_Dashboard_Installer_2.5.2.pkg'
+end
