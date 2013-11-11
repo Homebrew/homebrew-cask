7d1e59d635d742120835262c9b59d21f4a7502c1
Lucas Mendelowski <lucas@mendelowski.com>
2013-11-11 20:25:00 +0800

    Fix checksum in order to follow 'latest' scheme

--- Casks/jing.rb
+++ Casks/jing.rb
@@ -2,6 +2,6 @@ class Jing < Cask
   url 'http://download.techsmith.com/jing/mac/jing.dmg'
   homepage 'http://www.techsmith.com/jing.html'
   version 'latest'
-  sha1 '2800da21d78294b64a53d6f03d371b7b768d40e7'
+  sha1 'no_checksum'
   link 'Jing.app'
 end
