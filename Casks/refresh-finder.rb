92e09401d3b93e9ed26452f431193008a207bcad
nettolt <nettolt@users.noreply.github.com>
2013-11-10 23:16:13 -0700

    Create refresh-finder.rb

---
+++ Casks/refresh-finder.rb
@@ -0,0 +1,8 @@
+class RefreshFinder < Cask
+  url 'https://www.macupdate.com/download/24714/Refresh_Finder_1.5.0.dmg.zip'
+  homepage 'https://www.macupdate.com/app/mac/24714/refresh-finder'
+  version '1.5.0'
+  sha1 'd915658f4b8eca23301b4837ba53cb4591439455'
+  nested_container 'Refresh_Finder_1.5.0.dmg'
+  link 'Refresh Finder.app'
+end
