2e8bd6bed756df2c8e574177df5634fba947767c
alpyne <alan.pyne+github@gmail.com>
2013-11-11 23:12:23 +0100

    Update to 10.2.0 (for Mavericks)

--- Casks/pc-keyboardhack.rb
+++ Casks/pc-keyboardhack.rb
@@ -1,7 +1,7 @@
 class PcKeyboardhack < Cask
-  url 'https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-10.0.0.dmg'
+  url 'https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-10.2.0.dmg'
   homepage 'https://pqrs.org/macosx/keyremap4macbook/pckeyboardhack.html.en'
-  version '10.0.0'
-  sha1 'b9df8ba7e6417391394b6e086afeb5d0d25f07f5'
+  version '10.2.0'
+  sha1 'f37d06a84ec75adb45040a144067709a6beb64fd'
   install 'PCKeyboardHack.pkg'
 end
