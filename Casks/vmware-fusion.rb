13fe883c7531b536c20d4e0521a7be0dcd84a050
nettolt <nettolt@users.noreply.github.com>
2013-11-10 23:20:48 -0700

    Updated to version 6.0.2-1398658

--- Casks/vmware-fusion.rb
+++ Casks/vmware-fusion.rb
@@ -1,7 +1,7 @@
 class VmwareFusion < Cask
-  url 'https://s3.amazonaws.com/boxen-downloads/vmware/VMware-Fusion-5.0.3-1040386-light.dmg'
+  url 'https://download3.vmware.com/software/fusion/file/VMware-Fusion-6.0.2-1398658-light.dmg'
   homepage 'http://www.vmware.com/products/fusion/'
-  version '5.0.3-1040386'
-  sha1 'a34284daa1d0e66494530498a57fac933ca68aff'
+  version '6.0.2-1398658'
+  sha1 '13e159b7b3c934d2d7066c249277e1be8493841e'
   link 'VMware Fusion.app'
 end
