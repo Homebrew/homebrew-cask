78e181797e4a103961e71314bb19aac15c24fb6c
Glenn 'devalias' Grant <glenn@devalias.net>
2013-11-12 11:49:30 +1100

    Add Armitage

---
+++ Casks/armitage.rb
@@ -0,0 +1,7 @@
+class Armitage < Cask
+  url 'http://www.fastandeasyhacking.com/download/armitage20130821.dmg'
+  homepage 'http://www.fastandeasyhacking.com/'
+  version '08.21.13'
+  sha1 '667f25eae9dbd05fa7a91223e01deffd9a124c6a'
+  link 'Armitage.app'
+end
