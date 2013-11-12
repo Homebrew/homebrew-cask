c44707632c8d87ade0c8280b8315c598b620acbd
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-12 16:12:19 -0600

    SketchUp 2013 -> latest

--- Casks/sketchup.rb
+++ Casks/sketchup.rb
@@ -1,7 +1,7 @@
 class Sketchup < Cask
   url 'http://dl.trimble.com/sketchup/SketchUpMEN.dmg'
-  homepage 'http://www.sketchup.com/intl/en/'
-  version '2013'
-  sha1 '839461e9e65181330da8656bd92ae03951a53a9d'
+  homepage 'http://www.sketchup.com'
+  version 'latest'
+  no_checksum
   link 'SketchUp 2013'
 end
