8231bc5727c32c7f0a1b7932f12b15f191ec2042
Andrew <andrew.oke@gmail.com>
2013-11-12 14:08:05 -0500

    update ynab to minor version 318

--- Casks/ynab.rb
+++ Casks/ynab.rb
@@ -1,7 +1,7 @@
 class Ynab < Cask
-  url 'http://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.195.dmg'
+  url 'http://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.318.dmg'
   homepage 'http://www.youneedabudget.com/'
-  version '4.3.195'
-  sha1 '19a15d5d744c42453155d82645e7715d68ecbca8'
+  version '4.3.318'
+  sha1 'a7f31366fbe43ef3b0d8bde293801df50291a815'
   link 'YNAB 4.app'
 end
