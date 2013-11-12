64f110b4925ff1fb9f5b98e26d5a5c5ad2ad37ee
Joel Kuzmarski <leoj3n@gmail.com>
2013-11-12 15:21:06 -0600

    Delete PandoraJam

--- Casks/pandorajam.rb
+++
@@ -1,7 +0,0 @@
-class Pandorajam < Cask
-  url 'http://www.bitcartel.com/downloads/pandorajam.zip'
-  homepage 'http://www.bitcartel.com/pandorajam/'
-  version 'latest'
-  no_checksum
-  link 'PandoraJam.app'
-end
