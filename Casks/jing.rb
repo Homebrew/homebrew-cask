21034786c7dc8ea7340a30684d3bafa08c5f77e9
Lucas Mendelowski <lucas@mendelowski.com>
2013-11-12 20:27:42 +0800

    Remove quotation marks for no_checksum

--- Casks/jing.rb
+++ Casks/jing.rb
@@ -2,6 +2,6 @@ class Jing < Cask
   url 'http://download.techsmith.com/jing/mac/jing.dmg'
   homepage 'http://www.techsmith.com/jing.html'
   version 'latest'
-  sha1 'no_checksum'
+  no_checksum
   link 'Jing.app'
 end
