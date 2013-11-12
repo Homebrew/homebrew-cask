ceb3040f908980a743fa65e5b617d09d3c5dec2f
Jonathan Chu <jonathan.chu@me.com>
2013-11-12 13:56:36 -0500

    Add new cask for Sqwiggle.app.

---
+++ Casks/sqwiggle.rb
@@ -0,0 +1,7 @@
+class Sqwiggle < Cask
+  url 'https://www.sqwiggle.com/download/mac'
+  homepage 'https://www.sqwiggle.com'
+  version '0.4.2'
+  sha1 '15c49c5443780749ca833605bd61f9ee0b4d8105'
+  link 'Sqwiggle.app'
+end
