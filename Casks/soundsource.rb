365fdeb6ef56f549c00eeb26c4e8b2a189e28382
Samer N <samer@nakfour.com>
2013-11-12 23:23:53 +0200

    added a cask for SoundSource

---
+++ Casks/soundsource.rb
@@ -0,0 +1,7 @@
+class Soundsource < Cask
+  url 'http://www.rogueamoeba.com/legacy/downloads/SoundSource-251.zip'
+  homepage 'http://www.rogueamoeba.com/legacy/'
+  version '2.5.1'
+  link 'SoundSource.app'
+  no_checksum
+end
