9b295445c32327f466bc66c62b0bdf226a54beb6
Jonathan Chu <jonathan.chu@me.com>
2013-11-13 00:19:06 -0500

    Modify for no version or checksum since no specific version.

--- Casks/sqwiggle.rb
+++ Casks/sqwiggle.rb
@@ -1,7 +1,7 @@
 class Sqwiggle < Cask
   url 'https://www.sqwiggle.com/download/mac'
   homepage 'https://www.sqwiggle.com'
-  version '0.4.2'
-  sha1 '15c49c5443780749ca833605bd61f9ee0b4d8105'
+  version 'latest'
+  no_checksum
   link 'Sqwiggle.app'
 end
