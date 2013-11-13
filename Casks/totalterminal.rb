396ea76def154788a7f2337595223ca1b418ba8c
Jim Smitley <jsmitley+github@gmail.com>
2013-11-12 19:42:35 -0600

    Fixed checksum value for TotalTerminal 1.4.6

--- Casks/totalterminal.rb
+++ Casks/totalterminal.rb
@@ -2,7 +2,7 @@ class Totalterminal < Cask
   url 'http://downloads.binaryage.com/TotalTerminal-1.4.6.dmg'
   homepage 'http://totalterminal.binaryage.com'
   version '1.4.6'
-  sha1 'f77ca7d01fe27cc1b3ed5631c7455a85faf6500b'
+  sha1 'd0163f80f4c993b05952cef84b55aabaa37af0cc'
   install 'TotalTerminal.pkg'
   uninstall :script => 'TotalTerminal Uninstaller.app/Contents/MacOS/TotalTerminal Uninstaller',
             :args => %w[--headless]
