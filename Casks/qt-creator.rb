9b688eab60ee7e674ee20ed36c6c796246d30a66
Matt Keeter <matt.j.keeter@gmail.com>
2013-11-14 18:04:59 -0500

    Add cask for Qt Creator

---
+++ Casks/qt-creator.rb
@@ -0,0 +1,7 @@
+class QtCreator < Cask
+  url 'http://download.qt-project.org/official_releases/qtcreator/2.8/2.8.1/qt-creator-mac-opensource-2.8.1.dmg'
+  homepage 'http://qt-project.org/'
+  version '2.8.1'
+  sha1 '49033ed85f191ab8c6f191b74a47a7f945ce5c0b'
+  link 'Qt Creator.app'
+end
