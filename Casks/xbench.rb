119a23d8a3487cba51f637b1cce7512a5aa5f292
Lucas Garron <lucasg@gmx.de>
2013-11-14 08:23:15 -0800

    Added XBench 1.3.

---
+++ Casks/xbench.rb
@@ -0,0 +1,7 @@
+class Xbench < Cask
+  url 'http://xbench.com/Xbench_1.3.dmg'
+  homepage 'http://xbench.com/'
+  version '1.3'
+  sha1 '9146b478f609e4da1f58a462fc756f81d8c91358'
+  link 'XBench.app'
+end
