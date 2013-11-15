12606aeef9abb06558a1274755286bf6ac9f0efc
Alexander Kucera <a.kucera@babylondreams.de>
2013-11-15 15:09:01 +0100

    Added Screens Connect
    Screens Connect lets you connect to your Mac when you're away from home or the office. It is the companion app to Screens (available on the App Store).

---
+++ Casks/screens-connect.rb
@@ -0,0 +1,7 @@
+class ScreensConnect < Cask
+  url 'https://screensconnect.com/downloads/screensconnect.dmg'
+  homepage 'https://screensconnect.com'
+  version 'latest'
+  no_checksum
+  install 'Screens Connect.pkg'
+end
