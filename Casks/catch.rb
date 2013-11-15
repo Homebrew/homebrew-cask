96d1d724d54c492777f258095646c7cb6b35d5e1
Alexander Kucera <a.kucera@babylondreams.de>
2013-11-15 13:13:31 +0100

    Added Catch
    Catch is the easiest way to use ShowRSS on OS X.

---
+++ Casks/catch.rb
@@ -0,0 +1,7 @@
+class Catch < Cask
+  url 'https://github.com/mipstian/catch/releases/download/1.6/Catch-1.6.zip'
+  homepage 'http://www.giorgiocalderolla.com/index.html'
+  version '1.6'
+  sha1 '3af75e482c745a96a8ad8155461a5184bf9ed7f4'
+  link 'Catch.app'
+end
