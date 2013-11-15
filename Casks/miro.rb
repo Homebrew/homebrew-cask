d95c252ef7608694b3a1f1755a55d2c3495a47b2
Rimenes Ribeiro <eu@rimenes.net>
2013-11-15 10:02:28 -0200

    Remove versinoing details

--- Casks/miro.rb
+++ Casks/miro.rb
@@ -1,7 +1,7 @@
 class Miro < Cask
   url 'http://ftp.osuosl.org/pub/pculture.org/miro/osx/Miro.dmg'
   homepage 'http://www.getmiro.com/'
-  version '6.0'
-  sha1 '242f3b7a279d7b2885c3161249b7c5ba19c15226'
+  version 'latest'
+  no_checksum
   link 'Miro.app'
 end
