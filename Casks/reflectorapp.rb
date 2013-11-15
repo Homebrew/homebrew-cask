0e7ab1b4949fc3d718d81a5766aa8a2ec16c1dfb
Henning Hoyer <henninghoyer@gmail.com>
2013-11-15 21:46:54 +0100

    Added Reflector Cask
    This adds a cask to install Reflector by Airsquirrels.
    Their download link always points to the latest version,
    hence no hash as that would break when they update.
    When first launching after installation the app will
    request to be copied into /Applications rather than run
    from ~/Applications.

---
+++ Casks/reflectorapp.rb
@@ -0,0 +1,7 @@
+class Reflectorapp < Cask
+  url 'http://download.airsquirrels.com/Reflector/Mac/Reflector.dmg'
+  homepage 'http://www.airsquirrels.com/reflector/'
+  version 'latest'
+  no_checksum 
+  link 'Reflector.app'
+end
