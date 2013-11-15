f75b06b83ea14c3f6f9b58f774f06d8fbc945ee4
Lucas Garron <lucasg@gmx.de>
2013-11-14 20:07:33 -0800

    Added qlplugin cask for ScriptQL.

---
+++ Casks/scriptql.rb
@@ -0,0 +1,7 @@
+class Scriptql < Cask
+  url 'http://www.kainjow.com/downloads/ScriptQL_qlgenerator.zip'
+  homepage 'http://www.kainjow.com/'
+  version 'latest'
+  no_checksum
+  qlplugin 'ScriptQL.qlgenerator'
+end
