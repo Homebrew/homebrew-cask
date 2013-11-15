21bd12bbbdc2701203886b9ba998c3ae90096583
Vítor Galvão <info@vitorgalvao.com>
2013-11-15 02:09:23 +0000

    Moved homepage line to be under url line

--- Casks/vlc.rb
+++ Casks/vlc.rb
@@ -1,6 +1,6 @@
 class Vlc < Cask
-  homepage 'http://www.videolan.org/vlc/'
   url 'http://get.videolan.org/vlc/2.1.1/macosx/vlc-2.1.1.dmg'
+  homepage 'http://www.videolan.org/vlc/'
   version '2.1.1'
   sha1 'ef3ded27e0ca622ae2262840ba2d7d05090b0a0c'
   link 'VLC.app'
