afde85e3a3650dd9a0bc1415e65b93d77655e38e
Rimenes Ribeiro <eu@rimenes.net>
2013-11-15 09:38:26 -0200

    Update Instacast to version 1.0.6 (2048)

--- Casks/instacast.rb
+++ Casks/instacast.rb
@@ -1,7 +1,7 @@
 class Instacast < Cask
-  url 'http://assets.vemedio.com/software/instacast/Instacast-1.0.5-2047.dmg'
+  url 'http://assets.vemedio.com/software/instacast/Instacast-1.0.6-2048.dmg'
   homepage 'http://vemedio.com/products/instacast-mac'
-  version '1.0.5 (2047)'
-  sha1 '202c9dc96db8bdf0af51609c1f5796a8bf0fd7c3'
+  version '1.0.6 (2048)'
+  sha1 '355c934e363f478df4cd4c02da70c7ef17b55834'
   link 'Instacast.app'
 end
