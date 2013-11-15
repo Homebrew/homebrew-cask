603cb45faf591e3adc3c35386b16fe04591a9f3c
Julio Napurí <julionc@gmail.com>
2013-11-14 23:30:49 -0500

    Update JDownloader URL

--- Casks/jdownloader.rb
+++ Casks/jdownloader.rb
@@ -1,5 +1,5 @@
 class Jdownloader < Cask
-  url 'http://94.23.204.158/jDownloader_Java_1_5.dmg'
+  url 'http://installer.jdownloader.org/JDownloader09Setup_mac.dmg'
   homepage 'http://jdownloader.org/'
   version 'latest'
   no_checksum
