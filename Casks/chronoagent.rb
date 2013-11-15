416ad12f128c4a7da5def4da38c09bc6a2bfca58
Alexander Kucera <a.kucera@babylondreams.de>
2013-11-15 14:00:47 +0100

    Fixed homepage link

--- Casks/chronoagent.rb
+++ Casks/chronoagent.rb
@@ -1,6 +1,6 @@
 class Chronoagent < Cask
   url 'http://downloads.econtechnologies.com/CA_Mac_Download.dmg'
-  homepage 'http://downloads.econtechnologies.com/CA_Mac_Download.dmg'
+  homepage 'http://www.econtechnologies.com'
   version 'latest'
   no_checksum
   install 'Install.pkg'
