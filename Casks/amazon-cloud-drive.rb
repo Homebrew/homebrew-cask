573758aeeb061a4f1f99352fa8e34295d3922076
Rimenes Ribeiro <eu@rimenes.net>
2013-11-15 00:24:28 -0200

    Remove versioning details

--- Casks/amazon-cloud-drive.rb
+++ Casks/amazon-cloud-drive.rb
@@ -1,7 +1,7 @@
 class AmazonCloudDrive < Cask
   url 'https://d29x207vrinatv.cloudfront.net/AmazonCloudDrive.dmg'
   homepage 'https://www.amazon.com/clouddrive'
-  version '2.1'
-  sha1 'd832aafa413cec1e6dbbc11824a1fd6159ff6662'
+  version 'latest'
+  no_checksum
   link 'Amazon Cloud Drive.app'
 end
