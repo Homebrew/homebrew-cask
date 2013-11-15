5e11e93486577ceb7e0fc96adf85fc1caa7dd5c8
sani <sani@users.noreply.github.com>
2013-11-15 15:56:18 +0000

    Update blender.rb
    new version

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,7 +1,7 @@
 class Blender < Cask
-  url 'http://download.blender.org/release/Blender2.68/blender-2.68a-OSX_10.6-x86_64.zip'
+  url 'http://download.blender.org/release/Blender2.69/blender-2.69-OSX_10.6-x86_64.zip'
   homepage 'http://www.blender.org/'
-  version '2.68a'
-  sha1 '0ba47d7cfa740c5cb6b9b9d6a5bd96c49c35176c'
+  version '2.69'
+  sha1 '19b9416a30985cb6535b24517c19446d227ed6a4'
   link 'Blender/blender.app', 'Blender/blenderplayer.app'
 end
