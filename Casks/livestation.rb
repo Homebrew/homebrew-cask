0dccaf9c6111337684fffae709834af47d2d0a31
Rimenes Ribeiro <eu@rimenes.net>
2013-11-15 09:57:39 -0200

    Update Livestation to version 3.3.0

--- Casks/livestation.rb
+++ Casks/livestation.rb
@@ -1,7 +1,7 @@
 class Livestation < Cask
-  url 'http://updates.livestation.com/releases/Livestation-3.2.0-intel.dmg'
+  url 'http://updates.livestation.com/releases/Livestation-3.3.0.dmg'
   homepage 'http://www.livestation.com'
-  version '3.2.0'
-  sha1 '7c3334e2c710d8c89bfc91591ffed56c1b74643c'
+  version '3.3.0'
+  sha1 '2d91b4807a6e63446bd569e7c819a49129f1a7c1'
   link 'Livestation.app'
 end
