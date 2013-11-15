d06694e576b24ccbf6e31982b27684aa9cb14869
Alexander Kucera <a.kucera@babylondreams.de>
2013-11-15 13:41:27 +0100

    Added ChronoSync
    The complete backup and sync solution for your Mac

---
+++ Casks/chronosync.rb
@@ -0,0 +1,8 @@
+class Chronosync < Cask
+  url 'http://downloads.econtechnologies.com/CS4_Download.dmg'
+  homepage 'http://www.econtechnologies.com'
+  version 'latest'
+  no_checksum
+  install 'Install.pkg'
+  uninstall 'Remove.pkg'
+end
