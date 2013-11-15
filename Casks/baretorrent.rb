6a93f7046011d5e95b900dc03d92cefc674cef65
Rimenes Ribeiro <eu@rimenes.net>
2013-11-15 01:00:16 -0200

    Update baretorrent to version 0.4.4

--- Casks/baretorrent.rb
+++ Casks/baretorrent.rb
@@ -1,7 +1,7 @@
 class Baretorrent < Cask
-  url 'https://launchpad.net/baretorrent/trunk/0.4.3/+download/baretorrent-0.4.3-osx-x64.dmg'
+  url 'https://launchpad.net/baretorrent/trunk/0.4.4/+download/baretorrent-0.4.4-osx-x64.dmg'
   homepage 'http://baretorrent.org'
-  version '0.4.3'
-  sha1 '3f748c19a6b0ff535f7fa1fc9da542ebf5e86334'
+  version '0.4.4'
+  sha1 '896923204c91dafa286ced2487e16aeb53e72965'
   link 'baretorrent.app'
 end
