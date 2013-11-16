d8d5d0961bec409b48c0d686757e1ceab98c8178
Scott Reeves <cottser@mailworks.org>
2013-11-15 22:29:00 -0500

    Hazel 3.2.1

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -1,7 +1,7 @@
 class Hazel < Cask
-  url 'https://s3.amazonaws.com/Noodlesoft/Hazel-3.2.dmg'
+  url 'https://s3.amazonaws.com/Noodlesoft/Hazel-3.2.1.dmg'
   homepage 'http://www.noodlesoft.com/hazel.php'
-  version '3.2'
-  sha1 'cd430a26c9ff4cf69bda87d8ac40d2245bd66c3b'
+  version '3.2.1'
+  sha1 '483a03f1961647739baabda05ed6b021bd79afac'
   prefpane 'Hazel.prefPane'
 end
