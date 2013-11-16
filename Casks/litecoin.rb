ade64d48d4b8b7d8a3e66db07b6497d1326fb892
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-16 09:48:45 +0800

    Add litecoin v 0.8.5.1

---
+++ Casks/litecoin.rb
@@ -0,0 +1,7 @@
+class Litecoin < Cask
+  url 'https://litecoin.org/downloads/macosx/Litecoin-Qt-0.8.5.1.dmg'
+  homepage 'https://litecoin.org/'
+  version '0.8.5.1'
+  sha1 '2e9bcbaf8f7da934a7f1095bdcd400a5bed99478'
+  link 'Litecoin-Qt.app'
+end
