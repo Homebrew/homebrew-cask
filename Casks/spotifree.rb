789177f2f53880a409dba9d79a2364b55ec04cce
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-17 22:52:16 +0800

    Create Spotifree Cask

--- Casks/spotifree.rb
+++ Casks/spotifree.rb
@@ -1,6 +1,6 @@
 class Spotifree < Cask
   url 'http://spotifree.gordinskiy.com/files/Spotifree.dmg'
-  homepage 'http://spotifree.gordinskiy.com'
+  homepage 'http://spotifree.gordinskiy.com/'
   version 'latest'
   no_checksum
   link 'Spotifree.app'
