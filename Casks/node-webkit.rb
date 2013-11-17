03e72fca229f9d5efb17a80b09b75c9dea4f9462
Rimenes Ribeiro <eu@rimenes.net>
2013-11-17 11:57:47 -0200

    Update node-webkit to version 0.8.0

--- Casks/node-webkit.rb
+++ Casks/node-webkit.rb
@@ -1,7 +1,7 @@
 class NodeWebkit < Cask
-  url 'https://s3.amazonaws.com/node-webkit/v0.7.4/node-webkit-v0.7.4-osx-ia32.zip'
+  url 'https://s3.amazonaws.com/node-webkit/v0.8.0/node-webkit-v0.8.0-osx-ia32.zip'
   homepage 'https://github.com/rogerwang/node-webkit'
-  version '0.7.4'
-  sha1 '601bbf27d6f6298841104c276bd18c7378efdacc'
+  version '0.8.0'
+  sha1 'a9bce8ae38815efc436aaaac4ebd15d9c015f9a2'
   link 'node-webkit.app'
 end
