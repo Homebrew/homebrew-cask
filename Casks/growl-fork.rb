6aef72d0a689f692caa623b9ae252ccbbd513e3f
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-17 22:05:57 +0800

    New Grow-Fork v 1.2.2f1 Cask

---
+++ Casks/growl-fork.rb
@@ -0,0 +1,7 @@
+class GrowlFork < Cask
+  url 'https://www.macupdate.com/download/41038/Growl-1.2.2f1.dmg'
+  homepage 'https://www.macupdate.com/app/mac/41038/growl-fork'
+  version '1.2.2f1'
+  sha1 '34ee60ee276cc85f144667a25d70999c308afa36'
+  install 'Growl.pkg'
+end
