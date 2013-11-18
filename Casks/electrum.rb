206b0b7415b10c8496abcc7890b533c47eec2b75
Michael Ford <fanquake@gmail.com>
2013-11-18 12:27:22 +0800

    Electrum 1.9.4

---
+++ Casks/electrum.rb
@@ -0,0 +1,7 @@
+class Electrum < Cask
+  url 'http://download.electrum.org/electrum-1.9.4.dmg'
+  homepage 'http://electrum.org/'
+  version '1.9.4'
+  sha1 'e67b3f87ad6edb7baab18d50419cd2377c297d91'
+  link 'Electrum.app'
+end
