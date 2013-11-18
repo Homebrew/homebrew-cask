a9978421be911a956f3317d0f32d3388bdcf5ec6
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-18 22:28:56 +0800

    Remove version and checksum, as requested by @vitorgalvao

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -1,8 +1,8 @@
 class Onyx < Cask
   url 'http://joel.barriere.pagesperso-orange.fr/dl/109/OnyX.dmg'
   homepage 'http://www.titanium.free.fr/downloadonyx.php'
-  version '2.8.1'
-  sha1 '68060332fa87fb21ebae08570e6117f641a3fd59'
+  version 'latest'
+  no_checksum
   link 'OnyX.app'
   
   def caveats; <<-EOS.undent
