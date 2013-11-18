69235c30877ee16e532b57e75cd1e0f1ab3bedd4
Michael Ford <fanquake@users.noreply.github.com>
2013-11-18 10:08:32 +0800

    MySQL Workbench 6.0.8

--- Casks/mysql-workbench.rb
+++ Casks/mysql-workbench.rb
@@ -1,7 +1,7 @@
 class MysqlWorkbench < Cask
-  url 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.0.7.1-osx-i686.dmg'
+  url 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.0.8-osx-i686.dmg'
   homepage 'http://www.mysql.com/products/workbench'
-  version '6.0.7.1'
-  sha1 '9989e1ece9459419b965eeb3912be0394b5570df'
+  version '6.0.8'
+  sha1 '6bae0feef64e832b65d426c45634d1201d3a9789'
   link 'MySQLWorkbench.app'
 end
