a0af89e7827c22defb101327c15a7caaa97b8b56
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-18 17:14:59 +0800

    Update OnyX to OS X Mavericks version

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -1,7 +1,12 @@
 class Onyx < Cask
-  url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
+  url 'http://joel.barriere.pagesperso-orange.fr/dl/109/OnyX.dmg'
   homepage 'http://www.titanium.free.fr/downloadonyx.php'
-  version '2.7.4'
-  sha1 'dadd7f82fd972e73a7a8e360583b8d4d7963482c'
+  version '2.8.1'
+  sha1 '68060332fa87fb21ebae08570e6117f641a3fd59'
   link 'OnyX.app'
+  
+  def caveats; <<-EOS.undent
+    This version of OnyX is for OS X Mavericks only. If you are using other versions of  OS X, please run 'brew tap caskroom/versions' and install onyx-mountainlion / onyx-lion / onyx-snowleopard
+    EOS
+  end
 end
