94e0a0ad5a624a9d8b73dc8b0afd523c9182bdcd
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-18 14:31:24 +0800

    Add caveat for Cocktail: for OS X Mavericks only; instructions to tap from caskroom-versions

--- Casks/cocktail.rb
+++ Casks/cocktail.rb
@@ -4,4 +4,11 @@ class Cocktail < Cask
   version 'latest'
   no_checksum
   link 'Cocktail.app'
+  
+  def caveats; <<-EOS.undent
+    This version of Cocktail is for OS X Mavericks only. If you are using other versions of 
+    OS X, please run 'brew tap caskroom/versions' and install cocktail-mountainlion /
+    cocktail-lion / cocktail-snowleopard
+    EOS
+  end
 end
