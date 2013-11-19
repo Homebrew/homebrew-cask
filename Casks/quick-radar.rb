10df296cf4efae1ad8bef24644bd445beef6e0fb
Rimenes Ribeiro <eu@rimenes.net>
2013-11-19 09:05:18 -0200

    Add Quick Radar

---
+++ Casks/quick-radar.rb
@@ -0,0 +1,7 @@
+class QuickRadar < Cask
+  url 'http://www.quickradar.com/0.8.zip'
+  homepage 'http://www.quickradar.com/'
+  version '0.8'
+  sha1 '55053143bedf9d75ae3fc15f67184fb179503ea9'
+  link 'QuickRadar.app'
+end
