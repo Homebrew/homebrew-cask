b7ccac96a0eb5455fff1c3663a68be0d613fbb56
Bobby DeSimone <desimone@users.noreply.github.com>
2013-11-19 23:58:03 -0500

    Update Tor Browser Bundle to Version 2.3.25-15 - OS X (10.6+)

--- Casks/tor-browser.rb
+++ Casks/tor-browser.rb
@@ -1,7 +1,7 @@
 class TorBrowser < Cask
-  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.3.25-14-osx-x86_64-en-US.zip'
+  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.3.25-15-osx-x86_64-en-US.zip'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   version '2.3.25-14'
-  sha1 '1d54067d657102ea525f9176e8ebb937bad30985'
+  sha1 'e1316337819f02bdad78db7d60caa4423dea75e4'
   link 'TorBrowser_en-US.app'
 end
