da122112b8cf951039744ba3a719276381e1c6c4
Bobby DeSimone <desimone@users.noreply.github.com>
2013-11-20 00:04:13 -0500

    Update tor-browser.rb

--- Casks/tor-browser.rb
+++ Casks/tor-browser.rb
@@ -2,6 +2,6 @@ class TorBrowser < Cask
   url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.3.25-15-osx-x86_64-en-US.zip'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   version '2.3.25-14'
-  sha1 'e1316337819f02bdad78db7d60caa4423dea75e4'
+  sha1 '2cbd2dc7426d88e1f29d2a8e5b810938e5b98df2'
   link 'TorBrowser_en-US.app'
 end
