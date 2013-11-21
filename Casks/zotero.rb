b43f5e8a28cbc27c691419cc44e61d0b6d501a7b
Rimenes Ribeiro <eu@rimenes.net>
2013-11-21 18:55:28 -0200

    Update Zotero to version 4.0.16

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,7 +1,7 @@
 class Zotero < Cask
-  url 'http://download.zotero.org/standalone/4.0.15/Zotero-4.0.15.dmg'
+  url 'http://download.zotero.org/standalone/4.0.16/Zotero-4.0.16.dmg'
   homepage 'http://www.zotero.org/'
-  version '4.0.15'
-  sha1 'fcc66cc47a14afcb10475533b7cebcf0548d5f36'
+  version '4.0.16'
+  sha1 '461752e20adb669471063b5ea1c573037d73ada3'
   link 'Zotero.app'
 end
