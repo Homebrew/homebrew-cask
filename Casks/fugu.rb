c8132ce1ce24fa0dc9bf3e7474e9154d1573afe3
Roland Walker <walker@pobox.com>
2013-11-21 08:20:14 -0500

    Add Fugu.app
    This commit contains Fugu.app version 1.2.1pre1.  Although the version
    string contains "pre1", this is the recommended build for Intel Mac
    hardware.

---
+++ Casks/fugu.rb
@@ -0,0 +1,7 @@
+class Fugu < Cask
+  url 'http://downloads.sourceforge.net/project/fugussh/Unstable/fugu-1.2.1pre1/Fugu-1.2.1pre1.zip'
+  homepage 'http://rsug.itd.umich.edu/software/fugu/'
+  version '1.2.1pre1'
+  sha1 '43b50d1fec476a26f9e015f0d6808ab8c15c11d4'
+  link 'Fugu.app'
+end
