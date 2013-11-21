6360ae46e0b293ab1a78aa5cfd0026c79198ba5d
Roland Walker <walker@pobox.com>
2013-11-21 09:52:22 -0500

    Add Google Refine.app
    This commit contains Google Refine.app version 2.5

---
+++ Casks/google-refine.rb
@@ -0,0 +1,7 @@
+class GoogleRefine < Cask
+  url 'https://github.com/OpenRefine/OpenRefine/releases/download/2.5/google-refine-2.5-r2407.dmg'
+  homepage 'http://openrefine.org/'
+  version '2.5-r2407'
+  sha1 'f0e50b07ca8ce2aa2241cc1b14c42bad3adaa433'
+  link 'Google Refine.app'
+end
