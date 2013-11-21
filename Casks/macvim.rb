8bb588bc366ace3862885618692799d97cc0251f
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-21 14:14:19 +0800

    Update MacVim Cask. - Add version for Lion - Add error if using unsupported OS X version

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -2,9 +2,14 @@ class Macvim < Cask
   if MacOS.version == :mavericks
     url 'https://github.com/b4winckler/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Mavericks.tbz'
     sha1 'dc983ae1e3ffae1c80f06eea9eacee49019a0c8a'
-  else
+  else if MacOS.version == :mountainlion
     url 'https://github.com/eee19/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Mountain-Lion.tbz'
     sha1 'bc3b899634d73908ddba5afd9b9a74778988aec3'
+  else if MacOS.version == :lion
+    url 'https://github.com/douglasdrumond/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Lion.tbz'
+    sha1 '1b86aa04d7149b13d27f6a4c82c7ed7cd9333dcf'
+  else
+    onoe "No available MacVim cask for #{MacOS.version}."
   end
   homepage 'http://code.google.com/p/macvim/'
   version '7.4-72'
