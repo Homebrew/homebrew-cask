0dd8b7b8e2f9950b23802829431ade345ef3f92c
Roland Walker <walker@pobox.com>
2013-11-21 08:36:38 -0500

    correct Flip4Mac homepage

--- Casks/flip4mac.rb
+++ Casks/flip4mac.rb
@@ -1,6 +1,6 @@
 class Flip4mac < Cask
   url 'http://dvd-mac.com/download/Flip4Mac_3.2.0.16.dmg'
-  homepage 'http://dvd-mac.com/flip4mac.html'
+  homepage 'http://www.telestream.net/flip4mac/'
   version '3.2.0.16'
   sha1 'b4d24b715848a6a4723c21555a9787a5d541a8a8'
   install 'Flip4Mac.pkg'
