a20b1417650de5d99a3e04e47f92dd2c38728da0
Vítor Galvão <info@vitorgalvao.com>
2013-11-21 16:32:04 +0000

    corrected platypus link

--- Casks/platypus.rb
+++ Casks/platypus.rb
@@ -1,7 +1,7 @@
 class Platypus < Cask
   url 'http://sveinbjorn.org/files/software/platypus.zip'
   homepage 'http://sveinbjorn.org/platypus'
-  version 'latest'
-  no_checksum
-  link 'Platypus.app'
+  version '4.8'
+  sha1 '3c019ce84f0868f856c4a37db7af14ab5a456416'
+  link 'Platypus-4.8/Platypus.app'
 end
