1e469b46e76081be905616a8dde772f17b6407e3
phette23 <phette23@gmail.com>
2013-11-21 18:04:04 -0500

    add Sophos Anti-Virus.app

---
+++ Casks/sophos-antivirus.rb
@@ -0,0 +1,7 @@
+class SophosAntivirus < Cask
+  url 'http://downloads.sophos.com/home-edition/savosx_90_he.zip'
+  homepage 'http://www.sophos.com/en-us/products/free-tools/sophos-antivirus-for-mac-home-edition.aspx/'
+  version '9.0'
+  sha1 '622bcede6d6a1d6ac2b2285aa9d57b7c5bd8b842'
+  link 'Sophos Anti-Virus Home Edition.app'
+end
