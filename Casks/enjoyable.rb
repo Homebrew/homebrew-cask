7cb7ffff8508ecb8c3a49313f7e792a5ee2d7b97
Vítor Galvão <info@vitorgalvao.com>
2013-11-21 01:01:03 +0000

    added enjoyable

---
+++ Casks/enjoyable.rb
@@ -0,0 +1,7 @@
+class Enjoyable < Cask
+  url 'http://yukkurigames.com/enjoyable/Enjoyable-1.1.zip'
+  homepage 'http://yukkurigames.com/enjoyable/'
+  version '1.1'
+  sha1 'd2e1ee559a84cce8d76b30fe210d9447207c76d3'
+  link 'Enjoyable.app'
+end
