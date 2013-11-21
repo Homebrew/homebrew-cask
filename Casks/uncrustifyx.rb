11c968bf6f7f1b984af6796fa14a24d5e874fae3
Roland Walker <walker@pobox.com>
2013-11-21 08:30:08 -0500

    Standardize quoting.

--- Casks/uncrustifyx.rb
+++ Casks/uncrustifyx.rb
@@ -3,5 +3,5 @@ class Uncrustifyx < Cask
   homepage 'https://github.com/ryanmaxwell/UncrustifyX'
   version '0.4.3'
   sha1 '10dea3d913dfc1a0c39404e244d0f0225a2ba880'
-  link "UncrustifyX.app"
+  link 'UncrustifyX.app'
 end
