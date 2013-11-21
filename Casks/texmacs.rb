573ee0c70c852f734fe15847f06b0ed5c02ea18c
Roland Walker <walker@pobox.com>
2013-11-21 13:17:44 -0500

    Add TeXmacs
    This commit includes TeXmacs version 1.0.7.21

---
+++ Casks/texmacs.rb
@@ -0,0 +1,7 @@
+class Texmacs < Cask
+  url 'http://www.texmacs.org/Download/ftp/tmftp/macos/TeXmacs-1.0.7.21.dmg'
+  homepage 'http://www.texmacs.org/'
+  version '1.0.7.21'
+  sha1 '372ad88359d5e19a4fa850529fa658f24f2f2fba'
+  link 'TeXmacs-1.0.7.21.app'
+end
