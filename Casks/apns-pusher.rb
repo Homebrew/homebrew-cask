c4ff2aba73697508fe8188969a268c496e3c8df3
Rimenes Ribeiro <eu@rimenes.net>
2013-11-22 12:34:17 -0200

    Add APNS Pusher

---
+++ Casks/apns-pusher.rb
@@ -0,0 +1,7 @@
+class ApnsPusher < Cask
+  url 'https://github.com/blommegard/APNS-Pusher/releases/download/2.2.1/APNS.Pusher.app.zip'
+  homepage 'https://github.com/blommegard/APNS-Pusher'
+  version '2.2.1'
+  sha1 'f8c68ef99e94d8afaf46b0aa4d9001421e48e874'
+  link 'APNS Pusher.app'
+end
