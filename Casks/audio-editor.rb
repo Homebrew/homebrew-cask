7aaf10a9fd44a660abf920c6f557a8d972920723
Roland Walker <walker@pobox.com>
2013-11-22 14:00:16 -0500

    add Audio Editor.app

---
+++ Casks/audio-editor.rb
@@ -0,0 +1,7 @@
+class AudioEditor < Cask
+  url 'http://www.macsome.com/AudioEditor.dmg'
+  homepage 'http://www.macsome.com/audio-editor-mac/index.html'
+  version 'latest'
+  no_checksum
+  link 'Audio Editor.app'
+end
