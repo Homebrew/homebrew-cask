4e1360b09a70c7a8a803615d7e640a5766ae1c06
James Hart <hjhart@gmail.com>
2013-11-22 01:55:02 -0800

    added support for wunderlist 2.3.2

---
+++ Casks/wunderlist.rb
@@ -0,0 +1,7 @@
+class Wunderlist < Cask
+  url 'https://www.macupdate.com/download/35862/Wunderlist.zip'
+  homepage 'https://www.wunderlist.com/en/'
+  version '2.3.2'
+  sha1 '4e151db4b914fadd393660b638810f9019958aa5'
+  link 'Wunderlist.app'
+end
