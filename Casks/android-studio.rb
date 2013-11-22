eb6fcad82f88d767242037d144d6da09304edec4
Paspartout <paspartout@fogglabs.de>
2013-11-22 16:27:18 +0100

    Updated Android Studio

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,7 +1,7 @@
 class AndroidStudio < Cask
-  url 'http://dl.google.com/dl/android/studio/ide-zips/0.3.5/android-studio-ide-132.910074-mac.zip'
+  url 'http://dl.google.com/dl/android/studio/ide-zips/0.3.6/android-studio-ide-132.917931-mac.zip'
   homepage 'https://developer.android.com/sdk/installing/studio.html'
-  version '0.3.5 build-132.910074'
-  sha1 '507a34d38f633010133542ea96df821462788aee'
+  version '0.3.6 build-132.917931'
+  sha1 '93b07f36fd8fb1c5696f148acf72ef9cc8ee88ed'
   link 'Android Studio.app'
 end
