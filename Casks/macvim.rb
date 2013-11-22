b66f7c9441d36cef3e30144e2b3da60e148bd3a4
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-22 11:01:31 +0800

    Fix elseif syntax

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -2,10 +2,10 @@ class Macvim < Cask
   if MacOS.version == :mavericks
     url 'https://github.com/b4winckler/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Mavericks.tbz'
     sha1 'dc983ae1e3ffae1c80f06eea9eacee49019a0c8a'
-  else if MacOS.version == :mountainlion
+  elseif MacOS.version == :mountainlion
     url 'https://github.com/eee19/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Mountain-Lion.tbz'
     sha1 'bc3b899634d73908ddba5afd9b9a74778988aec3'
-  else if MacOS.version == :lion
+  elseif MacOS.version == :lion
     url 'https://github.com/douglasdrumond/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Lion.tbz'
     sha1 '1b86aa04d7149b13d27f6a4c82c7ed7cd9333dcf'
   else
