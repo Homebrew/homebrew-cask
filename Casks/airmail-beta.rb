da3e5cc02ad5dfc92311fa3af1d6377b80398ccb
Joshua Guy <jglenes@gmail.com>
2013-11-23 11:57:11 -0500

    Airmail Beta v1.3.0 (217)

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,7 +1,7 @@
 class AirmailBeta < Cask
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   homepage 'http://airmailapp.com/'
-  version '1.3.0 (215)'
-  sha1 '63daa353d5f4473c3184b49506213ebdd3eb7e36'
+  version '1.3.0 (217)'
+  sha1 '3a4c093eff8ae8e746380ac49e89ff88411cdd33'
   link 'AirMail Beta.app'
 end
