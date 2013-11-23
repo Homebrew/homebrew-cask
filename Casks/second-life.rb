3479f188e27a28aa743e7966fc4dcad168c81f3c
Katharine Berry <katharine@kathar.in>
2013-11-22 20:50:17 -0500

    Add Second Life cask.
    Includes version 3.6.11.283787.

---
+++ Casks/second-life.rb
@@ -0,0 +1,7 @@
+class SecondLife < Cask
+  url 'http://download.cloud.secondlife.com/Viewer_3/SecondLife_3_6_11_283787.dmg'
+  homepage 'http://secondlife.com/'
+  version '3.6.11.283787'
+  sha1 'a90f6b5da1e68f3f60cd3938979a3e9f7c99cc11'
+  link 'Second Life Viewer.app'
+end
