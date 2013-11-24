10ae500ae4a0427f12fd446749edd7ee885f138d
Michael Herb <exherb@4leaf.me>
2013-11-24 23:57:30 +0800

    genymotion v2.0.1

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -1,7 +1,7 @@
 class Genymotion < Cask
-  url 'https://ssl-files.genymotion.com/genymotion/genymotion-1.3.1/genymotion-1.3.1.dmg'
+  url 'https://ssl-files.genymotion.com/genymotion/genymotion-2.0.1/genymotion-2.0.1.dmg'
   homepage 'http://www.genymotion.com/'
-  version '1.3.1'
-  sha1 '92a4f3b25acd6fc777d9cf42da3efdd2d4d589fa'
+  version '2.0.1'
+  sha1 'ce2a8e8a0c3760df4f2a76e368f35c963235189e'
   link 'Genymotion.app', 'Genymotion Shell.app'
 end
