d6b05318f3c548626b7ddc8e0ca6027c79a15a46
Michael Ford <fanquake@users.noreply.github.com>
2013-11-24 11:03:22 +0800

    Cyberduck 4.4.2

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -1,7 +1,7 @@
 class Cyberduck < Cask
-  url 'https://update.cyberduck.ch/Cyberduck-4.4.zip'
-  homepage 'http://cyberduck.ch/'
-  version '4.4.0'
-  sha1 'edd734985c0ad7684759f7d5a07a04ef9fd344dd'
+  url 'https://update.cyberduck.io/Cyberduck-4.4.2.zip'
+  homepage 'http://cyberduck.io/'
+  version '4.4.2'
+  sha1 'ecba8c5ffae0317908d5d2041efb758e7f954263'
   link 'Cyberduck.app'
 end
