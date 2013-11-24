ca6915962fed03bd1bfa2e466b608ec1e3756c17
Rimenes Ribeiro <eu@rimenes.net>
2013-11-24 12:23:19 -0200

    Add Anxiety

---
+++ Casks/anxiety.rb
@@ -0,0 +1,8 @@
+class Anxiety < Cask
+  url 'http://www.anxietyapp.com/Anxiety.zip'
+  homepage 'http://www.anxietyapp.com/'
+  version 'latest'
+  no_checksum
+  nested_container 'Anxiety.dmg'
+  link 'Anxiety.app'
+end
