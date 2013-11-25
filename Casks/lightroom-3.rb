25317be2de06f3966a255e433c37b95e314088ee
James Hart <hjhart@gmail.com>
2013-11-25 15:50:09 -0800

    Added lightroom support!

---
+++ Casks/lightroom-3.rb
@@ -0,0 +1,7 @@
+class Lightroom3 < Cask
+  url 'http://download.adobe.com/pub/adobe/lightroom/mac/3.x/Lightroom_3_LS11_mac_3_3.dmg'
+  homepage 'http://www.adobe.com/support/downloads/thankyou.jsp?ftpID=4927&fileID=4588'
+  version '3.3'
+  sha1 'd2a44dfdf49b28051c28ce2f80a06832ff2afa51'
+  install 'Adobe Lightroom 3.pkg'
+end
