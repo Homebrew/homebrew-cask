7d90729ac5d9a2303cfcb7968e9962e30ca7bf1a
Michael Ford <fanquake@users.noreply.github.com>
2013-11-25 18:13:07 +0800

    Sigil 0.7.4

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -1,7 +1,7 @@
 class Sigil < Cask
-  url 'https://sigil.googlecode.com/files/Sigil-0.7.2-Mac-Package.dmg'
+  url 'https://sigil.googlecode.com/files/Sigil-0.7.4-Mac-Package.dmg'
   homepage 'http://code.google.com/p/sigil/'
-  version '0.7.2'
-  sha1 '96ef5bc726d633c1acdb08fe4adc2dfb0c8250d7'
+  version '0.7.4'
+  sha1 '7e59f8a4d31a4b444c9d4f3baf859799cb2e351e'
   link 'Sigil.app'
 end
