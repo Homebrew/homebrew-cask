1fe0b331090d7b9abc313df027c3fe88c7cda75b
Michael Ford <fanquake@users.noreply.github.com>
2013-11-25 18:27:41 +0800

    Thunderbird 24.1.1

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,7 +1,7 @@
 class Thunderbird < Cask
-  url 'http://download-installer.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/24.1.0/mac/en-US/Thunderbird%2024.1.0.dmg'
+  url 'http://download-installer.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/24.1.1/mac/en-US/Thunderbird%2024.1.1.dmg'
   homepage 'http://www.mozilla.org/en-US/thunderbird/'
-  version '24.1.0'
-  sha1 '04968b3ad034576fe17dfa923b5fa9fb7bb51877'
+  version '24.1.1'
+  sha1 '50fc199ae547e7bef449e4cc615ff13cbc6586ac'
   link 'Thunderbird.app'
 end
