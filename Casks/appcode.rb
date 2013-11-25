d64e91f284fc78c71ea2509bb7753768ee3b82e8
Michael Ford <fanquake@users.noreply.github.com>
2013-11-25 17:35:32 +0800

    AppCode 2.5.2

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,7 +1,7 @@
 class Appcode < Cask
-  url 'http://download.jetbrains.com/objc/AppCode-2.5.1.dmg'
+  url 'http://download.jetbrains.com/objc/AppCode-2.5.2.dmg'
   homepage 'http://www.jetbrains.com/objc/'
-  version '2.5.1'
-  sha1 'cb3e9b8e1517bc68ab39814692a73f387d9e4ae0'
+  version '2.5.2'
+  sha1 '550031231c1ff0e7ab216fdfacebad360543f72c'
   link 'AppCode.app'
 end
