d7273c612013cd8f4ae8952d35eea6aa1d715cd1
Roland Walker <walker@pobox.com>
2013-11-26 08:50:56 -0500

    add font family DejaVu

---
+++ Casks/font-dejavu.rb
@@ -0,0 +1,27 @@
+class FontDejavu < Cask
+  url 'http://downloads.sourceforge.net/sourceforge/dejavu/dejavu-fonts-ttf-2.34.zip'
+  homepage 'http://dejavu-fonts.org/wiki/Main_Page'
+  version '2.34'
+  sha1 '3417fdcb2bc9a2fb02aff9b8ee8eb236e678ead1'
+  font 'dejavu-fonts-ttf-2.34/ttf/DejaVuSans-Bold.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSans-BoldOblique.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSans-ExtraLight.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSans-Oblique.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSans.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSansCondensed-Bold.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSansCondensed-BoldOblique.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSansCondensed-Oblique.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSansCondensed.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSansMono-Bold.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSansMono-BoldOblique.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSansMono-Oblique.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSansMono.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSerif-Bold.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSerif-BoldItalic.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSerif-Italic.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSerif.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSerifCondensed-Bold.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSerifCondensed-BoldItalic.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSerifCondensed-Italic.ttf',
+       'dejavu-fonts-ttf-2.34/ttf/DejaVuSerifCondensed.ttf'
+end
