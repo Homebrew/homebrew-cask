50e458d21462437788cc0702328338392152ebf0
Rimenes Ribeiro <eu@rimenes.net>
2013-11-26 22:12:57 -0200

    Update Launch Control to version 1.10.3

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,7 +1,7 @@
 class Launchcontrol < Cask
-  url 'http://www.soma-zone.com/download/files/LaunchControl_1.10.2.tar.bz2'
+  url 'http://www.soma-zone.com/download/files/LaunchControl_1.10.3.tar.bz2'
   homepage 'http://www.soma-zone.com/LaunchControl/'
-  version '1.10.2'
-  sha1 'adf52faa3e2bb4d70e778d1da16bf30ace2cbee3'
+  version '1.10.3'
+  sha1 '063428c17614434b5b07174782845a3f58f2fe1d'
   link 'LaunchControl.app'
 end
