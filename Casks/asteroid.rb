8edff1adfb80face063268e8af805ed3a774e1c3
David Baumgold <david@davidbaumgold.com>
2013-11-27 15:50:54 -0500

    Asteroid: BTC and LTC miner

---
+++ Casks/asteroid.rb
@@ -0,0 +1,7 @@
+class Asteroid < Cask
+  url "http://www.asteroidapp.com/downloader.php?Ver=0.9.4"
+  homepage 'http://www.asteroidapp.com/'
+  version '0.9.4'
+  sha1 '4e714da69457f99d8101ef3ee7ba7f17405fc276'
+  link 'Asteroid.app'
+end
