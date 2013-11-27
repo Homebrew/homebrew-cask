a3e2828bc90bceaaa5f444de40fee6cd6482ea4a
Dave Caunt <dcaunt@gmail.com>
2013-11-27 15:42:05 +0000

    Added Provisioning QuickLook plugin 1.0.4

---
+++ Casks/provisioning.rb
@@ -0,0 +1,7 @@
+class Provisioning < Cask
+  url 'https://github.com/chockenberry/Provisioning/releases/download/1.0.4/Provisioning-1.0.4.zip'
+  homepage 'https://github.com/chockenberry/Provisioning'
+  version '1.0.4'
+  sha256 '554590ab653776babbc453892653d052f1ec09c6473fc91f4a071f1a7a953144'
+  qlplugin 'Provisioning-1.0.4/Provisioning.qlgenerator'
+end
