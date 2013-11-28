1a2db129a59072085f39e12d82b8781bd88573f0
Fernando Paredes <nano@fdp.io>
2013-11-28 09:54:59 -0800

    Carbon Copy Cloner 3.5.4
    The old url was limited to downloading a webpage when using `curl`

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -1,7 +1,7 @@
 class CarbonCopyCloner < Cask
-  url 'http://www.bombich.com/software/download_ccc.php'
+  url 'http://files.bombich.com/ccc-3.5.4.zip'
   homepage 'http://bombich.com/'
-  version 'latest'
-  no_checksum
+  version '3.5.4'
+  sha1 'e24115cb03fd902d5465785f6fc35675a06f6f45'
   link 'Carbon Copy Cloner.app'
 end
