c7930054f2d975c7ea9b9b8dc79d0f297f6139dc
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-28 16:54:26 +0800

    lazarus 1.0.14

---
+++ Casks/lazarus.rb
@@ -0,0 +1,7 @@
+class Lazarus < Cask
+  url 'http://downloads.sourceforge.net/lazarus/lazarus-1.0.14-20131116-i386-macosx.dmg'
+  homepage 'http://lazarus.freepascal.org/'
+  version '1.0.14'
+  sha1 '01bf5a32ef937f9893732ad6d2bd2f888ac6fd9a'
+  install 'lazarus.pkg'
+end
