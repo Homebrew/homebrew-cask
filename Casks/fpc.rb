17a053dead1131dcd4c0db044b4e755a0a695348
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-28 17:53:10 +0800

    fpc 2.6.2

---
+++ Casks/fpc.rb
@@ -0,0 +1,7 @@
+class Fpc < Cask
+  url 'http://downloads.sourceforge.net/freepascal/fpc-2.6.2.intel-macosx.dmg'
+  homepage 'http://www.freepascal.org/'
+  version '2.6.2'
+  sha1 'b784f338c049c83eadb988f2a0dab170f9cc5c76'
+  install 'fpc-2.6.2rc1.intel-macosx.pkg'
+end
