98bb0daf63872fc8a85473a1b90299bee2096ac5
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-29 22:54:09 +0800

    Pref-Setter 2.0

---
+++ Casks/pref-setter.rb
@@ -0,0 +1,7 @@
+class PrefSetter < Cask
+  url 'http://www.nightproductions.net/downloads/prefsetter.zip'
+  homepage 'http://www.nightproductions.net/prefsetter.html'
+  version '2.0'
+  sha1 '62d4ca4e4c2eea1775f00bfe76ab2c0ab3be648f'
+  link 'Pref Setter/Pref Setter.app'
+end
