de027604371b08494622a1f44ccd5df21ae5dd10
Michael Ford <fanquake@users.noreply.github.com>
2013-11-29 22:12:38 +0800

    Electrum 1.9.5

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,7 +1,7 @@
 class Electrum < Cask
-  url 'http://download.electrum.org/electrum-1.9.4.dmg'
+  url 'http://download.electrum.org/electrum-1.9.5.dmg'
   homepage 'http://electrum.org/'
-  version '1.9.4'
-  sha1 'e67b3f87ad6edb7baab18d50419cd2377c297d91'
+  version '1.9.5'
+  sha1 '0815ee20c3a94b998a735577f6486f314a155b3c'
   link 'Electrum.app'
 end
