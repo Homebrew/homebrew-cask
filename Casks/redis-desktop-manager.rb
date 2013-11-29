6b85daa543269bd257fb61ec53de8737f5954dd1
Alexey <anakros.symbian@gmail.com>
2013-11-29 15:17:17 +0700

    Add Redis Desktop Manager 0.6.2

---
+++ Casks/redis-desktop-manager.rb
@@ -0,0 +1,7 @@
+class RedisDesktopManager < Cask
+  url 'https://github.com/uglide/RedisDesktopManager/releases/download/0.6.2/rdm-0.6.2.dmg'
+  homepage 'http://redisdesktop.com'
+  version '0.6.2'
+  sha1 '9b76691d37558e1c94ffd446279b90e68c3b60b1'
+  link 'rdm.app'
+end
