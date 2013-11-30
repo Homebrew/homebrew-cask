839b8984db4e3ddf36a5cf16ceea572330dc226e
Pavel Sakun <pavel.sakun@gmail.com>
2013-11-30 22:33:15 +0300

    Intellij idea casks renamed to include full product name

--- Casks/intellij-idea-community.rb
+++ Casks/intellij-idea-community.rb
@@ -1,4 +1,4 @@
-class IntellijCommunity < Cask
+class IntellijIdeaCommunity < Cask
   url 'http://download.jetbrains.com/idea/ideaIC-12.1.6.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
   version '12.1.6'
