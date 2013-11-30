c0afbfb80eb433f6cdd42586ccb8ec414415731a
Vítor Galvão <info@vitorgalvao.com>
2013-11-30 14:38:02 +0000

    updated to always use latest version

--- Casks/xbox360-controller-driver.rb
+++ Casks/xbox360-controller-driver.rb
@@ -1,8 +1,8 @@
 class Xbox360ControllerDriver < Cask
-  url 'http://files.tattiebogle.net/360/360ControllerInstall.0.12.dmg'
+  url 'http://files.tattiebogle.net/360/360ControllerInstall.dmg'
   homepage 'http://tattiebogle.net/index.php/ProjectRoot/Xbox360Controller/OsxDriver'
-  version '0.12'
-  sha1 '933bd56e077a68c6517decd2758638c69452cd26'
+  version 'latest'
+  no_checksum
   install 'Install360Controller.pkg'
   uninstall :pkgutil => 'com.mice.pkg.Xbox360controller'
 end
