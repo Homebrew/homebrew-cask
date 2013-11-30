feba894e235443fedb419e70b528274832aea1bb
Chulki Lee <chulki@asperasoft.com>
2013-11-30 14:00:06 -0800

    virtualbox-4.3.4-91027

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,8 +1,8 @@
 class Virtualbox < Cask
-  url 'http://download.virtualbox.org/virtualbox/4.3.2/VirtualBox-4.3.2-90405-OSX.dmg'
+  url 'http://download.virtualbox.org/virtualbox/4.3.4/VirtualBox-4.3.4-91027-OSX.dmg'
   homepage 'http://www.virtualbox.org'
-  version '4.3.2-90405'
-  sha1 '0c5816dada6a106a1eae4bde834f3e1863d2611d'
+  version '4.3.4-91027'
+  sha1 '3583f2198940ff6461a526305d3c49a0638845bc'
   install 'VirtualBox.pkg'
   uninstall :script => 'VirtualBox_Uninstall.tool', :args => %w[--unattended]
 end
