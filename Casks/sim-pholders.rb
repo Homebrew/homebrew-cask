2457b2c16bfbfebcf2dd0a0d5d561231787d5c86
Michael Herb <exherb@4leaf.me>
2013-12-01 21:22:14 +0800

    SimPholders 1.5.0

---
+++ Casks/sim-pholders.rb
@@ -0,0 +1,7 @@
+class SimPholders < Cask
+  url 'http://simpholders.com/site/assets/files/1007/simpholders-1_5_0.dmg'
+  homepage 'http://simpholders.com/'
+  version '1.5.0'
+  sha1 '6ed67eb09973a2d6092bc54825b8effeab0ee533'
+  link 'SimPholders.app'
+end
