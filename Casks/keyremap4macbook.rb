7bd04689b3e060cca42aa65000f1321b35d38d96
Beng Hee Eu <beng@benghee.eu>
2013-12-01 16:15:16 +0800

    KeyRemap4MacBook 9.2.0

--- Casks/keyremap4macbook.rb
+++ Casks/keyremap4macbook.rb
@@ -1,7 +1,7 @@
 class Keyremap4macbook < Cask
-  url 'https://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-9.0.0.dmg'
+  url 'https://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-9.2.0.dmg'
   homepage 'https://pqrs.org/macosx/keyremap4macbook/'
-  version '9.0.0'
-  sha1 'dc01547d60eec94f53d4914b8d0d76d6c75e17c0'
+  version '9.2.0'
+  sha1 'f614c318d2dcf35c5e1e9a85bab58b12238db4de'
   install 'KeyRemap4MacBook.pkg'
 end
