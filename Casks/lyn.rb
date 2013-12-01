a2d479240f63ebc0d0f806fc88a4c1d0eeb87655
Michael Ford <fanquake@users.noreply.github.com>
2013-12-01 15:11:42 +0800

    Lyn 1.3.7

--- Casks/lyn.rb
+++ Casks/lyn.rb
@@ -1,7 +1,7 @@
 class Lyn < Cask
-  url 'http://www.lynapp.com/downloads/Lyn-1.3.4.dmg'
+  url 'http://www.lynapp.com/downloads/Lyn-1.3.7.dmg'
   homepage 'http://www.lynapp.com/'
-  version '1.3.4'
-  sha1 '9d9e3dae26f6236bb930ee0b92eb96e46ea74a7c'
+  version '1.3.7'
+  sha1 'bfd04ebb0304225ed9d86462cccff1eedb72738f'
   link 'Lyn.app'
 end
