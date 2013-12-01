6afadeacf1c5b65e0ce0d8e99d103c32e6e34b45
Don Okuda <don.okuda@gmail.com>
2013-11-30 17:19:57 -0800

    Update Color Oracle cask
    * Use `version 'latest'` scheme based on URL.

--- Casks/color-oracle.rb
+++ Casks/color-oracle.rb
@@ -1,7 +1,7 @@
 class ColorOracle < Cask
   url 'http://colororacle.org/ColorOracleMac.zip'
   homepage 'http://colororacle.org/'
-  version '1.1.4'
-  sha1 'f893b006616ac77c1b1d2dcb3aaef7bc3a0e7348'
+  version 'latest'
+  no_checksum
   link 'Color Oracle.app'
 end
