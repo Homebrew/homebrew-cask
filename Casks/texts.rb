c7e9996c06ad132726b1883513ead3436fc0808a
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-02 18:17:22 +0800

    Add texts 0.17 Cask

---
+++ Casks/texts.rb
@@ -0,0 +1,7 @@
+class Texts < Cask
+  url 'http://www.texts.io/Texts-0.17.dmg'
+  homepage 'http://www.texts.io'
+  version '0.17'
+  sha1 'f9782f922390198e96aa464a099d8e20ef210ba2'
+  link 'Texts.app'
+end
