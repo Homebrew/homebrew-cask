7a9e1fdf75045145357c20b13dd22baaef1bb68e
Casey Watson <watsoncj@gmail.com>
2013-12-02 16:11:43 -0700

    Add cask for ReadyTalk, an audio and web conferencing app.
    add version, fix class name

---
+++ Casks/readytalk.rb
@@ -0,0 +1,7 @@
+class Readytalk < Cask
+  url 'https://core.readytalk.com/ql/bins/QuickLauncherInstall.dmg'
+  homepage 'https://www.readytalk.com/'
+  version 'latest'
+  no_checksum
+  link 'ReadyTalk Quick Launcher.app'
+end
