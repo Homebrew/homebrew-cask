b60f37277e9c210cad73779270cf570966e6ad33
Sean Anderson <sean.martin.anderson@gmail.com>
2013-12-01 22:15:01 -0600

    Added a cask for R-Name
    R-Name (http://www.jacek-dom.net/software/R-Name/) is a handy open
    source file renaming utility.

---
+++ Casks/rname.rb
@@ -0,0 +1,7 @@
+class Rname < Cask
+  url 'http://www.jacek-dom.net/software/R-Name/R-Name.app.zip'
+  homepage 'http://www.jacek-dom.net/software/R-Name/'
+  version 'latest'
+  no_checksum
+  link 'R-Name.app'
+end
