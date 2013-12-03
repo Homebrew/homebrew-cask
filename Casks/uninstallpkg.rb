72398ca3ba56ea47835558e2b699c59d77863240
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-03 14:17:07 +0800

    Add UninstallPKG 1.0b6 Cask

---
+++ Casks/uninstallpkg.rb
@@ -0,0 +1,7 @@
+class Uninstallpkg < Cask
+  url 'http://www.corecode.at/downloads/uninstallpkg_1.0b6.zip'
+  homepage 'http://www.corecode.at/uninstallpkg/'
+  version '1.0b6'
+  sha1 '10452fb8cb58450a4ab8909caef27dbf990d15f4'
+  link 'UninstallPKG.app'
+end
