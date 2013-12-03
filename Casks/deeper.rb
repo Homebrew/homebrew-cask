54e1f534e7b79621437a78fec05dd6c8a653e70a
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-02 21:37:21 -0500

    Add caveat to Deeper

--- Casks/deeper.rb
+++ Casks/deeper.rb
@@ -4,4 +4,8 @@ class Deeper < Cask
   version 'latest'
   no_checksum
   link 'Deeper.app'
+  def caveats; <<-EOS.undent
+    This version of Deeper is for OS X Mavericks only. If you are using other versions of OS X, please run 'brew tap caskroom/versions' and install deeper-mountainlion / deeper-lion / deeper-snowleopard
+    EOS
+  end
 end
