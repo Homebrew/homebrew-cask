e5c329e92a886966199bb7770d4d8b76da13922c
jimmynotjim <jimmynotjim@me.com>
2013-12-02 23:00:07 -0500

    Fixed no_checksum and added uninstall
    Tested and verified package is removed

--- Casks/ormr.rb
+++ Casks/ormr.rb
@@ -2,6 +2,7 @@ class Ormr < Cask
   url 'http://getormr.com/dn/Ormr.pkg'
   homepage 'http://getormr.com/home/'
   version 'latest'
-  sha1 no_checksum
+  no_checksum
   install 'Ormr.pkg'
+  uninstall :pkgutil => 'com.sadcatsoft.ormr.Ormr.pkg'
 end
