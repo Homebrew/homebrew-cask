5ceb82815d622ac22703cbbbfebfd409e38b48b8
jimmynotjim <jimmynotjim@me.com>
2013-12-02 22:38:17 -0500

    Changed version to 'latest' and sha1 to 'no_checksum'

--- Casks/ormr.rb
+++ Casks/ormr.rb
@@ -1,7 +1,7 @@
 class Ormr < Cask
   url 'http://getormr.com/dn/Ormr.pkg'
   homepage 'http://getormr.com/home/'
-  version '0.8.444'
-  sha1 '7332b6778933d1f906f040edebfd0bf4ed0a5f2d'
+  version 'latest'
+  sha1 no_checksum
   install 'Ormr.pkg'
 end
