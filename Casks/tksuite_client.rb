d8108cd96ef2f21a7bd09d187f9cbdc2b03eb318
Andreas Boeckler <abo@netlands.de>
2013-12-04 09:52:23 +0100

    TK-Suite-Client Cask

---
+++ Casks/tksuite_client.rb
@@ -0,0 +1,7 @@
+class Tksuite_client < Cask
+  url 'ftp://ftp.agfeo.de/pub/software/TK-Suite-Client_intel32_agfeo_4.4.13.dmg'
+  homepage 'http://agfeo.de/agfeo_web/hp3.nsf/lu/2064'
+  version '4.4.13'
+  sha1 '0a79b962788e54f8527532fccda403d9b5c09ffd'
+  link 'TK-Suite-Client.app'
+end
