44068c56748f31472e56f36fc77f75db60732191
sidapa <me@adriantumusok.com>
2013-12-04 22:25:50 -0800

    fix SHA1 mismatch for version 3.5.4

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -2,6 +2,6 @@ class CarbonCopyCloner < Cask
   url 'http://files.bombich.com/ccc-3.5.4.zip'
   homepage 'http://bombich.com/'
   version '3.5.4'
-  sha1 'e24115cb03fd902d5465785f6fc35675a06f6f45'
+  sha1 'd586a1e3bd745ad6955fc8242a078e7b059fa356'
   link 'Carbon Copy Cloner.app'
 end
