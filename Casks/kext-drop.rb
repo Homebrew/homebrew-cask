611c70c826e265e39b1b6d80066ce4d3308cca5c
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-05 13:09:08 +0800

    Add Kext Drop 1.1 Cask

---
+++ Casks/kext-drop.rb
@@ -0,0 +1,7 @@
+class KextDrop < Cask
+  url 'http://www.groths.org/kextdrop/KextDrop.dmg'
+  homepage 'http://www.groths.org/software/kextdrop/'
+  version 'latest'
+  no_checksum
+  link 'Kext Drop.app'
+end
