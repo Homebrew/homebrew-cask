8d22a083f0576d8da6c35d9772572ba6fbc9d9c1
Paul Hinze <paul.t.hinze@gmail.com>
2013-12-05 20:16:49 -0600

    add eye-fi cask
    thanks to @leoj3n for this
    
    closes #1381

---
+++ Casks/eye-fi.rb
@@ -0,0 +1,8 @@
+class EyeFi < Cask
+  url 'http://d2mje64xs0b9gu.cloudfront.net/3.4.35/Eye-Fi.dmg'
+  homepage 'http://support.eye.fi/downloads'
+  version '3.4.35'
+  sha1 '21841d2679ca7f92193687417d2f68819c026a64'
+  install 'Eye-Fi.mpkg'
+  uninstall :pkgutil => 'fi.eye.*'
+end
