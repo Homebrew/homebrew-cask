59c13c1bb677070d86c763f7ac93539da429a78e
Tony Cundari <tcundari@popcap.com>
2013-12-06 10:25:17 -0800

    Update PyCharm Community and Pro to 3.0.2

--- Casks/pycharm-pro.rb
+++ Casks/pycharm-pro.rb
@@ -1,7 +1,7 @@
 class PycharmPro < Cask
-  url 'http://download-ln.jetbrains.com/python/pycharm-professional-3.0.1.dmg'
+  url 'http://download-ln.jetbrains.com/python/pycharm-professional-3.0.2.dmg'
   homepage 'http://www.jetbrains.com/pycharm/'
-  version '3.0.1'
-  sha1 '9109148b106b8541ec080022691b1f018845780a'
+  version '3.0.2'
+  sha1 '3f116baa7b70435c505941865d818bdcea552d7d'
   link 'PyCharm.app'
 end
