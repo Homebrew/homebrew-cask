a16df95cdaa37107b82b415e9ac464b54c19a5b5
Michael Ford <fanquake@users.noreply.github.com>
2013-12-06 21:48:14 +0800

    Default Folder X 4.6.1

--- Casks/default-folder-x.rb
+++ Casks/default-folder-x.rb
@@ -1,8 +1,8 @@
 class DefaultFolderX < Cask
-  url 'http://stclairsoft.s3.amazonaws.com/DefaultFolderX-4.5.12.dmg'
+  url 'https://stclairsoft.s3.amazonaws.com/DefaultFolderX-4.6.1.dmg'
   homepage 'http://www.stclairsoft.com/DefaultFolderX'
-  version '4.5.12'
-  sha1 '7cee468089c7244ca46e5ceb230a21cf3bf7afa9'
+  version '4.6.1'
+  sha1 'e18306fa47d1d04717b206745de83cfb7e112002'
   link 'Default Folder X Installer.app'
 
   def caveats; <<-EOS.undent
