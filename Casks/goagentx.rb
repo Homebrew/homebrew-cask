ff3b1d2d01f777f2aa66dc46667401d293f16ea5
Michael Ford <fanquake@users.noreply.github.com>
2013-12-06 22:22:34 +0800

    GoAgentX 2.2.2

--- Casks/goagentx.rb
+++ Casks/goagentx.rb
@@ -1,7 +1,7 @@
 class Goagentx < Cask
-  url 'https://goagentx.googlecode.com/files/GoAgentX-v2.2.0.dmg'
-  homepage 'https://github.com/ohdarling/GoAgentX'
+  url 'https://goagentx.googlecode.com/files/GoAgentX-v2.2.2.dmg'
+  homepage 'http://goagentx.com/'
   version '2.2.0'
-  sha1 '5306f86b9ee6f88312bc8c367f7245efc72db69d'
+  sha1 'bdd5eea305950f7af97d317df96921544d1c505a'
   link 'GoAgentX.app'
 end
