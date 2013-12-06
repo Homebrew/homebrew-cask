4a37fea0aef01ab3c7d7d69378a4ba23dc4d8fbf
Michael Ford <fanquake@users.noreply.github.com>
2013-12-07 06:30:41 +0800

    Update Opera Next Homepage
    Fixes #1957

--- Casks/opera-next.rb
+++ Casks/opera-next.rb
@@ -1,6 +1,6 @@
 class OperaNext < Cask
   url 'http://get.geo.opera.com/pub/opera-next/18.0.1284.26/mac/Opera_Next_18.0.1284.26_Setup.dmg'
-  homepage 'http://www.opera.com/developer/next'
+  homepage 'http://www.opera.com/computer/next'
   version '18.0.1284.26'
   sha1 'e61e4e0960a2cb54dd9736b21720eea2b119af46'
   link 'Opera Next.app'
