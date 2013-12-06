5d96879be480f05d8367b665657ef7cc8729bacd
Michael Ford <fanquake@users.noreply.github.com>
2013-12-06 22:37:14 +0800

    Instacast 1.1 (2079)

--- Casks/instacast.rb
+++ Casks/instacast.rb
@@ -1,7 +1,7 @@
 class Instacast < Cask
-  url 'http://assets.vemedio.com/software/instacast/Instacast-1.0.6-2048.dmg'
+  url 'http://assets.vemedio.com/software/instacast/Instacast-1.1-2079.dmg'
   homepage 'http://vemedio.com/products/instacast-mac'
-  version '1.0.6 (2048)'
-  sha1 '355c934e363f478df4cd4c02da70c7ef17b55834'
+  version '1.1 (2079)'
+  sha1 '45b4de8b3b1bb319648003394f84c28a07947a01'
   link 'Instacast.app'
 end
