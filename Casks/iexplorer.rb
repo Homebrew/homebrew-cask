fb0b119ebee771d1339c8c90e21ce4bd787f0048
Michael Ford <fanquake@users.noreply.github.com>
2013-12-06 22:33:50 +0800

    iExplorer 3.2.3.1

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,7 +1,7 @@
 class Iexplorer < Cask
-  url 'http://downloads.macroplant.com/iExplorer-3.2.3.0b.dmg'
+  url 'http://downloads.macroplant.com/iExplorer-3.2.3.1.dmg'
   homepage 'http://www.macroplant.com/'
-  version '3.2.3.0b'
-  sha1 'd49c2e96bee9fea4cbcfe9bd894193d5cc132402'
+  version '3.2.3.1'
+  sha1 'a4c6b3922e8f08cc90bca59c2dd365c90180a7a4'
   link 'iExplorer.app'
 end
