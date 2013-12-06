94878ca6b39fbdc30a5ef9cae46a89f5cdae8027
Michael Ford <fanquake@users.noreply.github.com>
2013-12-06 22:26:58 +0800

    Hazel 3.2.2

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -1,7 +1,7 @@
 class Hazel < Cask
-  url 'https://s3.amazonaws.com/Noodlesoft/Hazel-3.2.1.dmg'
+  url 'https://s3.amazonaws.com/Noodlesoft/Hazel-3.2.2.dmg'
   homepage 'http://www.noodlesoft.com/hazel.php'
-  version '3.2.1'
-  sha1 '483a03f1961647739baabda05ed6b021bd79afac'
+  version '3.2.2'
+  sha1 '721b154bfb564e5a93c33838e4ed46a62b8227ca'
   prefpane 'Hazel.prefPane'
 end
