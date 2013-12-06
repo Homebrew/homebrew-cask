34fa713900b113f373c1c139806def4d637a62b8
Michael Ford <fanquake@users.noreply.github.com>
2013-12-06 22:17:48 +0800

    Fix field order in gasmask Cask

--- Casks/gas-mask.rb
+++ Casks/gas-mask.rb
@@ -1,7 +1,7 @@
 class GasMask < Cask
   url 'http://gmask.clockwise.ee/files/gas_mask_0.7.zip'
-  version '0.7'
   homepage 'http://www.clockwise.ee/gasmask/'
+  version '0.7'
   sha1 '5d0286e594a3fcc21db800105eecd78d5a92f11c'
   link 'Gas Mask.app'
 end
