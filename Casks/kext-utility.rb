9214573b5c0147a5a56895d61626e00924a6ae23
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-06 22:44:05 -0500

    Add Kext Utility

---
+++ Casks/kext-utility.rb
@@ -0,0 +1,7 @@
+class KextUtility < Cask
+  url 'http://cvad-mac.narod.ru/files/Kext_Utility.app.v2.5.6.zip'
+  homepage 'http://cvad-mac.narod.ru/index/0-4'
+  version '2.5.6'
+  sha1 '83843231812e73734ff803538accd963ec0435e4'
+  link 'Kext Utility.app'
+end
