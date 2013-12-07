97f28de630f613405a9be7e3ce497a4a26c80ea3
Roland Walker <walker@pobox.com>
2013-12-07 18:58:06 -0500

    add font Google Cousine
    this commit includes version 1.21

---
+++ Casks/font-cousine-bold.rb
@@ -0,0 +1,7 @@
+class FontCousineBold < Cask
+  url 'http://noto.googlecode.com/svn/trunk/fonts/individual/hinted/Cousine-Bold.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Cousine'
+  version '1.21'
+  sha1 '178fada9e2f88aa461cc6a938a254c69b2d5329d'
+  font 'Cousine-Bold.ttf'
+end
