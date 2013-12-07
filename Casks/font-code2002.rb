e9e97d77f62a7906dfd51b9f12beb68a495ec31b
Roland Walker <walker@pobox.com>
2013-12-07 18:20:38 -0500

    add font Code2000 and friends
    This commmit includes Code2000 v1.171, Code2001 v0.919, Code2002 v0.917

---
+++ Casks/font-code2002.rb
@@ -0,0 +1,7 @@
+class FontCode2002 < Cask
+  url 'http://www.ffonts.net/Code2002.font.zip'
+  homepage 'http://www.ffonts.net/Code2002.font.download'
+  version '0.917'
+  sha1 'ff45f7d5244018ce1a8b9afd45228d99900a6024'
+  font 'CODE2002.TTF'
+end
