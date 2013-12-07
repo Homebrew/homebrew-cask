92ed388a12eb834d39a53ed3134724f67f3a2b93
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-06 21:40:16 -0500

    Add GrowlVoice

---
+++ Casks/growlvoice.rb
@@ -0,0 +1,7 @@
+class Growlvoice < Cask
+  url 'http://www.growlvoice.com/trials/growlvoice-2.0.3.zip'
+  homepage 'http://www.growlvoice.com/'
+  version '2.0.3'
+  sha1 'b6de4cdcb3dbfeb9409ed3009b38130ef9b2fedd'
+  link 'GrowlVoice.app'
+end
