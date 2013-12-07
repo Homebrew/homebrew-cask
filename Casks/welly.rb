0446890b1cc9cd760faa8b297e9a667532db797a
Wang Guan <momocraft@gmail.com>
2013-12-08 02:03:23 +0900

    add Welly, a telnet BBS client

---
+++ Casks/welly.rb
@@ -0,0 +1,7 @@
+class Welly < Cask
+  url 'http://welly.googlecode.com/files/Welly.v2.7.fix.zip'
+  homepage 'https://code.google.com/p/welly/'
+  version '2.7+'
+  sha1 '9e391615f8f67e8d1048c47a3df19e03dd8126ed'
+  link 'Welly.app'
+end
