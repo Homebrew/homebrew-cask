89a003d75ef5f25140a88a7227d4fe9c38d656c3
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-06 22:12:49 -0500

    Add Moneywell

---
+++ Casks/moneywell.rb
@@ -0,0 +1,7 @@
+class Moneywell < Cask
+  url 'http://downloads.nothirst.com/MoneyWell_23.zip'
+  homepage 'http://nothirst.com/moneywell/'
+  version '2.3.1'
+  sha1 '4160b0f41584dd8408838b9a81b3ba995a7600f5'
+  link 'MoneyWell.app'
+end
