6aeddddbde467b1790eae79653df3c5230c358a8
Michael Ford <fanquake@users.noreply.github.com>
2013-12-07 08:50:51 +0800

    MakeMKV 1.8.6

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,7 +1,7 @@
 class Makemkv < Cask
-  url 'http://www.makemkv.com/download/makemkv_v1.8.5_osx.dmg'
+  url 'http://www.makemkv.com/download/makemkv_v1.8.6_osx.dmg'
   homepage 'http://www.makemkv.com/'
-  version '1.8.5'
-  sha1 'f228bb7462c3f43caefc20e1f048931c4620894b'
+  version '1.8.6'
+  sha1 '83fee73ef519a3c223745252a85d6e39e6544a1d'
   link 'MakeMKV.app'
 end
