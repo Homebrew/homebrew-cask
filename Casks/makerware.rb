abc1c6fe93e1f5481d48546193fc1e580c26f340
Michael Ford <fanquake@users.noreply.github.com>
2013-12-07 09:11:46 +0800

    MakerWare 2.4.0.22

--- Casks/makerware.rb
+++ Casks/makerware.rb
@@ -1,8 +1,8 @@
 class Makerware < Cask
-  url 'http://s3.amazonaws.com/downloads.makerbot.com/makerware/MakerWare%20Bundle%20of%20Awesome%202.3.3.42.dmg'
+  url 'https://s3.amazonaws.com/downloads.makerbot.com/makerware/MakerWare%20Bundle%20of%20Awesome%202.4.0.22.dmg'
   homepage 'http://www.makerbot.com/makerware/'
-  version '2.3.3.42'
-  sha1 'a93f20c599cf1c4cf69161063401bde332dacd44'
-  install 'MakerWare Bundle of Awesome 2.3.3.42.pkg'
+  version '2.4.0.22'
+  sha1 '9be2aa99b53db999f4fbc8fa05f10603dec5934c'
+  install 'MakerWare Bundle of Awesome 2.4.0.22.pkg'
   uninstall :pkgutil => 'com.makerbot.*'
 end
