5843ba872a6b67d327d3b17aadcdb6589792f27d
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-06 21:53:33 -0500

    add debt quencher

---
+++ Casks/debt-quencher.rb
@@ -0,0 +1,7 @@
+class DebtQuencher < Cask
+  url 'http://downloads.nothirst.com/DebtQuencher_1.2.zip'
+  homepage 'http://nothirst.com/debtquencher/'
+  version '1.2.2'
+  sha1 'e6ba452009f668500413fe597d03de61e5fa16f8'
+  link 'Debt Quencher.app'
+end
