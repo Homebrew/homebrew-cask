9d8cb7a5745b90e507caba52a7c0dd168671ac32
Joel Kuzmarski <leoj3n@gmail.com>
2013-12-07 13:07:00 -0600

    quicklook-csv latest

---
+++ Casks/quicklook-csv.rb
@@ -0,0 +1,7 @@
+class QuicklookCsv < Cask
+  url 'http://quicklook-csv.googlecode.com/files/QuickLookCSV.dmg'
+  homepage 'https://code.google.com/p/quicklook-csv'
+  version 'latest'
+  sha1 '3a6cbfefc94502291acd1540e9ae4a15aa1079ad'
+  qlplugin 'QuickLookCSV.qlgenerator'
+end
