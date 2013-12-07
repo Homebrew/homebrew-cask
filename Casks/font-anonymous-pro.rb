eb2dff94debc6fc097d3988687a47cf9707c0194
Roland Walker <walker@pobox.com>
2013-12-07 12:05:16 -0500

    add monospace font Anonymous Pro
    This commit contains font version 1.002

---
+++ Casks/font-anonymous-pro.rb
@@ -0,0 +1,10 @@
+class FontAnonymousPro < Cask
+  url 'http://www.marksimonson.com/assets/content/fonts/AnonymousPro-1.002.zip'
+  homepage 'http://www.marksimonson.com/fonts/view/anonymous-pro'
+  version '1.002'
+  sha1 '87651de93312fdd3f27e50741d2a0630a41ec30d'
+  font 'AnonymousPro-1.002.001/Anonymous Pro B.ttf',
+       'AnonymousPro-1.002.001/Anonymous Pro BI.ttf',
+       'AnonymousPro-1.002.001/Anonymous Pro I.ttf',
+       'AnonymousPro-1.002.001/Anonymous Pro.ttf'
+end
