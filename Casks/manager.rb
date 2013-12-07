98564a4529e463b507e45a7e0869911f1899a3b9
Michael Ford <fanquake@users.noreply.github.com>
2013-12-07 09:13:59 +0800

    Manager 13.12.1

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -1,7 +1,7 @@
 class Manager < Cask
-  url 'http://cdn.manager.io/downloads/v13.10.5040.17151/Manager.dmg'
+  url 'http://cdn.manager.io/downloads/v13.12.1/Manager.dmg'
   homepage 'http://manager.io'
-  version '13.10.5040.17151'
-  sha1 '3de9d92fc7a6673c0b84bcad652f7f781218f537'
+  version '13.12.1'
+  sha1 'b91337f001397f868113b8e31eca4067bbae8f8f'
   link 'Manager.app'
 end
