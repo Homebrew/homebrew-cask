c11e6990450006bbc7fd369024c835293b0be0fa
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-08 01:51:48 +0800

    New Nightingale 1.12 Cask

---
+++ Casks/nightingale.rb
@@ -0,0 +1,7 @@
+class Nightingale < Cask
+  url 'http://downloads.sourceforge.net/ngale/Nightingale_1.12-2432_macosx-i686.dmg'
+  homepage 'http://getnightingale.com/'
+  version '1.12-2432'
+  sha1 '8d646183547215c18d5a2d75331c1230d9cb4376'
+  link 'Nightingale.app'
+end
