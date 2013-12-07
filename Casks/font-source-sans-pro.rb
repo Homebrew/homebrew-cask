8aafc344b833b84ec6b56f91700f222bd421276c
Roland Walker <walker@pobox.com>
2013-12-07 13:34:49 -0500

    add Adobe fonts Source Code Pro, Source Sans Pro
    These are released under SIL Open Font License. Included in this commit:
    Source Code Pro version 1.017, Source Sans Pro version 1.050.

---
+++ Casks/font-source-sans-pro.rb
@@ -0,0 +1,18 @@
+class FontSourceSansPro < Cask
+  url 'http://downloads.sourceforge.net/sourceforge/sourcesans.adobe/SourceSansPro_FontsOnly-1.050.zip'
+  homepage 'http://store1.adobe.com/cfusion/store/html/index.cfm?store=OLS-US&event=displayFontPackage&code=1959'
+  version '1.050'
+  sha1 'f3af1201c80a86f1dd6e045a565c96bdd12d445f'
+  font 'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-Black.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-BlackIt.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-Bold.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-BoldIt.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-ExtraLight.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-ExtraLightIt.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-It.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-Light.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-LightIt.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-Regular.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-Semibold.otf',
+       'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-SemiboldIt.otf'
+end
