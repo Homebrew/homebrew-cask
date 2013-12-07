59e9dbf6bb68fae8ea0cc61df55140c3474de5b0
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-06 21:32:46 -0500

    Add Coda 2

---
+++ Casks/coda.rb
@@ -0,0 +1,7 @@
+class Coda < Cask
+  url 'https://panic.com/coda/d/Coda%202.0.12.zip'
+  homepage 'https://panic.com/Coda/'
+  version '2.0.12'
+  sha1 '7157f0213edf39aca296c8bc4cb5a0e6f5ca6f75'
+  link 'Coda 2.app'
+end
