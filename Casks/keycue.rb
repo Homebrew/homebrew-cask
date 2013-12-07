4110433d11f4b28eb327904a6df132318fb8ac43
Michael Ford <fanquake@users.noreply.github.com>
2013-12-07 08:30:07 +0800

    KeyCue 7.1

--- Casks/keycue.rb
+++ Casks/keycue.rb
@@ -1,7 +1,7 @@
 class Keycue < Cask
-  url 'http://www.ergonis.com/downloads/products/keycue/KeyCue70-Install.dmg'
+  url 'http://www.ergonis.com/downloads/products/keycue/KeyCue71-Install.dmg'
   homepage 'http://www.ergonis.com/products/keycue/'
-  version '7.0'
-  sha1 '3ef4f70f6d8d6c87e83071d36242b985291709e5'
+  version '7.1'
+  sha1 '43584fe002194131f000b61f3ae9c04515f9389e'
   link 'KeyCue.app'
 end
