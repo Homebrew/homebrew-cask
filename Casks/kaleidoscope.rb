e931a240d266d1f0646041caff9beca3c64e7d95
Scott Reeves <cottser@mailworks.org>
2013-12-06 21:09:14 -0500

    Kaleidoscope: Suppress move to /Applications prompt

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -4,4 +4,9 @@ class Kaleidoscope < Cask
   version '2.0.2_116'
   sha1 '4a35f6e826f0553e6d7ed02486617589f262f7fc'
   link 'Kaleidoscope.app'
+
+  after_install do
+    # Don't ask to move the app bundle to /Applications
+    system 'defaults write com.blackpixel.kaleidoscope moveToApplicationsFolderAlertSuppress -bool true'
+  end
 end
