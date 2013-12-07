651fd497cea1fb4384f9c252680bf72216c7c3eb
Michael Ford <fanquake@users.noreply.github.com>
2013-12-07 08:45:27 +0800

    Mactracker 7.2.1

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,7 +1,7 @@
 class Mactracker < Cask
-  url 'http://www.mactracker.ca/downloads/Mactracker_7.2.zip'
+  url 'http://www.mactracker.ca/downloads/Mactracker_7.2.1.zip'
   homepage 'http://mactracker.ca/'
-  version '7.2'
-  sha1 '643389b7832e74f5429543b963926820e0c84954'
+  version '7.2.1'
+  sha1 '90d244a16b24894ded0c67ae19aceb767f84f1b9'
   link 'Mactracker.app'
 end
