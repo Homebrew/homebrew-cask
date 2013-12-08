8fa27800012a9a240013d3bdeb3e0c69084f6a07
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-08 13:33:01 -0500

    Add Duplicate Annihilator

---
+++ Casks/duplicate-annihilator.rb
@@ -0,0 +1,8 @@
+class DuplicateAnnihilator < Cask
+  url 'http://brattoo.com/propaganda/downloadDa.php'
+  homepage 'http://brattoo.com/propaganda/'
+  version '4.16.0'
+  sha1 '80af306c21f1f6c16d08fe6a672af3d20a43591d'
+  nested_container 'Duplicate Annihilator.dmg'
+  link 'Duplicate Annihilator.app'
+end
