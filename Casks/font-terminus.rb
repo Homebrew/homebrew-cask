982a20f08e04462f9b05542d77f9aa1834d18823
Roland Walker <walker@pobox.com>
2013-12-09 11:45:01 -0500

    remove Terminus checksums

--- Casks/font-terminus.rb
+++ Casks/font-terminus.rb
@@ -1,7 +1,7 @@
 class FontTerminus < Cask
   url 'http://misc.nybergh.net/pub/fonts/terminus/ttf_for_osx/Terminus.ttf'
   homepage 'http://misc.nybergh.net/pub/fonts/terminus/ttf_for_osx/'
-  version '001.000'
-  sha1 'e1550e089c48b7fbe894b72fe39be11fdeabeb04'
+  version 'latest'
+  no_checksum
   font 'Terminus.ttf'
 end
--- Casks/font-terminusbold.rb
+++ Casks/font-terminusbold.rb
@@ -1,7 +1,7 @@
 class FontTerminusbold < Cask
   url 'http://misc.nybergh.net/pub/fonts/terminus/ttf_for_osx/TerminusBold.ttf'
   homepage 'http://misc.nybergh.net/pub/fonts/terminus/ttf_for_osx/'
-  version '001.000'
-  sha1 'a7e5ca44089ce256e9c8c233ce220b3784f74d2e'
+  version 'latest'
+  no_checksum
   font 'TerminusBold.ttf'
 end
