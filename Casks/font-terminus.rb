f0d1cdd80a1c9176ec7ac309c9ba11b9951dba06
Roland Walker <walker@pobox.com>
2013-12-09 11:10:06 -0500

    add Terminus font
    This commit includes versions 001.000 of Terminus and Terminus Bold

---
+++ Casks/font-terminus.rb
@@ -0,0 +1,7 @@
+class FontTerminus < Cask
+  url 'http://misc.nybergh.net/pub/fonts/terminus/ttf_for_osx/Terminus.ttf'
+  homepage 'http://misc.nybergh.net/pub/fonts/terminus/ttf_for_osx/'
+  version '001.000'
+  sha1 'e1550e089c48b7fbe894b72fe39be11fdeabeb04'
+  font 'Terminus.ttf'
+end
