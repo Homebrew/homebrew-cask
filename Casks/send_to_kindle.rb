7aae214eff5e86e495cac64da554c50380b9aff6
James Hart <hjhart@gmail.com>
2013-12-10 09:13:24 -0800

    Send to kindle recipe added.

---
+++ Casks/send_to_kindle.rb
@@ -0,0 +1,7 @@
+class Send_to_kindle < Cask
+  url 'http://s3.amazonaws.com/sendtokindle/SendToKindleForMac-installer-v1.0.0.218.pkg'
+  homepage 'http://www.amazon.com/gp/sendtokindle/mac'
+  version '1.0.0.218'
+  sha1 'a1c2759d220e707b12cfefa8fcd1f58f5d3165cf'
+  install 'SendToKindleForMac-installer-v1.0.0.218.pkg'
+end
