3b3ba3d64d110f3b6107dc88ddc4e21a167aa4ce
Kirk McCutcheon <kmccutcheon@resource.com>
2013-12-10 12:02:22 -0500

    Add Mpeg Streamclip cask
    Cask installationfor MPEG Streamclip http://www.squared5.com/

---
+++ Casks/mpeg-streamclip.rb
@@ -0,0 +1,7 @@
+class MpegStreamclip < Cask
+  url 'http://www.alfanet.it/squared5/MPEG_Streamclip_1.9.2.dmg'
+  homepage 'http://www.squared5.com/'
+  version '1.9.2'
+  no_checksum
+  link 'MPEG Streamclip.app'
+end
