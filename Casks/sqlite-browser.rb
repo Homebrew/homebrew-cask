4d50be731411353285316076a81610fdb2bacc23
Kirk McCutcheon <kmccutcheon@resource.com>
2013-12-10 11:41:12 -0500

    Add cask for SQLite Database Browser
    Cask installation for SQLite Database Browser
    sqlitebrowser.sourceforge.net/

---
+++ Casks/sqlite-browser.rb
@@ -0,0 +1,7 @@
+class SqliteBrowser < Cask
+  url 'http://downloads.sourceforge.net/sourceforge/sqlitebrowser/sqlitebrowser_200_b1_osx.zip'
+  homepage 'http://sqlitebrowser.sourceforge.net/'
+  version '2.0.0b1'
+  sha1 '79cbfb68e4c540f7a5f7fab1b6432db7751386e6'
+  link 'SQLite Database Browser 2.0 b1.app'
+end
