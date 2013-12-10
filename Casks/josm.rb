eab9593acdcb8d4a478baf867e974765eebce773
Marcus Jaschen <mail@marcusjaschen.de>
2013-12-10 22:05:48 +0100

    Change Cask for proper use of version-less download URL

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,7 +1,7 @@
 class Josm < Cask
   url 'http://josm.openstreetmap.de/download/macosx/josm-macosx.zip'
   homepage 'http://josm.openstreetmap.de'
-  version '6388'
-  sha1 'b7973548df871e7d7edd6f14bd0953f4786fae5a'
+  version 'latest'
+  no_checksum
   link 'JOSM.app'
 end
