0c306474d12829b13dc885cbf0bc233ea77d8cbc
Marcus Jaschen <mail@marcusjaschen.de>
2013-12-10 10:21:43 +0100

    Added Cask for JOSM OpenStreetMap Editor

---
+++ Casks/josm.rb
@@ -0,0 +1,7 @@
+class Josm < Cask
+  url 'http://josm.openstreetmap.de/download/macosx/josm-macosx.zip'
+  homepage 'http://josm.openstreetmap.de'
+  version '6388'
+  sha1 'b7973548df871e7d7edd6f14bd0953f4786fae5a'
+  link 'JOSM.app'
+end
