8f3839f6542101bd390eac4a118afc2bb62d311e
Kirk McCutcheon <kmccutcheon@resource.com>
2013-12-10 13:00:22 -0500

    Add sha1 checksum

--- Casks/mpeg-streamclip.rb
+++ Casks/mpeg-streamclip.rb
@@ -2,6 +2,6 @@ class MpegStreamclip < Cask
   url 'http://www.alfanet.it/squared5/MPEG_Streamclip_1.9.2.dmg'
   homepage 'http://www.squared5.com/'
   version '1.9.2'
-  no_checksum
+  sha1 '346ea3aef9b7638a0e39e30e494f04010c73afe1'
   link 'MPEG Streamclip.app'
 end
