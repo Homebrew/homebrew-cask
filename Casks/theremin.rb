c287dc90c4878d29869bdf9395ea8c5d5403d9e4
Axel Eirola <axel.eirola@iki.fi>
2013-12-11 00:13:25 +0200

    Add Theremin

---
+++ Casks/theremin.rb
@@ -0,0 +1,8 @@
+class Theremin < Cask
+  url 'http://f.nn.lv/ms/l5/29/Theremin.app.zip'
+  homepage 'https://github.com/TheStalwart/Theremin'
+  version '0.7'
+  sha1 '1ac5e88f99e4dcd01d3152450276bae33594d736'
+  link 'Theremin.app'
+  uninstall :quit => 'at.justp.Theremin'
+end
