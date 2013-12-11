ce91f6329bda2fb6c6289381d4300bf2cbcc7466
Joel Kuzmarski <leoj3n@gmail.com>
2013-12-10 18:14:12 -0600

    Macs Fan Control latest

---
+++ Casks/macs-fan-control.rb
@@ -0,0 +1,7 @@
+class MacsFanControl < Cask
+  url 'http://www.crystalidea.com/downloads/macsfancontrol.zip'
+  homepage 'http://www.crystalidea.com/macs-fan-control'
+  version 'latest'
+  no_checksum
+  link 'Macs Fan Control.app'
+end
