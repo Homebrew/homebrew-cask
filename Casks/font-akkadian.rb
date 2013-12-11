be929f05aac7d48599c64ec932b18e4024b05479
Roland Walker <walker@pobox.com>
2013-12-11 14:06:35 -0500

    add font Akkadian
    This commit includes Akkadian version 7.05

---
+++ Casks/font-akkadian.rb
@@ -0,0 +1,7 @@
+class FontAkkadian < Cask
+  url 'http://users.teilar.gr/~g1951d/Akkadian.ttf'
+  homepage 'http://users.teilar.gr/~g1951d/'
+  version 'latest'
+  no_checksum
+  font 'Akkadian.ttf'
+end
