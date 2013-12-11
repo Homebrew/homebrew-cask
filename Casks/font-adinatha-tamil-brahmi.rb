20ee426a1adda0207e281f041c973a375d7bfe59
Roland Walker <walker@pobox.com>
2013-12-11 13:48:24 -0500

    add font Adinatha Tamil Brahmi
    This commit includes font version 1.00

---
+++ Casks/font-adinatha-tamil-brahmi.rb
@@ -0,0 +1,7 @@
+class FontAdinathaTamilBrahmi < Cask
+  url 'http://www.virtualvinodh.com/download/Adinatha-Tamil-Brahmi.zip'
+  homepage 'http://www.virtualvinodh.com/downloads'
+  version 'latest'
+  no_checksum
+  font 'Adinatha-Tamil-Brahmi.otf'
+end
