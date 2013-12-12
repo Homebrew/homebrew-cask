cd3fb717f3637335767497b8db4dbba14bd46f65
Gabor Nagy <mail@aigeruth.hu>
2013-12-12 02:14:04 +0100

    Downloads the driver from the manufacturer's homepage.

--- Casks/prolific-usb-serial-driver.rb
+++ Casks/prolific-usb-serial-driver.rb
@@ -1,9 +1,9 @@
 class ProlificUsbSerialDriver < Cask
-  url 'http://pbxbook.com/other/sw/PL2303_MacOSX_v1_5_1.zip'
-  homepage 'http://pbxbook.com/other/mac-tty.html'
+  url 'http://www.prolific.com.tw/UserFiles/files/md_PL2303_MacOSX-10_6up_v1_5_1.zip'
+  homepage 'http://www.prolific.com.tw/US/ShowProduct.aspx?p_id=229&pcid=41'
   version '1.5.1'
-  sha1 'e251e2a3da821d8ed8124e78181a78e5f57dc046'
-  install 'PL2303_MacOSX-10_6up_v1_5_1/PL2303_MacOSX_v1.5.1.pkg'
+  sha1 '2e30c876b159f6be76f5a9d58a0cd512fb2a5478'
+  install 'md_PL2303_MacOSX-10_6up_v1_5_1/PL2303_MacOSX_v1.5.1.pkg'
   uninstall :pkgutil => 'com.prolific.prolificUsbserialCableDriverV151.ProlificUsbSerial.pkg',
   			:kext => 'com.prolific.driver.PL2303'
 end
