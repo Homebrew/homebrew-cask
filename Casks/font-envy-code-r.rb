2eae3cf22a94c064e1b64caeec8cb150f3483eec
xcezx <main.xcezx@gmail.com>
2013-12-14 03:19:23 +0900

    no checksum

--- Casks/font-envy-code-r.rb
+++ Casks/font-envy-code-r.rb
@@ -2,7 +2,7 @@ class FontEnvyCodeR < Cask
   url 'http://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip'
   homepage 'http://damieng.com/blog/2008/05/26/envy-code-r-preview-7-coding-font-released'
   version 'latest'
-  sha1 '492e729708568d4188f053d7f88d148c0808ca03'
+  no_checksum
   font 'Envy Code R PR7/Envy Code R Bold.ttf',
        'Envy Code R PR7/Envy Code R Italic.ttf',
        'Envy Code R PR7/Envy Code R.ttf',
