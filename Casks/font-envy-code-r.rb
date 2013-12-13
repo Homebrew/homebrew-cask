ee3217c9b55a6f539bf133fa1ebda1475f04b86b
xcezx <main.xcezx@gmail.com>
2013-12-14 03:11:22 +0900

    add font Envy Code R

---
+++ Casks/font-envy-code-r.rb
@@ -0,0 +1,11 @@
+class FontEnvyCodeR < Cask
+  url 'http://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip'
+  homepage 'http://damieng.com/blog/2008/05/26/envy-code-r-preview-7-coding-font-released'
+  version 'latest'
+  sha1 '492e729708568d4188f053d7f88d148c0808ca03'
+  font 'Envy Code R PR7/Envy Code R Bold.ttf',
+       'Envy Code R PR7/Envy Code R Italic.ttf',
+       'Envy Code R PR7/Envy Code R.ttf',
+       'Envy Code R PR7/Visual Studio italics-as-bold/Envy Code R VS Italic-as-bold.ttf',
+       'Envy Code R PR7/Visual Studio italics-as-bold/Envy Code R VS.ttf'
+end
