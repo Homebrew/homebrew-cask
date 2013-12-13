435b3c7ca5b5a42bed3c76cdd4370cbedb6a7237
Beng Hee Eu <beng@benghee.eu>
2013-12-13 09:40:31 +0800

    xScope 3.6.3

--- Casks/xscope.rb
+++ Casks/xscope.rb
@@ -1,7 +1,7 @@
 class Xscope < Cask
-  url 'http://iconfactory.com/assets/software/xscope/xScope-3.6.2.zip'
+  url 'http://iconfactory.com/assets/software/xscope/xScope-3.6.3.zip'
   homepage 'http://iconfactory.com/software/xscope'
-  version '3.6.2'
-  sha1 'be15205e05e30791ac4d5efe23b69d5c7d2a568b'
+  version '3.6.3'
+  sha1 '31e0d59765606595441e1489777ad46bdd9531af'
   link 'xScope.app'
 end
