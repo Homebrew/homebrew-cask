78f98a90cd8cad27f801f0ebb14d57e0c8ae88be
Ali Karbassi <ali@karbassi.com>
2013-12-14 01:55:16 -0600

    Updated AquaTerm to correct download url.
    Signed-off-by: Fernando Paredes <nano@fdp.io>
    
    Fixes #2083

--- Casks/aquaterm.rb
+++ Casks/aquaterm.rb
@@ -1,5 +1,5 @@
 class Aquaterm < Cask
-  url 'http://downloads.sourceforge.net/project/aquaterm/AquaTerm/AquaTerm1.1.1.dmg'
+  url 'http://downloads.sourceforge.net/project/aquaterm/AquaTerm/v1.1.1/AquaTerm-1.1.1.dmg'
   homepage 'http://aquaterm.sourceforge.net/'
   version '1.1.1'
   sha1 '0127f9815fdb99373ecfad13d84cf1d64d777781'
