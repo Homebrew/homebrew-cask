5143445fe968a7c07104d0d9122083afcbd893cf
Michael Ford <fanquake@users.noreply.github.com>
2013-12-14 21:46:32 +0800

    Correct field order in gfxCardStatus

--- Casks/gfxcardstatus.rb
+++ Casks/gfxcardstatus.rb
@@ -1,6 +1,6 @@
 class Gfxcardstatus < Cask
-  homepage 'http://gfx.io/'
   url 'http://codykrieger.com/downloads/gfxCardStatus-2.3.zip'
+  homepage 'http://gfx.io/'
   version '2.3'
   sha1 '0bd1d9d5b1da69b77217949b788597a952cbd846'
   link 'gfxCardStatus.app'
