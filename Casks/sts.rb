750b026dbf3a59b4ca48d1027e23de32eb52c721
apinske <alex@pinske.eu>
2013-12-14 15:22:37 +0100

    Added Cask for Spring Tool Suite (STS).

---
+++ Casks/sts.rb
@@ -0,0 +1,7 @@
+class Sts < Cask
+  url 'http://download.springsource.com/release/STS/3.4.0/dist/e4.3/spring-tool-suite-3.4.0.RELEASE-e4.3.1-macosx-cocoa-x86_64.tar.gz'
+  homepage 'http://spring.io/tools/sts'
+  version '3.4.0'
+  sha1 '2c62fc661c4978be91a524c02469b8e67754e610'
+  link 'springsource/sts-3.4.0.RELEASE/STS.app'
+end
