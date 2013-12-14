a79f3a2bf28cb780fb3de96443bf6f0f8ecf45da
Michael Ford <fanquake@users.noreply.github.com>
2013-12-14 12:41:19 +0800

    Tapaal 2.4.1

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -1,7 +1,7 @@
 class Tapaal < Cask
-  url 'http://www.tapaal.net/fileadmin/download/tapaal-2.4/tapaal-2.4.0-mac.dmg'
+  url 'http://www.tapaal.net/fileadmin/download/tapaal-2.4/tapaal-2.4.1-mac.dmg'
   homepage 'http://www.tapaal.net'
-  version '2.4.0'
-  sha1 'e02f47e3a8ae83bb951b0d6f167437e19f1a6ba7'
+  version '2.4.1'
+  sha1 '06d0b96a9b14f43372f54acec2ef4e017dc53754'
   link 'Tapaal.app'
 end
