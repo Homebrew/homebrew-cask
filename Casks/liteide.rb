7b2918a4bb92cfd14a671707b1c77f9751c3e735
Michael Ford <fanquake@users.noreply.github.com>
2013-12-14 17:04:09 +0800

    Liteidex20.1

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,7 +1,7 @@
 class Liteide < Cask
-  url 'https://golangide.googlecode.com/files/liteidex19.macosx.zip'
+  url 'https://golangide.googlecode.com/files/liteidex20.1.macosx.zip'
   homepage 'https://code.google.com/p/golangide/'
-  version 'x19'
-  sha1 '9a236e9ee0780ce0563c2fae8a10b6a683e43d4c'
+  version 'x20.1'
+  sha1 'e45fc86502cc4826bcafa95a2d722dd7ac195f3b'
   link 'liteide/LiteIDE.app'
 end
