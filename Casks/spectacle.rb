4cfdfd0a79b5c9e0310abac7333dda1266d8e7f8
Michael Ford <fanquake@users.noreply.github.com>
2013-12-14 22:28:20 +0800

    Update Spectacle App homepage

--- Casks/spectacle.rb
+++ Casks/spectacle.rb
@@ -1,6 +1,6 @@
 class Spectacle < Cask
   url 'https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.4.zip'
-  homepage 'http://www.spectacleapp.com/'
+  homepage 'http://spectacleapp.com/'
   version '0.8.4'
   sha1 'd2585cd30ec2c14f14c05bdc1d743d631cb616a9'
   link 'Spectacle.app'
