0ab06574c2325bd2499b50747834067ac0349a8a
Michael Ford <fanquake@users.noreply.github.com>
2013-12-14 12:44:19 +0800

    TestFlight 1.0-Beta (313)

--- Casks/testflight.rb
+++ Casks/testflight.rb
@@ -1,7 +1,7 @@
 class Testflight < Cask
-  url 'https://d193ln56du8muy.cloudfront.net/desktop_app/1370377714/TestFlight-Desktop-1.0-Beta(306).zip'
+  url 'https://d193ln56du8muy.cloudfront.net/desktop_app/1381509820/TestFlight-Desktop-1.0-Beta(313).zip'
   homepage 'http://testflightapp.com'
-  version '1.0_beta306'
-  sha1 '86180feb6e48c30501ec2a0428bb45f7b4fc3396'
+  version '1.0_beta313'
+  sha1 'cc060b7124a02fc430fca90232276f7302de0262'
   link 'TestFlight.app'
 end
