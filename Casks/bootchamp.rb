b14db55d7cd7ca54e9ef6216aedab22197de2983
Chris Chernoff <chrischernoff@users.noreply.github.com>
2013-12-15 11:42:03 -0800

    Set version to latest for BootChamp Cask, remove checksum

--- Casks/bootchamp.rb
+++ Casks/bootchamp.rb
@@ -1,7 +1,7 @@
 class Bootchamp < Cask
   url 'http://www.kainjow.com/downloads/BootChamp.zip'
   homepage 'http://www.kainjow.com/'
-  version '1.5.2'
-  sha1 '59af36502dd68372181307ff23712b416503d8da'
+  version 'latest'
+  no_checksum
   link 'BootChamp.app'
 end
