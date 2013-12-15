3ee7c26a0cca9376b80644ea849c1c5ab37b9a86
Chris Chernoff <chris@burnedpixel.com>
2013-12-14 22:42:11 -0800

    Add BootChamp Cask

---
+++ Casks/bootchamp.rb
@@ -0,0 +1,7 @@
+class Bootchamp < Cask
+  url 'http://www.kainjow.com/downloads/BootChamp.zip'
+  homepage 'http://www.kainjow.com/'
+  version '1.5.2'
+  sha1 '59af36502dd68372181307ff23712b416503d8da'
+  link 'BootChamp.app'
+end
