2ed5bd0b841993f1c6296d250d53bff4e6a6400b
Adam Schay <adamschay@gmail.com>
2013-12-15 11:44:57 -0700

    Unexpected change of download URLs between releases - fixes 404ing

--- Casks/litecoin.rb
+++ Casks/litecoin.rb
@@ -1,5 +1,5 @@
 class Litecoin < Cask
-  url 'https://litecoin.org/downloads/macosx/Litecoin-Qt-0.8.6.1.dmg'
+  url 'https://download.litecoin.org/litecoin-0.8.6.1/macosx/Litecoin-Qt-0.8.6.1.dmg'
   homepage 'https://litecoin.org/'
   version '0.8.6.1'
   sha1 '2e9bcbaf8f7da934a7f1095bdcd400a5bed99478'
