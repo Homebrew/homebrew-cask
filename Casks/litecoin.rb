ea914e041fc27bc832d57cd4fad38658d3805a33
Adam Schay <adamschay@gmail.com>
2013-12-15 12:34:31 -0700

    SHA-1 key was still for 0.8.5.1 - fixes error in installation

--- Casks/litecoin.rb
+++ Casks/litecoin.rb
@@ -2,6 +2,6 @@ class Litecoin < Cask
   url 'https://download.litecoin.org/litecoin-0.8.6.1/macosx/Litecoin-Qt-0.8.6.1.dmg'
   homepage 'https://litecoin.org/'
   version '0.8.6.1'
-  sha1 '2e9bcbaf8f7da934a7f1095bdcd400a5bed99478'
+  sha1 'ab040208aaa34c6d490d98d718489f1a23ffad8d'
   link 'Litecoin-Qt.app'
 end
