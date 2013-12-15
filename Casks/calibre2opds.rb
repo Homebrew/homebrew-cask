efbf9a42a115cf2284a7ae396fcdce42a7ea24bc
Beng Hee Eu <beng@benghee.eu>
2013-12-15 12:31:09 +0800

    calibre2opds 3.3-246

---
+++ Casks/calibre2opds.rb
@@ -0,0 +1,7 @@
+class Calibre2opds < Cask
+  url 'https://calibre2opds.googlecode.com/files/calibre2opds-3.3-246.dmg'
+  homepage 'http://calibre2opds.com/'
+  version '3.3-246'
+  sha1 '609b10b98d792671e0c43c0c64de7513a24ab59e'
+  link 'Calibre2Opds.app'
+end
