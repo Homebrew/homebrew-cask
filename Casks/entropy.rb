eac96011f8ad8a611a0d1fb868767531e1122e62
Penn Su <pennsu@gmail.com>
2013-12-15 15:32:17 +0800

    Added Entropy

---
+++ Casks/entropy.rb
@@ -0,0 +1,7 @@
+class Entropy < Cask
+  url 'http://www.eigenlogik.com/entropy/download'
+  homepage 'http://www.eigenlogik.com/entropy/'
+  version 'latest'
+  no_checksum
+  link 'Entropy.app'
+end
