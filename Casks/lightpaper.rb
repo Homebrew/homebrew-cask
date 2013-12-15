e3fd24f1289db1ee137b956de1df5a620cd658ea
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-12-15 17:10:46 -0200

    removed unnecessary url param
    removed the `?ref=downloadbtn` from the URL.

--- Casks/lightpaper.rb
+++ Casks/lightpaper.rb
@@ -1,5 +1,5 @@
 class Lightpaper < Cask
-  url 'http://cl.ly/2S203Q1n3r22/download/LightPaper_v0.8.0.dmg?ref=downloadbtn'
+  url 'http://cl.ly/2S203Q1n3r22/download/LightPaper_v0.8.0.dmg'
   homepage 'http://clockworkengine.com/lightpaper-mac/'
   version '0.8.0'
   sha1 '9bfeeeee837908097709def546dcec16c469f0b5'
