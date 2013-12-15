454600b13226c80e694ac8be3099322de1d8ad99
Beng Hee Eu <beng@benghee.eu>
2013-12-15 10:59:15 +0800

    SourceTree 1.8.0.3

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,7 +1,7 @@
 class Sourcetree < Cask
-  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.8.0.2.dmg'
+  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.8.0.3.dmg'
   homepage 'http://www.sourcetreeapp.com/'
-  version '1.8.0.2'
-  sha1 'bc876b8e23aaa4254a3d5a97740e6921e23e60b4'
+  version '1.8.0.3'
+  sha1 '302da44a20f855f72f2f0184d1efb7653232b241'
   link 'SourceTree.app'
 end
