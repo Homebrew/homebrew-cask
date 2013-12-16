e3f38967f1f6cdbbd766303ad28113b90f2dc56f
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-16 15:34:15 +0800

    Sparkle 1.5b6

---
+++ Casks/sparkle.rb
@@ -0,0 +1,7 @@
+class Sparkle < Cask
+  url 'http://sparkle.andymatuschak.org/files/Sparkle%201.5b6.zip'
+  homepage 'http://sparkle.andymatuschak.org/'
+  version '1.5b6'
+  sha1 '74d72e99ac1656bdeee4111e5a25d219107c12e8'
+  link 'Sparkle Test App.app'
+end
