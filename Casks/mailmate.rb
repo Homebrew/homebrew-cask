3372679f69822e22a362eb9b1105f9efa3372684
Stratos Moros <stratoukos@timett.local>
2013-12-17 14:09:59 +0200

    add mailmate

---
+++ Casks/mailmate.rb
@@ -0,0 +1,7 @@
+class Mailmate < Cask
+  url 'http://dl.mailmate-app.com/MailMate.tbz'
+  homepage 'http://freron.com/'
+  version 'latest'
+  sha1 '0bbc84d90a4540c81cc2b1f0e612112fbc5a2b7a'
+  link 'MailMate.app'
+end
