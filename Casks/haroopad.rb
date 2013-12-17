981739df5c65bde32ba79ad6a2df2499849e720f
Chulki Lee <chulki@asperasoft.com>
2013-12-17 10:14:48 -0800

    haroopad 0.9.1

---
+++ Casks/haroopad.rb
@@ -0,0 +1,7 @@
+class Haroopad < Cask
+  url 'https://dl.dropbox.com/s/virlns1wo817ci5/haroopad-v0.9.1.dmg'
+  homepage 'http://pad.haroopress.com/'
+  version '0.9.1'
+  sha1 'd93ee3c3e4d049cea3f22426367ae1b3f3c1e929'
+  link 'haroopad.app'
+end
