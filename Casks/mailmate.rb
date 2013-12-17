b10b20aa7f47f38c9fc4e05a2a926e2d5f215fc8
Stratos Moros <s.moros@indev.gr>
2013-12-17 15:47:06 +0200

    replace sha1 with no_checksum
    realized that a checksum doesn't make any sense for a cask whose
    version is "latest"

--- Casks/mailmate.rb
+++ Casks/mailmate.rb
@@ -2,6 +2,6 @@ class Mailmate < Cask
   url 'http://dl.mailmate-app.com/MailMate.tbz'
   homepage 'http://freron.com/'
   version 'latest'
-  sha1 '0bbc84d90a4540c81cc2b1f0e612112fbc5a2b7a'
+  no_checksum
   link 'MailMate.app'
 end
