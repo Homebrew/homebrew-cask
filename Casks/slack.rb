73456792d90fe85bd6f4425ee4f9e9871b7dc895
David O'Trakoun <davidosomething@users.noreply.github.com>
2013-12-17 11:23:37 -0500

    Fix home url for slack.rb

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,6 +1,6 @@
 class Slack < Cask
   url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/747/368/original/7eeaf12b3a8c610df252bf02713c6e29/Slack_0.34.zip'
-  homepage 'www.slack.com'
+  homepage 'http://www.slack.com'
   version '0.34'
   sha1 'ec584efd8dd27070ca4c1acb8666ee6ce30156b5'
   link 'Slack.app'
