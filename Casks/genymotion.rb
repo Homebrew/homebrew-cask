68befc342cbf3506d146316c0540cf4393899bbe
Alex Selesse <alex@selesse.com>
2013-12-17 21:51:08 -0500

    Update to 2.0.2

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -1,7 +1,7 @@
 class Genymotion < Cask
-  url 'https://ssl-files.genymotion.com/genymotion/genymotion-2.0.1/genymotion-2.0.1.dmg'
+  url 'https://ssl-files.genymotion.com/genymotion/genymotion-2.0.2/genymotion-2.0.2.dmg'
   homepage 'http://www.genymotion.com/'
-  version '2.0.1'
-  sha1 'ce2a8e8a0c3760df4f2a76e368f35c963235189e'
+  version '2.0.2'
+  sha1 '2bfbd9175f75bb1727fbfa13144dfe7a96502a9c'
   link 'Genymotion.app', 'Genymotion Shell.app'
 end
