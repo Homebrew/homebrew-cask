61847ddc5de73bfb20c5b73195d611a1dc2c25c3
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-12-18 21:44:08 -0200

    added subtitle master

---
+++ Casks/subtitle-master.rb
@@ -0,0 +1,7 @@
+class SubtitleMaster < Cask
+  url 'http://updates.subtitlemaster.com/app/Subtitle%20Master%200.6.zip'
+  homepage 'http://subtitlemaster.com/'
+  version '0.6'
+  sha1 'c034458c9f748443002c828987333fef4cbc1264'
+  link 'Subtitle Master.app'
+end
