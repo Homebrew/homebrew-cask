a3358f9ce545a40b1c1a79fe10eaa10e06363433
Steven <github.good@lowbatteries.com>
2013-12-18 10:19:45 -0800

    Updated MailFollowUp to latest version

--- Casks/mailfollowup.rb
+++ Casks/mailfollowup.rb
@@ -1,9 +1,9 @@
 class Mailfollowup < Cask
-  url 'http://www.cs.unc.edu/~welch/MailFollowup/media/MailFollowUp_1.6.1.dmg.zip'
+  url 'http://www.cs.unc.edu/~welch/MailFollowup/media/MailFollowUp_1.6.2.dmg.zip'
   homepage 'http://www.cs.unc.edu/~welch/MailFollowup/'
-  version '1.6.1'
-  sha1 'd21d8b87171d3e861b60168afc53b4d0cfe6ce09'
-  nested_container 'MailFollowUp_1.6.1.dmg'
+  version '1.6.2'
+  sha1 '538c0441786be4a89a68ce168de2087fa9304c95'
+  nested_container 'MailFollowUp_1.6.2.dmg'
   install 'Install MailFollowUp.pkg'
   
   uninstall(
