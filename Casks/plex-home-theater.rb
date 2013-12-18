f0f959424847331a41b8e815d32ec58a98c54429
darrenli <darren@thanx.com>
2013-12-18 00:11:40 -0800

    Rename Plex to PlexHomeTheater and update version to 1.0.7.169.

---
+++ Casks/plex-home-theater.rb
@@ -0,0 +1,7 @@
+class PlexHomeTheater < Cask
+  url 'http://downloads.plexapp.com/plex-home-theater/1.0.7.169-303ab8cc/PlexHomeTheater-1.0.7.169-303ab8cc-macosx-i386.zip'
+  homepage 'http://plexapp.com'
+  version '1.0.7.169'
+  sha1 '1926d76623c5bb4205db7421042d4b5fdb671bfd'
+  link 'Plex Home Theater.app'
+end
