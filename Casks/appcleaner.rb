82e0d6bd3aa1190157e58d1ee4b48be393753d44
Fernando Paredes <nano@fdp.io>
2013-12-19 07:55:18 -0800

    AppCleaner SHA1 Update
    Fixes #2158

--- Casks/appcleaner.rb
+++ Casks/appcleaner.rb
@@ -2,6 +2,6 @@ class Appcleaner < Cask
   url 'http://www.freemacsoft.net/downloads/AppCleaner_2.2.1.zip'
   homepage 'http://www.freemacsoft.net/appcleaner/'
   version '2.2.1'
-  sha1 'af1c58b09cc7afe43fba9ceb44b20d588d17707a'
+  sha1 'd52dfbed9903799d21753be7fdd229c7277fba2a'
   link 'AppCleaner.app'
 end
