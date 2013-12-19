10d0feef275af56735dee22bf4943c1f97cfe3ff
Dillon Benson <dillon.benson93@gmail.com>
2013-12-19 00:02:38 -0500

    shows that there are no casks matching a search term that did not result in anything

--- test/cask/cli/search_test.rb
+++ test/cask/cli/search_test.rb
@@ -9,4 +9,10 @@ describe Cask::CLI::Search do
       intellij-idea-ultimate
     OUTPUT
   end
+
+  it "shows that there are no casks matching a search term that did not result in anything" do
+  	lambda {
+  		Cask::CLI::Search.run('foo-bar-baz')
+  	}.must_output("No casks matching: foo-bar-baz\n")
+  end
 end
