93767bcbe91e39d6976ebe8cf16b3d2a02c0d0fd
Brad Batt <brad@batt.cc>
2013-12-19 15:12:34 -0500

    Add Put.io Adder

---
+++ Casks/putio-adder.rb
@@ -0,0 +1,7 @@
+class PutioAdder < Cask
+  url 'https://github.com/nicoSWD/put.io-adder/releases/download/v2.5/put.io-adder-v2.5.zip'
+  homepage 'https://github.com/nicoSWD/put.io-adder'
+  version '2.5'
+  sha1 '10aea92409c04d77d4874f13f01c800c7913dff4'
+  link 'put.io adder.app'
+end
