82cb2f48d91d0b0f8915e98546d9f5cd4451e35f
Dominique Orban <dominique.orban@gmail.com>
2013-12-19 16:02:11 -0500

    New cask for Terminal Notifier 1.5.0.
    A command-line tool to send Mac OS X User Notifications, which are
    available in Mac OS X 10.8 and higher.

---
+++ Casks/terminal-notifier.rb
@@ -0,0 +1,7 @@
+class TerminalNotifier < Cask
+  url 'https://github.com/alloy/terminal-notifier/releases/download/1.5.0/terminal-notifier-1.5.0.zip'
+  homepage 'https://github.com/alloy/terminal-notifier'
+  version '1.5.0'
+  sha1 'd122d1e84f8d34053490dc80bc68381c1b731d47'
+  link 'terminal-notifier-1.5.0/terminal-notifier.app'
+end
