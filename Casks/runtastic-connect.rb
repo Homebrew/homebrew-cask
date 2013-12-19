0540545ae20f36154b38ffd490d3df52010cf071
Gabor Nagy <mail@aigeruth.hu>
2013-12-19 01:23:36 +0100

    Adds Runtastic Connect.

---
+++ Casks/runtastic-connect.rb
@@ -0,0 +1,7 @@
+class RuntasticConnect < Cask
+  url 'http://download.runtastic.com/connect/mac/runtasticConnect.dmg'
+  homepage 'https://www.runtastic.com/connect'
+  version 'latest'
+  no_checksum
+  link 'Runtastic Connect.app'
+end
