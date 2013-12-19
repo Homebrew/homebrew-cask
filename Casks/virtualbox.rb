c8596b04e88fd7984336a2808305c50486efc6fe
Yoichi Fujimoto <wozozo@gmail.com>
2013-12-19 16:53:47 +0900

    updated to VirtualBox 4.3.6-91406

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,8 +1,8 @@
 class Virtualbox < Cask
-  url 'http://download.virtualbox.org/virtualbox/4.3.4/VirtualBox-4.3.4-91027-OSX.dmg'
+  url 'http://download.virtualbox.org/virtualbox/4.3.6/VirtualBox-4.3.6-91406-OSX.dmg'
   homepage 'http://www.virtualbox.org'
-  version '4.3.4-91027'
-  sha1 '3583f2198940ff6461a526305d3c49a0638845bc'
+  version '4.3.6-91406'
+  sha1 'b268e52552449304ba8021687645a6f9d7d18920'
   install 'VirtualBox.pkg'
   uninstall :script => 'VirtualBox_Uninstall.tool', :args => %w[--unattended]
 end
