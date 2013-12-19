3259c9bc8bea12cfabb73889a1c4729ffa61672b
Fernando Paredes <nano@fdp.io>
2013-12-19 10:19:59 -0800

    Switch AppCleaner to no checksum
    #2160 #2158

--- Casks/appcleaner.rb
+++ Casks/appcleaner.rb
@@ -2,6 +2,6 @@ class Appcleaner < Cask
   url 'http://www.freemacsoft.net/downloads/AppCleaner_2.2.1.zip'
   homepage 'http://www.freemacsoft.net/appcleaner/'
   version '2.2.1'
-  sha1 'd52dfbed9903799d21753be7fdd229c7277fba2a'
+  no_checksum
   link 'AppCleaner.app'
 end
