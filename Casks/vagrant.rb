8d4351991107a8b0f358deee7b8f800e470280b1
Yoichi Fujimoto <wozozo@gmail.com>
2013-12-19 16:53:05 +0900

    updated to Vagrant 1.4.1

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,8 +1,8 @@
 class Vagrant < Cask
-  url 'https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.0.dmg'
+  url 'https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.1.dmg'
   homepage 'http://www.vagrantup.com'
-  version '1.4.0'
-  sha1 '84b27313b81c827fa9e831b9e4761224ca478451'
+  version '1.4.1'
+  sha1 '54a1ad09a99e79a59706bc7b0f0bfd8170786cbf'
   install 'Vagrant.pkg'
   uninstall :script => 'uninstall.tool', :input => %w[Yes]
 end
