b32a97a05cf348fa86b2153be45999d8a87855ff
Brad Batt <brad@batt.cc>
2013-12-19 14:54:12 -0500

    Add ScreenFlow

---
+++ Casks/screenflow.rb
@@ -0,0 +1,7 @@
+class Screenflow < Cask
+  url 'http://www.telestream.net/download-files/screenflow/4-5/ScreenFlow-4.5.dmg'
+  homepage 'http://www.telestream.net/screenflow/'
+  version '4.5'
+  sha1 '98c2091659eedb3573ea7ff864857cc4eff68077'
+  link 'ScreenFlow.app'
+end
