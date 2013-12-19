5fc5de567dce7dbc94405b5fc2397579a65921da
Vítor Galvão <info@vitorgalvao.com>
2013-12-19 01:41:54 +0000

    added rrootage

---
+++ Casks/rrootage.rb
@@ -0,0 +1,7 @@
+class Rrootage < Cask
+  url 'http://workram.com/archives/rRootage-for-OS-X-0.23.7.dmg'
+  homepage 'http://workram.com/games/rrootage/'
+  version '0.23.7'
+  sha1 'd24395c69e0a4255d16e39502cce95f1f7d06778'
+  link 'rRootage.app'
+end
