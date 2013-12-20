3232f62f70b8488e4ad168a03bd2699fd2cd064b
Geoffrey Oxberry <goxberry@gmail.com>
2013-12-19 23:01:24 -0800

    Update Zotero cask version to 4.0.17.
    Updates Zotero version to latest release version, 4.0.17.

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,7 +1,7 @@
 class Zotero < Cask
-  url 'http://download.zotero.org/standalone/4.0.16/Zotero-4.0.16.dmg'
+  url 'http://download.zotero.org/standalone/4.0.17/Zotero-4.0.17.dmg'
   homepage 'http://www.zotero.org/'
-  version '4.0.16'
-  sha1 '461752e20adb669471063b5ea1c573037d73ada3'
+  version '4.0.17'
+  sha1 '470365f88714d72a3ef99d4fb3be8b1cdf881d5d'
   link 'Zotero.app'
 end
