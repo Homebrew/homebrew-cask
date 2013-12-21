5daf7d3d889922f094857b608e659f33dacdbce1
Aaron Kaufman <aaron@aaronkaufman.com>
2013-12-21 07:38:58 -0800

    Goldenfrog Dumptruck version 1.2.2.1172.

---
+++ Casks/dumptruck.rb
@@ -0,0 +1,7 @@
+class Dumptruck < Cask
+  url 'http://www.goldenfrog.com/downloads/dumptruck/dumptruck.dmg'
+  homepage 'http://www.goldenfrog.com/dumptruck'
+  version '1.2.2.1172'
+  sha1 '669f2fd8efb7c183b867f712b2ec41640e3a95ef'
+  link 'Dump Truck.app'
+end
