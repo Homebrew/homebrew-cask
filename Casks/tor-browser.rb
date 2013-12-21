c835cfaa6e6b21a8ce580de74c953c2c93422d1d
sceox <sceox@riseup.net>
2013-12-20 17:05:26 -0800

    Update TorBrowser to 3.5

--- Casks/tor-browser.rb
+++ Casks/tor-browser.rb
@@ -1,7 +1,7 @@
 class TorBrowser < Cask
-  url 'https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.3.25-15-osx-x86_64-en-US.zip'
+  url 'https://archive.torproject.org/tor-package-archive/torbrowser/3.5/TorBrowserBundle-3.5-osx32_en-US.zip'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  version '2.3.25-15'
-  sha1 '2cbd2dc7426d88e1f29d2a8e5b810938e5b98df2'
-  link 'TorBrowser_en-US.app'
+  version '3.5'
+  sha1 '6603e490712f2acd5ef7b8254a9bf392'
+  link 'TorBrowserBundle_en-US.app'
 end
