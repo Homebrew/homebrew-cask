9b977bf5bea95c598cf8dda65ea578c2e328d9b9
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2013-12-21 15:46:37 +0100

    NetLogo 5.0.5
    First version of netlogo cask

---
+++ Casks/netlogo.rb
@@ -0,0 +1,7 @@
+class Netlogo < Cask
+  url 'http://ccl.northwestern.edu/netlogo/5.0.5/NetLogo%205.0.5.dmg'
+  homepage 'http://ccl.northwestern.edu/netlogo/'
+  version '5.0.5'
+  sha1 '6254fedb7afaf39eaf9f8531f2e670e9cfcc92e8'
+  link 'NetLogo 5.0.5/NetLogo 5.0.5.app'
+end
