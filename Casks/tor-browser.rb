f95022517017060240b53b53bd10a18d804d4bfc
sceox <sceox@riseup.net>
2013-12-20 18:04:46 -0800

    Change link to 3.5

--- Casks/tor-browser.rb
+++ Casks/tor-browser.rb
@@ -1,5 +1,5 @@
 class TorBrowser < Cask
-  url 'https://archive.torproject.org/tor-package-archive/torbrowser/3.5/TorBrowserBundle-3.5-osx32_en-US.zip'
+  url 'https://www.torproject.org/dist/torbrowser/3.5/TorBrowserBundle-3.5-osx32_en-US.zip'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   version '3.5'
   sha1 '6603e490712f2acd5ef7b8254a9bf392'
