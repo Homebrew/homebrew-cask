28df8b9b4a7d60a29bcaa14e32446775041ed561
Felipe "Zoc" Silveira <TheZoc@users.github.com>
2013-12-22 17:02:13 -0200

    Updated p4merge from 2013.1 to 2013.4

--- Casks/p4merge.rb
+++ Casks/p4merge.rb
@@ -1,8 +1,8 @@
 class P4merge < Cask
-  url 'http://filehost.perforce.com/perforce/r13.1/bin.macosx106x86_64/P4V.dmg'
+  url 'http://filehost.perforce.com/perforce/r13.4/bin.macosx106x86_64/P4V.dmg'
   homepage 'http://www.perforce.com/product/components/perforce-visual-merge-and-diff-tools'
-  version '13.1'
-  sha1 'b082af5840282bcff5c2445c586316f72cf71551'
+  version '2013.4'
+  sha1 '5b22abe5a405e9b48f4c76ed31766b933bb20c32'
   link 'p4merge.app'
 
   def caveats; <<-EOS.undent
