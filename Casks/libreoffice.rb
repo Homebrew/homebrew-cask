14c249b8c48e22799666abc1a2a6bfbeead4cfac
Michael Ford <fanquake@users.noreply.github.com>
2013-12-22 11:13:14 +0800

    Correct Libreoffice sha1

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -2,6 +2,6 @@ class Libreoffice < Cask
   url 'http://download.documentfoundation.org/libreoffice/stable/4.1.4/mac/x86/LibreOffice_4.1.4_MacOS_x86.dmg'
   homepage 'http://www.libreoffice.org/'
   version '4.1.4'
-  sha1 '8c4cd5600a73fac2dd430dbf493c11bab8ffd773'
+  sha1 '2a9e641071481391005804007b869b382990c47d'
   link 'LibreOffice.app'
 end
