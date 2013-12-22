0b935fa48bca8182aa43998ce6060835a64d2af8
Felipe "Zoc" Silveira <TheZoc@users.noreply.github.com>
2013-12-22 16:04:07 -0200

    Fixed arduino download URL

--- Casks/arduino.rb
+++ Casks/arduino.rb
@@ -1,5 +1,5 @@
 class Arduino < Cask
-  url 'http://arduino.googlecode.com/files/arduino-1.5.5-macosx.zip'
+  url 'http://downloads.arduino.cc/arduino-1.5.5-macosx.zip'
   homepage 'http://arduino.cc/'
   version '1.5.5'
   sha1 '89c8ba2c094efe0f410f1d23d1b42fd8529b4de2'
