9eb87c7725d8efcc4bd05e42fff0660672282734
Felipe "Zoc" Silveira <TheZoc@users.github.com>
2013-12-22 14:48:43 -0200

    Updated arduino from version 1.0.5 to 1.5.5
    This update adds support for Arduino Yun and Arduino Due.

--- Casks/arduino.rb
+++ Casks/arduino.rb
@@ -1,7 +1,7 @@
 class Arduino < Cask
-  url 'http://arduino.googlecode.com/files/arduino-1.0.5-macosx.zip'
+  url 'http://arduino.googlecode.com/files/arduino-1.5.5-macosx.zip'
   homepage 'http://arduino.cc/'
-  version '1.0.5'
-  sha1 'f806036a5c52b288a00db539b0431142371d101a'
+  version '1.5.5'
+  sha1 '89c8ba2c094efe0f410f1d23d1b42fd8529b4de2'
   link 'Arduino.app'
 end
