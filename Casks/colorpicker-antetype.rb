4aefac0dcf2b788f47601e7ace300623e9c07c43
Kevin Suttle <kevin@suttle.io>
2013-12-23 11:49:47 -0500

    Add Antetype and Propicker colorpickers

---
+++ Casks/colorpicker-antetype.rb
@@ -0,0 +1,7 @@
+class ColorpickerAntetype < Cask
+  url 'http://www.antetype.com/blog/downloads/AntetypeColorPickerOSX-1-3-1.zip'
+  homepage 'http://www.antetype.com/blog/2013/12/updated-antetype-os-x-color-picker-1-3-1/'
+  version '1.3.1'
+  sha1 'a7287ad2a6e1d899e0dacce6d9e8562d29cbd1ae'
+  colorpicker 'AntetypeColorPicker.colorPicker'
+end
