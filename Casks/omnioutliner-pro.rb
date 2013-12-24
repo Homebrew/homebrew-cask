6687802ce56de3fd15ed7cdfc5f8f0ce14b804ec
Scott Reeves <cottser@mailworks.org>
2013-12-23 23:03:10 -0500

    Add OmniOutliner Standard
    * Rename Pro to omnioutliner-pro and update URL slightly
    * Update homepage

---
+++ Casks/omnioutliner-pro.rb
@@ -0,0 +1,7 @@
+class OmnioutlinerPro < Cask
+  url 'http://www.omnigroup.com/download/latest/omnioutlinerPro'
+  homepage 'http://www.omnigroup.com/omnioutliner/'
+  version 'latest'
+  no_checksum
+  link 'OmniOutliner Professional.app'
+end
