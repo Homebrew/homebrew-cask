a18a490e9b1d746d922a8d1a461ebf7a658dfbb9
oame <oame@oameya.com>
2013-12-25 19:07:23 +0900

    Removed fixed version number.

--- Casks/airparrot.rb
+++ Casks/airparrot.rb
@@ -1,7 +1,7 @@
 class Airparrot < Cask
   url 'http://download.airsquirrels.com/AirParrot/Mac/AirParrot.dmg'
   homepage 'http://www.airsquirrels.com/airparrot/'
-  version '1.5.3'
-  sha1 '94449aa750077ccd244959d9e092bd4f3c6c65c3'
+  version 'latest'
+  no_checksum
   link 'AirParrot.app'
 end
