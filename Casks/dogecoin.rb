924511f378bc9c4165dab45397f557e408c604a7
Jonathan Chu <jonathan.chu@me.com>
2013-12-27 17:25:10 -0500

    Adding dogecoin-qt.app.
    Such Cask. So Awesome.

---
+++ Casks/dogecoin.rb
@@ -0,0 +1,7 @@
+class Dogecoin < Cask
+  url 'https://github.com/dogecoin/dogecoin/releases/download/v1.3/dogecoin-qt-v13-Mac.zip'
+  homepage 'http://dogecoin.com/'
+  version '1.3'
+  sha1 'ab78275b219966220f9caafaa0f9235c64ae82e0'
+  link 'dogecoin-qt.app'
+end
