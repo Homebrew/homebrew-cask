317f18e6accd92b402f239893a0700246e8bd55a
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2013-12-26 22:14:37 -0600

    Fix widget install and uninstall

---
+++ lib/cask/artifact/hardlinked.rb
@@ -0,0 +1,18 @@
+class Cask::Artifact::Hardlinked < Cask::Artifact::Symlinked
+  def islink?(path)
+    return false unless path.respond_to?(:stat)
+    path.stat.nlink > 1
+  end
+
+  def link_action(source, target)
+    File.link(source, target)
+  end
+
+  def unlink_action(target)
+    FileUtils.rm_rf target
+  end
+
+  def link_name
+    'hardlink'
+  end
+end
