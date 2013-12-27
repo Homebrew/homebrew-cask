f71a0dfc0f7be87f65127a29e157269fbac19078
sceox <sceox@riseup.net>
2013-12-27 13:12:56 -0800

    Add Snow Leopard version
    Still installs latest on 10.7 or greater, but now installs legacy version for
    10.6 compatibility.

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -4,9 +4,9 @@ class Daisydisk < Cask
     version 'latest'
     no_checksum
   else
-      url 'http://www.daisydiskapp.com/downloads/DaisyDisk_2_1_2.dmg'
-      version '2.1.2'
-      sha1 '584eb479c41e2e73142fd69ce77bc57470d718de'
+    url 'http://www.daisydiskapp.com/downloads/DaisyDisk_2_1_2.dmg'
+    version '2.1.2'
+    sha1 '584eb479c41e2e73142fd69ce77bc57470d718de'
   end
   homepage 'http://www.daisydiskapp.com'
   link 'DaisyDisk.app'
