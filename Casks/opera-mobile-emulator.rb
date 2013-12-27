e60510754e52721d514257b9e7421f27d698f988
Kevin Suttle <kevin@suttle.io>
2013-12-26 23:50:01 -0500

    Add cask for Opera Mobile Emulator

---
+++ Casks/opera-mobile-emulator.rb
@@ -0,0 +1,7 @@
+class OperaMobileEmulator < Cask
+  url 'http://get.geo.opera.com/pub/opera/sdlbream/1210/Opera_Mobile_Emulator_12.1_Mac.dmg'
+  homepage 'http://www.opera.com/developer/mobile-emulator'
+  version '12.1'
+  sha1 ''
+  link 'Opera Mobile Emulator.app'
+end
