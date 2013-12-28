b147929d186b31cde352c1976dea6eded7a4292d
Michael Ford <fanquake@users.noreply.github.com>
2013-12-28 22:57:06 +0800

    Dashlane 2.3.2.52081

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,7 +1,7 @@
 class Dashlane < Cask
-  url 'https://d3mfqat9ni8wb5.cloudfront.net/releases/2.3.0/2.3.0.51086/Dashlane.dmg'
+  url 'https://d3mfqat9ni8wb5.cloudfront.net/releases/2.3.2/2.3.2.52081/Dashlane.dmg'
   homepage 'https://www.dashlane.com/'
-  version '2.3.0.51086'
-  sha1 '78bc9172bcd3e28e58903a13a554e3b12cea324a'
+  version '2.3.2.52081'
+  sha1 '9b083d7f6b86a8791aa4f3f948fd7d0d2c3369c7'
   link 'Dashlane.app'
 end
