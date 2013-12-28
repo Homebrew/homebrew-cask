3b83054f92c13ab7dc976314b854db2294995c3b
Michael Ford <fanquake@users.noreply.github.com>
2013-12-28 23:24:46 +0800

    Doxygen 1.8.6

--- Casks/doxygen.rb
+++ Casks/doxygen.rb
@@ -1,7 +1,7 @@
 class Doxygen < Cask
-  url 'ftp://ftp.stack.nl/pub/users/dimitri/Doxygen-1.8.5.dmg'
+  url 'ftp://ftp.stack.nl/pub/users/dimitri/Doxygen-1.8.6.dmg'
   homepage 'http://www.stack.nl/~dimitri/doxygen/index.html'
-  version '1.8.5'
-  sha1 '38011a9ffd4a18892f511ecf83154f5d0687cfcc'
+  version '1.8.6'
+  sha1 '5416af8df876d7388ad6d062633acff03a9ec6c0'
   link 'Doxygen.app'
 end
