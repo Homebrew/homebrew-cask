49b4c2d567b8925cc7d6c1362f9d5f67903d6a8f
Yang Bo <yangbodotnet@gmail.com>
2013-12-29 11:55:56 +0800

    checksum on 1.3.0 version

--- Casks/qq-music.rb
+++ Casks/qq-music.rb
@@ -2,6 +2,6 @@ class QqMusic < Cask
   url 'http://dldir1.qq.com/music/clntupate/QQMusicForMacV1.3.0.dmg'
   homepage 'http://y.qq.com'
   version '1.3.0'
-  no_checksum
+  sha1 '5ce10f93466682ebe1ab5b8fbf981861be693c12'
   link 'QQ音乐.app'
 end
