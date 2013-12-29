97df37b7d80f9e19fb86dfcd4de0d1658bf228ca
Lincoln Lee <git@lincoln.hk>
2013-12-29 11:20:10 +0800

    Update kext-wizard with no_checksum
    If the version is latest there should be no_checksum

--- Casks/kext-wizard.rb
+++ Casks/kext-wizard.rb
@@ -2,6 +2,6 @@ class KextWizard < Cask
   url 'http://my-stead.com/Janek/kw.zip'
   homepage 'http://www.insanelymac.com/forum/topic/253395-kext-wizard-easy-to-use-kext-installer-and-more/'
   version 'latest'
-  sha1 '1f5d515b3aace3ccddbf0c759eaf149e59b2f9c6'
+  no_checksum
   link 'Kext Wizard.app'
 end
