12e23f1719096fbe245c34a3b0b1ea3c0713dec4
Lachlan Cooper <lachlancooper@gmail.com>
2013-12-30 17:17:07 +0100

    Add cask for Awareness

---
+++ Casks/awareness.rb
@@ -0,0 +1,7 @@
+class Awareness < Cask
+  url 'http://iamfutureproof.com/downloads/Awareness-1.1.dmg'
+  homepage 'http://iamfutureproof.com/tools/awareness/'
+  version '1.1'
+  sha1 '455ef7bb55ee941379773c8ff370f18ee7c91b88'
+  link 'Awareness.app'
+end
