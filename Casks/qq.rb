da1474902331e025a715975be2b74f72404a7f56
Sun Yuanhui <syhkiller@163.com>
2013-12-30 12:57:47 +0800

    fix qq v3.0.2 sha1sum

--- Casks/qq.rb
+++ Casks/qq.rb
@@ -2,6 +2,6 @@ class Qq < Cask
   url 'http://dldir1.qq.com/qqfile/QQforMac/QQ_V3.0.2.dmg'
   homepage 'http://im.qq.com/macqq/index.shtml'
   version '3.0.2'
-  sha1 '8d9d8c9ace5e11dacf73a270b2852945a5f73b57'
+  sha1 'ff45bde19ca35b8f8c83510288dd4575d23c4e1e'
   link 'QQ.app'
 end
