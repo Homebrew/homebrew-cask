13d208bd21617fe87778a65c87637290149799a5
Wang Guan <momocraft@gmail.com>
2014-01-01 04:15:33 +0900

    add curse client

---
+++ Casks/curse-client.rb
@@ -0,0 +1,7 @@
+class CurseClient < Cask
+  url 'http://addons.cursecdn.com/files/595/169/Curse_Client-4.0.0.425.dmg'
+  homepage 'http://www.curse.com/client'
+  version '4.0.0.425'
+  sha1 '4a095cff47e4bd9d68bafba9bfb8420fdfbc06d4'
+  link 'Curse Client.app'
+end
