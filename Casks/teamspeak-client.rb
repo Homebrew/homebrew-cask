0788929d190acfeda0c9c787f163d7c95820f0ec
Felipe "Zoc" Silveira <TheZoc@users.noreply.github.com>
2013-12-31 02:48:51 -0200

    Updated Teamspeak 3 Client to version 3.0.13.1

--- Casks/teamspeak-client.rb
+++ Casks/teamspeak-client.rb
@@ -1,7 +1,7 @@
 class TeamspeakClient < Cask
-  url 'http://ftp.4players.de/pub/hosted/ts3/releases/3.0.13/TeamSpeak3-Client-macosx-3.0.13.dmg'
+  url 'http://dl.4players.de/ts/releases/3.0.13.1/TeamSpeak3-Client-macosx-3.0.13.1.dmg'
   homepage 'http://www.teamspeak.com/'
-  version '3.0.13'
-  sha1 'a0d287dfb92de62ec8e1a3928da9719bff4b7d31'
+  version '3.0.13.1'
+  sha1 '99cd5c9acc7d824ff31ea76033c568d1460782fe'
   link 'TeamSpeak 3 Client.app'
 end
