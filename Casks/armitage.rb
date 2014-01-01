1b449debd48c6b79613f90174d05093676d6a51b
Michael Ford <fanquake@users.noreply.github.com>
2014-01-01 23:22:06 +0800

    Armitage 11.21.13

--- Casks/armitage.rb
+++ Casks/armitage.rb
@@ -1,7 +1,7 @@
 class Armitage < Cask
-  url 'http://www.fastandeasyhacking.com/download/armitage20130821.dmg'
+  url 'http://www.fastandeasyhacking.com/download/armitage20131121.dmg'
   homepage 'http://www.fastandeasyhacking.com/'
-  version '08.21.13'
-  sha1 '667f25eae9dbd05fa7a91223e01deffd9a124c6a'
+  version '11.21.13'
+  sha1 '6f69e7467eaddaaa350c726253887bc09126f4e1'
   link 'Armitage.app'
 end
