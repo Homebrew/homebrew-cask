9798bfb625e2c55806bb0fd359ceeb14fb9fb9eb
Michael Ford <fanquake@users.noreply.github.com>
2014-01-02 02:05:45 +0800

    TotalFinder 1.5.6

--- Casks/totalfinder.rb
+++ Casks/totalfinder.rb
@@ -1,8 +1,8 @@
 class Totalfinder < Cask
-  url 'http://downloads.binaryage.com/TotalFinder-1.5.4.dmg'
+  url 'http://downloads.binaryage.com/TotalFinder-1.5.6.dmg'
   homepage 'http://totalfinder.binaryage.com'
-  version '1.5.4'
-  sha1 'aafaca6d516740726ef8c5422622289ed9a5b76e'
+  version '1.5.6'
+  sha1 'ce0e2fe7fc3bd7b98622c95e2bb73e9fb2f55546'
   install 'TotalFinder.pkg'
   uninstall :script => 'TotalFinder Uninstaller.app/Contents/MacOS/TotalFinder Uninstaller',
             :args => %w[--headless]
