70694cec50b1d43525389e47cba43cdb5ff47c5c
Graham Miln <graham.miln@dssw.co.uk>
2014-01-02 18:48:20 +0100

    Removing duplicate authbuddy cask.

--- Casks/authbuddy.rb
+++
@@ -1,8 +0,0 @@
-class Authbuddy < Cask
-  url 'https://www.dssw.co.uk/authbuddy/dsswauthbuddy.dmg'
-  homepage 'https://www.dssw.co.uk/authbuddy'
-  version 'latest'
-  no_checksum
-  install 'DssW authbuddy.pkg'
-  uninstall :files => Array['/usr/local/bin/authbuddy']
-end
