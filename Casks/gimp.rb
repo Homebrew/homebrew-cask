ae3023843ab775d739f89a5b5efba3822280dc77
Ken Sato <ksato9700@gmail.com>
2014-01-03 08:51:05 +0900

    update gimp to v2.8.10

--- Casks/gimp.rb
+++ Casks/gimp.rb
@@ -1,7 +1,7 @@
 class Gimp < Cask
-  url 'ftp://ftp.gimp.org/pub/gimp/v2.8/osx/gimp-2.8.4-nopython-dmg-1.dmg'
+  url 'ftp://ftp.gimp.org/pub/gimp/v2.8/osx/gimp-2.8.10-dmg-1.dmg'
   homepage 'http://www.gimp.org'
-  version '2.8.4'
-  sha1 'f478ffd8c865bb67b50b6c75f2a68278544d6f79'
+  version '2.8.10'
+  sha1 'd4610fb4d9164179112a7b1d0cb43387887301c8'
   link 'GIMP.app'
 end
