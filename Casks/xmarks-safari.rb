d22272fb488993d22244ea1e764a9e94008535dd
Kevin Suttle <kevin@suttle.io>
2014-01-01 23:40:36 -0500

    Add Xmarks for Safari cask

---
+++ Casks/xmarks-safari.rb
@@ -0,0 +1,7 @@
+class XmarksSafari < Cask
+ url 'https://static.xmarks.com/clients/safari/xmarks_for_safari_2.0.11.dmg'
+ homepage 'http://www.xmarks.com/'
+ version '2.0.11'
+ sha1 'eae9ce0865bf1d4a3e6a2ff020793b36efdf1b51'
+ install 'Xmarks for Safari Installer.pkg'
+end
