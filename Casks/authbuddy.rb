91d51ed8f3b0dd416e8183b6b9e58abeb0e74411
Graham Miln <graham.miln@dssw.co.uk>
2014-01-02 18:31:39 +0100

    Updated to latest version approach
    Following @NanoXD advice to use latest for version and disable checksum.

--- Casks/authbuddy.rb
+++ Casks/authbuddy.rb
@@ -1,8 +1,8 @@
 class Authbuddy < Cask
   url 'https://www.dssw.co.uk/authbuddy/dsswauthbuddy.dmg'
   homepage 'https://www.dssw.co.uk/authbuddy'
-  version '1.1.0'
-  sha1 'c994c499d618f8aa4659655131453b1986e140fe'
+  version 'latest'
+  no_checksum
   install 'DssW authbuddy.pkg'
   uninstall :files => Array['/usr/local/bin/authbuddy']
 end
