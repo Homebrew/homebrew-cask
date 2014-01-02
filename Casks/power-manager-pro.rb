ad6fafb92b3e2bc13e380bffd2fbb919dbbd3d1b
Graham Miln <graham.miln@dssw.co.uk>
2014-01-02 18:35:11 +0100

    Updated to latest version approach
    Following @NanoXD advice to use latest for version and disable checksum.

--- Casks/power-manager-pro.rb
+++ Casks/power-manager-pro.rb
@@ -1,7 +1,7 @@
 class PowerManagerPro < Cask
   url 'https://www.dssw.co.uk/powermanager/dsswpowermanagerpro.dmg'
   homepage 'https://www.dssw.co.uk/powermanager'
-  version '4.1.8'
-  sha1 '0aa8b098771e2b793b26b6734e3b2e9230c87f31'
+  version 'latest'
+  no_checksum
   link 'Power Manager Pro.app'
 end
