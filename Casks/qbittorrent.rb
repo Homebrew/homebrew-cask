ee8dbe5e33c8ed87cdfe56645c0a203656b75d9e
pyed <iAbdulelah@Gmail.com>
2014-01-03 21:54:09 +0300

    Add cask for qBittorrent
    Free and reliable P2P Bittorrent client.

---
+++ Casks/qbittorrent.rb
@@ -0,0 +1,7 @@
+class Qbittorrent < Cask
+  url 'http://sourceforge.net/projects/qbittorrent/files/qbittorrent-mac/qbittorrent-3.1.3/qbittorrent-3.1.3.dmg/download'
+  homepage 'http://www.qbittorrent.org'
+  version '3.1.3'
+  sha1 'bc0421cf2481e3740d1522689bd90b0c626a8544'
+  link 'qBittorrent.app'
+end
