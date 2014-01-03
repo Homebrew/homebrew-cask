f90789aa1efe4e820ac6dca30d3f80a5a88a3dfa
Zach Olauson <zmolauson@gmail.com>
2014-01-03 10:16:51 -0600

    add navicat casks
    added a cask for each navicat:
    Data Modeler, MariaDB, MySQL, Oracle, PostgreSQL, Premium, SQLite, and SQL Server

---
+++ Casks/navicat-postgresql.rb
@@ -0,0 +1,7 @@
+class NavicatPostgresql < Cask
+  url 'http://download.navicat.com/download/navicat110_pgsql_en.dmg'
+  homepage 'http://www.navicat.com/products/navicat-for-postgresql'
+  version '11.0.14'
+  sha1 '350e590c97b2b066db43a33f2c6aa2a49544a01f'
+  link 'Navicat for PostgreSQL.app'
+end
