bba5576871affcae1d77d351e05477a7183764ba
omardelarosa <thedelarosa@gmail.com>
2014-01-03 17:06:31 -0500

    Added a Cask for EasySIMBL as easysimbl.rb
    EasySIMBL is a MacOSX Lion-friendly version of SIMBL, a bundle loader.

---
+++ Casks/easysimbl.rb
@@ -0,0 +1,7 @@
+class Easysimbl < Cask
+  url 'http://github.com/norio-nomura/EasySIMBL/releases/download/EasySIMBL-1.6/EasySIMBL-1.6.zip'
+  homepage 'https://github.com/norio-nomura/EasySIMBL/'
+  version '1.6'
+  sha1 '52ac6c3575cbefcad25bb13703e8b09de319bbe9'
+  link 'EasySIMBL.app'
+end
