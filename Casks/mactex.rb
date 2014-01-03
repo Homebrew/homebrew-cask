6e398ca9b2aedf23eba5c1a972e5024f5c11db8a
Colin Dean <git@cad.cx>
2014-01-02 19:39:06 -0500

    Add PATH caveats to MacTeX Cask
    Warns user that they may need to add the mactex `/usr/texbin` directory to their
    PATH manually, as the package does not seem to do this or symlink automatically.
    
    Fixes #2277.
    
    See also [this comment](https://github.com/phinze/homebrew-cask/pull/2284#commitcomment-4974770)
    on #2284 for more information.

--- Casks/mactex.rb
+++ Casks/mactex.rb
@@ -5,4 +5,12 @@ class Mactex < Cask
   no_checksum
   install 'MacTeX.pkg'
   uninstall :pkgutil => 'org.tug.mactex.texlive2013'
+  
+  def caveats; <<-EOS.undent
+    You may need to add the MacTeX bin directory to your PATH.
+    
+    export PATH=/usr/texbin:$PATH
+    
+    EOS
+  end
 end
