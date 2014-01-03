c9776138d8c13c93213d9f623880e881dacb985a
Alexey <anakros.symbian@gmail.com>
2014-01-03 18:50:13 +0700

    updated redis desktop manager to 0.7.5

--- Casks/redis-desktop-manager.rb
+++ Casks/redis-desktop-manager.rb
@@ -1,7 +1,7 @@
 class RedisDesktopManager < Cask
-  url 'https://github.com/uglide/RedisDesktopManager/releases/download/0.7.1/redis-desktop-manager-0.7.1.dmg'
+  url 'https://github.com/uglide/RedisDesktopManager/releases/download/0.7.5/redis-desktop-manager-0.7.5.dmg'
   homepage 'http://redisdesktop.com'
-  version '0.7.1'
-  sha1 'ca743be26f08a6127c89f8a59e1fe252cfc716cc'
+  version '0.7.5'
+  sha1 '8e6dbe110be546e6a0660c95429f83ffbb06b3fe'
   link 'rdm.app'
 end
