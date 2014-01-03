d45ef4809f012f99a8402cb1570ec51d9172c6ec
finitum <adriftinhilbertspace@lavabit.com>
2014-01-03 13:05:54 +0100

    Update and rename sonicvisualiser.rb to sonic-visualiser.rb
    Rename class to SonicVisualiser, according to the corrected formula filename.

--- Casks/sonic-visualiser.rb
+++ Casks/sonic-visualiser.rb
@@ -1,4 +1,4 @@
-class Sonicvisualiser < Cask
+class SonicVisualiser < Cask
   url 'http://code.soundsoftware.ac.uk/attachments/download/907/Sonic%20Visualiser-2.3.dmg'
   homepage 'http://www.sonicvisualiser.org/'
   version '2.3'
