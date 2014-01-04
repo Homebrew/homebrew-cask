a67d9ce75dff0eaa81bd9cc07cf9281aa210bdba
grahammiln <graham.miln@miln.eu>
2014-01-04 12:18:11 +0100

    Adding cask for Activity Audit
    Adding a cask for application time tracking software DssW Activity
    Audit.

---
+++ Casks/activity-audit.rb
@@ -0,0 +1,7 @@
+class ActivityAudit < Cask
+  url 'https://www.dssw.co.uk/activityaudit/dsswactivityaudit.dmg'
+  homepage 'https://www.dssw.co.uk/activityaudit'
+  version 'latest'
+  no_checksum
+  link 'Activity Audit.app'
+end
