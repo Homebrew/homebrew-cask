d0225d49596d9bf53a318e84d0d77894c8d63ead
Andreas Boeckler <abo@netlands.de>
2014-01-05 13:14:06 +0100

    Gimp 2.8.10->2.8.10p2; fixes nasty outline bug

--- Casks/gimp-lisanet.rb
+++ Casks/gimp-lisanet.rb
@@ -1,7 +1,7 @@
 class GimpLisanet < Cask
-  url 'http://downloads.sourceforge.net/gimponosx/Gimp-2.8.10-Mavericks.dmg'
+  url 'http://downloads.sourceforge.net/gimponosx/Gimp-2.8.10p2-Mavericks.dmg'
   homepage 'http://gimp.lisanet.de'
-  version '2.8.10'
-  sha1 '026d1fb3eb9ebdc1365bfc71fb8d66f943731bd9'
+  version '2.8.10p2'
+  sha1 '4da8384b13171bd3eb5c46ec7d35be1331b4e256'
   link 'GIMP.app'
 end
