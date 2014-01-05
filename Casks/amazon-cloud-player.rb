2236e7cab3b26723e9da8eadbc2669ca5c9ed391
Kevin Suttle <kevin@suttle.io>
2014-01-04 23:38:48 -0500

    Add cask for Amazon Cloud Player

---
+++ Casks/amazon-cloud-player.rb
@@ -0,0 +1,7 @@
+class AmazonCloudPlayer < Cask
+  url 'https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/AmazonCloudPlayerInstaller_85.dmg'
+  homepage 'https://www.amazon.com/gp/feature.html/ref=dm_mo_cpw_fb_lm?docId=1001067901'
+  version '0.85'
+  sha1 '14421459ff5ac409835652297989112654389d83'
+  link 'Amazon Cloud Player Installer.app'
+end
