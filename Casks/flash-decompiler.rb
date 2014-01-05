9df7bf80364aa6d286ec16c10c3f8e6b105d14b9
Lee Hanxue <hanxue.developer@gmail.com>
2014-01-06 07:50:37 +0800

    Flash Decompiler

---
+++ Casks/flash-decompiler.rb
@@ -0,0 +1,7 @@
+class FlashDecompiler < Cask
+  url 'http://www.flash-decompiler.com/download/flash_decompiler.dmg'
+  homepage 'http://www.flash-decompiler.com/mac.html'
+  version 'latest'
+  no_checksum
+  link 'Flash Decompiler Trillix.app'
+end
