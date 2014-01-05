a1da1fcdc9f8762a7f6362a265348b1e033afc30
Kane Dou <douqilong@gmail.com>
2014-01-05 14:09:17 +0800

    fix dbeaver link path

--- Casks/dbeaver.rb
+++ Casks/dbeaver.rb
@@ -3,5 +3,5 @@ class Dbeaver < Cask
   homepage 'http://dbeaver.jkiss.org/'
   version '2.3.5'
   sha1 'd50feb84e211bd9e8911502a20280f748ba45498'
-  link 'dbeaver.app'
+  link 'dbeaver/dbeaver.app'
 end
