aff087967a0ad04b178cb80502969fcd7f507093
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-01-06 14:36:02 +0100

    Update transmission-remote-gui to v5.0.1

--- Casks/transmission-remote-gui.rb
+++ Casks/transmission-remote-gui.rb
@@ -1,8 +1,8 @@
 class TransmissionRemoteGui < Cask
-  url 'https://transmisson-remote-gui.googlecode.com/files/transgui-5.0.dmg'
+  url 'https://transmisson-remote-gui.googlecode.com/files/transgui-5.0.1.dmg'
   homepage 'https://code.google.com/p/transmisson-remote-gui/'
-  version '5.0'
-  sha1 '3a59a1ff69e4f80500b7fb010146d644920e7cc0'
+  version '5.0.1'
+  sha1 '52905d9ba6c3247410c6048a367d602942ea3456'
   install 'transgui.pkg'
   uninstall :pkgutil => 'com.transgui.*'
 end
