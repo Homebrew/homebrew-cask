9752dcbedc6060e7f877e94a56ad933e51b3e850
Ali Karbassi <ali@karbassi.com>
2014-01-06 01:37:43 -0500

    Update processing link to pass

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -1,5 +1,5 @@
 class Processing < Cask
-  url 'http://download.processing.org/processing-2.1-macosx.zip'
+  url 'http://processing.googlecode.com/files/processing-2.1-macosx.zip'
   homepage 'http://processing.org/'
   version '2.1'
   sha1 '4e51f26c8bd0d13a28cf70f367d4966c396d6079'
