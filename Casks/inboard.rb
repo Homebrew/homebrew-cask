ba8ec3a4f787dc1b3e62efa498e78c570994781b
Ali Karbassi <ali@karbassi.com>
2014-01-06 01:39:22 -0500

    No version.

--- Casks/inboard.rb
+++ Casks/inboard.rb
@@ -1,7 +1,7 @@
 class Inboard < Cask
   url 'http://dl.devmate.com/com.ideabits.Inboard/Inboard.dmg'
   homepage 'http://inboardapp.com'
-  version '183'
-  sha1 'a2fb883416517a88811382d846e10f4737ee410f'
+  version 'latest'
+  sha1 ''
   link 'Inboard.app'
 end
