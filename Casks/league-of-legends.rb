8c2173f00037512e485e517f4ca74e83ae9a303f
Paul Hinze <paul.t.hinze@gmail.com>
2014-01-10 13:13:59 -0600

    fix league-of-legends with proper no_checksum

--- Casks/league-of-legends.rb
+++ Casks/league-of-legends.rb
@@ -2,9 +2,9 @@ class LeagueOfLegends < Cask
   url 'http://l3cdn.riotgames.com/Installer/NA_Mac_Installer/League%20of%20Legends%20NA.dmg'
   homepage 'http://signup.leagueoflegends.com/'
   version 'latest'
-  sha1 'no_checksum'
+  no_checksum
   link 'League of Legends.app'
-  
+
   def caveats; <<-EOS.undent
     You need to run #{destination_path/'League of Legends.app'} to update the game to the latest version.
     EOS
