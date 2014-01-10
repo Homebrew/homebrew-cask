3261ccb60fe66478c8b7c1d7f2617661a09aa393
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2014-01-10 21:35:15 +0100

    Added :pkgutil uninstall option

--- Casks/mosh.rb
+++ Casks/mosh.rb
@@ -4,5 +4,5 @@ class Mosh < Cask
   version '1.2.4'
   sha1 '1ea08056a7b0bb30c0a8b0ddbb9f2f3c26e60ef2'
   install 'mosh-1.2.4-3.pkg'
-  uninstall :files => ["/usr/bin/mosh", "/usr/bin/mosh-client", "/usr/bin/mosh-server", "/usr/share/man/man1/mosh-client.1", "/usr/share/man/man1/mosh-server.1?", "/usr/share/man/man1/mosh.1"]
+  uninstall :pkgutil => 'edu.mit.mosh.mosh.pkg'
 end
