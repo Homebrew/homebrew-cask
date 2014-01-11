7df6b38fb897865446523d8f89b34cf58009a087
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-01-11 10:47:29 -0600

    Aptana Studio 3.5.0

---
+++ Casks/aptana-studio.rb
@@ -0,0 +1,7 @@
+class AptanaStudio < Cask
+  url 'http://download.aptana.com/studio3/standalone/3.5.0/mac/Aptana_Studio_3_Setup_3.5.0.dmg'
+  homepage 'http://www.aptana.com/'
+  version '3.5.0'
+  sha1 '6a509859583a53ef72d240dee1c4552bc2382614'
+  link 'Aptana Studio 3'
+end
