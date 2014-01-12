64904223fe6d5a1a8c2cda4230fd2d8121da2d82
gaoyifan <git@gaoyifan.com>
2014-01-12 18:46:58 +0800

    update xunlei to 2.2.0

--- Casks/xunlei.rb
+++ Casks/xunlei.rb
@@ -1,7 +1,7 @@
 class Xunlei < Cask
-  url 'http://down.sandai.net/mac/thunder_dl2.1.0.1120_Beta.dmg'
+  url 'http://down.sandai.net/mac/thunder_dl2.2.0.1228_Beta.dmg'
   homepage 'http://mac.xunlei.com/'
-  version '2.1.0.1120_Beta'
-  sha1 '82ec6d5751feb1f4c7c811873cad5edfb0a2c536'
+  version '2.2.0.1228_Beta'
+  sha1 'cd27d995d46a8de1a245dc7ba728177fad25bb49'
   link 'Thunder.app'
 end
