8322909504e6b918524fa370e55332205696d601
gaoyifan <git@gaoyifan.com>
2014-01-12 19:20:31 +0800

    add baiduCloud with version 2.4.0

---
+++ Casks/baiducloud.rb
@@ -0,0 +1,7 @@
+class Baiducloud < Cask
+  url 'http://bcscdn.baidu.com/netdisk/BaiduYun_2.4.0.dmg'
+  homepage 'http://pan.baidu.com'
+  version '2.4.0'
+  sha1 'dbc2f55028049d1dcef8effe12a47c6e9d355ac2'
+  link '百度云同步盘.app'
+end
