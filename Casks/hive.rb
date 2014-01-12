6c33dd20a5b366945197fabbc3c3a3c268f50615
Aaron Kaufman <aaron@aaronkaufman.com>
2014-01-11 17:30:03 -0800

    Added Hive app
    Hive is an integrated, user-friendly Bitcoin wallet for Mac OS X.

---
+++ Casks/hive.rb
@@ -0,0 +1,7 @@
+class Hive < Cask
+  url 'https://github.com/hivewallet/hive-osx/releases/download/2014011002/Hive-2014011002.zip'
+  homepage 'http://www.grabhive.com'
+  version '0.9'
+  sha1 'd83c1e6ad9b1eb7b0068981d72112ee1d4b8f9e3'
+  link 'Hive.app'
+end
