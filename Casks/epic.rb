ca5e91262bd8b883c0567189579ff18c9458d915
Aaron Kaufman <aaron@aaronkaufman.com>
2014-01-12 12:59:01 -0800

    Added Epic browser

---
+++ Casks/epic.rb
@@ -0,0 +1,7 @@
+class Epic < Cask
+  url 'https://ed5b681d56298a85550d-7d665255a6e48f36b11ee3cfeece77e0.ssl.cf1.rackcdn.com/epic_mac_33_alternate/Epic.dmg'
+  homepage 'http://www.epicbrowser.com'
+  version 'latest'
+  no_checksum
+  link 'Epic.app'
+end
