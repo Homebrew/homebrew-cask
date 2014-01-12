b078990f566e5f2be5ea22c759b5666da83b90ee
finitum <adriftinhilbertspace@lavabit.com>
2014-01-12 22:53:08 +0100

    Change sourceforge url for SuperCollider to latest

--- Casks/supercollider.rb
+++ Casks/supercollider.rb
@@ -1,5 +1,5 @@
 class Supercollider < Cask
-  url 'http://sourceforge.net/projects/supercollider/files/Mac%20OS%20X/3.6/SuperCollider-3.6.6-OSX.dmg'
+  url 'http://sourceforge.net/projects/supercollider/files/latest/download'
   homepage 'http://supercollider.sourceforge.net/'
   version '3.6.6'
   sha1 '8ca786becbc8c6d2464d39615aab39ea66066506'
