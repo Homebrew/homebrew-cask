6207358e576fefd27d1f1ebd7206bda23d50ef52
Aaron Kaufman <aaron@aaronkaufman.com>
2014-01-11 18:00:46 -0800

    VyprVPN 2.1.0

---
+++ Casks/vyprvpn.rb
@@ -0,0 +1,7 @@
+class Vyprvpn < Cask
+  url 'http://www.goldenfrog.com/downloads/vyprvpn/desktop/VyprVPN-2.1.0.dmg'
+  homepage 'http://www.goldenfrog.com/vyprvpn'
+  version '2.1.0'
+  sha1 '2d53d74fb64c17f3d68453c4dd6d58dabf345260'
+  link 'VyprVPN.app'
+end
