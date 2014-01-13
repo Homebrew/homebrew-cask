6cbe7fb19120d29f7a4ff6036ec6eec7bcb5f3f8
gaoyifan <git@gaoyifan.com>
2014-01-13 20:25:06 +0800

    add kugou with version 1.0.1

---
+++ Casks/kugou.rb
@@ -0,0 +1,7 @@
+class Kugou < Cask
+  url 'http://macservice.kugou.com/download/KugouMusicForMac.dmg'
+  homepage 'http://www.kugou.com'
+  version '1.0.1'
+  sha1 '73a5333001be30fe67a7875dc5cb833c344af299'
+  link 'KugouMusic.app'
+end
