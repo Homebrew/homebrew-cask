13a7f74afb4fb199d4cdbf20a2edd41270063fe1
gaoyifan <git@gaoyifan.com>
2014-01-13 21:57:25 +0800

    add powerword with version 1.0.0_beta

---
+++ Casks/powerword.rb
@@ -0,0 +1,7 @@
+class Powerword < Cask
+  url 'http://mac.iciba.com/download/powerword_macosx_beta_1.0.0.dmg'
+  homepage 'http://mac.iciba.com'
+  version '1.0.0_beta'
+  sha1 'a74ff001960d2d03111b4d5dbdc546ea2769d356'
+  link '金山词霸.app'
+end
