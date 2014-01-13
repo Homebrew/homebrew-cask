d590503690979b9462b2abca2ea4b5b06970d1df
gaoyifan <git@gaoyifan.com>
2014-01-13 23:17:11 +0800

    add new cask kugou

---
+++ Casks/kugou.rb
@@ -0,0 +1,7 @@
+class Kugou < Cask
+  url 'http://macservice.kugou.com/download/KugouMusicForMac.dmg'
+  homepage 'http://www.kugou.com'
+  version 'latest'
+  no_checksum
+  link 'KugouMusic.app'
+end
