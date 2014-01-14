0691178dfc756358c62ab4fb8ccb5bb0c7215b20
Roland Walker <walker@pobox.com>
2014-01-13 20:38:43 -0500

    Fix #2343, wireshark "can't dup Symbol"

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -3,7 +3,6 @@ class Wireshark < Cask
   homepage 'http://www.wireshark.org'
   version '1.10.5'
   sha1 '59c496f52437ce0d2b836abbf546191f50345c8e'
-  link :none
   install 'Wireshark 1.10.5 Intel 64.pkg'
   uninstall :pkgutil => 'org.wireshark.*', 
             :files => %w(capinfos dftest dumpcap editcap mergecap randpkt
