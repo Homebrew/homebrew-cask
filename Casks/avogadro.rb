a57a82c96fa2177756c5737f3c8ffeec6c4de316
Sean Anderson <sean.martin.anderson@gmail.com>
2014-01-14 01:38:55 -0600

    Added a cask for Avogadro.
    Avogadro is an open source molecule toolkit. You can analyze, visualize,
    and manipulate molecular structures from a variety of formats.

---
+++ Casks/avogadro.rb
@@ -0,0 +1,7 @@
+class Avogadro < Cask
+  url 'http://sourceforge.net/projects/avogadro/files/latest/download'
+  homepage 'http://avogadro.openmolecules.net/'
+  version 'latest'
+  no_checksum
+  link 'Avogadro.app'
+end
