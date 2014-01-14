8e421c17efa081bf98daa4d7a4f1b56018706be5
Mirco Macrelli <mmacrelli@gmail.com>
2014-01-14 19:52:19 +0100

    Add a new cask for MP4Tools

---
+++ Casks/mp4tools.rb
@@ -0,0 +1,8 @@
+class Mp4tools < Cask
+  url 'http://www.emmgunn.com/downloads/mp4tools3.3.7.zip', 
+    :referer => 'http://www.emmgunn.com/mp4tools/mp4toolsdownload.html'
+  homepage 'http://www.emmgunn.com/mp4tools/mp4toolshome.html'
+  version '3.3.7'
+  sha1 '88583e29dcd22fb07a3a6993905d0dd83b31d516'
+  link 'mp4tools3.3.7/MP4Tools.app'
+end
