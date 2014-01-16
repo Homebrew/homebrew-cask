4d3025ac3c638f909c42a9609132b6e612795d18
Sherman Mui <sherman@meatchunks.com>
2014-01-16 10:04:22 -0800

    QLRest: fixed versioning

--- Casks/qlrest.rb
+++ Casks/qlrest.rb
@@ -1,7 +1,7 @@
 class Qlrest < Cask
   url 'https://github.com/downloads/cluther/qlrest/QLRest.qlgenerator.zip'
   homepage 'https://github.com/cluther/qlrest'
-  version '2.0'
-  sha256 '7800e17fae9afc640791cf94dcd50f50b1ff2d24ceaeb2f484d1e18b7bf10ea7'
+  version 'latest'
+  no_checksum
   qlplugin 'QLRest.qlgenerator'
 end
