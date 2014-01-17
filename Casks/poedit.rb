0bbdbe6dd7fff51704e7ac3ceef535c7c30b1702
Michael Ford <fanquake@users.noreply.github.com>
2014-01-17 21:05:25 +0800

    Poedit 1.6.3b

--- Casks/poedit.rb
+++ Casks/poedit.rb
@@ -1,7 +1,7 @@
 class Poedit < Cask
-  url 'http://www.poedit.net/dl/Poedit-1.6.3.zip'
+  url 'http://www.poedit.net/dl/Poedit-1.6.3b.zip'
   homepage 'http://www.poedit.net'
-  version '1.6.3'
-  sha1 '69896fc6d45af68f598635572c62116dd91e21ad'
+  version '1.6.3b'
+  sha1 '0000f4ad33bd23e0a16b41055ecb1d8ef27a401b'
   link 'Poedit.app'
 end
