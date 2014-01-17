9e9507b5b8e2acce0a5be32895c6a5f655310e65
Roland Walker <walker@pobox.com>
2014-01-17 12:01:08 -0500

    tikz-editor.rb Cask missing .app extension
    fails install without this change

--- Casks/tikz-editor.rb
+++ Casks/tikz-editor.rb
@@ -3,5 +3,5 @@ class TikzEditor < Cask
   homepage 'https://github.com/fredokun/TikZ-Editor'
   version '1.0'
   sha1 'e497afbb6d47f5ec978abcd4b2d0da3dc07e0d1e'
-  link 'TikZ Editor'
+  link 'TikZ Editor.app'
 end
