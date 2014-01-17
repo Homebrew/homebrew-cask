4125bb0ad3a2eafa41f6798793752eadd8188d1e
Josh Butts <josh@joshbutts.com>
2014-01-17 17:39:17 -0600

    Add perian

---
+++ Casks/perian.rb
@@ -0,0 +1,7 @@
+class Perian < Cask
+  url 'http://perian.cachefly.net/Perian_1.2.3.dmg'
+  homepage 'http://www.perian.org/'
+  version '1.2.3'
+  sha1 'aa0a6a3f7c65cb9f17dca138d3db0be059c8da0e'
+  prefpane 'Perian.prefPane'
+end
