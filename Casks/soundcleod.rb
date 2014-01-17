ff36e8b2aa5a2f53d461c3850cb441c81fa90058
Jonathan Trujillo <trujillo.jonathan@gmail.com>
2014-01-17 19:11:19 -0430

    Use latest soundcleod
    Use latest version of SoundCleod.app. #2448 #2452

--- Casks/soundcleod.rb
+++ Casks/soundcleod.rb
@@ -1,7 +1,7 @@
 class Soundcleod < Cask
-  url 'https://github.com/salomvary/soundcleod/raw/0.12/dist/SoundCleod.dmg'
+  url 'https://github.com/salomvary/soundcleod/raw/master/dist/SoundCleod.dmg'
   homepage 'http://salomvary.github.io/soundcleod/'
-  version '0.12'
-  sha1 '668bd5a69b746bfbd4077f668ea1e4923f4037ca'
+  version 'latest'
+  no_checksum
   link 'SoundCleod.app'
 end
