e67e469c14114734c1b7852b6ebc63fb1a0866e6
Michael Ford <fanquake@users.noreply.github.com>
2014-01-17 21:15:34 +0800

    Rest Client 3.2.2

--- Casks/rest-client.rb
+++ Casks/rest-client.rb
@@ -1,7 +1,7 @@
 class RestClient < Cask
-  url 'https://rest-client.googlecode.com/files/restclient-ui-3.2.1-app.zip'
+  url 'https://rest-client.googlecode.com/files/restclient-ui-3.2.2.dmg'
   homepage 'https://code.google.com/p/rest-client'
-  version '3.2.1'
-  sha1 'af3ba630c417524ae6a8a972680d26e6ad470dd1'
+  version '3.2.2'
+  sha1 '8000138f21058a5f65531408c023d24e1f13e403'
   link 'WizTools.org RESTClient.app'
 end
