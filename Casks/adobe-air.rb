09069b26be6a00cee176e900347bd7fdf1479eb3
Victor Robertson <victor.robertson.iv@gmail.com>
2014-01-17 20:35:30 -0600

    Update adobe-air to use silent install/uninstall

--- Casks/adobe-air.rb
+++ Casks/adobe-air.rb
@@ -3,12 +3,14 @@ class AdobeAir < Cask
   homepage 'https://get.adobe.com/air/'
   version '3.9'
   sha1 '69863ef58dca864c9b9016b512ea79e5d1429498'
-  link 'Adobe AIR Installer.app'
+  caskroom_only true
   
-  def caveats; <<-EOS.undent
-    You need to run #{destination_path/'AdobeAIRInstaller.app'} to actually install Adobe AIR
-    EOS
+  after_install do
+    system "sudo #{destination_path}/Adobe\\ AIR\\ Installer.app/Contents/MacOS/Adobe\\ AIR\\ Installer -silent"
   end
+
+  uninstall :script => {
+    :executable => 'Adobe AIR Installer.app/Contents/MacOS/Adobe AIR Installer',
+    :args => %w[-uninstall]
+  }
 end
-
-
