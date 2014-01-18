9bd870ed2e915e871688c7f68f1c89f151e27f26
guttertec <guttertec@Metaversum.local>
2014-01-18 21:00:14 +0100

    Add Cask for Sickbeard Anime version 13.05.25

---
+++ Casks/sickbeard-anime.rb
@@ -0,0 +1,7 @@
+class SickbeardAnime < Cask
+  url 'http://sickbeard.lad1337.de/download.php?f=SickBeard-OSX-anime-13.05.25.dmg'
+  homepage 'http://sickbeard.lad1337.de/'
+  version '13.05.25'
+  sha1 '93ce44188479b5c586303a3885f237cdca002df2'
+  link 'SickBeard.app'
+end
