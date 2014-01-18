95a20d5ef6889c9b3fba9cbc1b2cc4fd5679a736
Geoffrey Oxberry <goxberry@gmail.com>
2014-01-17 23:52:52 -0800

    Update: Paraview 4.1

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,7 +1,7 @@
 class Paraview < Cask
-  url 'http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.0&type=binary&os=osx&downloadFile=ParaView-4.0.1-Darwin-64bit.dmg'
+  url 'http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.1&type=binary&os=osx&downloadFile=ParaView-4.1.0-Darwin-64bit.dmg'
   homepage 'http://www.paraview.org/'
-  version '4.0.1'
-  sha1 'e4c96e49709d95ecfe691e5d575ad07f021485d6'
+  version '4.1.0'
+  sha1 '292333198dbb6ec09fca404dee5bf3fcce2525f9'
   link 'paraview.app'
 end
