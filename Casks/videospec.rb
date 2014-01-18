9f87e288776570e2c04b8ba78e7a605b8ecb74c5
Espen Hammer <espen@SPNAir.local>
2014-01-18 23:54:10 +0530

    VideoSpec clear
     0.9.8

---
+++ Casks/videospec.rb
@@ -0,0 +1,7 @@
+class Videospec < Cask
+  url 'http://videospec.free.fr/VideoSpec_0.9.8.dmg'
+  homepage 'http://videospec.free.fr/english/'
+  version '0.9.8'
+  sha1 'fb615c0c88f7c4d1fc2c53cc8b84b00eff328a3a'
+  link 'VideoSpec.app'
+end
