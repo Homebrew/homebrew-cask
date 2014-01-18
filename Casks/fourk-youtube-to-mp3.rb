07f9a3d3cca1f52fc8b98d71e06350fe0c52d395
Giovanni Polimeni <giofilo@gmail.com>
2014-01-18 21:39:01 +0100

    add ruby file for 4K YouTube to Mp3 (+1 squashed commit) Squashed commits: [2fdf25b] fix Classname
    add ruby file for 4K YouTube to Mp3

---
+++ Casks/fourk-youtube-to-mp3.rb
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+class FourkYoutubeToMp3 < Cask
+=======
+class FourKYoutubeToMp3 < Cask
+>>>>>>> 2e13f38... add ruby file for 4K YouTube to Mp3
+  url 'https://4kdownload.googlecode.com/files/4kyoutubetomp3_2.5.dmg'
+  homepage 'http://www.4kdownload.com/products/product-youtubetomp3'
+  version '3.2.5'
+  sha1 '41f2ad57b6c9416f62520e2ee9d847cb24edda15'
+  link '4K YouTube to MP3.app'
+end
