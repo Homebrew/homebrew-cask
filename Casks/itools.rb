6a603d0046c65decb1d03b9322738d013db9c22a
RainFlying <rainflying302@gmail.com>
2014-01-18 10:49:24 +0800

    Upgrade to version 2.3.1

--- Casks/itools.rb
+++ Casks/itools.rb
@@ -1,7 +1,7 @@
 class Itools < Cask
-  url 'http://dl2.itools.hk/dl/iTools_2.3.0.dmg'
+  url 'http://dl2.itools.hk/dl/iTools_2.3.1.dmg'
   homepage 'http://www.itools.cn/download.php?v=mac_en'
-  version '2.3.0'
-  sha1 '7edb17103ceb8fb481ddc96214d4664f47df581e'
+  version '2.3.1'
+  sha1 '2818d7b030738bce0e79eda8a9af4023b19eeb18'
   link 'iTools.app'
 end
