b8293212544c9b03c8b1182f9abf91f5e5037387
guttertec <guttertec@Metaversum.local>
2014-01-18 20:55:15 +0100

    Add Cask for Sickbeard version 13.05.25

---
+++ Casks/sickbeard.rb
@@ -0,0 +1,7 @@
+class Sickbeard < Cask
+  url 'http://sickbeard.lad1337.de/download.php?f=SickBeard-OSX-master-13.05.25.dmg'
+  homepage 'http://sickbeard.lad1337.de/'
+  version '13.05.25'
+  sha1 '830fafa783858cd6636ce0177d246804e0d1adf7'
+  link 'SickBeard.app'
+end
