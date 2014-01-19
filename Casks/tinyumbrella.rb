c5e86ca375aef5f261be809d55c249624f2ffd01
RainFlying <rainflying302@gmail.com>
2014-01-19 19:40:46 +0800

    Add cask for TinyUmbrella.

---
+++ Casks/tinyumbrella.rb
@@ -0,0 +1,7 @@
+class Tinyumbrella < Cask
+  url 'http://cache.firmwareumbrella.com/downloads/TinyUmbrella-7.04.00.app.zip'
+  homepage 'http://blog.firmwareumbrella.com/'
+  version '7.04'
+  sha1 'bdbb272d67baf909b2fcb852b0b8e3cd34a9e2aa'
+  link 'TinyUmbrella.app'
+end
