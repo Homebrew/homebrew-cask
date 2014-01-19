fb8e68fce73758ff8620251fc97ad726adc10c33
Tom Vincent <git@tlvince.com>
2014-01-19 16:16:48 +0000

    Add Color Picker Pro

---
+++ Casks/color-picker-pro.rb
@@ -0,0 +1,7 @@
+class ColorPickerPro < Cask
+  url 'http://fructivity.s3.amazonaws.com/ColorPickerPro/Color%20Picker%20Pro.zip'
+  homepage 'https://github.com/oscardelben/Color-Picker-Pro'
+  version 'latest'
+  no_checksum
+  link 'Color Picker Pro.app'
+end
