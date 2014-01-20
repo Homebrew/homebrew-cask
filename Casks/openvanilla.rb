2688e8dd3386458bc60b36098a061582f29fd5a2
Roland Walker <walker@pobox.com>
2014-01-20 15:07:02 -0500

    updt openvanilla for new caveats DSL

--- Casks/openvanilla.rb
+++ Casks/openvanilla.rb
@@ -4,9 +4,7 @@ class Openvanilla < Cask
   version '1.0.10'
   sha1 '53926d5b4348344de862dd2154e2b7cac9314916'
   input_method 'OpenVanillaInstaller.app/Contents/Resources/OpenVanilla.app'
-
-  def caveats; <<-EOS.undent
-    After installing OpenVanilla, please log out and then log in again.
-    EOS
+  caveats do
+    logout
   end
 end
