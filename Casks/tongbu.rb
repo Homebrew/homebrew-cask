c7ac6e474768a1b85d6e1e73535e83ebc8614c3a
Yang Bo <yangbodotnet@gmail.com>
2014-01-20 21:55:30 +0800

    update tongbu app's version to 1.0.7

--- Casks/tongbu.rb
+++ Casks/tongbu.rb
@@ -1,7 +1,7 @@
 class Tongbu < Cask
-  url 'http://qd.leaderhero.com/qd/zsmac/Tongbu_mac_v1.0.5.dmg'
+  url 'http://qd.leaderhero.com/qd/zsmac/Tongbu_mac_v1.0.7.dmg'
   homepage 'http://www.tongbu.com'
-  version '1.0.5'
-  sha1 '8546b515dd09d374979db0a59f10b9a088d128d1'
+  version '1.0.7'
+  sha1 '66f2307337ef3cb130c2c3f0702d575cb49a065e'
   link 'Tongbu.app'
 end
