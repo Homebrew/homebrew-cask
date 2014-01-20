b3d0834ce533309fdfd06497cc67c45a087f8d67
Vítor Galvão <info@vitorgalvao.com>
2014-01-20 23:24:48 +0000

    corrected picasa shasum

--- Casks/picasa.rb
+++ Casks/picasa.rb
@@ -2,6 +2,6 @@ class Picasa < Cask
   url 'http://dl.google.com/photos/picasamac39.dmg'
   homepage 'http://picasa.google.com/'
   version '3.9'
-  sha1 '981fc2126bee2163278340ed644b87cee003ae4b'
+  sha1 '16234b3ad9fa6614179a4d9ab25e22e1b4bccbb5'
   link 'Picasa.app'
 end
