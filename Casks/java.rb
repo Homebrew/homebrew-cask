25ca477227fa1dfd3ee3a37dd4dba386c776778a
Roland Walker <walker@pobox.com>
2014-01-20 15:13:10 -0500

    updt java.rb for new caveats DSL

--- Casks/java.rb
+++ Casks/java.rb
@@ -9,12 +9,10 @@ class Java < Cask
   sha1 'd53f71dfb7f24b6c0fc470a17fa0cac3ee304194'
   install 'JDK 7 Update 51.pkg'
   uninstall :pkgutil => 'com.oracle.jdk7u51'
-
-  def caveats; <<-EOS.undent
+  caveats <<-EOS.undent
     If your Java application still asks for JRE installation, you need to run:
     sudo sed -i \'\' 's/\\<string\\>CommandLine\\<\\/string\\>/\\<string\\>CommandLine\\<\\/string\\>\\<string\\>JNI\\<\\/string\\>\\<string\\>BundledApp\\<\\/string\\>\\<string\\>WebStart\\<\\/string\\>\\<string\\>Applets\\<\\/string\\>/' /Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Info.plist
     You might also need to reboot or Logout/Login
     Above is a workaround for JRE packaging bug discussed here: https://bugs.eclipse.org/bugs/show_bug.cgi?id=411361
     EOS
-  end
 end
