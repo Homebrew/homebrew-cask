7617a62fe8ecdbc5bced86f99cabeddd8ce3c4c9
C. Bess <cbess@quantumquinn.com>
2014-01-22 00:12:07 -0600

    Add iColors.app v3.0

---
+++ Casks/icolors.rb
@@ -0,0 +1,7 @@
+class Icolors < Cask
+  url 'http://www.fadingred.com/files/icolors/icolors_3.0.zip'
+  homepage 'http://www.fadingred.com/icolors/'
+  version '3.0'
+  sha1 'c2cd81a8473ae0b922a0354867841a7c1b7b8cc7'
+  link 'iColors.app'
+end
