ba218993a189e08736589b776cdfe472c2ad3e11
Roland Walker <walker@pobox.com>
2014-01-22 17:56:37 -0500

    Loud caveats against installing Dashboard Widgets
    And doc note, pending a fix of the underlying issue.

--- Casks/tv-show-tracker.rb
+++ Casks/tv-show-tracker.rb
@@ -4,4 +4,16 @@ class TvShowTracker < Cask
   version '1.3.3'
   sha1 '81e613d6525c49196625271dd76090866c1b2ddd'
   widget 'TV Show Tracker.wdgt'
+  caveats do
+    puts <<-EOS.undent
+    Currently, Dashboard Widgets such as '#{@cask}' do NOT work correctly
+    when installed via brew-cask.  The bug is being tracked here:
+
+        https://github.com/phinze/homebrew-cask/issues/2206
+
+    It is recommended that you do not install this Cask unless you are
+    a developer working on the problem.
+
+    EOS
+    end
 end
