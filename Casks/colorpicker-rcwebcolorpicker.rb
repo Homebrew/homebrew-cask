0f45d8b8143c3803c768d6feade0a9f151f0599c
Roland Walker <walker@pobox.com>
2014-01-22 13:52:16 -0500

    re-implement RCWebColorPicker as colorpicker
    This Cask dates from before the existence of the colorpicker artifact type.
    - chg artifcact type to colorpicker
    - remove caveats
    - rename to match other colorpickers

---
+++ Casks/colorpicker-rcwebcolorpicker.rb
@@ -0,0 +1,7 @@
+class ColorpickerRcwebcolorpicker < Cask
+  url 'http://www.rubicode.com/Downloads/RCWebColorPicker-1.2.X.dmg'
+  homepage 'http://www.rubicode.com/Software/RCWebColorPicker'
+  version '1.2.x'
+  sha1 'bebc90d7ddd40d6979714cdbbee01f3ef156fb55'
+  colorpicker 'RCWebColorPicker.colorPicker'
+end
