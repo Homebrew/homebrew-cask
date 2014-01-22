eeb657b6f2e635f80ed9c2a815f01c9905893635
Roland Walker <walker@pobox.com>
2014-01-22 14:12:45 -0500

    link AptanaStudio3.app instead of parent dir

--- Casks/aptana-studio.rb
+++ Casks/aptana-studio.rb
@@ -1,7 +1,7 @@
 class AptanaStudio < Cask
   url 'http://download.aptana.com/studio3/standalone/3.4.2/mac/Aptana_Studio_3_Setup_3.4.2.dmg'
   homepage 'http://www.aptana.com/'
-  link 'Aptana Studio 3'
+  link 'Aptana Studio 3/AptanaStudio3.app'
   version '3.4.2'
   sha1 '2c73735ada5a5cf81c149787ec1b3665b1813917'
 end
