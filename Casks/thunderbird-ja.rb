6ef07ffa34c848375dd4eb98db3fc7ca1d7276f6
yuokada <callistoiv+git@gmail.com>
2014-01-24 00:40:26 +0900

    Add thunderbird-ja.rb

---
+++ Casks/thunderbird-ja.rb
@@ -0,0 +1,7 @@
+class ThunderbirdJa < Cask
+  url 'http://download-installer.cdn.mozilla.net/pub/thunderbird/releases/24.2.0/mac/ja-JP-mac/Thunderbird%2024.2.0.dmg'
+  homepage 'http://www.mozilla.jp/thunderbird/'
+  version '24.2.0'
+  sha1 'aeb874de260df0cfb0cec06d24d9fc3baf343059'
+  link 'Thunderbird.app'
+end
