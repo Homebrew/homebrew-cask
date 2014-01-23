763c713cb94884cf752fe2b638282b91af7621f1
Victor Robertson <victor.robertson.iv@gmail.com>
2014-01-22 18:41:27 -0600

    New Cask: MAME OS X, v0.135
    MAME OS X is a native Mac OS X port of the popular MAME emulator. It
    is designed to take advantage of all the latest Mac OS X technologies,
    like Core Video and Core Image.

---
+++ Casks/mame-os-x.rb
@@ -0,0 +1,7 @@
+class MameOsX < Cask
+  url 'http://downloads.sourceforge.net/mameosx/MAMEOSX-0.135.dmg'
+  homepage 'http://mameosx.sourceforge.net/'
+  version '0.135'
+  sha1 '3da5ff069077a1eda66fa9dd02d198026e411198'
+  link 'MAME OS X.app'
+end
