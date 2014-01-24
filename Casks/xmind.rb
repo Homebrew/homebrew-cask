b18556a9a284249833fa0b5d5a285168bb427b03
Matt Behrens <askedrelic@gmail.com>
2014-01-24 14:12:58 -0800

    upgrade to latest xmind

--- Casks/xmind.rb
+++ Casks/xmind.rb
@@ -1,7 +1,7 @@
 class Xmind < Cask
-  url 'http://dl2.xmind.net/xmind-downloads/xmind-macosx-3.4.0.201311050558.dmg'
+  url 'http://dl2.xmind.net/xmind-downloads/xmind-macosx-3.4.1.201401221918.dmg'
   homepage 'http://www.xmind.net'
-  version '3.4.0'
-  sha1 '87bbea8de73f70ef308247baabae86c9cc61f3ef'
+  version '3.4.1'
+  sha1 '41d8a664ed6bdff83b46342765b31c91e99c3d5f'
   link 'XMind.app'
 end
