cdbaec6e9ad821fc4c28f04a54f0198d6289fd35
Mailo Svetel <development@rooland.cz>
2014-01-25 01:28:25 +0100

    Add Wedge.app for App.net

---
+++ Casks/app-dot-net-wedge.rb
@@ -0,0 +1,8 @@
+class AppDotNetWedge < Cask
+  url       'http://wedge.natestedman.com/release/Wedge.app.zip'
+  homepage  'http://wedge.natestedman.com'
+  version   'latest'
+  no_checksum
+  link      'Wedge.app'
+  caveats   'A Mac client for App.net. In beta.'
+end
