9ab92a7f7a41845dd23e5ddca26d8e154da17202
Will Styler <will@savethevowels.org>
2014-01-25 13:36:03 -0700

    Tweaked URL to meet standards.
    Oh Sourceforge, you so crazy.

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,5 +1,5 @@
 class Psychopy < Cask
-  url 'http://sourceforge.net/projects/psychpy/files/PsychoPy/StandalonePsychoPy-1.79.01-OSX.dmg'
+  url 'http://downloads.sourceforge.net/sourceforge/psychpy/StandalonePsychoPy-1.79.01-OSX.dmg'
   homepage 'http://www.psychopy.org/'
   version '1.79.00'
   sha1 'cbeaa6e747b3c3ccd6834829df36744ccf1ae382'
