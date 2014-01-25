f0718a2ef29b929f0414871b8008e47ca9251610
Douglas <doug@dougstewart.org>
2014-01-25 10:45:16 -0700

    Update processing.app
    Processing was updated to 2.1.1 on January 21st, 2014.

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -1,7 +1,7 @@
 class Processing < Cask
-  url 'http://processing.googlecode.com/files/processing-2.1-macosx.zip'
+  url 'http://processing.googlecode.com/files/processing-2.1.1-macosx.zip'
   homepage 'http://processing.org/'
-  version '2.1'
-  sha1 '4e51f26c8bd0d13a28cf70f367d4966c396d6079'
+  version '2.1.1'
+  sha1 'aa45c4c80675a03e925333d3a78c1613bdc2c642'
   link 'Processing.app'
 end
