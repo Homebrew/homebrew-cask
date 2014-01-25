858429b46ce6d0fa4231c9748b619da11c2a87f1
mAAdhaTTah <jamesorodig@gmail.com>
2014-01-25 14:27:58 -0500

    Update Coda to latest version

--- Casks/coda.rb
+++ Casks/coda.rb
@@ -1,7 +1,7 @@
 class Coda < Cask
-  url 'https://panic.com/coda/d/Coda%202.0.12.zip'
+  url 'https://panic.com/coda/d/Coda%202.0.13.zip'
   homepage 'https://panic.com/Coda/'
-  version '2.0.12'
-  sha1 '7157f0213edf39aca296c8bc4cb5a0e6f5ca6f75'
+  version '2.0.13'
+  sha1 'f6576e182159482279d38dc662a8347214e6201f'
   link 'Coda 2.app'
 end
