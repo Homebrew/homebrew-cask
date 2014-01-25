960681edabb52e63f426222e61a9497a19925081
Will Styler <will@savethevowels.org>
2014-01-25 09:25:36 -0700

    Added cask for xact 2.24
    Enjoy!

---
+++ Casks/xact.rb
@@ -0,0 +1,7 @@
+class Xact < Cask
+  url 'http://xact.scottcbrown.org/xACT2.24.zip'
+  homepage 'http://xact.scottcbrown.org'
+  version '2.24'
+  sha1 '241e9921b9b45d896baca65e7f1bf791fcd854bf'
+  link 'xACT 2.24/xACT.app'
+end
