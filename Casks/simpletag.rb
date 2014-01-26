1dba2397b0b869c5b20e19c140dcbf7d4aea6dfb
adyton <adyton@mmer.net>
2014-01-26 22:49:55 +0530

    SimpleTag for Homebrew/Cask

---
+++ Casks/simpletag.rb
@@ -0,0 +1,7 @@
+class Simpletag < Cask
+  url 'http://kaz.dl.sourceforge.net/project/simpletag/Mac/simpletag-gui-1.5.3-osx.zip'
+  homepage 'http://sourceforge.net/projects/simpletag/'
+  version '1.5.3'
+  no_checksum
+  link 'simpletag-gui-jface-1.5.3/SimpleTAG-GUI.app'
+end
