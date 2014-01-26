8464fdc4d426564a8544bdcb7111ce8aa44e9922
Chris Ng <chaoselixir@gmail.com>
2014-01-26 09:26:11 -0800

    Created xrpa application provides Window-Switch.app

---
+++ Casks/xpra.rb
@@ -0,0 +1,7 @@
+class Xpra < Cask
+  url 'http://xpra.org/dists/osx/x86/Window-Switch.dmg'
+  homepage 'http://xpra.org/'
+  version ''
+  sha1 'e4d3e763817ac4dc433ca0a60bcae2e978035a34'
+  link 'Window-Switch.app'
+end
