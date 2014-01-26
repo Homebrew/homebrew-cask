0fbbfa40b5eaace192cb7816e92b186380aa916b
decaylala <gen3137@gmail.com>
2014-01-27 00:51:21 +0800

    Add Macmoney.app v3.6.3
    a personal finance app

---
+++ Casks/macmoney.rb
@@ -0,0 +1,8 @@
+class Macmoney < Cask
+  url 'http://www.devon.riceball.net/downloads/macmoney36.zip'
+  homepage 'http://www.devon.riceball.net/display.php?file=m01'
+  version '3.6.3'
+  sha1 '86b4b963521b823b82a2d726ddfae137aa2d535e'
+  nested_container 'MacMoney_3.6.3.dmg'
+  link 'MacMoney.app'
+end
