ab679ad8853d62ca5e5f40826603e6c92a2e724d
adyton <adyton@mmer.net>
2014-01-26 18:36:15 +0530

    Jawbone Updater installer
    Signed-off-by: Fernando Paredes <nano@fdp.io>
    
    Added the sha1 for JawboneUpdater
    
    Closes #2369

---
+++ Casks/jawbone-updater.rb
@@ -0,0 +1,7 @@
+class JawboneUpdater < Cask
+  url 'http://content.jawbone.com/store/dashboard/Jawbone_Updater-2.2.3.pkg'
+  homepage 'http://jawbone.com/'
+  version '2.2.3'
+  sha1 'f729fc0d59ab84ae5bb8741c7033b8c9c12238bd'
+  install 'Jawbone_Updater-2.2.3.pkg'
+end
