07bfdc9a563ad82dcc16f431ab28831df527a885
Charles McLaughlin <mclaughlinct@gmail.com>
2014-01-26 21:45:38 -0800

    # This is a combination of 2 commits. # The first commit's message is:
    Adding a cask for Logitech Harmony
    
    # This is the 2nd commit message:
    
    Uninstall for logitech-harmony

---
+++ Casks/logitech-harmony.rb
@@ -0,0 +1,14 @@
+class LogitechHarmony < Cask
+  url 'http://logitech-sjca.navisite.net/web/ftp/pub/techsupport/harmony/LogitechHarmonyRemoteSoftware7.8.1-OSX.dmg'
+  homepage 'http://http://www.logitech.com/'
+  version '7.8.1'
+  sha1 '915c735154df58213f77a9196bfdeac6f95c76aa'
+  install 'LogitechRemoteSoftware.pkg'
+  uninstall :quit => 'com.logitech.harmony.cappuccino.client.logitech',
+            :kext => [
+                      'com.RemoteControl.USBLAN.usbpart',
+                      'com.Belcarra.iokit.USBLAN_netpart',
+                      'com.Belcarra.iokit.USBLAN_usbpart',
+                     ],
+            :pkgutil => 'com.logitech.harmony.logitechRemoteSoftware.*'
+end
