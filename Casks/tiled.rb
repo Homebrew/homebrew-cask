b45239d4cf9a7ffccc4362d661edeb318e52f883
Shogo Iwano <shiwano@gmail.com>
2014-01-28 01:36:53 +0900

    Add a cask for Tiled.app

---
+++ Casks/tiled.rb
@@ -0,0 +1,7 @@
+class Tiled < Cask
+  url 'http://downloads.sourceforge.net/project/tiled/tiled-qt/0.9.1/tiled-qt-0.9.1.dmg'
+  homepage 'http://www.mapeditor.org/'
+  version '0.9.1'
+  sha1 'f7f30ec8761ffd7f293242287ff4d462e7ee17fb'
+  link 'Tiled.app'
+end
