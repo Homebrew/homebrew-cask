dfce84c65767315237f445fa2281460133677480
Kevin Suttle <kevin@suttle.io>
2014-01-27 10:55:32 -0600

    Add Xampp cask

---
+++ Casks/xampp.rb
@@ -0,0 +1,7 @@
+class Xampp < Cask
+  url 'http://sourceforge.net/projects/xampp/files/latest/download'
+  homepage 'http://www.apachefriends.org/index.html'
+  version '1.8.3'
+  sha1 '7176f99223555e222873f5e148fe14a4118ee465'
+  link 'xampp-osx-1.8.3-2-installer.app'
+end
