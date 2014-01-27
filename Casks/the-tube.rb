e2b8710d592acab42fb2f4596ed0b8aa8c938ae5
Mailo Svetel <development@rooland.cz>
2014-01-27 22:48:03 +0100

    Add TheTube.app

---
+++ Casks/the-tube.rb
@@ -0,0 +1,7 @@
+class TheTube < Cask
+  url 'http://download2.equinux.com/files/other/TheTube_2.11.3_Web.zip'
+  homepage 'http://www.equinux.com/us/products/thetube/index.html'
+  version '2.11.3'
+  sha1 '7bfe78b5801cdf609ffaf99f8ee1c0caa6545400'
+  link 'TheTube.app'
+end
