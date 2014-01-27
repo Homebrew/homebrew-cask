72bf4ab9539eef5ceb49547106f85bce1d681678
Taran Hubbert <taran@twinbrother.com.au>
2014-01-27 16:29:32 +1100

    DeathToDSStore.app

---
+++ Casks/deathtodsstore.rb
@@ -0,0 +1,7 @@
+class Deathtodsstore < Cask
+  url 'http://www.aorensoftware.com/Downloads/Files/DeathToDSStore.zip'
+  homepage 'http://www.aorensoftware.com/blog/2011/12/24/death-to-ds_store/'
+  version '1.0.3'
+  sha1 '4f1667dce6a85dc85193b6e90c85845d43b69dde'
+  link 'DeathToDSStore.app'
+end
