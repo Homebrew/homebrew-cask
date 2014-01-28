986c00cfb39000aaaf5ea207f7698ba64ca50c25
Charles McLaughlin <mclaughlinct@gmail.com>
2014-01-27 20:08:01 -0800

    Uninstall for logitech-harmony

--- Casks/logitech-harmony.rb
+++ Casks/logitech-harmony.rb
@@ -4,5 +4,11 @@ class LogitechHarmony < Cask
   version '7.8.1'
   sha1 '915c735154df58213f77a9196bfdeac6f95c76aa'
   install 'LogitechRemoteSoftware.pkg'
-  uninstall 'Uninstall Logitech Harmony Remote Software.app'
+  uninstall :quit => 'com.logitech.harmony.cappuccino.client.logitech',
+            :kext => [
+                      'com.RemoteControl.USBLAN.usbpart',
+                      'com.Belcarra.iokit.USBLAN_netpart',
+                      'com.Belcarra.iokit.USBLAN_usbpart',
+                     ],
+            :pkgutil => 'com.logitech.harmony.logitechRemoteSoftware.*'
 end
