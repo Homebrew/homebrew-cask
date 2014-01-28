cf2ec1b9508d287950d74c294117bdf28ee7a225
muphli <hallo@muph.li>
2014-01-29 00:11:24 +0100

    Add checksum

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -2,6 +2,6 @@ class Texpad < Cask
   url 'http://cloud.texpadapp.com/bundles/Texpad_1_6_8.zip'
   homepage 'https://www.texpadapp.com/osx'
   version '1.6.8'
-  no_checksum
+  sha1 'e92eedbe828863884bf044a8f1f9c6ce6a834e7b'
   link 'Texpad.app'
 end
