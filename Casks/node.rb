8446963191776e56735ccdb21c62c5896979a975
Jamie Hoover ⚔ <uipoet@users.noreply.github.com>
2014-01-27 16:13:11 -0800

    Upgrade node to 0.10.25

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,8 +1,8 @@
 class Node < Cask
-  url 'http://nodejs.org/dist/v0.10.24/node-v0.10.24.pkg'
+  url 'http://nodejs.org/dist/v0.10.25/node-v0.10.25.pkg'
   homepage 'http://nodejs.org'
-  version '0.10.24'
-  sha1 '74aba302d8b34e1fc93b3e0babc3f5d9bd8c09f3'
-  install  'node-v0.10.24.pkg'
+  version '0.10.25'
+  sha1 'abab975e86250b51a7434d229d13b30acdf4e82e'
+  install  'node-v0.10.25.pkg'
   uninstall :pkgutil => 'org.nodejs'
 end
