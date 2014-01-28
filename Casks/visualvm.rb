178b7920eb43db01d02242568a576e46da543595
Michael Ford <fanquake@users.noreply.github.com>
2014-01-28 10:20:34 +0800

    Fix incorrect link update in visualvm cask

--- Casks/visualvm.rb
+++ Casks/visualvm.rb
@@ -1,7 +1,7 @@
 class Visualvm < Cask
-  url 'https://java.net/projects/visualvm/downloads/download/release137/visualvm_137.zip'
+  url 'https://java.net/projects/visualvm/downloads/download/release137/VisualVM_137.dmg'
   homepage 'http://visualvm.java.net'
   version '1.3.7'
-  sha1 '49e01d6c547c0bddcab38dbe46f688fd1c5f36f7'
+  sha1 'cd27c0d31ac17ddfd008a44c3da5b3099ad78a2d'
   link 'VisualVM.app'
 end
