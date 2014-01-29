4f08d69626482ed3e2c2d155f3d1fda235dd9691
Roland Walker <walker@pobox.com>
2014-01-29 09:42:24 -0500

    update ios-screensaver for artifact screen_saver

--- Casks/ios7-screensaver.rb
+++ Casks/ios7-screensaver.rb
@@ -3,12 +3,5 @@ class Ios7Screensaver < Cask
   homepage 'http://bodysoulspirit.weebly.com/ios-7-screensaver-for-mac-os-x-by-bodysoulspirit.html'
   version '2.8'
   sha1 'bcf3041dbc3f0a45699ba8bb43704fc90299ce0c'
-  caskroom_only true
-  caveats do
-    manual_installer('Install iOS 7 screensaver.app')
-    puts <<-EOS.undent
-      You also will have to go to "System Preferences" > "Desktop & Screen Saver" > "Screen Saver"
-      and choose "iOS7 lockscreensaver" to make it work.
-    EOS
-  end
+  screen_saver 'Install iOS 7 screensaver.app/Contents/Resources/iOS 7 lockscreen by bodysoulspirit.qtz'
 end
