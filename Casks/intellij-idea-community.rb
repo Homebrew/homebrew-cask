4996dae37fe4b40602a51172f21f164c935a4315
Pavel Sakun <pavel_sakun@epam.com>
2014-01-29 22:14:41 +0300

    Update Intellij IDEA to 13.0.2

--- Casks/intellij-idea-community.rb
+++ Casks/intellij-idea-community.rb
@@ -1,7 +1,7 @@
 class IntellijIdeaCommunity < Cask
-  url 'http://download-ln.jetbrains.com/idea/ideaIC-13.0.1.dmg'
+  url 'http://download-ln.jetbrains.com/idea/ideaIC-13.0.2.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '13.0.1'
-  sha1 '99d18d3fcd49e9b93759dec13a950caf01b8570e'
+  version '13.0.2'
+  sha1 'bdfb69ed9c34c2916ebdacda14b46c2b95989838'
   link 'IntelliJ IDEA 13 CE.app'
 end
--- Casks/intellij-idea-ultimate.rb
+++ Casks/intellij-idea-ultimate.rb
@@ -1,7 +1,7 @@
 class IntellijIdeaUltimate < Cask
-  url 'http://download-ln.jetbrains.com/idea/ideaIU-13.0.1.dmg'
+  url 'http://download-ln.jetbrains.com/idea/ideaIU-13.0.2.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '13.0.1'
-  sha1 '539b6b3e10e03ed9b54518fa3ccee75c9cefdcf2'
+  version '13.0.2'
+  sha1 '360d2b96afb3adb9b86e5de5674b1f62993b61e1'
   link 'IntelliJ IDEA 13.app'
 end
