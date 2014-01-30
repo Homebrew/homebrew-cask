c95a7f2534ff9d027a1fd429eb0815ab9ce501a0
Roland Walker <walker@pobox.com>
2014-01-29 21:17:46 -0500

    suggest replacements on "brew cask install" fail

--- lib/cask/cli/install.rb
+++ lib/cask/cli/install.rb
@@ -5,8 +5,19 @@ class Cask::CLI::Install
     force = args.include? '--force'
     cask_names.each do |cask_name|
       odebug "Installing Cask #{cask_name}"
-      cask = Cask.load(cask_name)
-      Cask::Installer.new(cask).install(force)
+      begin
+        cask = Cask.load(cask_name)
+        Cask::Installer.new(cask).install(force)
+      rescue CaskUnavailableError => e
+        exact_match, partial_matches, search_term = Cask::CLI::Search.search(cask_name)
+        errmsg = "#{cask_name}"
+        if exact_match
+          errmsg.concat(". Did you mean:\n#{exact_match}")
+        elsif !partial_matches.empty?
+          errmsg.concat(". Did you mean one of:\n#{stringify_columns(partial_matches.take(20))}\n")
+        end
+        raise CaskUnavailableError.new(errmsg)
+      end
     end
   end
 
