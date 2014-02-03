ff685cb31f0157b4e538610711aec2af4d4b9c2b
Roland Walker <walker@pobox.com>
2014-02-03 17:49:40 -0500

    suppress prompt to move Alfred to /Applications

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -5,4 +5,8 @@ class Alfred < Cask
   sha1 'db28d2a5c655a4611c780c3f46252530118ddd9d'
   link 'Alfred 2.app'
   link 'Alfred 2.app/Contents/Preferences/Alfred Preferences.app'
+  after_install do
+    # Don't ask to move the app bundle to /Applications
+    system 'defaults write com.runningwithcrayons.alfred-2 moveToApplicationsFolderAlertSuppress -bool true'
+  end
 end
