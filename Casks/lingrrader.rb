ae64c44fdf6f64303aeba8061480be957fd07c29
Yasuyuki Oka <yasuyk@gmail.com>
2014-02-05 20:16:51 +0900

    Add LingrRadar.app

---
+++ Casks/lingrrader.rb
@@ -0,0 +1,7 @@
+class Lingrrader < Cask
+  url 'http://radar.lingr.com/mac/LingrRadar_2.26.tbz'
+  homepage 'http://radar.lingr.com/'
+  version '2.26'
+  sha1 'f2b35c38ae09e98effaedded0ad075ead9e3c877'
+  link 'LingrRadar.app'
+end
