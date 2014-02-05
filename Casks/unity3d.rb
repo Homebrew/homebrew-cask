64a8506d437cd79a2c61afd8bbeabd0a3e74f98e
unalone <unalone@me.com>
2014-02-05 23:44:16 +0800

    Update Unity3d to 4.3.4f1.

--- Casks/unity3d.rb
+++ Casks/unity3d.rb
@@ -1,8 +1,8 @@
 class Unity3d < Cask
-  url 'http://netstorage.unity3d.com/unity/unity-4.3.0.dmg'
+  url 'http://netstorage.unity3d.com/unity/unity-4.3.4.dmg'
   homepage 'http://unity3d.com/unity/'
-  version '4.3.0'
-  sha1 '03a270fcefc605142fb53398b3fe8409d0b2837f'
+  version '4.3.4.1'
+  sha1 '4aaa29db229f59c9d2b549e09546cfee1c4a01cc'
   install 'Unity.pkg'
   uninstall(
     :pkgutil => 'com.unity3d.*'
