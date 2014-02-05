34ffda8864e5f5f03f1f8090d87838e7fd5e135a
Beng Hee Eu <beng@benghee.eu>
2014-02-05 08:44:52 +0800

    VLC 2.1.3

--- Casks/vlc.rb
+++ Casks/vlc.rb
@@ -1,7 +1,7 @@
 class Vlc < Cask
-  url 'http://get.videolan.org/vlc/2.1.2/macosx/vlc-2.1.2.dmg'
+  url 'http://get.videolan.org/vlc/2.1.3/macosx/vlc-2.1.3.dmg'
   homepage 'http://www.videolan.org/vlc/'
-  version '2.1.2'
-  sha1 '2a4dceba046682a0f56528045ed637aacae03a33'
+  version '2.1.3'
+  sha1 '805d84408b64e1715f331e5df66e5f141db7d0d2'
   link 'VLC.app'
 end
