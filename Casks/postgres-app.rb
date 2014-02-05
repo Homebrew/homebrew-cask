35ad6e1cee25e7dcd1e9a05e68449913db76bd1b
Alexey <anakros.symbian@gmail.com>
2014-02-05 14:04:02 +0700

    updated Postgres App to 9.3.2.0-RC2

--- Casks/postgres-app.rb
+++ Casks/postgres-app.rb
@@ -1,7 +1,7 @@
 class PostgresApp < Cask
-  url 'https://github.com/PostgresApp/PostgresApp/releases/download/9.3.1.0-alpha1/Postgres93.zip'
+  url 'https://github.com/PostgresApp/PostgresApp/releases/download/9.3.2.0-RC2/Postgres-9.3.2.0-RC2.zip'
   homepage 'http://postgresapp.com/'
-  version '9.3.1.0-alpha1'
-  sha1 'fa2c504fd791564484995226dd339b5b9eaa2519'
-  link 'Postgres93.app'
+  version '9.3.2.0-RC2'
+  sha1 'c0087f0fc86ffcf1b5cecc72f7c7486024fa2ab9'
+  link 'Postgres.app'
 end
