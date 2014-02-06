75985427986921629390a420de030423920c6f14
Alberto A. Colon Viera (Albertico) <aacv@alberti.co>
2014-02-05 20:14:37 -0400

    Add invisibliX.app v3.2

---
+++ Casks/invisiblix.rb
@@ -0,0 +1,7 @@
+class Invisiblix < Cask
+  url 'http://downloads.sourceforge.net/project/invisiblix/invisibliX-3.2.zip'
+  homepage 'http://www.read-write.fr/invisiblix/'
+  version '3.2'
+  sha1 '3691e5fbae93ca7abfd1177815a2bd2f1c9c51db'
+  link 'invisibliX.app'
+end
