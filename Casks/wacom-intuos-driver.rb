034ca23a52f1fd8c90beb9fded21fbf037c006a2
Newton <iam@chrisopedia.com>
2014-02-05 22:19:38 -0800

    Create Wacom Intuos Driver v6.3.7-3 cask
    A hardware driver to interact with Wacom products

---
+++ Casks/wacom-intuos-driver.rb
@@ -0,0 +1,7 @@
+class WacomIntuosDriver < Cask
+  url 'http://cdn.wacom.com/u/drivers/mac/pro/WacomTablet_6.3.7-3.dmg'
+  homepage 'http://www.wacom.com/'
+  version '6.3.7-3'
+  sha1 '09f6232ad200de159f3bf204a18f3d978cb9e9ea'
+  install 'Install Wacom Tablet.pkg'
+end
