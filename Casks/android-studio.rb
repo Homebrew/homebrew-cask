5fe3028d3db3fa8bb87ab04fc35dadb4a5133a66
Alex Selesse <alex@selesse.com>
2014-02-05 22:13:10 -0500

    Android Studio 0.4.4 build 133.1005886

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,7 +1,7 @@
 class AndroidStudio < Cask
-  url 'http://dl.google.com/dl/android/studio/ide-zips/0.4.3/android-studio-ide-133.988757-mac.zip'
+  url 'http://dl.google.com/dl/android/studio/ide-zips/0.4.4/android-studio-ide-133.1005886-mac.zip'
   homepage 'https://developer.android.com/sdk/installing/studio.html'
-  version '0.4.3 build-133.988757'
-  sha1 'caa13abee862578682743111446ffb82f4d03165'
+  version '0.4.4 build-133.1005886'
+  sha1 '3b3b44c0d703bf094fe972c75d02c18f9d549d39'
   link 'Android Studio.app'
 end
