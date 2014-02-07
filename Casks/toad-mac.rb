0929122a5e50f258371895b357150341d37f80e6
Simon Schlachter <github@survive.ch>
2014-02-07 08:06:53 +0100

    upgrade toad-mac to latest version

--- Casks/toad-mac.rb
+++ Casks/toad-mac.rb
@@ -1,8 +1,8 @@
 class ToadMac < Cask
-  url 'http://community-downloads.quest.com/toadsoft/toadmacedition/ToadMacEdition_106.pkg'
+  url 'http://community-downloads.quest.com/toadsoft/toadmacedition/ToadMacEdition_108.pkg'
   homepage 'http://www.toadworld.com/products/toad-mac-edition/default.aspx'
-  version '1.0.6'
-  sha1 '9538b44efdc15d2bfc9355b06449ecaa8a48945d'
-  install 'ToadMacEdition_106.pkg'
+  version '1.0.8'
+  sha1 '2f95668c4a1f02ea8ba6edd4e09586590d0abefd'
+  install 'ToadMacEdition_108.pkg'
   uninstall :pkgutil => 'com.dell.Toad', :files => '/Applications/Toad.app'
 end
