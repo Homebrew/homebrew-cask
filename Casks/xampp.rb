97d48cc5d4167180ec766d0f2b8e56df7f29239a
Kevin Suttle <kevin@suttle.io>
2014-02-06 22:53:09 -0600

    Add caveats block for xampp, update SourceForge URL

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -1,7 +1,9 @@
 class Xampp < Cask
-  url 'http://sourceforge.net/projects/xampp/files/latest/download'
-  homepage 'http://www.apachefriends.org/index.html'
-  version '1.8.3'
-  sha1 '7176f99223555e222873f5e148fe14a4118ee465'
-  link 'xampp-osx-1.8.3-2-installer.app'
+	url 'http://softlayer-dal.dl.sourceforge.net/project/xampp/XAMPP%20Mac%20OS%20X/1.8.3/xampp-osx-1.8.3-2-installer.dmg'
+	homepage 'http://www.apachefriends.org/index.html'
+	version '1.8.3.2'
+	sha1 '541c6de82ebd508c1a45175dc7f0ad1952647d11'
+	caveats do
+		manual_installer 'xampp-osx-1.8.3-2-installer.app'
+	end
 end
