86485c6bc0f63f18466657acf316caaae13f1eaa
LN <ln@mbp.local>
2014-02-07 23:49:45 +0100

    Add Cask for Sbooth's Tag

---
+++ Casks/tag.rb
@@ -0,0 +1,7 @@
+class Tag < Cask
+  url 'http://prdownloads.sourceforge.net/tagosx/Tag-0.4.1.zip'
+  homepage 'http://sbooth.org/Tag/'
+  version '0.4.1'
+  sha1 'b3956508e253d1a64f1346832b302b90ec1c84bc'
+  link 'Tag-0.4.1/Tag.app'
+end
