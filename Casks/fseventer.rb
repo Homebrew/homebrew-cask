0a3f4eddbb5d75459fdcace8bc24444bda547b2f
Fernando Paredes <nano@fdp.io>
2014-02-06 18:57:11 -0800

    Fseventer latest

--- Casks/fseventer.rb
+++ Casks/fseventer.rb
@@ -1,8 +1,8 @@
 class Fseventer < Cask
   url 'http://www.fernlightning.com/lib/exe/fetch.php?id=software%3Afseventer%3Astart&cache=cache&media=software:fseventer:fseventer.zip'
   homepage 'http://www.fernlightning.com/doku.php?id=software:fseventer:start'
-  version '2.7.6'
-  sha1 'ee3604ee7286d78bd90bd278f133de71fb1d3647'
+  version 'latest'
+  no_checksum
   link 'fseventer/fseventer.app'
   uninstall :script => 'fseventer/HelperToolUninstall.sh'
 end
