c8cea4adf07800acca9b249f85029daf3c9ebce8
Jonathan Dahan <jonathan@jedahan.com>
2014-02-08 10:07:52 -0800

    update dogecoin to 1.5.1

--- Casks/dogecoin.rb
+++ Casks/dogecoin.rb
@@ -1,7 +1,7 @@
 class Dogecoin < Cask
-  url 'https://github.com/dogecoin/dogecoin/releases/download/1.4/dogecoin-qt-v14-Mac.zip'
+  url 'https://github.com/dogecoin/dogecoin/releases/download/1.5.1/dogecoin-qt-1_5_1-mac.zip'
   homepage 'http://dogecoin.com/'
-  version '1.4'
-  sha1 '3df9378fb7de417a72042308be813cf03068ce68'
-  link 'Dogecoin.app'
+  version '1.5.1'
+  sha1 '3e55306bc87d992f20317d6f7a2e6b284e057574'
+  link 'Dogecoin-Qt.app'
 end
