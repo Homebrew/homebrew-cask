e485a08530373fca7a7b7079cfe62ceeb574c81a
jk <jens.kohl@gmail.com>
2014-02-10 11:47:11 +0100

    Add cert-quicklook v3

---
+++ Casks/cert-quicklook.rb
@@ -0,0 +1,7 @@
+class CertQuicklook < Cask
+  url 'https://cert-quicklook.googlecode.com/files/CertQuickLook-v3.qlgenerator.zip'
+  homepage 'https://code.google.com/p/cert-quicklook/'
+  version '3'
+  sha1 'e21cca7216f029609941e5f763e16dd8a41c12c8'
+  qlplugin 'CertQuickLook.qlgenerator'
+end
