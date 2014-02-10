2763a8d052a36d9ae1ae231b2c1e40d9270ad025
Tomohiro Ikebe <ikebe@linecorp.com>
2014-02-10 14:20:21 +0900

    add DragonDisk.app v1.05

---
+++ Casks/dragondisk.rb
@@ -0,0 +1,7 @@
+class Dragondisk < Cask
+  url 'http://download.dragondisk.com/DragonDisk-1.05.dmg'
+  homepage 'http://www.dragondisk.com/'
+  version '1.05'
+  sha1 'a1df52f1c0a0f2d5b1d589ef4e1a06a625dba497'
+  link 'dragondisk.app'
+end
