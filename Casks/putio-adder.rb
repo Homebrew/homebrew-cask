5107ed34eb9a7fc4d82ad12b5485c1da1d8f93f0
Michael Ford <fanquake@users.noreply.github.com>
2014-02-10 11:09:24 +0800

    put.io-adder 2.6.1

--- Casks/putio-adder.rb
+++ Casks/putio-adder.rb
@@ -1,7 +1,7 @@
 class PutioAdder < Cask
-  url 'https://github.com/nicoSWD/put.io-adder/releases/download/v2.5/put.io-adder-v2.5.zip'
+  url 'https://github.com/nicoSWD/put.io-adder/releases/download/v2.6.1/put.io.adder.zip'
   homepage 'https://github.com/nicoSWD/put.io-adder'
-  version '2.5'
-  sha1 '10aea92409c04d77d4874f13f01c800c7913dff4'
+  version '2.6.1'
+  sha256 'd89aed7a40de99ad3e75ca05995c088c5147282cb387beaaa5c496c5fc9b2307'
   link 'put.io adder.app'
 end
