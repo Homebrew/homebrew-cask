0e4040817b1d15d8bceaca704299bc5aa2a123ee
Michael Ford <fanquake@users.noreply.github.com>
2014-02-10 11:35:07 +0800

    Node-webkit 0.9.0

--- Casks/node-webkit.rb
+++ Casks/node-webkit.rb
@@ -1,7 +1,7 @@
 class NodeWebkit < Cask
-  url 'https://s3.amazonaws.com/node-webkit/v0.8.4/node-webkit-v0.8.4-osx-ia32.zip'
+  url 'https://s3.amazonaws.com/node-webkit/v0.9.0/node-webkit-v0.9.0-osx-ia32.zip'
   homepage 'https://github.com/rogerwang/node-webkit'
-  version '0.8.4'
-  sha1 'd9ed2379077c3d322c4759aa1337393196697acc'
+  version '0.9.0'
+  sha256 'e38633273071a08c58878a8e8ea6d62dd5d51f235fe29c2684f39446af758b07'
   link 'node-webkit.app'
 end
