b5fe56349e50a43cb68b0446edbb98e95628c2f1
Michael Ford <fanquake@users.noreply.github.com>
2014-02-10 11:14:58 +0800

    Update Portmap checksum to SHA256

--- Casks/port-map.rb
+++ Casks/port-map.rb
@@ -2,6 +2,6 @@ class PortMap < Cask
   url 'http://tcmportmapper.googlecode.com/files/PortMap-1.3.1-r46.zip'
   homepage 'http://www.codingmonkeys.de/portmap'
   version '1.3.1-r46'
-  sha1 'bc294028f665a2e42cc32a6dc69493fd34542d8c'
+  sha256 '50e29f121139ee2f68ad978cb3921ef52f2dbfb5a0131c417516e4e5f1fa64af'
   link 'Port Map.app'
 end
