bd97aeee9a30dd2f522e626d586e36ef4087c884
Lucas Garron <lucasg@gmx.de>
2014-02-10 05:44:51 -0800

    Added TreeMaker 5.

---
+++ Casks/treemaker.rb
@@ -0,0 +1,7 @@
+class Treemaker < Cask
+  url 'http://www.langorigami.com/science/computational/treemaker/TreeMaker.dmg'
+  homepage 'http://www.langorigami.com/science/computational/treemaker/treemaker.php'
+  version '5.0.1'
+  sha256 '2377c8c4277890de9f746eaa9d021f89fdd52cabfe9ab82fdfa741a47ed1f554'
+  link 'TreeMaker 5.app'
+end
