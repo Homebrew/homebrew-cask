1618489966f0228b7f2f54a619f3dfbf0dbc0421
Tsukasa OMOTO <henry0312@gmail.com>
2014-02-10 13:55:11 +0900

    mpv 0.3.4

--- Casks/mpv.rb
+++ Casks/mpv.rb
@@ -1,8 +1,8 @@
 class Mpv < Cask
-  url 'https://github.com/mpv-player/mpv/releases/download/v0.3.3/mpv_v0.3.3_osx.zip'
+  url 'https://github.com/mpv-player/mpv/releases/download/v0.3.4/mpv_v0.3.4_osx.zip'
   homepage 'http://mpv.io/'
-  version '0.3.3'
-  sha1 '6bfe4a8f4d191a1642cedb09ae36437d1709141f'
+  version '0.3.4'
+  sha1 'a22e671ed7cf0ff756c283ab3dbbe9c364212597'
   link 'mpv.app'
   binary 'mpv.app/Contents/MacOS/mpv'
 end
