a9b31458e990168edb787d5c4dc0a18076efe5ef
Michael Ford <fanquake@users.noreply.github.com>
2014-02-10 11:19:59 +0800

    Update Tor Browser checksum to SHA256

--- Casks/tor-browser.rb
+++ Casks/tor-browser.rb
@@ -2,6 +2,6 @@ class TorBrowser < Cask
   url 'https://www.torproject.org/dist/torbrowser/3.5.1/TorBrowserBundle-3.5.1-osx32_en-US.zip'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   version '3.5.1'
-  sha1 'd155070b90d9244d52663b7fcf4127c8b24f5be5'
+  sha256 '81305a009a1d939621ac1e651d7a1bf6569ff7b714bf3e1cf0b7cd4890f80e3d'
   link 'TorBrowserBundle_en-US.app'
 end
