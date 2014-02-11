ffd006dcfd96e9d434588a401271f9c0a73326e2
Bobby DeSimone <desimone@users.noreply.github.com>
2014-02-11 18:55:05 -0500

    Update SHA1 value for p4merge.
    SHA1 Value changed.

--- Casks/p4merge.rb
+++ Casks/p4merge.rb
@@ -2,7 +2,7 @@ class P4merge < Cask
   url 'http://filehost.perforce.com/perforce/r13.4/bin.macosx106x86_64/P4V.dmg'
   homepage 'http://www.perforce.com/product/components/perforce-visual-merge-and-diff-tools'
   version '2013.4'
-  sha1 '5b22abe5a405e9b48f4c76ed31766b933bb20c32'
+  sha1 '927dbbc79cd27be47a08f38a57dcff7b14cb4ac8'
   link 'p4merge.app'
   caveats <<-EOS.undent
     You can set up git to use p4merge as a merge tool by following the instructions available here:
