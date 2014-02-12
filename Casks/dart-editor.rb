38d1238b7c6fa5c8ad411e7ed5adf4dfdba5b9de
Chris Oei <code@oei.io>
2014-02-12 15:27:05 -0800

    Use latest for dart editor

--- Casks/dart-editor.rb
+++ Casks/dart-editor.rb
@@ -1,7 +1,7 @@
 class DartEditor < Cask
   url 'http://storage.googleapis.com/dart-archive/channels/stable/release/latest/editor/darteditor-macos-x64.zip'
   homepage 'https://www.dartlang.org/tools/editor/'
-  version '1.1.3'
-  sha256 '48cced0709aa9bddfa3d03365b0fc4b3b9b157799f244746dc127ea95052825e'
+  version 'latest'
+  no_checksum
   link 'dart/DartEditor.app'
 end
