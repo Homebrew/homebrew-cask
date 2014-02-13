8f10c109724590a91b6217eaf268001d34cee223
Glenn / devalias <glenn@ensquared.net>
2014-02-13 11:16:18 +1100

    Change to latest version
    Use latest version

--- Casks/majic.rb
+++ Casks/majic.rb
@@ -1,7 +1,7 @@
 class Majic < Cask
-  url 'http://erikhinterbichler.com/blog/wp-content/uploads/software/Majic.zip?v=1.0.4'
+  url 'http://erikhinterbichler.com/blog/wp-content/uploads/software/Majic.zip'
   homepage 'http://erikhinterbichler.com/apps/majic/'
-  version '1.0.4'
-  sha1 '5507652120209e612f74ae3d7e7038c5bea0e0fd'
+  version 'latest'
+  no_checksum
   link 'Majic.app'
 end
