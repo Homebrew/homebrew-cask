b1f14fd4be0f4b5d7874bc0bdf3725bb937e93a3
deepj <deepjungle.maca@gmail.com>
2014-02-13 18:08:54 +0100

    Add Widelands Build 17

---
+++ Casks/widelands.rb
@@ -0,0 +1,7 @@
+class Widelands < Cask
+  url 'https://launchpad.net/widelands/build17/build-17/+download/widelands-build17-mac.dmg'
+  homepage 'https://wl.widelands.org/'
+  version 'Build 17'
+  sha256 '3812ae9f95697269a94970fc83c2c16ab962f450b5a114057046fde3bcfc5a2c'
+  link 'Widelands.app'
+end
