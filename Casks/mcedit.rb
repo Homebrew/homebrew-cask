364a24d57598f986b273b1b7ced43a66807617e2
Caleb Xu <calebcenter@live.com>
2014-02-13 18:15:08 -0500

    Added MCEdit
    MCEdit is a Java-based tool for editor world maps used in the popular,
    Java-based indie game Minecraft.

---
+++ Casks/mcedit.rb
@@ -0,0 +1,7 @@
+class Mcedit < Cask
+  url 'https://bitbucket.org/codewarrior0/mcedit/downloads/MCEdit-0.1.7.1.macosx-10_6-x86_64.zip'
+  homepage 'http://www.mcedit.net'
+  version '0.1.7.1'
+  sha1 '39e0203c78f9d2b85ab3d6035a140bf2c988bf6b'
+  link 'MCEdit.app'
+end
