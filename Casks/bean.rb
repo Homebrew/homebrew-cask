09241d80501475dc5257035db65f204ff72d9fb4
Caleb Xu <calebcenter@live.com>
2014-02-13 17:40:46 -0500

    Added bean
    Bean is a lightweight rich text editor for the Mac OS X platform.

---
+++ Casks/bean.rb
@@ -0,0 +1,7 @@
+class Bean < Cask
+  url 'http://www.bean-osx.com/releases/Bean-Install.zip'
+  homepage 'http://www.bean-osx.com'
+  version 'latest'
+  no_checksum
+  link 'Bean-Install/Bean.app'
+end
