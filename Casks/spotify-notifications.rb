197166b4e6bdf37afee3d3b2dee11f3d2ec55254
Anuj Das <adas@lookout.com>
2014-02-14 00:12:37 -0800

    Create Spotify Notifications v0.4.8 cask

---
+++ Casks/spotify-notifications.rb
@@ -0,0 +1,7 @@
+class SpotifyNotifications < Cask
+  url 'https://github.com/citruspi/Spotify-Notifications/releases/download/0.4.8/Spotify.Notifications.-.0.4.8.zip'
+  homepage 'http://spotify-notifications.citruspi.io/'
+  version '0.4.8'
+  sha256 '953028e9a1aad445005869598050cb8612980821a796563936f557e03b319f50'
+  link 'Spotify Notifications.app'
+end
