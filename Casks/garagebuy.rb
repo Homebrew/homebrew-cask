4cb65bca9e7be320087f689eb93f79f1dfd8b329
Caleb Xu <calebcenter@live.com>
2014-02-14 11:50:26 -0500

    Added GarageBuy
    GarageBuy is a Mac client for using the e-commerce platform eBay.

---
+++ Casks/garagebuy.rb
@@ -0,0 +1,7 @@
+class Garagebuy < Cask
+  url 'http://www.iwascoding.de/downloads/GarageBuy_3.1.1.dmg'
+  homepage 'http://www.iwascoding.com/GarageBuy'
+  version '3.1.1'
+  sha1 'c33c76b165c9734dc00e0744488c6ec6d6e6f912'
+  link 'GarageBuy.app'
+end
