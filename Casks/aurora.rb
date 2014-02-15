19af63ea8a96f55866c99b25643e12d7b85f904e
moqada <moqada@gmail.com>
2014-02-15 18:34:40 +0900

    Add Aurora.app v5.0.3

---
+++ Casks/aurora.rb
@@ -0,0 +1,7 @@
+class Aurora < Cask
+  url 'https://www.oneperiodic.com/files/Aurora%20v5.0.3.zip'
+  homepage 'http://www.oneperiodic.com/products/aurora/'
+  version '5.0.3'
+  sha1 '4fec068f6b0e48849915a46447541b6b33b98a4b'
+  link 'Aurora.app'
+end
