80091a17803ba6a6821158a6efa3b8100c7ae0af
Michael Ford <fanquake@users.noreply.github.com>
2014-02-15 12:34:51 +0800

    Pester 1.1b18

--- Casks/pester.rb
+++ Casks/pester.rb
@@ -1,7 +1,7 @@
 class Pester < Cask
-  url 'http://sabi.net/nriley/software/Pester-1.1b17.dmg'
+  url 'http://sabi.net/nriley/software/Pester-1.1b18.dmg'
   homepage 'http://sabi.net/nriley/software/index.html#pester'
-  version '1.1b17'
-  sha1 '0cb20fb60d95e14739bcbe2d2239308e18129b09'
+  version '1.1b18'
+  sha256 '50cddd6fa813e5e2935e0e66ea13ee0605ada711c0141498a4d19c93eb554c86'
   link 'Pester.app'
 end
