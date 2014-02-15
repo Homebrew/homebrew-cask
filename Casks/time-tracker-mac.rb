020d11315c05c4ef0b036a579e1d609a874a94c8
Michael Ford <fanquake@users.noreply.github.com>
2014-02-15 15:13:54 +0800

    Update Time Tracker Cask to use SHA256

--- Casks/time-tracker-mac.rb
+++ Casks/time-tracker-mac.rb
@@ -2,6 +2,6 @@ class TimeTrackerMac < Cask
   url 'https://time-tracker-mac.googlecode.com/files/Time%20Tracker-1.3.13.zip'
   homepage 'https://code.google.com/p/time-tracker-mac'
   version '1.3.13'
-  sha1 '1d3fb3f824658856e18eb285d0ed92d81a1e9784'
+  sha256 'a6c982ee82f8babcf0f42d49950e5b3a1d613946736a7ee4a272b05916271aeb'
   link 'Time Tracker.app'
 end
