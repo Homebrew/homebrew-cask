ec7cc70bb90bc7344566ec6a2b93401246e05261
moqada <moqada@gmail.com>
2014-02-15 18:38:49 +0900

    Add iVolume.app latest

---
+++ Casks/ivolume.rb
@@ -0,0 +1,7 @@
+class Ivolume < Cask
+  url 'http://www.mani.de/download/ivolume/iVolume3Mac.dmg'
+  homepage 'http://www.mani.de/en/ivolume/'
+  version 'latest'
+  no_checksum
+  link 'iVolume.app'
+end
