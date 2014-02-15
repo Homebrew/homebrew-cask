4cea70cb18b1e4a4ec92e7c1ab913e5ff5d3d7e5
Michael Ford <fanquake@users.noreply.github.com>
2014-02-15 15:09:25 +0800

    TextWrangler 4.5.6

--- Casks/textwrangler.rb
+++ Casks/textwrangler.rb
@@ -1,7 +1,7 @@
 class Textwrangler < Cask
-  url 'http://ash.barebones.com/TextWrangler_4.5.5.dmg'
+  url 'http://ash.barebones.com/TextWrangler_4.5.6.dmg'
   homepage 'http://www.barebones.com/products/textwrangler'
-  version '4.5.5'
-  sha1 '3901ef49e82efbbdb7c639e25a2202c02c7e0956'
+  version '4.5.6'
+  sha256 'e4896aae3e51788677fbb5e9190295c5b1406823793288e83a5e46208e4f0e57'
   link 'TextWrangler.app'
 end
