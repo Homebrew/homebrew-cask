9b58ecb3674e2ece54be2f4e2c1a30425c1c7344
Caleb Xu <calebcenter@live.com>
2014-02-15 10:55:44 -0500

    Added Bino
    Bino is a media player that supports 3D stereoscopic viewing, among
    other 3D features.

---
+++ Casks/bino.rb
@@ -0,0 +1,7 @@
+class Bino < Cask
+  url 'http://devernay.free.fr/hacks/bino/Bino-1.4.4-OSX-SnowLeopard-GPL.zip'
+  homepage 'http://bino3d.org'
+  version '1.4.4'
+  sha256 '924aaef6e95d969bded524647b7a6a35e1c9a8478a7bb855972cfefe25165743'
+  link 'Bino.app'
+end
