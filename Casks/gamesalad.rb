493fc4e05f9cadad51123d1ab5c67e69ca3ee3d3
Caleb Xu <calebcenter@live.com>
2014-02-15 12:19:06 -0500

    Added GameSalad
    GameSalad is a tool for easily building games that can be quickly
    deployed to Windows, OS X, Android, and iOS devices.

---
+++ Casks/gamesalad.rb
@@ -0,0 +1,7 @@
+class Gamesalad < Cask
+  url 'http://gs.releases.s3.amazonaws.com/creator/0.10.4.1/GameSalad-Creator-0.10.4.1-beta.dmg'
+  homepage 'http://gamesalad.com'
+  version '0.14.4.1-beta'
+  sha256 '05f68db92cee37fedffe23a2950fa55c375fd53acd7d19ae8fd42884345814e7'
+  link 'GameSalad.app'
+end
