5529e842bbbfcd613fdb942f7ec93208e927d79e
Michael Ford <fanquake@users.noreply.github.com>
2014-02-15 12:37:25 +0800

    Update Perian Cask to use SHA256

--- Casks/perian.rb
+++ Casks/perian.rb
@@ -2,7 +2,7 @@ class Perian < Cask
   url 'http://perian.cachefly.net/Perian_1.2.3.dmg'
   homepage 'http://www.perian.org/'
   version '1.2.3'
-  sha1 'aa0a6a3f7c65cb9f17dca138d3db0be059c8da0e'
+  sha256 '4d1738104613ab4a7322637584ce7b851e4ef85888895360ad827a5f27c62e08'
   prefpane 'Perian.prefPane'
   caveats <<-EOS.undent
     Perian development officially stopped as of 2012, and 1.2.3 was the final released version.
