3188390081a29716f9b5646b9b03c1a8711dc2b0
Caleb Xu <calebcenter@live.com>
2014-02-15 10:28:17 -0500

    Added Wyzo
    Wyzo is an awesome new browser that optimizes your online media
    experience. It will accelerate your web downloads, let you download
    torrents with a single click, discover media in your browser and much
    more.

---
+++ Casks/wyzo.rb
@@ -0,0 +1,7 @@
+class Wyzo < Cask
+  url 'http://download.wyzo.com/files/mac/WyzoSetup-3.5.6.dmg'
+  homepage 'http://wyzo.com'
+  version '3.5.6'
+  sha256 '18cda3c323544c729d7e532151a32c70d8427dae2762695229d102762b313026'
+  link 'Wyzo.app'
+end
