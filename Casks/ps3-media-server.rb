08a3b638ac09d7dbee4a1048be80bb9c3eae259d
Michael Ford <fanquake@users.noreply.github.com>
2014-02-15 12:43:24 +0800

    Update PS3 Media Server Cask to use SHA256

--- Casks/ps3-media-server.rb
+++ Casks/ps3-media-server.rb
@@ -2,6 +2,6 @@ class Ps3MediaServer < Cask
   url 'http://downloads.sourceforge.net/project/ps3mediaserver/pms-1.90.1-setup-macosx.tar.gz'
   homepage 'http://www.ps3mediaserver.org/'
   version '1.90.1'
-  sha1 '2cba80b54b8c3bcd48676d6718c7eed5f6300a24'
+  sha256 '3ebe75ce0dbdc1313c10fb901f845564cc343dc3b7487f07e15db9d757850df5'
   link 'PS3 Media Server Setup.app'
 end
