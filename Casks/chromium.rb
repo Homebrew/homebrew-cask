e3b41dc24dd4a706899353de18decfe5bd4c2a98
Lucas Mendelowski <lucas@mendelowski.com>
2014-02-16 20:58:14 +0800

    Reverted Chromium to the latest stable version

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,7 +1,7 @@
 class Chromium < Cask
-  url 'https://download-chromium.appspot.com/dl/Mac'
-  homepage 'https://download-chromium.appspot.com/'
-  version 'latest'
-  no_checksum
-  link 'chrome-mac/Chromium.app'
+  url 'http://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_32.0.1700.107.dmg'
+  homepage 'http://www.freesmug.org/chromium'
+  version '32.0.1700.107'
+  sha1 '45891ec0f94bac57602d7d98dceb4e1c037595c9'
+  link 'Chromium.app'
 end
