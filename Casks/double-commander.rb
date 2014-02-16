d75ae7ea7e8394ea59e6ce5102b9d17535e71dab
Caleb Xu <calebcenter@live.com>
2014-02-16 15:40:01 -0500

    Added Double Commander
    Double Commander is a cross platform open source file manager with two
    panels side by side. It is inspired by Total Commander and features
    some new ideas.

---
+++ Casks/double-commander.rb
@@ -0,0 +1,7 @@
+class DoubleCommander < Cask
+  url 'http://downloads.sourceforge.net/sourceforge/doublecmd/doublecmd-0.5.8-5390.qt.x86_64.dmg'
+  homepage 'http://doublecmd.sourceforge.net/'
+  version '0.5.8'
+  sha256 'a4bf455f028783a4c14d600068d27556984bfc3357a80dc0715f047efbf6dbfe'
+  link 'Double Commander.app'
+end
