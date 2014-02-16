a89ab50d6edfd3dcba7b9dccba694f00b63cac2f
Caleb Xu <calebcenter@live.com>
2014-02-16 17:33:22 -0500

    Added Battle for Wesnoth
    The Battle for Wesnoth is a Free, turn-based tactical strategy game
    with a high fantasy theme, featuring both single-player, and
    online/hotseat multiplayer combat.

---
+++ Casks/battle-for-wesnoth.rb
@@ -0,0 +1,7 @@
+class BattleForWesnoth < Cask
+  url 'http://downloads.sourceforge.net/sourceforge/wesnoth/Wesnoth_1.10.7.dmg'
+  homepage 'http://wesnoth.org'
+  version '1.10.7'
+  sha256 'cdd7788e55e26c9d619b7c98b87db1b99c6a0fc9f525ddb63a6bd33923d94a6f'
+  link 'Wesnoth.app'
+end
