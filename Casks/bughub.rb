ac09137eaa75145bf28d053597eae87adceaa4a1
Vítor Galvão <info@vitorgalvao.com>
2014-02-17 15:42:18 +0000

    Removed bughub
    Pending consideration, since it includes an unofficial download source

--- Casks/bughub.rb
+++
@@ -1,7 +0,0 @@
-class Bughub < Cask
-  url 'https://github.com/trough/trough/raw/master/osx/BugHub.dmg'
-  homepage 'https://github.com/Me1000/BugHub'
-  version '1.0.1'
-  sha256 'a01eeecf1c87f5f5c0ead422c72be988c62dc88e30f2b35df0611342cdccf5a2'
-  link 'BugHub.app'
-end
