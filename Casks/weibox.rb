7e8337eaa410787f4c4602ffd4f6044293d654bc
Kane Dou <douqilong@gmail.com>
2014-02-17 11:50:37 +0800

    Add WeiboX.app v2.6.1

---
+++ Casks/weibox.rb
@@ -0,0 +1,7 @@
+class Weibox < Cask
+  url 'http://weiboformac.sinaapp.com/downloads/2.6.1.release.zip'
+  homepage 'http://weiboformac.sinaapp.com'
+  version '2.6.1'
+  sha256 '60c023ba7cc9f93ac72a7b1a4e8a518595348f4f6d8c4fbef0af8822c318a908'
+  link 'WeiboX.app'
+end
