2007d0370d007b73eb3d3d224405aa37fef540a5
Beng Hee Eu <beng@benghee.eu>
2014-02-17 12:44:42 +0800

    CrashPlan 3.6.3

--- Casks/crashplan.rb
+++ Casks/crashplan.rb
@@ -1,8 +1,8 @@
 class Crashplan < Cask
-  url 'http://download.crashplan.com/installs/mac/install/CrashPlan/CrashPlan_3.5.3_Mac.dmg'
+  url 'http://download.crashplan.com/installs/mac/install/CrashPlan/CrashPlan_3.6.3_Mac.dmg'
   homepage 'http://www.crashplan.com/'
-  version '3.5.3'
-  sha1 'b5c9b304c7191f2dd5773e0c373c495726097d0d'
+  version '3.6.3'
+  sha1 '19bdfd407e86a26f852d36d317d01418597159a8'
   install 'Install CrashPlan.pkg'
   uninstall(
     :script => 'Uninstall.app/Contents/Resources/uninstall.sh',
