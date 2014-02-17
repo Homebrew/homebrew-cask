c8954036171029212569b1846a002a1c33979e23
謝致邦 <Yeking@Red54.com>
2014-02-18 02:39:55 +0800

    OSXFUSE 2.6.2

--- Casks/osxfuse.rb
+++ Casks/osxfuse.rb
@@ -1,8 +1,8 @@
 class Osxfuse < Cask
-  url 'http://downloads.sourceforge.net/project/osxfuse/osxfuse-2.6.1/osxfuse-2.6.1.dmg'
+  url 'http://downloads.sourceforge.net/project/osxfuse/osxfuse-2.6.2/osxfuse-2.6.2.dmg'
   homepage 'http://osxfuse.github.io/'
-  version '2.6.1'
-  sha1 '2e43a6593aee5fa45166b8abc70ad054a83d083b'
+  version '2.6.2'
+  sha1 '44696348ccfb10d3e6a6514d73f090db6b69ee05'
   install 'Install OSXFUSE 2.6.pkg'
   uninstall :pkgutil => 'com.github.osxfuse.pkg.Core|com.github.osxfuse.pkg.PrefPane',
             :kext => 'com.github.osxfuse.filesystems.osxfusefs'
