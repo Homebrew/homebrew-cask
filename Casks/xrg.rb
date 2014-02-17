01e5774f686b85aa43597fa64ceffa0aaf875628
Caleb Xu <calebcenter@live.com>
2014-02-17 17:39:53 -0500

    Added XRG
    XRG is an open source system monitor for Mac OS X. XRG allows you to
    monitor CPU activity, memory usage, battery status, machine
    temperature, network activity, disk I/O, current weather, and stock
    market data. The goal of XRG is to be a functional system monitor that
    has a clean and flexible interface, so it doesn't intrude on the rest
    of your Mac experience.

---
+++ Casks/xrg.rb
@@ -0,0 +1,7 @@
+class Xrg < Cask
+  url 'http://download.gauchosoft.com/xrg/XRG-release-1.7.3.zip'
+  homepage 'http://www.gauchosoft.com/Products/XRG/'
+  version '1.7.3'
+  sha256 '972ee45e202dee87aa5a696d2393f5eece21c8e6a54368ab6fefb6a371187063'
+  link 'XRG.app'
+end
