39ef9b3da94e464d1481881bfc0cedaebc02f743
Alexey <anakros.symbian@gmail.com>
2014-02-18 13:54:12 +0700

    added clearview 1.3.0

---
+++ Casks/clearview.rb
@@ -0,0 +1,7 @@
+class Clearview < Cask
+  url 'http://www.canoejoy.com/downloads/Clearview-v1.3.0-trial.zip'
+  homepage 'http://www.canoejoy.com/'
+  version '1.3.0'
+  sha1 'fc74c9a73ea5754bb1c538c530f12876e501d61e'
+  link 'Clearview.app'
+end
