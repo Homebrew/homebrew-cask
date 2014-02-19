721aee723669b2a971ed283e62633a4b8568596e
chibee64 <chibee64@gmail.com>
2014-02-20 02:40:40 +0900

    Add Mangao

---
+++ Casks/mangao.rb
@@ -0,0 +1,7 @@
+class Mangao < Cask
+  url 'https://dl.dropboxusercontent.com/u/262366138/mangao/Mangao.app.zip'
+  homepage 'http://ryotaminami93.appspot.com/mangao.html'
+  version 'latest'
+  no_checksum
+  link 'Mangao.app'
+end
