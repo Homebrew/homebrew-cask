2e1969950f4cb148420092a7ba59d326af241967
Tolbkni Kao <tolbkni@gmail.com>
2014-02-20 00:24:39 +0800

    Add Trickster.app v2.2.1

---
+++ Casks/trickster.rb
@@ -0,0 +1,7 @@
+class Trickster < Cask
+    url 'http://dl.apparentsoft.com/Trickster_2.2.1.zip'
+    homepage 'http://www.apparentsoft.com/trickster/'
+    version '2.2.1'
+    sha256 '6a6531e8c8c7672bf422c496a478ee26f184f1c63036d7f1125913d69a067292'
+    link 'Trickster.app'
+end
