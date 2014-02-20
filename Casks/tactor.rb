5e769fce7fd2088064738da6a465c2bcf45fa07c
Joel Kuzmarski <leoj3n@gmail.com>
2014-02-20 11:11:16 -0600

    Tactor 0.4.6 BETA

---
+++ Casks/tactor.rb
@@ -0,0 +1,7 @@
+class Tactor < Cask
+  url 'https://dl.dropboxusercontent.com/u/7614970/Tactor.zip'
+  homepage 'http://onflapp.wordpress.com/tactor/'
+  version '0.4.6-BETA'
+  sha256 'bc9a2ad1bf9e86f2cf3081d9d0c815845d87712b58986d78e250eeac4224292e'
+  link 'Tactor.app'
+end
