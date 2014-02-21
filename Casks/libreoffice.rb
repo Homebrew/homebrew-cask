a3b03db9bc7619b05d3c76ffe49aae782d63d722
ndr <ndr@qef.io>
2014-02-21 15:47:25 +0100

    LibreOffice 4.2.1

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -1,7 +1,7 @@
 class Libreoffice < Cask
-  url 'http://download.documentfoundation.org/libreoffice/stable/4.2.0/mac/x86_64/LibreOffice_4.2.0_MacOS_x86-64.dmg'
+  url 'http://download.documentfoundation.org/libreoffice/stable/4.2.1/mac/x86_64/LibreOffice_4.2.1_MacOS_x86-64.dmg'
   homepage 'http://www.libreoffice.org/'
-  version '4.2.0'
-  sha1 '267c50b3018aa784420bb967954942a4447ad99c'
+  version '4.2.1'
+  sha1 '77cae1d8867270e1a90a8b210b2db987735b9b56'
   link 'LibreOffice.app'
 end
