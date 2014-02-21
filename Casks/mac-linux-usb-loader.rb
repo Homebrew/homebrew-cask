57ed2a18ac97b61bbd7902df13b07a4bff97e493
SevenBits <meokid2@yahoo.com>
2014-02-21 18:05:34 -0500

    Added cask for Mac Linux USB Loader.

---
+++ Casks/mac-linux-usb-loader.rb
@@ -0,0 +1,7 @@
+class MacLinuxUSBLoader < Cask
+  url 'http://sevenbits.github.io/downloads/Mac-Linux-USB-Loader.zip'
+  homepage 'http://sevenbits.github.io/Mac-Linux-USB-Loader/'
+  version '2.0'
+  no_checksum
+  link 'Mac Linux USB Loader.app'
+end
