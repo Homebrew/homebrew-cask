be464ac57103b340c859bc98bd0e64c3c7ed6ef0
BingFoon Lee <BingFoon@Gmail.com>
2014-02-21 23:02:34 +0800

    Add ZendStudio
    Add ZendServer
    
    Update and rename zend-studio.rb to zendstudio.rb
    
    Update and rename zend-server.rb to zendserver.rb

---
+++ Casks/zendstudio.rb
@@ -0,0 +1,7 @@
+class Zendstudio < Cask
+  url 'http://downloads.zend.com/studio-eclipse/10.6.0/ZendStudio-10.6.0-macosx.cocoa.x86_64.dmg'
+  homepage 'http://www.zend.com/en/products/studio/'
+  version '10.6.0'
+  sha256 '391c77f62e281f7d6dc8039962d4fcb03b66e8c1a28267840b9f3af3f9cc30e7'
+  link 'Zend Studio/ZendStudio.app'
+end
