4caded98bef80bb0008f4e79e0a5ac97e9514b41
Tolbkni Kao <tolbkni@gmail.com>
2014-02-24 02:06:24 +0800

    Bump Maxthon version to 4.1.3.2600

--- Casks/maxthon.rb
+++ Casks/maxthon.rb
@@ -1,7 +1,7 @@
 class Maxthon < Cask
-  url 'http://dl.maxthon.com/mac/Maxthon-4.1.3.2000.dmg'
+  url 'http://dl.maxthon.com/mac/Maxthon-4.1.3.2600.dmg'
   homepage 'http://www.maxthon.com/'
-  version '4.1.3.2000'
-  sha1 '80176508dd94b65c86f6db65e295ba27b76876f3'
+  version '4.1.3.2600'
+  sha1 '5e39758d1d73a710f91352db6447a4e33a3bc2dd'
   link 'Maxthon.app'
 end
