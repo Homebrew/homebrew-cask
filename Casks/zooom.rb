b1282ffe59480d40acf98e79d27cc6e389bde83e
Roland Walker <walker@pobox.com>
2014-02-24 09:24:06 -0500

    Zooom is not supported on Mavericks
    fixes #2463

--- Casks/zooom.rb
+++ Casks/zooom.rb
@@ -4,4 +4,7 @@ class Zooom < Cask
   version '2.5.0'
   sha256 '944d8c6f0869963ea0c0b11491d05eaa4357953072c43519f674fa81216e9e34'
   install 'Zooom2.pkg'
+  caveats do
+    os_version_only '10.8', '10.7', '10.6', '10.5'
+  end
 end
