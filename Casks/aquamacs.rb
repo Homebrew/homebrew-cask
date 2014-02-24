1feb70ed3e7faf96c09722c110b63304db43f80b
Roland Walker <walker@pobox.com>
2014-02-24 12:07:02 -0500

    use hardware conditional for Aquamacs
    instead of caveats

--- Casks/aquamacs.rb
+++ Casks/aquamacs.rb
@@ -1,12 +1,13 @@
 class Aquamacs < Cask
-  url 'https://github.com/davidswelt/aquamacs-emacs/releases/download/Aquamacs-3.0a/Aquamacs-Emacs-3.0a.dmg'
+  if Hardware::CPU.is_64_bit?
+    url 'https://github.com/davidswelt/aquamacs-emacs/releases/download/Aquamacs-3.0a/Aquamacs-Emacs-3.0a.dmg'
+    version '3.0a'
+    sha256 '1d833cb2e40c1af96713badc4efa7c1c9259317b91edcfe17059c73e989f9e07'
+  else
+    url 'http://braeburn.aquamacs.org/releases/Aquamacs-Emacs-2.5.dmg'
+    version '2.5'
+    sha256 '5857848d8d46bba43d160c02393b098a370e2156608be24b288419f668210be9'
+  end
   homepage 'http://aquamacs.org/'
-  version '3.0a'
-  sha256 '1d833cb2e40c1af96713badc4efa7c1c9259317b91edcfe17059c73e989f9e07'
   link 'Aquamacs.app'
-  caveats <<-EOS.undent
-    This version of Aquamacs is for 64-bit Intel-based Macs only. If you have
-    a 32-bit Intel-based Mac, please run 'brew tap caskroom/versions' and
-    install aquamacs25.
-    EOS
 end
