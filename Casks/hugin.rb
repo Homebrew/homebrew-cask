6632ba671f7a673a621be2e342457aa2e9737fe9
Caleb Xu <calebcenter@live.com>
2014-02-23 19:52:44 -0500

    Added Hugin
     With Hugin you can assemble a mosaic of photographs into a complete
    immersive panorama, stitch any series of overlapping pictures and much
    more.

---
+++ Casks/hugin.rb
@@ -0,0 +1,7 @@
+class Hugin < Cask
+  url 'http://downloads.sourceforge.net/sourceforge/hugin/Hugin-2013.0.0.dmg'
+  homepage 'http://hugin.sourceforge.net/'
+  version '2013.0.0'
+  sha256 '528cc2249d4cdf9267e85ab836ff36cc0e2450de6663661adfc5e2cc3ef1ec60'
+  link 'Hugin.app'
+end
