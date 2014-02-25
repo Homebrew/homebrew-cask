9f91378cfd59ba5b70379950d1b523acf0aeaef2
Caleb Xu <calebcenter@live.com>
2014-02-25 17:34:14 -0500

    Update/fix Postbox

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -2,6 +2,6 @@ class Postbox < Cask
   url 'http://www.postbox-inc.com/php/download.php?a=308m'
   homepage 'http://www.postbox-inc.com/'
   version '3.0.8'
-  sha1 '78613f2111a6345ff1aeba63682a33888e2c256b'
+  sha256 '2a80f6c79d29c8bb499cdaf5818e2e38000ddbb5f7cb73cc8476da74c6b6b199'
   link 'Postbox.app'
 end
