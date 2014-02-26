414570a53ead99c3cd9f23cb86def9ec8f2f2050
Roland Walker <walker@pobox.com>
2014-02-26 07:33:11 -0500

    PS3 Media Server uses a manual installer

--- Casks/ps3-media-server.rb
+++ Casks/ps3-media-server.rb
@@ -3,5 +3,8 @@ class Ps3MediaServer < Cask
   homepage 'http://www.ps3mediaserver.org/'
   version '1.90.1'
   sha256 '3ebe75ce0dbdc1313c10fb901f845564cc343dc3b7487f07e15db9d757850df5'
-  link 'PS3 Media Server Setup.app'
+  caskroom_only true
+  caveats do
+    manual_installer 'PS3 Media Server Setup.app'
+  end
 end
