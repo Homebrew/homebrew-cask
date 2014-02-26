588da31f66139047c9aec2216a54869f7610d50b
Roland Walker <walker@pobox.com>
2014-02-26 07:50:53 -0500

    rename pycharm-pro.rb
    to pycharm.rb
    per naming rules in CONTRIBUTING.md

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,4 +1,4 @@
-class PycharmPro < Cask
+class Pycharm < Cask
   url 'http://download.jetbrains.com/python/pycharm-professional-3.1.dmg'
   homepage 'http://www.jetbrains.com/pycharm/'
   version '3.1'
