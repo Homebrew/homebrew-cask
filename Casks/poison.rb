32017f7f6f15f972790d3dbcf4d2250611d0dacb
Roland Walker <walker@pobox.com>
2014-02-26 07:52:16 -0500

    rename tox-poison.rb
    to poison.rb
    per naming rules in CONTRIBUTING.md

--- Casks/poison.rb
+++ Casks/poison.rb
@@ -1,4 +1,4 @@
-class ToxPoison < Cask
+class Poison < Cask
   url 'http://jenkins.tox.im/job/Poison_OS_X/lastSuccessfulBuild/artifact/arc/poison-with-extras.zip'
   homepage 'http://tox.im'
   version 'latest'
