3f74e8f02010e978482e63fecab10e39e500ffc5
BingFoon Lee <bingfoon@gmail.com>
2014-02-27 01:50:10 +0800

    Add WeChat.app (zh_CN)

---
+++ Casks/weixin.rb
@@ -0,0 +1,7 @@
+class Weixin < Cask
+  url 'http://dldir1.qq.com/foxmail/Mac/WeChat-zh_CN.dmg'
+  homepage 'http://weixin.qq.com/cgi-bin/readtemplate?t=mac&lang=zh_CN'
+  version 'latest'
+  no_checksum
+  link 'WeChat.app'
+end
