9f0719325b153878f2149d12a93c541aaf0c2a67
Roland Walker <walker@pobox.com>
2014-02-26 11:36:24 -0500

    quiet "move to applications" popup for github.rb

--- Casks/github.rb
+++ Casks/github.rb
@@ -4,4 +4,7 @@ class Github < Cask
   version 'latest'
   no_checksum
   link 'GitHub.app'
+  after_install do
+    system 'defaults', 'write', 'com.github.GitHub', 'moveToApplicationsFolderAlertSuppress', '-bool', 'true'
+  end
 end
