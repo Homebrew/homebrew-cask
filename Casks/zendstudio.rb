031a6a8c286015f5ddf2d24011ce808edd9e4ef5
BingFoon Lee <bingfoon@gmail.com>
2014-02-27 01:36:41 +0800

    Fix symlink path error in ZendStudio.app Cask

--- Casks/zendstudio.rb
+++ Casks/zendstudio.rb
@@ -3,5 +3,5 @@ class Zendstudio < Cask
   homepage 'http://www.zend.com/en/products/studio/'
   version '10.6.0'
   sha256 '391c77f62e281f7d6dc8039962d4fcb03b66e8c1a28267840b9f3af3f9cc30e7'
-  link 'Zend Studio/ZendStudio.app'
+  link 'ZendStudio.app'
 end
