dec670495b08cd2fc4d98ecab610480d5efb8728
Roland Walker <walker@pobox.com>
2014-02-26 06:37:32 -0500

    use vendor site for URL
    closes #3208

--- Casks/clix.rb
+++ Casks/clix.rb
@@ -1,7 +1,7 @@
 class Clix < Cask
-  url 'https://www.macupdate.com/download/13983/CLIX.zip'
+  url 'ftp://rixstep.com/CLIX.tar.bz2'
   homepage 'http://rixstep.com/4/0/clix/index.shtml'
   version '2.1'
-  sha256 'ab85b3d1d9b298a6f476854de778203767fd13c1ede16e3cdd3aeea0f63e9c05'
+  sha256 'a4f9d270792e9da698326924e4c899c7a5f13f157c7793b82187688f8c189008'
   link 'CLIX2.1/CLIX.app'
 end
