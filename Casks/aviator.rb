061af60e4f9330f9de3f5ebf9019349d76a9e351
Eugene Ching <eugene@enegue.com>
2014-02-27 22:04:26 +0800

    Add Aviator.app.
    Cask for Aviator, a secure browser developed by WhiteHat Security.
    Aviator focuses on security, privacy and speed.

---
+++ Casks/aviator.rb
@@ -0,0 +1,7 @@
+class Aviator < Cask
+  url 'https://updates.aviatorbrowser.com/Aviator.dmg'
+  homepage 'https://www.whitehatsec.com/aviator/'
+  version 'latest'
+  sha1 'd55a879f4ba3beb14729ed5efda8e83f058dd0b8'
+  link 'Aviator.app'
+end
