17679f7d0612b02d48802700ec96bc8be8ba9b02
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2014-02-27 14:16:21 -0500

    New cask: stella 3.9.3

---
+++ Casks/stella.rb
@@ -0,0 +1,7 @@
+class Stella < Cask
+  url 'http://downloads.sourceforge.net/project/stella/stella/3.9.3/Stella-3.9.3_intel-macosx.dmg'
+  homepage 'http://stella.sourceforge.net'
+  version '3.9.3'
+  sha256 '83870088368be20224ed51f52e9babe53a75575495279adc596203f123e8477c'
+  link 'Stella.app'
+end
