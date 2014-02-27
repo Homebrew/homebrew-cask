61661ba524fb0c48853bb1e3482e78dbfb41204f
warpcode <git@warpcode.eu>
2014-02-27 22:04:02 +0000

    Update MakeMKV to version 1.8.9

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,7 +1,7 @@
 class Makemkv < Cask
-  url 'http://www.makemkv.com/download/makemkv_v1.8.8_osx.dmg'
+  url 'http://www.makemkv.com/download/makemkv_v1.8.9_osx.dmg'
   homepage 'http://www.makemkv.com/'
-  version '1.8.8'
-  sha256 '3781cc41523417bd87d44177ed5d3943d8e37f18d8c398f90d799a76f4761dcd'
+  version '1.8.9'
+  sha256 '9a854fb40b035c77e470ae523692243eb8f7d13485fb57a1e760f5f519cd705b'
   link 'MakeMKV.app'
 end
