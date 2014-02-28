a195d521602f33630d25a85fa71eee911725aed7
Beng Hee Eu <beng@benghee.eu>
2014-02-28 23:18:14 +0800

    Skitch 2.7.2

--- Casks/skitch.rb
+++ Casks/skitch.rb
@@ -1,7 +1,7 @@
 class Skitch < Cask
-  url 'http://cdn1.evernote.com/skitch/mac/release/Skitch-2.7.1.zip'
+  url 'http://cdn1.evernote.com/skitch/mac/release/Skitch-2.7.2.zip'
   homepage 'http://evernote.com/skitch/'
-  version '2.7.1'
-  sha256 'a87967579b77461d753e8e4956185016a687c0a07bb9e614f63599b4f5ee4a10'
+  version '2.7.2'
+  sha256 '2388c60445782ac7afd1c7fc311007156a98b8e7b5488ede65cc5a68263da597'
   link 'Skitch.app'
 end
