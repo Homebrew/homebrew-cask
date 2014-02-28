ba07c931db86b90d894b7b438461823b726774d6
Ali Karbassi <ali@karbassi.com>
2014-02-27 22:47:19 -0600

    Updated Obsidian Menu Bar.
    Updated URLs and sha1 to sha256 checksum.

--- Casks/obsidian-menu-bar-mountain-lion.rb
+++ Casks/obsidian-menu-bar-mountain-lion.rb
@@ -1,8 +1,8 @@
 class ObsidianMenuBarMountainLion < Cask
-  url 'http://www.maxrudberg.com/downloads/Obsidian%20Menu%20Bar%2010.8.dmg'
-  homepage 'http://www.maxrudberg.com'
+  url 'http://www.obsidianmenubar.com/downloads/Obsidian%20Menu%20Bar%2010.8.dmg'
+  homepage 'http://www.obsidianmenubar.com'
   version '10.8'
-  sha1 'e4098264356624803df62b6409a0e2016a2a72cd'
+  sha256 '97c8545c623ec78a0a81c2b7bb6a7afa9d0dd517a64b74f5320e2e279a2786aa'
   install 'Obsidian Menu Bar.pkg'
   # uninstall :script => 'Restore OS X Menu Bar.pkg'
 end
