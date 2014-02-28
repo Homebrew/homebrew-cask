f47ab893d0c46a12e7a3e4d71489388e55f7d8e7
Minami <lolicon@mail.az>
2014-02-28 16:48:24 +0800

    add wechat cask

---
+++ Casks/wechat.rb
@@ -0,0 +1,8 @@
+class Wechat < Cask
+  url 'http://dldir1.qq.com/foxmail/Mac/WeChat-zh_CN.dmg'
+  homepage 'http://weixin.qq.com'
+  version 'latest'
+  #sha1 '723e750fdc42001913a2daabfe9c3245a737a0a6'
+  no_checksum
+  link 'WeChat.app'
+end
