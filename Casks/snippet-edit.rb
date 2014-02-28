84155d854e86ba4ee0bc4faf06d0f97cd7f4f480
Dave Caunt <dcaunt@gmail.com>
2014-02-28 17:32:47 +0000

    Add Snippet Edit v1.1

---
+++ Casks/snippet-edit.rb
@@ -0,0 +1,7 @@
+class SnippetEdit < Cask
+  url 'http://cocoaholic.com/downloads/snippet_edit/Snippet_Edit_1.1.zip'
+  homepage 'http://cocoaholic.com/snippet_edit/'
+  version '1.1'
+  sha256 '90cc8cca7ddbf1e17cc4dc8838a99c5ec2c0dcea3758e09c0ff2b6fa472eeb82'
+  link 'Snippet Edit.app'
+end
