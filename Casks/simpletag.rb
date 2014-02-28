c8bb16e6403273bf0c6e30aea84202222721fe9e
Ali Karbassi <ali@karbassi.com>
2014-02-27 23:59:43 -0600

    Adding SimpleTAG
    Fixes and closes #2642

--- Casks/simpletag.rb
+++ Casks/simpletag.rb
@@ -1,7 +1,7 @@
 class Simpletag < Cask
-  url 'http://kaz.dl.sourceforge.net/project/simpletag/Mac/simpletag-gui-1.5.3-osx.zip'
+  url 'http://sourceforge.net/projects/simpletag/files/latest/download'
   homepage 'http://sourceforge.net/projects/simpletag/'
-  version '1.5.3'
-  sha256 'e589eea30075c7c327f6601400dca77af1cc1415611b5635fbdff16d2046115b'
+  version 'latest'
+  no_checksum
   link 'simpletag-gui-jface-1.5.3/SimpleTAG-GUI.app'
 end
