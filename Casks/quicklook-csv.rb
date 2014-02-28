e7ed2fa37c41a5fbf0f8e8c8ab745533060e58ac
Shaun Jackman <sjackman@gmail.com>
2014-02-28 12:58:20 -0800

    quicklook-csv 1.3: Add version and SHA

--- Casks/quicklook-csv.rb
+++ Casks/quicklook-csv.rb
@@ -1,7 +1,7 @@
 class QuicklookCsv < Cask
   url 'http://quicklook-csv.googlecode.com/files/QuickLookCSV.dmg'
   homepage 'https://github.com/p2/quicklook-csv'
-  version 'latest'
-  no_checksum
+  version '1.3'
+  sha256 'e052e89f8003aed08bb2058e3fb3335ac0a5cdaa0171bfb23b762976e095ef5b'
   qlplugin 'QuickLookCSV.qlgenerator'
 end
