908e5efaf7572c7f9db7e72f849be9bf0d8eb26b
phpwutz <phpwutz@gmail.com>
2014-03-01 12:08:53 +0100

    Update owncloud.rb
    updated to version 1.5.2.2335

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,7 +1,7 @@
 class Owncloud < Cask
-  url 'http://download.owncloud.com/desktop/stable/ownCloud-1.5.1.2275.dmg'
+  url 'http://download.owncloud.com/desktop/stable/ownCloud-1.5.2.2335.dmg'
   homepage 'http://owncloud.com'
-  version '1.5.1.2275'
-  sha256 'c976bdfb89f47be29fed670626c923811b8289e8fdfed8621bb8ec0f56743a5f'
+  version '1.5.2.2335'
+  sha256 '8e0f320b1146241a9337fb47c32ebc9073181ea377988984b94da36fe45f14bf'
   link 'owncloud.app'
 end
