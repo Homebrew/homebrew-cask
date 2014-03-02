445edbe5fb9d9444f5a49ad83d68f52def8cd57f
Joel Kuzmarski <leoj3n@gmail.com>
2014-03-02 12:44:19 -0600

    aseprite 0.9.5

---
+++ Casks/aseprite.rb
@@ -0,0 +1,7 @@
+class Aseprite < Cask
+  url 'http://aseprite.googlecode.com/files/ASEPRITE_0.9.5.dmg'
+  homepage 'http://www.aseprite.org'
+  version '0.9.5'
+  sha256 '299eda3e5f11ae60d58bccdd26156169db6ebc01be8e806d7909f0b8f22e2928'
+  link 'aseprite.app'
+end
