602db1cb06409c2c2822ca721402a357b1f0ef91
narango <slickarango@gmail.com>
2014-03-03 11:08:53 -0500

    added ltspice 4.0

---
+++ Casks/ltspice.rb
@@ -0,0 +1,7 @@
+class Ltspice < Cask
+  url 'http://ltspice.linear-tech.com/LTspiceIV.dmg'
+  homepage 'http://www.linear.com/designtools/software/'
+  version '4.0'
+  sha1 '9fa21d5dab4248961a81294471f018fec270a1a9'
+  link 'LTspice.app'
+end
