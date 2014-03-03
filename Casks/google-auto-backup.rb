756b2ed07d114f0f1e420c7e79df8d73430d1a5e
Sam Tsai <TsaiSa@ae.com>
2014-03-03 12:35:44 -0500

    Add Google+ Auto Backup.app latest version
    This is the backup tool that comes with latest version of Picasa.
    Add it as its own cask if you don't want to download Picasa.
    
    More info:
    https://support.google.com/picasa/answer/4392268?p=gpautobackup&rd=1

---
+++ Casks/google-auto-backup.rb
@@ -0,0 +1,7 @@
+class GoogleAutoBackup < Cask
+  url 'https://dl.google.com/dl/edgedl/picasa/gpautobackup_setup.dmg'
+  homepage ''
+  version 'latest'
+  no_checksum
+  link 'Google+ Auto Backup.app'
+end
