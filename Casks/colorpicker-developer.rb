3fd1f8035f449a1cae45703f5e2547038572737f
Kyle Mathews <mathews.kyle@gmail.com>
2014-03-04 12:21:58 -0800

    Update colorpicker-developer.rb
    URLs changed

--- Casks/colorpicker-developer.rb
+++ Casks/colorpicker-developer.rb
@@ -1,6 +1,6 @@
 class ColorpickerDeveloper < Cask
-  url 'http://panic.com/~wade/picker/developercolorpicker.zip'
-  homepage 'http://panic.com/~wade/picker/'
+  url 'http://download.panic.com/picker/developercolorpicker.zip'
+  homepage 'http://download.panic.com/picker/'
   version 'latest'
   no_checksum
   colorpicker 'Developer Color Picker/DeveloperColorPicker.colorPicker'
