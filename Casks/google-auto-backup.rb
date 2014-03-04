147cb065195aae2dcccceb3821cbefe330de5af2
Sam Tsai <TsaiSa@ae.com>
2014-03-03 20:04:40 -0500

    Add Picasa as Homepage
    The Google+ Auto Backup comes as part of Picasa and so this homepage
    makes the most contextual sense, even though you don't have to install
    Picasa to use it.

--- Casks/google-auto-backup.rb
+++ Casks/google-auto-backup.rb
@@ -1,6 +1,6 @@
 class GoogleAutoBackup < Cask
   url 'https://dl.google.com/dl/edgedl/picasa/gpautobackup_setup.dmg'
-  homepage ''
+  homepage 'http://picasa.google.com/'
   version 'latest'
   no_checksum
   link 'Google+ Auto Backup.app'
