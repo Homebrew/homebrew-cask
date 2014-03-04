cd15aa7124ab6451c0d7d2ed0b0fde0d28c17141
Caleb Xu <calebcenter@live.com>
2014-03-04 09:40:44 -0500

    Added Xee
    Xee is a lightweight, fast and convenient image viewer and browser. It
    is designed to be a serious tool for image viewing and management, with
    a sleek and powerful interface.
    
    Xee is useful as a more powerful replacement for Preview, or most any
    other image viewer available on Mac OS X. It is very fast, and uses
    less memory than most other image viewing tools. It also uses OpenGL to
    display and scroll images much more smoothly than other viewers. Its
    interface is highly streamlined and has easily configurable keyboard
    shortcuts.

---
+++ Casks/xee.rb
@@ -0,0 +1,7 @@
+class Xee < Cask
+  url 'http://wakaba.c3.cx/releases/mac/Xee3.3.zip'
+  homepage 'http://xee.c3.cx/'
+  version '3.3'
+  sha256 '8728d70c392b6da025a292ab62b47698c1e7f5a45b75eb797c9832b3a37d0062'
+  link 'Xee.app'
+end
