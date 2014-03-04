fa07ed312cc6ddea626385d83357f08e6e8056a3
Newton <iam@chrisopedia.com>
2014-03-03 18:02:22 -0800

    Update MinimalClock to use 'no_checksum'

--- Casks/minimalclock.rb
+++ Casks/minimalclock.rb
@@ -2,6 +2,6 @@ class Minimalclock < Cask
   url 'http://www.ilovecolorz.net/download/MinimalClockScreenSaver.dmg'
   homepage 'http://ilovecolorz.net/minimalclock/'
   version 'latest'
-  sha1 'no_checksum'
+  no_checksum
   screen_saver 'MinimalClock.qtz'
 end
