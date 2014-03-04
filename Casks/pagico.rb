02df8f0d0c6db463559feb545d741cd781ec5cb8
Caleb Xu <calebcenter@live.com>
2014-03-04 13:25:03 -0500

    Updated Pagico

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,7 +1,10 @@
 class Pagico < Cask
-  url 'http://pagico.com/downloads/Pagico_Desktop_r1475.dmg'
+  url 'http://pagico.com/downloads/Pagico_Desktop_r1549.dmg'
   homepage 'http://pagico.com/'
-  version '6.3 (r1475)'
-  sha1 '6dbc4e075f1792bed488f3c15efc58a039a93521'
+  version '6.5 (r1549)'
+  sha256 'ad28214b082208025f9103dfbc2149b8224f0c488dfdcb06b51090e23d285bed'
   install 'Install Pagico 6.pkg'
+  uninstall :pkgutil => 'com.pagico.*pkg'
+  uninstall :pkgutil => 'com.pagico.*'
+  uninstall :files => '/Applications/Pagico'
 end
