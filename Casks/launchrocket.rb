9cc2a1dbb85d07da9f83e5433f3f578a95d4c4ae
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-03-04 22:07:27 -0600

    Update launchrocket to sha256

--- Casks/launchrocket.rb
+++ Casks/launchrocket.rb
@@ -2,6 +2,6 @@ class Launchrocket < Cask
   url 'https://github.com/jimbojsb/launchrocket/releases/download/v0.5.1/LaunchRocket.prefPane.zip'
   homepage 'http://guthub.com/jimbojsb/launchrocket'
   version '0.5.1'
-  sha1 '8aab19f421ab15e48e250b3de824eb3d60236e0f'
+  sha256 '3248ddcb529a91c90f9894586b205ecfb83c8ecfb819ce7bbdb2f783b92cb406'
   prefpane 'LaunchRocket.prefPane'
 end
