2ef46818cac4cf2ce8d879a3fa4bf00bd40a27e0
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-03-04 19:48:10 -0600

    Update Sage to 6.1.1

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -1,10 +1,10 @@
 class Sage < Cask
-  url 'http://boxen.math.washington.edu/home/sagemath/sage-mirror/osx/intel/sage-6.0-x86_64-Darwin-OSX_10.6_x86_64-app.dmg'
+  url 'http://boxen.math.washington.edu/home/sagemath/sage-mirror/osx/intel/sage-6.1.1-x86_64-Darwin-OSX_10.9_x86_64-app.dmg'
   homepage 'http://www.sagemath.org/'
-  version '6.0'
-  sha1 '0690169fb945237b3b5ef854e5ed460baae55a1c'
-  link 'Sage-6.0.app'
-  binary 'Sage-6.0.app/Contents/Resources/sage/sage'
+  version '6.1.1'
+  sha256 'adb970fc837f67e1db73fb78f1ef142e536ecf0553672440ca485c33745fb33e'
+  link 'Sage-6.1.1.app'
+  binary 'Sage-6.1.1.app/Contents/Resources/sage/sage'
   caveats do
     files_in_usr_local
   end
