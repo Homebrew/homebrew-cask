56231698ac844ea7c103c4d22e823dcf847e3950
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-03-04 22:15:18 -0600

    Update LTSpice to sha256

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -2,6 +2,6 @@ class Ltspice < Cask
   url 'http://ltspice.linear-tech.com/LTspiceIV.dmg'
   homepage 'http://www.linear.com/designtools/software/'
   version '4.0'
-  sha1 '9fa21d5dab4248961a81294471f018fec270a1a9'
+  sha256 '68b7be25d86d34f1b03a80cfbef50e94075e7649686d9ff160a441ade258384f'
   link 'LTspice.app'
 end
