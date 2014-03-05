050d8567e42150710755fae343775da80b8f62bf
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-03-04 18:59:04 -0600

    Update Opera Next to 18.0.1284.26

--- Casks/opera-next.rb
+++ Casks/opera-next.rb
@@ -1,7 +1,7 @@
 class OperaNext < Cask
-  url 'http://get.geo.opera.com/pub/opera-next/18.0.1284.26/mac/Opera_Next_18.0.1284.26_Setup.dmg'
+  url 'http://get.geo.opera.com/pub/opera-next/20.0.1387.59/mac/Opera_Next_20.0.1387.59_Setup.dmg'
   homepage 'http://www.opera.com/computer/next'
-  version '18.0.1284.26'
-  sha1 'e61e4e0960a2cb54dd9736b21720eea2b119af46'
+  version '20.0.1387.59'
+  sha256 '9052e043eecfa9b85aafd0cf6c5881994ca08bf034b2967765b9c56e6d8affd3'
   link 'Opera Next.app'
 end
