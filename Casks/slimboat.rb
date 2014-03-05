d9be7b05668f4b0d6c333f7dc3c272e6a5d978ac
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-03-04 19:13:56 -0600

    Update Slimboat

--- Casks/slimboat.rb
+++ Casks/slimboat.rb
@@ -1,7 +1,7 @@
 class Slimboat < Cask
-  url 'http://dl.filehorse.com/mac/browsers-and-plugins/slimboat-for-mac/SlimBoat-for-Mac-1.1.45.dmg?st=quc9LQO-3SW71akjdYcgpw&e=1392514118&fn=slimboat.dmg'
+  url 'http://www.slimboat.com/release/slimboat.dmg'
   homepage 'http://www.slimboat.com'
-  version '1.1.45'
-  sha1 '5cc6d4ef103490faf51ddb5085377027b96077c4'
+  version 'latest'
+  no_checksum
   link 'SlimBoat.app'
 end
