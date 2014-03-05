215e47e906c481dc3e03c73840a9491d17c87f1d
Caleb Xu <calebcenter@live.com>
2014-03-04 21:53:07 -0500

    Added Spark
    Spark is a powerful, and easy Shortcuts manager. With Spark you can
    create Hot Keys to launch applications and documents, execute
    AppleScript, control iTunes, and more...

---
+++ Casks/spark.rb
@@ -0,0 +1,7 @@
+class Spark < Cask
+  url 'http://www.shadowlab.org/softwares/Spark/Spark.dmg'
+  homepage 'http://www.shadowlab.org/softwares/spark.php'
+  version 'latest'
+  no_checksum
+  link 'Spark.app'
+end
