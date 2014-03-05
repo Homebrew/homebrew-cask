d97f4a3bf3d6328b83a10ea28c69048427e662ce
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-03-04 18:28:05 -0600

    Update GPower to 3.1.9

--- Casks/gpower.rb
+++ Casks/gpower.rb
@@ -1,7 +1,7 @@
 class Gpower < Cask
-  url 'http://www.psycho.uni-duesseldorf.de/abteilungen/aap/gpower3/download-and-register/Dokumente/GPower3.1.7Mac.zip'
-  homepage 'http://www.psycho.uni-duesseldorf.de/abteilungen/aap/gpower3'
-  version '3.1.7'
-  sha1 '2fd5ba47a3d943fe35505a9a8e781f7b4c6231c6'
-  link 'G*Power 3.app'
+  url 'http://www.gpower.hhu.de/fileadmin/redaktion/Fakultaeten/Mathematisch-Naturwissenschaftliche_Fakultaet/Psychologie/AAP/gpower/GPowerMac_3.1.9.zip'
+  homepage 'http://www.gpower.hhu.de/'
+  version '3.1.9'
+  sha256 '6da484105c316789a411a493eb3c2c0c522cb7c5a4fccf45d75ff9bcd1f7f715'
+  link 'G*Power.app'
 end
