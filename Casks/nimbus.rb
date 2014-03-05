3dc5726071bbfd2b08b9836332b666cff889d787
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-03-04 18:53:31 -0600

    Update nimbus to sha256

--- Casks/nimbus.rb
+++ Casks/nimbus.rb
@@ -2,6 +2,6 @@ class Nimbus < Cask
   url 'https://github.com/jnordberg/irccloudapp/releases/download/0.7.1/Nimbus-0.7.1.zip'
   homepage 'https://github.com/jnordberg/irccloudapp'
   version '0.7.1'
-  sha1 'daa60412e78bcb22e2fa65f5a0175c53e5142f82'
+  sha256 'a755d8a32cd9bf5be2e4de182aa3b6edb34250bcf4966a1ced8cfdd19911e1a3'
   link 'Nimbus.app'
 end
