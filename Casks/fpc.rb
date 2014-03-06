bfd6d29b5ff53e8525ee1ce1011240c300e8fd59
Caleb Xu <caleb@caleb.local>
2014-03-06 17:23:12 -0500

    Add uninstall stanza to FPC

--- Casks/fpc.rb
+++ Casks/fpc.rb
@@ -4,4 +4,5 @@ class Fpc < Cask
   version '2.6.2'
   sha256 '09b0964c6fb11eaa04e0fa065e479674384aab81e69e377bb1e030ec1d3398a6'
   install 'fpc-2.6.2rc1.intel-macosx.pkg'
+  uninstall :pkgutil => 'org.freepascal.freePascalCompiler262.fpcinst386.pkg'
 end
