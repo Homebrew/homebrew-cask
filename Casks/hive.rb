59911fdb0dc80f633f87cd76aeb24ce7e8d48c69
ndr <ndr@qef.io>
2014-03-07 05:33:48 +0100

    Update Hive to 1.1.3

--- Casks/hive.rb
+++ Casks/hive.rb
@@ -1,7 +1,7 @@
 class Hive < Cask
-  url 'https://github.com/hivewallet/hive-osx/releases/download/2014012801/Hive-1.0.zip'
+  url 'https://github.com/hivewallet/hive-osx/releases/download/1.1.3/Hive-1.1.3.zip'
   homepage 'http://www.hivewallet.com'
-  version '1.0'
-  sha256 '156bfd147d3c4397cd13d3bbefa8d3ad92104261afcd991607c8a3a6947a874d'
+  version '1.1.3'
+  sha256 '5ab5dee7be22567be1e41c1fca779377a72724f9e35185fe7d22be9310d93cb0'
   link 'Hive.app'
 end
