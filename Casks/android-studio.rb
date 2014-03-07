67d48d86fbf5b7590ff8dfab6fa9e00194d87eaa
Alex Selesse <alex@selesse.com>
2014-03-06 23:47:57 -0500

    Android Studio 0.5.0 build-134.1058404

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,7 +1,7 @@
 class AndroidStudio < Cask
-  url 'http://dl.google.com/dl/android/studio/ide-zips/0.4.6/android-studio-ide-133.1028713-mac.zip'
+  url 'http://dl.google.com/dl/android/studio/ide-zips/0.5.0/android-studio-ide-134.1058404-mac.zip'
   homepage 'https://developer.android.com/sdk/installing/studio.html'
-  version '0.4.6 build-133.1028713'
-  sha256 'e4329de3ddeec093857258cf774faa7ed2bb447af1fd345a5f616e4dd9e1e958'
+  version '0.5.0 build-134.1058404'
+  sha256 'a500c88ac0b407f3f2db5563bef332880cbcf17aa4bde2828165ba2687e793cb'
   link 'Android Studio.app'
 end
