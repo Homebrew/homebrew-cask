b1e74af69be00237ebb8b9ff202cf0eb7d71d420
Roland Walker <walker@pobox.com>
2014-03-07 09:11:33 -0500

    Avoid multiple `uninstall` artifacts.
    In #3431, we recently discovered that this form is valid
    to the DSL, but can exercise a bug at uninstall time.

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -4,7 +4,6 @@ class Pagico < Cask
   version '6.5 (r1549)'
   sha256 'ad28214b082208025f9103dfbc2149b8224f0c488dfdcb06b51090e23d285bed'
   install 'Install Pagico 6.pkg'
-  uninstall :pkgutil => 'com.pagico.*pkg'
-  uninstall :pkgutil => 'com.pagico.*'
-  uninstall :files => '/Applications/Pagico'
+  uninstall :pkgutil => 'com.pagico.*',
+            :files => '/Applications/Pagico'
 end
