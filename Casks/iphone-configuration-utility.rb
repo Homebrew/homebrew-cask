02eb46da63b098a61171ad27452b555f2ed74778
Caleb Xu <caleb@caleb.local>
2014-03-06 22:03:11 -0500

    Add uninstall stanza for iPhone Configuration Utility

--- Casks/iphone-configuration-utility.rb
+++ Casks/iphone-configuration-utility.rb
@@ -4,4 +4,5 @@ class IphoneConfigurationUtility < Cask
   version '3.5'
   sha256 '822204947ae5f7739bcb1e9694814f8e3ef65cd184952b76104f525d97a28163'
   install 'iPhoneConfigurationUtility.pkg'
+  uninstall :pkgutil => 'com.apple.pkg.iPhoneConfigurationUtility.*'
 end
