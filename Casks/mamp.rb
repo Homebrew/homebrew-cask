d07e20c04972fc26796a8ac22d4a732845725ae0
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-03-07 22:20:52 -0300

    Update MAMP to version 3.0.2

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,9 +1,9 @@
 class Mamp < Cask
-  url 'http://downloads9.mamp.info/MAMP-PRO/releases/3.0.1/MAMP_MAMP_PRO_3.0.1.pkg'
+  url 'http://downloads8.mamp.info/MAMP-PRO/releases/3.0.2/MAMP_MAMP_PRO_3.0.2.pkg'
   homepage 'http://www.mamp.info/en/index.html'
-  version '3.0.1'
-  sha256 'd43ff397203bef82f80dd237128026f3acc508cea114d560304e3793ca297e0f'
-  install 'MAMP_MAMP_PRO_3.0.1.pkg'
+  version '3.0.2'
+  sha256 '060cc78b6457776a64a864ee69de7e52c95ac2332e906fe4b4afb65826207315'
+  install 'MAMP_MAMP_PRO_3.0.2.pkg'
   after_install do
     system '/usr/bin/sudo', '-E', '--',
            '/usr/sbin/chown', '-R', "#{Etc.getpwuid(Process.euid).name}:staff", '/Applications/MAMP', '/Applications/MAMP PRO'
