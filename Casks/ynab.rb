cb27b241a5f91bc2c862774a9949690d9375970c
Yurii Samsoniuk <mr.sigito@gmail.com>
2014-03-08 23:47:44 +0200

    Update YNAB to new version.

--- Casks/ynab.rb
+++ Casks/ynab.rb
@@ -1,7 +1,7 @@
 class Ynab < Cask
-  url 'http://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.351.dmg'
+  url 'http://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.451.dmg'
   homepage 'http://www.youneedabudget.com/'
-  version '4.3.351'
-  sha256 '37cecbef4cfee46684856ac8455759ce9d2bb063c077eac3602940c46a96b949'
+  version '4.3.451'
+  sha256 '088f61e0e772193f6dac61e22067b1696246f214d7c9887f95d180cfabb57e25'
   link 'YNAB 4.app'
 end
