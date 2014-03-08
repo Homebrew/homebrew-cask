3f9a471fc3d13655712afcec35564d2c58ce6fc5
Paul Repin <paul.repin@gmail.com>
2014-03-08 16:40:51 +0400

    Add iBetterCharge.app v.1.0.2

---
+++ Casks/ibettercharge.rb
@@ -0,0 +1,7 @@
+class Ibettercharge < Cask
+  url 'http://softorino.s3.amazonaws.com/ibettercharge/ibettercharge_1.0.2.zip'
+  homepage 'http://softorino.com/ibettercharge/'
+  version '1.0.2'
+  sha256 '652722f493bf019529b6cf4b72274fcab032d22981ea929ce0fb402d9b13705d'
+  link 'iBetterCharge.app'
+end
