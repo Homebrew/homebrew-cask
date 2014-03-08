d3b0d75afa8fe853d4fa12f6a820c7cecd7106fb
Roland Walker <walker@pobox.com>
2014-03-08 17:13:36 -0500

    codekit.rb whitespace + sha256

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -1,7 +1,7 @@
 class Codekit < Cask
-	url 'http://incident57.com/codekit/files/codekit-16828.zip'
-	homepage 'http://incident57.com/codekit/'
-	version '2.0 (16828)'
-	sha1 '95642edbb7e9e79d537c18d5bac7339520c25d02'
-	link 'CodeKit.app'
+  url 'http://incident57.com/codekit/files/codekit-16828.zip'
+  homepage 'http://incident57.com/codekit/'
+  version '2.0 (16828)'
+  sha256 '631080f34b13504fca3e840272cde1fb4115d78549d1238e769c80e7cb431371'
+  link 'CodeKit.app'
 end
