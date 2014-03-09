e3943f5435a4406add0fbbfc22a5c3a775d9bfac
Alexey Zakhlestin <indeyets@gmail.com>
2014-03-09 17:27:31 +0400

    Rocks'n'Diamonds 3.3.1.2

---
+++ Casks/rocks-n-diamonds.rb
@@ -0,0 +1,7 @@
+class RocksNDiamonds < Cask
+  url 'http://www.artsoft.org/RELEASES/macosx/rocksndiamonds/rocksndiamonds-3.3.1.2.dmg'
+  homepage 'http://www.artsoft.org/rocksndiamonds/'
+  version '3.3.1.2'
+  sha256 '5405786ac296e4052e25f26a2a200a01d49e9c0ae77070a9b94ecbce893ae543'
+  link "Rocks'n'Diamonds 3.3.1.2/Rocks'n'Diamonds.app"
+end
