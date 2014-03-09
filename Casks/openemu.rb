71d9a02dd486caa1d1ebca504b006dd259d0e978
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-03-09 12:58:47 -0600

    Update OpenEmu to 1.0.2

--- Casks/openemu.rb
+++ Casks/openemu.rb
@@ -1,7 +1,7 @@
 class Openemu < Cask
-  url 'https://github.com/OpenEmu/OpenEmu/releases/download/v1.0.1/OpenEmu_1.0.1.zip'
+  url 'https://github.com/OpenEmu/OpenEmu/releases/download/v1.0.2/OpenEmu_1.0.2.zip'
   homepage 'http://openemu.org/'
-  version '1.0.1'
-  sha256 '5ffbe6c0af897f4023bf705c9836ca1d699cb65e6972e7f3639e1c600928f0d5'
+  version '1.0.2'
+  sha256 '150e2e560581b1d796fcb1bf644473dd9a917b11ed27757442f3b1f586c8b77c'
   link 'OpenEmu.app'
 end
