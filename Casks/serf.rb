697d29a4a889d20fd64f9c0d75edb84a61b756e1
lalyos <lalyos@yahoo.com>
2014-03-10 21:23:39 +0100

    Add serf binary version 0.4.5

---
+++ Casks/serf.rb
@@ -0,0 +1,7 @@
+class Serf < Cask
+  url 'https://dl.bintray.com/mitchellh/serf/0.4.5_darwin_amd64.zip'
+  homepage 'http://www.serfdom.io/'
+  version '0.4.5'
+  sha1 '92eca23bd25d4482611e3ac639855828ba241103'
+  binary 'serf'
+end
