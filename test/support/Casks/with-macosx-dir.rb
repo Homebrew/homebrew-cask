a8acf3de1a4d8f7adeed97f99742c8aabaa2b01d
Roland Walker <walker@pobox.com>
2014-03-10 11:09:53 -0400

    bugfix: preserve resource forks when unzipping
    Use the unarchiving ability built into the `ditto` utility,
    rather explicitly calling the `unzip` utility.  Fixes
    caskroom/homebrew-fonts#90.

--- test/support/Casks/with-macosx-dir.rb
+++ test/support/Casks/with-macosx-dir.rb
@@ -2,6 +2,6 @@ class WithMacosxDir < TestCask
   url TestHelper.local_binary('MyFancyApp.zip')
   homepage 'http://example.com/MyFancyApp'
   version '1.2.3'
-  sha1 'b0475c34136c8e00ceb4b95c245291d53d5deab3'
-  link 'MyFancyApp.app'
+  sha256 '5633c3a0f2e572cbf021507dec78c50998b398c343232bdfc7e26221d0a5db4d'
+  link 'MyFancyApp/MyFancyApp.app'
 end
