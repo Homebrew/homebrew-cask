4de360f23217346a9a53df86e65df2754a379ecc
Florian Harbecke <florian@harbecke.me>
2014-03-11 18:30:22 +0100

    Added Firefox-DE (Latest)

---
+++ Casks/firefox-de.rb
@@ -0,0 +1,7 @@
+class FirefoxDe < Cask
+  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=de'
+  homepage 'https://www.mozilla.org/de/firefox/'
+  version 'latest'
+  no_checksum
+  link 'Firefox.app'
+end
