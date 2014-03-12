80b134f52a39126808f796bd8c2336df4e20f142
Chris Hasenpflug <github@chris.hasenpflug.net>
2014-03-11 22:35:43 -0500

    Add Citrix Receiver

---
+++ Casks/citrix-receiver.rb
@@ -0,0 +1,13 @@
+class CitrixReceiver < Cask
+  url 'http://downloadplugins.citrix.com.edgesuite.net/Mac/CitrixReceiverWeb.dmg'
+  homepage 'http://www.citrix.com/receiver'
+  version 'latest'
+  no_checksum
+  install 'Install Citrix Receiver.pkg'
+  uninstall :launchctl => [
+                           'com.citrix.AuthManager_Mac',
+                           'com.citrix.ReceiverHelper',
+                           'com.citrix.ServiceRecords',
+                          ],
+            :pkgutil   => 'com.citrix.ICAClient'
+end
