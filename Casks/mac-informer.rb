9031f2f3bae1c098b4b87270d8e973752aa1fd85
Caleb Xu <caleb@caleb.local>
2014-03-11 22:29:57 -0400

    Added Mac Informer
    Mac Informer has all the updates for your apps in one place, even for
    those that don't come from the Mac App Store. It automatically detects
    which of the installed apps need updating, fetches the download links,
    and displays them in a clean and simple interface.

---
+++ Casks/mac-informer.rb
@@ -0,0 +1,7 @@
+class MacInformer < Cask
+  url 'http://files.informer.com/simac.dmg'
+  homepage 'http://macdownload.informer.com/landing/'
+  version 'latest'
+  no_checksum
+  link 'Mac Informer.app'
+end
