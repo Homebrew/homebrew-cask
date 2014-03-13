9ac41635e897a71e3a059d76fbed73957919b5f2
BingFoon Lee <bingfoon@gmail.com>
2014-03-13 06:09:04 -0700

    Add InsomniaX.app v2.1.3

---
+++ Casks/insomniax.rb
@@ -0,0 +1,7 @@
+class Insomniax < Cask
+  url 'https://www.macupdate.com/download/22211/InsomniaX-2.1.3.tgz'
+  homepage 'http://semaja2.net/insomniaxinfo'
+  version '2.1.3'
+  sha256 '50f41f5f40bd7a8896139c838b48d07abc0ad28419650865187383d1b9165707'
+  link 'InsomniaX.app'
+end
