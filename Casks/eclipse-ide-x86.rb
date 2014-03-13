58abeb08855b570d7b9bc4ec313c7a7bc71d1d39
Camillus Gerard Cai <cgcai@qxcg.net>
2014-03-14 01:31:47 +0800

    Added Eclipse IDE x86 stable.

---
+++ Casks/eclipse-ide-x86.rb
@@ -0,0 +1,7 @@
+class EclipseIdeX86 < Cask
+  url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-macosx-cocoa.tar.gz'
+  homepage 'http://eclipse.org/'
+  version '4.3.2'
+  sha256 '0a1a5b6924daa1a3bd2482a537fac6f59c177da2265ed13f57cf0e5a5772b903'
+  link 'eclipse/Eclipse.app'
+end
