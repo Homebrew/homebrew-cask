e008f640d50239041790b5094f568ea8d7956468
Fernando Paredes <nano@fdp.io>
2014-03-13 16:40:50 -0700

    Remove Hockeycoach
    Replaced by HockeyApp

--- Casks/hockeycoach.rb
+++
@@ -1,7 +0,0 @@
-class Hockeycoach < Cask
-  url 'http://download.hockeyapp.net/apps/HockeyCoach/HockeyCoach-1.0.2.zip'
-  homepage 'http://hockeyapp.net/releases/hockeycoach/'
-  version '1.0.2'
-  sha256 '423ef6c54dfe21f9fe6a34d6a1daafa86d6bcd9d26c89e0262112b2a9c78c893'
-  link 'HockeyCoach.app'
-end
