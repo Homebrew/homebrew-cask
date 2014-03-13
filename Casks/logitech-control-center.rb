eed5eaa8c2355f1231ee05d83b386e3ccd2a847a
Vítor Galvão <info@vitorgalvao.com>
2014-03-13 03:22:54 +0000

    corrected logitech control center uninstall

--- Casks/logitech-control-center.rb
+++ Casks/logitech-control-center.rb
@@ -4,5 +4,5 @@ class LogitechControlCenter < Cask
   version '3.9.0.60'
   sha256 '1eab6118dc5ad0b3c790b9132b5968050dab0117b07d8f338c471aff00078df1'
   install 'LCC Installer.app/Contents/Resources/Logitech Control Center.mpkg'
-  uninstall 'LCC Installer.app/Contents/Resources/LCC Uninstaller Tool'
+  uninstall :script => 'LCC Installer.app/Contents/Resources/LCC Uninstaller Tool'
 end
