a82b6bd860ce05154bf31a79835bdbbff9791d6f
Leonard van Driel <leonardvandriel@gmail.com>
2014-03-13 08:54:36 +0100

    Update Pusher 0.3.3

--- Casks/pusher.rb
+++ Casks/pusher.rb
@@ -1,7 +1,7 @@
 class Pusher < Cask
-  url 'https://github.com/noodlewerk/NWPusher/releases/download/0.3.2/Pusher.app.zip'
+  url 'https://github.com/noodlewerk/NWPusher/releases/download/0.3.3/Pusher.app.zip'
   homepage 'https://github.com/noodlewerk/NWPusher'
-  version '0.3.2'
-  sha256 'db4959f4b2d80f40b343a94c3a23a341f87351fe57e3e3818fc4b55a62b0cdc5'
+  version '0.3.3'
+  sha256 '7602b7a4050f241a69950d1d79d6e14f84e895f7432c787681db82ceecd29251'
   link 'Pusher.app'
 end
