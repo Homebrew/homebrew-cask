73e019dea77ca746c3668495582ffa972baa0551
Camillus Gerard Cai <cgcai@qxcg.net>
2014-03-14 01:30:48 +0800

    Updated Eclipse IDE to 4.3.2.
    Taking the stable x64 build to be default.

--- Casks/eclipse-ide.rb
+++ Casks/eclipse-ide.rb
@@ -1,7 +1,7 @@
 class EclipseIde < Cask
-  url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/SR1/eclipse-standard-kepler-SR1-macosx-cocoa-x86_64.tar.gz'
-  homepage 'http://eclipse.org'
-  version '4.3.1'
-  sha256 'a491476cf91893e8e0495ae16bafd1352c80103be4bf7d09b087e1047561636a'
+  url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/SR2/eclipse-standard-kepler-SR2-macosx-cocoa-x86_64.tar.gz'
+  homepage 'http://eclipse.org/'
+  version '4.3.2'
+  sha256 '7fd761853ae7f5b280963059fcf8da6cea14c93563a3dfe7cc3491a7a977966e'
   link 'eclipse/Eclipse.app'
 end
