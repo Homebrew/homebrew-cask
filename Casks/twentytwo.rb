1e701eb0bf3e486c596400170ad3dae60fb40c1d
Larry Gordon <larry.gordon@xero.com>
2014-03-13 14:39:36 -0700

    Add TwentyTwo.app latest with `sha256 :no_check`

---
+++ Casks/twentytwo.rb
@@ -0,0 +1,7 @@
+class Twentytwo < Cask
+  url 'https://github.com/marcw/twentytwo/raw/master/dist/TwentyTwo.dmg'
+  homepage 'https://github.com/marcw/twentytwo'
+  version 'latest'
+  sha256 :no_check
+  link 'TwentyTwo.app'
+end
