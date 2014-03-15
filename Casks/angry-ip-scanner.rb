17e3193b63a790018ebac68a42e88f4266a716fd
Dan Kuehling <macprince@gmail.com>
2014-03-15 14:45:01 -0500

    Add Angry IP Scanner v3.2.2

---
+++ Casks/angry-ip-scanner.rb
@@ -0,0 +1,7 @@
+class AngryIpScanner < Cask
+  url 'http://github.com/angryziber/ipscan/releases/download/3.2.2/ipscan-mac-3.2.2.zip'
+  homepage 'http://angryip.org'
+  version '3.2.2'
+  sha256 'a9cb54d1e2377be31945692f6206a98056419b6ca641a3e79eada2a259e22226'
+  link 'Angry IP Scanner.app'
+end
