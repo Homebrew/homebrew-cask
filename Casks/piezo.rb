3aef49b15dde38b1357a322172aceaa32648df67
Caleb Xu <caleb@caleb.local>
2014-03-15 19:33:27 -0400

    Added Piezo
    Piezo makes it a snap to record audio on your Mac. In seconds, you'll
    be recording audio from any application or from audio inputs like
    microphones.

---
+++ Casks/piezo.rb
@@ -0,0 +1,7 @@
+class Piezo < Cask
+  url 'http://neutral.rogueamoeba.com/mirror/files/Piezo.zip'
+  homepage 'http://rogueamoeba.com/piezo/'
+  version 'latest'
+  no_checksum
+  link 'Piezo.app'
+end
