e3c0dd01f1aa0ac3aa59c7ba0b559b7532c60157
Michael Ford <fanquake@users.noreply.github.com>
2014-03-15 14:00:24 +0800

    Asteroid 0.9.7

--- Casks/asteroid.rb
+++ Casks/asteroid.rb
@@ -1,7 +1,7 @@
 class Asteroid < Cask
-  url "http://www.asteroidapp.com/downloader.php?Ver=0.9.6"
+  url "http://www.asteroidapp.com/downloader.php?Ver=0.9.7"
   homepage 'http://www.asteroidapp.com/'
-  version '0.9.6'
-  sha256 'a867b90f998e4192dc40618a9e8ba15b4e0623ddd5213bece4e12f683c5d4412'
+  version '0.9.7'
+  sha256 'a4e5de12093ccf5fee7a0515504d758cbe7c721db36695dd467d60a3ed6a2f16'
   link 'Asteroid.app'
 end
