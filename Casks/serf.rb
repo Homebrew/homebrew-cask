1374931d76cc7134dc6be2df33414a73da3a82ac
lalyos <lalyos@yhoo.com>
2014-03-16 18:23:09 +0100

    update serf 0.5.0

--- Casks/serf.rb
+++ Casks/serf.rb
@@ -1,7 +1,7 @@
 class Serf < Cask
-  url 'https://dl.bintray.com/mitchellh/serf/0.4.5_darwin_amd64.zip'
+  url 'https://dl.bintray.com/mitchellh/serf/0.5.0_darwin_amd64.zip'
   homepage 'http://www.serfdom.io/'
-  version '0.4.5'
-  sha256 '2dec10ebd084aba2eaacb0052a871db0396910dd6fbedc9ed0ed37ee3b397386'
+  version '0.5.0'
+  sha256 'af445fbb4423da533ccee4c7c9bb4e0438339dbe65b3226d93af88065ecda0d0'
   binary 'serf'
 end
