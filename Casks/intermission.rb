6dadef194ff8632efcc8d98f9897cf6ea8640333
Caleb Xu <caleb@caleb.local>
2014-03-16 16:56:39 -0400

    Added Intermission
    Turn back time? Yes! Intermission lets you pause and rewind live audio
    on your computer. Streaming audio will never be the same!
    
    With Intermission, you can jump back and replay something you missed,
    then resume live playback. Use it to pause any audio while you take a
    call, answer the door, or use the restroom. You can even pause
    streaming audio on services like Pandora, iTunes Radio, or Spotify to
    build a buffer, then skip right past the ads and songs you don't want
    to hear!

---
+++ Casks/intermission.rb
@@ -0,0 +1,7 @@
+class Intermission < Cask
+  url 'http://dm.rogueamoeba.com/mirror/files/Intermission.zip'
+  homepage 'http://rogueamoeba.com/intermission/'
+  version 'latest'
+  no_checksum
+  link 'Intermission.app'
+end
