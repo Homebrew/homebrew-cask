4544f673b8fcd937dc91436c50f913a075d6c7d1
Trevor Parker <trevor@trevorparker.com>
2014-03-16 17:17:38 -0400

    Update chromium to 33.0.1750.117

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,7 +1,7 @@
 class Chromium < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_32.0.1700.107.dmg'
+  url 'http://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_33.0.1750.117.dmg'
   homepage 'http://www.freesmug.org/chromium'
-  version '32.0.1700.107'
-  sha256 '30a0cbc55c484a055364b180413715c1c65e6f679e30c231d712069a9e602b15'
+  version '33.0.1750.117'
+  sha256 'cb8eb0654e18c0f7431722a6fc5fa8eec73d0011cb88a023538d98773e512efb'
   link 'Chromium.app'
 end
