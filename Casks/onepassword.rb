5a5088f81d9079f1d71dd2359aae10b9fc191178
Camillus Gerard Cai <cgcai@qxcg.net>
2014-03-16 13:51:29 +0800

    Updated 1Password to 4.2.2.

--- Casks/onepassword.rb
+++ Casks/onepassword.rb
@@ -1,7 +1,7 @@
 class Onepassword < Cask
-  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.2.zip'
+  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.2.2.zip'
   homepage 'https://agilebits.com/onepassword'
-  version '4.2'
-  sha256 '5a65d2a6b2d5eb1315d545c78a2b2040c1899ca4985a85ca8d9da06303df1eed'
+  version '4.2.2'
+  sha256 'f472d12c879cec5620d22ec805e74d881cd6b41f2c254e960ce5d8ecb525dc6f'
   link '1Password 4.app'
 end
