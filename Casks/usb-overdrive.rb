890f29436aa81f7b0adf85440643a264fc155057
Caleb Xu <caleb@caleb.local>
2014-03-16 08:22:27 -0400

    Add uninstall stanza for USB Overdrive

--- Casks/usb-overdrive.rb
+++ Casks/usb-overdrive.rb
@@ -4,4 +4,5 @@ class UsbOverdrive < Cask
   version '3.0.2'
   sha256 'd6bed831521a5ab2b82d7eede7be46953e1c010cc6e02ff11f3dd2532d7b0b13'
   install 'Install USB Overdrive.pkg'
+  uninstall :pkgutil => 'com.usboverdrive.installer'
 end
