ceaaf5617615982b5b0ec032f3c51ad6300a86b8
Larry Gordon <larry.gordon@xero.com>
2014-03-17 10:02:53 -0700

    Renames cask `sublime-text` to `sublime-text2` to match convention for installing multiple versions of Sublime Text (`sublime-text2`, `sublime-text3`)

--- Casks/sublime-text2.rb
+++ Casks/sublime-text2.rb
@@ -1,4 +1,4 @@
-class SublimeText < Cask
+class SublimeText2 < Cask
   url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
   homepage 'http://www.sublimetext.com/2'
   version '2.0.2'
