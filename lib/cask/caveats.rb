266718d047cdd1c2438f47e27c2f37c9b3dc336c
Roland Walker <walker@pobox.com>
2014-03-17 09:56:22 -0400

    don't downcase a Pathname object
    Fixes #3572.

--- lib/cask/caveats.rb
+++ lib/cask/caveats.rb
@@ -61,7 +61,7 @@ class Cask::CaveatsDSL
 
   def files_in_usr_local
     localpath = '/usr/local'
-    if HOMEBREW_PREFIX.downcase.index(localpath) == 0
+    if HOMEBREW_PREFIX.to_s.downcase.index(localpath) == 0
       puts <<-EOS.undent
       Cask #{@cask} installs files under "#{localpath}".  The presence of such
       files can cause warnings when running "brew doctor", which is considered
