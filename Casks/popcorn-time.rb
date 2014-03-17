d540b2faeaf3aba1d9c78765ac4bcb78b585db23
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-03-16 22:21:50 -0400

    Add Popcorn-Time.app 0.2.7

---
+++ Casks/popcorn-time.rb
@@ -0,0 +1,7 @@
+class PopcornTime < Cask
+  url 'http://static.cdnjd.com/releases/popcorn/Popcorn-Time-2.7-Mac.tgz'
+  homepage 'https://popcorn.yts.re/'
+  version '0.2.7'
+  sha256 '51a35b1951ba7c6f4adfc963e6b85ffbed82c5fa6cdc101d64c8d4ec87cd0f39'
+  link 'Popcorn-Time.app'
+end
