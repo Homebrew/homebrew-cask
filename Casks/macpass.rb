3d1c3d6f941013a197072d22dbf5940439742661
phpwutz <phpwutz@gmail.com>
2014-03-17 21:22:47 +0100

    update macpass to 0.4

--- Casks/macpass.rb
+++ Casks/macpass.rb
@@ -1,7 +1,7 @@
 class Macpass < Cask
-  url 'https://github.com/mstarke/MacPass/releases/download/0.3.8-alpha/MacPass-0.3.8-alpha.zip'
+  url 'https://github.com/mstarke/MacPass/releases/download/0.4-alpha/MacPass-0.4-alpha.zip'
   homepage 'http://mstarke.github.io/MacPass/'
   version '0.3.8-alpha'
-  sha256 'b3b084465bf3e27c83cae3f36b3bea676ad37165200e225d2f7d0f23c9fae159'
+  sha256 '27c486f9a2a965da4a9916c6d0e000dfe16445bef1d5175934e527ef92254eed'
   link 'MacPass/MacPass.app'
 end
