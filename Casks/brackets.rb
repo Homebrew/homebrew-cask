88a0924aed3ea59275b097ce69261ac7fd8d20e9
ndr <ndr@qef.io>
2014-03-18 14:33:36 +0100

    Use proper version number in Brackets cask.

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,7 +1,7 @@
 class Brackets < Cask
   url 'http://download.brackets.io/file.cfm?platform=OSX&build=37'
   homepage 'http://brackets.io'
-  version 'Sprint 37'
+  version '0.37.0-12014'
   sha256 '3f9fbbcfa12015b30656d83c5b55f0c11f98a298499d6b9b2fd7c2a92bf38f99'
   link 'Brackets.app'
 end
