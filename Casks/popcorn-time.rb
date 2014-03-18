779de762e6c6185ef8d9b0bde500eea3059377ab
Tamir Duberstein <tamird@squareup.com>
2014-03-18 16:15:39 -0700

    Add PopcornTime beta2.7

---
+++ Casks/popcorn-time.rb
@@ -0,0 +1,7 @@
+class PopcornTime < Cask
+  url 'http://popcorn.cdnjd.com/releases/Popcorn-Time-2.7-Mac.tgz'
+  homepage 'http://popcorn.cdnjd.com/'
+  version 'beta2.7'
+  sha256 'ae0b6bd37e4caded13b3f6ab693530f4510ed1d7246f0afde12a98e415b89305'
+  link 'Popcorn-Time.app'
+end
