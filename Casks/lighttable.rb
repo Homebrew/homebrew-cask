5757abe8e6a5722907fb77524292e9a3cc0c5e47
ndr <ndr@qef.io>
2014-03-19 01:19:28 +0100

    Update Light Table to 0.6.5

--- Casks/lighttable.rb
+++ Casks/lighttable.rb
@@ -1,7 +1,7 @@
 class Lighttable < Cask
-  url 'http://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.4/LightTableMac.zip'
+  url 'http://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.5/LightTableMac.zip'
   homepage 'http://www.lighttable.com/'
-  version '0.6.4'
-  sha256 'b6a925c50cb2dfe8de9b215456763145d8d7c6432cb013e39e0fb6432fb8dda7'
+  version '0.6.5'
+  sha256 '05b816605b901e63c47e5add543a21b59c811ac424b00fc3f3bcc3145180be77'
   link 'LightTable/LightTable.app'
 end
