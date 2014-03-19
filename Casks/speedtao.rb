177bfe209045f2da1d5e4e71059ccef65f4bccc4
Jiangge Zhang <tonyseek@gmail.com>
2014-03-19 11:37:59 +0800

    Fix checksum in SpeedTao.app Cask

--- Casks/speedtao.rb
+++ Casks/speedtao.rb
@@ -2,6 +2,6 @@ class Speedtao < Cask
   url 'http://www.speedtao.net/download/SpeedTao_Beta26.dmg'
   homepage 'http://www.speedtao.net/'
   version 'Beta26'
-  sha256 'c8864775c92f2a92914cdba6b70d67998c484fa3f798715456577cf1704748f3'
+  sha256 '2a1c6a742a4a1e52c2459c0958e3bc2f93f80f4f6977ee35c674b06399502058'
   link 'SpeedTao.app'
 end
