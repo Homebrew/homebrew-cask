b10d893153589f4bce8ff09dd327d4d06cbb1133
Timo Sand <timo.j.sand@gmail.com>
2014-03-20 23:21:32 +0200

    Fixed homepage url of spotify-menubar

--- Casks/spotify-menubar.rb
+++ Casks/spotify-menubar.rb
@@ -1,6 +1,6 @@
 class SpotifyMenubar < Cask
   url 'http://lifeupnorth.co.uk/files/Spotify_Menubar.zip'
-  homepage 'http://lifeupnorth.co.uk/spotifymenubar/'
+  homepage 'http://lifeupnorth.co.uk/Spotify-Menubar/'
   version 'latest'
   no_checksum
   link 'Spotify Menubar.app'
