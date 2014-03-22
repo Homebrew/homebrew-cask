8db8d5238f3b1febf82efe75b6220bc021b7e044
George Song <george@55minutes.com>
2014-03-22 12:03:53 -0700

    Added formula for dispcalGUI

---
+++ Casks/dispcalgui.rb
@@ -0,0 +1,7 @@
+class Dispcalgui < Cask
+  url 'http://sourceforge.net/projects/dispcalgui/files/release/1.7.5.7/dispcalGUI-1.7.5.7.dmg'
+  homepage 'http://dispcalgui.hoech.net'
+  version '1.7.5.7'
+  sha256 'a615cb4f6ccf934c0045c8035237ab39bfb0e0a70afec947e6dcc2f2e9bbf163'
+  link 'dispcalGUI.app'
+end
