e5a2730c148471b5972e1474f753f42579a20e74
Tolbkni Kao <tolbkni@gmail.com>
2014-03-23 01:09:29 +0800

    Upgrade Thunder.app to v2.5.0.1408_Beta

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -1,7 +1,7 @@
 class Thunder < Cask
-  url 'http://down.sandai.net/mac/thunder_dl2.4.0.1356_Beta.dmg'
+  url 'http://down.sandai.net/mac/thunder_dl2.5.0.1408_Beta.dmg'
   homepage 'http://mac.xunlei.com/'
-  version '2.4.0.1356_Beta'
-  sha256 '16e401c19e1131871a42646406fa75313c1611e85843074eb170f5c5d6382195'
+  version '2.5.0.1408_Beta'
+  sha256 '2ccb65cc18a5407eed7347cf3120d8b3512f008a64b6ceeda668c79299589940'
   link 'Thunder.app'
 end
