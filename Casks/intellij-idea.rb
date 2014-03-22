b012c68de0be1dcc846bef60f4f01aabba3f27d9
Beng Hee Eu <beng@benghee.eu>
2014-03-22 13:34:58 +0800

    IntelliJ IDEA 13.1.1

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,7 +1,7 @@
 class IntellijIdea < Cask
-  url 'http://download-cf.jetbrains.com/idea/ideaIU-13.1.dmg'
+  url 'http://download-cf.jetbrains.com/idea/ideaIU-13.1.1.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '13.1'
-  sha256 'e5fe85c2c1beb2e5bc1b133885b482730914669a060e5d79559b44ddcd5aaa77'
+  version '13.1.1'
+  sha256 'cf9501cf9992328bc3f5a6e2b6e9dee2995fe88d7a3c3ab7894f816f2aa77734'
   link 'IntelliJ IDEA 13.app'
 end
