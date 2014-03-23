cc46e7f39de4ffabbfb83f6a7aaa7f202f63a70e
George Song <george@55minutes.com>
2014-03-22 23:15:19 -0700

    Use the URL from dispcalgui's website

--- Casks/dispcalgui.rb
+++ Casks/dispcalgui.rb
@@ -1,7 +1,7 @@
 class Dispcalgui < Cask
-  url 'http://sourceforge.net/projects/dispcalgui/files/release/1.7.5.7/dispcalGUI-1.7.5.7.dmg'
+  url 'http://dispcalgui.hoech.net/download/dispcalGUI.dmg'
   homepage 'http://dispcalgui.hoech.net'
-  version '1.7.5.7'
-  sha256 'a615cb4f6ccf934c0045c8035237ab39bfb0e0a70afec947e6dcc2f2e9bbf163'
+  version 'latest'
+  no_checksum
   link 'dispcalGUI.app'
 end
