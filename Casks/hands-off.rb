1f7e55ae1548961e485bd5eaab68a41eca043044
Michael Ford <fanquake@users.noreply.github.com>
2014-03-23 12:05:20 +0800

    Hands Off! 2.1.2

--- Casks/hands-off.rb
+++ Casks/hands-off.rb
@@ -1,7 +1,7 @@
 class HandsOff < Cask
-  url 'https://www.oneperiodic.com/files/Hands%20Off!%20v2.1.1.dmg'
+  url 'https://www.oneperiodic.com/files/Hands%20Off!%20v2.1.2.dmg'
   homepage 'http://www.metakine.com/products/handsoff/'
-  version '2.1.1'
-  sha256 '9988cc959bc4803e6ae2ccb8a5d7b62929f692003f96f0ab3ca4c9dd31403d53'
+  version '2.1.2'
+  sha256 '3dc5a3239560a95ca356641c6cd73dec7a605ecaec52904e2b2a7a1c029357f6'
   link 'Hands Off!.app'
 end
