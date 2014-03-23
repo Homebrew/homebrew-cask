862a0e5657f205a9ae091061a7b844497524cef9
David Dinh <david@dinh.io>
2014-03-22 19:12:52 -0700

    Add MultiDoge.app 0.1.2

---
+++ Casks/multidoge.rb
@@ -0,0 +1,7 @@
+class Multidoge < Cask
+  url 'https://github.com/langerhans/multidoge/releases/download/v0.1.2/multidoge-0.1.2.dmg'
+  homepage 'http://multidoge.org/'
+  version '0.1.2'
+  sha256 'e6639dac77df7a6ea34ce086d68dedda98d8537bd31d32b17598243e26122bcc'
+  link 'MultiDoge.app'
+end
