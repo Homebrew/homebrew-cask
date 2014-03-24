f375af8198fdef7de5d585eafdcc24b99bdb9eee
Cameron Johnston <cameron@rootdown.net>
2014-03-23 18:10:21 -0700

    Upgrade Fluid to 1.8.2

--- Casks/fluid.rb
+++ Casks/fluid.rb
@@ -1,7 +1,7 @@
 class Fluid < Cask
-  url 'http://www.fluidapp.com/dist/Fluid_1.7.2.zip'
+  url 'http://www.fluidapp.com/dist/Fluid_1.8.2.zip'
   homepage 'http://fluidapp.com/'
-  version '1.7.2'
-  sha256 'd2d83e1d888593a810f42fabe3bb37e70022ad2cc8d6be6c8cc3ee8fb1055e19'
+  version '1.8.2'
+  sha256 '9f5d58e4049a0bfcca9f291fc097e0cec1f5df34986e9f9c0c1b385dc2a3cec8'
   link 'Fluid.app'
 end
