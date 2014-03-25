8e4c94485ddc33271982d391ab8ea83a334f8e0f
Vítor Galvão <info@vitorgalvao.com>
2014-03-25 19:28:28 +0000

    readded amadeus-pro

---
+++ Casks/amadeus-pro.rb
@@ -0,0 +1,7 @@
+class AmadeusPro < Cask
+  url 'http://s3.amazonaws.com/AmadeusPro2/AmadeusPro.dmg'
+  homepage 'http://www.hairersoft.com/pro.html'
+  version 'latest'
+  no_checksum
+  link 'Amadeus Pro.app'
+end
