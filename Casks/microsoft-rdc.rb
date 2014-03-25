e9e6fbe868f4d3cd1d4cdfc459a26c1b15ebb958
Drew Gross <drew.a.gross@gmail.com>
2014-03-25 01:30:31 -0400

    Add Microsoft Remote Desktop Connection Client

---
+++ Casks/microsoft-rdc.rb
@@ -0,0 +1,7 @@
+class MicrosoftRdc < Cask
+  url 'http://download.microsoft.com/download/C/F/0/CF0AE39A-3307-4D39-9D50-58E699C91B2F/RDC_2.1.1_ALL.dmg'
+  homepage 'http://www.microsoft.com/en-us/download/details.aspx?id=18140'
+  version '2.1.1'
+  sha256 '4ebe551c9ee0e2da6b8f746be13c2df342c6f14cd3fbedbf2ab490f09b44616f'
+  install 'RDC Installer.mpkg'
+end
