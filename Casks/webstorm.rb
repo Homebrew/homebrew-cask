f894023ba4a9ae25f2a8ccf1ae8a82ce60a455b7
Nils Winkler <nils.winkler@sungard.com>
2014-03-26 16:31:59 +0100

    Upgrade WebStorm.app to v8.0
    Updated WebStorm cask to version 8.0

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -1,7 +1,7 @@
 class Webstorm < Cask
-  url 'http://download.jetbrains.com/webstorm/WebStorm-7.0.3.dmg'
+  url 'http://download.jetbrains.com/webstorm/WebStorm-8.0.dmg'
   homepage 'http://www.jetbrains.com/webstorm/'
-  version '7.0.3'
-  sha256 'b0bd03278da91bf71fec29f12dc4da15d587449cb39b03e4deb94dababc67d87'
+  version '8.0.0'
+  sha256 '39535d8d8ed4c6b61d28ddfe03775d3a0ec435df6520f0ea17831d6bbf9f8694'
   link 'WebStorm.app'
 end
