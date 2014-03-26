0e7bfd981874a020df40f37a8fd03c53e4a2ef5a
Florian Harbecke <florian@harbecke.me>
2014-03-26 17:50:52 +0100

    Added Messenger-for-Telegram.app (Latest)
    Previously managed in caskroom-versions, no version-less :)

---
+++ Casks/messenger-for-telegram.rb
@@ -0,0 +1,7 @@
+class MessengerForTelegram < Cask
+  url 'https://rink.hockeyapp.net/api/2/apps/c55f5e74ae5d0ad254df29f71a1b5f0e/?format=zip'
+  homepage 'https://vk.com/telegram_osx'
+  version 'latest'
+  no_checksum
+  link 'Messenger for Telegram.app'
+end
