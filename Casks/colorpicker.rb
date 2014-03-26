1f598587038b5275d5c026013e287fc9a40f472c
Dimitrios Mistriotis <dimitrismistriotis@gmail.com>
2014-03-26 07:55:22 +0000

    Colorpicker from NorthernSpy
    Running as a standalone application provides a handy way to pick colors from open windows.

---
+++ Casks/colorpicker.rb
@@ -0,0 +1,7 @@
+class Colorpicker < Cask
+  url 'http://www.northernspysoftware.com/download/ColorPicker_1.6.1.dmg'
+  homepage 'http://www.northernspysoftware.com/software/colorpicker'
+  version '1.6.1'
+  sha256 '23f2e782e20ff172b06746803ad2879ab5e7208dc04818a1716c18249d1e360d'
+  link 'ColorPicker.app'
+end
