af3eae9d47b0c1fd80ee987f246ed9f46d280525
Edwin Kwok <edwinkcw@gmail.com>
2014-03-26 10:40:56 +0800

    update Colloquy to 2.4.3

--- Casks/colloquy.rb
+++ Casks/colloquy.rb
@@ -1,7 +1,7 @@
 class Colloquy < Cask
-  url 'http://colloquy.info/downloads/colloquy-2.4.2.zip'
+  url 'http://colloquy.info/downloads/colloquy-2.4.3.zip'
   homepage 'http://colloquy.info/'
-  version '2.4.2'
-  sha256 'cdfca1c34932397ffd633f318aa815c9f7354b0bb86d2db1e2917c76c6e27d27'
+  version '2.4.3'
+  sha256 'b1c54047b9a4aed313e9a4a86073aa5b1215cb8a4ea462c59e7a52d8be6b1a93'
   link 'Colloquy.app'
 end
