cdfc66d4a651f8f27d4250e77265f38ea709a9f4
Jade Lau <jadeyslau@gmail.com>
2014-03-27 10:14:09 +0000

    Update Virtualbox cask: sha256 and version

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,8 +1,8 @@
 class Virtualbox < Cask
   url 'http://download.virtualbox.org/virtualbox/4.3.10/VirtualBox-4.3.10-93012-OSX.dmg'
   homepage 'http://www.virtualbox.org'
-  version '4.3.10-92995'
-  sha256 '531b011ce1c19a44d56d76a11c2d9300b3949f8840ee113998607a7626bdd754'
+  version '4.3.10-93012'
+  sha256 '8bf24a7afbde0cdb560b40abd8ab69584621ca6de59026553f007a0da7b4d443'
   install 'VirtualBox.pkg'
   uninstall :script => { :executable => 'VirtualBox_Uninstall.tool', :args => %w[--unattended] }
 end
