145c8964fd7365769af0a52c6800c2bb83325592
Yurii Samsoniuk <yurii.samsoniuk@gmail.com>
2014-03-27 22:21:53 +0200

    Google Music player renamed to Radiant Player. Updated to version 1.1.2.

---
+++ Casks/radiant-player.rb
@@ -0,0 +1,7 @@
+class RadiantPlayer < Cask
+  url 'https://github.com/kbhomes/google-music-mac/releases/download/v1.1.2/Radiant.Player.zip'
+  homepage 'http://kbhomes.github.io/google-music-mac/'
+  version '1.1.2'
+  sha256 '139e742af85e0ceade606b35478c26d1a01b43973587db5fe54eddaa2641957e'
+  link 'Radiant Player.app'
+end
