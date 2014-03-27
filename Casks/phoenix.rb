5951fb0c8938971f0942a3d161255b62bf6d1fb8
Matthew Michihara <matthew.michihara@gmail.com>
2014-03-26 21:02:02 -0700

    Add Phoenix

---
+++ Casks/phoenix.rb
@@ -0,0 +1,7 @@
+class Phoenix < Cask
+  url 'https://raw.github.com/sdegutis/phoenix/master/Builds/Phoenix-LATEST.app.tar.gz'
+  homepage 'https://github.com/sdegutis/Phoenix'
+  version 'latest'
+  no_checksum
+  link 'Phoenix.app'
+end
