7d616504372631fed4542dc1487f7355f4e92338
Jade Lau <jadeyslau@gmail.com>
2014-03-27 01:23:59 +0000

    Updating URL to latest version

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,5 +1,5 @@
 class Virtualbox < Cask
-  url 'http://download.virtualbox.org/virtualbox/4.3.10/VirtualBox-4.3.10-92995-OSX.dmg'
+  url 'http://download.virtualbox.org/virtualbox/4.3.10/VirtualBox-4.3.10-93012-OSX.dmg'
   homepage 'http://www.virtualbox.org'
   version '4.3.10-92995'
   sha256 '531b011ce1c19a44d56d76a11c2d9300b3949f8840ee113998607a7626bdd754'
