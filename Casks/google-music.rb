d6cd4d9c85b22b927ca6f63818839d8eee6fea59
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-03-27 16:53:13 -0400

    Rename Google Music to Radiant Player

--- Casks/google-music.rb
+++
@@ -1,7 +0,0 @@
-class GoogleMusic < Cask
-  url 'https://github.com/kbhomes/google-music-mac/releases/download/v1.1.1/Google.Music.zip'
-  homepage 'http://kbhomes.github.io/google-music-mac/'
-  version '1.1.1'
-  sha256 '62f38da3adad507e35ff6aaff01feecb065bab2ba9f59e9684c7c5971fcbdf9c'
-  link 'Google Music.app'
-end
