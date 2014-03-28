217d0859f32b4c2f15ebfed47a4e0d4d244f5ee3
Lucas Garron <code@garron.net>
2014-03-28 15:29:24 -0700

    Add `Nothing To Hide` (game).

---
+++ Casks/nothing-to-hide.rb
@@ -0,0 +1,7 @@
+class NothingToHide < Cask
+  url 'http://nothingtohide.s3.amazonaws.com/pc/Nothing_To_Hide_MAC.zip'
+  homepage 'https://back.nothingtohide.cc/'
+  version 'latest'
+  no_checksum
+  link 'Nothing To Hide.app'
+end
