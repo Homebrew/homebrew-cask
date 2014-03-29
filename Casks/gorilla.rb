e16e09c39285fcc1048818c8d6adfbcc4904272c
Han Su Kim <hkim823@gmail.com>
2014-03-29 19:55:03 -0400

    adding gorilla

---
+++ Casks/gorilla.rb
@@ -0,0 +1,8 @@
+class Gorilla < Cask
+  url 'http://zdia.de/downloads/gorilla/gorilla15372.zip'
+  homepage 'https://github.com/zdia/gorilla'
+  version '1.5.3.7.2'
+  sha256 '4d026a5fa088fe9622cbe00c5811da2f360949c7cfb46ce6e4899a09645cb83a'
+  link 'Password Gorilla.app'
+  caveat 'Must be used in 32-bit mode, Must turn off Gatekeeper for first launch, Gatekeeper can be reneabled afterwards'
+end
