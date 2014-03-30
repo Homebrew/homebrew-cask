1fc0e038ada76c8c6e8c0c420e53bc7e24fd253f
David Smith <david.smith@wirewool.com>
2014-03-30 20:30:15 +0100

    Initial code to add BBC iPlayer Downloads (version 1.4.1)

---
+++ Casks/bbc-iplayer-downloads.rb
@@ -0,0 +1,7 @@
+class BbcIplayerDownloads < Cask
+  url 'http://www.bbc.co.uk/iplayer/dm/downloads/mac/latest'
+  homepage 'http://www.bbc.co.uk/iplayer/install'
+  version 'latest'
+  no_checksum
+  link 'BBC iPlayer Downloads.app'
+end
