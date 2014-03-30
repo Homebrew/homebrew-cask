f6ae874ea04109e12db97b54378473d3b6be317d
Han Su Kim <hkim823@gmail.com>
2014-03-29 22:05:14 -0400

    Add Firefox ESR
    Firefox-ESR is for those who need to stay on a version longer term.
    For more information see
    http://www.mozilla.org/en-US/firefox/organizations/faq

---
+++ Casks/firefox-esr.rb
@@ -0,0 +1,7 @@
+class Firefox-ESR < Cask
+  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US'
+  homepage 'http://www.mozilla.org/en-US/firefox/organizations/faq/'
+  version '24.4.0'
+  no_checksum
+  link 'Firefox.app'
+end
