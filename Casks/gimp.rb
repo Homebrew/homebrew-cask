10ee627d32ab11141db3eb729e8c4fca58df7525
Vítor Galvão <info@vitorgalvao.com>
2014-03-31 18:23:09 +0100

    Fixed gimp download link
    As per referenced in #3737

--- Casks/gimp.rb
+++ Casks/gimp.rb
@@ -1,5 +1,5 @@
 class Gimp < Cask
-  url 'ftp://ftp.gimp.org/pub/gimp/v2.8/osx/gimp-2.8.10-dmg-1.dmg'
+  url 'http://ftp.gimp.org/pub/gimp/v2.8/osx/gimp-2.8.10-dmg-1.dmg'
   homepage 'http://www.gimp.org'
   version '2.8.10'
   sha256 'e93a84cd5eff4fe1c987c9c358f9de5c3532ee516bce3cd5206c073048cddba5'
