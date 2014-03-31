36ae14c1274f40a282014c7d271643e4ef148af6
David Smith <david.smith@wirewool.com>
2014-03-31 10:15:18 +0100

    Updated BBC iPlyaer Downloads to use version 'latest'
    Changes made for pull request (#3727) feedback.  Removed sha256
    information and switch to use version latest

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -1,7 +1,7 @@
 class BbcIplayerDownloads < Cask
   url 'http://www.bbc.co.uk/iplayer/dm/downloads/mac/latest'
   homepage 'http://www.bbc.co.uk/iplayer/install'
-  version '1.4.1'
-  sha256 'b48b84090e25dfa996cb332b9d4fc92545aa457579ee4ed923bbeacf163c7094'
+  version 'latest'
+  no_checksum
   link 'BBC iPlayer Downloads.app'
 end
