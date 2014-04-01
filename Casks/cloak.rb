cd14894f0d203bbee0069b9efb23b87beea9251f
Dan Kuehling <macprince@gmail.com>
2014-04-01 17:56:25 -0500

    Update Cloak to v2.0.1

--- Casks/cloak.rb
+++ Casks/cloak.rb
@@ -1,7 +1,7 @@
 class Cloak < Cask
-  url 'https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-1.2.3.dmg'
+  url 'https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-2.0.1.dmg'
   homepage 'https://www.getcloak.com'
-  version '1.2.3'
-  sha256 'e5a21f24c116045554144ff5149506b6edfc46e96f97083043a3cc1b4e14f74b'
+  version '2.0.1'
+  sha256 'c4037ec78e92fadc34443037ebdfcbbb1902737414f81f1c364c1812ed5c61cb'
   link 'Cloak.app'
 end
