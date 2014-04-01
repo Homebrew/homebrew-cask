ba1f5b0a2c10894b7227d9d230596995ebc3d0aa
Will Kruse <wskruse@gmail.com>
2014-04-01 10:13:33 -0400

    Added CocoaSplit.app
    CocoaSplit is an open source project for streaming video to web sources.
    CocaSplit is not currently versioned, but does have a current release available.
    Source code can be found at https://github.com/zakk4223/CocoaSplit
    Binary downloads and update information at http://krylon.rsdio.com/zakk/cocoasplit/

---
+++ Casks/cocoasplit.rb
@@ -0,0 +1,7 @@
+class Cocoasplit < Cask
+  url 'http://krylon.rsdio.com/zakk/cocoasplit/CocoaSplit-current.zip'
+  homepage 'http://krylon.rsdio.com/zakk/cocoasplit/'
+  version 'latest'
+  no_checksum
+  link 'CocoaSplit.app'
+end
