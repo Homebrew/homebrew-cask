69a3d2b46ce40ec862736474d8b061785562d82d
James Ottaway <git@james.ottaway.io>
2014-04-02 10:02:59 +1000

    Bump Xamarin Studio to v4.2.3

--- Casks/xamarin-studio.rb
+++ Casks/xamarin-studio.rb
@@ -1,7 +1,7 @@
 class XamarinStudio < Cask
-  url 'http://download.xamarin.com/studio/Mac/XamarinStudio-4.2-2.dmg'
+  url 'http://download.xamarin.com/studio/Mac/XamarinStudio-4.2.3.60-60.dmg'
   homepage 'http://xamarin.com/studio'
-  version '4.2.2'
-  sha256 '94dc448e9293635e01c096bb41da0d7a92ed4be6e27f26dd8593fb3ffde32174'
+  version '4.2.3'
+  sha256 'e95bd713b8790990c769dc9cb7ca49eeb52c3393f2f882a91e3fed37b2a20f65'
   link 'Xamarin Studio.app'
 end
