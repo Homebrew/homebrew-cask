8cec129884118fd2e2b6a3e2b83ddefdacb09eea
Dan Kuehling <macprince@gmail.com>
2014-04-01 23:42:35 -0500

    Fixing checksum for CrashPlan v3.6.3

--- Casks/crashplan.rb
+++ Casks/crashplan.rb
@@ -2,7 +2,7 @@ class Crashplan < Cask
   url 'http://download.crashplan.com/installs/mac/install/CrashPlan/CrashPlan_3.6.3_Mac.dmg'
   homepage 'http://www.crashplan.com/'
   version '3.6.3'
-  sha256 '85d49c6742e390a24f0a0931d3c2cbb9ab077a1cd5361d980992ba2908acbd23'
+  sha256 'af6959a5a2e8584195fa7381014ba462ced114763995b50266d6353a95b84a7d'
   install 'Install CrashPlan.pkg'
   uninstall(
     :script => 'Uninstall.app/Contents/Resources/uninstall.sh',
