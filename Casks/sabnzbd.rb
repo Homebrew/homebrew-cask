0959dbddc8e09d67fcfaf1e8e361bce7bf0abf20
Alexander Ruppel <ruppelalexander@gmail.com>
2014-04-04 00:37:07 +0200

    Upgrade SABnzbd+ to 0.7.17

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -1,7 +1,7 @@
 class Sabnzbd < Cask
-  url 'http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/0.7.16/SABnzbd-0.7.16-osx.dmg'
+  url 'http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/0.7.17/SABnzbd-0.7.17-osx.dmg'
   homepage 'http://sabnzbd.org/'
-  version '0.7.16'
-  sha256 'dd56ccca53b1fdf426661d255a10d938d41111c6f377720d21621dd5626b2117'
-  link 'OS X 10.8 (Mountain Lion)/SABnzbd.app'
+  version '0.7.17'
+  sha256 '31cf257d951afbd0527f13969ba673808594f2b883c1aa8f532296db7b26d5e1'
+  link '10.8(M-Lion) 10.9(Mavericks)/SABnzbd.app'
 end
