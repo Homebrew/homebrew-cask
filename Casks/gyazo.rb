21411351437d4e81d78c17d4fc19a62c83b709a7
Chulki Lee <chulki.lee@gmail.com>
2014-04-03 12:22:30 -0700

    Use s3 url for gyazo

--- Casks/gyazo.rb
+++ Casks/gyazo.rb
@@ -1,5 +1,5 @@
 class Gyazo < Cask
-  url 'https://files.gyazo.com/setup/Gyazo_2.0.dmg'
+  url 'https://gyazo.s3.amazonaws.com/setup/Gyazo_2.0.dmg'
   homepage 'https://gyazo.com/'
   version '2.0'
   sha256 '41242a421ee0c2b467ac0ff7fa1baa895c824e940bc5aee92a4ec2bf8e204eb3'
