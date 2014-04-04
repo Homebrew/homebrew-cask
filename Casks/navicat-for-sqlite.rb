3934333fcc92aaedb6256509b7cfe548615305a5
Vítor Galvão <info@vitorgalvao.com>
2014-04-04 05:04:59 +0100

    updated navicat-for-sqlite.rb

--- Casks/navicat-for-sqlite.rb
+++ Casks/navicat-for-sqlite.rb
@@ -1,7 +1,7 @@
 class NavicatForSqlite < Cask
   url 'http://download.navicat.com/download/navicat110_sqlite_en.dmg'
   homepage 'http://www.navicat.com/products/navicat-for-sqlite'
-  version '11.0.15'
-  sha256 '807adaad112ea07fbbc693b72a0a9066d6e527a90425f962e0f79d22563b96dc'
+  version '11.0.16'
+  sha256 '583ec1190deaf83a3c3e0d6e6199d80a9089d7b04f6241d88bf5b70871e7a5e6'
   link 'Navicat for SQLite.app'
 end
