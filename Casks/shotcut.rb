43bb05b307d548bfa54f680ccbc46b179b0380e6
Vítor Galvão <info@vitorgalvao.com>
2014-04-04 05:13:32 +0100

    updated shotcut.rb

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,7 +1,7 @@
 class Shotcut < Cask
-  url 'http://d1av856udzjaks.cloudfront.net/shotcut/shotcut-osx-x86_64-140317.dmg'
+  url 'http://d1av856udzjaks.cloudfront.net/shotcut/shotcut-osx-x86_64-140402.dmg'
   homepage 'http://www.shotcut.org/'
-  version '140317'
-  sha256 'dab4facbd9498a019b45be189a9c1e4bc4bb837e4c16837388dc808cd1fd20bf'
+  version '140402'
+  sha256 '9006b4534dc228a77fb2813da4d533134a40197c2044c8119ae078e0cb546555'
   link 'Shotcut.app'
 end
