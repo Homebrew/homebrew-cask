2e0f04f218303237e53f7dd166c92ea63fef8fb4
Vítor Galvão <info@vitorgalvao.com>
2014-04-04 04:57:01 +0100

    updated navicat-for-mariadb.rb

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,7 +1,7 @@
 class NavicatForMariadb < Cask
   url 'http://download.navicat.com/download/navicat110_mariadb_en.dmg'
   homepage 'http://www.navicat.com/products/navicat-for-mariadb'
-  version '11.0.15'
-  sha256 'ae3c3d54452d5a5344ed5758303430e74cad3be021a751adfc71f6e9d441021a'
+  version '11.0.16'
+  sha256 '203c6e56d717052fa1381bd65d85f3f8fea01d86400327589c1097d854760027'
   link 'Navicat for MariaDB.app'
 end
