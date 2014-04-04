a88f4f87bba2b93da77099b694a9ae9146c4c52a
Vítor Galvão <info@vitorgalvao.com>
2014-04-04 05:03:30 +0100

    updated navicat-for-sql-server.rb

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,7 +1,7 @@
 class NavicatForSqlServer < Cask
   url 'http://download.navicat.com/download/navicat110_sqlserver_en.dmg'
   homepage 'http://www.navicat.com/products/navicat-for-sqlserver'
-  version '11.0.15'
-  sha256 'f1fb36b01eb0925f9d8b23c6550467eccd3781b5f8b372bda3a5d47eee4e91cc'
+  version '11.0.16'
+  sha256 'cf7ffa01f235073817365b43558c8ee42a9c7a07091ee5eeecdfe96be0c75860'
   link 'Navicat For SQL Server.app'
 end
