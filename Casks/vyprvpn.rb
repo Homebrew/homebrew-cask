34f440edfea05c3feecae0d2b7e02ede6dea3c07
Vítor Galvão <info@vitorgalvao.com>
2014-04-06 01:39:58 +0100

    updated vyprvpn.rb

--- Casks/vyprvpn.rb
+++ Casks/vyprvpn.rb
@@ -1,7 +1,7 @@
 class Vyprvpn < Cask
-  url 'http://www.goldenfrog.com/downloads/vyprvpn/desktop/VyprVPN-2.1.0.dmg'
+  url 'http://www.goldenfrog.com/downloads/vyprvpn/desktop/VyprVPN-2.3.1.dmg'
   homepage 'http://www.goldenfrog.com/vyprvpn'
-  version '2.1.0'
-  sha256 '29340d1b398898b6f9ffab2966bebb503aeea37ee84a617e3c69c2bf05092083'
+  version '2.3.1'
+  sha256 'cf21a305c23c3bf964c549131c98fea833df5c21c1cb9401f385dc83a854eb90'
   link 'VyprVPN.app'
 end
