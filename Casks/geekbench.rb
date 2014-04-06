4f27d16610b32c876ddf49b4d689c141b5402f90
Beng Hee Eu <beng@benghee.eu>
2014-04-06 22:35:31 +0800

    Geekbench 3.1.5

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -1,7 +1,7 @@
 class Geekbench < Cask
-  url 'http://geekbench.s3.amazonaws.com/Geekbench-3.1.4-Mac.zip'
+  url 'http://geekbench.s3.amazonaws.com/Geekbench-3.1.5-Mac.zip'
   homepage 'http://www.primatelabs.com/geekbench/'
-  version '3.1.4'
-  sha256 '97f8602775b5d8eb49c2ac67fe906ed29363009ba5d53023402de409e9e1ddd6'
+  version '3.1.5'
+  sha256 '720eb6afbed35a21e218df50e02fbda098f1d52c7627b2f22347c525e1d3cef7'
   link 'Geekbench 3.app'
 end
