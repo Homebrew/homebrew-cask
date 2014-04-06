47586da61e3484c3f929c0a4e10f2e25ed2c40ef
Vítor Galvão <info@vitorgalvao.com>
2014-04-06 04:19:41 +0100

    updated foxmail

--- Casks/foxmail.rb
+++ Casks/foxmail.rb
@@ -2,6 +2,6 @@ class Foxmail < Cask
   url 'http://dldir1.qq.com/foxmail/MacFoxmail/Foxmail_for_Mac_V1.2.0.dmg'
   homepage 'http://www.foxmail.com'
   version '1.2.0'
-  sha256 'bd76dcb16b82243a27109963232c26a6d5718c44c8845ccc4a736d6ca0772533'
+  sha256 'b71c373f96f69835ead265ab47957d3f81920025adeea0d37374415d0d03d06c'
   link 'Foxmail.app'
 end
