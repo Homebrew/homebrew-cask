50d38905d326b8993543b8c3bb0fbe5a4d25c91b
Beng Hee Eu <beng@benghee.eu>
2014-04-06 18:35:14 +0800

    Shortcat 0.6.5

--- Casks/shortcat.rb
+++ Casks/shortcat.rb
@@ -1,7 +1,7 @@
 class Shortcat < Cask
-  url 'https://files.shortcatapp.com/v0.6.4/Shortcat.zip'
+  url 'https://files.shortcatapp.com/v0.6.5/Shortcat.zip'
   homepage 'http://shortcatapp.com/'
-  version '0.6.4'
-  sha256 'eab003857313021af9984cd7c85740d702c4766232facb4378200ebc61045421'
+  version '0.6.5'
+  sha256 '58600af9912c3275eb2e85028d7a60f82c75a782cad5ebfe3a03fb3b80270864'
   link 'Shortcat.app'
 end
