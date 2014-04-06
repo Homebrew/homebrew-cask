59ce491759012c95b7c96f4e9dbb0b686393075d
Vítor Galvão <info@vitorgalvao.com>
2014-04-06 04:41:00 +0100

    preferencecleaner now uses latest version

--- Casks/preferencecleaner.rb
+++ Casks/preferencecleaner.rb
@@ -1,7 +1,7 @@
 class Preferencecleaner < Cask
   url 'http://www.echomist.co.uk/software/downloads/PreferenceCleaner.dmg'
   homepage 'http://www.echomist.co.uk/software/PreferenceCleaner.shtml'
-  version '1.5'
-  sha256 'f0fed8d13cdfd24b7b395941eae3ef60f8c0f08484839d2a472712954c50c051'
+  version 'latest'
+  no_checksum
   link 'PreferenceCleaner.app'
 end
