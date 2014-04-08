0cbf98412500829072730940e87b948b1598ba45
Vítor Galvão <info@vitorgalvao.com>
2014-04-08 19:25:38 +0100

    updated refresh-finder

--- Casks/refresh-finder.rb
+++ Casks/refresh-finder.rb
@@ -1,5 +1,5 @@
 class RefreshFinder < Cask
-  url 'https://www.macupdate.com/download/24714/Refresh_Finder_1.5.0.dmg.zip'
+  url 'http://soderhavet.com/refresh/Refresh_Finder_1.5.0.dmg.zip'
   homepage 'http://soderhavet.com/refresh/refresh-finder/'
   version '1.5.0'
   sha256 'd7e5faec85b3910776ca77f09c98b1b90e540cc3d493853e46eaeb8c7332472e'
