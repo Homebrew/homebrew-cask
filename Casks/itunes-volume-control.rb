3c3943c2482b5081139ac56c3fd76db70def703e
Vítor Galvão <info@vitorgalvao.com>
2014-04-08 19:17:48 +0100

    updated itunes-volume-control

--- Casks/itunes-volume-control.rb
+++ Casks/itunes-volume-control.rb
@@ -1,7 +1,7 @@
 class ItunesVolumeControl < Cask
-  url 'https://github.com/alberti42/iTunes-Volume-Control/raw/288c9f3f2c0d00dc76afde2fcc6a7362505113f1/iTunes%20Volume%20Control.dmg'
+  url 'https://github.com/alberti42/iTunes-Volume-Control/blob/master/iTunes%20Volume%20Control.dmg?raw=true'
   homepage 'https://github.com/alberti42/iTunes-Volume-Control'
-  version '1.4.6'
-  sha256 '0ea54d9b5189882074d822fe83c71cd61c953a2a073fb19c166cd1863a78c90c'
+  version 'latest'
+  no_checksum
   link 'iTunes Volume Control.app'
 end
