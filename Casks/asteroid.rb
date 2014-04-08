161ae38d1b7b6274ba5e27f0682ea44ec389e741
Vítor Galvão <info@vitorgalvao.com>
2014-04-08 19:08:43 +0100

    updated asteroid

--- Casks/asteroid.rb
+++ Casks/asteroid.rb
@@ -1,7 +1,7 @@
 class Asteroid < Cask
-  url "http://www.asteroidapp.com/downloader.php?Ver=0.9.7"
+  url "http://www.asteroidapp.com/downloader.php?Ver=latest"
   homepage 'http://www.asteroidapp.com/'
-  version '0.9.7'
-  sha256 'a4e5de12093ccf5fee7a0515504d758cbe7c721db36695dd467d60a3ed6a2f16'
+  version 'latest'
+  no_checksum
   link 'Asteroid.app'
 end
