3088445eb0c637c684593e27fe0f7a6f3c5e8333
Michael Ford <fanquake@users.noreply.github.com>
2014-04-09 20:34:32 +0800

    Tor Browser Bundle 3.5.4

--- Casks/torbrowserbundle.rb
+++ Casks/torbrowserbundle.rb
@@ -1,7 +1,7 @@
 class Torbrowserbundle < Cask
-  url 'https://www.torproject.org/dist/torbrowser/3.5.3/TorBrowserBundle-3.5.3-osx32_en-US.zip'
+  url 'https://www.torproject.org/dist/torbrowser/3.5.4/TorBrowserBundle-3.5.4-osx32_en-US.zip'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  version '3.5.3'
-  sha256 'b321e4752fdd56f4146976be1d5379904503b63a1f36a2600a702910d2feaa55'
+  version '3.5.4'
+  sha256 'e2ffd4173a618097e61e4b5391b832b5cceec38613599b67a3bfb5bafdbdca48'
   link 'TorBrowserBundle_en-US.app'
 end
