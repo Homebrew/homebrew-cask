0c7588fa6180652c00a08f3fc8164a728040df87
Dan Kuehling <macprince@gmail.com>
2014-04-08 21:54:25 -0500

    Changing Scapple cask to no_checksum

---
+++ Casks/scapple.rb
@@ -0,0 +1,7 @@
+class Scapple < Cask
+  url 'http://scrivener.s3.amazonaws.com/Scapple.dmg'
+  homepage 'https://www.literatureandlatte.com/scapple.php'
+  version 'latest'
+  no_checksum
+  link 'Scapple.app'
+end
