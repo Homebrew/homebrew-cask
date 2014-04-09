1efc85b92c18b183dfcc9d022378d47b2778a777
Dan Kuehling <macprince@gmail.com>
2014-04-08 20:58:11 -0500

    Adding Scapple v1.1 from Literature & Latte Ltd

---
+++ Casks/scapple.rb
@@ -0,0 +1,7 @@
+class Scapple < Cask
+  url 'http://scrivener.s3.amazonaws.com/Scapple.dmg'
+  homepage 'https://www.literatureandlatte.com/scapple.php'
+  version '1.1'
+  sha256 'a769d10cbdb91a381818b27506c0c19463aa46edd4183bd8b4fa74a2bd4515d0'
+  link 'Scapple.app'
+end
