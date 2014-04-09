776261d6b7b23f6666ed70272610d3c9813bcdf2
Michael Ford <fanquake@users.noreply.github.com>
2014-04-09 20:40:41 +0800

    Armory 0.91-beta

--- Casks/armory.rb
+++ Casks/armory.rb
@@ -1,7 +1,7 @@
 class Armory < Cask
-  url 'https://s3.amazonaws.com/bitcoinarmory-releases/armory_0.90-beta_OSX.app.tar.gz'
+  url 'https://s3.amazonaws.com/bitcoinarmory-releases/armory_0.91-beta_osx.tar.gz'
   homepage 'https://bitcoinarmory.com/'
-  version '0.90-beta'
-  sha256 '5565f356e04f71d9d7471d79aac8740ebf6827e6ca9a89cbc10ff9f91fe3d26f'
+  version '0.91-beta'
+  sha256 'ff93bb120d5065d5ffd157ca53bc78c1cff3d908d8d0100090c03845bb1c1990'
   link 'Armory.app'
 end
