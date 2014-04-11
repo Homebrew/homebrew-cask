65ef5330331f4ddc3af65174fad6c128dc061b28
Lucas Garron <code@garron.net>
2014-04-11 12:49:50 -0700

    Update bitcoin to 0.9.1 due to Heartbleed vulnerability.
    See https://bitcoin.org/en/alert/2014-04-11-heartbleed

--- Casks/bitcoin.rb
+++ Casks/bitcoin.rb
@@ -1,7 +1,7 @@
 class Bitcoin < Cask
-  url 'https://bitcoin.org/bin/0.9.0/bitcoin-0.9.0-macosx.dmg'
+  url 'https://bitcoin.org/bin/0.9.1/bitcoin-0.9.1-macosx-setup.dmg'
   homepage 'https://bitcoin.org/'
-  version '0.9.0'
-  sha256 '80040488c1c03b69beee43c05da472d1cb2f9052fcfa93b73f094298017304e9'
+  version '0.9.1'
+  sha256 '0390fbf5bf1ef876d95f277319983342c47252bf8369bab45def9d13afcbf4e2'
   link 'Bitcoin-Qt.app'
 end
