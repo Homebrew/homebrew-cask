4378dd30a4535f6f70f4ef5d4457c3693d356d1b
Vítor Galvão <info@vitorgalvao.com>
2014-04-12 21:02:18 +0100

    Remove YANVI
    The download for this *cask* is from mediafire. Similar to the case with
    [FreeRapid](https://github.com/phinze/homebrew-cask/pull/615), having
    the download source be a one click hoster means the download url expires
    and is unreliable.
    
    Will leave this open for a few days, in case someone has an alternative.

--- Casks/yanvi.rb
+++
@@ -1,7 +0,0 @@
-class Yanvi < Cask
-  url 'http://download1167.mediafire.com/2ve2xmcd85ug/iy2s1yp233pzj80/Yet_Another_NFO_Viewer_v1.0.2.dmg'
-  homepage 'https://www.macupdate.com/app/mac/39748/yet-another-nfo-viewer'
-  version '1.0.2'
-  sha256 '7e7db828e404d1594ca847c4ccbb4fe7924dd5d3a116e69ddef94884345749ed'
-  link 'YaNvi.app'
-end
