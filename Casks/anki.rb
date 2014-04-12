556f0420d52defaa1f17d4aabfdbe059992ebfba
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-04-12 22:01:52 +1000

    Updated anki to v2.0.24

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,7 +1,7 @@
 class Anki < Cask
-  url 'http://ankisrs.net/download/mirror/anki-2.0.22.dmg'
+  url 'http://ankisrs.net/download/mirror/anki-2.0.24.dmg'
   homepage 'http://ankisrs.net/'
-  version '2.0.22'
-  sha256 '86c0333d9254bd57972067fdcbcd7c3dc6742a4b4405c3078bb696255a637fd0'
+  version '2.0.24'
+  sha256 'e193c1882322b11139e273500fab55c00da3f8100c5fcf8a04121d05d14e50df'
   link 'Anki.app'
 end
