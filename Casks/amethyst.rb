8ad409c71ddaec8a3bb267e02a931b072a060f43
Ian Ynda-Hummel <ianynda@gmail.com>
2014-04-12 15:12:24 -0400

    Update Amethyst to 0.8.5

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -1,7 +1,7 @@
 class Amethyst < Cask
-  url 'http://ianyh.com/amethyst/versions/Amethyst-0.8.4.zip'
+  url 'http://ianyh.com/amethyst/versions/Amethyst-0.8.5.zip'
   homepage 'http://ianyh.com/amethyst'
-  version '0.8.4'
-  sha256 '5df0295fa0756cd44da9e9c7215b4e09ca521f2636116def2effe5cb997d6a47'
+  version '0.8.5'
+  sha256 'f4798450250b0995800af2e24e62ecb8e0696df0e3bd8458c5cfa32ec8e6c781'
   link 'Amethyst.app'
 end
