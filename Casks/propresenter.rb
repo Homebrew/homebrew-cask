fbb3da0a859788d035101a0004aeb53b74757430
Caleb Xu <calebcenter@live.com>
2014-04-12 11:35:17 -0400

    Added ProPresenter
    Church worship software for Mac and Windows for displaying lyrics and
    videos. Designed to be simple for volunteers to use.

---
+++ Casks/propresenter.rb
@@ -0,0 +1,7 @@
+class Propresenter < Cask
+  url 'http://www.renewedvision.com/downloads/ProPresenter5_5.2.8_b11499.dmg'
+  homepage 'http://www.renewedvision.com/propresenter.php'
+  version '5.2.8'
+  sha256 '47981ded0b025db373897b0103e9fa2894f4f0a073596da3725a739d33716e11'
+  link 'ProPresenter 5.app'
+end
