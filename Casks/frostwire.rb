cab9981149be2fb3cda6b312d7a9c092689f6af5
Caleb Xu <calebcenter@live.com>
2014-04-11 23:11:00 -0400

    Added FrostWire
    Share your creations with millions of people right from your computer
    on BitTorrent and the Cloud, absolutely free.

---
+++ Casks/frostwire.rb
@@ -0,0 +1,7 @@
+class Frostwire < Cask
+  url 'http://dl.frostwire.com/frostwire/5.7.2/frostwire-5.7.2.dmg'
+  homepage 'http://www.frostwire.com'
+  version '5.7.2'
+  sha256 'c559d214576684a97cee0bea15c91caf8dd2c3609cc498039813724ea475642e'
+  link 'FrostWire.app'
+end
