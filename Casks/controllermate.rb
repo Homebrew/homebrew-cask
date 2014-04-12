1b5e4887703ddb78e7bc2ce90d26ee9c3cdfaed5
Beng Hee Eu <beng@benghee.eu>
2014-04-12 22:40:40 +0800

    ControllerMate 4.8.1

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -1,8 +1,8 @@
 class Controllermate < Cask
-  url 'http://s3.amazonaws.com/orderedbytes/ControllerMate480.dmg'
+  url 'http://s3.amazonaws.com/orderedbytes/ControllerMate481.dmg'
   homepage 'http://www.orderedbytes.com/controllermate/'
-  version '4.8.0'
-  sha256 'a3a25989a5cc47600f1d72cdeefdb7505b3af23c7cdfcc235f6e2cde0197a879'
+  version '4.8.1'
+  sha256 '43a21d9740364c283ffdbeb67392b48f56fa31b084f1607799321e5ed24f4a5c'
   install 'ControllerMate.pkg'
   uninstall :script => 'ControllerMate Uninstaller.app/Contents/MacOS/ControllerMate Uninstaller'
 end
