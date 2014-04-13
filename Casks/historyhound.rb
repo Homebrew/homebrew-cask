25f03665ca349e994e699cc91a730d108495f236
Joel Kuzmarski <leoj3n@gmail.com>
2014-04-13 10:35:57 -0500

    HistoryHound 1.9.9

---
+++ Casks/historyhound.rb
@@ -0,0 +1,7 @@
+class Historyhound < Cask
+  url 'http://www.stclairsoft.com/download/HistoryHound-1.9.9.dmg'
+  homepage 'http://www.stclairsoft.com/HistoryHound/'
+  version '1.9.9'
+  sha256 '3b0873d2425115680c347d5e4be607e9232781e1711aec58ef76e07a07847ad3'
+  link 'HistoryHound.app'
+end
