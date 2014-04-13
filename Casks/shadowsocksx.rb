08a1aa278666edbc4eafc65022a40c2cdb5a8954
Tolbkni Kao <tolbkni@gmail.com>
2014-04-13 22:04:48 +0800

    Upgrade ShadowsocksX.app to v1.0.6

--- Casks/shadowsocksx.rb
+++ Casks/shadowsocksx.rb
@@ -1,7 +1,7 @@
 class Shadowsocksx < Cask
-  url 'http://downloads.sourceforge.net/project/shadowsocksgui/dist/ShadowsocksX-1.0.5.zip'
+  url 'http://downloads.sourceforge.net/project/shadowsocksgui/dist/ShadowsocksX-1.0.6.dmg'
   homepage 'https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help'
-  version '1.0.5'
-  sha256 '24331ad5f7dab9f3a1f0a7d8bb69797e9a071c427cab6603a01594c265ddaea2'
+  version '1.0.6'
+  sha256 '3b2d5289043f905897abc9ac395189201718acde79bd782c46a1e9ed6c32892d'
   link 'ShadowsocksX.app'
 end
