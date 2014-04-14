6f5b9830d5e3d27e968794a06606f7519f615126
billwanjohi <kaguru@gmail.com>
2014-04-14 03:35:01 +0000

    add reeder cask

---
+++ Casks/reeder.rb
@@ -0,0 +1,7 @@
+class Reeder < Cask
+  url 'http://reederapp.com/beta-reeder-for-mac/Reeder2.0b1.zip'
+  homepage 'http://reederapp.com/mac/'
+  version '2.0b1'
+  sha1 'd2a520c21c03dcdcab390a93bda3c7fc04e988f6'
+  link 'Reeder.app'
+end
