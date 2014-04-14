cac4986c88645bc2257720c1086898ea07896a6a
billwanjohi <kaguru@gmail.com>
2014-04-14 16:20:37 +0000

    reeder cask: switch to sha256

--- Casks/reeder.rb
+++ Casks/reeder.rb
@@ -2,6 +2,6 @@ class Reeder < Cask
   url 'http://reederapp.com/beta-reeder-for-mac/Reeder2.0b1.zip'
   homepage 'http://reederapp.com/mac/'
   version '2.0b1'
-  sha1 'd2a520c21c03dcdcab390a93bda3c7fc04e988f6'
+  sha256 '2a781d547e15d61f3aa01d3b0fae1cad9282afaf4eecd23f2bd83eeb3b22efd8'
   link 'Reeder.app'
 end
