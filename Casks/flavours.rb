23aaeb05dd4c1f5454b9f6dc482ba0b6034e94af
Beng Hee Eu <beng@benghee.eu>
2014-04-14 23:07:36 +0800

    Flavours latest

---
+++ Casks/flavours.rb
@@ -0,0 +1,7 @@
+class Flavours < Cask
+  url 'http://flavours-updates.interacto.net/Flavours.dmg'
+  homepage 'http://flavours.interacto.net/'
+  version 'latest'
+  no_checksum
+  link 'Flavours.app'
+end
