ef8f024fb5e78e90285855d7cf9ff85ba2e1d3af
JP Simard <jp@jpsim.com>
2014-04-14 16:14:59 -0700

    Fixed wrong hash for istat-menus

--- Casks/istat-menus.rb
+++ Casks/istat-menus.rb
@@ -2,6 +2,6 @@ class IstatMenus < Cask
   url 'http://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus4.21.zip'
   homepage 'http://bjango.com/mac/istatmenus/'
   version '4.21'
-  sha256 '376dfa849d5e2dcdd0d50c08b7329743b7a5b21dd6ab762f99b65fac710d169e'
+  sha256 'c0747dad57116f538d06c24bddbc962bd33f05320339f8884ac3f0e0cf04757a'
   link 'iStat Menus.app'
 end
