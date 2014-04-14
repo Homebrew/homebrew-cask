f9e7d4be3275566f52353714d3cce42e81d6d6f5
Joel Kuzmarski <leoj3n@gmail.com>
2014-04-13 23:33:51 -0500

    Desktop Log latest

---
+++ Casks/desktop-log.rb
@@ -0,0 +1,7 @@
+class DesktopLog < Cask
+  url 'http://www.nightproductions.net/downloads/desklog.dmg'
+  homepage 'http://www.nightproductions.net/desklog.html'
+  version 'latest'
+  no_checksum
+  link 'Desktop Log.app'
+end
