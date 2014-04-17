afcf4bd16e11602d5cf5eaf9e8593d7472f16f7a
Johan Carlquist <johan@rymdvarel.se>
2014-04-17 18:18:42 +0200

    New cask, Automatic 2
    Added Automatic 2, an prefpane-based RSS downloader.

---
+++ Casks/automatic.rb
@@ -0,0 +1,7 @@
+class Automatic < Cask
+  url 'http://codingcurious.com/downloads/Automatic.2.zip'
+  homepage 'http://codingcurious.com/automatic/'
+  version 'latest'
+  no_checksum
+  prefpane 'Automatic.prefPane'
+end
