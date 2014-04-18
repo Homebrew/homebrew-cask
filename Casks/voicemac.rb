f6833c49aa89af73dc9a715ee4b07fdd69c9bff7
Sean Zhu <interestinglythere@gmail.com>
2014-04-18 12:28:15 -0700

    Added permissions fix after_install for VoiceMac

--- Casks/voicemac.rb
+++ Casks/voicemac.rb
@@ -4,4 +4,7 @@ class Voicemac < Cask
   version 'latest'
   no_checksum
   link 'VoiceMac/VoiceMac.app'
+  after_install do
+    system '/bin/chmod', 'a+r', "#{destination_path}/VoiceMac/VoiceMac.app/Contents/Info.plist"
+  end
 end
