6a17ec2503c0722fb22ea9f3a0fa86fa23d4784b
Tolbkni Kao <tolbkni@gmail.com>
2014-04-18 19:20:51 +0800

    Add Rythem 2014-02-21

---
+++ Casks/rythem.rb
@@ -0,0 +1,7 @@
+class Rythem < Cask
+  url 'https://github.com/AlloyTeam/Rythem/releases/download/filter/Rythem-2014-2-21.dmg'
+  homepage 'https://github.com/AlloyTeam/Rythem'
+  version '2014-02-21'
+  sha256 'd4c5c578fb0f4d155cc07af22184f88a9b190a7453d0dc45962fd179f72da34d'
+  link 'Rythem.app'
+end
