a271c23661aeb4e3fbb1c5d68c2983f2e400dfae
Mailo Svetel <development@rooland.cz>
2014-04-18 11:37:30 +0200

    Rubymine 6.3.1

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -1,7 +1,7 @@
 class Rubymine < Cask
-  url 'http://download-ln.jetbrains.com/ruby/RubyMine-6.3.dmg'
+  url 'http://download-cf.jetbrains.com/ruby/RubyMine-6.3.1.dmg'
   homepage 'http://www.jetbrains.com/ruby/'
-  version '6.3'
-  sha256 '4999d1c83d5c9ba9bfed88e070181ef2b79cb04027c79099f977783bad6b82d8'
+  version '6.3.1'
+  sha256 'c0c5d32b94f6b12f871d76712ba8d1837cd6c190f14237b51c10feb1aa9fe1f3'
   link 'RubyMine.app'
 end
