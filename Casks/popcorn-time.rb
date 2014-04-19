f4dfc28b4a392935160496f389a2f7c0214bbfb4
Marc Riera <mrc2407@gmail.com>
2014-04-19 11:22:06 +0200

    Add Popcorn Time 0.2.9

---
+++ Casks/popcorn-time.rb
@@ -0,0 +1,7 @@
+class PopcornTime < Cask
+  url 'http://cdn.get-popcorn.com/build/Popcorn-Time-0.2.9-Mac.tar.gz'
+  homepage 'http://get-popcorn.com/'
+  version '0.2.9'
+  sha256 '1e964b57494a748e22915eb6653a9816f0dbacee73709c94b7f130adc5e162dc'
+  link 'Popcorn-Time.app'
+end
