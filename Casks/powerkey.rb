7d82434d993991e3609c3eba4431e1b0334498b4
Allen Wu <allen.wu@ucla.edu>
2014-04-20 17:13:12 -0700

    Update PowerKey to v1.2

--- Casks/powerkey.rb
+++ Casks/powerkey.rb
@@ -1,7 +1,7 @@
 class Powerkey < Cask
-  url 'https://github.com/pkamb/PowerKey/releases/download/v1.1/PowerKey.zip'
+  url 'https://github.com/pkamb/PowerKey/releases/download/v1.2/PowerKey1.2.zip'
   homepage 'http://pkamb.github.io/PowerKey/'
-  version '1.1'
-  sha256 '711a4e76d3013278c9e0e1ed09c044634636f7dc53f1348532a6de399aef3687'
+  version '1.2'
+  sha256 '47bfb13458883218cab6106bd948b8516c3a42a733180b49c410f26d4d465ca1'
   link 'PowerKey.app'
 end
