ad541a0911990cc63e650abb5e9ea07220e2a189
Nikola Majksner <majksner@users.noreply.github.com>
2014-04-22 08:11:23 +0200

    Bump to Vagrant 1.5.4

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,8 +1,8 @@
 class Vagrant < Cask
-  url 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.3.dmg'
+  url 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.4.dmg'
   homepage 'http://www.vagrantup.com'
-  version '1.5.3'
-  sha256 '7bb8a7cc1246bbb5831e78b31f0335777bcd9d16bc88016430a054dd2f2e038c'
+  version '1.5.4'
+  sha256 'c8ded45183a03a8bfabe67ee82886ecfdafeb24030df9e631717ae1209475ba8'
   install 'Vagrant.pkg'
   uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] }
 end
