01ae863501ca23b38c6ade566baee44c76ad1c54
David O'Trakoun <me@davidosomething.com>
2014-04-22 13:23:09 -0400

    update skyfonts name and filename

--- Casks/monotype-skyfonts.rb
+++ Casks/monotype-skyfonts.rb
@@ -1,8 +1,7 @@
 class MonotypeSkyfonts < Cask
-  url 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_4.1.0.0.dmg.zip'
-  homepage 'http://www.fonts.com/web-fonts/google'
-  version '4.1.0.0'
-  sha256 '7084704117be1a21081b4a6e33327cb524a4c7d8938bb947aaa361737046fb40'
-  nested_container 'Monotype_SkyFonts_Mac64_4.1.0.0.dmg'
-  link 'Monotype Skyfonts.app'
+  url 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_4.4.0.0.dmg.zip'
+  homepage 'http://skyfonts.com'
+  version '4.4.0.0'
+  no_checksum
+  link 'Monotype SkyFonts.app'
 end
--- Casks/skyfonts.rb
+++
@@ -1,7 +0,0 @@
-class Skyfonts < Cask
-  url 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_4.4.0.0.dmg.zip'
-  homepage 'http://skyfonts.com'
-  version '4.4.0.0'
-  no_checksum
-  link 'Monotype SkyFonts.app'
-end
