d0d10a5e9f4f2269dbe196e4f6a23fb5126b0202
David O'Trakoun <me@davidosomething.com>
2014-04-22 00:04:02 -0400

    add monotype skyfonts

---
+++ Casks/skyfonts.rb
@@ -0,0 +1,7 @@
+class Skyfonts < Cask
+  url 'http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_4.4.0.0.dmg.zip'
+  homepage 'http://skyfonts.com'
+  version '4.4.0.0'
+  no_checksum
+  link 'Monotype SkyFonts.app'
+end
