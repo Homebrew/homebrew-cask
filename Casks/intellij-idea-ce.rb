9b127f36994d7a2aff8f6e5fbfccd17e3bd79ca9
Beng Hee Eu <beng@benghee.eu>
2014-04-22 16:56:30 +0800

    IntelliJ IDEA Community Edition 13.1.2

--- Casks/intellij-idea-ce.rb
+++ Casks/intellij-idea-ce.rb
@@ -1,7 +1,7 @@
 class IntellijIdeaCe < Cask
-  url 'http://download-cf.jetbrains.com/idea/ideaIC-13.1.1.dmg'
+  url 'http://download-cf.jetbrains.com/idea/ideaIC-13.1.2.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '13.1.1'
-  sha256 '49ce24a8b11b9f3c439af4ca3d7bdf6f083c498aec2e9fd0111cfae2a9178cd4'
+  version '13.1.2'
+  sha256 '99422ba5faacd4d6aabf029b83cf1f9dd22fe445116b984df25d49ae8609e46e'
   link 'IntelliJ IDEA 13 CE.app'
 end
