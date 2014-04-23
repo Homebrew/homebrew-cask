7225c07c37364b209e2da313fa0daadee2a7b506
Vincent Klaiber <vincent@schimpanz.com>
2014-04-23 09:13:22 +0200

    Add Torpedo version 1.2.2.
    Share files, passwords, and more with self-destructing private links. https://usetorpedo.com

---
+++ Casks/torpedo.rb
@@ -0,0 +1,7 @@
+class Torpedo < Cask
+  url 'http://usetorpedo.com/downloads/mac/Torpedo-1.2.2.app.zip'
+  homepage 'https://usetorpedo.com'
+  version '1.2.2'
+  sha256 '72f0a027ea525755e07e43d90464aa00f1c45167b30d333272017f64e0496dcf'
+  link 'Torpedo.app'
+end
