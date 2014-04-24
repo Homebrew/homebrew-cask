8f58443c98a0e8b5f3d3316f509d8a8102161e45
tcarlsen <thomascarlsen86@gmail.com>
2014-04-24 09:01:53 +0200

    updating hex to beta

--- Casks/hex.rb
+++ Casks/hex.rb
@@ -1,5 +1,5 @@
 class Hex < Cask
-  url 'http://origin.dl.hex.gameforge.com/HexInstaller.dmg'
+  url 'http://dl.hex.gameforge.com/HexInstaller.dmg'
   homepage 'http://hextcg.com/'
   version 'latest'
   no_checksum
