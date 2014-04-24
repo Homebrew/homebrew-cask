c780e31016cbf137614c0127df7d3541ef0a42e7
Shaun Jackman <sjackman@gmail.com>
2014-04-24 12:10:36 -0700

    figtree 1.4.0: New formula
    FigTree is designed as a graphical viewer of phylogenetic trees and as
    a program for producing publication-ready figures.

---
+++ Casks/figtree.rb
@@ -0,0 +1,8 @@
+class Figtree < Cask
+  url 'http://tree.bio.ed.ac.uk/download.php?id=86'
+  homepage 'http://tree.bio.ed.ac.uk/software/figtree/'
+  version 'latest'
+  no_checksum
+  link 'FigTree v1.4.0.app'
+  qlplugin 'QuickLook Plugin/FigTreeQuickLookPlugin.qlgenerator'
+end
