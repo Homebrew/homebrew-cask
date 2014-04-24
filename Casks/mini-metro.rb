21cfab4eaa2e854948efddb69f3761b9a1680375
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-04-24 14:08:44 -0400

    Update mini metro to Aplha 9e

--- Casks/mini-metro.rb
+++ Casks/mini-metro.rb
@@ -1,7 +1,7 @@
 class MiniMetro < Cask
-  url 'http://static.dinopoloclub.com/minimetro/builds/alpha9/MiniMetro-alpha9d-osx.zip'
+  url 'http://static.dinopoloclub.com/minimetro/builds/alpha9/MiniMetro-alpha9e-osx.zip'
   homepage 'http://dinopoloclub.com/minimetro/'
-  version 'Alpha 9d'
-  sha256 '6ab2aa60fe4d65f347b2277ebb6ddb3712ef1efa30c8489cc559e5d9f817881b'
-  link 'MiniMetro-alpha9d-osx.app', :target => 'Mini Metro.app'
+  version 'Alpha 9e'
+  sha256 'd2261a4e2ab6d71114af99524fa86c590b66f880c32c8546d556a2b80cc0be16'
+  link 'MiniMetro-alpha9e-osx.app', :target => 'Mini Metro.app'
 end
