06a36eb330f290d601e511fb7d4d648d6929af87
Ngoc Pham <ngocphamm@users.noreply.github.com>
2014-04-25 16:13:48 -0400

    Update TeXnicle to latest version 2.2.11

--- Casks/texnicle.rb
+++ Casks/texnicle.rb
@@ -1,7 +1,7 @@
 class Texnicle < Cask
-  url 'http://www.bobsoft-mac.de/resources/TeXnicle/2.2/TeXnicle.app.2.2.9.zip'
+  url 'http://www.bobsoft-mac.de/resources/TeXnicle/2.2/TeXnicle.app.2.2.11.zip'
   homepage 'http://www.bobsoft-mac.de/texnicle/texnicle.html'
-  version '2.2.9'
-  sha256 '2076a2922bd156ac6e6af1afbb58bd928579a457f500fae40024f24f65a5e5dd'
+  version '2.2.11'
+  sha256 'b522e551220bfc98ff3d3c09db224c0db2a31c1dff4c73353a26d1f13217733b'
   link 'TeXnicle.app'
 end
