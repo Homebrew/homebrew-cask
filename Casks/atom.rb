2e770d15d79052b542a494e924bdce4f92a4bd9f
BingFoon Lee <bingfoon@gmail.com>
2014-04-25 07:31:27 -0700

    Fix CLI tool installation in Atom Editor.

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -4,4 +4,9 @@ class Atom < Cask
   version 'latest'
   no_checksum
   link 'Atom.app'
+  binary 'Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm', :target => 'apm'
+  binary 'Atom.app/Contents/Resources/app/atom.sh', :target => 'atom'
+  after_install do
+    system '/usr/bin/defaults', 'write', 'com.github.atom', 'moveToApplicationsFolderAlertSuppress', '-bool', 'true'
+  end
 end
