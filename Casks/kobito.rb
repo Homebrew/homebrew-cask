279f13b8da73e4540b0f5d49e1a6ab6b06ac7b34
Yuku Takahashi <taka84u9@gmail.com>
2014-04-25 10:41:37 +0900

    Update Kobito to latest version

--- Casks/kobito.rb
+++ Casks/kobito.rb
@@ -1,7 +1,7 @@
 class Kobito < Cask
-  url 'http://kobito.qiita.com/download/Kobito_v1.8.7.zip'
+  url 'http://kobito.qiita.com/download/Kobito_v1.9.1.zip'
   homepage 'http://kobito.qiita.com/'
-  version '1.8.7'
-  sha256 '33e05d9eb6fa24d6bc03073aa0f80c3dd5dc6f07c2f28729aae2327b9a4de41e'
+  version '1.9.1'
+  sha256 '62fd1379ddd70a6dc2993aa945818c4dbb997bd06c2e6ecbbc90d3187861104c'
   link 'Kobito.app'
 end
