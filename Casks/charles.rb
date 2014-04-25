d2d50f28adfe15801623531c45a29389b98955ae
Bob Fanger <bfanger@gmail.com>
2014-04-25 13:36:14 +0200

    Upgrade Charles.app to v3.9

--- Casks/charles.rb
+++ Casks/charles.rb
@@ -1,7 +1,7 @@
 class Charles < Cask
-  url 'http://www.charlesproxy.com/assets/release/3.8.3/charles-proxy-3.8.3-applejava.dmg'
+  url 'http://www.charlesproxy.com/assets/release/3.9/charles-proxy-3.9-applejava.dmg'
   homepage 'http://www.charlesproxy.com/'
-  version '3.8.3'
-  sha256 '2c1b2d9b5085c34d95d580fec4575bbd0ff183eb1b323b7f260745f91e109083'
+  version '3.9'
+  sha256 'a5d8c557fc97ed87dbf844cb1cf0d771c7a7d342c7c730cc22032995aa912d37'
   link 'Charles.app'
 end
