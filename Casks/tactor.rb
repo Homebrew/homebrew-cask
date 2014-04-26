c1eebc27b3e9069f445d39d363ea896fd0fe3791
Fernando Paredes <nano@fdp.io>
2014-04-25 20:00:00 -0700

    Tactor: Fix shasum

--- Casks/tactor.rb
+++ Casks/tactor.rb
@@ -2,6 +2,6 @@ class Tactor < Cask
   url 'https://dl.dropboxusercontent.com/u/7614970/Tactor.zip'
   homepage 'http://onflapp.wordpress.com/tactor/'
   version '0.4.6-BETA'
-  sha256 'bc9a2ad1bf9e86f2cf3081d9d0c815845d87712b58986d78e250eeac4224292e'
+  sha256 'cfc24f13e7b604c3ffa315a99874bb22faeb1e0d2b5a4db6e31510fcdb248bd4'
   link 'Tactor.app'
 end
