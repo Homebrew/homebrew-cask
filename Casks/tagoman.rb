ce67da376f5948450a5ff866ce4fcdb28fa55c0c
Fernando Paredes <nano@fdp.io>
2014-04-25 19:58:23 -0700

    Tagoman: Fix SHA

--- Casks/tagoman.rb
+++ Casks/tagoman.rb
@@ -2,6 +2,6 @@ class Tagoman < Cask
   url 'https://dl.dropboxusercontent.com/u/7614970/TagoMan.zip'
   homepage 'http://onflapp.wordpress.com/tagoman'
   version '0.6.1-BETA'
-  sha256 '7fbab6bb92867860e24ae0c1fc3ed9434e45056480b1c55a8f13a0c8683f2099'
+  sha256 '75c1525272ef37b426928e28e2e16ebe3ca9a9c4615d739de0727d16a9939718'
   link 'TagoMan.app'
 end
