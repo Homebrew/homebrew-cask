448fbf0597d65b6eb2870b43fcb831abf73fc4df
Fernando Paredes <nano@fdp.io>
2014-04-25 20:15:48 -0700

    Actotracker: Fix checksum

--- Casks/actotracker.rb
+++ Casks/actotracker.rb
@@ -2,6 +2,6 @@ class Actotracker < Cask
   url 'https://dl.dropboxusercontent.com/u/7614970/ActoTracker.zip'
   homepage 'http://onflapp.wordpress.com/actotracker'
   version '0.9-BETA'
-  sha256 '443a7e8a1f2830266d53288b482719d5fe7e710bff251869667dec075eae9fda'
+  sha256 '5ac8837e039444a46ae29d9920186e7ff14d52a4f80e7efa7eb6a5d887e3aff7'
   link 'ActoTracker.app'
 end
