1f0fbcad67b2c2625af879a0ba9edbe4bf5c42ce
Julio Napurí <julionc@gmail.com>
2014-04-26 15:35:58 -0500

    Add gethub

---
+++ Casks/gethub.rb
@@ -0,0 +1,7 @@
+class Gethub < Cask
+  url 'http://gethub.jack.ly/0.1.3/darwin_amd64/gethub_0.1.3_darwin_amd64.zip'
+  homepage 'https://github.com/pearkes/gethub'
+  version '0.1.3'
+  sha256 '6db6c17a54e8fefef61ecd34774d98e4e43818e7cd42a6ea686538f6d653747f'
+  binary 'gethub'
+end
