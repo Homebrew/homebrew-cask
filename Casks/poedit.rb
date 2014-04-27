58fe019b55eba30806e37a935dbd45e34c295305
Fernando Paredes <nano@fdp.io>
2014-04-27 11:34:13 -0700

    Fix Poedit's homepage

--- Casks/poedit.rb
+++ Casks/poedit.rb
@@ -1,6 +1,6 @@
 class Poedit < Cask
   url 'http://poedit.net/dl/Poedit-1.6.4.2569.zip'
-  homepage 'http://www.trankynam.com/xtrafinder/'
+  homepage 'http://www.poedit.net'
   version '1.6.4.2569'
   sha256 'fc2ecab42ba3a166feb9ef753aeed05714a8064064bb2745603f965f08b812fd'
   link 'Poedit.app'
