563d77f5eecf254f1340b9c6f5499dec8769300a
Daniel Garzon <dg2796@columbia.edu>
2014-04-28 17:40:23 -0400

    Add Weka Apple JVM.

--- Casks/weka-apple-jvm.rb
+++ Casks/weka-apple-jvm.rb
@@ -1,5 +1,5 @@
 class WekaAppleJvm < Cask
-  url 'http://prdownloads.sourceforge.net/weka/weka-3-6-11-apple-jvm.dmg'
+  url 'http://downloads.sourceforge.net/sourceforge/weka/weka-3-6-11-apple-jvm.dmg'
   homepage 'http://www.cs.waikato.ac.nz/ml/weka/'
   version '3.6.11'
   sha256 '2d5d59e6f0a25d26ba3f8b6107b7914d120b59c085da7a3019ea4642325cecdd'
