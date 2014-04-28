7858bd2db0ff060d226ce6bc068c83cec439d349
Vítor Galvão <info@vitorgalvao.com>
2014-04-28 01:15:37 +0100

    updated instacast

--- Casks/instacast.rb
+++ Casks/instacast.rb
@@ -1,7 +1,7 @@
 class Instacast < Cask
-  url 'http://assets.vemedio.com/software/instacast/Instacast-1.1-2079.dmg'
+  url 'http://assets.vemedio.com/software/instacast/Instacast-1.2-2169.dmg'
   homepage 'http://vemedio.com/products/instacast-mac'
-  version '1.1 (2079)'
-  sha256 'b9fdb58447859d26240aec691aa47c44fa2f8db8d4f1c207a8a1abdf69ac0761'
+  version '1.2 (2169)'
+  sha256 '5b84ad6daab7386d35d958f919a8e920f3d01f5c0aa17b7753c57d493e71b524'
   link 'Instacast.app'
 end
