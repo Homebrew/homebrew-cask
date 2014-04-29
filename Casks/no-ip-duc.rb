f3bd1af4416b67419d58c18ef4493f138958fe08
Lee Hanxue <hanxue.developer@gmail.com>
2014-04-29 16:52:17 +0800

    no-ip 3.1.5
    Rename no-ip to no-ip-duc

---
+++ Casks/no-ip-duc.rb
@@ -0,0 +1,7 @@
+class NoIpDuc < Cask
+  url 'http://www.noip.com/client/mac/noip3.1.5.dmg'
+  homepage 'http://www.noip.com/download?page=mac'
+  version '3.1.5'
+  sha256 'a5c68a13188fd88f00c0588baa1b54a0ac2529a02b09dc436770183c059bfaf5'
+  link 'No-IP DUC.app'
+end
