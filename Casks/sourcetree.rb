8ebaebf6a5b762550d82d79b209d4bc6015c5e89
Ngoc Pham <ngocphamm@users.noreply.github.com>
2014-04-29 12:24:50 -0400

    Update SourceTree to latest version 1.9.0

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,8 +1,8 @@
 class Sourcetree < Cask
-  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.8.1.dmg'
+  url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.9.0.dmg'
   homepage 'http://www.sourcetreeapp.com/'
-  version '1.8.1'
-  sha256 '37a42f2d83940cc7e1fbd573a70c3c74a44134c956ac3305f6b153935dc01b80'
+  version '1.9.0'
+  sha256 '78867c7b9781254bcdb1e366770677519305213f35d4efd106e23d7c5a74ca82'
   link 'SourceTree.app'
   binary 'SourceTree.app/Contents/Resources/stree'
   caveats do
