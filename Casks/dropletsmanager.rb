ff9545c459d0d4940bf970dbf0daf4e51d594553
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-04-29 19:23:02 -0400

    Rename DigitalOcean Droplet Manager and change host to be github releases

--- Casks/dropletsmanager.rb
+++
@@ -1,7 +0,0 @@
-class Dropletsmanager < Cask
-  url 'https://dl.dropboxusercontent.com/u/115750/files/DropletsManager0.1.zip'
-  homepage 'https://github.com/deivuh/DODropletManager-OSX'
-  version '0.1'
-  sha256 'e41875c0b4c24f510aff324d189dfc4fc7227e78117fff2374c5064c2ecf00bd'
-  link 'DropletsManager.app'
-end
