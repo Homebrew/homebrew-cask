79a6b6cffa4492d9f1c6ea2346741df9fb36339c
Vincent Klaiber <vincent@schimpanz.com>
2014-04-30 10:18:36 +0200

    Add Squire version 0.91
    Not your average media center. [http://squireapp.com](http://squireapp.com)

---
+++ Casks/squire.rb
@@ -0,0 +1,7 @@
+class Squire < Cask
+  url 'https://s3.amazonaws.com/Squire_Mac_Builds/Squire_0.91.zip'
+  homepage 'http://squireapp.com'
+  version '0.91'
+  sha256 'd37f2cdf5f08ed85e5a1f10a66c70a964e51e3a64e7c7335d97c50b1a7a1691e'
+  link 'Squire.app'
+end
