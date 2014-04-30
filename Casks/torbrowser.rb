2589a94c83c138a64a3626ee7a52608eba7d96d1
Mike English <mike.english@atomicobject.com>
2014-04-30 10:13:27 -0400

    Upgrade torbrowserbundle to v3.6 and rename to torbrowser.

---
+++ Casks/torbrowser.rb
@@ -0,0 +1,7 @@
+class Torbrowser < Cask
+  url 'https://www.torproject.org/dist/torbrowser/3.6/TorBrowser-3.6-osx32_en-US.dmg'
+  homepage 'https://www.torproject.org/projects/torbrowser.html'
+  version '3.6'
+  sha256 '54f98f22d95663144d7c31fa3f1b2690574abbed9be6ca1718be99c9a9c74623'
+  link 'TorBrowser.app'
+end
