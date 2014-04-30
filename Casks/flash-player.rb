8211ffb27fbebc5e27b6f5533a51751079650971
shin1x1 <shin1x1@gmail.com>
2014-04-30 19:30:27 +0900

    Update flash-player 13.0.0.206

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,7 +1,7 @@
 class FlashPlayer < Cask
   url 'http://fpdownload.macromedia.com/pub/flashplayer/updaters/13/flashplayer_13_sa.dmg'
   homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
-  version '13.0.0.201'
-  sha256 '4674abf1740679e221167e872071625ec3a470c5d9e050f76c5efc78419abff7'
+  version '13.0.0.206'
+  sha256 'eeb47ba093876fc25d4993e0f7652e398c66c9f0a0e89d01586ab33c7a82bab2'
   link 'Flash Player.app'
 end
