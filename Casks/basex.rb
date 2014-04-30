cf81566ae64823fd2513bf7b48208d722873a805
John Dziurzynski <john@evesaddiction.com>
2014-04-30 15:59:01 -0400

    Remove superfluous container_type

--- Casks/basex.rb
+++ Casks/basex.rb
@@ -3,6 +3,5 @@ class Basex < Cask
   homepage 'http://basex.org/home/'
   version '7.8.1'
   sha256 '495b4c32cb916c02543a5f19b702c01516b1b8f452be537259dd6d1ad040ca87'
-  container_type :zip
   link 'BaseX.app'
 end
