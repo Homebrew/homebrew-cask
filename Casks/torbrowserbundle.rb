fa53faa3fe8109275d6057c445829badeb6de71c
Daniel Garzon <dg2796@columbia.edu>
2014-04-30 13:43:43 -0400

    Rename TorBrowserBundle to TorBrowser and Update to v3.6

--- Casks/torbrowserbundle.rb
+++
@@ -1,7 +0,0 @@
-class Torbrowserbundle < Cask
-  url 'https://www.torproject.org/dist/torbrowser/3.5.4/TorBrowserBundle-3.5.4-osx32_en-US.zip'
-  homepage 'https://www.torproject.org/projects/torbrowser.html'
-  version '3.5.4'
-  sha256 'e2ffd4173a618097e61e4b5391b832b5cceec38613599b67a3bfb5bafdbdca48'
-  link 'TorBrowserBundle_en-US.app'
-end
