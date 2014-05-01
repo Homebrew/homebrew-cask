9bb4708cfcd23951253554a795f4e5e7342d338b
gerstorfer <dominik.gerstorfer@gmail.com>
2014-05-01 18:11:09 +0200

    Add arRsync

---
+++ Casks/arRsync.rb
@@ -0,0 +1,7 @@
+class Arrsync < Cask
+  url 'http://sourceforge.net/projects/arrsync/files/latest/download'
+  homepage 'http://arrsync.sourceforge.net'
+  version 'latest'
+  no_checksum
+  link 'arRsync.app'
+end
