325ca7c21f36c8f4b328fef4e71f3b11be9fa36f
Lincoln Lee <git@lincoln.hk>
2014-05-01 20:19:21 +0800

    Remove utorrent.app

--- Casks/utorrent.rb
+++
@@ -1,7 +0,0 @@
-class Utorrent < Cask
-  homepage 'http://www.utorrent.com/'
-  version 'latest-beta'
-  url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/beta/'
-  no_checksum
-  link 'uTorrent.app'
-end
