b062a152db0f0a08bbe686d93140776aa6f8721b
gerstorfer <dominik.gerstorfer@gmail.com>
2014-05-01 18:11:09 +0200

    Add arRsync v0.4.1

---
+++ Casks/arRsync.rb
@@ -0,0 +1,7 @@
+class Arrsync < Cask
+  url 'http://prdownloads.sourceforge.net/arrsync/arrsync-0.4.1.dmg?download'
+  homepage 'http://arrsync.sourceforge.net'
+  version '0.4.1'
+  sha256 '8a254771065f90a9f9f882589eccaf7bc51ad5d0ac86faf15847381bcdbe64f0'
+  link 'arRsync.app'
+end
