ba8edf81db5524e6125938c47211ff8c02465e34
gerstorfer <dominik.gerstorfer@gmail.com>
2014-05-01 21:30:01 +0200

    Add arRsync
    Changed URL and checksum to meet requirements for SourceForge projects.

--- Casks/arRsync.rb
+++ Casks/arRsync.rb
@@ -1,7 +1,7 @@
 class Arrsync < Cask
-  url 'http://prdownloads.sourceforge.net/arrsync/arrsync-0.4.1.dmg?download'
+  url 'http://sourceforge.net/projects/arrsync/files/latest/download'
   homepage 'http://arrsync.sourceforge.net'
-  version '0.4.1'
-  sha256 '8a254771065f90a9f9f882589eccaf7bc51ad5d0ac86faf15847381bcdbe64f0'
+  version 'latest'
+  no_checksum
   link 'arRsync.app'
 end
