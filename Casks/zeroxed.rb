9089a477c5b986ec5a134213dab58d0b1250fcca
Vincent Klaiber <vincent@schimpanz.com>
2014-05-01 12:47:37 +0200

    Update ZeroXed to use the latest version.

--- Casks/zeroxed.rb
+++ Casks/zeroxed.rb
@@ -1,7 +1,7 @@
 class Zeroxed < Cask
   url 'http://www.suavetech.com/cgi-bin/download.cgi?0xED.tar.bz2'
   homepage 'http://www.suavetech.com/0xed/'
-  version '1.1.3'
-  sha256 '56b68898c7a8c1169e9f42790091b100841c3d43549445dc3c4986fae7152793'
+  version 'latest'
+  no_checksum
   link '0xED.app'
 end
