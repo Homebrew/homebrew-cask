93120d14639de5a756d396da39d26dc76566113a
Falko Richter <falko@briefhansa.de>
2014-05-03 18:33:27 +0200

    added jitouch

---
+++ Casks/jitouch.rb
@@ -0,0 +1,7 @@
+class Jitouch < Cask
+  url 'http://www.jitouch.com/jitouch_mavericks.zip'
+  homepage 'http://www.jitouch.com'
+  version 'latest'
+  no_checksum
+  prefpane 'jitouch/Jitouch.prefPane'
+end
