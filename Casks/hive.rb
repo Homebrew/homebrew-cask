9635cd547a367aed6c98e1e414ab21730dc871ee
ndr <ndr@qef.io>
2014-05-03 16:33:01 +0200

    Update Hive to 1.2.1

--- Casks/hive.rb
+++ Casks/hive.rb
@@ -1,8 +1,8 @@
 class Hive < Cask
-  url 'https://github.com/hivewallet/hive-osx/releases/download/1.2/Hive-1.2.zip'
+  url 'https://github.com/hivewallet/hive-osx/releases/download/1.2.1/Hive-1.2.1.zip'
   homepage 'http://www.hivewallet.com'
-  version '1.2'
-  sha256 '512b5afe0135a87f10a2aa484506d2d38731960130f875de358023074d2ca19a'
+  version '1.2.1'
+  sha256 'eab97d7a1800e6ba369403c73336a531b90e8c19de8c3fc84372c40779f3f918'
   link 'Hive.app'
 
   after_install do
