d34be2e3e0d62413334b70f7df1ab5e639557885
Lincoln Lee <git@lincoln.hk>
2014-05-03 11:39:23 +0800

    Update version to latest for Noisy.app

--- Casks/noisy.rb
+++ Casks/noisy.rb
@@ -1,7 +1,7 @@
 class Noisy < Cask
   url 'http://github.com/downloads/jonshea/Noisy/Noisy.zip'
   homepage 'https://github.com/jonshea/Noisy'
-  version 'checksum'
+  version 'latest'
   no_checksum
   link 'Noisy.app'
 end
