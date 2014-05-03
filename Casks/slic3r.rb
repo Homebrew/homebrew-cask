fb7e86ca6c78c15f0b4d5dfcdd4ca2b55efae5dd
Falko Richter <falko@briefhansa.de>
2014-05-03 18:33:15 +0200

    added slic3r

---
+++ Casks/slic3r.rb
@@ -0,0 +1,7 @@
+class Slic3r < Cask
+  url 'http://dl.slic3r.org/mac/slic3r-osx-uni-1-0-1-stable.dmg'
+  homepage 'http://slic3r.org/'
+  version '1.0.1'
+  sha256 '2c1b07f41f392b5b111307e796158fe21038bca0a296c4d93756a902586c8629'
+  link 'Slic3r.app'
+end
