6330c83d0ff8f0314fb4898db674810bb4d593bb
Egor Yurtaev <yurtaev.egor@gmail.com>
2014-05-03 23:01:09 +0700

    Added Yandex browser cask

---
+++ Casks/yandex.rb
@@ -0,0 +1,7 @@
+class Yandex < Cask
+  url 'http://download.cdn.yandex.net/browser/yandex/ru/Yandex.dmg'
+  homepage 'http://browser.yandex.com/'
+  version 'latest'
+  no_checksum
+  link 'Yandex.app'
+end
