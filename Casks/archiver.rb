d40e9f23a3a0134f1da8b650f5ff2500f0177819
Zac <zac@z4c.us>
2014-05-03 13:00:15 -0700

    Add Archiver v.latest
    New cask for Archiver tool (zip, rar, etc)

---
+++ Casks/archiver.rb
@@ -0,0 +1,7 @@
+class Archiver < Cask
+  url 'http://commondatastorage.googleapis.com/incrediblebee/apps/Archiver/Archiver.zip'
+  homepage 'http://archiverapp.com/'
+  version 'latest'
+  no_checksum
+  link 'Archiver.app'
+end
