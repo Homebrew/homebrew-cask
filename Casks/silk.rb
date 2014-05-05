2cf629cd9c888feaaebab6561191075b1b715897
David Riccitelli <david@insideout.io>
2014-05-05 12:29:28 +0300

    remove linking app in Applications

--- Casks/silk.rb
+++ Casks/silk.rb
@@ -3,6 +3,5 @@ class Silk < Cask
   homepage 'http://silk.wbsg.de/'
   version '2.6.0'
   sha256 'a0a4ce95d5cbe589038dd82b311a5a32d751cdc90b34d841d5cc5f8dbc64b878'
-  link 'silk_2.6.0/workbench/bin/silk-workbench', :target => 'Silk Workbench'
   binary 'silk_2.6.0/workbench/bin/silk-workbench'
 end
