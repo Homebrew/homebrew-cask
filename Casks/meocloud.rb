aae9bc0769eb6eb7f41e3d446dc7250d3e864b86
Carlos Fonseca <carlosefonseca@gmail.com>
2014-05-05 10:41:51 +0100

    Add MEOCloud.app v0.1.115

---
+++ Casks/meocloud.rb
@@ -0,0 +1,7 @@
+class Meocloud < Cask
+  url 'https://meocloud.pt/binaries/osx/MEOCloud-0.1.115.dmg'
+  homepage 'https://meocloud.pt'
+  version '0.1.115'
+  sha256 '7ec0658eed5e77730275bf97e196ac3fea5ed4946e9632c0ae8458d8093768b1'
+  link 'MEOCloud.app'
+end
