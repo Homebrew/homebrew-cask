4df75627997348d928d8d0cc2819636104e077c8
Mark Janssen <mark@praseodym.net>
2014-05-05 18:37:44 +0200

    Weka 3.6.11

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -1,7 +1,7 @@
 class Weka < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/weka/weka-3-6-10-oracle-jvm.dmg'
+  url 'http://downloads.sourceforge.net/sourceforge/weka/weka-3-6-11-oracle-jvm.dmg'
   homepage 'http://www.cs.waikato.ac.nz/ml/weka/'
-  version '3.6.10'
-  sha256 '0681d0bf72ac0d1b55765072afb2e227d27308b7fae09c7db8afed1fd9717e79'
-  link 'weka-3-6-10-oracle-jvm.app'
+  version '3.6.11'
+  sha256 '51643edc349f46d76b96460ab16b97cf8e2d63ae6a4f6e5ba4c89dd404b56cea'
+  link 'weka-3-6-11-oracle-jvm.app'
 end
