b6983ab5b210de5203f00bf0b0d4786c3b537e4c
David Riccitelli <david@insideout.io>
2014-05-05 11:25:36 +0300

    create Silk cask
    for the Silk Workbench

---
+++ Casks/silk.rb
@@ -0,0 +1,7 @@
+class Silk < Cask
+  url 'http://silk.wbsg.de/releases/silk_2.6.0.zip'
+  homepage 'http://silk.wbsg.de/'
+  version '2.6.0'
+  sha256 'a0a4ce95d5cbe589038dd82b311a5a32d751cdc90b34d841d5cc5f8dbc64b878'
+  link 'silk_2.6.0/workbench/bin/silk-workbench'
+end
