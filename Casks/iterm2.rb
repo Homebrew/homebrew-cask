a5c6578f75f4a1a1070da2b8b93de4004ebb62c9
blogabe <abe@homer.local>
2014-05-06 15:07:01 -0700

    iterm2: upgrade to bug fix release

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -1,8 +1,8 @@
 class Iterm2 < Cask
   # note: "2" is not a version number, but indicates a different vendor
-  url 'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip'
+  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20140421.zip'
   homepage 'http://www.iterm2.com/'
-  version '1.0.0'
-  sha256 '2afad022b1e1f08b3ed40f0c2bde7bf7cce003852c83f85948c7f57a5578d9c5'
+  version '1.0.0.20140421'
+  sha256 'd9f3dd198febf46c272a8d4b473e35cae4cdfbf1963a7796114df53b5cb06c63'
   link 'iTerm.app'
 end
