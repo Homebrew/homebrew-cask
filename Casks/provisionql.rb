07a5477a061d054a78519f9f61bca8ce4266d703
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2014-05-06 17:27:48 +0400

    Add ProvisionQL QuickLook plugin v1.0.0

---
+++ Casks/provisionql.rb
@@ -0,0 +1,7 @@
+class Provisionql < Cask
+  url 'https://github.com/ealeksandrov/ProvisionQL/releases/download/1.0.0/ProvisionQL.zip'
+  homepage 'https://github.com/ealeksandrov/ProvisionQL'
+  version '1.0.0'
+  sha256 'fc21f2996050711a2496351092f3ffe88d9d017594ae913cce6ef059d1cf2c2a'
+  qlplugin 'ProvisionQL.qlgenerator'
+end
