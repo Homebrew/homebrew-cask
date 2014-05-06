6e1d2c9e15f1b380fcec2ad04ec3652637569760
Bob Example <the.keith.hamilton@gmail.com>
2014-05-05 20:31:33 -0700

    added araxis merge cask

---
+++ Casks/araxis-merge.rb
@@ -0,0 +1,7 @@
+class Araxismerge < Cask
+  url 'http://www.araxis.com/download/Merge2014.4459-OSX10.9.dmg'
+  homepage 'http://www.araxis.com/merge/'
+  version 'latest'
+  no_checksum
+  link 'Araxis Merge.app'
+end
