1299afbadcb064a17d9b38a850d736d40fc23f1b
David Riccitelli <david@insideout.io>
2014-05-07 08:33:38 +0300

    Add JDiskReport 1.4.1

---
+++ Casks/jdiskreport.rb
@@ -0,0 +1,7 @@
+class Jdiskreport < Cask
+  url 'http://www.jgoodies.com/download/jdiskreport/jdiskreport-1_4_1-mac.zip'
+  homepage 'http://www.jgoodies.com/freeware/jdiskreport/'
+  version '1.4.1'
+  sha256 'cb64f3d7ea8e43b142a69cabdded832b8de8e503766ccf1c7a16b314d2bc0d44'
+  link 'JDiskReport 1.4.1/JDiskReport.app'
+end
