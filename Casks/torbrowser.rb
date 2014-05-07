0f5f8843bfc5eabfc876bae9bd5437a8b9936992
Frank Louwers <frank@openminds.be>
2014-05-07 20:39:16 +0200

    Upgrade Torbrowser to 3.6.1

--- Casks/torbrowser.rb
+++ Casks/torbrowser.rb
@@ -1,7 +1,7 @@
 class Torbrowser < Cask
-  url 'https://www.torproject.org/dist/torbrowser/3.6/TorBrowser-3.6-osx32_en-US.dmg'
+  url 'https://www.torproject.org/dist/torbrowser/3.6.1/TorBrowser-3.6.1-osx32_en-US.dmg'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  version '3.6'
-  sha256 '54f98f22d95663144d7c31fa3f1b2690574abbed9be6ca1718be99c9a9c74623'
+  version '3.6.1'
+  sha256 'ecd3357e84712ada929aebeab26eaf0a68f50000b081bf1d4df154f949a939e8'
   link 'TorBrowser.app'
 end
