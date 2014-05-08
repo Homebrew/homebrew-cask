0a2e0ebaf66cd8229ac3b40d88f545ad3d90c324
Lukas Spieß <lukas.spiess@gmail.com>
2014-05-09 00:17:15 +0200

    Make HockeyApp adhere to naming reference

--- Casks/hockeyapp.rb
+++ Casks/hockeyapp.rb
@@ -1,4 +1,4 @@
-class HockeyApp < Cask
+class Hockeyapp < Cask
   url 'https://rink.hockeyapp.net/api/2/apps/67503a7926431872c4b6c1549f5bd6b1?format=zip'
   homepage 'http://hockeyapp.net/releases/mac/'
   version 'latest'
