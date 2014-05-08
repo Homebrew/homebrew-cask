ae8b13f820630e83fbe9e188cbfcb13decb2c41b
Vincent Klaiber <vincent@schimpanz.com>
2014-05-08 16:39:09 +0200

    Add Clear version 1.1
    Untangle your life with Clear - the to-do and reminders app that ensures you stay focused on what matters.

---
+++ Casks/clear.rb
@@ -0,0 +1,7 @@
+class Clear < Cask
+  url 'http://realmacsoftware.com/redirects/clear/direct'
+  homepage 'http://realmacsoftware.com/clear'
+  version 'latest'
+  no_checksum
+  link 'Clear.app'
+end
