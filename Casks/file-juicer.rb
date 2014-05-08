6e4d66db934a532d11636370907937157b4cc8ce
Matt Swain <m.swain@me.com>
2014-05-08 18:40:36 +0100

    Add File Juicer v4.36

---
+++ Casks/file-juicer.rb
@@ -0,0 +1,7 @@
+class FileJuicer < Cask
+  url 'http://echoone.com/filejuicer/FileJuicer-4.36.zip'
+  homepage 'http://echoone.com/filejuicer/'
+  version '4.36'
+  sha256 '8c3c94d6a7306974d18452f6fc5bb97cc46a94c8fd693f7184da0faa9f892040'
+  link 'File Juicer.app'
+end
