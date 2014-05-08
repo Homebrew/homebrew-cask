fcf8d50c49171324383a9400bc48639c2f6d51cf
jiska <jiska.ym+github@gmail.com>
2014-05-09 00:55:16 +0900

    Upgrade Trailer.app to version 1.0.26

--- Casks/trailer.rb
+++ Casks/trailer.rb
@@ -1,7 +1,7 @@
 class Trailer < Cask
-  url 'http://dev.housetrip.com/trailer/trailer1019.zip'
+  url 'http://dev.housetrip.com/trailer/trailer1026.zip'
   homepage 'http://dev.housetrip.com/trailer/'
-  version '1.0.19'
-  sha256 '651b211c0c678de0719a6535a3c2be390cfd0d100108d310410efecae9eafd5f'
+  version '1.0.26'
+  sha256 '2cad2a5af786429d6917b22cd131e7425054f3792a284620902dcb985308aab8'
   link 'Trailer.app'
 end
