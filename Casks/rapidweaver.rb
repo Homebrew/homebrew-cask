10da28594070772d829ce5987f93f29aa1501f66
Vítor Galvão <info@vitorgalvao.com>
2014-05-08 15:56:15 +0100

    Corrected a few caks with 'no_checksum' that slipped through
    Namely clear, idocument-plus, rapidweaver, and sparkbox

--- Casks/rapidweaver.rb
+++ Casks/rapidweaver.rb
@@ -2,6 +2,6 @@ class Rapidweaver < Cask
   url 'http://realmacsoftware.com/redirects/rapidweaver/direct'
   homepage 'http://realmacsoftware.com/rapidweaver'
   version 'latest'
-  no_checksum
+  sha256 :no_check
   link 'RapidWeaver.app'
 end
