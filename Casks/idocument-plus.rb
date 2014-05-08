35bd371177e2b8a3848cba16d6ae7f91ee4d2c67
Vincent Klaiber <vincent@schimpanz.com>
2014-05-08 15:32:11 +0200

    Add iDocument Plus version 2.6
    Browse All Documents in One Place.

---
+++ Casks/idocument-plus.rb
@@ -0,0 +1,7 @@
+class IdocumentPlus < Cask
+  url 'http://t.icyblaze.com/idpluslatest'
+  homepage 'http://www.icyblaze.com/idocument/'
+  version 'latest'
+  no_checksum
+  link 'iDocument Plus.app'
+end
