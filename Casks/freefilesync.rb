fab46d6c728382382eb3334ea4521db21390a548
Kevin Kaland <kevin@wizonesolutions.com>
2014-05-08 14:09:49 +0200

    Add FreeFileSync 6.5 for Mac.

---
+++ Casks/freefilesync.rb
@@ -0,0 +1,8 @@
+class Freefilesync < Cask
+  url 'http://www.fosshub.com/download/FreeFileSync_6.5_Mac_OS_X_64-bit.zip', :referer => 'http://www.fosshub.com/FreeFileSync.html'
+  homepage 'http://freefilesync.sourceforge.net/'
+  version '6.5'
+  sha256 'df00b3b719618894d495ed20b7cc8452fceb0c464111374a4d300c2a7da02cf7'
+  link 'FreeFileSync.app'
+  link 'RealtimeSync.app'
+end
