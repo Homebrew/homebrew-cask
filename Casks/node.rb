47361d2b9456c77a3746f86cf9193d892560d9c3
Waldinar Oliveira Neto <netoht@users.noreply.github.com>
2014-05-08 22:36:55 -0300

    Update node.rb

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,8 +1,8 @@
 class Node < Cask
-  url 'http://nodejs.org/dist/v0.10.26/node-v0.10.26.pkg'
+  url 'http://nodejs.org/dist/v0.10.28/node-v0.10.28.pkg'
   homepage 'http://nodejs.org'
-  version '0.10.26'
-  sha256 '99b718692a77195e3aafab819b42ad7dba7e90e05a9b6f078bc5171c041331f8'
-  install  'node-v0.10.26.pkg'
+  version '0.10.28'
+  sha256 '7785c80dcdaa948df58db62fb92deaac46bedfaa8bf7677b95614d151ea80c3d'
+  install  'node-v0.10.28.pkg'
   uninstall :pkgutil => 'org.nodejs'
 end
