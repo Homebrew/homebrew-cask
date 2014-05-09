fef341325d991e2dcf628a20f32785ac4b8bd7e4
Matt Swain <m.swain@me.com>
2014-05-09 13:01:58 +0100

    Add Latexian v1.2.4

---
+++ Casks/latexian.rb
@@ -0,0 +1,7 @@
+class Latexian < Cask
+  url 'http://tacosw.com/latexian/Latexian.zip'
+  homepage 'http://tacosw.com/latexian/'
+  version 'latest'
+  sha256 :no_check
+  link 'Latexian/Latexian.app'
+end
