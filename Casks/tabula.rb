491d3a9b3763a9221aea901976e5dd34438aded5
Matt Swain <m.swain@me.com>
2014-05-09 12:54:24 +0100

    Add Tabula v0.9.3

---
+++ Casks/tabula.rb
@@ -0,0 +1,7 @@
+class Tabula < Cask
+  url 'https://github.com/jazzido/tabula/releases/download/v0.9.3/tabula-mac-0.9.3d.zip'
+  homepage 'http://tabula.nerdpower.org'
+  version '0.9.3'
+  sha256 'fca11ca0f21e8ad42090ee7cc43aba356fa03b2877e8054af2ee39148c9f1cc9'
+  link 'tabula/Tabula.app'
+end
