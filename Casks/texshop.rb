b0ddb76f53051baad6d01981c88b9d8251731539
Federico Bond <federicobond@gmail.com>
2014-05-09 21:55:14 -0300

    Update TeXShop to 3.35

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,7 +1,7 @@
 class Texshop < Cask
-  url 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshop334.zip'
+  url 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshop335.zip'
   homepage 'http://pages.uoregon.edu/koch/texshop'
-  version '3.34'
-  sha256 '60f2d3962d12de5663292bd28942fc4191d553e48fa756ad5106bfd51acadbbd'
+  version '3.35'
+  sha256 '26fb20bb7c63abb2a59c334741e967899bf34b6cb47c29d7173d3d1c4538143f'
   link 'TexShop.app'
 end
