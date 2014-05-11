adb4972b9b59e4983aa3a0f46ac4351cff4c0326
Kevin Kaland <kevin@wizonesolutions.com>
2014-05-11 13:17:41 +0200

    Add Mountain 1.4.6_23.

---
+++ Casks/mountain.rb
@@ -0,0 +1,7 @@
+class Mountain < Cask
+  url 'http://appgineers.de/mountain/files/Mountain.zip'
+  homepage 'http://appgineers.de/mountain/'
+  version '1.4.6_23'
+  sha256 '86daf10772eeff5dc9a1f65fd97dc31e89a694075cdb84015a4f0d0e3f38f94c'
+  link 'Mountain.app'
+end
