d6e6c087e96a40a2fb1490477c347da66501a08f
lalyos <lalyos@yahoo.com>
2014-05-12 05:41:17 +0200

    Upgrade serf to v0.6.0

--- Casks/serf.rb
+++ Casks/serf.rb
@@ -1,7 +1,7 @@
 class Serf < Cask
-  url 'https://dl.bintray.com/mitchellh/serf/0.5.0_darwin_amd64.zip'
+  url 'https://dl.bintray.com/mitchellh/serf/0.6.0_darwin_amd64.zip'
   homepage 'http://www.serfdom.io/'
-  version '0.5.0'
-  sha256 'af445fbb4423da533ccee4c7c9bb4e0438339dbe65b3226d93af88065ecda0d0'
+  version '0.6.0'
+  sha256 '356f72abf85766b120fc2c2bc6438c13b91950d7ec932a36241551a347f80369'
   binary 'serf'
 end
