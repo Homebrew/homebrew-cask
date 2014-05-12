e9c1a9b809a6f138ff57a043e4b2c7ce99f6b51e
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-05-11 20:01:53 -0400

    Upgrade Vagrant Manager v1.3.2

--- Casks/vagrant-manager.rb
+++ Casks/vagrant-manager.rb
@@ -1,7 +1,7 @@
 class VagrantManager < Cask
-  url 'https://github.com/lanayotech/vagrant-manager/releases/download/1.3.1/vagrant-manager-1.3.1.dmg'
+  url 'https://github.com/lanayotech/vagrant-manager/releases/download/1.3.2/vagrant-manager-1.3.2.dmg'
   homepage 'http://vagrantmanager.com/'
-  version '1.3.1'
-  sha256 'd67a3eb56c482f4a0b3b332ff780fb1f37feffe5452072d05160c12dca8cafeb'
+  version '1.3.2'
+  sha256 '7788bfa70e2e1b73a384332403be83547284418b34482f7acb21a98c15fb1232'
   link 'Vagrant Manager.app'
 end
