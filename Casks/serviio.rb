fb4e23061ec73bb1eb6e9e6c20a9b9bb27ae9926
Arik Fraimovich <arik@arikfr.com>
2014-05-13 11:51:57 +0300

    Add Serviio v1.4.1.2

---
+++ Casks/serviio.rb
@@ -0,0 +1,8 @@
+class Serviio < Cask
+  url 'http://download.serviio.org/releases/serviio-1.4.1.2-osx.tar.gz'
+  homepage 'http://serviio.org/'
+  version '1.4.1.2'
+  sha256 '132ed6ba9baf466eec5d89789cd8d50163fa5b534899f10a9232e00f711707aa'
+  link 'Serviio.app'
+  link 'Serviio-Console.app'
+end
