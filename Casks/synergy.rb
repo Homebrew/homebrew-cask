dcee5ded75a43abadfc1522280fe3bb50c5e556a
Vítor Galvão <info@vitorgalvao.com>
2014-05-13 15:54:43 +0100

    updated synergy (1.4.17)

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,5 +1,5 @@
 class Synergy < Cask
-  url 'http://fossfiles.com/synergy/synergy-1.4.17-r2055-MacOSX109-x86_64.dmg'
+  url 'http://synergy-project.org/files/packages/synergy-1.4.17-r2055-MacOSX109-x86_64.dmg'
   homepage 'http://synergy-foss.org/'
   version '1.4.17'
   sha256 '492412f9ba7891ef09eed8d166bc3efea577a38931b796a0feab52730f7fc868'
