e5cea859ad6f5f239d860adc7a7a83ef687c6759
Mirco Macrelli <mmacrelli@gmail.com>
2014-05-13 19:14:17 +0200

    update mp4tools to version 3.4.2

--- Casks/mp4tools.rb
+++ Casks/mp4tools.rb
@@ -1,8 +1,8 @@
 class Mp4tools < Cask
-  url 'http://www.emmgunn.com/downloads/mp4tools3.4.1.zip',
+  url 'http://www.emmgunn.com/downloads/mp4tools3.4.2.zip',
       :referer => 'http://www.emmgunn.com/mp4tools/mp4toolsdownload.html'
   homepage 'http://www.emmgunn.com/mp4tools/mp4toolshome.html'
-  version '3.4.1'
-  sha256 '194a764eec87c846a5efed44f98795bcbaab7b4de60c8938597b1f6db03ff408'
-  link 'mp4tools3.4.1/MP4Tools.app'
+  version '3.4.2'
+  sha256 'dbaf78d8889e272da249b898900e7cb432a082059d3f041ba3d6f8834221f72a'
+  link 'mp4tools3.4.2/MP4Tools.app'
 end
