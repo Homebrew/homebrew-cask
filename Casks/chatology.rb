ed8b6d5033dad0c419f69097ac6dc086bd684628
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for chatology.rb

--- Casks/chatology.rb
+++ Casks/chatology.rb
@@ -1,5 +1,6 @@
 class Chatology < Cask
   url 'http://flexibits.com/chatology/download'
+  appcast 'https://flexibits.com/chatology/appcast.php'
   homepage 'http://flexibits.com/chatology'
   version 'latest'
   sha256 :no_check
