0da7ffdb8b007c4300837d92a6d8b066ba3192f4
Roland Walker <walker@pobox.com>
2014-05-14 09:40:04 -0400

    add appcast URL for versions.rb

--- Casks/versions.rb
+++ Casks/versions.rb
@@ -1,5 +1,6 @@
 class Versions < Cask
   url 'http://versionsapp.com/redirect/versionslatest'
+  appcast 'https://updates.blackpixel.com/updates?app=vs'
   homepage 'http://versionsapp.com/'
   version 'latest'
   sha256 :no_check
