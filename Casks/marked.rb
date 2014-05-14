05ade0b6557e382fcc1f46bcc7063ce335fccdc5
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for marked.rb

--- Casks/marked.rb
+++ Casks/marked.rb
@@ -1,5 +1,6 @@
 class Marked < Cask
   url 'http://marked2app.com/download/Marked.zip'
+  appcast 'http://abyss.designheresy.com/marked/marked.xml'
   homepage 'http://marked2app.com'
   version 'latest'
   sha256 :no_check
