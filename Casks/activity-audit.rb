0278402363af4591bd74d9acd5b29f9e5348ce94
Roland Walker <walker@pobox.com>
2014-05-14 09:39:58 -0400

    add appcast URL for activity-audit.rb

--- Casks/activity-audit.rb
+++ Casks/activity-audit.rb
@@ -1,5 +1,6 @@
 class ActivityAudit < Cask
   url 'https://www.dssw.co.uk/activityaudit/dsswactivityaudit.dmg'
+  appcast 'http://version.dssw.co.uk/activityaudit/standard'
   homepage 'https://www.dssw.co.uk/activityaudit'
   version 'latest'
   sha256 :no_check
