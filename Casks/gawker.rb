95f99a32b5421b308a7b9139793be1d61d1185ae
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for gawker.rb

--- Casks/gawker.rb
+++ Casks/gawker.rb
@@ -1,5 +1,6 @@
 class Gawker < Cask
   url 'http://sourceforge.net/projects/gawker/files/latest/download'
+  appcast 'http://gawker.sourceforge.net/appcast.xml'
   homepage 'http://gawker.sourceforge.net/Gawker.html'
   version 'latest'
   sha256 :no_check
