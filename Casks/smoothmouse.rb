863c226d0f2aab9cc29e291dd8b1f60b5885f827
Roland Walker <walker@pobox.com>
2014-05-14 08:46:51 -0400

    uninstall com.cyberic.smoothmouseupdater launchctl

--- Casks/smoothmouse.rb
+++ Casks/smoothmouse.rb
@@ -4,7 +4,10 @@ class Smoothmouse < Cask
   version 'latest'
   sha256 :no_check
   install 'SmoothMouse.pkg'
-  uninstall :launchctl => 'com.cyberic.smoothmouse',
+  uninstall :launchctl => [
+                           'com.cyberic.smoothmouse',
+                           'com.cyberic.smoothmouseupdater',
+                          ],
             :quit      => 'com.test.SmoothMouseDaemon',
             :kext      => 'com.cyberic.SmoothMouse',
             :pkgutil   => [
