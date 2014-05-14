c37419ccf963f0ef06a7e84fc6f97b7f9b155855
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for moom.rb

--- Casks/moom.rb
+++ Casks/moom.rb
@@ -1,5 +1,6 @@
 class Moom < Cask
   url 'http://manytricks.com/download/moom'
+  appcast 'http://manytricks.com/moom/appcast.xml'
   homepage 'http://manytricks.com/moom/'
   version 'latest'
   sha256 :no_check
