d9525954e566f756b414a7745f66694a1325aac3
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for snagit.rb

--- Casks/snagit.rb
+++ Casks/snagit.rb
@@ -1,5 +1,6 @@
 class Snagit < Cask
   url 'http://download.techsmith.com/snagitmac/enu/Snagit.dmg'
+  appcast 'http://techsmithredirect.appspot.com/'
   homepage 'http://www.techsmith.com/snagit.html'
   version 'latest'
   sha256 :no_check
