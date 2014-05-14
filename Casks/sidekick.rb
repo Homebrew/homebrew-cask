bfe509e8b343b60b45760b1e1ebd7519376929f2
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for sidekick.rb

--- Casks/sidekick.rb
+++ Casks/sidekick.rb
@@ -1,5 +1,6 @@
 class Sidekick < Cask
   url 'http://oomphalot.com/sidekick/release/Sidekick.zip'
+  appcast 'http://updates.oomphalot.com/?app=Sidekick'
   homepage 'http://oomphalot.com/sidekick/'
   version 'latest'
   sha256 :no_check
