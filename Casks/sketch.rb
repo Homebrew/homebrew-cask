b9ed55619625815db7ba5c3df55de92e3e102e65
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for sketch.rb

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -1,5 +1,6 @@
 class Sketch < Cask
   url 'http://bohemiancoding.com/static/download/sketch.zip'
+  appcast 'http://www.bohemiancoding.com/sketch/appcast.xml'
   homepage 'http://www.bohemiancoding.com/sketch/'
   version 'latest'
   sha256 :no_check
