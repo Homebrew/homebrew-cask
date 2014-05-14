265d11da62f4f35474b924156c35db8050ba2a86
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for name-mangler.rb

--- Casks/name-mangler.rb
+++ Casks/name-mangler.rb
@@ -1,5 +1,6 @@
 class NameMangler < Cask
   url 'http://manytricks.com/download/namemangler'
+  appcast 'http://manytricks.com/namemangler/appcast.xml'
   version 'latest'
   homepage 'http://manytricks.com/namemangler/'
   sha256 :no_check
