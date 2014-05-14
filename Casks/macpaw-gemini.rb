4286017f96761f1652fcb5adba759649823edd46
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for macpaw-gemini.rb

--- Casks/macpaw-gemini.rb
+++ Casks/macpaw-gemini.rb
@@ -1,5 +1,6 @@
 class MacpawGemini < Cask
   url 'http://dl.devmate.com/download/com.macpaw.site.Gemini/macpaw%20gemini.dmg'
+  appcast 'http://updates.devmate.com/com.macpaw.site.Gemini.xml'
   homepage 'http://macpaw.com/gemini'
   version 'latest'
   sha256 :no_check
