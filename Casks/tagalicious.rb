99ac12780b50ea8963eb318fb5692c35964e241e
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for tagalicious.rb

--- Casks/tagalicious.rb
+++ Casks/tagalicious.rb
@@ -1,5 +1,6 @@
 class Tagalicious < Cask
   url 'http://files.thelittleappfactory.com/tagalicious/Tagalicious.zip'
+  appcast 'https://files.thelittleappfactory.com/tagalicious/appcast.xml'
   homepage 'http://thelittleappfactory.com/tagalicious/'
   version 'latest'
   sha256 :no_check
