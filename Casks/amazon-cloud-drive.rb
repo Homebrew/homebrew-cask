1d99d6d002416141770e4820d2e30f546dc94167
Roland Walker <walker@pobox.com>
2014-05-14 09:39:58 -0400

    add appcast URL for amazon-cloud-drive.rb

--- Casks/amazon-cloud-drive.rb
+++ Casks/amazon-cloud-drive.rb
@@ -1,5 +1,6 @@
 class AmazonCloudDrive < Cask
   url 'https://d29x207vrinatv.cloudfront.net/AmazonCloudDrive.dmg'
+  appcast 'https://d29x207vrinatv.cloudfront.net/Sync/Mac/20130517-2.1/CloudDriveInstallerAppcast.xml'
   homepage 'https://www.amazon.com/clouddrive'
   version 'latest'
   sha256 :no_check
