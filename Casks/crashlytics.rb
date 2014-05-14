9c43f8ccf04acf8edb89c2766ff27ffabe58b985
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for crashlytics.rb

--- Casks/crashlytics.rb
+++ Casks/crashlytics.rb
@@ -1,5 +1,6 @@
 class Crashlytics < Cask
   url 'http://crashlytics.com/download/mac'
+  appcast 'https://ssl-download-crashlytics-com.s3.amazonaws.com/mac/version.xml'
   homepage 'http://crashlytics.com'
   version 'latest'
   sha256 :no_check
