53f8ff2f03d2cce4e7d03858dc50de86417a4761
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for hiss.rb

--- Casks/hiss.rb
+++ Casks/hiss.rb
@@ -1,5 +1,6 @@
 class Hiss < Cask
   url 'http://collect3.com.au/hiss/Hiss.zip'
+  appcast 'http://collect3.com.au/hiss/appcast.xml'
   homepage 'http://collect3.com.au/hiss/'
   version 'latest'
   sha256 :no_check
