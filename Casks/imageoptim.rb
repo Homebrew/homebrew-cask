b21a0ffdb552c25fe670166688151b788048a74f
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for imageoptim.rb

--- Casks/imageoptim.rb
+++ Casks/imageoptim.rb
@@ -1,5 +1,6 @@
 class Imageoptim < Cask
   url 'http://imageoptim.com/ImageOptim.tbz2'
+  appcast 'http://imageoptim.com/appcast.xml'
   homepage 'http://imageoptim.com/'
   version 'latest'
   sha256 :no_check
