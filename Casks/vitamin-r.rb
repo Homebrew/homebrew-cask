fa9494b581a753fb436601609a3f192471845255
Roland Walker <walker@pobox.com>
2014-05-14 09:40:04 -0400

    add appcast URL for vitamin-r.rb

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -1,5 +1,6 @@
 class VitaminR < Cask
   url 'http://www.publicspace.net/download/Vitamin.dmg'
+  appcast 'http://www.publicspace.net/app/vitamin2.xml'
   homepage 'http://www.publicspace.net/Vitamin-R/'
   version 'latest'
   sha256 :no_check
