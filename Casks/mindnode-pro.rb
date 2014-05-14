c478a9e9591f69ddc22502ed88cb3b8b284e8867
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for mindnode-pro.rb

--- Casks/mindnode-pro.rb
+++ Casks/mindnode-pro.rb
@@ -1,5 +1,6 @@
 class MindnodePro < Cask
   url 'http://www.mindnode.com/download/MindNodePro.zip'
+  appcast 'https://www.mindnode.com/softwareupdate/mindnodepro.xml'
   homepage 'https://mindnode.com/'
   version 'latest'
   sha256 :no_check
