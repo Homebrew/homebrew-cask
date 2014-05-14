6e2ad3dd9da817cb33c78655b85509b1fdf96a31
Roland Walker <walker@pobox.com>
2014-05-14 09:39:58 -0400

    add appcast URL for audio-editor.rb

--- Casks/audio-editor.rb
+++ Casks/audio-editor.rb
@@ -1,5 +1,6 @@
 class AudioEditor < Cask
   url 'http://www.macsome.com/AudioEditor.dmg'
+  appcast 'http://www.macsome.com/audio-editor-mac/su_feed.xml'
   homepage 'http://www.macsome.com/audio-editor-mac/index.html'
   version 'latest'
   sha256 :no_check
