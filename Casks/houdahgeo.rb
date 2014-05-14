1f403ae2b29e91dc0d1abc98270613b705953029
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for houdahgeo.rb

--- Casks/houdahgeo.rb
+++ Casks/houdahgeo.rb
@@ -1,5 +1,6 @@
 class Houdahgeo < Cask
   url 'http://houdah.com/houdahGeo/download_assets/HoudahGeo_latest.zip'
+  appcast 'http://www.houdah.com/houdahGeo/updates3/profileInfo.php'
   homepage 'http://houdah.com/houdahGeo/'
   version 'latest'
   sha256 :no_check
