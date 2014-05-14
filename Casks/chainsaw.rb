4ee9df674e1940bb2a9d71e739709e96b7396ee0
Vítor Galvão <info@vitorgalvao.com>
2014-05-14 01:06:32 +0100

    updated chainsaw (latest)

--- Casks/chainsaw.rb
+++ Casks/chainsaw.rb
@@ -1,7 +1,7 @@
 class Chainsaw < Cask
-  url 'http://people.apache.org/~sdeboy/apache-chainsaw-2.1.0-SNAPSHOT.dmg'
+  url 'http://logging.apache.org/chainsaw/webstart/chainsaw.dmg'
   homepage 'http://logging.apache.org/chainsaw/'
-  version '2.1.0'
-  sha256 'df69bae641b2f7e07f78135d6413755a43e673512045fa90c7660ed0cbf8ef1b'
+  version 'latest'
+  sha256 :no_check
   link 'Chainsaw.app'
 end
