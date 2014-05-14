7de131c98a6cccfd116b9b516f308c5c7c90834e
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for spotifree.rb

--- Casks/spotifree.rb
+++ Casks/spotifree.rb
@@ -1,5 +1,6 @@
 class Spotifree < Cask
   url 'http://spotifree.gordinskiy.com/files/Spotifree.dmg'
+  appcast 'http://spotifree.gordinskiy.com/appcast.xml'
   homepage 'http://spotifree.gordinskiy.com'
   version 'latest'
   sha256 :no_check
