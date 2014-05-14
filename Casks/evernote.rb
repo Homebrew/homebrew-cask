dc3e10d5e7fec261480cca3d6ac9939090361a59
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for evernote.rb

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -1,5 +1,6 @@
 class Evernote < Cask
   url 'https://www.evernote.com/about/download/get.php?file=EvernoteMac'
+  appcast 'http://update.evernote.com/public/ENMac/EvernoteMacUpdate.xml'
   homepage 'https://evernote.com/'
   version 'latest'
   sha256 :no_check
