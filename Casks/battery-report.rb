8772afbcd8d9c9ca36ccc90503d771c5b5b44759
Roland Walker <walker@pobox.com>
2014-05-14 09:39:58 -0400

    add appcast URL for battery-report.rb

--- Casks/battery-report.rb
+++ Casks/battery-report.rb
@@ -1,5 +1,6 @@
 class BatteryReport < Cask
   url 'https://www.dssw.co.uk/batteryreport/dsswbatteryreport.dmg'
+  appcast 'http://version.dssw.co.uk/batteryreport/standard'
   homepage 'https://www.dssw.co.uk/batteryreport'
   version 'latest'
   sha256 :no_check
