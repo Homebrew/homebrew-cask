58ca9cb5c3a1215a50c8163ca16feba3c648b7de
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for expandrive.rb

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,5 +1,6 @@
 class Expandrive < Cask
   url 'http://updates.expandrive.com/apps/expandrive/download_latest'
+  appcast 'http://updates.expandrive.com/appcast/expandrive.xml?version=3'
   homepage 'http://www.expandrive.com/expandrive'
   version 'latest'
   sha256 :no_check
