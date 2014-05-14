a15b4ae4ac126802d7a25a87cf7bdf63403e20a4
Roland Walker <walker@pobox.com>
2014-05-14 09:40:02 -0400

    add appcast URL for reflector.rb

--- Casks/reflector.rb
+++ Casks/reflector.rb
@@ -1,5 +1,6 @@
 class Reflector < Cask
   url 'http://download.airsquirrels.com/Reflector/Mac/Reflector.dmg'
+  appcast 'http://reflectorapp.com/updates/reflector.xml'
   homepage 'http://www.airsquirrels.com/reflector/'
   version 'latest'
   sha256 :no_check
