d50f161f0b365ab7b722c66f470d1684fdf26f52
Roland Walker <walker@pobox.com>
2014-05-14 09:40:02 -0400

    add appcast URL for pacifist.rb

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -1,5 +1,6 @@
 class Pacifist < Cask
   url 'http://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=dmg'
+  appcast 'http://www.charlessoft.com/cgi-bin/pacifist_sparkle.cgi'
   homepage 'http://www.charlessoft.com/'
   version 'latest'
   sha256 :no_check
