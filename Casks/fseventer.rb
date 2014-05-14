59382a8901c86091191660a260f3ac90d248bbcc
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for fseventer.rb

--- Casks/fseventer.rb
+++ Casks/fseventer.rb
@@ -1,5 +1,6 @@
 class Fseventer < Cask
   url 'http://www.fernlightning.com/lib/exe/fetch.php?id=software%3Afseventer%3Astart&cache=cache&media=software:fseventer:fseventer.zip'
+  appcast 'http://www.fernLightning.com/appcasts/fseventer.xml'
   homepage 'http://www.fernlightning.com/doku.php?id=software:fseventer:start'
   version 'latest'
   sha256 :no_check
