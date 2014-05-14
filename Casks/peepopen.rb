6c8d96def552ff2776b57aaeccdaa901b779c48d
Roland Walker <walker@pobox.com>
2014-05-14 09:40:02 -0400

    add appcast URL for peepopen.rb

--- Casks/peepopen.rb
+++ Casks/peepopen.rb
@@ -1,5 +1,6 @@
 class Peepopen < Cask
   url 'http://topfunky.github.io/PeepOpen/dl/PeepOpen.dmg'
+  appcast 'https://peepcode.com/system/apps/PeepOpen/appcast.xml'
   homepage 'http://topfunky.github.io/PeepOpen/'
   version 'latest'
   sha256 :no_check
