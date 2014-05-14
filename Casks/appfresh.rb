0ea7c443dec987d9c645eb2a8fff71baa33cbf04
Roland Walker <walker@pobox.com>
2014-05-14 09:39:58 -0400

    add appcast URL for appfresh.rb

--- Casks/appfresh.rb
+++ Casks/appfresh.rb
@@ -1,5 +1,6 @@
 class Appfresh < Cask
   url 'http://backend.metaquark.de/download/appfresh'
+  appcast 'http://backend.metaquark.de/appcast/appfresh.xml'
   homepage 'http://metaquark.de/appfresh/mac'
   version 'latest'
   sha256 :no_check
