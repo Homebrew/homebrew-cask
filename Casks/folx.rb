c7afbc3d9d0115d5715382c8db0fac386a99a547
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for folx.rb

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -1,5 +1,6 @@
 class Folx < Cask
   url 'http://mac.eltima.com/download/downloader_mac.dmg'
+  appcast 'http://mac.eltima.com/download/folx-update/folx3.xml'
   homepage 'http://mac.eltima.com/de/download-manager.html'
   version 'latest'
   sha256 :no_check
