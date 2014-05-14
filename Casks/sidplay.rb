8f74c9ec30beeb7d27acf701f4fa3ca7e73dacb1
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for sidplay.rb

--- Casks/sidplay.rb
+++ Casks/sidplay.rb
@@ -1,5 +1,6 @@
 class Sidplay < Cask
   url 'http://www.twinbirds.com/sidplay/SIDPLAY4.zip'
+  appcast 'http://www.sidmusic.org/sidplay/mac/sidplay_appcast.xml'
   homepage 'http://www.sidmusic.org/sidplay/mac/'
   version 'latest'
   sha256 :no_check
