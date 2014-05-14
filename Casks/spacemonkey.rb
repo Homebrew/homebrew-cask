7d3b055576f3c1d619a1df6f873c84c3375d5289
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for spacemonkey.rb

--- Casks/spacemonkey.rb
+++ Casks/spacemonkey.rb
@@ -1,5 +1,6 @@
 class Spacemonkey < Cask
   url 'http://downloads.spacemonkey.com/client/mac/latest'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/aa33b6780fdfc71247b2995fa47b5d7c'
   homepage 'https://www.spacemonkey.com'
   version 'latest'
   sha256 :no_check
