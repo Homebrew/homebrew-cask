0080dd81477d7eb1ef7dae467f8eddc638686616
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for soundcleod.rb

--- Casks/soundcleod.rb
+++ Casks/soundcleod.rb
@@ -1,5 +1,6 @@
 class Soundcleod < Cask
   url 'https://github.com/salomvary/soundcleod/raw/master/dist/SoundCleod.dmg'
+  appcast 'https://raw.github.com/salomvary/soundcleod/master/appcast.xml'
   homepage 'http://salomvary.github.io/soundcleod/'
   version 'latest'
   sha256 :no_check
