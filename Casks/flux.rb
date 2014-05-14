775f62e7b91a9f0d9513cf956391795776e6d162
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for flux.rb

--- Casks/flux.rb
+++ Casks/flux.rb
@@ -1,5 +1,6 @@
 class Flux < Cask
   url 'https://justgetflux.com/mac/Flux.zip'
+  appcast 'https://justgetflux.com/mac/macflux.xml'
   homepage 'http://justgetflux.com'
   version 'latest'
   sha256 :no_check
