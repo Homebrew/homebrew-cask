c32be852c7d04543326b03dc58d8d95ade570def
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for soulver.rb

--- Casks/soulver.rb
+++ Casks/soulver.rb
@@ -1,5 +1,6 @@
 class Soulver < Cask
   url 'http://www.acqualia.com/files/download.php?product=soulver'
+  appcast 'http://www.acqualia.com/soulver/appcast/soulver2.xml'
   homepage 'http://www.acqualia.com/soulver/'
   version 'latest'
   sha256 :no_check
