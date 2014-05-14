888a8bb4e307f6d4e6e0692646e91489cd43beae
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for chameleon-ssd-optimizer.rb

--- Casks/chameleon-ssd-optimizer.rb
+++ Casks/chameleon-ssd-optimizer.rb
@@ -1,5 +1,6 @@
 class ChameleonSsdOptimizer < Cask
   url 'http://chameleon.alessandroboschini.it/download.php'
+  appcast 'http://chameleon.alessandroboschini.it/sparkle/profileInfo.php'
   homepage 'http://chameleon.alessandroboschini.it/'
   version 'latest'
   sha256 :no_check
