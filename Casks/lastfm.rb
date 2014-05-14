a4f025000eeae6156c5adb8b981233f5699b709f
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for lastfm.rb

--- Casks/lastfm.rb
+++ Casks/lastfm.rb
@@ -1,5 +1,6 @@
 class Lastfm < Cask
   url 'http://www.lastfm.de/download/mac'
+  appcast 'http://cdn.last.fm/client/Mac/updates.xml'
   homepage 'http://www.lastfm.com'
   version 'latest'
   sha256 :no_check
