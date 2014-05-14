db4d487860d5ce4d8c53becf45de313844919782
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for doubanradio.rb

--- Casks/doubanradio.rb
+++ Casks/doubanradio.rb
@@ -1,5 +1,6 @@
 class Doubanradio < Cask
   url 'http://douban.fm/desktop_mac'
+  appcast 'http://douban.fm/static/radio/desktop/doubanradio-appcast.xml'
   homepage 'http://douban.fm'
   version 'latest'
   sha256 :no_check
