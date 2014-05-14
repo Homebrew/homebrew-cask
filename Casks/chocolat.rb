8491d6c8008e02b27ac37fe83f17f265b62b0138
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for chocolat.rb

--- Casks/chocolat.rb
+++ Casks/chocolat.rb
@@ -1,5 +1,6 @@
 class Chocolat < Cask
   url 'http://chocolatapp.com/download'
+  appcast 'http://chocolatapp.com/userspace/appcast/appcast_alpha.php'
   homepage 'http://chocolatapp.com/'
   version 'latest'
   sha256 :no_check
