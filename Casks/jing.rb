5c52fb7369738271b4ca6e6148c5c511a5190b17
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for jing.rb

--- Casks/jing.rb
+++ Casks/jing.rb
@@ -1,5 +1,6 @@
 class Jing < Cask
   url 'http://download.techsmith.com/jing/mac/jing.dmg'
+  appcast 'http://www.techsmith.com/redirect.asp?product=jing&ver=2.0.0&lang=enu&target=SparkleAppcast'
   homepage 'http://www.techsmith.com/jing.html'
   version 'latest'
   sha256 :no_check
