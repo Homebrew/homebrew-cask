b3912626ba1aef47acb4a0689d3af452883a8f9c
Roland Walker <walker@pobox.com>
2014-05-14 09:40:04 -0400

    add appcast URL for tubbler.rb

--- Casks/tubbler.rb
+++ Casks/tubbler.rb
@@ -1,5 +1,6 @@
 class Tubbler < Cask
   url 'http://www.celmaro.com/files/tubbler/Tubbler.zip'
+  appcast 'https://ssl.webpack.de/celmaro.com/updates/tubbler/tubbler.xml'
   homepage 'http://www.celmaro.com/tubbler'
   version 'latest'
   sha256 :no_check
