07849bfd2a88bb31e5dc96e90da49d41b1829894
Roland Walker <walker@pobox.com>
2014-05-14 09:10:50 -0400

    use doubledash on external cmd in consul.rb
    chmod

--- Casks/consul.rb
+++ Casks/consul.rb
@@ -5,6 +5,6 @@ class Consul < Cask
   sha256 '0a03a42fa3ea945d19152bc2429b4098a195a68f7a8f10a1b63e805f7f251fe9'
   binary 'consul'
   after_install do
-    system '/bin/chmod', '755', "#{destination_path}/consul"
+    system '/bin/chmod', '--', '755', "#{destination_path}/consul"
   end
 end
