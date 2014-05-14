2f6462ffb7c3be2256c50aec5f18e2abd829e508
Roland Walker <walker@pobox.com>
2014-05-14 09:40:04 -0400

    add appcast URL for tower.rb

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -1,5 +1,6 @@
 class Tower < Cask
   url 'http://www.git-tower.com/download'
+  appcast 'https://macapps.fournova.com/tower1-1060/updates.xml'
   homepage 'http://www.git-tower.com/'
   version 'latest'
   sha256 :no_check
