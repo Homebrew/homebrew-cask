c7f32420eb6504c9728908c3d2a34291bb3bd905
Roland Walker <walker@pobox.com>
2014-05-14 09:39:58 -0400

    add appcast URL for battery-guardian.rb

--- Casks/battery-guardian.rb
+++ Casks/battery-guardian.rb
@@ -1,5 +1,6 @@
 class BatteryGuardian < Cask
   url 'https://www.dssw.co.uk/batteryguardian/dsswbatteryguardian.dmg'
+  appcast 'http://version.dssw.co.uk/batteryguardian/standard'
   homepage 'https://www.dssw.co.uk/batteryguardian'
   version 'latest'
   sha256 :no_check
