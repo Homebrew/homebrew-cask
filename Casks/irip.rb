3bf2d4e2d74ce855e80ffd4adb9c5f10d46d8e8f
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for irip.rb

--- Casks/irip.rb
+++ Casks/irip.rb
@@ -1,5 +1,6 @@
 class Irip < Cask
   url 'http://files.thelittleappfactory.com/iRip2/iRip.zip'
+  appcast 'https://files.thelittleappfactory.com/iRip2/appcast.xml'
   version 'latest'
   homepage 'http://thelittleappfactory.com/irip/'
   sha256 :no_check
