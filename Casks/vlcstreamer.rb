865b423d1045eb184d8ae5bf9444c8b06049bc7e
Roland Walker <walker@pobox.com>
2014-05-14 09:40:04 -0400

    add appcast URL for vlcstreamer.rb

--- Casks/vlcstreamer.rb
+++ Casks/vlcstreamer.rb
@@ -1,5 +1,6 @@
 class Vlcstreamer < Cask
   url 'http://hobbyistsoftware.com/Downloads/VLCStreamer/latest-mac.php?cdn'
+  appcast 'http://hobbyistsoftware.com/Downloads/VLCStreamer/vlcStreamerVersions.xml'
   homepage 'http://hobbyistsoftware.com/vlcstreamer'
   version 'latest'
   sha256 :no_check
