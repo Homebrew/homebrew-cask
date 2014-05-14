32bf6bb9c293bc3e2da7bcd94dd4f6f174878d33
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for delibar.rb

--- Casks/delibar.rb
+++ Casks/delibar.rb
@@ -1,5 +1,6 @@
 class Delibar < Cask
   url 'http://static.shinyfrog.net/downloads/delibar/Delibar.zip'
+  appcast 'http://apps.shinynode.com/apps/delibar_appcast.xml'
   homepage 'http://www.delibarapp.com/'
   version 'latest'
   sha256 :no_check
