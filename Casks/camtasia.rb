e03ec61b6b69220268068c8f609277cb42062468
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for camtasia.rb

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,5 +1,6 @@
 class Camtasia < Cask
   url 'http://download.techsmith.com/camtasiamac/enu/Camtasia.dmg'
+  appcast 'http://techsmithredirect.appspot.com/cmac?target=sparkleappcast&product=camtasiamac&lang=enu&ver=2.7.1&os=mac&code=none'
   homepage 'http://www.techsmith.com/camtasia.html'
   version 'latest'
   sha256 :no_check
