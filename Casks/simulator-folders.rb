44d89479297df7b8554c767ed350ca7614fbad56
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for simulator-folders.rb

--- Casks/simulator-folders.rb
+++ Casks/simulator-folders.rb
@@ -1,5 +1,6 @@
 class SimulatorFolders < Cask
   url 'http://www.gettracktime.com/dbrd/download.php?id=8'
+  appcast 'http://www.gettracktime.com/dbrd/appcast.php?id=8'
   homepage 'http://nimbleworks.co.uk/blog/simulator-folders/'
   version 'latest'
   sha256 :no_check
