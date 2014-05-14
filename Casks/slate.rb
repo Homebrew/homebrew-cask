f38d98617db88436062215042ab07a8935ece8d8
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for slate.rb

--- Casks/slate.rb
+++ Casks/slate.rb
@@ -1,5 +1,6 @@
 class Slate < Cask
   url 'http://slate.ninjamonkeysoftware.com/Slate.dmg'
+  appcast 'https://www.ninjamonkeysoftware.com/slate/appcast.xml'
   homepage 'https://github.com/jigish/slate'
   version 'latest'
   sha256 :no_check
