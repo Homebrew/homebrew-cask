e694e399bc99ca46665dd637a76820713ad00858
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for moneymoney.rb

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,5 +1,6 @@
 class Moneymoney < Cask
   url 'http://moneymoney-app.com/download/MoneyMoney.zip'
+  appcast 'http://moneymoney-app.com/update/appcast.xml'
   homepage 'http://moneymoney-app.com/'
   version 'latest'
   sha256 :no_check
