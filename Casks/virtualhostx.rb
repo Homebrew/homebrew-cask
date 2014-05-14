f19100a3d5e4bd9528f189a780ca11d320596e7c
Roland Walker <walker@pobox.com>
2014-05-14 09:40:04 -0400

    add appcast URL for virtualhostx.rb

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -1,5 +1,6 @@
 class Virtualhostx < Cask
   url 'http://clickontyler.com/virtualhostx/download/'
+  appcast 'http://shine.clickontyler.com/appcast.php?id=23'
   homepage 'http://clickontyler.com/virtualhostx/'
   version 'latest'
   sha256 :no_check
