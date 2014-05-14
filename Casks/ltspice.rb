ebea6ec22dd4b34b6e29dca975fb45a5455e190f
Vítor Galvão <info@vitorgalvao.com>
2014-05-14 01:18:27 +0100

    updated ltspice (4)

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -2,6 +2,6 @@ class Ltspice < Cask
   url 'http://ltspice.linear-tech.com/LTspiceIV.dmg'
   homepage 'http://www.linear.com/designtools/software/'
   version '4'
-  sha256 '4722b3dcf28e2d1c2ef08119bbced6cb0ac5053d6bab7b46b52c2a0c352894c5'
+  sha256 'bb7414894b20bffda819560a9604c8c6218b9ca0cb72468ea6f82468f6cf3cb1'
   link 'LTspice.app'
 end
