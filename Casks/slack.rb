9ad8f41318126682c329ecfa6ec2eeb883e64a88
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for slack.rb

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,5 +1,6 @@
 class Slack < Cask
   url 'https://rink.hockeyapp.net/api/2/apps/89dc0a9e3d01fb65e383b13c9ce0d3ac?format=zip'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/89dc0a9e3d01fb65e383b13c9ce0d3ac'
   homepage 'http://slack.com'
   version 'latest'
   sha256 :no_check
