55f0da52b4b04ac6f28dbaac97a93620cf1aca74
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for netshade.rb

--- Casks/netshade.rb
+++ Casks/netshade.rb
@@ -1,5 +1,6 @@
 class Netshade < Cask
   url 'http://raynersoftware.com/downloads/NetShade.app.zip'
+  appcast 'https://secure.raynersw.com/appcast.php'
   homepage 'http://raynersoftware.com/netshade/'
   version 'latest'
   sha256 :no_check
