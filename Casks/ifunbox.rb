b377d78baaabd14a5044482fca3a80c9584a5728
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for ifunbox.rb

--- Casks/ifunbox.rb
+++ Casks/ifunbox.rb
@@ -1,5 +1,6 @@
 class Ifunbox < Cask
   url 'http://dl.i-funbox.com/mac/ifunboxmac.dmg'
+  appcast 'http://dl.i-funbox.com/updates/ifunbox.mac/update.xml'
   homepage 'http://www.i-funbox.com/'
   version 'latest'
   sha256 :no_check
