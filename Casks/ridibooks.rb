9ce624179d8b434063a78c71fa23d82bc48db4a2
Roland Walker <walker@pobox.com>
2014-05-14 09:12:49 -0400

    use doubledash on external cmds in ridibooks.rb
    mv

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -5,7 +5,7 @@ class Ridibooks < Cask
   sha256 :no_check
   container_type :naked
   before_install do
-    system '/bin/mv', destination_path.join('getapp'), destination_path.join('ridibooks.pkg')
+    system '/bin/mv', '--', destination_path.join('getapp'), destination_path.join('ridibooks.pkg')
   end
   install 'ridibooks.pkg'
   uninstall :pkgutil => 'com.ridibooks.Ridibooks'
