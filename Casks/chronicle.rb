cdd8a5f2723f331400dc079fa7589ede7e8b472e
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for chronicle.rb

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -1,5 +1,6 @@
 class Chronicle < Cask
   url 'http://chronicleapp.com/static/downloads/chronicle.zip'
+  appcast 'http://www.littlefin.com/downloads/chronicle3.xml'
   homepage 'http://chronicleapp.com/'
   version 'latest'
   sha256 :no_check
