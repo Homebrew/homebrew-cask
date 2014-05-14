e4f146e46f85ad3832b1fbca010db61543adb151
Roland Walker <walker@pobox.com>
2014-05-14 09:39:58 -0400

    add appcast URL for anxiety.rb

--- Casks/anxiety.rb
+++ Casks/anxiety.rb
@@ -1,5 +1,6 @@
 class Anxiety < Cask
   url 'http://www.anxietyapp.com/Anxiety.zip'
+  appcast 'http://www.anxietyapp.com/appcast/appcast.xml'
   homepage 'http://www.anxietyapp.com/'
   version 'latest'
   sha256 :no_check
