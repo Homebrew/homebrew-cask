d6364b42ea1927a0a77807fb752ebdd2c4a48bc9
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for loom.rb

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -1,5 +1,6 @@
 class Loom < Cask
   url 'https://loom.com/download/loom-mac.dmg'
+  appcast 'http://www.loom.com/download/macupdate_1.1.xml'
   homepage 'http://loom.com/'
   version 'latest'
   sha256 :no_check
