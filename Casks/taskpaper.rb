b1aef0484bfecdc04b6abf6b1a05e77b36b1dd2c
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for taskpaper.rb

--- Casks/taskpaper.rb
+++ Casks/taskpaper.rb
@@ -1,5 +1,6 @@
 class Taskpaper < Cask
   url 'http://taskpaper.s3.amazonaws.com/TaskPaper.dmg'
+  appcast 'http://www.hogbaysoftware.com/products/taskpaper/releases.rss'
   homepage 'http://www.hogbaysoftware.com/products/taskpaper'
   version 'latest'
   sha256 :no_check
