b8526939533f08c778c1cc3f9c04b90e09fa3779
Roland Walker <walker@pobox.com>
2014-05-14 09:39:58 -0400

    add appcast URL for base.rb

--- Casks/base.rb
+++ Casks/base.rb
@@ -1,5 +1,6 @@
 class Base < Cask
   url 'http://menial.co.uk/base/download/'
+  appcast 'http://update.menial.co.uk/software/base/'
   homepage 'http://menial.co.uk/base/'
   version 'latest'
   sha256 :no_check
