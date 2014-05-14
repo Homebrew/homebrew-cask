e3c4065f1237bb044b71dff42b0944c9f1891ec4
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for miro-video-converter.rb

--- Casks/miro-video-converter.rb
+++ Casks/miro-video-converter.rb
@@ -1,5 +1,6 @@
 class MiroVideoConverter < Cask
   url 'http://ftp.osuosl.org/pub/pculture.org/mirovideoconverter/mac/Miro%20Video%20Converter.dmg'
+  appcast 'http://miro-updates.participatoryculture.org/mvc-appcast-osx.xml'
   homepage 'http://www.mirovideoconverter.com/'
   version 'latest'
   sha256 :no_check
