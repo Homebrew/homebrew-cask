59b8483f8efbdba07e4ca287435943033be10058
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for objektiv.rb

--- Casks/objektiv.rb
+++ Casks/objektiv.rb
@@ -1,5 +1,6 @@
 class Objektiv < Cask
   url 'http://nthloop.com/objektiv/objektiv-latest.zip'
+  appcast 'http://nthloop.com/objektiv/appcast.xml'
   homepage 'http://nthloop.github.io/Objektiv/'
   version 'latest'
   sha256 :no_check
