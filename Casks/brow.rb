80f032ffa7e8f6015eefcbabe1a2c9900e9d3bb3
Sahn Lam <sahn@pobox.com>
2014-05-13 22:02:41 -0700

    Add Brow.app v1.0

---
+++ Casks/brow.rb
@@ -0,0 +1,7 @@
+class Brow < Cask
+  url 'http://www.timschroeder.net/brow/brow.zip'
+  homepage 'http://www.timschroeder.net/brow/'
+  version '1.0'
+  sha256 'f646b89e63853edab7dfae701f10074b0b966f624d2c1e412f0c07cdeeff2eeb'
+  link 'Brow.app'
+end
