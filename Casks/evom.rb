34ab8ba684fae20a88a07cfee40e601ac3f2483e
Roland Walker <walker@pobox.com>
2014-05-14 09:40:00 -0400

    add appcast URL for evom.rb

--- Casks/evom.rb
+++ Casks/evom.rb
@@ -1,5 +1,6 @@
 class Evom < Cask
   url 'http://files.thelittleappfactory.com/evom/Evom.zip'
+  appcast 'https://files.thelittleappfactory.com/evom/appcast.xml'
   homepage 'http://thelittleappfactory.com/evom/'
   version 'latest'
   sha256 :no_check
