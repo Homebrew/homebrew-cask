f6295ec6a08803a67517e027eaa8a69806c4e148
mAAdhaTTah <jamesorodig@gmail.com>
2014-05-14 15:53:15 -0400

    Add Google AdWords editor

---
+++ Casks/google-adwords-editor.rb
@@ -0,0 +1,7 @@
+class GoogleAdwordsEditor < Cask
+  url 'https://dl.google.com/adwords_editor/10.4.1/mac/en-US/AdWords-Editor-10.4.1-en-US.dmg'
+  homepage 'http://www.google.com/intl/en_US/adwordseditor/'
+  version '10.4.1'
+  sha1 '22e0198798d11e02a81146c62e3b8bb2a7cf494c'
+  install 'AdWords-Editor-10.4.1-en-US.pkg'
+end
