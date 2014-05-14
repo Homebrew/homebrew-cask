0f1f2d95b8ad52d0cb6e406f082a82cc81884642
Roland Walker <walker@pobox.com>
2014-05-14 09:40:02 -0400

    add appcast URL for optimal-layout.rb

--- Casks/optimal-layout.rb
+++ Casks/optimal-layout.rb
@@ -1,5 +1,6 @@
 class OptimalLayout < Cask
   url 'http://files.windowflow.com/OptimalLayout2.zip'
+  appcast 'http://most-advantageous.com/sparkle/OL-AppCast.cfm'
   homepage 'http://most-advantageous.com/optimal-layout/'
   version 'latest'
   sha256 :no_check
