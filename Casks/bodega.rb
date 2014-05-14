c188eae6d42ac6936fde674362fe2f4b557066de
Roland Walker <walker@pobox.com>
2014-05-14 09:39:59 -0400

    add appcast URL for bodega.rb

--- Casks/bodega.rb
+++ Casks/bodega.rb
@@ -1,5 +1,6 @@
 class Bodega < Cask
   url 'http://downloads.appbodega.com.s3.amazonaws.com/bodega/latest/Bodega.zip'
+  appcast 'https://dev.appbodega.com/sparkle/appcast'
   homepage 'http://appbodega.com/'
   version 'latest'
   sha256 :no_check
