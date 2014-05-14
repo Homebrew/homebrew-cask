569db4e5bed359a10b4e0778a9464bfcc8a254c4
Roland Walker <walker@pobox.com>
2014-05-14 09:40:03 -0400

    add appcast URL for sleep-monitor.rb

--- Casks/sleep-monitor.rb
+++ Casks/sleep-monitor.rb
@@ -1,5 +1,6 @@
 class SleepMonitor < Cask
   url 'https://www.dssw.co.uk/sleepmonitor/dsswsleepmonitor.dmg'
+  appcast 'http://version.dssw.co.uk/sleepmonitor/standard'
   homepage 'https://www.dssw.co.uk/sleepmonitor'
   version 'latest'
   sha256 :no_check
