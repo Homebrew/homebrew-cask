6b0ac50b9209373c653cadf106327f2019e391e1
Roland Walker <walker@pobox.com>
2014-05-14 09:40:02 -0400

    add appcast URL for prizmo.rb

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -1,5 +1,6 @@
 class Prizmo < Cask
   url 'http://www.creaceed.com/downloads/prizmo2.zip'
+  appcast 'http://www.creaceed.com/appcasts/prizmo2.xml'
   homepage 'http://www.creaceed.com/prizmo'
   version 'latest'
   sha256 :no_check
