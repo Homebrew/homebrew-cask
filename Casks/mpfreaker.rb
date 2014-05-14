6482c1b39d020206f89e9409356ee2495a6ea4af
Roland Walker <walker@pobox.com>
2014-05-14 09:40:01 -0400

    add appcast URL for mpfreaker.rb

--- Casks/mpfreaker.rb
+++ Casks/mpfreaker.rb
@@ -1,5 +1,6 @@
 class Mpfreaker < Cask
   url 'http://www.lairware.com/download/MPFreaker.dmg'
+  appcast 'http://lwupdate.dyndns.org/mpfreaker.xml'
   homepage 'http://www.lairware.com/mpfreaker/'
   version 'latest'
   sha256 :no_check
