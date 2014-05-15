331a00afb94a3795c267a64e16e2ced25b903b4e
Daniel Craig Jallits <dan@jallits.com>
2014-05-15 13:32:05 -0500

    Vagrant Manager v1.5.0

--- Casks/vagrant-manager.rb
+++ Casks/vagrant-manager.rb
@@ -1,7 +1,7 @@
 class VagrantManager < Cask
-  url 'https://github.com/lanayotech/vagrant-manager/releases/download/1.4.3/vagrant-manager-1.4.3.dmg'
+  url 'https://github.com/lanayotech/vagrant-manager/releases/download/1.5.0/vagrant-manager-1.5.0.dmg'
   homepage 'http://vagrantmanager.com/'
-  version '1.4.3'
-  sha256 'beebed12bc157b3e1fb49edb467ab84c1d247917b9a71770f141514080d07011'
+  version '1.5.0'
+  sha256 '18708df86a0a59e17d1c164ef5c423881b8bac1e34292a5b501ec4de55a81e56'
   link 'Vagrant Manager.app'
 end
