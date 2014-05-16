8c922d831b980c18e28b5cac5ff15cc9af249092
Vincent Klaiber <vincent@schimpanz.com>
2014-05-16 21:33:38 +0200

    Add imagemin version 0.0.1
    imagemin as an OS X, Linux and Windows image optimization app.

---
+++ Casks/imagemin.rb
@@ -0,0 +1,7 @@
+class Imagemin < Cask
+  url 'https://github.com/kevva/imagemin-app/releases/download/0.0.1/imagemin.app.zip'
+  homepage 'https://github.com/kevva/imagemin-app'
+  version '0.0.1'
+  sha256 '63f298564952b691567a1eeb36c0c472c194c2658e90ebca5feb30038d592ba4'
+  link 'imagemin.app'
+end
