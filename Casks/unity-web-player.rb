a46a53d845b93b60524f04dd58114287d360077f
Vítor Galvão <info@vitorgalvao.com>
2014-05-18 00:13:21 +0100

    moved sha256 line under version in unity-web-player
    For consistency with other casks.

--- Casks/unity-web-player.rb
+++ Casks/unity-web-player.rb
@@ -2,8 +2,8 @@ class UnityWebPlayer < Cask
   url 'http://webplayer.unity3d.com/download_webplayer-3.x/webplayer-mini.dmg'
   homepage 'https://unity3d.com/webplayer'
   version 'latest'
-  install 'Install Unity Web Player.pkg'
   sha256 :no_check
+  install 'Install Unity Web Player.pkg'
   uninstall :pkgutil => 'com.unity.UnityWebPlayer',
             :files   => '/Library/Internet Plug-Ins/Unity Web Player.plugin'
 end
