234dfea7da623444a32fc403be1b8c9b0bd7783a
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-05-17 18:00:33 -0400

    Update Mini Metro to Alpha 12a

--- Casks/mini-metro.rb
+++ Casks/mini-metro.rb
@@ -1,7 +1,7 @@
 class MiniMetro < Cask
-  url 'http://static.dinopoloclub.com/minimetro/builds/alpha12/MiniMetro-alpha12-osx.zip'
+  url 'http://static.dinopoloclub.com/minimetro/builds/alpha12/MiniMetro-alpha12a-osx.zip'
   homepage 'http://dinopoloclub.com/minimetro/'
-  version 'Alpha 12'
-  sha256 '0c81781fff8984f41276fe2c9c0a1da7bdfd18c8970fbe4b8c4ff2376936e7bd'
-  link 'MiniMetro-alpha12-osx.app', :target => 'Mini Metro.app'
+  version 'Alpha 12a'
+  sha256 'cbbf5e60c39bb4d850e65703e220d41a0410212fc6f87d997abe36dc2d7658f9'
+  link 'MiniMetro-alpha12a-osx.app', :target => 'Mini Metro.app'
 end
