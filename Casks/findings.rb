e311c2cab4d05f253865be494872968cbd90e847
Matt Swain <m.swain@me.com>
2014-05-20 15:31:55 +0100

    Add Findings.app v1.0

---
+++ Casks/findings.rb
@@ -0,0 +1,7 @@
+class Findings < Cask
+  url 'http://downloads.findingsapp.com/Findings_2133_1.0.zip'
+  homepage 'http://findingsapp.com'
+  version '1.0'
+  sha256 'f46d73e1f384d08136ef95c0d2881c422122d677c2bf78fe524e8c47aad98259'
+  link 'Findings.app'
+end
