2ec410c0db0345b94fd5aaa75e490228c3ec1ee5
Ian P. McCullough <ipmcc@pobox.com>
2014-05-21 11:34:24 -0400

    Update to current version of JetBrains WebStorm.

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -1,7 +1,7 @@
 class Webstorm < Cask
-  url 'http://download.jetbrains.com/webstorm/WebStorm-8.0.dmg'
+  url 'http://download.jetbrains.com/webstorm/WebStorm-8.0.2.dmg'
   homepage 'http://www.jetbrains.com/webstorm/'
-  version '8.0.0'
-  sha256 '39535d8d8ed4c6b61d28ddfe03775d3a0ec435df6520f0ea17831d6bbf9f8694'
+  version '8.0.2'
+  sha256 '4ba90cec20a7b115f840adc26892d76e71e049a65570af9fa5d0f54ba7caa9f8'
   link 'WebStorm.app'
 end
