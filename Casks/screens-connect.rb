166a7953d1968620165ac08594f9f020bd7ab5ec
Vítor Galvão <info@vitorgalvao.com>
2014-05-21 02:36:47 +0100

    added uninstall to screens-connect

--- Casks/screens-connect.rb
+++ Casks/screens-connect.rb
@@ -4,4 +4,9 @@ class ScreensConnect < Cask
   version 'latest'
   sha256 :no_check
   install 'Screens Connect.pkg'
+  uninstall :script => 'Uninstall Screens Connect.app/Contents/Resources/sc-uninstaller.tool'
+
+  before_uninstall do
+    system '/bin/chmod', '+x', "#{destination_path}/Uninstall Screens Connect.app/Contents/Resources/sc-uninstaller.tool"
+  end
 end
