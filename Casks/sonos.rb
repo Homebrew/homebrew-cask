60a3d827154b8b564f086b505b34f05bfb99340a
Dan Barber <hello@danbarber.me>
2014-05-21 21:22:50 +0100

    Update Sonos controller app to 5.0.

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,7 +1,7 @@
 class Sonos < Cask
-  url 'http://update.sonos.com/software/mac/mdcr/SonosDesktopController43.dmg'
+  url 'http://update.sonos.com/software/mac/mdcr/SonosDesktopController50.dmg'
   homepage 'http://www.sonos.com/'
-  version '4.3'
-  sha256 '4e8027da6efa047fefd39b234f9917503b0bd994093775da6f08ce9cf6faef4a'
+  version '5.0'
+  sha256 'cd38c3247f16580d2af3d2cb5a9a96ea3652e910bae4526957c05f4d7a9a62e2'
   link 'Sonos.app'
 end
