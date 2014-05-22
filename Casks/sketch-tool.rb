242b18cef4be5b47b1a78e9b14dc8b561420cb1b
Vítor Galvão <info@vitorgalvao.com>
2014-05-22 18:37:14 +0100

    sketch-tool was using 'no_checksum', instead of 'sha256 :no_check'

--- Casks/sketch-tool.rb
+++ Casks/sketch-tool.rb
@@ -2,7 +2,7 @@ class SketchTool < Cask
   url 'http://sketchtool.bohemiancoding.com/sketchtool-latest.zip'
   homepage 'http://bohemiancoding.com/sketch/tool/'
   version 'latest'
-  no_checksum
+  sha256 :no_check
   binary 'sketchtool/sketchtool'
   # Placing this bundle in the binary directory is contrary to Unix convention,
   # However the SketchTool README states:
