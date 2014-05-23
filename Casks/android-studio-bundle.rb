2b00bf7b1552f626676acee81a5871c2aa55faca
Zachary Waldowski <zach@waldowski.me>
2014-05-23 17:19:49 -0400

    Java 8 compatibility hack for Android Studio

--- Casks/android-studio-bundle.rb
+++ Casks/android-studio-bundle.rb
@@ -4,4 +4,12 @@ class AndroidStudioBundle < Cask
   version '0.5.2 build-135.1078000'
   sha256 'fbb0500af402c8fa5435dfac65e16101a70a22c4c8930f072db52ac41556fb8e'
   link 'Android Studio.app'
+  after_install do
+    java_ver = "1.6*"
+    if (!File.exists?("/System/Library/Java/JavaVirtualMachines/1.6.0.jdk") and !Dir.glob('/Library/Java/JavaVirtualMachines/jdk1.8.0_*.jdk').empty?)
+      java_ver = "1.8*"
+    end
+
+    system "/usr/libexec/PlistBuddy", "-c", "Set :JVMOptions:JVMVersion #{java_ver}", "#{destination_path}/Android Studio.app/Contents/Info.plist"
+  end
 end
--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -4,4 +4,12 @@ class AndroidStudio < Cask
   version '0.5.8 build-135.1155795'
   sha256 '5209b2a01d42d8102e31cbe1c41bb8554cd0617414c08bfc276b62f9bae11e40'
   link 'Android Studio.app'
+  after_install do
+    java_ver = "1.6*"
+    if (!File.exists?("/System/Library/Java/JavaVirtualMachines/1.6.0.jdk") and !Dir.glob('/Library/Java/JavaVirtualMachines/jdk1.8.0_*.jdk').empty?)
+      java_ver = "1.8*"
+    end
+
+    system "/usr/libexec/PlistBuddy", "-c", "Set :JVMOptions:JVMVersion #{java_ver}", "#{destination_path}/Android Studio.app/Contents/Info.plist"
+  end
 end
