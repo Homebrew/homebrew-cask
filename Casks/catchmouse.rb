7e4b016ab067ba25e75b3cf9b407365e422395df
Johannes Hertenstein <johannes@nimius.net>
2014-05-23 11:55:54 +0200

    Add CatchMouse.app
    New Application added: CatchMouse

---
+++ Casks/catchmouse.rb
@@ -0,0 +1,7 @@
+class Catchmouse < Cask
+  url 'http://ftnew.com/files/CatchMouse.zip'
+  homepage 'http://ftnew.com/catchmouse.html'
+  version 'latest'
+  sha256 :no_check
+  link 'CatchMouse.app'
+end
