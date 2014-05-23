7ae9ce4bf81752feab7e814ffac369910a12e521
Richard Lee <dlackty@gmail.com>
2014-05-23 12:58:28 +0800

    Add Blink1Control

---
+++ Casks/blink1control.rb
@@ -0,0 +1,7 @@
+class Blink1control < Cask
+  url 'http://thingm.com/blink1/downloads/Blink1Control-mac.zip'
+  homepage 'http://blink1.thingm.com/'
+  version 'latest'
+  sha256 :no_check
+  link 'Blink1Control.app'
+end
