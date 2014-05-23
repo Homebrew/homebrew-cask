aa3cb6b94810225a712a6e914f74ffd4f2610b88
Johannes Hertenstein <johannes@nimius.net>
2014-05-23 11:56:42 +0200

    New Application: "Validator SAC.app"
    Validator SAC: W3c Validator on your local machine

---
+++ Casks/validator_sac.rb
@@ -0,0 +1,7 @@
+class Validator_sac < Cask
+  url 'http://habilis.net/download/Validator-SAC_0.10.3.zip'
+  homepage 'http://habilis.net/validator-sac/'
+  version '0.10.3'
+  sha256 '9f2cfd64b9f16b53a411da41cab4ac5c827b6d5d6175889ddd7c14080171cfd9'
+  link 'Validator-SAC.app'
+end
