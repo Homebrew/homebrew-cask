0a8a7a41cd83848900fe9a6ccc87bbe911d8c361
Zachary Waldowski <zach@waldowski.me>
2014-05-23 17:19:33 -0400

    Java 8 compatibility hack for Jetbrains AppCode

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -4,4 +4,12 @@ class Appcode < Cask
   version '3.0'
   sha256 'd8e9e72fe1a9050148edb971c88c39c22adf9a1499f34dd8b29fce4e6308d35e'
   link 'AppCode.app'
+  after_install do
+    java_ver = "1.6*"
+    if (!File.exists?("/System/Library/Java/JavaVirtualMachines/1.6.0.jdk") and !Dir.glob('/Library/Java/JavaVirtualMachines/jdk1.8.0_*.jdk').empty?)
+      java_ver = "1.8*"
+    end
+
+    system "/usr/libexec/PlistBuddy", "-c", "Set :JVMOptions:JVMVersion #{java_ver}", "#{destination_path}/AppCode.app/Contents/Info.plist"
+  end
 end
