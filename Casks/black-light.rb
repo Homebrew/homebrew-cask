1bcde6b54e3e629ac4af56abc7beacf141218fef
Michael Ford <fanquake@users.noreply.github.com>
2014-05-24 12:19:04 +0800

    Black Light 2.0.1

--- Casks/black-light.rb
+++ Casks/black-light.rb
@@ -1,7 +1,7 @@
 class BlackLight < Cask
-  url 'http://littoral.michelf.ca/apps/black-light/black-light-2.0.zip'
+  url 'http://littoral.michelf.ca/apps/black-light/black-light-2.0.1.zip'
   homepage 'http://michelf.ca/projects/black-light'
-  version '2.0'
-  sha256 '9c9b77636c541089ad4828b33e571ac4134ee7e3a22fa87adc2c25e633c76fec'
+  version '2.0.1'
+  sha256 '6f358e319427ddaf91343a05ffb662894502902875bd9b0703b17baaf0c30d10'
   link 'Black Light.app'
 end
