e28e5d26da68be9432ddb72c9368891f80531485
Pan <aga.panlong@gmail.com>
2014-05-25 22:16:29 +0800

    Added new formulae for Scilab
    Scilab is an open source software for numerical computation, an alternative for matlab.

---
+++ Casks/scilab.rb
@@ -0,0 +1,7 @@
+class Scilab < Cask
+  url 'http://www.scilab.org/download/5.5.0/scilab-5.5.0-x86_64.dmg'
+  homepage 'https://www.scilab.org'
+  version '5.5.0'
+  sha256 :no_check
+  link 'Scilab.app'
+end
