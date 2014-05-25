9ed3172e3f86ea943ac4a8272671df8b913366f8
Koen Rouwhorst <koen@rwhrst.nl>
2014-05-25 21:04:06 +0200

    Upgrade Reeder.app to v2.0b7

--- Casks/reeder.rb
+++ Casks/reeder.rb
@@ -1,7 +1,7 @@
 class Reeder < Cask
-  url 'http://reederapp.com/beta-reeder-for-mac/Reeder2.0b5.zip'
+  url 'http://reederapp.com/beta-reeder-for-mac/Reeder2.0b7.zip'
   homepage 'http://reederapp.com/mac/'
-  version '2.0b5'
-  sha256 '3841689b9642bcd94268964d6afa43d364eb1795ba1292f4816e21b343eb499c'
+  version '2.0b7'
+  sha256 '6778fe2747d907e153666c01b3ee8700ed31b05e128c8433fb24f429a62dfdd2'
   link 'Reeder.app'
 end
