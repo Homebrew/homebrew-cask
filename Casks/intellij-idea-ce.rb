37e072b8ae21eedd7a0d12d38b4e07ac627381e7
Roland Walker <walker@pobox.com>
2014-05-26 10:01:32 -0400

    add Java version caveats to intellij-idea-ce.rb

--- Casks/intellij-idea-ce.rb
+++ Casks/intellij-idea-ce.rb
@@ -4,4 +4,17 @@ class IntellijIdeaCe < Cask
   version '13.1.2'
   sha256 '99422ba5faacd4d6aabf029b83cf1f9dd22fe445116b984df25d49ae8609e46e'
   link 'IntelliJ IDEA 13 CE.app'
+  caveats do
+    <<-EOS.undent
+    #{@cask} may require Java 7 (an older version) available from the
+    caskroom-versions repo via
+
+        brew cask install caskroom/versions/java7
+
+    Alternatively, #{@cask} can be modified to use Java 8 as described in
+
+        https://github.com/caskroom/homebrew-cask/issues/4500#issuecomment-43955932
+
+    EOS
+  end
 end
