cb1489275eadc364032289c37128037c3a8f1aca
Trevor Parker <trevor@trevorparker.com>
2014-05-27 19:57:27 -0400

    Upgrade Chromium to 35.0.1916.114
    Conflicts:
    	Casks/chromium.rb

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,7 +1,7 @@
 class Chromium < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_34.0.1847.137.dmg'
+  url 'http://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_35.0.1916.114.dmg'
   homepage 'http://www.freesmug.org/chromium'
-  version '34.0.1847.137'
-  sha256 '5fa1d5ca489329fc1c2e6d7ae5901da24e34fb3c69d37f18aeb1b5432ff7456d'
+  version '35.0.1916.114'
+  sha256 '7d87ec478cddf06a35d123b5a658f2733d15e8d90f444a8c70505a66655cf24d'
   link 'Chromium.app'
 end
