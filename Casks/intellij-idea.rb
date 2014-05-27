1d54f57938eef105c3f56593c2ac70dd356e80d7
Nathan Kleyn <nathan@nathankleyn.com>
2014-05-27 10:30:18 +0100

    Upgrade IntelliJ and IntelliJ CE to 13.1.3.

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,8 +1,8 @@
 class IntellijIdea < Cask
-  url 'http://download-cf.jetbrains.com/idea/ideaIU-13.1.2.dmg'
+  url 'http://download-cf.jetbrains.com/idea/ideaIU-13.1.3.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '13.1.2'
-  sha256 '7968149499106fb078d89fe41135bd1611cfcd124fc2992f55e1090da8f71365'
+  version '13.1.3'
+  sha256 'ae5239e0a5670dec88c39a5157103084d0ddd2c0b8d732d6af07764f0b98b624'
   link 'IntelliJ IDEA 13.app'
   caveats do
     <<-EOS.undent
