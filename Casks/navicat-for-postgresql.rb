b606289bb1984ba0e6bda1749e8f6816cdf93cd8
fengb <contact@fengb.info>
2014-05-28 12:57:20 -0500

    Bump navicat-for-postgresql to v11.0.18

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,7 +1,7 @@
 class NavicatForPostgresql < Cask
   url 'http://download.navicat.com/download/navicat110_pgsql_en.dmg'
   homepage 'http://www.navicat.com/products/navicat-for-postgresql'
-  version '11.0.17'
-  sha256 'bd4e21035fefe23ecbd3e5d3863c0797ef95f880756398ead6357581bd01a21e'
+  version '11.0.18'
+  sha256 'de6e44791b99d33c05da075b33d473a4e512297b17d1549fcda43a831b00ddac'
   link 'Navicat for PostgreSQL.app'
 end
