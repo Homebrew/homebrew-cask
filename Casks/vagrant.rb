7a3faf0aa87259d98f92cc8df24bb303b5bf0a44
Ngoc Pham <ngocphamm@users.noreply.github.com>
2014-05-29 17:03:48 -0400

    Fix hash mismatch with Vagrant
    A space was added. Sorry!

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -2,7 +2,7 @@ class Vagrant < Cask
   url 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.dmg'
   homepage 'http://www.vagrantup.com'
   version '1.6.3'
-  sha256 'ed1de41ca11a2251e4748a7a88fb46c3312780b7952f2248683f8ed2a007bc72 '
+  sha256 'ed1de41ca11a2251e4748a7a88fb46c3312780b7952f2248683f8ed2a007bc72'
   install 'Vagrant.pkg'
   uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] }
 end
