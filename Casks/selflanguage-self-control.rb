f7a7f987127d6cd7109ae1d8ccbee51e58b28d6a
Enrico Spinielli <enrico.spinielli@gmail.com>
2014-05-29 14:49:25 +0200

    Add Self "Mallard" 4.5.0 A prototype-based dynamic OO programming language, environment, and VM

---
+++ Casks/selflanguage-self-control.rb
@@ -0,0 +1,9 @@
+class SelflanguageSelfControl < Cask
+  url 'http://files.selflanguage.org/releases/4.5/Self-4.5.0.dmg'
+  homepage 'http://selflanguage.org/'
+  version '4.5.0'
+  sha256 'fa5edc50ec517c06547e8c57246075f1fcd4ad98b5b9c681b9a60e357f4d3f04'
+  link 'Self Control.app'
+  binary 'Clean.snap', :target => '/Users/Shared/Self/Clean.snap'
+  caveats 'A clean snapshot is available in /Users/Shared/Self/Clean.snap'
+end
