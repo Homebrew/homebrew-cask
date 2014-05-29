16567dc58b1bcce3fd5d8b77d87d83070bdbb2bd
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-05-29 08:07:13 +0300

    smartgithg: update to 6.0.0

--- Casks/smartgithg.rb
+++ Casks/smartgithg.rb
@@ -1,10 +1,10 @@
 class Smartgithg < Cask
-  url 'http://www.syntevo.com/download/smartgithg/smartgithg-macosx-5_0_8.dmg'
+  url 'http://www.syntevo.com/download/smartgithg/smartgithg-macosx-6_0_0.dmg'
   homepage 'http://www.syntevo.com'
-  version '5.0.8'
-  sha256 '3c445ada002d44bb07d1f5c3a4d3a96576d0f971b0b7e5ef6bf38054e1059b10'
-  link 'SmartGitHg 5.app'
-  binary 'SmartGitHg 5.app/Contents/MacOS/SmartGit'
+  version '6.0.0'
+  sha256 'c169e268335035e16b7f87b6e429a7587e89f717b6cad681c0167307d204694c'
+  link 'SmartGitHg 6.app'
+  binary 'SmartGitHg 6.app/Contents/MacOS/SmartGit'
   caveats do
     files_in_usr_local
   end
