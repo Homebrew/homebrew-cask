56339d3034813ed5feacc3410f360b216671e4f5
Josh Lieberman <josh.lieberman92@gmail.com>
2014-05-29 20:02:53 -0400

    fixed version and sha256 lines

--- Casks/sixtyforce.rb
+++ Casks/sixtyforce.rb
@@ -1,7 +1,7 @@
 class Sixtyforce < Cask
   url 'http://sixtyforce.com/download/sixtyforce.zip'
   homepage 'http://sixtyforce.com/'
-  version '0.9.8'
-  sha256 '747c61cea03591ec78b057caef7a125af8a859e40df9502c804883793fff9eb4'
+  version 'latest'
+  sha256 :no_check
   link 'sixtyforce.app'
 end
