6cfb9a0d8df65f68d515b05378fccd3f703bbf5e
Tolbkni Kao <tolbkni@gmail.com>
2014-06-01 07:41:25 +0800

    Upgrade Thunder.app to v2.6.2.1576

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -1,7 +1,7 @@
 class Thunder < Cask
-  url 'http://down.sandai.net/mac/thunder_dl2.6.2.1524_Beta.dmg'
+  url 'http://down.sandai.net/mac/thunder_dl2.6.3.1576_Beta.dmg'
   homepage 'http://mac.xunlei.com/'
-  version '2.6.2.1524'
-  sha256 '2289e531a36bc09ca8188fa6abc5e0e7adbe901cb439d63d50663daf44f87a64'
+  version '2.6.3.1576'
+  sha256 '0ba42722fc50bc40211d08c0816bca780c6115e04f5ce25f2389ad85c258aaca'
   link 'Thunder.app'
 end
