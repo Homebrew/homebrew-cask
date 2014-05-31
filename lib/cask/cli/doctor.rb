9fdc5b3fd94434b4ffb590be7fb5d557accdfdaa
Roland Walker <walker@pobox.com>
2014-05-31 07:58:22 -0400

    show Homebrew repo & origin in doctor
    references: #4581, #4571

--- lib/cask/cli/doctor.rb
+++ lib/cask/cli/doctor.rb
@@ -5,6 +5,7 @@ class Cask::CLI::Doctor
     alt_taps           = notfound_msg
     default_cask_count = notfound_msg
     privileged_uid     = notfound_msg
+    homebrew_origin    = notfound_msg
 
     begin
       privileged_uid = Process.euid == 0 ? "Yes #{Tty.red}(warning: not recommended)#{Tty.reset}" : 'No'
@@ -26,6 +27,21 @@ class Cask::CLI::Doctor
       default_cask_count = "0 #{Tty.red}(Error reading #{fq_default_tap})#{Tty.reset}"
     end
 
+    begin
+      HOMEBREW_REPOSITORY.cd do
+        homebrew_origin = Cask::SystemCommand.run('git',
+                                                  :args => %w{config --get remote.origin.url},
+                                                  :stderr => :silence).strip
+      end
+      if homebrew_origin !~ %r{\S}
+        homebrew_origin = "<NONE> #{Tty.red}(Error)#{Tty.reset}"
+      elsif homebrew_origin !~ %r{(mxcl|Homebrew)/homebrew(\.git)?\Z}
+        homebrew_origin.concat " #{Tty.red}(warning: nonstandard origin)#{Tty.reset}"
+      end
+    rescue StandardError
+      homebrew_origin = "#{Tty.red}Not Found - Error running git#{Tty.reset}"
+    end
+
     ohai 'OS X Version:',                                    MACOS_FULL_VERSION
     ohai "Hardware Architecture:",                           "#{Hardware::CPU.type}-#{Hardware::CPU.bits}"
     ohai 'Ruby Version:',                                    "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}"
@@ -33,6 +49,8 @@ class Cask::CLI::Doctor
     ohai 'Homebrew Version:',                                HOMEBREW_VERSION
     ohai 'Homebrew Executable Path:',                        HOMEBREW_BREW_FILE
     ohai 'Homebrew Cellar Path:',                            HOMEBREW_CELLAR
+    ohai 'Homebrew Repository Path:',                        HOMEBREW_REPOSITORY
+    ohai 'Homebrew Origin:',                                 homebrew_origin
     ohai 'Homebrew-cask Version:',                           HOMEBREW_CASK_VERSION
     ohai 'Homebrew-cask Default Tap Path:',                  fq_default_tap
     ohai 'Homebrew-cask Alternate Cask Taps:',               alt_taps
