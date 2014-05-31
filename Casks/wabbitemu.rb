55bd45aaa06e34e215d2d5cb1b1990f40630521c
Caleb Xu <calebcenter@live.com>
2014-05-31 08:09:03 -0400

    Added WabbitEmu

---
+++ Casks/wabbitemu.rb
@@ -0,0 +1,7 @@
+class Wabbitemu < Cask
+  url 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wabbit&DownloadId=187234&FileTime=130242671530600000&Build=20907'
+  homepage 'http://wabbit.codeplex.com'
+  version '2.1'
+  sha256 '23fc164767da10389eec858d022967fb58fdca1debaa5b9e022f2e9280a4f4e8'
+  link 'WabbitEmu.app'
+end
