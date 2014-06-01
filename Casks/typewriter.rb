7dddbda2144fcf3d6c8a5cdc5dcf848b11c7ccf3
Radek Simko <radek.simko@gmail.com>
2014-06-01 21:25:31 +0100

    Typewriter download URL updated

--- Casks/typewriter.rb
+++ Casks/typewriter.rb
@@ -1,5 +1,5 @@
 class Typewriter < Cask
-  url 'http://llllll.li/typewriter/Typewriter.zip'
+  url 'http://llllll.li/typewriter/download/Typewriter.zip'
   homepage 'http://llllll.li/typewriter'
   version 'latest'
   sha256 :no_check
