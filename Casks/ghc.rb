c6f09f4183c730d37b8cf6ca888ff9fa8770b785
ndr <ndr@qef.io>
2014-06-01 13:22:13 +0200

    Update GHC to 7.8.2-r5

--- Casks/ghc.rb
+++ Casks/ghc.rb
@@ -1,8 +1,8 @@
 class Ghc < Cask
-  url 'https://github.com/ghcformacosx/ghc-dot-app/releases/download/v7.8.2-r2/ghc-7.8.2-r2.zip'
+  url 'https://github.com/ghcformacosx/ghc-dot-app/releases/download/v7.8.2-r5/ghc-7.8.2-r5.zip'
   homepage 'http://ghcformacosx.github.io/'
-  version '7.8.2-r2'
-  sha256 '7e9de6d9d772f42ab0b31bc234de86ebe302d5bd3f0c057253377950e8020197'
+  version '7.8.2-r5'
+  sha256 '05f42227eae3905f19fe17cf11af469c3673df3fd48f1239ddfb2f2c9ee8e083'
   link 'ghc-7.8.2.app'
   caveats 'To add GHC to your PATH, launch the app and follow the included instructions.'
 end
