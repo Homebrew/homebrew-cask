d2b58aa3118054684665661a358faf86d2f28f09
Radek Simko <radek.simko@gmail.com>
2014-06-01 16:48:55 +0100

    Amazon Previewer cask made more generic

--- Casks/kindle-previewer.rb
+++ Casks/kindle-previewer.rb
@@ -1,7 +1,7 @@
 class KindlePreviewer < Cask
   url 'http://kindlepreviewer.s3.amazonaws.com/KindlePreviewer.zip'
   homepage 'http://www.amazon.com/gp/feature.html/?docId=1000765261'
-  version '2.92'
-  sha256 '6d156bcc29d9283b0326c35abfce34941d6c4b454cea6518a331c13fa0de17be'
+  version 'latest'
+  sha256 :no_check
   link 'Kindle Previewer.app'
 end
