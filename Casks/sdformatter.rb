7ac17abb816219fffef22a94811470e0718e9e5f
Josh Lieberman <josh.lieberman92@gmail.com>
2014-06-01 15:23:12 -0400

    added SDFormatter 4.0

---
+++ Casks/sdformatter.rb
@@ -0,0 +1,7 @@
+class Sdformatter < Cask
+  url 'https://www.sdcard.org/downloads/formatter_4/eula_mac/SDFormatter_4.00B.pkg'
+  homepage 'https://www.sdcard.org'
+  version '4.0'
+  sha256 '5b88a851d375ef471523a0f8371edda144c4650d40cfc94902440fece77aa27e'
+  install 'SDFormatter_4.00B.pkg'
+end
