4dae58fc205fe05d3bcc17bc86437851dcf645fb
Roland Walker <walker@pobox.com>
2014-06-02 14:14:58 -0400

    add appcast stanza for imagealpha.rb

--- Casks/imagealpha.rb
+++ Casks/imagealpha.rb
@@ -1,5 +1,6 @@
 class Imagealpha < Cask
   url 'http://pngmini.com/ImageAlpha1.3.5.tar.bz2'
+  appcast 'http://pngmini.com/appcast.xml'
   homepage 'http://pngmini.com/'
   version '1.3.5'
   sha256 'ba1ca1f50fa5b2a3472e6c2dbe410ecc3a66f425975cf9e39380e5c3421cca49'
