3ce49e9c508746efdaf97cca533074ff23dad3f7
Roland Walker <walker@pobox.com>
2014-06-02 13:38:03 -0400

    add appcast stanza for forklift.rb

--- Casks/forklift.rb
+++ Casks/forklift.rb
@@ -1,5 +1,6 @@
 class Forklift < Cask
   url 'http://download.binarynights.com/ForkLift2.5.9.zip'
+  appcast 'http://update.binarynights.com/ForkLift2/update.xml'
   homepage 'http://www.binarynights.com/'
   version '2.5.9'
   sha256 'cc5c171d0baa246ced8dd84ad85d00dcf4ab834249e888a636b529cbe8536342'
