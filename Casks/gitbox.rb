62da935e2c3f32d3e6ed7a4c02d6fb47f9fc5f3e
Roland Walker <walker@pobox.com>
2014-06-02 13:38:03 -0400

    add appcast stanza for gitbox.rb

--- Casks/gitbox.rb
+++ Casks/gitbox.rb
@@ -1,5 +1,6 @@
 class Gitbox < Cask
   url 'http://d1oa71y4zxyi0a.cloudfront.net/gitbox-1.6.2-ml.zip'
+  appcast 'http://gitboxapp.com/updates.xml'
   homepage 'http://gitboxapp.com/'
   version '1.6.2'
   sha256 '181ac55515221bf07c9994f25a11e962a9057e4249600b4d95424bcb6bb5c6d7'
