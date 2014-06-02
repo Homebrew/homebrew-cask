1da0aa0609abb61bf3b380ccc489d68ddd3587a2
Roland Walker <walker@pobox.com>
2014-06-02 13:38:03 -0400

    add appcast stanza for gitifier.rb

--- Casks/gitifier.rb
+++ Casks/gitifier.rb
@@ -1,5 +1,6 @@
 class Gitifier < Cask
   url 'http://github.com/downloads/jsuder/Gitifier/Gitifier-1.3.zip'
+  appcast 'http://sparkle.psionides.eu/feed/gitifier'
   homepage 'http://psionides.github.io/Gitifier/'
   version '1.3'
   sha256 'b199743bdae2305db99d9d3a247f9c8846031d1fbb19201ea81262b25e629d2f'
