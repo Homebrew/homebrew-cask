61f913e9e2fa5f0f48e8c133188032ab65ce01f8
Roland Walker <walker@pobox.com>
2014-06-02 13:38:02 -0400

    add appcast stanza for codekit.rb

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -1,5 +1,6 @@
 class Codekit < Cask
   url 'http://incident57.com/codekit/files/codekit-16895.zip'
+  appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml'
   homepage 'http://incident57.com/codekit/'
   version '2.0.3 (16895)'
   sha256 'd98ac91e4e44c833217f55b7dcb389f5730213bb7d801188b3823f695f00b741'
