0341c765f97d07223567c1cc8333f7b6a06720fe
Roland Walker <walker@pobox.com>
2014-06-02 13:38:01 -0400

    add appcast stanza for aurora.rb

--- Casks/aurora.rb
+++ Casks/aurora.rb
@@ -1,5 +1,6 @@
 class Aurora < Cask
   url 'https://www.oneperiodic.com/files/Aurora%20v5.0.3.zip'
+  appcast 'http://www.oneperiodic.com/aurora5.xml'
   homepage 'http://www.oneperiodic.com/products/aurora/'
   version '5.0.3'
   sha256 '4293c5ffdf6eb8e5d7031f02b1c83238472f486fe8a964f2f62ac71c72c22eb2'
