61aa6be9ee0584e087b87e6b95c9aaf864c26ccf
Vítor Galvão <info@vitorgalvao.com>
2014-06-02 14:19:48 +0100

    updated imitone (0.5.1b)

--- Casks/imitone.rb
+++ Casks/imitone.rb
@@ -1,7 +1,7 @@
 class Imitone < Cask
-  url 'http://imitone.com/beta/imitone-0.5.1a.dmg'
+  url 'http://imitone.com/beta/imitone-0.5.1b.dmg'
   homepage 'http://imitone.com/'
-  version '0.5.1a'
-  sha256 '3d1c4fa7b971455a3a0b6e3b118cd780f6e837d04b74f9b33180ea21684df487'
+  version '0.5.1b'
+  sha256 '55ed164bbe301d324625bbe780f2b3b429d68973b36493c0d741a4303f1270ae'
   link 'imitone.app'
 end
