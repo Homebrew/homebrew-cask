4e2b190639c8ff9bfe14489dfe7223129a724dcf
Roland Walker <walker@pobox.com>
2014-06-02 13:38:03 -0400

    add appcast stanza for hedgewars.rb

--- Casks/hedgewars.rb
+++ Casks/hedgewars.rb
@@ -1,5 +1,6 @@
 class Hedgewars < Cask
   url 'http://download.gna.org/hedgewars/Hedgewars-0.9.20-3.dmg'
+  appcast 'http://www.hedgewars.org/download/appcast.xml'
   homepage 'http://hedgewars.org'
   version '0.9.20'
   sha256 '167667f32f4798733b7bfd50423493067e6a9fd19f02f45e737b324038f0d559'
