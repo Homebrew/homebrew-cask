0de7c763f5e48b51936a8c44c0b4bb0f2a0852ee
Roland Walker <walker@pobox.com>
2014-06-02 14:14:58 -0400

    add appcast stanza for mplayer-osx-extended.rb

--- Casks/mplayer-osx-extended.rb
+++ Casks/mplayer-osx-extended.rb
@@ -1,5 +1,6 @@
 class MplayerOsxExtended < Cask
   url 'https://mplayerosxext.googlecode.com/files/MPlayer-OSX-Extended_rev15-test3.zip'
+  appcast 'http://mplayerosx.ch/updates.xml'
   homepage 'http://www.mplayerosx.ch/'
   version 'rev15-test3'
   sha256 '1543feec27fcc911e35b20a51a00957145f35b1b970b0cb35004c86727e4a8fe'
