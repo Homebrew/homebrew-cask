72a3ec60ad978c01ce10664d4e1754bc64de260b
Roland Walker <walker@pobox.com>
2014-06-02 14:14:57 -0400

    add appcast stanza for disk-drill.rb

--- Casks/disk-drill.rb
+++ Casks/disk-drill.rb
@@ -1,5 +1,6 @@
 class DiskDrill < Cask
   url 'http://dl.cleverfiles.com/diskdrill.dmg'
+  appcast 'http://www.cleverfiles.com/releases/auto-update/dd2-newestr.xml'
   homepage 'http://www.cleverfiles.com/'
   version 'latest'
   sha256 :no_check
