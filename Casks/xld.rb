4ceab75edb1bad4a3b354cf567908c707efff484
Roland Walker <walker@pobox.com>
2014-06-02 14:14:58 -0400

    add appcast stanza for xld.rb

--- Casks/xld.rb
+++ Casks/xld.rb
@@ -1,5 +1,6 @@
 class Xld < Cask
   url 'http://downloads.sourceforge.net/project/xld/xld-20140504.dmg'
+  appcast 'http://xld.googlecode.com/svn/appcast/xld-appcast_e.xml'
   homepage 'http://tmkk.undo.jp/xld/index_e.html'
   version '20140504'
   sha256 '48183d866d9b62f5a71d885d6391040b76a736081ce52c7247e0ba6208e82918'
