b5ded413af3f70b0476b14c3f17c73f5b169ab6e
Roland Walker <walker@pobox.com>
2014-06-02 14:14:58 -0400

    add appcast stanza for sourcetree.rb

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,5 +1,6 @@
 class Sourcetree < Cask
   url 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.9.3.1.dmg'
+  appcast 'http://www.sourcetreeapp.com/update/SparkleAppcast.xml'
   homepage 'http://www.sourcetreeapp.com/'
   version '1.9.3.1'
   sha256 '2a1556001c74fd0afa88eaef9927f35114ac1cf8489eca283ae94e42ad150397'
