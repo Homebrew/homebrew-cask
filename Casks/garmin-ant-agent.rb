029f8ef4d72a0e03ef2bf5c962b40b11c9457d46
Felix Kosmalla <felixkosmalla@gmail.com>
2014-06-03 23:48:08 +0200

    Added cask for Garmin ANT Agent

---
+++ Casks/garmin-ant-agent.rb
@@ -0,0 +1,9 @@
+class GarminAntAgent < Cask
+  url 'http://download.garmin.com/software/ANTAgentforMac_222.dmg'
+  homepage 'http://www8.garmin.com/support/download_details.jsp?id=4417'
+  version '2.2.2'
+  sha256 '11df82fdd8d39a9d83df03c8efe8b1b1cb3e8f65d1e1bd47f823e9eda9bfcc57'
+  install 'Install Garmin ANT Agent.pkg'
+  uninstall :quit => 'com.garmin.AntAgent',
+            :pkgutil => 'com.garmin.garminANTAgent.pkg'
+end
