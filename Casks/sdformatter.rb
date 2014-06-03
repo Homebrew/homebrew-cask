f3e54ca7a4ef529da54d36d020d4697e026f5ba9
Josh Lieberman <josh.lieberman92@gmail.com>
2014-06-03 15:46:31 -0400

    reformatted formula

--- Casks/sdformatter.rb
+++ Casks/sdformatter.rb
@@ -4,4 +4,5 @@ class Sdformatter < Cask
   version '4.0'
   sha256 '5b88a851d375ef471523a0f8371edda144c4650d40cfc94902440fece77aa27e'
   install 'SDFormatter_4.00B.pkg'
+  uninstall :pkgutil => 'jp.co.trendy.sdformatter.SDFormatter.pkg'
 end
