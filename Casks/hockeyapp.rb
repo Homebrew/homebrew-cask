1b94e620b7df89f7b1a755e0072a0bc00b5cfd16
Roland Walker <walker@pobox.com>
2014-06-04 07:51:01 -0400

    add appcast stanza for hockeyapp.rb

--- Casks/hockeyapp.rb
+++ Casks/hockeyapp.rb
@@ -1,5 +1,6 @@
 class Hockeyapp < Cask
   url 'https://rink.hockeyapp.net/api/2/apps/67503a7926431872c4b6c1549f5bd6b1?format=zip'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/67503a7926431872c4b6c1549f5bd6b1'
   homepage 'http://hockeyapp.net/releases/mac/'
   version 'latest'
   sha256 :no_check
