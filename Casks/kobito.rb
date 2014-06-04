fc8652a16c384ffa3236b8f75858d5c05c6402cf
Roland Walker <walker@pobox.com>
2014-06-04 07:51:02 -0400

    add appcast stanza for kobito.rb

--- Casks/kobito.rb
+++ Casks/kobito.rb
@@ -1,5 +1,6 @@
 class Kobito < Cask
   url 'http://kobito.qiita.com/download/Kobito_v1.9.1.zip'
+  appcast 'http://kobito.qiita.com/appcast.xml'
   homepage 'http://kobito.qiita.com/'
   version '1.9.1'
   sha256 '62fd1379ddd70a6dc2993aa945818c4dbb997bd06c2e6ecbbc90d3187861104c'
