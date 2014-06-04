51d6d14c9374e3aa5ddfccaa9bc070a277e83bf9
Roland Walker <walker@pobox.com>
2014-06-04 07:51:03 -0400

    add appcast stanza for lingrradar.rb

--- Casks/lingrradar.rb
+++ Casks/lingrradar.rb
@@ -1,5 +1,6 @@
 class Lingrradar < Cask
   url 'http://radar.lingr.com/mac/LingrRadar_2.26.tbz'
+  appcast 'http://radar.lingr.com/mac_appcast.xml'
   homepage 'http://radar.lingr.com/'
   version '2.26'
   sha256 'c1883e5bb0d31055a34651c1315dd5fdbdc5126dc493a84ea40cf518e4ff23b6'
