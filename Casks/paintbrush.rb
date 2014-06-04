958b4fbbe02630d7f364a90ebf3cd4cda9adb23c
Roland Walker <walker@pobox.com>
2014-06-04 07:51:04 -0400

    add appcast stanza for paintbrush.rb

--- Casks/paintbrush.rb
+++ Casks/paintbrush.rb
@@ -1,5 +1,6 @@
 class Paintbrush < Cask
   url 'http://downloads.sourceforge.net/project/paintbrush/Paintbrush%202.x/Paintbrush%202.1.1/Paintbrush-2.1.1.zip'
+  appcast 'http://paintbrush.sourceforge.net/updates2x.xml'
   homepage 'http://paintbrush.sourceforge.net/'
   version '2.1.1'
   sha256 '1832bfb62de7247c17c522b49a9c88e3613ba5686ff05abb9b7fb545003b577d'
