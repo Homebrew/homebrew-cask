8d94053d876b64c85c6aedaf9a4b124163aa5daa
Roland Walker <walker@pobox.com>
2014-06-04 07:51:03 -0400

    add appcast stanza for mavensmate.rb

--- Casks/mavensmate.rb
+++ Casks/mavensmate.rb
@@ -1,5 +1,6 @@
 class Mavensmate < Cask
   url 'http://push.mavensconsulting.netdna-cdn.com/mavensmate/builds/MavensMate.zip'
+  appcast 'http://mavensconsulting.com/mavensmate/builds/appcast.xml'
   homepage 'http://mavensmate.com'
   version 'latest'
   sha256 :no_check
