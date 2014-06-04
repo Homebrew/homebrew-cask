7466e43e3acee40c7385645f8bba7f29b0417297
Roland Walker <walker@pobox.com>
2014-06-04 07:51:03 -0400

    add appcast stanza for mixture.rb

--- Casks/mixture.rb
+++ Casks/mixture.rb
@@ -1,5 +1,6 @@
 class Mixture < Cask
   url 'https://s3.amazonaws.com/mixture-mixed-app/mac/Mixture.zip'
+  appcast 'https://s3.amazonaws.com/mixture-mixed-app/mac/appcast.xml'
   homepage 'http://mixture.io/'
   version 'latest'
   sha256 :no_check
