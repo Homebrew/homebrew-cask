745208b52a76ae56ff64a3ec1255847b40d302b0
Roland Walker <walker@pobox.com>
2014-06-04 07:51:04 -0400

    add appcast stanza for papers.rb

--- Casks/papers.rb
+++ Casks/papers.rb
@@ -1,5 +1,6 @@
 class Papers < Cask
   url 'http://papersapp.com/papers/download'
+  appcast 'http://www.papersapp.com/papers/appcast_v3.xml'
   homepage 'http://www.papersapp.com/papers/'
   version 'latest'
   sha256 :no_check
