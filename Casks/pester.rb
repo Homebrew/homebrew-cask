77ec68c79ab2a26e9a756e3b4e02a4f24beebd04
Roland Walker <walker@pobox.com>
2014-06-04 07:51:04 -0400

    add appcast stanza for pester.rb

--- Casks/pester.rb
+++ Casks/pester.rb
@@ -1,5 +1,6 @@
 class Pester < Cask
   url 'http://sabi.net/nriley/software/Pester-1.1b18.dmg'
+  appcast 'http://sabi.net/nriley/software/Pester/updates.xml'
   homepage 'http://sabi.net/nriley/software/index.html#pester'
   version '1.1b18'
   sha256 '50cddd6fa813e5e2935e0e66ea13ee0605ada711c0141498a4d19c93eb554c86'
