d7543690d8f0cb383af96d3d4d709654e007b5d2
Caleb Xu <calebcenter@live.com>
2014-06-04 15:04:15 -0400

    Add uninstall stanza for send-to-kindle

--- Casks/send-to-kindle.rb
+++ Casks/send-to-kindle.rb
@@ -4,4 +4,6 @@ class SendToKindle < Cask
   version '1.0.0.218'
   sha256 '9c3a9eca8fe9f09ff43dfb4132a8be163ef517008979075df3d947699c421494'
   install 'SendToKindleForMac-installer-v1.0.0.218.pkg'
+  uninstall :launchctl => 'com.amazon.sendtokindle.launcher',
+            :pkgutil   => 'com.amazon.SendToKindleMacInstaller.pkg'
 end
