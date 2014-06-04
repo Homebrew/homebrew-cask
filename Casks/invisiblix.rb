ab20c43f9f4c6d34cb08182db43661159a2062da
Roland Walker <walker@pobox.com>
2014-06-04 07:51:02 -0400

    add appcast stanza for invisiblix.rb

--- Casks/invisiblix.rb
+++ Casks/invisiblix.rb
@@ -1,5 +1,6 @@
 class Invisiblix < Cask
   url 'http://downloads.sourceforge.net/project/invisiblix/invisibliX-3.2.zip'
+  appcast 'http://www.read-write.fr/invisiblix/appcast.xml'
   homepage 'http://www.read-write.fr/invisiblix/'
   version '3.2'
   sha256 '528328a0e7c3f0a72e763ea766324d491dfe20d6f18a2882eefda1a5a2c4d68e'
