382dce944f5d29f68045a3bd518b6266f62a1f65
Roland Walker <walker@pobox.com>
2014-06-04 07:51:02 -0400

    add appcast stanza for ionu.rb

--- Casks/ionu.rb
+++ Casks/ionu.rb
@@ -1,5 +1,6 @@
 class Ionu < Cask
   url 'https://isis.ionu.nu/release/OSX/IONU.dmg'
+  appcast 'https://isis.ionu.nu/release/OSX/appcast.xml'
   homepage 'https://ionu.com'
   version 'latest'
   sha256 :no_check
