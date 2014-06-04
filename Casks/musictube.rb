2aa3034ac45e8c82d701dea34587599f3b43f9a3
Roland Walker <walker@pobox.com>
2014-06-04 07:51:04 -0400

    add appcast stanza for musictube.rb

--- Casks/musictube.rb
+++ Casks/musictube.rb
@@ -1,5 +1,6 @@
 class Musictube < Cask
   url 'http://flavio.tordini.org/files/musictube/musictube.dmg'
+  appcast 'http://flavio.tordini.org/musictube-ws/appcast.xml'
   homepage 'http://flavio.tordini.org/musictube'
   version 'latest'
   sha256 :no_check
