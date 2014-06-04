6673c24217a61ffbf6c309f54154511ec595f987
Roland Walker <walker@pobox.com>
2014-06-04 07:51:03 -0400

    add appcast stanza for majic.rb

--- Casks/majic.rb
+++ Casks/majic.rb
@@ -1,5 +1,6 @@
 class Majic < Cask
   url 'http://erikhinterbichler.com/blog/wp-content/uploads/software/Majic.zip'
+  appcast 'http://erikhinterbichler.com/blog/wp-content/uploads/updates/majic-update.xml'
   homepage 'http://erikhinterbichler.com/apps/majic/'
   version 'latest'
   sha256 :no_check
