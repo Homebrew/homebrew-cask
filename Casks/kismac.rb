44855583c14288014454e897c182da8dfec87a64
Roland Walker <walker@pobox.com>
2014-06-04 07:51:02 -0400

    add appcast stanza for kismac.rb

--- Casks/kismac.rb
+++ Casks/kismac.rb
@@ -1,5 +1,6 @@
 class Kismac < Cask
   url 'http://update.kismacmirror.com/binaries/KisMAC-0.3.3.dmg'
+  appcast 'http://update.kismac-ng.org/sparkle/profileInfo.php'
   homepage 'http://kismac-ng.org/'
   version '0.3.3'
   sha256 'd48f307c1c46a1d1a6b4465d653cf417e583bc09b7ea76d21183068066bc49bb'
