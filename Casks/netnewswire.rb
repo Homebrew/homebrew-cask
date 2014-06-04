0d0a3cf8066b4732b45fa92b8d15935cb847a08e
Roland Walker <walker@pobox.com>
2014-06-04 07:51:04 -0400

    add appcast stanza for netnewswire.rb

--- Casks/netnewswire.rb
+++ Casks/netnewswire.rb
@@ -1,5 +1,6 @@
 class Netnewswire < Cask
   url 'http://cdn.netnewswireapp.com/releases/NetNewsWire-4.0.0-143.zip'
+  appcast 'https://updates.blackpixel.com/updates?app=nnw'
   homepage 'http://netnewswireapp.com/'
   version '4.0.0-143'
   sha256 '68e8a4bd881d8bf7cc13ad859519400138b9e16bdb1d52af3a75511ebf4311c0'
