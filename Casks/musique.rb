e0e21cddd8b12009a973fd5a24d0ea5003de340c
Roland Walker <walker@pobox.com>
2014-06-04 07:51:04 -0400

    add appcast stanza for musique.rb

--- Casks/musique.rb
+++ Casks/musique.rb
@@ -1,5 +1,6 @@
 class Musique < Cask
   url 'http://flavio.tordini.org/files/musique/musique.dmg'
+  appcast 'http://flavio.tordini.org/musique-ws/appcast.xml'
   homepage 'http://flavio.tordini.org/musique'
   version 'latest'
   sha256 :no_check
