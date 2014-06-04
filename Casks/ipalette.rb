78e039d8d414522b2a821393da3b8ee8a7dec367
Roland Walker <walker@pobox.com>
2014-06-04 07:51:02 -0400

    add appcast stanza for ipalette.rb

--- Casks/ipalette.rb
+++ Casks/ipalette.rb
@@ -1,5 +1,6 @@
 class Ipalette < Cask
   url 'http://ipalette.info/downloads/iPalette-2.2.zip'
+  appcast 'http://ipalette.info/appcast.xml'
   homepage 'http://ipalette.info/'
   version '2.2'
   sha256 '1a2b56a3c9c3072e4857e8982d5197583463e3d745b8cdce44d7dec18a8d868d'
