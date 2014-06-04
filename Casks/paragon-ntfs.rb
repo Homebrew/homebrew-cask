bed30b48029fb3ce4c47ec574c9c5c4455ef97ed
Caleb Xu <calebcenter@live.com>
2014-06-04 15:43:04 -0400

    Add unisntall stanza for paragon-ntfs

--- Casks/paragon-ntfs.rb
+++ Casks/paragon-ntfs.rb
@@ -4,4 +4,11 @@ class ParagonNtfs < Cask
   version 'latest'
   sha256 :no_check
   install 'FSInstaller.app/Contents/Resources/Paragon NTFS for Mac OS X.pkg'
+  uninstall :pkgutil => 'com.paragon-software.filesystems.NTFS.pkg'
+  uninstall :files  => [
+                        '/System/Library/Extensions/ntfs.kext',
+                        '/System/Library/Filesystems/ntfs.fs',
+                        '/System/Library/Filesystems/ufsd.fs',
+                        '/System/Library/Filesystems/ufsd_NTFS.fs',
+                       ]	
 end
