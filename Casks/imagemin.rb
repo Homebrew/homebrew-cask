4d3f5128ecca52cb9707acc83f55d27a76ff39c0
Radek Simko <radek.simko@gmail.com>
2014-06-05 17:58:40 +0100

    Imagemin updated to 0.1.0

--- Casks/imagemin.rb
+++ Casks/imagemin.rb
@@ -1,7 +1,7 @@
 class Imagemin < Cask
-  url 'https://github.com/kevva/imagemin-app/releases/download/0.0.1/imagemin.app.zip'
+  url 'https://github.com/kevva/imagemin-app/releases/download/0.1.0/imagemin-app-v0.1.0-darwin.zip'
   homepage 'https://github.com/kevva/imagemin-app'
-  version '0.0.1'
-  sha256 '63f298564952b691567a1eeb36c0c472c194c2658e90ebca5feb30038d592ba4'
-  link 'imagemin.app'
+  version '0.1.0'
+  sha256 '8a4304d37eaa8a71fbeb550aece6a80c98dbcdf7a9fb6eb09faae1ad93df40d6'
+  link 'imagemin-app-v0.1.0-darwin/Atom.app', :target => 'imagemin.app'
 end
