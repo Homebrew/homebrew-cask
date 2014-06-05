c68a7476fe7b36badaa5bfd397cca093a39ab64e
Radek Simko <radek.simko@gmail.com>
2014-06-05 17:17:32 +0100

    Elm platform download URL & sha sum fixed

--- Casks/elm-platform.rb
+++ Casks/elm-platform.rb
@@ -1,5 +1,5 @@
 class ElmPlatform < Cask
-  url 'https://www.dropbox.com/s/qfz9n90jszcxa5q/Elm-Platform-0.12.3.pkg'
+  url 'https://dl.dropboxusercontent.com/s/qfz9n90jszcxa5q/Elm-Platform-0.12.3.pkg'
   homepage 'http://www.elm-lang.org'
   version '0.12.3'
   sha256 '155569928a868095b2b27e705f2904496918f28fcb59990ff4449ca4aace330f'
