c44dda0f00d0cce57f2b974aff3a369f2d980d55
Robbie Holmes <rholme14@its.jnj.com>
2014-06-05 16:43:34 -0500

    Upgrading YacReader to 7.0.2

--- Casks/yacreader.rb
+++ Casks/yacreader.rb
@@ -1,8 +1,8 @@
 class Yacreader < Cask
-  url 'https://bitbucket.org/luisangelsm/yacreader/downloads/YACReader-7.0.1-MacOSX-Intel.dmg'
+  url 'https://bitbucket.org/luisangelsm/yacreader/downloads/YACReader-7.0.2-MacOSX-Intel.dmg'
   homepage 'http://www.yacreader.com'
-  version '7.0.1'
-  sha256 'a8b042f0e8e3c00553f09621067a6a416743098b23277e438d2681b8a95ed890'
+  version '7.0.2'
+  sha256 '813c27b6effa62a17ab939a4734fb5f0245308e9e00cc907bb1210636e43e1d7'
   link 'YACReader.app'
   link 'YACReaderLibrary.app'
 end
