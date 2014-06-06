72e376e0ac9ad6b4c999dd035eaef285397f67e7
Caleb Xu <calebcenter@live.com>
2014-06-05 20:57:20 -0400

    Add HipHop 0.4.5

---
+++ Casks/hiphop.rb
@@ -0,0 +1,7 @@
+class Hiphop < Cask
+  url 'http://download.gethiphop.net/releases/0.4.5/mac/HipHop-0.4.5.dmg'
+  homepage 'http://gethiphop.net'
+  version '0.4.5'
+  sha256 '38ac0fa04fdc9d991fde8c5a683baa187c7c3b3e44e5d9399e887c126bae457e'
+  link 'HipHop.app'
+end
