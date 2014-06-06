e6a7efb1db1a6cee3c9c6d3338a03fabd24f3b2e
Radek Simko <radek.simko@gmail.com>
2014-06-06 15:58:03 +0100

    Lincastor updated to latest version

--- Casks/lincastor.rb
+++ Casks/lincastor.rb
@@ -1,7 +1,7 @@
 class Lincastor < Cask
   url 'https://dl.dropboxusercontent.com/u/7614970/LinCastor.zip'
   homepage 'http://onflapp.wordpress.com/lincastor/'
-  version '0.8.5'
-  sha256 'f3b9af3387229d81633c5f91e07d01c9f6d9c78013e3e2a8e22cd6148853ed17'
+  version 'latest'
+  sha256 :no_check
   link 'LinCastor.app'
 end
