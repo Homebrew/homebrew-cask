58c675c7ac0f949ee71adba36bd54596696d1c20
Xiaotian Ruan <MrOpen@outlook.com>
2014-06-06 18:04:14 +0800

    Upgrade Xiami.app to v1.3.1-1821

--- Casks/xiami.rb
+++ Casks/xiami.rb
@@ -1,7 +1,7 @@
 class Xiami < Cask
-  url 'http://dbison.alicdn.com/updates/xiami-1.1.6-1747.dmg'
+  url 'http://dbison.alicdn.com/updates/xiami-1.3.1-1821.dmg'
   homepage 'http://www.xiami.com/'
-  version '1.1.6-1747'
-  sha256 'ed077d7c90d2bc900f81623f54e5cce58f8154694403110e19886607e66e3541'
+  version '1.3.1-1821'
+  sha256 'c59a31f766e7a937543e2740d02cb152391508b5299e26573e395ffe214647a0'
   link 'Xiami.app'
 end
