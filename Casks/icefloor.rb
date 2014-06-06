57949509bf480b1adb37a20fe4cdd53a5a2c700e
Beng Hee Eu <beng@benghee.eu>
2014-06-06 11:35:54 +0800

    Icefloor 2.0.2

--- Casks/icefloor.rb
+++ Casks/icefloor.rb
@@ -1,8 +1,8 @@
 class Icefloor < Cask
-  url 'http://www.hanynet.com/icefloor-2.0.1.zip'
+  url 'http://www.hanynet.com/icefloor-2.0.2.zip'
   homepage 'http://www.hanynet.com/icefloor/'
-  version '2.0.1'
-  sha256 'f05eb90e28a99a547c59cf12d87201f7f6761307e25375d82cb14ade0417e6a8'
-  nested_container 'IceFloor 2.0.1.dmg'
+  version '2.0.2'
+  sha256 'b326bab20c022c12be9ed01c3aa0e5f072853ce7dc5fb756bddebc2c3abddf64'
+  nested_container 'IceFloor 2.0.2.dmg'
   link 'IceFloor.app'
 end
