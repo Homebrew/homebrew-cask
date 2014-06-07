5d063d2944aa4d997737a71484883f83c6147141
Hugo Haas <hugoh@hugoh.net>
2014-06-07 12:15:20 -0700

    Add EasyTAG alpha5

---
+++ Casks/easytag.rb
@@ -0,0 +1,7 @@
+class Easytag < Cask
+  url 'http://marcoschuh.de/wp/static/d.php?file=EasyTAG_alpha5.dmg'
+  homepage 'http://marcoschuh.de/wp/?page_id=411'
+  version 'alpha5'
+  sha256 '21c68dfd229d42bc31acb20de1c547574123db77a396159363f4b48c0b771057'
+  link 'EasyTAG.app'
+end
