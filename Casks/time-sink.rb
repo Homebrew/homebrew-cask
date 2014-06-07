26bd910f8056fd267f6141fb7bba2ec2da198ced
Hugo Haas <hugoh@hugoh.net>
2014-06-07 14:15:22 -0700

    Add Time Sink 1.2.2 Add new formula Note that there is a direct link to version 1.2.2: http://manytricks.com/download/_do_not_hotlink_/timesink122.dmg However, given the path, I avoided it

---
+++ Casks/time-sink.rb
@@ -0,0 +1,7 @@
+class TimeSink < Cask
+  url 'http://manytricks.com/download/timesink'
+  homepage 'http://manytricks.com/timesink/'
+  version '1.2.2'
+  sha256 '9fc5d5a3934782ddffbcac645098300d98fbf289ef19a2cb5d0f522aeeb6369a'
+  link 'Time Sink.App'
+end
