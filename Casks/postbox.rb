b74a2636240d95b4edc71277e2f80fdb9359e1b1
Radek Simko <radek.simko@gmail.com>
2014-06-07 13:41:17 +0100

    Postbox updated to latest version

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,7 +1,7 @@
 class Postbox < Cask
-  url 'http://www.postbox-inc.com/php/download.php?a=3010m'
+  url 'http://www.postbox-inc.com/php/download.php?a=3011m'
   homepage 'http://www.postbox-inc.com/'
-  version '3.0.10'
-  sha256 '2c4ccb1fe34edc7b680516d6fb998b67a8e0d1c972d5d5d91a0ef86d5c0a2461'
+  version 'latest'
+  sha256 :no_check
   link 'Postbox.app'
 end
