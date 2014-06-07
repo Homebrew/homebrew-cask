20f4125b73bd4638e7ec83e23a461479fa583143
Radek Simko <radek.simko@gmail.com>
2014-06-07 13:04:11 +0100

    Mountain appcast URL added

--- Casks/mountain.rb
+++ Casks/mountain.rb
@@ -1,5 +1,6 @@
 class Mountain < Cask
   url 'http://appgineers.de/mountain/files/Mountain.zip'
+  appcast 'http://appgineers.de/mountain/files/mountaincast.xml'
   homepage 'http://appgineers.de/mountain/'
   version 'latest'
   sha256 :no_check
