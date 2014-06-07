f2d5e36b8589c6e439faa929aef186b21df947a6
Radek Simko <radek.simko@gmail.com>
2014-06-07 11:29:07 +0100

    LTSpice cask made more generic

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -1,7 +1,7 @@
 class Ltspice < Cask
   url 'http://ltspice.linear-tech.com/LTspiceIV.dmg'
   homepage 'http://www.linear.com/designtools/software/'
-  version '4'
-  sha256 'bb7414894b20bffda819560a9604c8c6218b9ca0cb72468ea6f82468f6cf3cb1'
+  version 'latest'
+  sha256 :no_check
   link 'LTspice.app'
 end
