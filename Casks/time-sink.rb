b19262071879e5bf12be8bb0b5daa8e09fa079c9
Hugo Haas <hugoh@hugoh.net>
2014-06-07 16:22:22 -0700

    Using latest version as per guidelines

--- Casks/time-sink.rb
+++ Casks/time-sink.rb
@@ -1,7 +1,7 @@
 class TimeSink < Cask
   url 'http://manytricks.com/download/timesink'
   homepage 'http://manytricks.com/timesink/'
-  version '1.2.2'
-  sha256 '9fc5d5a3934782ddffbcac645098300d98fbf289ef19a2cb5d0f522aeeb6369a'
+  version 'latest'
+  sha256 :no_check
   link 'Time Sink.App'
 end
