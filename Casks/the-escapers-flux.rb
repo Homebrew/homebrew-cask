b8af8709f77a1adffb13ce03bac2350a99d7a971
Roland Walker <walker@pobox.com>
2014-06-09 10:41:38 -0400

    add appcast stanza for the-escapers-flux.rb

--- Casks/the-escapers-flux.rb
+++ Casks/the-escapers-flux.rb
@@ -1,5 +1,6 @@
 class TheEscapersFlux < Cask
   url 'http://instruktion.net/theescapers/downloads/FluxV4.zip'
+  appcast 'http://www.theescapers.com/flux/flux.xml'
   homepage 'http://www.theescapers.com/flux/'
   version 'latest'
   sha256 :no_check
