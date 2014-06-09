8c0d068e16b4cc6b29e1d9b81a70ad3d233e2c20
Roland Walker <walker@pobox.com>
2014-06-09 10:41:39 -0400

    add appcast stanza for videomonkey.rb

--- Casks/videomonkey.rb
+++ Casks/videomonkey.rb
@@ -1,5 +1,6 @@
 class Videomonkey < Cask
   url 'http://videomonkey.org/download/videomonkey-0.16.zip'
+  appcast 'http://videomonkey.org/releases/videomonkeycast.xml'
   homepage 'http://videomonkey.org/'
   version '0.16'
   sha256 'ab09a3bb98a78d32ae48c55b6572e4d1e10ead0f26d5ba895a9a2ee5c105088c'
