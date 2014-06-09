9077de8efcfc7bb0909c864d618b2851ecd9b2bd
Roland Walker <walker@pobox.com>
2014-06-09 11:29:34 -0400

    add appcast stanza for macupdate-desktop.rb

--- Casks/macupdate-desktop.rb
+++ Casks/macupdate-desktop.rb
@@ -1,5 +1,6 @@
 class MacupdateDesktop < Cask
   url 'https://www.macupdate.com/download/8544/MacUpdate-Desktop-5.2.3.dmg'
+  appcast 'https://www.macupdate.com/desktop/updates.xml'
   homepage 'https://www.macupdate.com/desktop'
   version '5.2.3'
   sha256 'd636dc76d1e4573b885f1b65aa5cdad3435ba3cc15387c4b3100f7045e75dfa8'
