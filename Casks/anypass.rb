9ae30af634729ade84285363ae32e1c1fd121e51
Vincent Klaiber <vincent@schimpanz.com>
2014-06-09 08:13:45 +0200

    Remove www from URL for Anypass.

--- Casks/anypass.rb
+++ Casks/anypass.rb
@@ -1,5 +1,5 @@
 class Anypass < Cask
-  url 'http://www.icyblaze.com/anypass/anypass_mac_1.0.zip'
+  url 'http://icyblaze.com/anypass/anypass_mac_1.0.zip'
   homepage 'http://www.icyblaze.com/anypass'
   version '1.0.0'
   sha256 '364d108736eddfdb6e7db56430806d790b79e9f8561aedc544fc78d7e41bac54'
