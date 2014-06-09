8b36f02a72b60c28233d06225a0b45b2a1ddfaf0
Semenyuk Dmitriy <git@semenyukdmitriy.com>
2014-06-09 13:19:28 +0600

    Google Chrome Canary

---
+++ Casks/google-chrome-canary.rb
@@ -0,0 +1,7 @@
+class GoogleChromeCanary < Cask
+  url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
+  homepage 'http://www.google.com/intl/en/chrome/browser/canary.html'
+  version 'latest'
+  sha256 :no_check
+  link 'Google Chrome Canary.app'
+end
