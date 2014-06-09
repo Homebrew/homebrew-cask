fc8d7967b5274d4311710234d6287e2217a091eb
Roland Walker <walker@pobox.com>
2014-06-09 10:41:40 -0400

    add appcast stanza for yemuzip.rb

--- Casks/yemuzip.rb
+++ Casks/yemuzip.rb
@@ -1,5 +1,6 @@
 class Yemuzip < Cask
   url 'http://www.yellowmug.com/download/YemuZip.dmg'
+  appcast 'http://yellowmug.com/yemuzip/appcast.xml'
   homepage 'http://www.yellowmug.com/yemuzip'
   version 'latest'
   sha256 :no_check
