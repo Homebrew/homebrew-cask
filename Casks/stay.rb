2ac8cc259b1658c98a22ee9ee6a51157947e265f
Roland Walker <walker@pobox.com>
2014-06-09 10:41:38 -0400

    add appcast stanza for stay.rb

--- Casks/stay.rb
+++ Casks/stay.rb
@@ -1,5 +1,6 @@
 class Stay < Cask
   url 'http://cordlessdog.com/stay/versions/Stay%201.2.3.zip'
+  appcast 'http://cordlessdog.com/stay/appcast.xml'
   homepage 'http://cordlessdog.com/stay/'
   version '1.2.3'
   sha256 'afe5abc2757bcee547a0d9ad8ebae55744ac197bc28c9fa28fc7987b9b512464'
