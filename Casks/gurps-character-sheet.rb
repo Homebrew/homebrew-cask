c502e08fe92a92bf59cb3a7e19ed7af877bc3dc6
David Baumgold <david@davidbaumgold.com>
2014-06-09 17:55:26 -0400

    GURPS Character Sheet version 4.0.0

--- Casks/gurps-character-sheet.rb
+++ Casks/gurps-character-sheet.rb
@@ -1,7 +1,7 @@
 class GurpsCharacterSheet < Cask
-  url 'http://downloads.sourceforge.net/project/gcs-java/GCS-2011.01.02.1054.zip'
+  url 'http://downloads.sourceforge.net/project/gcs-java/gcs-4.0.0-mac.zip'
   homepage 'http://gurpscharactersheet.com'
-  version '2011.01.02.1054'
-  sha256 'd0fff0f9fb1cae790747309a312e2ebda48f56d2d272f99ea7a5ea0a72f9351b'
-  link 'GCS-2011.01.02.1054/GURPS Character Sheet.app'
+  version '4.0.0'
+  sha256 '67ccecc44d28f8cf104094ae988779b8219f79a6e6fbe9c695828034d3307816'
+  link 'gcs-4.0.0-mac/GURPS Character Sheet.app'
 end
