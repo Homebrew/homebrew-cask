d76e127aa1b8415a0200bc67ff8354dddbf06c84
Roland Walker <walker@pobox.com>
2014-06-09 10:41:38 -0400

    add appcast stanza for textwrangler.rb

--- Casks/textwrangler.rb
+++ Casks/textwrangler.rb
@@ -1,5 +1,6 @@
 class Textwrangler < Cask
   url 'http://ash.barebones.com/TextWrangler_4.5.8.dmg'
+  appcast 'https://versioncheck.barebones.com/TextWrangler.xml'
   homepage 'http://www.barebones.com/products/textwrangler'
   version '4.5.8'
   sha256 'ac63be1a83f6e611a67753e63304a2549f95e031562f6245c5688f3d307537ae'
