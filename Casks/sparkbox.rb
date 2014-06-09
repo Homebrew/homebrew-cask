20e763f11cd80098c1e0130d12dd58969c2a3112
Roland Walker <walker@pobox.com>
2014-06-09 10:41:38 -0400

    add appcast stanza for sparkbox.rb

--- Casks/sparkbox.rb
+++ Casks/sparkbox.rb
@@ -1,5 +1,6 @@
 class Sparkbox < Cask
   url 'http://t.icyblaze.com/sblatest'
+  appcast 'http://matrix.icyblaze.com/index.php/checkupdate/p/8'
   homepage 'http://www.icyblaze.com/sparkbox'
   version 'latest'
   sha256 :no_check
