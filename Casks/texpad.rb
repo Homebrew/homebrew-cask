086cf9aaeadb7390369e390bbc4b8473e67ad3f8
Roland Walker <walker@pobox.com>
2014-06-09 10:41:38 -0400

    add appcast stanza for texpad.rb

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -1,5 +1,6 @@
 class Texpad < Cask
   url 'http://cloud.texpadapp.com/bundles/Texpad_1_6_12.zip'
+  appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml'
   homepage 'https://www.texpadapp.com/osx'
   version '1.6.12'
   sha256 'bca8da9be6c2af599455eb9e936b50667f25b55a842af41e87f2e72e396c243e'
