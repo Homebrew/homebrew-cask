f7f9172e7842a27dd7d8e383a250f02bd6d88083
Roland Walker <walker@pobox.com>
2014-06-09 10:41:37 -0400

    add appcast stanza for pwnagetool.rb

--- Casks/pwnagetool.rb
+++ Casks/pwnagetool.rb
@@ -1,5 +1,6 @@
 class Pwnagetool < Cask
   url 'https://sites.google.com/a/ipad-dev.com/files/pwnagetool/PwnageTool_5.1.1.dmg'
+  appcast 'http://www.iphone-dev.org/appcast/PwnageTool2.xml'
   homepage 'http://blog.iphone-dev.org/tagged/PwnageTool'
   version '5.1.1'
   sha256 '84262734ad9f9186bce14a4f939d7ea290ed187782fdfa549a82c28bf837c808'
