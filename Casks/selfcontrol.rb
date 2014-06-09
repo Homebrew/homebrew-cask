77041abd699cde4123121f53c856b8cd7957608a
Roland Walker <walker@pobox.com>
2014-06-09 10:41:37 -0400

    add appcast stanza for selfcontrol.rb

--- Casks/selfcontrol.rb
+++ Casks/selfcontrol.rb
@@ -1,5 +1,6 @@
 class Selfcontrol < Cask
   url 'http://downloads.selfcontrolapp.com/SelfControl-1.5.1.zip'
+  appcast 'http://selfcontrolapp.com/SelfControlAppcast.xml'
   homepage 'http://selfcontrolapp.com/'
   version '1.5.1'
   sha256 'd3823a1e9ba0b47dc2cb39c93cd51837c2dafc7d5a5a564825f4a440fd2ab9ad'
