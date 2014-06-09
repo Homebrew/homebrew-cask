bfae7ce91295a5e9da018571996e136f9ebd9ef6
Roland Walker <walker@pobox.com>
2014-06-09 11:29:34 -0400

    add appcast stanza for narrative-uploader.rb

--- Casks/narrative-uploader.rb
+++ Casks/narrative-uploader.rb
@@ -1,5 +1,6 @@
 class NarrativeUploader < Cask
   url 'http://dl.getnarrative.com/appcast/installers/NarrativeUploader.dmg'
+  appcast 'http://dl.getnarrative.com/appcast/osx.xml'
   homepage 'http://getnarrative.com'
   version 'latest'
   sha256 :no_check
