b103f266d89b9b9e5b85d1fd90b358a49c28d1d8
Semenyuk Dmitriy <git@semenyukdmitriy.com>
2014-06-09 13:12:05 +0600

    GitX-dev

---
+++ Casks/gitx-dev.rb
@@ -0,0 +1,7 @@
+class GitxDev < Cask
+  url 'http://builds.phere.net/GitX/development/GitX-dev.dmg'
+  homepage 'http://rowanj.github.io/gitx/'
+  version 'latest'
+  sha256 :no_check
+  link 'GitX.app'
+end
