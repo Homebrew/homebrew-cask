22b5877378520f364a7bdaac2aac0cfc2705b613
Roland Walker <walker@pobox.com>
2014-06-09 10:41:37 -0400

    add appcast stanza for shimo.rb

--- Casks/shimo.rb
+++ Casks/shimo.rb
@@ -1,5 +1,6 @@
 class Shimo < Cask
   url 'http://www.chungwasoft.com/files/Shimo_3.2.3_2587.zip'
+  appcast 'http://www.chungwasoft.com/library/appcasts/Shimo3/shimocast.php'
   homepage 'http://www.chungwasoft.com/shimo/'
   version '3.2.3_2587'
   sha256 'ff8a5bc7254d6860ecae72a863e1da4faec5948c2b07e8b7da4c96dcb34ec5b4'
