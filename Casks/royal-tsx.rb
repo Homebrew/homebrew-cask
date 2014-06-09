f519d3246b979c842208ac6921fe5b93fdd1e8de
Roland Walker <walker@pobox.com>
2014-06-09 11:29:34 -0400

    add appcast stanza for royal-tsx.rb

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -1,5 +1,6 @@
 class RoyalTsx < Cask
   url 'http://app.royaltsx.com/updates/royaltsx_1.3.0.0.dmg'
+  appcast 'http://app.royaltsx.com/updates_stable.php'
   homepage 'http://www.royaltsx.com'
   version '1.3.0.0'
   sha256 '2306ae0579b097ccf4796f9cc08012819d3e55d5025a1424c8eca5330cdaedf5'
