4db63e308f1cc379bcc61fc2d9c4e2653f24f644
Roland Walker <walker@pobox.com>
2014-06-09 10:41:37 -0400

    add appcast stanza for safemonk.rb

--- Casks/safemonk.rb
+++ Casks/safemonk.rb
@@ -1,5 +1,6 @@
 class Safemonk < Cask
   url 'https://www.safemonk.com/downloads/osx'
+  appcast 'https://www.safemonk.com:/data/client/osx/appcast.xml'
   homepage 'https://www.safemonk.com/'
   version 'latest'
   sha256 :no_check
