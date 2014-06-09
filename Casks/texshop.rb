82a9aed89e4a94d4c8d5c3ee060aaaad57862feb
Roland Walker <walker@pobox.com>
2014-06-09 10:41:38 -0400

    add appcast stanza for texshop.rb

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,5 +1,6 @@
 class Texshop < Cask
   url 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshop336.1.zip'
+  appcast 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml'
   homepage 'http://pages.uoregon.edu/koch/texshop'
   version '3.36.1'
   sha256 '78940561dde093f9ef2211b0e2f8acb332e4f3d141ffd37e3d654ce7dc112831'
