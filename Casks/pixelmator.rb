0e9b03c4e6f26d1fe17fa0b6ebfd75dcdadff27b
Callum Macrae <callum@macr.ae>
2014-06-09 16:27:26 +0100

    Added pixelmator.

---
+++ Casks/pixelmator.rb
@@ -0,0 +1,7 @@
+class Pixelmator < Cask
+	url 'http://pixelmator-download.s3.amazonaws.com/trial/Pixelmator.zip'
+	homepage 'http://www.pixelmator.com/'
+	version 'latest'
+	sha256 :no_check
+	link 'Pixelmator.app'
+end
