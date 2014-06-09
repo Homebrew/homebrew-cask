bc05358a41519f5c9e08c321f508a82af67c2c4c
Roland Walker <walker@pobox.com>
2014-06-09 10:41:38 -0400

    add appcast stanza for subsmarine.rb

--- Casks/subsmarine.rb
+++ Casks/subsmarine.rb
@@ -1,5 +1,6 @@
 class Subsmarine < Cask
   url 'http://www.cocoawithchurros.com/downloads/subsmarine.app.1.2.zip'
+  appcast 'http://www.cocoawithchurros.com/shine/appcast.php?id=7'
   homepage 'http://www.cocoawithchurros.com/subsmarine.php'
   version '1.2'
   sha256 '337722dedb0682d34f11f2decc3ad827849032f9a0e795d2f89c5c4bd0c32286'
