0fa69362359449368986a7eb43aa7722ed6a6a4a
Roland Walker <walker@pobox.com>
2014-06-09 11:29:34 -0400

    add appcast stanza for idocument-plus.rb

--- Casks/idocument-plus.rb
+++ Casks/idocument-plus.rb
@@ -1,5 +1,6 @@
 class IdocumentPlus < Cask
   url 'http://t.icyblaze.com/idpluslatest'
+  appcast 'http://t.icyblaze.com/idplusc4ud'
   homepage 'http://www.icyblaze.com/idocument/'
   version 'latest'
   sha256 :no_check
