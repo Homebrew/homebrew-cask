2b70b85eb4625f3b84cf3772d64391f8df53ef30
Roland Walker <walker@pobox.com>
2014-06-09 10:41:39 -0400

    add appcast stanza for tunnelblick.rb

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -1,5 +1,6 @@
 class Tunnelblick < Cask
   url 'http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_3.3.2.dmg'
+  appcast 'https://www.tunnelblick.net/appcast.rss'
   homepage 'https://code.google.com/p/tunnelblick/'
   version '3.3.2'
   sha256 '1e17563771a9536313e68d5a7ff4bddfebcc97a19704ed2a504517b9c7796026'
