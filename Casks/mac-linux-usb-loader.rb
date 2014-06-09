f6b5acdf3df9b826ee1e407e58a43992113a9f41
Roland Walker <walker@pobox.com>
2014-06-09 11:29:34 -0400

    add appcast stanza for mac-linux-usb-loader.rb

--- Casks/mac-linux-usb-loader.rb
+++ Casks/mac-linux-usb-loader.rb
@@ -1,5 +1,6 @@
 class MacLinuxUsbLoader < Cask
   url 'http://sevenbits.github.io/downloads/Mac-Linux-USB-Loader.zip'
+  appcast 'http://sevenbits.github.io/appcasts/mlul.xml'
   homepage 'http://sevenbits.github.io/Mac-Linux-USB-Loader/'
   version 'latest'
   sha256 :no_check
