698c515328fd4a3baed1839767b5d2809a5ca5e0
Allan Odgaard <git@abetarda.com>
2014-06-10 23:22:34 +0200

    Add MacTubes.app v3.1.6
    MacTubes is a YouTube video player and downloader.

---
+++ Casks/mactubes.rb
@@ -0,0 +1,7 @@
+class Mactubes < Cask
+  url 'http://macapps.sakura.ne.jp/mactubes/archives/MacTubes_v3.1.6.zip'
+  homepage 'http://macapps.sakura.ne.jp/mactubes/index_en.html'
+  version '3.1.6'
+  sha256 'd3fbe0ee7fb95c1eea0f70b84140a816561601f0a1f36ba2b16ba63bfa57cd19'
+  link 'MacTubes_v3.1.6/MacTubes.app'
+end
