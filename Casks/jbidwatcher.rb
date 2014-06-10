9ece9f4cd9b42fbfd1d7a57cc08e74e7d4254e1f
Anthony Green <anthony.green@bbc.co.uk>
2014-06-10 12:31:13 +0100

    JBidwatcher 2.5.6

---
+++ Casks/jbidwatcher.rb
@@ -0,0 +1,7 @@
+class Jbidwatcher < Cask
+  url 'http://www.jbidwatcher.com/download/JBidwatcher-2.5.6.dmg'
+  homepage 'http://www.jbidwatcher.com'
+  version '2.5.6'
+  sha256 '659de073d9e0d71fa86e21db1524b7df57b36ff7826600a01ed93da6df5f5c9f'
+  link 'JBidwatcher.app'
+end
