0be7c04cf3e1d93dc2c81cd48e2fea98aafa938f
Caleb Xu <calebcenter@live.com>
2014-06-09 23:12:57 -0400

    Add uninstall stanza for TotalSpaces

--- Casks/totalspaces.rb
+++ Casks/totalspaces.rb
@@ -4,4 +4,10 @@ class Totalspaces < Cask
   version '1.2.11'
   sha256 'fd54c6ea092f6fae2035745959ff6e080953e77ec6c76715e532b4b0352235d4'
   install 'TotalSpaces.pkg'
+  uninstall :pkgutil => 'com.switchstep.totalspaces',
+            :quit    => 'com.binaryage.TotalSpaces',
+            :signal  => [
+                         ['INT', 'com.binaryage.totalspacescrashwatcher'],
+                         ['KILL', 'com.binaryage.totalspacescrashwatcher'],
+                        ]
 end
