210f289ccf275609c02d33bc309603104835f995
Vítor Galvão <info@vitorgalvao.com>
2014-06-10 21:45:48 +0100

    updated findings (latest)

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -1,8 +1,8 @@
 class Findings < Cask
-  url 'http://downloads.findingsapp.com/Findings_2133_1.0.zip'
+  url 'http://downloads.findingsapp.com/findings'
   appcast 'http://downloads.findingsapp.com/appcast.xml'
   homepage 'http://findingsapp.com'
-  version '1.0'
-  sha256 'f46d73e1f384d08136ef95c0d2881c422122d677c2bf78fe524e8c47aad98259'
+  version 'latest'
+  sha256 :no_check
   link 'Findings.app'
 end
