eafac3227e2d2c7553919db3ce9cec24b82462f0
Beng Hee Eu <beng@benghee.eu>
2014-06-11 22:53:08 +0800

    Flash Player 14.0.0.125

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,7 +1,7 @@
 class FlashPlayer < Cask
-  url 'http://fpdownload.macromedia.com/pub/flashplayer/updaters/13/flashplayer_13_sa.dmg'
+  url 'http://fpdownload.macromedia.com/pub/flashplayer/updaters/14/flashplayer_14_sa.dmg'
   homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
-  version '13.0.0.214'
-  sha256 'ad5042e515c6caf3dd14b6a94e5dfaa0bd2f19af7112653b31290a4682f30f72'
+  version '14.0.0.125'
+  sha256 'fa5eef90162516da0ba4de071abdff202f6bb5281d5a39b62169cad60d214a90'
   link 'Flash Player.app'
 end
