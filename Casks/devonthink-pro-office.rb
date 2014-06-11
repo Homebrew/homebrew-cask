3ce9eda3b7f5051b235bc08b624d31dab84b427f
Roland Walker <walker@pobox.com>
2014-06-11 15:55:39 -0400

    add appcast stanza for devonthink-pro-office.rb

--- Casks/devonthink-pro-office.rb
+++ Casks/devonthink-pro-office.rb
@@ -1,5 +1,6 @@
 class DevonthinkProOffice < Cask
   url 'https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/2.7.4/DEVONthink_Pro_Office.dmg.zip'
+  appcast 'http://www.devon-technologies.com/Sparkle/DEVONthinkProOffice2.xml'
   homepage 'http://www.devontechnologies.com/products/devonthink/devonthink-pro-office.html'
   version '2.7.4'
   sha256 'fab41794b3739d28ac936caef8d2c921105604f959a37c4e19a16b3ac9481e59'
