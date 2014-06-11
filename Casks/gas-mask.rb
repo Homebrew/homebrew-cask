9947dfd3d4ee66769d58afb667c331965dec4e35
Roland Walker <walker@pobox.com>
2014-06-11 15:55:39 -0400

    add appcast stanza for gas-mask.rb

--- Casks/gas-mask.rb
+++ Casks/gas-mask.rb
@@ -1,5 +1,6 @@
 class GasMask < Cask
   url 'http://gmask.clockwise.ee/files/gas_mask_0.7.zip'
+  appcast 'http://gmask.clockwise.ee/check_update/'
   homepage 'http://www.clockwise.ee/gasmask/'
   version '0.7'
   sha256 '64250f2c50c32dda5cb336aa7daa311fa60e9827b5298b685a078fb7a1a42443'
