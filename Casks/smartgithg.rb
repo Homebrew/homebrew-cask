6bb4fbbc5cef35c0ffddd4087c51a91dfa14e050
Lee Hanxue <hanxue.developer@gmail.com>
2014-06-12 18:26:28 +0800

    Smartgithg 6.0.2

--- Casks/smartgithg.rb
+++ Casks/smartgithg.rb
@@ -1,8 +1,8 @@
 class Smartgithg < Cask
-  url 'http://www.syntevo.com/download/smartgithg/smartgithg-macosx-6_0_0.dmg'
+  url 'http://www.syntevo.com/download/smartgithg/smartgithg-macosx-6_0_2.dmg'
   homepage 'http://www.syntevo.com'
-  version '6.0.0'
-  sha256 'c169e268335035e16b7f87b6e429a7587e89f717b6cad681c0167307d204694c'
+  version '6.0.2'
+  sha256 'ca43c8cb5d14f5b0ef74f28a5575a059c241bf6d5d18f38eb93052f2a283c40d'
   link 'SmartGitHg 6.app'
   binary 'SmartGitHg 6.app/Contents/MacOS/SmartGit'
   caveats do
