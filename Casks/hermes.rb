b94564d7e996694a00ea3de1849a27f1d3e3eec6
Will Smidlein <will@willsmidlein.com>
2014-06-12 09:20:42 -0700

    Updated Hermes
    Bring version to latest (1.2.1)

--- Casks/hermes.rb
+++ Casks/hermes.rb
@@ -1,8 +1,8 @@
 class Hermes < Cask
-  url 'https://s3.amazonaws.com/alexcrichton-hermes/Hermes-1.2.0.zip'
+  url 'https://s3.amazonaws.com/alexcrichton-hermes/Hermes-1.2.1.zip'
   appcast 'http://hermesapp.org/versions.xml'
   homepage 'http://hermesapp.org/'
-  version '1.2.0'
-  sha256 'ac1412575c0fe1a72a69761b711e9ffe7fb6972661c1501e1cec83bb9078b035'
+  version '1.2.1'
+  sha256 'ebee6b922dbe836db3a80320415ea5841f9a9679d6539ac00d6c6cb6e271fb18'
   link 'Hermes.app'
 end
