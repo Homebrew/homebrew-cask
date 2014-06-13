ba685922a4aa121992c0cb55f33e2634b0712905
Chris Spicer <github@cspicer.ca>
2014-06-12 17:09:34 -0700

    Add AutoDMG 1.4.3

---
+++ Casks/autodmg.rb
@@ -0,0 +1,7 @@
+class Autodmg < Cask
+  url 'https://github.com/MagerValp/AutoDMG/releases/download/v1.4.3/AutoDMG-1.4.3.dmg'
+  homepage 'https://github.com/MagerValp/AutoDMG'
+  version '1.4.3'
+  sha256 '62233d162ccf2109bf0a03f1e74c7c74da323958ad1f13a0c06f18a611e44592'
+  link 'AutoDMG.app'
+end
