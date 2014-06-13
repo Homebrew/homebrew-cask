401461b2e3789beb7654702c9874722ff2d02719
Paco Gomez <contact@pacogomez.com>
2014-06-12 20:38:51 -0500

    Upgrade PlyCounter.app to v1.9

--- Casks/plycounter.rb
+++ Casks/plycounter.rb
@@ -1,7 +1,7 @@
 class Plycounter < Cask
   url 'http://www.plycount.com/plycounter/downloads/PlyCounter.dmg'
   homepage 'http://www.plycount.com'
-  version '1.8'
-  sha256 'f96cba814640596c0aec51a9fc8a0a78db00dfda365de340c21917e0cc1fc091'
+  version '1.9'
+  sha256 '07a015d543902e5533bd497ddc7ec70564da639ca618283e16c736bd015335a3'
   link 'PlyCounter.app'
 end
