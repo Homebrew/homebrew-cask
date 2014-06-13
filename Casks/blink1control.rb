12c6600887c6e8d2a2dd8dabe09c7d276274d240
Alberto A. Colon Viera (Albertico) <aacv@alberti.co>
2014-06-13 19:29:10 -0400

    Upgrade Blink1Control.app to v1.7 Enable checksum in Blink1Control.app

--- Casks/blink1control.rb
+++ Casks/blink1control.rb
@@ -1,7 +1,7 @@
 class Blink1control < Cask
-  url 'http://thingm.com/blink1/downloads/Blink1Control-mac.zip'
+  url 'https://github.com/todbot/blink1/releases/download/v1.7/Blink1Control-mac.zip'
   homepage 'http://blink1.thingm.com/'
-  version 'latest'
-  sha256 :no_check
+  version '1.7'
+  sha256 '1ac3637931471bce8f386616051126200b5555fdcc566af999b448d494415d19'
   link 'Blink1Control.app'
 end
