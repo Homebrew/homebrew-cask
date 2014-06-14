aa31907f2ef51728a3e49197efef0667f865c474
Simon Olofsson <simon@olofsson.de>
2014-06-14 22:07:06 +0200

    Casks/cakebrew: Update to 1.0.7

--- Casks/cakebrew.rb
+++ Casks/cakebrew.rb
@@ -1,8 +1,8 @@
 class Cakebrew < Cask
-  url 'https://www.cakebrew.com/files/cakebrew-1.0.5.dmg'
+  url 'https://www.cakebrew.com/files/cakebrew-1.0.7.dmg'
   appcast 'http://www.cakebrew.com/appcast/profileInfo.php'
   homepage 'http://www.cakebrew.com'
-  version '1.0.5'
-  sha256 'd9f9c013fd5e4789c4137aa82bc8f7e4848e8bf4d2565852118897f2091458de'
+  version '1.0.7'
+  sha256 '647ce281fa3218e7d4531cac0e5b69d19136883d2b95d154b7a818c4d130cace'
   link 'Cakebrew.app'
 end
