de69ea9d04d88365fbce0c9197a91cdae7c61c38
Radek Simko <radek.simko@gmail.com>
2014-06-14 12:50:18 +0100

    AppCast feed added to Xquartz cask

--- Casks/xquartz.rb
+++ Casks/xquartz.rb
@@ -1,6 +1,7 @@
 class Xquartz < Cask
   url 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg'
   homepage 'http://xquartz.macosforge.org/'
+  appcast 'http://xquartz-dl.macosforge.org/sparkle/release.xml'
   version '2.7.6'
   sha256 '02aa3268af0bd7dcd5dfbc10d673f5c6834bba6371a928d2a3fc44a8039b194e'
   install 'XQuartz.pkg'
