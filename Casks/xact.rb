e4fbc7287a6e3467dd1005ba85a71a7e245e5654
Vítor Galvão <info@vitorgalvao.com>
2014-06-15 15:50:19 +0100

    xact — moved sha256 underneath version, at top, so it's clearer what needs updating

--- Casks/xact.rb
+++ Casks/xact.rb
@@ -1,9 +1,9 @@
 class Xact < Cask
   version '2.29'
+  sha256 'bc8805f94b9f2320da64ca23b53e1b029fd65920ded2bc56c0291f0a73866674'
 
   url "http://xact.scottcbrown.org/xACT#{version}.zip"
   appcast 'http://xactupdate.scottcbrown.org/xACT.xml'
   homepage 'http://xact.scottcbrown.org'
-  sha256 'bc8805f94b9f2320da64ca23b53e1b029fd65920ded2bc56c0291f0a73866674'
   link "xACT #{version}/xACT.app"
 end
