e9171404ff8de6b8bf6b65ee93e5665b3f587503
Vítor Galvão <info@vitorgalvao.com>
2014-06-16 15:26:40 +0100

    separated parts that need updating from the rest, in sts

--- Casks/sts.rb
+++ Casks/sts.rb
@@ -1,10 +1,13 @@
 class Sts < Cask
-  url 'http://download.springsource.com/release/STS/3.5.1/dist/e4.3/spring-tool-suite-3.5.1.RELEASE-e4.3.2-macosx-cocoa-x86_64-installer.dmg'
-  homepage 'http://spring.io/tools/sts'
   version '3.5.1'
   sha256 'f71274c9f946d2af6bbd12e811d7c8d371d3031415839b9aa6ed35347d2980f8'
+  # find eclise version at http://spring.io/tools/sts/all
+  based_on_eclipse = '4.3.2'
+
+  url "http://download.springsource.com/release/STS/#{version}/dist/e#{based_on_eclipse.gsub(/\.\d$/, '')}/spring-tool-suite-#{version}.RELEASE-e#{based_on_eclipse}-macosx-cocoa-x86_64-installer.dmg"
+  homepage 'http://spring.io/tools/sts'
   caskroom_only true
   caveats do
-    manual_installer 'Installer - Spring Tool Suite 3.5.1.RELEASE.app'
+    manual_installer "Installer - Spring Tool Suite #{@cask.version}.RELEASE.app"
   end
 end
