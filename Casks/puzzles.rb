0492129d7ab0e7f662264986e68e6d5fd9982af2
Carlos Precioso <kalprestito@users.noreply.github.com>
2014-06-17 00:18:23 +0200

    Add Simon Tatham's Puzzle Collection

---
+++ Casks/puzzles.rb
@@ -0,0 +1,7 @@
+class Puzzles < Cask
+  url 'http://www.chiark.greenend.org.uk/~sgtatham/puzzles/Puzzles.dmg'
+  homepage 'http://www.chiark.greenend.org.uk/~sgtatham/puzzles/'
+  version 'latest'
+  sha256 :no_check
+  link 'Puzzles.app'
+end
