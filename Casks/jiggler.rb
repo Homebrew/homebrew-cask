10adf451c4b7f9a96bd8642b696ce1a5c54e4779
Trey Harris <treyharris@gmail.com>
2014-06-16 16:47:33 -0400

    Add Jiggler.app latest
    Jiggler periodically jiggles the mouse to keep the Mac awake. No
    versioned downloads are available.

---
+++ Casks/jiggler.rb
@@ -0,0 +1,7 @@
+class Jiggler < Cask
+  url 'http://downloads.sticksoftware.com/Jiggler.dmg'
+  homepage 'http://www.sticksoftware.com/software/Jiggler.html'
+  version 'latest'
+  sha256 :no_check
+  link 'Jiggler.app'
+end
