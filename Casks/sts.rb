c42b289c3fd8790c7efabcfbf01ded49485c5a0e
Vítor Galvão <info@vitorgalvao.com>
2014-06-16 15:05:41 +0100

    updated sts to 3.5.1

--- Casks/sts.rb
+++ Casks/sts.rb
@@ -1,7 +1,10 @@
 class Sts < Cask
-  url 'http://download.springsource.com/release/STS/3.4.0/dist/e4.3/spring-tool-suite-3.4.0.RELEASE-e4.3.1-macosx-cocoa-x86_64.tar.gz'
+  url 'http://download.springsource.com/release/STS/3.5.1/dist/e4.3/spring-tool-suite-3.5.1.RELEASE-e4.3.2-macosx-cocoa-x86_64-installer.dmg'
   homepage 'http://spring.io/tools/sts'
-  version '3.4.0'
-  sha256 'bfe6de0b0882487e09afca845aa553acec6c43a6352547237b880e829e1aa59c'
-  link 'springsource/sts-3.4.0.RELEASE/STS.app'
+  version '3.5.1'
+  sha256 'f71274c9f946d2af6bbd12e811d7c8d371d3031415839b9aa6ed35347d2980f8'
+  caskroom_only true
+  caveats do
+    manual_installer 'Installer - Spring Tool Suite 3.5.1.RELEASE.app'
+  end
 end
