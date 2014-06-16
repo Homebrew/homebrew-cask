ba797f043ebf6c9649316e641804798a0b9a3489
Roland Walker <walker@pobox.com>
2014-06-16 18:00:21 -0400

    move read_script_argument to Cask::Artifact::Base

--- lib/cask/artifact/base.rb
+++ lib/cask/artifact/base.rb
@@ -24,6 +24,37 @@ class Cask::Artifact::Base
      cask.artifacts[self.artifact_dsl_key].any?
   end
 
+  def self.read_script_arguments(uninstall_options, key)
+    script_arguments = uninstall_options[key]
+
+    # backwards-compatible string value
+    if script_arguments.kind_of?(String)
+      script_arguments = { :executable => script_arguments }
+    end
+
+    # key sanity
+    permitted_keys = [:args, :input, :executable, :must_succeed]
+    unknown_keys = script_arguments.keys - permitted_keys
+    unless unknown_keys.empty?
+      opoo "Unknown arguments to uninstall :#{key} -- :#{unknown_keys.join(", :")} (ignored). Running `brew update; brew upgrade brew-cask` will likely fix it.'"
+    end
+    script_arguments.reject! {|k,v| ! permitted_keys.include?(k)}
+
+    # extract executable
+    if script_arguments.key?(:executable)
+      executable = script_arguments.delete(:executable)
+    else
+      executable = nil
+    end
+
+    unless script_arguments.key?(:must_succeed)
+      script_arguments[:must_succeed] = true
+    end
+
+    script_arguments.merge!(:sudo => true, :print => true)
+    return executable, script_arguments
+  end
+
   def summary
     {}
   end
--- lib/cask/artifact/uninstall_base.rb
+++ lib/cask/artifact/uninstall_base.rb
@@ -11,37 +11,6 @@ class Cask::Artifact::UninstallBase < Cask::Artifact::Base
     dispatch_uninstall_directives(self.class.artifact_dsl_key)
   end
 
-  def self.read_script_arguments(uninstall_options, key)
-    script_arguments = uninstall_options[key]
-
-    # backwards-compatible string value
-    if script_arguments.kind_of?(String)
-      script_arguments = { :executable => script_arguments }
-    end
-
-    # key sanity
-    permitted_keys = [:args, :input, :executable, :must_succeed]
-    unknown_keys = script_arguments.keys - permitted_keys
-    unless unknown_keys.empty?
-      opoo %Q{Unknown arguments to #{stanza} #{key.inspect} -- #{unknown_keys.inspect} (ignored). Running "brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup" will likely fix it.}
-    end
-    script_arguments.reject! {|k,v| ! permitted_keys.include?(k)}
-
-    # extract executable
-    if script_arguments.key?(:executable)
-      executable = script_arguments.delete(:executable)
-    else
-      executable = nil
-    end
-
-    unless script_arguments.key?(:must_succeed)
-      script_arguments[:must_succeed] = true
-    end
-
-    script_arguments.merge!(:sudo => true, :print => true)
-    return executable, script_arguments
-  end
-
   def dispatch_uninstall_directives(stanza)
     directives_set = @cask.artifacts[stanza]
     ohai "Running #{stanza} process for #{@cask}; your password may be necessary"
