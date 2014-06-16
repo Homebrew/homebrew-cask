79fbe2f6a99b6c77ce01ffe1dbfa1020b00396d8
Vítor Galvão <info@vitorgalvao.com>
2014-06-16 15:53:51 +0100

    added grafx2

---
+++ Casks/grafx2.rb
@@ -0,0 +1,7 @@
+class Grafx2 < Cask
+  url 'http://pulkomandy.tk/projects/GrafX2/downloads/24'
+  homepage 'http://pulkomandy.tk/projects/GrafX2'
+  version 'latest'
+  sha256 :no_check
+  link 'Grafx2.app'
+end
