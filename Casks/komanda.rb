7add27fb102c58f42500f45df99ee0571c340135
Caleb Xu <calebcenter@live.com>
2014-06-16 19:22:01 -0400

    Add Komanda

---
+++ Casks/komanda.rb
@@ -0,0 +1,7 @@
+class Komanda < Cask
+  url 'https://github.com/mephux/komanda/releases/download/1.0.0.beta/Komanda-macosx.zip'
+  homepage 'https://github.com/mephux/komanda'
+  version '1.0.0 beta'
+  sha256 'e54f195185d92b0b4a9e9be3b1db4b2cbd99723c93e9b6e57feb0df90b27a5d1'
+  link 'Komanda.app'
+end
