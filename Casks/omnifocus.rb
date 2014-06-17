78858537739269081c970911a6c376e73d829bf8
Keng Kiat Lim <kkin@crnrc.com>
2014-06-18 05:59:51 +0800

    OmniFocus 2.0.1
    Update OmniFocus to 2.0.1
    Add shasum for integrity check

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -1,7 +1,7 @@
 class Omnifocus < Cask
-  url 'http://www.omnigroup.com/download/latest/omnifocus'
+  url 'http://downloads2.omnigroup.com/software/MacOSX/10.9/OmniFocus-2.0.1.dmg'
   homepage 'http://www.omnigroup.com/products/omnifocus/'
-  version 'latest'
-  sha256 :no_check
+  version '2.0.1'
+  sha256 '6973a8c046ffecbca694048bace1426c9fe28c658a5207cb6bdf992b5d0fc682'
   link 'OmniFocus.app'
 end
