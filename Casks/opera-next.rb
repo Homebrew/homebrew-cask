0c40f93f3fe6337aa511424815ae0af103b513e9
Beng Hee Eu <beng@benghee.eu>
2014-06-17 18:02:18 +0800

    Opera Next 23.0.1522.14

--- Casks/opera-next.rb
+++ Casks/opera-next.rb
@@ -1,7 +1,7 @@
 class OperaNext < Cask
-  url 'http://get.geo.opera.com/pub/opera-next/22.0.1471.40/mac/Opera_Next_22.0.1471.40_Setup.dmg'
+  url 'http://get.geo.opera.com/pub/opera-next/23.0.1522.14/mac/Opera_Next_23.0.1522.14_Setup.dmg'
   homepage 'http://www.opera.com/computer/next'
-  version '22.0.1471.40'
-  sha256 'e2dbeebb1780873bd886e252a59ae5e46c7238ef94530cc19d81cc5b3ff87dac'
+  version '23.0.1522.14'
+  sha256 '55f5ea5638d4d73bff775004769d58816ae61d088206d84004238f66db215e8c'
   link 'Opera Next.app'
 end
