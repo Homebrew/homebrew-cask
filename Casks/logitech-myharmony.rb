e97d57dbe86d452add2e3c9b0a681c2f612c90f1
Jason Sallis <jason@sall.is>
2014-06-17 09:47:19 -0700

    Update sha for Logitech MyHarmony

--- Casks/logitech-myharmony.rb
+++ Casks/logitech-myharmony.rb
@@ -2,7 +2,7 @@ class LogitechMyharmony < Cask
   url 'http://app.myharmony.com/prod/mac/1.0/MyHarmony-App.dmg'
   homepage 'https://setup.myharmony.com/'
   version '1.0'
-  sha256 '40969157ae8a7c45b7bd0711c9136da9e87b6ced71cbd0afa23817fc9fb569b5'
+  sha256 '537a11f2174dc748c27aff02bf01569b5bb2c93539b339c6fd61a7da07bb059f'
   install 'MyHarmonySetup.pkg'
   uninstall :quit => 'org.logitech.MyHarmony',
             :pkgutil => 'MyHarmony.pkg'
