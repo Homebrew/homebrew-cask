7fdcdf2ace4b419bb1e4135a4e45724320dae96d
Roland Walker <walker@pobox.com>
2014-06-18 10:55:26 -0400

    use HTTPS url in thyme.rb

--- Casks/thyme.rb
+++ Casks/thyme.rb
@@ -1,5 +1,5 @@
 class Thyme < Cask
-  url 'http://joaomoreno.github.io/thyme/dist/Thyme-0.4.2.dmg'
+  url 'https://joaomoreno.github.io/thyme/dist/Thyme-0.4.2.dmg'
   homepage 'http://joaomoreno.github.io/thyme/'
   version '0.4.2'
   sha256 '5963d199ff9d64f3cc6966ed532b949fa500c9887d391aef8eedba68ae469a45'
