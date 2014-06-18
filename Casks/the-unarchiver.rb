4daba819f648f34e45ba5157e22ceb9c56c97544
Roland Walker <walker@pobox.com>
2014-06-18 10:55:26 -0400

    use HTTPS url in the-unarchiver.rb

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -1,5 +1,5 @@
 class TheUnarchiver < Cask
-  url 'http://theunarchiver.googlecode.com/files/TheUnarchiver3.9.1.zip'
+  url 'https://theunarchiver.googlecode.com/files/TheUnarchiver3.9.1.zip'
   homepage 'http://unarchiver.c3.cx/'
   version '3.9.1'
   sha256 '34fa3410237e17b2cdceb801a84ed8db93c74ac0db551ffe65913c2134ebbf05'
