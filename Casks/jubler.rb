78ea69f556723289e3de9d68e62bdd6f241eb99b
Roland Walker <walker@pobox.com>
2014-06-18 10:55:23 -0400

    use HTTPS url in jubler.rb

--- Casks/jubler.rb
+++ Casks/jubler.rb
@@ -1,5 +1,5 @@
 class Jubler < Cask
-  url 'http://jubler.googlecode.com/files/Jubler-4.6.1.dmg'
+  url 'https://jubler.googlecode.com/files/Jubler-4.6.1.dmg'
   homepage 'http://www.jubler.org/'
   version '4.6.1'
   sha256 '7a5d08d950ccd7869fedb25f6d0ed8b8e8277e99a9032601de641b3de6545a13'
