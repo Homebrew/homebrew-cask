40217ccad7d6dea70607909a8b3f8273b2a7db34
Roland Walker <walker@pobox.com>
2014-06-18 11:11:04 -0400

    use HTTPS url in josm.rb

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,5 +1,5 @@
 class Josm < Cask
-  url 'http://josm.openstreetmap.de/download/macosx/josm-macosx.zip'
+  url 'https://josm.openstreetmap.de/download/macosx/josm-macosx.zip'
   homepage 'http://josm.openstreetmap.de'
   version 'latest'
   sha256 :no_check
