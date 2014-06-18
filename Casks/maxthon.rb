8c436c8d3207b2060f8947d40b80e007570ead69
Roland Walker <walker@pobox.com>
2014-06-18 10:55:23 -0400

    use HTTPS url in maxthon.rb

--- Casks/maxthon.rb
+++ Casks/maxthon.rb
@@ -1,5 +1,5 @@
 class Maxthon < Cask
-  url 'http://dl.maxthon.com/mac/Maxthon-4.1.3.4000.dmg'
+  url 'https://dl.maxthon.com/mac/Maxthon-4.1.3.4000.dmg'
   homepage 'http://www.maxthon.com/'
   version '4.1.3.4000'
   sha256 '6c9c5b3aa49827907d83b641fba816b0f9d8cb240e3afb6872f2bd9de203a31e'
