7936004b09b710d3d68e958cd00b56fc43d3b776
Roland Walker <walker@pobox.com>
2014-06-18 10:55:22 -0400

    use HTTPS url in garmin-training-center.rb

--- Casks/garmin-training-center.rb
+++ Casks/garmin-training-center.rb
@@ -1,5 +1,5 @@
 class GarminTrainingCenter < Cask
-  url 'http://www8.garmin.com/software/TrainingCenterforMac_321.dmg'
+  url 'https://www8.garmin.com/software/TrainingCenterforMac_321.dmg'
   homepage 'http://www.garmin.com/garmin/cms/intosports/training_center'
   version '3.2.1'
   sha256 '82bda2f40b3ad8402f1e88fd7750b2827dc8f8f17ea45165e0c85fe6dbd7dfb6'
