e0a75b0ad108492e4300a139c97f9dc188ea2df3
Roland Walker <walker@pobox.com>
2014-06-18 10:55:25 -0400

    use HTTPS url in sketchup.rb

--- Casks/sketchup.rb
+++ Casks/sketchup.rb
@@ -1,5 +1,5 @@
 class Sketchup < Cask
-  url 'http://dl.trimble.com/sketchup/SketchUpMEN.dmg'
+  url 'https://dl.trimble.com/sketchup/SketchUpMEN.dmg'
   homepage 'http://www.sketchup.com/intl/en/'
   version '2013'
   sha256 '0a335e432eabafeaeed000c5471a0818fd5e92e65db4e3e145f99ea261a41844'
