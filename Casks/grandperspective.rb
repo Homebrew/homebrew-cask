acbf69bb14b4f3a45e1035227e76091026b4d501
Roland Walker <walker@pobox.com>
2014-06-18 10:55:22 -0400

    use HTTPS url in grandperspective.rb

--- Casks/grandperspective.rb
+++ Casks/grandperspective.rb
@@ -1,5 +1,5 @@
 class Grandperspective < Cask
-  url 'http://downloads.sourceforge.net/project/grandperspectiv/grandperspective/1.5.1/GrandPerspective-1_5_1.dmg'
+  url 'https://downloads.sourceforge.net/project/grandperspectiv/grandperspective/1.5.1/GrandPerspective-1_5_1.dmg'
   homepage 'http://grandperspectiv.sourceforge.net/'
   version '1.5.1'
   sha256 '92204458042a337c1091879e167ea95e45cae33a7be16fa6c11e80572c54d135'
