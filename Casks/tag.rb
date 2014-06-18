dec9fa577f4715e1ba966891a2a9ecca807d24b2
Roland Walker <walker@pobox.com>
2014-06-18 11:11:06 -0400

    use HTTPS url in tag.rb

--- Casks/tag.rb
+++ Casks/tag.rb
@@ -1,5 +1,5 @@
 class Tag < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/tagosx/Tag-0.4.1.zip'
+  url 'https://downloads.sourceforge.net/sourceforge/tagosx/Tag-0.4.1.zip'
   homepage 'http://sbooth.org/Tag/'
   version 'latest'
   sha256 :no_check
