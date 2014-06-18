e837bf46fab983319759ae498845870232c7a2e8
Roland Walker <walker@pobox.com>
2014-06-18 11:11:05 -0400

    use HTTPS url in parallels.rb

--- Casks/parallels.rb
+++ Casks/parallels.rb
@@ -1,5 +1,5 @@
 class Parallels < Cask
-  url 'http://www.parallels.com/directdownload/pd9'
+  url 'https://www.parallels.com/directdownload/pd9'
   homepage 'http://www.parallels.com/products/desktop/'
   version 'latest'
   sha256 :no_check
