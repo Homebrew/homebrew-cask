07c5126f2144229a8febc17237aa256c84aaa372
Roland Walker <walker@pobox.com>
2014-06-18 11:11:06 -0400

    use HTTPS url in web-sharing.rb

--- Casks/web-sharing.rb
+++ Casks/web-sharing.rb
@@ -1,5 +1,5 @@
 class WebSharing < Cask
-  url 'http://clickontyler.com/web-sharing/download/'
+  url 'https://clickontyler.com/web-sharing/download/'
   homepage 'http://clickontyler.com/web-sharing/'
   version 'latest'
   sha256 :no_check
