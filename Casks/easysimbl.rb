cf23931f7b05af92344caf6969d8454590da35e1
Roland Walker <walker@pobox.com>
2014-06-18 10:55:21 -0400

    use HTTPS url in easysimbl.rb

--- Casks/easysimbl.rb
+++ Casks/easysimbl.rb
@@ -1,5 +1,5 @@
 class Easysimbl < Cask
-  url 'http://github.com/norio-nomura/EasySIMBL/releases/download/EasySIMBL-1.6/EasySIMBL-1.6.zip'
+  url 'https://github.com/norio-nomura/EasySIMBL/releases/download/EasySIMBL-1.6/EasySIMBL-1.6.zip'
   homepage 'https://github.com/norio-nomura/EasySIMBL/'
   version '1.6'
   sha256 '42139885e35946f1b2decb9cdfa755b8df8c050134463d76b2e3e0f68e783e26'
