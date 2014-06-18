1c62dc1960594eafcab704d4a5b8f64a6ecb621e
Roland Walker <walker@pobox.com>
2014-06-18 10:55:24 -0400

    use HTTPS url in ntfs-free.rb

--- Casks/ntfs-free.rb
+++ Casks/ntfs-free.rb
@@ -1,5 +1,5 @@
 class NtfsFree < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/ntfsfree/NTFS-free-10.8.2.pkg'
+  url 'https://downloads.sourceforge.net/sourceforge/ntfsfree/NTFS-free-10.8.2.pkg'
   homepage 'http://sourceforge.net/projects/ntfsfree/'
   version '10.8.2'
   sha256 '1bc7122a2d869b740a35e2dba1fe29c380f21ad07481fc0a46b2f55bf79c574e'
