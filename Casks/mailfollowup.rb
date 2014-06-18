8d8c74024bf5a2ed27a4ba716cccba0b3356ab23
Roland Walker <walker@pobox.com>
2014-06-18 10:55:23 -0400

    use HTTPS url in mailfollowup.rb

--- Casks/mailfollowup.rb
+++ Casks/mailfollowup.rb
@@ -1,5 +1,5 @@
 class Mailfollowup < Cask
-  url 'http://www.cs.unc.edu/~welch/MailFollowup/media/MailFollowUp_1.6.2.dmg.zip'
+  url 'https://www.cs.unc.edu/~welch/MailFollowup/media/MailFollowUp_1.6.2.dmg.zip'
   homepage 'http://www.cs.unc.edu/~welch/MailFollowup/'
   version '1.6.2'
   sha256 'f656c121e04fac6185b24a96888613729c366275151c891258b634c71372b2af'
