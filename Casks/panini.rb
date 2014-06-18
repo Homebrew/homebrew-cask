4afeba10da82bb91da1e31b93cd65c1e2343f88c
Roland Walker <walker@pobox.com>
2014-06-18 10:55:24 -0400

    use HTTPS url in panini.rb

--- Casks/panini.rb
+++ Casks/panini.rb
@@ -1,5 +1,5 @@
 class Panini < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/pvqt/Panini-0.71.104B-mac.dmg'
+  url 'https://downloads.sourceforge.net/sourceforge/pvqt/Panini-0.71.104B-mac.dmg'
   homepage 'http://sourceforge.net/projects/pvqt/'
   version '0.71.104'
   sha256 '2a2836b0035bc1be8ada1e07138c72276a89624321dbfaa475fc4cd88fffe544'
