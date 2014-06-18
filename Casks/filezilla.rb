f204f064a64be88902a7e9984e1a7f193327d1be
Roland Walker <walker@pobox.com>
2014-06-18 10:55:21 -0400

    use HTTPS url in filezilla.rb

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,5 +1,5 @@
 class Filezilla < Cask
-  url 'http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.8.1/FileZilla_3.8.1_macosx-x86.app.tar.bz2'
+  url 'https://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.8.1/FileZilla_3.8.1_macosx-x86.app.tar.bz2'
   homepage 'https://filezilla-project.org/'
   version '3.8.1'
   sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
