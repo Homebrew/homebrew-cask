d460cce67ef8c1b35b2daed567aee2122b748bbf
Roland Walker <walker@pobox.com>
2014-06-18 10:55:20 -0400

    use HTTPS url in aquaterm.rb

--- Casks/aquaterm.rb
+++ Casks/aquaterm.rb
@@ -1,5 +1,5 @@
 class Aquaterm < Cask
-  url 'http://downloads.sourceforge.net/project/aquaterm/AquaTerm/v1.1.1/AquaTerm-1.1.1.dmg'
+  url 'https://downloads.sourceforge.net/project/aquaterm/AquaTerm/v1.1.1/AquaTerm-1.1.1.dmg'
   homepage 'http://aquaterm.sourceforge.net/'
   version '1.1.1'
   sha256 '94b33efea2ec037e6c06beef54b4b3cc48595453c874de863f25c26b3a7ffdb2'
