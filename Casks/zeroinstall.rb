2aab23412e8bf884cbb55da1d6b8009162539b23
Roland Walker <walker@pobox.com>
2014-06-18 10:55:28 -0400

    use HTTPS url in zeroinstall.rb

--- Casks/zeroinstall.rb
+++ Casks/zeroinstall.rb
@@ -1,5 +1,5 @@
 class Zeroinstall < Cask
-  url 'http://downloads.sourceforge.net/project/zero-install/0install/2.7/ZeroInstall.pkg'
+  url 'https://downloads.sourceforge.net/project/zero-install/0install/2.7/ZeroInstall.pkg'
   homepage 'http://0install.net'
   version '2.7'
   sha256 '12246277e9d03fb1a083f6a0d7d088d7b90170dbd5fd8fb900b36d0470fb88e0'
