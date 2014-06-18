92e19a8e1bc20240624ced011360048a80b31e35
Roland Walker <walker@pobox.com>
2014-06-18 10:55:21 -0400

    use HTTPS url in darktable.rb

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,5 +1,5 @@
 class Darktable < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/darktable/darktable-1.4.dmg'
+  url 'https://downloads.sourceforge.net/sourceforge/darktable/darktable-1.4.dmg'
   homepage 'http://www.darktable.org/'
   version '1.4'
   sha256 'c145772a7645da82a5bb8cabfb8fe44023b566135f60f3a93f9551312835234d'
