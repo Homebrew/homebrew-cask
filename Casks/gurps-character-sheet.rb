33b1600171995913825eda56d795bae2849e9a65
Roland Walker <walker@pobox.com>
2014-06-18 10:55:22 -0400

    use HTTPS url in gurps-character-sheet.rb

--- Casks/gurps-character-sheet.rb
+++ Casks/gurps-character-sheet.rb
@@ -1,5 +1,5 @@
 class GurpsCharacterSheet < Cask
-  url 'http://downloads.sourceforge.net/project/gcs-java/gcs-4.0.0-mac.zip'
+  url 'https://downloads.sourceforge.net/project/gcs-java/gcs-4.0.0-mac.zip'
   homepage 'http://gurpscharactersheet.com'
   version '4.0.0'
   sha256 '67ccecc44d28f8cf104094ae988779b8219f79a6e6fbe9c695828034d3307816'
