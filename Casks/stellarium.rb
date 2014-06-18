a7f6bfde361cf013ead9fa805a7eb46520a4dd6f
Roland Walker <walker@pobox.com>
2014-06-18 10:55:26 -0400

    use HTTPS url in stellarium.rb

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,5 +1,5 @@
 class Stellarium < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/stellarium/Stellarium-0.12.4.dmg'
+  url 'https://downloads.sourceforge.net/sourceforge/stellarium/Stellarium-0.12.4.dmg'
   homepage 'http://stellarium.org'
   version '0.12.4'
   sha256 'fe4f3c58a64780888d6ca6e28ec948fa1d885c7f0dd0a85beb4b5ec7339398db'
