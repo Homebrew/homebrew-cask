8adc2ce8fa50a3d3c2759b568111bfd7cb90076f
Roland Walker <walker@pobox.com>
2014-06-18 10:55:25 -0400

    use HTTPS url in retinizer.rb

--- Casks/retinizer.rb
+++ Casks/retinizer.rb
@@ -1,5 +1,5 @@
 class Retinizer < Cask
-  url 'http://sites.google.com/a/mikelpr.com/files/home/Retinizer050.zip'
+  url 'https://sites.google.com/a/mikelpr.com/files/home/Retinizer050.zip'
   homepage 'http://retinizer.mikelpr.com/'
   version '0.5'
   sha256 '34d82f6beeb934ebd73ac231c364298456374d8e52f5e3826077999507832922'
