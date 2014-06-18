22b1b9df364f01f266cfb215e6e056342091d6c4
Roland Walker <walker@pobox.com>
2014-06-18 11:11:03 -0400

    use HTTPS url in audio-hijack-pro.rb

--- Casks/audio-hijack-pro.rb
+++ Casks/audio-hijack-pro.rb
@@ -1,5 +1,5 @@
 class AudioHijackPro < Cask
-  url 'http://rogueamoeba.com/audiohijackpro/download/AudioHijackPro.zip'
+  url 'https://rogueamoeba.com/audiohijackpro/download/AudioHijackPro.zip'
   homepage 'http://www.rogueamoeba.com/audiohijackpro/'
   version 'latest'
   sha256 :no_check
