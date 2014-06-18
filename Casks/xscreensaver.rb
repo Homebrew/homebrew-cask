492b97409e890a3af53ae4c3cb05a09e6250bc39
Mike English <mike.english@atomicobject.com>
2014-06-17 20:05:57 -0400

    Add XScreenSaver v5.29

---
+++ Casks/xscreensaver.rb
@@ -0,0 +1,7 @@
+class Xscreensaver < Cask
+  url 'http://www.jwz.org/xscreensaver/xscreensaver-5.29.dmg'
+  homepage 'http://www.jwz.org/xscreensaver/'
+  version '5.29'
+  sha256 '5821584b18dc400da5b74b58e27b39a5c9b36d12e87378bfbe4e40e569fbc78e'
+  install 'Install Everything.pkg'
+end
