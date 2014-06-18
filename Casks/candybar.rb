0368b51723262fcf5cb44c7b5ab1ed0cc1f8584b
Roland Walker <walker@pobox.com>
2014-06-18 10:55:20 -0400

    use HTTPS url in candybar.rb

--- Casks/candybar.rb
+++ Casks/candybar.rb
@@ -1,5 +1,5 @@
 class Candybar < Cask
-  url 'http://panic.com/candybar/d/CandyBar%203.3.4.zip'
+  url 'https://panic.com/candybar/d/CandyBar%203.3.4.zip'
   homepage 'http://www.panic.com/blog/candybar-mountain-lion-and-beyond'
   version '3.3.4'
   sha256 'f305596f195445016b35c9d99a40789c6671195e9cbad0b6e92e808b6c633ad6'
