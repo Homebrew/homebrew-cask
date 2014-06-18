f57293efc34a520469236d4309c405f839439889
Roland Walker <walker@pobox.com>
2014-06-18 10:55:25 -0400

    use HTTPS url in send-to-kindle.rb

--- Casks/send-to-kindle.rb
+++ Casks/send-to-kindle.rb
@@ -1,5 +1,5 @@
 class SendToKindle < Cask
-  url 'http://s3.amazonaws.com/sendtokindle/SendToKindleForMac-installer-v1.0.0.218.pkg'
+  url 'https://s3.amazonaws.com/sendtokindle/SendToKindleForMac-installer-v1.0.0.218.pkg'
   homepage 'http://www.amazon.com/gp/sendtokindle/mac'
   version '1.0.0.218'
   sha256 '9c3a9eca8fe9f09ff43dfb4132a8be163ef517008979075df3d947699c421494'
