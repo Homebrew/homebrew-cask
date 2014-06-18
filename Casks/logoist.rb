4027683698b359fda92065f7b05e082222e99a11
Tolbkni Kao <tolbkni@gmail.com>
2014-06-18 22:21:36 +0800

    Add Logoist.app

---
+++ Casks/logoist.rb
@@ -0,0 +1,7 @@
+class Logoist < Cask
+  url 'http://www.syniumsoftware.com/download/logoist.dmg'
+  homepage 'http://www.syniumsoftware.com/logoist/'
+  version 'latest'
+  sha256 :no_check
+  link 'Logoist.app'
+end
