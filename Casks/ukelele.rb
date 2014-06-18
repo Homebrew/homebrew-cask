7008a9e823f9ae9d3d29b09d8b4315bf56a8e9c0
Roland Walker <walker@pobox.com>
2014-06-18 10:55:27 -0400

    use HTTPS url in ukelele.rb

--- Casks/ukelele.rb
+++ Casks/ukelele.rb
@@ -1,5 +1,5 @@
 class Ukelele < Cask
-  url 'http://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Ukelele_2.2.8&filename=Ukelele_2.2.8.dmg'
+  url 'https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Ukelele_2.2.8&filename=Ukelele_2.2.8.dmg'
   appcast 'http://scripts.sil.org/cms/scripts/render_download.php?site_id=nrsi&format=file&media_id=ukelele_su_feed&filename=ukelele_su_feed.xml'
   homepage 'http://scripts.sil.org/ukelele'
   version '2.2.8'
