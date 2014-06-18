3927a89de96508f680c9ce60e812bb40465a88b3
Roland Walker <walker@pobox.com>
2014-06-18 10:55:25 -0400

    use HTTPS url in scribus.rb

--- Casks/scribus.rb
+++ Casks/scribus.rb
@@ -1,5 +1,5 @@
 class Scribus < Cask
-  url 'http://downloads.sourceforge.net/project/scribus/scribus/1.4.4/scribus-1.4.4.dmg'
+  url 'https://downloads.sourceforge.net/project/scribus/scribus/1.4.4/scribus-1.4.4.dmg'
   homepage 'http://www.scribus.net/canvas/Scribus'
   version '1.4.4'
   sha256 '57cdfacbfa6c60c035b746ac40ea8c46718fdfd4a9ac382b3b6c56a318fa162c'
