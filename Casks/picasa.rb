2b393790f632a8cb5e1e8785ac6acdb50ccec2f0
Roland Walker <walker@pobox.com>
2014-06-18 10:55:24 -0400

    use HTTPS url in picasa.rb

--- Casks/picasa.rb
+++ Casks/picasa.rb
@@ -1,5 +1,5 @@
 class Picasa < Cask
-  url 'http://dl.google.com/photos/picasamac39.dmg'
+  url 'https://dl.google.com/photos/picasamac39.dmg'
   homepage 'http://picasa.google.com/'
   version '3.9'
   sha256 'a3c4f0f0fdb94471e024469f83b29be333a8f3359aea1c5e0bfeca0d6b5c5d62'
