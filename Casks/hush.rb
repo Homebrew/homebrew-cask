93ef9c8e1ae11eb2b90a1962a8637c98666bbb99
Roland Walker <walker@pobox.com>
2014-06-18 10:55:22 -0400

    use HTTPS url in hush.rb

--- Casks/hush.rb
+++ Casks/hush.rb
@@ -1,5 +1,5 @@
 class Hush < Cask
-  url 'http://coffitivity.com/hush/files/Hush.dmg.zip'
+  url 'https://coffitivity.com/hush/files/Hush.dmg.zip'
   homepage 'http://coffitivity.com/hush/'
   version '1.0'
   sha256 'da31f0abd7c531e87c9f171493d318535cb24f1335e7e2b1c0fa68eeed685b2e'
