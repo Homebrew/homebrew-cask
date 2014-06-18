de26a7a350c971b6f4789ae0905f0d7a5806a902
Roland Walker <walker@pobox.com>
2014-06-18 11:11:06 -0400

    use HTTPS url in textmate.rb

--- Casks/textmate.rb
+++ Casks/textmate.rb
@@ -1,5 +1,5 @@
 class Textmate < Cask
-  url 'http://api.textmate.org/downloads/release'
+  url 'https://api.textmate.org/downloads/release'
   homepage 'http://macromates.com/'
   version 'latest'
   sha256 :no_check
