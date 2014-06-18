c4f605294228a115140ef33bce654410a589aac0
Roland Walker <walker@pobox.com>
2014-06-18 11:11:03 -0400

    use HTTPS url in anvil.rb

--- Casks/anvil.rb
+++ Casks/anvil.rb
@@ -1,5 +1,5 @@
 class Anvil < Cask
-  url 'http://sparkler.herokuapp.com/apps/3/download'
+  url 'https://sparkler.herokuapp.com/apps/3/download'
   appcast 'http://sparkler.herokuapp.com/apps/3/updates.xml'
   homepage 'http://anvilformac.com/'
   version 'latest'
