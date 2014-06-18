09f7a49cafe25521b4cc2f43f6e2d28134364ebd
Roland Walker <walker@pobox.com>
2014-06-18 10:55:27 -0400

    use HTTPS url in visit.rb

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -1,5 +1,5 @@
 class Visit < Cask
-  url 'http://portal.nersc.gov/svn/visit/trunk/releases/2.7.2/VisIt-2.7.2.dmg'
+  url 'https://portal.nersc.gov/svn/visit/trunk/releases/2.7.2/VisIt-2.7.2.dmg'
   homepage 'https://wci.llnl.gov/codes/visit/home.html'
   version '2.7.2'
   sha256 '3c1f1b201358876c1b23717a1b3a2c5a1777755e3fc7f242c08b53a033402fbc'
