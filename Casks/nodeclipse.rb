b346826e24318afa79d7bb05ce09f8a9f7f00b37
Roland Walker <walker@pobox.com>
2014-06-18 10:55:24 -0400

    use HTTPS url in nodeclipse.rb

--- Casks/nodeclipse.rb
+++ Casks/nodeclipse.rb
@@ -1,5 +1,5 @@
 class Nodeclipse < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/nodeclipse/Enide-Studio-2014-011-20140228-macosx-cocoa-x86_64.zip'
+  url 'https://downloads.sourceforge.net/sourceforge/nodeclipse/Enide-Studio-2014-011-20140228-macosx-cocoa-x86_64.zip'
   homepage 'http://www.nodeclipse.org/'
   version '0.11-preview'
   sha256 '01f630446313cb981ce2ee9b934977cfdbf318e09761dee244a3256f9a559003'
