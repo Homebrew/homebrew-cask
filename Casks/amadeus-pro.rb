b44cb17c3df4af9f9db70d3b448fc83231626e27
Roland Walker <walker@pobox.com>
2014-06-18 11:11:03 -0400

    use HTTPS url in amadeus-pro.rb

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -1,5 +1,5 @@
 class AmadeusPro < Cask
-  url 'http://s3.amazonaws.com/AmadeusPro2/AmadeusPro.dmg'
+  url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.dmg'
   homepage 'http://www.hairersoft.com/pro.html'
   version 'latest'
   sha256 :no_check
