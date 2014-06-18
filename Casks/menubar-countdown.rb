7b5ea38ad39612671bcae590f0d426d66cc0b9f4
Roland Walker <walker@pobox.com>
2014-06-18 10:55:23 -0400

    use HTTPS url in menubar-countdown.rb

--- Casks/menubar-countdown.rb
+++ Casks/menubar-countdown.rb
@@ -1,5 +1,5 @@
 class MenubarCountdown < Cask
-  url 'http://s3.amazonaws.com/capablehands/downloads/MenubarCountdown-1.2.zip'
+  url 'https://s3.amazonaws.com/capablehands/downloads/MenubarCountdown-1.2.zip'
   homepage 'http://capablehands.net/menubarcountdown'
   version '1.2'
   sha256 '4ee0a7a87dbd4013c461b59316c749a5f9a92160bdf6d90afb1ff029f9381c01'
