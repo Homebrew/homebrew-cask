c175d3a20b7124bdac3112f7c426320bad234d13
Roland Walker <walker@pobox.com>
2014-06-18 10:55:28 -0400

    use HTTPS url in ynab.rb

--- Casks/ynab.rb
+++ Casks/ynab.rb
@@ -1,5 +1,5 @@
 class Ynab < Cask
-  url 'http://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.451.dmg'
+  url 'https://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.451.dmg'
   homepage 'http://www.youneedabudget.com/'
   version '4.3.451'
   sha256 '088f61e0e772193f6dac61e22067b1696246f214d7c9887f95d180cfabb57e25'
