0ee7c0b666f7aa42623a9c6222fbcd18ccba75d4
Roland Walker <walker@pobox.com>
2014-06-18 10:55:25 -0400

    use HTTPS url in psychopy.rb

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,5 +1,5 @@
 class Psychopy < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/psychpy/StandalonePsychoPy-1.79.01-OSX.dmg'
+  url 'https://downloads.sourceforge.net/sourceforge/psychpy/StandalonePsychoPy-1.79.01-OSX.dmg'
   homepage 'http://www.psychopy.org/'
   version '1.79.01'
   sha256 'a2b174aee30ca4a8858238404adfd9271da9ec14243aa30be0b7f9a6abb047b8'
