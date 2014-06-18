08d8f2dc5bf50f297df01450fc248e84fe0c5332
Roland Walker <walker@pobox.com>
2014-06-18 11:11:05 -0400

    use HTTPS url in noisy.rb

--- Casks/noisy.rb
+++ Casks/noisy.rb
@@ -1,5 +1,5 @@
 class Noisy < Cask
-  url 'http://github.com/downloads/jonshea/Noisy/Noisy.zip'
+  url 'https://github.com/downloads/jonshea/Noisy/Noisy.zip'
   homepage 'https://github.com/jonshea/Noisy'
   version 'latest'
   sha256 :no_check
