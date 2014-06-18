0a4a927d9c12329fc600d716b8e83def076e45aa
Roland Walker <walker@pobox.com>
2014-06-18 10:55:26 -0400

    use HTTPS url in synergy.rb

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,5 +1,5 @@
 class Synergy < Cask
-  url 'http://synergy-project.org/files/packages/synergy-1.5.0-r2278-MacOSX109-x86_64.dmg'
+  url 'https://synergy-project.org/files/packages/synergy-1.5.0-r2278-MacOSX109-x86_64.dmg'
   homepage 'http://synergy-foss.org/'
   version '1.5.0'
   sha256 'b7f024720a3174ad68aa3489fa8674ca4bdc95a785b82044d57685325f60c913'
