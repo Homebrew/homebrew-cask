268520a0a60b17cbbc20c28dbeb932269aee1140
Roland Walker <walker@pobox.com>
2014-06-18 10:55:23 -0400

    use HTTPS url in logisim.rb

--- Casks/logisim.rb
+++ Casks/logisim.rb
@@ -2,7 +2,7 @@ class Logisim < Cask
   version '2.7.1'
   sha256 '41c5555b8021794e268a3fc2c9c51301d919680ae780b000b99380fc492bae7c'
 
-  url "http://downloads.sourceforge.net/project/circuit/#{version.gsub(/\d$/, 'x')}/#{version}/logisim-macosx-#{version}.tar.gz"
+  url "https://downloads.sourceforge.net/project/circuit/#{version.gsub(/\d$/, 'x')}/#{version}/logisim-macosx-#{version}.tar.gz"
   homepage 'http://ozark.hendrix.edu/~burch/logisim/'
   link 'Logisim.app'
 end
