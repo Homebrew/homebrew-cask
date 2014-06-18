5d7b2e88be2af69b359ce5d7a62bd824e683e059
Roland Walker <walker@pobox.com>
2014-06-18 10:55:27 -0400

    use HTTPS url in tuxguitar.rb

--- Casks/tuxguitar.rb
+++ Casks/tuxguitar.rb
@@ -1,5 +1,5 @@
 class Tuxguitar < Cask
-  url 'http://downloads.sourceforge.net/project/tuxguitar/TuxGuitar/TuxGuitar-1.2/tuxguitar-1.2-macosx10.5-cocoa-64.dmg'
+  url 'https://downloads.sourceforge.net/project/tuxguitar/TuxGuitar/TuxGuitar-1.2/tuxguitar-1.2-macosx10.5-cocoa-64.dmg'
   homepage 'http://www.tuxguitar.com.ar/'
   version '1.2'
   sha256 '2d79ffdfdde9205073fdaa1c34701ea8f1961f822709b5270dc57555eb926d16'
