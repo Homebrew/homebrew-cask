ef2f3bec00473f372382d78d7e9030549fed16fb
Roland Walker <walker@pobox.com>
2014-06-18 10:55:27 -0400

    use HTTPS url in vyprvpn.rb

--- Casks/vyprvpn.rb
+++ Casks/vyprvpn.rb
@@ -1,5 +1,5 @@
 class Vyprvpn < Cask
-  url 'http://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/2.3.3.1851/VyprVPN_v2.3.3.1851.dmg'
+  url 'https://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/2.3.3.1851/VyprVPN_v2.3.3.1851.dmg'
   homepage 'http://www.goldenfrog.com/vyprvpn'
   version '2.3.3.1851'
   sha256 '598967057d8e932bb4e2b332c0664ce31b708ceb36b89bae4a8c2a0c07ea03ec'
