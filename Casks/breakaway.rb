227dbd9fd5ab0ddeb6a05634490c476ba43601c0
Roland Walker <walker@pobox.com>
2014-06-18 10:55:20 -0400

    use HTTPS url in breakaway.rb

--- Casks/breakaway.rb
+++ Casks/breakaway.rb
@@ -1,5 +1,5 @@
 class Breakaway < Cask
-  url 'http://downloads.sourceforge.net/project/breakaway/breakaway-2.0.1.zip'
+  url 'https://downloads.sourceforge.net/project/breakaway/breakaway-2.0.1.zip'
   homepage 'http://mutablecode.com/apps/breakaway.html'
   version '2.0.1'
   sha256 'df5a3d42558f9cdc778c434f88f7e062032cf33dd751ef01e6109a9848a7f76a'
