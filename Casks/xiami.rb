b0fc9c8a4e8e7ba60985e53b989746926e8ae21a
Zili Yin <viztor@users.noreply.github.com>
2014-06-18 12:44:04 +0800

    Update xiami.rb

--- Casks/xiami.rb
+++ Casks/xiami.rb
@@ -2,6 +2,6 @@ class Xiami < Cask
   url 'http://www.xiami.com/software/download?app=music_mac'
   homepage 'http://www.xiami.com/'
   version '1.3.2-1826'
-  sha256 '283c25d9312f7390cf53648926228614b8cdadf32ec627d8e3306e000cf8f989'
+  sha256 :no_check
   link 'Xiami.app'
 end
