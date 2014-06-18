885c534595cbe6677fbab4b3041e72c550a74603
Roland Walker <walker@pobox.com>
2014-06-18 10:55:22 -0400

    use HTTPS url in growlnotify.rb

--- Casks/growlnotify.rb
+++ Casks/growlnotify.rb
@@ -1,5 +1,5 @@
 class Growlnotify < Cask
-  url 'http://growl.cachefly.net/GrowlNotify-2.1.zip'
+  url 'https://growl.cachefly.net/GrowlNotify-2.1.zip'
   homepage 'http://growl.info/downloads'
   version '2.1'
   sha256 'eec601488b19c9e9b9cb7f0081638436518bce782d079f6e43ddc195727c04ca'
