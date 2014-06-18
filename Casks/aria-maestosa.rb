ab307ae32cd925bb03972e738a5548c4e706ba9f
Roland Walker <walker@pobox.com>
2014-06-18 10:55:20 -0400

    use HTTPS url in aria-maestosa.rb

--- Casks/aria-maestosa.rb
+++ Casks/aria-maestosa.rb
@@ -1,5 +1,5 @@
 class AriaMaestosa < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/ariamaestosa/AriaMaestosa-osx-1.4.9.zip'
+  url 'https://downloads.sourceforge.net/sourceforge/ariamaestosa/AriaMaestosa-osx-1.4.9.zip'
   homepage 'http://ariamaestosa.sourceforge.net'
   version '1.4.9'
   sha256 '3a8d8bc625a6e27b2312ff1c008e78b64c94faa5ef263c85c6005e72a98ec1bf'
