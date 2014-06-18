c9f78ac45583a8fdf86fe8e9d0fd4eb07c19eab0
Roland Walker <walker@pobox.com>
2014-06-18 10:55:23 -0400

    use HTTPS url in marble.rb

--- Casks/marble.rb
+++ Casks/marble.rb
@@ -1,5 +1,5 @@
 class Marble < Cask
-  url 'http://files.kde.org/marble/downloads/MacOSX/Marble-1.5.0.dmg'
+  url 'https://files.kde.org/marble/downloads/MacOSX/Marble-1.5.0.dmg'
   homepage 'http://www.marble.kde.org'
   version '1.5.0'
   sha256 '6d1bf3e02c34ef0df0d5d0311d580bfae1d5259e3db45036163a5330dc139c04'
