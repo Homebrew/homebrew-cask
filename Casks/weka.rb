417db8bbf9abc6dc5298b8c19481391cef95beae
Roland Walker <walker@pobox.com>
2014-06-18 10:55:27 -0400

    use HTTPS url in weka.rb

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -1,5 +1,5 @@
 class Weka < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/weka/weka-3-6-11-oracle-jvm.dmg'
+  url 'https://downloads.sourceforge.net/sourceforge/weka/weka-3-6-11-oracle-jvm.dmg'
   homepage 'http://www.cs.waikato.ac.nz/ml/weka/'
   version '3.6.11'
   sha256 '51643edc349f46d76b96460ab16b97cf8e2d63ae6a4f6e5ba4c89dd404b56cea'
