65e636ead019f37361dacd46964ed796de256888
Roland Walker <walker@pobox.com>
2014-06-18 10:55:26 -0400

    use HTTPS url in synology-cloud-station.rb

--- Casks/synology-cloud-station.rb
+++ Casks/synology-cloud-station.rb
@@ -1,5 +1,5 @@
 class SynologyCloudStation < Cask
-  url 'http://global.download.synology.com/download/Tools/CloudStation/3109/Mac/CloudStation-3109-Mac-Installer.dmg'
+  url 'https://global.download.synology.com/download/Tools/CloudStation/3109/Mac/CloudStation-3109-Mac-Installer.dmg'
   homepage 'http://www.synology.com/'
   version '3109'
   sha256 '32b3260a8da765840f602830dadfc95c2451e0d3eaded202ec8d315221a25d82'
