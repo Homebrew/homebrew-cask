8222f365781aa5c74f5fd12ee26cdfedead73c7c
Roland Walker <walker@pobox.com>
2014-06-18 11:11:06 -0400

    use HTTPS url in tunewiki.rb

--- Casks/tunewiki.rb
+++ Casks/tunewiki.rb
@@ -1,5 +1,5 @@
 class Tunewiki < Cask
-  url 'http://www.tunewiki.com/download/desktop/TuneWiki_Installer.dmg'
+  url 'https://www.tunewiki.com/download/desktop/TuneWiki_Installer.dmg'
   homepage 'http://www.tunewiki.com/'
   version 'latest'
   sha256 :no_check
