0f50085ed3b4b1363374bc6be248351b728cf737
Roland Walker <walker@pobox.com>
2014-06-19 16:08:54 -0400

    escape metacharacters in sourceforce URL patterns

--- lib/cask/audit.rb
+++ lib/cask/audit.rb
@@ -65,9 +65,9 @@ class Cask::Audit
   def _bad_sourceforge_url?
     return false unless cask.url.to_s =~ /sourceforge/
     valid_url_formats = [
-      %r{https?://sourceforge.net/projects/.*/files/latest/download},
-      %r{https?://downloads.sourceforge.net/},
-      %r{https?://dl.sourceforge.jp/},
+      %r{https?://sourceforge\.net/projects/.*/files/latest/download},
+      %r{https?://downloads\.sourceforge\.net/},
+      %r{https?://dl\.sourceforge\.jp/},
     ]
     valid_url_formats.none? { |format| cask.url.to_s =~ format }
   end
