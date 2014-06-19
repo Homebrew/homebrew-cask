e5ae8beec2005abd0efb4ff70e6404665cc66825
Kevin O'Connor <koconnor@panoramaed.com>
2014-06-19 10:27:59 -0400

    Upgrade radiant player to v1.2.1

--- Casks/radiant-player.rb
+++ Casks/radiant-player.rb
@@ -1,7 +1,7 @@
 class RadiantPlayer < Cask
-  url 'https://github.com/kbhomes/google-music-mac/releases/download/v1.2.0/Radiant.Player.zip'
+  url 'https://github.com/kbhomes/google-music-mac/releases/download/v1.2.1/Radiant.Player.zip'
   homepage 'http://kbhomes.github.io/google-music-mac/'
-  version '1.2.0'
-  sha256 '7a9a0d7b1a17cee599012f216cc285bcabe6a1005ddb87d531e7a44875a67131'
+  version '1.2.1'
+  sha256 'dc1ed98170e05c430779f527191717cb412e74f398324878286df4a2bbad79d4'
   link 'Radiant Player.app'
 end
