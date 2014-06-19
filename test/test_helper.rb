e7e9ca58c7f8557c38fbbe443a4ed7b3f7b334a2
Roland Walker <walker@pobox.com>
2014-06-19 10:24:35 -0400

    WIP re-enable minitest-colorize
    Travis expected to fail.  Local tests fail.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -46,8 +46,7 @@ HOMEBREW_CACHE.join('Casks').mkpath
 
 # must be called after testing_env so at_exit hooks are in proper order
 require 'minitest/autorun'
-# todo, re-enable minitest-colorize, broken under current test environment for unknown reasons
-# require 'minitest-colorize'
+require 'minitest-colorize'
 
 # Force mocha to patch MiniTest since we have both loaded thanks to homebrew's testing_env
 require 'mocha/api'
