d94956fc9b240bd15407f1d6dff6aa1bd18b1e3e
Trevor Parker <trevor@trevorparker.com>
2014-06-19 21:43:30 -0400

    Upgrade OpenTTD to 1.4.1

--- Casks/openttd.rb
+++ Casks/openttd.rb
@@ -1,7 +1,7 @@
 class Openttd < Cask
-  url 'http://binaries.openttd.org/releases/1.4.0/openttd-1.4.0-macosx-universal.zip'
+  url 'http://binaries.openttd.org/releases/1.4.1/openttd-1.4.1-macosx-universal.zip'
   homepage 'http://openttd.org'
-  version '1.4.0'
-  sha256 'dc5d66d41cf2de3d933bff63ee62bf4e70bbd64ef7fc3d791800c0e9361fed88'
+  version '1.4.1'
+  sha256 '61987f2260375c800ac20f851b43e833a3377c7b961cc337a93353ab50e5bb40'
   link 'OpenTTD.app'
 end
