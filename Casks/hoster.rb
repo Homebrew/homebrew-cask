af9773b930256b99f74cc7fe139da15272035de9
massat <masato.hirai@gmail.com>
2014-06-20 23:01:38 +0900

    Fix install method from `link` to `install` in Hoster.app Cask.

--- Casks/hoster.rb
+++ Casks/hoster.rb
@@ -1,7 +1,8 @@
 class Hoster < Cask
   url 'http://www.redwinder.com/macapp/hoster/tmp/Hoster1.208.zip'
-  homepage 'http://gamelab.mit.edu/games/a-slower-speed-of-light/'
+  homepage 'http://www.redwinder.com/macapp/hoster/'
   version '1.2'
   sha256 'aad3404d5127bd3299e57ecf152c911776bf47bea877be56c4a9cefbc0cbf095'
-  link 'Hoster.app'
+  install 'Hoster1.208.mpkg', :allow_untrusted => true
+  uninstall :pkgutil => 'com.redwinder.hoster.hoster.pkg'
 end
