d8fd7404dc7d9b86d216871eaaf5bfe218abe6c0
Hugo Haas <hugoh@hugoh.net>
2014-06-19 19:30:18 -0700

    Lightroom 5.5

--- Casks/adobe-photoshop-lightroom.rb
+++ Casks/adobe-photoshop-lightroom.rb
@@ -1,8 +1,8 @@
 class AdobePhotoshopLightroom < Cask
-  url 'http://download.adobe.com/pub/adobe/lightroom/mac/5.x/Lightroom_5_LS11_mac_5_4.dmg'
+  url 'http://download.adobe.com/pub/adobe/lightroom/mac/5.x/Lightroom_5_LS11_mac_5_5.dmg'
   homepage 'http://www.adobe.com/products/photoshop-lightroom.html'
-  version '5.4'
-  sha256 'd935735cac3cae088568a1fb0f3fd57a15ceb55c4daa4d64659fff002e339cee'
+  version '5.5'
+  sha256 '6b1949f0833d37a410c5e9dc54abd9595830c4818167a07c460c6d4d9d288f17'
   install 'Adobe Photoshop Lightroom 5.pkg'
   uninstall :pkgutil => 'com.adobe.Lightroom5',
             :quit => 'com.adobe.Lightroom5',
