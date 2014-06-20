b788eb36d95a6cf9ecc98aeabf0cd1c6eb7b6a50
massat <masato.hirai@gmail.com>
2014-06-20 22:14:14 +0900

    Add uninstall stanza.

--- Casks/hoster.rb
+++ Casks/hoster.rb
@@ -4,4 +4,5 @@ class Hoster < Cask
   version '1.2'
   sha256 'aad3404d5127bd3299e57ecf152c911776bf47bea877be56c4a9cefbc0cbf095'
   install 'Hoster1.208.mpkg', :allow_untrusted => true
+  uninstall :pkgutil => 'com.redwinder.hoster.hoster.pkg'
 end
