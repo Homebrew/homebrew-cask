c0b75c7cd08a466c2bbf37ac4bc38785d7e33e80
Beng Hee Eu <beng@benghee.eu>
2014-06-21 18:43:34 +0800

    Razer Synapse 2.0 v1.25

--- Casks/razer-synapse.rb
+++ Casks/razer-synapse.rb
@@ -1,8 +1,8 @@
 class RazerSynapse < Cask
-  url 'http://dl.razerzone.com/drivers/Synapse2/mac/Razer_Synapse_2.0_Mac_Driver_v1.24.dmg'
+  url 'http://dl.razerzone.com/drivers/Synapse2/mac/Razer_Synapse_2.0_Mac_Driver_v1.25.dmg'
   homepage 'http://www.razerzone.com/synapse2'
-  version '2.0 v1.24'
-  sha256 '1db4c1ea6428a5d8ea7823c05fa1e48d51424a7b8b665611e6efa81d32d39a6d'
+  version '2.0 v1.25'
+  sha256 'e0f2f3504c40147982dadf41a33d5606fce083ce36f63bae0cebf360fbc7ea22'
   install 'Razer Synapse 2.0.pkg'
   uninstall :script => '/Applications/Utilities/Uninstall Razer Synapse 2.0.app/Contents/MacOS/Uninstall Razer Synapse 2.0'
 end
