da12239282719f4f68e064837e4f356554ed39f8
srba <srba@users.noreply.github.com>
2014-06-22 23:28:13 +0200

    Update tapaal.rb
    Updated the URL for TAPAAL project into the most recent version.

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -1,7 +1,7 @@
 class Tapaal < Cask
-  url 'http://www.tapaal.net/fileadmin/download/tapaal-2.4/tapaal-2.4.3-mac64.dmg'
+  url 'http://www.tapaal.net/fileadmin/download/tapaal-3.1/tapaal-3.1.0-mac64.dmg'
   homepage 'http://www.tapaal.net'
-  version '2.4.3'
+  version '3.1.0'
   sha256 '16e9f1a008309595ea44a958d984b8a46faad1a688d0d0edee26bcf0ba1e42b9'
   link 'Tapaal.app'
 end
