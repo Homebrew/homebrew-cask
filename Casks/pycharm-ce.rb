3e078daf7cdf1a61bf100ef20ecc011de34194a6
Fei Gao <feigao.chn@gmail.com>
2014-06-23 11:14:55 +0800

    Upgrade Pycharm CE.app to v2.4.1

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,7 +1,7 @@
 class PycharmCe < Cask
-  url 'http://download.jetbrains.com/python/pycharm-community-3.4.dmg'
+  url 'http://download.jetbrains.com/python/pycharm-community-3.4.1.dmg'
   homepage 'http://www.jetbrains.com/pycharm'
-  version '3.4'
-  sha256 'da083ac2761e4060e6b3c7446671ae9487045114a82dd64894e113dc9eb3e596'
+  version '3.4.1'
+  sha256 '18b4d2d61badb81feaf22a51a7c76b85618e5a7a01f4f4c5e46079d134c64002'
   link 'PyCharm CE.app'
 end
