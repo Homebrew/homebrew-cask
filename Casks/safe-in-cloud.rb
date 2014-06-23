8ff2cc67f6da1cc9f2de8545b3f34503d6637586
Vítor Galvão <info@vitorgalvao.com>
2014-06-23 02:32:45 +0100

    updated safe-in-cloud (latest)

--- Casks/safe-in-cloud.rb
+++ Casks/safe-in-cloud.rb
@@ -1,7 +1,7 @@
 class SafeInCloud < Cask
   url 'http://www.safe-in-cloud.com/images/downloads/Safe%20In%20Cloud.dmg'
   homepage 'http://www.safe-in-cloud.com'
-  version '1.0'
-  sha256 'e4920ef423f10b6189f25049f00e876ebd09b111fd75f36289d0a893de49e94f'
+  version 'latest'
+  sha256 :no_check
   link 'Safe In Cloud.app'
 end
