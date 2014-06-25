46163d996d18160f0d69df35e975b618c6a9c850
Joel Kuzmarski <leoj3n@gmail.com>
2014-06-24 20:11:05 -0500

    Fix Padre homepage
    Add `http://` prefix.

--- Casks/padre.rb
+++ Casks/padre.rb
@@ -1,6 +1,6 @@
 class Padre < Cask
   url 'http://wildperl.com/wp-content/uploads/Padre/padre-osx-uni-0-92-0.dmg'
-  homepage 'padre.perlide.org'
+  homepage 'http://padre.perlide.org'
   version '0.92.0'
   sha256 '481088a40bc71bd62d13ac23ed8fbfa3b8fc651587b9e47461795a9a78032d87'
   link 'Padre.app'
