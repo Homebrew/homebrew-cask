9f8bd4efd3de605ec891f0a276accc608da15a62
Vítor Galvão <info@vitorgalvao.com>
2014-06-25 15:59:07 +0100

    made filebot easier to update

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -1,11 +1,11 @@
 class Filebot < Cask
-  homepage "http://www.filebot.net/"
-  url "https://downloads.sourceforge.net/project/filebot/filebot/HEAD/FileBot_4.1.1/FileBot_4.1.app.tar.gz"
-  sha256 "4aa3dd0532bd0266ec3bcb60553507dfb51c054a406fba95bd0c6d5c0dce7ec2"
-  version "4.1.1"
+  version '4.1.1'
+  sha256 '4aa3dd0532bd0266ec3bcb60553507dfb51c054a406fba95bd0c6d5c0dce7ec2'
 
-  link "FileBot.app"
-  binary "FileBot.app/Contents/MacOS/filebot.sh", :target => "filebot"
+  homepage 'http://www.filebot.net/'
+  url "https://downloads.sourceforge.net/project/filebot/filebot/HEAD/FileBot_#{version}/FileBot_#{version.gsub(/\.\d/, '')}.app.tar.gz"
 
-  caveats "FileBot requires Java 8. Run `java -version` to verify."
+  link 'FileBot.app'
+  binary 'FileBot.app/Contents/MacOS/filebot.sh', :target => 'filebot'
+  caveats 'FileBot requires Java 8. Run `java -version` to verify.'
 end
