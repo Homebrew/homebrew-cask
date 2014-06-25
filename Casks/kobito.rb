b2ee56af1f8a4cd579c656886bc1738d09b895e2
Yusuke Komatsu <tienlen042@gmail.com>
2014-06-25 18:47:08 +0900

    Update kobito to 1.9.2

--- Casks/kobito.rb
+++ Casks/kobito.rb
@@ -1,8 +1,8 @@
 class Kobito < Cask
-  url 'http://kobito.qiita.com/download/Kobito_v1.9.1.zip'
+  url 'http://kobito.qiita.com/download/Kobito_v1.9.2.zip'
   appcast 'http://kobito.qiita.com/appcast.xml'
   homepage 'http://kobito.qiita.com/'
-  version '1.9.1'
-  sha256 '62fd1379ddd70a6dc2993aa945818c4dbb997bd06c2e6ecbbc90d3187861104c'
+  version '1.9.2'
+  sha256 '8129e70fda4595fe4e4be5406f2383f5900421da2bc9b7c53cd0e74762f42ba2'
   link 'Kobito.app'
 end
