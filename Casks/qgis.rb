c827cb923645d9b968b3278ba7b0bad6415c95b5
Lee Hanxue <hanxue.developer@gmail.com>
2014-06-26 11:57:43 +0800

    New QGIS 2.0.0 cask
    - See issue #5074 - requires GDAL Framework and Matplotlib

---
+++ Casks/qgis.rb
@@ -0,0 +1,22 @@
+class Qgis < Cask
+  version '2.2.0-10'
+  sha256 '00ddf8511c11664600b680d569153a040bfd7579b40ccab53659d23999d4ad43'
+
+  url "http://www.kyngchaos.com/files/software/qgis/QGIS-#{version}.dmg"
+  homepage 'http://www.kyngchaos.com/software/qgis'
+  install 'Install QGIS.pkg'
+  uninstall :pkgutil => 'org.qgis.qgis-*'
+  caveats do
+    <<-EOS.undent
+    #{@cask} requires the GDAL framework and Matplotlib to be installed first,
+    otherwise the installation will fail. In case of issues, try installing it
+
+        brew cask install gdal-framework matplotlib
+
+    and then reinstall QGIS
+
+        brew cask uninstall qgis
+        brew cask install qgis
+    EOS
+  end
+end
