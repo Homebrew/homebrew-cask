40ae734127a355ef110546beb44faffeddd05e17
Martin madsen <martin2madsen@gmail.com>
2014-06-26 19:53:17 +0200

    Skim 1.4.9
    Closes #5083

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,8 +1,10 @@
 class Skim < Cask
-  url 'https://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.4.8/Skim-1.4.8.dmg'
+  version '1.4.9'
+  sha256 'b8964263e06b37b81443659d4c96385866d5b0af6e65ce393095b46e95591546'
+
+  url 'https://downloads.sourceforge.net/project/skim-app/Skim/Skim-1.4.9/Skim-1.4.9.dmg'
   appcast 'http://skim-app.sourceforge.net/skim.xml'
   homepage 'http://skim-app.sourceforge.net/'
-  version '1.4.8'
-  sha256 '664e2e3dae52758ccff84f1205da85022c041cf92273002979e7aa16d51277ff'
+
   link 'Skim.app'
 end
