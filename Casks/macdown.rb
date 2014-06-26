d23f15309ae400c7ce505f90b743ab4f3ca9dc96
Lee Hanxue <hanxue.developer@gmail.com>
2014-06-26 15:56:22 +0800

    MacDown 0.1
    New cask MacDown, version 0.1 - with appcast xml
    
    Closes #5076

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -2,7 +2,8 @@ class Macdown < Cask
   version '0.1'
   sha256 'd050ee502fb27fd0f431abc1cc2f4a490670346821e269415d184bf08f245eb2'
 
-  url 'https://github.com/uranusjr/macdown/releases/download/v0.1/MacDown.app.zip'
+  url 'http://macdown.uranusjr.com/download/v0.1/'
+  appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml'
   homepage 'http://macdown.uranusjr.com/'
 
   link 'MacDown.app'
