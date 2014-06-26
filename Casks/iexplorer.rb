668b1a0391c070137a007fbac0b741e1d6ea819c
Beng Hee Eu <beng@benghee.eu>
2014-06-26 12:03:16 +0800

    iExplorer 3.3.2.0
    Closes #5073

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,8 +1,10 @@
 class Iexplorer < Cask
-  url 'http://downloads.macroplant.com/iExplorer-3.3.0.1.dmg'
-  appcast 'http://www.macroplant.com/iexplorer/ie3-appcast.xml'
+  version '3.3.2.0'
+  sha256 'c57c435599ac2480fb09af753fd42208034183f1176121ad1894e5865de1ce39'
+
   homepage 'http://www.macroplant.com/'
-  version '3.3.0.1'
-  sha256 '2cfbf10e3c8e05fefe98fcd15f1203bc04dd67399b8294c0159b9b18f4e45907'
+  url 'http://downloads.macroplant.com/iExplorer-3.3.2.0.dmg'
+  appcast 'http://www.macroplant.com/iexplorer/ie3-appcast.xml'
+
   link 'iExplorer.app'
 end
