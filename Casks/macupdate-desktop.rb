8584025c05309a28cb58935f50518e1b0b479027
Beng Hee Eu <beng@benghee.eu>
2014-06-26 12:00:20 +0800

    MacUpdate Desktop 6.0; suppress move to /Applications alert
    Closes #5072

--- Casks/macupdate-desktop.rb
+++ Casks/macupdate-desktop.rb
@@ -1,8 +1,14 @@
 class MacupdateDesktop < Cask
-  url 'https://www.macupdate.com/download/8544/MacUpdate-Desktop-5.2.3.dmg'
-  appcast 'https://www.macupdate.com/desktop/updates.xml'
+  version '6.0'
+  sha256 '5d1b967e20a5c94f37010da576a87f17a49beadae6eac5dab253c605fd3ed338'
+
   homepage 'https://www.macupdate.com/desktop'
-  version '5.2.3'
-  sha256 'd636dc76d1e4573b885f1b65aa5cdad3435ba3cc15387c4b3100f7045e75dfa8'
+  url 'http://dl.macupdate.com/MacUpdateDesktop6.zip'
+  appcast 'https://www.macupdate.com/desktop/updates.xml'
+
   link 'MacUpdate Desktop.app'
+  after_install do
+    # Don't ask to move the app bundle to /Applications
+    system '/usr/bin/defaults', 'write', 'com.macupdate.desktop6', 'moveToApplicationsFolderAlertSuppress', '-bool', 'true'
+  end
 end
