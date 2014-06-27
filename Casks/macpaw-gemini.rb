a1c46f36ea84c475b127ff2d004f6564b297ec34
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat macpaw-gemini.rb according to readability conventions

--- Casks/macpaw-gemini.rb
+++ Casks/macpaw-gemini.rb
@@ -1,8 +1,10 @@
 class MacpawGemini < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://dl.devmate.com/download/com.macpaw.site.Gemini/macpaw%20gemini.dmg'
   appcast 'http://updates.devmate.com/com.macpaw.site.Gemini.xml'
   homepage 'http://macpaw.com/gemini'
-  version 'latest'
-  sha256 :no_check
+
   link 'MacPaw Gemini.app'
 end
