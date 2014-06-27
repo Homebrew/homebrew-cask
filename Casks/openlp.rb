921e1658442e4c2dcc0ad0e6a3b5e4240c368b1b
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat openlp.rb according to readability conventions

--- Casks/openlp.rb
+++ Casks/openlp.rb
@@ -1,7 +1,9 @@
 class Openlp < Cask
-  url 'http://builds.openlp.org/OpenLP-2.0.4.dmg'
-  homepage 'http://openlp.org'
   version '2.0.4'
   sha256 '24850da1e2d75b17d76cf102721d4c5e56d8faaa97ce1194577264c8edc6cc70'
+
+  url 'http://builds.openlp.org/OpenLP-2.0.4.dmg'
+  homepage 'http://openlp.org'
+
   link 'OpenLP.app'
 end
