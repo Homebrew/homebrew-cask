b7eb5e30fb33eabc941c81f6d512ab9661bf22a3
ndr <ndr@qef.io>
2014-06-27 02:01:42 +0200

    Reformat servus.rb according to readability conventions

--- Casks/servus.rb
+++ Casks/servus.rb
@@ -1,7 +1,9 @@
 class Servus < Cask
-  url 'https://servus.io/download'
-  homepage 'https://servus.io/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://servus.io/download'
+  homepage 'https://servus.io/'
+
   link 'Servus.app'
 end
