29db5e3a2a2d6e30f681606ab5cea824a9139176
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat slimboat.rb according to readability conventions

--- Casks/slimboat.rb
+++ Casks/slimboat.rb
@@ -1,7 +1,9 @@
 class Slimboat < Cask
-  url 'http://www.slimboat.com/release/slimboat.dmg'
-  homepage 'http://www.slimboat.com'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.slimboat.com/release/slimboat.dmg'
+  homepage 'http://www.slimboat.com'
+
   link 'SlimBoat.app'
 end
