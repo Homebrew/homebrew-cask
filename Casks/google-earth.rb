dbcd12cfaacc5ecddfd05e6e926c3f6841d45ece
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat google-earth.rb according to readability conventions

--- Casks/google-earth.rb
+++ Casks/google-earth.rb
@@ -1,7 +1,9 @@
 class GoogleEarth < Cask
-  url 'https://dl.google.com/earth/client/advanced/current/GoogleEarthMac-Intel.dmg'
-  homepage 'https://www.google.com/earth/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://dl.google.com/earth/client/advanced/current/GoogleEarthMac-Intel.dmg'
+  homepage 'https://www.google.com/earth/'
+
   link 'Google Earth.app'
 end
