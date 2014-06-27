c71cdf193c77f60010a96d26db5f2495ba89386f
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat cord.rb according to readability conventions

--- Casks/cord.rb
+++ Casks/cord.rb
@@ -1,8 +1,10 @@
 class Cord < Cask
+  version '0.5.7'
+  sha256 '8f505b12b94167100b3b8b44ed3cee32ffcc94b73dc44fe0ecc896151f114100'
+  
   url 'https://downloads.sourceforge.net/sourceforge/cord/CoRD_0.5.7.zip'
   appcast 'http://cord.sourceforge.net/sparkle.xml'
   homepage 'http://cord.sourceforge.net/'
-  version '0.5.7'
-  sha256 '8f505b12b94167100b3b8b44ed3cee32ffcc94b73dc44fe0ecc896151f114100'
+
   link 'CoRD.app'
 end
