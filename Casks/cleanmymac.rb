7fb84e34d7f6a53f595f6bdb1f7c97fef14ab14a
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat cleanmymac.rb according to readability conventions

--- Casks/cleanmymac.rb
+++ Casks/cleanmymac.rb
@@ -1,8 +1,10 @@
 class Cleanmymac < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://dl.devmate.com/com.macpaw.CleanMyMac2/CleanMyMac2.dmg'
   appcast 'http://updates.devmate.com/com.macpaw.CleanMyMac2.xml'
   homepage 'http://macpaw.com/cleanmymac'
-  version 'latest'
-  sha256 :no_check
+
   link 'CleanMyMac 2.app'
 end
