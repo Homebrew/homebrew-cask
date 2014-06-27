499d2d9f5a60f7acbe74fe80702b4d6ebb17df19
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat noiz2sa.rb according to readability conventions

--- Casks/noiz2sa.rb
+++ Casks/noiz2sa.rb
@@ -1,7 +1,9 @@
 class Noiz2sa < Cask
-  url 'http://workram.com/archives/Noiz2sa-for-OS-X-0.51.5.dmg'
-  homepage 'http://workram.com/games/noiz2sa/'
   version '0.51.5'
   sha256 'eb4d7f0a133b5e1541edb3b13209af58093f9a6a9fcc1296fec88552a967306d'
+
+  url 'http://workram.com/archives/Noiz2sa-for-OS-X-0.51.5.dmg'
+  homepage 'http://workram.com/games/noiz2sa/'
+
   link 'Noiz2sa.app'
 end
