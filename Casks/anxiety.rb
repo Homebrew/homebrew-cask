cba29811bbff8594ebf53cdc02cd990cf94513c9
ndr <ndr@qef.io>
2014-06-27 02:00:56 +0200

    Reformat anxiety.rb according to readability conventions

--- Casks/anxiety.rb
+++ Casks/anxiety.rb
@@ -1,9 +1,11 @@
 class Anxiety < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.anxietyapp.com/Anxiety.zip'
   appcast 'http://www.anxietyapp.com/appcast/appcast.xml'
   homepage 'http://www.anxietyapp.com/'
-  version 'latest'
-  sha256 :no_check
+
   nested_container 'Anxiety.dmg'
   link 'Anxiety.app'
 end
