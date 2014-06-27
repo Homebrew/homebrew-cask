54005c7d18de09733eb0320046312e85050a6fae
ndr <ndr@qef.io>
2014-06-27 02:01:06 +0200

    Reformat debt-quencher.rb according to readability conventions

--- Casks/debt-quencher.rb
+++ Casks/debt-quencher.rb
@@ -1,8 +1,10 @@
 class DebtQuencher < Cask
+  version '1.2.2'
+  sha256 '2083fdb28d3138998a0d6b4b5c4405a18a2e8dd7b71554786386623313c166ad'
+  
   url 'http://downloads.nothirst.com/DebtQuencher_1.2.zip'
   appcast 'http://nothirst.com/feeds/DebtQuencherAppcast.xml'
   homepage 'http://nothirst.com/debtquencher/'
-  version '1.2.2'
-  sha256 '2083fdb28d3138998a0d6b4b5c4405a18a2e8dd7b71554786386623313c166ad'
+
   link 'Debt Quencher.app'
 end
