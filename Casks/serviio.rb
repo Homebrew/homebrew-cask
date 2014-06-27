6ad18f34aae4384fe8202a562339133c86e1dec6
ndr <ndr@qef.io>
2014-06-27 02:01:42 +0200

    Reformat serviio.rb according to readability conventions

--- Casks/serviio.rb
+++ Casks/serviio.rb
@@ -1,8 +1,10 @@
 class Serviio < Cask
-  url 'http://download.serviio.org/releases/serviio-1.4.1.2-osx.tar.gz'
-  homepage 'http://serviio.org/'
   version '1.4.1.2'
   sha256 '132ed6ba9baf466eec5d89789cd8d50163fa5b534899f10a9232e00f711707aa'
+
+  url 'http://download.serviio.org/releases/serviio-1.4.1.2-osx.tar.gz'
+  homepage 'http://serviio.org/'
+
   link 'Serviio.app'
   link 'Serviio-Console.app'
 end
