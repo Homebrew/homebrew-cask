856d99be6f9e7df9b0c14fc66ecd86079121f941
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat pentaho-data-integration.rb according to readability conventions

--- Casks/pentaho-data-integration.rb
+++ Casks/pentaho-data-integration.rb
@@ -1,7 +1,9 @@
 class PentahoDataIntegration < Cask
-  url 'https://sourceforge.net/projects/pentaho/files/latest/download'
-  homepage 'http://community.pentaho.com'
   version 'latest'
   sha256 :no_check
+
+  url 'https://sourceforge.net/projects/pentaho/files/latest/download'
+  homepage 'http://community.pentaho.com'
+
   link 'data-integration/Data Integration.app'
 end
