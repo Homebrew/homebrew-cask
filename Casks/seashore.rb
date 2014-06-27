e8dd45066972041491812ce478bc92d8e260dc12
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat seashore.rb according to readability conventions

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,7 +1,9 @@
 class Seashore < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/seashore/Seashore.zip'
-  homepage 'http://seashore.sourceforge.net/'
   version '0.5.1'
   sha256 '96463a3642f162a20b160d8df273e9b27a5fdbf9708bee6ebf6a6c8528047765'
+
+  url 'https://downloads.sourceforge.net/sourceforge/seashore/Seashore.zip'
+  homepage 'http://seashore.sourceforge.net/'
+
   link 'Seashore.app'
 end
