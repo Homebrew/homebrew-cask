4cfdd28aa717065270cd17e5c49e69986b38aa7a
ndr <ndr@qef.io>
2014-06-27 02:01:45 +0200

    Reformat speedtao.rb according to readability conventions

--- Casks/speedtao.rb
+++ Casks/speedtao.rb
@@ -1,7 +1,9 @@
 class Speedtao < Cask
-  url 'http://www.speedtao.net/beta_latest'
-  homepage 'http://www.speedtao.net/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.speedtao.net/beta_latest'
+  homepage 'http://www.speedtao.net/'
+
   link 'SpeedTao.app'
 end
