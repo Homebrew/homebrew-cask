24f972642974dcb9d0fbb1843c9e84fbb482214d
ndr <ndr@qef.io>
2014-06-27 02:00:57 +0200

    Reformat arrsync.rb according to readability conventions

--- Casks/arrsync.rb
+++ Casks/arrsync.rb
@@ -1,7 +1,9 @@
 class Arrsync < Cask
-  url 'https://sourceforge.net/projects/arrsync/files/latest/download'
-  homepage 'http://arrsync.sourceforge.net'
   version 'latest'
   sha256 :no_check
+
+  url 'https://sourceforge.net/projects/arrsync/files/latest/download'
+  homepage 'http://arrsync.sourceforge.net'
+
   link 'arRsync.app'
 end
