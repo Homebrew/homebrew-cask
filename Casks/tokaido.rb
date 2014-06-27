9138d5e50f3b419e73fc1c85a6ca9616525f7203
ndr <ndr@qef.io>
2014-06-27 02:01:49 +0200

    Reformat tokaido.rb according to readability conventions

--- Casks/tokaido.rb
+++ Casks/tokaido.rb
@@ -1,7 +1,9 @@
 class Tokaido  < Cask
-  url 'https://github.com/tokaido/tokaidoapp/releases/download/v1.0/Tokaido.zip'
-  homepage 'https://github.com/tokaido/tokaidoapp/releases'
   version '1.0'
   sha256 '72752b8e4e9f0e554661d8fb58879a82cac6dc5d19762367c89ca191f497d258'
+
+  url 'https://github.com/tokaido/tokaidoapp/releases/download/v1.0/Tokaido.zip'
+  homepage 'https://github.com/tokaido/tokaidoapp/releases'
+
   link 'Tokaido.app'
 end
