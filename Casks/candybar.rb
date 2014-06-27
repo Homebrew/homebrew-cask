acb8fc95a4432e06d52584dd525111cd527de38b
ndr <ndr@qef.io>
2014-06-27 02:01:02 +0200

    Reformat candybar.rb according to readability conventions

--- Casks/candybar.rb
+++ Casks/candybar.rb
@@ -1,8 +1,10 @@
 class Candybar < Cask
-  url 'https://panic.com/candybar/d/CandyBar%203.3.4.zip'
-  homepage 'http://www.panic.com/blog/candybar-mountain-lion-and-beyond'
   version '3.3.4'
   sha256 'f305596f195445016b35c9d99a40789c6671195e9cbad0b6e92e808b6c633ad6'
+
+  url 'https://panic.com/candybar/d/CandyBar%203.3.4.zip'
+  homepage 'http://www.panic.com/blog/candybar-mountain-lion-and-beyond'
+
   link 'CandyBar.app'
   caveats <<-EOS.undent
     Candybar is free of charge.  Visit the following link for a license
