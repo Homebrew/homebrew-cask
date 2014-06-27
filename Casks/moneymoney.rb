1ec2a98fbbafe281bcc1ce91b581ebd41561d4cf
ndr <ndr@qef.io>
2014-06-27 02:01:29 +0200

    Reformat moneymoney.rb according to readability conventions

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,8 +1,10 @@
 class Moneymoney < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://moneymoney-app.com/download/MoneyMoney.zip'
   appcast 'http://moneymoney-app.com/update/appcast.xml'
   homepage 'http://moneymoney-app.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'MoneyMoney.app'
 end
