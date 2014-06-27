478524126e83351eb10f5ffc27961bb9cf78e5ec
ndr <ndr@qef.io>
2014-06-27 02:01:12 +0200

    Reformat fourk-youtube-to-mp3.rb according to readability conventions

--- Casks/fourk-youtube-to-mp3.rb
+++ Casks/fourk-youtube-to-mp3.rb
@@ -1,7 +1,9 @@
 class FourkYoutubeToMp3 < Cask
-  url 'https://4kdownload.googlecode.com/files/4kyoutubetomp3_2.5.dmg'
-  homepage 'http://www.4kdownload.com/products/product-youtubetomp3'
   version '3.2.5'
   sha256 '6c60b0830964bf682d9140e79bd732baa01299fb5f27f4eb67cdbe94231c6094'
+
+  url 'https://4kdownload.googlecode.com/files/4kyoutubetomp3_2.5.dmg'
+  homepage 'http://www.4kdownload.com/products/product-youtubetomp3'
+
   link '4K YouTube to MP3.app'
 end
