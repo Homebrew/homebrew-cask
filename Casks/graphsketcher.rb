fe0c3bb91c5b1ae425680261a7efbfeb6f1cf9e9
ndr <ndr@qef.io>
2014-06-27 02:01:16 +0200

    Reformat graphsketcher.rb according to readability conventions

--- Casks/graphsketcher.rb
+++ Casks/graphsketcher.rb
@@ -1,7 +1,9 @@
 class Graphsketcher < Cask
-  url 'https://github.com/graphsketcher/GraphSketcher/releases/download/v2.0_test_45/GraphSketcher.zip'
-  homepage 'https://github.com/graphsketcher/GraphSketcher'
   version 'v2.0_test_45'
   sha256 '4a628aae8c02b0e236ef8749e6669d8cb7d15df9b9792818d45eededb8b57113'
+
+  url 'https://github.com/graphsketcher/GraphSketcher/releases/download/v2.0_test_45/GraphSketcher.zip'
+  homepage 'https://github.com/graphsketcher/GraphSketcher'
+
   link 'GraphSketcher/GraphSketcher.app'
 end
