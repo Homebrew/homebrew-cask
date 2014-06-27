df8f4ed09a8ca5adc3902cc53ea4301d0812da2a
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat limechat.rb according to readability conventions

--- Casks/limechat.rb
+++ Casks/limechat.rb
@@ -1,8 +1,10 @@
 class Limechat < Cask
+  version '2.42'
+  sha256 '708d10591784e5beb7ed80236d809d5cd4f992c133483bc2a82775acdc6f1f0f'
+  
   url 'https://downloads.sourceforge.net/project/limechat/limechat/LimeChat_2.42.tbz'
   appcast 'http://limechat.net/mac/appcast.xml'
   homepage 'http://limechat.net/mac/'
-  version '2.42'
-  sha256 '708d10591784e5beb7ed80236d809d5cd4f992c133483bc2a82775acdc6f1f0f'
+
   link 'LimeChat.app'
 end
