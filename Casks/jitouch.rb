4a0f716f11279c72700adc3a1f54ffc440a01955
ndr <ndr@qef.io>
2014-06-27 02:01:21 +0200

    Reformat jitouch.rb according to readability conventions

--- Casks/jitouch.rb
+++ Casks/jitouch.rb
@@ -1,7 +1,9 @@
 class Jitouch < Cask
-  url 'http://www.jitouch.com/jitouch_mavericks.zip'
-  homepage 'http://www.jitouch.com'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.jitouch.com/jitouch_mavericks.zip'
+  homepage 'http://www.jitouch.com'
+
   prefpane 'jitouch/Jitouch.prefPane'
 end
