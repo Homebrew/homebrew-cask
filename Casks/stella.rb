98b7cb55c3e7ec9ff1d97f6e399705bfdb190137
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat stella.rb according to readability conventions

--- Casks/stella.rb
+++ Casks/stella.rb
@@ -1,7 +1,9 @@
 class Stella < Cask
-  url 'https://downloads.sourceforge.net/project/stella/stella/3.9.3/Stella-3.9.3_intel-macosx.dmg'
-  homepage 'http://stella.sourceforge.net'
   version '3.9.3'
   sha256 '83870088368be20224ed51f52e9babe53a75575495279adc596203f123e8477c'
+
+  url 'https://downloads.sourceforge.net/project/stella/stella/3.9.3/Stella-3.9.3_intel-macosx.dmg'
+  homepage 'http://stella.sourceforge.net'
+
   link 'Stella.app'
 end
