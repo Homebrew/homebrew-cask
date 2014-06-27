6503a282eaab16d9ec68c47b8417a7ef8cc93430
ndr <ndr@qef.io>
2014-06-27 02:01:09 +0200

    Reformat electric-sheep.rb according to readability conventions

--- Casks/electric-sheep.rb
+++ Casks/electric-sheep.rb
@@ -1,8 +1,10 @@
 class ElectricSheep < Cask
-  url 'https://electricsheep.googlecode.com/files/electricsheep-2.7b35b.dmg'
-  homepage 'http://www.electricsheep.org'
   version '2.7b35b'
   sha256 '767062403cc93aa75192def84b509b4530caca3e271ba23267ffe79cb50da44f'
+
+  url 'https://electricsheep.googlecode.com/files/electricsheep-2.7b35b.dmg'
+  homepage 'http://www.electricsheep.org'
+
   install 'Electric Sheep.mpkg'
   uninstall :pkgutil => 'org.electricsheep.electricSheep.*'
 end
