63d4a4a2e4bc471c9c66502de5d651f39714e753
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat tickets.rb according to readability conventions

--- Casks/tickets.rb
+++ Casks/tickets.rb
@@ -1,8 +1,10 @@
 class Tickets < Cask
+  version '2.5'
+  sha256 '8f2d7879581fb2604158367fb6e7eda8bc9ebf15e97781b69b64355f2832af6e'
+  
   url 'https://www.irradiatedsoftware.com/download/Tickets.zip'
   appcast 'http://www.irradiatedsoftware.com/updates/profiles/tickets.php'
   homepage 'http://www.irradiatedsoftware.com/tickets/'
-  version '2.5'
-  sha256 '8f2d7879581fb2604158367fb6e7eda8bc9ebf15e97781b69b64355f2832af6e'
+
   link 'Tickets.app'
 end
