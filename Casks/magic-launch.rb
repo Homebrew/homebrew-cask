f534ef82aff1b92469ebbd80005916c9bdec9e03
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat magic-launch.rb according to readability conventions

--- Casks/magic-launch.rb
+++ Casks/magic-launch.rb
@@ -1,7 +1,9 @@
 class MagicLaunch < Cask
-  url 'https://www.oneperiodic.com/files/Magic%20Launch%20v1.4.3.zip'
-  homepage 'http://www.oneperiodic.com/products/magiclaunch/'
   version '1.4.3'
   sha256 'f209f58e22c5881dc226b294181cc324a9e446425548b5de35c6f26bab1c7a4d'
+
+  url 'https://www.oneperiodic.com/files/Magic%20Launch%20v1.4.3.zip'
+  homepage 'http://www.oneperiodic.com/products/magiclaunch/'
+
   prefpane 'Magic Launch.prefPane'
 end
