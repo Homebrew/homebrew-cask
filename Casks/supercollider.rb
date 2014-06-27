1ac9a742efe47c2cf7622e2ac1a4c339307b877e
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat supercollider.rb according to readability conventions

--- Casks/supercollider.rb
+++ Casks/supercollider.rb
@@ -1,7 +1,9 @@
 class Supercollider < Cask
-  url 'https://downloads.sourceforge.net/project/supercollider/Mac%20OS%20X/3.6/SuperCollider-3.6.5-OSX-universal.dmg'
-  homepage 'http://supercollider.sourceforge.net/'
   version '3.6.5'
   sha256 '3a942aed9d28071bd8354b387d1e436205a1c21f55667e17689d9b361af3409b'
+
+  url 'https://downloads.sourceforge.net/project/supercollider/Mac%20OS%20X/3.6/SuperCollider-3.6.5-OSX-universal.dmg'
+  homepage 'http://supercollider.sourceforge.net/'
+
   link 'SuperCollider/SuperCollider.app'
 end
