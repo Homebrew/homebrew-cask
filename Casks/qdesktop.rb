3899ef056402a7b76be0143bb22001336e24067d
ndr <ndr@qef.io>
2014-06-27 02:01:38 +0200

    Reformat qdesktop.rb according to readability conventions

--- Casks/qdesktop.rb
+++ Casks/qdesktop.rb
@@ -1,8 +1,10 @@
 class Qdesktop < Cask
+  version '0.1.1'
+  sha256 '1eddd3513cca892fb8e53452d79d4589ef5b1e3cd885a1f52748b6df64588094'
+  
   url 'https://bitbucket.org/qvacua/qvacua/downloads/Qdesktop-0.1.1.zip'
   appcast 'http://qvacua.com/qdesktop/appcast.xml'
   homepage 'http://qvacua.com'
-  version '0.1.1'
-  sha256 '1eddd3513cca892fb8e53452d79d4589ef5b1e3cd885a1f52748b6df64588094'
+
   link 'Qdesktop.app'
 end
