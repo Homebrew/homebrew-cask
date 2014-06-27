25ab188427a300dfb8c2ba77179c9f06e2977765
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat wacom-bamboo-tablet.rb according to readability conventions

--- Casks/wacom-bamboo-tablet.rb
+++ Casks/wacom-bamboo-tablet.rb
@@ -1,8 +1,10 @@
 class WacomBambooTablet < Cask
-  url 'http://cdn.wacom.com/U/Drivers/Mac/Consumer/533/PenTablet_5.3.3-3.dmg'
-  homepage 'http://us.wacom.com/en/support/drivers'
   version '5.3.3-3'
   sha256 '6ba4469ad70d779c4dc95e1ba71469325932601bffda74d6c1d7e6bb49d0fe70'
+
+  url 'http://cdn.wacom.com/U/Drivers/Mac/Consumer/533/PenTablet_5.3.3-3.dmg'
+  homepage 'http://us.wacom.com/en/support/drivers'
+
   install 'Install Wacom Tablet.pkg'
   uninstall :launchctl => 'com.wacom.pentablet',
     :quit => [
