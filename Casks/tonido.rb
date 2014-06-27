d56a27ae75edae877cc8b3d02391296bc8f546c9
ndr <ndr@qef.io>
2014-06-27 02:01:49 +0200

    Reformat tonido.rb according to readability conventions

--- Casks/tonido.rb
+++ Casks/tonido.rb
@@ -1,7 +1,9 @@
 class Tonido < Cask
-  url 'http://www.tonido.com/download.php?TonidoSetup.dmg'
-  homepage 'http://www.tonido.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.tonido.com/download.php?TonidoSetup.dmg'
+  homepage 'http://www.tonido.com/'
+
   link 'Tonido.app'
 end
