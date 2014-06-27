9d0e932e1d2d0d31866b7c084f443a625ee71eb4
ndr <ndr@qef.io>
2014-06-27 02:01:49 +0200

    Reformat tomahawk.rb according to readability conventions

--- Casks/tomahawk.rb
+++ Casks/tomahawk.rb
@@ -1,8 +1,10 @@
 class Tomahawk < Cask
+  version '0.7.0'
+  sha256 'df82d7c8ce9694f115a2e69e2533bde4233818afad5f6b5020bb881dd8cb6504'
+  
   url 'http://www.tomahawk-player.org/download.php?file=Tomahawk-0.7.0.dmg'
   appcast 'http://download.tomahawk-player.org/sparkle/update.php'
   homepage 'http://www.tomahawk-player.org/'
-  version '0.7.0'
-  sha256 'df82d7c8ce9694f115a2e69e2533bde4233818afad5f6b5020bb881dd8cb6504'
+
   link 'Tomahawk.app'
 end
