6776a222abc49fbee70fb8eeaa2d4abb580d903f
ndr <ndr@qef.io>
2014-06-27 02:01:36 +0200

    Reformat plex-media-server.rb according to readability conventions

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,7 +1,9 @@
 class PlexMediaServer < Cask
-  url 'http://downloads.plexapp.com/plex-media-server/0.9.9.7.429-f80a8d6/PlexMediaServer-0.9.9.7.429-f80a8d6-OSX.zip'
-  homepage 'http://plexapp.com'
   version '0.9.9.7.429'
   sha256 '7d00adaf8b3254d0a34d4c4cb6ec65f97f9fc5101a16005c2ef1297b41f71900'
+
+  url 'http://downloads.plexapp.com/plex-media-server/0.9.9.7.429-f80a8d6/PlexMediaServer-0.9.9.7.429-f80a8d6-OSX.zip'
+  homepage 'http://plexapp.com'
+
   link 'Plex Media Server.app'
 end
