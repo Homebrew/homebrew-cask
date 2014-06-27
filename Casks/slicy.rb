f759def201ca6d9c5aba71466a47d539b035944f
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat slicy.rb according to readability conventions

--- Casks/slicy.rb
+++ Casks/slicy.rb
@@ -1,8 +1,10 @@
 class Slicy < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://macrabbit.com/slicy/get/'
   appcast 'http://update.macrabbit.com/slicy/1.1.3.xml'
   homepage 'http://macrabbit.com/slicy/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Slicy.app'
 end
