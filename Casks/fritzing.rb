7e3e71f7a0b70b24434bd63f965980f539a22689
ndr <ndr@qef.io>
2014-06-27 02:01:13 +0200

    Reformat fritzing.rb according to readability conventions

--- Casks/fritzing.rb
+++ Casks/fritzing.rb
@@ -1,7 +1,9 @@
 class Fritzing < Cask
-  url 'http://fritzing.org/download/0.8.7b/mac-os-x-105/fritzing.0.8.7b.cocoa.dmg'
-  homepage 'http://fritzing.org/'
   version '0.8.7b'
   sha256 '40b9290320bd33fc96745c68c020bcd36a18f3ee4d525f3aecc0a946e6edcd8f'
+
+  url 'http://fritzing.org/download/0.8.7b/mac-os-x-105/fritzing.0.8.7b.cocoa.dmg'
+  homepage 'http://fritzing.org/'
+
   link 'Fritzing.app'
 end
