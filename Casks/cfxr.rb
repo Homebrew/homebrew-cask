319128b24cbe1853ca3bc67f2734a72067b6983c
ndr <ndr@qef.io>
2014-06-27 02:01:02 +0200

    Reformat cfxr.rb according to readability conventions

--- Casks/cfxr.rb
+++ Casks/cfxr.rb
@@ -1,7 +1,9 @@
 class Cfxr < Cask
-  url 'https://github.com/downloads/nevyn/cfxr/cfxr%200.2.1.zip'
-  homepage 'http://thirdcog.eu/apps/cfxr'
   version '0.2.1'
   sha256 '106e06fe1c76581ffb89b3f44419a1669526edb1609f54022390b22d0947a1f2'
+
+  url 'https://github.com/downloads/nevyn/cfxr/cfxr%200.2.1.zip'
+  homepage 'http://thirdcog.eu/apps/cfxr'
+
   link 'cfxr.app'
 end
