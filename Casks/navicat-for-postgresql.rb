ed606b1e348274b5f607df814983a288c74183f3
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat navicat-for-postgresql.rb according to readability conventions

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,7 +1,9 @@
 class NavicatForPostgresql < Cask
-  url 'http://download.navicat.com/download/navicat110_pgsql_en.dmg'
-  homepage 'http://www.navicat.com/products/navicat-for-postgresql'
   version '11.0.18'
   sha256 'de6e44791b99d33c05da075b33d473a4e512297b17d1549fcda43a831b00ddac'
+
+  url 'http://download.navicat.com/download/navicat110_pgsql_en.dmg'
+  homepage 'http://www.navicat.com/products/navicat-for-postgresql'
+
   link 'Navicat for PostgreSQL.app'
 end
