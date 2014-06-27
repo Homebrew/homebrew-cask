7812dd27824e6a25ddf1fa13446b29951612949b
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat icefloor.rb according to readability conventions

--- Casks/icefloor.rb
+++ Casks/icefloor.rb
@@ -1,8 +1,10 @@
 class Icefloor < Cask
-  url 'http://www.hanynet.com/icefloor-2.0.2.zip'
-  homepage 'http://www.hanynet.com/icefloor/'
   version '2.0.2'
   sha256 'b326bab20c022c12be9ed01c3aa0e5f072853ce7dc5fb756bddebc2c3abddf64'
+
+  url 'http://www.hanynet.com/icefloor-2.0.2.zip'
+  homepage 'http://www.hanynet.com/icefloor/'
+
   nested_container 'IceFloor 2.0.2.dmg'
   link 'IceFloor.app'
 end
