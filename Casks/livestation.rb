44b1eb9697b83fbf4296574c3fcad8d0df29a0a8
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat livestation.rb according to readability conventions

--- Casks/livestation.rb
+++ Casks/livestation.rb
@@ -1,7 +1,9 @@
 class Livestation < Cask
-  url 'http://updates.livestation.com/releases/Livestation-3.3.0.dmg'
-  homepage 'http://www.livestation.com'
   version '3.3.0'
   sha256 '8ed0bdcb6aee4dca18e64c6c6390b3f83c67c6d9c36358ff800ece7a870f9dd3'
+
+  url 'http://updates.livestation.com/releases/Livestation-3.3.0.dmg'
+  homepage 'http://www.livestation.com'
+
   link 'Livestation.app'
 end
