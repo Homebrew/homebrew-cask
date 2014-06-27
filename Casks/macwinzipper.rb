93806cf169d086866b43abd03c9efdab6a92fdd4
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat macwinzipper.rb according to readability conventions

--- Casks/macwinzipper.rb
+++ Casks/macwinzipper.rb
@@ -1,7 +1,9 @@
 class Macwinzipper < Cask
-  url 'http://tidajapan.com/files/MacWinZipper-2.5.dmg?download'
-  homepage 'http://tidajapan.com/macwinzipper'
   version '2.5'
   sha256 'fc3afd92ecfbde16bbdd388285e16ef8f7aa5f5edf669496115823fef92a0186'
+
+  url 'http://tidajapan.com/files/MacWinZipper-2.5.dmg?download'
+  homepage 'http://tidajapan.com/macwinzipper'
+
   link 'MacWinZipper.app'
 end
