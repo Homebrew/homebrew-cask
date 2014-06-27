c69be8f71604f76f31ca6de1e44058bcdf443e31
ndr <ndr@qef.io>
2014-06-27 02:01:20 +0200

    Reformat ionu.rb according to readability conventions

--- Casks/ionu.rb
+++ Casks/ionu.rb
@@ -1,8 +1,10 @@
 class Ionu < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://isis.ionu.nu/release/OSX/IONU.dmg'
   appcast 'https://isis.ionu.nu/release/OSX/appcast.xml'
   homepage 'https://ionu.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'IONU.app'
 end
