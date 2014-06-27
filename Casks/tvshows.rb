7e30599d1cf6587bd0874e4bb69ac5433b43d2ce
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat tvshows.rb according to readability conventions

--- Casks/tvshows.rb
+++ Casks/tvshows.rb
@@ -1,7 +1,9 @@
 class Tvshows < Cask
-  url 'http://tvshowsapp.com/TVShows.zip'
-  homepage 'http://tvshowsapp.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://tvshowsapp.com/TVShows.zip'
+  homepage 'http://tvshowsapp.com/'
+
   prefpane 'TvShows.prefpane'
 end
