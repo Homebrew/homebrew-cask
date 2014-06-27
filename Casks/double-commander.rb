7e1123f463fb437164227104f0da1c5633121084
ndr <ndr@qef.io>
2014-06-27 02:01:08 +0200

    Reformat double-commander.rb according to readability conventions

--- Casks/double-commander.rb
+++ Casks/double-commander.rb
@@ -1,7 +1,9 @@
 class DoubleCommander < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/doublecmd/doublecmd-0.5.8-5390.qt.x86_64.dmg'
-  homepage 'http://doublecmd.sourceforge.net/'
   version '0.5.8'
   sha256 'a4bf455f028783a4c14d600068d27556984bfc3357a80dc0715f047efbf6dbfe'
+
+  url 'https://downloads.sourceforge.net/sourceforge/doublecmd/doublecmd-0.5.8-5390.qt.x86_64.dmg'
+  homepage 'http://doublecmd.sourceforge.net/'
+
   link 'Double Commander.app'
 end
