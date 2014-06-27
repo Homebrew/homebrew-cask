618bc65f0a3fa4b1f3092e3f1a406cbe8b3c3a72
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat sleep-monitor.rb according to readability conventions

--- Casks/sleep-monitor.rb
+++ Casks/sleep-monitor.rb
@@ -1,8 +1,10 @@
 class SleepMonitor < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.dssw.co.uk/sleepmonitor/dsswsleepmonitor.dmg'
   appcast 'http://version.dssw.co.uk/sleepmonitor/standard'
   homepage 'https://www.dssw.co.uk/sleepmonitor'
-  version 'latest'
-  sha256 :no_check
+
   link 'Sleep Monitor.app'
 end
