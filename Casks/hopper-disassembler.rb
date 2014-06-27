e3c45e1a663b24323717331641024e1f5477403a
ndr <ndr@qef.io>
2014-06-27 02:01:17 +0200

    Reformat hopper-disassembler.rb according to readability conventions

--- Casks/hopper-disassembler.rb
+++ Casks/hopper-disassembler.rb
@@ -1,8 +1,10 @@
 class HopperDisassembler < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.hopperapp.com/HopperWeb/download_last_v3.php'
   appcast 'http://www.hopperapp.com/HopperWeb/appcast.php'
   homepage 'http://www.hopperapp.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Hopper Disassembler v3.app'
 end
