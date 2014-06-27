1448a2161388e9ac7732016b7a6a88fb81a4828c
ndr <ndr@qef.io>
2014-06-27 02:01:38 +0200

    Reformat qqbrowser.rb according to readability conventions

--- Casks/qqbrowser.rb
+++ Casks/qqbrowser.rb
@@ -1,7 +1,9 @@
 class Qqbrowser < Cask
-  url 'http://dldir1.qq.com/invc/tt/QQBrowser_2160.dmg'
-  homepage 'http://browser.qq.com/mac/'
   version '2.3.2160'
   sha256 '6e0a3cf5d889f733cc5a4b38c8441bb32201ad1634c038d599be6e0864a65d14'
+
+  url 'http://dldir1.qq.com/invc/tt/QQBrowser_2160.dmg'
+  homepage 'http://browser.qq.com/mac/'
+
   link 'QQBrowser.app'
 end
