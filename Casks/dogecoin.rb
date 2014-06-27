452865cc676c486c1842f1263bfbcd33fcdf6dfd
ndr <ndr@qef.io>
2014-06-27 02:01:08 +0200

    Reformat dogecoin.rb according to readability conventions

--- Casks/dogecoin.rb
+++ Casks/dogecoin.rb
@@ -1,7 +1,9 @@
 class Dogecoin < Cask
-  url 'https://github.com/dogecoin/dogecoin/releases/download/v1.7.1/dogecoin-core-1.7.1-mac.zip'
-  homepage 'http://dogecoin.com/'
   version '1.7.1'
   sha256 '04fe6443e5541c9c1d5a94ebdf2418e8dfe91a013322229c93e91090bda822bb'
+
+  url 'https://github.com/dogecoin/dogecoin/releases/download/v1.7.1/dogecoin-core-1.7.1-mac.zip'
+  homepage 'http://dogecoin.com/'
+
   link 'Dogecoin-Qt.app'
 end
