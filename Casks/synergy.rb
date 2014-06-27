b1424a7f2161d8138d837b811ab3805ce4c838e4
ndr <ndr@qef.io>
2014-06-27 02:01:47 +0200

    Reformat synergy.rb according to readability conventions

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,7 +1,9 @@
 class Synergy < Cask
-  url 'https://synergy-project.org/files/packages/synergy-1.5.0-r2278-MacOSX109-x86_64.dmg'
-  homepage 'http://synergy-foss.org/'
   version '1.5.0'
   sha256 'b7f024720a3174ad68aa3489fa8674ca4bdc95a785b82044d57685325f60c913'
+
+  url 'https://synergy-project.org/files/packages/synergy-1.5.0-r2278-MacOSX109-x86_64.dmg'
+  homepage 'http://synergy-foss.org/'
+
   link 'Synergy.app'
 end
