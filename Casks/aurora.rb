2bedde080414a85ce3c5a2fd512e4ea0cc02c5c1
ndr <ndr@qef.io>
2014-06-27 02:00:58 +0200

    Reformat aurora.rb according to readability conventions

--- Casks/aurora.rb
+++ Casks/aurora.rb
@@ -1,8 +1,10 @@
 class Aurora < Cask
+  version '5.0.3'
+  sha256 '4293c5ffdf6eb8e5d7031f02b1c83238472f486fe8a964f2f62ac71c72c22eb2'
+  
   url 'https://www.oneperiodic.com/files/Aurora%20v5.0.3.zip'
   appcast 'http://www.oneperiodic.com/aurora5.xml'
   homepage 'http://www.oneperiodic.com/products/aurora/'
-  version '5.0.3'
-  sha256 '4293c5ffdf6eb8e5d7031f02b1c83238472f486fe8a964f2f62ac71c72c22eb2'
+
   link 'Aurora.app'
 end
