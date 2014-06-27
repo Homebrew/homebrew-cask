d0e730dc515142fd158a81030ad219b730c0f07c
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat pinegrow-web-designer.rb according to readability conventions

--- Casks/pinegrow-web-designer.rb
+++ Casks/pinegrow-web-designer.rb
@@ -1,7 +1,9 @@
 class PinegrowWebDesigner < Cask
-  url 'https://pinegrow.s3.amazonaws.com/PinegrowMac.zip'
-  homepage 'http://pinegrow.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://pinegrow.s3.amazonaws.com/PinegrowMac.zip'
+  homepage 'http://pinegrow.com/'
+
   link 'Pinegrow Web Designer.app'
 end
