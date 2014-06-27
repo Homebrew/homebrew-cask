4961559d2cec31b1241a1e960d9ce0786ce6d533
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat hugin.rb according to readability conventions

--- Casks/hugin.rb
+++ Casks/hugin.rb
@@ -1,7 +1,9 @@
 class Hugin < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/hugin/Hugin-2013.0.0.dmg'
-  homepage 'http://hugin.sourceforge.net/'
   version '2013.0.0'
   sha256 '528cc2249d4cdf9267e85ab836ff36cc0e2450de6663661adfc5e2cc3ef1ec60'
+
+  url 'https://downloads.sourceforge.net/sourceforge/hugin/Hugin-2013.0.0.dmg'
+  homepage 'http://hugin.sourceforge.net/'
+
   link 'Hugin.app'
 end
