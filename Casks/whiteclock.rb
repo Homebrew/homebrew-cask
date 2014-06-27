5c59b31fe6432709ea7bf4c8b356a210cc2776ed
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat whiteclock.rb according to readability conventions

--- Casks/whiteclock.rb
+++ Casks/whiteclock.rb
@@ -1,7 +1,9 @@
 class Whiteclock < Cask
-  url 'http://www.taimila.com/downloads/WhiteClock2.zip'
-  homepage 'http://www.taimila.com/?p=1221'
   version '2.0'
   sha256 '9ad4a713dca77cfbca5bf4d8d1263126ec2b95fa588ad80be4f5a79140eefb6d'
+
+  url 'http://www.taimila.com/downloads/WhiteClock2.zip'
+  homepage 'http://www.taimila.com/?p=1221'
+
   link 'WhiteClock.app'
 end
