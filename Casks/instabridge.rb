da4501651f7cc72ece93e1bd3b657c56583860f3
ndr <ndr@qef.io>
2014-06-27 02:01:19 +0200

    Reformat instabridge.rb according to readability conventions

--- Casks/instabridge.rb
+++ Casks/instabridge.rb
@@ -1,7 +1,9 @@
 class Instabridge < Cask
-  url 'http://cdn.instabridge.com/mac/instabridge-osx.dmg'
-  homepage 'http://instabridge.com/'
   version '1.0'
   sha256 '62fa0d8766e1be5f21404c5d530c87c3c7351cced0a43c1176aefffee0c2c788'
+
+  url 'http://cdn.instabridge.com/mac/instabridge-osx.dmg'
+  homepage 'http://instabridge.com/'
+
   link 'Instabridge.app'
 end
