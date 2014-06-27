d2798ad1db07d0e583a583d7d41e8515cfa0d368
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat secrets.rb according to readability conventions

--- Casks/secrets.rb
+++ Casks/secrets.rb
@@ -1,7 +1,9 @@
 class Secrets < Cask
-  url 'https://blacktree-secrets.googlecode.com/files/Secrets_1.0.6.zip'
-  homepage 'http://secrets.blacktree.com'
   version '1.0.6'
   sha256 '9604bc37a52fe32cd5146715fe5f401512b214847284151155d28c43413e0d25'
+
+  url 'https://blacktree-secrets.googlecode.com/files/Secrets_1.0.6.zip'
+  homepage 'http://secrets.blacktree.com'
+
   prefpane 'Secrets.prefpane'
 end
