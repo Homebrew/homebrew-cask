138b44cb2b919150a749f50f6087a9db5b727a45
ndr <ndr@qef.io>
2014-06-27 02:01:13 +0200

    Reformat frizzix.rb according to readability conventions

--- Casks/frizzix.rb
+++ Casks/frizzix.rb
@@ -1,8 +1,10 @@
 class Frizzix < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://mac.frizzix.de/downloads/currentVersion.dmg'
   appcast 'http://frizzix.de/downloads/FrizzixUpdate.xml'
   homepage 'http://mac.frizzix.de/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Frizzix.app'
 end
