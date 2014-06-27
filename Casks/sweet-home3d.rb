c1eaf38f4f64874b9047b26320399dd072f265ad
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat sweet-home3d.rb according to readability conventions

--- Casks/sweet-home3d.rb
+++ Casks/sweet-home3d.rb
@@ -1,7 +1,9 @@
 class SweetHome3d < Cask
-  url 'https://downloads.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-4.3/SweetHome3D-4.3-macosx.dmg'
-  homepage 'http://www.sweethome3d.com/'
   version '4.3'
   sha256 'c8004cbcec8d587139751a7a39539903f0f555185ce41378056a4549d0f65962'
+
+  url 'https://downloads.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-4.3/SweetHome3D-4.3-macosx.dmg'
+  homepage 'http://www.sweethome3d.com/'
+
   link 'Sweet Home 3D.app'
 end
