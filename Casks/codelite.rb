349385fea2fc6a6392012bdd8573f2b57b6cedef
ndr <ndr@qef.io>
2014-06-27 02:01:04 +0200

    Reformat codelite.rb according to readability conventions

--- Casks/codelite.rb
+++ Casks/codelite.rb
@@ -1,7 +1,9 @@
 class Codelite < Cask
-  url 'http://downloads.codelite.org/downloads.php?osx'
-  homepage 'http://codelite.org'
   version 'latest'
   sha256 :no_check
+
+  url 'http://downloads.codelite.org/downloads.php?osx'
+  homepage 'http://codelite.org'
+
   link 'codelite.app'
 end
