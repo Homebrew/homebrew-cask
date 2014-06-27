121715d3df637875bf28bf0add7f97ed077d1645
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat opennx.rb according to readability conventions

--- Casks/opennx.rb
+++ Casks/opennx.rb
@@ -1,8 +1,10 @@
 class Opennx < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/opennx/OpenNX-0.16.0.729.dmg'
-  homepage 'http://opennx.net/'
   version '0.16.0.729'
   sha256 '65dde1a3504a17ac58ed2a7178536347d829ee8c27cc90bebdae3e98c36fc6c6'
+
+  url 'http://downloads.sourceforge.net/sourceforge/opennx/OpenNX-0.16.0.729.dmg'
+  homepage 'http://opennx.net/'
+
   install 'OpenNX.pkg'
   uninstall :script => { :executable => '/Library/OpenNX/bin/macuninstall', :args => ['--batch'] }
 end
