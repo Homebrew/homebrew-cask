7f78aaa6d5e21a96b9581ef78f345df852b30fbf
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat visit.rb according to readability conventions

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -1,7 +1,9 @@
 class Visit < Cask
-  url 'https://portal.nersc.gov/svn/visit/trunk/releases/2.7.2/VisIt-2.7.2.dmg'
-  homepage 'https://wci.llnl.gov/codes/visit/home.html'
   version '2.7.2'
   sha256 '3c1f1b201358876c1b23717a1b3a2c5a1777755e3fc7f242c08b53a033402fbc'
+
+  url 'https://portal.nersc.gov/svn/visit/trunk/releases/2.7.2/VisIt-2.7.2.dmg'
+  homepage 'https://wci.llnl.gov/codes/visit/home.html'
+
   link 'VisIt.app'
 end
