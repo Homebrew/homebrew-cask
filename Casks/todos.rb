b66c4eb9a46046b31851512a0c8e3589705fed5e
ndr <ndr@qef.io>
2014-06-27 02:01:49 +0200

    Reformat todos.rb according to readability conventions

--- Casks/todos.rb
+++ Casks/todos.rb
@@ -1,8 +1,10 @@
 class Todos < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://dbachrach.com/opensoft/downloads/apps/Todos.dmg'
   appcast 'http://www.dbachrach.com/opensoft/appcasts/Todos.xml'
   homepage 'http://dbachrach.com/opensoft/index.php?page=Todos'
-  version 'latest'
-  sha256 :no_check
+
   link 'Todos.app'
 end
