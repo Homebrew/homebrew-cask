e1f736fe8a5ee1f2a89e5bc1b68ac9b3e7805b5c
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat twitterrific.rb according to readability conventions

--- Casks/twitterrific.rb
+++ Casks/twitterrific.rb
@@ -1,8 +1,10 @@
 class Twitterrific < Cask
+  version '4.5.1'
+  sha256 '1b43504307e8a541c97a93ecd4c56bc443f66cdd622d319db965e7e0eb760b46'
+  
   url 'https://iconfactory.com/assets/software/twitterrific/Twitterrific-4.5.1.zip'
   appcast 'http://iconfactory.com/appcasts/Twitterrific/appcast.xml'
   homepage 'http://twitterrific.com/mac'
-  version '4.5.1'
-  sha256 '1b43504307e8a541c97a93ecd4c56bc443f66cdd622d319db965e7e0eb760b46'
+
   link 'Twitterrific.app'
 end
