033944de0791474f62510545f20c16433bff04e1
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat name-mangler.rb according to readability conventions

--- Casks/name-mangler.rb
+++ Casks/name-mangler.rb
@@ -1,8 +1,10 @@
 class NameMangler < Cask
+  version 'latest'
+  sha256 :no_check
+
   url 'http://manytricks.com/download/namemangler'
   appcast 'http://manytricks.com/namemangler/appcast.xml'
-  version 'latest'
   homepage 'http://manytricks.com/namemangler/'
-  sha256 :no_check
+
   link 'Name Mangler.app'
 end
