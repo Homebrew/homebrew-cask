cb2f1e98e1ed1a7084e46b86a3be323d835bef47
ndr <ndr@qef.io>
2014-06-27 02:01:55 +0200

    Reformat zooom.rb according to readability conventions

--- Casks/zooom.rb
+++ Casks/zooom.rb
@@ -1,8 +1,10 @@
 class Zooom < Cask
-  url 'http://software.coderage-software.com/zooom/Zooom_2.5.0.dmg'
-  homepage 'http://coderage-software.com/zooom'
   version '2.5.0'
   sha256 '944d8c6f0869963ea0c0b11491d05eaa4357953072c43519f674fa81216e9e34'
+
+  url 'http://software.coderage-software.com/zooom/Zooom_2.5.0.dmg'
+  homepage 'http://coderage-software.com/zooom'
+
   install 'Zooom2.pkg'
   caveats do
     os_version_only '10.8', '10.7', '10.6', '10.5'
