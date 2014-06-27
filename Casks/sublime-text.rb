8b59c15581824ca1ca9c7f7e74b388ef44675fc8
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat sublime-text.rb according to readability conventions

--- Casks/sublime-text.rb
+++ Casks/sublime-text.rb
@@ -1,9 +1,11 @@
 class SublimeText < Cask
+  version '2.0.2'
+  sha256 '906e71e19ae5321f80e7cf42eab8355146d8f2c3fd55be1f7fe5c62c57165add'
+  
   url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
   appcast 'http://www.sublimetext.com/updates/2/stable/appcast_osx.xml'
   homepage 'http://www.sublimetext.com/2'
-  version '2.0.2'
-  sha256 '906e71e19ae5321f80e7cf42eab8355146d8f2c3fd55be1f7fe5c62c57165add'
+
   link 'Sublime Text 2.app'
   binary 'Sublime Text 2.app/Contents/SharedSupport/bin/subl'
   caveats do
