0371554cf4d76bd32e21cf095d84603e18d2a0d1
ndr <ndr@qef.io>
2014-06-27 02:01:49 +0200

    Reformat todoist.rb according to readability conventions

--- Casks/todoist.rb
+++ Casks/todoist.rb
@@ -1,8 +1,10 @@
 class Todoist < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://d2dq6e731uoz0t.cloudfront.net/3334959e6780bf9ff7dbe8fc936e0700/as/Todoist.app.zip'
   appcast 'http://todoist.com/static/native_apps/mac_app.xml'
   homepage 'https://todoist.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'Todoist.app'
 end
