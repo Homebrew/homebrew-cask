f2e2437bf7098440cce6642da064b4e1283e8cf7
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat optimal-layout.rb according to readability conventions

--- Casks/optimal-layout.rb
+++ Casks/optimal-layout.rb
@@ -1,8 +1,10 @@
 class OptimalLayout < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://files.windowflow.com/OptimalLayout2.zip'
   appcast 'http://most-advantageous.com/sparkle/OL-AppCast.cfm'
   homepage 'http://most-advantageous.com/optimal-layout/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Optimal Layout.app'
 end
