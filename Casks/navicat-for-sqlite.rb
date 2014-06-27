938a4e3a98bdf18e049bb4b7f1544142bc00efa3
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat navicat-for-sqlite.rb according to readability conventions

--- Casks/navicat-for-sqlite.rb
+++ Casks/navicat-for-sqlite.rb
@@ -1,7 +1,9 @@
 class NavicatForSqlite < Cask
-  url 'http://download.navicat.com/download/navicat110_sqlite_en.dmg'
-  homepage 'http://www.navicat.com/products/navicat-for-sqlite'
   version '11.0.18'
   sha256 '65522f29707cb26fc2b39aa52687dbd9308150b92c78e4be5c58e0cceaa2629e'
+
+  url 'http://download.navicat.com/download/navicat110_sqlite_en.dmg'
+  homepage 'http://www.navicat.com/products/navicat-for-sqlite'
+
   link 'Navicat for SQLite.app'
 end
