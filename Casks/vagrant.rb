1e83bc10eed61c993708f9259eeef96b712088ec
ndr <ndr@qef.io>
2014-06-27 02:01:51 +0200

    Reformat vagrant.rb according to readability conventions

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,8 +1,10 @@
 class Vagrant < Cask
-  url 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.dmg'
-  homepage 'http://www.vagrantup.com'
   version '1.6.3'
   sha256 'ed1de41ca11a2251e4748a7a88fb46c3312780b7952f2248683f8ed2a007bc72'
+
+  url 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.dmg'
+  homepage 'http://www.vagrantup.com'
+
   install 'Vagrant.pkg'
   uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] }
 end
