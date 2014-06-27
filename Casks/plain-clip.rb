938c225dda2efef445c529eafa5d58c60fcc6146
ndr <ndr@qef.io>
2014-06-27 02:01:36 +0200

    Reformat plain-clip.rb according to readability conventions

--- Casks/plain-clip.rb
+++ Casks/plain-clip.rb
@@ -1,7 +1,9 @@
 class PlainClip < Cask
-  url 'https://www.bluem.net/files/Plain-Clip.dmg'
-  homepage 'http://www.bluem.net/en/mac/plain-clip'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.bluem.net/files/Plain-Clip.dmg'
+  homepage 'http://www.bluem.net/en/mac/plain-clip'
+
   link 'Plain Clip.app'
 end
