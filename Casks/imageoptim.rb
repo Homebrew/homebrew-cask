9ba0fa725a100bfe8f445d4ac1b3b89f0570e599
ndr <ndr@qef.io>
2014-06-27 02:01:19 +0200

    Reformat imageoptim.rb according to readability conventions

--- Casks/imageoptim.rb
+++ Casks/imageoptim.rb
@@ -1,8 +1,10 @@
 class Imageoptim < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://imageoptim.com/ImageOptim.tbz2'
   appcast 'http://imageoptim.com/appcast.xml'
   homepage 'http://imageoptim.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'ImageOptim.app'
 end
