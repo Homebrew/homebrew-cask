fcf30e64dcf63bc696771f451684d89392e4d8cb
ndr <ndr@qef.io>
2014-06-27 02:01:13 +0200

    Reformat functionflip.rb according to readability conventions

--- Casks/functionflip.rb
+++ Casks/functionflip.rb
@@ -1,7 +1,9 @@
 class Functionflip < Cask
-  url 'http://kevingessner.com/public/downloads/FunctionFlip/2.2.2/FunctionFlip.prefPane.zip'
-  homepage 'http://kevingessner.com/software/functionflip/'
   version '2.2.2'
   sha256 '66ac937e9ade979eca2d94881093ee5301bce660da1ace98e3c792da29b33c72'
+
+  url 'http://kevingessner.com/public/downloads/FunctionFlip/2.2.2/FunctionFlip.prefPane.zip'
+  homepage 'http://kevingessner.com/software/functionflip/'
+
   prefpane 'FunctionFlip.prefPane'
 end
