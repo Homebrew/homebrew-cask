ab79d7fda47c5630bf644ec44b411d46a3ea5581
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat mpv.rb according to readability conventions

--- Casks/mpv.rb
+++ Casks/mpv.rb
@@ -1,8 +1,10 @@
 class Mpv < Cask
-  url 'https://github.com/mpv-player/mpv/releases/download/v0.3.5/mpv_v0.3.5_osx.zip'
-  homepage 'http://mpv.io/'
   version '0.3.5'
   sha256 '7502c74aebf0edf9e5fde1bbac1a3f34a5b9df49fec7dac72729153e646ef414'
+
+  url 'https://github.com/mpv-player/mpv/releases/download/v0.3.5/mpv_v0.3.5_osx.zip'
+  homepage 'http://mpv.io/'
+
   link 'mpv.app'
   binary 'mpv.app/Contents/MacOS/mpv'
   caveats do
