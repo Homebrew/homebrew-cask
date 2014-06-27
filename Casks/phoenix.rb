5f3de9ad4b2cf97388866afed7de2e3873e60c1c
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat phoenix.rb according to readability conventions

--- Casks/phoenix.rb
+++ Casks/phoenix.rb
@@ -1,7 +1,9 @@
 class Phoenix < Cask
-  url 'https://raw.github.com/sdegutis/phoenix/master/Builds/Phoenix-LATEST.app.tar.gz'
-  homepage 'https://github.com/sdegutis/Phoenix'
   version 'latest'
   sha256 :no_check
+
+  url 'https://raw.github.com/sdegutis/phoenix/master/Builds/Phoenix-LATEST.app.tar.gz'
+  homepage 'https://github.com/sdegutis/Phoenix'
+
   link 'Phoenix.app'
 end
