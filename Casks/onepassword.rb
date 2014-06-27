d43450c864231a5919ad04286da026a3ef9019f0
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat onepassword.rb according to readability conventions

--- Casks/onepassword.rb
+++ Casks/onepassword.rb
@@ -1,7 +1,9 @@
 class Onepassword < Cask
-  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.4.1.zip'
-  homepage 'https://agilebits.com/onepassword'
   version '4.4.1'
   sha256 '007b554067cc0016bedfdcb75577f88b68daeabb5474a219960080e2e9159c90'
+
+  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.4.1.zip'
+  homepage 'https://agilebits.com/onepassword'
+
   link '1Password 4.app'
 end
