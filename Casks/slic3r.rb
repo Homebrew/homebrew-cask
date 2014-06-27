017aaa0925b09525b1558e27c27fbd8422fdee96
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat slic3r.rb according to readability conventions

--- Casks/slic3r.rb
+++ Casks/slic3r.rb
@@ -1,7 +1,9 @@
 class Slic3r < Cask
-  url 'http://dl.slic3r.org/mac/slic3r-osx-uni-1-0-1-stable.dmg'
-  homepage 'http://slic3r.org/'
   version '1.0.1'
   sha256 '2c1b07f41f392b5b111307e796158fe21038bca0a296c4d93756a902586c8629'
+
+  url 'http://dl.slic3r.org/mac/slic3r-osx-uni-1-0-1-stable.dmg'
+  homepage 'http://slic3r.org/'
+
   link 'Slic3r.app'
 end
