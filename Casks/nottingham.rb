34d720f8effac5df78899d615486c1dfea6d0f32
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat nottingham.rb according to readability conventions

--- Casks/nottingham.rb
+++ Casks/nottingham.rb
@@ -1,7 +1,9 @@
 class Nottingham < Cask
-  url 'http://dl.clickontyler.com/nottingham/nottingham20_2.1.3.zip'
-  homepage 'https://clickontyler.com/nottingham/'
   version '2.1.3'
   sha256 'ccaceba0bc6566ba2850126ae2d456fd6d44b78122117b8e1f3e597ef812bd55'
+
+  url 'http://dl.clickontyler.com/nottingham/nottingham20_2.1.3.zip'
+  homepage 'https://clickontyler.com/nottingham/'
+
   link 'Nottingham.app'
 end
