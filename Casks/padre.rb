17c1ca8a4f87c2e3d57535c8d1ba8156f69905c0
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat padre.rb according to readability conventions

--- Casks/padre.rb
+++ Casks/padre.rb
@@ -1,7 +1,9 @@
 class Padre < Cask
-  url 'http://wildperl.com/wp-content/uploads/Padre/padre-osx-uni-0-92-0.dmg'
-  homepage 'http://padre.perlide.org'
   version '0.92.0'
   sha256 '481088a40bc71bd62d13ac23ed8fbfa3b8fc651587b9e47461795a9a78032d87'
+
+  url 'http://wildperl.com/wp-content/uploads/Padre/padre-osx-uni-0-92-0.dmg'
+  homepage 'http://padre.perlide.org'
+
   link 'Padre.app'
 end
