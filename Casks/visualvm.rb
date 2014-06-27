7e49d33f9ac079102ff3d8b0a2e4863e96d3734f
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat visualvm.rb according to readability conventions

--- Casks/visualvm.rb
+++ Casks/visualvm.rb
@@ -1,7 +1,9 @@
 class Visualvm < Cask
-  url 'https://java.net/projects/visualvm/downloads/download/release137/VisualVM_137.dmg'
-  homepage 'http://visualvm.java.net'
   version '1.3.7'
   sha256 '16d95033659703b03ad0fc98e0787d13c6814cdfd065be91024fd3b5a1c27e3b'
+
+  url 'https://java.net/projects/visualvm/downloads/download/release137/VisualVM_137.dmg'
+  homepage 'http://visualvm.java.net'
+
   link 'VisualVM.app'
 end
