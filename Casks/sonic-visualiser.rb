baf8f571089ad2f3b832257c45e19ebeb969007c
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat sonic-visualiser.rb according to readability conventions

--- Casks/sonic-visualiser.rb
+++ Casks/sonic-visualiser.rb
@@ -1,7 +1,9 @@
 class SonicVisualiser < Cask
-  url 'https://code.soundsoftware.ac.uk/attachments/download/907/Sonic%20Visualiser-2.3.dmg'
-  homepage 'http://www.sonicvisualiser.org/'
   version '2.3'
   sha256 '8a8c6c5268f10dd6614afeabd40a4fa962a31d55db0aed1ddf369c5a5b8b4981'
+
+  url 'https://code.soundsoftware.ac.uk/attachments/download/907/Sonic%20Visualiser-2.3.dmg'
+  homepage 'http://www.sonicvisualiser.org/'
+
   link 'Sonic Visualiser.app'
 end
