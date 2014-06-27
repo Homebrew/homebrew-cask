6479802e01ea081f0f8f9bdaac44f10ae5bebd07
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat scilab.rb according to readability conventions

--- Casks/scilab.rb
+++ Casks/scilab.rb
@@ -1,7 +1,9 @@
 class Scilab < Cask
-  url 'http://www.scilab.org/download/5.5.0/scilab-5.5.0-x86_64.dmg'
-  homepage 'https://www.scilab.org'
   version '5.5.0'
   sha256 '58ed168011296c680db12cd4f3a55359647318cd959a9e97b37881ff5e26d592'
+
+  url 'http://www.scilab.org/download/5.5.0/scilab-5.5.0-x86_64.dmg'
+  homepage 'https://www.scilab.org'
+
   link 'scilab-5.5.0.app'
 end
