4b032439a347a962dbceab8fea4604165506b78d
Martin Madsen <martinbmadsen@gmail.com>
2014-06-27 17:35:52 +0200

    Do not add explicit version numbers in cask

--- Casks/xamarin-platform.rb
+++ Casks/xamarin-platform.rb
@@ -1,8 +1,8 @@
 class XamarinPlatform < Cask
   url 'http://download.xamarin.com/Installer/Mac/XamarinInstaller.dmg'
   homepage 'http://xamarin.com/platform'
-  version '5.0.1-build-3'
-  sha256 '063935ac9c5c5726946614d382c0b412093a961d1473436dfac8b1969e7b7c31'
+  version 'latest'
+  sha256 :no_check
 
   after_install do
     system '/usr/bin/sudo', '-E', '--',
