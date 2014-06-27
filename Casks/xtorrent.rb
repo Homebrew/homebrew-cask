7c5b92647c822f80839b33d3edd7fd8d4f903bf1
ndr <ndr@qef.io>
2014-06-27 02:01:54 +0200

    Reformat xtorrent.rb according to readability conventions

--- Casks/xtorrent.rb
+++ Casks/xtorrent.rb
@@ -1,8 +1,10 @@
 class Xtorrent < Cask
+  version '2.1 (v171)'
+  sha256 '26ea235dcb827c6e58ab3409bec83396e86704d742d517e527016ecd44672379'
+  
   url 'http://acquisition.dreamhosters.com/xtorrent/Xtorrent2.1(v171).dmg'
   appcast 'http://xtorrent.s3.amazonaws.com/appcast.xml'
   homepage 'http://www.xtorrent.com'
-  version '2.1 (v171)'
-  sha256 '26ea235dcb827c6e58ab3409bec83396e86704d742d517e527016ecd44672379'
+
   link 'Xtorrent.app'
 end
