9e145e73d38a01f0034df96a8e04df26ccfcebb9
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat transmission-remote-gui.rb according to readability conventions

--- Casks/transmission-remote-gui.rb
+++ Casks/transmission-remote-gui.rb
@@ -1,8 +1,10 @@
 class TransmissionRemoteGui < Cask
-  url 'https://transmisson-remote-gui.googlecode.com/files/transgui-5.0.1.dmg'
-  homepage 'https://code.google.com/p/transmisson-remote-gui/'
   version '5.0.1'
   sha256 'b961aeb244b2519563837745f3475d21379e3da32bae2b3cbb20ca91d1a90d75'
+
+  url 'https://transmisson-remote-gui.googlecode.com/files/transgui-5.0.1.dmg'
+  homepage 'https://code.google.com/p/transmisson-remote-gui/'
+
   install 'transgui.pkg'
   uninstall :pkgutil => 'com.transgui.*'
 end
