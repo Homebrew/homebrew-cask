10474600913b2a5ec599346cf06954dccc2178c0
ndr <ndr@qef.io>
2014-06-27 02:01:55 +0200

    Reformat yojimbo.rb according to readability conventions

--- Casks/yojimbo.rb
+++ Casks/yojimbo.rb
@@ -1,8 +1,10 @@
 class Yojimbo < Cask
-  url 'https://s3.amazonaws.com/BBSW-download/Yojimbo_4.0.3.dmg'
-  homepage 'http://www.barebones.com/products/yojimbo/'
   version '4.0.3'
   sha256 '3d56523799db955ddf362e3b4b43a4a193b426ea76cf02d1647ecd56e5231c3b'
+
+  url 'https://s3.amazonaws.com/BBSW-download/Yojimbo_4.0.3.dmg'
+  homepage 'http://www.barebones.com/products/yojimbo/'
+
   link 'Yojimbo.app'
   after_install do
     system '/usr/bin/defaults', 'write', 'com.barebones.yojimbo4', 'moveToApplicationsFolderAlertSuppress', '-bool', 'true'
