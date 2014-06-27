5fcabfe5b8ae4f10c5d86f8af1f6464a6de5d5dc
ndr <ndr@qef.io>
2014-06-27 02:01:09 +0200

    Reformat easysimbl.rb according to readability conventions

--- Casks/easysimbl.rb
+++ Casks/easysimbl.rb
@@ -1,7 +1,9 @@
 class Easysimbl < Cask
-  url 'https://github.com/norio-nomura/EasySIMBL/releases/download/EasySIMBL-1.6/EasySIMBL-1.6.zip'
-  homepage 'https://github.com/norio-nomura/EasySIMBL/'
   version '1.6'
   sha256 '42139885e35946f1b2decb9cdfa755b8df8c050134463d76b2e3e0f68e783e26'
+
+  url 'https://github.com/norio-nomura/EasySIMBL/releases/download/EasySIMBL-1.6/EasySIMBL-1.6.zip'
+  homepage 'https://github.com/norio-nomura/EasySIMBL/'
+
   link 'EasySIMBL.app'
 end
