6e54e8002c9e9bbaf9e87c09d8656f5d90bdc59a
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat charles.rb according to readability conventions

--- Casks/charles.rb
+++ Casks/charles.rb
@@ -1,7 +1,9 @@
 class Charles < Cask
-  url 'http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-applejava.dmg'
-  homepage 'http://www.charlesproxy.com/'
   version '3.9.2'
   sha256 '89b017744181ccec18e8a5ff67b9728a2d2ded5de0620a100a98c8686d867b7b'
+
+  url 'http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-applejava.dmg'
+  homepage 'http://www.charlesproxy.com/'
+
   link 'Charles.app'
 end
