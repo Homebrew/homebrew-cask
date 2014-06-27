cd877524b19cd35b80cf1209f25ca332af7341d3
ndr <ndr@qef.io>
2014-06-27 02:00:55 +0200

    Reformat actiontastic.rb according to readability conventions

--- Casks/actiontastic.rb
+++ Casks/actiontastic.rb
@@ -1,7 +1,9 @@
 class Actiontastic < Cask
-  url 'https://s3.amazonaws.com/kaboomerang/actiontastic/ActiontasticBeta8.zip'
-  homepage 'http://actiontastic.com'
   version 'beta8'
   sha256 'f6536cd0cb477dd3867ccf359469b7a4377fb307a0307b27566c1f4219b79c44'
+
+  url 'https://s3.amazonaws.com/kaboomerang/actiontastic/ActiontasticBeta8.zip'
+  homepage 'http://actiontastic.com'
+
   link 'ActiontasticBeta8/Actiontastic.app'
 end
