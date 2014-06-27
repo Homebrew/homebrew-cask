76392e1357906f0df90c65f06b60c328bb53c922
ndr <ndr@qef.io>
2014-06-27 02:00:56 +0200

    Reformat antirsi.rb according to readability conventions

--- Casks/antirsi.rb
+++ Casks/antirsi.rb
@@ -1,7 +1,9 @@
 class Antirsi < Cask
-  url 'http://sabi.net/nriley/software/AntiRSI-1.4njr4.zip'
-  homepage 'http://sabi.net/nriley/software/#antirsi'
   version '1.4njr4'
   sha256 'b8c36cac71fe0b097b9435ab869b2a11e97e0f99b3227929cb333d14d5e88552'
+
+  url 'http://sabi.net/nriley/software/AntiRSI-1.4njr4.zip'
+  homepage 'http://sabi.net/nriley/software/#antirsi'
+
   link 'AntiRSI.app'
 end
