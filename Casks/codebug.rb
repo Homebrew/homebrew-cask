4649feadfe073aab4c576584125ea028582262ce
ndr <ndr@qef.io>
2014-06-27 02:01:04 +0200

    Reformat codebug.rb according to readability conventions

--- Casks/codebug.rb
+++ Casks/codebug.rb
@@ -1,8 +1,10 @@
 class Codebug < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://codebugapp.com/downloads/Codebug.dmg'
   appcast 'http://codebugapp.com/update.xml'
   homepage 'http://www.codebugapp.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Codebug.app'
 end
