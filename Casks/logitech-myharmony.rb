7056bc8c1d59da63a116bf05228ef56b1a76d62a
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat logitech-myharmony.rb according to readability conventions

--- Casks/logitech-myharmony.rb
+++ Casks/logitech-myharmony.rb
@@ -1,8 +1,10 @@
 class LogitechMyharmony < Cask
-  url 'http://app.myharmony.com/prod/mac/1.0/MyHarmony-App.dmg'
-  homepage 'https://setup.myharmony.com/'
   version '1.0'
   sha256 '537a11f2174dc748c27aff02bf01569b5bb2c93539b339c6fd61a7da07bb059f'
+
+  url 'http://app.myharmony.com/prod/mac/1.0/MyHarmony-App.dmg'
+  homepage 'https://setup.myharmony.com/'
+
   install 'MyHarmonySetup.pkg'
   uninstall :quit => 'org.logitech.MyHarmony',
             :pkgutil => 'MyHarmony.pkg'
