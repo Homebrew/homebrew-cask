7c54d322d51e21aeef1902b9ab17afd7760dd4f7
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat gnucash.rb according to readability conventions

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,8 +1,10 @@
 class Gnucash < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-2.6.3-1.dmg'
-  homepage 'http://www.gnucash.org'
   version '2.6.3-1'
   sha256 '58d0a9c95cd1835eeafc0b15b965d7aac9e71980b2ba7a7aac5707445fa17828'
+
+  url 'https://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-2.6.3-1.dmg'
+  homepage 'http://www.gnucash.org'
+
   link 'Gnucash.app'
   link 'FinanceQuote Update.app'
 end
