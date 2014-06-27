77b1e4daa54de65cd1bf7aa025d625d84d81d72c
ndr <ndr@qef.io>
2014-06-27 02:01:01 +0200

    Reformat bonjour-browser.rb according to readability conventions

--- Casks/bonjour-browser.rb
+++ Casks/bonjour-browser.rb
@@ -1,7 +1,9 @@
 class BonjourBrowser < Cask
-  url 'http://www.tildesoft.com/files/BonjourBrowser.dmg'
-  homepage 'http://www.tildesoft.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.tildesoft.com/files/BonjourBrowser.dmg'
+  homepage 'http://www.tildesoft.com/'
+
   link 'Bonjour Browser.app'
 end
