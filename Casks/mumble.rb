f79d2da1114dd2dc30d4038313c4e9dbf07ddb75
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat mumble.rb according to readability conventions

--- Casks/mumble.rb
+++ Casks/mumble.rb
@@ -1,7 +1,9 @@
 class Mumble < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/mumble/Mumble-1.2.7.dmg'
-  homepage 'http://mumble.sourceforge.net'
   version '1.2.7'
   sha256 '236c973a925309df696856bea63969cbf7fe82d1f88a93af0683407824e1430d'
+
+  url 'https://downloads.sourceforge.net/sourceforge/mumble/Mumble-1.2.7.dmg'
+  homepage 'http://mumble.sourceforge.net'
+
   link 'Mumble.app'
 end
