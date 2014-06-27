30d00d5d2da1492e16dc6bb019a2b13ea5566b4f
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat ltspice.rb according to readability conventions

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -1,7 +1,9 @@
 class Ltspice < Cask
-  url 'http://ltspice.linear-tech.com/LTspiceIV.dmg'
-  homepage 'http://www.linear.com/designtools/software/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://ltspice.linear-tech.com/LTspiceIV.dmg'
+  homepage 'http://www.linear.com/designtools/software/'
+
   link 'LTspice.app'
 end
