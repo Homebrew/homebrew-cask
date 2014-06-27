fc646dc42f24bafa9a65f8b219a9182fb4a94a68
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat gfxcardstatus.rb according to readability conventions

--- Casks/gfxcardstatus.rb
+++ Casks/gfxcardstatus.rb
@@ -1,8 +1,10 @@
 class Gfxcardstatus < Cask
+  version '2.3'
+  sha256 '092b3e2fad44681ba396cf498707c8b6c228fd55310770a8323ebb9344b4d9a1'
+  
   url 'http://codykrieger.com/downloads/gfxCardStatus-2.3.zip'
   appcast 'http://gfx.io/appcast.xml'
   homepage 'http://gfx.io/'
-  version '2.3'
-  sha256 '092b3e2fad44681ba396cf498707c8b6c228fd55310770a8323ebb9344b4d9a1'
+
   link 'gfxCardStatus.app'
 end
