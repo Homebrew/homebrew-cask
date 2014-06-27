cf45f83410ab5456d9c17834e39c17c14d4614cc
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat ideskcal.rb according to readability conventions

--- Casks/ideskcal.rb
+++ Casks/ideskcal.rb
@@ -1,8 +1,10 @@
 class Ideskcal < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.hashbangind.com/files/iDeskCal-Latest.zip'
   appcast 'https://hashbangind.com/appcasts/iDeskCal-profileInfo.php'
   homepage 'http://www.hashbangind.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'iDeskCal.app'
 end
