c1292d9d38f4345137f725c9b15199835f3c5d79
ndr <ndr@qef.io>
2014-06-27 02:00:55 +0200

    Reformat air-connect.rb according to readability conventions

--- Casks/air-connect.rb
+++ Casks/air-connect.rb
@@ -1,7 +1,9 @@
 class AirConnect < Cask
-  url 'http://downloads.avatron.com/AirConnectInstaller.zip'
-  homepage 'http://www.avatron.com/get-air-connect/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://downloads.avatron.com/AirConnectInstaller.zip'
+  homepage 'http://www.avatron.com/get-air-connect/'
+
   link 'Air Connect.app'
 end
