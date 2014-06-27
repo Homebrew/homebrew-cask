cb949064d6a1c2b33e89d70be6a70eb44e180c25
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat ibabel.rb according to readability conventions

--- Casks/ibabel.rb
+++ Casks/ibabel.rb
@@ -1,7 +1,9 @@
 class Ibabel < Cask
-  url 'http://macinchem.org/ibabel/ibabel3_files/iBabel.zip'
-  homepage 'http://www.macinchem.org/ibabel/ibabel3.php'
   version 'latest'
   sha256 :no_check
+
+  url 'http://macinchem.org/ibabel/ibabel3_files/iBabel.zip'
+  homepage 'http://www.macinchem.org/ibabel/ibabel3.php'
+
   link 'iBabel.app'
 end
