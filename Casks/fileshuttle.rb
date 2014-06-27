91bbcf4ebe30c2f92ee53ef2e6129347512da6a1
ndr <ndr@qef.io>
2014-06-27 02:01:11 +0200

    Reformat fileshuttle.rb according to readability conventions

--- Casks/fileshuttle.rb
+++ Casks/fileshuttle.rb
@@ -1,8 +1,10 @@
 class Fileshuttle < Cask
+  version '2.1'
+  sha256 '431b7cb2161eace5b8d39146bdd1b7cc9a85d335eb29175c1a1879208928565a'
+  
   url 'http://fileshuttle.io/fileshuttle-2.1.zip'
   appcast 'http://updates.getfileshuttle.com/update.xml'
   homepage 'http://fileshuttle.io/'
-  version '2.1'
-  sha256 '431b7cb2161eace5b8d39146bdd1b7cc9a85d335eb29175c1a1879208928565a'
+
   link 'FileShuttle.app'
 end
