d3ec333c7ac65f6217432c8c3daf4e92e2a884e7
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat skype.rb according to readability conventions

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,7 +1,9 @@
 class Skype < Cask
-  url 'https://www.skype.com/go/getskype-macosx.dmg'
-  homepage 'http://www.skype.com'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.skype.com/go/getskype-macosx.dmg'
+  homepage 'http://www.skype.com'
+
   link 'Skype.app'
 end
