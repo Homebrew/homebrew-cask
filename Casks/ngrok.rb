d6f49b214dab70f9f23afe5caec3b1fcc625fa3f
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat ngrok.rb according to readability conventions

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -1,7 +1,9 @@
 class Ngrok < Cask
-  url 'https://dl.ngrok.com/darwin_amd64/ngrok.zip'
-  homepage 'https://ngrok.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://dl.ngrok.com/darwin_amd64/ngrok.zip'
+  homepage 'https://ngrok.com/'
+
   binary 'ngrok'
 end
