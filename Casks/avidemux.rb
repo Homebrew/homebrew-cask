d195e26a0e0d1dce2ec4daf28ed3eb39611c31f0
ndr <ndr@qef.io>
2014-06-27 02:00:58 +0200

    Reformat avidemux.rb according to readability conventions

--- Casks/avidemux.rb
+++ Casks/avidemux.rb
@@ -1,7 +1,9 @@
 class Avidemux < Cask
-  url 'https://downloads.sourceforge.net/avidemux/Avidemux_2.6.8_ml_64bits.dmg'
-  homepage 'http://www.avidemux.org/'
   version '2.6.8'
   sha256 '2478fbdf6e7bfc32dd98a53bb95e6a7d217d294d446910942c4904bf2094b90a'
+
+  url 'https://downloads.sourceforge.net/avidemux/Avidemux_2.6.8_ml_64bits.dmg'
+  homepage 'http://www.avidemux.org/'
+
   link 'Avidemux2.6.app'
 end
