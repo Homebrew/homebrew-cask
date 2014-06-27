90d35526b3a0ea494f645cf0a41063ecfea6e967
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat chainsaw.rb according to readability conventions

--- Casks/chainsaw.rb
+++ Casks/chainsaw.rb
@@ -1,7 +1,9 @@
 class Chainsaw < Cask
-  url 'http://people.apache.org/~sdeboy/apache-chainsaw-2.1.0-SNAPSHOT.dmg'
-  homepage 'http://logging.apache.org/chainsaw/'
   version '2.1.0'
   sha256 '06171e5bc285224611c4a1f22922ab781276ce387f8f290948617de7bbfc6f6d'
+
+  url 'http://people.apache.org/~sdeboy/apache-chainsaw-2.1.0-SNAPSHOT.dmg'
+  homepage 'http://logging.apache.org/chainsaw/'
+
   link 'Chainsaw.app'
 end
