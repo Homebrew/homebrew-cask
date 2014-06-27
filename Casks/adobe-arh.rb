d731f67bbcc3019f259f4f8f10f97dc6d95d30c9
ndr <ndr@qef.io>
2014-06-27 02:00:55 +0200

    Reformat adobe-arh.rb according to readability conventions

--- Casks/adobe-arh.rb
+++ Casks/adobe-arh.rb
@@ -1,8 +1,10 @@
 class AdobeArh < Cask
-  url 'http://airdownload.adobe.com/air/distribution/latest/mac/arh'
-  homepage 'http://help.adobe.com/en_US/air/redist/WS485a42d56cd19641-70d979a8124ef20a34b-8000.html'
   version 'latest'
   sha256 :no_check
+
+  url 'http://airdownload.adobe.com/air/distribution/latest/mac/arh'
+  homepage 'http://help.adobe.com/en_US/air/redist/WS485a42d56cd19641-70d979a8124ef20a34b-8000.html'
+
   caskroom_only true
   binary 'arh'
   container_type :naked
