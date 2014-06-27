87ec79649befb1b46e249b073683526f5e438358
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat projectlibre.rb according to readability conventions

--- Casks/projectlibre.rb
+++ Casks/projectlibre.rb
@@ -1,7 +1,9 @@
 class Projectlibre < Cask
-  url 'https://downloads.sourceforge.net/project/projectlibre/ProjectLibre/1.5.8/projectlibre-1.5.8.dmg'
-  homepage 'http://www.projectlibre.org/'
   version '1.5.8'
   sha256 'efd7d228d2e71a2a5af185d2fe1ddcea4abc55af32188344e06e1871facd0c6b'
+
+  url 'https://downloads.sourceforge.net/project/projectlibre/ProjectLibre/1.5.8/projectlibre-1.5.8.dmg'
+  homepage 'http://www.projectlibre.org/'
+
   link 'ProjectLibre.app'
 end
