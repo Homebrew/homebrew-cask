dda09ee9a48d76dffb274e9fb2c3a52e900609c1
ndr <ndr@qef.io>
2014-06-27 02:01:36 +0200

    Reformat plycounter.rb according to readability conventions

--- Casks/plycounter.rb
+++ Casks/plycounter.rb
@@ -1,7 +1,9 @@
 class Plycounter < Cask
-  url 'https://www.plycount.com/plycounter/downloads/PlyCounter.dmg'
-  homepage 'http://www.plycount.com'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.plycount.com/plycounter/downloads/PlyCounter.dmg'
+  homepage 'http://www.plycount.com'
+
   link 'PlyCounter.app'
 end
