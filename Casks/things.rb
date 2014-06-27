115f9325bdff2af8d7e06a7978aedb22043be42b
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat things.rb according to readability conventions

--- Casks/things.rb
+++ Casks/things.rb
@@ -1,8 +1,10 @@
 class Things < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://culturedcode.com/things/download/'
   appcast 'http://downloads.culturedcode.com/things/download/Things_Updates.php'
   homepage 'http://culturedcode.com/things/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Things.app'
 end
