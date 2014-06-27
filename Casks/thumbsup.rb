1a16cab0fdd68d1cca6df07a8a1cc79abe2b7145
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat thumbsup.rb according to readability conventions

--- Casks/thumbsup.rb
+++ Casks/thumbsup.rb
@@ -1,8 +1,10 @@
 class Thumbsup < Cask
-  url 'https://s3.amazonaws.com/DTWebsiteSupport/download/freeware/thumbsup/4.5/ThumbsUp.dmg.zip'
-  homepage 'http://www.devontechnologies.com/products/freeware.html#c966'
   version '4.5'
   sha256 '7a8e4fa4aaeb8ead174853b64cfcd32a1320d9fdb4d1ca8f0eb9baba18ee625d'
+
+  url 'https://s3.amazonaws.com/DTWebsiteSupport/download/freeware/thumbsup/4.5/ThumbsUp.dmg.zip'
+  homepage 'http://www.devontechnologies.com/products/freeware.html#c966'
+
   nested_container 'ThumbsUp.dmg'
   link 'ThumbsUp.app'
 end
