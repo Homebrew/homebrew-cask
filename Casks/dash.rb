6d96dcdbfd5975f6b77c3ea2b1334fd334cd5c79
ndr <ndr@qef.io>
2014-06-27 02:01:06 +0200

    Reformat dash.rb according to readability conventions

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -1,9 +1,11 @@
 class Dash < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://kapeli.com/Dash.zip'
   appcast 'http://kapeli.com/Dash.xml'
   homepage 'http://kapeli.com/dash'
-  version 'latest'
-  sha256 :no_check
+
   link 'Dash.app'
 
   after_install do
