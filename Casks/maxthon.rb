002bde2b5188393a763ca6c14f7e094fb2cf9b4c
ndr <ndr@qef.io>
2014-06-27 02:01:27 +0200

    Reformat maxthon.rb according to readability conventions

--- Casks/maxthon.rb
+++ Casks/maxthon.rb
@@ -1,7 +1,9 @@
 class Maxthon < Cask
-  url 'https://dl.maxthon.com/mac/Maxthon-4.1.3.4000.dmg'
-  homepage 'http://www.maxthon.com/'
   version '4.1.3.4000'
   sha256 '6c9c5b3aa49827907d83b641fba816b0f9d8cb240e3afb6872f2bd9de203a31e'
+
+  url 'https://dl.maxthon.com/mac/Maxthon-4.1.3.4000.dmg'
+  homepage 'http://www.maxthon.com/'
+
   link 'Maxthon.app'
 end
