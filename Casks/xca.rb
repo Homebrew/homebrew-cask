3f6f4c1f6c17b4d9bb39f17f54798b716462aa8f
ndr <ndr@qef.io>
2014-06-27 02:01:54 +0200

    Reformat xca.rb according to readability conventions

--- Casks/xca.rb
+++ Casks/xca.rb
@@ -1,8 +1,10 @@
 class Xca < Cask
+  version 'latest'
+  sha256 :no_check
+
   url 'https://sourceforge.net/projects/xca/files/latest/download',
     :user_agent => :fake
   homepage 'http://xca.sourceforge.net/'
-  version 'latest'
-  sha256 :no_check
+
   link 'xca.app'
 end
