3b5f7eba2278f87a23c6a9fc9595a44058ac9db5
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat coteditor.rb according to readability conventions

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -1,8 +1,10 @@
 class Coteditor < Cask
+  version '1.5.3'
+  sha256 'f70bcd376d2658127c5bd274a73e7bb92a0c1c9d4aa7a08376e8b9cc2b6630df'
+  
   url 'https://github.com/coteditor/CotEditor/releases/download/1.5.3/CotEditor_1.5.3.dmg'
   appcast 'http://coteditor.github.io/appcast.xml'
-  sha256 'f70bcd376d2658127c5bd274a73e7bb92a0c1c9d4aa7a08376e8b9cc2b6630df'
   homepage 'http://coteditor.github.io/'
-  version '1.5.3'
+
   link 'CotEditor.app'
 end
