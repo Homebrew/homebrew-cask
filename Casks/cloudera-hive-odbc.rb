b940a834ecba3c272a5a02f2fd290b9859a89961
ndr <ndr@qef.io>
2014-06-27 02:01:04 +0200

    Reformat cloudera-hive-odbc.rb according to readability conventions

--- Casks/cloudera-hive-odbc.rb
+++ Casks/cloudera-hive-odbc.rb
@@ -1,8 +1,10 @@
 class ClouderaHiveOdbc < Cask
-  url 'https://downloads.cloudera.com/connectors/hive-2.5.5.1006/MacOSX/ClouderaHiveODBC.dmg'
-  homepage 'http://www.cloudera.com'
   version '2.5.5'
   sha256 'cf31ace79ca995e8b7b57f1b49761777168c9f4b103092a74f6cb111084d71d8'
+
+  url 'https://downloads.cloudera.com/connectors/hive-2.5.5.1006/MacOSX/ClouderaHiveODBC.dmg'
+  homepage 'http://www.cloudera.com'
+
   install 'ClouderaHiveODBC.pkg'
   uninstall :pkgutil => 'cloudera.hiveodbc'
   caveats do
