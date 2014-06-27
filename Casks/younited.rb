4d1906149a6a086ef4a12d597336905a2552cbec
ndr <ndr@qef.io>
2014-06-27 02:01:55 +0200

    Reformat younited.rb according to readability conventions

--- Casks/younited.rb
+++ Casks/younited.rb
@@ -1,7 +1,9 @@
 class Younited < Cask
-  url 'https://download.sp.f-secure.com/younited/younited.dmg'
-  homepage 'http://www.younited.com/index.html'
   version 'latest'
   sha256 :no_check
+
+  url 'https://download.sp.f-secure.com/younited/younited.dmg'
+  homepage 'http://www.younited.com/index.html'
+
   link 'younited.app'
 end
