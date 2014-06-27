ec9d69584dab1c2d104545655e51aa13b7d9efb3
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat neofinder.rb according to readability conventions

--- Casks/neofinder.rb
+++ Casks/neofinder.rb
@@ -1,8 +1,10 @@
 class Neofinder < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.cdfinder.de/neofinder.zip'
   appcast 'http://www.wfs-apps.de/updates/neofinder-appcast-64.xml'
   homepage 'http://www.cdfinder.de'
-  version 'latest'
-  sha256 :no_check
+
   link 'NeoFinder.app'
 end
