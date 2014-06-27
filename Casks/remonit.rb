fced53bd9ad42b02c97c75eb61ebce16f393172f
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat remonit.rb according to readability conventions

--- Casks/remonit.rb
+++ Casks/remonit.rb
@@ -1,7 +1,9 @@
 class Remonit < Cask
-  url 'http://874390f0461dc5bbf96b-8953e31051b5247f1143d89b1a42aa7d.r65.cf2.rackcdn.com/remonit-0.2.0-mac.zip'
-  homepage 'http://zef.io/remonit/'
   version '0.2.0'
   sha256 'fb8e51c7d68d83274b2acff107e7ef4857d28c9daf4d5ac2599c14cb3d6516fe'
+
+  url 'http://874390f0461dc5bbf96b-8953e31051b5247f1143d89b1a42aa7d.r65.cf2.rackcdn.com/remonit-0.2.0-mac.zip'
+  homepage 'http://zef.io/remonit/'
+
   link 'Remonit.app'
 end
