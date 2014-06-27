41ebb6c0b5eff4d25ecd08eb8290dcd7c97296bf
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat textmate.rb according to readability conventions

--- Casks/textmate.rb
+++ Casks/textmate.rb
@@ -1,7 +1,9 @@
 class Textmate < Cask
-  url 'https://api.textmate.org/downloads/release'
-  homepage 'http://macromates.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://api.textmate.org/downloads/release'
+  homepage 'http://macromates.com/'
+
   link 'TextMate.app'
 end
