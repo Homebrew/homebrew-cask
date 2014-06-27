a337a339d6a75d7dc91cd031e254623e818045c7
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat navicat-for-sql-server.rb according to readability conventions

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,7 +1,9 @@
 class NavicatForSqlServer < Cask
-  url 'http://download.navicat.com/download/navicat110_sqlserver_en.dmg'
-  homepage 'http://www.navicat.com/products/navicat-for-sqlserver'
   version '11.0.18'
   sha256 '3aeec407860327b1ac2d6622316b6cb5297dbc4ee2a6fe964b300c0a79891bce'
+
+  url 'http://download.navicat.com/download/navicat110_sqlserver_en.dmg'
+  homepage 'http://www.navicat.com/products/navicat-for-sqlserver'
+
   link 'Navicat For SQL Server.app'
 end
