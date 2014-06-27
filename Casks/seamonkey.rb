a01f5ad2e26982dd9a6a2e72f4e60278347f993e
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat seamonkey.rb according to readability conventions

--- Casks/seamonkey.rb
+++ Casks/seamonkey.rb
@@ -1,7 +1,9 @@
 class Seamonkey < Cask
-  url 'https://download.mozilla.org/?product=seamonkey-2.25&os=osx&lang=en-US'
-  homepage 'http://www.seamonkey-project.org/'
   version '2.25'
   sha256 '3217e3d1fb94376d01ca38cc926b1b23306579c96e96adfdd50f0f66fc71c5e8'
+
+  url 'https://download.mozilla.org/?product=seamonkey-2.25&os=osx&lang=en-US'
+  homepage 'http://www.seamonkey-project.org/'
+
   link 'SeaMonkey.app'
 end
