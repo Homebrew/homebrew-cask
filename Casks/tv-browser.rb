21edd76de567cb92560a97ef2f9101858cf33cbc
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat tv-browser.rb according to readability conventions

--- Casks/tv-browser.rb
+++ Casks/tv-browser.rb
@@ -1,7 +1,9 @@
 class TvBrowser < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/tvbrowser/tvbrowser_3.3.3_mac.dmg'
-  homepage 'http://www.tvbrowser.org/index.php?setlang=en'
   version '3.3.3'
   sha256 '951fe8478fa2efa5f7784c23f153bcc8b0a6fcb8a7405c4990c44b3c967e73cc'
+
+  url 'https://downloads.sourceforge.net/sourceforge/tvbrowser/tvbrowser_3.3.3_mac.dmg'
+  homepage 'http://www.tvbrowser.org/index.php?setlang=en'
+
   link 'TV-Browser.app'
 end
