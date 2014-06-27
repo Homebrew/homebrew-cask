32dcfd51ee480b4c1b61dc0984e4e53b868ffe17
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat propane.rb according to readability conventions

--- Casks/propane.rb
+++ Casks/propane.rb
@@ -1,8 +1,10 @@
 class Propane < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://propaneapp.com/appcast/Propane.zip'
   appcast 'http://propaneapp.com/appcast/release.xml'
   homepage 'http://propaneapp.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Propane.app'
 end
