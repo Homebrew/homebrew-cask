d5936304ce9df02c868b2c29ffb30dc95b48f0b8
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat pharo.rb according to readability conventions

--- Casks/pharo.rb
+++ Casks/pharo.rb
@@ -1,7 +1,9 @@
 class Pharo < Cask
-  url 'http://files.pharo.org/platform/Pharo3.0-mac.zip'
-  homepage 'http://www.pharo-project.org/home'
   version '3.0'
   sha256 '57f251d31726176bf7dff866198ca75f03bb477e1dd024307eb157725acf596f'
+
+  url 'http://files.pharo.org/platform/Pharo3.0-mac.zip'
+  homepage 'http://www.pharo-project.org/home'
+
   link 'Pharo3.0.app'
 end
