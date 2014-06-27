093eab384b9b9ff6763f69e52c7eb03bbaafec8c
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat hyro.rb according to readability conventions

--- Casks/hyro.rb
+++ Casks/hyro.rb
@@ -1,7 +1,9 @@
 class Hyro < Cask
-  url 'https://jawerty.github.io/Hyro/apps/Hyro-0.0.3.dmg'
-  homepage 'http://jawerty.github.io/Hyro/'
   version '0.0.3'
   sha256 '2a1fed542c0e9ecc148ac0e7241adf774762f06fc6bfbbf8b2e96437a4c915f7'
+
+  url 'https://jawerty.github.io/Hyro/apps/Hyro-0.0.3.dmg'
+  homepage 'http://jawerty.github.io/Hyro/'
+
   link 'Hyro.app'
 end
