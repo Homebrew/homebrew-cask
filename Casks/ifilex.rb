6abb8d38a50aa0a090b10aa0e80d8b0d1206894c
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat ifilex.rb according to readability conventions

--- Casks/ifilex.rb
+++ Casks/ifilex.rb
@@ -1,7 +1,9 @@
 class Ifilex < Cask
-  url 'http://www.osxbytes.com/iFileX.dmg'
-  homepage 'http://www.osxbytes.com/page3/index.html'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.osxbytes.com/iFileX.dmg'
+  homepage 'http://www.osxbytes.com/page3/index.html'
+
   link 'iFileX.app'
 end
