c7f26baba009c51204043192cfe84ea4adfc4b0c
ndr <ndr@qef.io>
2014-06-27 02:01:11 +0200

    Reformat filezilla.rb according to readability conventions

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,7 +1,9 @@
 class Filezilla < Cask
-  url 'https://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.8.1/FileZilla_3.8.1_macosx-x86.app.tar.bz2'
-  homepage 'https://filezilla-project.org/'
   version '3.8.1'
   sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
+
+  url 'https://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.8.1/FileZilla_3.8.1_macosx-x86.app.tar.bz2'
+  homepage 'https://filezilla-project.org/'
+
   link 'FileZilla.app'
 end
