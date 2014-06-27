ae25adf49204af69d559ebcbdcef5b6de6b53e36
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat soundflower.rb according to readability conventions

--- Casks/soundflower.rb
+++ Casks/soundflower.rb
@@ -1,8 +1,10 @@
 class Soundflower < Cask
-  url 'https://soundflower.googlecode.com/files/Soundflower-1.6.6b.dmg'
-  homepage 'https://code.google.com/p/soundflower/'
   version '1.6.6b'
   sha256 '61ca31d7478d057e32caaeac3c739f965ba9eb2a27570b3cc715e706d4740dfb'
+
+  url 'https://soundflower.googlecode.com/files/Soundflower-1.6.6b.dmg'
+  homepage 'https://code.google.com/p/soundflower/'
+
   install 'Soundflower.pkg', :allow_untrusted => true
   uninstall :pkgutil => 'com.cycling74.soundflower.*'
   caveats do
