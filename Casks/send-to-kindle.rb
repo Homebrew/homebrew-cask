efb1afa2925a7bbd413eb0466c55e63b68f3fa14
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat send-to-kindle.rb according to readability conventions

--- Casks/send-to-kindle.rb
+++ Casks/send-to-kindle.rb
@@ -1,8 +1,10 @@
 class SendToKindle < Cask
-  url 'https://s3.amazonaws.com/sendtokindle/SendToKindleForMac-installer-v1.0.0.218.pkg'
-  homepage 'http://www.amazon.com/gp/sendtokindle/mac'
   version '1.0.0.218'
   sha256 '9c3a9eca8fe9f09ff43dfb4132a8be163ef517008979075df3d947699c421494'
+
+  url 'https://s3.amazonaws.com/sendtokindle/SendToKindleForMac-installer-v1.0.0.218.pkg'
+  homepage 'http://www.amazon.com/gp/sendtokindle/mac'
+
   install 'SendToKindleForMac-installer-v1.0.0.218.pkg'
   uninstall :launchctl => 'com.amazon.sendtokindle.launcher',
             :pkgutil   => 'com.amazon.SendToKindleMacInstaller.pkg'
