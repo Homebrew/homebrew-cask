b8835ba1138ef82f539514caa70132b0ba95446e
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat no-ip-duc.rb according to readability conventions

--- Casks/no-ip-duc.rb
+++ Casks/no-ip-duc.rb
@@ -1,7 +1,9 @@
 class NoIpDuc < Cask
-  url 'https://www.noip.com/client/mac/noip3.1.5.dmg'
-  homepage 'http://www.noip.com/download?page=mac'
   version '3.1.5'
   sha256 'a5c68a13188fd88f00c0588baa1b54a0ac2529a02b09dc436770183c059bfaf5'
+
+  url 'https://www.noip.com/client/mac/noip3.1.5.dmg'
+  homepage 'http://www.noip.com/download?page=mac'
+
   link 'No-IP DUC.app'
 end
