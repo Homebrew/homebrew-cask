74a31061bdbcbc58a5d66ff4b8c1bf049943e61d
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat love.rb according to readability conventions

--- Casks/love.rb
+++ Casks/love.rb
@@ -1,7 +1,9 @@
 class Love < Cask
-  url 'https://bitbucket.org/rude/love/downloads/love-0.9.1-macosx-x64.zip'
-  homepage 'http://love2d.org'
   version '0.9.1'
   sha256 'ce8e31a1bbb5eb628c9edf8a823b877743f2e0fa60ad8ec51db4957a13c09084'
+
+  url 'https://bitbucket.org/rude/love/downloads/love-0.9.1-macosx-x64.zip'
+  homepage 'http://love2d.org'
+
   link 'love.app'
 end
