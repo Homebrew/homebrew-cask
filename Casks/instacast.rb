e6aac0a795f4892da7e5491fbdd8f749e9eda395
ndr <ndr@qef.io>
2014-06-27 02:01:19 +0200

    Reformat instacast.rb according to readability conventions

--- Casks/instacast.rb
+++ Casks/instacast.rb
@@ -1,7 +1,9 @@
 class Instacast < Cask
-  url 'http://assets.vemedio.com/software/instacast/Instacast-1.2.4-2250.dmg'
-  homepage 'http://vemedio.com/products/instacast-mac'
   version '1.2.4 (2250)'
   sha256 'e1266e152ab753d743c891a929dfffb9ab037bf164671ac6172f0caa1c7576f3'
+
+  url 'http://assets.vemedio.com/software/instacast/Instacast-1.2.4-2250.dmg'
+  homepage 'http://vemedio.com/products/instacast-mac'
+
   link 'Instacast.app'
 end
