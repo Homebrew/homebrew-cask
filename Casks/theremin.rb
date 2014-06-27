0acbbdc641f0f10bc6ba7e6d1564722a4179b946
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat theremin.rb according to readability conventions

--- Casks/theremin.rb
+++ Casks/theremin.rb
@@ -1,8 +1,10 @@
 class Theremin < Cask
+  version '0.7'
+  sha256 '809d0f7527d072a43f33b9e1088dc2387e08bb1a3696bb60bfd8e82b8853102d'
+  
   url 'http://f.nn.lv/ms/l5/29/Theremin.app.zip'
   appcast 'http://theremin.amd.co.at/appcastProfileInfo.php'
   homepage 'https://github.com/TheStalwart/Theremin'
-  version '0.7'
-  sha256 '809d0f7527d072a43f33b9e1088dc2387e08bb1a3696bb60bfd8e82b8853102d'
+
   link 'Theremin.app'
 end
