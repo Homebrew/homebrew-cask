fdde0f230e132af3212fa415ab9d7dc6c71ec04c
ndr <ndr@qef.io>
2014-06-27 02:00:57 +0200

    Reformat arq.rb according to readability conventions

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,7 +1,9 @@
 class Arq < Cask
-  url 'http://www.haystacksoftware.com/arq/Arq.zip'
-  homepage 'http://www.haystacksoftware.com/arq/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.haystacksoftware.com/arq/Arq.zip'
+  homepage 'http://www.haystacksoftware.com/arq/'
+
   link 'Arq.app'
 end
