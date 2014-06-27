9242116f793cc1fa1e5baa2a94fbfb61526d8d67
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat mame.rb according to readability conventions

--- Casks/mame.rb
+++ Casks/mame.rb
@@ -1,7 +1,9 @@
 class Mame < Cask
-  url 'https://downloads.sourceforge.net/mameosx/MAMEOSX-0.135.dmg'
-  homepage 'http://mameosx.sourceforge.net/'
   version '0.135'
   sha256 'fce1a45e53e0f6bc2ef20e5b6fc84bd48806f1bc2a38acec57fd9fafe7e2af7e'
+
+  url 'https://downloads.sourceforge.net/mameosx/MAMEOSX-0.135.dmg'
+  homepage 'http://mameosx.sourceforge.net/'
+
   link 'MAME OS X.app'
 end
