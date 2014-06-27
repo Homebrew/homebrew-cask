5a04ce6d5eb9dea3bb6d7952fe5fd2bbeb1f447e
ndr <ndr@qef.io>
2014-06-27 02:01:16 +0200

    Reformat growlvoice.rb according to readability conventions

--- Casks/growlvoice.rb
+++ Casks/growlvoice.rb
@@ -1,7 +1,9 @@
 class Growlvoice < Cask
-  url 'http://www.growlvoice.com/trials/growlvoice-2.0.3.zip'
-  homepage 'http://www.growlvoice.com/'
   version '2.0.3'
   sha256 '6c9e15b83894746e2bf9488228e95eb2f47520b1fdac728da52d16532e2ee979'
+
+  url 'http://www.growlvoice.com/trials/growlvoice-2.0.3.zip'
+  homepage 'http://www.growlvoice.com/'
+
   link 'GrowlVoice.app'
 end
