42cc8411ef0bd78b75c75642377c463e4e4dcac7
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat scribus.rb according to readability conventions

--- Casks/scribus.rb
+++ Casks/scribus.rb
@@ -1,7 +1,9 @@
 class Scribus < Cask
-  url 'https://downloads.sourceforge.net/project/scribus/scribus/1.4.4/scribus-1.4.4.dmg'
-  homepage 'http://www.scribus.net/canvas/Scribus'
   version '1.4.4'
   sha256 '57cdfacbfa6c60c035b746ac40ea8c46718fdfd4a9ac382b3b6c56a318fa162c'
+
+  url 'https://downloads.sourceforge.net/project/scribus/scribus/1.4.4/scribus-1.4.4.dmg'
+  homepage 'http://www.scribus.net/canvas/Scribus'
+
   link 'Scribus.app'
 end
