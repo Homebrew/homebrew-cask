68f0197400bfe4a9791309a512b4e97ff69609dc
ndr <ndr@qef.io>
2014-06-27 02:01:10 +0200

    Reformat fantastical.rb according to readability conventions

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,9 +1,11 @@
 class Fantastical < Cask
+  version '1.3.15'
+  sha256 'df76970ba48d831e99f1d1564f4076271ca48476ed8af5b8996341481e8bfb74'
+  
   url 'http://cdn.flexibits.com/Fantastical_1.3.15.zip'
   appcast 'https://flexibits.com/fantastical/appcast.php'
   homepage 'http://flexibits.com/fantastical'
-  version '1.3.15'
-  sha256 'df76970ba48d831e99f1d1564f4076271ca48476ed8af5b8996341481e8bfb74'
+
   link 'Fantastical.app'
 
   after_install do
