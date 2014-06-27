d8d45bdb2a663bb70a141f0621bed5f8c50d12aa
ndr <ndr@qef.io>
2014-06-27 02:01:27 +0200

    Reformat mediathekview.rb according to readability conventions

--- Casks/mediathekview.rb
+++ Casks/mediathekview.rb
@@ -1,7 +1,9 @@
 class Mediathekview < Cask
-  url 'https://downloads.sourceforge.net/project/zdfmediathk/Mediathek/Mediathek%204/MediathekView_4_OSX.dmg'
-  homepage 'http://sourceforge.net/projects/zdfmediathk/'
   version '4'
   sha256 '41f53b86b6475fa0d8e347e9430597a975fb4f7c13eae70e82b723dace5b7c8a'
+
+  url 'https://downloads.sourceforge.net/project/zdfmediathk/Mediathek/Mediathek%204/MediathekView_4_OSX.dmg'
+  homepage 'http://sourceforge.net/projects/zdfmediathk/'
+
   link 'MediathekView.app'
 end
