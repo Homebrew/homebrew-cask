65dfc9c412adae5af826ba8e5e053bde7f5e91b5
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat papers.rb according to readability conventions

--- Casks/papers.rb
+++ Casks/papers.rb
@@ -1,8 +1,10 @@
 class Papers < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://papersapp.com/papers/download'
   appcast 'http://www.papersapp.com/papers/appcast_v3.xml'
   homepage 'http://www.papersapp.com/papers/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Papers.app'
 end
