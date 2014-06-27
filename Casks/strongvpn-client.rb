b9edca6099f63dd9cf088545238eac98573675b3
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat strongvpn-client.rb according to readability conventions

--- Casks/strongvpn-client.rb
+++ Casks/strongvpn-client.rb
@@ -1,8 +1,10 @@
 class StrongvpnClient < Cask
+  version '1.0.4'
+  sha256 'cacf4f7a94b1601bae2cf19fe94b6dbae827ed531e05ff0f91723aea5b3845b9'
+  
   url 'https://mirror2.reliablehosting.com/mac/StrongVPN_Mac_1.0.4.dmg'
   appcast 'https://colomovers.com/mac.xml'
   homepage 'http://strongvpn.com/vpnclient.shtml'
-  version '1.0.4'
-  sha256 'cacf4f7a94b1601bae2cf19fe94b6dbae827ed531e05ff0f91723aea5b3845b9'
+
   link 'StrongVPN Client.app'
 end
