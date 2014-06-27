f5a47ada885f674d8441fc43c1777959386f859b
ndr <ndr@qef.io>
2014-06-27 02:01:36 +0200

    Reformat pixelpeeper.rb according to readability conventions

--- Casks/pixelpeeper.rb
+++ Casks/pixelpeeper.rb
@@ -1,8 +1,10 @@
 class Pixelpeeper < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.irradiatedsoftware.com/download/PixelPeeper.zip'
   appcast 'http://www.irradiatedsoftware.com/updates/profiles/pixelpeeper.php'
   homepage 'http://www.irradiatedsoftware.com/labs'
-  version 'latest'
-  sha256 :no_check
+
   link 'PixelPeeper.app'
 end
