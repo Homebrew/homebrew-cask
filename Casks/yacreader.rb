54fa85bf39fdd045fe2fbae00fda624b03d29c1f
ndr <ndr@qef.io>
2014-06-27 02:01:54 +0200

    Reformat yacreader.rb according to readability conventions

--- Casks/yacreader.rb
+++ Casks/yacreader.rb
@@ -1,8 +1,10 @@
 class Yacreader < Cask
-  url 'https://bitbucket.org/luisangelsm/yacreader/downloads/YACReader-7.0.2-MacOSX-Intel.dmg'
-  homepage 'http://www.yacreader.com'
   version '7.0.2'
   sha256 '813c27b6effa62a17ab939a4734fb5f0245308e9e00cc907bb1210636e43e1d7'
+
+  url 'https://bitbucket.org/luisangelsm/yacreader/downloads/YACReader-7.0.2-MacOSX-Intel.dmg'
+  homepage 'http://www.yacreader.com'
+
   link 'YACReader.app'
   link 'YACReaderLibrary.app'
 end
