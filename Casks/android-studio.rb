928d7f7aca6f0519851d2673fd7c2d52db9b88db
ndr <ndr@qef.io>
2014-06-27 02:00:56 +0200

    Reformat android-studio.rb according to readability conventions

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,7 +1,9 @@
 class AndroidStudio < Cask
-  url 'https://dl.google.com/dl/android/studio/ide-zips/0.6.1/android-studio-ide-135.1224218-mac.zip'
-  homepage 'https://developer.android.com/sdk/installing/studio.html'
   version '0.6.1 build-135.1224218'
   sha256 '64c395fd0e4a0d69ff6f8d5c06a5f7fc6f0f7a06096408b9849ce92d5d515182'
+
+  url 'https://dl.google.com/dl/android/studio/ide-zips/0.6.1/android-studio-ide-135.1224218-mac.zip'
+  homepage 'https://developer.android.com/sdk/installing/studio.html'
+
   link 'Android Studio.app'
 end
