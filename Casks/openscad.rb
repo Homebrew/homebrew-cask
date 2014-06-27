df7a21fe365794e13e8a0a917317b6707c96a18e
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat openscad.rb according to readability conventions

--- Casks/openscad.rb
+++ Casks/openscad.rb
@@ -1,7 +1,9 @@
 class Openscad < Cask
-  url 'http://files.openscad.org/OpenSCAD-2014.03.dmg'
-  homepage 'http://www.openscad.org/'
   version '2014.03'
   sha256 'c324c19c2d36f21517b602f8ae2ec04fa14c90896c91fc8dbb37aed5b3ba16bd'
+
+  url 'http://files.openscad.org/OpenSCAD-2014.03.dmg'
+  homepage 'http://www.openscad.org/'
+
   link 'OpenSCAD.app'
 end
