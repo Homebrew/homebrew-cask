148c69fe657a1363368f0ac4afa91d05068beabb
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat preference-manager.rb according to readability conventions

--- Casks/preference-manager.rb
+++ Casks/preference-manager.rb
@@ -1,8 +1,10 @@
 class PreferenceManager < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://download.digitalrebellion.com/Pref_Man.dmg'
   appcast 'http://www.digitalrebellion.com/rss/appcasts/pref_man_appcast.xml'
   homepage 'http://www.digitalrebellion.com/prefman'
-  version 'latest'
-  sha256 :no_check
+
   link 'Preference Manager.app'
 end
