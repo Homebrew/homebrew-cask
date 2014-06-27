83888ad26f97c419bc3219de9febbae4eaf0f8b4
ndr <ndr@qef.io>
2014-06-27 02:01:20 +0200

    Reformat iupx.rb according to readability conventions

--- Casks/iupx.rb
+++ Casks/iupx.rb
@@ -1,8 +1,10 @@
 class Iupx < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://sourceforge.net/projects/iupx/files/latest/download'
   appcast 'http://iupx.sourceforge.net/updates/appcast.xml'
   homepage 'http://iupx.sourceforge.net'
-  version 'latest'
-  sha256 :no_check
+
   link 'iUPX.app'
 end
