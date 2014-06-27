0cfa5c7c230b25d89dec8bde9985f30281a30ae9
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat notifyr.rb according to readability conventions

--- Casks/notifyr.rb
+++ Casks/notifyr.rb
@@ -1,7 +1,9 @@
 class Notifyr < Cask
-  url 'http://getnotifyr.com/app/Notifyr.zip'
-  homepage 'http://getnotifyr.com'
   version 'latest'
   sha256 :no_check
+
+  url 'http://getnotifyr.com/app/Notifyr.zip'
+  homepage 'http://getnotifyr.com'
+
   prefpane 'Notifyr.prefPane'
 end
