839796fa895e6c7e94e17f79176297e801fd7ed3
ndr <ndr@qef.io>
2014-06-27 02:00:56 +0200

    Reformat amadeus-pro.rb according to readability conventions

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -1,7 +1,9 @@
 class AmadeusPro < Cask
-  url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.dmg'
-  homepage 'http://www.hairersoft.com/pro.html'
   version 'latest'
   sha256 :no_check
+
+  url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.dmg'
+  homepage 'http://www.hairersoft.com/pro.html'
+
   link 'Amadeus Pro.app'
 end
