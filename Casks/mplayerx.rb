eba39c8c2fb1ae1f90826de39919b737232d2099
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat mplayerx.rb according to readability conventions

--- Casks/mplayerx.rb
+++ Casks/mplayerx.rb
@@ -1,7 +1,9 @@
 class Mplayerx < Cask
-  url 'https://downloads.sourceforge.net/project/mplayerx-osx/MPlayerX-1.0.22.1.zip'
-  homepage 'http://mplayerx.org/'
   version '1.0.22.1'
   sha256 '08ce85671814e65b8c7ec8438b85be593b6deaf7d5c3b242e686a6b0176a2c77'
+
+  url 'https://downloads.sourceforge.net/project/mplayerx-osx/MPlayerX-1.0.22.1.zip'
+  homepage 'http://mplayerx.org/'
+
   link 'MPlayerX.app'
 end
