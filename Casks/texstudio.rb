8b91cca3f5c833b87237b99751b70d817b14ca5a
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat texstudio.rb according to readability conventions

--- Casks/texstudio.rb
+++ Casks/texstudio.rb
@@ -1,7 +1,9 @@
 class Texstudio < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/texstudio/texstudio_2.7.0_osx_qt5.dmg'
-  homepage 'http://texstudio.sourceforge.net/'
   version '2.7.0'
   sha256 '70a95e7602a790b71744ce84a649dfe5f7b597caa42007e3c34c3e631ffadb22'
+
+  url 'https://downloads.sourceforge.net/sourceforge/texstudio/texstudio_2.7.0_osx_qt5.dmg'
+  homepage 'http://texstudio.sourceforge.net/'
+
   link 'texstudio.app'
 end
