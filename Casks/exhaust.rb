a7c95ad9bf0658521557e2913285bac344832082
ndr <ndr@qef.io>
2014-06-27 02:01:10 +0200

    Reformat exhaust.rb according to readability conventions

--- Casks/exhaust.rb
+++ Casks/exhaust.rb
@@ -1,8 +1,10 @@
 class Exhaust < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://download.mrgeckosmedia.com/Exhaust.zip'
   appcast 'http://mrgeckosmedia.com/applications/appcast/Exhaust'
   homepage 'https://mrgeckosmedia.com/applications/info/Exhaust'
-  version 'latest'
-  sha256 :no_check
+
   link 'Exhaust/Exhaust.app'
 end
