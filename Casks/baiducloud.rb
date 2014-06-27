150ff389b0da90d605bdc1a90b92449a82711f50
ndr <ndr@qef.io>
2014-06-27 02:00:59 +0200

    Reformat baiducloud.rb according to readability conventions

--- Casks/baiducloud.rb
+++ Casks/baiducloud.rb
@@ -1,8 +1,10 @@
 # encoding: UTF-8
 class Baiducloud < Cask
-  url 'http://bcscdn.baidu.com/netdisk/BaiduYun_2.4.2.dmg'
-  homepage 'http://pan.baidu.com'
   version '2.4.2'
   sha256 '194e049020bc11110807d7a157c5ab1f25f987b3e14ead7b452319420b97ad51'
+
+  url 'http://bcscdn.baidu.com/netdisk/BaiduYun_2.4.2.dmg'
+  homepage 'http://pan.baidu.com'
+
   link '百度云同步盘.app'
 end
