9992bc49c828bdfb5b04e5dd76501c6e34c60e5a
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat musicplayer.rb according to readability conventions

--- Casks/musicplayer.rb
+++ Casks/musicplayer.rb
@@ -1,7 +1,9 @@
 class Musicplayer < Cask
-  url 'http://ufd.dk/res/MusicPlayer.app.zip'
-  homepage 'http://ufd.dk/musicplayer'
   version 'latest'
   sha256 :no_check
+
+  url 'http://ufd.dk/res/MusicPlayer.app.zip'
+  homepage 'http://ufd.dk/musicplayer'
+
   link 'MusicPlayer.app'
 end
