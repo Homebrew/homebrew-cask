24c2e2f1574d6626cac8199aba8d9c6e8f559a38
ndr <ndr@qef.io>
2014-06-27 02:01:23 +0200

    Reformat kod.rb according to readability conventions

--- Casks/kod.rb
+++ Casks/kod.rb
@@ -1,8 +1,10 @@
 class Kod < Cask
-  url 'http://data.hunch.se/kod/kod-0.0.3.zip'
-  homepage 'https://github.com/rsms/kod/'
   version '0.0.3'
   sha256 'bbc645f03e3aa49438606f2c874b6773c648e8409a395149492ec000b10d4ddb'
+
+  url 'http://data.hunch.se/kod/kod-0.0.3.zip'
+  homepage 'https://github.com/rsms/kod/'
+
   link 'Kod.app'
   binary 'Kod.app/Contents/SharedSupport/kod'
 end
