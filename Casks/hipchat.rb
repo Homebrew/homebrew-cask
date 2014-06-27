07a3d857ddc20507591df5e00a13b49b52cf46be
ndr <ndr@qef.io>
2014-06-27 02:01:17 +0200

    Reformat hipchat.rb according to readability conventions

--- Casks/hipchat.rb
+++ Casks/hipchat.rb
@@ -1,8 +1,10 @@
 class Hipchat < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.hipchat.com/downloads/latest/mac'
   appcast 'https://www.hipchat.com/release_notes/appcast/mac'
   homepage 'https://www.hipchat.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'HipChat.app'
 end
