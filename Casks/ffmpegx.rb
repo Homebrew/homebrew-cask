ac2437939580640a69bbff0d7e1367e7601b4757
ndr <ndr@qef.io>
2014-06-27 02:01:11 +0200

    Reformat ffmpegx.rb according to readability conventions

--- Casks/ffmpegx.rb
+++ Casks/ffmpegx.rb
@@ -1,7 +1,9 @@
 class Ffmpegx < Cask
-  url 'ftp://ftp2.ffmpegx.com/ffmpegx/ffmpegX.dmg'
-  homepage 'http://www.ffmpegx.com/download.html'
   version 'latest'
   sha256 :no_check
+
+  url 'ftp://ftp2.ffmpegx.com/ffmpegx/ffmpegX.dmg'
+  homepage 'http://www.ffmpegx.com/download.html'
+
   link 'ffmpegX.app'
 end
