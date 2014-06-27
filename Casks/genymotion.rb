fc36a61caeeaee605a5d4a57ff8b3033b3e44c37
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat genymotion.rb according to readability conventions

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -1,8 +1,10 @@
 class Genymotion < Cask
-  url 'http://files2.genymotion.com/genymotion/genymotion-2.2.2/genymotion-2.2.2.dmg'
-  homepage 'http://www.genymotion.com/'
   version '2.2.2'
   sha256 '216e05f20ce198324a5aabae737369b185e6284b449af372eba0abbf3525183d'
+
+  url 'http://files2.genymotion.com/genymotion/genymotion-2.2.2/genymotion-2.2.2.dmg'
+  homepage 'http://www.genymotion.com/'
+
   link 'Genymotion.app'
   link 'Genymotion Shell.app'
 end
