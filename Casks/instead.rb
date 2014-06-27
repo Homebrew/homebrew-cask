346744b1cd1efccbbc938f5b8f42a57d593187fb
ndr <ndr@qef.io>
2014-06-27 02:01:19 +0200

    Reformat instead.rb according to readability conventions

--- Casks/instead.rb
+++ Casks/instead.rb
@@ -1,7 +1,9 @@
 class Instead < Cask
-  url 'https://downloads.sourceforge.net/project/instead/instead/2.0.3/Instead-2.0.3.dmg'
-  homepage 'http://instead.syscall.ru/'
   version '2.0.3'
   sha256 'ccad05fce186df643c1312eb07f8b6e2c537df56de54f5a2f7059830822b5500'
+
+  url 'https://downloads.sourceforge.net/project/instead/instead/2.0.3/Instead-2.0.3.dmg'
+  homepage 'http://instead.syscall.ru/'
+
   link 'Instead.app'
 end
