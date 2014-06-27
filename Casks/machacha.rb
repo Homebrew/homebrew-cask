0564c1a0b57e294bd3b1a76f5e7b40c5f21f94a8
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat machacha.rb according to readability conventions

--- Casks/machacha.rb
+++ Casks/machacha.rb
@@ -1,7 +1,9 @@
 class Machacha < Cask
-  url 'http://www.julifos.com/soft/machacha/machacha.dmg'
-  homepage 'http://www.julifos.com/soft/machacha/index.html'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.julifos.com/soft/machacha/machacha.dmg'
+  homepage 'http://www.julifos.com/soft/machacha/index.html'
+
   link 'MacHacha.app'
 end
