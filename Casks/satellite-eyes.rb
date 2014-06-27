c7e95c0c6c8c0b1a26764666619f72cca5daa518
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat satellite-eyes.rb according to readability conventions

--- Casks/satellite-eyes.rb
+++ Casks/satellite-eyes.rb
@@ -1,8 +1,10 @@
 class SatelliteEyes < Cask
+  version '1.2.4'
+  sha256 'adc1554c9095e3f316474e0f3712fa2a086d9405d126eca6359b8ebdc7451902'
+  
   url 'https://satellite-eyes.s3.amazonaws.com/satellite-eyes-1.2.4.zip'
   appcast 'https://satellite-eyes.s3.amazonaws.com/appcast.xml'
   homepage 'http://satelliteeyes.tomtaylor.co.uk/'
-  version '1.2.4'
-  sha256 'adc1554c9095e3f316474e0f3712fa2a086d9405d126eca6359b8ebdc7451902'
+
   link 'Satellite Eyes.app'
 end
