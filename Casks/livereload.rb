1e630b286dedb949db7932b584d75e50de58fbd2
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat livereload.rb according to readability conventions

--- Casks/livereload.rb
+++ Casks/livereload.rb
@@ -1,8 +1,10 @@
 class Livereload < Cask
+  version '2.3.26'
+  sha256 '94ede4d8b0c991f3e712659628e9dc01d20991ebaabb3a5579647ea3646c9ad4'
+  
   url 'http://download.livereload.com/LiveReload-2.3.26.zip'
   appcast 'https://s3.amazonaws.com/download.livereload.com/LiveReload-Mac-appcast.xml'
   homepage 'http://www.livereload.com'
-  version '2.3.26'
-  sha256 '94ede4d8b0c991f3e712659628e9dc01d20991ebaabb3a5579647ea3646c9ad4'
+
   link 'LiveReload.app'
 end
