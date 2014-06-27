423b2b3426aadb1a064d2cf5af4798054355c8a8
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat lightpaper.rb according to readability conventions

--- Casks/lightpaper.rb
+++ Casks/lightpaper.rb
@@ -1,8 +1,10 @@
 class Lightpaper < Cask
+  version '0.8.7'
+  sha256 'e4ec5c87f8e35b9688f25808def9efa610923858d60c26917315422094f0108a'
+  
   url 'http://cl.ly/3c370B2o0J2A/download/LightPaper_v0.8.7.dmg'
   appcast 'http://links.clockworkengine.com/lp-mac-update-feed'
   homepage 'http://clockworkengine.com/lightpaper-mac/'
-  version '0.8.7'
-  sha256 'e4ec5c87f8e35b9688f25808def9efa610923858d60c26917315422094f0108a'
+
   link 'LightPaper.app'
 end
