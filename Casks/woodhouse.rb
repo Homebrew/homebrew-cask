476b662a93a797f342f038adf9b9aca74da77c49
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat woodhouse.rb according to readability conventions

--- Casks/woodhouse.rb
+++ Casks/woodhouse.rb
@@ -1,8 +1,10 @@
 class Woodhouse < Cask
+  version '0.5.0'
+  sha256 '3ed1b6711eb9ec862e34addc141d681d9b0fed2950b9d9b7189734087eeb7541'
+  
   url 'https://github.com/downloads/phinze/woodhouse/Woodhouse-0.5.0.dmg'
   appcast 'http://phinze.github.com/woodhouse/appcast.xml'
   homepage 'https://github.com/phinze/woodhouse/'
-  version '0.5.0'
-  sha256 '3ed1b6711eb9ec862e34addc141d681d9b0fed2950b9d9b7189734087eeb7541'
+
   link 'Woodhouse.app'
 end
