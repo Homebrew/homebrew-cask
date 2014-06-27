a545ca68f5d1fcb35689bbb6d5d7869f03c88b20
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat vistrails.rb according to readability conventions

--- Casks/vistrails.rb
+++ Casks/vistrails.rb
@@ -1,7 +1,9 @@
 class Vistrails < Cask
-  url 'https://downloads.sourceforge.net/project/vistrails/vistrails/v2.1.1/vistrails-mac-10.6-intel-2.1.1-90975fc00211.dmg'
-  homepage 'http://www.vistrails.org/index.php/Main_Page'
   version '2.1.1'
   sha256 '38ac9da9af7a557cee6eb703517e38ea1bcc430706a436a182eec6f976275b02'
+
+  url 'https://downloads.sourceforge.net/project/vistrails/vistrails/v2.1.1/vistrails-mac-10.6-intel-2.1.1-90975fc00211.dmg'
+  homepage 'http://www.vistrails.org/index.php/Main_Page'
+
   link 'VisTrails'
 end
