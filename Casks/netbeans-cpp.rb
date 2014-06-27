1854012ca10737974210a2efa3b16992c1143231
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat netbeans-cpp.rb according to readability conventions

--- Casks/netbeans-cpp.rb
+++ Casks/netbeans-cpp.rb
@@ -1,8 +1,10 @@
 class NetbeansCpp < Cask
-  url 'http://download.netbeans.org/netbeans/8.0/final/bundles/netbeans-8.0-cpp-macosx.dmg'
-  homepage 'https://netbeans.org/'
   version '8.0'
   sha256 '55e58e5c01d38860919b8765a9ed5b048aef20d474c913305973bc03343b9d98'
+
+  url 'http://download.netbeans.org/netbeans/8.0/final/bundles/netbeans-8.0-cpp-macosx.dmg'
+  homepage 'https://netbeans.org/'
+
   install 'NetBeans 8.0.mpkg'
   uninstall :files => [
                        '/Applications/NetBeans'
