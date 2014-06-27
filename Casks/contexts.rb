e1a5ce10e215dd88b31ff49fda2c56d5f640cebe
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat contexts.rb according to readability conventions

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -1,8 +1,10 @@
 class Contexts < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://contextsformac.com/releases/Contexts.zip'
   appcast 'http://www.contextsformac.com/releases/appcast.xml'
   homepage 'http://contextsformac.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Contexts.app'
 end
