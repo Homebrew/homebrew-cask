518d881f4e2b5aaf695d19c5b494448317324878
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat r-name.rb according to readability conventions

--- Casks/r-name.rb
+++ Casks/r-name.rb
@@ -1,7 +1,9 @@
 class RName < Cask
-  url 'http://www.jacek-dom.net/software/R-Name/R-Name.app.zip'
-  homepage 'http://www.jacek-dom.net/software/R-Name/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.jacek-dom.net/software/R-Name/R-Name.app.zip'
+  homepage 'http://www.jacek-dom.net/software/R-Name/'
+
   link 'R-Name.app'
 end
