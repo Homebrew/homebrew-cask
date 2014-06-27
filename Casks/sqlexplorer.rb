84973d8dc33da65f5b3e04ea6b341da75315cdb6
ndr <ndr@qef.io>
2014-06-27 02:01:45 +0200

    Reformat sqlexplorer.rb according to readability conventions

--- Casks/sqlexplorer.rb
+++ Casks/sqlexplorer.rb
@@ -1,7 +1,9 @@
 class Sqlexplorer < Cask
-  url 'https://downloads.sourceforge.net/project/eclipsesql/SQL%20Explorer%20RCP%20%28exc%20JRE%29/3.6.1/sqlexplorer_rcp-3.6.1.macosx.cocoa.x86.tgz'
-  homepage 'http://eclipsesql.sourceforge.net/'
   version '3.6.1'
   sha256 '57b40bf7f06d8ea125126de9c370bbd0415f5d9c68ce5200765e146acac0b05e'
+
+  url 'https://downloads.sourceforge.net/project/eclipsesql/SQL%20Explorer%20RCP%20%28exc%20JRE%29/3.6.1/sqlexplorer_rcp-3.6.1.macosx.cocoa.x86.tgz'
+  homepage 'http://eclipsesql.sourceforge.net/'
+
   link 'SQLExplorer/sqlexplorer.app'
 end
