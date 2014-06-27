ab09108f6f3a6da234f7e04c7dca8f82653232bc
ndr <ndr@qef.io>
2014-06-27 02:01:27 +0200

    Reformat meerkat.rb according to readability conventions

--- Casks/meerkat.rb
+++ Casks/meerkat.rb
@@ -1,8 +1,10 @@
 class Meerkat < Cask
+  version '1.5.3'
+  sha256 'bf5a5e492463a7ec1c3e959a55227dd6fcec5bb902124f9bde819bf4f5933982'
+  
   url 'http://codesorcery.net/downloads/Meerkat_1.5.3.dmg'
   appcast 'http://codesorcery.net/appcasts/Meerkat.xml'
   homepage 'http://codesorcery.net/meerkat'
-  version '1.5.3'
-  sha256 'bf5a5e492463a7ec1c3e959a55227dd6fcec5bb902124f9bde819bf4f5933982'
+
   link 'Meerkat.app'
 end
