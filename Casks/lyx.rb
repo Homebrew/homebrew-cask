74574232a686f2cdec0cb96dd3985d1e1c4af343
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat lyx.rb according to readability conventions

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -1,7 +1,9 @@
 class Lyx < Cask
-  url 'ftp://ftp.lyx.org/pub/lyx/bin/2.1.0/LyX-2.1.0+qt4-cocoa.dmg'
-  homepage 'http://www.lyx.org'
   version '2.1.0'
   sha256 'b6a44dffb9692e450827c6e39b88b77480ec7c757c458cdbb9000cc751963ffc'
+
+  url 'ftp://ftp.lyx.org/pub/lyx/bin/2.1.0/LyX-2.1.0+qt4-cocoa.dmg'
+  homepage 'http://www.lyx.org'
+
   link 'LyX.app'
 end
