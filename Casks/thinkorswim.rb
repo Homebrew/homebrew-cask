a607e65d23295a05a905d35d146ebb9f7f209dbf
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat thinkorswim.rb according to readability conventions

--- Casks/thinkorswim.rb
+++ Casks/thinkorswim.rb
@@ -1,8 +1,10 @@
 class Thinkorswim < Cask
-  url 'https://mediaserver.thinkorswim.com/installer/InstFiles/thinkorswim_installer.dmg'
-  homepage 'http://mediaserver.thinkorswim.com/installer/install.html#macosx'
   version 'latest'
   sha256 :no_check
+
+  url 'https://mediaserver.thinkorswim.com/installer/InstFiles/thinkorswim_installer.dmg'
+  homepage 'http://mediaserver.thinkorswim.com/installer/install.html#macosx'
+
   caveats do
     manual_installer 'thinkorswim Installer.app'
   end
