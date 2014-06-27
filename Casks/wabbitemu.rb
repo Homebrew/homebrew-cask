e2d530a056ce09a6fb19b4429e71f0f5ed375d51
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat wabbitemu.rb according to readability conventions

--- Casks/wabbitemu.rb
+++ Casks/wabbitemu.rb
@@ -1,7 +1,9 @@
 class Wabbitemu < Cask
-  url 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wabbit&DownloadId=187234&FileTime=130242671530600000&Build=20907'
-  homepage 'http://wabbit.codeplex.com'
   version '2.1'
   sha256 '23fc164767da10389eec858d022967fb58fdca1debaa5b9e022f2e9280a4f4e8'
+
+  url 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wabbit&DownloadId=187234&FileTime=130242671530600000&Build=20907'
+  homepage 'http://wabbit.codeplex.com'
+
   link 'WabbitEmu.app'
 end
