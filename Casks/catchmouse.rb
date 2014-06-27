a2e6bb33853fa66a82e51332c47b051b21ba6623
ndr <ndr@qef.io>
2014-06-27 02:01:02 +0200

    Reformat catchmouse.rb according to readability conventions

--- Casks/catchmouse.rb
+++ Casks/catchmouse.rb
@@ -1,7 +1,9 @@
 class Catchmouse < Cask
-  url 'http://ftnew.com/files/CatchMouse.zip'
-  homepage 'http://ftnew.com/catchmouse.html'
   version 'latest'
   sha256 :no_check
+
+  url 'http://ftnew.com/files/CatchMouse.zip'
+  homepage 'http://ftnew.com/catchmouse.html'
+
   link 'CatchMouse.app'
 end
