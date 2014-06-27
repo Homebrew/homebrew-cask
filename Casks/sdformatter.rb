ca6afa4966c6fff1acab7a29d4548da0d725758c
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat sdformatter.rb according to readability conventions

--- Casks/sdformatter.rb
+++ Casks/sdformatter.rb
@@ -1,8 +1,10 @@
 class Sdformatter < Cask
-  url 'https://www.sdcard.org/downloads/formatter_4/eula_mac/SDFormatter_4.00B.pkg'
-  homepage 'https://www.sdcard.org'
   version '4.0'
   sha256 '5b88a851d375ef471523a0f8371edda144c4650d40cfc94902440fece77aa27e'
+
+  url 'https://www.sdcard.org/downloads/formatter_4/eula_mac/SDFormatter_4.00B.pkg'
+  homepage 'https://www.sdcard.org'
+
   install 'SDFormatter_4.00B.pkg'
   uninstall :pkgutil => 'jp.co.trendy.sdformatter.SDFormatter.pkg'
 end
