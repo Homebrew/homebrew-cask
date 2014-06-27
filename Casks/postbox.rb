508fe59d2759043b414afb3db445bf7287eded00
ndr <ndr@qef.io>
2014-06-27 02:01:36 +0200

    Reformat postbox.rb according to readability conventions

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,7 +1,9 @@
 class Postbox < Cask
-  url 'http://www.postbox-inc.com/php/download.php?a=3011m'
-  homepage 'http://www.postbox-inc.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.postbox-inc.com/php/download.php?a=3011m'
+  homepage 'http://www.postbox-inc.com/'
+
   link 'Postbox.app'
 end
