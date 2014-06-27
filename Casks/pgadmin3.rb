c25ab30c676e09c525ebf5554cfb8b4d5ee52a5d
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat pgadmin3.rb according to readability conventions

--- Casks/pgadmin3.rb
+++ Casks/pgadmin3.rb
@@ -1,8 +1,10 @@
 class Pgadmin3 < Cask
   # note: "3" is not a version number, but indicates a different vendor
-  url 'http://ftp.postgresql.org/pub/pgadmin3/release/v1.18.1/osx/pgadmin3-1.18.1.dmg'
-  homepage 'http://pgadmin.org'
   version '1.18.1'
   sha256 '6fe924168c7a8ee9224527fcafc3e612de60c011a27dc44b8293401ff346627d'
+
+  url 'http://ftp.postgresql.org/pub/pgadmin3/release/v1.18.1/osx/pgadmin3-1.18.1.dmg'
+  homepage 'http://pgadmin.org'
+
   link 'pgAdmin3.app'
 end
