28831af53c2288fe4b5a49b4b918728dc62736da
ndr <ndr@qef.io>
2014-06-27 02:01:28 +0200

    Reformat mindnode-pro.rb according to readability conventions

--- Casks/mindnode-pro.rb
+++ Casks/mindnode-pro.rb
@@ -1,8 +1,10 @@
 class MindnodePro < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.mindnode.com/download/MindNodePro.zip'
   appcast 'https://www.mindnode.com/softwareupdate/mindnodepro.xml'
   homepage 'https://mindnode.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'MindNode Pro.app'
 end
