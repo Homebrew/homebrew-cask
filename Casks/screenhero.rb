8d5afa70ac1fee3b1341dedaf3d57c14be839a87
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat screenhero.rb according to readability conventions

--- Casks/screenhero.rb
+++ Casks/screenhero.rb
@@ -1,8 +1,10 @@
 class Screenhero < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://dl.screenhero.com/update/screenhero/Screenhero.dmg'
   appcast 'http://dl.screenhero.com/update/screenhero/sparkle.xml'
   homepage 'http://screenhero.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'Screenhero.app'
 end
