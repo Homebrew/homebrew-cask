a783781868f5b1664d20559cf3696c8640ea7e7d
ndr <ndr@qef.io>
2014-06-27 02:01:16 +0200

    Reformat haroopad.rb according to readability conventions

--- Casks/haroopad.rb
+++ Casks/haroopad.rb
@@ -1,7 +1,9 @@
 class Haroopad < Cask
-  url 'https://dl.dropbox.com/s/yvjb90ywib551ex/haroopad-v0.12.0.dmg'
-  homepage 'http://pad.haroopress.com/'
   version '0.12.0'
   sha256 '05297bdd532f23534fc3dab475d4b3262c18604d8a219a5e9188c34a84ad1472'
+
+  url 'https://dl.dropbox.com/s/yvjb90ywib551ex/haroopad-v0.12.0.dmg'
+  homepage 'http://pad.haroopress.com/'
+
   link 'haroopad.app'
 end
