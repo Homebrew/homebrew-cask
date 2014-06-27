377b44b954a24b8faae5d10b258538e2baf09a48
ndr <ndr@qef.io>
2014-06-27 02:01:07 +0200

    Reformat diumoo.rb according to readability conventions

--- Casks/diumoo.rb
+++ Casks/diumoo.rb
@@ -1,7 +1,9 @@
 class Diumoo < Cask
-  url 'http://share.diumoo.net/static/diumoo.zip'
-  homepage 'http://diumoo.net/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://share.diumoo.net/static/diumoo.zip'
+  homepage 'http://diumoo.net/'
+
   link 'diumoo.app'
 end
