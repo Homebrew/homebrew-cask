73df1736e6c6870d18e0eed85ba196656b11a985
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat comictagger.rb according to readability conventions

--- Casks/comictagger.rb
+++ Casks/comictagger.rb
@@ -1,7 +1,9 @@
 class Comictagger < Cask
-  url 'https://comictagger.googlecode.com/files/ComicTagger-1.1.9-beta.dmg'
-  homepage 'http://code.google.com/p/comictagger/'
   version '1.1.9-beta'
   sha256 'd940b9fc5878f7d1e05e1b4e343ecf3e8ec5a5352180fc6c9792d58bb4469b89'
+
+  url 'https://comictagger.googlecode.com/files/ComicTagger-1.1.9-beta.dmg'
+  homepage 'http://code.google.com/p/comictagger/'
+
   link 'ComicTagger.app'
 end
