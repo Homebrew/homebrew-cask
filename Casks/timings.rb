4ddf16eb8f7c0ce2a7c988b0f681d6e969d14ee2
ndr <ndr@qef.io>
2014-06-27 02:01:49 +0200

    Reformat timings.rb according to readability conventions

--- Casks/timings.rb
+++ Casks/timings.rb
@@ -1,8 +1,10 @@
 class Timings < Cask
+  version '1.0.4'
+  sha256 'e7601405312bd6142514aef98154b2ebe8e06a3154c7afa7bcc03edf0090ddff'
+  
   url 'http://mediaatelier.com/Timings/Timings_1.0.4.zip'
   appcast 'http://mediaatelier.com/Timings/feed.php'
   homepage 'http://mediaatelier.com/Timings'
-  version '1.0.4'
-  sha256 'e7601405312bd6142514aef98154b2ebe8e06a3154c7afa7bcc03edf0090ddff'
+
   link 'Timings.app'
 end
