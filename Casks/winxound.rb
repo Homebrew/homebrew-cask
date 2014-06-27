21c9bb6fd2674fe5b2bd6bc7088cbe1f22ede93f
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat winxound.rb according to readability conventions

--- Casks/winxound.rb
+++ Casks/winxound.rb
@@ -1,7 +1,9 @@
 class Winxound < Cask
-  url 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=winxound&DownloadId=206628&FileTime=129937664917570000&Build=20907'
-  homepage 'https://winxound.codeplex.com/'
   version '3.4.0'
   sha256 'c3460eeeff3fec917fb91cad0aafe5df5779f226fc6296bfa6d0c757fbd6ad47'
+
+  url 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=winxound&DownloadId=206628&FileTime=129937664917570000&Build=20907'
+  homepage 'https://winxound.codeplex.com/'
+
   link 'WinXound.app'
 end
