127d532d1b5ac9088928affd3f6e0cde1830cf21
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat slate.rb according to readability conventions

--- Casks/slate.rb
+++ Casks/slate.rb
@@ -1,8 +1,10 @@
 class Slate < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://slate.ninjamonkeysoftware.com/Slate.dmg'
   appcast 'https://www.ninjamonkeysoftware.com/slate/appcast.xml'
   homepage 'https://github.com/jigish/slate'
-  version 'latest'
-  sha256 :no_check
+
   link 'Slate.app'
 end
