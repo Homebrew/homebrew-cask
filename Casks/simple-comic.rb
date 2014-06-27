93ce44c460281ec315c5766ace718f80ee74ef71
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat simple-comic.rb according to readability conventions

--- Casks/simple-comic.rb
+++ Casks/simple-comic.rb
@@ -1,7 +1,9 @@
 class SimpleComic < Cask
-  url 'http://dancingtortoisedownload.s3.amazonaws.com/SimpleComic_1.7_252.zip'
-  homepage 'http://dancingtortoise.com/simplecomic/'
   version '1.7.252'
   sha256 '06b05ca40dbd4833b6d30e9dbc8840237a476f7bc91970867e3de5c47f72a0b4'
+
+  url 'http://dancingtortoisedownload.s3.amazonaws.com/SimpleComic_1.7_252.zip'
+  homepage 'http://dancingtortoise.com/simplecomic/'
+
   link 'Simple Comic.app'
 end
