d0439615f79ec698e28b68b847b31e1ecc8af0cf
ndr <ndr@qef.io>
2014-06-27 02:01:20 +0200

    Reformat iphoney.rb according to readability conventions

--- Casks/iphoney.rb
+++ Casks/iphoney.rb
@@ -1,8 +1,10 @@
 class Iphoney < Cask
+  version '1.2'
+  sha256 'f5bffb663944eea228bb9acb315ac23e8f95b6562ae71d0cb031ecc046f72ff4'
+  
   url 'https://download.marketcircle.com/s3/iPhoney_1.2.zip'
   appcast 'http://www.marketcircle.com/iphoney/iPhoneyAppcast.xml'
   homepage 'https://www.marketcircle.com/iphoney'
-  version '1.2'
-  sha256 'f5bffb663944eea228bb9acb315ac23e8f95b6562ae71d0cb031ecc046f72ff4'
+
   link 'iPhoney.app'
 end
