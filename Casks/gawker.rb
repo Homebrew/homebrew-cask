dd63534da6a966bc045705e5c22a4d50fcb9cea3
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat gawker.rb according to readability conventions

--- Casks/gawker.rb
+++ Casks/gawker.rb
@@ -1,8 +1,10 @@
 class Gawker < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://sourceforge.net/projects/gawker/files/latest/download'
   appcast 'http://gawker.sourceforge.net/appcast.xml'
   homepage 'http://gawker.sourceforge.net/Gawker.html'
-  version 'latest'
-  sha256 :no_check
+
   link 'Gawker.app'
 end
