bee66b7bdb9a4ae4f7a342b72a85ab177088d52c
ndr <ndr@qef.io>
2014-06-27 02:00:55 +0200

    Reformat adventure.rb according to readability conventions

--- Casks/adventure.rb
+++ Casks/adventure.rb
@@ -1,8 +1,10 @@
 class Adventure < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.lobotomo.com/products/downloads/Adventure.dmg'
   appcast 'http://www.lobotomo.com/products/Adventure/profileInfo.php'
   homepage 'http://www.lobotomo.com/products/Adventure/index.html'
-  version 'latest'
-  sha256 :no_check
+
   link 'Adventure.app'
 end
