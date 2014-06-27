8c10b706396709f4bfd81a3b2e224e2c396223d6
ndr <ndr@qef.io>
2014-06-27 02:01:27 +0200

    Reformat media-converter.rb according to readability conventions

--- Casks/media-converter.rb
+++ Casks/media-converter.rb
@@ -1,7 +1,9 @@
 class MediaConverter < Cask
-  url 'https://downloads.sourceforge.net/project/media-converter/media-converter/1.2/media-converter-1.2.zip'
-  homepage 'http://media-converter.sourceforge.net/'
   version '1.2'
   sha256 '4893e6e5bc18b9cb0f0d8e1bcb861f92595314e4f9768ef638affdfc866f37e1'
+
+  url 'https://downloads.sourceforge.net/project/media-converter/media-converter/1.2/media-converter-1.2.zip'
+  homepage 'http://media-converter.sourceforge.net/'
+
   link 'Media Converter.localized/Media Converter.app'
 end
