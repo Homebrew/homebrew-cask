6e97862ae22e370d9de5ef9d77a0f4753c60bf94
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat omnigraffle.rb according to readability conventions

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -1,7 +1,9 @@
 class Omnigraffle < Cask
-  url 'https://www.omnigroup.com/download/latest/omnigraffle'
-  homepage 'http://www.omnigroup.com/products/omnigraffle'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.omnigroup.com/download/latest/omnigraffle'
+  homepage 'http://www.omnigroup.com/products/omnigraffle'
+
   link 'OmniGraffle.app'
 end
