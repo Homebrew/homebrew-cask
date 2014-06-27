3bb7fe10ebbf61e464f7eb4417ccb60d7808c076
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat retroshare.rb according to readability conventions

--- Casks/retroshare.rb
+++ Casks/retroshare.rb
@@ -1,7 +1,9 @@
 class Retroshare < Cask
-  url 'https://downloads.sourceforge.net/project/retroshare/RetroShare/0.5.5b/Retroshare-V0.5.5b-svn6877_OSX10.6u.dmg'
-  homepage 'http://retroshare.sourceforge.net/'
   version '0.5.5b'
   sha256 '24beb2e1087e7a93ba996030c0785cd9f3a575d3adc5dcfda0d58aab3b40aebe'
+
+  url 'https://downloads.sourceforge.net/project/retroshare/RetroShare/0.5.5b/Retroshare-V0.5.5b-svn6877_OSX10.6u.dmg'
+  homepage 'http://retroshare.sourceforge.net/'
+
   link 'Retroshare.app'
 end
