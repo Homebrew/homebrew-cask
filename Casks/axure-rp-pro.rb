c1ec7625025b87467a14ca8d1c65808fde041076
ndr <ndr@qef.io>
2014-06-27 02:00:58 +0200

    Reformat axure-rp-pro.rb according to readability conventions

--- Casks/axure-rp-pro.rb
+++ Casks/axure-rp-pro.rb
@@ -1,7 +1,9 @@
 class AxureRpPro < Cask
-  url 'https://axure.cachefly.net/AxureRP-Pro-Setup.dmg'
-  homepage 'http://www.axure.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://axure.cachefly.net/AxureRP-Pro-Setup.dmg'
+  homepage 'http://www.axure.com/'
+
   link 'Axure RP Pro 7.0.app'
 end
