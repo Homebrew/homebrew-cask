fa59435a2feef3f71d6e45c7537ab6b87f828fd7
ndr <ndr@qef.io>
2014-06-27 02:01:29 +0200

    Reformat mnemosyne.rb according to readability conventions

--- Casks/mnemosyne.rb
+++ Casks/mnemosyne.rb
@@ -1,7 +1,9 @@
 class Mnemosyne < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/mnemosyne-proj/Mnemosyne-2.3-Mac.dmg'
-  homepage 'http://mnemosyne-proj.org/'
   version '2.3'
   sha256 '094c4f6fb50de376a5190c3712b935089579717641ce90685aa48932bf0efa07'
+
+  url 'https://downloads.sourceforge.net/sourceforge/mnemosyne-proj/Mnemosyne-2.3-Mac.dmg'
+  homepage 'http://mnemosyne-proj.org/'
+
   link 'Mnemosyne.app'
 end
