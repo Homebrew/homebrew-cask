1c2560fa79783b744fed380c175ba1dfc0ed8641
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat preen.rb according to readability conventions

--- Casks/preen.rb
+++ Casks/preen.rb
@@ -1,8 +1,10 @@
 class Preen < Cask
+  version '1.61'
+  sha256 '04d10c5e06cd22704c8fb53ee2d9f67692bacdeb1cd90e44e08e3dff8afe15e3'
+  
   url 'http://anomiesoftware.com/downloads/preenImage1.61.dmg'
   appcast 'http://www.anomiesoftware.com/PreenAppcast.xml'
   homepage 'http://anomiesoftware.com/Anomie_Software/Preen.html'
-  version '1.61'
-  sha256 '04d10c5e06cd22704c8fb53ee2d9f67692bacdeb1cd90e44e08e3dff8afe15e3'
+
   link 'Preen.app'
 end
