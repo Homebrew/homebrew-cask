c92d8d5d94abaa73a7a09aa71272f38048641b40
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat wd-security.rb according to readability conventions

--- Casks/wd-security.rb
+++ Casks/wd-security.rb
@@ -1,7 +1,9 @@
 class WdSecurity < Cask
-  url 'http://download.wdc.com/wdapp/WD_Security_For_Web_Mac_1_0_8_2.zip'
-  homepage 'http://www.wdc.com/en/'
   version '1.0.8.2'
   sha256 '1a82e8cd9d8e7d23b8201c6460c75cd6c388f8c588b6608caf2c31394b3fd37a'
+
+  url 'http://download.wdc.com/wdapp/WD_Security_For_Web_Mac_1_0_8_2.zip'
+  homepage 'http://www.wdc.com/en/'
+
   link 'WD Security.app'
 end
