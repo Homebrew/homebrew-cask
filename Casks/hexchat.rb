40075f2067475e139146e25f0df13631811a9c7c
ndr <ndr@qef.io>
2014-06-27 02:01:17 +0200

    Reformat hexchat.rb according to readability conventions

--- Casks/hexchat.rb
+++ Casks/hexchat.rb
@@ -1,7 +1,9 @@
 class Hexchat < Cask
-  url 'http://dl.hexchat.net/hexchat/osx/HexChat-2.10.0.app.zip'
-  homepage 'http://hexchat.github.io'
   version '2.10.0'
   sha256 'b23f36027d81c6b1152e9ad32793e5466df33dafb5bce508d1391c6de82c156d'
+
+  url 'http://dl.hexchat.net/hexchat/osx/HexChat-2.10.0.app.zip'
+  homepage 'http://hexchat.github.io'
+
   link 'HexChat.app'
 end
