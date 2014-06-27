a39053ec4784be76cf3dcaa122fda02ea1ec5594
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat suspicious-package.rb according to readability conventions

--- Casks/suspicious-package.rb
+++ Casks/suspicious-package.rb
@@ -1,7 +1,9 @@
 class SuspiciousPackage < Cask
-  url 'http://www.mothersruin.com/software/downloads/SuspiciousPackage.dmg'
-  homepage 'http://www.mothersruin.com/software/SuspiciousPackage/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.mothersruin.com/software/downloads/SuspiciousPackage.dmg'
+  homepage 'http://www.mothersruin.com/software/SuspiciousPackage/'
+
   qlplugin 'Suspicious Package.qlgenerator'
 end
