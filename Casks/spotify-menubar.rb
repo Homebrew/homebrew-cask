28f55a4560456f8b50dc53286f258e4c3e6edf67
ndr <ndr@qef.io>
2014-06-27 02:01:45 +0200

    Reformat spotify-menubar.rb according to readability conventions

--- Casks/spotify-menubar.rb
+++ Casks/spotify-menubar.rb
@@ -1,8 +1,10 @@
 class SpotifyMenubar < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://lifeupnorth.co.uk/files/Spotify_Menubar.zip'
   appcast 'http://lifeupnorth.co.uk/lun/sparkle/sm.xml'
   homepage 'http://lifeupnorth.co.uk/Spotify-Menubar/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Spotify Menubar.app'
 end
