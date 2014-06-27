f974733db33470f0378f234c49edf5cae1b17ffd
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat icolors.rb according to readability conventions

--- Casks/icolors.rb
+++ Casks/icolors.rb
@@ -1,7 +1,9 @@
 class Icolors < Cask
-  url 'https://www.fadingred.com/files/icolors/icolors_3.0.zip'
-  homepage 'http://www.fadingred.com/icolors/'
   version '3.0'
   sha256 '92fab479c7916ee7c52efcc457d8d120acc555a05825b01ebca047f017c097b9'
+
+  url 'https://www.fadingred.com/files/icolors/icolors_3.0.zip'
+  homepage 'http://www.fadingred.com/icolors/'
+
   link 'iColors.app'
 end
