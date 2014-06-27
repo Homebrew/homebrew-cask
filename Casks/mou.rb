c46f9225c8082a5206a8eb5fc679f85960402e9d
ndr <ndr@qef.io>
2014-06-27 02:01:29 +0200

    Reformat mou.rb according to readability conventions

--- Casks/mou.rb
+++ Casks/mou.rb
@@ -1,8 +1,10 @@
 class Mou < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://mouapp.com/download/Mou.zip'
   appcast 'http://mouapp.com/up/updates.xml'
   homepage 'http://mouapp.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Mou.app'
 end
