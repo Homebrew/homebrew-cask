8dcb9e80c2cded42d145311e06605377c842783a
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat piezo.rb according to readability conventions

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -1,7 +1,9 @@
 class Piezo < Cask
-  url 'http://neutral.rogueamoeba.com/mirror/files/Piezo.zip'
-  homepage 'http://rogueamoeba.com/piezo/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://neutral.rogueamoeba.com/mirror/files/Piezo.zip'
+  homepage 'http://rogueamoeba.com/piezo/'
+
   link 'Piezo.app'
 end
