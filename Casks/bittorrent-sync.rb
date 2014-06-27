a2e3299995c25d05849eae19728018f6a2f97d1b
ndr <ndr@qef.io>
2014-06-27 02:01:00 +0200

    Reformat bittorrent-sync.rb according to readability conventions

--- Casks/bittorrent-sync.rb
+++ Casks/bittorrent-sync.rb
@@ -1,9 +1,11 @@
 class BittorrentSync < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://download-lb.utorrent.com/endpoint/btsync/os/osx/track/stable'
   # todo: response was not XML
   # appcast 'http://www.usyncapp.com/cfu.php'
   homepage 'http://www.bittorrent.com/sync'
-  version 'latest'
-  sha256 :no_check
+
   link 'BitTorrent Sync.app'
 end
