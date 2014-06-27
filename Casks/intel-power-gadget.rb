15473d0a27242ebb7936186fd6e747d4bb688238
ndr <ndr@qef.io>
2014-06-27 02:01:19 +0200

    Reformat intel-power-gadget.rb according to readability conventions

--- Casks/intel-power-gadget.rb
+++ Casks/intel-power-gadget.rb
@@ -1,9 +1,11 @@
 # encoding: UTF-8
 class IntelPowerGadget < Cask
-  url 'https://software.intel.com/sites/default/files/IntelPowerGadget3.0.1.zip'
-  homepage 'http://software.intel.com/en-us/articles/intel-power-gadget-20'
   version '3.0.1'
   sha256 '538a792721604e2155b3a48caa4084db751a91b170e5fa62bf0331d3147f2239'
+
+  url 'https://software.intel.com/sites/default/files/IntelPowerGadget3.0.1.zip'
+  homepage 'http://software.intel.com/en-us/articles/intel-power-gadget-20'
+
   # this bogus-looking character accurately reflects an upstream error
   nested_container 'IntelÆ Power Gadget.dmg'
   install 'Install Intel Power Gadget.pkg'
