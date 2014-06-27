2437c488e9cd6507a7a37abf907a930a6e3100e3
ndr <ndr@qef.io>
2014-06-27 02:01:17 +0200

    Reformat honer.rb according to readability conventions

--- Casks/honer.rb
+++ Casks/honer.rb
@@ -1,7 +1,9 @@
 class Honer < Cask
-  url 'https://github.com/puffnfresh/Honer.app/releases/download/v1.0/Honer-6e3863f2.zip'
-  homepage 'https://github.com/puffnfresh/Honer.app'
   version '1.0'
   sha256 'bae10bea6875dbc5c948fb2e86b168c15ee3738d485bd7ab2a9758e64812c531'
+
+  url 'https://github.com/puffnfresh/Honer.app/releases/download/v1.0/Honer-6e3863f2.zip'
+  homepage 'https://github.com/puffnfresh/Honer.app'
+
   link 'Honer.app'
 end
