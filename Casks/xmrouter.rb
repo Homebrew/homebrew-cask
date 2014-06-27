003fed90ddfeb9988aec9b0782f13fa97e8e841b
ndr <ndr@qef.io>
2014-06-27 02:01:54 +0200

    Reformat xmrouter.rb according to readability conventions

--- Casks/xmrouter.rb
+++ Casks/xmrouter.rb
@@ -1,7 +1,9 @@
 class Xmrouter < Cask
-  url 'http://bigota.miwifi.com/xiaoqiang/client/xqmac_client.dmg'
-  homepage 'http://www1.miwifi.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://bigota.miwifi.com/xiaoqiang/client/xqmac_client.dmg'
+  homepage 'http://www1.miwifi.com/'
+
   link 'XMRouter.app'
 end
