e43b7792cc78d5258b7e96552672200b7dcf35b8
ndr <ndr@qef.io>
2014-06-27 02:01:09 +0200

    Reformat electrum.rb according to readability conventions

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,7 +1,9 @@
 class Electrum < Cask
-  url 'https://download.electrum.org/electrum-1.9.8.dmg'
-  homepage 'http://electrum.org/'
   version '1.9.8'
   sha256 'ad3818e34a9b3a292257377e337603ed362ff928f2054e92f764d771f0e49ec0'
+
+  url 'https://download.electrum.org/electrum-1.9.8.dmg'
+  homepage 'http://electrum.org/'
+
   link 'Electrum.app'
 end
