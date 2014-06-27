bcecfc6ea2de17bd249fced531972030fea9c126
ndr <ndr@qef.io>
2014-06-27 02:01:29 +0200

    Reformat mixture.rb according to readability conventions

--- Casks/mixture.rb
+++ Casks/mixture.rb
@@ -1,8 +1,10 @@
 class Mixture < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://s3.amazonaws.com/mixture-mixed-app/mac/Mixture.zip'
   appcast 'https://s3.amazonaws.com/mixture-mixed-app/mac/appcast.xml'
   homepage 'http://mixture.io/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Mixture.app'
 end
