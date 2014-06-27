42c695f1915f994af2b8d3fb1c76bf488022f837
ndr <ndr@qef.io>
2014-06-27 02:01:07 +0200

    Reformat dfontsplitter.rb according to readability conventions

--- Casks/dfontsplitter.rb
+++ Casks/dfontsplitter.rb
@@ -1,8 +1,10 @@
 class Dfontsplitter < Cask
+  version '0.4.1'
+  sha256 'd9345ffe0e9be0c096e65114be21fc3aac676a9a3a53e84f1d6cebff0b7796b3'
+  
   url 'http://peter.upfold.org.uk/files/dfontsplitter/dfontsplitter-0.4.1-mac.zip'
   appcast 'http://apps.upfold.org.uk/appupdate/dfontsplitter.xml'
   homepage 'http://peter.upfold.org.uk/projects/dfontsplitter'
-  version '0.4.1'
-  sha256 'd9345ffe0e9be0c096e65114be21fc3aac676a9a3a53e84f1d6cebff0b7796b3'
+
   link 'DfontSplitter.app'
 end
