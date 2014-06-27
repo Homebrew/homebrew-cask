f4e56092cbd37f8708558d1eec2ef8228346d299
ndr <ndr@qef.io>
2014-06-27 02:01:22 +0200

    Reformat kid3.rb according to readability conventions

--- Casks/kid3.rb
+++ Casks/kid3.rb
@@ -1,8 +1,10 @@
 class Kid3 < Cask
   # note: "3" is not a version number, but an intrinsic part of the product name (ID3 tags)
-  url 'https://downloads.sourceforge.net/sourceforge/kid3/kid3-3.0.2-Darwin.dmg'
-  homepage 'http://kid3.sourceforge.net/'
   version '3.0.2'
   sha256 'a1b60a4eea11ca62001365dd28d418c595ce2d073dd4e0ba07bd28a94cb5c260'
+
+  url 'https://downloads.sourceforge.net/sourceforge/kid3/kid3-3.0.2-Darwin.dmg'
+  homepage 'http://kid3.sourceforge.net/'
+
   link 'Kid3.app'
 end
