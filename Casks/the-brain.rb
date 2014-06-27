a5e6c7c80d64a61d8c6d32a9f22cdd5aa7c60e68
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat the-brain.rb according to readability conventions

--- Casks/the-brain.rb
+++ Casks/the-brain.rb
@@ -1,7 +1,9 @@
 class TheBrain < Cask
-  url 'http://assets.thebrain.com/downloads/TheBrain_macos_8_0_0_7-a.dmg'
-  homepage 'http://www.thebrain.com/'
   version '8.0.0.7'
   sha256 'a3a28e810961652e7b436f81cff4510e0a7c2beeca7936892f9194d11f957493'
+
+  url 'http://assets.thebrain.com/downloads/TheBrain_macos_8_0_0_7-a.dmg'
+  homepage 'http://www.thebrain.com/'
+
   link 'TheBrain.app'
 end
