dcb49c96217d181e9bb1795f6372383f1320a169
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat pandoc.rb according to readability conventions

--- Casks/pandoc.rb
+++ Casks/pandoc.rb
@@ -1,8 +1,10 @@
 class Pandoc < Cask
-  url 'https://github.com/jgm/pandoc/releases/download/1.12.4/pandoc-1.12.4-osx.pkg.zip'
-  homepage 'http://johnmacfarlane.net/pandoc'
   version '1.12.4'
   sha256 '86324520853bc6e2ea0d36bf4d3bb4954ff494e99c04b29a5b66803e9cc50c89'
+
+  url 'https://github.com/jgm/pandoc/releases/download/1.12.4/pandoc-1.12.4-osx.pkg.zip'
+  homepage 'http://johnmacfarlane.net/pandoc'
+
   install 'pandoc-1.12.4.pkg'
   uninstall :pkgutil => 'net.johnmacfarlane.pandoc'
   caveats do
