777960edd0cfde75478accee1d4e474aaf2a6d92
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat quicksilver.rb according to readability conventions

--- Casks/quicksilver.rb
+++ Casks/quicksilver.rb
@@ -1,7 +1,9 @@
 class Quicksilver < Cask
-  url 'http://cdn.qsapp.com/plugins/files/com.blacktree.Quicksilver__16391.dmg'
-  homepage 'http://qsapp.com/'
   version '1.1.3'
   sha256 '7c2ebec0d478d711859991991a65e4deced9f9521ac555c07b6be42999504535'
+
+  url 'http://cdn.qsapp.com/plugins/files/com.blacktree.Quicksilver__16391.dmg'
+  homepage 'http://qsapp.com/'
+
   link 'Quicksilver.app'
 end
