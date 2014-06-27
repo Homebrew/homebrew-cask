00ed4270944e2cdbd5d4210748f489f2441910ca
ndr <ndr@qef.io>
2014-06-27 02:01:54 +0200

    Reformat xld.rb according to readability conventions

--- Casks/xld.rb
+++ Casks/xld.rb
@@ -1,8 +1,10 @@
 class Xld < Cask
+  version '20140504'
+  sha256 '48183d866d9b62f5a71d885d6391040b76a736081ce52c7247e0ba6208e82918'
+  
   url 'https://downloads.sourceforge.net/project/xld/xld-20140504.dmg'
   appcast 'http://xld.googlecode.com/svn/appcast/xld-appcast_e.xml'
   homepage 'http://tmkk.undo.jp/xld/index_e.html'
-  version '20140504'
-  sha256 '48183d866d9b62f5a71d885d6391040b76a736081ce52c7247e0ba6208e82918'
+
   link 'XLD.app'
 end
