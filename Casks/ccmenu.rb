e2e54523454bc0f14d9b86a488a63e6b6c29b6a7
ndr <ndr@qef.io>
2014-06-27 02:01:02 +0200

    Reformat ccmenu.rb according to readability conventions

--- Casks/ccmenu.rb
+++ Casks/ccmenu.rb
@@ -1,8 +1,10 @@
 class Ccmenu < Cask
+  version '1.7'
+  sha256 '974a2022dbc9494958334ee8f02e08df7ed184e1f421a53d623dfbeaadf08a2c'
+  
   url 'https://downloads.sourceforge.net/project/ccmenu/CCMenu/1.7/ccmenu-1.7-b.dmg'
   appcast 'http://ccmenu.sourceforge.net/update-stable.xml'
   homepage 'http://ccmenu.sourceforge.net/'
-  version '1.7'
-  sha256 '974a2022dbc9494958334ee8f02e08df7ed184e1f421a53d623dfbeaadf08a2c'
+
   link 'CCMenu.app'
 end
