078d5295280420d89674926725ca80f4c197f9a7
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat nothing-to-hide.rb according to readability conventions

--- Casks/nothing-to-hide.rb
+++ Casks/nothing-to-hide.rb
@@ -1,7 +1,9 @@
 class NothingToHide < Cask
-  url 'https://nothingtohide.s3.amazonaws.com/pc/Nothing_To_Hide_MAC.zip'
-  homepage 'https://back.nothingtohide.cc/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://nothingtohide.s3.amazonaws.com/pc/Nothing_To_Hide_MAC.zip'
+  homepage 'https://back.nothingtohide.cc/'
+
   link 'Nothing To Hide.app'
 end
