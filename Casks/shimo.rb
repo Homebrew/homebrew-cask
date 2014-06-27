28e8b9123f5476c8a1364c2a712f513163ff4dee
ndr <ndr@qef.io>
2014-06-27 02:01:42 +0200

    Reformat shimo.rb according to readability conventions

--- Casks/shimo.rb
+++ Casks/shimo.rb
@@ -1,8 +1,10 @@
 class Shimo < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.chungwasoft.com/files/Shimo_latest.zip'
   appcast 'http://www.chungwasoft.com/library/appcasts/Shimo3/shimocast.php'
   homepage 'http://www.chungwasoft.com/shimo/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Shimo.app'
 end
