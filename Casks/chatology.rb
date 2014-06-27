04b787f9f0d3942e6a26c27c345122390ad5f6e6
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat chatology.rb according to readability conventions

--- Casks/chatology.rb
+++ Casks/chatology.rb
@@ -1,8 +1,10 @@
 class Chatology < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://flexibits.com/chatology/download'
   appcast 'https://flexibits.com/chatology/appcast.php'
   homepage 'http://flexibits.com/chatology'
-  version 'latest'
-  sha256 :no_check
+
   link 'Chatology.app'
 end
