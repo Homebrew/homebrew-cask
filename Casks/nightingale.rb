faf35f244750547fa469fc9f77d1264cf9894d4f
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat nightingale.rb according to readability conventions

--- Casks/nightingale.rb
+++ Casks/nightingale.rb
@@ -1,7 +1,9 @@
 class Nightingale < Cask
-  url 'https://downloads.sourceforge.net/ngale/Nightingale_1.12-2432_macosx-i686.dmg'
-  homepage 'http://getnightingale.com/'
   version '1.12-2432'
   sha256 '854b02a22f2846284618dc8d3a64a766e8e7a34e65cf35934f6b357f4bc1000e'
+
+  url 'https://downloads.sourceforge.net/ngale/Nightingale_1.12-2432_macosx-i686.dmg'
+  homepage 'http://getnightingale.com/'
+
   link 'Nightingale.app'
 end
