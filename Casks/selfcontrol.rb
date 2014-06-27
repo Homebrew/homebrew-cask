234d2309ef49c8ca052a0d26658697fd50647eff
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat selfcontrol.rb according to readability conventions

--- Casks/selfcontrol.rb
+++ Casks/selfcontrol.rb
@@ -1,8 +1,10 @@
 class Selfcontrol < Cask
+  version '1.5.1'
+  sha256 'd3823a1e9ba0b47dc2cb39c93cd51837c2dafc7d5a5a564825f4a440fd2ab9ad'
+  
   url 'http://downloads.selfcontrolapp.com/SelfControl-1.5.1.zip'
   appcast 'http://selfcontrolapp.com/SelfControlAppcast.xml'
   homepage 'http://selfcontrolapp.com/'
-  version '1.5.1'
-  sha256 'd3823a1e9ba0b47dc2cb39c93cd51837c2dafc7d5a5a564825f4a440fd2ab9ad'
+
   link 'SelfControl.app'
 end
