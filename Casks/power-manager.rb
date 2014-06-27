07e46f8c0bf99251908b044e1570686a65e12111
ndr <ndr@qef.io>
2014-06-27 02:01:36 +0200

    Reformat power-manager.rb according to readability conventions

--- Casks/power-manager.rb
+++ Casks/power-manager.rb
@@ -1,8 +1,10 @@
 class PowerManager < Cask
-  url 'https://www.dssw.co.uk/powermanager/dsswpowermanager.dmg'
-  homepage 'https://www.dssw.co.uk/powermanager'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.dssw.co.uk/powermanager/dsswpowermanager.dmg'
+  homepage 'https://www.dssw.co.uk/powermanager'
+
   install 'DssW Power Manager.pkg'
   uninstall :script => '/Library/Application Support/Power Manager/Remove Power Manager.app/Contents/Resources/Remove Power Manager.sh'
 end
