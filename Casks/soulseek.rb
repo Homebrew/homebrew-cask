4b7a8682ce58bbff51d8325bde2d9a61311d1e67
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat soulseek.rb according to readability conventions

--- Casks/soulseek.rb
+++ Casks/soulseek.rb
@@ -1,8 +1,10 @@
 class Soulseek < Cask
-  url 'http://www.soulseekqt.net/SoulseekQt/Mac/SoulseekQt-2014-4-11.pkg'
-  homepage 'http://www.soulseekqt.net/'
   version '2014-4-11'
   sha256 'cfd1ece49b44beb0f8a8e4725152b93a36d641a4ae350ea5a7e7de4d6be79b46'
+
+  url 'http://www.soulseekqt.net/SoulseekQt/Mac/SoulseekQt-2014-4-11.pkg'
+  homepage 'http://www.soulseekqt.net/'
+
   install 'SoulseekQt-2014-4-11.pkg'
   uninstall :pkgutil => 'com.soulseek.SoulseekQt.pkg'
 end
