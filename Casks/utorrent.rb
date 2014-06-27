849231691fe4a8ce0b3ed55c4c0c39e482de5f2b
ndr <ndr@qef.io>
2014-06-27 02:01:51 +0200

    Reformat utorrent.rb according to readability conventions

--- Casks/utorrent.rb
+++ Casks/utorrent.rb
@@ -1,7 +1,9 @@
 class Utorrent < Cask
+  
   url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/stable/'
   appcast 'http://update.utorrent.com/checkupdate.php'
   homepage 'http://www.utorrent.com/'
+
   version 'latest'
   caskroom_only true
   sha256 :no_check
