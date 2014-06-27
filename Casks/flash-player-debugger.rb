10c1614256241bdc71e7d33d7d1afd5414d7b9b6
ndr <ndr@qef.io>
2014-06-27 02:01:11 +0200

    Reformat flash-player-debugger.rb according to readability conventions

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,7 +1,9 @@
 class FlashPlayerDebugger < Cask
-  url 'https://fpdownload.macromedia.com/pub/flashplayer/updaters/14/flashplayer_14_sa_debug.dmg'
-  homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
   version '14.0.0.125'
   sha256 'c10647ecb3c77d2f70437b5146add5785336b3e77f9b0e9ade53f7404f711d90'
+
+  url 'https://fpdownload.macromedia.com/pub/flashplayer/updaters/14/flashplayer_14_sa_debug.dmg'
+  homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
+
   link 'Flash Player.app', :target => 'Flash Player Debugger.app'
 end
