d28960bba273c1386355319633c92f37e9b413da
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat gas-mask.rb according to readability conventions

--- Casks/gas-mask.rb
+++ Casks/gas-mask.rb
@@ -1,8 +1,10 @@
 class GasMask < Cask
+  version '0.8'
+  sha256 '16353247ea14da7675578fda340005645c4610a478a748c1ed410296c3dab52a'
+  
   url 'http://gmask.clockwise.ee/files/gas_mask_0.8.zip'
   appcast 'http://gmask.clockwise.ee/check_update/'
   homepage 'http://www.clockwise.ee/gasmask/'
-  version '0.8'
-  sha256 '16353247ea14da7675578fda340005645c4610a478a748c1ed410296c3dab52a'
+
   link 'Gas Mask.app'
 end
