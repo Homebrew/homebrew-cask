915d27941e2fae314ce2701a808d8072681de5e4
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat subler.rb according to readability conventions

--- Casks/subler.rb
+++ Casks/subler.rb
@@ -1,8 +1,10 @@
 class Subler < Cask
+  version '0.25'
+  sha256 '7b93d8a6afc1db00ea86b3bb2f6f8927012e66f1d74d7bfc7293d87cc8565f86'
+  
   url 'https://subler.googlecode.com/files/Subler-0.25.zip'
   appcast 'http://subler.googlecode.com/svn/doc/appcast.xml'
   homepage 'https://code.google.com/p/subler/'
-  version '0.25'
-  sha256 '7b93d8a6afc1db00ea86b3bb2f6f8927012e66f1d74d7bfc7293d87cc8565f86'
+
   link 'Subler.app'
 end
