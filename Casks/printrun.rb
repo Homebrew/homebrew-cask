135bbca1bb4e9b66c324c23c10856086af455ffb
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat printrun.rb according to readability conventions

--- Casks/printrun.rb
+++ Casks/printrun.rb
@@ -1,7 +1,9 @@
 class Printrun < Cask
-  url 'http://koti.kapsi.fi/~kliment/printrun/Printrun-Mac-10Mar2014.zip'
-  homepage 'https://github.com/kliment/Printrun'
   version '1.2'
   sha256 'b270edb951cbee3e957eab3ecd3bbd4fe25e93ce478393c61f029bfff4e3b902'
+
+  url 'http://koti.kapsi.fi/~kliment/printrun/Printrun-Mac-10Mar2014.zip'
+  homepage 'https://github.com/kliment/Printrun'
+
   link 'Printrun-Mac-10Mar2014.app'
 end
