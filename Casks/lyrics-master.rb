3569fa5c56b18406f582e1a95c8ac9453e3fd3d1
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat lyrics-master.rb according to readability conventions

--- Casks/lyrics-master.rb
+++ Casks/lyrics-master.rb
@@ -1,7 +1,9 @@
 class LyricsMaster < Cask
-  url 'http://www.kenichimaehashi.com/lyricsmaster/download/LyricsMaster2410.dmg'
-  homepage 'http://www.kenichimaehashi.com/lyricsmaster/'
   version '2.4.1'
   sha256 '773cb3996b5f946b506214a0087afebdb7f7350ac17f477cc3f31bedf2a9be0c'
+
+  url 'http://www.kenichimaehashi.com/lyricsmaster/download/LyricsMaster2410.dmg'
+  homepage 'http://www.kenichimaehashi.com/lyricsmaster/'
+
   link 'Lyrics Master'
 end
