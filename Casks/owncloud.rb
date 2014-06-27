42058b596f0d0cbe43ec2d1af3ad390921b1abf1
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat owncloud.rb according to readability conventions

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,7 +1,9 @@
 class Owncloud < Cask
-  url 'https://download.owncloud.com/desktop/stable/ownCloud-1.5.4.2580.dmg'
-  homepage 'http://owncloud.com'
   version '1.5.4.2580'
   sha256 '2dd1b7cb5263a7cb5d5363290ec02d76305f6599941c1cd2f35399b319972503'
+
+  url 'https://download.owncloud.com/desktop/stable/ownCloud-1.5.4.2580.dmg'
+  homepage 'http://owncloud.com'
+
   link 'owncloud.app'
 end
