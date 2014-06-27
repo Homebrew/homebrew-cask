05323b23a9504cc34e6f3e3b04b64055c451a9b0
ndr <ndr@qef.io>
2014-06-27 02:01:38 +0200

    Reformat qq.rb according to readability conventions

--- Casks/qq.rb
+++ Casks/qq.rb
@@ -1,7 +1,9 @@
 class Qq < Cask
-  url 'http://dldir1.qq.com/qqfile/QQforMac/QQ_V3.1.2.dmg'
-  homepage 'http://im.qq.com/macqq/index.shtml'
   version '3.1.2'
   sha256 'e74ec441a95d36e22e5e78a7ecc5a3d0d02cf749b413f88198ff74bc3bb85da0'
+
+  url 'http://dldir1.qq.com/qqfile/QQforMac/QQ_V3.1.2.dmg'
+  homepage 'http://im.qq.com/macqq/index.shtml'
+
   link 'QQ.app'
 end
