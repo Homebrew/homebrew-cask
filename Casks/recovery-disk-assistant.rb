db382d49542fc6188aa96d022540decec777bdec
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat recovery-disk-assistant.rb according to readability conventions

--- Casks/recovery-disk-assistant.rb
+++ Casks/recovery-disk-assistant.rb
@@ -1,7 +1,9 @@
 class RecoveryDiskAssistant < Cask
-  url 'https://support.apple.com/downloads/DL1433/en_US/RecoveryDiskAssistant.dmg'
-  homepage 'http://support.apple.com/kb/HT4848'
   version '1.0'
   sha256 '4991086d733ceff060b1e2aedf7788c17be407db5f5a6bf6dde0c23f10db26c2'
+
+  url 'https://support.apple.com/downloads/DL1433/en_US/RecoveryDiskAssistant.dmg'
+  homepage 'http://support.apple.com/kb/HT4848'
+
   link 'Recovery Disk Assistant.app'
 end
