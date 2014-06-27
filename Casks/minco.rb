9ef94c750512fddd28b51465e71bb4f38726464d
ndr <ndr@qef.io>
2014-06-27 02:01:28 +0200

    Reformat minco.rb according to readability conventions

--- Casks/minco.rb
+++ Casks/minco.rb
@@ -1,8 +1,10 @@
 class Minco < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.celmaro.com/files/minco/Minco.zip'
   appcast 'https://ssl.webpack.de/celmaro.com/updates/minco/minco.xml'
   homepage 'http://www.celmaro.com/minco/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Minco.app'
 end
