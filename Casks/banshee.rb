f7e0fcb3c2fa4e4c95d64b94a7ef175ecbc139d4
ndr <ndr@qef.io>
2014-06-27 02:00:59 +0200

    Reformat banshee.rb according to readability conventions

--- Casks/banshee.rb
+++ Casks/banshee.rb
@@ -1,7 +1,9 @@
 class Banshee < Cask
-  url 'http://ftp.gnome.org/pub/GNOME/binaries/mac/banshee/banshee-2.6.1.macosx.intel.dmg'
-  homepage 'http://banshee.fm'
   version '2.6.1'
   sha256 '26fc3c77a72860fc2dfaee0741c88e9b8d7b8dac6ece2943e42917112e55c933'
+
+  url 'http://ftp.gnome.org/pub/GNOME/binaries/mac/banshee/banshee-2.6.1.macosx.intel.dmg'
+  homepage 'http://banshee.fm'
+
   link 'Banshee.app'
 end
