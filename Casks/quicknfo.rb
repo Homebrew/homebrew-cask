7d36a5ba4a8fee9da5c0e8fb25e47fce47d91854
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat quicknfo.rb according to readability conventions

--- Casks/quicknfo.rb
+++ Casks/quicknfo.rb
@@ -1,7 +1,9 @@
 class Quicknfo < Cask
-  url 'https://github.com/The-Master777/QuickNFO/releases/download/v1.2/QuickNFO.qlgenerator.zip'
-  homepage 'https://github.com/planbnet/QuickNFO'
   version '1.2'
   sha256 'ee5c03f78ff60e69e776bec39896ac48496915de35fcb3e2bd0d9c20ad92b5bb'
+
+  url 'https://github.com/The-Master777/QuickNFO/releases/download/v1.2/QuickNFO.qlgenerator.zip'
+  homepage 'https://github.com/planbnet/QuickNFO'
+
   qlplugin 'QuickNFO.qlgenerator'
 end
