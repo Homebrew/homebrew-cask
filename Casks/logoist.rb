c64879d949f421098e3e80b02616357c93e43acf
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat logoist.rb according to readability conventions

--- Casks/logoist.rb
+++ Casks/logoist.rb
@@ -1,7 +1,9 @@
 class Logoist < Cask
-  url 'http://www.syniumsoftware.com/download/logoist.dmg'
-  homepage 'http://www.syniumsoftware.com/logoist/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.syniumsoftware.com/download/logoist.dmg'
+  homepage 'http://www.syniumsoftware.com/logoist/'
+
   link 'Logoist.app'
 end
