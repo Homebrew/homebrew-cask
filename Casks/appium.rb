4aa23da6296ffbaa2d6a9ced783829d2a3708edc
ndr <ndr@qef.io>
2014-06-27 02:00:57 +0200

    Reformat appium.rb according to readability conventions

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -1,7 +1,9 @@
 class Appium < Cask
-  url 'https://bitbucket.org/appium/appium.app/downloads/appium-1.0.0.dmg'
-  homepage 'http://appium.io'
   version '1.0.0'
   sha256 'c54a7af32870ec7046d9978ecd3545d8e10496a1599211debeb723b8deeeea68'
+
+  url 'https://bitbucket.org/appium/appium.app/downloads/appium-1.0.0.dmg'
+  homepage 'http://appium.io'
+
   link 'Appium.app'
 end
