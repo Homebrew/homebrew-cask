b46ad6d3213f903c50a0c66517d4faddbc2ca968
ndr <ndr@qef.io>
2014-06-27 02:01:42 +0200

    Reformat sidplay.rb according to readability conventions

--- Casks/sidplay.rb
+++ Casks/sidplay.rb
@@ -1,8 +1,10 @@
 class Sidplay < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.twinbirds.com/sidplay/SIDPLAY4.zip'
   appcast 'http://www.sidmusic.org/sidplay/mac/sidplay_appcast.xml'
   homepage 'http://www.sidmusic.org/sidplay/mac/'
-  version 'latest'
-  sha256 :no_check
+
   link 'SIDPLAY.app'
 end
