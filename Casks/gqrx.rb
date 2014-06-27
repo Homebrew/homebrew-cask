2b0ea2619b32f7d108bef25b4a76287f89fad876
ndr <ndr@qef.io>
2014-06-27 02:01:16 +0200

    Reformat gqrx.rb according to readability conventions

--- Casks/gqrx.rb
+++ Casks/gqrx.rb
@@ -1,7 +1,9 @@
 class Gqrx < Cask
-  url 'https://downloads.sourceforge.net/project/gqrx/2.2.0/gqrx-2.2.0.dmg'
-  homepage 'http://gqrx.dk/'
   version '2.2.0'
   sha256 'b4d81d542ea095bb882d465461d33f75ace2fca8806f8074464e9a586314ed07'
+
+  url 'https://downloads.sourceforge.net/project/gqrx/2.2.0/gqrx-2.2.0.dmg'
+  homepage 'http://gqrx.dk/'
+
   link 'Gqrx.app'
 end
