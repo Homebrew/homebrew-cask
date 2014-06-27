af9da51d73c3f00cb809110e2ca697bf48611d59
ndr <ndr@qef.io>
2014-06-27 02:00:55 +0200

    Reformat activity-audit.rb according to readability conventions

--- Casks/activity-audit.rb
+++ Casks/activity-audit.rb
@@ -1,8 +1,10 @@
 class ActivityAudit < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.dssw.co.uk/activityaudit/dsswactivityaudit.dmg'
   appcast 'http://version.dssw.co.uk/activityaudit/standard'
   homepage 'https://www.dssw.co.uk/activityaudit'
-  version 'latest'
-  sha256 :no_check
+
   link 'Activity Audit.app'
 end
