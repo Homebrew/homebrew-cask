4d0b835c30832ed0c6b90ff73ba637c0251a4a13
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat smartsynchronize.rb according to readability conventions

--- Casks/smartsynchronize.rb
+++ Casks/smartsynchronize.rb
@@ -1,8 +1,10 @@
 class Smartsynchronize < Cask
-  url 'http://www.syntevo.com/download/smartsynchronize/smartsynchronize-macosx-3_3_4.dmg'
-  homepage 'http://www.syntevo.com'
   version '3.3.4'
   sha256 'dc3732ef768d06a610742b0e1cf562c3cfce973e378b4c2dead6bf6cff9966f8'
+
+  url 'http://www.syntevo.com/download/smartsynchronize/smartsynchronize-macosx-3_3_4.dmg'
+  homepage 'http://www.syntevo.com'
+
   link 'SmartSynchronize 3.3.app'
   binary 'SmartSynchronize 3.3.app/Contents/MacOS/SmartSynchronize'
   caveats do
