30cce35539b48f9b07955ebe5c06f29fb75abba6
ndr <ndr@qef.io>
2014-06-27 02:00:57 +0200

    Reformat arora.rb according to readability conventions

--- Casks/arora.rb
+++ Casks/arora.rb
@@ -1,7 +1,9 @@
 class Arora < Cask
-  url 'https://arora.googlecode.com/files/Arora%200.10.1%20Intel-Qt4.5.3.dmg'
-  homepage 'https://code.google.com/p/arora/'
   version '0.10.1'
   sha256 '7cd9d54beb326dbb1a3eebaee1f5252a2df611d738f90d1398e857812ffc49ac'
+
+  url 'https://arora.googlecode.com/files/Arora%200.10.1%20Intel-Qt4.5.3.dmg'
+  homepage 'https://code.google.com/p/arora/'
+
   link 'Arora.app'
 end
