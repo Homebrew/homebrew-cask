9adbe8d3f01e55dbd34b00007a9ef71864213aa8
ndr <ndr@qef.io>
2014-06-27 02:01:29 +0200

    Reformat mountain.rb according to readability conventions

--- Casks/mountain.rb
+++ Casks/mountain.rb
@@ -1,8 +1,10 @@
 class Mountain < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://appgineers.de/mountain/files/Mountain.zip'
   appcast 'http://appgineers.de/mountain/files/mountaincast.xml'
   homepage 'http://appgineers.de/mountain/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Mountain.app'
 end
