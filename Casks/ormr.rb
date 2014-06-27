2d08d514c40519ab4d862f36ee6640e04dede4fa
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat ormr.rb according to readability conventions

--- Casks/ormr.rb
+++ Casks/ormr.rb
@@ -1,8 +1,10 @@
 class Ormr < Cask
-  url 'http://getormr.com/dn/Ormr.pkg'
-  homepage 'http://getormr.com/home/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://getormr.com/dn/Ormr.pkg'
+  homepage 'http://getormr.com/home/'
+
   install 'Ormr.pkg'
   uninstall :pkgutil => 'com.sadcatsoft.ormr.Ormr.pkg'
 end
