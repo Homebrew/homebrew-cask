6485ce26f2dab61763e6626486cbb138047ae03d
ndr <ndr@qef.io>
2014-06-27 02:01:22 +0200

    Reformat keymo.rb according to readability conventions

--- Casks/keymo.rb
+++ Casks/keymo.rb
@@ -1,8 +1,10 @@
 class Keymo < Cask
+  version '1.2.0'
+  sha256 '29dffdc3409c998ae8971f45c8bf1257b2e4eed6e602f1db2b17be4f06ba16a5'
+  
   url 'http://manytricks.com/download/_do_not_hotlink_/keymo120.dmg'
   appcast 'http://manytricks.com/keymo/appcast.xml'
   homepage 'http://manytricks.com/keymo'
-  version '1.2.0'
-  sha256 '29dffdc3409c998ae8971f45c8bf1257b2e4eed6e602f1db2b17be4f06ba16a5'
+
   link 'Keymo.app'
 end
