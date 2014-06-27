f29469d629c1768324d658e1aa5ccbea8229b0e0
ndr <ndr@qef.io>
2014-06-27 02:01:06 +0200

    Reformat dataurlmaker.rb according to readability conventions

--- Casks/dataurlmaker.rb
+++ Casks/dataurlmaker.rb
@@ -1,7 +1,9 @@
 class Dataurlmaker < Cask
-  url 'https://github.com/sveinbjornt/Data-URL-Toolkit/raw/master/Releases/DataURLMaker-1.1.zip'
-  homepage 'https://github.com/sveinbjornt/Data-URL-Toolkit'
   version '1.1'
   sha256 'dc297a57ea7180273a17fe8503f7ed7423afb66e210f67d32c4e566fa8e797e1'
+
+  url 'https://github.com/sveinbjornt/Data-URL-Toolkit/raw/master/Releases/DataURLMaker-1.1.zip'
+  homepage 'https://github.com/sveinbjornt/Data-URL-Toolkit'
+
   link 'DataURLMaker.app'
 end
