0dfdf19f5c7690feadb923a5ecac219988ade4f8
ndr <ndr@qef.io>
2014-06-27 02:01:29 +0200

    Reformat mongohub.rb according to readability conventions

--- Casks/mongohub.rb
+++ Casks/mongohub.rb
@@ -1,8 +1,10 @@
 class Mongohub < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://mongohub.s3.amazonaws.com/MongoHub.zip'
   appcast 'https://mongohub.s3.amazonaws.com/mongohub_su_feed.xml'
   homepage 'https://github.com/fotonauts/MongoHub-Mac'
-  version 'latest'
-  sha256 :no_check
+
   link 'MongoHub.app'
 end
