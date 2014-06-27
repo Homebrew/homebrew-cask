07bb839535aab1f4a9f7e9e9b330f8371c697f4b
ndr <ndr@qef.io>
2014-06-27 02:01:49 +0200

    Reformat torbrowser.rb according to readability conventions

--- Casks/torbrowser.rb
+++ Casks/torbrowser.rb
@@ -1,7 +1,9 @@
 class Torbrowser < Cask
-  url 'https://www.torproject.org/dist/torbrowser/3.6.2/TorBrowser-3.6.2-osx32_en-US.dmg'
-  homepage 'https://www.torproject.org/projects/torbrowser.html'
   version '3.6.2'
   sha256 '30778b5d164514c971cc4019ac8724f1aa4a4adc1d9cfcb07a50ce76179259c9'
+
+  url 'https://www.torproject.org/dist/torbrowser/3.6.2/TorBrowser-3.6.2-osx32_en-US.dmg'
+  homepage 'https://www.torproject.org/projects/torbrowser.html'
+
   link 'TorBrowser.app'
 end
