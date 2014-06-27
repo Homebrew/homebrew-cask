3d7dd6738e7d26f7b6ae2985f6d37ec3502e3e76
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat stay.rb according to readability conventions

--- Casks/stay.rb
+++ Casks/stay.rb
@@ -1,8 +1,10 @@
 class Stay < Cask
+  version '1.2.3'
+  sha256 'afe5abc2757bcee547a0d9ad8ebae55744ac197bc28c9fa28fc7987b9b512464'
+  
   url 'https://cordlessdog.com/stay/versions/Stay%201.2.3.zip'
   appcast 'http://cordlessdog.com/stay/appcast.xml'
   homepage 'http://cordlessdog.com/stay/'
-  version '1.2.3'
-  sha256 'afe5abc2757bcee547a0d9ad8ebae55744ac197bc28c9fa28fc7987b9b512464'
+
   link 'Stay.app'
 end
