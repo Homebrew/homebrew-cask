6cc99cad53955a91288c623a198ed931cc88736e
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat wedge.rb according to readability conventions

--- Casks/wedge.rb
+++ Casks/wedge.rb
@@ -1,8 +1,10 @@
 class Wedge < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://wedge.natestedman.com/release/Wedge.app.zip'
   appcast 'http://wedge.natestedman.com/appcast.xml'
   homepage 'http://wedge.natestedman.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'Wedge.app'
 end
