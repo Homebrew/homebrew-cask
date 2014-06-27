a4ee6d77399a07dcaa81bb1151e57007bf614c4a
ndr <ndr@qef.io>
2014-06-27 02:01:04 +0200

    Reformat coccinellida.rb according to readability conventions

--- Casks/coccinellida.rb
+++ Casks/coccinellida.rb
@@ -1,8 +1,10 @@
 class Coccinellida < Cask
+  version '0.6.1'
+  sha256 'bd590bf54667b85b4c14dae95d727bfa44cc893a75f231490b48a4a65049d8df'
+  
   url 'https://downloads.sourceforge.net/project/coccinellida/Coccinellida-0.6.1.zip'
   appcast 'http://coccinellida.sourceforge.net/sparkle.xml'
   homepage 'http://coccinellida.sourceforge.net/'
-  version '0.6.1'
-  sha256 'bd590bf54667b85b4c14dae95d727bfa44cc893a75f231490b48a4a65049d8df'
+
   link 'Coccinellida.app'
 end
