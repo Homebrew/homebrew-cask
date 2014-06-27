20cf98ff049104c075ebb64e88fc4e85cc035666
ndr <ndr@qef.io>
2014-06-27 02:01:00 +0200

    Reformat big-mean-folder-machine.rb according to readability conventions

--- Casks/big-mean-folder-machine.rb
+++ Casks/big-mean-folder-machine.rb
@@ -1,8 +1,10 @@
 class BigMeanFolderMachine < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.publicspace.net/download/BMFM.dmg'
   appcast 'http://www.publicspace.net/app/bmfm2.xml'
   homepage 'http://www.publicspace.net/BigMeanFolderMachine/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Big Mean Folder Machine 2.app'
 end
