a6f2aa12f484b9d7a820b0780c24849a80498113
ndr <ndr@qef.io>
2014-06-27 02:01:45 +0200

    Reformat sparkbox.rb according to readability conventions

--- Casks/sparkbox.rb
+++ Casks/sparkbox.rb
@@ -1,8 +1,10 @@
 class Sparkbox < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://t.icyblaze.com/sblatest'
   appcast 'http://matrix.icyblaze.com/index.php/checkupdate/p/8'
   homepage 'http://www.icyblaze.com/sparkbox'
-  version 'latest'
-  sha256 :no_check
+
   link 'Sparkbox.app'
 end
