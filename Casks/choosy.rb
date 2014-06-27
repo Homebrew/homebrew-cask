c0059bda8b8611651490c7bb0a27f5fa5f20812d
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat choosy.rb according to readability conventions

--- Casks/choosy.rb
+++ Casks/choosy.rb
@@ -1,7 +1,9 @@
 class Choosy < Cask
-  url 'http://downloads.choosyosx.com/choosy_1.0.3.zip'
-  homepage 'http://www.choosyosx.com/'
   version '1.0.3'
   sha256 '87513395c94221ae9a83c787ac78f1a1028099dc9be4883e19924768a354c6ca'
+
+  url 'http://downloads.choosyosx.com/choosy_1.0.3.zip'
+  homepage 'http://www.choosyosx.com/'
+
   prefpane 'Choosy.prefPane'
 end
