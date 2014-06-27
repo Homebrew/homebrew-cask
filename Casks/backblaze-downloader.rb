4321ce930ce1bfd89f7f3eebba9437f80bc8055b
ndr <ndr@qef.io>
2014-06-27 02:00:59 +0200

    Reformat backblaze-downloader.rb according to readability conventions

--- Casks/backblaze-downloader.rb
+++ Casks/backblaze-downloader.rb
@@ -1,7 +1,9 @@
 class BackblazeDownloader < Cask
-  url 'https://secure.backblaze.com/mac_restore_downloader'
-  homepage 'http://www.backblaze.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://secure.backblaze.com/mac_restore_downloader'
+  homepage 'http://www.backblaze.com/'
+
   link 'Backblaze Downloader.app'
 end
