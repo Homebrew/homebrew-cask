cdba340ee459222f9e9c06ba993eefb669e45faa
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat changes-meter.rb according to readability conventions

--- Casks/changes-meter.rb
+++ Casks/changes-meter.rb
@@ -1,8 +1,10 @@
 class ChangesMeter < Cask
-  url 'http://intuiware.com/downloads/changes-meter'
-  homepage 'http://intuiware.com/apps/changes-meter'
   version 'latest'
   sha256 :no_check
+
+  url 'http://intuiware.com/downloads/changes-meter'
+  homepage 'http://intuiware.com/apps/changes-meter'
+
   link 'Changes Meter.app'
   after_install do
     # Don't ask to move the app bundle to /Applications
