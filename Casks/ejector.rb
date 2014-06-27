18698fd920ba22ea6f3ca205af49373e723b22b5
ndr <ndr@qef.io>
2014-06-27 02:01:09 +0200

    Reformat ejector.rb according to readability conventions

--- Casks/ejector.rb
+++ Casks/ejector.rb
@@ -1,7 +1,9 @@
 class Ejector < Cask
-  url 'http://www.jeb.com.fr/soft/Ejector-v0.9.0.dmg'
-  homepage 'http://www.jeb.com.fr/en/ejector.shtml'
   version '0.9.0'
   sha256 '78e588ba2f9b02c753af7e4f432c0dc980d3745c3b8d3978b6b11e113e109f19'
+
+  url 'http://www.jeb.com.fr/soft/Ejector-v0.9.0.dmg'
+  homepage 'http://www.jeb.com.fr/en/ejector.shtml'
+
   link 'Ejector.app'
 end
