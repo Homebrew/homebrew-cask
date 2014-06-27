42774cb09205f2ff8eeb9c8ec65247886fb43bdb
ndr <ndr@qef.io>
2014-06-27 02:01:06 +0200

    Reformat cursorcerer.rb according to readability conventions

--- Casks/cursorcerer.rb
+++ Casks/cursorcerer.rb
@@ -1,7 +1,9 @@
 class Cursorcerer < Cask
-  url 'http://www.doomlaser.com/downloads/Cursorcerer.zip'
-  homepage 'http://doomlaser.com/cursorcerer-hide-your-cursor-at-will/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.doomlaser.com/downloads/Cursorcerer.zip'
+  homepage 'http://doomlaser.com/cursorcerer-hide-your-cursor-at-will/'
+
   prefpane 'Cursorcerer.prefPane'
 end
