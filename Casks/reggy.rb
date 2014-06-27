0cf7578f19e523b064ac073864c22e07546de7f4
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat reggy.rb according to readability conventions

--- Casks/reggy.rb
+++ Casks/reggy.rb
@@ -1,8 +1,10 @@
 class Reggy < Cask
+  version '1.3'
+  sha256 '5a4d72158bc524ab2f21c6cfad7b703f413707d9d078ec1923c268b110ff8dda'
+  
   url 'https://github.com/downloads/samsouder/reggy/Reggy_v1.3.tbz'
   appcast 'http://reggyapp.com/appcast.xml'
   homepage 'http://reggyapp.com/'
-  version '1.3'
-  sha256 '5a4d72158bc524ab2f21c6cfad7b703f413707d9d078ec1923c268b110ff8dda'
+
   link 'Reggy.app'
 end
