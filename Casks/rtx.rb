fa33e9f058993c526c39ebe528cf6217895e18f3
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat rtx.rb according to readability conventions

--- Casks/rtx.rb
+++ Casks/rtx.rb
@@ -1,7 +1,9 @@
 class Rtx < Cask
-  url 'http://www.rtxapp.com/download'
-  homepage 'http://www.rtxapp.com/mac/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.rtxapp.com/download'
+  homepage 'http://www.rtxapp.com/mac/'
+
   link 'RTX.app'
 end
