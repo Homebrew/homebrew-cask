5397b63d81eb58bb653dbcb48b5d219383d189f5
ndr <ndr@qef.io>
2014-06-27 02:01:47 +0200

    Reformat teamviz.rb according to readability conventions

--- Casks/teamviz.rb
+++ Casks/teamviz.rb
@@ -1,7 +1,9 @@
 class Teamviz < Cask
-  url 'http://www.teamviz.com/d/TeamViz.3.2.dmg'
-  homepage 'http://www.teamviz.com/'
   version '3.2'
   sha256 '58bbff611e1344c1a0ccb555400c212d0969ad4035469b654437f1adeafa7d37'
+
+  url 'http://www.teamviz.com/d/TeamViz.3.2.dmg'
+  homepage 'http://www.teamviz.com/'
+
   link 'TeamViz.app'
 end
