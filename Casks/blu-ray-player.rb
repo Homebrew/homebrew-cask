5b20dbfd86d11de31ec4c4bf9b31ebbbb076537a
ndr <ndr@qef.io>
2014-06-27 02:01:00 +0200

    Reformat blu-ray-player.rb according to readability conventions

--- Casks/blu-ray-player.rb
+++ Casks/blu-ray-player.rb
@@ -1,7 +1,9 @@
 class BluRayPlayer < Cask
-  url 'http://www.macblurayplayer.com/user/download/Mac_Bluray_Player.dmg'
-  homepage 'http://www.macblurayplayer.com/'
   version '2.10.2'
   sha256 '23105d6b8703b9ded387ba13b09d7e1b50d5bbb563fd91a7492a7ee7fd0de2bf'
+
+  url 'http://www.macblurayplayer.com/user/download/Mac_Bluray_Player.dmg'
+  homepage 'http://www.macblurayplayer.com/'
+
   link 'Blu-ray Player.app'
 end
