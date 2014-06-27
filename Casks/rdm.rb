43ab669afda7ca884338cded799f7f3052737a44
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat rdm.rb according to readability conventions

--- Casks/rdm.rb
+++ Casks/rdm.rb
@@ -1,7 +1,9 @@
 class Rdm < Cask
-  url 'https://github.com/uglide/RedisDesktopManager/releases/download/0.7.5/redis-desktop-manager-0.7.5.dmg'
-  homepage 'http://redisdesktop.com'
   version '0.7.5'
   sha256 'aeae3aa51b140f451ce7f97d02c81d6fe89f6d17957c7e9345270c64b03604d7'
+
+  url 'https://github.com/uglide/RedisDesktopManager/releases/download/0.7.5/redis-desktop-manager-0.7.5.dmg'
+  homepage 'http://redisdesktop.com'
+
   link 'rdm.app'
 end
