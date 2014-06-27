28db43da3ed797abdc4249f19253ca2f8b4731d7
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat viscosity.rb according to readability conventions

--- Casks/viscosity.rb
+++ Casks/viscosity.rb
@@ -1,8 +1,10 @@
 class Viscosity < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.sparklabs.com/downloads/Viscosity.dmg'
   appcast 'http://www.viscosityvpn.com/update/viscosity.xml'
   homepage 'http://www.sparklabs.com/viscosity/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Viscosity.app'
 end
