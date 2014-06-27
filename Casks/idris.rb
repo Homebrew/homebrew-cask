25749cca45d230bb800727a07f40f87f910d4013
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat idris.rb according to readability conventions

--- Casks/idris.rb
+++ Casks/idris.rb
@@ -1,8 +1,10 @@
 class Idris < Cask
-  url 'http://www.idris-lang.org/pkgs/idris-current.pkg'
-  homepage 'http://www.idris-lang.org'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.idris-lang.org/pkgs/idris-current.pkg'
+  homepage 'http://www.idris-lang.org'
+
   install 'idris-current.pkg'
   uninstall :pkgutil => 'org.idris-lang'
 end
