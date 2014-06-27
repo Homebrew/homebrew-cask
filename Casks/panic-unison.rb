43eac90da19c18618851ecd11625325bfa54938b
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat panic-unison.rb according to readability conventions

--- Casks/panic-unison.rb
+++ Casks/panic-unison.rb
@@ -1,8 +1,10 @@
 class PanicUnison < Cask
+  version '2.1.10'
+  sha256 'd7b15d7726dc892caa72a7476219ed3d06e86df704757670bb3236a82bdde5dc'
+  
   url 'http://download.panic.com/Unison/Unison%202.1.10.zip'
   appcast 'http://www.panic.com/updates/update.php'
   homepage 'http://panic.com/unison/'
-  version '2.1.10'
-  sha256 'd7b15d7726dc892caa72a7476219ed3d06e86df704757670bb3236a82bdde5dc'
+
   link 'Unison.app'
 end
