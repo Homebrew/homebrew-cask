04891ca7fe047ddc0d1dd9a03f0365659ab878a4
ndr <ndr@qef.io>
2014-06-27 02:01:09 +0200

    Reformat dvdstyler.rb according to readability conventions

--- Casks/dvdstyler.rb
+++ Casks/dvdstyler.rb
@@ -1,7 +1,9 @@
 class Dvdstyler < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/dvdstyler/DVDStyler-2.6-MacOSX.zip'
-  homepage 'http://dvdstyler.org'
   version '2.6'
   sha256 'fbb26aeb2ee50890b4a192d06d16ddc8a98a8465aa4eeff75d16fdc4de375ed6'
+
+  url 'https://downloads.sourceforge.net/sourceforge/dvdstyler/DVDStyler-2.6-MacOSX.zip'
+  homepage 'http://dvdstyler.org'
+
   link 'DVDStyler.app'
 end
