327b3f9a5d599dd697cd019c0c599b072c5318bb
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat noisy.rb according to readability conventions

--- Casks/noisy.rb
+++ Casks/noisy.rb
@@ -1,7 +1,9 @@
 class Noisy < Cask
-  url 'https://github.com/downloads/jonshea/Noisy/Noisy.zip'
-  homepage 'https://github.com/jonshea/Noisy'
   version 'latest'
   sha256 :no_check
+
+  url 'https://github.com/downloads/jonshea/Noisy/Noisy.zip'
+  homepage 'https://github.com/jonshea/Noisy'
+
   link 'Noisy.app'
 end
