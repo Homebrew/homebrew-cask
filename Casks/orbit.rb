856d1a0e20372ce9c93f0ab9339de9fe4f45b5a2
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat orbit.rb according to readability conventions

--- Casks/orbit.rb
+++ Casks/orbit.rb
@@ -1,8 +1,10 @@
 class Orbit < Cask
-  url       'http://orbitapp.net/updates/Orbit.zip'
-  appcast 'http://orbitapp.net/updates/appcast.xml'
-  homepage  'http://orbitapp.net'
-  version   'latest'
+  version 'latest'
   sha256 :no_check
+
+  url 'http://orbitapp.net/updates/Orbit.zip'
+  appcast 'http://orbitapp.net/updates/appcast.xml'
+  homepage 'http://orbitapp.net'
+
   link      'Orbit.app'
 end
