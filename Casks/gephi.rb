699b351ac7b52376885c621744a4033dfad69580
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat gephi.rb according to readability conventions

--- Casks/gephi.rb
+++ Casks/gephi.rb
@@ -1,7 +1,9 @@
 class Gephi < Cask
-  url 'https://launchpad.net/gephi/0.8/0.8.2beta/+download/gephi-0.8.2-beta.dmg'
-  homepage 'http://gephi.org/'
   version '0.8.2-beta'
   sha256 'ee8f7845ade6109de7e88ed03cc92885c6d936c34d666503d7c92bf3c5d0659d'
+
+  url 'https://launchpad.net/gephi/0.8/0.8.2beta/+download/gephi-0.8.2-beta.dmg'
+  homepage 'http://gephi.org/'
+
   link 'Gephi.app'
 end
