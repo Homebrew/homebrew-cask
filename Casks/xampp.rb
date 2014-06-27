ad93a528869f323171b2b2cbdaaf0277413bfa4c
ndr <ndr@qef.io>
2014-06-27 02:01:54 +0200

    Reformat xampp.rb according to readability conventions

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -1,8 +1,10 @@
 class Xampp < Cask
-  url 'https://downloads.sourceforge.net/project/xampp/XAMPP%20Mac%20OS%20X/1.8.3/xampp-osx-1.8.3-3-installer.dmg'
-  homepage 'http://www.apachefriends.org/index.html'
   version '1.8.3.3'
   sha256 'b54cced0697caa418851a4bdb11ee1697b3f7f71130e1e6bce19582d60e41530'
+
+  url 'https://downloads.sourceforge.net/project/xampp/XAMPP%20Mac%20OS%20X/1.8.3/xampp-osx-1.8.3-3-installer.dmg'
+  homepage 'http://www.apachefriends.org/index.html'
+
   caveats do
     manual_installer 'xampp-osx-1.8.3-3-installer.app'
   end
