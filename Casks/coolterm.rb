d520f912b57a9f9796abd3f889009f10df55214f
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat coolterm.rb according to readability conventions

--- Casks/coolterm.rb
+++ Casks/coolterm.rb
@@ -1,7 +1,9 @@
 class Coolterm < Cask
-  url 'http://freeware.the-meiers.org/CoolTermMac.zip'
-  homepage 'http://freeware.the-meiers.org/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://freeware.the-meiers.org/CoolTermMac.zip'
+  homepage 'http://freeware.the-meiers.org/'
+
   link 'CoolTermMac/CoolTerm.app'
 end
