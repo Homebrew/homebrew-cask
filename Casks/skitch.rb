8b4bb77ce5e691f8c228894d7a67cc26575a7dd6
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat skitch.rb according to readability conventions

--- Casks/skitch.rb
+++ Casks/skitch.rb
@@ -1,7 +1,9 @@
 class Skitch < Cask
-  url 'http://cdn1.evernote.com/skitch/mac/release/Skitch-2.7.2.zip'
-  homepage 'http://evernote.com/skitch/'
   version '2.7.2'
   sha256 '2388c60445782ac7afd1c7fc311007156a98b8e7b5488ede65cc5a68263da597'
+
+  url 'http://cdn1.evernote.com/skitch/mac/release/Skitch-2.7.2.zip'
+  homepage 'http://evernote.com/skitch/'
+
   link 'Skitch.app'
 end
