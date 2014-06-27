df8c00d62f5dbdb8b39c9ef55bb1d10f243d1e7d
ndr <ndr@qef.io>
2014-06-27 02:01:19 +0200

    Reformat intellij-idea-ce.rb according to readability conventions

--- Casks/intellij-idea-ce.rb
+++ Casks/intellij-idea-ce.rb
@@ -1,8 +1,10 @@
 class IntellijIdeaCe < Cask
-  url 'http://download-cf.jetbrains.com/idea/ideaIC-13.1.3.dmg'
-  homepage 'https://www.jetbrains.com/idea/index.html'
   version '13.1.3'
   sha256 '4b0e3cb665aa2e3523d3c90b0075292f5ba3eaaff2bfc4872e4438193e561067'
+
+  url 'http://download-cf.jetbrains.com/idea/ideaIC-13.1.3.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+
   link 'IntelliJ IDEA 13 CE.app'
   caveats do
     <<-EOS.undent
