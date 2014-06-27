0ec0ec56a8dd0b6eb91fd9a44d6602ec8498f484
ndr <ndr@qef.io>
2014-06-27 02:00:59 +0200

    Reformat baiduinput.rb according to readability conventions

--- Casks/baiduinput.rb
+++ Casks/baiduinput.rb
@@ -1,9 +1,11 @@
 # encoding: UTF-8
 class Baiduinput < Cask
-  url 'http://wuxian.baidu.com/download/1000e/baiduinput_mac_v3.2_1000e.dmg'
-  homepage 'http://wuxian.baidu.com/input/mac.html'
   version '3.2_1000e'
   sha256 'a8599116bb9248a06b7a26f7be73061cb00263263fe685cb0b7c6c99fce6cf56'
+
+  url 'http://wuxian.baidu.com/download/1000e/baiduinput_mac_v3.2_1000e.dmg'
+  homepage 'http://wuxian.baidu.com/input/mac.html'
+
   install '安装百度输入法.pkg'
   uninstall :pkgutil  => 'com.baidu.inputmethod.*',
             :files    => [
