75265e95c3bc94b1df205b08b7872cd6e2ce38fe
ndr <ndr@qef.io>
2014-06-27 02:01:47 +0200

    Reformat teeworlds.rb according to readability conventions

--- Casks/teeworlds.rb
+++ Casks/teeworlds.rb
@@ -1,8 +1,10 @@
 class Teeworlds < Cask
-  url 'https://teeworlds.com/files/teeworlds-0.6.2-osx.dmg'
-  homepage 'https://www.teeworlds.com/'
   version '0.6.2'
   sha256 '954e8f213f22b0c47c529d6e4d92f5bc1008a6a1ee54229dc62e706ffd1cab64'
+
+  url 'https://teeworlds.com/files/teeworlds-0.6.2-osx.dmg'
+  homepage 'https://www.teeworlds.com/'
+
   link 'Teeworlds.app'
   link 'Teeworlds Server.app'
 end
