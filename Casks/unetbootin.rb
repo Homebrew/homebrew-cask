75ed14d04b9fcf6a5710e18773947ab110f54dcc
ndr <ndr@qef.io>
2014-06-27 02:01:51 +0200

    Reformat unetbootin.rb according to readability conventions

--- Casks/unetbootin.rb
+++ Casks/unetbootin.rb
@@ -1,7 +1,9 @@
 class Unetbootin < Cask
-  homepage 'http://unetbootin.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/sourceforge/unetbootin/unetbootin-mac-585.zip'
   version '585'
   sha256 '2a6cc88feb01b666710c094b49b4cbb4e33a301d699b1bd4dbd229bfa0b5572e'
+
+  url 'https://downloads.sourceforge.net/sourceforge/unetbootin/unetbootin-mac-585.zip'
+  homepage 'http://unetbootin.sourceforge.net/'
+  
   link 'unetbootin.app'
 end
