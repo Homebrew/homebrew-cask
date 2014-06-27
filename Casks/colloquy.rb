71058c8135900e2e2fdb8fe73065391d5d6beb31
ndr <ndr@qef.io>
2014-06-27 02:01:04 +0200

    Reformat colloquy.rb according to readability conventions

--- Casks/colloquy.rb
+++ Casks/colloquy.rb
@@ -1,8 +1,10 @@
 class Colloquy < Cask
+  version '2.4.3'
+  sha256 'b1c54047b9a4aed313e9a4a86073aa5b1215cb8a4ea462c59e7a52d8be6b1a93'
+  
   url 'http://colloquy.info/downloads/colloquy-2.4.3.zip'
   appcast 'http://colloquy.info/update.php?rss'
   homepage 'http://colloquy.info/'
-  version '2.4.3'
-  sha256 'b1c54047b9a4aed313e9a4a86073aa5b1215cb8a4ea462c59e7a52d8be6b1a93'
+
   link 'Colloquy.app'
 end
