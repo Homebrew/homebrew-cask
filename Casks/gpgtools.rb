524895de7f62e7152e0f55947ca9f8a2bcc93b82
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat gpgtools.rb according to readability conventions

--- Casks/gpgtools.rb
+++ Casks/gpgtools.rb
@@ -1,8 +1,10 @@
 class Gpgtools < Cask
-  url 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg'
-  homepage 'https://gpgtools.org/index.html'
   version '2013.10.22'
   sha256 'd37ccf01e5ddd07dd84b76574e99b605ca9ead89cb0c6c126f4045e271eb3841'
+
+  url 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg'
+  homepage 'https://gpgtools.org/index.html'
+
   install 'Install.pkg'
   after_install do
     system '/usr/bin/sudo', '-E', '--',
