3f606f2281208ca09d46b4796a5268ac38997233
ndr <ndr@qef.io>
2014-06-27 02:01:45 +0200

    Reformat spark.rb according to readability conventions

--- Casks/spark.rb
+++ Casks/spark.rb
@@ -1,7 +1,9 @@
 class Spark < Cask
-  url 'http://www.shadowlab.org/softwares/Spark/Spark.dmg'
-  homepage 'http://www.shadowlab.org/softwares/spark.php'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.shadowlab.org/softwares/Spark/Spark.dmg'
+  homepage 'http://www.shadowlab.org/softwares/spark.php'
+
   link 'Spark.app'
 end
