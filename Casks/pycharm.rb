4cbef7f707dadfddac9eb002bc0a6bbada5f695f
ndr <ndr@qef.io>
2014-06-27 02:01:38 +0200

    Reformat pycharm.rb according to readability conventions

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,7 +1,9 @@
 class Pycharm < Cask
-  url 'http://download.jetbrains.com/python/pycharm-professional-3.4.dmg'
-  homepage 'http://www.jetbrains.com/pycharm/'
   version '3.4.0'
   sha256 '5fc1d1c38057349e4009fcab9a78981a647cd187e55f671f5c2bd347c682249a'
+
+  url 'http://download.jetbrains.com/python/pycharm-professional-3.4.dmg'
+  homepage 'http://www.jetbrains.com/pycharm/'
+
   link 'PyCharm.app'
 end
