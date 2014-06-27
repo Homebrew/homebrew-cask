48f703063c19bde7b850130bae3ef84eb9f5798b
ndr <ndr@qef.io>
2014-06-27 02:01:10 +0200

    Reformat emin-webpquicklook.rb according to readability conventions

--- Casks/emin-webpquicklook.rb
+++ Casks/emin-webpquicklook.rb
@@ -1,7 +1,9 @@
 class EminWebpquicklook < Cask
-  url 'https://raw.github.com/emin/WebPQuickLook/master/WebpQuickLook.tar.gz'
-  homepage 'https://github.com/emin/WebPQuickLook'
   version 'latest'
   sha256 :no_check
+
+  url 'https://raw.github.com/emin/WebPQuickLook/master/WebpQuickLook.tar.gz'
+  homepage 'https://github.com/emin/WebPQuickLook'
+
   qlplugin 'WebpQuickLook.qlgenerator'
 end
