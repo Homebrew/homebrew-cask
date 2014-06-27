58978f259a0517b7ff0a5b04fdf1aa8e8e6ae143
ndr <ndr@qef.io>
2014-06-27 02:01:02 +0200

    Reformat capsee.rb according to readability conventions

--- Casks/capsee.rb
+++ Casks/capsee.rb
@@ -1,8 +1,10 @@
 class Capsee < Cask
-  url 'http://www.threemagination.com/CapSee12.zip'
-  homepage 'http://www.threemagination.com/capsee/'
   version '1.2'
   sha256 'e78cdfe435cca259e0111a2b2131ad3be7d5ba6160cf69c8e7cbcc033eac2fc4'
+
+  url 'http://www.threemagination.com/CapSee12.zip'
+  homepage 'http://www.threemagination.com/capsee/'
+
   nested_container 'CapSee12.dmg'
   link 'CapSee.app'
 end
