00a004a4366036d8ebac6f0d83ede3fea8451127
ndr <ndr@qef.io>
2014-06-27 02:01:01 +0200

    Reformat bootchamp.rb according to readability conventions

--- Casks/bootchamp.rb
+++ Casks/bootchamp.rb
@@ -1,8 +1,10 @@
 class Bootchamp < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.kainjow.com/downloads/BootChamp.zip'
   appcast 'http://kainjow.com/updates/bootchamp.xml'
   homepage 'http://www.kainjow.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'BootChamp.app'
 end
