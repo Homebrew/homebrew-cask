018aed874bc89fb3018ae2b87bdd64d51bb31d65
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat stellarium.rb according to readability conventions

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,7 +1,9 @@
 class Stellarium < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/stellarium/Stellarium-0.12.4.dmg'
-  homepage 'http://stellarium.org'
   version '0.12.4'
   sha256 'fe4f3c58a64780888d6ca6e28ec948fa1d885c7f0dd0a85beb4b5ec7339398db'
+
+  url 'https://downloads.sourceforge.net/sourceforge/stellarium/Stellarium-0.12.4.dmg'
+  homepage 'http://stellarium.org'
+
   link 'Stellarium.app'
 end
