2d4bd983412eb3c06f4c82e60cef05d9491fb5c5
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat phpstorm.rb according to readability conventions

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -1,7 +1,9 @@
 class Phpstorm < Cask
-  url 'http://download.jetbrains.com/webide/PhpStorm-7.1.3.dmg'
-  homepage 'http://www.jetbrains.com/phpstorm/'
   version '7.1.3'
   sha256 '37ab8cb0739388da77d226fbdbf956ef441a8d7cb59e316e22815d57d11f6a1d'
+
+  url 'http://download.jetbrains.com/webide/PhpStorm-7.1.3.dmg'
+  homepage 'http://www.jetbrains.com/phpstorm/'
+
   link 'PhpStorm.app'
 end
