bcd4b4232eca1856fe73ebdfd21503f67cb69b55
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat wesnoth.rb according to readability conventions

--- Casks/wesnoth.rb
+++ Casks/wesnoth.rb
@@ -1,7 +1,9 @@
 class Wesnoth < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/wesnoth/Wesnoth_1.10.7.dmg'
-  homepage 'http://wesnoth.org'
   version '1.10.7'
   sha256 'cdd7788e55e26c9d619b7c98b87db1b99c6a0fc9f525ddb63a6bd33923d94a6f'
+
+  url 'https://downloads.sourceforge.net/sourceforge/wesnoth/Wesnoth_1.10.7.dmg'
+  homepage 'http://wesnoth.org'
+
   link 'Wesnoth.app'
 end
