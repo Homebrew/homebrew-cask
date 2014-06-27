c07f4c8a259ce01d1d56b3bbf01c33936c9a271e
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat soundnote.rb according to readability conventions

--- Casks/soundnote.rb
+++ Casks/soundnote.rb
@@ -1,8 +1,10 @@
 class Soundnote < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://download.mrgeckosmedia.com/SoundNote.zip'
   appcast 'http://mrgeckosmedia.com/applications/appcast/SoundNote'
   homepage 'https://mrgeckosmedia.com/applications/info/SoundNote'
-  version 'latest'
-  sha256 :no_check
+
   link 'SoundNote.app'
 end
