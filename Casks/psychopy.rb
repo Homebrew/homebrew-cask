d75036e4f03642d3c87079a111246e30cea8d596
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat psychopy.rb according to readability conventions

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,7 +1,9 @@
 class Psychopy < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/psychpy/StandalonePsychoPy-1.79.01-OSX.dmg'
-  homepage 'http://www.psychopy.org/'
   version '1.79.01'
   sha256 'a2b174aee30ca4a8858238404adfd9271da9ec14243aa30be0b7f9a6abb047b8'
+
+  url 'https://downloads.sourceforge.net/sourceforge/psychpy/StandalonePsychoPy-1.79.01-OSX.dmg'
+  homepage 'http://www.psychopy.org/'
+
   link 'PsychoPy2.app'
 end
