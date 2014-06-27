d2de3418fe62a3cc1faba735d5f075caf1c18f16
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat gifrocket.rb according to readability conventions

--- Casks/gifrocket.rb
+++ Casks/gifrocket.rb
@@ -1,7 +1,9 @@
 class Gifrocket < Cask
-  url 'http://www.gifrocket.com/Gifrocket.0.1.2.zip'
-  homepage 'http://www.gifrocket.com/'
   version '0.1.2'
   sha256 'c5407c9caad8c038f604d35da9a6554dede79611daaf5174116ea9517a704593'
+
+  url 'http://www.gifrocket.com/Gifrocket.0.1.2.zip'
+  homepage 'http://www.gifrocket.com/'
+
   link 'Gifrocket.app'
 end
