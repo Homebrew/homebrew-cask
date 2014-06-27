2ae9e8eb3685253a9e65c17f903b70765358d0b6
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat truecrypt.rb according to readability conventions

--- Casks/truecrypt.rb
+++ Casks/truecrypt.rb
@@ -1,8 +1,10 @@
 class Truecrypt < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/truecrypt/TrueCrypt-7.2-Mac-OS-X.dmg'
-  homepage 'http://truecrypt.org/'
   version '7.2'
   sha256 '01acf85be9b23a1c718193c40f3ecaaf6551695e0dc67c28345e560cca56c94e'
+
+  url 'https://downloads.sourceforge.net/sourceforge/truecrypt/TrueCrypt-7.2-Mac-OS-X.dmg'
+  homepage 'http://truecrypt.org/'
+
   install 'TrueCrypt 7.2.mpkg'
   caveats do
     files_in_usr_local
