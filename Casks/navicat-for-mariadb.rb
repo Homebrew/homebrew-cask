0f7540a25023f2f82a50e54e85869038d97ba696
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat navicat-for-mariadb.rb according to readability conventions

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,7 +1,9 @@
 class NavicatForMariadb < Cask
-  url 'http://download.navicat.com/download/navicat110_mariadb_en.dmg'
-  homepage 'http://www.navicat.com/products/navicat-for-mariadb'
   version '11.0.18'
   sha256 'c0163c0a5027b16a38b6f95635728aee7b36aebb32f3b4603fd5af87627609c0'
+
+  url 'http://download.navicat.com/download/navicat110_mariadb_en.dmg'
+  homepage 'http://www.navicat.com/products/navicat-for-mariadb'
+
   link 'Navicat for MariaDB.app'
 end
