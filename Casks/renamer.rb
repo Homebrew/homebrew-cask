235578ba833e0e9ff6c8a18a94212e08afa0935f
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat renamer.rb according to readability conventions

--- Casks/renamer.rb
+++ Casks/renamer.rb
@@ -1,7 +1,9 @@
 class Renamer < Cask
-  url 'http://creativebe.com/download/renamer'
-  homepage 'http://renamer.com'
   version 'latest'
   sha256 :no_check
+
+  url 'http://creativebe.com/download/renamer'
+  homepage 'http://renamer.com'
+
   link 'Renamer.app'
 end
