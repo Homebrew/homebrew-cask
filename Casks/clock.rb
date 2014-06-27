a3c01749cd252a32a0e4735bfdcc958501d2348d
ndr <ndr@qef.io>
2014-06-27 02:01:04 +0200

    Reformat clock.rb according to readability conventions

--- Casks/clock.rb
+++ Casks/clock.rb
@@ -1,7 +1,9 @@
 class Clock < Cask
-  url 'https://github.com/downloads/zachwaugh/Clock.app/Clock-1.1.zip'
-  homepage 'http://zachwaugh.me/clock/'
   version '1.1'
   sha256 'f2def6485626b4d10a535cb86eb7c877da8d59a2103f8da7b211970c88b96913'
+
+  url 'https://github.com/downloads/zachwaugh/Clock.app/Clock-1.1.zip'
+  homepage 'http://zachwaugh.me/clock/'
+
   link 'Clock.app'
 end
