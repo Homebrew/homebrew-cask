3ede180f9fa6797b78143f1b3e6af7e83e873706
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat clarify.rb according to readability conventions

--- Casks/clarify.rb
+++ Casks/clarify.rb
@@ -1,7 +1,9 @@
 class Clarify < Cask
-  url 'http://www.bluemangolearning.com/download/clarify/1_0/release/clarify.dmg'
-  homepage 'http://www.clarify-it.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.bluemangolearning.com/download/clarify/1_0/release/clarify.dmg'
+  homepage 'http://www.clarify-it.com/'
+
   link 'Clarify.app'
 end
