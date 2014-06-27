0a7c022f7b7b2f12ecdb182a789d60df2dc7a0b6
ndr <ndr@qef.io>
2014-06-27 02:01:07 +0200

    Reformat desmume.rb according to readability conventions

--- Casks/desmume.rb
+++ Casks/desmume.rb
@@ -1,7 +1,9 @@
 class Desmume < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/desmume/desmume-0.9.10-mac.dmg'
-  homepage 'http://www.desmume.org'
   version '0.9.10'
   sha256 '5124a3ea2c64e55bf6b31bb8eb3408d3157e3fa739021dba16e39801388fbb2d'
+
+  url 'https://downloads.sourceforge.net/sourceforge/desmume/desmume-0.9.10-mac.dmg'
+  homepage 'http://www.desmume.org'
+
   link 'DeSmuME.app'
 end
