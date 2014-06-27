d38f9a24d5b29980085eb95d55e16a85c1d12663
ndr <ndr@qef.io>
2014-06-27 02:00:58 +0200

    Reformat atext.rb according to readability conventions

--- Casks/atext.rb
+++ Casks/atext.rb
@@ -1,8 +1,10 @@
 class Atext < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.trankynam.com/atext/downloads/aText.dmg'
   appcast 'http://www.trankynam.com/atext/aText-Appcast.xml'
   homepage 'http://www.trankynam.com/atext/'
-  version 'latest'
-  sha256 :no_check
+
   link 'aText.app'
 end
