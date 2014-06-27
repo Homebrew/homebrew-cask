e97b344b8216692dca8c2371ac45fcf9e1e9a491
ndr <ndr@qef.io>
2014-06-27 02:00:57 +0200

    Reformat appcode.rb according to readability conventions

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,7 +1,9 @@
 class Appcode < Cask
-  url 'http://download.jetbrains.com/objc/AppCode-3.0.1.dmg'
-  homepage 'http://www.jetbrains.com/objc/'
   version '3.0.1'
   sha256 '3ed3b836fb61ab3468d899dbf2c890248564c28da6720b80249db55cf2cb041e'
+
+  url 'http://download.jetbrains.com/objc/AppCode-3.0.1.dmg'
+  homepage 'http://www.jetbrains.com/objc/'
+
   link 'AppCode.app'
 end
