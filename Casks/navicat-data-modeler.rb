e95cc09b9051f6248e0395405939a8650adb5e91
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat navicat-data-modeler.rb according to readability conventions

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -1,7 +1,9 @@
 class NavicatDataModeler < Cask
-  url 'http://download.navicat.com/download/modeler010_en.dmg'
-  homepage 'http://www.navicat.com/products/navicat-data-modeler'
   version '1.0.5'
   sha256 'de0f1dbb450401f9ce29948886b60167508ccca621162a9ab9a805d54d574bbb'
+
+  url 'http://download.navicat.com/download/modeler010_en.dmg'
+  homepage 'http://www.navicat.com/products/navicat-data-modeler'
+
   link 'Navicat Data Modeler.app'
 end
