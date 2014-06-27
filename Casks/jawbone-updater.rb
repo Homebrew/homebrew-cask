d2fcbef30ca52c9047e7fb86fac3eaff4be01c96
ndr <ndr@qef.io>
2014-06-27 02:01:21 +0200

    Reformat jawbone-updater.rb according to readability conventions

--- Casks/jawbone-updater.rb
+++ Casks/jawbone-updater.rb
@@ -1,8 +1,10 @@
 class JawboneUpdater < Cask
-  url 'http://content.jawbone.com/store/dashboard/Jawbone_Updater-2.2.3.pkg'
-  homepage 'http://jawbone.com/'
   version '2.2.3'
   sha256 '3085edf935347e45573405ee7e51fbacce366847f5f06f783a4e1ea89d70aee6'
+
+  url 'http://content.jawbone.com/store/dashboard/Jawbone_Updater-2.2.3.pkg'
+  homepage 'http://jawbone.com/'
+
   install 'Jawbone_Updater-2.2.3.pkg'
   uninstall :quit => 'com.aliph.Jawbone_Updater',
             :pkgutil => 'com.Aliph.[Jj]awbone(|Updater.*).pkg'
