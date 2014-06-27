9530962e1ef5000fae9038bb5b94922921289d14
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat openoffice.rb according to readability conventions

--- Casks/openoffice.rb
+++ Casks/openoffice.rb
@@ -1,7 +1,9 @@
 class Openoffice < Cask
-  url 'https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.0/binaries/en-US/Apache_OpenOffice_4.1.0_MacOS_x86-64_install_en-US.dmg'
-  homepage 'http://www.openoffice.org/'
   version '4.1.0'
   sha256 'ac77ff69cbbd87523e019d3c68d839cba2f952b0b63c13fb948863c8f25eaa9a'
+
+  url 'https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.0/binaries/en-US/Apache_OpenOffice_4.1.0_MacOS_x86-64_install_en-US.dmg'
+  homepage 'http://www.openoffice.org/'
+
   link 'OpenOffice.app'
 end
