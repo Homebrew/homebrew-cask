06a11367a6d77d901def9d6f25f81e0996e3fcbc
ndr <ndr@qef.io>
2014-06-27 02:01:45 +0200

    Reformat spacemonkey.rb according to readability conventions

--- Casks/spacemonkey.rb
+++ Casks/spacemonkey.rb
@@ -1,8 +1,10 @@
 class Spacemonkey < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://downloads.spacemonkey.com/client/mac/latest'
   appcast 'https://rink.hockeyapp.net/api/2/apps/aa33b6780fdfc71247b2995fa47b5d7c'
   homepage 'https://www.spacemonkey.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'SpaceMonkey.app'
 end
