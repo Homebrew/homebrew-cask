17abb67f0d3eb968a00387ffea5e295ff93a8105
ndr <ndr@qef.io>
2014-06-27 02:01:17 +0200

    Reformat hostbuddy.rb according to readability conventions

--- Casks/hostbuddy.rb
+++ Casks/hostbuddy.rb
@@ -1,8 +1,10 @@
 class Hostbuddy < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://clickontyler.com/hostbuddy/download/'
   appcast 'http://shine.clickontyler.com/appcast.php?id=22'
   homepage 'http://clickontyler.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'Hostbuddy.app'
 end
