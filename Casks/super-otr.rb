fe2470ceb0b0b0b120a2513be42c5d55f9f2f2c9
ndr <ndr@qef.io>
2014-06-27 02:01:46 +0200

    Reformat super-otr.rb according to readability conventions

--- Casks/super-otr.rb
+++ Casks/super-otr.rb
@@ -1,8 +1,10 @@
 class SuperOtr < Cask
+  version '0.9.6.0b76'
+  sha256 '610d74c43d2fa38527b66fa677c6eff07543f8a853950160954e59cb251b066b'
+  
   url 'http://apfel-a.macbay.de/wordpress/wp-content/plugins/download-monitor/download.php?id=Super-OTR-0.9.6.0b79.zip'
   appcast 'http://apfel-a.macbay.de/software/superotr/appcast-superotr.xml'
   homepage 'http://apfel-a.macbay.de/super-otr/'
-  version '0.9.6.0b76'
-  sha256 '610d74c43d2fa38527b66fa677c6eff07543f8a853950160954e59cb251b066b'
+
   link 'Super OTR.app'
 end
