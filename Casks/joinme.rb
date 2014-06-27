87080c9a4954fb47553331a7e46f697a5db916f2
ndr <ndr@qef.io>
2014-06-27 02:01:21 +0200

    Reformat joinme.rb according to readability conventions

--- Casks/joinme.rb
+++ Casks/joinme.rb
@@ -1,7 +1,9 @@
 class Joinme < Cask
-  url 'https://secure.join.me/Download.aspx?installer=mac&webdownload=true'
-  homepage 'https://join.me/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://secure.join.me/Download.aspx?installer=mac&webdownload=true'
+  homepage 'https://join.me/'
+
   link 'join.me.app'
 end
