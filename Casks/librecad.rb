073df2358041861580b762641824142e94136e1f
ndr <ndr@qef.io>
2014-06-27 02:01:23 +0200

    Reformat librecad.rb according to readability conventions

--- Casks/librecad.rb
+++ Casks/librecad.rb
@@ -1,7 +1,9 @@
 class Librecad < Cask
-  url 'https://downloads.sourceforge.net/project/librecad/OSX/2.0.4/LibreCAD-2.0.4.dmg'
-  homepage 'http://librecad.org/'
   version '2.0.4'
   sha256 'f9a2e56c5a4b79275d45d5578f728fbe3e981e04f550229579de71b0e8ce0604'
+
+  url 'https://downloads.sourceforge.net/project/librecad/OSX/2.0.4/LibreCAD-2.0.4.dmg'
+  homepage 'http://librecad.org/'
+
   link 'LibreCAD.app'
 end
