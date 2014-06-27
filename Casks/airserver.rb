0015743016a237a3c8ceb3aab16a8d79be6b89e2
ndr <ndr@qef.io>
2014-06-27 02:00:56 +0200

    Reformat airserver.rb according to readability conventions

--- Casks/airserver.rb
+++ Casks/airserver.rb
@@ -1,7 +1,9 @@
 class Airserver < Cask
-  url 'http://dl.airserver.com/mac/AirServer-5.0.4.0.dmg'
-  homepage 'http://www.airserver.com'
   version '5.0.4.0'
   sha256 'd6e667bf91a0fb87d000ed778b3cf46c0d9d6e6d397a85de196f5da09f7f1a7a'
+
+  url 'http://dl.airserver.com/mac/AirServer-5.0.4.0.dmg'
+  homepage 'http://www.airserver.com'
+
   link 'AirServer.app'
 end
