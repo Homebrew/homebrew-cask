37a68916af50c41a232b7282421a5e69769ac343
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat pacifist.rb according to readability conventions

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -1,8 +1,10 @@
 class Pacifist < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=dmg'
   appcast 'http://www.charlessoft.com/cgi-bin/pacifist_sparkle.cgi'
   homepage 'http://www.charlessoft.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Pacifist.app'
 end
