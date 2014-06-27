c26ea6bd7288ee93df7fb032786ba3926d1a0b07
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat ghostlab.rb according to readability conventions

--- Casks/ghostlab.rb
+++ Casks/ghostlab.rb
@@ -1,8 +1,10 @@
 class Ghostlab < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://awesome.vanamco.com/downloads/ghostlab/Ghostlab.dmg'
   appcast 'http://awesome.vanamco.com/update/ghostlab-cast.xml'
   homepage 'http://vanamco.com/ghostlab/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Ghostlab.app'
 end
