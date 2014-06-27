d1cc669e1f9718b42eb81023164d307ff4c9e4ed
ndr <ndr@qef.io>
2014-06-27 02:01:07 +0200

    Reformat dewdrop.rb according to readability conventions

--- Casks/dewdrop.rb
+++ Casks/dewdrop.rb
@@ -1,8 +1,10 @@
 class Dewdrop < Cask
+  version '0.4'
+  sha256 '88cf0ff182f5b4c49e761186b735e69b4a2d724e4e8781d4f87121faac850c57'
+  
   url 'http://dewdrop.dangelov.com/binaries/DewdropClient-0.4-Mac.tar.gz'
   appcast 'http://dewdrop.dangelov.com/mac-appcast/updates.xml'
   homepage 'http://dewdrop.dangelov.com/'
-  version '0.4'
-  sha256 '88cf0ff182f5b4c49e761186b735e69b4a2d724e4e8781d4f87121faac850c57'
+
   link 'Dewdrop.app'
 end
