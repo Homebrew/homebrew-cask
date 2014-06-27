fa876f003892a106e3e31ab0165f67ee3d798a73
ndr <ndr@qef.io>
2014-06-27 02:01:12 +0200

    Reformat freecol.rb according to readability conventions

--- Casks/freecol.rb
+++ Casks/freecol.rb
@@ -1,7 +1,9 @@
 class Freecol < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/freecol/freecol-0.10.7-mac.tar.bz2'
-  homepage 'http://freecol.org'
   version '0.10.7'
   sha256 'eb05ea179df6adc6bcf4234ce3809c48b8c2d5597da20a10910ded677f2b6a2d'
+
+  url 'https://downloads.sourceforge.net/sourceforge/freecol/freecol-0.10.7-mac.tar.bz2'
+  homepage 'http://freecol.org'
+
   link 'FreeCol.app'
 end
