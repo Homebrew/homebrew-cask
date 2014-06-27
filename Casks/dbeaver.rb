679671bb87a32636d8927bd8ac1e022a5b2bc30f
ndr <ndr@qef.io>
2014-06-27 02:01:06 +0200

    Reformat dbeaver.rb according to readability conventions

--- Casks/dbeaver.rb
+++ Casks/dbeaver.rb
@@ -1,7 +1,9 @@
 class Dbeaver < Cask
-  url 'http://dbeaver.jkiss.org/files/dbeaver-2.4.2-macosx.cocoa.x86_64.zip'
-  homepage 'http://dbeaver.jkiss.org/'
   version '2.4.2'
   sha256 'bc9255302f1770b1706465f87191249303532695901a731245f96764708435a6'
+
+  url 'http://dbeaver.jkiss.org/files/dbeaver-2.4.2-macosx.cocoa.x86_64.zip'
+  homepage 'http://dbeaver.jkiss.org/'
+
   link 'dbeaver/dbeaver.app'
 end
