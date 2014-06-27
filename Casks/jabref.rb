24dfea2e2806215b4f170070b4fa121ede6525c6
ndr <ndr@qef.io>
2014-06-27 02:01:21 +0200

    Reformat jabref.rb according to readability conventions

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -1,7 +1,9 @@
 class Jabref < Cask
-  url 'https://downloads.sourceforge.net/project/jabref/jabref/2.10/JabRef-2.10-OSX.zip'
-  homepage 'http://jabref.sourceforge.net/'
   version '2.10'
   sha256 'c63a49e47a43bdb026dde7fb695210d9a3f8c0e71445af7d6736c5379b23baa2'
+
+  url 'https://downloads.sourceforge.net/project/jabref/jabref/2.10/JabRef-2.10-OSX.zip'
+  homepage 'http://jabref.sourceforge.net/'
+
   link 'JabRef.app'
 end
