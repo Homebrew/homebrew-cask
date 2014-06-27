f6e0315d917fb70db3230c9f6968fe734db7d2d9
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat ntfs-free.rb according to readability conventions

--- Casks/ntfs-free.rb
+++ Casks/ntfs-free.rb
@@ -1,8 +1,10 @@
 class NtfsFree < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/ntfsfree/NTFS-free-10.8.2.pkg'
-  homepage 'http://sourceforge.net/projects/ntfsfree/'
   version '10.8.2'
   sha256 '1bc7122a2d869b740a35e2dba1fe29c380f21ad07481fc0a46b2f55bf79c574e'
+
+  url 'https://downloads.sourceforge.net/sourceforge/ntfsfree/NTFS-free-10.8.2.pkg'
+  homepage 'http://sourceforge.net/projects/ntfsfree/'
+
   install 'NTFS-free-10.8.2.pkg'
   uninstall :files => [
     '/System/Library/Extensions/ntfs-free.kext',
