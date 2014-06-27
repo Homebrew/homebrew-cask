0d14a64185b6f20c4b8d032faaa0189611b0140a
ndr <ndr@qef.io>
2014-06-27 02:01:10 +0200

    Reformat fakethunder.rb according to readability conventions

--- Casks/fakethunder.rb
+++ Casks/fakethunder.rb
@@ -1,8 +1,10 @@
 class Fakethunder < Cask
+  version '2.0.16'
+  sha256 '42ce5c0f836a17b18591816359a5a4656a7910ae3a80f83e1d1422dd995fde00'
+  
   url 'http://martianlaboratory.com/fakethunder/fakeThunder216.zip'
   appcast 'http://martianlaboratory.com/fakethunder/update.xml'
   homepage 'http://martianz.cn/fakethunder/'
-  version '2.0.16'
-  sha256 '42ce5c0f836a17b18591816359a5a4656a7910ae3a80f83e1d1422dd995fde00'
+
   link 'fakeThunder.app'
 end
