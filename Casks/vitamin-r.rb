db4836b405f811396d831aa7e6807b9f141f92a8
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat vitamin-r.rb according to readability conventions

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -1,8 +1,10 @@
 class VitaminR < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.publicspace.net/download/Vitamin.dmg'
   appcast 'http://www.publicspace.net/app/vitamin2.xml'
   homepage 'http://www.publicspace.net/Vitamin-R/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Vitamin-R 2.app'
 end
