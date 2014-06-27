2697c66955ab776a6c953c8b5e9ea564082ba785
ndr <ndr@qef.io>
2014-06-27 02:01:27 +0200

    Reformat marked.rb according to readability conventions

--- Casks/marked.rb
+++ Casks/marked.rb
@@ -1,8 +1,10 @@
 class Marked < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://marked2app.com/download/Marked.zip'
   appcast 'http://abyss.designheresy.com/marked/marked.xml'
   homepage 'http://marked2app.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'Marked.app'
 end
