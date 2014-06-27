ba5e1613f7118a35b5824895c71c3a9b2db9715b
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat colorschemer-studio.rb according to readability conventions

--- Casks/colorschemer-studio.rb
+++ Casks/colorschemer-studio.rb
@@ -1,8 +1,10 @@
 class ColorschemerStudio < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.colorschemer.com/colorschemerstudio.dmg'
   appcast 'http://www.colorschemer.com/appcast/studio2_mac.xml'
   homepage 'http://www.colorschemer.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'ColorSchemer Studio 2.app'
 end
