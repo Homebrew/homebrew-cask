553887b949f8224a55d03d6ac620838543080b87
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat pd-extended.rb according to readability conventions

--- Casks/pd-extended.rb
+++ Casks/pd-extended.rb
@@ -1,8 +1,10 @@
 class PdExtended < Cask
-  url 'https://downloads.sourceforge.net/project/pure-data/pd-extended/0.43.4/Pd-0.43.4-extended-macosx105-i386.dmg'
-  homepage 'http://puredata.info/downloads/pd-extended'
   version '0.43.4'
   sha256 'abe7bd637b1495ad9d5a500f0a18550c1600e34ee17e60aa1a48e4dbdee59bb9'
+
+  url 'https://downloads.sourceforge.net/project/pure-data/pd-extended/0.43.4/Pd-0.43.4-extended-macosx105-i386.dmg'
+  homepage 'http://puredata.info/downloads/pd-extended'
+
   link 'Pd-extended.app'
 
   before_uninstall do
