f9b4f29e333e42c660556b426501bac24f5e3257
ndr <ndr@qef.io>
2014-06-27 02:01:00 +0200

    Reformat bee.rb according to readability conventions

--- Casks/bee.rb
+++ Casks/bee.rb
@@ -1,8 +1,10 @@
 class Bee < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://neat.io/bee/download.html'
   appcast 'http://neat.io/appcasts/bee-appcast.xml'
   homepage 'http://neat.io/bee/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Bee.app'
 end
