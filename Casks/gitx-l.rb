37991ba2f843dcf3a7f526a4b5a7c8e0fd9f174f
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat gitx-l.rb according to readability conventions

--- Casks/gitx-l.rb
+++ Casks/gitx-l.rb
@@ -1,9 +1,11 @@
 class GitxL < Cask
+  version '0.8.4'
+  sha256 'c5f4088497abf5a219bb7bde4fae643fec61647be25bf836fd679567dcabd7df'
+  
   url 'https://github.com/downloads/laullon/gitx/GitX-L_v0.8.4.zip'
   appcast 'http://gitx.laullon.com/appcast.xml'
   homepage 'http://gitx.laullon.com/'
-  version '0.8.4'
-  sha256 'c5f4088497abf5a219bb7bde4fae643fec61647be25bf836fd679567dcabd7df'
+
   link 'GitX.app'
   binary 'GitX.app/Contents/Resources/gitx'
 end
