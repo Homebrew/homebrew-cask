2449798fa642347b070653992f78cf701ab5487f
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat objektiv.rb according to readability conventions

--- Casks/objektiv.rb
+++ Casks/objektiv.rb
@@ -1,8 +1,10 @@
 class Objektiv < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://nthloop.com/objektiv/objektiv-latest.zip'
   appcast 'http://nthloop.com/objektiv/appcast.xml'
   homepage 'http://nthloop.github.io/Objektiv/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Objektiv.app'
 end
