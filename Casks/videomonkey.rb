c7586812d2d191ad0ec3a01b24645e4a4f531a03
ndr <ndr@qef.io>
2014-06-27 02:01:51 +0200

    Reformat videomonkey.rb according to readability conventions

--- Casks/videomonkey.rb
+++ Casks/videomonkey.rb
@@ -1,8 +1,10 @@
 class Videomonkey < Cask
+  version '0.16'
+  sha256 'ab09a3bb98a78d32ae48c55b6572e4d1e10ead0f26d5ba895a9a2ee5c105088c'
+  
   url 'http://videomonkey.org/download/videomonkey-0.16.zip'
   appcast 'http://videomonkey.org/releases/videomonkeycast.xml'
   homepage 'http://videomonkey.org/'
-  version '0.16'
-  sha256 'ab09a3bb98a78d32ae48c55b6572e4d1e10ead0f26d5ba895a9a2ee5c105088c'
+
   link 'VideoMonkey.app'
 end
