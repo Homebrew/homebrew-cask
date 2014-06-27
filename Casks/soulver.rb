b5d038524117dfdfdf0e7cdcfb1ae06080cb469c
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat soulver.rb according to readability conventions

--- Casks/soulver.rb
+++ Casks/soulver.rb
@@ -1,9 +1,11 @@
 class Soulver < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.acqualia.com/files/download.php?product=soulver'
   appcast 'http://www.acqualia.com/soulver/appcast/soulver2.xml'
   homepage 'http://www.acqualia.com/soulver/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Soulver.app'
 
   after_install do
