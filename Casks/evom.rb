d37655fc9c7b5b82d442101c76038b9e733fe044
ndr <ndr@qef.io>
2014-06-27 02:01:10 +0200

    Reformat evom.rb according to readability conventions

--- Casks/evom.rb
+++ Casks/evom.rb
@@ -1,8 +1,10 @@
 class Evom < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://files.thelittleappfactory.com/evom/Evom.zip'
   appcast 'https://files.thelittleappfactory.com/evom/appcast.xml'
   homepage 'http://thelittleappfactory.com/evom/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Evom.app'
 end
