917e5bca8a3c5b247b198aae1afac60c15a793d3
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat snippets.rb according to readability conventions

--- Casks/snippets.rb
+++ Casks/snippets.rb
@@ -1,8 +1,10 @@
 class Snippets < Cask
+  version '0.8.2'
+  sha256 '32167664dacd3301dd89e618093c3ce7a7dcd0204061d34f7759bad3c9a7cc78'
+  
   url 'https://snippets.me/download/mac/Snippets-468.zip'
   appcast 'http://snippets.me/mac/appcast.xml'
   homepage 'http://snippets.me/'
-  version '0.8.2'
-  sha256 '32167664dacd3301dd89e618093c3ce7a7dcd0204061d34f7759bad3c9a7cc78'
+
   link 'Snippets.app'
 end
