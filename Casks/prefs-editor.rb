3b3ad684571dd104dca48131e3f39994c193d753
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat prefs-editor.rb according to readability conventions

--- Casks/prefs-editor.rb
+++ Casks/prefs-editor.rb
@@ -1,7 +1,9 @@
 class PrefsEditor < Cask
-  url 'http://files.tempel.org/Various/OSX_Prefs_Editor/PrefsEditor.zip'
-  homepage 'http://www.tempel.org/PrefsEditor'
   version 'latest'
   sha256 :no_check
+
+  url 'http://files.tempel.org/Various/OSX_Prefs_Editor/PrefsEditor.zip'
+  homepage 'http://www.tempel.org/PrefsEditor'
+
   link 'Prefs Editor.app'
 end
