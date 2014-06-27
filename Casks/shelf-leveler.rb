51552be1109f7ce46827ff65a9ecd14d36c400fc
ndr <ndr@qef.io>
2014-06-27 02:01:42 +0200

    Reformat shelf-leveler.rb according to readability conventions

--- Casks/shelf-leveler.rb
+++ Casks/shelf-leveler.rb
@@ -1,7 +1,9 @@
 class ShelfLeveler < Cask
-  url 'http://download.mrgeckosmedia.com/ShelfLeveler.zip'
-  homepage 'https://mrgeckosmedia.com/applications/info/Shelf-Leveler'
   version 'latest'
   sha256 :no_check
+
+  url 'http://download.mrgeckosmedia.com/ShelfLeveler.zip'
+  homepage 'https://mrgeckosmedia.com/applications/info/Shelf-Leveler'
+
   link 'Shelf Leveler.app'
 end
