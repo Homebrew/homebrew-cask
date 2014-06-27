8eac4510d274fd5e85a5534d1fd8b1a14e94eadd
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat quickradar.rb according to readability conventions

--- Casks/quickradar.rb
+++ Casks/quickradar.rb
@@ -1,8 +1,10 @@
 class Quickradar < Cask
+  version '0.8'
+  sha256 'fbe74ffe8f1615a6af64868b697321fe68b505d4f2e3eba7d5e3cf4cab082a1c'
+  
   url 'http://www.quickradar.com/0.8.zip'
   appcast 'http://www.quickradar.com/appcast.xml'
   homepage 'http://www.quickradar.com/'
-  version '0.8'
-  sha256 'fbe74ffe8f1615a6af64868b697321fe68b505d4f2e3eba7d5e3cf4cab082a1c'
+
   link 'QuickRadar.app'
 end
