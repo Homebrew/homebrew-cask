539b8c9bbdcb85943998a137c95ab938df43583f
ndr <ndr@qef.io>
2014-06-27 02:01:20 +0200

    Reformat istumbler.rb according to readability conventions

--- Casks/istumbler.rb
+++ Casks/istumbler.rb
@@ -1,7 +1,9 @@
 class Istumbler < Cask
-  url 'https://istumbler.net/beta/istumbler-100gmc5b.zip'
-  homepage 'http://istumbler.net/'
   version '100gmc5b'
   sha256 '867a62f891c8fdc31883f45c1a9d0a3bfef6786acbf0a0eec947c4efd8661eb3'
+
+  url 'https://istumbler.net/beta/istumbler-100gmc5b.zip'
+  homepage 'http://istumbler.net/'
+
   link 'iStumbler.app'
 end
