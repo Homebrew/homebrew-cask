813163f663c0cce3b0fcead126dbcb27754afee6
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat garmin-training-center.rb according to readability conventions

--- Casks/garmin-training-center.rb
+++ Casks/garmin-training-center.rb
@@ -1,8 +1,10 @@
 class GarminTrainingCenter < Cask
-  url 'https://www8.garmin.com/software/TrainingCenterforMac_321.dmg'
-  homepage 'http://www.garmin.com/garmin/cms/intosports/training_center'
   version '3.2.1'
   sha256 '82bda2f40b3ad8402f1e88fd7750b2827dc8f8f17ea45165e0c85fe6dbd7dfb6'
+
+  url 'https://www8.garmin.com/software/TrainingCenterforMac_321.dmg'
+  homepage 'http://www.garmin.com/garmin/cms/intosports/training_center'
+
   install 'Garmin Training Center.pkg'
   uninstall :pkgutil => 'com.garmin.garminTrainingCenter.*pkg'
 end
