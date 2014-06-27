1c49c2021c302e00c5df59412fac12716f07090d
ndr <ndr@qef.io>
2014-06-27 02:01:00 +0200

    Reformat betterzip.rb according to readability conventions

--- Casks/betterzip.rb
+++ Casks/betterzip.rb
@@ -1,8 +1,10 @@
 class Betterzip < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://macitbetter.com/BetterZip.zip'
   appcast 'http://macitbetter.com/BetterZip2.rss'
   homepage 'http://macitbetter.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'BetterZip.app'
 end
