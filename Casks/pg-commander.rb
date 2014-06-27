1d27973eb00396a3e8a29f6c9ca4a816cb7d102c
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat pg-commander.rb according to readability conventions

--- Casks/pg-commander.rb
+++ Casks/pg-commander.rb
@@ -1,7 +1,9 @@
 class PgCommander < Cask
-  url 'https://eggerapps.at/pgcommander/download/pgcommander-1.3.0.zip'
-  homepage 'http://eggerapps.at/pgcommander/'
   version '1.3.0'
   sha256 'ca9dcb73ada73fcd5d4896b82ca2f84f37543ea3e1f8866edc7b3bd66e97c817'
+
+  url 'https://eggerapps.at/pgcommander/download/pgcommander-1.3.0.zip'
+  homepage 'http://eggerapps.at/pgcommander/'
+
   link 'PG Commander.app'
 end
