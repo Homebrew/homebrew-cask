bc7e2526270d886344dae448d49fabdb744db4fc
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat goagentx.rb according to readability conventions

--- Casks/goagentx.rb
+++ Casks/goagentx.rb
@@ -1,8 +1,10 @@
 class Goagentx < Cask
+  version '2.3.3'
+  sha256 'a711a894d1c44d3e32bda0321adcaab4acca6219dbff798eec29ce8b40402673'
+  
   url 'https://github.com/ohdarling/GoAgentX/releases/download/rel-v2.3.3/GoAgentX-v2.3.3.dmg'
   appcast 'https://goagentx.com/update/SUAppcast.xml'
   homepage 'http://goagentx.com/'
-  version '2.3.3'
-  sha256 'a711a894d1c44d3e32bda0321adcaab4acca6219dbff798eec29ce8b40402673'
+
   link 'GoAgentX.app'
 end
