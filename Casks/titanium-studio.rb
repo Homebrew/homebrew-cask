1c09dfd44b5bd613ee3b79e2e26f993cdf024a35
ndr <ndr@qef.io>
2014-06-27 02:01:49 +0200

    Reformat titanium-studio.rb according to readability conventions

--- Casks/titanium-studio.rb
+++ Casks/titanium-studio.rb
@@ -1,8 +1,10 @@
 class TitaniumStudio < Cask
-  url 'https://titanium-studio.s3.amazonaws.com/latest/Titanium_Studio.dmg'
-  homepage 'https://my.appcelerator.com/resources'
   version 'latest'
   sha256 :no_check
+
+  url 'https://titanium-studio.s3.amazonaws.com/latest/Titanium_Studio.dmg'
+  homepage 'https://my.appcelerator.com/resources'
+
   caskroom_only true
   caveats do
     manual_installer 'Titanium Studio.app'
