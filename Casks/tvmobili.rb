f40b632593dd12191fcfb2b6c745033f8c4c23c7
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat tvmobili.rb according to readability conventions

--- Casks/tvmobili.rb
+++ Casks/tvmobili.rb
@@ -1,8 +1,10 @@
 class Tvmobili < Cask
-  url 'http://www.tvmobili.com/binaries/stable/tvmobili-mountainlion-universal.dmg'
-  homepage 'http://www.tvmobili.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.tvmobili.com/binaries/stable/tvmobili-mountainlion-universal.dmg'
+  homepage 'http://www.tvmobili.com/'
+
   install 'tvmobili-mountainlion-universal.2.1.4481.pkg'
   uninstall :pkgutil   => 'com.tvmobili.tvmobilisvcd',
             :launchctl => [
