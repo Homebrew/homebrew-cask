f78c4399648a35324bda3d6fc07bef27c88dbba0
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat slingshot.rb according to readability conventions

--- Casks/slingshot.rb
+++ Casks/slingshot.rb
@@ -1,8 +1,10 @@
 class Slingshot < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://download.airsquirrels.com/Slingshot/Mac/Slingshot.dmg'
   appcast 'https://updates.airsquirrels.com/Slingshot/Mac/Slingshot.xml'
   homepage 'http://www.airsquirrels.com/slingshot/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Slingshot.app'
 end
