7c384d5ff9841218b7452f39b758ddeaf6b9c838
ndr <ndr@qef.io>
2014-06-27 02:01:54 +0200

    Reformat xmarks-safari.rb according to readability conventions

--- Casks/xmarks-safari.rb
+++ Casks/xmarks-safari.rb
@@ -1,8 +1,10 @@
 class XmarksSafari < Cask
-  url 'https://static.xmarks.com/clients/safari/xmarks_for_safari_2.0.13.dmg'
-  homepage 'http://www.xmarks.com/'
   version '2.0.13'
   sha256 '802666565765dd5d60b6aa6d61e073cea4fb4f59a899b105b6195e7525409775'
+
+  url 'https://static.xmarks.com/clients/safari/xmarks_for_safari_2.0.13.dmg'
+  homepage 'http://www.xmarks.com/'
+
   install 'Xmarks for Safari Installer.pkg'
   uninstall :pkgutil => [
                          'com.xmarks.XmarksForSafari.pkg',
