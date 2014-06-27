05fac5929b1080a0410e8290b97f61a08d57b74e
ndr <ndr@qef.io>
2014-06-27 02:01:08 +0200

    Reformat dterm.rb according to readability conventions

--- Casks/dterm.rb
+++ Casks/dterm.rb
@@ -1,8 +1,10 @@
 class Dterm < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://files.decimus.net/DTerm/DTerm.zip'
   appcast 'http://decimus.net/appcasts/DTerm.xml'
   homepage 'http://decimus.net/DTerm'
-  version 'latest'
-  sha256 :no_check
+
   link 'DTerm.app'
 end
