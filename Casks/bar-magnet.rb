385dcb92704f1527d6eee3dcb0d48e6bae424e04
ndr <ndr@qef.io>
2014-06-27 02:00:59 +0200

    Reformat bar-magnet.rb according to readability conventions

--- Casks/bar-magnet.rb
+++ Casks/bar-magnet.rb
@@ -1,7 +1,9 @@
 class BarMagnet < Cask
-  url 'http://qata.cc/app/Bar%20Magnet%201.0.zip'
-  homepage 'http://qata.cc/Site/Bar_Magnet.html'
   version '1.0'
   sha256 'b8242cbef4ecf537770e53ae3e7a750e6241f4185898476b021f356d9328567c'
+
+  url 'http://qata.cc/app/Bar%20Magnet%201.0.zip'
+  homepage 'http://qata.cc/Site/Bar_Magnet.html'
+
   link 'Bar Magnet.app'
 end
