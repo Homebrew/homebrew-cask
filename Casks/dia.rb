4baeb74322cb5fa718e4e9158d4a91b024c4b4e0
ndr <ndr@qef.io>
2014-06-27 02:01:07 +0200

    Reformat dia.rb according to readability conventions

--- Casks/dia.rb
+++ Casks/dia.rb
@@ -1,7 +1,9 @@
 class Dia < Cask
-  url 'https://downloads.sourceforge.net/dia-installer/dia/0.97.2/Dia-0.97.2-7.dmg'
-  homepage 'http://dia-installer.de/'
   version '0.97.2-7'
   sha256 '9d3038c01347716800688830eaf52204deb78affe74a5f0c6e0a48fd414d44be'
+
+  url 'https://downloads.sourceforge.net/dia-installer/dia/0.97.2/Dia-0.97.2-7.dmg'
+  homepage 'http://dia-installer.de/'
+
   link 'Dia.app'
 end
