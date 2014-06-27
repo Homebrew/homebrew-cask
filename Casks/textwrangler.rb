8577ab3227f6c422c4eac96514d97d5a5433c599
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat textwrangler.rb according to readability conventions

--- Casks/textwrangler.rb
+++ Casks/textwrangler.rb
@@ -1,8 +1,10 @@
 class Textwrangler < Cask
+  version '4.5.8'
+  sha256 'ac63be1a83f6e611a67753e63304a2549f95e031562f6245c5688f3d307537ae'
+  
   url 'http://ash.barebones.com/TextWrangler_4.5.8.dmg'
   appcast 'https://versioncheck.barebones.com/TextWrangler.xml'
   homepage 'http://www.barebones.com/products/textwrangler'
-  version '4.5.8'
-  sha256 'ac63be1a83f6e611a67753e63304a2549f95e031562f6245c5688f3d307537ae'
+
   link 'TextWrangler.app'
 end
