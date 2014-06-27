ca45b305324376e9f6df278a06b108eda708e560
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat whatpulse.rb according to readability conventions

--- Casks/whatpulse.rb
+++ Casks/whatpulse.rb
@@ -1,8 +1,10 @@
 class Whatpulse < Cask
-  url 'http://amcdn.whatpulse.org/files/whatpulse-mac-2.4.dmg'
-  homepage 'http://www.whatpulse.org/'
   version '2.4'
   sha256 'baa7f17829cf70241845642891bceb4c4e4f276152a90e804eb271bdf1ffdbe8'
+
+  url 'http://amcdn.whatpulse.org/files/whatpulse-mac-2.4.dmg'
+  homepage 'http://www.whatpulse.org/'
+
   install 'WhatPulse 2.4.mpkg'
   uninstall :files => [
                        '/Applications/WhatPulse.app',
