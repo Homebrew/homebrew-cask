d00487f94cf7fc6168be15fc46121a4c508b2b4d
ndr <ndr@qef.io>
2014-06-27 02:01:20 +0200

    Reformat intellij-idea.rb according to readability conventions

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,8 +1,10 @@
 class IntellijIdea < Cask
-  url 'http://download-cf.jetbrains.com/idea/ideaIU-13.1.3.dmg'
-  homepage 'https://www.jetbrains.com/idea/index.html'
   version '13.1.3'
   sha256 'ae5239e0a5670dec88c39a5157103084d0ddd2c0b8d732d6af07764f0b98b624'
+
+  url 'http://download-cf.jetbrains.com/idea/ideaIU-13.1.3.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+
   link 'IntelliJ IDEA 13.app'
   caveats do
     <<-EOS.undent
