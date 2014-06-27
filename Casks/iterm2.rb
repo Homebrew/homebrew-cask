a1c01580554bea7d699ea3141b502d8b618ef676
ndr <ndr@qef.io>
2014-06-27 02:01:20 +0200

    Reformat iterm2.rb according to readability conventions

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -1,8 +1,10 @@
 class Iterm2 < Cask
   # note: "2" is not a version number, but indicates a different vendor
-  url 'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip'
-  homepage 'http://www.iterm2.com/'
   version '1.0.0'
   sha256 '2afad022b1e1f08b3ed40f0c2bde7bf7cce003852c83f85948c7f57a5578d9c5'
+
+  url 'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip'
+  homepage 'http://www.iterm2.com/'
+
   link 'iTerm.app'
 end
