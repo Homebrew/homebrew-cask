5f836823356288ca21da794e7848d8420d6eb576
ndr <ndr@qef.io>
2014-06-27 02:01:00 +0200

    Reformat bettertouchtool.rb according to readability conventions

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,8 +1,10 @@
 class Bettertouchtool < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.boastr.de/BetterTouchTool.zip'
   appcast 'http://appcast.boastr.net'
   homepage 'http://blog.boastr.net/'
-  version 'latest'
-  sha256 :no_check
+
   link 'BetterTouchTool.app'
 end
