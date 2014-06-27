ae02054f6be660c8ff2f5a7f0209569af84bc39f
ndr <ndr@qef.io>
2014-06-27 02:01:04 +0200

    Reformat cloudpull.rb according to readability conventions

--- Casks/cloudpull.rb
+++ Casks/cloudpull.rb
@@ -1,8 +1,10 @@
 class Cloudpull < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://downloads.goldenhillsoftware.com/cloudpull/CloudPull.zip'
   appcast 'https://secure.goldenhillsoftware.com/updates/cloudpull/appcast.xml'
   homepage 'http://www.goldenhillsoftware.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'CloudPull.app'
 end
