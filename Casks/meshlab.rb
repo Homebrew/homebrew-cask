288fe5798d01c683eefcbe80cd9e4a778436026c
ndr <ndr@qef.io>
2014-06-27 02:01:28 +0200

    Reformat meshlab.rb according to readability conventions

--- Casks/meshlab.rb
+++ Casks/meshlab.rb
@@ -1,7 +1,9 @@
 class Meshlab < Cask
-  url 'https://downloads.sourceforge.net/project/meshlab/meshlab/MeshLab%20v1.3.2/MeshLabMac_v132.dmg'
-  homepage 'http://meshlab.sourceforge.net/'
   version '1.3.2'
   sha256 '4ea9f5d99bf1c55c870fe75919397e6788e441e0dcd564311089eb63f93ec989'
+
+  url 'https://downloads.sourceforge.net/project/meshlab/meshlab/MeshLab%20v1.3.2/MeshLabMac_v132.dmg'
+  homepage 'http://meshlab.sourceforge.net/'
+
   link 'meshlab.app'
 end
