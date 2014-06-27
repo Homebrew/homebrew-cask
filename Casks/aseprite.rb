cb5d0f09e32d5fbdc6efcc20a8fc803eeb1005eb
ndr <ndr@qef.io>
2014-06-27 02:00:57 +0200

    Reformat aseprite.rb according to readability conventions

--- Casks/aseprite.rb
+++ Casks/aseprite.rb
@@ -1,7 +1,9 @@
 class Aseprite < Cask
-  url 'https://aseprite.googlecode.com/files/ASEPRITE_0.9.5.dmg'
-  homepage 'http://www.aseprite.org'
   version '0.9.5'
   sha256 '299eda3e5f11ae60d58bccdd26156169db6ebc01be8e806d7909f0b8f22e2928'
+
+  url 'https://aseprite.googlecode.com/files/ASEPRITE_0.9.5.dmg'
+  homepage 'http://www.aseprite.org'
+
   link 'aseprite.app'
 end
