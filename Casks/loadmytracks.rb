662e91238d4a8edf7533e4db1c876477ac30cc59
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat loadmytracks.rb according to readability conventions

--- Casks/loadmytracks.rb
+++ Casks/loadmytracks.rb
@@ -1,8 +1,10 @@
 class Loadmytracks < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.cluetrust.com/Downloads/LoadMyTracks.dmg'
   appcast 'http://www.cluetrust.com/AppCasts/LoadMyTracks.xml'
   homepage 'http://www.cluetrust.com/loadmytracks.html'
-  version 'latest'
-  sha256 :no_check
+
   link 'LoadMyTracks.app'
 end
