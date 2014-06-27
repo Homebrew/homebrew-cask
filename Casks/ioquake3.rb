440700864f015c8b2cf642e6c9be314a4a87dfd6
ndr <ndr@qef.io>
2014-06-27 02:01:20 +0200

    Reformat ioquake3.rb according to readability conventions

--- Casks/ioquake3.rb
+++ Casks/ioquake3.rb
@@ -1,8 +1,10 @@
 class Ioquake3 < Cask
-  url 'http://ioquake3.org/files/1.36/ioquake3%201.36.dmg'
-  homepage 'http://ioquake3.org/'
   version '1.36'
   sha256 'ff310471aa641cc27980055691b3e3cf5496ac262f10967c9d5052fd3815a3fc'
+
+  url 'http://ioquake3.org/files/1.36/ioquake3%201.36.dmg'
+  homepage 'http://ioquake3.org/'
+
   link 'ioquake3'
   caveats <<-EOS.undent
     To complete the installation of #{title}, you will have to copy the file
