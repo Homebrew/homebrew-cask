7e1ffd56430ad32b0361bfa885b45b415baff58a
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat liteicon.rb according to readability conventions

--- Casks/liteicon.rb
+++ Casks/liteicon.rb
@@ -1,7 +1,9 @@
 class Liteicon < Cask
-  url 'http://www.freemacsoft.net/downloads/LiteIcon_3.0b.zip'
-  homepage 'http://www.freemacsoft.net/liteicon/'
   version '3.0b'
   sha256 '84b9af7429e900be0bea4b9aa1ce9767bda85beef8d9c38dfad91984054f9abd'
+
+  url 'http://www.freemacsoft.net/downloads/LiteIcon_3.0b.zip'
+  homepage 'http://www.freemacsoft.net/liteicon/'
+
   link 'LiteIcon.app'
 end
