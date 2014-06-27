3af606a62c54f97add22f8d130edd8c47db9530c
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat lingrradar.rb according to readability conventions

--- Casks/lingrradar.rb
+++ Casks/lingrradar.rb
@@ -1,8 +1,10 @@
 class Lingrradar < Cask
+  version '2.26'
+  sha256 'c1883e5bb0d31055a34651c1315dd5fdbdc5126dc493a84ea40cf518e4ff23b6'
+  
   url 'http://radar.lingr.com/mac/LingrRadar_2.26.tbz'
   appcast 'http://radar.lingr.com/mac_appcast.xml'
   homepage 'http://radar.lingr.com/'
-  version '2.26'
-  sha256 'c1883e5bb0d31055a34651c1315dd5fdbdc5126dc493a84ea40cf518e4ff23b6'
+
   link 'LingrRadar.app'
 end
