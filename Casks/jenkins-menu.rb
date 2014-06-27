70e0f11c8cce3c8e630941dd5684938242252c87
ndr <ndr@qef.io>
2014-06-27 02:01:21 +0200

    Reformat jenkins-menu.rb according to readability conventions

--- Casks/jenkins-menu.rb
+++ Casks/jenkins-menu.rb
@@ -1,8 +1,10 @@
 class JenkinsMenu < Cask
+  version '0.1.0'
+  sha256 '8fd5c85a5615bd0470cb69bd8c28cfbfc7189f74c1abd490181bb74b166fce37'
+  
   url 'https://bitbucket.org/qvacua/qvacua/downloads/JenkinsMenu-0.1.0.zip'
   appcast 'http://qvacua.com/jenkinsmenu/appcast.xml'
   homepage 'http://qvacua.com'
-  version '0.1.0'
-  sha256 '8fd5c85a5615bd0470cb69bd8c28cfbfc7189f74c1abd490181bb74b166fce37'
+
   link 'Jenkins Menu.app'
 end
