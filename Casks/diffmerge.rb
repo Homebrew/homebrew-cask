9643ab77cd37c15f25305a59907e57a40a2f6e35
ndr <ndr@qef.io>
2014-06-27 02:01:07 +0200

    Reformat diffmerge.rb according to readability conventions

--- Casks/diffmerge.rb
+++ Casks/diffmerge.rb
@@ -1,8 +1,10 @@
 class Diffmerge < Cask
-  url 'http://download-us.sourcegear.com/DiffMerge/4.2.0/DiffMerge.4.2.0.697.intel.stable.dmg'
-  homepage 'http://www.sourcegear.com/diffmerge'
   version '4.2.0.697'
   sha256 '2c6368653af2440bb4460aef9bb1b2b5d8b84b5a3f47994c4abafc4d7ddbff9e'
+
+  url 'http://download-us.sourcegear.com/DiffMerge/4.2.0/DiffMerge.4.2.0.697.intel.stable.dmg'
+  homepage 'http://www.sourcegear.com/diffmerge'
+
   link 'DiffMerge.app'
   binary 'Extras/diffmerge.sh', :target => 'diffmerge'
 end
