336f16d4041c2c4245df6122e39f86fc544e9653
ndr <ndr@qef.io>
2014-06-27 02:01:16 +0200

    Reformat harvest.rb according to readability conventions

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -1,8 +1,10 @@
 class Harvest < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.getharvest.com/harvest/mac/Harvest.zip'
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml'
   homepage 'http://www.getharvest.com/mac'
-  version 'latest'
-  sha256 :no_check
+
   link 'Harvest.app'
 end
