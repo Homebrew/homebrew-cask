f17db5aab1f652b3338445b5cfceae8f2ad883cb
ndr <ndr@qef.io>
2014-06-27 02:01:27 +0200

    Reformat maratis.rb according to readability conventions

--- Casks/maratis.rb
+++ Casks/maratis.rb
@@ -1,8 +1,10 @@
 class Maratis < Cask
-  url 'https://maratis.googlecode.com/files/Maratis-3.21-beta-osx10.6.zip'
-  homepage 'http://www.maratis3d.org/'
   version '3.21'
   sha256 '7ec6dd97f1ce1ce09e08df0947fcb6ceb7a9f63024998f44afed3d3965829707'
+
+  url 'https://maratis.googlecode.com/files/Maratis-3.21-beta-osx10.6.zip'
+  homepage 'http://www.maratis3d.org/'
+
   link 'Maratis.app'
   link 'MaratisPlayer.app'
 end
