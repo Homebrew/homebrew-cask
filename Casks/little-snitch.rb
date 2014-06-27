4118d7640e7a9f176b96e6f1422c57103fdc6c8f
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat little-snitch.rb according to readability conventions

--- Casks/little-snitch.rb
+++ Casks/little-snitch.rb
@@ -1,8 +1,10 @@
 class LittleSnitch < Cask
-  url 'http://www.obdev.at/downloads/LittleSnitch/LittleSnitch-3.3.2.dmg'
-  homepage 'http://www.obdev.at/products/littlesnitch/index.html'
   version '3.3.2'
   sha256 '835395e13b9c96b0a5fa32ae9b9e196fb79fb54cb33f1861dd32d16774d7d3bd'
+
+  url 'http://www.obdev.at/downloads/LittleSnitch/LittleSnitch-3.3.2.dmg'
+  homepage 'http://www.obdev.at/products/littlesnitch/index.html'
+
   caveats do
     manual_installer 'Little Snitch Installer.app'
   end
