94d4b41cefbabfdee7d08c42fd179b087a0dbe02
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat musique.rb according to readability conventions

--- Casks/musique.rb
+++ Casks/musique.rb
@@ -1,8 +1,10 @@
 class Musique < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://flavio.tordini.org/files/musique/musique.dmg'
   appcast 'http://flavio.tordini.org/musique-ws/appcast.xml'
   homepage 'http://flavio.tordini.org/musique'
-  version 'latest'
-  sha256 :no_check
+
   link 'Musique.app'
 end
