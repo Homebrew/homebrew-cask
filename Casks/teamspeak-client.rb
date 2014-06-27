44ac8ea358ec8d2e5b7e6062a02fb8cfc6f83a1c
ndr <ndr@qef.io>
2014-06-27 02:01:47 +0200

    Reformat teamspeak-client.rb according to readability conventions

--- Casks/teamspeak-client.rb
+++ Casks/teamspeak-client.rb
@@ -1,7 +1,9 @@
 class TeamspeakClient < Cask
-  url 'http://dl.4players.de/ts/releases/3.0.15/TeamSpeak3-Client-macosx-3.0.15.dmg'
-  homepage 'http://www.teamspeak.com/'
   version '3.0.15'
   sha256 '69fe499b38e3ebd53339cb1ce571ea8f012b3de1553ab3fab95a04d4b3cb131a'
+
+  url 'http://dl.4players.de/ts/releases/3.0.15/TeamSpeak3-Client-macosx-3.0.15.dmg'
+  homepage 'http://www.teamspeak.com/'
+
   link 'TeamSpeak 3 Client.app'
 end
