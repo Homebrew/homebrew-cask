b6fc5b3ce06a10ce2ce02d5cce3b4e9fe20d5559
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat sketchup.rb according to readability conventions

--- Casks/sketchup.rb
+++ Casks/sketchup.rb
@@ -1,7 +1,9 @@
 class Sketchup < Cask
-  url 'https://dl.trimble.com/sketchup/SketchUpMEN.dmg'
-  homepage 'http://www.sketchup.com/intl/en/'
   version '2013'
   sha256 '0a335e432eabafeaeed000c5471a0818fd5e92e65db4e3e145f99ea261a41844'
+
+  url 'https://dl.trimble.com/sketchup/SketchUpMEN.dmg'
+  homepage 'http://www.sketchup.com/intl/en/'
+
   link 'SketchUp 2013'
 end
