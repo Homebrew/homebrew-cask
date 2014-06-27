e78a1a07507b948f90fd9901e9f9d14c3e159216
ndr <ndr@qef.io>
2014-06-27 02:01:07 +0200

    Reformat disk-inventory-x.rb according to readability conventions

--- Casks/disk-inventory-x.rb
+++ Casks/disk-inventory-x.rb
@@ -1,8 +1,10 @@
 class DiskInventoryX < Cask
+  version '1.0'
+  sha256 'f61c070a1ec8f29ee78b8a7c84dd4124553098acc87134e2ef05dbaf2a442636'
+
   url 'http://www.derlien.com/diskinventoryx/downloads/dev/DIX1.0Universal.dmg',
     :user_agent => :fake
   homepage 'http://www.derlien.com/'
-  version '1.0'
-  sha256 'f61c070a1ec8f29ee78b8a7c84dd4124553098acc87134e2ef05dbaf2a442636'
+  
   link 'Disk Inventory X.app'
 end
