6fe13bc70e888e471d17f981858fd2157718d372
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat omnipresence.rb according to readability conventions

--- Casks/omnipresence.rb
+++ Casks/omnipresence.rb
@@ -1,7 +1,9 @@
 class Omnipresence < Cask
-  url 'http://downloads.omnigroup.com/software/MacOSX/10.8/OmniPresence-1.1.dmg'
-  homepage 'http://www.omnigroup.com/omnipresence'
   version '1.1'
   sha256 '4b6304fffdeed85054379bde7b43776957b5061f0bd089f29fe3005ed19ca49f'
+
+  url 'http://downloads.omnigroup.com/software/MacOSX/10.8/OmniPresence-1.1.dmg'
+  homepage 'http://www.omnigroup.com/omnipresence'
+
   link 'OmniPresence.app'
 end
