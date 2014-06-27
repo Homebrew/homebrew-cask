1d98959e9f7e0816a10f90fa1aa18a29864b736b
ndr <ndr@qef.io>
2014-06-27 02:00:57 +0200

    Reformat appfresh.rb according to readability conventions

--- Casks/appfresh.rb
+++ Casks/appfresh.rb
@@ -1,8 +1,10 @@
 class Appfresh < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://backend.metaquark.de/download/appfresh'
   appcast 'http://backend.metaquark.de/appcast/appfresh.xml'
   homepage 'http://metaquark.de/appfresh/mac'
-  version 'latest'
-  sha256 :no_check
+
   link 'appfresh.app'
 end
