0aecb8fb5cf3c766a6eb97e4383bf087643d911d
ndr <ndr@qef.io>
2014-06-27 02:01:22 +0200

    Reformat kaleidoscope.rb according to readability conventions

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -1,9 +1,11 @@
 class Kaleidoscope < Cask
+  version '2.1.0_134'
+  sha256 'e16360273fb6d37560c7974b95728d82a0482268641753c49f903bf36445d35b'
+  
   url 'http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-2.1.0-134.zip'
   appcast 'https://updates.blackpixel.com/updates?app=ks'
   homepage 'http://www.kaleidoscopeapp.com/'
-  version '2.1.0_134'
-  sha256 'e16360273fb6d37560c7974b95728d82a0482268641753c49f903bf36445d35b'
+
   link 'Kaleidoscope.app'
   binary 'Kaleidoscope.app/Contents/Resources/bin/ksdiff'
 
