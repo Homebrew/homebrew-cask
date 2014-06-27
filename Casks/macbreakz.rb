8ccea90916915fbd0d918b3050839a6f8ff7d4fb
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat macbreakz.rb according to readability conventions

--- Casks/macbreakz.rb
+++ Casks/macbreakz.rb
@@ -1,8 +1,10 @@
 class Macbreakz < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.publicspace.net/download/MacBreakZ5.dmg'
   appcast 'http://www.publicspace.net/app/signed_mb5.xml'
   homepage 'http://www.publicspace.net/MacBreakZ/'
-  version 'latest'
-  sha256 :no_check
+
   link 'MacBreakZ 5.app'
 end
