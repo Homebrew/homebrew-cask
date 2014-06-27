41cc4ae843c1513e633dc48ed87f8c40efe12a5d
ndr <ndr@qef.io>
2014-06-27 02:01:22 +0200

    Reformat justlooking.rb according to readability conventions

--- Casks/justlooking.rb
+++ Casks/justlooking.rb
@@ -1,7 +1,9 @@
 class Justlooking < Cask
-  url 'http://chipmunkninja.com/download/JustLooking-3.3.3.dmg'
-  homepage 'http://chipmunkninja.com/JustLooking'
   version '3.3.3'
   sha256 '730b852c020498886ed061996d9fd5c13c85da707dd18dabbe4cb804afcf1786'
+
+  url 'http://chipmunkninja.com/download/JustLooking-3.3.3.dmg'
+  homepage 'http://chipmunkninja.com/JustLooking'
+
   link 'JustLooking.app'
 end
