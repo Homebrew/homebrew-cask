bd6e8306b154b3b73f24a014baa61e4fb5530296
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat music-manager.rb according to readability conventions

--- Casks/music-manager.rb
+++ Casks/music-manager.rb
@@ -1,7 +1,9 @@
 class MusicManager < Cask
-  url 'https://dl.google.com/dl/androidjumper/mac/1046528/musicmanager.dmg'
-  homepage 'https://play.google.com/music/'
   version '1.0.104.6528'
   sha256 'a1e4e48e008958f9a725bfee1e2d8360dc8efad38ef2532fc1b3e4b9c3df8f0d'
+
+  url 'https://dl.google.com/dl/androidjumper/mac/1046528/musicmanager.dmg'
+  homepage 'https://play.google.com/music/'
+
   link 'MusicManager.app', :target => 'Music Manager.app'
 end
