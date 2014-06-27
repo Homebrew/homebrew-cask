2980039aa08d2181d6e11faf198efde0602cfff0
ndr <ndr@qef.io>
2014-06-27 02:01:28 +0200

    Reformat meteorologist.rb according to readability conventions

--- Casks/meteorologist.rb
+++ Casks/meteorologist.rb
@@ -1,7 +1,9 @@
 class Meteorologist < Cask
-  url 'https://downloads.sourceforge.net/heat-meteo/Meteorologist-1.6.1.dmg'
-  homepage 'http://heat-meteo.sourceforge.net/'
   version '1.6.1'
   sha256 '15243a845062d87bf67e6a26b8da011e00c50a5f24def460cc685532d7d720a4'
+
+  url 'https://downloads.sourceforge.net/heat-meteo/Meteorologist-1.6.1.dmg'
+  homepage 'http://heat-meteo.sourceforge.net/'
+
   link 'Meteorologist.app'
 end
