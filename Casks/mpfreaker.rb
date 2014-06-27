02c4886bf935733f81405d6cfa3eb6aae8f85c06
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat mpfreaker.rb according to readability conventions

--- Casks/mpfreaker.rb
+++ Casks/mpfreaker.rb
@@ -1,8 +1,10 @@
 class Mpfreaker < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.lairware.com/download/MPFreaker.dmg'
   appcast 'http://lwupdate.dyndns.org/mpfreaker.xml'
   homepage 'http://www.lairware.com/mpfreaker/'
-  version 'latest'
-  sha256 :no_check
+
   link 'MPFreaker.app'
 end
