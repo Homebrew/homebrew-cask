4f3fbbbbf8ba4e87c31b10bd739055a1f1a26ac0
ndr <ndr@qef.io>
2014-06-27 02:01:42 +0200

    Reformat silverback.rb according to readability conventions

--- Casks/silverback.rb
+++ Casks/silverback.rb
@@ -1,8 +1,10 @@
 class Silverback < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://silverback.s3.amazonaws.com/silverback2.zip'
   appcast 'http://silverback.s3.amazonaws.com/release/appcast.xml'
   homepage 'http://silverbackapp.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Silverback.app'
 end
