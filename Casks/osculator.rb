bc7cd6727e8446ff02cc9c1ad8f3da425f56f130
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat osculator.rb according to readability conventions

--- Casks/osculator.rb
+++ Casks/osculator.rb
@@ -1,7 +1,9 @@
 class Osculator < Cask
-  url 'http://dl.osculator.net/releases/osculator-2.13.1-20-g81f7dd4.dmg'
-  homepage 'http://www.osculator.net'
   version '2.13.1'
   sha256 '3565e9044b541149630235326a441369012ac6d98c54231c23975a53dc69dca5'
+
+  url 'http://dl.osculator.net/releases/osculator-2.13.1-20-g81f7dd4.dmg'
+  homepage 'http://www.osculator.net'
+
   link 'OSCulator ƒ/OSCulator.app'
 end
