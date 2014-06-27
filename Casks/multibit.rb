62f1ee55d1cb5d25fb633344ca596615597e7d14
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat multibit.rb according to readability conventions

--- Casks/multibit.rb
+++ Casks/multibit.rb
@@ -1,7 +1,9 @@
 class Multibit < Cask
-  url 'https://multibit.org/releases/multibit-0.5.18/multibit-0.5.18.dmg'
-  homepage 'https://multibit.org/'
   version '0.5.18'
   sha256 '0d2fe6fa68385c1ca964d9588272787dabffbc2061f29ebaab422317d0972257'
+
+  url 'https://multibit.org/releases/multibit-0.5.18/multibit-0.5.18.dmg'
+  homepage 'https://multibit.org/'
+
   link 'MultiBit.app'
 end
