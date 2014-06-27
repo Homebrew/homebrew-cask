9eb6e6016eef433cf63066b83f9be7dfc0a5e3b2
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat runtimebrowser.rb according to readability conventions

--- Casks/runtimebrowser.rb
+++ Casks/runtimebrowser.rb
@@ -1,7 +1,9 @@
 class Runtimebrowser < Cask
-  url 'http://seriot.ch/temp/runtimebrowser.zip'
-  homepage 'https://github.com/nst/RuntimeBrowser'
   version 'latest'
   sha256 :no_check
+
+  url 'http://seriot.ch/temp/runtimebrowser.zip'
+  homepage 'https://github.com/nst/RuntimeBrowser'
+
   link 'RuntimeBrowser.app'
 end
