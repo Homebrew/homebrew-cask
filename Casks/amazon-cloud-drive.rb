1e5a22a00c1085536a6905339528368f2c4e0669
ndr <ndr@qef.io>
2014-06-27 02:00:56 +0200

    Reformat amazon-cloud-drive.rb according to readability conventions

--- Casks/amazon-cloud-drive.rb
+++ Casks/amazon-cloud-drive.rb
@@ -1,8 +1,10 @@
 class AmazonCloudDrive < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://d29x207vrinatv.cloudfront.net/AmazonCloudDrive.dmg'
   appcast 'https://d29x207vrinatv.cloudfront.net/Sync/Mac/20130517-2.1/CloudDriveInstallerAppcast.xml'
   homepage 'https://www.amazon.com/clouddrive'
-  version 'latest'
-  sha256 :no_check
+
   link 'Amazon Cloud Drive.app'
 end
