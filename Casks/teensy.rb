44f31b745230871eacf5ea8b72fae413b45b867f
ndr <ndr@qef.io>
2014-06-27 02:01:47 +0200

    Reformat teensy.rb according to readability conventions

--- Casks/teensy.rb
+++ Casks/teensy.rb
@@ -1,7 +1,9 @@
 class Teensy < Cask
-  url 'https://www.pjrc.com/teensy/teensy.dmg'
-  homepage 'http://pjrc.com/teensy/loader_mac.html'
   version '1.17'
   sha256 'da5a499303fb8f7ba95ff9f9a69b96f65280870ccff8f71d4dded00a5770f502'
+
+  url 'https://www.pjrc.com/teensy/teensy.dmg'
+  homepage 'http://pjrc.com/teensy/loader_mac.html'
+
   link 'teensy.app'
 end
