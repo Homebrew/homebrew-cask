c83043b89e34cc96f50aa476c3c71ef712d2ef66
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat thunderbird.rb according to readability conventions

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,7 +1,9 @@
 class Thunderbird < Cask
-  url 'https://download.mozilla.org/?product=thunderbird-24.6.0&os=osx&lang=en-US'
-  homepage 'http://www.mozilla.org/en-US/thunderbird/'
   version '24.6.0'
   sha256 'd40769e1b6f1943d712527f983e4c0be66f261008a3409515652ae4bba6da494'
+
+  url 'https://download.mozilla.org/?product=thunderbird-24.6.0&os=osx&lang=en-US'
+  homepage 'http://www.mozilla.org/en-US/thunderbird/'
+
   link 'Thunderbird.app'
 end
