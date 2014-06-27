7933db19b3c285cbb8f47075d057391ed8583476
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat pi-filler.rb according to readability conventions

--- Casks/pi-filler.rb
+++ Casks/pi-filler.rb
@@ -1,7 +1,9 @@
 class PiFiller < Cask
-  url 'http://ivanx.com/raspberrypi/files/PiFiller.zip'
-  homepage 'http://ivanx.com/raspberrypi'
   version '1.1.1'
   sha256 '566eb386dd48993bceb6d170f563adb1ff45342e42044023161631e7b17e25cb'
+
+  url 'http://ivanx.com/raspberrypi/files/PiFiller.zip'
+  homepage 'http://ivanx.com/raspberrypi'
+
   link 'Pi Filler.app'
 end
