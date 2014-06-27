a7f06b595f605337cfd1a030e076a67bf8ce2145
ndr <ndr@qef.io>
2014-06-27 02:01:17 +0200

    Reformat hpuload.rb according to readability conventions

--- Casks/hpuload.rb
+++ Casks/hpuload.rb
@@ -1,7 +1,9 @@
 class Hpuload < Cask
-  url 'http://www.fernlightning.com/lib/exe/fetch.php?id=software%3Ahpuload%3Astart&cache=cache&media=software:hpuload:hpuload.dmg'
-  homepage 'http://www.fernlightning.com/doku.php?id=software:hpuload:start'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.fernlightning.com/lib/exe/fetch.php?id=software%3Ahpuload%3Astart&cache=cache&media=software:hpuload:hpuload.dmg'
+  homepage 'http://www.fernlightning.com/doku.php?id=software:hpuload:start'
+
   link 'HPULoad.app'
 end
