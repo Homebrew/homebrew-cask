add315b9828e72e48f589974048ec7966ab5a424
ndr <ndr@qef.io>
2014-06-27 02:01:09 +0200

    Reformat emacs.rb according to readability conventions

--- Casks/emacs.rb
+++ Casks/emacs.rb
@@ -1,7 +1,9 @@
 class Emacs < Cask
-  url 'http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg'
-  homepage 'http://emacsformacosx.com/'
   version '24.3'
   sha256 '92b3a6dd0a32b432f45ea925cfa34834c9ac9f7f0384c38775f6760f1e89365a'
+
+  url 'http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg'
+  homepage 'http://emacsformacosx.com/'
+
   link 'Emacs.app'
 end
