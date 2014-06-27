b166df35bea1f171cab0576e4cdf72c2b47423d8
ndr <ndr@qef.io>
2014-06-27 02:01:30 +0200

    Reformat mucommander.rb according to readability conventions

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -1,7 +1,9 @@
 class Mucommander < Cask
-  url 'http://www.mucommander.com/download/mucommander-0_9_0.dmg'
   version '0.9.0'
-  homepage 'http://www.mucommander.com/index.php'
   sha256 '24665a248bd0f027b399277c2e7096a57e75120048302f3ac2f89ce4f7f1acae'
+
+  url 'http://www.mucommander.com/download/mucommander-0_9_0.dmg'
+  homepage 'http://www.mucommander.com/index.php'
+  
   link 'muCommander.app'
 end
