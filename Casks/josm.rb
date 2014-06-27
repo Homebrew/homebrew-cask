52ec396befd267cc1f9604b4f490e4ff8285da79
ndr <ndr@qef.io>
2014-06-27 02:01:21 +0200

    Reformat josm.rb according to readability conventions

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,7 +1,9 @@
 class Josm < Cask
-  url 'https://josm.openstreetmap.de/download/macosx/josm-macosx.zip'
-  homepage 'http://josm.openstreetmap.de'
   version 'latest'
   sha256 :no_check
+
+  url 'https://josm.openstreetmap.de/download/macosx/josm-macosx.zip'
+  homepage 'http://josm.openstreetmap.de'
+
   link 'JOSM.app'
 end
