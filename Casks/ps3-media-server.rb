394cc340709f4fe8dfc09100a081bf66c5e4e001
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat ps3-media-server.rb according to readability conventions

--- Casks/ps3-media-server.rb
+++ Casks/ps3-media-server.rb
@@ -1,8 +1,10 @@
 class Ps3MediaServer < Cask
-  url 'https://downloads.sourceforge.net/project/ps3mediaserver/pms-1.90.1-setup-macosx.tar.gz'
-  homepage 'http://www.ps3mediaserver.org/'
   version '1.90.1'
   sha256 '3ebe75ce0dbdc1313c10fb901f845564cc343dc3b7487f07e15db9d757850df5'
+
+  url 'https://downloads.sourceforge.net/project/ps3mediaserver/pms-1.90.1-setup-macosx.tar.gz'
+  homepage 'http://www.ps3mediaserver.org/'
+
   caskroom_only true
   caveats do
     manual_installer 'PS3 Media Server Setup.app'
