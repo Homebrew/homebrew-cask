f5c25f2628911f8b448de0d80923809695653f81
ndr <ndr@qef.io>
2014-06-27 02:01:02 +0200

    Reformat ccleaner.rb according to readability conventions

--- Casks/ccleaner.rb
+++ Casks/ccleaner.rb
@@ -1,7 +1,9 @@
 class Ccleaner < Cask
-  url 'http://download.piriform.com/mac/CCMacSetup107.dmg'
-  homepage 'http://www.piriform.com/ccleaner'
   version '1.07.236'
   sha256 'add42bb7d3fdf6bd3cda0d1ecf8bf7ddcb927a510997261ba5ba2d1bdfa98f6e'
+
+  url 'http://download.piriform.com/mac/CCMacSetup107.dmg'
+  homepage 'http://www.piriform.com/ccleaner'
+
   link 'CCleaner.app'
 end
