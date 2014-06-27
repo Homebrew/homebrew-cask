b259a99f2248da4bffd44dd545bde20d20397920
ndr <ndr@qef.io>
2014-06-27 02:01:06 +0200

    Reformat ctivo.rb according to readability conventions

--- Casks/ctivo.rb
+++ Casks/ctivo.rb
@@ -1,8 +1,10 @@
 class Ctivo < Cask
+  version '2.1'
+  sha256 'fda20c1d4cbd465e9e404c29ccf716b080179afd87cf42a090ad35f98d934b25'
+  
   url 'https://ctivo.googlecode.com/files/cTiVo_2.1_439.zip'
   appcast 'http://ctivo.googlecode.com/svn/trunk/update/sparklecast.xml'
   homepage 'https://code.google.com/p/ctivo/'
-  version '2.1'
-  sha256 'fda20c1d4cbd465e9e404c29ccf716b080179afd87cf42a090ad35f98d934b25'
+
   link 'cTiVo.app'
 end
