da4e47a39024c8d336e410810279a9e5f8f4c490
ndr <ndr@qef.io>
2014-06-27 02:01:31 +0200

    Reformat netshade.rb according to readability conventions

--- Casks/netshade.rb
+++ Casks/netshade.rb
@@ -1,8 +1,10 @@
 class Netshade < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://raynersoftware.com/downloads/NetShade.app.zip'
   appcast 'https://secure.raynersw.com/appcast.php'
   homepage 'http://raynersoftware.com/netshade/'
-  version 'latest'
-  sha256 :no_check
+
   link 'NetShade.app'
 end
