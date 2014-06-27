348a2a8a614748d237166824e6f9b59bb9682a42
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat rftg.rb according to readability conventions

--- Casks/rftg.rb
+++ Casks/rftg.rb
@@ -1,7 +1,9 @@
 class Rftg < Cask
-  url 'https://dl.dropbox.com/u/51639697/rftg-osx-081.zip'
-  homepage 'http://keldon.net/rftg/'
   version '0.8.1'
   sha256 '0cc507f0713d33aa7fdc5d26f86a7384d5b55d94506b2a8cfec994ac6882fab1'
+
+  url 'https://dl.dropbox.com/u/51639697/rftg-osx-081.zip'
+  homepage 'http://keldon.net/rftg/'
+
   link 'RFTG.app'
 end
