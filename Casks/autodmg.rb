0e16dd3d41ee39ec008312d5c2b12c992789a58b
ndr <ndr@qef.io>
2014-06-27 02:00:58 +0200

    Reformat autodmg.rb according to readability conventions

--- Casks/autodmg.rb
+++ Casks/autodmg.rb
@@ -1,7 +1,9 @@
 class Autodmg < Cask
-  url 'https://github.com/MagerValp/AutoDMG/releases/download/v1.4.3/AutoDMG-1.4.3.dmg'
-  homepage 'https://github.com/MagerValp/AutoDMG'
   version '1.4.3'
   sha256 '62233d162ccf2109bf0a03f1e74c7c74da323958ad1f13a0c06f18a611e44592'
+
+  url 'https://github.com/MagerValp/AutoDMG/releases/download/v1.4.3/AutoDMG-1.4.3.dmg'
+  homepage 'https://github.com/MagerValp/AutoDMG'
+
   link 'AutoDMG.app'
 end
