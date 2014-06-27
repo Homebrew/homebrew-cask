ac2571f985610f5db2d2624735e67ade9d6fd65a
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat paparazzi.rb according to readability conventions

--- Casks/paparazzi.rb
+++ Casks/paparazzi.rb
@@ -1,8 +1,10 @@
 class Paparazzi < Cask
+  version '0.6.7'
+  sha256 '111c94994176a7690512aa5303bff14761f308c720b527b541e39edd054777bd'
+  
   url 'https://derailer.org/paparazzi/Paparazzi!%200.6.7.dmg'
   appcast 'https://derailer.org/paparazzi/appcast/'
   homepage 'http://derailer.org/paparazzi/'
-  version '0.6.7'
-  sha256 '111c94994176a7690512aa5303bff14761f308c720b527b541e39edd054777bd'
+
   link 'Paparazzi!.app'
 end
