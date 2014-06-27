8ec118b5ee70589448f968f42f41bde97b9ece21
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat omniplan.rb according to readability conventions

--- Casks/omniplan.rb
+++ Casks/omniplan.rb
@@ -1,7 +1,9 @@
 class Omniplan < Cask
-  url 'https://www.omnigroup.com/download/latest/omniplan'
-  homepage 'http://www.omnigroup.com/products/omniplan/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.omnigroup.com/download/latest/omniplan'
+  homepage 'http://www.omnigroup.com/products/omniplan/'
+
   link 'OmniPlan.app'
 end
