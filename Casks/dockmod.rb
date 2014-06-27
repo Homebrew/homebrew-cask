a7a18eabb10ca1d23fc7f5595a4dd6e0b7c0a491
ndr <ndr@qef.io>
2014-06-27 02:01:08 +0200

    Reformat dockmod.rb according to readability conventions

--- Casks/dockmod.rb
+++ Casks/dockmod.rb
@@ -1,8 +1,10 @@
 class Dockmod < Cask
+  version '2.03'
+  sha256 'e7a018f3d503f8124b8dc39c5b1113d0477f1d2f04aeb71ecefc67a405ee2034'
+  
   url 'http://spyresoft.com/dockmod/download.php?version=2.03'
   appcast 'http://spyresoft.com/dockmod/updates.xml'
   homepage 'http://spyresoft.com/dockmod/'
-  version '2.03'
-  sha256 'e7a018f3d503f8124b8dc39c5b1113d0477f1d2f04aeb71ecefc67a405ee2034'
+
   link 'DockMod.app'
 end
