ae8c9e9d19bda28b77460e4b9163b5fc5c8f03d5
ndr <ndr@qef.io>
2014-06-27 02:01:21 +0200

    Reformat julia-studio.rb according to readability conventions

--- Casks/julia-studio.rb
+++ Casks/julia-studio.rb
@@ -1,8 +1,10 @@
 class JuliaStudio < Cask
-  url 'https://s3.amazonaws.com/cdn-common.forio.com/julia-studio/0.4.4/julia-studio-macx-installer-0.4.4.dmg'
-  homepage 'http://forio.com/products/julia-studio/'
   version '0.4.4'
   sha256 'ce165f2ff8b1fd76ad3d1f4cb822dbf23c4d87390e6b4089b0ea17ab18bfd3ed'
+
+  url 'https://s3.amazonaws.com/cdn-common.forio.com/julia-studio/0.4.4/julia-studio-macx-installer-0.4.4.dmg'
+  homepage 'http://forio.com/products/julia-studio/'
+
   link 'JuliaStudio.app'
   caveats do
     os_version_only '10.7', '10.8', '10.9'
