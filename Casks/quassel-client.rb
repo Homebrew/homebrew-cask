269ec59a313031c71ae12156ef5e64df8025c83f
ndr <ndr@qef.io>
2014-06-27 02:01:38 +0200

    Reformat quassel-client.rb according to readability conventions

--- Casks/quassel-client.rb
+++ Casks/quassel-client.rb
@@ -1,7 +1,9 @@
 class QuasselClient < Cask
-  url 'http://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_0.9.2.dmg'
-  homepage 'http://quassel-irc.org'
   version '0.9.2'
   sha256 'bdb91065020c56cdb60c252838795e57c8859bd916e29d9559aea47b48b100e8'
+
+  url 'http://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_0.9.2.dmg'
+  homepage 'http://quassel-irc.org'
+
   link 'Quassel Client.app'
 end
