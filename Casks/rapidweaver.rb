1fb4cad8a10247ea9eac92b69918d3190a994dfd
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat rapidweaver.rb according to readability conventions

--- Casks/rapidweaver.rb
+++ Casks/rapidweaver.rb
@@ -1,8 +1,10 @@
 class Rapidweaver < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://realmacsoftware.com/redirects/rapidweaver/direct'
   appcast 'http://www.realmacsoftware.com/stats/rapidweaver5.php'
   homepage 'http://realmacsoftware.com/rapidweaver'
-  version 'latest'
-  sha256 :no_check
+
   link 'RapidWeaver.app'
 end
