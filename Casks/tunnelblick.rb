558f256a4b76f03f86acebfa945883011fcdaab5
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat tunnelblick.rb according to readability conventions

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -1,9 +1,11 @@
 class Tunnelblick < Cask
+  version '3.3.4'
+  sha256 'c14e4b2d4e30679851de7701c8a0b3167158c5ce029c59b49e49eb58946de913'
+  
   url 'https://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_3.3.4.dmg'
   appcast 'https://www.tunnelblick.net/appcast.rss'
   homepage 'https://code.google.com/p/tunnelblick/'
-  version '3.3.4'
-  sha256 'c14e4b2d4e30679851de7701c8a0b3167158c5ce029c59b49e49eb58946de913'
+
   link 'Tunnelblick.app'
   caveats 'For security reasons, Tunnelblick must be installed to /Applications and will request to be moved at launch.'
 end
