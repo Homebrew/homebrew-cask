2694c31d0744656baf412893996f06d38ec601a3
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat linein.rb according to readability conventions

--- Casks/linein.rb
+++ Casks/linein.rb
@@ -1,7 +1,9 @@
 class Linein < Cask
-  url 'https://www.rogueamoeba.com/freebies/download/LineIn.zip'
-  homepage 'http://www.rogueamoeba.com/freebies/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.rogueamoeba.com/freebies/download/LineIn.zip'
+  homepage 'http://www.rogueamoeba.com/freebies/'
+
   link 'LineIn.app'
 end
