e1d0af79548311bfe2acf3122277b92e9fc76db8
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat makemkv.rb according to readability conventions

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,7 +1,9 @@
 class Makemkv < Cask
-  url 'http://www.makemkv.com/download/makemkv_v1.8.10_osx.dmg'
-  homepage 'http://www.makemkv.com/'
   version '1.8.10'
   sha256 '143bec1c7a2e073450dd60b77fd927cb2c097af5ada47949fe14deb38e9a0421'
+
+  url 'http://www.makemkv.com/download/makemkv_v1.8.10_osx.dmg'
+  homepage 'http://www.makemkv.com/'
+
   link 'MakeMKV.app'
 end
