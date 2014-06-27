3d25e7b206e824a6f5444182cfce6e5aa74119fc
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat makehuman.rb according to readability conventions

--- Casks/makehuman.rb
+++ Casks/makehuman.rb
@@ -1,7 +1,9 @@
 class Makehuman < Cask
-  url 'http://download.tuxfamily.org/makehuman/releases/1.0.0/makehuman-1.0.0-osx.dmg'
-  homepage 'http://www.makehuman.org/'
   version '1.0.0'
   sha256 'f104963fc5fcf627069324cc3d3b9dc7bae65608fd9b7354fb914a1f3c755e70'
+
+  url 'http://download.tuxfamily.org/makehuman/releases/1.0.0/makehuman-1.0.0-osx.dmg'
+  homepage 'http://www.makehuman.org/'
+
   link 'MakeHuman.app'
 end
