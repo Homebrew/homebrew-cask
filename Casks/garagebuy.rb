7d1cc3376848c32f02d991bbd378e5dcdcc0d2d6
ndr <ndr@qef.io>
2014-06-27 02:01:13 +0200

    Reformat garagebuy.rb according to readability conventions

--- Casks/garagebuy.rb
+++ Casks/garagebuy.rb
@@ -1,7 +1,9 @@
 class Garagebuy < Cask
-  url 'http://www.iwascoding.de/downloads/GarageBuy_3.1.1.dmg'
-  homepage 'http://www.iwascoding.com/GarageBuy'
   version '3.1.1'
   sha256 '4f7b7b941a40be516d923338df84a43073149f785d123ea2ff938f412757f8cf'
+
+  url 'http://www.iwascoding.de/downloads/GarageBuy_3.1.1.dmg'
+  homepage 'http://www.iwascoding.com/GarageBuy'
+
   link 'GarageBuy.app'
 end
