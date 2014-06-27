698965747738e438fb010a0827e98010d7a727d0
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat linkinus.rb according to readability conventions

--- Casks/linkinus.rb
+++ Casks/linkinus.rb
@@ -1,7 +1,9 @@
 class Linkinus < Cask
-  url 'http://conceited.net/downloads/Linkinus.zip'
-  homepage 'http://conceited.net/products/linkinus'
   version 'latest'
   sha256 :no_check
+
+  url 'http://conceited.net/downloads/Linkinus.zip'
+  homepage 'http://conceited.net/products/linkinus'
+
   link 'Linkinus.app'
 end
