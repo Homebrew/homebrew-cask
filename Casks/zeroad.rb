2abf6ad77ad6e4ffe09036ccd05b8c5019f185fc
ndr <ndr@qef.io>
2014-06-27 02:01:55 +0200

    Reformat zeroad.rb according to readability conventions

--- Casks/zeroad.rb
+++ Casks/zeroad.rb
@@ -1,7 +1,9 @@
 class Zeroad < Cask
-  url 'http://releases.wildfiregames.com/0ad-0.0.15-alpha-osx64.dmg'
-  homepage 'http://www.play0ad.com/'
   version '0.0.15-alpha'
   sha256 '7593c7d0620288c36bf4cebde946d6fd3bef17970f5fa1c66c1d6e92443f6f3e'
+
+  url 'http://releases.wildfiregames.com/0ad-0.0.15-alpha-osx64.dmg'
+  homepage 'http://www.play0ad.com/'
+
   link '0ad.app'
 end
