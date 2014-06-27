e2f775057f4afb305946e957035545b5895475ff
ndr <ndr@qef.io>
2014-06-27 02:01:00 +0200

    Reformat blender.rb according to readability conventions

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,8 +1,10 @@
 class Blender < Cask
-  url 'http://download.blender.org/release/Blender2.70/blender-2.70a-OSX_10.6-x86_64.zip'
-  homepage 'http://www.blender.org/'
   version '2.70a'
   sha256 '590461502fe257883c78be3d6cf0eb329bced07c9aeea499bb812e2d55c3e8ba'
+
+  url 'http://download.blender.org/release/Blender2.70/blender-2.70a-OSX_10.6-x86_64.zip'
+  homepage 'http://www.blender.org/'
+
   link 'Blender/blender.app'
   link 'Blender/blenderplayer.app'
 end
