9da6bf0509110a9930e995b71b04d7f3c2cbe628
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat wineskin-winery.rb according to readability conventions

--- Casks/wineskin-winery.rb
+++ Casks/wineskin-winery.rb
@@ -1,7 +1,9 @@
 class WineskinWinery < Cask
-  url 'https://downloads.sourceforge.net/project/wineskin/Wineskin%20Winery.app%20Version%201.7.zip'
-  homepage 'http://wineskin.urgesoftware.com/'
   version '1.7'
   sha256 'ef3ae1fe17a7bc622a59171985f304f506aea7ca0ad342281536dac8609eac32'
+
+  url 'https://downloads.sourceforge.net/project/wineskin/Wineskin%20Winery.app%20Version%201.7.zip'
+  homepage 'http://wineskin.urgesoftware.com/'
+
   link 'Wineskin Winery.app'
 end
