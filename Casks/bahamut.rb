3a4050f4ac3909ce6018ade7f71f6bf7e94487a1
ndr <ndr@qef.io>
2014-06-27 02:00:59 +0200

    Reformat bahamut.rb according to readability conventions

--- Casks/bahamut.rb
+++ Casks/bahamut.rb
@@ -1,8 +1,10 @@
 class Bahamut < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://raw.github.com/sdegutis/bahamut/master/Builds/Bahamut-LATEST.app.tar.gz'
   appcast 'https://raw.github.com/sdegutis/bahamut/master/appcast.xml'
   homepage 'https://github.com/sdegutis/bahamut'
-  version 'latest'
-  sha256 :no_check
+
   link 'Bahamut.app'
 end
