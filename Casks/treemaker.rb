13808861f9cc693382b63fbe3c0cff592bcb56bb
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat treemaker.rb according to readability conventions

--- Casks/treemaker.rb
+++ Casks/treemaker.rb
@@ -1,7 +1,9 @@
 class Treemaker < Cask
-  url 'http://www.langorigami.com/science/computational/treemaker/TreeMaker.dmg'
-  homepage 'http://www.langorigami.com/science/computational/treemaker/treemaker.php'
   version '5.0.1'
   sha256 '2377c8c4277890de9f746eaa9d021f89fdd52cabfe9ab82fdfa741a47ed1f554'
+
+  url 'http://www.langorigami.com/science/computational/treemaker/TreeMaker.dmg'
+  homepage 'http://www.langorigami.com/science/computational/treemaker/treemaker.php'
+
   link 'TreeMaker 5.app'
 end
