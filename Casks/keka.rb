1ac910f61eea9aa6e9a58b512b299fae43569959
ndr <ndr@qef.io>
2014-06-27 02:01:22 +0200

    Reformat keka.rb according to readability conventions

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -1,8 +1,10 @@
 class Keka < Cask
+  version '1.0.4'
+  sha256 '0075741ed52e2c86d7749dfe2baf54c8b6dad75a780b4b51ca5fb14337124701'
+  
   url 'http://www.kekaosx.com/release/Keka-1.0.4-intel.dmg'
   appcast 'http://update.kekaosx.com'
   homepage 'http://kekaosx.com/'
-  version '1.0.4'
-  sha256 '0075741ed52e2c86d7749dfe2baf54c8b6dad75a780b4b51ca5fb14337124701'
+
   link 'Keka.app'
 end
