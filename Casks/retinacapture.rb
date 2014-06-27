8a40d5ba28ddf4aa984555d2c0c883bc560abeed
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat retinacapture.rb according to readability conventions

--- Casks/retinacapture.rb
+++ Casks/retinacapture.rb
@@ -1,7 +1,9 @@
 class Retinacapture < Cask
-  url 'http://www.retinacapture.com/files/RetinaCapture.zip'
-  homepage 'http://www.retinacapture.com'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.retinacapture.com/files/RetinaCapture.zip'
+  homepage 'http://www.retinacapture.com'
+
   link 'RetinaCapture.app'
 end
