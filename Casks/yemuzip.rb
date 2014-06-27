2b055557106cf44ec958e7715b2045fb4054f959
ndr <ndr@qef.io>
2014-06-27 02:01:55 +0200

    Reformat yemuzip.rb according to readability conventions

--- Casks/yemuzip.rb
+++ Casks/yemuzip.rb
@@ -1,8 +1,10 @@
 class Yemuzip < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.yellowmug.com/download/YemuZip.dmg'
   appcast 'http://yellowmug.com/yemuzip/appcast.xml'
   homepage 'http://www.yellowmug.com/yemuzip'
-  version 'latest'
-  sha256 :no_check
+
   link 'YemuZip.app'
 end
