dae4f1df239b5a3aa89afe18cd096a0fcc0d030d
ndr <ndr@qef.io>
2014-06-27 02:01:08 +0200

    Reformat doubanradio.rb according to readability conventions

--- Casks/doubanradio.rb
+++ Casks/doubanradio.rb
@@ -1,8 +1,10 @@
 class Doubanradio < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://douban.fm/desktop_mac'
   appcast 'http://douban.fm/static/radio/desktop/doubanradio-appcast.xml'
   homepage 'http://douban.fm'
-  version 'latest'
-  sha256 :no_check
+
   link 'DoubanRadio.app'
 end
