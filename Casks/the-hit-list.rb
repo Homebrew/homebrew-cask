fdc4b81fadefd4ae36f6de20f1bd7c4c5eaab641
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat the-hit-list.rb according to readability conventions

--- Casks/the-hit-list.rb
+++ Casks/the-hit-list.rb
@@ -1,8 +1,10 @@
 class TheHitList < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.potionfactory.com/files/thehitlist/TheHitList.zip'
   appcast 'http://www.potionfactory.com/appcast/thehitlist.php'
   homepage 'http://www.potionfactory.com/thehitlist'
-  version 'latest'
-  sha256 :no_check
+
   link 'The Hit List.app'
 end
