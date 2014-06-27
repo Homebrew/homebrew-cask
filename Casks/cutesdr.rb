ea050362f4ad2d9ae8b27bb66462e67a1c91863d
ndr <ndr@qef.io>
2014-06-27 02:01:06 +0200

    Reformat cutesdr.rb according to readability conventions

--- Casks/cutesdr.rb
+++ Casks/cutesdr.rb
@@ -1,7 +1,9 @@
 class Cutesdr < Cask
-  url 'https://downloads.sourceforge.net/project/cutesdr/CuteSdr111.dmg'
-  homepage 'http://sourceforge.net/projects/cutesdr'
   version '1.11'
   sha256 'feabe63c84e0227c0bacbb4d5b22c6e269fe29f7ab834c28a656d9fb33cd10b3'
+
+  url 'https://downloads.sourceforge.net/project/cutesdr/CuteSdr111.dmg'
+  homepage 'http://sourceforge.net/projects/cutesdr'
+
   link 'cutesdr.app'
 end
