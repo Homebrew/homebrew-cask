31a2239e6c5419546f227a4b0251d1cc851d3841
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat magican.rb according to readability conventions

--- Casks/magican.rb
+++ Casks/magican.rb
@@ -1,8 +1,10 @@
 class Magican < Cask
-  url 'http://www.magicansoft.com/download/Magican.pkg'
-  homepage 'http://www.magicansoft.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.magicansoft.com/download/Magican.pkg'
+  homepage 'http://www.magicansoft.com/'
+
   install 'Magican.pkg'
   uninstall :pkgutil => 'com.magican.*'
 end
