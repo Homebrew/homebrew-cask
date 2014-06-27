cc8ef6f8252fcfc738edba120dc45663d6e50705
Richard Lee <dlackty@gmail.com>
2014-06-27 11:33:07 +0800

    Update MacDown to 0.1.1

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -1,8 +1,8 @@
 class Macdown < Cask
-  version '0.1'
-  sha256 'd050ee502fb27fd0f431abc1cc2f4a490670346821e269415d184bf08f245eb2'
+  version '0.1.1'
+  sha256 'b716963c19b006421d770350139e822e8f891d259a1fe999067854150176268a'
 
-  url 'http://macdown.uranusjr.com/download/v0.1/'
+  url 'http://macdown.uranusjr.com/download/v0.1.1/'
   appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml'
   homepage 'http://macdown.uranusjr.com/'
 
