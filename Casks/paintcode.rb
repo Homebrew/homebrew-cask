92f2b616a660ecfbaea3d6843becf05f861c8760
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat paintcode.rb according to readability conventions

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -1,8 +1,10 @@
 class Paintcode < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.pixelcut.com/paintcode/paintcode.zip'
   appcast 'http://www.pixelcut.com/paintcode/appcast.xml'
   homepage 'http://www.paintcodeapp.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'PaintCode.app'
 end
