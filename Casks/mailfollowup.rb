acf656e47fa995eadb6bbe0de221d8b8361bd04a
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat mailfollowup.rb according to readability conventions

--- Casks/mailfollowup.rb
+++ Casks/mailfollowup.rb
@@ -1,8 +1,10 @@
 class Mailfollowup < Cask
-  url 'https://www.cs.unc.edu/~welch/MailFollowup/media/MailFollowUp_1.6.2.dmg.zip'
-  homepage 'http://www.cs.unc.edu/~welch/MailFollowup/'
   version '1.6.2'
   sha256 'f656c121e04fac6185b24a96888613729c366275151c891258b634c71372b2af'
+
+  url 'https://www.cs.unc.edu/~welch/MailFollowup/media/MailFollowUp_1.6.2.dmg.zip'
+  homepage 'http://www.cs.unc.edu/~welch/MailFollowup/'
+
   nested_container 'MailFollowUp_1.6.2.dmg'
   install 'Install MailFollowUp.pkg'
 
