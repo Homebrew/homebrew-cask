0238460b14ef78fcc1362d79fb0d62cc22b5402b
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat clamxav.rb according to readability conventions

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -1,9 +1,11 @@
 class Clamxav < Cask
+  version '2.6.3'
+  sha256 '34791173e31b297353025013e0296f7014e34310542a6e1cdf46e83a0254691f'
+  
   url 'http://www.clamxav.com/downloads/ClamXav_2.6.3.dmg'
   appcast 'http://www.clamxav.com/sparkle/profileInfo.php'
   homepage 'http://www.clamxav.com/'
-  version '2.6.3'
-  sha256 '34791173e31b297353025013e0296f7014e34310542a6e1cdf46e83a0254691f'
+
   link 'ClamXav.app'
   after_install do
     # Don't ask to move the app bundle to /Applications
