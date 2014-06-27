187408cea4894bb9117208536bc08ef472f724da
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat glimmerblocker.rb according to readability conventions

--- Casks/glimmerblocker.rb
+++ Casks/glimmerblocker.rb
@@ -1,8 +1,10 @@
 class Glimmerblocker < Cask
-  url 'http://glimmerblocker.org/downloads/GlimmerBlocker-1.5.3.dmg'
-  homepage 'http://glimmerblocker.org'
   version '1.5.3'
   sha256 '872f3edc5f6dc3b92ba17eaf00236308e561bf353ffb1579cc5d7afc27bbf0a5'
+
+  url 'http://glimmerblocker.org/downloads/GlimmerBlocker-1.5.3.dmg'
+  homepage 'http://glimmerblocker.org'
+
   install 'GlimmerBlocker.pkg'
   uninstall :pkgutil => 'org.glimmerblocker.pkg'
 end
