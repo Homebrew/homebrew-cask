1674b86b97cf62299f44b9f6def8b2ba531afd87
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat outwit-hub.rb according to readability conventions

--- Casks/outwit-hub.rb
+++ Casks/outwit-hub.rb
@@ -1,7 +1,9 @@
 class OutwitHub < Cask
-  url 'https://www.outwit.com/downloads/release/outwit-hub.en-US.mac64.dmg'
-  homepage 'http://www.outwit.com'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.outwit.com/downloads/release/outwit-hub.en-US.mac64.dmg'
+  homepage 'http://www.outwit.com'
+
   link 'Outwit Hub.app'
 end
