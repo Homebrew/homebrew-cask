1ed334d90970b3973587022fc1d62c6e0dfa36c4
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat nodeclipse.rb according to readability conventions

--- Casks/nodeclipse.rb
+++ Casks/nodeclipse.rb
@@ -1,8 +1,10 @@
 class Nodeclipse < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/nodeclipse/Enide-Studio-2014-011-20140228-macosx-cocoa-x86_64.zip'
-  homepage 'http://www.nodeclipse.org/'
   version '0.11-preview'
   sha256 '01f630446313cb981ce2ee9b934977cfdbf318e09761dee244a3256f9a559003'
+
+  url 'https://downloads.sourceforge.net/sourceforge/nodeclipse/Enide-Studio-2014-011-20140228-macosx-cocoa-x86_64.zip'
+  homepage 'http://www.nodeclipse.org/'
+
   before_install do
     system '/bin/mv', '--', destination_path.join('eclipse/Eclipse.app'), destination_path.join('eclipse/Nodeclipse.app')
   end
