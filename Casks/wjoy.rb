803143ae327a710445dbf8e86d7f84367de2409b
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat wjoy.rb according to readability conventions

--- Casks/wjoy.rb
+++ Casks/wjoy.rb
@@ -1,7 +1,9 @@
 class Wjoy < Cask
-  url 'https://wjoy.googlecode.com/files/wjoy%200.7.1.dmg'
-  homepage 'https://code.google.com/p/wjoy/'
   version '0.7.1'
   sha256 '98a9f825634b09b901ad979a6cdda241a04a1cbd7dcd14b4b17b0bebe3f40ee1'
+
+  url 'https://wjoy.googlecode.com/files/wjoy%200.7.1.dmg'
+  homepage 'https://code.google.com/p/wjoy/'
+
   link 'Wjoy.app'
 end
