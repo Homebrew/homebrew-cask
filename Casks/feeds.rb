4560279e6b7bf4f0343b74539a3b0046b19d20df
ndr <ndr@qef.io>
2014-06-27 02:01:11 +0200

    Reformat feeds.rb according to readability conventions

--- Casks/feeds.rb
+++ Casks/feeds.rb
@@ -1,8 +1,10 @@
 class Feeds < Cask
+  version '2.0.5.1'
+  sha256 'abc72e4ef77a0bef4ea89d62b570698d47b197f2d56b74a48a9f3da52c6cd71e'
+  
   url 'https://feedswww.appspot.com/releases/Feeds-2.0.5.1.zip'
   appcast 'https://feedswww.appspot.com/appcast.xml'
   homepage 'http://www.feedsapp.com/'
-  version '2.0.5.1'
-  sha256 'abc72e4ef77a0bef4ea89d62b570698d47b197f2d56b74a48a9f3da52c6cd71e'
+
   link 'Feeds.app'
 end
