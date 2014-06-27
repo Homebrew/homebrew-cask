267b07273e23388e16b0b92515cd8cf4fc8d648b
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat opera.rb according to readability conventions

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,7 +1,9 @@
 class Opera < Cask
-  url 'http://get.geo.opera.com/pub/opera/desktop/22.0.1471.50/mac/Opera_22.0.1471.50_Setup.dmg'
-  homepage 'http://www.opera.com/'
   version '22.0.1471.50'
   sha256 '33906fef881e3b6df5d750fe15ac1a90908122cc01d7bf25cb6f22ac347f3082'
+
+  url 'http://get.geo.opera.com/pub/opera/desktop/22.0.1471.50/mac/Opera_22.0.1471.50_Setup.dmg'
+  homepage 'http://www.opera.com/'
+
   link 'Opera.app'
 end
