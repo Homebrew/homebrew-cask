1521415e75572481055fc88424feb00007dc5823
ndr <ndr@qef.io>
2014-06-27 02:01:11 +0200

    Reformat feeder.rb according to readability conventions

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -1,8 +1,10 @@
 class Feeder < Cask
+  version '2.5.1'
+  sha256 '8c3334e7eccff45006707c44257eb5ab7946b6c4f51dae0fecf8a3552c6168ae'
+  
   url 'http://reinventedsoftware.com/feeder/downloads/Feeder_2.5.1.dmg'
   appcast 'http://reinventedsoftware.com/feeder/downloads/Feeder25.xml'
   homepage 'http://reinventedsoftware.com/feeder/'
-  version '2.5.1'
-  sha256 '8c3334e7eccff45006707c44257eb5ab7946b6c4f51dae0fecf8a3552c6168ae'
+
   link 'Feeder.app'
 end
