5e3f8c6c2802414195247cdd51d3d8a2a0d39a39
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat tiled.rb according to readability conventions

--- Casks/tiled.rb
+++ Casks/tiled.rb
@@ -1,7 +1,9 @@
 class Tiled < Cask
-  url 'https://downloads.sourceforge.net/project/tiled/tiled-qt/0.9.1/tiled-qt-0.9.1.dmg'
-  homepage 'http://www.mapeditor.org/'
   version '0.9.1'
   sha256 '17560ca54389c077e80636ac96ddb905d2f5a3a1932562a0be6d91c76bf395a1'
+
+  url 'https://downloads.sourceforge.net/project/tiled/tiled-qt/0.9.1/tiled-qt-0.9.1.dmg'
+  homepage 'http://www.mapeditor.org/'
+
   link 'Tiled.app'
 end
