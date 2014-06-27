88b63b5bc6194f61059c9edd76d0ad27d61252de
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat virtaal.rb according to readability conventions

--- Casks/virtaal.rb
+++ Casks/virtaal.rb
@@ -1,7 +1,9 @@
 class Virtaal < Cask
-  url 'https://downloads.sourceforge.net/project/translate/Virtaal/0.7.1/Virtaal-0.7.1-Mac-Beta-2.dmg'
-  homepage 'http://virtaal.translatehouse.org/'
   version '0.7.1b2'
   sha256 '41fec069ca06eb627c75b8d66460110b7970b0894e19df4f41510ac7b91bdbd0'
+
+  url 'https://downloads.sourceforge.net/project/translate/Virtaal/0.7.1/Virtaal-0.7.1-Mac-Beta-2.dmg'
+  homepage 'http://virtaal.translatehouse.org/'
+
   link 'Virtaal.app'
 end
