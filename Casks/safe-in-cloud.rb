8ebfbefb0185a006e70bf54c884e500cadd93ef1
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat safe-in-cloud.rb according to readability conventions

--- Casks/safe-in-cloud.rb
+++ Casks/safe-in-cloud.rb
@@ -1,7 +1,9 @@
 class SafeInCloud < Cask
-  url 'http://www.safe-in-cloud.com/images/downloads/Safe%20In%20Cloud.dmg'
-  homepage 'http://www.safe-in-cloud.com'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.safe-in-cloud.com/images/downloads/Safe%20In%20Cloud.dmg'
+  homepage 'http://www.safe-in-cloud.com'
+
   link 'Safe In Cloud.app'
 end
