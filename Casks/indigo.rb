6c42b8470a342935b0bc5362208c4a2c573d2677
ndr <ndr@qef.io>
2014-06-27 02:01:19 +0200

    Reformat indigo.rb according to readability conventions

--- Casks/indigo.rb
+++ Casks/indigo.rb
@@ -1,8 +1,10 @@
 class Indigo < Cask
-  url 'http://cloud.goprism.com/download/Indigo.dmg'
-  homepage 'http://www.perceptiveautomation.com/indigo/index.html'
   version 'latest'
   sha256 :no_check
+
+  url 'http://cloud.goprism.com/download/Indigo.dmg'
+  homepage 'http://www.perceptiveautomation.com/indigo/index.html'
+
   install 'Indigo Installer.pkg'
   uninstall :kext => 'com.perceptiveautomation.indigo_overrides.kext',
     :pkgutil => 'com.perceptiveautomation.pkg.*'
