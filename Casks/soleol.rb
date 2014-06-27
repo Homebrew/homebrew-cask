66bd939be74e124eed00bdba94c08eafaa41ee4c
ndr <ndr@qef.io>
2014-06-27 02:01:44 +0200

    Reformat soleol.rb according to readability conventions

--- Casks/soleol.rb
+++ Casks/soleol.rb
@@ -1,7 +1,9 @@
 class Soleol < Cask
-  url 'http://eduo.info/soleol-support/releases/SolEol-Mac.zip'
-  homepage 'http://eduo.info/apps/soleol'
   version 'latest'
   sha256 :no_check
+
+  url 'http://eduo.info/soleol-support/releases/SolEol-Mac.zip'
+  homepage 'http://eduo.info/apps/soleol'
+
   link 'SolEol/SolEol.app'
 end
