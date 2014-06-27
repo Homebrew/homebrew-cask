61328e4eddb8801a5d518c578a53c184b0c9baa4
ndr <ndr@qef.io>
2014-06-27 02:00:56 +0200

    Reformat anypass.rb according to readability conventions

--- Casks/anypass.rb
+++ Casks/anypass.rb
@@ -1,7 +1,9 @@
 class Anypass < Cask
-  url 'http://icyblaze.com/anypass/anypass_mac_1.0.zip'
-  homepage 'http://icyblaze.com/anypass'
   version '1.0.0'
   sha256 '364d108736eddfdb6e7db56430806d790b79e9f8561aedc544fc78d7e41bac54'
+
+  url 'http://icyblaze.com/anypass/anypass_mac_1.0.zip'
+  homepage 'http://icyblaze.com/anypass'
+
   link 'Anypass.app'
 end
