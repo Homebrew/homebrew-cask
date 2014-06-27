0d571dd0f46e9b5b84bc3629e0eb4aa6833f6f37
ndr <ndr@qef.io>
2014-06-27 02:01:42 +0200

    Reformat shadowsocksx.rb according to readability conventions

--- Casks/shadowsocksx.rb
+++ Casks/shadowsocksx.rb
@@ -1,7 +1,9 @@
 class Shadowsocksx < Cask
-  url 'https://downloads.sourceforge.net/project/shadowsocksgui/dist/ShadowsocksX-1.0.6.dmg'
-  homepage 'https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help'
   version '1.0.6'
   sha256 '3b2d5289043f905897abc9ac395189201718acde79bd782c46a1e9ed6c32892d'
+
+  url 'https://downloads.sourceforge.net/project/shadowsocksgui/dist/ShadowsocksX-1.0.6.dmg'
+  homepage 'https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help'
+
   link 'ShadowsocksX.app'
 end
