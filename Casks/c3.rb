3eaef09bc1c9b50b32e298760cb475cd4be1f9e6
ndr <ndr@qef.io>
2014-06-27 02:01:01 +0200

    Reformat c3.rb according to readability conventions

--- Casks/c3.rb
+++ Casks/c3.rb
@@ -1,7 +1,9 @@
 class C3 < Cask
-  url 'https://d2xj26462na9l3.cloudfront.net/c3/C3-FNF-0.5.2.2143.dmg'
-  homepage 'http://www.downloadc3.com/'
   version '0.5.2.2143'
   sha256 '1f039168caf6487c4ded7fdec5ebf80f4400b03188a40b1bc06f0abc8bf3d3ff'
+
+  url 'https://d2xj26462na9l3.cloudfront.net/c3/C3-FNF-0.5.2.2143.dmg'
+  homepage 'http://www.downloadc3.com/'
+
   link 'c3.app'
 end
