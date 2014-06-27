fe57eb95d6c93bc72e308cfc871b4e9ad6f9ffc9
ndr <ndr@qef.io>
2014-06-27 02:01:02 +0200

    Reformat cdock.rb according to readability conventions

--- Casks/cdock.rb
+++ Casks/cdock.rb
@@ -1,8 +1,10 @@
 class Cdock < Cask
-  url 'https://downloads.sourceforge.net/project/cdock/cDock/cDock_v4.2.zip'
-  homepage 'http://sourceforge.net/projects/cdock'
   version '4.2'
   sha256 '586f1a8972e12b4d71439ef915eca1396d915e28efbcee66ce84bee921aeb0ad'
+
+  url 'https://downloads.sourceforge.net/project/cdock/cDock/cDock_v4.2.zip'
+  homepage 'http://sourceforge.net/projects/cdock'
+
   link 'cDock.app'
   caveats do
     puts <<-EOS.undent
