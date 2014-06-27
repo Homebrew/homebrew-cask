e9ee94aef0394f24b46d4f5e7cb5b0ccf08a63c1
ndr <ndr@qef.io>
2014-06-27 02:01:37 +0200

    Reformat prismatik.rb according to readability conventions

--- Casks/prismatik.rb
+++ Casks/prismatik.rb
@@ -1,7 +1,9 @@
 class Prismatik < Cask
-  url 'https://github.com/Atarity/Lightpack/releases/download/5.10.5/Prismatik.5.10.5.dmg'
-  homepage 'http://lightpack.tv/'
   version '5.10.5'
   sha256 'c1536aea371ee99fd07243c105e465a13bda2b69a81d80b213ea6ab0e7306650'
+
+  url 'https://github.com/Atarity/Lightpack/releases/download/5.10.5/Prismatik.5.10.5.dmg'
+  homepage 'http://lightpack.tv/'
+
   link 'Prismatik.app'
 end
