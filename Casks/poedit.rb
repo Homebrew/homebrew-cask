30796210bc5d6d72f2c9fd27db2742750266a516
ndr <ndr@qef.io>
2014-06-27 02:01:36 +0200

    Reformat poedit.rb according to readability conventions

--- Casks/poedit.rb
+++ Casks/poedit.rb
@@ -1,8 +1,10 @@
 class Poedit < Cask
+  version '1.6.5b2662'
+  sha256 '432c9822d5b4adeeaae80831a9498b04e774221400038692c0083ee6473a3298'
+  
   url 'http://poedit.net/dl/Poedit-1.6.5.zip'
   appcast 'http://releases.poedit.net/appcast-osx'
   homepage 'http://www.poedit.net'
-  version '1.6.5b2662'
-  sha256 '432c9822d5b4adeeaae80831a9498b04e774221400038692c0083ee6473a3298'
+
   link 'Poedit.app'
 end
