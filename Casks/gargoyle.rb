520b4bee7bd6c7c450214569a27345649a51b924
ndr <ndr@qef.io>
2014-06-27 02:01:13 +0200

    Reformat gargoyle.rb according to readability conventions

--- Casks/gargoyle.rb
+++ Casks/gargoyle.rb
@@ -1,7 +1,9 @@
 class Gargoyle < Cask
-  url 'https://garglk.googlecode.com/files/gargoyle-2011.1-mac.dmg'
-  homepage 'https://code.google.com/p/garglk/'
   version '2011.1'
   sha256 '4e830ad3feec78d623ce5c13ce14f440f9769d302ac46604afc4b9785baa038c'
+
+  url 'https://garglk.googlecode.com/files/gargoyle-2011.1-mac.dmg'
+  homepage 'https://code.google.com/p/garglk/'
+
   link 'Gargoyle.app'
 end
