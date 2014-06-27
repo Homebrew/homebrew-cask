4c705deca0e75773351625912ced570732df63e0
Tolbkni Kao <tolbkni@gmail.com>
2014-06-27 11:07:29 +0800

    Android Studio 0.8.0 build-135.1245622
    Closes #5089

--- Casks/android-studio-bundle.rb
+++ Casks/android-studio-bundle.rb
@@ -1,8 +1,8 @@
 class AndroidStudioBundle < Cask
-  version '0.5.2 build-135.1078000'
-  sha256 'fbb0500af402c8fa5435dfac65e16101a70a22c4c8930f072db52ac41556fb8e'
+  version '0.8.0 build-135.1245622'
+  sha256 '7174b6d45c89e986e4a8495078559a161d4b9e091bf6a3556a0237ff5cacfdef'
 
-  url 'https://dl.google.com/android/studio/install/0.5.2/android-studio-bundle-135.1078000-mac.dmg'
+  url 'https://dl.google.com/android/studio/install/0.8.0/android-studio-bundle-135.1245622-mac.dmg'
   homepage 'http://developer.android.com/sdk/installing/studio.html'
 
   link 'Android Studio.app'
