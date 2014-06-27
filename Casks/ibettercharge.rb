280d9f74f0d59b96bded333e163852d4abe084f3
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat ibettercharge.rb according to readability conventions

--- Casks/ibettercharge.rb
+++ Casks/ibettercharge.rb
@@ -1,8 +1,10 @@
 class Ibettercharge < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://download.softorino.com/ibc/last/'
   appcast 'http://hook.softorino.com/ibc/appcast.xml'
   homepage 'http://softorino.com/ibettercharge/'
-  version 'latest'
-  sha256 :no_check
+
   link 'iBetterCharge.app'
 end
