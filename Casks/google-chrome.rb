690cead96bda6fb1e05c3727f7d06e653e515704
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat google-chrome.rb according to readability conventions

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,7 +1,9 @@
 class GoogleChrome < Cask
-  url 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
-  homepage 'https://www.google.com/chrome/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
+  homepage 'https://www.google.com/chrome/'
+
   link 'Google Chrome.app'
 end
