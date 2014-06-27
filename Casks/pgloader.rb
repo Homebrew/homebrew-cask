9e5d2e1331bc9d68a4295fa8f3b4e201abcdf1f7
ndr <ndr@qef.io>
2014-06-27 02:01:35 +0200

    Reformat pgloader.rb according to readability conventions

--- Casks/pgloader.rb
+++ Casks/pgloader.rb
@@ -1,8 +1,10 @@
 class Pgloader < Cask
-  url 'http://pgloader.io/files/pgloader-3.0.99.pkg'
-  homepage 'http://pgloader.io'
   version '3.0.99'
   sha256 'df64c17a3b2aa62b9cdaab48c078e9a9ecb0580d660db036e11f1d63c4bee8ac'
+
+  url 'http://pgloader.io/files/pgloader-3.0.99.pkg'
+  homepage 'http://pgloader.io'
+
   install 'pgloader-3.0.99.pkg'
   uninstall :pkgutil => 'org.tapoueh.pgloader'
 end
