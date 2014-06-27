3d74c2e0dcda59a3f5b494712bac4361bee1f9ca
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat gitx.rb according to readability conventions

--- Casks/gitx.rb
+++ Casks/gitx.rb
@@ -1,8 +1,10 @@
 class Gitx < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://frim.frim.nl/GitXStable.app.zip'
   appcast 'http://gitx.frim.nl/Downloads/appcast.xml'
   homepage 'http://gitx.frim.nl/'
-  version 'latest'
-  sha256 :no_check
+
   link 'GitX.app'
 end
