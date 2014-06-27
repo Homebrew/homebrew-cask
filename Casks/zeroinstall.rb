c7100e54e7febb372b0743abd106e77524c29959
ndr <ndr@qef.io>
2014-06-27 02:01:55 +0200

    Reformat zeroinstall.rb according to readability conventions

--- Casks/zeroinstall.rb
+++ Casks/zeroinstall.rb
@@ -1,8 +1,10 @@
 class Zeroinstall < Cask
-  url 'https://downloads.sourceforge.net/project/zero-install/0install/2.7/ZeroInstall.pkg'
-  homepage 'http://0install.net'
   version '2.7'
   sha256 '12246277e9d03fb1a083f6a0d7d088d7b90170dbd5fd8fb900b36d0470fb88e0'
+
+  url 'https://downloads.sourceforge.net/project/zero-install/0install/2.7/ZeroInstall.pkg'
+  homepage 'http://0install.net'
+
   install 'ZeroInstall.pkg'
   uninstall :pkgutil => 'net.0install.pkg'
 end
