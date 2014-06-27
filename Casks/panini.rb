6096b5c1c5f60952cbaf948e70e7830881c012b2
ndr <ndr@qef.io>
2014-06-27 02:01:34 +0200

    Reformat panini.rb according to readability conventions

--- Casks/panini.rb
+++ Casks/panini.rb
@@ -1,7 +1,9 @@
 class Panini < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/pvqt/Panini-0.71.104B-mac.dmg'
-  homepage 'http://sourceforge.net/projects/pvqt/'
   version '0.71.104'
   sha256 '2a2836b0035bc1be8ada1e07138c72276a89624321dbfaa475fc4cd88fffe544'
+
+  url 'https://downloads.sourceforge.net/sourceforge/pvqt/Panini-0.71.104B-mac.dmg'
+  homepage 'http://sourceforge.net/projects/pvqt/'
+
   link 'Panini.app'
 end
