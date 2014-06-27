df4bcc21764e874e33ec06ff20fa181d8d52bd13
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat geektool.rb according to readability conventions

--- Casks/geektool.rb
+++ Casks/geektool.rb
@@ -1,8 +1,10 @@
 class Geektool < Cask
-    url 'http://download.tynsoe.org/GeekTool-3.1.1-311.zip'
-  appcast 'http://dl.dropboxusercontent.com/u/1760713/appcast/appcast.xml'
-    homepage 'http://projects.tynsoe.org/en/geektool/'
     version '3.1.1-311'
-    sha256 'ef1a7439d91f0de9e459a4677c6f95fe35bd7e02c300adc9ce315b5590cbbfc9'
+  sha256 'ef1a7439d91f0de9e459a4677c6f95fe35bd7e02c300adc9ce315b5590cbbfc9'
+  
+  url 'http://download.tynsoe.org/GeekTool-3.1.1-311.zip'
+  appcast 'http://dl.dropboxusercontent.com/u/1760713/appcast/appcast.xml'
+  homepage 'http://projects.tynsoe.org/en/geektool/'
+
     link 'GeekTool.app'
 end
