58943dc622251d257e04c07c0313e3377af8e9fa
ndr <ndr@qef.io>
2014-06-27 02:00:56 +0200

    Reformat aliwangwang.rb according to readability conventions

--- Casks/aliwangwang.rb
+++ Casks/aliwangwang.rb
@@ -1,8 +1,10 @@
 class Aliwangwang < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://update.labs.etao.com/macww-nosandbox/versions/lastest'
   appcast 'http://update.labs.etao.com/macww/updates.xml'
   homepage 'http://wangwang.taobao.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'AliWangwang.app'
 end
