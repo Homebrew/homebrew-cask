1bb92e40dc63800db2bf439e557312041223650b
ndr <ndr@qef.io>
2014-06-27 02:01:27 +0200

    Reformat mediafire-desktop.rb according to readability conventions

--- Casks/mediafire-desktop.rb
+++ Casks/mediafire-desktop.rb
@@ -1,7 +1,9 @@
 class MediafireDesktop < Cask
-  url 'https://www.mediafire.com/?4xcr491804ncktz/'
-  homepage 'https://www.mediafire.com/software/desktop/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.mediafire.com/?4xcr491804ncktz/'
+  homepage 'https://www.mediafire.com/software/desktop/'
+
   link 'MediaFire Desktop.app'
 end
