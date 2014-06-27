23a44c38d3fd43464d5bd57e49a51e9bd2cd7816
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat lingon-x.rb according to readability conventions

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,8 +1,10 @@
 class LingonX < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.peterborgapps.com/downloads/LingonX.zip'
   appcast 'http://www.peterborgapps.com/updates/lingonx-appcast.xml'
   homepage 'http://www.peterborgapps.com/lingon/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Lingon X.app'
 end
