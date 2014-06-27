c0b1dbce9955e065995ded5e730b60ba0d47e736
ndr <ndr@qef.io>
2014-06-27 02:01:53 +0200

    Reformat x-moto.rb according to readability conventions

--- Casks/x-moto.rb
+++ Casks/x-moto.rb
@@ -1,7 +1,9 @@
 class XMoto < Cask
-  url 'http://download.tuxfamily.org/xmoto/xmoto/0.5.10/xmoto-0.5.10-macosx.zip'
-  homepage 'http://xmoto.tuxfamily.org'
   version '0.5.10'
   sha256 '0b032d21fb0644db252fe9210f9c0a3f465616996aad6d50b20ead2123a3507a'
+
+  url 'http://download.tuxfamily.org/xmoto/xmoto/0.5.10/xmoto-0.5.10-macosx.zip'
+  homepage 'http://xmoto.tuxfamily.org'
+
   link 'X-Moto.app'
 end
