cd73d7a1763ac11ddef80ed9d1905d9d029ae665
ndr <ndr@qef.io>
2014-06-27 02:01:14 +0200

    Reformat gisto.rb according to readability conventions

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -1,7 +1,9 @@
 class Gisto < Cask
-  url 'http://download.gistoapp.com/Gisto-0.2.5b-OSX_x86.dmg'
-  homepage 'http://www.gistoapp.com/'
   version '0.2.5b'
   sha256 '4f0b3e599f601483c232363867f3c62754631174c688fba143eadda8ddc9aaf4'
+
+  url 'http://download.gistoapp.com/Gisto-0.2.5b-OSX_x86.dmg'
+  homepage 'http://www.gistoapp.com/'
+
   link 'Gisto.app'
 end
