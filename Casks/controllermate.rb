ef8aa0913980771f5a702f7379ff7b508725a5c6
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat controllermate.rb according to readability conventions

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -1,8 +1,10 @@
 class Controllermate < Cask
-  url 'https://s3.amazonaws.com/orderedbytes/ControllerMate482.dmg'
-  homepage 'http://www.orderedbytes.com/controllermate/'
   version '4.8.2'
   sha256 '654096120355452f69de00f46c55ad295515a497255e035707ca195d1b17eac4'
+
+  url 'https://s3.amazonaws.com/orderedbytes/ControllerMate482.dmg'
+  homepage 'http://www.orderedbytes.com/controllermate/'
+
   install 'ControllerMate.pkg'
   uninstall :script => 'ControllerMate Uninstaller.app/Contents/MacOS/ControllerMate Uninstaller'
 end
