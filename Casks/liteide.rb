1c5e5cbbf77d4e0658732724b1771d3f76c5e7c8
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat liteide.rb according to readability conventions

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,7 +1,9 @@
 class Liteide < Cask
-  url 'https://downloads.sourceforge.net/project/liteide/X22/liteidex22.macosx.zip'
-  homepage 'https://code.google.com/p/golangide/'
   version 'x22'
   sha256 '994b2bf68bb7f99ef7ac8001546f437644e9751109e5bf207adee7fb6180c689'
+
+  url 'https://downloads.sourceforge.net/project/liteide/X22/liteidex22.macosx.zip'
+  homepage 'https://code.google.com/p/golangide/'
+
   link 'LiteIDE.app'
 end
