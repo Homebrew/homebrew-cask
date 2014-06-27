249d3a017a588913aacf2c5853bedf3ca44f0617
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat tunewiki.rb according to readability conventions

--- Casks/tunewiki.rb
+++ Casks/tunewiki.rb
@@ -1,7 +1,9 @@
 class Tunewiki < Cask
-  url 'https://www.tunewiki.com/download/desktop/TuneWiki_Installer.dmg'
-  homepage 'http://www.tunewiki.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.tunewiki.com/download/desktop/TuneWiki_Installer.dmg'
+  homepage 'http://www.tunewiki.com/'
+
   link 'TuneWiki.app'
 end
