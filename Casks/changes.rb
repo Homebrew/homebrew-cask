c7bd57f6f0ec202132255d206b2e954745603f0b
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat changes.rb according to readability conventions

--- Casks/changes.rb
+++ Casks/changes.rb
@@ -1,8 +1,10 @@
 class Changes < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://bitbq.com/changes/download.php'
   appcast 'https://bitbq_changes.s3.amazonaws.com/changes-production.xml'
   homepage 'http://bitbq.com/changes/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Changes.app'
 end
