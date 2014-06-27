12e20f000a8e71a2ff0bdc1d3e6bec22387f8029
ndr <ndr@qef.io>
2014-06-27 02:01:51 +0200

    Reformat ultrastardeluxe.rb according to readability conventions

--- Casks/ultrastardeluxe.rb
+++ Casks/ultrastardeluxe.rb
@@ -1,7 +1,9 @@
 class Ultrastardeluxe < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/ultrastardx/UltraStarDeluxe-1.1.dmg'
-  homepage 'http://ultrastardx.sourceforge.net/'
   version '1.1'
   sha256 '18e36476ec6994b1dd54e6272c08ceb86b10abf4984a18d834003e696153ca9b'
+
+  url 'https://downloads.sourceforge.net/sourceforge/ultrastardx/UltraStarDeluxe-1.1.dmg'
+  homepage 'http://ultrastardx.sourceforge.net/'
+
   link 'UltraStarDeluxe.app'
 end
