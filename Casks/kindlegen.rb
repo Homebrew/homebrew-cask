5c7a3f7631c24467727227e700771d0e7deab6e7
ndr <ndr@qef.io>
2014-06-27 02:01:22 +0200

    Reformat kindlegen.rb according to readability conventions

--- Casks/kindlegen.rb
+++ Casks/kindlegen.rb
@@ -1,7 +1,9 @@
 class Kindlegen < Cask
-  url 'https://kindlegen.s3.amazonaws.com/KindleGen_Mac_i386_v2_9.zip'
-  homepage 'http://www.amazon.com/gp/feature.html?docId=1000765211'
   version '2.9'
   sha256 'e09ad8f985c7096556f978df15903b71f9cf44d2e1f501139a420ff16931f980'
+
+  url 'https://kindlegen.s3.amazonaws.com/KindleGen_Mac_i386_v2_9.zip'
+  homepage 'http://www.amazon.com/gp/feature.html?docId=1000765211'
+
   binary 'kindlegen'
 end
