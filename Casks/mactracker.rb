3712fe3e7cecfa45d8f4a35ea22709ae03251d3f
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat mactracker.rb according to readability conventions

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,8 +1,10 @@
 class Mactracker < Cask
+  version '7.3.2'
+  sha256 '3a156082b09b3c4e1ef2d94cba91fe67c42ee0d4010fa7e73a213481c384dab5'
+  
   url 'http://www.mactracker.ca/downloads/Mactracker_7.3.2.zip'
   appcast 'http://update.mactracker.ca/appcast-b.xml'
   homepage 'http://mactracker.ca/'
-  version '7.3.2'
-  sha256 '3a156082b09b3c4e1ef2d94cba91fe67c42ee0d4010fa7e73a213481c384dab5'
+
   link 'Mactracker.app'
 end
