5830dbccbc584be9df58cfb038f6232e8ba099a2
ndr <ndr@qef.io>
2014-06-27 02:01:28 +0200

    Reformat miro-video-converter.rb according to readability conventions

--- Casks/miro-video-converter.rb
+++ Casks/miro-video-converter.rb
@@ -1,8 +1,10 @@
 class MiroVideoConverter < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://ftp.osuosl.org/pub/pculture.org/mirovideoconverter/mac/Miro%20Video%20Converter.dmg'
   appcast 'http://miro-updates.participatoryculture.org/mvc-appcast-osx.xml'
   homepage 'http://www.mirovideoconverter.com/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Miro Video Converter.app'
 end
