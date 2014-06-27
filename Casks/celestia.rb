4ed3eb0b09c6f2106cc5ffb60595fcb515b25b8a
ndr <ndr@qef.io>
2014-06-27 02:01:02 +0200

    Reformat celestia.rb according to readability conventions

--- Casks/celestia.rb
+++ Casks/celestia.rb
@@ -1,7 +1,9 @@
 class Celestia < Cask
-  url 'https://downloads.sourceforge.net/sourceforge/celestia/celestia-osx-1.6.1.dmg'
-  homepage 'http://sourceforge.net/projects/celestia'
   version '1.6.1'
   sha256 'db09842a10b377038bedef87fda8d159549234b2e072fa22e096ade84aa3f52a'
+
+  url 'https://downloads.sourceforge.net/sourceforge/celestia/celestia-osx-1.6.1.dmg'
+  homepage 'http://sourceforge.net/projects/celestia'
+
   link 'Celestia.app'
 end
