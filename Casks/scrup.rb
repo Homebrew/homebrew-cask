cda3a4b4567e85312f79c0b9bf9fc7ff1e0fe390
ndr <ndr@qef.io>
2014-06-27 02:01:41 +0200

    Reformat scrup.rb according to readability conventions

--- Casks/scrup.rb
+++ Casks/scrup.rb
@@ -1,8 +1,10 @@
 class Scrup < Cask
+  version '1.3.3'
+  sha256 '5004222db9a6ddd4e6cb525d00e95f8a38e9fb623bc1397e5258b2ef2c4bd3b0'
+  
   url 'http://data.hunch.se/scrup/Scrup-1.3.3-bd23160.zip'
   appcast 'https://s.rsms.me/scrup/appcast.xml'
   homepage 'https://github.com/rsms/scrup'
-  version '1.3.3'
-  sha256 '5004222db9a6ddd4e6cb525d00e95f8a38e9fb623bc1397e5258b2ef2c4bd3b0'
+
   link 'Scrup.app'
 end
