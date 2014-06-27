ebd89a71ec0c2b1a87f0ef48108db9ad8f58415f
ndr <ndr@qef.io>
2014-06-27 02:01:22 +0200

    Reformat kismac.rb according to readability conventions

--- Casks/kismac.rb
+++ Casks/kismac.rb
@@ -1,8 +1,10 @@
 class Kismac < Cask
+  version '0.3.3'
+  sha256 'd48f307c1c46a1d1a6b4465d653cf417e583bc09b7ea76d21183068066bc49bb'
+  
   url 'http://update.kismacmirror.com/binaries/KisMAC-0.3.3.dmg'
   appcast 'http://update.kismac-ng.org/sparkle/profileInfo.php'
   homepage 'http://kismac-ng.org/'
-  version '0.3.3'
-  sha256 'd48f307c1c46a1d1a6b4465d653cf417e583bc09b7ea76d21183068066bc49bb'
+
   link 'KisMAC.app'
 end
