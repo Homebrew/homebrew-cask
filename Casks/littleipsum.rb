67cf9e9aa2e6891eb9ce5d5b691968f4c7ecf92d
ndr <ndr@qef.io>
2014-06-27 02:01:24 +0200

    Reformat littleipsum.rb according to readability conventions

--- Casks/littleipsum.rb
+++ Casks/littleipsum.rb
@@ -1,7 +1,9 @@
 class Littleipsum < Cask
-  url 'http://littleipsum.com/download/LittleIpsum%201.1.2.zip'
-  homepage 'http://littleipsum.com'
   version '1.1.2'
   sha256 '5fa88e5cd2cc1c837ac3e9ff3a23ce1569835d56c71e2741243ce9f0424099db'
+
+  url 'http://littleipsum.com/download/LittleIpsum%201.1.2.zip'
+  homepage 'http://littleipsum.com'
+
   link 'LittleIpsum.app'
 end
