880daea3b3166c0fc393ca23d4c41018cd8b1d44
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat comicbooklover.rb according to readability conventions

--- Casks/comicbooklover.rb
+++ Casks/comicbooklover.rb
@@ -1,8 +1,10 @@
 class Comicbooklover < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.bitcartel.com/downloads/comicbooklover.zip'
   appcast 'http://www.bitcartel.com/appcast/comicbooklover-1.7-dsa.xml'
   homepage 'http://www.bitcartel.com/comicbooklover/'
-  version 'latest'
-  sha256 :no_check
+
   link 'ComicBookLover.app'
 end
