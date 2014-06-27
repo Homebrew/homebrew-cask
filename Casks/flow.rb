c520d5467385e57e93908b3463106001432ce7a0
ndr <ndr@qef.io>
2014-06-27 02:01:12 +0200

    Reformat flow.rb according to readability conventions

--- Casks/flow.rb
+++ Casks/flow.rb
@@ -1,8 +1,10 @@
 class Flow < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://fivedetails.com/flow/download'
   appcast 'http://extendmac.com/flow/updates/update.php'
   homepage 'http://fivedetails.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'Flow.app'
 end
