61d570574e436cf81394480891916e46ddf6d432
ndr <ndr@qef.io>
2014-06-27 02:01:50 +0200

    Reformat treesheets.rb according to readability conventions

--- Casks/treesheets.rb
+++ Casks/treesheets.rb
@@ -1,7 +1,9 @@
 class Treesheets < Cask
-  url 'http://strlen.com/treesheets/treesheets_osx.zip'
-  homepage 'http://strlen.com/treesheets/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://strlen.com/treesheets/treesheets_osx.zip'
+  homepage 'http://strlen.com/treesheets/'
+
   link 'TreeSheetsBeta/TreeSheets.app'
 end
