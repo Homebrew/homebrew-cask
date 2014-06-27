94c19cecbee956d67d057d8c8a3166e5bb740fc4
ndr <ndr@qef.io>
2014-06-27 02:01:06 +0200

    Reformat deathtodsstore.rb according to readability conventions

--- Casks/deathtodsstore.rb
+++ Casks/deathtodsstore.rb
@@ -1,7 +1,9 @@
 class Deathtodsstore < Cask
-  url 'http://www.aorensoftware.com/Downloads/Files/DeathToDSStore.zip'
-  homepage 'http://www.aorensoftware.com/blog/2011/12/24/death-to-ds_store/'
   version '1.0.3'
   sha256 '32e44486a06b76b59e894119fa5a55895e2d74be2212805159bdcb9aee0e9e10'
+
+  url 'http://www.aorensoftware.com/Downloads/Files/DeathToDSStore.zip'
+  homepage 'http://www.aorensoftware.com/blog/2011/12/24/death-to-ds_store/'
+
   link 'DeathToDSStore.app'
 end
