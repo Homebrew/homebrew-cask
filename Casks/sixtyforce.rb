8445e86b1306e937f06454599a1ae2505e3e602d
ndr <ndr@qef.io>
2014-06-27 02:01:43 +0200

    Reformat sixtyforce.rb according to readability conventions

--- Casks/sixtyforce.rb
+++ Casks/sixtyforce.rb
@@ -1,7 +1,9 @@
 class Sixtyforce < Cask
-  url 'http://sixtyforce.com/download/sixtyforce.zip'
-  homepage 'http://sixtyforce.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://sixtyforce.com/download/sixtyforce.zip'
+  homepage 'http://sixtyforce.com/'
+
   link 'sixtyforce.app'
 end
