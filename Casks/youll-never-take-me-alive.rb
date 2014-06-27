eb11bd162cfdec9e7dc7646eac395a7f901e73ac
ndr <ndr@qef.io>
2014-06-27 02:01:55 +0200

    Reformat youll-never-take-me-alive.rb according to readability conventions

--- Casks/youll-never-take-me-alive.rb
+++ Casks/youll-never-take-me-alive.rb
@@ -1,7 +1,9 @@
 class YoullNeverTakeMeAlive < Cask
-  url 'https://github.com/iSECPartners/yontma-mac/releases/download/0.9.9/yontma-0.9.9.dmg'
-  homepage 'https://github.com/iSECPartners/yontma-mac'
   version '0.9.9'
   sha256 'e055e96ed1816b30b420533af8f9de90da088b339d51aa99a5c20c636e8ba9c8'
+
+  url 'https://github.com/iSECPartners/yontma-mac/releases/download/0.9.9/yontma-0.9.9.dmg'
+  homepage 'https://github.com/iSECPartners/yontma-mac'
+
   link 'You\'ll Never Take Me Alive!.app'
 end
