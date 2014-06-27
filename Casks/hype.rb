48569aa8b3731b6633d35f0639a23322f266e8a4
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat hype.rb according to readability conventions

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -1,7 +1,9 @@
 class Hype < Cask
-  url 'http://tumult.com/hype/download/Hype.zip'
-  homepage 'http://tumult.com/hype/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://tumult.com/hype/download/Hype.zip'
+  homepage 'http://tumult.com/hype/'
+
   link 'Hype2.app'
 end
