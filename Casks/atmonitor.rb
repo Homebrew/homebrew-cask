542baceafd13397f095392ac9a9b535fb7fcb845
ndr <ndr@qef.io>
2014-06-27 02:00:58 +0200

    Reformat atmonitor.rb according to readability conventions

--- Casks/atmonitor.rb
+++ Casks/atmonitor.rb
@@ -1,9 +1,11 @@
 class Atmonitor < Cask
+  version 'latest'
+  sha256 :no_check  
+
   url 'http://download.atpurpose.com/atMonitor/atMonitor.zip'
   # todo: no response
   # appcast 'http://support.atPurpose.com/atMonitor/updates.xml'
   homepage 'http://www.atpurpose.com/atMonitor/'
-  version 'latest'
-  sha256 :no_check
+
   link 'atMonitor.app'
 end
