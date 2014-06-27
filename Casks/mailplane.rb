36573bbd027d2d83c583843509338485a5947f2b
ndr <ndr@qef.io>
2014-06-27 02:01:26 +0200

    Reformat mailplane.rb according to readability conventions

--- Casks/mailplane.rb
+++ Casks/mailplane.rb
@@ -1,7 +1,9 @@
 class Mailplane < Cask
-  url 'http://update.mailplaneapp.com/mailplane_3.php'
-  homepage 'http://mailplaneapp.com'
   version 'latest'
   sha256 :no_check
+
+  url 'http://update.mailplaneapp.com/mailplane_3.php'
+  homepage 'http://mailplaneapp.com'
+
   link 'Mailplane 3.app'
 end
