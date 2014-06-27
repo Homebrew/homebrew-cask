8fcaf82c0ce4afbfe0c71a37c8f4e4bc8c50a0b3
ndr <ndr@qef.io>
2014-06-27 02:01:28 +0200

    Reformat metanota.rb according to readability conventions

--- Casks/metanota.rb
+++ Casks/metanota.rb
@@ -1,8 +1,10 @@
 class Metanota < Cask
+  version '2.4.1'
+  sha256 'baac3496d1fe723ef6cb905aca9a718aac828c56ef4b64cde171bac340bbc124'
+  
   url 'https://j.mp/metanota-2-4-1'
   appcast 'https://s3.amazonaws.com/sparkle.metanota.com/appcast.xml'
   homepage 'http://www.metanota.com/'
-  version '2.4.1'
-  sha256 'baac3496d1fe723ef6cb905aca9a718aac828c56ef4b64cde171bac340bbc124'
+
   link 'Metanota.app'
 end
