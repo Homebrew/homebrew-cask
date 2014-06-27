1d2ea8d64046b019a5cd144dffbca3286a1cc92c
ndr <ndr@qef.io>
2014-06-27 02:00:57 +0200

    Reformat apptivate.rb according to readability conventions

--- Casks/apptivate.rb
+++ Casks/apptivate.rb
@@ -1,7 +1,9 @@
 class Apptivate < Cask
-  url 'http://www.apptivateapp.com/resources/Apptivate.app.zip'
-  homepage 'http://www.apptivateapp.com'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.apptivateapp.com/resources/Apptivate.app.zip'
+  homepage 'http://www.apptivateapp.com'
+
   link 'Apptivate.app'
 end
