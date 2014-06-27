ceb21ae73a8f49491ef42eed4e9c338a2163c0f6
ndr <ndr@qef.io>
2014-06-27 02:01:01 +0200

    Reformat boot2docker.rb according to readability conventions

--- Casks/boot2docker.rb
+++ Casks/boot2docker.rb
@@ -1,8 +1,10 @@
 class Boot2docker < Cask
-  url 'https://github.com/boot2docker/osx-installer/releases/download/v1.0.0/Boot2Docker-1.0.0.pkg'
-  homepage 'https://github.com/boot2docker/osx-installer'
   version '1.0.0'
   sha256 '4bcdc3b4f309c7957aa8b91768f9c59d23f63258bc83d22cbdf268f5bbf2785f'
+
+  url 'https://github.com/boot2docker/osx-installer/releases/download/v1.0.0/Boot2Docker-1.0.0.pkg'
+  homepage 'https://github.com/boot2docker/osx-installer'
+
   install 'Boot2Docker-1.0.0.pkg'
   uninstall :pkgutil => ['io.boot2docker.pkg.boot2docker', 'io.boot2docker.pkg.boot2dockerapp', 'io.boot2dockeriso.pkg.boot2dockeriso', 'io.docker.pkg.docker']
 end
