e1aff360417dd8481c42283971822a16a193db7b
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat colorpicker-developer.rb according to readability conventions

--- Casks/colorpicker-developer.rb
+++ Casks/colorpicker-developer.rb
@@ -1,7 +1,9 @@
 class ColorpickerDeveloper < Cask
-  url 'http://download.panic.com/picker/developercolorpicker.zip'
-  homepage 'http://download.panic.com/picker/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://download.panic.com/picker/developercolorpicker.zip'
+  homepage 'http://download.panic.com/picker/'
+
   colorpicker 'Developer Color Picker/DeveloperColorPicker.colorPicker'
 end
