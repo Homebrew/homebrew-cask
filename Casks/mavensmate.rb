6ced0cbab2ac3fb65079e3e7b0ed18cbb75b8618
ndr <ndr@qef.io>
2014-06-27 02:01:27 +0200

    Reformat mavensmate.rb according to readability conventions

--- Casks/mavensmate.rb
+++ Casks/mavensmate.rb
@@ -1,8 +1,10 @@
 class Mavensmate < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://push.mavensconsulting.netdna-cdn.com/mavensmate/builds/MavensMate.zip'
   appcast 'http://mavensconsulting.com/mavensmate/builds/appcast.xml'
   homepage 'http://mavensmate.com'
-  version 'latest'
-  sha256 :no_check
+
   link 'MavensMate.app'
 end
