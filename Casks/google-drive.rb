4ba5ffb03d8c90873941374f723197a40722da6f
ndr <ndr@qef.io>
2014-06-27 02:01:15 +0200

    Reformat google-drive.rb according to readability conventions

--- Casks/google-drive.rb
+++ Casks/google-drive.rb
@@ -1,7 +1,9 @@
 class GoogleDrive < Cask
-  url 'https://dl.google.com/drive/installgoogledrive.dmg'
-  homepage 'https://drive.google.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://dl.google.com/drive/installgoogledrive.dmg'
+  homepage 'https://drive.google.com/'
+
   link 'Google Drive.app'
 end
