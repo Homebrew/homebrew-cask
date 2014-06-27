55c3056be25eabb92c4c936ad2e73f6dbb142736
ndr <ndr@qef.io>
2014-06-27 02:01:07 +0200

    Reformat divvy.rb according to readability conventions

--- Casks/divvy.rb
+++ Casks/divvy.rb
@@ -1,8 +1,10 @@
 class Divvy < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://mizage.com/downloads/Divvy.zip'
   appcast 'http://mizage.com/updates/profiles/divvy.php'
   homepage 'http://mizage.com/divvy/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Divvy.app'
 end
