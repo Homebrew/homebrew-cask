0c9d8a0347e131c43398b6d3644d40fbdfab363f
ndr <ndr@qef.io>
2014-06-27 02:01:25 +0200

    Reformat macmoney.rb according to readability conventions

--- Casks/macmoney.rb
+++ Casks/macmoney.rb
@@ -1,8 +1,10 @@
 class Macmoney < Cask
-  url 'http://www.devon.riceball.net/downloads/macmoney36.zip'
-  homepage 'http://www.devon.riceball.net/display.php?file=m01'
   version '3.6.6'
   sha256 '34168a92ab65daf5280c879887011e30e9a3e01398f5402a32034049932a075b'
+
+  url 'http://www.devon.riceball.net/downloads/macmoney36.zip'
+  homepage 'http://www.devon.riceball.net/display.php?file=m01'
+
   nested_container 'MacMoney_3.6.6.dmg'
   link 'MacMoney.app'
 end
