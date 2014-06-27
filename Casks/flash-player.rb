efad2c289a9a1f96df256bc9d2eb729a9d5d64dc
ndr <ndr@qef.io>
2014-06-27 02:01:11 +0200

    Reformat flash-player.rb according to readability conventions

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,7 +1,9 @@
 class FlashPlayer < Cask
-  url 'https://fpdownload.macromedia.com/pub/flashplayer/updaters/14/flashplayer_14_sa.dmg'
-  homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
   version '14.0.0.125'
   sha256 'fa5eef90162516da0ba4de071abdff202f6bb5281d5a39b62169cad60d214a90'
+
+  url 'https://fpdownload.macromedia.com/pub/flashplayer/updaters/14/flashplayer_14_sa.dmg'
+  homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
+
   link 'Flash Player.app'
 end
