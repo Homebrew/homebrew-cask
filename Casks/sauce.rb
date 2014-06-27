d62f28b8c81311c54f542ec00c36fa6bb0ce8ee0
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat sauce.rb according to readability conventions

--- Casks/sauce.rb
+++ Casks/sauce.rb
@@ -1,7 +1,9 @@
 class Sauce < Cask
-  url 'https://saucelabs.com/downloads/Sauce-for-Mac.dmg'
-  homepage 'https://saucelabs.com/mac'
   version 'latest'
   sha256 :no_check
+
+  url 'https://saucelabs.com/downloads/Sauce-for-Mac.dmg'
+  homepage 'https://saucelabs.com/mac'
+
   link 'Sauce.app'
 end
