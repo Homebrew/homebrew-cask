9c924aa1c9223681d57d10001236cd57d0920067
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat omnifocus.rb according to readability conventions

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -1,7 +1,9 @@
 class Omnifocus < Cask
-  url 'https://www.omnigroup.com/download/latest/omnifocus'
-  homepage 'http://www.omnigroup.com/products/omnifocus/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://www.omnigroup.com/download/latest/omnifocus'
+  homepage 'http://www.omnigroup.com/products/omnifocus/'
+
   link 'OmniFocus.app'
 end
