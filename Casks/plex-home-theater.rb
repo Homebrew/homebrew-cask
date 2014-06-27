910e73bdc86e40be07000b521609cdd03fddb500
ndr <ndr@qef.io>
2014-06-27 02:01:36 +0200

    Reformat plex-home-theater.rb according to readability conventions

--- Casks/plex-home-theater.rb
+++ Casks/plex-home-theater.rb
@@ -1,7 +1,9 @@
 class PlexHomeTheater < Cask
-  url 'http://downloads.plexapp.com/plex-home-theater/1.0.9.180-bde1e61d/PlexHomeTheater-1.0.9.180-bde1e61d-macosx-i386.zip'
-  homepage 'https://plex.tv'
   version '1.0.9.180'
   sha256 'f7c51b212febafbca77e0af193819c7d7035a38600d65550db1362edadee31b7'
+
+  url 'http://downloads.plexapp.com/plex-home-theater/1.0.9.180-bde1e61d/PlexHomeTheater-1.0.9.180-bde1e61d-macosx-i386.zip'
+  homepage 'https://plex.tv'
+
   link 'Plex Home Theater.app'
 end
