33bfb16c86c9c3aae162994f0d03e7e28f9f3d91
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat webp-quicklook.rb according to readability conventions

--- Casks/webp-quicklook.rb
+++ Casks/webp-quicklook.rb
@@ -1,7 +1,9 @@
 class WebpQuicklook < Cask
-  url 'https://github.com/dchest/webp-quicklook/releases/download/v2.1/WebP-2.1.qlgenerator.zip'
-  homepage 'https://github.com/dchest/webp-quicklook'
   version '2.1'
   sha256 '9efbdd2b3dcb44d41a53c1cb6ce64397fd00bd399189b1b742d2f4b2de28f437'
+
+  url 'https://github.com/dchest/webp-quicklook/releases/download/v2.1/WebP-2.1.qlgenerator.zip'
+  homepage 'https://github.com/dchest/webp-quicklook'
+
   qlplugin 'WebP.qlgenerator'
 end
