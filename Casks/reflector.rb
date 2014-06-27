ee638fd5025685e1bcb2eed171479689b7069604
ndr <ndr@qef.io>
2014-06-27 02:01:39 +0200

    Reformat reflector.rb according to readability conventions

--- Casks/reflector.rb
+++ Casks/reflector.rb
@@ -1,8 +1,10 @@
 class Reflector < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://download.airsquirrels.com/Reflector/Mac/Reflector.dmg'
   appcast 'http://reflectorapp.com/updates/reflector.xml'
   homepage 'http://www.airsquirrels.com/reflector/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Reflector.app'
 end
