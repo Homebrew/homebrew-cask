fec44a0801282aa47a715b1b6a5c737a2f797a36
ndr <ndr@qef.io>
2014-06-27 02:01:03 +0200

    Reformat cinch.rb according to readability conventions

--- Casks/cinch.rb
+++ Casks/cinch.rb
@@ -1,8 +1,10 @@
 class Cinch < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'https://www.irradiatedsoftware.com/download/Cinch.zip'
   appcast 'https://www.irradiatedsoftware.com/updates/profiles/cinch.php'
   homepage 'http://www.irradiatedsoftware.com/cinch/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Cinch.app'
 end
