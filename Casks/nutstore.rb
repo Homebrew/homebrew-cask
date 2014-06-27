cccf3d671feca70718a27a6897a98614a8a46b80
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat nutstore.rb according to readability conventions

--- Casks/nutstore.rb
+++ Casks/nutstore.rb
@@ -1,7 +1,9 @@
 class Nutstore < Cask
-  url 'https://jianguoyun.com/static/exe/installer/NutstoreOSXInstaller.dmg'
-  homepage 'https://jianguoyun.com'
   version 'latest'
   sha256 :no_check
+
+  url 'https://jianguoyun.com/static/exe/installer/NutstoreOSXInstaller.dmg'
+  homepage 'https://jianguoyun.com'
+
   link 'Nutstore.app'
 end
