0130161b1dab5bb3287cc2e0a87599c4480787b7
ndr <ndr@qef.io>
2014-06-27 02:01:18 +0200

    Reformat iannix.rb according to readability conventions

--- Casks/iannix.rb
+++ Casks/iannix.rb
@@ -1,7 +1,9 @@
 class Iannix < Cask
-  url 'http://www.iannix.org/download/iannix_mac_64__0_9_16.dmg'
-  homepage 'http://www.iannix.org/'
   version '0.9.16'
   sha256 'b45d332fc450342ad9a5b73fedcd554c299d435f989f0fe099522554434d00de'
+
+  url 'http://www.iannix.org/download/iannix_mac_64__0_9_16.dmg'
+  homepage 'http://www.iannix.org/'
+
   link 'IanniX/IanniX.app'
 end
