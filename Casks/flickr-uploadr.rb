c9ac3853ffecd9918d80e52bb12c06cf7a00d0f7
ndr <ndr@qef.io>
2014-06-27 02:01:12 +0200

    Reformat flickr-uploadr.rb according to readability conventions

--- Casks/flickr-uploadr.rb
+++ Casks/flickr-uploadr.rb
@@ -1,7 +1,9 @@
 class FlickrUploadr < Cask
-  url 'http://h.yimg.com/ce/flickr/FlickrUploadr-3.2/FlickrUploadr-3.2.1-2009.06.10.01-en.dmg'
-  homepage 'http://www.flickr.com/tools/'
   version '3.2.1'
   sha256 'c41a1e4a8b6f7e3c9cb5d1adfc1b3bdc4a835dfbc31f16c0dbd657b74523214a'
+
+  url 'http://h.yimg.com/ce/flickr/FlickrUploadr-3.2/FlickrUploadr-3.2.1-2009.06.10.01-en.dmg'
+  homepage 'http://www.flickr.com/tools/'
+
   link 'Flickr Uploadr.app'
 end
