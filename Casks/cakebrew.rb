10d14ddb284f802de05a649316e89d8d9fbef82c
ndr <ndr@qef.io>
2014-06-27 02:01:01 +0200

    Reformat cakebrew.rb according to readability conventions

--- Casks/cakebrew.rb
+++ Casks/cakebrew.rb
@@ -1,8 +1,10 @@
 class Cakebrew < Cask
+  version '1.0.7'
+  sha256 '647ce281fa3218e7d4531cac0e5b69d19136883d2b95d154b7a818c4d130cace'
+  
   url 'https://www.cakebrew.com/files/cakebrew-1.0.7.dmg'
   appcast 'http://www.cakebrew.com/appcast/profileInfo.php'
   homepage 'http://www.cakebrew.com'
-  version '1.0.7'
-  sha256 '647ce281fa3218e7d4531cac0e5b69d19136883d2b95d154b7a818c4d130cace'
+
   link 'Cakebrew.app'
 end
