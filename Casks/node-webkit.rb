37203565a27a079f6bf9dd8c8ba2c15014707ece
ndr <ndr@qef.io>
2014-06-27 02:01:32 +0200

    Reformat node-webkit.rb according to readability conventions

--- Casks/node-webkit.rb
+++ Casks/node-webkit.rb
@@ -1,7 +1,9 @@
 class NodeWebkit < Cask
-  url 'http://dl.node-webkit.org/v0.9.2/node-webkit-v0.9.2-osx-ia32.zip'
-  homepage 'https://github.com/rogerwang/node-webkit'
   version '0.9.2'
   sha256 '2133f498d2607e02a1af1b1988fb191ed0467a268afffabbeb3b99ee62384407'
+
+  url 'http://dl.node-webkit.org/v0.9.2/node-webkit-v0.9.2-osx-ia32.zip'
+  homepage 'https://github.com/rogerwang/node-webkit'
+
   link 'node-webkit.app'
 end
