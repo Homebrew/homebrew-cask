9cdf8dc8a3338e4bce6ec7bd3c5ce2d5973a1472
ndr <ndr@qef.io>
2014-06-27 02:01:52 +0200

    Reformat vmware-fusion.rb according to readability conventions

--- Casks/vmware-fusion.rb
+++ Casks/vmware-fusion.rb
@@ -1,8 +1,10 @@
 class VmwareFusion < Cask
-  url 'https://download3.vmware.com/software/fusion/file/VMware-Fusion-6.0.3-1747349-light.dmg'
-  homepage 'http://www.vmware.com/products/fusion/'
   version '6.0.3-1747349'
   sha256 'd51daed60baef6252136d9024ac6dd59df3b402bc8a9aa7cd1d663d61e6dc6d8'
+
+  url 'https://download3.vmware.com/software/fusion/file/VMware-Fusion-6.0.3-1747349-light.dmg'
+  homepage 'http://www.vmware.com/products/fusion/'
+
   binary 'VMware Fusion.app/Contents/Library/vmrun'
   link 'VMware Fusion.app'
   before_uninstall do
