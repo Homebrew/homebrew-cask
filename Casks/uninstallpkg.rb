ca3dc5d20aabc5fc577a0c9fb80d14b4fc24144c
ndr <ndr@qef.io>
2014-06-27 02:01:51 +0200

    Reformat uninstallpkg.rb according to readability conventions

--- Casks/uninstallpkg.rb
+++ Casks/uninstallpkg.rb
@@ -1,8 +1,10 @@
 class Uninstallpkg < Cask
+  version '1.0.5'
+  sha256 '199cbae47644572aade93bb81a152934178016da7cf90f3c7136b36d4819e752'
+  
   url 'http://www.corecode.at/downloads/uninstallpkg_1.0.5.zip'
   appcast 'http://www.corecode.at/uninstallpkg/uninstallpkg.xml'
   homepage 'http://www.corecode.at/uninstallpkg/'
-  version '1.0.5'
-  sha256 '199cbae47644572aade93bb81a152934178016da7cf90f3c7136b36d4819e752'
+
   link 'UninstallPKG.app'
 end
