65cf99e2baa772e199a76e025d806fcb1b825934
ndr <ndr@qef.io>
2014-06-27 02:01:05 +0200

    Reformat cornerstone.rb according to readability conventions

--- Casks/cornerstone.rb
+++ Casks/cornerstone.rb
@@ -1,8 +1,10 @@
 class Cornerstone < Cask
+  version '2.7.10'
+  sha256 '297237f24cbd605d6e7497fb893db3e7fbc6efef1333f5f25be8238a418d7650'
+  
   url 'https://www.zennaware.com/cornerstone/downloads/Cornerstone-2.7.10.zip'
   appcast 'http://www.zennaware.com/cornerstone/appcast/feed2.php'
   homepage 'http://www.zennaware.com/cornerstone/index.php'
-  version '2.7.10'
-  sha256 '297237f24cbd605d6e7497fb893db3e7fbc6efef1333f5f25be8238a418d7650'
+
   link 'Cornerstone.app'
 end
