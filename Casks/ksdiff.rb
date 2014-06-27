36a4c0ff3a56c467a4122781adc3aa43f5d329e4
ndr <ndr@qef.io>
2014-06-27 02:01:23 +0200

    Reformat ksdiff.rb according to readability conventions

--- Casks/ksdiff.rb
+++ Casks/ksdiff.rb
@@ -1,8 +1,10 @@
 class Ksdiff < Cask
-  url 'http://cdn.kaleidoscopeapp.com/releases/ksdiff-111.zip'
-  homepage 'http://www.kaleidoscopeapp.com/ksdiff2'
   version '2.0.1 (111)'
   sha256 '59f1f090135148a0ab6f04df503a5bb229c3ecd0ea5c96da19a1bf6bb1a4c930'
+
+  url 'http://cdn.kaleidoscopeapp.com/releases/ksdiff-111.zip'
+  homepage 'http://www.kaleidoscopeapp.com/ksdiff2'
+
   install 'Install ksdiff.pkg'
   uninstall :pkgutil => 'com.blackpixel.kaleidoscope.ksdiff.installer.pkg'
   # todo: conflicts_with_cask kaleidoscope
