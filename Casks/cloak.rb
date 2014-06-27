6b352d41f6a975dbb6dd2993e1d9bf829caefa5a
ndr <ndr@qef.io>
2014-06-27 02:01:04 +0200

    Reformat cloak.rb according to readability conventions

--- Casks/cloak.rb
+++ Casks/cloak.rb
@@ -1,7 +1,9 @@
 class Cloak < Cask
-  url 'https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-2.0.5.dmg'
-  homepage 'https://www.getcloak.com'
   version '2.0.5'
   sha256 'afaebc1aff58d5dbb9558c1c8bd35eab5bdcb084975404f68796b66381e1c990'
+
+  url 'https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-2.0.5.dmg'
+  homepage 'https://www.getcloak.com'
+
   link 'Cloak.app'
 end
