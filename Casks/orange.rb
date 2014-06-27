9a55ab1a50d6ff56f02cd099f872823762779fec
ndr <ndr@qef.io>
2014-06-27 02:01:33 +0200

    Reformat orange.rb according to readability conventions

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,7 +1,9 @@
 class Orange < Cask
-  url 'http://orange.biolab.si/download/Orange-2.7.5-11947.dmg'
-  homepage 'http://orange.biolab.si/'
   version '2.7.5-11947'
   sha256 'c2941bc4cdd9ea827f5c0481bf4e0c2c0582fc56f86cefa216238fbc4c1d925b'
+
+  url 'http://orange.biolab.si/download/Orange-2.7.5-11947.dmg'
+  homepage 'http://orange.biolab.si/'
+
   link 'Orange.app'
 end
