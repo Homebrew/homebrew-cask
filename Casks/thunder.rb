d66b41314b3166b045d01c2933667cf0a93a9567
ndr <ndr@qef.io>
2014-06-27 02:01:48 +0200

    Reformat thunder.rb according to readability conventions

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -1,7 +1,9 @@
 class Thunder < Cask
-  url 'http://down.sandai.net/mac/thunder_dl2.6.3.1576_Beta.dmg'
-  homepage 'http://mac.xunlei.com/'
   version '2.6.3.1576'
   sha256 '0ba42722fc50bc40211d08c0816bca780c6115e04f5ce25f2389ad85c258aaca'
+
+  url 'http://down.sandai.net/mac/thunder_dl2.6.3.1576_Beta.dmg'
+  homepage 'http://mac.xunlei.com/'
+
   link 'Thunder.app'
 end
