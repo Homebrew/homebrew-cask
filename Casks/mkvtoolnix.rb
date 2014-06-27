c2f785c493ae25193fa516a840fbc9001d82db35
ndr <ndr@qef.io>
2014-06-27 02:01:29 +0200

    Reformat mkvtoolnix.rb according to readability conventions

--- Casks/mkvtoolnix.rb
+++ Casks/mkvtoolnix.rb
@@ -1,8 +1,10 @@
 class Mkvtoolnix < Cask
+  version '7.0.0'
+  sha256 'ae0f2dff515a3b82188b50b1358023ca3a738e34a9262775fb6de6f06e7c4b00'
+
   url 'http://www.fosshub.com/download/Mkvtoolnix-7.0.0.dmg',
 	:referer => 'http://www.fosshub.com/MKVToolNix.html'
   homepage 'http://www.bunkus.org/videotools/mkvtoolnix/'
-  version '7.0.0'
-  sha256 'ae0f2dff515a3b82188b50b1358023ca3a738e34a9262775fb6de6f06e7c4b00'
+  
   link 'Mkvtoolnix-7.0.0.app'
 end
