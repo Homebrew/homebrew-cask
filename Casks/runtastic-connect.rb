8b20a1fa16f9eeb5824ceb5bab19be26060364c8
ndr <ndr@qef.io>
2014-06-27 02:01:40 +0200

    Reformat runtastic-connect.rb according to readability conventions

--- Casks/runtastic-connect.rb
+++ Casks/runtastic-connect.rb
@@ -1,8 +1,10 @@
 class RuntasticConnect < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://download.runtastic.com/connect/mac/runtasticConnect.dmg'
   appcast 'http://download.runtastic.com/connect/mac/appcast.xml'
   homepage 'https://www.runtastic.com/connect'
-  version 'latest'
-  sha256 :no_check
+
   link 'Runtastic Connect.app'
 end
