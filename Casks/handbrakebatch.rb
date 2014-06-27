0d3555fda9e5059bc41150fcf9db339b6ac443a6
ndr <ndr@qef.io>
2014-06-27 02:01:16 +0200

    Reformat handbrakebatch.rb according to readability conventions

--- Casks/handbrakebatch.rb
+++ Casks/handbrakebatch.rb
@@ -1,8 +1,10 @@
 class Handbrakebatch < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://www.osomac.com/appcasts/handbrakebatch/HandBrakeBatch.zip'
   appcast 'https://www.osomac.com/appcasts/handbrakebatch/HandBrakeBatch.xml'
   homepage 'http://www.osomac.com/apps/osx/handbrake-batch/'
-  version 'latest'
-  sha256 :no_check
+
   link 'HandBrakeBatch.app'
 end
