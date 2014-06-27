23758394aafb9fc8d2add07d15d5e832dcf5c56f
ndr <ndr@qef.io>
2014-06-27 02:00:59 +0200

    Reformat base.rb according to readability conventions

--- Casks/base.rb
+++ Casks/base.rb
@@ -1,8 +1,10 @@
 class Base < Cask
+  version 'latest'
+  sha256 :no_check
+  
   url 'http://menial.co.uk/base/download/'
   appcast 'http://update.menial.co.uk/software/base/'
   homepage 'http://menial.co.uk/base/'
-  version 'latest'
-  sha256 :no_check
+
   link 'Base.app'
 end
