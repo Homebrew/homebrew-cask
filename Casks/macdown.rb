76ac2d078b29187dee770e288e7c3cc62caf0778
Vítor Galvão <info@vitorgalvao.com>
2014-06-28 23:59:58 +0100

    made macdown easier to update

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -2,7 +2,7 @@ class Macdown < Cask
   version '0.1.2'
   sha256 '18c5c4117733a3e639189e29895568a9c2b6e33a96a2dd60c91cf892c43cdf49'
 
-  url 'http://macdown.uranusjr.com/download/v0.1.2/'
+  url "http://macdown.uranusjr.com/download/v#{version}/"
   appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml'
   homepage 'http://macdown.uranusjr.com/'
 
