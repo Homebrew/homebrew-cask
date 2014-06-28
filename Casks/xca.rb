ea1146956e6f6f722e5d9d1a183873f3bc7ca33c
Vítor Galvão <info@vitorgalvao.com>
2014-06-28 12:52:36 +0100

    corrected xca
    Fixes #3939.

--- Casks/xca.rb
+++ Casks/xca.rb
@@ -1,9 +1,8 @@
 class Xca < Cask
-  version 'latest'
-  sha256 :no_check
+  version '0.9.3'
+  sha256 'f2daef9c1be16c9ff57613b13f32d015944187c6971467e13857fe3f50889222'
 
-  url 'https://sourceforge.net/projects/xca/files/latest/download',
-    :user_agent => :fake
+  url "http://downloads.sourceforge.net/sourceforge/xca/xca-#{version}_x86.dmg"
   homepage 'http://xca.sourceforge.net/'
 
   link 'xca.app'
