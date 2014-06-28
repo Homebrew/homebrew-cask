de1f1341d7b456b77b77f1833d5456195427c88e
elnappo <elnappo@users.noreply.github.com>
2014-06-29 00:22:30 +0200

    Updated Macdown to version 0.1.2

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -1,8 +1,8 @@
 class Macdown < Cask
-  version '0.1.1'
-  sha256 'b716963c19b006421d770350139e822e8f891d259a1fe999067854150176268a'
+  version '0.1.2'
+  sha256 '18c5c4117733a3e639189e29895568a9c2b6e33a96a2dd60c91cf892c43cdf49'
 
-  url 'http://macdown.uranusjr.com/download/v0.1.1/'
+  url 'http://macdown.uranusjr.com/download/v0.1.2/'
   appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml'
   homepage 'http://macdown.uranusjr.com/'
 
