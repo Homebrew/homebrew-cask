615177b287db7f3d1b163e91e89b4648773b9140
Vítor Galvão <info@vitorgalvao.com>
2014-06-28 22:12:45 +0100

    updated macaw to 1.0.13

--- Casks/macaw.rb
+++ Casks/macaw.rb
@@ -1,6 +1,6 @@
 class Macaw < Cask
-  version '1.0.6'
-  sha256 '20b380d29a155aec27827771c0c6d099a2bd8ee4b8aaa8c3ba9a291248ecdd3e'
+  version '1.0.13'
+  sha256 'b7c0ff0a0ae178b64fa100790f0a790e578c3ab272dc493fd12ed288be2f28ad'
 
   url "http://download.macaw.co/#{version}/Macaw#{version}.dmg"
   appcast 'http://download.macaw.co/appcast.xml'
