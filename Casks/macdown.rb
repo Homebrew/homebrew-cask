be1128876524a50a7a485b0a086f44f4fe78cad3
Thomas Sturm <ths@nerdli.ch>
2014-06-28 20:38:27 +1000

    Add MacDown.app 0.1.2
    MacDown is a markdown editor written by Tzu-ping Chung

---
+++ Casks/macdown.rb
@@ -0,0 +1,7 @@
+class Macdown < Cask
+  url 'http://macdown.uranusjr.com/download/v0.1.2/'
+  homepage 'http://macdown.uranusjr.com/'
+  version '0.1.2'
+  sha256 '18c5c4117733a3e639189e29895568a9c2b6e33a96a2dd60c91cf892c43cdf49'
+  link 'MacDown.app'
+end
