a3bb64413216d57e94844afebc74d86b858d95e1
Cameron Eagans <cweagans@gmail.com>
2014-06-29 13:41:04 -0600

    Adding Phpstorm EAP cask

---
+++ Casks/phpstorm-eap.rb
@@ -0,0 +1,9 @@
+class PhpstormEap < Cask
+  version '138.826'
+  sha256 'c22869c4c1e4e0a83ab89e1e4cdafe5b2a6eead22d2ee3463a2b0fdb6113eb19'
+
+  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-138.826.dmg'
+  homepage 'http://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
+
+  link 'PhpStorm EAP.app'
+end
