937b74abd922e8e6e9b764e6c090cf3739a93a53
Cameron Eagans <cweagans@gmail.com>
2014-06-29 15:47:29 -0600

    Use #{version} for building the URL to Phpstorm EAP

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -2,7 +2,7 @@ class PhpstormEap < Cask
   version '138.826'
   sha256 'c22869c4c1e4e0a83ab89e1e4cdafe5b2a6eead22d2ee3463a2b0fdb6113eb19'
 
-  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-138.826.dmg'
+  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-#{version}.dmg'
   homepage 'http://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
 
   link 'PhpStorm EAP.app'
