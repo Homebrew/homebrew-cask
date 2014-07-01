b8109d50eacc3dabc9b826c3cb1aaffd7cfa9a29
Vítor Galvão <info@vitorgalvao.com>
2014-07-01 01:12:59 +0100

    added `:format => :unknown` to epub-to-pdf
    As per [comment in 5057](https://github.com/caskroom/homebrew-cask/issues/5057#issuecomment-47093426).

--- Casks/epub-to-pdf.rb
+++ Casks/epub-to-pdf.rb
@@ -1,10 +1,10 @@
 class EpubToPdf < Cask
-  # non-Sparkle appcast
-  appcast 'https://code.google.com/feeds/p/epub-2-pdf/downloads/basic'
   version '3.1'
   sha256 'dcfc59d57f756802e844614b7dae43bca67284ec85fe6b909f244e41f20987b3'
 
   url 'http://epub-2-pdf.googlecode.com/files/e2p-3.dmg'
+  appcast 'https://code.google.com/feeds/p/epub-2-pdf/downloads/basic',
+          :format => :unknown
   homepage 'https://code.google.com/p/epub-2-pdf'
 
   link 'epub-to-pdf.app'
