a6cfcba25ab74d6a8b6ad4f84267ca7fdb257a06
Vítor Galvão <info@vitorgalvao.com>
2014-07-01 01:34:39 +0100

    reseting comment on non-sparkle appcast in epub-to-pdf, until new version ships

--- Casks/epub-to-pdf.rb
+++ Casks/epub-to-pdf.rb
@@ -3,8 +3,8 @@ class EpubToPdf < Cask
   sha256 'dcfc59d57f756802e844614b7dae43bca67284ec85fe6b909f244e41f20987b3'
 
   url 'http://epub-2-pdf.googlecode.com/files/e2p-3.dmg'
-  appcast 'https://code.google.com/feeds/p/epub-2-pdf/downloads/basic',
-          :format => :unknown
+  # non-Sparkle appcast
+  appcast 'https://code.google.com/feeds/p/epub-2-pdf/downloads/basic'
   homepage 'https://code.google.com/p/epub-2-pdf'
 
   link 'epub-to-pdf.app'
