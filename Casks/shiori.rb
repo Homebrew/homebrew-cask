ac5caf5301deaa5b8c6f1b67fb4f60a53fcd7428
Kane Dou <douqilong@gmail.com>
2014-07-01 14:07:34 +0800

    update Shiori to 1.0.2

--- Casks/shiori.rb
+++ Casks/shiori.rb
@@ -1,8 +1,8 @@
 class Shiori < Cask
-  version '1.0.1'
-  sha256 'e6ca6cbd9502de280303cb46deade6a6b938da7bae3ecd99bf7b23c539747c26'
+  version '1.0.2'
+  sha256 'c832edc2762fdaadc80bfe042ee6f7bd128955e3d7b27bdc5baf5806a0c8a719'
 
-  url 'http://aki-null.net/shiori/release/Shiori_1.0.1.zip'
+  url 'http://aki-null.net/shiori/release/Shiori_1.0.2.zip'
   appcast 'http://aki-null.net/shiori/appcast.xml'
   homepage 'http://aki-null.net/shiori/'
 
