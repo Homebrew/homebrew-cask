a43924f2f6bd040ed0bc3697cf79f74e8762b238
Toni Förster <toni.foerster@gmail.com>
2014-07-02 12:18:23 +0200

    no message

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -2,7 +2,11 @@ class Owncloud < Cask
   version '1.6.1.359'
   sha256 '225ca73e3d57a56c68ba28cc9ea47aa7eb7f5abc00841528d5ee3f8a371fb0d3'
 
+<<<<<<< HEAD
   url "https://download.owncloud.com/desktop/stable/ownCloud-#{version}.dmg"
+=======
+  url 'https://download.owncloud.com/desktop/stable/ownCloud-1.6.1.359.dmg'
+>>>>>>> a03dfdbe41880364b014364c17f61a32f2456b24
   homepage 'http://owncloud.com'
 
   link 'owncloud.app'
