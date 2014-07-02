7623c93b2ff9f831d809d7bb414acc1402eebf3e
Martin madsen <martin2madsen@gmail.com>
2014-07-02 22:55:28 +0200

    Update YNAB 4 from 4.3.451 to 4.3.541
    Redo of PR #5084 overwritten by commit d4605c8.

--- Casks/ynab.rb
+++ Casks/ynab.rb
@@ -1,8 +1,8 @@
 class Ynab < Cask
-  version '4.3.451'
-  sha256 '088f61e0e772193f6dac61e22067b1696246f214d7c9887f95d180cfabb57e25'
+  version '4.3.541'
+  sha256 '38ec8a050fde9be1375f3fe184988eb81358b3a26b74c308e67bb872ac5e2d50'
 
-  url 'https://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.451.dmg'
+  url 'https://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.541.dmg'
   homepage 'http://www.youneedabudget.com/'
 
   link 'YNAB 4.app'
