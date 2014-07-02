5270eac28f7242fe109bc5c0a5e15feed7c2a5f4
Lee Hanxue <hanxue.developer@gmail.com>
2014-07-02 18:42:55 +0800

    Update MacDown 0.1.2
    Moved the stanzas to follow current format

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -1,8 +1,9 @@
 class Macdown < Cask
-  url 'http://macdown.uranusjr.com/download/v0.1/'
+  version '0.1.2'
+  sha256 '18c5c4117733a3e639189e29895568a9c2b6e33a96a2dd60c91cf892c43cdf49'
+
+  url 'http://macdown.uranusjr.com/download/v0.1.2/'
   appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml'
   homepage 'http://macdown.uranusjr.com/'
-  version '0.1'
-  sha256 'd050ee502fb27fd0f431abc1cc2f4a490670346821e269415d184bf08f245eb2'
   link 'MacDown.app'
 end
