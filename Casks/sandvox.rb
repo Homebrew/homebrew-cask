d2e1a1bf9fbaeec0df6c5a278ceb4518e588c0e6
Caleb Xu <calebcenter@live.com>
2014-07-03 14:32:49 -0400

    Add Sandvox (latest)

---
+++ Casks/sandvox.rb
@@ -0,0 +1,9 @@
+class Sandvox < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://www.karelia.com/files/8/Sandvox.dmg'
+  homepage 'http://www.karelia.com/products/sandvox/'
+
+  link 'Sandvox.app'
+end
