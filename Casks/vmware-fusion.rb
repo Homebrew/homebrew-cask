a7f9501e3c085ca174be83b48b143d382e97f5fc
Havanna <registerdedicated@yeah.net>
2014-07-03 19:05:00 +0800

    Update vmware-fusion.rb

--- Casks/vmware-fusion.rb
+++ Casks/vmware-fusion.rb
@@ -2,7 +2,7 @@ class VmwareFusion < Cask
   version '6.0.4-1887983'
   sha256 'fe392850cd3578b101b3601f19704485fbb66ce1c5fa1367e464a0cf91e41a68'
 
-  url 'https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}-light.dmg'
+  url 'https://download3.vmware.com/software/fusion/file/VMware-Fusion-6.0.4-1887983-light.dmg'
   homepage 'http://www.vmware.com/products/fusion/'
 
   binary 'VMware Fusion.app/Contents/Library/vmrun'
