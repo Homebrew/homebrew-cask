25ea2ed8348900dc6884c84e888eb48e2b5a58ab
Steven Degutis <steven@cleancoders.com>
2014-07-04 17:44:47 -0500

    Update Hydra to beta 7

--- Casks/hydra.rb
+++ Casks/hydra.rb
@@ -1,8 +1,8 @@
 class Hydra < Cask
-  version '1.0 beta 6'
-  sha256 'b1d16db3b6a38b85c7a60b81e1eab98c47ca417263672847e73021df885c16cb'
+  version '1.0 beta 7'
+  sha256 '4cc3a9094125fbc94fc86b67b62f5d59ce0bb28937765f74a1813ac8554f0aba'
 
-  url 'https://github.com/sdegutis/hydra/releases/download/v1.0.b6/Hydra-1.0.0.b6.app.tar.gz'
+  url 'https://github.com/sdegutis/hydra/releases/download/v1.0.b7/Hydra-v1.0.b7.app.tar.gz'
   homepage 'https://github.com/sdegutis/hydra'
 
   link 'Hydra.app'
