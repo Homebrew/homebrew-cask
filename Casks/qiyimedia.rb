a93faad21c05f09d866c17328792709d5c2905fd
MrGba2z <MrRegister@outlook.com>
2014-07-04 17:02:37 +0800

    Add 爱奇艺视频.app v2.6.0

---
+++ Casks/qiyimedia.rb
@@ -0,0 +1,9 @@
+class Qiyimedia < Cask
+  version '2.6.0'
+  sha256 '9d66ba83afa84decb5b1a8a5f0032c6f1aae24d05ddf6da2f07ce5e3702e0033'
+
+  url 'http://static.qiyi.com/ext/common/QIYImedia_Mac_5.dmg'
+  homepage 'http://www.iqiyi.com'
+
+  link '爱奇艺视频.app'
+end
