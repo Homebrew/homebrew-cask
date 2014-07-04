391067fe88b158e2092558dc8eb109b0377bc0af
Guilherme Paula <guilhermepaula@users.noreply.github.com>
2014-07-04 00:41:20 -0300

    Updating EclipseJee version to Luna (4.4)
    Configuration like eclipse-java, eclipse-platform and eclipse-ide.

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -1,14 +1,12 @@
 class EclipseJee < Cask
-  version 'latest'
-  sha256 :no_check
-
   if Hardware::CPU.is_64_bit?
-    url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/SR2/eclipse-jee-kepler-SR2-macosx-cocoa-x86_64.tar.gz'
+    url 'http://download.eclipse.org/technology/epp/downloads/release/luna/R/eclipse-jee-luna-R-macosx-cocoa-x86_64.tar.gz'
+    sha256 '6767a56d906ec79d1ab58158d1815cc3cee15a82ce3805309164b8466ca45d79'
   else
-    url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/SR2/eclipse-jee-kepler-SR2-macosx-cocoa.tar.gz'
+    url 'http://download.eclipse.org/technology/epp/downloads/release/luna/R/eclipse-jee-luna-R-macosx-cocoa.tar.gz'
+    sha256 '97141e1fb9230d81eb3a59788e7941d062cfcfafcb781c8dde1cc561aa914b8a'
   end
-
+  version '4.4.0'
   homepage 'http://eclipse.org/'
-
   link 'eclipse/Eclipse.app'
 end
