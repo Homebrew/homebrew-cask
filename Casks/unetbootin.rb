3c14cfcc6162a712ac674bef9f6debeb106a3a5e
Vítor Galvão <info@vitorgalvao.com>
2014-07-05 13:41:55 +0100

    set unetbootin to latest

--- Casks/unetbootin.rb
+++ Casks/unetbootin.rb
@@ -1,8 +1,8 @@
 class Unetbootin < Cask
-  version '585'
-  sha256 '2a6cc88feb01b666710c094b49b4cbb4e33a301d699b1bd4dbd229bfa0b5572e'
+  version 'latest'
+  sha256 :no_check
 
-  url 'https://downloads.sourceforge.net/sourceforge/unetbootin/unetbootin-mac-585.zip'
+  url 'http://sourceforge.net/projects/unetbootin/files/latest/download'
   homepage 'http://unetbootin.sourceforge.net/'
 
   link 'unetbootin.app'
