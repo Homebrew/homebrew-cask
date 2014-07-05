0e0352c21a4cef21274d6c6eb635ad126adb1cad
marudor <marudor@marudor.de>
2014-07-05 21:16:01 +0200

    Add Dockdrop.app 1.5.3
    This app lets you upload stuff via scp/ftp/webdav just by dropping it to your dock!

---
+++ Casks/dockdrop.rb
@@ -0,0 +1,9 @@
+class Dockdrop < Cask
+  version '1.5.3'
+  sha256 '34d29b02a02e84abed6121024e4b0a1a5e8bdaf1d568fffa7684bc72f76b75dc'
+
+  url 'https://marudor.de/cask/dockdrop/Dockdrop-1.5.3.dmg'
+  homepage ''
+
+  link 'Dockdrop.app'
+end
