0726bacf133774da31ecd7981b31dd9b8b698aad
Toni Förster <toni.foerster@gmail.com>
2014-07-05 23:54:06 +0200

    updated the file according to fernandos advice

--- Casks/moneyplex.rb
+++ Casks/moneyplex.rb
@@ -1,11 +1,10 @@
 class Moneyplex < Cask
-  version '12.0'
-  sha256 'e3eb8261bf5ba93e7189429007112a00bc8791baff2641013f48b8fc97526d65'
+  version 'latest'
+  sha256 :no_check
 
   url 'http://www.matrica.com/download/mac/moneyplex.dmg'
   homepage 'http://www.matrica.de/'
 
-  install 'moneyplex.pkg', :allow_untrusted => true
-  
+  install 'moneyplex.pkg', :allow_untrusted => true  
   uninstall :pkgutil => 'de.matrica.moneyplex.Moneyplex.pkg'
 end
