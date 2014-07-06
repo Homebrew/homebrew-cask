76a5e4476ef042a5eaa054d33b3251e9ed622caf
Florian Harbecke <florian@harbecke.me>
2014-07-06 19:25:11 +0200

    Updated OSX Fuse to Version 2.7
    also cleaned up cask-file

--- Casks/osxfuse.rb
+++ Casks/osxfuse.rb
@@ -1,11 +1,11 @@
 class Osxfuse < Cask
-  version '2.6.4'
-  sha256 '942ca4ac026e4545b915308b01e6706be04483d48e1257664add37564122e4ca'
+  version '2.7.0'
+  sha256 'fab4c8d16d0fc6995826d74f2c0ab04cd7264b00c566d5cc3b219bd589da8114'
 
-  url 'https://downloads.sourceforge.net/project/osxfuse/osxfuse-2.6.4/osxfuse-2.6.4.dmg'
-  homepage 'http://osxfuse.github.io/'
+  url "https://downloads.sourceforge.net/project/osxfuse/osxfuse-#{version}/osxfuse-#{version}.dmg"
+  homepage 'https://osxfuse.github.io/'
 
-  install 'Install OSXFUSE 2.6.pkg'
+  install "Install OSXFUSE #{version[0..-3]}.pkg"
   uninstall :pkgutil => 'com.github.osxfuse.pkg.Core|com.github.osxfuse.pkg.PrefPane',
             :kext => 'com.github.osxfuse.filesystems.osxfusefs'
 end
