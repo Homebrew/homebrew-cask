7a6d9f69e57d78f17d91ab2f0791db71b3d4114c
Beng Hee Eu <beng@benghee.eu>
2014-07-06 10:53:40 +0800

    LaunchBar 6.0.2

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -1,8 +1,8 @@
 class Launchbar < Cask
-  version '6.0.1'
-  sha256 'a0279114a37bfa5ccf423693ecc142c00c9cc86efdde163f42455abe153c98a9'
+  version '6.0.2'
+  sha256 'ce6ac67e32d805895ac98cf1026d6a94d16c103cd5c728b990f845aafaecad7d'
 
-  url 'http://www.obdev.at/downloads/launchbar/LaunchBar-6.0.1.dmg'
+  url "http://www.obdev.at/downloads/launchbar/LaunchBar-#{version}.dmg"
   homepage 'http://www.obdev.at/products/launchbar/'
 
   link 'LaunchBar.app'
