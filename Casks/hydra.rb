1ecae5542e7543b30402c7e9f8c1b5e360ab3c41
Chris Jones <cmsj@tenshu.net>
2014-07-06 19:58:23 +0100

    Upgrade Hydra to 1.0 beta 8.
    New upstream release.

--- Casks/hydra.rb
+++ Casks/hydra.rb
@@ -1,8 +1,8 @@
 class Hydra < Cask
-  version '1.0 beta 7'
-  sha256 '4cc3a9094125fbc94fc86b67b62f5d59ce0bb28937765f74a1813ac8554f0aba'
+  version '1.0 beta 8'
+  sha256 '083dce1b5370d88bdf1ece3d3608cbcd28b95543d612797c746ad5b628b9067c'
 
-  url 'https://github.com/sdegutis/hydra/releases/download/v1.0.b7/Hydra-v1.0.b7.app.tar.gz'
+  url 'https://github.com/sdegutis/hydra/releases/download/v1.0.b8/Hydra-v1.0.b8.app.tar.gz'
   homepage 'https://github.com/sdegutis/hydra'
 
   link 'Hydra.app'
