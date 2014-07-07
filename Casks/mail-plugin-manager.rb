7f146560333196d390194994c2b98232f01fbce1
Hugo Haas <hugoh@hugoh.net>
2014-07-07 05:45:08 -0700

    Add Mail Plugin Manager v2.0.6

---
+++ Casks/mail-plugin-manager.rb
@@ -0,0 +1,9 @@
+class MailPluginManager < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://www.chungwasoft.com/files/MPM_latest.zip'
+  homepage 'http://www.chungwasoft.com/mailpluginmanager/'
+
+  link 'Mail Plugin Manager.app'
+end
