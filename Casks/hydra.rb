70f402e6ec22ae46fe11e1f99703e57257d2c679
Vítor Galvão <info@vitorgalvao.com>
2014-07-07 19:54:10 +0100

    made hydra easier to update

--- Casks/hydra.rb
+++ Casks/hydra.rb
@@ -1,8 +1,8 @@
 class Hydra < Cask
-  version '1.0 beta 9'
+  version '1.0.b9'
   sha256 '24e8c77e566253eafcb27e04045d236dc5f484c387c7897242eff42e5d08af13'
 
-  url 'https://github.com/sdegutis/hydra/releases/download/v1.0.b9/Hydra-v1.0.b9.app.zip'
+  url "https://github.com/sdegutis/hydra/releases/download/v#{version}/Hydra-v#{version}.app.zip"
   homepage 'https://github.com/sdegutis/hydra'
 
   link 'Hydra.app'
