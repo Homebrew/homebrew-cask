20686a0ef95120ccf97086fba9abf145a48d38fb
Vítor Galvão <info@vitorgalvao.com>
2014-07-08 13:12:37 +0100

    updated mono-mre (3.4.0)

--- Casks/mono-mre.rb
+++ Casks/mono-mre.rb
@@ -2,9 +2,9 @@ class MonoMre < Cask
   version '3.4.0'
   sha256 '3f4b805513c564e551e42d7043f783d29e7580617fda8ee974f6841c8b358c91'
 
-  url 'http://download.mono-project.com/archive/3.4.0/macos-10-x86/MonoFramework-MRE-3.4.0.macos10.xamarin.x86.pkg'
+  url "http://download.mono-project.com/archive/#{version}/macos-10-x86/MonoFramework-MRE-#{version}.macos10.xamarin.x86.pkg"
   homepage 'http://mono-project.com'
 
-  install 'MonoFramework-MRE-3.4.0.macos10.xamarin.x86.pkg'
+  install "MonoFramework-MRE-#{version}.macos10.xamarin.x86.pkg"
   uninstall :pkgutil => 'com.xamarin.mono-MRE.pkg'
 end
