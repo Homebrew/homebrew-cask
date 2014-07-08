78d3c35dae3fc45757bdab7a9cedb9ec6d2c0344
Fernando Paredes <nano@fdp.io>
2014-07-08 10:22:14 -0700

    Iconping: Conform to new layout

--- Casks/iconping.rb
+++ Casks/iconping.rb
@@ -1,7 +1,9 @@
 class Iconping < Cask
-  url 'http://antirez.com/iconping/iconping-1.02.app.dmg'
-  homepage 'http://antirez.com/iconping/'
   version '1.02'
   sha256 '9f02c99b360ed2a0a424e1bdeb4f810ffb120a3cf78b6889b8a6c5b29b06d0c9'
+
+  url 'http://antirez.com/iconping/iconping-1.02.app.dmg'
+  homepage 'http://antirez.com/iconping/'
+
   link 'iconping.app'
 end
