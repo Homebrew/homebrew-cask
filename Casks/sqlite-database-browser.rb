f0b8544e594fce829f38ded840dabf9427492c23
Fernando Paredes <nano@fdp.io>
2014-07-09 10:40:55 -0700

    SQLite Database Browser: Make easier to modify

--- Casks/sqlite-database-browser.rb
+++ Casks/sqlite-database-browser.rb
@@ -1,7 +1,9 @@
 class SqliteDatabaseBrowser < Cask
   version '3.2.0'
-  url 'https://github.com/sqlitebrowser/sqlitebrowser/releases/download/sqlb-3.2.0/sqlitebrowser-3.2.0.dmg'
   sha256 '77ff563d108dc2bf224b3628172df31238fe2d74d7a2e1954b120463a865c4d8'
+
+  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/sqlb-#{version}/sqlitebrowser-#{version}.dmg"
   homepage 'http://sqlitebrowser.org'
-  link 'SQLite Database Browser 3.2.0.app'
+
+  link "SQLite Database Browser #{version}.app"
 end
