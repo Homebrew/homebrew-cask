2583bc01e76fa3fee4d603d18473f9428cc6d1c7
TheCompander <thilina.arachchi@gmnail.com>
2014-07-08 17:32:52 -0700

    Add chirp-0.4.0.app
    CHIRP is a free, open-source tool for programming your
    amateur radio. It supports a large number of manufacturers and models,
    as well as provides a way to interface with multiple data sources and
    formats.

---
+++ Casks/chirp.rb
@@ -0,0 +1,9 @@
+class Chirp < Cask
+  version '0.4.0'
+  sha256 '76842b583f0dbb19cd700a156893b0cd1356dde18bbe730261fdf786772a26f0'
+
+  url 'http://chirp.danplanet.com/download/0.4.0/chirp-0.4.0.app.zip'
+  homepage 'http://chirp.danplanet.com/'
+
+  link 'chirp-0.4.0.app'
+end
