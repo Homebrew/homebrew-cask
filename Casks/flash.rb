db8805ac3c4926fa30b162f087715027097c9ef4
blogabe <abe@homer.local>
2014-07-09 10:56:01 -0700

    adobe flash player update

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,11 +1,11 @@
 class Flash < Cask
-  version '14.0.0.125'
-  sha256 '30fe1064b2b4864afd5adf26c08981c8d5351a507fbcc3fc5f2bb6ef7e352384'
+  version '14.0.0.145'
+  sha256 'c0c13f43692530cadcd8aab5552fa9f235d45d793d6feb820b95ac948155dce3'
 
-  url 'https://download.macromedia.com/pub/flashplayer/installers/archive/fp_14.0.0.125_archive.zip'
+  url 'https://download.macromedia.com/pub/flashplayer/installers/archive/fp_14.0.0.145_archive.zip'
   homepage 'http://get.adobe.com/flashplayer/'
 
-  nested_container 'fp_14.0.0.125_archive/14_0_r0_125/flashplayer14_0r0_125_mac.dmg'
+  nested_container 'fp_14.0.0.145_archive/14_0_r0_145/flashplayer14_0r0_145_mac.dmg'
   install 'Install Adobe Flash Player.app/Contents/Resources/Adobe Flash Player.pkg'
   uninstall :pkgutil => 'com.adobe.pkg.FlashPlayer',
             :files => '/Library/Internet Plug-Ins/Flash Player.plugin'
