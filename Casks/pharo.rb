30e16ade307f7dc1c512574c439f1d41fa2c7634
Justin Campbell <justin@justincampbell.me>
2014-07-09 15:53:19 -0400

    Update Pharo 3.0 SHA

--- Casks/pharo.rb
+++ Casks/pharo.rb
@@ -1,6 +1,6 @@
 class Pharo < Cask
   version '3.0'
-  sha256 '57f251d31726176bf7dff866198ca75f03bb477e1dd024307eb157725acf596f'
+  sha256 'd2e643440f4d768e0659ed01007ac0c061a0f93d6982621ea77a0662699cebfa'
 
   url 'http://files.pharo.org/platform/Pharo3.0-mac.zip'
   homepage 'http://www.pharo-project.org/home'
