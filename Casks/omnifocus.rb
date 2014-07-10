8686016106874d2316701152eedf2806b84b4bf9
Benjamin Nørgaard <benjamin@webtoad.dk>
2014-07-10 10:22:31 +0200

    Fix download protocol in OmniFocus.app Cask
    Changed https to http because the former has stopped working

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -2,7 +2,7 @@ class Omnifocus < Cask
   version 'latest'
   sha256 :no_check
 
-  url 'https://www.omnigroup.com/download/latest/omnifocus'
+  url 'http://www.omnigroup.com/download/latest/omnifocus'
   homepage 'http://www.omnigroup.com/products/omnifocus/'
 
   link 'OmniFocus.app'
