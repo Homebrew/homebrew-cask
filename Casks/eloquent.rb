4f7916d99f4558a226aa818c1739955abfa1d1ec
Wes Widner <gwwidner@intelligent-es.com>
2014-07-10 14:04:18 -0400

    adding a cask for the open source bible study app, eloquent

---
+++ Casks/eloquent.rb
@@ -0,0 +1,7 @@
+class Eloquent < Cask
+  url 'https://launchpad.net/eloquent/trunk/2.4.8/+download/Eloquent-2.4.8.app.zip'
+  homepage 'https://launchpad.net/eloquent'
+  version '2.4.8'
+  sha256 '3116ce1e74a6305ad60bb60ba010ccd864f3dce71eaa1a64fee2791651119857'
+  link 'Eloquent.app'
+end
