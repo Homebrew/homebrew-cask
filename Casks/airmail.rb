9b9ccaade28d526c965ab7bb6defcd1d0b06f326
lufeihaidao <lufeihaidao+github@gmail.com>
2014-07-10 10:36:39 +0800

    Update airmail.rb

--- Casks/airmail.rb
+++ Casks/airmail.rb
@@ -1,5 +1,5 @@
 class Airmail < Cask
-  version '1.4 (246)'
+  version 'latest'
   sha256 :no_check
   
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/143?format=zip'
