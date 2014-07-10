4635058c1aaadfc28d713f19a3b3eeba9f274d4d
Lee Hanxue <hanxue.developer@gmail.com>
2014-07-10 19:08:12 +0800

    Update Boot2Docker 1.1.1

--- Casks/boot2docker.rb
+++ Casks/boot2docker.rb
@@ -1,10 +1,10 @@
 class Boot2docker < Cask
-  version '1.1.0'
-  sha256 '6a98e0311557199a71b23d596a0547e3a7902a1a04f42905c6141a08f2a1bb7d'
+  version '1.1.1'
+  sha256 'cde211e758737d290d927afc8167c55ebb490c5569998332fb0a524d5f7a02eb'
 
-  url 'https://github.com/boot2docker/osx-installer/releases/download/v1.1.0/Boot2Docker-1.1.0.pkg'
+  url 'https://github.com/boot2docker/osx-installer/releases/download/v1.1.1/Boot2Docker-1.1.1.pkg'
   homepage 'https://github.com/boot2docker/osx-installer'
 
-  install 'Boot2Docker-1.1.0.pkg'
+  install 'Boot2Docker-1.1.1.pkg'
   uninstall :pkgutil => ['io.boot2docker.pkg.boot2docker', 'io.boot2docker.pkg.boot2dockerapp', 'io.boot2dockeriso.pkg.boot2dockeriso', 'io.docker.pkg.docker']
 end
