897af7e03c6d1521c455057aec15dbed99b7a3db
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-07-11 16:01:24 +0800

    Fix Simple Comic sha.

--- Casks/simple-comic.rb
+++ Casks/simple-comic.rb
@@ -1,6 +1,6 @@
 class SimpleComic < Cask
   version '1.7.252'
-  sha256 '06b05ca40dbd4833b6d30e9dbc8840237a476f7bc91970867e3de5c47f72a0b4'
+  sha256 '4ddd18a02a79fc8201824e6ab99291c6d4c8680f79f94bc372bf71f0535def35'
 
   url 'http://dancingtortoisedownload.s3.amazonaws.com/SimpleComic_1.7_252.zip'
   homepage 'http://dancingtortoise.com/simplecomic/'
