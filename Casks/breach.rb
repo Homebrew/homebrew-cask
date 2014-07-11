c771914920e33f3dee79854bc1ac1630a6d84822
Vincent Klaiber <vincent@schimpanz.com>
2014-07-11 08:41:51 +0200

    Add Breach version 0.3.20
    A browser for the HTML5 era. Entirely written in Javascript. Free. Modular. Hackable. http://breach.cc/

---
+++ Casks/breach.rb
@@ -0,0 +1,9 @@
+class Breach < Cask
+  version '0.3.20'
+  sha256 '7b2389932bf32a48c4bb485a5bbe0f80fb49ae5c85e7d8319a51aec14714d4e1'
+
+  url 'https://raw.githubusercontent.com/breach/releases/master/v0.3.20-alpha.5/breach-v0.3.20-alpha.5-darwin-ia32.tar.gz'
+  homepage 'http://breach.cc'
+
+  link 'breach-v0.3.20-alpha.5-darwin-ia32/Breach.app'
+end
