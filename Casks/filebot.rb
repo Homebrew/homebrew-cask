6530fdad1d79459089c56c2cb6e4978858382acb
Vítor Galvão <info@vitorgalvao.com>
2014-07-11 16:42:09 +0100

    fixed filebot url

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -3,7 +3,7 @@ class Filebot < Cask
   sha256 '4aa3dd0532bd0266ec3bcb60553507dfb51c054a406fba95bd0c6d5c0dce7ec2'
 
   homepage 'http://www.filebot.net/'
-  url "https://downloads.sourceforge.net/project/filebot/filebot/HEAD/FileBot_#{version}/FileBot_#{version.gsub(/\.\d/, '')}.app.tar.gz"
+  url "https://downloads.sourceforge.net/project/filebot/filebot/HEAD/FileBot_#{version}/FileBot_#{version.gsub(/\.\d$/, '')}.app.tar.gz"
 
   link 'FileBot.app'
   binary 'FileBot.app/Contents/MacOS/filebot.sh', :target => 'filebot'
