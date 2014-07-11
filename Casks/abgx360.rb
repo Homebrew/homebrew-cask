e8a927aff315d13e8412571b45618f3c7a6d2046
Wes Widner <gwwidner@intelligent-es.com>
2014-07-11 19:33:43 -0400

    Add abgx360 app v1.0.6

---
+++ Casks/abgx360.rb
@@ -0,0 +1,14 @@
+class Abgx360 < Cask
+  version '1.0.6'
+  sha256 'b2b066d1500e6c6a64d103b2c1a9e1760be0a4e16df314b4868024c19df8c741'
+
+  url 'http://dl.dropbox.com/u/59058148/abgx360-1.0.6.pkg'
+  homepage 'http://abgx360.net/'
+
+  install 'abgx360-1.0.6.pkg'
+  uninstall :quit => 'net.abgx360.abgx360gui',
+            :pkgutil => [
+                         'net.abgx360.abgx360.pkg',
+                         'net.abgx360.abgx360gui.pkg',
+                        ]
+end
