67dc656fb446b7d4a07a349efd3e77e1c21d9557
Tolbkni Kao <tolbkni@gmail.com>
2014-07-13 14:50:49 +0800

    Upgrade Android Studio.app to v0.8.2 build-135.1267975

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,8 +1,8 @@
 class AndroidStudio < Cask
-  version '0.8.1 build-135.1248636'
-  sha256 '1ed112d2d6230b88c57a29ceda59ff473e6c0005e5e371f4480c93e7187c3567'
+  version '0.8.2 build-135.1267975'
+  sha256 '55a20e6f9a9e76468344aacf2909b6db224fa3a87e4eac0d76d277ae8c479f89'
 
-  url 'https://dl.google.com/dl/android/studio/ide-zips/0.8.1/android-studio-ide-135.1248636-mac.zip'
+  url 'https://dl.google.com/dl/android/studio/ide-zips/0.8.2/android-studio-ide-135.1267975-mac.zip'
   homepage 'https://developer.android.com/sdk/installing/studio.html'
 
   link 'Android Studio.app'
