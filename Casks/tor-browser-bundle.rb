04b487d7352954ca04a9c9d954bf20333a55c6cc
Josh <kaizoku@phear.cc>
2014-07-14 16:11:03 -0700

    Add Tor Browser Bundle 3.6.2

---
+++ Casks/tor-browser-bundle.rb
@@ -0,0 +1,9 @@
+class TorBrowserBundle < Cask
+  version '3.6.2'
+
+  url 'https://www.torproject.org/dist/torbrowser/3.6.2/TorBrowser-3.6.2-osx32_en-US.dmg'
+  sha256 '30778b5d164514c971cc4019ac8724f1aa4a4adc1d9cfcb07a50ce76179259c9'
+  homepage 'https://www.torproject.org/projects/torbrowser.html.en'
+
+  link 'TorBrowser.app'
+end
