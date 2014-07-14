20b37a98b454db8039ed5128192279c5839b5ba6
Steven Degutis <steven@cleancoders.com>
2014-07-14 14:57:42 -0500

    Removing Hydra per Hydra installation instructions
    See https://github.com/sdegutis/hydra/#install.

--- Casks/hydra.rb
+++
@@ -1,9 +0,0 @@
-class Hydra < Cask
-  version '0.13'
-  sha256 '837bc4050a765b53685ca30d6ceaae240c3070fbf4f9194634b01ee90d45e294'
-
-  url "https://github.com/sdegutis/hydra/releases/download/#{version}/Hydra-#{version}.zip"
-  homepage 'https://github.com/sdegutis/hydra'
-
-  link 'Hydra.app'
-end
