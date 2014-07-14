6066b622a4e3aefc159489ec71951ba12dabef79
Radek Simko <radek_simko@ipcmedia.com>
2014-07-14 10:50:02 +0100

    Chirp - version number reused

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -2,8 +2,8 @@ class Chirp < Cask
   version '0.4.0'
   sha256 '76842b583f0dbb19cd700a156893b0cd1356dde18bbe730261fdf786772a26f0'
 
-  url 'http://chirp.danplanet.com/download/0.4.0/chirp-0.4.0.app.zip'
+  url "http://chirp.danplanet.com/download/#{version}/chirp-#{version}.app.zip"
   homepage 'http://chirp.danplanet.com/'
 
-  link 'chirp-0.4.0.app'
+  link "chirp-#{version}.app"
 end
