fa236340a24a71ee5161a74fb310550db3f31259
Junior Hsu <junxy@users.noreply.github.com>
2014-07-14 16:40:47 +0800

    Update navicat-premium.rb
    Updated navicat-premium version: 11.0.18 to 11.0.19

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 class NavicatPremium < Cask
-  version '11.0.18'
-  sha256 '561a5e20e7223f6a1c1431110dd6cc164b7a11c73432667a7a3dbc66b565cc91'
+  version '11.0.19'
+  sha256 '03d32415c29ab3f6c72bcb42f9bdb8167ac864ad15ca36b2639c9a2402c39dad'
 
   url 'http://download.navicat.com/download/navicat110_premium_en.dmg'
   homepage 'http://www.navicat.com/products/navicat-premium'
