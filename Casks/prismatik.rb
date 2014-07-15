7e0e3b2bfe3072d2ea21d55bfc228781de69c03a
Radek Simko <radek.simko@gmail.com>
2014-07-15 21:23:09 +0100

    Prismatik cask version reused to make updates easier

--- Casks/prismatik.rb
+++ Casks/prismatik.rb
@@ -2,7 +2,7 @@ class Prismatik < Cask
   version '5.11.1'
   sha256 '178ea51c143c2cb005199f5cfaf24695324b951ba98c55cec83c7882e6420903'
 
-  url 'https://github.com/woodenshark/Lightpack/releases/download/5.11.1/Prismatik.5.11.1.dmg'
+  url "https://github.com/woodenshark/Lightpack/releases/download/#{version}/Prismatik.#{version}.dmg"
   homepage 'http://lightpack.tv/'
 
   link 'Prismatik.app'
