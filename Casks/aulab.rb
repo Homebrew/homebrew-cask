4e1896ca16462c65a39f7824e9301047441ab87a
Johannes Hertenstein <johannes@nimius.net>
2014-07-15 11:43:28 +0200

    New Cask: AULab

---
+++ Casks/aulab.rb
@@ -0,0 +1,8 @@
+class Aulab < Cask
+  url 'http://images.apple.com/itunes/mastered-for-itunes/docs/au_lab.zip'
+  nested_container 'AU Lab Image.dmg'
+  homepage 'http://www.apple.com/itunes/mastered-for-itunes/'
+  version 'latest'
+  sha256 :no_check
+  link 'AU Lab.app'
+end
