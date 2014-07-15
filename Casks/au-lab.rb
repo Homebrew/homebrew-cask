2aa1c957776557f9d848e1221297a20d02e2b9dd
Johannes Hertenstein <johannes@nimius.net>
2014-07-15 13:07:24 +0200

    AuLab: changing order (As suggested in #5357)

--- Casks/au-lab.rb
+++ Casks/au-lab.rb
@@ -1,8 +1,10 @@
 class AuLab < Cask
-  url 'http://images.apple.com/itunes/mastered-for-itunes/docs/au_lab.zip'
-  nested_container 'AU Lab Image.dmg'
-  homepage 'http://www.apple.com/itunes/mastered-for-itunes/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://images.apple.com/itunes/mastered-for-itunes/docs/au_lab.zip'
+  homepage 'http://www.apple.com/itunes/mastered-for-itunes/'
+  
+  nested_container 'AU Lab Image.dmg'
   link 'AU Lab.app'
 end
