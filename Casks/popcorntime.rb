c1caa9120fd0ee0757873183bc3e329ff696b271
Andrei Constantinescu <andrei.c@icloud.com>
2014-07-17 00:43:08 +0300

    Add Popcorn-Time.app v. 3.2

---
+++ Casks/popcorntime.rb
@@ -0,0 +1,9 @@
+class Popcorntime < Cask
+  version '3.2'
+  sha256 '8108137e560391debd681b9578f4d878159164c82745cbb322e2edbd741df1ea'
+
+  url 'http://www.time4popcorn.eu/Popcorn-Time-Mac.zip'
+  homepage 'http://time4popcorn.eu'
+
+  link 'Popcorn-Time.app'
+end
