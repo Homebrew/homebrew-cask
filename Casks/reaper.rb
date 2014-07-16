edab36b54dd7187782b540ebfd013f40e5eb1305
Toni Förster <toni.foerster@gmail.com>
2014-07-16 09:04:27 +0200

    REAPER Update to v.4.71

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,15 +1,15 @@
 class Reaper < Cask
   homepage 'http://www.reaper.fm/'
-  version '4.7'
+  version '4.71'
 
   if Hardware::CPU.is_64_bit?
-    url 'http://www.reaper.fm/files/4.x/reaper47_x86_64.dmg'
-    sha256 '32d133d1bba84e812a8f50b18d58ace4f726eaa1b649c151286ddabbfde6952c'
+    url 'http://www.reaper.fm/files/4.x/reaper471_x86_64.dmg'
+    sha256 '3045fd59189bd0fe398b50f511d014a5e385c8cfb88f8a188a8437050f783d71'
     link 'REAPER64.app'
     link 'ReaMote64.app'
   else
-    url 'http://www.reaper.fm/files/4.x/reaper47_i386.dmg'
-    sha256 '98eb52be6e52257285cd4953a79a08591d92cc516890414cda2711418c833c08'
+    url 'http://www.reaper.fm/files/4.x/reaper471_i386.dmg'
+    sha256 '1a670d5afb5956e1486c1fdef008ccfb4faa7987cc4d5ea8171a2794ff86ffca'
     link 'REAPER.app'
     link 'ReaMote.app'
   end
