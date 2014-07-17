b8bc05f1aa1d2fff5a7ca53eab5a9c6fbbc326e1
Jonathan Edenström <me@jedenstrom.se>
2014-07-18 00:45:18 +0200

    Update droplr to version 4.1.2

--- Casks/droplr.rb
+++ Casks/droplr.rb
@@ -1,8 +1,8 @@
 class Droplr < Cask
-  version '4.1.1'
+  version '4.1.2'
   sha256 '60e45c4ea0e4658f78a0a78a36fd3f6e1c5d37efca1984f19f46e12ef6024f15'
 
-  url 'http://files.droplr.com.s3.amazonaws.com/apps/mac/Droplr+34.zip'
+  url 'http://files.droplr.com.s3.amazonaws.com/apps/mac/Droplr+35.zip'
   appcast 'https://droplr.com/appcast/appcast.xml'
   homepage 'https://www.droplr.com/'
 
