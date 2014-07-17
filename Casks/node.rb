33da3ee9b69c5ebc9f1656c292445df2f16f74e6
Jeffrey Cafferata <jeffrey@jcid.nl>
2014-07-17 10:59:57 +0200

    Upgrade node to 0.10.29

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,10 +1,10 @@
 class Node < Cask
-  version '0.10.28'
-  sha256 '7785c80dcdaa948df58db62fb92deaac46bedfaa8bf7677b95614d151ea80c3d'
+  version '0.10.29'
+  sha256 'fda80c7b09d55079b6d8082310a491695e7d76fce22595c229a20152936bfab0'
 
-  url 'http://nodejs.org/dist/v0.10.28/node-v0.10.28.pkg'
+  url "http://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   homepage 'http://nodejs.org'
 
-  install  'node-v0.10.28.pkg'
+  install  "node-v#{version}.pkg"
   uninstall :pkgutil => 'org.nodejs'
 end
