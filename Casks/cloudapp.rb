427ef070cbc55e7f9676b4f4fec27bf5a0fb6ead
amanaplan <amanaplan@icloud.com>
2014-07-17 17:59:44 -0400

    Add Cloud.app v2.0.2

---
+++ Casks/cloudapp.rb
@@ -0,0 +1,9 @@
+class Cloudapp < Cask
+  version '2.0.2'
+  sha256 'e9d638c0ee25b5269c1c9bd48f031ebfddd5997ec6543c1d4e202627cf5e28d4'
+
+  url 'http://downloads.getcloudapp.com/mac/CloudApp-2.0.2.zip'
+  homepage 'http://www.getcloudapp.com/download'
+
+  link 'Cloud.app'
+end
