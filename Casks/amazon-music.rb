6d912ec05f57f19c23871ee34e36b31787d543a3
blogabe <abe@homer.local>
2014-07-16 20:49:32 -0700

    amazon music update

--- Casks/amazon-music.rb
+++ Casks/amazon-music.rb
@@ -1,8 +1,8 @@
 class AmazonMusic < Cask
   version '3.0'
-  sha256 'e058212d7064bd4c557a22225293d0a3a4156fd42d5172700e502b5e685c8cb2'
+  sha256 '77a402077ae3f43aafa5433f7b26017643080a73fa0ad49953dedbeaca09574a'
 
-  url 'https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/20140604/025610578b/AmazonMusicInstaller.dmg'
+  url 'https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/20140702/064904ec21/AmazonMusicInstaller.dmg'
   homepage 'https://www.amazon.com/gp/feature.html/ref=dm_mo_cpw_fb_lm?docId=1001067901'
 
   caskroom_only true
