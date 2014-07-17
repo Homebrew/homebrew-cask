e000961a51acf0843f4098f616bc6afb841d0f2d
Vincent Klaiber <vincent@schimpanz.com>
2014-07-17 10:18:36 +0200

    Update Hush.app to use latest with :no_check.

--- Casks/hush.rb
+++ Casks/hush.rb
@@ -1,6 +1,6 @@
 class Hush < Cask
-  version '1.0'
-  sha256 'da31f0abd7c531e87c9f171493d318535cb24f1335e7e2b1c0fa68eeed685b2e'
+  version 'latest'
+  sha256 :no_check
 
   url 'https://coffitivity.com/hush/files/Hush.dmg.zip'
   homepage 'http://coffitivity.com/hush/'
