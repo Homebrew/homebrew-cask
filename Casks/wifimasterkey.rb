0359df519c6ed13833117cd6d2491c9b3e650be6
alswl <alswlx@gmail.com>
2014-07-19 17:29:53 +0800

    add wifimasterkey

---
+++ Casks/wifimasterkey.rb
@@ -0,0 +1,9 @@
+class Wifimasterkey < Cask
+  version '1.0.2'
+  sha256 '7b0145247e9b269fbd5ac7bcd955e313f772595cfa10ba5be5ce2473321e335b'
+
+  url 'http://www.lianwifi.com/download/mac/WiFiMasterKey_Mac.dmg'
+  homepage 'http://www.lianwifi.com/'
+
+  link 'WiFiMasterKey.app'
+end
