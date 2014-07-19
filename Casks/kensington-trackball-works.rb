227af5b78f955422ea3994a37ef170da90b0f325
fainder <fainder.info@gmail.com>
2014-07-20 00:54:36 +0900

    Delete kensington-trackball-works.rb

--- Casks/kensington-trackball-works.rb
+++
@@ -1,7 +0,0 @@
-class KensingtonTrackballWorks < Cask
-  url 'http://accoblobstorageus.blob.core.windows.net/software/38c5e777-b2ef-4434-8091-6290cb41fc16.dmg'
-  homepage 'http://www.kensington.com/'
-  version '1.1.2'
-  sha256 '02e3fed2fe01c234206d3fcf2c85f71f10aa2a3bc3d3dd6f4694ac41b725e3df'
-  install 'Kensington TrackballWorks.pkg'
-end
