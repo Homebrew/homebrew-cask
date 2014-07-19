2ff51f362287f3bf5bc31e72c7014f4de2ee1ed8
Jean Mertz <jean@mertz.fm>
2014-07-19 14:27:01 +0200

    Fix eye-fi download URL

--- Casks/eye-fi.rb
+++ Casks/eye-fi.rb
@@ -2,7 +2,7 @@ class EyeFi < Cask
   version '3.4.35'
   sha256 '7de117d0ee443683ad5ca7b3ab4ef5fc9e96c1a3d226cf87358c46b1270fca4e'
 
-  url 'https://d2mje64xs0b9gu.cloudfront.net/3.4.35/Eye-Fi.dmg'
+  url 'http://download.eyefi.com/x2/3.4.35/Eye-Fi.dmg'
   homepage 'http://support.eye.fi/downloads'
 
   install 'Eye-Fi.mpkg'
