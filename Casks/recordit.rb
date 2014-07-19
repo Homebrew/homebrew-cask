b2ccb32751776b4d5d57a878f8000b1a4fbace57
fainder <fainder.info@gmail.com>
2014-07-20 00:24:00 +0900

    Update Recordit v1.3.1

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -2,7 +2,8 @@ class Recordit < Cask
   version 'latest'
   sha256 :no_check
 
-  url 'http://recordit.co/latest'
+  url 'https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790/app_versions/36?format=zip'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790'
   homepage 'http://recordit.co/'
 
   link 'RecordIt.app'
