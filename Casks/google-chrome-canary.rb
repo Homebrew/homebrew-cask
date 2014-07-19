d65dfd81baabdbc7a82575c771df0571413dbb04
Narendran Sankaran <narendrans@users.noreply.github.com>
2014-07-19 00:48:52 -0400

    Update google-chrome-canary.rb

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -3,7 +3,7 @@ class GoogleChromeCanary < Cask
   sha256 :no_check
 
   url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
-  homepage 'https://www.google.com/chrome/'
+  homepage 'http://www.google.com/intl/en/chrome/browser/canary.html'
 
   link 'Google Chrome Canary.app'
 end
