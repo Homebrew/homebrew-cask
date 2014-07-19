0191b3eba5a990930ab3d378b64773e2211c0c55
Havanna <registerdedicated@yeah.net>
2014-07-19 21:25:08 +0800

    Upgrade Blu-Ray-Player to latest version

--- Casks/blu-ray-player.rb
+++ Casks/blu-ray-player.rb
@@ -1,6 +1,6 @@
 class BluRayPlayer < Cask
-  version '2.10.4'
-  sha256 '76e9731115b3305ba21f5c451f90d2123fd357defca8206a496a8f9b63480cc1'
+  version 'latest'
+  sha256 :no_check
 
   url 'http://www.macblurayplayer.com/user/download/Mac_Bluray_Player.dmg'
   homepage 'http://www.macblurayplayer.com/'
