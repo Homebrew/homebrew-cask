f14d16f5353c61fadc6bcd8d578b11bfab0e29b3
alswl <alswlx@gmail.com>
2014-07-20 16:41:42 +0800

    Fix wifimasterkey version to latest

--- Casks/wifimasterkey.rb
+++ Casks/wifimasterkey.rb
@@ -1,6 +1,6 @@
 class Wifimasterkey < Cask
-  version '1.0.2'
-  sha256 '7b0145247e9b269fbd5ac7bcd955e313f772595cfa10ba5be5ce2473321e335b'
+  version 'latest'
+  sha256 :no_check
 
   url 'http://www.lianwifi.com/download/mac/WiFiMasterKey_Mac.dmg'
   homepage 'http://www.lianwifi.com/'
