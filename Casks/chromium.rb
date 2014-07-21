fa0472f52c3b452542494cf5bddd8fa88afe120f
Trevor Parker <trevor@trevorparker.com>
2014-07-20 21:15:42 -0400

    Upgrade Chromium to 36.0.1985.125

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,8 +1,8 @@
 class Chromium < Cask
-  version '35.0.1916.153'
-  sha256 '9445b16e9814575d1194d60197aad2c20e89110fd2aaf1e8120a41637768ff92'
+  version '36.0.1985.125'
+  sha256 '2a645ee42f9a9d2bb22c03fe90b6f9082ebb6a8351697b7112b060ed6b766339'
 
-  url 'https://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_35.0.1916.153.dmg'
+  url "https://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_#{version}.dmg"
   appcast 'http://osxportableapps.sourceforge.net/chromium/chrcast.xml'
   homepage 'http://www.freesmug.org/chromium'
 
