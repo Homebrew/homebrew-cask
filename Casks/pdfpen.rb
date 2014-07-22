d09904dfbadd0a3c13f36d569aeb51ec0ee19b5b
mskblackbelt <mskblackbelt@me.com>
2014-07-22 11:25:13 -0400

    Updated PDFpen to version 'latest'

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -1,8 +1,8 @@
 class Pdfpen < Cask
-  version '6.3'
-  sha256 '71c91a2d8b0727c7fb5d1613622c24db798e965ebceebc3abc26a7ce1f21562b'
+  version 'latest'
+  sha256 :no_check
 
-  url 'http://cdn.smilesoftware.com/PDFpen_6.3.zip'
+  url 'http://dl.smilesoftware.com/com.smileonmymac.PDFpen/PDFpen.zip'
   homepage 'http://smilesoftware.com/PDFpen/index.html'
 
   link 'PDFpen.app'
