044e2b7116c9d196648416731cf5791d9b5f2406
Marius Butuc <marius.butuc@gmail.com>
2014-07-22 14:09:46 -0400

    Update Vagrant Manager to v1.5.5
      from version 1.5.2

--- Casks/vagrant-manager.rb
+++ Casks/vagrant-manager.rb
@@ -1,8 +1,8 @@
 class VagrantManager < Cask
-  version '1.5.2'
-  sha256 '8b694afb8774e83088d317e18e679199ddb17bace499d519ba81d6ef4c7d7be3'
+  version '1.5.5'
+  sha256 '778815d9b5a41e30363f8ca652fc5964beb4fbca0691feaad2595e1382870762'
 
-  url 'https://github.com/lanayotech/vagrant-manager/releases/download/1.5.2/vagrant-manager-1.5.2.dmg'
+  url 'https://github.com/lanayotech/vagrant-manager/releases/download/1.5.5/vagrant-manager-1.5.5.dmg'
   appcast 'http://api.lanayo.com/appcast/vagrant_manager.xml'
   homepage 'http://vagrantmanager.com/'
 
