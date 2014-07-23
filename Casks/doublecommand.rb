14c6f017d84a4a77382de7febdf40d79fcb9b322
Galymzhan Kozhayev <kozhayev@outlook.com>
2014-07-23 22:01:32 +0600

    Add DoubleCommand v1.7

---
+++ Casks/doublecommand.rb
@@ -0,0 +1,10 @@
+class Doublecommand < Cask
+  version '1.7'
+  sha256 '312aaf1a60517c694b24131bf502945dc23a22c917971c3e7a3adca163560503'
+
+  url 'http://doublecommand.sourceforge.net/files/DoubleCommand-1.7.dmg'
+  homepage 'http://doublecommand.sourceforge.net'
+
+  install 'DoubleCommand-1.7.pkg'
+  uninstall :script => '/Library/StartupItems/DoubleCommand/uninstall.command'
+end
