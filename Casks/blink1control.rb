ae739bb71050500189e29470ccecb80ce1f2624d
Eddie Zaneski <eddiezane@gmail.com>
2014-07-25 16:51:16 -0400

    Update blink1control to v1.91

--- Casks/blink1control.rb
+++ Casks/blink1control.rb
@@ -1,9 +1,9 @@
 class Blink1control < Cask
-  version '1.82'
-  sha256 'ba44651eaa7cec8d732b16994f34574f1b1656ae22f22e7fd515b6b713c27ccd'
+  version '1.91'
+  sha256 'fc3624a041f561f086cdce95bd70470b072c9b586e9ee1247d88b78e6abce094'
 
   homepage 'http://blink1.thingm.com/'
-  url 'https://github.com/todbot/blink1/releases/download/v1.82/Blink1Control-mac.zip'
+  url 'https://github.com/todbot/blink1/releases/download/v1.91/Blink1Control-mac.zip'
 
   link 'Blink1Control.app'
 end
