352f6f79f90b883149cfcd30596db2558a645561
jottr <j@ovvvo.co>
2014-07-25 22:48:29 +0200

    Updated anki to 2.2.8

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,7 +1,7 @@
 class Anki < Cask
-  url 'http://ankisrs.net/download/mirror/anki-2.0.26.dmg'
+  url 'http://ankisrs.net/download/mirror/anki-2.0.28.dmg'
   homepage 'http://ankisrs.net/'
-  version '2.0.26'
-  sha256 '2848127ef8ac30b922cd60f5e6b92632789d0c822723e96aa68aea246b9e4883'
+  version '2.0.28'
+  sha256 '8071e6f1cdf661df6c8b9997e0618e8dc722ff8aeaf8b99390567204791f23cd'
   link 'Anki.app'
 end
