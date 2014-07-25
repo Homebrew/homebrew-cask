f8fce0c72dfd4e6931316454decf2050764bd637
Lucas Berezy <lberezy@users.noreply.github.com>
2014-07-25 15:31:37 +1000

    TotalTerminal 1.5
    This brings support for OS X 10.10 Yosemite.

--- Casks/totalterminal.rb
+++ Casks/totalterminal.rb
@@ -1,5 +1,5 @@
 class Totalterminal < Cask
-  version '1.4.11'
+  version '1.5'
   sha256 'c0689bc4912042441c5e056cae9a04a681d4dbe34c1fc157c890cc3febcd41f2'
 
   url 'http://downloads.binaryage.com/TotalTerminal-1.4.11.dmg'
