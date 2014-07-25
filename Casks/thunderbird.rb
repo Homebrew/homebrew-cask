7849e2bad3bdef280027bd85d49ae62eac3cfffb
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-07-25 06:30:32 +0300

    update thunderbird to 31.0

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,8 +1,8 @@
 class Thunderbird < Cask
-  version '24.6.0'
-  sha256 'd40769e1b6f1943d712527f983e4c0be66f261008a3409515652ae4bba6da494'
+  version '31.0'
+  sha256 '8383892d3c228c6c9ced1ca44ff1de2507d12c3dcbf1a1e863fb1dfa4b5a7ea0'
 
-  url 'https://download.mozilla.org/?product=thunderbird-24.6.0&os=osx&lang=en-US'
+  url 'https://download.mozilla.org/?product=thunderbird-31.0&os=osx&lang=en-US'
   homepage 'http://www.mozilla.org/en-US/thunderbird/'
 
   link 'Thunderbird.app'
