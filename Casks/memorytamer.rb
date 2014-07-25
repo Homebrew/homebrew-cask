8dd3c553fc7755545f80ca77fb1a04c253cd0d17
Eric Henderson <henderea@gmail.com>
2014-07-25 12:07:42 -0400

    Add MemoryTamer.app v0.7.5
    Add my own app MemoryTamer (has 7-day free trial, $1.99 for a license via Paddle)

---
+++ Casks/memorytamer.rb
@@ -0,0 +1,9 @@
+class Memorytamer < Cask
+  version '0.7.5'
+  sha256 '58f0357a2b018bd27a3c25865a5dcaa1b3e7fb844325a85a9277592c2fb790fe'
+
+  url 'https://myepg-ds.s3.amazonaws.com/MemoryTamer-0.7.5'
+  homepage 'https://github.com/henderea/MemoryTamer'
+
+  link 'MemoryTamer.app'
+end
