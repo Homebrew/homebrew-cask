3a66e1d1915824f7ceb9e71cc709f9a0a31e8f47
Eric Henderson <henderea@gmail.com>
2014-07-26 14:34:22 -0400

    Upgrade MemoryTamer.app to v0.7.7
    Upgrade my MemoryTamer app to v0.7.7 and fix a mistake in the download link

--- Casks/memorytamer.rb
+++ Casks/memorytamer.rb
@@ -1,8 +1,8 @@
 class Memorytamer < Cask
-  version '0.7.5'
-  sha256 '58f0357a2b018bd27a3c25865a5dcaa1b3e7fb844325a85a9277592c2fb790fe'
+  version '0.7.7'
+  sha256 'b45acb3d58a74ec2f1002e6288c4a6757b6155d22ae1b27be154273d92185e0b'
 
-  url 'https://myepg-ds.s3.amazonaws.com/MemoryTamer-0.7.5'
+  url 'https://memorytamer.s3.amazonaws.com/MemoryTamer-0.7.7.zip'
   homepage 'https://github.com/henderea/MemoryTamer'
 
   link 'MemoryTamer.app'
