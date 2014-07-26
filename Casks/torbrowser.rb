07559fcf2160f6a3090f8fbac1127ecfbf3be602
sks <solntsev@gmail.com>
2014-07-26 12:55:20 -0500

    Update torbrowser.rb

--- Casks/torbrowser.rb
+++ Casks/torbrowser.rb
@@ -1,8 +1,8 @@
 class Torbrowser < Cask
-  version '3.6.2'
-  sha256 '30778b5d164514c971cc4019ac8724f1aa4a4adc1d9cfcb07a50ce76179259c9'
+  version '3.6.3'
+  sha256 'c0a4c391a83d55cfd57f7448c1ecad843187a9ee474387d3147ba2b0694771f3'
 
-  url 'https://www.torproject.org/dist/torbrowser/3.6.2/TorBrowser-3.6.2-osx32_en-US.dmg'
+  url "https://www.torproject.org/dist/torbrowser/#{version}/TorBrowser-#{version}-osx32_en-US.dmg"
   homepage 'https://www.torproject.org/projects/torbrowser.html'
 
   link 'TorBrowser.app'
