f3ff9f8fb0f407a0bf0492b68ee851c286c86beb
Fernando Paredes <nano@fdp.io>
2014-07-26 09:31:23 -0700

    Totalfinder: Make cask easier to update

--- Casks/totalfinder.rb
+++ Casks/totalfinder.rb
@@ -2,7 +2,7 @@ class Totalfinder < Cask
   version '1.6.2'
   sha256 '7f60614a81f3efcbf34fd6213f9f0a912e5f8472c3039ce1bbde0e58236dc659'
 
-  url 'http://downloads.binaryage.com/TotalFinder-1.6.2.dmg'
+  url "http://downloads.binaryage.com/TotalFinder-#{version}.dmg"
   homepage 'http://totalfinder.binaryage.com'
 
   install 'TotalFinder.pkg'
