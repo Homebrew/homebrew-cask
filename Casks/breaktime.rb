6639a336fc40255ff0c687087369ccc54c64d54a
Andrew Shaw Care <andrewshawcare@gmail.com>
2014-07-26 17:37:10 -0400

    Added Breaktime app

---
+++ Casks/breaktime.rb
@@ -0,0 +1,9 @@
+class Breaktime < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://s3.amazonaws.com/breaktimeapp.com/BreakTime.zip'
+  homepage 'http://breaktimeapp.com/'
+
+  link 'BreakTime.app'
+end
