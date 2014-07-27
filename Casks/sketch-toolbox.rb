96a72a34896d8c227382605f5ef1a005fb2859ce
Fernando Paredes <nano@fdp.io>
2014-07-27 13:58:32 -0700

    Sketch Toolbox:
    A super simple plugin manager for sketch

---
+++ Casks/sketch-toolbox.rb
@@ -0,0 +1,9 @@
+class SketchToolbox < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://sketchtoolbox.com/Sketch%20Toolbox.zip'
+  homepage 'http://sketchtoolbox.com'
+
+  link 'Sketch Toolbox.app'
+end
