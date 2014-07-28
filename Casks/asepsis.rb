e7ce5b2486da712ecf8607700302440ccd7fbd17
C. Blake Barber <clakeb@gmail.com>
2014-07-27 19:51:53 -0500

    Update asepsis.rb

--- Casks/asepsis.rb
+++ Casks/asepsis.rb
@@ -1,8 +1,8 @@
 class Asepsis < Cask
-  version '1.4.1'
-  sha256 '1e757a51bfeb1cf57e179761d7f931a1bf3e0216dd9d66fb56158273c7acdf9e'
+  version '1.5'
+  sha256 'b7bf18dfe58612de89f744e11e25f6eb6bb708a48941fc975d55a614716e1436'
 
-  url 'http://downloads.binaryage.com/Asepsis-1.4.1.dmg'
+  url 'http://downloads.binaryage.com/Asepsis-1.5.dmg'
   homepage 'http://asepsis.binaryage.com/'
 
   install 'Asepsis.pkg'
