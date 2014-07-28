188be7f3f5af2cedc2d38134704df43c0a6f1d0e
joker1007 <kakyoin.hierophant@gmail.com>
2014-07-28 21:14:28 +0900

    Add iSwiff cask
    iSwiff is SWF Player.

---
+++ Casks/iswiff.rb
@@ -0,0 +1,9 @@
+class Iswiff < Cask
+  version '1.6'
+  sha256 '6af83a2eb78136d70439fdee4598cb8faf9f77af35c323ff7fd57a86e5550d1d'
+
+  url 'http://echoone.com/iswiff/iSwiff.dmg'
+  homepage 'http://echoone.com/iswiff/'
+
+  link 'iSwiff.app'
+end
