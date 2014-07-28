ade56e1b3acf58e290957326d4935908a6dc08fe
Bryan Black <reelsense@users.noreply.github.com>
2014-07-28 16:30:07 -0700

    Update xACT 2.29 to 2.30
    Update xACT 2.29 to 2.30

--- Casks/xact.rb
+++ Casks/xact.rb
@@ -1,6 +1,6 @@
 class Xact < Cask
-  version '2.29'
-  sha256 'bc8805f94b9f2320da64ca23b53e1b029fd65920ded2bc56c0291f0a73866674'
+  version '2.30'
+  sha256 '49f7770ad682bdb36f63bebaf98875c22c1ffc7bb9a14b616213039506a9cf74'
 
   url "http://xact.scottcbrown.org/xACT#{version}.zip"
   appcast 'http://xactupdate.scottcbrown.org/xACT.xml'
