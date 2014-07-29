11ed9e81d430a2d79dcf3a9cb5e68c5d8b2f7409
Chulki Lee <chulki.lee@gmail.com>
2014-07-29 14:45:45 -0700

    audio-mate 2.0.4

---
+++ Casks/audio-mate.rb
@@ -0,0 +1,9 @@
+class AudioMate < Cask
+  version '2.0.4'
+  sha256 'dd66b67ff57c90d7f2c62c46f6a4049f5cecfbf1d5eb4fd502c447c9f0bef28b'
+
+  url 'https://s3.amazonaws.com/apps-leftbee/products/downloadables/000/000/005/original/AudioMate-v2.0.4.dmg'
+  homepage 'http://audiomateapp.com/'
+
+  link 'AudioMate.app'
+end
