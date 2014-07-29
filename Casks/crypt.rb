7f3e5560b87d0947a343c278402f85c8ee99ccbd
Pedro Borges <oi@pedroborg.es>
2014-07-29 09:16:30 -0300

    Add Crypt3.app v3

---
+++ Casks/crypt.rb
@@ -0,0 +1,7 @@
+class Crypt < Cask
+  url 'http://voluntary.net.s3.amazonaws.com/Crypt3_20100429.zip'
+  homepage 'http://voluntary.net/crypt'
+  version 'latest'
+  sha256 :no_check 
+  link 'Crypt3.app'
+end
