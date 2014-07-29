1462d842590943193d139f111ceab83dbe0385a2
Lucas Horton <lucas@lucashorton.com>
2014-07-28 22:29:38 -0500

    Add TextSoap.app

---
+++ Casks/textsoap.rb
@@ -0,0 +1,9 @@
+class Textsoap < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://unmarked.s3.amazonaws.com/textsoap7.zip'
+  homepage 'http://www.unmarked.com/textsoap/'
+
+  link 'textsoap7.app', :target => 'TextSoap.app'
+end
