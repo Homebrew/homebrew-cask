f6418d25a9444e7430605e5223327fb371b5b46a
Matthias Neugebauer <mtneug@mailbox.org>
2014-07-29 14:30:44 +0200

    Update FileZilla.app to v3.9.0.1

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,8 +1,8 @@
 class Filezilla < Cask
-  version '3.8.1'
-  sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
+  version '3.9.0.1'
+  sha256 'e9de997630265ad17bd1006624f76a20276da45578a9ffa839d4326eed84b17a'
 
-  url 'https://downloads.sourceforge.net/project/filezilla/FileZilla_Client/3.8.1/FileZilla_3.8.1_macosx-x86.app.tar.bz2'
+  url "https://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   homepage 'https://filezilla-project.org/'
 
   link 'FileZilla.app'
