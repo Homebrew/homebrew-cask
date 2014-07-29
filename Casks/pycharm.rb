aa914a9a78de326356c5c1b072a777549b9eafbf
吕杰 <764664@gmail.com>
2014-07-29 22:43:31 +0800

    Upgrade PyCharm.app to v3.4.1

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,8 +1,8 @@
 class Pycharm < Cask
-  version '3.4.0'
-  sha256 '5fc1d1c38057349e4009fcab9a78981a647cd187e55f671f5c2bd347c682249a'
+  version '3.4.1'
+  sha256 'f2af3859b0376927f591d5eac3b8d2c8196d712af811f27cc269965a03324a78'
 
-  url 'http://download.jetbrains.com/python/pycharm-professional-3.4.dmg'
+  url 'http://download.jetbrains.com/python/pycharm-professional-3.4.1.dmg'
   homepage 'http://www.jetbrains.com/pycharm/'
 
   link 'PyCharm.app'
