89e14661fbd4017f7cdf3fe1849da1d826e5d40f
Martin Madsen <martinbmadsen@gmail.com>
2014-07-29 13:57:03 +0200

    Add Sony Homestream v1.1 cask

---
+++ Casks/homestream.rb
@@ -0,0 +1,10 @@
+class Homestream < Cask
+  version '1.1'
+  sha256 '193dd9fb3488dfb754057b9c216e09fe2f0528b20a95bbe9b7d5f4b0556a4e2b'
+
+  url 'http://homestreamdownload.sony-europe.com/homestream-1.1-osx.tar.gz'
+  homepage 'http://www.sony.co.uk/hub/1237485339460'
+
+  link 'Homestream.app'
+  link 'Homestream-Console.app'
+end
