8ea970f69b4fd9339227e0ede2256826e4c25860
Niko Dziemba <niko@dziemba.com>
2014-07-29 22:53:57 +0200

    Add Beatport Pro v2.5.0

---
+++ Casks/beatport-pro.rb
@@ -0,0 +1,9 @@
+class BeatportPro < Cask
+  version '2.0.5_124'
+  sha256 '8c1c7dd3bc180eef7f5d12f258241522a659aab1f6dcdc8f077cf151952d1ff2'
+
+  url 'http://pro.beatport.com/mac/2.0.5_124/beatportpro_2.0.5_124.dmg'
+  homepage 'http://pro.beatport.com/'
+
+  link 'Beatport Pro.app'
+end
