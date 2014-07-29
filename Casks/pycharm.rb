39a1758399a19d9d251f31fecb5cbc059b99f3c9
Fernando Paredes <nano@fdp.io>
2014-07-29 08:19:28 -0700

    Pycharm: Make easier to update

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -2,7 +2,7 @@ class Pycharm < Cask
   version '3.4.1'
   sha256 'f2af3859b0376927f591d5eac3b8d2c8196d712af811f27cc269965a03324a78'
 
-  url 'http://download.jetbrains.com/python/pycharm-professional-3.4.1.dmg'
+  url "http://download.jetbrains.com/python/pycharm-professional-#{version}.dmg"
   homepage 'http://www.jetbrains.com/pycharm/'
 
   link 'PyCharm.app'
