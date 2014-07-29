b6f6e9bf7ceb2f3260b44258f605a6ed08181042
joker1007 <kakyoin.hierophant@gmail.com>
2014-07-29 23:16:32 +0900

    sha256 is :no_check, version is 'latest'

--- Casks/iswiff.rb
+++ Casks/iswiff.rb
@@ -1,6 +1,6 @@
 class Iswiff < Cask
-  version '1.6'
-  sha256 '6af83a2eb78136d70439fdee4598cb8faf9f77af35c323ff7fd57a86e5550d1d'
+  version 'latest'
+  sha256 :no_check
 
   url 'http://echoone.com/iswiff/iSwiff.dmg'
   homepage 'http://echoone.com/iswiff/'
