66bb20f4b26954a8dda722fc862fcdc6bff49cd2
Chip Warden <lgw4@introspectiverobot.com>
2014-07-30 09:40:38 -0500

    Corrected url to download latest version of VirtualHostX.

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -2,7 +2,7 @@ class Virtualhostx < Cask
   version 'latest'
   sha256 :no_check
 
-  url 'https://clickontyler.com/virtualhostx/download/'
+  url 'https://clickontyler.com/virtualhostx/download/v5/'
   appcast 'http://shine.clickontyler.com/appcast.php?id=23'
   homepage 'http://clickontyler.com/virtualhostx/'
 
