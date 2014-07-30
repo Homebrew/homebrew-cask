045162d32777b65a7f3174c19ca235ceed8ae011
Kevin Krauss <earlkrauss@gmail.com>
2014-07-30 10:12:54 -0700

    updated googleappenginelauncher to 1.9.7

--- Casks/googleappenginelauncher.rb
+++ Casks/googleappenginelauncher.rb
@@ -1,7 +1,6 @@
 class Googleappenginelauncher < Cask
-  version '1.9.6'
-  sha256 '82b97a2d02be6834a87783a76d88d1c9fc436e1ee0832476df30d4f5ea938d7a'
-
+  version '1.9.7'
+  sha256 '583da1171f3769fb697588ed9c6153eb7e8af007af801026b4226b447e923af7'
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
   homepage 'https://developers.google.com/appengine/'
 
