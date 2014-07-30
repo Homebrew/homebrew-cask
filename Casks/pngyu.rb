f7c823b256f2b8e0460c956c74ea98d8cafb5289
Ryan Senkbeil <ryan.senkbeil@gsdesign.com>
2014-07-30 13:11:02 -0500

    Add formula for pngyu.

---
+++ Casks/pngyu.rb
@@ -0,0 +1,9 @@
+class Pngyu < Cask
+  version '1.0.1'
+  sha256 'f853a3566236200391a40d12df9519b39f4d30053ab6ed6f60670b7eacca6217'
+
+  url 'http://nukesaq88.github.io/Pngyu/download/Pngyu_mac_101.zip'
+  homepage 'http://nukesaq88.github.io/Pngyu/'
+
+  link 'Pngyu.app'
+end
