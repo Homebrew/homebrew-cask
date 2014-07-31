9190f2de1525437d74f27f61ec290ed7d66a0fc6
Roland Walker <walker@pobox.com>
2014-07-31 14:58:28 -0400

    make platypus use a versioned URL and add appcast

--- Casks/platypus.rb
+++ Casks/platypus.rb
@@ -2,7 +2,8 @@ class Platypus < Cask
   version '4.8'
   sha256 '754d9153b92e32faf7e56a4e181de59c6f0b72b3208663042a22e1767d87cb05'
 
-  url 'http://sveinbjorn.org/files/software/platypus.zip'
+  url "http://sveinbjorn.org/files/software/platypus/platypus#{version}.zip"
+  appcast 'http://www.sveinbjorn.org/files/appcasts/PlatypusAppcast.xml'
   homepage 'http://sveinbjorn.org/platypus'
 
   link "Platypus-#{version}/Platypus.app"
