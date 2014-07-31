12359df4a69492b1151ad9ee340b4361a3f8de0b
Ishiguro Yusuke <ishiguro_yusuke@cyberagent.co.jp>
2014-07-31 12:55:51 +0900

    Upgrade Unity3d to 4.5.2f1

--- Casks/unity3d.rb
+++ Casks/unity3d.rb
@@ -1,8 +1,8 @@
 class Unity3d < Cask
-  version '4.5.1f3'
-  sha256 '1b648596ca4283d8ec244ac1c45944e03ad301aed53bccb63dd57571de976570'
+  version '4.5.2f1'
+  sha256 'a6330f8b42865636a8c2055b72fbdff5775b7019254ca41437eca0be1b9baedf'
 
-  url 'http://netstorage.unity3d.com/unity/unity-4.5.1.dmg'
+  url 'http://netstorage.unity3d.com/unity/unity-4.5.2.dmg'
   homepage 'http://unity3d.com/unity/'
 
   install 'Unity.pkg'
