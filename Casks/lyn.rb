6b3f955692ebd6d4aff76ac1945561521a8226fc
Ngoc Pham <ngocphamm@users.noreply.github.com>
2014-08-03 11:40:22 -0400

    Update Lyn app to latest version 1.5.2

--- Casks/lyn.rb
+++ Casks/lyn.rb
@@ -1,8 +1,8 @@
 class Lyn < Cask
-  version '1.3.7'
-  sha256 '5a1b937e4226dc0cb69f187296821410a150ac0994fea629e789f84baecb7c28'
+  version '1.5.2'
+  sha256 'a4200b35eb4e51c3932a9e7cd40e1be3291a26921da350d396d337fa667798f6'
 
-  url 'http://www.lynapp.com/downloads/Lyn-1.3.7.dmg'
+  url 'http://www.lynapp.com/downloads/Lyn-1.5.2.dmg'
   appcast 'http://www.lynapp.com/lyn/update.xml'
   homepage 'http://www.lynapp.com/'
 
