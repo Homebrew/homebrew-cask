c2ad7a11aea163f3f98f2d055e731b7eed078a0d
Mohammed Tangestani <tangestani@gmail.com>
2014-08-03 01:01:13 -0400

    Add Popcorn-Time v0.3.2

---
+++ Casks/popcorn-time.rb
@@ -0,0 +1,9 @@
+class PopcornTime < Cask
+  version '0.3.2'
+  sha256 'abeea0d1de945311d1552764a9842142c8d317ba7ea0b01c15d45f4bdd815206'
+
+  url 'https://cdn.popcorntime.io/build/Popcorn-Time-0.3.2-Mac.dmg'
+  homepage 'https://popcorntime.io'
+
+  link 'Popcorn-Time.app'
+end
