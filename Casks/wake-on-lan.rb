0d7f789c7f3a2d1bd9d130c79a98458c52ab6c9e
Johannes Hertenstein <johannes@nimius.net>
2014-08-04 17:30:39 +0200

    New cask: WakeOnLan

---
+++ Casks/wake-on-lan.rb
@@ -0,0 +1,9 @@
+class WakeOnLan < Cask
+  version '1.0'
+  sha256 '00d86efd23e9d5de5451e26d6957f3af1a1a3bc66d11c50b8563454113fd5ab1'
+
+  url 'http://www.readpixel.com/downloads/files/WakeOnLan1.0.zip'
+  homepage 'http://www.readpixel.com/wakeonlan/'
+
+  link 'WakeOnLan/WakeOnLan.app'
+end
