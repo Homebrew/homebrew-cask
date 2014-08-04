dda1702686692c7d43a7f2a051a2e48fa9c1dd18
Ted Wise <cwise@hdlabinc.com>
2014-08-04 11:03:38 -0400

    Update Adobe DNG Convert to 8.6

--- Casks/adobe-dng-converter.rb
+++ Casks/adobe-dng-converter.rb
@@ -1,7 +1,7 @@
 class AdobeDngConverter < Cask
-  url 'http://download.adobe.com/pub/adobe/dng/mac/DNGConverter_8_4.dmg'
+  url 'http://download.adobe.com/pub/adobe/dng/mac/DNGConverter_8_6.dmg'
   homepage 'http://www.adobe.com/support/downloads/product.jsp?product=106&platform=Macintosh'
-  version '8.4'
-  sha256 'a749af57177bd0c9e2bb16175906ddba70bb6f5fd0fb6445200a0ff58d3af2e4'
+  version '8.6'
+  sha256 '3bb43ca608b7e62727512c813b395ea46aad545f68f9323cc78c9c5f47145650'
   install 'Adobe DNG Converter.pkg'
 end
