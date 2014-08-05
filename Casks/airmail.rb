2bd545b65905725053a95bfb68b5c97766ca6169
Vítor Galvão <info@vitorgalvao.com>
2014-08-05 18:32:13 +0100

    removed airmail

--- Casks/airmail.rb
+++
@@ -1,9 +0,0 @@
-class Airmail < Cask
-  version 'latest'
-  sha256 :no_check
-
-  url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
-  homepage 'http://airmailapp.com/'
-
-  link 'AirMail Beta.app'
-end
