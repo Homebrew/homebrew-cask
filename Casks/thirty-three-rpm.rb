f9a081a9812be2fc82df082dcc060ce1d50a5c1c
Peter Lyons <pete@peterlyons.com>
2014-08-04 21:48:16 -0600

    Add 33 RPM.app v1.1.8

---
+++ Casks/thirty-three-rpm.rb
@@ -0,0 +1,9 @@
+class ThirtyThreeRpm < Cask
+  version '1.1.8'
+  sha256 '44937d70c41c20dd72ddfef6266c288beac66eeb01914cdc7901ae8111a4aa90'
+
+  url 'http://www.edenwaith.com/downloads/33rpm.dmg'
+  homepage 'http://www.edenwaith.com/products/33rpm/'
+
+  link '33 RPM.app'
+end
