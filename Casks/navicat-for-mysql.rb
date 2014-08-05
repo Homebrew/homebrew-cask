6b8d123327173a940ab87405db6eaad5f4efc544
Jon Chen <bsd@voltaire.sh>
2014-08-05 10:36:00 -0400

    update navicat-for-mysql to 11.0.20

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,6 +1,6 @@
 class NavicatForMysql < Cask
-  version '11.0.18'
-  sha256 'e70d31ce4469a694568ede570ae2f3ecc7d6be13b5e9b95651c068b8403c2ab1'
+  version '11.0.20'
+  sha256 '05f5084d2fdece158a2b8e48d3040a1d66984c6ae06b7f43371b4183ab1a5f6d'
 
   url 'http://download.navicat.com/download/navicat110_mysql_en.dmg'
   homepage 'http://www.navicat.com/products/navicat-for-mysql'
