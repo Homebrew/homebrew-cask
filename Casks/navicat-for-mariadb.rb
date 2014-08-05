f9674135be5c0e46d0b3ebc24cc5545c7e89213c
Jon Chen <bsd@voltaire.sh>
2014-08-05 13:45:20 -0400

    bump navicat-for-mariadb to 11.0.20

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,6 +1,6 @@
 class NavicatForMariadb < Cask
-  version '11.0.18'
-  sha256 'c0163c0a5027b16a38b6f95635728aee7b36aebb32f3b4603fd5af87627609c0'
+  version '11.0.20'
+  sha256 'a266aaf54c56ec16de1574bc6bfbc454c6c6c5dbd91902c7717bc9e679c78741'
 
   url 'http://download.navicat.com/download/navicat110_mariadb_en.dmg'
   homepage 'http://www.navicat.com/products/navicat-for-mariadb'
