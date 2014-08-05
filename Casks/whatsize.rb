edac3397ebf032d1e491acf21253b9e866aca210
Ted Wise <cwise@hdlabinc.com>
2014-08-05 15:12:20 -0400

    Add WhatSize app.

---
+++ Casks/whatsize.rb
@@ -0,0 +1,10 @@
+class Whatsize < Cask
+  version 'latest'
+  sha256 :no_check
+  	
+  url 'http://whatsizemac.com/software/whatsize/whatsize.dmg'
+  appcast 'http://www.id-design.com/software/whatsize/release/notes.xml'
+  homepage 'http://whatsizemac.com/'
+
+  link 'WhatSize.app'
+end
