8330ac4c9f7af41c1046679704b4926749e20916
Ted Wise <cwise@hdlabinc.com>
2014-08-05 15:10:49 -0400

    Bump version for LaunchControl app.

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,8 +1,8 @@
 class Launchcontrol < Cask
-  version '1.14.2'
-  sha256 '9351c57b3956e63f5d995dfe4b9f45fa78a39476dbc3a4b7aa8797b600c4d8ad'
+  version '1.15.3'
+  sha256 'fecdd0dd58e937d3d3e9894ba046f52f61f001826bb2ab1beacedc23e8da3fd7'
 
-  url 'http://www.soma-zone.com/download/files/LaunchControl_1.14.2.tar.bz2'
+  url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml'
   homepage 'http://www.soma-zone.com/LaunchControl/'
 
