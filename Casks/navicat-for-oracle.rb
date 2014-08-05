99d32a33cda549fd5fd55c8b13d388eff5cbe93a
Jon Chen <bsd@voltaire.sh>
2014-08-05 13:47:30 -0400

    bump navicat-for-oracle to 11.0.20

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,6 +1,6 @@
 class NavicatForOracle < Cask
-  version '11.0.18'
-  sha256 '800693a4188f43d5095267bec9f23fe7fc4df22d87caf399d3abcfec17c9ba56'
+  version '11.0.20'
+  sha256 '531e6f5836203dd1f155dfa9e5734d2b7f0a615da45e533004efa3778355a155'
 
   url 'http://download.navicat.com/download/navicat110_ora_en.dmg'
   homepage 'http://www.navicat.com/products/navicat-for-oracle'
