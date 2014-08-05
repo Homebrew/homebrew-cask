4e220b614b9997dc23a3536bfed584d6aa9170b9
rhiokim <rhio.kim@gmail.com>
2014-08-05 11:17:31 +0900

    Upgrade haroopad.app to v0.12.2

--- Casks/haroopad.rb
+++ Casks/haroopad.rb
@@ -1,8 +1,8 @@
 class Haroopad < Cask
-  version '0.12.0'
-  sha256 '05297bdd532f23534fc3dab475d4b3262c18604d8a219a5e9188c34a84ad1472'
+  version '0.12.2'
+  sha256 'f8fe84ec70090c7517b4ef9f8c2896a4dedb9774d0a87800fdd98f706a64929b'
 
-  url 'https://dl.dropbox.com/s/yvjb90ywib551ex/haroopad-v0.12.0.dmg'
+  url 'https://bitbucket.org/rhiokim/haroopad-download/downloads/haroopad-v0.12.2.dmg'
   homepage 'http://pad.haroopress.com/'
 
   link 'haroopad.app'
