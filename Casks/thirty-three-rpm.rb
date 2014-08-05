e94afd5e44ffe83ada9620cc7ec1d05ea9b3c491
Peter Lyons <pete@peterlyons.com>
2014-08-05 08:27:55 -0600

    fix 33 RPM Cask latest version

--- Casks/thirty-three-rpm.rb
+++ Casks/thirty-three-rpm.rb
@@ -1,6 +1,6 @@
 class ThirtyThreeRpm < Cask
-  version '1.1.8'
-  sha256 '44937d70c41c20dd72ddfef6266c288beac66eeb01914cdc7901ae8111a4aa90'
+  version 'latest'
+  sha256 :no_check
 
   url 'http://www.edenwaith.com/downloads/33rpm.dmg'
   homepage 'http://www.edenwaith.com/products/33rpm/'
