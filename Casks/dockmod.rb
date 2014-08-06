74019adf16cc31d0f30a3ba1f2fae6cf89fe2916
charliecano <github@theanomaly.net>
2014-08-06 14:29:04 -0700

    Updated DockMod to v2.04
    Upgrade

--- Casks/dockmod.rb
+++ Casks/dockmod.rb
@@ -1,8 +1,8 @@
 class Dockmod < Cask
-  version '2.03'
-  sha256 'e7a018f3d503f8124b8dc39c5b1113d0477f1d2f04aeb71ecefc67a405ee2034'
+  version '2.04'
+  sha256 '86c92aa446d436296a800ee832466afa845048316a09df15d0e793f5a4cad55d'
 
-  url 'http://spyresoft.com/dockmod/download.php?version=2.03'
+  url 'http://spyresoft.com/dockmod/download.php?version=2.04'
   appcast 'http://spyresoft.com/dockmod/updates.xml'
   homepage 'http://spyresoft.com/dockmod/'
 
