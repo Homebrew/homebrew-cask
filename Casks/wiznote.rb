f8ab2ea18cfea5a2e3ad94c3219fa33b8ffa3c37
Jason <jsfaint@gmail.com>
2014-08-07 09:47:24 +0800

    Create wiznote.rb
    Wiznote is a cloud based notes solution which helps personal and professional to take notes and collaborate with team members.

---
+++ Casks/wiznote.rb
@@ -0,0 +1,9 @@
+class Wiznote < Cask
+  version '2.1.11'
+  sha256 '2902349c5131236bbb464bf247955b15234f20d883e84cb340099c32c5cb9a9d'
+
+  url 'http://release.wiz.cn/wiznote-macos-2014-07-28.dmg'
+  homepage 'http://www.wiznote.com/'
+
+  link 'WizNote.app'
+end
