25e8bfce076928549d5e16d8e2ac1dd8bc31a555
Roland Walker <walker@pobox.com>
2014-08-09 10:06:35 -0400

    geppetto conditional: newest hardware default

--- Casks/geppetto.rb
+++ Casks/geppetto.rb
@@ -1,10 +1,10 @@
 class Geppetto < Cask
-  if Hardware::CPU.is_64_bit?
-    url 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.2.0-R201407250959.zip'
-    sha256 '7a09c823cea9900cb51d009f47fab69569e1d8115c6326f3e91db62714480d69'
-  else
+  if Hardware::CPU.is_32_bit?
     url 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86-4.2.0-R201407250959.zip'
     sha256 '78f578ff4cf0a9eadf85cc5a821e55125ee98ab4a8e1d4f0f5d1607487314804'
+  else
+    url 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.2.0-R201407250959.zip'
+    sha256 '7a09c823cea9900cb51d009f47fab69569e1d8115c6326f3e91db62714480d69'
   end
   homepage 'http://puppetlabs.github.io/geppetto/'
   version '4.2.0'
