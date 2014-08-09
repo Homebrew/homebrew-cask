c548d24e9de7e846a814106788ad63f8a3133904
Michael Ford <fanquake@users.noreply.github.com>
2014-08-09 18:07:23 +0800

    Seil 10.10.0

--- Casks/seil.rb
+++ Casks/seil.rb
@@ -1,8 +1,8 @@
 class Seil < Cask
-  version '10.9.0'
-  sha256 '25698cdc0b7e52b1c6cee58cdccd5104714d28d586b6707582c6d7ff2cde11b3'
+  version '10.10.0'
+  sha256 'a61653ce058dc943a403583281a1a10b2b8bd496362ecf7b93989fc90f5547df'
 
-  url 'https://pqrs.org/macosx/keyremap4macbook/files/Seil-10.9.0.dmg'
+  url 'https://pqrs.org/macosx/keyremap4macbook/files/Seil-10.10.0.dmg'
   homepage 'https://pqrs.org/macosx/keyremap4macbook/seil.html.en'
 
   install 'Seil.pkg'
