eecd99dd90b89b3ca085ca4a224aa7a93f664d64
Roland Walker <walker@pobox.com>
2014-08-09 10:10:15 -0400

    macvim conditional: newest OS default

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,11 +1,11 @@
 class Macvim < Cask
-  if MacOS.version >= :mavericks
-    url 'https://github.com/b4winckler/macvim/releases/download/snapshot-73/MacVim-snapshot-73-Mavericks.tbz'
-  appcast 'http://b4winckler.github.com/macvim/appcast/stable.xml'
-    sha256 '557c60f3487ab68426cf982c86270f2adfd15e8a4d535f762e6d55602754d224'
-  else
+  if MacOS.version < :mavericks
     url 'https://github.com/eee19/macvim/releases/download/snapshot-73/MacVim-snapshot-73-Mountain-Lion.tbz'
     sha256 '7f573fb9693052a86845c0a9cbb0b3c3c33ee23294f9d8111187377e4d89f72c'
+  else
+    url 'https://github.com/b4winckler/macvim/releases/download/snapshot-73/MacVim-snapshot-73-Mavericks.tbz'
+    appcast 'http://b4winckler.github.com/macvim/appcast/stable.xml'
+    sha256 '557c60f3487ab68426cf982c86270f2adfd15e8a4d535f762e6d55602754d224'
   end
   homepage 'http://code.google.com/p/macvim/'
   version '7.4-73'
