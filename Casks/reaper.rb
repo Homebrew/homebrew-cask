ee3fd857eebe896c621e648843c8e7705cc8f88b
Roland Walker <walker@pobox.com>
2014-08-09 10:06:35 -0400

    reaper conditional: newest hardware default

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -2,15 +2,15 @@ class Reaper < Cask
   homepage 'http://www.reaper.fm/'
   version '4.71'
 
-  if Hardware::CPU.is_64_bit?
-    url 'http://www.reaper.fm/files/4.x/reaper471_x86_64.dmg'
-    sha256 '3045fd59189bd0fe398b50f511d014a5e385c8cfb88f8a188a8437050f783d71'
-    link 'REAPER64.app'
-    link 'ReaMote64.app'
-  else
+  if Hardware::CPU.is_32_bit?
     url 'http://www.reaper.fm/files/4.x/reaper471_i386.dmg'
     sha256 '1a670d5afb5956e1486c1fdef008ccfb4faa7987cc4d5ea8171a2794ff86ffca'
     link 'REAPER.app'
     link 'ReaMote.app'
+  else
+    url 'http://www.reaper.fm/files/4.x/reaper471_x86_64.dmg'
+    sha256 '3045fd59189bd0fe398b50f511d014a5e385c8cfb88f8a188a8437050f783d71'
+    link 'REAPER64.app'
+    link 'ReaMote64.app'
   end
 end
