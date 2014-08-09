223b224a92ec41906c2aed7bea5d0dc4e06381f6
Roland Walker <walker@pobox.com>
2014-08-09 12:45:20 -0400

    plex-home-theater conditional: newest is default

--- Casks/plex-home-theater.rb
+++ Casks/plex-home-theater.rb
@@ -1,12 +1,12 @@
 class PlexHomeTheater < Cask
   version '1.2.1.314'
 
-  if MacOS.prefer_64_bit?
-    sha256 '0e243ca7112cccd11f75bf799ff21a69413dc1eca6652f934ed456ac54fab5ae'
-    url 'http://downloads.plexapp.com/plex-home-theater/1.2.1.314-7cb0133e/PlexHomeTheater-1.2.1.314-7cb0133e-macosx-x86_64.zip'
-  else
+  if Hardware::CPU.is_32_bit?
     sha256 '87954578b4aa1ec93876115967b0da61d6fa47f3f1125743a55f688366d56860'
     url 'http://downloads.plexapp.com/plex-home-theater/1.2.1.314-7cb0133e/PlexHomeTheater-1.2.1.314-7cb0133e-macosx-i386.zip'
+  else
+    sha256 '0e243ca7112cccd11f75bf799ff21a69413dc1eca6652f934ed456ac54fab5ae'
+    url 'http://downloads.plexapp.com/plex-home-theater/1.2.1.314-7cb0133e/PlexHomeTheater-1.2.1.314-7cb0133e-macosx-x86_64.zip'
   end
 
   homepage 'https://plex.tv'
