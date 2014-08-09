cfe8356196f6dc2497f23f51d18478e9510cc2d7
brice <brice@chatard.fr>
2014-08-09 07:36:35 +0200

    Upgrade Plex Home Theater to v1.2.1.314 and add 64bits version

--- Casks/plex-home-theater.rb
+++ Casks/plex-home-theater.rb
@@ -1,8 +1,14 @@
 class PlexHomeTheater < Cask
-  version '1.0.9.180'
-  sha256 'f7c51b212febafbca77e0af193819c7d7035a38600d65550db1362edadee31b7'
+  version '1.2.1.314'
 
-  url 'http://downloads.plexapp.com/plex-home-theater/1.0.9.180-bde1e61d/PlexHomeTheater-1.0.9.180-bde1e61d-macosx-i386.zip'
+  if MacOS.prefer_64_bit?
+    sha256 '0e243ca7112cccd11f75bf799ff21a69413dc1eca6652f934ed456ac54fab5ae'
+    url 'http://downloads.plexapp.com/plex-home-theater/1.2.1.314-7cb0133e/PlexHomeTheater-1.2.1.314-7cb0133e-macosx-x86_64.zip'
+  else
+    sha256 '87954578b4aa1ec93876115967b0da61d6fa47f3f1125743a55f688366d56860'
+    url 'http://downloads.plexapp.com/plex-home-theater/1.2.1.314-7cb0133e/PlexHomeTheater-1.2.1.314-7cb0133e-macosx-i386.zip'
+  end
+
   homepage 'https://plex.tv'
 
   link 'Plex Home Theater.app'
