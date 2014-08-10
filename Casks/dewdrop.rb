985e2dc368f9e0bc6f0ed3307165acc8648b9c40
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-08-10 09:59:37 -0500

    add dewdrop caveats

--- Casks/dewdrop.rb
+++ Casks/dewdrop.rb
@@ -7,4 +7,19 @@ class Dewdrop < Cask
   homepage 'http://dewdrop.dangelov.com/'
 
   link 'Dewdrop.app'
+
+  caveats do
+    <<-EOF.undent
+    The official PHP server software is available here  https://github.com/dewdrop-org/Server-PHP
+    An unofficial Node.js server is also available `npm install dewdrop-server-node`
+
+    You may wish to script your preferences using `defaults` ... e.g.:
+
+      defaults write dangelov.Dewdrop ddUsername "$USER"
+      defaults write dangelov.Dewdrop ddPassword "secret"
+      defaults write dangelov.Dewdrop ddServer "https://dewdrop.example.org"
+      defaults write dangelov.Dewdrop automaticallyUploadScreenshots 1
+
+    EOF
+  end
 end
