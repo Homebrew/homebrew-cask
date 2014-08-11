28a3fb6f5a54e54db512a3bd9bd8b40ac445d25e
Vítor Galvão <info@vitorgalvao.com>
2014-08-11 23:47:41 +0100

    updated p5 to 0.1.7

--- Casks/p5.rb
+++ Casks/p5.rb
@@ -1,6 +1,6 @@
 class P5 < Cask
-  version '0.1.6'
-  sha256 '06a6773bd6775b8f46db7b893f9ae44e7b2f365b5babf1bc0e7574dc42905d0b'
+  version '0.1.7'
+  sha256 '7b033360db4a113b2069482f442797b1e071084499586d0f1cf18567698ee5e8'
 
   url "https://github.com/antiboredom/jside/releases/download/v#{version}/p5.zip"
   homepage 'http://p5js.org/download/#editor'
