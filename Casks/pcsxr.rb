b0df0bf991e2bccbc552c9c6e87e0f42495ff06e
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-08-11 22:32:47 +0700

    Add PCSXR cask

---
+++ Casks/pcsxr.rb
@@ -0,0 +1,9 @@
+class Pcsxr < Cask
+  url 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pcsxr&DownloadId=722115&Build=20919'
+  sha256 'e7361ceb8dc9089cae084ce119a5dc4119cab22bb5d79ebf55af596b7e7eb916'
+
+  homepage 'https://pcsxr.codeplex.com'
+  version '1.9.93'
+
+  link 'PCSXR.app'
+end
