41e3f27806a0d86538c1509a25cf1a11c3eae2f2
mAAdhaTTah <jamesorodig@gmail.com>
2014-08-11 16:52:05 -0400

    Update Kext Wizard to new download location

--- Casks/kext-wizard.rb
+++ Casks/kext-wizard.rb
@@ -2,8 +2,8 @@ class KextWizard < Cask
   version 'latest'
   sha256 :no_check
 
-  url 'https://dl.dropboxusercontent.com/u/7085278/Kext_Wizard/KextWizard.zip'
-  appcast 'http://dl.dropbox.com/u/7085278/Kext_Wizard/update.xml'
+  url 'http://wizards.osxlatitude.com/kext/kw.zip'
+  appcast 'http://wizards.osxlatitude.com/kext/update.xml'
   homepage 'http://www.insanelymac.com/forum/topic/253395-kext-wizard-easy-to-use-kext-installer-and-more/'
 
   link 'Kext Wizard.app'
