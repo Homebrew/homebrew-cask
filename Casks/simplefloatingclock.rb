3dd6efa6448c5b4b46064c0f4d5f2dadcf378eb2
rcmdnk <rcmdnk@gmail.com>
2014-08-11 14:39:32 +0900

    Add SimpleFloatingClock.app v1.8

---
+++ Casks/simplefloatingclock.rb
@@ -0,0 +1,9 @@
+class Simplefloatingclock < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://www.splook.com/Software/distrib/sfc.zip'
+  homepage 'http://www.splook.com/Software/Simple_Floating_Clock.html'
+
+  link 'Simple Floating Clock/SimpleFloatingClock.app'
+end
