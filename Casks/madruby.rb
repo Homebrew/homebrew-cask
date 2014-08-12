ac547172b4a85bb320e4dfdff7ae252f9b64ccf7
rcmdnk <rcmdnk@gmail.com>
2014-08-12 14:11:22 +0900

    Add MadRuby.app v1.0.3

---
+++ Casks/madruby.rb
@@ -0,0 +1,9 @@
+class Madruby < Cask
+  version '1.0.3'
+  sha256 'a0d45b892b28e9fd4e5ae17578e3afc6e385307635c422d71292d47b177d7480'
+
+  url "http://www.wingsforpigs.com/downloads/MadRuby_#{version}.dmg"
+  homepage 'http://www.wingsforpigs.com/MadRuby/MadRuby.html'
+
+  link 'MadRuby.app'
+end
