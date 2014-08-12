73e53560a33ea269a6fb9daca5970c0c72c6833c
Dana Merrick <dana.merrick@gmail.com>
2014-08-12 14:19:27 -0700

    Update jaikoz.rb

--- Casks/jaikoz.rb
+++ Casks/jaikoz.rb
@@ -1,8 +1,8 @@
 class Jaikoz < Cask
-  version '6.2.0'
-  sha256 '5293e2094846a733384e18b61c61798b1a15b248155c6ce990c532d9a6c52170'
+  version 'latest'
+  sha256 :no_check
 
-  url 'http://jthink.net/jaikoz/jsp/manualdownload/jaikoz-osx.dmg?val=136'
+  url 'http://jthink.net/jaikoz/jsp/manualdownload/jaikoz-osx.dmg'
   homepage 'http://jthink.net/jaikoz'
 
   link 'Jaikoz.app'
