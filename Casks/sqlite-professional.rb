60b9bf966bb0782b986c08340aefb3e2e5032449
Wil Moore III <wil.moore@wilmoore.com>
2014-08-12 20:20:44 -0600

    add SQLite Professional

---
+++ Casks/sqlite-professional.rb
@@ -0,0 +1,9 @@
+class SqliteProfessional < Cask
+  version '1.0.39'
+  sha256 '1df9746a6d936fb98d3f095973f9a65ef74494a8a9a2925ba49e9cc8e400afda'
+
+  url 'https://www.sqlitepro.com/versions/SQLitePro1.0.39.zip'
+  homepage 'https://www.sqlitepro.com'
+
+  link 'SQLite Professional.app'
+end
