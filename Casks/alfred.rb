705194cd45d7b271399119d5bdfb647eeee597aa
Alejandro Beltrán <alebelcor@gmail.com>
2014-08-14 09:04:05 -0700

    Update alfred to 2.4_279

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,8 +1,8 @@
 class Alfred < Cask
-  version '2.4_278'
-  sha256 '4cd7fd4a9fb0168649dcf7cb68e2e247fe2a313630063d6222a7276fcfba31bb'
+  version '2.4_279'
+  sha256 '5faaa5e7029adb6a884433c3c442440cee6398241bc8098f02186e5e1f010dea'
 
-  url 'http://cachefly.alfredapp.com/Alfred_2.4_278.zip'
+  url 'http://cachefly.alfredapp.com/Alfred_2.4_279.zip'
   homepage 'http://www.alfredapp.com/'
 
   link 'Alfred 2.app'
