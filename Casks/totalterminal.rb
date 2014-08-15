9cda863c362ca74e49ccec2e8d857050be6725bc
Jon Chen <bsd@voltaire.sh>
2014-08-15 11:48:13 -0400

    bump totalterminal to 1.5.2

--- Casks/totalterminal.rb
+++ Casks/totalterminal.rb
@@ -1,8 +1,8 @@
 class Totalterminal < Cask
-  version '1.5'
-  sha256 '3d2083db08fddf532aa31ff72b140e799e97b9cbaa27e016d06f2133395add29'
+  version '1.5.2'
+  sha256 '5ed1490942b62e0d1e72c4f5e6f2dacb3dc2bde0919fd474fea1cadf28f7df42'
 
-  url 'http://downloads.binaryage.com/TotalTerminal-1.5.dmg'
+  url 'http://downloads.binaryage.com/TotalTerminal-1.5.2.dmg'
   homepage 'http://totalterminal.binaryage.com'
 
   install 'TotalTerminal.pkg'
