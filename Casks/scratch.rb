89a55cceef9befa4dc9b2fb237fce71c27efea5c
Johnathan Conley <johnathan.conley@gmail.com>
2014-08-15 11:45:03 -0500

    scratch: latest download url moved to new location

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -2,7 +2,7 @@ class Scratch < Cask
   version 'latest'
   sha256 :no_check
 
-  url 'http://download.scratch.mit.edu/Scratch.air'
+  url 'http://cdn.scratch.mit.edu/scratchr2/static/sa/Scratch.air'
   homepage 'http://scratch.mit.edu/scratch2download/'
 
   link 'Scratch 2.app'
