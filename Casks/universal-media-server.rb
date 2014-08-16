a37f508532822c39622c8d89e9f70872535a8cfa
digiou <digitalbckp@gmail.com>
2014-08-16 20:11:14 +0300

    Upgrade UMS to v4.0.3

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,8 +1,8 @@
 class UniversalMediaServer < Cask
-  version '4.0.2'
-  sha256 '9fabc3561151aa256237cded007a25f776383bbcaf18d6bb58bb5a16c11e4e8c'
+  version '4.0.3'
+  sha256 '3abec6ff17469108a8be33584583ce40d68eefac1f2d4252b4e23012047f5f96'
 
-  url 'http://downloads.sourceforge.net/sourceforge/unimediaserver/Official%20Releases/OS%20X/UMS-4.0.2-Java8.dmg'
+  url 'http://downloads.sourceforge.net/sourceforge/unimediaserver/Official%20Releases/OS%20X/UMS-4.0.3-Java8.dmg'
   homepage 'www.universalmediaserver.com'
 
   link 'Universal Media Server.app'
