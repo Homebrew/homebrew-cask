748d9202a6b17bdf8b7013530fea285070f091de
ZHANG Cheng <czhang.oss@gmail.com>
2014-08-16 12:51:20 +0800

    Add Vessel.app 0.3.0

---
+++ Casks/vessel.rb
@@ -0,0 +1,9 @@
+class Vessel < Cask
+  version '0.3.0'
+  sha256 'd53717d6de3f9d216585af56fd7e61f7a89abcd0436017f5861a17389c7a169a'
+
+  url 'http://awvessel.github.io/binaries/vessel-0.3.0.tbz2'
+  homepage 'http://awvessel.github.io'
+
+  link 'Vessel.app'
+end
