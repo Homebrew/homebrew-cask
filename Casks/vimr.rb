a52ef6ade22fc6ef820cf0e529a736afbb17f0b6
David Dinh <david@dinh.io>
2014-08-16 11:02:21 -0700

    Add VimR 0.1.4

---
+++ Casks/vimr.rb
@@ -0,0 +1,9 @@
+class Vimr < Cask
+  version '0.1.4'
+  sha256 '6ce530a59a81afe157b6f254a20b1d5f957dae94d613b4a4a73a69e0cf1c502c'
+
+  url 'https://github.com/qvacua/vimr/releases/download/v0.1.4-16/VimR-0.1.4.tar.bz2'
+  homepage 'http://vimr.org/'
+
+  link 'VimR.app'
+end
