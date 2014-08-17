e08ac04be03ac5640f93508f0b581bdd3a3d7488
Michael Ford <fanquake@users.noreply.github.com>
2014-08-17 17:59:46 +0800

    cTiVo 2.3.1_468

--- Casks/ctivo.rb
+++ Casks/ctivo.rb
@@ -1,8 +1,8 @@
 class Ctivo < Cask
-  version '2.2'
-  sha256 '50d8b6319039a5c1c6cac400706d3c59b9f90501bbc91283ca0157e4a2b68144'
+  version '2.3.1'
+  sha256 '55ad469432634a77be432741ee6dfc5480c3baf652a6d2b3cde70f919c1d13d7'
 
-  url 'https://github.com/dscottbuch/cTiVo/releases/download/v2.2.451/cTiVo_2.2.451.zip'
+  url 'https://github.com/dscottbuch/cTiVo/releases/download/v2.3.1.468/cTiVo_2.3.1_468.zip'
   appcast 'https://github.com/dscottbuch/cTiVo/blob/master/update/sparklecast.xml'
   homepage 'https://github.com/dscottbuch/cTiVo'
 
