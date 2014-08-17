839d61d15a928870ac9ffe31532b484a7c40ea3e
tacgnol <doug.hall@gmail.com>
2014-08-17 02:47:59 +0100

    Add ZOC terminal emulator - zoc6.app v6.59

---
+++ Casks/zoc.rb
@@ -0,0 +1,9 @@
+class Zoc < Cask
+  version '6.59'
+  sha256 'e5bab1efba9920129e7e829f255a73bf6abea443533c428ec797b787379b7827'
+
+  url 'http://www.emtec.com/downloads/zoc/zoc659.dmg'
+  homepage 'http://www.emtec.com/zoc/'
+
+  link 'zoc6.app'
+end
