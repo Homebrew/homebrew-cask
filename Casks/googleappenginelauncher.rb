ca1e7fa3d0eca58f786a71b3885d591378de35e4
Andy Li <andy@onthewings.net>
2014-08-17 16:17:44 +0800

    googleappenginelauncher 1.9.9

--- Casks/googleappenginelauncher.rb
+++ Casks/googleappenginelauncher.rb
@@ -1,6 +1,6 @@
 class Googleappenginelauncher < Cask
-  version '1.9.7'
-  sha256 '583da1171f3769fb697588ed9c6153eb7e8af007af801026b4226b447e923af7'
+  version '1.9.9'
+  sha256 '78e8a5235411a7f8122fbf0870273b39024a05c60fdb94430f5e78d95e353d1f'
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
   homepage 'https://developers.google.com/appengine/'
 
