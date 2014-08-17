f61a938ea52963dcd7d009ad0aca475b834a7899
Michael Ford <fanquake@users.noreply.github.com>
2014-08-17 20:00:28 +0800

    GraphSketcher 2.0_test_46

--- Casks/graphsketcher.rb
+++ Casks/graphsketcher.rb
@@ -1,8 +1,8 @@
 class Graphsketcher < Cask
-  version 'v2.0_test_45'
-  sha256 '4a628aae8c02b0e236ef8749e6669d8cb7d15df9b9792818d45eededb8b57113'
+  version 'v2.0_test_46'
+  sha256 '34fe6fc319b2fcfe0bf51e02bc05d020b2867ff1ee20587becb60d7e67309be6'
 
-  url 'https://github.com/graphsketcher/GraphSketcher/releases/download/v2.0_test_45/GraphSketcher.zip'
+  url 'https://github.com/graphsketcher/GraphSketcher/releases/download/v2.0_test_46/GraphSketcher.zip'
   homepage 'https://github.com/graphsketcher/GraphSketcher'
 
   link 'GraphSketcher/GraphSketcher.app'
