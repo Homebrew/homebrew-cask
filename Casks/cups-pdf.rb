e3e31038a4f42dd2e516ca70607620d6580a6a0b
Michael Ford <fanquake@users.noreply.github.com>
2014-08-17 18:02:45 +0800

    Cups PDF 2.6.1

--- Casks/cups-pdf.rb
+++ Casks/cups-pdf.rb
@@ -1,8 +1,8 @@
 class CupsPdf < Cask
-  version '2.5.0'
-  sha256 'a51a44dfb2f12fd6a291a94dcd863690c8246f6d66e10d74b0a30c1b67b080f7'
+  version '2.6.1'
+  sha256 '24bf65884dda2d3fd60266f96bf76791a61ec9095f11073047d880de692784b9'
 
-  url 'https://bitbucket.org/codepoet/cups-pdf-for-mac-os-x/downloads/CUPS-PDF%202.5.0%20Installer.zip'
+  url 'https://bitbucket.org/codepoet/cups-pdf-for-mac-os-x/downloads/CUPS-PDF%202.6.1%20Installer.pkg'
   homepage 'http://www.cups-pdf.de'
 
   install 'CUPS-PDF.mpkg'
