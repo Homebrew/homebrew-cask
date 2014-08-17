88ca657a5dfcd0b98b7b6acd0f03501e9947b8a4
Michael Ford <fanquake@users.noreply.github.com>
2014-08-17 18:11:31 +0800

    Droplet Manager 0.5.0

--- Casks/dropletmanager.rb
+++ Casks/dropletmanager.rb
@@ -1,8 +1,8 @@
 class Dropletmanager < Cask
-  version '0.3.1'
-  sha256 '70351fc8c77a9c4f71f1f25567b33ab623fce1cf899469f5e31d9cad171ceedd'
+  version '0.5.0'
+  sha256 'ed8011cef55f3bcdde4e7e7e331775808e4701fc41acc3191d3e4d80f8ab8335'
 
-  url 'https://github.com/deivuh/DODropletManager-OSX/releases/download/v0.3.1/DropletManagerv0.3.1.zip'
+  url 'https://github.com/deivuh/DODropletManager-OSX/releases/download/v0.5.0/DropletManager.v0.5.0.zip'
   homepage 'https://github.com/deivuh/DODropletManager-OSX'
 
   link 'DropletManager.app'
