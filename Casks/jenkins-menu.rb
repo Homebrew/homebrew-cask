c9db91553e5838eb8168a7da4206fe38e5de8304
Michael Ford <fanquake@users.noreply.github.com>
2014-08-17 20:17:10 +0800

    Jenkins Menu 0.1.1

--- Casks/jenkins-menu.rb
+++ Casks/jenkins-menu.rb
@@ -1,8 +1,8 @@
 class JenkinsMenu < Cask
-  version '0.1.0'
-  sha256 '8fd5c85a5615bd0470cb69bd8c28cfbfc7189f74c1abd490181bb74b166fce37'
+  version '0.1.1'
+  sha256 'c53e72473ff1d64b0b63b28ebd6297553cd6045c383b48e15887041487b588fb'
 
-  url 'https://bitbucket.org/qvacua/qvacua/downloads/JenkinsMenu-0.1.0.zip'
+  url 'https://bitbucket.org/qvacua/qvacua/downloads/JenkinsMenu-0.1.1.zip'
   appcast 'http://qvacua.com/jenkinsmenu/appcast.xml'
   homepage 'http://qvacua.com'
 
