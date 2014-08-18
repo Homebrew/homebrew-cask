0eb8229352de52ff74325b0bdf81e284272df6cd
Michael Ford <fanquake@users.noreply.github.com>
2014-08-18 09:29:54 +0800

    Yubiswitch 0.7

--- Casks/yubiswitch.rb
+++ Casks/yubiswitch.rb
@@ -1,8 +1,8 @@
 class Yubiswitch < Cask
-  version '0.3'
-  sha256 '9e22748a812ca8cfe0316d3dd90ffcbeedaa4358c3e1b0f25f075155bdcdaf19'
+  version '0.7'
+  sha256 '680577971e037ca52292e62809e0709adce1d79946a7d740627e83cda6d4d6ca'
 
-  url 'https://github.com/pallotron/yubiswitch/releases/download/v0.3/yubiswitch_0.3.dmg'
+  url 'https://github.com/pallotron/yubiswitch/releases/download/v0.7/yubiswitch_0.7.dmg'
   homepage 'https://github.com/pallotron/yubiswitch'
 
   link 'yubiswitch.app'
