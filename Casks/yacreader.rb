9f5f53cfecdff6522ea9974afe501505345c99b7
Michael Ford <fanquake@users.noreply.github.com>
2014-08-18 09:32:49 +0800

    YACReader 7.1.0

--- Casks/yacreader.rb
+++ Casks/yacreader.rb
@@ -1,8 +1,8 @@
 class Yacreader < Cask
-  version '7.0.2'
-  sha256 '813c27b6effa62a17ab939a4734fb5f0245308e9e00cc907bb1210636e43e1d7'
+  version '7.1.0'
+  sha256 '70cc5b3abb8fd22369ed4a2e930479e841165e0b4adf688b1d970f7f4f8b42bb'
 
-  url 'https://bitbucket.org/luisangelsm/yacreader/downloads/YACReader-7.0.2-MacOSX-Intel.dmg'
+  url 'https://bitbucket.org/luisangelsm/yacreader/downloads/YACReader-7.1.0-MacOSX-Intel.dmg'
   homepage 'http://www.yacreader.com'
 
   link 'YACReader.app'
