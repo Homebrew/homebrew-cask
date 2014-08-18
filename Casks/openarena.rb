9a021c3a7173e402fb73763dcbfa74968737b6cd
Bruno Buccolo <bruno.buccolo@gmail.com>
2014-08-18 10:38:31 -0300

    Add OpenArena.app v0.8.8

---
+++ Casks/openarena.rb
@@ -0,0 +1,13 @@
+class Openarena < Cask
+  version '0.8.8'
+  sha256 '5a8faf7f5b51f351b0a1618c06b6b98a5f1a6758f1d39818de2c87df2a0bac4a'
+
+  url 'http://openarena.ws/request.php?4'
+  homepage 'http://openarena.ws'
+
+  link 'openarena-0.8.8/OpenArena.app'
+
+  after_install do
+    system '/bin/chmod', '--', '755', "#{destination_path}/openarena-0.8.8/OpenArena.app/Contents/MacOS/openarena.ub"
+  end
+end
