2d1f8423227b895de72bb26857f7370b5d9a6b82
Vincent Arel-Bundock <varel@umich.edu>
2014-08-17 22:37:01 -0400

    rstudio 0.98.1028

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -1,6 +1,6 @@
 class Rstudio < Cask
-  version '0.98.978'
-  sha256 '8484c18c356fe046682be7abe64312ca4defe2fc8b72ef55fbb396f8097e48e2'
+  version '0.98.1028'
+  sha256 '82979dbaef5eb072e392e7e31ebb46760ac028ce238a946687917a33b35fc811'
 
   url "http://download1.rstudio.org/RStudio-#{version}.dmg"
   homepage 'http://www.rstudio.com/'
