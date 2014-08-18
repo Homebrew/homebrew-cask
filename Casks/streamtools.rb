e4af10258d20e4fd0160981afa282814f135c05a
Michael Ford <fanquake@users.noreply.github.com>
2014-08-18 11:18:15 +0800

    Streamtools 0.2.8

--- Casks/streamtools.rb
+++ Casks/streamtools.rb
@@ -1,13 +1,13 @@
 class Streamtools < Cask
   if Hardware::CPU.is_32_bit?
-    url 'https://github.com/nytlabs/streamtools/releases/download/v0.2.3/st_darwin_386-0.2.3.tar.gz'
-    sha256 'dd69376923570ea12846bcd75b3a9a46a621cb8882de81e93e71f67308b1d723'
-    binary 'st_darwin_386-0.2.3/st'
+    url 'https://github.com/nytlabs/streamtools/releases/download/0.2.8/st_darwin_386-0.2.8.tar.gz'
+    sha256 'de5763ffcd6689d2157ab115e1a3d8a34e85e1e4de7b186f7f5079fe01a5f004'
+    binary 'st_darwin_386-0.2.8/st'
   else
-    url 'https://github.com/nytlabs/streamtools/releases/download/v0.2.3/st_darwin_amd64-0.2.3.tar.gz'
-    sha256 'dd8a2d82b018ad283fbdea8bb2427814e87f6bbe070367ba07b4c054d758d75c'
-    binary 'st_darwin_amd64-0.2.3/st'
+    url 'https://github.com/nytlabs/streamtools/releases/download/0.2.8/st_darwin_amd64-0.2.8.tar.gz'
+    sha256 '5ca21f4c4c2091c96c508bb277cb5c022f18a8cd3c53abb1ceb11ca0f6454309'
+    binary 'st_darwin_amd64-0.2.8/st'
   end
   homepage 'https://github.com/nytlabs/streamtools'
-  version '0.2.3'
+  version '0.2.8'
 end
