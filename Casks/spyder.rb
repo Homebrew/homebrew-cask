8a2a04a6469bb99706a66c19301fdf89177adf75
Michael Ford <fanquake@users.noreply.github.com>
2014-08-18 11:25:58 +0800

    Spyder 2.3.0

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -1,8 +1,8 @@
 class Spyder < Cask
-  version '2.2.5'
-  sha256 '573f75aa60a9533a674938536588edf5d1bc12d1fdebcff3e5397b8e4f96546a'
+  version '2.3.0'
+  sha256 '1a665fffc14c04647716da6a4108b152e983603ff7fd99680bafa915e361f27e'
 
-  url 'https://bitbucket.org/spyder-ide/spyderlib/downloads/spyder-2.2.5.dmg'
+  url 'https://bitbucket.org/spyder-ide/spyderlib/downloads/spyder-2.3.0.dmg'
   homepage 'https://code.google.com/p/spyderlib/'
 
   link 'Spyder.app'
