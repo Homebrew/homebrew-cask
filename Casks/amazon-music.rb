0b10cce672c0e852288baa0ee62bf013e21031b6
Michael Ford <fanquake@users.noreply.github.com>
2014-08-18 08:18:04 +0800

    Amazon Music 3.2

--- Casks/amazon-music.rb
+++ Casks/amazon-music.rb
@@ -1,8 +1,8 @@
 class AmazonMusic < Cask
-  version '3.1'
-  sha256 '77a402077ae3f43aafa5433f7b26017643080a73fa0ad49953dedbeaca09574a'
+  version '3.2'
+  sha256 'fc003860aceaa3cb4200d9f4e66794fda654dd1db4fa646a88e79502547728a3'
 
-  url 'https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/20140702/064904ec21/AmazonMusicInstaller.dmg'
+  url 'https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/20140722/2032026e85/AmazonMusicInstaller.dmg'
   homepage 'https://www.amazon.com/gp/feature.html/ref=dm_mo_cpw_fb_lm?docId=1001067901'
 
   caskroom_only true
