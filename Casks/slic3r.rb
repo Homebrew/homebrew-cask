052e6d57586b4bd0c9b22dd6d490c305d5c3357b
Michael Ford <fanquake@users.noreply.github.com>
2014-08-18 11:40:59 +0800

    Slic3r 1.1.7

--- Casks/slic3r.rb
+++ Casks/slic3r.rb
@@ -1,6 +1,6 @@
 class Slic3r < Cask
-  version '1.1.6'
-  sha256 '544cb36ff905d32790f66c3872b0806e6854f5214fa38fd705afbb610a5f718f'
+  version '1.1.7'
+  sha256 '41ae1c3c5ee7ddeb2431698b63f6e4112c68b830de9f024730388a1eb062f80b'
 
   url "http://dl.slic3r.org/mac/slic3r-osx-uni-#{version.gsub('.', '-')}-stable.dmg"
   homepage 'http://slic3r.org/'
