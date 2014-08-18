0b47eb3eae3d94307d2f91963d15a0583b4be10c
Michael Ford <fanquake@gmail.com>
2014-08-18 09:34:24 +0800

    Add Crypt App

---
+++ Casks/crypt.rb
@@ -0,0 +1,9 @@
+class Crypt < Cask
+  version '3'
+  sha256 'e9e82ad331fe26a26ddb9625a9e1ac8e65086770c1abf4bfa503c8336bdf37bd'
+
+  url 'http://voluntary.net.s3.amazonaws.com/Crypt3_20100429.zip'
+  homepage 'http://voluntary.net/crypt/'
+
+  link 'Crypt3.app'
+end
