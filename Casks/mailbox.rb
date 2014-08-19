bc8d8c41418700965c36fbfac792fc13ddf56001
Richard Lee <dlackty@gmail.com>
2014-08-20 00:55:11 +0800

    Add Mailbox

---
+++ Casks/mailbox.rb
@@ -0,0 +1,9 @@
+class Mailbox < Cask
+  version '0.3.6'
+  sha256 '4592335b95f5bd5e2c77deec121ba8f1f39fd17c3e295087ee4aa8766975c116'
+
+  url 'https://download.mailboxapp.com/Mailbox-Beta-0.3.6.dmg'
+  homepage 'http://www.mailboxapp.com/'
+
+  link 'Mailbox (Beta).app'
+end
