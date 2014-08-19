991a1f028595a505ddf1c2dd93078882b3c7c64c
Roland Walker <walker@pobox.com>
2014-08-19 11:52:34 -0400

    fix basictex filename (install stanza value)
    re-use version number in stanzas
    closes #5825

--- Casks/basictex.rb
+++ Casks/basictex.rb
@@ -2,10 +2,10 @@ class Basictex < Cask
   version '20140807'
   sha256 '02b2c0160ddd7a74f25b2ea58d1a79deec9b2230ff41d0c45c1c3157fa607afc'
 
-  url 'http://mirror.ctan.org/systems/mac/mactex/basictex20140807.pkg'
+  url "http://mirror.ctan.org/systems/mac/mactex/basictex#{version}.pkg"
   homepage 'http://www.tug.org/mactex/morepackages.html'
 
-  install 'mactex-basic.pkg'
+  install "basictex#{version}.pkg"
   uninstall :pkgutil => 'org.tug.mactex.basictex2014'
   caveats do
     path_environment_variable '/usr/texbin'
