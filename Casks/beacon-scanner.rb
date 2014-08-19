5d504cf97582eaf833519ff78558ff58edefa1e5
Michael Ford <fanquake@users.noreply.github.com>
2014-08-19 08:45:26 +0800

    Beacon Scanner 1.1.5

--- Casks/beacon-scanner.rb
+++ Casks/beacon-scanner.rb
@@ -1,8 +1,8 @@
 class BeaconScanner < Cask
-  version '1.1.4'
-  sha256 '5210c5a56325c1129f5975738d4c5113f79b015c18340af1e2ed83a1e367de67'
+  version '1.1.5'
+  sha256 '1c7662420ab616004e0f76320b76bf9ee5c3c75292c553bc891ac4986208bbb4'
 
-  url 'https://github.com/mlwelles/BeaconScanner/releases/download/1.1/BeaconScanner-1.1.4.zip'
+  url 'https://github.com/mlwelles/BeaconScanner/releases/download/1.1.5/BeaconScanner.zip'
   homepage 'https://github.com/mlwelles/BeaconScanner/'
 
   link 'Beacon Scanner.app'
