bdbad0615464a5672f807fbce935b8d3dac2ede6
ZHANG Cheng <czhang.oss@gmail.com>
2014-08-19 23:25:28 +0800

    Add screen saver fliqlo 1.5.2

---
+++ Casks/fliqlo.rb
@@ -0,0 +1,9 @@
+class Fliqlo < Cask
+  version '1.5.2'
+  sha256 '132f760525e2b1956e708c99676b1f1a6d30c10d64eda912a669c0a25c38ad46'
+
+  url 'http://fliqlo.com/download/fliqlo_152.dmg', :referer => 'http://fliqlo.com/#about'
+  homepage 'http://fliqlo.com/'
+
+  screen_saver 'Fliqlo.saver'
+end
