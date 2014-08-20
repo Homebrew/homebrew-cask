905361137d950003d78278e60967be4f7355e6cd
Michael Ford <fanquake@users.noreply.github.com>
2014-08-20 16:30:57 +0800

    Breach 3.22-alpha.6

--- Casks/breach.rb
+++ Casks/breach.rb
@@ -1,6 +1,6 @@
 class Breach < Cask
-  version '0.3.20-alpha.5'
-  sha256 '7b2389932bf32a48c4bb485a5bbe0f80fb49ae5c85e7d8319a51aec14714d4e1'
+  version '0.3.22-alpha.6'
+  sha256 '5f9cd82b3a6e510b8876d3fe16c64dfbc748041a1610710055a4d608169b2a35'
 
   url "https://raw.githubusercontent.com/breach/releases/master/v#{version}/breach-v#{version}-darwin-ia32.tar.gz"
   homepage 'http://breach.cc'
