1e5f2cca1974de3bbee8ebb0036053e6a1efc23f
Emmanuel Gomez <emmanuel@gomez.io>
2014-08-20 13:18:00 -0700

    Add Mnemosyne.app v1.1.

---
+++ Casks/subclassed-mnemosyne.rb
@@ -0,0 +1,9 @@
+class SubclassedMnemosyne < Cask
+  version '1.1'
+  sha256 'c641f423f449104615f614bedc7a54938ae67b63a97842351a3258b0a283f6b3'
+
+  url 'http://www.subclassed.com/download/Mnemosyne.zip'
+  homepage 'http://www.subclassed.com/apps/mnemosyne/details'
+
+  link 'Mnemosyne.app'
+end
