f5938c466bd7230c00600c0ebaebe3b79aebeb1f
Michael Ford <fanquake@users.noreply.github.com>
2014-08-20 16:28:48 +0800

    Aptana Studio 3.6.0

--- Casks/aptanastudio.rb
+++ Casks/aptanastudio.rb
@@ -1,8 +1,8 @@
 class Aptanastudio < Cask
-  version '3.4.2'
-  sha256 '26d40abf9152f16d8190a1968f16c0158a4992d28333351db6fa455af68958f4'
+  version '3.6.0'
+  sha256 'd2207934d485cca10d6ad624a67d9f21c1bef46e0a9d58a9db334cec7fb55948'
 
-  url 'http://download.aptana.com/studio3/standalone/3.4.2/mac/Aptana_Studio_3_Setup_3.4.2.dmg'
+  url 'http://download.aptana.com/studio3/standalone/3.6.0/mac/Aptana_Studio_3_Setup_3.6.0.dmg'
   homepage 'http://www.aptana.com/'
 
   link 'Aptana Studio 3/AptanaStudio3.app'
