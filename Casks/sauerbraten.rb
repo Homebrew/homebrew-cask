f00df3ac3725bfb72cc51088b8c341b7102b0420
Johannes Hertenstein <johannes@nimius.net>
2014-08-20 10:52:09 +0200

    New Cask: Sauerbraten (Cube2)

---
+++ Casks/sauerbraten.rb
@@ -0,0 +1,9 @@
+class Sauerbraten < Cask
+  version '2013.01.26'
+  sha256 'c6807484fa0d2c42ac774b97f9b21e9eace720e4403abc11b0321b2645d3589d'
+
+  url 'http://downloads.sourceforge.net/sauerbraten/sauerbraten_2013_01_26_collect_edition_macosx.dmg'
+  homepage 'http://sauerbraten.org'
+
+  link 'Sauerbraten.app'
+end
