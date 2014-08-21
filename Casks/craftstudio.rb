8da5c863ccd74b748284caca465258601f4bb171
thesbros <ops991923@gmail.com>
2014-08-20 23:12:58 -0700

    Add CraftStudio

---
+++ Casks/craftstudio.rb
@@ -0,0 +1,17 @@
+class Craftstudio < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://craftstud.io/files/OSX/CraftStudio.pkg'
+  homepage 'http://craftstud.io'
+
+  install 'CraftStudio.pkg'
+
+  uninstall :quit => 'com.sparklinlabs.CraftStudioLauncher',
+            :pkgutil => 'com.sparklinlabs.CraftStudioLauncher',
+            :files => '~/Library/CraftStudio'
+
+  caveats do
+    "Requires mono-mre. You can install this by running brew cask install mono-mre."
+  end
+end
