c82b1bd698bfa1a7f0d9052b9e1ecdc8fe269b03
Douglas Ligertwood <dfaligertwood@dunelm.org.uk>
2014-08-21 22:50:05 +0100

    Add Pliny.app v1.1.0

---
+++ Casks/pliny.rb
@@ -0,0 +1,9 @@
+class Pliny < Cask
+  version '1.1.0'
+  sha256 '1c263c181988e3cbbc110aa2802470272d7d3969cd6aa309af98b63a2680b215'
+
+  url 'http://pliny.cch.kcl.ac.uk/pliny-app-1.1.0.tar.gz'
+  homepage 'http://pliny.cch.kcl.ac.uk'
+
+  link 'Pliny/pliny.app'
+end
