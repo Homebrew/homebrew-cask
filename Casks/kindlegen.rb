712f3ca061cb81968364e1c4c9102a9b31731e91
Vítor Galvão <info@vitorgalvao.com>
2014-08-22 18:50:05 +0100

    kindlegen — facilitated updates

--- Casks/kindlegen.rb
+++ Casks/kindlegen.rb
@@ -2,8 +2,8 @@ class Kindlegen < Cask
   version '2.9'
   sha256 '45e5feac97f0590038a911fffd7d332973d88a40ad52135dce89afed33e7236c'
 
-  url 'https://kindlegen.s3.amazonaws.com/KindleGen_Mac_i386_v2_9.zip'
+  url "https://kindlegen.s3.amazonaws.com/KindleGen_Mac_i386_v#{version.gsub('.', '_')}.zip"
   homepage 'http://www.amazon.com/gp/feature.html?docId=1000765211'
 
-  binary 'KindleGen_Mac_i386_v2_9/kindlegen'
+  binary "KindleGen_Mac_i386_v#{version.gsub('.', '_')}/kindlegen"
 end
