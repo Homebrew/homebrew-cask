891c4a284040c94c2b6be37b97da133809cb785f
rochefort <terasawan@gmail.com>
2014-08-26 01:51:53 +0900

    Upgrade Lyrics Master.app to v2.4.2

--- Casks/lyrics-master.rb
+++ Casks/lyrics-master.rb
@@ -1,8 +1,8 @@
 class LyricsMaster < Cask
-  version '2.4.1'
-  sha256 '773cb3996b5f946b506214a0087afebdb7f7350ac17f477cc3f31bedf2a9be0c'
+  version '2.4.2'
+  sha256 '1cd0f639ea1e557e0abef320ee8ea61877c24bab444cb573bfd41d0887605d7f'
 
-  url 'http://www.kenichimaehashi.com/lyricsmaster/download/LyricsMaster2410.dmg'
+  url 'http://www.kenichimaehashi.com/lyricsmaster/download/LyricsMaster2420.dmg'
   homepage 'http://www.kenichimaehashi.com/lyricsmaster/'
 
   link 'Lyrics Master'
