281cf70563ddca8041d4bf0bffaafbb5f513dfc9
Arnaud RENARD <arenard@gmail.com>
2014-08-25 10:27:39 +0200

    Update MakeMKV to version 1.8.12

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,8 +1,8 @@
 class Makemkv < Cask
-  version '1.8.11'
-  sha256 '6551e167079d49c4bc414ce9494afae2857338ea5fbcd2ed63d705d2313b48a6'
+  version '1.8.12'
+  sha256 '06a7868764871cbf9b1db90def501bee1f4563a7c9bc54f2b1dab2302317aef0'
 
-  url 'http://www.makemkv.com/download/makemkv_v1.8.11_osx.dmg'
+  url 'http://www.makemkv.com/download/makemkv_v1.8.12_osx.dmg'
   homepage 'http://www.makemkv.com/'
 
   link 'MakeMKV.app'
