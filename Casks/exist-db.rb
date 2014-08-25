0ad60e8aa9662d020e2e14e17f0fced1852e911c
Janusz Dziurzynski <john@evesaddiction.com>
2014-08-24 22:07:17 -0400

    Add eXist-db 2.2RC2 (http://exist-db.org)

---
+++ Casks/exist-db.rb
@@ -0,0 +1,9 @@
+class ExistDb < Cask
+  version '2.2RC2'
+  sha256 '35aa7e36f21043629a7d562968cc803e6dae84429be4a0d1c77ed62bdf6f9f2f'
+
+  url 'http://sourceforge.net/projects/exist/files/Stable/2.2/eXist-db-2.2RC2.dmg/download'
+  homepage 'http://exist-db.org/'
+
+  link 'eXist-db.app'
+end
