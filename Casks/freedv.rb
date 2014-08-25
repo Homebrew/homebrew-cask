c952d601e96320ff874b3cbec195cecdd056439e
xyb <xieyanbo@gmail.com>
2014-08-25 11:34:06 +0800

    Add FreeDV.app v0.96.6

---
+++ Casks/freedv.rb
@@ -0,0 +1,9 @@
+class Freedv < Cask
+  version '0.96.6'
+  sha256 'e8c42dbb7fc5b8dd88020aa000149185adc5f9583094617966008e5b39d54970'
+
+  url 'http://files.freedv.org/OSX/FreeDV-0.96.6.dmg'
+  homepage 'http://freedv.org/tiki-index.php'
+
+  link 'FreeDV.app'
+end
