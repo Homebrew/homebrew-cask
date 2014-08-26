49530903241aeb78981675883bc24d9b4612f9a0
Keng Kiat Lim <kkin@crnrc.com>
2014-08-26 14:56:02 +0800

    Upgrade Sparkle to 1.8.0

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -1,10 +1,10 @@
 class Sparkle < Cask
-  version '1.5b6'
-  sha256 'ad64ca186995492e4dbfe2ed20cd22688a11670ef5af1fd6f0aea35f2abdd83b'
+  version '1.8.0'
+  sha256 '882f2e414d2d8a3e776f9a9888cbfcb744df88f3b061325c2aad3c965af5ac1a'
 
-  url 'http://sparkle.andymatuschak.org/files/Sparkle%201.5b6.zip'
-  appcast 'http://www.andymatuschak.org/files/sparkletestcast.xml'
-  homepage 'http://sparkle.andymatuschak.org/'
+  url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.bz2"
+  appcast 'http://sparkle-project.org/files/sparkletestcast.xml'
+  homepage 'http://sparkle-project.org/'
 
   link 'Sparkle Test App.app'
 end
