0a2e3464259f3745dfc34f9245618cd057f9fe0c
Rob Donnelly <rndonnelly@gmail.com>
2014-08-26 22:52:37 -0400

    Update JuliaStudio.app v0.4.4
    correct Cask name
    support older OS X version < 10.7

--- Casks/julia-studio.rb
+++ Casks/julia-studio.rb
@@ -1,12 +1,13 @@
-class JuliaStudio < Cask
-  version '0.4.4'
-  sha256 'ce165f2ff8b1fd76ad3d1f4cb822dbf23c4d87390e6b4089b0ea17ab18bfd3ed'
-
-  url 'https://s3.amazonaws.com/cdn-common.forio.com/julia-studio/0.4.4/julia-studio-macx-installer-0.4.4.dmg'
-  homepage 'http://forio.com/products/julia-studio/'
-
-  link 'JuliaStudio.app'
-  caveats do
-    os_version_only '10.7', '10.8', '10.9'
+class Juliastudio < Cask
+  if MacOS.version < '10.7'
+    url 'https://s3.amazonaws.com/cdn-common.forio.com/julia-studio/0.4.4/julia-studio-mac10.6-installer-0.4.4.dmg'
+    version '0.4.4'
+    sha256 '380006f00db5b19ab1a68b25b72dba669d7ff8f109c68d6ecf1fcc34dbfe485f'
+  else
+    url 'https://s3.amazonaws.com/cdn-common.forio.com/julia-studio/0.4.4/julia-studio-macx-installer-0.4.4.dmg'
+    version '0.4.4'
+    sha256 'ce165f2ff8b1fd76ad3d1f4cb822dbf23c4d87390e6b4089b0ea17ab18bfd3ed'
   end
+  homepage 'http://forio.com/labs/julia-studio/'
+  link 'JuliaStudio.app'
 end
