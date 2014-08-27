09e6c114a802e1f6398796a8846f81dc5329a3ac
周明智 <michael@michael-mbp.local>
2014-08-27 10:54:35 +0800

    Add HBuilder.

---
+++ Casks/hbuilder.rb
@@ -0,0 +1,9 @@
+class Hbuilder < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://download.dcloud.net.cn/HBuilder.macosx.zip'
+  homepage 'http://dcloud.io/'
+
+  link 'HBuilder.app'
+end
