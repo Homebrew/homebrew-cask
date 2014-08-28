0a2e243889f1915f941e8b334540ce7ef5714378
Tolbkni Kao <tolbkni@gmail.com>
2014-08-28 19:59:19 +0800

    Upgrade LiteIDE.app to v23.2

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,8 +1,8 @@
 class Liteide < Cask
-  version '23.1'
-  sha256 '87445988ad3f8074b7c16ebf1a2eeab732621f32794fdb2d59bc40ffe2995820'
+  version '23.2'
+  sha256 '29cf71bfb324e5d648038fd3e5b4ddc9335b8ca6393281d14c40067e9cb01b8b'
 
-  url "https://downloads.sourceforge.net/project/liteide/X#{version}/liteidex#{version}.macosx.zip"
+  url "https://downloads.sourceforge.net/project/liteide/X#{version}/liteidex#{version}-1.macosx.zip"
   homepage 'https://github.com/visualfc/liteide'
 
   link 'LiteIDE.app'
