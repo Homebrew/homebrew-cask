106276b45d51c47ccf0d770ca265d19da74ce7cb
alswl <alswlx@gmail.com>
2014-08-29 21:44:02 +0800

    add toodledo

---
+++ Casks/toodledo.rb
@@ -0,0 +1,9 @@
+class Toodledo < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.toodledo.com/tools/Toodledo.dmg'
+  homepage 'https://www.toodledo.com/tools/mac_menubar.php'
+
+  link 'Toodledo.app'
+end
