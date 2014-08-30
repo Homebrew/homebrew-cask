588566ef482a568debb8c9dfe8f8c8cab8e534bf
Keng Kiat Lim <kkin@crnrc.com>
2014-08-31 01:22:42 +0800

    Update Shortcat to 0.7.2

--- Casks/shortcat.rb
+++ Casks/shortcat.rb
@@ -1,6 +1,6 @@
 class Shortcat < Cask
-  version '0.7.1'
-  sha256 '9d7c2b0f2ec6ce03d0ccdf5ad15db3136ae344d26c09d1e5a8753e7b5698dc1a'
+  version '0.7.2'
+  sha256 '5ddb9edbf79c627f19b67f237fa899982ae47064fae01168abe7e3be2be0ce31'
 
   url "https://files.shortcatapp.com/v#{version}/Shortcat.zip"
   appcast 'https://shortcatapp.com/updates/appcast.xml'
