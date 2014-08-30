483c79ff8c0ee22d0ca324273c1c12c9ab37a007
Tristan Naumann <tristan.naumann@gmail.com>
2014-08-30 10:09:21 -0700

    New forumla: Pixlr

---
+++ Casks/pixlr.rb
@@ -0,0 +1,9 @@
+class Pixlr < Cask
+  version '1.0.1_26'
+  sha256 'a68fb58bd8f10eaa31bafcdee1c6004fcae12f750830cf8a7b7399bf23ba8087'
+
+  url 'https://cdn2.pixlr.com/mac/Autodesk_Pixlr_ver1.0.1_26_Mac_Installer.dmg'
+  homepage 'https://pixlr.com'
+
+  link 'Pixlr.app'
+end
