d6e039d82faa6b086e5d4107e8454b184ebfb1f4
kkdd <kkondo_z0@nifty.com>
2014-08-31 16:44:22 +0900

    Create abscissa.rb

---
+++ Casks/abscissa.rb
@@ -0,0 +1,9 @@
+class Abscissa < Cask
+  version '3.4.3'
+  sha256 :no_check
+
+  url 'http://rbruehl.macbay.de/Abscissa/Downloads/Abscissa-3.4.3.zip'
+  homepage 'http://rbruehl.macbay.de/Abscissa'
+
+  link 'Abscissa.app'
+end
