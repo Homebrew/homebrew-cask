2d66bbf5e6a3b75732fd653ce5c38048b5f1ae07
ndr <ndr@qef.io>
2014-09-03 22:49:55 +0200

    Update node-webkit to 0.10.3

--- Casks/node-webkit.rb
+++ Casks/node-webkit.rb
@@ -1,6 +1,6 @@
 class NodeWebkit < Cask
-  version '0.10.2'
-  sha256 '3bd4d90d21ca58f38a9de74e712f2f98afc1c75e6d05895d748492db45c40adc'
+  version '0.10.3'
+  sha256 'f5c6776406c85249ec58cd0c71bd2840a1e0bfd4d0c1764a767e4386ff0e4c70'
 
   url "http://dl.node-webkit.org/v#{version}/node-webkit-v#{version}-osx-x64.zip"
   homepage 'https://github.com/rogerwang/node-webkit'
