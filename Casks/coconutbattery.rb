49c41a682bccc6ede0d0c85a218299864efa6a56
Syi <syi@email.wm.edu>
2014-09-04 15:00:51 -0400

    Upgrade coconutBattery.app to v3.1.1

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -8,10 +8,10 @@ class Coconutbattery < Cask
     version '2.8'
     sha256 'fcfc81214ff26afff9f5c6c7cdc455b23ac898b6918f864b641a9e31526692d4'
   else
-    url 'http://www.coconut-flavour.com/downloads/coconutBattery_3_1.zip'
+    url 'http://www.coconut-flavour.com/downloads/coconutBattery_3_1_1.zip'
     appcast 'http://updates.coconut-flavour.com/coconutBatteryIntel.xml'
-    version '3.1'
-    sha256 'fee4c3e0ccc9c6ea1acc0dc7b3191ff6bf727df03cb9cb269a9347cd0e931618'
+    version '3.1.1'
+    sha256 'd95951e14b77a3deff5503fd8ff7098d1a38fabe5c38764f4c9f5bd20d9bea16'
   end
   homepage 'http://www.coconut-flavour.com/coconutbattery/'
   link 'coconutBattery.app'
