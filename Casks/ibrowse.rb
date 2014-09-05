4c8222efac862a10a4b9ef712485ae2371f7f911
Vítor Galvão <info@vitorgalvao.com>
2014-09-05 20:22:29 +0100

    Added iBrowse (1.0.0.5)
    Closes #6024.

---
+++ Casks/ibrowse.rb
@@ -0,0 +1,9 @@
+class Ibrowse < Cask
+  version '1.0.0.5'
+  sha256 'c3cae297d676e64c3d21638c21583f7c30a8fdbe6a4e127adb0857bafcd8037d'
+
+  url "http://cdn.macroplant.com/release/iBrowse-#{version}x.dmg"
+  homepage 'http://www.ibrowseapp.com/'
+
+  link 'iBrowse.app'
+end
