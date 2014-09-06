086a6507eb0ef4fcdf8404eaaece1cbc4b8074bd
Keng Kiat Lim <kkin@crnrc.com>
2014-09-06 15:12:40 +0800

    Upgrade Carbon Copy Cloner to 3.5.7

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -1,8 +1,8 @@
 class CarbonCopyCloner < Cask
-  version '3.5.6'
-  sha256 '2ded0bb0d8f1d050bdb4a84fd7b88f4af849d4a1806756994fb4cf2c79687c0c'
+  version '3.5.7'
+  sha256 '0b689c610b377f839bcf27239c5ff17824586e44808d5f6317084b1bd5220ddc'
 
-  url 'http://bombich.com/software/files/ccc-3.5.6.zip'
+  url "http://bombich.com/software/files/ccc-#{version}.zip"
   appcast 'http://www.bombich.com/software/updates/ccc.php'
   homepage 'http://bombich.com/'
 
