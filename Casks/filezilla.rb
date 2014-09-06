57b9250fd5105c63cb5c55c33724258e44f43dd6
Keng Kiat Lim <kkin@crnrc.com>
2014-09-07 02:24:10 +0800

    Upgrade FileZilla.app to 3.9.0.5

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,6 +1,6 @@
 class Filezilla < Cask
-  version '3.9.0.3'
-  sha256 'e7b642a6ebc5d785651c0c84477dd55e7e419d2094798205b814171c64fc7095'
+  version '3.9.0.5'
+  sha256 '805e7a83faa13235d15eeab572eab06d6033901a196ba3ebcf753793f81219cd'
 
   url "https://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   homepage 'https://filezilla-project.org/'
