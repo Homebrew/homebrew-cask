52e274c1445fa8b7ae34cfca3c73fcd5de52de99
Akagi201 <akagi201@gmail.com>
2014-09-07 23:37:53 +0800

    add: add happygrep to Homebrew Cask

---
+++ Casks/happygrep.rb
@@ -0,0 +1,9 @@
+class Happygrep < Cask
+  version '1.0'
+  sha256 'd8efc5bb8c17e70fb87403c194f9c1cb74ae6a0927be0fdbd381d12d55eba067'
+
+  url 'https://github.com/Akagi201/happygrep/releases/download/v1.0/happygrep.zip'
+  homepage 'https://github.com/Akagi201/happygrep'
+
+  binary 'happygrep'
+end
