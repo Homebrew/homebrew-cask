8b306a3d84b5de80ddd41a1df5217e75053850a8
John Britton <public@johndbritton.com>
2014-09-07 00:15:40 -0700

    Add Semulov.app v2.0 (latest)

---
+++ Casks/semulov.rb
@@ -0,0 +1,9 @@
+class Semulov < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://www.kainjow.com/downloads/Semulov.zip'
+  homepage 'http://www.kainjow.com'
+
+  link 'Semulov.app'
+end
