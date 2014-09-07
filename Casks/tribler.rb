578258ac396141f34ce34455feec52951bad809b
Vítor Galvão <info@vitorgalvao.com>
2014-09-07 18:44:42 +0100

    updated tribler (6.3.2)

--- Casks/tribler.rb
+++ Casks/tribler.rb
@@ -1,6 +1,6 @@
 class Tribler < Cask
-  version '6.3.1'
-  sha256 '5ec0ca3bb71105d182117ad9f7afd41a0cfc4844ac88ac7c1d5d6c4010d7d96b'
+  version '6.3.2'
+  sha256 'd82c29349f70f7bb260012299f8d60a5da0f5a474c75e2bb74c37133f45878b2'
 
   url "https://github.com/Tribler/tribler/releases/download/v#{version}/Tribler-#{version}.dmg"
   homepage 'http://www.tribler.org'
