bd0b68bdf245173f7f176230796c05600c7fd1e7
Beng Hee Eu <beng@benghee.eu>
2014-09-07 00:20:41 -0400

    LaTeXiT 2.7.0

--- Casks/latexit.rb
+++ Casks/latexit.rb
@@ -1,8 +1,8 @@
 class Latexit < Cask
-  version '2.6.1'
-  sha256 '1d1f764d75627f5112ec771d74994ed50e7fb27d996e23f3e5157131eeb9cbb2'
+  version '2.7.0'
+  sha256 '3eebccd00cfc51033352f8742c446b34dc58d96e0bb345fce41d7cf85a31dfde'
 
-  url 'http://www.chachatelier.fr/latexit/downloads/LaTeXiT-2_6_1.dmg'
+  url 'http://www.chachatelier.fr/latexit/downloads/LaTeXiT-2_7_0.dmg'
   appcast 'http://pierre.chachatelier.fr/latexit/downloads/latexit-sparkle-en.rss'
   homepage 'http://www.chachatelier.fr/latexit'
 
