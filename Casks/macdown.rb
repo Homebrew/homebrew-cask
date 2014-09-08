cd278cce9765c92f75974bb04dba5ea273145c69
Tzu-ping Chung <uranusjr@gmail.com>
2014-09-08 16:22:22 +0800

    Fix SHA 256 hash for MacDown 0.2.4

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -1,6 +1,6 @@
 class Macdown < Cask
   version '0.2.4'
-  sha256 '8720bb9644b391a9a6fa06af50a57f7fff495c2da80573fc0077445d9ca67367'
+  sha256 'd8bdec9c696a70ca88a885fad0d5f1804501921d48ea875ac08e3f5b0a9fe859'
 
   url "http://macdown.uranusjr.com/download/v#{version}/"
   appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml'
