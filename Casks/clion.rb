35eb2702647e8df3d3172665b014fafca801e14b
Dmitry Wolf <dmitry.s.wolf@gmail.com>
2014-09-08 22:42:47 +0400

    Add JetBrains CLion 138.1965.18

---
+++ Casks/clion.rb
@@ -0,0 +1,10 @@
+class Clion < Cask
+  version '138.1965.18'
+  sha256 'c5c57019141de3369aa62a44d94ee5a410956a9fa88e88d91dd898863da3042f'
+
+  url 'http://download.jetbrains.com/cpp/CLion-138.1965.18.dmg'
+  homepage 'http://www.jetbrains.com/clion/'
+
+  link 'CLion EAP.app'
+end
+
