523e1e439b942f3157bba7f67060a7b8e847b393
Marc Abramowitz <marc@marc-abramowitz.com>
2014-09-08 07:02:52 -0700

    Add MenuCalendarClock 4.5.2

---
+++ Casks/menucalendarclock-ical.rb
@@ -0,0 +1,10 @@
+class MenucalendarclockIcal < Cask
+  version '4.5.2'
+  sha256 '7c78428fd22d0a3fa02e5df4c92fbb45dad881a7ba09205dc47c4bfaaf7f7f9e'
+
+  url 'http://www.objectpark.net/download/MenuCalendarClock-4.5.2.dmg.gz'
+  nested_container 'menucalendarclock-ical-4.5.2'
+  homepage 'http://www.objectpark.net/en/mcc.html'
+
+  link 'MenuCalendarClock iCal.app'
+end
