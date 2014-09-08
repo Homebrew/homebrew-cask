5d998bc7aed79b6b6f70ec9fe49154be20efe738
Andrei Neculau <andrei.neculau@gmail.com>
2014-09-08 19:41:07 +0200

    Update bankid cask to use latest

--- Casks/bankid.rb
+++ Casks/bankid.rb
@@ -1,10 +1,15 @@
 class Bankid < Cask
-  version '4.19.4'
-  sha256 'b646b0d37ab7e5fa606ece299c6b8fba861dbd5a5cf99fd5fd39eeaba5ff807c'
+  version 'latest'
+  sha256 :no_check
 
-  url 'https://install.bankid.com/Repository/BankID_installation_4.19.4.pkg'
+  url 'https://install.bankid.com/FileDownloader?fileId=Mac'
   homepage 'http://www.bankid.com/'
 
-  install 'BankID_installation_4.19.4.pkg'
+  container_type :naked
+  before_install do
+    system '/bin/mv', '--', destination_path.join('FileDownloader'), destination_path.join('bankid-latest.pkg')
+  end
+
+  install 'bankid-latest.pkg'
   uninstall :pkgutil => 'com.bankid.bankid.BankID.pkg'
 end
