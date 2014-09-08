2547c2c780b83e2ca3b56c22ba6209f52075299e
ndr <ndr@qef.io>
2014-09-08 05:09:45 +0200

    Add Isabelle, version 2014

---
+++ Casks/isabelle.rb
@@ -0,0 +1,9 @@
+class Isabelle < Cask
+  version '2014'
+  sha256 'e4a4b5f454d6cb24d5ef7271adc54e9fa7109addda8c6707bd7414b4d4061a23'
+
+  url "http://www.cl.cam.ac.uk/research/hvg/Isabelle/dist/Isabelle#{version}.dmg"
+  homepage 'http://www.cl.cam.ac.uk/research/hvg/Isabelle/'
+
+  link "Isabelle#{version}.app"
+end
