a8c2569edc24841aab903871be186e357dfcf9dc
Hiroaki Nakamura <hnakamur@gmail.com>
2014-09-09 11:42:10 +0900

    Use the skype installer with version for old MacOS version

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,8 +1,13 @@
 class Skype < Cask
-  version 'latest'
-  sha256 :no_check
-
-  url 'https://www.skype.com/go/getskype-macosx.dmg'
+  if MacOS.version < :mavericks
+    version '6.15.0.335'
+    sha256 '592abdd157df12d718576a86c8f8e62fced55292fd7e6909d53aa5eaaa9218f4'
+    url 'http://download.skype.com/macosx/Skype_6.15.0.335.dmg'
+  else
+    version 'latest'
+    sha256 :no_check
+    url 'https://www.skype.com/go/getskype-macosx.dmg'
+  end
   homepage 'http://www.skype.com'
 
   link 'Skype.app'
