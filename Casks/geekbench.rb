eb7ad9c490858c6c5e2bbb43e6188539c79f81ea
Keng Kiat Lim <kkin@crnrc.com>
2014-09-09 19:00:01 +0800

    Upgrade Geekbench.app to 3.2.0

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -1,8 +1,8 @@
 class Geekbench < Cask
-  version '3.1.6'
-  sha256 '72882e847061f12f07e3d5dec4bd1a9117599a684dac3a1d6d5558d7086b8c5c'
+  version '3.2.0'
+  sha256 'b355ad73a412e89567e5953498299ff69aad57609733903533fd01666aee57f8'
 
-  url 'https://d34wv75roto0rl.cloudfront.net/Geekbench-3.1.6-Mac.zip'
+  url "https://d34wv75roto0rl.cloudfront.net/Geekbench-#{version}-Mac.zip"
   appcast 'http://www.primatelabs.com/appcast/geekbench3.xml'
   homepage 'http://www.primatelabs.com/geekbench/'
 
