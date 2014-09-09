27970305521bade4242debdf7517846bf79971ce
John Britton <public@johndbritton.com>
2014-09-08 23:07:24 -0700

    Upgrade Gmail Notifr to 0.9.0

--- Casks/gmail-notifr.rb
+++ Casks/gmail-notifr.rb
@@ -1,8 +1,8 @@
 class GmailNotifr < Cask
-  version '0.8.0'
-  sha256 '423330044f92e28cadacb875ba332579552c78cb80931b5a8a8a5dfba3d374ea'
+  version '0.9.0'
+  sha256 '47ecf1edf08678e6a1c53cb5a3e39d5c6c5f63a9145fc7cbc7df2ebada42b662'
 
-  url 'https://s3.amazonaws.com/assets.ashchan.com/gmailnotifr/GmailNotifr-0.8.0.zip'
+  url 'https://s3.amazonaws.com/assets.ashchan.com/gmailnotifr/GmailNotifr-0.9.0.zip'
   appcast 'https://s3.amazonaws.com/assets.ashchan.com/gmailnotifr/update.xml'
   homepage 'http://ashchan.com/projects/gmail-notifr'
 
