ed9ee163d2336f9dbc00ee267ca8c803c4499b21
Scott Robertson <scottrobertson@users.noreply.github.com>
2014-09-09 14:26:12 +0100

    Update ynab.rb
    Closes #6098.

--- Casks/ynab.rb
+++ Casks/ynab.rb
@@ -1,8 +1,8 @@
 class Ynab < Cask
-  version '4.3.541'
-  sha256 '38ec8a050fde9be1375f3fe184988eb81358b3a26b74c308e67bb872ac5e2d50'
+  version '4.3.622'
+  sha256 '262f4bcf0285e2f52fb7a61f6fde0a5f4f23fb8ed1f80d9b9a3c6e6c7da31e5e'
 
-  url 'https://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_4.3.541.dmg'
+  url "https://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_#{version}.dmg"
   homepage 'http://www.youneedabudget.com/'
 
   link 'YNAB 4.app'
