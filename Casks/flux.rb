b3e772938c09d0639ce8a68c9c9cfadaf25a7c14
Roland Walker <walker@pobox.com>
2014-09-09 08:55:26 -0400

    pre/postflight stanza in flux

--- Casks/flux.rb
+++ Casks/flux.rb
@@ -8,7 +8,7 @@ class Flux < Cask
 
   link 'Flux.app'
 
-  after_install do
+  postflight do
     # Don't ask to move the app bundle to /Applications
     system '/usr/bin/defaults', 'write', 'org.herf.Flux', 'moveToApplicationsFolderAlertSuppress', '-bool', 'true'
   end
