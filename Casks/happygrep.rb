6f4ef2101df1a330a76c6550ed9d28a21a9f0d5e
Vítor Galvão <info@vitorgalvao.com>
2014-09-09 12:15:07 +0100

    corrected quotes in happygrep url

--- Casks/happygrep.rb
+++ Casks/happygrep.rb
@@ -2,7 +2,7 @@ class Happygrep < Cask
   version '1.0'
   sha256 '572c3eaa6b0609c05b85c6934155b8e8943d28464438d9ec2838efbf6e29e863'
 
-  url 'https://github.com/happypeter/happygrep/releases/download/v#{version}/happygrep.zip'
+  url "https://github.com/happypeter/happygrep/releases/download/v#{version}/happygrep.zip"
   homepage 'https://github.com/happypeter/happygrep'
 
   binary 'happygrep'
