e48ae5798f5dd932c4c540612b35d602d2349821
Roland Walker <walker@pobox.com>
2014-09-09 08:55:26 -0400

    pre/postflight stanza in cheatsheet

--- Casks/cheatsheet.rb
+++ Casks/cheatsheet.rb
@@ -8,7 +8,7 @@ class Cheatsheet < Cask
 
   link 'CheatSheet.app'
 
-  after_install do
+  postflight do
     # Don't ask to move the app bundle to /Applications
     system '/usr/bin/defaults', 'write', 'com.mediaatelier.CheatSheet', 'moveToApplicationsFolderAlertSuppress', '-bool', 'true'
   end
