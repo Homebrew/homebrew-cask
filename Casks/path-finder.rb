e547c6addd4e8228dd2150c1988876d6260d6bae
Roland Walker <walker@pobox.com>
2014-09-09 08:55:27 -0400

    pre/postflight stanza in path-finder

--- Casks/path-finder.rb
+++ Casks/path-finder.rb
@@ -7,7 +7,7 @@ class PathFinder < Cask
 
   link 'Path Finder.app'
 
-  after_install do
+  postflight do
     # Don't ask to move the app bundle to /Applications
     system '/usr/bin/defaults', 'write', 'com.cocoatech.PathFinder', 'kNTMoveToApplicationsFolderAlertSuppress', '-bool', 'true'
   end
