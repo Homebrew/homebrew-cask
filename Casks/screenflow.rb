b0c894eb12ae6470c94b3c16e30c99ea4aae9d69
Ted Wise <cwise@hdlabinc.com>
2014-09-10 15:53:51 -0400

    Bump version of screenflow.

--- Casks/screenflow.rb
+++ Casks/screenflow.rb
@@ -1,8 +1,8 @@
 class Screenflow < Cask
-  version '4.5'
-  sha256 '9ec0f4c1bb1b778ea37f3e37bf0ad494baffe08e567553027c13178f05c2fbcd'
+  version '4.5.2'
+  sha256 '1bcf27fad19dae65e11a792da1d5cb83b128899a92cab7485a9149ae9105c149'
 
-  url 'http://www.telestream.net/download-files/screenflow/4-5/ScreenFlow-4.5.dmg'
+  url "http://www.telestream.net/download-files/screenflow/4-5/ScreenFlow-#{version}.dmg"
   homepage 'http://www.telestream.net/screenflow/'
 
   link 'ScreenFlow.app'
