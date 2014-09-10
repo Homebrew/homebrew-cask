3cd9730d5061e3ff679562386baf22033a5e27d2
Roland Walker <walker@pobox.com>
2014-09-10 11:37:04 -0400

    minilyrics.rb

--- Casks/minilyrics.rb
+++ Casks/minilyrics.rb
@@ -5,6 +5,6 @@ class Minilyrics < Cask
   url 'http://www.crintsoft.com/download/MiniLyrics.dmg'
   homepage 'http://www.crintsoft.com/'
 
-  install 'MiniLyrics.pkg'
+  pkg 'MiniLyrics.pkg'
   uninstall :pkgutil => 'com.crintsoft.MiniLyrics.pkg'
 end
