4b9d103875ea50ad5011d032d2ff00ce91ec1231
Roland Walker <walker@pobox.com>
2014-09-10 11:37:03 -0400

    microsoft-intellitype.rb

--- Casks/microsoft-intellitype.rb
+++ Casks/microsoft-intellitype.rb
@@ -5,7 +5,7 @@ class MicrosoftIntellitype < Cask
   url 'http://download.microsoft.com/download/B/1/0/B109F931-70E2-425F-8681-EAAB75845AB8/Microsoft-Desktop_d305.dmg'
   homepage 'http://www.microsoft.com/hardware/en-us/keyboards'
 
-  install 'Microsoft Desktop Installer.app/Contents/Resources/Microsoft Desktop.mpkg/Contents/Packages/Microsoft Keyboard.pkg'
+  pkg 'Microsoft Desktop Installer.app/Contents/Resources/Microsoft Desktop.mpkg/Contents/Packages/Microsoft Keyboard.pkg'
 
   uninstall :pkgutil => 'com.microsoft.keyboard.installer',
             :kext => [
