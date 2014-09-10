973d732e6e6b6983704a7ac74bedf7ea37dc45c9
Roland Walker <walker@pobox.com>
2014-09-10 11:37:02 -0400

    idris.rb

--- Casks/idris.rb
+++ Casks/idris.rb
@@ -5,6 +5,6 @@ class Idris < Cask
   url 'http://www.idris-lang.org/pkgs/idris-current.pkg'
   homepage 'http://www.idris-lang.org'
 
-  install 'idris-current.pkg'
+  pkg 'idris-current.pkg'
   uninstall :pkgutil => 'org.idris-lang'
 end
