a29c070927d5568fcb81ba5e97099fdf7880d7bb
Roland Walker <walker@pobox.com>
2014-09-10 11:37:01 -0400

    google-japanese-ime.rb

--- Casks/google-japanese-ime.rb
+++ Casks/google-japanese-ime.rb
@@ -5,7 +5,7 @@ class GoogleJapaneseIme < Cask
   url 'https://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg'
   homepage 'https://www.google.co.jp/ime/'
 
-  install 'GoogleJapaneseInput.pkg'
+  pkg 'GoogleJapaneseInput.pkg'
   uninstall :pkgutil => 'com.google.pkg.GoogleJapaneseInput',
             :launchctl => [
                            'com.google.inputmethod.Japanese.Converter',
