d606f6afccaa63e0bc85427bd8be94cc0d85cacd
Roland Walker <walker@pobox.com>
2014-09-10 11:36:59 -0400

    baiduinput.rb

--- Casks/baiduinput.rb
+++ Casks/baiduinput.rb
@@ -6,7 +6,7 @@ class Baiduinput < Cask
   url "http://wuxian.baidu.com/download/1000e/baiduinput_mac_v#{version}.dmg"
   homepage 'http://wuxian.baidu.com/input/mac.html'
 
-  install '安装百度输入法.pkg'
+  pkg '安装百度输入法.pkg'
   uninstall :pkgutil  => 'com.baidu.inputmethod.*',
             :files    => [
                           '/Library/Input Methods/BaiduIM.app'
