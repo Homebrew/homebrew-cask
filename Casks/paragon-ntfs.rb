9abef78c0ecd1642d8bb8da3b44331a24fac331d
Roland Walker <walker@pobox.com>
2014-09-10 11:37:05 -0400

    paragon-ntfs.rb

--- Casks/paragon-ntfs.rb
+++ Casks/paragon-ntfs.rb
@@ -5,5 +5,5 @@ class ParagonNtfs < Cask
   url 'http://dl.paragon-software.com/demo/ntfsmac_trial_u.dmg'
   homepage 'http://www.paragon-software.com/home/ntfs-mac/'
 
-  install 'FSInstaller.app/Contents/Resources/Paragon NTFS for Mac OS X.pkg'
+  pkg 'FSInstaller.app/Contents/Resources/Paragon NTFS for Mac OS X.pkg'
 end
