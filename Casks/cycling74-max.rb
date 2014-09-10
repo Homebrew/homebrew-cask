370391b78fe6a618133e20eede07f889617ed548
Roland Walker <walker@pobox.com>
2014-09-10 11:37:00 -0400

    cycling74-max.rb

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -5,7 +5,7 @@ class Cycling74Max < Cask
   url 'https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max617_140421.dmg'
   homepage 'http://cycling74.com'
 
-  install 'Max 6.1 Installer.pkg'
+  pkg 'Max 6.1 Installer.pkg'
   uninstall :pkgutil => 'com.cycling74.Max*',
             :files   => '/Applications/Max 6.1'
 end
