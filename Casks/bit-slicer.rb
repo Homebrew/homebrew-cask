342e34a6eb33271e0c8ed0f25ed6dbaeb6555c49
Mayur Pawashe <zorgiepoo@gmail.com>
2014-09-10 01:19:58 -0400

    Update Bit-Slicer to 1.7.3

--- Casks/bit-slicer.rb
+++ Casks/bit-slicer.rb
@@ -1,8 +1,8 @@
 class BitSlicer < Cask
-  version '1.7.2'
-  sha256 'd2860221b46ab5a3b27936ac71cba55aa9796a2bb336b4e130e2aceb5bac48d3'
+  version '1.7.3'
+  sha256 'd1a2fa33852ec95f52070e6b4d9c16f4a8741b41d384f8d1aab7c501d5a78b24'
 
-  url 'https://bitbucket.org/zorgiepoo/bit-slicer/downloads/Bit%20Slicer%201.7.2.zip'
+  url 'https://bitbucket.org/zorgiepoo/bit-slicer/downloads/Bit%20Slicer%201.7.3.zip'
   appcast 'http://zorg.tejat.net/bitslicer/update.php'
   homepage 'https://github.com/zorgiepoo/bit-slicer/'
 
