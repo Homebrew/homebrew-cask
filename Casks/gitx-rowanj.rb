8aed8928713e564adc7c595bd4bfb4a4711d4e20
Trey Harris <treyharris@gmail.com>
2014-09-10 15:31:12 -0400

    gitx-rowanj: Version and checksum
    According to #1021, versioned/checksummed downloads should be preferred
    when available and the URL contains the version number. This is the case
    with gitx-rowanj—in fact, the appcast referenced links to the versioned
    downloads, not the unversioned one.

--- Casks/gitx-rowanj.rb
+++ Casks/gitx-rowanj.rb
@@ -1,8 +1,8 @@
 class GitxRowanj < Cask
-  version 'latest'
-  sha256 :no_check
+  version '0.15.1949'
+  sha256 '17301ee1209bd4b12c126d7a58405efdb366f99f70b4f923a9c96e16c334ce2a'
 
-  url 'http://builds.phere.net/GitX/development/GitX-dev.dmg'
+  url 'http://builds.phere.net/GitX/development/GitX-dev-1949.dmg'
   appcast 'https://s3.amazonaws.com/builds.phere.net/GitX/development/GitX-dev.xml'
   homepage 'http://rowanj.github.io/gitx/'
 
