b56885a0795fe4c7c4a41e0bffa6c1f3dfb5203b
Roland Walker <walker@pobox.com>
2014-09-10 11:36:59 -0400

    chrome-remote-desktop-host.rb

--- Casks/chrome-remote-desktop-host.rb
+++ Casks/chrome-remote-desktop-host.rb
@@ -5,7 +5,7 @@ class ChromeRemoteDesktopHost < Cask
   url 'https://dl.google.com/chrome-remote-desktop/chromeremotedesktop.dmg'
   homepage 'https://chrome.google.com/remotedesktop'
 
-  install 'Chrome Remote Desktop Host.pkg'
+  pkg 'Chrome Remote Desktop Host.pkg'
   uninstall :script => {
     :executable => '/Applications/Chrome Remote Desktop Host Uninstaller.app/Contents/MacOS/remoting_host_uninstaller',
     :args => %w[--no-ui]
