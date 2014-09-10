0f471876bb268605f64deabcf92c13eb02553eb3
Roland Walker <walker@pobox.com>
2014-09-10 11:37:07 -0400

    teamviewer.rb

--- Casks/teamviewer.rb
+++ Casks/teamviewer.rb
@@ -5,7 +5,7 @@ class Teamviewer < Cask
   url 'https://download.teamviewer.com/download/TeamViewer.dmg'
   homepage 'http://www.teamviewer.com/'
 
-  install 'Install TeamViewer.pkg'
+  pkg 'Install TeamViewer.pkg'
   uninstall :pkgutil   => 'com.teamviewer.*',
             :launchctl => 'com.teamviewer.service',
             :files     => [
