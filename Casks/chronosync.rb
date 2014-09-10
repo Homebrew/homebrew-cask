c936455b57b3df6dcc76c7ae6ea1d0420529591c
Roland Walker <walker@pobox.com>
2014-09-10 11:36:59 -0400

    chronosync.rb

--- Casks/chronosync.rb
+++ Casks/chronosync.rb
@@ -5,6 +5,6 @@ class Chronosync < Cask
   url 'http://downloads.econtechnologies.com/CS4_Download.dmg'
   homepage 'http://www.econtechnologies.com'
 
-  install 'Install.pkg'
+  pkg 'Install.pkg'
   uninstall :pkgutil => 'com.econtechnologies.pkg.ChronoSyncApplication'
 end
