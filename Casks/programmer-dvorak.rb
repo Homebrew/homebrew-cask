955310c10ae2fb46d4e99e0937f0213c946ea269
Roland Walker <walker@pobox.com>
2014-09-10 11:37:05 -0400

    programmer-dvorak.rb

--- Casks/programmer-dvorak.rb
+++ Casks/programmer-dvorak.rb
@@ -5,7 +5,7 @@ class ProgrammerDvorak < Cask
   url 'http://www.kaufmann.no/downloads/macos/ProgrammerDvorak-1_2b9.pkg.zip'
   homepage 'http://kaufmann.no/roland/dvorak/'
 
-  install 'Programmer Dvorak v1.2.pkg'
+  pkg 'Programmer Dvorak v1.2.pkg'
   uninstall :pkgutil => 'com.apple.keyboardlayout.Programmer Dvorak',
             :files => [
                         '/Library/Keyboard Layouts/Programmer Dvorak.bundle/',
