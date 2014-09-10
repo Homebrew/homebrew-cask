c56833f1c1337654e193adb1986f9d734d859153
Roland Walker <walker@pobox.com>
2014-09-10 11:37:06 -0400

    remote-desktop-connection.rb

--- Casks/remote-desktop-connection.rb
+++ Casks/remote-desktop-connection.rb
@@ -5,6 +5,6 @@ class RemoteDesktopConnection < Cask
   url 'http://download.microsoft.com/download/C/F/0/CF0AE39A-3307-4D39-9D50-58E699C91B2F/RDC_2.1.1_ALL.dmg'
   homepage 'http://www.microsoft.com/en-us/download/details.aspx?id=18140'
 
-  install 'RDC Installer.mpkg'
+  pkg 'RDC Installer.mpkg'
   uninstall :pkgutil => 'com.microsoft.rdc.all.*'
 end
