6ad409121d314d7d3744e7227b97d80b9da4b530
Roland Walker <walker@pobox.com>
2014-09-10 11:37:07 -0400

    unity-web-player.rb

--- Casks/unity-web-player.rb
+++ Casks/unity-web-player.rb
@@ -5,7 +5,7 @@ class UnityWebPlayer < Cask
   url 'http://webplayer.unity3d.com/download_webplayer-3.x/webplayer-mini.dmg'
   homepage 'https://unity3d.com/webplayer'
 
-  install 'Install Unity Web Player.pkg'
+  pkg 'Install Unity Web Player.pkg'
   uninstall :pkgutil => 'com.unity.UnityWebPlayer',
             :files   => '/Library/Internet Plug-Ins/Unity Web Player.plugin'
 end
