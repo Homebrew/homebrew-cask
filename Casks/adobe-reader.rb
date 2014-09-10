295c687404342903c39edfe8ab020288e106108e
Roland Walker <walker@pobox.com>
2014-09-10 11:36:58 -0400

    adobe-reader.rb

--- Casks/adobe-reader.rb
+++ Casks/adobe-reader.rb
@@ -5,6 +5,6 @@ class AdobeReader < Cask
   url 'http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.07/en_US/AdbeRdr11007_en_US.dmg'
   homepage 'http://www.adobe.com/products/reader.html'
 
-  install 'Adobe Reader XI Installer.pkg'
+  pkg 'Adobe Reader XI Installer.pkg'
   uninstall :pkgutil => 'com.adobe.acrobat.reader.11007.*'
 end
