4ec5704eb7d7aee1cf96a92333db31b94cca1e98
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-09-10 19:40:51 +0300

    FreeFileSync updated to 6.9

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,8 +1,8 @@
 class Freefilesync < Cask
-  version '6.8'
-  sha256 'e58877de19545e069bf44a0ba2a5c62081fc7d47861cfa997847a762180b9ad0'
+  version '6.9'
+  sha256 '5f14625c37381ea33c068df458835344f24f5c0ea9cb071535236740be9c855f'
 
-  url 'http://www.fosshub.com/download/FreeFileSync_6.8_Mac_OS_X_64-bit.zip', :referer => 'http://www.fosshub.com/FreeFileSync.html'
+  url "http://www.fosshub.com/download/FreeFileSync_#{version}_Mac_OS_X_64-bit.zip", :referer => 'http://www.fosshub.com/FreeFileSync.html'
   homepage 'http://freefilesync.sourceforge.net/'
 
   link 'FreeFileSync.app'
