d2a6af3ba64637d63eabf8b0d3e8f252c8d04761
Roland Walker <walker@pobox.com>
2014-09-10 11:36:59 -0400

    ax88179.rb

--- Casks/ax88179.rb
+++ Casks/ax88179.rb
@@ -8,6 +8,6 @@ class Ax88179 < Cask
   sha256 '26b3dac14839d93db175c5a5d11083578f3171333f8f7d6574cde389cf58a991'
 
   nested_container "#{basename}/AX88179_178A.dmg"
-  install "AX88179_178A_v#{version[0..-10]}.pkg"
+  pkg "AX88179_178A_v#{version[0..-10]}.pkg"
   uninstall :script => { :executable => 'AX88179_178A_Uninstall_v140' }
 end
