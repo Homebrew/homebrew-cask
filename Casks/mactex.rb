40f81f565e42c0f3a29033ed3ce7345586f51e06
Roland Walker <walker@pobox.com>
2014-09-10 11:37:03 -0400

    mactex.rb

--- Casks/mactex.rb
+++ Casks/mactex.rb
@@ -5,7 +5,7 @@ class Mactex < Cask
   url 'http://mirror.ctan.org/systems/mac/mactex/mactex-20140525.pkg'
   homepage 'http://www.tug.org/mactex/'
 
-  install 'mactex-20140525.pkg'
+  pkg 'mactex-20140525.pkg'
   uninstall :pkgutil => [
                          'org.tug.mactex.ghostscript9.10',
                          'org.tug.mactex.gui2014',
