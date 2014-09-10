76c0277d1924edcd800cd8b5e1cc8906df57354d
Roland Walker <walker@pobox.com>
2014-09-10 11:37:06 -0400

    rust.rb

--- Casks/rust.rb
+++ Casks/rust.rb
@@ -5,6 +5,6 @@ class Rust < Cask
    url "http://static.rust-lang.org/dist/rust-nightly-x86_64-apple-darwin.pkg"
    homepage 'http://www.rust-lang.org/'
    
-   install 'rust-nightly-x86_64-apple-darwin.pkg'
+   pkg 'rust-nightly-x86_64-apple-darwin.pkg'
    uninstall :pkgutil => 'org.rust-lang.rust'
 end
