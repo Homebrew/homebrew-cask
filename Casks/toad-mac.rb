c5e4863bc888c3fc325749aa9a70738f746d359d
Roland Walker <walker@pobox.com>
2014-09-10 11:37:07 -0400

    toad-mac.rb

--- Casks/toad-mac.rb
+++ Casks/toad-mac.rb
@@ -5,6 +5,6 @@ class ToadMac < Cask
   url 'http://community-downloads.quest.com/toadsoft/toadmacedition/ToadMacEdition_108.pkg'
   homepage 'http://www.toadworld.com/products/toad-mac-edition/default.aspx'
 
-  install 'ToadMacEdition_108.pkg'
+  pkg 'ToadMacEdition_108.pkg'
   uninstall :pkgutil => 'com.dell.Toad', :files => '/Applications/Toad.app'
 end
