c0ae5b3113d6f45a7a4b9738cb0d0f854733d2f2
Ted Wise <cwise@hdlabinc.com>
2014-09-10 16:05:54 -0400

    Bump version of textexpander.

--- Casks/textexpander.rb
+++ Casks/textexpander.rb
@@ -1,8 +1,8 @@
 class Textexpander < Cask
   version '4.3.3'
-  sha256 '95883aad6a735664ba45edfd5fe6c2adb794d9296d7a8d19d34ff925ac9779bb'
+  sha256 '1aa42fa3b1bcb6e41f662c040f3efde5713ddfe8f1aee9d03c8f60bcd9d65ddf'
 
-  url 'http://cdn.smilesoftware.com/TextExpander_4.3.2.zip'
+  url "http://cdn.smilesoftware.com/TextExpander_#{version}.zip"
   appcast 'http://www.smilesoftware.com/appcast/update.php'
   homepage 'http://www.smilesoftware.com/TextExpander/index.html'
 
