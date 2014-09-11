99aa00283ed8a8fdc529c190b44c078be217cde7
ndr <ndr@qef.io>
2014-09-11 20:16:21 +0200

    Update Brackets to 0.43

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,6 +1,6 @@
 class Brackets < Cask
-  version '0.42'
-  sha256 'd4c1d5f473b22311fc47dcd2546262ea46e468441d93dacaaaacaaf1ecdaa788'
+  version '0.43'
+  sha256 '582b6a1425b9b4574b7ad5b5e797899e6e8bacc47560f71203549fe5a1863aa9'
 
   url "https://github.com/adobe/brackets/releases/download/release-#{version}/Brackets.Release.#{version}.dmg"
   homepage 'http://brackets.io'
