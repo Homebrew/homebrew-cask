e9e0eca8606aafe1a0907c51cd75040549ee7b06
Keng Kiat Lim <kkin@crnrc.com>
2014-09-12 10:12:52 -0400

    Upgrade SmartGitHg to 6.0.6
    Closes #6154.

--- Casks/smartgithg.rb
+++ Casks/smartgithg.rb
@@ -1,8 +1,8 @@
 class Smartgithg < Cask
-  version '6.0.5'
-  sha256 '4fc8d5db3b373a613cf6be348f001adc48249ac4b37ec03c64b61ce500e0637c'
+  version '6.0.6'
+  sha256 '25616f860cb03a9be9aa9ec21d2625673253c4dc702647d3c1f417f6b425fc96'
 
-  url 'http://www.syntevo.com/download/smartgithg/smartgithg-macosx-6_0_5.dmg'
+  url "http://www.syntevo.com/download/smartgithg/smartgithg-macosx-#{version.gsub('.', '_')}.dmg"
   homepage 'http://www.syntevo.com'
 
   link 'SmartGitHg 6.app'
