06c7b769bff61c7755ba9a91ae528a9e59f3d85c
Chai Feng <chaifeng@chaifeng.com>
2014-09-13 23:03:14 +0800

    Upgrade Mono MDK to v3.8.0

--- Casks/mono-mdk.rb
+++ Casks/mono-mdk.rb
@@ -1,6 +1,6 @@
 class MonoMdk < Cask
-  version '3.6.0'
-  sha256 '04a4543b00510f5df7d52eac3cd8583e44a30fdf12af536646de9351f360647e'
+  version '3.8.0'
+  sha256 '88daab3fc775b771b5fb15ae0d14fad9a6f04b3e67733952a394ef84fb01ed72'
 
   url "http://download.mono-project.com/archive/#{version}/macos-10-x86/MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
   homepage 'http://mono-project.com/'
