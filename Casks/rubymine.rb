641071d721cfb6b72b2558b34b35242403092a52
Roland Walker <walker@pobox.com>
2014-09-13 13:19:44 -0400

    zap stanza from mackup for rubymine.rb

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -10,4 +10,12 @@ class Rubymine < Cask
   postflight do
     system "/usr/libexec/PlistBuddy", "-c", "Set :JVMOptions:JVMVersion 1.6+", "#{destination_path}/RubyMine.app/Contents/Info.plist"
   end
+  zap :files => [
+                 '~/Library/Application Support/RubyMine40',
+                 '~/Library/Preferences/RubyMine40',
+                 '~/Library/Application Support/RubyMine50',
+                 '~/Library/Preferences/RubyMine50',
+                 '~/Library/Application Support/RubyMine60',
+                 '~/Library/Preferences/RubyMine60',
+                ]
 end
