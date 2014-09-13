20333ca7eca8c64cea2f3c838aff1b1540364f59
Roland Walker <walker@pobox.com>
2014-09-13 13:19:42 -0400

    zap stanza from mackup for geektool.rb

--- Casks/geektool.rb
+++ Casks/geektool.rb
@@ -1,10 +1,17 @@
 class Geektool < Cask
-    version '3.1.1-311'
+  version '3.1.1-311'
   sha256 'ef1a7439d91f0de9e459a4677c6f95fe35bd7e02c300adc9ce315b5590cbbfc9'
 
   url 'http://download.tynsoe.org/GeekTool-3.1.1-311.zip'
   appcast 'http://dl.dropboxusercontent.com/u/1760713/appcast/appcast.xml'
   homepage 'http://projects.tynsoe.org/en/geektool/'
 
-    app 'GeekTool.app'
+  app 'GeekTool.app'
+  zap :files => [
+                 '~/Library/Preferences/org.tynsoe.GeekTool.plist',
+                 '~/Library/Preferences/org.tynsoe.geeklet.file.plist',
+                 '~/Library/Preferences/org.tynsoe.geeklet.image.plist',
+                 '~/Library/Preferences/org.tynsoe.geeklet.shell.plist',
+                 '~/Library/Preferences/org.tynsoe.geektool3.plist',
+                ]
 end
