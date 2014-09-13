8a468db5abcc0fc37b680a4b32e5d7d579c6af45
Roland Walker <walker@pobox.com>
2014-09-13 13:19:44 -0400

    zap stanza from mackup for omnifocus.rb

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -6,4 +6,9 @@ class Omnifocus < Cask
   homepage 'http://www.omnigroup.com/products/omnifocus/'
 
   app 'OmniFocus.app'
+  zap :files => [
+                 '~/Library/Application Support/OmniFocus/Plug-Ins',
+                 '~/Library/Application Support/OmniFocus/Themes',
+                 '~/Library/Preferences/com.omnigroup.OmniFocus.plist',
+                ]
 end
