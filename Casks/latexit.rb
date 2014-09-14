fbaed3f371f7f2762a669a9174acdab5d776da74
Vítor Galvão <info@vitorgalvao.com>
2014-09-14 01:43:07 +0100

    updated latexit to use gsub instead of tr, for consistency with other casks

--- Casks/latexit.rb
+++ Casks/latexit.rb
@@ -2,7 +2,7 @@ class Latexit < Cask
   version '2.7.2'
   sha256 'e58b0d74e6560fb9d28aeff7e355ab7b2bcd433b474ec3dda0fb3bd3b05b2f2c'
 
-  url "http://www.chachatelier.fr/latexit/downloads/LaTeXiT-#{version.tr('.', '_')}.dmg"
+  url "http://www.chachatelier.fr/latexit/downloads/LaTeXiT-#{version.gsub('.', '_')}.dmg"
   appcast 'http://pierre.chachatelier.fr/latexit/downloads/latexit-sparkle-en.rss'
   homepage 'http://www.chachatelier.fr/latexit'
 
