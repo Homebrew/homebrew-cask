8f764f6cdb4322083dc3607c54da815604ede82e
Vincent Klaiber <vincent@schimpanz.com>
2014-09-14 22:00:47 +0200

    Add Hunt X.app v0.1
    Product Hunt for Mac. The Startup List for awesome people!

---
+++ Casks/hunt-x.rb
@@ -0,0 +1,9 @@
+class HuntX < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://huntx.mobilefirst.in/Apps/Hunt%20X.zip'
+  homepage 'http://huntx.mobilefirst.in/'
+
+  link 'Hunt X.app'
+end
