7c6feb797b6a6544b93f8e95479b47df764673ce
ndr <ndr@qef.io>
2014-09-14 18:02:07 +0200

    Add gpg stanza to torbrowser

--- Casks/torbrowser.rb
+++ Casks/torbrowser.rb
@@ -3,6 +3,8 @@ class Torbrowser < Cask
   sha256 '45843982df1c339f3aa165bf09b96b27306749c38aaf669a13cc9c508b679141'
 
   url "https://www.torproject.org/dist/torbrowser/#{version}/TorBrowser-#{version}-osx32_en-US.dmg"
+  gpg "#{url}.asc",
+      :key_id => '416f061063fee659'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   license :unknown
 
