947fb510fe4a5fda7b68bc8e2bde5ba9822d9b5a
Chai Feng <chaifeng@chaifeng.com>
2014-09-14 12:36:51 +0800

    Upgrade Mono MRE to v3.8.0

--- Casks/mono-mre.rb
+++ Casks/mono-mre.rb
@@ -1,6 +1,6 @@
 class MonoMre < Cask
-  version '3.6.0'
-  sha256 '9ea30e00491cbc2feb6a57725d96c8fa42066254e03dec498b0a1683393827ba'
+  version '3.8.0'
+  sha256 '85615c5bd6af84b6e3da263ef4571dc226c03177bc319f9909b86711163073e6'
 
   url "http://download.mono-project.com/archive/#{version}/macos-10-x86/MonoFramework-MRE-#{version}.macos10.xamarin.x86.pkg"
   homepage 'http://mono-project.com'
