2b8ba8fefb4949e18b9ca0dfe8280aacd8248134
Roland Walker <walker@pobox.com>
2014-09-15 12:05:31 -0400

    app stanza in emacs.rb

--- Casks/emacs.rb
+++ Casks/emacs.rb
@@ -5,5 +5,5 @@ class Emacs < Cask
   url 'http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg'
   homepage 'http://emacsformacosx.com/'
 
-  link 'Emacs.app'
+  app 'Emacs.app'
 end
