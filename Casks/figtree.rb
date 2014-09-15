b7935aa5687e0646436815eaa59f82adfb117470
Roland Walker <walker@pobox.com>
2014-09-15 12:05:33 -0400

    app stanza in figtree.rb

--- Casks/figtree.rb
+++ Casks/figtree.rb
@@ -5,6 +5,6 @@ class Figtree < Cask
   url 'http://tree.bio.ed.ac.uk/download.php?id=86'
   homepage 'http://tree.bio.ed.ac.uk/software/figtree/'
 
-  link 'FigTree v1.4.0.app'
+  app 'FigTree v1.4.0.app'
   qlplugin 'QuickLook Plugin/FigTreeQuickLookPlugin.qlgenerator'
 end
