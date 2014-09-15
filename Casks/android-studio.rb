03b2807bbe0a0e1538a1c3174683ae2d0b498256
Roland Walker <walker@pobox.com>
2014-09-15 12:05:14 -0400

    app stanza in android-studio.rb

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -5,7 +5,7 @@ class AndroidStudio < Cask
   url 'http://dl.google.com/android/studio/install/0.8.6/android-studio-bundle-135.1339820-mac.dmg'
   homepage 'https://developer.android.com/sdk/installing/studio.html'
 
-  link 'Android Studio.app'
+  app 'Android Studio.app'
 
   postflight do
     system "/usr/libexec/PlistBuddy", "-c", "Set :JVMOptions:JVMVersion 1.6+", "#{destination_path}/Android Studio.app/Contents/Info.plist"
