cc38325d9dbcfafa84cb6e1f964e2bcb5974baa7
Roland Walker <walker@pobox.com>
2014-09-15 12:06:09 -0400

    app stanza in poison.rb

--- Casks/poison.rb
+++ Casks/poison.rb
@@ -5,7 +5,7 @@ class Poison < Cask
   url 'http://jenkins.tox.im/job/Poison_OS_X/lastSuccessfulBuild/artifact/arc/poison-with-extras.zip'
   homepage 'http://tox.im'
 
-  link 'poison-with-extras/Poison.app'
-  link 'poison-with-extras/Silica.app'
-  link 'poison-with-extras/Hibiki.app'
+  app 'poison-with-extras/Poison.app'
+  app 'poison-with-extras/Silica.app'
+  app 'poison-with-extras/Hibiki.app'
 end
