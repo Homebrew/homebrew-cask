24161eb0769c6a8f8a5a0416140acb198e3371ea
Roland Walker <walker@pobox.com>
2014-09-15 12:05:19 -0400

    app stanza in blu-ray-player.rb

--- Casks/blu-ray-player.rb
+++ Casks/blu-ray-player.rb
@@ -5,5 +5,5 @@ class BluRayPlayer < Cask
   url 'http://www.macblurayplayer.com/user/download/Mac_Bluray_Player.dmg'
   homepage 'http://www.macblurayplayer.com/'
 
-  link 'Blu-ray Player.app'
+  app 'Blu-ray Player.app'
 end
