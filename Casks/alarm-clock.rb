bcf99859838af2764b7725394e31c7451a009007
Roland Walker <walker@pobox.com>
2014-09-15 12:05:13 -0400

    app stanza in alarm-clock.rb

--- Casks/alarm-clock.rb
+++ Casks/alarm-clock.rb
@@ -5,5 +5,5 @@ class AlarmClock < Cask
   url 'http://wayback.archive.org/web/20130123192255/http://www.robbiehanson.com/alarmclock/downloads/Alarm%20Clock%20(2.4.5).dmg'
   homepage 'http://wayback.archive.org/web/20130123192255/http://www.robbiehanson.com/alarmclock/index.html'
 
-  link 'Alarm Clock.app'
+  app 'Alarm Clock.app'
 end
