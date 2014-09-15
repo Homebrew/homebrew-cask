e1005f3aaf8937b5e658169dba6c96452f68029d
Roland Walker <walker@pobox.com>
2014-09-15 12:06:02 -0400

    app stanza in noiz2sa.rb

--- Casks/noiz2sa.rb
+++ Casks/noiz2sa.rb
@@ -5,5 +5,5 @@ class Noiz2sa < Cask
   url 'https://workram.com/downloads.php?f=Noiz2sa', :referer => 'https://workram.com/games/noiz2sa'
   homepage 'http://workram.com/games/noiz2sa/'
 
-  link 'Noiz2sa.app'
+  app 'Noiz2sa.app'
 end
