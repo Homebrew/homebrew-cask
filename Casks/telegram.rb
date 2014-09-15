cf2d8f935cb38b62404b15207e735dce3303cf82
Roland Walker <walker@pobox.com>
2014-09-15 12:06:27 -0400

    app stanza in telegram.rb

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -6,5 +6,5 @@ class Telegram < Cask
   appcast 'https://rink.hockeyapp.net/api/2/apps/c55f5e74ae5d0ad254df29f71a1b5f0e'
   homepage 'https://vk.com/telegram_osx'
 
-  link 'Telegram.app'
+  app 'Telegram.app'
 end
