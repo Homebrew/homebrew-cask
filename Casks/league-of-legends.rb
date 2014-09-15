7874f9126c20dcabfe8345f8d066621238484415
Roland Walker <walker@pobox.com>
2014-09-15 12:05:50 -0400

    app stanza in league-of-legends.rb

--- Casks/league-of-legends.rb
+++ Casks/league-of-legends.rb
@@ -5,7 +5,7 @@ class LeagueOfLegends < Cask
   url 'http://l3cdn.riotgames.com/Installer/NA_Mac_Installer/League%20of%20Legends%20NA.dmg'
   homepage 'http://signup.leagueoflegends.com/'
 
-  link 'League of Legends.app'
+  app 'League of Legends.app'
   caveats do
     manual_installer 'League of Legends.app'
   end
