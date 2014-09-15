24c60ea9e7381ad50e643a37fb26c3e388802e60
Roland Walker <walker@pobox.com>
2014-09-15 12:05:58 -0400

    app stanza in moreamp.rb

--- Casks/moreamp.rb
+++ Casks/moreamp.rb
@@ -5,5 +5,5 @@ class Moreamp < Cask
   url 'https://downloads.sourceforge.net/project/moreamp/moreamp/MoreAmp-0.1.28/MoreAmp-0.1.28-binOSXintel.dmg'
   homepage 'http://sourceforge.net/projects/moreamp/'
 
-  link 'MoreAmp.app'
+  app 'MoreAmp.app'
 end
