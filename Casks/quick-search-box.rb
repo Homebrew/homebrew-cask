300e7876a9712daff80065e1cb9a1f4e27d1de54
Roland Walker <walker@pobox.com>
2014-09-15 12:06:12 -0400

    app stanza in quick-search-box.rb

--- Casks/quick-search-box.rb
+++ Casks/quick-search-box.rb
@@ -5,7 +5,7 @@ class QuickSearchBox < Cask
   url 'https://qsb-mac.googlecode.com/files/GoogleQuickSearchBox-2.0.0.1447.Release.dmg'
   homepage 'http://www.google.com/quicksearchbox/'
 
-  link 'Quick Search Box.app'
+  app 'Quick Search Box.app'
   postflight do
     system '/bin/chmod', '-R', '--', 'u+w', destination_path
   end
