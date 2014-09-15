a420cda98bcb5d56af00215540de8d48cb16a685
Roland Walker <walker@pobox.com>
2014-09-15 12:05:12 -0400

    app stanza in activity-audit.rb

--- Casks/activity-audit.rb
+++ Casks/activity-audit.rb
@@ -6,5 +6,5 @@ class ActivityAudit < Cask
   appcast 'http://version.dssw.co.uk/activityaudit/standard'
   homepage 'https://www.dssw.co.uk/activityaudit'
 
-  link 'Activity Audit.app'
+  app 'Activity Audit.app'
 end
