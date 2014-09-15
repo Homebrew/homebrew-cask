af55bed6f633661c472e3f14c4d229aa46d69ea5
ndr <ndr@qef.io>
2014-09-15 20:17:27 +0200

    Add gpg stanza to vlc

--- Casks/vlc.rb
+++ Casks/vlc.rb
@@ -3,6 +3,8 @@ class Vlc < Cask
   sha256 '923504e296829f4253af8276b992636f72e56232ecdbdcf6003647ee662257f2'
 
   url "https://get.videolan.org/vlc/#{version}/macosx/vlc-#{version}.dmg"
+  gpg "#{url}.asc",
+      :key_id => '65f7c6b4206bd057a7eb73787180713be58d1adc'
   appcast 'http://update.videolan.org/vlc/sparkle/vlc-intel64.xml'
   homepage 'http://www.videolan.org/vlc/'
   license :oss
