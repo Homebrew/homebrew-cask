b539d784fdf6fb39e0d52698243861a07f9b3e22
Roland Walker <walker@pobox.com>
2014-09-15 12:06:17 -0400

    app stanza in serial-tools.rb

--- Casks/serial-tools.rb
+++ Casks/serial-tools.rb
@@ -5,5 +5,5 @@ class SerialTools < Cask
   url 'http://www.w7ay.net/site/Downloads/Serial%20Tools/Serial%20Tools%20app.dmg'
   homepage 'http://www.w7ay.net/site/Applications/Serial%20Tools/'
 
-  link 'Serial Tools.app'
+  app 'Serial Tools.app'
 end
