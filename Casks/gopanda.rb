24decc076830b53cf5724605b151963ffb245f15
Roland Walker <walker@pobox.com>
2014-09-15 12:05:39 -0400

    app stanza in gopanda.rb

--- Casks/gopanda.rb
+++ Casks/gopanda.rb
@@ -5,5 +5,5 @@ class Gopanda < Cask
   url 'http://pandanet-igs.com/gopanda2/installer/stable/mac-32/gopanda2-mac-32.zip'
   homepage 'http://pandanet-igs.com/communities/gopanda2'
 
-  link 'GoPanda2.app'
+  app 'GoPanda2.app'
 end
