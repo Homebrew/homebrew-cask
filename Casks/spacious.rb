58a2b03e3609ff63b1981b64d6226eed8004aeed
Roland Walker <walker@pobox.com>
2014-09-15 12:06:22 -0400

    app stanza in spacious.rb

--- Casks/spacious.rb
+++ Casks/spacious.rb
@@ -5,5 +5,5 @@ class Spacious < Cask
   url 'http://static.iospirit.com/static/objectfiles/file/159/Spacious.zip'
   homepage 'http://www.iospirit.com/products/spacious'
 
-  link 'Spacious.app'
+  app 'Spacious.app'
 end
