8c4453973cb8d64dec95e0a82b5a152d8c8c65ab
Roland Walker <walker@pobox.com>
2014-09-15 12:05:42 -0400

    app stanza in ibabel.rb

--- Casks/ibabel.rb
+++ Casks/ibabel.rb
@@ -5,5 +5,5 @@ class Ibabel < Cask
   url 'http://macinchem.org/ibabel/ibabel3_files/iBabel.zip'
   homepage 'http://www.macinchem.org/ibabel/ibabel3.php'
 
-  link 'iBabel.app'
+  app 'iBabel.app'
 end
