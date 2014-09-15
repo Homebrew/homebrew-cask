f52ae923b3fec7f7f5cfe3e10b98f86e79015835
Roland Walker <walker@pobox.com>
2014-09-15 12:05:41 -0400

    app stanza in hex-fiend.rb

--- Casks/hex-fiend.rb
+++ Casks/hex-fiend.rb
@@ -5,5 +5,5 @@ class HexFiend < Cask
   url 'http://ridiculousfish.com/hexfiend/files/HexFiend.zip'
   homepage 'http://ridiculousfish.com/hexfiend/'
 
-  link 'Hex Fiend.app'
+  app 'Hex Fiend.app'
 end
