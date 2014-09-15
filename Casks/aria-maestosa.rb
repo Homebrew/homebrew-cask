6c0442dbd3bac6027bf411f688a6b85ffeef4df4
Roland Walker <walker@pobox.com>
2014-09-15 12:05:15 -0400

    app stanza in aria-maestosa.rb

--- Casks/aria-maestosa.rb
+++ Casks/aria-maestosa.rb
@@ -5,5 +5,5 @@ class AriaMaestosa < Cask
   url "https://downloads.sourceforge.net/sourceforge/ariamaestosa/AriaMaestosa-osx-#{version}.zip"
   homepage 'http://ariamaestosa.sourceforge.net'
 
-  link "AriaMaestosa-#{version}/Aria Maestosa.app"
+  app "AriaMaestosa-#{version}/Aria Maestosa.app"
 end
