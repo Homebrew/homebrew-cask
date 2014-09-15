caf6f5beb252bbb36247c8d74f6e6006ec80ccd9
Roland Walker <walker@pobox.com>
2014-09-15 12:06:26 -0400

    app stanza in tagoman.rb

--- Casks/tagoman.rb
+++ Casks/tagoman.rb
@@ -5,5 +5,5 @@ class Tagoman < Cask
   url 'https://dl.dropboxusercontent.com/u/7614970/TagoMan.zip'
   homepage 'http://onflapp.wordpress.com/tagoman'
 
-  link 'TagoMan.app'
+  app 'TagoMan.app'
 end
