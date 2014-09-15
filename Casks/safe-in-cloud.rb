af68f486a9fc1358224171a3bea1428105b0cac8
Roland Walker <walker@pobox.com>
2014-09-15 12:06:15 -0400

    app stanza in safe-in-cloud.rb

--- Casks/safe-in-cloud.rb
+++ Casks/safe-in-cloud.rb
@@ -5,5 +5,5 @@ class SafeInCloud < Cask
   url 'http://www.safe-in-cloud.com/images/downloads/Safe%20In%20Cloud.dmg'
   homepage 'http://www.safe-in-cloud.com'
 
-  link 'Safe In Cloud.app'
+  app 'Safe In Cloud.app'
 end
