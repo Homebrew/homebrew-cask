6d5033abe7816a62c7119d6b2a19023fb8821771
Roland Walker <walker@pobox.com>
2014-09-15 12:05:52 -0400

    app stanza in logmein-client.rb

--- Casks/logmein-client.rb
+++ Casks/logmein-client.rb
@@ -5,5 +5,5 @@ class LogmeinClient < Cask
   url 'https://secure.logmein.com/welcome/labs/LogMeInIgnition.dmg'
   homepage 'https://secure.logmein.com/products/pro/learnmore/desktopapp.aspx'
 
-  link 'LogMeInIgnition.app', :target => 'LogMeIn Client.app'
+  app 'LogMeInIgnition.app', :target => 'LogMeIn Client.app'
 end
