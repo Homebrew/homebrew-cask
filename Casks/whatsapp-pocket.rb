3461ef277a908bebabe2f0b34da3f11289074a0a
Roland Walker <walker@pobox.com>
2014-09-15 12:06:36 -0400

    app stanza in whatsapp-pocket.rb

--- Casks/whatsapp-pocket.rb
+++ Casks/whatsapp-pocket.rb
@@ -5,5 +5,5 @@ class WhatsappPocket < Cask
   url 'http://www.fireebok.com/product/WhatsApp_Pocket_Mac.dmg'
   homepage 'http://www.fireebok.com/whatsapp-pocket.html'
 
-  link 'WhatsApp Pocket.app'
+  app 'WhatsApp Pocket.app'
 end
