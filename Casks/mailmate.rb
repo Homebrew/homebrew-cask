46109ebc3b40e91d028e86acc9fc877e8c4fe5f6
Roland Walker <walker@pobox.com>
2014-09-15 12:05:54 -0400

    app stanza in mailmate.rb

--- Casks/mailmate.rb
+++ Casks/mailmate.rb
@@ -5,7 +5,7 @@ class Mailmate < Cask
   url 'http://dl.mailmate-app.com/MailMate.tbz'
   homepage 'http://freron.com/'
 
-  link 'MailMate.app'
+  app 'MailMate.app'
   binary 'MailMate.app/Contents/Resources/emate'
   caveats do
     files_in_usr_local
