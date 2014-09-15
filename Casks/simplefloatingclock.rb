fa968118add550a5544197e87836cf25e8c479f6
Roland Walker <walker@pobox.com>
2014-09-15 12:06:19 -0400

    app stanza in simplefloatingclock.rb

--- Casks/simplefloatingclock.rb
+++ Casks/simplefloatingclock.rb
@@ -5,5 +5,5 @@ class Simplefloatingclock < Cask
   url 'http://www.splook.com/Software/distrib/sfc.zip'
   homepage 'http://www.splook.com/Software/Simple_Floating_Clock.html'
 
-  link 'Simple Floating Clock/SimpleFloatingClock.app'
+  app 'Simple Floating Clock/SimpleFloatingClock.app'
 end
