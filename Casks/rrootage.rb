31d6ca6d409cf493e3650c9d83d1c03733295236
Roland Walker <walker@pobox.com>
2014-09-15 12:06:14 -0400

    app stanza in rrootage.rb

--- Casks/rrootage.rb
+++ Casks/rrootage.rb
@@ -5,5 +5,5 @@ class Rrootage < Cask
   url 'https://workram.com/downloads.php?f=rRootage', :referer => 'https://workram.com/games/rrootage'
   homepage 'http://workram.com/games/rrootage/'
 
-  link 'rRootage.app'
+  app 'rRootage.app'
 end
