d156cbfcb2dbb5d15664124fa80966a80a5c6b75
Roland Walker <walker@pobox.com>
2014-09-15 13:50:09 -0400

    add `--force` option for `brew cask uninstall`
    also update some related docs for verbs `install` and `uninstall`

--- lib/cask/cli/uninstall.rb
+++ lib/cask/cli/uninstall.rb
@@ -2,11 +2,12 @@ class Cask::CLI::Uninstall < Cask::CLI::Base
   def self.run(*args)
     raise CaskUnspecifiedError if args.empty?
     cask_names = args.reject { |a| a.chars.first == '-' }
+    force = args.include? '--force'
     cask_names.each do |cask_name|
       odebug "Uninstalling Cask #{cask_name}"
       cask = Cask.load(cask_name)
-      raise CaskNotInstalledError.new(cask) unless cask.installed?
-      Cask::Installer.new(cask).uninstall
+      raise CaskNotInstalledError.new(cask) unless cask.installed? or force
+      Cask::Installer.new(cask).uninstall(force)
     end
   end
 
