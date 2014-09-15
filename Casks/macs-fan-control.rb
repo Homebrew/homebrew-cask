6d7358c83e691320f1fa1aaf6976d25b5121700e
Roland Walker <walker@pobox.com>
2014-09-15 12:05:53 -0400

    app stanza in macs-fan-control.rb

--- Casks/macs-fan-control.rb
+++ Casks/macs-fan-control.rb
@@ -5,5 +5,5 @@ class MacsFanControl < Cask
   url 'http://www.crystalidea.com/downloads/macsfancontrol.zip'
   homepage 'http://www.crystalidea.com/macs-fan-control'
 
-  link 'Macs Fan Control.app'
+  app 'Macs Fan Control.app'
 end
