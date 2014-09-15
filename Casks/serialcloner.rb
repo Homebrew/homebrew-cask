62aed34a0e53e4c80aac995effc90315acd4fade
Roland Walker <walker@pobox.com>
2014-09-15 12:06:17 -0400

    app stanza in serialcloner.rb

--- Casks/serialcloner.rb
+++ Casks/serialcloner.rb
@@ -5,5 +5,5 @@ class Serialcloner < Cask
   url 'http://serialbasics.free.fr/Serial_Cloner-Download_files/SerialCloner2-6.dmg'
   homepage 'http://serialbasics.free.fr/Serial_Cloner.html'
 
-  link 'SerialCloner2-6/SerialCloner 2-6-1.app'
+  app 'SerialCloner2-6/SerialCloner 2-6-1.app'
 end
