c1265ce1e989ddd42acd77971ac0ce298924d3f5
Roland Walker <walker@pobox.com>
2014-09-15 12:06:28 -0400

    app stanza in tftpserver.rb

--- Casks/tftpserver.rb
+++ Casks/tftpserver.rb
@@ -5,5 +5,5 @@ class Tftpserver < Cask
   url 'http://ww2.unime.it/flr/tftpserver/TftpServer.dmg'
   homepage 'http://ww2.unime.it/flr/tftpserver/'
 
-  link 'TftpServer.app'
+  app 'TftpServer.app'
 end
