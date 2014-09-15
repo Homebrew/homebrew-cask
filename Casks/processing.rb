54fcef60721e8959a501094b4a4ffdbb73a3cde8
Roland Walker <walker@pobox.com>
2014-09-15 12:06:10 -0400

    app stanza in processing.rb

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -5,5 +5,5 @@ class Processing < Cask
   url 'http://download.processing.org/processing-2.2.1-macosx.zip'
   homepage 'http://processing.org/'
 
-  link 'Processing.app'
+  app 'Processing.app'
 end
