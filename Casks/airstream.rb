298496927f37bb5b1b55ca946e315c5166ea09fb
Roland Walker <walker@pobox.com>
2014-09-16 08:31:24 -0400

    double slash in airstream

--- Casks/airstream.rb
+++ Casks/airstream.rb
@@ -10,7 +10,7 @@ class Airstream < Cask
     puts <<-EOS.undent
       If you have Java 7 or above installed, you may have to run the following commands:
         sudo mkdir -p /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/bundle/Libraries/
-        sudo ln -s /Library/Java/JavaVirtualMachines//jdk1.8.0_05.jdk/Contents/Home/jre/lib/server/libjvm.dylib /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/bundle/Libraries/libserver.dylib
+        sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/jre/lib/server/libjvm.dylib /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/bundle/Libraries/libserver.dylib
     EOS
   end
 end
