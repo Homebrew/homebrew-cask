c5b77514b14e94eab501f8ffbf836a26fbc65f2a
ndr <ndr@qef.io>
2014-09-16 07:42:46 +0200

    Add gpg stanza to linphone

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -3,6 +3,8 @@ class Linphone < Cask
   sha256 '4d4a01354a7b5cd011746d3477a93ffb6e531ff8e2afccd2b9bb031f06cc42cc'
 
   url "http://download-mirror.savannah.gnu.org/releases/linphone/3.7.x/macos/linphone-#{version}.dmg"
+  gpg "#{url}.sig",
+      :key_id => '3ecd52dee2f56985'
   homepage 'http://www.linphone.org/'
   license :unknown
 
