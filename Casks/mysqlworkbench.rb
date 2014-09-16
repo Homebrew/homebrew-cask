aa2b69752615a5e7bffe52aa238d2621dc08ce1d
ndr <ndr@qef.io>
2014-09-16 07:43:15 +0200

    Add gpg stanza to mysqlworkbench

--- Casks/mysqlworkbench.rb
+++ Casks/mysqlworkbench.rb
@@ -3,6 +3,8 @@ class Mysqlworkbench < Cask
   sha256 '0a66707a03d83a70f1ced521ac0954ee56ff9a6e86f81e01d03df723755e9186'
 
   url "https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-#{version}-osx-i686.dmg"
+  gpg "#{url}.asc",
+      :key_id => '8c718d3b5072e1f5'
   homepage 'http://www.mysql.com/products/workbench'
   license :unknown
 
