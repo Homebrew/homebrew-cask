8b244fbb936bf8619ff5bb3da2c30a36c04f7b18
Roland Walker <walker@pobox.com>
2014-09-16 08:31:24 -0400

    double slash in microsoft-lync-plugin

--- Casks/microsoft-lync-plugin.rb
+++ Casks/microsoft-lync-plugin.rb
@@ -2,7 +2,7 @@ class MicrosoftLyncPlugin < Cask
   version 'latest'
   sha256 :no_check
 
-  url 'https://lync12.lyncweb.microsoft.com//lwa/Plugins/LWAPluginInstaller.pkg'
+  url 'https://lync12.lyncweb.microsoft.com/lwa/Plugins/LWAPluginInstaller.pkg'
   homepage 'http://office.microsoft.com/Lync'
 
   pkg 'LWAPluginInstaller.pkg'
