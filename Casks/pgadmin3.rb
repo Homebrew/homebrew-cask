38092bd0f1d0c4b31b5cbc3bd6db8e69c3f63551
ndr <ndr@qef.io>
2014-09-16 07:43:35 +0200

    Add gpg stanza to pgadmin3

--- Casks/pgadmin3.rb
+++ Casks/pgadmin3.rb
@@ -4,6 +4,8 @@ class Pgadmin3 < Cask
   sha256 '6fe924168c7a8ee9224527fcafc3e612de60c011a27dc44b8293401ff346627d'
 
   url "http://ftp.postgresql.org/pub/pgadmin3/release/v#{version}/osx/pgadmin3-#{version}.dmg"
+  gpg "#{url}.sig",
+      :key_id => '96020e041a19643b'
   homepage 'http://pgadmin.org'
   license :unknown
 
