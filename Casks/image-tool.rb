8af2ab3e6f4713deb83b83cc1e9d2c7894a064d3
DerZyklop <mail@der-zyklop.de>
2014-09-16 11:53:26 +0200

    Add Image Tool v1.4.1

---
+++ Casks/image-tool.rb
@@ -0,0 +1,9 @@
+class ImageTool < Cask
+  version '1.4.1'
+  sha256 '66ef0c6cdf2e90981bc8bd0d1131e6e0f141744406997712550b8b5a05022d39'
+
+  url "http://www.jimmcgowan.net/diskimages/ImageTool#{version}.dmg"
+  homepage 'http://www.jimmcgowan.net/Site/ImageTool.html'
+
+  app 'Image Tool.app'
+end
