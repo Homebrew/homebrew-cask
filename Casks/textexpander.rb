d32a3b67a03f07bbd729ec9595bd6045955481bf
Ted Wise <cwise@hdlabinc.com>
2014-09-16 09:20:51 -0400

    Textexpander appcast url was wrong.

--- Casks/textexpander.rb
+++ Casks/textexpander.rb
@@ -3,7 +3,7 @@ class Textexpander < Cask
   sha256 '1aa42fa3b1bcb6e41f662c040f3efde5713ddfe8f1aee9d03c8f60bcd9d65ddf'
 
   url "http://cdn.smilesoftware.com/TextExpander_#{version}.zip"
-  appcast 'http://www.smilesoftware.com/appcast/update.php'
+  appcast 'http://updates.smilesoftware.com/com.smileonmymac.textexpander.xml'
   homepage 'http://www.smilesoftware.com/TextExpander/index.html'
 
   app 'TextExpander.app'
