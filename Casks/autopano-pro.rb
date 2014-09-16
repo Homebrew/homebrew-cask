fd293fcc70655cc05e26163d0e0690f0a5a26236
Falko Richter <falko.richter@ecabo.de>
2014-09-16 17:14:25 +0200

    added Autopano Pro

---
+++ Casks/autopano-pro.rb
@@ -0,0 +1,9 @@
+class AutopanoPro < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://download.kolor.com/app/stable/macleopard'
+  homepage 'http://www.kolor.com/panorama-software-autopano-pro.html'
+
+  pkg 'Autopano Pro 3.6.pkg'
+end
