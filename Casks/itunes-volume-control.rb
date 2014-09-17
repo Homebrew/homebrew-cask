bdad18c40e17d11472b0f337aee086596da13b78
Roland Walker <walker@pobox.com>
2014-09-17 07:42:49 -0400

    :latest as symbol in itunes-volume-control

--- Casks/itunes-volume-control.rb
+++ Casks/itunes-volume-control.rb
@@ -1,5 +1,5 @@
 class ItunesVolumeControl < Cask
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'https://github.com/alberti42/iTunes-Volume-Control/blob/master/iTunes%20Volume%20Control.dmg?raw=true'
