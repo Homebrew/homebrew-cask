2ad24edf47361656f0f713dbd4e1714456d1f69b
Maurice Kelly <osv-github@chatswood.org.uk>
2014-09-17 15:55:31 +0100

    Upgrade iBank to v5
    In moving to v5 Igg Software changed the download URL and app name. This
    PR addresses both those issues.

--- Casks/ibank.rb
+++ Casks/ibank.rb
@@ -2,8 +2,8 @@ class Ibank < Cask
   version 'latest'
   sha256 :no_check
 
-  url 'https://www.iggsoft.com/ibank/iBank4_Web.dmg'
+  url 'https://www.iggsoft.com/ibank/iBank5_Web.dmg'
   homepage 'http://www.iggsoftware.com/ibank'
 
-  app 'iBank.app'
+  app 'iBank 5.app'
 end
