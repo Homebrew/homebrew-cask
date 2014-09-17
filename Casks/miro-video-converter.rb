5a6a746b7b97d639a9531d3fec4ef0041043bd1c
Roland Walker <walker@pobox.com>
2014-09-17 07:42:53 -0400

    :latest as symbol in miro-video-converter

--- Casks/miro-video-converter.rb
+++ Casks/miro-video-converter.rb
@@ -1,5 +1,5 @@
 class MiroVideoConverter < Cask
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'http://ftp.osuosl.org/pub/pculture.org/mirovideoconverter/mac/Miro%20Video%20Converter.dmg'
