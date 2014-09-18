4b9e37975f512092a64a11f13c77bd6f847314b2
Roland Walker <walker@pobox.com>
2014-09-18 07:57:55 -0400

    cvt to :delete dolphin

--- Casks/dolphin.rb
+++ Casks/dolphin.rb
@@ -6,8 +6,8 @@ class Dolphin < Cask
   homepage 'http://www.dolphin-emu.org/'
 
   app 'Dolphin.app'
-  zap :files => [
-                 '~/Library/Application Support/Dolphin',
-                 '~/Library/Preferences/org.dolphin-emu.dolphin.plist',
-                ]
+  zap :delete => [
+                  '~/Library/Application Support/Dolphin',
+                  '~/Library/Preferences/org.dolphin-emu.dolphin.plist',
+                 ]
 end
