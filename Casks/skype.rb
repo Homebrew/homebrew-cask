8aee9706ed4f5bcd2c4eb9d72e62b20a5eebd294
Roland Walker <walker@pobox.com>
2014-09-18 07:57:59 -0400

    cvt to :delete skype

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -11,5 +11,5 @@ class Skype < Cask
   homepage 'http://www.skype.com'
 
   app 'Skype.app'
-  zap :files => '~/Library/Application Support/Skype'
+  zap :delete => '~/Library/Application Support/Skype'
 end
