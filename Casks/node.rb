fd336ae257538e11910f70f8a0bdafe34e4d96b6
Michael Ford <fanquake@users.noreply.github.com>
2014-09-18 18:48:30 +0800

    Node 0.10.32

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,6 +1,6 @@
 class Node < Cask
-  version '0.10.31'
-  sha256 '450fc1ff7f3b716d01cec78826b3b708292327a34434871bafd5a05a3429756e'
+  version '0.10.32'
+  sha256 '3ce16fa81f47da38bcf726ef384e1bc0658d52f880d17aa17fd5b613bea035cf'
 
   url "http://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   homepage 'http://nodejs.org'
