583345bd3a88656f432778456e5352e0e8a75518
Roland Walker <walker@pobox.com>
2014-09-18 08:48:20 -0400

    remove caskroom_only in adobe-arh

--- Casks/adobe-arh.rb
+++ Casks/adobe-arh.rb
@@ -5,7 +5,6 @@ class AdobeArh < Cask
   url 'http://airdownload.adobe.com/air/distribution/latest/mac/arh'
   homepage 'http://help.adobe.com/en_US/air/redist/WS485a42d56cd19641-70d979a8124ef20a34b-8000.html'
 
-  caskroom_only true
   binary 'arh'
   container_type :naked
 
