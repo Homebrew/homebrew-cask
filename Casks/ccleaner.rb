92b866f3cf36b241a14dd8b47d26f806453d7706
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-09-19 12:00:23 +0800

    Update ccleaner 1.09.313 sha256.

--- Casks/ccleaner.rb
+++ Casks/ccleaner.rb
@@ -1,6 +1,6 @@
 class Ccleaner < Cask
-  version '1.09.311'
-  sha256 'c0aca1010085ccfd282e9e2356604a2f7513e9c1fada27c217d71660151775aa'
+  version '1.09.313'
+  sha256 '464a75b9d038dec0334f70846d5ea68679ff907e57b9ec373331c5da18cb4865'
 
   url 'http://download.piriform.com/mac/CCMacSetup109.dmg'
   homepage 'http://www.piriform.com/ccleaner'
