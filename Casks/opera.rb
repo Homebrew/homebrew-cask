5d0c79dc7895c34fde2113c43428779523a25b3c
Beng Hee Eu <beng@benghee.eu>
2014-09-19 23:05:49 -0400

    Opera 24.0.1558.61

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 class Opera < Cask
-  version '24.0.1558.53'
-  sha256 'c75e05f0c85ae50939be05f122d74c2f1228aca6012aa1ff6b34bd9ca769c83d'
+  version '24.0.1558.61'
+  sha256 '2f23178ef04d318523096ef44f15b74f2b2201e6780f1b46a04207c8f2879aac'
 
   url "http://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/'
