351b69a6d59bc6cdae283b4bba2f46e3bbdd7f97
Michael Ford <fanquake@users.noreply.github.com>
2014-09-21 17:34:33 +0800

    Fix Silverlight Checksum
    Fixes #6285

--- Casks/silverlight.rb
+++ Casks/silverlight.rb
@@ -1,6 +1,6 @@
 class Silverlight < Cask
   version '5.1.30514.0'
-  sha256 '7425532f16a5ee9aa4db68154803bbc7df20a84e1d8ff46f38f23c0e678e240d'
+  sha256 '469a7b19cf4a87a1248caa850c7de61c88e67c0c5511fe77032817cd79df9dea'
 
   url 'http://silverlight.dlservice.microsoft.com/download/F/8/C/F8C0EACB-92D0-4722-9B18-965DD2A681E9/30514.00/Silverlight.dmg'
   homepage 'http://www.microsoft.com/silverlight/'
