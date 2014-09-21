367f60c76e0b0558ad78f26b89b0ad721cc8e1e2
Lucas Horton <lucas@lucashorton.com>
2014-09-21 14:38:30 -0500

    Add Curio.app

---
+++ Casks/curio.rb
@@ -0,0 +1,9 @@
+class Curio < Cask
+  version :latest 
+  sha256 :no_check 
+
+  url 'http://zengobi.com/downloads/Curio.zip'
+  homepage 'http://zengobi.com/products/curio/'
+
+  app 'Curio.app'
+end
