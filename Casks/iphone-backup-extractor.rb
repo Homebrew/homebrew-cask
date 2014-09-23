b8aeb32f08ca2077580cb172e94562e2ff864bfb
Maurice Kelly <osv-github@chatswood.org.uk>
2014-09-23 22:22:15 +0100

    Adding cask for iPhone Backup Extractor v1.2.4
    This is an unversioned download.

---
+++ Casks/iphone-backup-extractor.rb
@@ -0,0 +1,9 @@
+class IphoneBackupExtractor < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://supercrazyawesome.com/downloads/iPhone%20Backup%20Extractor.app.zip'
+  homepage 'http://supercrazyawesome.com/'
+
+  app 'iPhone Backup Extractor.app'
+end
