f8bcac30508ba4ae7653fed1149c03233aaf677d
lalyos <lalyos@yahoo.com>
2014-09-23 13:39:55 +0200

    Upgrade zed to 1.0.0

--- Casks/zed.rb
+++ Casks/zed.rb
@@ -1,8 +1,8 @@
 class Zed < Cask
-  version '0.13.0'
-  sha256 '8b7d79177928d04bf97f5f116623543a62f20c87935e1a8eb45b10d809d212a1'
+  version '1.0.0'
+  sha256 '38adf6049be293b8fca425e7976f4d51d2aea296c5177110be8fc872f9fa76aa'
 
-  url 'http://download.zedapp.org/zed-mac-v0.13.0.tar.gz'
+  url 'http://download.zedapp.org/zed-mac-v1.0.0.tar.gz'
   homepage 'http://zedapp.org'
 
   app 'Zed.app'
