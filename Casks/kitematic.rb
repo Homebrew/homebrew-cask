1d79f958efe9f41f51de07965b36415a937360f2
Rocky Madden <git@rockymadden.com>
2014-09-22 21:16:52 -0600

    Fixed Kitematic version to state 0.2.3.

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,5 +1,5 @@
 class Kitematic < Cask
-  version :latest
+  version '0.2.3'
   sha256 '22a0c8bed6b531de6349b44912e433d582b18573d365bb117dc05d4520f201f9'
 
   url 'https://s3.amazonaws.com/kite-installer/Kitematic-0.2.3.zip'
