f528c70b2d0f865c721048121f173a29f63859f9
Roland Walker <walker@pobox.com>
2014-09-23 09:14:23 -0400

    remove caskroom_only in titanium-studio

--- Casks/titanium-studio.rb
+++ Casks/titanium-studio.rb
@@ -5,7 +5,6 @@ class TitaniumStudio < Cask
   url 'https://titanium-studio.s3.amazonaws.com/latest/Titanium_Studio.dmg'
   homepage 'https://my.appcelerator.com/resources'
 
-  caskroom_only true
   caveats do
     manual_installer 'Titanium Studio.app'
   end
