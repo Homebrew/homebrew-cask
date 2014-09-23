a05de93498c1b7ec6e1f6654271d1b3f8ca62f3c
Rocky Madden <git@rockymadden.com>
2014-09-22 21:14:19 -0600

    Updated Kitematic to 0.2.3. Note, per the developers the Kitematic.zip file will NOT contain the newest version as was previously assumed.

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,8 +1,8 @@
 class Kitematic < Cask
   version :latest
-  sha256 :no_check
+  sha256 '22a0c8bed6b531de6349b44912e433d582b18573d365bb117dc05d4520f201f9'
 
-  url 'https://s3.amazonaws.com/kite-installer/Kitematic.zip'
+  url 'https://s3.amazonaws.com/kite-installer/Kitematic-0.2.3.zip'
   homepage 'https://kitematic.com/'
 
   app 'Kitematic.app'
