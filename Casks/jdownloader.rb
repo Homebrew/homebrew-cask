6e4d00db6b16b7f7d672e79c55e489693829caec
Roland Walker <walker@pobox.com>
2014-09-23 09:14:23 -0400

    remove caskroom_only in jdownloader

--- Casks/jdownloader.rb
+++ Casks/jdownloader.rb
@@ -5,7 +5,6 @@ class Jdownloader < Cask
   url 'http://installer.jdownloader.org/JDownloader09Setup_mac.dmg'
   homepage 'http://jdownloader.org/'
 
-  caskroom_only true
   caveats do
     manual_installer 'JDownloader Installer.app'
   end
