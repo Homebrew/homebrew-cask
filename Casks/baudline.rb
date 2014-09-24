6c4cbd626323eda3b68d3914e90f5fd8510bd654
Xie Yanbo <xieyanbo@gmail.com>
2014-09-24 19:18:28 +0800

    Add Baudline v1.08
    Closes #6322.

---
+++ Casks/baudline.rb
@@ -0,0 +1,9 @@
+class Baudline < Cask
+  version '1.08'
+  sha256 'f47201069812333c36715beaacb17de93bc8fa5f7e68952fe51296f04df69f83'
+
+  url "http://www.baudline.com/baudline_#{version}_macosx_universal.dmg"
+  homepage 'http://www.baudline.com/'
+
+  app 'baudline.app'
+end
