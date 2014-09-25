4a19de3cd4954b4766c7241daa0b4b258a0bd785
Ethan Sherbondy <sherbondye@gmail.com>
2014-09-25 15:30:38 -0400

    Add PreForm.app 1.6.5
    This is the first cask file submission for PreForm,
    a desktop application to help prepare models
    for Formlabs 3D printers.

---
+++ Casks/preform.rb
@@ -0,0 +1,9 @@
+class Preform < Cask
+  version '1.6.5'
+  sha256 '465bd7f85a44d66d0e0bf160ed744f9d014e1acf11478e1c884a72414b7053ab'
+
+  url 'https://s3.amazonaws.com/FormlabsReleases/Release/1.6/PreForm_1.6_5.dmg'
+  homepage 'http://formlabs.com/en/products/preform/'
+
+  app 'PreForm.app'
+end
