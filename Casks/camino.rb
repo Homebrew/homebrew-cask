0e16dd2c1e4e778987472eca9c68ef2cd236398d
Vítor Galvão <info@vitorgalvao.com>
2014-09-25 03:52:39 +0100

    Delete camino.rb
    Deleting camino, as development stopped.

--- Casks/camino.rb
+++
@@ -1,9 +0,0 @@
-class Camino < Cask
-  version '2.1.2'
-  sha256 'e212aa2b68fb3fa1a63c5d74d570843fd5b432bfde29baeed6c5c4d73ad31b51'
-
-  url 'http://download.cdn.mozilla.net/pub/mozilla.org/camino/releases/en-US/Camino-2.1.2.dmg'
-  homepage 'http://caminobrowser.org/'
-
-  app 'Camino.app'
-end
