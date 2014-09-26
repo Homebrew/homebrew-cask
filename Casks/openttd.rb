046c6eede82a8adda9b5fc6e4d1669220e744f4c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:05 -0400

    re-use version in openttd

--- Casks/openttd.rb
+++ Casks/openttd.rb
@@ -2,7 +2,7 @@ class Openttd < Cask
   version '1.4.2'
   sha256 '4ea6b4fa1baf03edafa30c6475dc3f9f219239d04840bf5d4a3228bf3f6f33ff'
 
-  url 'http://binaries.openttd.org/releases/1.4.2/openttd-1.4.2-macosx-universal.zip'
+  url "http://binaries.openttd.org/releases/#{version}/openttd-#{version}-macosx-universal.zip"
   homepage 'http://openttd.org'
 
   app 'OpenTTD.app'
