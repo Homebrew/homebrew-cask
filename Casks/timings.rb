e760514484686c4b1794a0e7ee0e16bdd19fe23a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in timings

--- Casks/timings.rb
+++ Casks/timings.rb
@@ -2,7 +2,7 @@ class Timings < Cask
   version '1.0.4'
   sha256 'e7601405312bd6142514aef98154b2ebe8e06a3154c7afa7bcc03edf0090ddff'
 
-  url 'http://mediaatelier.com/Timings/Timings_1.0.4.zip'
+  url "http://mediaatelier.com/Timings/Timings_#{version}.zip"
   appcast 'http://mediaatelier.com/Timings/feed.php'
   homepage 'http://mediaatelier.com/Timings'
 
