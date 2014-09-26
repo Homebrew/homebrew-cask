ac0b5bd2162944ee23706aaae03827aa45d1526b
Roland Walker <walker@pobox.com>
2014-09-26 08:09:11 -0400

    re-use version in shoes

--- Casks/shoes.rb
+++ Casks/shoes.rb
@@ -2,7 +2,7 @@ class Shoes < Cask
   version '3.1.0'
   sha256 '4d2489c302dce56c33389a170ab97ed5c9619a3d8bea3e0a958ab9dc7355e640'
 
-  url 'http://cloud.github.com/downloads/shoes/shoes/shoes-3.1.0.dmg'
+  url "http://cloud.github.com/downloads/shoes/shoes/shoes-#{version}.dmg"
   homepage 'http://shoesrb.com/'
 
   app 'Shoes.app'
