f45dde4b9ec3c98e02a742d994e9738437bd3865
Roland Walker <walker@pobox.com>
2014-09-26 08:09:11 -0400

    re-use version in send-to-kindle

--- Casks/send-to-kindle.rb
+++ Casks/send-to-kindle.rb
@@ -2,10 +2,10 @@ class SendToKindle < Cask
   version '1.0.0.220'
   sha256 'f43009bb0e48ed7e5a750a90163a6362f49e6518c698c5c49be0dce0d01fe3af'
 
-  url 'https://s3.amazonaws.com/sendtokindle/SendToKindleForMac-installer-v1.0.0.220.pkg'
+  url "https://s3.amazonaws.com/sendtokindle/SendToKindleForMac-installer-v#{version}.pkg"
   homepage 'http://www.amazon.com/gp/sendtokindle/mac'
 
-  pkg 'SendToKindleForMac-installer-v1.0.0.220.pkg'
+  pkg "SendToKindleForMac-installer-v#{version}.pkg"
   uninstall :launchctl => 'com.amazon.sendtokindle.launcher',
             :pkgutil   => 'com.amazon.SendToKindleMacInstaller.pkg'
 end
