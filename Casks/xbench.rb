f9060523197a8fd737e5f568e6c5993c8f406ad3
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in xbench

--- Casks/xbench.rb
+++ Casks/xbench.rb
@@ -2,7 +2,7 @@ class Xbench < Cask
   version '1.3'
   sha256 '285d860273b22054b807930ac496735e287d124f233da489e4104d2411690e55'
 
-  url 'http://xbench.com/Xbench_1.3.dmg'
+  url "http://xbench.com/Xbench_#{version}.dmg"
   homepage 'http://xbench.com/'
 
   app 'XBench.app'
