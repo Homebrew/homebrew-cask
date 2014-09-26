13b2018d6d7092317ceab0e423eed4e87b51a422
Roland Walker <walker@pobox.com>
2014-09-26 08:09:07 -0400

    re-use version in processing

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -2,7 +2,7 @@ class Processing < Cask
   version '2.2.1'
   sha256 '8c237b3eb50626e8ffc648bfdeddaa18ceffbd6a48f8fec77a8eab5b774971fc'
 
-  url 'http://download.processing.org/processing-2.2.1-macosx.zip'
+  url "http://download.processing.org/processing-#{version}-macosx.zip"
   homepage 'http://processing.org/'
 
   app 'Processing.app'
