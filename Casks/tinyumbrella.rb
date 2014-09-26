1ac4fa1c319c9b729f2ad23122d44879c2ad0644
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in tinyumbrella

--- Casks/tinyumbrella.rb
+++ Casks/tinyumbrella.rb
@@ -2,7 +2,7 @@ class Tinyumbrella < Cask
   version '7.04'
   sha256 '2ce5ea70bbdf216aaff9fc30c1a33a58a6fc19a5ad5e4f0029aafae61c622db1'
 
-  url 'http://cache.firmwareumbrella.com/downloads/TinyUmbrella-7.04.00.app.zip'
+  url "http://cache.firmwareumbrella.com/downloads/TinyUmbrella-#{version}.00.app.zip"
   homepage 'http://blog.firmwareumbrella.com/'
 
   app 'TinyUmbrella.app'
