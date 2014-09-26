63bd82bc90318fe2012425aabf58c1dd2ad3a63b
Roland Walker <walker@pobox.com>
2014-09-26 08:08:55 -0400

    re-use version in gephi

--- Casks/gephi.rb
+++ Casks/gephi.rb
@@ -2,7 +2,7 @@ class Gephi < Cask
   version '0.8.2-beta'
   sha256 'ee8f7845ade6109de7e88ed03cc92885c6d936c34d666503d7c92bf3c5d0659d'
 
-  url 'https://launchpad.net/gephi/0.8/0.8.2beta/+download/gephi-0.8.2-beta.dmg'
+  url "https://launchpad.net/gephi/0.8/0.8.2beta/+download/gephi-#{version}.dmg"
   homepage 'http://gephi.org/'
 
   app 'Gephi.app'
