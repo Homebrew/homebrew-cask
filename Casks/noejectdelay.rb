64f88ccf5f205c3db46abad27b134b2325e9711f
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in noejectdelay

--- Casks/noejectdelay.rb
+++ Casks/noejectdelay.rb
@@ -2,7 +2,7 @@ class Noejectdelay < Cask
   version '5.4.0'
   sha256 '7b480ade6537a2d98924ad1dabf87b8f3d396c2035addfd032839c2473659c21'
 
-  url 'https://pqrs.org/macosx/keyremap4macbook/files/NoEjectDelay-5.4.0.dmg'
+  url "https://pqrs.org/macosx/keyremap4macbook/files/NoEjectDelay-#{version}.dmg"
   homepage 'https://pqrs.org/macosx/keyremap4macbook/noejectdelay.html.en'
 
   pkg 'NoEjectDelay.pkg'
