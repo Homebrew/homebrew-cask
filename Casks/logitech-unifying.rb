6b7b26ef530a95f616411cd5071f139f679e7325
Roland Walker <walker@pobox.com>
2014-09-26 08:09:01 -0400

    re-use version in logitech-unifying

--- Casks/logitech-unifying.rb
+++ Casks/logitech-unifying.rb
@@ -2,7 +2,7 @@ class LogitechUnifying < Cask
   version '1.10.421'
   sha256 'd9e196411cc4c0aec72fd01575eaffed228f95bc7d7ededc532d53f8602caa03'
 
-  url 'http://www.logitech.com/pub/controldevices/unifying/unifying1.10.421.dmg'
+  url "http://www.logitech.com/pub/controldevices/unifying/unifying#{version}.dmg"
   homepage 'http://www.logitech.com/en-us/promotions/6072'
 
   pkg 'Logitech Unifying Software.mpkg'
