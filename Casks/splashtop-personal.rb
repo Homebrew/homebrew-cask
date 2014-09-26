d73a8f7747fb62befbf63b8c5a16f166cb6448dd
Roland Walker <walker@pobox.com>
2014-09-26 08:09:12 -0400

    re-use version in splashtop-personal

--- Casks/splashtop-personal.rb
+++ Casks/splashtop-personal.rb
@@ -2,7 +2,7 @@ class SplashtopPersonal < Cask
   version '2.4.3.1'
   sha256 'a0167ad134b3134e9b3e5e0a26238aed04ef230f3cabcddd7fd0fef9d82687aa'
 
-  url 'https://d17kmd0va0f0mp.cloudfront.net/macclient/STP/Splashtop_Personal_v2.4.3.1.dmg'
+  url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STP/Splashtop_Personal_v#{version}.dmg"
   homepage 'http://www.splashtop.com/personal'
 
   pkg 'Splashtop Personal.pkg'
