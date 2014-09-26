0b6e3d0621a5922816c6d1fa92bc9f31a0b5c5e1
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in xamarin-ios

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -2,11 +2,11 @@ class XamarinIos < Cask
   version '7.2.4.4'
   sha256 'd6056e2ab4e529d7a04e9bcca915d780e3d9a76593c14791040b29651f6f2e5d'
 
-  url 'http://download.xamarin.com/MonoTouch/Mac/monotouch-7.2.4.4.pkg'
+  url "http://download.xamarin.com/MonoTouch/Mac/monotouch-#{version}.pkg"
   # non-Sparkle appcast
   appcast 'http://xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml'
   homepage 'http://xamarin.com/ios'
 
-  pkg 'monotouch-7.2.4.4.pkg'
+  pkg "monotouch-#{version}.pkg"
   uninstall :pkgutil => 'com.xamarin.monotouch.pkg'
 end
