ae6968253da7d6fb412fe5c3075845302123af5d
Roland Walker <walker@pobox.com>
2014-09-26 08:09:01 -0400

    re-use version in lyx

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -2,7 +2,7 @@ class Lyx < Cask
   version '2.1.1'
   sha256 '6e354ef25b2b4b0b46f1c95d048bef93917b794d227d841f03c0c5d1002ba923'
 
-  url 'ftp://ftp.lyx.org/pub/lyx/bin/2.1.1/LyX-2.1.1+qt4-x86_64-cocoa.dmg'
+  url "ftp://ftp.lyx.org/pub/lyx/bin/#{version}/LyX-#{version}+qt4-x86_64-cocoa.dmg"
   homepage 'http://www.lyx.org'
 
   app 'LyX.app'
