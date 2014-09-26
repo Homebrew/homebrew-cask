318fd446d792c31489975c7c84169d1793ad1901
Roland Walker <walker@pobox.com>
2014-09-26 08:08:59 -0400

    re-use version in jubler

--- Casks/jubler.rb
+++ Casks/jubler.rb
@@ -2,7 +2,7 @@ class Jubler < Cask
   version '5.0.1'
   sha256 '295d23433a88a26c113529e6eb15548b260525acc2d86fec1b90cbfbf9572b1e'
 
-  url 'http://downloads.sourceforge.net/sourceforge/jubler/Jubler-5.0.1.dmg'
+  url "http://downloads.sourceforge.net/sourceforge/jubler/Jubler-#{version}.dmg"
   homepage 'http://www.jubler.org/'
 
   app 'Jubler.app'
