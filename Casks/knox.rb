bbb765923329d9ac84745fc5a75c8a7c67c44cf2
Roland Walker <walker@pobox.com>
2014-09-26 08:08:59 -0400

    re-use version in knox

--- Casks/knox.rb
+++ Casks/knox.rb
@@ -2,7 +2,7 @@ class Knox < Cask
   version '2.2.0'
   sha256 'c19c56a35d299a2cd85c612e1e99009bff9d7536ddd24f9d565910702be9742a'
 
-  url 'https://d13itkw33a7sus.cloudfront.net/dist/K/Knox-2.2.0.zip'
+  url "https://d13itkw33a7sus.cloudfront.net/dist/K/Knox-#{version}.zip"
   homepage 'https://agilebits.com/knox'
 
   app 'Knox.app'
