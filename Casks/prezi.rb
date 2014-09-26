33ad206bb8f2242e52103c77f600faf9e0993fb3
Roland Walker <walker@pobox.com>
2014-09-26 08:09:07 -0400

    re-use version in prezi

--- Casks/prezi.rb
+++ Casks/prezi.rb
@@ -2,7 +2,7 @@ class Prezi < Cask
   version '5.1.1'
   sha256 'cd77adf228b237f24407a4a7c8e57463b27ad98d9885451e42cc7416484cce56'
 
-  url 'https://prezi-a.akamaihd.net/desktop/Prezi5.1.1.dmg'
+  url "https://prezi-a.akamaihd.net/desktop/Prezi#{version}.dmg"
   homepage 'http://www.prezi.com/'
 
   app 'Prezi.app'
