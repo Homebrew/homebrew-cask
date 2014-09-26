5ce730ac882ba2e0c6aead17857676c39b5de7d9
Roland Walker <walker@pobox.com>
2014-09-26 08:09:11 -0400

    re-use version in serviio

--- Casks/serviio.rb
+++ Casks/serviio.rb
@@ -2,7 +2,7 @@ class Serviio < Cask
   version '1.4.1.2'
   sha256 '132ed6ba9baf466eec5d89789cd8d50163fa5b534899f10a9232e00f711707aa'
 
-  url 'http://download.serviio.org/releases/serviio-1.4.1.2-osx.tar.gz'
+  url "http://download.serviio.org/releases/serviio-#{version}-osx.tar.gz"
   homepage 'http://serviio.org/'
 
   app 'Serviio.app'
