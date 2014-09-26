6536af671430effb10fd1206590d49212f178b22
Roland Walker <walker@pobox.com>
2014-09-26 08:09:05 -0400

    re-use version in openlp

--- Casks/openlp.rb
+++ Casks/openlp.rb
@@ -2,7 +2,7 @@ class Openlp < Cask
   version '2.0.4'
   sha256 '24850da1e2d75b17d76cf102721d4c5e56d8faaa97ce1194577264c8edc6cc70'
 
-  url 'http://builds.openlp.org/OpenLP-2.0.4.dmg'
+  url "http://builds.openlp.org/OpenLP-#{version}.dmg"
   homepage 'http://openlp.org'
 
   app 'OpenLP.app'
