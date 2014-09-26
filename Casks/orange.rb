687e39e0c5a0009dcb6479d64ed66d47f7a74691
Roland Walker <walker@pobox.com>
2014-09-26 08:09:05 -0400

    re-use version in orange

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -2,7 +2,7 @@ class Orange < Cask
   version '2.7.6-11994'
   sha256 '3a46e41bfc3c6a5f799b47239d47a85a2815b466af3a770b7a1c2145dc8b7808'
 
-  url 'http://orange.biolab.si/download/Orange-2.7.6-11994.dmg'
+  url "http://orange.biolab.si/download/Orange-#{version}.dmg"
   homepage 'http://orange.biolab.si/'
 
   app 'Orange.app'
