9173f26880438c0b574ebfb42674556ec04e31c5
Roland Walker <walker@pobox.com>
2014-09-26 08:09:07 -0400

    re-use version in pokertracker

--- Casks/pokertracker.rb
+++ Casks/pokertracker.rb
@@ -2,7 +2,7 @@ class Pokertracker < Cask
   version '4.10.6'
   sha256 '913b47d2593dab9ecde628a754677acdeff2992e1f20d428ce6913f3a74af4d9'
 
-  url 'http://s3-us1.ptrackupdate.com/releases/PT-Install-v4.10.6.dmg'
+  url "http://s3-us1.ptrackupdate.com/releases/PT-Install-v#{version}.dmg"
   homepage 'https://www.pokertracker.com'
 
   app 'PokerTracker 4.app'
