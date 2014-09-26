ffec7d657f8b61628c6c23979a3d008f53af9ce5
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in yojimbo

--- Casks/yojimbo.rb
+++ Casks/yojimbo.rb
@@ -2,7 +2,7 @@ class Yojimbo < Cask
   version '4.0.3'
   sha256 '3d56523799db955ddf362e3b4b43a4a193b426ea76cf02d1647ecd56e5231c3b'
 
-  url 'https://s3.amazonaws.com/BBSW-download/Yojimbo_4.0.3.dmg'
+  url "https://s3.amazonaws.com/BBSW-download/Yojimbo_#{version}.dmg"
   homepage 'http://www.barebones.com/products/yojimbo/'
 
   app 'Yojimbo.app'
