de6ad8848ff2d74cad4950c0489b9f984fd8c73d
Roland Walker <walker@pobox.com>
2014-09-26 08:08:47 -0400

    re-use version in awareness

--- Casks/awareness.rb
+++ Casks/awareness.rb
@@ -2,7 +2,7 @@ class Awareness < Cask
   version '1.1'
   sha256 'a5ce31fef63aeabde3c8cabac367fb44868de318800b4fcd9032f05b17f74cdf'
 
-  url 'http://iamfutureproof.com/downloads/Awareness-1.1.dmg'
+  url "http://iamfutureproof.com/downloads/Awareness-#{version}.dmg"
   homepage 'http://iamfutureproof.com/tools/awareness/'
 
   app 'Awareness.app'
