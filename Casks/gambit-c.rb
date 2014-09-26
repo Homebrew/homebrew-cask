c745db60ce52ca4c20723ee131fc00ecac70cfa0
Roland Walker <walker@pobox.com>
2014-09-26 08:08:55 -0400

    re-use version in gambit-c

--- Casks/gambit-c.rb
+++ Casks/gambit-c.rb
@@ -1,4 +1,6 @@
 class GambitC < Cask
+  version '4.7.2'
+
   if Hardware::CPU.is_32_bit?
     url 'http://www.iro.umontreal.ca/~gambit/download/gambit/v4.7/prebuilt/gambc-v4_7_2-macosx-intel32.dmg'
     sha256 'fb19aceeeac5e7ce4ae7e1a07dbf8ab81906d372fbea56493171aa0bfe47899c'
@@ -8,10 +10,11 @@ class GambitC < Cask
     sha256 '2be0b846bb469fad9c4501efa3d34ed7aab6cc2d2fdc5acbe4eb39c4dc39b4bd'
     pkg 'gambc-v4_7_2-macosx-intel64.pkg'
   end
+
   homepage 'http://gambitscheme.org/wiki/index.php/Main_Page'
-  version '4.7.2'
+
   uninstall :script => {
-                        :executable => '/Library/Gambit-C/v4.7.2/bin/uninstall-gambc',
-                        :args => ['v4.7.2']
+                        :executable => "/Library/Gambit-C/v#{version}/bin/uninstall-gambc",
+                        :args => ["v#{version}"]
                        }
 end
