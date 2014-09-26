60fa29d1ab3fe2b6ad5275850501708840b21445
Roland Walker <walker@pobox.com>
2014-09-26 08:08:54 -0400

    re-use version in feedbinnotifier

--- Casks/feedbinnotifier.rb
+++ Casks/feedbinnotifier.rb
@@ -2,7 +2,7 @@ class Feedbinnotifier < Cask
   version '1.0.4'
   sha256 '20f94b5cecc2b730da1cfaae3633ce41f7b6ef6b28f56b7684538394544515f8'
 
-  url 'https://github.com/kmikael/FeedbinNotifier/releases/download/v1.0.4/FeedbinNotifier.zip'
+  url "https://github.com/kmikael/FeedbinNotifier/releases/download/v#{version}/FeedbinNotifier.zip"
   homepage 'http://kmikael.github.io/FeedbinNotifier'
 
   app 'FeedbinNotifier.app'
