f7374c8b941b882f965b5a10e27a29d544fc6400
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in bootxchanger

--- Casks/bootxchanger.rb
+++ Casks/bootxchanger.rb
@@ -2,7 +2,7 @@ class Bootxchanger < Cask
   version '2.0'
   sha256 'cfb05680ab6a7d0b1793a33135dd15562a7b5fd59bb1ebf3ad6067c2c9fad6c1'
 
-  url 'http://namedfork.net/_media/bootxchanger_2.0.dmg'
+  url "http://namedfork.net/_media/bootxchanger_#{version}.dmg"
   appcast 'http://swupdate.namedfork.net/bootxchanger.xml'
   homepage 'http://namedfork.net/bootxchanger'
 
