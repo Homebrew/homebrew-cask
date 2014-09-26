a24fe2782796f976a6ddebc15e06a867f47059a8
Roland Walker <walker@pobox.com>
2014-09-26 08:08:52 -0400

    re-use version in doublecommand

--- Casks/doublecommand.rb
+++ Casks/doublecommand.rb
@@ -2,9 +2,9 @@ class Doublecommand < Cask
   version '1.7'
   sha256 '312aaf1a60517c694b24131bf502945dc23a22c917971c3e7a3adca163560503'
 
-  url 'http://doublecommand.sourceforge.net/files/DoubleCommand-1.7.dmg'
+  url "http://doublecommand.sourceforge.net/files/DoubleCommand-#{version}.dmg"
   homepage 'http://doublecommand.sourceforge.net'
 
-  pkg 'DoubleCommand-1.7.pkg'
+  pkg "DoubleCommand-#{version}.pkg"
   uninstall :script => '/Library/StartupItems/DoubleCommand/uninstall.command'
 end
