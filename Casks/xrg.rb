4906bb6863742944cd7ac736f6f75c13f1ad24fa
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in xrg

--- Casks/xrg.rb
+++ Casks/xrg.rb
@@ -2,7 +2,7 @@ class Xrg < Cask
   version '1.7.3'
   sha256 '972ee45e202dee87aa5a696d2393f5eece21c8e6a54368ab6fefb6a371187063'
 
-  url 'http://download.gauchosoft.com/xrg/XRG-release-1.7.3.zip'
+  url "http://download.gauchosoft.com/xrg/XRG-release-#{version}.zip"
   homepage 'http://www.gauchosoft.com/Products/XRG/'
 
   app 'XRG.app'
