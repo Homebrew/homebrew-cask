f346dd29022f2e5b924363afcba03679a46cfde6
Roland Walker <walker@pobox.com>
2014-09-26 10:58:33 -0400

    re-use version in crypt

--- Casks/crypt.rb
+++ Casks/crypt.rb
@@ -2,8 +2,8 @@ class Crypt < Cask
   version '3'
   sha256 'e9e82ad331fe26a26ddb9625a9e1ac8e65086770c1abf4bfa503c8336bdf37bd'
 
-  url 'http://voluntary.net.s3.amazonaws.com/Crypt3_20100429.zip'
+  url "http://voluntary.net.s3.amazonaws.com/Crypt#{version}_20100429.zip"
   homepage 'http://voluntary.net/crypt/'
 
-  app 'Crypt3.app'
+  app "Crypt#{version}.app"
 end
