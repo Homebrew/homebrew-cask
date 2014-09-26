2bea0ac70a4d3cf2f31a97b8fb3313c0ec334c7a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:00 -0400

    re-use version in levelator

--- Casks/levelator.rb
+++ Casks/levelator.rb
@@ -2,7 +2,7 @@ class Levelator < Cask
   version '2.1.1'
   sha256 '2f22f1cf7851a987dcb194a504e43f8255261d428074ea7144e2683b79d6975d'
 
-  url 'http://cdn.conversationsnetwork.org/Levelator-2.1.1.dmg'
+  url "http://cdn.conversationsnetwork.org/Levelator-#{version}.dmg"
   homepage 'http://www.conversationsnetwork.org/levelator/'
 
   app 'Levelator.app'
