b2d4c914bbe52defc2c3b115c8282d0ec75237b5
Roland Walker <walker@pobox.com>
2014-09-26 08:08:55 -0400

    re-use version in ganttproject

--- Casks/ganttproject.rb
+++ Casks/ganttproject.rb
@@ -2,7 +2,7 @@ class Ganttproject < Cask
   version '2.6.2-r1544'
   sha256 '8db9b2da23c581f4907ec091db07b7d19e784f4fcbc7fd7cea03d5ab0de23f70'
 
-  url 'https://ganttproject.googlecode.com/files/ganttproject-2.6.2-r1544.dmg'
+  url "https://ganttproject.googlecode.com/files/ganttproject-#{version}.dmg"
   homepage 'http://www.ganttproject.biz'
 
   app 'GanttProject 2.6.2.app'
