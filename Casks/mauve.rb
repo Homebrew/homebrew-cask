3547314bd4c3710ea7e6105176ba4a1bc72ab310
Roland Walker <walker@pobox.com>
2014-09-26 08:09:02 -0400

    re-use version in mauve

--- Casks/mauve.rb
+++ Casks/mauve.rb
@@ -2,7 +2,7 @@ class Mauve < Cask
   version '2.3.1'
   sha256 '586fe214eb6c430f2a98fd865856111b79502ff526ab2f6268dab1d99b337907'
 
-  url 'http://gel.ahabs.wisc.edu/mauve/downloads/Mauve-2.3.1.dmg'
+  url "http://gel.ahabs.wisc.edu/mauve/downloads/Mauve-#{version}.dmg"
   homepage 'http://gel.ahabs.wisc.edu/mauve/'
 
   app 'Mauve.app'
