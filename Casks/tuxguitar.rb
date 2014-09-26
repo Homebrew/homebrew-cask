13714277a5a6684c8fd372765a12b2e162586b30
Roland Walker <walker@pobox.com>
2014-09-26 08:09:16 -0400

    re-use version in tuxguitar

--- Casks/tuxguitar.rb
+++ Casks/tuxguitar.rb
@@ -2,7 +2,7 @@ class Tuxguitar < Cask
   version '1.2'
   sha256 '2d79ffdfdde9205073fdaa1c34701ea8f1961f822709b5270dc57555eb926d16'
 
-  url 'https://downloads.sourceforge.net/project/tuxguitar/TuxGuitar/TuxGuitar-1.2/tuxguitar-1.2-macosx10.5-cocoa-64.dmg'
+  url "https://downloads.sourceforge.net/project/tuxguitar/TuxGuitar/TuxGuitar-#{version}/tuxguitar-#{version}-macosx10.5-cocoa-64.dmg"
   homepage 'http://www.tuxguitar.com.ar/'
 
   app 'Tuxguitar.app'
