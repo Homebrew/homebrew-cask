6eaf2c0b096a9bafa041605ef5837739356c4b2c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in tilemill

--- Casks/tilemill.rb
+++ Casks/tilemill.rb
@@ -2,7 +2,7 @@ class Tilemill < Cask
   version '0.10.1'
   sha256 '4d3c9c0a8f7b530d08598b8c245b128c982529a39127fab9dfabd6a2a92ba08f'
 
-  url 'https://tilemill.s3.amazonaws.com/latest/TileMill-0.10.1.zip'
+  url "https://tilemill.s3.amazonaws.com/latest/TileMill-#{version}.zip"
   appcast 'http://mapbox.com/tilemill/platforms/osx/appcast2.xml'
   homepage 'http://www.mapbox.com/tilemill/'
 
