df09b95aa509474c6058430e8139b5d03c16b6c5
Roland Walker <walker@pobox.com>
2014-09-26 08:08:53 -0400

    re-use version in electrum

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -2,7 +2,7 @@ class Electrum < Cask
   version '1.9.8'
   sha256 'ad3818e34a9b3a292257377e337603ed362ff928f2054e92f764d771f0e49ec0'
 
-  url 'https://download.electrum.org/electrum-1.9.8.dmg'
+  url "https://download.electrum.org/electrum-#{version}.dmg"
   homepage 'http://electrum.org/'
 
   app 'Electrum.app'
