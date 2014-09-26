f95c8f6575317ab74de0fcf6a3354be218ddd543
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in no-ip-duc

--- Casks/no-ip-duc.rb
+++ Casks/no-ip-duc.rb
@@ -2,7 +2,7 @@ class NoIpDuc < Cask
   version '3.2.1'
   sha256 'b59b159d1f9e2b64d6514577946b77f2c7f11767aa507a9c2bc2aa39ae9e4ffd'
 
-  url 'https://www.noip.com/client/mac/noip3.2.1.dmg'
+  url "https://www.noip.com/client/mac/noip#{version}.dmg"
   homepage 'http://www.noip.com/download?page=mac'
 
   app 'No-IP DUC.app'
