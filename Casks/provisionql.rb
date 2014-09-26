5f0882fb94021597a9bbe6a99958e79dabfc8c4a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:08 -0400

    re-use version in provisionql

--- Casks/provisionql.rb
+++ Casks/provisionql.rb
@@ -2,7 +2,7 @@ class Provisionql < Cask
   version '1.0.0'
   sha256 'fc21f2996050711a2496351092f3ffe88d9d017594ae913cce6ef059d1cf2c2a'
 
-  url 'https://github.com/ealeksandrov/ProvisionQL/releases/download/1.0.0/ProvisionQL.zip'
+  url "https://github.com/ealeksandrov/ProvisionQL/releases/download/#{version}/ProvisionQL.zip"
   homepage 'https://github.com/ealeksandrov/ProvisionQL'
 
   qlplugin 'ProvisionQL.qlgenerator'
