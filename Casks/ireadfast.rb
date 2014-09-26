e3824549757f6806f39e526d1fb18ce2f2e5e8ee
Roland Walker <walker@pobox.com>
2014-09-26 08:08:58 -0400

    re-use version in ireadfast

--- Casks/ireadfast.rb
+++ Casks/ireadfast.rb
@@ -2,7 +2,7 @@ class Ireadfast < Cask
   version '2.0'
   sha256 '0bc213c6da72a7917ceba8a9de46e307933608cd6d2bf397770517401ab3d98c'
 
-  url 'http://www.gengis.net/downloads/iReadFast%202.0.dmg'
+  url "http://www.gengis.net/downloads/iReadFast%20#{version}.dmg"
   homepage 'http://www.gengis.net/prodotti/iReadFast_Mac/en/index.php'
 
   app 'iReadFast.app'
