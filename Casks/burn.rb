5d6804b1015393309497fb35cf2c4b492d72d3c7
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in burn

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -2,7 +2,7 @@ class Burn < Cask
   version '2.5.1'
   sha256 'e82d2b7ef6a99e5a139706ff4e360659830d618ab8743e4b55ec80cdcdc97596'
 
-  url 'https://downloads.sourceforge.net/project/burn-osx/Burn/2.5.1/burn251.zip'
+  url "https://downloads.sourceforge.net/project/burn-osx/Burn/#{version}/burn251.zip"
   homepage 'http://burn-osx.sourceforge.net/'
 
   app 'Burn.localized/Burn.app'
