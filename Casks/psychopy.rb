e608fb77a016afe54340cc37d7a467c50bf62b6f
Roland Walker <walker@pobox.com>
2014-09-26 08:09:08 -0400

    re-use version in psychopy

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -2,7 +2,7 @@ class Psychopy < Cask
   version '1.79.01'
   sha256 'a2b174aee30ca4a8858238404adfd9271da9ec14243aa30be0b7f9a6abb047b8'
 
-  url 'https://downloads.sourceforge.net/sourceforge/psychpy/StandalonePsychoPy-1.79.01-OSX.dmg'
+  url "https://downloads.sourceforge.net/sourceforge/psychpy/StandalonePsychoPy-#{version}-OSX.dmg"
   homepage 'http://www.psychopy.org/'
 
   app 'PsychoPy2.app'
