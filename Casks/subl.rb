008751f12536b25d586d6d7a3a3adc0555e9396d
Roland Walker <walker@pobox.com>
2014-09-26 08:09:13 -0400

    re-use version in subl

--- Casks/subl.rb
+++ Casks/subl.rb
@@ -2,7 +2,7 @@ class Subl < Cask
   version '1.2'
   sha256 '2f66cd6f873b51fa7bf31a6fe0bff1af4ac2cf0d9fff21a4b0f647c1cdcd6eef'
 
-  url 'https://github.com/dhoulb/subl/releases/download/v1.2/Subl.app.zip'
+  url "https://github.com/dhoulb/subl/releases/download/v#{version}/Subl.app.zip"
   homepage 'https://github.com/dhoulb/subl'
 
   app 'Subl.app'
