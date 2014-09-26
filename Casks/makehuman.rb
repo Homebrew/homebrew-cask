7c8fa0284086abf507def1b51a2acbc4b18df902
Roland Walker <walker@pobox.com>
2014-09-26 08:09:01 -0400

    re-use version in makehuman

--- Casks/makehuman.rb
+++ Casks/makehuman.rb
@@ -2,7 +2,7 @@ class Makehuman < Cask
   version '1.0.1'
   sha256 'c3a4d693a23aff5e4f6dd93c0452d7d753703d48b9d3600d210c436b6dc756f4'
 
-  url 'http://download.tuxfamily.org/makehuman/releases/1.0.1/makehuman-1.0.1-osx.dmg'
+  url "http://download.tuxfamily.org/makehuman/releases/#{version}/makehuman-#{version}-osx.dmg"
   homepage 'http://www.makehuman.org/'
 
   app 'MakeHuman.app'
