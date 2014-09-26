e68ba0a95f9e6837cbbe1071d765e7afe264702a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in xbmc

--- Casks/xbmc.rb
+++ Casks/xbmc.rb
@@ -2,7 +2,7 @@ class Xbmc < Cask
   version '13.1'
   sha256 '5db5b0cdfe1b90048ac70ae2f2a539003327ad378ebcb3faf94533e33bea0f6f'
 
-  url 'http://mirrors.xbmc.org/releases/osx/x86_64/xbmc-13.1-Gotham-x86_64.dmg'
+  url "http://mirrors.xbmc.org/releases/osx/x86_64/xbmc-#{version}-Gotham-x86_64.dmg"
   homepage 'http://xbmc.org/'
 
   app 'XBMC.app'
