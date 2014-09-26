ef0a602645da55c3317325c046915338006d4e1d
Roland Walker <walker@pobox.com>
2014-09-26 08:08:52 -0400

    re-use version in djv

--- Casks/djv.rb
+++ Casks/djv.rb
@@ -2,8 +2,8 @@ class Djv < Cask
   version '0.9.0'
   sha256 '201a3220d6ddce30af49add669fc5e663edf4fdd458ff48b12ba24c2ad7a2a46'
 
-  url 'http://downloads.sourceforge.net/project/djv/djv-stable/0.9.0/djv-0.9.0_osx-x64.dmg'
+  url "http://downloads.sourceforge.net/project/djv/djv-stable/#{version}/djv-#{version}_osx-x64.dmg"
   homepage 'http://djv.sourceforge.net'
 
-  app 'djv-0.9.0.app'
+  app "djv-#{version}.app"
 end
