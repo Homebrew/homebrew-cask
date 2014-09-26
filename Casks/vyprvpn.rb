738aee63a4384bf6fffff4bdb13a3b731c585931
Roland Walker <walker@pobox.com>
2014-09-26 08:09:18 -0400

    re-use version in vyprvpn

--- Casks/vyprvpn.rb
+++ Casks/vyprvpn.rb
@@ -2,7 +2,7 @@ class Vyprvpn < Cask
   version '2.3.3.1851'
   sha256 '598967057d8e932bb4e2b332c0664ce31b708ceb36b89bae4a8c2a0c07ea03ec'
 
-  url 'https://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/2.3.3.1851/VyprVPN_v2.3.3.1851.dmg'
+  url "https://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/#{version}/VyprVPN_v#{version}.dmg"
   homepage 'http://www.goldenfrog.com/vyprvpn'
 
   app 'VyprVPN.app'
