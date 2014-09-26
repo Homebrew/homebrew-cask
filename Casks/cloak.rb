ad8ec750cd6c5c1a0c6e6d74806cc7ce79485304
Roland Walker <walker@pobox.com>
2014-09-26 08:08:49 -0400

    re-use version in cloak

--- Casks/cloak.rb
+++ Casks/cloak.rb
@@ -2,7 +2,7 @@ class Cloak < Cask
   version '2.0.7'
   sha256 '109b0d60d01fa2cf978bdf94e4108901816f689da95604c86ac7c520fda6fc92'
 
-  url 'https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-2.0.7.dmg'
+  url "https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-#{version}.dmg"
   homepage 'https://www.getcloak.com'
 
   app 'Cloak.app'
