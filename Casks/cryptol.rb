7b8791d4fbb84bdbd298186dcf9df4c86dedc78d
Roland Walker <walker@pobox.com>
2014-09-26 08:08:51 -0400

    re-use version in cryptol

--- Casks/cryptol.rb
+++ Casks/cryptol.rb
@@ -2,11 +2,11 @@ class Cryptol < Cask
   version '2.0.0'
   sha256 '19b3c24390ccb66584f42f34fed91cefc12f667879ab0228cdf806016bcd53c4'
 
-  url 'https://github.com/GaloisInc/cryptol/releases/download/v2.0.0/cryptol-2.0.0-MacOSX-64.tar.gz'
+  url "https://github.com/GaloisInc/cryptol/releases/download/v#{version}/cryptol-#{version}-MacOSX-64.tar.gz"
   homepage 'http://cryptol.net/'
 
-  binary 'cryptol-2.0.0-MacOSX-64/bin/cryptol'
-  binary 'cryptol-2.0.0-MacOSX-64/lib/Cryptol.cry', :target => '/usr/local/lib/Cryptol.cry'
+  binary "cryptol-#{version}-MacOSX-64/bin/cryptol"
+  binary "cryptol-#{version}-MacOSX-64/lib/Cryptol.cry", :target => '/usr/local/lib/Cryptol.cry'
 
   caveats do
     files_in_usr_local
