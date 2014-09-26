f485d07cf9cd7d1543f10271992e2f8c82e00345
Roland Walker <walker@pobox.com>
2014-09-26 08:08:47 -0400

    re-use version in autodmg

--- Casks/autodmg.rb
+++ Casks/autodmg.rb
@@ -2,7 +2,7 @@ class Autodmg < Cask
   version '1.4.3'
   sha256 '62233d162ccf2109bf0a03f1e74c7c74da323958ad1f13a0c06f18a611e44592'
 
-  url 'https://github.com/MagerValp/AutoDMG/releases/download/v1.4.3/AutoDMG-1.4.3.dmg'
+  url "https://github.com/MagerValp/AutoDMG/releases/download/v#{version}/AutoDMG-#{version}.dmg"
   homepage 'https://github.com/MagerValp/AutoDMG'
 
   app 'AutoDMG.app'
