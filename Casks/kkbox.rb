5a0560c2cf0579135b4236cfef8ab40c1b135b07
Roland Walker <walker@pobox.com>
2014-09-26 08:08:59 -0400

    re-use version in kkbox

--- Casks/kkbox.rb
+++ Casks/kkbox.rb
@@ -2,7 +2,7 @@ class Kkbox < Cask
   version '5.0.10'
   sha256 '0015c867479ab836504f973dab88f93bb3c4ce4e6e6c7287d7fdbfe3af565a50'
 
-  url 'http://download.kkbox.com/files/KKBOX-5.0.10-ca460d0.dmg'
+  url "http://download.kkbox.com/files/KKBOX-#{version}-ca460d0.dmg"
   homepage 'http://www.kkbox.com/'
 
   app 'KKBOX.app'
