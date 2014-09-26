5650927c6488dc76a8a12ff02a3a897203154dcc
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in thisservice

--- Casks/thisservice.rb
+++ Casks/thisservice.rb
@@ -2,7 +2,7 @@ class Thisservice < Cask
   version '3.0.1'
   sha256 'ddf9635421834f1d6bd9fb257ad164f7b59ec2d2b65590d691ec5d2699ec8da2'
 
-  url 'http://wafflesoftware.net/thisservice/download/ThisService3.0.1.zip'
+  url "http://wafflesoftware.net/thisservice/download/ThisService#{version}.zip"
   appcast 'http://wafflesoftware.net/thisservice/sparkle/sparkle.xml'
   homepage 'http://wafflesoftware.net/thisservice/'
 
