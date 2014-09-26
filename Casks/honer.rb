c80c4c01853ca3f94e0e99b3cd28d74eb7df0f7e
Roland Walker <walker@pobox.com>
2014-09-26 08:08:57 -0400

    re-use version in honer

--- Casks/honer.rb
+++ Casks/honer.rb
@@ -2,7 +2,7 @@ class Honer < Cask
   version '1.0'
   sha256 'bae10bea6875dbc5c948fb2e86b168c15ee3738d485bd7ab2a9758e64812c531'
 
-  url 'https://github.com/puffnfresh/Honer.app/releases/download/v1.0/Honer-6e3863f2.zip'
+  url "https://github.com/puffnfresh/Honer.app/releases/download/v#{version}/Honer-6e3863f2.zip"
   homepage 'https://github.com/puffnfresh/Honer.app'
 
   app 'Honer.app'
