5eece2277ef0e13986b7a8cb76db8dabc98867e1
Roland Walker <walker@pobox.com>
2014-09-26 08:09:20 -0400

    re-use version in zeroinstall

--- Casks/zeroinstall.rb
+++ Casks/zeroinstall.rb
@@ -2,7 +2,7 @@ class Zeroinstall < Cask
   version '2.7'
   sha256 '12246277e9d03fb1a083f6a0d7d088d7b90170dbd5fd8fb900b36d0470fb88e0'
 
-  url 'https://downloads.sourceforge.net/project/zero-install/0install/2.7/ZeroInstall.pkg'
+  url "https://downloads.sourceforge.net/project/zero-install/0install/#{version}/ZeroInstall.pkg"
   homepage 'http://0install.net'
 
   pkg 'ZeroInstall.pkg'
