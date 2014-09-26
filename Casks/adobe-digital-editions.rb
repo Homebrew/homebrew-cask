33e6f1d6de772e9d9895f7e67a4003df090caaea
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in adobe-digital-editions

--- Casks/adobe-digital-editions.rb
+++ Casks/adobe-digital-editions.rb
@@ -2,9 +2,9 @@ class AdobeDigitalEditions < Cask
   version '3.0'
   sha256 '540d0fdeeddd1aea5cf31a105b5f31b1c838219c4cf9baddfc02d729c098bb45'
 
-  url 'http://download.adobe.com/pub/adobe/digitaleditions/ADE_3.0_Installer.dmg'
+  url "http://download.adobe.com/pub/adobe/digitaleditions/ADE_#{version}_Installer.dmg"
   homepage 'http://www.adobe.com/pt/products/digital-editions.html'
 
-  pkg 'Digital Editions 3.0 Installer.pkg'
+  pkg "Digital Editions #{version} Installer.pkg"
   uninstall :pkgutil => 'com.adobe.adobedigitaleditions.app'
 end
