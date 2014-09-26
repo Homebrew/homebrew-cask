f4721a3b747172507688aa2f40acd23ebf6ab0d0
Roland Walker <walker@pobox.com>
2014-09-26 08:08:57 -0400

    re-use version in icefloor

--- Casks/icefloor.rb
+++ Casks/icefloor.rb
@@ -2,9 +2,9 @@ class Icefloor < Cask
   version '2.0.2'
   sha256 'b326bab20c022c12be9ed01c3aa0e5f072853ce7dc5fb756bddebc2c3abddf64'
 
-  url 'http://www.hanynet.com/icefloor-2.0.2.zip'
+  url "http://www.hanynet.com/icefloor-#{version}.zip"
   homepage 'http://www.hanynet.com/icefloor/'
 
-  nested_container 'IceFloor 2.0.2.dmg'
+  nested_container "IceFloor #{version}.dmg"
   app 'IceFloor.app'
 end
