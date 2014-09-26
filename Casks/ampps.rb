08f05065780bd9bc6b2c6575c4102b975a45054b
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in ampps

--- Casks/ampps.rb
+++ Casks/ampps.rb
@@ -2,7 +2,7 @@ class Ampps < Cask
   version '2.3'
   sha256 'ce7a1a7e71f0314edbc994e10fe4f6a80303c1999904ca1fb14d98c7477ae8e0'
 
-  url 'http://files.ampps.com/AMPPS-2.3.dmg'
+  url "http://files.ampps.com/AMPPS-#{version}.dmg"
   homepage 'http://www.ampps.com'
 
   suite 'AMPPS'
