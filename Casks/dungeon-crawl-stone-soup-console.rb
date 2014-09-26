2bc97d433acf184a36818ec481c71747fea9fbd3
Roland Walker <walker@pobox.com>
2014-09-26 08:08:53 -0400

    re-use version in dungeon-crawl-stone-soup-console

--- Casks/dungeon-crawl-stone-soup-console.rb
+++ Casks/dungeon-crawl-stone-soup-console.rb
@@ -2,7 +2,7 @@ class DungeonCrawlStoneSoupConsole < Cask
   version '0.14.0'
   sha256 'aea657c098a662f83dd227a2e3aa1f2f2c566bfea5076d686f601691538d298e'
 
-  url 'https://crawl.develz.org/release/stone_soup-0.14.0-console-macosx.zip'
+  url "https://crawl.develz.org/release/stone_soup-#{version}-console-macosx.zip"
   homepage 'http://crawl.develz.org'
 
   app 'Dungeon Crawl Stone Soup - Console.app'
