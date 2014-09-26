fbfe15fc6fc33c77f186374d46b6ef4f136265a3
Roland Walker <walker@pobox.com>
2014-09-26 08:08:51 -0400

    re-use version in curse-client

--- Casks/curse-client.rb
+++ Casks/curse-client.rb
@@ -2,7 +2,7 @@ class CurseClient < Cask
   version '4.0.0.425'
   sha256 'f76eda5d21b85ee4db080c794a3e852ecadaa71fe2d32308d698d95286f36a1f'
 
-  url 'http://addons.cursecdn.com/files/595/169/Curse_Client-4.0.0.425.dmg'
+  url "http://addons.cursecdn.com/files/595/169/Curse_Client-#{version}.dmg"
   appcast 'http://www.curseforge.com/projects/curse-client-osx/sparkle.xml?type=b'
   homepage 'http://www.curse.com/client'
 
