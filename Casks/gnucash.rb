57dbd2c9628d9538cd8ca271255df9754d717f0f
Roland Walker <walker@pobox.com>
2014-09-26 08:08:56 -0400

    re-use version in gnucash

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -2,7 +2,7 @@ class Gnucash < Cask
   version '2.6.3-1'
   sha256 '58d0a9c95cd1835eeafc0b15b965d7aac9e71980b2ba7a7aac5707445fa17828'
 
-  url 'https://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-2.6.3-1.dmg'
+  url "https://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-#{version}.dmg"
   homepage 'http://www.gnucash.org'
 
   app 'Gnucash.app'
