df4147b463657bf7f74570add8e2d699ebc46b13
Roland Walker <walker@pobox.com>
2014-09-26 08:08:54 -0400

    re-use version in firewall-builder

--- Casks/firewall-builder.rb
+++ Casks/firewall-builder.rb
@@ -2,8 +2,8 @@ class FirewallBuilder < Cask
   version '5.1.0.3599'
   sha256 '447e05956fdfe676e87a10ac2f81259d0e00f519bbf66da38e7f1b0c922f85f3'
 
-  url 'http://downloads.sourceforge.net/sourceforge/fwbuilder/fwbuilder-5.1.0.3599.dmg'
+  url "http://downloads.sourceforge.net/sourceforge/fwbuilder/fwbuilder-#{version}.dmg"
   homepage 'http://www.fwbuilder.org'
 
-  app 'fwbuilder-5.1.0.3599.app', :target => 'Firewall Builder.app'
+  app "fwbuilder-#{version}.app", :target => 'Firewall Builder.app'
 end
