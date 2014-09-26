94a7ff7a1725ca7b9cc1fb40e9417f40bebcdb7a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in x-moto

--- Casks/x-moto.rb
+++ Casks/x-moto.rb
@@ -2,7 +2,7 @@ class XMoto < Cask
   version '0.5.10'
   sha256 '0b032d21fb0644db252fe9210f9c0a3f465616996aad6d50b20ead2123a3507a'
 
-  url 'http://download.tuxfamily.org/xmoto/xmoto/0.5.10/xmoto-0.5.10-macosx.zip'
+  url "http://download.tuxfamily.org/xmoto/xmoto/#{version}/xmoto-#{version}-macosx.zip"
   homepage 'http://xmoto.tuxfamily.org'
 
   app 'X-Moto.app'
