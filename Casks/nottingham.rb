d46103673a7e1bf025e66db7a849b49929fadef0
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in nottingham

--- Casks/nottingham.rb
+++ Casks/nottingham.rb
@@ -2,7 +2,7 @@ class Nottingham < Cask
   version '2.1.3'
   sha256 'ccaceba0bc6566ba2850126ae2d456fd6d44b78122117b8e1f3e597ef812bd55'
 
-  url 'http://dl.clickontyler.com/nottingham/nottingham20_2.1.3.zip'
+  url "http://dl.clickontyler.com/nottingham/nottingham20_#{version}.zip"
   homepage 'https://clickontyler.com/nottingham/'
 
   app 'Nottingham.app'
