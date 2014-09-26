828e96181056d36fe8fe3d43ec2c8fefb864a801
Roland Walker <walker@pobox.com>
2014-09-26 08:09:08 -0400

    re-use version in putio-adder

--- Casks/putio-adder.rb
+++ Casks/putio-adder.rb
@@ -2,7 +2,7 @@ class PutioAdder < Cask
   version '2.7'
   sha256 'b4ac5fc97da0a8b83e56bd5cac0e1795ce7102aae3a69fb77fa64d9ed2f22c14'
 
-  url 'https://github.com/nicoSWD/put.io-adder/releases/download/v2.7/put.io-adder-v2.7.zip'
+  url "https://github.com/nicoSWD/put.io-adder/releases/download/v#{version}/put.io-adder-v#{version}.zip"
   homepage 'https://github.com/nicoSWD/put.io-adder'
 
   app 'put.io adder.app'
