e0f0760d6b93cb4ed7f9f3b3f3d9d68ce2ee9150
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in wyzo

--- Casks/wyzo.rb
+++ Casks/wyzo.rb
@@ -2,7 +2,7 @@ class Wyzo < Cask
   version '3.5.6'
   sha256 '18cda3c323544c729d7e532151a32c70d8427dae2762695229d102762b313026'
 
-  url 'http://download.wyzo.com/files/mac/WyzoSetup-3.5.6.dmg'
+  url "http://download.wyzo.com/files/mac/WyzoSetup-#{version}.dmg"
   homepage 'http://wyzo.com'
 
   app 'Wyzo.app'
