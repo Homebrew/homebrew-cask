daa8fd0b10663fd0b1b6a2ee74eb2657ae7e737a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:06 -0400

    re-use version in pgloader

--- Casks/pgloader.rb
+++ Casks/pgloader.rb
@@ -2,9 +2,9 @@ class Pgloader < Cask
   version '3.0.99'
   sha256 'df64c17a3b2aa62b9cdaab48c078e9a9ecb0580d660db036e11f1d63c4bee8ac'
 
-  url 'http://pgloader.io/files/pgloader-3.0.99.pkg'
+  url "http://pgloader.io/files/pgloader-#{version}.pkg"
   homepage 'http://pgloader.io'
 
-  pkg 'pgloader-3.0.99.pkg'
+  pkg "pgloader-#{version}.pkg"
   uninstall :pkgutil => 'org.tapoueh.pgloader'
 end
