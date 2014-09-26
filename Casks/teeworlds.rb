b158c008256d50d6148cb09af8006c987e867c7d
Roland Walker <walker@pobox.com>
2014-09-26 08:09:14 -0400

    re-use version in teeworlds

--- Casks/teeworlds.rb
+++ Casks/teeworlds.rb
@@ -2,7 +2,7 @@ class Teeworlds < Cask
   version '0.6.2'
   sha256 '954e8f213f22b0c47c529d6e4d92f5bc1008a6a1ee54229dc62e706ffd1cab64'
 
-  url 'https://teeworlds.com/files/teeworlds-0.6.2-osx.dmg'
+  url "https://teeworlds.com/files/teeworlds-#{version}-osx.dmg"
   homepage 'https://www.teeworlds.com/'
 
   app 'Teeworlds.app'
