64ab193fabb0e40425308f50b82483d62e4d25cc
Roland Walker <walker@pobox.com>
2014-09-26 08:08:49 -0400

    re-use version in catch

--- Casks/catch.rb
+++ Casks/catch.rb
@@ -2,7 +2,7 @@ class Catch < Cask
   version '1.8'
   sha256 '0fd58d79ba869583406b7aa9ed707e4c77480417da9f09805f1497098fbbb138'
 
-  url 'https://github.com/mipstian/catch/releases/download/1.8/Catch-1.8.zip'
+  url "https://github.com/mipstian/catch/releases/download/#{version}/Catch-#{version}.zip"
   appcast 'https://raw.github.com/mipstian/catch/master/update/appcast.xml'
   homepage 'http://www.giorgiocalderolla.com/index.html'
 
