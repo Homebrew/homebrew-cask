316a0defcdbc08f95c7201a59766cab538adc5a5
Roland Walker <walker@pobox.com>
2014-09-26 08:08:51 -0400

    re-use version in deltawalker

--- Casks/deltawalker.rb
+++ Casks/deltawalker.rb
@@ -2,7 +2,7 @@ class Deltawalker < Cask
   version '2.0.1'
   sha256 'eae3127fb05d84885c91e7ce5da5277aaaf128fd53225d1721d66456719b82fa'
 
-  url 'https://s3.amazonaws.com/deltawalker/DeltaWalker-2.0.1_64.dmg'
+  url "https://s3.amazonaws.com/deltawalker/DeltaWalker-#{version}_64.dmg"
   homepage 'http://www.deltopia.com/compare-merge-sync/macosx/'
 
   app 'DeltaWalker.app'
