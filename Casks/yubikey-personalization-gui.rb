31213c6c9e214a68a4a3bb0224c178b9105f1dc5
Roland Walker <walker@pobox.com>
2014-09-26 08:09:20 -0400

    re-use version in yubikey-personalization-gui

--- Casks/yubikey-personalization-gui.rb
+++ Casks/yubikey-personalization-gui.rb
@@ -2,9 +2,9 @@ class YubikeyPersonalizationGui < Cask
   version '3.1.14'
   sha256 'f0c3d016b90f54e69525d2fec4dee89df499862e4a8a1ad3323e0fca57287408'
 
-  url 'https://www.yubico.com/wp-content/uploads/2012/09/yubikey-personalization-gui-3.1.14.pkg'
+  url "https://www.yubico.com/wp-content/uploads/2012/09/yubikey-personalization-gui-#{version}.pkg"
   homepage 'http://www.yubico.com/products/services-software/personalization-tools/use/'
 
-  pkg 'yubikey-personalization-gui-3.1.14.pkg'
+  pkg "yubikey-personalization-gui-#{version}.pkg"
   uninstall :pkgutil => 'com.yubico.YKPersonalization'
 end
