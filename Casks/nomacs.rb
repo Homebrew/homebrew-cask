aa361b2a1cdb70b8c8de99b189d38dd58e11b3d7
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in nomacs

--- Casks/nomacs.rb
+++ Casks/nomacs.rb
@@ -2,7 +2,7 @@ class Nomacs < Cask
   version '1.0.0'
   sha256 '2ec4bfd6390ab7c4b2a929a1e9d14f46f6f168608bf2aee8da67af797f73a54a'
 
-  url 'https://downloads.sourceforge.net/nomacs/nomacs-1.0.0-x86_64.dmg'
+  url "https://downloads.sourceforge.net/nomacs/nomacs-#{version}-x86_64.dmg"
   homepage 'http://www.nomacs.org/'
 
   app 'nomacs.app'
