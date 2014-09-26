24edc50033c071d48065c3b4890f2ecf9abc8ca5
Roland Walker <walker@pobox.com>
2014-09-26 08:09:09 -0400

    re-use version in quickradar

--- Casks/quickradar.rb
+++ Casks/quickradar.rb
@@ -2,7 +2,7 @@ class Quickradar < Cask
   version '0.8.1'
   sha256 'c2a275634c9f07e9dbffe762256b8b7068562ce6ae92cfdb51fc23e5e43979e5'
 
-  url 'http://www.quickradar.com/0.8.1.zip'
+  url "http://www.quickradar.com/#{version}.zip"
   appcast 'http://www.quickradar.com/appcast.xml'
   homepage 'http://www.quickradar.com/'
 
