97be3b6d42a92a87dcd308a3bb03b9bac7b8f19c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:10 -0400

    re-use version in scilab

--- Casks/scilab.rb
+++ Casks/scilab.rb
@@ -2,8 +2,8 @@ class Scilab < Cask
   version '5.5.0'
   sha256 '58ed168011296c680db12cd4f3a55359647318cd959a9e97b37881ff5e26d592'
 
-  url 'http://www.scilab.org/download/5.5.0/scilab-5.5.0-x86_64.dmg'
+  url "http://www.scilab.org/download/#{version}/scilab-#{version}-x86_64.dmg"
   homepage 'https://www.scilab.org'
 
-  app 'scilab-5.5.0.app'
+  app "scilab-#{version}.app"
 end
