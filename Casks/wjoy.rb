9148fcf1815e776ad69db460af24b9d22824c371
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in wjoy

--- Casks/wjoy.rb
+++ Casks/wjoy.rb
@@ -2,7 +2,7 @@ class Wjoy < Cask
   version '0.7.1'
   sha256 '98a9f825634b09b901ad979a6cdda241a04a1cbd7dcd14b4b17b0bebe3f40ee1'
 
-  url 'https://wjoy.googlecode.com/files/wjoy%200.7.1.dmg'
+  url "https://wjoy.googlecode.com/files/wjoy%20#{version}.dmg"
   homepage 'https://code.google.com/p/wjoy/'
 
   app 'Wjoy.app'
