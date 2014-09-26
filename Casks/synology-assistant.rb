7ad7aa59d06669ead667ab5e35353ecfe1e49b16
Roland Walker <walker@pobox.com>
2014-09-26 08:09:14 -0400

    re-use version in synology-assistant

--- Casks/synology-assistant.rb
+++ Casks/synology-assistant.rb
@@ -2,7 +2,7 @@ class SynologyAssistant < Cask
   version '5.0-4448'
   sha256 'b4e23a43c2b11bd41596809fa84dfad1d17832140145ddf1b79cb8301f2f2014'
 
-  url 'https://global.download.synology.com/download/Tools/SynologyAssistant/4448/Mac/Synology-Assistant-5.0-4448.dmg'
+  url "https://global.download.synology.com/download/Tools/SynologyAssistant/4448/Mac/Synology-Assistant-#{version}.dmg"
   homepage 'http://www.synology.com/'
 
   app 'Synology Assistant.app'
