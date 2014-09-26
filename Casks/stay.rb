efc780224742b7c0ade19af2a2c845eb96ac3270
Roland Walker <walker@pobox.com>
2014-09-26 08:09:13 -0400

    re-use version in stay

--- Casks/stay.rb
+++ Casks/stay.rb
@@ -2,7 +2,7 @@ class Stay < Cask
   version '1.2.3'
   sha256 'afe5abc2757bcee547a0d9ad8ebae55744ac197bc28c9fa28fc7987b9b512464'
 
-  url 'https://cordlessdog.com/stay/versions/Stay%201.2.3.zip'
+  url "https://cordlessdog.com/stay/versions/Stay%20#{version}.zip"
   appcast 'http://cordlessdog.com/stay/appcast.xml'
   homepage 'http://cordlessdog.com/stay/'
 
