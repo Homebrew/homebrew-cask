bf9c9c6463c0556f59781e827bce13a5507ce4d2
Roland Walker <walker@pobox.com>
2014-09-26 08:08:54 -0400

    re-use version in fourk-video-downloader

--- Casks/fourk-video-downloader.rb
+++ Casks/fourk-video-downloader.rb
@@ -2,7 +2,7 @@ class FourkVideoDownloader < Cask
   version '3.1'
   sha256 '5f2243e9d2352b411414e3819d2dbb36cbd2c4f4d9a72a9579e4fbc1f820a324'
 
-  url 'http://downloads.4kdownload.com/app/4kvideodownloader_3.1.dmg'
+  url "http://downloads.4kdownload.com/app/4kvideodownloader_#{version}.dmg"
   homepage 'http://www.4kdownload.com/products/product-videodownloader'
 
   app '4K Video Downloader.app'
