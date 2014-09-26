dd2758b377015cf5c283dae778fadf0ec357b81c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:18 -0400

    re-use version in webp-quicklook

--- Casks/webp-quicklook.rb
+++ Casks/webp-quicklook.rb
@@ -2,7 +2,7 @@ class WebpQuicklook < Cask
   version '2.1'
   sha256 '9efbdd2b3dcb44d41a53c1cb6ce64397fd00bd399189b1b742d2f4b2de28f437'
 
-  url 'https://github.com/dchest/webp-quicklook/releases/download/v2.1/WebP-2.1.qlgenerator.zip'
+  url "https://github.com/dchest/webp-quicklook/releases/download/v#{version}/WebP-#{version}.qlgenerator.zip"
   homepage 'https://github.com/dchest/webp-quicklook'
 
   qlplugin 'WebP.qlgenerator'
