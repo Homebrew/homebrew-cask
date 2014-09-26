c6ad456e861d5f7813ff4fa2c910d7ffdbdf2854
Roland Walker <walker@pobox.com>
2014-09-26 08:08:49 -0400

    re-use version in calibre2opds

--- Casks/calibre2opds.rb
+++ Casks/calibre2opds.rb
@@ -2,7 +2,7 @@ class Calibre2opds < Cask
   version '3.3-246'
   sha256 '7a91b4cc5215ae337f0581e54db7c507ec7f463c87c5237e186168bf4fa9522d'
 
-  url 'https://calibre2opds.googlecode.com/files/calibre2opds-3.3-246.dmg'
+  url "https://calibre2opds.googlecode.com/files/calibre2opds-#{version}.dmg"
   homepage 'http://calibre2opds.com/'
 
   app 'Calibre2Opds.app'
