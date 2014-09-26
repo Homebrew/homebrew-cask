bad6a80f4bb706d6892edb6c19470dccd333e5d3
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in netnewswire

--- Casks/netnewswire.rb
+++ Casks/netnewswire.rb
@@ -2,7 +2,7 @@ class Netnewswire < Cask
   version '4.0.0-152'
   sha256 'f82539a4a53f6b3b05a6d63f459fee705bd28b83034bd0fc7e20be76e4592945'
 
-  url 'http://cdn.netnewswireapp.com/releases/NetNewsWire-4.0.0-152.zip'
+  url "http://cdn.netnewswireapp.com/releases/NetNewsWire-#{version}.zip"
   appcast 'https://updates.blackpixel.com/updates?app=nnw'
   homepage 'http://netnewswireapp.com/'
 
