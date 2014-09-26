107d8d5efacd2ddd91a9673ede47e8e0d3213dc3
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in nide

--- Casks/nide.rb
+++ Casks/nide.rb
@@ -2,7 +2,7 @@ class Nide < Cask
   version '0.2.0'
   sha256 '95a83ded032c26f0235924035efd1b345997b8fbb578d315099a918d03427b2b'
 
-  url 'https://github.com/downloads/Coreh/nide/nide-v0.2.0.dmg'
+  url "https://github.com/downloads/Coreh/nide/nide-v#{version}.dmg"
   homepage 'http://coreh.github.io/nide/'
 
   app 'Nide.app'
