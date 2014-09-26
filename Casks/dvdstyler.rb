54bf1bccf0fe2fc1af2d10aba003c9e8b745b682
Roland Walker <walker@pobox.com>
2014-09-26 08:08:53 -0400

    re-use version in dvdstyler

--- Casks/dvdstyler.rb
+++ Casks/dvdstyler.rb
@@ -2,7 +2,7 @@ class Dvdstyler < Cask
   version '2.6'
   sha256 'fbb26aeb2ee50890b4a192d06d16ddc8a98a8465aa4eeff75d16fdc4de375ed6'
 
-  url 'https://downloads.sourceforge.net/sourceforge/dvdstyler/DVDStyler-2.6-MacOSX.zip'
+  url "https://downloads.sourceforge.net/sourceforge/dvdstyler/DVDStyler-#{version}-MacOSX.zip"
   homepage 'http://dvdstyler.org'
 
   app 'DVDStyler.app'
