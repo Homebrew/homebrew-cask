30811cce33527d7124918af0b1b0c891d783e461
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in aquaterm

--- Casks/aquaterm.rb
+++ Casks/aquaterm.rb
@@ -2,7 +2,7 @@ class Aquaterm < Cask
   version '1.1.1'
   sha256 '94b33efea2ec037e6c06beef54b4b3cc48595453c874de863f25c26b3a7ffdb2'
 
-  url 'https://downloads.sourceforge.net/project/aquaterm/AquaTerm/v1.1.1/AquaTerm-1.1.1.dmg'
+  url "https://downloads.sourceforge.net/project/aquaterm/AquaTerm/v#{version}/AquaTerm-#{version}.dmg"
   homepage 'http://aquaterm.sourceforge.net/'
 
   pkg 'AquaTermInstaller.pkg'
