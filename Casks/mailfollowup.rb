156f439c0a325d6c3f54a7c314b5cf15625582bf
Roland Walker <walker@pobox.com>
2014-09-26 08:09:01 -0400

    re-use version in mailfollowup

--- Casks/mailfollowup.rb
+++ Casks/mailfollowup.rb
@@ -2,10 +2,10 @@ class Mailfollowup < Cask
   version '1.6.2'
   sha256 'f656c121e04fac6185b24a96888613729c366275151c891258b634c71372b2af'
 
-  url 'https://www.cs.unc.edu/~welch/MailFollowup/media/MailFollowUp_1.6.2.dmg.zip'
+  url "https://www.cs.unc.edu/~welch/MailFollowup/media/MailFollowUp_#{version}.dmg.zip"
   homepage 'http://www.cs.unc.edu/~welch/MailFollowup/'
 
-  nested_container 'MailFollowUp_1.6.2.dmg'
+  nested_container "MailFollowUp_#{version}.dmg"
   pkg 'Install MailFollowUp.pkg'
 
   uninstall :quit  => 'com.apple.mail'
