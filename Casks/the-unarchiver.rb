6c80c9e3431621ddd3c2e17f8e4811fc83b19a58
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in the-unarchiver

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -2,7 +2,7 @@ class TheUnarchiver < Cask
   version '3.9.1'
   sha256 '34fa3410237e17b2cdceb801a84ed8db93c74ac0db551ffe65913c2134ebbf05'
 
-  url 'https://theunarchiver.googlecode.com/files/TheUnarchiver3.9.1.zip'
+  url "https://theunarchiver.googlecode.com/files/TheUnarchiver#{version}.zip"
   homepage 'http://unarchiver.c3.cx/'
 
   app 'The Unarchiver.app'
