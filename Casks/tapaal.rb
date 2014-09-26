23ef00ddacac0295bced34b21c6e7b972f467591
Roland Walker <walker@pobox.com>
2014-09-26 08:09:14 -0400

    re-use version in tapaal

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -2,7 +2,7 @@ class Tapaal < Cask
   version '3.1.0'
   sha256 '62314055445808d5ae2074623625c9ac38174aa294abd0d43590fe886c16fc46'
 
-  url 'http://www.tapaal.net/fileadmin/download/tapaal-3.1/tapaal-3.1.0-mac64.dmg'
+  url "http://www.tapaal.net/fileadmin/download/tapaal-3.1/tapaal-#{version}-mac64.dmg"
   homepage 'http://www.tapaal.net'
 
   app 'Tapaal.app'
