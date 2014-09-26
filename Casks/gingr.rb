8b3e236ed133b968415768a7e18bad2e32a6ffd7
Roland Walker <walker@pobox.com>
2014-09-26 08:08:55 -0400

    re-use version in gingr

--- Casks/gingr.rb
+++ Casks/gingr.rb
@@ -2,7 +2,7 @@ class Gingr < Cask
   version '1.0.1'
   sha256 '29a705836ae11114f4e9aa787c885e1960861cc9119c23afda22ea8618322498'
 
-  url "https://github.com/marbl/gingr/releases/download/v#{version}/gingr-OSX64-v1.0.1.zip"
+  url "https://github.com/marbl/gingr/releases/download/v#{version}/gingr-OSX64-v#{version}.zip"
   homepage 'http://harvest.readthedocs.org/en/latest/content/gingr.html'
 
   app 'gingr-OSX64.app', :target => 'Gingr.app'
