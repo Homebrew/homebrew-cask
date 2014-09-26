34f96b25a4b848ab1f74b6a70f83fc5ef8c07320
Roland Walker <walker@pobox.com>
2014-09-26 08:08:53 -0400

    re-use version in escritorio-movistar

--- Casks/escritorio-movistar.rb
+++ Casks/escritorio-movistar.rb
@@ -2,10 +2,10 @@ class EscritorioMovistar < Cask
   version '8.9'
   sha256 '0709299be3faf37e99497ffdf6668c6f0f90cb335fa86e797f8a04548a65689b'
 
-  url 'http://www.movistar.es/estaticos/descargaaplicaciones/Escritorio%20Movistar_v8.9.pkg'
+  url "http://www.movistar.es/estaticos/descargaaplicaciones/Escritorio%20Movistar_v#{version}.pkg"
   homepage 'http://www.movistar.es/particulares/servicios/descargaaplicaciones'
 
-  pkg 'Escritorio Movistar_v8.9.pkg'
+  pkg "Escritorio Movistar_v#{version}.pkg"
   uninstall :quit => 'com.movistar.tgcm.devel.tme',
             :pkgutil => [
                           'com.ZTE.DatacardDriver.MacV1.3.41NDISICAEM1000.01.02',
