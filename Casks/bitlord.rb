cf82e78f63872b0d5f1ef2fa2894a910397f4f7c
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in bitlord

--- Casks/bitlord.rb
+++ Casks/bitlord.rb
@@ -2,7 +2,7 @@ class Bitlord < Cask
   version '2.3.2'
   sha256 'd9af26b1626fd471550fbc3374158b09ae7c129bd3ab17ffbdf559d813e42af1'
 
-  url 'http://www.bitlord.com/osx/BitLord-Lion-2.3.2.dmg'
+  url "http://www.bitlord.com/osx/BitLord-Lion-#{version}.dmg"
   homepage 'http://www.bitlord.com'
 
   app 'BitLord.app'
