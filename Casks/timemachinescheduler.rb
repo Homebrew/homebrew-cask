99ed76d42c748c7a4ab6a1b625ddde11d33e8bc1
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in timemachinescheduler

--- Casks/timemachinescheduler.rb
+++ Casks/timemachinescheduler.rb
@@ -2,7 +2,7 @@ class Timemachinescheduler < Cask
   version '3.1.4'
   sha256 '22b0976139c42d163aa7d905d845594af41d4f3d4254a21b334f0232b3815f0a'
 
-  url 'http://www.klieme.com/Downloads/TimeMachineScheduler/TimeMachineScheduler_3.1.4.zip'
+  url "http://www.klieme.com/Downloads/TimeMachineScheduler/TimeMachineScheduler_#{version}.zip"
   homepage 'http://www.klieme.com/TimeMachineScheduler.html'
 
   nested_container 'TimeMachineScheduler3_Installer.dmg'
