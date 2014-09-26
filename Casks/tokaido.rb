0523e300831bafe983994a91513391070413b460
Roland Walker <walker@pobox.com>
2014-09-26 08:09:16 -0400

    re-use version in tokaido

--- Casks/tokaido.rb
+++ Casks/tokaido.rb
@@ -2,7 +2,7 @@ class Tokaido  < Cask
   version '1.0'
   sha256 '72752b8e4e9f0e554661d8fb58879a82cac6dc5d19762367c89ca191f497d258'
 
-  url 'https://github.com/tokaido/tokaidoapp/releases/download/v1.0/Tokaido.zip'
+  url "https://github.com/tokaido/tokaidoapp/releases/download/v#{version}/Tokaido.zip"
   homepage 'https://github.com/tokaido/tokaidoapp/releases'
 
   app 'Tokaido.app'
