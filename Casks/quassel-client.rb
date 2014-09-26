ee22ddad2b0ba5204ce3aa11a82a1b56a97d5081
Roland Walker <walker@pobox.com>
2014-09-26 08:09:09 -0400

    re-use version in quassel-client

--- Casks/quassel-client.rb
+++ Casks/quassel-client.rb
@@ -2,7 +2,7 @@ class QuasselClient < Cask
   version '0.10.0'
   sha256 '240b6c377efa38bb2d1d37fe9dd707b7376ad87e29c8e25130749adb78d9ddb4'
 
-  url 'http://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_0.10.0.dmg'
+  url "http://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_#{version}.dmg"
   homepage 'http://quassel-irc.org'
 
   app 'Quassel Client.app'
