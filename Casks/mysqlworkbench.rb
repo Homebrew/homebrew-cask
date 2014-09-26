7c01c038704918a7428602302bb4518b7fb6e3b7
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in mysqlworkbench

--- Casks/mysqlworkbench.rb
+++ Casks/mysqlworkbench.rb
@@ -2,7 +2,7 @@ class Mysqlworkbench < Cask
   version '6.1.7'
   sha256 'b5c5d35df4b8099db639622aa49e22c51eb5127858cfa0d41339967554564e08'
 
-  url 'https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-6.1.7-osx-i686.dmg'
+  url "https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community-#{version}-osx-i686.dmg"
   homepage 'http://www.mysql.com/products/workbench'
 
   app 'MySQLWorkbench.app'
