29641b830110f49211b2e46bb4e078d0f726c0c5
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in thyme

--- Casks/thyme.rb
+++ Casks/thyme.rb
@@ -2,7 +2,7 @@ class Thyme < Cask
   version '0.4.2'
   sha256 '5963d199ff9d64f3cc6966ed532b949fa500c9887d391aef8eedba68ae469a45'
 
-  url 'https://joaomoreno.github.io/thyme/dist/Thyme-0.4.2.dmg'
+  url "https://joaomoreno.github.io/thyme/dist/Thyme-#{version}.dmg"
   homepage 'http://joaomoreno.github.io/thyme/'
 
   app 'Thyme.app'
