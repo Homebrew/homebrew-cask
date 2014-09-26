39bb558b1904265e81f58a945e6ab188c343fb1a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:08 -0400

    re-use version in qqmusic

--- Casks/qqmusic.rb
+++ Casks/qqmusic.rb
@@ -3,7 +3,7 @@ class Qqmusic < Cask
   version '1.3.0'
   sha256 '2f1198f9b3e1407822a771fcdfdd643b65f35f6b51cc0af8c6b11fa11fc30a0d'
 
-  url 'http://dldir1.qq.com/music/clntupate/QQMusicForMacV1.3.0.dmg'
+  url "http://dldir1.qq.com/music/clntupate/QQMusicForMacV#{version}.dmg"
   homepage 'http://y.qq.com'
 
   app 'QQ音乐.app'
