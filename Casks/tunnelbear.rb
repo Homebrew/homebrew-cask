e2dd5075dab5d97f3596813af5e25408ad6109c1
Roland Walker <walker@pobox.com>
2014-09-26 08:09:16 -0400

    re-use version in tunnelbear

--- Casks/tunnelbear.rb
+++ Casks/tunnelbear.rb
@@ -2,7 +2,7 @@ class Tunnelbear < Cask
   version '2.4.2'
   sha256 'ad53bff32098e6871b3c3416b632c634672500b415a54d5afbbe7ea4f605d33d'
 
-  url 'https://tunnelbear.s3.amazonaws.com/downloads/mac/TunnelBear-2.4.2.zip'
+  url "https://tunnelbear.s3.amazonaws.com/downloads/mac/TunnelBear-#{version}.zip"
   appcast 'https://s3.amazonaws.com/tunnelbear/downloads/mac/appcast.xml'
   homepage 'https://www.tunnelbear.com/'
 
