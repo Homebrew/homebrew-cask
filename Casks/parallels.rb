d02ec2c5400002099fc811b86725d79e95da1edd
Roland Walker <walker@pobox.com>
2014-09-26 08:09:06 -0400

    re-use version in parallels

--- Casks/parallels.rb
+++ Casks/parallels.rb
@@ -2,7 +2,7 @@ class Parallels < Cask
   version '9.0.24237.1028877'
   sha256 'da71645ff1f0076ab5b2f8fa5eefb63bcd921e5f52161fd4dd85e6fb19ae2c57'
 
-  url 'http://download.parallels.com/desktop/v9/update2.hotfix2/ParallelsDesktop-9.0.24237.1028877.dmg'
+  url "http://download.parallels.com/desktop/v9/update2.hotfix2/ParallelsDesktop-#{version}.dmg"
   homepage 'http://www.parallels.com/products/desktop/'
 
   pkg 'Install.mpkg'
