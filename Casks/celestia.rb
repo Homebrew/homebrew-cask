9bb8a978a84cb293ca572ea0d244e96635e59ce5
Roland Walker <walker@pobox.com>
2014-09-26 08:08:49 -0400

    re-use version in celestia

--- Casks/celestia.rb
+++ Casks/celestia.rb
@@ -2,7 +2,7 @@ class Celestia < Cask
   version '1.6.1'
   sha256 'db09842a10b377038bedef87fda8d159549234b2e072fa22e096ade84aa3f52a'
 
-  url 'https://downloads.sourceforge.net/sourceforge/celestia/celestia-osx-1.6.1.dmg'
+  url "https://downloads.sourceforge.net/sourceforge/celestia/celestia-osx-#{version}.dmg"
   homepage 'http://sourceforge.net/projects/celestia'
 
   app 'Celestia.app'
