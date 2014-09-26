0b2c8fffdda0223b153eea6608c3322a03065dd7
Roland Walker <walker@pobox.com>
2014-09-26 10:28:01 -0400

    whitespace nits in datagraph

--- Casks/datagraph.rb
+++ Casks/datagraph.rb
@@ -1,6 +1,6 @@
 class Datagraph < Cask
   version :latest
-  sha256 :no_check 
+  sha256 :no_check
 
   url 'http://www.visualdatatools.com/DataGraph/DataGraph.dmg'
   homepage 'http://www.visualdatatools.com/DataGraph/'
