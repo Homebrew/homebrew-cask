384e320b34ea78639007b8e80995ae125c17747a
Roland Walker <walker@pobox.com>
2014-09-26 08:08:53 -0400

    re-use version in electric-sheep

--- Casks/electric-sheep.rb
+++ Casks/electric-sheep.rb
@@ -2,7 +2,7 @@ class ElectricSheep < Cask
   version '2.7b35b'
   sha256 '767062403cc93aa75192def84b509b4530caca3e271ba23267ffe79cb50da44f'
 
-  url 'https://electricsheep.googlecode.com/files/electricsheep-2.7b35b.dmg'
+  url "https://electricsheep.googlecode.com/files/electricsheep-#{version}.dmg"
   homepage 'http://www.electricsheep.org'
 
   pkg 'Electric Sheep.mpkg'
