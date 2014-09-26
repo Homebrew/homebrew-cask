5d93029778fa4ab11c7af024bf08029d1e7da3dc
Roland Walker <walker@pobox.com>
2014-09-26 08:08:52 -0400

    re-use version in dragthing

--- Casks/dragthing.rb
+++ Casks/dragthing.rb
@@ -2,7 +2,7 @@ class Dragthing < Cask
   version '5.9.12'
   sha256 '4a351c593aff1c3214613d622a4e81f184e8ae238df6db921dd822efeefe27e6'
 
-  url 'https://s3.amazonaws.com/tlasystems/DragThing-5.9.12.dmg'
+  url "https://s3.amazonaws.com/tlasystems/DragThing-#{version}.dmg"
   homepage 'http://www.dragthing.com'
 
   app 'DragThing.app'
