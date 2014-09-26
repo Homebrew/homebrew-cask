035303a6ca95002aa7372236a2103c6f6e27374c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:20 -0400

    re-use version in zooom

--- Casks/zooom.rb
+++ Casks/zooom.rb
@@ -2,7 +2,7 @@ class Zooom < Cask
   version '2.6.0'
   sha256 'eda52f42d06a6cd32c2fc419358a4a589087a1592a38e717577e3ae9a656036f'
 
-  url 'http://software.coderage-software.com/zooom/Zooom_2.6.0.dmg'
+  url "http://software.coderage-software.com/zooom/Zooom_#{version}.dmg"
   homepage 'http://coderage-software.com/zooom'
 
   pkg 'Zooom2.pkg'
