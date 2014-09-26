77019abb57e33f4147bdfbe6026da9ea4b88ee7a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:11 -0400

    re-use version in shadowsocksx

--- Casks/shadowsocksx.rb
+++ Casks/shadowsocksx.rb
@@ -2,7 +2,7 @@ class Shadowsocksx < Cask
   version '2.2'
   sha256 '8cedc4fba2e1c64ca110c57edcc435f3006b8d5567966685b97c429c8f7c844e'
 
-  url 'https://downloads.sourceforge.net/project/shadowsocksgui/dist/ShadowsocksX-2.2.dmg'
+  url "https://downloads.sourceforge.net/project/shadowsocksgui/dist/ShadowsocksX-#{version}.dmg"
   homepage 'https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help'
 
   app 'ShadowsocksX.app'
