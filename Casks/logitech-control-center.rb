631704990835c57bfbbb88eb395ee724fe5d024f
Roland Walker <walker@pobox.com>
2014-09-26 08:09:00 -0400

    re-use version in logitech-control-center

--- Casks/logitech-control-center.rb
+++ Casks/logitech-control-center.rb
@@ -2,7 +2,7 @@ class LogitechControlCenter < Cask
   version '3.9.0.60'
   sha256 '1eab6118dc5ad0b3c790b9132b5968050dab0117b07d8f338c471aff00078df1'
 
-  url 'http://www.logitech.com/pub/techsupport/mouse/mac/lcc3.9.0.60.zip'
+  url "http://www.logitech.com/pub/techsupport/mouse/mac/lcc#{version}.zip"
   homepage 'http://www.logitech.com'
 
   pkg 'LCC Installer.app/Contents/Resources/Logitech Control Center.mpkg'
