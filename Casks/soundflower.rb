dcd91ca9c3c6bf5fe97ec7dedae20d9ddf31008c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:12 -0400

    re-use version in soundflower

--- Casks/soundflower.rb
+++ Casks/soundflower.rb
@@ -2,7 +2,7 @@ class Soundflower < Cask
   version '1.6.6b'
   sha256 '61ca31d7478d057e32caaeac3c739f965ba9eb2a27570b3cc715e706d4740dfb'
 
-  url 'https://soundflower.googlecode.com/files/Soundflower-1.6.6b.dmg'
+  url "https://soundflower.googlecode.com/files/Soundflower-#{version}.dmg"
   homepage 'https://code.google.com/p/soundflower/'
 
   pkg 'Soundflower.pkg', :allow_untrusted => true
