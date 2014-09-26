36908ef0cfeadef3b7a825a30396d1b7c30d6a8c
Roland Walker <walker@pobox.com>
2014-09-26 08:08:57 -0400

    re-use version in ibackup

--- Casks/ibackup.rb
+++ Casks/ibackup.rb
@@ -2,7 +2,7 @@ class Ibackup < Cask
   version '7.5'
   sha256 'faa69e95f62dfeb04a6ede20e7b856d43645f1bd07aede743c5d364d2845aabf'
 
-  url 'http://www.grapefruit.ch/iBackup/versions/iBackup%207.x/iBackup%207.5.dmg'
+  url "http://www.grapefruit.ch/iBackup/versions/iBackup%207.x/iBackup%20#{version}.dmg"
   homepage 'http://www.grapefruit.ch/iBackup'
 
   app 'iBackup.app'
