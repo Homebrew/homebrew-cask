4473426fd8386decda9cc88a7cc5da1ac4f47b2b
Nathan Green <nathan.green@vibriant.net>
2014-09-26 14:20:43 -0500

    Upgrade to 3.0.1

--- Casks/dbeaver.rb
+++ Casks/dbeaver.rb
@@ -1,6 +1,6 @@
 class Dbeaver < Cask
-  version '2.4.2'
-  sha256 'bc9255302f1770b1706465f87191249303532695901a731245f96764708435a6'
+  version '3.0.1'
+  sha256 '0e2e7e403c8c0c5f6b2291e8a2bdfba5730dffa9a2fb363c1c98e60e868412d7'
 
   url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-macosx.cocoa.x86_64.zip"
   homepage 'http://dbeaver.jkiss.org/'
