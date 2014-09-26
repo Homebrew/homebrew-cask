ddc1e8bed6e3b4e27bcea9283f23be7fd67ecf43
Roland Walker <walker@pobox.com>
2014-09-26 08:08:52 -0400

    re-use version in double-commander

--- Casks/double-commander.rb
+++ Casks/double-commander.rb
@@ -2,7 +2,7 @@ class DoubleCommander < Cask
   version '0.5.8'
   sha256 'a4bf455f028783a4c14d600068d27556984bfc3357a80dc0715f047efbf6dbfe'
 
-  url 'https://downloads.sourceforge.net/sourceforge/doublecmd/doublecmd-0.5.8-5390.qt.x86_64.dmg'
+  url "https://downloads.sourceforge.net/sourceforge/doublecmd/doublecmd-#{version}-5390.qt.x86_64.dmg"
   homepage 'http://doublecmd.sourceforge.net/'
 
   app 'Double Commander.app'
