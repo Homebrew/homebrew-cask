a4e2caf9e5a96564d4be0f612f387fb03a2f6022
Roland Walker <walker@pobox.com>
2014-09-26 08:09:09 -0400

    re-use version in rdm

--- Casks/rdm.rb
+++ Casks/rdm.rb
@@ -2,7 +2,7 @@ class Rdm < Cask
   version '0.7.6.8'
   sha256 '7f5bb2ac47c7a01f99a5d3fd7ed1019d7cc9fb6230c617bf2eff9336f0eb8a16'
 
-  url 'https://github.com/uglide/RedisDesktopManager/releases/download/0.7.6/redis-desktop-manager-0.7.6.8.dmg'
+  url "https://github.com/uglide/RedisDesktopManager/releases/download/0.7.6/redis-desktop-manager-#{version}.dmg"
   homepage 'http://redisdesktop.com'
 
   app 'rdm.app'
