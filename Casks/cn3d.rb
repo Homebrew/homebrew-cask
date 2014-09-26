3a5e72eefed3b284dd0805f206e7309ee01619bf
Roland Walker <walker@pobox.com>
2014-09-26 08:08:50 -0400

    re-use version in cn3d

--- Casks/cn3d.rb
+++ Casks/cn3d.rb
@@ -2,7 +2,7 @@ class Cn3d < Cask
   version '4.3.1'
   sha256 '0d47a1ec7ee2e445d997eee2caa56f9cdffd68cba0384f5dd71edc00d8812df4'
 
-  url 'ftp://ftp.ncbi.nlm.nih.gov/cn3d/Cn3D-4.3.1-OSX.zip'
+  url "ftp://ftp.ncbi.nlm.nih.gov/cn3d/Cn3D-#{version}-OSX.zip"
   homepage 'http://www.ncbi.nlm.nih.gov/Structure/CN3D/cn3d.shtml'
 
   app 'Cn3D.app'
