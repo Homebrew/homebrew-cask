d6bb350e896ba73f2874e7581fa4d7854aa086ab
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in brain-workshop

--- Casks/brain-workshop.rb
+++ Casks/brain-workshop.rb
@@ -2,7 +2,7 @@ class BrainWorkshop < Cask
   version '4.8.4'
   sha256 'b795220306862f55e79e3de9c8cf8b4e8667e7bb1b0e3f59604b4d5f29f3f623'
 
-  url 'https://downloads.sourceforge.net/project/brainworkshop/brainworkshop/Brain%20Workshop%204.8/brainworkshop-4.8.4-MacOSX.zip'
+  url "https://downloads.sourceforge.net/project/brainworkshop/brainworkshop/Brain%20Workshop%204.8/brainworkshop-#{version}-MacOSX.zip"
   homepage 'http://brainworkshop.sourceforge.net/'
 
   app 'Brain Workshop.app'
