475c840d6bacca2e4d8117a6b4622e954540e646
Roland Walker <walker@pobox.com>
2014-09-26 08:09:01 -0400

    re-use version in lucidor

--- Casks/lucidor.rb
+++ Casks/lucidor.rb
@@ -2,7 +2,7 @@ class Lucidor < Cask
   version '0.9.8-1'
   sha256 'b94cca22b82ba508a8a9bd5ced62ec3ff093e74c1bf2841a0962c3bba1306a8d'
 
-  url 'http://lucidor.org/lucidor/lucidor-0.9.8-1.dmg'
+  url "http://lucidor.org/lucidor/lucidor-#{version}.dmg"
   homepage 'http://lucidor.org'
 
   app 'Lucidor.app'
