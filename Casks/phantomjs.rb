99b53f00d03aa84c21bd026e3f08ae415f690ba3
Roland Walker <walker@pobox.com>
2014-09-26 08:09:06 -0400

    re-use version in phantomjs

--- Casks/phantomjs.rb
+++ Casks/phantomjs.rb
@@ -2,8 +2,8 @@ class Phantomjs < Cask
   version '1.9.7'
   sha256 '72731f8ff68db17ecb5f6c78bf036adb429317b9bdbe69e2f5f60514fa7e4a6f'
 
-  url 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-macosx.zip'
+  url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-#{version}-macosx.zip"
   homepage 'http://phantomjs.org/'
 
-  binary 'phantomjs-1.9.7-macosx/bin/phantomjs'
+  binary "phantomjs-#{version}-macosx/bin/phantomjs"
 end
