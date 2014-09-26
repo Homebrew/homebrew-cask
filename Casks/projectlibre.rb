3ea8d4de3dbfeb3cb26a5c7d9fc900e20171000c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:07 -0400

    re-use version in projectlibre

--- Casks/projectlibre.rb
+++ Casks/projectlibre.rb
@@ -2,7 +2,7 @@ class Projectlibre < Cask
   version '1.5.8'
   sha256 'efd7d228d2e71a2a5af185d2fe1ddcea4abc55af32188344e06e1871facd0c6b'
 
-  url 'https://downloads.sourceforge.net/project/projectlibre/ProjectLibre/1.5.8/projectlibre-1.5.8.dmg'
+  url "https://downloads.sourceforge.net/project/projectlibre/ProjectLibre/#{version}/projectlibre-#{version}.dmg"
   homepage 'http://www.projectlibre.org/'
 
   app 'ProjectLibre.app'
