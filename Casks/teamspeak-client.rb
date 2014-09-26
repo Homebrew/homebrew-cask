b83b89caf886164d03ee0e38d946f374f33100f1
Roland Walker <walker@pobox.com>
2014-09-26 08:09:14 -0400

    re-use version in teamspeak-client

--- Casks/teamspeak-client.rb
+++ Casks/teamspeak-client.rb
@@ -3,7 +3,7 @@ class TeamspeakClient < Cask
   version '3.0.14'
   sha256 '305f1d33df4bff324c232811253dd65b0caca477fc9196a1e82cbc6c6796e680'
 
-  url 'http://dl.4players.de/ts/releases/3.0.14/TeamSpeak3-Client-macosx-3.0.14.dmg'
+  url "http://dl.4players.de/ts/releases/#{version}/TeamSpeak3-Client-macosx-#{version}.dmg"
   homepage 'http://www.teamspeak.com/'
 
   app 'TeamSpeak 3 Client.app'
