3b29222aad687478d6475ba75f89296dd10c02fc
Roland Walker <walker@pobox.com>
2014-09-26 08:09:00 -0400

    re-use version in launchrocket

--- Casks/launchrocket.rb
+++ Casks/launchrocket.rb
@@ -2,7 +2,7 @@ class Launchrocket < Cask
   version '0.7'
   sha256 '51dc78902fecfb7ec26ab5c6516b84d1c62692349864ef48aca2fde81bd2ef4a'
 
-  url 'https://github.com/jimbojsb/launchrocket/releases/download/v0.7/LaunchRocket.prefPane.zip'
+  url "https://github.com/jimbojsb/launchrocket/releases/download/v#{version}/LaunchRocket.prefPane.zip"
   homepage 'https://github.com/jimbojsb/launchrocket'
 
   prefpane 'LaunchRocket.prefPane'
