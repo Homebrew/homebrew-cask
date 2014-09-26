a7cc6eac20716855fb2b191be8e0e720c518f702
Roland Walker <walker@pobox.com>
2014-09-26 08:09:07 -0400

    re-use version in prepros

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -2,7 +2,7 @@ class Prepros < Cask
   version '4.0.1'
   sha256 '84510d1252274898b0fc4f95828b60e29a2c946a158bbf7d3188611c62dcd3b1'
 
-  url 'http://download.alphapixels.com/Prepros-4.0.1.zip'
+  url "http://download.alphapixels.com/Prepros-#{version}.zip"
   homepage 'http://alphapixels.com/prepros/'
 
   app 'Prepros.app'
