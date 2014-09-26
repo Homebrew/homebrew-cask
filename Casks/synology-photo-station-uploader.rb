d08b4858de7a3a0fc2eb49f29ca2a0b486e01b42
Roland Walker <walker@pobox.com>
2014-09-26 10:58:33 -0400

    re-use version in synology-photo-station-uploader

--- Casks/synology-photo-station-uploader.rb
+++ Casks/synology-photo-station-uploader.rb
@@ -2,9 +2,9 @@ class SynologyPhotoStationUploader < Cask
   version '046'
   sha256 'd8f3462c2590c9a302ec3ea091c779682f37bbacdae7c8038dcccddc9f920bc6'
 
-  url 'https://global.download.synology.com/download/Tools/PhotoStationUploader/046/Mac/PhotoStationUploader-046-Mac-Installer.dmg'
+  url "https://global.download.synology.com/download/Tools/PhotoStationUploader/#{version}/Mac/PhotoStationUploader-#{version}-Mac-Installer.dmg"
   homepage 'http://www.synology.com/'
 
-  pkg 'PhotoStationUploader-046-Mac-Installer.pkg'
+  pkg "PhotoStationUploader-#{version}-Mac-Installer.pkg"
   uninstall :pkgutil => 'com.synology.photostationuploader.installer'
 end
