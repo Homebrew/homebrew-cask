c79246e89a453fb759bae0546ed129c9acf993cc
Roland Walker <walker@pobox.com>
2014-09-26 08:08:47 -0400

    re-use version in audiobookbinder

--- Casks/audiobookbinder.rb
+++ Casks/audiobookbinder.rb
@@ -2,7 +2,7 @@ class Audiobookbinder < Cask
   version '2.0'
   sha256 '7f66fab2041666b2b4d2f4547035804ee9fa7cc5487d6a428f5354e0a793064b'
 
-  url 'http://bluezbox.com/audiobookbinder/AudiobookBinder-2.0.dmg'
+  url "http://bluezbox.com/audiobookbinder/AudiobookBinder-#{version}.dmg"
   appcast 'http://bluezbox.com/audiobookbinder/appcast.xml'
   homepage 'http://bluezbox.com/audiobookbinder.html'
 
