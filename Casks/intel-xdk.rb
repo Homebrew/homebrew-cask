9e317b6976812818403b7b21fef9b038aba4c349
Roland Walker <walker@pobox.com>
2014-09-26 10:58:33 -0400

    re-use version in intel-xdk

--- Casks/intel-xdk.rb
+++ Casks/intel-xdk.rb
@@ -2,9 +2,9 @@ class IntelXdk < Cask
   version '0769'
   sha256 '365a1850ccbf52fb3f1946bb569c454ccd6ea1a72d43074a9e92d9a646881489'
 
-  url 'https://d2bnc8freht07j.cloudfront.net/xdk_web_mac_master_0769.dmg'
+  url "https://d2bnc8freht07j.cloudfront.net/xdk_web_mac_master_#{version}.dmg"
   homepage 'http://xdk-software.intel.com/'
 
-  pkg 'xdk_full_0769.pkg'
+  pkg "xdk_full_#{version}.pkg"
   uninstall :pkgutil => 'com.intel.pkg.xdk_new'
 end
