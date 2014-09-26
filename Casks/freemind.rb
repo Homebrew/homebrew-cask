546720c15108b4ab75a38a380155f4d761563c4b
Roland Walker <walker@pobox.com>
2014-09-26 08:08:55 -0400

    re-use version in freemind

--- Casks/freemind.rb
+++ Casks/freemind.rb
@@ -2,7 +2,7 @@ class Freemind < Cask
   version '1.0.1'
   sha256 '0bd93317567f947e7fa3b3e8d2e0d908300642edc5e07f1929157469ffd14ea3'
 
-  url 'https://downloads.sourceforge.net/project/freemind/freemind/1.0.1/FreeMind_1.0.1.dmg'
+  url "https://downloads.sourceforge.net/project/freemind/freemind/#{version}/FreeMind_#{version}.dmg"
   homepage 'freemind.sourceforge.net'
 
   app 'FreeMind.app'
