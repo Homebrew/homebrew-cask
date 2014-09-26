55bc6ef012fd555ecbc1796e28e93196102ae15d
Roland Walker <walker@pobox.com>
2014-09-26 08:08:51 -0400

    re-use version in ctivo

--- Casks/ctivo.rb
+++ Casks/ctivo.rb
@@ -2,7 +2,7 @@ class Ctivo < Cask
   version '2.3.1'
   sha256 '55ad469432634a77be432741ee6dfc5480c3baf652a6d2b3cde70f919c1d13d7'
 
-  url 'https://github.com/dscottbuch/cTiVo/releases/download/v2.3.1.468/cTiVo_2.3.1_468.zip'
+  url "https://github.com/dscottbuch/cTiVo/releases/download/v#{version}.468/cTiVo_#{version}_468.zip"
   appcast 'https://github.com/dscottbuch/cTiVo/blob/master/update/sparklecast.xml'
   homepage 'https://github.com/dscottbuch/cTiVo'
 
