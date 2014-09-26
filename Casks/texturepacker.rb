c5cbfa777d6a15a9a74f66bbbd37bf07c0b77706
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in texturepacker

--- Casks/texturepacker.rb
+++ Casks/texturepacker.rb
@@ -2,7 +2,7 @@ class Texturepacker < Cask
   version '3.3.1'
   sha256 '00f922d036bd7ec991bbe5ee5fcad1431c3c4884bc2b371fdff92cecdaa11339'
 
-  url 'https://www.codeandweb.com/download/texturepacker/3.3.1/TexturePacker-3.3.1-uni.dmg'
+  url "https://www.codeandweb.com/download/texturepacker/#{version}/TexturePacker-#{version}-uni.dmg"
   homepage 'http://www.codeandweb.com/texturepacker'
 
   app 'TexturePacker.app'
