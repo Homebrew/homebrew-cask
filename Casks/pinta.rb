08139ce0f5adb346b40c0be2dbf5ba235e3a6b85
Roland Walker <walker@pobox.com>
2014-09-26 08:09:06 -0400

    re-use version in pinta

--- Casks/pinta.rb
+++ Casks/pinta.rb
@@ -2,7 +2,7 @@ class Pinta < Cask
   version '1.4'
   sha256 'ebe9e8c271e5c445144ecbbce62d0e5bb20a27b75f34b90bf1a898e1cc452edf'
 
-  url 'https://github.com/downloads/PintaProject/Pinta/pinta-1.4.app.zip'
+  url "https://github.com/downloads/PintaProject/Pinta/pinta-#{version}.app.zip"
   homepage 'http://pinta-project.com/'
 
   app 'Pinta.app'
