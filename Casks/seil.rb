c79456a59bfaac87001f8c3b39a5e0722a25de12
Roland Walker <walker@pobox.com>
2014-09-26 08:09:10 -0400

    re-use version in seil

--- Casks/seil.rb
+++ Casks/seil.rb
@@ -2,7 +2,7 @@ class Seil < Cask
   version '10.10.0'
   sha256 'a61653ce058dc943a403583281a1a10b2b8bd496362ecf7b93989fc90f5547df'
 
-  url 'https://pqrs.org/macosx/keyremap4macbook/files/Seil-10.10.0.dmg'
+  url "https://pqrs.org/macosx/keyremap4macbook/files/Seil-#{version}.dmg"
   homepage 'https://pqrs.org/macosx/keyremap4macbook/seil.html.en'
 
   pkg 'Seil.pkg'
