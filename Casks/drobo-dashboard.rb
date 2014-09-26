54a916ead08b27309c89c701d44bd67836430c48
Roland Walker <walker@pobox.com>
2014-09-26 08:08:52 -0400

    re-use version in drobo-dashboard

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -2,9 +2,9 @@ class DroboDashboard < Cask
   version '2.5.3'
   sha256 '3e17cc8d55c62ab3676628abb783c3f11d686f5bba064e64609294af449bc949'
 
-  url 'http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-2.5.3.dmg'
+  url "http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version}.dmg"
   homepage 'http://www.drobo.com'
 
-  pkg 'Install.app/Contents/Drobo_Dashboard_Installer_2.5.3.pkg'
+  pkg "Install.app/Contents/Drobo_Dashboard_Installer_#{version}.pkg"
   uninstall :script => 'Uninstall.app/Contents/Resources/Scripts/Drobo_Dashboard_uninstall.sh'
 end
