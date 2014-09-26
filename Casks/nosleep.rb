ae8003f2e5d5e7aa9d2cac703522b6a87564828d
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in nosleep

--- Casks/nosleep.rb
+++ Casks/nosleep.rb
@@ -2,7 +2,7 @@ class Nosleep < Cask
   version '1.3.3'
   sha256 '5fe90c54e975d3f1de9f7fdb66def0188c571eab083b18a7e94abaa5f0d41ba9'
 
-  url 'https://macosx-nosleep-extension.googlecode.com/files/NoSleep-1.3.3.dmg'
+  url "https://macosx-nosleep-extension.googlecode.com/files/NoSleep-#{version}.dmg"
   homepage 'https://code.google.com/p/macosx-nosleep-extension/'
 
   pkg 'NoSleep.mpkg'
