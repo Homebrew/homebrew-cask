d42e06e6402ca509d8a184dbbb0a81e74ab66d3d
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in breakaway

--- Casks/breakaway.rb
+++ Casks/breakaway.rb
@@ -2,7 +2,7 @@ class Breakaway < Cask
   version '2.0.1'
   sha256 'df5a3d42558f9cdc778c434f88f7e062032cf33dd751ef01e6109a9848a7f76a'
 
-  url 'https://downloads.sourceforge.net/project/breakaway/breakaway-2.0.1.zip'
+  url "https://downloads.sourceforge.net/project/breakaway/breakaway-#{version}.zip"
   homepage 'http://mutablecode.com/apps/breakaway.html'
 
   app 'Breakaway.app'
