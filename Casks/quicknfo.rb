ea7886fe780aee38c24903a6c14ab03ce81e5d02
Roland Walker <walker@pobox.com>
2014-09-26 08:09:09 -0400

    re-use version in quicknfo

--- Casks/quicknfo.rb
+++ Casks/quicknfo.rb
@@ -2,7 +2,7 @@ class Quicknfo < Cask
   version '1.2'
   sha256 'ee5c03f78ff60e69e776bec39896ac48496915de35fcb3e2bd0d9c20ad92b5bb'
 
-  url 'https://github.com/The-Master777/QuickNFO/releases/download/v1.2/QuickNFO.qlgenerator.zip'
+  url "https://github.com/The-Master777/QuickNFO/releases/download/v#{version}/QuickNFO.qlgenerator.zip"
   homepage 'https://github.com/planbnet/QuickNFO'
 
   qlplugin 'QuickNFO.qlgenerator'
