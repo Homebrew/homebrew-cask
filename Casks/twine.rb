6b6a120f21607c567c0bd4518dd4580fdf5f658a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:16 -0400

    re-use version in twine

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -2,7 +2,7 @@ class Twine < Cask
   version '1.4.1'
   sha256 '717e0f7849f8216a7bae68c66656cf0e3cd0496c8a9d95e3c61e175d56f010e4'
 
-  url 'http://twinery.org/downloads/twine_1.4.1_osx.zip'
+  url "http://twinery.org/downloads/twine_#{version}_osx.zip"
   homepage 'http://twinery.org/'
 
   app 'Twine.app'
