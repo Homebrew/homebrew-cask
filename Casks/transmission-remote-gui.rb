1f2d4cefe990b65c1d82524d533eaf6946ef204d
Roland Walker <walker@pobox.com>
2014-09-26 08:09:16 -0400

    re-use version in transmission-remote-gui

--- Casks/transmission-remote-gui.rb
+++ Casks/transmission-remote-gui.rb
@@ -2,7 +2,7 @@ class TransmissionRemoteGui < Cask
   version '5.0.1'
   sha256 'b961aeb244b2519563837745f3475d21379e3da32bae2b3cbb20ca91d1a90d75'
 
-  url 'https://transmisson-remote-gui.googlecode.com/files/transgui-5.0.1.dmg'
+  url "https://transmisson-remote-gui.googlecode.com/files/transgui-#{version}.dmg"
   homepage 'https://code.google.com/p/transmisson-remote-gui/'
 
   pkg 'transgui.pkg'
