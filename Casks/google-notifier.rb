bf81bb0681b3fc75e16974decf2fda66fb068820
Roland Walker <walker@pobox.com>
2014-09-26 08:08:56 -0400

    re-use version in google-notifier

--- Casks/google-notifier.rb
+++ Casks/google-notifier.rb
@@ -2,7 +2,7 @@ class GoogleNotifier < Cask
   version '1.10.7'
   sha256 '055775cb02773e4ec0e133111c7720134d32bb8d3a1f747231dc0c7d4078f5f7'
 
-  url 'https://dl.google.com/mac/download/GoogleNotifier_1.10.7.dmg'
+  url "https://dl.google.com/mac/download/GoogleNotifier_#{version}.dmg"
   homepage 'http://toolbar.google.com/gmail-helper/notifier_mac.html'
 
   app 'Google Notifier.app'
