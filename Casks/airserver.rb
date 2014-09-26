1d4f2e51445ec682569eee67339a2039df4299b3
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in airserver

--- Casks/airserver.rb
+++ Casks/airserver.rb
@@ -2,7 +2,7 @@ class Airserver < Cask
   version '5.0.4.0'
   sha256 'd6e667bf91a0fb87d000ed778b3cf46c0d9d6e6d397a85de196f5da09f7f1a7a'
 
-  url 'http://dl.airserver.com/mac/AirServer-5.0.4.0.dmg'
+  url "http://dl.airserver.com/mac/AirServer-#{version}.dmg"
   homepage 'http://www.airserver.com'
 
   app 'AirServer.app'
