2c05e2024a43d4bdc0b391f0d7142901e110a573
Roland Walker <walker@pobox.com>
2014-09-26 08:08:50 -0400

    re-use version in coda

--- Casks/coda.rb
+++ Casks/coda.rb
@@ -2,7 +2,7 @@ class Coda < Cask
   version '2.0.13'
   sha256 '89ae6432997915fa93c5a5eafaf4ea1774af78961ff0b7f37722a149618387aa'
 
-  url 'https://panic.com/coda/d/Coda%202.0.13.zip'
+  url "https://panic.com/coda/d/Coda%20#{version}.zip"
   appcast 'http://www.panic.com/updates/update.php'
   homepage 'https://panic.com/Coda/'
 
