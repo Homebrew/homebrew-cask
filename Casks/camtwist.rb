a6c528760cf385829838b24b76bd555d7c46044f
Roland Walker <walker@pobox.com>
2014-09-26 08:08:49 -0400

    re-use version in camtwist

--- Casks/camtwist.rb
+++ Casks/camtwist.rb
@@ -2,7 +2,7 @@ class Camtwist < Cask
   version '2.5'
   sha256 'd81576971abe2bc1a8b1e3d8c894cfe18f0442c49a6a242a6ab25b247de01fec'
 
-  url 'http://camtwiststudio.com/release/CamTwist_2.5.dmg'
+  url "http://camtwiststudio.com/release/CamTwist_#{version}.dmg"
   homepage 'http://camtwiststudio.com/'
 
   pkg 'CamTwist.pkg'
