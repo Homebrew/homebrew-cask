c11fc78f199caf19cc02cf50a480deb00412b127
Roland Walker <walker@pobox.com>
2014-09-26 08:08:53 -0400

    re-use version in eid-mw

--- Casks/eid-mw.rb
+++ Casks/eid-mw.rb
@@ -2,7 +2,7 @@ class EidMw < Cask
   version '4.0.6'
   sha256 'd88dad806fd4a8e01e636a22f9322a14f8bfdec596ce61f194c47a69d64668bb'
 
-  url 'https://eid-mw.googlecode.com/files/beid-4.0.6-7422-signed.dmg'
+  url "https://eid-mw.googlecode.com/files/beid-#{version}-7422-signed.dmg"
   homepage 'https://code.google.com/p/eid-mw/'
 
   pkg 'beid-7422-signed.pkg'
