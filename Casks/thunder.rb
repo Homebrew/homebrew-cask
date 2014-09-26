364b7a530f9a174eb02fe37d20ea564566a76f4f
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in thunder

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -2,7 +2,7 @@ class Thunder < Cask
   version '2.6.5.1672'
   sha256 '45b3b2971a94c17ff8abf8e8f6724268eb3dcbd62cacf2c4d65806a99afe76c4'
 
-  url 'http://down.sandai.net/mac/thunder_dl2.6.5.1672_Beta.dmg'
+  url "http://down.sandai.net/mac/thunder_dl#{version}_Beta.dmg"
   homepage 'http://mac.xunlei.com/'
 
   app 'Thunder.app'
