633c42dc2a73ae5e3d58a1e65da33653ab443931
Roland Walker <walker@pobox.com>
2014-09-26 08:09:14 -0400

    re-use version in td-agent

--- Casks/td-agent.rb
+++ Casks/td-agent.rb
@@ -2,7 +2,7 @@ class TdAgent < Cask
   version '2.0.3'
   sha256 '7e730682879dc199cc712205f22ba93fb9bee20a5dad16b113c8f32d11109cb8'
 
-  url 'http://packages.treasuredata.com/2/macosx/td-agent-2.0.3-0.dmg'
+  url "http://packages.treasuredata.com/2/macosx/td-agent-#{version}-0.dmg"
   homepage 'http://www.fluentd.org/'
 
   pkg 'td-agent2.pkg'
