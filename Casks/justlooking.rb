78f67d30a98e743affe3e7c9cab6cad9ab1d7e7b
Roland Walker <walker@pobox.com>
2014-09-26 08:08:59 -0400

    re-use version in justlooking

--- Casks/justlooking.rb
+++ Casks/justlooking.rb
@@ -2,7 +2,7 @@ class Justlooking < Cask
   version '3.3.3'
   sha256 '730b852c020498886ed061996d9fd5c13c85da707dd18dabbe4cb804afcf1786'
 
-  url 'http://chipmunkninja.com/download/JustLooking-3.3.3.dmg'
+  url "http://chipmunkninja.com/download/JustLooking-#{version}.dmg"
   homepage 'http://chipmunkninja.com/JustLooking'
 
   app 'JustLooking.app'
