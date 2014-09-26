3413818ab06913b6dcc882ce1f9b4e5b340b8678
Roland Walker <walker@pobox.com>
2014-09-26 08:09:00 -0400

    re-use version in librecad

--- Casks/librecad.rb
+++ Casks/librecad.rb
@@ -2,7 +2,7 @@ class Librecad < Cask
   version '2.0.4'
   sha256 'f9a2e56c5a4b79275d45d5578f728fbe3e981e04f550229579de71b0e8ce0604'
 
-  url 'https://downloads.sourceforge.net/project/librecad/OSX/2.0.4/LibreCAD-2.0.4.dmg'
+  url "https://downloads.sourceforge.net/project/librecad/OSX/#{version}/LibreCAD-#{version}.dmg"
   homepage 'http://librecad.org/'
 
   app 'LibreCAD.app'
