971070d5637ef97b7569b3937e4b35e66e6563b1
Roland Walker <walker@pobox.com>
2014-09-26 08:09:07 -0400

    re-use version in pivotalbooster

--- Casks/pivotalbooster.rb
+++ Casks/pivotalbooster.rb
@@ -2,7 +2,7 @@ class Pivotalbooster < Cask
   version '1.1.4.beta'
   sha256 'c32ca0eecd595340f255485ec6cff0c975d3549c2db58b3ddc7b736922a5e159'
 
-  url 'http://pivotalbooster.com/downloads/releases/1.1.4.beta/PivotalBooster_1.1.4.beta.dmg'
+  url "http://pivotalbooster.com/downloads/releases/#{version}/PivotalBooster_#{version}.dmg"
   homepage 'http://pivotalbooster.com/'
 
   app 'PivotalBooster.app'
