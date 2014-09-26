9e4ec2b1198141013e6de912427e3b21efd0cbcd
Roland Walker <walker@pobox.com>
2014-09-26 08:09:16 -0400

    re-use version in traktable

--- Casks/traktable.rb
+++ Casks/traktable.rb
@@ -2,7 +2,7 @@ class Traktable < Cask
   version '0.5.2'
   sha256 '77d3800f02f2b5a2aef8a56e7986b731831738673e35c633d743df87bc556225'
 
-  url 'https://github.com/yo-han/Traktable/releases/download/0.5.2/Traktable.zip'
+  url "https://github.com/yo-han/Traktable/releases/download/#{version}/Traktable.zip"
   homepage 'http://yo-han.github.io/Traktable/'
 
   app 'Traktable.app'
