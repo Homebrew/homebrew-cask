738c5913ba8d777f4ac69cb6bced6151c24d3d16
Roland Walker <walker@pobox.com>
2014-09-26 08:09:14 -0400

    re-use version in subtitles

--- Casks/subtitles.rb
+++ Casks/subtitles.rb
@@ -2,7 +2,7 @@ class Subtitles < Cask
   version '2.2'
   sha256 'c8f00cd00a85f16b7a74de846b9f03fcd0cd0e144336438aa09aef50d527ce0f'
 
-  url 'http://subtitlesapp.com/download/Subtitles-mac-2.2.zip'
+  url "http://subtitlesapp.com/download/Subtitles-mac-#{version}.zip"
   appcast 'http://subtitlesapp.com/updates.xml'
   homepage 'http://subtitlesapp.com'
 
