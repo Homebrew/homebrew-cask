7c41915a4e7f5ffd06ce65f179f199435ad3888b
Roland Walker <walker@pobox.com>
2014-09-26 08:08:59 -0400

    re-use version in kext-utility

--- Casks/kext-utility.rb
+++ Casks/kext-utility.rb
@@ -2,7 +2,7 @@ class KextUtility < Cask
   version '2.5.6'
   sha256 'ae3533e15b646f6b3cd8b8ad84e0bbd3edcf51661ef573255658546d9fbeda5a'
 
-  url 'http://cvad-mac.narod.ru/files/Kext_Utility.app.v2.5.6.zip'
+  url "http://cvad-mac.narod.ru/files/Kext_Utility.app.v#{version}.zip"
   homepage 'http://cvad-mac.narod.ru/index/0-4'
 
   app 'Kext Utility.app'
