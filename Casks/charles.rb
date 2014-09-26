731320290bb1b22b3e2a44cec84aa4dc789055b3
Roland Walker <walker@pobox.com>
2014-09-26 08:08:49 -0400

    re-use version in charles

--- Casks/charles.rb
+++ Casks/charles.rb
@@ -2,7 +2,7 @@ class Charles < Cask
   version '3.9.2'
   sha256 '89b017744181ccec18e8a5ff67b9728a2d2ded5de0620a100a98c8686d867b7b'
 
-  url 'http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-applejava.dmg'
+  url "http://www.charlesproxy.com/assets/release/#{version}/charles-proxy-#{version}-applejava.dmg"
   homepage 'http://www.charlesproxy.com/'
 
   app 'Charles.app'
