155f78fe5400c253954ee49fcd1695facf14aef2
Roland Walker <walker@pobox.com>
2014-09-26 08:08:58 -0400

    re-use version in jbidwatcher

--- Casks/jbidwatcher.rb
+++ Casks/jbidwatcher.rb
@@ -2,7 +2,7 @@ class Jbidwatcher < Cask
   version '2.5.6'
   sha256 '659de073d9e0d71fa86e21db1524b7df57b36ff7826600a01ed93da6df5f5c9f'
 
-  url 'https://www.jbidwatcher.com/download/JBidwatcher-2.5.6.dmg'
+  url "https://www.jbidwatcher.com/download/JBidwatcher-#{version}.dmg"
   homepage 'http://www.jbidwatcher.com'
 
   app 'JBidwatcher.app'
