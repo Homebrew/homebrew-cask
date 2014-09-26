c4a363c5a1a4858bdf939ef1fa534bcb1f64b6d9
Roland Walker <walker@pobox.com>
2014-09-26 08:09:01 -0400

    re-use version in mailbox

--- Casks/mailbox.rb
+++ Casks/mailbox.rb
@@ -2,7 +2,7 @@ class Mailbox < Cask
   version '0.3.10'
   sha256 '8c879e3a79963ddc65bcd56e47cc8f4706096563c6658beeb3e406f882447a99'
 
-  url 'https://mb-dtop.s3.amazonaws.com/external-beta/Mailbox_Ext_Beta_0.3.10_140918.zip'
+  url "https://mb-dtop.s3.amazonaws.com/external-beta/Mailbox_Ext_Beta_#{version}_140918.zip"
   appcast 'https://mb-dtop.s3.amazonaws.com/external-beta/external-beta-appcast.xml'
   homepage 'http://www.mailboxapp.com/'
 
