10e8098e7bb9b85ad9bd168e111ccfbaa1a01c6b
Roland Walker <walker@pobox.com>
2014-09-26 08:09:03 -0400

    re-use version in musicbrainz-picard

--- Casks/musicbrainz-picard.rb
+++ Casks/musicbrainz-picard.rb
@@ -2,7 +2,7 @@ class MusicbrainzPicard < Cask
   version '1.2'
   sha256 '48f3e483fd422f39518134b7c1d68cf05bda084865776783efe3ed08fa099245'
 
-  url 'ftp://ftp.musicbrainz.org/pub/musicbrainz/picard/MusicBrainz-Picard-1.2-x86_64.dmg'
+  url "ftp://ftp.musicbrainz.org/pub/musicbrainz/picard/MusicBrainz-Picard-#{version}-x86_64.dmg"
   homepage 'http://musicbrainz.org/doc/MusicBrainz_Picard'
 
   app 'MusicBrainz Picard.app'
