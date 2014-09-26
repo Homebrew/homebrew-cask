08c12ba44d880426d6e18d441993fd2c982a633c
Peter Brunner <pbrunner@gmail.com>
2014-09-26 00:42:03 -0400

    Updated Url to be more specific

--- Casks/remote-desktop-manager.rb
+++ Casks/remote-desktop-manager.rb
@@ -3,7 +3,7 @@ class RemoteDesktopManager < Cask
   sha256 '7c63fcc059500eba6dc604c3e0115489e8baa3c7ae406c7eb51e0429eceb2a93'
 
   url 'http://download.devolutions.net/Mac/Devolutions.RemoteDesktopManager.Mac.1.0.1.0.dmg'
-  homepage 'http://devolutions.net/'
+  homepage 'http://remotedesktopmanager.com/'
 
   app 'Remote Desktop Manager.app'
 end
