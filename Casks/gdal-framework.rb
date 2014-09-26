d3a890d706cc27ed8ab4dc91579459df23aacb1c
ndr <ndr@qef.io>
2014-09-26 04:06:16 +0200

    Fix GDAL Framework's shasum

--- Casks/gdal-framework.rb
+++ Casks/gdal-framework.rb
@@ -2,7 +2,7 @@ class GdalFramework < Cask
   url 'http://www.kyngchaos.com/files/software/frameworks/GDAL_Complete-1.11.dmg'
   homepage 'http://www.kyngchaos.com/software/frameworks'
   version '1.11'
-  sha256 '8611b80e9e0f2a1ef87ff11429418fbf5996efc6a8041a3e096c3408dcceaf14'
+  sha256 'd8aa6d59236a75519e94a764dfc71d3122c8e88fa4f99072046c3a8a0bfb4124'
   pkg 'GDAL Complete.pkg'
   pkg 'Numpy.pkg'
   uninstall :pkgutil  => [
