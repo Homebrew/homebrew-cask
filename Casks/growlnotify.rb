74d10b125771710e7edadd583a26697315150702
Roland Walker <walker@pobox.com>
2014-09-26 08:08:56 -0400

    re-use version in growlnotify

--- Casks/growlnotify.rb
+++ Casks/growlnotify.rb
@@ -2,7 +2,7 @@ class Growlnotify < Cask
   version '2.1'
   sha256 'eec601488b19c9e9b9cb7f0081638436518bce782d079f6e43ddc195727c04ca'
 
-  url 'https://growl.cachefly.net/GrowlNotify-2.1.zip'
+  url "https://growl.cachefly.net/GrowlNotify-#{version}.zip"
   homepage 'http://growl.info/downloads'
 
   pkg 'GrowlNotify.pkg'
