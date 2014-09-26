70aee1157095645571b144cf5d0b0a7707d254d6
Roland Walker <walker@pobox.com>
2014-09-26 08:09:02 -0400

    re-use version in manager

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -2,7 +2,7 @@ class Manager < Cask
   version '14.2.35'
   sha256 'aa5f89f9e1323cf0ac44dbfb3287e57615c585a6c747879e24cce7187db46b35'
 
-  url 'http://cdn.manager.io/downloads/v14.2.35/Manager.dmg'
+  url "http://cdn.manager.io/downloads/v#{version}/Manager.dmg"
   homepage 'http://manager.io'
 
   app 'Manager.app'
