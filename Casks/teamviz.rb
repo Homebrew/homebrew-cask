9a3e7f35c00e8a26c2978724934833a5cdba1d6c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:14 -0400

    re-use version in teamviz

--- Casks/teamviz.rb
+++ Casks/teamviz.rb
@@ -2,7 +2,7 @@ class Teamviz < Cask
   version '3.2'
   sha256 '58bbff611e1344c1a0ccb555400c212d0969ad4035469b654437f1adeafa7d37'
 
-  url 'http://www.teamviz.com/d/TeamViz.3.2.dmg'
+  url "http://www.teamviz.com/d/TeamViz.#{version}.dmg"
   homepage 'http://www.teamviz.com/'
 
   app 'TeamViz.app'
