4c24a86a6f0fc041e1bb5c4776bd7ca91b9ee9be
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in bit-slicer

--- Casks/bit-slicer.rb
+++ Casks/bit-slicer.rb
@@ -2,7 +2,7 @@ class BitSlicer < Cask
   version '1.7.3'
   sha256 'be8e7638ef29b2fe85f4dc8d237176887c7de86debdb514ef4160d73597d99e7'
 
-  url 'https://bitbucket.org/zorgiepoo/bit-slicer/downloads/Bit%20Slicer%201.7.3.zip'
+  url "https://bitbucket.org/zorgiepoo/bit-slicer/downloads/Bit%20Slicer%20#{version}.zip"
   appcast 'http://zorg.tejat.net/bitslicer/update.php'
   homepage 'https://github.com/zorgiepoo/bit-slicer/'
 
