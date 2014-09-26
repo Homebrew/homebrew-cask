7a85e42943472ebc80b587fd3be37795090926b3
Roland Walker <walker@pobox.com>
2014-09-26 08:09:02 -0400

    re-use version in mapture

--- Casks/mapture.rb
+++ Casks/mapture.rb
@@ -2,8 +2,8 @@ class Mapture < Cask
   version '0.0.16'
   sha256 '54301ca09e3f781dd979fc41978ee2dc547c56bc5776d4b25876bad1ab37b85d'
 
-  url 'http://anatoo.jp/mapture/Mapture-0.0.16.app.zip'
+  url "http://anatoo.jp/mapture/Mapture-#{version}.app.zip"
   homepage 'http://anatoo.jp/mapture/'
 
-  app 'Mapture-0.0.16.app'
+  app "Mapture-#{version}.app"
 end
