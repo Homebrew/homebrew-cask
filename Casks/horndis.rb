d8dc82c4c89e7b8427562c0f3f73c58fef67f088
Roland Walker <walker@pobox.com>
2014-09-26 10:58:33 -0400

    re-use version in horndis

--- Casks/horndis.rb
+++ Casks/horndis.rb
@@ -2,10 +2,10 @@ class Horndis < Cask
   version '5'
   sha256 'd183b8613b7e3811580afa9f1a8691163c36745cea3b8531bf1a50aba180565d'
 
-  url 'http://joshuawise.com/downloads/HoRNDIS-rel5.pkg'
+  url "http://joshuawise.com/downloads/HoRNDIS-rel#{version}.pkg"
   homepage 'http://joshuawise.com/horndis'
 
-  pkg 'HoRNDIS-rel5.pkg'
+  pkg "HoRNDIS-rel#{version}.pkg"
   uninstall :kext => 'com.joshuawise.kexts.HoRNDIS',
             :pkgutil => 'com.joshuawise.*'
 end
