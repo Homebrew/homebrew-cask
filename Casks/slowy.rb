bdcdb7d66b9d14627ed10cbec6892e96fba9fd68
Roland Walker <walker@pobox.com>
2014-09-26 08:09:12 -0400

    re-use version in slowy

--- Casks/slowy.rb
+++ Casks/slowy.rb
@@ -2,7 +2,7 @@ class Slowy < Cask
   version '1.6'
   sha256 '0ecb2fbe06f3d7a050efab30d8ee6b370c83c2b44fec8b005fdc805cbc7728c7'
 
-  url 'http://slowyapp.com/downloads/Slowyapp_v1.6.dmg'
+  url "http://slowyapp.com/downloads/Slowyapp_v#{version}.dmg"
   homepage 'http://slowyapp.com/'
 
   app 'Slowy.app'
