dbf5d7b781f55fef2d06c051ad329ab9d6f0b4c7
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in tikz-editor

--- Casks/tikz-editor.rb
+++ Casks/tikz-editor.rb
@@ -2,7 +2,7 @@ class TikzEditor < Cask
   version '1.0'
   sha256 '0462af2994a5d2c3f495eb922e3cf989e3f4cff27bc545426ddc83da4a5f82db'
 
-  url 'https://github.com/downloads/fredokun/TikZ-Editor/TikZ%20Editor-1.0.dmg'
+  url "https://github.com/downloads/fredokun/TikZ-Editor/TikZ%20Editor-#{version}.dmg"
   homepage 'https://github.com/fredokun/TikZ-Editor'
 
   app 'TikZ Editor.app'
