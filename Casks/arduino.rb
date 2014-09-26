ef57c167a43df5ec5a1c6b2b27d83fa277b09e2a
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in arduino

--- Casks/arduino.rb
+++ Casks/arduino.rb
@@ -2,7 +2,7 @@ class Arduino < Cask
   version '1.0.5'
   sha256 '12f2d649b2cfd537317f63d9cb102dc052647c32a5b07c76d344ed959319c05e'
 
-  url 'https://arduino.googlecode.com/files/arduino-1.0.5-macosx.zip'
+  url "https://arduino.googlecode.com/files/arduino-#{version}-macosx.zip"
   homepage 'http://arduino.cc/'
 
   app 'Arduino.app'
