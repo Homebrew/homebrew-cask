bbd3e5d3b37676138bfce1d92786160ba970b82c
Roland Walker <walker@pobox.com>
2014-09-26 10:58:33 -0400

    re-use version in j

--- Casks/j.rb
+++ Casks/j.rb
@@ -2,7 +2,7 @@ class J < Cask
   version '803'
   sha256 'a595ab134a72dd8948ba3930678be41d27796f257f67ceb2ee4df58cdf1a82fa'
 
-  url 'http://www.jsoftware.com/download/j803/install/j803_mac64.zip'
+  url "http://www.jsoftware.com/download/j#{version}/install/j#{version}_mac64.zip"
   homepage 'http://www.jsoftware.com'
 
   %w<jbrk jcon jhs jqt>.each do |a|
