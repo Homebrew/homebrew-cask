94e648a689aa7136f597aa49ba0a9fb24fc5a1b6
Roland Walker <walker@pobox.com>
2014-09-26 08:09:02 -0400

    re-use version in menucalendarclock-ical

--- Casks/menucalendarclock-ical.rb
+++ Casks/menucalendarclock-ical.rb
@@ -2,8 +2,8 @@ class MenucalendarclockIcal < Cask
   version '4.5.2'
   sha256 '7c78428fd22d0a3fa02e5df4c92fbb45dad881a7ba09205dc47c4bfaaf7f7f9e'
 
-  url 'http://www.objectpark.net/download/MenuCalendarClock-4.5.2.dmg.gz'
-  nested_container 'menucalendarclock-ical-4.5.2'
+  url "http://www.objectpark.net/download/MenuCalendarClock-#{version}.dmg.gz"
+  nested_container "menucalendarclock-ical-#{version}"
   homepage 'http://www.objectpark.net/en/mcc.html'
 
   app 'MenuCalendarClock iCal.app'
