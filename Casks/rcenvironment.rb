239ef78d0de89078beb8b25ce76afd7f8d7d3131
Roland Walker <walker@pobox.com>
2014-09-26 08:09:09 -0400

    re-use version in rcenvironment

--- Casks/rcenvironment.rb
+++ Casks/rcenvironment.rb
@@ -2,7 +2,7 @@ class Rcenvironment < Cask
   version '1.4'
   sha256 '8c2db3e0e2493393cad70ba55157ce5b4aed2d718dff792a7249b118a8f51456'
 
-  url 'http://www.rubicode.com/Downloads/RCEnvironment-1.4.X.dmg'
+  url "http://www.rubicode.com/Downloads/RCEnvironment-#{version}.X.dmg"
   homepage 'http://www.rubicode.com/Software/RCEnvironment/'
 
   prefpane 'RCEnvironment.prefPane'
