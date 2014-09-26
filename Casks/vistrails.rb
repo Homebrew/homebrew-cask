f774e8bff9faa63186ddd1e91e6c18507831a08a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:18 -0400

    re-use version in vistrails

--- Casks/vistrails.rb
+++ Casks/vistrails.rb
@@ -2,7 +2,7 @@ class Vistrails < Cask
   version '2.1.1'
   sha256 '38ac9da9af7a557cee6eb703517e38ea1bcc430706a436a182eec6f976275b02'
 
-  url 'https://downloads.sourceforge.net/project/vistrails/vistrails/v2.1.1/vistrails-mac-10.6-intel-2.1.1-90975fc00211.dmg'
+  url "https://downloads.sourceforge.net/project/vistrails/vistrails/v#{version}/vistrails-mac-10.6-intel-#{version}-90975fc00211.dmg"
   homepage 'http://www.vistrails.org/index.php/Main_Page'
 
   suite 'VisTrails'
