8f8f81075639bf2b7e01f94cb4850c24d5345be0
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in appium

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -2,7 +2,7 @@ class Appium < Cask
   version '1.2.2'
   sha256 'fb7b96244e6dbcdf4d580eae0262b2dec38c008e39ddee4654bb488037f24971'
 
-  url 'https://bitbucket.org/appium/appium.app/downloads/appium-1.2.2.dmg'
+  url "https://bitbucket.org/appium/appium.app/downloads/appium-#{version}.dmg"
   homepage 'http://appium.io'
 
   app 'Appium.app'
