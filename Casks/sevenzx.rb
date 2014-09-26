7d84cffa9029bfaa84652e9af19f160402492393
Roland Walker <walker@pobox.com>
2014-09-26 08:09:11 -0400

    re-use version in sevenzx

--- Casks/sevenzx.rb
+++ Casks/sevenzx.rb
@@ -2,7 +2,7 @@ class Sevenzx < Cask
   version '1.7.1'
   sha256 '6be03312017fdd6e360293f94d7a529aba65dc1d86b561fd2c3f35dc3e815247'
 
-  url 'https://www.macupdate.com/download/20526/7zX_1.7.1.-5003b6dcbb4d8.dmg'
+  url "https://www.macupdate.com/download/20526/7zX_#{version}.-5003b6dcbb4d8.dmg"
   homepage 'http://sixtyfive.xmghosting.com/products/7zx/'
 
   app '7zX.app'
