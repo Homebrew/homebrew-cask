cba72e6fd379324c0f8d64e0e51f04eb54dfc37c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in time-out

--- Casks/time-out.rb
+++ Casks/time-out.rb
@@ -2,7 +2,7 @@ class TimeOut < Cask
   version '1.7.1'
   sha256 '3c9892344c8313b8ccf0a76cceb00834ddbe26e5114bcd674c4fd53aeb44e310'
 
-  url 'http://www.dejal.com/download/timeout-1.7.1.zip'
+  url "http://www.dejal.com/download/timeout-#{version}.zip"
   homepage 'http://www.dejal.com/timeout/'
 
   app 'Time Out.app'
