216c9b6560f7e4ff23ed57f79f5eabf4dc32e9e4
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in nocturne

--- Casks/nocturne.rb
+++ Casks/nocturne.rb
@@ -2,7 +2,7 @@ class Nocturne < Cask
   version '2.0.0'
   sha256 '062ae6b4619ab518650b2f502aaeb7a864bf69e45ce08dec8b5a3f34a027a347'
 
-  url 'https://blacktree-nocturne.googlecode.com/files/Nocturne.2.0.0.zip'
+  url "https://blacktree-nocturne.googlecode.com/files/Nocturne.#{version}.zip"
   homepage 'http://code.google.com/p/blacktree-nocturne/'
 
   app 'Nocturne.app'
