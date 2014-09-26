5b49cfc7d97e1e11b15d5fce6e1259263f4395ec
Roland Walker <walker@pobox.com>
2014-09-26 08:09:11 -0400

    re-use version in sickbeard-anime

--- Casks/sickbeard-anime.rb
+++ Casks/sickbeard-anime.rb
@@ -2,7 +2,7 @@ class SickbeardAnime < Cask
   version '13.05.25'
   sha256 '402d6eb6df5d0c75f190cfe23e0d59cacaa857f94a902f2728fb20cae64d2f36'
 
-  url 'http://sickbeard.lad1337.de/download.php?f=SickBeard-OSX-anime-13.05.25.dmg'
+  url "http://sickbeard.lad1337.de/download.php?f=SickBeard-OSX-anime-#{version}.dmg"
   homepage 'http://sickbeard.lad1337.de/'
 
   app 'SickBeard.app'
