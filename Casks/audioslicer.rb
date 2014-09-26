bbeb203352f1024ea71c670fbd71aba9482f0024
Roland Walker <walker@pobox.com>
2014-09-26 08:08:47 -0400

    re-use version in audioslicer

--- Casks/audioslicer.rb
+++ Casks/audioslicer.rb
@@ -2,7 +2,7 @@ class Audioslicer < Cask
   version '1.1.1'
   sha256 'c0ef4d9b1690943def00ce4c7bb2838ff08cfe0d3ff85d39ca1e4ddc97593d01'
 
-  url 'https://downloads.sourceforge.net/sourceforge/audioslicer/AudioSlicer-1.1.1.dmg'
+  url "https://downloads.sourceforge.net/sourceforge/audioslicer/AudioSlicer-#{version}.dmg"
   homepage 'http://audioslicer.sourceforge.net'
 
   app 'AudioSlicer.app'
