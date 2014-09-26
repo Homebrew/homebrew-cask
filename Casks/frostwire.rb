e6c4c4fc878e53ff6c73345a71a0063b982a1c8e
Roland Walker <walker@pobox.com>
2014-09-26 08:08:55 -0400

    re-use version in frostwire

--- Casks/frostwire.rb
+++ Casks/frostwire.rb
@@ -2,7 +2,7 @@ class Frostwire < Cask
   version '5.7.2'
   sha256 'c559d214576684a97cee0bea15c91caf8dd2c3609cc498039813724ea475642e'
 
-  url 'http://dl.frostwire.com/frostwire/5.7.2/frostwire-5.7.2.dmg'
+  url "http://dl.frostwire.com/frostwire/#{version}/frostwire-#{version}.dmg"
   homepage 'http://www.frostwire.com'
 
   app 'FrostWire.app'
