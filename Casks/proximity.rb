72ae4b1ea8dbd9950f95d3351777e887d7e337e8
Roland Walker <walker@pobox.com>
2014-09-26 08:09:08 -0400

    re-use version in proximity

--- Casks/proximity.rb
+++ Casks/proximity.rb
@@ -2,7 +2,7 @@ class Proximity < Cask
   version '1.5'
   sha256 '3ac0e90ffee62a6fadf2bb697393442f3cd87f4084ffdccf9cccf10cb86b3203'
 
-  url 'https://reduxcomputing-proximity.googlecode.com/files/Proximity%201.5.zip'
+  url "https://reduxcomputing-proximity.googlecode.com/files/Proximity%20#{version}.zip"
   homepage 'https://code.google.com/p/reduxcomputing-proximity/'
 
   app 'Proximity.app'
