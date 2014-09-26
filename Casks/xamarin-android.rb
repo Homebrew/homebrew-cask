9975408efb9f759475f4a2a57f72e74f450fd4f1
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in xamarin-android

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -2,11 +2,11 @@ class XamarinAndroid < Cask
   version '4.12.5-2'
   sha256 '4ca0cce28879f7db8cd5117c425212e2fab772ad1dbb3e2fc32ccefbc166e14e'
 
-  url 'http://download.xamarin.com/MonoforAndroid/Mac/mono-android-4.12.5-2.pkg'
+  url "http://download.xamarin.com/MonoforAndroid/Mac/mono-android-#{version}.pkg"
   # non-Sparkle appcast
   appcast 'http://xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml'
   homepage 'http://xamarin.com/android'
 
-  pkg 'mono-android-4.12.5-2.pkg'
+  pkg "mono-android-#{version}.pkg"
   uninstall :pkgutil => 'com.xamarin.android.pkg'
 end
