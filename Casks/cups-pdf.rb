ee2efff1acec0532854d9cc9919f3bda5e9c227e
Roland Walker <walker@pobox.com>
2014-09-26 08:08:51 -0400

    re-use version in cups-pdf

--- Casks/cups-pdf.rb
+++ Casks/cups-pdf.rb
@@ -2,7 +2,7 @@ class CupsPdf < Cask
   version '2.6.1'
   sha256 '24bf65884dda2d3fd60266f96bf76791a61ec9095f11073047d880de692784b9'
 
-  url 'https://bitbucket.org/codepoet/cups-pdf-for-mac-os-x/downloads/CUPS-PDF%202.6.1%20Installer.pkg'
+  url "https://bitbucket.org/codepoet/cups-pdf-for-mac-os-x/downloads/CUPS-PDF%20#{version}%20Installer.pkg"
   homepage 'http://www.cups-pdf.de'
 
   pkg 'CUPS-PDF.mpkg'
