2fc7fbb5b347b77f129c7c455f8b343b7f3482d6
Roland Walker <walker@pobox.com>
2014-09-26 08:08:56 -0400

    re-use version in growlvoice

--- Casks/growlvoice.rb
+++ Casks/growlvoice.rb
@@ -2,7 +2,7 @@ class Growlvoice < Cask
   version '2.0.3'
   sha256 '6c9e15b83894746e2bf9488228e95eb2f47520b1fdac728da52d16532e2ee979'
 
-  url 'http://www.growlvoice.com/trials/growlvoice-2.0.3.zip'
+  url "http://www.growlvoice.com/trials/growlvoice-#{version}.zip"
   homepage 'http://www.growlvoice.com/'
 
   app 'GrowlVoice.app'
