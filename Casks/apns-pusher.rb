4677b3064d30d316588047db5bddd646d9082c87
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in apns-pusher

--- Casks/apns-pusher.rb
+++ Casks/apns-pusher.rb
@@ -2,7 +2,7 @@ class ApnsPusher < Cask
   version '2.3'
   sha256 '6aa54050bea8603b45e6737bf2cc6997180b1a58f1d02095f5141176a1335205'
 
-  url 'https://github.com/blommegard/APNS-Pusher/releases/download/v2.3/APNS.Pusher.app.zip'
+  url "https://github.com/blommegard/APNS-Pusher/releases/download/v#{version}/APNS.Pusher.app.zip"
   homepage 'https://github.com/blommegard/APNS-Pusher'
 
   app 'APNS Pusher.app'
