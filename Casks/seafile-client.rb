99564834cb853af771206c492634d3d3074e1dc2
Roland Walker <walker@pobox.com>
2014-09-26 08:09:10 -0400

    re-use version in seafile-client

--- Casks/seafile-client.rb
+++ Casks/seafile-client.rb
@@ -2,7 +2,7 @@ class SeafileClient < Cask
   version '3.1.5'
   sha256 '25eedd712d57bcf94fd19ba2783d5f0278c3e95403ee07f300e17dabbd6bb75a'
 
-  url 'https://bitbucket.org/haiwen/seafile/downloads/seafile-client-3.1.5.dmg'
+  url "https://bitbucket.org/haiwen/seafile/downloads/seafile-client-#{version}.dmg"
   homepage 'http://seafile.com/'
 
   app 'Seafile Client.app'
