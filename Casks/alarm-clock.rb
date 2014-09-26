bfd52c4c6a63c15db19fde7305ba43208b4b14b9
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in alarm-clock

--- Casks/alarm-clock.rb
+++ Casks/alarm-clock.rb
@@ -2,7 +2,7 @@ class AlarmClock < Cask
   version '2.4.5'
   sha256 '285d277572be83c632c696d565a8413c2d5149a460392177e9e1b601ffce8778'
 
-  url 'http://wayback.archive.org/web/20130123192255/http://www.robbiehanson.com/alarmclock/downloads/Alarm%20Clock%20(2.4.5).dmg'
+  url "http://wayback.archive.org/web/20130123192255/http://www.robbiehanson.com/alarmclock/downloads/Alarm%20Clock%20(#{version}).dmg"
   homepage 'http://wayback.archive.org/web/20130123192255/http://www.robbiehanson.com/alarmclock/index.html'
 
   app 'Alarm Clock.app'
