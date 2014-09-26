8688dc9bb888e3682ab2f601225481af51c3e95a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:05 -0400

    re-use version in onepassword

--- Casks/onepassword.rb
+++ Casks/onepassword.rb
@@ -2,7 +2,7 @@ class Onepassword < Cask
   version '4.4.1'
   sha256 '007b554067cc0016bedfdcb75577f88b68daeabb5474a219960080e2e9159c90'
 
-  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.4.1.zip'
+  url "http://i.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
 
   app '1Password 4.app'
