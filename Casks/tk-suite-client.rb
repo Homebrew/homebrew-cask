7a3d263b0611f0b628c7ea8d9b11e999c18baeff
Roland Walker <walker@pobox.com>
2014-09-26 08:09:16 -0400

    re-use version in tk-suite-client

--- Casks/tk-suite-client.rb
+++ Casks/tk-suite-client.rb
@@ -2,7 +2,7 @@ class TkSuiteClient < Cask
   version '4.4.13'
   sha256 '4047cf17e9ca009a2c95e5127fafc145a74ba8320a4c4c94c866aeb49203a5ef'
 
-  url 'ftp://ftp.agfeo.de/pub/software/TK-Suite-Client_intel32_agfeo_4.4.13.dmg'
+  url "ftp://ftp.agfeo.de/pub/software/TK-Suite-Client_intel32_agfeo_#{version}.dmg"
   homepage 'http://agfeo.de/agfeo_web/hp3.nsf/lu/2064'
 
   app 'TK-Suite-Client.app'
