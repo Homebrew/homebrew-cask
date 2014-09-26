c37b9e5091a5824594e8686a8bbbbe411e11784d
Roland Walker <walker@pobox.com>
2014-09-26 08:08:47 -0400

    re-use version in aseprite

--- Casks/aseprite.rb
+++ Casks/aseprite.rb
@@ -2,7 +2,7 @@ class Aseprite < Cask
   version '0.9.5'
   sha256 '299eda3e5f11ae60d58bccdd26156169db6ebc01be8e806d7909f0b8f22e2928'
 
-  url 'https://aseprite.googlecode.com/files/ASEPRITE_0.9.5.dmg'
+  url "https://aseprite.googlecode.com/files/ASEPRITE_#{version}.dmg"
   homepage 'http://www.aseprite.org'
 
   app 'aseprite.app'
