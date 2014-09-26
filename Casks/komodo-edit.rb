2f3ea621942af9486e23cf7cb641d367af6cec5a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:00 -0400

    re-use version in komodo-edit

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -2,7 +2,7 @@ class KomodoEdit < Cask
   version '8.5.4'
   sha256 '424558813e2926386e9d1de2bf93eb772a9b61b2f90688efc13e073d0d1b452e'
 
-  url 'http://downloads.activestate.com/Komodo/releases/8.5.4/Komodo-Edit-8.5.4-14424-macosx-x86_64.dmg'
+  url "http://downloads.activestate.com/Komodo/releases/#{version}/Komodo-Edit-#{version}-14424-macosx-x86_64.dmg"
   homepage 'http://komodoide.com/komodo-edit'
 
   app 'Komodo Edit 8.app'
