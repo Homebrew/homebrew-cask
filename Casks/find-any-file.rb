6c0605eda9ec9f6a323fde56f91d5cf0974d44f4
Roland Walker <walker@pobox.com>
2014-09-26 08:08:54 -0400

    re-use version in find-any-file

--- Casks/find-any-file.rb
+++ Casks/find-any-file.rb
@@ -2,7 +2,7 @@ class FindAnyFile < Cask
   version '1.8.8'
   sha256 '3ef7b2a0f5373ac904198fb574d61d39316a0c4f6cb6740742b719bd7c842255'
 
-  url 'http://files.tempel.org.s3.amazonaws.com/FindAnyFile_1.8.8.zip'
+  url "http://files.tempel.org.s3.amazonaws.com/FindAnyFile_#{version}.zip"
   homepage 'http://apps.tempel.org/FindAnyFile/'
 
   app 'Find Any File.app'
