0b190fa1a9a421b719f3df4a000434adb14f320e
Roland Walker <walker@pobox.com>
2014-09-26 08:09:03 -0400

    re-use version in moreamp

--- Casks/moreamp.rb
+++ Casks/moreamp.rb
@@ -2,7 +2,7 @@ class Moreamp < Cask
   version '0.1.28'
   sha256 '2f07c82bb5b29e12185cd35c3f3cd9c472e8cc837fe426af1c82af9428c89695'
 
-  url 'https://downloads.sourceforge.net/project/moreamp/moreamp/MoreAmp-0.1.28/MoreAmp-0.1.28-binOSXintel.dmg'
+  url "https://downloads.sourceforge.net/project/moreamp/moreamp/MoreAmp-#{version}/MoreAmp-#{version}-binOSXintel.dmg"
   homepage 'http://sourceforge.net/projects/moreamp/'
 
   app 'MoreAmp.app'
