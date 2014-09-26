76434477def255bb65dd4030b06ea24facc96e1a
Roland Walker <walker@pobox.com>
2014-09-26 08:08:54 -0400

    re-use version in fender-amp-drivers

--- Casks/fender-amp-drivers.rb
+++ Casks/fender-amp-drivers.rb
@@ -2,7 +2,7 @@ class FenderAmpDrivers < Cask
   version '2.7.1'
   sha256 'f73c4c66c6d9bc6492d8f22126415f5a36eb555714adc147590fb3a5d25b8c34'
 
-  url 'http://support.fender.com/software/fender_software/fender_fuse/mac/FenderFUSE_FULL_2.7.1.dmg'
+  url "http://support.fender.com/software/fender_software/fender_fuse/mac/FenderFUSE_FULL_#{version}.dmg"
   homepage 'https://fuse.fender.com/'
 
   pkg 'Fender FUSE Installer.app/Contents/Resources/FenderDrivers.pkg'
