ad1ff415f4baedd4d5fc41444c960dc36897fba1
Roland Walker <walker@pobox.com>
2014-09-26 08:08:58 -0400

    re-use version in injection

--- Casks/injection.rb
+++ Casks/injection.rb
@@ -2,10 +2,10 @@ class Injection < Cask
   version '5.1'
   sha256 'c5e6dbaca14e3b9ee7e084d6787047cc61345ec38c2614b7c8016525ef18de9c'
 
-  url 'http://injectionforxcode.johnholdsworth.com/InjectionPluginV5.1.pkg'
+  url "http://injectionforxcode.johnholdsworth.com/InjectionPluginV#{version}.pkg"
   homepage 'http://injectionforxcode.com/'
 
-  pkg 'InjectionPluginV5.1.pkg'
+  pkg "InjectionPluginV#{version}.pkg"
   uninstall :pkgutil => 'com.injectionforxcode.injectionPluginForXcode.InjectionPlugin.pkg',
             :quit => 'com.johnholdsworth.InjectionPluginIII'
   zap       :delete => '~/Library/Application Support/Developer/Shared/Xcode/Plug-ins/InjectionPlugin.xcplugin'
