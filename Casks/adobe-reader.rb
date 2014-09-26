05f970e7e7012e9f7f3d5459338e137352449bec
Roland Walker <walker@pobox.com>
2014-09-26 08:08:46 -0400

    re-use version in adobe-reader

--- Casks/adobe-reader.rb
+++ Casks/adobe-reader.rb
@@ -2,7 +2,7 @@ class AdobeReader < Cask
   version '11.0.07'
   sha256 '817a9f475f0a05b3b6d353eb10430769176506f6066c48b492dc13ff627b3a39'
 
-  url 'http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.07/en_US/AdbeRdr11007_en_US.dmg'
+  url "http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/#{version}/en_US/AdbeRdr11007_en_US.dmg"
   homepage 'http://www.adobe.com/products/reader.html'
 
   pkg 'Adobe Reader XI Installer.pkg'
