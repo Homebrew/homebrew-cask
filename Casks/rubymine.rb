9c803928173c46c4fc0e22107b721784f6b0a5ad
Roland Walker <walker@pobox.com>
2014-09-26 08:09:10 -0400

    re-use version in rubymine

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -2,7 +2,7 @@ class Rubymine < Cask
   version '6.3.3'
   sha256 'c79216de02f2564ea60592420342ab9fb5014da7e7c96f92e2856dc49f2090dd'
 
-  url 'http://download-cf.jetbrains.com/ruby/RubyMine-6.3.3.dmg'
+  url "http://download-cf.jetbrains.com/ruby/RubyMine-#{version}.dmg"
   homepage 'http://www.jetbrains.com/ruby/'
 
   app 'RubyMine.app'
