c75daae3c706b411d03bafcac4a7ca93144baece
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in blink1control

--- Casks/blink1control.rb
+++ Casks/blink1control.rb
@@ -3,7 +3,7 @@ class Blink1control < Cask
   sha256 'fc3624a041f561f086cdce95bd70470b072c9b586e9ee1247d88b78e6abce094'
 
   homepage 'http://blink1.thingm.com/'
-  url 'https://github.com/todbot/blink1/releases/download/v1.91/Blink1Control-mac.zip'
+  url "https://github.com/todbot/blink1/releases/download/v#{version}/Blink1Control-mac.zip"
 
   app 'Blink1Control.app'
 end
