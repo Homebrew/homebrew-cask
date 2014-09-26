f7f2b3b59861aaf3fa9c0bbbced5ee1c917bed8a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:17 -0400

    re-use version in visit

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -2,7 +2,7 @@ class Visit < Cask
   version '2.7.2'
   sha256 '3c1f1b201358876c1b23717a1b3a2c5a1777755e3fc7f242c08b53a033402fbc'
 
-  url 'https://portal.nersc.gov/svn/visit/trunk/releases/2.7.2/VisIt-2.7.2.dmg'
+  url "https://portal.nersc.gov/svn/visit/trunk/releases/#{version}/VisIt-#{version}.dmg"
   homepage 'https://wci.llnl.gov/codes/visit/home.html'
 
   app 'VisIt.app'
