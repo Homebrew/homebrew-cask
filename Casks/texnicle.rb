25d783fee01935b23194c904ad95a16970e97974
Roland Walker <walker@pobox.com>
2014-09-26 08:09:15 -0400

    re-use version in texnicle

--- Casks/texnicle.rb
+++ Casks/texnicle.rb
@@ -2,7 +2,7 @@ class Texnicle < Cask
   version '2.2.11'
   sha256 'b522e551220bfc98ff3d3c09db224c0db2a31c1dff4c73353a26d1f13217733b'
 
-  url 'http://www.bobsoft-mac.de/resources/TeXnicle/2.2/TeXnicle.app.2.2.11.zip'
+  url "http://www.bobsoft-mac.de/resources/TeXnicle/2.2/TeXnicle.app.#{version}.zip"
   appcast 'http://www.bobsoft-mac.de/profileInfo.php'
   homepage 'http://www.bobsoft-mac.de/texnicle/texnicle.html'
 
