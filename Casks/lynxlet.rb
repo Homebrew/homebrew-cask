138077eb8d9468b0c831f175b6ccaa24fbb0b9c2
Roland Walker <walker@pobox.com>
2014-09-26 08:09:01 -0400

    re-use version in lynxlet

--- Casks/lynxlet.rb
+++ Casks/lynxlet.rb
@@ -2,7 +2,7 @@ class Lynxlet < Cask
   version '0.8.1'
   sha256 '78224e5bfcfcea7d63a22e3baaeac0df215673b94af32c572714b061cf05789f'
 
-  url 'http://habilis.net/lynxlet/Lynxlet_0.8.1.dmg'
+  url "http://habilis.net/lynxlet/Lynxlet_#{version}.dmg"
   homepage 'http://habilis.net/lynxlet/'
 
   app 'Lynxlet.app'
