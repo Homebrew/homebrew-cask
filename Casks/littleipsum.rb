d1d66a25ad9b85b64a8decef64ad85fa30ff168b
Roland Walker <walker@pobox.com>
2014-09-26 08:09:00 -0400

    re-use version in littleipsum

--- Casks/littleipsum.rb
+++ Casks/littleipsum.rb
@@ -2,7 +2,7 @@ class Littleipsum < Cask
   version '1.1.2'
   sha256 '5fa88e5cd2cc1c837ac3e9ff3a23ce1569835d56c71e2741243ce9f0424099db'
 
-  url 'http://littleipsum.com/download/LittleIpsum%201.1.2.zip'
+  url "http://littleipsum.com/download/LittleIpsum%20#{version}.zip"
   homepage 'http://littleipsum.com'
 
   app 'LittleIpsum.app'
