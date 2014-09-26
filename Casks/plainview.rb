fa9d5d836291832202a778aeeaa41655eaeb3d83
Roland Walker <walker@pobox.com>
2014-09-26 08:09:07 -0400

    re-use version in plainview

--- Casks/plainview.rb
+++ Casks/plainview.rb
@@ -2,7 +2,7 @@ class Plainview < Cask
   version '1.0.178'
   sha256 'ff442b3e8995ab42c0908e2fafe07d14233bad72fff677a83cf9cf09b18a2e2c'
 
-  url 'https://s3.amazonaws.com/plainviewapp/plainview_1.0.178.zip'
+  url "https://s3.amazonaws.com/plainviewapp/plainview_#{version}.zip"
   homepage 'http://barbariangroup.com/'
 
   app 'Plainview.app'
