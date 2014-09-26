0977c7ec9c76c700a25e9fddf14ebc2b0eeea9c5
Roland Walker <walker@pobox.com>
2014-09-26 08:09:08 -0400

    re-use version in provisioning

--- Casks/provisioning.rb
+++ Casks/provisioning.rb
@@ -2,8 +2,8 @@ class Provisioning < Cask
   version '1.0.4'
   sha256 '554590ab653776babbc453892653d052f1ec09c6473fc91f4a071f1a7a953144'
 
-  url 'https://github.com/chockenberry/Provisioning/releases/download/1.0.4/Provisioning-1.0.4.zip'
+  url "https://github.com/chockenberry/Provisioning/releases/download/#{version}/Provisioning-#{version}.zip"
   homepage 'https://github.com/chockenberry/Provisioning'
 
-  qlplugin 'Provisioning-1.0.4/Provisioning.qlgenerator'
+  qlplugin "Provisioning-#{version}/Provisioning.qlgenerator"
 end
