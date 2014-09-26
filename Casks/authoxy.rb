147a0d3907ff46c599ff90b97b3f512765cc9623
Roland Walker <walker@pobox.com>
2014-09-26 08:08:47 -0400

    re-use version in authoxy

--- Casks/authoxy.rb
+++ Casks/authoxy.rb
@@ -2,7 +2,7 @@ class Authoxy < Cask
   version '3.6'
   sha256 '921720e5bd1d7cd9f08e29bee928ea667662ba704f59340280194c8b4649b632'
 
-  url 'http://www.hrsoftworks.net/downloads/Authoxy3.6.dmg'
+  url "http://www.hrsoftworks.net/downloads/Authoxy#{version}.dmg"
   homepage 'http://www.hrsoftworks.net'
 
   pkg 'Authoxy (double click me).pkg'
