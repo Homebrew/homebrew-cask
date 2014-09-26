5aeda49decdb2cc306d65cb66db7fdecf8cadf07
Roland Walker <walker@pobox.com>
2014-09-26 08:09:02 -0400

    re-use version in mediainfo

--- Casks/mediainfo.rb
+++ Casks/mediainfo.rb
@@ -2,7 +2,7 @@ class Mediainfo < Cask
   version '0.7.66'
   sha256 'c96dc8998fa323d04d82ee146a7aeee63b32a6e67c5750ba099f79900788a442'
 
-  url 'https://mediaarea.net/download/binary/mediainfo-gui/0.7.66/MediaInfo_GUI_0.7.66_Mac.dmg'
+  url "https://mediaarea.net/download/binary/mediainfo-gui/#{version}/MediaInfo_GUI_#{version}_Mac.dmg"
   homepage 'http://mediaarea.net/en/MediaInfo'
 
   app 'MediaInfo.app'
