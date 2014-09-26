2716d3bb435cb1c29aed940bd517d2fb2363b3b7
Roland Walker <walker@pobox.com>
2014-09-26 08:09:05 -0400

    re-use version in opera-mail

--- Casks/opera-mail.rb
+++ Casks/opera-mail.rb
@@ -2,7 +2,7 @@ class OperaMail < Cask
   version '1.0'
   sha256 'afd192e308f8ea8ddb3d426fd6663d97078570417ee78b8e1fa15f515ae3d677'
 
-  url 'https://get-ash-1.opera.com/pub/opera/mail/1.0/mac/Opera-Mail-1.0-1040.i386.dmg'
+  url "https://get-ash-1.opera.com/pub/opera/mail/#{version}/mac/Opera-Mail-#{version}-1040.i386.dmg"
   homepage 'http://www.opera.com/computer/mail'
 
   app 'Opera Mail.app'
