ff188791a446b1b9b59343575079e0f3a472d8e2
Roland Walker <walker@pobox.com>
2014-09-26 08:09:17 -0400

    re-use version in ubersicht

--- Casks/ubersicht.rb
+++ Casks/ubersicht.rb
@@ -2,7 +2,7 @@ class Ubersicht < Cask
   version '0.2.7'
   sha256 '84a104490c06806205a30008472df92a983855f2bf8fba55ad4312d53518a362'
 
-  url 'http://tracesof.net/uebersicht/releases/Uebersicht-0.2.7.app.zip'
+  url "http://tracesof.net/uebersicht/releases/Uebersicht-#{version}.app.zip"
   homepage 'http://tracesof.net/uebersicht'
 
   app 'Übersicht.app'
