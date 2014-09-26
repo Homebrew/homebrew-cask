b2b496de3a6231b48110bf0f9678ef9055dbcda6
Roland Walker <walker@pobox.com>
2014-09-26 08:08:51 -0400

    re-use version in corsixth

--- Casks/corsixth.rb
+++ Casks/corsixth.rb
@@ -2,7 +2,7 @@ class Corsixth < Cask
   version '0.30'
   sha256 '2d9fbe47ac31955643ee1de0e53fd30ddac9e64d5a1c205c46ecd3bfda1929b4'
 
-  url 'https://github.com/CorsixTH/CorsixTH/releases/download/v0.30/CorsixTH-0.30-OSX.dmg'
+  url "https://github.com/CorsixTH/CorsixTH/releases/download/v#{version}/CorsixTH-#{version}-OSX.dmg"
   homepage 'http://th.corsix.org'
 
   app 'CorsixTH.app'
