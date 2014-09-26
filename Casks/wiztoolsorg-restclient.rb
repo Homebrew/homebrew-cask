b82bfb0dfc45aeaf27968299299f74ef40bea2e6
Roland Walker <walker@pobox.com>
2014-09-26 08:09:19 -0400

    re-use version in wiztoolsorg-restclient

--- Casks/wiztoolsorg-restclient.rb
+++ Casks/wiztoolsorg-restclient.rb
@@ -2,7 +2,7 @@ class WiztoolsorgRestclient < Cask
   version '3.2.2'
   sha256 'd920279ef6406acb02a12485e1e8adaf1942cf196f0628c8e6b40744f6120aac'
 
-  url 'https://rest-client.googlecode.com/files/restclient-ui-3.2.2.dmg'
+  url "https://rest-client.googlecode.com/files/restclient-ui-#{version}.dmg"
   homepage 'https://code.google.com/p/rest-client'
 
   app 'WizTools.org RESTClient.app'
