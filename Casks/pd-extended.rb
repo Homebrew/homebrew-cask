ab056a453b2ba93bff204832572fa1d61391c3ae
Roland Walker <walker@pobox.com>
2014-09-26 08:09:06 -0400

    re-use version in pd-extended

--- Casks/pd-extended.rb
+++ Casks/pd-extended.rb
@@ -2,7 +2,7 @@ class PdExtended < Cask
   version '0.43.4'
   sha256 'abe7bd637b1495ad9d5a500f0a18550c1600e34ee17e60aa1a48e4dbdee59bb9'
 
-  url 'https://downloads.sourceforge.net/project/pure-data/pd-extended/0.43.4/Pd-0.43.4-extended-macosx105-i386.dmg'
+  url "https://downloads.sourceforge.net/project/pure-data/pd-extended/#{version}/Pd-#{version}-extended-macosx105-i386.dmg"
   homepage 'http://puredata.info/downloads/pd-extended'
 
   app 'Pd-extended.app'
