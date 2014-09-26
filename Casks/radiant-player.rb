8d3fee0888f6ac3cf8265121f62f067772234655
Roland Walker <walker@pobox.com>
2014-09-26 08:09:09 -0400

    re-use version in radiant-player

--- Casks/radiant-player.rb
+++ Casks/radiant-player.rb
@@ -2,7 +2,7 @@ class RadiantPlayer < Cask
   version '1.2.1'
   sha256 'dc1ed98170e05c430779f527191717cb412e74f398324878286df4a2bbad79d4'
 
-  url 'https://github.com/kbhomes/google-music-mac/releases/download/v1.2.1/Radiant.Player.zip'
+  url "https://github.com/kbhomes/google-music-mac/releases/download/v#{version}/Radiant.Player.zip"
   homepage 'http://kbhomes.github.io/google-music-mac/'
 
   app 'Radiant Player.app'
