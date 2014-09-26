073d375c0ac5f77fe58d796b7fc8ca1e4a413d2e
Roland Walker <walker@pobox.com>
2014-09-26 08:09:17 -0400

    re-use version in vine-server

--- Casks/vine-server.rb
+++ Casks/vine-server.rb
@@ -2,7 +2,7 @@ class VineServer < Cask
   version '4.01'
   sha256 '10e32d9594614b5169261b5b9bbeadb6f5adf2cc1c4bf2fdce68be3ccd7ca4a9'
 
-  url 'http://www.testplant.com/downloads/Vine/VineServer4.01.dmg'
+  url "http://www.testplant.com/downloads/Vine/VineServer#{version}.dmg"
   homepage 'http://www.testplant.com/products/vine/vine-server/'
 
   app 'Vine Server.app'
