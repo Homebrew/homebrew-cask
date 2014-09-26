0159cf1b03adb76e6a49dd5b9067cde1fac113ae
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in bluegriffon

--- Casks/bluegriffon.rb
+++ Casks/bluegriffon.rb
@@ -2,7 +2,7 @@ class Bluegriffon < Cask
   version '1.7.2'
   sha256 'a54cfe176dbc67ffa4fdd20b41447fb4c4f2fbf1f80282d81e93e63b5d4b996f'
 
-  url 'http://bluegriffon.org/freshmeat/1.7.2/bluegriffon-1.7.2.mac.dmg'
+  url "http://bluegriffon.org/freshmeat/#{version}/bluegriffon-#{version}.mac.dmg"
   homepage 'http://bluegriffon.org'
 
   app 'BlueGriffon.app'
