fb37c182e6b97749a3b9cb657a76467d805633c1
Roland Walker <walker@pobox.com>
2014-09-26 08:09:09 -0400

    re-use version in refresh-finder

--- Casks/refresh-finder.rb
+++ Casks/refresh-finder.rb
@@ -2,9 +2,9 @@ class RefreshFinder < Cask
   version '1.5.0'
   sha256 'd7e5faec85b3910776ca77f09c98b1b90e540cc3d493853e46eaeb8c7332472e'
 
-  url 'http://soderhavet.com/refresh/Refresh_Finder_1.5.0.dmg.zip'
+  url "http://soderhavet.com/refresh/Refresh_Finder_#{version}.dmg.zip"
   homepage 'http://soderhavet.com/refresh/refresh-finder/'
 
-  nested_container 'Refresh_Finder_1.5.0.dmg'
+  nested_container "Refresh_Finder_#{version}.dmg"
   app 'Refresh Finder.app'
 end
