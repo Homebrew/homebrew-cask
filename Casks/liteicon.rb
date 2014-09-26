5e983bce64171d076d404ab2e55c145474f4bc61
Roland Walker <walker@pobox.com>
2014-09-26 08:09:00 -0400

    re-use version in liteicon

--- Casks/liteicon.rb
+++ Casks/liteicon.rb
@@ -2,7 +2,7 @@ class Liteicon < Cask
   version '3.2.1'
   sha256 '94642d3d6ea84276bc9418daa865e35b306eb7ec0118d3934e9ba51bbb8ae96d'
 
-  url 'http://www.freemacsoft.net/downloads/LiteIcon_3.2.1.zip'
+  url "http://www.freemacsoft.net/downloads/LiteIcon_#{version}.zip"
   homepage 'http://www.freemacsoft.net/liteicon/'
 
   app 'LiteIcon.app'
