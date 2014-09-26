7e3d7fd5ec5144908e050cd1f16cd3583576f95b
Roland Walker <walker@pobox.com>
2014-09-26 08:09:12 -0400

    re-use version in skitch

--- Casks/skitch.rb
+++ Casks/skitch.rb
@@ -2,7 +2,7 @@ class Skitch < Cask
   version '2.7.2'
   sha256 '2388c60445782ac7afd1c7fc311007156a98b8e7b5488ede65cc5a68263da597'
 
-  url 'http://cdn1.evernote.com/skitch/mac/release/Skitch-2.7.2.zip'
+  url "http://cdn1.evernote.com/skitch/mac/release/Skitch-#{version}.zip"
   homepage 'http://evernote.com/skitch/'
 
   app 'Skitch.app'
