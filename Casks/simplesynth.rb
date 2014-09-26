1e768f709915db6869b167a1ca7bb6934b671751
Roland Walker <walker@pobox.com>
2014-09-26 08:09:12 -0400

    re-use version in simplesynth

--- Casks/simplesynth.rb
+++ Casks/simplesynth.rb
@@ -2,7 +2,7 @@ class Simplesynth < Cask
   version '1.1'
   sha256 '5d70295d5a72075a26b158f5a92b1488170e8ff569f0a52c4e5e5bcfe7ffcdb2'
 
-  url 'https://s3.amazonaws.com/notahat/SimpleSynth-1.1.zip'
+  url "https://s3.amazonaws.com/notahat/SimpleSynth-#{version}.zip"
   homepage 'http://notahat.com/simplesynth/'
 
   app 'SimpleSynth.app'
