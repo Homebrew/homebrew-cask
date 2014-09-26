dd996943bc1ffbb00d6d3fcaf265e76011c488b4
Roland Walker <walker@pobox.com>
2014-09-26 08:09:03 -0400

    re-use version in mplayerx

--- Casks/mplayerx.rb
+++ Casks/mplayerx.rb
@@ -2,7 +2,7 @@ class Mplayerx < Cask
   version '1.0.22.1'
   sha256 '08ce85671814e65b8c7ec8438b85be593b6deaf7d5c3b242e686a6b0176a2c77'
 
-  url 'https://downloads.sourceforge.net/project/mplayerx-osx/MPlayerX-1.0.22.1.zip'
+  url "https://downloads.sourceforge.net/project/mplayerx-osx/MPlayerX-#{version}.zip"
   homepage 'http://mplayerx.org/'
 
   app 'MPlayerX.app'
