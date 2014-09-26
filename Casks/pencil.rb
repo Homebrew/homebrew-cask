ae1a9b5e07f54252b582dc93537e392b7274b871
Roland Walker <walker@pobox.com>
2014-09-26 08:09:06 -0400

    re-use version in pencil

--- Casks/pencil.rb
+++ Casks/pencil.rb
@@ -2,7 +2,7 @@ class Pencil < Cask
   version '2.0.6'
   sha256 'dacd76c658b12101457d17a4ade0b3f9c6a012f8eddacd379c41e372545ffac6'
 
-  url 'https://evoluspencil.googlecode.com/files/Pencil-2.0.6-mac.tar.bz2'
+  url "https://evoluspencil.googlecode.com/files/Pencil-#{version}-mac.tar.bz2"
   homepage 'http://pencil.evolus.vn'
 
   app 'Pencil.app'
