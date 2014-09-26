cdc50de708fdfa9e4a3561a20057f3a4f8449c58
Roland Walker <walker@pobox.com>
2014-09-26 08:09:05 -0400

    re-use version in panic-unison

--- Casks/panic-unison.rb
+++ Casks/panic-unison.rb
@@ -2,7 +2,7 @@ class PanicUnison < Cask
   version '2.1.10'
   sha256 'd7b15d7726dc892caa72a7476219ed3d06e86df704757670bb3236a82bdde5dc'
 
-  url 'http://download.panic.com/Unison/Unison%202.1.10.zip'
+  url "http://download.panic.com/Unison/Unison%20#{version}.zip"
   appcast 'http://www.panic.com/updates/update.php'
   homepage 'http://panic.com/unison/'
 
