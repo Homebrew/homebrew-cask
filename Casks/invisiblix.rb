a4da2767ef6abe009b26742833228cb72fbdf6e9
Roland Walker <walker@pobox.com>
2014-09-26 08:08:58 -0400

    re-use version in invisiblix

--- Casks/invisiblix.rb
+++ Casks/invisiblix.rb
@@ -2,7 +2,7 @@ class Invisiblix < Cask
   version '3.2'
   sha256 '528328a0e7c3f0a72e763ea766324d491dfe20d6f18a2882eefda1a5a2c4d68e'
 
-  url 'https://downloads.sourceforge.net/project/invisiblix/invisibliX-3.2.zip'
+  url "https://downloads.sourceforge.net/project/invisiblix/invisibliX-#{version}.zip"
   appcast 'http://www.read-write.fr/invisiblix/appcast.xml'
   homepage 'http://www.read-write.fr/invisiblix/'
 
