cdaa2c6fea7ccabbc2a69a9fa58c1b2b00d9f09f
Roland Walker <walker@pobox.com>
2014-09-26 08:08:52 -0400

    re-use version in dia

--- Casks/dia.rb
+++ Casks/dia.rb
@@ -2,7 +2,7 @@ class Dia < Cask
   version '0.97.2-7'
   sha256 '9d3038c01347716800688830eaf52204deb78affe74a5f0c6e0a48fd414d44be'
 
-  url 'https://downloads.sourceforge.net/dia-installer/dia/0.97.2/Dia-0.97.2-7.dmg'
+  url "https://downloads.sourceforge.net/dia-installer/dia/0.97.2/Dia-#{version}.dmg"
   homepage 'http://dia-installer.de/'
 
   app 'Dia.app'
