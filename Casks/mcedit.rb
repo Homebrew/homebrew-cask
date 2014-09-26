7d4505e71390258f3acafbfcc21871f6267d3ead
Roland Walker <walker@pobox.com>
2014-09-26 08:09:02 -0400

    re-use version in mcedit

--- Casks/mcedit.rb
+++ Casks/mcedit.rb
@@ -2,7 +2,7 @@ class Mcedit < Cask
   version '0.1.7.1'
   sha256 '9ab4e66a3d4d898921cd38725da36567931fe4b8d0285c3a73dba6bef35fbea4'
 
-  url 'https://bitbucket.org/codewarrior0/mcedit/downloads/MCEdit-0.1.7.1.macosx-10_6-x86_64.zip'
+  url "https://bitbucket.org/codewarrior0/mcedit/downloads/MCEdit-#{version}.macosx-10_6-x86_64.zip"
   homepage 'http://www.mcedit.net'
 
   app 'MCEdit.app'
