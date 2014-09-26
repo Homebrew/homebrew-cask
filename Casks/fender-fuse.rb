1de72057be8655748470dd59c80d27166be4fc33
Roland Walker <walker@pobox.com>
2014-09-26 08:08:54 -0400

    re-use version in fender-fuse

--- Casks/fender-fuse.rb
+++ Casks/fender-fuse.rb
@@ -2,7 +2,7 @@ class FenderFuse < Cask
   version '2.7.1'
   sha256 'e68de1a1c1068d34dda354e2678ddac4a796b2ccdface95b034a438455442919'
 
-  url 'https://www.fmicassets.com/fender/support/software/fender_software/fender_fuse/mac/FenderFUSE_FULL_2.7.1.dmg'
+  url "https://www.fmicassets.com/fender/support/software/fender_software/fender_fuse/mac/FenderFUSE_FULL_#{version}.dmg"
   homepage 'https://fuse.fender.com/'
 
   pkg 'Fender FUSE Installer.app/Contents/Resources/Fender FUSE.pkg'
