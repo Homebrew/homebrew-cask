2bfb3acf5c880e428757435a44b4aa3c5a7ea5b7
Roland Walker <walker@pobox.com>
2014-09-26 08:09:02 -0400

    re-use version in mamp

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -2,10 +2,10 @@ class Mamp < Cask
   version '3.0.6'
   sha256 'c615d4107a8b1ffee204b93af3809f6d46e9ddc3dfe05d79dd65c41d2aeec050'
 
-  url 'http://downloads9.mamp.info/MAMP-PRO/releases/3.0.6/MAMP_MAMP_PRO_3.0.6.pkg'
+  url "http://downloads9.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
   homepage 'http://www.mamp.info/en/index.html'
 
-  pkg 'MAMP_MAMP_PRO_3.0.6.pkg'
+  pkg "MAMP_MAMP_PRO_#{version}.pkg"
   postflight do
     system '/usr/bin/sudo', '-E', '--',
            '/usr/sbin/chown', '-R', '--', "#{Etc.getpwuid(Process.euid).name}:staff", '/Applications/MAMP', '/Applications/MAMP PRO'
