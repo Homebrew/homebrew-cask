99c22c8ace4218d0b68ed7fb05689f951ebb953d
Roland Walker <walker@pobox.com>
2014-09-26 08:09:12 -0400

    re-use version in speedcrunch

--- Casks/speedcrunch.rb
+++ Casks/speedcrunch.rb
@@ -2,7 +2,7 @@ class Speedcrunch < Cask
   version '0.11'
   sha256 '1ce5ef9d167614a2e63daad43a23bd8df60b8ea641df6be9aabdf826bbb5a826'
 
-  url 'https://bitbucket.org/heldercorreia/speedcrunch/downloads/SpeedCrunch-0.11.dmg'
+  url "https://bitbucket.org/heldercorreia/speedcrunch/downloads/SpeedCrunch-#{version}.dmg"
   homepage 'http://www.speedcrunch.org'
 
   app 'SpeedCrunch.app'
