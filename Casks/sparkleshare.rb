24707b35320e419ec4264f62ce38da6a3a3e06e5
Roland Walker <walker@pobox.com>
2014-09-26 08:09:12 -0400

    re-use version in sparkleshare

--- Casks/sparkleshare.rb
+++ Casks/sparkleshare.rb
@@ -2,7 +2,7 @@ class Sparkleshare < Cask
   version '1.4'
   sha256 '571a8e39591883f0af6fb9056db8c1a0835cc0babb73b07050a188165552c038'
 
-  url 'https://bitbucket.org/hbons/sparkleshare/downloads/sparkleshare-mac-1.4.zip'
+  url "https://bitbucket.org/hbons/sparkleshare/downloads/sparkleshare-mac-#{version}.zip"
   homepage 'http://sparkleshare.org/'
 
   app 'SparkleShare.app'
