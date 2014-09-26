e889d86a25fe8f7c49ae07470fc30ae9310cf501
Roland Walker <walker@pobox.com>
2014-09-26 08:08:52 -0400

    re-use version in dockmod

--- Casks/dockmod.rb
+++ Casks/dockmod.rb
@@ -2,7 +2,7 @@ class Dockmod < Cask
   version '2.04'
   sha256 '86c92aa446d436296a800ee832466afa845048316a09df15d0e793f5a4cad55d'
 
-  url 'http://spyresoft.com/dockmod/download.php?version=2.04'
+  url "http://spyresoft.com/dockmod/download.php?version=#{version}"
   appcast 'http://spyresoft.com/dockmod/updates.xml'
   homepage 'http://spyresoft.com/dockmod/'
 
