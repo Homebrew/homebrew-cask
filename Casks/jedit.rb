5f01cf0f7207aea272c2cdc43f0b5fb55d4d335a
Roland Walker <walker@pobox.com>
2014-09-26 08:08:58 -0400

    re-use version in jedit

--- Casks/jedit.rb
+++ Casks/jedit.rb
@@ -2,7 +2,7 @@ class Jedit < Cask
   version '5.1.0'
   sha256 '29922afea1411631436f5f1154e45ddf8d3f6db53c9e41b832cefe86f8b81446'
 
-  url 'https://downloads.sourceforge.net/sourceforge/jedit/jedit5.1.0install.dmg'
+  url "https://downloads.sourceforge.net/sourceforge/jedit/jedit#{version}install.dmg"
   homepage 'http://www.jedit.org'
 
   app 'jEdit.app'
