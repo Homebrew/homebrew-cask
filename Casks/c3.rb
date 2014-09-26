db61f3656870a7a53b72f55665e8d1313058fb61
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in c3

--- Casks/c3.rb
+++ Casks/c3.rb
@@ -2,7 +2,7 @@ class C3 < Cask
   version '0.5.2.2143'
   sha256 '1f039168caf6487c4ded7fdec5ebf80f4400b03188a40b1bc06f0abc8bf3d3ff'
 
-  url 'https://d2xj26462na9l3.cloudfront.net/c3/C3-FNF-0.5.2.2143.dmg'
+  url "https://d2xj26462na9l3.cloudfront.net/c3/C3-FNF-#{version}.dmg"
   homepage 'http://www.downloadc3.com/'
 
   app 'c3.app'
