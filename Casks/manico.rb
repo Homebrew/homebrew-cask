ff7e065d98abe67bc035ee9ea83c1c94557d2032
Keng Kiat Lim <kkin@crnrc.com>
2014-09-26 15:58:59 +0800

    Upgrade Manico to 1.4.1

--- Casks/manico.rb
+++ Casks/manico.rb
@@ -1,8 +1,8 @@
 class Manico < Cask
-  version '1.2.1'
-  sha256 'e671efbc13693f51bc55606b9e0f44370dad306dbd5a197abfda8c62961d0008'
+  version '1.4.1'
+  sha256 'e29c4b0ccba830f0296fbec27122b2016c6dae0f76d5635d5d080d2031fe2d88'
 
-  url 'http://manico.im/static/Manico_1.2.1.dmg'
+  url "http://manico.im/static/Manico_#{version}.dmg"
   appcast 'http://manico.im/static/manico-appcast.xml'
   homepage 'http://manico.im/'
 
