7dd1e49a3b2f5557507da9ae2ae7bcf717af0006
Roland Walker <walker@pobox.com>
2014-09-26 08:09:08 -0400

    re-use version in qlmarkdown

--- Casks/qlmarkdown.rb
+++ Casks/qlmarkdown.rb
@@ -2,7 +2,7 @@ class Qlmarkdown < Cask
   version '1.3.3'
   sha256 '045712562665673924397bbbef1ee1157b44e23c9744feda6feda27e107802d3'
 
-  url 'https://github.com/toland/qlmarkdown/releases/download/v1.3.3/QLMarkdown.qlgenerator.zip'
+  url "https://github.com/toland/qlmarkdown/releases/download/v#{version}/QLMarkdown.qlgenerator.zip"
   homepage 'https://github.com/toland/qlmarkdown'
 
   # Fix broken zip file with no toplevel bundle directory.  This was
