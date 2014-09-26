6ca18a220ec6c1b9def3f1383185ff05390001a6
Roland Walker <walker@pobox.com>
2014-09-26 08:09:14 -0400

    re-use version in synthesia

--- Casks/synthesia.rb
+++ Casks/synthesia.rb
@@ -2,7 +2,7 @@ class Synthesia < Cask
   version '9.0'
   sha256 '4b3ed729994c4a76ca0bb777516f0737f218eb033d9e02e8af2a5a1a531f6ce8'
 
-  url 'https://synthesia.s3.amazonaws.com/files/Synthesia-9.0.dmg'
+  url "https://synthesia.s3.amazonaws.com/files/Synthesia-#{version}.dmg"
   homepage 'http://www.synthesiagame.com'
 
   app 'Synthesia.app'
