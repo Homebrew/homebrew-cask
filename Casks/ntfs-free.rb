2f96874ef1ced0d4e00b4dc799151951142d91dc
Roland Walker <walker@pobox.com>
2014-09-26 08:09:04 -0400

    re-use version in ntfs-free

--- Casks/ntfs-free.rb
+++ Casks/ntfs-free.rb
@@ -2,10 +2,10 @@ class NtfsFree < Cask
   version '10.8.2'
   sha256 '1bc7122a2d869b740a35e2dba1fe29c380f21ad07481fc0a46b2f55bf79c574e'
 
-  url 'https://downloads.sourceforge.net/sourceforge/ntfsfree/NTFS-free-10.8.2.pkg'
+  url "https://downloads.sourceforge.net/sourceforge/ntfsfree/NTFS-free-#{version}.pkg"
   homepage 'http://sourceforge.net/projects/ntfsfree/'
 
-  pkg 'NTFS-free-10.8.2.pkg'
+  pkg "NTFS-free-#{version}.pkg"
   uninstall :delete => [
                         '/System/Library/Extensions/ntfs-free.kext',
                         '/sbin/mount_ntfs-free',
