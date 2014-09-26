7b1fb888f530ab193aeafec8e60c0bb011f4e6d7
Roland Walker <walker@pobox.com>
2014-09-26 08:08:47 -0400

    re-use version in banshee

--- Casks/banshee.rb
+++ Casks/banshee.rb
@@ -2,7 +2,7 @@ class Banshee < Cask
   version '2.6.1'
   sha256 '26fc3c77a72860fc2dfaee0741c88e9b8d7b8dac6ece2943e42917112e55c933'
 
-  url 'http://ftp.gnome.org/pub/GNOME/binaries/mac/banshee/banshee-2.6.1.macosx.intel.dmg'
+  url "http://ftp.gnome.org/pub/GNOME/binaries/mac/banshee/banshee-#{version}.macosx.intel.dmg"
   homepage 'http://banshee.fm'
 
   app 'Banshee.app'
