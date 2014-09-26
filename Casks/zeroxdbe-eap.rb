3879b93c3545558303672c6a374017605e992cb0
Roland Walker <walker@pobox.com>
2014-09-26 08:09:20 -0400

    re-use version in zeroxdbe-eap

--- Casks/zeroxdbe-eap.rb
+++ Casks/zeroxdbe-eap.rb
@@ -2,7 +2,7 @@ class ZeroxdbeEap < Cask
   version '138.1400.3'
   sha256 '1f16618a29f004e5e846d6a7ccfa3afa2115886b94294b71c28781427a9589b5'
 
-  url 'http://download.jetbrains.com/dbe/0xdbe-138.1400.3.dmg'
+  url "http://download.jetbrains.com/dbe/0xdbe-#{version}.dmg"
   homepage 'http://www.jetbrains.com/dbe/'
 
   app '0xDBE EAP.app'
