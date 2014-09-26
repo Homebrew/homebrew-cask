95b3f5d2a915b6eac4a4468b3b5372f478a890b5
Roland Walker <walker@pobox.com>
2014-09-26 08:08:47 -0400

    re-use version in armory

--- Casks/armory.rb
+++ Casks/armory.rb
@@ -2,7 +2,7 @@ class Armory < Cask
   version '0.92.1'
   sha256 '75d6b7566d6c40a93f7dcf995dfb778b577d0b4698cc52d1ecea8f6fadfdf460'
 
-  url 'https://s3.amazonaws.com/bitcoinarmory-releases/armory_0.92.1_osx.tar.gz'
+  url "https://s3.amazonaws.com/bitcoinarmory-releases/armory_#{version}_osx.tar.gz"
   homepage 'https://bitcoinarmory.com/'
 
   app 'Armory.app'
