9dbc5376bbe52ec9ac76db4f2b85e8042d283ee3
Roland Walker <walker@pobox.com>
2014-09-26 08:09:20 -0400

    re-use version in youll-never-take-me-alive

--- Casks/youll-never-take-me-alive.rb
+++ Casks/youll-never-take-me-alive.rb
@@ -2,7 +2,7 @@ class YoullNeverTakeMeAlive < Cask
   version '0.9.9'
   sha256 'e055e96ed1816b30b420533af8f9de90da088b339d51aa99a5c20c636e8ba9c8'
 
-  url 'https://github.com/iSECPartners/yontma-mac/releases/download/0.9.9/yontma-0.9.9.dmg'
+  url "https://github.com/iSECPartners/yontma-mac/releases/download/#{version}/yontma-#{version}.dmg"
   homepage 'https://github.com/iSECPartners/yontma-mac'
 
   app 'You\'ll Never Take Me Alive!.app'
