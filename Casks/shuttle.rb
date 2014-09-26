7a96e2b22c8a6ee497ea87644cb72ea5df24560c
Roland Walker <walker@pobox.com>
2014-09-26 08:09:11 -0400

    re-use version in shuttle

--- Casks/shuttle.rb
+++ Casks/shuttle.rb
@@ -2,7 +2,7 @@ class Shuttle < Cask
   version '1.2.0'
   sha256 'a6a20b461556e54c99f14eaaaa4ee8e41cb0250561525be87340f0fda635d2b3'
 
-  url 'https://github.com/fitztrev/shuttle/releases/download/v1.2.0/Shuttle.dmg'
+  url "https://github.com/fitztrev/shuttle/releases/download/v#{version}/Shuttle.dmg"
   homepage 'http://fitztrev.github.io/shuttle/'
 
   app 'Shuttle.app'
