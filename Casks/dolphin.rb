ea28cc49555e7bc2412594813d01d661ecd1c9a3
Roland Walker <walker@pobox.com>
2014-09-26 08:08:52 -0400

    re-use version in dolphin

--- Casks/dolphin.rb
+++ Casks/dolphin.rb
@@ -2,7 +2,7 @@ class Dolphin < Cask
   version '4.0'
   sha256 'c5968f668225d76273c77ad4ddbcb1e9eb2fc5df59918395c04ebf53f80ca8a8'
 
-  url 'https://dl-mirror.dolphin-emu.org/4.0/dolphin-4.0.dmg'
+  url "https://dl-mirror.dolphin-emu.org/#{version}/dolphin-#{version}.dmg"
   homepage 'http://www.dolphin-emu.org/'
 
   app 'Dolphin.app'
