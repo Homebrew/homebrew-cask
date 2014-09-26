031ebda714b38fa4262280b90fb66c23a5c7ec82
Roland Walker <walker@pobox.com>
2014-09-26 08:09:05 -0400

    re-use version in openscad

--- Casks/openscad.rb
+++ Casks/openscad.rb
@@ -2,7 +2,7 @@ class Openscad < Cask
   version '2014.03'
   sha256 'c324c19c2d36f21517b602f8ae2ec04fa14c90896c91fc8dbb37aed5b3ba16bd'
 
-  url 'http://files.openscad.org/OpenSCAD-2014.03.dmg'
+  url "http://files.openscad.org/OpenSCAD-#{version}.dmg"
   homepage 'http://www.openscad.org/'
 
   app 'OpenSCAD.app'
