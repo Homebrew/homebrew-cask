5f258d94e3bda407f14a1ed3d8db5c439e494ac5
Roland Walker <walker@pobox.com>
2014-09-26 08:09:03 -0400

    re-use version in mpeg-streamclip

--- Casks/mpeg-streamclip.rb
+++ Casks/mpeg-streamclip.rb
@@ -2,7 +2,7 @@ class MpegStreamclip < Cask
   version '1.9.2'
   sha256 'f539e527a7232a9ac4398c0d3e7730010058b300c8d7fd33c4baf8c9ac232b85'
 
-  url 'http://www.alfanet.it/squared5/MPEG_Streamclip_1.9.2.dmg'
+  url "http://www.alfanet.it/squared5/MPEG_Streamclip_#{version}.dmg"
   homepage 'http://www.squared5.com/'
 
   app 'MPEG Streamclip.app'
