eace59dea280bee54b3e72d735232e05225bdba0
Roland Walker <walker@pobox.com>
2014-09-26 08:08:50 -0400

    re-use version in controlplane

--- Casks/controlplane.rb
+++ Casks/controlplane.rb
@@ -2,7 +2,7 @@ class Controlplane < Cask
   version '1.5.3'
   sha256 '3a0b807da7fa6f45ab507e44ff01209e4beae633b6e7708d4c61907de8b78fbd'
 
-  url 'http://www.controlplaneapp.com/download/1.5.3'
+  url "http://www.controlplaneapp.com/download/#{version}"
   appcast 'http://www.controlplaneapp.com/appcast.xml'
   homepage 'http://www.controlplaneapp.com/'
 
