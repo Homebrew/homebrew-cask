fc1f34a22968385933d33255bdae3181c563d822
Roland Walker <walker@pobox.com>
2014-09-26 08:09:20 -0400

    re-use version in yubiswitch

--- Casks/yubiswitch.rb
+++ Casks/yubiswitch.rb
@@ -2,7 +2,7 @@ class Yubiswitch < Cask
   version '0.7'
   sha256 '680577971e037ca52292e62809e0709adce1d79946a7d740627e83cda6d4d6ca'
 
-  url 'https://github.com/pallotron/yubiswitch/releases/download/v0.7/yubiswitch_0.7.dmg'
+  url "https://github.com/pallotron/yubiswitch/releases/download/v#{version}/yubiswitch_#{version}.dmg"
   homepage 'https://github.com/pallotron/yubiswitch'
 
   app 'yubiswitch.app'
