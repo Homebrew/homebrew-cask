856ef649ad054b8a8d6c0584a21b97abd99f7c24
Roland Walker <walker@pobox.com>
2014-09-26 08:09:12 -0400

    re-use version in splayerx

--- Casks/splayerx.rb
+++ Casks/splayerx.rb
@@ -2,7 +2,7 @@ class Splayerx < Cask
   version '1.1.8'
   sha256 '75a3decacd5a8efe5b6f5638999281159cb7ddf598d158a89f3d6e157c276433'
 
-  url 'http://cdn.bitbucket.org/Tomasen/splayerx/downloads/SPlayerX_1.1.8.zip'
+  url "http://cdn.bitbucket.org/Tomasen/splayerx/downloads/SPlayerX_#{version}.zip"
   homepage 'https://bitbucket.org/Tomasen/splayerx/wiki/Home'
 
   app 'SPlayerX.app'
