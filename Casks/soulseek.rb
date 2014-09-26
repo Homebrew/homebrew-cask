bc038a2d1f610c37f99280f104035d10a4da5821
Roland Walker <walker@pobox.com>
2014-09-26 10:58:33 -0400

    re-use version in soulseek

--- Casks/soulseek.rb
+++ Casks/soulseek.rb
@@ -2,9 +2,9 @@ class Soulseek < Cask
   version '2014-6-15'
   sha256 'fb583b2ac25e429e5db89931d208a86c7f03177140f944b31936daa02778dc4f'
 
-  url 'http://www.soulseekqt.net/SoulseekQt/Mac/SoulseekQt-2014-6-15.pkg'
+  url "http://www.soulseekqt.net/SoulseekQt/Mac/SoulseekQt-#{version}.pkg"
   homepage 'http://www.soulseekqt.net/'
 
-  pkg 'SoulseekQt-2014-6-15.pkg'
+  pkg "SoulseekQt-#{version}.pkg"
   uninstall :pkgutil => 'com.soulseek.SoulseekQt.pkg'
 end
