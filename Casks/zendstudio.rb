f5ffd960c00775b2f66bac919cd750ae3290285a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:20 -0400

    re-use version in zendstudio

--- Casks/zendstudio.rb
+++ Casks/zendstudio.rb
@@ -2,7 +2,7 @@ class Zendstudio < Cask
   version '10.6.1'
   sha256 '98cafacf5e2f08ae22561b4f9c756400e3c28202f1e97c345357816d3b38b40d'
 
-  url 'http://downloads.zend.com/studio-eclipse/10.6.1/ZendStudio-10.6.1-macosx.cocoa.x86_64.dmg'
+  url "http://downloads.zend.com/studio-eclipse/#{version}/ZendStudio-#{version}-macosx.cocoa.x86_64.dmg"
   homepage 'http://www.zend.com/en/products/studio/'
 
   app 'ZendStudio.app'
