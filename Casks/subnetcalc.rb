707d03bbcab353c656b198120b73a5dcd7ceb0e8
Roland Walker <walker@pobox.com>
2014-09-26 08:09:13 -0400

    re-use version in subnetcalc

--- Casks/subnetcalc.rb
+++ Casks/subnetcalc.rb
@@ -2,7 +2,7 @@ class Subnetcalc < Cask
   version '1.1'
   sha256 '19157baacac0cede6849356cb305a0ed2e4290e1e9e8ac4b691fd1d7ce1920cf'
 
-  url 'http://subnetcalc.free.fr/download/subnetcalc-1.1.dmg'
+  url "http://subnetcalc.free.fr/download/subnetcalc-#{version}.dmg"
   homepage 'http://subnetcalc.free.fr/'
 
   app 'SubnetCalc.app'
