6706578680bfd974ede21e613c9d4e11ced867b2
Roland Walker <walker@pobox.com>
2014-09-26 08:08:49 -0400

    re-use version in clementine

--- Casks/clementine.rb
+++ Casks/clementine.rb
@@ -2,7 +2,7 @@ class Clementine < Cask
   version '1.2.2'
   sha256 '7e3dac7fb184058e69afbf9cf68850debbbacdfbbd3023fbcdce7c159a2eed75'
 
-  url 'https://github.com/clementine-player/Clementine/releases/download/1.2.2/clementine-1.2.2.dmg'
+  url "https://github.com/clementine-player/Clementine/releases/download/#{version}/clementine-#{version}.dmg"
   appcast 'https://clementine-data.appspot.com/sparkle'
   homepage 'http://www.clementine-player.org/'
 
