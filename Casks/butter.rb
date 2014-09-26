12ecb22c0f39963b409a07a10ab88c9aba180789
Roland Walker <walker@pobox.com>
2014-09-26 08:08:48 -0400

    re-use version in butter

--- Casks/butter.rb
+++ Casks/butter.rb
@@ -2,7 +2,7 @@ class Butter < Cask
   version '0.1.4'
   sha256 '98cdd021f1c940ac1066600bef344400c3f932747882e971096152397f19b350'
 
-  url 'https://github.com/harukasan/butter/releases/download/v0.1.4/Butter_0.1.4.dmg'
+  url "https://github.com/harukasan/butter/releases/download/v#{version}/Butter_#{version}.dmg"
   homepage 'http://harukasan.jp'
 
   app 'Butter.app'
