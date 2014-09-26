24c30c5d8b11546ddcb062f4c714f508d24777fa
Roland Walker <walker@pobox.com>
2014-09-26 08:09:16 -0400

    re-use version in totals

--- Casks/totals.rb
+++ Casks/totals.rb
@@ -2,7 +2,7 @@ class Totals < Cask
   version '2.2.11'
   sha256 '594c58ea110bef590b03aa03a495f7e086cdda9f7fa8a9e6fe4c5af1fb892d09'
 
-  url 'http://www.kedisoft.com/dl/totals/Totals-2.2.11.dmg'
+  url "http://www.kedisoft.com/dl/totals/Totals-#{version}.dmg"
   homepage 'http://www.kedisoft.com/totals/'
 
   app 'Totals.app'
