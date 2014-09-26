0b8bad7e439875cd949cde5f396e1983d569709a
Roland Walker <walker@pobox.com>
2014-09-26 08:09:08 -0400

    re-use version in pusher

--- Casks/pusher.rb
+++ Casks/pusher.rb
@@ -2,7 +2,7 @@ class Pusher < Cask
   version '0.4.3'
   sha256 'cb52416dd88e7a0afc0e7492860fed0bd7f4568a9ef5ae75befb3216cec36fdb'
 
-  url 'https://github.com/noodlewerk/NWPusher/releases/download/0.4.3/pusher.app.zip'
+  url "https://github.com/noodlewerk/NWPusher/releases/download/#{version}/pusher.app.zip"
   homepage 'https://github.com/noodlewerk/NWPusher'
 
   app 'Pusher.app'
