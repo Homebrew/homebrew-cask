76b836193757d8834bf6a233c096791420513d96
Roland Walker <walker@pobox.com>
2014-09-26 08:08:55 -0400

    re-use version in gas-mask

--- Casks/gas-mask.rb
+++ Casks/gas-mask.rb
@@ -2,7 +2,7 @@ class GasMask < Cask
   version '0.8'
   sha256 '16353247ea14da7675578fda340005645c4610a478a748c1ed410296c3dab52a'
 
-  url 'http://gmask.clockwise.ee/files/gas_mask_0.8.zip'
+  url "http://gmask.clockwise.ee/files/gas_mask_#{version}.zip"
   appcast 'http://gmask.clockwise.ee/check_update/'
   homepage 'http://www.clockwise.ee/gasmask/'
 
