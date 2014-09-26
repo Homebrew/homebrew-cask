bf01edc65030e67eeb36d27258594da14d3024ed
Roland Walker <walker@pobox.com>
2014-09-26 08:09:03 -0400

    re-use version in mixxx

--- Casks/mixxx.rb
+++ Casks/mixxx.rb
@@ -2,7 +2,7 @@ class Mixxx < Cask
   version '1.11.0'
   sha256 '21fa06153a1a019c001f96024caa940ea6307a29f4cebd334fb2b7f071013314'
 
-  url 'http://downloads.mixxx.org/mixxx-1.11.0/mixxx-1.11.0-macintel.dmg'
+  url "http://downloads.mixxx.org/mixxx-#{version}/mixxx-#{version}-macintel.dmg"
   homepage 'http://www.mixxx.org'
 
   app 'Mixxx.app'
