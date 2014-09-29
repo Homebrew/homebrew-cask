58d05482f1d88f07eb66d392281c248e5f97abbb
Shaun Jackman <sjackman@gmail.com>
2014-09-29 12:56:08 -0700

    manopen 2.6: New formula
    Closes #6388.

---
+++ Casks/manopen.rb
@@ -0,0 +1,11 @@
+class Manopen < Cask
+  version '2.6'
+  sha256 '7b383ca493b0b360bb58e65f7e7ce0a92383ff38c5221cc410eaf03f1117a958'
+
+  url "http://www.clindberg.org/projects/ManOpen-#{version}.dmg"
+  homepage 'http://www.clindberg.org/projects/ManOpen.html'
+
+  app 'ManOpen.app'
+  binary 'openman'
+  artifact 'openman.1', :target => '/usr/local/share/man/man1/openman.1'
+end
