3dcb7355fa3391f9139f0aa92f7f81266101d667
David House <dave.a.house@gmail.com>
2014-09-29 13:19:13 -0600

    Add bs2b-au.component audio plugin
    bs2b/crossfeed plugin from MagisterLeoninus
    https://github.com/MagisterLeoninus/bs2b-au

---
+++ Casks/bs2b-au.rb
@@ -0,0 +1,7 @@
+class Bs2bAu < Cask
+  version ''
+  homepage 'https://github.com/MagisterLeoninus/bs2b-au'
+  sha256 'c202e794dd24708b870f179e71ea2e56d2374702a95fd1f7e9563e768603cd4f'
+  url 'https://github.com/downloads/MagisterLeoninus/bs2b-au/bs2b-au.component.zip'
+  artifact 'bs2b-au.component', :target => '../Library/Audio/Plug-Ins/Components/bs2b-au.component'
+end
