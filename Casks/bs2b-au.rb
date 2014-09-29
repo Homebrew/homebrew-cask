79fed9cdf23e0310d83170e87ef8fc37824d3484
David House <dave.a.house@gmail.com>
2014-09-29 13:29:26 -0600

    note caveat target location

--- Casks/bs2b-au.rb
+++ Casks/bs2b-au.rb
@@ -4,4 +4,5 @@ class Bs2bAu < Cask
   sha256 'c202e794dd24708b870f179e71ea2e56d2374702a95fd1f7e9563e768603cd4f'
   url 'https://github.com/downloads/MagisterLeoninus/bs2b-au/bs2b-au.component.zip'
   artifact 'bs2b-au.component', :target => '../Library/Audio/Plug-Ins/Components/bs2b-au.component'
+  caveats 'This installs bs2b-au.component to your ~/Library/Audio/Plug-Ins/Components/'
 end
