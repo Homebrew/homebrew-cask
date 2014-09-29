a352b3a63383d2d05f8a07e6558663f5b3dfdabc
Jason Riddle <jason-riddle@users.noreply.github.com>
2014-09-29 15:13:12 -0400

    Updated to correct sha256 sum

--- Casks/zeroxdbe-eap.rb
+++ Casks/zeroxdbe-eap.rb
@@ -1,6 +1,6 @@
 class ZeroxdbeEap < Cask
   version '138.2221.3'
-  sha256 'be7f67e2d4e21351dcb8a6f935ec975a23b0b54e'
+  sha256 'fc88c53d55fa13330421aebf77c0fb136eaa3e6ccff8e08ed577e51c53195fba'
 
   url "http://download.jetbrains.com/dbe/0xdbe-#{version}.dmg"
   homepage 'http://www.jetbrains.com/dbe/'
