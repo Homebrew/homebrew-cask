baa55763106b23930707fe67b7c25dea08e9faef
dwihn0r <dwihn0r@users.noreply.github.com>
2014-09-30 13:05:06 +0200

    Add Jump Desktop.

---
+++ Casks/jump-desktop.rb
@@ -0,0 +1,9 @@
+class JumpDesktop < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://jumpdesktop.com/downloads/jdmac'
+  homepage 'http://jumpdesktop.com/#jdmac'
+
+  app 'Jump Desktop.app'
+end
