6d006b3cb2c1688f158bc3ee6568c826b2daf7fb
dwihn0r <dwihn0r@users.noreply.github.com>
2014-09-30 13:01:01 +0200

    Add FStream.

---
+++ Casks/fstream.rb
@@ -0,0 +1,9 @@
+class Fstream < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.sourcemac.com/fstream_FStream.tgz'
+  homepage 'http://www.sourcemac.com/?page=fstream'
+
+  app 'FStream.app'
+end
