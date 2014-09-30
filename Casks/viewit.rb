91ddfc26174d873ec36fe05e4a13949d326fdf5c
dwihn0r <dwihn0r@users.noreply.github.com>
2014-09-30 13:10:09 +0200

    Add ViewIt 2.50.

---
+++ Casks/viewit.rb
@@ -0,0 +1,9 @@
+class Viewit < Cask
+  version '2.50'
+  sha256 '188f82c1294a2b63f817c3cd1897ff2c43af71d29305a4db614a9378994e7014'
+
+  url "http://www.hexcat.com/downloads/ViewIt-#{version}.dmg"
+  homepage 'http://www.hexcat.com/viewit/'
+
+  app 'ViewIt.app'
+end
