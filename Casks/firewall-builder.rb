ebb5baba57ce8d9a29a3db6d076960303ab8805f
Vítor Galvão <info@vitorgalvao.com>
2014-09-30 23:12:16 +0100

    don't :target rename firewall-builder

--- Casks/firewall-builder.rb
+++ Casks/firewall-builder.rb
@@ -5,5 +5,5 @@ class FirewallBuilder < Cask
   url "http://downloads.sourceforge.net/sourceforge/fwbuilder/fwbuilder-#{version}.dmg"
   homepage 'http://www.fwbuilder.org'
 
-  app "fwbuilder-#{version}.app", :target => 'Firewall Builder.app'
+  app "fwbuilder-#{version}.app"
 end
