2cee90b42d5ccb9b8701307047334547510681c5
Vítor Galvão <info@vitorgalvao.com>
2014-10-01 00:28:24 +0100

    removed app stanza from google-nik-collection

--- Casks/google-nik-collection.rb
+++ Casks/google-nik-collection.rb
@@ -5,9 +5,7 @@ class GoogleNikCollection < Cask
   url 'http://dl.google.com/dl/edgedl/photos/nikcollection-latest.dmg'
   homepage 'https://www.google.com/nikcollection/'
 
-  app 'Nik Collection.app', :target => 'Install Nik Collection.app'
-
   caveats do
-    manual_installer('Install Nik Collection.app')
+    manual_installer 'Nik Collection.app'
   end
 end
