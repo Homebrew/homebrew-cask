48770f72a3f10a1e99aba1ebd48e5e8dbc1b1a1f
Roland Walker <walker@pobox.com>
2014-10-01 11:33:40 -0400

    add license stanza to preferencecleaner

--- Casks/preferencecleaner.rb
+++ Casks/preferencecleaner.rb
@@ -4,6 +4,7 @@ class Preferencecleaner < Cask
 
   url 'http://www.echomist.co.uk/software/downloads/PreferenceCleaner.dmg'
   homepage 'http://www.echomist.co.uk/software/PreferenceCleaner.shtml'
+  license :unknown
 
   app 'PreferenceCleaner.app'
 end
