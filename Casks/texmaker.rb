3bdb64bcb8e2ae1679c2d83f08d3301341256d75
Roland Walker <walker@pobox.com>
2014-10-01 11:34:04 -0400

    add license stanza to texmaker

--- Casks/texmaker.rb
+++ Casks/texmaker.rb
@@ -4,6 +4,7 @@ class Texmaker < Cask
 
   url 'http://www.xm1math.net/texmaker/TexmakerMacosxLion.zip'
   homepage 'http://www.xm1math.net/texmaker'
+  license :unknown
 
   app 'TexmakerMacosxLion/texmaker.app'
 end
