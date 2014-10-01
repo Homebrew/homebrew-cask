1b35dbf0f812bc737e349b5564b60a952fe8be79
Roland Walker <walker@pobox.com>
2014-10-01 11:32:51 -0400

    add license stanza to faux-pas

--- Casks/faux-pas.rb
+++ Casks/faux-pas.rb
@@ -5,6 +5,7 @@ class FauxPas < Cask
   url 'http://api.fauxpasapp.com/download_latest'
   appcast 'http://api.fauxpasapp.com/appcast'
   homepage 'http://fauxpasapp.com'
+  license :unknown
 
   app 'FauxPas.app'
 end
