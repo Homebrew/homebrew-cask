6fb98f2d14c1a5c6b30bd4ecc99f5f3fc2a05869
Roland Walker <walker@pobox.com>
2014-10-01 11:32:42 -0400

    add license stanza to coqide

--- Casks/coqide.rb
+++ Casks/coqide.rb
@@ -4,6 +4,7 @@ class Coqide < Cask
 
   url "http://coq.inria.fr/distrib/V#{version}/files/coqide-#{version}.dmg"
   homepage 'http://coq.inria.fr/'
+  license :unknown
 
   app "CoqIde_#{version}.app"
 end
