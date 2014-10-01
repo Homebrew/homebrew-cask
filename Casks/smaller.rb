abb16fe22e52becba75594736c677086e9a26cf4
Roland Walker <walker@pobox.com>
2014-10-01 11:33:55 -0400

    add license stanza to smaller

--- Casks/smaller.rb
+++ Casks/smaller.rb
@@ -5,6 +5,7 @@ class Smaller < Cask
   url 'http://smallerapp.com/download/Smaller.zip'
   appcast 'http://smallerapp.com/up/updates.xml'
   homepage 'http://smallerapp.com/'
+  license :unknown
 
   app 'Smaller.app'
 end
