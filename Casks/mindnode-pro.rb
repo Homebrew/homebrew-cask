81f20dbae7abddd217a42abacf9c24240c016c34
Roland Walker <walker@pobox.com>
2014-10-01 11:33:23 -0400

    add license stanza to mindnode-pro

--- Casks/mindnode-pro.rb
+++ Casks/mindnode-pro.rb
@@ -5,6 +5,7 @@ class MindnodePro < Cask
   url 'https://www.mindnode.com/download/MindNodePro.zip'
   appcast 'https://www.mindnode.com/softwareupdate/mindnodepro.xml'
   homepage 'https://mindnode.com/'
+  license :unknown
 
   app 'MindNode Pro.app'
 end
