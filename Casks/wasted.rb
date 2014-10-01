9eebec1e156ba38a996177a038fa301fafb3f9e2
Roland Walker <walker@pobox.com>
2014-10-01 11:34:15 -0400

    add license stanza to wasted

--- Casks/wasted.rb
+++ Casks/wasted.rb
@@ -5,6 +5,7 @@ class Wasted < Cask
   url 'http://wasted.werk01.de/Wasted.zip'
   appcast 'http://wasted.werk01.de/appcast.xml'
   homepage 'http://wasted.werk01.de'
+  license :unknown
 
   app 'Wasted.app'
 end
