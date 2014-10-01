0397faa062ff2754b57442f7fdaa8358ccf0f58a
Roland Walker <walker@pobox.com>
2014-10-01 11:32:32 -0400

    add license stanza to battery-report

--- Casks/battery-report.rb
+++ Casks/battery-report.rb
@@ -5,6 +5,7 @@ class BatteryReport < Cask
   url 'https://www.dssw.co.uk/batteryreport/dsswbatteryreport.dmg'
   appcast 'http://version.dssw.co.uk/batteryreport/standard'
   homepage 'https://www.dssw.co.uk/batteryreport'
+  license :unknown
 
   app 'Battery Report.app'
 end
