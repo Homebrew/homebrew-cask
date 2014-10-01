c52bad49e6426cdbb34e75fa8af36ade8df553b3
Roland Walker <walker@pobox.com>
2014-10-01 11:33:45 -0400

    add license stanza to recordit

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -5,6 +5,7 @@ class Recordit < Cask
   url 'http://recordit.co/latest'
   appcast 'https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790'
   homepage 'http://recordit.co/'
+  license :unknown
 
   app 'RecordIt.app'
 end
