edf81e0b12cb001139a5866ddcc9abd42afeceb6
Roland Walker <walker@pobox.com>
2014-10-01 11:32:41 -0400

    add license stanza to colorpicker-antetype

--- Casks/colorpicker-antetype.rb
+++ Casks/colorpicker-antetype.rb
@@ -4,6 +4,7 @@ class ColorpickerAntetype < Cask
 
   url 'http://www.antetype.com/blog/downloads/AntetypeColorPickerOSX-1-3-1.zip'
   homepage 'http://www.antetype.com/blog/2013/12/updated-antetype-os-x-color-picker-1-3-1/'
+  license :unknown
 
   colorpicker 'AntetypeColorPicker.colorPicker'
 end
