c7ac4cd5902c0e6b3350f803d4bed0c1e9a9b099
Roland Walker <walker@pobox.com>
2014-10-01 11:32:59 -0400

    add license stanza to gopanda

--- Casks/gopanda.rb
+++ Casks/gopanda.rb
@@ -4,6 +4,7 @@ class Gopanda < Cask
 
   url 'http://pandanet-igs.com/gopanda2/installer/stable/mac-32/gopanda2-mac-32.zip'
   homepage 'http://pandanet-igs.com/communities/gopanda2'
+  license :unknown
 
   app 'GoPanda2.app'
 end
