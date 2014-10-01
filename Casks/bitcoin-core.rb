a7b91bd5174e8ea7eb56feeb9492d6edbcc53eb8
Michael Ford <fanquake@users.noreply.github.com>
2014-10-01 12:58:33 +0800

    Bitcoin Core 0.9.3

--- Casks/bitcoin-core.rb
+++ Casks/bitcoin-core.rb
@@ -1,8 +1,8 @@
 class BitcoinCore < Cask
-  version '0.9.2.1'
-  sha256 'b50c966932474a79e869dee4bb84dd155c98c9f458a4d31bcfbee9481ba3c6b4'
+  version '0.9.3'
+  sha256 '251938650bd79681dd93dcce346589aa5d1217d012a6f8e749165ef2149662d2'
 
-  url "https://bitcoin.org/bin/#{version}/bitcoin-#{version}-osx.dmg"
+  url "https://bitcoin.org/bin/#{version}/bitcoin-#{version}-macosx.dmg"
   homepage 'https://bitcoin.org/'
 
   app 'Bitcoin-Qt.app', :target => 'Bitcoin Core.app'
