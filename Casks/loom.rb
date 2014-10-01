90a62b4503b6b9725537b68962de3aeeb4c25dbb
Roland Walker <walker@pobox.com>
2014-10-01 11:33:17 -0400

    add license stanza to loom

--- Casks/loom.rb
+++ Casks/loom.rb
@@ -5,6 +5,7 @@ class Loom < Cask
   url 'https://loom.com/download/loom-mac.dmg'
   appcast 'http://www.loom.com/download/macupdate_1.1.xml'
   homepage 'http://loom.com/'
+  license :unknown
 
   app 'Loom.app'
 end
