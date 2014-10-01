8a8ec9af56a27c8fae0c0622eaaf344dd4ce97a7
Roland Walker <walker@pobox.com>
2014-10-01 11:33:03 -0400

    add license stanza to hoster

--- Casks/hoster.rb
+++ Casks/hoster.rb
@@ -5,6 +5,7 @@ class Hoster < Cask
   url "http://www.redwinder.com/macapp/hoster/tmp/Hoster#{version}08.zip"
   appcast 'http://www.redwinder.com/macapp/hoster/hoster12.xml'
   homepage 'http://www.redwinder.com/macapp/hoster/'
+  license :unknown
 
   pkg "Hoster#{version}08.mpkg", :allow_untrusted => true
   uninstall :pkgutil => 'com.redwinder.hoster.hoster.pkg'
