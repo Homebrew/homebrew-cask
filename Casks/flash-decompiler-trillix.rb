26607a2c6b9e2b1b89febc76a49a901a6dae8894
Roland Walker <walker@pobox.com>
2014-10-01 11:32:53 -0400

    add license stanza to flash-decompiler-trillix

--- Casks/flash-decompiler-trillix.rb
+++ Casks/flash-decompiler-trillix.rb
@@ -5,6 +5,7 @@ class FlashDecompilerTrillix < Cask
   url 'http://www.flash-decompiler.com/download/flash_decompiler.dmg'
   appcast 'http://www.eltima.com/download/fd-mac-update/fd-mac.xml'
   homepage 'http://www.flash-decompiler.com/mac.html'
+  license :unknown
 
   app 'Flash Decompiler Trillix.app'
 end
