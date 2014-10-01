38f4abade0332478612305a92ff3e4d4e398a723
Roland Walker <walker@pobox.com>
2014-10-01 11:33:10 -0400

    add license stanza to jing

--- Casks/jing.rb
+++ Casks/jing.rb
@@ -5,6 +5,7 @@ class Jing < Cask
   url 'http://download.techsmith.com/jing/mac/jing.dmg'
   appcast 'http://www.techsmith.com/redirect.asp?product=jing&ver=2.0.0&lang=enu&target=SparkleAppcast'
   homepage 'http://www.techsmith.com/jing.html'
+  license :unknown
 
   app 'Jing.app'
 end
