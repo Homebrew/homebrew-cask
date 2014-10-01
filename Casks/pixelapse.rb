f16684135ba0435f68b8358ca1ab18dd8e4b216e
Roland Walker <walker@pobox.com>
2014-10-01 08:11:57 -0400

    method spelling error in pixelapse.rb
    refs: #6417, #6416

--- Casks/pixelapse.rb
+++ Casks/pixelapse.rb
@@ -2,7 +2,7 @@ class Pixelapse < Cask
   version '2.0.0'
   sha256 '6822de800b29b02f7ff6f94ef28bbbb3c38aef05fad0cf6c5cb293623227547c'
 
-  url "https://s3.amazonaws.com/download-pixelapse/Pixelapse%20#{vesion}.dmg"
+  url "https://s3.amazonaws.com/download-pixelapse/Pixelapse%20#{version}.dmg"
   homepage 'https://www.pixelapse.com'
 
   app 'Pixelapse.app'
