d58d83a3d384cb7ce8cc4a355ae82bee20539b81
Roland Walker <walker@pobox.com>
2014-10-01 11:32:54 -0400

    add license stanza to fm-fw-installer

--- Casks/fm-fw-installer.rb
+++ Casks/fm-fw-installer.rb
@@ -4,6 +4,7 @@ class FmFwInstaller < Cask
 
   url 'http://www.flymaster-avionics.com/LinkClick.aspx?fileticket=Udjm4iuMhL8%3d&tabid=199&mid=804&forcedownload=true'
   homepage 'http://www.flymaster-avionics.com/Support/Downloads/ForMacOSX/tabid/199/Default.aspx'
+  license :unknown
 
   app 'Firmware Installer.app'
 end
