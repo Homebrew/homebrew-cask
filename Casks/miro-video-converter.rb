5969c09986ca0362888314b2558bfed4e0789f5a
Roland Walker <walker@pobox.com>
2014-10-01 11:33:23 -0400

    add license stanza to miro-video-converter

--- Casks/miro-video-converter.rb
+++ Casks/miro-video-converter.rb
@@ -5,6 +5,7 @@ class MiroVideoConverter < Cask
   url 'http://ftp.osuosl.org/pub/pculture.org/mirovideoconverter/mac/Miro%20Video%20Converter.dmg'
   appcast 'http://miro-updates.participatoryculture.org/mvc-appcast-osx.xml'
   homepage 'http://www.mirovideoconverter.com/'
+  license :unknown
 
   app 'Miro Video Converter.app'
 end
