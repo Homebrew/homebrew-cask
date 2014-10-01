14c2ee988ec82582b950f1eef20cd48f0b963112
Roland Walker <walker@pobox.com>
2014-10-01 11:33:11 -0400

    add license stanza to kext-drop

--- Casks/kext-drop.rb
+++ Casks/kext-drop.rb
@@ -5,6 +5,7 @@ class KextDrop < Cask
   url 'http://www.groths.org/kextdrop/KextDrop.dmg'
   appcast 'http://groths.org/kextdrop/updates/update.xml'
   homepage 'http://www.groths.org/software/kextdrop/'
+  license :unknown
 
   app 'Kext Drop.app'
 end
