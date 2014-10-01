c4cbf1007d0ce9f1b842307a83dc643260a3c614
Roland Walker <walker@pobox.com>
2014-10-01 11:33:34 -0400

    add license stanza to paragon-extfs

--- Casks/paragon-extfs.rb
+++ Casks/paragon-extfs.rb
@@ -4,6 +4,7 @@ class ParagonExtfs < Cask
 
   url 'http://dl.paragon-software.com/demo/extmac_trial_u.dmg'
   homepage 'http://www.paragon-software.com/home/extfs-mac/'
+  license :unknown
 
   pkg 'FSInstaller.app/Contents/Resources/Paragon ExtFS for Mac OS X.pkg'
 end
