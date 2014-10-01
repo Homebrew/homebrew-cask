ef64a5af4e2c4cf62c402da564dccdc73b248c59
Roland Walker <walker@pobox.com>
2014-10-01 11:33:46 -0400

    add license stanza to reveal

--- Casks/reveal.rb
+++ Casks/reveal.rb
@@ -5,6 +5,7 @@ class Reveal < Cask
   url 'http://download.revealapp.com/Reveal.app.zip'
   appcast 'http://download.revealapp.com/reveal-release.xml'
   homepage 'http://revealapp.com/'
+  license :unknown
 
   app 'Reveal.app'
 end
