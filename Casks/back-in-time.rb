62b46f3e0a6eec52ccedda0bd3f8bc72d60db1f7
Roland Walker <walker@pobox.com>
2014-10-01 11:32:31 -0400

    add license stanza to back-in-time

--- Casks/back-in-time.rb
+++ Casks/back-in-time.rb
@@ -4,6 +4,7 @@ class BackInTime < Cask
 
   url 'http://www.tri-edre.fr/pub/files/backintime3.dmg'
   homepage 'http://www.tri-edre.fr/english/backintime.html'
+  license :unknown
 
   app 'Back-In-Time 3.app'
 end
