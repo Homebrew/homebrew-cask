2009cd07f4ca1115b4c44eec4841292a22f2e2e8
Roland Walker <walker@pobox.com>
2014-10-01 11:34:08 -0400

    add license stanza to trailrunner

--- Casks/trailrunner.rb
+++ Casks/trailrunner.rb
@@ -5,6 +5,7 @@ class Trailrunner < Cask
   url 'http://downloads.trailrunnerx.com/TrailRunner.app.zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/83c4086e3f968b874757ba689e71f610'
   homepage 'http://trailrunnerx.com/'
+  license :unknown
 
   app 'TrailRunner.app'
 end
