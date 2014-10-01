3b8990f34df3ce48bdf59bbc70b443c78a21e754
Roland Walker <walker@pobox.com>
2014-10-01 11:33:26 -0400

    add license stanza to multifirefox

--- Casks/multifirefox.rb
+++ Casks/multifirefox.rb
@@ -5,6 +5,7 @@ class Multifirefox < Cask
   url 'https://mff.s3.amazonaws.com/MFF2_latest.dmg'
   appcast 'https://s3.amazonaws.com/mff_sparkle/MultiFirefoxAppcast2.xml'
   homepage 'http://davemartorana.com/multifirefox'
+  license :unknown
 
   app 'MultiFirefox.app'
 end
