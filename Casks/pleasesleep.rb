091b3b78fec1b4e08aec082111522c4b2105ab27
Roland Walker <walker@pobox.com>
2014-10-01 11:33:38 -0400

    add license stanza to pleasesleep

--- Casks/pleasesleep.rb
+++ Casks/pleasesleep.rb
@@ -4,6 +4,7 @@ class Pleasesleep < Cask
 
   url 'http://www.dragonone.com/downloads/vault/macosx/PleaseSleep.dmg'
   homepage 'http://www.dragonone.com/products/macosx/pleasesleep/'
+  license :unknown
 
   app 'PleaseSleep.app'
 end
