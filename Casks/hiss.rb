2a9a392bd218c629b3060ff6110e2cce4a2dcce0
Roland Walker <walker@pobox.com>
2014-10-01 11:33:02 -0400

    add license stanza to hiss

--- Casks/hiss.rb
+++ Casks/hiss.rb
@@ -5,6 +5,7 @@ class Hiss < Cask
   url 'http://collect3.com.au/hiss/Hiss.zip'
   appcast 'http://collect3.com.au/hiss/appcast.xml'
   homepage 'http://collect3.com.au/hiss/'
+  license :unknown
 
   app 'Hiss.app'
 end
