8e09569ae9b97e104fe77df228b2ac05d8e5e06f
Roland Walker <walker@pobox.com>
2014-10-01 11:33:24 -0400

    add license stanza to mono-mdk

--- Casks/mono-mdk.rb
+++ Casks/mono-mdk.rb
@@ -4,6 +4,7 @@ class MonoMdk < Cask
 
   url "http://download.mono-project.com/archive/#{version}/macos-10-x86/MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
   homepage 'http://mono-project.com/'
+  license :oss
 
   pkg "MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
   uninstall :pkgutil => 'com.xamarin.mono-MDK.pkg'
