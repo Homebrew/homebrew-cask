26290160d62e10f6cd3e251279444a8f3e90ff5e
Roland Walker <walker@pobox.com>
2014-10-01 11:33:24 -0400

    add license stanza to moneymoney

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -5,6 +5,7 @@ class Moneymoney < Cask
   url 'http://moneymoney-app.com/download/MoneyMoney.zip'
   appcast 'http://moneymoney-app.com/update/appcast.xml'
   homepage 'http://moneymoney-app.com/'
+  license :unknown
 
   app 'MoneyMoney.app'
 end
