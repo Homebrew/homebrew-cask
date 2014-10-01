ea4da021ba4fd379d755a00ed8405e3286a59b35
Roland Walker <walker@pobox.com>
2014-10-01 11:32:40 -0400

    add license stanza to cocoasplit

--- Casks/cocoasplit.rb
+++ Casks/cocoasplit.rb
@@ -4,6 +4,7 @@ class Cocoasplit < Cask
 
   url 'http://krylon.rsdio.com/zakk/cocoasplit/CocoaSplit-current.zip'
   homepage 'http://krylon.rsdio.com/zakk/cocoasplit/'
+  license :unknown
 
   app 'CocoaSplit.app'
 end
