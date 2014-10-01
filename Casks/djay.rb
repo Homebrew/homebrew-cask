f1f65fb355d118682b2ec19f6797d0968a82d5a5
Roland Walker <walker@pobox.com>
2014-10-01 11:32:46 -0400

    add license stanza to djay

--- Casks/djay.rb
+++ Casks/djay.rb
@@ -5,6 +5,7 @@ class Djay < Cask
   url 'https://www.algoriddim.com/files/djay.zip'
   appcast 'http://www.algoriddim.com/djay-mac/releasenotes/appcast'
   homepage 'http://algoriddim.com/djay-mac'
+  license :unknown
 
   app 'djay.app'
 end
