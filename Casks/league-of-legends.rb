9e729d763c45486884a1394dc5468dc1225dd68a
Roland Walker <walker@pobox.com>
2014-10-01 11:33:14 -0400

    add license stanza to league-of-legends

--- Casks/league-of-legends.rb
+++ Casks/league-of-legends.rb
@@ -4,6 +4,7 @@ class LeagueOfLegends < Cask
 
   url 'http://l3cdn.riotgames.com/Installer/NA_Mac_Installer/League%20of%20Legends%20NA.dmg'
   homepage 'http://signup.leagueoflegends.com/'
+  license :unknown
 
   app 'League of Legends.app'
   caveats do
