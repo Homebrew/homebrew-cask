32f66e0242f3b6d24465a3141c259a1b413ba13e
Roland Walker <walker@pobox.com>
2014-10-01 11:34:16 -0400

    add license stanza to wifimasterkey

--- Casks/wifimasterkey.rb
+++ Casks/wifimasterkey.rb
@@ -4,6 +4,7 @@ class Wifimasterkey < Cask
 
   url 'http://www.lianwifi.com/download/mac/WiFiMasterKey_Mac.dmg'
   homepage 'http://www.lianwifi.com/'
+  license :unknown
 
   app 'WiFiMasterKey.app'
 end
