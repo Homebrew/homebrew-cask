4689ef730d7b0e4573dca912684dbb76e4490fa6
Roland Walker <walker@pobox.com>
2014-10-01 11:33:49 -0400

    add license stanza to scratch

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -4,6 +4,7 @@ class Scratch < Cask
 
   url 'http://cdn.scratch.mit.edu/scratchr2/static/sa/Scratch.air'
   homepage 'http://scratch.mit.edu/scratch2download/'
+  license :unknown
 
   app 'Scratch 2.app'
 end
