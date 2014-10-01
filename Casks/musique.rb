1b89fef2daab6a339f2a4f9d35e742533cdde7b7
Roland Walker <walker@pobox.com>
2014-10-01 11:33:27 -0400

    add license stanza to musique

--- Casks/musique.rb
+++ Casks/musique.rb
@@ -5,6 +5,7 @@ class Musique < Cask
   url 'http://flavio.tordini.org/files/musique/musique.dmg'
   appcast 'http://flavio.tordini.org/musique-ws/appcast.xml'
   homepage 'http://flavio.tordini.org/musique'
+  license :unknown
 
   app 'Musique.app'
 end
