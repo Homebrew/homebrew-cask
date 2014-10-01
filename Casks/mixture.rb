3c3ac8e794ddb1009c7ab37f9332fd2d64c54af5
Roland Walker <walker@pobox.com>
2014-10-01 11:33:24 -0400

    add license stanza to mixture

--- Casks/mixture.rb
+++ Casks/mixture.rb
@@ -5,6 +5,7 @@ class Mixture < Cask
   url 'https://s3.amazonaws.com/mixture-mixed-app/mac/Mixture.zip'
   appcast 'https://s3.amazonaws.com/mixture-mixed-app/mac/appcast.xml'
   homepage 'http://mixture.io/'
+  license :unknown
 
   app 'Mixture.app'
 end
