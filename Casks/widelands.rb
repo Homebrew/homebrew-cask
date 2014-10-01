48bb724a07ef647d8659684f0db38a4da62306bd
Roland Walker <walker@pobox.com>
2014-10-01 11:34:16 -0400

    add license stanza to widelands

--- Casks/widelands.rb
+++ Casks/widelands.rb
@@ -4,6 +4,7 @@ class Widelands < Cask
 
   url 'https://launchpad.net/widelands/build18/build-18/+download/widelands-build18-mac.dmg'
   homepage 'https://wl.widelands.org/'
+  license :unknown
 
   app 'Widelands.app'
 end
