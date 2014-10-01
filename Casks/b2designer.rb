44e1601ec3e582feae72b0d54442a5b91459aeea
Roland Walker <walker@pobox.com>
2014-10-01 11:32:31 -0400

    add license stanza to b2designer

--- Casks/b2designer.rb
+++ Casks/b2designer.rb
@@ -4,6 +4,7 @@ class B2designer < Cask
 
   url 'http://www.flymaster-avionics.com/LinkClick.aspx?fileticket=jC2PKHRWXAA%3d&tabid=199&mid=804&forcedownload=true'
   homepage 'http://www.flymaster-avionics.com/Support/Downloads/ForMacOSX/tabid/199/Default.aspx'
+  license :unknown
 
   app 'B2designer.app'
 end
