0fa98e81cc237d679653bc921b557b544f359f6f
Roland Walker <walker@pobox.com>
2014-10-01 11:33:43 -0400

    add license stanza to qtspim

--- Casks/qtspim.rb
+++ Casks/qtspim.rb
@@ -4,6 +4,7 @@ class Qtspim < Cask
 
   url 'https://sourceforge.net/projects/spimsimulator/files/latest/download'
   homepage 'http://spimsimulator.sourceforge.net/'
+  license :oss
 
   app 'QtSpim.app'
 end
