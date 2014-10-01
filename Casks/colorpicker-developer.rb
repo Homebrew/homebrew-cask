f735281bbd82a724cca5926044a5bbf7318bd909
Roland Walker <walker@pobox.com>
2014-10-01 11:32:41 -0400

    add license stanza to colorpicker-developer

--- Casks/colorpicker-developer.rb
+++ Casks/colorpicker-developer.rb
@@ -4,6 +4,7 @@ class ColorpickerDeveloper < Cask
 
   url 'http://download.panic.com/picker/developercolorpicker.zip'
   homepage 'http://download.panic.com/picker/'
+  license :unknown
 
   colorpicker 'Developer Color Picker/DeveloperColorPicker.colorPicker'
 end
