ce7205f2dd73a7c27e462c38af1871ca7fdea6bb
Roland Walker <walker@pobox.com>
2014-10-01 11:33:54 -0400

    add license stanza to sketchupviewer

--- Casks/sketchupviewer.rb
+++ Casks/sketchupviewer.rb
@@ -5,6 +5,7 @@ class Sketchupviewer < Cask
   # downloads can be found at http://www.sketchup.com/download/all
   url 'https://dl.trimble.com/sketchup/SketchUpViewer-en.dmg'
   homepage 'http://www.sketchup.com/intl/en/'
+  license :unknown
 
   app 'SketchUpViewer.app'
 end
