182ba42c0116c7eebb33b54f2f96199813857324
Roland Walker <walker@pobox.com>
2014-10-01 11:32:42 -0400

    add license stanza to controllermate

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -4,6 +4,7 @@ class Controllermate < Cask
 
   url 'https://s3.amazonaws.com/orderedbytes/ControllerMate.dmg'
   homepage 'http://www.orderedbytes.com/controllermate/'
+  license :unknown
 
   pkg 'ControllerMate.pkg'
   uninstall :script => 'ControllerMate Uninstaller.app/Contents/MacOS/ControllerMate Uninstaller',
