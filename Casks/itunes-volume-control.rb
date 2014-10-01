a5a9bf2cce53cc606a04138e33fe139c0550a32f
Roland Walker <walker@pobox.com>
2014-10-01 11:33:08 -0400

    add license stanza to itunes-volume-control

--- Casks/itunes-volume-control.rb
+++ Casks/itunes-volume-control.rb
@@ -4,6 +4,7 @@ class ItunesVolumeControl < Cask
 
   url 'https://github.com/alberti42/iTunes-Volume-Control/blob/master/iTunes%20Volume%20Control.dmg?raw=true'
   homepage 'https://github.com/alberti42/iTunes-Volume-Control'
+  license :oss
 
   app 'iTunes Volume Control.app'
 end
