72ffca21c861e4d4cccf25f244063d9bcf2d0304
Roland Walker <walker@pobox.com>
2014-10-01 11:32:27 -0400

    add license stanza to alinof-timer

--- Casks/alinof-timer.rb
+++ Casks/alinof-timer.rb
@@ -4,6 +4,7 @@ class AlinofTimer < Cask
 
   url 'http://www.alinofsoftware.ch/resources/AlinofTimer.pkg'
   homepage 'http://www.alinofsoftware.ch/en/products/products-timer/index.html'
+  license :unknown
 
   pkg 'AlinofTimer.pkg', :allow_untrusted => true
   uninstall :pkgutil => 'com.alinofsoftware.alinoftimer'
