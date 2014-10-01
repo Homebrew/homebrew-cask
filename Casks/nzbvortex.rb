5b76dfebba9b1b8e3da9b268016f15b6770f0081
Roland Walker <walker@pobox.com>
2014-10-01 11:33:30 -0400

    add license stanza to nzbvortex

--- Casks/nzbvortex.rb
+++ Casks/nzbvortex.rb
@@ -5,6 +5,7 @@ class Nzbvortex < Cask
   url 'http://www.nzbvortex.com/downloads/NZBVortex.dmg'
   appcast 'http://www.nzbvortex.com/update/appcast.xml'
   homepage 'http://www.nzbvortex.com/'
+  license :unknown
 
   app 'NZBVortex.app'
 end
