ced659daaf328b39423e6beb0d2f42544b8bd5a2
Roland Walker <walker@pobox.com>
2014-10-01 11:33:58 -0400

    add license stanza to spotify

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -4,6 +4,7 @@ class Spotify < Cask
 
   url 'http://download.spotify.com/Spotify.dmg'
   homepage 'https://www.spotify.com'
+  license :unknown
 
   app 'Spotify.app'
   zap :delete => '~/Library/Preferences/com.spotify.client.plist'
