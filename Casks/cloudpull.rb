a04b6c787434ef2fc8b2d8e1db62487c8104031f
Roland Walker <walker@pobox.com>
2014-10-01 11:32:39 -0400

    add license stanza to cloudpull

--- Casks/cloudpull.rb
+++ Casks/cloudpull.rb
@@ -5,6 +5,7 @@ class Cloudpull < Cask
   url 'http://downloads.goldenhillsoftware.com/cloudpull/CloudPull.zip'
   appcast 'https://secure.goldenhillsoftware.com/updates/cloudpull/appcast.xml'
   homepage 'http://www.goldenhillsoftware.com/'
+  license :unknown
 
   app 'CloudPull.app'
 end
