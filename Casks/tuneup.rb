d76ef83ecf6f2a8bde7ac868aa31f40fc25a0dd5
Saul Shanabrook <s.shanabrook@gmail.com>
2014-09-30 20:45:40 -0400

    Add TuneUp.app v2.4.8

---
+++ Casks/tuneup.rb
@@ -0,0 +1,9 @@
+class Tuneup < Cask
+  version '2.4.8'
+  sha256 'a59beb6cded581d337326ad4a7d032b1216ef934546b265cdef0594c3255d61f'
+
+  url 'http://www.tuneupmedia.com/redirect/mac-download'
+  homepage 'http://www.tuneupmedia.com/'
+
+  app 'TuneUp.app'
+end
