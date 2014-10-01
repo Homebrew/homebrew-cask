0820010e9275cfaebde447fb804dfc3e0fe3fa93
Roland Walker <walker@pobox.com>
2014-10-01 11:34:03 -0400

    add license stanza to telegram

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -5,6 +5,7 @@ class Telegram < Cask
   url 'https://rink.hockeyapp.net/api/2/apps/c55f5e74ae5d0ad254df29f71a1b5f0e/?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/c55f5e74ae5d0ad254df29f71a1b5f0e'
   homepage 'https://vk.com/telegram_osx'
+  license :unknown
 
   app 'Telegram.app'
 end
