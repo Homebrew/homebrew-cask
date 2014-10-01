370f5146046906824db9a8b054572d8d92dc58cb
Roland Walker <walker@pobox.com>
2014-10-01 11:33:01 -0400

    add license stanza to harvest

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -5,6 +5,7 @@ class Harvest < Cask
   url 'https://www.getharvest.com/harvest/mac/Harvest.zip'
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml'
   homepage 'http://www.getharvest.com/mac'
+  license :unknown
 
   app 'Harvest.app'
 end
