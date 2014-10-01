6164fd6914677d9abd762766aba2f6466148a4b7
Roland Walker <walker@pobox.com>
2014-10-01 11:33:11 -0400

    add license stanza to kext-wizard

--- Casks/kext-wizard.rb
+++ Casks/kext-wizard.rb
@@ -5,6 +5,7 @@ class KextWizard < Cask
   url 'http://wizards.osxlatitude.com/kext/kw.zip'
   appcast 'http://wizards.osxlatitude.com/kext/update.xml'
   homepage 'http://www.insanelymac.com/forum/topic/253395-kext-wizard-easy-to-use-kext-installer-and-more/'
+  license :unknown
 
   app 'Kext Wizard.app'
 end
