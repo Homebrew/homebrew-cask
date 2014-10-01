86647bf60018bdd135d9c7f3888b51b7038f5109
Roland Walker <walker@pobox.com>
2014-10-01 11:34:02 -0400

    add license stanza to tactor

--- Casks/tactor.rb
+++ Casks/tactor.rb
@@ -4,6 +4,7 @@ class Tactor < Cask
 
   url 'https://dl.dropboxusercontent.com/u/7614970/Tactor.zip'
   homepage 'http://onflapp.wordpress.com/tactor/'
+  license :unknown
 
   app 'Tactor.app'
 end
