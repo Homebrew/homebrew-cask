d3a0db2c3a545b6301bbfefd7c7835b548e116de
Roland Walker <walker@pobox.com>
2014-10-01 11:32:41 -0400

    add license stanza to color-picker-pro

--- Casks/color-picker-pro.rb
+++ Casks/color-picker-pro.rb
@@ -5,6 +5,7 @@ class ColorPickerPro < Cask
   url 'https://fructivity.s3.amazonaws.com/ColorPickerPro/Color%20Picker%20Pro.zip'
   appcast 'http://fructivity.s3.amazonaws.com/ColorPickerPro/Appcast.xml'
   homepage 'https://github.com/oscardelben/Color-Picker-Pro'
+  license :oss
 
   app 'Color Picker Pro.app'
 end
