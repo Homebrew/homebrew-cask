74e75b7ec482e5c28eb404feb700561e02928e93
Roland Walker <walker@pobox.com>
2014-10-01 11:33:02 -0400

    add license stanza to hopper-disassembler

--- Casks/hopper-disassembler.rb
+++ Casks/hopper-disassembler.rb
@@ -5,6 +5,7 @@ class HopperDisassembler < Cask
   url 'http://www.hopperapp.com/HopperWeb/download_last_v3.php'
   appcast 'http://www.hopperapp.com/HopperWeb/appcast.php'
   homepage 'http://www.hopperapp.com/'
+  license :unknown
 
   app 'Hopper Disassembler v3.app'
 end
