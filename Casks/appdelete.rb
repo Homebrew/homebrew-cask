6ace6f0d7f6437223b5bce02188a9a1ccfd6b203
Roland Walker <walker@pobox.com>
2014-10-01 11:32:28 -0400

    add license stanza to appdelete

--- Casks/appdelete.rb
+++ Casks/appdelete.rb
@@ -5,6 +5,7 @@ class Appdelete < Cask
   url 'http://www.reggieashworth.com/downloads/AppDelete.dmg'
   appcast 'http://www.reggieashworth.com/AD4Appcast.xml'
   homepage 'http://www.reggieashworth.com/appdelete'
+  license :unknown
 
   app 'AppDelete.app'
 end
