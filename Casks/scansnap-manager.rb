206a87e05c8c124236bc22f25e3dbfe1ac36222d
Roland Walker <walker@pobox.com>
2014-10-01 11:33:48 -0400

    add license stanza to scansnap-manager

--- Casks/scansnap-manager.rb
+++ Casks/scansnap-manager.rb
@@ -4,6 +4,7 @@ class ScansnapManager < Cask
 
   url 'https://www.fujitsu.com/downloads/IMAGE/driver/ss/mgr/m-sv600/MacScanSnapV62L10WW.dmg'
   homepage 'http://www.fujitsu.com/global/support/computing/peripheral/scanners/software/'
+  license :unknown
 
   pkg 'ScanSnap Manager.pkg'
   uninstall :pkgutil => 'jp.co.pfu.ScanSnap.*'
