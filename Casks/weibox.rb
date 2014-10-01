2dcce31ad0ce2a616828b6cc71f78769201eea33
Roland Walker <walker@pobox.com>
2014-10-01 11:34:15 -0400

    add license stanza to weibox

--- Casks/weibox.rb
+++ Casks/weibox.rb
@@ -5,6 +5,7 @@ class Weibox < Cask
   url "https://weiboformac.sinaapp.com/downloads/#{version}.release.zip"
   appcast 'http://weiboformac.sinaapp.com/appcast/wm2.xml'
   homepage 'http://weiboformac.sinaapp.com'
+  license :unknown
 
   app 'WeiboX.app'
 end
