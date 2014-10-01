c0f506f7134a5e61f6e48c97bfde4c51489f96be
Roland Walker <walker@pobox.com>
2014-10-01 11:34:13 -0400

    add license stanza to vinoteka

--- Casks/vinoteka.rb
+++ Casks/vinoteka.rb
@@ -5,6 +5,7 @@ class Vinoteka < Cask
   url 'http://download.vinotekasoft.com/Vinoteka.zip'
   appcast 'http://download.vinotekasoft.com/vinoteka_update.xml'
   homepage 'http://www.vinotekasoft.com/'
+  license :unknown
 
   app 'Vinoteka.app'
 end
