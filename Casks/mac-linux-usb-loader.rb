1fe5b7d49d757ae885947412030345a0684b8358
Roland Walker <walker@pobox.com>
2014-10-01 11:33:17 -0400

    add license stanza to mac-linux-usb-loader

--- Casks/mac-linux-usb-loader.rb
+++ Casks/mac-linux-usb-loader.rb
@@ -5,6 +5,7 @@ class MacLinuxUsbLoader < Cask
   url 'https://sevenbits.github.io/downloads/Mac-Linux-USB-Loader.zip'
   appcast 'http://sevenbits.github.io/appcasts/mlul.xml'
   homepage 'http://sevenbits.github.io/Mac-Linux-USB-Loader/'
+  license :oss
 
   app 'Mac Linux USB Loader.app'
 end
