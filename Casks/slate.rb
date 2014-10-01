10a3b8ba95a86b1b5ede0ac4cf6e0d919311c88f
Roland Walker <walker@pobox.com>
2014-10-01 11:33:54 -0400

    add license stanza to slate

--- Casks/slate.rb
+++ Casks/slate.rb
@@ -5,6 +5,7 @@ class Slate < Cask
   url 'http://slate.ninjamonkeysoftware.com/Slate.dmg'
   appcast 'https://www.ninjamonkeysoftware.com/slate/appcast.xml'
   homepage 'https://github.com/jigish/slate'
+  license :oss
 
   app 'Slate.app'
   zap :delete => [
