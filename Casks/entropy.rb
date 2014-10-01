2df5052c359f885c37e019333e90fa303ebc6568
Roland Walker <walker@pobox.com>
2014-10-01 11:32:50 -0400

    add license stanza to entropy

--- Casks/entropy.rb
+++ Casks/entropy.rb
@@ -5,6 +5,7 @@ class Entropy < Cask
   url 'http://www.eigenlogik.com/entropy/download'
   appcast 'http://hyperion.eigenlogik.com/appcast/feed/entropy/'
   homepage 'http://www.eigenlogik.com/entropy/'
+  license :unknown
 
   app 'Entropy.app'
 end
