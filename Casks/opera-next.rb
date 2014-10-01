f9126b303432e230843597c4ba0b009234d6af90
Roland Walker <walker@pobox.com>
2014-10-01 11:33:32 -0400

    add license stanza to opera-next

--- Casks/opera-next.rb
+++ Casks/opera-next.rb
@@ -4,6 +4,7 @@ class OperaNext < Cask
 
   url "http://get.geo.opera.com/pub/opera-next/#{version}/mac/Opera_Next_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/computer/next'
+  license :unknown
 
   app 'Opera Next.app'
 end
