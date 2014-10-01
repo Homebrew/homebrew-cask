d5b345d0126ccf095425a51346c2d1e11705f69f
Roland Walker <walker@pobox.com>
2014-10-01 11:33:45 -0400

    add license stanza to radbeacon

--- Casks/radbeacon.rb
+++ Casks/radbeacon.rb
@@ -4,6 +4,7 @@ class Radbeacon < Cask
 
   url 'https://s3.amazonaws.com/downloads.radiusnetworks.com/cc3cef5f-fd6f-4789-8030-cf82d13bf487/RadBeacon.zip'
   homepage 'http://store.radiusnetworks.com/collections/all/products/radbeacon-config'
+  license :unknown
 
   app 'RadBeacon.app'
 end
