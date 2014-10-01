f225109baf5cd168ff413d881d9b7329f46b602f
Roland Walker <walker@pobox.com>
2014-10-01 11:33:18 -0400

    add license stanza to macpaw-gemini

--- Casks/macpaw-gemini.rb
+++ Casks/macpaw-gemini.rb
@@ -5,6 +5,7 @@ class MacpawGemini < Cask
   url 'http://dl.devmate.com/download/com.macpaw.site.Gemini/macpaw%20gemini.dmg'
   appcast 'http://updates.devmate.com/com.macpaw.site.Gemini.xml'
   homepage 'http://macpaw.com/gemini'
+  license :unknown
 
   app 'MacPaw Gemini.app'
 end
