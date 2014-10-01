89730fb4fecc48e5c54ddb08306334d5aa361fdf
Roland Walker <walker@pobox.com>
2014-10-01 11:32:46 -0400

    add license stanza to displaperture

--- Casks/displaperture.rb
+++ Casks/displaperture.rb
@@ -4,6 +4,7 @@ class Displaperture < Cask
 
   url 'http://manytricks.com/download/displaperture'
   homepage 'http://manytricks.com/displaperture'
+  license :unknown
 
   app 'Displaperture.app'
 end
