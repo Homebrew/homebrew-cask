bd07c397e4a9ee3f2decc2bb177fffef07e27882
Roland Walker <walker@pobox.com>
2014-10-01 11:34:05 -0400

    add license stanza to things

--- Casks/things.rb
+++ Casks/things.rb
@@ -5,6 +5,7 @@ class Things < Cask
   url 'https://culturedcode.com/things/download/'
   appcast 'http://downloads.culturedcode.com/things/download/Things_Updates.php'
   homepage 'http://culturedcode.com/things/'
+  license :unknown
 
   app 'Things.app'
 end
