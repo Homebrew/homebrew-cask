a73a54a39541491ee7832a9c089afbc3f13606bb
Roland Walker <walker@pobox.com>
2014-10-01 11:34:01 -0400

    add license stanza to swinsian

--- Casks/swinsian.rb
+++ Casks/swinsian.rb
@@ -5,6 +5,7 @@ class Swinsian < Cask
   url 'https://swinsian.com/sparkle/Swinsian.zip'
   appcast 'http://www.swinsian.com/sparkle/sparklecast.xml'
   homepage 'http://swinsian.com'
+  license :unknown
 
   app 'Swinsian.app'
 end
