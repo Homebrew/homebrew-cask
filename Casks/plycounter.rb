9f70cb7d490c4a5548ea74bf7a9369cfa2cdb523
Roland Walker <walker@pobox.com>
2014-10-01 11:33:39 -0400

    add license stanza to plycounter

--- Casks/plycounter.rb
+++ Casks/plycounter.rb
@@ -4,6 +4,7 @@ class Plycounter < Cask
 
   url 'https://www.plycount.com/plycounter/downloads/PlyCounter.dmg'
   homepage 'http://www.plycount.com'
+  license :unknown
 
   app 'PlyCounter.app'
 end
