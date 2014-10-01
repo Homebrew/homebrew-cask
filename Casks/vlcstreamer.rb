e5c77df050722d31063cb4ef5f110c608355a7b4
Roland Walker <walker@pobox.com>
2014-10-01 11:34:14 -0400

    add license stanza to vlcstreamer

--- Casks/vlcstreamer.rb
+++ Casks/vlcstreamer.rb
@@ -5,6 +5,7 @@ class Vlcstreamer < Cask
   url 'http://hobbyistsoftware.com/Downloads/VLCStreamer/latest-mac.php?cdn'
   appcast 'http://hobbyistsoftware.com/Downloads/VLCStreamer/vlcStreamerVersions.xml'
   homepage 'http://hobbyistsoftware.com/vlcstreamer'
+  license :unknown
 
   app 'VLCStreamer.app'
 end
