1588d57af710b2bde27f52743bf2f66e8a5ae34c
Roland Walker <walker@pobox.com>
2014-10-01 11:33:23 -0400

    add license stanza to mirrordisplays

--- Casks/mirrordisplays.rb
+++ Casks/mirrordisplays.rb
@@ -5,6 +5,7 @@ class Mirrordisplays < Cask
   url 'http://www.fabiancanas.com/downloads/MirrorDisplays.zip',
     :referer => 'http://www.fabiancanas.com/Projects/MirrorDisplays'
   homepage 'http://www.fabiancanas.com/Projects/MirrorDisplays'
+  license :unknown
 
   app 'MirrorDisplays.app'
 end
