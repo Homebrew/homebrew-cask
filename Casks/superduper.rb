18519b4a54460c07988cb88c2ef728d683976e69
Roland Walker <walker@pobox.com>
2014-10-01 11:34:01 -0400

    add license stanza to superduper

--- Casks/superduper.rb
+++ Casks/superduper.rb
@@ -4,6 +4,7 @@ class Superduper < Cask
 
   url 'http://www.shirt-pocket.com/downloads/SuperDuper!.dmg'
   homepage 'http://www.shirt-pocket.com/SuperDuper/SuperDuperDescription.html'
+  license :unknown
 
   app 'SuperDuper!.app'
   zap :delete => '~/Library/Application Support/SuperDuper!'
