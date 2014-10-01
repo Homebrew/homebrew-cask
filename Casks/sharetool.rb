2768252e8dce47e6b6d45384bf69cf4b4c9caaa6
Roland Walker <walker@pobox.com>
2014-10-01 11:33:51 -0400

    add license stanza to sharetool

--- Casks/sharetool.rb
+++ Casks/sharetool.rb
@@ -4,6 +4,7 @@ class Sharetool < Cask
 
   url 'http://software-files-a.cnet.com/s/software/12/85/93/73/sharetool2.zip?lop=link&ptype=3001&ontid=7240&siteId=4&edId=3&spi=b95d1f82803e8d0dd208a759ca8ecc48&pid=12859373&psid=198769&token=1394020831_2637325d8e249b00ddbf1f29824144ac&fileName=sharetool2.zip'
   homepage 'http://bainsware.com/'
+  license :unknown
 
   nested_container 'ShareTool 2.dmg'
   app 'ShareTool.app'
