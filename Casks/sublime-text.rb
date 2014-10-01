ef41f3474bb44d91ccaa798e46d3f5618ae08839
Roland Walker <walker@pobox.com>
2014-10-01 11:34:00 -0400

    add license stanza to sublime-text

--- Casks/sublime-text.rb
+++ Casks/sublime-text.rb
@@ -5,6 +5,7 @@ class SublimeText < Cask
   url "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20#{version}.dmg"
   appcast 'http://www.sublimetext.com/updates/2/stable/appcast_osx.xml'
   homepage 'http://www.sublimetext.com/2'
+  license :unknown
 
   app 'Sublime Text 2.app'
   binary 'Sublime Text 2.app/Contents/SharedSupport/bin/subl'
