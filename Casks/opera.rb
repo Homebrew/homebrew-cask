dd1e955e7b80fc44dea2848ca09a61f17cf9c35d
Vítor Galvão <info@vitorgalvao.com>
2014-10-01 11:04:41 +0100

    updated opera to 24.0.1558.64

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 class Opera < Cask
-  version '24.0.1558.61'
-  sha256 '2f23178ef04d318523096ef44f15b74f2b2201e6780f1b46a04207c8f2879aac'
+  version '24.0.1558.64'
+  sha256 '77564b24e82de2a6e545b67dd97ce74dd218fa332689de424870da5db9c37314'
 
   url "http://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/'
