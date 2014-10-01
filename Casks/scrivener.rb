a341d8136642a9257b59b6b8b308d58c5e8199e8
Roland Walker <walker@pobox.com>
2014-10-01 11:33:49 -0400

    add license stanza to scrivener

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -5,6 +5,7 @@ class Scrivener < Cask
   url 'https://scrivener.s3.amazonaws.com/Scrivener.dmg'
   appcast 'http://www.literatureandlatte.com/downloads/scrivener-2.xml'
   homepage 'http://literatureandlatte.com/scrivener.php'
+  license :unknown
 
   app 'Scrivener.app'
 end
