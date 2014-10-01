ff5f6ecbfdddc6374ec04d2740ce21c7b7241f89
Roland Walker <walker@pobox.com>
2014-10-01 11:32:55 -0400

    add license stanza to freezer

--- Casks/freezer.rb
+++ Casks/freezer.rb
@@ -5,6 +5,7 @@ class Freezer < Cask
   url 'http://download.mrgeckosmedia.com/Freezer.zip'
   appcast 'https://mrgeckosmedia.com/applications/appcast/Freezer'
   homepage 'https://mrgeckosmedia.com/applications/info/Freezer'
+  license :unknown
 
   app 'Freezer/Freezer.app'
 end
