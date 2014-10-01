170589b33933e1763e634f7e1088c1e6aafaeeb1
Roland Walker <walker@pobox.com>
2014-10-01 11:33:12 -0400

    add license stanza to keyboard-maestro

--- Casks/keyboard-maestro.rb
+++ Casks/keyboard-maestro.rb
@@ -4,6 +4,7 @@ class KeyboardMaestro < Cask
 
   url 'http://www.keyboardmaestro.com/action/download?km-kmi-2-b'
   homepage 'http://www.keyboardmaestro.com/'
+  license :unknown
 
   app 'Keyboard Maestro.app'
 end
