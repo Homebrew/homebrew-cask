21c1e04243ebfa2bb94934dc1d3a8dc0b41dc96e
Roland Walker <walker@pobox.com>
2014-10-01 11:32:41 -0400

    add license stanza to colorschemer-studio

--- Casks/colorschemer-studio.rb
+++ Casks/colorschemer-studio.rb
@@ -5,6 +5,7 @@ class ColorschemerStudio < Cask
   url 'https://www.colorschemer.com/colorschemerstudio.dmg'
   appcast 'http://www.colorschemer.com/appcast/studio2_mac.xml'
   homepage 'http://www.colorschemer.com'
+  license :unknown
 
   app 'ColorSchemer Studio 2.app'
 end
