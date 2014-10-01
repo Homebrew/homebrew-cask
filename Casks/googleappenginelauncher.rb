618f0d79914cfaf86b7b70da308077c0423e4342
Roland Walker <walker@pobox.com>
2014-10-01 11:32:59 -0400

    add license stanza to googleappenginelauncher

--- Casks/googleappenginelauncher.rb
+++ Casks/googleappenginelauncher.rb
@@ -3,6 +3,7 @@ class Googleappenginelauncher < Cask
   sha256 'e198b384760686012438d3a4b2890e682e39e16007b22fc12693d16323bdf5ed'
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
   homepage 'https://developers.google.com/appengine/'
+  license :unknown
 
   app 'GoogleAppEngineLauncher.app'
 end
