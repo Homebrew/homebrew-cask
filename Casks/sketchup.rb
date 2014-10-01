ecaf220c446f541da2cdadf8b4ceaec0696984d0
Roland Walker <walker@pobox.com>
2014-10-01 11:33:54 -0400

    add license stanza to sketchup

--- Casks/sketchup.rb
+++ Casks/sketchup.rb
@@ -5,6 +5,7 @@ class Sketchup < Cask
   # downloads can be found at http://www.sketchup.com/download/all
   url 'https://dl.trimble.com/sketchup/SketchUpMake-en.dmg'
   homepage 'http://www.sketchup.com/intl/en/'
+  license :unknown
 
   suite 'SketchUp 2014'
 end
