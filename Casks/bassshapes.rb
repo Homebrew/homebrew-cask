9a3f33632138410bccca0f39b076fb785810912a
Roland Walker <walker@pobox.com>
2014-10-01 11:32:32 -0400

    add license stanza to bassshapes

--- Casks/bassshapes.rb
+++ Casks/bassshapes.rb
@@ -4,6 +4,7 @@ class Bassshapes < Cask
 
   url "http://yellquietly.com/downloads/BassShapes_v#{version.gsub('.', '')}.zip"
   homepage 'http://yellquietly.com/bass-shapes/'
+  license :unknown
 
   app "BassShapes_v#{version.gsub('.', '')}/BassShapes.app"
 end
