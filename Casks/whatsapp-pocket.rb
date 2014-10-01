7be99db49b726e993528c0ec64e327e84a395922
Roland Walker <walker@pobox.com>
2014-10-01 11:34:16 -0400

    add license stanza to whatsapp-pocket

--- Casks/whatsapp-pocket.rb
+++ Casks/whatsapp-pocket.rb
@@ -4,6 +4,7 @@ class WhatsappPocket < Cask
 
   url 'http://www.fireebok.com/product/WhatsApp_Pocket_Mac.dmg'
   homepage 'http://www.fireebok.com/whatsapp-pocket.html'
+  license :unknown
 
   app 'WhatsApp Pocket.app'
 end
