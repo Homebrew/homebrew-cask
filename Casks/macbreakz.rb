a18cebad290a968c362813c20839a862082b1950
Roland Walker <walker@pobox.com>
2014-10-01 11:33:17 -0400

    add license stanza to macbreakz

--- Casks/macbreakz.rb
+++ Casks/macbreakz.rb
@@ -5,6 +5,7 @@ class Macbreakz < Cask
   url 'http://www.publicspace.net/download/MacBreakZ5.dmg'
   appcast 'http://www.publicspace.net/app/signed_mb5.xml'
   homepage 'http://www.publicspace.net/MacBreakZ/'
+  license :unknown
 
   app 'MacBreakZ 5.app'
 end
