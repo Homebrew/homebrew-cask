36c39e9d3caf1414df1ee925e6ff8f1866c9f3cb
dwihn0r <dwihn0r@users.noreply.github.com>
2014-10-01 17:26:09 +0200

    Updated Chromium to 37.0.2062.124

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 class Chromium < Cask
-  version '37.0.2062.94'
-  sha256 '72eef5d4281083bf2ea7eb848b81d145f3764c9ed0c9e207508640134172afea'
+  version '37.0.2062.124'
+  sha256 '34638a6b212e58a64120e6d7ea04487216310b12b516a5974a7d245de979cc3a'
 
   url "https://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_#{version}.dmg"
   appcast 'http://osxportableapps.sourceforge.net/chromium/chrcast.xml'
