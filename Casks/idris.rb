0acb5905aeba28fc67b6c12c9d785a6aa958b610
Roland Walker <walker@pobox.com>
2014-10-01 11:33:05 -0400

    add license stanza to idris

--- Casks/idris.rb
+++ Casks/idris.rb
@@ -4,6 +4,7 @@ class Idris < Cask
 
   url 'http://www.idris-lang.org/pkgs/idris-current.pkg'
   homepage 'http://www.idris-lang.org'
+  license :unknown
 
   pkg 'idris-current.pkg'
   uninstall :pkgutil => 'org.idris-lang'
