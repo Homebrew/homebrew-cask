5f975127d35f78f8a2634d130d4215bccfa3c117
Roland Walker <walker@pobox.com>
2014-10-01 11:34:10 -0400

    add license stanza to tvmobili

--- Casks/tvmobili.rb
+++ Casks/tvmobili.rb
@@ -4,6 +4,7 @@ class Tvmobili < Cask
 
   url 'http://www.tvmobili.com/binaries/stable/tvmobili-mountainlion-universal.dmg'
   homepage 'http://www.tvmobili.com/'
+  license :unknown
 
   pkg 'tvmobili-mountainlion-universal.2.1.4481.pkg'
   uninstall :pkgutil   => 'com.tvmobili.tvmobilisvcd',
