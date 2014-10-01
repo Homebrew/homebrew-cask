3b703dfdd54356be9729b6229e9fb50948169727
Keng Kiat Lim <kkin@crnrc.com>
2014-10-01 12:35:00 +0800

    Upgrade Opera to 24.0.1558.64

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 class Opera < Cask
-  version '24.0.1558.53'
-  sha256 'c75e05f0c85ae50939be05f122d74c2f1228aca6012aa1ff6b34bd9ca769c83d'
+  version '24.0.1558.64'
+  sha256 '77564b24e82de2a6e545b67dd97ce74dd218fa332689de424870da5db9c37314'
 
   url "http://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/'
