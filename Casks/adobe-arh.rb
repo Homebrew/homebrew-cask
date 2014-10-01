6d513792d44bfb0e85e5e9534d028db9f0449885
Roland Walker <walker@pobox.com>
2014-10-01 11:32:25 -0400

    add license stanza to adobe-arh

--- Casks/adobe-arh.rb
+++ Casks/adobe-arh.rb
@@ -4,6 +4,7 @@ class AdobeArh < Cask
 
   url 'http://airdownload.adobe.com/air/distribution/latest/mac/arh'
   homepage 'http://help.adobe.com/en_US/air/redist/WS485a42d56cd19641-70d979a8124ef20a34b-8000.html'
+  license :unknown
 
   binary 'arh'
   container_type :naked
