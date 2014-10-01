8e808bfa0a533b01314a790357ffc3369a418d82
Roland Walker <walker@pobox.com>
2014-10-01 11:33:53 -0400

    add license stanza to sizzlingkeys

--- Casks/sizzlingkeys.rb
+++ Casks/sizzlingkeys.rb
@@ -4,6 +4,7 @@ class Sizzlingkeys < Cask
 
   url 'http://www.yellowmug.com/download/SizzlingKeys.dmg'
   homepage 'http://www.yellowmug.com/sk4it/'
+  license :unknown
 
   app 'SizzlingKeys.app'
 end
