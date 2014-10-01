dffb1348389c21e23e580083edb2e8fdd69f040b
Roland Walker <walker@pobox.com>
2014-10-01 11:33:40 -0400

    add license stanza to pref-setter

--- Casks/pref-setter.rb
+++ Casks/pref-setter.rb
@@ -4,6 +4,7 @@ class PrefSetter < Cask
 
   url 'http://www.nightproductions.net/downloads/prefsetter.zip'
   homepage 'http://www.nightproductions.net/prefsetter.html'
+  license :unknown
 
   app 'Pref Setter/Pref Setter.app'
 end
