b486316fff33df726dcb9633c8ebbda3c45055e6
Roland Walker <walker@pobox.com>
2014-10-01 11:33:28 -0400

    add license stanza to netshade

--- Casks/netshade.rb
+++ Casks/netshade.rb
@@ -5,6 +5,7 @@ class Netshade < Cask
   url 'http://raynersoftware.com/downloads/NetShade.app.zip'
   appcast 'https://secure.raynersw.com/appcast.php'
   homepage 'http://raynersoftware.com/netshade/'
+  license :unknown
 
   app 'NetShade.app'
 end
