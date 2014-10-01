ca990cf11f1937fe208811eecfd6ea18e6f9a2b2
Roland Walker <walker@pobox.com>
2014-10-01 11:32:50 -0400

    add license stanza to evom

--- Casks/evom.rb
+++ Casks/evom.rb
@@ -5,6 +5,7 @@ class Evom < Cask
   url 'http://files.thelittleappfactory.com/evom/Evom.zip'
   appcast 'https://files.thelittleappfactory.com/evom/appcast.xml'
   homepage 'http://thelittleappfactory.com/evom/'
+  license :unknown
 
   app 'Evom.app'
 end
