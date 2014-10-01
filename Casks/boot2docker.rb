e247bf0f8e4fa74f18ea29038f286c474e014ca9
Roland Walker <walker@pobox.com>
2014-10-01 11:32:34 -0400

    add license stanza to boot2docker

--- Casks/boot2docker.rb
+++ Casks/boot2docker.rb
@@ -4,6 +4,7 @@ class Boot2docker < Cask
 
   url "https://github.com/boot2docker/osx-installer/releases/download/v#{version}/Boot2Docker-#{version}.pkg"
   homepage 'https://github.com/boot2docker/osx-installer'
+  license :oss
 
   pkg "Boot2Docker-#{version}.pkg"
   uninstall :pkgutil => ['io.boot2docker.pkg.boot2docker', 'io.boot2docker.pkg.boot2dockerapp', 'io.boot2dockeriso.pkg.boot2dockeriso', 'io.docker.pkg.docker']
