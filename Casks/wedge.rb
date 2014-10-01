4a35c5147586df986578d1c90c1ebf908a813037
Roland Walker <walker@pobox.com>
2014-10-01 11:34:15 -0400

    add license stanza to wedge

--- Casks/wedge.rb
+++ Casks/wedge.rb
@@ -5,6 +5,7 @@ class Wedge < Cask
   url 'http://wedge.natestedman.com/release/Wedge.app.zip'
   appcast 'http://wedge.natestedman.com/appcast.xml'
   homepage 'http://wedge.natestedman.com'
+  license :unknown
 
   app 'Wedge.app'
 end
