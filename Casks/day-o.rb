809a678aac0ebddcbcb37e8c4d336d9e60f9716d
Roland Walker <walker@pobox.com>
2014-10-01 11:32:44 -0400

    add license stanza to day-o

--- Casks/day-o.rb
+++ Casks/day-o.rb
@@ -4,6 +4,7 @@ class DayO < Cask
 
   url 'http://shauninman.com/assets/downloads/Day-O.zip'
   homepage 'http://www.shauninman.com/archive/2011/10/20/day_o_mac_menu_bar_clock'
+  license :unknown
 
   app 'Day-O/Day-O.app'
 end
