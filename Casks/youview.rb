07dacbb2a84794c9b0740c90e26791d6297607e1
Roland Walker <walker@pobox.com>
2014-10-01 11:34:20 -0400

    add license stanza to youview

--- Casks/youview.rb
+++ Casks/youview.rb
@@ -5,6 +5,7 @@ class Youview < Cask
   url 'http://download.mrgeckosmedia.com/YouView.zip'
   appcast 'http://mrgeckosmedia.com/applications/appcast/YouView'
   homepage 'https://mrgeckosmedia.com/applications/info/YouView'
+  license :unknown
 
   app 'YouView/YouView.app'
 end
