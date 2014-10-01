a9bccde3fe62573858698966f410eabc48be3456
Roland Walker <walker@pobox.com>
2014-10-01 11:32:51 -0400

    add license stanza to exhaust

--- Casks/exhaust.rb
+++ Casks/exhaust.rb
@@ -5,6 +5,7 @@ class Exhaust < Cask
   url 'http://download.mrgeckosmedia.com/Exhaust.zip'
   appcast 'http://mrgeckosmedia.com/applications/appcast/Exhaust'
   homepage 'https://mrgeckosmedia.com/applications/info/Exhaust'
+  license :unknown
 
   app 'Exhaust/Exhaust.app'
 end
