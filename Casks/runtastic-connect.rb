0be4b2a0035ff54dfb77b060e66c482b46bf99a1
Roland Walker <walker@pobox.com>
2014-10-01 11:33:48 -0400

    add license stanza to runtastic-connect

--- Casks/runtastic-connect.rb
+++ Casks/runtastic-connect.rb
@@ -5,6 +5,7 @@ class RuntasticConnect < Cask
   url 'http://download.runtastic.com/connect/mac/runtasticConnect.dmg'
   appcast 'http://download.runtastic.com/connect/mac/appcast.xml'
   homepage 'https://www.runtastic.com/connect'
+  license :unknown
 
   app 'Runtastic Connect.app'
 end
