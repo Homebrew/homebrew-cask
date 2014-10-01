e9e68bf500924c09d6396a294cb9175b28c71e71
Roland Walker <walker@pobox.com>
2014-10-01 11:32:55 -0400

    add license stanza to fs-uae

--- Casks/fs-uae.rb
+++ Casks/fs-uae.rb
@@ -4,6 +4,7 @@ class FsUae < Cask
 
   url "http://fs-uae.net/fs-uae/stable/#{version}/fs-uae-plus-#{version}-macosx.tar.gz"
   homepage 'http://fs-uae.net/'
+  license :unknown
 
   app "fs-uae-#{version}-macosx/FS-UAE.app"
   app "fs-uae-#{version}-macosx/FS-UAE Launcher.app"
