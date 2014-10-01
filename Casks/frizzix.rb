459ecbd578aef8e3dda5c9379d3728a2f47aab50
Roland Walker <walker@pobox.com>
2014-10-01 11:32:55 -0400

    add license stanza to frizzix

--- Casks/frizzix.rb
+++ Casks/frizzix.rb
@@ -5,6 +5,7 @@ class Frizzix < Cask
   url 'http://mac.frizzix.de/downloads/currentVersion.dmg'
   appcast 'http://frizzix.de/downloads/FrizzixUpdate.xml'
   homepage 'http://mac.frizzix.de/'
+  license :unknown
 
   app 'Frizzix.app'
 end
