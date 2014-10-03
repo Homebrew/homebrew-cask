e0e5b424e64a9ad4288dd42151cfa77c728df76d
Roland Walker <walker@pobox.com>
2014-10-03 10:42:40 -0400

    use container :type in adobe-arh

--- Casks/adobe-arh.rb
+++ Casks/adobe-arh.rb
@@ -6,8 +6,8 @@ class AdobeArh < Cask
   homepage 'http://help.adobe.com/en_US/air/redist/WS485a42d56cd19641-70d979a8124ef20a34b-8000.html'
   license :unknown
 
+  container :type => :naked
   binary 'arh'
-  container_type :naked
 
   postflight do
     system '/bin/chmod', '--', '755', "#{destination_path}/arh"
