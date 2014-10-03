283231a05afe742a932110022cdd84aacb2528d2
Matt Whiteley <mwhiteley@engineyard.com>
2014-10-03 15:13:02 -0700

    Update Beamer appcast link

--- Casks/beamer.rb
+++ Casks/beamer.rb
@@ -3,9 +3,9 @@ class Beamer < Cask
   sha256 :no_check
 
   url 'http://beamer-app.com/download'
-  appcast 'http://beamer-app.com/sparkle-appcast.xml'
+  appcast 'http://beamer-app.com/beamer2-appcast.xml'
   homepage 'http://beamer-app.com'
-  license :unknown
+  license :commercial
 
   app 'Beamer.app'
 end
