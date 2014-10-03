7d081d36d706b640749d9f918289e6d2fa3a6719
Chris Born <cborn@llu.edu>
2014-10-03 13:58:52 -0700

    Upgrade Codekit.app to v2.1.7

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -1,8 +1,8 @@
 class Codekit < Cask
-  version '2.0.6 (17018)'
-  sha256 '97070e0fd9949991186652e2b3655c26459c5d7c0ab852c2ea3241078894573e'
+  version '2.1.7 (17945)'
+  sha256 '2d68f834e911527cce1c45315b8d8a986191b3aa0226371585a52ef0c9856c91'
 
-  url 'https://incident57.com/codekit/files/codekit-17018.zip'
+  url 'https://incident57.com/codekit/files/codekit-17945.zip'
   appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml'
   homepage 'http://incident57.com/codekit/'
   license :unknown
