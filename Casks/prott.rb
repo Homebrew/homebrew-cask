74444a8f2b64164cca2793b52fe79689f2530ed3
fainder <chee@fainder.info>
2014-10-03 19:25:03 +0900

    Add Prott.app

---
+++ Casks/prott.rb
@@ -0,0 +1,10 @@
+class Prott < Cask
+  version :latest
+  sha256 :no_check
+
+  url "https://prottapp.com/app/gadgets/prott.dmg"
+  homepage 'http://prottapp.com/'
+  license :unknown
+
+  app 'Prott.app'
+end
