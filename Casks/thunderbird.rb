177c7bdadef50ec618266c05283ff87df8171b0d
Eugene San (eugenesan) <eugenesan@gmail.com>
2014-10-04 10:03:41 +0300

    update thunderbird to 31.1.2

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,6 +1,6 @@
 class Thunderbird < Cask
-  version '31.1.1'
-  sha256 '1cccb367aefb95912fdc735a87095e728668ef269047e4813812b469431b2e57'
+  version '31.1.2'
+  sha256 '491fc8c061055a9e1f52961c488ea84f7f2fcf633a41a983e4c8985859db75af'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   homepage 'http://www.mozilla.org/en-US/thunderbird/'
