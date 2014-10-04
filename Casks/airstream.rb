84b8b2809c718e901c025573ab2c8dfb2195220f
Roland Walker <walker@pobox.com>
2014-10-04 09:49:55 -0400

    string form `caveats`, indenting in airstream.rb

--- Casks/airstream.rb
+++ Casks/airstream.rb
@@ -7,11 +7,10 @@ class Airstream < Cask
   license :unknown
   app 'AirStream.app'
 
-  caveats do
-    puts <<-EOS.undent
-      If you have Java 7 or above installed, you may have to run the following commands:
-        sudo mkdir -p /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/bundle/Libraries/
-        sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/jre/lib/server/libjvm.dylib /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/bundle/Libraries/libserver.dylib
-    EOS
-  end
+  caveats <<-EOS.undent
+    If you have Java 7 or above installed, you may have to run the following:
+
+      sudo mkdir -p /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/bundle/Libraries/
+      sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/jre/lib/server/libjvm.dylib /Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/bundle/Libraries/libserver.dylib
+  EOS
 end
