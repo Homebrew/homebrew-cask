f77e87b6a798a4e9beb93a3cf10b6fbd666941c0
Roland Walker <walker@pobox.com>
2014-10-04 10:13:21 -0400

    layout, re-use version numbers in totalspaces.rb

--- Casks/totalspaces.rb
+++ Casks/totalspaces.rb
@@ -1,9 +1,12 @@
 class Totalspaces < Cask
+
   if MacOS.version < :mavericks
-    url 'http://downloads.binaryage.com/TotalSpaces-1.2.11.zip'
     version '1.2.11'
-    pkg 'TotalSpaces.pkg'
     sha256 'fd54c6ea092f6fae2035745959ff6e080953e77ec6c76715e532b4b0352235d4'
+
+    url "http://downloads.binaryage.com/TotalSpaces-#{version}.zip"
+
+    pkg 'TotalSpaces.pkg'
     uninstall :pkgutil => 'com.switchstep.totalspaces',
               :quit    => 'com.binaryage.TotalSpaces',
               :signal  => [
@@ -11,10 +14,12 @@ class Totalspaces < Cask
                            ['KILL', 'com.binaryage.totalspacescrashwatcher'],
                           ]
   else
-    url "http://downloads.binaryage.com/TotalSpaces2-2.2.6.zip"
     version '2.2.6'
-    pkg 'TotalSpaces2.pkg'
     sha256 '900ece3f5ceae479b4019f854e1875eb402edf3ef1b17f813a70fe42290a0a12'
+
+    url "http://downloads.binaryage.com/TotalSpaces-#{version}.zip"
+
+    pkg 'TotalSpaces2.pkg'
     uninstall :pkgutil => 'com.binaryage.TotalSpaces2',
               :quit    => 'com.binaryage.TotalSpaces2',
               :signal  => [
@@ -22,6 +27,7 @@ class Totalspaces < Cask
                            ['KILL', 'com.binaryage.totalspacescrashwatcher'],
                           ]
   end
+
   homepage 'http://totalspaces.binaryage.com/'
   license :unknown
 end
