eb04c893f2288880b5d064d73940093ec96be389
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-10-05 00:42:49 -0600

    Add Bitwig Studio

---
+++ Casks/bitwig-studio.rb
@@ -0,0 +1,10 @@
+class BitwigStudio < Cask
+  version '1.0.14'
+  sha256 'f968118c93c3449310d48c32f545e83d0f918c8e312e5a4b54cd30467e860b1e'
+
+  url 'http://packs.bitwig.com/downloads/Bitwig%20Studio%201.0.14.dmg'
+  homepage 'https://www.bitwig.com'
+  license :unknown
+
+  app 'Bitwig Studio.app'
+end
