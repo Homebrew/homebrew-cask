baf6efd6baecb7196bd9a7c7905396683c6e64f7
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-10-05 16:16:37 -0600

    Add Tableau Public.app

---
+++ Casks/tableau-public.rb
@@ -0,0 +1,10 @@
+class TableauPublic < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'https://downloads.tableausoftware.com/public/TableauPublic.dmg'
+  homepage 'http://www.tableausoftware.com/public'
+  license :unknown
+
+  app 'Tableau Public.app'
+end
