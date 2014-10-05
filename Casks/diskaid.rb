cb924a7df70a92a37b31bb1cdd2fbc036c38d48c
Kir Belevich <kir@soulshine.in>
2014-10-05 22:59:25 +0700

    Upgrade DiskAid.app to v6.7.6

--- Casks/diskaid.rb
+++ Casks/diskaid.rb
@@ -1,9 +1,9 @@
 class Diskaid < Cask
-  version '6.7.2'
-  sha256 '3cdf38d0f37073ec6051955a294952d3c491df7600e212d5337dc752decb31ad'
+  version '6.7.6'
+  sha256 '59845b1343ab6d8d52802ed4348dfcfedfbb5fa7fd9d59e245599ea50bdfd0db'
 
-  url 'http://www.digidna.net/downloads/DiskAid_6_7_2.dmg'
-  homepage 'http://www.digidna.net/diskaid'
+  url 'http://imazing.com/downloads/DiskAid_6_7_6.dmg'
+  homepage 'http://imazing.com/diskaid'
   license :unknown
 
   app 'DiskAid.app'
