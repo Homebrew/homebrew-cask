205f4f0158b0401f5b76f0e88149c520be5040a9
Brian Shumate <brian@couchbase.com>
2014-10-06 11:48:57 -0400

    update for Couchbase Server 3.0.0 versions ʕノ◔ϖ◔ʔノ

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -1,8 +1,8 @@
 class CouchbaseServerEnterprise < Cask
-  version '2.5.1'
-  sha256 'fce890b3c68893ad651da2103ae56d8be6a6310e607aa42b0376fea1a1ca1b41'
+  version '3.0.0'
+  sha256 'b3ea2d7dd10e97b51aa2492b4e1877f53d7ff3d45f401565dd117caf77e5f37d'
 
-  url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}_x86_64.zip"
+  url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
   homepage 'http://www.couchbase.com/'
   license :unknown
 
