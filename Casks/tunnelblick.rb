600df3e3dce9b837646aeef86e7fdef2e856c763
Andrew Minion <andrew@andrewrminion.com>
2014-10-06 11:59:06 -0400

    update TunnelBlick version

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -3,8 +3,8 @@ class Tunnelblick < Cask
     version '3.3.4'
     sha256 'c14e4b2d4e30679851de7701c8a0b3167158c5ce029c59b49e49eb58946de913'
   else
-    version '3.4beta38_r3002'
-    sha256 '3cd7f7e55adbd1fd30fc01c0eb8b7919d48602b648af15f841ee1cffd6e5708c'
+    version '3.40_r3007'
+    sha256 '3296266375812c954e0f3ce91adca676d06767f9ac26e3ad78452e0de1425279'
   end
   url "https://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_#{version}.dmg"
   appcast 'https://www.tunnelblick.net/appcast.rss'
