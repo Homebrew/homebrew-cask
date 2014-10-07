9365e10205dd484205ac13cd6ce9418937a406b1
luckyluckyluke <mail@lucasribic.com>
2014-10-07 11:22:24 +0200

    Update Brackets to 0.44

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,6 +1,6 @@
 class Brackets < Cask
-  version '0.43'
-  sha256 '582b6a1425b9b4574b7ad5b5e797899e6e8bacc47560f71203549fe5a1863aa9'
+  version '0.44'
+  sha256 '3619f01dddc55f47be87306909b7264cbdad9fb43be8c88661bec0a087b0f8a6'
 
   url "https://github.com/adobe/brackets/releases/download/release-#{version}/Brackets.Release.#{version}.dmg"
   homepage 'http://brackets.io'
