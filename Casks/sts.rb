184daeecebcc57fe68865cb3ec52286d210e59b4
Roland Walker <walker@pobox.com>
2014-10-07 11:28:49 -0400

    use `Utils` namespace in sts.rb

--- Casks/sts.rb
+++ Casks/sts.rb
@@ -1,10 +1,14 @@
 class Sts < Cask
   version '3.5.1'
   sha256 'f71274c9f946d2af6bbd12e811d7c8d371d3031415839b9aa6ed35347d2980f8'
-  # find eclise version at http://spring.io/tools/sts/all
-  based_on_eclipse = '4.3.2'
 
-  url "http://download.springsource.com/release/STS/#{version}/dist/e#{based_on_eclipse.gsub(/\.\d$/, '')}/spring-tool-suite-#{version}.RELEASE-e#{based_on_eclipse}-macosx-cocoa-x86_64-installer.dmg"
+  module Utils
+    def self.based_on_eclipse
+      '4.3.2'   # find eclipse version at http://spring.io/tools/sts/all
+    end
+  end
+
+  url "http://download.springsource.com/release/STS/#{version}/dist/e#{Utils.based_on_eclipse.gsub(/\.\d$/, '')}/spring-tool-suite-#{version}.RELEASE-e#{Utils.based_on_eclipse}-macosx-cocoa-x86_64-installer.dmg"
   homepage 'http://spring.io/tools/sts'
   license :unknown
 
