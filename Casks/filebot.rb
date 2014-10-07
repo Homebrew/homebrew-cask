18440a658ab2effaa9100ccabbaf75b42c4584d7
Roland Walker <walker@pobox.com>
2014-10-07 10:53:45 -0400

    stanza layout/order, filebot.rb

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -2,9 +2,9 @@ class Filebot < Cask
   version '4.5'
   sha256 '389e663588f241c1aa97a2797f54d4e52e7c4fe47d394cd70cb5b2c7395a6761'
 
+  url "https://downloads.sourceforge.net/project/filebot/filebot/FileBot_#{version}/FileBot_#{version}.app.tar.gz"
   homepage 'http://www.filebot.net/'
   license :oss
-  url "https://downloads.sourceforge.net/project/filebot/filebot/FileBot_#{version}/FileBot_#{version}.app.tar.gz"
 
   app 'FileBot.app'
   binary 'FileBot.app/Contents/MacOS/filebot.sh', :target => 'filebot'
