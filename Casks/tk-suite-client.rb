f6be90927a59c66f5b637716003cc0a6df7da7b3
Andreas Boeckler <abo@netlands.de>
2014-10-07 10:30:17 +0200

    tk-suite-client 4.4.25

--- Casks/tk-suite-client.rb
+++ Casks/tk-suite-client.rb
@@ -1,6 +1,6 @@
 class TkSuiteClient < Cask
-  version '4.4.13'
-  sha256 '4047cf17e9ca009a2c95e5127fafc145a74ba8320a4c4c94c866aeb49203a5ef'
+  version '4.4.25'
+  sha256 '2d6679ccb7225e212288f46a6c0239ab3305de829895e297590e78864b1150ce'
 
   url "ftp://ftp.agfeo.de/pub/software/TK-Suite-Client_intel32_agfeo_#{version}.dmg"
   homepage 'http://agfeo.de/agfeo_web/hp3.nsf/lu/2064'
