0c976815d6a8b98edeb39b4ef57213dca6c01193
Roland Walker <walker@pobox.com>
2014-10-07 10:42:40 -0400

    stanza layout/re-use version in gepetto.rb

--- Casks/geppetto.rb
+++ Casks/geppetto.rb
@@ -1,13 +1,15 @@
 class Geppetto < Cask
+  version '4.2.0'
+
   if Hardware::CPU.is_32_bit?
-    url 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86-4.2.0-R201407250959.zip'
     sha256 '78f578ff4cf0a9eadf85cc5a821e55125ee98ab4a8e1d4f0f5d1607487314804'
+    url "https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86-#{version}-R201407250959.zip"
   else
-    url 'https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-4.2.0-R201407250959.zip'
     sha256 '7a09c823cea9900cb51d009f47fab69569e1d8115c6326f3e91db62714480d69'
+    url "https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-#{version}-R201407250959.zip"
   end
   homepage 'http://puppetlabs.github.io/geppetto/'
   license :oss
-  version '4.2.0'
+
   app 'geppetto/Geppetto.app'
 end
