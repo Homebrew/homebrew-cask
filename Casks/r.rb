55acc1b148065ccad0d8268c20779166132985b4
Roland Walker <walker@pobox.com>
2014-10-07 10:56:51 -0400

    stanza layout/order+re-use version, r.rb

--- Casks/r.rb
+++ Casks/r.rb
@@ -1,16 +1,19 @@
 class R < Cask
+  version '3.1.1'
+
   if MacOS.version < :mavericks
-    url 'http://cran.rstudio.com/bin/macosx/R-3.1.1-snowleopard.pkg'
     sha256 '4db95d2bffdaa342a89d01088f47cfe6575ed7e953c31ea4dea629a0942b56b6'
-    pkg 'R-3.1.1-snowleopard.pkg'
+    url "http://cran.rstudio.com/bin/macosx/R-#{version}-snowleopard.pkg"
+    pkg "R-#{version}-snowleopard.pkg"
   else
-    url 'http://cran.rstudio.com/bin/macosx/R-3.1.1-mavericks.pkg'
     sha256 'd2f4e4f68628d998f81146eacd929ef6fb9bc01ca93d968e6562a3a6372c4d93'
-    pkg 'R-3.1.1-mavericks.pkg'
+    url "http://cran.rstudio.com/bin/macosx/R-#{version}-mavericks.pkg"
+    pkg "R-#{version}-mavericks.pkg"
   end
+
   homepage 'http://www.r-project.org/'
   license :unknown
-  version '3.1.1'
+
   uninstall :pkgutil => [
                          # eg org.r-project.R.maverics.fw.pkg
                          #   org.r-project.R.mavericks.GUI.pkg
