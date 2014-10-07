4ff192004cb38e82805c814c6a68143859ded449
Roland Walker <walker@pobox.com>
2014-10-07 10:57:20 -0400

    stanza layout/order, raw-photo-processor

--- Casks/raw-photo-processor.rb
+++ Casks/raw-photo-processor.rb
@@ -1,12 +1,12 @@
 class RawPhotoProcessor < Cask
+  version '4.7.2'
+  sha256 'ce003b7c78916baaf51b39e26633f9ac069e78b7e5bf8904b966c4c409c06f39'
 
   url 'http://www.raw-photo-processor.com/RPP/RPP_64.zip'
   appcast 'http://www.raw-photo-processor.com/rpp_updates.xml'
   homepage 'http://www.raw-photo-processor.com/RPP/Overview.html'
   license :unknown
 
-  version '4.7.2'
   nested_container 'RPP472_1672_64.dmg'
-  sha256 'ce003b7c78916baaf51b39e26633f9ac069e78b7e5bf8904b966c4c409c06f39'
   app 'Raw Photo Processor 64.app'
 end
