b10df29ce32705e6935a74f5d5ee5c2d922964d4
Roland Walker <walker@pobox.com>
2014-10-07 10:57:34 -0400

    stanza layout/order, reaper.rb

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,17 +1,18 @@
 class Reaper < Cask
-  homepage 'http://www.reaper.fm/'
-  license :unknown
   version '4.71'
 
   if Hardware::CPU.is_32_bit?
-    url 'http://www.reaper.fm/files/4.x/reaper471_i386.dmg'
     sha256 '1a670d5afb5956e1486c1fdef008ccfb4faa7987cc4d5ea8171a2794ff86ffca'
+    url 'http://www.reaper.fm/files/4.x/reaper471_i386.dmg'
     app 'REAPER.app'
     app 'ReaMote.app'
   else
-    url 'http://www.reaper.fm/files/4.x/reaper471_x86_64.dmg'
     sha256 '3045fd59189bd0fe398b50f511d014a5e385c8cfb88f8a188a8437050f783d71'
+    url 'http://www.reaper.fm/files/4.x/reaper471_x86_64.dmg'
     app 'REAPER64.app'
     app 'ReaMote64.app'
   end
+
+  homepage 'http://www.reaper.fm/'
+  license :unknown
 end
