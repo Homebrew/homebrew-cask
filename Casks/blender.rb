eac44295c0a103c6a4b3e78ae5a5632dac077a45
luckyluckyluke <mail@lucasribic.com>
2014-10-07 11:19:15 +0200

    Update Blender to 2.72

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,6 +1,6 @@
 class Blender < Cask
-  version '2.71'
-  sha256 'facd97d6d4c6ff9088c1ee994468d35342435054fb5d02c3ae7eee38045d0bc6'
+  version '2.72'
+  sha256 '07f77c68c171ec6c32af85ce8430c843dabd5d208c0d352bb15f6208fb77dab1'
 
   url "http://download.blender.org/release/Blender#{version}/blender-#{version}-OSX_10.6-x86_64.zip"
   homepage 'http://www.blender.org/'
