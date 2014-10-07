98474d84eea4c65feadc394e096b61b55e95eab9
Roland Walker <walker@pobox.com>
2014-10-07 09:06:46 -0400

    reformat maintenance.rb, provide `else` in `if`

--- Casks/maintenance.rb
+++ Casks/maintenance.rb
@@ -1,23 +1,25 @@
 class Maintenance < Cask
   version :latest
   sha256 :no_check
-  homepage 'http://www.titanium.free.fr/downloadmaintenance.php'
-  license :closed
-  app 'Maintenance.app'
 
-  if MacOS.version == :mavericks
-    url 'http://joel.barriere.pagesperso-orange.fr/dl/109/Maintenance.dmg'
-  elsif MacOS.version == :mountain_lion
-    url 'http://joel.barriere.pagesperso-orange.fr/dl/108/Maintenance.dmg'
-  elsif MacOS.version == :lion
-    url 'http://www.titanium.free.fr/download/107/Maintenance.dmg'
-  elsif MacOS.version == :snow_leopard
-    url 'http://www.titanium.free.fr/download/106/Maintenance.dmg'
+  if MacOS.version == :tiger
+    url 'http://www.titanium.free.fr/download/104/Maintenance.dmg'
   elsif MacOS.version == :leopard
     url 'http://www.titanium.free.fr/download/105/Maintenance.dmg'
-  elsif MacOS.version == :tiger
-    url 'http://www.titanium.free.fr/download/104/Maintenance.dmg'
+  elsif MacOS.version == :snow_leopard
+    url 'http://www.titanium.free.fr/download/106/Maintenance.dmg'
+  elsif MacOS.version == :lion
+    url 'http://www.titanium.free.fr/download/107/Maintenance.dmg'
+  elsif MacOS.version == :mountain_lion
+    url 'http://joel.barriere.pagesperso-orange.fr/dl/108/Maintenance.dmg'
+  else
+    url 'http://joel.barriere.pagesperso-orange.fr/dl/109/Maintenance.dmg'
   end
+
+  homepage 'http://www.titanium.free.fr/downloadmaintenance.php'
+  license :closed
+
+  app 'Maintenance.app'
 
   caveats do
     os_version_only('10.4', '10.5', '10.6', '10.7', '10.8', '10.9')
