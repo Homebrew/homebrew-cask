ff6a3dd0bd3ce4c5fbe03e3884d7c761226e0e4f
Roland Walker <walker@pobox.com>
2014-10-07 10:43:56 -0400

    stanza layout/order, googleappenginelauncher.rb

--- Casks/googleappenginelauncher.rb
+++ Casks/googleappenginelauncher.rb
@@ -1,6 +1,7 @@
 class Googleappenginelauncher < Cask
   version '1.9.11'
   sha256 'e198b384760686012438d3a4b2890e682e39e16007b22fc12693d16323bdf5ed'
+
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
   homepage 'https://developers.google.com/appengine/'
   license :unknown
