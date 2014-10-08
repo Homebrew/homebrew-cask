7a8d50d53fd7df6865f06f1aea0e4ac476670e97
Vítor Galvão <info@vitorgalvao.com>
2014-10-08 12:14:25 +0100

    re-use version in track-o-bot

--- Casks/track-o-bot.rb
+++ Casks/track-o-bot.rb
@@ -2,7 +2,7 @@ class TrackOBot < Cask
   version '0.4.2'
   sha256 '3746f662cda62047c8b9e870997bfc8d4f04040b72e1573177c4da2cfc5e8cb3'
 
-  url 'https://github.com/stevschmid/track-o-bot/releases/download/0.4.2/Track-o-Bot_0.4.2.dmg'
+  url "https://github.com/stevschmid/track-o-bot/releases/download/#{version}/Track-o-Bot_#{version}.dmg"
   homepage 'https://trackobot.com/'
   license :gpl
 
