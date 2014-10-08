6322aeae610dbc3c4f3c7180bf960e05aae76735
FinalDes <alex@usj.edu.mo>
2014-10-08 11:12:44 +0800

    Update cura.rb

--- Casks/cura.rb
+++ Casks/cura.rb
@@ -1,5 +1,5 @@
 class Cura < Cask
-  version '14.07'
+  version '14.09'
   sha256 '6f627cd60bc849f86d9f4b29b3d2971744187b7dc5392965464d16dd45a35f9a'
 
   url "http://software.ultimaker.com/current/Cura-#{version}-MacOS.dmg"
