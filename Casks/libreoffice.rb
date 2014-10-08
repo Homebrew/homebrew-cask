b118ac4e2388560c345744dae8841d6082a62517
Daniel Sager <daniel@sager1.de>
2014-10-08 16:34:43 +0200

    updated libreoffice to version 4.3.2

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -1,11 +1,11 @@
 class Libreoffice < Cask
-  version '4.3.1'
+  version '4.3.2'
 
   if Hardware::CPU.is_32_bit? or MacOS.version < :mountain_lion
-    sha256 'a2d507f643b952282ff5ce90ac227bea9bd412748ffcb93050db75256b2f803c'
+    sha256 '5a41ebe0c09f663e2ba420e1ddd50601d0ba1c828b08eb8617e72bbeebe826a5'
     url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86/LibreOffice_#{version}_MacOS_x86.dmg"
   else
-    sha256 '9a212ca4b77770c57f8b7ac375b5a98824c93dabd6e238dc019dc1139b6d3b7f'
+    sha256 'e5246aa91a62fcb85596ecaa16ca608e34eca79caddf529750a8126f554f97cb'
     url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   end
   gpg "#{url}.asc",
