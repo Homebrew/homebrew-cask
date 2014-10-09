b9b18ea9137a0e1b2b76c6ae1bf38af74b7f360e
Keith Finlay <kfinlay@gmail.com>
2014-10-09 11:40:30 -0500

    Fixed name and uninstall errors

--- Casks/stattransfer.rb
+++ Casks/stattransfer.rb
@@ -1,10 +1,11 @@
-class StatTransfer < Cask
-  version :latest
+class Stattransfer < Cask
+  version '12'
   sha256 :no_check
 
   url "http://www.stattransfer.com/downloads/stdemo.dmg"
   homepage 'https://stattransfer.com/'
   license :commercial
 
-  pkg 'st_installer_v12.pkg'
+  pkg 'st_installer_v#{version}.pkg'
+  uninstall :pkgutil => 'com.circlesys.pkg',
 end
