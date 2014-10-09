2814a6de6380bb57ed189ab67ee4b93e30f51e08
Vítor Galvão <info@vitorgalvao.com>
2014-10-09 16:28:11 +0100

    comment use of dropbox in actotracker.rb

--- Casks/actotracker.rb
+++ Casks/actotracker.rb
@@ -2,6 +2,7 @@ class Actotracker < Cask
   version :latest
   sha256 :no_check
 
+  # dropbox is the official download host per the vendor homepage
   url 'https://dl.dropboxusercontent.com/u/7614970/ActoTracker.zip'
   homepage 'http://onflapp.wordpress.com/actotracker'
   license :unknown
