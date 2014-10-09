9c7249710a4f4dd14c8d54e2fcabc6f47235f638
Vítor Galvão <info@vitorgalvao.com>
2014-10-09 16:28:11 +0100

    comment use of dropbox in jsonlook.rb

--- Casks/jsonlook.rb
+++ Casks/jsonlook.rb
@@ -2,6 +2,7 @@ class Jsonlook < Cask
   version :latest
   sha256 :no_check
 
+  # dropbox is the official download host per the vendor homepage
   url 'https://dl.dropbox.com/u/3878216/github/jsonlook.qlgenerator.zip'
   homepage 'https://github.com/rjregenold/jsonlook'
   license :oss
