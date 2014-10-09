83886a6a75c5da790efadb1c9cb4b2e610d312d7
Keith Finlay <kfinlay@gmail.com>
2014-10-09 11:19:10 -0500

    Create splitshow.rb

---
+++ Casks/splitshow.rb
@@ -0,0 +1,10 @@
+class Splitshow < Cask
+  version '0.9.1'
+  sha256 'd82b4f4d69dec6307901747018841e4617bfaa411acc83e43af75ca7d8b80a04'
+
+  url "https://splitshow.googlecode.com/files/SplitShow_v#{version}.dmg"
+  homepage 'https://code.google.com/p/splitshow/'
+  license :mit
+
+  app 'SplitShow.app'
+end
