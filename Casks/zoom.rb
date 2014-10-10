3fd9b618357d6f94343f0dd50ca52a333d0c566e
dwsarber <dylan.sarber@gmail.com>
2014-10-09 21:08:20 -0700

    Add Zoom v1.1.5
    Closes #6593.

---
+++ Casks/zoom.rb
@@ -0,0 +1,10 @@
+class Zoom < Cask
+  version '1.1.5'
+  sha256 'ab9584758d922f3fa2c607ae6bf58841969fdd5740ff17af76f41ecfd7c6ae11'
+
+  url "http://www.logicalshift.co.uk/mac/Zoom-#{version}.dmg"
+  homepage 'http://www.logicalshift.co.uk/unix/zoom/'
+  license :gpl
+
+  app 'Zoom.app'
+end
