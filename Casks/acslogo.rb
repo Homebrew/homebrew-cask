69cb40215245bcb5320d0f240dae2855704d703d
Pawel Niewiadomski <11110000b@gmail.com>
2014-10-10 20:14:59 +0200

    Add ACSLogo.app v1.5.1
    Adding Mac OS X Logo interpreter.

---
+++ Casks/acslogo.rb
@@ -0,0 +1,9 @@
+class Acslogo < Cask
+  version '1.5.1'
+  sha256 'fcdab9679a9cc783d4b7912d611442faa1fe1293497c4cb649f6808a58d27082'
+
+  url "http://www.alancsmith.co.uk/logo/ACSLogo#{version.gsub('.', '')}.dmg"
+  homepage 'http://www.alancsmith.co.uk/logo/'
+
+  app 'ACSLogo/ACSLogo.app'
+end
