e3edda5d16b72634a82d65b16270492722ad86d5
Leask Huang <i@leaskh.com>
2014-10-10 15:59:21 +0800

    update skype infos to version 7

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -5,9 +5,9 @@ class Skype < Cask
     sha256 '592abdd157df12d718576a86c8f8e62fced55292fd7e6909d53aa5eaaa9218f4'
     url "http://download.skype.com/macosx/Skype_#{version}.dmg"
   else
-    version :latest
-    sha256 :no_check
-    url 'https://www.skype.com/go/getskype-macosx.dmg'
+    version '7.0.653'
+    sha256 '58049191825e5f73daeb3118d405f84d5cadb9c84ca21b0cf4882ffb2f6c87fc'
+    url "http://download.skype.com/macosx/0ddb8638badf78e8ce1d4904fb1e1a8c/Skype_#{version}.dmg"
   end
 
   homepage 'http://www.skype.com'
