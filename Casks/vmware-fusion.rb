a61dd107fa93754e134a64cfc2d4f79affbb8aa8
Roland Walker <walker@pobox.com>
2014-10-11 10:35:29 -0400

    add zap stanza, vmware-fusion

--- Casks/vmware-fusion.rb
+++ Casks/vmware-fusion.rb
@@ -12,8 +12,16 @@ class VmwareFusion < Cask
   binary 'VMware Fusion.app/Contents/Library/vmware-vdiskmanager'
   binary 'VMware Fusion.app/Contents/Library/VMware OVF Tool/ovftool'
   app 'VMware Fusion.app'
+
   uninstall_preflight do
     system '/usr/bin/sudo', '-E', '--',
            '/usr/sbin/chown', '-R', '--', "#{Etc.getpwuid(Process.euid).name}:staff", "#{destination_path}/VMware Fusion.app"
   end
+  zap :delete => [
+                  # note: '~/Library/Application Support/VMware Fusion' is not safe
+                  # to delete.  In older versions, VM images were located there.
+                  '~/Library/Caches/com.vmware.fusion',
+                  '~/Library/Logs/VMware',
+                  '~/Library/Logs/VMware Fusion',
+                 ]
 end
