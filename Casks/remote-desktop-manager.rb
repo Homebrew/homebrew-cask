6f6b501b0d7575d9bd63a1eaf10dabdf4fd7b8d1
Peter Brunner <pbrunner@gmail.com>
2014-10-11 09:09:22 -0400

    Remote Desktop Manager 1.0.2.0

--- Casks/remote-desktop-manager.rb
+++ Casks/remote-desktop-manager.rb
@@ -1,6 +1,6 @@
 class RemoteDesktopManager < Cask
-  version '1.0.1.0'
-  sha256 '7c63fcc059500eba6dc604c3e0115489e8baa3c7ae406c7eb51e0429eceb2a93'
+  version '1.0.2.0'
+  sha256 'd724df0195169b4b3bac4db6273433f4d64b1877dd50c1745ff1bc3b6115a258'
 
   url "http://download.devolutions.net/Mac/Devolutions.RemoteDesktopManager.Mac.#{version}.dmg"
   homepage 'http://remotedesktopmanager.com/'
