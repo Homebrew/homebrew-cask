020cabb9f9154896ee4fff2a32a64ed3a1cbd4c4
Roland Walker <walker@pobox.com>
2014-10-11 10:27:44 -0400

    add zap stanza, google-chrome.rb

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -8,4 +8,19 @@ class GoogleChrome < Cask
   tags :vendor => 'Google'
 
   app 'Google Chrome.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Google/Chrome',
+                  '~/Library/Caches/Google/Chrome',
+                  '~/Library/Caches/com.google.Chrome',
+                  '~/Library/Caches/com.google.Chrome.helper.EH',
+                  '~/Library/Caches/com.google.Keystone.Agent',
+                  '~/Library/Caches/com.google.SoftwareUpdate',
+                  '~/Library/Google/GoogleSoftwareUpdate',
+                  '~/Library/Logs/GoogleSoftwareUpdateAgent.log',
+                 ],
+      :rmdir  => [
+                  '~/Library/Caches/Google',
+                  '~/Library/Google',
+                 ]
 end
