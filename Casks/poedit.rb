69bb0c0d3590f505be85343ba1b8d0dc2b4f6e61
Federico Bond <federicobond@gmail.com>
2014-10-12 17:26:12 -0300

    Update Poedit to 1.6.9

--- Casks/poedit.rb
+++ Casks/poedit.rb
@@ -1,6 +1,6 @@
 class Poedit < Cask
-  version '1.6.6'
-  sha256 '2e1dcbea9867a38720f1d86520b2279d2c4f0857e9da9c6aac4eec0907891234'
+  version '1.6.9'
+  sha256 '401a917bee98994d6455e8d56c53b4ac04b2cf8a5abc0be4466c12f3b2a2a0ab'
 
   url "http://poedit.net/dl/Poedit-#{version}.zip"
   appcast 'http://releases.poedit.net/appcast-osx'
