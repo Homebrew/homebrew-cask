8003c4811dd25ec4d45f8134783e7f199695e85a
Keng Kiat Lim <kkin@crnrc.com>
2014-10-12 23:06:13 +0800

    Upgrade InsomniaX to 2.1.8

--- Casks/insomniax.rb
+++ Casks/insomniax.rb
@@ -1,6 +1,6 @@
 class Insomniax < Cask
-  version '2.1.6'
-  sha256 'da7ad2450102f20df5eeee499172cc42ad49932b29e705b44fdfa55e21cf6fa6'
+  version '2.1.8'
+  sha256 'abbf4b27e86da934042a1dcedd339ffb59e16c4aa8e0c0d44534533be42e3efb'
 
   url "http://insomniax.semaja2.net/InsomniaX-#{version}.tgz"
   homepage 'http://semaja2.net/projects/insomniaxinfo/'
