e4d62156fbffa98a772d6b919f14fe0d353b9f38
Lee Hanxue <hanxue.developer@gmail.com>
2014-10-14 07:01:02 +0800

    Update GoogleAppEngineLauncher 1.9.13

--- Casks/googleappenginelauncher.rb
+++ Casks/googleappenginelauncher.rb
@@ -1,6 +1,6 @@
 class Googleappenginelauncher < Cask
-  version '1.9.11'
-  sha256 'e198b384760686012438d3a4b2890e682e39e16007b22fc12693d16323bdf5ed'
+  version '1.9.13'
+  sha256 '6b5f8253518e31cc1723b69956a36f29577ab84b7431413406a33d807e92c15d'
 
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
   homepage 'https://developers.google.com/appengine/'
