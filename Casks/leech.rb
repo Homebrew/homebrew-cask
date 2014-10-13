6aa8b6bc633bcddd83f060bc3aa7e70344118f32
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-10-13 18:43:45 +0700

    Add Leech cask
    See <http://manytricks.com/leech/> for more info.

---
+++ Casks/leech.rb
@@ -0,0 +1,10 @@
+class Leech < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://manytricks.com/download/leech'
+  homepage 'http://manytricks.com/leech/'
+  license :commercial
+
+  app 'Leech.app'
+end
