17f5ffcf5e0b49a834ab5ac82f35d6c1ae4665a9
Roland Walker <walker@pobox.com>
2014-10-13 19:34:20 -0400

    fix openoffice url

--- Casks/openoffice.rb
+++ Casks/openoffice.rb
@@ -2,7 +2,7 @@ class Openoffice < Cask
   version '4.1.1'
   sha256 '7becbdbe2abbcfd9d27953d2e11dc07da308d80f208b5f27f6213d225dc95e64'
 
-  url "https://downloads.sourceforge.net/project/openofficeorg.mirror/#{version}/binaries/en-US/Apache_OpenOffice_#{version}_MacOS_x86-64_install_en-US.dmg"
+  url "http://downloads.sourceforge.net/sourceforge/openofficeorg.mirror/Apache_OpenOffice_#{version}_MacOS_x86-64_install_en-US.dmg"
   homepage 'http://www.openoffice.org/'
   license :oss
 
