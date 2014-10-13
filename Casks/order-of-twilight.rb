0481afb5862796ebceee9d3aaa2f43f15b5d8adb
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-10-13 19:03:33 +0700

    Add Order of Twilight cask

---
+++ Casks/order-of-twilight.rb
@@ -0,0 +1,10 @@
+class OrderOfTwilight < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://stabyourself.net/dl.php?file=orderoftwilight/orderoftwilight-osx.zip'
+  homepage 'http://stabyourself.net/orderoftwilight/'
+  license :oss
+
+  app 'Order of Twilight.app'
+end
