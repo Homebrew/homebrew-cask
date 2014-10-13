2bcf7509a44b86580a2db8242d6434616275c638
Roscoe Brooks <roscoe@roscoebrooks.com>
2014-10-13 12:38:36 -0400

    Add Bookends.app latest
    Fix license in Bookends.app Cask

---
+++ Casks/bookends.rb
@@ -0,0 +1,10 @@
+class Bookends < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.sonnysoftware.com/Bookends.dmg'
+  homepage 'http://www.sonnysoftware.com'
+  license :closed
+
+  app 'Bookends.app'
+end
