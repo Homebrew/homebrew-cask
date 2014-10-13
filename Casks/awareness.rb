2efaa4ca0c527e46cda0dd472ae5363d0c4b6622
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-10-13 22:05:59 +0700

    Specify that Awareness is gratis

--- Casks/awareness.rb
+++ Casks/awareness.rb
@@ -4,7 +4,7 @@ class Awareness < Cask
 
   url "http://iamfutureproof.com/downloads/Awareness-#{version}.dmg"
   homepage 'http://iamfutureproof.com/tools/awareness/'
-  license :unknown
+  license :gratis
 
   app 'Awareness.app'
   zap :delete => '~/Library/Preferences/com.futureproof.awareness.plist'
