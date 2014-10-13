d6026b78be80e4ba4872f81cadde56d3fd6ad21a
Federico Bond <federicobond@gmail.com>
2014-10-13 01:44:01 -0300

    Update VirtualBox to 4.3.18-96516

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,6 +1,6 @@
 class Virtualbox < Cask
-  version '4.3.16-95972'
-  sha256 '09bd6b352a425f199344092482ee0a72f398f784138810ef17b88d2aa30ad0c6'
+  version '4.3.18-96516'
+  sha256 'a0c6a9a9455397a929150065b2883714a1dd630f63ef88b8d8cf527944becfc6'
 
   url "http://download.virtualbox.org/virtualbox/#{version.gsub(/-.*/, '')}/VirtualBox-#{version}-OSX.dmg"
   homepage 'http://www.virtualbox.org'
