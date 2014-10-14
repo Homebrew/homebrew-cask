d556b506b975e8da7774b809acc89d007915ea26
Keng Kiat Lim <kkin@crnrc.com>
2014-10-14 22:24:32 +0800

    Upgrade TextExpander to 4.3.4

--- Casks/textexpander.rb
+++ Casks/textexpander.rb
@@ -1,6 +1,6 @@
 class Textexpander < Cask
-  version '4.3.3'
-  sha256 '1aa42fa3b1bcb6e41f662c040f3efde5713ddfe8f1aee9d03c8f60bcd9d65ddf'
+  version '4.3.4'
+  sha256 'f5862333a3c9da5292c9c91bff55a3c7a1358481aa6871237b9661a22694eda1'
 
   url "http://cdn.smilesoftware.com/TextExpander_#{version}.zip"
   appcast 'http://updates.smilesoftware.com/com.smileonmymac.textexpander.xml'
