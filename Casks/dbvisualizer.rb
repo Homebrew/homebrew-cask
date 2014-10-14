3ba602adbb6902608a161d7948eb63d3246e5598
Junior Ales <edilson.ales.jr@gmail.com>
2014-10-14 20:24:57 -0300

    Add Dbvisualizer.app v9.1.11

---
+++ Casks/dbvisualizer.rb
@@ -0,0 +1,10 @@
+class Dbvisualizer < Cask
+  version '9.1.11'
+  sha256 '066e63bfda0032dde126a64e51cfe037f5c8dfd226b43b069d09a2ff7df3d17b'
+
+  url 'http://www.dbvis.com/product_download/dbvis-9.1.11/media/dbvis_macos_9_1_11.dmg'
+  homepage 'http://www.dbvis.com/'
+  license :commercial
+
+  app 'DbVisualizer Installer.app'
+end
