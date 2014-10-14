77b203a9f072469952cf15732b6cf4836334294d
Alex Bukin <EMAIL>
2014-10-14 10:51:24 -0700

    update flash to 15.0.0.189

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,6 +1,6 @@
 class Flash < Cask
-  version '15.0.0.152'
-  sha256 'dfd22c6f5cc42b8c4dedf1214321d982d32bb7f7969a674463c4b13e55ceb2d4'
+  version '15.0.0.189'
+  sha256 'e9fba4902df0abaa6054c7e4572841abf25564de6ffdf6d30416fb7e3f30ab27'
 
   url 'http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_15_osx_pkg.dmg'
   homepage 'https://www.adobe.com/products/flashplayer/distribution3.html'
