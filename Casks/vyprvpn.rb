745d4b0e9fec3311e059c80685f6b17587a27edf
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-10-15 03:15:19 +0700

    Update vyprvpn.rb to 2.6.0.2408

--- Casks/vyprvpn.rb
+++ Casks/vyprvpn.rb
@@ -1,10 +1,10 @@
 class Vyprvpn < Cask
-  version '2.3.3.1851'
-  sha256 '598967057d8e932bb4e2b332c0664ce31b708ceb36b89bae4a8c2a0c07ea03ec'
+  version '2.6.0.2408'
+  sha256 'eafade7cd1ed880cefb412f67015d9bdc259670f83cae269913d5dfa97b502e4'
 
   url "https://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/#{version}/VyprVPN_v#{version}.dmg"
   homepage 'http://www.goldenfrog.com/vyprvpn'
-  license :unknown
+  license :commercial
 
   app 'VyprVPN.app'
 end
