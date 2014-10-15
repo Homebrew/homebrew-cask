ab9a3020a4708003aa9000fa672b6be6debc220b
Roland Walker <walker@pobox.com>
2014-10-15 11:43:52 -0400

    add appcast :sha256 in lyn

--- Casks/lyn.rb
+++ Casks/lyn.rb
@@ -3,7 +3,8 @@ class Lyn < Cask
   sha256 'a4200b35eb4e51c3932a9e7cd40e1be3291a26921da350d396d337fa667798f6'
 
   url "http://www.lynapp.com/downloads/Lyn-#{version}.dmg"
-  appcast 'http://www.lynapp.com/lyn/update.xml'
+  appcast 'http://www.lynapp.com/lyn/update.xml',
+          :sha256 => '76e8a68df36fb35893e5ad0c5fd464ec0feb0b6a6f857d6b57792e3c7eb82fb5'
   homepage 'http://www.lynapp.com/'
   license :unknown
 
