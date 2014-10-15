384489624c66b241ee29c67cd70dd587fba8a8cf
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in selfcontrol

--- Casks/selfcontrol.rb
+++ Casks/selfcontrol.rb
@@ -3,7 +3,8 @@ class Selfcontrol < Cask
   sha256 '8148b29d5c5bca08149b15ae1841bac87619624f8fbdebb071daa71ccb5a8efa'
 
   url "http://downloads.selfcontrolapp.com/SelfControl-#{version}.zip"
-  appcast 'http://selfcontrolapp.com/SelfControlAppcast.xml'
+  appcast 'http://selfcontrolapp.com/SelfControlAppcast.xml',
+          :sha256 => '05d48c097c072ffd92d4d938b6984c8a09f446a556dc30acf25e3896cc3e0826'
   homepage 'http://selfcontrolapp.com/'
   license :unknown
 
