1080195809efb61f436bfb68cc6bdde539499112
Roland Walker <walker@pobox.com>
2014-10-15 11:43:51 -0400

    add appcast :sha256 in jitsi

--- Casks/jitsi.rb
+++ Casks/jitsi.rb
@@ -3,7 +3,8 @@ class Jitsi < Cask
   sha256 'ceb6b2ab04206a51faf1dbffb704a7a60ae2b7c47834b50f87da5557f543ad13'
 
   url "https://download.jitsi.org/jitsi/macosx/jitsi-#{version}-latest.dmg"
-  appcast 'https://download.jitsi.org/jitsi/macosx/sparkle/updates.xml'
+  appcast 'https://download.jitsi.org/jitsi/macosx/sparkle/updates.xml',
+          :sha256 => 'db2939816e8a38c7197160ab455626af9aadc2c5603f2212e372cffb85b31949'
   homepage 'https://jitsi.org/'
   license :unknown
 
