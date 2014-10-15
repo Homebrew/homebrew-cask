305e605850c8cc6664f71887a667cc528815fc9b
Roland Walker <walker@pobox.com>
2014-10-15 11:43:52 -0400

    add appcast :sha256 in mactracker

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -3,7 +3,8 @@ class Mactracker < Cask
   sha256 '3a156082b09b3c4e1ef2d94cba91fe67c42ee0d4010fa7e73a213481c384dab5'
 
   url "http://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
-  appcast 'http://update.mactracker.ca/appcast-b.xml'
+  appcast 'http://update.mactracker.ca/appcast-b.xml',
+          :sha256 => '0185504097321035adee92a20eaf0138cea94420d302c4368499b5783675e67e'
   homepage 'http://mactracker.ca/'
   license :unknown
 
