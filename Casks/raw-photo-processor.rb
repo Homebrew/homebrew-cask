d1ebedb879bc93a440bd14c971381e884ce8634b
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in raw-photo-processor

--- Casks/raw-photo-processor.rb
+++ Casks/raw-photo-processor.rb
@@ -3,7 +3,8 @@ class RawPhotoProcessor < Cask
   sha256 'ce003b7c78916baaf51b39e26633f9ac069e78b7e5bf8904b966c4c409c06f39'
 
   url 'http://www.raw-photo-processor.com/RPP/RPP_64.zip'
-  appcast 'http://www.raw-photo-processor.com/rpp_updates.xml'
+  appcast 'http://www.raw-photo-processor.com/rpp_updates.xml',
+          :sha256 => '83e550582ec77d965383ba778ce68e0e0c3f71501a546fab8619119ec2825287'
   homepage 'http://www.raw-photo-processor.com/RPP/Overview.html'
   license :unknown
 
