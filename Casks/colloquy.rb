4e281a323e998e8b791e1c74dbf1b7197b58b7ee
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in colloquy

--- Casks/colloquy.rb
+++ Casks/colloquy.rb
@@ -3,7 +3,8 @@ class Colloquy < Cask
   sha256 'b1c54047b9a4aed313e9a4a86073aa5b1215cb8a4ea462c59e7a52d8be6b1a93'
 
   url "http://colloquy.info/downloads/colloquy-#{version}.zip"
-  appcast 'http://colloquy.info/update.php?rss'
+  appcast 'http://colloquy.info/update.php?rss',
+          :sha256 => 'd1eb727b05c5146585aa249354f016cc29a0ee1a71102c4c25fcdf56bc207f92'
   homepage 'http://colloquy.info/'
   license :unknown
 
