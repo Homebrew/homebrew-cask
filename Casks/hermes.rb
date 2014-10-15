d336d06069946d26c9173ea6ceeb8f5efb4fa9fa
Roland Walker <walker@pobox.com>
2014-10-15 11:43:50 -0400

    add appcast :sha256 in hermes

--- Casks/hermes.rb
+++ Casks/hermes.rb
@@ -3,7 +3,8 @@ class Hermes < Cask
   sha256 'f03963ce4fdb8461c269882080beebc56101158bce13e13d3f1adce8b20fd5d2'
 
   url "https://s3.amazonaws.com/alexcrichton-hermes/Hermes-#{version}.zip"
-  appcast 'http://hermesapp.org/versions.xml'
+  appcast 'http://hermesapp.org/versions.xml',
+          :sha256 => '2fddeaa4e839650ce49be65b8cac2b9fb5906f641a63b4ee7960ca0570efc79d'
   homepage 'http://hermesapp.org/'
   license :unknown
 
