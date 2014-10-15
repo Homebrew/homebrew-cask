e3f77adc604347789c2c8fe8dfe22442e1aebab7
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in sequel-pro

--- Casks/sequel-pro.rb
+++ Casks/sequel-pro.rb
@@ -3,7 +3,8 @@ class SequelPro < Cask
   sha256 'facd99344d0124cf4444acbef9006d947eadc6f96127b09e7380f014c7775f85'
 
   url "https://sequel-pro.googlecode.com/files/sequel-pro-#{version}.dmg"
-  appcast 'http://www.sequelpro.com/appcast/app-releases.xml'
+  appcast 'http://www.sequelpro.com/appcast/app-releases.xml',
+          :sha256 => 'd6137595bccddd81edfb3a07a82b4ed818b8b1af79750397f929bf74b91d3e32'
   homepage 'http://www.sequelpro.com/'
   license :oss
 
