e8978a3b8f56e41c0d11cac7ade6c7a69dbfc41e
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-10-16 02:03:11 +0700

    Specify that Bee is commercial software

--- Casks/bee.rb
+++ Casks/bee.rb
@@ -5,7 +5,7 @@ class Bee < Cask
   url 'http://neat.io/bee/download.html'
   appcast 'http://neat.io/appcasts/bee-appcast.xml'
   homepage 'http://neat.io/bee/'
-  license :unknown
+  license :commercial
 
   app 'Bee.app'
 end
