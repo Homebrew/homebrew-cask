e58dc0c9b35000270261bcab31b6e5f7c15569c0
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in cyberduck

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -3,7 +3,8 @@ class Cyberduck < Cask
   sha256 'cff337f880b6ebbcc7f2e246b677fa5346fd225ae83289dca71ab35cf4b6eb8f'
 
   url "https://update.cyberduck.io/Cyberduck-#{version}.zip"
-  appcast 'https://version.cyberduck.io/changelog.rss'
+  appcast 'https://version.cyberduck.io/changelog.rss',
+          :sha256 => '9c35b816cb54cd4b984c7e18e239360a840da871b4fea148ce896d305860385e'
   homepage 'http://cyberduck.io/'
   license :gpl
 
