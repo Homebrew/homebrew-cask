c5b9dddd23aae91686968f26aeebcecfda42766b
Roland Walker <walker@pobox.com>
2014-10-15 11:43:57 -0400

    add appcast :sha256 in uninstallpkg

--- Casks/uninstallpkg.rb
+++ Casks/uninstallpkg.rb
@@ -3,7 +3,8 @@ class Uninstallpkg < Cask
   sha256 '199cbae47644572aade93bb81a152934178016da7cf90f3c7136b36d4819e752'
 
   url "http://www.corecode.at/downloads/uninstallpkg_#{version}.zip"
-  appcast 'http://www.corecode.at/uninstallpkg/uninstallpkg.xml'
+  appcast 'http://www.corecode.at/uninstallpkg/uninstallpkg.xml',
+          :sha256 => '3ca809f49f5210dcf5d20e049dc71397628d9405ae92a08d4b74a945e74a1076'
   homepage 'http://www.corecode.at/uninstallpkg/'
   license :unknown
 
