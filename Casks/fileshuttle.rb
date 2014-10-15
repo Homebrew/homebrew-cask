be5bb7a77fe1be0c0069308b80a16cc7d54ffa91
Roland Walker <walker@pobox.com>
2014-10-15 11:43:49 -0400

    add appcast :sha256 in fileshuttle

--- Casks/fileshuttle.rb
+++ Casks/fileshuttle.rb
@@ -3,7 +3,8 @@ class Fileshuttle < Cask
   sha256 '431b7cb2161eace5b8d39146bdd1b7cc9a85d335eb29175c1a1879208928565a'
 
   url "http://fileshuttle.io/fileshuttle-#{version}.zip"
-  appcast 'http://updates.getfileshuttle.com/update.xml'
+  appcast 'http://updates.getfileshuttle.com/update.xml',
+          :sha256 => 'ebb32260fb12727694a0f888cb264285ae219fa98ef6458d725b647f47a2c9e5'
   homepage 'http://fileshuttle.io/'
   license :unknown
 
