34ab58c49883144621ef898cec53e6d3233d347b
Roland Walker <walker@pobox.com>
2014-10-15 09:07:14 -0400

    update librecad.rb to 2.0.5
    * provide mountain lion-specific download
    * update license to :gpl

--- Casks/librecad.rb
+++ Casks/librecad.rb
@@ -1,10 +1,15 @@
 class Librecad < Cask
-  version '2.0.4'
-  sha256 'f9a2e56c5a4b79275d45d5578f728fbe3e981e04f550229579de71b0e8ce0604'
+  version '2.0.5'
 
-  url "https://downloads.sourceforge.net/project/librecad/OSX/#{version}/LibreCAD-#{version}.dmg"
+  if MacOS.version <= :mountain_lion
+    url "https://downloads.sourceforge.net/project/librecad/OSX/#{version}/LibreCAD-#{version}-MountainLion.dmg"
+    sha256 '6f1f1fe3f49ac404608965c1d40e7a109b7f416b7b0624c4d8e11e72f01c7ef6'
+  else
+    url "https://downloads.sourceforge.net/project/librecad/OSX/#{version}/LibreCAD-#{version}-Mavericks.dmg"
+    sha256 'ab8ccd20cb1f80b038e2aa471981d4e3e63a38ff632eba6382ef5904c4d3f2dd'
+  end
   homepage 'http://librecad.org/'
-  license :oss
+  license :gpl
 
   app 'LibreCAD.app'
 end
