c0fe4837b00d2f4a16c2d0753cd8209d3ee48fc1
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in quickradar

--- Casks/quickradar.rb
+++ Casks/quickradar.rb
@@ -3,7 +3,8 @@ class Quickradar < Cask
   sha256 'c2a275634c9f07e9dbffe762256b8b7068562ce6ae92cfdb51fc23e5e43979e5'
 
   url "http://www.quickradar.com/#{version}.zip"
-  appcast 'http://www.quickradar.com/appcast.xml'
+  appcast 'http://www.quickradar.com/appcast.xml',
+          :sha256 => 'a619b28824bf188921dc93eb9d475111c0f5d68ea3ec865562d922a7c312d1f3'
   homepage 'http://www.quickradar.com/'
   license :unknown
 
