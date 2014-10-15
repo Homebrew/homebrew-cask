4dd1eb70250b632b5b07bdd2b9ebafc9ead38042
Roland Walker <walker@pobox.com>
2014-10-15 11:43:56 -0400

    add appcast :sha256 in tex-live-utility

--- Casks/tex-live-utility.rb
+++ Casks/tex-live-utility.rb
@@ -3,7 +3,8 @@ class TexLiveUtility < Cask
   sha256 '9a913cbf0e158258c513fc0905f65632b36d372d1b4b9afe6b6f08dd005ff232'
 
   url "https://mactlmgr.googlecode.com/files/TeX%20Live%20Utility.app-#{version}.tar.gz"
-  appcast 'http://mactlmgr.googlecode.com/svn/trunk/appcast/tlu_appcast.xml'
+  appcast 'http://mactlmgr.googlecode.com/svn/trunk/appcast/tlu_appcast.xml',
+          :sha256 => '46284194061f7d6cc766ac15ed11267a4dbaa27a6f6643b3afe930fe7d89d746'
   homepage 'https://code.google.com/p/mactlmgr/'
   license :oss
 
