e6416b39730154d8042dabf96f17e2143a55b376
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in coteditor

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -3,7 +3,8 @@ class Coteditor < Cask
   sha256 '444133083698c7c94c2b029644f39a0e36982ae34c24745789fa890626188347'
 
   url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg"
-  appcast 'http://coteditor.github.io/appcast.xml'
+  appcast 'http://coteditor.github.io/appcast.xml',
+          :sha256 => '2a2df0599832911cc1596f23666303c6d7621b5e03cdbb3ecd602084b538ae47'
   homepage 'http://coteditor.github.io/'
   license :gpl
 
