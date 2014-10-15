ec10d70b2d0d50526b3a926c0a16503255f3ed53
Roland Walker <walker@pobox.com>
2014-10-15 11:43:52 -0400

    add appcast :sha256 in macdown

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -3,7 +3,8 @@ class Macdown < Cask
   sha256 '4929637cf1997809dc3070037b5e52b039561751e47118ea3aa436fecf6d4a90'
 
   url "http://macdown.uranusjr.com/download/v#{version}/"
-  appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml'
+  appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml',
+          :sha256 => 'fa671c25b375b36486df979070ec4bdece1e9ef8b281893d3bebf683d89cdba3'
   homepage 'http://macdown.uranusjr.com/'
   license :unknown
 
