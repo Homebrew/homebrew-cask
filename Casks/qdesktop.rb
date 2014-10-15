a29ba234fb02a42fcce5b35e104ad994bc3aa7ff
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in qdesktop

--- Casks/qdesktop.rb
+++ Casks/qdesktop.rb
@@ -3,7 +3,8 @@ class Qdesktop < Cask
   sha256 '1eddd3513cca892fb8e53452d79d4589ef5b1e3cd885a1f52748b6df64588094'
 
   url "https://bitbucket.org/qvacua/qvacua/downloads/Qdesktop-#{version}.zip"
-  appcast 'http://qvacua.com/qdesktop/appcast.xml'
+  appcast 'http://qvacua.com/qdesktop/appcast.xml',
+          :sha256 => '9714a19fae1e50cfdc06a1f300dae791d400b4c64a0f679b29d633ea8a59b46b'
   homepage 'http://qvacua.com'
   license :oss
 
