4cbe4964bd9cc0864540c548fb506c6cfee4d146
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in royal-tsx

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -3,7 +3,8 @@ class RoyalTsx < Cask
   sha256 '2306ae0579b097ccf4796f9cc08012819d3e55d5025a1424c8eca5330cdaedf5'
 
   url "http://app.royaltsx.com/updates/royaltsx_#{version}.dmg"
-  appcast 'http://app.royaltsx.com/updates_stable.php'
+  appcast 'http://app.royaltsx.com/updates_stable.php',
+          :sha256 => 'bb602755a8168a181b849f8d35c543207780d42c6b68ac69715bcdc713e5fd8e'
   homepage 'http://www.royaltsx.com'
   license :unknown
 
