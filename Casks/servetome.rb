71ca0e8071ceba96dcd5f56e8ab61d29b96aa023
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in servetome

--- Casks/servetome.rb
+++ Casks/servetome.rb
@@ -3,7 +3,8 @@ class Servetome < Cask
   sha256 '87bf1184b7656a96088b25880e7e1c772f30d8dfa1e602f9b43ccbdd0608fdf2'
 
   url "http://downloads.zqueue.com/ServeToMe-v#{version}.dmg"
-  appcast 'http://zqueue.com/servetome/stm3_mac_appcast.xml'
+  appcast 'http://zqueue.com/servetome/stm3_mac_appcast.xml',
+          :sha256 => '48cc93d336da8f71ab2a804d609e54d2e81ce4cd17f626e57aa4b7a76624ea69'
   homepage 'http://zqueue.com/servetome/'
   license :unknown
 
