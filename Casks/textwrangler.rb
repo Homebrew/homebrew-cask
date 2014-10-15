471bdebc25e95519e11cf434610dad690797f47a
Roland Walker <walker@pobox.com>
2014-10-15 11:43:56 -0400

    add appcast :sha256 in textwrangler

--- Casks/textwrangler.rb
+++ Casks/textwrangler.rb
@@ -3,7 +3,8 @@ class Textwrangler < Cask
   sha256 '52eb092165f86a7a6faddd9b97c75f9781ef13462fcdf9a16d919960db8613a8'
 
   url "http://ash.barebones.com/TextWrangler_#{version}.dmg"
-  appcast 'https://versioncheck.barebones.com/TextWrangler.xml'
+  appcast 'https://versioncheck.barebones.com/TextWrangler.xml',
+          :sha256 => '8a7c9a77fe921fe87d1503b9d0f47596c0a32e029f4fd13fac25c4d8ff8ad31e'
   homepage 'http://www.barebones.com/products/textwrangler'
   license :unknown
 
