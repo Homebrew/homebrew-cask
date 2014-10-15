74522221714f6640d5e37d4df951523cebb2e581
Roland Walker <walker@pobox.com>
2014-10-15 11:43:56 -0400

    add appcast :sha256 in tickets

--- Casks/tickets.rb
+++ Casks/tickets.rb
@@ -3,7 +3,8 @@ class Tickets < Cask
   sha256 '8f2d7879581fb2604158367fb6e7eda8bc9ebf15e97781b69b64355f2832af6e'
 
   url 'https://www.irradiatedsoftware.com/download/Tickets.zip'
-  appcast 'http://www.irradiatedsoftware.com/updates/profiles/tickets.php'
+  appcast 'http://www.irradiatedsoftware.com/updates/profiles/tickets.php',
+          :sha256 => '24a551412959452baf56b0cba06fdd502057f76812d596136b7fda1c14895386'
   homepage 'http://www.irradiatedsoftware.com/tickets/'
   license :unknown
 
