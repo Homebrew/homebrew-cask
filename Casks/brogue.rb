0ca89b13db610153c5769b91e5794215d2a33b8f
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-10-16 02:53:44 +0700

    Brogue is under GNU AGPL
    Sources are distributed in the same archive with the binary. It contains
    a license-agpl.txt file.

--- Casks/brogue.rb
+++ Casks/brogue.rb
@@ -4,7 +4,7 @@ class Brogue < Cask
 
   url "https://sites.google.com/site/broguegame/Brogue%20OS%20X%2010.6%2B%20v#{version}.zip?attredirects=0&d=1"
   homepage 'https://sites.google.com/site/broguegame/'
-  license :unknown
+  license :affero
 
   app "Brogue v#{version}/Brogue.app"
 end
