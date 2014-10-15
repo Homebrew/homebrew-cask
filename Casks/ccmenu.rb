bd205c66bda2400d697d349a7525331f60b7227a
Roland Walker <walker@pobox.com>
2014-10-15 11:43:47 -0400

    add appcast :sha256 in ccmenu

--- Casks/ccmenu.rb
+++ Casks/ccmenu.rb
@@ -3,7 +3,8 @@ class Ccmenu < Cask
   sha256 '974a2022dbc9494958334ee8f02e08df7ed184e1f421a53d623dfbeaadf08a2c'
 
   url "https://downloads.sourceforge.net/project/ccmenu/CCMenu/#{version}/ccmenu-#{version}-b.dmg"
-  appcast 'http://ccmenu.sourceforge.net/update-stable.xml'
+  appcast 'http://ccmenu.sourceforge.net/update-stable.xml',
+          :sha256 => 'a72951f416906d309cb7ec8d233bf5546540641efa99332e0f1bdde119b51cac'
   homepage 'http://ccmenu.sourceforge.net/'
   license :oss
 
