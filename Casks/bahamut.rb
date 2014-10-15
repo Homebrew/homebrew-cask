779bb8263d6a055f5ba65d9ff6f4ccff06e08a25
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-10-16 01:18:21 +0700

    Specify that Bahamut is under MIT license
    See the last section of
    https://github.com/sdegutis/bahamut/blob/master/README.md

--- Casks/bahamut.rb
+++ Casks/bahamut.rb
@@ -5,7 +5,7 @@ class Bahamut < Cask
   url 'https://raw.github.com/sdegutis/bahamut/master/Builds/Bahamut-LATEST.app.tar.gz'
   appcast 'https://raw.github.com/sdegutis/bahamut/master/appcast.xml'
   homepage 'https://github.com/sdegutis/bahamut'
-  license :oss
+  license :mit
 
   app 'Bahamut.app'
 end
