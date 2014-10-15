957da92e650bd330e4b3e5256b844942845b6824
Roland Walker <walker@pobox.com>
2014-10-15 11:43:52 -0400

    add appcast :sha256 in launchcontrol

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -3,7 +3,8 @@ class Launchcontrol < Cask
   sha256 '1475d384d1bfdb76e49a288b96a310b23a150946c568e045db0c0597b6c0b5a2'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
-  appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml'
+  appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
+          :sha256 => 'a82b64b1b68b0fc013145825eee9b18930378caa24a664d93d5777848c6fcc15'
   homepage 'http://www.soma-zone.com/LaunchControl/'
   license :unknown
 
