dcaf44795c986f2b4b89a83eef1dc6f20511319f
Roland Walker <walker@pobox.com>
2014-10-15 11:43:55 -0400

    add appcast :sha256 in sqleditor

--- Casks/sqleditor.rb
+++ Casks/sqleditor.rb
@@ -3,7 +3,8 @@ class Sqleditor < Cask
   sha256 '38f9d93d0ff7d680eff64949601ee4693eb91c38f89bd2f42a7e86809b36e9b5'
 
   url "https://www.malcolmhardie.com/sqleditor/releases/#{version}/SQLEditor-2-7.zip"
-  appcast 'https://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml'
+  appcast 'https://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml',
+          :sha256 => '927ddc279f600c4716b68fe0fc568c879ba4fb10a4a9cc19b535767994d00f65'
   homepage 'http://www.malcolmhardie.com/sqleditor/'
   license :unknown
 
