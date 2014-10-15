ab36648c1e6f41e1c6c91e57603f8b51b820cd34
Roland Walker <walker@pobox.com>
2014-10-15 11:43:51 -0400

    add appcast :sha256 in hive

--- Casks/hive.rb
+++ Casks/hive.rb
@@ -3,7 +3,8 @@ class Hive < Cask
   sha256 'bbf99210f9d359d1f75482abebd24eb7cb3103d437c47a0dbf107f4bf9dcd97b'
 
   url "https://github.com/hivewallet/hive-osx/releases/download/#{version}/Hive-#{version}.zip"
-  appcast 'https://hivewallet.com/hive-osx-appcast.xml'
+  appcast 'https://hivewallet.com/hive-osx-appcast.xml',
+          :sha256 => 'ab99fa8762d778ae379837c083214d70b3693b42cc8aa596d57a90b1d7e55c67'
   homepage 'http://www.hivewallet.com'
   license :oss
 
