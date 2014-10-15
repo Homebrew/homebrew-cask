6f8ec94eb12a32408ec03379fa197f7b86005c4b
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in coconutbattery

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -12,7 +12,8 @@ class Coconutbattery < Cask
     version '3.1.1'
     sha256 'd95951e14b77a3deff5503fd8ff7098d1a38fabe5c38764f4c9f5bd20d9bea16'
     url "http://www.coconut-flavour.com/downloads/coconutBattery_#{version.gsub('.','_')}.zip"
-    appcast 'http://updates.coconut-flavour.com/coconutBatteryIntel.xml'
+    appcast 'http://updates.coconut-flavour.com/coconutBatteryIntel.xml',
+            :sha256 => 'c7bbc2c79d4cb5ef74bf45b90700c773b9321c1846b19d6d4492d12748c23b27'
   end
 
   homepage 'http://www.coconut-flavour.com/coconutbattery/'
