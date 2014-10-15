2cf31535f74e0226501f6ad6c90be6e677da214c
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in connected-desktop

--- Casks/connected-desktop.rb
+++ Casks/connected-desktop.rb
@@ -3,7 +3,8 @@ class ConnectedDesktop < Cask
   sha256 '3c64d4234a7a2dc69112718be79eae2bad6b0c6bcd9514ce0dab08b5e2e64c98'
 
   url "https://secure.connecteddata.com/mac/2.4/software/Connected_Desktop_#{version}.dmg"
-  appcast 'http://appsoftware.connecteddata.com/mac/2.0/Appcast.xml'
+  appcast 'http://appsoftware.connecteddata.com/mac/2.0/Appcast.xml',
+          :sha256 => '51d51ee36b94570f4c02379f9aef8d0b1387abf97509757da89f0dd82b30cb95'
   homepage 'http://www.filetransporter.com/'
   license :unknown
 
