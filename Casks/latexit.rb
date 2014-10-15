d3501f86d2b1bdf2e3cfd2e70a55c35d531c7d6d
Roland Walker <walker@pobox.com>
2014-10-15 11:43:52 -0400

    add appcast :sha256 in latexit

--- Casks/latexit.rb
+++ Casks/latexit.rb
@@ -3,7 +3,8 @@ class Latexit < Cask
   sha256 '45efeeea0d7bde36ba08aa663d6dd10092ec66d7622bccccf73732257e1e82f0'
 
   url "http://www.chachatelier.fr/latexit/downloads/LaTeXiT-#{version.gsub('.', '_')}.dmg"
-  appcast 'http://pierre.chachatelier.fr/latexit/downloads/latexit-sparkle-en.rss'
+  appcast 'http://pierre.chachatelier.fr/latexit/downloads/latexit-sparkle-en.rss',
+          :sha256 => 'bc1bd88bf1d7a9770f0527652db2fc082214240a9b66684d9a95a0beaf2f260a'
   homepage 'http://www.chachatelier.fr/latexit'
   license :unknown
 
