d7c68559beaf325a06b49a710f63380aa753b145
Roland Walker <walker@pobox.com>
2014-10-15 11:43:47 -0400

    add appcast :sha256 in catch

--- Casks/catch.rb
+++ Casks/catch.rb
@@ -3,7 +3,8 @@ class Catch < Cask
   sha256 '0fd58d79ba869583406b7aa9ed707e4c77480417da9f09805f1497098fbbb138'
 
   url "https://github.com/mipstian/catch/releases/download/#{version}/Catch-#{version}.zip"
-  appcast 'https://raw.github.com/mipstian/catch/master/update/appcast.xml'
+  appcast 'https://raw.github.com/mipstian/catch/master/update/appcast.xml',
+          :sha256 => 'd559363812ead3090bf597711821d4e47dd281e7695f4e377767fffd56637a2d'
   homepage 'http://www.giorgiocalderolla.com/index.html'
   license :oss
 
