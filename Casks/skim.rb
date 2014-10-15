605efcf3237196ebf640a645d4e9e7522e2231b7
Roland Walker <walker@pobox.com>
2014-10-15 11:43:55 -0400

    add appcast :sha256 in skim

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -3,7 +3,8 @@ class Skim < Cask
   sha256 'b8964263e06b37b81443659d4c96385866d5b0af6e65ce393095b46e95591546'
 
   url "https://downloads.sourceforge.net/project/skim-app/Skim/Skim-#{version}/Skim-#{version}.dmg"
-  appcast 'http://skim-app.sourceforge.net/skim.xml'
+  appcast 'http://skim-app.sourceforge.net/skim.xml',
+          :sha256 => '92ff99e126c3daf99d680dd23f16ab84e26430e96478bbaeb71180756dd12ce1'
   homepage 'http://skim-app.sourceforge.net/'
   license :oss
 
