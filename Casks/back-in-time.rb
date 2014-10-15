eee623d96cfe55f902a254f8446b6c1382086ed1
Yegor Timoschenko <hello@yegortimoschenko.com>
2014-10-16 01:08:46 +0700

    Specify that Back in Time is commercial software

--- Casks/back-in-time.rb
+++ Casks/back-in-time.rb
@@ -4,7 +4,7 @@ class BackInTime < Cask
 
   url 'http://www.tri-edre.fr/pub/files/backintime3.dmg'
   homepage 'http://www.tri-edre.fr/english/backintime.html'
-  license :unknown
+  license :commercial
 
   app 'Back-In-Time 3.app'
 end
