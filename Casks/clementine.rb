98c06a936a34f062c1b9fc88141a37231aa48f23
Roland Walker <walker@pobox.com>
2014-10-15 11:43:47 -0400

    add appcast :sha256 in clementine

--- Casks/clementine.rb
+++ Casks/clementine.rb
@@ -3,7 +3,8 @@ class Clementine < Cask
   sha256 '7e3dac7fb184058e69afbf9cf68850debbbacdfbbd3023fbcdce7c159a2eed75'
 
   url "https://github.com/clementine-player/Clementine/releases/download/#{version}/clementine-#{version}.dmg"
-  appcast 'https://clementine-data.appspot.com/sparkle'
+  appcast 'https://clementine-data.appspot.com/sparkle',
+          :sha256 => 'aa4ef8bb841b9eea028181b5a884073fe51893f4a15d7061eb84f117ff161383'
   homepage 'http://www.clementine-player.org/'
   license :oss
 
