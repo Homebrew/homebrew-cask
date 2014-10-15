3ccb12f6b95f0780618c84f171b4fe5bf7d1e824
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in debt-quencher

--- Casks/debt-quencher.rb
+++ Casks/debt-quencher.rb
@@ -3,7 +3,8 @@ class DebtQuencher < Cask
   sha256 '2083fdb28d3138998a0d6b4b5c4405a18a2e8dd7b71554786386623313c166ad'
 
   url 'http://downloads.nothirst.com/DebtQuencher_1.2.zip'
-  appcast 'http://nothirst.com/feeds/DebtQuencherAppcast.xml'
+  appcast 'http://nothirst.com/feeds/DebtQuencherAppcast.xml',
+          :sha256 => 'c4cf59373c8956515d82f9ede91e326a770154ec10d637772f48c6aab1149b73'
   homepage 'http://nothirst.com/debtquencher/'
   license :unknown
 
