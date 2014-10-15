d3f1c80b62ed9f9fb12f86b9b413fff9ab2a4b51
Roland Walker <walker@pobox.com>
2014-10-15 11:43:49 -0400

    add appcast :sha256 in fake

--- Casks/fake.rb
+++ Casks/fake.rb
@@ -3,7 +3,8 @@ class Fake < Cask
   sha256 'a350b00e17e690077feb05c3b26a6ca173a21bfbe39d51d8db1cd5689a4574c5'
 
   url "http://fakeapp.com/dist/Fake_#{version}.zip"
-  appcast 'http://fakeapp.com/appcast/fake.rss'
+  appcast 'http://fakeapp.com/appcast/fake.rss',
+          :sha256 => '286ba147f304651cfbb44b03a1b5fd6b5f131cde3b04530a42cd4c5504e54b8a'
   homepage 'http://fakeapp.com/'
   license :unknown
 
