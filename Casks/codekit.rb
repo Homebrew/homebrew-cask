f8a92eef5aafaf6f7dd9c9752cdbaaf0d644be78
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in codekit

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -3,7 +3,8 @@ class Codekit < Cask
   sha256 '2d68f834e911527cce1c45315b8d8a986191b3aa0226371585a52ef0c9856c91'
 
   url 'https://incident57.com/codekit/files/codekit-17945.zip'
-  appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml'
+  appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml',
+          :sha256 => 'ff5efaf30dbd3190c9c06c8cbfcf1eb6c0f49a1ca803aac6d8e913685a35aacd'
   homepage 'http://incident57.com/codekit/'
   license :unknown
 
