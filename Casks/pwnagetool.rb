b4282a57377c3976760dc4baf3c5520082287cec
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in pwnagetool

--- Casks/pwnagetool.rb
+++ Casks/pwnagetool.rb
@@ -3,7 +3,8 @@ class Pwnagetool < Cask
   sha256 '84262734ad9f9186bce14a4f939d7ea290ed187782fdfa549a82c28bf837c808'
 
   url "https://sites.google.com/a/ipad-dev.com/files/pwnagetool/PwnageTool_#{version}.dmg"
-  appcast 'http://www.iphone-dev.org/appcast/PwnageTool2.xml'
+  appcast 'http://www.iphone-dev.org/appcast/PwnageTool2.xml',
+          :sha256 => '83d334e863f2a0ab58615cbc03805b9ed6a83daf496c0ce315285fbe635e35ce'
   homepage 'http://blog.iphone-dev.org/tagged/PwnageTool'
   license :unknown
 
