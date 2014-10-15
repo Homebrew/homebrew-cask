c2421bfa7004a9dea52286d90dbd90e181870844
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in cornerstone

--- Casks/cornerstone.rb
+++ Casks/cornerstone.rb
@@ -3,7 +3,8 @@ class Cornerstone < Cask
   sha256 '297237f24cbd605d6e7497fb893db3e7fbc6efef1333f5f25be8238a418d7650'
 
   url "https://www.zennaware.com/cornerstone/downloads/Cornerstone-#{version}.zip"
-  appcast 'http://www.zennaware.com/cornerstone/appcast/feed2.php'
+  appcast 'http://www.zennaware.com/cornerstone/appcast/feed2.php',
+          :sha256 => '333fee9fc2c478a0d5305ad2d9faecdb2c44e5554e8d43762c0917a7531fb145'
   homepage 'http://www.zennaware.com/cornerstone/index.php'
   license :unknown
 
