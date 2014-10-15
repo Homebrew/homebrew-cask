a6538354b83f489b4547a60996d032650edee9dd
Roland Walker <walker@pobox.com>
2014-10-15 11:43:53 -0400

    add appcast :sha256 in paintbrush

--- Casks/paintbrush.rb
+++ Casks/paintbrush.rb
@@ -3,7 +3,8 @@ class Paintbrush < Cask
   sha256 '1832bfb62de7247c17c522b49a9c88e3613ba5686ff05abb9b7fb545003b577d'
 
   url "https://downloads.sourceforge.net/project/paintbrush/Paintbrush%202.x/Paintbrush%20#{version}/Paintbrush-#{version}.zip"
-  appcast 'http://paintbrush.sourceforge.net/updates2x.xml'
+  appcast 'http://paintbrush.sourceforge.net/updates2x.xml',
+          :sha256 => 'ac96943a5d298201b711c719416073357f60545ef5ee7bf0853bbcf191bdc3d9'
   homepage 'http://paintbrush.sourceforge.net/'
   license :oss
 
