088d0603dce5ca5be84736bf048d6fff62374155
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in satellite-eyes

--- Casks/satellite-eyes.rb
+++ Casks/satellite-eyes.rb
@@ -3,7 +3,8 @@ class SatelliteEyes < Cask
   sha256 'adc1554c9095e3f316474e0f3712fa2a086d9405d126eca6359b8ebdc7451902'
 
   url "https://satellite-eyes.s3.amazonaws.com/satellite-eyes-#{version}.zip"
-  appcast 'https://satellite-eyes.s3.amazonaws.com/appcast.xml'
+  appcast 'https://satellite-eyes.s3.amazonaws.com/appcast.xml',
+          :sha256 => 'ef859a7550ea22ebbd91423e2336228a35e60bb901112cfe1f17c8f0481ccbf9'
   homepage 'http://satelliteeyes.tomtaylor.co.uk/'
   license :unknown
 
