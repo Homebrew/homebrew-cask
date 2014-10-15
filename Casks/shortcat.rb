9d01feb92b8bb27e54cecca327f1073fd1dc68d5
Roland Walker <walker@pobox.com>
2014-10-15 11:43:55 -0400

    add appcast :sha256 in shortcat

--- Casks/shortcat.rb
+++ Casks/shortcat.rb
@@ -3,7 +3,8 @@ class Shortcat < Cask
   sha256 '2f637b6ca13d7ea7f2635d617045695125a5a0ad6c20e913e9ea148ff9bed242'
 
   url "https://files.shortcatapp.com/v#{version}/Shortcat.zip"
-  appcast 'https://shortcatapp.com/updates/appcast.xml'
+  appcast 'https://shortcatapp.com/updates/appcast.xml',
+          :sha256 => '3cedb390a4cc871ccf6b279c711ba350bbc09cf4a710af65796bededa26530a7'
   homepage 'http://shortcatapp.com/'
   license :unknown
 
