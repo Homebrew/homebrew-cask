74d07b14855f981c87908bc376aa49baf61ab63c
Roland Walker <walker@pobox.com>
2014-10-15 11:43:52 -0400

    add appcast :sha256 in macupdate-desktop

--- Casks/macupdate-desktop.rb
+++ Casks/macupdate-desktop.rb
@@ -3,7 +3,8 @@ class MacupdateDesktop < Cask
   sha256 '1aa04e0bec920f28af5417a2b875e40af832d5303a29dca27e527e30c4db42f5'
 
   url "http://dl.macupdate.com/MacUpdateDesktop#{version}.zip"
-  appcast 'https://www.macupdate.com/desktop/updates.xml'
+  appcast 'https://www.macupdate.com/desktop/updates.xml',
+          :sha256 => 'f34602a28e842a77a7159342463747c05d783d44613feed26bbf742d88e3607f'
   homepage 'https://www.macupdate.com/desktop'
   license :unknown
 
