84540e77a24d18f0227420a81ecc10ad78e773df
Roland Walker <walker@pobox.com>
2014-10-15 11:43:47 -0400

    add appcast :sha256 in bootxchanger

--- Casks/bootxchanger.rb
+++ Casks/bootxchanger.rb
@@ -3,7 +3,8 @@ class Bootxchanger < Cask
   sha256 'cfb05680ab6a7d0b1793a33135dd15562a7b5fd59bb1ebf3ad6067c2c9fad6c1'
 
   url "http://namedfork.net/_media/bootxchanger_#{version}.dmg"
-  appcast 'http://swupdate.namedfork.net/bootxchanger.xml'
+  appcast 'http://swupdate.namedfork.net/bootxchanger.xml',
+          :sha256 => 'afbae3b2a378ebb18f1668c062ba48436e956cad840fcc86cba610d351931187'
   homepage 'http://namedfork.net/bootxchanger'
   license :gpl
 
