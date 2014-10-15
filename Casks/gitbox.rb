b0b8d407e62935d00bea691b62417df4945a0fd2
Roland Walker <walker@pobox.com>
2014-10-15 11:43:50 -0400

    add appcast :sha256 in gitbox

--- Casks/gitbox.rb
+++ Casks/gitbox.rb
@@ -3,7 +3,8 @@ class Gitbox < Cask
   sha256 '181ac55515221bf07c9994f25a11e962a9057e4249600b4d95424bcb6bb5c6d7'
 
   url "https://d1oa71y4zxyi0a.cloudfront.net/gitbox-#{version}-ml.zip"
-  appcast 'http://gitboxapp.com/updates.xml'
+  appcast 'http://gitboxapp.com/updates.xml',
+          :sha256 => '4a771aa4d35cd0bf9f821fbf1e60741cb1d9ee3724ff66550e46fba98bace8c1'
   homepage 'http://gitboxapp.com/'
   license :unknown
 
