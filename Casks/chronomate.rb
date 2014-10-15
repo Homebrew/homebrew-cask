290d7c7b4b3b4496dc64ad73bbddcd25ece75612
Roland Walker <walker@pobox.com>
2014-10-15 11:43:47 -0400

    add appcast :sha256 in chronomate

--- Casks/chronomate.rb
+++ Casks/chronomate.rb
@@ -3,7 +3,8 @@ class Chronomate < Cask
   sha256 '76b2f87b088c57aac7dfd18215b07e078d453ed351af056db25bb78d67e4862b'
 
   url "https://studio-qb.com/sites/studio-qb.com/files/releases/chronomate_#{version}.dmg"
-  appcast 'http://chronomateapp.com/update'
+  appcast 'http://chronomateapp.com/update',
+          :sha256 => 'fc538460e350c638a7e1b87e91e0cc79bbf8e324dc28819a818deee090037feb'
   homepage 'http://chronomateapp.com/'
   license :unknown
 
