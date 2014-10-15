963c1862b87d29603710100a44e4d7797e41e874
Roland Walker <walker@pobox.com>
2014-10-15 11:43:47 -0400

    add appcast :sha256 in cloudytabs

--- Casks/cloudytabs.rb
+++ Casks/cloudytabs.rb
@@ -3,7 +3,8 @@ class Cloudytabs < Cask
   sha256 '7ba67b0f7415fe2d2cb545866eb0e9c7c2bea8980445921a9da5d2bf55711d76'
 
   url "https://github.com/josh-/CloudyTabs/releases/download/v#{version}/CloudyTabs.zip"
-  appcast 'http://joshparnham.com/projects/cloudytabs/appcast.xml'
+  appcast 'http://joshparnham.com/projects/cloudytabs/appcast.xml',
+          :sha256 => '70140cb26a2a25589d739d6dac6f2e04118a938814909fe08c720b0174adf2b4'
   homepage 'https://github.com/josh-/CloudyTabs/'
   license :oss
 
