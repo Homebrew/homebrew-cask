4e892166fa0d0410a87a9e23e28d9306907665bd
Roland Walker <walker@pobox.com>
2014-10-15 11:43:56 -0400

    add appcast :sha256 in texpad

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -3,7 +3,8 @@ class Texpad < Cask
   sha256 'e3fa71a11747d9698f72a8b9b6d5bcfca83f71ddec3dd69c3749362d47e85dcb'
 
   url 'https://cloud.texpadapp.com/bundles/Texpad_1_6_13.zip'
-  appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml'
+  appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml',
+          :sha256 => 'a8522a4533df93fbf9d50489aa5f1c91bb01916faebd81ee20f56ea96dad8d97'
   homepage 'https://www.texpadapp.com/osx'
   license :unknown
 
