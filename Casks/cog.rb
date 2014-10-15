d02d434ed052661c68b1fbe100c3e1d9aa8437cf
Roland Walker <walker@pobox.com>
2014-10-15 11:43:48 -0400

    add appcast :sha256 in cog

--- Casks/cog.rb
+++ Casks/cog.rb
@@ -3,7 +3,8 @@ class Cog < Cask
   sha256 '1cc55f718a15810cccc5141312c01079e1da230a2412e4bc66c172de7aee36fe'
 
   url 'http://cogx.org/nightly_builds/cog-r2051.tbz'
-  appcast 'http://mamburu.net/cog/stable.xml'
+  appcast 'http://mamburu.net/cog/stable.xml',
+          :sha256 => 'f5770d73ad0c4a19af24cf25195c01d1cc05b937a79416fe82ead0949beee62e'
   homepage 'http://cogx.org'
   license :unknown
 
