c7f9901437154cd77e622cc9db17984b023f25f0
Roland Walker <walker@pobox.com>
2014-10-15 11:43:57 -0400

    add appcast :sha256 in tunnelblick

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -3,7 +3,8 @@ class Tunnelblick < Cask
   sha256 '3296266375812c954e0f3ce91adca676d06767f9ac26e3ad78452e0de1425279'
 
   url "https://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_#{version}.dmg"
-  appcast 'https://www.tunnelblick.net/appcast.rss'
+  appcast 'https://www.tunnelblick.net/appcast.rss',
+          :sha256 => '7fa119cda4d782dc61cb75895c70b3572652df737c908270c48a09d67a874592'
   homepage 'https://code.google.com/p/tunnelblick/'
   license :oss
 
