cb9c4cbf2dfbed492ad55b09725213457116bdcd
Roland Walker <walker@pobox.com>
2014-10-15 11:43:55 -0400

    add appcast :sha256 in soqlxplorer

--- Casks/soqlxplorer.rb
+++ Casks/soqlxplorer.rb
@@ -3,7 +3,8 @@ class Soqlxplorer < Cask
   sha256 '72c5d9dc582bc30d757c5199cdebdc79ffee479a3435252906554d52b236217d'
 
   url "http://www.pocketsoap.com/osx/soqlx/soqlXplorer_v#{version}.zip"
-  appcast 'http://www.pocketsoap.com/osx/soqlx/appcast.xml'
+  appcast 'http://www.pocketsoap.com/osx/soqlx/appcast.xml',
+          :sha256 => '16b61d4f11f57698ffb54da8f25c1ca443f3f35606e1e794eca4c12ee6ebabae'
   homepage 'http://www.pocketsoap.com/osx/soqlx/'
   license :unknown
 
