edadacf7d6e9eae12e36f693f3274af53a777744
Roland Walker <walker@pobox.com>
2014-10-15 11:43:54 -0400

    add appcast :sha256 in scroll-reverser

--- Casks/scroll-reverser.rb
+++ Casks/scroll-reverser.rb
@@ -3,7 +3,8 @@ class ScrollReverser < Cask
   sha256 'babce7e360ec76a797ac8843e20d5b75a0475807c487df87c01bc29cd8050584'
 
   url "https://d20vhy8jiniubf.cloudfront.net/downloads/ScrollReverser-#{version}.zip"
-  appcast 'http://softwareupdate.pilotmoon.com/update/scrollreverser/appcast.xml'
+  appcast 'http://softwareupdate.pilotmoon.com/update/scrollreverser/appcast.xml',
+          :sha256 => 'ed8e2d6748eb8b79897ca1b84a1597e86afb7845b469a278c83c07e08e6eb886'
   homepage 'https://pilotmoon.com/scrollreverser/'
   license :unknown
 
