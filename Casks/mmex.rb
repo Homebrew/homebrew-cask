4ea89269a52027f19b9580ae2c5a094a15909ea3
Alex Weber <alexweber15@gmail.com>
2014-10-15 21:38:19 -0300

    fix typo in uri stanza

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -1,12 +1,7 @@
 class Mmex < Cask
-  # This just isn't working...
-  # version :latest
-  # sha256 :no_check
-  # url 'http://sourceforge.net/projects/moneymanagerex/files/latest/download'
-
   version '1.1.0'
   sha256 '37042d0255e4bf89aecab1373e8c27597ad654f9164a1ee1d2773f021c79a925'
-  url "http://sourceforge.net/projects/moneymanagerex/files/moneymanagerex/Version%20#{version}/mmex_#{version}_mac.dmg/download"
+  url "http://sourceforge.net/projects/moneymanagerex/files/moneymanagerex/Version%20#{version}/mmex_#{version}_mac.dmg"
 
   homepage 'http://www.moneymanagerex.org'
   license :gpl
