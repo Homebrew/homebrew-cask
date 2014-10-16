9ced1490fda5172161e3e00e0a6abb75279387ca
Claudia <claui@users.noreply.github.com>
2014-10-16 09:21:47 +0200

    Upgrade WebStorm.app to 8.0.5

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -1,6 +1,6 @@
 class Webstorm < Cask
-  version '8.0.4'
-  sha256 '33abeb4712e8515a7eae6c076edef0922bf96bf46538ff45bf627787e5e2eb9e'
+  version '8.0.5'
+  sha256 '48a320515a2ce809fadac81a3b688663e1538e91959248bbaded8418624ca585'
 
   url "http://download-cf.jetbrains.com/webstorm/WebStorm-#{version}.dmg"
   homepage 'http://www.jetbrains.com/webstorm/'
