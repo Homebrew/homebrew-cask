88cc4d0a294ce48a8c5dc2a65acee3670270ae56
Jason Thomas <jasonthomas@jasons-mbp.home>
2014-10-16 17:38:13 -0400

    added import.io Cask

---
+++ Casks/Import_io.rb
@@ -0,0 +1,10 @@
+class Import_io < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'https://d24s8iufolpmlr.cloudfront.net/prod/dist-simple/import.io.dmg'
+  homepage 'https://import.io/'
+  license :commercial
+
+  pkg 'install.pkg'
+end
