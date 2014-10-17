4d032a88a3825e884749fbce97fac8e6b4ca9d79
Brian Finney <bri@nfinney.com>
2014-10-16 20:01:05 -0700

    Update radiant-player to 1.3.1

--- Casks/radiant-player.rb
+++ Casks/radiant-player.rb
@@ -1,6 +1,6 @@
 class RadiantPlayer < Cask
-  version '1.3.0'
-  sha256 '6b9fb744d88148a4d74ee9a5f3f3101b57b0051c6376171c1966a48980d19229'
+  version '1.3.1'
+  sha256 'eed8d2c0d5457938b65ad41fd01ff9ea3745518e7951e62f68974b2884016dbf'
 
   url "https://github.com/kbhomes/radiant-player-mac/releases/download/v#{version}/Radiant.Player.zip"
   homepage 'http://kbhomes.github.io/radiant-player-mac/'
