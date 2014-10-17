40979e3b5fea52c8660c640458ca8bb5a8c86995
Roland Walker <walker@pobox.com>
2014-10-17 17:02:46 -0400

    point `chromium.rb` at canonical source
    fixes #6747

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,12 +1,10 @@
 class Chromium < Cask
-  version '38.0.2125.101'
-  sha256 '23e345fccc7863cb1d9e746487011a21c9e06ddd7d5e218360ef805a01f4b69d'
+  version :latest
+  sha256 :no_check
 
-  url "https://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_#{version}.dmg"
-  appcast 'http://osxportableapps.sourceforge.net/chromium/chrcast.xml',
-          :sha256 => 'e7774d6af3604681a096ab22d2d3b63bcb6fa779532ae137ff05ab6e482c209a'
-  homepage 'http://www.freesmug.org/chromium'
+  url 'https://download-chromium.appspot.com/dl/Mac'
+  homepage 'http://www.chromium.org/Home'
   license :oss
 
-  app 'Chromium.app'
+  app 'chrome-mac/Chromium.app'
 end
