0a1c2d1a46c610408caba28467dadea8bfeda5b4
Peter Josling <peterjosling@users.noreply.github.com>
2014-10-17 13:29:22 +0100

    Update dolphin (4.0-3587)
    Previous version was giving a 404

--- Casks/dolphin.rb
+++ Casks/dolphin.rb
@@ -1,6 +1,6 @@
 class Dolphin < Cask
-  version '4.0-3517'
-  sha256 '413adc0fc7ff4ffb2a1254939fbfe4cff2aa603ed955f37f48dcedb8483be1d4'
+  version '4.0-3587'
+  sha256 'c2b526d6dd8d9ff74ac908d2b13562910486b1647dbf022b36ac91f42ebfa499'
 
   url "http://dl.dolphin-emu.org/builds/dolphin-master-#{version}.dmg"
   homepage 'http://www.dolphin-emu.org/'
