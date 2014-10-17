60357ed1d5146d1b1b822fd7fa5e1741a87eb860
Beng Hee Eu <beng@benghee.eu>
2014-10-16 22:03:03 -0400

    1Password 5.0.1

--- Casks/onepassword.rb
+++ Casks/onepassword.rb
@@ -1,10 +1,10 @@
 class Onepassword < Cask
-  version '4.4.3'
-  sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
+  version '5.0.1'
+  sha256 '359c69dadb9422caab64e3db34c8c5ef5e9355938e811873a57daaf74560605c'
 
   url "http://i.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
   license :unknown
 
-  app '1Password 4.app'
+  app '1Password 5.app'
 end
