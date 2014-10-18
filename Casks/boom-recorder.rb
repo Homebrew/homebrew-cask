14e3173dcea0892e4a06a11fe87277db30b73f87
Roland Walker <walker@pobox.com>
2014-10-18 13:04:28 -0400

    reuse version in boom-recorder.rb

--- Casks/boom-recorder.rb
+++ Casks/boom-recorder.rb
@@ -2,7 +2,7 @@ class BoomRecorder < Cask
   version '8.4.0'
   sha256 '6090543358b142d4d14197ef810f9e3641e10c6e1309ac5f8c18c486ab1258a7'
 
-  url 'http://www.vosgames.nl/media/downloads/Boom%20Recorder-8.4.0.dmg'
+  url "http://www.vosgames.nl/media/downloads/Boom%20Recorder-#{version}.dmg"
   homepage 'http://www.vosgames.nl/products/BoomRecorder/'
   license :commercial
 
