8fd80f3a42a88e962647daed101a671b5a3aad06
iggyvolz <iggyvolz@aim.com>
2014-10-18 11:36:59 -0400

    Fix sophos checksum, fix #6779
    The Sophos checksum has been updated.  This fixes #6779

--- Casks/sophos-anti-virus-home-edition.rb
+++ Casks/sophos-anti-virus-home-edition.rb
@@ -1,6 +1,6 @@
 class SophosAntiVirusHomeEdition < Cask
   version '9.0'
-  sha256 '6a871dbcef238f4fdbb8f068ac556691abb965a430e6fce0061d735404f352ae'
+  sha256 '86bcd0a10de1dc2455f3f1e9330c0b083e30ce435822daaa7ea10f04f63f5f10'
 
   url 'http://downloads.sophos.com/home-edition/savosx_90_he.zip'
   homepage 'http://www.sophos.com/en-us/products/free-tools/sophos-antivirus-for-mac-home-edition.aspx/'
