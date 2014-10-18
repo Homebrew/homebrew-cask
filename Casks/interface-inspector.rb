98f12929a6c2ed760411d8077863dc5878ab7c76
Herb <i@4leaf.me>
2014-10-18 21:38:47 +0800

    interface-inspector

---
+++ Casks/interface-inspector.rb
@@ -0,0 +1,10 @@
+class InterfaceInspector < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.interface-inspector.com/download/Interface-Inspector-Latest.zip'
+  homepage 'http://www.interface-inspector.com/'
+  license :closed
+
+  app 'Interface Inspector.app'
+end
