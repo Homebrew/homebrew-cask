95dd479d97bd83b7b28a17a350db7e08c090df61
José Padilla <jpadilla@webapplicate.com>
2014-10-18 17:25:34 -0400

    Upgrade Mailbox to 0.3.14_141017

--- Casks/mailbox.rb
+++ Casks/mailbox.rb
@@ -1,10 +1,10 @@
 class Mailbox < Cask
-  version '0.3.12_141007'
-  sha256 'd5d388832ebf3590a24fea4ceb3312108e1c061250d4e6d8e83c02fd8d6cc8c6'
+  version '0.3.14_141017'
+  sha256 '93a0ebd06399b899a8742148c1df6876f82716790dd394009fd8edc59078e902'
 
   url "https://mb-dtop.s3.amazonaws.com/external-beta/Mailbox_Ext_Beta_#{version}.zip"
   appcast 'https://mb-dtop.s3.amazonaws.com/external-beta/external-beta-appcast.xml',
-          :sha256 => '77e7aab1ee7ac2387ee2951aad986c6cfc1cf8d4ce5d09b26fa2837b783b581c'
+          :sha256 => '28d87eb6a36077e16b8c52ede51d81316eea6bb0636608616173e0edbaa5be11'
   homepage 'http://www.mailboxapp.com/'
   license :unknown
 
