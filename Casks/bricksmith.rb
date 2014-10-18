2a5cecacbb7d4f57d81210a57c4dea7110c4439f
Herb <i@4leaf.me>
2014-10-18 21:37:22 +0800

    bricksmith

---
+++ Casks/bricksmith.rb
@@ -0,0 +1,10 @@
+class Bricksmith < Cask
+  version '3.0'
+  sha256 'bf7dcd7508d2d6e37f6079080e8ae51095e14d05eb507ad85ac4bbaf5b9ae7c5'
+
+  url 'http://sourceforge.net/projects/bricksmith/files/bricksmith/Bricksmith%203.0/BricksmithComplete3.0.zip/download'
+  homepage 'http://bricksmith.sourceforge.net/'
+  license :oss
+
+  suite 'Bricksmith'
+end
