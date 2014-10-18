4f2ad3aeedfd9b94c4276b13e08fee511f990277
Roland Walker <walker@pobox.com>
2014-10-18 13:05:52 -0400

    reuse version in sauerbraten.rb

--- Casks/sauerbraten.rb
+++ Casks/sauerbraten.rb
@@ -2,7 +2,7 @@ class Sauerbraten < Cask
   version '2013.01.26'
   sha256 'c6807484fa0d2c42ac774b97f9b21e9eace720e4403abc11b0321b2645d3589d'
 
-  url 'http://downloads.sourceforge.net/sauerbraten/sauerbraten_2013_01_26_collect_edition_macosx.dmg'
+  url "http://downloads.sourceforge.net/sauerbraten/sauerbraten_#{version.gsub('.','_')}_collect_edition_macosx.dmg"
   homepage 'http://sauerbraten.org'
   license :oss
 
