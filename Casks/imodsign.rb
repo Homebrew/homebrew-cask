6c146e532e5fbbdd4df63717a1b47c0c88fb5ce5
Herb <i@4leaf.me>
2014-10-18 21:56:03 +0800

    imodsign

---
+++ Casks/imodsign.rb
@@ -0,0 +1,10 @@
+class Imodsign < Cask
+  version '3.6'
+  sha256 '7cbb66e9871d480f0d265b43ebffe26934527d151f9154f90fbf6039beaa9abe'
+
+  url 'https://imzdl.com/iModSign/iModSign.zip'
+  homepage 'https://imzdl.com/downloads.php'
+  license :closed
+
+  app 'iModSign.app'
+end
