8098a8651d47945cf4b8bfc9f0c302bd5bda6eaf
Herb <i@4leaf.me>
2014-10-19 13:13:55 +0800

    Update Airserver to 5.0.7

--- Casks/airserver.rb
+++ Casks/airserver.rb
@@ -1,6 +1,6 @@
 class Airserver < Cask
-  version '5.0.4.0'
-  sha256 'd6e667bf91a0fb87d000ed778b3cf46c0d9d6e6d397a85de196f5da09f7f1a7a'
+  version '5.0.7'
+  sha256 '56ac7cdba5dddc349776feb1390e84e4ba1f61fa94c2f4f8fea34193b7327228'
 
   url "http://dl.airserver.com/mac/AirServer-#{version}.dmg"
   homepage 'http://www.airserver.com'
