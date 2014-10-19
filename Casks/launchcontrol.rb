d426224d31c85b4bce252269f86db9cb2729051e
oleg <obutovich@gmail.com>
2014-10-19 09:10:00 +0200

    update for backuploupe and launchcontrol

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,6 +1,6 @@
 class Launchcontrol < Cask
-  version '1.16.1'
-  sha256 '1475d384d1bfdb76e49a288b96a310b23a150946c568e045db0c0597b6c0b5a2'
+  version '1.16.2'
+  sha256 '207b90ff2342ad3eee66036c7e97057d1c73f5c81abd243a4804e715d371e015'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
