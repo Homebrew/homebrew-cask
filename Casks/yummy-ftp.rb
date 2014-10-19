85b537ed1eb15031a142976874bfcfb472f0b2bc
Pan Long <aga.panlong@me.com>
2014-10-19 21:56:48 +0800

    added yummy ftp

---
+++ Casks/yummy-ftp.rb
@@ -0,0 +1,10 @@
+class YummyFtp < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.yummysoftware.com/downloads/YummyFTP.zip'
+  homepage 'http://www.yummysoftware.com'
+  license :unknown
+
+  app 'Yummy FTP.app'
+end
