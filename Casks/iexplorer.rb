8c242aaed86801c12c3f3ec1ed9b6e5e4e6237a6
Angelo Olivera <aolivera@gmail.com>
2014-10-20 17:40:43 -0400

    Update iexplorer

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,6 +1,6 @@
 class Iexplorer < Cask
-  version '3.5.1.1'
-  sha256 '0789b544aef8c94c64e5a85629f97b58499ba5579ebbb03be442dd950d092813'
+  version '3.5.1.2'
+  sha256 'd94d00f9eaad1f6b7c0ac1d9c6f426ba152ad0434ac8b41b55761121b37dcd31'
 
   url "http://cdn.macroplant.com/release/iExplorer-#{version}.dmg"
   appcast 'http://www.macroplant.com/iexplorer/ie3-appcast.xml',
