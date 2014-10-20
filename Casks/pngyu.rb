ce57d28b576cd40fc91e118989b03799cac32d6d
Roland Walker <walker@pobox.com>
2014-10-20 11:10:51 -0400

    re-use version in pngyu.rb

--- Casks/pngyu.rb
+++ Casks/pngyu.rb
@@ -2,7 +2,7 @@ class Pngyu < Cask
   version '1.0.1'
   sha256 'f853a3566236200391a40d12df9519b39f4d30053ab6ed6f60670b7eacca6217'
 
-  url 'http://nukesaq88.github.io/Pngyu/download/Pngyu_mac_101.zip'
+  url "http://nukesaq88.github.io/Pngyu/download/Pngyu_mac_#{version.gsub('.','')}.zip"
   homepage 'http://nukesaq88.github.io/Pngyu/'
   license :oss
 
