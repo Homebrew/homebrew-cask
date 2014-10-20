c8eb67b6308667aa53f8b4d1279dffd15359daef
Roland Walker <walker@pobox.com>
2014-10-20 11:11:49 -0400

    re-use version in rekordbox.rb

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -2,11 +2,11 @@ class Rekordbox < Cask
   version '3.0.1'
   sha256 '6a0b04fcf2a0fdb4893a3a6efc07e6988e385dce187f1c08803a2b8699f1d95e'
 
-  url 'http://rekordbox.com/_app/files/Install_rekordbox_3_0_1.pkg.zip'
+  url "http://rekordbox.com/_app/files/Install_rekordbox_#{version.gsub('.','_')}.pkg.zip"
   homepage 'http://rekordbox.com/en/'
   license :unknown
 
-  pkg 'Install_rekordbox_3_0_1.pkg'
-  uninstall :pkgutil => 'com.pioneer.rekordbox.3.*',
+  pkg "Install_rekordbox_#{version.gsub('.','_')}.pkg"
+  uninstall :pkgutil => 'com.pioneer.rekordbox.*',
             :delete  => '/Applications/rekordbox 3/'
 end
