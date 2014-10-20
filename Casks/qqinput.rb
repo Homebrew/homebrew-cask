bc67b5f691ed7191ed20c408e3f8e7269cfdcfb3
Roland Walker <walker@pobox.com>
2014-10-20 11:11:28 -0400

    re-use version in qqinput.rb
    and add UTF-8 encoding comment for older Rubies

--- Casks/qqinput.rb
+++ Casks/qqinput.rb
@@ -1,11 +1,12 @@
+# encoding: UTF-8
 class Qqinput < Cask
   version '2.8.86.400'
   sha256 '5b90cf604230013d5afa974b5b4835c0d6faf55da932cebce4f72478b5665a4d'
 
-  url 'http://qqime.cdn.sogou.com/QQInput_Mac_Setup_2_8_86_400.dmg'
+  url "http://qqime.cdn.sogou.com/QQInput_Mac_Setup_#{version.gsub('.','_')}.dmg"
   homepage 'http://qq.pinyin.cn/'
   license :unknown
 
-  pkg '安装QQ输入法2.8.pkg'
+  pkg "安装QQ输入法#{version.sub(%r{^(\d+\.\d+).*},'\1')}.pkg"
   uninstall :pkgutil => 'com.tencent.inputmethod.QQInput.*'
 end
