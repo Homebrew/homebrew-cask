5233d254ff5084cbf2fa6379147d7415afadc01b
Roland Walker <walker@pobox.com>
2014-10-20 11:15:59 -0400

    update qiyimedia to version 3.1.14
    and add UTF-8 comment for older Rubies

--- Casks/qiyimedia.rb
+++ Casks/qiyimedia.rb
@@ -1,6 +1,7 @@
+# encoding: UTF-8
 class Qiyimedia < Cask
-  version '2.6.0'
-  sha256 'b83ab73c2275d09056fdda7cb8f48e6f2956dc6b05e59e1b1683f600009cfa74'
+  version '3.1.14'
+  sha256 'bab6c18060eabb2ae3326c562de6e33041ab6a0476817ae7fc1f39d2265734b5'
 
   url 'http://static.qiyi.com/ext/common/QIYImedia_Mac_5.dmg'
   homepage 'http://www.iqiyi.com'
