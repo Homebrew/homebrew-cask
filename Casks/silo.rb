a6396696b7bd8be545f8599b375d27df1a693f29
Roland Walker <walker@pobox.com>
2014-10-20 11:11:57 -0400

    re-use version in silo.rb

--- Casks/silo.rb
+++ Casks/silo.rb
@@ -2,10 +2,10 @@ class Silo < Cask
   version '2.3.02'
   sha256 '7d84001382c3d12ad2f436fdb5572a60335a9e1dbac6505fd959fc776efaf365'
 
-  url 'https://nevercenter.com/download/Install_Silo_2_3_02_mac.zip'
+  url "https://nevercenter.com/download/Install_Silo_#{version.gsub('.','_')}_mac.zip"
   homepage 'http://nevercenter.com/silo/'
   license :unknown
-  container :nested => 'Install_Silo_2_3_02_mac.dmg'
+  container :nested => "Install_Silo_#{version.gsub('.','_')}_mac.dmg"
 
-  app 'Silo 2.3.app'
+  app "Silo #{version.gsub(%r{^(\d+\.\d+).*},'\1')}.app"
 end
