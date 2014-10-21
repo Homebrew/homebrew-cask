e2c24a01fe6adcecf2d84688a7207eaea9a175a1
Daan van Dijk <drvdijk@gmail.com>
2014-10-21 19:56:44 -0400

    1Password only v5.0.1 on Yosemite
    Version 5.x does not run on Mavericks or lower.
    Licence is commercial.

--- Casks/onepassword.rb
+++ Casks/onepassword.rb
@@ -1,10 +1,16 @@
 class Onepassword < Cask
-  version '5.0.1'
-  sha256 '359c69dadb9422caab64e3db34c8c5ef5e9355938e811873a57daaf74560605c'
+
+  if MacOS.version < :yosemite
+    version '4.4.3'
+    sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
+    app '1Password 4.app'
+  else
+    version '5.0.1'
+    sha256 '359c69dadb9422caab64e3db34c8c5ef5e9355938e811873a57daaf74560605c'
+    app '1Password 5.app'
+  end
 
   url "http://i.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
-  license :unknown
-
-  app '1Password 5.app'
+  license :commercial
 end
