bc8a8b51cd570d6285c8e6910bd00c0476a55353
Daniele Sluijters <daenney@users.noreply.github.com>
2014-10-21 19:44:02 +0200

    Cask/spectacle: upgrade to 0.8.6.
    Change the license to MIT as per @vmsp's suggestion, it's been there
    since the beginning:
    eczarny/spectacle@813e01ac420a13af4b62d1d0d632e81fd4479732

--- Casks/spectacle.rb
+++ Casks/spectacle.rb
@@ -1,12 +1,12 @@
 class Spectacle < Cask
-  version '0.8.5'
-  sha256 'f0d40b4c78c8b6509b8e42b8d8b03144b9ecb5ed6ff89d278a24ffd74bfe61b4'
+  version '0.8.6'
+  sha256 '0fa1eb4a5798d77ec1440ad96ad169af14dbb87fe73ca22268bf5b1e18024b29'
 
   url "https://s3.amazonaws.com/spectacle/downloads/Spectacle+#{version}.zip"
   appcast 'http://spectacleapp.com/updates/appcast.xml',
-          :sha256 => 'caa9f23c6ba36fc40f7fe35eb8cfb1a02ba4ed7eb2f62f8f137d01bab8de61dd'
+          :sha256 => '42e1e1a399593ca106bb9f23084ac3112ad46703a64af7cc425fa9964bf14159'
   homepage 'http://spectacleapp.com/'
-  license :unknown
+  license :mit
 
   app 'Spectacle.app'
   zap :delete => '~/Library/Preferences/com.divisiblebyzero.Spectacle.plist'
