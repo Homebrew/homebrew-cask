12cdc5270638d203443ddce429cffd7706c149ec
Felix Zandanel <felix@zandanel.me>
2014-10-21 14:10:10 +0200

    Edit Cocktail cask in order to use conditionals

--- Casks/cocktail.rb
+++ Casks/cocktail.rb
@@ -2,17 +2,25 @@ class Cocktail < Cask
   version :latest
   sha256 :no_check
 
-  url 'http://usa.maintain.se/CocktailYE.dmg'
-  appcast 'http://www.maintain.se/downloads/sparkle/yosemite/yosemite.xml'
-  homepage 'http://maintain.se/cocktail'
+  if MacOS.version == :snow_leopard
+    url 'http://usa.maintain.se/CocktailSLE.dmg'
+    appcast 'http://www.maintain.se/downloads/sparkle/snowleopard/snowleopard.xml'
+  elsif MacOS.version == :lion
+    url 'http://usa.maintain.se/CocktailLionEdition.dmg'
+    appcast 'http://www.maintain.se/downloads/sparkle/lion/lion.xml'
+  elsif MacOS.version == :mountain_lion
+    url 'http://usa.maintain.se/CocktailMLE.dmg'
+    appcast 'http://www.maintain.se/downloads/sparkle/mountainlion/mountainlion.xml'
+  elsif MacOS.version == :mavericks
+    url 'http://usa.maintain.se/CocktailME.dmg'
+    appcast 'http://www.maintain.se/downloads/sparkle/mavericks/mavericks.xml'
+  else
+    url 'http://usa.maintain.se/CocktailYE.dmg'
+    appcast 'http://www.maintain.se/downloads/sparkle/yosemite/yosemite.xml'
+  end
+
+  homepage 'http://www.maintain.se/cocktail'
   license :unknown
 
   app 'Cocktail.app'
-
-  # todo replace this with a conditional
-  caveats <<-EOS.undent
-    This version of Cocktail is for OS X Yosemite only. If you are using other versions of
-    OS X, please run 'brew tap caskroom/versions' and install cocktail-mountainlion /
-    cocktail-lion / cocktail-snowleopard / cocktail-mavericks
-  EOS
 end
