8e4cbdba9f01f8264cbf4140914211852b83a298
Roland Walker <walker@pobox.com>
2014-10-21 07:19:04 -0400

    add zap stanza for onepassword

--- Casks/onepassword.rb
+++ Casks/onepassword.rb
@@ -13,4 +13,11 @@ class Onepassword < Cask
   url "http://i.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
   license :commercial
+
+  zap :delete => [
+                  '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
+                  '~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
+                  '~/Library/Containers/com.agilebits.onepassword-osx',
+                  '~/Library/Group Containers/2BUA8C4S2C.com.agilebits',
+                 ]
 end
