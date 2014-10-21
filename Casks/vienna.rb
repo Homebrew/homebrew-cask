79f41f43568258bbafd5d4d9102ac46d4fd4bfdf
Keng Kiat Lim <kkin@crnrc.com>
2014-10-21 14:18:33 +0800

    Upgrade Vienna to 3.0.0 RC9

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -1,6 +1,6 @@
 class Vienna < Cask
-  version '3.0.0_rc8'
-  sha256 'e707769442e3064234bbd0450a22d28de0fa87ae194b1e6c5a423e555e58a5d1'
+  version '3.0.0_rc9'
+  sha256 '5d6c85ab3e59d581efe37aa0e7a035ebeae1688a4d7e283bb9eed72c8c2bb606'
 
   url "https://downloads.sourceforge.net/vienna-rss/Vienna#{version}.tgz"
   appcast 'http://vienna-rss.org/changelog_beta.xml'
