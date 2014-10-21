7fdcc6ad1605c8db3660207cf96ec08d4c55c0bf
Chris Rowe <mail@chrisrowe.net>
2014-10-21 08:24:15 +0100

    Update VirtualHostX to V6 (Yosemite)

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -2,8 +2,8 @@ class Virtualhostx < Cask
   version :latest
   sha256 :no_check
 
-  url 'https://clickontyler.com/virtualhostx/download/v5/'
-  appcast 'http://shine.clickontyler.com/appcast.php?id=23'
+  url 'https://clickontyler.com/virtualhostx/download/v6/'
+  appcast 'http://shine.clickontyler.com/appcast.php?id=30'
   homepage 'http://clickontyler.com/virtualhostx/'
   license :unknown
 
