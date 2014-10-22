40d8f9c0b36ad72acc53a34aa0679a51c2de49c4
Roland Walker <walker@pobox.com>
2014-10-22 10:35:42 -0400

    re-use version in music-manager

--- Casks/music-manager.rb
+++ Casks/music-manager.rb
@@ -2,7 +2,7 @@ class MusicManager < Cask
   version '1.0.104.6528'
   sha256 'a1e4e48e008958f9a725bfee1e2d8360dc8efad38ef2532fc1b3e4b9c3df8f0d'
 
-  url 'https://dl.google.com/dl/androidjumper/mac/1046528/musicmanager.dmg'
+  url "https://dl.google.com/dl/androidjumper/mac/#{version.sub(%r{^\d+\.\d+\.},'').gsub('.','')}/musicmanager.dmg"
   homepage 'https://play.google.com/music/'
   license :unknown
 
