d745228928ca0396dfb4b673f93d39af3ad5d722
Roland Walker <walker@pobox.com>
2014-10-22 10:35:42 -0400

    re-use version in inform

--- Casks/inform.rb
+++ Casks/inform.rb
@@ -2,7 +2,7 @@ class Inform < Cask
   version '7.6L02'
   sha256 '182134881d44b1c096af979d9cff195d33ec51d7c1f81f59a7c25d5a917d8987'
 
-  url 'http://inform7.com/download/content/6L02/I7-6L02-OSX.dmg'
+  url "http://inform7.com/download/content/6L02/I#{version.gsub('.','-')}-OSX.dmg"
   homepage 'http://inform7.com/'
   license :unknown
 
