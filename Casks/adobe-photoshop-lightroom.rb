01a71ee47542b2603eb289b9677a8f1d7cda8241
Roland Walker <walker@pobox.com>
2014-10-22 10:35:41 -0400

    re-use version + zap in adobe-photoshop-lightroom
    dial back zap stanza to only version-specific files

--- Casks/adobe-photoshop-lightroom.rb
+++ Casks/adobe-photoshop-lightroom.rb
@@ -2,20 +2,17 @@ class AdobePhotoshopLightroom < Cask
   version '5.6'
   sha256 '794fa6b364985a6e3c830f78c9ad9c52c46208676bf01576976cbcfc818aa61a'
 
-  url 'http://download.adobe.com/pub/adobe/lightroom/mac/5.x/Lightroom_5_LS11_mac_5_6.dmg'
+  url "http://download.adobe.com/pub/adobe/lightroom/mac/#{version.to_i}.x/Lightroom_#{version.to_i}_LS11_mac_#{version.gsub('.','_')}.dmg"
   homepage 'http://www.adobe.com/products/photoshop-lightroom.html'
   license :unknown
 
-  pkg 'Adobe Photoshop Lightroom 5.pkg'
+  pkg "Adobe Photoshop Lightroom #{version.to_i}.pkg"
 
-  uninstall :pkgutil => 'com.adobe.Lightroom5',
-            :quit => 'com.adobe.Lightroom5',
-            :delete => '/Applications/Adobe Photoshop Lightroom 5.app'
+  uninstall :pkgutil => "com.adobe.Lightroom#{version.to_i}",
+            :quit => "com.adobe.Lightroom#{version.to_i}",
+            :delete => "/Applications/Adobe Photoshop Lightroom #{version.to_i}.app"
   zap       :delete => [
                         '~/Library/Application Support/Adobe/Lightroom',
-                        '~/Library/Preferences/com.adobe.Lightroom2.plist',
-                        '~/Library/Preferences/com.adobe.Lightroom3.plist',
-                        '~/Library/Preferences/com.adobe.Lightroom4.plist',
-                        '~/Library/Preferences/com.adobe.Lightroom5.plist',
+                        "~/Library/Preferences/com.adobe.Lightroom#{version.to_i}.plist",
                        ]
 end
