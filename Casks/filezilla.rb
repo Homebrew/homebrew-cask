8cd417fec6a4e3a42a25715ed957efe2de176c4e
Benjamin Asbach <benjamin.asbach@gmail.com>
2014-10-23 00:10:13 +0200

    updated filezilla from version 3.9.0.5 to 3.9.0.6

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,6 +1,6 @@
 class Filezilla < Cask
-  version '3.9.0.5'
-  sha256 '805e7a83faa13235d15eeab572eab06d6033901a196ba3ebcf753793f81219cd'
+  version '3.9.0.6'
+  sha256 '9486be1f5dec41df8ef1b60893ca612e6791b8d98609c6b9e42ced977210d15b'
 
   url "https://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   homepage 'https://filezilla-project.org/'
