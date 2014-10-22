616af3a313a1a4400d0b4002790c9cb20d4296c6
Roland Walker <walker@pobox.com>
2014-10-22 10:35:42 -0400

    re-use version in texpad

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -2,7 +2,7 @@ class Texpad < Cask
   version '1.6.13'
   sha256 'e3fa71a11747d9698f72a8b9b6d5bcfca83f71ddec3dd69c3749362d47e85dcb'
 
-  url 'https://cloud.texpadapp.com/bundles/Texpad_1_6_13.zip'
+  url "https://cloud.texpadapp.com/bundles/Texpad_#{version.gsub('.','_')}.zip"
   appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml',
           :sha256 => 'a8522a4533df93fbf9d50489aa5f1c91bb01916faebd81ee20f56ea96dad8d97'
   homepage 'https://www.texpadapp.com/osx'
