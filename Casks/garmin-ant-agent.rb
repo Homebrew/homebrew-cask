a7cb45232a9ee8cd86602373d8d70537a1f8300d
Roland Walker <walker@pobox.com>
2014-10-22 10:35:42 -0400

    re-use version in garmin-ant-agent

--- Casks/garmin-ant-agent.rb
+++ Casks/garmin-ant-agent.rb
@@ -2,7 +2,7 @@ class GarminAntAgent < Cask
   version '2.2.2'
   sha256 '11df82fdd8d39a9d83df03c8efe8b1b1cb3e8f65d1e1bd47f823e9eda9bfcc57'
 
-  url 'http://download.garmin.com/software/ANTAgentforMac_222.dmg'
+  url "http://download.garmin.com/software/ANTAgentforMac_#{version.gsub('.','')}.dmg"
   homepage 'http://www8.garmin.com/support/download_details.jsp?id=4417'
   license :unknown
 
