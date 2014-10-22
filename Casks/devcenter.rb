5e220841c25a1e86fbf4978a97fa0382848bf3fe
Vítor Galvão <info@vitorgalvao.com>
2014-10-22 02:08:49 +0100

    corrected separation of stanzas in devcenter

--- Casks/devcenter.rb
+++ Casks/devcenter.rb
@@ -1,9 +1,8 @@
 class Devcenter < Cask
   version '1.2.0'
-
   sha256 '39820d3a7ec66ae3011cf72bee029d4f9817490319648837cc72b59d01a870f2'
-  url "http://downloads.datastax.com/devcenter/DevCenter-#{version}-macosx-x86_64.tar.gz"
 
+  url "http://downloads.datastax.com/devcenter/DevCenter-#{version}-macosx-x86_64.tar.gz"
   homepage 'http://www.datastax.com/what-we-offer/products-services/devcenter'
   license :gratis
 
