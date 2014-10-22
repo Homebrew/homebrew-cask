61c81952fd440309dc27585fc4b2e7b3732592ff
Roland Walker <walker@pobox.com>
2014-10-22 10:35:41 -0400

    re-use version in anypass

--- Casks/anypass.rb
+++ Casks/anypass.rb
@@ -2,7 +2,7 @@ class Anypass < Cask
   version '1.0.0'
   sha256 '364d108736eddfdb6e7db56430806d790b79e9f8561aedc544fc78d7e41bac54'
 
-  url 'http://icyblaze.com/anypass/anypass_mac_1.0.zip'
+  url "http://icyblaze.com/anypass/anypass_mac_#{version.sub(%r{^(\d+\.\d+).*},'\1')}.zip"
   homepage 'http://icyblaze.com/anypass'
   license :unknown
 
