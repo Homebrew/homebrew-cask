58d6db58b878dcca01f1cb812eac404a4a2e97a2
Roland Walker <walker@pobox.com>
2014-10-22 10:35:41 -0400

    re-use version in capsee

--- Casks/capsee.rb
+++ Casks/capsee.rb
@@ -2,10 +2,10 @@ class Capsee < Cask
   version '1.2'
   sha256 'e78cdfe435cca259e0111a2b2131ad3be7d5ba6160cf69c8e7cbcc033eac2fc4'
 
-  url 'http://www.threemagination.com/CapSee12.zip'
+  url "http://www.threemagination.com/CapSee#{version.gsub('.','')}.zip"
   homepage 'http://www.threemagination.com/capsee/'
   license :unknown
 
-  container :nested => 'CapSee12.dmg'
+  container :nested => "CapSee#{version.gsub('.','')}.dmg"
   app 'CapSee.app'
 end
