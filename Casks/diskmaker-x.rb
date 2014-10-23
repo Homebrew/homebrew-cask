77ff7f2a0e7182df85228c25ccca2455df701c47
Vítor Galvão <info@vitorgalvao.com>
2014-10-23 18:17:43 +0100

    fixed diskmakerx with new url, version, and making it easier to update

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -1,10 +1,10 @@
 class DiskmakerX < Cask
-  version :latest
-  sha256 :no_check
+  version '4b4'
+  sha256 '84c7c61973182573ba4d819f493528b665ad2c06a77698999f21ee2026b18a15'
 
-  url 'http://t.co/FZzVCGqOab'
+  url "http://diskmakerx.com/downloads/DiskMakerX#{version}.dmg"
   homepage 'http://diskmakerx.com/'
   license :unknown
 
-  app 'DiskMaker X 4b4.app'
+  app "DiskMaker X #{version}.app"
 end
