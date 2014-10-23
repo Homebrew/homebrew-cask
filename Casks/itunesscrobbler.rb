e24b40e89f2e7e8d8a64836326ac17cb42d55504
Roland Walker <walker@pobox.com>
2014-10-23 10:18:38 -0400

    re-use version in itunesscrobbler

--- Casks/itunesscrobbler.rb
+++ Casks/itunesscrobbler.rb
@@ -2,7 +2,7 @@ class Itunesscrobbler < Cask
   version '0.6.0'
   sha256 '01dcacb1738e824d853dab7825cee88b017f47a65f8e22fdbd7bf762802cc893'
 
-  url 'http://www.easyclasspage.de/lastfm/files/iTunesScrobbler_060.zip'
+  url "http://www.easyclasspage.de/lastfm/files/iTunesScrobbler_#{version.gsub('.','')}.zip"
   homepage 'http://www.easyclasspage.de/lastfm/seite-19.html'
   license :unknown
 
