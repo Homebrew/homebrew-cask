b55fa8e1adb25441008b85d3cb04f5fe94da24c1
Roland Walker <walker@pobox.com>
2014-10-23 10:18:38 -0400

    re-use version in cx

--- Casks/cx.rb
+++ Casks/cx.rb
@@ -2,7 +2,7 @@ class Cx < Cask
   version '1.0.3.6'
   sha256 'b78a6af89942e34d77638d98b86a321928c1eaa1d44604dabba4aa9e02366b12'
 
-  url 'https://www.cx.com/install/desktop/versions/cx-1-0-3-6.zip'
+  url "https://www.cx.com/install/desktop/versions/cx-#{version.gsub('.','-')}.zip"
   homepage 'http://www.cx.com'
   license :unknown
 
