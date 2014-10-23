20d09c9d41bef2e0b36bd235599b1f1d567ee8ee
George Song <george@55minutes.com>
2014-10-23 07:56:36 -0700

    Update download URL for Balsamiq Mockups
    Closes #6944.

--- Casks/balsamiq-mockups.rb
+++ Casks/balsamiq-mockups.rb
@@ -2,7 +2,8 @@ class BalsamiqMockups < Cask
   version :latest
   sha256 :no_check
 
-  url 'http://builds.balsamiq.com/b/mockups-desktop/MockupsForDesktop.dmg'
+  # amazonaws is the official download host per the vendor homepage
+  url 'http://s3.amazonaws.com/build_production/mockups-desktop/MockupsForDesktop.dmg'
   homepage 'http://balsamiq.com/'
   license :commercial
 
