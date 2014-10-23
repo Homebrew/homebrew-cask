509c3b231b64aeae64ef99925374903fcaf1b2a5
Roland Walker <walker@pobox.com>
2014-10-23 10:18:38 -0400

    re-use version in sococo

--- Casks/sococo.rb
+++ Casks/sococo.rb
@@ -2,7 +2,7 @@ class Sococo < Cask
   version '3.2.7'
   sha256 '6772f2c2ef5c7b046612de408b79a71ed0936041fb9286785af69d862f653d1f'
 
-  url 'http://download.sococo.com/10069/Sococo_3_2_7_10069.dmg'
+  url "http://download.sococo.com/10069/Sococo_#{version.gsub('.','_')}_10069.dmg"
   homepage 'http://www.sococo.com'
   license :unknown
 
