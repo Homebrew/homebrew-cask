357fcba3cfaa3b828b9ee253868dd443b5f6d119
George Song <george@55minutes.com>
2014-10-23 08:24:30 -0700

    Update Vuescan version number and checksums

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,12 +1,12 @@
 class Vuescan < Cask
-  version '9.4.37'
+  version '9.4.49'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '9f12d5a180a79c9d297913cdfb793382b512f25388de66f007cb621f4d69f461'
     url 'http://www.hamrick.com/files/vuex3294.dmg'
+    sha256 'ad85605fdde2f6bf3c7bfa6207e15af0b5a0b40acfc0b869a9bf2c5a24fbcc2d'
   else
-    sha256 'a6e82863f7d6fc34400f678fb4de3cf5af6cbde9a20693ff4bfb5d95ad59f398'
     url 'http://www.hamrick.com/files/vuex6494.dmg'
+    sha256 '0e87cdc0e8e6d26f57ed89046148d0ca8f0a561d4dc025e2240a6b2de4367475'
   end
 
   homepage 'http://www.hamrick.com'
