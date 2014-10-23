866483448bd5cd39d7c2c93ac03cd08a052d1b9b
Roland Walker <walker@pobox.com>
2014-10-23 10:18:38 -0400

    re-use version in iannix

--- Casks/iannix.rb
+++ Casks/iannix.rb
@@ -2,7 +2,7 @@ class Iannix < Cask
   version '0.9.16'
   sha256 'b45d332fc450342ad9a5b73fedcd554c299d435f989f0fe099522554434d00de'
 
-  url 'http://www.iannix.org/download/iannix_mac_64__0_9_16.dmg'
+  url "http://www.iannix.org/download/iannix_mac_64__#{version.gsub('.','_')}.dmg"
   homepage 'http://www.iannix.org/'
   license :unknown
 
