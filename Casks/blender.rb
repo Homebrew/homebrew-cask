ecc02f293da0fc4d67ddbed2e7f17224a19d2f54
Felix Zandanel <felix@zandanel.me>
2014-10-24 10:39:44 +0200

    Update blender to 2.72b

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,6 +1,6 @@
 class Blender < Cask
-  version '2.72a'
-  sha256 '8c6d9260ca2ab87cfb37a34f4225a05441497dbc274872e85624509a70fae3b0'
+  version '2.72b'
+  sha256 'b015f030c416d4aa09617f33e93bba3429624dbbb203fb4b6e728257e61b3d5c'
 
   url "http://download.blender.org/release/Blender2.72/blender-#{version}-OSX_10.6-x86_64.zip"
   homepage 'http://www.blender.org/'
