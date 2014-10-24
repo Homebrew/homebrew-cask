4b0d43bca967fdeb1a4b0be46875dd8fd9be0675
Herb <i@4leaf.me>
2014-10-25 00:53:19 +0800

    Update fastscripts to 2.6.8

--- Casks/fastscripts.rb
+++ Casks/fastscripts.rb
@@ -1,6 +1,6 @@
 class Fastscripts < Cask
-  version '2.6.7'
-  sha256 '2bbd44582216f7c9961dbd5e6bc9bc78023da20d1dab4a5c499ea5883d9dd968'
+  version '2.6.8'
+  sha256 '7cc170ecacad63872fe3faddd8dbaaa8f0e232687dbd8365594221ff64999def'
 
   url "https://www.red-sweater.com/fastscripts/FastScripts#{version}.zip"
   appcast 'http://www.red-sweater.com/fastscripts/appcast2.php'
