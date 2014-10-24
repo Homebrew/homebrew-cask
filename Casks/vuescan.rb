e2ace8b71ec7b9190271d2f26a4b51fc37ca7d7e
Vítor Galvão <info@vitorgalvao.com>
2014-10-24 03:59:38 +0100

    moved sha256 above url in vuescan

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -2,11 +2,11 @@ class Vuescan < Cask
   version '9.4.49'
 
   if Hardware::CPU.is_32_bit?
-    url 'http://www.hamrick.com/files/vuex3294.dmg'
     sha256 'ad85605fdde2f6bf3c7bfa6207e15af0b5a0b40acfc0b869a9bf2c5a24fbcc2d'
+    url 'http://www.hamrick.com/files/vuex3294.dmg'
   else
-    url 'http://www.hamrick.com/files/vuex6494.dmg'
     sha256 '0e87cdc0e8e6d26f57ed89046148d0ca8f0a561d4dc025e2240a6b2de4367475'
+    url 'http://www.hamrick.com/files/vuex6494.dmg'
   end
 
   homepage 'http://www.hamrick.com'
