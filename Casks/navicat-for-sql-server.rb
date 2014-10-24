76f208b752ae16a69e6d43f190568771862f796a
Roland Walker <walker@pobox.com>
2014-10-24 10:33:29 -0400

    update navicat-for-sql-server to 11.1.5
    * reuse version
    * set license

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,10 +1,10 @@
 class NavicatForSqlServer < Cask
-  version '11.0.20'
-  sha256 '9e3be372e9bc0dea7c20096c8a6b66a67b0c8652e87c27654e4d89028632b195'
+  version '11.1.5'
+  sha256 '6f7273798585129ba58a0d8f5eb8cb724f3f2dfd3f977ac408cd801c5be28bfc'
 
-  url 'http://download.navicat.com/download/navicat110_sqlserver_en.dmg'
+  url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_sqlserver_en.dmg"
   homepage 'http://www.navicat.com/products/navicat-for-sqlserver'
-  license :unknown
+  license :commercial
 
   app 'Navicat For SQL Server.app'
 end
