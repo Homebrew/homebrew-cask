6d815bd388c5a79cdc998e16668c611f428b8d23
Alberth Matos <amatos@MacBook-Pro.home.matos.cc>
2014-10-23 21:15:01 -0400

    Updated bbedit to 11.0
    Updated bbedit download URL to current version, 11.0

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -3,7 +3,8 @@ class Bbedit < Cask
   sha256 '3c15fef50245e93073e73136bb7097faf1f03a061559eed84a17cb8678fdf033'
 
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
-  appcast 'https://versioncheck.barebones.com/BBEdit.xml'
+  appcast 'https://versioncheck.barebones.com/BBEdit.xml',
+          :sha256 => '4ce2f9c44758a42b4c9bad5be5311861a23492ec41974fdb05f6e0ba5db33e65'
   homepage 'http://www.barebones.com/products/bbedit/'
   license :commercial
 
