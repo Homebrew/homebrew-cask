1a58dd19be3517fe302338c4c526a8c429bf3dd0
Roland Walker <walker@pobox.com>
2014-10-24 10:02:14 -0400

    update praat.rb to version 5.4.00
    * update version/checksums
    * re-use `version` value
    * define `license`

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,16 +1,16 @@
 class Praat < Cask
-  version '5.3.82'
+  version '5.4.00'
 
   if Hardware::CPU.is_32_bit?
-    sha256 'c1ef06ed8fd9e36d5eb96699daf5fba3e04e2c32edcda0c2981d8f817c019371'
-    url 'http://www.fon.hum.uva.nl/praat/praat5382_mac32.dmg'
+    sha256 '63a28727f280096cb8b72dbb2fc3c38a0ce8279d095dd6a14ffa217349f2d5e8'
+    url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac32.dmg"
   else
-    sha256 'c02aa1ae0e896325cbc8a917e5cfef37a0f6e38e78d2b71757134158f7e563f3'
-    url 'http://www.fon.hum.uva.nl/praat/praat5382_mac64.dmg'
+    sha256 'd694ce3045ad70a600fa621f9dccb9f006b75703b28bdd5c16378843bb8cae8b'
+    url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac64.dmg"
   end
 
   homepage 'http://www.fon.hum.uva.nl/praat/'
-  license :unknown
+  license :gpl
 
   app 'Praat.app'
 end
