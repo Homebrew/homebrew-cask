c0b87f7021a67577e87c52fce5113d28758ea59f
Roland Walker <walker@pobox.com>
2014-10-25 11:52:19 -0400

    re-use version in pastor

--- Casks/pastor.rb
+++ Casks/pastor.rb
@@ -2,7 +2,7 @@ class Pastor < Cask
   version '1.8.2'
   sha256 'ac41e7f300d722fcd9733a4f13a984d211b20f95c94db4dc07ab537fd45f8370'
 
-  url 'https://mehlau.net/pastor/Pastor182-signed.dmg'
+  url "https://mehlau.net/pastor/Pastor#{version.gsub('.','')}-signed.dmg"
   homepage 'http://mehlau.net/pastor'
   license :unknown
 
