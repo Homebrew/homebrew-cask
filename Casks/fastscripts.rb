cd99e6aab7a6f329d20ec3ef8c5210cbb3748ca3
Roland Walker <walker@pobox.com>
2014-10-25 11:09:05 -0400

    add appcast checksum in fastscripts

--- Casks/fastscripts.rb
+++ Casks/fastscripts.rb
@@ -3,7 +3,8 @@ class Fastscripts < Cask
   sha256 '7cc170ecacad63872fe3faddd8dbaaa8f0e232687dbd8365594221ff64999def'
 
   url "https://www.red-sweater.com/fastscripts/FastScripts#{version}.zip"
-  appcast 'http://www.red-sweater.com/fastscripts/appcast2.php'
+  appcast 'http://www.red-sweater.com/fastscripts/appcast2.php',
+          :sha256 => '500cd40fd4f6a40f76d8329d7246f79414f4d5aeba5bf605be18c16a6de657f5'
   homepage 'http://www.red-sweater.com/fastscripts/'
   license :unknown
 
