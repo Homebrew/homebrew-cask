9d5faad732142489e86eb1e13f08f477842e77fe
Roland Walker <walker@pobox.com>
2014-10-25 11:52:19 -0400

    re-use version in fontexplorer-x-pro

--- Casks/fontexplorer-x-pro.rb
+++ Casks/fontexplorer-x-pro.rb
@@ -2,7 +2,7 @@ class FontexplorerXPro < Cask
   version '4.0.2'
   sha256 '6e7d2bfb89e65e78a8551db705ffca86ec6a25371388ad2124b1cbf956f48472'
 
-  url 'http://fex.linotype.com/download/mac/FontExplorerXPro402.dmg'
+  url "http://fex.linotype.com/download/mac/FontExplorerXPro#{version.gsub('.','')}.dmg"
   homepage 'http://www.fontexplorerx.com/'
   license :unknown
 
