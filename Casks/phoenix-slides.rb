52f5d066c4260b978236a5bf0054edfdc61260f5
Roland Walker <walker@pobox.com>
2014-10-25 11:52:19 -0400

    re-use version in phoenix-slides

--- Casks/phoenix-slides.rb
+++ Casks/phoenix-slides.rb
@@ -2,7 +2,7 @@ class PhoenixSlides < Cask
   version '1.3.1'
   sha256 '6e5df9e586bb33b79249d9917f99f50395f255b85bf39c5fafca64fe66aef758'
 
-  url 'http://blyt.net/phxslides/phoenix-slides-131.zip'
+  url "http://blyt.net/phxslides/phoenix-slides-#{version.gsub('.','')}.zip"
   homepage 'http://blyt.net/phxslides'
   license :unknown
 
