7f98d0c2de1b5f73260f3c896b9a6ba078386442
billytrend <billy.trend@gmail.com>
2014-10-27 18:45:37 +0000

    Fix checksum in controlplane.app cask

--- Casks/controlplane.rb
+++ Casks/controlplane.rb
@@ -1,6 +1,6 @@
 class Controlplane < Cask
   version '1.5.4'
-  sha256 'e2e151797eb356c43c9a07fa39df375c87fa8671541cad0895c696fd6026c5eb'
+  sha256 'd35f24b8559a0554b501bc6ff6a0b5b8b4f3ab3c123db1464d5720df9359152b'
 
   url "http://www.controlplaneapp.com/download/#{version}"
   appcast 'http://www.controlplaneapp.com/appcast.xml',
