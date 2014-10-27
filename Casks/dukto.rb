d4aac5512ac6b66912ec04de0b8bdcbc2fce9baa
Roland Walker <walker@pobox.com>
2014-10-27 10:43:03 -0400

    re-use version in dukto

--- Casks/dukto.rb
+++ Casks/dukto.rb
@@ -1,8 +1,8 @@
 class Dukto < Cask
-  version 'r6'
+  version 'R6'
   sha256 '86f06ca92a28b5835cb3eaadfb09897f581a5da3fb8ee8246897b1bf4ee7348a'
 
-  url 'https://dukto.googlecode.com/files/DuktoR6-OSX.dmg'
+  url "https://dukto.googlecode.com/files/Dukto#{version}-OSX.dmg"
   homepage 'https://code.google.com/p/dukto/'
   license :oss
 
