2e98a4d42c313235efeaf01b44b7111b967638d5
Steve Vandervalk <stevenvandervalk@users.noreply.github.com>
2014-10-27 13:07:44 +1000

    Update hr.rb
    The URL was changed as well... /download/ instead of /releases/
    
    Apologies, forgot to test until now.

--- Casks/hr.rb
+++ Casks/hr.rb
@@ -2,7 +2,7 @@ class Hr < Cask
   version '0.43'
   sha256 'd4bba32b712fa456165b13c1a934fbd4da4dbd7391546756c7f92deabcacbd04'
 
-  url "http://www.hrmacapp.com/releases/hr_#{version.gsub('.', '')}.zip"
+  url "http://www.hrmacapp.com/download/hr_#{version.gsub('.', '')}.zip"
   homepage 'http://www.hrmacapp.com/'
   license :unknown
 
