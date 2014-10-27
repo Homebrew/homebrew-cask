aab34ea7d88f66a273a27562fe82c123d20ba1c6
Roland Walker <walker@pobox.com>
2014-10-27 10:43:03 -0400

    re-use version in cocoapacketanalyzer

--- Casks/cocoapacketanalyzer.rb
+++ Casks/cocoapacketanalyzer.rb
@@ -2,7 +2,7 @@ class Cocoapacketanalyzer < Cask
   version '1.31'
   sha256 'fc3586868fcf90e3ca766c31dbf184de78e5c907210dfdabbd13a831712fc864'
 
-  url 'http://tastycocoabytes.com/_downloads/CPA_131.dmg'
+  url "http://tastycocoabytes.com/_downloads/CPA_#{version.gsub('.','')}.dmg"
   appcast 'http://www.tastycocoabytes.com/cpa/updates/appcasting.php',
           :sha256 => 'e5471a829511ba4c4dd1cbf89bc2ea091fe5f27cc5eaae19fecffc94f8384f8a'
   homepage 'http://www.tastycocoabytes.com/cpa/'
