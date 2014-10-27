ef9d1be55b1847a5392bb6b1f8bd5c97b1eb80fb
Roland Walker <walker@pobox.com>
2014-10-27 10:43:04 -0400

    re-use version in toad

--- Casks/toad.rb
+++ Casks/toad.rb
@@ -2,7 +2,7 @@ class Toad < Cask
   version '1.0.8'
   sha256 'd02861e2319197730b16d112134f46f75b263d79ba6be29f6d9e33837c031fb9'
 
-  url 'http://community-downloads.quest.com/toadsoft/toadmacedition/ToadMacEdition_108.pkg'
+  url "http://community-downloads.quest.com/toadsoft/toadmacedition/ToadMacEdition_#{version.gsub('.','')}.pkg"
   homepage 'http://www.toadworld.com/products/toad-mac-edition/default.aspx'
   license :unknown
 
