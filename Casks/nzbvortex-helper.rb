9ec0197ed8c7c7612309708aea97702b061b911e
elseym <elseym@me.com>
2014-10-27 14:46:51 +0100

    Added uninstall stanza to NZBVortex Helper

--- Casks/nzbvortex-helper.rb
+++ Casks/nzbvortex-helper.rb
@@ -7,4 +7,11 @@ class NzbvortexHelper < Cask
   license :unknown
 
   installer :manual => 'NZBVortex Helper Installer.app'
+  
+  uninstall :quit => 'com.NZBVortex.NZBVortex-Helper',
+            :delete => [
+              '~/Library/Application Support/NZBVortex Helper',
+              '~/Library/Caches/com.NZBVortex.NZBVortex-Helper',
+              '~/Library/Preferences/com.NZBVortex.NZBVortex-Helper.plist',
+            ]
 end
