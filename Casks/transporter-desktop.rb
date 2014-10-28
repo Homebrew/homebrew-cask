a9c518ab6b3cf5c071013c5676e4d99d543ef7b1
Vítor Galvão <info@vitorgalvao.com>
2014-10-28 13:27:15 +0000

    Included more in transporter-desktop’s version

--- Casks/transporter-desktop.rb
+++ Casks/transporter-desktop.rb
@@ -1,8 +1,8 @@
 class TransporterDesktop < Cask
-  version '2.8.1'
+  version '2.8.1_16078'
   sha256 '43d0f47993d233fee6209b040b77f42e1b909cfedcbe873b7fb4700782af4c75'
 
-  url "https://secure.connecteddata.com/mac/2.5/software/Transporter_Desktop_#{version}_16078.dmg"
+  url "https://secure.connecteddata.com/mac/2.5/software/Transporter_Desktop_#{version}.dmg"
   homepage 'http://www.filetransporter.com/'
   license :unknown
 
