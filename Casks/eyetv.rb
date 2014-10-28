08143aa800e141a930c6bb3d8100bd86d3ae02c6
Roland Walker <walker@pobox.com>
2014-10-28 10:18:18 -0400

    re-use version in eyetv

--- Casks/eyetv.rb
+++ Casks/eyetv.rb
@@ -2,7 +2,7 @@ class Eyetv < Cask
   version '3.6.5'
   sha256 'cc8d7b1db43eabe827bab3a2401be97c6687cf83806ff585e3a125768c8c05f7'
 
-  url 'http://files.elgato.com/eyetvdownloads/support/eyetv_365_7310.dmg'
+  url "http://files.elgato.com/eyetvdownloads/support/eyetv_#{version.gsub('.','')}_7310.dmg"
   homepage 'http://www.elgato.com/'
   license :unknown
 
