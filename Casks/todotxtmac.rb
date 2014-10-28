a6e8cad7be435819ae9eefbeaeaf38ec1a0438ce
Vítor Galvão <info@vitorgalvao.com>
2014-10-28 02:50:18 +0000

    todotxtmac: facilitated updates

--- Casks/todotxtmac.rb
+++ Casks/todotxtmac.rb
@@ -2,7 +2,7 @@ class Todotxtmac < Cask
   version '1.2.4'
   sha256 '368010bec057387bdc6ee9cef4646f90d8eb1af8e1b4ec1666045e7e2166db4a'
 
-  url 'https://github.com/mjdescy/TodoTxtMac/releases/download/1.2.4/TodoTxtMac.app.zip'
+  url "https://github.com/mjdescy/TodoTxtMac/releases/download/#{version}/TodoTxtMac.app.zip"
   homepage 'https://mjdescy.github.io/TodoTxtMac/'
   license :mit
 
