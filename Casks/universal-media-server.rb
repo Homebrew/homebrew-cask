e99ee4ebdf9620612cdd64f22f2aa11aec2cdfc7
Mohammed Tangestani <tangestani@gmail.com>
2014-10-28 19:31:02 -0400

    Upgrade Universal Media Server to v4.2.0

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,6 +1,6 @@
 class UniversalMediaServer < Cask
-  version '4.1.1'
-  sha256 'e7e21dd12cb3884f1cdc2398f6645a583cdb893761da0172dd90b5d2e1656bc9'
+  version '4.2.0'
+  sha256 '07f5aa01f9cc57db4501034495471c70151537356ad01a31daf30d0aabb69bdf'
 
   url "http://downloads.sourceforge.net/sourceforge/unimediaserver/Official%20Releases/OS%20X/UMS-#{version}-Java8.dmg"
   homepage 'www.universalmediaserver.com'
