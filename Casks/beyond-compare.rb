977512fb94422a28451f5b67525a39013192fa2d
Brandon McCraw <bmccraw@Brandons-MacBook-Pro.local>
2014-10-27 20:57:13 -0500

    Add Beyond-Compare.app v4.0.0

---
+++ Casks/beyond-compare.rb
@@ -0,0 +1,10 @@
+class BeyondCompare < Cask
+  version '4.0.0'
+  sha256 'cb9987b62ac68a2493b7bc5678125fbf1b8919796bc82718ddd23958768fd457'
+
+  url 'http://www.scootersoftware.com/BCompareOSX-4.0.0.18847.zip'
+  homepage 'http://www.scootersoftware.com'
+  license :commercial
+
+  app 'Beyond Compare.app'
+end
