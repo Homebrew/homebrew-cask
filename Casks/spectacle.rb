220b43de2974d6ba6912f334e377e73e4e19a39c
Beng Hee Eu <beng@benghee.eu>
2014-10-29 00:39:08 -0400

    Spectacle 0.8.7

--- Casks/spectacle.rb
+++ Casks/spectacle.rb
@@ -1,10 +1,10 @@
 class Spectacle < Cask
-  version '0.8.6'
-  sha256 '3e367d2d7e6fe7d5f41d717d49cb087ba7432624b71ddd91c0cfa9d5a5459b7c'
+  version '0.8.7'
+  sha256 'a13063aca1689aff86f9c865930193498a5c469cd6f323f5ab077043884c494a'
 
   url "https://s3.amazonaws.com/spectacle/downloads/Spectacle+#{version}.zip"
   appcast 'http://spectacleapp.com/updates/appcast.xml',
-          :sha256 => '42e1e1a399593ca106bb9f23084ac3112ad46703a64af7cc425fa9964bf14159'
+          :sha256 => '5d75e2e07886ca135916e224b4b5c1468d9af1ea8ef355db33b28bff511fa6b2'
   homepage 'http://spectacleapp.com/'
   license :mit
 
