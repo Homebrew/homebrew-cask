1639f1b61d82de33a5a50ae7f38f3d9a61bf854f
Andrew Minion <andrew@andrewrminion.com>
2014-10-29 21:40:40 -0400

    Add Tag by Feisty Dog

---
+++ Casks/feisty-dog-tag.rb
@@ -0,0 +1,10 @@
+class FeistyDogTag < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'https://s3.amazonaws.com/feisty-dog/Tag.zip'
+  homepage 'http://www.feisty-dog.com/tag/'
+  license :commercial
+
+  app 'Tag.app'
+end
