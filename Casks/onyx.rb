3c7b0b718647a82e4760401434e7779e4bb553a6
James Moon <mail@remailed.net>
2014-10-31 14:28:21 -0400

    Add Mavericks conditional and new default conditional for onyx

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -8,8 +8,10 @@ class Onyx < Cask
     url 'http://www.titanium.free.fr/download/107/OnyX.dmg'
   elsif MacOS.version == :mountain_lion
     url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
-  else
+  elsif MacOS.version == :mavericks
     url 'http://joel.barriere.pagesperso-orange.fr/dl/109/OnyX.dmg'
+  else
+    url 'http://joel.barriere.pagesperso-orange.fr/dl/1010/OnyX.dmg'
   end
   homepage 'http://www.titanium.free.fr/downloadonyx.php'
   license :unknown
