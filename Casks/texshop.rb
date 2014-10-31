b83b923f2f3e256cdf3075af47ea9734e4dc884c
Keng Kiat Lim <kkin@crnrc.com>
2014-11-01 04:09:32 +0800

    Upgrade TexShop to 3.44

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,6 +1,6 @@
 class Texshop < Cask
-  version '3.43'
-  sha256 '81cc938d3d2c3b5ac91554127a398357b8844ec41f7a2ba3e5614cf19a4ca4a5'
+  version '3.44'
+  sha256 '8cfd2d5edf2a30b1744c04ae29bf44674113ab35a1ec6cb438de47b00b298c4e'
 
   url "http://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.gsub('.','')}.zip"
   appcast 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml',
