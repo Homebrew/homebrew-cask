bb77c8f692265d39f7f30d7fb65a03c644dfa3e8
FinalDes <alex@Alexs-Mac.local>
2014-10-31 16:58:46 +0800

    update itools version and hash

--- Casks/itools.rb
+++ Casks/itools.rb
@@ -1,6 +1,6 @@
 class Itools < Cask
-  version '2.4.1'
-  sha256 '9991e75df60588280837e62b3704b23c5bb47076b45660921e439ac7e7e16d24'
+  version '2.4.2'
+  sha256 'd927a4e6d4f96b1641e48ce09c083d7cba92ba00694eb96a9907bd1c4a7b2a9e'
 
   url "http://dl2.itools.hk/dl/iTools_#{version}.dmg"
   homepage 'http://pro.itools.cn/mac'
