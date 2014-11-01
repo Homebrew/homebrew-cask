4d291b4c755f21a535ddb0f6bf98a2a51c858449
Kensuke Hoshikawa <kensuke@Kensuke-no-MacBook-Pro.local>
2014-11-01 23:20:35 +0900

    Add Gifzo.app v1.0.2

---
+++ Casks/gifzo.rb
@@ -0,0 +1,10 @@
+class Gifzo < Cask
+  version '1.0.2'
+  sha256 'c4a521ad9fdbebda8efad5aadfd2cf43b8ac31c778795eba67ebbf8928a12f19'
+
+  url 'http://gifzo.net/Gifzo.zip'
+  homepage 'http://gifzo.net'
+  license :unknown
+
+  app 'Gifzo.app'
+end
