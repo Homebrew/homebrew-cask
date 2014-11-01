0a4feac4faae4a6402b02baf34760d5ca1d111ef
Federico Bond <federicobond@gmail.com>
2014-11-01 04:47:36 -0300

    Update RStudio to 0.98.1087

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -1,6 +1,6 @@
 class Rstudio < Cask
-  version '0.98.1073'
-  sha256 '115d0e7670d4da08fa98446a1c12d4d25bb5748b42f483677e8be6c477a79af1'
+  version '0.98.1087'
+  sha256 'c7832ad0c1ec0fc4ed7e0617cca8a840a413c0e5eccc50734b27cb4f76ae8a4d'
 
   url "http://download1.rstudio.org/RStudio-#{version}.dmg"
   homepage 'http://www.rstudio.com/'
