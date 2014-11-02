3a3da15a3aa76030d1c0e80e8490b76f0bc252bf
Federico Bond <federicobond@gmail.com>
2014-11-02 16:47:49 -0300

    Use postflight DSL for plist in rubymine

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -9,7 +9,7 @@ cask :v1 => 'rubymine' do
   app 'RubyMine.app'
 
   postflight do
-    system '/usr/libexec/PlistBuddy', '-c', 'Set :JVMOptions:JVMVersion 1.6+', "#{staged_path}/RubyMine.app/Contents/Info.plist"
+    plist_set(':JVMOptions:JVMVersion', '1.6+')
   end
 
   zap :delete => [
