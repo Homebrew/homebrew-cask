109dfd0f351a6b0fee06ace378f69206599ec539
Geoffrey Oxberry <oxberry1@llnl.gov>
2014-11-03 12:18:43 -0800

    Upgrade Zotero.app to v4.0.23

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 class Zotero < Cask
-  version '4.0.22'
-  sha256 'add2d7541f610533f37f4a29fd999d0973c5587696359c864410ee3de60aa9b4'
+  version '4.0.23'
+  sha256 'f97ef96f005674ad8227ae6eb2096c81d334b771347c566c3581bee5100675e3'
 
   url "https://download.zotero.org/standalone/#{version}/Zotero-#{version}.dmg"
   homepage 'http://www.zotero.org/'
