053cff971ff68e7a4db66a870bde3d9dbb170945
adyton <adyton@mmer.net>
2014-11-03 10:36:30 +0530

    New Application AdwareMedic
    AdwareMedic

---
+++ Casks/adwaremedic.rb
@@ -0,0 +1,10 @@
+class Adwaremedic < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.adwaremedic.com/AdwareMedic.dmg'
+  homepage 'http://www.adwaremedic.com/'
+  license :unknown
+
+  app 'AdwareMedic.app'
+end
