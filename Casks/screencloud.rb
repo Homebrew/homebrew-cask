498b876461f7b4ffcb4c60058b33f036d5d28bb6
Todor Evtimov <todor.ev@gmail.com>
2014-11-05 00:06:54 +0200

    Create Screencloud.rb cask

---
+++ Casks/screencloud.rb
@@ -0,0 +1,10 @@
+class Screencloud < Cask
+  version '1.1.6'
+  sha256 :no_check
+
+  url 'http://sourceforge.net/projects/screencloud/files/1.1.6/mac/ScreenCloud-1.1.6.dmg/download'
+  homepage 'https://screencloud.net/'
+  license :unknown
+
+  app 'ScreenCloud.app'
+end
