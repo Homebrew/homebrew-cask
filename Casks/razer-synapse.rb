01c2ff838a97ba683baa1b5b29c16dce374d14d9
Morgan Estes <morgan.estes@gmail.com>
2014-11-04 14:56:24 -0600

    Updated Razer Synapse to 1.29.

--- Casks/razer-synapse.rb
+++ Casks/razer-synapse.rb
@@ -1,9 +1,9 @@
 class RazerSynapse < Cask
-  version '1.26'
-  sha256 '4b82742d34e9bdce82c405958cdb45b6f67b9ad778e0ec622b947e8c2c57d10e'
+  version '1.29'
+  sha256 'c37266bb55df1f0344d797419ff10e498bbbb90cdf15b769487e245728f2eb43'
 
   url "http://dl.razerzone.com/drivers/Synapse2/mac/Razer_Synapse_2.0_Mac_Driver_v#{version}.dmg"
-  homepage 'http://www.razerzone.com/synapse2'
+  homepage 'http://www.razerzone.com/synapse/'
   license :unknown
 
   pkg 'Razer Synapse 2.0.pkg'
