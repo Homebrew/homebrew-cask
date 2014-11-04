f2c0f329bdc27f89c3d482ac391749cfb710b5b1
Michael Ford <fanquake@users.noreply.github.com>
2014-11-04 20:51:12 +0800

    Armory 0.92.3
    Correct license

--- Casks/armory.rb
+++ Casks/armory.rb
@@ -1,10 +1,10 @@
 class Armory < Cask
-  version '0.92.1'
+  version '0.92.3'
   sha256 '75d6b7566d6c40a93f7dcf995dfb778b577d0b4698cc52d1ecea8f6fadfdf460'
 
   url "https://s3.amazonaws.com/bitcoinarmory-releases/armory_#{version}_osx.tar.gz"
   homepage 'https://bitcoinarmory.com/'
-  license :unknown
+  license :affero
 
   app 'Armory.app'
 end
