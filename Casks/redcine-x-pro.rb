608b1aec9c7a61ae15c87e748762dbb305a9ee84
extrinsicmedia <miles@finvfx.com>
2014-11-09 12:32:34 -0800

    Fix `version` syntax for RedcineXPro

--- Casks/redcine-x-pro.rb
+++ Casks/redcine-x-pro.rb
@@ -1,5 +1,5 @@
 class RedcineXPro < Cask
-  version :31.0.0
+  version '31.0.0'
   sha256 '9393f84d839214ef4b692434ced8f4f9ecd12f8f49d8230d84c1bef9fe44d4db'
 
   url 'http://s3.amazonaws.com/red_3/downloads/software/rcx/REDCINE-X_PRO_Build_31_OSX.zip'
