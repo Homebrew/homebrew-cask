7d9c8b1e52ad08e4979688493b4d95127ac4b2a4
Stephen Rushe <srushe@shopkeep.com>
2014-11-10 09:21:57 +0000

    Update Trailer.app to 1.1.3
    Also, update links to http://ptsochantaris.github.io/
                     from http://dev.housetrip.com/trailer/

--- Casks/trailer.rb
+++ Casks/trailer.rb
@@ -1,11 +1,11 @@
 class Trailer < Cask
-  version '1.1.2'
-  sha256 'b3c19fa0f15176b1b4244a0572d9488618854de54889b5d40bbb36e73692b357'
+  version '1.1.3'
+  sha256 '5f59c927658ea1adfde83b5214438b485117f32024b8f73eaa4da7538d2bc95f'
 
-  url "http://dev.housetrip.com/trailer/trailer#{version.gsub('.','')}.zip"
-  appcast 'http://dev.housetrip.com/trailer/appcast.xml',
-          :sha256 => 'e4f8d3bf70078922985b48e9f9b8f291c583b14a214abc6789d2467b8afc2957'
-  homepage 'http://dev.housetrip.com/trailer/'
+  url "http://ptsochantaris.github.io/trailer/#{version.gsub('.','')}.zip"
+  appcast 'http://ptsochantaris.github.io/trailer/appcast.xml',
+          :sha256 => '287e8eb967e9cfb3e98cc21e927406423cdbed3c147e24ce45701c07b6a2f252'
+  homepage 'http://ptsochantaris.github.io/trailer/'
   license :mit
 
   app 'Trailer.app'
