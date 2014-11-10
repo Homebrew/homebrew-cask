fbd318401aea777eeeccc28ea1416472bf681bdf
extrinsicmedia <miles@finvfx.com>
2014-11-10 12:12:33 -0800

    Fix `version` and `url`

--- Casks/avidcodecsle.rb
+++ Casks/avidcodecsle.rb
@@ -1,8 +1,8 @@
 class Avidcodecsle < Cask
-  version '2_3_7'
+  version '2.3.7'
   sha256 '0a3e9b24e1af3ea0e01167a944c08b086a6818eacb38bb637bd0dbe85486323b'
 
-  url "http://resources.avid.com/supportfiles/attach/AvidCodecsLE_#{version.to_s}/Mac/AvidCodecsLE.pkg.zip"
+  url "http://resources.avid.com/supportfiles/attach/AvidCodecsLE_#{version.gsub('.','_')}/Mac/AvidCodecsLE.pkg.zip"
   homepage 'http://www.avid.com/US/industries/workflow/DNxHD-Codec'
   license :commercial
 
