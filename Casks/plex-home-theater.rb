cf0748a85f66c871c68a2fd2b8bc668256314cbe
James Ottaway <git@james.ottaway.io>
2014-11-11 11:02:19 +1000

    Download all things Plex from HTTPS URLs

--- Casks/plex-home-theater.rb
+++ Casks/plex-home-theater.rb
@@ -3,10 +3,10 @@ class PlexHomeTheater < Cask
 
   if Hardware::CPU.is_32_bit?
     sha256 '7c2fd9541104346478fd7fa1e34c609bc6494202e82dcb91533b1e3931add232'
-    url "http://downloads.plex.tv/plex-home-theater/#{version}/PlexHomeTheater-#{version}-macosx-i386.zip"
+    url "https://downloads.plex.tv/plex-home-theater/#{version}/PlexHomeTheater-#{version}-macosx-i386.zip"
   else
     sha256 '329011165014f20110c258049ab5de6c84a2957065e7dd01dfa68599ae9d4810'
-    url "http://downloads.plex.tv/plex-home-theater/#{version}/PlexHomeTheater-#{version}-macosx-x86_64.zip"
+    url "https://downloads.plex.tv/plex-home-theater/#{version}/PlexHomeTheater-#{version}-macosx-x86_64.zip"
   end
 
   homepage 'https://plex.tv'
