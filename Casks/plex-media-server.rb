3aff18a90615cdb06d7c0c8a1c83a28f9db0bf6b
James Ottaway <git@james.ottaway.io>
2014-11-11 10:59:57 +1000

    Update the Plex URL to use the new `plex.tv`

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -2,7 +2,7 @@ class PlexMediaServer < Cask
   version '0.9.9.14.531-7eef8c6'
   sha256 '7cd91e89f74569409b12f8151d66c731ca8658e6df61aaaac41accf6e6cba337'
 
-  url "http://downloads.plexapp.com/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
+  url "http://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   homepage 'https://plex.tv/'
   license :unknown
 
