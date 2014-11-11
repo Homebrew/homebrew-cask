cada02fb41704e3e182f8965d52fb8283a4ada36
Sönke Kluth <soenke.kluth@gmail.com>
2014-11-11 16:28:21 +0100

    update genymotion.rb to version 2.3.1

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -1,5 +1,5 @@
 class Genymotion < Cask
-  version '2.3.0'
+  version '2.3.1'
   sha256 'a9af8f9ec2793daa1c53edc13af211367762960e3a3cffc444d7516d1df3c984'
 
   url "http://files2.genymotion.com/genymotion/genymotion-#{version}/genymotion-#{version}.dmg"
