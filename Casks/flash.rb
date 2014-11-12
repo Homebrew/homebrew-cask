04591f2722711279b3ede4ae185c724fc3f373b8
dwkns <dwkns@me.com>
2014-11-12 19:48:34 +0000

    Update flash.rb
    Updated sha256.

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,6 +1,6 @@
 class Flash < Cask
   version '15.0.0.189'
-  sha256 'e9fba4902df0abaa6054c7e4572841abf25564de6ffdf6d30416fb7e3f30ab27'
+  sha256 'd5fd0973439a9250751930735508152cb8b9923fc4bf8c2501ca6750b9ca8623'
 
   url "http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_#{version.to_i}_osx_pkg.dmg"
   homepage 'https://www.adobe.com/products/flashplayer/distribution3.html'
