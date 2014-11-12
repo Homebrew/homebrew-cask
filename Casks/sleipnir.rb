727839c0ecc58c4559d801fcbf650fc674e45a51
Tetsuya Kaneuchi <kaneuchi@fenrir.co.jp>
2014-11-12 09:52:09 +0900

    Add new cask for Sleipnir Browser.

---
+++ Casks/sleipnir.rb
@@ -0,0 +1,10 @@
+class Sleipnir < Cask
+  version '4.5.1'
+  sha256 'e0f59e6f3ecc59fbec4cea6b9295f7f5639769845d0a877c9cca5bb329af9327'
+
+  url 'http://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
+  homepage 'http://www.fenrir-inc.com/sleipnir/'
+  license :closed
+
+  app 'Sleipnir.app'
+end
