00251e5abf46731a4e34d8a9bd5c701cd0d62720
Kevin R. McCraney <kevinrmccraney@gmail.com>
2014-11-12 17:27:10 -0500

    fixed checksum error in carbon-copy-cloner
    changed version flag to :latest to remedy sha256 error; changed url

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'carbon-copy-cloner' do
-  version '4.0.3274'
-  sha256 '7d6479cd68ba9e8f9e0a74b2426d38c99ad3dea1c3a29dd7aaad8d8854d8a753'
+  version :latest
+  sha256 :no_check
 
-  url "http://bombich.com/software/files/ccc-#{version}.zip"
+  url 'http://www.bombich.com/software/download_ccc.php?v=latest&l=alternate'
   appcast 'http://www.bombich.com/software/updates/ccc.php',
           :sha256 => 'ec02ebdd3e4bee0527d46e8256372249780fb1a4fb93ddb782e9da87787bbdff'
   homepage 'http://bombich.com/'
