add55663f47b5e88eec5cc47ce6489bc5d23443b
Roland Walker <walker@pobox.com>
2014-11-12 19:14:12 -0500

    convert to staged_path in heart

--- Casks/heart.rb
+++ Casks/heart.rb
@@ -9,7 +9,7 @@ class Heart < Cask
   screen_saver 'presstube-heart.app/Contents/Resources/Presstube - Heart.saver'
 
   postflight do
-    system '/usr/libexec/PlistBuddy', '-c', 'Set :CFBundleName Heart (Presstube)', "#{destination_path}/presstube-heart.app/Contents/Resources/Presstube - Heart.saver/Contents/Info.plist"
+    system '/usr/libexec/PlistBuddy', '-c', 'Set :CFBundleName Heart (Presstube)', "#{staged_path}/presstube-heart.app/Contents/Resources/Presstube - Heart.saver/Contents/Info.plist"
   end
 
   caveats <<-EOS.undent
