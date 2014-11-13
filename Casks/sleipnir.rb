38cc9018a63d51645d65da4b4f2fd1c782b47bfb
Tetsuya Kaneuchi <kaneuchi@fenrir.co.jp>
2014-11-13 11:44:18 +0900

    Fix version and sha entries for point to the latest version.

--- Casks/sleipnir.rb
+++ Casks/sleipnir.rb
@@ -1,6 +1,6 @@
 class Sleipnir < Cask
-  version '4.5.1'
-  sha256 'e0f59e6f3ecc59fbec4cea6b9295f7f5639769845d0a877c9cca5bb329af9327'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
   homepage 'http://www.fenrir-inc.com/sleipnir/'
