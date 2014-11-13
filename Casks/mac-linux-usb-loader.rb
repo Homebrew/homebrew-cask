a0d11c2dcecd2fc114c35cdf7767e351c2492754
SevenBits <meokid2@yahoo.com>
2014-11-12 20:16:38 -0500

    Updated Mac Linux USB Loader cask to version 2.0.2

--- Casks/mac-linux-usb-loader.rb
+++ Casks/mac-linux-usb-loader.rb
@@ -1,11 +1,11 @@
 class MacLinuxUsbLoader < Cask
-  version :latest
+  version '2.0.2'
   sha256 :no_check
 
-  url 'https://sevenbits.github.io/downloads/Mac-Linux-USB-Loader.zip'
+  url 'https://github.com/SevenBits/Mac-Linux-USB-Loader/releases/download/v2.0.2/Mac.Linux.USB.Loader.zip'
   appcast 'http://sevenbits.github.io/appcasts/mlul.xml'
   homepage 'http://sevenbits.github.io/Mac-Linux-USB-Loader/'
-  license :oss
+  license :bsd
 
   app 'Mac Linux USB Loader.app'
 end
