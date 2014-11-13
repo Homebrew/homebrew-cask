5c8daca933f61511ce697a6a53464b1b1047acf2
blogabe <abe@homer.local>
2014-11-13 15:02:38 -0800

    disconnect: new cask
    online protection, simplified

---
+++ Casks/disconnect.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'disconnect' do
+  version :latest
+  sha256 :no_check
+
+  # amazonaws is the official download host per the vendor homepage
+  url 'https://s3.amazonaws.com/disconnect-desktop/Disconnect+Desktop.pkg'
+  homepage 'https://disconnect.me'
+
+  pkg 'Disconnect+Desktop.pkg'
+  uninstall :pkgutil => [
+    'com.disconnect.pkg.DisconnectDependencies',
+    'com.disconnect.pkg.DisconnectDesktop',
+  ]
+end
