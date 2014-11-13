ac368247f04e5c353e09074346f8bb43ed9779f2
Roland Walker <walker@pobox.com>
2014-11-13 12:25:36 -0500

    new-style header in colorpicker-rcwebcolorpicker

--- Casks/colorpicker-rcwebcolorpicker.rb
+++ Casks/colorpicker-rcwebcolorpicker.rb
@@ -1,4 +1,4 @@
-class ColorpickerRcwebcolorpicker < Cask
+cask :v1 => 'colorpicker-rcwebcolorpicker' do
   version '1.2.X'
   sha256 '04a1965f756695ef0387e388aceb598d3fcde39ec82119d298bf3802f09b3f70'
 
