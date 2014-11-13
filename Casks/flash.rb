20cd325da63455af915735bfc246cd7b6bca69bc
Vítor Galvão <info@vitorgalvao.com>
2014-11-13 19:14:51 +0000

    updated flash version

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'flash' do
-  version '15.0.0.189'
+  version '15.0.0.223'
   sha256 'd5fd0973439a9250751930735508152cb8b9923fc4bf8c2501ca6750b9ca8623'
 
   url "http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_#{version.to_i}_osx_pkg.dmg"
