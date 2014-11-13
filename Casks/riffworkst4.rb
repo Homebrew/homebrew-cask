2ad7be8c69f673dd6a359dd738997beb43843492
Roland Walker <walker@pobox.com>
2014-11-13 12:26:49 -0500

    new-style header in riffworkst4

--- Casks/riffworkst4.rb
+++ Casks/riffworkst4.rb
@@ -1,4 +1,4 @@
-class Riffworkst4 < Cask
+cask :v1 => 'riffworkst4' do
   # note: "4" is not a version number, but an intrinsic part of product name "T4"
   version '2.6'
   sha256 '83c51fa4a08ad19f92418572e11b09ebb33f9a0341ff9e0173d0eb0c7709cddb'
