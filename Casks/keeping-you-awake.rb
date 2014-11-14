66ba7631f405b0f24974e0bea50934608f4bf8a0
Doan Truong Thi <t@thi.im>
2014-11-14 20:14:00 +0900

    Add KeepingYouAwake

---
+++ Casks/keeping-you-awake.rb
@@ -0,0 +1,10 @@
+class KeepingYouAwake < Cask
+  version '1.2beta1'
+  sha256 'b7907bfacf6408492fd47599afc005b91db2839f373f967176e3d79c7b50bb38'
+
+  url 'https://github.com/newmarcel/KeepingYouAwake/releases/download/1.2beta1/KeepingYouAwake-1.2beta1.zip'
+  homepage 'https://github.com/newmarcel/KeepingYouAwake'
+  license :mit
+
+  app 'KeepingYouAwake.app'
+end
