b7d27efe0145ce7f03b2eb62514aafd39ba2d16d
Beng Hee Eu <beng@benghee.eu>
2014-11-13 22:13:23 -0500

    Flash Player 15.0.0.223

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'flash-player' do
-  version '15.0.0.189'
+  version '15.0.0.223'
   sha256 '91d21b45f3a5639b89029b1cf07bf9e3bc424255a62ed62d5180ea7e0ad107f0'
 
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.to_i}/flashplayer_#{version.to_i}_sa.dmg"
