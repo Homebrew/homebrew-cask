282744e5f4d8eee55a6b74fff3f33d056282b2c6
Vítor Galvão <info@vitorgalvao.com>
2014-11-14 14:43:17 +0000

    updated zooom (:latest)

--- Casks/zooom.rb
+++ Casks/zooom.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'zooom' do
-  version '2.6.0'
-  sha256 'eda52f42d06a6cd32c2fc419358a4a589087a1592a38e717577e3ae9a656036f'
+  version :latest
+  sha256 :no_check
 
-  url "http://software.coderage-software.com/zooom/Zooom_#{version}.dmg"
+  url 'http://software.coderage-software.com/zooom/Zooom_Latest.dmg'
   homepage 'http://coderage-software.com/zooom'
   license :unknown
 
