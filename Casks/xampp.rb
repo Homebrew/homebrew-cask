974e5989d264c0add0655fa04a63ef1154632d30
Vítor Galvão <info@vitorgalvao.com>
2014-11-15 16:39:19 +0000

    added vendor note to xampp

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -2,6 +2,7 @@ cask :v1 => 'xampp' do
   version '1.8.3.3'
   sha256 'b54cced0697caa418851a4bdb11ee1697b3f7f71130e1e6bce19582d60e41530'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "https://downloads.sourceforge.net/project/xampp/XAMPP%20Mac%20OS%20X/#{version.sub(%r{\.\d$},'')}/xampp-osx-#{version}-installer.dmg"
   homepage 'http://www.apachefriends.org/index.html'
   license :oss
