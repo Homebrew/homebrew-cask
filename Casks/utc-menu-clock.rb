db1d5a883ba91fc6f0bad3852a72d52476f6c298
Roland Walker <walker@pobox.com>
2014-11-15 12:35:33 -0500

    uninstall stanza for utc-menu-clock

--- Casks/utc-menu-clock.rb
+++ Casks/utc-menu-clock.rb
@@ -5,5 +5,8 @@ cask :v1 => 'utc-menu-clock' do
   url "https://github.com/downloads/netik/UTCMenuClock/UTCMenuClock_#{version}_installer.pkg"
   homepage 'https://github.com/netik/UTCMenuClock'
   license :oss
+
   pkg "UTCMenuClock_#{version}_installer.pkg"
+
+  uninstall :pkgutil => 'net.retina.utcmenuclock.*'
 end
