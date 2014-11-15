cb1b872e4636ca85cb756c51ba6040c485c7b45e
jloveland <jason.c.loveland@gmail.com>
2014-11-15 13:13:04 -0500

    updating fitbit-connet to version 2.0.0.6563

--- Casks/fitbit-connect.rb
+++ Casks/fitbit-connect.rb
@@ -1,8 +1,8 @@
 class FitbitConnect < Cask
-  url 'http://cache.fitbit.com/FitbitConnect/FitbitConnect_Mac_2014110_1.0.1.5104.dmg'
+  url 'http://cache.fitbit.com/FitbitConnect/FitbitConnect_Mac_20141029_2.0.0.6563.dmg'
   homepage 'http://www.fitbit.com/'
-  version '1.0.1.5104'
-  sha256 'ffbb2d5501240313682780e0b98dc41844080260f707cd003c759b38ce9bc658'
+  version '2.0.0.6563'
+  sha256 '383a2bfbcd6455ef100846b49c25fedca0730310114bf03286e00403b86aeb4a'
   install 'Install Fitbit Connect.pkg'
   uninstall :script => { :executable => 'Uninstall Fitbit Connect.app/Contents/Resources/uninstall.sh' }
 end
