f49d0a59bb7edbaa3712232e7d88dfe826b6c2b4
Vítor Galvão <info@vitorgalvao.com>
2014-11-15 16:38:36 +0000

    made xampp easier to update

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -2,10 +2,10 @@ cask :v1 => 'xampp' do
   version '1.8.3.3'
   sha256 'b54cced0697caa418851a4bdb11ee1697b3f7f71130e1e6bce19582d60e41530'
 
-  url 'https://downloads.sourceforge.net/project/xampp/XAMPP%20Mac%20OS%20X/1.8.3/xampp-osx-1.8.3-3-installer.dmg'
+  url "https://downloads.sourceforge.net/project/xampp/XAMPP%20Mac%20OS%20X/#{version.sub(%r{\.\d$},'')}/xampp-osx-#{version}-installer.dmg"
   homepage 'http://www.apachefriends.org/index.html'
   license :oss
 
-  installer :manual => 'xampp-osx-1.8.3-3-installer.app'
+  installer :manual => "xampp-osx-#{version}-installer.app"
   uninstall :delete => '/Applications/XAMPP'
 end
