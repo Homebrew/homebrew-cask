4f4bd56790428ad25f755e6e3110b36efe3c6503
sachin21 <sachin21dev@gmail.com>
2014-11-16 23:53:15 +0900

    Set version to url

--- Casks/teitoku.rb
+++ Casks/teitoku.rb
@@ -1,8 +1,8 @@
 class Teitoku < Cask
-  version :latest
+  version '0.3.2'
   sha256 :no_check
 
-  url 'https://github.com/geta6/teitoku/releases/download/0.3.2/teitoku-0.3.2-osx.zip'
+  url "https://github.com/geta6/teitoku/releases/download/#{version}/teitoku-#{version}-osx.zip"
   homepage 'http://makebooth.com/i/1xkN1'
   license :mit
 
