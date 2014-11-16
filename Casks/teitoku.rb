b6713fbda73bf690d84cd6d460caefd2e81d4462
sachin21 <sachin21dev@gmail.com>
2014-11-17 00:18:35 +0900

    Set SHA-256

--- Casks/teitoku.rb
+++ Casks/teitoku.rb
@@ -1,6 +1,6 @@
 class Teitoku < Cask
   version '0.3.2'
-  sha256 :no_check
+  sha256 '87812a8a1167ee69942f35b55e402c86f1079ab0f946c1d3a049a716bd8406e1'
 
   url "https://github.com/geta6/teitoku/releases/download/#{version}/teitoku-#{version}-osx.zip"
   homepage 'http://makebooth.com/i/1xkN1'
