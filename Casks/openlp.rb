e98e534035990b850f1e86d1abaf154a2859a457
Vítor Galvão <info@vitorgalvao.com>
2014-11-16 22:42:02 +0000

    added TLD to host reference in 'official download host' comment in openlp.rb

--- Casks/openlp.rb
+++ Casks/openlp.rb
@@ -2,7 +2,7 @@ cask :v1 => 'openlp' do
   version '2.0.5'
   sha256 '0ba5a37c359394c277e221f8ca74191d3425cf3cfbd70699d81346c77c93d746'
 
-  # sourceforge is the official download host per the vendor homepage
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/openlp/openlp/#{version}/OpenLP-#{version}.dmg"
   homepage 'http://openlp.org'
   license :gpl
