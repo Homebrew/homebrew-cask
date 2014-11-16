04ca35eb02d00b748aac88478429e5de08ba88c9
Vítor Galvão <info@vitorgalvao.com>
2014-11-16 22:41:21 +0000

    added TLD to host reference in 'official download host' comment in kicad.rb

--- Casks/kicad.rb
+++ Casks/kicad.rb
@@ -2,7 +2,7 @@ cask :v1 => 'kicad' do
   version '2014-02-26'
   sha256 '066520f10e9646e88c2e1d7812adf54a73a331592076f394cb439a97c228410b'
 
-  # mdx4 is the official download host per the vendor homepage
+  # mdx4.org is the official download host per the vendor homepage
   url "http://www.mdx4.org/uploads/kicad/Kicad-product-#{version}.zip"
   homepage 'http://www.kicad-pcb.org/'
   license :gpl
