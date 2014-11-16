ff360be0811afcd5760dde3fb3ee0d2ece2bc390
Vítor Galvão <info@vitorgalvao.com>
2014-11-16 22:42:27 +0000

    added TLD to host reference in 'official download host' comment in sidekick.rb

--- Casks/sidekick.rb
+++ Casks/sidekick.rb
@@ -2,7 +2,7 @@ cask :v1 => 'sidekick' do
   version :latest
   sha256 :no_check
 
-  # amazonaws is the official download host per the vendor homepage
+  # amazonaws.com is the official download host per the vendor homepage
   url 'http://releases.oomphalot.com.s3-website-us-east-1.amazonaws.com/Sidekick/Sidekick.zip'
   appcast 'http://updates.oomphalot.com/?app=Sidekick'
   homepage 'http://oomphalot.com/sidekick/'
