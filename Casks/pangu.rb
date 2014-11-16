b322e05aad88c030642fb2c37befb9ba290beb2f
Vítor Galvão <info@vitorgalvao.com>
2014-11-16 22:42:09 +0000

    added TLD to host reference in 'official download host' comment in pangu.rb

--- Casks/pangu.rb
+++ Casks/pangu.rb
@@ -2,7 +2,7 @@ cask :v1 => 'pangu' do
   version '1.0.0'
   sha256 '910c2fcc6bbaea54f2944eb275de9674bc4613bf79f1f60dbf8ad0064269a252'
 
-  # 25pp is the official download host per the vendor homepage
+  # 25pp.com is the official download host per the vendor homepage
   url "http://dl.pangu.25pp.com/jb/Pangu8_v#{version}.dmg"
   homepage 'http://en.pangu.io/'
   license :unknown
