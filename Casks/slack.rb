69f093a440ddb98bf083650aa7c708993b94bdd9
Vítor Galvão <info@vitorgalvao.com>
2014-11-16 22:37:22 +0000

    added “official download host” info to slack

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -2,6 +2,7 @@ cask :v1 => 'slack' do
   version '1.0'
   sha256 '3cb5c52077388042006104d18ec1b7c230ededcf9f9691df2a42687e86f3b40c'
 
+  # hockeyapp.net is the official download host per the appcast feed
   url 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1/app_versions/4?format=zip&avtoken=826b2af93e6b167a3571c3823019254f0352efd3'
   appcast 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1',
           :sha256 => '0bfbef89c2b420d7485baf482cdd82d84f5e529c92cca653996e15ca5e6ba3c6'
