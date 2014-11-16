57d1101deacfedc7fe988f41eb96f54ad9a41a28
Vítor Galvão <info@vitorgalvao.com>
2014-11-16 22:42:55 +0000

    added TLD to host reference in 'official download host' comment in workamajig.rb

--- Casks/workamajig.rb
+++ Casks/workamajig.rb
@@ -2,7 +2,7 @@ cask :v1 => 'workamajig' do
   version :latest
   sha256 :no_check
 
-  # amazonaws is the official download host per the vendor homepage
+  # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/Workamajig/AIR/Workamajig.air'
   homepage 'http://www.workamajig.com/'
   license :unknown
