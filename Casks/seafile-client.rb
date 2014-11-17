2a95c89300dff9768152115fc74eef14e1d0efb9
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2014-11-17 22:08:36 +0100

    Update seafile-client to 3.1.11

--- Casks/seafile-client.rb
+++ Casks/seafile-client.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'seafile-client' do
-  version '3.1.5'
-  sha256 '25eedd712d57bcf94fd19ba2783d5f0278c3e95403ee07f300e17dabbd6bb75a'
+  version '3.1.11'
+  sha256 'abe35aae05b1716c51bedcf60963b9bc90fff7bfe27e5a60eb75d5afc9838c52'
 
   url "https://bitbucket.org/haiwen/seafile/downloads/seafile-client-#{version}.dmg"
   homepage 'http://seafile.com/'
