1b7f4a0661b29b51d6267cbc834bfc75f5930ebf
Vítor Galvão <info@vitorgalvao.com>
2014-11-17 21:47:42 +0000

    updated blueharvest ('6.1.3')

--- Casks/blueharvest.rb
+++ Casks/blueharvest.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'blueharvest' do
-  version '6.1.2'
-  sha256 '51ce55e1cadad80abf198088459e378c28ee7695c23f1ac7f6a48919c4f42c06'
+  version '6.1.3'
+  sha256 '6dde179090ce552da9d5cfc188e8b643a7beab8a923b90c0365b7c440f8018db'
 
   url "http://zeroonetwenty.com/downloads/BlueHarvest#{version.gsub('.','')}.dmg"
   appcast 'https://cp37.ezyreg.com/~zeroonet/downloads/versioninfo/sparkle/blueharvest.xml',
