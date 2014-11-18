d100f302411d87ae1f4aecbf3c5401c9a05b382b
Havanna <registerdedicated@yeah.net>
2014-11-18 15:46:23 +0800

    Havanna Add Stringsfile (QuickLook Plugin to preview .strings files)

---
+++ Casks/stringsfile.rb
@@ -0,0 +1,10 @@
+cask :v1 < 'stringsfile' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://blog.timac.org/post-images/StringsFileQuickLook/StringsFile.qlgenerator.zip'
+  homepage 'http://blog.timac.org/?p=933'
+  license :unknown
+
+  qlplugin 'StringsFile.qlgenerator'
+end
