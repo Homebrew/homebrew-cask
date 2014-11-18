000e2516387c097e70e15c976d232b22a06d27fd
Roland Walker <walker@pobox.com>
2014-11-18 10:01:06 -0500

    add missing test coverage for `suite` artifact
    refs: #7382

---
+++ test/support/Casks/with-suite.rb
@@ -0,0 +1,9 @@
+class WithSuite < TestCask
+  version '1.2.3'
+  sha256 'd1302a0dc25aff72ad395ed01a830468b92253ffd28269574f3ac0b5eb8aad54'
+
+  url TestHelper.local_binary_url('caffeine_suite.zip')
+  homepage 'http://example.com/with-suite'
+
+  suite 'caffeine_suite'
+end
