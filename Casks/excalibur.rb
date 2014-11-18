ee657a7653758b278e4db4d9915cf28eabaa3a02
takumim <tmurayama@gmail.com>
2014-11-18 17:19:00 -0500

    HOLD for next release: Excalibur cask
    Adding a cask for Excalibur; HOLD for next release since this required an
    exception for Sourceforge URL's.

---
+++ Casks/excalibur.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'Excalibur' do
+  version '4.0.7'
+  sha256 'e4bf9d459b1df49d80753c9ef32cc8fa4fe6df9cfaa7c21f912052ffb2061202'
+
+  url 'http://excalibur.sourceforge.net/get.php?id=407d'
+  homepage 'http://excalibur.sourceforge.net'
+  license :gpl
+
+  app 'Excalibur.app'
+end
