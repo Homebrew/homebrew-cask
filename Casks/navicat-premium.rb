4ebf97c04ccc39d8597f518bb3de9f8ed438079b
Josh Butts <josh@joshbutts.com>
2014-11-18 16:51:06 -0600

    Update Navicat Premium to 11.1.6
    Existing 11.1.5 sha256 was also wrong

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'navicat-premium' do
-  version '11.1.5'
-  sha256 '31229725466814d7882059d0504fa14a467229a1beba772b91e152cc403b130e'
+  version '11.1.6'
+  sha256 'd3a67be473f8317706c65e79e3d364eac37dd99559c3e97f148bd21534831a06'
 
   url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_premium_en.dmg"
   homepage 'http://www.navicat.com/products/navicat-premium'
