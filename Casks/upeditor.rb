ffd9df32c7895497cf89e3de3ce212d12177f64c
Havanna <registerdedicated@yeah.net>
2014-11-18 09:59:19 +0800

    Add upeditor (China UnionPay Plugin) v1.0.0.2

---
+++ Casks/upeditor.rb
@@ -0,0 +1,10 @@
+class Upeditor < Cask
+  version '1.0.0.2'
+  sha256 '5589672fabb8f8fa6320f56bb53b333e5adbda0d9bebd8460184208c4d490ee0'
+
+  url 'https://user.95516.com/ctrl/UPEditor_2.dmg'
+  homepage 'https://www.95516.com'
+  license :unknown
+
+  internet_plugin 'UPEditorSafari.plugin'
+end
