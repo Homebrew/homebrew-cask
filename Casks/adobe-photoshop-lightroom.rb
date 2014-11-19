f1a36106cb6ce7c6d31aa2fb38e946e23b8dca8c
Daan van Dijk <drvdijk@gmail.com>
2014-11-19 10:21:22 -0500

    Upgrade Adobe Lightroom to 5.7

--- Casks/adobe-photoshop-lightroom.rb
+++ Casks/adobe-photoshop-lightroom.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'adobe-photoshop-lightroom' do
-  version '5.6'
-  sha256 '794fa6b364985a6e3c830f78c9ad9c52c46208676bf01576976cbcfc818aa61a'
+  version '5.7'
+  sha256 'cb997a526ebf6e338896d21f48b1037e44009cc67d1cef789109d93b112679ac'
 
   url "http://download.adobe.com/pub/adobe/lightroom/mac/#{version.to_i}.x/Lightroom_#{version.to_i}_LS11_mac_#{version.gsub('.','_')}.dmg"
   homepage 'http://www.adobe.com/products/photoshop-lightroom.html'
-  license :unknown
+  license :commercial
 
   pkg "Adobe Photoshop Lightroom #{version.to_i}.pkg"
 
