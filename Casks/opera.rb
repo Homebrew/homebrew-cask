09d008619eb484617d76e5511d1a546f15b5297a
Keng Kiat Lim <kkin@crnrc.com>
2014-11-19 23:36:46 +0800

    Upgraded Opera to 25.0.1614.71

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera' do
-  version '25.0.1614.50'
-  sha256 '289cbce26977ed7ef6b082d64392c1addfc94642752bc17ae9bf56fcc753dd69'
+  version '25.0.1614.71'
+  sha256 'd0c45553f8d3dc22c98f2790f935a5254332526fa8e472ca18e3567515d5de99'
 
   url "http://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/'
