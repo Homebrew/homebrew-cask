5540c8949f2fb336b58ff43bfbd337d942c6e575
Florian Harbecke <florian@harbecke.me>
2014-11-19 10:52:33 +0100

    Added Deezer (Beta) v0.9.9_4104

---
+++ Casks/deezer.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'deezer' do
+  version '0.9.9_4104'
+  sha256 '2b9e8acb66a01a7b6e77da75a007b68648e07c66fd413bf57c2c29e029ee43ba'
+
+  url "http://cdn-content.deezer.com/builds/mac/Deezer#{version.sub(%r{^[^_]*(_\d+)},'\1')}.dmg"
+  homepage 'https://www.deezer.com/beta/formac'
+  license :gratis
+
+  app 'Deezer.app'
+
+  zap :delete => '~/Library/Preferences/com.deezer.Deezer.plist'
+end
