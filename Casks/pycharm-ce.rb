21e3243b6cc719f5975934ed0318bcf0457b8a3f
Jameel Al-Aziz <me@jalaziz.io>
2014-11-20 00:26:53 -0800

    Upgrade PyCharm Community Edition to 4.0

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pycharm-ce' do
-  version '3.4.1'
-  sha256 '18b4d2d61badb81feaf22a51a7c76b85618e5a7a01f4f4c5e46079d134c64002'
+  version '4.0'
+  sha256 'a879358d671b91df2ca91aaa682d603da971afcd7c440c46ec3f4ea466c4add4'
 
   url "http://download.jetbrains.com/python/pycharm-community-#{version}.dmg"
   homepage 'http://www.jetbrains.com/pycharm'
