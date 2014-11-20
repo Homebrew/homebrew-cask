31634f58042f49b811fdb44ba31d6034ced61689
Johnathan Conley <johnathan.conley@gmail.com>
2014-11-20 10:37:14 -0600

    Add uDig

---
+++ Casks/udig.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'udig' do
+  version '1.4.0'
+  sha256 '1364245d9e6f62a3ec4f21b5a0eb4046a0423ca778d3d550b348d5bb20706fa2'
+
+  url 'http://udig.refractions.net/files/downloads/udig-1.4.0.macosx.cocoa.x86_64.dmg'
+  homepage 'http://udig.refractions.net/'
+  license :oss
+
+  app 'udig'
+end
