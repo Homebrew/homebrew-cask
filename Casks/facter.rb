f559706f578b9040a629f4db7997fa5df16c879a
Florian Harbecke <florian@harbecke.me>
2014-11-20 10:52:31 +0100

    Added Puppetlabs Facter 2.3.0

---
+++ Casks/facter.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'facter' do
+  version '2.3.0'
+  sha256 '35c88c2af26e2c59364604788cd5ff4a1b3d8171e2690e65e38156278e32d6d1'
+
+  url "https://downloads.puppetlabs.com/mac/facter-#{version}.dmg"
+  homepage 'https://puppetlabs.com/facter'
+  license :unknown
+
+  pkg "facter-#{version}.pkg"
+  uninstall :pkgutil => 'com.puppetlabs.facter'
+end
