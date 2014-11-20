3c7156ca7644ca9a47f7637d38c8f12cebd3f6e8
Roland Walker <walker@pobox.com>
2014-11-20 09:05:46 -0500

    re-use version in puppet

--- Casks/puppet.rb
+++ Casks/puppet.rb
@@ -2,9 +2,9 @@ cask :v1 => 'puppet' do
   version '3.7.3'
   sha256 'f673b9c45dbefa410efa6d7f74ea820a1b01b4f659f142c92310f8ab3df477af'
 
-  url 'http://downloads.puppetlabs.com/mac/puppet-3.7.3.dmg'
+  url "http://downloads.puppetlabs.com/mac/puppet-#{version}.dmg"
   homepage 'http://puppetlabs.com/'
   license :unknown
 
-  pkg 'puppet-3.7.3.pkg'
+  pkg "puppet-#{version}.pkg"
 end
