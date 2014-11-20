478e55f05767645c747cfcf198ce4e8f1606509a
Julita Barkauskaite <julita.barkauskaite@screeninteraction.com>
2014-11-20 10:53:33 +0100

    Fixed syntax, add sha256

--- Casks/hiera.rb
+++ Casks/hiera.rb
@@ -1,6 +1,6 @@
-class Hiera < Cask
+cask :v1 => 'hiera' do
   version '1.3.4'
-  sha256 :no_check
+  sha256 'f0c5342b09b6f48c727ded033e03d207313d3ebf12d41b50b0f2f71fd1c411e9'
 
   url 'http://downloads.puppetlabs.com/mac/hiera-1.3.4.dmg'
   homepage 'https://projects.puppetlabs.com/projects/hiera'
