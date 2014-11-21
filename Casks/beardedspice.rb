86c6044dd74070071b4cc2b9da2584b11e4c52e8
Tomas Thor Jonsson <benregn@gmail.com>
2014-11-21 16:31:13 +0100

    update beardedspice to 0.2.2
    Closes #7509.

--- Casks/beardedspice.rb
+++ Casks/beardedspice.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'beardedspice' do
-  version '0.2.1'
-  sha256 '28233946d266a95c390f2393fd80fdd1a82c89499ebae7014d797c75cb28c110'
+  version '0.2.2'
+  sha256 '910e1ddf4e84ffc8cfce39aa93b85243189af3248d22d01f8eea1407bceba978'
 
   url "https://github.com/beardedspice/beardedspice/raw/releases/BeardedSpice-#{version}.tar.gz"
   homepage 'http://www.beardedspice.com'
