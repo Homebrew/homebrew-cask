9ed4f4bbb49b28041829682ba3947f49e4438939
Lee Hanxue <hanxue.developer@gmail.com>
2014-11-21 15:04:20 +0800

    Update Android Studio Bundle 0.8.14 build-135.1538390

--- Casks/android-studio-bundle.rb
+++ Casks/android-studio-bundle.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'android-studio-bundle' do
-  version '0.8.6 build-135.1339820'
-  sha256 '3a9f65434a2381019f4487481331f539a69b09b8ea81a8b4dfff9c6a126423f0'
+  version '0.8.14 build-135.1538390'
+  sha256 '05eb79f0c4025f510ff02d7205157eb94d42074a2d89c8a5ba4cbead1187948f'
 
-  url 'https://dl.google.com/android/studio/install/0.8.6/android-studio-bundle-135.1339820-mac.dmg'
+  url 'https://dl.google.com/dl/android/studio/ide-zips/0.8.14/android-studio-ide-135.1538390-mac.zip'
   homepage 'http://developer.android.com/sdk/installing/studio.html'
   license :unknown
 
