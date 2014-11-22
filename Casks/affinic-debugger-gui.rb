b369f18ee6164392ffbd29c94085d2a9f8c77809
Sean Zhu <interestinglythere@gmail.com>
2014-11-21 22:02:43 -0800

    Add Affinic Debugger GUI

---
+++ Casks/affinic-debugger-gui.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'affinic-debugger-gui' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.affinic.com/download/adg_macosx.dmg'
+  homepage 'http://www.affinic.com/?page_id=435'
+  license :unknown
+
+  app 'Affinic Debugger GUI.app'
+end
