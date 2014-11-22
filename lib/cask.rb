31f81cc8e995861fcd9459192d16891a1c908d6b
Roland Walker <walker@pobox.com>
2014-11-22 10:33:28 -0500

    remove all transitional tap migration code

--- lib/cask.rb
+++ lib/cask.rb
@@ -4,10 +4,6 @@ class Cask; end
 
 require 'download_strategy'
 
-# transitional, for set_up_taps (see below)
-require 'cmd/update'
-require 'rubygems'
-
 require 'cask/artifact'
 require 'cask/audit'
 require 'cask/auditor'
@@ -47,7 +43,6 @@ class Cask
   include Cask::Utils
 
   def self.init
-    set_up_taps
     # todo: Creating directories should be deferred until needed.
     #       Currently this fire and even asks for sudo password
     #       if a first-time user simply runs "brew cask --help".
@@ -76,47 +71,6 @@ class Cask
         end
       end
     end
-  end
-
-  def self.set_up_taps
-    odebug 'Initializing Taps'
-
-    return true if Cask.default_tap.match(%r{test[^/]*\Z})
-
-    # transitional: help with Homebrew's move of Tap dirs, Apr 2014
-    minimum_homebrew_version = '0.9.5'
-    unless Gem::Version.new(HOMEBREW_VERSION) >= Gem::Version.new(minimum_homebrew_version)
-      raise CaskError.new <<-EOS.undent
-        Minimum Homebrew version '#{minimum_homebrew_version}' required.
-        (Homebrew version #{HOMEBREW_VERSION} was detected.)
-        Try running "brew update".
-      EOS
-    end
-    begin
-      Homebrew.send(:rename_taps_dir_if_necessary)
-    rescue StandardError
-      opoo %q{Trouble with automatic Tap migration. You may need to run "brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup"}
-    end
-
-    # transitional: help with our own move to new GitHub project, May 2014
-    legacy_user = 'phinze'
-    new_user = 'caskroom'
-    repo = 'cask'
-    legacy_tap = HOMEBREW_LIBRARY/"Taps/#{legacy_user}/homebrew-#{repo}"
-    new_tap = HOMEBREW_LIBRARY/"Taps/#{new_user}/homebrew-#{repo}"
-    if legacy_tap.directory?
-      ohai 'Removing legacy Tap'
-      files = []
-      legacy_tap.find_formula { |file| files << file }
-      Homebrew.unlink_tap_formula(files)
-      legacy_tap.rmtree
-      legacy_tap.dirname.rmdir_if_possible
-    end
-    unless new_tap.directory?
-      ohai 'Adding caskroom Tap'
-      Homebrew.install_tap(new_user, repo)
-    end
-    Cask.reset_all_tapped_cask_dirs
   end
 
   def self.load(query)
