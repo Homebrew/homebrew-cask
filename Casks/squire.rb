53fa37d73930da48ebcdc30414410b955a3dc2a9
Roland Walker <walker@pobox.com>
2014-11-22 09:47:52 -0500

    update squire.rb
    * previous url was broken
    * newest url is unversioned
    * remove broken appcast
    * update license

--- Casks/squire.rb
+++ Casks/squire.rb
@@ -1,11 +1,10 @@
 cask :v1 => 'squire' do
-  version '0.91'
-  sha256 'd37f2cdf5f08ed85e5a1f10a66c70a964e51e3a64e7c7335d97c50b1a7a1691e'
+  version :latest
+  sha256 :no_check
 
-  url "https://s3.amazonaws.com/Squire_Mac_Builds/Squire_#{version}.zip"
-  appcast 'http://www.sylion.com/squireapp/sparkle/SquireMac/appcastMac.xml'
+  url 'https://s3.amazonaws.com/Squire_Mac_Builds/Squire.dmg'
   homepage 'http://squireapp.com'
-  license :unknown
+  license :commercial
 
   app 'Squire.app'
 end
