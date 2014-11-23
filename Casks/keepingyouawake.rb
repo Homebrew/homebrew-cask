90ba047bcab8b3e2040dad5cedfd338503bec548
James Moon <mail@remailed.net>
2014-11-23 13:28:57 -0500

    Update KeepingYouAwake to v1.2
    Closes #7551.

--- Casks/keepingyouawake.rb
+++ Casks/keepingyouawake.rb
@@ -1,8 +1,11 @@
 cask :v1 => 'keepingyouawake' do
-  version '1.1'
-  sha256 '78abc2e8451be3120dc49886feae1894a5eecaa1d2d0f988b767729f97560cc6'
+  version '1.2'
+  sha256 '8087d86ea40cbcb933b87f59a58a9dfcb35b3e6faaa17e7dd17125496ccad58f'
 
   url "https://github.com/newmarcel/KeepingYouAwake/releases/download/#{version}/KeepingYouAwake-#{version}.zip"
+  appcast 'https://newmarcel.github.io/KeepingYouAwake/appcast.xml',
+    :sha256 => '738fcec2775620870b759319b9c49fabb71cfff9eaf330048c9725816c52254d',
+    :format => :sparkle
   homepage 'https://github.com/newmarcel/KeepingYouAwake'
   license :mit
 
