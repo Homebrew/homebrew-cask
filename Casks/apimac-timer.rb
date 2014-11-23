53f21ef340f74c5240bdf94c241f35bca49fd178
jake romer <jkrmr@users.noreply.github.com>
2014-11-22 21:24:01 -0800

    Update Apimac Timer formula.
    Updates formula to reference the latest version of [Apimac Timer](http://www.apimac.com/downloads/), 8.0.2.

--- Casks/apimac-timer.rb
+++ Casks/apimac-timer.rb
@@ -2,7 +2,7 @@ cask :v1 => 'apimac-timer' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.apimac.com/download/timer.dmg'
+  url 'http://www.apimac.com/download/Timer.zip'
   homepage 'http://www.apimac.com/mac/timer/'
   license :gratis
 
