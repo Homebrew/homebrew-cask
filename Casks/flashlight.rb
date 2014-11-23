aedfe9ad5f1db557697b2707fa746ca484654761
Jia Sui <jsfaint@gmail.com>
2014-11-23 21:27:38 +0800

    Update flashlight to v0.8-alpha.
    new plugin browsing interface
    better support for localization

--- Casks/flashlight.rb
+++ Casks/flashlight.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'flashlight' do
-  version 'v0.71-alpha'
-  sha256 '63c0258267c67e7213afb565882a222788856cbd9e6105bbe966b30ef9722bae'
+  version 'v0.80-alpha'
+  sha256 'db6fcb73f84d83a8a8cf6702fc1972b2fc7f2d7eaed414e544d6b946f959fc6f'
 
   url "https://github.com/nate-parrott/Flashlight/releases/download/#{version}/Flashlight.zip"
   homepage 'http://flashlight.nateparrott.com/'
