7d3ded8e6809fb1d7399dac36073f3f8ff20cf63
Vítor Galvão <info@vitorgalvao.com>
2014-11-24 03:13:06 +0000

    updated shotcut ('14.11.01')

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'shotcut' do
-  version '14.10.05'
-  sha256 '8d2dc72bde1fd9cd2646a9bf93fd3e9c0a91c56a5274a046cb21706ba9a490a6'
+  version '14.11.01'
+  sha256 'f364c8849ed79376006209bfb0146d1449461bb3b18a1548c55b3c8fb93f00ba'
 
   url "https://github.com/mltframework/shotcut/releases/download/v#{version.gsub(/\.\d{2}$/, '')}/shotcut-osx-x86_64-#{version.gsub('.', '')}.dmg"
   homepage 'http://www.shotcut.org/'
