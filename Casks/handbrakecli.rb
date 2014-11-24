6ad83bf1e736df8bf013abcddbe4b66109598329
ndr <ndr@qef.io>
2014-11-24 15:14:31 +0000

    Use reliable URL in handbrakecli
    URLs in the previous format expire on each new release

--- Casks/handbrakecli.rb
+++ Casks/handbrakecli.rb
@@ -2,7 +2,7 @@ cask :v1 => 'handbrakecli' do
   version '0.10.0'
   sha256 '84dcc20cffd2cbbe1d7804dbb8dfdb4ce7ce78b2f44ae25a998f18771a9c05f9'
 
-  url "https://handbrake.fr/rotation.php?file=HandBrake-#{version}-MacOSX.6_CLI_x86_64.dmg"
+  url "http://download.handbrake.fr/releases/#{version}/HandBrake-#{version}-MacOSX.6_CLI_x86_64.dmg"
   homepage 'http://handbrake.fr'
   license :oss
 
