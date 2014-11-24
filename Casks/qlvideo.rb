58a547b94ea440e89cf33d3b4333e58587a2badd
Jonathan Harris <jonathan@marginal.org.uk>
2014-11-24 01:05:17 +0000

    Update qlvideo to 1.7.0

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qlvideo' do
-  version '1.5.0'
-  sha256 '48edc2259fd554f536aa4a99ffac6389f7b17c5ce8a0c8e0075d934a074ca430'
+  version '1.7.0'
+  sha256 'a942ad6046636be852495ebed66c2c91ebfe647e5a6052e3f43f94b4edac2ba0'
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.gsub('.', '')}/QLVideo_#{version.gsub('.', '')}.pkg"
   homepage 'https://github.com/Marginal/QLVideo'
