b06927991c4d1cdc0063e38e59de072f513a59bb
Lee Hanxue <hanxue.developer@gmail.com>
2014-11-24 10:45:40 +0800

    Add version and checksum for epic cask
    Closes #7561.

--- Casks/epic.rb
+++ Casks/epic.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'epic' do
-  version :latest
-  sha256 :no_check
+  version '37_4'
+  sha256 'b3dcd0d0e101629342d13c8683f3801412517274c75a70645d38813c07030a48'
 
-  url 'https://ed5b681d56298a85550d-7d665255a6e48f36b11ee3cfeece77e0.ssl.cf1.rackcdn.com/epic_mac_33_alternate/Epic.dmg'
+  # rackcdn.com is the official download host per the vendor homepage
+  url "https://ed5b681d56298a85550d-7d665255a6e48f36b11ee3cfeece77e0.ssl.cf1.rackcdn.com/epic_mac_#{version}_alternative/Epic.dmg"
   appcast 'https://updates.epicbrowser.com/mac_updates/appcast.xml'
   homepage 'http://www.epicbrowser.com'
   license :unknown
