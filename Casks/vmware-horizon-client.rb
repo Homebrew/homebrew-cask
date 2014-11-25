a95b6a5760e51616fce546a9ae2c7906f0619bfd
virtualswede <jonas.rosland@gmail.com>
2014-11-25 08:28:42 -0500

    Updated to version 3.1.0-2096621
    Closes #7584.

---
+++ Casks/vmware-horizon-client.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'vmware-horizon-client' do
+  version '3.1.0-2096621'
+  sha256 '2ef9b6a377e722f03c1b8c10ea9e7df5c56b5e3973e370905602b1ce5e4ed17b'
+
+  url "https://download3.vmware.com/software/view/viewclients/VMware-Horizon-Client-#{version}.dmg"
+  homepage 'http://www.vmware.com/'
+  license :unknown
+
+  app 'VMware Horizon Client.app'
+end
