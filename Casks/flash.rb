5a1096c9d8ef6193615711346e92553b0f9d2809
Tamir Duberstein <tamird@gmail.com>
2014-11-25 12:11:56 -0800

    Update flash.rb

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'flash' do
-  version '15.0.0.223'
-  sha256 'd5fd0973439a9250751930735508152cb8b9923fc4bf8c2501ca6750b9ca8623'
+  version '15.0.0.239'
+  sha256 '6c0a22ad48df4960e240fc08f4913eb24775eca42e99dc3cc9a441bf1ea3e68a'
 
   url "http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_#{version.to_i}_osx_pkg.dmg"
   homepage 'https://www.adobe.com/products/flashplayer/distribution3.html'
