a200fc53a3a081755aca015a4050def19cdc738b
Vítor Galvão <info@vitorgalvao.com>
2014-11-25 16:09:38 +0000

    add official download host line to lisanet-gimp

--- Casks/lisanet-gimp.rb
+++ Casks/lisanet-gimp.rb
@@ -2,6 +2,7 @@ cask :v1 => 'lisanet-gimp' do
   version '2.8.14p1'
   sha256 '2a755c08e50290b494c26c416b6dda6ad4151610f752216d0db126d479ed787e'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "https://downloads.sourceforge.net/gimponosx/Gimp-#{version}-Mavericks-Yosemite.dmg"
   homepage 'http://gimp.lisanet.de'
   license :oss
