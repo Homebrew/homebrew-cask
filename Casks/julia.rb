654ddcdf2df29ff7f20aae08007efeb0d5ac4a1e
ndr <ndr@qef.io>
2014-11-25 12:38:41 +0000

    Update julia to 0.3.3

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'julia' do
-  version '0.3.2'
-  sha256 '33d05ca32633fbec2837f724aa6b5ad0721966323a3a1b3bff59fc468a4c23e3'
+  version '0.3.3'
+  sha256 '24df35408e83d38bcf95fcaeefd613beeafe00e2b6d8435f760289c158adc7b9'
 
   url "https://s3.amazonaws.com/julialang/bin/osx/x64/0.3/julia-#{version}-osx10.7+.dmg"
   homepage 'http://julialang.org/'
