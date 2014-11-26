3636495268922804b7241cc525886d2911fd95fd
Roland Walker <walker@pobox.com>
2014-11-26 10:24:54 -0500

    re-use version in xslimmer

--- Casks/xslimmer.rb
+++ Casks/xslimmer.rb
@@ -2,7 +2,7 @@ cask :v1 => 'xslimmer' do
   version '1.9.4'
   sha256 '3b496e1f51c10e48d6d0d4ac35f35f993a256044e8923936813363d5ef04b56a'
 
-  url 'http://latenitesoft.com/xslimmer/download/Xslimmer_1.9.dmg'
+  url "http://latenitesoft.com/xslimmer/download/Xslimmer_#{version.sub(%r{^(\d+\.\d+).*},'\1')}.dmg"
   appcast 'http://www.xslimmer.com/releases.xml',
           :sha256 => '7f27ccf84109291c59781abe74950f67d53cdb89365497c2925a5d1106f3a15a'
   homepage 'http://latenitesoft.com/xslimmer/'
