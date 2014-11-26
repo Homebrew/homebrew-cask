f810867f5dc578288d0b30d531775445f44139a9
Roland Walker <walker@pobox.com>
2014-11-26 10:24:53 -0500

    re-use version in astah-community

--- Casks/astah-community.rb
+++ Casks/astah-community.rb
@@ -2,12 +2,12 @@ cask :v1 => 'astah-community' do
   version '6.9.0'
   sha256 '6e3119f2a723a4a8975583d6693bae975e95fd3339666c07cfb16dd4138e7ceb'
 
-  url 'http://cdn.astah.net/downloads/astah-community-6_9_0-b4c6e9-MacOs.dmg'
+  url "http://cdn.astah.net/downloads/astah-community-#{version.gsub('.','_')}-b4c6e9-MacOs.dmg"
   homepage 'http://astah.net/editions/community'
   license :gratis
   tags :vendor => 'Change Vision'
 
-  pkg 'astah community ver 6_9_0.pkg'
+  pkg "astah community ver #{version.gsub('.','_')}.pkg"
 
   uninstall :pkgutil => 'com.change-vision.astahcommunity.astahcommunity.pkg'
 end
