e0761072016d376f66012be43c8b315995508b04
Vincent Klaiber <vincent@schimpanz.com>
2014-11-27 08:28:18 +0100

    Add Kibako.app version 1.1.1
    A better way to share files https://www.kibakoapp.com/

---
+++ Casks/kibako.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'kibako' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://updates.kibakoapp.com/download/latest'
+  homepage 'https://www.kibakoapp.com/'
+  license :unknown
+
+  app 'Kibako.app'
+end
