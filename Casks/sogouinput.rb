67d931174a4e74c91ed532e321597a4df3eb38f1
nuomi1 <nuomi1@qq.com>
2014-11-29 22:37:03 +0800

    Update sogouinput.rb
    Fix.

--- Casks/sogouinput.rb
+++ Casks/sogouinput.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 cask :v1 => 'sogouinput' do
-  version '3.0.3.59116'
+  version '3.0.2.59116'
   sha256 '51df347ed60ea13bb39dd89ec5fc9eb6cd549f0080d9f94d97a7e0502b0edb0c'
 
   url "http://download.ime.sogou.com/1411725208/sogou_mac_30a.dmg"
