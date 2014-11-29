1dd19acafa60f6e4da450b8aa6d7b1ecb65443a2
Vítor Galvão <info@vitorgalvao.com>
2014-11-29 14:07:24 +0000

    updated arrsync ('0.4.1')

--- Casks/arrsync.rb
+++ Casks/arrsync.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'arrsync' do
-  version :latest
-  sha256 :no_check
+  version '0.4.1'
+  sha256 '8a254771065f90a9f9f882589eccaf7bc51ad5d0ac86faf15847381bcdbe64f0'
 
-  url 'https://sourceforge.net/projects/arrsync/files/latest/download'
+  url "http://downloads.sourceforge.net/sourceforge/arrsync/arrsync-#{version}.dmg"
   homepage 'http://arrsync.sourceforge.net'
   license :oss
 
