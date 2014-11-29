395f4f1b6c6534f90dd1b1d0c02beb30c6c75981
Magnus Öman <magnus@nani.se>
2014-11-29 22:32:00 +0100

    Added license stanza for freesmug-chromium

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -5,6 +5,7 @@ cask :v1 => 'freesmug-chromium' do
   url "https://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_#{version}.dmg"
   appcast 'http://osxportableapps.sourceforge.net/chromium/chrcast.xml'
   homepage 'http://www.freesmug.org/chromium'
+  license :oss
 
   app 'Chromium.app'
 end
