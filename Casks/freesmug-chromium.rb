841748e07783919f41d291be93f6e10ab1f43621
Magnus Öman <magnus@nani.se>
2014-11-29 22:12:43 +0100

    Re-add the FreeSMUG Chromium build (latest version: 38.0.2125.122) as a separate cask, which has also been adjusted to carry the new format

---
+++ Casks/freesmug-chromium.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'freesmug-chromium' do
+  version '38.0.2125.122'
+  sha256 '9c1b956f83b1e62a877389ecbb46a662cb9fda5e79f8d33673f27b70d76fba00'
+
+  url "https://downloads.sourceforge.net/sourceforge/osxportableapps/ChromiumOSX_#{version}.dmg"
+  appcast 'http://osxportableapps.sourceforge.net/chromium/chrcast.xml'
+  homepage 'http://www.freesmug.org/chromium'
+
+  app 'Chromium.app'
+end
