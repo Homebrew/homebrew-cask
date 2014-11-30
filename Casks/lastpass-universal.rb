d9be0ddab1646867a88582e0209060afd218b6a2
Josh Hagins <hagins.josh@gmail.com>
2014-11-30 03:18:13 -0500

    Update LastPass universal installer
    LastPass now uses a .zip file with an installer app rather than
    a .dmg with a .pkg installer.

--- Casks/lastpass-universal.rb
+++ Casks/lastpass-universal.rb
@@ -2,11 +2,11 @@ cask :v1 => 'lastpass-universal' do
   version :latest
   sha256 :no_check
 
-  url 'https://lastpass.com/lpmacosx.dmg'
+  url 'https://lastpass.com/download/cdn/lpmacosx.zip'
   homepage 'https://lastpass.com/'
   license :unknown
 
-  pkg 'lpmacosx.pkg'
+  installer :manual => 'LastPass Installer.app'
 
-  uninstall :pkgutil => 'com.lastpass.lpmacosx'
+  uninstall :script => 'Uninstaller.app/Contents/Resources/uninstall.sh'
 end
