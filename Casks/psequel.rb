ef56662a501714a56d13b5eaca31141184abc323
Stian Håklev <paul@paulmillr.com>
2014-12-01 17:57:30 -0500

    Add PSequel.app latest

---
+++ Casks/psequel.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'psequel' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.psequel.com/download'
+  homepage 'http://www.psequel.com'
+  license :unknown
+
+  app 'PSequel.app'
+end
