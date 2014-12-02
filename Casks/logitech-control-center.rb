b2ab8bc188639c8ca80d7273f29fcdeeb3396992
Larry Gilbert <larry@L2G.to>
2014-12-02 12:38:43 -0800

    logitech-control-center: add legacy version for OS X <= 10.5
    I've inspected the contents and it looks like all package IDs etc.
    remain the same.

--- Casks/logitech-control-center.rb
+++ Casks/logitech-control-center.rb
@@ -1,9 +1,16 @@
 cask :v1 => 'logitech-control-center' do
-  version '3.9.1.b20'
-  sha256 'e2c938286c4044bc6b83a7455f659e99d5854572d308cd6a9befd39eaed57d6c'
-  depends_on :macos => %w(10.6 10.7 10.8 10.9)
+  depends_on :macos => '<= 10.9'
 
-  url "http://www.logitech.com/pub/techsupport/mouse/mac/lcc#{version}.zip"
+  if MacOS.version <= '10.5'
+    version '3.5.1.23'
+    sha256 'b0b944edcb7549ff94d150d7caf72fb662fe825e3c829642c242180f4478d1ca'
+    url 'http://www.logitech.com/pub/techsupport/mouse/mac/lcc351.zip'
+  else
+    version '3.9.1.b20'
+    sha256 'e2c938286c4044bc6b83a7455f659e99d5854572d308cd6a9befd39eaed57d6c'
+    url "http://www.logitech.com/pub/techsupport/mouse/mac/lcc#{version}.zip"
+  end
+
   homepage 'http://support.logitech.com/en_us/product/3129'
   license :unknown
 
