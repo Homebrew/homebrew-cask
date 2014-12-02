eafc134eb322377d6921bba9e802041f57d9d168
Sebastian Nash <sebnash@gmail.com>
2014-12-02 17:36:32 +0000

    Add Typed.app v1.0
    Adding Realmac Software's new Typed for Mac markdown editor.

---
+++ Casks/typed.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'typed' do
+  version '1.0'
+  sha256 'df277c26b7a86fb770bb89a2499c4793da51bc9c3f50e3ed4427dd56f029e1f4'
+
+  url 'http://dl.devmate.com/com.realmacsoftware.typed/Typed.zip'
+  homepage 'http://realmacsoftware.com/typed'
+  license :unknown
+
+  app 'Typed.app'
+end
