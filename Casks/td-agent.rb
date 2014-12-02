7b6d1aa8d725553c8e3ad1b2e1302060e30ebfa1
n0ts <naoya.n@gmail.com>
2014-12-02 14:40:49 +0900

    Update td-agent version 2.1.1

--- Casks/td-agent.rb
+++ Casks/td-agent.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'td-agent' do
-  version '2.0.3'
-  sha256 '7e730682879dc199cc712205f22ba93fb9bee20a5dad16b113c8f32d11109cb8'
+  version '2.1.1'
+  sha256 '388f418e9bd7a2314335eb37bc62b5c2cc1b61fdf1f1d29f74a99dc020f486bb'
 
   url "http://packages.treasuredata.com/2/macosx/td-agent-#{version}-0.dmg"
   homepage 'http://www.fluentd.org/'
   license :unknown
 
-  pkg 'td-agent2.pkg'
+  pkg "tdagent-#{version}-0.pkg"
 
   uninstall :pkgutil => 'test.treasuredatainc.pkg.tdagent2',
             :launchctl => 'td-agent'
