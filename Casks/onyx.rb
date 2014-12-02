ce36c0232e701850750233c8a8ed6f094945fa31
Amory Meltzer <Amorymeltzer@gmail.com>
2014-12-01 16:51:29 -0800

    Fix download links for Onyx on Mountain Lion, Mavericks, and Yosemite; Add tests and links for Onyx under Tiger and Leopard

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -2,16 +2,20 @@ cask :v1 => 'onyx' do
   version :latest
   sha256 :no_check
 
-  if MacOS.version == :snow_leopard
+  if MacOS.version == :tiger
+    url 'http://www.titanium.free.fr/download/104/OnyX.dmg'
+  elsif MacOS.version == :leopard
+    url 'http://www.titanium.free.fr/download/105/OnyX.dmg'
+  elsif MacOS.version == :snow_leopard
     url 'http://www.titanium.free.fr/download/106/OnyX.dmg'
   elsif MacOS.version == :lion
     url 'http://www.titanium.free.fr/download/107/OnyX.dmg'
   elsif MacOS.version == :mountain_lion
-    url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
+    url 'http://www.titanium.free.fr/download/108/OnyX.dmg'
   elsif MacOS.version == :mavericks
-    url 'http://joel.barriere.pagesperso-orange.fr/dl/109/OnyX.dmg'
+    url 'http://www.titanium.free.fr/download/109/OnyX.dmg'
   else
-    url 'http://joel.barriere.pagesperso-orange.fr/dl/1010/OnyX.dmg'
+    url 'http://www.titanium.free.fr/download/1010/OnyX.dmg'
   end
   homepage 'http://www.titanium.free.fr/downloadonyx.php'
   license :unknown
