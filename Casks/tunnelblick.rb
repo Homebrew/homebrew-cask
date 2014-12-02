d211a84b841781fa26b2501722520cf544af8fc5
Larry Gilbert <larry@L2G.to>
2014-12-02 10:25:43 -0800

    tunnelblick: clean up whitespace

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -3,7 +3,6 @@ cask :v1 => 'tunnelblick' do
   sha256 '19cd879009d8f6989abd1f7e1ed1feb5dc68995f3146e08c547d4b64d3679c5b'
 
   url "https://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_#{version}.dmg"
-
   appcast 'https://www.tunnelblick.net/appcast.rss',
           :sha256 => '7fa119cda4d782dc61cb75895c70b3572652df737c908270c48a09d67a874592'
   homepage 'https://code.google.com/p/tunnelblick/'
