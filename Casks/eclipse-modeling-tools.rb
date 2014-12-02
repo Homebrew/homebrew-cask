58bee1bbef78e262dbecbcf921eee3dcd6b61f20
Leo Jweda <LeoJweda@Aluminum.local>
2014-12-02 11:17:18 -0500

    Add Eclipse Modeling Tools v4.4.0

---
+++ Casks/eclipse-modeling-tools.rb
@@ -0,0 +1,16 @@
+cask :v1 => 'eclipse-modeling-tools' do
+  version '4.4.0'
+
+  if Hardware::CPU.is_32_bit?
+    sha256 'df00541d1291c9f4dda6917ec8f07f22b1cad645f9641be53d9988a86528099d'
+    url 'http://download.eclipse.org/technology/epp/downloads/release/luna/SR1/eclipse-modeling-luna-SR1-macosx-cocoa.tar.gz'
+  else
+    sha256 'ad09c657dd1795eee42f1940473ce389168f819ecff7ea022ae0b9517e37f7be'
+    url 'http://download.eclipse.org/technology/epp/downloads/release/luna/SR1/eclipse-modeling-luna-SR1-macosx-cocoa-x86_64.tar.gz'
+  end
+
+  homepage 'http://eclipse.org/'
+  license :unknown
+
+  app 'eclipse/Eclipse.app'
+end
