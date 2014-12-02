4782909ba65e9a6b3df6ecdbb786500049a4daa5
Larry Gilbert <larry@L2G.to>
2014-12-02 12:27:56 -0800

    logitech-control-center: depends on specific Mac OS versions

--- Casks/logitech-control-center.rb
+++ Casks/logitech-control-center.rb
@@ -1,6 +1,7 @@
 cask :v1 => 'logitech-control-center' do
   version '3.9.1.b20'
   sha256 'e2c938286c4044bc6b83a7455f659e99d5854572d308cd6a9befd39eaed57d6c'
+  depends_on :macos => %w(10.6 10.7 10.8 10.9)
 
   url "http://www.logitech.com/pub/techsupport/mouse/mac/lcc#{version}.zip"
   homepage 'http://support.logitech.com/en_us/product/3129'
