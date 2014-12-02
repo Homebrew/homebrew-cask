02bfe34149f5828d285cbb8853f0dcc291c6465e
Vincent Klaiber <vincent@schimpanz.com>
2014-12-02 22:49:32 +0100

    Update Bowery.app to version 3.3.1

--- Casks/bowery.rb
+++ Casks/bowery.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bowery' do
-  version '3.3.0'
-  sha256 'b10ed33caaab237f8ba847325ed0b2f82ef6eaac856b32d018bf4d03add20bb0'
+  version '3.3.1'
+  sha256 '7ebe18bd25df7156027e5be857528a20c86ca8251a94d437086c77c6fb8b1f73'
 
   url "http://desktop.bowery.io/#{version}_darwin_amd64.zip"
   homepage 'http://bowery.io/'
