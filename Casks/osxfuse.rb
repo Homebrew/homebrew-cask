a905eab02e956671372aeb725fb9a0254b8ed075
Johnathan Conley <johnathan.conley@gmail.com>
2014-12-02 21:47:53 -0600

    Upgrade Osxfuse to 2.7.3

--- Casks/osxfuse.rb
+++ Casks/osxfuse.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'osxfuse' do
-  version '2.7.2'
-  sha256 '40ebab0919c6ae34221c51815830bc5eefe5f286619eeee85e2b7cb8df266d55'
+  version '2.7.3'
+  sha256 '88d0594e46191aeafa259535398d25c4ccfa03c178e10d925a69e4dcb6293fa2'
 
   url "https://downloads.sourceforge.net/project/osxfuse/osxfuse-#{version}/osxfuse-#{version}.dmg"
   homepage 'https://osxfuse.github.io/'
   license :oss
 
-  pkg "Install OSXFUSE #{version[0..-3]}.pkg"
+  pkg "Install OSXFUSE #{version.to_f}.pkg"
 
   uninstall :pkgutil => 'com.github.osxfuse.pkg.Core|com.github.osxfuse.pkg.PrefPane',
             :kext => 'com.github.osxfuse.filesystems.osxfusefs'
