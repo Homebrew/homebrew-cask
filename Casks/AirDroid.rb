ae10b2482b5e5cdd56eb0ad5710795faaca51af7
Thurston Sandberg <thurston@crowdspots.com>
2014-12-03 15:28:42 -0800

    added AirDroid

---
+++ Casks/AirDroid.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'AirDroid' do
+  version '3.0.0'
+  sha256 '85e21a3d111a0e0f151091b1bd920b38135c32152ec09a5a15f9572ee16bb1be'
+
+  url 'dl.airdroid.com/AirDroid_Desktop_Client_3.0.0.dmg'
+  homepage 'airdroid.com'
+  license :closed
+
+  app 'AirDroid.app'
+end
