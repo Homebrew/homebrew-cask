a73da43c971f7b5de06139dc2fd55f48a62bf75b
Vítor Galvão <info@vitorgalvao.com>
2014-12-03 19:47:39 +0000

    added "official download host" message to myo-connect.rb

--- Casks/myo-connect.rb
+++ Casks/myo-connect.rb
@@ -2,6 +2,7 @@ cask :v1 => 'myo-connect' do
   version '0.7.0'
   sha256 '61924ae6a813f33328f6275d37047ba7379c503d52be323e177b6f44220ce7fa'
 
+  # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/thalmicdownloads/mac/#{version}/MyoConnect.dmg"
   homepage 'https://developer.thalmic.com'
   license :unknown
