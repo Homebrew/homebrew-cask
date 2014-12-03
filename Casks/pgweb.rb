bd389ec9c80a3ff740c9f787533e9b5ceea0da34
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-12-03 14:40:24 -0600

    Update pgweb to 0.4.1

--- Casks/pgweb.rb
+++ Casks/pgweb.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pgweb' do
-  version '0.4.0'
-  sha256 'dac59afaef75db663e0f2a30ec035806b205a9acbe1e2a7a156b3e58dc571ea7'
+  version '0.4.1'
+  sha256 'd43493cf2d2255f9adc103fa88f9d714ff33ecc8988581d6b01425a395404eff'
 
   url "https://github.com/sosedoff/pgweb/releases/download/v#{version}/pgweb_darwin_amd64.zip"
   homepage 'https://github.com/sosedoff/pgweb'
