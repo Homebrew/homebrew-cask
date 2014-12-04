3e9e816221d59dd1ac1053d08df782d35b1951e8
Vítor Galvão <info@vitorgalvao.com>
2014-12-04 16:35:34 +0000

    factor.rb: updated to 0.98, with binary stanza
    Closes #7766.

--- Casks/factor.rb
+++ Casks/factor.rb
@@ -1,14 +1,11 @@
 cask :v1 => 'factor' do
-  version '0.97'
-  sha256 '218f21005a6aeef404365da87b0aabd424387f393df120ca98b50fda5db8a674'
+  version '0.98'
+  sha256 'to be defined'
 
   url "http://downloads.factorcode.org/releases/#{version}/factor-macosx-x86-64-#{version}.dmg"
   homepage 'http://factorcode.org/'
   license :bsd
 
   app 'factor/Factor.app'
-
-  caveats do
-    path_environment_variable "#{staged_path}/factor/"
-  end
+  binary 'factor/Factor.app/Contents/MacOS/factor'
 end
