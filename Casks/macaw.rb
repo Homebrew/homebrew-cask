18feaa3c5cff8add31071c09b359548323a6b0d5
Vítor Galvão <info@vitorgalvao.com>
2014-12-04 19:59:20 +0000

    updated macaw (1.5.9)

--- Casks/macaw.rb
+++ Casks/macaw.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'macaw' do
-  version '1.5.8'
-  sha256 '9c4350f6035823407a45c0ef465c10ff7bb8ec831ace69d2febc234babfc7a2a'
+  version '1.5.9'
+  sha256 '390a052022e92289a9c8ef2973e91d7f6928ff88eb3702f0feebe52c8036a717'
 
   url "http://download.macaw.co/#{version}/Macaw#{version}.dmg"
   appcast 'http://download.macaw.co/appcast.xml',
