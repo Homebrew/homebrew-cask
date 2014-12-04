fac6af93f72eb3ee0c3658ea0e88329e7c1ad2b8
Roland Walker <walker@pobox.com>
2014-12-04 15:29:21 -0500

    be explicit: no fallthrough on deeper.rb
    each version is specific to an OS X release

--- Casks/deeper.rb
+++ Casks/deeper.rb
@@ -14,8 +14,11 @@ cask :v1 => 'deeper' do
     url 'http://www.titanium.free.fr/download/108/Deeper.dmg'
   elsif MacOS.version == :mavericks
     url 'http://www.titanium.free.fr/download/109/Deeper.dmg'
-  else
+  elsif MacOS.version == :yosemite
     url 'http://www.titanium.free.fr/download/1010/Deeper.dmg'
+  else
+    # Unusual case: there is no fall-through.  Each version of the software is
+    # specific to an OS X release, so define nothing when the release is unknown.
   end
 
   homepage 'http://www.titanium.free.fr/downloaddeeper.php'
