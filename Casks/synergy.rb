30656017cfaf8a35ce7c7b65d094c86b5b41b952
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2014-12-04 12:35:27 -0200

    Update synergy project information

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -3,8 +3,8 @@ cask :v1 => 'synergy' do
   sha256 '259dd8367e0d9b6152e93d50854687d54714f649b6b018949621ba2194b3f987'
 
   url "http://synergy-project.org/files/packages/synergy-#{version}-MacOSX1010-x86_64.dmg"
-  homepage 'http://synergy-foss.org/'
-  license :unknown
+  homepage 'http://synergy-project.org'
+  license :oss
 
   app 'Synergy.app'
 end
