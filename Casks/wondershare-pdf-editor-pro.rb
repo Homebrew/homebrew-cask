94c8e1236df76549de0fb8fd1f76e07c48c13803
Chad Phillips <chad@apartmentlines.com>
2014-12-05 12:30:24 -0600

    Add Wondershare PDF Editor Pro.app v3.7.9

---
+++ Casks/wondershare-pdf-editor-pro.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'wondershare-pdf-editor-pro' do
+  version '3.7.9'
+  sha256 'de1a80e9e851ba5daa30894092cf8372f314ed6363f91e53307bfdb549cd18df'
+
+  url 'http://download.wondershare.com/mac-pdf-editor-pro_full841.dmg'
+  homepage 'http://www.wondershare.com/mac-pdf-editor'
+  license :commercial
+
+  app 'Wondershare PDF Editor Pro.app'
+end
