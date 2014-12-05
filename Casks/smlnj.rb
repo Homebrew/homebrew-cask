dccd90fa80fdd85d99b149baa17336da971984dc
Roland Walker <walker@pobox.com>
2014-12-04 19:31:07 -0500

    remove tags :name in smlnj

--- Casks/smlnj.rb
+++ Casks/smlnj.rb
@@ -3,9 +3,10 @@ cask :v1 => 'smlnj' do
   sha256 '77265ce1bdbca3e9c9b3053195503bf2bffafbba196596679fb64d9ceb4e25ee'
 
   url "http://smlnj.cs.uchicago.edu/dist/working/#{version}/smlnj-x86-#{version}.pkg"
+  # todo
+  # name 'Standard ML New Jersey'
   homepage 'http://www.smlnj.org/'
   license :oss
-  tags :name => 'Standard ML New Jersey'
 
   pkg "smlnj-x86-#{version}.pkg"
 
