a13a705b2703e5ddb89e605054dcf36aba3f6168
tim bacon <prime8@users.noreply.github.com>
2014-12-06 13:22:50 -0500

    Upgrade org.nodejs pkg to 0.10.33

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'node' do
-  version '0.10.32'
-  sha256 '3ce16fa81f47da38bcf726ef384e1bc0658d52f880d17aa17fd5b613bea035cf'
+  version '0.10.33'
+  sha256 '6f1ab706d6b5b960d7ce4a8a0edba2774b16757fe15dcc7ecb07c5464975d71f'
 
   url "http://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   homepage 'http://nodejs.org'
