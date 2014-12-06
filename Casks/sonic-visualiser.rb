7ce7928e7b9932fd9f0fb8c5fa587e5f28816c5f
Bennett Blodinger <benwa@users.noreply.github.com>
2014-12-06 14:30:23 -0500

    Update sonic-visualiser.rb
    Bump to 2.4.1 release 2014-10-02

--- Casks/sonic-visualiser.rb
+++ Casks/sonic-visualiser.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'sonic-visualiser' do
-  version '2.3'
-  sha256 '8a8c6c5268f10dd6614afeabd40a4fa962a31d55db0aed1ddf369c5a5b8b4981'
+  version '2.4.1'
+  sha256 '45edf5021376ed79e3a7a9a17745261f91362c05d90db17dc3179b1649c29332'
 
-  url "https://code.soundsoftware.ac.uk/attachments/download/907/Sonic%20Visualiser-#{version}.dmg"
+  url "https://code.soundsoftware.ac.uk/attachments/download/1186/Sonic%20Visualiser-#{version}.dmg"
   homepage 'http://www.sonicvisualiser.org/'
   license :unknown
 
