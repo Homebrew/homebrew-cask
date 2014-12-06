1bc469c450048ef3d9b233a5f13df0c73b241940
Mark Steve Samson <hello@marksteve.com>
2014-12-06 12:15:45 +0800

    Add PikoPixel.app v1.0b4

---
+++ Casks/pikopixel.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'pikopixel' do
+  version '1.0b4'
+  sha256 'bf024db394bfd6031f02f75313a591f949decea0cad80ec07357c0f32e34fa92'
+
+  url "http://twilightedge.com/downloads/PikoPixel#{version}.dmg"
+  homepage 'http://twilightedge.com/mac/pikopixel/index.html'
+  license :unknown
+
+  app 'PikoPixel.app'
+end
