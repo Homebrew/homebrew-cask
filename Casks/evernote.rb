5a4e7df6b721725eac65c08c76c2e6fa1654ff00
Basil Shubin <basil.shubin@gmail.com>
2014-12-07 13:42:19 +0600

    enable latest Snow Leopard compatible version

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -1,9 +1,15 @@
 cask :v1 => 'evernote' do
-  version :latest
-  sha256 :no_check
+  if MacOS.version < :lion
+    version '5.5.2'
+    sha256 '06b6da6d74ccab08deabfdd4c9519b9bc7f7ef0f0db2a0e8b0cd72e781f2e0ed'
+    url 'http://cdn1.evernote.com/mac/release/Evernote_402634.dmg'
+  else
+    version :latest
+    sha256 :no_check
+    url 'https://www.evernote.com/about/download/get.php?file=EvernoteMac'
+    appcast 'http://update.evernote.com/public/ENMac/EvernoteMacUpdate.xml'
+  end
 
-  url 'https://www.evernote.com/about/download/get.php?file=EvernoteMac'
-  appcast 'http://update.evernote.com/public/ENMac/EvernoteMacUpdate.xml'
   homepage 'https://evernote.com/'
   license :unknown
 
