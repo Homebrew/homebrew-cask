31c20ba6ef5a6962c5d1f181f4da32dbce34501e
Jussi Tuominen <Jussi@COLDwithin.local>
2014-12-07 17:23:12 +0200

    Add GyazMail.app v1.5.16

---
+++ Casks/gyazmail.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'gyazmail' do
+  version '1.5.16'
+  sha256 'fe15be1217dea0fa1eac39dd92be29073723b64b29fefa34ee9cdd3754725a4d'
+
+  url 'http://gyazsquare.com/gyazmail/GyazMail-1516.dmg'
+  homepage 'http://gyazsquare.com/'
+  license :commercial
+
+  app 'GyazMail.app'
+end
