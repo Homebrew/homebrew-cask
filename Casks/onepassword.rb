31c783fd45430a24dc4e4014b79d3c5c19970d02
Basil Shubin <basil.shubin@gmail.com>
2014-12-07 11:25:37 +0600

    enable Snow Leopard / Lion compatible version

--- Casks/onepassword.rb
+++ Casks/onepassword.rb
@@ -1,6 +1,10 @@
 cask :v1 => 'onepassword' do
 
-  if MacOS.version < :yosemite
+  if MacOS.version < :mountain_lion
+    version '3.8.22'
+    sha256 '3afd75f1bddf791dc7dbc9a7d92ab6eb91ee891407d750cedb7b5aff5fe8bf17'
+    app '1Password.app'
+  elsif MacOS.version < :yosemite
     version '4.4.3'
     sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
     app '1Password 4.app'
@@ -10,7 +14,11 @@ cask :v1 => 'onepassword' do
     app '1Password 5.app'
   end
 
-  url "http://i.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
+  if MacOS.version < :mountain_lion
+    url "http://i.agilebits.com/dist/1P/mac/1Password-#{version}.zip"
+  else
+    url "http://i.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
+  end
   homepage 'https://agilebits.com/onepassword'
   license :commercial
 
