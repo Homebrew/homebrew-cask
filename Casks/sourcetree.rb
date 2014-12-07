8b837af17bc28dd8e82a97148610ffe223a8e352
Basil Shubin <basil.shubin@gmail.com>
2014-12-07 12:11:58 +0600

    enable Snow Leopard compatible version

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,6 +1,12 @@
 cask :v1 => 'sourcetree' do
-  version '2.0.3'
-  sha256 '66f7f8186ff8afa1e8d26fc26bd55ad73380de3d5d9aa5985dfa6b7d143dd0ec'
+
+  if MacOS.version < :lion
+    version '1.8.1'
+    sha256 '37a42f2d83940cc7e1fbd573a70c3c74a44134c956ac3305f6b153935dc01b80'
+  else
+    version '2.0.3'
+    sha256 '66f7f8186ff8afa1e8d26fc26bd55ad73380de3d5d9aa5985dfa6b7d143dd0ec'
+  end
 
   url "https://downloads.atlassian.com/software/sourcetree/SourceTree_#{version}.dmg"
   appcast 'http://www.sourcetreeapp.com/update/SparkleAppcast.xml',
