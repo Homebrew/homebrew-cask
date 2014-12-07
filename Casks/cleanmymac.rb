ec7997cecc96f8b3abb2d762f19a1f11d0e08dfb
Basil Shubin <basil.shubin@gmail.com>
2014-12-07 13:16:02 +0600

    snow leopard compatible cleanmymac.rb
    enable "classic" version

--- Casks/cleanmymac.rb
+++ Casks/cleanmymac.rb
@@ -1,11 +1,19 @@
 cask :v1 => 'cleanmymac' do
-  version :latest
-  sha256 :no_check
 
-  url 'http://dl.devmate.com/com.macpaw.CleanMyMac2/CleanMyMac2.dmg'
-  appcast 'http://updates.devmate.com/com.macpaw.CleanMyMac2.xml'
+  if MacOS.version <= :snow_leopard
+    version :latest
+    sha256 :no_check
+    url 'http://dl.devmate.com/com.macpaw.CleanMyMac/CleanMyMacClassic.dmg'
+    appcast 'http://updates.devmate.com/com.macpaw.CleanMyMac.xml'
+    app 'CleanMyMac.app'
+  else
+    version :latest
+    sha256 :no_check
+    url 'http://dl.devmate.com/com.macpaw.CleanMyMac2/CleanMyMac2.dmg'
+    appcast 'http://updates.devmate.com/com.macpaw.CleanMyMac2.xml'
+    app 'CleanMyMac 2.app'
+  end
+
   homepage 'http://macpaw.com/cleanmymac'
   license :commercial
-
-  app 'CleanMyMac 2.app'
 end
