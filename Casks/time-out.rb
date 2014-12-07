5f06305779a284ec00b92be4dc88de46e35d32a8
Basil Shubin <basil.shubin@gmail.com>
2014-12-07 11:44:34 +0600

    enable Leopard / Snow Leopard compatible version

--- Casks/time-out.rb
+++ Casks/time-out.rb
@@ -1,10 +1,17 @@
 cask :v1 => 'time-out' do
-  version '1.7.1'
-  sha256 '3c9892344c8313b8ccf0a76cceb00834ddbe26e5114bcd674c4fd53aeb44e310'
+
+  if MacOS.version < :lion
+    version '1.6.5'
+    sha256 '2ceec8d8938af0acf32af601337c129087bb3b8c132c1f18e6eb3581788b8a49'
+    app 'Time Out.app'
+  else
+    version '1.7.1'
+    sha256 '3c9892344c8313b8ccf0a76cceb00834ddbe26e5114bcd674c4fd53aeb44e310'
+    app 'Time Out.app'
+  end
 
   url "http://www.dejal.com/download/timeout-#{version}.zip"
   homepage 'http://www.dejal.com/timeout/'
   license :unknown
 
-  app 'Time Out.app'
 end
