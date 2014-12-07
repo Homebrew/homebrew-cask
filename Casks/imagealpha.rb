45c494e29168d63e10d6367e755250a91afdd03d
Basil Shubin <basil.shubin@gmail.com>
2014-12-07 13:02:53 +0600

    enable Snow Leopard compatible version

--- Casks/imagealpha.rb
+++ Casks/imagealpha.rb
@@ -1,6 +1,11 @@
 cask :v1 => 'imagealpha' do
-  version '1.3.5'
-  sha256 'ba1ca1f50fa5b2a3472e6c2dbe410ecc3a66f425975cf9e39380e5c3421cca49'
+  if MacOS.version < :lion
+    version '1.2.3'
+    sha256 '79388edcaf5cb29234f722a672b069c6d51fb812e89969ba8db8e957b9a32bf3'
+  else
+    version '1.3.5'
+    sha256 'ba1ca1f50fa5b2a3472e6c2dbe410ecc3a66f425975cf9e39380e5c3421cca49'
+  end
 
   url "http://pngmini.com/ImageAlpha#{version}.tar.bz2"
   appcast 'http://pngmini.com/appcast.xml',
