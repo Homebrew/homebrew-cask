2c7ac7efda7c32fb389fcf329b21bf9b07595cf6
Roland Walker <walker@pobox.com>
2014-12-08 08:55:33 -0500

    use <= instead of < in evernote OS conditional
    marginally clearer to read

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -1,5 +1,6 @@
 cask :v1 => 'evernote' do
-  if MacOS.version < :lion
+
+  if MacOS.version <= :snow_leopard
     version '5.5.2'
     sha256 '06b6da6d74ccab08deabfdd4c9519b9bc7f7ef0f0db2a0e8b0cd72e781f2e0ed'
     url 'http://cdn1.evernote.com/mac/release/Evernote_402634.dmg'
