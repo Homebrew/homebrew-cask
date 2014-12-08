4b8f63f9f298e5b98631604518b9c8128ac15f7a
Vítor Galvão <info@vitorgalvao.com>
2014-12-08 16:50:29 +0000

    cookie.rb now uses :latest

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'cookie' do
-  version '3.3.4'
-  sha256 'd3cea7f3c4983d1e47fb0064beba6cd54a9a0f9fb9c077103e96068180769e74'
+  version :latest
+  sha256 :no_check
 
   url 'http://sweetpproductions.com/products/cookie/Cookie.dmg'
   homepage 'http://sweetpproductions.com/'
