47de1af0e066ddee00b859d7cc0c87a35c8fba44
Vítor Galvão <info@vitorgalvao.com>
2014-12-08 17:16:29 +0000

    moved appcast on scroll-reverser to else

--- Casks/scroll-reverser.rb
+++ Casks/scroll-reverser.rb
@@ -5,11 +5,11 @@ cask :v1 => 'scroll-reverser' do
   else
     version '1.6.1'
     sha256 'babce7e360ec76a797ac8843e20d5b75a0475807c487df87c01bc29cd8050584'
+    appcast 'http://softwareupdate.pilotmoon.com/update/scrollreverser/appcast.xml',
+            :sha256 => 'ed8e2d6748eb8b79897ca1b84a1597e86afb7845b469a278c83c07e08e6eb886'
   end
 
   url "https://d20vhy8jiniubf.cloudfront.net/downloads/ScrollReverser-#{version}.zip"
-  appcast 'http://softwareupdate.pilotmoon.com/update/scrollreverser/appcast.xml',
-          :sha256 => 'ed8e2d6748eb8b79897ca1b84a1597e86afb7845b469a278c83c07e08e6eb886'
   homepage 'https://pilotmoon.com/scrollreverser/'
   license :unknown
 
