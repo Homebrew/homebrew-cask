e18fe7e1b996ddd3ed6ecf47a78c0bbee8608874
Vítor Galvão <info@vitorgalvao.com>
2014-12-08 17:15:05 +0000

    simplified updated of dupin.rb

--- Casks/dupin.rb
+++ Casks/dupin.rb
@@ -2,7 +2,7 @@ cask :v1 => 'dupin' do
   version '2.8.2'
   sha256 '282116dbbfd75f73efe4655f21f42dd76720e71a8babea54f57a07e600396264'
 
-  url "http://dougscripts.com/itunes/scrx/dupinv#{version.gsub(/\.\d\.\d$/, '')}mv.zip"
+  url "http://dougscripts.com/itunes/scrx/dupinv#{version.to_i}mv.zip"
   homepage 'http://dougscripts.com/apps/dupinapp.php'
   license :commercial
 
