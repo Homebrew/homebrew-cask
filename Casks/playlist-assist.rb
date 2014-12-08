5d801ee7883c2bc72ac29f402eefcdeda1c72ef7
Vítor Galvão <info@vitorgalvao.com>
2014-12-08 17:13:55 +0000

    simplified updated of playlist-assist.rb

--- Casks/playlist-assist.rb
+++ Casks/playlist-assist.rb
@@ -2,7 +2,7 @@ cask :v1 => 'playlist-assist' do
   version '1.2.1'
   sha256 '70ef4c3cf78d39f8b1750ca44b6db42c3e72e24968ebd203fc146b2c189d5346'
 
-  url "http://dougscripts.com/itunes/scrx/playlistassistv#{version.gsub(/\.\d\.\d$/, '')}.zip"
+  url "http://dougscripts.com/itunes/scrx/playlistassistv#{version.to_i}.zip"
   homepage 'http://dougscripts.com/apps/playlistassistapp.php'
   license :commercial
 
