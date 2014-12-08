6393615fd78a3ca5a52dbd03a66db5bfe228bf58
Roland Walker <walker@pobox.com>
2014-12-08 08:54:22 -0500

    daisydisk depends_on :macos

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -1,4 +1,5 @@
 cask :v1 => 'daisydisk' do
+
   if MacOS.version == :snow_leopard
     version '2.1.2'
     sha256 'd0a606dee19e524d6fa7b79fd48b3b9865123ca4126fb8805f8e96c317b57b31'
@@ -15,6 +16,7 @@ cask :v1 => 'daisydisk' do
 
   app 'DaisyDisk.app'
 
+  depends_on :macos => '>= :snow_leopard'
   postflight do
     suppress_move_to_applications
   end
