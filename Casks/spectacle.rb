59c1d7aa6bfd58442a9915143f2475711e9765e0
Roland Walker <walker@pobox.com>
2014-12-09 07:25:25 -0500

    use inclusive conditional in spectacle

--- Casks/spectacle.rb
+++ Casks/spectacle.rb
@@ -1,5 +1,6 @@
 cask :v1 => 'spectacle' do
-  if MacOS.version < :mavericks
+
+  if MacOS.version <= :mountain_lion
     version '0.8.6'
     sha256 '3e367d2d7e6fe7d5f41d717d49cb087ba7432624b71ddd91c0cfa9d5a5459b7c'
   else
