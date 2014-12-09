6bd1ac68b2faa7580eb0ea817b359ce8a94e2a12
Roland Walker <walker@pobox.com>
2014-12-09 07:25:24 -0500

    use inclusive conditional in monolingual

--- Casks/monolingual.rb
+++ Casks/monolingual.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'monolingual' do
 
-  if MacOS.version < :leopard
+  if MacOS.version <= :tiger
     version '1.3.9'
     sha256 '7bf1ec8642ec8674443945dec12cf9c991306912e1e27c305c6db4776e037b16'
-  elsif MacOS.version < :lion
+  elsif MacOS.version <= :snow_leopard
     version '1.4.5'
     sha256 '7684b6b6d41b784d06e636f5e7993ca3730680ccbfa83e90e74b43be58ad3e21'
-  elsif MacOS.version < :yosemite
+  elsif MacOS.version <= :mavericks
     version '1.5.10'
     sha256 '2dd4d1164ced9c6ff431eb99db12e46faa10747c7753ed3889d6df493ee096a5'
   else
