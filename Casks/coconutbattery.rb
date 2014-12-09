13abe6915149c1301c410f1e47332a3806f3cf24
Roland Walker <walker@pobox.com>
2014-12-09 07:25:23 -0500

    use inclusive conditional in coconutbattery

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'coconutbattery' do
 
-  if MacOS.version < :leopard
+  if MacOS.version <= :tiger
     version '2.6.6'
     sha256 '8d235b237e42754ceda26af2babc160fd23f890d0fe6d7780b86a8e9c6effe42'
     url "http://www.coconut-flavour.com/downloads/coconutBattery_#{version}.zip"
-  elsif MacOS.version < :lion
+  elsif MacOS.version <= :snow_leopard
     version '2.8'
     sha256 'fcfc81214ff26afff9f5c6c7cdc455b23ac898b6918f864b641a9e31526692d4'
     url "http://www.coconut-flavour.com/downloads/coconutBattery_#{version}.zip"
