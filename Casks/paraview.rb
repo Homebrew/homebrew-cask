ce8cfb5c0b4bacfa792e654c386bc7b86c4edfba
Roland Walker <walker@pobox.com>
2014-12-09 07:25:24 -0500

    use inclusive conditional in paraview

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'paraview' do
   version '4.1.0'
 
-  if MacOS.version < :mavericks
+  if MacOS.version <= :mountain_lion
     sha256 '8784481c90b58b0c6158e21b7f978a7d78caa67c63d28d6d5d770ef43f0ad890'
     url "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.1&type=binary&os=osx&downloadFile=ParaView-#{version}-Darwin-64bit-Lion-Python27.dmg"
   else
