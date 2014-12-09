acfe827ad035f699ee197356781e444fa12b83cd
Roland Walker <walker@pobox.com>
2014-12-09 07:24:53 -0500

    use symbolic+inclusive conditional in julia.rb

--- Casks/juliastudio.rb
+++ Casks/juliastudio.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'juliastudio' do
   version '0.4.4'
 
-  if MacOS.version < '10.7'
+  if MacOS.version <= :snow_leopard
     sha256 '380006f00db5b19ab1a68b25b72dba669d7ff8f109c68d6ecf1fcc34dbfe485f'
     url "https://s3.amazonaws.com/cdn-common.forio.com/julia-studio/#{version}/julia-studio-mac10.6-installer-#{version}.dmg"
   else
