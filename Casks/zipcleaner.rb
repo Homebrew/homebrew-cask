aba56447b2e3ca64c92b9856b4031c059d17596a
Roland Walker <walker@pobox.com>
2014-12-10 08:51:26 -0500

    license todo comment in zipcleaner

--- Casks/zipcleaner.rb
+++ Casks/zipcleaner.rb
@@ -4,7 +4,7 @@ cask :v1 => 'zipcleaner' do
 
   url 'http://roger-jolly.nl/software/downloads/zipcleaner/ZipCleaner.zip'
   homepage 'http://roger-jolly.nl/software/#zipcleaner'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'ZipCleaner.app'
 end
