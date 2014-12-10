48d13077a7aa51cbf0cd2e759390a1157899c40c
Roland Walker <walker@pobox.com>
2014-12-10 08:50:17 -0500

    license todo comment in omnidisksweeper

--- Casks/omnidisksweeper.rb
+++ Casks/omnidisksweeper.rb
@@ -4,7 +4,7 @@ cask :v1 => 'omnidisksweeper' do
 
   url 'https://www.omnigroup.com/download/latest/OmniDiskSweeper'
   homepage 'http://www.omnigroup.com/products/omnidisksweeper/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'OmniDiskSweeper.app'
 end
