265f0106502e6cacc707b41803f3b4a8b22a1519
Roland Walker <walker@pobox.com>
2014-12-10 08:49:13 -0500

    license todo comment in dfontsplitter

--- Casks/dfontsplitter.rb
+++ Casks/dfontsplitter.rb
@@ -6,7 +6,7 @@ cask :v1 => 'dfontsplitter' do
   appcast 'http://apps.upfold.org.uk/appupdate/dfontsplitter.xml',
           :sha256 => 'b7a3ed81f5e160a4888afdb5810d97f5b2ed828ecec4569dc1d1a6bd825bcc59'
   homepage 'http://peter.upfold.org.uk/projects/dfontsplitter'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'DfontSplitter.app'
 end
