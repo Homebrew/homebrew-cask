5c1494011e41f1161e7e5fbcd8823eb2cc320020
Roland Walker <walker@pobox.com>
2014-12-10 08:49:07 -0500

    license todo comment in colorpicker-hex

--- Casks/colorpicker-hex.rb
+++ Casks/colorpicker-hex.rb
@@ -4,7 +4,7 @@ cask :v1 => 'colorpicker-hex' do
 
   url "http://wafflesoftware.net/hexpicker/download/HexColorPicker-#{version}.zip"
   homepage 'http://wafflesoftware.net/hexpicker/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   colorpicker 'Hex Color Picker/HexColorPicker.colorPicker'
 end
