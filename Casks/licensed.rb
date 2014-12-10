d7fcb546c554c6f481cbe245f8a93196910061ed
Roland Walker <walker@pobox.com>
2014-12-10 08:49:55 -0500

    license todo comment in licensed

--- Casks/licensed.rb
+++ Casks/licensed.rb
@@ -4,7 +4,7 @@ cask :v1 => 'licensed' do
 
   url 'http://amarsagoo.info/licensed/Licensed.dmg'
   homepage 'http://amarsagoo.info/licensed'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Licensed.app'
 end
