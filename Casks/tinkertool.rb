8b37c7df5fbd67b528ebfd180b1853e1697b202d
Roland Walker <walker@pobox.com>
2014-12-10 08:51:05 -0500

    license todo comment in tinkertool

--- Casks/tinkertool.rb
+++ Casks/tinkertool.rb
@@ -4,7 +4,7 @@ cask :v1 => 'tinkertool' do
 
   url 'http://dl.macupdate.com/prod/TinkerTool.dmg'
   homepage 'http://www.bresink.com/osx/TinkerTool.html'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'TinkerTool.app'
 end
