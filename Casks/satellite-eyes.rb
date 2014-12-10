99c09688d51b4f03e6b8d76e00b5c32fcd63e525
Kevin Wu <me@kevinformatics.com>
2014-12-09 18:09:52 -0800

    Upgrade Satellite Eyes.app to v1.4.0

--- Casks/satellite-eyes.rb
+++ Casks/satellite-eyes.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'satellite-eyes' do
-  version '1.2.4'
-  sha256 'adc1554c9095e3f316474e0f3712fa2a086d9405d126eca6359b8ebdc7451902'
+  version '1.4.0'
+  sha256 '9ef7e0e5e4ab5a341e4b7bfa2528e788caa2ad73d29949a305e1dfab94bb2574'
 
   url "https://satellite-eyes.s3.amazonaws.com/satellite-eyes-#{version}.zip"
   appcast 'https://satellite-eyes.s3.amazonaws.com/appcast.xml',
-          :sha256 => 'ef859a7550ea22ebbd91423e2336228a35e60bb901112cfe1f17c8f0481ccbf9'
+          :sha256 => '5853ce38284dd959729ea64a423c359d6f2e383be3f062b38825762dfb97860b'
   homepage 'http://satelliteeyes.tomtaylor.co.uk/'
   license :unknown
 
