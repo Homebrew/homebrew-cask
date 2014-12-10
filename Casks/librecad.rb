2d7dce6223536112d0491a3a0ac97a5900cdb878
Roland Walker <walker@pobox.com>
2014-12-10 11:55:20 -0500

    enable MacOS.release in librecad

--- Casks/librecad.rb
+++ Casks/librecad.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'librecad' do
   version '2.0.5'
 
-  if MacOS.version <= :mountain_lion
+  if MacOS.release <= :mountain_lion
     url "https://downloads.sourceforge.net/project/librecad/OSX/#{version}/LibreCAD-#{version}-MountainLion.dmg"
     sha256 '6f1f1fe3f49ac404608965c1d40e7a109b7f416b7b0624c4d8e11e72f01c7ef6'
   else
