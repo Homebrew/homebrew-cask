5418ed65785f61cb60a9d86c9c964ced3244fa13
Roland Walker <walker@pobox.com>
2014-12-10 08:49:19 -0500

    license todo comment in eloquent

--- Casks/eloquent.rb
+++ Casks/eloquent.rb
@@ -4,7 +4,7 @@ cask :v1 => 'eloquent' do
 
   url "https://launchpad.net/eloquent/trunk/#{version}/+download/Eloquent-#{version}.app.zip"
   homepage 'https://launchpad.net/eloquent'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Eloquent.app'
 
