17cd76e4165a9af41c43f572ff5c1c0baad1e014
Roland Walker <walker@pobox.com>
2014-12-10 08:50:04 -0500

    license todo comment in master-key

--- Casks/master-key.rb
+++ Casks/master-key.rb
@@ -5,7 +5,7 @@ cask :v1 => 'master-key' do
   url 'http://macinmind.com/MasterKey.dmg'
   appcast 'http://macinmind.com/pads/MasterKeyappcast.xml'
   homepage 'http://macinmind.com/?area=app&app=masterkey&pg=info'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Master Key.app'
 end
