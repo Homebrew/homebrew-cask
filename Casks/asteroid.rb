be3772d866b24a9cf3233f8d2af264d8a899b2cc
Roland Walker <walker@pobox.com>
2014-12-10 08:48:59 -0500

    license todo comment in asteroid

--- Casks/asteroid.rb
+++ Casks/asteroid.rb
@@ -5,7 +5,7 @@ cask :v1 => 'asteroid' do
   url 'http://www.asteroidapp.com/downloader.php?Ver=latest'
   appcast 'http://www.asteroidapp.com/appupdates.php'
   homepage 'http://www.asteroidapp.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Asteroid.app'
 end
