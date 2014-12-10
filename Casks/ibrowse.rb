8ff12a977f2ac0d46b8cb94b00dc4904d945c78e
Roland Walker <walker@pobox.com>
2014-12-10 08:49:41 -0500

    license todo comment in ibrowse

--- Casks/ibrowse.rb
+++ Casks/ibrowse.rb
@@ -4,7 +4,7 @@ cask :v1 => 'ibrowse' do
 
   url "http://cdn.macroplant.com/release/iBrowse-#{version}x.dmg"
   homepage 'http://www.ibrowseapp.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'iBrowse.app'
 end
