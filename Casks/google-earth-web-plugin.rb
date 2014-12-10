e492ab07579938e732323106af2f8094b2356b4a
Roland Walker <walker@pobox.com>
2014-12-10 08:49:33 -0500

    license todo comment in google-earth-web-plugin

--- Casks/google-earth-web-plugin.rb
+++ Casks/google-earth-web-plugin.rb
@@ -4,7 +4,7 @@ cask :v1 => 'google-earth-web-plugin' do
 
   url 'http://r2---sn-po4vapo3-j3ae.c.pack.google.com/edgedl/earth/plugin/current/googleearth-mac-plugin-intel.dmg'
   homepage 'http://www.google.com/intl/en/earth/explore/products/plugin.html'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   internet_plugin 'Google Earth Web Plug-in.plugin'
 end
