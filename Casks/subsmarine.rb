ca38e699d6ef6a7301098bc71f24fc9e4b445347
Roland Walker <walker@pobox.com>
2014-12-10 08:50:56 -0500

    license todo comment in subsmarine

--- Casks/subsmarine.rb
+++ Casks/subsmarine.rb
@@ -6,7 +6,7 @@ cask :v1 => 'subsmarine' do
   appcast 'http://www.cocoawithchurros.com/shine/appcast.php?id=7',
           :sha256 => '9659830b134ac12326639e1b3f7ba06945cc18db343890e06a4c7f9d9dce8912'
   homepage 'http://www.cocoawithchurros.com/subsmarine.php'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'SubsMarine.app'
 end
