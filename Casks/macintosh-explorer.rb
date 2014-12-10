e36e53db31638979da6f554e5a8187440f4a1ca5
Roland Walker <walker@pobox.com>
2014-12-10 08:50:00 -0500

    license todo comment in macintosh-explorer

--- Casks/macintosh-explorer.rb
+++ Casks/macintosh-explorer.rb
@@ -4,7 +4,7 @@ cask :v1 => 'macintosh-explorer' do
 
   url 'http://www.ragesw.com/downloads/ragesw/mac_explorer_alt.dmg'
   homepage 'http://www.ragesw.com/products/explorer.html'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Macintosh Explorer.app'
 end
