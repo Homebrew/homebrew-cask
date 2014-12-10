356d6333dde6055269c1680538c358cd711795f8
Roland Walker <walker@pobox.com>
2014-12-10 11:55:19 -0500

    enable MacOS.release in daisydisk

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'daisydisk' do
 
-  if MacOS.version == :snow_leopard
+  if MacOS.release == :snow_leopard
     version '2.1.2'
     sha256 'd0a606dee19e524d6fa7b79fd48b3b9865123ca4126fb8805f8e96c317b57b31'
     url "http://www.daisydiskapp.com/downloads/DaisyDisk_2_1_2.dmg"
