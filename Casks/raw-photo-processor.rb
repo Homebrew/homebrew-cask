332080bc7bdfc747d8c99b29e4cb71514f55f94b
Roland Walker <walker@pobox.com>
2014-12-10 08:50:34 -0500

    license todo comment in raw-photo-processor

--- Casks/raw-photo-processor.rb
+++ Casks/raw-photo-processor.rb
@@ -6,7 +6,7 @@ cask :v1 => 'raw-photo-processor' do
   appcast 'http://www.raw-photo-processor.com/rpp_updates.xml',
           :sha256 => '83e550582ec77d965383ba778ce68e0e0c3f71501a546fab8619119ec2825287'
   homepage 'http://www.raw-photo-processor.com/RPP/Overview.html'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   # note: this value changes with each version
   container :nested => 'RPP480_1703_64.dmg'
