51addc134f621f1391257b5ea8197748dddfe215
Roland Walker <walker@pobox.com>
2014-12-10 08:49:02 -0500

    license todo comment in cartao-de-cidadao

--- Casks/cartao-de-cidadao.rb
+++ Casks/cartao-de-cidadao.rb
@@ -4,7 +4,7 @@ cask :v1 => 'cartao-de-cidadao' do
 
   url "http://www.cartaodecidadao.pt/ccsoftware/Cartao_de_Cidadao_#{version}_LION_MOUNTAIN_LION_MAVERICKS.dmg"
   homepage 'http://www.cartaodecidadao.pt/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   pkg 'Cartao_de_Cidadao.pkg'
 
