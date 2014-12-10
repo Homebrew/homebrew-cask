68548cfa825c6a986afef3e3546d484a2cecad45
Roland Walker <walker@pobox.com>
2014-12-10 08:50:39 -0500

    license todo comment in sandbox

--- Casks/sandbox.rb
+++ Casks/sandbox.rb
@@ -4,7 +4,7 @@ cask :v1 => 'sandbox' do
 
   url 'http://www.mikey-san.net/sandbox/Sandbox.dmg'
   homepage 'http://www.mikey-san.net/sandbox/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Sandbox.app'
 end
