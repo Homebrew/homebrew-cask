0d8fb45ebddb862e5a0c52214419e5e55c317a4b
Roland Walker <walker@pobox.com>
2014-12-10 08:49:30 -0500

    license todo comment in ghostlab

--- Casks/ghostlab.rb
+++ Casks/ghostlab.rb
@@ -5,7 +5,7 @@ cask :v1 => 'ghostlab' do
   url 'http://awesome.vanamco.com/downloads/ghostlab/Ghostlab.dmg'
   appcast 'http://awesome.vanamco.com/update/ghostlab-cast.xml'
   homepage 'http://vanamco.com/ghostlab/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Ghostlab.app'
 end
