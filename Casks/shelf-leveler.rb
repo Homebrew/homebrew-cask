b3fadb00dc7906c8b61acb9e8c2f570f34176be8
Roland Walker <walker@pobox.com>
2014-12-10 08:50:44 -0500

    license todo comment in shelf-leveler

--- Casks/shelf-leveler.rb
+++ Casks/shelf-leveler.rb
@@ -4,7 +4,7 @@ cask :v1 => 'shelf-leveler' do
 
   url 'http://download.mrgeckosmedia.com/ShelfLeveler.zip'
   homepage 'https://mrgeckosmedia.com/applications/info/Shelf-Leveler'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Shelf Leveler.app'
 end
