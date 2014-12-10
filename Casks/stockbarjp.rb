7b5d3ec14d359c79029dd0801354d64411394cb6
Roland Walker <walker@pobox.com>
2014-12-10 08:50:56 -0500

    license todo comment in stockbarjp

--- Casks/stockbarjp.rb
+++ Casks/stockbarjp.rb
@@ -4,7 +4,7 @@ cask :v1 => 'stockbarjp' do
 
   url 'http://midnightsuyama.org/download/StockBarJP.zip'
   homepage 'http://midnightsuyama.org'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'StockBarJP.app'
 end
