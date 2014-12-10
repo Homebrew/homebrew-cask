fdc562fc0557e1d95b4a9367995f3eef8e684071
Roland Walker <walker@pobox.com>
2014-12-10 08:50:04 -0500

    license todo comment in mavensmate

--- Casks/mavensmate.rb
+++ Casks/mavensmate.rb
@@ -5,7 +5,7 @@ cask :v1 => 'mavensmate' do
   url 'http://push.mavensconsulting.netdna-cdn.com/mavensmate/builds/MavensMate.zip'
   appcast 'http://mavensconsulting.com/mavensmate/builds/appcast.xml'
   homepage 'http://mavensmate.com'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'MavensMate.app'
 end
