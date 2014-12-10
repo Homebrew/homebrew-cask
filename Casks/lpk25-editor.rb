b686c653e0ee0100d3bd6b73e919d09a092b97c9
Roland Walker <walker@pobox.com>
2014-12-10 08:49:58 -0500

    license todo comment in lpk25-editor

--- Casks/lpk25-editor.rb
+++ Casks/lpk25-editor.rb
@@ -4,7 +4,7 @@ cask :v1 => 'lpk25-editor' do
 
   url 'http://6be54c364949b623a3c0-4409a68c214f3a9eeca8d0265e9266c0.r0.cf2.rackcdn.com/453/downloads/lpk25_editor_mac_00.zip'
   homepage 'http://www.akaipro.com/index.php/product/lpk25'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'LPK25_Editor/LPK25 Editor.app'
 end
