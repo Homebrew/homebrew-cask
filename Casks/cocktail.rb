bf3fa46d07838989ecfb9e8442aaf05e29d4b62a
Roland Walker <walker@pobox.com>
2014-12-10 11:55:19 -0500

    enable MacOS.release in cocktail

--- Casks/cocktail.rb
+++ Casks/cocktail.rb
@@ -2,16 +2,16 @@ cask :v1 => 'cocktail' do
   version :latest
   sha256 :no_check
 
-  if MacOS.version == :snow_leopard
+  if MacOS.release == :snow_leopard
     url 'http://usa.maintain.se/CocktailSLE.dmg'
     appcast 'http://www.maintain.se/downloads/sparkle/snowleopard/snowleopard.xml'
-  elsif MacOS.version == :lion
+  elsif MacOS.release == :lion
     url 'http://usa.maintain.se/CocktailLionEdition.dmg'
     appcast 'http://www.maintain.se/downloads/sparkle/lion/lion.xml'
-  elsif MacOS.version == :mountain_lion
+  elsif MacOS.release == :mountain_lion
     url 'http://usa.maintain.se/CocktailMLE.dmg'
     appcast 'http://www.maintain.se/downloads/sparkle/mountainlion/mountainlion.xml'
-  elsif MacOS.version == :mavericks
+  elsif MacOS.release == :mavericks
     url 'http://usa.maintain.se/CocktailME.dmg'
     appcast 'http://www.maintain.se/downloads/sparkle/mavericks/mavericks.xml'
   else
