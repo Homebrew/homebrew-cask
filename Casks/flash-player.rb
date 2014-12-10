56c653aa2129b610b732db749d6a693c73790162
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-10 05:55:30 +0000

    flash 16.0.0.235
    Version bumps for the two Adobe Flash Player casks.

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'flash-player' do
-  version '15.0.0.239'
-  sha256 '496a608e363a4945c59b7790c5badc1b89d600b9321bb5bfbdc620accbf5811b'
+  version '16.0.0.235'
+  sha256 'df9820b39ade6bd530af4f5ab6fe2c95166d59f0d3b000bfd97c18f141b23294'
 
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.to_i}/flashplayer_#{version.to_i}_sa.dmg"
   homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'flash' do
-  version '15.0.0.239'
-  sha256 '6c0a22ad48df4960e240fc08f4913eb24775eca42e99dc3cc9a441bf1ea3e68a'
+  version '16.0.0.235'
+  sha256 'd47bdc510f35e35ecf5260e35f6c86a4750f95b8505841bc10e1b6a1af82d346'
 
   url "http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_#{version.to_i}_osx_pkg.dmg"
   homepage 'https://www.adobe.com/products/flashplayer/distribution3.html'
