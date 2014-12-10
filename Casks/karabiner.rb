0461c129b36fae9051103bce6af4751ce5a9305f
Roland Walker <walker@pobox.com>
2014-12-10 11:34:45 -0500

    enable name stanza in karabiner

--- Casks/karabiner.rb
+++ Casks/karabiner.rb
@@ -3,9 +3,8 @@ cask :v1 => 'karabiner' do
   sha256 'b1e3ffb7cd10ec8651c68b184c236589f182a273dad4b1c68ab2c15d36a34248'
 
   url "https://pqrs.org/osx/karabiner/files/Karabiner-#{version}.dmg"
-  # todo
-  # name 'Karabiner'
-  # name 'Keyremap4Macbook'
+  name 'Karabiner'
+  name 'KeyRemap4MacBook'
   homepage 'https://pqrs.org/osx/karabiner/'
   license :unknown    # todo: improve this machine-generated value
 
