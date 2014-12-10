95769f8456527af484316040e71fbd8b6365b39f
Roland Walker <walker@pobox.com>
2014-12-10 08:51:10 -0500

    license todo comment in ubuntu-one

--- Casks/ubuntu-one.rb
+++ Casks/ubuntu-one.rb
@@ -4,7 +4,7 @@ cask :v1 => 'ubuntu-one' do
 
   url 'https://one.ubuntu.com/download/mac/client'
   homepage 'http://one.ubuntu.com'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Ubuntu One.app'
 end
