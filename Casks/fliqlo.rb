b9ccd5e2a13be5de0759e1f86de111263fcfa468
Roland Walker <walker@pobox.com>
2014-12-10 08:49:25 -0500

    license todo comment in fliqlo

--- Casks/fliqlo.rb
+++ Casks/fliqlo.rb
@@ -4,7 +4,7 @@ cask :v1 => 'fliqlo' do
 
   url "http://fliqlo.com/download/fliqlo_#{version.gsub('.','')}.dmg", :referer => 'http://fliqlo.com/#about'
   homepage 'http://fliqlo.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   screen_saver 'Fliqlo.saver'
 end
