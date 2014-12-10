916ee0a8cb44dd0ce9b6f308c7143dd5717c9497
Amory Meltzer <Amorymeltzer@gmail.com>
2014-12-10 14:02:32 -0800

    Update mesasqlite homepage
    From http://www.desertsandsoftware.com to http://www.desertsandsoftware.com/wordpress/?page_id=17

--- Casks/mesasqlite.rb
+++ Casks/mesasqlite.rb
@@ -3,7 +3,7 @@ cask :v1 => 'mesasqlite' do
   sha256 '16741eb458127db0ce72e71824ddf938ee7b30a8b6ea4bc0e82357aab5b3359b'
 
   url "http://www.desertsandsoftware.com/DEMOS/MesaSQLite#{version.gsub('.','')}.zip"
-  homepage 'http://www.desertsandsoftware.com'
+  homepage 'http://www.desertsandsoftware.com/wordpress/?page_id=17'
   license :unknown    # todo: improve this machine-generated value
 
   app 'MesaSQLite.app'
