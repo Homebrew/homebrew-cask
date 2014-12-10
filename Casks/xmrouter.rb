7c7f1d09b20db5cacf1f21cc0c9a52f86a719292
Roland Walker <walker@pobox.com>
2014-12-10 08:51:21 -0500

    license todo comment in xmrouter

--- Casks/xmrouter.rb
+++ Casks/xmrouter.rb
@@ -4,7 +4,7 @@ cask :v1 => 'xmrouter' do
 
   url 'http://bigota.miwifi.com/xiaoqiang/client/xqmac_client.dmg'
   homepage 'http://www1.miwifi.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'XMRouter.app'
 end
