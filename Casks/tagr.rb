2f234769317982d2e311d1f1a5983c88258f7da4
Roland Walker <walker@pobox.com>
2014-12-10 08:50:59 -0500

    license todo comment in tagr

--- Casks/tagr.rb
+++ Casks/tagr.rb
@@ -6,7 +6,7 @@ cask :v1 => 'tagr' do
   appcast 'http://www.harald-schubert.net/data/tagr-appcast.xml',
           :sha256 => 'baa3012b4fc391b0f5f296e8fa804518cb8af103a4d6588ba774ce57a9607835'
   homepage 'http://www.entwicklungsfreu.de/tagr.html'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Tagr.app'
 end
