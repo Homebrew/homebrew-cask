ad81f825293d14abe946fba05375052e1e6f6cc0
Roland Walker <walker@pobox.com>
2014-12-10 11:34:45 -0500

    enable name stanza in laullon-gitx

--- Casks/laullon-gitx.rb
+++ Casks/laullon-gitx.rb
@@ -3,8 +3,7 @@ cask :v1 => 'laullon-gitx' do
   sha256 'c5f4088497abf5a219bb7bde4fae643fec61647be25bf836fd679567dcabd7df'
 
   url "https://github.com/downloads/laullon/gitx/GitX-L_v#{version}.zip"
-  # todo
-  # name 'GitX (L)'
+  name 'GitX (L)'
   appcast 'http://gitx.laullon.com/appcast.xml',
           :sha256 => '7ce5197de931145d75c57c2171fba559481e79e23bedd58ec107b476731f693b'
   homepage 'http://gitx.laullon.com/'
