c3434ccfd362e96d39175e91f4fd2d13bffd9e62
Roland Walker <walker@pobox.com>
2014-12-10 11:34:45 -0500

    enable name stanza in data-integration

--- Casks/data-integration.rb
+++ Casks/data-integration.rb
@@ -3,8 +3,7 @@ cask :v1 => 'data-integration' do
   sha256 :no_check
 
   url 'https://sourceforge.net/projects/pentaho/files/latest/download'
-  # todo
-  # name 'Pentaho Data Integration'
+  name 'Pentaho Data Integration'
   homepage 'http://community.pentaho.com'
   license :oss
   tags :vendor => 'Pentaho'
