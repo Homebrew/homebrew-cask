dc49a6ebc77cc3a55b28ebba12c5722fe9b501cf
Roland Walker <walker@pobox.com>
2014-12-10 08:51:16 -0500

    license todo comment in wedge

--- Casks/wedge.rb
+++ Casks/wedge.rb
@@ -5,7 +5,7 @@ cask :v1 => 'wedge' do
   url 'http://wedge.natestedman.com/release/Wedge.app.zip'
   appcast 'http://wedge.natestedman.com/appcast.xml'
   homepage 'http://wedge.natestedman.com'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Wedge.app'
 end
