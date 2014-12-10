0a8434258e22754acac41dfc1b8fe37bf9ff60fe
Roland Walker <walker@pobox.com>
2014-12-10 08:49:11 -0500

    license todo comment in darteditor

--- Casks/darteditor.rb
+++ Casks/darteditor.rb
@@ -4,7 +4,7 @@ cask :v1 => 'darteditor' do
 
   url 'https://storage.googleapis.com/dart-archive/channels/stable/release/latest/editor/darteditor-macos-x64.zip'
   homepage 'https://www.dartlang.org/tools/editor/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'dart/DartEditor.app'
 end
