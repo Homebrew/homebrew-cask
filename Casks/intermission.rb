eaadf5e838f8fa6efd75c9326a600d5081584752
Roland Walker <walker@pobox.com>
2014-12-10 08:49:45 -0500

    license todo comment in intermission

--- Casks/intermission.rb
+++ Casks/intermission.rb
@@ -4,7 +4,7 @@ cask :v1 => 'intermission' do
 
   url 'http://dm.rogueamoeba.com/mirror/files/Intermission.zip'
   homepage 'http://rogueamoeba.com/intermission/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Intermission.app'
 end
