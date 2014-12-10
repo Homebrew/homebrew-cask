b9541ee41741178ffabf39a7a52de4332f89341f
Roland Walker <walker@pobox.com>
2014-12-10 08:51:09 -0500

    license todo comment in tvmobili

--- Casks/tvmobili.rb
+++ Casks/tvmobili.rb
@@ -4,7 +4,7 @@ cask :v1 => 'tvmobili' do
 
   url 'http://www.tvmobili.com/binaries/stable/tvmobili-mountainlion-universal.dmg'
   homepage 'http://www.tvmobili.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   pkg 'tvmobili-mountainlion-universal.2.1.4481.pkg'
 
