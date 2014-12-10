6aff1845899f1b12ddde0f0461f8bd9c10aedfd6
Roland Walker <walker@pobox.com>
2014-12-10 08:50:15 -0500

    license todo comment in noiz2sa

--- Casks/noiz2sa.rb
+++ Casks/noiz2sa.rb
@@ -4,7 +4,7 @@ cask :v1 => 'noiz2sa' do
 
   url 'https://workram.com/downloads.php?f=Noiz2sa', :referer => 'https://workram.com/games/noiz2sa'
   homepage 'http://workram.com/games/noiz2sa/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Noiz2sa.app'
 end
