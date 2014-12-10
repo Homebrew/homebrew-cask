a2bdecd339e14dbe0f480d74871c9db10dd4fe7f
Roland Walker <walker@pobox.com>
2014-12-10 08:50:35 -0500

    license todo comment in recordit

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -5,7 +5,7 @@ cask :v1 => 'recordit' do
   url 'http://recordit.co/latest'
   appcast 'https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790'
   homepage 'http://recordit.co/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'RecordIt.app'
 end
