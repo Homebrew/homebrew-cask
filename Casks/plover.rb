232943c7e54bd27198f48512b3c5664da14796f6
Roland Walker <walker@pobox.com>
2014-12-10 11:49:45 -0500

    enable accessibility_access in plover

--- Casks/plover.rb
+++ Casks/plover.rb
@@ -8,13 +8,5 @@ cask :v1 => 'plover' do
 
   app 'plover.app'
 
-  # todo: replace with new assistive_devices stanza
-  caveats do
-    <<-EOS.undent
-      To use #{@cask}, you may need to give it access to assistive
-      devices (Accessibility).  For OS X Mavericks and Above:
-
-        System Preferences / Security & Privacy / Privacy / Accessibility
-    EOS
-  end
+  accessibility_access true
 end
