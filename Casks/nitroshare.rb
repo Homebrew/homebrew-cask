9bc3610ea62e2446c71f9fe6d762e71cb9e8a2db
Amory Meltzer <Amorymeltzer@gmail.com>
2014-12-10 14:05:05 -0800

    Update nitroshare homepage
    To https://launchpad.net/nitroshare

--- Casks/nitroshare.rb
+++ Casks/nitroshare.rb
@@ -3,7 +3,7 @@ cask :v1 => 'nitroshare' do
   sha256 '9f208d4b86ca13c4136265fa4a4693c7ef9ba9f0d6a0a84966cfd5f9934abe12'
 
   url "https://launchpad.net/nitroshare/#{version}/#{version}/+download/nitroshare_#{version}.app.dmg"
-  homepage 'https://quickmediasolutions.com/apps/14/nitroshare'
+  homepage 'https://launchpad.net/nitroshare'
   license :unknown    # todo: improve this machine-generated value
 
   app 'NitroShare.app'
