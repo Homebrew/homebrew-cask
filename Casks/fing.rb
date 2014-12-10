9c0c2027ef1515c2c9ae7cb0cefe7555d6b713e4
Roland Walker <walker@pobox.com>
2014-12-10 08:49:24 -0500

    license todo comment in fing

--- Casks/fing.rb
+++ Casks/fing.rb
@@ -4,7 +4,7 @@ cask :v1 => 'fing' do
 
   url 'http://www.overlooksoft.com/packages/download?plat=osx'
   homepage 'http://www.overlooksoft.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   pkg "overlook-fing-#{version}.pkg"
 
