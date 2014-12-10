f0869ff573b95e363aab0f1b69753ceaba6dff84
Roland Walker <walker@pobox.com>
2014-12-10 08:50:08 -0500

    license todo comment in minecraft

--- Casks/minecraft.rb
+++ Casks/minecraft.rb
@@ -4,7 +4,7 @@ cask :v1 => 'minecraft' do
 
   url 'https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.dmg'
   homepage 'http://minecraft.net'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Minecraft.app'
 end
