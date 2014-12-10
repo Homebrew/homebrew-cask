43874debf79b8e4a90f98120cf4eb59524d01a5e
Roland Walker <walker@pobox.com>
2014-12-10 08:49:16 -0500

    license todo comment in dropzone

--- Casks/dropzone.rb
+++ Casks/dropzone.rb
@@ -5,7 +5,7 @@ cask :v1 => 'dropzone' do
   url 'https://aptonic.com/dropzone3/latest'
   appcast 'https://aptonic.com/dropzone3/sparkle/updates.xml'
   homepage 'https://aptonic.com'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Dropzone 3.app'
 end
