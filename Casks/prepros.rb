4a86df12e84c5d582e1bcb225d0683cf060616ec
Roland Walker <walker@pobox.com>
2014-12-10 08:50:29 -0500

    license todo comment in prepros

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -4,7 +4,7 @@ cask :v1 => 'prepros' do
 
   url "http://download.alphapixels.com/Prepros-#{version}.zip"
   homepage 'http://alphapixels.com/prepros/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Prepros.app'
 end
