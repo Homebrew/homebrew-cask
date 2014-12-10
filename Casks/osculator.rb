4ed66dd63df850a564d01fb104012a9d8ac60eb4
Roland Walker <walker@pobox.com>
2014-12-10 08:50:20 -0500

    license todo comment in osculator

--- Casks/osculator.rb
+++ Casks/osculator.rb
@@ -4,7 +4,7 @@ cask :v1 => 'osculator' do
 
   url "http://dl.osculator.net/releases/osculator-#{version}-20-g81f7dd4.dmg"
   homepage 'http://www.osculator.net'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'OSCulator ƒ/OSCulator.app'
 end
