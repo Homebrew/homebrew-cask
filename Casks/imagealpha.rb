9c003ef5558a04fe909629453f79c7e6b9582c87
Roland Walker <walker@pobox.com>
2014-12-10 11:55:20 -0500

    enable MacOS.release in imagealpha

--- Casks/imagealpha.rb
+++ Casks/imagealpha.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'imagealpha' do
 
-  if MacOS.version <= :snow_leopard
+  if MacOS.release <= :snow_leopard
     version '1.2.3'
     sha256 '79388edcaf5cb29234f722a672b069c6d51fb812e89969ba8db8e957b9a32bf3'
   else
