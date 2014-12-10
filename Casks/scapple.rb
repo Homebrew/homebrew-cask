b0041fbd8e512f07f899bf1f9012af9cac8e9217
Roland Walker <walker@pobox.com>
2014-12-10 08:50:40 -0500

    license todo comment in scapple

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -5,7 +5,7 @@ cask :v1 => 'scapple' do
   url 'https://scrivener.s3.amazonaws.com/Scapple.dmg'
   appcast 'http://www.literatureandlatte.com/downloads/scapple/scapple.xml'
   homepage 'https://www.literatureandlatte.com/scapple.php'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Scapple.app'
 end
