d978080aa69a177f948eb04b3b5ebf4392793219
Roland Walker <walker@pobox.com>
2014-12-10 08:50:51 -0500

    license todo comment in sonic-pi

--- Casks/sonic-pi.rb
+++ Casks/sonic-pi.rb
@@ -4,7 +4,7 @@ cask :v1 => 'sonic-pi' do
 
   url 'http://sonic-pi.net/files/sonic-pi-mac-latest.dmg'
   homepage 'http://sonic-pi.net/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Sonic Pi.app'
 end
