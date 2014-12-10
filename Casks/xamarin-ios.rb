2a34c580a5ef0f9163e94075f69ca589ffc92a00
Roland Walker <walker@pobox.com>
2014-12-10 08:51:20 -0500

    license todo comment in xamarin-ios

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -7,7 +7,7 @@ cask :v1 => 'xamarin-ios' do
           :sha256 => '713f272a1e36262f1b2c5a06f4ed1b1eb8987d240018347a51312dfedeeafcf3',
           :format => :unknown
   homepage 'http://xamarin.com/ios'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   pkg "monotouch-#{version}.pkg"
 
