9e285ff3b37624a2cfde9f119a89f73fc4ef156f
Roland Walker <walker@pobox.com>
2014-12-10 08:50:19 -0500

    license todo comment in origin

--- Casks/origin.rb
+++ Casks/origin.rb
@@ -4,7 +4,7 @@ cask :v1 => 'origin' do
 
   url 'https://eaassets-a.akamaihd.net/Origin-Client-Download/origin/mac/Origin.dmg'
   homepage 'http://origin.com'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Origin.app'
 end
