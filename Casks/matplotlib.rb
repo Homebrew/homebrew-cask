b270aae02add053d2e77c9055decf97e3a9e8007
Roland Walker <walker@pobox.com>
2014-12-10 08:50:04 -0500

    license todo comment in matplotlib

--- Casks/matplotlib.rb
+++ Casks/matplotlib.rb
@@ -4,7 +4,7 @@ cask :v1 => 'matplotlib' do
 
   url "http://www.kyngchaos.com/files/software/python/matplotlib-#{version}.dmg"
   homepage 'http://www.kyngchaos.com/software/python'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   pkg 'matplotlib.pkg'
 
