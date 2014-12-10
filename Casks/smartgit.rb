c65a7d2a0cc65fae6b0016b27da3ebe34c6d9d5e
Amory Meltzer <Amorymeltzer@gmail.com>
2014-12-10 14:06:40 -0800

    Update smartgit homepage
    To http://www.syntevo.com/smartgit/

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -3,7 +3,7 @@ cask :v1 => 'smartgit' do
   sha256 '7e0ea9807cc68880daae408789d1ed219fe8faa9acb13a7e5557356025d75be9'
 
   url "http://www.syntevo.com/download/smartgit/smartgit-macosx-#{version.gsub('.', '_')}.dmg"
-  homepage 'http://www.syntevo.com'
+  homepage 'http://www.syntevo.com/smartgit/'
   license :unknown    # todo: improve this machine-generated value
 
   app 'SmartGit.app'
