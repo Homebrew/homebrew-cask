0ea1105379c6f75893c2447be79fc535773c340a
Roland Walker <walker@pobox.com>
2014-12-10 08:50:21 -0500

    license todo comment in papers

--- Casks/papers.rb
+++ Casks/papers.rb
@@ -5,7 +5,7 @@ cask :v1 => 'papers' do
   url 'http://papersapp.com/papers/download'
   appcast 'http://www.papersapp.com/papers/appcast_v3.xml'
   homepage 'http://www.papersapp.com/papers/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Papers.app'
 end
