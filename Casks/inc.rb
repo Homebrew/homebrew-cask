9faf210be67b439e620b623f0e7037949b0e4941
Roland Walker <walker@pobox.com>
2014-12-10 08:49:43 -0500

    license todo comment in inc

--- Casks/inc.rb
+++ Casks/inc.rb
@@ -4,7 +4,7 @@ cask :v1 => 'inc' do
 
   url 'http://inc-static.kippt.com.s3.amazonaws.com/apps/inc-osx.zip'
   homepage 'https://sendtoinc.com/apps/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Inc.app'
 end
