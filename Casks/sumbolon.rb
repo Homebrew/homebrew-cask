7e539061f631b42b18fcd307741d029e826bd9d5
Roland Walker <walker@pobox.com>
2014-12-10 08:50:57 -0500

    license todo comment in sumbolon

--- Casks/sumbolon.rb
+++ Casks/sumbolon.rb
@@ -4,7 +4,7 @@ cask :v1 => 'sumbolon' do
 
   url "http://www.rwe-uk.com/uploads/updates/Sumbolon%20#{version}.zip"
   homepage 'http://www.rwe-uk.com/app/sumbolon'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Sumbolon.app'
 end
