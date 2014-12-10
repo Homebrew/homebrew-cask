1ceb88a522e27b4d0ff84829adad0f54cade3be4
Roland Walker <walker@pobox.com>
2014-12-10 08:49:43 -0500

    license todo comment in inform

--- Casks/inform.rb
+++ Casks/inform.rb
@@ -4,7 +4,7 @@ cask :v1 => 'inform' do
 
   url "http://inform7.com/download/content/6L02/I#{version.gsub('.','-')}-OSX.dmg"
   homepage 'http://inform7.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Inform.app'
 end
