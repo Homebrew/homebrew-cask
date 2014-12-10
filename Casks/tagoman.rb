02aaa563ce09539b09e1a4c6629b3294b4d3c5a0
Roland Walker <walker@pobox.com>
2014-12-10 08:50:59 -0500

    license todo comment in tagoman

--- Casks/tagoman.rb
+++ Casks/tagoman.rb
@@ -5,7 +5,7 @@ cask :v1 => 'tagoman' do
   # dropboxusercontent.com is the official download host per the vendor homepage
   url 'https://dl.dropboxusercontent.com/u/7614970/TagoMan.zip'
   homepage 'http://onflapp.wordpress.com/tagoman'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'TagoMan.app'
 end
