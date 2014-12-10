9124710d90a588860f81f617f4865f464d4068ad
Roland Walker <walker@pobox.com>
2014-12-10 08:51:10 -0500

    license todo comment in ubar

--- Casks/ubar.rb
+++ Casks/ubar.rb
@@ -4,7 +4,7 @@ cask :v1 => 'ubar' do
 
   url "http://www.brawersoftware.com/downloads/ubar/ubar#{version.gsub('.','')}.zip"
   homepage 'http://brawersoftware.com/products/ubar'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'uBar.app'
 
