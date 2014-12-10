838032f0494be73c69ff808fee373999b2732be2
Roland Walker <walker@pobox.com>
2014-12-10 08:49:16 -0500

    license todo comment in dropin

--- Casks/dropin.rb
+++ Casks/dropin.rb
@@ -5,7 +5,7 @@ cask :v1 => 'dropin' do
   url 'http://excitedatom.com/downloads/dropin/?p=dropin'
   appcast 'http://excitedatom.com/dropin/updates.xml'
   homepage 'http://excitedatom.com/dropin/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'DropIn.app'
 end
