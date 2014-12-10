b1479ad175f0bf1e570e943142a68a061b9b04d1
Roland Walker <walker@pobox.com>
2014-12-10 08:50:21 -0500

    license todo comment in paragon-extfs

--- Casks/paragon-extfs.rb
+++ Casks/paragon-extfs.rb
@@ -4,7 +4,7 @@ cask :v1 => 'paragon-extfs' do
 
   url 'http://dl.paragon-software.com/demo/extmac_trial_u.dmg'
   homepage 'http://www.paragon-software.com/home/extfs-mac/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   pkg 'FSInstaller.app/Contents/Resources/Paragon ExtFS for Mac OS X.pkg'
 
