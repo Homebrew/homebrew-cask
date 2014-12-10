e4c9e3510e885892dba41ac413690467191cb77c
Roland Walker <walker@pobox.com>
2014-12-10 08:50:09 -0500

    license todo comment in miro-video-converter

--- Casks/miro-video-converter.rb
+++ Casks/miro-video-converter.rb
@@ -5,7 +5,7 @@ cask :v1 => 'miro-video-converter' do
   url 'http://ftp.osuosl.org/pub/pculture.org/mirovideoconverter/mac/Miro%20Video%20Converter.dmg'
   appcast 'http://miro-updates.participatoryculture.org/mvc-appcast-osx.xml'
   homepage 'http://www.mirovideoconverter.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Miro Video Converter.app'
 end
