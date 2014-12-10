dd3357b94a846707d7208082161b730d8b941cc8
Roland Walker <walker@pobox.com>
2014-12-10 08:50:09 -0500

    license todo comment in mocksmtp

--- Casks/mocksmtp.rb
+++ Casks/mocksmtp.rb
@@ -4,7 +4,7 @@ cask :v1 => 'mocksmtp' do
 
   url 'http://mocksmtpapp.com/MockSmtp.zip'
   homepage 'http://mocksmtpapp.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'MockSmtp.app'
 end
