7028f9762e98d7351ca0f266c9e15959e5b30480
Roland Walker <walker@pobox.com>
2014-12-10 08:51:07 -0500

    license todo comment in torustrooper

--- Casks/torustrooper.rb
+++ Casks/torustrooper.rb
@@ -4,7 +4,7 @@ cask :v1 => 'torustrooper' do
 
   url 'https://workram.com/downloads.php?f=TorusTrooper', :referer => 'https://workram.com/games'
   homepage 'http://workram.com/games/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'TorusTrooper.app'
 end
