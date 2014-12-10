ae474753acd18bb5b8b890e196f764908b72501e
Roland Walker <walker@pobox.com>
2014-12-10 08:50:45 -0500

    license todo comment in shiori

--- Casks/shiori.rb
+++ Casks/shiori.rb
@@ -6,7 +6,7 @@ cask :v1 => 'shiori' do
   appcast 'http://aki-null.net/shiori/appcast.xml',
           :sha256 => '244668dde7c677ddaaccc55c997874e13d3c8de5213c5e0810eeacd6fdcc713e'
   homepage 'http://aki-null.net/shiori/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Shiori.app'
 end
