c147f40b95b8d8fca2a9a217ea7ad8d020905381
Roland Walker <walker@pobox.com>
2014-12-10 08:49:14 -0500

    license todo comment in displaperture

--- Casks/displaperture.rb
+++ Casks/displaperture.rb
@@ -4,7 +4,7 @@ cask :v1 => 'displaperture' do
 
   url 'http://manytricks.com/download/displaperture'
   homepage 'http://manytricks.com/displaperture'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Displaperture.app'
 end
