e2c280a6a604f35b2a51236f8ff124fe6ab70764
Roland Walker <walker@pobox.com>
2014-12-10 08:49:12 -0500

    license todo comment in day-o

--- Casks/day-o.rb
+++ Casks/day-o.rb
@@ -4,7 +4,7 @@ cask :v1 => 'day-o' do
 
   url 'http://shauninman.com/assets/downloads/Day-O.zip'
   homepage 'http://www.shauninman.com/archive/2011/10/20/day_o_mac_menu_bar_clock'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Day-O/Day-O.app'
 end
