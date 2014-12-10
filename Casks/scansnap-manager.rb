93ce59c2746d9115854a8ff11f273dc083e81cd3
Roland Walker <walker@pobox.com>
2014-12-10 08:50:40 -0500

    license todo comment in scansnap-manager

--- Casks/scansnap-manager.rb
+++ Casks/scansnap-manager.rb
@@ -4,7 +4,7 @@ cask :v1 => 'scansnap-manager' do
 
   url "https://www.fujitsu.com/downloads/IMAGE/driver/ss/mgr/m-sv600/MacScanSnapV#{version.gsub('.','')}WW.dmg"
   homepage 'http://www.fujitsu.com/global/support/computing/peripheral/scanners/software/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   pkg 'ScanSnap Manager.pkg'
 
