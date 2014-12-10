8a6a0aaeec6f8ac76d85dae510c1c40a348e4753
Roland Walker <walker@pobox.com>
2014-12-10 08:50:20 -0500

    license todo comment in pacifist

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -5,7 +5,7 @@ cask :v1 => 'pacifist' do
   url 'https://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=dmg'
   appcast 'http://www.charlessoft.com/cgi-bin/pacifist_sparkle.cgi'
   homepage 'http://www.charlessoft.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'Pacifist.app'
 end
