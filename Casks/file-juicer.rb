8e07fe896b49df34001043848a8081a463c6dee6
Roland Walker <walker@pobox.com>
2014-12-10 08:49:23 -0500

    license todo comment in file-juicer

--- Casks/file-juicer.rb
+++ Casks/file-juicer.rb
@@ -4,7 +4,7 @@ cask :v1 => 'file-juicer' do
 
   url 'http://echoone.com/filejuicer/FileJuicer.dmg'
   homepage 'http://echoone.com/filejuicer/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
 
   app 'File Juicer.app'
 end
