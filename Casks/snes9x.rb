91881f418be352acfe42006a995a169602dbcd8c
Roland Walker <walker@pobox.com>
2014-12-10 08:50:50 -0500

    license todo comment in snes9x

--- Casks/snes9x.rb
+++ Casks/snes9x.rb
@@ -4,7 +4,7 @@ cask :v1 => 'snes9x' do
 
   url "http://files.ipherswipsite.com/snes9x/snes9x-#{version}-macosx-113.dmg.gz"
   homepage 'http://www.snes9x.com/'
-  license :unknown
+  license :unknown    # todo: improve this machine-generated value
   container :nested => "snes9x-#{version}"
 
   app 'Snes9x.app'
