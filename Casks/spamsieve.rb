38d3d26116da9811f8f234540183cfc5e5e449f8
Jussi Tuominen <Jussi@COLDwithin.local>
2014-12-11 21:05:35 +0200

    Fixed OS version checkings

--- Casks/spamsieve.rb
+++ Casks/spamsieve.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'spamsieve' do
 
-  if MacOS.release < :leopard
+  if MacOS.release <= :tiger
     version '2.9.6'
     sha256 'f060af29ab260450f0c0a906ada3e60408c9b6cd871e1df272dde2521bafeed3'
 
     url "http://c-command.com/downloads/SpamSieve-#{version}-tiger.dmg"
-  elsif MacOS.release < :snow_leopard
+  elsif MacOS.release == :leopard
     version '2.9.14'
     sha256 '9acd154e5c379b21ca752ff70bd59766cc2f91532a10ea12dce87f71101d2f11'
 
