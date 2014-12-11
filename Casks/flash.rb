3447f78abe6db193d676a5086c988af45a544402
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-12-11 10:31:05 +0800

    Update flashplayer_16_sa sha256

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'flash' do
   version '16.0.0.235'
-  sha256 'd47bdc510f35e35ecf5260e35f6c86a4750f95b8505841bc10e1b6a1af82d346'
+  sha256 'f7bd90f9ee7520195cb02c0bf7803b4b30da48e64da040cd7d84d638aa000102'
 
   url "http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_#{version.to_i}_osx_pkg.dmg"
   homepage 'https://www.adobe.com/products/flashplayer/distribution3.html'
