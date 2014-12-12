a377dda420c2edfb4a18aa348b0c824e6b750fe5
mgracer48 <mgracer48@yahoo.com>
2014-12-12 10:30:01 +0100

    Add wizard101.app
    Update wizard101.rb
    
    Update wizard101.rb
    
    Update wizard101.rb

--- Casks/wizard101.rb
+++ Casks/wizard101.rb
@@ -3,9 +3,29 @@ cask :v1 => 'wizard101' do
   sha256  :no_check
 
   url 'https://www.wizard101.com/downloadGameMac/otherdownload'
+<<<<<<< HEAD
+<<<<<<< HEAD
   name 'Wizard101'
   homepage 'wizard101.com'
   license :unknown
 
   app 'Wizard101.app'
+=======
+  name 'wizard101'
+  homepage 'wizard101.com'
+  license :unknown
+
+  app 'wizard101'
+>>>>>>> d946b45... Add wizard101.app
+=======
+  name 'Wizard101'
+  homepage 'wizard101.com'
+  license :unknown
+
+<<<<<<< HEAD
+  app 'Wizard101'
+>>>>>>> 8d5d074... Update wizard101.rb
+=======
+  app 'Wizard101.app'
+>>>>>>> e645a5c... Update wizard101.rb
 end
