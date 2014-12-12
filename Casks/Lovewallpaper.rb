f72010c6b9fca80914f513f289fd07a995f0a5c2
JingKe Wu <Hi.LaoW@Gmail.com>
2014-12-12 15:46:07 +0800

    add LoveWallpaper  package

---
+++ Casks/Lovewallpaper.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'Lovewallpaper' do
+  version '3.5.5'
+  sha256 'a472bd7ab2d0ab9af96baa8cdeb98fb1428ab6d1c6db54fbd2150772e1942fce'
+
+  url 'http://s.qdcdn.com/lovebizhi/LoveWallpaper4Mac.dmg'
+  homepage 'http://www.lovebizhi.com/'
+  license :commercial
+
+  app 'LoveWallpaper4Mac.app'
+end
