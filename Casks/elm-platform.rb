0de2cddac1920dec11686e003761eca280cf0ee3
Jeehyung Lee <jeehyungl@gmail.com>
2014-12-14 16:44:32 +0800

    Update with correct sha256 hash

--- Casks/elm-platform.rb
+++ Casks/elm-platform.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'elm-platform' do
   version '0.14'
-  sha256 'dec6255138eccd228402f6c50e5da297b161139e7c47a494c964df3f53ee56f6'
+  sha256 '8d15ddb9d7b09767a5279b6b68e953af2e268f4652668ee3d893c8e96ddcd861'
 
   url "http://install.elm-lang.org/Elm-Platform-#{version}.pkg"
   homepage 'http://www.elm-lang.org'
