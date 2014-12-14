35679f428c5a603f1770893c734e3adfd65d5314
mgracer48 <mgracer48@yahoo.com>
2014-12-14 18:10:56 +0100

    Update winebottler.rb

--- Casks/winebottler.rb
+++ Casks/winebottler.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'winebottler' do
-  version '1.7.32
+  version '1.7.32'
   sha256 '7e76b40a335ae7fa85903a7c0def2ea18fcde6458455e156450b813ddfb33c41'
 
   url 'http://winebottler.kronenberg.org/combo/builds/WineBottlerCombo_1.7.32.dmg'
