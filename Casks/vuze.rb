d8279847da9815601aab2b86a17abbd0e59b8642
Vítor Galvão <info@vitorgalvao.com>
2014-12-14 18:04:42 +0000

    Added Vuze
    Closes #8048

---
+++ Casks/vuze.rb
@@ -0,0 +1,16 @@
+cask :v1 => 'vuze' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://cf1.vuze.com/files/J7/VuzeBittorrentClientInstaller.dmg'
+  homepage 'http://www.vuze.com/'
+  license :unknown    # todo: improve this machine-generated value
+
+  installer :manual => 'Vuze Installer.app'
+
+  uninstall :delete => '/Applications/Vuze.app'
+
+  zap :delete => '~/Library/Application Support/Vuze'
+
+  caveats 'If you pick an installation directory other than /Applications when installing this cask, you will need to uninstall it manually'
+end
