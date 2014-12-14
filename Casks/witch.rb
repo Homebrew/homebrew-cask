6c693470fcf5338c17980e747d5e3f92cb6e95f8
Basil Shubin <basil.shubin@gmail.com>
2014-12-14 13:06:45 +0600

    Add Witch 3.9.1 (Snow Leopard compatible)
    Closes #8095.

--- Casks/witch.rb
+++ Casks/witch.rb
@@ -1,10 +1,17 @@
 cask :v1 => 'witch' do
-  version :latest
-  sha256 :no_check
+  if MacOS.release == :snow_leopard
+    version '3.9.1'
+    sha256 '5e46508e150ff16be14b9955abdcd15098376230ef71e3de6f15a056eec75e45'
+    url 'http://manytricks.com/download/witch/3.9.1'
+  else
+    version :latest
+    sha256 :no_check
 
-  url 'http://manytricks.com/download/witch'
+    url 'http://manytricks.com/download/witch'
+  end
+
   homepage 'http://manytricks.com/witch/'
-  license :unknown    # todo: improve this machine-generated value
+  license :commercial
 
   prefpane 'Witch.prefPane'
 
