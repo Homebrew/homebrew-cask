6c0e65dec1a9d250a913fd5d379d27d37cb9efe4
Vítor Galvão <info@vitorgalvao.com>
2014-12-15 15:11:30 +0000

    updated qlvideo (1.73)

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qlvideo' do
-  version '1.7.1'
-  sha256 'cb0d417b13fad20484233c68ec7cf5c84d9a2a1fb74e31e67fed161b7c84e1c5'
+  version '1.73'
+  sha256 '616e298c74a7d64b2c5af273c376ec98dafa317ebdd7b52c928d39dde2405756'
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.gsub('.', '')}/QLVideo_#{version.gsub('.', '')}.pkg"
   homepage 'https://github.com/Marginal/QLVideo'
