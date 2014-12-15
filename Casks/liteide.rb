6229762c748dc51527fa24ccf49ffaa6a7704f9d
dezon <dezonweb@163.com>
2014-12-15 13:20:45 +0800

    Update liteide.rb
    update liteide version

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'liteide' do
-  version '24.2'
+  version '25.1'
   sha256 '2178ec58f1aa3fe64be6d4f9c516fe6f712667bfe1abfa85aed6718cc7f3ef9a'
 
   url "https://downloads.sourceforge.net/project/liteide/X#{version}/liteidex#{version}.macosx.zip"
