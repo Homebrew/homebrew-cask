f2b2753df1dcf1d4414852969cd39becac43f23f
CJ Patoilo <cjpatoilo@gmail.com>
2014-12-16 05:23:13 -0300

    add sha256

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'popcorn-time' do
   version '0.3.5.4'
-  sha256 :no_check
+  sha256 'ed7012ac07bed4ccd7c6c5eb394b3573191a3bee'
 
   url 'https://get.popcorntime.io/build/Popcorn-Time-#{version}-Mac.dmg'
   homepage 'http://popcorntime.io/'
