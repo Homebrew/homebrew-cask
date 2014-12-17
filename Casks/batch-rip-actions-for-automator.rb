b5fb310d0297384fb1469981b14a425fa6ddae1a
plessbd <plessbd@users.noreply.github.com>
2014-12-16 23:34:42 -0500

    Update batch-rip-actions-for-automator.rb
    add zap stanza to remove services.
    Directories possibly created under ~/Movies are not deleted as they may contain data.

--- Casks/batch-rip-actions-for-automator.rb
+++ Casks/batch-rip-actions-for-automator.rb
@@ -7,4 +7,33 @@ cask :v1 => 'batch-rip-actions-for-automator' do
   license :unknown
 
   app 'Batch Rip Actions for Automator.app'
+	
+  zap :delete => [
+    '~/Library/Services/Batch Rip • Add Chapter Names (Custom).workflow',
+    '~/Library/Services/Batch Rip • Add Chapter Names (Filename).workflow',
+    '~/Library/Services/Batch Rip • Add Chapter Names (Search).workflow',
+    '~/Library/Services/Batch Rip • Add Genre to Movie File.workflow',
+    '~/Library/Services/Batch Rip • Add HD Flag.workflow',
+    '~/Library/Services/Batch Rip • Add Movie Comment.workflow',
+    '~/Library/Services/Batch Rip • Add Movie Poster.workflow',
+    '~/Library/Services/Batch Rip • Add Movie Tags (Filename).workflow',
+    '~/Library/Services/Batch Rip • Add Movie Tags (Search).workflow',
+    '~/Library/Services/Batch Rip • Add TV Show Comment.workflow',
+    '~/Library/Services/Batch Rip • Add TV Tags (Filename).workflow',
+    '~/Library/Services/Batch Rip • Add TV Tags (Search).workflow',
+    '~/Library/Services/Batch Rip • Add cnID to Selected (Same).workflow',
+    '~/Library/Services/Batch Rip • Add cnID to Selected (Unique).workflow',
+    '~/Library/Services/Batch Rip • Batch Encode (Finder).workflow',
+    '~/Library/Services/Batch Rip • Batch Encode.workflow',
+    '~/Library/Services/Batch Rip • Batch Rip (Finder).workflow',
+    '~/Library/Services/Batch Rip • Batch Rip.workflow',
+    '~/Library/Services/Batch Rip • Get Source Info from HandBrake.workflow',
+    '~/Library/Services/Batch Rip • Remove All Tags.workflow',
+    '~/Library/Services/Batch Rip • Rename & Add Custom Title Tag.workflow',
+    '~/Library/Services/Batch Rip • Rename & Tag Movie Items.workflow',
+    '~/Library/Services/Batch Rip • Rename & Tag TV Items.workflow',
+    '~/Library/Services/Batch Rip • Rename Movie Items.workflow',
+    '~/Library/Services/Batch Rip • Rename TV Items.workflow',
+    '~/Library/Services/Batch Rip • Tag Inspector.workflow'
+  ]
 end
