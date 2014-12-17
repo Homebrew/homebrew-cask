c35d0c2cf18198fe01c43263f130873b49624919
Roland Walker <walker@pobox.com>
2014-12-17 08:14:48 -0500

    no HTTPS sourceforge in sabnzbd

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -2,7 +2,7 @@ cask :v1 => 'sabnzbd' do
   version '0.7.20'
   sha256 'f7c13afe87ad91ab2f12fb6384feef79ae07bf2417395304b3d8961513d23611'
 
-  url "https://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/#{version}/SABnzbd-#{version}-osx.dmg"
+  url "http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/#{version}/SABnzbd-#{version}-osx.dmg"
   homepage 'http://sabnzbd.org/'
   license :oss
 
