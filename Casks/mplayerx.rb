48ced791df281e1b7e8dd11f805315945aeb2f45
Roland Walker <walker@pobox.com>
2014-12-17 08:14:45 -0500

    no HTTPS sourceforge in mplayerx

--- Casks/mplayerx.rb
+++ Casks/mplayerx.rb
@@ -2,7 +2,7 @@ cask :v1 => 'mplayerx' do
   version '1.0.22.1'
   sha256 '08ce85671814e65b8c7ec8438b85be593b6deaf7d5c3b242e686a6b0176a2c77'
 
-  url "https://downloads.sourceforge.net/project/mplayerx-osx/MPlayerX-#{version}.zip"
+  url "http://downloads.sourceforge.net/project/mplayerx-osx/MPlayerX-#{version}.zip"
   homepage 'http://mplayerx.org/'
   license :oss
 
