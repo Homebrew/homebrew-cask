5db84a3ce0c9cb55eca18988afa76a9c99ff9ad3
Roland Walker <walker@pobox.com>
2014-12-17 08:14:32 -0500

    no HTTPS sourceforge in burn

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -2,7 +2,7 @@ cask :v1 => 'burn' do
   version '2.5.1'
   sha256 'e82d2b7ef6a99e5a139706ff4e360659830d618ab8743e4b55ec80cdcdc97596'
 
-  url "https://downloads.sourceforge.net/project/burn-osx/Burn/#{version}/burn251.zip"
+  url "http://downloads.sourceforge.net/project/burn-osx/Burn/#{version}/burn251.zip"
   homepage 'http://burn-osx.sourceforge.net/'
   license :gpl
 
