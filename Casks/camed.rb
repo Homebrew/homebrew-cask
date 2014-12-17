774ea73f6907c81111f5006a4f65bc6d9327957f
Roland Walker <walker@pobox.com>
2014-12-17 08:14:32 -0500

    no HTTPS sourceforge in camed

--- Casks/camed.rb
+++ Casks/camed.rb
@@ -2,7 +2,7 @@ cask :v1 => 'camed' do
   version '3.1.1'
   sha256 'c9088796b6f5cee228341e2ec6c2ee0ff3e3a506ff0dde3643865ccbc9c4f744'
 
-  url "https://downloads.sourceforge.net/sourceforge/camprocessor/CAMEd-#{version}-macosx-cocoa-x86_64.tar.gz"
+  url "http://downloads.sourceforge.net/sourceforge/camprocessor/CAMEd-#{version}-macosx-cocoa-x86_64.tar.gz"
   homepage 'http://sourceforge.net/apps/mediawiki/camprocessor/index.php?title=Main_Page'
   license :oss
 
