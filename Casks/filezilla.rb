3debd228287af0e05181cd8190bcdf6aca96c198
Roland Walker <walker@pobox.com>
2014-12-17 08:14:36 -0500

    no HTTPS sourceforge in filezilla

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -2,7 +2,7 @@ cask :v1 => 'filezilla' do
   version '3.9.0.6'
   sha256 '9486be1f5dec41df8ef1b60893ca612e6791b8d98609c6b9e42ced977210d15b'
 
-  url "https://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
+  url "http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   homepage 'https://filezilla-project.org/'
   license :oss
 
