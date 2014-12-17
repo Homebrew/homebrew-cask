d82288a68f60bd6e78609944cd2173c672e1d964
Roland Walker <walker@pobox.com>
2014-12-17 08:14:37 -0500

    no HTTPS sourceforge in freecol

--- Casks/freecol.rb
+++ Casks/freecol.rb
@@ -2,7 +2,7 @@ cask :v1 => 'freecol' do
   version '0.10.7'
   sha256 'eb05ea179df6adc6bcf4234ce3809c48b8c2d5597da20a10910ded677f2b6a2d'
 
-  url "https://downloads.sourceforge.net/sourceforge/freecol/freecol-#{version}-mac.tar.bz2"
+  url "http://downloads.sourceforge.net/sourceforge/freecol/freecol-#{version}-mac.tar.bz2"
   homepage 'http://freecol.org'
   license :oss
 
