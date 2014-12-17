63d679140f0f6072931ebf5bbea361cd57685129
invlpg <invlpg@gmail.com>
2014-12-17 13:49:42 +0800

    Add iBackupBot

---
+++ Casks/ibackupbot.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'ibackupbot' do
+  version :latest
+  sha256 :no_check
+  url 'http://www.icopybot.com/iBackupBot-Setup.dmg'
+
+  homepage 'http://www.icopybot.com/itunes-backup-manager.htm'
+  license :closed
+
+  app 'iBackupBot.app'
+end
