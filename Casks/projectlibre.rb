dd53d7e57bf900f48842860fef579fd400589e99
Roland Walker <walker@pobox.com>
2014-12-17 08:14:46 -0500

    no HTTPS sourceforge in projectlibre

--- Casks/projectlibre.rb
+++ Casks/projectlibre.rb
@@ -2,7 +2,7 @@ cask :v1 => 'projectlibre' do
   version '1.5.8'
   sha256 'efd7d228d2e71a2a5af185d2fe1ddcea4abc55af32188344e06e1871facd0c6b'
 
-  url "https://downloads.sourceforge.net/project/projectlibre/ProjectLibre/#{version}/projectlibre-#{version}.dmg"
+  url "http://downloads.sourceforge.net/project/projectlibre/ProjectLibre/#{version}/projectlibre-#{version}.dmg"
   homepage 'http://www.projectlibre.org/'
   license :oss
 
