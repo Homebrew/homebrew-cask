c227a494b1c1aaecf496950955e7b9f4e1a1c524
Roland Walker <walker@pobox.com>
2014-12-17 08:14:52 -0500

    no HTTPS sourceforge in wineskin-winery

--- Casks/wineskin-winery.rb
+++ Casks/wineskin-winery.rb
@@ -2,7 +2,7 @@ cask :v1 => 'wineskin-winery' do
   version '1.7'
   sha256 'ef3ae1fe17a7bc622a59171985f304f506aea7ca0ad342281536dac8609eac32'
 
-  url "https://downloads.sourceforge.net/project/wineskin/Wineskin%20Winery.app%20Version%20#{version}.zip"
+  url "http://downloads.sourceforge.net/project/wineskin/Wineskin%20Winery.app%20Version%20#{version}.zip"
   homepage 'http://wineskin.urgesoftware.com/'
   license :oss
 
