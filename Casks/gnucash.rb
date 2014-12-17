9b5284cd58dd960a40e0869eac564757c7f19ab4
Roland Walker <walker@pobox.com>
2014-12-17 08:14:38 -0500

    no HTTPS sourceforge in gnucash

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -2,7 +2,7 @@ cask :v1 => 'gnucash' do
   version '2.6.4-5'
   sha256 '8c9701cbc6acf191b682d80c5e2eedd5dc4099ccaacacef3f89cecd967ca8651'
 
-  url "https://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-#{version}.dmg"
+  url "http://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-#{version}.dmg"
   homepage 'http://www.gnucash.org'
   license :oss
 
