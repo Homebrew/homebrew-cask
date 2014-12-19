b9c18a2ca2a978061ce65da20dad8f586abc2fa9
Dustin Brewer <mannkind@thenullpointer.net>
2014-12-19 11:55:43 -0800

    Plex Media Server updated to 0.9.11.4.739

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'plex-media-server' do
-  version '0.9.9.14.531-7eef8c6'
-  sha256 '7cd91e89f74569409b12f8151d66c731ca8658e6df61aaaac41accf6e6cba337'
+  version '0.9.11.4.739-a4e710f'
+  sha256 '69066c232cfbf4b8cd92881e954e8486042c0838bfd69c27b600d89d023abf96'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   homepage 'https://plex.tv/'
