d5f1740242474072b6cea283e7f8c58b770ece51
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-19 20:38:48 +0000

    vimr 0.5.1
    Version bump.

--- Casks/vimr.rb
+++ Casks/vimr.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'vimr' do
-  version '0.5.0-24'
-  sha256 '22a6203573e13d9e4ae2f277c297446edba3d2e5150375563668a9a2c430ab5f'
+  version '0.5.1-25'
+  sha256 'b316e879b546a89f5339a510a7168801c98e3180759ca76c3c8272be22f4ad62'
 
   url "https://github.com/qvacua/vimr/releases/download/v#{version}/VimR-#{version.gsub(/-.*/, '')}.tar.bz2"
   homepage 'http://vimr.org/'
