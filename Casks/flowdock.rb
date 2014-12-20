599fa83fa3c2f6502f031e7434a1dda422a71831
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:10 +0000

    flowdock.rb: change ':unknown' license comment

--- Casks/flowdock.rb
+++ Casks/flowdock.rb
@@ -5,7 +5,7 @@ cask :v1 => 'flowdock' do
   url 'https://flowdock-resources.s3.amazonaws.com/mac/Flowdock.zip'
   appcast 'https://s3.amazonaws.com/flowdock-resources/mac/appcast.xml'
   homepage 'https://www.flowdock.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Flowdock.app'
 end
