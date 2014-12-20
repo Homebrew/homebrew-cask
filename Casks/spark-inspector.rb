c8524cdf15b39834b634558c7610988004d9ddef
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:17 +0000

    spark-inspector.rb: change ':unknown' license comment

--- Casks/spark-inspector.rb
+++ Casks/spark-inspector.rb
@@ -5,7 +5,7 @@ cask :v1 => 'spark-inspector' do
   url 'http://sparkinspector.com/downloads/sparkinspector.dmg'
   appcast 'http://sparkinspector.com/sparkle/feed.xml'
   homepage 'http://sparkinspector.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Spark Inspector.app'
 end
