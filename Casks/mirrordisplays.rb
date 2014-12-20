b9384cffd29478db2e9a555938c760472474848e
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:41 +0000

    mirrordisplays.rb: change ':unknown' license comment

--- Casks/mirrordisplays.rb
+++ Casks/mirrordisplays.rb
@@ -5,7 +5,7 @@ cask :v1 => 'mirrordisplays' do
   url 'http://www.fabiancanas.com/downloads/MirrorDisplays.zip',
     :referer => 'http://www.fabiancanas.com/Projects/MirrorDisplays'
   homepage 'http://www.fabiancanas.com/Projects/MirrorDisplays'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'MirrorDisplays.app'
 end
