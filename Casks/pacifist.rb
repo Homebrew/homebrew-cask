0d5c0c458acbe53c29f3671c87ba277c86530a58
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:52 +0000

    pacifist.rb: change ':unknown' license comment

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -5,7 +5,7 @@ cask :v1 => 'pacifist' do
   url 'https://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=dmg'
   appcast 'http://www.charlessoft.com/cgi-bin/pacifist_sparkle.cgi'
   homepage 'http://www.charlessoft.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Pacifist.app'
 end
