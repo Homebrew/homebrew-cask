7a456271931e4b13ba5216f6d6fde46a50c90984
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:40 +0000

    4peaks.rb: change ':unknown' license comment

--- Casks/4peaks.rb
+++ Casks/4peaks.rb
@@ -4,7 +4,7 @@ cask :v1 => '4peaks' do
 
   url 'http://nucleobytes.com/4peaks/download'
   homepage 'http://nucleobytes.com/index.php/4peaks'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app '4Peaks.app'
 end
