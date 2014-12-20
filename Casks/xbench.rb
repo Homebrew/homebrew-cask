cdc89ad1ed91557a4098489aaee5b10fe45a7251
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:42 +0000

    xbench.rb: change ':unknown' license comment

--- Casks/xbench.rb
+++ Casks/xbench.rb
@@ -4,7 +4,7 @@ cask :v1 => 'xbench' do
 
   url "http://xbench.com/Xbench_#{version}.dmg"
   homepage 'http://xbench.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'XBench.app'
 end
