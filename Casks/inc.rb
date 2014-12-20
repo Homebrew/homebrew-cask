65c7e1a90365da6dda80e8d93f3b4c197c95ae32
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:22 +0000

    inc.rb: change ':unknown' license comment

--- Casks/inc.rb
+++ Casks/inc.rb
@@ -4,7 +4,7 @@ cask :v1 => 'inc' do
 
   url 'http://inc-static.kippt.com.s3.amazonaws.com/apps/inc-osx.zip'
   homepage 'https://sendtoinc.com/apps/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Inc.app'
 end
