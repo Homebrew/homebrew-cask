2937a3c6b9d5b446317080e5f50a004fa32fb758
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:43 +0000

    xtrafinder.rb: change ':unknown' license comment

--- Casks/xtrafinder.rb
+++ Casks/xtrafinder.rb
@@ -4,7 +4,7 @@ cask :v1 => 'xtrafinder' do
 
   url 'http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg'
   homepage 'http://www.trankynam.com/xtrafinder/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg 'XtraFinder.pkg'
 
