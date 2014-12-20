8158aade5cc2dd6ac68defc52e4178c094176fb6
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:56 +0000

    cocoaspell.rb: change ':unknown' license comment

--- Casks/cocoaspell.rb
+++ Casks/cocoaspell.rb
@@ -4,7 +4,7 @@ cask :v1 => 'cocoaspell' do
 
   url "http://people.ict.usc.edu/~leuski/cocoaspell/cocoAspell.#{version}.dmg"
   homepage 'http://cocoaspell.leuski.net/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg 'cocoAspell.pkg'
 
