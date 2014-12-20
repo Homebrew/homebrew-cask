baa15ac04e16754076d5c9f2bf159627e3d215b0
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:09 +0000

    scrivener.rb: change ':unknown' license comment

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -5,7 +5,7 @@ cask :v1 => 'scrivener' do
   url 'https://scrivener.s3.amazonaws.com/Scrivener.dmg'
   appcast 'http://www.literatureandlatte.com/downloads/scrivener-2.xml'
   homepage 'http://literatureandlatte.com/scrivener.php'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Scrivener.app'
 end
