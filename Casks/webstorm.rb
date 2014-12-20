79e641bfe1e8a2bb96bcb1f9023edeb81be046b6
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:39 +0000

    webstorm.rb: change ':unknown' license comment

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -4,7 +4,7 @@ cask :v1 => 'webstorm' do
 
   url "http://download-cf.jetbrains.com/webstorm/WebStorm-#{version}.dmg"
   homepage 'http://www.jetbrains.com/webstorm/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'WebStorm.app'
 
