b3d27b238c3934a3b42ff5bca7d6911d889ff512
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:59 +0000

    prefs-editor.rb: change ':unknown' license comment

--- Casks/prefs-editor.rb
+++ Casks/prefs-editor.rb
@@ -4,7 +4,7 @@ cask :v1 => 'prefs-editor' do
 
   url 'http://files.tempel.org/Various/OSX_Prefs_Editor/PrefsEditor.zip'
   homepage 'http://www.tempel.org/PrefsEditor'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Prefs Editor.app'
 end
