d2710098e9bc7eea4f27b9dc767e8092c3b69889
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:43 +0000

    ampps.rb: change ':unknown' license comment

--- Casks/ampps.rb
+++ Casks/ampps.rb
@@ -4,7 +4,7 @@ cask :v1 => 'ampps' do
 
   url "http://files.ampps.com/AMPPS-#{version}.dmg"
   homepage 'http://www.ampps.com'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   suite 'AMPPS'
 end
