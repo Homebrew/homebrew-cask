275c61941993c4dd5b8f63b4d5273a6b174df59e
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:44 +0000

    musictube.rb: change ':unknown' license comment

--- Casks/musictube.rb
+++ Casks/musictube.rb
@@ -5,7 +5,7 @@ cask :v1 => 'musictube' do
   url 'http://flavio.tordini.org/files/musictube/musictube.dmg'
   appcast 'http://flavio.tordini.org/musictube-ws/appcast.xml'
   homepage 'http://flavio.tordini.org/musictube'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Musictube.app'
 end
