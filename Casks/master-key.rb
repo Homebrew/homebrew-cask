dde357069e744dda630c24ddb545f7fe7264f5da
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:38 +0000

    master-key.rb: change ':unknown' license comment

--- Casks/master-key.rb
+++ Casks/master-key.rb
@@ -5,7 +5,7 @@ cask :v1 => 'master-key' do
   url 'http://macinmind.com/MasterKey.dmg'
   appcast 'http://macinmind.com/pads/MasterKeyappcast.xml'
   homepage 'http://macinmind.com/?area=app&app=masterkey&pg=info'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Master Key.app'
 end
