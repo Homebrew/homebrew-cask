f2b8ac406c0e8cdf74a68878a05b128db2900df9
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:52 +0000

    camtwist.rb: change ':unknown' license comment

--- Casks/camtwist.rb
+++ Casks/camtwist.rb
@@ -4,7 +4,7 @@ cask :v1 => 'camtwist' do
 
   url "http://camtwiststudio.com/release/CamTwist_#{version}.dmg"
   homepage 'http://camtwiststudio.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg 'CamTwist.pkg'
 
