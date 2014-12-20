f0ddb45b00b5b92ce63f4adf86e7e0cf43786541
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:08 +0000

    ffmpegx.rb: change ':unknown' license comment

--- Casks/ffmpegx.rb
+++ Casks/ffmpegx.rb
@@ -4,7 +4,7 @@ cask :v1 => 'ffmpegx' do
 
   url 'ftp://ftp2.ffmpegx.com/ffmpegx/ffmpegX.dmg'
   homepage 'http://www.ffmpegx.com/download.html'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'ffmpegX.app'
 end
