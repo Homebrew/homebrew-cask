cf11c1403ebb90bedfc9c28fd8c949219a1b79e2
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:57 +0000

    colorschemer-studio.rb: change ':unknown' license comment

--- Casks/colorschemer-studio.rb
+++ Casks/colorschemer-studio.rb
@@ -5,7 +5,7 @@ cask :v1 => 'colorschemer-studio' do
   url 'https://www.colorschemer.com/colorschemerstudio.dmg'
   appcast 'http://www.colorschemer.com/appcast/studio2_mac.xml'
   homepage 'http://www.colorschemer.com'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'ColorSchemer Studio 2.app'
 end
