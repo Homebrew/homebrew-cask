28b88b5998757b41f0575a0b0661d9818722f22f
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:30 +0000

    klayout.rb: change ':unknown' license comment

--- Casks/klayout.rb
+++ Casks/klayout.rb
@@ -4,7 +4,7 @@ cask :v1 => 'klayout' do
 
   url "http://178.77.72.242/downloads/klayout.#{version}.pkg"
   homepage 'http://www.klayout.de/index.html'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg "klayout.#{version}.pkg"
 
