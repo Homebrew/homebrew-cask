72c1a0a9e5fe89b2cf2592535c0d21526deb25ef
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:28 +0000

    titanium-studio.rb: change ':unknown' license comment

--- Casks/titanium-studio.rb
+++ Casks/titanium-studio.rb
@@ -4,7 +4,7 @@ cask :v1 => 'titanium-studio' do
 
   url 'https://titanium-studio.s3.amazonaws.com/latest/Titanium_Studio.dmg'
   homepage 'https://my.appcelerator.com/resources'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   installer :manual => 'Titanium Studio.app'
 end
