f00ea33ef88f0b757758979666d96956df69587c
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:43 +0000

    movieply.rb: change ':unknown' license comment

--- Casks/movieply.rb
+++ Casks/movieply.rb
@@ -4,7 +4,7 @@ cask :v1 => 'movieply' do
 
   url "http://www.movieply.com/download/MoviePly-#{version}.OSX.zip"
   homepage 'http://www.movieply.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'MoviePly.app'
 end
