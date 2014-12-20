6e441c8b2d0b2d53a7fbeadfa78ceb04fd27c07b
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:29 +0000

    keyboardcleantool.rb: change ':unknown' license comment

--- Casks/keyboardcleantool.rb
+++ Casks/keyboardcleantool.rb
@@ -4,7 +4,7 @@ cask :v1 => 'keyboardcleantool' do
 
   url 'http://bettertouchtool.net/KeyboardCleanTool.zip'
   homepage 'http://bettertouchtool.net'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'KeyboardCleanTool.app'
 end
