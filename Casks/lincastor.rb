83e0738dda0afd9bdceb665e8012343217804a6f
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:32 +0000

    lincastor.rb: change ':unknown' license comment

--- Casks/lincastor.rb
+++ Casks/lincastor.rb
@@ -5,7 +5,7 @@ cask :v1 => 'lincastor' do
   # dropboxusercontent.com is the official download host per the vendor homepage
   url 'https://dl.dropboxusercontent.com/u/7614970/LinCastor.zip'
   homepage 'http://onflapp.wordpress.com/lincastor/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'LinCastor.app'
 end
