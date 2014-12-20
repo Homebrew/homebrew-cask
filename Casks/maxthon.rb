329657598e65bf7e95d5ea676b35ed0d24787420
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:38 +0000

    maxthon.rb: change ':unknown' license comment

--- Casks/maxthon.rb
+++ Casks/maxthon.rb
@@ -4,7 +4,7 @@ cask :v1 => 'maxthon' do
 
   url "https://dl.maxthon.com/mac/Maxthon-#{version}.dmg"
   homepage 'http://www.maxthon.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Maxthon.app'
 end
