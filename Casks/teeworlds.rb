faf9ab909d1a5d6165c43e6294701675e3e251c1
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:25 +0000

    teeworlds.rb: change ':unknown' license comment

--- Casks/teeworlds.rb
+++ Casks/teeworlds.rb
@@ -4,7 +4,7 @@ cask :v1 => 'teeworlds' do
 
   url "https://teeworlds.com/files/teeworlds-#{version}-osx.dmg"
   homepage 'https://www.teeworlds.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Teeworlds.app'
   app 'Teeworlds Server.app'
