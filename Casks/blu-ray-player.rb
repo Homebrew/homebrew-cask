3249be26b5258e41005b21f9541f402f698304f0
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:49 +0000

    blu-ray-player.rb: change ':unknown' license comment

--- Casks/blu-ray-player.rb
+++ Casks/blu-ray-player.rb
@@ -4,7 +4,7 @@ cask :v1 => 'blu-ray-player' do
 
   url 'http://www.macblurayplayer.com/user/download/Mac_Bluray_Player.dmg'
   homepage 'http://www.macblurayplayer.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Blu-ray Player.app'
 end
