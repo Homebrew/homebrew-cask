a3684c723a71885051e61323318f3d9a43647279
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:26 +0000

    izip.rb: change ':unknown' license comment

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -4,7 +4,7 @@ cask :v1 => 'izip' do
 
   url 'http://www.izip.com/izip.dmg'
   homepage 'http://www.izip.com'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'iZip.app'
 end
