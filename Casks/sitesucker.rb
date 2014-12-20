3058e4eeb4bc05699a4ff2b11d5d7b61b7766e82
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:13 +0000

    sitesucker.rb: change ':unknown' license comment

--- Casks/sitesucker.rb
+++ Casks/sitesucker.rb
@@ -4,7 +4,7 @@ cask :v1 => 'sitesucker' do
 
   url 'http://www.sitesucker.us/archive/latest/SiteSucker.zip'
   homepage 'http://www.sitesucker.us/mac/mac.html'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'SiteSucker/SiteSucker.app'
 end
