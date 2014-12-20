55ca31bcfae783df8f49cdd1475da535f2191ab4
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:20 +0000

    hunt-x.rb: change ':unknown' license comment

--- Casks/hunt-x.rb
+++ Casks/hunt-x.rb
@@ -4,7 +4,7 @@ cask :v1 => 'hunt-x' do
 
   url 'http://huntx.mobilefirst.in/Apps/Hunt%20X.zip'
   homepage 'http://huntx.mobilefirst.in/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Hunt X.app'
 end
