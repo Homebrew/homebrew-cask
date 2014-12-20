42d2efeb2a29d8648b7b3bdd1e1de376689436ca
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:31 +0000

    tuneup.rb: change ':unknown' license comment

--- Casks/tuneup.rb
+++ Casks/tuneup.rb
@@ -4,7 +4,7 @@ cask :v1 => 'tuneup' do
 
   url 'http://www.tuneupmedia.com/redirect/mac-download'
   homepage 'http://www.tuneupmedia.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'TuneUp.app'
 end
