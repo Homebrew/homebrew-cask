c10a8c69709fadb98c52e2582ef0e36d8664cc32
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:14 +0000

    sketchupviewer.rb: change ':unknown' license comment

--- Casks/sketchupviewer.rb
+++ Casks/sketchupviewer.rb
@@ -5,7 +5,7 @@ cask :v1 => 'sketchupviewer' do
   # downloads can be found at http://www.sketchup.com/download/all
   url 'https://dl.trimble.com/sketchup/SketchUpViewer-en.dmg'
   homepage 'http://www.sketchup.com/intl/en/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'SketchUpViewer.app'
 end
