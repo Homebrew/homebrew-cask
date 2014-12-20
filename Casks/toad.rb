a31a066989bc30d82365441c34beee6b86bb387a
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:29 +0000

    toad.rb: change ':unknown' license comment

--- Casks/toad.rb
+++ Casks/toad.rb
@@ -4,7 +4,7 @@ cask :v1 => 'toad' do
 
   url "http://community-downloads.quest.com/toadsoft/toadmacedition/ToadMacEdition_#{version.gsub('.','')}.pkg"
   homepage 'http://www.toadworld.com/products/toad-mac-edition/default.aspx'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg 'ToadMacEdition_108.pkg'
 
