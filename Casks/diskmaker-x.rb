f8f1de3a7e297cbd5d2b96a9159bd481c73db5a3
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:02 +0000

    diskmaker-x.rb: change ':unknown' license comment

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -4,7 +4,7 @@ cask :v1 => 'diskmaker-x' do
 
   url "http://diskmakerx.com/downloads/DiskMakerX#{version}.dmg"
   homepage 'http://diskmakerx.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app "DiskMaker X #{version}.app"
 end
