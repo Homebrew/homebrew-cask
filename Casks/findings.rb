5f31b80ae512dae1fbd342282d014d8cf9a45535
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:09 +0000

    findings.rb: change ':unknown' license comment

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -5,7 +5,7 @@ cask :v1 => 'findings' do
   url 'http://downloads.findingsapp.com/findings'
   appcast 'http://downloads.findingsapp.com/appcast.xml'
   homepage 'http://findingsapp.com'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Findings.app'
 end
