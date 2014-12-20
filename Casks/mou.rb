bc6a78e4cfbc3ae3cee86bbbb8e725a465f94e45
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:43 +0000

    mou.rb: change ':unknown' license comment

--- Casks/mou.rb
+++ Casks/mou.rb
@@ -5,7 +5,7 @@ cask :v1 => 'mou' do
   url 'http://25.io/mou/download/Mou.zip'
   appcast 'http://25.io/mou/up/updates.xml'
   homepage 'http://25.io/mou/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Mou.app'
 
