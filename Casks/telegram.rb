e6a713dfb0abb651908b5fe6d3959b45a2c93029
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:25 +0000

    telegram.rb: change ':unknown' license comment

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -5,7 +5,7 @@ cask :v1 => 'telegram' do
   url 'https://rink.hockeyapp.net/api/2/apps/c55f5e74ae5d0ad254df29f71a1b5f0e/?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/c55f5e74ae5d0ad254df29f71a1b5f0e'
   homepage 'https://vk.com/telegram_osx'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Telegram.app'
 end
