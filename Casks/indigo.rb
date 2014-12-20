1dcc601dfea5e7ee6378eb0ae49d17b4a86ca9d3
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:23 +0000

    indigo.rb: change ':unknown' license comment

--- Casks/indigo.rb
+++ Casks/indigo.rb
@@ -4,7 +4,7 @@ cask :v1 => 'indigo' do
 
   url 'http://cloud.goprism.com/download/Indigo.dmg'
   homepage 'http://www.perceptiveautomation.com/indigo/index.html'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg 'Indigo Installer.pkg'
 
