0cc60463c386586ed5506fefbe85777793b8df20
Roland Walker <walker@pobox.com>
2014-12-20 15:22:55 -0500

    remove String class monkeypatches: start_with? and
    undent_..._72

--- lib/homebrew-fork/Library/Homebrew/extend/string.rb
+++ lib/homebrew-fork/Library/Homebrew/extend/string.rb
@@ -3,27 +3,6 @@ class String
     gsub(/^.{#{(slice(/^ +/) || '').length}}/, '')
   end
 
-  # eg:
-  #   if foo then <<-EOS.undent_________________________________________________________72
-  #               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-  #               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-  #               minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-  #               ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-  #               voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-  #               sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-  #               mollit anim id est laborum.
-  #               EOS
-  alias_method :undent_________________________________________________________72, :undent
-
-  def start_with?(*prefixes)
-    prefixes.any? do |prefix|
-      if prefix.respond_to?(:to_str)
-        prefix = prefix.to_str
-        self[0, prefix.length] == prefix
-      end
-    end
-  end unless method_defined?(:start_with?)
-
   # String.chomp, but if result is empty: returns nil instead.
   # Allows `chuzzle || foo` short-circuits.
   def chuzzle
