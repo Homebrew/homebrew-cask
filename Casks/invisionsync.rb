674abb850d144f175a4941b742786f9a65e1a7ca
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:24 +0000

    invisionsync.rb: change ':unknown' license comment

--- Casks/invisionsync.rb
+++ Casks/invisionsync.rb
@@ -4,7 +4,7 @@ cask :v1 => 'invisionsync' do
 
   url 'http://www.invisionapp.com/app/InVisionSync.zip'
   homepage 'http://invisionapp.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'InVisionSync.app'
 end
