b79a4a3b74371882ddd9159e9ca53a2b828d88cd
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:49 +0000

    onlive-client.rb: change ':unknown' license comment

--- Casks/onlive-client.rb
+++ Casks/onlive-client.rb
@@ -4,7 +4,7 @@ cask :v1 => 'onlive-client' do
 
   url 'https://games.onlive.com/client/mac.pkg'
   homepage 'http://games.onlive.com'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg 'mac.pkg'
 
