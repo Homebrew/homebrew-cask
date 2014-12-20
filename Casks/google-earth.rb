532e8cd0b6b6cf9b00494006efbe378694d9d824
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:15 +0000

    google-earth.rb: change ':unknown' license comment

--- Casks/google-earth.rb
+++ Casks/google-earth.rb
@@ -4,7 +4,7 @@ cask :v1 => 'google-earth' do
 
   url 'https://dl.google.com/earth/client/advanced/current/GoogleEarthMac-Intel.dmg'
   homepage 'https://www.google.com/earth/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Google Earth.app'
 
