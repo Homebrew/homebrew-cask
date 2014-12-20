2bf1b41f5567cb58a849c0ce7ef130384a8c017a
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:54 +0000

    chronicle.rb: change ':unknown' license comment

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -5,7 +5,7 @@ cask :v1 => 'chronicle' do
   url 'http://chronicleapp.com/static/downloads/chronicle.zip'
   appcast 'http://www.littlefin.com/downloads/chronicle3.xml'
   homepage 'http://chronicleapp.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Chronicle.app'
 end
