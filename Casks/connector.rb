3dac821f32f48499d7341361e027d1c6ee4ec52e
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:57 +0000

    connector.rb: change ':unknown' license comment

--- Casks/connector.rb
+++ Casks/connector.rb
@@ -4,7 +4,7 @@ cask :v1 => 'connector' do
 
   url "http://update.mediaware.sk/numpad/connector-#{version}.zip"
   homepage 'http://mediaware.sk/connector'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Connector.app'
 end
