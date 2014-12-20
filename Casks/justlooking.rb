7dc5612f9b3481d2fedaba4d9e30390746378d59
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:28 +0000

    justlooking.rb: change ':unknown' license comment

--- Casks/justlooking.rb
+++ Casks/justlooking.rb
@@ -4,7 +4,7 @@ cask :v1 => 'justlooking' do
 
   url "http://chipmunkninja.com/download/JustLooking-#{version}.dmg"
   homepage 'http://chipmunkninja.com/JustLooking'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'JustLooking.app'
 end
