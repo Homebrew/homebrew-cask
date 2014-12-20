ad515b382ff3fa60c26855c03a12f9d5392871e3
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:55 +0000

    picturesque.rb: change ':unknown' license comment

--- Casks/picturesque.rb
+++ Casks/picturesque.rb
@@ -5,7 +5,7 @@ cask :v1 => 'picturesque' do
   url 'http://www.acqualia.com/files/download.php?product=picturesque'
   appcast 'http://www.acqualia.com/picturesque/appcast/appcast3.xml'
   homepage 'http://www.acqualia.com/picturesque/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Picturesque.app'
 end
