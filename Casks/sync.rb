5346603fd25da0cefc4169e78d7d655a210d7d59
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:23 +0000

    sync.rb: change ':unknown' license comment

--- Casks/sync.rb
+++ Casks/sync.rb
@@ -4,7 +4,7 @@ cask :v1 => 'sync' do
 
   url 'https://www.sync.com/download/apple/Sync.dmg'
   homepage 'https://www.sync.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Sync.app'
 end
