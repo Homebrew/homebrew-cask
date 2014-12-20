f2132e3cd3d8bef2501d6949421b2796dedccba8
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:38 +0000

    mapture.rb: change ':unknown' license comment

--- Casks/mapture.rb
+++ Casks/mapture.rb
@@ -4,7 +4,7 @@ cask :v1 => 'mapture' do
 
   url "http://anatoo.jp/mapture/Mapture-#{version}.app.zip"
   homepage 'http://anatoo.jp/mapture/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app "Mapture-#{version}.app"
 end
