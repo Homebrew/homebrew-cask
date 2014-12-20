1f7f1eb9abeb127a218a717d49b027cd66b27a63
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:57 +0000

    pleasesleep.rb: change ':unknown' license comment

--- Casks/pleasesleep.rb
+++ Casks/pleasesleep.rb
@@ -4,7 +4,7 @@ cask :v1 => 'pleasesleep' do
 
   url 'http://www.dragonone.com/downloads/vault/macosx/PleaseSleep.dmg'
   homepage 'http://www.dragonone.com/products/macosx/pleasesleep/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'PleaseSleep.app'
 end
