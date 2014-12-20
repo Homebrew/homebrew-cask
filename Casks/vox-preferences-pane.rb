4257c3a5cb3350aaf2d6b2330d595c194126abc5
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:37 +0000

    vox-preferences-pane.rb: change ':unknown' license comment

--- Casks/vox-preferences-pane.rb
+++ Casks/vox-preferences-pane.rb
@@ -4,7 +4,7 @@ cask :v1 => 'vox-preferences-pane' do
 
   url 'http://dl.devmate.com/com.coppertino.VoxPrefs/VoxPrefs.dmg'
   homepage 'http://coppertino.com/vox/addon.html'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   prefpane 'Vox Preferences.prefPane'
 end
