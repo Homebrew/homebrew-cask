527eb842833619f7d32459363adf7f4c5d5427ef
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:42 +0000

    all2mp3.rb: change ':unknown' license comment

--- Casks/all2mp3.rb
+++ Casks/all2mp3.rb
@@ -5,7 +5,7 @@ cask :v1 => 'all2mp3' do
   url 'http://www.tresrrr.com/Program/All2MP3.zip'
   appcast 'http://www.tresrrr.com/All2MP3/Appcast.xml'
   homepage 'http://www.tresrrr.com/All2MP3/ENGLISH.html'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'All2MP3.app'
 end
