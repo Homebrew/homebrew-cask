33d7a4787b301f9c5f9cd3ce8b1a6f9175baa63f
Roland Walker <walker@pobox.com>
2014-12-20 14:46:14 -0500

    pare back homebrew-fork global.rb
    first pass: more can be removed

--- lib/homebrew-fork/Library/Homebrew/global.rb
+++ lib/homebrew-fork/Library/Homebrew/global.rb
@@ -9,9 +9,6 @@ require 'rbconfig'
 
 ARGV.extend(HomebrewArgvExtension)
 
-HOMEBREW_VERSION = '0.9.5'
-HOMEBREW_WWW = 'http://brew.sh'
-
 def cache
   if ENV['HOMEBREW_CACHE']
     Pathname.new(ENV['HOMEBREW_CACHE'])
@@ -37,9 +34,6 @@ end
 HOMEBREW_CACHE = cache
 undef cache
 
-# Where brews installed via URL are cached
-HOMEBREW_CACHE_FORMULA = HOMEBREW_CACHE+"Formula"
-
 if not defined? HOMEBREW_BREW_FILE
   HOMEBREW_BREW_FILE = ENV['HOMEBREW_BREW_FILE'] || which('brew').to_s
 end
@@ -47,40 +41,14 @@ end
 HOMEBREW_PREFIX = Pathname.new(HOMEBREW_BREW_FILE).dirname.parent # Where we link under
 HOMEBREW_REPOSITORY = Pathname.new(HOMEBREW_BREW_FILE).realpath.dirname.parent # Where .git is found
 HOMEBREW_LIBRARY = HOMEBREW_REPOSITORY/"Library"
-HOMEBREW_CONTRIB = HOMEBREW_REPOSITORY/"Library/Contributions"
-
-# Where we store built products; /usr/local/Cellar if it exists,
-# otherwise a Cellar relative to the Repository.
-HOMEBREW_CELLAR = if (HOMEBREW_PREFIX+"Cellar").exist?
-  HOMEBREW_PREFIX+"Cellar"
-else
-  HOMEBREW_REPOSITORY+"Cellar"
-end
-
-HOMEBREW_LOGS = Pathname.new(ENV['HOMEBREW_LOGS'] || '~/Library/Logs/Homebrew/').expand_path
 
 HOMEBREW_TEMP = Pathname.new(ENV.fetch('HOMEBREW_TEMP', '/tmp'))
 
-if RbConfig.respond_to?(:ruby)
-  RUBY_PATH = Pathname.new(RbConfig.ruby)
-else
-  RUBY_PATH = Pathname.new(RbConfig::CONFIG["bindir"]).join(
-    RbConfig::CONFIG["ruby_install_name"] + RbConfig::CONFIG["EXEEXT"]
-  )
-end
-RUBY_BIN = RUBY_PATH.dirname
-
-if RUBY_PLATFORM =~ /darwin/
-  MACOS_FULL_VERSION = `/usr/bin/sw_vers -productVersion`.chomp
-  MACOS_VERSION = MACOS_FULL_VERSION[/10\.\d+/]
-  OS_VERSION = "Mac OS X #{MACOS_FULL_VERSION}"
-else
-  MACOS_FULL_VERSION = MACOS_VERSION = "0"
-  OS_VERSION = RUBY_PLATFORM
-end
+MACOS_FULL_VERSION = `/usr/bin/sw_vers -productVersion`.chomp
+MACOS_VERSION = MACOS_FULL_VERSION[/10\.\d+/]
 
 HOMEBREW_GITHUB_API_TOKEN = ENV["HOMEBREW_GITHUB_API_TOKEN"]
-HOMEBREW_USER_AGENT = "Homebrew #{HOMEBREW_VERSION} (Ruby #{RUBY_VERSION}-#{RUBY_PATCHLEVEL}; #{OS_VERSION})"
+HOMEBREW_USER_AGENT = "Homebrew-cask v0.51+ (Ruby #{RUBY_VERSION}-#{RUBY_PATCHLEVEL}; #{MACOS_VERSION})"
 
 HOMEBREW_CURL_ARGS = '-f#LA'
 
@@ -91,13 +59,3 @@ module Homebrew
   attr_accessor :failed
   alias_method :failed?, :failed
 end
-
-HOMEBREW_PULL_OR_COMMIT_URL_REGEX = %r[https://github\.com/([\w-]+)/homebrew(-[\w-]+)?/(?:pull/(\d+)|commit/[0-9a-fA-F]{4,40})]
-
-ORIGINAL_PATHS = ENV['PATH'].split(File::PATH_SEPARATOR).map{ |p| Pathname.new(p).expand_path rescue nil }.compact.freeze
-
-SUDO_BAD_ERRMSG = <<-EOS.undent
-  You can use brew with sudo, but only if the brew executable is owned by root.
-  However, this is both not recommended and completely unsupported so do so at
-  your own risk.
-EOS
