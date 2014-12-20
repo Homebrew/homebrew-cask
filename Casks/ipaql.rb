bfe5c65ccfd51b8185409d9177f552cd6a752687
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:24 +0000

    ipaql.rb: change ':unknown' license comment

--- Casks/ipaql.rb
+++ Casks/ipaql.rb
@@ -4,7 +4,7 @@ cask :v1 => 'ipaql' do
 
   url "http://ipaql.com/site/assets/files/1006/ipaql_#{version.gsub('.','-')}.zip"
   homepage 'http://ipaql.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   qlplugin 'ipaql.qlgenerator'
 end
