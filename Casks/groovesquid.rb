fcca3cf070d1a58d6917ec07e2b9194621946584
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:17 +0000

    groovesquid.rb: change ':unknown' license comment

--- Casks/groovesquid.rb
+++ Casks/groovesquid.rb
@@ -4,7 +4,7 @@ cask :v1 => 'groovesquid' do
 
   url 'http://groovesquid.com/download.php?file=Groovesquid.dmg'
   homepage 'http://groovesquid.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Groovesquid.app'
 end
