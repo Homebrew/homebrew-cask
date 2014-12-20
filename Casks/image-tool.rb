399f70e51b9357be03b7e271bb502220a361bbc9
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:22 +0000

    image-tool.rb: change ':unknown' license comment

--- Casks/image-tool.rb
+++ Casks/image-tool.rb
@@ -4,7 +4,7 @@ cask :v1 => 'image-tool' do
 
   url "http://www.jimmcgowan.net/diskimages/ImageTool#{version}.dmg"
   homepage 'http://www.jimmcgowan.net/Site/ImageTool.html'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Image Tool.app'
 end
