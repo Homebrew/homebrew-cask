9b156642e73173be9f62d2e6cbffb1dab92185b6
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:23 +0000

    synology-photo-station-uploader.rb: change ':unknown' license comment

--- Casks/synology-photo-station-uploader.rb
+++ Casks/synology-photo-station-uploader.rb
@@ -4,7 +4,7 @@ cask :v1 => 'synology-photo-station-uploader' do
 
   url "https://global.download.synology.com/download/Tools/PhotoStationUploader/#{version}/Mac/PhotoStationUploader-#{version}-Mac-Installer.dmg"
   homepage 'http://www.synology.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg "PhotoStationUploader-#{version}-Mac-Installer.pkg"
 
