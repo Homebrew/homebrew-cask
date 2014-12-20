15d36c9d50642ab266599f49c7214812fcbabff4
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:59 +0000

    prizmo.rb: change ':unknown' license comment

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -5,7 +5,7 @@ cask :v1 => 'prizmo' do
   url 'http://www.creaceed.com/downloads/prizmo2.zip'
   appcast 'http://www.creaceed.com/appcasts/prizmo2.xml'
   homepage 'http://www.creaceed.com/prizmo'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Prizmo.app'
 end
