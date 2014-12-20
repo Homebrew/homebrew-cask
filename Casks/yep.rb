e7f45b11f7917d9c913f42628fd2a9abd63da061
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:44 +0000

    yep.rb: change ':unknown' license comment

--- Casks/yep.rb
+++ Casks/yep.rb
@@ -4,7 +4,7 @@ cask :v1 => 'yep' do
 
   url 'http://www.ironicsoftware.com/downloads/Yep.zip'
   homepage 'http://www.ironicsoftware.com/yep/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Yep.app'
 end
