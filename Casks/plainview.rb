6c297f350fcefbdbfb98e573949a7af0ee9fcf80
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:56 +0000

    plainview.rb: change ':unknown' license comment

--- Casks/plainview.rb
+++ Casks/plainview.rb
@@ -4,7 +4,7 @@ cask :v1 => 'plainview' do
 
   url "https://s3.amazonaws.com/plainviewapp/plainview_#{version}.zip"
   homepage 'http://barbariangroup.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Plainview.app'
 end
