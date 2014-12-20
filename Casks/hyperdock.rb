31219b4d70e49be26f85949be1c2105664178c0c
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:20 +0000

    hyperdock.rb: change ':unknown' license comment

--- Casks/hyperdock.rb
+++ Casks/hyperdock.rb
@@ -4,7 +4,7 @@ cask :v1 => 'hyperdock' do
 
   url 'http://hyperdock.bahoom.com/HyperDock.dmg'
   homepage 'http://hyperdock.bahoom.com/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   prefpane 'HyperDock.prefpane'
 end
