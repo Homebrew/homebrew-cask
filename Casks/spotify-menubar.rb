ce55cdf3c1cd9896718ec6695c626688d5022687
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:19 +0000

    spotify-menubar.rb: change ':unknown' license comment

--- Casks/spotify-menubar.rb
+++ Casks/spotify-menubar.rb
@@ -5,7 +5,7 @@ cask :v1 => 'spotify-menubar' do
   url 'http://lifeupnorth.co.uk/files/Spotify_Menubar.zip'
   appcast 'http://lifeupnorth.co.uk/lun/sparkle/sm.xml'
   homepage 'http://lifeupnorth.co.uk/Spotify-Menubar/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Spotify Menubar.app'
 end
