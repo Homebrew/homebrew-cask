4917e85d962d9e8aa106ec340e49b1fb5c79ee0f
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:58 +0000

    preference-manager.rb: change ':unknown' license comment

--- Casks/preference-manager.rb
+++ Casks/preference-manager.rb
@@ -5,7 +5,7 @@ cask :v1 => 'preference-manager' do
   url 'http://download.digitalrebellion.com/Pref_Man.dmg'
   appcast 'http://www.digitalrebellion.com/rss/appcasts/pref_man_appcast.xml'
   homepage 'http://www.digitalrebellion.com/prefman'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Preference Manager.app'
 end
