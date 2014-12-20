985aadbdb7011b7d422096a999f2c8bc3779ba10
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:42 +0000

    alinof-timer.rb: change ':unknown' license comment

--- Casks/alinof-timer.rb
+++ Casks/alinof-timer.rb
@@ -4,7 +4,7 @@ cask :v1 => 'alinof-timer' do
 
   url 'http://www.alinofsoftware.ch/resources/AlinofTimer.pkg'
   homepage 'http://www.alinofsoftware.ch/en/products/products-timer/index.html'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg 'AlinofTimer.pkg', :allow_untrusted => true
 
