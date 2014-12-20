68450356499667a9d75207ca6e46ff9eb59b549d
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:41 +0000

    minitimer.rb: change ':unknown' license comment

--- Casks/minitimer.rb
+++ Casks/minitimer.rb
@@ -4,7 +4,7 @@ cask :v1 => 'minitimer' do
 
   url "http://kevingessner.com/public/downloads/miniTimer/#{version}/miniTimer.dmg"
   homepage 'http://kevingessner.com/software/minitimer/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'miniTimer.app'
 end
