1a8ad717077e0aa15f64c9c452e6a09fe5046be4
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:41 +0000

    mixture.rb: change ':unknown' license comment

--- Casks/mixture.rb
+++ Casks/mixture.rb
@@ -5,7 +5,7 @@ cask :v1 => 'mixture' do
   url 'https://s3.amazonaws.com/mixture-mixed-app/mac/Mixture.zip'
   appcast 'https://s3.amazonaws.com/mixture-mixed-app/mac/appcast.xml'
   homepage 'http://mixture.io/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Mixture.app'
 end
