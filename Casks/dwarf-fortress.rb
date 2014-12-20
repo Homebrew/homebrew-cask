a6f1788babe0f34341d954fe5ffb61f9294eeb15
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:04 +0000

    dwarf-fortress.rb: change ':unknown' license comment

--- Casks/dwarf-fortress.rb
+++ Casks/dwarf-fortress.rb
@@ -4,7 +4,7 @@ cask :v1 => 'dwarf-fortress' do
 
   url "http://www.bay12games.com/dwarves/df_#{version.sub(%r{^0+\.},'').gsub('.','_')}_osx.tar.bz2"
   homepage 'http://www.bay12games.com/dwarves/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   # todo: should this be
   #
