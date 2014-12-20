fab86fe4862d885a2cd1c09fe668363dce41c261
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:45:55 +0000

    clion-eap.rb: change ':unknown' license comment

--- Casks/clion-eap.rb
+++ Casks/clion-eap.rb
@@ -4,7 +4,7 @@ cask :v1 => 'clion-eap' do
 
   url "http://download.jetbrains.com/cpp/CLion-#{version}.dmg"
   homepage 'http://www.jetbrains.com/clion/'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'CLion EAP.app'
 end
