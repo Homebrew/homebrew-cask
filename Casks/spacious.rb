15df11563b672457a8686700e532fbabe6efe5da
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:17 +0000

    spacious.rb: change ':unknown' license comment

--- Casks/spacious.rb
+++ Casks/spacious.rb
@@ -4,7 +4,7 @@ cask :v1 => 'spacious' do
 
   url 'http://static.iospirit.com/static/objectfiles/file/159/Spacious.zip'
   homepage 'http://www.iospirit.com/products/spacious'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Spacious.app'
 end
