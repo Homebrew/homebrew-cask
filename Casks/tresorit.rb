094be02594b81cbc7cc1f5bd5f22399be6978cd2
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:47:31 +0000

    tresorit.rb: change ':unknown' license comment

--- Casks/tresorit.rb
+++ Casks/tresorit.rb
@@ -4,7 +4,7 @@ cask :v1 => 'tresorit' do
 
   url 'https://installerstorage.blob.core.windows.net/install/Tresorit.dmg'
   homepage 'http://tresorit.com'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Tresorit.app'
 end
