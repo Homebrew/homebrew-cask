18d6e5e5f13e1c7fd92267ba57e79954bedc9c74
Vítor Galvão <info@vitorgalvao.com>
2014-12-20 13:46:56 +0000

    pixel-winch.rb: change ':unknown' license comment

--- Casks/pixel-winch.rb
+++ Casks/pixel-winch.rb
@@ -4,7 +4,7 @@ cask :v1 => 'pixel-winch' do
 
   url 'http://ricciadams.com/try/pixel-winch'
   homepage 'http://ricciadams.com/projects/pixel-winch'
-  license :unknown    # todo: improve this machine-generated value
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Pixel Winch.app'
 end
