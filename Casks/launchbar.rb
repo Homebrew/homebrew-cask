9bfb90088dbde416821d67e5b0f78cda6170baad
Marcel Bischoff <marcel@herrbischoff.com>
2014-12-21 05:04:40 +0100

    Update Launchbar to v6.1.6

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -5,8 +5,8 @@ cask :v1 => 'launchbar' do
     sha256 '22a1ec0c10de940e5efbcccd18b8b048d95fb7c63213a01c7976a76d6be69a4d'
     url "http://www.obdev.at/downloads/launchbar/legacy/LaunchBar-#{version}.dmg"
   else
-    version '6.1.5'
-    sha256 '19e226f46e06efac9c294f5940ddf69fefe353bc2705f987d1a301fa6c0e2781'
+    version '6.1.6'
+    sha256 '7ae2d6fdb9d12817cd1ed49cfd35606cb7f3a3162c369108555ee8adc2940335'
     url "http://www.obdev.at/downloads/launchbar/LaunchBar-#{version}.dmg"
   end
 
