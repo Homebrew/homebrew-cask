e854c151f09a153a711f7f2db8b9111d24ea57aa
dwihn0r <dwihn0r@users.noreply.github.com>
2014-12-21 16:07:51 +0100

    Updated Kid3 to 3.1.2

--- Casks/kid3.rb
+++ Casks/kid3.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'kid3' do
   # note: "3" is not a version number, but an intrinsic part of the product name (ID3 tags)
-  version '3.1.1'
-  sha256 '1a27b9743a4029221601fb872a38aca235a23f8234c7a07fa0cb547eb58129c6'
+  version '3.1.2'
+  sha256 'da90a482d92164e3fbc2493b10aab9ddf3669f3feec14fbfdf4eafbed78aab95'
 
   url "http://downloads.sourceforge.net/sourceforge/kid3/kid3-#{version}-Darwin.dmg"
   homepage 'http://kid3.sourceforge.net/'
