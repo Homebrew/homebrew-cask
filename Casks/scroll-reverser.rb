dd66e156cfb0a14eada5ad660b42223c62ea2a35
Roland Walker <walker@pobox.com>
2014-12-22 08:46:14 -0500

    scroll-reverser 1.7 update
    * new version 1.7
    * update license
    * comment cloudfront.net download URL

--- Casks/scroll-reverser.rb
+++ Casks/scroll-reverser.rb
@@ -4,15 +4,16 @@ cask :v1 => 'scroll-reverser' do
     version '1.5.1'
     sha256 '7d31ac1f2a2fbdc06db200da0aeb6ce7fea9ffff712b70ddffbfb769d4fd0f25'
   else
-    version '1.6.1'
-    sha256 'babce7e360ec76a797ac8843e20d5b75a0475807c487df87c01bc29cd8050584'
+    version '1.7'
+    sha256 'acd0e1fe66951299181b2dff8acc772c124ec15df33645a4a9f33d237562716d'
     appcast 'http://softwareupdate.pilotmoon.com/update/scrollreverser/appcast.xml',
             :sha256 => 'ed8e2d6748eb8b79897ca1b84a1597e86afb7845b469a278c83c07e08e6eb886'
   end
 
+  # cloudfront.net is the official download host per the vendor appcast
   url "https://d20vhy8jiniubf.cloudfront.net/downloads/ScrollReverser-#{version}.zip"
   homepage 'https://pilotmoon.com/scrollreverser/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'Scroll Reverser.app'
 end
