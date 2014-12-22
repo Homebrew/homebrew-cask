3b9378246cd80ab336faf76b705d5fd6fee41153
Johnathan Conley <johnathan.conley@gmail.com>
2014-12-22 16:46:24 -0600

    update clamxav to 2.7

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'clamxav' do
-  version '2.6.4'
-  sha256 'bbde8181307566bd592930f7318a7c43e253788bf44bab9bae1140b1e50e694f'
+  version '2.7'
+  sha256 '2144b4bbd444e18e16d9a07419247ba2377ac46e1c7631d5e88970045885ce80'
 
   url "http://www.clamxav.com/downloads/ClamXav_#{version}.dmg"
   appcast 'http://www.clamxav.com/sparkle/profileInfo.php',
-          :sha256 => '1f7fa2a5dfa1e59e32982721d5cf3292d1d2568ee22d802904566275b9e680e0'
+          :sha256 => '01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b'
   homepage 'http://www.clamxav.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
