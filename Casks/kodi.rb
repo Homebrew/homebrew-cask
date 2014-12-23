1e691812c5c689151ef693f2056b5fe7932cd2f6
Chris Burke <chris.burke@cakedc.com>
2014-12-23 20:12:44 +0000

    Rename XBMC to Kodi and amend homepage/license

---
+++ Casks/kodi.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'kodi' do
+  version '14.0'
+  sha256 'bf0f5b836a517edeb78b75424b929c9d42067463232e6f62a2df327a947d9d35'
+
+  url "http://mirrors.xbmc.org/releases/osx/x86_64/kodi-#{version}-Helix-x86_64.dmg"
+  homepage 'http://kodi.tv/'
+  license :gpl
+
+  app 'Kodi.app'
+end
