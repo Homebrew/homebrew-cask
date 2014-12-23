c0dd475bc0b3add9e2104e226e0e44551008045d
anurag-banerjee <anuragb2010@gmail.com>
2014-12-23 14:26:04 -0500

    Add Nibbble.app v1.2

---
+++ Casks/nibbble.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'nibbble' do
+  version '1.2'
+  sha256 '7e6bff1d0ab8951c528cbe0ae34c01f890f9e02a52fa18940174cbe965eab5d4'
+
+  url 'http://uglyapps.co.uk/nibbble/nibbble.1.2.zip'
+  name 'Nibbble'
+  homepage 'http://uglyapps.co.uk/nibbble/'
+  license :gratis
+
+  screen_saver 'Nibbble.saver'
+end
