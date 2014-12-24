d4ccd0de3d10586746116418e309fdd3c11c88f6
Vítor Galvão <info@vitorgalvao.com>
2014-12-24 17:51:04 +0000

    textexpander.rb: simplified homepage

--- Casks/textexpander.rb
+++ Casks/textexpander.rb
@@ -10,7 +10,7 @@ cask :v1 => 'textexpander' do
   end
 
   url "http://cdn.smilesoftware.com/TextExpander_#{version}.zip"
-  homepage 'http://www.smilesoftware.com/TextExpander/index.html'
+  homepage 'http://www.smilesoftware.com/TextExpander/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'TextExpander.app'
