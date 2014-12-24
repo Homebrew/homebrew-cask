dcd2ee9d1b6b749c3aa6611f5720a57763995089
Vítor Galvão <info@vitorgalvao.com>
2014-12-24 17:47:32 +0000

    pdfpen.rb: simplified homepage

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -3,7 +3,7 @@ cask :v1 => 'pdfpen' do
   sha256 :no_check
 
   url 'http://dl.smilesoftware.com/com.smileonmymac.PDFpen/PDFpen.zip'
-  homepage 'http://smilesoftware.com/PDFpen/index.html'
+  homepage 'http://smilesoftware.com/PDFpen/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'PDFpen.app'
