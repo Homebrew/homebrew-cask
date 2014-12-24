f44f83cfefef575cb06a3fcc7a6b0c67d5aa4ac2
Vítor Galvão <info@vitorgalvao.com>
2014-12-24 17:43:54 +0000

    mactubes.rb: simplified homepage

--- Casks/mactubes.rb
+++ Casks/mactubes.rb
@@ -3,7 +3,7 @@ cask :v1 => 'mactubes' do
   sha256 'd3fbe0ee7fb95c1eea0f70b84140a816561601f0a1f36ba2b16ba63bfa57cd19'
 
   url "http://macapps.sakura.ne.jp/mactubes/archives/MacTubes_v#{version}.zip"
-  homepage 'http://macapps.sakura.ne.jp/mactubes/index_en.html'
+  homepage 'http://macapps.sakura.ne.jp/mactubes/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app "MacTubes_v#{version}/MacTubes.app"
