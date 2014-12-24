ac8a661b8c0512289772e8093eb8ee056861b872
Vítor Galvão <info@vitorgalvao.com>
2014-12-24 17:45:32 +0000

    murasaki.rb: simplified homepage

--- Casks/murasaki.rb
+++ Casks/murasaki.rb
@@ -3,7 +3,7 @@ cask :v1 => 'murasaki' do
   sha256 '2dd07c47d59aff053b8be804e08b087ce8d9e127365de43a206011a63ba42966'
 
   url "http://genjiapp.com/mac/murasaki/downloads/murasaki_v#{version}.zip"
-  homepage 'http://genjiapp.com/mac/murasaki/index_en.html'
+  homepage 'http://genjiapp.com/mac/murasaki/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Murasaki.app'
