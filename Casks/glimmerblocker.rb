9e71d0124618411dc1df38e1add838743d9e1cf3
Josh Hagins <hagins.josh@gmail.com>
2014-12-24 15:22:29 -0600

    Improve uninstall for glimmerblocker

--- Casks/glimmerblocker.rb
+++ Casks/glimmerblocker.rb
@@ -3,10 +3,26 @@ cask :v1 => 'glimmerblocker' do
   sha256 '872f3edc5f6dc3b92ba17eaf00236308e561bf353ffb1579cc5d7afc27bbf0a5'
 
   url "http://glimmerblocker.org/downloads/GlimmerBlocker-#{version}.dmg"
+  name 'GlimmerBlocker'
   homepage 'http://glimmerblocker.org'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg 'GlimmerBlocker.pkg'
 
-  uninstall :pkgutil => 'org.glimmerblocker.pkg'
+  uninstall :pkgutil   => 'org.glimmerblocker.pkg',
+            :launchctl => [
+                           'org.glimmerblocker.proxy',
+                           'org.glimmerblocker.updater'
+                          ],
+            :delete    => '/Library/PreferencePanes/GlimmerBlocker.prefPane'
+
+  zap       :delete    => [
+                           '/Library/GlimmerBlocker',
+                           '/Library/Logs/GlimmerBlocker'
+                          ]
+
+  caveats <<-EOS.undent
+    You must deactivate GlimmerBlocker from the installed preference
+    pane before uninstalling. See http://glimmerblocker.org/wiki/Uninstall.
+  EOS
 end
