f21c37bd51329322ea2f1aa884173ddbf896a3c3
Vítor Galvão <info@vitorgalvao.com>
2014-12-24 17:50:26 +0000

    stackroom.rb: simplified homepage

--- Casks/stackroom.rb
+++ Casks/stackroom.rb
@@ -3,7 +3,7 @@ cask :v1 => 'stackroom' do
   sha256 'b5904f8c39a3941a827cea31e77dc9c62b12025cccddc5b42869615392a543ce'
 
   url "http://www.geocities.jp/aromaticssoft/stackroom/download/stackroom_#{version}.zip"
-  homepage 'http://www.geocities.jp/aromaticssoft/stackroom/index.html'
+  homepage 'http://www.geocities.jp/aromaticssoft/stackroom/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Stackroom.app'
