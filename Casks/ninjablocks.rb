70c3efa166b7dd1fd81ac3ae95c09eab13c4e0b0
Vítor Galvão <info@vitorgalvao.com>
2014-12-24 17:46:15 +0000

    ninjablocks.rb: simplified homepage

--- Casks/ninjablocks.rb
+++ Casks/ninjablocks.rb
@@ -4,7 +4,7 @@ cask :v1 => 'ninjablocks' do
 
   # dropboxusercontent.com is the official download host per the vendor homepage
   url "https://dl.dropboxusercontent.com/u/428557/NinjaBlocks-#{version}.dmg"
-  homepage 'http://forums.ninjablocks.com/index.php?p=/discussion/1655/ninja-osx-client/p1'
+  homepage 'https://discuss.ninjablocks.com/t/ninja-osx-client/1449'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'NinjaBlocks.app'
