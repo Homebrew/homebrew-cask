692aed51562f703cadf9818f468b166783d71183
Josh Hagins <hagins.josh@gmail.com>
2014-12-25 11:45:57 -0600

    Update git-annex
    Add Yosemite and Lion urls, and modify existing urls to use https.
    Also add stricter OS release dependencies.

--- Casks/git-annex.rb
+++ Casks/git-annex.rb
@@ -2,8 +2,10 @@ cask :v1 => 'git-annex' do
   version :latest
   sha256 :no_check
 
-  if MacOS.release <= :mountain_lion
-    url 'http://downloads.kitenet.net/git-annex/OSX/current/10.8.2_Mountain_Lion/git-annex.dmg.bz2'
+  if MacOS.release == :lion
+    url 'https://downloads.kitenet.net/git-annex/OSX/current/10.7.5_Lion/git-annex.dmg'
+  elsif MacOS.release == :mountain_lion
+    url 'https://downloads.kitenet.net/git-annex/OSX/current/10.8.2_Mountain_Lion/git-annex.dmg.bz2'
 
     # This is a horrible hack to force the file extension.  The
     # backend code should be fixed so that this is not needed.
@@ -11,8 +13,13 @@ cask :v1 => 'git-annex' do
       system '/bin/mv', '--', staged_path.join('git-annex-latest'), staged_path.join('git-annex-latest.dmg')
     end
     container :nested => 'git-annex-latest.dmg'
+  elsif MacOS.release == :mavericks
+    url 'https://downloads.kitenet.net/git-annex/OSX/current/10.9_Mavericks/git-annex.dmg'
+  elsif MacOS.release == :yosemite
+    url 'https://downloads.kitenet.net/git-annex/OSX/current/10.10_Yosemite/git-annex.dmg'
   else
-    url 'http://downloads.kitenet.net/git-annex/OSX/current/10.9_Mavericks/git-annex.dmg'
+    # Unusual case: there is no fall-through.  Each version of the software is
+    # specific to an OS X release, so define nothing when the release is unknown.
   end
 
   gpg "#{url}.sig",
@@ -23,6 +30,14 @@ cask :v1 => 'git-annex' do
   app 'git-annex.app'
   binary 'git-annex.app/Contents/MacOS/git-annex'
   binary 'git-annex.app/Contents/MacOS/git-annex-shell'
+
+  depends_on :macos => %w{
+                          :lion
+                          :mountain_lion
+                          :mavericks
+                          :yosemite
+                         }
+
   caveats do
     files_in_usr_local
   end
