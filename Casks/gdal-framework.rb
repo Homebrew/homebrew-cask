9f8f3f813c00bee84a97ef32fc0da89fde11e60a
ndr <ndr@qef.io>
2014-12-26 21:13:07 +0000

    Specify license of gdal-framework

--- Casks/gdal-framework.rb
+++ Casks/gdal-framework.rb
@@ -4,7 +4,7 @@ cask :v1 => 'gdal-framework' do
 
   url "http://www.kyngchaos.com/files/software/frameworks/GDAL_Complete-#{version.sub(%r{^(\d+\.\d+).*},'\1')}.dmg"
   homepage 'http://www.kyngchaos.com/software/frameworks'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :oss
 
   pkg 'GDAL Complete.pkg'
   pkg 'Numpy.pkg'
