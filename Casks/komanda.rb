11c2a3806945693f02484c33752a3e5d43508633
Roland Walker <walker@pobox.com>
2014-12-26 12:12:36 -0500

    remove needless version sub in komanda.rb

--- Casks/komanda.rb
+++ Casks/komanda.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'komanda' do
-  version '1.0.0 beta'
+  version '1.0.0.beta'
   sha256 'e54f195185d92b0b4a9e9be3b1db4b2cbd99723c93e9b6e57feb0df90b27a5d1'
 
-  url "https://github.com/mephux/komanda/releases/download/#{version.gsub(' ','.')}/Komanda-macosx.zip"
+  url "https://github.com/mephux/komanda/releases/download/#{version}/Komanda-macosx.zip"
   homepage 'https://github.com/mephux/komanda'
   license :mit
 
