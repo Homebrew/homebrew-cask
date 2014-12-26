682e2ec443613957cbeeb1c3bd7ca915b8a8153e
William Melody <hi@williammelody.com>
2014-12-25 19:20:59 -0800

    Update GnuCash to 2.6.5-6

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'gnucash' do
-  version '2.6.5-3'
-  sha256 'ed48b381c851446b2f43762a7a0ba833dc90052ce0623cc531ea94794b46f7f4'
+  version '2.6.5-6'
+  sha256 '84a55674ef9a78b0aebf530017cca31204d1270110675c916bee0fb8d838030a'
 
   url "http://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-#{version}.dmg"
   homepage 'http://www.gnucash.org'
