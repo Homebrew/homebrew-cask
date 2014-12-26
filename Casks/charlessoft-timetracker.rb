3e6db46ad289f672f2de6e0ca8bf363cffecff29
Todd Eddy <vr@vrillusions.com>
2014-12-26 12:16:43 -0500

    Add TimeTracker.app v0.4.5

---
+++ Casks/charlessoft-timetracker.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'charlessoft-timetracker' do
+  version '0.4.5'
+  sha256 '6d1b4dd29b2866afa195d7fd18df7868e8c329781e15246f6b1e200451914c04'
+
+  url 'http://charlessoft.com/TimeTracker.zip'
+  name 'TimeTracker'
+  homepage 'http://charlessoft.com/'
+  license :unknown
+
+  app 'TimeTracker.app'
+end
