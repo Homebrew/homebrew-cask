d5c10759e1e6211cf639359f37b6dc50ce6217b2
Benjamin Asbach <benjamin.asbach@gmail.com>
2014-12-27 17:40:56 +0100

    set license to oss
    According the license information on download page (see https://netbeans.org/downloads/ - "Important Legal Information"):
    License: CDDL v1 & GPL v2

--- Casks/netbeans.rb
+++ Casks/netbeans.rb
@@ -4,7 +4,7 @@ cask :v1 => 'netbeans' do
 
   url "http://download.netbeans.org/netbeans/#{version}/final/bundles/netbeans-#{version}-macosx.dmg"
   homepage 'https://netbeans.org/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :oss
 
   pkg "NetBeans #{version}.mpkg"
   # Theoretically this uninstall could conflict with a separate GlassFish
