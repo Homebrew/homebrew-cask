5bbaed909d6570243159a3b1e26a1e11a500e8cb
cncc <cncdvincentc@hotmail.com>
2014-12-27 12:00:56 +0800

    Update qq.rb

--- Casks/qq.rb
+++ Casks/qq.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qq' do
   version '4.0.0'
-  sha256 'dc206b856dda0ef3db8420d7678ddaefafd1e6919a9dc3d3115fc926b20bce35'
+  sha256 '91a6a5f62d9224fec1a4cc53db1855ecc1c73444483fc0efd0fbfcf909ba7ac6'
 
   url "http://dldir1.qq.com/qqfile/QQforMac/QQ_V#{version}.dmg"
   homepage 'http://im.qq.com/macqq/'
