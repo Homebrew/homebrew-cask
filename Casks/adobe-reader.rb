f9d1d7cd96f1375c3746c6a214660c8d912d9900
Richard Lee <dlackty@gmail.com>
2014-12-28 16:51:50 +0800

    Update Adobe Reader to 11.0.10

--- Casks/adobe-reader.rb
+++ Casks/adobe-reader.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'adobe-reader' do
-  version '11.0.09'
-  sha256 '259957f1434bcdf47dc6a7c12affc40dd3c17288009dc229aa51f20ec4e8b1c5'
+  version '11.0.10'
+  sha256 '6aeb93bdd0da7662721e8a81493aef8bd5610d7386ac359bb35f089fbe0ee391'
 
   url "http://ardownload.adobe.com/pub/adobe/reader/mac/#{version.to_i}.x/#{version}/en_US/AdbeRdr#{version.gsub('.', '')}_en_US.dmg"
   homepage 'http://www.adobe.com/products/reader.html'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   pkg 'Adobe Reader XI Installer.pkg'
 
