791b61abd3a22461df0292d7b331415665c3a5a3
Kirill Korolyov <kirill.korolyov@gmail.com>
2014-12-29 23:20:40 +0000

    Add mplayerx name stanza, expanded zap stanza

--- Casks/mplayerx.rb
+++ Casks/mplayerx.rb
@@ -3,10 +3,17 @@ cask :v1 => 'mplayerx' do
   sha256 '08ce85671814e65b8c7ec8438b85be593b6deaf7d5c3b242e686a6b0176a2c77'
 
   url "http://downloads.sourceforge.net/project/mplayerx-osx/MPlayerX-#{version}.zip"
+  name 'MPlayerX'
   homepage 'http://mplayerx.org/'
   license :oss
 
   app 'MPlayerX.app'
 
-  zap :delete => '~/.mplayer'
+  zap :delete => [
+                  '~/.mplayer',
+                  '~/Library/Application Support/MPlayerX',
+                  '~/Library/Preferences/org.niltsh.MPlayerX.LSSharedFileList.plist',
+                  '~/Library/Preferences/org.niltsh.MPlayerX.plist',
+                  '~/Library/Caches/org.niltsh.MPlayerX'
+                 ]
 end
