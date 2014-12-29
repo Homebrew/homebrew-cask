b49365fdfa5ae9b53e5fe6ada9990063cac052d2
Nyx0uf <benjgodard@me.com>
2014-12-29 12:05:46 +0100

    Update qlimagesize.rb
    URL was broken, changed to a .pkg for installation.

--- Casks/qlimagesize.rb
+++ Casks/qlimagesize.rb
@@ -2,9 +2,11 @@ cask :v1 => 'qlimagesize' do
   version :latest
   sha256 :no_check
 
-  url 'http://repo.whine.fr/qlImageSize.qlgenerator-10.8.zip'
+  url 'http://repo.whine.fr/qlImageSize.pkg'
   homepage 'https://github.com/Nyx0uf/qlImageSize'
   license :oss
 
-  qlplugin 'qlImageSize.qlgenerator'
+  pkg 'qlImageSize.pkg'
+
+  uninstall :pkgutil => 'io.whine.qlimagesize.pkg'
 end
