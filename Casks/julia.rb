c4bfb26d7587ea45eac9e77d41e5c4a3b4e5a443
Vladimir Andrijevik <vladimir.andrijevik@gmail.com>
2014-12-29 02:38:19 +0100

    Update julia to v0.3.4

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'julia' do
-  version '0.3.3'
-  sha256 '24df35408e83d38bcf95fcaeefd613beeafe00e2b6d8435f760289c158adc7b9'
+  version '0.3.4'
+  sha256 '34cf0b928f20994a087c0cc550576992bc7abd4cc4e8db206125bb5d04c2ae47'
 
   url "https://s3.amazonaws.com/julialang/bin/osx/x64/0.3/julia-#{version}-osx10.7+.dmg"
   homepage 'http://julialang.org/'
