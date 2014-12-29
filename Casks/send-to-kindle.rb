46b2f22818a7e8a8c07ca81c17bac5a16abf0552
Kirill Korolyov <kirill.korolyov@gmail.com>
2014-12-29 21:43:59 +0000

    Add send-to-kindle plist to zap stanza

--- Casks/send-to-kindle.rb
+++ Casks/send-to-kindle.rb
@@ -12,6 +12,7 @@ cask :v1 => 'send-to-kindle' do
             :pkgutil   => 'com.amazon.SendToKindleMacInstaller.pkg'
   zap       :delete    => [
                            '~/Library/Application Support/Amazon/SendToKindle',
+                           '~/Library/Preferences/com.amazon.SendToKindle.plist',
                            '~/Library/Logs/SendToKindleInstall.log',
                           ]
 end
