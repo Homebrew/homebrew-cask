0c10c1660d56ffe50696cede48b2feef87614691
Ftard5000 <Ftard5000@users.noreply.github.com>
2014-12-29 23:40:58 -0500

    Update aegisub.rb
    Updated version to latest stable, 3.2.2

--- Casks/aegisub.rb
+++ Casks/aegisub.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'aegisub' do
-  version '3.0.4'
-  sha256 '37d8aca8a30619f09b66bcd1d6d4a925388c74b8a06287045e76ba84ca116e70'
+  version '3.2.2'
+  sha256 '3bc2ffed465408e4e79d6b550d49c5adeee7f480430837bb8e14f88e72042e2f'
 
   url "http://ftp.aegisub.org/pub/releases/Aegisub-#{version}.dmg"
   homepage 'http://www.aegisub.org/'
