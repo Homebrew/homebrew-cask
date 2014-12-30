2cbfe34864ca97191034eb7b7bd235535caa7cad
Johnathan Conley <johnathan.conley@gmail.com>
2014-12-30 09:58:38 -0600

    update hermes to 1.2.5

--- Casks/hermes.rb
+++ Casks/hermes.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'hermes' do
-  version '1.2.4'
-  sha256 '28bd29a7daf7665af9caa4a3a9bfaf46479fe6853bea7d345602f673ddd67ada'
+  version '1.2.5'
+  sha256 '786e20040bdd6e943b646f74d7cc9a460b37444272c4a7af8d1f5ef77a7d2f2f'
 
   url "https://s3.amazonaws.com/hermesmacapp/Hermes-#{version}.zip"
   appcast 'http://hermesapp.org/versions.xml',
-          :sha256 => '75880bde3306b76b80dd41dc9e99ca6adb1bcbf1a124dcaa82953e3023aa7c5d'
+          :sha256 => 'cc89f8c651e0335c91d52bb120f769ca4c418c13144155476c572ca5da21fb0e'
   homepage 'http://hermesapp.org/'
   license :mit
 
