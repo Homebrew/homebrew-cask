ed90e8eb2df4d8ff49d9909db9fe44be382a0095
Mahmood Shafeie Zargar <mahmood@gmail.com>
2014-12-31 18:46:37 -0500

    Upgrade dbeaver community/enterprise to 3.1.1
    Upgraded both dbeaver-enterprise and dbeaver-community casks to 3.1.1.

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'dbeaver-enterprise' do
-  version '3.0.2'
+  version '3.1.1'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '8943411eff3074ff7f986fa2f9db4ab185519c4ba20abef67f8fa18d9e270e5b'
+    sha256 '692bf7ab300f0876ab3dac996e61f0b7c367db307c8190754fbf798dbb38daeb'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-ee-macosx.cocoa.x86.zip"
   else
-    sha256 '7751a5a97de1f78735eaba3d69486cc39b8bddb1f4b2fb81173d111a6ee2b8c8'
+    sha256 '27564968a4fd27dcaca5a9639cd787f63f006cab844017c7d094f922d506922d'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-ee-macosx.cocoa.x86_64.zip"
   end
 
