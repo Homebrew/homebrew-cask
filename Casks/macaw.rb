ac6e78c597c371868a7ba8c39f65dfd4935ab6b0
Vítor Galvão <info@vitorgalvao.com>
2014-12-31 00:23:54 +0000

    Added license to macaw

--- Casks/macaw.rb
+++ Casks/macaw.rb
@@ -6,7 +6,7 @@ cask :v1 => 'macaw' do
   appcast 'http://download.macaw.co/appcast.xml',
           :sha256 => '6db73e26f6d1d9a891762e9622e594c79fc0d2bb57ff14a98951e7d8ade37d92'
   homepage 'http://macaw.co/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'Macaw.app'
 end
