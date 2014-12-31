64c3864811c19d0c0569a445879c08f67fa7c2e7
SevenBits <meokid2@yahoo.com>
2014-12-31 15:59:52 -0500

    Updated Mac Linux USB Loader cask

--- Casks/mac-linux-usb-loader.rb
+++ Casks/mac-linux-usb-loader.rb
@@ -1,8 +1,8 @@
-cask :v1 => 'MacLinuxUsbLoader' do
+cask :v1 => 'mac-linux-usb-loader' do
   version '3.0.1'
   sha256 'e3414a390a69d217bdc0583b34dd6212139f7ba23ac0f54986b1a41a82702a08'
 
-  url 'https://github.com/SevenBits/Mac-Linux-USB-Loader/releases/download/v3.0.1/Mac.Linux.USB.Loader.zip'
+  url 'https://github.com/SevenBits/Mac-Linux-USB-Loader/releases/download/v#{version}/Mac.Linux.USB.Loader.zip'
   name 'Mac Linux USB Loader'
   homepage 'http://sevenbits.github.io/Mac-Linux-USB-Loader/'
   license :bsd
