d1b5bc419ba7d3e2633508fbf31d1e63a6c9da89
retrography <mahmood@gmail.com>
2015-01-01 15:08:48 -0500

    Fixed the 64-bit architecture code snippet
    As promised, I fixed the architecture check and reverted it to the standard way.

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'dbeaver-community' do
   version '3.1.1'
 
-  if Hardware::CPU.is_64_bit?
-    sha256 '77eff21ec2b49cf7473aa1fff3b7ccf99ddf93c48470414376f002263f324ef7'
-    url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-macosx.cocoa.x86_64.zip"
-  else
+  if Hardware::CPU.is_32_bit?
     sha256 '0e1eef7f80d880a895fdd8bc205edfa3c82b2d4165de25cfd5c6bd8292682707'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-macosx.cocoa.x86.zip"
+  else
+    sha256 '77eff21ec2b49cf7473aa1fff3b7ccf99ddf93c48470414376f002263f324ef7'
+    url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-macosx.cocoa.x86_64.zip"
   end
 
   homepage 'http://dbeaver.jkiss.org/'
