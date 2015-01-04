ea1f6a878cd3836efc1341fe01b9bb7cf34ca055
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:35:00 +0000

    serviio.rb: added name

--- Casks/serviio.rb
+++ Casks/serviio.rb
@@ -3,6 +3,7 @@ cask :v1 => 'serviio' do
   sha256 '132ed6ba9baf466eec5d89789cd8d50163fa5b534899f10a9232e00f711707aa'
 
   url "http://download.serviio.org/releases/serviio-#{version}-osx.tar.gz"
+  name 'Serviio'
   homepage 'http://serviio.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
