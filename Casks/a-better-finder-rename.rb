95f3e2f2c96054d0b5197dccf5c5d8a61de62088
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 02:05:23 +0000

    a-better-finder-rename.rb: added name

--- Casks/a-better-finder-rename.rb
+++ Casks/a-better-finder-rename.rb
@@ -3,6 +3,7 @@ cask :v1 => 'a-better-finder-rename' do
   sha256 :no_check
 
   url 'http://www.publicspace.net/download/ABFRX.dmg'
+  name 'A Better Finder Rename'
   appcast 'http://www.publicspace.net/app/signed_abfr9.xml'
   homepage 'http://www.publicspace.net/ABetterFinderRename/'
   license :commercial
