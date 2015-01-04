2c5feeec88fe4f12df090d6f79a1e2d0c64b8150
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:54:49 +0000

    moreamp.rb: added name

--- Casks/moreamp.rb
+++ Casks/moreamp.rb
@@ -3,6 +3,7 @@ cask :v1 => 'moreamp' do
   sha256 '770dad0b69979f51807dee3f873e0ebbb755f9f2f2f60c734ac55094e6a6f707'
 
   url "http://downloads.sourceforge.net/project/moreamp/moreamp/MoreAmp-#{version}/MoreAmp-#{version}-binOSX104intel.dmg"
+  name 'MoreAmp'
   homepage 'http://sourceforge.net/projects/moreamp/'
   license :oss
 
