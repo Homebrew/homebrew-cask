fe1919c52d32bc0c2713bff548779218dbccbad0
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 03:10:16 +0000

    archiver.rb: added name

--- Casks/archiver.rb
+++ Casks/archiver.rb
@@ -3,6 +3,7 @@ cask :v1 => 'archiver' do
   sha256 :no_check
 
   url 'https://commondatastorage.googleapis.com/incrediblebee/apps/Archiver/Archiver.zip'
+  name 'Archiver'
   homepage 'http://archiverapp.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
