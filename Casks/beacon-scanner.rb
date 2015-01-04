217814326034f9c884c8d63fcf11508c6a5f0b14
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 21:02:37 +0000

    beacon-scanner.rb: added name

--- Casks/beacon-scanner.rb
+++ Casks/beacon-scanner.rb
@@ -3,6 +3,7 @@ cask :v1 => 'beacon-scanner' do
   sha256 '1c7662420ab616004e0f76320b76bf9ee5c3c75292c553bc891ac4986208bbb4'
 
   url "https://github.com/mlwelles/BeaconScanner/releases/download/#{version}/BeaconScanner.zip"
+  name 'BeaconScanner'
   homepage 'https://github.com/mlwelles/BeaconScanner/'
   license :mit
 
