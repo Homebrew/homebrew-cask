19aca9ab848874c4d9b7a463bb0a4c4e79eb9d8d
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:10:12 +0000

    virtualhostx.rb: added name

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -3,6 +3,7 @@ cask :v1 => 'virtualhostx' do
   sha256 :no_check
 
   url 'https://clickontyler.com/virtualhostx/download/v6/'
+  name 'VirtualHostX'
   appcast 'http://shine.clickontyler.com/appcast.php?id=30'
   homepage 'http://clickontyler.com/virtualhostx/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
