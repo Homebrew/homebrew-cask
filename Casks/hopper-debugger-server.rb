b1f45a3cac52092a5c903e972bd99989405b8594
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 15:49:08 +0000

    renamed hopper-debugger-server and added name

--- Casks/hopper-debugger-server.rb
+++ Casks/hopper-debugger-server.rb
@@ -1,12 +1,15 @@
 cask :v1 => 'hoppergdbserver' do
-  version '1.6'
-  sha256 '124ea3e34f970f412de28de0e482edf527e064efdb954b3804aab248edc3c017'
+  version '2.3'
+  sha256 '291bcabcd84f395d9aba08532c1fff54101ec8d02aeb46e32c82a8a9b621c098'
 
   url "http://www.hopperapp.com/HopperGDBServer/HopperGDBServer-#{version}.zip"
+  name 'Hopper Debugger Server'
+  name 'Hopper GDB Server'
+  name 'HopperGDBServer'
   appcast 'http://www.hopperapp.com/HopperGDBServer/appcast.xml',
           :sha256 => 'b554d3f681960d00d94bdce4db2efe1cc6addc69db9441e359839d6f2379924b'
   homepage 'http://www.hopperapp.com/HopperGDBServer/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  app 'HopperGDBServer.app'
+  app 'Hopper Debugger Server.app'
 end
