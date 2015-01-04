93f71a83f4099ea71b2f1fa322ee29ed1e033867
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:04:18 +0000

    p4merge.rb: added name

--- Casks/p4merge.rb
+++ Casks/p4merge.rb
@@ -3,10 +3,12 @@ cask :v1 => 'p4merge' do
   sha256 'cb8f210435de8186af92e5d376a64e6b5f48c67d1b4e892628de88d54ce4918e'
 
   url "http://filehost.perforce.com/perforce/r#{version.sub(%r{^20},'')}/bin.macosx107x86_64/P4V.dmg"
+  name 'P4Merge'
   homepage 'http://www.perforce.com/product/components/perforce-visual-merge-and-diff-tools'
   license :oss
 
   app 'p4merge.app'
+
   caveats <<-EOS.undent
     git can be configured to use p4merge as a merge tool via
 
