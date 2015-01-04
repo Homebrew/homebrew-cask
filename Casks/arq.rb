9e418694db671eed90d3cde80e80afd424276f6d
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 03:16:14 +0000

    arq.rb: added name

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -3,6 +3,7 @@ cask :v1 => 'arq' do
   sha256 :no_check
 
   url 'http://www.haystacksoftware.com/arq/Arq.zip'
+  name 'Arq'
   homepage 'http://www.haystacksoftware.com/arq/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
