279534c837cd2a0dd22ecb53e0566323fc2894cb
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 16:05:58 +0000

    openoffice.rb: added name

--- Casks/openoffice.rb
+++ Casks/openoffice.rb
@@ -3,8 +3,10 @@ cask :v1 => 'openoffice' do
   sha256 '7becbdbe2abbcfd9d27953d2e11dc07da308d80f208b5f27f6213d225dc95e64'
 
   url "http://downloads.sourceforge.net/sourceforge/openofficeorg.mirror/Apache_OpenOffice_#{version}_MacOS_x86-64_install_en-US.dmg"
+  name 'OpenOffice'
   homepage 'http://www.openoffice.org/'
   license :oss
+  tags :vendor => 'Apache'
 
   app 'OpenOffice.app'
 end
