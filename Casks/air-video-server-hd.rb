0d42bebbccdf361c304e25d6d28f0f715e987295
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 02:36:24 +0000

    air-video-server-hd.rb: added name

--- Casks/air-video-server-hd.rb
+++ Casks/air-video-server-hd.rb
@@ -3,6 +3,7 @@ cask :v1 => 'air-video-server-hd' do
   sha256 'b0a0ef30fcb19de24051fe32e5de0eb601d65664fe71a8cfb4e3243d49c1e1d5'
 
   url "https://s3.amazonaws.com/AirVideoHD/Download/Air+Video+Server+HD+#{version}.dmg"
+  name 'Air Video Server HD'
   homepage 'http://www.inmethod.com/airvideohd'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
