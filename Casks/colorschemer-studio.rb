f98c6a66b742bb5090d52a789612636dce463835
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 15:54:42 +0000

    colorschemer-studio.rb: added name

--- Casks/colorschemer-studio.rb
+++ Casks/colorschemer-studio.rb
@@ -3,6 +3,7 @@ cask :v1 => 'colorschemer-studio' do
   sha256 :no_check
 
   url 'https://www.colorschemer.com/colorschemerstudio.dmg'
+  name 'ColorSchemer Studio'
   appcast 'http://www.colorschemer.com/appcast/studio2_mac.xml'
   homepage 'http://www.colorschemer.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
