ed6f1d83a31a9fcad7f7612b4677ef438ff734e2
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 02:28:36 +0000

    adapter.rb: added name

--- Casks/adapter.rb
+++ Casks/adapter.rb
@@ -3,6 +3,7 @@ cask :v1 => 'adapter' do
   sha256 '215987042772e1a55fb3a4592895286644344dddef478b3cccfd64235b20f6b7'
 
   url "http://downloads.macroplant.com/Adapter-#{version}.dmg"
+  name 'Adapter'
   appcast 'http://www.macroplant.com/adapter/adapterAppcast.xml',
           :sha256 => 'b3c13d29cccda4e5c30660f2eba270259c4481d39422106b01c669a58d655802'
   homepage 'http://www.macroplant.com/adapter/'
