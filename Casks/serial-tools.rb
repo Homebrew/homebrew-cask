0d9ed3c92cd0006f7e4a2fc17d30a4ba650f51eb
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:58:33 +0000

    serial-tools.rb: added name

--- Casks/serial-tools.rb
+++ Casks/serial-tools.rb
@@ -3,6 +3,7 @@ cask :v1 => 'serial-tools' do
   sha256 :no_check
 
   url 'http://www.w7ay.net/site/Downloads/Serial%20Tools/Serial%20Tools%20app.dmg'
+  name 'Serial Tools'
   homepage 'http://www.w7ay.net/site/Applications/Serial%20Tools/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
