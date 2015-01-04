42678830157bb3b570c7129cb0dcde2d85394f3b
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 16:59:24 +0000

    keepassx.rb: added name

--- Casks/keepassx.rb
+++ Casks/keepassx.rb
@@ -3,6 +3,7 @@ cask :v1 => 'keepassx' do
   sha256 '55aeaba8257d728b62ba173ba56df27897552737a556dc1e4e4ed6dcd3d6dd8a'
 
   url "https://www.keepassx.org/dev/attachments/download/72/KeePassX-#{version}.dmg"
+  name 'KeePassX'
   homepage 'http://www.keepassx.org'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
