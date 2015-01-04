75708a4b02680c7ea4f8e36d7d0cdba7706edb90
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 15:57:46 +0000

    onionshare.rb: added name

--- Casks/onionshare.rb
+++ Casks/onionshare.rb
@@ -3,6 +3,7 @@ cask :v1 => 'onionshare' do
   sha256 '80c583f1dc280f08c11dc60b308e2f6d8fe31c554d59f795ccf0f3252733a6ce'
 
   url "https://onionshare.org/files/#{version}/OnionShare-#{version}.dmg"
+  name 'OnionShare'
   gpg "#{url}.sig",
       :key_url => 'https://onionshare.org/signing-key.asc'
   homepage 'https://onionshare.org/'
