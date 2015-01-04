8bcb9c37c2922c8043adcb5112f85cfdceec66c4
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 16:33:31 +0000

    password-practice.rb: added name

--- Casks/password-practice.rb
+++ Casks/password-practice.rb
@@ -3,6 +3,7 @@ cask :v1 => 'password-practice' do
   sha256 :no_check
 
   url 'https://mrgeckosmedia.com/applications/download/PasswordPractice'
+  name 'Password Practice'
   homepage 'https://mrgeckosmedia.com/applications/info/PasswordPractice'
   license :public_domain
 
