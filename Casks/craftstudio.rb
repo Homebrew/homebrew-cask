df7bb9527d948a84c9f2f476c62dd50bddf1bbab
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 21:36:34 +0000

    craftstudio.rb: add name

--- Casks/craftstudio.rb
+++ Casks/craftstudio.rb
@@ -3,6 +3,7 @@ cask :v1 => 'craftstudio' do
   sha256 :no_check
 
   url 'http://craftstud.io/files/OSX/CraftStudio.pkg'
+  name 'CraftStudio'
   homepage 'http://craftstud.io'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
