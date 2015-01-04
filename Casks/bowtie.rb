fc8774f6f37826eb2e4a78d79160bac65abee3bd
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 16:00:44 +0000

    bowtie.rb: added name

--- Casks/bowtie.rb
+++ Casks/bowtie.rb
@@ -3,6 +3,7 @@ cask :v1 => 'bowtie' do
   sha256 'd8406b066851c0730ca052036bedd5ded82019403de1fd58b579da34cfa4a948'
 
   url "http://bowtieapp.com/bowtie-#{version}.zip"
+  name 'Bowtie'
   appcast 'http://updates.13bold.com/appcasts/bowtie',
           :sha256 => '006ab04ddddeb487b3b16807d58508549e067baf601656f1f95740791d1cca66'
   homepage 'http://bowtieapp.com/'
