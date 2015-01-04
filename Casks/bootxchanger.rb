41947e5bd1f54f4365fe05d6ac629ec6ad25dd70
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 21:44:16 +0000

    corrected name position in bootxchanger.rb

--- Casks/bootxchanger.rb
+++ Casks/bootxchanger.rb
@@ -3,9 +3,9 @@ cask :v1 => 'bootxchanger' do
   sha256 'cfb05680ab6a7d0b1793a33135dd15562a7b5fd59bb1ebf3ad6067c2c9fad6c1'
 
   url "http://namedfork.net/_media/bootxchanger_#{version}.dmg"
-  name 'BootXChanger'
   appcast 'http://swupdate.namedfork.net/bootxchanger.xml',
           :sha256 => 'afbae3b2a378ebb18f1668c062ba48436e956cad840fcc86cba610d351931187'
+  name 'BootXChanger'
   homepage 'http://namedfork.net/bootxchanger'
   license :gpl
 
