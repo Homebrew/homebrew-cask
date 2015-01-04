b25b12262e367a09c3782891927b0132a4634496
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:06:35 +0000

    base.rb: added name

--- Casks/base.rb
+++ Casks/base.rb
@@ -3,9 +3,11 @@ cask :v1 => 'base' do
   sha256 :no_check
 
   url 'http://menial.co.uk/base/download/'
+  name 'Base'
   appcast 'http://update.menial.co.uk/software/base/'
   homepage 'http://menial.co.uk/base/'
   license :commercial
+  tags :vendor => 'Menial'
 
   app 'Base.app'
 end
