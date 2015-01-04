f46cb5f40e60f94cd64fefa8171e4daada948e2d
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 03:07:31 +0000

    appium.rb: added name

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -3,6 +3,7 @@ cask :v1 => 'appium' do
   sha256 'fb7b96244e6dbcdf4d580eae0262b2dec38c008e39ddee4654bb488037f24971'
 
   url "https://bitbucket.org/appium/appium.app/downloads/appium-#{version}.dmg"
+  name 'Appium'
   homepage 'http://appium.io'
   license :oss
 
