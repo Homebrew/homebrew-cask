cd6d6637a31a74e243cd1527eff820c86a626c6b
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 21:13:46 +0000

    pusher.rb: added name

--- Casks/pusher.rb
+++ Casks/pusher.rb
@@ -3,6 +3,8 @@ cask :v1 => 'pusher' do
   sha256 'f2a2c6a39f105c10d5653fa1afe364aa8e930a9cb411c738197e5dc97f378edb'
 
   url "https://github.com/noodlewerk/NWPusher/releases/download/#{version}/pusher.app.zip"
+  name 'Pusher'
+  name 'NWPusher'
   homepage 'https://github.com/noodlewerk/NWPusher'
   license :oss
 
