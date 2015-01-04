217ca0bdee9bb99d16b7181bcd070fba3abb6da3
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 21:49:06 +0000

    corrected name position in mongohub.rb

--- Casks/mongohub.rb
+++ Casks/mongohub.rb
@@ -3,8 +3,8 @@ cask :v1 => 'mongohub' do
   sha256 :no_check
 
   url 'https://mongohub.s3.amazonaws.com/MongoHub.zip'
-  name 'MongoHub'
   appcast 'https://mongohub.s3.amazonaws.com/mongohub_su_feed.xml'
+  name 'MongoHub'
   homepage 'https://github.com/fotonauts/MongoHub-Mac'
   license :oss
 
