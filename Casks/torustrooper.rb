af80af9c6e86eed71f6661bfe731cba5f47161a8
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 19:53:33 +0000

    torustrooper.rb: added name

--- Casks/torustrooper.rb
+++ Casks/torustrooper.rb
@@ -3,6 +3,7 @@ cask :v1 => 'torustrooper' do
   sha256 :no_check
 
   url 'https://workram.com/downloads.php?f=TorusTrooper', :referer => 'https://workram.com/games'
+  name 'Torus Trooper'
   homepage 'http://workram.com/games/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
