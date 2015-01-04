d70edc44093c4f61eae57bde608d706ec9e45b1f
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 21:25:48 +0000

    tftpserver.rb: added name

--- Casks/tftpserver.rb
+++ Casks/tftpserver.rb
@@ -3,6 +3,7 @@ cask :v1 => 'tftpserver' do
   sha256 :no_check
 
   url 'http://ww2.unime.it/flr/tftpserver/TftpServer.dmg'
+  name 'TFTP Server'
   homepage 'http://ww2.unime.it/flr/tftpserver/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
