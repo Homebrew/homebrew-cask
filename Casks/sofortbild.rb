c2380e52e610da2bfb9326762010e10f30c81dfd
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:34:46 +0000

    sofortbild.rb: added name

--- Casks/sofortbild.rb
+++ Casks/sofortbild.rb
@@ -3,6 +3,7 @@ cask :v1 => 'sofortbild' do
   sha256 :no_check
 
   url 'http://www.sofortbildapp.com/download/Sofortbild.zip'
+  name 'Sofortbild'
   homepage 'http://www.sofortbildapp.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
