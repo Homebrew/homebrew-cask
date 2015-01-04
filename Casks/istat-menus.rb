5a19742b3d8bbf0e0d85295a7a2c056d5e954508
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:48:57 +0000

    istat-menus.rb: added name

--- Casks/istat-menus.rb
+++ Casks/istat-menus.rb
@@ -3,6 +3,7 @@ cask :v1 => 'istat-menus' do
   sha256 :no_check
 
   url 'http://download.bjango.com/istatmenus/'
+  name 'iStats Menus'
   homepage 'http://bjango.com/mac/istatmenus/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
