1ced95365c5c3d0f0053302cfb56c0eb447c2d6a
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 16:56:51 +0000

    kamakiri.rb: added name

--- Casks/kamakiri.rb
+++ Casks/kamakiri.rb
@@ -3,7 +3,8 @@ cask :v1 => 'kamakiri' do
   sha256 :no_check
 
   url 'https://mochidev.com/appresources/downloads/Kamakiri%20Latest.zip'
-  homepage 'https://mochidev.com/apps/kamakiri'
+  name 'Kamakiri'
+  homepage 'https://mochidev.com/apps/kamakiri/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Kamakiri.app'
