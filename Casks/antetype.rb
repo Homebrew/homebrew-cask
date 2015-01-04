3ef5dc3dc869097af28c496474f09853b7023129
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 03:03:53 +0000

    antetype.rb: added name

--- Casks/antetype.rb
+++ Casks/antetype.rb
@@ -3,6 +3,7 @@ cask :v1 => 'antetype' do
   sha256 :no_check
 
   url 'https://s3.amazonaws.com/AntetypeDownload/Antetype.zip'
+  name 'Antetype'
   homepage 'http://antetype.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
