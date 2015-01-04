25e0aee11173cdcdc71bfaa636adcac5ca469dae
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 03:30:27 +0000

    awareness.rb: added name

--- Casks/awareness.rb
+++ Casks/awareness.rb
@@ -3,6 +3,7 @@ cask :v1 => 'awareness' do
   sha256 'a5ce31fef63aeabde3c8cabac367fb44868de318800b4fcd9032f05b17f74cdf'
 
   url "http://iamfutureproof.com/downloads/Awareness-#{version}.dmg"
+  name 'Awareness'
   homepage 'http://iamfutureproof.com/tools/awareness/'
   license :gratis
 
