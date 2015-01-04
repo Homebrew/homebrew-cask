f6f57e85b69714de546bc992b52066d452c17936
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 02:31:07 +0000

    adobe-digital-editions.rb: added name

--- Casks/adobe-digital-editions.rb
+++ Casks/adobe-digital-editions.rb
@@ -3,6 +3,7 @@ cask :v1 => 'adobe-digital-editions' do
   sha256 '00b846565da5962993bf3d9f4e629764d439da8269e4c84da49b441fa48d0e86'
 
   url "http://download.adobe.com/pub/adobe/digitaleditions/ADE_#{version}_Installer.dmg"
+  name 'Adobe Digital Editions'
   homepage 'http://www.adobe.com/pt/products/digital-editions.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
