5851c38a86ae8a306868e97b289bb758326a0e88
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 01:47:09 +0000

    010-editor: added name

--- Casks/010-editor.rb
+++ Casks/010-editor.rb
@@ -3,6 +3,7 @@ cask :v1 => '010-editor' do
   sha256 :no_check
 
   url 'http://www.sweetscape.com/download/010EditorMacInstaller.dmg'
+  name '010 Editor'
   homepage 'http://www.sweetscape.com/'
   license :commercial
 
