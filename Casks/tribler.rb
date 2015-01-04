0ad87ed148388be8eb6525adc872817f2b8b65c0
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 15:49:55 +0000

    tribler.rb: added name

--- Casks/tribler.rb
+++ Casks/tribler.rb
@@ -3,6 +3,7 @@ cask :v1 => 'tribler' do
   sha256 'aef90693c230f94ceeb5a4edf172344bd41d5d2cc918df0c2c1a2d2444495073'
 
   url "https://github.com/Tribler/tribler/releases/download/v#{version}/Tribler-#{version}.dmg"
+  name 'Tribler'
   homepage 'http://www.tribler.org'
   license :oss
 
