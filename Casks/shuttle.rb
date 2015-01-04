3c305f77b7d4a97f7b073efd6a98b8bc04537096
Dieter Blomme <dieterblomme@gmail.com>
2015-01-04 22:50:32 +0100

    Update shuttle to version 1.2.2
    Use the zip file now

--- Casks/shuttle.rb
+++ Casks/shuttle.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'shuttle' do
-  version '1.2.0'
+  version '1.2.2'
   sha256 'a6a20b461556e54c99f14eaaaa4ee8e41cb0250561525be87340f0fda635d2b3'
 
-  url "https://github.com/fitztrev/shuttle/releases/download/v#{version}/Shuttle.dmg"
+  url "https://github.com/fitztrev/shuttle/releases/download/#{version}/Shuttle.zip"
   homepage 'http://fitztrev.github.io/shuttle/'
   license :mit
 
