2317a1567459257b17761cb3f15cf123398101f8
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 21:42:52 +0000

    corrected name position in inboard.rb

--- Casks/inboard.rb
+++ Casks/inboard.rb
@@ -3,9 +3,9 @@ cask :v1 => 'inboard' do
   sha256 '3bb602cde63f0b3401fc681cd50a3545c5a47c2326ea81cf66844039fb100522'
 
   url "http://dl.devmate.com/com.ideabits.Inboard/#{version}/1390822305/Inboard-#{version}.dmg"
-  name 'Inboard'
   appcast 'http://updates.devmate.com/com.ideabits.Inboard.xml',
           :sha256 => '3bde9bcd42058928757ce5b3edc9de1ef4f488b190b8efec40e6c7e4fd69a020'
+  name 'Inboard'
   homepage 'http://inboardapp.com/beta'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
