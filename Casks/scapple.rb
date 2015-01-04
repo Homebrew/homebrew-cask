5a4d9189df91ed9658fca208ff719046cfb97dce
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 15:50:33 +0000

    scapple.rb: added name

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -3,6 +3,7 @@ cask :v1 => 'scapple' do
   sha256 :no_check
 
   url 'https://scrivener.s3.amazonaws.com/Scapple.dmg'
+  name 'Scapple'
   appcast 'http://www.literatureandlatte.com/downloads/scapple/scapple.xml'
   homepage 'https://www.literatureandlatte.com/scapple.php'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
