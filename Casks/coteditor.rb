16d146fb91cdbc91d4c75aa39cff63184a31e38b
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 16:59:32 +0000

    coteditor.rb: added name

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -3,6 +3,7 @@ cask :v1 => 'coteditor' do
   sha256 'b656aa0b6526c89d7d52cf12b715cf529f8e5048c7e13720673c3e32318a26d8'
 
   url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg"
+  name 'CotEditor'
   appcast 'http://coteditor.com/appcast.xml',
           :sha256 => '86c24c497701e51df3e0b35e72be5f1cc1d2e3b307a8deb0a188c3443ccd553f'
   homepage 'http://coteditor.com/'
