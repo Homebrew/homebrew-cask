15323d6ec1d6f72288acddcb514cd05f6c5c1b6b
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 20:08:25 +0000

    contexts.rb: added name

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -3,6 +3,7 @@ cask :v1 => 'contexts' do
   sha256 :no_check
 
   url 'http://contextsformac.com/releases/Contexts.zip'
+  name 'Contexts'
   appcast 'http://www.contextsformac.com/releases/appcast.xml'
   homepage 'http://contextsformac.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
