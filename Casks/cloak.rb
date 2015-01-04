8097963b3080e3d37d5bcd19746aee435df6dee2
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 19:58:06 +0000

    cloak.rb: added name

--- Casks/cloak.rb
+++ Casks/cloak.rb
@@ -3,6 +3,7 @@ cask :v1 => 'cloak' do
   sha256 '18ebfe76aeee4da03546ed4969355202b7aec846b82bbea187dbc5891de35bb0'
 
   url "https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-#{version}.dmg"
+  name 'Cloak'
   homepage 'https://www.getcloak.com'
   license :gratis
 
