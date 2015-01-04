197860084df8e8f04800b100606b8d11fb2b86b8
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 21:44:28 +0000

    corrected name position in ccmenu.rb

--- Casks/ccmenu.rb
+++ Casks/ccmenu.rb
@@ -3,9 +3,9 @@ cask :v1 => 'ccmenu' do
   sha256 'e2d36083388b4c68c1dff396393f4d3b2df614ff09deca1fdd9bbba286f7faa5'
 
   url "http://downloads.sourceforge.net/project/ccmenu/CCMenu/#{version}/ccmenu-#{version}-b.dmg"
-  name 'CCMenu'
   appcast 'http://ccmenu.sourceforge.net/update-stable.xml',
           :sha256 => 'a72951f416906d309cb7ec8d233bf5546540641efa99332e0f1bdde119b51cac'
+  name 'CCMenu'
   homepage 'http://ccmenu.sourceforge.net/'
   license :oss
 
