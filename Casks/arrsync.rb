d90c68032b332043f2760bba240eec7fa46dccaf
Vítor Galvão <info@vitorgalvao.com>
2015-01-04 03:16:42 +0000

    arrsync.rb: added name

--- Casks/arrsync.rb
+++ Casks/arrsync.rb
@@ -3,6 +3,7 @@ cask :v1 => 'arrsync' do
   sha256 '8a254771065f90a9f9f882589eccaf7bc51ad5d0ac86faf15847381bcdbe64f0'
 
   url "http://downloads.sourceforge.net/sourceforge/arrsync/arrsync-#{version}.dmg"
+  name 'arRsync'
   homepage 'http://arrsync.sourceforge.net'
   license :oss
 
