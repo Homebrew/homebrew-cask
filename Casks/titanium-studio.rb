b10a04a1157aaeb9c0959e4caaf49c2fb90d9596
Vítor Galvão <info@vitorgalvao.com>
2015-01-05 16:26:00 +0000

    titanium-studio.rb: added cask vendor comment

--- Casks/titanium-studio.rb
+++ Casks/titanium-studio.rb
@@ -2,6 +2,7 @@ cask :v1 => 'titanium-studio' do
   version :latest
   sha256 :no_check
 
+  # amazonaws.com is the official download host per the vendor homepage
   url 'https://titanium-studio.s3.amazonaws.com/latest/Titanium_Studio.dmg'
   homepage 'https://my.appcelerator.com/resources'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
