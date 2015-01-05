ead14065d487d90bd6ad3a968d58c02c81d9df76
Vítor Galvão <info@vitorgalvao.com>
2015-01-05 16:25:16 +0000

    eve.rb: added cask vendor comment

--- Casks/eve.rb
+++ Casks/eve.rb
@@ -2,6 +2,7 @@ cask :v1 => 'eve' do
   version :latest
   sha256 :no_check
 
+  # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/hotkeyeve/Downloads/EVE-latest.zip'
   appcast 'https://s3.amazonaws.com/hotkeyeve/hotkeyEVEappcast.xml'
   homepage 'http://www.hotkey-eve.com/'
