da43f6b08e2a8e4dd283ef5855a62893b1a5d17a
Vítor Galvão <info@vitorgalvao.com>
2015-01-05 16:25:23 +0000

    kindle-previewer.rb: added cask vendor comment

--- Casks/kindle-previewer.rb
+++ Casks/kindle-previewer.rb
@@ -2,6 +2,7 @@ cask :v1 => 'kindle-previewer' do
   version :latest
   sha256 :no_check
 
+  # amazonaws.com is the official download host per the vendor homepage
   url 'https://kindlepreviewer.s3.amazonaws.com/KindlePreviewer.zip'
   homepage 'http://www.amazon.com/gp/feature.html/?docId=1000765261'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
