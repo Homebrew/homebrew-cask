5261cd0a741ca635a16dbe574dc90bf9c7ff4394
Vítor Galvão <info@vitorgalvao.com>
2015-01-05 16:25:49 +0000

    scrivener.rb: added cask vendor comment

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -2,6 +2,7 @@ cask :v1 => 'scrivener' do
   version :latest
   sha256 :no_check
 
+  # amazonaws.com is the official download host per the vendor homepage
   url 'https://scrivener.s3.amazonaws.com/Scrivener.dmg'
   appcast 'http://www.literatureandlatte.com/downloads/scrivener-2.xml'
   homepage 'http://literatureandlatte.com/scrivener.php'
