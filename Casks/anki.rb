5b6b764998b55804706b4240b0c2c77fa1fd323b
Basil Shubin <basil.shubin@gmail.com>
2015-01-05 09:46:50 +0600

    Update Anki.app 10.6 compatible version

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,6 +1,12 @@
 cask :v1 => 'anki' do
-  version '2.0.31'
-  sha256 '7afccba7d7998c187156702afb943e0be2b9d14729c8147b9a6fd20e40530fe2'
+
+  if MacOS.release <= :snow_leopard
+    version '2.0.31.qt47'
+    sha256 '43f3bd526b45ab2af2b0017dd33ba7725b3116ef4c6b3abc964eb974109d0c02'
+  else
+    version '2.0.31'
+    sha256 '7afccba7d7998c187156702afb943e0be2b9d14729c8147b9a6fd20e40530fe2'
+  end
 
   url "http://ankisrs.net/download/mirror/anki-#{version}.dmg"
   name 'Anki'
