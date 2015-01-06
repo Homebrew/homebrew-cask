6124f5e73f223382c1de879f8de777513eac9063
Vítor Galvão <info@vitorgalvao.com>
2015-01-06 02:13:43 +0000

    webpquicklook.rb: added name

--- Casks/webpquicklook.rb
+++ Casks/webpquicklook.rb
@@ -3,6 +3,7 @@ cask :v1 => 'webpquicklook' do
   sha256 'b7495e90589f2f9ee13c331d840ff638399817bd906da61aa32bb21a9c26f32b'
 
   url "https://github.com/dchest/webp-quicklook/releases/download/v#{version}/WebP-#{version}.qlgenerator.zip"
+  name 'WebP QuickLook Plugin'
   homepage 'https://github.com/dchest/webp-quicklook'
   license :mit
 
