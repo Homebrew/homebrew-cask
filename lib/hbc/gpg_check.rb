9ed79a45760ad1fe98ef8997f51b1ba6c5a0b84a
ndr <ndr@qef.io>
2015-01-06 18:25:38 +0000

    Style nits in Hbc::GpgCheck

--- lib/hbc/gpg_check.rb
+++ lib/hbc/gpg_check.rb
@@ -9,11 +9,10 @@ class Hbc::GpgCheck
   end
 
   def installed?
-    cmd = @command.run('/usr/bin/type',
-                       :args => ['-p', 'gpg'])
+    gpg_bin_path = @command.run('/usr/bin/type',
+                                :args => ['-p', 'gpg'])
 
-    # if `gpg` is found, return its absolute path
-    cmd.success? ? cmd.stdout : false
+    gpg_bin_path.success? ? gpg_bin_path.stdout : false
   end
 
 
@@ -22,7 +21,7 @@ class Hbc::GpgCheck
     cached = @cask.metadata_subdir('gpg') unless unversioned_cask
 
     meta_dir = cached || @cask.metadata_subdir('gpg', :now, true)
-    sig_path = meta_dir.join("signature.asc")
+    sig_path = meta_dir.join('signature.asc')
 
     curl(@cask.gpg.signature, '-o', sig_path.to_s) unless cached || force
 
