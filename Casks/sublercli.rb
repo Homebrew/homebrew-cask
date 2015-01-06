91e9d26474b9af3b7b92644d52963b282bd6fd90
Jason Masker <jason@masker.net>
2015-01-06 18:28:38 -0500

    sublercli latest

--- Casks/sublercli.rb
+++ Casks/sublercli.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'sublercli' do
-  version '0.19'
-  sha256 '968c6c0ff530ee603a5a3deec93911cc1c5c1fd8b100883cb485278fccc79104'
+  version :latest
+  sha256 :no_check
 
-  url "https://subler.googlecode.com/files/SublerCLI-#{version}.zip"
-  homepage 'https://code.google.com/p/subler/'
+  url "https://bitbucket.org/galad87/sublercli/downloads/SublerCLI-Test.zip"
+  homepage 'https://bitbucket.org/galad87/sublercli/'
   license :oss
 
   binary 'SublerCLI'
