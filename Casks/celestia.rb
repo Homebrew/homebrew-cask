e7305a974eff87a3316346fac40ce75b032f99e4
Vítor Galvão <info@vitorgalvao.com>
2015-01-06 02:38:30 +0000

    celestia.rb: added name

--- Casks/celestia.rb
+++ Casks/celestia.rb
@@ -3,6 +3,7 @@ cask :v1 => 'celestia' do
   sha256 'db09842a10b377038bedef87fda8d159549234b2e072fa22e096ade84aa3f52a'
 
   url "http://downloads.sourceforge.net/sourceforge/celestia/celestia-osx-#{version}.dmg"
+  name 'Celestia'
   homepage 'http://sourceforge.net/projects/celestia'
   license :oss
 
