e7807fa1bcc978e54b4b8fd00ea75be5484baddb
Vítor Galvão <info@vitorgalvao.com>
2015-01-06 03:11:17 +0000

    mozart.rb: added name

--- Casks/mozart.rb
+++ Casks/mozart.rb
@@ -3,6 +3,7 @@ cask :v1 => 'mozart' do
   sha256 'df03e42ca0da5700f49d3ae036758bac762cbc3a95d4e69c4bc4aabb54e6f92b'
 
   url "http://downloads.sourceforge.net/sourceforge/mozart-oz/Mozart-#{version}.zip"
+  name 'Mozart'
   homepage 'http://www.mozart-oz.org'
   license :oss
 
