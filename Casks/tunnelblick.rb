42ae9f5d779786a99dc3a8c91e36df7e0cbf93b1
Vítor Galvão <info@vitorgalvao.com>
2015-01-07 21:00:39 +0000

    tunnelblick.rb: use #{token} in caveat

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -16,7 +16,7 @@ cask :v1 => 'tunnelblick' do
   depends_on :macos => '>= :tiger'
 
   caveats <<-EOS.undent
-    For security reasons, Tunnelblick must be installed to /Applications,
+    For security reasons, #{token} must be installed to /Applications,
     and will request to be moved at launch.
   EOS
 end
