55db02bec810c26fa2b6ed4a1b7e2a5cc90816c7
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-01-07 01:59:13 +0000

    Add apache-couchdb v1.6.1

---
+++ Casks/apache-couchdb.rb
@@ -0,0 +1,19 @@
+cask :v1 => 'apache-couchdb' do
+  version '1.6.1'
+  sha256 'c94dcc4e2ff163dfd3df52a3170f5f18be05beda3bf64c3e12a78dfe622dbf8f'
+
+  url "http://www.mirrorservice.org/sites/ftp.apache.org/couchdb/binary/mac/#{version}/Apache-CouchDB-#{version}.zip"
+  name 'Apache CouchDB'
+  homepage 'http://couchdb.apache.org/'
+  license :apache
+
+  app 'Apache CouchDB.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/CouchDB',
+                  '~/Library/Caches/org.apache.couchdb',
+                  '~/Library/Logs/couchdb.log',
+                  '~/Library/Logs/couchdb.log.old',
+                  '~/Library/Preferences/org.apache.couchdb.plist'
+                 ]
+end
