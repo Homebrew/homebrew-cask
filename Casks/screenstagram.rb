f51728ebbc44d39730335028172c57d06f04ceee
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 04:34:18 +0000

    screenstagram.rb: added comment regarding vendor url

--- Casks/screenstagram.rb
+++ Casks/screenstagram.rb
@@ -2,6 +2,7 @@ cask :v1 => 'screenstagram' do
   version '2.01'
   sha256 '5cb9f7be247fdec897248f5091818bf7b25153fa99782e96f9d9b2c2431cdbb1'
 
+  # amazonaws.com is the official download host per the vendor homepage
   url "https://screenstagram.s3.amazonaws.com/screenstagram_#{version}.dmg"
   homepage 'http://screenstagram.s3.amazonaws.com/download.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
