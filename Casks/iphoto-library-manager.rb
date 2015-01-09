8a852952f23fd4c9bdb3000560f17931217b6943
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 21:00:47 +0000

    iphoto-library-manager.rb: added name

--- Casks/iphoto-library-manager.rb
+++ Casks/iphoto-library-manager.rb
@@ -3,6 +3,7 @@ cask :v1 => 'iphoto-library-manager' do
   sha256 '2ab9ec177b2c6ee94847ea6c2164a51f86e2c9e288433f9f7a4f20ab175be519'
 
   url "https://s3.amazonaws.com/fatcatsoftware/iplm/iPhotoLibraryManager_#{version.gsub('.', '')}.zip"
+  name 'iPhoto Library Manager'
   homepage 'http://www.fatcatsoftware.com/iplm/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
