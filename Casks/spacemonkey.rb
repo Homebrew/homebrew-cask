b6b03095b6df454bfad2ff351aee38daf54edf28
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 21:38:24 +0000

    spacemonkey.rb: added name

--- Casks/spacemonkey.rb
+++ Casks/spacemonkey.rb
@@ -4,6 +4,7 @@ cask :v1 => 'spacemonkey' do
 
   url 'http://downloads.spacemonkey.com/client/mac/latest'
   appcast 'https://rink.hockeyapp.net/api/2/apps/aa33b6780fdfc71247b2995fa47b5d7c'
+  name 'Space Monkey'
   homepage 'https://www.spacemonkey.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
