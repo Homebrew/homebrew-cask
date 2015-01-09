b4d4ea791a1f15ec7040ec975fd828d9aa405300
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 21:09:57 +0000

    xaos.rb: added name

--- Casks/xaos.rb
+++ Casks/xaos.rb
@@ -3,6 +3,8 @@ cask :v1 => 'xaos' do
   sha256 '1a3864f354759c03c13150ef541b48d06cf74351360a85a8b46e73a45edc7054'
 
   url "http://downloads.sourceforge.net/xaos/xaos-#{version}-macosx.dmg"
+  name 'XaoS'
+  name 'GNU XaoS'
   homepage 'http://matek.hu/xaos'
   license :oss
 
