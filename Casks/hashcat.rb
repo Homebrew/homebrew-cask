7ac754ed9ea4c6ad557b6b3ca5904c437dd8fb93
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 21:17:08 +0000

    hashcat.rb: added name

--- Casks/hashcat.rb
+++ Casks/hashcat.rb
@@ -3,6 +3,7 @@ cask :v1 => 'hashcat' do
   sha256 '39fee1757b33b3e7232ce3797f0be024fa784dbbd0939b475b5b1ea7462cec5e'
 
   url "https://hashcat.net/files/hashcat-#{version}.7z"
+  name 'hashcat'
   homepage 'https://hashcat.net/hashcat/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
