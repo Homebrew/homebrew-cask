146cccd758f8ca0161dec5581a2839a267dc1885
Lincoln Lee <git@lincoln.hk>
2015-01-09 18:44:43 +0800

    Add GPL license for licecap

--- Casks/licecap.rb
+++ Casks/licecap.rb
@@ -4,7 +4,7 @@ cask :v1 => 'licecap' do
 
   url "http://www.cockos.com/licecap/licecap#{version.gsub('.','')}.dmg"
   homepage 'http://www.cockos.com/licecap/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gpl
 
   app 'LICEcap.app'
 end
