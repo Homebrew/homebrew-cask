71a7e4312668fe3156b9649388020f2f62980a31
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 21:13:12 +0000

    videobox.rb: added name

--- Casks/videobox.rb
+++ Casks/videobox.rb
@@ -3,6 +3,7 @@ cask :v1 => 'videobox' do
   sha256 'bd9e6ae942b729b02c40ad076814be7c5e2f966325c9c5e982070d03443408c5'
 
   url "http://download.tastyapps.com/videobox_#{version}.dmg"
+  name 'Videobox'
   homepage 'http://www.tastyapps.com/videobox/'
   license :closed
 
