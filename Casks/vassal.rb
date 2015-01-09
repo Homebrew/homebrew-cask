093ac5e02741a07fa5a9246370b4b8e5bfd716fa
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 20:50:56 +0000

    vassal.rb: added name

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -3,6 +3,7 @@ cask :v1 => 'vassal' do
   sha256 'f9840fe23a48611307a1e42919fc4dcd6ff79165eeda2da61b834f81ab7bf7f6'
 
   url "http://downloads.sourceforge.net/project/vassalengine/VASSAL-current/VASSAL-#{version}/VASSAL-#{version}-macosx.dmg"
+  name 'VASSAL'
   homepage 'http://www.vassalengine.org'
   license :oss
 
