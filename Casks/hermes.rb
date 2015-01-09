2a94ee762f7d85e82bde81721511d72c530ca28e
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 04:22:24 +0000

    hermes.rb: added comment regarding vendor url

--- Casks/hermes.rb
+++ Casks/hermes.rb
@@ -2,6 +2,7 @@ cask :v1 => 'hermes' do
   version '1.2.5'
   sha256 '786e20040bdd6e943b646f74d7cc9a460b37444272c4a7af8d1f5ef77a7d2f2f'
 
+  # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/hermesmacapp/Hermes-#{version}.zip"
   appcast 'http://hermesapp.org/versions.xml',
           :sha256 => 'cc89f8c651e0335c91d52bb120f769ca4c418c13144155476c572ca5da21fb0e'
