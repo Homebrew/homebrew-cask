5019179c5e8fef4aafdab6c8731a98c6dbb3707e
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 04:30:23 +0000

    menubar-countdown.rb: added comment regarding vendor url

--- Casks/menubar-countdown.rb
+++ Casks/menubar-countdown.rb
@@ -2,6 +2,7 @@ cask :v1 => 'menubar-countdown' do
   version '1.2'
   sha256 '4ee0a7a87dbd4013c461b59316c749a5f9a92160bdf6d90afb1ff029f9381c01'
 
+  # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/capablehands/downloads/MenubarCountdown-#{version}.zip"
   homepage 'http://capablehands.net/menubarcountdown'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
