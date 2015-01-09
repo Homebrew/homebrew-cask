9b71d11586f8d303bf6a6eebd788b7c718430804
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 21:11:53 +0000

    jsonlook.rb: added name

--- Casks/jsonlook.rb
+++ Casks/jsonlook.rb
@@ -4,6 +4,7 @@ cask :v1 => 'jsonlook' do
 
   # dropbox.com is the official download host per the vendor homepage
   url 'https://dl.dropbox.com/u/3878216/github/jsonlook.qlgenerator.zip'
+  name 'jsonlook'
   homepage 'https://github.com/rjregenold/jsonlook'
   license :oss
 
