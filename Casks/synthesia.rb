7380bb3dbcbbeeca763e1addf4bff0fd3b54abd8
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 04:35:50 +0000

    synthesia.rb: added comment regarding vendor url

--- Casks/synthesia.rb
+++ Casks/synthesia.rb
@@ -2,6 +2,7 @@ cask :v1 => 'synthesia' do
   version '10.1'
   sha256 '394418e9a32cd2dc53858f8721c81832be3ca460c0cf210bd8e179f1d6b2ba1d'
 
+  # amazonaws.com is the official download host per the vendor homepage
   url "https://synthesia.s3.amazonaws.com/files/Synthesia-#{version}.dmg"
   homepage 'http://www.synthesiagame.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
