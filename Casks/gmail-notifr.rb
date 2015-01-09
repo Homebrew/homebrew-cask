f9ff630036d8fb9eec51a135869495b3245ac981
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 04:20:38 +0000

    gmail-notifr.rb: added comment regarding vendor url

--- Casks/gmail-notifr.rb
+++ Casks/gmail-notifr.rb
@@ -2,6 +2,7 @@ cask :v1 => 'gmail-notifr' do
   version '0.9.0'
   sha256 '47ecf1edf08678e6a1c53cb5a3e39d5c6c5f63a9145fc7cbc7df2ebada42b662'
 
+  # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/assets.ashchan.com/gmailnotifr/GmailNotifr-#{version}.zip"
   appcast 'https://s3.amazonaws.com/assets.ashchan.com/gmailnotifr/update.xml',
           :sha256 => '20876c0cef8d54463a5189948259e7ad268a28424b96e7fa2fbad860b0ec9554'
