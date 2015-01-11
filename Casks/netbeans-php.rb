7e0883da8aee921bfac612b5c33ce53f0ee52b10
Martijn Gastkemper <martijngastkemper@eresults.nl>
2015-01-11 23:03:53 +0100

    Change double quotes to single quotes

--- Casks/netbeans-php.rb
+++ Casks/netbeans-php.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'netbeans-php' do
-  version "8.0.2"
-  sha256 "8087d4327506e22f8e371551e28411e91a3b96f407ad381f349dbcbc2ecc291f"
+  version '8.0.2'
+  sha256 '8087d4327506e22f8e371551e28411e91a3b96f407ad381f349dbcbc2ecc291f'
 
   url "http://download.netbeans.org/netbeans/#{version}/final/bundles/netbeans-#{version}-php-macosx.dmg"
   homepage 'https://netbeans.org/'
