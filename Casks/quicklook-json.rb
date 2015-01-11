ecd87e484a2b2b41c1b43035b8eac8bf4a2b8371
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 18:00:01 +0000

    quicklook-json.rb: added name

--- Casks/quicklook-json.rb
+++ Casks/quicklook-json.rb
@@ -3,6 +3,7 @@ cask :v1 => 'quicklook-json' do
   sha256 :no_check
 
   url 'http://www.sagtau.com/media/QuickLookJSON.qlgenerator.zip'
+  name 'quick look JSON'
   homepage 'http://www.sagtau.com/quicklookjson.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
