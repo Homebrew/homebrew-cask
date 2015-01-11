c55acc42f539ce96241b640a8a421e670276014c
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 19:06:33 +0000

    preferencecleaner.rb: added name

--- Casks/preferencecleaner.rb
+++ Casks/preferencecleaner.rb
@@ -3,6 +3,7 @@ cask :v1 => 'preferencecleaner' do
   sha256 :no_check
 
   url 'http://www.echomist.co.uk/software/downloads/PreferenceCleaner.dmg'
+  name 'PreferenceCleaner'
   homepage 'http://www.echomist.co.uk/software/PreferenceCleaner.shtml'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
