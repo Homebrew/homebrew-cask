72350c608f8c349fc1a0294f793e188951e4d861
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 18:57:34 +0000

    mcedit.rb: added name

--- Casks/mcedit.rb
+++ Casks/mcedit.rb
@@ -3,6 +3,7 @@ cask :v1 => 'mcedit' do
   sha256 '9ab4e66a3d4d898921cd38725da36567931fe4b8d0285c3a73dba6bef35fbea4'
 
   url "https://bitbucket.org/codewarrior0/mcedit/downloads/MCEdit-#{version}.macosx-10_6-x86_64.zip"
+  name 'MCEdit'
   homepage 'http://www.mcedit.net'
   license :oss
 
