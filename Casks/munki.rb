775d91b83fd49d46dafeba6d72246881592cfbda
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 18:12:47 +0000

    munki.rb: added name

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -3,6 +3,7 @@ cask :v1 => 'munki' do
   sha256 'f4b586c254fcd47f54d4b58ea7c43308ee1472b41f03e7b59ceb9fd6605a54b8'
 
   url "https://github.com/munki/munki/releases/download/v#{version}/munkitools-#{version}.2253.pkg"
+  name 'Munki'
   homepage 'http://munki.github.io/munki/'
   license :oss
 
