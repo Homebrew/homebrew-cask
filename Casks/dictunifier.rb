ca4bfa1bd3d3a437434517d5ef59379a3aa0e4ab
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 18:22:34 +0000

    dictunifier.rb: added name

--- Casks/dictunifier.rb
+++ Casks/dictunifier.rb
@@ -3,6 +3,7 @@ cask :v1 => 'dictunifier' do
   sha256 'ff80b354ebcbe7ddad0e01d64c667e6a026d92f6bac01d380ec009205679f14c'
 
   url "https://mac-dictionary-kit.googlecode.com/files/DictUnifier-#{version}.zip"
+  name 'DictUnifier'
   homepage 'http://code.google.com/p/mac-dictionary-kit/'
   license :oss
 
