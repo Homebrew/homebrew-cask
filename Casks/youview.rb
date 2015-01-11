7576d3622a85974173e5a81f8d2e1b4b39f54dc5
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 18:56:38 +0000

    youview.rb: added name

--- Casks/youview.rb
+++ Casks/youview.rb
@@ -4,6 +4,7 @@ cask :v1 => 'youview' do
 
   url 'http://download.mrgeckosmedia.com/YouView.zip'
   appcast 'http://mrgeckosmedia.com/applications/appcast/YouView'
+  name 'YouView'
   homepage 'https://mrgeckosmedia.com/applications/info/YouView'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
