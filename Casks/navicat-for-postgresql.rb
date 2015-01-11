dbebf3b457a36055ea040f5faee8179ce14c8a76
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 19:17:05 +0000

    navicat-for-postgresql.rb: added name

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -3,6 +3,7 @@ cask :v1 => 'navicat-for-postgresql' do
   sha256 '9149775caec5c381f6e28ac4cdf7492d2d42d181e503a25295a8c57e8ebae892'
 
   url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_pgsql_en.dmg"
+  name 'Navicat for PostgreSQL'
   homepage 'http://www.navicat.com/products/navicat-for-postgresql'
   license :commercial
 
