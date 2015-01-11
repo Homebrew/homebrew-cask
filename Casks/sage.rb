3599874afddc4a631c0caa92ae2e3075ae9139d0
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 17:50:20 +0000

    sage.rb: added name

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -3,6 +3,7 @@ cask :v1 => 'sage' do
   sha256 '3ae99dbddd5609271aa87ef38db58dd53fe2add3a58abed3b80750884100391e'
 
   url "http://boxen.math.washington.edu/home/sagemath/sage-mirror/osx/intel/sage-#{version}-x86_64-Darwin-OSX_10.10_x86_64-app.dmg"
+  name 'Sage'
   homepage 'http://www.sagemath.org/'
   license :gpl
 
