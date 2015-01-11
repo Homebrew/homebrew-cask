ec24d2b87f35c10a425480b8fd2767915af50344
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 18:59:39 +0000

    free-ruler.rb: added name

--- Casks/free-ruler.rb
+++ Casks/free-ruler.rb
@@ -3,6 +3,7 @@ cask :v1 => 'free-ruler' do
   sha256 'f746e49ae012bee5ccef9871592276de47d3b6affbe92ab3f45f2591cb05b915'
 
   url "http://www.pascal.com/software/freeruler/FreeRuler#{version}.zip"
+  name 'Free Ruler'
   homepage 'http://www.pascal.com/software/freeruler/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
