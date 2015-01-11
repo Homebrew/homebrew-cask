c10a6642bad4a9aba26d55050e634de1e3c15c36
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 18:07:29 +0000

    flash-player.rb: added name

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -3,8 +3,10 @@ cask :v1 => 'flash-player' do
   sha256 'f7bd90f9ee7520195cb02c0bf7803b4b30da48e64da040cd7d84d638aa000102'
 
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.to_i}/flashplayer_#{version.to_i}_sa.dmg"
+  name 'Adobe Flash Player'
   homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  tags :vendor => 'Adobe'
 
   app 'Flash Player.app'
 
