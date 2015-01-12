5dd543529470c29224163ceb7e489f1bdc21a17f
Ganesh S <hackerone@gmail.com>
2015-01-12 10:42:30 +0000

    Update filezilla to 3.10.0

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -4,8 +4,8 @@ cask :v1 => 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.9.0.6'
-    sha256 '9486be1f5dec41df8ef1b60893ca612e6791b8d98609c6b9e42ced977210d15b'
+    version '3.10.0'
+    sha256 '12ff3a2ecea426abce5a3d70718d13670b9042375b24ab2e78fcd582e7684815'
   end
 
   url "http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
