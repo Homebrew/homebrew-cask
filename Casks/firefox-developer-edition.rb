f9ab5c8db5900b3e1209273f309115d090754174
Ingo Richter <ingo.richter@gmail.com>
2015-01-11 23:23:29 -0800

    Fixed typo

--- Casks/firefox-developer-edition.rb
+++ Casks/firefox-developer-edition.rb
@@ -3,7 +3,7 @@ cask :v1 => 'firefox-developer-edition' do
   sha256 :no_check
 
   url 'https://download.mozilla.org/?product=firefox-aurora-latest-ssl&os=osx&lang=en-US'
-  name 'Firefox Developer Editon'
+  name 'Firefox Developer Edition'
   homepage 'https://www.mozilla.org/en-US/firefox/developer'
   license :mpl
   tags :vendor => 'Firefox'
@@ -11,8 +11,8 @@ cask :v1 => 'firefox-developer-edition' do
   app 'FirefoxDeveloperEdition.app'
 
   zap :delete => [
-		  '~/Library/Application Support/Firefox',
-		  '~/Library/Caches/Firefox',
-		  '~/Library/Preferences/org.mozilla.firefoxdeveloperedition.plist'
+		   '~/Library/Application Support/Firefox',
+		   '~/Library/Caches/Firefox',
+		   '~/Library/Preferences/org.mozilla.firefoxdeveloperedition.plist'
          	  ]
 end
