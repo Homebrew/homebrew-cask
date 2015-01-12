84f919aa4f43bd224e23f1b86a64ca9c3b929b8e
Gibson Starkweather <gibson@gibson.fm>
2015-01-12 09:23:05 -0800

    Add Lingon X 2.0, moved Lingon X 1.0 to versions
    Per the discussion on caskroom/homebrew-cask#8844, Lingon X v1.0 will
    be moved to caskroom/homebrew-versions and v2.0 will replace v1.0 here
    
    (+2 squashed commits)
    
    [280f122] Add OS version conditional to Lingon X Cask
    Added a conditional to determine if we should install Lingon 1 or Lingon
    2 depending on the user's version of OS X.
    
    [80d3909] Add Lingon X.app v2.0
    Lingon X version 1.0 already exists as lingon-x but Lingon X 2.0 is an
    entirely new version of Lingon X which only runs on Yosemite 10.10 or
    higher. In my opinion, Lingon X 1.0 is still a valid Cask for this very
    reason, and I think including 2.0 as lingon-x2 is the best way to handle
    this updated version.

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -2,10 +2,12 @@ cask :v1 => 'lingon-x' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.peterborgapps.com/downloads/LingonX.zip'
-  appcast 'http://www.peterborgapps.com/updates/lingonx-appcast.xml'
+  url 'http://www.peterborgapps.com/downloads/LingonX2.zip'
+  appcast 'http://www.peterborgapps.com/updates/lingonx2-appcast.xml'
   homepage 'http://www.peterborgapps.com/lingon/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'Lingon X.app'
+
+  depends_on :macos => '>= :yosemite'
 end
