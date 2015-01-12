c086ae20bb19386dec6ca1fb3f1a4637be9b740e
Ingo Richter <ingo.richter@gmail.com>
2015-01-11 23:17:28 -0800

    Add FirefoxDeveloperEdition Latest
    Initial commit for Firefox Developer Edition

---
+++ Casks/firefox-developer-edition.rb
@@ -0,0 +1,18 @@
+cask :v1 => 'firefox-developer-edition' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://download.mozilla.org/?product=firefox-aurora-latest-ssl&os=osx&lang=en-US'
+  name 'Firefox Developer Editon'
+  homepage 'https://www.mozilla.org/en-US/firefox/developer'
+  license :mpl
+  tags :vendor => 'Firefox'
+
+  app 'FirefoxDeveloperEdition.app'
+
+  zap :delete => [
+		  '~/Library/Application Support/Firefox',
+		  '~/Library/Caches/Firefox',
+		  '~/Library/Preferences/org.mozilla.firefoxdeveloperedition.plist'
+         	  ]
+end
