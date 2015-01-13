9483954805cb6d5a1674afa792b63e02920a3e29
ndr <ndr@qef.io>
2015-01-13 12:08:49 +0000

    netbeans-php: specify license

--- Casks/netbeans-php.rb
+++ Casks/netbeans-php.rb
@@ -4,7 +4,7 @@ cask :v1 => 'netbeans-php' do
 
   url "http://download.netbeans.org/netbeans/#{version}/final/bundles/netbeans-#{version}-php-macosx.dmg"
   homepage 'https://netbeans.org/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :oss
 
   pkg "NetBeans #{version}.pkg"
 
