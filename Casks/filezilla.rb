d86e1b7558e2f42b856d0fa873e85fe8eb0cbf3e
Halil Özgür <halil.ozgur@gmail.com>
2015-01-13 16:42:12 +0200

    Update filezilla to 3.10.0.1
    https://filezilla-project.org/versions.php#3.10.0.1

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -4,8 +4,8 @@ cask :v1 => 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.10.0'
-    sha256 '5334f2ed4a5b6b6666a125aaedd0d4f79b4410bfcda90e609f56656d4ffdb7f3'
+    version '3.10.0.1'
+    sha256 'c3317b8052736ce042a5d27279c888e321065b87fe3cbc5c43c3600c3e52f40c'
   end
 
   url "http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
