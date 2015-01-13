252b1f4324596e0b6516ac1c356cd64fd9b6cc35
Caio Costa <bitbucket@bycosta.com>
2015-01-13 00:05:15 -0200

    Upgrade Telegram.app from 0.7.4 to 0.7.6

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'telegram' do
-  version '0.7.4'
-  sha256 'ed3252a04c99594e4177cf5c4d8b14bfc958a6b61e9801f4bd256755a147bc13'
+  version '0.7.6'
+  sha256 '6a3e8bdb90d16b881b565ea1a013a108de63886b9e6003371362c4990e25add3'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
