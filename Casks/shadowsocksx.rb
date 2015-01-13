22a7defd54f2f4cc64242cc32cf7cc0fc6af63a6
Havanna <registerdedicated@yeah.net>
2015-01-13 09:24:37 +0800

    shadowsocksx 2.6.1

--- Casks/shadowsocksx.rb
+++ Casks/shadowsocksx.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'shadowsocksx' do
-  version '2.6'
-  sha256 'd7b152f2eb76caa8af48ae275eb9b5cecc716f14bdd5d93a090d7b1025b3cd26'
+  version '2.6.1'
+  sha256 '79650f9c592b69b0cbe867962f2aeedfcd07623c12db9585b533e7c5786c6d6f'
 
   url "http://downloads.sourceforge.net/project/shadowsocksgui/dist/ShadowsocksX-#{version}.dmg"
   homepage 'https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help'
