77a1e04c9496907be5385aacbc8811d982e25f01
Roger W Thiede <RogerThiede@gmail.com>
2015-01-14 00:05:10 -0800

    Clarifying araxis-merge with additional Cask stanzas for installation and uninstallation

--- Casks/araxis-merge.rb
+++ Casks/araxis-merge.rb
@@ -10,8 +10,22 @@ cask :v1 => 'araxis-merge' do
   end
 
   name 'Araxis Merge'
+  tags :vendor => 'Araxis'
   homepage 'http://www.araxis.com/merge'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
-
+  license :commercial
+  zap :delete => [
+                 '~/Library/Preferences/com.araxis.merge.LSSharedFileList.plist',
+                 '~/Library/Preferences/com.araxis.merge.plist',
+                 ]
   app 'Araxis Merge.app'
+  caveats <<-EOS.undent
+    Registration may still be required for proper operation of Araxis Merge.
+    Araxis offers no-cost licences of its products to bona fide contributors to Open Source projects.
+  
+    Additional steps are required to integrate Araxis Merge with Finder,
+    installing the Araxis Merge command-line utilities, or integrating
+    Araxis Merge with other applications.
+  
+    See http://www.araxis.com/merge/documentation-os-x/installing.en
+  EOS
 end
