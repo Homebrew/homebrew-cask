065a5ecf3398d096ed6d7942669000a774abbf53
Roger W Thiede <RogerThiede@gmail.com>
2015-01-14 00:05:10 -0800

    Expand araxis-merge to cover binaries, zap…
    … and additional information: license, vendor, caveats linking to
    integration instructions.

--- Casks/araxis-merge.rb
+++ Casks/araxis-merge.rb
@@ -1,5 +1,6 @@
 cask :v1 => 'araxis-merge' do
   version '2014.4581'
+
   depends_on :macos => '>= :mavericks'
   if MacOS.release <= :mavericks
     sha256 '4029be850bbffd8a46b30b7701434474c33e07e678848ae47e094ddba04668d2'
@@ -11,7 +12,30 @@ cask :v1 => 'araxis-merge' do
 
   name 'Araxis Merge'
   homepage 'http://www.araxis.com/merge'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
+  tags :vendor => 'Araxis'
 
   app 'Araxis Merge.app'
+
+  binary 'Utilities/araxisgitdiff'
+  binary 'Utilities/araxisopendiff'
+  binary 'Utilities/araxissvndiff'
+  binary 'Utilities/compare'
+  binary 'Utilities/araxisgitmerge'
+  binary 'Utilities/araxisp4diff'
+  binary 'Utilities/araxissvndiff3'
+  binary 'Utilities/compare2'
+  binary 'Utilities/araxishgmerge'
+  binary 'Utilities/araxisp4winmrg'
+  binary 'Utilities/araxissvnmerge'
+
+  zap :delete => [
+                 '~/Library/Preferences/com.araxis.merge.LSSharedFileList.plist',
+                 '~/Library/Preferences/com.araxis.merge.plist',
+                 ]
+
+  caveats <<-EOS.undent
+    For instructions to integrate Araxis Merge with Finder or other applications,
+    see http://www.araxis.com/merge/documentation-os-x/installing.en
+  EOS
 end
