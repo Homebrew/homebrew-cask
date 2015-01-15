b3d61c5af03147c72e03893a40ebd75dd35851de
Jon Stuart <jon@zomo.co.uk>
2015-01-15 08:20:27 +0700

    Casks/thunderbird.rb - bump Thunderbird to 31.4.0
    https://www.mozilla.org/en-US/thunderbird/31.4.0/releasenotes/

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird' do
-  version '31.3.0'
-  sha256 'b420b7b04cde0c57645bf2ae0f8b9fc507e854f82690b26551a54c17518885a6'
+  version '31.4.0'
+  sha256 '6de56ad6d215e3505afa376834cf251e1a1d0d4d4d1d570cfb507afefd164490'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   homepage 'http://www.mozilla.org/en-US/thunderbird/'
