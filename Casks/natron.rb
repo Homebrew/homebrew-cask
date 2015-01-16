e5ac3f975091834eefbb33ea47aa8c8eee3fdae7
William Melody <hi@williammelody.com>
2015-01-16 11:12:30 -0800

    Add Natron.app v1.1.0
    Natron is an open-source, crossplatform, nodal compositing software.
    
    Note: the preferred sourceforce URL format didn't return a valid dmg
    after several tries, but the fallback preferred format appears to work
    without any issues.

---
+++ Casks/natron.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'natron' do
+  version '1.1.0'
+  sha256 'e06352b0b11acbf0bbeb2d7b3258916233b1fb41fadfff08257fdc50d9738b14'
+
+  url "http://downloads.sourceforge.net/sourceforge/natron/Natron_MaxOSX_x86_64bits_v#{version}.dmg"
+  name 'Natron'
+  homepage 'https://natron.inria.fr/'
+  license :mpl
+
+  app 'Natron.app'
+end
+
+
+
