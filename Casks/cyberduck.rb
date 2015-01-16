3c6437550e30b6665f5bb34f32492ea3d59d4fac
Johnathan Conley <johnathan.conley@gmail.com>
2015-01-15 21:43:27 -0600

    update cyberduck to 4.6.2

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'cyberduck' do
-  version '4.6.1'
-  sha256 'f6a18dd8e6422038ae0ccda7a8243c06824188b50dc6e1bfcd58bf345d6be4e9'
+  version '4.6.2'
+  sha256 '88f697f886c4e95e7ce4d126594067aee0330b762045f42e75ba354ccb546243'
 
   url "https://update.cyberduck.io/Cyberduck-#{version}.zip"
   appcast 'https://version.cyberduck.io/changelog.rss',
-          :sha256 => '1eeb448a209f9ba84690c15ecde08906e4b199b0ef8e99b4272f228744cda821'
+          :sha256 => 'f73e5eccd507833db458a174858b0c08f665b7cf0f61e840b4b7a2a380769a67'
   homepage 'http://cyberduck.io/'
   license :gpl
 
