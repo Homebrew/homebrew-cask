84cd950781c79f0934a94d3378945ec1fde4d40a
johntrandall <john@johnrandall.com>
2015-01-16 12:21:28 -0500

    Edit BitPim in response to pull request critisism.
    Changed Mac OS lower bound to Tiger.
    Removed erroneous zap.
    Stylistic improvements.

--- Casks/bitpim.rb
+++ Casks/bitpim.rb
@@ -1,19 +1,16 @@
 cask :v1 => 'bitpim' do
   version '1.0.7'
   sha256 '567d41ececc8c416746c3aa9365182797b339001d255dc4da7acc285bb289880'
-  url 'http://downloads.sourceforge.net/project/bitpim/bitpim/1.0.7/LEOPARD-bitpim-1.0.7.dmg'
+
+  depends_on :macos => '>= :tiger' #BitPim actually runs in 10.3, but homebrew-cask doesn't go back that far.
 
   name 'BitPim'
-  homepage 'http://www.bitpim.org/'
   license :gpl
+  homepage 'http://www.bitpim.org/'
+  url 'http://downloads.sourceforge.net/project/bitpim/bitpim/#{version}/LEOPARD-bitpim-1.0.7.dmg'
+
   app 'BitPim.app'
-  depends_on :macos => '>= 10.3'
 
   uninstall :quit => 'org.bitpim.bitpim'
-
-  zap :quit => 'org.bitpim.bitpim'
-  zap :delete => [
-    '~/Library/Preferences/org.bitpim.bitpim.plist',
-    '~/Library/Preferences/LaunchpadCleaner/Local\ Store/applicattons/appicons/org.bitpim.bitpim.png',
-  ]
+  zap :delete => '~/Library/Preferences/org.bitpim.bitpim.plist'
 end
