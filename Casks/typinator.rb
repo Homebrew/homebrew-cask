2f47451a91b6ea2a4db9341a990ffe470bc01cab
YeJun Su <goofansu@users.noreply.github.com>
2015-01-16 18:39:00 +0800

    Fix typinator download link

--- Casks/typinator.rb
+++ Casks/typinator.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'typinator' do
-  version '6.2'
-  sha256 '4b9b3acc26716dbf01951043f0102489ffd32436cdad6820e382dbb5b1fa0eb6'
+  version :latest
+  sha256 :no_check
 
-  url "http://www.ergonis.com/downloads/products/typinator/Typinator#{version.gsub('.', '')}-Install.dmg"
+  url "http://www.ergonis.com/downloads/typinator-install.dmg"
   name 'Typinator'
   homepage 'http://www.ergonis.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
