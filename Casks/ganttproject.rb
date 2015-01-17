cb82caffac763e86ea87b0e6539c28255daada77
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 19:34:11 +0000

    ganttproject.rb: added vendor host comment

--- Casks/ganttproject.rb
+++ Casks/ganttproject.rb
@@ -2,6 +2,7 @@ cask :v1 => 'ganttproject' do
   version '2.6.2-r1544'
   sha256 '8db9b2da23c581f4907ec091db07b7d19e784f4fcbc7fd7cea03d5ab0de23f70'
 
+  # googlecode.com is the official download host per the vendor homepage
   url "https://ganttproject.googlecode.com/files/ganttproject-#{version}.dmg"
   homepage 'http://www.ganttproject.biz'
   license :oss
