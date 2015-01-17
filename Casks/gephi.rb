89ee181b3e1fc8e9af931b709ad1ced2da649392
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 20:43:27 +0000

    updated gephi (0.8.2-beta)

--- Casks/gephi.rb
+++ Casks/gephi.rb
@@ -2,7 +2,8 @@ cask :v1 => 'gephi' do
   version '0.8.2-beta'
   sha256 'ee8f7845ade6109de7e88ed03cc92885c6d936c34d666503d7c92bf3c5d0659d'
 
-  url "https://launchpad.net/gephi/0.8/0.8.2beta/+download/gephi-#{version}.dmg"
+  # launchpadlibrarian.net is the official download host per the vendor homepage
+  url "https://launchpadlibrarian.net/127456772/gephi-#{version}.dmg"
   homepage 'http://gephi.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
