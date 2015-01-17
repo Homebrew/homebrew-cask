607167154643d9ed75d3c6b84b7a79009c593bdd
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 02:42:04 +0000

    cryptol.rb: added vendor host comment

--- Casks/cryptol.rb
+++ Casks/cryptol.rb
@@ -2,6 +2,7 @@ cask :v1 => 'cryptol' do
   version '2.1.0'
   sha256 '3d2ee4a8c7c4284458bbd5f07da52d9c3fae8ddd46faeafb7779dfa07f5cdd63'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/GaloisInc/cryptol/releases/download/v#{version}/cryptol-#{version}-MacOSX-64.tar.gz"
   gpg "#{url}.sig",
       :key_url => 'http://cryptol.net/files/Galois.asc'
