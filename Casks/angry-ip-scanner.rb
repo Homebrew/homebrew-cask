a91c27ac88c4bc878ab7a03d796881e2d0f39021
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 01:57:30 +0000

    angry-ip-scanner.rb: added vendor host comment

--- Casks/angry-ip-scanner.rb
+++ Casks/angry-ip-scanner.rb
@@ -2,6 +2,7 @@ cask :v1 => 'angry-ip-scanner' do
   version '3.3.2'
   sha256 'ed8e23f58c3539380673c26d1ed265f703207cc2866f6c3e9e004859a0a559e5'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/angryziber/ipscan/releases/download/#{version}/ipscan-mac-#{version}.zip"
   name 'Angry IP Scanner'
   homepage 'http://angryip.org'
