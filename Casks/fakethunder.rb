a5ea9e3202302cc0b223edf95e9b9f7bab45b6a6
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 18:55:06 +0000

    fakethunder.rb: added vendor host comment

--- Casks/fakethunder.rb
+++ Casks/fakethunder.rb
@@ -2,6 +2,7 @@ cask :v1 => 'fakethunder' do
   version '2.0.17'
   sha256 'c4f2d26535279e84b4b82d1e203b0efcc3bfaaed796d9fa7b587b17282f57a10'
 
+  # martianlaboratory.com is the official download host per the vendor homepage
   url 'http://martianlaboratory.com/fakethunder/fakeThunder217.zip'
   appcast 'http://martianlaboratory.com/fakethunder/update.xml',
           :sha256 => '71c38833d0f6e1fb929d6914e2cbaa3978a5155cde21353eae7a73d046b10731'
