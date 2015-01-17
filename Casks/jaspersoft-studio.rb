4c362186153adfc511cb1407c72643de9f5de77b
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 23:10:51 +0000

    updated jaspersoft-studio (6.0.1)

--- Casks/jaspersoft-studio.rb
+++ Casks/jaspersoft-studio.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'jaspersoft-studio' do
-  version '5.6.1'
-  sha256 '339222ba8c1d640f90d6339b9f5ce5292e620c2bc256a09f75b6f413acd061d2'
+  version '6.0.1'
+  sha256 '4fc50e92957d1cbf9750db9a4323ff4298c404f77c8e49a6707c3eca6b1a29d2'
 
-  url "http://downloads.sourceforge.net/project/jasperstudio/JaspersoftStudio-#{version}/jaspersoftstudio-#{version}.final-mac-x86_64.dmg"
+  # sourceforge.net is the official download host per the vendor homepage
+  url "http://downloads.sourceforge.net/project/jasperstudio/JaspersoftStudio-#{version}/TIBCOJaspersoftStudio-#{version}.final-mac-x86_64.dmg"
   name 'Jaspersoft Studio'
   homepage 'http://community.jaspersoft.com/project/jaspersoft-studio'
   license :oss
