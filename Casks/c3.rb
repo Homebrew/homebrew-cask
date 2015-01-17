6cc9a086c589405c563cf5b7d83493ef134b3ebb
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 02:54:32 +0000

    updated c3 (0.6.8.6120)

--- Casks/c3.rb
+++ Casks/c3.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'c3' do
   # note: "3" is not a version number, but an intrinsic part of the product name
-  version '0.5.2.2143'
-  sha256 '1f039168caf6487c4ded7fdec5ebf80f4400b03188a40b1bc06f0abc8bf3d3ff'
+  version '0.6.8.6120'
+  sha256 'ce8f1f18376b0c46bedca4853c5b51c3af973a12c8762522bad48c49a286b5d7'
 
-  url "https://d2xj26462na9l3.cloudfront.net/c3/C3-FNF-#{version}.dmg"
+  url "https://d2xj26462na9l3.cloudfront.net/c3/C3-#{version}.dmg"
   name 'C3'
   homepage 'http://www.downloadc3.com/'
   license :gratis
