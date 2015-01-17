4765afebdf0ec475a4974964facaf528dfc0986a
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 02:29:42 +0000

    cfxr.rb: added vendor host comment

--- Casks/cfxr.rb
+++ Casks/cfxr.rb
@@ -2,6 +2,7 @@ cask :v1 => 'cfxr' do
   version '0.2.1'
   sha256 '106e06fe1c76581ffb89b3f44419a1669526edb1609f54022390b22d0947a1f2'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/downloads/nevyn/cfxr/cfxr%20#{version}.zip"
   homepage 'http://thirdcog.eu/apps/cfxr'
   license :oss
