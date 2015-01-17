a92b0ff0f835243f4b562ba1377128c5ee2d31ad
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 18:06:29 +0000

    diashapes.rb: added vendor host comment

--- Casks/diashapes.rb
+++ Casks/diashapes.rb
@@ -2,6 +2,7 @@ cask :v1 => 'diashapes' do
   version '0.3.0'
   sha256 '2b858e180f7ed1fff23c0fd7c917ad161b6ed4e561bc7a7ce4d71bd368947925'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/dia-installer/diashapes/#{version}/diashapes-#{version}.dmg"
   homepage 'http://dia-installer.de/'
   license :oss
