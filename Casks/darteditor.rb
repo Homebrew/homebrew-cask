151cf65ed12e5426eda68243c045285c2274a38a
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 17:53:48 +0000

    darteditor.rb: added vendor host comment

--- Casks/darteditor.rb
+++ Casks/darteditor.rb
@@ -2,6 +2,7 @@ cask :v1 => 'darteditor' do
   version :latest
   sha256 :no_check
 
+  # googleapis.com is the official download host per the vendor homepage
   url 'https://storage.googleapis.com/dart-archive/channels/stable/release/latest/editor/darteditor-macos-x64.zip'
   name 'Dart Editor'
   homepage 'https://www.dartlang.org/tools/editor/'
