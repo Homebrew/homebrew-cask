98717aee50bc04212d2d0025fa6305213eaaef63
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 19:34:40 +0000

    garagebuy.rb: added vendor host comment

--- Casks/garagebuy.rb
+++ Casks/garagebuy.rb
@@ -2,6 +2,7 @@ cask :v1 => 'garagebuy' do
   version '3.1.1'
   sha256 '4f7b7b941a40be516d923338df84a43073149f785d123ea2ff938f412757f8cf'
 
+  # iwascoding.de is the official download host per the vendor homepage
   url "http://www.iwascoding.de/downloads/GarageBuy_#{version}.dmg"
   homepage 'http://www.iwascoding.com/GarageBuy'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
