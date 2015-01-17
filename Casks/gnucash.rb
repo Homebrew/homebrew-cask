932e150e62a2916a0698cfa5b3d66019afa3a587
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 19:44:38 +0000

    gnucash.rb: added vendor host comment

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -2,6 +2,7 @@ cask :v1 => 'gnucash' do
   version '2.6.5-6'
   sha256 '84a55674ef9a78b0aebf530017cca31204d1270110675c916bee0fb8d838030a'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-#{version}.dmg"
   homepage 'http://www.gnucash.org'
   license :oss
