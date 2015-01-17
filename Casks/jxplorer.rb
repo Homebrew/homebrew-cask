692510e3190ffa8233752e3e439d19d1ced7ee69
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 23:25:20 +0000

    jxplorer.rb: added vendor host comment

--- Casks/jxplorer.rb
+++ Casks/jxplorer.rb
@@ -2,6 +2,7 @@ cask :v1 => 'jxplorer' do
   version '3.3.1'
   sha256 'b51995a93203590e6690d8ad54f73cd7af1c9f2bef6219adca79c58eda71d860'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/jxplorer/jxplorer/version%20#{version}/jxplorer-#{version}-osx.zip"
   homepage 'http://jxplorer.org'
   license :oss
