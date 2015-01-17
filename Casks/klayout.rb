0cdcd85d544ce441c6b89e6e2c596ae28ab3b70d
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 23:28:09 +0000

    klayout.rb: added vendor host comment

--- Casks/klayout.rb
+++ Casks/klayout.rb
@@ -2,6 +2,7 @@ cask :v1 => 'klayout' do
   version '0.23.2'
   sha256 '96ce3fdead710248ed2ed4f25c9a94859949466d42eaa4f87881c17567dc1f15'
 
+  # 178.77.72.242 is the official download host per the vendor homepage
   url "http://178.77.72.242/downloads/klayout.#{version}.pkg"
   name 'KLayout'
   homepage 'http://www.klayout.de/'
