b774d75e050d4ccdb510f7be5bfd463a44cc45f3
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 03:03:12 +0000

    updated angry-ip-scanner (3.3.2)

--- Casks/angry-ip-scanner.rb
+++ Casks/angry-ip-scanner.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'angry-ip-scanner' do
-  version '3.3.1'
-  sha256 '5e3df2bbf690bb6e9c58ac2ca4a1ae825d5242159846e5b712c89afd839f6f0c'
+  version '3.3.2'
+  sha256 'ed8e23f58c3539380673c26d1ed265f703207cc2866f6c3e9e004859a0a559e5'
 
   url "https://github.com/angryziber/ipscan/releases/download/#{version}/ipscan-mac-#{version}.zip"
   name 'Angry IP Scanner'
