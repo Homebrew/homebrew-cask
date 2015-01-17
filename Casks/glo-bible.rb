b71392efae809117f66364689b19e33a32c0b1c4
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 19:44:12 +0000

    glo-bible.rb: added vendor host comment

--- Casks/glo-bible.rb
+++ Casks/glo-bible.rb
@@ -2,6 +2,7 @@ cask :v1 => 'glo-bible' do
   version :latest
   sha256 :no_check
 
+  # llnwd.net is the official download host per the vendor homepage
   url 'http://immersion.vo.llnwd.net/o42/pub/glo/mac/en-us/Glo.dmg'
   homepage 'http://globible.com/gloformac/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
