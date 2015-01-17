ba9e111742cdaf6656ca75bc83e33c781ea0fb9f
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 02:55:26 +0000

    updated blink1control (1.94)

--- Casks/blink1control.rb
+++ Casks/blink1control.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'blink1control' do
-  version '1.91'
-  sha256 'fc3624a041f561f086cdce95bd70470b072c9b586e9ee1247d88b78e6abce094'
+  version '1.94'
+  sha256 '0c6443258658dc7dc0ad7bb4c72c4d47665b2cc080c1d620532ad72ee5d6210e'
 
   url "https://github.com/todbot/blink1/releases/download/v#{version}/Blink1Control-mac.zip"
   homepage 'http://blink1.thingm.com/'
