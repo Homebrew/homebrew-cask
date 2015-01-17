4c7c8cd7118a12ab6bc4732c014c75d2bef1f107
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 20:13:19 +0000

    inssider.rb: added vendor host comment

--- Casks/inssider.rb
+++ Casks/inssider.rb
@@ -2,6 +2,7 @@ cask :v1 => 'inssider' do
   version :latest
   sha256 :no_check
 
+  # metageek.net is the official download host per the vendor homepage
   url 'http://files.metageek.net/downloads/inSSIDer4-installer.dmg'
   name 'inSSIDer'
   homepage 'http://www.inssider.com/'
