4ebd8c1f28c7ceb56c1e654d88561daa47559545
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 19:30:16 +0000

    fpc.rb: added vendor host comment

--- Casks/fpc.rb
+++ Casks/fpc.rb
@@ -2,6 +2,7 @@ cask :v1 => 'fpc' do
   version '2.6.2'
   sha256 '09b0964c6fb11eaa04e0fa065e479674384aab81e69e377bb1e030ec1d3398a6'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/freepascal/fpc-#{version}.intel-macosx.dmg"
   homepage 'http://www.freepascal.org/'
   license :oss
