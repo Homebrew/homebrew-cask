3b1a9ab8084610ae3725ea40d835ec2c76e878af
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 18:46:51 +0000

    exist-db.rb: added vendor host comment

--- Casks/exist-db.rb
+++ Casks/exist-db.rb
@@ -2,6 +2,7 @@ cask :v1 => 'exist-db' do
   version '2.2RC2'
   sha256 '35aa7e36f21043629a7d562968cc803e6dae84429be4a0d1c77ed62bdf6f9f2f'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/exist/eXist-db-#{version}.dmg"
   homepage 'http://exist-db.org/'
   license :oss
