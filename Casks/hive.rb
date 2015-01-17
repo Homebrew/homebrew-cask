46dc553fd1465aadc458c3eb41a0166b51fc3975
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 20:02:02 +0000

    hive.rb: added vendor host comment

--- Casks/hive.rb
+++ Casks/hive.rb
@@ -2,6 +2,7 @@ cask :v1 => 'hive' do
   version '1.4.1'
   sha256 '380f056ee5f88c86e69f1f08723db341aa482f36eb048972664d661f44efa58b'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/hivewallet/hive-osx/releases/download/#{version}/Hive-#{version}.zip"
   appcast 'https://hivewallet.com/hive-osx-appcast.xml',
           :sha256 => '53a58f4b4bc888cde3e036ee3b09b44f0ba19b321492c82a93acc01891d310e1'
