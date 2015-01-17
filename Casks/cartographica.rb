a9ec822069ea506639f124de9622bac846d0f33e
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 02:29:04 +0000

    cartographica.rb: added vendor host comment

--- Casks/cartographica.rb
+++ Casks/cartographica.rb
@@ -2,6 +2,7 @@ cask :v1 => 'cartographica' do
   version :latest
   sha256 :no_check
 
+  # cluetrust.com is the official download host per the vendor homepage
   url 'http://www.cluetrust.com/Downloads/Cartographica.dmg'
   homepage 'https://www.macgis.com/'
   license :commercial
