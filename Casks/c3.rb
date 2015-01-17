ffbb2a6f6d67fb3cb6edc2f515dc3781b118675a
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 02:26:05 +0000

    c3.rb: added vendor host comment

--- Casks/c3.rb
+++ Casks/c3.rb
@@ -3,6 +3,7 @@ cask :v1 => 'c3' do
   version '0.6.8.6120'
   sha256 'ce8f1f18376b0c46bedca4853c5b51c3af973a12c8762522bad48c49a286b5d7'
 
+  # cloudfront.net is the official download host per the vendor homepage
   url "https://d2xj26462na9l3.cloudfront.net/c3/C3-#{version}.dmg"
   name 'C3'
   homepage 'http://www.downloadc3.com/'
