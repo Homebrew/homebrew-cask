7d93d60674ebd851145e5959aeab08c18c0f1684
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 14:50:19 +0000

    p4v.rb: removed extraneous whitespace

--- Casks/p4v.rb
+++ Casks/p4v.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'p4v' do
   version '2014.2-985932'
   sha256 '766b6f6b8669f889f1186dd96408b5b8af6b9dc6c602784d6d6ea25130007709'
- 
+
   url "http://filehost.perforce.com/perforce/r#{version.sub(%r{\A20(\d\d\.\d+).*},'\1')}/bin.macosx107x86_64/P4V.dmg"
   name 'P4V'
   name 'Perforce Visual Client'
