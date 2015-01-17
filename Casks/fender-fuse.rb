305cd0f19992653cef0a7563ab1d4fac894da55e
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 18:56:50 +0000

    fender-fuse.rb: added vendor host comment

--- Casks/fender-fuse.rb
+++ Casks/fender-fuse.rb
@@ -2,6 +2,7 @@ cask :v1 => 'fender-fuse' do
   version '2.7.1'
   sha256 'e68de1a1c1068d34dda354e2678ddac4a796b2ccdface95b034a438455442919'
 
+  # fmicassets.com is the official download host per the vendor homepage
   url "https://www.fmicassets.com/fender/support/software/fender_software/fender_fuse/mac/FenderFUSE_FULL_#{version}.dmg"
   homepage 'https://fuse.fender.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
