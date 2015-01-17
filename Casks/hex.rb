e0a0615ab78631ea49bd4126243a435289d49730
Vítor Galvão <info@vitorgalvao.com>
2015-01-17 20:01:17 +0000

    hex.rb: added vendor host comment

--- Casks/hex.rb
+++ Casks/hex.rb
@@ -2,6 +2,7 @@ cask :v1 => 'hex' do
   version :latest
   sha256 :no_check
 
+  # gameforge.com is the official download host per the vendor homepage
   url 'http://dl.hex.gameforge.com/HexInstaller.dmg'
   homepage 'http://hextcg.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
