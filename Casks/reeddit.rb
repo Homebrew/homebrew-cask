5abf1239c0dd4f81dc1640662cca96b07e76e100
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 02:51:00 +0000

    reeddit.rb: added vendor host comment

--- Casks/reeddit.rb
+++ Casks/reeddit.rb
@@ -2,6 +2,7 @@ cask :v1 => 'reeddit' do
   version '1.9'
   sha256 '7bb465aa5a8c80adfb6f73430caee9cc977afae77d2a55be97371b9516aa01a7'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/berbaquero/Reeddit-app/releases/download/v#{version}/Reeddit.app.zip"
   homepage 'http://mac.reedditapp.com'
   license :oss
