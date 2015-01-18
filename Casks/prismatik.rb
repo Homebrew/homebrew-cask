56e24137b2cad49ec3190d80fd505a199587ab83
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 02:33:34 +0000

    prismatik.rb: added vendor host comment

--- Casks/prismatik.rb
+++ Casks/prismatik.rb
@@ -2,6 +2,7 @@ cask :v1 => 'prismatik' do
   version '5.11.1'
   sha256 '178ea51c143c2cb005199f5cfaf24695324b951ba98c55cec83c7882e6420903'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/woodenshark/Lightpack/releases/download/#{version}/Prismatik.#{version}.dmg"
   name 'Prismatik'
   homepage 'http://lightpack.tv/'
