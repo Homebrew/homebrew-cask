1b0582a88417eaeea04752937d788aa03dffd99e
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 01:42:24 +0000

    mongodb.rb: added vendor host comment

--- Casks/mongodb.rb
+++ Casks/mongodb.rb
@@ -2,6 +2,7 @@ cask :v1 => 'mongodb' do
   version '1.2'
   sha256 '9b7f6e8988a3169bf5f234ee10f93823a16750b3c1d4de524cd2e2f09452fd02'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/orelord/mongodbx-app/releases/download/v#{version}/MongoDBX-#{version}-2.4.9.zip"
   homepage 'http://mongodbx-app.orelord.com/'
   license :oss
