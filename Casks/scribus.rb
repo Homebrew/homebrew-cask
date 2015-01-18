2dc1c561a57028b11caeabe3e751378ac126a60e
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 03:10:03 +0000

    scribus.rb: added vendor host comment

--- Casks/scribus.rb
+++ Casks/scribus.rb
@@ -2,6 +2,7 @@ cask :v1 => 'scribus' do
   version '1.4.4'
   sha256 '57cdfacbfa6c60c035b746ac40ea8c46718fdfd4a9ac382b3b6c56a318fa162c'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/scribus/scribus/#{version}/scribus-#{version}.dmg"
   name 'Scribus'
   homepage 'http://www.scribus.net/canvas/Scribus'
