fbe24e0897f33422b1b88e34db5bbac87715f162
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 02:33:54 +0000

    projectlibre.rb: added vendor host comment

--- Casks/projectlibre.rb
+++ Casks/projectlibre.rb
@@ -2,6 +2,7 @@ cask :v1 => 'projectlibre' do
   version '1.5.8'
   sha256 'efd7d228d2e71a2a5af185d2fe1ddcea4abc55af32188344e06e1871facd0c6b'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/projectlibre/ProjectLibre/#{version}/projectlibre-#{version}.dmg"
   homepage 'http://www.projectlibre.org/'
   license :oss
