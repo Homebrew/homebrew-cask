7a335f8150834beb25ae7d73aebadb4133689512
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 02:00:58 +0000

    opennx.rb: added vendor host comment

--- Casks/opennx.rb
+++ Casks/opennx.rb
@@ -2,6 +2,7 @@ cask :v1 => 'opennx' do
   version '0.16.0.729'
   sha256 '65dde1a3504a17ac58ed2a7178536347d829ee8c27cc90bebdae3e98c36fc6c6'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/opennx/OpenNX-#{version}.dmg"
   homepage 'http://opennx.net/'
   license :oss
