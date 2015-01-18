ab0fa5adcb1c922f47646763abb638937a2cdf83
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 02:34:40 +0000

    psi.rb: added vendor host comment

--- Casks/psi.rb
+++ Casks/psi.rb
@@ -2,6 +2,7 @@ cask :v1 => 'psi' do
   version '0.15'
   sha256 '76d50b5314d0a7d9f9a0a71d90e0f806f2da25436135dbd90bd1959286747742'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/psi/Psi-#{version}.dmg"
   name 'Psi'
   homepage 'http://psi-im.org/'
