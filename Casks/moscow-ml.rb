1381b4205aafd64a3000d21cce9c2aa469c2564c
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 01:43:14 +0000

    moscow-ml.rb: added vendor host comment

--- Casks/moscow-ml.rb
+++ Casks/moscow-ml.rb
@@ -2,6 +2,7 @@ cask :v1 => 'moscow-ml' do
   version '2.10.1'
   sha256 '4b3e2035b106c688e43e7d415ca74ca8970f74656cc2c17326c5fb7d1f948ca0'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/kfl/mosml/releases/download/ver-#{version}/mosml-#{version}.pkg"
   name 'Moscow ML'
   homepage 'http://mosml.org/'
