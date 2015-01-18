9f3214a910e1f22987539809f6b2e2e450cf8410
Jon Chen <bsd@voltaire.sh>
2015-01-18 09:19:06 -0800

    Upgrade Plex Home Theater to 1.2.3.378-0c92ed32

--- Casks/plex-home-theater.rb
+++ Casks/plex-home-theater.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'plex-home-theater' do
-  version '1.2.3.378-0c92ed32'
+  version '1.3.5.431-7966a4df'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '7c2fd9541104346478fd7fa1e34c609bc6494202e82dcb91533b1e3931add232'
+    sha256 'c7ee467979ae0401db98cb7e8309a1bf81380c224d5c1bf58437f792ac95a0dd'
     url "https://downloads.plex.tv/plex-home-theater/#{version}/PlexHomeTheater-#{version}-macosx-i386.zip"
   else
-    sha256 '329011165014f20110c258049ab5de6c84a2957065e7dd01dfa68599ae9d4810'
+    sha256 '02f36aba9524563e6f67309d63f6c48e75ebaaddb5dc8dc825609f67d1701552'
     url "https://downloads.plex.tv/plex-home-theater/#{version}/PlexHomeTheater-#{version}-macosx-x86_64.zip"
   end
 
