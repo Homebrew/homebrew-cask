b20e185345ea158f11546887a6d9d2200c36ca17
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 03:09:47 +0000

    updated schnapps (latest)

--- Casks/schnapps.rb
+++ Casks/schnapps.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'schnapps' do
-  version '1.0'
-  sha256 '93beb5d29311f692e1eb1b2843d0ae8cce8a8b7beb200bea50d021f163190cc5'
+  version :latest
+  sha256 :no_check
 
-  url "http://d15xn61otjv90c.cloudfront.net/download/Schnapps_v#{version}.zip"
+  url 'http://d15xn61otjv90c.cloudfront.net/download/Schnapps_latest.zip'
   name 'Schnapps'
   homepage 'http://schnappsformac.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
