0483d302db3efc9050e6f0a630afaf59baa2c50f
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 00:53:04 +0000

    linphone.rb: added vendor host comment

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -2,6 +2,7 @@ cask :v1 => 'linphone' do
   version '3.7.0'
   sha256 '4d4a01354a7b5cd011746d3477a93ffb6e531ff8e2afccd2b9bb031f06cc42cc'
 
+  # gnu.org is the official download host per the vendor homepage
   url "http://download-mirror.savannah.gnu.org/releases/linphone/3.7.x/macos/linphone-#{version}.dmg"
   gpg "#{url}.sig",
       :key_id => '3ecd52dee2f56985'
