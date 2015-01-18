6dd808b6cecf39a08296a5a14e7ce43db498cae3
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 00:59:35 +0000

    love.rb: added vendor host comment

--- Casks/love.rb
+++ Casks/love.rb
@@ -2,6 +2,7 @@ cask :v1 => 'love' do
   version '0.9.1'
   sha256 '40dfeb1069f6c056b06d0e87c64f3950fd1b1523a9e19af2b03912a5d5c03b13'
 
+  # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/rude/love/downloads/love-#{version}-macosx-x64.zip"
   name 'LÖVE'
   homepage 'http://love2d.org'
