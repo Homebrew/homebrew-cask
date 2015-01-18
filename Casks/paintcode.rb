3780e8a6bb598f75392a0242daa42246dc681403
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 02:07:58 +0000

    updated paintcode (2.2.4)

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'paintcode' do
-  version :latest
-  sha256 :no_check
+  version '2.2.4'
+  sha256 '645f0042a5d2111905482b0a574004c83f7e1d053f6e0d1b992ebbe57daf2776'
 
-  url 'http://www.pixelcut.com/paintcode/paintcode.zip'
+  url "http://www.paintcodeapp.com/content/versions/#{version}/paintcode-trial.zip"
   appcast 'http://www.pixelcut.com/paintcode/appcast.xml'
   homepage 'http://www.paintcodeapp.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'PaintCode.app'
 end
