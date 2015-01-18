28f4a90647e3b273739e26143be787d5f95f10ae
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 02:19:38 +0000

    plover.rb: added vendor host comment

--- Casks/plover.rb
+++ Casks/plover.rb
@@ -2,6 +2,7 @@ cask :v1 => 'plover' do
   version '2.5.8'
   sha256 'a8bbeddb5d6fb8d4499844257035edd62e431a4f1eb7959bdf17a15c1cbe12de'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/openstenoproject/plover/releases/download/v#{version}/Plover.dmg"
   name 'Plover'
   homepage 'http://stenoknight.com/wiki/Main_Page'
