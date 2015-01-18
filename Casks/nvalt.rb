b528dc53ff2cb3917bea9c126bb2699e5b26b00b
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 01:58:25 +0000

    nvalt.rb: added vendor host comment

--- Casks/nvalt.rb
+++ Casks/nvalt.rb
@@ -2,6 +2,7 @@ cask :v1 => 'nvalt' do
   version '2.2b111'
   sha256 'd787ddf92730bb03ba084e72bc6fb5f4fbd42731fa3531476af9eb3ce39e1cd0'
 
+  # designheresy.com is the official download host per the vendor homepage
   url "http://abyss.designheresy.com/nvaltb/nvalt#{version}.zip"
   homepage 'http://brettterpstra.com/project/nvalt/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
