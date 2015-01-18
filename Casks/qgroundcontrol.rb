540753693c8321777867f9680d82e779a50c9d0e
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 03:08:51 +0000

    updated qgroundcontrol (2.2.1)

--- Casks/qgroundcontrol.rb
+++ Casks/qgroundcontrol.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'qgroundcontrol' do
-  version :latest
-  sha256 :no_check
+  version '2.2.1'
+  sha256 '4a35ac5051a408a9aaef9127c27967fed15232a6b91ea9f93341e0fd4805f1c8'
 
-  url 'http://latestfiasco.com/ftp/qgroundcontrol-pixhawk.dmg'
+  # latestfiasco.com is the official download host per the vendor homepage
+  url "http://latestfiasco.com/ftp/QGroundControl-Stable-V#{version}.dmg"
   homepage 'http://qgroundcontrol.org'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
