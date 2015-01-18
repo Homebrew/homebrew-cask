762ec6ffb398b5a8ab00d6ed87aa157c4bd0d1a0
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 03:21:59 +0000

    sequel-pro.rb: added vendor host comment

--- Casks/sequel-pro.rb
+++ Casks/sequel-pro.rb
@@ -2,6 +2,7 @@ cask :v1 => 'sequel-pro' do
   version '1.0.2'
   sha256 'facd99344d0124cf4444acbef9006d947eadc6f96127b09e7380f014c7775f85'
 
+  # googlecode.com is the official download host per the vendor homepage
   url "https://sequel-pro.googlecode.com/files/sequel-pro-#{version}.dmg"
   appcast 'http://www.sequelpro.com/appcast/app-releases.xml',
           :sha256 => 'd6137595bccddd81edfb3a07a82b4ed818b8b1af79750397f929bf74b91d3e32'
