6148d4aa54a4a13133de6120244989a274b530f9
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 01:04:52 +0000

    makehuman.rb: added vendor host comment

--- Casks/makehuman.rb
+++ Casks/makehuman.rb
@@ -2,6 +2,7 @@ cask :v1 => 'makehuman' do
   version '1.0.2'
   sha256 'ea74381b1fd9c0f699b2cf1a3476d7cf9447fad3beb3c48de0a4017032c44de9'
 
+  # tuxfamily.org is the official download host per the vendor homepage
   url "http://download.tuxfamily.org/makehuman/releases/#{version}/makehuman-#{version}-osx.dmg"
   homepage 'http://www.makehuman.org/'
   license :affero
