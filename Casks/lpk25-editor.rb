3bc0183c849d267dc14939302f091f26aa8c33bc
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 01:00:10 +0000

    lpk25-editor.rb: added vendor host comment

--- Casks/lpk25-editor.rb
+++ Casks/lpk25-editor.rb
@@ -2,6 +2,7 @@ cask :v1 => 'lpk25-editor' do
   version :latest
   sha256 :no_check
 
+  # rackcdn.com is the official download host per the vendor homepage
   url 'http://6be54c364949b623a3c0-4409a68c214f3a9eeca8d0265e9266c0.r0.cf2.rackcdn.com/453/downloads/lpk25_editor_mac_00.zip'
   homepage 'http://www.akaipro.com/index.php/product/lpk25'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
