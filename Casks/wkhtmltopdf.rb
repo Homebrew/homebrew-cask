e6f2cf691044ebcb732fc58ab73a5ff11daf0d41
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 15:22:07 +0000

    wkhtmltopdf.rb: added 32bit conditional

--- Casks/wkhtmltopdf.rb
+++ Casks/wkhtmltopdf.rb
@@ -1,13 +1,19 @@
 cask :v1 => 'wkhtmltopdf' do
   version '0.12.2'
-  sha256 'f53fc73dde8eee05a3a3c05e9666123e5e71fef77b8b658f78801a9a8223b6a9'
 
-  url "http://downloads.sourceforge.net/project/wkhtmltopdf/#{version}/wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
+  if Hardware::CPU.is_32_bit?
+    sha256 '63a3aaf56602e1630e136c5b43903e4d4757412f56184381d27be2fe9395967b'
+    url "http://downloads.sourceforge.net/project/wkhtmltopdf/#{version}/wkhtmltox-#{version}_osx-carbon-i386.pkg"
+    pkg "wkhtmltox-#{version}_osx-carbon-i386.pkg"
+  else
+    sha256 'f53fc73dde8eee05a3a3c05e9666123e5e71fef77b8b658f78801a9a8223b6a9'
+    url "http://downloads.sourceforge.net/project/wkhtmltopdf/#{version}/wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
+    pkg "wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
+  end
+
   name 'wkhtmltopdf'
   homepage 'http://wkhtmltopdf.org/'
   license :gpl
-
-  pkg "wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
 
   uninstall :pkgutil => 'org.wkhtmltopdf.wkhtmltox'
 end
