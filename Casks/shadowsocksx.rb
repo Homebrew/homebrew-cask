8bf7da1c0e2e31f7fe9807bfd3f26c760d533c63
Victor Yin <viztor@users.noreply.github.com>
2015-01-19 01:22:07 +0800

    Update shadowsocksx.rb
    Now in single quotes

--- Casks/shadowsocksx.rb
+++ Casks/shadowsocksx.rb
@@ -2,7 +2,7 @@ cask :v1 => 'shadowsocksx' do
   version :latest
   sha256 :no_check
 
-  url "http://sourceforge.net/projects/shadowsocksgui/files/latest/download"
+  url 'http://sourceforge.net/projects/shadowsocksgui/files/latest/download'
   homepage 'https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help'
   license :gpl
 
