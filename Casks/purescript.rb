22d0ec5ce0ec19bad4deb87ebc25c6dec050bb13
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 02:35:45 +0000

    purescript.rb: added vendor host comment

--- Casks/purescript.rb
+++ Casks/purescript.rb
@@ -2,6 +2,7 @@ cask :v1 => 'purescript' do
   version '0.6.3'
   sha256 'f2b2281b2db1d66570b16bbe6d104ba36787ed7d684b640917e673ea0cc5cdc2'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/purescript/purescript/releases/download/v#{version}/macos.tar.gz"
   name 'PureScript'
   homepage 'http://purescript.org'
