671e7cf06cd4658914d4d45c9067a9dbe7da7f8c
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 00:33:07 +0000

    kompozer.rb: added vendor host comment

--- Casks/kompozer.rb
+++ Casks/kompozer.rb
@@ -2,6 +2,7 @@ cask :v1 => 'kompozer' do
   version '0.8b3'
   sha256 '415e019c9b3ec1c76465bf4f561fa515f403e57ac6f92c76365d902241dc14ed'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/kompozer/current/#{version}/macosx/kompozer-#{version}.en-US.mac-universal.dmg"
   homepage 'http://www.kompozer.net/'
   license :oss
