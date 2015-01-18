a73ea6f2eac18a297c938e42f937f9a4843c093d
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 03:21:08 +0000

    seamonkey.rb: added vendor host comment

--- Casks/seamonkey.rb
+++ Casks/seamonkey.rb
@@ -2,6 +2,7 @@ cask :v1 => 'seamonkey' do
   version '2.25'
   sha256 '3217e3d1fb94376d01ca38cc926b1b23306579c96e96adfdd50f0f66fc71c5e8'
 
+  # mozilla.org is the official download host per the vendor homepage
   url "https://download.mozilla.org/?product=seamonkey-#{version}&os=osx&lang=en-US"
   homepage 'http://www.seamonkey-project.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
