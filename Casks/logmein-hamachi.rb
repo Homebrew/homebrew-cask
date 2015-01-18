432ba228709bd09bf6803b1a678aad7c3a40862d
Vítor Galvão <info@vitorgalvao.com>
2015-01-18 00:59:26 +0000

    logmein-hamachi.rb: added vendor host comment

--- Casks/logmein-hamachi.rb
+++ Casks/logmein-hamachi.rb
@@ -2,6 +2,7 @@ cask :v1 => 'logmein-hamachi' do
   version :latest
   sha256 :no_check
 
+  # logmein.com is the official download host per the vendor homepage
   url 'https://secure.logmein.com/LogMeInHamachi.zip'
   name 'Hamachi'
   homepage 'http://vpn.net'
