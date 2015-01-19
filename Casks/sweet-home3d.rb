03eb70e1a7788fb094c9e5aa44f90097cf72a6ef
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 02:10:46 +0000

    sweet-home3d.rb: added vendor host comment

--- Casks/sweet-home3d.rb
+++ Casks/sweet-home3d.rb
@@ -2,6 +2,7 @@ cask :v1 => 'sweet-home3d' do
   version '4.5'
   sha256 'f1d341046fbf066d92cc625b44bb201e905017c72e9755151587a01539f5242b'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-#{version}/SweetHome3D-#{version}-macosx.dmg"
   homepage 'http://www.sweethome3d.com/'
   license :oss
