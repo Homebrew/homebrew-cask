4c2e4cab22af4bacf5a24d4997d195e5bdc68dbf
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 02:32:31 +0000

    updated the-cheat (1.2.5)

--- Casks/the-cheat.rb
+++ Casks/the-cheat.rb
@@ -2,8 +2,8 @@ cask :v1 => 'the-cheat' do
   version '1.2.5'
   sha256 '24ed774cc21adc2355077123d04c2657295a41183fd5555c41a2342063c3dedc'
 
-  url "ftp://ftp.dogcows.com/pub/mac/thecheat/thecheat-#{version}.dmg"
-  homepage 'http://the-cheat.en.softonic.com/mac'
+  url "http://chazmcgarvey.github.com/thecheat/thecheat-#{version}.dmg"
+  homepage 'https://github.com/chazmcgarvey/thecheat'
   license :gratis
 
   app 'The Cheat.app'
