cf23ebeb66acaa860fc30b40fc4d1e1a5b0d2460
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 03:02:32 +0000

    vagrant.rb: added vendor host comment

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -2,6 +2,7 @@ cask :v1 => 'vagrant' do
   version '1.7.2'
   sha256 '78d02afada2f066368bd0ce1883f900f89b6dc20f860463ce125e7cb295e347c'
 
+  # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/mitchellh/vagrant/vagrant_#{version}.dmg"
   homepage 'http://www.vagrantup.com'
   license :mit
