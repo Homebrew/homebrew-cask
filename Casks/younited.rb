bdfb62c6cf7ca3e5c066a1d0f9cd082fe4700127
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 03:21:06 +0000

    younited.rb: added vendor host comment

--- Casks/younited.rb
+++ Casks/younited.rb
@@ -2,6 +2,7 @@ cask :v1 => 'younited' do
   version :latest
   sha256 :no_check
 
+  # secure.com is the official download host per the vendor homepage
   url 'https://download.sp.f-secure.com/younited/younited.dmg'
   homepage 'http://www.younited.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
