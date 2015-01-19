4571a3fa841ed996dfe5b5871aa17ecf0f870b1c
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 03:20:50 +0000

    updated visit (2.8.2)

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -1,7 +1,8 @@
 cask :v1 => 'visit' do
-  version '2.7.2'
-  sha256 '3c1f1b201358876c1b23717a1b3a2c5a1777755e3fc7f242c08b53a033402fbc'
+  version '2.8.2'
+  sha256 'efd91ce3b3deec5b7703a3ae3cf8a12355b6e27334050b176e56552917221d9f'
 
+  # nersc.gov is the official download host per the vendor homepage
   url "https://portal.nersc.gov/svn/visit/trunk/releases/#{version}/VisIt-#{version}.dmg"
   name 'VisIt'
   homepage 'https://wci.llnl.gov/simulation/computer-codes/visit'
