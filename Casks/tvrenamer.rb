d01823897645278fc180baf1d56602e23de6e287
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 02:56:47 +0000

    tvrenamer.rb: added vendor host comment

--- Casks/tvrenamer.rb
+++ Casks/tvrenamer.rb
@@ -2,6 +2,7 @@ cask :v1 => 'tvrenamer' do
   version '0.6'
   sha256 'fe38d8f7fab69bc54673ef9822b01401d5d72453865842f27023ecd7de4a531a'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/tvrenamer/tvrenamer/releases/download/v#{version}/TVRenamer-#{version}-osx64.zip"
   homepage 'http://tvrenamer.org'
   license :gpl
