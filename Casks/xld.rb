2b5a28c6dd7a10c4b50febbc7434f1a22c602e6c
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 03:19:55 +0000

    xld.rb: added vendor host comment

--- Casks/xld.rb
+++ Casks/xld.rb
@@ -2,6 +2,7 @@ cask :v1 => 'xld' do
   version '20141129'
   sha256 'adbf053cb2b24fb342302932b90ec745740d0421d6b5fbf48c5c35367538d9fd'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/xld/xld-#{version}.dmg"
   appcast 'http://xld.googlecode.com/svn/appcast/xld-appcast_e.xml',
           :sha256 => '16420b367df9b64870bd2a38ee20e688d17b065783b93371065ac7094ab93cb0'
