2105c82c60d8bdd8798e7d7a50d3c5c29f2637e6
Vítor Galvão <info@vitorgalvao.com>
2015-01-19 02:26:13 +0000

    temperaturemonitor.rb: added vendor host comment

--- Casks/temperaturemonitor.rb
+++ Casks/temperaturemonitor.rb
@@ -2,6 +2,7 @@ cask :v1 => 'temperaturemonitor' do
   version :latest
   sha256 :no_check
 
+  # bresink.eu is the official download host per the vendor homepage
   url 'http://www.bresink.eu/Downloads/TemperatureMonitor.dmg'
   homepage 'http://www.bresink.com/osx/LegacyProducts.html'
   license :commercial
