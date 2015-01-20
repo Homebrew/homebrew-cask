f375f335f8685b6f8ce00ef314d00e9d799c27f6
Vítor Galvão <info@vitorgalvao.com>
2015-01-20 01:21:12 +0000

    maratis.rb: include '-beta' in version

--- Casks/maratis.rb
+++ Casks/maratis.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'maratis' do
-  version '3.21'
+  version '3.21-beta'
   sha256 '7ec6dd97f1ce1ce09e08df0947fcb6ceb7a9f63024998f44afed3d3965829707'
 
-  url "https://maratis.googlecode.com/files/Maratis-#{version}-beta-osx10.6.zip"
+  url "https://maratis.googlecode.com/files/Maratis-#{version}-osx10.6.zip"
   name 'Maratis'
   homepage 'http://www.maratis3d.org/'
   license :oss
