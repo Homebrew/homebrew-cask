b3838a7e90169c14d805e00b812f66dc1d423f67
Robert Brown <robert@slan124.info>
2015-01-20 20:32:46 +0000

    Update Vagrant Manager to version 2.2.1

--- Casks/vagrant-manager.rb
+++ Casks/vagrant-manager.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'vagrant-manager' do
-  version '2.1.2'
-  sha256 '8510664b4834c98ed9b72baa30aae611f640ca2292725a8cd6ae6d99d412dd4b'
+  version '2.2.1'
+  sha256 'c9d63396cb322c65573a3a0c54dd700db18cff736b0f8b21a7813ac51d6ea5dd'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/lanayotech/vagrant-manager/releases/download/#{version}/vagrant-manager-#{version}.dmg"
   appcast 'http://api.lanayo.com/appcast/vagrant_manager.xml',
-          :sha256 => '3dbe30121d4f32c4d600e30d58d6cfea142d9510823be7a125952aa2e05ea3af'
+          :sha256 => '599d9d60e76b718094569353ddd31850cdd3efaed6de61d320f53e7bf83073fb'
   name 'Vagrant Manager'
   homepage 'http://vagrantmanager.com/'
   license :mit
