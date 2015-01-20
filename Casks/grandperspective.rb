0629f5563b798434b3c28d7acfefe3b8eae832a3
ndr <ndr@qef.io>
2015-01-20 18:20:57 +0000

    Cask grandperspective: reuse version in url

--- Casks/grandperspective.rb
+++ Casks/grandperspective.rb
@@ -2,7 +2,7 @@ cask :v1 => 'grandperspective' do
   version '1.5.1'
   sha256 '92204458042a337c1091879e167ea95e45cae33a7be16fa6c11e80572c54d135'
 
-  url "http://downloads.sourceforge.net/project/grandperspectiv/grandperspective/#{version}/GrandPerspective-1_5_1.dmg"
+  url "http://downloads.sourceforge.net/project/grandperspectiv/grandperspective/#{version}/GrandPerspective-#{version.gsub('.','_')}.dmg"
   homepage 'http://grandperspectiv.sourceforge.net/'
   license :oss
 
