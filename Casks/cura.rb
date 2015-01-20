ac7fa0d912be1f242b1cf10ba14f96a1f3223f68
Vítor Galvão <info@vitorgalvao.com>
2015-01-20 13:31:03 +0000

    cura.rb: refine homepage

--- Casks/cura.rb
+++ Casks/cura.rb
@@ -3,7 +3,7 @@ cask :v1 => 'cura' do
   sha256 'ace005ffd7549a6cf242f961da411d4d789e8352f8dd3b2ba0a0f8a766e99dc9'
 
   url "https://ultimaker.com/en/software/download/15/Cura-#{version}-MacOS.dmg"
-  homepage 'https://ultimaker.com/'
+  homepage 'https://ultimaker.com/en/products/software'
   license :oss
 
   app 'Cura/Cura.app'
