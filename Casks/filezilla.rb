661ac6c2874f771177b95973181114aa44d8ac7f
Mads Gram <mgmadsgram@gmail.com>
2015-01-21 03:36:46 +0100

    Upgrade FileZilla.app to v3.10.0.2 and added name

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -4,12 +4,13 @@ cask :v1 => 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.10.0.1'
-    sha256 'c3317b8052736ce042a5d27279c888e321065b87fe3cbc5c43c3600c3e52f40c'
+    version '3.10.0.2'
+    sha256 '676abc887161c96302725defe963e5f049097339f165d19746dcfa7731620eb1'
   end
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
+  url 'http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2'
+  name 'FileZilla'
   homepage 'https://filezilla-project.org/'
   license :oss
 
