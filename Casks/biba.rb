f8ceb51467d6257f62f75b57ef2bb6d50070e1e0
Derek Harms <derekharms+github@gmail.com>
2015-01-21 10:04:32 -0600

    Add Biba.app 2.14.2503
    Adds a cask for the Biba conferencing tool.

---
+++ Casks/biba.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'biba' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.biba.com/osx_downloads/dmg'
+  name 'Biba'
+  homepage 'http://www.biba.com'
+  license :closed
+
+  app 'Biba.app'
+end
