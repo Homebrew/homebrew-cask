e1b9499d6c91af082489bb58422c6eb36755c54f
Denis Patin <denpatin@gmail.com>
2015-01-21 22:20:58 +0300

    Upgrade Mono MDK to 3.12.0

--- Casks/mono-mdk.rb
+++ Casks/mono-mdk.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'mono-mdk' do
-  version '3.10.0'
+  version '3.12.0'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "http://download.mono-project.com/archive/#{version}/macos-10-x86/MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
-  homepage 'http://mono-project.com/'
+  homepage 'http://mono-project.com'
   license :oss
 
   pkg "MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
