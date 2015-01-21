c9ee36b64bb46431f702f1cb6860d135f8c376d7
Sebastian Nash <sebnash@gmail.com>
2015-01-21 17:25:50 +0500

    Upgrade Audio Hijack to v3.0
    New version of Audio Hijack Pro. In the upgrade, the app has lost the "Pro".
    Renamed cask, url, name, etc. accordingly.

---
+++ Casks/audio-hijack.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'audio-hijack' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://rogueamoeba.com/audiohijack/download/AudioHijack.zip'
+  name 'Audio Hijack'
+  homepage 'http://www.rogueamoeba.com/audiohijack/'
+  license :commercial
+
+  app 'Audio Hijack.app'
+end
