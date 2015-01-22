9c1ae49ebab062e0cd008e4e1a84a0ccecd00108
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:49:52 +0000

    tabula.rb: added name

--- Casks/tabula.rb
+++ Casks/tabula.rb
@@ -3,6 +3,7 @@ cask :v1 => 'tabula' do
   sha256 'b6a2663cbf440d27cc94ac9ac05ac1875136e81f63980a25e1acaf6a3250e131'
 
   url "https://github.com/jazzido/tabula/releases/download/v0.9.5/tabula-mac-#{version}.zip"
+  name 'Tabula'
   homepage 'http://tabula.nerdpower.org'
   license :mit
 
