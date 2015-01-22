3c9997c1c6cad5271c9e22e8048a27852cc1504e
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:22:51 +0000

    ibackup.rb: added name

--- Casks/ibackup.rb
+++ Casks/ibackup.rb
@@ -3,6 +3,7 @@ cask :v1 => 'ibackup' do
   sha256 'faa69e95f62dfeb04a6ede20e7b856d43645f1bd07aede743c5d364d2845aabf'
 
   url "http://www.grapefruit.ch/iBackup/versions/iBackup%207.x/iBackup%20#{version}.dmg"
+  name 'iBackup'
   homepage 'http://www.grapefruit.ch/iBackup'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
