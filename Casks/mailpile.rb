46ee8c38c992656373970ed4df7bb175ea477988
David Routhieau <root-io@users.noreply.github.com>
2015-01-22 15:32:57 +0000

    Mailpile Beta II

--- Casks/mailpile.rb
+++ Casks/mailpile.rb
@@ -2,7 +2,7 @@ cask :v1 => 'mailpile' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.mailpile.is/files/releases/Mailpile-Installer-Beta.dmg'
+  url 'https://www.mailpile.is/files/releases/Mailpile-Installer-Beta-II.dmg'
   name 'Mailpile'
   homepage 'https://www.mailpile.is/'
   license :oss
