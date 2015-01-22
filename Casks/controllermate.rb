c60bcb9c502a9fc2e3553e98d262ea2e6d602581
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:18:33 +0000

    controllermate.rb: added name

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -4,6 +4,7 @@ cask :v1 => 'controllermate' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/orderedbytes/ControllerMate.dmg'
+  name 'ControllerMate'
   homepage 'http://www.orderedbytes.com/controllermate/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
