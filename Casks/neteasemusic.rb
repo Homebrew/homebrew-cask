78abb7339096125892377e9918270ced38244f26
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:36:45 +0000

    neteasemusic.rb: added name

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -4,6 +4,8 @@ cask :v1 => 'neteasemusic' do
   sha256 :no_check
 
   url "http://music.163.com/api/osx/download/latest"
+  name '网易云音乐'
+  name 'NetEase cloud music'
   homepage 'http://music.163.com/#/download'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
