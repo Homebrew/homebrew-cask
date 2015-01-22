8db9f6de9c7bbb6bfc0c8cfefa0237f18b202aeb
Matthew Sanchez <matthewsanchez@cox.net>
2015-01-22 12:23:27 -0700

    Update download link and sha for Eclipse.app casks
    eclipse-cpp.rb had the correct URL however the sha was downlevel.
    The remaining casks had both URL & SHA updated.

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -2,10 +2,10 @@ cask :v1 => 'eclipse-cpp' do
   version '4.4.1'
 
   if Hardware::CPU.is_32_bit?
-    sha256 'bc208f2d3cedeecd721dc8e19d1d1a379ba5b3a295d5622071f8f5a2c1b98905'
+    sha256 '5240642f6b27ace256a02799c27af49f3b9cc3036259247bdd9e848bbea999c7'
     url 'http://download.eclipse.org/technology/epp/downloads/release/luna/SR1a/eclipse-cpp-luna-SR1a-macosx-cocoa.tar.gz'
   else
-    sha256 'bd2fe24d261f8ae7e49cd33cf3b111aad279bdc64cc31efb2cd1fea789d5ae46'
+    sha256 '8c69c32083943c27c38c38808e652020443256c511f28832ee6a7bc31b835241'
     url 'http://download.eclipse.org/technology/epp/downloads/release/luna/SR1a/eclipse-cpp-luna-SR1a-macosx-cocoa-x86_64.tar.gz'
   end
 
