20d1cc56aa22e198d3eb670290424d0efb24723d
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:32:25 +0000

    windownaut.rb: added name

--- Casks/windownaut.rb
+++ Casks/windownaut.rb
@@ -3,6 +3,7 @@ cask :v1 => 'windownaut' do
   sha256 :no_check
 
   url 'http://www.binarybakery.com/products/Windownaut.dmg'
+  name 'Windownaut'
   homepage 'http://www.binarybakery.com/aprod/windownaut.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
