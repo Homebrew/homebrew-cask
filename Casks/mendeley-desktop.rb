6bd764042c87a86c1118c485659eedac3e3009b3
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:10:15 +0000

    mendeley-desktop.rb: added name

--- Casks/mendeley-desktop.rb
+++ Casks/mendeley-desktop.rb
@@ -3,6 +3,7 @@ cask :v1 => 'mendeley-desktop' do
   sha256 'd6db48ca2a5044e69a6897da03e97b200e691b59ae14031fb10f547c08c5d59f'
 
   url "http://desktop-download.mendeley.com/download/Mendeley-Desktop-#{version}-OSX-Universal.dmg"
+  name 'Mendeley'
   homepage 'http://www.mendeley.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
