76de481c49d06e9ceee664ffa1987b580f2c77ac
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:25:17 +0000

    openzfs.rb: added name

--- Casks/openzfs.rb
+++ Casks/openzfs.rb
@@ -3,6 +3,7 @@ cask :v1 => 'openzfs' do
   sha256 'a0030181a91ecace2d31cfab26d10b2284602e2b442f71a344a9da11165cb6c7'
 
   url "https://openzfsonosx.org/w/images/0/0d/OpenZFS_on_OS_X_#{version}.dmg"
+  name 'OpenZFS on OS X'
   homepage 'https://openzfsonosx.org'
   license :oss
 
