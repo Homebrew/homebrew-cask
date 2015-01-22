579e243130976f6712cb1b061f2b3ed002ae167d
Diego Zamboni <diego.zamboni@swisscom-labs.com>
2015-01-22 09:41:42 -0800

    cscreen
    Resolution-setting utility

---
+++ Casks/cscreen.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'cscreen' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.pyehouse.com/wp-content/uploads/2012/09/cscreenIntel.dmg'
+  name 'cscreen'
+  homepage '"http://www.pyehouse.com/cscreen/"'
+  license :gratis
+
+  binary 'cscreen'
+end
