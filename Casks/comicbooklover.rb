afd8e35016f34a59061a2c6fcb955012f60aab78
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:27:37 +0000

    comicbooklover.rb: added name

--- Casks/comicbooklover.rb
+++ Casks/comicbooklover.rb
@@ -4,6 +4,7 @@ cask :v1 => 'comicbooklover' do
 
   url 'https://www.bitcartel.com/downloads/comicbooklover.zip'
   appcast 'http://www.bitcartel.com/appcast/comicbooklover-1.7-dsa.xml'
+  name 'ComicBookLover'
   homepage 'http://www.bitcartel.com/comicbooklover/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
