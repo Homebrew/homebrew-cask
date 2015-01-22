5535c6fd3881326a8abb45bb075f1b40824344ad
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 16:37:38 +0000

    updated shadowsocksx (2.6.2)

--- Casks/shadowsocksx.rb
+++ Casks/shadowsocksx.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'shadowsocksx' do
-  version :latest
-  sha256 :no_check
+  version '2.6.2'
+  sha256 'dda2763e07ca707cb46fb131b82be53bf752d93a91d646d3683613054dcc251f'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://sourceforge.net/projects/shadowsocksgui/files/latest/download"
+  url "http://downloads.sourceforge.net/sourceforge/shadowsocksgui/ShadowsocksX-#{version}.dmg"
   homepage 'https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help'
   license :gpl
 
