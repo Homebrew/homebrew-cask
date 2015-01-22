f960e6e006c4b4e66183859cbe58f77009004f0a
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:22:20 +0000

    Update and rename callnote.rb to callnote-premium.rb

--- Casks/callnote-premium.rb
+++ Casks/callnote-premium.rb
@@ -1,8 +1,9 @@
-cask :v1 => 'callnote' do
+cask :v1 => 'callnote-premium' do
   version '2.3.7'
   sha256 '968c61f2cd1e71f91ec45b9f69023765210010e3d28256eeb65942ee075e756b'
 
   url "http://callnote.kandasoft.com/callnote-premium-install-#{version}.pkg"
+  name 'Callnote Premium'
   homepage 'http://www.kandasoft.com/home/kanda-apps/skype-call-recorder.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
