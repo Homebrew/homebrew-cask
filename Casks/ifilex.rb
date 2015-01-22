6f672da7bcb02bea5984a6a3fb8d30c2cb8bc84d
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:19:37 +0000

    ifilex.rb: added name

--- Casks/ifilex.rb
+++ Casks/ifilex.rb
@@ -3,6 +3,7 @@ cask :v1 => 'ifilex' do
   sha256 :no_check
 
   url 'http://www.osxbytes.com/iFileX.dmg'
+  name 'iFileX'
   homepage 'http://www.osxbytes.com/page3/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
