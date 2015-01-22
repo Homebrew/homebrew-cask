1e06a1348a9b9f42a6d5675103e7f69a099b1c55
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:29:42 +0000

    hyperdither.rb: added name

--- Casks/hyperdither.rb
+++ Casks/hyperdither.rb
@@ -3,6 +3,7 @@ cask :v1 => 'hyperdither' do
   sha256 'd91952bff7faf69255c03896a26c907fca256bbad4d1444a05b75b2ebdf10785'
 
   url 'http://www.tinrocket.com/wp-content/plugins/download-monitor/download.php?id=1'
+  name 'HyperDither'
   homepage 'http://www.tinrocket.com/hyperdither/'
   license :gratis
 
