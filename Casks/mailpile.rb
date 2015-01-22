3e26ae6d8cc9ccd9736faf214ac9a6c73797da1e
David Routhieau <root-io@users.noreply.github.com>
2015-01-22 19:44:40 +0000

    Set version and sha256 for Mailpile Beta II

--- Casks/mailpile.rb
+++ Casks/mailpile.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mailpile' do
-  version :latest
-  sha256 :no_check
+  version '0.4.2'
+  sha256 '82cb79e2602de183a10303a9f38ca69ed2b84e72f17339c275bae79b3ebd5cfd'
 
   url 'https://www.mailpile.is/files/releases/Mailpile-Installer-Beta-II.dmg'
   name 'Mailpile'
