9bf9d5d524404d961d7b6410af33056b8e1c7de4
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:07:35 +0000

    ghostlab.rb: added name

--- Casks/ghostlab.rb
+++ Casks/ghostlab.rb
@@ -4,6 +4,7 @@ cask :v1 => 'ghostlab' do
 
   url 'http://awesome.vanamco.com/downloads/ghostlab/Ghostlab.dmg'
   appcast 'http://awesome.vanamco.com/update/ghostlab-cast.xml'
+  name 'Ghostlab'
   homepage 'http://vanamco.com/ghostlab/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
