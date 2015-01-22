a28845db9338ce6b11da405df4a16810879b3e6d
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:19:25 +0000

    pins.rb: added name

--- Casks/pins.rb
+++ Casks/pins.rb
@@ -4,6 +4,7 @@ cask :v1 => 'pins' do
 
   url 'http://pinsapp.com/download/Pins.dmg'
   appcast 'http://pinsapp.com/appcast.xml'
+  name 'Pins'
   homepage 'http://pinsapp.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
