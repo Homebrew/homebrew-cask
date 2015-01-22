da075517d97bdbdab8f19560ac262f571336e8e9
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 22:31:27 +0000

    mixture.rb: added name

--- Casks/mixture.rb
+++ Casks/mixture.rb
@@ -5,6 +5,7 @@ cask :v1 => 'mixture' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/mixture-mixed-app/mac/Mixture.zip'
   appcast 'https://s3.amazonaws.com/mixture-mixed-app/mac/appcast.xml'
+  name 'Mixture'
   homepage 'http://mixture.io/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
