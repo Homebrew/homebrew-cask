7dbdfc95e230cf61ad44788c653a229efafec365
Leo Vandriel <leonardvandriel@gmail.com>
2015-01-23 11:07:16 -0800

    updated pusher to 0.6.3

--- Casks/pusher.rb
+++ Casks/pusher.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pusher' do
-  version '0.6.1'
-  sha256 '772b36d99b254805764d33a0d2147a7cf5f7a3ccca87b1e363106a0b533a1ecc'
+  version '0.6.3'
+  sha256 '735ad86c7139058e0ae1d2fb7ad0bc516b4b9284e6e0046a1255c360d58dba3c'
 
   url "https://github.com/noodlewerk/NWPusher/releases/download/#{version}/pusher.app.zip"
   name 'Pusher'
