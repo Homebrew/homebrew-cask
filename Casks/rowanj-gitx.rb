bb95f36a8d093e387b59e2c3b68c04c06d8b787c
Thomas Weber <thomas.weber@myracloud.com>
2015-01-23 20:38:35 +0100

    - Changed from version :latest to current 0.15.1964 - Changed download URL from vendor homepage to GitHub (vendor now uses the GitHub download on his homepage too) - Added sha256 hash - Added legacy versions for Snow Leopard and Lion (latest version needs at least Mountain Lion) - Added name GitX-dev (used by vendor on his homepage) - Changed license from generic :oss to more precise :gpl - Added depends_on >= :snow_leopard (earliest supported OS version) - Added depends_on :intel - Added conflicts_with cask 'gitx' (rowanj-gitx is a fork using the same app name and binary filename)

--- Casks/rowanj-gitx.rb
+++ Casks/rowanj-gitx.rb
@@ -1,13 +1,28 @@
 cask :v1 => 'rowanj-gitx' do
-  version :latest
-  sha256 :no_check
+  if MacOS.release == :snow_leopard
+    version '0.14.81'
+    url 'https://github.com/rowanj/gitx/releases/download/builds%2F0.14%2F81/GitX-dev-81.dmg'
+    sha256 '17301ee1209bd4b12c126d7a58405efdb366f99f70b4f923a9c96e16c334ce2a'
+  elsif MacOS.release == :lion
+    version '0.15.1949'
+    url 'https://github.com/rowanj/gitx/releases/download/builds%2F0.15%2F1949/GitX-dev-1949.dmg'
+    sha256 '17301ee1209bd4b12c126d7a58405efdb366f99f70b4f923a9c96e16c334ce2a'
+  else
+    version '0.15.1964'
+    url 'https://github.com/rowanj/gitx/releases/download/builds%2F0.15%2F1964/GitX-dev-1964.dmg'
+    sha256 'd88bcb7f92ca1cdf31cb3f1d2e24c03e2091ab330319aeef2e770c0dbd6f7817'
+  end
 
-  # phere.net is the official download host per the vendor homepage
-  url 'http://builds.phere.net/GitX/development/GitX-dev.dmg'
-  appcast 'https://s3.amazonaws.com/builds.phere.net/GitX/development/GitX-dev.xml'
+  name 'GitX'
+  name 'GitX-dev'
   homepage 'http://rowanj.github.io/gitx/'
-  license :oss
+  appcast 'https://s3.amazonaws.com/builds.phere.net/GitX/development/GitX-dev.xml'
+  license :gpl
 
   app 'GitX.app'
   binary 'GitX.app/Contents/Resources/gitx'
+
+  depends_on :macos => '>= :snow_leopard'
+  depends_on :arch => :intel
+  conflicts_with :cask => 'gitx'
 end
