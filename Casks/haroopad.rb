7473b8b92f4f8985b8bb166573bfb2875c0bcd1f
Juan Ripoll <jripoll@iprocuratio.com>
2015-01-23 18:01:43 +0100

    Upgrade Haroopad.app to v0.13.0
    Upgrade version from v0.12.2 to v0.13.0 and updated source url (app
    name capitalized and added 'x64' label).

--- Casks/haroopad.rb
+++ Casks/haroopad.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'haroopad' do
-  version '0.12.2'
-  sha256 'f8fe84ec70090c7517b4ef9f8c2896a4dedb9774d0a87800fdd98f706a64929b'
+  version '0.13.0'
+  sha256 '79a003c4db33b393ff5e47e493e54cd27ceb2a94141a116cf58373c1882a798c'
 
   # bitbucket.org is the official download host per the vendor homepage
-  url "https://bitbucket.org/rhiokim/haroopad-download/downloads/haroopad-v#{version}.dmg"
+  url "https://bitbucket.org/rhiokim/haroopad-download/downloads/Haroopad-v#{version}-x64.dmg"
   homepage 'http://pad.haroopress.com/'
   license :oss
 
