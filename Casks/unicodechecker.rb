e63e281cc60384e9db04452a51c85ffd03ad3cf6
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-23 17:48:38 -0800

    Update unicodechecker license from unknown to gratis
    http://earthlingsoft.net/news/unicodechecker-1.15.html

--- Casks/unicodechecker.rb
+++ Casks/unicodechecker.rb
@@ -4,7 +4,7 @@ cask :v1 => 'unicodechecker' do
 
   url 'http://earthlingsoft.net/UnicodeChecker/UnicodeChecker.dmg'
   homepage 'http://earthlingsoft.net/UnicodeChecker/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'UnicodeChecker.app'
 end
