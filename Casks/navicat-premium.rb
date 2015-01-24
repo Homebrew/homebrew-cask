931a023cf23886dc076d4145a68d033976d45ca0
Vítor Galvão <info@vitorgalvao.com>
2015-01-24 01:54:45 +0000

    updated navicat-premium (11.1.7)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'navicat-premium' do
   version '11.1.7'
-  sha256 'c15bf23197136ae3b1c9925dd1cc8beda7c77e7d9d44b6951fc116f38a11b420'
+  sha256 'c5e94e3609d802c1fa5ef1807b42a28b131fa34789a712399a0bb50f78e79822'
 
   url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_premium_en.dmg"
   name 'Navicat Premium'
