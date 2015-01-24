3eb1eb0a893316035de92ead312972fefb25dac1
henrique matias <hems.inlet@gmail.com>
2015-01-24 05:56:05 +0000

    updating soulseek cask to latest version

--- Casks/soulseek.rb
+++ Casks/soulseek.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'soulseek' do
-  version '2014-11-30'
+  version '2014-12-19'
   sha256 '64ddff73a6744a316f1159e7b6267a2837c82091d6045265dd6807eaae5e6124'
 
   url "http://www.soulseekqt.net/SoulseekQt/Mac/SoulseekQt-#{version}.dmg"
