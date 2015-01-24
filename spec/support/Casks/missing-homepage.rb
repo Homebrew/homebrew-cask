adc695a0357bf28f88c850f266825fcf41b6ecaf
Radek Simko <radek.simko@gmail.com>
2015-01-24 21:12:13 +0000

    Test casks decoupled

---
+++ spec/support/Casks/missing-homepage.rb
@@ -0,0 +1,4 @@
+cask :v1test => 'missing-homepage' do
+  url 'http://localhost/something.dmg'
+  version '1.2.3'
+end
---
+++ spec/support/Casks/missing-url.rb
@@ -0,0 +1,4 @@
+cask :v1test => 'missing-url' do
+  version '1.2.3'
+  homepage 'http://example.com'
+end
