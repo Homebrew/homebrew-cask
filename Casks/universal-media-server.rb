b76298dca6ba5253aebaf773005fcda2ec200ff4
digiou <digitalbckp@gmail.com>
2015-01-26 10:42:59 +0200

    upgrade UMS to v5.0.0

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'universal-media-server' do
-  version '4.4.0'
-  sha256 'ad73091e9ef5c6d7d884df35d3c2b55534765e56629932790989f6e4e0d78b06'
+  version '5.0.0'
+  sha256 '415d44f453db2c6d0a253ced2ef1897606a0004852c68443c117da1b7bfd5b32'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/unimediaserver/Official%20Releases/OS%20X/UMS-#{version}-Java8.dmg"
