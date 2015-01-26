9219b81c872357a0b7e627d60cfa2fc65e4cbdc8
Roger Thiede <RogerThiede@gmail.com>
2015-01-26 00:53:11 -0800

    Updated makemkv to 1.9.1

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,10 +1,15 @@
 cask :v1 => 'makemkv' do
-  version '1.9.0'
-  sha256 'd7889b105ad7ef29c69a255279b129a8555c0101875e353431b6eb50b48dc222'
+  version '1.9.1'
+  sha256 '15f476d52fa60c878469487dd654bbc1474d86490439d5e6327a8d39749a28de'
 
   url "http://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   homepage 'http://www.makemkv.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
 
   app 'MakeMKV.app'
+
+  zap :delete => [
+                 '~/Library/Preferences/com.makemkv.MakeMKV.plist',
+                 '~/Library/Saved Application State/com.makemkv.MakeMKV.savedState',
+                 ]
 end
