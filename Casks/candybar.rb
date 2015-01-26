6b8da4c3081fc063e6cb21e08abb09a100a05fb4
Vítor Galvão <info@vitorgalvao.com>
2015-01-26 17:13:24 +0000

    candybar.rb: added free_license caveat

--- Casks/candybar.rb
+++ Casks/candybar.rb
@@ -1,4 +1,4 @@
-cask :v1 => 'candybar' do
+cask :v1_1 => 'candybar' do
   version '3.3.4'
   sha256 'f305596f195445016b35c9d99a40789c6671195e9cbad0b6e92e808b6c633ad6'
 
@@ -10,9 +10,6 @@ cask :v1 => 'candybar' do
   app 'CandyBar.app'
   caveats do
     discontinued
-
-    'Candybar is free of charge.  Visit the following link for a license
-
-      http://panic.com/bin/setup.php/cb3/PPQA-YAMA-E3KP-VHXG-B6AL-L'
+    free_license 'http://panic.com/bin/setup.php/cb3/PPQA-YAMA-E3KP-VHXG-B6AL-L'
   end
 end
