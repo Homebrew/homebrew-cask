240e36d23fb449bac48925bf3067e5e6536ef442
zchee <zchee@zsh.us>
2015-01-28 05:00:14 +0900

    Fix checksum in Vivaldi Cask

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'vivaldi' do
   version '1.0.83.38'
-  sha256 'ef048f271526c53574f453dfdcac157482fc51e9f669c22aadaf1b25b5f952fa'
+  sha256 '84153229b9d59561c9d6f570aa77875ed8b410ff43d2b4298167a0dc300b2a59'
 
   url "http://vivaldi.com/download/Vivaldi_TP_#{version}.dmg"
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
-  license :gratis 
+  license :gratis
 
   app 'Vivaldi.app'
 end
