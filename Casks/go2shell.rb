d07cdee985a8f901f070989bc2d2228e226f9922
Thomas Weber <thomas.weber@myracloud.com>
2015-01-28 12:54:40 +0100

    Add Go2Shell.app Latest

---
+++ Casks/go2shell.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'go2shell' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://zipzapmac.com/DMGs/Go2Shell.dmg'
+  name 'Go2Shell'
+  homepage 'http://zipzapmac.com/Go2Shell'
+  license :closed
+
+  app 'Go2Shell.app'
+end
