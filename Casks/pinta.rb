ad991c90aad5aa54612ec49cc294802eba3c8647
Jonathan Dahan <jonathan@jedahan.com>
2015-01-28 14:13:54 -0500

    pinta requires mono-mdk
    Closes #9309.

--- Casks/pinta.rb
+++ Casks/pinta.rb
@@ -3,10 +3,12 @@ cask :v1 => 'pinta' do
   sha256 'd23093ce67c6aac77c3380aff63963ca615fcf9ef85ee1683655c599070b766e'
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com//PintaProject/Pinta/releases/download/#{version}/Pinta.app.zip"
+  url "https://github.com/PintaProject/Pinta/releases/download/#{version}/Pinta.app.zip"
   name 'Pinta'
   homepage 'http://pinta-project.com/'
   license :oss
 
   app 'Pinta.app'
+
+  depends_on :cask => 'mono-mdk'
 end
