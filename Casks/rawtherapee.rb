88262d6b241f4af34c0eb067c0d563d1d19f9a2a
ndr <ndr@qef.io>
2015-01-29 07:17:54 +0000

    rawtherapee: add Yosemite version
    and OS/arch dependencies; also specify license.

--- Casks/rawtherapee.rb
+++ Casks/rawtherapee.rb
@@ -1,11 +1,21 @@
 cask :v1 => 'rawtherapee' do
   version '4.2.21'
-  sha256 '4daddff5fbb6cc8c3cca80dd1c25bf2adb42ff8068e6d79db040690113a22539'
 
-  url "http://rawtherapee.com/releases_head/mac/RawTherapee_OSX_10.6_64_#{version}.zip"
+  if MacOS.release <= :mavericks
+    sha256 '4daddff5fbb6cc8c3cca80dd1c25bf2adb42ff8068e6d79db040690113a22539'
+    url "http://rawtherapee.com/releases_head/mac/RawTherapee_OSX_10.6_64_#{version}.zip"
+  else
+    sha256 'b7c2a0614c849cdfb206b06eb967d6ceefc674a8e6bade1c1db7bac2f096bd13'
+    url "http://rawtherapee.com/releases_head/mac/RawTherapee_OSX_10.10_64_#{version}.zip"
+  end
+
+  name 'RawTherapee'
   homepage 'http://rawtherapee.com'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gpl
 
   container :nested => "RawTherapee_OSX_10.6_64_#{version}.dmg"
   app 'RawTherapee.app'
+
+  depends_on :macos => '>= :snow_leopard',
+             :arch => :x86_64
 end
