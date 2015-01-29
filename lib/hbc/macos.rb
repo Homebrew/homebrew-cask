cbcc293ee3b01d1277f0d2f9eb36952cc26fb67a
Josh Hagins <hagins.josh@gmail.com>
2015-01-29 14:34:04 -0500

    Move `coerce_os_release` method to MacOS
    This method will be used by both `depends_on` and `conflicts_with`, so
    it should be outsourced to a shared module.

--- lib/hbc/macos.rb
+++ lib/hbc/macos.rb
@@ -15,4 +15,24 @@ module Hbc::MacOS
   def release
     @@release ||= Release.new(ENV.fetch('MACOS_RELEASE', release_with_patchlevel.to_s[/10\.\d+/]))
   end
+
+  def coerce_os_release(arg)
+
+    @osx_symbols ||= Release::SYMBOLS
+    @inverted_osx_symbols ||= @osx_symbols.invert
+
+    begin
+      if arg.is_a?(Symbol)
+        Gem::Version.new(@osx_symbols.fetch(arg))
+      elsif arg =~ %r{^\s*:?([a-z]\S+)\s*$}i
+        Gem::Version.new(@osx_symbols.fetch($1.downcase.to_sym))
+      elsif @inverted_osx_symbols.has_key?(arg)
+        Gem::Version.new(arg)
+      else
+        raise
+      end
+    rescue StandardError
+      raise "invalid MacOS value: #{arg.inspect}"
+    end
+  end
 end
