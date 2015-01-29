73f9348b8fa378eeb2eea3b1a14432b21ebc20bb
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 20:33:26 +0000

    insync.rb: added name

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -3,6 +3,7 @@ cask :v1 => 'insync' do
   sha256 '782c66bab03e1edf9235b88aed68680e63116242e696c9d1016800dc9ffbe92c'
 
   url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
+  name 'Insync'
   homepage 'https://insynchq.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
