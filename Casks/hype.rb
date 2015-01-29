868a9160fd28f1314ebe4a0a7faae82727b95928
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 20:31:14 +0000

    hype.rb: added name

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -3,8 +3,10 @@ cask :v1 => 'hype' do
   sha256 :no_check
 
   url 'http://tumult.com/hype/download/Hype.zip'
+  name 'Hype'
   homepage 'http://tumult.com/hype/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  tags :vendor => 'Tumult'
 
   app 'Hype2.app'
 end
