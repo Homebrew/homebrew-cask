593042a57b6717bc0c3faf7589d719f373333330
prime8 <prime8@users.noreply.github.com>
2015-01-28 19:30:10 -0500

    Update org.nodejs pkg to 0.10.36

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'node' do
-  version '0.10.35'
-  sha256 '5fc8d4f7137d0ee6e23cb6368dd2d219f389153b7a9898f26f7e35aee19c1dcf'
+  version '0.10.36'
+  sha256 'f6702b77c7b2f269834acab2210fc5bf43bc20467652ddefb55ccec61c58193a'
 
   url "http://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   homepage 'http://nodejs.org'
