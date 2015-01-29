d8ce2cc9a6cd6238f652554475211432f3617031
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 20:47:47 +0000

    suspicious-package.rb: added name

--- Casks/suspicious-package.rb
+++ Casks/suspicious-package.rb
@@ -3,6 +3,7 @@ cask :v1 => 'suspicious-package' do
   sha256 :no_check
 
   url 'http://www.mothersruin.com/software/downloads/SuspiciousPackage.pkg'
+  name 'Suspicious Package'
   homepage 'http://www.mothersruin.com/software/SuspiciousPackage/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
