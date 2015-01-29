42f77109f336a5b8ea1aedf90b6acdce0a73241a
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 19:16:42 +0000

    emin-webpquicklook.rb: added name

--- Casks/emin-webpquicklook.rb
+++ Casks/emin-webpquicklook.rb
@@ -3,6 +3,7 @@ cask :v1 => 'emin-webpquicklook' do
   sha256 :no_check
 
   url 'https://raw.github.com/emin/WebPQuickLook/master/WebpQuickLook.tar.gz'
+  name 'WebPQuickLook'
   homepage 'https://github.com/emin/WebPQuickLook'
   license :oss
 
