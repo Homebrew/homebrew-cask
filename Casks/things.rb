c16c1ec3c525128370dc755f11b589333ccaeafe
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 19:37:19 +0000

    things.rb: added name

--- Casks/things.rb
+++ Casks/things.rb
@@ -4,6 +4,7 @@ cask :v1 => 'things' do
 
   url 'https://culturedcode.com/things/download/'
   appcast 'http://downloads.culturedcode.com/things/download/Things_Updates.php'
+  name 'Things'
   homepage 'http://culturedcode.com/things/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
