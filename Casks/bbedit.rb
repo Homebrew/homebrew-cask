743e09d62b97275b06afe174586e60c1fa767703
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 19:33:55 +0000

    bbedit.rb: added name

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -5,6 +5,7 @@ cask :v1 => 'bbedit' do
   url "http://pine.barebones.com/files/BBEdit_#{version}.dmg"
   appcast 'https://versioncheck.barebones.com/BBEdit.xml',
           :sha256 => '56c7ab1c28592ff27d42cfe28c188b79c6fa81ae9a9639dcc82b1a83174f451e'
+  name 'BBEdit'
   homepage 'http://www.barebones.com/products/bbedit/'
   license :commercial
 
