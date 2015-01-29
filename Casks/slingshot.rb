38dc83f3ea550ffe5853d364cdec4300e136ec8b
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 19:19:58 +0000

    slingshot.rb: added name

--- Casks/slingshot.rb
+++ Casks/slingshot.rb
@@ -4,6 +4,7 @@ cask :v1 => 'slingshot' do
 
   url 'http://download.airsquirrels.com/Slingshot/Mac/Slingshot.dmg'
   appcast 'https://updates.airsquirrels.com/Slingshot/Mac/Slingshot.xml'
+  name 'Slingshot'
   homepage 'http://www.airsquirrels.com/slingshot/'
   license :commercial
 
