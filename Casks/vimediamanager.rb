292d8fa2f2ce8602181612b76e52791aef99d648
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 20:44:49 +0000

    vimediamanager.rb: added name

--- Casks/vimediamanager.rb
+++ Casks/vimediamanager.rb
@@ -3,7 +3,8 @@ cask :v1 => 'vimediamanager' do
   sha256 '3642a38fdc4d995943e42bad538eb117ab225fb564c05701850fe01d9c99a035'
 
   url "http://mariusth.channelwood.org/vimediamanager/files/vimediamanager_v#{version}.dmg"
-  homepage 'http://mariusth.channelwood.org/vimediamanager/'
+  name 'ViMediaManager'
+  homepage 'http://mariusth.heliohost.org/vimediamanager/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'ViMediaManager.app'
