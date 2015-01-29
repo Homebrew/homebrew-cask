6ba4929a03aac7efdc6908ad0736a71db6413e8d
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 19:18:50 +0000

    jdownloader.rb: added name

--- Casks/jdownloader.rb
+++ Casks/jdownloader.rb
@@ -3,6 +3,7 @@ cask :v1 => 'jdownloader' do
   sha256 :no_check
 
   url 'http://installer.jdownloader.org/JDownloader09Setup_mac.dmg'
+  name 'JDownloader'
   homepage 'http://jdownloader.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
