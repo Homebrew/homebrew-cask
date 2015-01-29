e42dad695ec92ba1f7fd3c5ec85f470f64acab85
ndr <ndr@qef.io>
2015-01-29 09:15:00 +0000

    silverlight: set sha256 to :no_check

--- Casks/silverlight.rb
+++ Casks/silverlight.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'silverlight' do
   version '5.1.30514.0'
-  sha256 '469a7b19cf4a87a1248caa850c7de61c88e67c0c5511fe77032817cd79df9dea'
+  sha256 :no_check    # required as upstream package is updated in-place
 
   url 'http://silverlight.dlservice.microsoft.com/download/F/8/C/F8C0EACB-92D0-4722-9B18-965DD2A681E9/30514.00/Silverlight.dmg'
   homepage 'http://www.microsoft.com/silverlight/'
