d92c622ea192eac9e1051da8f7a16e5132d918ea
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 20:40:11 +0000

    cocoasplit.rb: added name

--- Casks/cocoasplit.rb
+++ Casks/cocoasplit.rb
@@ -3,6 +3,7 @@ cask :v1 => 'cocoasplit' do
   sha256 :no_check
 
   url 'http://krylon.rsdio.com/zakk/cocoasplit/CocoaSplit-current.zip'
+  name 'CocoaSplit'
   homepage 'http://krylon.rsdio.com/zakk/cocoasplit/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
