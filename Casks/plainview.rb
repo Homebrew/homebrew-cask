6bad01f66753f2efcaa30ba2cd43a776938fa739
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 20:31:51 +0000

    plainview.rb: added name

--- Casks/plainview.rb
+++ Casks/plainview.rb
@@ -4,6 +4,7 @@ cask :v1 => 'plainview' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/plainviewapp/plainview_#{version}.zip"
+  name 'Plainview'
   homepage 'http://barbariangroup.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
