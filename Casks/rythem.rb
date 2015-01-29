b69f8cb8e12d5c2ba18f7d2f0b51062e23d5faa5
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 19:32:11 +0000

    rythem.rb: added name

--- Casks/rythem.rb
+++ Casks/rythem.rb
@@ -3,6 +3,7 @@ cask :v1 => 'rythem' do
   sha256 'd4c5c578fb0f4d155cc07af22184f88a9b190a7453d0dc45962fd179f72da34d'
 
   url "https://github.com/AlloyTeam/Rythem/releases/download/filter/Rythem-#{version}.dmg"
+  name 'Rythem'
   homepage 'https://github.com/AlloyTeam/Rythem'
   license :oss
 
