616f369ae3086bc9840a83711decea0c9f9c6902
Vítor Galvão <info@vitorgalvao.com>
2015-01-29 19:37:10 +0000

    flash-player-debugger.rb: added name

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -4,8 +4,10 @@ cask :v1 => 'flash-player-debugger' do
 
   # macromedia.com is the official download host per the vendor homepage
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.to_i}/flashplayer_#{version.to_i}_sa_debug.dmg"
+  name 'Adobe Flash Player Debugger'
   homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
+  tags :vendor => 'Adobe'
 
   # Renamed to avoid conflict with flash-player.
   app 'Flash Player.app', :target => 'Flash Player Debugger.app'
