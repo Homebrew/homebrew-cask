43b5206ecfa2b7730b69992f56381302e2e955fd
Vítor Galvão <info@vitorgalvao.com>
2015-01-30 14:30:49 +0000

    sony-ericsson-bridge: fix license

--- Casks/sony-ericsson-bridge.rb
+++ Casks/sony-ericsson-bridge.rb
@@ -5,7 +5,7 @@ cask :v1 => 'sony-ericsson-bridge' do
   url 'http://www-support-downloads.sonymobile.com/Software%20Downloads/Bridge%20for%20Mac/SonyBridgeForMac_web.dmg'
   name 'Sony Ericsson Bridge for Mac'
   homepage 'http://support.sonymobile.com/au/tools/bridge-for-mac/'
-  license :commercial
+  license :gratis
 
   app 'Sony Ericsson Bridge for Mac.app'
 end
