5a42788934dfa9384ee64914021cadb38da3364f
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-01-30 22:24:03 +0000

    Blender 2.73a
    Upgrade blender to 2..73a bugfix release

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'blender' do
-  version '2.73'
-  sha256 'b95fc8caaf988a355242bd73bab44f7a95933826e49268c48bf46b9a672fa911'
+  version '2.73a'
+  sha256 '9c68aad9f1cb907d261d7577e3966079691ab6f8eb0da924e7bcc9b9d3229fa5'
 
-  url "http://download.blender.org/release/Blender#{version}/blender-#{version}-OSX_10.6-x86_64.zip"
+  url "http://download.blender.org/release/Blender#{version.to_f}/blender-#{version}-OSX_10.6-x86_64.zip"
   name 'Blender'
   homepage 'http://www.blender.org/'
   license :gpl
