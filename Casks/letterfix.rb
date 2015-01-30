7130e760d5bc8f262a7e17df457fd16ee9b8839a
WATANABE Yutaka <wtnb_ytk@icloud.com>
2015-01-30 10:54:42 +0900

    update letter-fix to 2.3.2

--- Casks/letterfix.rb
+++ Casks/letterfix.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'letterfix' do
-  version '2.3.1'
-  sha256 'c8edc894938dbfe1d55c628b040bcce0a739b55b339d7e8557f8c324ae8d4a95'
+  version '2.3.2'
+  sha256 '0830e868adc90e71ddfb42ce3f4a2cd5592b0bb5e9df0caf3d45213e083cc9c0'
 
-  url "http://dl.sourceforge.jp/letter-fix/62248/LetterFix-#{version}.dmg"
+  url "http://dl.sourceforge.jp/letter-fix/62451/LetterFix-#{version}a.dmg"
   homepage 'http://sourceforge.jp/projects/letter-fix/'
   license :oss
 
