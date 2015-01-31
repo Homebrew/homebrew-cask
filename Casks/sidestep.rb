2b341e83fef5c225f9408f1b3744f13ed74b7d5a
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-31 10:24:33 -0800

    Update sidestep license from oss to mit
    https://github.com/chetan51/sidestep/blob/master/LICENSE

--- Casks/sidestep.rb
+++ Casks/sidestep.rb
@@ -7,7 +7,7 @@ cask :v1 => 'sidestep' do
   appcast 'http://chetansurpur.com/projects/sidestep/appcast.xml',
           :sha256 => '1d043f565c44c03f6ab5e7c90f025aba3cf480f1623a8a530f87206fab12f86a'
   homepage 'http://chetansurpur.com/projects/sidestep/'
-  license :oss
+  license :mit
 
   app 'Sidestep.app'
 end
