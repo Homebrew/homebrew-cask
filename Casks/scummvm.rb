1bdd74948b72b336346696ed0a223f11d2701f48
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-31 10:24:22 -0800

    Update scummvm license from oss to gpl
    http://sourceforge.net/projects/scummvm/ and https://github.com/scummvm/scummvm/blob/v1.7.0/COPYING

--- Casks/scummvm.rb
+++ Casks/scummvm.rb
@@ -7,7 +7,7 @@ cask :v1 => 'scummvm' do
   appcast 'http://www.scummvm.org/appcasts/macosx/release.xml',
           :sha256 => 'bea2fd9739e102c781753c991b6ee6481a1c0c3f00621e96dca06a2b83e42135'
   homepage 'http://scummvm.org/'
-  license :oss
+  license :gpl
 
   app 'ScummVM.app'
 end
