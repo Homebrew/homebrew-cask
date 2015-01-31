287380be54d49c4a9df64a596f7bef182d0df533
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-31 10:25:18 -0800

    Update sweet-home3d license from oss to gpl
    http://www.sweethome3d.com/license.jsp

--- Casks/sweet-home3d.rb
+++ Casks/sweet-home3d.rb
@@ -5,7 +5,7 @@ cask :v1 => 'sweet-home3d' do
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-#{version}/SweetHome3D-#{version}-macosx.dmg"
   homepage 'http://www.sweethome3d.com/'
-  license :oss
+  license :gpl
 
   app 'Sweet Home 3D.app'
 end
