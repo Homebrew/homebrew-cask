70042f2e2a9407a84f2ea9c21164ae7e8e319fc4
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-31 10:24:25 -0800

    Update shotcut license from oss to gpl
    https://github.com/mltframework/shotcut/blob/master/COPYING

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -5,7 +5,7 @@ cask :v1 => 'shotcut' do
   # github.com is the official download host per the vendor homepage
   url "https://github.com/mltframework/shotcut/releases/download/v#{version.sub(/\.\d+$/, '')}/shotcut-osx-x86_64-#{version.gsub('.', '')}.dmg"
   homepage 'http://www.shotcut.org/'
-  license :oss
+  license :gpl
 
   app 'Shotcut.app'
 end
