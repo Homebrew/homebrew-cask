cc33c4f5da2a900a589648a148be75dd1edc0c4a
Den Patin <denpatin@gmail.com>
2015-02-01 14:33:02 +0300

    Added name to WebStorm.app

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -3,6 +3,7 @@ cask :v1 => 'webstorm' do
   sha256 'adc04ee25e7d3821659d2c4dbe1cdd8d0c15622ebcdaa3db2b6e02f79abb34b8'
 
   url "http://download-cf.jetbrains.com/webstorm/WebStorm-#{version}.dmg"
+  name 'WebStorm'
   homepage 'http://www.jetbrains.com/webstorm/'
   license :commercial
 
