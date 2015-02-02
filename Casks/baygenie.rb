af767b86ebf6cdffa5d2468d2b40bc279ccb48d0
Den Patin <denpatin@gmail.com>
2015-02-02 23:34:10 +0300

    Added name to the baygenie.app

--- Casks/baygenie.rb
+++ Casks/baygenie.rb
@@ -3,6 +3,7 @@ cask :v1 => 'baygenie' do
   sha256 :no_check
 
   url 'https://www.baygenie.com/Download/BayGenie4Mac.dmg'
+  name 'BayGenie'
   homepage 'http://www.baygenie.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
