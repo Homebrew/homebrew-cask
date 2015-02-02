ede2bc7c669c37c0916c1ec7d8e4a986e5b71a50
Den Patin <denpatin@gmail.com>
2015-02-02 23:13:08 +0300

    Added name to the aliwangwang.app

--- Casks/aliwangwang.rb
+++ Casks/aliwangwang.rb
@@ -4,6 +4,7 @@ cask :v1 => 'aliwangwang' do
 
   # etao.com is the official download host per the vendor homepage
   url 'http://update.labs.etao.com/macww-nosandbox/versions/lastest'
+  name 'Ali Wangwang'
   appcast 'http://update.labs.etao.com/macww/updates.xml'
   homepage 'http://wangwang.taobao.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
