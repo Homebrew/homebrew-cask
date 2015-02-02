9cdd8f34f7182934d3cbbec4957e74b14e7f4963
Amory Meltzer <Amorymeltzer@gmail.com>
2015-02-02 09:31:16 -0800

    Add note about vendor/official homepage

--- Casks/mac2imgur.rb
+++ Casks/mac2imgur.rb
@@ -2,6 +2,7 @@ cask :v1 => 'mac2imgur' do
   version :latest
   sha256 :no_check
 
+  # mileswd.com is the official download host per the vendor homepage
   url 'https://mac2imgur.mileswd.com/static/mac2imgur.zip'
   name 'mac2imgur'
   homepage 'https://github.com/mileswd/mac2imgur'
