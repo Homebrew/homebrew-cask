b7046c6f54964eae50d84ceba24b31bee7fdf704
Den Patin <denpatin@gmail.com>
2015-02-02 23:34:37 +0300

    Added name to the beamer.app

--- Casks/beamer.rb
+++ Casks/beamer.rb
@@ -3,6 +3,7 @@ cask :v1 => 'beamer' do
   sha256 :no_check
 
   url 'http://beamer-app.com/download'
+  name 'Beamer'
   appcast 'http://beamer-app.com/beamer2-appcast.xml'
   homepage 'http://beamer-app.com'
   license :commercial
