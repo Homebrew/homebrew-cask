ef890eb3a36101b890aa472c32b16cfc50b4bde6
Hoffmann József <illusionfield@gmail.com>
2015-02-02 08:07:44 +0100

    Update Koala.app url

--- Casks/koala.rb
+++ Casks/koala.rb
@@ -2,7 +2,7 @@ cask :v1 => 'koala' do
   version '2.0.4'
   sha256 '8b0d3564db80bad52cbad1b0755a5ff55f6d1549ea8f55e4a309552261a61327'
 
-  url "https://content-na.drive.amazonaws.com/cdproxy/templink/1NXZ32XO4l8CspGTBhtc_PcOCeXPMTHHzsCCcXbR3pAE0Xnc3?download=true"
+  url "http://fieldgroup.net/cask/koala_#{version}_mac.zip"
   name 'Koala'
   homepage 'http://koala-app.com'
   license :apache
