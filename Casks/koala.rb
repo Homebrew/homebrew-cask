87ddcc431d23900400cc22c46c2f9fb09d4456ab
Hoffmann József <illusionfield@gmail.com>
2015-02-02 05:19:51 +0100

    Update Koala.app sha256

--- Casks/koala.rb
+++ Casks/koala.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'koala' do
   version '2.0.4'
-  sha256 :no_check
+  sha256 '8b0d3564db80bad52cbad1b0755a5ff55f6d1549ea8f55e4a309552261a61327'
 
   url "https://content-na.drive.amazonaws.com/cdproxy/templink/1NXZ32XO4l8CspGTBhtc_PcOCeXPMTHHzsCCcXbR3pAE0Xnc3?download=true"
   name 'Koala'
@@ -9,4 +9,3 @@ cask :v1 => 'koala' do
 
   app 'Koala.app'
 end
-
