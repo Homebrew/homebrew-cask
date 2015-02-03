c65e2e62a1e02c807e1157fece47c6a8a448d4c5
Amory Meltzer <Amorymeltzer@gmail.com>
2015-02-03 15:56:33 -0800

    segmentant - use version.gsub in url

--- Casks/segmentant.rb
+++ Casks/segmentant.rb
@@ -2,10 +2,10 @@ cask :v1 => 'segmentant' do
   version '1.0.0'
   sha256 '547a31a50389a342a6e22c4dea3828dfb3ef4d01dffbfb83fb75483b514c16d4'
 
-  url 'http://www.laurenceanthony.net/software/segmentant/releases/SegmentAnt100/SegmentAnt.zip'
+  url "http://www.laurenceanthony.net/software/segmentant/releases/SegmentAnt#{version.gsub('.','')}/SegmentAnt.zip"
   name 'SegmentAnt'
   homepage 'http://www.laurenceanthony.net/software/segmentant'
-  license :gratis 
+  license :gratis
 
   app 'SegmentAnt.app'
 end
