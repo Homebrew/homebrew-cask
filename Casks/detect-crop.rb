3f4ee3aae4c500c8b1770d6eabb2004ca3dfcc29
Vítor Galvão <info@vitorgalvao.com>
2015-02-03 02:27:51 +0000

    updated detect-crop (:latest)

--- Casks/detect-crop.rb
+++ Casks/detect-crop.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'detect-crop' do
-  version '5.8'
-  sha256 '31e068d4c8589473d6a3c13afc7062cafa6e8a80ea1821e2974590d9aaed8de5'
+  version :latest
+  sha256 :no_check
 
   url 'https://github.com/donmelton/video-transcoding-scripts/archive/master.zip'
   name 'Video Transcoding Scripts'
