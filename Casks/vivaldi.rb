5710cbe77ecc11f72c4c325ef7600ba6026c43fa
Ryan Duryea <aguynamedryan@gmail.com>
2015-02-02 21:03:03 -0800

    vivaldi sha256 to no_check
    As per my earlier suggestion to avoid more issues
    like #9292 and #9308, set sha256 to no_check

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'vivaldi' do
   version '1.0.83.38'
-  sha256 '1cffe405d04a477e3c8d3901259e855c782920569bd0ff59cc1cb98a46706698'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "http://vivaldi.com/download/Vivaldi_TP_#{version}.dmg"
   name 'Vivaldi'
