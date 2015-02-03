45f276e39591cec1d4568a0bbe9184f8d6a5b751
Amory Meltzer <Amorymeltzer@gmail.com>
2015-02-03 15:55:45 -0800

    antwordprofiler - use version.gsub in url

--- Casks/antwordprofiler.rb
+++ Casks/antwordprofiler.rb
@@ -2,10 +2,10 @@ cask :v1 => 'antwordprofiler' do
   version '1.4.1'
   sha256 'b7cea938e38001b54fce28d1efb1336bbfe4f5af37559c7b3ffd3a14a8aed608'
 
-  url 'http://www.laurenceanthony.net/software/antwordprofiler/releases/AntWordProfiler141/AntWordProfiler.zip'
+  url "http://www.laurenceanthony.net/software/antwordprofiler/releases/AntWordProfiler#{version.gsub('.','')}/AntWordProfiler.zip"
   name 'AntWordProfiler'
   homepage 'http://www.laurenceanthony.net/software/antwordprofiler'
-  license :gratis 
+  license :gratis
 
   app 'AntWordProfiler.app'
 end
