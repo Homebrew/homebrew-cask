124598b554da9984f9d6b123c72015539bf2d85c
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2015-02-04 15:27:50 +0100

    Bump to 0.6.0

--- Casks/vimr.rb
+++ Casks/vimr.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'vimr' do
-  version '0.5.2-26'
-  sha256 'df708cbb468c9b27fda61134f965b907131b91aeea26df7d3c546cdcc922abe7'
+  version '0.6.0-27'
+  sha256 '0a885d0b96c4b8b4761e238c21ff4522c27273b694627b6df07e862aafc1b5df'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/qvacua/vimr/releases/download/v#{version}/VimR-#{version.sub(%r{-.*},'')}.tar.bz2"
