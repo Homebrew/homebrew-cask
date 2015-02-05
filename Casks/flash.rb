7f2318609ca0c786a80fdcbf58866b20973a598f
Josh Butts <josh@joshbutts.com>
2015-02-05 10:14:58 -0600

    Update SHA has for flash

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'flash' do
   version '16.0.0.296'
-  sha256 '0c9715a9eb70b2e6fb324da675c4cadfc7a9845dfc8ecc0b5745ac7a80de0509'
+  sha256 '56dbb982a2d48fb281dc69287c476590deeae7eb13fee2b5258b7d7dcfabdd23'
 
   # macromedia.com is the official download host per the vendor homepage
   url "http://fpdownload.macromedia.com/get/flashplayer/current/licensing/mac/install_flash_player_#{version.to_i}_osx_pkg.dmg"
