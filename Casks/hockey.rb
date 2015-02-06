766b780d76adcdc1ce1774270804541f96966949
Vincent Klaiber <vincentklaiber@gmail.com>
2015-02-06 09:27:12 +0100

    Update HockeyApp.app license to :oss

--- Casks/hockey.rb
+++ Casks/hockey.rb
@@ -6,7 +6,7 @@ cask :v1 => 'hockey' do
   appcast 'https://rink.hockeyapp.net/api/2/apps/67503a7926431872c4b6c1549f5bd6b1'
   name 'HockeyApp'
   homepage 'http://hockeyapp.net/releases/mac/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :oss
 
   app 'HockeyApp.app'
   binary 'HockeyApp.app/Contents/Resources/puck'
