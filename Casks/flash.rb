d2853b666fa747555c8f9d151f981bd433f3e5e2
Sven Ertel <sven@ertel-net.de>
2015-02-06 14:38:28 +0100

    Upgrade flash to 16.0.0.305

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'flash' do
-  version '16.0.0.296'
+  version '16.0.0.305'
   sha256 '56dbb982a2d48fb281dc69287c476590deeae7eb13fee2b5258b7d7dcfabdd23'
 
   # macromedia.com is the official download host per the vendor homepage
