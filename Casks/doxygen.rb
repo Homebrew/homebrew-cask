e2123a19b40629599f5099268cb0785d6cbcd70a
Guyzmo <guyzmo+github@m0g.net>
2015-02-06 17:59:20 +0000

    updated doxygen cask
     * changed license to gpl from unknown
     * updated to newer version
    
    Signed-off-by: Guyzmo <guyzmo+github@m0g.net>

--- Casks/doxygen.rb
+++ Casks/doxygen.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'doxygen' do
-  version '1.8.7'
-  sha256 'd4d23df0372362358d26ab3c3c13d701b1f1751a7b80d427742665739a4f49c0'
+  version '1.8.9.1'
+  sha256 '6d50c7374b6897aabf41a96aee21bcd993d3258e46f7954408b5a1bd436735eb'
 
   url "ftp://ftp.stack.nl/pub/users/dimitri/Doxygen-#{version}.dmg"
   name 'Doxygen'
   homepage 'http://www.stack.nl/~dimitri/doxygen/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gpl
 
   app 'Doxygen.app'
 end
