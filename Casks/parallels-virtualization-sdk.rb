82665fa56ce35435e7ec340a99c2316c7c32d637
Den Patin <denpatin@gmail.com>
2015-02-06 20:36:01 +0300

    Fixed dl-link for Parallels Virtualization SDK.app

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -2,7 +2,7 @@ cask :v1 => 'parallels-virtualization-sdk' do
   version '10.1.3-28868'
   sha256 '085c34bd4e292ed718ae5c7c428e111c715480142bd15c42540420888b1df4ca'
 
-  url "http://download.parallels.com/desktop/v#{version[/^\w+/]}/#{version.sub(/-.*$/, '')}/ParallelsVirtualizationSDK-#{version}.dmg"
+  url "http://download.parallels.com/desktop/v#{version[/^\w+/]}/#{version.sub(/-.*$/, '')}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name 'Parallels Virtualization SDK'
   homepage 'http://www.parallels.com/products/desktop/download/'
   license :gratis
