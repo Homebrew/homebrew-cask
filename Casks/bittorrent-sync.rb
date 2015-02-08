6872d5cbf5a3a81fb24cd07be250900b68fb7c53
Den Patin <denpatin@gmail.com>
2015-02-08 13:59:26 +0300

    Added name to the bittorrent-sync.rb cask

--- Casks/bittorrent-sync.rb
+++ Casks/bittorrent-sync.rb
@@ -4,6 +4,7 @@ cask :v1 => 'bittorrent-sync' do
 
   # getsyncapp.com is the official download host per the vendor homepage
   url 'http://download.getsyncapp.com/endpoint/btsync/os/osx/track/stable'
+  name 'BitTorrent Sync'
   # todo: response was not XML
   # appcast 'http://www.usyncapp.com/cfu.php'
   homepage 'http://www.bittorrent.com/sync'
