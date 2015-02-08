f9625e08989310e46b7a5f07bda99c9d6e4a3877
Den Patin <denpatin@gmail.com>
2015-02-08 14:28:15 +0300

    Added name to the ExpanDrive.app

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -3,6 +3,7 @@ cask :v1 => 'expandrive' do
   sha256 :no_check
 
   url 'http://updates.expandrive.com/apps/expandrive/download_latest'
+  name 'ExpanDrive'
   appcast 'http://updates.expandrive.com/appcast/expandrive.xml?version=3'
   homepage 'http://www.expandrive.com/expandrive'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
