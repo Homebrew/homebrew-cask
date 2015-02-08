502165306425f4a73f2d18cd2f2aefeb778f552f
Den Patin <denpatin@gmail.com>
2015-02-08 13:50:31 +0300

    Added name to the beatport-pro.rb cask

--- Casks/beatport-pro.rb
+++ Casks/beatport-pro.rb
@@ -3,6 +3,8 @@ cask :v1 => 'beatport-pro' do
   sha256 '62526d08723e5a54953473a2c1530e5b298ab2a50a491dd2a846bf7c0b9499cb'
 
   url "http://pro.beatport.com/mac/#{version}/beatportpro_#{version}.dmg"
+  name 'Beatport'
+  name 'Beatport Pro'
   homepage 'http://pro.beatport.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
