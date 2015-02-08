2ad7de3d2c0c67a269516fbeeecbfc5c9e9f953c
Den Patin <denpatin@gmail.com>
2015-02-08 14:01:06 +0300

    Added name to the bob.rb cask

--- Casks/bob.rb
+++ Casks/bob.rb
@@ -3,6 +3,7 @@ cask :v1 => 'bob' do
   sha256 'e73e5a5a2a4750cfa62f405846d87abc6fc32dba6df4b1bf74f910cee2cf8e87'
 
   url "https://github.com/casperstorm/Bob/releases/download/#{version}/Bob.zip"
+  name 'Bob'
   homepage 'https://github.com/casperstorm/Bob'
   license :mit
 
