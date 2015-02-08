1e67e632aa9a7db8c6d000f03315ef4de28fa902
Den Patin <denpatin@gmail.com>
2015-02-08 14:28:24 +0300

    Added name to the EyeTV.app

--- Casks/eyetv.rb
+++ Casks/eyetv.rb
@@ -3,6 +3,7 @@ cask :v1 => 'eyetv' do
   sha256 'cc8d7b1db43eabe827bab3a2401be97c6687cf83806ff585e3a125768c8c05f7'
 
   url "http://files.elgato.com/eyetvdownloads/support/eyetv_#{version.gsub('.','')}_7310.dmg"
+  name 'EyeTV'
   homepage 'http://www.elgato.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
