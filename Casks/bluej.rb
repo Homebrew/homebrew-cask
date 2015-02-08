03e13587382d8fe3980133c2bd6f209b72968f8f
Den Patin <denpatin@gmail.com>
2015-02-08 14:00:56 +0300

    Added name to the bluej.rb cask

--- Casks/bluej.rb
+++ Casks/bluej.rb
@@ -3,6 +3,7 @@ cask :v1 => 'bluej' do
   sha256 '507ce17037010e7a52684f97b32a07a391aca8fb1fa194748d35036242c87f80'
 
   url "http://www.bluej.org/download/files/BlueJ-#{version.gsub('.', '')}.zip"
+  name 'BlueJ'
   homepage 'http://www.bluej.org'
   license :gpl
 
