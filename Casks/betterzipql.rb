ee0ca9d5c71f10ec501071ef76e618491838148a
Den Patin <denpatin@gmail.com>
2015-02-08 14:08:47 +0300

    Added name to the betterzipql.rb cask

--- Casks/betterzipql.rb
+++ Casks/betterzipql.rb
@@ -3,6 +3,7 @@ cask :v1 => 'betterzipql' do
   sha256 :no_check
 
   url 'http://macitbetter.com/BetterZipQL.zip'
+  name 'BetterZipQL'
   homepage 'http://macitbetter.com/BetterZip-Quick-Look-Generator/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
