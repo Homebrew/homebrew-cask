bedcbe8a7af3e7aa4f58fd51854db7f5d590ce0c
Pfeil <der.andreas.pfeil@gmail.com>
2015-02-09 18:21:05 +0100

    Upgrade darktable.app to v1.6.2
    Upgrade to new stable version. Adjust version and sha256.

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'darktable' do
-  version '1.6.1'
-  sha256 'fcb45835f703bc394072fd5ee1f043f44097e2f48843da1cbfc414c3103606d0'
+  version '1.6.2'
+  sha256 'bd613994c9754313144e8804026b7faf672fa816801b687ff7d64a8d82880332'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version}/darktable-#{version}.dmg"
