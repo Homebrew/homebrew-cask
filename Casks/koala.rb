9062f3c19a756aa3fa61308810afe658c5b06ac1
Hoffmann József <illusionfield@gmail.com>
2015-02-09 09:28:11 +0100

    Koala app amazon download link fix

--- Casks/koala.rb
+++ Casks/koala.rb
@@ -1,8 +1,14 @@
 cask :v1 => 'koala' do
+  require 'yaml'
+  require 'open-uri'
+  response = open("https://www.amazon.com/drive/v1/shares/_7ThHQlpGzzY1IMGa72x2Kc4I3JBVTbdChWZz8KvIAI").read
+  metaData = YAML::load(response)
+
   version '2.0.4'
   sha256 '8b0d3564db80bad52cbad1b0755a5ff55f6d1549ea8f55e4a309552261a61327'
 
-  url "http://fieldgroup.net/cask/koala_#{version}_mac.zip"
+  #url "https://www.amazon.com/drive/v1/shares/_7ThHQlpGzzY1IMGa72x2Kc4I3JBVTbdChWZz8KvIAI"
+  url "#{metaData["nodeInfo"]["tempLink"]}"
   name 'Koala'
   homepage 'http://koala-app.com'
   license :apache
