98eaf94a322f6aaba4d958d380e9cfd05ce0c50f
Amory Meltzer <Amorymeltzer@gmail.com>
2015-02-08 18:14:56 -0800

    Update sigil homepage and version (0.8.4)
    Add note re official download host

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'sigil' do
-  version '0.8.2'
-  sha256 '2c8dd9f573c4549adde0778cbcc1c5535fdc68a705fd3aa470700dea678117cd'
+  version '0.8.4'
+  sha256 '755dc457cd37bd08cf5eac68fdc93831c03eb4f16cabc710469f361aab45332d'
 
+  # github.com is the official download host per the vendor homepage
   url "https://github.com/user-none/Sigil/releases/download/#{version}/Sigil-#{version}-Mac-Package.dmg"
   name 'Sigil'
-  homepage 'http://code.google.com/p/sigil/'
+  homepage 'http://sigil-ebook.com/'
   license :gpl
 
   app 'Sigil.app'
