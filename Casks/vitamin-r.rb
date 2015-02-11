308bb695f8ef03a0fa3d4165ae3649538ea1046f
Roland Walker <walker@pobox.com>
2015-02-11 09:26:20 -0500

    restore app stanza in vitamin-r.rb
    This should have never made it past Travis in #9510 .
    Apparently, Travis is not running Yosemite.

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -24,6 +24,7 @@ cask :v1 => 'vitamin-r' do
     sha256 :no_check
     url 'http://www.publicspace.net/download/Vitamin.dmg'
     appcast 'http://www.publicspace.net/app/vitamin2.xml'
+    app 'Vitamin-R 2.app'
   end
 
     name 'Vitamin-R'
