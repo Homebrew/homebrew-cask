21bf29a6ff6e4fe071b08fd21b2365284cdebf0b
David Routhieau <root-io@users.noreply.github.com>
2015-02-12 20:04:04 +0000

    Update Virtualbox

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'virtualbox' do
-  version '4.3.20-96996'
-  sha256 '744e77119a640a5974160213c9912568a3d88dbd06a2fc6b6970070941732705'
+  version '4.3.22-98236'
+  sha256 '886d0e6d17ee09cc1f48d7319257d597ed35428237a4d0d2305a7e03347b717d'
 
   url "http://download.virtualbox.org/virtualbox/#{version.sub(/-.*$/, '')}/VirtualBox-#{version}-OSX.dmg"
   name 'VirtualBox'
