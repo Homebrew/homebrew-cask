1bfc4b66230d06d763db3b260fa72335643e8c5b
Tim D. Smith <git@tim-smith.us>
2015-02-12 12:32:39 -0800

    rstudio 0.98.1102

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rstudio' do
-  version '0.98.1091'
-  sha256 'ea8baebe66903312bb84816669711b11f0dcd377a6f8c1c4c9f4c669ebd3dc18'
+  version '0.98.1102'
+  sha256 '1fc867cff35a79f71d104b6fc0a164bed6124dcaf2d1229fd77f93d6b2faf303'
 
   # rstudio.org is the official download host per the vendor homepage
   url "http://download1.rstudio.org/RStudio-#{version}.dmg"
