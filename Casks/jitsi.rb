d3568c775773255986f2d1f991ed15b0c5281d0f
David Routhieau <root-io@users.noreply.github.com>
2015-02-12 17:09:28 +0000

    Update Jitsi
    Jitsi 2.6 is the new stable build line: https://jitsi.org/Main/Download
    jitsi-2.6-latest link does not exist: https://download.jitsi.org/jitsi/macosx/

--- Casks/jitsi.rb
+++ Casks/jitsi.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'jitsi' do
-  version '2.4'
-  sha256 'ceb6b2ab04206a51faf1dbffb704a7a60ae2b7c47834b50f87da5557f543ad13'
+  version '2.6.5390'
+  sha256 '574bae43116792ce1f2ab5e61486388eee07cdfab618f8a251f66ed7ed01d27d'
 
-  url "https://download.jitsi.org/jitsi/macosx/jitsi-#{version}-latest.dmg"
+  url "https://download.jitsi.org/jitsi/macosx/jitsi-#{version}.dmg"
   appcast 'https://download.jitsi.org/jitsi/macosx/sparkle/updates.xml',
           :sha256 => 'db2939816e8a38c7197160ab455626af9aadc2c5603f2212e372cffb85b31949'
   homepage 'https://jitsi.org/'
