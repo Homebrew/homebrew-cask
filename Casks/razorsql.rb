bf4bf28746ba98708d7584db765c2a0b2f31a1b6
Ted Wise <cwise@hdlabinc.com>
2015-02-13 11:21:22 -0500

    Bump razorsql version to 6.3.17.

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'razorsql' do
-  version '6.3.16'
-  sha256 'e4e2838be5860648332358ad76725c521e537f156925210ce4ae3793e0c0f96b'
+  version '6.3.17'
+  sha256 '8a95889f9fbb5761d09245a2c9da0fd76f51d369930f1e570ab60d7e84d129a7'
 
   url "http://downloads.razorsql.com/downloads/#{version.gsub('.', '_')}/razorsql#{version.gsub('.', '_')}_x64.dmg"
   name 'RazorSQL'
