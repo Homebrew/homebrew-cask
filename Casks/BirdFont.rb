945a78bf3c6bd46518c9d9a470da79d113802119
Burim Beriša <berisa.burim@yahoo.com>
2015-02-14 16:27:59 +0100

    Creating BirdFont Cask

---
+++ Casks/BirdFont.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'BirdFont' do
+  version '1.0'
+  sha256 'a32b41608250bcba58572be36591a8a9c0907d8d724f31c6e6d18fc8137e5903'
+
+  url 'http://birdfont.org/download/birdfont-1.9-free.dmg'
+  name 'BirdFont'
+  homepage 'http://www.birdfont.org/'
+  license :freemium
+
+  app 'BirdFont.app'
+en
