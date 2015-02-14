18497a9782d0a93ad68ae88d24d4b280936fba00
Ryan Miller <ryanm@chefsteps.com>
2015-02-14 12:15:59 -0800

    Update Cloudup.app to use release channel.

--- Casks/cloudup.rb
+++ Casks/cloudup.rb
@@ -2,7 +2,7 @@ cask :v1 => 'cloudup' do
   version :latest
   sha256 :no_check
 
-  url 'https://zip.cloudup.com/f41gmwzoDkioEVevEkBW'
+  url 'https://updates.cloudup.com/update?os=osx&app=Cloudup&format=zip&channel=release'
   homepage 'https://cloudup.com/download'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
