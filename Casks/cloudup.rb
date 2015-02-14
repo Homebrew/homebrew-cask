194a5cd210795047cef6f09355369bf386b31286
Ryan Miller <ryanm@chefsteps.com>
2015-02-14 12:16:38 -0800

    Update Cloudup.app license to Freemium.

--- Casks/cloudup.rb
+++ Casks/cloudup.rb
@@ -4,7 +4,7 @@ cask :v1 => 'cloudup' do
 
   url 'https://updates.cloudup.com/update?os=osx&app=Cloudup&format=zip&channel=release'
   homepage 'https://cloudup.com/download'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
 
   app 'Cloudup.app'
 end
