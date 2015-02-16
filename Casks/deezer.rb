b8487c4132c8d1ae6bf996e71475c875b3990c33
Florian Harbecke <florian@harbecke.me>
2015-02-16 19:07:41 +0100

    Updated Deezer.app to v0.9.15_4141

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'deezer' do
-  version '0.9.14_4136'
-  sha256 'e4986a5ea10854c48ac5b15b4fd5964fe2611096f4311f428fe6e82fcddad717'
+  version '0.9.15_4141'
+  sha256 '0d1fd724eb5d22f4c35b22e5200e583ef277473ace366542ba62a77faccd0c41'
 
   url "http://cdn-content.deezer.com/builds/mac/Deezer#{version.sub(%r{^[^_]*(_\d+)},'\1')}.dmg"
   name 'Deezer'
