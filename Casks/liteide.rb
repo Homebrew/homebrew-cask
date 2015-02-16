566514241cfad667abd22387a74b74269af301a8
Philipp Wahala <philipp.wahala@gmail.com>
2015-02-16 08:33:22 +0100

    Upgrade liteide to 27.1

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'liteide' do
-  version '25.2'
-  sha256 '92cb684a4832cf32d06ca0fa60f1155d64bd63fb9af708277bd57b70f5eb2cf8'
+  version '27.1'
+  sha256 'ed757c9983a52b2e61bd8cecad0f386d0060244ce3169e7be14fe2cdbd213369'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/liteide/X#{version}/liteidex#{version}.macosx.zip"
