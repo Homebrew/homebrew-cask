2e9a41121512f93ce3a742c2c67ab3fe00fe9ca7
Florian Harbecke <florian@harbecke.me>
2015-02-17 19:19:39 +0100

    Changed url protocol to https

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -2,7 +2,7 @@ cask :v1 => 'deezer' do
   version '0.9.15_4141'
   sha256 '0d1fd724eb5d22f4c35b22e5200e583ef277473ace366542ba62a77faccd0c41'
 
-  url "http://cdn-content.deezer.com/builds/mac/Deezer#{version.sub(%r{^[^_]*(_\d+)},'\1')}.dmg"
+  url "https://cdns-content.deezer.com/builds/mac/Deezer#{version.sub(%r{^[^_]*(_\d+)},'\1')}.dmg"
   name 'Deezer'
   homepage 'https://www.deezer.com/beta/formac'
   license :gratis
