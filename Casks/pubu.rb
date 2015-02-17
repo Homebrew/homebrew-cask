e7bac13c5d9d544a73703cb515eb620c12dc0909
n3xtchen <echenwen@gmail.com>
2015-02-17 11:55:30 +0800

    add pubu app

---
+++ Casks/pubu.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'pubu' do
+  version '2.0.0'
+  sha256 'f0ce2abd31867de7f545a1d9a04a082e597178ca6697c0ae9c3404eb1f7c1b71'
+
+  url 'http://facecdn.u.qiniudn.com/downloads/%E7%80%91%E5%B8%83_OSX_V2.0.0_Beta.zip'
+  name '瀑布'
+  homepage 'http://pubu.im'
+  license :unknown
+
+  app '瀑布.app'
+end
