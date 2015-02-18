0ecb54a4a3f7bd09022753b60fc8e05fba0f6c79
Willy Morin <willy.morin@blablacar.com>
2015-02-18 15:31:43 +0100

    Update dbeaver-enterprise to v3.1.4

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'dbeaver-enterprise' do
-  version '3.1.2'
+  version '3.1.4'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '69e3fbb77f6dfa4039f2535451849ae3811e0430764bd507ecc5df3cd7eb28ba'
+    sha256 '5e3c5bab9d1ee2ba811c019f8c062cdaec8930b39ae97b16fa0fe2011e997496'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-ee-macosx.cocoa.x86.zip"
   else
-    sha256 'f868445eecae115245b5d0882917e548c1a799c3ea8e6e47c551557d62a857ff'
+    sha256 '71e61e4b1f3900689de515103edfd6b743a78227ccf153bdb851be8a23b04664'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-ee-macosx.cocoa.x86_64.zip"
   end
 
