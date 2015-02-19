f8b25a750ed032dd8e4354e6c30e5b32c60b16f5
Dennis Hoppe <github@dhoppe.it>
2015-02-19 23:13:22 +0100

    Update consul.rb
    Update to version 0.5.

--- Casks/consul.rb
+++ Casks/consul.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'consul' do
-  version '0.4.1'
-  sha256 '957fe9ba27bbaf99539cd534db8ac8ec4c9fa1c6b3b4675d0c0eb3a7fbfb646c'
+  version '0.5.0'
+  sha256 '24d9758c873e9124e0ce266f118078f87ba8d8363ab16c2e59a3cd197b77e964'
 
   # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/mitchellh/consul/#{version}_darwin_amd64.zip"
