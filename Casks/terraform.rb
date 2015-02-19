825418f5e13dbb655c3fff666f200652c3177f7c
Dennis Hoppe <github@dhoppe.it>
2015-02-19 23:17:56 +0100

    Update terraform.rb
    Update to version 0.3.7.

--- Casks/terraform.rb
+++ Casks/terraform.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'terraform' do
-  version '0.3.6'
-  sha256 '65b4c5bfc34bb0464b691b31ac554132c87ac0c5d7acef936c039777a27dccad'
+  version '0.3.7'
+  sha256 'aecdc8119cd637e3e60967c97f9912735400814546b8e925152203fb6e99c732'
 
   # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/mitchellh/terraform/terraform_#{version}_darwin_amd64.zip"
