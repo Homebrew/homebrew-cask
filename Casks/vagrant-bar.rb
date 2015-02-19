18f80e5cebce603d6b14c11c7df1b3574fb6d23d
Willy Morin <willy.morin@blablacar.com>
2015-02-19 16:37:58 +0100

    Update Vagrant Bar to v1.16

--- Casks/vagrant-bar.rb
+++ Casks/vagrant-bar.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'vagrant-bar' do
-  version '1.15'
-  sha256 'dc4f77ad00e2751e7375eab0172323d9bd9110ced170dc4033036b530ed7fdeb'
+  version '1.16'
+  sha256 'f5c58690960269be9a7a2597a0aad098b4bd1676c9494f6022e9c7d1e82d81bc'
 
-  url "https://github.com/BipSync/VagrantBar/releases/download/#{version}/Vagrant.Bar.zip"
+  url "https://github.com/BipSync/VagrantBar/releases/download/#{version.gsub('1.','')}/Vagrant.Bar.zip"
   homepage 'https://github.com/BipSync/VagrantBar'
   license :oss
 
