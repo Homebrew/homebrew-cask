cc94f227c3f5edaaa19b75a5ad51da3212a1cad5
Hernan Payrumani <hernan.payrumani@gmail.com>
2015-02-19 20:29:10 -0400

    Removed preflight in favor of install
    Removed preflight in favor of install and moved installation folder to /opt

--- Casks/sencha.rb
+++ Casks/sencha.rb
@@ -1,18 +1,17 @@
 cask :v1 => 'sencha' do
   version '5.1.1.39'
   sha256 'bb3de716b825fe9f8908f96a3f7570499ffbf41bac860a16f57f680b3cd341ad'
-  
+
   url "http://cdn.sencha.com/cmd/#{version}/SenchaCmd-#{version}-osx.app.zip"
   name 'Sencha Cmd'
   homepage 'http://www.sencha.com/products/sencha-cmd/'
   license :freemium
 
-  preflight do
-    system "#{staged_path}/SenchaCmd-#{version}-osx.app/Contents/MacOS/installbuilder.sh", '--mode', 'unattended', '--prefix' , "#{staged_path}"
-  end
+  installer :script => "SenchaCmd-#{version}-osx.app/Contents/MacOS/installbuilder.sh",
+            :args   => ['--mode', 'unattended', '--prefix' , '/opt']
 
   uninstall :script => {
-                         :executable => "Sencha/Cmd/#{version}/uninstall.app/Contents/MacOS/installbuilder.sh",
+                         :executable => "/opt/Sencha/Cmd/#{version}/uninstall.app/Contents/MacOS/installbuilder.sh",
                          :args => ["--mode", "unattended"]
                        }
 
@@ -24,10 +23,8 @@ cask :v1 => 'sencha' do
 
       Sencha Cmd adds two changes to your ~/.bashrc file:
 
-
-        export PATH=#{staged_path}/Sencha/Cmd/#{version}:$PATH
-        export SENCHA_CMD_3_0_0="#{staged_path}/Sencha/Cmd/#{version}"
-
+        export PATH=/opt/Sencha/Cmd/#{version}:$PATH
+        export SENCHA_CMD_3_0_0="/opt/Sencha/Cmd/#{version}"
 
       If you are a zshell user, copy at the end of your .zshrc file both lines.
     EOS
