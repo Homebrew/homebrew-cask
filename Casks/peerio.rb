e0112349c24d6671e9f580ab2ac6296a1233f517
0xADADA <0xadada.pub@0xadada.pub>
2015-02-21 16:27:11 -0500

    Added a sha 256 sum for latest zip
    SHA 256 sum for the peerio app added to the manifest.

--- Casks/peerio.rb
+++ Casks/peerio.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'peerio' do
   version :latest
-  sha256 :no_check
+  sha256 '3a3689f9a511dd701fc0ef7733016e828eb8c01b7c4ba23a484f289b654a315e'
 
   url 'https://peerio.com/download/peerio-mac.zip'
   name 'Peerio'
