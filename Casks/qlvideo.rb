dab0f9688f0c2f6b4a950a93613130aa008a7948
Zhiming Wang <zmwangx@gmail.com>
2015-02-21 16:57:56 -0800

    qlvideo: update to 1.83

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qlvideo' do
-  version '1.81'
-  sha256 'e901bf38a02c630dfddb34eb31a897155d7e16e503dd44ec9e6c8fed09ee62bb'
+  version '1.83'
+  sha256 'df4db7b2f43a365ee528a7cb5cc921a59b46950060708d2c97551be606aa58d5'
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.gsub('.', '')}/QLVideo_#{version.gsub('.', '')}.pkg"
   name 'QuickLook Video'
