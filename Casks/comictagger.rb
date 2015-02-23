2ccc99286699129d9c0a9d826356256a8395c86e
maulthet <maulthet@gmail.com>
2015-02-23 18:02:30 +0100

    ComicTagger 1.1.15-beta
    1.1.15-beta
    13-Jun-2014
    
    - WebP support
    - Added user-configurable API key for Comic Vine access
    - Experimental option to wait and retry after exceeding Comic Vine rate
    limit

--- Casks/comictagger.rb
+++ Casks/comictagger.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'comictagger' do
-  version '1.1.10-beta'
-  sha256 'f59514cfd15e64a8c595c4ad9707266b2c01d7744ca9f5836bee484954ce2fa6'
+  version '1.1.15-beta'
+  sha256 '6640834d966c1cc760de6aa32729bf139bf06727f8d650a4534cdf780f084960'
 
   url "https://comictagger.googlecode.com/files/ComicTagger-#{version}.dmg"
   name 'ComicTagger'
