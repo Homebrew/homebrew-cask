9b0a87e1740f0eb6671acf9cd877e3d39f479593
Mihir Singh <git.service@mihirsingh.com>
2015-02-22 19:52:49 -0600

    Upgraded Spotify Notifications to 0.5.1

--- Casks/spotify-notifications.rb
+++ Casks/spotify-notifications.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'spotify-notifications' do
-  version '0.4.8'
-  sha256 '953028e9a1aad445005869598050cb8612980821a796563936f557e03b319f50'
+  version '0.5.1'
+  sha256 'd9adb821d82be7f1266d8042d2b677a4c91fe7686631c02e2d0307f9f536a722'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/citruspi/Spotify-Notifications/releases/download/#{version}/Spotify.Notifications.-.#{version}.zip"
