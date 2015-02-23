8937991687bf598ca80d0571bd9f46e96e8f2213
Roland Walker <walker@pobox.com>
2015-02-23 06:46:26 -0500

    fix checksum and homepage for amazon-cloud-drive

--- Casks/amazon-cloud-drive.rb
+++ Casks/amazon-cloud-drive.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'amazon-cloud-drive' do
   version 'mac'
-  sha256 'ef47650da13dbbef227d985d1e9ae06435dd6319'
+  sha256 '22bca820ff5f13c9bc6c986fffff14bb540534c7d739711ee2cab48845a847b5'
 
   url "https://d29x207vrinatv.cloudfront.net/#{version}/AmazonCloudDriveInstaller.dmg"
   name 'Amazon Cloud Drive App'
-  homepage 'http://www.amazon.com/clouddrive'
+  homepage 'https://www.amazon.com/clouddrive/home/'
   license :gratis
 
   app 'Amazon Cloud Drive App.app'
