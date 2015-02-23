88b33f04472fecbe238624827119ce7e214d4190
Julien Lavergne <lavergne.julien@gmail.com>
2015-02-23 03:04:18 +0100

    fix sha256 cloudapp v3.2.0

--- cloudapp.rb
+++ cloudapp.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'cloudapp' do
   version '3.2.0'
-  sha256 'fce1a45e53e0f6bc2ef20e5b6fc84bd48806f1bc2a38acec57fd9fafe7e2af7e'
+  sha256 '1e1868f61790c07e83480fe2d6b5e54efd1bb5a5'
 
   url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.dmg"
   name 'CloudApp'
