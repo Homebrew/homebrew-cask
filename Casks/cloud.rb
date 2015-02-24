2a4ccd24fe902ae3289db7354857a84ac47ff63e
Amory Meltzer <Amorymeltzer@gmail.com>
2015-02-24 12:17:07 -0800

    Update cloud download to dmg from zip
    Official download link changed format

--- Casks/cloud.rb
+++ Casks/cloud.rb
@@ -3,7 +3,7 @@ cask :v1 => 'cloud' do
   sha256 '713c8eeb91506661af97cf31f07ea4a31e65a773579fe51ef5194f775df748a7'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.zip"
+  url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.dmg"
   name 'CloudApp'
   homepage 'http://getcloudapp.com/'
   license :gratis
