6fa86a70eac9d0f327c06c3bcad6fdf167dd3f45
Ariel Allon <aallon@Ariels-MBP-2.gorillagroup.lan>
2015-02-25 12:46:20 -0600

    Add Liya.app v3.0.5

---
+++ Casks/liya.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'liya' do
+  version '3.0.5'
+  sha256 '9441df5c2790e26b3496d7dde666c52dd8c6a432799fae0221c52490ca079997'
+
+  url 'https://cutedgesystems.com/downloads/Liya.zip'
+  name 'Liya'
+  homepage 'http://cutedgesystems.com/software/liya/'
+  license :unknown
+
+  app 'Liya.app'
+end
