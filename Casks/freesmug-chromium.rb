efd93b5ef3f06e979fe7d63ce7ef07d1e79120e3
Jussi Tuominen <Jussi@COLDwithin.local>
2015-02-25 21:40:44 +0200

    Update FreeSMUG Chromium to v40.0.2214.115

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'freesmug-chromium' do
-  version '40.0.2214.94'
-  sha256 '404d93a048f86d500320c7d2a11b58bf8391aca190278036688c095fd8d183b2'
+  version '40.0.2214.115'
+  sha256 '00b40f4b903b2ea985e2d1dfd4f822fb23b6954b145c1ebe4cb8bde0d2a60260'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/osxportableapps/Chromium_OSX_#{version}.dmg"
