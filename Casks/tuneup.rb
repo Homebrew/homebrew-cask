535be853c1f414f62ce535bd6fd4695e747fe39a
Roland Walker <walker@pobox.com>
2015-02-26 10:22:02 -0500

    add uninstall todo

--- Casks/tuneup.rb
+++ Casks/tuneup.rb
@@ -7,5 +7,6 @@ cask :v1 => 'tuneup' do
   homepage 'http://www.tuneupmedia.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
+  # todo: installer :manual should have a matching uninstall
   installer :manual => 'TuneUp-Installer.app'
 end
