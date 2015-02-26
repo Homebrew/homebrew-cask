2f7202313b8207799b8467618916707206e3fd8f
ScreenDriver <github@echooff.de>
2015-02-26 10:45:23 +0100

    Updated thunderbird to 31.5.0

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird' do
-  version '31.4.0'
-  sha256 '6de56ad6d215e3505afa376834cf251e1a1d0d4d4d1d570cfb507afefd164490'
+  version '31.5.0'
+  sha256 '5cec9186419853d902e0e61a52c20d679b27812b5a5e631d31ecbe5917b47ea1'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   homepage 'http://www.mozilla.org/en-US/thunderbird/'
