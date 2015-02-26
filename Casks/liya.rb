76331ef5b34a679e620051b7ec2cd801b23e5afa
Ariel Allon <arielallon@users.noreply.github.com>
2015-02-25 21:46:05 -0600

    Update liya.rb
    Updates to match @vitorgalvao's suggestions

--- Casks/liya.rb
+++ Casks/liya.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'liya' do
-  version '3.0.5'
-  sha256 '9441df5c2790e26b3496d7dde666c52dd8c6a432799fae0221c52490ca079997'
+  version :latest
+  sha256 :no_check
 
   url 'https://cutedgesystems.com/downloads/Liya.zip'
   name 'Liya'
   homepage 'http://cutedgesystems.com/software/liya/'
-  license :unknown
+  license :gratis
 
   app 'Liya.app'
 end
