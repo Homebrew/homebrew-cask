4327ba2836975cc171ba74c582da3e7508d5be69
oniatsu <pblcusr@gmail.com>
2015-02-27 22:43:36 +0900

    Upgrade HotSwitch to latest
    And uniform string quotes

--- Casks/hotswitch.rb
+++ Casks/hotswitch.rb
@@ -1,11 +1,11 @@
-cask :v1 => "hotswitch" do
-  version "1.2"
-  sha256 "f9db052a40efa5e6520434567c99ac965874e63b6afd429b5dc760e71a3aeefa"
+cask :v1 => 'hotswitch' do
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/oniatsu/HotSwitch/releases/download/v#{version}/HotSwitch-#{version}.zip"
-  name "HotSwitch"
-  homepage "http://oniatsu.github.io/HotSwitch/"
+  url 'http://oniatsu.github.io/HotSwitch/release/zip/HotSwitch.zip'
+  name 'HotSwitch'
+  homepage 'http://oniatsu.github.io/HotSwitch/'
   license :mit
 
-  app "HotSwitch.app"
+  app 'HotSwitch.app'
 end
