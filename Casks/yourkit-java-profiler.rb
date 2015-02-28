cb72bc466ade9055e3d14c7121af2a2c6e16f6ab
Jochen Schalanda <jochen@schalanda.name>
2015-02-28 12:02:42 +0100

    Upgrade YourKit Java Profiler 2014 to build 14120

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'yourkit-java-profiler' do
-  version '2014-build-14118'
-  sha256 'b74c6c5d88445d01c497ad4e027f9416e9e75cce47572843f75e450f959dcc9d'
+  version '2014-build-14120'
+  sha256 '88ae356404b150121522a941dbd5efdaa796b29cbbb7ca4cd173e893bfbbbf51'
 
   url "http://www.yourkit.com/download/yjp-#{version}-mac.zip"
   name 'YourKit Java Profiler'
