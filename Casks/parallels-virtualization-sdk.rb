1115535b31abbec1c51abd43eb1af1a8cd745a4a
zchee <zcheeee@gmail.com>
2015-03-01 00:43:23 +0900

    Upgrade Parallels Virtualization SDK to v10.1.4-28883

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'parallels-virtualization-sdk' do
-  version '10.1.3-28868'
-  sha256 '085c34bd4e292ed718ae5c7c428e111c715480142bd15c42540420888b1df4ca'
+  version '10.1.4-28883'
+  sha256 '7d3bca4a9b4b17e5033c132b03fb01d7c08402e6d2cd8c0a9dd42743a435dc04'
 
   url "http://download.parallels.com/desktop/v#{version[/^\w+/]}/#{version.sub(/-.*$/, '')}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name 'Parallels Virtualization SDK'
