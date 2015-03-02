b07d853e2e181f47b8d8edcb646808e9347f5a07
Julien Duponchelle <julien@duponchelle.info>
2015-03-02 17:57:16 +0100

    Upgrade QT Creator to v3.3.1

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qt-creator' do
-  version '3.3.0'
-  sha256 '5e5491f6c82d3cb57b3ccc186b5b239ca52c2144034da92fc72ad7e8e6788045'
+  version '3.3.1'
+  sha256 '4e7f697e49330abc7883f0bbf58b5c6b7d57dc7d365f5430a9beda1cc3c5ec43'
 
   url "http://download.qt-project.org/official_releases/qtcreator/#{version.sub(%r{^(\d+\.\d+).*},'\1')}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   homepage 'http://qt-project.org/'
