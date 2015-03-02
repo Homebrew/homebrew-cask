003e20bbe18377951085960859fb3428b6e67f16
Amory Meltzer <Amorymeltzer@gmail.com>
2015-03-02 10:29:28 -0800

    Update coteditor to v 2.1.1

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'coteditor' do
-  version '2.1.0'
-  sha256 '4d2f31697d8bb621cec3284d0a40601433deaff9733aa0010d93e9eb122660d3'
+  version '2.1.1'
+  sha256 '3de8cc6fd3788dc593955f41eafa83af713c31faa68b77a5a70fc85491f0d0f0'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg"
   appcast 'http://coteditor.com/appcast.xml',
-          :sha256 => '4abe532801c9944267d5e6f6279604d0c202cc93b10065f4ad714f9c4cb47cef'
+          :sha256 => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   name 'CotEditor'
   homepage 'http://coteditor.com/'
   license :gpl
