b1c1dd38f9c6d2fb095ea3773775b8efaeb68231
Willy Morin <willy.morin@blablacar.com>
2015-03-03 11:53:30 +0100

    Update Data Science Studio to v1.4.2

--- Casks/data-science-studio.rb
+++ Casks/data-science-studio.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'data-science-studio' do
-  version '1.4.1a'
-  sha256 '09d7d5523befae8efc7063024dab34ad227052e55651d264db9e5b6806607650'
+  version '1.4.2'
+  sha256 'f7849625ac4bd47c7cf5624e30ee33cee99941beee695af4002f74d888aa36b8'
 
-  url 'http://downloads.dataiku.com/public/studio/Data%20Science%20Studio%201.4.1a.dmg'
+  url 'http://downloads.dataiku.com/public/studio/Data%20Science%20Studio%201.4.2.dmg'
   name 'Data Science Studio'
   homepage 'https://www.dataiku.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
