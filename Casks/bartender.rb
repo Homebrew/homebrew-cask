cf70c17d2ee33812a9bb91cb3d09e6905077a135
Nicolas Genty <nicolas.genty@gmail.com>
2015-03-03 10:04:13 +0100

    update bartender v1.2.32

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -1,10 +1,12 @@
 cask :v1 => 'bartender' do
-  version :latest
-  sha256 :no_check
+  version '1.2.32'
+  sha256 'b663d780b20e6b2565d30507973e56bed3f3831df97118a16935b5d11fc2b9ac'
 
-  url 'http://www.macbartender.com/Demo/Bartender.zip', :referer => 'http://www.macbartender.com'
+  url "http://macbartender.com/updates/#{version.gsub('.', '-')}/Bartender.zip",
+      :referer => 'http://www.macbartender.com'
   name 'Bartender'
-  appcast 'http://www.macbartender.com/updates/updates.php'
+  appcast 'http://www.macbartender.com/updates/Appcast.xml',
+          :sha256 => 'cb01076d5f91bf81f8b3074c5bc205f74b55e81ee880b756a2898057a2ffc953'
   homepage 'http://www.macbartender.com/'
   license :commercial
 
