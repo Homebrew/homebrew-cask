c920c8b119aa66e9c68eddb99685b4f1d3cb2ae9
Nick Silkey <nick.silkey@rackspace.com>
2015-03-03 20:07:38 -0600

    Update drobo-dashboard
    It looks like the pkg contains a revision marker in it.  We need to
    include it.  Without it, this formula is broken.
    
    Confirmed Uninstall is a-ok.

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -1,12 +1,13 @@
 cask :v1 => 'drobo-dashboard' do
   version '2.6.4'
+  rev '72278'
   sha256 '36a2ce104ea86b25db40098677144f3342a9fa7f910d48a59c136a3ca41214c8'
 
   url "http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version}.dmg"
   homepage 'http://www.drobo.com'
   license :gratis
 
-  pkg "Install.app/Contents/Drobo_Dashboard_Installer_#{version}.pkg"
+  pkg "Install.app/Contents/Drobo_Dashboard_Installer_#{version}_#{rev}.pkg"
 
   uninstall :script => 'Uninstall.app/Contents/Resources/Scripts/Drobo_Dashboard_uninstall.sh',
             :pkgutil => 'com.datarobotics.drobodashboard'
