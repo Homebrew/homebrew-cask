87154ee358b7fc39081c53df7f1c3aec7eb60ae9
Nick Silkey <nick.silkey@rackspace.com>
2015-03-03 20:07:38 -0600

    Update drobo-dashboard
    Closes #9865.

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'drobo-dashboard' do
-  version '2.6.4'
+  version '2.6.4_72278'
   sha256 '36a2ce104ea86b25db40098677144f3342a9fa7f910d48a59c136a3ca41214c8'
 
-  url "http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version}.dmg"
+  url "http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version.sub(%r{_.*},'')}.dmg"
   homepage 'http://www.drobo.com'
   license :gratis
 
