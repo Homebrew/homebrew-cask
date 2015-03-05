06b70324608776653c94134696f7a4fc652c8d70
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-05 17:38:43 +0200

    Upgrade Vivaldi.app to 1.0.118.19

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'vivaldi' do
-  version '1.0.118.2'
+  version '1.0.118.19'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://vivaldi.com/download/download.php?f=Vivaldi.#{version}.dmg"
+  url "https://vivaldi.com/download/Vivaldi_TP2.#{version}.dmg"
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
   license :gratis
