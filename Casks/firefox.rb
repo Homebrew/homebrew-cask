1676879efe5f5f39dc4f52ca91a14481d3705e12
Caleb Xu <calebcenter@live.com>
2015-03-07 12:08:29 -0500

    Switch firefox to official download via FTP (served by CDN)

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -2,7 +2,7 @@ cask :v1 => 'firefox' do
   version '36.0' 
   sha256 '97ccc2f03a8af73903b0cae61e17a7b9f336fe320592380f1c811dbc3b0e9e5a'
 
-  url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
+  url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
   name 'Firefox'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/'
