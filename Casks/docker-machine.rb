e2eb1d4353cc0a9e02e896cee3785be62970b199
Kirill Sevastyanenko <kirill.sevastyanenko@avantcredit.com>
2015-03-07 16:26:27 -0600

    Add docker-machine cask v0.1.0

---
+++ Casks/docker-machine.rb
@@ -0,0 +1,18 @@
+cask :v1 => 'docker-machine' do
+  version 'v0.1.0'
+  sha256 '9915d88f779915aa7f1d7ba2537433b15665030574d61b9e348dd1f7397606c4'
+
+  url "https://github.com/docker/machine/releases/download/#{version}/docker-machine_darwin-amd64"
+  name 'Docker Machine'
+  homepage 'https://docs.docker.com/machine'
+  license :apache
+  depends_on :cask => 'boot2docker'
+  depends_on :arch => :x86_64
+
+  container :type => :naked
+  binary 'docker-machine_darwin-amd64', :target => 'docker-machine'
+
+  postflight do
+    system '/bin/chmod', '--', '0755', "#{staged_path}/docker-machine_darwin-amd64"
+  end
+end
