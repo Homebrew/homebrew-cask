8008e02077172dbf0263e08abc24e49146d96e17
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-03-07 12:18:56 +0900

    bump version to 4.4.2

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'eclipse-platform' do
-  version '4.4.1-201409250400'
+  version '4.4.2-201502041700'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '99d1f13d65a54c008905c0c0fd7c306c1cce1adc2208b8e0546c60b084f976dd'
+    sha256 '8ea83c03a714300008e2d2859a6be9238cdfe7a74cdb0bf7b4ef9e3e3bafcf5f'
     url "http://download.eclipse.org/eclipse/downloads/drops#{version.to_i}/R-#{version}/eclipse-SDK-#{version.sub(%r{-.*},'')}-macosx-cocoa.tar.gz"
   else
-    sha256 'a269e0f129ebaba6522b4a4d2cd07950b0634d44c353a2558106ca8289fc8463'
+    sha256 'e49cc9b6379a4eed7613f997b0b4c415f34bb858069a134f8ad46b1585761395'
     url "http://download.eclipse.org/eclipse/downloads/drops#{version.to_i}/R-#{version}/eclipse-SDK-#{version.sub(%r{-.*},'')}-macosx-cocoa-x86_64.tar.gz"
   end
 
