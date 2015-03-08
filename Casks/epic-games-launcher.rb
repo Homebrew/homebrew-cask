6fbc8000c08a84edfcde1349561430de3aa76590
Vítor Galvão <info@vitorgalvao.com>
2015-03-08 17:42:25 +0100

    epic-games-launcher.rb: easier to update

--- Casks/epic-games-launcher.rb
+++ Casks/epic-games-launcher.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'epic-games-launcher' do
-  version '2.0.1'
+  version '2.0.1-2467307'
   sha256 'e04e5b6232df5bd81d4b796f5c4556a55b56a34e0089c9f65f67c2f3b9c830d1'
 
-  url 'https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicGamesLauncher-2.0.1-2467307.dmg'
+  url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicGamesLauncher-#{version}.dmg"
   homepage 'https://www.unrealengine.com/what-is-unreal-engine-4'
   license :commercial
   tags :vendor => 'EpicGames'
