6453b3d9ff303a9111d8c266a021bc5e899f2b42
Martijn Hemeryck <martijn.hemeryck@soundtalks.com>
2015-03-09 13:38:55 +0100

    Upgrade filezilla to v3.10.2
    Upgrade filezilla from v3.10.1.1 to v3.10.2.

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -4,8 +4,8 @@ cask :v1 => 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.10.1.1'
-    sha256 '1af40a7ac98f3b36ef74313c5eca132c2383391589b4e71bc709af8dc6b97a9f'
+    version '3.10.2'
+    sha256 '7a225cac2a015111e20e9b869c454dd07ff82bb0e8615f8a4102b4ff6d8c73f6'
   end
 
   # sourceforge.net is the official download host per the vendor homepage
