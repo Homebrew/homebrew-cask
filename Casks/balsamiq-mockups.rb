a5bae1a6997608299dc559c1db9c0ee682035900
Vítor Galvão <info@vitorgalvao.com>
2015-03-09 17:51:42 +0100

    updated balsamiq-mockups (3.0.2)

--- Casks/balsamiq-mockups.rb
+++ Casks/balsamiq-mockups.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'balsamiq-mockups' do
-  version :latest
-  sha256 :no_check
+  version '3.0.2'
+  sha256 '9fc0de506e7275a4468f238f38ff11304877330a2fa5c082fbbf8ce90e07c994'
 
   # amazonaws is the official download host per the vendor homepage
-  url 'http://s3.amazonaws.com/build_production/mockups-desktop/MockupsForDesktop.dmg'
+  url "https://s3.amazonaws.com/build_production/mockups-desktop/Balsamiq_Mockups_#{version}.dmg"
   name 'Balsamiq Mockups'
   homepage 'http://balsamiq.com/'
   license :commercial
