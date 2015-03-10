028ed7124d2c67eea19c17018f9c1d4e4859b6c1
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-11 00:09:03 +0200

    Upgrade Opera.app to 28.0.1750.40

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera' do
-  version '27.0.1689.76'
-  sha256 'eb6f107865eb195c989a560e3d35fe44f50ae531376fc9f53e65d4a655e78195'
+  version '28.0.1750.40'
+  sha256 '8f5e7e6e1b8ba466e05b228aed5acc791b93308b0fc47c76653eb124da4d646f'
 
   url "http://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/'
