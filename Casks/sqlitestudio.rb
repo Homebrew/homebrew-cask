8bb97d37f8b81c6513430cbfa53b6d734f7d1426
Joao C Costa <joaocc@users.noreply.github.com>
2015-03-10 22:11:35 +0000

    Update to v3.0.3

--- Casks/sqlitestudio.rb
+++ Casks/sqlitestudio.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'sqlitestudio' do
-  version '2.1.5'
-  sha256 '7df2d3bc8a4aafac738b7b412f2bde82f2b1114dc30f173363ec28ae5514f882'
+  version '3.0.3'
+  sha256 '5b5e5dd370467ec2d3c0a6c2d1a1d01d2346ad10cbc322a2534fb5ec75579a87'
 
-  url "http://sqlitestudio.pl/files/free/stable/macosx/sqlitestudio-#{version}.zip"
+  url "http://sqlitestudio.pl/files/sqlitestudio3/complete/macosx/sqlitestudio-#{version}.dmg"
   homepage 'http://sqlitestudio.pl'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
