126f415b7c3b49eb4e963b892dba95817416023c
Vítor Galvão <info@vitorgalvao.com>
2015-03-12 00:01:40 +0100

    updated hub (2.2.0)

--- Casks/hub.rb
+++ Casks/hub.rb
@@ -1,17 +1,11 @@
 cask :v1 => 'hub' do
-  version '2.2.0-rc1'
+  version '2.2.0'
+  sha256 'dd34cbdc644f2c486af6af34eb193b56e6ea96e37756a0411e8f0dc065cdcf2b'
 
-  if Hardware::CPU.is_32_bit?
-    sha256 '6e41b6e964e56caa9cc363411b4a54e22ece5613c0c425dbb755fce6928ba2db'
-    url "https://github.com/github/hub/releases/download/#{version}/hub_#{version}_mac_386.gz.tar"
-    binary "hub_#{version}_mac_386/hub"
-  else
-    sha256 '57e62305b983e4e292b0a503ab5e17701a5624ae640fd67c3d776415a85733f6'
-    url "https://github.com/github/hub/releases/download/v#{version}/hub_#{version}_mac_amd64.gz.tar"
-    binary "hub_#{version}_mac_amd64/hub"
-  end
-
+  url "https://github.com/github/hub/releases/download/v#{version}/hub-mac-amd64-#{version}.tar.gz"
   name 'hub'
   homepage 'https://github.com/github/hub'
   license :mit
+
+  binary "hub-mac-amd64-#{version}/hub"
 end
