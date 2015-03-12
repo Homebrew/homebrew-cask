071f0326b8906df9289678eaa8cfc945807974eb
Vítor Galvão <info@vitorgalvao.com>
2015-03-13 00:12:08 +0100

    cloudfoundry-cli.rb: revert to amazonaws.com url

--- Casks/cloudfoundry-cli.rb
+++ Casks/cloudfoundry-cli.rb
@@ -2,8 +2,8 @@ cask :v1 => 'cloudfoundry-cli' do
   version '6.10.0'
   sha256 '35e8552119b989ab6f3acac10f8dfdf49c3efe20d6f96c141c2129a519342222'
 
-  # pivotal.io is the official download host per the vendor homepage
-  url "https://cli.run.pivotal.io/stable?release=macosx64&version=#{version}&source=github-rel"
+  # amazonaws.com is the official download host per the vendor homepage
+  url "http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v#{version}/installer-osx-amd64.pkg"
   name 'Cloud Foundry CLI'
   homepage 'https://github.com/cloudfoundry/cli'
   license :apache
