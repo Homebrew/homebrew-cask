46b29ceaf2441a5e44fa1c3ec528d794474f8b12
Sergiu Gatlan <sergiug@gmail.com>
2015-03-12 10:37:44 +0200

    Update Boom to 1.1.1

--- Casks/boom.rb
+++ Casks/boom.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'boom' do
-  version '1.0.1'
-  sha256 '9ee325c8901bfeaae54ca10be5eac129f42c956a0daef772a5e9b0192fd4ab2c'
+  version '1.1.1'
+  sha256 '21e789d4ec2a35a74b2fea649926ffa63fac5313040e7393f9e01c727c6d6ed4'
 
-  url "http://www.globaldelight.com/boom/download/2x/v#{version}/boom2.dmg"
+  url "http://www.globaldelight.com/boom/download/2x/web/boom2.dmg"
   name 'Boom'
   homepage 'http://www.globaldelight.com/boom/'
   license :commercial
