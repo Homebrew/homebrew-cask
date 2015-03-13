59ac916e094bd2dd5db85f362be605147c67b91e
Adrian Petrescu <apetresc@gmail.com>
2015-03-13 15:13:26 -0400

    Upgrade calibre to 2.21.0

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'calibre' do
-  version '2.20.0'
-  sha256 '235d891a90a87a72e16d4c8979497268055ef6e22c1c3d8aff3d0ce2114c41c7'
+  version '2.21.0'
+  sha256 'be3ebde825d291ef0e212939061ea2e9a095419b9ec45ab9fa766f7bad07094d'
 
   # github.com is an official download host per the vendor homepage, and a faster mirror than the main one
   url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}.dmg"
