b669d1e5dbb76509f6e20ef3be7b586843037255
glimpsed <glimpsed>
2015-03-13 10:22:32 +0200

    Revert "Update Boom to 1.1.1"
    This reverts commit 46b29ceaf2441a5e44fa1c3ec528d794474f8b12.
    
    Conflicts:
    	Casks/boom.rb

--- Casks/boom.rb
+++ Casks/boom.rb
@@ -1,8 +1,15 @@
 cask :v1 => 'boom' do
+<<<<<<< HEAD
   version :latest
   sha256 :no_check
 
   url 'http://www.globaldelight.com/boom/download/2x/web/boom2.dmg'
+=======
+  version '1.0.1'
+  sha256 '9ee325c8901bfeaae54ca10be5eac129f42c956a0daef772a5e9b0192fd4ab2c'
+
+  url "http://www.globaldelight.com/boom/download/2x/v#{version}/boom2.dmg"
+>>>>>>> parent of 46b29ce... Update Boom to 1.1.1
   name 'Boom'
   homepage 'http://www.globaldelight.com/boom/'
   license :commercial
