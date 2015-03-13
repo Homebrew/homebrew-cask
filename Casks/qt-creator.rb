fc4335972ed16ac04109ccc7ec60d6cf93afd18b
Nicolas Jeker <n.jeker@gmx.net>
2015-03-13 15:11:01 +0100

    Update Qt Creator to 3.2.2

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qt-creator' do
-  version '3.3.1'
-  sha256 '4e7f697e49330abc7883f0bbf58b5c6b7d57dc7d365f5430a9beda1cc3c5ec43'
+  version '3.3.2'
+  sha256 '6b44dd8e01251b485dad7d412db1898b297f129993fea74694be1d7688e845ba'
 
   url "http://download.qt-project.org/official_releases/qtcreator/#{version.sub(%r{^(\d+\.\d+).*},'\1')}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   homepage 'http://qt-project.org/'
