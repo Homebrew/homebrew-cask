67442017081eec1ea3f1681c1b56aabc8d829665
Jean-Paul Pelteret <jppelteret@gmail.com>
2015-03-14 09:53:45 +0100

    Updated Texstudio to v2.9.0

--- Casks/texstudio.rb
+++ Casks/texstudio.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'texstudio' do
-  version '2.8.8'
-  sha256 'bc0212fe6897128982cbd7227ec10562fdb421ba8e127e45d79e9c766896665a'
+  version '2.9.0'
+  sha256 '716635c930375e095de018e88243d8964d6df6af32f3f0c286e21f81967db342'
 
   url "http://downloads.sourceforge.net/sourceforge/texstudio/texstudio_#{version}_osx_qt5.zip"
   homepage 'http://texstudio.sourceforge.net/'
