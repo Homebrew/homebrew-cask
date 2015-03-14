169d19139d48d3389531ea721d52a853544a3b7e
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-14 22:23:29 +0200

    Add name, license, vendor and zap for Gyazo.app

--- Casks/gyazo.rb
+++ Casks/gyazo.rb
@@ -3,8 +3,18 @@ cask :v1 => 'gyazo' do
   sha256 '88491cc2a9d481fdb99b822ca49560427ed11578b304203c4504d83fc2562061'
 
   url "https://files.gyazo.com/setup/Gyazo_#{version}.dmg"
+  name 'Gyazo'
+  name 'Gyazo GIF'
   homepage 'https://gyazo.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :other
+  tags :vendor => 'Nota'
+
+  zap :delete => [
+    '~/Library/Caches/com.gyazo.gif',
+    '~/Library/Caches/com.gyazo.mac',
+    '~/Library/Preferences/com.gyazo.gif.plist',
+    '~/Library/Preferences/com.gyazo.mac.plist'
+  ]
 
   app 'Gyazo.app'
   app 'Gyazo GIF.app'
