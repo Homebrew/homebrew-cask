7d02c0be3743f88d6525c5434c8f883dc6be8632
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-14 23:07:42 +0200

    Add name, license and zap for NetLogo.app

--- Casks/netlogo.rb
+++ Casks/netlogo.rb
@@ -3,8 +3,14 @@ cask :v1 => 'netlogo' do
   sha256 'e2c56ba16fedba36b9868321c774fa1a5e4f4a2ec0a5268381488f376a4f4d3e'
 
   url "https://ccl.northwestern.edu/netlogo/#{version}/NetLogo%20#{version}.dmg"
+  name 'NetLogo'
   homepage 'http://ccl.northwestern.edu/netlogo/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gpl
+
+  zap :delete => [
+    '~/Library/Preferences/org.nlogo.netlogo.plist',
+    '~/Library/Saved Application State/org.nlogo.NetLogo.savedState'
+  ]
 
   app "NetLogo #{version}/NetLogo #{version}.app"
 end
