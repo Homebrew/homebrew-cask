e8f917ed738bb22895a0655d88a22a8247dea69e
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:51:18 +0100

    happygrep.rb: added github appcast

--- Casks/happygrep.rb
+++ Casks/happygrep.rb
@@ -3,6 +3,7 @@ cask :v1 => 'happygrep' do
   sha256 '05c5f33142c9ea4559b20ca421c76fa0b081ae3edc0d6e3b8f7c3dd8ba21a518'
 
   url "https://github.com/happypeter/happygrep/releases/download/v#{version}/happygrep.zip"
+  appcast 'https://github.com/happypeter/happygrep/releases.atom'
   homepage 'https://github.com/happypeter/happygrep'
   license :mit
 
