bad044403b5535cb03a0eae340f72a6c470b43a7
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:54:26 +0100

    provisionql.rb: added github appcast

--- Casks/provisionql.rb
+++ Casks/provisionql.rb
@@ -3,6 +3,7 @@ cask :v1 => 'provisionql' do
   sha256 '463d66986316ceadbca6acbd58e1381d26f6a80d464532342678ac46ec2492c0'
 
   url "https://github.com/ealeksandrov/ProvisionQL/releases/download/#{version}/ProvisionQL.zip"
+  appcast 'https://github.com/ealeksandrov/ProvisionQL/releases.atom'
   homepage 'https://github.com/ealeksandrov/ProvisionQL'
   license :mit
 
