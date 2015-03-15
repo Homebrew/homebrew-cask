5baf6abd31d9e867d1c30a1436639000104e02a0
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:51:48 +0100

    lunchy.rb: added github appcast

--- Casks/lunchy.rb
+++ Casks/lunchy.rb
@@ -3,6 +3,7 @@ cask :v1 => 'lunchy' do
   sha256 '13afd65921d3251dbd5f6c44bdf6f6a037d10228a69876c658470a23fc573f7f'
 
   url "https://github.com/sosedoff/lunchy-go/releases/download/v#{version}/#{version}_darwin_amd64.zip"
+  appcast 'https://github.com/sosedoff/lunchy-go/releases.atom'
   homepage 'https://github.com/sosedoff/lunchy-go'
   license :mit
 
