b12f1bd16797d6382f131386d32582eb35e4a5ee
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:54:32 +0100

    qlgradle.rb: added github appcast

--- Casks/qlgradle.rb
+++ Casks/qlgradle.rb
@@ -3,6 +3,7 @@ cask :v1 => 'qlgradle' do
   sha256 'ea76846953ecfbd180d65167ec31cb7c316030f500a6669cd79857c03b951b63'
 
   url "https://github.com/Urucas/QLGradle/releases/download/#{version}/QLGradle.qlgenerator.zip"
+  appcast 'https://github.com/Urucas/QLGradle/releases.atom'
   name 'qlgradle'
   homepage 'https://github.com/Urucas/QLGradle'
   license :mit
