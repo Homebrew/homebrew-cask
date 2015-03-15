ff96f83b3d47a229c2853a7b1f4985054850cba1
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:55:03 +0100

    sidestep.rb: added github appcast

--- Casks/sidestep.rb
+++ Casks/sidestep.rb
@@ -4,8 +4,7 @@ cask :v1 => 'sidestep' do
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/chetan51/sidestep/releases/download/#{version}/Sidestep.zip"
-  appcast 'http://chetansurpur.com/projects/sidestep/appcast.xml',
-          :sha256 => '1d043f565c44c03f6ab5e7c90f025aba3cf480f1623a8a530f87206fab12f86a'
+  appcast 'https://github.com/chetan51/sidestep/releases.atom'
   homepage 'http://chetansurpur.com/projects/sidestep/'
   license :mit
 
