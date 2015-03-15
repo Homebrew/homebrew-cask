95dce7a281ed14cacc432556a0234dbc1ac22539
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:50:56 +0100

    gcollazo-mongodb.rb: added github appcast

--- Casks/gcollazo-mongodb.rb
+++ Casks/gcollazo-mongodb.rb
@@ -4,6 +4,7 @@ cask :v1 => 'gcollazo-mongodb' do
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/gcollazo/mongodbapp/releases/download/#{version}/MongoDB.zip"
+  appcast 'https://github.com/gcollazo/mongodbapp/releases.atom'
   homepage 'http://elweb.co/mongodb-app/'
   license :mit
 
