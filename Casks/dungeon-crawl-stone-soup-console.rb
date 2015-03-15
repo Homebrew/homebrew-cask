bb5a7840f08d86ccca088f82bccebe1ae2e609dc
Marc Bernstein <marc.bernstein@hudl.com>
2015-03-14 20:02:52 -0700

    Fix download path

--- Casks/dungeon-crawl-stone-soup-console.rb
+++ Casks/dungeon-crawl-stone-soup-console.rb
@@ -2,7 +2,7 @@ cask :v1 => 'dungeon-crawl-stone-soup-console' do
   version '0.16.0'
   sha256 '3b4750f6bedb4b7e6d0279f476b3cb55124f93871eecb1c53ebda2621ce51b43'
 
-  url "https://crawl.develz.org/release/stone_soup-#{version}-console-macosx.zip"
+  url "https://crawl.develz.org/release/stone_soup-#{version}-console-macos.zip"
   name 'Dungeon Crawl Stone Soup'
   homepage 'http://crawl.develz.org'
   license :gpl
--- Casks/dungeon-crawl-stone-soup-tiles.rb
+++ Casks/dungeon-crawl-stone-soup-tiles.rb
@@ -2,7 +2,7 @@ cask :v1 => 'dungeon-crawl-stone-soup-tiles' do
   version '0.16.0'
   sha256 '1e75166bedce080c74dc64c2389d5487e21f271251e7aab6916774c99529e01f'
 
-  url "https://crawl.develz.org/release/stone_soup-#{version}-tiles-macosx.zip"
+  url "https://crawl.develz.org/release/stone_soup-#{version}-tiles-macos.zip"
   homepage 'http://crawl.develz.org'
   license :gpl
 
