4ae8b221c4e2671b01fbbccab3ce2de9c7c46d41
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:55:58 +0100

    vagrant-bar.rb: added github appcast

--- Casks/vagrant-bar.rb
+++ Casks/vagrant-bar.rb
@@ -3,6 +3,7 @@ cask :v1 => 'vagrant-bar' do
   sha256 'f5c58690960269be9a7a2597a0aad098b4bd1676c9494f6022e9c7d1e82d81bc'
 
   url "https://github.com/BipSync/VagrantBar/releases/download/#{version}/Vagrant.Bar.zip"
+  appcast 'https://github.com/BipSync/VagrantBar/releases.atom'
   homepage 'https://github.com/BipSync/VagrantBar'
   license :oss
 
