e7310a6d25b8040ffbf7a51ddb93a9dd5daf770e
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:47:27 +0100

    butter.rb: added github appcast

--- Casks/butter.rb
+++ Casks/butter.rb
@@ -3,6 +3,7 @@ cask :v1 => 'butter' do
   sha256 '32c9c20453bb4702e10f4d51e93b316eb0f9d4cc57224c526cc5050346913099'
 
   url "https://github.com/harukasan/butter/releases/download/v#{version}/Butter_#{version}.dmg"
+  appcast 'https://github.com/harukasan/butter/releases.atom'
   name 'Butter'
   homepage 'https://github.com/harukasan/butter'
   license :mit
