33b68ae5d51b2a5646c9f3d553e8debb95210031
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:54:30 +0100

    putio-adder.rb: added github appcast

--- Casks/putio-adder.rb
+++ Casks/putio-adder.rb
@@ -3,6 +3,7 @@ cask :v1 => 'putio-adder' do
   sha256 'f0ed3f2a8c4bd5ab3d83dcef72403785c72f990692190de58f1f7d3edf85c0a3'
 
   url "https://github.com/nicoSWD/put.io-adder/releases/download/v#{version}/put.io-adder-v#{version}.app.zip"
+  appcast 'https://github.com/nicoSWD/put.io-adder/releases.atom'
   name 'Put.IO Adder'
   homepage 'https://github.com/nicoSWD/put.io-adder'
   license :mit
