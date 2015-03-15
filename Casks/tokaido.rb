645f937452c94151dc873bbf80f360949c9d6c21
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:55:47 +0100

    tokaido.rb: added github appcast

--- Casks/tokaido.rb
+++ Casks/tokaido.rb
@@ -3,6 +3,7 @@ cask :v1 => 'tokaido' do
   sha256 '72752b8e4e9f0e554661d8fb58879a82cac6dc5d19762367c89ca191f497d258'
 
   url "https://github.com/tokaido/tokaidoapp/releases/download/v#{version}/Tokaido.zip"
+  appcast 'https://github.com/tokaido/tokaidoapp/releases.atom'
   name 'Tokaido'
   homepage 'https://github.com/tokaido/tokaidoapp'
   license :mit
