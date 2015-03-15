3ec2d912378cf725117de90527db845bcf78ee20
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:51:02 +0100

    gitifier.rb: added github appcast

--- Casks/gitifier.rb
+++ Casks/gitifier.rb
@@ -3,8 +3,7 @@ cask :v1 => 'gitifier' do
   sha256 '59068f250654d0d1588c544f34148bd81a8855b78f868fca8ae49dd3d24d1ee4'
 
   url "https://github.com/jsuder/Gitifier/releases/download/#{version}/Gitifier-#{version}.zip"
-  appcast 'http://sparkle.psionides.eu/feed/gitifier',
-          :sha256 => '5bf8682d28e0a59966f8107efbca6be05b60a32252da7288952b35c3fb4e269b'
+  appcast 'https://github.com/jsuder/Gitifier/releases.atom'
   name 'Gitifier'
   homepage 'http://psionides.github.io/Gitifier/'
   license :eclipse
