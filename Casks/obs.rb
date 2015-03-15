856f241448d54cdcaa574d629352bef745a4d474
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:53:59 +0100

    obs.rb: added github appcast

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -4,6 +4,7 @@ cask :v1 => 'obs' do
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/jp9000/obs-studio/releases/download/#{version}/obs-#{version}-installer.dmg"
+  appcast 'https://github.com/jp9000/obs-studio/releases.atom'
   homepage 'http://obsproject.com/'
   license :gpl
 
