796a7df571f273559f2a25802032b2a646b40be6
Vítor Galvão <info@vitorgalvao.com>
2015-03-15 12:50:38 +0100

    d235j-xbox360-controller-driver.rb: added github appcast

--- Casks/d235j-xbox360-controller-driver.rb
+++ Casks/d235j-xbox360-controller-driver.rb
@@ -3,6 +3,7 @@ cask :v1 => 'd235j-xbox360-controller-driver' do
   sha256 'a7efe48fae89aa592e904d1dc3ba9a73b2d644362eac88feb5e73a774e3fcf88'
 
   url "https://github.com/d235j/360Controller/releases/download/v#{version}-unofficial/360ControllerInstall_#{version}_unofficial.dmg"
+  appcast 'https://github.com/d235j/360Controller/releases.atom'
   homepage 'https://github.com/d235j/360Controller'
   license :gpl
 
