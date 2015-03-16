430aaee49003b6a1a6d8579f5c28d5dbadbb1faf
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:40:52 +0100

    latexit.rb: added name

--- Casks/latexit.rb
+++ Casks/latexit.rb
@@ -5,6 +5,7 @@ cask :v1 => 'latexit' do
   url "http://www.chachatelier.fr/latexit/downloads/LaTeXiT-#{version.gsub('.', '_')}.dmg"
   appcast 'http://pierre.chachatelier.fr/latexit/downloads/latexit-sparkle-en.rss',
           :sha256 => 'bc1bd88bf1d7a9770f0527652db2fc082214240a9b66684d9a95a0beaf2f260a'
+  name 'LaTeXiT'
   homepage 'http://www.chachatelier.fr/latexit'
   license :oss
 
