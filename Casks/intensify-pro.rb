7777066a3106c4f33f9ba980e4d98f8f18645d8b
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:40:35 +0100

    intensify-pro.rb: added name

--- Casks/intensify-pro.rb
+++ Casks/intensify-pro.rb
@@ -3,7 +3,8 @@ cask :v1 => 'intensify-pro' do
   sha256 'd02fc658bc422bd3b60d9c69d7bb2868dd2abc9585ab1ae4832f620f2e49979f'
 
   url "http://cdn.macphun.com/updates/IntensifyPro/IntensifyPro_Distribution_v#{version.gsub('.','_')}.zip"
-  homepage 'https://macphun.com/intensify'
+  name 'Intensify Pro'
+  homepage 'http://macphun.com/intensify'
   license :commercial
 
   app 'Intensify Pro.app'
