acecc1cf18903fcfe8543c9d2854434330701c78
Amory Meltzer <Amorymeltzer@gmail.com>
2015-03-16 14:38:51 -0700

    Update slimboat license from unknown to gratis
    Freeware (see http://www.slimboat.com/en/webhelp/index.htm), can't modify, translate, reverse program, decompile, etc.

--- Casks/slimboat.rb
+++ Casks/slimboat.rb
@@ -5,7 +5,7 @@ cask :v1 => 'slimboat' do
   url 'http://www.slimboat.com/release/slimboat.dmg'
   name 'SlimBoat'
   homepage 'http://www.slimboat.com'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'SlimBoat.app'
 end
