2e71b4e4eaff634578a08028a03b8d2485339368
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:19:10 +0100

    opacity.rb: added name

--- Casks/opacity.rb
+++ Casks/opacity.rb
@@ -3,6 +3,7 @@ cask :v1 => 'opacity' do
   sha256 :no_check
 
   url 'http://downloads.likethought.com/opacity.zip'
+  name 'Opacity'
   homepage 'http://likethought.com/opacity/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
