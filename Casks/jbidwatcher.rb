91d8672cc9d2b775c45a88ad5e10f234fb9091a7
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:21:06 +0100

    jbidwatcher.rb: added name

--- Casks/jbidwatcher.rb
+++ Casks/jbidwatcher.rb
@@ -3,6 +3,7 @@ cask :v1 => 'jbidwatcher' do
   sha256 '659de073d9e0d71fa86e21db1524b7df57b36ff7826600a01ed93da6df5f5c9f'
 
   url "https://www.jbidwatcher.com/download/JBidwatcher-#{version}.dmg"
+  name 'JBidwatcher'
   homepage 'http://www.jbidwatcher.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
