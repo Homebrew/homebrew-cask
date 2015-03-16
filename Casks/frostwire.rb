ceaae09af33a50e754c696e551a98d37eaee4a54
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:37:10 +0100

    frostwire.rb: added name

--- Casks/frostwire.rb
+++ Casks/frostwire.rb
@@ -3,6 +3,7 @@ cask :v1 => 'frostwire' do
   sha256 'c559d214576684a97cee0bea15c91caf8dd2c3609cc498039813724ea475642e'
 
   url "http://dl.frostwire.com/frostwire/#{version}/frostwire-#{version}.dmg"
+  name 'FrostWire'
   homepage 'http://www.frostwire.com'
   license :gpl
 
