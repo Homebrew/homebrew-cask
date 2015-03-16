b507147ba8556d33b338f5a88b59ba6b004294ee
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:20:34 +0100

    zipeg.rb: added name

--- Casks/zipeg.rb
+++ Casks/zipeg.rb
@@ -3,6 +3,7 @@ cask :v1 => 'zipeg' do
   sha256 :no_check
 
   url 'http://www.zipeg.net/downloads/zipeg_mac.dmg'
+  name 'Zipeg'
   homepage 'http://www.zipeg.net/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
