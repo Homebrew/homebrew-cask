b33f5119944cf03d4f7b8fc68c1166726ae873f2
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:34:14 +0100

    voodoopad.rb: added name

--- Casks/voodoopad.rb
+++ Casks/voodoopad.rb
@@ -3,6 +3,7 @@ cask :v1 => 'voodoopad' do
   sha256 :no_check
 
   url 'https://plausible.coop/static/download/VoodooPad.zip'
+  name 'VoodooPad'
   homepage 'https://plausible.coop/voodoopad/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
