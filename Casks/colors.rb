16172559a5e272ec71cac56218a435dc4a525ad0
Vítor Galvão <info@vitorgalvao.com>
2015-03-17 00:07:53 +0100

    updated colors (1.10)

--- Casks/colors.rb
+++ Casks/colors.rb
@@ -1,9 +1,10 @@
 cask :v1 => 'colors' do
-  version '1.9'
-  sha256 '60c8b53d0030624677dc500fd235e5ef41ba3b8a50d4d3ab68a8fba34de84e2c'
+  version '1.10'
+  sha256 '073f3055613b4f57e4f6a1acc8540ad8eca316e61a5202fa72f055928d83d600'
 
-  url "https://mattpatenaude.com/software/colors-#{version}.zip"
-  homepage 'http://mattpatenaude.com/'
+  # googlecode.com is the official download host per the vendor homepage
+  url "http://tmitter.googlecode.com/files/Colors-#{version}.zip"
+  homepage 'https://github.com/13bold/Colors/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Colors.app'
