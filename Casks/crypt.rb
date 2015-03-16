b4288dc1782d106c668cb68a08814ac8a780b7b6
Amory Meltzer <Amorymeltzer@gmail.com>
2015-03-16 14:10:31 -0700

    Update crypt license from unknown to mit
    https://github.com/Voluntarynet/Crypt/blob/master/LICENSE

--- Casks/crypt.rb
+++ Casks/crypt.rb
@@ -6,7 +6,7 @@ cask :v1 => 'crypt' do
   url "http://voluntary.net.s3.amazonaws.com/Crypt#{version}_20100429.zip"
   name 'Crypt'
   homepage 'http://voluntary.net/crypt/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :mit
 
   app "Crypt#{version}.app"
 end
