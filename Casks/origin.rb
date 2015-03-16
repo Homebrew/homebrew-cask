6cb7fff252f0fa5f61a66e65e913bc18d478ef30
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:22:03 +0100

    origin.rb: added name

--- Casks/origin.rb
+++ Casks/origin.rb
@@ -4,6 +4,7 @@ cask :v1 => 'origin' do
 
   # akamaihd.net is the official download host per the vendor homepage
   url 'https://eaassets-a.akamaihd.net/Origin-Client-Download/origin/mac/Origin.dmg'
+  name 'Origin'
   homepage 'http://origin.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
