47ca2c0f00cc85853238a1dbfbddabc647385239
Vítor Galvão <info@vitorgalvao.com>
2015-03-16 01:37:44 +0100

    jedit.rb: added name

--- Casks/jedit.rb
+++ Casks/jedit.rb
@@ -4,6 +4,7 @@ cask :v1 => 'jedit' do
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/jedit/jedit#{version}install.dmg"
+  name 'jEdit'
   homepage 'http://www.jedit.org'
   license :gpl
 
