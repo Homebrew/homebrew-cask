10446d71525e4ddb2178cb468eb4feaad2f1cb51
Robert Brown <robert@slan124.info>
2015-03-17 21:09:41 +0000

    Update Vienna.app to version 3.0.4

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'vienna' do
-  version '3.0.3'
-  sha256 'c575b069ba0fa504891c104a9652b427c559d798ceff83109eb2b86e14015c76'
+  version '3.0.4'
+  sha256 'd66a2d2f4109c8c7a882c4c7f90c01196f4d89a849742b9ed5363a1077060fa3'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/vienna-rss/Vienna#{version}.tgz"
   appcast 'http://vienna-rss.org/changelog.xml',
-          :sha256 => '7ea4ef75c789e1ba1be0d977b5a6d20e8331dba5e6ae34339412cb71670955a6'
+          :sha256 => '713e3a705ec2910fa45cab395df5d6780bf41c4b1bd0ea87bfb17b150ede2c82'
   name 'Vienna'
   homepage 'http://www.vienna-rss.org'
   license :apache
