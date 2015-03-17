c726ac65e9677ece182142ea70a607a64ac08e58
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-03-17 17:23:10 +0100

    upgrading owncloud

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,6 +1,7 @@
 cask :v1 => 'owncloud' do
   version '1.8.0.2139'
   sha256 'd0c68f3c29e9f9c05629e6eef87b826b6e64f10360964ede081257f4be0a4892'
+
   url "https://download.owncloud.com/desktop/stable/ownCloud-#{version}.pkg"
   name 'ownCloud'
   homepage 'http://owncloud.com'
