efa77a78fb5537eae7c4b964e3fecfcc5e301b38
ndr <ndr@qef.io>
2015-03-18 18:03:08 +0000

    purescript: update to 0.6.9.3

--- Casks/purescript.rb
+++ Casks/purescript.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'purescript' do
-  version '0.6.9'
-  sha256 '38261d62c9e632e4b24f8d62ef0be7569b4db43a1365aa8e06783ba1e1d8de53'
+  version '0.6.9.3'
+  sha256 '9dfa8d93f9000718e8c77d0e5abd1a353a47b12acae8482626f22ece43e1dede'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/purescript/purescript/releases/download/v#{version}/macos.tar.gz"
