a2d7997b11a7993616f0a284d27b7600f84173df
André de Jager <andrethehunter@gmail.com>
2015-03-20 09:14:19 +0000

    Updated iojs to 1.6.1

--- Casks/iojs.rb
+++ Casks/iojs.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'iojs' do
-  version '1.6.0'
-  sha256 'd1e47121d1434ded8949b393fe53c191c256e67a072dcc9c492577f24611b75b'
+  version '1.6.1'
+  sha256 '76dc162dc632b43daaedd0e1339f2f000f7cfe0c8a307b86427c1cc8f5b4f0c6'
 
   url "https://iojs.org/dist/v#{version}/iojs-v#{version}.pkg"
   name 'io.js'
