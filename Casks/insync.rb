c296a12d2a0c0777f82702eb4503b3113ea677c6
Mark Steve Samson <hello@marksteve.com>
2015-03-21 15:29:49 +0800

    Update license type and version of insync to v1.2.35090

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'insync' do
-  version '1.0.37.31804'
-  sha256 '782c66bab03e1edf9235b88aed68680e63116242e696c9d1016800dc9ffbe92c'
+  version '1.2.3.35090'
+  sha256 '4200a514c7bdec87182f61b8bff72accc3a635a81aeaf7088d0a5a493c87ff28'
 
   url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
   name 'Insync'
-  homepage 'https://insynchq.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  homepage 'https://www.insynchq.com/'
+  license :closed
 
   app 'Insync.app'
 end
