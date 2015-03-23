0994610ed75a58a1a9e38d90e8414de87f0472c0
mrsim0ns <chris@simonsmail.net>
2015-03-22 17:07:34 -0700

    Added Structurer 1.2

---
+++ Casks/structurer.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'structurer' do
+  version '1.2'
+  sha256 '3238e9730b3d7c570c981a0172ae67ed055139021c17ddba0a507a0f03243895'
+
+  url "http://cdn.tutsplus.com/net/uploads/legacy/892_structurer/Structurer.zip"
+  name 'Structurer'
+  homepage 'http://code.tutsplus.com/articles/free-mac-utility-app-structurer--net-17153'
+  license :gratis
+
+  app 'Structurer.app'
+end
