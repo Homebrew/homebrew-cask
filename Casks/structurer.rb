2610a3bdb7ae0fd42fc4117634a455cd8be6a1f7
Chris <mrsim0ns@users.noreply.github.com>
2015-03-23 17:50:12 -0700

    Updated Structurer to :latest :no_check, fixed url quotes

--- Casks/structurer.rb
+++ Casks/structurer.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'structurer' do
-  version '1.2'
-  sha256 '3238e9730b3d7c570c981a0172ae67ed055139021c17ddba0a507a0f03243895'
+  version :latest
+  sha256 :no_check
 
-  url "http://cdn.tutsplus.com/net/uploads/legacy/892_structurer/Structurer.zip"
+  url 'http://cdn.tutsplus.com/net/uploads/legacy/892_structurer/Structurer.zip'
   name 'Structurer'
   homepage 'http://code.tutsplus.com/articles/free-mac-utility-app-structurer--net-17153'
   license :gratis
