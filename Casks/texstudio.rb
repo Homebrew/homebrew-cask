48b8853ea4143281248bdbf80102e839586cb3dd
Mike Wilmer <mwilmer@gmail.com>
2015-03-24 11:57:28 +0100

    Upgrade TeXstudio.app to v2.9.4
    Upgraded version number and checksum to reflect version 2.9.4

--- Casks/texstudio.rb
+++ Casks/texstudio.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'texstudio' do
-  version '2.8.8'
-  sha256 'cd9fd18b0147f43db64af0bb9bb468c9532a8967aa64d1b0aaa09ea98980e91e'
+  version '2.9.4'
+  sha256 '1ce348f00310c4091d27b292081186626f1d186a4f529f1e2685ad2e03e7906c'
 
   url "http://downloads.sourceforge.net/sourceforge/texstudio/texstudio_#{version}_osx_qt5.zip"
   homepage 'http://texstudio.sourceforge.net/'
