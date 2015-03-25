33a64c434625fdeb41434247a3dcfc8ca9254c13
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-25 05:02:09 +0200

    catchmouse.rb: add name

--- Casks/catchmouse.rb
+++ Casks/catchmouse.rb
@@ -3,6 +3,7 @@ cask :v1 => 'catchmouse' do
   sha256 :no_check
 
   url 'http://ftnew.com/files/CatchMouse.zip'
+  name 'CatchMouse'
   homepage 'http://ftnew.com/catchmouse.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
