f01151d4d082fa79ec33c4883dfc40e793fc20c3
Pfeil <der.andreas.pfeil@gmail.com>
2015-03-25 20:46:32 +0100

    Upgrade Musescore.app to v2.0
    Closes #10226.

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'musescore' do
-  version '1.3'
-  sha256 'fcd106ec700f14053c9b4f3fd411d2335915c040f9071ea6da8d109e6827c3a5'
+  version '2.0.0'
+  sha256 'b5db0b39b6d204e997d5fb4dfcb8dabe40a0a37d08964dfc6c0df52b8a578238'
 
-  # osuosl.org is the official download host per the vendor homepage
-  url "http://ftp.osuosl.org/pub/musescore/releases/MuseScore-#{version}/MuseScore-#{version}.dmg"
+  # osuosl.org is the official download host per the vendor homepage.
+  url "http://ftp.osuosl.org/pub/musescore/releases/MuseScore-#{version.sub(%r{\.\d+},'')}/MuseScore-#{version}.dmg"
   name 'MuseScore'
   homepage 'http://musescore.org/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gpl
 
-  app 'MuseScore.app'
+  app 'MuseScore 2.app'
 end
