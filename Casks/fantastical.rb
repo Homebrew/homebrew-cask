65a3c0dde65b6e9dbd4e06ac951888b0f3bc1334
Paul Hinze <phinze@phinze.com>
2015-03-25 09:45:58 -0500

    fantastical: change 2.0 from addition to upgrade
    If anybody is interested in keeping the old Fantastical they're welcome
    to add the 1.x series to the -versions repo.
    
    Refs #10223

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,15 +1,15 @@
 cask :v1 => 'fantastical' do
-  version '1.3.18'
-  sha256 '00069dca92be66b04351cb9b19146c305696fb5970b33e71c1b2d687bbc5f2a8'
+  version '2.0'
+  sha256 '002cac160c3d1dc4a0ecccdc8215ba0b7a4cffd7ec2aff3b61bba8598d09bf95'
 
   url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
-  appcast 'https://flexibits.com/fantastical/appcast.php',
-          :sha256 => '3d789ff8dd91c8ebc1b63d2811ec17e0a950433b03605d8c10eef010733cf79a'
+  appcast 'https://flexibits.com/fantastical/appcast2.php',
+          :sha256 => '25b925b46633c66b6d10dda0f213614da63da9c80a231dc39c8ffb86e8b883e4'
   name 'Fantastical'
   homepage 'http://flexibits.com/fantastical'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
 
-  app 'Fantastical.app'
+  app 'Fantastical 2.app'
 
   postflight do
     suppress_move_to_applications
