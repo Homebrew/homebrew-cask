c9873dc8fb32cd115828e7aed201f0bb2672d3bd
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-25 05:16:38 +0200

    cloudup.rb: add name

--- Casks/cloudup.rb
+++ Casks/cloudup.rb
@@ -3,6 +3,7 @@ cask :v1 => 'cloudup' do
   sha256 :no_check
 
   url 'https://updates.cloudup.com/update?os=osx&app=Cloudup&format=zip&channel=release'
+  name 'Cloudup'
   homepage 'https://cloudup.com/download'
   license :freemium
 
