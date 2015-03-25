04d76946e1fb7dca132f57aade26df170f2035d1
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-25 05:11:15 +0200

    clarify.rb: add name

--- Casks/clarify.rb
+++ Casks/clarify.rb
@@ -3,6 +3,7 @@ cask :v1 => 'clarify' do
   sha256 :no_check
 
   url 'http://www.clarify-it.com/download/mac/Clarify.dmg'
+  name 'Clarify'
   homepage 'http://www.clarify-it.com/'
   license :commercial
 
