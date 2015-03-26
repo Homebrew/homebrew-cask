f652d031cbb25091df12abc431ce52faf0313739
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-26 05:27:10 +0200

    couchbase-server-community.rb: add name

--- Casks/couchbase-server-community.rb
+++ Casks/couchbase-server-community.rb
@@ -3,6 +3,7 @@ cask :v1 => 'couchbase-server-community' do
   sha256 '6693176f6799ce43f71b9fa1f09af8ff6857309382e0d115a9a2253474e5a31a'
 
   url "http://packages.couchbase.com/releases/#{version}/couchbase-server-community_#{version}-macos_x86_64.zip"
+  name 'Couchbase Server'
   homepage 'http://www.couchbase.com/'
   license :apache
 
