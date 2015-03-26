4e98dffba644eb63a198cc5408d7d0c2856a9b90
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-26 05:29:55 +0200

    cuppa.rb: add name

--- Casks/cuppa.rb
+++ Casks/cuppa.rb
@@ -4,6 +4,7 @@ cask :v1 => 'cuppa' do
 
   url 'http://www.nathanatos.com/software/downloads/Cuppa.zip'
   appcast 'http://www.nathanatos.com/software/cuppa.xml'
+  name 'Cuppa'
   homepage 'http://www.nathanatos.com/software'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
