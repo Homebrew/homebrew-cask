5e6ab8c3fe3960950d0155af03d018a01a7e9e5b
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-26 05:29:29 +0200

    ctivo.rb: add name

--- Casks/ctivo.rb
+++ Casks/ctivo.rb
@@ -4,6 +4,7 @@ cask :v1 => 'ctivo' do
 
   url "https://github.com/dscottbuch/cTiVo/releases/download/v#{version}.468/cTiVo_#{version}_468.zip"
   appcast 'https://github.com/dscottbuch/cTiVo/releases.atom'
+  name 'cTiVo'
   homepage 'https://github.com/dscottbuch/cTiVo'
   license :oss
 
