0122575af9c34deab0d7ceefdc1417c38c62dfd9
Anthony Sterling <mail@anthonysterling.com>
2015-03-26 20:36:09 +0000

    Updated Komodo IDE and Edit to version 9

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'komodo-edit' do
-  version '8.5.4-14424'
-  sha256 '424558813e2926386e9d1de2bf93eb772a9b61b2f90688efc13e073d0d1b452e'
+  version '9.0.0-15707'
+  sha256 '1937614d15bfe81e1488e9e67a26fb12e86870c1edafc77d1cb72df151febf85'
 
   # activestate.com is the official download host per the vendor homepage
   url "http://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*},'')}/Komodo-Edit-#{version}-macosx-x86_64.dmg"
   homepage 'http://komodoide.com/komodo-edit'
   license :mpl
 
-  app 'Komodo Edit 8.app'
+  app 'Komodo Edit 9.app'
 end
