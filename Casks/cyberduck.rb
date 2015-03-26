2d89f0ff7ff00f7ad961b81f8ed50cd050f3613c
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-26 05:32:24 +0200

    cyberduck.rb: add name

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -5,6 +5,7 @@ cask :v1 => 'cyberduck' do
   url "https://update.cyberduck.io/Cyberduck-#{version}.zip"
   appcast 'https://version.cyberduck.io/changelog.rss',
           :sha256 => '8d3c4f2c2b0ec3f83a3230b38b1dfaabb25a05b5a91c4d316a6c4ef0c0fe4de8'
+  name 'Cyberduck'
   homepage 'http://cyberduck.io/'
   license :gpl
 
