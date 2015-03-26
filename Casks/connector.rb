ed1dc2e7b692c914f6ede08fd9bd9569a9921e69
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-26 05:23:58 +0200

    connector.rb: add name

--- Casks/connector.rb
+++ Casks/connector.rb
@@ -3,6 +3,7 @@ cask :v1 => 'connector' do
   sha256 'fa05fc0332ead81a16ad43ce64e67e645f6a73e98b845405bbcd827caec6834f'
 
   url "http://update.mediaware.sk/numpad/connector-#{version}.zip"
+  name 'Connector'
   homepage 'http://mediaware.sk/connector'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
