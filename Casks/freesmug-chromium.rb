1a8552b72a53a1693f8641b4669d5f2529e63ab5
elseym <elseym@me.com>
2015-03-26 16:43:50 +0100

    version bump for freesmug-chromium (41.0.2272.89 to 41.0.2272.101)

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'freesmug-chromium' do
-  version '41.0.2272.89'
-  sha256 '1f2d91c82461c5e00ac30b50f1b75fb5df2c41696643dca3d48cbe4df2e9a29d'
+  version '41.0.2272.101'
+  sha256 'c156b5ac0b64e53609ce3e07545195af379c2c234fc1f8050dd656949abc69a2'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/osxportableapps/Chromium_OSX_#{version}.dmg"
