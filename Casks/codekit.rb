aa6941df33cda28ab691420e8c03c2ea319150b6
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-26 05:19:12 +0200

    codekit.rb: add name

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -5,6 +5,7 @@ cask :v1 => 'codekit' do
   url "http://incident57.com/codekit/files/codekit-#{version.sub(%r{.*-},'')}.zip"
   appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml',
           :sha256 => '433feaa40f89043a2cf591145b8d4392d92abee9b84b258d0a3d99833eff4e39'
+  name 'CodeKit'
   homepage 'http://incident57.com/codekit/'
   license :commercial
 
