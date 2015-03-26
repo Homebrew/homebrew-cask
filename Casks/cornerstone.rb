6aaf628db735386d5b41411f8fae272a08174b7e
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-26 05:26:33 +0200

    cornerstone.rb: add name

--- Casks/cornerstone.rb
+++ Casks/cornerstone.rb
@@ -5,6 +5,7 @@ cask :v1 => 'cornerstone' do
   url "https://www.zennaware.com/cornerstone/downloads/Cornerstone-#{version}.zip"
   appcast 'http://www.zennaware.com/cornerstone/appcast/feed2.php',
           :sha256 => '333fee9fc2c478a0d5305ad2d9faecdb2c44e5554e8d43762c0917a7531fb145'
+  name 'Cornerstone'
   homepage 'http://www.zennaware.com/cornerstone/'
   license :commercial
 
