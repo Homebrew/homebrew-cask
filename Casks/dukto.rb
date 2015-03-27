c77ca9d1a52495c30f6eae871b2fd98d8431b7e1
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-27 19:17:41 +0200

    dukto.rb: add name

--- Casks/dukto.rb
+++ Casks/dukto.rb
@@ -3,6 +3,7 @@ cask :v1 => 'dukto' do
   sha256 '86f06ca92a28b5835cb3eaadfb09897f581a5da3fb8ee8246897b1bf4ee7348a'
 
   url "https://dukto.googlecode.com/files/Dukto#{version}-OSX.dmg"
+  name 'Dukto'
   homepage 'https://code.google.com/p/dukto/'
   license :oss
 
