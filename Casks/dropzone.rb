f5c24e6764d758de247ed0375f54b6ac33e03ed3
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-27 19:15:56 +0200

    dropzone.rb: add name

--- Casks/dropzone.rb
+++ Casks/dropzone.rb
@@ -4,6 +4,7 @@ cask :v1 => 'dropzone' do
 
   url 'https://aptonic.com/dropzone3/latest'
   appcast 'https://aptonic.com/dropzone3/sparkle/updates.xml'
+  name 'Dropzone'
   homepage 'https://aptonic.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
