92299d9e2f08269973853b13a601adddfe626621
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-27 19:19:41 +0200

    dupin.rb: add name

--- Casks/dupin.rb
+++ Casks/dupin.rb
@@ -3,6 +3,7 @@ cask :v1 => 'dupin' do
   sha256 '282116dbbfd75f73efe4655f21f42dd76720e71a8babea54f57a07e600396264'
 
   url "http://dougscripts.com/itunes/scrx/dupinv#{version.to_i}mv.zip"
+  name 'Dupin'
   homepage 'http://dougscripts.com/apps/dupinapp.php'
   license :commercial
 
