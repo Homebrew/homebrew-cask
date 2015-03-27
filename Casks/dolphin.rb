d3ffc18b6aece5acabd577b4a2f4c6e55cf0b762
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-27 19:12:42 +0200

    dolphin.rb: add name

--- Casks/dolphin.rb
+++ Casks/dolphin.rb
@@ -3,6 +3,7 @@ cask :v1 => 'dolphin' do
   sha256 '7d7ddb2bbd2e3255079fee707fe0723b5c5679cf9f21b70dc83feeeb10aa4b4b'
 
   url "http://dl.dolphin-emu.org/builds/dolphin-master-#{version}.dmg"
+  name 'Dolphin'
   homepage 'http://www.dolphin-emu.org/'
   license :gpl
 
