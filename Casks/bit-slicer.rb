1300ce3aef2b7af866bae2db8943cb5391e0053f
Mayur Pawashe <zorgiepoo@gmail.com>
2015-03-28 12:36:46 -0400

    Update Bit Slicer to 1.7.5

--- Casks/bit-slicer.rb
+++ Casks/bit-slicer.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'bit-slicer' do
-  version '1.7.4'
-  sha256 '02766dcb88e21b39b78c91718eff5547904ad121e55b89b5bc11846a0c7e6ed4'
+  version '1.7.5'
+  sha256 '286487687fbd9a46f3d7bfa943a912a3e05183af06ebfaef8f6820253d5f93e7'
 
   # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/zorgiepoo/bit-slicer/downloads/Bit%20Slicer%20#{version}.zip"
   name 'Bit Slicer'
   appcast 'http://zorg.tejat.net/bitslicer/update.php',
-          :sha256 => '11d0afe33a4cbf65a8df4c4d323b150853a62b0cbfe77d36dc70b5113aeca631'
+          :sha256 => '2dca07d414e8888b06c0354fe4b381e91ff0945b381081db8f32bc0563d3b51e'
   homepage 'https://github.com/zorgiepoo/bit-slicer/'
   license :bsd
 
