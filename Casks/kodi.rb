7a366656c8155b2d6080d736dedd31af3e67b4ce
plessbd <plessbd@users.noreply.github.com>
2015-03-28 15:18:44 -0400

    update kodi
    Update to 14.2

--- Casks/kodi.rb
+++ Casks/kodi.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'kodi' do
-  version '14.1'
-  sha256 'cf71db03a6878673df95c7983feac4a6d750c9b798553ea19e80d95ba2f5d6c8'
+  version '14.2'
+  sha256 'b674fd6287315b4cf0ec2a1775a746b51ecbbd782d1c572334cee80ad1ee81ed'
 
   # xbmc.org is the official download host per the vendor homepage
   url "http://mirrors.xbmc.org/releases/osx/x86_64/kodi-#{version}-Helix-x86_64.dmg"
