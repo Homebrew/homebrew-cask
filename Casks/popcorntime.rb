d3e2374306edec8abcd1da2db9519a5ba588df8f
Cástor Rodríguez <castorm@gmail.com>
2015-03-28 10:39:57 +0100

    Add PopcornTime.app Latest

---
+++ Casks/popcorntime.rb
@@ -0,0 +1,19 @@
+cask :v1 => 'popcorntime' do
+  version :latest
+  sha256 :no_check
+ 
+  url "http://popcorn-time.se/PopcornTime-latest.dmg"
+  name 'Popcorn Time'
+  homepage 'http://popcorn-time.se'
+  license :gpl
+ 
+  app 'PopcornTime.app'
+ 
+  depends_on :macos => '>= :lion'
+ 
+  zap :delete => [
+                   '~/Library/Caches/PopcornTime',
+                   '~/Library/Application Support/PopcornTime',
+                   '~/Library/Saved Application State/se.popcorn-time.PopcornTime.savedState'
+                 ]
+end
