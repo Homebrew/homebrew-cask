d7d3bb3e575f277e08341bbafac43a7aaa2c3c41
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-29 19:22:24 +0300

    flickr-uploadr.rb: add name

--- Casks/flickr-uploadr.rb
+++ Casks/flickr-uploadr.rb
@@ -3,6 +3,7 @@ cask :v1 => 'flickr-uploadr' do
   sha256 :no_check
 
   url 'https://downloads.flickr.com/uploadr/FlickrUploadr.dmg'
+  name 'Flickr Uploadr'
   homepage 'http://www.flickr.com/tools/'
   license :gratis
 
