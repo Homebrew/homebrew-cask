185563b39b44d72a8f8c8b2e80e72c63afd4ae37
Chase Southard <chase.southard@gmail.com>
2015-03-29 16:56:29 -0400

    add streakerbar 1.0

---
+++ Casks/streakerbar.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'streakerbar' do
+  version '1.0'
+  sha256 '70c4e5863d1eaaf4c8ed96e1e110ca9f48de811bdffb92beb024827e08947e72'
+
+  url 'http://github.com/chaserx/streakerbar/releases/download/v1.0/streakerbar.zip'
+  name 'streakerbar'
+  homepage 'https://github.com/chaserx/streakerbar'
+  license :mit
+  app 'streakerbar/streakerbar.app'
+end
