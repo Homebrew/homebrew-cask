908af6c6f58c3d83f0f13f728cf6d76b0a398d87
Radek Simko <radek.simko@gmail.com>
2015-03-29 22:14:22 +0100

    jdownloader: Add license

--- Casks/jdownloader.rb
+++ Casks/jdownloader.rb
@@ -5,7 +5,7 @@ cask :v1 => 'jdownloader' do
   url 'http://installer.jdownloader.org/JDownloader09Setup_mac.dmg'
   name 'JDownloader'
   homepage 'http://jdownloader.org/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gpl
 
   installer :script => 'JDownloader Installer.app/Contents/MacOS/JavaApplicationStub',
             :args => [ '-q' ]
