81f4d8e047801a34773948a758b2cb13c267b050
Radek Simko <radek.simko@gmail.com>
2015-03-29 15:44:33 +0100

    blast2go: SHA sum updated

--- Casks/blast2go.rb
+++ Casks/blast2go.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'blast2go' do
   version '3.0'
-  sha256 '7bb741384c7cca3f26e0f1fcc7d3de84f084c8902787c6498a67dbef84ae3053'
+  sha256 '79ca1eadae292c55c0b9df5b6a97b727536ee835b90d8ca4f764eb9954c54c18'
 
   url 'http://download.blast2go.com/html/software/blast2go/latest/3_0/Blast2GO_macos_3_0.dmg'
   name 'Blast2GO'
