25939097583b0f31275b1a47f591d6b13c762a09
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-29 19:25:02 +0300

    folx.rb: add name

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -4,6 +4,7 @@ cask :v1 => 'folx' do
 
   url 'http://mac.eltima.com/download/downloader_mac.dmg'
   appcast 'http://mac.eltima.com/download/folx-update/folx3.xml'
+  name 'Folx'
   homepage 'http://mac.eltima.com/download-manager.html'
   license :freemium
 
