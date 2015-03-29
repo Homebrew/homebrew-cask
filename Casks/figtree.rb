0b677303440d37bbdfe1b913a19d5ec48e61b56d
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-29 19:12:10 +0300

    figtree.rb: add name

--- Casks/figtree.rb
+++ Casks/figtree.rb
@@ -3,6 +3,7 @@ cask :v1 => 'figtree' do
   sha256 '3b97cb9b57bbce2466d850faac699128b7b72310b094cf9260c1a172561674f1'
 
   url 'http://tree.bio.ed.ac.uk/download.php?id=90'
+  name 'FigTree'
   homepage 'http://tree.bio.ed.ac.uk/software/figtree/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
