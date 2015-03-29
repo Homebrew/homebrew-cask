8acf0bb1e6d76f014da2d81a4adb1fe34a87d610
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-29 19:05:24 +0300

    epic-games-launcher.rb: add name

--- Casks/epic-games-launcher.rb
+++ Casks/epic-games-launcher.rb
@@ -3,6 +3,7 @@ cask :v1 => 'epic-games-launcher' do
   sha256 'e04e5b6232df5bd81d4b796f5c4556a55b56a34e0089c9f65f67c2f3b9c830d1'
 
   url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicGamesLauncher-#{version}.dmg"
+  name 'Epic Games Launcher'
   homepage 'https://www.unrealengine.com/what-is-unreal-engine-4'
   license :commercial
   tags :vendor => 'EpicGames'
