d45d86018da6f18a246dbab69a041286b6863eb8
Radek Simko <radek.simko@gmail.com>
2015-03-29 21:16:17 +0100

    jdownloader: (un)installation automated + java caveat added

--- Casks/jdownloader.rb
+++ Casks/jdownloader.rb
@@ -7,5 +7,14 @@ cask :v1 => 'jdownloader' do
   homepage 'http://jdownloader.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  installer :manual => 'JDownloader Installer.app'
+  installer :script => 'JDownloader Installer.app/Contents/MacOS/JavaApplicationStub',
+            :args => [ '-q' ]
+
+  caveats <<-EOS.undent
+    #{token} requires Java 6+, you can install the latest Java using
+
+      brew cask install java
+  EOS
+
+  uninstall :delete => '/Applications/JDownloader.app'
 end
