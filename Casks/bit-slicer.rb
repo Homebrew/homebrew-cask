4496344e10c15567eeed13ea76504e924df1c539
Mayur Pawashe <zorgiepoo@gmail.com>
2015-03-28 21:56:02 -0400

    Update Bit Slicer (quick 1.7.5 bug-fix)
    Had to fix a potential crash on startup.

--- Casks/bit-slicer.rb
+++ Casks/bit-slicer.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'bit-slicer' do
   version '1.7.5'
-  sha256 '286487687fbd9a46f3d7bfa943a912a3e05183af06ebfaef8f6820253d5f93e7'
+  sha256 '1180666794b41ddba4895c6f90ba9e428f49c115f0eb260d76061c6c6aa9f1a9'
 
   # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/zorgiepoo/bit-slicer/downloads/Bit%20Slicer%20#{version}.zip"
   name 'Bit Slicer'
   appcast 'http://zorg.tejat.net/bitslicer/update.php',
-          :sha256 => '2dca07d414e8888b06c0354fe4b381e91ff0945b381081db8f32bc0563d3b51e'
+          :sha256 => 'ad8cdd11fb138f98a29bf24a31d3f82f086e6abf813787f91679ce5ec015239c'
   homepage 'https://github.com/zorgiepoo/bit-slicer/'
   license :bsd
 
