cc848b90b66cf71fbd7eb8f8a8fbc81944e9e2b8
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-30 16:23:46 +0300

    fontexplorer-x-pro.rb: add name

--- Casks/fontexplorer-x-pro.rb
+++ Casks/fontexplorer-x-pro.rb
@@ -3,6 +3,7 @@ cask :v1 => 'fontexplorer-x-pro' do
   sha256 '9fd225ff73804231d094f16bdb435355a3b7557d74ec1aeb9d89e925f0673350'
 
   url "http://fast.fontexplorerx.com/FontExplorerXPro#{version.gsub('.','')}.dmg"
+  name 'FontExplorer X Pro'
   homepage 'http://www.fontexplorerx.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
