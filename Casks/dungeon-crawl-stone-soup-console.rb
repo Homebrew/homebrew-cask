97474268ceda7a6eff52ba60fa7f4246f9027699
Marc Bernstein <marc.bernstein@hudl.com>
2015-03-30 11:19:54 -0700

    Updated Dungeon Crawl Stone Soup from 0.16.0 to 0.16.1 for both the tiles and console versions.

--- Casks/dungeon-crawl-stone-soup-console.rb
+++ Casks/dungeon-crawl-stone-soup-console.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'dungeon-crawl-stone-soup-console' do
-  version '0.16.0'
-  sha256 '3b4750f6bedb4b7e6d0279f476b3cb55124f93871eecb1c53ebda2621ce51b43'
+  version '0.16.1'
+  sha256 '384527e0b0ce8c3df216a577461266d405bb7652d6efcaaf3730c9718590e319'
 
   url "https://crawl.develz.org/release/stone_soup-#{version}-console-macos.zip"
   name 'Dungeon Crawl Stone Soup'
