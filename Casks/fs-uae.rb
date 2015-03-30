d96e85f5e4ddec687a844ed5665ca614e0201f3f
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-30 16:31:48 +0300

    fs-uae.rb: add name

--- Casks/fs-uae.rb
+++ Casks/fs-uae.rb
@@ -3,6 +3,7 @@ cask :v1 => 'fs-uae' do
   sha256 '3e949a673fb44abd8c788aa70361315fb7fb389575966dddee89f66fbda354ca'
 
   url "http://fs-uae.net/fs-uae/stable/#{version}/fs-uae-plus-#{version}-macosx.tar.gz"
+  name 'FS-UAE'
   homepage 'http://fs-uae.net/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
