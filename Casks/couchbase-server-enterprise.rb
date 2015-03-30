2f74eddbadac9e675bd9800e778b14e928361bec
Brian Shumate <brian@couchbase.com>
2015-03-30 15:59:26 -0400

    Couchbase Server Enterprise Edition version 3.0.3

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'couchbase-server-enterprise' do
-  version '3.0.2'
-  sha256 '6129bd810ed8d5a27cc5d25e82b998e90f7b6f283fca8841592e0e842c8578d9'
+  version '3.0.3'
+  sha256 'ee3e42317ea35cea4e2ef3b67c238bc82b9bdcec5b1867a1a160a490cbbf2469'
 
   url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
   name 'Couchbase Server'
