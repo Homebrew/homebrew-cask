09557f2ad741bc46edf0c5993e0868ea4d1a8d99
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-30 16:41:32 +0300

    gifrocket.rb: add name

--- Casks/gifrocket.rb
+++ Casks/gifrocket.rb
@@ -3,6 +3,7 @@ cask :v1 => 'gifrocket' do
   sha256 'c5407c9caad8c038f604d35da9a6554dede79611daaf5174116ea9517a704593'
 
   url "http://www.gifrocket.com/Gifrocket.#{version}.zip"
+  name 'Gifrocket'
   homepage 'http://www.gifrocket.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
