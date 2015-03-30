df153e4a86229f207235088b474f93451809e54a
Alex Alonso <alex4551@gmail.com>
2015-03-30 19:15:19 +0800

    update Itools Casks

--- Casks/itools.rb
+++ Casks/itools.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'itools' do
-  version '2.4.6'
-  sha256 '412dcd5a082ebf88566a4422e4c1d127e063a65ad9d8f3917e0b29e871da6783'
+  version '2.5.0'
+  sha256 '196ad7aec8732ecdc66b0281fe2065eb5abe2ec97fe260f5de4b144a2bd721ab'
 
   # itools.hk is the official download host per the vendor homepage
   url "http://dl2.itools.hk/dl/iTools_#{version}.dmg"
