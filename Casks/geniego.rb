c89dd61be014882bfab364ee705376f9a3910c94
Matt Cowger <matt@cowger.us>
2015-03-31 08:36:59 -0700

    GenieGO - Update stanza order and comment on name change.

--- Casks/geniego.rb
+++ Casks/geniego.rb
@@ -6,9 +6,12 @@ cask :v1 => 'geniego' do
   name 'DIRECTV GenieGO'
   name 'GenieGO'
   name 'Nomad'
-
   homepage 'http://www.directv.com/technology/geniego'
+  license :closed
 
   app 'Nomad.app', :target => 'GenieGO.app'
-  license :closed
+  #This app is renamed because although the filename is Nomas (a reference to the old version of the marketing),
+  #If a user were to actually try to install it manually, it shows up in the GUI as 'GenieGO.app', indicating the
+  #intent of the producer for the actual name visible to the user.  Example screenshot here:
+  # http://i.imgur.com/mfgZke7.png
 end
