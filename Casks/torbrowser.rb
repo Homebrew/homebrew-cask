d647a85097bd0c30ecf4bb3576463b684f1ad4d6
Michael <fanquake@users.noreply.github.com>
2015-03-31 18:10:55 +0800

    Update TorBrowser license to OSS

--- Casks/torbrowser.rb
+++ Casks/torbrowser.rb
@@ -6,7 +6,7 @@ cask :v1 => 'torbrowser' do
   gpg "#{url}.asc",
       :key_id => '4e2c6e8793298290'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :oss
 
   app 'TorBrowser.app'
 end
