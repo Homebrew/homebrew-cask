7880438dba58f75b990eb30fde93be3e63eff89c
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-31 23:16:39 +0300

    google-earth.rb: add name

--- Casks/google-earth.rb
+++ Casks/google-earth.rb
@@ -3,6 +3,7 @@ cask :v1 => 'google-earth' do
   sha256 :no_check
 
   url 'https://dl.google.com/earth/client/advanced/current/GoogleEarthMac-Intel.dmg'
+  name 'Google Earth'
   homepage 'https://www.google.com/earth/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
