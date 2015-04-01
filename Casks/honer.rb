bf2aa0cf31aa0823dd4543dd0f2d70ac9542eee3
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-01 23:16:54 +0300

    honer.rb: add name

--- Casks/honer.rb
+++ Casks/honer.rb
@@ -4,6 +4,7 @@ cask :v1 => 'honer' do
 
   url "https://github.com/puffnfresh/Honer.app/releases/download/v#{version}/Honer-6e3863f2.zip"
   appcast 'https://github.com/puffnfresh/Honer.app/releases.atom'
+  name 'Honer'
   homepage 'https://github.com/puffnfresh/Honer.app'
   license :mit
 
