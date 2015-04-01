c36c8c8a20698d19b7905431bccbbd268199903a
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2015-04-01 18:41:21 +0200

    update praat cask

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'praat' do
-  version '5.4.05'
+  version '5.4.08'
 
   if Hardware::CPU.is_32_bit?
-    sha256 'cae9d2910ec0e99e990798e30dd93778296e1e298c82b5051397eaa34b0a615b'
+    sha256 'c0eff18cbd8966a5a178831a7203d1b4c8c058f8342a0570a9eba03f9ff744b6'
     url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac32.dmg"
   else
-    sha256 '9c9c836cbfaf3a1204bfea7b41531a5bda7a4bbff11ef255530a7e91600eb541'
+    sha256 '6a6d41b00fb44a95885a54885e267cc8244f93b3647d61fa6048aac27a2b4a5d'
     url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac64.dmg"
   end
 
