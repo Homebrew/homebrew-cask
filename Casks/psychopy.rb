ef50447c6227b913515afd8e1ee6d204846a2a7b
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-04-01 12:05:53 +0200

    upgrading psychopy

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'psychopy' do
-  version '1.81.00'
-  sha256 '121a3ee098ea6a978e7888da8de6dc3a096205455518aabf9e31edf21bbe0183'
+  version '1.82.00'
+  sha256 '42f980455815d6dd883d125265ed97cecdf4b366608620dadbf12965f40254ad'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/psychpy/StandalonePsychoPy-#{version}-OSX.dmg"
