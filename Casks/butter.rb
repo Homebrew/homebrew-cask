f32bb54372f24704fc04ce579ea0cfc02aa2da1c
flada-auxv <aseknock@gmail.com>
2015-04-01 17:04:06 +0900

    Update butter to 0.2.0

--- Casks/butter.rb
+++ Casks/butter.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'butter' do
-  version '0.1.5'
-  sha256 '32c9c20453bb4702e10f4d51e93b316eb0f9d4cc57224c526cc5050346913099'
+  version '0.2.0'
+  sha256 '3bcec1396c944bb10d84db1354cd214a2e7f398c746d535ffb0ca503fd0ba686'
 
   url "https://github.com/harukasan/butter/releases/download/v#{version}/Butter_#{version}.dmg"
   appcast 'https://github.com/harukasan/butter/releases.atom'
