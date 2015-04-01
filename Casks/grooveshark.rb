7e481565ffd6d1d32fcc844886a8780d81a83398
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-01 23:11:43 +0300

    grooveshark.rb: add name

--- Casks/grooveshark.rb
+++ Casks/grooveshark.rb
@@ -3,6 +3,7 @@ cask :v1 => 'grooveshark' do
   sha256 :no_check
 
   url 'http://adops-fio.grooveshark.com/desktop-app/downloads/grooveshark.zip'
+  name 'Grooveshark'
   homepage 'http://www.grooveshark.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
