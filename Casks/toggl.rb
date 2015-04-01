9930bcc49601e1f6bc16f9be80c528166b44b38e
Johan Bichel Lindegaard <public@johan.cc>
2015-04-01 00:46:04 -0400

    Update toggl.rb

--- Casks/toggl.rb
+++ Casks/toggl.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'toggl' do
-  version :latest
+  version :no_check
   sha256 '89cbb9d701e5bc612542cdfd35ca182e93ff5c81ddc21c183b2cb867fad84937'
 
   url 'https://toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
