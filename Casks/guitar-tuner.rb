c8ad3da7eb08b991718f65b31754425098bffa31
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-01 23:13:08 +0300

    guitar-tuner.rb: add name

--- Casks/guitar-tuner.rb
+++ Casks/guitar-tuner.rb
@@ -3,6 +3,7 @@ cask :v1 => 'guitar-tuner' do
   sha256 :no_check
 
   url 'http://www.gieson.com/downloads/utilities/guitar_tuner_mac.zip'
+  name 'Guitar Tuner'
   homepage 'http://www.gieson.com/Library/projects/utilities/tuner/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
