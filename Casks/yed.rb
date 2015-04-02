a20e00ffb0ac3870d42dda52183a9aa3ce2e4b7f
Josh Hagins <hagins.josh@gmail.com>
2015-04-02 01:41:53 -0400

    Add name to yed.rb

--- Casks/yed.rb
+++ Casks/yed.rb
@@ -3,6 +3,7 @@ cask :v1 => 'yed' do
   sha256 '1db07a4e108c13ed4e28ebec30d6acf4fe7b593a78ceeeed3e5a1c9d1132c512'
 
   url "https://www.yworks.com/products/yed/demo/yEd-#{version}_with-JRE8.dmg"
+  name 'yEd'
   homepage 'http://www.yworks.com/en/products/yfiles/yed/'
   license :gratis
   tags :vendor => 'yWorks'
