19ccb730e9d2be72a82d43347e6e9106985d4810
Josh Hagins <hagins.josh@gmail.com>
2015-04-02 01:41:53 -0400

    Add name to yandex.rb

--- Casks/yandex.rb
+++ Casks/yandex.rb
@@ -3,6 +3,7 @@ cask :v1 => 'yandex' do
   sha256 :no_check
 
   url 'https://download.cdn.yandex.net/browser/yandex/ru/Yandex.dmg'
+  name 'Yandex.Browser'
   homepage 'http://browser.yandex.com/'
   license :gratis
 
