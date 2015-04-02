5b1bfa09a8d35d2ee875e48cd673e9fe24b133a7
Josh Hagins <hagins.josh@gmail.com>
2015-04-02 01:41:52 -0400

    Add name to xact.rb

--- Casks/xact.rb
+++ Casks/xact.rb
@@ -3,6 +3,7 @@ cask :v1 => 'xact' do
   sha256 'f1c587262b19e171845911ad1231e9295825d3ab592b75a6a571a5e76da8097a'
 
   url "http://xact.scottcbrown.org/xACT#{version}.zip"
+  name 'xACT'
   appcast 'http://xactupdate.scottcbrown.org/xACT.xml',
           :sha256 => 'fc41532e1aff82fbfd45fd7d9a65c5a5d661d536b535d36f3b0c3658b44f8d73'
   homepage 'http://xact.scottcbrown.org'
