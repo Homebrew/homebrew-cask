2c36635125b38c27f7b268228510fb7789b522fc
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-03 01:10:06 +0300

    iboostup.rb: add name

--- Casks/iboostup.rb
+++ Casks/iboostup.rb
@@ -3,6 +3,7 @@ cask :v1 => 'iboostup' do
   sha256 :no_check
 
   url 'http://www.iboostup.com/iboostup.dmg'
+  name 'iBoostUp'
   homepage 'http://www.iboostup.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
