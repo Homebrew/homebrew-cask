6c3e29f7fda3f5d2e5632cc7fc975549cef5b5b7
Josh Hagins <hagins.josh@gmail.com>
2015-04-02 01:41:54 -0400

    Add name to zerobranestudio.rb

--- Casks/zerobranestudio.rb
+++ Casks/zerobranestudio.rb
@@ -3,6 +3,7 @@ cask :v1 => 'zerobranestudio' do
   sha256 'f77ccf80fb3b7ad31c4aae404a3d0883edbfd32741e76175cf5fe5f8149da259'
 
   url "https://download.zerobrane.com/ZeroBraneStudioEduPack-#{version}-macos.dmg"
+  name 'ZeroBrane Studio'
   homepage 'https://studio.zerobrane.com'
   license :mit
 
