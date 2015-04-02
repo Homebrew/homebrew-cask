39a56178fe976e05e14f6b6aee392eefc0783e06
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-03 01:15:55 +0300

    imitone.rb: add name

--- Casks/imitone.rb
+++ Casks/imitone.rb
@@ -3,6 +3,7 @@ cask :v1 => 'imitone' do
   sha256 '86179bdc32364bb85cdeb04d0ec1d8cb8bee4cfc1dd0f15a0656412a1f9c74dc'
 
   url "http://imitone.com/beta/imitone-#{version}.dmg"
+  name 'imitone'
   homepage 'http://imitone.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
