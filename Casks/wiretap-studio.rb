3ce5f56f611501f3bed1c784bf754521bb7008da
Josh Hagins <hagins.josh@gmail.com>
2015-04-02 01:41:51 -0400

    Add name to wiretap-studio.rb

--- Casks/wiretap-studio.rb
+++ Casks/wiretap-studio.rb
@@ -3,6 +3,7 @@ cask :v1 => 'wiretap-studio' do
   sha256 'cfb0eb8b10828153f900d93a920b650cbba8a21d1f7e28bedc0414ccb2c3f8d8'
 
   url 'http://www.ambrosiasw.com/dl/wiretap'
+  name 'WireTap Studio'
   homepage 'http://www.ambrosiasw.com/utilities/wiretap/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
