145a29f2e36e1bf5c6360ed7f3abf36538a0ffd5
Josh Hagins <hagins.josh@gmail.com>
2015-04-02 01:41:54 -0400

    Add name to yubiswitch.rb

--- Casks/yubiswitch.rb
+++ Casks/yubiswitch.rb
@@ -3,6 +3,7 @@ cask :v1 => 'yubiswitch' do
   sha256 '590a89bea366f126044507226163499ae639a6ceb729fa03f93d24250f67066e'
 
   url "https://github.com/pallotron/yubiswitch/releases/download/v#{version}/yubiswitch_#{version}.dmg"
+  name 'yubiswitch'
   appcast 'https://github.com/pallotron/yubiswitch/releases.atom'
   homepage 'https://github.com/pallotron/yubiswitch'
   license :gpl
