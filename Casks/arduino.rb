80d1c931bb9b58e707f46360afcca3e77aa73559
Leo Lou <louyuhong@gmail.com>
2015-04-02 22:55:30 +0800

    Add Arduino 1.6.3

--- Casks/arduino.rb
+++ Casks/arduino.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'arduino' do
-  version '1.6.2'
-  sha256 '7b8ca0a51032c08da53e232bbfb14dd29531fe6cf7620aaf65f93c5e51dc27d6'
+  version '1.6.3'
+  sha256 '4cdb3332ff9a80218e6923db61ca18e346e753e137f111de9fe81b380af88897'
 
   url "http://arduino.cc/download.php?f=/arduino-#{version}-macosx.zip"
   name 'Arduino'
