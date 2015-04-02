794ef2fdb98469f46a6469417d697bba395b0fd4
Josh Hagins <hagins.josh@gmail.com>
2015-04-02 01:41:53 -0400

    Add name to xmplify.rb

--- Casks/xmplify.rb
+++ Casks/xmplify.rb
@@ -3,6 +3,7 @@ cask :v1 => 'xmplify' do
   sha256 :no_check
 
   url 'http://xmplifyapp.com/releases/Xmplify.dmg'
+  name 'Xmplify'
   homepage 'http://xmplifyapp.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
