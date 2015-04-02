1b6f4714c48c0ac46934b006f84ab37f86b0b147
Oliver Conzen <Miladiir@users.noreply.github.com>
2015-04-02 20:11:07 +0200

    Update tinyumbrella.rb

--- Casks/tinyumbrella.rb
+++ Casks/tinyumbrella.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'tinyumbrella' do
-  version '8.2.0.48'
-  sha256 '1de31924e50cbb2038ecdc795e4c01a518e468d45537622fdda63920e3ca2364'
+  version :latest
+  sha256 :no_check
 
-  url "http://blog.firmwareumbrella.com/download/343/"
+  url 'http://blog.firmwareumbrella.com/download/343/'
   homepage 'http://blog.firmwareumbrella.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
