76913b7947b65427df5964be91d14acb88149070
Jeffrey Morgan <jeffdm@users.noreply.github.com>
2015-04-03 10:59:57 -0400

    Update kitematic.rb

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'kitematic' do
-  version '0.5.12'
-  sha256 'a853fee877facde49f0c64e8d38e528c15305b815de3384d3cec06b8ca7ae189'
+  version '0.5.13'
+  sha256 'abcf79d4852956992c5cc94be035df32945bcd1202210b52a1dfe3cc1e4b276a'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}.zip"
