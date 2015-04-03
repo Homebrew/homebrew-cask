3808d8d53071a723eeb103b0edc86df56a44cceb
ndr <ndr@qef.io>
2015-04-03 18:09:21 +0100

    rust: move to 1.0.0 beta

--- Casks/rust.rb
+++ Casks/rust.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'rust' do
-  version :latest
-  sha256 :no_check
+  version '1.0.0'
+  sha256 '90ec7a87041a14fd20af56427912d6c048ed268bde0be607a9d2376feeeb1c2e'
 
-  url 'http://static.rust-lang.org/dist/rust-nightly-x86_64-apple-darwin.pkg'
+  url "https://static.rust-lang.org/dist/rust-#{version}-beta-x86_64-apple-darwin.pkg"
   homepage 'http://www.rust-lang.org/'
   license :oss
 
-  pkg 'rust-nightly-x86_64-apple-darwin.pkg'
+  pkg "rust-#{version}-beta-x86_64-apple-darwin.pkg"
 
   uninstall :pkgutil => 'org.rust-lang.rust'
 end
