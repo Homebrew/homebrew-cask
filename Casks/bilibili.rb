e4b2efc9655afda549a1fa154401cd9290f8993b
Niu Tengyu <typcn@users.noreply.github.com>
2015-04-04 06:15:17 +0800

    Update bilibili
    remove sha check

--- Casks/bilibili.rb
+++ Casks/bilibili.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bilibili' do
-  version '1.2'
-  sha256 '1e98cc0069c487db2dccaa3a80ea3f1a1255cadd0268fed05a3621c6fe000e8a'
+  version :latest
+  sha256 :no_check
 
   url "https://github.com/typcn/bilibili-mac-client/releases/download/#{version}/Bilibili.dmg.zip"
   name 'Bilibili'
