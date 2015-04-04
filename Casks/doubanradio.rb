19d86f25004032cfc04906044b4be3a99f2dd7f4
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-04 06:51:35 +0300

    doubanradio.rb: add name

--- Casks/doubanradio.rb
+++ Casks/doubanradio.rb
@@ -4,6 +4,7 @@ cask :v1 => 'doubanradio' do
 
   url 'http://douban.fm/desktop_mac'
   appcast 'http://douban.fm/static/radio/desktop/doubanradio-appcast.xml'
+  name '豆瓣FM'
   homepage 'http://douban.fm'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
