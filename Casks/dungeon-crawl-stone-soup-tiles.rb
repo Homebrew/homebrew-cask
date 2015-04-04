d3e603e142f2aedf6b94ff603d914f21b6d7f7d2
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-04 06:55:39 +0300

    dungeon-crawl-stone-soup-tiles.rb: add name

--- Casks/dungeon-crawl-stone-soup-tiles.rb
+++ Casks/dungeon-crawl-stone-soup-tiles.rb
@@ -3,6 +3,7 @@ cask :v1 => 'dungeon-crawl-stone-soup-tiles' do
   sha256 '3fb69a2de46b051da86ad7c038ef91b8364a3c79ec8699c6120d2d041fe8bd6a'
 
   url "https://crawl.develz.org/release/stone_soup-#{version}-tiles-macos.zip"
+  name 'Dungeon Crawl Stone Soup'
   homepage 'http://crawl.develz.org'
   license :gpl
 
