a8eecad3b4bbfac830331e4438e021c0c4d13520
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-04 07:25:41 +0300

    iphone-backup-extractor.rb: add name

--- Casks/iphone-backup-extractor.rb
+++ Casks/iphone-backup-extractor.rb
@@ -3,6 +3,7 @@ cask :v1 => 'iphone-backup-extractor' do
   sha256 :no_check
 
   url 'http://supercrazyawesome.com/downloads/iPhone%20Backup%20Extractor.app.zip'
+  name 'iPhone Backup Extractor'
   homepage 'http://supercrazyawesome.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
