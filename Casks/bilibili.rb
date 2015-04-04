cad54a3851324fb35befcf69da5878bb7c218b0e
Niu Tengyu <typcn@users.noreply.github.com>
2015-04-05 05:25:17 +0800

    Update bilibili.rb

--- Casks/bilibili.rb
+++ Casks/bilibili.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bilibili' do
-  version '1.1'
-  sha256 'd1f7bfec4c3ebb7b75252b27a98125be9be57c6dd2780ec3e2af2686fb61034b'
+  version '1.2'
+  sha256 '58bb0c1bbb9c6cad945b077aaead9409941fdf13ad6d73dc61248e8697107e11'
 
   url "https://github.com/typcn/bilibili-mac-client/releases/download/#{version}/Bilibili.dmg.zip"
   name 'Bilibili'
