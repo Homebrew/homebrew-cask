46c2e06287a927d63d35103398a4e9b17dbad971
Sebastian Röder <no-spam-github@sebastianroeder.de>
2015-04-04 18:40:01 +0200

    Clean up Dash formula
    - provide license information
    - switch all URLs to https
    - zap complete `~/Library/Application Support/Dash` folder

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -2,11 +2,11 @@ cask :v1 => 'dash' do
   version :latest
   sha256 :no_check
 
-  url 'http://kapeli.com/Dash.zip'
-  appcast 'http://kapeli.com/Dash.xml'
+  url 'https://kapeli.com/Dash.zip'
+  appcast 'https://kapeli.com/Dash.xml'
   name 'Dash'
-  homepage 'http://kapeli.com/dash'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  homepage 'https://kapeli.com/dash'
+  license :commercial
 
   app 'Dash.app'
 
@@ -15,7 +15,7 @@ cask :v1 => 'dash' do
   end
 
   zap :delete => [
-                  '~/Library/Application Support/Dash/library.dash',
+                  '~/Library/Application Support/Dash',
                   '~/Library/Preferences/com.kapeli.dash.plist',
                  ]
 end
