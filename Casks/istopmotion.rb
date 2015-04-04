1dcd2e845f5ce95469172ed7dc8feb1597d5890d
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-04 07:27:50 +0300

    istopmotion.rb: add name

--- Casks/istopmotion.rb
+++ Casks/istopmotion.rb
@@ -3,6 +3,7 @@ cask :v1 => 'istopmotion' do
   sha256 '080c56eb60094247855be2a8b16a89f23741fb5fe2bb06ade13d31d8ff2ed9fc'
 
   url "https://cdn.boinx.com/software/istopmotion/Boinx_iStopMotion_#{version}.app.zip"
+  name 'iStopMotion'
   homepage 'http://www.boinx.com/istopmotion/mac/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
