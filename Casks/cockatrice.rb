6d6e6f2316147dc48bb6ba60d10b96ce263c482f
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-04 06:35:06 +0300

    cockatrice.rb: add name

--- Casks/cockatrice.rb
+++ Casks/cockatrice.rb
@@ -3,6 +3,7 @@ cask :v1 => 'cockatrice' do
   sha256 :no_check
 
   url 'http://www.woogerworks.com/files/cockatrice_weeklybuilds/Cockatrice-MacClient.dmg'
+  name 'Cockatrice'
   homepage 'http://www.woogerworks.com/'
   license :gpl
 
