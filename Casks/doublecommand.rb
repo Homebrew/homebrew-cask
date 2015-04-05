24ed07eee9477a916602c87332f10431081e07c1
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-05 08:36:28 +0300

    doublecommand.rb: add name

--- Casks/doublecommand.rb
+++ Casks/doublecommand.rb
@@ -3,6 +3,7 @@ cask :v1 => 'doublecommand' do
   sha256 '312aaf1a60517c694b24131bf502945dc23a22c917971c3e7a3adca163560503'
 
   url "http://doublecommand.sourceforge.net/files/DoubleCommand-#{version}.dmg"
+  name 'DoubleCommand'
   homepage 'http://doublecommand.sourceforge.net'
   license :gpl
 
