0aa9a49ae9b56bd9aa8173d82eb418ea8c5f2c72
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-05 08:25:30 +0300

    colorpicker-hex.rb: add name

--- Casks/colorpicker-hex.rb
+++ Casks/colorpicker-hex.rb
@@ -3,6 +3,7 @@ cask :v1 => 'colorpicker-hex' do
   sha256 'c35b00fbe1f6b0efcc4520a45deae15809729f3fec3dff8f4027789d26284089'
 
   url "http://wafflesoftware.net/hexpicker/download/HexColorPicker-#{version}.zip"
+  name 'Hex Color Picker'
   homepage 'http://wafflesoftware.net/hexpicker/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
