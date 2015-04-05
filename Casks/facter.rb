9447bc66d21cf5b310e53c81004c012a06409a4f
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-05 08:38:11 +0300

    facter.rb: add name

--- Casks/facter.rb
+++ Casks/facter.rb
@@ -3,6 +3,7 @@ cask :v1 => 'facter' do
   sha256 'd1a5bd1dce79878096f089facecae7e7d4114ca2253db4d4e93345b96705a25d'
 
   url "https://downloads.puppetlabs.com/mac/facter-#{version}.dmg"
+  name 'Facter'
   homepage 'https://puppetlabs.com/facter'
   license :apache
 
