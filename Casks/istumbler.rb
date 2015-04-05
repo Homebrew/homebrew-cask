c521196d26e56589ad9df1c42843cc9f06c3dde0
oleg <epushiron+github@gmail.com>
2015-04-05 12:15:38 +0500

    Update iStumbler 100 → 101 (is now commercial & Yosemite only)

--- Casks/istumbler.rb
+++ Casks/istumbler.rb
@@ -4,15 +4,20 @@ cask :v1 => 'istumbler' do
     version '99'
     sha256 'ac30e44fe86132c93a5b33699de00e86628e2f51a42015d2225b91521a198b63'
     url "http://istumbler.net/archive/release#{version}/downloads/iStumbler-#{version}.zip"
-  else
+  elsif MacOS.release < :yosemite
+    # Last freeware version. v101+ do now cost $25
     version '100'
     sha256 '71f6a6b0e255a853664ed4900835a42f2d23dcb05de35acfb3ac2ec1c5fb2edc'
+    url "https://istumbler.net/downloads/istumbler-#{version}.dmg"
+  else
+    version '101'
+    sha256 'cc40826c8f674c0be0b13921b67a005dc1fcda6df1c5a0c10d09e02d59eaea52'
     url "https://istumbler.net/downloads/istumbler-#{version}.dmg"
   end
 
   name 'iStumbler'
   homepage 'http://istumbler.net/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'iStumbler.app'
 end
