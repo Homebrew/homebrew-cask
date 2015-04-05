9ead7a78ed7ab6afb7107b033745f8086b029a38
Von Welch <von@vwelch.com>
2015-04-05 16:13:08 -0400

    Add FormulatePro 0.0.6

---
+++ Casks/formulatepro.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'formulatepro' do
+  version '0.0.6'
+  sha256 '9a5c37bad02a9dea7448e4ebe6fc6b0887efa05b0530603b9be1e5b0b3db2542'
+
+  url 'https://formulatepro.googlecode.com/files/FormulatePro-0.0.6.dmg'
+  name 'formulatepro'
+  homepage 'https://code.google.com/p/formulatepro/'
+  license :gpl
+
+  app 'FormulatePro.app'
+end
