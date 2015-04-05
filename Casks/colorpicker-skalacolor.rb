635bb27cffadfe429b11f8d335476c358f5f9996
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-05 08:26:14 +0300

    colorpicker-skalacolor.rb: add name

--- Casks/colorpicker-skalacolor.rb
+++ Casks/colorpicker-skalacolor.rb
@@ -3,6 +3,7 @@ cask :v1 => 'colorpicker-skalacolor' do
   sha256 :no_check
 
   url 'http://download.bjango.com/skalacolor/'
+  name 'Skala Color'
   homepage 'http://bjango.com/mac/skalacolor/'
   license :gratis
 
