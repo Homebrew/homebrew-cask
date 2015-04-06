5f1555a3577f2af365d48fba0aec945c50fb926d
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-06 11:50:57 +0300

    fuze.rb: add name

--- Casks/fuze.rb
+++ Casks/fuze.rb
@@ -4,6 +4,7 @@ cask :v1 => 'fuze' do
 
   # fuzemeeting.com is the official download host per the vendor homepage
   url 'https://www.fuzemeeting.com/fuze/mac_download'
+  name 'Fuze'
   homepage 'https://www.fuze.com/'
   license :commercial
 
