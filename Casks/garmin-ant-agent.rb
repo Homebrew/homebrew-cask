720456b01b812bd6b5a50558fcf38747fa97684d
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-06 11:53:45 +0300

    garmin-ant-agent.rb: add name

--- Casks/garmin-ant-agent.rb
+++ Casks/garmin-ant-agent.rb
@@ -3,6 +3,7 @@ cask :v1 => 'garmin-ant-agent' do
   sha256 '11df82fdd8d39a9d83df03c8efe8b1b1cb3e8f65d1e1bd47f823e9eda9bfcc57'
 
   url "http://download.garmin.com/software/ANTAgentforMac_#{version.gsub('.','')}.dmg"
+  name 'Garmin ANT Agent'
   homepage 'http://www8.garmin.com/support/download_details.jsp?id=4417'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
