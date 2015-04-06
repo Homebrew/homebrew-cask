b82ac984b5a8b3f751b7cf374c04e9aeec8ef102
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-06 11:56:20 +0300

    garmin-express.rb: add name

--- Casks/garmin-express.rb
+++ Casks/garmin-express.rb
@@ -3,6 +3,7 @@ cask :v1 => 'garmin-express' do
   sha256 :no_check
 
   url 'http://download.garmin.com/omt/express/GarminExpress.dmg'
+  name 'Garmin Express'
   homepage 'http://www.garmin.com/express'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
