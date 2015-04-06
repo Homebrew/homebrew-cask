b0ac33edde76867cfdcaac7f863e19d4313f3db8
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-06 12:27:43 +0300

    ivolume.rb: add name

--- Casks/ivolume.rb
+++ Casks/ivolume.rb
@@ -3,6 +3,7 @@ cask :v1 => 'ivolume' do
   sha256 :no_check
 
   url 'http://www.mani.de/download/ivolume/iVolume3Mac.dmg'
+  name 'iVolume'
   homepage 'http://www.mani.de/en/ivolume/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
