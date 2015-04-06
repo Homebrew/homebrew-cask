d24b90c7371da213872f05e6cfc993c0b63fb7fb
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-06 11:55:41 +0300

    garmin-communicator.rb: add name

--- Casks/garmin-communicator.rb
+++ Casks/garmin-communicator.rb
@@ -3,6 +3,7 @@ cask :v1 => 'garmin-communicator' do
   sha256 'a706a48926fc3907e84781bdf642ec1754c6081dd3d727ce09ca26c1ae8d9fde'
 
   url "http://download.garmin.com/software/CommunicatorPluginforMac_#{version.gsub('.','')}.dmg"
+  name 'Garmin Communicator Plugin'
   homepage 'http://www8.garmin.com/support/download_details.jsp?id=3739'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
