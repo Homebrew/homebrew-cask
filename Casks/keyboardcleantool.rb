8f23019f74221f5c010077aaee5421253f3c47cb
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-07 16:35:15 +0300

    keyboardcleantool.rb: add name

--- Casks/keyboardcleantool.rb
+++ Casks/keyboardcleantool.rb
@@ -4,6 +4,7 @@ cask :v1 => 'keyboardcleantool' do
 
   # bettertouchtool.net is the official download host per the vendor homepage
   url 'http://bettertouchtool.net/KeyboardCleanTool.zip'
+  name 'KeyboardCleanTool'
   homepage 'http://blog.boastr.net/keyboardcleantool/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
