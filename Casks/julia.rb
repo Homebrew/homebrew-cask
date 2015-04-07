5a0d039956b268fd9c3787c1f6ca7f3b0601ef86
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-07 16:28:53 +0300

    julia.rb: add name

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -4,6 +4,7 @@ cask :v1 => 'julia' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/julialang/bin/osx/x64/#{version.sub(/\.\d+$/, '')}/julia-#{version}-osx10.7+.dmg"
+  name 'Julia'
   homepage 'http://julialang.org/'
   license :mit
 
