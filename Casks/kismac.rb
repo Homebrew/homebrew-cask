69c4372a833decfbea48c8e46ac7052019acb9c8
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-07 16:38:27 +0300

    kismac.rb: add name

--- Casks/kismac.rb
+++ Casks/kismac.rb
@@ -6,6 +6,7 @@ cask :v1 => 'kismac' do
   url "http://update.kismacmirror.com/binaries/KisMAC-#{version}.dmg"
   appcast 'http://update.kismac-ng.org/sparkle/profileInfo.php',
           :sha256 => '18095f38358d65f5cbaa2a876745c7b1213d56893bc26c182138f51a2d3fa8df'
+  name 'KisMAC'
   homepage 'http://kismac-ng.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
