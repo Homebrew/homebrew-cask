b1a942bd6390cf505ba23e3ef3c1528ef728b4d5
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-08 21:31:47 +0300

    linkliar.rb: add name

--- Casks/linkliar.rb
+++ Casks/linkliar.rb
@@ -3,6 +3,7 @@ cask :v1 => 'linkliar' do
   sha256 :no_check
 
   url 'https://github.com/halo/LinkLiar/blob/master/latest_build/LinkLiar.zip?raw=true'
+  name 'LinkLiar'
   homepage 'https://github.com/halo/LinkLiar'
   license :mit
 
