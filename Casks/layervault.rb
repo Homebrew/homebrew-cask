5358a1c7f7f51404fe4385d0d2535c87998844ca
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-08 21:25:21 +0300

    layervault.rb: add name

--- Casks/layervault.rb
+++ Casks/layervault.rb
@@ -3,6 +3,7 @@ cask :v1 => 'layervault' do
   sha256 :no_check
 
   url 'https://client.layervault.com/latest_version'
+  name 'LayerVault'
   homepage 'http://layervault.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
