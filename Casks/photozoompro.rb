76e111fa000e15149c0847f55bcc219bb3f3dac7
BenVista <support@benvista.com>
2015-04-08 14:48:20 +0200

    Add BenVista PhotoZoom Pro v6.0.4

---
+++ Casks/photozoompro.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'photozoompro' do
+  version '6.0.4'
+  sha256 '283b3f878535d6cddd0d482baa49e04d295b84b572154f35706b4bd8c2bca19f'
+
+  url 'https://www.benvista.com/photozoompro/download/mac'
+  name 'PhotoZoom Pro'
+  homepage 'http://www.benvista.com/photozoompro'
+  license :closed
+
+  app 'PhotoZoom Pro 6.app'
+end
