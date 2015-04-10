cfd67bb03230d8724dbaad11270948d617a3d12d
Steve Hedrick <stevehedrick@mac.com>
2015-04-10 09:53:55 -0400

    Add Angband.app v3.5.1
    Updated as requested to parse the major/minor/release versioning
    from the version string and use them to populate the download link.
    
    Scheme handles Angband as versioned from 3.3 onward for download
    links.  As for version numbering, it handles anything the various
    maintainers have created since switching to the major/minor/release
    versioning scheme with 2.5.x.  This includes appending letters with
    the numbers.
    
    (I however did not manage to find a 'frog-knows' version on rephial
    to test, but it's positively ancient, and should work anyway...)

--- Casks/angband.rb
+++ Casks/angband.rb
@@ -2,7 +2,9 @@ cask :v1 => 'angband' do
   version '3.5.1'
   sha256 'de8af2db62de6ce19034bcb5d542e9c161a069d5b240a77d9c0497cb473ea82c'
 
-  url 'http://rephial.org/downloads/3.5/Angband-v3.5.1-osx.dmg'
+  versionnumber = /(.*)\.(.*)\.(.*)/.match(version)
+
+  url "http://rephial.org/downloads/#{versionnumber[1]}.#{versionnumber[2]}/Angband-v#{version}-osx.dmg"
   name 'Angband'
   homepage 'http://rephial.org'
   license :gpl
