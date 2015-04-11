93f6ed327dc7d9fdccdcbdd5f82254c4955220f1
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-11 21:42:49 +0300

    macdown.rb: add name

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -5,6 +5,7 @@ cask :v1 => 'macdown' do
   url 'http://macdown.uranusjr.com/download/latest/'
   appcast 'http://macdown.uranusjr.com/sparkle/macdown/appcast.xml',
           :sha256 => '81345496a64ca0d0d7ff8936e43ff0e1041ab6ebd61d534bc67f3357291b7478'
+  name 'MacDown'
   homepage 'http://macdown.uranusjr.com/'
   license :mit
 
