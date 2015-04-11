ebf8a9f0bbcefe58b7d9f77a2085134752fd6361
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-11 21:40:16 +0300

    ltspice.rb: add name

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -4,6 +4,7 @@ cask :v1 => 'ltspice' do
 
   # tech.com is the official download host per the vendor homepage
   url 'http://ltspice.linear-tech.com/LTspiceIV.dmg'
+  name 'LTspice'
   homepage 'http://www.linear.com/designtools/software/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
