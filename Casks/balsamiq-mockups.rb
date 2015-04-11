3c220a2dd7dbeb80cc2062a48e64964acf8fbf2f
陈兴 <WangKaifei@users.noreply.github.com>
2015-04-11 23:21:33 +0800

    upgrade Balsamiq Mockups to 3.0.8

--- Casks/balsamiq-mockups.rb
+++ Casks/balsamiq-mockups.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'balsamiq-mockups' do
-  version '3.0.7'
-  sha256 'db96ef5316039b44a9d5cebea17ac285e39d7293440240cb649c3daa004023e8'
+  version '3.0.8'
+  sha256 '4c59bbf509c2cb812635a7405c6b8c1197a3d8b6373f3db5c4c29f69c8bb860a'
 
   # amazonaws is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/build_production/mockups-desktop/Balsamiq_Mockups_#{version}.dmg"
