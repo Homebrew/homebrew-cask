e60b11fc656e21f672c848ee09291df1815ef42b
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-11 21:38:34 +0300

    logitech-myharmony.rb: add name

--- Casks/logitech-myharmony.rb
+++ Casks/logitech-myharmony.rb
@@ -3,6 +3,7 @@ cask :v1 => 'logitech-myharmony' do
   sha256 '537a11f2174dc748c27aff02bf01569b5bb2c93539b339c6fd61a7da07bb059f'
 
   url "http://app.myharmony.com/prod/mac/#{version}/MyHarmony-App.dmg"
+  name 'MyHarmony'
   homepage 'https://setup.myharmony.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
