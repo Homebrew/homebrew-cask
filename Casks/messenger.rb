094d2b7234b0c2ad20de3c25264cf5c9a6ccbb03
Patrik Ragnarsson <patrik@starkast.net>
2015-04-12 02:37:08 +0200

    Add Messenger for Mac v0.0.4

---
+++ Casks/messenger.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'messenger' do
+  version '0.0.4'
+  sha256 '87fefab0e0a30dfafe2fe1754cbdf014ad66fa5a6cf33ea3848b635bbe13ec61'
+
+  url 'http://fbmacmessenger.rsms.me/dist/Messenger-0.0.4.1428634580-a80cddf5538e66d6.zip'
+  name 'Messenger for Mac'
+  homepage 'http://fbmacmessenger.rsms.me/'
+  license :mit
+
+  app 'Messenger.app'
+end
