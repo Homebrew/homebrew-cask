0ce8c5ad4d10bc19f0b071bbceea51c18f20d4df
Radek Simko <radek.simko@gmail.com>
2015-04-12 15:32:42 +0100

    scribus: Wrong download URL fixed

--- Casks/scribus.rb
+++ Casks/scribus.rb
@@ -3,7 +3,7 @@ cask :v1 => 'scribus' do
   sha256 'e07be2772e8e1b897d21e636d05d4065af17757d33b85022fac3c0f99fdc6b63'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/project/scribus/scribus/#{version}/scribus-#{version}.dmg"
+  url "http://downloads.sourceforge.net/project/scribus/scribus/#{version.gsub(/(\-.+)?/, '')}/scribus-#{version}.dmg"
   name 'Scribus'
   homepage 'http://www.scribus.net/canvas/Scribus'
   license :gpl
