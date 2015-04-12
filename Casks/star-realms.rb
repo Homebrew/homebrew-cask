e1f8a9afc940a1c2cfc22b5f486827693416e452
Ben Alman <cowboy@rj3.net>
2015-04-12 17:25:54 -0400

    Update star-realms cask with current download URL.

--- Casks/star-realms.rb
+++ Casks/star-realms.rb
@@ -3,7 +3,7 @@ cask :v1 => 'star-realms' do
   sha256 '90d2cb6610a6195a85cb2e1a9bcbbc1da42dce57be9dce5186a4b3f12601529a'
 
   # cloudfront.net is the official download host per the vendor homepage
-  url "http://d3dyghjp0ukdwm.cloudfront.net/StarRealms_v#{version.gsub('.','')}.dmg"
+  url "http://d3dyghjp0ukdwm.cloudfront.net/StarRealms_v#{version.gsub('.','_')}.dmg"
   name 'Star Realms'
   homepage 'http://www.starrealms.com/digital-game/'
   license :freemium
