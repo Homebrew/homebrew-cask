fcb5a00159a03ccf7d4d2a9ed4043958b0193a5b
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-12 20:18:39 +0300

    midikeys.rb: add name

--- Casks/midikeys.rb
+++ Casks/midikeys.rb
@@ -3,6 +3,7 @@ cask :v1 => 'midikeys' do
   sha256 '673a11c0317ec8930940cc18bf8eef3de0d3040845c635722e99907f866a9b80'
 
   url "http://www.manyetas.com/creed/MidiKeys_#{version}.zip"
+  name 'MidiKeys'
   homepage 'http://www.manyetas.com/creed/midikeys.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
