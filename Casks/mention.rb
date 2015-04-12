bddee3093903c34926f8c16e9f7ff6598eea521a
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-12 20:15:14 +0300

    mention.rb: add name

--- Casks/mention.rb
+++ Casks/mention.rb
@@ -3,6 +3,7 @@ cask :v1 => 'mention' do
   sha256 :no_check
 
   url 'https://en.mention.com/downloads/mac/mention.dmg'
+  name 'Mention'
   homepage 'https://en.mention.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
