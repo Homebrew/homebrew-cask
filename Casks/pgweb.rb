3d793de8fbbffceee5519a0a5fbe3adbbefbcb7d
Dan Sosedoff <dan.sosedoff@gmail.com>
2015-04-13 09:14:02 -0500

    Update pgweb to 0.5.2

--- Casks/pgweb.rb
+++ Casks/pgweb.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pgweb' do
-  version '0.5.1'
-  sha256 'ffc7f22d9591660902f8725bb392ec861f80fb9747021d93262a640d672fea02'
+  version '0.5.2'
+  sha256 'ecca54470db7b29d40114feeb767f5991ee598c7aaa856ca220591070d67659b'
 
   url "https://github.com/sosedoff/pgweb/releases/download/v#{version}/pgweb_darwin_amd64.zip"
   appcast 'https://github.com/sosedoff/pgweb/releases.atom'
