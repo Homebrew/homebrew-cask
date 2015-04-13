58deb6247c4b56bc16f3eaae1e99c764d9ac3485
Caleb Xu <calebcenter@live.com>
2015-04-13 16:59:31 -0400

    Make ngrok use version string interpolation

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -2,7 +2,7 @@ cask :v1 => 'ngrok' do
   version '2.0.15'
   sha256 '5dbe5411c36654441c8a6d561194bed7a37d662ac55c9de5e8d6b56b839badf6'
 
-  url 'https://dl.ngrok.com/ngrok_2.0.15_darwin_amd64.zip'
+  url "https://dl.ngrok.com/ngrok_#{version}_darwin_amd64.zip"
   name 'ngrok'
   homepage 'https://ngrok.com/'
   license :apache
