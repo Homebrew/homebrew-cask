2e96ec8ae4b47e3592c7058b54c9204a58eaee2e
Anant Shrivastava <anant@anantshri.info>
2015-04-13 16:03:08 +0530

    multiple updates
    version changed to latest and checksum removed.
    dependency added 
    and non needed comment removed.

--- Casks/fiddler.rb
+++ Casks/fiddler.rb
@@ -1,8 +1,7 @@
 cask :v1 => 'fiddler' do
-  version '4.4.5.0'
-  sha256 '35ca5899d19269b24e7d648fbf6b5f20275075691392a122c93a2365bff70acd'
+  version :latest
+  sha256 :no_check
 
-  # sourceforge.net is the official download host per the vendor homepage
   url 'http://ericlawrence.com/dl/InstallFiddler.dmg'
   name 'Telerik Fiddler Proxy'
   name 'Fiddler Proxy'
@@ -10,6 +9,6 @@ cask :v1 => 'fiddler' do
   homepage 'http://www.telerik.com/fiddler'
   license :apache
   tags :vendor => 'Telerik'
-
+  depends_on :formula => 'Mono'
   app 'Fiddler.app'
 end
