2e5fe612a79b4ef9a29093fd5a6f30066c4dab03
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-13 23:21:23 +0300

    milkytracker.rb: add name

--- Casks/milkytracker.rb
+++ Casks/milkytracker.rb
@@ -3,6 +3,7 @@ cask :v1 => 'milkytracker' do
   sha256 '0029fe9a5e62d2b0db11fcbedb3c13997a4199259d1f36c55f966b0417d818b0'
 
   url "http://www.milkytracker.org/files/milkytracker-#{version}-osx_universal.zip"
+  name 'MilkyTracker'
   homepage 'http://www.milkytracker.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
