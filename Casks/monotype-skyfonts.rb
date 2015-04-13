4babd2cf7372824e14a87f54414d7d6fd4344fc5
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-13 23:25:44 +0300

    monotype-skyfonts.rb: add name

--- Casks/monotype-skyfonts.rb
+++ Casks/monotype-skyfonts.rb
@@ -4,6 +4,7 @@ cask :v1 => 'monotype-skyfonts' do
 
   # skyfonts.com is the official download host per the vendor homepage
   url "http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_#{version}.dmg"
+  name 'Monotype SkyFonts'
   homepage 'http://www.fonts.com/web-fonts/google'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
