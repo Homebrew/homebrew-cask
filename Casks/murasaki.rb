e165d4879d23e9e2fefda162fb85c41e526a1406
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-13 23:28:06 +0300

    murasaki.rb: add name

--- Casks/murasaki.rb
+++ Casks/murasaki.rb
@@ -3,6 +3,7 @@ cask :v1 => 'murasaki' do
   sha256 '2dd07c47d59aff053b8be804e08b087ce8d9e127365de43a206011a63ba42966'
 
   url "http://genjiapp.com/mac/murasaki/downloads/murasaki_v#{version}.zip"
+  name 'Murasaki'
   homepage 'http://genjiapp.com/mac/murasaki/index_en.html'
   license :gratis
 
