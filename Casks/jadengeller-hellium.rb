9235082a03d858d42eeb0950e110da46e743574c
Vítor Galvão <info@vitorgalvao.com>
2015-04-14 13:31:30 +0100

    Create jadengeller-hellium.rb

---
+++ Casks/jadengeller-hellium.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'jadengeller-hellium' do
+  version '1.5.1'
+  sha256 '3be70bd4a20b252ecd1e633374145cfab36cc16c20088670a8801cd7e8e004b6'
+
+  url "https://github.com/JadenGeller/Helium/releases/download/v#{version}/Helium.app.zip"
+  appcast 'https://github.com/JadenGeller/Helium/releases.atom'
+  name 'Helium'
+  homepage 'http://jadengeller.github.io/Helium/'
+  license :mit
+
+  app 'Helium.app'
+end
