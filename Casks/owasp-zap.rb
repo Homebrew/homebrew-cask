0f9dcbacdf1d883688d8475a9462831c5ce3de06
Vítor Galvão <info@vitorgalvao.com>
2015-04-14 13:53:02 +0100

    updated owasp-zap (2.4.0)

--- Casks/owasp-zap.rb
+++ Casks/owasp-zap.rb
@@ -3,7 +3,7 @@ cask :v1 => 'owasp-zap' do
   sha256 'd8e296cc09908f7df9970ac6f701191bf7ccdff628d95194196a58689f8186be'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/sourceforge/zaproxy/ZAP_#{version}_Mac_OS_X.zip"
+  url "http://downloads.sourceforge.net/sourceforge/zaproxy/ZAP_#{version}_Mac_OS_X.dmg"
   name 'OWASP Zed Attack Proxy'
   name 'OWASP ZAP'
   name 'ZAP'
