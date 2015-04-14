15468a54dea6b50bf71137ce12a4036187545c78
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-04-14 14:28:32 -0700

    Update mendeley-desktop to 1.13.8, set license

--- Casks/mendeley-desktop.rb
+++ Casks/mendeley-desktop.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'mendeley-desktop' do
-  version '1.13.6'
-  sha256 '77eef115c488662494b2fcdcbbddcdde63bfe7d81800e5445a02228a0630160e'
+  version '1.13.8'
+  sha256 'f90b5fec479b8dcb7bbf9d3dc6c4afa6a11d10636097653a28687c5f2415362a'
 
   url "http://desktop-download.mendeley.com/download/Mendeley-Desktop-#{version}-OSX-Universal.dmg"
   name 'Mendeley'
   homepage 'http://www.mendeley.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'Mendeley Desktop.app'
 end
