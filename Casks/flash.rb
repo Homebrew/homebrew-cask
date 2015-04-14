f6f1ee77951d05993e5476fd56589bee65be9039
Jorge Maroto <patoroco@gmail.com>
2015-04-14 10:53:02 +0200

    Update flash.rb

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'flash' do
-  version '17.0.0.134'
+  version '17.0.0.169'
   sha256 'fc566ceb1eff75790c6d5b5755a926c25cb1e354ba29ead48903f378c2be661f'
 
   # macromedia.com is the official download host per the vendor homepage
