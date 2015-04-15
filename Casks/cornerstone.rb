75d7ecdd62044b65a3fa1daa9edd541d55642b82
Anthony Green <anthony.green@bbc.co.uk>
2015-04-15 14:54:12 +0100

    Update to version 2.7.16

--- Casks/cornerstone.rb
+++ Casks/cornerstone.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'cornerstone' do
-  version '2.7.14'
-  sha256 '55cc253f7eabfe2aaaf3a9ba1d51d77e8bad8ec20035340a01054285e5085d51'
+  version '2.7.16'
+  sha256 '4898f5c4a5d1e1ab30c6e6eef55f3854ce5cd31292c554ef51b3f8470969caf3'
 
   url "https://www.zennaware.com/cornerstone/downloads/Cornerstone-#{version}.zip"
   appcast 'http://www.zennaware.com/cornerstone/appcast/feed2.php',
