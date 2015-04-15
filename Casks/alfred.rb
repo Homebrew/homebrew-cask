c8a7bd9381634f773203ca6a8f16b0c0cccfffc9
knakayama <knakayama.sh@gmail.com>
2015-04-16 04:24:09 +0900

    Upgrade Alfred.app to v2.7.1_387
    I updated Alfred cask formula.
    Changed lists I did is below.
    
    * change version
    * change sha256

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'alfred' do
-  version '2.7_385'
-  sha256 'a097fd1c8a49ddb62ad7b45243b34dccd6e663426ea0cef3c0e5f92529a83904'
+  version '2.7.1_387'
+  sha256 'a3738d0513d736918a6d71535ef3d85dd184af267c05698e49ac4c6b48f38e17'
 
   url "https://cachefly.alfredapp.com/Alfred_#{version}.zip"
   name 'Alfred'
