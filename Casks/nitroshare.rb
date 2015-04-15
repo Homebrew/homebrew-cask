d9d0587958f44fa69a6fb8b7f4cec1b69a88ae02
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-15 20:21:42 +0300

    nitroshare.rb: add name

--- Casks/nitroshare.rb
+++ Casks/nitroshare.rb
@@ -3,6 +3,7 @@ cask :v1 => 'nitroshare' do
   sha256 '9f208d4b86ca13c4136265fa4a4693c7ef9ba9f0d6a0a84966cfd5f9934abe12'
 
   url "https://launchpad.net/nitroshare/#{version}/#{version}/+download/nitroshare_#{version}.app.dmg"
+  name 'NitroShare'
   homepage 'https://launchpad.net/nitroshare'
   license :gpl
 
