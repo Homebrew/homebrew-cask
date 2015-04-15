e0f9452819c5369067d276e01f275263443cff5c
jab <jab@math.brown.edu>
2015-04-15 12:10:04 -0400

    update nw to v0.12.1
    release notes: https://groups.google.com/d/msg/nwjs-general/bvdnDJAx91U/0womlMli4FUJ

--- Casks/nw.rb
+++ Casks/nw.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'nw' do
-  version '0.12.0'
-  sha256 '3ad40bbb1c45c97274343266be017c0f54a1fb95c56adc32e246a43781801b72'
+  version '0.12.1'
+  sha256 'e0c08ca3a3057310a1d84c1b288a313464c1922ea3cc00dd75991df62d90e845'
 
   url "http://dl.nwjs.io/v#{version}/nwjs-v#{version}-osx-x64.zip"
   name 'NW.js'
