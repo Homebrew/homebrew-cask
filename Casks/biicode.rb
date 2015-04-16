bfbee32c7874ffe409ecd378be72d8ad0a3abdeb
Jesus Lopez <jesus@jesusla.com>
2015-04-16 13:10:15 -0700

    biicode: 3.0

--- Casks/biicode.rb
+++ Casks/biicode.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'biicode' do
-  version '2.8'
-  sha256 'eb7c2b0751e3ed2cb22af90a943a9bf4168b1469c33cef926bb186318be7f9d2'
+  version '3.0'
+  sha256 'c6b6d9bfa35387bb1b7bc7da385ca0690ea5400c096a1a5d52da3f00fddf23b3'
 
   # amazonaws is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/biibinaries/release/#{version}/bii-macos-64_#{version.gsub('.', '_')}.pkg"
