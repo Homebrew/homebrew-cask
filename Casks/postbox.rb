1dbb35f6501e7efa395c0803f8fb93c4f28928e6
Torsten Heinemann <theinemann@me.com>
2015-04-16 16:58:23 -0700

    Added name stanza to Postbox.rb

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -3,6 +3,7 @@ cask :v1 => 'postbox' do
   sha256 :no_check
 
   url 'http://www.postbox-inc.com/php/download.php?a=3011m'
+  name 'Postbox'
   homepage 'http://www.postbox-inc.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
