5c90d8b2e08b41e660dd09c8e2f0a89fc7594d84
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-16 22:26:56 +0300

    node.rb: add name

--- Casks/node.rb
+++ Casks/node.rb
@@ -3,6 +3,7 @@ cask :v1 => 'node' do
   sha256 '5fc20b6f2ebe3ec4c14ccffe2c1c7d1f3453b84e4e6b5d80b749c8a1fc46bc6f'
 
   url "http://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
+  name 'Node.js'
   homepage 'http://nodejs.org'
   license :mit
 
