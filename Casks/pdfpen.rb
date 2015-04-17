ea87614c50f241807e9ef8a4e3113e3d756bce06
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-17 22:05:41 +0300

    pdfpen.rb: add name

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -3,6 +3,7 @@ cask :v1 => 'pdfpen' do
   sha256 :no_check
 
   url 'http://dl.smilesoftware.com/com.smileonmymac.PDFpen/PDFpen.zip'
+  name 'PDFpen'
   homepage 'http://smilesoftware.com/PDFpen/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
