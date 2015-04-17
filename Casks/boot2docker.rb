be2d537ec61cc847efd9712b4bc22f89de7dbe12
Sébastien Fievet <_@sebastien-fievet.fr>
2015-04-17 09:45:01 +0200

    Update boot2docker to v1.6.0

--- Casks/boot2docker.rb
+++ Casks/boot2docker.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'boot2docker' do
-  version '1.5.0'
-  sha256 'd9399912d3cee93f862fc7b39a4be73807a4759162e27a85bf9de53d9f938059'
+  version '1.6.0'
+  sha256 'a6fc560d27bdfacd89a621bf89ed73e3e40f8e86ebdc85b142ddb328dd06a4b5'
 
   url "https://github.com/boot2docker/osx-installer/releases/download/v#{version}/Boot2Docker-#{version}.pkg"
   appcast 'https://github.com/boot2docker/osx-installer/releases.atom'
