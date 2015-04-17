71e5769d89c424edde88bc010cf9a8b37d7352fc
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-17 22:07:15 +0300

    paye-tools.rb: add name

--- Casks/paye-tools.rb
+++ Casks/paye-tools.rb
@@ -3,6 +3,7 @@ cask :v1 => 'paye-tools' do
   sha256 '7a8a055930bdcf6969f9c8887c9d4dcb6b8a6eba399743383866bfa3458acee3'
 
   url "https://www.gov.uk/government/uploads/uploaded/hmrc/payetools-rti-#{version}-osx.zip"
+  name 'Basic PAYE Tools'
   homepage 'http://www.hmrc.gov.uk/payerti/payroll/bpt/paye-tools.htm'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
