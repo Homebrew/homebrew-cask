a3230d8b88ac9f90f89b8982fdc77ff673ea4c61
José Padilla <jpadilla@webapplicate.com>
2015-04-17 07:56:23 -0400

    Add MongoDB.app v3.0.2-build-4

---
+++ Casks/mongodbapp.rb
@@ -0,0 +1,18 @@
+cask :v1 => 'mongodbapp' do
+  version '3.0.2-build-4'
+  sha256 'c0941911a8dead6b833b19913c4cf17e08d0a33b77eb987c72edc04757a6310f'
+
+  # github.com is the official download host per the vendor homepage
+  url "https://github.com/gcollazo/mongodbapp/releases/download/#{version}/MongoDB.zip"
+  appcast 'https://github.com/gcollazo/mongodbapp/releases.atom'
+  name 'MongoDB'
+  homepage 'http://elweb.co/mongodb-app/'
+  license :mit
+
+  app 'MongoDB.app'
+
+  zap :delete => [
+                  '~/Library/Caches/io.blimp.MongoDB',
+                  '~/Library/Preferences/io.blimp.MongoDB.plist'
+                 ]
+end
