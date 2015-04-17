f5248296b40a6083b3ad073988b01b1c35d14577
Brad Rowe <gord1anknot@users.noreply.github.com>
2015-04-17 16:21:36 -0600

    Update MPS to 3.2.1
    Added zap and caveat stanzas found on casks for other intellij
    based IDEs. Remove section for a workaround that doesn't apply
    to MPS.

--- Casks/mps.rb
+++ Casks/mps.rb
@@ -1,12 +1,28 @@
 cask :v1 => 'mps' do
-  version '3.2'
-  sha256 '1e015f575ad740a0f5849e2d511ac8e82a1656ceeded5b06cb2b7f3fb542b3ca'
+  version '3.2.1'
+  sha256 '3122634120c347c4ebb6e4a2de98f30fc9fa63427b607063ebb7096f4d4aec4e'
 
-  url "http://download-cf.jetbrains.com/mps/#{version.tr('.','')}/MPS-#{version}-macos.dmg"
+  url "http://download-cf.jetbrains.com/mps/#{version.tr('.','')[0,2]}/MPS-#{version}-macos.dmg"
   name 'MPS'
   name 'JetBrains MPS'
   homepage 'https://www.jetbrains.com/mps'
   license :apache
 
-  app "MPS #{version}.app"
+  app "MPS #{version[0,3]}.app"
+
+  zap :delete => [
+                  "~/Library/Application Support/MPS#{version.tr('.','')[0,2]}",
+                  "~/Library/Preferences/MPS#{version.tr('.','')[0,2]}",
+                 ]
+
+  caveats <<-EOS.undent
+    #{token} requires Java 6 like any other IntelliJ-based IDE.
+    You can install it with
+
+      brew cask install caskroom/homebrew-versions/java6
+
+    The vendor (JetBrains) doesn't support newer versions of Java (yet)
+    due to several critical issues, see details at
+    https://intellij-support.jetbrains.com/entries/27854363
+  EOS
 end
