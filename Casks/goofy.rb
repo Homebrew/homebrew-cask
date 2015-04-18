9210b9b8b578f2ca179f47ee8d971afbb1cb6a76
Alexander Seifert <alexander.seifert@gmail.com>
2015-04-18 14:52:17 +0200

    update url for Goofy.app

--- Casks/goofy.rb
+++ Casks/goofy.rb
@@ -2,8 +2,8 @@ cask :v1 => 'goofy' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
-  url 'https://goofyapp.s3.amazonaws.com/Goofy.app.zip'
+  # uberspace.de is the official download host per the vendor homepage
+  url 'https://dani.taurus.uberspace.de/goofyapp/Goofy.app.zip'
   name 'Goofy'
   homepage 'http://www.goofyapp.com/'
   license :gratis
