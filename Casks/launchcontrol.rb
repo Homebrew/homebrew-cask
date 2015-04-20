cc80651dbb5e674758d2c65f8e50255f16ec65e6
Jeffrey Cafferata <jeffrey@jcid.nl>
2015-04-20 17:02:29 +0200

    Update LaunchControl to 1.19.2

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'launchcontrol' do
-  version '1.19.1'
-  sha256 '9adbe6acc858c09625de21519fc38dbf230ae8f7316e455fbfb768a8c97b1db2'
+  version '1.19.2'
+  sha256 '58e8a5888b64438b8dae01dab61dd7909f498f4b03ddee7c7ac4781205a5f9b8'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
