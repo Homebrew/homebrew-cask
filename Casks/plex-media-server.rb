793bc3f33e20a8c0514f98b30379391cd3ac2ce5
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-20 23:18:08 +0300

    plex-media-server.rb: add name

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -3,6 +3,7 @@ cask :v1 => 'plex-media-server' do
   sha256 '69066c232cfbf4b8cd92881e954e8486042c0838bfd69c27b600d89d023abf96'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
+  name 'Plex Media Server'
   homepage 'https://plex.tv/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
