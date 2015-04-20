ef025a54b6b78f71be5f09d618cbb9b88fc01c3f
Alexander Seifert <alexander.seifert@gmail.com>
2015-04-20 14:23:55 +0200

    add comment stating that this is the official download host

--- Casks/goofy.rb
+++ Casks/goofy.rb
@@ -2,6 +2,7 @@ cask :v1 => 'goofy' do
   version :latest
   sha256 :no_check
 
+  # uberspace.de is the official download host per the vendor homepage
   url 'https://dani.taurus.uberspace.de/goofyapp/Goofy.app.zip'
   name 'Goofy'
   homepage 'http://www.goofyapp.com/'
