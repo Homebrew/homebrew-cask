a6a16f8fe8370d7dedee2c6fd64a27beed7aa945
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-20 19:03:46 +0300

    connected-desktop.rb: add name

--- Casks/connected-desktop.rb
+++ Casks/connected-desktop.rb
@@ -6,6 +6,7 @@ cask :v1 => 'connected-desktop' do
   url "https://secure.connecteddata.com/mac/2.4/software/Connected_Desktop_#{version}.dmg"
   appcast 'http://appsoftware.connecteddata.com/mac/2.0/Appcast.xml',
           :sha256 => '51d51ee36b94570f4c02379f9aef8d0b1387abf97509757da89f0dd82b30cb95'
+  name 'Connected Desktop'
   homepage 'http://www.filetransporter.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
