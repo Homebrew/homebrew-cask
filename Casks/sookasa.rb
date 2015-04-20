f37b4e3268a9204835e93159bcdcfc965c8c0cba
Kyle Stark <kyle@goodrx.com>
2015-04-20 12:54:45 -0700

    Update sookasa.rb
    Update from version 3.7.2 to version 3.9.7

--- Casks/sookasa.rb
+++ Casks/sookasa.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'sookasa' do
-  version '3.7.2'
-  sha256 '27192c3b1bc5a199071656e1f1e937d21ddd1c2f027c70768192e06f63ce67ee'
+  version '3.9.7'
+  sha256 '103aba04e26dcc09fb1ae8a0b917a328e01863cf5f37157b1d505c9d73ec1c85'
 
-  url 'https://d2rs8uj3cnos4.cloudfront.net/mac-apps/releases/Sookasa_3.7.2.pkg'
+  url 'https://d2rs8uj3cnos4.cloudfront.net/mac-apps/releases/Sookasa_3.9.7.pkg'
   homepage 'https://www.sookasa.com'
   license :commercial
 
-  pkg 'Sookasa_3.7.2.pkg'
+  pkg 'Sookasa_3.9.7.pkg'
 
   uninstall :quit => 'com.sookasa.Sookasa',
             :pkgutil => 'com.sookasa.Sookasa'
