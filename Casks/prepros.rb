4dbbed9d5625206ae370d2adf5c83fc7302964ff
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-21 17:53:37 +0300

    prepros.rb: add name

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -3,6 +3,7 @@ cask :v1 => 'prepros' do
   sha256 '84510d1252274898b0fc4f95828b60e29a2c946a158bbf7d3188611c62dcd3b1'
 
   url "http://download.alphapixels.com/Prepros-#{version}.zip"
+  name 'Prepros'
   homepage 'http://alphapixels.com/prepros/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
