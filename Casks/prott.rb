66756b52de97308a529a8a2e89451a47ef0f3971
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-21 17:58:13 +0300

    prott.rb: add name

--- Casks/prott.rb
+++ Casks/prott.rb
@@ -3,6 +3,7 @@ cask :v1 => 'prott' do
   sha256 :no_check
 
   url 'https://prottapp.com/app/gadgets/prott.dmg'
+  name 'Prott'
   homepage 'http://prottapp.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
