27b9fb77d8350acad250c905a2be52f7f1b350fd
Matt Dering <matthew.dering@gmail.com>
2015-04-21 10:15:56 -0400

    updated cmake

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'cmake' do
-  version '3.1.1'
-  sha256 '5106e940d8abcdbd2c8b5117ee9618c90207aeb4235837e5170aaf1a950230f8'
+  version '3.2.2'
+  sha256 '430b9f94a363ff19d5f66eacc5d6bc55bd9dac75d6fc6bcb813d8b451c1475c4'
 
-  url "http://www.cmake.org/files/v3.1/cmake-#{version}-Darwin-x86_64.dmg"
+  url "http://www.cmake.org/files/v#{version.sub(%r{\.\d+$},'')}/cmake-#{version}-Darwin-x86_64.dmg"
   name 'CMake'
   homepage 'http://cmake.org'
   license :bsd
