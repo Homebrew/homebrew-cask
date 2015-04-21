f87ae4652914090a9aebdc2e2a6c672ad811f4db
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-21 17:49:55 +0300

    pomotodo.rb: add name

--- Casks/pomotodo.rb
+++ Casks/pomotodo.rb
@@ -4,6 +4,7 @@ cask :v1 => 'pomotodo' do
 
   # hackplan.com is the official download host per the vendor homepage
   url 'http://cdn.hackplan.com/theair/1419250299/Pomotodo.dmg'
+  name 'Pomodoro'
   homepage 'https://pomotodo.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
