6c2cf929724c0cf8ce7fbf94e0bc9940d185ce57
Csineneo <csineneo@gmail.com>
2015-04-21 09:37:32 +0800

    Update near lock to 1.7

--- Casks/near-lock.rb
+++ Casks/near-lock.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'near-lock' do
-  version :latest
-  sha256 :no_check
+  version 1.7
+  sha256 'e11f2a7d2d3ad4b2bddccb3d3bf369e9c1c84e40cf824a028ad199ba3ed728ec'
 
   url 'http://nearlock.me/downloads/nearlock.dmg'
   name 'Near Lock'
