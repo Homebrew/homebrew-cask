c9bac058891a8d1f857c9850d11f709ebf8d5938
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-21 17:51:22 +0300

    power-manager-pro.rb: add name

--- Casks/power-manager-pro.rb
+++ Casks/power-manager-pro.rb
@@ -4,6 +4,7 @@ cask :v1 => 'power-manager-pro' do
 
   url 'https://www.dssw.co.uk/powermanager/dsswpowermanagerpro.dmg'
   appcast 'http://version.dssw.co.uk/powermanager/professional'
+  name 'Power Manager Pro'
   homepage 'https://www.dssw.co.uk/powermanager'
   license :commercial
 
