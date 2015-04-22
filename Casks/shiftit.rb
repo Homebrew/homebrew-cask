4fe241e9c4c7e807d83b342d614c9f4af3e47635
Filip Krikava <krikava@gmail.com>
2015-04-22 15:05:19 +0200

    Added releases.atom to ShiftIt

--- Casks/shiftit.rb
+++ Casks/shiftit.rb
@@ -2,7 +2,8 @@ cask :v1 => 'shiftit' do
   version '1.6.1'
   sha256 '64b9fdb4b8104a3e8efc307119ca4caba68a983916a4424db507b1e05cec3644'
 
-  url "https://github.com/fikovnik/ShiftIt/releases/download/version-#{version}/ShiftIt-#{version}.zip"
+  url 'https://github.com/fikovnik/ShiftIt/releases/download/version-#{version}/ShiftIt-#{version}.zip'
+  url 'https://github.com/fikovnik/ShiftIt/releases.atom'
   name 'ShiftIt'
   appcast 'https://raw.github.com/fikovnik/ShiftIt/develop/release/appcast.xml',
           :sha256 => '16b9da1fa91aa964ad0fda907147d55e50da844d39a5fe5abcd0224abb954be3'
