ddd76e46012cd1f91340a75cf5e805e6c527b3f6
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-23 00:20:33 +0300

    qlrest.rb: add name

--- Casks/qlrest.rb
+++ Casks/qlrest.rb
@@ -3,6 +3,7 @@ cask :v1 => 'qlrest' do
   sha256 :no_check
 
   url 'https://github.com/downloads/cluther/qlrest/QLRest.qlgenerator.zip'
+  name 'QLRest'
   homepage 'https://github.com/cluther/qlrest'
   license :oss
 
