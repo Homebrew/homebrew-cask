411a90e454e1bf1861e92565dd7d17bc92b816a5
Filip Krikava <krikava@gmail.com>
2015-04-22 13:44:24 +0200

    Added `name` to ShiftIt

--- Casks/shiftit.rb
+++ Casks/shiftit.rb
@@ -3,6 +3,7 @@ cask :v1 => 'shiftit' do
   sha256 '64b9fdb4b8104a3e8efc307119ca4caba68a983916a4424db507b1e05cec3644'
 
   url "https://github.com/fikovnik/ShiftIt/releases/download/version-#{version}/ShiftIt-#{version}.zip"
+  name 'ShiftIt'
   appcast 'https://raw.github.com/fikovnik/ShiftIt/develop/release/appcast.xml',
           :sha256 => '16b9da1fa91aa964ad0fda907147d55e50da844d39a5fe5abcd0224abb954be3'
   homepage 'https://github.com/fikovnik/ShiftIt'
