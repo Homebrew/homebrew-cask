850bdbfa17bb993b324e8ed46425c992dd3f101d
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-23 00:31:26 +0300

    quicklook-pfm.rb: add name

--- Casks/quicklook-pfm.rb
+++ Casks/quicklook-pfm.rb
@@ -3,6 +3,7 @@ cask :v1 => 'quicklook-pfm' do
   sha256 '53308ac3d7f0f8ed02adb2feff282764fdeadb32352b0776caeaec3d6a514333'
 
   url "https://quicklook-pfm.googlecode.com/files/quicklook-pfm-#{version}.zip"
+  name 'quicklook-pfm'
   homepage 'https://code.google.com/p/quicklook-pfm/'
   license :oss
 
