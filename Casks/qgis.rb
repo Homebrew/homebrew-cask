9f8998d59ad741f426f5b35ff023e96360cfbe7f
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-23 00:19:27 +0300

    qgis.rb: add name

--- Casks/qgis.rb
+++ Casks/qgis.rb
@@ -3,6 +3,7 @@ cask :v1 => 'qgis' do
   sha256 'cd6d3ebd779ee6801807fe4d54a77654ed430dd275f4a084a5d11cf67f1b26c4'
 
   url "http://www.kyngchaos.com/files/software/qgis/QGIS-#{version}.dmg"
+  name 'QGIS'
   homepage 'http://www.kyngchaos.com/software/qgis'
   license :gpl
   pkg 'Install QGIS.pkg'
