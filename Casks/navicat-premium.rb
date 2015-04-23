020b9401b476622b85023900a5cda9f3a20f27d3
Hassan Khalid <hkhalid@intellicentrics.com>
2015-04-23 19:29:29 +0100

    navicat-premium upgraded to 11.1.12

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'navicat-premium' do
-  version '11.1.9'   # navicat-premium.rb and navicat-for-* should be upgraded together
-  sha256 'a30a03d88d71b620de5bab432e54f9cfe8fee344e946806d558467a4dda39791'
+  version '11.1.12'   # navicat-premium.rb and navicat-for-* should be upgraded together
+  sha256 'f0d5f93972bd39e9a174d70f897c14afeda17db7390b15de2c417e48084b2ccf'
 
   url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_premium_en.dmg"
   name 'Navicat Premium'
