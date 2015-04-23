e753c7b972466e1e23c2dff3c0e229a04e52a9ba
lcrees <lcrees@gmail.com>
2015-04-23 11:34:48 -0600

    Update QT Creator to 3.4.0

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qt-creator' do
-  version '3.3.2'
-  sha256 '6b44dd8e01251b485dad7d412db1898b297f129993fea74694be1d7688e845ba'
+  version '3.4.0'
+  sha256 '13a232534adf50f928ebf1ad1fde912a423cd7524d86d0c3d6a8efd321d3f858'
 
   url "http://download.qt-project.org/official_releases/qtcreator/#{version.sub(%r{^(\d+\.\d+).*},'\1')}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
