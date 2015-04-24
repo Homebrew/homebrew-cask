f314995fff7dcf74f1fb4e1d61ac5d4b8d043a1d
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-24 14:15:51 +0300

    Upgrade Scilab.app to v5.5.2

--- Casks/scilab.rb
+++ Casks/scilab.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'scilab' do
-  version '5.5.1'
-  sha256 '0e28ddd7dbf9f812bf13be12e3217d4c9e346a48e946b613a731a1c0ed9e0a4a'
+  version '5.5.2'
+  sha256 '6e855c4aae6f75d37ced77bea64ac5cf33f65f3925107c65547b2a5fede3bd91'
 
   url "http://www.scilab.org/download/#{version}/scilab-#{version}-x86_64_yosemite.dmg"
   homepage 'https://www.scilab.org'
