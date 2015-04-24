2fc1d0c434788971d7fdc47d6dc9661735165808
Willy Morin <willy.morin@blablacar.com>
2015-04-24 12:38:43 +0200

    Update DBeaver community to v3.3.2

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'dbeaver-community' do
-  version '3.3.1'
+  version '3.3.2'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '0ad6a3a7011415cfb990b6027bf62b93b334a12dff9740ebc03993c9e235f332'
+    sha256 '540a8e571bf759d84bea1e7ed1b032c8b1994fda13b09d2c2a2e62c4acfb8af5'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-macosx.cocoa.x86.zip"
   else
-    sha256 'f4aaf0314ff4fd1f9084f7d634566c82aac37f208161fe4d4f6f670745f3c1f7'
+    sha256 '1978e7c437dca237f6a21237f271b410d65c49d4b584bf87e4423c0d0e140c2e'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-macosx.cocoa.x86_64.zip"
   end
 
