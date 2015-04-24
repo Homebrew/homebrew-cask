5cf1423aba04d0b8385a0187d5d51c4a50e68da8
aruntho <aruntho@cisco.com>
2015-04-23 22:09:08 -0700

    Add Mindjet Mindmanager

---
+++ Casks/mindjet-mindmanager.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'mindjet-mindmanager' do
+  version '10.3.635'
+  sha256 'bb567a2f23e6cc08706de80486398df4ce9f6987d7dd2b782efa4629730ff3e3'
+
+  url 'http://www.mindjet.com/mm-mac-dmg'
+  name 'Mindjet Mindmanager'
+  homepage 'http://www.mindjet.com/mindmanager/'
+  license :commercial    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'Mindjet MindManager.app'
+end
