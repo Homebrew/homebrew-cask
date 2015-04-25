f186de1f62e2a12eccaf0c3a9d3b57325f3c9e76
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-25 17:37:44 +0300

    quassel-client.rb: add name

--- Casks/quassel-client.rb
+++ Casks/quassel-client.rb
@@ -3,6 +3,7 @@ cask :v1 => 'quassel-client' do
   sha256 '00e1a1b7a3fc0b5c1c382c488ab93d533a183896ca236d6170cc11e1fc28ac96'
 
   url "http://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_#{version}.dmg"
+  name 'Quassel IRC'
   homepage 'http://quassel-irc.org'
   license :gpl
 
