a960b7b185b8f3711610c14f8af62da5516ae255
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-25 17:51:22 +0300

    retinacapture.rb: add name

--- Casks/retinacapture.rb
+++ Casks/retinacapture.rb
@@ -3,6 +3,7 @@ cask :v1 => 'retinacapture' do
   sha256 :no_check
 
   url 'http://www.retinacapture.com/files/RetinaCapture.zip'
+  name 'RetinaCapture'
   homepage 'http://www.retinacapture.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
