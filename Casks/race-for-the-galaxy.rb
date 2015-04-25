645a04eda28a12b598db958302dc25fdffa9ee01
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-25 17:44:24 +0300

    race-for-the-galaxy.rb: add name

--- Casks/race-for-the-galaxy.rb
+++ Casks/race-for-the-galaxy.rb
@@ -3,6 +3,7 @@ cask :v1 => 'race-for-the-galaxy' do
   sha256 'f60585e3f47cf09df77c578bb1c7dad31dfe4dbe223da35f190b2573a2803bd7'
 
   url "http://keldon.net/rftg/rftg-osx-#{version}.zip"
+  name 'Race for the Galaxy'
   homepage 'http://keldon.net/rftg/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
