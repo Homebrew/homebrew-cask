218e006f1f0fec28a51d7e518879d978513af200
Alex <alex@Alexs-MacBook-Pro.local>
2015-04-25 16:46:28 +0800

    update telegram version number and hash

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'telegram' do
-  version '0.8.4'
-  sha256 '9489789f458f280827397564c4cf55b864da54598f703380e41060da6c245a9f'
+  version '0.8.7'
+  sha256 'ad342077d93af9e1d62b746551a019fb7ee916fd2c8ba757c053c149f58b71ab'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
