83602dfda5fedb4af58f2093dde5dc73eb710847
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-26 18:46:51 +0300

    scummvm.rb: add name

--- Casks/scummvm.rb
+++ Casks/scummvm.rb
@@ -6,6 +6,7 @@ cask :v1 => 'scummvm' do
   url "http://downloads.sourceforge.net/project/scummvm/scummvm/#{version}/scummvm-#{version}-macosx.dmg"
   appcast 'http://www.scummvm.org/appcasts/macosx/release.xml',
           :sha256 => 'bea2fd9739e102c781753c991b6ee6481a1c0c3f00621e96dca06a2b83e42135'
+  name 'ScummVM'
   homepage 'http://scummvm.org/'
   license :gpl
 
