70d8dd31e36e45b9e371292f2e10cc636004cbed
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-26 18:45:51 +0300

    screensteps.rb: add name

--- Casks/screensteps.rb
+++ Casks/screensteps.rb
@@ -3,6 +3,7 @@ cask :v1 => 'screensteps' do
   sha256 'e48082731531198d8c22e5218a7aa59500843e4fd4fd7f44161688aebcfc621b'
 
   url 'http://www.bluemangolearning.com/download/screensteps/2_0/release/ScreenSteps.dmg'
+  name 'ScreenSteps'
   homepage 'http://www.bluemangolearning.com/screensteps'
   license :commercial
 
