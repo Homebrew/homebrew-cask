1d31499f1631b7db10c024ef3c1138fd202976e8
Vítor Galvão <info@vitorgalvao.com>
2015-04-26 01:06:29 +0100

    Update screens-connect.rb
    Improved comments

--- Casks/screens-connect.rb
+++ Casks/screens-connect.rb
@@ -2,7 +2,7 @@ cask :v1 => 'screens-connect' do
   version '3.2.3'
   sha256 'bdffd7b7e750d5bcb23d2895588b5d14c8d0cdd5211391d537ee781b43321afc'
 
-  # edovia.com is the official download host per the appcast
+  # edovia.com is the official download host per the appcast feed
   # Original discussion: https://github.com/caskroom/homebrew-cask/pull/8816
   url "http://download.edovia.com/screensconnect/screensconnect_#{version}.dmg"
   appcast 'https://screensconnect.com/sparkle/appcast.xml'
