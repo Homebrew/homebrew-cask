0082cc73f82f01871d2f0b4cf46e1ee5774d2bd1
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-26 18:48:10 +0300

    seashore.rb: add name

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -3,6 +3,7 @@ cask :v1 => 'seashore' do
   sha256 '96463a3642f162a20b160d8df273e9b27a5fdbf9708bee6ebf6a6c8528047765'
 
   url 'http://downloads.sourceforge.net/sourceforge/seashore/Seashore.zip'
+  name 'Seashore'
   homepage 'http://seashore.sourceforge.net/'
   license :gpl
 
