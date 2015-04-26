e7ae5fdd8f8b64379b5224ec7fc4cb5ad99ca7e2
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-26 18:34:14 +0300

    royal-tsx.rb: add name

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -5,6 +5,7 @@ cask :v1 => 'royal-tsx' do
   url "http://v2.royaltsx.com/updates/royaltsx_#{version}.dmg"
   appcast 'http://v2.royaltsx.com/updates_beta.php',
           :sha256 => '322a251ba213a4ea9873ce4d5a1386cd9f17234ff683c88bcd939c6d9a458310'
+  name 'Royal TSX'
   homepage 'http://www.royaltsx.com'
   license :freemium
 
