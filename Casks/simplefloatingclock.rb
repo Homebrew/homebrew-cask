5edbdc912f58bfb3c6a79fbb805d36ad825cf8ac
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-27 21:07:44 +0300

    simplefloatingclock.rb: add name

--- Casks/simplefloatingclock.rb
+++ Casks/simplefloatingclock.rb
@@ -3,6 +3,7 @@ cask :v1 => 'simplefloatingclock' do
   sha256 :no_check
 
   url 'http://www.splook.com/Software/distrib/sfc.zip'
+  name 'Simple Floating Clock'
   homepage 'http://www.splook.com/Software/Simple_Floating_Clock.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
