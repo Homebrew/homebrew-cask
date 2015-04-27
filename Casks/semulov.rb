109a6d1a34a8f5464cc31213e096d6d7670a5b06
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-27 20:56:37 +0300

    semulov.rb: add name

--- Casks/semulov.rb
+++ Casks/semulov.rb
@@ -3,6 +3,7 @@ cask :v1 => 'semulov' do
   sha256 'de2b9d4e874885a6421c17fb716e2072827c4d111c946d15ada2b4d31392e803'
 
   url "http://kainjow.com/downloads/Semulov_#{version}.zip"
+  name 'Semulov'
   homepage 'http://www.kainjow.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
