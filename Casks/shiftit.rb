1ef95234373f865461332d0aac00d5a9af76573c
Filip Krikava <krikava@gmail.com>
2015-04-28 00:14:59 +0200

    Upgrade ShiftIt to 1.6.3
    Accidentally left out a critical bug.

--- Casks/shiftit.rb
+++ Casks/shiftit.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'shiftit' do
-  version '1.6.2'
-  sha256 '1fda1050f529707a3471c301587271a8ae65f90825d3b9fc6b939b14a6157bf7'
+  version '1.6.3'
+  sha256 '3b01d74cc39e4efad64b2c9b135bea528730cb750c55a386bf74e1203b92ca68'
 
-  url 'https://github.com/fikovnik/ShiftIt/releases/download/version-#{version}/ShiftIt-#{version}.zip'
-  url 'https://github.com/fikovnik/ShiftIt/releases.atom'
+  url "https://github.com/fikovnik/ShiftIt/releases/download/version-#{version}/ShiftIt-#{version}.zip"
+  # url 'https://github.com/fikovnik/ShiftIt/releases.atom'
   name 'ShiftIt'
   appcast 'https://raw.github.com/fikovnik/ShiftIt/master/release/appcast.xml',
-          :sha256 => 'c9434230d6142af0c429716af2a7628528a5d5721983238592556fd1f5d56aff'
+          :sha256 => '04830e525345d734515b717ef2f68a1f8e2bba9c8de940bd2dc072f992f21a08'
   homepage 'https://github.com/fikovnik/ShiftIt'
   license :gpl
 
