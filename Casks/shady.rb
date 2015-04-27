a463481394e2a6b9918571a76dda30f64fa83faf
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-27 20:59:41 +0300

    shady.rb: add name

--- Casks/shady.rb
+++ Casks/shady.rb
@@ -3,6 +3,7 @@ cask :v1 => 'shady' do
   sha256 '5905e8738af657bdb21b27a9615996a72b6080e502ca28fa328139aadc00277c'
 
   url "http://instinctivecode.com/shady/shady_#{version}.zip"
+  name 'Shady'
   homepage 'http://instinctivecode.com/shady/'
   license :oss
 
