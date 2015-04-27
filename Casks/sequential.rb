f49f1809893215e5cbfbff9b699c3f361d9cea46
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-27 20:57:41 +0300

    sequential.rb: add name

--- Casks/sequential.rb
+++ Casks/sequential.rb
@@ -5,6 +5,7 @@ cask :v1 => 'sequential' do
   url "http://sequentialx.com/Sequential#{version}.zip"
   appcast 'http://www.sequentialx.com/sequential.xml',
           :sha256 => '447db3c7237bdd750ad128556b1d3ff6f3f3c26f8c2bd0131b9993b5365f7d6b'
+  name 'Sequential'
   homepage 'http://sequentialx.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
