d867d89a09ee1918125e97394c31976b6d7b4f13
jonasrosland <jonas.rosland@gmail.com>
2015-04-28 16:43:41 -0400

    Updated vmware-horizon-client to version 3.2.0-2331566

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'vmware-horizon-client' do
-  version '3.1.0-2096621'
-  sha256 '2ef9b6a377e722f03c1b8c10ea9e7df5c56b5e3973e370905602b1ce5e4ed17b'
+  version '3.2.0-2331566'
+  sha256 '12a9c79ecdf9de719999955ba350f7829d1b6be179eb757728429fe2eef54e99'
 
-  url "https://download3.vmware.com/software/view/viewclients/VMware-Horizon-Client-#{version}.dmg"
+  url "https://download3.vmware.com/software/view/viewclients/CART14Q4/VMware-Horizon-Client-#{version}.dmg"
   homepage 'http://www.vmware.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
