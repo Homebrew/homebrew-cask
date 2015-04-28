3d168f107d51e113d7208f7620cfaa75ede49238
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-28 20:58:19 +0300

    Upgrade Vivaldi.app to v1.0.162.4 (Technical Preview 3)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'vivaldi' do
-  version '1.0.118.19'
+  version '1.0.162.4'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://vivaldi.com/download/Vivaldi_TP2.#{version}.dmg"
+  url "https://vivaldi.com/download/Vivaldi_TP3.#{version}.dmg"
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
   license :gratis
