ae86e7089d79488a0090c9661e4b121a9d921c00
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-28 20:54:29 +0300

    Upgrade Opera.app to v29.0.1795.47

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera' do
-  version '28.0.1750.51'
-  sha256 '650a722d9fbc43a6ca447ad1b0911b7ddf511c279759462c29f4531372fa198a'
+  version '29.0.1795.47'
+  sha256 'e2b7bffb593c27c76a5df8aa297ab2802f003feb836d43f8769301fe15e30562'
 
   url "http://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
