a6acb141f776a144f6bcc3f3a4a17a4d5cf8e150
Matt Whiteley <mattwhiteley@gmail.com>
2015-04-29 15:07:08 -0700

    Upgrade ChefDK to 0.5.0

--- Casks/chefdk.rb
+++ Casks/chefdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'chefdk' do
-  version '0.4.0-1'
-  sha256 '01b89caff1fb29b4a949666a5016e2455f449fbb7d1027320578b1e043a63e9a'
+  version '0.5.0-1'
+  sha256 '3225a292f754946890fa4fe5d118c1f8ef572f2531d240dd6f0a3122d57c692c'
 
   # amazonaws is the official download host per the vendor homepage
   url "https://opscode-omnibus-packages.s3.amazonaws.com/mac_os_x/10.8/x86_64/chefdk-#{version}.dmg"
@@ -18,17 +18,16 @@ cask :v1 => 'chefdk' do
                          '/usr/bin/chef',
                          '/usr/bin/chef-apply',
                          '/usr/bin/chef-client',
+                         '/usr/bin/chef-service-manager',
                          '/usr/bin/chef-shell',
                          '/usr/bin/chef-solo',
-                         '/usr/bin/chef-zero',
+                         '/usr/bin/chef-vault',
+                         '/usr/bin/chef-windows-service',
                          '/usr/bin/fauxhai',
                          '/usr/bin/foodcritic',
                          '/usr/bin/kitchen',
                          '/usr/bin/knife',
                          '/usr/bin/ohai',
                          '/usr/bin/rubocop',
-                         '/usr/bin/shef',
-                         '/usr/bin/strain',
-                         '/usr/bin/strainer',
                         ]
 end
