ccde2960ba1012507df4797208d0726d4792afb0
Alexey Naidyonov <alexey.naidyonov@itoolabs.com>
2015-04-29 22:33:21 +0300

    Oolite 1.80 added

--- Casks/oolite.rb
+++ Casks/oolite.rb
@@ -2,7 +2,8 @@ cask :v1 => 'oolite' do
   version '1.80'
   sha256 'da6dc76f526dccf29c7c2180353f88979778dd26630cb8c032eaa437c6d197f2'
 
-  url 'https://github.com/OoliteProject/oolite/releases/download/1.80b/Oolite-1.80.zip'
+  # github.com is the official download host per the vendor homepage
+  url "https://github.com/OoliteProject/oolite/releases/download/#{version}b/Oolite-#{version}.zip"
   name 'oolite'
   homepage 'http://www.oolite.org/'
   license :gpl
