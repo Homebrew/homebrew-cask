9532c46533b8ced136fe9dcbea3a649865c48d51
will <githubutilities@163.com>
2015-04-30 11:57:15 +0800

    add pphelper cask

---
+++ Casks/pphelper.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'pphelper' do
+  version :latest
+  sha256 '1a9dee9e9057b533093694869212df0fda1412f756a156b648c042aaa62c333e'
+
+  url 'http://ghost.25pp.com/soft/pp_mac.dmg'
+  name 'pphelper'
+  name 'pp助手'
+  homepage 'http://pro.25pp.com/pp_mac_ios'
+  license :gratis
+
+  app 'pp助手.app'
+end
