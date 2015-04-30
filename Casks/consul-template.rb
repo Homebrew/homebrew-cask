eaf2f6363504e4407b94017f08cc261c070c5c7e
jacoelho <jose.coelho@linkedcare.com>
2015-04-30 17:22:16 +0100

    Update consult-template to 0.9.0
    Add Vault functionality for querying secrets from Vault (GH-264)
    Add regexMatch template helper to determine if a result matches
    the given regular expressions (GH-246)
    Add support for ssl-cert and ss-ca-cert options (GH-255)

--- Casks/consul-template.rb
+++ Casks/consul-template.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'consul-template' do
-  version '0.8.0'
-  sha256 'c7140c50d57e991e8ce200303ec33d002e7dff1a2cedb79b0942cb8fe3123590'
+  version '0.9.0'
+  sha256 'ea054bb62a556ffa58bbad29aa17b370694ba7c49a7539195f9afeafbec16be1'
 
   url "https://github.com/hashicorp/consul-template/releases/download/v#{version}/consul-template_#{version}_darwin_amd64.tar.gz"
   appcast 'https://github.com/hashicorp/consul-template/releases.atom'
