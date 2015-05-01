e4f0f3040dbd76eb2fb586ab560a9d10bc6865ee
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-01 23:12:21 +0300

    speedtao.rb: add name

--- Casks/speedtao.rb
+++ Casks/speedtao.rb
@@ -3,6 +3,7 @@ cask :v1 => 'speedtao' do
   sha256 :no_check
 
   url 'http://www.speedtao.net/beta_latest'
+  name 'SpeedTao'
   homepage 'http://www.speedtao.net/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
