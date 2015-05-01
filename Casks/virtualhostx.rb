67add663a6c4ea509e4534270ed195ba6a01049d
Ben Huebscher <huebs@me.com>
2015-04-30 17:15:46 -0700

    Change VirtualHostX URLs to HTTPS
    Switched URL scheme to HTTPS from HTTP because encryption is good.

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -3,9 +3,9 @@ cask :v1 => 'virtualhostx' do
   sha256 'd0ab4ef0049639e94887dc011dbe36a46fbe963494a39af7ad190417c08620a3'
 
   url "https://downloads-clickonideas.netdna-ssl.com/virtualhostx/virtualhostx60_#{version}.zip"
-  appcast 'http://shine.clickontyler.com/appcast.php?id=30'
+  appcast 'https://shine.clickontyler.com/appcast.php?id=30'
   name 'VirtualHostX'
-  homepage 'http://clickontyler.com/virtualhostx/'
+  homepage 'https://clickontyler.com/virtualhostx/'
   license :commercial
 
   app 'VirtualHostX.app'
