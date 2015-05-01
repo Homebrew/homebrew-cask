7d9dba522ea162ad24ea5a53ddc1b87c4f07833f
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-01 23:13:00 +0300

    splashtop-personal.rb: add name

--- Casks/splashtop-personal.rb
+++ Casks/splashtop-personal.rb
@@ -3,6 +3,7 @@ cask :v1 => 'splashtop-personal' do
   sha256 'a0167ad134b3134e9b3e5e0a26238aed04ef230f3cabcddd7fd0fef9d82687aa'
 
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STP/Splashtop_Personal_v#{version}.dmg"
+  name 'Splashtop Personal'
   homepage 'http://www.splashtop.com/personal'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
