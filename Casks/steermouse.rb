590e4bad4cea9e828fd0ed05f0731268bfd9c760
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-01 23:25:12 +0300

    steermouse.rb: add name

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -3,6 +3,7 @@ cask :v1 => 'steermouse' do
   sha256 'a31773ca4a3f137e63900388a267140af6d92a9260c42f9e026a3b75a38afa04'
 
   url "http://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
+  name 'SteerMouse'
   homepage 'http://plentycom.jp/en/steermouse/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
