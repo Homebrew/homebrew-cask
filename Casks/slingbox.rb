024757fd43716b9cf9a13f4d60341989ee4035c5
dwkns <dwkns@me.com>
2015-05-01 21:29:17 +0100

    Added new cask for the Slingbox Desktop player.

---
+++ Casks/slingbox.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'slingbox' do
+  version '5.0.92'
+  sha256 :no_check
+
+  url 'http://download.slingmedia.com/SlingplayerDesktop/Mac/5.0.92/SlingplayerDesktop-5.0.92.dmg'
+  name 'Slingplayer Desktop'
+  homepage 'http://www.slingbox.com'
+  license :unknown
+
+  app 'Slingplayer Desktop.app'
+end
+
