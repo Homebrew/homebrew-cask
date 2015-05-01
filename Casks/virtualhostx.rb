e21ca03bb0b4e33d0129852c4114425ad984915e
Ben Huebscher <huebs@me.com>
2015-04-30 17:08:27 -0700

    Hardcode Version into VirtualHostX Cask
      * Change URL to version specific format
      * Set current version to 6.0.4
      * Add sha256 fingerprint of download

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'virtualhostx' do
-  version :latest
-  sha256 :no_check
+  version '6.0.4'
+  sha256 'd0ab4ef0049639e94887dc011dbe36a46fbe963494a39af7ad190417c08620a3'
 
-  url 'https://clickontyler.com/virtualhostx/download/v6/'
+  url "https://downloads-clickonideas.netdna-ssl.com/virtualhostx/virtualhostx60_#{version}.zip"
   appcast 'http://shine.clickontyler.com/appcast.php?id=30'
   name 'VirtualHostX'
   homepage 'http://clickontyler.com/virtualhostx/'
