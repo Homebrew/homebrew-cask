994df5f0d69637d45ededcd89edcecb60352b203
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-01 23:15:07 +0300

    sqlitestudio.rb: add name

--- Casks/sqlitestudio.rb
+++ Casks/sqlitestudio.rb
@@ -3,6 +3,7 @@ cask :v1 => 'sqlitestudio' do
   sha256 '5b5e5dd370467ec2d3c0a6c2d1a1d01d2346ad10cbc322a2534fb5ec75579a87'
 
   url "http://sqlitestudio.pl/files/sqlitestudio3/complete/macosx/sqlitestudio-#{version}.dmg"
+  name 'SQLiteStudio'
   homepage 'http://sqlitestudio.pl'
   license :gpl
 
