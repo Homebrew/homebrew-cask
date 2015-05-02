56530b9b0e959025fe2a60adfa3e6f24438848a8
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-02 23:28:02 +0300

    sweet-home3d.rb: add name

--- Casks/sweet-home3d.rb
+++ Casks/sweet-home3d.rb
@@ -4,6 +4,7 @@ cask :v1 => 'sweet-home3d' do
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-#{version}/SweetHome3D-#{version}-macosx.dmg"
+  name 'Sweet Home 3D'
   homepage 'http://www.sweethome3d.com/'
   license :gpl
 
