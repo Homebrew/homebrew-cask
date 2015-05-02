110b0784356afad2274737a6f5239cc924631cb0
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-02 23:21:22 +0300

    subsurface.rb: add name

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -3,6 +3,7 @@ cask :v1 => 'subsurface' do
   sha256 'b4eef933e507ffb5a56501a249c1dfcf389edd3cdfb82f5ac0448183c4b8113c'
 
   url "https://subsurface.hohndel.org/downloads/Subsurface-#{version}.dmg"
+  name 'Subsurface'
   homepage 'http://subsurface.hohndel.org/'
   license :gpl
 
