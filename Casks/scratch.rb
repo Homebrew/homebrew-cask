c59f0f6ff6a65f4073096b483046eec8392ccf9c
tyage <namatyage@gmail.com>
2015-05-02 16:38:20 +0900

    fix for current scratch url

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -1,11 +1,20 @@
 cask :v1 => 'scratch' do
-  version :latest
-  sha256 :no_check
+  version '436'
+  sha256 '9cfad2ccbf24ef085de21130894107c284637321d5415ae68f7327736b533c49'
 
-  url 'http://cdn.scratch.mit.edu/scratchr2/static/sa/Scratch.air'
+  url 'https://scratch.mit.edu/scratchr2/static/sa/Scratch-436.dmg'
   name 'Scratch'
-  homepage 'http://scratch.mit.edu/scratch2download/'
+  homepage 'https://scratch.mit.edu/scratch2download/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  app 'Scratch 2.app'
+  installer :script => 'Install Scratch 2.app/Contents/MacOS/Install Scratch 2',
+            :args   => %w[-silent],
+            :sudo   => true
+
+  uninstall :script => {
+              :executable => '/Applications/Utilities/Adobe AIR Application Installer.app/Contents/MacOS/Adobe AIR Application Installer',
+              :args => %w[-uninstall -silent /Applications/Scratch\ 2.app]
+            }
+
+  depends_on :cask => 'adobe-air'
 end
