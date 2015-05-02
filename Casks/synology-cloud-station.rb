cf21b0dcc5918b2157ae670675cc5a4cab81b093
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-02 23:38:16 +0300

    synology-cloud-station.rb: add name

--- Casks/synology-cloud-station.rb
+++ Casks/synology-cloud-station.rb
@@ -3,6 +3,7 @@ cask :v1 => 'synology-cloud-station' do
   sha256 'c800dca63285cc754b34806c4f0dde11fa1ca4d3d31aa8eeb8ca129555c094e2'
 
   url "https://global.download.synology.com/download/Tools/CloudStation/#{version}/Mac/synology-cloud-station-#{version}.dmg"
+  name 'Synology Cloud Station'
   homepage 'http://www.synology.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
