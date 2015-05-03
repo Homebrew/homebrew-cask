7798e3f477358dec1f29fe03e7fd9280c459c632
Renato Moya <imexto@gmail.com>
2015-05-02 21:47:47 -0300

    Fix calibre cask version.

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'calibre' do
-  version '2.26.0'
-  sha256 'e6dc8f60f085a50f223f03837c1d9f48fc40d8f63e65fc12947f36faf1d812ea'
+  version '2.27.0'
+  sha256 '564a3298715e7f5a2be66ac3a863121ba82b18b881f8f06746027ae101e00e58'
 
   # github.com is an official download host per the vendor homepage, and a faster mirror than the main one
   url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}.dmg"
