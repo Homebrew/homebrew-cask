abcc941f49ed91c84bce55da8cc0c1e5c97b130c
buo <buo@users.noreply.github.com>
2015-05-03 20:43:08 +0900

    air-video-server-hd.rb: add appcast stanza

--- Casks/air-video-server-hd.rb
+++ Casks/air-video-server-hd.rb
@@ -5,6 +5,8 @@ cask :v1 => 'air-video-server-hd' do
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/AirVideoHD/Download/Air+Video+Server+HD+#{version}.dmg"
   name 'Air Video Server HD'
+  appcast 'https://s3.amazonaws.com/AirVideoHD/Download/appcast.xml',
+          :sha256 => 'b4b89e84e52dc52b699826f65fdb04a32b43c87f134ada629904af84fdff1bb4'
   homepage 'http://www.inmethod.com/airvideohd'
   license :gratis
 
