d76cd0d8ecdef2cb74e166818ced64da541517f4
My Name <fdsfgs@krutt.org>
2015-05-03 14:43:28 +0300

    Revert past change in qtox.rb

--- Casks/qtox.rb
+++ Casks/qtox.rb
@@ -2,8 +2,8 @@ cask :v1 => 'qtox' do
   version :latest
   sha256 :no_check
 
-  # libtoxcore.so is the official download host per the vendor homepage
-  url 'https://jenkins.libtoxcore.so/job/qTox%20OS%20X/lastSuccessfulBuild/artifact/qtox.dmg'
+  # tox.im is the official download host per the vendor homepage
+  url 'https://dist-build.tox.im/qtox/osx/qtox.zip'
   name 'qTox'
   name 'Tox'
   homepage 'https://tox.im/'
