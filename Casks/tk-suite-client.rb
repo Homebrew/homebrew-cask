6efea219c09e7f18899b05e3c3bb8abdcc4079ec
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-03 23:52:23 +0300

    tk-suite-client.rb: add name

--- Casks/tk-suite-client.rb
+++ Casks/tk-suite-client.rb
@@ -3,6 +3,7 @@ cask :v1 => 'tk-suite-client' do
   sha256 '3a8ed2a04962d5fe98af24c40aba0a684dc56513440e73b1c4d19b379f7be3b0'
 
   url "ftp://ftp.agfeo.de/pub/software/TK-Suite-Client_intel32_agfeo_#{version}.dmg"
+  name 'TK-Suite Client'
   homepage 'http://agfeo.de/agfeo_web/hp3.nsf/lu/2064'
   license :closed
 
