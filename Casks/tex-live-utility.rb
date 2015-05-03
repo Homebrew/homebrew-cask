b67b936523df27d91040128f48d54d5a81046a0a
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-03 23:38:40 +0300

    tex-live-utility.rb: add name

--- Casks/tex-live-utility.rb
+++ Casks/tex-live-utility.rb
@@ -5,6 +5,7 @@ cask :v1 => 'tex-live-utility' do
   url 'https://github.com/amaxwell/tlutility/releases/download/1.19/TeX.Live.Utility.app-1.19.tar.gz'
   appcast 'https://raw.githubusercontent.com/amaxwell/tlutility/master/appcast/tlu_appcast.xml',
           :sha256 => '9ca9e537751be33f6757984dd7d1d28a84d680bddb53c37fc7942adcd72b0eca'
+  name 'TeX Live Utility'
   homepage 'https://github.com/amaxwell/tlutility'
   license :oss
 
