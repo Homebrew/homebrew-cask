f30ce73a4021c84130d81646c5dc8edce12364c7
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-03 23:45:14 +0300

    thebrain.rb: add name

--- Casks/thebrain.rb
+++ Casks/thebrain.rb
@@ -3,6 +3,7 @@ cask :v1 => 'thebrain' do
   sha256 '2ac492c6defc6d1156de434e84272daf9deb26edf7581da737ac077b244ad7f7'
 
   url "http://assets.thebrain.com/downloads/TheBrain_macos_J7_#{version.gsub('.', '_')}-a.dmg"
+  name 'TheBrain'
   homepage 'http://www.thebrain.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
