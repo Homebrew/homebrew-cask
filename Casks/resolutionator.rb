afb38ea96f16c517c49f0537f1be8c5887eb9a37
Bret Comnes <bcomnes@gmail.com>
2015-05-03 11:09:56 -0700

    Added resolutionator.app

---
+++ Casks/resolutionator.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'resolutionator' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://manytricks.com/download/resolutionator'
+  name 'Resolutionator'
+  homepage 'http://manytricks.com/blog/?cat=34'
+  license :gratis
+
+  app 'Resolutionator.app'
+end
