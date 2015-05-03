c3de088265a4ac9389f4a8d27f49ee654bd1cf87
Andy Chase <asperous2@gmail.com>
2015-05-03 14:46:14 -0700

    Add techstoreclub-simple-comic.rb

---
+++ Casks/techstoreclub-simple-comic.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'techstoreclub-simple-comic' do
+  version '1.7_252'
+  sha256 '4ddd18a02a79fc8201824e6ab99291c6d4c8680f79f94bc372bf71f0535def35'
+
+  url 'https://github.com/techstoreclub/Simple-Comic/releases/download/1.7_252/SimpleComic_1.7_252.zip'
+  name 'Simple Comic'
+  homepage 'https://github.com/techstoreclub/Simple-Comic'
+  license :mit
+
+  app 'Simple Comic.app'
+end
