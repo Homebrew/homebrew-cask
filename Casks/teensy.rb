d0bb41a159fae76dfe6ca075c6fda5d27d7a7fbc
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-03 23:35:54 +0300

    teensy.rb: add name

--- Casks/teensy.rb
+++ Casks/teensy.rb
@@ -3,6 +3,7 @@ cask :v1 => 'teensy' do
   sha256 '8a273dc915afa5752b9283168819dc0191462d4bd7eb990748a87e90d4b9baa8'
 
   url 'https://www.pjrc.com/teensy/teensy.dmg'
+  name 'Teensy'
   homepage 'http://pjrc.com/teensy/loader_mac.html'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
