16af57cd01b7d8fd2fb76ef4d186017a9558298e
Vítor Galvão <info@vitorgalvao.com>
2015-05-04 16:29:50 +0100

    Create rightfont.rb

---
+++ Casks/rightfont.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'rightfont' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://rightfontapp.com/download/rightfont_beta.zip'
+  name 'RightFont'
+  homepage 'http://rightfontapp.com/'
+  license :unknown
+
+  app 'RightFont.app'
+end
