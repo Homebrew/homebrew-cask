ca503521f48dd7470b727eecaeba92e1663b250f
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-04 23:36:23 +0300

    tonido.rb: add name

--- Casks/tonido.rb
+++ Casks/tonido.rb
@@ -3,6 +3,7 @@ cask :v1 => 'tonido' do
   sha256 :no_check
 
   url 'http://www.tonido.com/download.php?TonidoSetup.dmg'
+  name 'Tonido'
   homepage 'http://www.tonido.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
