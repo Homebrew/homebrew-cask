211b2b6f75809f6772437c51ad78abaab8381941
yonix <xxyonixx@gmail.com>
2015-05-04 18:24:57 +0200

    Andy fix

--- Casks/andy.rb
+++ Casks/andy.rb
@@ -2,8 +2,8 @@ cask :v1 => 'andy' do
   version '43'
   sha256 'ed55fb3a389a0afa62043fbdbe31e418aa96b789a743d987903a817fe4a8c75b'
 
-  # andyroid.vonetize.com.edgesuite.net is the official download host per the vendor homepage
-  url "http://andyroid.vonetize.com.edgesuite.net/Mac/v#{version}/Andy_OSX_v43.dmg"
+  # edgesuite.net is the official download host per the vendor homepage
+  url "http://andyroid.vonetize.com.edgesuite.net/Mac/v#{version}/Andy_OSX_v#{version}.dmg"
   name 'Andy'
   homepage 'http://www.andyroid.net/'
   license :commercial
