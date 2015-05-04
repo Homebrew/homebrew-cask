505a0a3e8618aed6e3bcbd394918fc4a3b97a028
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-04 23:40:53 +0300

    tortoisehg.rb: add name

--- Casks/tortoisehg.rb
+++ Casks/tortoisehg.rb
@@ -3,6 +3,7 @@ cask :v1 => 'tortoisehg' do
   sha256 'c529539e20f4ff2f6d181a5c7f64ad5dcd9346605011ab4c3725c7cedf6b5a8c'
 
   url "https://bitbucket.org/tortoisehg/files/downloads/TortoiseHg-#{version}-mac-x64.zip"
+  name 'TortoiseHg'
   homepage 'http://tortoisehg.bitbucket.org/'
   license :gpl
 
