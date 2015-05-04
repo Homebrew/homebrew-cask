1fc168134cb6caf187bcae0b5e49640e063b0147
Murray Tannock <murraytannock@gmail.com>
2015-05-04 09:54:59 +0000

    Update julia to v0.3.8

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'julia' do
-  version '0.3.7'
-  sha256 'f83e42c42539518adc639b38a62bb7f0f31e3bfa879e5d43b688f06082556a8c'
+  version '0.3.8'
+  sha256 '078afe227d5f429869494376f130b344ff149175dd4656f5abc736af802dfb7d'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/julialang/bin/osx/x64/#{version.sub(/\.\d+$/, '')}/julia-#{version}-osx10.7+.dmg"
