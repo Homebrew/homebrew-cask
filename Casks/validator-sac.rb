bc4ca2af74c2698c25c651aca48978a9210af527
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-05 22:35:05 +0300

    validator-sac.rb: add name

--- Casks/validator-sac.rb
+++ Casks/validator-sac.rb
@@ -3,6 +3,7 @@ cask :v1 => 'validator-sac' do
   sha256 '45ed10a41d49cdce8c048dae50f96abc78d9a45d5d0137d5757ed69de333a67f'
 
   url "http://habilis.net/download/Validator-SAC_#{version}.zip"
+  name 'Validator S.A.C.'
   homepage 'http://habilis.net/validator-sac/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
