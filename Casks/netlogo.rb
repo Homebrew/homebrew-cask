391f5b486df7c0e0861beeecfab1d3345a2f61eb
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2015-05-05 15:57:22 +0200

    Update netlogo.rb
    Closes #11028.

--- Casks/netlogo.rb
+++ Casks/netlogo.rb
@@ -2,7 +2,7 @@ cask :v1 => 'netlogo' do
   version '5.2.0'
   sha256 '72ea2f242e3f580ad19250361d792628f4e95e6996be9007d8b21ffd134d6252'
 
-  url "https://ccl.northwestern.edu/netlogo/#{version}/NetLogo%20#{version}.dmg"
+  url "https://ccl.northwestern.edu/netlogo/#{version}/NetLogo%20#{version.sub(%r{\.\d+$},'')}.dmg"
   name 'NetLogo'
   homepage 'http://ccl.northwestern.edu/netlogo/'
   license :gpl
