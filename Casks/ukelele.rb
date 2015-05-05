e8da855d64388220eae1af1ed221b0b8e9dbfb83
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-05 22:27:12 +0300

    ukelele.rb: add name

--- Casks/ukelele.rb
+++ Casks/ukelele.rb
@@ -5,6 +5,7 @@ cask :v1 => 'ukelele' do
   url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Ukelele_#{version}&filename=Ukelele_#{version}.dmg"
   appcast 'http://scripts.sil.org/cms/scripts/render_download.php?site_id=nrsi&format=file&media_id=ukelele_su_feed&filename=ukelele_su_feed.xml',
           :sha256 => '5409cb8f49f6de2b0a4c2d638a637d2c460c79532914c57ae87adc4551ed8d16'
+  name 'Ukelele'
   homepage 'http://scripts.sil.org/ukelele'
   license :gratis
 
