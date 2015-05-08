3eeac5d8462a4ebc670da2682de0d1087ff53eaa
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-08 23:19:29 +0300

    quickpvr.rb: add name

--- Casks/quickpvr.rb
+++ Casks/quickpvr.rb
@@ -3,6 +3,7 @@ cask :v1 => 'quickpvr' do
   sha256 '806ddbe5615c3ec73086783b63475fa07ee589031e2c90a9a807e8f1b9584391'
 
   url 'http://www.limbic.com/downloads/QuickPVR_v4.zip'
+  name 'QuickPVR'
   homepage 'http://www.limbic.com/quickpvr.html'
   license :bsd
 
