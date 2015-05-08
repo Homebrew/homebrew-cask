4d47baf20bc747ac91289e5591893347e6b303c7
Steven Maguire <stevenmaguire@gmail.com>
2015-05-07 22:19:23 -0500

    adding sha256

--- Casks/polar-websync.rb
+++ Casks/polar-websync.rb
@@ -1,16 +1,17 @@
 cask :v1 => 'polar-websync' do
   version '2.8.2'
-  sha256 :no_check
+  sha256 '287230ee323cca9887f31e448cbac759c770448e25c31c164b87f69bb3ead9b9'
 
   url "https://www.polarpersonaltrainer.com/downloads/websync_#{version}.dmg"
   name 'Polar WebSync Software'
   homepage 'http://www.polar.com/us-en/support/downloads/Polar_WebSync_Software'
   license :closed
+
   caveats "Installation of this application requires user input."
 
   pkg 'Polar WebSync.pkg'
 
   uninstall :pkgutil => 'fi.polar.websync.pkg',
-            :delete => '/Applications/WebSync.app',
-            :quit => 'fi.polar.WebSync'
+            :quit => 'fi.polar.WebSync',
+            :delete => '/Applications/WebSync.app'
 end
