8c54091e8111cb224a7c63b268e869b714f82e0c
Steven Maguire <stevenmaguire@gmail.com>
2015-05-07 19:36:09 -0500

    adding uninstall routines

--- Casks/polar-websync.rb
+++ Casks/polar-websync.rb
@@ -1,12 +1,16 @@
 cask :v1 => 'polar-websync' do
   version '2.8.2'
   sha256 :no_check
+
   url "https://www.polarpersonaltrainer.com/downloads/websync_#{version}.dmg"
   name 'Polar WebSync Software'
   homepage 'http://www.polar.com/us-en/support/downloads/Polar_WebSync_Software'
   license :closed
+  caveats "Installation of this application requires user input."
+
   pkg 'Polar WebSync.pkg'
-  zap :delete => [
-        '/Applications/WebSync.app'
-    ]
+
+  uninstall :pkgutil => 'fi.polar.websync.pkg',
+            :delete => '/Applications/WebSync.app',
+            :quit => 'fi.polar.WebSync'
 end
