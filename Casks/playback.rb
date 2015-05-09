bb00e71b216b2b2029b09db3defb05a631cc5514
Vincent Klaiber <vincentklaiber@gmail.com>
2015-05-09 13:32:52 +0200

    Update Playback.app to version 1.3.1

--- Casks/playback.rb
+++ Casks/playback.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'playback' do
-  version '1.3.0'
-  sha256 'e161c0589f57f840428e946f3d12301377cdcb9aaba79a5f39c1a5048314b944'
+  version '1.3.1'
+  sha256 'bffca6b43363b8a0a511eae6b3376d0bd0fbcf2c04a6d810cf09acbcd087bb38'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/mafintosh/playback/releases/download/v#{version}/Playback.app.zip"
