533e5ba19a63e008b1f4e8f42e38158a17d5d97e
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-09 20:58:13 +0300

    futuniuniu.rb: add name

--- Casks/futuniuniu.rb
+++ Casks/futuniuniu.rb
@@ -3,6 +3,8 @@ cask :v1 => 'futuniuniu' do
   sha256 '9e18b41ec55732af06c04e5fad8d1e1e49bdc4846738120b44ad82003787b1dc'
 
   url "https://www.futu5.com/client/nn/mac/FTNNForMac_#{version}.dmg"
+  name '富途牛牛'
+  name 'FutuNiuniu'
   homepage 'http://www.futu5.com'
   license :commercial
 
