a936cdcc971f0f2157c2e1cda6fddc95ba0006ac
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-09 21:04:42 +0300

    kid3.rb: add name

--- Casks/kid3.rb
+++ Casks/kid3.rb
@@ -4,6 +4,7 @@ cask :v1 => 'kid3' do
   sha256 'da90a482d92164e3fbc2493b10aab9ddf3669f3feec14fbfdf4eafbed78aab95'
 
   url "http://downloads.sourceforge.net/sourceforge/kid3/kid3-#{version}-Darwin.dmg"
+  name 'Kid3'
   homepage 'http://kid3.sourceforge.net/'
   license :gpl
 
