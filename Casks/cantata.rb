8afceed9b37b93b013f60ff8db99aecf4470d8e0
Lukas Eipert <git@leipert.io>
2015-05-10 23:25:00 +0200

    Added comment to cantana.

--- Casks/cantata.rb
+++ Casks/cantata.rb
@@ -2,6 +2,7 @@ cask :v1 => 'cantata' do
   version '1.5.2'
   sha256 '564eec8b60d7e46670b8c67686db8a69210f540626bdbaf8c837b48c0bd2ffc8'
 
+  # google.com is the official download host per the vendor homepage
   url 'https://drive.google.com/uc?export=download&id=0Bzghs6gQWi60WmNlSDh6dTVzYTg'
   name 'cantata'
   homepage 'https://github.com/cdrummond/cantata'
