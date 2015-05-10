f9af510a8657746f529fd301dbde7a7f31f34675
Lukas Eipert <git@leipert.io>
2015-05-10 23:21:47 +0200

    Updated subler cask, appcast is not using google code.

--- Casks/subler.rb
+++ Casks/subler.rb
@@ -3,7 +3,7 @@ cask :v1 => 'subler' do
   sha256 'b9894827ae93ecd2e087cf333cd79f0a622ec4ac3a06737a418635e26940ca7a'
 
   url "https://bitbucket.org/galad87/subler/downloads/Subler-#{version}.zip"
-  appcast 'http://subler.googlecode.com/svn/doc/appcast.xml',
+  appcast 'http://subler.org/appcast/appcast.xml',
           :sha256 => 'b8d659a84089c8ebd4969c7e5393b0472d844c79d43bad33cbfbbe153f2dfb51'
   name 'Subler'
   homepage 'https://bitbucket.org/galad87/subler'
