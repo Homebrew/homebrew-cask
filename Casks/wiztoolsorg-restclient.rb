388c5963f405c9f73a4032f1624982197fbc23cc
Lukas Eipert <git@leipert.io>
2015-05-10 21:03:12 +0200

    Updated wiztoolsorg-restclient cask. Not using google code anymore.

--- Casks/wiztoolsorg-restclient.rb
+++ Casks/wiztoolsorg-restclient.rb
@@ -2,9 +2,9 @@ cask :v1 => 'wiztoolsorg-restclient' do
   version '3.2.2'
   sha256 'd920279ef6406acb02a12485e1e8adaf1942cf196f0628c8e6b40744f6120aac'
 
-  url "https://rest-client.googlecode.com/files/restclient-ui-#{version}.dmg"
+  url "http://download.wiztools.org/rest-client/archive/restclient-ui-#{version}.dmg"
   name 'RESTClient'
-  homepage 'https://code.google.com/p/rest-client'
+  homepage 'https://github.com/wiztools/rest-client'
   license :oss
 
   app 'WizTools.org RESTClient.app'
