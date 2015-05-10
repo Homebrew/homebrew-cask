3aec555d0e7675cdf89354e7c6020e444843a9db
Lukas Eipert <git@leipert.io>
2015-05-10 20:49:37 +0200

    Changed dukto cask. Not using google code anymore.
    Closes #11115.

--- Casks/dukto.rb
+++ Casks/dukto.rb
@@ -2,9 +2,10 @@ cask :v1 => 'dukto' do
   version 'R6'
   sha256 '86f06ca92a28b5835cb3eaadfb09897f581a5da3fb8ee8246897b1bf4ee7348a'
 
-  url "https://dukto.googlecode.com/files/Dukto#{version}-OSX.dmg"
+  # sourceforge.net is the official download host per the vendor homepage
+  url "https://downloads.sourceforge.net/sourceforge/dukto/Dukto#{version}-OSX.dmg"
   name 'Dukto'
-  homepage 'https://code.google.com/p/dukto/'
+  homepage 'http://www.msec.it/dukto/'
   license :oss
 
   app 'Dukto.app'
