c9e4a11c35f9e07d07329c6e20b364091967170d
Marcel Asio <marcel@asio.se>
2015-05-10 09:57:50 +0200

    Update cask yEd to latest version 3.14.2

--- Casks/yed.rb
+++ Casks/yed.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'yed' do
-  version '3.14.1'
-  sha256 '1db07a4e108c13ed4e28ebec30d6acf4fe7b593a78ceeeed3e5a1c9d1132c512'
+  version '3.14.2'
+  sha256 '8d15d7bcc04ec5ab15f11934158ce97179642ba45a87b0b3bb15e8d712ce43ee'
 
   url "https://www.yworks.com/products/yed/demo/yEd-#{version}_with-JRE8.dmg"
   name 'yEd'
