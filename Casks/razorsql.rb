3be27fc4d9d1dafbbecfd31aa4ea6f571aaeaea8
John Dyer <jdyer@tropo.com>
2015-05-11 10:42:05 -0400

    Update razorsql to latest version ( 6.3.23 )

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'razorsql' do
-  version '6.3.17'
-  sha256 '8a95889f9fbb5761d09245a2c9da0fd76f51d369930f1e570ab60d7e84d129a7'
+  version '6.3.23'
+  sha256 'a57fc8c92b8ccda950be3da6f15a2dbfdc6eff7462bc4f477e0bc95877f5fbb0'
 
   url "http://downloads.razorsql.com/downloads/#{version.gsub('.', '_')}/razorsql#{version.gsub('.', '_')}_x64.dmg"
   name 'RazorSQL'
