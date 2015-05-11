efff98ff3a3ee56e7eec26fa775172a7bda2e400
Herb <i@4leaf.me>
2015-05-11 10:20:57 +0800

    update blackvue-viewer to 1.30

--- Casks/blackvue-viewer.rb
+++ Casks/blackvue-viewer.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'blackvue-viewer' do
-  version '1.25'
-  sha256 '80135f4d537a5ce67ed465a58b29e4701c6f2672c7b012ab6f6bea2c4d884fcc'
+  version '1.30_20150324'
+  sha256 'f78f76f2e799cc3f4faf0254b270e5c4a8d59a499a09332f4b11af338f521bba'
 
-  url "http://blackvue.com/en/common/downloadHIT.asp?downfile=BlackVue%20Viewer_#{version}.zip&path=board&idx=1431&sort=0"
+  url "http://blackvue.com/en/common/downloadHIT.asp?downfile=BlackVue%20Viewer_#{version.sub(%r{^(\d+)\.(\d+)_.*},'\1.\2')}.zip&path=board&idx=1431"
   name 'BlackVue Viewer'
   homepage 'http://blackvue.com'
   license :closed
