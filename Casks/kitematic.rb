7d07afbc1fb6b7a79dcd4caa86b68f218d19a023
Dmitry Grach <dmitrygrach@gmail.com>
2015-05-12 13:18:04 +0300

    Update Kitematic to 0.5.25

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'kitematic' do
-  version '0.5.23'
-  sha256 'e644f2f45fd9074f4446a54660a9f7d4a13f07d3349bde0ba3cbd6b587132b71'
+  version '0.5.25'
+  sha256 '18a322cb61a6009f41712135d86ff064e8e33183bdc4b9ae086656ff026271a8'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}.zip"
