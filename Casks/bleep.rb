b43849a57d340027eee2e0c3ff05e1a1952b2e08
Mark Gardner <mjg+github@phoenixtrap.com>
2015-05-12 18:02:32 -0400

    Update Bleep.app homepage, add vendor and zap ~/Library files

--- Casks/bleep.rb
+++ Casks/bleep.rb
@@ -5,8 +5,16 @@ cask :v1 => 'bleep' do
   # utorrent.com is the official download host per the vendor homepage
   url 'https://download-new.utorrent.com/endpoint/bleep/os/osx/track/stable/'
   name 'Bleep'
-  homepage 'http://labs.bittorrent.com/bleep/'
+  homepage 'http://www.bleep.pm/'
   license :gratis
+  tags :vendor => 'BitTorrent'
 
   app 'Bleep.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Bleep',
+                  '~/Library/Caches/com.bittorrent.bleep.osx',
+                  '~/Library/Preferences/com.bittorrent.bleep.osx.plist',
+                  '~/Library/Saved Application State/com.bittorrent.bleep.osx.savedState',
+                 ]
 end
