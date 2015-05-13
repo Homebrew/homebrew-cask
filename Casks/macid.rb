0204616f786c5995a487f9e4e3acde39c19e1978
lyuha <beingfunnylife@gmail.com>
2015-05-13 22:06:13 +0900

    Upgrade MacID.app to v1.2

--- Casks/macid.rb
+++ Casks/macid.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'macid' do
-  version '1.1b14'
-  sha256 '80f9ac0b682f53b4c28c5958c0e4dddb3804aac31d48751109805865b9f3fbae'
+  version '1.2b'
+  sha256 '71dd713f6090b0c6038c274fa32bdb59bb8cfe7f39ec06c252f71dcae932e6a6'
 
   url "http://macid.co/app/#{version.sub(%r{b.*},'')}/#{version.sub(%r{.*b},'')}/MacID-for-OS-X.zip"
   name 'MacID'
