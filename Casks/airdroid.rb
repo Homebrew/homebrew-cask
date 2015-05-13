3fb5b58bbf1057abb29aafe174b3aa9852f4b32d
Vítor Galvão <info@vitorgalvao.com>
2015-05-13 13:53:42 +0100

    airdrop.rb: fix sha256

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'airdroid' do
   version '3.1.0'
-  sha256 '32272e2b938453ccadd3130895689f6d527128ddc716d95df5741d334e88ff59'
+  sha256 'a6909a15b47c50c376c1cddd2690bf89fa1c8992063cf286b0ba5a3df16c6d6d'
 
   url "http://dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
   name 'AirDroid'
