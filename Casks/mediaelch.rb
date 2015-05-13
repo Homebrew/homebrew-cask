fc4242e7b8ada2cbbb66eb036d079e83242a2807
Ethan Bruning <ebruning@gmail.com>
2015-05-13 15:02:12 -0700

    Update MediaElch to 2.2.2

--- Casks/mediaelch.rb
+++ Casks/mediaelch.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mediaelch' do
-  version '2.1.2'
-  sha256 '6387b815a81de7526521811b90cc9f7b3f56821f6c01b22f25cae3f3b5d46efe'
+  version '2.2.2'
+  sha256 '0968767db15fe505ed39359566ef446badcdd4ccbd880b40dbd3b4f609a6e9aa'
 
   url "http://www.kvibes.de/releases/mediaelch/#{version}/MediaElch-#{version}.dmg"
   name 'MediaElch'
