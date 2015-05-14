35b8158e3d29606748eb4fa4eeddc71d28947c30
Vladimir Lyzo <vladimir.lyzo@saltpepper.ru>
2015-05-14 20:41:35 +0300

    Update eagle up to 7.3.0

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -1,15 +1,15 @@
 cask :v1 => 'eagle' do
-  version '7.2.0'
-  sha256 '9cae311072d8be5a16631ce08d9e0653bdc21e336cc90df2463d7df35521ff2a'
+  version '7.3.0'
+  sha256 '5dcda474ac96c65d5948e85f0f638706d5f3da105b29c796f076ad79047bcbf4'
 
   # cadsoft.de is the official download host per the vendor homepage
-  url "http://web.cadsoft.de/ftp/eagle/program/#{version.sub(%r{\.\d+$},'')}/eagle-mac-#{version}.zip"
+  url "http://web.cadsoft.de/ftp/eagle/program/#{version.sub(%r{\.\d+$},'')}/eagle-mac64-#{version}.zip"
   name 'EAGLE'
   homepage 'http://www.cadsoftusa.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
   tags :vendor => 'CadSoft'
 
-  pkg "eagle-#{version}.pkg"
+  pkg "eagle-mac64-#{version}.pkg"
 
   uninstall :pkgutil => 'com.CadSoftComputerGmbH.EAGLE',
             :delete => "/Applications/EAGLE-#{version}"
