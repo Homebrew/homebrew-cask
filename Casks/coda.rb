0a08e3c23b3ae7013c7b5d1f755b6f86d4525975
buo <buo@users.noreply.github.com>
2015-05-15 18:23:51 +0800

    Update Coda to 2.5.10

--- Casks/coda.rb
+++ Casks/coda.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'coda' do
-  version '2.5.8'
-  sha256 '8c8ac78f50e256fbe8a3c7311bc7f1aff2c7482bf4919c841675d360566674f5'
+  version '2.5.10'
+  sha256 'bb2e9c9f5b5e3a1afb1cf9cb8884afd8c833d5aa7fea7b3b9c7254da1de1fead'
 
   url "https://download.panic.com/coda/Coda%20#{version}.zip"
   appcast 'http://www.panic.com/updates/update.php'
