4109d5cfbb7bc809552dcd989536c405ec3507d4
Xie Yanbo <xieyanbo@gmail.com>
2015-05-15 16:43:08 +0800

    Updated synology-cloud-station to version 3475

--- Casks/synology-cloud-station.rb
+++ Casks/synology-cloud-station.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'synology-cloud-station' do
-  version '3423'
-  sha256 'c800dca63285cc754b34806c4f0dde11fa1ca4d3d31aa8eeb8ca129555c094e2'
+  version '3475'
+  sha256 'ce777034689f05a2b03d937addeb2b2560f912288f8638df8188050672e55e7d'
 
   url "https://global.download.synology.com/download/Tools/CloudStation/#{version}/Mac/synology-cloud-station-#{version}.dmg"
   name 'Synology Cloud Station'
