3c4198d7da36400ebea745af9e10f27bdf20012e
Reinhard Pointner <rednoah@users.noreply.github.com>
2015-05-16 22:28:02 +0800

    sencha.rb: added postflight
    Is there any way to change permissions after install? Maybe cask formulae support that? Maybe some post-install hook?

--- Casks/sencha.rb
+++ Casks/sencha.rb
@@ -15,18 +15,23 @@ cask :v1 => 'sencha' do
                          :args => ['--mode', 'unattended']
                        }
 
+  postflight do
+    system  '/usr/bin/sudo', '-E', '--',
+            '/usr/sbin/chown', '-R', '--', "#{Etc.getpwuid(Process.euid).name}:staff", '/opt/Sencha'
+  end
+
   caveats do
     <<-EOS.undent
       Installing this Cask means you have AGREED to the Sencha Cmd License
 
         http://www.sencha.com/legal/sencha-cmd-license
 
-      Sencha Cmd adds two changes to your ~/.bashrc file:
+      Sencha Cmd appends 2 lines to your ~/.bashrc or ~/.profile file:
 
         export PATH=/opt/Sencha/Cmd/#{version}:$PATH
         export SENCHA_CMD_3_0_0="/opt/Sencha/Cmd/#{version}"
 
-      If you are a zshell user, copy at the end of your .zshrc file both lines.
+      If you are a zshell user, append both lines to your .zshrc file.
     EOS
   end
 end
