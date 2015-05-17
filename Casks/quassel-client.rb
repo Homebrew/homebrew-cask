7a649c5506dfa6a3558c42cdb07e2eb8e6f113da
Jochen Schalanda <jochen@schalanda.name>
2015-05-17 18:04:25 +0200

    Update Quassel IRC to version 0.12.2

--- Casks/quassel-client.rb
+++ Casks/quassel-client.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'quassel-client' do
-  version '0.11.0'
-  sha256 '00e1a1b7a3fc0b5c1c382c488ab93d533a183896ca236d6170cc11e1fc28ac96'
+  version '0.12.2'
+  sha256 'fee7d5db4980a91b8a812b46f5420cf11b9185938679f7ca3191bf66327bdd2d'
 
   url "http://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_#{version}.dmg"
   name 'Quassel IRC'
