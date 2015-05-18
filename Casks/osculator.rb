b4c74a73ca9bedbab683db594ec257a4b75124fa
Camille Troillard <camille@osculator.net>
2015-05-18 11:22:34 +0200

    Update osculator.rb
    Closes #11242.

--- Casks/osculator.rb
+++ Casks/osculator.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'osculator' do
-  version '2.13.1'
-  sha256 '3565e9044b541149630235326a441369012ac6d98c54231c23975a53dc69dca5'
+  version '2.13.2-12-gbab8aab'
+  sha256 'adfa94655876daebee61ddcf2bfe3781c1591d52bcc026044d4a51c5ee979ea4'
 
-  url "http://dl.osculator.net/releases/osculator-#{version}-20-g81f7dd4.dmg"
+  url "http://dl.osculator.net/releases/osculator-#{version}.dmg"
   name 'OSCulator'
   homepage 'http://www.osculator.net'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
