777aff3d71bd0f5af94b6476e389c30c77f01afc
Adam Scott <ascott.ca@gmail.com>
2015-05-17 21:09:19 -0400

    Upgrade GOG.com Galaxy Client to v1.0.2.939

--- Casks/gog-galaxy.rb
+++ Casks/gog-galaxy.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'gog-galaxy' do
-  version '1.0.0.836'
-  sha256 '878319173e0f157981fd3d5009e451653024bdb8e3b4f0ff5565c491ba815f4d'
+  version '1.0.2.939'
+  sha256 '2c46d7aafe862bc0b2aa6b4bc95490683f8ec80f96feffb7c6f7918e2203e4e7'
 
-  url "http://cdn.gog.com/open/galaxy/client/galaxy_client_#{version}.pkg"
+  url "http://cdn.gog.com/open/galaxy/client/installers/galaxy_client_#{version}.pkg"
   name 'GOG Galaxy Client'
   homepage 'http://www.gog.com/galaxy'
   license :gratis
