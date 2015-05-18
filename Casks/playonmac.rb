be675c6dcd2fe608270ab636c5bc01488dd23abe
Kristian Antonsen <kristian@derfor.dk>
2015-05-18 20:02:17 +0200

    Upgrade PlayOnMac.app to v2.4.8

--- Casks/playonmac.rb
+++ Casks/playonmac.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'playonmac' do
-  version '4.2.7'
-  sha256 '06d3bd8220424913bd893612c2584a1eee1367b56780c9223fee40b418001590'
+  version '4.2.8'
+  sha256 '11f0c6a24486c45bbc4d0d01afa27fcaea361278289e9db8277a9fe54ffbe4f0'
 
   url "http://repository.playonmac.com/PlayOnMac/PlayOnMac_#{version}.dmg"
   name 'PlayOnMac'
