a82c6c9296a1f17737a4e6035c4692f93a3fa2de
buo <buo@users.noreply.github.com>
2015-05-19 00:51:35 +0800

    Update Pinegrow Web Designer to 2.01

--- Casks/pinegrow-web-designer.rb
+++ Casks/pinegrow-web-designer.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pinegrow-web-designer' do
-  version '2.00'
-  sha256 '9cfaf6538e52d00022f928dd109f1bb064af7f4a42c01cce83c10ba18957a081'
+  version '2.01'
+  sha256 'f6811ca1cda6fcb370a5dbdc7742cc4aa7b2f26b7573d7ad75f83a5c10860782'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://pinegrow.s3.amazonaws.com/PinegrowMac.#{version}.dmg"
