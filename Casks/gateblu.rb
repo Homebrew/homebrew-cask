5d5d1c8c163148623b185f8bcf3e860b128503fa
Sqrt of Saturn <sqrt@octoblu.com>
2015-05-20 11:11:09 -0700

    Fix version and sha256 stanzas

---
+++ Casks/gateblu.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'gateblu' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://s3-us-west-2.amazonaws.com/gateblu/Gateblu.dmg'
+  name 'Gateblu'
+  homepage 'https://gateblu.octoblu.com/'
+  license :mit
+
+  app 'Gateblu.app'
+end
