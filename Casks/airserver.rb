2c36d45052cae0658f67b8cd433a8fa000382a51
Niels van Zwieten <niels@vanzwieten.pro>
2015-05-20 16:41:06 +0200

    Upgrade AirServer to version 5.3.2

--- Casks/airserver.rb
+++ Casks/airserver.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'airserver' do
-  version '5.2'
-  sha256 '021cf849f121df9f6b91d181b917240a76b6f129ef4676d0b491d0bc54e4b06b'
+  version '5.3.2'
+  sha256 '554e664324a00ebfb263ab4a2813a697425b21182509478ad88d324f1a704b81'
 
   url "http://dl.airserver.com/mac/AirServer-#{version}.dmg"
   name 'AirServer'
