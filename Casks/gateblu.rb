bf274248ae6f1c87ac623d3983cf47e605e361f2
Sqrt of Saturn <sqrt@octoblu.com>
2015-05-20 16:25:55 -0700

    Adding gateblu to cask

---
+++ Casks/gateblu.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'gateblu' do
+  version :latest
+  sha256 :no_check
+
+  # amazonaws.com is the official download host per the vendor homepage
+  url 'https://s3-us-west-2.amazonaws.com/gateblu/Gateblu.dmg'
+  name 'Gateblu'
+  homepage 'https://gateblu.octoblu.com'
+  license :mit
+
+  app 'Gateblu.app'
+end
