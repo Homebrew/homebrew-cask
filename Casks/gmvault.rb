c3feb1c18cfeb2900d27fa6b206f9a94ad32d6a5
jota3g <jeremie@asaitec.eu>
2015-05-21 00:19:23 +0200

    Add Gmvault binary v1.9

---
+++ Casks/gmvault.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'gmvault' do
+  version '1.9'
+  sha256 'a48fb7fb90a7286a47d255a289e7636b1ac8f22f990fa5ce782e6077a6c9a45e'
+
+  # bitbucket.org is the official download host per the vendor homepage
+  url "https://bitbucket.org/gaubert/gmvault-official-download/downloads/gmvault-v#{version}-macosx-intel.tar.gz"
+  name 'Gmvault'
+  homepage 'http://gmvault.org'
+  license :gpl
+
+  binary "gmvault-v#{version}/bin/gmvault"
++end
