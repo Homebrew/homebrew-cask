bddf8d4b7feae4bdf3ecb3c697b4d3ac68b0447c
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-22 20:19:59 +0300

    beotsmusic.rb: add appcast

--- Casks/beotsmusic.rb
+++ Casks/beotsmusic.rb
@@ -5,6 +5,8 @@ cask :v1 => 'beotsmusic' do
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kiding/beotsmusic/releases/download/#{version}/BeotsMusic.dmg"
   name 'BeotsMusic'
+  appcast 'https://raw.github.com/kiding/beotsmusic/master/appcast.xml',
+          :sha256 => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   homepage 'http://beotsmusic.kiding.net/'
   license :mit
 
