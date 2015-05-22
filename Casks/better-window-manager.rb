eaa842c41baa70cb8a287e1ad07a383ba18f0818
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-22 20:19:59 +0300

    better-window-manager.rb: add appcast

--- Casks/better-window-manager.rb
+++ Casks/better-window-manager.rb
@@ -4,6 +4,8 @@ cask :v1 => 'better-window-manager' do
 
   url 'http://www.gngrwzrd.com/BetterWindowManager.zip'
   name 'Better Window Manager'
+  appcast 'http://www.gngrwzrd.com/betterwindowmanager-appcast.xml',
+          :sha256 => '321b6bbe7df6f95f49b0c855748d4da1cee655e0ce252988686825068b02f6d9'
   homepage 'http://www.gngrwzrd.com/better-window-manager/'
   license :commercial
 
