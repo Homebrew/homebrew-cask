4b052d32a4cd55f2df19ca83db8ca82ab738c33b
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-22 20:19:51 +0300

    adwaremedic.rb: add appcast

--- Casks/adwaremedic.rb
+++ Casks/adwaremedic.rb
@@ -4,6 +4,8 @@ cask :v1 => 'adwaremedic' do
 
   url 'http://www.adwaremedic.com/AdwareMedic.dmg'
   name 'AdwareMedic'
+  appcast 'http://www.adwaremedic.com/versions/updates.xml',
+          :sha256 => 'f93747bc91165da2321ad80277177eec745e316dc6fd7c5c511b72c8a521ad4f'
   homepage 'http://www.adwaremedic.com/'
   license :gratis
 
