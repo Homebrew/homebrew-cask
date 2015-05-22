0e5d2e74f5c25d6cc727bdf1a71cee613f10b669
Frederik Winkelsdorf <winkelsdorf@gmail.com>
2015-05-23 01:30:59 +0200

    Reordering Cask Stanza, Added AWS Comment (+1 squashed commit) Squashed commits: [1df5c40] Added cask for deploymate 1.2.8 (+3 squashed commits) Squashed commits: [c72ad8f] Added cask for Panic ShrinkIt 1.3.2 [cf05823] Update mkvtools.rb to 3.4.5 [42779c6] Updated packet-peeper.rb to 2014-06-15

---
+++ Casks/deploymate.rb
@@ -0,0 +1,19 @@
+cask :v1 => 'deploymate' do
+  version '1.2.8'
+  sha256 '7e990c4ce807510ccfed544b968f049c28aec6a7988d068b5412fc93466b79f6'
+
+  # amazonaws.com is the official download host per the vendor homepage
+  url "https://s3.amazonaws.com/deploymate/download/Deploymate#{version.gsub('.', '')}.zip"
+
+  name 'Deploymate'
+  homepage 'http://www.deploymateapp.com'
+  license :commercial
+
+  app 'Deploymate.app'
+
+  zap :delete => [
+                  '~/Library/Caches/com.ivanvasic.deploymate',
+                  '~/Library/Preferences/com.ivanvasic.deploymate.plist',
+                  '~/Library/Preferences/com.ivanvasic.deploymate.LSSharedFileList.plist'
+                 ]
+end
---
+++ Casks/shrinkit.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'shrinkit' do
+  version '1.3.2'
+  sha256 'c076168a05d8a664cfac9bdee2f7a5f7cb317ade7d9f00c2cd0d49b33294a943'
+  
+  url "https://download.panic.com/shrinkit/ShrinkIt%20#{version}.zip"
+
+  name 'ShrinkIt'
+  homepage 'http://www.panic.com/blog/shrinkit-1-2/'
+  license :gratis
+
+  app 'ShrinkIt.app'
+end
