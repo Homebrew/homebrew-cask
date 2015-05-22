8491ed37997ed77e4a1647002ed0b2f84ffcd26d
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-22 20:20:02 +0300

    bleep.rb: add appcast

--- Casks/bleep.rb
+++ Casks/bleep.rb
@@ -5,6 +5,8 @@ cask :v1 => 'bleep' do
   # utorrent.com is the official download host per the vendor homepage
   url 'https://download-new.utorrent.com/endpoint/bleep/os/osx/track/stable/'
   name 'Bleep'
+  appcast 'https://bleeposx.bittorrent.com/appcast.xml',
+          :sha256 => '266fc57b32cbbe67b46513eba330c1986681f767892d97ad66b0a61510d4aaf1'
   homepage 'http://www.bleep.pm/'
   license :gratis
   tags :vendor => 'BitTorrent'
