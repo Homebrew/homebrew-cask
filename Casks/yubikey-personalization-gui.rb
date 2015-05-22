4d2d79e0f5e3593bde0a7865bbd0697408db55b2
forkbomber <forkbomber@users.noreply.github.com>
2015-05-22 00:17:34 -0400

    Upgrade YubiKey Personalization Tool.app to v3.1.20

--- Casks/yubikey-personalization-gui.rb
+++ Casks/yubikey-personalization-gui.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'yubikey-personalization-gui' do
-  version '3.1.18'
-  sha256 'b6acac4b25cc0758e3333ee11ef9fdd966766e063cb051a9f9092bb5bf8e0dc0'
+  version '3.1.20'
+  sha256 '6b0ba303d52f86f971d7fe8e4fb2cc0b05a1ef9c4b4167090dd6a5ef23e10e01'
 
   url "https://developers.yubico.com/yubikey-personalization-gui/Releases/yubikey-personalization-gui-#{version}.pkg"
   name 'YubiKey Personalization GUI'
