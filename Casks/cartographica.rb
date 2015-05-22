f8d673490f588a0efd6850b215cf4eaf151518a4
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-22 20:20:06 +0300

    cartographica.rb: add appcast

--- Casks/cartographica.rb
+++ Casks/cartographica.rb
@@ -5,6 +5,8 @@ cask :v1 => 'cartographica' do
   # cluetrust.com is the official download host per the vendor homepage
   url 'http://www.cluetrust.com/Downloads/Cartographica.dmg'
   name 'Cartographica'
+  appcast 'https://www.cluetrust.com/AppCasts/Cartographica.xml',
+          :sha256 => '9bb9075d8da457886cb86043d4807cbdd210e9f6c883f99ade7898911f90dd35'
   homepage 'https://www.macgis.com/'
   license :commercial
 
