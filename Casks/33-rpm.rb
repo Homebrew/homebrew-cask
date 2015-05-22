0df5620511190a0fc5d316856b8beac834d85044
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-22 20:19:48 +0300

    33-rpm.rb: add appcast

--- Casks/33-rpm.rb
+++ Casks/33-rpm.rb
@@ -4,6 +4,8 @@ cask :v1 => '33-rpm' do
 
   url 'http://www.edenwaith.com/downloads/33rpm.dmg'
   name '33 RPM'
+  appcast 'http://www.edenwaith.com/xml/33rpm.xml',
+          :sha256 => '2b0e3f1d3f0221e6c59c703e07366003bd8608adf10e4374c4d4b41ec1273276'
   homepage 'http://www.edenwaith.com/products/33rpm/'
   license :gratis
 
