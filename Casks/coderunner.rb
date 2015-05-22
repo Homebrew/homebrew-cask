40e0072a121e70a56e2267569413e0dedec9e9ff
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-22 20:20:11 +0300

    coderunner.rb: add appcast

--- Casks/coderunner.rb
+++ Casks/coderunner.rb
@@ -4,6 +4,8 @@ cask :v1 => 'coderunner' do
 
   url 'https://coderunnerapp.com/download'
   name 'CodeRunner'
+  appcast 'https://coderunnerapp.com/appcast.xml',
+          :sha256 => 'bc0a476a000d50dfc0e4fe997838b13794c483dd1d41c85752b77c01c9a07594'
   homepage 'https://coderunnerapp.com/'
   license :unknown
 
