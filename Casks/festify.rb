82f57812702c853df6369032fd3724100b5df9c5
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-23 19:24:06 +0300

    festify.rb: add appcast

--- Casks/festify.rb
+++ Casks/festify.rb
@@ -4,6 +4,8 @@ cask :v1 => 'festify' do
 
   url 'http://getfestify.com/downloads/mac/Festify.zip'
   name 'Festify'
+  appcast 'http://getfestify.com/updates/mac/festify.xml',
+          :sha256 => 'a82f851ef2ac753411d9d749b8f26d684896e881fdab7a4eed02920d37f8517c'
   homepage 'http://getfestify.com/'
   license :gratis
 
