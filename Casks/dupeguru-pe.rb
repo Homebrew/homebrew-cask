3821611d4c69a91459283bc9c873826f83a205a5
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-23 19:24:01 +0300

    dupeguru-pe.rb: add appcast

--- Casks/dupeguru-pe.rb
+++ Casks/dupeguru-pe.rb
@@ -4,6 +4,8 @@ cask :v1 => 'dupeguru-pe' do
 
   url "http://download.hardcoded.net/dupeguru_pe_osx_#{version.gsub('.', '_')}.dmg"
   name 'dupeGuru Picture Edition'
+  appcast 'http://www.hardcoded.net/updates/dupeguru_pe.appcast',
+          :sha256 => '0701e9b52a39df2198dae34a4bfef88ca84d75d4ce191dd88943437af921737a'
   homepage 'http://www.hardcoded.net/dupeguru_pe/'
   license :bsd
 
