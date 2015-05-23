dcef3e534d1030419bd28d5354e2091bf3127568
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2015-05-23 14:21:24 -0300

    Fix #11342 - Bump boot2docker version to 1.6.2

--- Casks/boot2docker.rb
+++ Casks/boot2docker.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'boot2docker' do
-  version '1.6.0'
-  sha256 'a6fc560d27bdfacd89a621bf89ed73e3e40f8e86ebdc85b142ddb328dd06a4b5'
+  version '1.6.2'
+  sha256 'c3cf9e5cd1695fd4965292201e67e6b8ef06516b925cf1db19174489c9e2daa5'
 
   url "https://github.com/boot2docker/osx-installer/releases/download/v#{version}/Boot2Docker-#{version}.pkg"
   appcast 'https://github.com/boot2docker/osx-installer/releases.atom'
