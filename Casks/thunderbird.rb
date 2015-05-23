59546974eb5e7a85ae2b7d9322abc949f930c07e
Jussi Tuominen <Jussi@COLDwithin.local>
2015-05-23 08:10:07 +0300

    Update Thunderbird.app to v31.7.0

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird' do
-  version '31.6.0'
-  sha256 '9c99eaf11a97c3ee17ae04b73271877d410e9af2b6134b0358c5d94c58f2818e'
+  version '31.7.0'
+  sha256 '0c1059adb32dc78dd3597e651f91d57288698e26a1845b1f0d77ecc3ca7b1228'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   name 'Mozilla Thunderbird'
