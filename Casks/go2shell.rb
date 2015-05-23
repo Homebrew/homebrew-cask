75b366863629063fef5448e31e3abede92da7f86
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-23 19:24:10 +0300

    go2shell.rb: add appcast

--- Casks/go2shell.rb
+++ Casks/go2shell.rb
@@ -4,6 +4,8 @@ cask :v1 => 'go2shell' do
 
   url 'http://zipzapmac.com/download/Go2Shell'
   name 'Go2Shell'
+  appcast 'http://appcast.zipzapmac.com/go2shell.xml',
+          :sha256 => '2a824962eb4bf47c11f69ce020dc07ad423d1c1d47df49e754fceb8b70ec2b1d'
   homepage 'http://zipzapmac.com/go2shell'
   license :gratis
 
