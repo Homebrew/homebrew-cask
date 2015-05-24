0b61c0d6328f0b9c7498ad05ff32297f65c82bed
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:37:54 +0300

    imazing.rb: add appcast

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -5,6 +5,8 @@ cask :v1 => 'imazing' do
   # devmate.com is the official download host per the vendor homepage
   url 'http://dl.devmate.com/com.DigiDNA.iMazingMac/iMazingforMac.dmg'
   name 'iMazing'
+  appcast 'http://updates.devmate.com/com.DigiDNA.iMazingMac.xml',
+          :sha256 => '20c1ab602462b7fc0d5b4cbd555cacf127b69a07a737579598ebcbc0f5b21319'
   homepage 'http://imazing.com/'
   license :commercial
 
