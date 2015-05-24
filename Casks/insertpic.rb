e9a78e5ef8b1fa367e135903166031e9e37d57a4
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:37:55 +0300

    insertpic.rb: add appcast

--- Casks/insertpic.rb
+++ Casks/insertpic.rb
@@ -5,6 +5,8 @@ cask :v1 => 'insertpic' do
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/insertPic/InsertPic_#{version}.zip"
   name 'InsertPic'
+  appcast 'http://getinsertpic.com/app_cast',
+          :sha256 => '20c1ab602462b7fc0d5b4cbd555cacf127b69a07a737579598ebcbc0f5b21319'
   homepage 'http://www.getinsertpic.com/'
   license :gratis
 
