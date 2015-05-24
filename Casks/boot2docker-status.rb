0689e6bb997eca15f5673363173d78959cc8d462
Vítor Galvão <info@vitorgalvao.com>
2015-05-24 17:15:36 +0100

    boot2docker-status.rb: added vendor comment

--- Casks/boot2docker-status.rb
+++ Casks/boot2docker-status.rb
@@ -2,6 +2,7 @@ cask :v1 => 'boot2docker-status' do
   version '1.0.0'
   sha256 '95b05b0a9b2047c7e2df6f649f322da70b59b1054ae586e82f1d6c741ede40de'
 
+  # ghithub.com is the official download host per the vendor homepage
   url "https://github.com/nickgartmann/boot2docker-status/releases/download/#{version}/Boot2Docker.Status-v#{version}.zip"
   appcast 'https://github.com/nickgartmann/boot2docker-status/releases.atom'
   name 'Boot2Docker Status'
