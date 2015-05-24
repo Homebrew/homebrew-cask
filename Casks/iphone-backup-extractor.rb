102692aabaaa1dc190183260eff1531c3e9c3f52
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:38:00 +0300

    iphone-backup-extractor.rb: add appcast

--- Casks/iphone-backup-extractor.rb
+++ Casks/iphone-backup-extractor.rb
@@ -4,6 +4,8 @@ cask :v1 => 'iphone-backup-extractor' do
 
   url 'http://supercrazyawesome.com/downloads/iPhone%20Backup%20Extractor.app.zip'
   name 'iPhone Backup Extractor'
+  appcast 'http://supercrazyawesome.com/sparkle.xml',
+          :sha256 => '9741720f2335c72c43b297545c4c834d4ad1064f24fc582af9802cd627bb6d83'
   homepage 'http://supercrazyawesome.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
