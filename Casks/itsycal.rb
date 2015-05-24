e50fa615abcfb3814afd0872ae18329629b2cb42
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:38:04 +0300

    itsycal.rb: add appcast

--- Casks/itsycal.rb
+++ Casks/itsycal.rb
@@ -5,6 +5,8 @@ cask :v1 => 'itsycal' do
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/itsycal/Itsycal-#{version}.zip"
   name 'Itsycal'
+  appcast 'https://s3.amazonaws.com/itsycal/itsycal.xml',
+          :sha256 => '9497668c81ab21b9e3021774659315e96c797e77a5a6c0bf037bc3828dec335b'
   homepage 'http://www.mowglii.com/itsycal/'
   license :gratis
 
