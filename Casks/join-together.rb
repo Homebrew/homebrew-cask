65aa2fcbc8220e8b6dc98cb078b3bfe234535222
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:38:07 +0300

    join-together.rb: add appcast

--- Casks/join-together.rb
+++ Casks/join-together.rb
@@ -4,6 +4,8 @@ cask :v1 => 'join-together' do
 
   url 'http://dougscripts.com/itunes/scrx/jointogetherml.zip'
   name 'Join Together'
+  appcast 'http://dougscripts.com/itunes/itinfo/jointogether_appcast.xml',
+          :sha256 => '7dfe8ccbbe9d33ca5ea39259bd66e30b3747041120f18647cfe3900645c32e78'
   homepage 'http://dougscripts.com/itunes/itinfo/jointogether.php'
   license :commercial
 
