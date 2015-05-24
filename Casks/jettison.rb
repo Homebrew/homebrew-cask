0a253ae9bba10e3a690f0bbfa86a27d34577178e
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:38:06 +0300

    jettison.rb: add appcast

--- Casks/jettison.rb
+++ Casks/jettison.rb
@@ -4,6 +4,8 @@ cask :v1 => 'jettison' do
 
   url "http://www.stclairsoft.com/download/Jettison-#{version}.dmg"
   name 'Jettison'
+  appcast 'https://www.stclairsoft.com/cgi-bin/sparkle.cgi?JT',
+          :sha256 => '0373bcc3c6c29abf5878f4db77bcf310241676ee0e4957940e041d158aad8442'
   homepage 'http://www.stclairsoft.com/Jettison/'
   license :freemium
 
