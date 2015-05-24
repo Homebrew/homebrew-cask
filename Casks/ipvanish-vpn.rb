d4a86f5ac76bdd9195dd09a2fdcc743f93e89ef6
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:38:01 +0300

    ipvanish-vpn.rb: add appcast

--- Casks/ipvanish-vpn.rb
+++ Casks/ipvanish-vpn.rb
@@ -4,6 +4,8 @@ cask :v1 => 'ipvanish-vpn' do
 
   url 'https://www.ipvanish.com/software/osx/IPVanish.dmg'
   name 'IPVanish'
+  appcast 'https://www.ipvanish.com/software/osx/updates.xml',
+          :sha256 => 'c64bd51aec091bb09da69aff1b44d7c8c9ef15d78be55cf5702a9805ee693385'
   homepage 'https://www.ipvanish.com/'
   license :gratis
 
