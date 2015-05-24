878741105f5cb8f9d576064d3e6f286c3aeb6c2b
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:37:53 +0300

    hotswitch.rb: add appcast

--- Casks/hotswitch.rb
+++ Casks/hotswitch.rb
@@ -4,6 +4,8 @@ cask :v1 => 'hotswitch' do
 
   url 'http://oniatsu.github.io/HotSwitch/release/zip/HotSwitch.zip'
   name 'HotSwitch'
+  appcast 'http://oniatsu.github.io/HotSwitch/release/appcast.xml',
+          :sha256 => 'c0a545a269cf17dcded22fe34da603cca9d793a8d5f8dbf60d4e760e2a6cb4cf'
   homepage 'http://oniatsu.github.io/HotSwitch/'
   license :mit
 
