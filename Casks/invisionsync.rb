f273644661f0923d90222dce81fbc69ad4a91051
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-24 22:38:00 +0300

    invisionsync.rb: add appcast

--- Casks/invisionsync.rb
+++ Casks/invisionsync.rb
@@ -4,6 +4,8 @@ cask :v1 => 'invisionsync' do
 
   url 'http://www.invisionapp.com/app/InVisionSync.zip'
   name 'InVision Sync'
+  appcast 'https://projects.invisionapp.com/native_app/mac/sparkle/appcast_v2.xml',
+          :sha256 => 'd061f71101d85d806b19c532847e468b4a93222805dcd464ea25028961638a80'
   homepage 'http://invisionapp.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
