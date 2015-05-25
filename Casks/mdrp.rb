4c5238d9314ca70e444f9dd1f9961107f5cc65d5
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:29 +0300

    mdrp.rb: add appcast

--- Casks/mdrp.rb
+++ Casks/mdrp.rb
@@ -4,6 +4,8 @@ cask :v1 => 'mdrp' do
 
   url 'http://www.macdvdripperpro.com/download/'
   name 'Mac DVDRipper Pro'
+  appcast 'http://www.macdvdripperpro.com/mdrp_sparkle5.xml',
+          :sha256 => '8e4f7c395dda29438cf1f4a931a626d484886109a01ea9ee733fa6d7c24f4634'
   homepage 'http://www.macdvdripperpro.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
