290521c8bcac82a9744d7dbe96c7270c3eb0780b
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:23 +0300

    mac2imgur.rb: add appcast

--- Casks/mac2imgur.rb
+++ Casks/mac2imgur.rb
@@ -5,6 +5,8 @@ cask :v1 => 'mac2imgur' do
   # mileswd.com is the official download host per the vendor homepage
   url 'https://mileswd.com/mac2imgur/latest'
   name 'mac2imgur'
+  appcast 'https://mileswd.com/mac2imgur/update',
+          :sha256 => '1235c533476928d64f6787886a3dc78b421fe8e379cc4e936aeb44152dc09bd1'
   homepage 'https://github.com/mileswd/mac2imgur'
   license :gpl
 
