97b07e77d9dc970e5e5812cf55a8307c3178e1a7
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:45 +0300

    opendns-updater.rb: add appcast

--- Casks/opendns-updater.rb
+++ Casks/opendns-updater.rb
@@ -4,6 +4,8 @@ cask :v1 => 'opendns-updater' do
 
   url 'http://www.opendns.com/download/mac/'
   name 'OpenDNS Updater'
+  appcast 'https://opendnsupdate.appspot.com/macupdatecheck/ipupdater/AppCast.xml',
+          :sha256 => 'd5938a67e84d710e93b5e74a1b515f6881a3f0ff251bfbb5ea61ecdf1596a4a6'
   homepage 'https://support.opendns.com/entries/23218654-Where-do-I-download-an-OpenDNS-Dynamic-IP-updater-client-'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
