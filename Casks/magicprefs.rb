f6bce93f1991afe7929f8e3318aa7335d3683b0b
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:27 +0300

    magicprefs.rb: add appcast

--- Casks/magicprefs.rb
+++ Casks/magicprefs.rb
@@ -4,6 +4,8 @@ cask :v1 => 'magicprefs' do
 
   url 'http://magicprefs.com/MagicPrefs.app.zip'
   name 'MagicPrefs'
+  appcast 'http://magicprefs.com/appcast.xml',
+          :sha256 => '907873f5493a31dc57884c1b34cdc36ef53eccda2a857593285e26e2016fd348'
   homepage 'http://magicprefs.com/'
   license :gratis
 
