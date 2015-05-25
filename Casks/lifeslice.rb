0a7dfacc1bb87463b7aec8d12ebc1fee41d9bcd2
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:15 +0300

    lifeslice.rb: add appcast

--- Casks/lifeslice.rb
+++ Casks/lifeslice.rb
@@ -5,6 +5,8 @@ cask :v1 => 'lifeslice' do
   # wanderingstan.com is the official download host per the vendor homepage
   url 'http://wanderingstan.com/apps/lifeslice/LifeSlice.dmg'
   name 'LifeSlice'
+  appcast 'http://wanderingstan.com/apps/lifeslice/sparkle-lifeslice-appcast-xml.php',
+          :sha256 => '90bd0939501fdc8a5b52bb24532919424ee1201a1b6281761b9e91d3b9068c61'
   homepage 'http://wanderingstan.github.io/Lifeslice/'
   license :oss
 
