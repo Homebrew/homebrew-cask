bf8d0ef1ae5e18e1b243aee6fecf0359f25aff53
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:20 +0300

    m3unify.rb: add appcast

--- Casks/m3unify.rb
+++ Casks/m3unify.rb
@@ -4,6 +4,8 @@ cask :v1 => 'm3unify' do
 
   url 'http://dougscripts.com/itunes/scrx/m3unifyml.zip'
   name 'M3Unify'
+  appcast 'http://dougscripts.com/itunes/itinfo/m3unify_appcast.xml',
+          :sha256 => 'e2a0cd2a135e0f54344babb3300f8aeb99498953474955a9366e5f5989e05cc1'
   homepage 'http://dougscripts.com/itunes/itinfo/m3unify.php'
   license :commercial
 
