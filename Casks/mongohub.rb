68e7b5c64151572945e083bbcb99029f2f7d0e98
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:36 +0300

    mongohub.rb: add appcast

--- Casks/mongohub.rb
+++ Casks/mongohub.rb
@@ -4,6 +4,8 @@ cask :v1 => 'mongohub' do
 
   url 'https://mongohub.s3.amazonaws.com/MongoHub.zip'
   name 'MongoHub'
+  appcast 'https://mongohub.s3.amazonaws.com/mongohub_su_feed.xml',
+          :sha256 => 'c5c16c0ca7331624f6e8f6154b88bb16648ee66706d613c5fc0f4863ff7f1eb3'
   homepage 'https://github.com/jeromelebel/MongoHub-Mac'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
