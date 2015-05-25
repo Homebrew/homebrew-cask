c2900d79bc422deed0943bddc1af83409b606c38
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:12:00 +0300

    language-switcher.rb: add appcast

--- Casks/language-switcher.rb
+++ Casks/language-switcher.rb
@@ -4,6 +4,8 @@ cask :v1 => 'language-switcher' do
 
   url 'http://www.tj-hd.co.uk/downloads/Language_Switcher_1_1_7.dmg'
   name 'Language Switcher'
+  appcast 'http://feeds.tj-hd.co.uk/feeds/language_switcher/appcast.xml',
+          :sha256 => '0a33d4efed28803122f154fd8a9eb0c62f60534ca542f1fa80a98341fcce4f15'
   homepage 'http://www.tj-hd.co.uk/en-gb/languageswitcher/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
