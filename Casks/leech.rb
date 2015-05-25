a002dc4505a65fc4b97d54cbf2ea9dc49c492668
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:12:00 +0300

    leech.rb: add appcast

--- Casks/leech.rb
+++ Casks/leech.rb
@@ -4,6 +4,8 @@ cask :v1 => 'leech' do
 
   url 'http://manytricks.com/download/leech'
   name 'Leech'
+  appcast 'http://manytricks.com/leech/appcast.xml',
+          :sha256 => 'ef4c85840f37dc92c0999f88e5e64acc5eb76b3e3933582710cdb703583473a1'
   homepage 'http://manytricks.com/leech/'
   license :commercial
 
