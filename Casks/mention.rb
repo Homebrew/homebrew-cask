7d534b86d597762b6ec3d2a2966318f834756dcd
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:30 +0300

    mention.rb: add appcast

--- Casks/mention.rb
+++ Casks/mention.rb
@@ -4,6 +4,8 @@ cask :v1 => 'mention' do
 
   url 'https://en.mention.com/downloads/mac/mention.dmg'
   name 'Mention'
+  appcast 'https://web.mention.com/uploads/appcast-mention.xml',
+          :sha256 => 'e3ee4b1624ab193b8236392da1c2051ca1ad5d8bf1c6baae76fed80cfd9d59c4'
   homepage 'https://en.mention.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
