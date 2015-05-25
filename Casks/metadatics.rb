d4bc9f1bf66a6e9e7345b13214cc9e0c8338c220
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-25 23:13:32 +0300

    metadatics.rb: add appcast

--- Casks/metadatics.rb
+++ Casks/metadatics.rb
@@ -4,6 +4,8 @@ cask :v1 => 'metadatics' do
 
   url 'http://www.markvapps.com/applications/metadatics/Metadatics.zip'
   name 'Metadatics'
+  appcast 'http://www.markvapps.com/applications/metadatics/metadatics_appcast.xml',
+          :sha256 => 'be165a851e1f29743a79ad4252ccda747ed2a5a5ceb11a096e83134594919eb0'
   homepage 'http://markvapps.com/metadatics'
   license :commercial
 
