39f700dcb493187f7b56c7e5130ec1eb44a507a1
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-26 20:50:29 +0300

    sony-ericsson-bridge.rb: add appcast

--- Casks/sony-ericsson-bridge.rb
+++ Casks/sony-ericsson-bridge.rb
@@ -4,6 +4,8 @@ cask :v1 => 'sony-ericsson-bridge' do
 
   url 'http://www-support-downloads.sonymobile.com/Software%20Downloads/Bridge%20for%20Mac/SonyBridgeForMac_web.dmg'
   name 'Sony Ericsson Bridge for Mac'
+  appcast 'http://dl-desktop-macapps.sonyericsson.com/production/Bridge/Databases_Prod/appcast.xml',
+          :sha256 => '0e516c239fff99ac24ec531546195ba185675bd890a0a09599e6f3fb5cd30fba'
   homepage 'http://support.sonymobile.com/au/tools/bridge-for-mac/'
   license :gratis
   tags :vendor => 'Sony'
