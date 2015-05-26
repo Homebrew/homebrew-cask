da23f97ade7cb544ff0aa8180613422942b8114a
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-26 20:50:29 +0300

    sleipnir.rb: add appcast

--- Casks/sleipnir.rb
+++ Casks/sleipnir.rb
@@ -4,6 +4,8 @@ cask :v1 => 'sleipnir' do
 
   url 'http://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
   name 'Sleipnir'
+  appcast 'http://update.fenrir.co.jp/smartupdate/mac/sleipnir/appcast.xml',
+          :sha256 => '1d8966375a83449bfc53ce47a41f1cd960b0f199a4ea319bf81333ab8a7ad030'
   homepage 'http://www.fenrir-inc.com/sleipnir/'
   license :closed
 
