b9ea52dca889c75c2e249510df0dd2b47ff4ca83
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-26 20:50:20 +0300

    pomotodo.rb: add appcast

--- Casks/pomotodo.rb
+++ Casks/pomotodo.rb
@@ -5,6 +5,8 @@ cask :v1 => 'pomotodo' do
   # hackplan.com is the official download host per the vendor homepage
   url 'http://air.hackplan.com/v1/p/com.pomotodo.PomotodoMac/download'
   name 'Pomodoro'
+  appcast 'http://air.hackplan.com/projects/5455f382437315386000d4d5/versions/latest.xml',
+          :sha256 => '6babb8c063ac36c7769d50564f143eafe510d6e07a3ed6ea4375f8433e27ae28'
   homepage 'https://pomotodo.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
