f5774ab79c174233d29d25464477defca9646946
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-26 20:50:22 +0300

    protonet.rb: add appcast

--- Casks/protonet.rb
+++ Casks/protonet.rb
@@ -4,6 +4,8 @@ cask :v1 => 'protonet' do
 
   url 'http://clients.protonet.info/osx/Protonet.dmg'
   name 'Protonet'
+  appcast 'http://clients.protonet.info/osx/stable/appcast.xml',
+          :sha256 => '592f270de11c6ef85081388c2c23d89808654db75a3cd144373c2f656c308e2e'
   homepage 'http://clients.protonet.info/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
