1b59869ae229c18ad07bf0587702477e2916dec9
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-26 20:50:26 +0300

    sandvox.rb: add appcast

--- Casks/sandvox.rb
+++ Casks/sandvox.rb
@@ -4,6 +4,8 @@ cask :v1 => 'sandvox' do
 
   url 'http://www.karelia.com/files/8/Sandvox.dmg'
   name 'Sandvox'
+  appcast 'http://launch.karelia.com/appcast.php',
+          :sha256 => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   homepage 'http://www.karelia.com/products/sandvox/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
