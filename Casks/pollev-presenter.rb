98ff3183b4c26356dfb2b0faeca0b713c41a37c2
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-26 20:50:19 +0300

    pollev-presenter.rb: add appcast

--- Casks/pollev-presenter.rb
+++ Casks/pollev-presenter.rb
@@ -5,6 +5,8 @@ cask :v1 => 'pollev-presenter' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/polleverywhere-app/mac-beta/pollev.dmg'
   name 'PollEv Presenter'
+  appcast 'https://polleverywhere-app.s3.amazonaws.com/mac-beta/appcast.xml',
+          :sha256 => 'dbfce32dced0e0370f6757d34aa89e176cab2495dcaaafc4a4fba85090e35bf6'
   homepage 'http://www.polleverywhere.com/'
   license :gratis
   tags :vendor => 'Poll Everywhere'
