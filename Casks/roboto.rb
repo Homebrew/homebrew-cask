76e77de5289ff0f3ab703ac185bfa117f8ca08f4
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-26 20:50:25 +0300

    roboto.rb: add appcast

--- Casks/roboto.rb
+++ Casks/roboto.rb
@@ -5,6 +5,8 @@ cask :v1 => 'roboto' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3-eu-west-1.amazonaws.com/electric-kangaroo-roboto/worker/Roboto.zip'
   name 'Roboto'
+  appcast 'https://roboto.build/mac/app-cast.xml',
+          :sha256 => 'e8ace5183bbceaa8972a5e493f256f73f5a2d595a588231e43de8d21b6a105c2'
   homepage 'http://roboto.build/'
   license :gratis
 
