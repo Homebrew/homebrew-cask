b94c1f11446ff1697fa89783ba0ad990816e12d6
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-26 20:50:15 +0300

    pdfpen.rb: add appcast

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -4,6 +4,8 @@ cask :v1 => 'pdfpen' do
 
   url 'http://dl.smilesoftware.com/com.smileonmymac.PDFpen/PDFpen.zip'
   name 'PDFpen'
+  appcast 'http://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml',
+          :sha256 => '20c1ab602462b7fc0d5b4cbd555cacf127b69a07a737579598ebcbc0f5b21319'
   homepage 'http://smilesoftware.com/PDFpen/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
