0651d167688b7716c1c118ea08f9f4d595ba8d16
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-27 23:32:19 +0300

    svnx.rb: add appcast

--- Casks/svnx.rb
+++ Casks/svnx.rb
@@ -4,6 +4,8 @@ cask :v1 => 'svnx' do
 
   url "https://svnx.googlecode.com/files/svnX%20#{version}.dmg"
   name 'SvnX'
+  appcast 'http://svnx.googlecode.com/svn/wiki/svnX.rss.xml',
+          :sha256 => '35166bbbcb22ee3704eddabcf6946eb60f415859c198c8b5480b346334c91056'
   homepage 'https://code.google.com/p/svnx/'
   license :oss
 
