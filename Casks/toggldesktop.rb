f47931ace3195b5e844ea9a3d161c7c249c945a6
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-27 23:32:25 +0300

    toggldesktop.rb: add appcast

--- Casks/toggldesktop.rb
+++ Casks/toggldesktop.rb
@@ -4,6 +4,8 @@ cask :v1 => 'toggldesktop' do
 
   url 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
   name 'TogglDesktop'
+  appcast 'https://assets.toggl.com/installers/darwin_stable_appcast.xml',
+          :sha256 => '9ab19ddd0df173611eeb42344254e108b36583f517e91561cbeff997897d85df'
   homepage 'https://www.toggl.com'
   license :bsd
 
