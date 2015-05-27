fdb82f61b432f5ab9f3bb60a9519273d4251ee52
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-27 23:32:24 +0300

    time-relic.rb: add appcast

--- Casks/time-relic.rb
+++ Casks/time-relic.rb
@@ -5,6 +5,8 @@ cask :v1 => 'time-relic' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://timerelic-download.s3.amazonaws.com/Time%20Relic.zip'
   name 'Time Relic'
+  appcast 'https://timerelic-download.s3.amazonaws.com/appcast.xml',
+          :sha256 => '671ff3726eab6e24fdafb47d2b0db7fc70dc09525bc3d5b15be7c12f1f5df196'
   homepage 'http://timerelic.com/'
   license :gratis
 
