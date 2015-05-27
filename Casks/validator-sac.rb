1ac7142edad20526e5d30bc8618dd8ee82b1c2a8
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-27 23:32:27 +0300

    validator-sac.rb: add appcast

--- Casks/validator-sac.rb
+++ Casks/validator-sac.rb
@@ -4,6 +4,8 @@ cask :v1 => 'validator-sac' do
 
   url "http://habilis.net/download/Validator-SAC_#{version}.zip"
   name 'Validator S.A.C.'
+  appcast 'http://habilis.net/download/validator-sac.xml',
+          :sha256 => '0b01f16e91ba9915b50b443f807deb6aa252966de6ac7e8b73ae78ccbb78a0c6'
   homepage 'http://habilis.net/validator-sac/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
