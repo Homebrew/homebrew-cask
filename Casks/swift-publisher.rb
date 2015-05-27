7eea20f6607377347485b1ba0a9386877f65b13b
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-27 23:32:20 +0300

    swift-publisher.rb: add appcast

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -5,6 +5,8 @@ cask :v1 => 'swift-publisher' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'http://s3.amazonaws.com/belightsoft/SwiftPublisher.dmg'
   name 'Swift Publisher'
+  appcast 'http://www.belightsoft.com/download/updates/appcast_SwiftPublisher3.xml',
+          :sha256 => '73bf29fcb6fa689fa32e5a1666ffb8db22401a6033a91e2866fc95d1be7ffd96'
   homepage 'http://www.belightsoft.com/products/swiftpublisher/overview.php'
   license :closed
 
