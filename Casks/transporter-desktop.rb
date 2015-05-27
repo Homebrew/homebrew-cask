fbae6dd69b97a810881b713a2179e78305a8eecf
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-27 23:32:26 +0300

    transporter-desktop.rb: add appcast

--- Casks/transporter-desktop.rb
+++ Casks/transporter-desktop.rb
@@ -5,6 +5,8 @@ cask :v1 => 'transporter-desktop' do
   # connecteddata.com is the official download host per the vendor homepage
   url "https://secure.connecteddata.com/mac/2.5/software/Transporter_Desktop_#{version}.dmg"
   name 'Transporter Desktop'
+  appcast 'http://appsoftware.connecteddata.com/mac/2.5/Appcast.xml',
+          :sha256 => '7b36fe42eab19068fe4d1e8bbb0efd4d02d7be8d1440d367bf2118c11ccbff3d'
   homepage 'http://www.filetransporter.com/'
   license :commercial
 
