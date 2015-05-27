6d060dd1af1fd223900b26d3bed341e56b33fc86
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-27 23:32:31 +0300

    wordcounter.rb: add appcast

--- Casks/wordcounter.rb
+++ Casks/wordcounter.rb
@@ -4,6 +4,8 @@ cask :v1 => 'wordcounter' do
 
   url 'http://wordcounterapp.com/download'
   name 'WordCounter'
+  appcast 'http://appcasts.christiantietze.de/wordcounter/v1/beta.xml',
+          :sha256 => '43d85462e4502f62fc4dcc39f2df83b6c1cfdbddb801a8cc30c7c9bace1e13c5'
   homepage 'http://wordcounterapp.com/'
   license :commercial
 
