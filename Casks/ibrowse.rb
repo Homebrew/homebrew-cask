466439d7f73696b59d66f70ab0b1acc21248ac70
Michał Ordon <michal@tehidea.com>
2015-05-29 00:46:32 +0100

    Update iBrowse.app to v1.0.3.0

--- Casks/ibrowse.rb
+++ Casks/ibrowse.rb
@@ -1,14 +1,14 @@
 cask :v1 => 'ibrowse' do
-  version '1.0.0.5'
-  sha256 'c3cae297d676e64c3d21638c21583f7c30a8fdbe6a4e127adb0857bafcd8037d'
+  version '1.0.3.0'
+  sha256 '15dfc22ca7a97b9593994115c5adc016357a443131582e4d68a9bcb32e08ab7c'
 
   # macroplant.com is the official download host per the vendor homepage
-  url "http://cdn.macroplant.com/release/iBrowse-#{version}x.dmg"
+  url "http://cdn.macroplant.com/release/iBrowse-#{version}.dmg"
   name 'iBrowse'
   appcast 'https://www.macroplant.com/ibrowse/ib-appcast.xml',
           :sha256 => '2dd21aed73c84b3b7c85ca29292f1c347fbadd5f9fd78866bd5db55d2c70e88f'
   homepage 'http://www.ibrowseapp.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'iBrowse.app'
 end
