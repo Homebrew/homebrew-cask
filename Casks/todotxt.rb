5625e11469ef69821749d8b9022d837abc100969
mjdescy <mjdescy@gmail.com>
2015-05-27 20:59:03 -0400

    Update TodoTxtMac to v1.5.1.

--- Casks/todotxt.rb
+++ Casks/todotxt.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'todotxt' do
-  version '1.5.0'
-  sha256 'a1ca515ba0f521e7a4eab8da5db684323fe655621d7dc68f77b837a94c47528a'
+  version '1.5.1'
+  sha256 'c848d808d2ea7147cd6f822f518cfcf622041a1d907406be56bb9caf58e5ca4c'
 
   url "https://github.com/mjdescy/TodoTxtMac/releases/download/#{version}/TodoTxtMac.app.zip"
   appcast 'https://github.com/mjdescy/TodoTxtMac/releases.atom'
