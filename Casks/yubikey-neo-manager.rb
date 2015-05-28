ca6ecd8636bfb8d97cb2b13b46a4c3bf757e2d8f
forkbomber <forkbomber@users.noreply.github.com>
2015-05-28 01:31:53 -0400

    Upgrade YubiKey NEO Manager.app to v1.3.0

--- Casks/yubikey-neo-manager.rb
+++ Casks/yubikey-neo-manager.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'yubikey-neo-manager' do
-  version '1.1.0'
-  sha256 '3af16efa1ac02ea4244cf906d8d63d98e4b867b3f4ceaed668d405bd6d2b809e'
+  version '1.3.0'
+  sha256 '9b28bc6350a7afe2e7e3e4397423359487de876ee67aafeca7902f9aaa5f1756'
 
   url "https://developers.yubico.com/yubikey-neo-manager/Releases/yubikey-neo-manager-#{version}-mac.pkg"
   name 'YubiKey NEO Manager'
