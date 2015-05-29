e030ff4209a22754f7de5d01f3b1396fd3c18de9
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-29 19:49:40 +0300

    resolutionator.rb: add appcast

--- Casks/resolutionator.rb
+++ Casks/resolutionator.rb
@@ -4,6 +4,8 @@ cask :v1 => 'resolutionator' do
 
   url 'http://manytricks.com/download/resolutionator'
   name 'Resolutionator'
+  appcast 'http://manytricks.com/resolutionator/appcast.xml',
+          :sha256 => 'daf1d476cf2235d77b3f0303b5f2672cafff7b713914a50a28d68c6747fece2f'
   homepage 'http://manytricks.com/blog/?cat=34'
   license :gratis
 
