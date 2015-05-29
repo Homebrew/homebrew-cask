baf77ac7bb6aa0e38cd1f41c577556a6f15ef703
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-29 19:47:09 +0300

    xtabulator.rb: add appcast

--- Casks/xtabulator.rb
+++ Casks/xtabulator.rb
@@ -4,6 +4,8 @@ cask :v1 => 'xtabulator' do
 
   url 'http://www.bartastechnologies.com/products/xtabulator/XTabulator2.zip'
   name 'XTabulator 2'
+  appcast 'http://www.bartastechnologies.com/products/xtabulator/sparkleupdates/xtappcast.php',
+          :sha256 => '1ee23eeaded78d9f1c9bbc1cffa6db165fb11e06e33e120db1f3c928d582132c'
   homepage 'http://www.bartastechnologies.com/products/xtabulator/'
   license :commercial
 
