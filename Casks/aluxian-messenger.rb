0db460582bb7d0368dca469c7b844337ab7db98e
Jeff Tang <mrjefftang@users.noreply.github.com>
2015-05-29 08:16:34 -0400

    Update Aluxian Messenger to 1.3.3

--- Casks/aluxian-messenger.rb
+++ Casks/aluxian-messenger.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'aluxian-messenger' do
-  version '1.1.0'
-  sha256 '35c6325049af1d3752e02420b893987ee699aa394dc866d0c6aac2a57e6ab541'
+  version '1.3.3'
+  sha256 'e95f5009907134b190403af98e007b74bec65c8b147541b1307e34f669c01237'
 
-  url "https://github.com/Aluxian/Facebook-Messenger-Desktop/releases/download/v#{version}/messenger_osx64.zip"
+  url "https://github.com/Aluxian/Facebook-Messenger-Desktop/releases/download/v#{version}/Messenger.dmg"
   name 'Messenger'
   homepage 'http://messengerfordesktop.com/'
   license :oss
