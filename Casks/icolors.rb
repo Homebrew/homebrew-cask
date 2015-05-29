ce021fa31c85839715acc0994a255fdfdfbda100
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-29 19:47:04 +0300

    icolors.rb: add appcast

--- Casks/icolors.rb
+++ Casks/icolors.rb
@@ -4,6 +4,8 @@ cask :v1 => 'icolors' do
 
   url "https://www.fadingred.com/files/icolors/icolors_#{version}.zip"
   name 'iColors'
+  appcast 'http://www.fadingred.com/icolors/updates.xml',
+          :sha256 => 'fa4f6c175627a5cb3fac7a8c44752fb288dbfa13ed72ae6fe06a2b89d01c97d8'
   homepage 'http://www.fadingred.com/icolors/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
