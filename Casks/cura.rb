3eedcead48ec22fd280905f5bcc943cdfdc8577a
BruegelN <BruegelN@users.noreply.github.com>
2015-05-29 10:17:34 +0200

    FIX: changed Cura.app URL v15.02.01
    Adapted the new URL to download Cura.app

--- Casks/cura.rb
+++ Casks/cura.rb
@@ -2,7 +2,7 @@ cask :v1 => 'cura' do
   version '15.02.1'
   sha256 'a8d174a6921281704b408c74fd7414b3b2a67ddd2787707db71c02c36a004088'
 
-  url "http://software.ultimaker.com/current/Cura-#{version}-MacOS.dmg"
+  url "https://ultimaker.com/en/software/download/23/Cura-#{version}-MacOS.dmg"
   name 'Cura'
   homepage 'https://ultimaker.com/en/products/software'
   license :oss
