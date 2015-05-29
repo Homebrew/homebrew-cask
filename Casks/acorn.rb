10a93d4e22920c55bd12288e5942714561b8cff8
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-29 19:46:55 +0300

    acorn.rb: add appcast

--- Casks/acorn.rb
+++ Casks/acorn.rb
@@ -4,6 +4,8 @@ cask :v1 => 'acorn' do
 
   url 'http://flyingmeat.com/download/Acorn.zip'
   name 'Acorn'
+  appcast 'http://www.flyingmeat.com/download/acorn4update.xml',
+          :sha256 => '190b994807371fa5d7acde2178af97d549ecf95b349e5d9cf40ca854c0160caf'
   homepage 'http://flyingmeat.com/acorn/'
   license :commercial
 
