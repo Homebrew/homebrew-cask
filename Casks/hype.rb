5b90663cc4429e5039f854b7658c1536d141f0f0
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-29 19:47:03 +0300

    hype.rb: add appcast

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -4,6 +4,8 @@ cask :v1 => 'hype' do
 
   url 'http://tumult.com/hype/download/Hype.zip'
   name 'Hype'
+  appcast 'http://tumult.com/hype/appcast_hype2.xml',
+          :sha256 => 'b8435a77bb13d39ddaecb92a1510786ceb135225e207aaaac91e7ef230d9edea'
   homepage 'http://tumult.com/hype/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
   tags :vendor => 'Tumult'
