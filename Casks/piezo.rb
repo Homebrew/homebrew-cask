a7524735b3bd56ce8c9c9fd4b72e9ea30b35bfb8
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-31 23:52:50 +0300

    piezo.rb: add appcast

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -4,6 +4,8 @@ cask :v1 => 'piezo' do
 
   url 'http://neutral.rogueamoeba.com/mirror/files/Piezo.zip'
   name 'Piezo'
+  appcast 'http://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo',
+          :sha256 => 'c8dc37b74425eb9f11e86989ea9e11ae6878afd3d542516224c7e7b36281ccf3'
   homepage 'http://rogueamoeba.com/piezo/'
   license :commercial
 
