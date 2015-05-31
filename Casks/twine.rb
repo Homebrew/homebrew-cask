25d8d2402019de631d2e02ca1a9262865431d79f
Larry Gilbert <larry@l2g.to>
2015-05-30 22:32:17 -0700

    twine 2.0.5

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'twine' do
-  version '1.4.1'
-  sha256 '717e0f7849f8216a7bae68c66656cf0e3cd0496c8a9d95e3c61e175d56f010e4'
+  version '2.0.5'
+  sha256 'e385a1dfc411186c571620da446bb746989d6dc16c2f126156a13df74ec934f5'
 
   url "http://twinery.org/downloads/twine_#{version}_osx.zip"
   name 'Twine'
