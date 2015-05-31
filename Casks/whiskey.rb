46922ec10a8ed3b815bc992183d1972accc41c48
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-31 23:52:57 +0300

    whiskey.rb: add appcast

--- Casks/whiskey.rb
+++ Casks/whiskey.rb
@@ -4,6 +4,8 @@ cask :v1 => 'whiskey' do
 
   url 'http://usewhiskey.com/latest'
   name 'Whiskey'
+  appcast 'http://usewhiskey.com/appcast.xml',
+          :sha256 => '96dea545ba04bfa604924f6e2d995d3a71ca2ef16997f494724543f8c0b60ef4'
   homepage 'http://usewhiskey.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
