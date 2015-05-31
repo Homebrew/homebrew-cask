76d0840cced9e051f9a1702a329a087b7a7a5dfe
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-31 23:52:43 +0300

    knock.rb: add appcast

--- Casks/knock.rb
+++ Casks/knock.rb
@@ -4,6 +4,8 @@ cask :v1 => 'knock' do
 
   url 'http://knocktounlock.com/download'
   name 'Knock'
+  appcast 'https://knock-updates.s3.amazonaws.com/Knock.xml',
+          :sha256 => 'c1976fb467cc420bf5e6d5137f01ca70baa3b758dcf8c23860c019bf6dd882c6'
   homepage 'http://knocktounlock.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
