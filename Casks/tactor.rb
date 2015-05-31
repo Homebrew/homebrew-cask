7dddbd47a595e259bec019262bcfd00feba63c5f
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-31 23:52:54 +0300

    tactor.rb: add appcast

--- Casks/tactor.rb
+++ Casks/tactor.rb
@@ -5,6 +5,8 @@ cask :v1 => 'tactor' do
   # dropbox is the official download host per the vendor homepage
   url 'https://dl.dropboxusercontent.com/u/7614970/Tactor.zip'
   name 'Tactor'
+  appcast 'https://onflapp.appspot.com/tactor',
+          :sha256 => 'de6620c8fd971681e356f2c24280ae9d38d09b78cafd2f25593fe50a235fd997'
   homepage 'http://onflapp.wordpress.com/tactor/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
