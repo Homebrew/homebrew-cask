f2dcca33185512d9c8841d916105a9e40a1af802
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-31 23:52:42 +0300

    itunes-volume-control.rb: add appcast

--- Casks/itunes-volume-control.rb
+++ Casks/itunes-volume-control.rb
@@ -4,6 +4,8 @@ cask :v1 => 'itunes-volume-control' do
 
   url 'https://github.com/alberti42/iTunes-Volume-Control/blob/master/iTunes%20Volume%20Control.dmg?raw=true'
   name 'iTunes Volume Control'
+  appcast 'http://quantum-technologies.iap.uni-bonn.de/alberti/iTunesVolumeControl/iTunesVolumeControlCast.xml.php',
+          :sha256 => 'fe941e41722f19122558cfad9f0bae80cb1fdaa3e88292c56bfb5d945434d86f'
   homepage 'https://github.com/alberti42/iTunes-Volume-Control'
   license :oss
 
