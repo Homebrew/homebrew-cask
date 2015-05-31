8036894665bb95f22faa949786b98735d6ea5f4d
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-31 23:52:45 +0300

    linein.rb: add appcast

--- Casks/linein.rb
+++ Casks/linein.rb
@@ -4,6 +4,8 @@ cask :v1 => 'linein' do
 
   url 'https://www.rogueamoeba.com/freebies/download/LineIn.zip'
   name 'LineIn'
+  appcast 'https://rogueamoeba.com/freebies/version-linein.rss',
+          :sha256 => '212705064a7c63980fa1eb4ec1f1334212bf8e23a639b507c31b0d09b65d7db8'
   homepage 'http://www.rogueamoeba.com/freebies/'
   license :gratis
 
