dc057f1fe43af73b4843b17f030b707144573b6f
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-31 23:52:49 +0300

    oolite.rb: add appcast

--- Casks/oolite.rb
+++ Casks/oolite.rb
@@ -5,6 +5,8 @@ cask :v1 => 'oolite' do
   # github.com is the official download host per the vendor homepage
   url "https://github.com/OoliteProject/oolite/releases/download/#{version}b/Oolite-#{version}.zip"
   name 'oolite'
+  appcast 'http://www.oolite.org/updates/oolite-mac-appcast.xml',
+          :sha256 => '04af78784ecdf907b4bdd0c89d12c72eebfa1feed0b61a55451a9052585beb5d'
   homepage 'http://www.oolite.org/'
   license :gpl
 
