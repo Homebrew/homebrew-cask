e5749d913908c3baf3ea04e643fb4a278520be01
Daniel Öbrink <goyoghurt@users.noreply.github.com>
2015-06-01 22:47:22 +0200

    Firetask for mac
    Updated with latest and no_check

--- firetask.rb
+++ firetask.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firetask' do
-  version '4.7'
-  sha256 '4b1f6fbdaf441f84df8e27534517dbdb0a92a6815f39be7d6182409b9130b661'
+  version :latest
+  sha256 :no_check
 
   url 'http://downloads.firetask.com/firetask-mac/Firetask-Mac.dmg'
   name 'FireTask'
