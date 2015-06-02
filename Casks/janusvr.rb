cf47c3c5b5b3ebf360e75a88679819f9eb63a02e
Tim Miller <neoparasak@gmail.com>
2015-06-02 12:06:39 -0400

    Addressed issue with SHA 256 check

--- Casks/janusvr.rb
+++ Casks/janusvr.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'janusvr' do
   version :latest
-  sha256 'aa67202ce3841c78222de71ec2aa3fc450d95a0f8b2a78a843bf404a14476c31'
+  sha256 :no_check
 
   # toronto.edu is the official download host per the vendor homepage
   url 'http://www.dgp.toronto.edu/~mccrae/projects/firebox/downloads/janusvr.dmg'
