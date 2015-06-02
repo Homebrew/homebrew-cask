711180fb4143cee72f63025a32c173f85201f7f8
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-02 19:51:06 +0300

    Upgrade Framer Studio.app to v1.11.188

--- Casks/framer-studio.rb
+++ Casks/framer-studio.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'framer-studio' do
-  version '1.9.39'
-  sha256 '38fc750d8b1de5f86819c860a81624520af92d309faf9ddd1b784e2207bc0b0d'
+  version '1.11.188'
+  sha256 'b1fff971456a4af5ad88661344dbad079d5ba08e25dfc2c6f252824d1916206b'
 
   url "http://studio.update.framerjs.com/static/downloads/Framer%20Studio-#{version}.zip"
   name 'Framer Studio'
