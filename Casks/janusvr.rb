fce61cb8a8b248eb5100779f0fd72504f7fb9640
Tim Miller <neoparasak@gmail.com>
2015-06-02 12:00:38 -0400

    Added Janus VR cask

--- Casks/janusvr.rb
+++ Casks/janusvr.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'janusvr' do
   version :latest
-  sha256 ''
+  sha256 'aa67202ce3841c78222de71ec2aa3fc450d95a0f8b2a78a843bf404a14476c31'
 
   # toronto.edu is the official download host per the vendor homepage
   url 'http://www.dgp.toronto.edu/~mccrae/projects/firebox/downloads/janusvr.dmg'
   name 'Janus VR'
   homepage 'http://janusvr.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
-  app ''
+  app 'janusvr.app', :target => 'Janus VR.app'
 end
