c65c41d9b76a5d91c9f3821ff1d1db18419cdcde
Zhongke Chen <ch3n2k@gmail.com>
2015-06-03 14:27:00 +0800

    update visual-studio-code

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,14 +1,14 @@
 cask :v1 => 'visual-studio-code' do
-  version :latest
-  sha256 :no_check
+  version '0.3.0'
+  sha256 '72c9c2e5182c7d7674659f232ec1e34165892bde7c63a3cce2f63e3803b7da8f'
 
-  url 'http://download.microsoft.com/download/0/D/5/0D57186C-834B-463A-AECB-BC55A8E466AE/VSCode-osx.zip'
+  url 'https://az764295.vo.msecnd.net/public/0.3.0/VSCode-darwin.zip'
   name 'Visual Studio Code'
   homepage 'https://code.visualstudio.com/'
   license :gratis
   tags :vendor => 'Microsoft'
 
-  app 'Visual Studio Code.app'
+  app 'VSCode-darwin/Visual Studio Code.app'
 
   zap :delete => [
                   '~/Library/Application Support/Code',
