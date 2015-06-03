25e3c54ff4e71eb149d1938a258ceedf3ef58060
Dave Peck <davepeck@gmail.com>
2015-06-03 15:22:40 -0700

    Point Cloak download URL to our new official URL.

--- Casks/cloak.rb
+++ Casks/cloak.rb
@@ -2,8 +2,7 @@ cask :v1 => 'cloak' do
   version '2.0.11'
   sha256 '8dbebffcfe6db8b20a15ce11231fea248a42281cde73d08b661b1e9861ec7eed'
 
-  # amazonaws.com is the official download host per the vendor homepage
-  url "https://s3.amazonaws.com/static.getcloak.com/osx/updates/Release/Cloak-#{version}.dmg"
+  url "https://static.getcloak.com/downloads/osx/updates/Release/Cloak-#{version}.dmg"
   name 'Cloak'
   homepage 'https://www.getcloak.com'
   license :gratis
