84e5cf863d2c18dfe916155e876be143d53b93fa
Hugo Rodde <rodde.hugo@gmail.com>
2015-06-03 02:02:01 -0300

    Change weka-3-6-12-oracle-jvm.dmg sourceforge url
    Remove weka-3-6-12-oracle-jvm.dmg old url comment

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -3,7 +3,7 @@ cask :v1 => 'weka' do
   sha256 'a0a8683342c6b367a29f4f2ca226db115e9a603b2ffc91c34525bb0f664d7b8c'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/sourceforge/weka/weka-#{version.gsub('.','-')}-oracle-jvm.dmg"
+  url "http://sourceforge.net/projects/weka/files/weka-3-6-osx/3.6.12/weka-#{version.gsub('.','-')}-oracle-jvm.dmg/download"
   name 'Weka'
   homepage 'http://www.cs.waikato.ac.nz/ml/weka/'
   license :gpl
