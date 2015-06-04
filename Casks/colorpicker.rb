6500d4117aeeee27002de7ae48be852ee9e32f2f
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-04 10:37:09 +0300

    Upgrade ColorPicker.app to v1.7

--- Casks/colorpicker.rb
+++ Casks/colorpicker.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'colorpicker' do
-  version '1.6.1'
-  sha256 '23f2e782e20ff172b06746803ad2879ab5e7208dc04818a1716c18249d1e360d'
+  version '1.7'
+  sha256 'cb5aac90b50b5541bd3e9e17e365a23ce59031839de3a6c0c92901d40ee87db1'
 
   url "http://www.northernspysoftware.com/download/ColorPicker_#{version}.dmg"
   appcast 'http://www.northernspysoftware.com/feed/colorpicker.xml',
