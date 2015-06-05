79db54d2a77f45a4eeaeaff3ce435fae8ccb31cf
Jared Sartin <jaredsartin@gmail.com>
2015-06-05 12:47:09 -0700

    Improves Polycode's caska syntax
    Changes double quotes into single quotes.

--- Casks/polycode.rb
+++ Casks/polycode.rb
@@ -1,14 +1,14 @@
-cask :v1 => "polycode" do
-  version "0.8.4"
+cask :v1 => 'polycode' do
+  version '0.8.4'
   sha256 :no_check
 
-  depends_on :macos => ">= :lion"
+  depends_on :macos => '>= :lion'
   depends_on :arch => :x86_64
 
   url "http://polycode.org/download/content/PolycodeDarwin_#{version}.zip"
-  name "Polycode"
-  homepage "http://polycode.org/"
+  name 'Polycode'
+  homepage 'http://polycode.org/'
   license :gratis
 
-  app "Darwin/Polycode.app"
+  app 'Darwin/Polycode.app'
 end
