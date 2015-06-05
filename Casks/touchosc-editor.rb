2181369741ba23f498287f456fca3ac9424067ea
Daniel Bayley <daniel.bayley@me.com>
2015-06-05 21:55:33 +0100

    Add TouchOSC Editor v 1.7.0
    TouchOSC layout editor.
    
    Signed-off-by: Daniel Bayley <daniel.bayley@me.com>

---
+++ Casks/touchosc-editor.rb
@@ -0,0 +1,16 @@
+cask :v1 => 'touchosc-editor' do
+  version '1.7.0'
+  sha256 'c23baa9eb9f3c15bb71f83f713a6b2e9aa01cc0ffe0acc15f23a6a8042acb766'
+
+  url "http://hexler.net/pub/touchosc/touchosc-editor-#{version}-osx.zip"
+  name 'TouchOSC Editor'
+  homepage 'http://hexler.net/software/touchosc'
+  license :unknown
+
+  app "touchosc-editor-#{version}-osx/TouchOSC Editor.app"
+
+  #suite "touchosc-editor-#{version}-osx"
+  #artifact "touchosc-editor-#{version}-osx/layouts" > "~/Library/App*Support/TouchOSCEditor/layouts"
+
+  zap :delete => '~/Library/*/*TouchOSCEditor*'
+end
