0c71425a673d3b4e8b1e0ac7b07ed836202fc13a
Daniel Bayley <daniel.bayley@me.com>
2015-06-06 23:19:45 +0100

    Add Splice v1.5.12
    Splice cloud platform for music.
    
    Signed-off-by: Daniel Bayley <daniel.bayley@me.com>

---
+++ Casks/splice.rb
@@ -0,0 +1,23 @@
+cask :v1 => 'splice' do
+  version :latest
+  sha256 :no_check
+
+  # amazonaws.com is the official download host per the vendor homepage
+  url 'https://spliceosx.s3.amazonaws.com/Splice.dmg'
+  name 'Splice'
+  homepage 'http://splice.com'
+  license :gratis
+
+  installer :script => 'Splice Installer.app/Contents/MacOS/Splice Installer',
+            :args => [ '-q' ],
+            :sudo => false
+
+  uninstall :quit => 'com.splice.Splice',
+            :delete => '/Applications/Splice.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/*Splice*',
+                  '~/Library/Caches/com.splice*',
+                  '~/Library/Preferences/com.splice*'
+                 ]
+end
