3002d1fe16cde0329fc462a70310f35c9015182c
Maurice Kelly <osv-github@chatswood.org.uk>
2015-06-06 20:23:43 +0100

    Update license stanzas
    Not very many in this PR but I had a couple lying about.

--- Casks/ibank.rb
+++ Casks/ibank.rb
@@ -19,5 +19,5 @@ cask :v1 => 'ibank' do
 
   name 'iBank'
   homepage 'http://www.iggsoftware.com/ibank'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 end
