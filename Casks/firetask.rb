36115c1eb5c8f18242721dcbca84f280349bc8a2
Vítor Galvão <info@vitorgalvao.com>
2015-06-08 22:40:12 +0100

    fixed firetask.rb

--- Casks/firetask.rb
+++ Casks/firetask.rb
@@ -5,7 +5,7 @@ cask :v1 => 'firetask' do
   url 'http://downloads.firetask.com/firetask-mac/Firetask-Mac.dmg'
   name 'FireTask'
   homepage 'http://firetask.com'
-  license :closed    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :closed
 
   app 'Firetask.app'
 end
