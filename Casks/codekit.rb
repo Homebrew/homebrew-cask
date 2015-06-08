a368f40950d335f9ce47b592c5b0620433cf0b06
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-08 11:11:28 +0300

    codekit.rb: upgrade to v2.3.10-19033

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'codekit' do
-  version '2.3.7-18917'
-  sha256 '5958b170026f37bb78b31a9251cd1ccafb2239d9a85e9729593948b9d00255fc'
+  version '2.3.10-19033'
+  sha256 '6ace696b81de91d7f5d2b24cb27a6609a72aeadc2d36fb6cda56af2b768b34c8'
 
   url "http://incident57.com/codekit/files/codekit-#{version.sub(%r{.*-},'')}.zip"
   appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml',
