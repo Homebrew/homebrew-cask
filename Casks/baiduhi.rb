55cb692e7fcbcb8cdf0317c2f713cb962816d381
changhai <changhai@baidu.com>
2015-06-09 11:57:04 +0800

    baidu hi upgrade

--- Casks/baiduhi.rb
+++ Casks/baiduhi.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'baiduhi' do
-  version '1.3.0.5'
-  sha256 '372229bee83fda1e392624455cac7f94cc4e1d9ed00f80344f3fae037f442542'
+  version '1.4.0.1'
+  sha256 '1dffe0933902e5c49ed31762ecbd67bd2f11ba6af0fcf4ba7fcae0dd5bc52067'
 
-  url "http://im.baidu.com/download/mac/machi_#{version}.dmg"
+  url "http://mac.im.baidu.com/machi_#{version}.dmg"
   name '百度Hi'
   name 'Baidu Hi'
   homepage 'http://im.baidu.com/'
