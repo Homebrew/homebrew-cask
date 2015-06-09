9614b8738784681e6797fa69fe3deefe72b259fd
Andy Li <andy@onthewings.net>
2015-06-10 03:55:33 +0800

    flash-player and flash-player-debugger 18.0.0.160

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'flash-player' do
-  version '17.0.0.188'
-  sha256 '698c577af0c3e496e0f36a8faf599ccc1c20c8dd0953a1ed4a43e4cd8b26abc2'
+  version '18.0.0.160'
+  sha256 '389c1031216933f76a400005c97329ef319e24cdd3ebf86e0be922b5dc3c8148'
 
   # macromedia.com is the official download host per the vendor homepage
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.to_i}/flashplayer_#{version.to_i}_sa.dmg"
