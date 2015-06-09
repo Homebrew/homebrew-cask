543367a782c0fdd36243f407074777d078bd9f9c
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 20:03:06 +0100

    orange.rb: fixed sha256

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'orange' do
   version '2.7.8-dcf9af9'
-  sha256 '2f91e55f41f08b0d691b553f70c38266491ba8efd1c0bd2c60ad8791cd25d81f'
+  sha256 'd5ea2f528e3115a1e9ddfdbaf57e7674249817f0dcd93e91bd55180e80727d7f'
 
   url "http://orange.biolab.si/download/files/Orange-#{version}.dmg"
   name 'Orange'
