dbbebb79cb70be65edcd0c073df9c1380d26d8e8
Jan Sauer <jan@jansauer.de>
2015-06-09 23:07:50 +0200

    Fix url, appcast and homepage to use SSL in Focus Cask
    The HTTP URL is already getting redirected to HTTPS. Using the HTTPS URL directly
    makes it more secure and saves a HTTP round-trip.

--- Casks/focus.rb
+++ Casks/focus.rb
@@ -2,10 +2,10 @@ cask :v1 => 'focus' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.heyfocus.com/releases/Focus-latest.zip'
-  appcast 'http://www.heyfocus.com/appcast.xml'
+  url 'https://heyfocus.com/releases/Focus-latest.zip'
+  appcast 'https://heyfocus.com/appcast.xml'
   name 'Focus'
-  homepage 'http://www.heyfocus.com/'
+  homepage 'https://heyfocus.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Focus.app'
