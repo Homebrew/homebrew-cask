20423a11d12c7ebc6255dfcb769dc58bb6767c9e
Vítor Galvão <info@vitorgalvao.com>
2015-06-09 23:02:27 +0100

    finderminder.rb: fix url

--- Casks/finderminder.rb
+++ Casks/finderminder.rb
@@ -2,7 +2,7 @@ cask :v1 => 'finderminder' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.irradiatedsoftware.com/downloads/?file=FinderMinder.zip'
+  url 'http://www.irradiatedsoftware.com/download/FinderMinder.zip'
   name 'FinderMinder'
   homepage 'http://irradiatedsoftware.com/labs'
   license :gratis
