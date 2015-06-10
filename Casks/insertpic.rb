0a6a9e528c7e043e62813e49acafbf76ff989cbb
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-10 11:38:11 +0300

    insertpic.rb: upgrade to v4.0

--- Casks/insertpic.rb
+++ Casks/insertpic.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'insertpic' do
-  version '3.5'
-  sha256 'f12f9ec1f023f8c38fd735ecc2d2c725ee0e7dab757f6a5250e22198ea179e26'
+  version '4.0'
+  sha256 'cb2bd2aa1f00637a49ef035018eba66df3095bc3c9a69520a9287d6f8c81aa70'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/insertPic/InsertPic_#{version}.zip"
