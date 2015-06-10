37ffd2688f23fda3196283ce1b3e58ba209c5b6b
Jan Sauer <jan@jansauer.de>
2015-06-10 23:27:10 +0200

    Fix homepage to use SSL in Tresorit Cask
    The HTTP URL is already getting redirected to HTTPS. Using the HTTPS URL directly
    makes it more secure and saves a HTTP round-trip.

--- Casks/tresorit.rb
+++ Casks/tresorit.rb
@@ -5,7 +5,7 @@ cask :v1 => 'tresorit' do
   # windows.net is the official download host per the vendor homepage
   url 'https://installerstorage.blob.core.windows.net/public/install/Tresorit.dmg'
   name 'Tresorit'
-  homepage 'http://tresorit.com'
+  homepage 'https://tresorit.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Tresorit.app'
