5f66cd5c2bccb3f69ca9ff2a6cf8b5f56367cac8
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-10 11:41:08 +0300

    machg.rb: upgrade to v1.0.2

--- Casks/machg.rb
+++ Casks/machg.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'machg' do
-  version '0.9.25'
-  sha256 '4eaf4abf4a8c52ce0bf4075a3aae7b0b94c8f551340245b3cb72d2a97fb93531'
+  version '1.0.2'
+  sha256 'af15173111c1d350ba5b62edf6fe5ab3a186cc8bdb6302455c4fb04f2a809305'
 
   url "http://jasonfharris.com/machg/downloads/assets/MacHg#{version}.zip"
   appcast 'http://jasonfharris.com/machg/machg_appcast.xml',
