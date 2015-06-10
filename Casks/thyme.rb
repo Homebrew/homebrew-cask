9d6232a9c70c2d43ce7278fea2a22ed1c70082d7
Vítor Galvão <info@vitorgalvao.com>
2015-06-10 19:48:56 +0100

    added appcast to thyme

--- Casks/thyme.rb
+++ Casks/thyme.rb
@@ -3,6 +3,7 @@ cask :v1 => 'thyme' do
   sha256 '9757fff3198d379d3fc1d6231669d1eea6e0d1ba7aa2b876782998415d050ae2'
 
   url "https://github.com/joaomoreno/thyme/releases/download/#{version}/Thyme.#{version}.dmg"
+  appcast 'https://github.com/joaomoreno/thyme/releases.atom'
   name 'Thyme'
   homepage 'http://joaomoreno.github.io/thyme/'
   license :mit
