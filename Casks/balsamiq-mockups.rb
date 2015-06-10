9a96f29e52a1b00fec2a8037aa7472aaa7dff308
Jan Sauer <jan@jansauer.de>
2015-06-10 12:33:20 +0200

    Fix homepage to use SSL in Balsamiq Mockups Cask
    The HTTP URL is already getting redirected to HTTPS. Using the HTTPS URL directly
    makes it more secure and saves a HTTP round-trip.

--- Casks/balsamiq-mockups.rb
+++ Casks/balsamiq-mockups.rb
@@ -5,7 +5,7 @@ cask :v1 => 'balsamiq-mockups' do
   # amazonaws is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/build_production/mockups-desktop/Balsamiq_Mockups_#{version}.dmg"
   name 'Balsamiq Mockups'
-  homepage 'http://balsamiq.com/'
+  homepage 'https://balsamiq.com/'
   license :commercial
 
   app "Balsamiq Mockups #{version.to_i}.app"
