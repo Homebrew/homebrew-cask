c0646567c7cab6d9ec83c9184f52e0c68b38d1ec
Joel Glovacki <saetia@gmail.com>
2015-06-10 16:51:53 -0500

    Upgrade Ghostlab.app to v2.0

--- Casks/ghostlab.rb
+++ Casks/ghostlab.rb
@@ -2,11 +2,11 @@ cask :v1 => 'ghostlab' do
   version :latest
   sha256 :no_check
 
-  url 'http://awesome.vanamco.com/downloads/ghostlab/Ghostlab.dmg'
-  appcast 'http://awesome.vanamco.com/update/ghostlab-cast.xml'
+  url 'http://awesome.vanamco.com/Ghostlab2/downloads/Ghostlab2.dmg'
+  appcast 'http://awesome.vanamco.com/Ghostlab2/update/ghostlab2-cast.xml'
   name 'Ghostlab'
   homepage 'http://vanamco.com/ghostlab/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  app 'Ghostlab.app'
+  app 'Ghostlab2.app'
 end
