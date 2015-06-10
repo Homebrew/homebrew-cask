ec65c0d19e8f4fce56af2322736b588fad344ec7
Jan Sauer <jan@jansauer.de>
2015-06-10 23:52:37 +0200

    Update iExplorer to 3.7.5.0
    The new url is taken from the appcast entry.

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'iexplorer' do
-  version '3.7.4.0'
-  sha256 'd55e905def1a0b691fefdcf3ae2b0d3771b6a640f17b5168dac7f625d904e08c'
+  version '3.7.5.0'
+  sha256 '724249be5603f224e52fa1bf20e6be11c92d30ff3422eb92521cb1e40d07ad93'
 
-  url "http://cdn.macroplant.com/release/iExplorer-#{version}.dmg"
+  url "https://www.macroplant.com/release/iExplorer-#{version}.dmg"
   appcast 'https://www.macroplant.com/iexplorer/ie3-appcast.xml',
           :sha256 => '7d204dc3e1c42c6f50e30f1175c859b515f708d86a406f040ed684e2a5fda59c'
   name 'iExplorer'
