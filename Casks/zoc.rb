10513913c8d4445da768737cc041036eacb912ff
Sam Doran <github@samdoran.com>
2015-06-11 00:18:31 -0500

    Update zoc to 7.04.1

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'zoc' do
-  version '7.01.0'
-  sha256 '6d4d35be75ad87f8b11644c1823e5c11c17e5667a11cc8f94ea00f2a8e53461b'
+  version '7.04.1'
+  sha256 '057d2b283caa1aacec66d162b1aefd6638f7c78b97fa3d3b6ea4aab3b9480b70'
 
   url "http://www.emtec.com/downloads/zoc/zoc#{version.gsub('.','')}.dmg"
   name 'ZOC'
