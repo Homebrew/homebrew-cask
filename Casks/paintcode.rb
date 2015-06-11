2bc4af08032dd7ec642c7e79fe47eaee2cd788c2
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-11 17:34:53 +0300

    paintcode.rb: upgrade to v2.3.2

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'paintcode' do
-  version '2.3.1'
-  sha256 '6355a0ea775de821982e0110fa094b5995ba37daf612363b75d24cf74be7a702'
+  version '2.3.2'
+  sha256 '7a91f926ff352659abb5a189d0533c572a2b0bdced93d7b8bb94692d1b9e3487'
 
   url "http://www.paintcodeapp.com/content/versions/#{version}/paintcode-trial.zip"
   appcast 'http://www.pixelcut.com/paintcode/appcast.xml'
