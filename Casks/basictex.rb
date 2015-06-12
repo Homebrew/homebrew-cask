56bf1eea14a34edc76d5546c9594bdb35ce52f88
Tim D. Smith <git@tim-smith.us>
2015-06-12 12:12:13 -0700

    basictex: update URL

--- Casks/basictex.rb
+++ Casks/basictex.rb
@@ -3,14 +3,14 @@ cask :v1 => 'basictex' do
   sha256 :no_check
 
   # ctan.org is the official download host per the vendor homepage
-  url 'http://mirror.ctan.org/systems/mac/mactex/mactex-basic.pkg'
+  url 'http://mirror.ctan.org/systems/mac/mactex/BasicTeX.pkg'
   name 'BasicTeX'
   homepage 'http://www.tug.org/mactex/morepackages.html'
   license :oss
 
-  pkg 'mactex-basic.pkg'
+  pkg 'BasicTeX.pkg'
 
-  uninstall :pkgutil => 'org.tug.mactex.basictex2014'
+  uninstall :pkgutil => 'org.tug.mactex.basictex2015'
   caveats do
     path_environment_variable '/usr/texbin'
   end
