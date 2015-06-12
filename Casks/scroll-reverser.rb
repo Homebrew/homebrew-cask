07480d58fc93c4ebb08332e9f34bf7889208df27
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-12 20:34:42 +0300

    scroll-reverser.rb: upgrade to v1.7.2

--- Casks/scroll-reverser.rb
+++ Casks/scroll-reverser.rb
@@ -4,8 +4,8 @@ cask :v1 => 'scroll-reverser' do
     version '1.5.1'
     sha256 '7d31ac1f2a2fbdc06db200da0aeb6ce7fea9ffff712b70ddffbfb769d4fd0f25'
   else
-    version '1.7'
-    sha256 'acd0e1fe66951299181b2dff8acc772c124ec15df33645a4a9f33d237562716d'
+    version '1.7.2'
+    sha256 'f0ad4d18daae486b14ea5b6809c5222af6c5fc74536574151dce2271d3a67c06'
     appcast 'http://softwareupdate.pilotmoon.com/update/scrollreverser/appcast.xml',
             :sha256 => 'ed8e2d6748eb8b79897ca1b84a1597e86afb7845b469a278c83c07e08e6eb886'
   end
