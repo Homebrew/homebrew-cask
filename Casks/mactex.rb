c2667dfa63fed4a3a9a6492e7abdcb6b337ac029
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-06-12 11:14:59 +0200

    fix indent

--- Casks/mactex.rb
+++ Casks/mactex.rb
@@ -3,7 +3,7 @@ cask :v1 => 'mactex' do
   sha256 'e5e3c3f0c753878dd7bd55660cef87a37816179c9b323b75f36daf11a6824b62'
 
   # ctan.org is the official download host per the vendor homepage
-   url "http://mirror.ctan.org/systems/mac/mactex/mactex-#{version}.pkg"
+  url "http://mirror.ctan.org/systems/mac/mactex/mactex-#{version}.pkg"
 name 'MacTeX'
   homepage 'http://www.tug.org/mactex/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
