b7a58b3513d590f303ba6740360d68be4fe5d24d
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-13 15:23:02 +0300

    feeds.rb: upgrade to v2.1.1

--- Casks/feeds.rb
+++ Casks/feeds.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'feeds' do
-  version '2.0.5.1'
-  sha256 'abc72e4ef77a0bef4ea89d62b570698d47b197f2d56b74a48a9f3da52c6cd71e'
+  version '2.1.1'
+  sha256 '098dc0ceec2ca1ef26e6ff9f3a7ab4fd7c71877a7fc698584c986525a2204fdf'
 
   # googleapis.com is the official download host per the vendor homepage
   url "https://storage.googleapis.com/feeds-releases/Feeds-#{version}.zip"
