5441a0928b2608588c357ad67e3067c8911cfeb3
Nathan Henrie <n8henrie@gmail.com>
2015-06-15 16:40:35 -0600

    Add QuickCal.app Latest

---
+++ Casks/quickcal.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'quickcal' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://quickcalapp.com/app/quickcal-latest.zip'
+  name 'QuickCal'
+  homepage 'http://quickcalapp.com/'
+  license :commercial 
+
+  app 'QuickCal.app'
+end
