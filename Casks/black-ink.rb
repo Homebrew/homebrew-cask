fa5fedab6423e61c08896da58bc09709e2206355
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-17 15:51:17 +0300

    black-ink.rb: update appcast checksum

--- Casks/black-ink.rb
+++ Casks/black-ink.rb
@@ -4,7 +4,7 @@ cask :v1 => 'black-ink' do
 
   url "http://www.red-sweater.com/blackink/BlackInk#{version}.zip"
   appcast 'http://www.red-sweater.com/blackink/appcast1.php',
-          :sha256 => '0415edc784d3ade2f4f53a501cac7a584a01ebf09d51d5ea2803cbfbc5648496'
+          :sha256 => '2c18470c2510e32cf8eacc19867067f88d5f01fd8f9fd6aa590d0237d4984695'
   name 'Black Ink'
   homepage 'http://www.red-sweater.com/blackink/'
   license :commercial
