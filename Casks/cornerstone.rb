61cce04056a82e9570ca1ffd8db06d907ecfa67f
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-17 15:58:37 +0300

    cornerstone.rb: update appcast checksum and switch it to use SSL

--- Casks/cornerstone.rb
+++ Casks/cornerstone.rb
@@ -3,8 +3,8 @@ cask :v1 => 'cornerstone' do
   sha256 '792882881b5e1f7642433f720e1673f7711d5dfca3d333e7a0fe5c7783092f09'
 
   url "https://www.zennaware.com/cornerstone/downloads/Cornerstone-#{version}.zip"
-  appcast 'http://www.zennaware.com/cornerstone/appcast/feed2.php',
-          :sha256 => '333fee9fc2c478a0d5305ad2d9faecdb2c44e5554e8d43762c0917a7531fb145'
+  appcast 'https://www.zennaware.com/cornerstone/appcast/feed2.php',
+          :sha256 => '9c86ba0e2bdbcede056976040a507ebd3c34f4229ae2fd1fa402ab937adc95e6'
   name 'Cornerstone'
   homepage 'http://www.zennaware.com/cornerstone/'
   license :commercial
