9cf37c45f1c2a535ea5fe10b9ec922f5d125c789
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-17 15:52:02 +0300

    clamxav.rb: update appcast checksum

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -4,7 +4,7 @@ cask :v1 => 'clamxav' do
 
   url "http://www.clamxav.com/downloads/ClamXav_#{version}.dmg"
   appcast 'http://www.clamxav.com/sparkle/profileInfo.php',
-          :sha256 => '02c70a63eb2b650523d6fdbba82ffb102b19676d576c4a2baa8332194828feb3'
+          :sha256 => '01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b'
   name 'ClamXav'
   homepage 'http://www.clamxav.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
