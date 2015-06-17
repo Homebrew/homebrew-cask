6b155b72584d8a6cc30acb56c5b60662172ed5aa
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-17 16:03:40 +0300

    geekbench.rb: update appcast checksum

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -4,7 +4,7 @@ cask :v1 => 'geekbench' do
 
   url "http://cdn.primatelabs.com/Geekbench-#{version}-Mac.zip"
   appcast 'http://www.primatelabs.com/appcast/geekbench3.xml',
-          :sha256 => 'e16473af8653e0c163f02ad054e9e6b7b3c5e99c4768da123d8e21c8f8ffeeaf'
+          :sha256 => 'c8f482bf12e4ec327801466c46bc937d913a6e3a4f8689f54001bed06681faee'
   name 'Geekbench'
   homepage 'http://www.primatelabs.com/geekbench/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
