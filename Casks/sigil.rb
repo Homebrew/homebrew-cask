ac5ba7d04c70e301ba521c2b91a2ab1d5003a3be
Georg Stadler <shorshe@users.noreply.github.com>
2015-06-18 10:00:28 +0200

    sigil.rb: updated sha256 due to updated binary
    Version 0.8.7 is out.

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'sigil' do
-  version '0.8.6'
-  sha256 'b69d642d7f1126b7f7d1c542773267c1dbaa764e2ec473258ff93d3a8f385c71'
+  version '0.8.7'
+  sha256 'a34ad28a758c64202cb380dcb3831d8f3a6afc100926be7be93d12f39f9493d7'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/user-none/Sigil/releases/download/#{version}/Sigil-#{version}-Mac-Package.dmg"
