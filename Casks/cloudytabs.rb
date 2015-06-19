09f9b6256b5c79c62f73a16fde1b8d3ee08819e1
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-19 23:57:47 +0300

    cloudytabs.rb: upgrade to v1.6

--- Casks/cloudytabs.rb
+++ Casks/cloudytabs.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'cloudytabs' do
-  version '1.4'
-  sha256 '7ba67b0f7415fe2d2cb545866eb0e9c7c2bea8980445921a9da5d2bf55711d76'
+  version '1.6'
+  sha256 '0007f978204e55f3d52b099b143ee8dedb418f3d25ab1805ee1ce16e75a13983'
 
   url "https://github.com/josh-/CloudyTabs/releases/download/v#{version}/CloudyTabs.zip"
   appcast 'https://github.com/josh-/CloudyTabs/releases.atom'
