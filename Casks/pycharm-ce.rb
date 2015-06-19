ba64d76aa1f92a55a5dd80b1dc3f31bb89ad9366
Noel Merket <noel.merket@nrel.gov>
2015-06-19 09:37:46 -0600

    New patch version of pycharm

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pycharm-ce' do
-  version '4.5.1'
-  sha256 '472ade2737d1d86b6d83f1d6e3ae85e011a2194d2a94f1397ae09eeb229f79af'
+  version '4.5.2'
+  sha256 'e1541e68a3c59464f7f8266f144ebc4dc99499da914d001f768e0e0b1dc2d6fd'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version}.dmg"
   name 'PyCharm'
