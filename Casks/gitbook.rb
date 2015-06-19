a5db55be9a2fa2603153272f05940218aad7efe9
Samy Pessé <samypesse@gmail.com>
2015-06-19 14:32:57 +0200

    Update gitbook.rb

--- Casks/gitbook.rb
+++ Casks/gitbook.rb
@@ -2,10 +2,10 @@ cask :v1 => 'gitbook' do
   version '1.1.0'
   sha256 'f8e2f7b28e7dfa18b3736f8d43d68068228774c0827fdc8c685846a129459c5f'
 
-  url "https://github.com/GitbookIO/editor/releases/download/#{version}/gitbook-mac.dmg"
-  appcast 'https://github.com/GitbookIO/editor/releases.atom'
+  url "https://github.com/GitbookIO/editor-legacy/releases/download/#{version}/gitbook-mac.dmg"
+  appcast 'https://github.com/GitbookIO/editor-legacy/releases.atom'
   name 'GitBook'
-  homepage 'https://github.com/GitbookIO/editor'
+  homepage 'https://github.com/GitbookIO/editor-legacy'
   license :apache
 
   app 'GitBook.app'
