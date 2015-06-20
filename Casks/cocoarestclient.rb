ba3bb4a6c6156fd0b7bfa2d2db6e85874805aca9
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-20 22:27:57 +0300

    cocoarestclient.rb: upgrade to v1.3.9

--- Casks/cocoarestclient.rb
+++ Casks/cocoarestclient.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'cocoarestclient' do
-  version '1.3.7'
-  sha256 '78400a16afc4017a68b5506e7a6270d72ade48b3e77a802eaaaaae80a0f6319f'
+  version '1.3.9'
+  sha256 '611cf1112c8188d58ad5b00d507c4a5429fc6d1dff0bc0eb438116b672a880ba'
 
   url "https://github.com/mmattozzi/cocoa-rest-client/releases/download/#{version}/CocoaRestClient-#{version}.dmg"
   appcast 'https://github.com/mmattozzi/cocoa-rest-client/releases.atom'
