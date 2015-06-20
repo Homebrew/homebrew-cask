223eb8d9c59757b2fc9c19013882eb4e516bde40
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2015-06-20 21:09:18 +0900

    Upgrade Kitematic.app to v0.7.2

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'kitematic' do
-  version '0.6.6'
-  sha256 'c590aa9bfe1dbb403b7f57ef5de4719051a34f26c0803db1d62b7e93c4ba6ed8'
+  version '0.7.2'
+  sha256 '8a9a0d090799025c47b6729f05da1fe29858e2b92ff982a201541913603fcf25'
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}.zip"
+  url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
   appcast 'https://github.com/kitematic/kitematic/releases.atom'
   name 'Kitematic'
   homepage 'https://kitematic.com/'
