074d662f437dfed36c88a432eb9fa2135c7b78f2
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-20 22:29:02 +0300

    consul-template.rb: upgrade to v0.10.0

--- Casks/consul-template.rb
+++ Casks/consul-template.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'consul-template' do
-  version '0.9.0'
-  sha256 'ea054bb62a556ffa58bbad29aa17b370694ba7c49a7539195f9afeafbec16be1'
+  version '0.10.0'
+  sha256 '178e8e59d2e380440ac5582fb7f49c946ff931c1589ac85258d7dba82aefaabe'
 
   url "https://github.com/hashicorp/consul-template/releases/download/v#{version}/consul-template_#{version}_darwin_amd64.tar.gz"
   appcast 'https://github.com/hashicorp/consul-template/releases.atom'
