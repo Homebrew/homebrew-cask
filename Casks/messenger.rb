845e25194f4ac1c430ca581cf511092e991f6625
Patrik Ragnarsson <patrik@starkast.net>
2015-06-20 23:36:49 +0200

    Update Messenger to 0.0.9

--- Casks/messenger.rb
+++ Casks/messenger.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'messenger' do
-  version '0.0.8.1433711909-d640fa4b728ab130'
-  sha256 '18b7797830c948e48801563d12b24192eb7149b9984f84c4425f3ba7c1238c2d'
+  version '0.0.9.1434655833-20a46b37192bfacc'
+  sha256 '3425494ad9d45cb0458254296e5e09103dbfe3c05f1c1b7b0c0a0abc19a7264c'
 
   url "http://fbmacmessenger.rsms.me/dist/Messenger-#{version}.zip"
   name 'Messenger'
