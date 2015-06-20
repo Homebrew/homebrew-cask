4fdd07f500ef48a5fc2e2fcd098b21c539cb939c
Koichi Shiraishi <zcheeee@gmail.com>
2015-06-20 15:05:13 +0900

    Upgrade Kensington Trackball Works to 1.2.1
    Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- Casks/kensington-trackball-works.rb
+++ Casks/kensington-trackball-works.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'kensington-trackball-works' do
-  version '1.2.0'
-  sha256 '11c67355484bb55e2a2aca33f40fa8294612aa70a0be21a1da18d4cbc6816975'
+  version '1.2.1'
+  sha256 '285511269aea2e0517198b354923676655c72142062dcba7bdc41bc29d1f08d1'
 
   # windows.net is the official download host per the vendor homepage
-  url 'http://accoblobstorageus.blob.core.windows.net/software/b54b0855-21e0-4cfa-a084-2b62e6f4aa93.dmg'
+  url 'http://accoblobstorageus.blob.core.windows.net/software/926df442-8736-45a1-85f2-435a67723bb0.dmg'
   name 'Kensington TrackballWorks'
   homepage 'http://www.kensington.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
