08e62412340b67efa352bd40fe7baca13e09ae90
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-20 22:31:30 +0300

    darktable.rb: upgrade to v1.6.7

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'darktable' do
-  version '1.6.6'
-  sha256 'bce9a792ee362c47769839ec3e49973c07663dbdf6533ef5a987c93301358607'
+  version '1.6.7'
+  sha256 '6630230049e6d2c4cdfd39585f95fbd1ee439a8dad107f7332aefeb1dd75b831'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version}/darktable-#{version}.dmg"
