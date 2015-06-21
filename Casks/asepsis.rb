0615944dc3f7f4fd89644fc29bc20e3e6047ee75
Markus Reiter <me@reitermark.us>
2015-06-21 06:15:03 +0200

    Fix El Capitan Support for Asepsis 1.5.1

--- Casks/asepsis.rb
+++ Casks/asepsis.rb
@@ -1,13 +1,15 @@
 cask :v1 => 'asepsis' do
   version '1.5.1'
   sha256 '3bf6c1bd9c2a4bb81d05c8142595e552681e45ee44abd96994168a7001f46807'
-
   url "http://downloads.binaryage.com/Asepsis-#{version}.dmg"
+
   name 'Asepsis'
   homepage 'http://asepsis.binaryage.com/'
   license :bsd
 
   pkg 'Asepsis.pkg'
+  
+  depends_on :macos => '<= :yosemite'
 
   uninstall :pkgutil => 'com.binaryage.pkg.asepsis'
 end
