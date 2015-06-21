6bf78db2b4d6067353f511fe8e427e7a58acda90
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-21 15:04:26 +0300

    gamesalad.rb: upgrade to v0.13.23

--- Casks/gamesalad.rb
+++ Casks/gamesalad.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'gamesalad' do
-  version '0.13.22'
-  sha256 '4f8672e2f2b7763c6b057aa6238d5289fbb600988ace37db7bfa7c022876c13a'
+  version '0.13.23'
+  sha256 '3d340d1bed34e750477f978ca09e37090f959d1c1ccb8b66184c89d2a1dad73a'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "http://gs.releases.s3.amazonaws.com/creator/#{version}/GameSalad-Creator-#{version}-beta.dmg"
   name 'GameSalad'
   appcast 'https://gamesalad.com/download/studioUpdates',
-          :sha256 => 'b2f1f4c2a63bc553ab714aca3c92340b0e6612bdab13349459aa344934cdb298'
+          :sha256 => '620516c35dc6c5abcb475eccf8d29af953225fb6990ac85d00779ae68ddd383a'
   homepage 'http://gamesalad.com/'
   license :commercial
 
