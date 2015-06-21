20a641a6ad5315795be11d3d48270210338fa72b
Brad Rowe <gord1anknot@users.noreply.github.com>
2015-06-21 11:41:32 -0600

    Update groovesquid to 0.9.2

--- Casks/groovesquid.rb
+++ Casks/groovesquid.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'groovesquid' do
-  version '0.9.1'
-  sha256 'ca6e7278619d8a21906faec6f58cbcc6b5fa6eae30537bf076ff656b102bf9f8'
+  version '0.9.2'
+  sha256 'caf86c4d0dcbafb141fdaf45361003377ebdaefa0e3173981516e044efab2f4e'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/groovesquid/groovesquid/releases/download/v#{version}/Groovesquid.dmg"
