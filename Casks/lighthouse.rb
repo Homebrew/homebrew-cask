71c7f4ebb1a92a32d99392cd2b46c70dffc8aada
Sean Smith <sean@seansith.com>
2015-06-20 22:02:37 -0400

    Added Vinumeris Lighthouse
    Closes #10330

---
+++ Casks/lighthouse.rb
@@ -0,0 +1,13 @@
+cask :v1 => 'lighthouse' do
+  version :latest
+  sha256 :no_check
+
+  # amazonaws.com is the official download host per the vendor homepage
+  url 'https://s3-eu-west-1.amazonaws.com/vinumeris/lighthouse/Lighthouse.dmg'
+  name 'Vinumeris Lighthouse'
+  homepage 'https://www.vinumeris.com/lighthouse'
+  license :closed
+  tags :vendor => 'Vinumeris'
+
+  app 'Lighthouse.app'
+end
