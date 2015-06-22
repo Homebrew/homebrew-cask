40b77391a91f670e50dda2306657c4a2860f6dcc
arran ubels <a.ubels@base2services.com>
2015-06-22 14:45:16 +1000

    Version subin as suggested

--- Casks/Jedict.rb
+++ Casks/Jedict.rb
@@ -3,12 +3,12 @@ cask :v1 => 'Jedict' do
   if MacOS.release < :snow_leopard
     version '3.8.1'
     sha256 '0af8380f86f7d1de1dd81e8ba57ef204dc2525ce18d1a7ba322af6343e428ed2'
-    url "http://jedict.com/Downloads/JEDict381.sit"
+    url "http://jedict.com/Downloads/JEDict#{version.gsub('.','')}.sit"
     app 'JEDict.app'
   else
     version '4.9.5'
     sha256 '6051434c77ad48af7b11f2259a91b12ded21ce3357c8db4780718ce83f44b668'
-    url "http://jedict.com/Downloads/JEDict495.zip"
+    url "http://jedict.com/Downloads/JEDict#{version.gsub('.','')}.zip"
     app 'JEDict.app'
   end
   name 'Jedict'
