ea071567bbdad9ef0a46c5b9810260238a53dc6d
Stevie Howard <stvhwrd@users.noreply.github.com>
2015-06-22 10:13:31 -0700

    Version bump to 43.0.2357.124

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'freesmug-chromium' do
-  version '43.0.2357.81'
-  sha256 'fc2c43f899d192d52ea84ae6940911094d6f670062b3c03e69499c689601c2fa'
+  version '43.0.2357.124'
+  sha256 'f3ded99268dad92c17f9c3d1ec2b0c82e3d8f27ec33e1881f3aafa9d3a0a5f83'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/osxportableapps/Chromium_OSX_#{version}.dmg"
