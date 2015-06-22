0f134ff1db0704c21fd07b5e79b709630e6d7f30
Christopher LILJENSTOLPE <cdl@asgaard.org>
2015-06-22 02:20:51 -0700

    fixed a typo in the ruby file

--- Casks/photo-supreme-postgresql.rb
+++ Casks/photo-supreme-postgresql.rb
@@ -2,7 +2,6 @@ cask :v1 => 'photo-supreme-postgresql' do
   version '3'
   sha256 'ed1d669e46cf073a4720224c0f08aca70da2e6c6687be1a9d733bda9e45a5304'
 
-
   url 'http://www.idimager.com/Trial/PhotoSupremePostgreSQL_V3.pkg'
   name 'Photo Supreme with PostreSQL'
   homepage 'http://www.idimager.com/WP/?page_id=20'
