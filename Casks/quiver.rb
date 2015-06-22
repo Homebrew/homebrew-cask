e5226f6b29a36f0c04ba4ff0ac21f23c03271085
Zach Schneider <schneidmaster@gmail.com>
2015-06-21 19:21:53 -0500

    Add Quiver.app v2.1.2

---
+++ Casks/quiver.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'quiver' do
+  version '2.1.2'
+  sha256 '2dfcf9ba63bab5a09acc834163f4d3aae391d10182959cc3e3a671fd4ebf42b3'
+
+  url 'http://happenapps.com/downloads/QuiverFreeTrial.zip'
+  name 'Quiver'
+  homepage 'http://happenapps.com/#quiver'
+  license :closed
+
+  app 'Quiver.app'
+end
