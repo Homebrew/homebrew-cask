519769a10f1da71cde07db94ca32b662f259a870
arran ubels <a.ubels@base2services.com>
2015-06-22 14:45:44 +1000

    Removed SIT file for older PCs as I am not sure it is supported

--- Casks/Jedict.rb
+++ Casks/Jedict.rb
@@ -1,16 +1,8 @@
 cask :v1 => 'Jedict' do
-  # Site on mainpage denotes that 3.8.1 is for older systems, anything supporting 'carbon' what ever that means.
-  if MacOS.release < :snow_leopard
-    version '3.8.1'
-    sha256 '0af8380f86f7d1de1dd81e8ba57ef204dc2525ce18d1a7ba322af6343e428ed2'
-    url "http://jedict.com/Downloads/JEDict#{version.gsub('.','')}.sit"
-    app 'JEDict.app'
-  else
-    version '4.9.5'
-    sha256 '6051434c77ad48af7b11f2259a91b12ded21ce3357c8db4780718ce83f44b668'
-    url "http://jedict.com/Downloads/JEDict#{version.gsub('.','')}.zip"
-    app 'JEDict.app'
-  end
+	version '4.9.5'
+	sha256 '6051434c77ad48af7b11f2259a91b12ded21ce3357c8db4780718ce83f44b668'
+	url "http://jedict.com/Downloads/JEDict#{version.gsub('.','')}.zip"
+	app 'JEDict.app'
   name 'Jedict'
   homepage 'http://www.jedict.com/'
   license :commercial
