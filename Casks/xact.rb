657deeee529d98cd252dd14e8e2a3c68361378a6
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-22 23:38:58 +0300

    xact.rb: upgrade to v2.36
    Signed-off-by: Fernando Paredes <nano@fdp.io>

--- Casks/xact.rb
+++ Casks/xact.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'xact' do
-  version '2.35'
-  sha256 'f1c587262b19e171845911ad1231e9295825d3ab592b75a6a571a5e76da8097a'
+  version '2.36'
+  sha256 '8b25b05bdcbee23f58866df3c69ea8d3d14236643a802316423c10c3e058e80f'
 
   url "http://xact.scottcbrown.org/xACT#{version}.zip"
   name 'xACT'
   appcast 'http://xactupdate.scottcbrown.org/xACT.xml',
-          :sha256 => 'fc41532e1aff82fbfd45fd7d9a65c5a5d661d536b535d36f3b0c3658b44f8d73'
+          :sha256 => 'cb5df4e8d11f77927b2ebc9dd0281426252f7feb8abb5ec62fa3819748344a45'
   homepage 'http://xact.scottcbrown.org'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
