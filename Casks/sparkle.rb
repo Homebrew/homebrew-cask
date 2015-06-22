b88ae279a18f86946e7df1e61412c1f6c8d43560
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-22 21:05:01 +0300

    sparkle.rb: upgrade to v1.10.0

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'sparkle' do
-  version '1.8.0'
-  sha256 '882f2e414d2d8a3e776f9a9888cbfcb744df88f3b061325c2aad3c965af5ac1a'
+  version '1.10.0'
+  sha256 '415d9a860453e24aa5cb07fe850eeb1e90a8296f926617d7e1a7521a2e2f7cae'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.bz2"
