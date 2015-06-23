20a738e9fa76db6e7fbf627ef93a6a86fbf8fc02
lalyos <lalyos@yahoo.com>
2015-06-23 21:43:43 +0200

    Update packer to 0.8.0

--- Casks/packer.rb
+++ Casks/packer.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'packer' do
-  version '0.7.5'
-  sha256 'c0e149c4515fe548c1daeafabec3b4a091f2aa0c6936723382b3f6fe5a617880'
+  version '0.8.0'
+  sha256 'fabdd4815d44314ccb253ab87b872d77522c813af8a0ba19837fa490400547bb'
 
   # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/mitchellh/packer/packer_#{version}_darwin_amd64.zip"
@@ -14,6 +14,7 @@ cask :v1 => 'packer' do
   binary 'packer-builder-amazon-instance'
   binary 'packer-builder-digitalocean'
   binary 'packer-builder-docker'
+  binary 'packer-builder-file'
   binary 'packer-builder-googlecompute'
   binary 'packer-builder-null'
   binary 'packer-builder-openstack'
@@ -37,8 +38,12 @@ cask :v1 => 'packer' do
   binary 'packer-provisioner-chef-client'
   binary 'packer-provisioner-chef-solo'
   binary 'packer-provisioner-file'
+  binary 'packer-provisioner-powershell'
   binary 'packer-provisioner-puppet-masterless'
   binary 'packer-provisioner-puppet-server'
   binary 'packer-provisioner-salt-masterless'
   binary 'packer-provisioner-shell'
+  binary 'packer-provisioner-shell-local'
+  binary 'packer-provisioner-windows-restart'
+  binary 'packer-provisioner-windows-shell'
 end
