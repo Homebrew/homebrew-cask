3713318857373e51dfe032a7e5b53367c1585cdf
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-23 16:23:10 +0300

    feeder.rb: upgrade to v2.5.12

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'feeder' do
-  version '2.5.11'
-  sha256 '9e59b1c4a891a6d65636b32983f49ffb7e75ad5bc59a63c2b10206efed8fcdfe'
+  version '2.5.12'
+  sha256 'd48c058a64f3850468aa3ede8c56ff2d6452e0afda5683199d50b2ec63685bda'
 
   url "http://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
   appcast 'http://reinventedsoftware.com/feeder/downloads/Feeder25.xml',
-          :sha256 => '4d454364411c22a093e63e780e4026bb45a54bf1e7ab329a45a77b4c73020dc4'
+          :sha256 => '10c7d4c97d51eea3a98199a34e7734effe35f4de2d2daedb1d6930864f99780f'
   name 'Feeder'
   homepage 'http://reinventedsoftware.com/feeder/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
