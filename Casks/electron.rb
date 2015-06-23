94dfeb29c1a1ac8f2d5d826a1b795de6ba4465d7
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-23 16:26:01 +0300

    electron.rb: upgrade to v0.28.3

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'electron' do
-  version '0.28.2'
-  sha256 '46ce4b140240fdbd34664db523dcc286684d815efa469132e6d2dd4ee2d6914a'
+  version '0.28.3'
+  sha256 'ea3d8c7b4494c9268691bf8ae8b0fba1f14bc8db4377a122b60ab5d0e4c933cd'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom'
