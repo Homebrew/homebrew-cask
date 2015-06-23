e6651850454e0110fcf735ef8ab86061a76f8041
Andrew Saint <andrew.james.saint@gmail.com>
2015-06-23 15:54:59 +0100

    Fix sha256 checksum for MAMP 3.3

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mamp' do
   version '3.3'
-  sha256 '4ae0a04bfd3db982969410045f9297f7b6647b4c0366687c413cfb0da5d90e71'
+  sha256 'fb50cfbf54c82f92808ae46bef50944a1c40fc607b43ad1767c78edc18d0b1d1'
 
   url "http://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
   name 'MAMP'
