fb932a6a2d73dcb1e4cc708c539014f1c59caa89
Sven Ertel <sven@ertel-net.de>
2015-06-23 23:58:35 +0200

    updated flash to flash-18.0.0.194

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'flash' do
-  version '18.0.0.160'
+  version '18.0.0.194'
   sha256 '6a1aa09aae039d52601a2408480e0be8e841e9e74d9ccf0deaf0000d095ac2d8'
 
   # macromedia.com is the official download host per the vendor homepage
