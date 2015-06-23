3fe488f4ed3c6f75c68597496e816143d7c4e062
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2015-06-23 08:59:59 +0100

    Add window wrangler v1.0

---
+++ Casks/window-wrangler.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'window-wrangler' do
+  version '1.0.1'
+  sha256 '889efeccd1ef6b75e11a7050e18317681e3a4c118825bc0b52e892cb017439c7'
+
+  url 'http://mindflow.dk/software/windowwrangler/windowwrangler.dmg'
+  name 'Window Wrangler'
+  name 'WindowWrangler'
+  homepage 'http://mindflow.dk/software/windowwrangler/'
+  license :gratis
+
+  app 'Window Wrangler.app'
+end
