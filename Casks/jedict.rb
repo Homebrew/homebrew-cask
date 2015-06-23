170a9643a9dc3eec6ee7aa3183eb870daa0a5d17
arran ubels <a.ubels@base2services.com>
2015-06-23 10:09:40 +1000

    Got sha wrong

--- Casks/jedict.rb
+++ Casks/jedict.rb
@@ -1,9 +1,9 @@
-cask :v1 => 'Jedict' do
+cask :v1 => 'jedict' do
 	version '4.9.5'
-	sha256 '6051434c77ad48af7b11f2259a91b12ded21ce3357c8db4780718ce83f44b668'
+	sha256 '0af8380f86f7d1de1dd81e8ba57ef204dc2525ce18d1a7ba322af6343e428ed2'
 	url "http://jedict.com/Downloads/JEDict#{version.gsub('.','')}.zip"
 	app 'JEDict.app'
-  name 'Jedict'
+  name 'jedict'
   homepage 'http://www.jedict.com/'
   license :commercial
 end
