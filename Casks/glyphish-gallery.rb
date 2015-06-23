9f8154c2b0011e64bc82bd5f5679ed0cb9a9f604
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-23 16:29:54 +0300

    glyphish-gallery.rb: upgrade to v2.1

--- Casks/glyphish-gallery.rb
+++ Casks/glyphish-gallery.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'glyphish-gallery' do
-  version '2.0'
-  sha256 'c5f224d44ecf4a853a51105d1fce8b4e3eab099ddc16791ce412a7a679b104a7'
+  version '2.1'
+  sha256 '86edda062a651799517990cc96e7f23f38818ecd3f0194e3ad040de0fa82f2d2'
 
   url "https://github.com/glyphish/gallery/releases/download/v#{version}/v#{version}.zip"
   appcast 'https://github.com/glyphish/gallery/releases.atom'
