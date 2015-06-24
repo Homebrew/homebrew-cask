77f4a749f82edb35c4c5191c2d771f4b6cc72db3
Josh Tilles <josh@signafire.com>
2015-06-24 17:30:09 -0400

    Mendeley Desktop 1.14

--- Casks/mendeley-desktop.rb
+++ Casks/mendeley-desktop.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mendeley-desktop' do
-  version '1.13.8'
-  sha256 'f90b5fec479b8dcb7bbf9d3dc6c4afa6a11d10636097653a28687c5f2415362a'
+  version '1.14'
+  sha256 '11d04e1f0a9ea79dccd87aa5f84dd1c0927448a392ca98ff39307861f68df7ab'
 
   url "http://desktop-download.mendeley.com/download/Mendeley-Desktop-#{version}-OSX-Universal.dmg"
   name 'Mendeley'
