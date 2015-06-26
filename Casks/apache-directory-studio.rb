e47930b7d7d3fc0c482e47c17da07ca17d2bd86a
Chris Blankenship <chris@dscoduc.com>
2015-06-26 13:54:12 -0700

    Updated url with variable instead of fixed version

--- Casks/apache-directory-studio.rb
+++ Casks/apache-directory-studio.rb
@@ -3,7 +3,7 @@ cask :v1 => 'apache-directory-studio' do
   sha256 '9eca84d081a500fec84943600723782a6edac05eeab6791fe8a964e49c6d834e'
 
   # apache.org is the official download host per the vendor homepage
-  url "http://www.us.apache.org/dist/directory/studio/2.0.0.v20150606-M9/ApacheDirectoryStudio-#{version}-macosx.cocoa.x86_64.tar.gz"
+  url "http://www.us.apache.org/dist/directory/studio/#{version}/ApacheDirectoryStudio-#{version}-macosx.cocoa.x86_64.tar.gz"
   name 'Apache Directory Studio'
   homepage 'http://directory.apache.org/studio/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
