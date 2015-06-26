e023567bb374bb6f9ece2c931f38166d008c8ef4
Nelson Chen <crazysim@gmail.com>
2015-06-25 23:39:10 -0700

    rust: update to 1.1.0 stable

--- Casks/rust.rb
+++ Casks/rust.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rust' do
-  version '1.0.0'
-  sha256 '6781d3fb15d91e50b5a05a5e2858f55c2716760ce059b657b51239c294fe817b'
+  version '1.1.0'
+  sha256 '5886de03ef376c29e9b3b79dc70a0b66f41e1a62c54da85475ee0d685ae2774a'
 
   url "https://static.rust-lang.org/dist/rust-#{version}-x86_64-apple-darwin.pkg"
   name 'Rust'
