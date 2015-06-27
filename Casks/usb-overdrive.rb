57d7166fa60113a06cfc8515725997a47b2edcef
Ryan Hendrickson <ryan.hendrickson@alum.mit.edu>
2015-06-27 18:41:42 -0400

    usb-overdrive: add old version for pre-Mountain Lion

--- Casks/usb-overdrive.rb
+++ Casks/usb-overdrive.rb
@@ -1,6 +1,11 @@
 cask :v1 => 'usb-overdrive' do
-  version '3.1'
-  sha256 '1a93082f1a2236bca088372c54b1933b3e491c3a986a1f13f78b59fda0c40fa2'
+  if MacOS.release < :mountain_lion
+    version '3.0.2'
+    sha256 'd6bed831521a5ab2b82d7eede7be46953e1c010cc6e02ff11f3dd2532d7b0b13'
+  else
+    version '3.1'
+    sha256 '1a93082f1a2236bca088372c54b1933b3e491c3a986a1f13f78b59fda0c40fa2'
+  end
 
   # senlick.com is the official download host per the vendor homepage
   url "http://www.senlick.com/macsw/USB-Overdrive-#{version.gsub('.','')}.dmg"
