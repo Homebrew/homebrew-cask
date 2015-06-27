381e7675483b99a68af4a1f1274472a98fc7a097
scribblemaniac <connor.deptuck@gmail.com>
2015-06-26 20:13:48 -0600

    Changed link from https to http
    Reason for the change to each formula:
    - chronomate: expired certificate
    - cx: expired certificate
    - inc: expired certificate
    - nothing-to-hide: invalid certificate
    - snippets: download redirected to http

--- Casks/chronomate.rb
+++ Casks/chronomate.rb
@@ -3,7 +3,7 @@ cask :v1 => 'chronomate' do
   sha256 '4002fe0924ba57cb70c77f729d60fdf2182586dcbc8b871d6dcc8051280f83f7'
 
   # qb.com is the official download host per the vendor homepage
-  url "https://studio-qb.com/sites/studio-qb.com/files/releases/chronomate_#{version}.dmg"
+  url "http://studio-qb.com/sites/studio-qb.com/files/releases/chronomate_#{version}.dmg"
   appcast 'http://chronomateapp.com/update',
           :sha256 => 'fc538460e350c638a7e1b87e91e0cc79bbf8e324dc28819a818deee090037feb'
   name 'ChronoMate'
