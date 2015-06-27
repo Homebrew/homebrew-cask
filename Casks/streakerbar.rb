5691cf8995889ce930a316988089a1d75a40bdc1
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-27 17:33:20 +0300

    streakerbar.rb: update appcast to use SSL

--- Casks/streakerbar.rb
+++ Casks/streakerbar.rb
@@ -3,7 +3,7 @@ cask :v1 => 'streakerbar' do
   sha256 '61735052d8e7613dc4d898c4236ed8b277ad742456c2067e4a750c7a937529f5'
 
   url "http://github.com/chaserx/streakerbar/releases/download/v#{version}/streakerbar.zip"
-  appcast 'http://github.com/chaserx/streakerbar/releases.atom'
+  appcast 'https://github.com/chaserx/streakerbar/releases.atom'
   name 'streakerbar'
   homepage 'https://github.com/chaserx/streakerbar'
   license :mit
