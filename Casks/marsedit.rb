7f2ee130c303d72f337652f7fb93cbbaa3ec6cad
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-27 16:57:58 +0300

    marsedit.rb: upgrade to v3.7.1

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'marsedit' do
-  version '3.7'
-  sha256 'cb8010e27ee3d6708b3512884c67ca1422514c20413f06413a88b37189fa5ef0'
+  version '3.7.1'
+  sha256 'c0ff0b59aae840b53a54b7515bfa4b13e5a3b38275a281ab9d5b5a097367a474'
 
   url "http://www.red-sweater.com/marsedit/MarsEdit#{version}.zip"
   appcast 'http://www.red-sweater.com/marsedit/appcast3.php',
-          :sha256 => '3f45ba546de497b40ef88b7e6bea2b057a3935e71b4768ba626ea282edcef744'
+          :sha256 => '02f5ae0eddaa596db862bda36a4c84c47848cfd15cfe384b0d0322d8f73a80ad'
   name 'MarsEdit'
   homepage 'http://www.red-sweater.com/marsedit/'
   license :commercial
