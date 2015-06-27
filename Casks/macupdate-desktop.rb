3a9490790c3b9852ffc90599b921267bdf3faa98
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-27 17:25:22 +0300

    macupdate-desktop.rb: update appcast to not use SSL

--- Casks/macupdate-desktop.rb
+++ Casks/macupdate-desktop.rb
@@ -3,7 +3,7 @@ cask :v1 => 'macupdate-desktop' do
   sha256 '1aa04e0bec920f28af5417a2b875e40af832d5303a29dca27e527e30c4db42f5'
 
   url "http://dl.macupdate.com/MacUpdateDesktop#{version}.zip"
-  appcast 'https://www.macupdate.com/desktop/updates.xml',
+  appcast 'http://www.macupdate.com/desktop/updates.xml',
           :sha256 => 'f34602a28e842a77a7159342463747c05d783d44613feed26bbf742d88e3607f'
   name 'MacUpdate Desktop'
   homepage 'https://www.macupdate.com/desktop'
