ed390e2525205d9bac03fa3b07c330448ec3a8c7
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-27 17:37:24 +0300

    uninstallpkg.rb: update appcast to use SSL

--- Casks/uninstallpkg.rb
+++ Casks/uninstallpkg.rb
@@ -3,7 +3,7 @@ cask :v1 => 'uninstallpkg' do
   sha256 '6f5b88434635afe3eee07dcd0b183f00233ccd089ba0c45e3c4a3c8ddaa1fa4a'
 
   url "http://www.corecode.at/downloads/uninstallpkg_#{version}.zip"
-  appcast 'http://www.corecode.at/uninstallpkg/uninstallpkg.xml',
+  appcast 'https://www.corecode.at/uninstallpkg/uninstallpkg.xml',
           :sha256 => '5f5de8cb9ee55d7c96582f7359a41c9530170f589f45bdad5ee3f04dd22c829d'
   name 'UninstallPKG'
   homepage 'http://www.corecode.at/uninstallpkg/'
