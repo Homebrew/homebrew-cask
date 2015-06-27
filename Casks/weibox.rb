35d2bd434c641fe6427d7dc14114abb1fc0157ab
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-27 17:38:43 +0300

    weibox.rb: update appcast to use SSL

--- Casks/weibox.rb
+++ Casks/weibox.rb
@@ -3,7 +3,7 @@ cask :v1 => 'weibox' do
   sha256 '60c023ba7cc9f93ac72a7b1a4e8a518595348f4f6d8c4fbef0af8822c318a908'
 
   url "https://weiboformac.sinaapp.com/downloads/#{version}.release.zip"
-  appcast 'http://weiboformac.sinaapp.com/appcast/wm2.xml',
+  appcast 'https://weiboformac.sinaapp.com/appcast/wm2.xml',
           :sha256 => '2aefd31728fb15674f7afd1bc2fd4b47f42b6720227f2ee81b8a6d80344815c3'
   name 'WeiboX'
   homepage 'http://weiboformac.sinaapp.com'
