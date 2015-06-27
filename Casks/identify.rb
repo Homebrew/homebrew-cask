9a6dc4e482ce72b2a723e9934a6111facc34e51d
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-27 17:19:14 +0300

    identify.rb: update appcast to use SSL

--- Casks/identify.rb
+++ Casks/identify.rb
@@ -3,7 +3,7 @@ cask :v1 => 'identify' do
   sha256 'e850d12335befe8c092b8758f0ca1b26cb7f0c1decda98e5ef0525b3a37fb2d2'
 
   url "http://identify2.arrmihardies.com/appCast/iDentifyLite#{version}.zip"
-  appcast 'http://dl.dropbox.com/u/1495067/iDentifySparkle/iDentifyAppcast.xml',
+  appcast 'https://dl.dropboxusercontent.com/u/1495067/iDentifySparkle/iDentifyAppcast.xml',
           :sha256 => 'f4136921951cb3bd6a0488d819a6283957adc94d7afcda7477d1bf618d307e85'
   name 'iDentify'
   homepage 'http://identify2.arrmihardies.com/'
