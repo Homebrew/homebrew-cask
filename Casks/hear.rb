a43bbd1e5bd1e004f4c78e51c978baa02947787a
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-28 17:06:58 +0300

    hear.rb: upgrade to v1.2.3

--- Casks/hear.rb
+++ Casks/hear.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'hear' do
-  version '1.2.1'
-  sha256 '6acd179b108283a51debac3c6a4f7cf20220d4129a702ce702f06cc7e2884649'
+  version '1.2.3'
+  sha256 'd28019bcb73d34b7ac1b1851444d94afd2048b94138724997bedbe6dc8df93f6'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/prosoft-engineering/hear/Hear_#{version}.dmg"
   appcast 'https://www.prosofteng.com/resources/sparkle/sparkle.php?psProduct=Hear',
-          :sha256 => '48edc9b03ce5d9709b27fb5099dc3cce25cc8920656b9cdb9a066ae7999c8d9d'
+          :sha256 => '5ff497340df135e5765942ab6f5ae4f083f06676a79be33073c00bd7c7814a88'
   name 'Hear'
   homepage 'https://www.prosofteng.com/hear/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
