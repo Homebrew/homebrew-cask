367418dd5ed97ac6f71ee4f0ce9307d5531b8f8f
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-28 17:12:31 +0300

    mcedit.rb: upgrade to v1.3.3.0

--- Casks/mcedit.rb
+++ Casks/mcedit.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mcedit' do
-  version '1.3.2.0'
-  sha256 '97095224d8b23247311e50f5c048f8fedad2b0063c5c713c3db92d4e9d8efdce'
+  version '1.3.3.0'
+  sha256 '7cb746127b5d0a9f4d40bb2724ad14767df5d8d3f120781efe03a74d99c56897'
 
   url "https://github.com/Khroki/MCEdit-Unified/releases/download/#{version}/MCEdit.v#{version}.OSX.64bit.zip"
   appcast 'https://github.com/Khroki/MCEdit-Unified/releases.atom'
