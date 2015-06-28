2dfab7fbc04d29b03c40ae3434bfe5bedfbddfb3
Peter Lewis <peter@peterlew.is>
2015-06-28 17:56:21 +0100

    Updated knox to 2.3.1

--- Casks/knox.rb
+++ Casks/knox.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'knox' do
-  version '2.2.0'
-  sha256 'c19c56a35d299a2cd85c612e1e99009bff9d7536ddd24f9d565910702be9742a'
+  version '2.3.1'
+  sha256 '93bac0f630bb90452e300d2aaaea38f1fe118f72fe6206377d76ebc691c81555'
 
   # cloudfront.net is the official download host per the vendor homepage
   url "https://d13itkw33a7sus.cloudfront.net/dist/K/Knox-#{version}.zip"
