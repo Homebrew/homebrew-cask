1dc1b671a3bfd9514bad68ccf186ec3e781d2771
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 15:46:11 -0600

    soundcloud-downloader.rb: add version to url

--- Casks/soundcloud-downloader.rb
+++ Casks/soundcloud-downloader.rb
@@ -2,7 +2,7 @@ cask :v1 => 'soundcloud-downloader' do
   version '2.6.1'
   sha256 '88d228874f1fe8910159d92857ed63a6c16cefdfce4dac3da378bc3960fe7b51'
 
-  url 'http://black-burn.ch/scd/downloads/261/ex/b'
+  url "http://black-burn.ch/scd/downloads/#{version.gsub('.','')}/ex/b"
   name 'SoundCloud Downloader'
   appcast 'http://black-burn.ch/applications/scd/updates.php?hwni=1',
           :sha256 => 'eb64aab11d5c3a1c0f9eed554cef2b6866f667f19c2592c30ced3e8dfb0523fa'
