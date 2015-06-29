d9616366d02a08f3a941397ba429ae4b9bae4073
Vítor Galvão <info@vitorgalvao.com>
2015-06-29 16:44:52 +0100

    conductor.rb: easier to update

--- Casks/conductor.rb
+++ Casks/conductor.rb
@@ -2,7 +2,7 @@ cask :v1 => 'conductor' do
   version '1.2.1'
   sha256 '0df00414d64474093aeefd9f7b81fd80bb8a98da2b713164e498df6cd0e60cca'
 
-  url 'https://github.com/keith/conductor/releases/download/1.2.1/Conductor.app.zip'
+  url "https://github.com/keith/conductor/releases/download/#{version}/Conductor.app.zip"
   appcast 'https://github.com/keith/conductor/releases.atom'
   name 'Conductor'
   homepage 'https://github.com/keith/conductor'
