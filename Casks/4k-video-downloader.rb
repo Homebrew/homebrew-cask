d553c75c74ddba16646f244fb76a244ae9439bd1
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 12:20:42 -0600

    4k-video-downloader.rb: update to 3.6

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,6 +1,6 @@
 cask :v1 => '4k-video-downloader' do
-  version '3.5'
-  sha256 :no_check # required as upstream package is updated in-place
+  version '3.6'
+  sha256 'ea2bcdb0be1358d7de11777f134547483dab71519873b16e6c1d393376143269'
 
   url "http://downloads.4kdownload.com/app/4kvideodownloader_#{version}.dmg"
   name '4K Video Downloader'
