538680f732700ad0d34cfbd362d4e85b457a02b7
Dylan Arbour <dylan.arbour@gmail.com>
2015-06-28 21:58:31 -0400

    Upgrade Postico.app to v0.24

--- Casks/postico.rb
+++ Casks/postico.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'postico' do
-  version '0.23'
-  sha256 '0d4527683f13c9565325008329d1cebd717d68a31f8cdaf3d01135399fdb5772'
+  version '0.24'
+  sha256 '9628701be37ede0c41bcd95d75a137a5a95bafee9eb0df17577e83c9ca598487'
 
   url "https://eggerapps.at/postico/download/postico-#{version}.zip"
   name 'Postico'
