2f141d3413a3743e00f545b1ac2a0b9dcc5733f1
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 14:27:55 -0600

    clamxav.rb: add older versions

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -1,10 +1,19 @@
 cask :v1 => 'clamxav' do
-  version '2.8.1'
-  sha256 'c75f73348b94658210263ee3afa07da53e54884a07257aa707fd41226be24490'
+  if MacOS.release <= :tiger
+    version '2.2.1'
+    sha256 'e075b21fe5154f31dcbde86e492531c87c67ab44ad75294d3063f32ae1e58278'
+  elsif MacOS.release <= :leopard
+    version '2.5.1'
+    sha256 '02a7529c74d11724e2d0e8226ac83a0d3cfb599afb354d02f6609632d69d9eb1'
+  else
+    version '2.8.1'
+    sha256 'c75f73348b94658210263ee3afa07da53e54884a07257aa707fd41226be24490'
+
+    appcast 'http://www.clamxav.com/sparkle/profileInfo.php',
+            :sha256 => '01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b'
+  end
 
   url "https://www.clamxav.com/downloads/ClamXav_#{version}.dmg"
-  appcast 'http://www.clamxav.com/sparkle/profileInfo.php',
-          :sha256 => '01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b'
   name 'ClamXav'
   homepage 'https://www.clamxav.com/'
   license :commercial
