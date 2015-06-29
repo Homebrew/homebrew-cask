7a2b60c0b688f3779d26bf65997462336a52380a
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 17:17:40 -0600

    scratch.rb: add versioning to url

--- Casks/scratch.rb
+++ Casks/scratch.rb
@@ -2,7 +2,7 @@ cask :v1 => 'scratch' do
   version '436'
   sha256 '9cfad2ccbf24ef085de21130894107c284637321d5415ae68f7327736b533c49'
 
-  url 'https://scratch.mit.edu/scratchr2/static/sa/Scratch-436.dmg'
+  url "https://scratch.mit.edu/scratchr2/static/sa/Scratch-#{version}.dmg"
   name 'Scratch'
   homepage 'https://scratch.mit.edu/scratch2download/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
