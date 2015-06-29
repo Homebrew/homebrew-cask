5e8a7406d6452828ff1ab16a7308d54767b4d41c
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 14:52:24 -0600

    daisydisk.rb: add version to snow leopard url

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -3,7 +3,7 @@ cask :v1 => 'daisydisk' do
   if MacOS.release == :snow_leopard
     version '2.1.2'
     sha256 'd0a606dee19e524d6fa7b79fd48b3b9865123ca4126fb8805f8e96c317b57b31'
-    url 'http://www.daisydiskapp.com/downloads/DaisyDisk_2_1_2.dmg'
+    url "http://www.daisydiskapp.com/downloads/DaisyDisk_#{version}.dmg"
   else
     version :latest
     sha256 :no_check
