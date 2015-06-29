08751481905a4c5a5fa415b0aaff9290d14e5cec
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-29 16:48:07 +0300

    appserver.rb: upgrade to v1.0.5

--- Casks/appserver.rb
+++ Casks/appserver.rb
@@ -1,15 +1,15 @@
 cask :v1 => 'appserver' do
-  version '1.0.2'
-  sha256 '1a578fdb626b0fab16422d003d09e89cff4f83afddc4095ae283352fe42ad3d8'
+  version '1.0.5-14'
+  sha256 '7c89590446687749a7f4b6624c276675d1faf4b955ce69642e0410d1b6489ae4'
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com/appserver-io/appserver/releases/download/#{version}/appserver-dist_#{version}-56_x86_64.pkg"
+  url "https://github.com/appserver-io/appserver/releases/download/#{version.sub(/-.*/, '')}/appserver-dist_#{version}_x86_64.pkg"
   appcast 'https://github.com/appserver-io/appserver/releases.atom'
   name 'appserver.io'
   homepage 'http://www.appserver.io'
   license :oss
 
-  pkg "appserver-dist_#{version}-56_x86_64.pkg"
+  pkg "appserver-dist_#{version}_x86_64.pkg"
 
   uninstall :pkgutil => [
                          'com.techdivision.appserver-io.runtime',
