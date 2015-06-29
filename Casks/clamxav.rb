f8263c0d6ad34e1fd2a60d0f2ab7928547ab2c55
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 14:17:39 -0600

    clamxav.rb: add license

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -3,11 +3,11 @@ cask :v1 => 'clamxav' do
   sha256 'c75f73348b94658210263ee3afa07da53e54884a07257aa707fd41226be24490'
 
   url "https://www.clamxav.com/downloads/ClamXav_#{version}.dmg"
-  appcast 'https://www.clamxav.com/sparkle/profileInfo.php',
+  appcast 'http://www.clamxav.com/sparkle/profileInfo.php',
           :sha256 => '01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b'
   name 'ClamXav'
   homepage 'https://www.clamxav.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'ClamXav.app'
 
