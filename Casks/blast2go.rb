e1cbc592d28a7c169d1b3aec4d376a5346834fd6
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 15:53:23 -0600

    blast2go.rb: add version to url

--- Casks/blast2go.rb
+++ Casks/blast2go.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'blast2go' do
   version '3.0'
-  sha256 '79ca1eadae292c55c0b9df5b6a97b727536ee835b90d8ca4f764eb9954c54c18'
+  sha256 '20739dea328c65ab35a7db765e1b7e90e625c39639c0076fcacf43166be1270e'
 
-  url 'http://download.blast2go.com/html/software/blast2go/latest/3_0/Blast2GO_macos_3_0.dmg'
+  url "http://download.blast2go.com/html/software/blast2go/latest/#{version.gsub('.','_')}/Blast2GO_macos_#{version.gsub('.','_')}.dmg"
   name 'Blast2GO'
   homepage 'https://www.blast2go.com/'
   license :freemium
