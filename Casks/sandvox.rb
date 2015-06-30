38d266cf7c814cfcbc1e477409c4eec083131aea
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 22:20:52 -0600

    sandvox.rb: remove appcast
    This link simply goes to an empty page.

--- Casks/sandvox.rb
+++ Casks/sandvox.rb
@@ -4,8 +4,6 @@ cask :v1 => 'sandvox' do
 
   url 'http://www.karelia.com/files/8/Sandvox.dmg'
   name 'Sandvox'
-  appcast 'http://launch.karelia.com/appcast.php',
-          :sha256 => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   homepage 'https://www.karelia.com/products/sandvox/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
