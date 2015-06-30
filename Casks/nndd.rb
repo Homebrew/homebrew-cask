c632abbe49ba1dc03d117944df66da9dafe64412
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 20:53:55 -0600

    nndd.rb: add version to url

--- Casks/nndd.rb
+++ Casks/nndd.rb
@@ -2,9 +2,9 @@ cask :v1 => 'nndd' do
   version '2.4.3'
   sha256 '6a73dcad2e73d877ad1503ed1162cae1a1c84f21d1abaa6aaf9b31bb2fbca531'
 
-  url 'http://dl.osdn.jp/nndd/62201/NNDD_v2_4_3.dmg'
+  url "http://dl.osdn.jp/nndd/62201/NNDD_v#{version.gsub('.','_')}.dmg"
   name 'NNDD'
-  homepage 'http://osdn.jp/projects/nndd/'
+  homepage 'https://osdn.jp/projects/nndd/'
   license :x11
 
   preflight do
