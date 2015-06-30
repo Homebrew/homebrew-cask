d4422b1832c479f11ed34d6434e128463f568fb6
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 22:15:55 -0600

    retas-studio.rb: add version to url

--- Casks/retas-studio.rb
+++ Casks/retas-studio.rb
@@ -3,7 +3,7 @@ cask :v1 => 'retas-studio' do
   sha256 '387d299ac77dad4a4116605e492e7c69614c463cde2ce5df439caaa55c9befb5'
 
   # clip-studio.com is the official download host per the vendor homepage
-  url 'http://www.clip-studio.com/clip_site/rental/rental_download/rsrental/dl?f=lib/retasstudio/data/660/RS_660_app.dmg'
+  url "http://www.clip-studio.com/clip_site/rental/rental_download/rsrental/dl?f=lib/retasstudio/data/#{version.gsub('.','')}/RS_#{version.gsub('.','')}_app.dmg"
   name 'RETAS STUDIO'
   homepage 'http://www.retasstudio.net/'
   license :commercial
