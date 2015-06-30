da8bcca80d4c449da08561e3bcfe76c4e4317887
Timo Sand <timo.j.sand@gmail.com>
2015-06-30 10:35:15 +0300

    Updates Star Realms version, checksum, download URL and app name

--- Casks/star-realms.rb
+++ Casks/star-realms.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'star-realms' do
-  version '2.22'
-  sha256 '90d2cb6610a6195a85cb2e1a9bcbbc1da42dce57be9dce5186a4b3f12601529a'
+  version '2.31'
+  sha256 '2d64a2b7a44d4a4f21ae5356a65eec76006f9186c9581e19a02a026683951137'
 
   # cloudfront.net is the official download host per the vendor homepage
-  url "http://d3dyghjp0ukdwm.cloudfront.net/StarRealms_v#{version.gsub('.','_')}.dmg"
+  url "http://downloads.starrealms.com/StarRealms-v#{version.gsub('.','')}.dmg"
   name 'Star Realms'
   homepage 'http://www.starrealms.com/digital-game/'
   license :freemium
 
-  app 'Star Realms.app'
+  app 'StarRealms.app', :target => 'Star Realms.app'
 end
