818a3f0891ed6f984676129330c5c67fa57c6874
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 22:49:19 -0600

    sente.rb: add old version

--- Casks/sente.rb
+++ Casks/sente.rb
@@ -1,6 +1,11 @@
 cask :v1 => 'sente' do
-  version '6.7.8.11870'
-  sha256 'f75bb32db9f0dbfa3d5dbf7639371b5892f6071e740de38e768a203899bb4b5b'
+  if MacOS.release <= :snow_leopard
+    version '6.6.5.11312'
+    sha256 '0a848a2eada32651707e2deec991f06cc068a4c78b23447d38453853d434c2c8'
+  else
+    version '6.7.8.11870'
+    sha256 'f75bb32db9f0dbfa3d5dbf7639371b5892f6071e740de38e768a203899bb4b5b'
+  end
 
   url "https://www.thirdstreetsoftware.com/downloads/Sente-#{version}.zip"
   appcast 'https://www.thirdstreetsoftware.com/rss/Sente65.xml?v=6.6.0',
