74c6a411cadd8c894c28d5eea139766e9ca49ae1
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 22:45:27 -0600

    sente.rb: update to 6.7.8.11870

--- Casks/sente.rb
+++ Casks/sente.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'sente' do
-  version '6.7.6'
-  sha256 'b8c6c2ad8b95810fd7107a606a1ce08013bc217b3d2a13a51599ccb3cd29c631'
+  version '6.7.8.11870'
+  sha256 'f75bb32db9f0dbfa3d5dbf7639371b5892f6071e740de38e768a203899bb4b5b'
 
-  url "https://www.thirdstreetsoftware.com/downloads/Sente-#{version}.11672.zip"
+  url "https://www.thirdstreetsoftware.com/downloads/Sente-#{version}.zip"
   appcast 'https://www.thirdstreetsoftware.com/rss/Sente65.xml?v=6.6.0',
           :sha256 => '2083ee7a01313014c7d600e13018709521fd3f53d767bd31cd8d2caec2120c7f'
   name 'Sente'
