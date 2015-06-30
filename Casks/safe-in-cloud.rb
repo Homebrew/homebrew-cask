f12a8d1b889ae55250e93338f034c1188d746033
scribblemaniac <connor.deptuck@gmail.com>
2015-06-30 11:48:48 -0600

    safe-in-cloud.rb: update url

--- Casks/safe-in-cloud.rb
+++ Casks/safe-in-cloud.rb
@@ -2,7 +2,7 @@ cask :v1 => 'safe-in-cloud' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.safe-in-cloud.com/images/downloads/Safe%20In%20Cloud.dmg'
+  url 'https://www.safe-in-cloud.com/images/downloads/SafeInCloud.app.dmg'
   name 'SafeInCloud'
   name 'SafeInCloud Password Manager'
   homepage 'https://www.safe-in-cloud.com'
