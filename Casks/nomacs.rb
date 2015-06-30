45a65f4d9128824a138202e89c5c61d7f0cb173f
scribblemaniac <connor.deptuck@gmail.com>
2015-06-30 11:14:17 -0600

    nomacs.rb: update to 2.4.5

--- Casks/nomacs.rb
+++ Casks/nomacs.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'nomacs' do
-  version '1.0.0'
-  sha256 '2ec4bfd6390ab7c4b2a929a1e9d14f46f6f168608bf2aee8da67af797f73a54a'
+  version '2.4.5'
+  sha256 '90da9d3d77a659c6ad14ebfb8f19f795f59a7ded5e2262ec610786b4b182106f'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/nomacs/nomacs-#{version}-x86_64.dmg"
+  url "http://downloads.sourceforge.net/sourceforge/nomacs/nomacs-#{version}-x86.zip"
   name 'nomacs'
   homepage 'https://www.nomacs.org/'
   license :gpl
