ff212201e229f6aa7e3e815c4a07088e588eb291
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-30 17:32:06 +0300

    powerkey.rb: upgrade to v1.3

--- Casks/powerkey.rb
+++ Casks/powerkey.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'powerkey' do
-  version '1.2'
-  sha256 '47bfb13458883218cab6106bd948b8516c3a42a733180b49c410f26d4d465ca1'
+  version '1.3'
+  sha256 '5f6246133953a65709252880738de7f69198ed3f23dc61bbfc062887e2313102'
 
   url "https://github.com/pkamb/PowerKey/releases/download/v#{version}/PowerKey#{version}.zip"
   appcast 'https://github.com/pkamb/PowerKey/releases.atom'
