d080ce500822db44946c0b1e4ba595a0483f8ada
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 23:16:23 -0600

    boxofsnoo-fairmount.rb: remove appcast
    I'm pretty sure the appcast is not hosted on example.com.

--- Casks/boxofsnoo-fairmount.rb
+++ Casks/boxofsnoo-fairmount.rb
@@ -3,8 +3,6 @@ cask :v1 => 'boxofsnoo-fairmount' do
   sha256 '0a775f48595ce0c9d0eead76b6d50ab190815cd4ce0e80017f318226e00a5c0e'
 
   url "https://github.com/downloads/pmetzger/Fairmount/Fairmount-#{version}.dmg"
-  appcast 'http://www.example.com/sparkle/fairmount/checkupdate.php',
-          :sha256 => '3587cb776ce0e4e8237f215800b7dffba0f25865cb84550e87ea8bbac838c423'
   name 'Fairmount'
   homepage 'https://github.com/BoxOfSnoo/Fairmount'
   license :gpl
