237be5a1ae4c8bf79cdfa166cdf35d054cfc0c55
scribblemaniac <connor.deptuck@gmail.com>
2015-06-29 23:14:22 -0600

    coda.rb: remove appcast
    Link goes to a blank page

--- Casks/coda.rb
+++ Casks/coda.rb
@@ -3,7 +3,6 @@ cask :v1 => 'coda' do
   sha256 'bb2e9c9f5b5e3a1afb1cf9cb8884afd8c833d5aa7fea7b3b9c7254da1de1fead'
 
   url "https://download.panic.com/coda/Coda%20#{version}.zip"
-  appcast 'http://www.panic.com/updates/update.php'
   name 'Coda'
   homepage 'https://panic.com/Coda/'
   license :commercial
