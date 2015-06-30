fec12cd5ec27fe936febda42fa67d9af9793a71c
scribblemaniac <connor.deptuck@gmail.com>
2015-06-30 13:39:51 -0600

    project-squared.rb: update to cisco-spark
    Project Squared has been rebranded as Cisco Spark.
    I can't find evidence that the zap commands are still necessary,
    but I left them in just in case.

---
+++ Casks/cisco-spark.rb
@@ -0,0 +1,20 @@
+cask :v1 => 'cisco-spark' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://download.ciscospark.com/mac/Spark.dmg'
+  name 'Cisco Spark'
+  homepage 'http://www.webex.com/projectsquared/'
+  license :gratis
+  tags :vendor => 'Cisco Systems'
+
+  depends_on :macos => '>= :mavericks'
+
+  app 'Spark.app'
+
+  zap :delete => [
+                   '~/Library/Preferences/Cisco-Systems.Spark.plist',
+                   '~/Library/Caches/Cisco-Systems.Spark',
+                   '~/Library/Logs/Spark',
+                  ]
+end
