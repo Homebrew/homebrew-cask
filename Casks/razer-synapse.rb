12b2b3dfcaeec1fc82799dc2b56c2cb44d2a1b5b
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 10:50:15 -0600

    razer-synapse.rb: update to 1.39

--- Casks/razer-synapse.rb
+++ Casks/razer-synapse.rb
@@ -1,14 +1,16 @@
 cask :v1 => 'razer-synapse' do
-  version '1.36'
-  sha256 '129b68adea96e4f937c7d276ba97acac1be9fea367e7470a605d1dce1bc8031a'
+  version '1.39'
+  sha256 'ab13c1d13e8df6746eac7502ec52d0cbd29a9a2ddb2f256b2faa6ba35ab299f5'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://razerdrivers.s3.amazonaws.com/drivers/Synapse2/mac/Razer_Synapse_Mac_Driver_v#{version}.dmg"
   name 'Razer Synapse'
   homepage 'http://www.razerzone.com/synapse/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   pkg 'Razer Synapse.pkg'
+
+  depends_on :macos => '>= :lion'
 
   uninstall :script => '/Applications/Utilities/Uninstall Razer Synapse.app/Contents/MacOS/Uninstall Razer Synapse',
             :pkgutil => 'com.razerzone.*'
