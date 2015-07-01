0f88bb78bf6a2e19b48582d163d541c1ccd63b87
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-01 03:58:12 +0300

    colour-contrast-analyser.rb: add version to url

--- Casks/colour-contrast-analyser.rb
+++ Casks/colour-contrast-analyser.rb
@@ -2,7 +2,7 @@ cask :v1 => 'colour-contrast-analyser' do
   version '1.5.13'
   sha256 '2e737dde56179725ccbad1306bfa8db2711bbc910d046cdb4c567e6a5ad86bb4'
 
-  url 'http://files.paciellogroup.com/resources/CCA_1.5.13.dmg'
+  url "http://files.paciellogroup.com/resources/CCA_#{version}.dmg"
   name 'Colour Contrast Analyser'
   homepage 'http://www.paciellogroup.com/resources/contrastanalyser/'
   license :unknown
