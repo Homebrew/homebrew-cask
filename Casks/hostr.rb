1702c5e7691b404e8f9a234e16bbf04bc95a345a
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-01 04:39:49 +0300

    hostr.rb: add version to url

--- Casks/hostr.rb
+++ Casks/hostr.rb
@@ -2,7 +2,7 @@ cask :v1 => 'hostr' do
   version '0.8.0'
   sha256 '89e8b6a4d0168fb05520ff9ac1f69d378dade59a883034aaa549398c039faeed'
 
-  url 'https://hostr.co/apps/mac/Hostr-0.8.0.zip'
+  url "https://hostr.co/apps/mac/Hostr-#{version}.zip"
   name 'Hostr'
   appcast 'https://hostr.co/updaters/mac.xml',
           :sha256 => '74ea25a1e3444e616e4381ec322bd9f6f7dd15b86d08af9b31ccf5e3ff766df7'
