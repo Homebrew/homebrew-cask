32c56ff1a298869578602d2ba1f5efae61a86ca7
Michael <fanquake@gmail.com>
2015-07-01 16:59:51 +0800

    Correct AirVPN SHA256 Fixes #12167

--- Casks/airvpn.rb
+++ Casks/airvpn.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'airvpn' do
   version '2.9'
-  sha256 'd7bc9fc8d2f944a3e63f0ec0a86900952ee96bf28a6cd0293d6f53a510024f4a'
+  sha256 '06c49f4d2b8754e7eec7687445631b36fd3352418e466b2f571dda3a3c37827b'
 
   url "https://airvpn.org/repository/#{version}/airvpn_osx_x64_installer.pkg"
   name 'Air VPN'
