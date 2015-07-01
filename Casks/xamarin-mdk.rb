c694ba6f987c554828af4ce1365cf764d0adc0c1
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 14:05:14 -0600

    xamarin-mdk.rb: improved version handling

--- Casks/xamarin-mdk.rb
+++ Casks/xamarin-mdk.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'xamarin-mdk' do
-  version '3.12.0'
+  version '3.12.0.68'
   sha256 '9591affdb4a342cbba3da0a1d7013592f1c801e2deb7eb70ec9cc1545dda468a'
 
-  url 'https://download.xamarin.com/MonoFrameworkMDK/Macx86/MonoFramework-MDK-3.12.0.68.macos10.xamarin.x86.pkg'
+  url "https://download.xamarin.com/MonoFrameworkMDK/Macx86/MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
   name 'Xamarin Mono MDK'
   appcast 'https://xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
           :sha256 => '79c309d6dbe6f08f1d022c9376a4678cc94f57be084007df90c5a12839b35cdd',
@@ -10,7 +10,7 @@ cask :v1 => 'xamarin-mdk' do
   homepage 'https://xamarin.com/platform'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  pkg 'MonoFramework-MDK-3.12.0.68.macos10.xamarin.x86.pkg'
+  pkg "MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
 
   uninstall :pkgutil => 'com.xamarin.mono-MDK.pkg'
 end
