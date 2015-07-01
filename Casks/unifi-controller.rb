aafa98ff73871ee6d9bf086b70f0b3c281771b02
Maurice Kelly <osv-github@chatswood.org.uk>
2015-07-01 19:07:15 +0100

    Update UniFi Controller to 4.6.6
    This commit updates the version, sha256 and homepage stanzas

--- Casks/unifi-controller.rb
+++ Casks/unifi-controller.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'unifi-controller' do
-  version '3.2.10'
-  sha256 'e5aae4e05bac3d6903f91ac5fb74e7a3940795b549c71880e5dae4d163cddea2'
+  version '4.6.6'
+  sha256 '7ca063dfd368cd27fab2fd6fd60e317a736beafd8add9400b9b98553e8a6f858'
 
   url "https://dl.ubnt.com/unifi/#{version}/UniFi.pkg"
   name 'Unifi Controller'
-  homepage 'https://community.ubnt.com/t5/UniFi-Updates-Blog/bg-p/Blog_UniFi'
+  homepage 'https://www.ubnt.com/download/?platform=unifi'
   license :commercial
 
   pkg 'Unifi.pkg'
