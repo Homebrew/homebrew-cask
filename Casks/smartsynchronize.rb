c4a74d5316cb2c4f9791fa372a02583b1d8c7175
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 11:52:54 -0600

    smartsynchronize.rb: update to 3.4.2

--- Casks/smartsynchronize.rb
+++ Casks/smartsynchronize.rb
@@ -1,14 +1,17 @@
 cask :v1 => 'smartsynchronize' do
-  version '3.4.1'
-  sha256 '85ebf581932f81f109e83b29c1f03bd3d63c8397bbd970a7da4ff1db22ea2bce'
+  version '3.4.2'
+  sha256 'c8dbf08e1dc6f5aa6168d8a5d407da2c3b50f4406270feb37ce00ef34a8ac4ac'
 
-  url "http://www.syntevo.com/download/smartsynchronize/smartsynchronize-macosx-#{version.gsub('.','_')}.dmg"
+  url "http://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-macosx-#{version.gsub('.','_')}.dmg"
   name 'SmartSynchronize'
   homepage 'http://www.syntevo.com/smartsynchronize/'
   license :commercial
 
   app 'SmartSynchronize.app'
   binary 'SmartSynchronize.app/Contents/MacOS/SmartSynchronize'
+
+  depends_on :macos => '>= :lion'
+
   caveats do
     files_in_usr_local
   end
