d32afbd99e2bb1855310720519c3f78f68400f31
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 16:52:13 -0600

    kodi.rb: added 32 bit and updated url

--- Casks/kodi.rb
+++ Casks/kodi.rb
@@ -1,9 +1,14 @@
 cask :v1 => 'kodi' do
   version '14.2'
-  sha256 'b674fd6287315b4cf0ec2a1775a746b51ecbbd782d1c572334cee80ad1ee81ed'
 
-  # xbmc.org is the official download host per the vendor homepage
-  url "http://mirrors.xbmc.org/releases/osx/x86_64/kodi-#{version}-Helix-x86_64.dmg"
+  if Hardware::CPU.is_32_bit?
+    sha256 'c4bc7339a7abc2dc31eb2be6a012b513897d157c9a3b1b55e5d291c351c55a5c'
+    url "http://mirrors.kodi.tv/releases/osx/i386/kodi-#{version}-Helix-i386.dmg"
+  else
+    sha256 'b674fd6287315b4cf0ec2a1775a746b51ecbbd782d1c572334cee80ad1ee81ed'
+    url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-Helix-x86_64.dmg"
+  end
+
   name 'Kodi'
   name 'XBMC' # former
   homepage 'https://kodi.tv/'
