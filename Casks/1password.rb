3a2f04343c45ba961efaa9a3078694a4fb058cf3
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 10:42:02 -0600

    1password.rb: update urls

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -4,21 +4,24 @@ cask :v1 => '1password' do
     version '3.8.22'
     sha256 '3afd75f1bddf791dc7dbc9a7d92ab6eb91ee891407d750cedb7b5aff5fe8bf17'
 
-    url "http://i.agilebits.com/dist/1P/mac/1Password-#{version}.zip"
+    # cloudfront.net is the official download host per the vendor homepage
+    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac/1Password-#{version}.zip"
 
     app '1Password.app'
   elsif MacOS.release <= :mavericks
     version '4.4.3'
     sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
 
-    url "http://i.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
+    # cloudfront.net is the official download host per the vendor homepage
+    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac4/1Password-#{version}.zip"
 
     app "1Password #{version.to_i}.app"
   else
     version '5.3.2'
     sha256 '6051434c77ad48af7b11f2259a91b12ded21ce3357c8db4780718ce83f44b668'
 
-    url "http://i.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
+    # cloudfront.net is the official download host per the vendor homepage
+    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac4/1Password-#{version}.zip"
 
     app "1Password #{version.to_i}.app"
   end
