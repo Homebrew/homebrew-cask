06f1489fa6654f162986bd40bbee3f0171dee345
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 13:59:39 -0600

    xamarin-jdk.rb: improved version handling

--- Casks/xamarin-jdk.rb
+++ Casks/xamarin-jdk.rb
@@ -2,7 +2,7 @@ cask :v1 => 'xamarin-jdk' do
   version '1.7.0_71'
   sha256 '70a18547b529a111c4e5cf133532082e142908819b0d61e273c21dee86fcc87a'
 
-  url 'https://download.xamarin.com/Installer/MonoForAndroid/jdk-7u71-macosx-x64.dmg'
+  url "https://download.xamarin.com/Installer/MonoForAndroid/jdk-#{version.sub(%r{^\d+\.(\d+).*?_(.*)$},'\1u\2')}-macosx-x64.dmg"
   name 'Xamarin Java JDK'
   appcast 'https://xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
           :sha256 => '79c309d6dbe6f08f1d022c9376a4678cc94f57be084007df90c5a12839b35cdd',
@@ -10,7 +10,7 @@ cask :v1 => 'xamarin-jdk' do
   homepage 'https://xamarin.com/platform'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  pkg 'JDK 7 Update 71.pkg'
+  pkg "JDK #{version.split('.')[1]} Update #{version.split('_')[1]}.pkg"
 
-  uninstall :pkgutil => 'com.oracle.jdk7u71'
+  uninstall :pkgutil => "com.oracle.jdk#{version.sub(%r{^\d+\.(\d+).*?_(.*)$},'\1u\2')}"
 end
