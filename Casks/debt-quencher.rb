0ac99ca50b17b3208d65129d0dbb65465188aed4
scribblemaniac <connor.deptuck@gmail.com>
2015-07-02 08:35:07 -0600

    debt-quencher.rb: update to 1.2.3

--- Casks/debt-quencher.rb
+++ Casks/debt-quencher.rb
@@ -1,13 +1,15 @@
 cask :v1 => 'debt-quencher' do
-  version '1.2.2'
+  version '1.2.3'
   sha256 '2083fdb28d3138998a0d6b4b5c4405a18a2e8dd7b71554786386623313c166ad'
 
   url "http://downloads.nothirst.com/DebtQuencher_#{version.sub(%r{^(\d+)\.(\d+).*},'\1.\2')}.zip"
   appcast 'http://nothirst.com/feeds/DebtQuencherAppcast.xml',
-          :sha256 => 'c4cf59373c8956515d82f9ede91e326a770154ec10d637772f48c6aab1149b73'
+          :sha256 => '28091e568c2ebfacd37e9a0e33de29e8e0ac43ad5e596446d4cc4d9173239a89'
   name 'Debt Quencher'
   homepage 'http://nothirst.com/debtquencher/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
+
+  depends_on :macos => '>= :tiger'
 
   app 'Debt Quencher.app'
 end
