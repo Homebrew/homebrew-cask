a1f0b72ff82b8144c531cf3db1c36f5eb09383b2
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 23:15:58 -0600

    navicat-data-modeler.rb: update to 2.0.1 and add dependencies

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -1,13 +1,15 @@
 cask :v1 => 'navicat-data-modeler' do
-  version '1.0.7'
-  sha256 '72cd4c79d81af9c36b7e6caf484c1b265d8080d5a4f882061f8d2942b63b83d8'
+  version '2.0.1'
+  sha256 '59e7cc70285ecb50abbc5ae59343f5b4426aa87da5d9e3e8206711e68d2b519e'
 
   url "http://download.navicat.com/download/modeler0#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_en.dmg"
-  name 'Navicat Data Modeler'
   name 'Navicat Data Modeler'
   homepage 'http://www.navicat.com/products/navicat-data-modeler'
   license :commercial
   tags :vendor => 'Navicat'
+
+  depends_on :macos => '>= :lion'
+  depends_on :arch => :intel
 
   app 'Navicat Data Modeler.app'
 end
