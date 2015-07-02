9ab2faa9a53167af082e894bf317dcb1315e47a9
Sam Doran <github@samdoran.com>
2015-07-01 23:04:14 -0400

    Add CacheWarmer v13

---
+++ Casks/cachewarmer.rb
@@ -0,0 +1,20 @@
+cask :v1 => 'cachewarmer' do
+  version '13'
+  sha256 '97f9d743a41c4a38ea3b2af5c33716e72b02e9e11b0fed3000d0a3c584f104f3'
+
+  url "https://glencode.net/_downloads/CacheWarmer-#{version}.pkg"
+  name 'CacheWarmer'
+  homepage 'https://glencode.net/cachewarmer/'
+  license :freemium
+
+  pkg "CacheWarmer-#{version}.pkg"
+
+  uninstall :pkgutil => 'net.glencode.CacheWarmer',
+            :launchctl => 'net.glencode.CacheWarmer',
+            :delete => [
+                        '/etc/newsyslog.d/net.glencode.CacheWarmer.conf',
+                        '/usr/local/bin/CacheWarmer',
+                        '/Library/LaunchDaemons/net.glencode.CacheWarmer.plist',
+                        '/Library/Application Support/net.glencode.CacheWarmer'
+                       ]
+end
