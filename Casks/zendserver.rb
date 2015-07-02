f5389cb21a80e0c5afddfa105dba12a4d9fe89ad
scribblemaniac <connor.deptuck@gmail.com>
2015-07-02 09:42:34 -0600

    zendserver.rb: update to 8.0.2

--- Casks/zendserver.rb
+++ Casks/zendserver.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'zendserver' do
-  version '6.3.0'
-  sha256 'a1e7f5c4801ae0658508dcbde9e71e6d914cd2546f5d261c7eb7ae31ef4bd31f'
+  version '8.0.2'
+  sha256 '80fa2d34fde1f62ef8374bebea7bb0ccaa85ec0dd5371f45609fc6bf49dc7423'
 
-  url "http://downloads.zend.com/zendserver/#{version}/ZendServer-#{version}-php-5.5.7.dmg"
+  url "http://downloads.zend.com/zendserver/#{version}/ZendServer-#{version}-php-5.6.5.dmg"
   name 'Zend Server'
   homepage 'https://www.zend.com/en/products/server/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
