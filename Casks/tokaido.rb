043dfd0b1d3c1044e962353f8a194b274c7972d8
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-02 22:17:27 +0300

    tokaido.rb: upgrade to v2.2

--- Casks/tokaido.rb
+++ Casks/tokaido.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'tokaido' do
-  version '1.0'
-  sha256 '72752b8e4e9f0e554661d8fb58879a82cac6dc5d19762367c89ca191f497d258'
+  version '2.2'
+  sha256 '150f76ada617ad7588803cd987da8f790695eaa2779e55ca4b3405698ab4388c'
 
   url "https://github.com/tokaido/tokaidoapp/releases/download/v#{version}/Tokaido.zip"
   appcast 'https://github.com/tokaido/tokaidoapp/releases.atom'
