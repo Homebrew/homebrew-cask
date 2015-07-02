cd13b69c20abd80db53b44e361f84332064e580a
scribblemaniac <connor.deptuck@gmail.com>
2015-07-02 00:39:21 -0600

    droplr.rb: update to 4.5.1-65

--- Casks/droplr.rb
+++ Casks/droplr.rb
@@ -1,14 +1,17 @@
 cask :v1 => 'droplr' do
-  version '4.2.2-41'
-  sha256 'aafe5a9d6b3e9318a406d9283cb25d56ac1ed72d3d318361ba85f8e4d8aba8bf'
+  version '4.5.1-65'
+  sha256 'eff5612ff8588345120c50d2bc5ec946c7a74c1e5611cbc7f98ad39e8ac5a8a2'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "http://files.droplr.com.s3.amazonaws.com/apps/mac/Droplr+#{version.sub(%r{.*-},'')}.zip"
   appcast 'https://droplr.com/appcast/appcast.xml',
-          :sha256 => 'a9de5e093038bb95e9334c21a6e4f9e91148535b559756a2824790522df278f8'
+          :sha256 => '8fea3a3e6c84f48d332d1af23af0cdccd7ac5bc8817eb7829e4dea74c41c34fc'
   name 'Droplr'
   homepage 'https://www.droplr.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
+
+  depends_on :macos => '>= :mavericks'
+  depends_on :arch => [:x86_64, :ppc_64]
 
   app 'Droplr.app'
 
