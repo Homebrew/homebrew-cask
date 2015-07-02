62dfe3f7dbded668c1d832ebfccc1ef86dea6396
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 22:48:23 -0600

    timings.rb: update to 2.4.1

--- Casks/timings.rb
+++ Casks/timings.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'timings' do
-  version '1.0.4'
-  sha256 'e7601405312bd6142514aef98154b2ebe8e06a3154c7afa7bcc03edf0090ddff'
+  version '2.4.1'
+  sha256 'b68b3d01f552bf5c4679f740fb2ecbc1011a9e768208f236814131efa5bc3853'
 
-  url "http://mediaatelier.com/Timings/Timings_#{version}.zip"
+  url "http://mediaatelier.com/Timings2/Timings_#{version}.zip"
   appcast 'http://mediaatelier.com/Timings/feed.php',
-          :sha256 => '8802005fc763ac92339326583c2c34d3513766758669d03a1861a886e61ed319'
+          :sha256 => '10f7fc6bbb54db9916650e3d4c37705cb7900230c60f2375d50b1dbad6a61a3d'
   name 'Timings'
   homepage 'http://mediaatelier.com/Timings'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
