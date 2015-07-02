4a7ae2524fe6b483dc27c216b1353cb6081d7acb
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-02 22:00:44 +0300

    proxpn.rb: upgrade to v4.0.6

--- Casks/proxpn.rb
+++ Casks/proxpn.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'proxpn' do
-  version '4.0.3'
-  sha256 'ef4f436a6ce68f533f25ad9bf9c2b2616875f65d05abf9b0b884983f44ed7647'
+  version '4.0.6'
+  sha256 '32d73233fb8469781e8479ecdd5af87b2ef1694c03e02e4c42ab11f3d2e7bae6'
 
   url "https://www.proxpn.biz/updater/proXPN-MacOSX-10.5-#{version}.dmg"
   appcast 'https://www.proxpn.biz/updater/appcast.rss'
