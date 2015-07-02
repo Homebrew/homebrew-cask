9fb62f8c968afd3f4cdf772425c7e1e3adccd2fd
scribblemaniac <connor.deptuck@gmail.com>
2015-07-02 08:28:52 -0600

    smart-scroll.rb: update to 4.0.4

--- Casks/smart-scroll.rb
+++ Casks/smart-scroll.rb
@@ -1,11 +1,13 @@
 cask :v1 => 'smart-scroll' do
-  version :latest
-  sha256 :no_check
+  version '4.0.4'
+  sha256 '19b5fd9a5a35510e6f5b5b29bd97931f29d6d9e7a6c3bfe530c6f5378f3caf95'
 
   url 'http://www.marcmoini.com/SmartScroll.zip'
   name 'Smart Scroll'
   homepage 'http://www.marcmoini.com/sx_en.html'
   license :commercial
 
-  prefpane 'Smart Scroll 4.0.prefPane'
+  depends_on :macos => '>= :snow_leopard'
+
+  prefpane "Smart Scroll #{version}.prefPane"
 end
