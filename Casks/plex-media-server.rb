272f1d34780b8d59a6e9091b356c2f243ee1027a
scribblemaniac <connor.deptuck@gmail.com>
2015-07-01 23:08:55 -0600

    plex-media-server.rb: update to 0.9.12.4.1192-9a47d21

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,11 +1,13 @@
 cask :v1 => 'plex-media-server' do
-  version '0.9.11.4.739-a4e710f'
-  sha256 '69066c232cfbf4b8cd92881e954e8486042c0838bfd69c27b600d89d023abf96'
+  version '0.9.12.4.1192-9a47d21'
+  sha256 '984bfae6ec9aea2cbe24229c968225389c861124f8d3d09ab5bd5c6c43d71829'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   name 'Plex Media Server'
   homepage 'https://plex.tv/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
+
+  depends_on :macos => '>= :snow_leopard'
 
   app 'Plex Media Server.app'
 end
