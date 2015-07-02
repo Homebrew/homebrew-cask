2a6e8dea5d8ad3928fc81e742fb58d9e8f872396
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-02 21:39:39 +0300

    razer-synapse.rb: upgrade to v1.40

--- Casks/razer-synapse.rb
+++ Casks/razer-synapse.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'razer-synapse' do
-  version '1.39'
-  sha256 'ab13c1d13e8df6746eac7502ec52d0cbd29a9a2ddb2f256b2faa6ba35ab299f5'
+  version '1.40'
+  sha256 '921591dddd4d1496c51586357fb599518feaebd0976ec77107a9539f52189b7d'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://razerdrivers.s3.amazonaws.com/drivers/Synapse2/mac/Razer_Synapse_Mac_Driver_v#{version}.dmg"
