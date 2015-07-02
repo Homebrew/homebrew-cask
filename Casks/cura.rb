c5e67341213b9ff60017a3762661955d7712d9d0
Michael <fanquake@gmail.com>
2015-07-03 02:39:44 +0800

    Cura 15.06.01 Fixes #12263

--- Casks/cura.rb
+++ Casks/cura.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'cura' do
-  version '15.02.1'
-  sha256 'a8d174a6921281704b408c74fd7414b3b2a67ddd2787707db71c02c36a004088'
+  version '15.06.01'
+  sha256 'b26d759d2905fffdba37f6fbc1310d08d060a27245add2a91cfd1d1db18ef954'
 
-  url "https://ultimaker.com/en/software/download/23/Cura-#{version}-MacOS.dmg"
+  url "http://software.ultimaker.com/15.06/Cura-#{version}-Darwin.dmg"
   name 'Cura'
   homepage 'https://ultimaker.com/en/products/software'
   license :oss
