89e9141dc4f06a38a6479e551cc703078534d6ad
scribblemaniac <connor.deptuck@gmail.com>
2015-07-02 01:01:40 -0600

    videomonkey.rb: update to 0.17

--- Casks/videomonkey.rb
+++ Casks/videomonkey.rb
@@ -1,9 +1,10 @@
 cask :v1 => 'videomonkey' do
-  version '0.16'
-  sha256 'ab09a3bb98a78d32ae48c55b6572e4d1e10ead0f26d5ba895a9a2ee5c105088c'
+  version '0.17'
+  sha256 'f0f70793f13f6e52f4115d292d3d425749a8d70b36f47055640b4ce1f519df6b'
 
   url "http://videomonkey.org/download/videomonkey-#{version}.zip"
-  appcast 'http://videomonkey.org/releases/videomonkeycast.xml'
+  appcast 'http://videomonkey.org/releases/videomonkeycast.xml',
+          :sha256 => '76c09866931509d5fbe0de5c46fa1466520a43885a32f82e72dd16173af54daa'
   name 'Video Monkey'
   homepage 'http://videomonkey.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
