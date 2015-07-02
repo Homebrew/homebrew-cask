6f1453c172cf455f242a3c985573fbf161289334
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-02 21:46:18 +0300

    hammerspoon.rb: upgrade to v0.9.33

--- Casks/hammerspoon.rb
+++ Casks/hammerspoon.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'hammerspoon' do
-  version '0.9.32'
-  sha256 '9b30fcf132a636a5b7810069a3c77dcee81c19f61e88e22219ea6fb680d764d7'
+  version '0.9.33'
+  sha256 'a38adf163872c9a9d540f9f0738a0ed230f795bc1c1d967569e56a57f4261e82'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/Hammerspoon/hammerspoon/releases/download/#{version}/Hammerspoon-#{version}.zip"
