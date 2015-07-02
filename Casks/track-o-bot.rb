81d7afbbf0159930a6eae8582a2178558c463aec
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-02 22:19:39 +0300

    track-o-bot.rb: upgrade to v0.4.7

--- Casks/track-o-bot.rb
+++ Casks/track-o-bot.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'track-o-bot' do
-  version '0.4.2'
-  sha256 '3746f662cda62047c8b9e870997bfc8d4f04040b72e1573177c4da2cfc5e8cb3'
+  version '0.4.7'
+  sha256 '36603bffbdff2017ff381ad49b1ca3a7640c4921da0380e3a6ffe7e6f8440fee'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/stevschmid/track-o-bot/releases/download/#{version}/Track-o-Bot_#{version}.dmg"
