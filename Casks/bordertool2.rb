b5e3f2d645dd53961684a7f90828f842a30a0451
Tim Miller <neoparasak@gmail.com>
2015-07-03 12:56:00 -0400

    Added cask for bordertool2

---
+++ Casks/bordertool2.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'bordertool2' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://xvi.rpc1.org/BorderTool%202.zip'
+  name 'BorderTool 2'
+  homepage 'http://xvi.rpc1.org/'
+  license :gratis
+
+  app 'BorderTool 2.app'
+end
