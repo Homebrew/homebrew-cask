2c301bbca553c1b45e32a0f1dd0e0f1e12ac1fb8
Jussi Tuominen <Jussi@COLDwithin.local>
2015-07-03 16:32:23 +0300

    Add Unsplash Wallpaper.app Latest

---
+++ Casks/unsplash-wallpaper.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'unsplash-wallpaper' do
+  version :latest
+  sha256 :no_check
+
+  # devmate.com is the official download host per the vendor homepage
+  url 'http://dl.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/UnsplashWallpaper.dmg'
+  name 'Unsplash Wallpaper'
+  homepage 'http://leonspok.tumblr.com/unsplash-wallpaper'
+  license :gratis
+
+  app 'Unsplash Wallpaper.app'
+end
