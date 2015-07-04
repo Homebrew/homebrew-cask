6fb60683124edd419f2ab296cde9992c0d319c46
Peter Lewis <peter@peterlew.is>
2015-07-04 08:55:20 +0100

    Removed stray sha256 that was left in
    Removed stray sha256 that was left in. Also changed tab level of comment to match the line it is commenting.

--- Casks/bordertool2.rb
+++ Casks/bordertool2.rb
@@ -1,6 +1,5 @@
 cask :v1 => 'bordertool2' do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  b5e3f2d645dd53961684a7f90828f842a30a0451
   version :latest
   sha256 :no_check
 
