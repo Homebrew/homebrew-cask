18841c9759940d12640fdb6b349ee9166365bfca
scribblemaniac <connor.deptuck@gmail.com>
2015-07-05 11:19:49 -0600

    flip4mac.rb: update to 3.3.6

--- Casks/flip4mac.rb
+++ Casks/flip4mac.rb
@@ -1,11 +1,13 @@
 cask :v1 => 'flip4mac' do
-  version '3.3.3'
-  sha256 '2ef87c50b2f767231f1b56e2e3d6486b2b6bd963a9c71711fcd84e3abd92bad6'
+  version '3.3.6'
+  sha256 'd766ab49f22d28accd75b345b0bbd465b94ce3e90eef759a1c9b0276f280d7cb'
 
-  url "http://www.telestream.net/download-files/flip4mac/#{version.sub(%r{\.\d+$},'').sub('.','-')}/Flip4Mac-#{version}.dmg"
+  url "http://www.telestream.net/download-files/flip4mac/#{version.sub(%r{^(\d+)\.(\d+).*$},'\1-\2')}/Flip4Mac-#{version}.dmg"
   name 'Flip4Mac'
   homepage 'http://www.telestream.net/flip4mac/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
+
+  depends_on :macos => '>= :snow_leopard'
 
   pkg 'Flip4Mac.pkg'
 
