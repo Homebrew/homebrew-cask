48a23273f2daec57369ce4e6d9aca75b18a534e7
Jaden Choi <jaden@allwin.bid>
2015-07-05 19:48:12 +0900

    Add EdgeView.app v0.970

---
+++ Casks/edgeview.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'edgeview' do
+  version '0.970'
+  sha256 'd32e61cd7b309de1be168e32ed8c0e28cd561a8ec4745c5e9129e00ef8e992c2'
+
+  url 'https://dl.dropboxusercontent.com/u/168552/edgeViewUpdate/files/EdgeView_970.zip'
+  name 'EdgeView'
+  homepage 'https://edgehigh.wordpress.com'
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'EdgeView.app'
+end
