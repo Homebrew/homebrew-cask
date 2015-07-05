e0ec0fa61090f1479f261b4932bc317d03167f6b
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-05 18:02:44 +0300

    hsang.rb: upgrade to v1.5.1

--- Casks/hsang.rb
+++ Casks/hsang.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'hsang' do
-  version '1.3.0'
-  sha256 '2471b4593b61afbc06d09011d556e230f90a62fb55bcffda0e30d2dbae329273'
+  version '1.5.1'
+  sha256 'd04ad25278fb1305df0261e7b826d26ab2ef844c9326c28db7f49317acfe60e8'
 
   # netease.com is the official download host per the vendor homepage
   url "http://nie.gdl.netease.com/lushi/HSAng_#{version}.dmg"
   name 'HSAng'
   appcast 'http://lushi.163.com/mac.xml',
-          :sha256 => '0d4e71b5376ec785f85e9abe19fc60172280dd9030ea576db969af5014b6f7ce'
+          :sha256 => '8cbbe867641dffe7b41babc9f39a0bc06fac2e608176c896a09c7e1fcb2bbd95'
   homepage 'http://lushi.163.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
