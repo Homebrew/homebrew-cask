18440abe64c96702c227979ae41b860731f75d6d
Paul Breslin <paul.breslin@outlook.com>
2015-07-06 00:48:31 +0100

    expressvpn.rb: update to 4.0.4

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'expressvpn' do
-  version '4.0.1.288'
-  sha256 '720f03e3c64e3d84a18f92e5055eadb7df6dd75feee831330be50802a836bc16'
+  version '4.0.4.305'
+  sha256 'acb898d8fc1688cc1bbb6f2f1a76f6b3f985543649c25bf1ae16c3e786bf857c'
 
-  url "https://www.expressvpn.com/4568726248/mac/expressvpn-install_v#{version}.pkg"
+  # cloudfront.net is the official download host per the vendor homepage
+  url "https://d11cv8afjim9ws.cloudfront.net/clients/mac/expressvpn-install_v#{version}.pkg"
   name 'ExpressVPN'
   homepage 'https://www.expressvpn.com'
   license :commercial
@@ -12,5 +13,5 @@ cask :v1 => 'expressvpn' do
   uninstall :pkgutil => 'com.expressvpn.ExpressVPN',
             :quit => 'com.expressvpn.ExpressVPN'
 
-  depends_on :macos => '>= :yosemite'
+  depends_on :macos => '>= 10.8'
 end
