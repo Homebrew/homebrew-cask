ccdebd7820c4d8146835dc74a4e7d603f372d629
scribblemaniac <connor.deptuck@gmail.com>
2015-07-05 14:06:43 -0600

    flexiglass.rb: switch to versioned url

--- Casks/flexiglass.rb
+++ Casks/flexiglass.rb
@@ -1,12 +1,16 @@
 cask :v1 => 'flexiglass' do
-  version :latest
-  sha256 :no_check
+  version '1.6-12312'
+  sha256 '1fbe8aeac30254487cf69ccc556f8a214acfdcc340edc885697e4d0618a95641'
 
-  url 'http://downloads.nulana.com/flexiglass/Flexiglass.dmg'
-  appcast 'http://downloads.nulana.com/flexiglass/appcast.xml'
+  url "http://downloads.nulana.com/flexiglass/Flexiglass-#{version}.zip"
+  appcast 'http://downloads.nulana.com/flexiglass/appcast.xml',
+          :sha256 => 'f70568526030303c5d319daa0823eb80b24715e9abbcfb7952ad3b716fd6276b'
   name 'Flexiglass'
   homepage 'http://nulana.com/flexiglass/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
+
+  depends_on :macos => '>= :snow_leopard'
+  depends_on :arch => :intel
 
   app 'Flexiglass.app'
 end
