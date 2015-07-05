d9e78ea261071df66d5f4d207dda0b891fdbcdf5
scribblemaniac <connor.deptuck@gmail.com>
2015-07-05 12:02:32 -0600

    coda.rb: update to 2.5.11

--- Casks/coda.rb
+++ Casks/coda.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'coda' do
-  version '2.5.10'
-  sha256 'bb2e9c9f5b5e3a1afb1cf9cb8884afd8c833d5aa7fea7b3b9c7254da1de1fead'
+  version '2.5.11'
+  sha256 '7a1f723a661fe4fa0c6d3cfd13615492777167602ffea03eab34eaee0cbe9a5e'
 
   url "https://download.panic.com/coda/Coda%20#{version}.zip"
   name 'Coda'
@@ -18,6 +18,8 @@ cask :v1 => 'coda' do
     '~/Library/Preferences/com.panic.Coda2.LSSharedFileList.plist.lockfile',
     '~/Library/Saved Application State/com.panic.Coda2.savedState'
   ]
+
+  depends_on :macos => '>= :lion'
 
   app 'Coda 2.app'
 end
