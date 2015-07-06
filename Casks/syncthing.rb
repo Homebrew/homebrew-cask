9c9a23a7fea5e6f1d8a2b5613e369c4a1732c229
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-06 06:38:41 +0300

    syncthing.rb: upgrade to v0.11.13

--- Casks/syncthing.rb
+++ Casks/syncthing.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'syncthing' do
-  version '0.11.11'
+  version '0.11.13'
   appcast 'https://github.com/syncthing/syncthing/releases.atom'
   name 'Syncthing'
   homepage 'https://syncthing.net'
@@ -7,11 +7,11 @@ cask :v1 => 'syncthing' do
 
   if Hardware::CPU.is_32_bit?
     url "https://github.com/syncthing/syncthing/releases/download/v#{version}/syncthing-macosx-386-v#{version}.tar.gz"
-    sha256 '28a594aa2248e5750f0ee163a5a02b55c1c3d8eb58230b2c0ae36cec396b4432'
+    sha256 'd67a5329084cf1325a7d296668ef2fc9a47442c0398d54fed3a64e0ef6ff30d4'
     binary "syncthing-macosx-386-v#{version}/syncthing"
   else
     url "https://github.com/syncthing/syncthing/releases/download/v#{version}/syncthing-macosx-amd64-v#{version}.tar.gz"
-    sha256 '5e3ccbdba35a8ccbdc926f309fd84843a45e5edd65636693d1bee0017f19d7b8'
+    sha256 '2ddf74aff5f3d68a903ac52459ece73dc5161f49e5df675f47b9c7b1e12a7e5e'
     binary "syncthing-macosx-amd64-v#{version}/syncthing"
   end
 
