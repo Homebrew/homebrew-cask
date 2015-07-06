ec384b960e6e6a2f56a4dcefdce03ca3df08bc2c
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-07-06 15:45:46 +0500

    dropdmg.rb: convert tabs to spaces

--- Casks/dropdmg.rb
+++ Casks/dropdmg.rb
@@ -1,19 +1,19 @@
 cask :v1 => 'dropdmg' do
-	version '3.2.7'
-	app 'DropDMG.app'
-	sha256 '5f27c67364cef6569633b98e250f73119016249ec7ec06908c117a008dc6c55f'
+  version '3.2.7'
+  app 'DropDMG.app'
+  sha256 '5f27c67364cef6569633b98e250f73119016249ec7ec06908c117a008dc6c55f'
 
-	url 'https://c-command.com/downloads/DropDMG-3.2.7.dmg'
-	name 'DropDMG'
-	homepage 'https://c-command.com/dropdmg/'
+  url 'https://c-command.com/downloads/DropDMG-3.2.7.dmg'
+  name 'DropDMG'
+  homepage 'https://c-command.com/dropdmg/'
 
-	license :commercial
-	depends_on :macos => '>= :snow_leopard'
-	depends_on :arch => :intel
+  license :commercial
+  depends_on :macos => '>= :snow_leopard'
+  depends_on :arch => :intel
 
-	zap :delete => [
-		'~/Library/Automator/DropDMG.action',
-		'~/Library/Automator/Expand Disk Image.action',
-		'~Library/Preferences/com.c-command.DropDMG.plist',
-	]
+  zap :delete => [
+    '~/Library/Automator/DropDMG.action',
+    '~/Library/Automator/Expand Disk Image.action',
+    '~Library/Preferences/com.c-command.DropDMG.plist',
+  ]
 end
