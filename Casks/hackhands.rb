93beac61c6e2b66d0d023d4e62f9f179ad487bcb
Vítor Galvão <info@vitorgalvao.com>
2015-07-06 21:50:01 +0100

    updated hackhands (1.3.2.76)

--- Casks/hackhands.rb
+++ Casks/hackhands.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'hackhands' do
-  version '0.1.1'
-  sha256 'f3d5b67a79d963f88ddc03d2e5947a19a616a670442b2b1389159bd3960dcd8d'
+  version '1.3.2.76'
+  sha256 'ba2244b15274158b08a4680dac512761c3c2b8ab2127a4e465ead10bb48fe1b9'
 
-  url 'http://desktop.hackhands.com.s3-website-us-west-1.amazonaws.com/v0.1.1/osx/HackHands.zip'
+  # amazonaws.com is the official download host per the vendor homepage
+  url "http://desktop.hackhands.com.s3-website-us-west-1.amazonaws.com/builds/#{version}/osx/HackHands.dmg"
   name 'HackHands'
   homepage 'https://hackhands.com/desktop'
   license :gratis
