ceb59ea8305fe00002490b50ced9a68ab844c1c5
scribblemaniac <connor.deptuck@gmail.com>
2015-07-06 21:13:03 -0600

    clipy.rb: change to offical appcast

--- Casks/clipy.rb
+++ Casks/clipy.rb
@@ -3,10 +3,14 @@ cask :v1 => 'clipy' do
   sha256 '8ec7ab4e1a04034aa612f014579c7e73d2cbca5715fe3d057daaa84960a291eb'
 
   url "https://github.com/Clipy/Clipy/releases/download/#{version}/Clipy_#{version}.dmg"
-  appcast 'https://github.com/Clipy/Clipy/releases.atom'
+  appcast 'http://clipy-app.com/appcast.xml',
+          :sha256 => 'ad739513da663612cdcfe7c9ab58c3d72e958aabeb32873fb50ba4fd6aef702d',
+          :format => :sparkle
   name 'Clipy'
   homepage 'http://clipy-app.com/'
   license :mit
+
+  depends_on :macos => '>= :mavericks'
 
   app 'Clipy.app'
 end
