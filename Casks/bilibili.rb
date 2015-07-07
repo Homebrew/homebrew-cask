790c0e4816976954949baed956e4c1b18b0f05e9
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-07 05:11:02 +0300

    bilibili.rb: upgrade to v1.99

--- Casks/bilibili.rb
+++ Casks/bilibili.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'bilibili' do
-  version '1.98'
-  sha256 '06649c8a2a6f2949da0a32e5539d5687ece0e647f572481e2d11bf7fcdef182b'
+  version '1.99'
+  sha256 '251799c370c654098599a9b0ed894fd5ce2a55ddb4a2529283f4c6ffa3867e5a'
 
   url "https://github.com/typcn/bilibili-mac-client/releases/download/#{version}/Bilibili.dmg.zip"
   name 'Bilibili'
   appcast 'http://app.eqoe.cn/updates/bilimac.xml',
-          :sha256 => '607bcbe16cea248902c3a0fe9e549411cc7a6ea342d633bec9b8043f255d1e30'
+          :sha256 => '1513b3f83c9a4d55e8590383048a141b35de6c259744bde25944f59bc12811bf'
   homepage 'https://github.com/typcn/bilibili-mac-client/'
   license :gpl
 
