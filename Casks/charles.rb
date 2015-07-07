759787bc33d1bff0763ebcff033b0fd6e1857f07
Mike English <mike.english@atomicobject.com>
2015-07-07 08:40:45 -0400

    Upgrade Charles.app to v3.10.2

--- Casks/charles.rb
+++ Casks/charles.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'charles' do
-  version '3.10.1'
-  sha256 '5d94f04d4936dd1dd9293f406457f13a825422dc28c5146f19581de2f8e76c9d'
+  version '3.10.2'
+  sha256 'd68be46d7dc9654b4b3b094a2f451226376d7bf3c5d401aecba082b27e0b8b6a'
 
   url "http://www.charlesproxy.com/assets/release/#{version}/charles-proxy-#{version}.dmg"
   name 'Charles'
