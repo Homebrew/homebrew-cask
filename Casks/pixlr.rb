422dcfa06e1bdd79d37072aed3d9295ae21eb070
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-07 05:27:44 +0300

    pixlr.rb: upgrade to v1.0.3

--- Casks/pixlr.rb
+++ Casks/pixlr.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'pixlr' do
-  version '1.0.1_26'
-  sha256 'a68fb58bd8f10eaa31bafcdee1c6004fcae12f750830cf8a7b7399bf23ba8087'
+  version '1.0.3_323'
+  sha256 '625fe3f8b90101bf28edecf04417061f1f9641277d5ab0c3d8ddbdab23cd836f'
 
-  url "https://cdn2.pixlr.com/mac/Autodesk_Pixlr_ver#{version}_Mac_Installer.dmg"
+  url "https://cdn2.pixlr.com/mac/Autodesk_Pixlr_v#{version}_Mac_Installer.dmg"
   name 'Pixlr'
   appcast 'https://updatefeed.pixlr.com/pixlr-for-mac-appfeed.xml',
           :sha256 => '3748c8735f5f9441a1f12a170296628822372604c41feb41193156792f9e9512'
