b3b605e88a904b659b734ea0cf1538e5c95d5ea1
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-07 12:09:31 +0300

    audiomate.rb: add appcast

--- Casks/audiomate.rb
+++ Casks/audiomate.rb
@@ -4,6 +4,8 @@ cask :v1 => 'audiomate' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/downloads.9labs.io/audiomate/#{version.sub(%r{_.*},'')}/#{version.sub(%r{.*_},'')}/AudioMate-v#{version.sub(%r{_.*},'')}.dmg"
+  appcast 'http://backend.9labs.io/appcast/audiomate',
+          :sha256 => '9e431475ead844f83a2d9dbd7d5e975404a0a6fa9a5c80a769d08bf740654516'
   name 'AudioMate'
   homepage 'http://audiomateapp.com/'
   license :mit
