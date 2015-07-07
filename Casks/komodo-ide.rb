0bee93b7c1dee6de831a5c2d89542cf48b52548b
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-08 02:02:25 +0300

    komodo-ide.rb: upgrade to v9.1.0-87337

--- Casks/komodo-ide.rb
+++ Casks/komodo-ide.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'komodo-ide' do
-  version '9.0.1-87208'
-  sha256 'b140a291efad0b3a0451cb00cae699bd8485a785c3a9f79ad596b713e58a76bb'
+  version '9.1.0-87337'
+  sha256 'd39737cc24069723130296f68a401ab39e88f989fbdec659b9dafb3c1fef563b'
 
   # activestate.com is the official download host per the vendor homepage
   url "http://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*},'')}/Komodo-IDE-#{version}-macosx-x86_64.dmg"
