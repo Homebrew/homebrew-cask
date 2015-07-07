7e6b8570a45a3e1d46437989d98fa22a101a1501
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-07 12:33:52 +0300

    boom-recorder.rb: upgrade to v8.5.0

--- Casks/boom-recorder.rb
+++ Casks/boom-recorder.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'boom-recorder' do
-  version '8.4.0'
-  sha256 '6090543358b142d4d14197ef810f9e3641e10c6e1309ac5f8c18c486ab1258a7'
+  version '8.5.0'
+  sha256 'acb6540a9722702172f8a2c5a379467def479dc44a6d8e892c90dade12efe408'
 
   url "http://www.vosgames.nl/media/downloads/Boom%20Recorder-#{version}.dmg"
   name 'Boom Recorder'
