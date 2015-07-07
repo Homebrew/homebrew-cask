cd83662ba91d30b3d3ef95e02ec420b4f683bf40
jiasui <jsfaint@gmail.com>
2015-07-07 17:42:32 +0800

    Update gnucash to v2.6.7-1

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'gnucash' do
-  version '2.6.6-5'
-  sha256 'c0ac1ea91e1a71519d8a4664190b8e4c7b0af60cb68a610f7ce7de30e3feaed8'
+  version '2.6.7-1'
+  sha256 'd0ed3339ac2f483219aab0b2c3317116b8ddb3a36e86722295899f2eeabbe74e'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-#{version}.dmg"
