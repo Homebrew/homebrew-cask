c0928c12250bcef8cd7bb7afc930458f8e4104f3
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-07 05:32:17 +0300

    transporter-desktop.rb: upgrade to v3.1.36_19350

--- Casks/transporter-desktop.rb
+++ Casks/transporter-desktop.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'transporter-desktop' do
-  version '3.1.31_19160'
-  sha256 '5cf2da62934c8837e8c94e3574a63f7d30ccea935235902cac344bf26cdb8977'
+  version '3.1.36_19350'
+  sha256 '9e7ca3136f323c473ac548ea9908c8769c927734749d9bcaca5332ce7f000400'
 
   # connecteddata.com is the official download host per the vendor homepage
   url "https://secure.connecteddata.com/mac/2.5/software/Transporter_Desktop_#{version}.dmg"
   name 'Transporter Desktop'
   appcast 'http://appsoftware.connecteddata.com/mac/2.5/Appcast.xml',
-          :sha256 => '7b36fe42eab19068fe4d1e8bbb0efd4d02d7be8d1440d367bf2118c11ccbff3d'
+          :sha256 => 'e3ab545795691139e7d6062c747a707c6e1db3a1c6a0ab11c4de8caed249e23d'
   homepage 'http://www.filetransporter.com/'
   license :commercial
 
