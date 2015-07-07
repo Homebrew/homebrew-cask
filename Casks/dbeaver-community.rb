0e70b30add48b5d5ca47bb5ca45c34812eb583c4
Alberto A. Colon Viera (Albertico) <aacv@alberti.co>
2015-07-07 13:54:56 -0400

    Upgrade DBeaver-Community to v3.4.2

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'dbeaver-community' do
-  version '3.4.1'
+  version '3.4.2'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '78bf312ff90ae9d92e5b2530a132d30e8c9545211c9f451e2ac97b943cfc3c71'
+    sha256 'd6524f5b6ec75a919876b56d13503b3c14d985d807241161bb4d535b15f4ce26'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-macosx.cocoa.x86.zip"
   else
-    sha256 '2ead83a54137a2288bb48fd7261a976fffec24814e747e4bc8f6fc8b738a50c6'
+    sha256 '032bdf9785372c6538bdca3adce70cab0eac3d4a419c9f708466f2d156776723'
     url "http://dbeaver.jkiss.org/files/dbeaver-#{version}-macosx.cocoa.x86_64.zip"
   end
 
