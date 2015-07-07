f4a6be3a826b29270c71cc9b7a8e9cfc4f80d769
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-08 02:07:04 +0300

    launchbar.rb: upgrade to v6.4.1

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -5,8 +5,8 @@ cask :v1 => 'launchbar' do
     sha256 '22a1ec0c10de940e5efbcccd18b8b048d95fb7c63213a01c7976a76d6be69a4d'
     url "https://www.obdev.at/downloads/launchbar/legacy/LaunchBar-#{version}.dmg"
   else
-    version '6.4'
-    sha256 '320c177839bad91f3dda90bd2fb4476bb31c5a6d723437505d776456dd8688cc'
+    version '6.4.1'
+    sha256 '9cc78874db65548cb642361fae997fb652623d0ab8fdd0128ce2e71f438d47c4'
     url "https://www.obdev.at/downloads/launchbar/LaunchBar-#{version}.dmg"
   end
 
