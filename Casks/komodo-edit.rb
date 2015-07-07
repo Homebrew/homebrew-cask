db7d817a4242841023aa7a98c21c2bd6481dd004
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-08 01:47:43 +0300

    komodo-edit.rb: upgrade to v9.1.0-15798

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'komodo-edit' do
-  version '9.0.1-15745'
-  sha256 '39dc064cead52b3de0cab8f499867d26e1e8feeeca91488c00cd72ceb9d31089'
+  version '9.1.0-15798'
+  sha256 '8b5e549813f9990beb77430c92f11ac246e19edb63e4bfce51e1609dceac76b7'
 
   # activestate.com is the official download host per the vendor homepage
   url "https://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*},'')}/Komodo-Edit-#{version}-macosx-x86_64.dmg"
