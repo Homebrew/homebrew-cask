a04c273799ae092f87a5342219da010db2a3c518
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-08 19:11:28 +0300

    qcad.rb: upgrade to v3.9.4

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qcad' do
-  version '3.9.1'
-  sha256 'f96e063f6098c46f9a25a7d4f280c906e10133aebf291be663d5e5f559fab61c'
+  version '3.9.4'
+  sha256 '467933277d1df679bb2c2d0f0a0e5d222ad30a300a032d453de0683c5050f183'
 
   url "http://www.qcad.org/archives/qcad/qcad-#{version}-osx-10.6-10.10.dmg"
   name 'QCAD'
