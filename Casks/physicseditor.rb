4775f10acb92ba7a1c749ce97d34e8c6419a495b
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-08 09:55:27 +0300

    physicseditor.rb: upgrade to v1.0.11

--- Casks/physicseditor.rb
+++ Casks/physicseditor.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'physicseditor' do
-  version '1.0.10'
-  sha256 '14ddcddafe6972e257476108be315a459f73694330c510921274322310d68876'
+  version '1.0.11'
+  sha256 '8271a9786d43fd652932ccb19f5642dfec8c698201527ebc784b0c37ce53d1ee'
 
   url "https://www.codeandweb.com/download/physicseditor/#{version}/PhysicsEditor-#{version}-uni.dmg"
   name 'PhysicsEditor'
