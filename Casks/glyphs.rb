71f0c1a0f836809303835dfff9af0229217a2744
scribblemaniac <connor.deptuck@gmail.com>
2015-07-07 18:14:24 -0600

    glyphs.rb: add appcast

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -5,6 +5,9 @@ cask :v1 => 'glyphs' do
   url "https://updates.glyphsapp.com/Glyphs#{version}.zip"
   name 'Glyphs'
   homepage 'https://www.glyphsapp.com'
+  appcast "http://updates.glyphsapp.com/appcast#{version.to_i}.xml",
+          :sha256 => '044bc9b5748222115bc8fb8c783b141958190e3377730dd69d004fbdff3eee9b',
+          :format => :sparkle
   license :commercial
 
   app 'Glyphs.app'
