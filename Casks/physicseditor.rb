bc8c6cb3f9771c9f8f66546d7dc0d3e2baeb8ce6
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-08 09:56:56 +0300

    physicseditor.rb: add appcast

--- Casks/physicseditor.rb
+++ Casks/physicseditor.rb
@@ -3,6 +3,8 @@ cask :v1 => 'physicseditor' do
   sha256 '8271a9786d43fd652932ccb19f5642dfec8c698201527ebc784b0c37ce53d1ee'
 
   url "https://www.codeandweb.com/download/physicseditor/#{version}/PhysicsEditor-#{version}-uni.dmg"
+  appcast 'https://www.codeandweb.com/releases/PhysicsEditor/appcast-mac-release.xml',
+          :sha256 => 'a908f304de5309eba0ca29f3a4105fb97d780c8c9f77137f6f6d6c709bcef2c5'
   name 'PhysicsEditor'
   homepage 'https://www.codeandweb.com/physicseditor'
   license :freemium
