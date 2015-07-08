2929378fd1247dcad69caf36a0c042a82afa8f63
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-08 09:48:16 +0300

    spriteilluminator.rb: upgrade to v1.2.1

--- Casks/spriteilluminator.rb
+++ Casks/spriteilluminator.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'spriteilluminator' do
-  version '1.0.0'
-  sha256 '983017bb7bd27873ad2f09ea64894dc22191478f389def812dceb0ba8b18134e'
+  version '1.2.1'
+  sha256 'e8e389e300870e7a809cef4955dce4edc66203e6738796dee0341b5558ad2c3a'
 
   url "https://www.codeandweb.com/download/spriteilluminator/#{version}/SpriteIlluminator-#{version}-uni.dmg"
   name 'SpriteIlluminator'
