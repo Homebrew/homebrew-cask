811426bd9135fe07741f5f1b4b1ddba634f6762f
scribblemaniac <connor.deptuck@gmail.com>
2015-07-08 09:57:47 -0600

    electron.rb: update to 0.29.2

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,9 +1,10 @@
 cask :v1 => 'electron' do
-  version '0.29.1'
-  sha256 '7b587eba1dc1c4612cbb600b0c6484a61eb70e1d77207b201af771beace1d6b0'
+  version '0.29.2'
+  sha256 'a15a636a03965c63f522f4129523e2acbfb027547905706f704a68a6647ec248'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
-  appcast 'https://github.com/atom/electron/releases.atom'
+  appcast 'https://github.com/atom/electron/releases.atom',
+          :sha256 => '438dede832f397b75c417922be95f2793a870d779a7117938b05bf289ef173e3'
   name 'Electron'
   homepage 'http://electron.atom.io/'
   license :mit
