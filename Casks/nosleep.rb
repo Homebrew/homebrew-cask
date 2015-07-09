10b16727ea51a5a472550c585bb8d401eb30d3c7
scribblemaniac <connor.deptuck@gmail.com>
2015-07-09 12:09:17 -0600

    nosleep.rb: update to not use google code

--- Casks/nosleep.rb
+++ Casks/nosleep.rb
@@ -2,11 +2,10 @@ cask :v1 => 'nosleep' do
   version '1.4.0'
   sha256 '29e7f771970dce41936372687a5160700e2208357ef1ce37d81ac95c9188efe8'
 
-  # github.com is the official download host per the vendor homepage
   url "https://github.com/integralpro/nosleep/releases/download/v#{version}/NoSleep-#{version}.dmg"
   appcast 'https://github.com/integralpro/nosleep/releases.atom'
   name 'NoSleep'
-  homepage 'https://code.google.com/p/macosx-nosleep-extension/'
+  homepage 'https://integralpro.github.io/nosleep/'
   license :gpl
 
   pkg 'NoSleep.pkg'
