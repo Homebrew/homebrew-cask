85d31f5a7dfa096566ff9d1027fcc4354468cbbf
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:18 +0300

    numi.rb: add appcast

--- Casks/numi.rb
+++ Casks/numi.rb
@@ -3,6 +3,8 @@ cask :v1 => 'numi' do
   sha256 :no_check
 
   url 'http://numi.io/static/files/Numi.zip'
+  appcast 'http://numi.io/update.xml',
+          :sha256 => 'fc6e87987f66f478065ac353a270fa122fb329e85f5b7fd6cb256caefd22b7ed'
   name 'Numi'
   homepage 'http://numi.io/'
   license :gratis
