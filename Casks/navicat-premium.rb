c9e8be6e9d31e8095d15eacd58ff6a50e2ec218b
fanquake <fanquake@gmail.com>
2015-07-09 10:55:41 +0800

    Navicat Premium: Correct version

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'navicat-premium' do
-  version '11.1.12'   # navicat-premium.rb and navicat-for-* should be upgraded together
+  version '11.1.13'   # navicat-premium.rb and navicat-for-* should be upgraded together
   sha256 'a20731c590c75a04795c4f5993b0dc68e204482c43e7ce025661f401da6a67e7'
 
   url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_premium_en.dmg"
