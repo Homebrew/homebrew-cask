2356389d12a29301dfdb107193a7fcaf002432f9
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 04:08:23 +0300

    blueharvest.rb: add appcast

--- Casks/blueharvest.rb
+++ Casks/blueharvest.rb
@@ -3,6 +3,8 @@ cask :v1 => 'blueharvest' do
   sha256 '3ecb68a17a82a73d90c2451418114dcd925ed18da8bc4c03ccd5aeae15c0f24e'
 
   url "http://zeroonetwenty.com/downloads/BlueHarvest#{version.gsub('.','')}.dmg"
+  appcast 'https://cp37.ezyreg.com/~zeroonet/downloads/versioninfo/sparkle/blueharvest6.xml',
+          :sha256 => '0a6a7811abf74d36ee9fb78ce2b208fbded639da602f3900a2cda0865f17bda0'
   name 'BlueHarvest'
   homepage 'http://zeroonetwenty.com/blueharvest/'
   license :commercial
