bd11fcac44b5a02e5effc02679d9445decdcd69b
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:08 +0300

    debookee.rb: add appcast

--- Casks/debookee.rb
+++ Casks/debookee.rb
@@ -3,6 +3,8 @@ cask :v1 => 'debookee' do
   sha256 '7f2a67d6a8d80da39a9a5d3b200c3995868b608fc9abc34b0cd690d84db090fb'
 
   url 'http://www.iwaxx.com/debookee/debookee.zip'
+  appcast 'http://www.iwaxx.com/debookee/appcast.php',
+          :sha256 => '42d6b8de182952c1b32fbc12614a3690d125688c97f47618b26cc7aa192b761b'
   name 'Debookee'
   homepage 'http://www.iwaxx.com/debookee/'
   license :commercial
