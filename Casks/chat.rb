74a0954359055a918842b65f03f666e8f50319ff
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:03 +0300

    chat.rb: add appcast

--- Casks/chat.rb
+++ Casks/chat.rb
@@ -4,6 +4,8 @@ cask :v1 => 'chat' do
 
   # devmate.com is the official download host per the vendor homepage
   url 'http://dl.devmate.com/com.perma.chat/Chat.zip'
+  appcast 'http://updateinfo.devmate.com/com.perma.chat/updates.xml',
+          :sha256 => '8009a30326218077e451fc3e3096eb37c7eaa1c667d32f5beccdece99c6c8a4b'
   name 'Chat'
   homepage 'https://chatformac.com/'
   license :gratis
