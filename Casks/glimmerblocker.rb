fa6232e5c0fc9f99fc20a3088bddca5a82fc5572
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:14 +0300

    glimmerblocker.rb: add appcast

--- Casks/glimmerblocker.rb
+++ Casks/glimmerblocker.rb
@@ -3,6 +3,8 @@ cask :v1 => 'glimmerblocker' do
   sha256 'bcb9f53c4eb33a907fdaa42b7e5deb5b48d2495269e8746206691b393e2db507'
 
   url "https://glimmerblocker.org/downloads/GlimmerBlocker-#{version}.dmg"
+  appcast 'https://glimmerblocker.org/site/rss/sparkle-final.xml',
+          :sha256 => 'fce1b515c7ee075a7ce56f91753ca292eb786cc32180f7d8eed4c521a2aa3a03'
   name 'GlimmerBlocker'
   homepage 'https://glimmerblocker.org/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
