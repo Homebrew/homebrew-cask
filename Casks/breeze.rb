801a57e10d15b85907672e8e567f5d953d78d14e
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:02 +0300

    breeze.rb: add appcast

--- Casks/breeze.rb
+++ Casks/breeze.rb
@@ -3,6 +3,8 @@ cask :v1 => 'breeze' do
   sha256 :no_check
 
   url 'https://autumnapps.com/downloads/Breeze.dmg'
+  appcast 'https://www.autumnapps.com/updates/profiles/breeze.php',
+          :sha256 => '57f8c2cd010312440cc9785869c79094ce673492ac42e359f2db9999e87000ab'
   name 'Breeze'
   homepage 'https://autumnapps.com/breeze/'
   license :freemium
