f24a76b0d9954f9666138f087b9c937d260d39b3
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:13 +0300

    firetask.rb: add appcast

--- Casks/firetask.rb
+++ Casks/firetask.rb
@@ -3,6 +3,8 @@ cask :v1 => 'firetask' do
   sha256 :no_check
 
   url 'http://downloads.firetask.com/firetask-mac/Firetask-Mac.dmg'
+  appcast 'http://www.firetask.com/firetask-mac/updates/firetask-mac.xml',
+          :sha256 => '1965e72fe7c9fbe2d64b0b1f3ce20eb45ee491739069aa128eb9d3cb57ac85d0'
   name 'FireTask'
   homepage 'http://firetask.com'
   license :closed
