87063c1f05b2bdd6dd8db48e1267087ea2645aa0
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:17 +0300

    istat-menus.rb: add appcast

--- Casks/istat-menus.rb
+++ Casks/istat-menus.rb
@@ -3,6 +3,8 @@ cask :v1 => 'istat-menus' do
   sha256 :no_check
 
   url 'http://download.bjango.com/istatmenus/'
+  appcast 'http://bjango.com/istatmenus/appcast/appcast2.xml',
+          :sha256 => '4780e5d8414c45e00e3ac792d67bd8ba0b1d59f1e9394ee1db0478352b040db2'
   name 'iStats Menus'
   homepage 'http://bjango.com/mac/istatmenus/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
