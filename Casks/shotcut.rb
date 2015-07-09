aec40702415c42bc0669818647284bdb9892c1ae
Julio Napurí <julionc@gmail.com>
2015-07-09 15:19:51 -0500

    Upgrade Shotcut to v15.07.02

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'shotcut' do
-  version '15.06.01'
-  sha256 '8b543392309dbf5a42e1ca275d12d3aa27a2cfefbf465c7fc25c087fbadd04ae'
+  version '15.07.02'
+  sha256 'a5176318299e88e325cbece37403e3d3f4057808f2861962137f4e6d0de11705'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/mltframework/shotcut/releases/download/v#{version.sub(/\.\d+$/, '')}/shotcut-osx-x86_64-#{version.gsub('.', '')}.dmg"
