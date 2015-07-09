4a0116407ae9cb621495d1468d10833aa734a50c
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:13 +0300

    fstream.rb: add appcast

--- Casks/fstream.rb
+++ Casks/fstream.rb
@@ -3,6 +3,8 @@ cask :v1 => 'fstream' do
   sha256 :no_check
 
   url 'http://www.sourcemac.com/fstream_FStream.tgz'
+  appcast 'http://www.sourcemac.com/sparkle.php?id=156&content=xml',
+          :sha256 => '01ef6ac710afdc6e9bff5969ee9bb3f4a04d9e962d94bf8c6755ba44409192ad'
   name 'FStream'
   homepage 'http://www.sourcemac.com/?page=fstream'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
