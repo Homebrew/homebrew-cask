d40bf676c06d5b2fe4ed4b33dc3634cf6f02e9bd
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:10 +0300

    delayedlauncher.rb: add appcast

--- Casks/delayedlauncher.rb
+++ Casks/delayedlauncher.rb
@@ -3,6 +3,8 @@ cask :v1 => 'delayedlauncher' do
   sha256 :no_check
 
   url 'http://www.taoeffect.com/delayedlauncher/DelayedLauncher.zip'
+  appcast 'https://www.taoeffect.com/delayedlauncher/appcast.xml',
+          :sha256 => '7890874b09454101e7af04a798fea7a6daea3c18f820b628ce51c12c9d4b2f00'
   name 'DelayedLauncher'
   homepage 'https://www.taoeffect.com/blog/2010/03/delayedlauncher-2-0/'
   license :bsd
