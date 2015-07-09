83beea4203892742c520b2ab026ead89854f85c7
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:12 +0300

    finderminder.rb: add appcast

--- Casks/finderminder.rb
+++ Casks/finderminder.rb
@@ -3,6 +3,8 @@ cask :v1 => 'finderminder' do
   sha256 :no_check
 
   url 'https://www.irradiatedsoftware.com/download/FinderMinder.zip'
+  appcast 'https://www.irradiatedsoftware.com/updates/profiles/finderminder.php',
+          :sha256 => 'f546195134c20f6b04600b97c089467afeb02c7c3c86e683ca9545fbe8d49770'
   name 'FinderMinder'
   homepage 'http://irradiatedsoftware.com/labs'
   license :gratis
