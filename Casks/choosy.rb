35394bddfb17a5bc1bf045d7738cb32535c044b1
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:03 +0300

    choosy.rb: add appcast

--- Casks/choosy.rb
+++ Casks/choosy.rb
@@ -3,6 +3,8 @@ cask :v1 => 'choosy' do
   sha256 '87513395c94221ae9a83c787ac78f1a1028099dc9be4883e19924768a354c6ca'
 
   url "http://downloads.choosyosx.com/choosy_#{version}.zip"
+  appcast 'http://www.choosyosx.com/sparkle/feed',
+          :sha256 => '6c9105f7a2d400fb6928a1b0ed2384d098ef016a319b413684723033880d1601'
   name 'Choosy'
   homepage 'http://www.choosyosx.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
