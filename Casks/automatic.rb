79fa1b64fa201eda371574c12569d0dc4102a96b
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:57:58 +0300

    automatic.rb: add appcast

--- Casks/automatic.rb
+++ Casks/automatic.rb
@@ -3,6 +3,8 @@ cask :v1 => 'automatic' do
   sha256 :no_check
 
   url 'http://codingcurious.com/downloads/Automatic.2.zip'
+  appcast 'http://update.codingcurious.com/automatic/appcast2.xml',
+          :sha256 => 'a6815fa749fb59e725ddb525692bec4f34d8953b307114bf51fb21ab0d331fb4'
   name 'Automatic'
   homepage 'http://codingcurious.com/automatic/'
   license :commercial
