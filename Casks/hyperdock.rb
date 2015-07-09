56f882a5f6701d20f59eeb8c1bec0a6e2ca01cd7
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:16 +0300

    hyperdock.rb: add appcast

--- Casks/hyperdock.rb
+++ Casks/hyperdock.rb
@@ -3,6 +3,8 @@ cask :v1 => 'hyperdock' do
   sha256 :no_check
 
   url 'http://hyperdock.bahoom.com/HyperDock.dmg'
+  appcast 'https://bahoom.com/hyperdock/appcast.xml',
+          :sha256 => '22d00db14142a74fce6466bde7dd590ee7f3c3ec277e59b31a637885f97c9542'
   name 'HyperDock'
   homepage 'http://hyperdock.bahoom.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
