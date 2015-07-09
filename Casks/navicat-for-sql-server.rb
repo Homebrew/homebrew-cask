36d9402040a5c806a994ec40c3e7a7c9fd3ac282
fanquake <fanquake@gmail.com>
2015-07-09 10:55:25 +0800

    Navicat for SQL Server 11.1.13

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'navicat-for-sql-server' do
-  version '11.1.12'   # navicat-premium.rb and navicat-for-* should be upgraded together
-  sha256 'c051bb40b6c23bf3b60ce9b761f2bba779d64f839c03d7cc4060abbd43d5a10d'
+  version '11.1.13'   # navicat-premium.rb and navicat-for-* should be upgraded together
+  sha256 'c15d1cf3ef89b8cdcfae11fcaaba14d2dc7b0fc57a30f3ef83d7ee428f540962'
 
   url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_sqlserver_en.dmg"
   name 'Navicat for SQL Server'
