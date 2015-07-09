dbc2d93c9195f9dfbf8b535be6eb671e59be749e
fanquake <fanquake@gmail.com>
2015-07-09 11:20:35 +0800

    Navicat for Postgresql 11.1.13

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'navicat-for-postgresql' do
-  version '11.1.12'   # navicat-premium.rb and navicat-for-* should be upgraded together
-  sha256 '5c0a4fbf2f6e41f04db6660bdda876f5f8ed5e196ef683791f20bbe7c421f378'
+  version '11.1.13'   # navicat-premium.rb and navicat-for-* should be upgraded together
+  sha256 'ddc4dda1d15bc45550e10fc80c6a86f147dab8485198488f90ea3000045d5c79'
 
   url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_pgsql_en.dmg"
   name 'Navicat for PostgreSQL'
