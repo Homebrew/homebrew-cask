622aeeb629d54fcc20dc6311d223acd9a97f7fc3
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:11 +0300

    espresso.rb: add appcast

--- Casks/espresso.rb
+++ Casks/espresso.rb
@@ -3,6 +3,8 @@ cask :v1 => 'espresso' do
   sha256 :no_check
 
   url 'https://macrabbit.com/espresso/get/'
+  appcast 'https://update.macrabbit.com/espresso/2.2.3.xml',
+          :sha256 => '19e9805da329eab855c64bf36a2175a24e19cb2c32c98e14fdfa0828fe64e255'
   name 'Espresso'
   homepage 'http://macrabbit.com/espresso/'
   license :freemium
