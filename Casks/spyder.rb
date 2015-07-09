f2f4fd818f7e6884e0ecc01dd5a1516dc294a24a
scribblemaniac <connor.deptuck@gmail.com>
2015-07-09 12:49:51 -0600

    spyder.rb: update to not use google code

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'spyder' do
-  version '2.3.4'
-  sha256 'dbc71eb3c3952171fa37251ed441be80c525c47143626b4315f3775b8b4b1fed'
+  version '2.3.5.2'
+  sha256 '38c783b4f4ff83eee5a2ea9bf7a72b49cfc99252e8a7ad8e526621770d7b2c46'
 
   # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/spyder-ide/spyderlib/downloads/spyder-#{version}-py2.7.dmg"
