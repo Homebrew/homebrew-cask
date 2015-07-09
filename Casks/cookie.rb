1335d8cae9bb3673933f9c5e4cce5828ac168105
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-09 03:58:07 +0300

    cookie.rb: add appcast

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -3,6 +3,8 @@ cask :v1 => 'cookie' do
   sha256 :no_check
 
   url 'https://sweetpproductions.com/products/cookie/Cookie.dmg'
+  appcast 'https://sweetpproductions.com/products/cookie/appcast.xml',
+          :sha256 => 'dc629e015594e795d8c1f9ac94c4d2ead2317b18f3a06aaf701185106c49c8a3'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
   license :commercial
