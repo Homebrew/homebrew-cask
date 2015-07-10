4c3c68b4290653de85d055da1224d57a5e48222f
cclauss <cclauss@bluewin.ch>
2015-07-10 09:54:55 +0200

    Upgrade cloudfoundry-cli to v6.12.1
    Should close issue https://github.com/caskroom/homebrew-cask/issues/12504
    
    https://github.com/cloudfoundry/cli/releases

--- Casks/cloudfoundry-cli.rb
+++ Casks/cloudfoundry-cli.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'cloudfoundry-cli' do
-  version '6.11.3'
-  sha256 'a8534a3e4dcc285f127d7d32d62ebcf8dd03ed9003c80bf0c4633d0b72faec07'
+  version '6.12.1'
+  sha256 '4810c0dc3427db8f1c81c3f658dd5e6265120ae109ea438d1969ba7b96b6db84'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v#{version}/installer-osx-amd64.pkg"
