f0847f2bab66b61db674c2b62e7b4bbe5ebcb233
scribblemaniac <connor.deptuck@gmail.com>
2015-07-10 10:38:18 -0600

    omnigraffle.rb: switch to versioned url

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -8,9 +8,9 @@ cask :v1 => 'omnigraffle' do
     sha256 'a2eff19909d1ba38a4f01b2beecbde2f31f4af43d30e06d2c6921ae8880f85bc'
     url "http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.8/OmniGraffle-#{version}.dmg"
   else
-    version :latest
-    sha256 :no_check
-    url 'https://www.omnigroup.com/download/latest/omnigraffle'
+    version '6.2.4'
+    sha256 'f3b02a8ce77452209772d724c50a9ec5b4827eb86cd557435ae60938d3c6f5dd'
+    url "http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.10/OmniGraffle-#{version}.dmg"
   end
 
   name 'OmniGraffle'
