31ed5d0521bf3fedb7196d37a830ed8eb62bcfc9
scribblemaniac <connor.deptuck@gmail.com>
2015-07-10 11:25:17 -0600

    findings.rb: switched to versioned url

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -1,12 +1,15 @@
 cask :v1 => 'findings' do
-  version :latest
-  sha256 :no_check
+  version '1.2.3'
+  sha256 'f654eba4d4006f0f71399191ca94fa77408ece2ce9eeffad92632255bf2739cd'
 
-  url 'http://downloads.findingsapp.com/findings'
-  appcast 'http://downloads.findingsapp.com/appcast.xml'
+  url "http://downloads.findingsapp.com/Findings_2725_#{version}.zip"
+  appcast 'http://downloads.findingsapp.com/appcast.xml',
+          :sha256 => '51dae6921888aedc49e50375212746d9b92ebde1e653712c138af47eb5b8ab49'
   name 'Findings'
   homepage 'http://findingsapp.com'
-  license :commercial
+  license :freemium
+
+  depends_on :macos => '>= :mavericks'
 
   app 'Findings.app'
 end
