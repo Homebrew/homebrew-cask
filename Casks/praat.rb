cb89e3eacfed05bbc631e79656fd2c019b4b7614
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-07-10 11:01:44 +0200

    Upgrade praat to 5.4.11

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'praat' do
-  version '5.4.10'
+  version '5.4.11'
 
   if Hardware::CPU.is_32_bit?
-    sha256 'c6b52ba692ebaf67fdc9e44d5d85a0fb475e73002e72638a275ffb22dcc09a21'
+    sha256 '384f1cdb9f30ec67a5e2af3f3eca3a1af92a6487a6e97e7ed684f064ca94e3a4'
     url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac32.dmg"
   else
-    sha256 'eaf81041ea413d0b693cb9138ff428f89d892ccd6b6d9d39cf96a7ab160777f1'
+    sha256 '1072252a69f9e320427ed7e6b63255e94cdcb94074a070d3f9dd91bc38c3dfc4'
     url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac64.dmg"
   end
 
