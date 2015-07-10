ea1cb939a959ac7a7186da10ff1c8d012f79e997
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-10 09:19:12 +0300

    xtrafinder.rb: add appcast

--- Casks/xtrafinder.rb
+++ Casks/xtrafinder.rb
@@ -3,6 +3,8 @@ cask :v1 => 'xtrafinder' do
   sha256 :no_check
 
   url 'https://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg'
+  appcast 'https://www.trankynam.com/xtrafinder/XtraFinder-Appcast.xml',
+          :sha256 => 'f1246aa1bf1285d96d00d3210f6746b1d3b6183f96d221e5714754654aaa2cdb'
   name 'XtraFinder'
   homepage 'https://www.trankynam.com/xtrafinder/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
