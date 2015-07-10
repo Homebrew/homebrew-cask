fd388af2176a4ecfcb09ae0237448b8036abc0fb
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-10 09:19:01 +0300

    senuti.rb: add appcast

--- Casks/senuti.rb
+++ Casks/senuti.rb
@@ -3,6 +3,8 @@ cask :v1 => 'senuti' do
   sha256 '23bf22a7c468753ab71a6464d920f05223a093ec17bc9e3a7d3be8ea6913474f'
 
   url "https://www.fadingred.com/files/senuti/senuti_#{version}.zip"
+  appcast 'https://www.fadingred.com/senuti/updates.xml',
+          :sha256 => '119e7b3d8ffb2fdb0a16825318e5e8a707ec416617f0466e6c10785a8758db26'
   name 'Senuti'
   homepage 'https://www.fadingred.com/senuti/'
   license :commercial
