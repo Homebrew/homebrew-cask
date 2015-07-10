be457734a1cac2964c2e9fe6eaefddb6e1286a99
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-10 09:19:11 +0300

    vox-preferences-pane.rb: add appcast

--- Casks/vox-preferences-pane.rb
+++ Casks/vox-preferences-pane.rb
@@ -4,6 +4,8 @@ cask :v1 => 'vox-preferences-pane' do
 
   # devmate.com is the official download host per the vendor homepage
   url 'http://dl.devmate.com/com.coppertino.VoxPrefs/VoxPrefs.dmg'
+  appcast 'http://updateinfo.devmate.com/com.coppertino.VoxPrefs/updates.xml',
+          :sha256 => '25284589df6ed7b6b3819c23c70e77ac8f8122fec303f7ecb1e11cb173f7bb12'
   name 'VOX Preferences'
   homepage 'http://coppertino.com/addon'
   license :gratis
