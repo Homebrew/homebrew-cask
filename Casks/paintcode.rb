a913c96f472e1167ab47592b04a91b43d7a67626
Adelino Faria <adelino.faria@farfetch.com>
2015-07-10 17:21:28 +0100

    paintcode.rb: updated url, app name and changed version to :latest

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -1,12 +1,13 @@
 cask :v1 => 'paintcode' do
-  version '2.3.2'
-  sha256 '7a91f926ff352659abb5a189d0533c572a2b0bdced93d7b8bb94692d1b9e3487'
+  version :latest
+  sha256 :no_check
 
-  url "http://www.paintcodeapp.com/content/versions/#{version}/paintcode-trial.zip"
+  # pixelcut.com is the official download host per the appcast feed
+  url 'http://www.pixelcut.com/paintcode/paintcode.zip'
   appcast 'http://www.pixelcut.com/paintcode/appcast.xml'
   name 'PaintCode'
   homepage 'http://www.paintcodeapp.com/'
   license :commercial
 
-  app 'PaintCode Trial.app'
+  app 'PaintCode.app'
 end
