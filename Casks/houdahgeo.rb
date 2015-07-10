68990d71c5f3e2802c8dd67bf78e7ce5fdb06e7b
scribblemaniac <connor.deptuck@gmail.com>
2015-07-10 01:53:11 -0600

    houdahgeo.rb: change to versioned url

--- Casks/houdahgeo.rb
+++ Casks/houdahgeo.rb
@@ -1,9 +1,10 @@
 cask :v1 => 'houdahgeo' do
-  version :latest
-  sha256 :no_check
+  version '4.2.3'
+  sha256 '1eca862a9877e4640b064ba1d4c8f1f17bd688f5047bed632221ca17637754b7'
 
-  url 'http://houdah.com/houdahGeo/download_assets/HoudahGeo_latest.zip'
-  appcast 'http://www.houdah.com/houdahGeo/updates3/profileInfo.php'
+  url "http://houdah.com/houdahGeo/download_assets/HoudahGeo#{version}.zip"
+  appcast "http://www.houdah.com/houdahGeo/updates#{version.to_i}/profileInfo.php",
+          :sha256 => 'b958219d5a4fd405d1ec7855df315350810558e61397aa30ac1aeaf08b6477b3'
   name 'HoudahGeo'
   homepage 'http://houdah.com/houdahGeo/'
   license :commercial
