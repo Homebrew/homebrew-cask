9de8f9515a654609f5f297e077613d629330c3ba
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-10 09:18:55 +0300

    quickcal.rb: add appcast

--- Casks/quickcal.rb
+++ Casks/quickcal.rb
@@ -3,6 +3,8 @@ cask :v1 => 'quickcal' do
   sha256 :no_check
 
   url 'http://quickcalapp.com/app/quickcal-latest.zip'
+  appcast 'http://quickcalapp.com/app/appcast.xml',
+          :sha256 => '7524d0f790b23f051cb62bb9cef21e32acb1d85bd2f3195d30997e516edf7768'
   name 'QuickCal'
   homepage 'http://quickcalapp.com/'
   license :commercial
