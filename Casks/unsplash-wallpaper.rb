b57e68fc50b5d91db40cd092c2ae06d9ccf19457
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-10 09:19:11 +0300

    unsplash-wallpaper.rb: add appcast

--- Casks/unsplash-wallpaper.rb
+++ Casks/unsplash-wallpaper.rb
@@ -4,6 +4,8 @@ cask :v1 => 'unsplash-wallpaper' do
 
   # devmate.com is the official download host per the vendor homepage
   url 'http://dl.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/UnsplashWallpaper.dmg'
+  appcast 'http://updateinfo.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/updates.xml',
+          :sha256 => '9086fb92c1c4f96d4edfd356dd3b7e35c3c5ee10c9928c43f06f6817503e2640'
   name 'Unsplash Wallpaper'
   homepage 'http://leonspok.tumblr.com/unsplash-wallpaper'
   license :gratis
