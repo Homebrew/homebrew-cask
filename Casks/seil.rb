7f7a5ac5b2aa856e18d6adff5c95c54d3b435b91
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-10 09:19:00 +0300

    seil.rb: add appcast

--- Casks/seil.rb
+++ Casks/seil.rb
@@ -3,6 +3,8 @@ cask :v1 => 'seil' do
   sha256 '783426d11dbc2e75fdce3410ad8433f58d69b9a46696828f63f69e4847c53547'
 
   url "https://pqrs.org/osx/karabiner/files/Seil-#{version}.dmg"
+  appcast 'https://pqrs.org/osx/karabiner/files/seil-appcast.xml',
+          :sha256 => '1491ab1babc8ec5c65d845f9b0bf68b9ef575cc46b1ec7e41696b4eeb3fa3e7e'
   name 'Seil'
   homepage 'https://pqrs.org/osx/karabiner/seil.html'
   license :public_domain
