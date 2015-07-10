0b681066d13d691b8bcb7e2bf619153e27e60efc
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-10 09:19:03 +0300

    shelf-leveler.rb: add appcast

--- Casks/shelf-leveler.rb
+++ Casks/shelf-leveler.rb
@@ -3,6 +3,8 @@ cask :v1 => 'shelf-leveler' do
   sha256 :no_check
 
   url 'https://download.mrgeckosmedia.com/ShelfLeveler.zip'
+  appcast 'https://mrgeckosmedia.com/applications/appcast/Shelf-Leveler',
+          :sha256 => 'c105352e410ec86e4c28d524210532901be9efc0e59daf8231f0ade4eea1c39e'
   name 'Shelf Leveler'
   homepage 'https://mrgeckosmedia.com/applications/info/Shelf-Leveler'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
