2a979e5734945e018554eb71a6a50a28357f5d29
Arturo Herrero <arturo.herrero@gmail.com>
2015-07-10 11:06:56 +0100

    Upgrade ChitChat to 1.2.0

--- Casks/chitchat.rb
+++ Casks/chitchat.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'chitchat' do
-  version '1.1.0'
-  sha256 'e31b7df5585da3d52343c22ed3c9905adc6a72d22612d3bdb06e1ee5fcfd3e3a'
+  version '1.2.0'
+  sha256 '0e821745282fac10ea235087ad0e12971a5ffb4a5d879d8e0de9642337df373f'
 
   url "https://github.com/stonesam92/ChitCHat/releases/download/v#{version}/ChitChat.zip"
   appcast 'https://github.com/stonesam92/ChitChat/releases.atom'
