5f5c3071c3617a3bc4dcbac3e8004bd1bcaab9e2
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-10 09:19:06 +0300

    speedy.rb: add appcast

--- Casks/speedy.rb
+++ Casks/speedy.rb
@@ -3,6 +3,8 @@ cask :v1 => 'speedy' do
   sha256 :no_check
 
   url 'http://www.apimac.com/download/Speedy.zip'
+  appcast 'http://www.apimac.com/version_checking/speedy_mac.xml',
+          :sha256 => '7dddd319b0035c46f79e445d1e91d260a50f46f51963ef494c3f9e5cbe76fe58'
   name 'Speedy'
   homepage 'http://www.apimac.com/mac/speedy/'
   license :commercial
