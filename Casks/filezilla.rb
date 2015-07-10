62b2def007a77f3c0721f7deb1b149830956576d
scribblemaniac <connor.deptuck@gmail.com>
2015-07-10 01:39:25 -0600

    filezilla.rb: update to 3.12.0.2

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -4,8 +4,8 @@ cask :v1 => 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.11.0.2'
-    sha256 'fa64928e768bfcd946ec4c5963ad8de5dc0b0ab7cb43e000ecdcf7c7d31fd719'
+    version '3.12.0.2'
+    sha256 '628feaaf36a93bbd0fe2e21d9d9c201320d99ca43d9a2230325830ff1f95db78'
   end
 
   # sourceforge.net is the official download host per the vendor homepage
