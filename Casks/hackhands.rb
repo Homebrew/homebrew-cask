6350931914a272867b40ed5f2c7ea3d12457dddd
Sérgio Vilar <sergiovilar.r@gmail.com>
2015-07-09 22:44:51 -0300

    Fixed link to HackHands

--- Casks/hackhands.rb
+++ Casks/hackhands.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'hackhands' do
-  version :latest
-  sha256 :no_check
+  version '1.3.2.76'
+  sha256 '3300910a9a6d141cec8abc608f5d744c28008b1b3a82cadc1c2242846daa5e11'
 
-  url 'http://hackhands.com/osx/download'
+  url 'http://desktop.hackhands.com.s3.amazonaws.com/builds/#{version}/osx/HackHands.zip'
   name 'HackHands'
   homepage 'https://hackhands.com/desktop'
   license :gratis
