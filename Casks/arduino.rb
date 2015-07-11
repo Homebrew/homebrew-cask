ba0c77744414b55029b8bfa5d7c8f0faa8a1952d
Vítor Galvão <info@vitorgalvao.com>
2015-07-11 12:02:57 +0100

    arduino.rb: changed url back to http

--- Casks/arduino.rb
+++ Casks/arduino.rb
@@ -2,7 +2,7 @@ cask :v1 => 'arduino' do
   version '1.6.5'
   sha256 '945e65c86f476cb7f7f2d6c77b37d527ed654723aaa10d64e791a93f0568ab4b'
 
-  url "https://www.arduino.cc/download.php?f=/arduino-#{version}-macosx.zip"
+  url "http://downloads.arduino.cc/arduino-#{version}-macosx.zip"
   name 'Arduino'
   homepage 'https://arduino.cc/'
   license :oss
