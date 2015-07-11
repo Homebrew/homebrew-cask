acc8ce425b6744d13d87138c3331880e0804e4df
Martijn van der Ven <martijn@vanderven.se>
2015-07-11 15:00:36 +0200

    Update Mumble to v1.2.10

--- Casks/mumble.rb
+++ Casks/mumble.rb
@@ -1,12 +1,14 @@
 cask :v1 => 'mumble' do
-  version '1.2.9'
-  sha256 'd0c630a4fb65be50407080e2f97a47cd3aeeb67ddc52e9e02dd454421404e449'
+  version '1.2.10'
+  sha256 'f88f56e049b3375544f57777da20ca8461d4b631e4ef0de26fbfc5b789601504'
 
-  url "http://downloads.sourceforge.net/sourceforge/mumble/Mumble-#{version}.dmg"
+  # github.com is the official download host per the vendor homepage
+  url "https://github.com/mumble-voip/mumble/releases/download/#{version}/Mumble-#{version}.dmg"
+  appcast 'https://github.com/mumble-voip/mumble/releases.atom'
   gpg "#{url}.sig",
-      :key_url => 'http://mumble.info/gpg/mumble-auto-build-2014.asc'
+      :key_url => 'http://mumble.info/gpg/mumble-auto-build-2015.asc'
   name 'Mumble'
-  homepage 'http://mumble.sourceforge.net'
+  homepage 'http://www.mumble.info'
   license :bsd
 
   app 'Mumble.app'
