0c7ec315719c663f6cb4165009842645369c31bd
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-11 21:51:21 +0300

    appserver.rb: upgrade to v1.0.6-21

--- Casks/appserver.rb
+++ Casks/appserver.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'appserver' do
-  version '1.0.5-14'
-  sha256 '7c89590446687749a7f4b6624c276675d1faf4b955ce69642e0410d1b6489ae4'
+  version '1.0.6-21'
+  sha256 '33bd22f0b8852f37c6bbfcc1c9aa9e63a4916e12821642260a5054304916267b'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/appserver-io/appserver/releases/download/#{version.sub(/-.*/, '')}/appserver-dist_#{version}_x86_64.pkg"
