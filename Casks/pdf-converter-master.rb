e74edbc104a794e491eac56fb634c10888894d01
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-12 13:53:23 +0300

    pdf-converter-master.rb: use versioned url

--- Casks/pdf-converter-master.rb
+++ Casks/pdf-converter-master.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'pdf-converter-master' do
-  version :latest
-  sha256 :no_check
+  version '3.5.0'
+  sha256 'c927cea0b588dfc421c6f2feec6ab4a660c554a039e2bb160840b3bac17ed5ba'
 
-  url 'http://www.lightenpdf.com/Product/DownLoad.aspx?id=24&&t=0'
+  url "http://www.lightenpdf.com/download/download.aspx?file=PDF-Converter-Master-#{version.gsub('.','')}.zip"
   appcast 'http://www.lightenpdf.com/xml/PDF-Converter-Mac.xml',
           :sha256 => 'be7221e35d5e4e564f34bb364705595a9a0c73868627614e4629728f3bd3fdf8'
   name 'PDF Converter Master'
