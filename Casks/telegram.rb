3f4ee8b1d661ef359e97bda35b3d1bd367d0557a
Alex Alonso <alex4551@gmail.com>
2015-07-12 16:36:33 +0800

    update telegram version

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'telegram' do
-  version '0.8.21'
+  version '0.8.38'
   sha256 '343176aabd1a4451bda97d84e9f6090c973eb7613c0cdbfa464e0eba301fbaaa'
 
   # tdesktop.com is the official download host per the vendor homepage
