36d9018ee0f84ce7643510bc738d2ae98b747624
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-12 13:10:12 +0300

    airdisplay.rb: use versioned url

--- Casks/airdisplay.rb
+++ Casks/airdisplay.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'airdisplay' do
-  version :latest
-  sha256 :no_check
+  version '3.0.1'
+  sha256 '2da921517a94872b6909b225d8964081a1bad9d2b93be12592bd264665f4b769'
 
-  url 'http://downloads.avatron.com/AirDisplayInstaller.zip'
+  url "https://www.avatron.com/updates/software/airdisplay/ad#{version.gsub('.','')}.zip"
   name 'Air Display'
   appcast 'https://www.avatron.com/updates/software/airdisplay/appcast.xml',
           :sha256 => '5318742e7d9f7f4da9498e3100d8b5f92abc18a574988f1d5fa5a551ad0af062'
