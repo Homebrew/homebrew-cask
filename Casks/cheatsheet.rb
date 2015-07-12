3442d382f3f73c4eab9388062406d3728c4b1181
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-12 13:47:07 +0300

    cheatsheet.rb: use versioned url

--- Casks/cheatsheet.rb
+++ Casks/cheatsheet.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'cheatsheet' do
-  version :latest
-  sha256 :no_check
+  version '1.2.2'
+  sha256 '41cfec767f761e2400d5ad700c936339c8c2e80a9dfbaf44b66375e63192763c'
 
-  url 'http://www.cheatsheetapp.com/CheatSheet/download.php'
+  url "http://mediaatelier.com/CheatSheet/CheatSheet_#{version}.zip"
   appcast 'http://mediaatelier.com/CheatSheet/feed.php'
   name 'CheatSheet'
   homepage 'http://www.cheatsheetapp.com/CheatSheet/'
