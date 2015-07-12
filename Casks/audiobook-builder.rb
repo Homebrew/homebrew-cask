4b2bde88b51646425c0f0c3e1ffd3a12b5cbc81f
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-12 13:33:39 +0300

    audiobook-builder.rb: use versioned url

--- Casks/audiobook-builder.rb
+++ Casks/audiobook-builder.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'audiobook-builder' do
-  version :latest
-  sha256 :no_check
+  version '1.5.3'
+  sha256 '8ae30fcc3811292c84a23febb50c78e110bc7514f0d3085ce21b427f45be5d4c'
 
-  url 'http://www.splasm.com/downloads/audiobookbuilder/Audiobook%20Builder.dmg'
+  url "http://www.splasm.com/downloads/audiobookbuilder/Audiobook%20Builder%20#{version}.zip"
   name 'Audiobook Builder'
   appcast 'http://www.splasm.com/versions/audiobookbuilder_sparkle.xml',
           :sha256 => '0fa38f9d15f10be75de37b6ea556a486e9073b9a176fae5a2f6e8602b596ed5c'
