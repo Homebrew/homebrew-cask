83f4dc9574db09251211d71b8fb29f07934f5c8d
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-12 13:24:54 +0300

    anxiety.rb: use versioned url

--- Casks/anxiety.rb
+++ Casks/anxiety.rb
@@ -1,13 +1,12 @@
 cask :v1 => 'anxiety' do
-  version :latest
-  sha256 :no_check
+  version '1.0'
+  sha256 'dba5afb10069592fbef623b2ddd53009da84dbf9bb3fd6720e849c304282e708'
 
-  url 'http://www.anxietyapp.com/Anxiety.zip'
+  url "http://www.anxietyapp.com/downloads/Anxiety_#{version}.zip"
   name 'Anxiety'
   appcast 'http://www.anxietyapp.com/appcast/appcast.xml'
   homepage 'http://www.anxietyapp.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  container :nested => 'Anxiety.dmg'
   app 'Anxiety.app'
 end
