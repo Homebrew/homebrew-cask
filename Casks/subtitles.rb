e95965f674b1d13f73dc40d512a08c239e35a3f0
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-07-11 22:03:04 -0300

    Updated to last Subtitles App

--- Casks/subtitles.rb
+++ Casks/subtitles.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'subtitles' do
-  version '2.3'
-  sha256 '136c819187ed86d0c40de96577181906b9b8fc9c8bbe930c0eb989c8d4b0ed2d'
+  version '3.0.103'
+  sha256 'accc049b4f7d26ea31ca8693d43b8d3be785e4c529fb12b7a2ec8fda8f0109cd'
 
-  url "http://subtitlesapp.com/download/Subtitles-mac-#{version}.zip"
+  url "http://subtitlesapp.com/static/Subtitles-mac-#{version}.zip"
   appcast 'http://subtitlesapp.com/updates.xml',
           :sha256 => '7f06acee1a20d9ac324c90950952f2160b69839eee00a9d741a09a8799cb6a84'
   name 'Subtitles'
