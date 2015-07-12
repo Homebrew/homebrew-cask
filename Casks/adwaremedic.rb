bdff534e353dd902f076a7b9c03048d9b4daf689
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-12 11:49:18 +0300

    adwaremedic.rb: use versioned url

--- Casks/adwaremedic.rb
+++ Casks/adwaremedic.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'adwaremedic' do
-  version :latest
-  sha256 :no_check
+  version '2.2.6'
+  sha256 'e43517656b664856d4497a9edd9bfea353cf4167327311cd7c79f1d2fe355b61'
 
-  url 'http://www.adwaremedic.com/AdwareMedic.dmg'
+  url "http://www.adwaremedic.com/versions/AdwareMedic#{version}.zip"
   name 'AdwareMedic'
   appcast 'http://www.adwaremedic.com/versions/updates.xml',
           :sha256 => 'f93747bc91165da2321ad80277177eec745e316dc6fd7c5c511b72c8a521ad4f'
