c41c236fbf7c15c4dc5549f4d3c11a2d3fa7177b
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-12 13:46:27 +0300

    chatology.rb: use versioned url

--- Casks/chatology.rb
+++ Casks/chatology.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'chatology' do
-  version :latest
-  sha256 :no_check
+  version '1.0.6'
+  sha256 'ca29acf56a4be7e6415cfaaac7d0fa5994dc132fe94eaa45e1c37f6aa4d07225'
 
-  url 'https://flexibits.com/chatology/download'
+  url "http://cdn.flexibits.com/Chatology_#{version}.zip"
   appcast 'https://flexibits.com/chatology/appcast.php'
   name 'Chatology'
   homepage 'https://flexibits.com/chatology'
