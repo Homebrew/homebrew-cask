b73cb57374c952155c21bc8594da213c7ac5493f
Cătălin Mariș <alrraa@gmail.com>
2015-07-12 22:17:10 +0300

    Fix checksum for `lisanet-gimp`

--- Casks/lisanet-gimp.rb
+++ Casks/lisanet-gimp.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'lisanet-gimp' do
   version '2.8.14p1'
-  sha256 '2a755c08e50290b494c26c416b6dda6ad4151610f752216d0db126d479ed787e'
+  sha256 '9769d83ddbe8ed591e6c0bd828b9133460e231c06e416d948c1f7012b478b346'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/gimponosx/Gimp-#{version}-Mavericks-Yosemite.dmg"
