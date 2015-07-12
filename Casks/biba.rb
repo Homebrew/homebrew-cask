e451d9fc77912ed72b40ca8f85ddb696c6079eb6
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-12 13:38:11 +0300

    biba.rb: use versioned url

--- Casks/biba.rb
+++ Casks/biba.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'biba' do
-  version :latest
-  sha256 :no_check
+  version '3.2.3350'
+  sha256 '6d340ef45bf3e85f718d09d6df6e9309dbc9912bff96e0569647606089cc16f2'
 
-  url 'https://www.biba.com/osx_downloads/dmg'
+  # amazonaws.com is the official download host per the vendor homepage
+  url "https://s3-us-west-1.amazonaws.com/downloads.biba.com/builds/Biba-OSX-#{version}.dmg"
   name 'Biba'
   appcast 'https://biba.com/osx_downloads/appcast',
           :sha256 => '1ba615b670b0bc57c1ceef52a86399dfc848cff92a9e7f13f6e628217972d1db'
