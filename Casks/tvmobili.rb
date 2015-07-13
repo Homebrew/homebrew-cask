1d6f34523fcd21eaf6b910f9004ea4ebc869d73c
Jaden Choi <jaden@allwin.bid>
2015-07-13 21:59:49 +0900

    Remove a trailing whitespace

--- Casks/tvmobili.rb
+++ Casks/tvmobili.rb
@@ -6,7 +6,7 @@ cask :v1 => 'tvmobili' do
   name 'TVMOBiLi'
   homepage 'http://www.tvmobili.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
-  
+ 
   pkg 'tvmobili-yosemite-universal.2.1.4693.pkg'
 
   uninstall :pkgutil   => 'com.tvmobili.tvmobilisvcd',
