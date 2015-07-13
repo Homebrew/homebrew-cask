03bc1cdd77ad3ecc25941b2d4b299d3a8855f9cc
scribblemaniac <connor.deptuck@gmail.com>
2015-07-13 11:10:49 -0600

    unsplash-wallpaper.rb: switch to versioned url

--- Casks/unsplash-wallpaper.rb
+++ Casks/unsplash-wallpaper.rb
@@ -1,11 +1,12 @@
 cask :v1 => 'unsplash-wallpaper' do
-  version :latest
-  sha256 :no_check
+  version "1132"
+  sha256 'c00e83f17212dc9cd6768f090ecc41bdb30890ecc822a34f5196805ff87b134b'
 
   # devmate.com is the official download host per the vendor homepage
+  url "http://dl.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/1132/1436541007/UnsplashWallpaper-#{version}.zip"
   url 'http://dl.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/UnsplashWallpaper.dmg'
   appcast 'http://updateinfo.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/updates.xml',
-          :sha256 => '9086fb92c1c4f96d4edfd356dd3b7e35c3c5ee10c9928c43f06f6817503e2640'
+          :sha256 => '16756a17b97312c9baf0f72e64f40dab13f88fd508a09c981307d6e66cce960d'
   name 'Unsplash Wallpaper'
   homepage 'http://leonspok.tumblr.com/unsplash-wallpaper'
   license :gratis
