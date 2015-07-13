bf066a86234af29abc478a006c79ea04c5645459
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-13 20:09:24 +0300

    hands-off.rb: add suppress_move_to_applications

--- Casks/hands-off.rb
+++ Casks/hands-off.rb
@@ -11,5 +11,9 @@ cask :v1 => 'hands-off' do
 
   app 'Hands Off!.app'
 
+  postflight do
+    suppress_move_to_applications
+  end
+
   zap :delete => '~/Library/Preferences/com.metakine.handsoff.plist'
 end
