b8239f9eec29386694ad8944df37c827044f29cd
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-07-13 12:06:41 -0400

    Add Menubar Colors

---
+++ Casks/menubar-colors.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'menubar-colors' do
+  version '1.4.0'
+  sha256 :no_check
+
+  url 'https://github.com/nvzqz/Menubar-Colors/releases/download/v1.4.0/Menubar-Colors.zip'
+  name 'Menubar Colors'
+  homepage 'https://github.com/nvzqz/Menubar-Colors'
+  license :mit
+
+  app 'Menubar Colors.app'
+end
