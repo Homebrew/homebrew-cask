cd0caefe7bcb9c24a2e92040be33a17bce82e1a0
Vítor Galvão <info@vitorgalvao.com>
2015-07-13 21:10:59 +0100

    unsplash-wallpaper.rb: removed extra url

--- Casks/unsplash-wallpaper.rb
+++ Casks/unsplash-wallpaper.rb
@@ -4,7 +4,6 @@ cask :v1 => 'unsplash-wallpaper' do
 
   # devmate.com is the official download host per the vendor homepage
   url "http://dl.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/1132/1436541007/UnsplashWallpaper-#{version}.zip"
-  url 'http://dl.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/UnsplashWallpaper.dmg'
   appcast 'http://updateinfo.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/updates.xml',
           :sha256 => '16756a17b97312c9baf0f72e64f40dab13f88fd508a09c981307d6e66cce960d'
   name 'Unsplash Wallpaper'
