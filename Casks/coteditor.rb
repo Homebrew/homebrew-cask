379649e3383a02dc04ba5362a0c47da4b88145d4
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-14 21:38:24 +0300

    coteditor.rb: upgrade to v2.1.5

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -3,8 +3,8 @@ cask :v1 => 'coteditor' do
     version '2.0.3'
     sha256 'b656aa0b6526c89d7d52cf12b715cf529f8e5048c7e13720673c3e32318a26d8'
   elsif MacOS.release >= :mountain_lion
-    version '2.1.4'
-    sha256 '33c8e34270e46736be89079c83b1b648ce3bafbc48396e1b38c0b00a5a479511'
+    version '2.1.5'
+    sha256 'be9ffd9a150e4008ec4d99b1e9603be075f0a6edd6a3dfa73cc3f13964bbd425'
   end
 
   # github.com is the official download host per the vendor homepage
