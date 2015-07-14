f3391a08783b73673c647b97cc867441670402d0
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-14 21:36:38 +0300

    clipy.rb: upgrade to v1.0.2

--- Casks/clipy.rb
+++ Casks/clipy.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'clipy' do
-  version '1.0.1'
-  sha256 '8ec7ab4e1a04034aa612f014579c7e73d2cbca5715fe3d057daaa84960a291eb'
+  version '1.0.2'
+  sha256 'e8f09e7ad766f71e6f780c8a05f5cbdf5c71465c4586263f8b73a8e9ae632d93'
 
   url "https://github.com/Clipy/Clipy/releases/download/#{version}/Clipy_#{version}.dmg"
   appcast 'http://clipy-app.com/appcast.xml',
-          :sha256 => 'ad739513da663612cdcfe7c9ab58c3d72e958aabeb32873fb50ba4fd6aef702d',
+          :sha256 => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
           :format => :sparkle
   name 'Clipy'
   homepage 'http://clipy-app.com/'
