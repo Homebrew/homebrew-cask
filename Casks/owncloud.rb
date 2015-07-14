3c91414fdf2648b7dc4422d5bb3d29f0a7bbf858
Alex Markessinis <markea@almyz125.com>
2015-07-14 10:48:53 -0400

    owncloud 1.8.3 => 1.8.4

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'owncloud' do
-  version '1.8.3.2458'
-  sha256 'a4091364faed523bd1b77494e3e12947164e3d132bf16cd4cb8d28f6109d1399'
+  version '1.8.4.2531'
+  sha256 '4435bd4e1e6a07f8aadf47d33d42c08449cbe517433e21c7723c4815a97cee84'
 
   url "https://download.owncloud.com/desktop/stable/ownCloud-#{version}.pkg"
   name 'ownCloud'
