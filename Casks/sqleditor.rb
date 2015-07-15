5ef3ace826022b61cba276f3fc0aef451f7b9f92
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-15 12:01:10 +0300

    sqleditor.rb: upgrade to v2.9.8

--- Casks/sqleditor.rb
+++ Casks/sqleditor.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'sqleditor' do
-  version '2.9.7'
-  sha256 '3d5429e492d1407d89638b40afdb160f40149d4d71557dd5f6e5430040161e67'
+  version '2.9.8'
+  sha256 '61cdd782ac782cd4741c6bb179ea625ad50b607ae534308155e04b63f7236c60'
 
   url "https://www.malcolmhardie.com/sqleditor/releases/#{version}/SQLEditor-#{version.gsub('.','-')}.zip"
   appcast 'https://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml',
-          :sha256 => 'b7650b4e3cc7d8b369d962238f2a31fb7883cf76524c96781f7e29cc1de65e2f'
+          :sha256 => 'c213dbddb9627f7ccc7876793f6bd0b383f3b499c340cfc9f66aec0bd495ffc4'
   name 'SQLEditor'
   homepage 'https://www.malcolmhardie.com/sqleditor/'
   license :commercial
