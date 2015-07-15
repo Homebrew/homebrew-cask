da332d356defea4b70276a5c20ad561f44c6faa7
prime8 <prime8@users.noreply.github.com>
2015-07-15 10:09:05 -0400

    Update org.nodejs pkg to 0.12.7

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'node' do
-  version '0.12.5'
-  sha256 'f113a8cba38fb1b09a167d7703719a6b2f40ac7f6d55d0f0e66744937924eedc'
+  version '0.12.7'
+  sha256 '342080e569e39fd7e829591411ea7556d6739e7a33bd43ecf0833d26fb7da827'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   name 'Node.js'
