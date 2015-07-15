cbcedc41ef94ed5e9958bcea4390691670b190e1
Michael <fanquake@gmail.com>
2015-07-15 09:22:15 +0800

    Correct Isyncr URL, re fixes #12575

--- Casks/isyncr.rb
+++ Casks/isyncr.rb
@@ -2,7 +2,7 @@ cask :v1 => 'isyncr' do
   version '5.0.8'
   sha256 '1b1c0e6054b890fa7dd903cc680fd2543bc6b40b34cde2d23f4c2e355dcffb87'
 
-  url "http://www.jrtstudio.com/files/iSyncr%20#{version}.pkg"
+  url "http://www.jrtstudio.com/files/iSyncr%20Desktop%20#{version}.pkg"
   name 'iSyncr'
   homepage 'http://www.jrtstudio.com/iSyncr-iTunes-for-Android'
   license :unknown # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
