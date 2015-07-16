bc2394209a18853f9ffa1e37fb6aa5211f06275c
Joaquín Vicente <joakin@gmail.com>
2015-07-16 15:19:17 -0300

    Upgrade Vivaldi.app to TP4 v1.0.219.50

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'vivaldi' do
-  version '1.0.162.9'
+  version '1.0.219.50'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://vivaldi.com/download/Vivaldi_TP3.#{version}.dmg"
+  url "https://vivaldi.com/download/Vivaldi_TP4.#{version}.dmg"
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
   license :gratis
