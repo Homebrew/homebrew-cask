744ff9286621f1189518d72eef2dd31d0d5d8e1d
Sean Zhu <interestinglythere@gmail.com>
2015-07-16 09:19:02 -0700

    Patch Dia
    http://sourceforge.net/p/dia-installer/support-requests/98/

--- Casks/dia.rb
+++ Casks/dia.rb
@@ -9,4 +9,8 @@ cask :v1 => 'dia' do
   license :gpl
 
   app 'Dia.app'
+
+  postflight do
+    system '/usr/bin/sed', '-i', '--', 's/exec/exec env DISPLAY=:0/g', "#{staged_path}/Dia.app/Contents/Resources/bin/dia"
+  end
 end
