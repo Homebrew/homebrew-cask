8a82164cc944d21154a40e5071f2e9c883daad40
mecca831 <mecca831@gmail.com>
2015-07-16 18:45:31 -0700

    Update bittorrent-sync.rb

--- Casks/bittorrent-sync.rb
+++ Casks/bittorrent-sync.rb
@@ -2,8 +2,7 @@ cask :v1 => 'bittorrent-sync' do
   version :latest
   sha256 :no_check
 
-  # getsyncapp.com is the official download host per the vendor homepage
-  url 'https://download-cdn.getsyncapp.com/stable/osx/BitTorrent-Sync.dmg'
+  url 'https://download-cdn.getsync.com/stable/osx/BitTorrent-Sync.dmg'
   name 'BitTorrent Sync'
   # todo: response was not XML
   # appcast 'http://www.usyncapp.com/cfu.php'
