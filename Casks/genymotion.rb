5e88f0b8053aff5552cb64fb577d702bcf4b1e2d
Sven Jacobs <mail@svenjacobs.com>
2015-07-17 09:05:20 +0200

    Upgrade Genymotion to 2.5.1

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -1,13 +1,13 @@
 cask :v1 => 'genymotion' do
-  version '2.5.0'
-  sha256 '47f9e56116150942bd920a09ce94c2ef0448741a4b11ec3b7dc2e4c2356bbff4'
+  version '2.5.1'
+  sha256 '1201957a881c352c7170a5f976ea5cb354b6a1d69d88b3a463bb7ed3555545aa'
 
   depends_on :cask => 'virtualbox'
 
   url "http://files2.genymotion.com/genymotion/genymotion-#{version}/genymotion-#{version}.dmg"
   name 'Genymotion'
   homepage 'https://www.genymotion.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'Genymotion.app'
   app 'Genymotion Shell.app'
