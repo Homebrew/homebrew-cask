b75078fdb012315edc9494fc18c5dd36c194b7fa
Daehyun Y <hyuna917@me.com>
2015-07-18 02:38:47 +0900

    add bitdefender-adware-removal-tool.rb

---
+++ Casks/bitdefender-adware-removal-tool.rb
@@ -0,0 +1,17 @@
+cask :v1 => 'bitdefender-adware-removal-tool' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://download.bitdefender.com/mac/tools/Adware%20Removal%20Tool.zip'
+  name 'Bitdefender Adware Removal Tool for Mac'
+  homepage 'http://www.bitdefender.com'
+  license :gratis
+
+  app 'Adware Removal Tool.app'
+
+  uninstall :pkgutil => 'com.bitdefender.Adware-Removal-Tool'
+  zap :trash =>[
+    '~/Library/Preferences/com.bitdefender.com.bitdefender.Adware-Removal-Tool.plist',
+    '~/Library/Saved Application State/com.com.bitdefender.Adware-Removal-Tool.savedState'
+  ]
+end
