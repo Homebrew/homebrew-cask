2a6708c5a601729bf02e58f17f7e6b1ed394a3b6
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-18 20:08:54 +0300

    groovesquid.rb: upgrade to v0.9.4

--- Casks/groovesquid.rb
+++ Casks/groovesquid.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'groovesquid' do
-  version '0.9.3'
-  sha256 '5f7ad6c6ae6e47b993b6209021a12dfbba62a169a4cee7192e1b434319f43639'
+  version '0.9.4'
+  sha256 '93ad97b25c5065b60cdceae950c21b12d3802480dafbcb3336f3fd3c6dc77ebe'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/groovesquid/groovesquid/releases/download/v#{version}/Groovesquid.dmg"
