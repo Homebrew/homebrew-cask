62aafa739e0908a402b59edec523e00083f2287e
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-19 17:55:25 +0300

    pandoc.rb: upgrade to v1.15.0.6

--- Casks/pandoc.rb
+++ Casks/pandoc.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pandoc' do
-  version '1.15'
-  sha256 '09546bfd900668ca9949dbb05124ef9ce9778725952a7112141e4e00abbb702e'
+  version '1.15.0.6'
+  sha256 'e5b254480b43d5cce19c01723e93cd357ec285cfe8d320cae5bc15580afb92d4'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/jgm/pandoc/releases/download/#{version}/pandoc-#{version}-osx.pkg"
