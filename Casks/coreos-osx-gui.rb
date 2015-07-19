93539c81ce9f2ddfe741692fa3e1e1c22953a55a
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-19 16:37:04 +0300

    coreos-osx-gui.rb: use versioned url

--- Casks/coreos-osx-gui.rb
+++ Casks/coreos-osx-gui.rb
@@ -2,7 +2,7 @@ cask :v1 => 'coreos-osx-gui' do
   version '0.8.4'
   sha256 'acef0c3090f4c249c95d8cfb281da65787133b2e64ec4ae8d1925bdb2cc2a3cb'
 
-  url "https://github.com/rimusz/coreos-osx-gui/releases/download/v0.8.4/CoreOS_GUI_v#{version}.dmg"
+  url "https://github.com/rimusz/coreos-osx-gui/releases/download/v#{version}/CoreOS_GUI_v#{version}.dmg"
   appcast 'https://github.com/rimusz/coreos-osx-gui/releases.atom'
   name 'CoreOS GUI'
   name 'CoreOS-Vagrant GUI'
