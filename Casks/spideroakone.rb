4baeb9be54148d1fb5250e776357b57f4fbe5d49
fenque <fenque@users.noreply.github.com>
2015-07-20 23:43:54 +0300

    Update spideroak.rb to spideroakone.rb

--- Casks/spideroakone.rb
+++ Casks/spideroakone.rb
@@ -1,11 +1,13 @@
-cask :v1 => 'spideroak' do
+cask :v1 => 'spideroakone' do
   version :latest
   sha256 :no_check
 
   url 'https://spideroak.com/getbuild?platform=mac'
-  name 'SpiderOak'
+  name 'SpiderOakOne'
   homepage 'https://spideroak.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  app 'SpiderOak.app'
+  pkg 'SpiderOakONE.pkg'
+
+  uninstall :delete => '/Applications/SpiderOakONE.app'
 end
