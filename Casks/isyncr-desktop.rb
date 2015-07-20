83d2ef47c87395c628335c70f2cba82fce91b5fe
Vítor Galvão <info@vitorgalvao.com>
2015-07-20 01:27:23 +0100

    Renamed iSyncr

---
+++ Casks/isyncr-desktop.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'isyncr-desktop' do
+  version '5.0.8'
+  sha256 '1b1c0e6054b890fa7dd903cc680fd2543bc6b40b34cde2d23f4c2e355dcffb87'
+
+  url "http://www.jrtstudio.com/files/iSyncr%20Desktop%20#{version}.pkg"
+  name 'iSyncr Desktop'
+  homepage 'http://www.jrtstudio.com/iSyncr-iTunes-for-Android'
+  license :unknown # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  pkg "iSyncr Desktop #{version}.pkg"
+
+  uninstall :pkgutil => 'com.test.iSyncr.pkg',
+            :quit    => 'com.JRTStudio.iSyncrWiFi'
+end
