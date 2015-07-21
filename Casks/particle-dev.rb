e64a1a80d3b783e21f42438b35155b0cacfbf040
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-21 16:06:28 +0300

    particle-dev.rb: use versioned url

--- Casks/particle-dev.rb
+++ Casks/particle-dev.rb
@@ -1,9 +1,10 @@
 cask :v1 => 'particle-dev' do
-  version :latest
-  sha256 :no_check
+  version '0.0.24'
+  sha256 'c93dc366bc782bc08317942a334a4b4092cca3917112ac8d3d85c031c3684712'
 
-  # amazonaws.com is the official download host per the vendor homepage
-  url 'https://s3.amazonaws.com/spark-website/particle-dev-mac.zip'
+  # github.com is the official download host per the vendor homepage
+  url "https://github.com/spark/spark-dev/releases/download/v#{version}/particle-dev-mac.zip"
+  appcast 'https://github.com/spark/spark-dev/releases.atom'
   name 'Particle Dev'
   homepage 'https://www.particle.io/dev'
   license :apache
