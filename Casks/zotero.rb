b49f849444a67c0a8a667a90cc9328992a0b5629
Paul <hnbc1.reg@gmail.com>
2015-07-21 10:53:10 -0500

    Upgrade Zotero to v4.0.27.7

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'zotero' do
-  version '4.0.27'
-  sha256 '935388773f85219a3905ceb9bb8929555a1fefed0a7b0dc98a66c452f7ff240f'
+  version '4.0.27.7'
+  sha256 'bb9b989f20638ca8f3e3250dfeed60ec46b254ed19fed6f70868bfdb790ae08c'
 
   url "https://download.zotero.org/standalone/#{version}/Zotero-#{version}.dmg"
   name 'Zotero'
