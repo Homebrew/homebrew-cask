f770c391420c2b624afae37a2914a0ef4f400b48
Josa Gesell <josa.gesell@ligadigital.com>
2015-07-22 16:26:11 +0200

    Add Stack Exchange Notifier 1.1

---
+++ Casks/stack-exchange-notifier.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'stack-exchange-notifier' do
+  version '1.1'
+  sha256 '849b543bc724e735e9a951721159b45a5284fde44c4cd8caa74c0e7eefb7e30c'
+
+  url 'http://hewgill.com/senotifier/stack-exchange-notifier-1.1.dmg'
+  name 'Stack Exchange Notifier'
+  homepage 'http://hewgill.com/senotifier/'
+  license :bsd
+
+  app 'Stack Exchange Notifier.app'
+end
