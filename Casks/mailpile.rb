c715c89c0b7b050701604b732d7a50cfd4e3a191
scribblemaniac <connor.deptuck@gmail.com>
2015-07-23 13:50:31 -0600

    mailpile.rb: Update url and license

--- Casks/mailpile.rb
+++ Casks/mailpile.rb
@@ -2,10 +2,10 @@ cask :v1 => 'mailpile' do
   version '0.5.1'
   sha256 'abbdeb41d37ac3b79a44f603784b2185dc1f95ae8d54a81e9cf89af262428cd1'
 
-  url 'https://www.mailpile.is/files/releases/Mailpile-Installer-Beta-III-0.5.1.dmg'
+  url "https://www.mailpile.is/files/releases/Mailpile-Installer-Beta-III-#{version}.dmg"
   name 'Mailpile'
   homepage 'https://www.mailpile.is/'
-  license :oss
+  license :gpl
 
   app 'Mailpile.app'
 end
