0a0052f6e8999c3d9661d046d663ec2d54d5ae6c
scribblemaniac <connor.deptuck@gmail.com>
2015-07-23 13:47:18 -0600

    coteditor.rb: Add old versions

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -1,14 +1,21 @@
 cask :v1 => 'coteditor' do
-  if MacOS.release == :lion
-    version '2.0.3'
-    sha256 'b656aa0b6526c89d7d52cf12b715cf529f8e5048c7e13720673c3e32318a26d8'
-  elsif MacOS.release >= :mountain_lion
+  if MacOS.release <= :snow_leopard
+    version '1.3.1'
+    sha256 '5c871bd9de30fc3c76fc66acb4ea258d4d3762ae341181d65a7ef1f8de4751c5'
+    # github.com is the official download host per the vendor homepage
+    url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}_For10.4.dmg"
+  elsif MacOS.release <= :lion
+    version '1.5.4'
+    sha256 '444133083698c7c94c2b029644f39a0e36982ae34c24745789fa890626188347'
+    # github.com is the official download host per the vendor homepage
+    url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg"
+  else
     version '2.1.6'
     sha256 'a42f6c4d706ffb4dc00610c209ea449d18eafcc916a9b9c299febdcda8ca9c4d'
+    # github.com is the official download host per the vendor homepage
+    url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg"
   end
 
-  # github.com is the official download host per the vendor homepage
-  url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg"
   appcast 'https://github.com/coteditor/CotEditor/releases.atom'
   name 'CotEditor'
   homepage 'http://coteditor.com/'
