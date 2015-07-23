d7067bb023e9aa4557c08e4c983135ebabd667d6
scribblemaniac <connor.deptuck@gmail.com>
2015-07-23 14:09:08 -0600

    korean-spelling-checker.rb: Switch to versioned url

--- Casks/korean-spelling-checker.rb
+++ Casks/korean-spelling-checker.rb
@@ -1,11 +1,12 @@
 cask :v1 => 'korean-spelling-checker' do
-  version :latest
-  sha256 :no_check
+  version '1.2.1'
+  sha256 'd06e8f563357bc122ab921503e227241646f3b3db932a374236886987ebb07ab'
 
-  url 'http://appletree.or.kr/automator/korean-spelling-checker-workflow.zip'
+  # github.com is the official download host per the vendor homepage
+  url "https://github.com/miname/Korean-Spelling-Checker-Workflow/archive/#{version}.tar.gz"
   name '한국어 맞춤법 검사기'
   homepage 'http://appletree.or.kr/google-chrome-extensions/'
-  license :gratis
+  license :mit
 
   service '✔ 선택한 글의 한국어 맞춤법 검사하기.workflow'
 end
