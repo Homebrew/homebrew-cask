4d70fb9b5f2717b9053bc03c91e397315c1b2782
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 21:24:30 +0300

    codebug.rb: use versioned url

--- Casks/codebug.rb
+++ Casks/codebug.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'codebug' do
-  version :latest
-  sha256 :no_check
+  version '0.9.4'
+  sha256 '2a568b2eff8145823b552668d0b1caa75a98077afe7692ccb69ca030508c86b9'
 
-  url 'http://codebugapp.com/downloads/Codebug.dmg'
+  url "http://codebugapp.com/downloads/Codebug#{version.gsub('.','-')}.zip"
   appcast 'http://codebugapp.com/update.xml'
   name 'Codebug'
   homepage 'http://www.codebugapp.com/'
