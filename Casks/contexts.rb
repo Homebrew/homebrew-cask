78901fea749c8f3175a7901435f790fed93efd30
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 21:36:10 +0300

    contexts.rb: use versioned url

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'contexts' do
-  version :latest
-  sha256 :no_check
+  version '1.9.1'
+  sha256 'ea276539cb4c4dd92bbfca3279143f0d48940323ad910dd38e91a4424d5bab0a'
 
-  url 'https://contexts.co/releases/Contexts.zip'
+  url "https://contexts.co/releases/Contexts-#{version}.zip"
   appcast 'https://contexts.co/appcasts/stable.xml'
   name 'Contexts'
   homepage 'https://contexts.co'
