7be63aef680ad47366438cab2b7066e54c2a6f4e
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 21:30:00 +0300

    comicbooklover.rb: use versioned url

--- Casks/comicbooklover.rb
+++ Casks/comicbooklover.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'comicbooklover' do
-  version :latest
-  sha256 :no_check
+  version '1.7_1529'
+  sha256 'fe77d4a579e5401d1749fe78f51e03b8630daa62cb6acea842e1ea7eec552804'
 
-  url 'https://www.bitcartel.com/downloads/comicbooklover.zip'
+  url "https://www.bitcartel.com/downloads/comicbooklover_#{version.sub(%r{.*_},'')}.zip"
   appcast 'https://www.bitcartel.com/appcast/comicbooklover-1.7-dsa.xml'
   name 'ComicBookLover'
   homepage 'https://www.bitcartel.com/comicbooklover/'
