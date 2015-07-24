5dd4a73ef7616cce55a1b2c7eae607f5afb0f2ff
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 21:40:08 +0300

    controllermate.rb: use versioned url

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -1,16 +1,16 @@
 cask :v1 => 'controllermate' do
-  version :latest
-  sha256 :no_check
+  version '4.9.7'
+  sha256 'dad06ea5627a4848a3cf983977565fc36a1d3dd75702a2fda97a68ca06b23c14'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url 'https://s3.amazonaws.com/orderedbytes/ControllerMate.dmg'
+  url "https://s3.amazonaws.com/orderedbytes/ControllerMate#{version.gsub('.','')}.zip"
   name 'ControllerMate'
   appcast 'http://www.orderedbytes.com/sparkle/appcast_cm460.xml',
           :sha256 => '680a8928983f72e588e92a71d83517ceaec5701c5d6ee70cb4a13671d14e793c'
   homepage 'http://www.orderedbytes.com/controllermate/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  pkg 'ControllerMate.pkg'
+  pkg '#temp#/ControllerMate.pkg'
 
   uninstall :pkgutil => 'com.orderedbytes.controllermate.*'
   zap       :delete => [
