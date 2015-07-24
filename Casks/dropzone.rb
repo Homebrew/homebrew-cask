6178c7545406233a0c476d0e4cd11e6e0bf4a617
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 22:04:02 +0300

    dropzone.rb: use versioned url

--- Casks/dropzone.rb
+++ Casks/dropzone.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'dropzone' do
-  version :latest
-  sha256 :no_check
+  version '3.5.2'
+  sha256 '98c754dd91f9dd3c05a220b93e0ab264434ffc97d9ef56f41bc282c16fa9a8dc'
 
-  url 'https://aptonic.com/dropzone3/latest'
+  url "https://aptonic.com/dropzone3/sparkle/Dropzone-#{version}.zip"
   appcast 'https://aptonic.com/dropzone3/sparkle/updates.xml'
   name 'Dropzone'
   homepage 'https://aptonic.com'
