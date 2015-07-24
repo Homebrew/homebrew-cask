905b0a5129722520a27cbf9f41bc6f6bd591de29
ftc2 <ftc2@users.noreply.github.com>
2015-07-24 18:18:27 -0500

    Upgrade Linphone to v3.8.5
    Linphone went from .dmg to .pkg with a gzip'd cpio payload inside of it.

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -1,14 +1,25 @@
 cask :v1 => 'linphone' do
-  version '3.8.1'
-  sha256 '925943867ee62c49450bf423479a8eb8cb42107e1319b4054233c4e4ac4a252c'
+  version '3.8.5'
+  sha256 '250d6b0b8ca3fd5029185b479199ae47b5d8c57062007a2bbb864ca63034a5de'
 
   # gnu.org is the official download host per the vendor homepage
-  url "http://download-mirror.savannah.gnu.org/releases/linphone/#{version.sub(%r{\d+$},'')}x/macos/linphone-#{version}.dmg"
+  url "http://download-mirror.savannah.gnu.org/releases/linphone/#{version.sub(%r{\d+$},'')}x/macos/linphone-#{version}.pkg"
   gpg "#{url}.sig",
       :key_id => '3ecd52dee2f56985'
   name 'Linphone'
   homepage 'https://www.linphone.org/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gpl
+
+  container :type => :xar
+
+  preflight do
+    system '/usr/bin/tar', '-xf', "#{staged_path}/linphone.pkg/Payload", '-C', staged_path
+  end
+
+  postflight do
+    system 'rm', '-rf', "#{staged_path}/linphone.pkg"
+    system 'rm', '-f', "#{staged_path}/Distribution"
+  end
 
   app 'Linphone.app'
 end
