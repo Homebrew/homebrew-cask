3e3291b5c389b42227bf54e278bd15cbdb6faf37
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 21:28:20 +0300

    coderunner.rb: use versioned url

--- Casks/coderunner.rb
+++ Casks/coderunner.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'coderunner' do
-  version :latest
-  sha256 :no_check
+  version '2.0.3'
+  sha256 'ca0daec87dde3520e1dc1a486674f3a6048acf4f23dbe5c86d39c467800f142e'
 
-  url 'https://coderunnerapp.com/download'
+  # cloudfront.net is the official download host per the vendor homepage
+  url "https://dktfof1z89xc1.cloudfront.net/CodeRunner%20#{version}.zip"
   name 'CodeRunner'
   appcast 'https://coderunnerapp.com/appcast.xml',
           :sha256 => 'bc0a476a000d50dfc0e4fe997838b13794c483dd1d41c85752b77c01c9a07594'
