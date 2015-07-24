ada1eb5d39cb5fe646d459ddee5721a22086e209
scribblemaniac <connor.deptuck@gmail.com>
2015-07-23 21:50:41 -0600

    seashore.rb: Removed version and checksum

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'seashore' do
-  version '0.5.1'
-  sha256 '96463a3642f162a20b160d8df273e9b27a5fdbf9708bee6ebf6a6c8528047765'
+  version :latest
+  sha256 :no_check
 
   url 'http://downloads.sourceforge.net/sourceforge/seashore/Seashore.zip'
   name 'Seashore'
