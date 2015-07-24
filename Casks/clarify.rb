48c3a71ca12ab1d74e6ecb795b724b83903a21d2
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 21:17:00 +0300

    clarify.rb: use versioned url

--- Casks/clarify.rb
+++ Casks/clarify.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'clarify' do
-  version :latest
-  sha256 :no_check
+  version '2.0.5r4'
+  sha256 '454bff1e655bd8aee1c5bcb3c85339b2d09a386637b1cdb0246ab849c6283654'
 
-  url 'http://www.clarify-it.com/download/mac/Clarify.dmg'
+  url "http://files.clarify-it.com/v2/updaters/#{version}/Clarify.app.zip"
   name 'Clarify'
   appcast 'http://www.bluemangolearning.com/download/clarify/2_0/auto_update/release/clarify_appcast.xml',
           :sha256 => '4e03cb35a628946e3ea21ba547be2488f792f74364198d365f8f69d9ce4c93cb'
