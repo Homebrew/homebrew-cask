f1532a514f7d61cf548ba343322c54f2ffe52ceb
Will Stamper <epmatsw@gmail.com>
2015-07-24 10:36:46 -0500

    Upgrade Calibre.app to v2.33.0

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -4,8 +4,8 @@ cask :v1 => 'calibre' do
     sha256 '0533283965fbc9a6618d0b27c85bdf3671fe75ff0e89eeff406fe1457ee61b14'
     url "http://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
   else
-    version '2.32.1'
-    sha256 '073944e4da985bbb9404b1013181d56e1c25191cde4579ab58171d66e61d4f51'
+    version '2.33.0'
+    sha256 '4f7e8e884d77512ea01cbd78143c6a44c8fddd8860b754a1053254aa1bcb7c6e'
 
     # github.com is an official download host per the vendor homepage, and a faster mirror than the main one
     url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}.dmg"
