4fb19f56c962d4623db29e1f33beeab851e06a6f
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-07-24 07:51:35 +0200

    Update beardedspice.rb

--- Casks/beardedspice.rb
+++ Casks/beardedspice.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'beardedspice' do
-  version '1.0.8'
-  sha256 '38078bc6c7e1a744643041f70a3380e43ef606ec614bd38f9cfa4a1895177b8a'
+  version :latest
+  sha256 :no_check
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com/beardedspice/beardedspice/releases/download/v#{version}/BeardedSpice-#{version}.zip"
+  url "https://raw.github.com/beardedspice/beardedspice/distr/publish/releases/BeardedSpice-latest.zip"
   appcast 'https://github.com/beardedspice/beardedspice/releases.atom'
   name 'BeardedSpice'
   homepage 'http://www.beardedspice.com'
