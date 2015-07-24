faa3ccdd00de6a37df3a6d9244d22416c6a10c9e
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 21:54:41 +0300

    delicious-library.rb: use versioned url

--- Casks/delicious-library.rb
+++ Casks/delicious-library.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'delicious-library' do
-  version :latest
-  sha256 :no_check
+  version '3.3.5'
+  sha256 '7f039110430f02013e217ab2ca6c3aba1922f795961ee744d8465d155f004104'
 
-  url 'https://delicious-monster.com/downloads/DeliciousLibrary3.zip'
+  url "https://delicious-monster.com/downloads/DeliciousLibrary3/v#{version}/DeliciousLibrary3.zip"
   name 'Delicious Library'
   appcast 'https://www.delicious-monster.com/downloads/DeliciousLibrary3.xml',
           :sha256 => '5f9eaea34e47cd1255f02a48ddca41433cd27aec146387a3221d1267b286c6e6'
