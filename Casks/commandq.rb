b08c8938a90aba9847a39582c7d7ee67d969dcf9
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 21:31:40 +0300

    commandq.rb: use versioned url

--- Casks/commandq.rb
+++ Casks/commandq.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'commandq' do
-  version :latest
-  sha256 :no_check
+  version '1.0.4'
+  sha256 '1183a2baf3775bd47851c94636ebe41356e4b53fce1dd35b4b70b9298294a107'
 
-  url 'https://clickontyler.com/commandq/download/'
+  url "http://dl.clickontyler.com/commandq/commandq_#{version}.zip"
   name 'CommandQ'
   appcast 'https://shine.clickontyler.com/appcast.php?id=16',
           :sha256 => 'aded9f4d5543c963c0989f53ba18c35b2053703b543b10a54c61a6b42b53dd50'
