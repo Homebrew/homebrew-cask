007de872e0b01c0592b6254824cec2245e7ff31d
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-24 22:00:43 +0300

    dradio.rb: use versioned url

--- Casks/dradio.rb
+++ Casks/dradio.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'dradio' do
-  version :latest
-  sha256 :no_check
+  version '1.6.1_1576'
+  sha256 '1a297d0ccdf1554caf421f8774d3e6342d1ce600cb70392ca1bc3e345b04e9ea'
 
-  url 'http://dradio.me/download'
+  url "http://dradio.me/updates/dRadio-#{version.sub(%r{.*_},'')}.zip"
   appcast 'http://dradio.me/updates/appcast.xml'
   name 'dRadio'
   homepage 'http://dradio.me'
