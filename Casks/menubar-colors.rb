19e51e2e9f477fdc18d2784c003551b7c53046f4
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-07-23 23:25:59 -0400

    Upgrade Menubar Colors to v2.0.0

--- Casks/menubar-colors.rb
+++ Casks/menubar-colors.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'menubar-colors' do
-  version '1.4.1'
-  sha256 '98f7136e01ac5e8da0d265258100ffda98d5e95f84d86bcb78d3345b2e2f56d6'
+  version '2.0.0'
+  sha256 'e150d4d0b2abe1183c38393ec4ea186b753925e3c1978aeebce34586e027feef'
 
   url "https://github.com/nvzqz/menubar-colors/releases/download/v#{version}/Menubar-Colors.zip"
   appcast 'https://github.com/nvzqz/menubar-colors/releases.atom'
