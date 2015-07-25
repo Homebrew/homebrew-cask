d1c604ed750c7b3d8aa8b09fbb5268be40c3f909
scribblemaniac <connor.deptuck@gmail.com>
2015-07-25 10:53:44 -0600

    pacifist.rb: Switch to versioned url and add older version

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -1,12 +1,23 @@
 cask :v1 => 'pacifist' do
-  version :latest
-  sha256 :no_check
+  if MacOS.release <= :snow_leopard || Hardware::CPU.is_32_bit?
+    version '3.0.10'
+    sha256 '86da910b133ac3b7a38d165624e2657f6286bcca76af98c80d3c34d5b3f3a851'
+  elsif MacOS.release <= :lion
+    version '3.2.17'
+    sha256 'd38e12293bc6087ddb09275e3c5ab34faa670e87e9dd41e04a587dd387f7b1d3'
+  else
+    version '3.5.4'
+    sha256 'ef29b9a325d38dd32ba2e7f5cbacc0b3f52aae6f299a61a743a55dc57fc0e4f0'
+  end
 
-  url 'https://www.charlessoft.com/cgi-bin/pacifist_download.cgi?type=dmg'
-  appcast 'https://www.charlessoft.com/cgi-bin/pacifist_sparkle.cgi'
+  url "https://www.charlessoft.com/pacifist_download/Pacifist_#{version}.dmg"
+  appcast 'https://www.charlessoft.com/cgi-bin/pacifist_sparkle.cgi',
+          :sha256 => '1968fd8009efc6192aa5a66e78dd002fb104a34402a22363e2702368f4c1cb64'
   name 'Pacifist'
   homepage 'https://www.charlessoft.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
+
+  depends_on :macos => '>= :tiger'
 
   app 'Pacifist.app'
 end
