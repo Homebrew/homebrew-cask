1670bda3d4134d9fd8ae635e8410e2cc1f0bdce2
scribblemaniac <connor.deptuck@gmail.com>
2015-07-25 11:00:30 -0600

    qcad.rb: Upgrade to v3.9.6

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -1,11 +1,14 @@
 cask :v1 => 'qcad' do
-  version '3.9.5'
-  sha256 '69c1ab1b8227192d20c625510f7bc5e69cfa8895229be15692362f50ac7b34ca'
+  version '3.9.6'
+  sha256 '8554ea510430866359110d569b4c1dc1ae1f17e054eaac6aa40f37aba11adc90'
 
   url "http://www.qcad.org/archives/qcad/qcad-#{version}-osx-10.6-10.10.dmg"
   name 'QCAD'
   homepage 'http://www.qcad.org/'
   license :gpl
+
+  depends_on :macos => '>= :snow_leopard'
+  depends_on :arch => :intel
 
   app 'QCAD.app'
 end
