2068499062232fcd3477d0c1a12c8501a838cbe4
scribblemaniac <connor.deptuck@gmail.com>
2015-07-25 13:14:14 -0600

    sparkbox.rb: Switch to versioned url

--- Casks/sparkbox.rb
+++ Casks/sparkbox.rb
@@ -1,12 +1,16 @@
 cask :v1 => 'sparkbox' do
-  version :latest
-  sha256 :no_check
+  version '1.2.4'
+  sha256 '1b5eb4fb54de0e3151405ab3f8d643b13188a68ea4a1a2a2a2f552cf7f138b30'
 
-  url 'http://t.icyblaze.com/sblatest'
-  appcast 'http://matrix.icyblaze.com/index.php/checkupdate/p/8'
+  # amazonaws.com is the official download host per the vendor homepage
+  url "http://s3.amazonaws.com/IcyBlaze-iDocument2/Download/sparkbox_#{version}.zip"
+  appcast 'http://matrix.icyblaze.com/index.php/checkupdate/p/8',
+          :sha256 => '689d0cb42cfc174417072972c37919522a9a0f63e170424f7486c7b77d148650'
   name 'Sparkbox'
   homepage 'http://www.icyblaze.com/sparkbox'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
+
+  depends_on :macos => '>= :lion'
 
   app 'Sparkbox.app'
 end
