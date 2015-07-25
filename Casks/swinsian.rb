9539be001081d738e0d61eb627434a8f2ce4192d
scribblemaniac <connor.deptuck@gmail.com>
2015-07-25 12:00:11 -0600

    swinsian.rb: Switch to versioned url

--- Casks/swinsian.rb
+++ Casks/swinsian.rb
@@ -1,9 +1,11 @@
 cask :v1 => 'swinsian' do
-  version :latest
-  sha256 :no_check
+  version '1.10.15'
+  sha256 '5ec2ca8a2c7bff1d51db86970b989d89ef6084e27c5fe76946d1e9d3227726f9'
 
-  url 'https://swinsian.com/sparkle/Swinsian.zip'
-  appcast 'https://www.swinsian.com/sparkle/sparklecast.xml'
+  url "http://www.swinsian.com/sparkle/Swinsian_#{version}.zip"
+  appcast 'https://www.swinsian.com/sparkle/sparklecast.xml',
+          :sha256 => '424eb73016e1a9d473beab4c0d1f0b2ed670a6386130317dcc44574ee096417d',
+          :format => :sparkle
   name 'Swinsian'
   homepage 'https://swinsian.com'
   license :commercial
