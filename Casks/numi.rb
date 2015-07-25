ecfd5e7c6e5498172a8c53cc0fe83b334dbfcab8
Jiashu Wang <wjs9889@gmail.com>
2015-07-25 13:28:25 -0700

    Update url and remove deprecated appcast.
    Add new Numi official download host comment.

--- Casks/numi.rb
+++ Casks/numi.rb
@@ -2,9 +2,8 @@ cask :v1 => 'numi' do
   version :latest
   sha256 :no_check
 
-  url 'http://numi.io/static/files/Numi.zip'
-  appcast 'http://numi.io/update.xml',
-          :sha256 => 'fc6e87987f66f478065ac353a270fa122fb329e85f5b7fd6cb256caefd22b7ed'
+  # devmate.com is the official download host per the vendor homepage
+  url 'http://dl.devmate.com/com.dmitrynikolaev.numi/beta/Numi.zip'
   name 'Numi'
   homepage 'http://numi.io/'
   license :gratis
