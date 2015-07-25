18b4d70d6a817ff74215280dc383b582fa610256
scribblemaniac <connor.deptuck@gmail.com>
2015-07-25 11:45:59 -0600

    iexplorer.rb: Upgrade to v3.7.7.0

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,13 +1,15 @@
 cask :v1 => 'iexplorer' do
-  version '3.7.5.0'
-  sha256 '724249be5603f224e52fa1bf20e6be11c92d30ff3422eb92521cb1e40d07ad93'
+  version '3.7.7.0'
+  sha256 '2c626a0cd473bb69f7ad7ab17830b992336f173641a0931acb1a3ba331b293b8'
 
   url "https://www.macroplant.com/release/iExplorer-#{version}.dmg"
   appcast 'https://www.macroplant.com/iexplorer/ie3-appcast.xml',
-          :sha256 => '7d204dc3e1c42c6f50e30f1175c859b515f708d86a406f040ed684e2a5fda59c'
+          :sha256 => '04cd9d71597efdf1d05a7e4f92f33ae7c1f26e7ad3f4f537c4ca64365c0e0e55'
   name 'iExplorer'
   homepage 'https://www.macroplant.com/iexplorer/'
   license :freemium
+
+  depends_on :macos => '>= :snow_leopard'
 
   app 'iExplorer.app'
 end
