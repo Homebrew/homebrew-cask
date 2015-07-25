ac05cdba131c58cd54a25697c9330924a4a99666
scribblemaniac <connor.deptuck@gmail.com>
2015-07-25 13:50:29 -0600

    powerphotos.rb: Upgrade to v1.0.6

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -1,14 +1,16 @@
 cask :v1 => 'powerphotos' do
-  version '1.0.0'
-  sha256 '449a3b6acb91e890c931045dcaae430b6daa7910c47a6a3fb4211cd3770a3d6f'
+  version '1.0.6'
+  sha256 '927c1095858d259b9469c86d20ce39cf0bfc350ad0b64ae8ba0ca0557b632305'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/fatcatsoftware/powerphotos/PowerPhotos_#{version.gsub('.', '')}.zip"
   name 'PowerPhotos'
   appcast 'http://www.fatcatsoftware.com/powerphotos/powerphotos_appcast.xml',
-          :sha256 => 'f7ac6bab7dd82bf9298a90e4a7727b3213e91b1ef73b0f9c8e13480842062ff8'
+          :sha256 => 'edf08e27f7b1a42e1da538a536631382e1c92e2dd148b4ff4cae93dbde48f25e'
   homepage 'http://www.fatcatsoftware.com/powerphotos/'
   license :commercial
+
+  depends_on :macos => '>= :yosemite'
 
   app 'PowerPhotos.app'
 end
