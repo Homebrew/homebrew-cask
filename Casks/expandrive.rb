f2ab70422e5469bfc2aa52838654e4b5985bcb59
scribblemaniac <connor.deptuck@gmail.com>
2015-07-25 11:53:28 -0600

    expandrive.rb: Switch to versioned url

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,12 +1,13 @@
 cask :v1 => 'expandrive' do
-  version :latest
-  sha256 :no_check
+  version '5.0.10'
+  sha256 '4702399f6eec284d2de8896653c0d6f18052435665677523798b0ed29b92278d'
 
-  url 'http://updates.expandrive.com/apps/expandrive/download_latest'
+  url "http://updates.expandrive.com/apps/expandrive/v/#{version.gsub('.','-')}/download.dmg"
   name 'ExpanDrive'
-  appcast 'http://updates.expandrive.com/appcast/expandrive.xml?version=3'
+  appcast 'http://updates.expandrive.com/appcast/expandrive.xml?version=3',
+          :sha256 => 'cc088ffa8171c2d8fde2404f00a31711d9e8ff07172f28aac8088b79f60404fc'
   homepage 'https://www.expandrive.com/expandrive'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'ExpanDrive.app'
 
