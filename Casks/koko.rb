d2c3d2244eea0c949b6270cc365dc61524491260
Alberto A. Colon Viera (Albertico) <aacv@alberti.co>
2015-07-26 14:24:39 -0400

    Upgrade Koko.app to v0.1.5 with new repo/maintainer

--- Casks/koko.rb
+++ Casks/koko.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'koko' do
-  version '0.1.1'
-  sha256 '0e39444b40c92eeee0488b1158c2845e7e738b276b91f6c3b0332801486a0375'
+  version '0.1.5'
+  sha256 '1a11817863d21039f02837567112d5e97a5edf6e08f214255ca05ddbf8e80766'
 
-  url "https://github.com/hachibasu/koko/releases/download/v#{version}/koko-mac.zip"
+  url "https://github.com/noraesae/koko/releases/download/v#{version}/koko-mac.zip"
   name 'Koko'
-  homepage 'https://github.com/hachibasu/koko'
+  homepage 'https://github.com/noraesae/koko'
   license :mit
 
   app 'Koko.app'
