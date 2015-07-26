8671dd363fd06b974dbf44f72dae90b532b34d14
Joel Kuzmarski <leoj3n@gmail.com>
2015-07-26 09:35:51 -0500

    Update groovesquid.rb

--- Casks/groovesquid.rb
+++ Casks/groovesquid.rb
@@ -2,10 +2,9 @@ cask :v1 => 'groovesquid' do
   version :latest
   sha256 :no_check
 
-  # github.com is the official download host per the vendor homepage
   url 'http://groovesquid.com/download.php?file=Groovesquid.dmg'
   name 'Groovesquid'
-  homepage 'https://groovesquid.com'
+  homepage 'https://groovesquid.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Groovesquid.app'
