d7be61e66fca7a1428f8cab0fce9bd5f15ea57c7
Joel Kuzmarski <leoj3n@gmail.com>
2015-07-26 09:31:24 -0500

    Update mirrordisplays.rb
    Remove referer

--- Casks/mirrordisplays.rb
+++ Casks/mirrordisplays.rb
@@ -3,8 +3,7 @@ cask :v1 => 'mirrordisplays' do
   sha256 :no_check
 
   # github.com is the official download host per the vendor homepage
-  url 'https://github.com/fcanas/mirror-displays/releases/download/1.0/MirrorDisplays.zip',
-    :referer => 'http://www.fabiancanas.com/Projects/MirrorDisplays'
+  url 'https://github.com/fcanas/mirror-displays/releases/download/1.0/MirrorDisplays.zip'
   name 'Mirror Displays'
   homepage 'http://www.fabiancanas.com/Projects/MirrorDisplays'
   appcast 'https://github.com/fcanas/mirror-displays/releases.atom'
