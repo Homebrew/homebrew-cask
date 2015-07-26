5125b62ba35ae120db0ead6fb74664bb5ca63e86
Joel Kuzmarski <leoj3n@gmail.com>
2015-07-26 08:17:54 -0500

    Update mirrordisplays.rb
    Add official comment and appcast

--- Casks/mirrordisplays.rb
+++ Casks/mirrordisplays.rb
@@ -2,10 +2,12 @@ cask :v1 => 'mirrordisplays' do
   version :latest
   sha256 :no_check
 
+  # github.com is the official download host per the vendor homepage
   url 'https://github.com/fcanas/mirror-displays/releases/download/1.0/MirrorDisplays.zip',
     :referer => 'http://www.fabiancanas.com/Projects/MirrorDisplays'
   name 'Mirror Displays'
   homepage 'http://www.fabiancanas.com/Projects/MirrorDisplays'
+  appcast 'https://github.com/fcanas/mirror-displays/releases.atom'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'MirrorDisplays.app'
