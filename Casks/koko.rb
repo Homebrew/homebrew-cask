690dfae575f5cf7cace480ac6edc1b889573e86d
Vítor Galvão <info@vitorgalvao.com>
2015-07-26 19:33:33 +0100

    koko.rb: add appcast

--- Casks/koko.rb
+++ Casks/koko.rb
@@ -3,6 +3,7 @@ cask :v1 => 'koko' do
   sha256 '1a11817863d21039f02837567112d5e97a5edf6e08f214255ca05ddbf8e80766'
 
   url "https://github.com/noraesae/koko/releases/download/v#{version}/koko-mac.zip"
+  appcast 'https://github.com/noraesae/koko/releases.atom'
   name 'Koko'
   homepage 'https://github.com/noraesae/koko'
   license :mit
