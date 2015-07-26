4ad855b137a1bccc68896b8853545ecde355ba8f
Joel Kuzmarski <leoj3n@gmail.com>
2015-07-26 08:12:27 -0500

    Update mirrordisplays.rb
    Fix download location

--- Casks/mirrordisplays.rb
+++ Casks/mirrordisplays.rb
@@ -2,7 +2,7 @@ cask :v1 => 'mirrordisplays' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.fabiancanas.com/downloads/MirrorDisplays.zip',
+  url 'https://github.com/fcanas/mirror-displays/releases/download/1.0/MirrorDisplays.zip',
     :referer => 'http://www.fabiancanas.com/Projects/MirrorDisplays'
   name 'Mirror Displays'
   homepage 'http://www.fabiancanas.com/Projects/MirrorDisplays'
