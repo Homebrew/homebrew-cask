943624587a7e6465a27d79d6a0499082b3a0a80b
Vítor Galvão <info@vitorgalvao.com>
2015-07-26 02:53:26 +0100

    telegram.rb: remove appcast

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -4,8 +4,6 @@ cask :v1 => 'telegram' do
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
-  appcast 'https://rink.hockeyapp.net/api/2/apps/c55f5e74ae5d0ad254df29f71a1b5f0e',
-          :sha256 => 'f15dbd6d38563e5e5bd66c71fd02c0e319642233e4853672760a55e6e390684f'
   name 'Telegram'
   homepage 'https://telegram.org/'
   license :gpl
