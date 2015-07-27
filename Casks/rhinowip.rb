83a860b96202b22ba14a885f761da1033ecba328
Doug Speckhard <dspeckhard@bcj.com>
2015-07-27 15:17:41 -0700

    Rhino for Mac WIP
    Progress builds of Rhino for Mac WIP version

---
+++ Casks/rhinowip.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'rhinowip' do
+  version '5B92w' 
+  sha256 :no_check
+
+  url 'http://files.mcneel.com/Releases/Rhino/5.0/Mac/RhinoWIP_5B92w.dmg'
+  name 'RhinoWIP'
+  homepage 'http://www.rhino3d.com/download/rhino-for-mac/5B/wip'
+  license :closed
+  app 'RhinoWIP.app'
+end
