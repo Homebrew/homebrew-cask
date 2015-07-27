49fc666be02bcb08dd504ce82b81ddf26254a065
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-27 21:01:53 +0300

    brackets.rb: upgrade to v1.4

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'brackets' do
-  version '1.3'
-  sha256 '8ea96c374fdee7e1db7300a78b03b7c1391e26a65b976d7c0532957877c657fb'
+  version '1.4'
+  sha256 'c725999990fb20bf86cd23febea192f78e691a3b63e8425c50001d92fbee8dc9'
 
   url "https://github.com/adobe/brackets/releases/download/release-#{version}/Brackets.Release.#{version}.dmg"
   appcast 'https://github.com/adobe/brackets/releases.atom'
