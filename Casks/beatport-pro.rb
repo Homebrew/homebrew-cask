5732d4d5df852976b59983883b570358d8bee6fb
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-27 20:57:32 +0300

    beatport-pro.rb: upgrade to v2.2.0_158

--- Casks/beatport-pro.rb
+++ Casks/beatport-pro.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'beatport-pro' do
-  version '2.1.6_155'
-  sha256 '21581878a33921082167ba4eaddf031edf5c0b30b36928412c844d8c7572cf63'
+  version '2.2.0_158'
+  sha256 '5fc4a6a607ca8a1beef2e69099eb57e6219bc98df0af26348d6d80bb1b3c1ab3'
 
   url "https://pro.beatport.com/mac/#{version}/beatportpro_#{version}.dmg"
   name 'Beatport'
   name 'Beatport Pro'
   appcast 'https://pro.beatport.com/mac/appcast.xml',
-          :sha256 => 'c20601420d6fc2935de514d8b31ec06399d2d52861f5d942fce00d9c6b1ee280'
+          :sha256 => '41b5e2bb1ff833e9d92211874d6d71a1636234a3b9a5fc0dd72b57a6d2dde35e'
   homepage 'https://pro.beatport.com/'
   license :closed
 
