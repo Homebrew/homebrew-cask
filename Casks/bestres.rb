f9f3f38a33d149fc12dc809407e246820ff1a26a
Decors <decors1215@gmail.com>
2015-07-28 02:06:08 +0900

    Add BestRes Latest

---
+++ Casks/bestres.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'bestres' do
+  version 'latest'
+  sha256 '1b706a3550edbc1411bdc4665490cce3b22cef68a6abae82af0fbd46370ce697'
+
+  url 'http://dl.devmate.com/com.icyberon.BestRes/BestRes.zip'
+  name 'BestRes'
+  homepage 'http://icyberon.com/bestres/'
+  license :unknown
+
+  app 'BestRes.app'
+end
