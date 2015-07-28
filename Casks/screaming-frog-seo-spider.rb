a54570a9809641bb635ae7169d06ed50867321dd
Daniel Zuccon <daniel@zuccon.com>
2015-07-28 16:21:38 +1000

    Add Screaming Frog SEO Spider.app v4.1

---
+++ Casks/screaming-frog-seo-spider.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'screaming-frog-seo-spider' do
+  version '4.1'
+  sha256 '6594f7792f04651f37dd55c2e7926649c4f27efc43e158b86f0f308ac81ee063'
+
+  url 'http://www.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-4.1.dmg'
+  name 'Screaming Frog SEO Spider'
+  name 'Screaming Frog'
+  homepage 'http://www.screamingfrog.co.uk/seo-spider/'
+  license :freemium
+
+  app 'Screaming Frog SEO Spider.app'
+end
