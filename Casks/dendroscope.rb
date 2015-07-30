df4b20f7e9246e975301d15ba2b66b90028e8d3f
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-30 23:32:23 +0300

    dendroscope.rb: add vendor homepage comment

--- Casks/dendroscope.rb
+++ Casks/dendroscope.rb
@@ -2,6 +2,7 @@ cask :v1 => 'dendroscope' do
   version '3.3.2'
   sha256 '21eccfc97aba6400ecb9d81f1bc02a1f8fc9c4cd07e6c09b5f310d414b00c7e2'
 
+  # tuebingen.de is the official download host per the vendor homepage
   url "http://ab.inf.uni-tuebingen.de/data/software/dendroscope3/download/Dendroscope_macos_#{version.gsub('.','_')}.dmg"
   name 'Dendroscope'
   homepage 'http://dendroscope.org/'
