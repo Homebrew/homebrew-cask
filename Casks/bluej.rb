48a383ac4c15db838d615ce611ceb66690636ffd
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-30 22:38:08 +0300

    bluej.rb: upgrade to v3.1.5

--- Casks/bluej.rb
+++ Casks/bluej.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bluej' do
-  version '3.1.4'
-  sha256 '507ce17037010e7a52684f97b32a07a391aca8fb1fa194748d35036242c87f80'
+  version '3.1.5'
+  sha256 'a9b30c1dcfd8f4245f22fb000a64ec09c1a8c353d4880cb818cc14b1f6fdf6ef'
 
   url "http://www.bluej.org/download/files/BlueJ-#{version.gsub('.', '')}.zip"
   name 'BlueJ'
