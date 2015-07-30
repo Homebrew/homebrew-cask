8e32be5a45201cad6ab895512054f59d6dcfb48b
Daniel Barajas <dbarajas@ooyala.com>
2015-07-29 17:17:20 -0700

    yourkit-java-profiler.rb: update to 2015-build-15068

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'yourkit-java-profiler' do
-  version '2015-build-15064'
-  sha256 'd008782cc5f5a68aaf7fc24abaf8014bd99ba6360e77d6ecfba282022a495d78'
+  version '2015-build-15068'
+  sha256 'f4724f9d8d97dec025d5cb6c15227ae14036bb7d152696b77023300f17628cd7'
 
   url "http://www.yourkit.com/download/yjp-#{version}-mac.zip"
   name 'YourKit Java Profiler'
