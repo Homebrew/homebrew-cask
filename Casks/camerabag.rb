c19a713f750c5ada823b2b7fbf83f6d5085c79c9
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-30 22:41:36 +0300

    camerabag.rb: upgrade to v2.7.01

--- Casks/camerabag.rb
+++ Casks/camerabag.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'camerabag' do
-  version '2.5.06'
-  sha256 'cbc34e49c94dfe0e9434c4495e970808b8498bdd7a62251dadcdb192595aff58'
+  version '2.7.01'
+  sha256 'e120208a17692ebcc5ad7c7c3fc27125fe9870530174b2872a2a3b3fdaff6163'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "http://downloads.nevercenter.com.s3.amazonaws.com/CameraBag_Mac_#{version.gsub('.','_')}.dmg"
