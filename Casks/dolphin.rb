433ac54aa0271ae438a3acf520b72ab9f3bc5fa4
Martin Lindhe <martin@ubique.se>
2015-07-30 17:11:57 +0200

    Dolphin: update to 4.0-7133

--- Casks/dolphin.rb
+++ Casks/dolphin.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'dolphin' do
-  version '4.0-7010'
-  sha256 'b23dde678572ddc3d7f91631b105ac08df64f1dbfcade9cf37c21937755e9fa9'
+  version '4.0-7133'
+  sha256 'e05048611113197f9c006b7a200ffd88d6b0ddc5a6308d8b80910af589a87142'
 
   url "https://dl.dolphin-emu.org/builds/dolphin-master-#{version}.dmg"
   name 'Dolphin'
