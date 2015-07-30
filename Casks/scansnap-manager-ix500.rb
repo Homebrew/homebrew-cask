de3004935342d20285e8c08c28a59a986824ebde
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-30 22:21:42 +0300

    scansnap-manager-ix500.rb: update vendor

--- Casks/scansnap-manager-ix500.rb
+++ Casks/scansnap-manager-ix500.rb
@@ -7,7 +7,7 @@ cask :v1 => 'scansnap-manager-ix500' do
   name 'ScanSnap Manager for Fujitsu ScanSnap iX500'
   homepage 'https://www.fujitsu.com/global/support/products/computing/peripheral/scanners/scansnap/software/ix500.html'
   license :gratis
-  tags :vendor => 'YourKit'
+  tags :vendor => 'Fujitsu'
 
   pkg 'ScanSnap Manager.pkg'
 
