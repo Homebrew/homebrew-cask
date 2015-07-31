25f909680957c336f623b5988ed5f231e9ef177b
Sergio Vilar <sergiovilar.r@gmail.com>
2015-07-30 23:01:21 -0300

    Updated HackHands to 1.4.8

--- Casks/hackhands.rb
+++ Casks/hackhands.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'hackhands' do
 
-  version '1.4.2'
-  sha256 '23e72f0e57f5384893744accf0ea9e977cf8d45cd79966d86bbb8c625673416a'
+  version '1.4.8'
+  sha256 'd91ff1cda8d6e4d9c4e892e8ad1f6ab67968b7d2db53de4c8f8b6e5122a601e8'
 
   url "http://desktop.hackhands.com.s3-website-us-west-1.amazonaws.com/osx/#{version}/HackHands.zip"
   name 'HackHands'
