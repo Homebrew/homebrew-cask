e295a8676cc6db35c21d326253c356e5516b8850
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-31 04:59:16 +0300

    haskell-platform.rb: upgrade to v7.10.2

--- Casks/haskell-platform.rb
+++ Casks/haskell-platform.rb
@@ -1,13 +1,13 @@
 cask :v1 => 'haskell-platform' do
-  version '2014.2.0.0'
-  sha256 '62f39246ad95dd2aed6ece5138f6297f945d2b450f215d074820294310e0c48a'
+  version '7.10.2'
+  sha256 'f6a884b6304a15056d1692ba419a6d00e883c4eee998f4f4d8b4ace3d160b54b'
 
-  url "https://www.haskell.org/platform/download/#{version}/Haskell%20Platform%20#{version}%2064bit.signed.pkg"
+  url "https://www.haskell.org/platform/download/#{version}/Haskell%20Platform%20#{version}%2064bit-signed.pkg"
   name 'Haskell Platform'
   homepage 'https://www.haskell.org/platform/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  pkg "Haskell Platform #{version} 64bit.signed.pkg"
+  pkg "Haskell Platform #{version} 64bit-signed.pkg"
 
   uninstall :script => { :executable => '/usr/bin/uninstall-hs', :args => %w[all --remove] },
             :pkgutil => 'org.haskell.HaskellPlatform.*'
