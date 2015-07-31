cb238ee5ae1783d0f866af25d6f1267ff78b3b45
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-31 05:00:32 +0300

    haskell-platform.rb: update license and add depends_on

--- Casks/haskell-platform.rb
+++ Casks/haskell-platform.rb
@@ -5,10 +5,12 @@ cask :v1 => 'haskell-platform' do
   url "https://www.haskell.org/platform/download/#{version}/Haskell%20Platform%20#{version}%2064bit-signed.pkg"
   name 'Haskell Platform'
   homepage 'https://www.haskell.org/platform/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :bsd
 
   pkg "Haskell Platform #{version} 64bit-signed.pkg"
 
   uninstall :script => { :executable => '/usr/bin/uninstall-hs', :args => %w[all --remove] },
             :pkgutil => 'org.haskell.HaskellPlatform.*'
+
+  depends_on :macos => '>= :snow_leopard'
 end
