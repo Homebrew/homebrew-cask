e434a26785c758a77a1e3d2589b9c181ec169a2e
Andrew Saint <andrew.james.saint@gmail.com>
2015-07-31 21:21:13 +0100

    Update MAMP to version 3.4

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mamp' do
-  version '3.3'
-  sha256 'fb50cfbf54c82f92808ae46bef50944a1c40fc607b43ad1767c78edc18d0b1d1'
+  version '3.4'
+  sha256 '4351c048f770b99bc69da6d5240e7b1cff2dbaef485dec7e04327187f3d8df55'
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
   name 'MAMP'
