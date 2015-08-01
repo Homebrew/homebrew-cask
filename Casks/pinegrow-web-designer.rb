dc241d423e5f1ca01c243c84c069a2d6324a10cf
scribblemaniac <connor.deptuck@gmail.com>
2015-08-01 14:03:25 -0600

    pinegrow-web-designer.rb: Upgrade to v2.2

--- Casks/pinegrow-web-designer.rb
+++ Casks/pinegrow-web-designer.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pinegrow-web-designer' do
-  version '2.1'
-  sha256 '983fd7974606981c005631f41985e62cdf1a851fa2da19eeb6664335875ad2b4'
+  version '2.2'
+  sha256 'c0d1b9595963b8080298261c4d9744237427e5d2ed02d38d38fbdbcbb43a7f1a'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://pinegrow.s3.amazonaws.com/PinegrowMac.#{version}.dmg"
