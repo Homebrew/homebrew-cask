44fedb5cd528cd8946b07df58a8021a738d5cbe3
scribblemaniac <connor.deptuck@gmail.com>
2015-08-01 13:56:45 -0600

    tower.rb: Upgrade to v2.2.3

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'tower' do
-  version '2.2.2-284'
-  sha256 'f2336a0565a99436b9159b7d066f30f136b6fe72f803ab354e678fe0fbc351fd'
+  version '2.2.3-285-e057eb4f'
+  sha256 'c9d79a6a8bfc298f38a2d7dbe724d71468f86c1de09fabd1d9da754fd8a40f74'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "https://fournova-app-updates.s3.amazonaws.com/apps/tower2-mac/284-05b8a99e/Tower-2-#{version}.zip"
-  appcast 'https://updates.fournova.com/updates/tower2-mac/stable'
+  url "https://fournova-app-updates.s3.amazonaws.com/apps/tower#{version.to_i}-mac/#{version.sub(%r{^.*?-},'')}/Tower-#{version.to_i}-#{version.sub(%r{-[^-]*$},'')}.zip"
+  appcast "https://updates.fournova.com/updates/tower#{version.to_i}-mac/stable",
+          :sha256 => 'ac2bfa663be13048eaa4f2d714da0849e9bd7a125bef1a1f6218738a981efbae'
   name 'Tower'
   homepage 'http://www.git-tower.com/'
   license :commercial
@@ -13,9 +14,9 @@ cask :v1 => 'tower' do
   binary 'Tower.app/Contents/MacOS/gittower'
 
   zap :delete => [
-                  '~/Library/Application Support/com.fournova.Tower2',
-                  '~/Library/Caches/com.fournova.Tower2',
-                  '~/Library/Preferences/com.fournova.Tower2.plist',
+                  "~/Library/Application Support/com.fournova.Tower#{version.to_i}",
+                  "~/Library/Caches/com.fournova.Tower#{version.to_i}",
+                  "~/Library/Preferences/com.fournova.Tower#{version.to_i}.plist",
                  ]
 
   caveats do
