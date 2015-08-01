ff96ffdcabbbee7708d956353156d71f508a5246
scribblemaniac <connor.deptuck@gmail.com>
2015-08-01 12:56:46 -0600

    minco.rb: Upgrade to v2.0.22

--- Casks/minco.rb
+++ Casks/minco.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'minco' do
-  version '2.0.20'
-  sha256 'e4717098cb229e7ab0324bbe4dc700be4c463cc18d574e9c1fe3c4a8346b2e4f'
+  version '2.0.22'
+  sha256 'bf98d80e0273552559ae7b98ba411d62ccdca66ddd9f2f274bd2aa2cdcd44c58'
 
   # webpack.com is the official download host per the appcast feed
   url "https://ssl.webpack.de/celmaro.com/updates/minco2/Minco#{version.gsub('.','')}.zip"
   appcast 'https://ssl.webpack.de/celmaro.com/updates/minco2/minco.xml',
-          :sha256 => 'd1a43c75492761dec8d0bb8dd9f0226baa54df2ba6198334d962681000e6c0e4'
+          :sha256 => '3ab9e6d0a6e9ebecfeab805c399a7194fe2a88bdcc1b096446e9f8be7e7195f8'
   name 'Minco'
   homepage 'http://www.celmaro.com/minco/'
   license :commercial
