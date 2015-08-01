889218c56fe524ec0d3da1eacd9bfbd3006367a7
scribblemaniac <connor.deptuck@gmail.com>
2015-08-01 10:56:34 -0600

    slack.rb: Upgrade to v1.1.3

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'slack' do
-  version '1.1.1'
-  sha256 'e8266e75d250c6d2d732e41af7f0c5045f542396f60cb7693c64cf46693c7a87'
+  version '1.1.3'
+  sha256 '9b87c38c17e124b8753a5a2310de4d820fd023c025e938bbb3eb8b2062589ec6'
 
   # hockeyapp.net is the official download host per the appcast feed
-  url 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1/app_versions/13?format=zip&amp;avtoken=a300ec64cfc37d3d9c52882cfcb8acb21658a0a5'
+  url 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1/app_versions/15?format=zip&avtoken=8216365d9a223bf0a1656302aff5751d758cf473'
   appcast 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1',
-          :sha256 => 'e21d63e8c1f76fb09ba7d04c2a48bc8d5bb2cc5787ed0396fc2e9ba3a19969d8'
+          :sha256 => '861020e5e39f1c87fce3b5a1cd67b02845e0e0a7937ec818e8978c145d659f22'
   name 'Slack'
   homepage 'https://slack.com/'
   license :gratis
