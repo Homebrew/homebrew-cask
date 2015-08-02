6502a7b8ffd57caf1c3ae7c1b498f64746e4b19a
Josh Hagins <hagins.josh@gmail.com>
2015-08-02 12:38:42 -0400

    Update mamp to use set_ownership helper

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -8,9 +8,9 @@ cask :v1 => 'mamp' do
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   pkg "MAMP_MAMP_PRO_#{version}.pkg"
+
   postflight do
-    system '/usr/bin/sudo', '-E', '--',
-           '/usr/sbin/chown', '-R', '--', "#{Etc.getpwuid(Process.euid).name}:staff", '/Applications/MAMP', '/Applications/MAMP PRO'
+    set_ownership ['/Applications/MAMP', '/Applications/MAMP PRO']
   end
 
   uninstall :pkgutil => 'de.appsolute.installer.(mamp|mampacticon|mampendinstall|mamppro).pkg',
