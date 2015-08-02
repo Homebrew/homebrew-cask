6571b18d5ef5569420624e3073f72d203139b94f
Vítor Galvão <info@vitorgalvao.com>
2015-08-02 20:54:13 +0100

    colorpicker-propicker.rb: update license

--- Casks/colorpicker-propicker.rb
+++ Casks/colorpicker-propicker.rb
@@ -5,7 +5,7 @@ cask :v1 => 'colorpicker-propicker' do
   url "http://www.irradiated.net/appcasts/pro-picker/releases/#{version}/ProPicker.zip"
   name 'Pro Picker'
   homepage 'http://www.irradiated.net/?page=pro-picker'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   colorpicker 'ProPicker.colorPicker'
 end
