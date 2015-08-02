628eccac2cb221a11ab10a3f3b2eacb9e0becec6
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-02 21:16:57 +0300

    dropzone.rb: upgrade to v3.5.4

--- Casks/dropzone.rb
+++ Casks/dropzone.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'dropzone' do
-  version '3.5.3'
-  sha256 '79a3c4dde4378cbfef3480048b9e72bbeeae43304f14a6a23160b0501a564516'
+  version '3.5.4'
+  sha256 '4595172ca1ccdef7882dd856f7ef2fbda6419689f506b8dc9e5c56f39b11378d'
 
   url "https://aptonic.com/dropzone3/sparkle/Dropzone-#{version}.zip"
   appcast 'https://aptonic.com/dropzone3/sparkle/updates.xml'
