a087fc732e1e96fbe54b3b95e8aaa910544c9bfd
Daehyun Y <hyuna917@me.com>
2015-08-04 00:04:53 +0900

    add cask files: aos-firewall.rb, cross-web.rb, ip-inside-agent-citi.rb, ip-inside-agent-hana.rb, veraport.rb, xecure-web-unified.rb

---
+++ Casks/aos-firewall.rb
@@ -0,0 +1,27 @@
+cask :v1 => "aos-firewall" do
+  version :latest
+  sha256 :no_check
+
+  url "http://ahnlabdownload.nefficient.co.kr/aos/sup/aosfirewall.dmg"
+  name "AOS Firewall"
+  homepage "http://www.ahnlab.com"
+  license :gratis
+
+  pkg "AOSFirewall.pkg"
+  uninstall :pkgutil =>
+    [
+        # "com.ahnlab.firewallStatus",
+        "com.ahnlab.firewall*"
+    ],
+  :trash =>
+    [
+        "/Applications/AhnLab/ASP/Firewall/Uninstaller.app"
+    ]
+
+  zap :trash =>
+    [
+      "~/Library/Application Support/ahnlabfwStatus*",
+      "~/Library/Preferences/Uninstaller.plist",
+      "~/Library/Saved Application State/Uninstaller.savedState"
+    ]
+end
