2943c08faa6285bed7b54872fa8d0f0318797c64
Daehyun Y <hyuna917@me.com>
2015-08-04 01:42:17 +0900

    fix bug: bad double quotes to single one

--- Casks/xecure-web-unified.rb
+++ Casks/xecure-web-unified.rb
@@ -3,7 +3,7 @@ cask :v1 => "xecure-web-unified" do
   sha256 :no_check
 
   url "https://open.hanabank.com/softforum/xwuni/install/xw_unified_install_mac_universal.pkg"
-  name "XecureWeb Unified"
+  name 'XecureWeb Unified'
   homepage "https://open.hanabank.com"
   license :gratis
 
