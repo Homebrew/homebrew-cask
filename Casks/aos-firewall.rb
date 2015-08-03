d9a73542132aee419410bf395767571d5aeed175
Daehyun Y <hyuna917@me.com>
2015-08-04 08:16:50 +0900

    fix bug: bad double quotes to single one

--- Casks/aos-firewall.rb
+++ Casks/aos-firewall.rb
@@ -1,16 +1,16 @@
-cask :v1 => "aos-firewall" do
+cask :v1 => 'aos-firewall' do
   version :latest
   sha256 :no_check
 
-  url "http://ahnlabdownload.nefficient.co.kr/aos/sup/aosfirewall.dmg"
+  url 'http://ahnlabdownload.nefficient.co.kr/aos/sup/aosfirewall.dmg'
   name 'AOS Firewall'
-  homepage "http://www.ahnlab.com"
+  homepage 'http://www.ahnlab.com'
   license :gratis
 
-  pkg "AOSFirewall.pkg"
+  pkg 'AOSFirewall.pkg'
   uninstall :pkgutil =>
     [
-      "com.ahnlab.aosFirewall.*",
-      "com.ahnlab.firewall.*"
+      'com.ahnlab.aosFirewall.*',
+      'com.ahnlab.firewall.*'
     ]
 end
--- Casks/veraport.rb
+++ Casks/veraport.rb
@@ -1,13 +1,13 @@
-cask :v1 => "veraport" do
+cask :v1 => 'veraport' do
   version :latest
   sha256 :no_check
 
-  url "https://open.citibank.co.kr/3rdParty/wizvera/veraport/down/veraport.pkg"
+  url 'https://open.citibank.co.kr/3rdParty/wizvera/veraport/down/veraport.pkg'
   name 'VeraPort'
-  homepage "https://open.citibank.co.kr"
+  homepage 'https://open.citibank.co.kr'
   license :gratis
 
-  pkg "veraport.pkg"
-  uninstall :pkgutil => "com.wizvera.veraport.veraport.*",
-    :rmdir => "/Applications/Veraport"
+  pkg 'veraport.pkg'
+  uninstall :pkgutil => 'com.wizvera.veraport.veraport.*',
+    :rmdir => '/Applications/Veraport'
 end
