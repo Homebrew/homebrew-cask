b0c4a8d7e0a2273f37d2059fad727d9ff8764de0
Daehyun Y <hyuna917@me.com>
2015-08-04 08:16:50 +0900

    fix bug: bad double quotes to single one

--- Casks/veraport.rb
+++ Casks/veraport.rb
@@ -1,13 +1,19 @@
-cask :v1 => "veraport" do
+cask :v1 => 'veraport' do
   version :latest
   sha256 :no_check
 
+<<<<<<< e6453c8a012ff9f27e642f0d50c29d98691cf50c
   url "https://open.citibank.co.kr/3rdParty/wizvera/veraport/down/veraport.pkg"
   name "VeraPort"
   homepage "https://open.citibank.co.kr"
+=======
+  url 'https://open.citibank.co.kr/3rdParty/wizvera/veraport/down/veraport.pkg'
+  name 'VeraPort'
+  homepage 'https://open.citibank.co.kr'
+>>>>>>> fix bug: bad double quotes to single one
   license :gratis
 
-  pkg "veraport.pkg"
-  uninstall :pkgutil => "com.wizvera.veraport.veraport.*",
-    :rmdir => "/Applications/Veraport"
+  pkg 'veraport.pkg'
+  uninstall :pkgutil => 'com.wizvera.veraport.veraport.*',
+    :rmdir => '/Applications/Veraport'
 end
