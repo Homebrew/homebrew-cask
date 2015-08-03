ba8929b5380b83cd77e292b582675c000020e729
Daehyun Y <hyuna917@me.com>
2015-08-04 03:27:20 +0900

    fix bug: uninstall

--- Casks/ip-inside-agent-for-hana-bank.rb
+++ Casks/ip-inside-agent-for-hana-bank.rb
@@ -1,12 +1,13 @@
-cask :v1 => "ip-inside-agent-for-hana-bank" do
+cask :v1 => 'ip-inside-agent-for-hana-bank' do
   version :latest
   sha256 :no_check
 
-  url "https://open.hanabank.com/interezen/agent/IPinsideAgent.dmg"
+  url 'https://open.hanabank.com/interezen/agent/IPinsideAgent.dmg'
   name 'IP inside Agent for Hana Bank'
-  homepage "https://open.hanabank.com"
+  homepage 'https://open.hanabank.com'
   license :gratis
 
-  pkg "IPinsideAgent.pkg"
-  uninstall :script => "/opt/homebrew-cask/Caskroom/ip-inside-agent-for-hana-bank/latest/IPinside Uninstaller.app/Contents/MacOS/IPinside Uninstaller"
+  pkg 'IPinsideAgent.pkg'
+  uninstall :pkgutil => 'I3GManager.Plugin.ipinsideAgent.*',
+    :rmdir => '/Applications/IPinside'
 end
