2fb28f397bcd776ababb1539784893a458348a8a
Daehyun Y <hyuna917@me.com>
2015-08-04 01:42:17 +0900

    fix bug: bad double quotes to single one

--- Casks/cross-web.rb
+++ Casks/cross-web.rb
@@ -3,7 +3,7 @@ cask :v1 => "cross-web" do
   sha256 :no_check
 
   url "https://open.citibank.co.kr/3rdParty/initech/plugin/down/npCrossWeb_Mac.pkg"
-  name "Cross WEB"
+  name 'Cross WEB'
   homepage "https://open.citibank.co.kr"
   license :gratis
 
