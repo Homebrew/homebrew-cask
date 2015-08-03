7281ee62be8642b506a1fe54ed40fb18d4303331
Daehyun Y <hyuna917@me.com>
2015-08-04 00:04:53 +0900

    add cask files: aos-firewall.rb, cross-web.rb, ip-inside-agent-citi.rb, ip-inside-agent-hana.rb, veraport.rb, xecure-web-unified.rb

---
+++ Casks/xecure-web-unified.rb
@@ -0,0 +1,12 @@
+cask :v1 => "xecure-web-unified" do
+  version :latest
+  sha256 :no_check
+
+  url "https://open.hanabank.com/softforum/xwuni/install/xw_unified_install_mac_universal.pkg"
+  name "XecureWeb Unified"
+  homepage "https://open.hanabank.com"
+  license :gratis
+
+  pkg "xw_unified_install_mac_universal.pkg"
+  uninstall :script => "/Applications/XecureWebUnified/Uninstaller.app/Contents/MacOS/Uninstaller"
+end
