6e4170082a0f58f5cc5aec00c2e023a7a997f082
Daehyun Y <hyuna917@me.com>
2015-08-04 01:44:55 +0900

    change file names
    xecure-web-unified.rb to xecureweb-unified.rb
    ip-inside-agent-citi.rb to ip-inside-agent-for-citi-bank.rb
    ip-inside-agent-hana.rb to ip-inside-agent-for-hana-bank.rb

--- Casks/ip-inside-agent-for-hana-bank.rb
+++ Casks/ip-inside-agent-for-hana-bank.rb
@@ -1,13 +1,12 @@
-cask :v1 => 'ip-inside-agent-for-hana-bank' do
+cask :v1 => "ip-inside-agent-for-hana-bank" do
   version :latest
   sha256 :no_check
 
-  url 'https://open.hanabank.com/interezen/agent/IPinsideAgent.dmg'
+  url "https://open.hanabank.com/interezen/agent/IPinsideAgent.dmg"
   name 'IP inside Agent for Hana Bank'
-  homepage 'https://open.hanabank.com'
+  homepage "https://open.hanabank.com"
   license :gratis
 
-  pkg 'IPinsideAgent.pkg'
-  uninstall :pkgutil => 'I3GManager.Plugin.ipinsideAgent.*',
-    :rmdir => '/Applications/IPinside'
+  pkg "IPinsideAgent.pkg"
+  uninstall :script => "/opt/homebrew-cask/Caskroom/ip-inside-agent-for-hana-bank/latest/IPinside Uninstaller.app/Contents/MacOS/IPinside Uninstaller"
 end
