bc420453050194a3acb04b3d78575f76543e91b9
Daehyun Y <hyuna917@me.com>
2015-08-04 00:04:53 +0900

    add cask files: aos-firewall.rb, cross-web.rb, ip-inside-agent-citi.rb, ip-inside-agent-hana.rb, veraport.rb, xecure-web-unified.rb

--- Casks/cross-web.rb
+++ Casks/cross-web.rb
@@ -2,11 +2,11 @@ cask :v1 => 'cross-web' do
   version :latest
   sha256 :no_check
 
-  url "https://open.citibank.co.kr/3rdParty/initech/plugin/down/npCrossWeb_Mac.pkg"
+  url 'https://open.citibank.co.kr/3rdParty/initech/plugin/down/npCrossWeb_Mac.pkg'
   name 'Cross WEB'
-  homepage "https://open.citibank.co.kr"
+  homepage 'https://open.citibank.co.kr'
   license :gratis
 
-  pkg "npCrossWeb_Mac.pkg"
-  uninstall :pkgutil => "kr.co.iniline.pkg.CrossWebPackage"
+  pkg 'npCrossWeb_Mac.pkg'
+  uninstall :pkgutil => 'kr.co.iniline.pkg.CrossWebPackage'
 end
