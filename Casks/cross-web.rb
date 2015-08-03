8510574b1e68dac66308175cce70d8f9d734c663
Daehyun Y <hyuna917@me.com>
2015-08-04 08:16:50 +0900

    fix bug: bad double quotes to single one

--- Casks/cross-web.rb
+++ Casks/cross-web.rb
@@ -1,4 +1,4 @@
-cask :v1 => "cross-web" do
+cask :v1 => 'cross-web' do
   version :latest
   sha256 :no_check
 
--- Casks/ip-inside-agent-for-hana-bank.rb
+++ Casks/ip-inside-agent-for-hana-bank.rb
@@ -1,4 +1,4 @@
-cask :v1 => "ip-inside-agent-for-hana-bank" do
+cask :v1 => 'ip-inside-agent-for-hana-bank' do
   version :latest
   sha256 :no_check
 
