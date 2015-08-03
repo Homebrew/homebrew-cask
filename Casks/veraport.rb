3f992c56a71962c95c11fa3dea368abdc8e5d34d
Daehyun Y <hyuna917@me.com>
2015-08-04 00:04:53 +0900

    add cask files: aos-firewall.rb, cross-web.rb, ip-inside-agent-citi.rb, ip-inside-agent-hana.rb, veraport.rb, xecure-web-unified.rb

--- Casks/veraport.rb
+++ Casks/veraport.rb
@@ -1,3 +1,4 @@
+<<<<<<< 62d593f143f615aa1ca8c0bfb6112ca3492e295c
 cask :v1 => 'veraport' do
   version :latest
   sha256 :no_check
@@ -10,4 +11,17 @@ cask :v1 => 'veraport' do
   pkg 'veraport.pkg'
   uninstall :pkgutil => 'com.wizvera.veraport.veraport.*',
     :rmdir => '/Applications/Veraport'
+=======
+cask :v1 => "veraport" do
+  version :latest
+  sha256 :no_check
+
+  url "https://open.citibank.co.kr/3rdParty/wizvera/veraport/down/veraport.pkg"
+  name "VeraPort"
+  homepage "https://open.citibank.co.kr"
+  license :gratis
+
+  pkg "veraport.pkg"
+  uninstall :script => "/Applications/Veraport/Uninstaller.app/Contents/MacOS/Uninstaller"
+>>>>>>> add cask files: aos-firewall.rb, cross-web.rb, ip-inside-agent-citi.rb, ip-inside-agent-hana.rb, veraport.rb, xecure-web-unified.rb
 end
