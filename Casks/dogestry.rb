99bf6412058bdcc17e23171fc283b3fce7b4c499
Jonathan Owens <niralisse+github@gmail.com>
2015-08-04 10:01:44 -0700

    Upgrade to Dogestry 2.0.2

--- Casks/dogestry.rb
+++ Casks/dogestry.rb
@@ -1,6 +1,6 @@
 cask :v1_1 => 'dogestry' do
-  version '2.0.1'
-  sha256 '71a33b0c0b9c432df998d235693a765cdcefb0c7cefe9a45653a9f673da9349e'
+  version '2.0.2'
+  sha256 'ddbf2d08480c3a78a901d6c2a82c866f6d6d900dc1f6febefc02703318a581e3'
 
   url "https://github.com/dogestry/dogestry/releases/download/v#{version}/dogestry-darwin-#{version}"
   appcast 'https://github.com/dogestry/dogestry/releases.atom'
