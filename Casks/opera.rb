00bd7fe86ea91d1dbdab872254abe6a17b4cc98d
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-04 21:01:21 +0300

    opera.rb: upgrade to v31.0.1889.99

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera' do
-  version '30.0.1835.125'
-  sha256 'a270ae9c1b08da29615e7dff4828ce7698ea70e830cde2bb4f257831de85723c'
+  version '31.0.1889.99'
+  sha256 '607a7050f3c7114798946a185cafefc716a0ec2a8e2bd0820f63998ecf58e4f2'
 
   url "http://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
