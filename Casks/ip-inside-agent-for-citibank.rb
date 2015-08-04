d2527f81dfe5f9e30ecf90564a13c2bd4b9e4231
Daehyun Y <hyuna917@me.com>
2015-08-04 17:56:04 +0900

    change name "citi bank" to "citibank"

--- Casks/ip-inside-agent-for-citibank.rb
+++ Casks/ip-inside-agent-for-citibank.rb
@@ -3,7 +3,7 @@ cask :v1 => 'ip-inside-agent-for-citibank' do
   sha256 :no_check
 
   url 'https://open.citibank.co.kr/3rdParty/interezen/OSX-MAC/IPinsideAgent.dmg'
-  name 'IP inside Agent for Citi Bank'
+  name 'IP inside Agent for CitiBank'
   homepage 'https://open.citibank.co.kr'
   license :gratis
 
