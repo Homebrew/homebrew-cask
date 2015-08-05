16a4fa1af095d3ddb0974d2cdddbb63219b2b418
Geoffrey Oxberry <goxberry@gmail.com>
2015-08-04 23:57:49 -0700

    Paraview: compute more of URL from version.
    For the past several versions, Paraview has used the "major.minor"
    string (e.g., "4.1", "4.2", "4.3") in the download URLs of its
    binaries.  This commit computes this information from the version
    number and interpolates it directly into the download URL to prevent
    transcription errors.

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,12 +1,14 @@
 cask :v1 => 'paraview' do
   version '4.3.1'
 
+  major_minor = version.split('.')[0..1].join('.')
+
   if MacOS.release == :snow_leopard
     sha256 'f968263782d1407c769c8d7b28872a43adb263d0820c9bebe0e0ea8cf2aaa3a0'
-    url "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.3&type=binary&os=osx&downloadFile=ParaView-#{version}-Darwin-64bit-SnowLeopard.dmg"
+    url "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{major_minor}&type=binary&os=osx&downloadFile=ParaView-#{version}-Darwin-64bit-SnowLeopard.dmg"
   else
     sha256 'cac627512f7d764ee85c601eeb9fbf269d4990f2a2345b72107d3b23a24642e6'
-    url "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.3&type=binary&os=osx&downloadFile=ParaView-#{version}-Darwin-64bit.dmg"
+    url "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{major_minor}&type=binary&os=osx&downloadFile=ParaView-#{version}-Darwin-64bit.dmg"
   end
 
   name 'ParaView'
