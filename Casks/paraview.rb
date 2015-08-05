88b63950421092ae4f13475c47afce6ed07e1879
Geoffrey Oxberry <goxberry@gmail.com>
2015-08-04 23:44:00 -0700

    Paraview: fix versioning logic for installation.
    The existing Paraview cask installs Paraview 4.1 for any OS X
    version <= 10.8. This logic has three issues:
    
    1) It won't work for any OS X version < 10.6.
    
    2) The first branch of the if statement triggers for OS X version
    10.6, but the downloaded binary requires OS X version 10.7 or greater.
    (see requirements at http://www.paraview.org/Wiki/ParaView/Binaries)
    
    3) The requirements for the current version of Paraview are OS X
    version 10.7 or greater (again, see requirements at
    http://www.paraview.org/Wiki/ParaView/Binaries), so the current logic
    installs an older version of Paraview than is necessary.
    
    This commit addresses these issues with the following changes:
    
    - add OS X version requirement of >= 10.6
    - fix the if-else version logic

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,10 +1,7 @@
 cask :v1 => 'paraview' do
   version '4.3.1'
 
-  if MacOS.release <= :mountain_lion
-    sha256 '8784481c90b58b0c6158e21b7f978a7d78caa67c63d28d6d5d770ef43f0ad890'
-    url 'http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.1&type=binary&os=osx&downloadFile=ParaView-4.1.0-Darwin-64bit-Lion-Python27.dmg'
-  elsif MacOS.release == :snow_leopard
+  if MacOS.release == :snow_leopard
     sha256 'f968263782d1407c769c8d7b28872a43adb263d0820c9bebe0e0ea8cf2aaa3a0'
     url "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v4.3&type=binary&os=osx&downloadFile=ParaView-#{version}-Darwin-64bit-SnowLeopard.dmg"
   else
@@ -19,4 +16,5 @@ cask :v1 => 'paraview' do
   app 'paraview.app'
 
   depends_on :arch => :x86_64
+  depends_on :macos => '>= :snow_leopard'
 end
