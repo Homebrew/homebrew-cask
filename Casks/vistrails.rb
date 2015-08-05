996e51d096eef798d5601deb2504b97088525bbb
Geoffrey Oxberry <goxberry@gmail.com>
2015-08-05 01:11:18 -0700

    VisTrails: update version to 2.2.2.

--- Casks/vistrails.rb
+++ Casks/vistrails.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'vistrails' do
-  version '2.1.4-269e4808eca3'
-  sha256 '5f6d190cff7af60050357d993ee34b9544fe15672b535769766ab783cbd09f99'
+  version '2.2.2-358e9a9fc33c'
+  sha256 'efee1669d3ba39985079e44a423f6e7b6b17493669c5945a3bbd70ec8659fc22'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/vistrails/vistrails/v#{version.sub(%r{-.*},'')}/vistrails-mac-10.6-intel-#{version}.dmg"
