7aa4925bb6474eca2e3a59b07b62d867b61e4b23
Aditya Dalal <adityadalal924@gmail.com>
2015-08-06 16:53:38 -0400

    Update iSyncr to v5.1.3
    Version and shasum updated to latest.

--- Casks/isyncr-desktop.rb
+++ Casks/isyncr-desktop.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'isyncr-desktop' do
-  version '5.0.8'
-  sha256 '1b1c0e6054b890fa7dd903cc680fd2543bc6b40b34cde2d23f4c2e355dcffb87'
+  version '5.1.3'
+  sha256 'c258905a72cdfcd05ae45bf53c5939946adb7268fe6575a11661e2e11a88edb2'
 
   url "http://www.jrtstudio.com/files/iSyncr%20Desktop%20#{version}.pkg"
   name 'iSyncr Desktop'
