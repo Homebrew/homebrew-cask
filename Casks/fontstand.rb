480d36db27aaa55b9bd117a5121ba234e66f5282
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-07 15:31:14 +0300

    fontstand.rb: fix download issue

--- Casks/fontstand.rb
+++ Casks/fontstand.rb
@@ -2,7 +2,8 @@ cask :v1 => 'fontstand' do
   version :latest
   sha256 :no_check
 
-  url 'https://fontstand.com/application/download'
+  url 'https://fontstand.com/application/download',
+      :user_agent => :fake
   name 'Fontstand'
   homepage 'https://fontstand.com/'
   license :gratis
