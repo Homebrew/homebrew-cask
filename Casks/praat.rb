2752d08e19426f10e42acd165553f157618f0c2a
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-08-07 17:05:15 +0200

    praat: update to 5.4.15

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'praat' do
-  version '5.4.12'
+  version '5.4.15'
 
   if Hardware::CPU.is_32_bit? || MacOS.release <= :snow_leopard
-    sha256 '6944362346e198f9bc02cbebf192c87eaf9ea8d28513f06aaec52253135fa1d6'
+    sha256 '83ecf829b0b31a5b3813957f83cb01c71c0800ac3ef402020ce1954c427d4744'
     url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac32.dmg"
   else
-    sha256 '9a3248579542335813f6529d32a6fd72ee28789ebcec0c57efcef51fc06e50a4'
+    sha256 '9781f8a8fabd4d85daafeeaf6a015bf8304ecd97cf8e20ed39a0e2f910fc46bb'
     url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac64.dmg"
   end
 
