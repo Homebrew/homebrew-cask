227b5a019c716a0107388ec37ce9809402853443
scribblemaniac <connor.deptuck@gmail.com>
2015-08-08 09:48:33 -0600

    sage.rb: Upgrade to v6.8

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -1,9 +1,16 @@
 cask :v1 => 'sage' do
-  version '6.7'
-  sha256 'd5887741c1f3b85030ade9f623f55e308947a35c9c620e7ccb84bf23a6ec939f'
+  if MacOS.release <= :lion
+    version '6.6'
+    sha256 'bdd740d8c92df5467269787aaf00e8cd0b2430cead259a4f15ef04e92b274282'
+    # mit.edu is an official download host per the vendor download page
+    url "http://mirrors.mit.edu/sage/osx/intel/sage-#{version}-x86_64-Darwin-OSX_10.7_x86_64-app.dmg"
+  else
+    version '6.8'
+    sha256 'cffdb518f2683adbf1b790617aefcf4491b43744efa1fc73d638e189e4cf18d6'
+    # mit.edu is an official download host per the vendor download page
+    url "http://mirrors.mit.edu/sage/osx/intel/sage-#{version}-x86_64-Darwin-OSX_10.10_x86_64-app.dmg"
+  end
 
-  # mit.edu is an official download host per the vendor download page
-  url "http://mirrors.mit.edu/sage/osx/sage-#{version}-x86_64-Darwin-OSX_10.10_x86_64-app.dmg"
   name 'Sage'
   homepage 'http://www.sagemath.org/'
   license :gpl
