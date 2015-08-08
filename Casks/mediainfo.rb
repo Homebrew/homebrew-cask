d8c2f77351c73c27a1b30126f44ccdbbc276ca2b
scribblemaniac <connor.deptuck@gmail.com>
2015-08-08 10:00:10 -0600

    mediainfo.rb: Upgrade to v0.7.76

--- Casks/mediainfo.rb
+++ Casks/mediainfo.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'mediainfo' do
-  version '0.7.72'
-  sha256 'd7b283ec737a1e1e967b6ba4ab89c74ff3777e4b2ab53fecbc4381fd662dc723'
+  version '0.7.76'
+  sha256 'e41893836093c8174b8d82b621d56ecca553dfe701ef6eb7722b09daf61cd936'
 
   url "https://mediaarea.net/download/binary/mediainfo-gui/#{version}/MediaInfo_GUI_#{version}_Mac.dmg"
   name 'MediaInfo'
   homepage 'https://mediaarea.net/en/MediaInfo'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :bsd
 
   app 'MediaInfo.app'
 end
