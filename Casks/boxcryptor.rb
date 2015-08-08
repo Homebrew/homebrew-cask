40bedbb30bb0b077d3ea41ac438010a4c20b5d65
scribblemaniac <connor.deptuck@gmail.com>
2015-08-08 07:16:49 -0600

    boxcryptor.rb: Add depends_on and appcast

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -4,8 +4,13 @@ cask :v2 => 'boxcryptor' do
 
   url 'https://www.boxcryptor.com/l/download-macosx'
   name 'Boxcryptor'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8',
+          :sha256 => '7b62a9cdd0e94fded41b4e4b0d8a015af04e86a2ffa1b741076fae69e2b7ec91',
+          :format => :sparkle
   homepage 'https://www.boxcryptor.com/en'
   license :commercial
+
+  depends_on :macos => '>= :lion'
 
   app 'Boxcryptor.app'
 end
