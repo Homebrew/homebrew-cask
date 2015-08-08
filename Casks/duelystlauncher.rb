07efb3dbf70301a2f6421b72ecfd26e74d36af19
Vítor Galvão <info@vitorgalvao.com>
2015-08-08 17:12:58 +0100

    added DuelystLauncher

---
+++ Casks/duelystlauncher.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'duelystlauncher' do
+  version '0.0.3'
+  sha256 'a990d87dda6e86d9da826348ca81e5d96d0cc58145e94e4762adaeca6d5e636d'
+
+  # netdna-ssl.com is the official download host per the vendor homepage
+  url "https://downloads-counterplaygames.netdna-ssl.com/launcher/v#{version}/DuelystLauncher-v#{version}.dmg"
+  name 'Duelyst'
+  homepage 'http://forums.duelyst.com/t/desktop-client-early-preview/'
+  license :gratis
+
+  app 'DuelystLauncher.app'
+end
