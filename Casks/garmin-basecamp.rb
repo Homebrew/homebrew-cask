387d4c082fcf72c26cd3ebf59f73599d665338c4
scribblemaniac <connor.deptuck@gmail.com>
2015-08-08 11:23:14 -0600

    garmin-basecamp.rb: Upgrade to v4.5.0

--- Casks/garmin-basecamp.rb
+++ Casks/garmin-basecamp.rb
@@ -1,11 +1,14 @@
 cask :v1 => 'garmin-basecamp' do
-  version '4.4.8'
-  sha256 '1ae1033fb4c80189ecc3c50b2f9ee90cbee6662f068cfa08800d886e1cf5ec42'
+  version '4.5.0'
+  sha256 '78e07cb729a6bfac2e2e86db54853f1574130c5bbf207a5ff476368f1f997eaf'
 
   url "http://download.garmin.com/software/BaseCampforMac_#{version.gsub('.', '')}.dmg"
   name 'Garmin BaseCamp'
   homepage 'https://www.garmin.com/en-US/shop/downloads/basecamp'
   license :gratis
+
+  depends_on :macos => '>= :yosemite'
+  depends_on :arch => :intel
 
   pkg 'Install BaseCamp.pkg'
 
