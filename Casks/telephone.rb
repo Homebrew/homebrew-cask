0bdcee2bcf90261b89d0277a8b5c155a83f41b76
ftc2 <ftc2@users.noreply.github.com>
2015-08-08 13:25:17 -0600

    Add Telephone v1.0.4

---
+++ Casks/telephone.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'telephone' do
+  version '1.0.4'
+  sha256 'e2c06dffd13610fe69d90c5dd61cc80050076517f6a16e8a78ad48e2a12caf2e'
+
+  url "http://www.tlphn.com/resources/Telephone-#{version}.dmg"
+  name 'Telephone'
+  homepage 'http://www.tlphn.com/'
+  license :bsd
+
+  app 'Telephone.app'
+end
