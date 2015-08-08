97043cd6abbb44373d4ce729514f69710ce06832
scribblemaniac <connor.deptuck@gmail.com>
2015-08-08 09:04:46 -0600

    firetask.rb: Upgrade to v3.7.5

--- Casks/firetask.rb
+++ Casks/firetask.rb
@@ -1,13 +1,15 @@
 cask :v1 => 'firetask' do
-  version '3.7.2'
-  sha256 'c511577124e016c9333d67a0de1b7babc92e301ec98fce643f5f90ea63bee95f'
+  version '3.7.5'
+  sha256 'fe8ca19270ce248dd78fdba0183ee2e9f6b69a889f8e6b66974f4ed8e3b81e8d'
 
   url "http://downloads.firetask.com/firetask-mac/Firetask-Mac-#{version}.zip"
   appcast 'http://www.firetask.com/firetask-mac/updates/firetask-mac.xml',
-          :sha256 => '5002e88441b705c45f026b9855a35d4d9799daa90ac445545fa3fd256635c0db'
+          :sha256 => '421a92beebdd8ca567396d43cab5d540fb24bc8a55f6cefc7ba6720f9a537921'
   name 'FireTask'
   homepage 'http://firetask.com'
   license :commercial
+
+  depends_on :macos => '>= :mountain_lion'
 
   app "Firetask-Mac-#{version}/Firetask.app"
 end
