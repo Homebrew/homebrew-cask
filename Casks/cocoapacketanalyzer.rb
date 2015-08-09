2e37662735bbd4a8ec913b42879dc041e0cec6b7
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:58 +0100

    cocoapacketanalyzer.rb: changed 'gsub' into '' to 'delete'

--- Casks/cocoapacketanalyzer.rb
+++ Casks/cocoapacketanalyzer.rb
@@ -2,7 +2,7 @@ cask :v1 => 'cocoapacketanalyzer' do
   version '1.31'
   sha256 'fc3586868fcf90e3ca766c31dbf184de78e5c907210dfdabbd13a831712fc864'
 
-  url "http://tastycocoabytes.com/_downloads/CPA_#{version.gsub('.','')}.dmg"
+  url "http://tastycocoabytes.com/_downloads/CPA_#{version.delete('.')}.dmg"
   appcast 'http://www.tastycocoabytes.com/cpa/updates/appcasting.php',
           :sha256 => 'e5471a829511ba4c4dd1cbf89bc2ea091fe5f27cc5eaae19fecffc94f8384f8a'
   name 'Cocoa Packet Analyzer'
