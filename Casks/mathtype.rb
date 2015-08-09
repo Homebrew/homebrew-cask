b67c52902cdc86d89f52870e8fe737200b42cf1a
Daehyun Y <hyuna917@me.com>
2015-08-09 20:43:31 +0900

    fill out a form: 'sha256'

--- Casks/mathtype.rb
+++ Casks/mathtype.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mathtype' do
   version '6.7'
-  sha256 :no_check
+  sha256 'f512b322c9b1e16c0f9c523ade1dc7f5e743768a2ac4d7fa37780abfbe05a861'
 
   url "http://www.dessci.com/en/dl/MTM#{version.gsub('.', '')}h_EN.pkg"
   name 'MathType'
