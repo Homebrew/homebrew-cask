85aeb16b3ba3d648c9e24ffe846ca176141cad54
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 17:49:59 +0100

    cutesdr.rb: changed 'gsub' into '' to 'delete'

--- Casks/cutesdr.rb
+++ Casks/cutesdr.rb
@@ -2,7 +2,7 @@ cask :v1 => 'cutesdr' do
   version '1.18'
   sha256 'b765c33cdf5cc074c0bda15c2a24995546422b4e35209d46966e6fea7e344963'
 
-  url "http://downloads.sourceforge.net/project/cutesdr/CuteSdr#{version.gsub('.','')}.dmg"
+  url "http://downloads.sourceforge.net/project/cutesdr/CuteSdr#{version.delete('.')}.dmg"
   name 'CuteSDR'
   homepage 'http://sourceforge.net/projects/cutesdr'
   license :oss
