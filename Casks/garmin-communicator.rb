9664973a873acc08e379a532d9e77cc8dcf204c8
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:59 +0100

    garmin-communicator.rb: changed 'gsub' into '' to 'delete'

--- Casks/garmin-communicator.rb
+++ Casks/garmin-communicator.rb
@@ -2,7 +2,7 @@ cask :v1 => 'garmin-communicator' do
   version '4.20'
   sha256 'a706a48926fc3907e84781bdf642ec1754c6081dd3d727ce09ca26c1ae8d9fde'
 
-  url "http://download.garmin.com/software/CommunicatorPluginforMac_#{version.gsub('.','')}.dmg"
+  url "http://download.garmin.com/software/CommunicatorPluginforMac_#{version.delete('.')}.dmg"
   name 'Garmin Communicator Plugin'
   homepage 'https://www8.garmin.com/support/download_details.jsp?id=3739'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
