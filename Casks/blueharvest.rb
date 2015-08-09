91c3f08ffe8af9fd230d1511d3dab8c7e58594a3
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:58 +0100

    blueharvest.rb: changed 'gsub' into '' to 'delete'

--- Casks/blueharvest.rb
+++ Casks/blueharvest.rb
@@ -2,7 +2,7 @@ cask :v1 => 'blueharvest' do
   version '6.2.1'
   sha256 '3ecb68a17a82a73d90c2451418114dcd925ed18da8bc4c03ccd5aeae15c0f24e'
 
-  url "http://zeroonetwenty.com/downloads/BlueHarvest#{version.gsub('.','')}.dmg"
+  url "http://zeroonetwenty.com/downloads/BlueHarvest#{version.delete('.')}.dmg"
   appcast 'https://cp37.ezyreg.com/~zeroonet/downloads/versioninfo/sparkle/blueharvest6.xml',
           :sha256 => '0a6a7811abf74d36ee9fb78ce2b208fbded639da602f3900a2cda0865f17bda0'
   name 'BlueHarvest'
