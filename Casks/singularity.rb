5f76b02d9827786d5d01d51f03395ec93079160d
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:22:01 +0100

    singularity.rb: changed 'gsub' into '' to 'delete'

--- Casks/singularity.rb
+++ Casks/singularity.rb
@@ -3,7 +3,7 @@ cask :v1 => 'singularity' do
   sha256 '002597a1d7386ff1b505416476fce812037a38c61bd152212fd758db943de40c'
 
   # bitbucket.org is the official download host per the vendor homepage
-  url "https://bitbucket.org/SingularityViewer/singularityviewer/downloads/Singularity_#{version.gsub(/[.(]/, '_').gsub(')', '')}.dmg"
+  url "https://bitbucket.org/SingularityViewer/singularityviewer/downloads/Singularity_#{version.gsub(/[.(]/, '_').delete(')')}.dmg"
   name 'Singularity Viewer'
   homepage 'http://www.singularityviewer.org/'
   license :gpl
