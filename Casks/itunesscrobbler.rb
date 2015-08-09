6e8da30be16c6f4a9c591def95b9bcfb9ef1a9ec
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:59 +0100

    itunesscrobbler.rb: changed 'gsub' into '' to 'delete'

--- Casks/itunesscrobbler.rb
+++ Casks/itunesscrobbler.rb
@@ -2,7 +2,7 @@ cask :v1 => 'itunesscrobbler' do
   version '0.6.0'
   sha256 '01dcacb1738e824d853dab7825cee88b017f47a65f8e22fdbd7bf762802cc893'
 
-  url "http://www.easyclasspage.de/lastfm/files/iTunesScrobbler_#{version.gsub('.','')}.zip"
+  url "http://www.easyclasspage.de/lastfm/files/iTunesScrobbler_#{version.delete('.')}.zip"
   name 'iTunes (Radio) Scrobbler'
   name 'iTunesScrobbler'
   homepage 'http://www.easyclasspage.de/lastfm/seite-19.html'
