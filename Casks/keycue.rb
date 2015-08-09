59f7aef4faaeb0fb5daef0e66898b5ffe675d682
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:22:00 +0100

    keycue.rb: changed 'gsub' into '' to 'delete'

--- Casks/keycue.rb
+++ Casks/keycue.rb
@@ -2,7 +2,7 @@ cask :v1 => 'keycue' do
   version '7.5'
   sha256 '8d2ae27ac7a623e3cdcfe939caf996e81e39e3dbb7452f1a634b24dbe5050ad3'
 
-  url "http://www.ergonis.com/downloads/products/keycue/KeyCue#{version.gsub('.','')}-Install.dmg"
+  url "http://www.ergonis.com/downloads/products/keycue/KeyCue#{version.delete('.')}-Install.dmg"
   name 'KeyCue'
   homepage 'http://www.ergonis.com/products/keycue/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
