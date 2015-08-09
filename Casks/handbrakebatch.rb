bf8ae3fc07cc49a83722c1aa8158a3e997c85a94
scribblemaniac <connor.deptuck@gmail.com>
2015-08-08 18:20:58 -0600

    handbrakebatch.rb: Switch to versioned url

--- Casks/handbrakebatch.rb
+++ Casks/handbrakebatch.rb
@@ -1,8 +1,8 @@
 cask :v1_1 => 'handbrakebatch' do
-  version :latest
-  sha256 :no_check
+  version '2.25'
+  sha256 'd5f57d1a7ef9a85b32c800aa8c94ea73420940b6a9e463561df343fe82c64c6c'
 
-  url 'https://www.osomac.com/appcasts/handbrakebatch/HandBrakeBatch.zip'
+  url "https://www.osomac.com/appcasts/handbrakebatch/HandBrakeBatch_#{version}.zip"
   appcast 'https://www.osomac.com/appcasts/handbrakebatch/HandBrakeBatch.xml'
   name 'HandBrakeBatch'
   homepage 'https://www.osomac.com/apps/osx/handbrake-batch/'
