b0bd5dd7be18aaad60cd726e44b0829aa774fb39
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:58 +0100

    capsee.rb: changed 'gsub' into '' to 'delete'

--- Casks/capsee.rb
+++ Casks/capsee.rb
@@ -2,11 +2,11 @@ cask :v1 => 'capsee' do
   version '1.2'
   sha256 'e78cdfe435cca259e0111a2b2131ad3be7d5ba6160cf69c8e7cbcc033eac2fc4'
 
-  url "http://www.threemagination.com/CapSee#{version.gsub('.','')}.zip"
+  url "http://www.threemagination.com/CapSee#{version.delete('.')}.zip"
   name 'CapSee'
   homepage 'http://www.threemagination.com/capsee/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  container :nested => "CapSee#{version.gsub('.','')}.dmg"
+  container :nested => "CapSee#{version.delete('.')}.dmg"
   app 'CapSee.app'
 end
