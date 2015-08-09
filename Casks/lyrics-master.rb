c2ac1bc502e3fd062264fd2649f41e35f5a9471b
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:22:00 +0100

    lyrics-master.rb: changed 'gsub' into '' to 'delete'

--- Casks/lyrics-master.rb
+++ Casks/lyrics-master.rb
@@ -2,7 +2,7 @@ cask :v1 => 'lyrics-master' do
   version '2.4.4'
   sha256 '659b958c8a4b9d3e24245398d0a08318b354d92815bbfd15d157cdc9367466c5'
 
-  url "http://www.kenichimaehashi.com/lyricsmaster/download/LyricsMaster#{version.gsub('.','')}0.dmg"
+  url "http://www.kenichimaehashi.com/lyricsmaster/download/LyricsMaster#{version.delete('.')}0.dmg"
   name 'Lyrics Master'
   homepage 'http://www.kenichimaehashi.com/lyricsmaster/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
