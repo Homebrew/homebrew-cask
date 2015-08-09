54718fa71ff3818b0c5594f07e3b4a64d0a0ce2f
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:58 +0100

    cool-retro-term.rb: changed 'gsub' into '' to 'delete'

--- Casks/cool-retro-term.rb
+++ Casks/cool-retro-term.rb
@@ -2,7 +2,7 @@ cask :v1 => 'cool-retro-term' do
   version '1.0.0'
   sha256 'ccb1c78b54e1c2dcde1a660730da2b0f6d2e4213e3748e5ad81a5653926c920e'
 
-  url "https://github.com/Swordfish90/cool-retro-term/releases/download/v#{version}/cool-retro-term#{version.gsub('.','')}.dmg"
+  url "https://github.com/Swordfish90/cool-retro-term/releases/download/v#{version}/cool-retro-term#{version.delete('.')}.dmg"
   name 'cool-retro-term'
   homepage 'https://github.com/Swordfish90/cool-retro-term'
   license :gpl
