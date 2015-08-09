15fd450088e9652fc2a1ca050a0fcaf6ffc3d052
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:22:00 +0100

    keyboard-maestro.rb: changed 'gsub' into '' to 'delete'

--- Casks/keyboard-maestro.rb
+++ Casks/keyboard-maestro.rb
@@ -3,7 +3,7 @@ cask :v1 => 'keyboard-maestro' do
   sha256 '39caa5e5b5dac584da141cd5eb2b428b7c5ee562a5f858370c6338b538a9c8ad'
 
   # stairways.com is the official download host per the vendor homepage
-  url "http://files.stairways.com/keyboardmaestro-#{version.gsub('.','')}.zip"
+  url "http://files.stairways.com/keyboardmaestro-#{version.delete('.')}.zip"
   name 'Keyboard Maestro'
   homepage 'http://www.keyboardmaestro.com/'
   license :commercial
