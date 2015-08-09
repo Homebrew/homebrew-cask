c908f56bae69686c47164fb120f9993ffbba3d62
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:22:02 +0100

    zoc.rb: changed 'gsub' into '' to 'delete'

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -2,7 +2,7 @@ cask :v1 => 'zoc' do
   version '7.05.2'
   sha256 '32e7582df299b2527afb6f84ee239cca2d8fdcbdde2382c028438f296af6dd96'
 
-  url "http://www.emtec.com/downloads/zoc/zoc#{version.gsub('.','')}.dmg"
+  url "http://www.emtec.com/downloads/zoc/zoc#{version.delete('.')}.dmg"
   name 'ZOC'
   homepage 'http://www.emtec.com/zoc/'
   license :commercial
