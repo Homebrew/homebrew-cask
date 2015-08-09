4d227331ee7620829cd1716d396a8199c5044be2
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:22:01 +0100

    praat.rb: changed 'gsub' into '' to 'delete'

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -3,10 +3,10 @@ cask :v1 => 'praat' do
 
   if Hardware::CPU.is_32_bit? || MacOS.release <= :snow_leopard
     sha256 '83ecf829b0b31a5b3813957f83cb01c71c0800ac3ef402020ce1954c427d4744'
-    url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac32.dmg"
+    url "http://www.fon.hum.uva.nl/praat/praat#{version.delete('.')}_mac32.dmg"
   else
     sha256 '9781f8a8fabd4d85daafeeaf6a015bf8304ecd97cf8e20ed39a0e2f910fc46bb'
-    url "http://www.fon.hum.uva.nl/praat/praat#{version.gsub('.','')}_mac64.dmg"
+    url "http://www.fon.hum.uva.nl/praat/praat#{version.delete('.')}_mac64.dmg"
   end
 
   name 'Praat'
