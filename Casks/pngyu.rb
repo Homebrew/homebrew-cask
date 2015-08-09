4d1d9a770e82280a606be184e1a12110376b96fd
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:22:01 +0100

    pngyu.rb: changed 'gsub' into '' to 'delete'

--- Casks/pngyu.rb
+++ Casks/pngyu.rb
@@ -2,7 +2,7 @@ cask :v1 => 'pngyu' do
   version '1.0.1'
   sha256 'f853a3566236200391a40d12df9519b39f4d30053ab6ed6f60670b7eacca6217'
 
-  url "https://nukesaq88.github.io/Pngyu/download/Pngyu_mac_#{version.gsub('.','')}.zip"
+  url "https://nukesaq88.github.io/Pngyu/download/Pngyu_mac_#{version.delete('.')}.zip"
   name 'Pngyu'
   homepage 'https://nukesaq88.github.io/Pngyu/'
   license :bsd
