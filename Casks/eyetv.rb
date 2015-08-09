33db19b16da5b4a5573381f467f4dbddf9a55799
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:59 +0100

    eyetv.rb: changed 'gsub' into '' to 'delete'

--- Casks/eyetv.rb
+++ Casks/eyetv.rb
@@ -2,7 +2,7 @@ cask :v1 => 'eyetv' do
   version '3.6.5'
   sha256 'cc8d7b1db43eabe827bab3a2401be97c6687cf83806ff585e3a125768c8c05f7'
 
-  url "http://files.elgato.com/eyetvdownloads/support/eyetv_#{version.gsub('.','')}_7310.dmg"
+  url "http://files.elgato.com/eyetvdownloads/support/eyetv_#{version.delete('.')}_7310.dmg"
   name 'EyeTV'
   homepage 'https://www.elgato.com/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
