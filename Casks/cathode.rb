79e87f91dbff2cbf1ef6880d0a0de6563142716e
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:58 +0100

    cathode.rb: changed 'gsub' into '' to 'delete'

--- Casks/cathode.rb
+++ Casks/cathode.rb
@@ -3,7 +3,7 @@ cask :v1 => 'cathode' do
   sha256 'dd176890c8e8d6d37334440b13402d04b5344d009804f8624985cef3c081ac8b'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "http://s3.amazonaws.com/cjcaufield/products/cathode/cathode_#{version.gsub('.','')}.zip"
+  url "http://s3.amazonaws.com/cjcaufield/products/cathode/cathode_#{version.delete('.')}.zip"
   name 'Cathode'
   appcast 'http://store.secretgeometry.com/appcast.php?id=7',
           :sha256 => '4c319653dc0883e0534eae3b2e51ecf92e6ff7f91d9329e1c2e8c39139cb1eee'
