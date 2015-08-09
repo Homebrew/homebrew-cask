64a56743302cfb7fddcfd1b68cdc0cb346b9c4df
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:22:00 +0100

    pdf-converter-master.rb: changed 'gsub' into '' to 'delete'

--- Casks/pdf-converter-master.rb
+++ Casks/pdf-converter-master.rb
@@ -2,7 +2,7 @@ cask :v1 => 'pdf-converter-master' do
   version '3.5.0'
   sha256 'c927cea0b588dfc421c6f2feec6ab4a660c554a039e2bb160840b3bac17ed5ba'
 
-  url "http://www.lightenpdf.com/download/download.aspx?file=PDF-Converter-Master-#{version.gsub('.','')}.zip"
+  url "http://www.lightenpdf.com/download/download.aspx?file=PDF-Converter-Master-#{version.delete('.')}.zip"
   appcast 'http://www.lightenpdf.com/xml/PDF-Converter-Mac.xml',
           :sha256 => 'be7221e35d5e4e564f34bb364705595a9a0c73868627614e4629728f3bd3fdf8'
   name 'PDF Converter Master'
