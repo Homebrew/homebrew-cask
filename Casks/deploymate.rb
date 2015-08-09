964e4457ee3bdecb5a770eaf44e0ca30ea19d818
Vítor Galvão <info@vitorgalvao.com>
2015-08-09 14:21:58 +0100

    deploymate.rb: changed 'gsub' into '' to 'delete'

--- Casks/deploymate.rb
+++ Casks/deploymate.rb
@@ -3,7 +3,7 @@ cask :v1 => 'deploymate' do
   sha256 '7e990c4ce807510ccfed544b968f049c28aec6a7988d068b5412fc93466b79f6'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "https://s3.amazonaws.com/deploymate/download/Deploymate#{version.gsub('.', '')}.zip"
+  url "https://s3.amazonaws.com/deploymate/download/Deploymate#{version.delete('.')}.zip"
   appcast 'http://www.deploymateapp.com/deploymate-updates.xml',
           :sha256 => '03ce137e3e8dc6e8e29b79c1d2a8733a653f7d68af48e32c35172a78291f4ce7'
   name 'Deploymate'
