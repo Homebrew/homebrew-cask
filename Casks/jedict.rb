83a1d7f7ad993530463208336efde3d8c8014972
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-10 18:14:40 +0300

    jedict.rb: change gsub into  to delete

--- Casks/jedict.rb
+++ Casks/jedict.rb
@@ -2,7 +2,7 @@ cask :v1 => 'jedict' do
   version '4.9.6'
   sha256 '99dc7bd641a708c4d09e975e136c0dd05597620f2f47a77817b4604fc1731b9b'
 
-  url "http://jedict.com/Downloads/JEDict#{version.gsub('.','')}.zip"
+  url "http://jedict.com/Downloads/JEDict#{version.delete('.')}.zip"
   name 'Jedict'
   homepage 'http://www.jedict.com/'
   license :commercial
