45639da07d971c820243513e182aed8fb5fa6c37
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-10 18:04:19 +0300

    qldds.rb: change 'gsub' into '' to 'delete'

--- Casks/qldds.rb
+++ Casks/qldds.rb
@@ -2,13 +2,13 @@ cask :v1 => 'qldds' do
   version '1.20'
   sha256 '684633521706544774c4a0e2f4c87bb5a0e6da01fa4c70c1b2e81bdd93cd770f'
 
-  url "https://github.com/Marginal/QLdds/releases/download/rel-#{version.gsub('.','')}/QLdds_#{version.gsub('.','')}.pkg"
+  url "https://github.com/Marginal/QLdds/releases/download/rel-#{version.delete('.')}/QLdds_#{version.delete('.')}.pkg"
   appcast 'https://github.com/Marginal/QLdds/releases.atom'
   name 'QuickLook DDS'
   homepage 'https://github.com/Marginal/QLdds'
   license :gpl
 
-  pkg "QLdds_#{version.gsub('.','')}.pkg"
+  pkg "QLdds_#{version.delete('.')}.pkg"
 
   uninstall :pkgutil => 'uk.org.marginal.qldds'
 end
