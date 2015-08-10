3b18889d61ec9640103ccf3a12e427cbdb98cf42
Knut Ahlers <knut@ahlers.me>
2015-08-10 13:33:11 +0200

    monotype-skyfonts is now skyfonts
    This reverts commit 37f5f47287caa799c2971ad9b1b68e8f6e63d06d.

--- Casks/skyfonts.rb
+++ Casks/skyfonts.rb
@@ -1,12 +1,12 @@
-cask :v1 => 'monotype-skyfonts' do
+cask :v1 => 'skyfonts' do
   version '4.9.0.0'
   sha256 '0e6982d6fb27b3570fe529a9c055bc486ff995dbd72d21c6c0999208839f7456'
 
   # skyfonts.com is the official download host per the vendor homepage
   url "http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_#{version}.dmg"
-  name 'Monotype SkyFonts'
+  name 'SkyFonts'
   homepage 'https://www.fonts.com/web-fonts/google'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  app 'Monotype Skyfonts.app'
+  app 'SkyFonts.app'
 end
