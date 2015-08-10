e54bc2f432895c8fc8239105c888bcf06b5b2f9e
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-10 16:07:18 +0300

    cycling74-max.rb: change 'gsub' into '' to 'delete'

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -3,7 +3,7 @@ cask :v1 => 'cycling74-max' do
   sha256 '61d5d5e0a4ca4d56a1bffbc49343b5624c02f0a707bfed943e8c0eb15c91c557'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.sub('-','_').gsub('.','')}.dmg"
+  url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.delete('.')}.dmg"
   name 'Max'
   homepage 'https://cycling74.com/'
   license :commercial
