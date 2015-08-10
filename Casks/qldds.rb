36766617961085761165e96403aa98cd009641cb
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-10 18:01:03 +0300

    qldds.rb: update sha256 checksum

--- Casks/qldds.rb
+++ Casks/qldds.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qldds' do
   version '1.20'
-  sha256 '8abd0978eb90b1ef55b5ac079960b028cbc926673535a0802bccd590fb253bc6'
+  sha256 '684633521706544774c4a0e2f4c87bb5a0e6da01fa4c70c1b2e81bdd93cd770f'
 
   url "https://github.com/Marginal/QLdds/releases/download/rel-#{version.gsub('.','')}/QLdds_#{version.gsub('.','')}.pkg"
   appcast 'https://github.com/Marginal/QLdds/releases.atom'
