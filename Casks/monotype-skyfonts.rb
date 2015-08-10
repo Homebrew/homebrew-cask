37f5f47287caa799c2971ad9b1b68e8f6e63d06d
Knut Ahlers <knut@ahlers.me>
2015-08-10 10:32:25 +0200

    Upgraded SkyFonts to 4.9.0.0

--- Casks/monotype-skyfonts.rb
+++ Casks/monotype-skyfonts.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'monotype-skyfonts' do
-  version '4.7.1.0'
-  sha256 '9a473e2b2d89d62b4fc2d9d3400a064636c210c62dfd935dd26e693a4c5c5bad'
+  version '4.9.0.0'
+  sha256 '0e6982d6fb27b3570fe529a9c055bc486ff995dbd72d21c6c0999208839f7456'
 
   # skyfonts.com is the official download host per the vendor homepage
   url "http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_#{version}.dmg"
