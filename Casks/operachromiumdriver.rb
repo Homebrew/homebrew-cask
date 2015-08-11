ecd20406f862532cffcaca4a6edd26f882006c2c
Dan Robson <drobson@solu.digital>
2015-08-11 08:00:47 -0400

    Updates operachromium driver cask to validate checksum.
    The previous submission was not validating the downloaded .zip
    file, this version is performing the check.

--- Casks/operachromiumdriver.rb
+++ Casks/operachromiumdriver.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'operachromiumdriver' do
   version '0.2.2'
-  sha256 :no_check
+  sha256 '6584ea2b0ebb06a2c5639570fdac0a28de90bb88982ce9e74d531147106988bd'
 
   url 'https://github.com/operasoftware/operachromiumdriver/releases/download/v0.2.2/operadriver_mac64.zip'
   name 'operachromiumdriver'
