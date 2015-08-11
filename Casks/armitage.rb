e3d19bfe124b366d160f0fa7b9bdb66985d1842b
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-11 19:01:27 +0300

    armitage.rb: add license

--- Casks/armitage.rb
+++ Casks/armitage.rb
@@ -2,10 +2,10 @@ cask :v1 => 'armitage' do
   version '14.11.20'
   sha256 'b309fdef13c8a3a0d981ffc1ad2bfb4786a797f4e291dd4ef3bcc2806c1126f4'
 
-  url "http://www.fastandeasyhacking.com/download/armitage#{version.delete('.')}.dmg"
+  url "http://www.fastandeasyhacking.com/download/armitage#{version.gsub('.', '')}.dmg"
   name 'Armitage'
   homepage 'http://www.fastandeasyhacking.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :bsd
 
   app 'Armitage.app'
 end
