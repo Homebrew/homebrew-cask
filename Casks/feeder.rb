7212f7ceb8f4586daa8887a3b8dc78466dbfef87
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-11 23:32:08 +0300

    feeder.rb: update links to use SSL

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -2,11 +2,11 @@ cask :v1 => 'feeder' do
   version '3.0.2'
   sha256 '2e8822498b1f644c2760704c790d29aa472c358c0d6fe2845dcc2f35d0acaf4b'
 
-  url "http://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
-  appcast 'http://reinventedsoftware.com/feeder/downloads/Feeder3.xml',
+  url "https://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
+  appcast 'https://reinventedsoftware.com/feeder/downloads/Feeder3.xml',
           :sha256 => 'd44a2c090bb5f68dbc5984be7685a5976ce4ac03e4ef1b02491d5bcd4868e4c2'
   name 'Feeder'
-  homepage 'http://reinventedsoftware.com/feeder/'
+  homepage 'https://reinventedsoftware.com/feeder/'
   license :commercial
 
   app 'Feeder 3.app'
