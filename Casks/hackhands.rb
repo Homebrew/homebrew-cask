b6254dfe92e498592d8cd03818108655cb30af33
Caleb Xu <calebcenter@live.com>
2015-08-11 23:31:32 -0400

    updated hackhands (1.4.9)

--- Casks/hackhands.rb
+++ Casks/hackhands.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'hackhands' do
-  version '1.4.8'
-  sha256 'd91ff1cda8d6e4d9c4e892e8ad1f6ab67968b7d2db53de4c8f8b6e5122a601e8'
+  version '1.4.9'
+  sha256 'f34e8d01fbe1c47912ce9c5238852a11fc406f1ff143f9c995439d27ec7e8313'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "http://desktop.hackhands.com.s3-website-us-west-1.amazonaws.com/osx/#{version}/HackHands.zip"
