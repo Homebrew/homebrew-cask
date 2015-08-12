055861a775f5e6ea8b9d39c513d9a76887e7e149
Mike English <mike.english@atomicobject.com>
2015-08-12 13:12:25 -0400

    Add Docker Toolbox v1.8.0a.

---
+++ Casks/dockertoolbox.rb
@@ -0,0 +1,27 @@
+cask :v1 => 'dockertoolbox' do
+  version '1.8.0a'
+  sha256 '1ec3234e25970ecfaa85af85af6407deb9426917514f591e939371b4a006efc7'
+
+  url "https://github.com/docker/toolbox/releases/download/v#{version}/DockerToolbox-#{version}.pkg"
+  appcast 'https://github.com/docker/toolbox/releases.atom'
+  name 'Docker Toolbox'
+  homepage 'https://www.docker.com/toolbox'
+  license :apache
+
+  pkg "DockerToolbox-#{version}.pkg"
+
+  postflight do
+    set_ownership '~/.docker'
+  end
+
+  uninstall :pkgutil => [
+                         'io.boot2dockeriso.pkg.boot2dockeriso',
+                         'io.docker.pkg.docker',
+                         'io.docker.pkg.dockercompose',
+                         'io.docker.pkg.dockermachine',
+                         'io.docker.pkg.dockerquickstartterminalapp',
+                         'io.docker.pkg.kitematicapp',
+                        ]
+
+  depends_on :cask => 'virtualbox'
+end
