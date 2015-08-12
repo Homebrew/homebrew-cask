1cfa8d5c24300e0cb75e3d3d42d796d5001c6cac
Vítor Galvão <info@vitorgalvao.com>
2015-08-12 18:57:25 +0100

    synology-cloud-station.rb: use https

--- Casks/synology-cloud-station.rb
+++ Casks/synology-cloud-station.rb
@@ -2,7 +2,7 @@ cask :v1 => 'synology-cloud-station' do
   version '3.2-3482'
   sha256 '15bc32284c1466400291e863ba2541fd9b66c53d4ff2db9ca788f36a887314b4'
 
-  url "http://global.download.synology.com/download/Tools/CloudStation/#{version}/Mac/Installer/synology-cloud-station-#{version.sub(%r{.*-},'')}.dmg"
+  url "https://global.download.synology.com/download/Tools/CloudStation/#{version}/Mac/Installer/synology-cloud-station-#{version.sub(%r{.*-},'')}.dmg"
   name 'Synology Cloud Station'
   homepage 'https://www.synology.com/'
   license :gratis
