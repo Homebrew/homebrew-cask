bd6d4cabab2f59360a5d7120e2497a907835ea60
Caleb Xu <calebcenter@live.com>
2015-08-11 23:17:31 -0400

    updated firestr (0.10)

--- Casks/firestr.rb
+++ Casks/firestr.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firestr' do
   version '0.10'
-  sha256 '13174b218589f86cc6c13624fc5ef535addbc39516f9b6f44dbc22e168cc48de'
+  sha256 'a6d436a2095132382c059376e2d05a53390793abf3916a6a68c7e9d3d98cee26'
 
   url "https://mempko.com/firestr/build/#{version}/firestr_#{version}.dmg"
   name 'Firestr'
