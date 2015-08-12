80feb8fedb0bfe8306df17668c43ac09312cc2ad
Slava Shklyaev <shk.slava@gmail.com>
2015-08-12 12:46:42 +0300

    Upgrade Kitematic to 0.8.1

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'kitematic' do
-  version '0.7.6'
-  sha256 '297f6cd43de0474f194c9024ab01a6fa1225abb9b54e378653eeecc4627fa7c7'
+  version '0.8.1'
+  sha256 'e7d56f723dbf84c66cf1595aeb92c7240eee1ba56a9eb1ed6642b939215b7e53'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
