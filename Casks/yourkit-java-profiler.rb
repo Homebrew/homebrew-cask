28655f8885c4d67dc92b2eade2019a0f9cf68b8e
Mike Kotsur <miccots@gmail.com>
2015-08-12 11:20:48 +0200

    Fixed broken link to the distribution

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'yourkit-java-profiler' do
-  version '2015-build-15068'
+  version '2015-build-15072'
   sha256 'f4724f9d8d97dec025d5cb6c15227ae14036bb7d152696b77023300f17628cd7'
 
   url "http://www.yourkit.com/download/yjp-#{version}-mac.zip"
