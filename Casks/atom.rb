23e338253c7cb9e86f07b35d045a5911335cfb7a
Geoffrey Oxberry <goxberry@gmail.com>
2015-08-12 18:09:27 -0700

    Upgrade Atom to version 1.0.7.

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'atom' do
-  version '1.0.6'
-  sha256 '212643e118a6d1844a6dfb03481b314a99db7f0f5c7c37e5f528399cf626dbdf'
+  version '1.0.7'
+  sha256 'f68412561bbd0d46e92a4af0748f5f6fa9f31744bcfb9547f6f66de3073f47bd'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
