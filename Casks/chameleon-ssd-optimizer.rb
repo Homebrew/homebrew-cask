55af898cd94357642b0aa2c73329bf41e824fb60
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-13 23:06:48 +0300

    chameleon-ssd-optimizer.rb: add license

--- Casks/chameleon-ssd-optimizer.rb
+++ Casks/chameleon-ssd-optimizer.rb
@@ -6,7 +6,7 @@ cask :v1 => 'chameleon-ssd-optimizer' do
   appcast 'http://chameleon.alessandroboschini.com/sparkle/profileInfo.php'
   name 'Chameleon SSD optimizer'
   homepage 'http://chameleon.alessandroboschini.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'Chameleon SSD Optimizer.app'
 
