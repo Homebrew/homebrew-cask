a85df95066c89510dedbd0bb245586785fd1da6a
Shigenobu Nishikawa <shishi.s.n@gmail.com>
2015-08-14 17:08:58 +0900

    Update lyrics-master to 2.4.5

--- Casks/lyrics-master.rb
+++ Casks/lyrics-master.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'lyrics-master' do
-  version '2.4.4'
-  sha256 '659b958c8a4b9d3e24245398d0a08318b354d92815bbfd15d157cdc9367466c5'
+  version '2.4.5'
+  sha256 '5c66ec8da4bc2a21a41988c7d3cef11d2cca5324b7b943c4d5e3a11b1c6519b6'
 
   url "http://www.kenichimaehashi.com/lyricsmaster/download/LyricsMaster#{version.delete('.')}0.dmg"
   name 'Lyrics Master'
   homepage 'http://www.kenichimaehashi.com/lyricsmaster/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   suite 'Lyrics Master'
 end
