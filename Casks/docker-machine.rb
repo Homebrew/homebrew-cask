a57d11c7d5210361bf072710e07c0edf6ed8a5b3
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-15 18:56:38 +0300

    docker-machine.rb: remove 'v' from version

--- Casks/docker-machine.rb
+++ Casks/docker-machine.rb
@@ -1,9 +1,9 @@
 cask :v1_1 => 'docker-machine' do
-  version 'v0.4.0'
+  version '0.4.0'
   sha256 'e9d57ed56fd145223e203060c58e67259faac139a79b3942074dc0d4071b231d'
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com/docker/machine/releases/download/#{version}/docker-machine_darwin-amd64"
+  url "https://github.com/docker/machine/releases/download/v#{version}/docker-machine_darwin-amd64"
   appcast 'https://github.com/docker/machine/releases.atom'
   name 'Docker Machine'
   homepage 'https://docs.docker.com/machine'
