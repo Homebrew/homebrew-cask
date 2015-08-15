458f92d708b68d40d86e42e976d288ab87fb1d11
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-15 18:12:22 +0300

    boot2docker.rb: upgrade to v1.8.0

--- Casks/boot2docker.rb
+++ Casks/boot2docker.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'boot2docker' do
-  version '1.7.1'
-  sha256 '80658f99d0448793bfa7a35d8cdf9a368b4977f7a9977c9b375f6adab45cadaf'
+  version '1.8.0'
+  sha256 'b457bd33c046568a7a56ea89256563768b59ebaf5a50827f5e6f8703480dc329'
 
   url "https://github.com/boot2docker/osx-installer/releases/download/v#{version}/Boot2Docker-#{version}.pkg"
   appcast 'https://github.com/boot2docker/osx-installer/releases.atom'
