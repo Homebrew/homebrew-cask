400787bc4c2933cd91adebd708a3296ca4fc7177
Jiashu Wang <wjs9889@gmail.com>
2015-08-16 22:40:05 -0700

    Update sogouinput to 3.2.0.69954.

--- Casks/sogouinput.rb
+++ Casks/sogouinput.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'sogouinput' do
-  version '3.2.0.68659'
-  sha256 '45abc1adef879c3b4c090b5d2bac9c7b89adcda88dde6b030305c52c5b2734f3'
+  version '3.2.0.69954'
+  sha256 'c720c06592360f6df6c436d5ce5dc42fd080c2545df0deb0634959fec9fd1edf'
 
-  url 'http://download.ime.sogou.com/1429884508/sogou_mac_32a.dmg'
+  url 'http://download.ime.sogou.com/1433409088/sogou_mac_32c.dmg'
   name '搜狗输入法'
   name 'Sogou Input Method'
   homepage 'http://pinyin.sogou.com/mac/'
