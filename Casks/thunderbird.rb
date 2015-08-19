a0c04dbe31935b05b56cbc1845946cca88afa624
Jussi Tuominen <me@jussituominen.me>
2015-08-19 21:57:45 +0300

    Update Thunderbird.app to v38.2.0

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird' do
-  version '38.1.0'
-  sha256 '41ab11b8c5d5ef8ab09be16efcd8097b417d2532dc903d5983e55d7ac67fb827'
+  version '38.2.0'
+  sha256 '22fbba726dcb409b5ad48c35f02de7fe7544193b3a9faa108f4c85a38f20ae03'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   name 'Mozilla Thunderbird'
