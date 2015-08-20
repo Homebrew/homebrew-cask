ea53fa3d58fd538ea85b4228af6c4769fa2acca1
slack4u <slack4u@users.noreply.github.com>
2015-08-20 16:23:31 +0900

    Upgrade Unsplash Wallpaper to 1.3.0

--- Casks/unsplash-wallpaper.rb
+++ Casks/unsplash-wallpaper.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'unsplash-wallpaper' do
-  version '1240_1438381941'
-  sha256 '2e233d8b7c3613dbd3c0d0d89ae1a9d4ee76521a8ec360068b6aeba4c3890ed1'
+  version '1301_1439418481'
+  sha256 '8becb8675fb74d62b2ff70bd57b72c80f1e1d550e907cc00b927406da5862a35'
 
   # devmate.com is the official download host per the vendor homepage
   url "https://dl.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/#{version.sub(%r{_.*},'')}/#{version.sub(%r{.*_},'')}/UnsplashWallpaper-#{version.sub(%r{_.*},'')}.zip"
   appcast 'http://updateinfo.devmate.com/com.leonspok.osx.Unsplash-Wallpaper/updates.xml',
-          :sha256 => '209a7aceb636676b7586a9ab53f92095e282d58c44d58f4eff17454eece01821'
+          :sha256 => 'f94db4e59c4e6484d5c3ce4131d5b70e242d2253d18f4019f792e4f0f9280917'
   name 'Unsplash Wallpaper'
   homepage 'http://unsplash-wallpaper.tumblr.com'
   license :gratis
