890ae4aaa4652412f538b4ca7ec590f65a78d1c5
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-20 17:42:35 +0300

    commandq.rb: add license

--- Casks/commandq.rb
+++ Casks/commandq.rb
@@ -7,7 +7,7 @@ cask :v1 => 'commandq' do
   appcast 'https://shine.clickontyler.com/appcast.php?id=16',
           :sha256 => 'aded9f4d5543c963c0989f53ba18c35b2053703b543b10a54c61a6b42b53dd50'
   homepage 'https://clickontyler.com/commandq/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'CommandQ.app'
 end
