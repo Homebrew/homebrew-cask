674d7bf787d3f5bf51783c859ddaa4d0ec4bed4e
tkcfjips <tkcfjips@gmail.com>
2015-08-21 07:04:47 +0900

    add gInbox

---
+++ Casks/gInbox.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'gInbox' do
+  version '0.2.3c'
+  sha256 '15c60b91e2e067dd7ec7134c0ae7f806997bc38076cebda6513d065fbf67aa62'
+
+  url 'https://github.com/chenasraf/gInbox/releases/download/0.2.3c/gInbox-0.2.3c.zip'
+  name 'Inbox by Google'
+  homepage 'https://github.com/chenasraf/gInbox'
+  license :apache    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'Inbox by Google.app'
+end
