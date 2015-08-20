247b73cb59ecc14b2923fa71f46b13cb31a95630
tkcfjips <tkcfjips@gmail.com>
2015-08-21 07:13:35 +0900

    rm comment

--- Casks/gInbox.rb
+++ Casks/gInbox.rb
@@ -5,7 +5,7 @@ cask :v1 => 'gInbox' do
   url 'https://github.com/chenasraf/gInbox/releases/download/0.2.3c/gInbox-0.2.3c.zip'
   name 'Inbox by Google'
   homepage 'https://github.com/chenasraf/gInbox'
-  license :apache    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :apache
 
   app 'Inbox by Google.app'
 end
