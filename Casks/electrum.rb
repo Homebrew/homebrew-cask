cc96fc0f9af73c0cec65f080b27a5596d4521842
Matt Tanous <mtanous22@gmail.com>
2015-08-19 23:18:03 -0600

    Electrum v2.4.2 update

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'electrum' do
-  version '2.3.2'
-  sha256 '56c5a5fa3283de0b6d0af1e0700405be65bd081edaa11406a6e267ab48ed6df4'
+  version '2.4.2'
+  sha256 '4ff76222390507ec935d7b10433bfc59eb06f2f41c02b8a5acf47b16368f4b6a'
 
   url "https://download.electrum.org/electrum-#{version}.dmg"
   gpg "#{url}.asc",
