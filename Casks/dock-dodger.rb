1a61cf6b6c8ef477e34de9446679919eb02e45be
Muratcan Simsek <mail@muratcansimsek.com>
2015-08-22 15:45:59 +0300

    Add Dock Dodger
    An old app (left from 2007) that still works with 10.11.
    It removes Dock icon from the apps, as advertised. Only link
    to the app is on MacUpdates.

---
+++ Casks/dock-dodger.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'dock-dodger' do
+  version '0.1'
+  sha256 '0dd4c4956d2fe0b223cd84cdeca8f82ffc51bf90ebdd0fb599c61459dfba2eeb'
+
+  url 'http://dl.macupdate.com/prod/Dock%20Dodger.zip'
+  name 'Dock Dodger'
+  homepage 'http://foggynoggin.com'
+  license :unknown
+
+  app 'Dock Dodger.app'
+end
