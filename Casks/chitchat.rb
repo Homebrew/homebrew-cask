a67ddcc958403fa8006841d598e1378d069da90d
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-22 20:18:23 +0300

    chitchat.rb: upgrade to v1.3

--- Casks/chitchat.rb
+++ Casks/chitchat.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'chitchat' do
-  version '1.2.0'
-  sha256 '0e821745282fac10ea235087ad0e12971a5ffb4a5d879d8e0de9642337df373f'
+  version '1.3'
+  sha256 '0851d0ed5e7b45f80d24c1d253eefd39f384624bd75d948dbb01a2994e47464c'
 
   url "https://github.com/stonesam92/ChitCHat/releases/download/v#{version}/ChitChat.zip"
   appcast 'https://github.com/stonesam92/ChitChat/releases.atom'
