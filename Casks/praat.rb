2ea22be8cad9198cf54f7fd03683d308ee80866a
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-08-23 08:28:49 +0200

    praat: upgrade to 5.4.17 and update download URL pattern

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,14 +1,15 @@
 cask :v1 => 'praat' do
-  version '5.4.15'
+  version '5.4.17'
 
   if Hardware::CPU.is_32_bit? || MacOS.release <= :snow_leopard
-    sha256 '83ecf829b0b31a5b3813957f83cb01c71c0800ac3ef402020ce1954c427d4744'
-    url "http://www.fon.hum.uva.nl/praat/praat#{version.delete('.')}_mac32.dmg"
+    sha256 '7892904107b17aeff4c9cd4c08daadf2d57d821c1ee8ce0837b307fabacdf96c'
+    url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.delete('.')}_mac32.dmg"
   else
-    sha256 '9781f8a8fabd4d85daafeeaf6a015bf8304ecd97cf8e20ed39a0e2f910fc46bb'
-    url "http://www.fon.hum.uva.nl/praat/praat#{version.delete('.')}_mac64.dmg"
+    sha256 '0c5201831a215fa5e8c64e01dbeb5a07cf933fd5941d718df9b98e961f20f062'
+    url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.delete('.')}_mac64.dmg"
   end
 
+  appcast 'https://github.com/praat/praat/releases.atom'
   name 'Praat'
   homepage 'http://www.fon.hum.uva.nl/praat/'
   license :gpl
