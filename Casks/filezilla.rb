cd4fcf9fbe819263a73a75b1d1f71978aec360f1
Joao C Costa <joaocc@users.noreply.github.com>
2015-08-23 16:21:18 +0100

    Update filezilla.rb

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -4,8 +4,8 @@ cask :v1 => 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.12.0.2'
-    sha256 '628feaaf36a93bbd0fe2e21d9d9c201320d99ca43d9a2230325830ff1f95db78'
+    version '3.13.0'
+    sha256 '0b17c397c8bacd659c5eca58e7ff7b7661603104dae1c5c905ad8b13f91bf327'
   end
 
   # sourceforge.net is the official download host per the vendor homepage
