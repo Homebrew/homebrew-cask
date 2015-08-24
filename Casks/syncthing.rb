26607a4713a424299d9dbbcd4301303a960d6b3a
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2015-08-24 01:41:01 -0300

    Bump syncthing version to 0.11.21
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

--- Casks/syncthing.rb
+++ Casks/syncthing.rb
@@ -1,19 +1,14 @@
 cask :v1 => 'syncthing' do
-  version '0.11.19'
+  version '0.11.21'
+  sha256 'f7089e6e9c4382a197b6f8b1caa9b63239dcb43a4f950b12dba0db25b2b4af77'
+
+  url "https://github.com/syncthing/syncthing/releases/download/v#{version}/syncthing-macosx-amd64-v#{version}.tar.gz"
   appcast 'https://github.com/syncthing/syncthing/releases.atom'
   name 'Syncthing'
-  homepage 'https://syncthing.net'
+  homepage 'https://syncthing.net/'
   license :mpl
 
-  if Hardware::CPU.is_32_bit?
-    url "https://github.com/syncthing/syncthing/releases/download/v#{version}/syncthing-macosx-386-v#{version}.tar.gz"
-    sha256 '5c2cfd70252c36082495acaaf7b7eaa976803f53a264a971903d5d3fedab1689'
-    binary "syncthing-macosx-386-v#{version}/syncthing"
-  else
-    url "https://github.com/syncthing/syncthing/releases/download/v#{version}/syncthing-macosx-amd64-v#{version}.tar.gz"
-    sha256 'adeac70b7caabcb74d93e40e6a6522830c156482e03b04867c9dab8461e7bb1b'
-    binary "syncthing-macosx-amd64-v#{version}/syncthing"
-  end
+  binary "syncthing-macosx-amd64-v#{version}/syncthing"
 
   zap :delete => '~/Library/Application Support/Syncthing'
 end
