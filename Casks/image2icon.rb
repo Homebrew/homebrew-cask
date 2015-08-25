92c12428168b1737e52ac26794178ac3315145b8
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-26 00:24:59 +0300

    image2icon.rb: add depends_on and zap, update license and url

--- Casks/image2icon.rb
+++ Casks/image2icon.rb
@@ -3,10 +3,18 @@ cask :v1 => 'image2icon' do
   sha256 'c04c2cca6f6531271579af9f767322c9b08fc435dabb158929312ddceb1d824e'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "http://sf-applications.s3.amazonaws.com/Image2Icon/app-releases/Image2icon#{version}.zip"
+  url "https://sf-applications.s3.amazonaws.com/Image2Icon/app-releases/Image2icon#{version}.zip"
   name 'Image2icon'
   homepage 'http://www.img2icnsapp.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
+
+  depends_on :macos => '>= :mavericks'
 
   app 'Image2Icon.app'
+
+  zap :delete => [
+                  '~/Library/Caches/net.shinyfrog.image2icon',
+                  '~/Library/Preferences/net.shinyfrog.image2icon.plist',
+                  '~/Library/Saved Application State/net.shinyfrog.image2icon.savedState'
+                 ]
 end
