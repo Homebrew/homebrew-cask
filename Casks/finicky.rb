1e3c2d4c641a354630f0ab25f121aadbce24be0b
John Sterling <john.sterling@gooengine.com>
2015-08-25 09:56:30 +0200

    finicky.rb: upgrade to v0.5

--- Casks/finicky.rb
+++ Casks/finicky.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'finicky' do
-  version '0.4'
-  sha256 '72f482279ca346f86f5d7d906a7c55da3e8eb446d3105979cbfc2b02d3ead154'
+  version '0.5'
+  sha256 'a2eba88c657786056d0f867c983f31c786adfe5ecde1aa3c8df94e18c536775c'
 
   url "https://github.com/johnste/finicky/releases/download/v#{version}/Finicky.zip"
   appcast 'https://github.com/johnste/finicky/releases.atom'
