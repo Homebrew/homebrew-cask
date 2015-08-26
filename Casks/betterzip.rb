288fac107952695a3b1e067acbcec029c925adf8
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-26 22:35:45 +0300

    betterzip.rb: upgrade to v3.0.3

--- Casks/betterzip.rb
+++ Casks/betterzip.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'betterzip' do
-  version '3.0.2'
-  sha256 'f080e827353743ae4beb1e5606f53e3b026f6deafe1834445f9b492a996045d8'
+  version '3.0.3'
+  sha256 '5bcce666ff501957cf535d13d156ef7e5da79f7af283a2f911ef7e959c847594'
 
   url "http://macitbetter.com/dl/BetterZip-#{version}.zip"
   name 'BetterZip'
   appcast 'http://macitbetter.com/BetterZip3.rss',
-          :sha256 => '3cf5127339a91d7bd03b6cacf2794d6d2badf7954741312d350ccc3e008a26ae'
+          :sha256 => 'e219c96ec87b7df24d997134efc056f027523021ff4f26b033286d1dd884abe0'
   homepage 'http://macitbetter.com'
   license :commercial
 
