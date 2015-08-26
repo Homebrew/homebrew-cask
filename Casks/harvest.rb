4079d103a3c58cabb2becd72adc945a06c69629d
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-26 22:40:38 +0300

    harvest.rb: upgrade to v1.4.4

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'harvest' do
-  version '1.4.2'
-  sha256 '1c1945d27b87943ca62d125d6f125ab9d3222ca62e028c062759e41892296e39'
+  version '1.4.4'
+  sha256 '27d5f2a5dc9f954c6fb073a77ea6d17a16c69082a0b9a046682e26ab29fca9d9'
 
   url "https://www.getharvest.com/harvest/mac/Harvest.#{version}.zip"
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml'
