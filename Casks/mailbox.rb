9462fad8e3817bbd01a6d065d48e28c039f0594a
Sean Zhu <interestinglythere@gmail.com>
2015-08-27 03:53:05 -0700

    Update Mailbox to 0.7.3

--- Casks/mailbox.rb
+++ Casks/mailbox.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'mailbox' do
-  version '0.7.2'
-  sha256 '4472d761e05b99dc15f0476c7092ad6138bb8de1399c907452cfc5bf08196391'
+  version '0.7.3'
+  sha256 '9c2e83983a4034c1eafb16c10802b68c0979124a5843fb6470695806e68bf02f'
 
   # hockeyapp.net is the official download host per the appcast feed
-  url 'https://rink.hockeyapp.net/api/2/apps/0de2e5766e01cde1f6c0fd5b9862c730/app_versions/4?format=zip&avtoken=b70a5f71b15fc402ec1db83426c0ecd6d2601f6b'
+  url 'https://rink.hockeyapp.net/api/2/apps/0de2e5766e01cde1f6c0fd5b9862c730/app_versions/5?format=zip&avtoken=44ed8a6ca2e22c96feb72d8cd89f638e64628811'
   appcast 'https://rink.hockeyapp.net/api/2/apps/0de2e5766e01cde1f6c0fd5b9862c730',
-          :sha256 => '0a511ea99347c010c1df7ce45027ba412bedf47bde35de089bc5d13c3ba6c779'
+          :sha256 => 'e4899665be89ccf97261f8d8a70f0401259ce171b36180db0858c99e30e5a273'
   name 'Mailbox'
   homepage 'https://www.mailboxapp.com/'
   license :gratis
