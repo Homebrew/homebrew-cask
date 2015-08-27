a1b865faff6b8135cac6c46257080499cdf35ba3
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-27 19:40:45 +0300

    desktop-log.rb: add license

--- Casks/desktop-log.rb
+++ Casks/desktop-log.rb
@@ -5,7 +5,7 @@ cask :v1 => 'desktop-log' do
   url 'http://www.nightproductions.net/downloads/desklog.dmg'
   name 'Desktop Log'
   homepage 'http://www.nightproductions.net/desklog.html'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
   tags :vendor => 'Night Productions'
 
   app 'Desktop Log.app'
