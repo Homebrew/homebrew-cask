41175126a9051e55b0a678d1ed6840321acbc0c1
Scott Reeves <cottser@mailworks.org>
2015-08-26 21:30:03 -0400

    Update Acorn to 5.0

--- Casks/acorn.rb
+++ Casks/acorn.rb
@@ -4,8 +4,8 @@ cask :v1 => 'acorn' do
 
   url 'http://flyingmeat.com/download/Acorn.zip'
   name 'Acorn'
-  appcast 'http://www.flyingmeat.com/download/acorn4update.xml',
-          :sha256 => '190b994807371fa5d7acde2178af97d549ecf95b349e5d9cf40ca854c0160caf'
+  appcast 'http://www.flyingmeat.com/download/acorn5update.xml',
+          :sha256 => '62f48162e1f7d0d8d03e13dd47411714d86afb0e8a2f7b5c95bd3d9906e66da9'
   homepage 'http://flyingmeat.com/acorn/'
   license :commercial
 
