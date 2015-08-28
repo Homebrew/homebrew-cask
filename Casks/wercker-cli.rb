58100c94df6be14ae51c8b187c5c68ad69029831
Xavier Krantz <xkrantz@viadeoteam.com>
2015-08-28 16:50:22 -0700

    wercker (cli): Fixed brew for integration

--- Casks/wercker-cli.rb
+++ Casks/wercker-cli.rb
@@ -1,13 +1,13 @@
 cask :v1 => 'wercker-cli' do
-  version ':lastest'
-  sha256 '3db5ec63989170414293af1c07fd83ec86d1d14c6b5413d2604f97ac771a5294'
+  version :latest
+  sha256 :no_check
 
-  # The vendor use Amazon S3 storage service to deliver files over HTTP
+  # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker'
   name 'wercker'
   name 'wercker-cli'
   homepage 'http://wercker.com/'
-  license :unknown
+  license :mit
 
   container :type => :naked
   binary 'wercker'
