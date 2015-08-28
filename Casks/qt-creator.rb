45733d58a63e8c444310c0b8b99fca53c5335287
Vicken Simonian <vsimon@gmail.com>
2015-08-28 11:38:11 -0700

    Upgrade Qt-Creator to v3.5.0

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qt-creator' do
-  version '3.4.2'
-  sha256 '2908e39ebc1710746f2deee00ccf09758c4c1563231bf6940f154376b0ec6f4c'
+  version '3.5.0'
+  sha256 '14c9e90a71679eea7d70ee1f60ba1dd5ad595415326d12a9578d0fbcdc9ecec7'
 
   url "http://download.qt.io/official_releases/qtcreator/#{version.to_f}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
