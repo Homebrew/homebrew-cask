dcef4498bd2065ff75e01d2164ac50802c7453af
cfillion <cfillion@users.noreply.github.com>
2015-08-29 19:40:43 -0400

    spectacle 1.0

--- Casks/spectacle.rb
+++ Casks/spectacle.rb
@@ -4,11 +4,11 @@ cask :v1 => 'spectacle' do
     version '0.8.6'
     sha256 '3e367d2d7e6fe7d5f41d717d49cb087ba7432624b71ddd91c0cfa9d5a5459b7c'
   else
-    version '0.8.10'
-    sha256 '26e4ccc906a82c5df9d1d462f1b691fce746aea43405b178d3d230fb23551d44'
+    version '1.0'
+    sha256 '98c26000f5c3e4a1cd83d0c37ed6e4c5da9f8791281eb186d46a29651ff973b5'
 
     appcast 'http://spectacleapp.com/updates/appcast.xml',
-            :sha256 => '5d75e2e07886ca135916e224b4b5c1468d9af1ea8ef355db33b28bff511fa6b2'
+            :sha256 => '749481dae440e181fef0f538185454f1661fb9ae017dc8e370455894880865f6'
   end
 
   # amazonaws.com is the official download host per the vendor homepage
