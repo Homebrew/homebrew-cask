c0317c19e0ac3717dd42b70ea9adc5a9a0341478
Vítor Galvão <info@vitorgalvao.com>
2015-08-29 02:04:04 +0100

    updated duelystlauncher (0.0.4)

--- Casks/duelystlauncher.rb
+++ Casks/duelystlauncher.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'duelystlauncher' do
-  version '0.0.3'
-  sha256 'a990d87dda6e86d9da826348ca81e5d96d0cc58145e94e4762adaeca6d5e636d'
+  version '0.0.4'
+  sha256 '8a5fbe6fdba433cf7d57d8da8f238d136f8e0ce436cf250101df56a71f4da898'
 
   # netdna-ssl.com is the official download host per the vendor homepage
   url "https://downloads-counterplaygames.netdna-ssl.com/launcher/v#{version}/DuelystLauncher-v#{version}.dmg"
