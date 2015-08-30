a0c6c353e745891a8e2dc205ec87ddcd49cdf697
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-30 23:48:56 +0300

    qnapi.rb: upgrade to v0.1.9

--- Casks/qnapi.rb
+++ Casks/qnapi.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qnapi' do
-  version '0.1.7'
-  sha256 'bab249caa2ca961b97654679094e8dc535f67ea37cdb9a241ae54a20841eae17'
+  version '0.1.9'
+  sha256 '0c8c1ef1ca74af0abc37d5af6b31374e4aad93e16261c488bb6e581b2946606a'
 
   url "https://github.com/QNapi/qnapi/releases/download/#{version}/QNapi-#{version}.dmg"
   appcast 'https://github.com/QNapi/qnapi/releases.atom'
