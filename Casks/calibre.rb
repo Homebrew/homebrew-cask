918a5a259545cdd33b4aed0a393092a3fd36c7e7
Vítor Galvão <info@vitorgalvao.com>
2015-08-30 12:51:29 +0100

    calibre.rb: download from calibre-ebook.com again

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -2,16 +2,13 @@ cask :v1 => 'calibre' do
   if MacOS.release < :mountain_lion
     version '1.48.0'
     sha256 '0533283965fbc9a6618d0b27c85bdf3671fe75ff0e89eeff406fe1457ee61b14'
-    url "http://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
   else
     version '2.36.0'
     sha256 '2ab762f4d2829116bd0860b551f141ca5f20a7ff26ec8ab6e4b66dc59c210b61'
-
-    # github.com is an official download host per the vendor homepage, and a faster mirror than the main one
-    url "https://github.com/kovidgoyal/calibre/releases/download/v#{version}/calibre-#{version}.dmg"
     appcast 'https://github.com/kovidgoyal/calibre/releases.atom'
   end
 
+  url "http://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
   name 'calibre'
   homepage 'http://calibre-ebook.com/'
   license :gpl
