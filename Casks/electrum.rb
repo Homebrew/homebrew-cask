159ef513218defad0320be0bdce6aeddc6c5c663
wKovacs64 <justin.r.hall@gmail.com>
2015-08-31 09:20:50 -0600

    Update electrum to 2.4.4
    Also, update key ID. Binaries are now signed by ThomasV rather than Animazing.

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'electrum' do
-  version '2.4.2'
-  sha256 '4ff76222390507ec935d7b10433bfc59eb06f2f41c02b8a5acf47b16368f4b6a'
+  version '2.4.4'
+  sha256 '0cb4e9c1edabff6d2ec7884f27db6e21a94bdd05629677df9ab6f7d40692e1b4'
 
   url "https://download.electrum.org/electrum-#{version}.dmg"
   gpg "#{url}.asc",
-      :key_id => '9914864dfc33499c6ca2beea22453004695506fd'
+      :key_id => '6694d8de7be8ee5631bed9502bd5824b7f9470e6'
   name 'Electrum'
   homepage 'https://electrum.org/'
   license :gpl
