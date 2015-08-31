d3cc4a850efd6bff2c8e2fe3f73788c890b5bc62
teressa <blackbriar21@gmail.com>
2015-08-31 15:20:09 -0400

    paw.rb: change download URL to official site

--- Casks/paw.rb
+++ Casks/paw.rb
@@ -2,8 +2,7 @@ cask :v1 => 'paw' do
   version '2.2.2'
   sha256 '80af2f3bfd973c920d636453f9a9fb8b1d7c3fbd461e72132c2bf12373d06254'
 
-  # amazonaws.com is the official download host per the vendor homepage
-  url "https://luckymarmot-distribution.s3.amazonaws.com/paw/Paw-#{version}.zip"
+  url "https://luckymarmot.com/paw/download/#{version}"
   name 'Paw'
   homepage 'https://luckymarmot.com/paw'
   license :commercial
