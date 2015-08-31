f0ab965f5217b3361506854932b39d6ea08be53d
Tai Lee <real.human@mrmachine.net>
2015-08-31 16:55:17 +1000

    Update SHA for Navicat Premium 11.1.13

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'navicat-premium' do
   version '11.1.13'   # navicat-premium.rb and navicat-for-* should be upgraded together
-  sha256 'a20731c590c75a04795c4f5993b0dc68e204482c43e7ce025661f401da6a67e7'
+  sha256 'a69c3971fcd7f85afb3c8b139471970220ecdc2bcaf413340ab31e9d8286c2ba'
 
   url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_premium_en.dmg"
   name 'Navicat Premium'
