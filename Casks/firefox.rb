c92aee21fda2e8298796181a478678839a278811
JP Barbosa <jp@jp7.com.br>
2015-08-30 22:10:11 -0300

    Use Mozilla CDN for 4x faster download
    Without CDN: 4 minutes
    Using CDN: 1 minute
    Info: 30 Mbps Internet connection in São Paulo, Brazil

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -2,7 +2,7 @@ cask :v1 => 'firefox' do
   version '40.0.3'
   sha256 '99cfc9073453a41803f4b2992b8f2655376c17d0dcff3ee5ad0474e08ca26098'
 
-  url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
+  url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
   name 'Firefox'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/'
