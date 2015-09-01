daf3161c6c35b877af39551d10aac13b1ae01413
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-01 19:03:01 +0300

    bettertouchtool.rb: upgrade to v1.16

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -7,8 +7,8 @@ cask :v1 => 'bettertouchtool' do
     # bettertouchtool.com is the official download host per the vendor homepage
     url "http://bettertouchtool.net/btt#{version}.zip"
   else
-    version '1.15'
-    sha256 'a41f1a637de542fd36af67524e1ee83d964ed8ede086cab49074761705c316e7'
+    version '1.16'
+    sha256 '2e52ee5eeccdb5cc4fd48af9624c04be0ea16694e86a4cad2929d6d583f09765'
     url "http://boastr.net/releases/btt#{version}.zip"
   end
 
