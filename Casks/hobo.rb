68b29381126a26612dc1a447a85470ee145649d0
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-01 19:08:14 +0300

    hobo.rb: upgrade to v1.1.5

--- Casks/hobo.rb
+++ Casks/hobo.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'hobo' do
-  version '1.1.4'
-  sha256 'ff7b28a846ed10387d1d4bca18cbb514a735d1374798b03cf540adf5c27890c2'
+  version '1.1.5'
+  sha256 '7ecdf07aa0b9f396f5323e3ef134b5257a13ac5621195f665480158c82dd8eec'
 
   # netdna-ssl.com is the official download host per the vendor homepage
   url "https://downloads-clickonideas.netdna-ssl.com/hobo/hobo_#{version}.zip"
