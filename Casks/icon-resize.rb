776971608872254f74de528c68c932aec8bca4d9
赵新 <zhaoxin@bisai360.com>
2015-09-01 16:05:13 +0800

    test

---
+++ Casks/icon-resize.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'icon-resize' do
+  version '1.10'
+  sha256 '49fccdf461063cac3233c9472ce03cde690b8a6628fe964d17b269128b3e6c50'
+
+  url "https://itunes.apple.com/cn/app/icon-resize/id730352774?mt=12&ign-mpt=uo%3D8"
+  name 'WAN LUTFI WAN MD HATTA'
+  homepage 'https://www.zotero.org/'
+  license :affero
+
+  app 'icon-resize.app'
+end
