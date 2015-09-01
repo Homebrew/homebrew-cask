7fc718d46be87ec0eabad9c071f6aa6e2be96e98
kkdd <kkondo_z0@nifty.com>
2015-09-02 01:16:05 +0900

    Update imageup.rb

--- Casks/imageup.rb
+++ Casks/imageup.rb
@@ -1,14 +1,8 @@
 cask :v1 => 'imageup' do
   homepage 'http://software.cockscomb.info/imageup/'
-  version '0.7.3'
-  url "http://download.cockscomb.info/software/imageup/ImageUp.zip"
-  sha256 'ff845d526f2e99e0802e529a5934907df58c02d2a0d8a24baabdc0ae2a7adc9e'
+  version :latest
+  url 'http://download.cockscomb.info/software/imageup/ImageUp.zip'
+  sha256 :no_check
   license :unknown
   app 'ImageUp.app'
-  caveats <<-EOS.undent
-    ImageUp:
-    - Display the "input source" anywhere
-    - version 0.7.3 is observed to work on MacOSX 10.10
-    - 32-bit application
-  EOS
 end
