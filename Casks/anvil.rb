b84bb323c77c8b325716d6df9584a8e573a2c3b9
Carlos Precioso <kalprestito@gmail.com>
2015-09-02 23:31:51 +0200

    Upgrade Anvil to 1.1.5

--- Casks/anvil.rb
+++ Casks/anvil.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'anvil' do
-  version '1.1.4'
-  sha256 '757f64ec4b569eef104300fe111d1180fc5e38e0a83e41954b46e209af8a5c1b'
+  version '1.1.5'
+  sha256 '342c9041dea854d43fd0c9f78305c9e269e10b2f594079acb2af11675c53fa55'
 
   # herokuapp.com is the official download host as per the vendor homepage
-  url "https://sparkler.herokuapp.com/apps/3/versions/#{version.delete('.')}/download"
+  url 'https://sparkler.herokuapp.com/apps/3/versions/116/download'
   name 'Anvil'
   appcast 'https://sparkler.herokuapp.com/apps/3/updates.xml'
   homepage 'http://anvilformac.com/'
