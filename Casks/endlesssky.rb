05dbfb29ca332e6a1a8c60843dc17d9b0d8fd7b3
Ryan Duryea <aguynamedryan@gmail.com>
2015-09-02 12:00:03 -0700

    Update EndlessSky to v0.8.2

--- Casks/endlesssky.rb
+++ Casks/endlesssky.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'endlesssky' do
-  version '0.8.1'
-  sha256 'f1a02bcbce7af263a86f75ed469ae71b11a2c3e5371531ccd809fbce313ab1ea'
+  version '0.8.2'
+  sha256 '65ae266e629c17b46fa69593fdbb7b318620f4eb68085f135e63046dae6751ad'
 
   url "https://github.com/endless-sky/endless-sky/releases/download/v#{version}/endless-sky-macosx-#{version}.dmg"
   appcast 'https://github.com/endless-sky/endless-sky/releases.atom'
