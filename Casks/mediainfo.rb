53808336d00c8ce017edb7837560b963e9e2de7e
Terry Nycum <tjnycum@users.noreply.github.com>
2015-09-02 16:24:10 -0700

    mediainfo 0.7.77

--- Casks/mediainfo.rb
+++ Casks/mediainfo.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mediainfo' do
-  version '0.7.76'
-  sha256 'e41893836093c8174b8d82b621d56ecca553dfe701ef6eb7722b09daf61cd936'
+  version '0.7.77'
+  sha256 '564f4caaba98b8f12e28a4d731354f0ef1385b788bc0966ec466c79737469922'
 
   url "https://mediaarea.net/download/binary/mediainfo-gui/#{version}/MediaInfo_GUI_#{version}_Mac.dmg"
   name 'MediaInfo'
