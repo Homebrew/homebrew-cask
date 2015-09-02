09d6714877d2b3845c2658b891376b85a35e802d
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-02 23:25:16 +0300

    hype.rb: add license

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -7,7 +7,7 @@ cask :v1 => 'hype' do
   appcast 'https://tumult.com/hype/appcast_hype2.xml',
           :sha256 => 'b8435a77bb13d39ddaecb92a1510786ceb135225e207aaaac91e7ef230d9edea'
   homepage 'http://tumult.com/hype/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
   tags :vendor => 'Tumult'
 
   app "Hype #{version.to_i}.app"
