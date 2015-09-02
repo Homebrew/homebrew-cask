c214a9366a23cea0f672fd8a7bbfd9994e1cd33b
Steve Dignam <steve@dignam.xyz>
2015-09-01 23:29:41 -0400

    updated download link to official website

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -2,8 +2,7 @@ cask :v1 => 'the-unarchiver' do
   version '3.9.1'
   sha256 '4911c332df7f4bb23877652700e845fe097b793ae37450948319398009e923a3'
 
-  # googlecode.com is the official download host per the vendor homepage
-  url "https://theunarchiver.googlecode.com/files/TheUnarchiver#{version}_legacy.zip"
+  url "http://unarchiver.c3.cx/downloads/TheUnarchiver#{version}_legacy.zip"
   name 'The Unarchiver'
   homepage 'http://unarchiver.c3.cx/unarchiver'
   license :oss
