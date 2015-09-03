c34ddd99fa332ae554fc18909e28f5064168c87a
Vítor Galvão <info@vitorgalvao.com>
2015-09-03 11:17:32 +0100

    updated p5 (0.5.1)

--- Casks/p5.rb
+++ Casks/p5.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'p5' do
   # note: "5" is not a version number, but an intrinsic part of the product name
-  version '0.5.0'
-  sha256 'a78f54f19d750e74119c7bce77ef6b2cae4e6b591d692c149a99b2e7610c7ff2'
+  version '0.5.1'
+  sha256 '463c908f795a07b89ebc98cd4ba23e319d557cfb94b056f21a5a9ecbbe92b5f1'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/processing/p5.js-editor/releases/download/v#{version}/p5-mac.zip"
