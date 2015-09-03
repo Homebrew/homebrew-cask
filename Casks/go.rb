078bf7f626aa80109ae9eb693793cda7521dc671
Vítor Galvão <info@vitorgalvao.com>
2015-09-03 23:38:54 +0100

    updated go (1.5)

--- Casks/go.rb
+++ Casks/go.rb
@@ -1,14 +1,22 @@
 cask :v1 => 'go' do
-  version '1.4.2'
-
   if MacOS.release <= :snow_leopard
+    version '1.4.2'
     sha256 '8b8fe5c723663c2da0381643b2f04b0a597f623149095db61818c3fe22be9582'
+
     url "https://storage.googleapis.com/golang/go#{version}.darwin-amd64-osx10.6.pkg"
     pkg "go#{version}.darwin-amd64-osx10.6.pkg"
-  else
+  elsif MacOS.release <= :lion
+    version '1.4.2'
     sha256 '0407ca952f48315b3a6de99625c2fbe348c88936754539f4fcb244319e04a6b5'
+
     url "https://storage.googleapis.com/golang/go#{version}.darwin-amd64-osx10.8.pkg"
     pkg "go#{version}.darwin-amd64-osx10.8.pkg"
+  else
+    version '1.5'
+    sha256 '26891fed36b7fb4a0c8ce069d8adcb3120bc44fc28c91d1e43bc0d4b8b64dfcf'
+
+    url "https://storage.googleapis.com/golang/go#{version}.darwin-amd64.pkg"
+    pkg "go#{version}.darwin-amd64.pkg"
   end
 
   name 'Go'
