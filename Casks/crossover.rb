90368bc0d01fa3d1aba7e3a493b330fb93d9458d
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-03 21:00:25 +0300

    crossover.rb: upgrade to v14.1.6

--- Casks/crossover.rb
+++ Casks/crossover.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'crossover' do
-  version '14.1.4'
-  sha256 '613456ea10d16d17670501f2f1e8c19d9584e31c99efbf6363380139418a2876'
+  version '14.1.6'
+  sha256 '17eb7c8c19868abcec42c9ab8a43e20c32219c36da01ea2f831e5fddfeef33c6'
 
   url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version}.zip"
   name 'CrossOver'
   appcast 'https://www.codeweavers.com/xml/versions/cxmac.xml',
-          :sha256 => '75c89f7ed67f8fde23d9348c94fd9a801dff8d8c836e34410a9278599597b55c'
+          :sha256 => 'b3fb4692b914070c69ecc2ad4f7b4fa25a93016bb701760ac33c6741388a232e'
   homepage 'https://www.codeweavers.com/products/crossover-mac/'
   license :commercial
 
