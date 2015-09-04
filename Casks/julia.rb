c272aa03455730514e565a95b2b2238ae613faa1
Sean Garborg <sean.garborg@gmail.com>
2015-09-04 15:06:28 -0600

    Bump Julia to latest release

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'julia' do
-  version '0.3.10'
-  sha256 '0e02d51efb0111fa6ae5719470aa44d1ca411d7c154e8b9072e26f46c664bfb9'
+  version '0.3.11'
+  sha256 'b5d98ff9d181401b841e5accc24719b05c672700daf63c4ed54d50ebe87c59a8'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/julialang/bin/osx/x64/#{version.sub(/\.\d+$/, '')}/julia-#{version}-osx10.7+.dmg"
