00b547a77d7832b0765be8e283fd8030f49ced37
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-04 19:57:54 +0300

    invisionsync.rb: add license

--- Casks/invisionsync.rb
+++ Casks/invisionsync.rb
@@ -7,7 +7,7 @@ cask :v1 => 'invisionsync' do
   appcast 'https://projects.invisionapp.com/native_app/mac/sparkle/appcast_v2.xml',
           :sha256 => '13265e17eb103d1f566047dd2817e4dea53122291e07ac33d6331459261b3134'
   homepage 'http://invisionapp.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'InVisionSync.app'
 end
