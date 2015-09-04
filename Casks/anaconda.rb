1e98761f24abcc7430563009cdec92c6b10c0bb5
adityadalal924 <adityadalal924@users.noreply.github.com>
2015-09-04 17:24:09 -0400

    added app stanza

--- Casks/anaconda.rb
+++ Casks/anaconda.rb
@@ -1,15 +1,29 @@
 cask :v1 => 'anaconda' do
   version '2.3.0'
-  sha256 '6a0c94a49f41f9fda0138c8e966bd7b0a8965d6648fd21ffbd645d1453848ba5'
+  sha256 'c4bb59a57bf44dde80612041bbbcfd2e5cab8534842209ef456da7a46f919c33'
 
-  url "https://repo.continuum.io/archive/Anaconda3-#{version}-MacOSX-x86_64.sh"
+  url "https://repo.continuum.io/archive/Anaconda-#{version}-MacOSX-x86_64.sh"
   name 'Anaconda'
   homepage 'https://store.continuum.io/cshop/anaconda/'
   license :gratis
   tags :vendor => 'Continuum Analytics'
 
-  # TODO: install stanzas
+  installer :script => 'Anaconda-#{version}-MacOSX-x86_64.sh',
+            :args => [ '-b', '-p', '#{staged_path}' ]
+
+  app 'Launcher.app'
 
   depends_on :macos => '>= :lion'
 
+  caveats do
+      <<-EOS.undent
+      To use Anaconda from the command line, you must add the 
+      $ANACONDA_ROOT/bin directory to your PATH environment variable.
+      For bash shell, add these lines to ~/.bash_profile:
+
+        export ANACONDA_ROOT=#{staged_path}
+        export PATH=$ANACONDA_ROOT/bin:"$PATH"
+
+  EOS
+  end
 end
