303dc17adfbcdcaf227ea5e8b2e6a7ba365fb61a
Robert Sauer <robert.sauer@marklogic.com>
2015-09-04 10:15:53 +0200

    upgrading to SmartGit 7.0.0

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'smartgit' do
-  version '6.5.7'
-  sha256 'c56e43e25785384906111a724acdf353b1cfecbc9bc7f8d0f5b5020e95662a67'
+  version '7.0.0'
+  sha256 '21cf4da86c4c58a4147af51e189a12f0c4215477e6410c87c80306684036f783'
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.gsub('.', '_')}.dmg"
   name 'SmartGit'
