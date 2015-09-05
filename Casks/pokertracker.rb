3837e373f14784538d100249727a23a9a5bb9278
Gustavo Andrade <xgusth@gusth-pc.local>
2015-09-05 14:14:05 -0300

    Update PokerTracker Version

--- Casks/pokertracker.rb
+++ Casks/pokertracker.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'pokertracker' do
-  version '4.12.1'
+  version '4.14'
   sha256 'd549d431f0a792b723d0dba6121652eb565d484d358e29b082f138fa1657da28'
 
   url "http://s3-us1.ptrackupdate.com/releases/PT-Install-v#{version}.dmg"
