05b090aa014529ad1443713d11d6783cab386aa7
adityadalal924 <adityadalal924@users.noreply.github.com>
2015-09-06 10:46:36 -0400

    updated easyfig to 2.2.2

--- Casks/easyfig.rb
+++ Casks/easyfig.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'easyfig' do
-  version '2.1.0'
-  sha256 '470809860367a448cd931a2ed54caed116576f1cdf5795cba6ad1e8be186bec4'
+  version '2.2.2'
+  sha256 'd9375b3a1246bb81aa59c53e4391b1ba76db66bf0a8e1028b533f2aac8b44134'
 
-  url "https://github.com/mjsull/Easyfig/releases/download/#{version}/Easyfig_mac_#{version.to_f}.tar.gz"
+  url "https://github.com/mjsull/Easyfig/releases/download/#{version}/Easyfig_#{version}_OSX.zip"
   appcast 'https://github.com/mjsull/Easyfig/releases.atom'
   name 'EasyFig'
   homepage 'http://mjsull.github.io/Easyfig/'
