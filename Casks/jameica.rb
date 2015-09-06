b24d62b484221b3bbc3a3d7291dd35c8c9784b59
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-06 20:09:22 +0300

    jameica.rb: add license

--- Casks/jameica.rb
+++ Casks/jameica.rb
@@ -7,7 +7,7 @@ cask :v1 => 'jameica' do
       :key_id => '5a8ed9cfc0db6c70'
   name 'Jameica'
   homepage 'https://www.willuhn.de/products/jameica/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gpl
 
   app 'Jameica.app'
 end
