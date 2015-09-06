992f71efc630b5d8b62f789230830d840d829121
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-06 20:20:47 +0300

    kamakiri.rb: add license

--- Casks/kamakiri.rb
+++ Casks/kamakiri.rb
@@ -7,7 +7,7 @@ cask :v1 => 'kamakiri' do
   appcast 'https://mochidev.com/appresources/updates/kamakiridd.xml',
           :sha256 => 'cdaff5053910c08c9cf39d1e967c6581d157b564cac26a5f285f51d4ef6fae66'
   homepage 'https://mochidev.com/apps/kamakiri/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'Kamakiri.app'
 end
