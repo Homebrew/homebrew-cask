f40c950cff7f80e3b1af0c338d3fff99129b077e
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-06 20:18:22 +0300

    jump-desktop.rb: add license

--- Casks/jump-desktop.rb
+++ Casks/jump-desktop.rb
@@ -7,7 +7,7 @@ cask :v1 => 'jump-desktop' do
   appcast 'http://service.jumpdesktop.com/update/jdmac-web/appcast.xml',
           :sha256 => '146cdb8cdc75e641405a08aa9d409ac2aec195520aacd5c632fdac68ba3ba563'
   homepage 'http://jumpdesktop.com/#jdmac'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'Jump Desktop.app'
 end
