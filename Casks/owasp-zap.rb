8ab8ee087b114d4e02e38ef5db1777b4aec592c0
Bart Leusink <bartleusink@gmail.com>
2015-09-07 17:21:04 +0200

    owasp-zap.rb: upgrade to v2.4.2

--- Casks/owasp-zap.rb
+++ Casks/owasp-zap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'owasp-zap' do
-  version '2.4.1'
-  sha256 '98c9cab401dd95c021ee32cf4030aa63a64f08a82c6fe0d2493663e3c6e1c5a3'
+  version '2.4.2'
+  sha256 'b6d8f3a9632ab99bae4097084df74efd2321c22182aff3a119dd1e69ecc69a63'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/zaproxy/zaproxy/releases/download/#{version}/ZAP_#{version}_MAC_OS_X.dmg"
