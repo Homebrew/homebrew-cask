b4d9337306be7c61b12b08a385a25a6567395296
Beng Hee Eu <beng@benghee.eu>
2015-09-07 12:35:26 -0400

    MacUpdate Desktop 6.0.9

--- Casks/macupdate-desktop.rb
+++ Casks/macupdate-desktop.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'macupdate-desktop' do
-  version '6.0.8'
-  sha256 '79b41692aa623e922455f2ca93b19b6dae1946ea2e48b052df9310e293a2cb1f'
+  version '6.0.9'
+  sha256 '82f8b1bc8d9ed5d3881a8ca618df146fbb82634ecca4e5aac8f96ad65389e7c4'
 
   url "http://cdn.macupdate.com/MacUpdateDesktop#{version}.zip"
   appcast 'http://www.macupdate.com/desktop/updates.xml',
-          :sha256 => '439c1659219eccee31600600843685c6700a440227eb4692c9dec7762f089a81'
+          :sha256 => 'cecc073acecb3a7221854fae2f629759fb59de2de2e161bf68c7f1c9800d9ca2'
   name 'MacUpdate Desktop'
   homepage 'https://www.macupdate.com/desktop'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
