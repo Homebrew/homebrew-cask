63297b66099b152ee413dbc30504abc9cca30b1f
William Boman <william@redwill.se>
2015-09-07 20:56:34 +0200

    Add DisableMonitor.app v1.92

---
+++ Casks/disablemonitor.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'disablemonitor' do
+  version '1.92'
+  sha256 '5bdb362d779f7fbb6cf916d7d14545a79206b8158ce5309f2e5bbcb7203651fb'
+
+  url "https://github.com/Eun/DisableMonitor/releases/download/G#{version}/DisableMonitor-G#{version}.zip"
+  appcast 'https://github.com/Eun/DisableMonitor/releases.atom'
+  name 'DisableMonitor'
+  homepage 'https://github.com/Eun/DisableMonitor'
+  license :gpl
+
+  app 'DisableMonitor.app'
+end
