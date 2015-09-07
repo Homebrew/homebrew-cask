07773ee8e3404cd06e5d89503ef436256272c2d9
Victor Popkov <Dragon.vctr@gmail.com>
2015-09-07 20:13:33 +0300

    tiled.rb: upgrade to v0.13.1

--- Casks/tiled.rb
+++ Casks/tiled.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'tiled' do
-  version '0.13.0'
-  sha256 '07708d5081d811238c2178fd7fa13344961e9cfd3ee104822195e185d27fc42f'
+  version '0.13.1'
+  sha256 'cab94143b4fc0541b98491cbe160ee39164804d0bce3264bf6734c9275f16407'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/bjorn/tiled/releases/download/v#{version}/tiled-#{version}.dmg"
