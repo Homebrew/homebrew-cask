26f9219744e2962c2311363d487d574705a3120b
genewoo <genewoo@gmail.com>
2015-09-07 13:43:22 +0800

    Update qqmusic.rb
    Upgrade to QQ Music 3.0 Build 09

--- Casks/qqmusic.rb
+++ Casks/qqmusic.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'qqmusic' do
-  version '2.3'
-  sha256 '6593590eb5b7fab34141283b0a893309ddec90cd3764c3a066499b487c2d33ff'
+  version '3.0Build09'
+  sha256 'd719411af642f16a78e3f20eb608bd472694590ef34487f25e937103a2c0c72a'
 
-  url "http://dldir1.qq.com/music/clntupate/QQMusicForMacV#{version}.dmg"
+  url "http://dldir1.qq.com/music/clntupate/QQMusicMac#{version}.dmg"
   name 'QQ音乐'
   homepage 'http://y.qq.com'
   license :commercial
@@ -13,6 +13,7 @@ cask :v1 => 'qqmusic' do
 
   zap :delete => [
                   '~/Library/Caches/com.tencent.QQMusicMac',
+                  '~/Library/Application Support/com.tencent.QQMusicMac',
                   '~/Library/Preferences/com.tencent.QQMusicMac.plist',
                   '~/Library/Saved Application State/com.tencent.QQMusicMac.savedState'
                  ]
