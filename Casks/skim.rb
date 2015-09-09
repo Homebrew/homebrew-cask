e7db082d8d03c5bdefd0ca85d21e32a4c15e4c1a
Jussi Tuominen <me@jussituominen.me>
2015-09-09 19:27:31 +0300

    Update Skim.app to v1.4.13

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'skim' do
-  version '1.4.12'
-  sha256 '4984de6749589cda6a5c9d28a2a3541ba58407b7758afe3bffdbb43900831ba2'
+  version '1.4.13'
+  sha256 'bba918cf7a5f54ec1725399d5107ae1c861dd551000f78cf837af2347eff45b0'
 
   url "http://downloads.sourceforge.net/project/skim-app/Skim/Skim-#{version}/Skim-#{version}.dmg"
   appcast 'http://skim-app.sourceforge.net/skim.xml',
