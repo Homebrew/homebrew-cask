8a623c23deba478d300428c355d1d547dc096a9d
Christian Inzinger <christian@inz.io>
2015-09-09 16:04:18 +0200

    Add Audacity 2.1.1
    Using the alternative download link from OldFoss as listed at http://audacityteam.org/download/mac.

---
+++ Casks/audacity.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'audacity' do
+  version '2.1.1'
+  sha256 '266fa1b2f3aff3894730a8b0e9fcf5c24c93e726f15180855d2516c5c44de10e'
+
+  # oldfoss.com is the official download host per the vendor homepage
+  url "http://app.oldfoss.com:81/download/Audacity/audacity-macosx-ub-#{version}.dmg"
+  name 'Audacity'
+  homepage 'http://audacityteam.org'
+  license :gpl
+
+  app 'Audacity/Audacity.app'
+end
