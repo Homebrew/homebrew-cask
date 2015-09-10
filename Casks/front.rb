a73b395ad350b70524c1ea3a90bef56ecb673fa7
flesch <john@fles.ch>
2015-09-10 10:59:30 -0500

    Update Front.app with new download URL.

--- Casks/front.rb
+++ Casks/front.rb
@@ -2,7 +2,7 @@ cask :v1 => 'front' do
   version :latest
   sha256 :no_check
 
-  url 'http://dl.frontapp.com/front-latest.zip'
+  url 'https://dl.frontapp.com/osx/front.dmg'
   name 'Front'
   homepage 'https://frontapp.com/'
   license :gratis
