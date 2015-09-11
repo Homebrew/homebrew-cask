c5ca54bfa186f1b4f108ab1052075e2580d04668
Tomasz Naumowicz <tomasz@naumowicz.de>
2015-09-11 12:59:44 +0200

    MongoChef updated to version 3.0.1

--- Casks/mongochef.rb
+++ Casks/mongochef.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mongochef' do
-  version '2.0.7'
-  sha256 'bc699e2c9fd262acc4fdad5b519b9a1c95e53991c5c594b95b88b33bf17c0c'
+  version '3.0.1'
+  sha256 :no_check
 
   url "http://cdn.3t.io/mongochef/mac/#{version}/MongoChef.dmg"
   name 'MongoChef'
