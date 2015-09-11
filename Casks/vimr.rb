ee2eb2de4081e9a75bca33b50cad7f3f5b7690c9
Andrii Vakarev <avakarev@gmail.com>
2015-09-11 11:31:00 +0200

    Cleanup extra blank line

--- Casks/vimr.rb
+++ Casks/vimr.rb
@@ -2,7 +2,6 @@ cask :v1 => 'vimr' do
   version '0.8.0-32'
   sha256 '8660e7d5157b3165faab8a228f67a1bd63016c51bc300af131b90edc18dda678'
 
-
   # github.com is the official download host per the vendor homepage
   url "https://github.com/qvacua/vimr/releases/download/v#{version}/VimR-#{version.sub(%r{-.*},'')}.tar.bz2"
   appcast 'https://github.com/qvacua/vimr/releases.atom'
