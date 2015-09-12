abe44fed59103d497b3aff75df8cabdba1e25dd7
Victor Popkov <victor@popkov.me>
2015-09-12 21:29:50 +0300

    krita.rb: upgrade to v2.9.7.5

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'krita' do
-  version '2.9.7.4'
-  sha256 'ba8fec59b200f59cead036ff370f2013337a36780708c02e882987c708f8b963'
+  version '2.9.7.5'
+  sha256 'b810ca955d8e9c8e7d5491cf06f78b491522d637123df6c4fcdc863240cd6082'
 
-  # download dmg from kde official repository
+  # kde.org is the official download host per the vendor homepage
   url "http://files.kde.org/krita/osx/krita-#{version}.dmg"
   name 'Krita'
   homepage 'https://krita.org/'
