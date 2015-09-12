a2e0a8ea357af3e3e8121a9f0c08956e0bc43637
Victor Popkov <victor@popkov.me>
2015-09-12 21:35:29 +0300

    lighting.rb: upgrade to v1.1.2

--- Casks/lighting.rb
+++ Casks/lighting.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'lighting' do
-  version '1.1.0'
-  sha256 '2911c29cf2e06df363edfb57e320e2d91d795e4596bd79ae0cd293e223b57bdc'
+  version '1.1.2'
+  sha256 '43b5314d1ee6261b4675711773c5d59a5d028a51b2d4d1fb3274d1caa443054b'
 
   url "https://github.com/tatey/Lighting/releases/download/#{version}/Lighting-#{version}.zip"
   appcast 'https://github.com/tatey/Lighting/releases.atom'
