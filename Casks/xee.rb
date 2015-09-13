16a193af17a17bb01fe9b1d6fd0bb10875710ab4
Marko Musnjak <mmusnjak@ooyala.com>
2015-09-13 10:41:35 +0200

    Fix for download URL for Xee 3.4

--- Casks/xee.rb
+++ Casks/xee.rb
@@ -2,7 +2,7 @@ cask :v1 => 'xee' do
   version '3.4'
   sha256 'e9d47feb6fd1365333958e04372bbc02d3bc098ba7a14695a0c91957dfb1fb99'
 
-  url "http://wakaba.c3.cx/releases/mac/Xee#{version}.dmg"
+  url "http://wakaba.c3.cx/releases/Xee/Xee#{version}.dmg"
   name 'Xee³'
   homepage 'http://xee.c3.cx/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
