d126b1fc065ff698a32457e7c75b4a1e18b2afea
oxachloren <oxachloren@keemail.me>
2015-09-13 23:21:22 +0300

    telegram.rb: update to 0.8.57

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'telegram' do
-  version '0.8.38'
-  sha256 '1eadf4a061897070d993b2b17a208085834246548b8c6446f623e2710ec891a1'
+  version '0.8.57'
+  sha256 '869c56cab997e35c663a24fd2492f260ce957966325e66c6e186671bd661464b'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
