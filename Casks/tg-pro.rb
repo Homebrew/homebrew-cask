a43064ff3fbe1852d1b90b6b316028662b908e4c
Jussi Tuominen <me@jussituominen.me>
2015-09-14 21:22:29 +0300

    Update TG Pro.app to v2.8.4

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'tg-pro' do
-  version '2.8.3'
-  sha256 'c7aa687b4ae43b2bf7adf83b444c0e3716f0ae09cdc2f28b5a6cd79d151002c3'
+  version '2.8.4'
+  sha256 'e574e92e411b694f19c9a1ac0583361187f428524987d6a220ea82d39d64ac46'
 
   url "http://www.tunabellysoftware.com/resources/TGPro_#{version.gsub('.','_')}.zip"
   name 'TG Pro'
