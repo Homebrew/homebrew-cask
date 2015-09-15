18b64a67f162695effef0dcaeb0fa86132adf5a7
Victor Popkov <victor@popkov.me>
2015-09-15 19:39:57 +0300

    syncthing.rb: upgrade to v0.11.25

--- Casks/syncthing.rb
+++ Casks/syncthing.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'syncthing' do
-  version '0.11.24'
-  sha256 'cdda15d5bd031d44c776cc198ca311ef8e4baa449d47476eab5e348bcad6aeb2'
+  version '0.11.25'
+  sha256 '4c27f847fd1a3716335efc20fbfc5f009d50ee3554b1940659fd629e70c12262'
 
   url "https://github.com/syncthing/syncthing/releases/download/v#{version}/syncthing-macosx-amd64-v#{version}.tar.gz"
   appcast 'https://github.com/syncthing/syncthing/releases.atom'
