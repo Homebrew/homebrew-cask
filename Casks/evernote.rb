84e8df88836a2c11657e09264bd01b96783bb0d1
taherio <taher_adel@hotmail.com>
2015-09-15 15:56:25 -0400

    Update evernote to 6.1.1
    Changed version and sha256

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -5,8 +5,8 @@ cask :v1 => 'evernote' do
     sha256 '06b6da6d74ccab08deabfdd4c9519b9bc7f7ef0f0db2a0e8b0cd72e781f2e0ed'
     url 'https://cdn1.evernote.com/mac/release/Evernote_402634.dmg'
   else
-    version '6.1_452115'
-    sha256 'a52e04933da08596f745efa49ba5885743082a95d22bfd25a0cbd6521d775daa'
+    version '6.1.1_452253'
+    sha256 'ad2ad4d426362f8bc6fff205522365cfe9a5bd5f36369fc6c3c8bd7f9dd4e288'
     url "https://cdn1.evernote.com/mac-smd/public/Evernote_RELEASE_#{version}.dmg"
     appcast 'https://update.evernote.com/public/ENMac/EvernoteMacUpdate.xml'
   end
