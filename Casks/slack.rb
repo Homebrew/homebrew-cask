ade085316096d9e57ee28203f75cffdb842cba03
Ingo Richter <ingo.richter@gmail.com>
2015-09-15 11:29:46 -0700

    Upgrade Slack to v1.1.4

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'slack' do
-  version '1.1.3'
-  sha256 'd668bbf0aa1fd857dcb169df2e4040799a77e55e980255557e3e6e69b71773fd'
+  version '1.1.4'
+  sha256 'b3a6c2b651cf98d7bb817c063b3fbc9c5bb7f7ecfeb766359d1d85147267aa28'
 
   # hockeyapp.net is the official download host per the appcast feed
-  url 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1/app_versions/18?format=zip&avtoken=4519a445adc85152ea227dbf2b3675ee0a4ffe54'
+  url 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1/app_versions/19?format=zip&avtoken=d11b1b4d9f90d53702d26bd4972a757345f1cfc0'
   appcast 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1',
-          :sha256 => 'd9ed4686be46ffe4a3a67a2232c9bfe8dd5c96b7615b18467f42474b97c3678a'
+          :sha256 => 'b3a6c2b651cf98d7bb817c063b3fbc9c5bb7f7ecfeb766359d1d85147267aa28'
   name 'Slack'
   homepage 'https://slack.com/'
   license :gratis
