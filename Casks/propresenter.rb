a6abe555839980b1f1402e554ae062b434026327
Victor Popkov <victor@popkov.me>
2015-09-15 19:38:50 +0300

    propresenter.rb: upgrade to v6.0.8_b15006

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'propresenter' do
-  version '6.0.7_b14138'
-  sha256 'bd2c90621920a827ceedad3be4e0c02a3ab80c430383ebd3ed540a23aa4de24c'
+  version '6.0.8_b15006'
+  sha256 'd73286ba462820dc7c4885c2b67c7bef03751939f4440a51a205e0e97494263f'
 
   url "https://www.renewedvision.com/downloads/ProPresenter6_#{version}.dmg"
   appcast 'https://www.renewedvision.com/update/ProPresenter6.php',
-          :sha256 => '405dcd0e0d1c9c79fed50ecb6150076f360b6acb232f351b83c26dabb4dca81d'
+          :sha256 => 'e5dddb99a9cf20e2ca9713fac7cf8651c725eee16f7071afc36539278fe7637c'
   name 'ProPresenter'
   homepage 'https://www.renewedvision.com/propresenter.php'
   license :commercial
