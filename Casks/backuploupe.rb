1464cbfb2c51a7556e280167a1bce78967c648d3
Victor Popkov <victor@popkov.me>
2015-09-15 19:27:44 +0300

    backuploupe.rb: upgrade to v2.8.1

--- Casks/backuploupe.rb
+++ Casks/backuploupe.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'backuploupe' do
-  version '2.8'
-  sha256 '99112881a1b9e9ac5db8466cdad86f4ba2ef251eda61e59baaf6ec53e22571fb'
+  version '2.8.1'
+  sha256 'eda82d5de8e0b7d1e841a3b79ac6712d9a5221920dd1aed05c9d19f4f497fd66'
 
   url "http://www.soma-zone.com/download/files/BackupLoupe_#{version}.tar.bz2"
   name 'BackupLoupe'
   appcast 'http://www.soma-zone.com/BackupLoupe/a/appcast.xml',
-          :sha256 => 'fb2a32b33bcefd8fb5531fc0285cdf29131817e266219cdc4a17d9acbfb67ee0'
+          :sha256 => '21ad9c8633b2483e9e849ea00bfc2bc31f50fcc4f9edfbbeeb1fa47f2979fce5'
   homepage 'http://www.soma-zone.com/BackupLoupe/'
   license :commercial
 
