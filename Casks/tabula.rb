c1ede41fa84d4fa24c49752dfebf3a08743c4fd8
Victor Popkov <victor@popkov.me>
2015-09-15 19:42:29 +0300

    tabula.rb: upgrade to v1.0.1

--- Casks/tabula.rb
+++ Casks/tabula.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'tabula' do
-  version '1.0.0'
-  sha256 '1eeb7639f301ce0aafef4a8da6544744987712f558723a0a01e1c541aae182f1'
+  version '1.0.1'
+  sha256 '85e75f120c450bc702c01aa88a4ca9c2d227ef2906ec7020e892e0f174a7d70b'
 
   url "https://github.com/jazzido/tabula/releases/download/v#{version}/tabula-mac-#{version}.zip"
   appcast 'https://github.com/jazzido/tabula/releases.atom'
