af7c8dad822dd8d8107b753b9b0f867553f62e4a
buo <buo@users.noreply.github.com>
2015-09-16 19:28:11 +0900

    Update BlueHarvest to 6.2.2

--- Casks/blueharvest.rb
+++ Casks/blueharvest.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'blueharvest' do
-  version '6.2.1'
-  sha256 '3ecb68a17a82a73d90c2451418114dcd925ed18da8bc4c03ccd5aeae15c0f24e'
+  version '6.2.2'
+  sha256 'e1e9c93e5537444f314b0bb18c6f7bed9e34d43d8f8502d0755fca4340e7a6a6'
 
   url "http://zeroonetwenty.com/downloads/BlueHarvest#{version.delete('.')}.dmg"
   appcast 'https://cp37.ezyreg.com/~zeroonet/downloads/versioninfo/sparkle/blueharvest6.xml',
-          :sha256 => '0a6a7811abf74d36ee9fb78ce2b208fbded639da602f3900a2cda0865f17bda0'
+          :sha256 => 'f77096fccaa1a6f2ce2cd0c5ef6ac4c4b9197b67fe274cf63599bc12c3452084'
   name 'BlueHarvest'
   homepage 'http://zeroonetwenty.com/blueharvest/'
   license :commercial
