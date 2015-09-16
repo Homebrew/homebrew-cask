6f94b38171ec281b4ff334cf3c1dd03fec6d2f4f
Michael Pei <ambitious.pei@gmail.com>
2015-09-16 09:21:00 +0800

    Add SmartFinder Latest

---
+++ Casks/smart-finder.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'smart-finder' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://sf.smartisan.com/sf/release/download'
+  name 'Smart Finder'
+  homepage 'http://www.smartisan.com/apps/smartfinder'
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'SmartFinder.app'
+end
