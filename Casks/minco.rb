197b292bba8face955cab253231e84cca79dc5bb
Alex Weber <alexweber15@gmail.com>
2015-09-16 11:46:53 -0300

    Update Minco app to latest version: 2.0.23
    2.0.22 has expired and no longer works

--- Casks/minco.rb
+++ Casks/minco.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'minco' do
-  version '2.0.22'
-  sha256 'bf98d80e0273552559ae7b98ba411d62ccdca66ddd9f2f274bd2aa2cdcd44c58'
+  version '2.0.23'
+  sha256 'e8acbfb029f8df05f62164168653f0605aa0a11207c0b0fa255454ec033dea26'
 
   # webpack.com is the official download host per the appcast feed
   url "https://ssl.webpack.de/celmaro.com/updates/minco2/Minco#{version.delete('.')}.zip"
   appcast 'https://ssl.webpack.de/celmaro.com/updates/minco2/minco.xml',
-          :sha256 => '3ab9e6d0a6e9ebecfeab805c399a7194fe2a88bdcc1b096446e9f8be7e7195f8'
+          :sha256 => 'e8acbfb029f8df05f62164168653f0605aa0a11207c0b0fa255454ec033dea26'
   name 'Minco'
   homepage 'http://www.celmaro.com/minco/'
   license :commercial
