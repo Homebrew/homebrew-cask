e1b69d1aa4253f946c71af8989d9ba60ae874414
buo <buo@users.noreply.github.com>
2015-09-17 03:53:29 +0900

    Update IUEditor to 1.0.1.4

--- Casks/iueditor.rb
+++ Casks/iueditor.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'iueditor' do
-  version :latest
-  sha256 :no_check
+  version '1.0.1.4'
+  sha256 '80f1dbb5b959789e4fc809057ec173ffd2377357ce38cf858ca355a6845b3437'
 
-  url 'http://www.iueditor.org/download/IUEditor-latest.zip'
+  # amazonaws.com is the official download host per the vendor homepage
+  url "http://iueditor.s3.amazonaws.com/Release/IUEditorV#{version}.zip"
   name 'IUEditor'
   homepage 'http://www.iueditor.org'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
