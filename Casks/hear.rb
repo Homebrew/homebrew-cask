62f28e6ecde51ff34d8bf7c77a3a6eb42714aca0
buo <buo@users.noreply.github.com>
2015-09-17 15:52:57 +0900

    Update Hear to 1.2.4

--- Casks/hear.rb
+++ Casks/hear.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'hear' do
-  version '1.2.3'
-  sha256 'd28019bcb73d34b7ac1b1851444d94afd2048b94138724997bedbe6dc8df93f6'
+  version '1.2.4'
+  sha256 '54bd28a771cd26813a9177bbd41879694a3998bf712ca6b94b18c45895bc79e6'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/prosoft-engineering/hear/Hear_#{version}.dmg"
   appcast 'https://www.prosofteng.com/resources/sparkle/sparkle.php?psProduct=Hear',
-          :sha256 => '5ff497340df135e5765942ab6f5ae4f083f06676a79be33073c00bd7c7814a88'
+          :sha256 => '70719bdb2692b7d14ac2e410c4c9b75a2849519169b819fd85f6cf99a8caaf13'
   name 'Hear'
   homepage 'https://www.prosofteng.com/hear/'
   license :commercial
