028d023ae01391af8dd45782326e4eeabb0e2e2e
Victor Popkov <victor@popkov.me>
2015-09-18 21:42:22 +0300

    mongodb.rb: upgrade to v3.0.6-build.1

--- Casks/mongodb.rb
+++ Casks/mongodb.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mongodb' do
-  version '3.0.5-build.1'
-  sha256 'd3df85e7045a3446090b910eee1caa3b705f6e73c0b8c10b53753d80c6f60e01'
+  version '3.0.6-build.1'
+  sha256 '7e3276367a63419a5d3a7b062019e8b22d596bc3311313e56bda8ffbcae51445'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/gcollazo/mongodbapp/releases/download/#{version}/MongoDB.zip"
