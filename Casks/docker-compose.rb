dc60a4ddb393381c6cac5960b9f758e2c6d4c9ea
Victor Popkov <victor@popkov.me>
2015-09-18 21:34:35 +0300

    docker-compose.rb: upgrade to v1.4.1

--- Casks/docker-compose.rb
+++ Casks/docker-compose.rb
@@ -1,6 +1,6 @@
 cask :v1_1 => 'docker-compose' do
-  version '1.4.0'
-  sha256 'dcbf38164145705a36c1dc8d725aae045f2fc9b094fe443f2e66fc74366c4594'
+  version '1.4.1'
+  sha256 '8f93cd99f43a03be16f6607d5b47d86b4aee582e3d4b19223d90da1e253433bb'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/docker/compose/releases/download/#{version}/docker-compose-Darwin-x86_64"
