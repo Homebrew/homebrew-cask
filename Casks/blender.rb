0e14a8cfad2d1b6285c7efc36b9b5d8a2e8a7cac
Alexis Hildebrandt <afh@surryhill.net>
2015-09-18 14:48:24 +0200

    blender 2.75a

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'blender' do
-  version '2.75'
-  sha256 '78cec3ba19b7a37f1aae4e776169098127dccbd24bb1d12b53fbcd220d610ccc'
+  version '2.75a'
+  sha256 '5d120eac07a895fa80c7b022aceb0454e63d2e85b799d9e5b6e7acc79e742317'
 
   url "https://download.blender.org/release/Blender#{version.to_f}/blender-#{version}-OSX_10.6-x86_64.zip"
   name 'Blender'
