da46a3c7036cb0bb9a33e3aeae855b72872fe341
Gloomy-Mammoth <Gloomy-Mammoth@users.noreply.github.com>
2015-09-20 00:06:13 +0200

    Remove trailing whitespace

--- Casks/blockblock.rb
+++ Casks/blockblock.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'blockblock' do
   version '0.9.3'
   sha256 '719761707b8fca83014bfdd58d01d37ad0c32142d57913f8ed46c56a3011ad0c'
-  
+
   # bitbucket.org is the official download host per the vendor homepage
   url 'https://bitbucket.org/objective-see/deploy/downloads/BlockBlock_0.9.3.zip'
   name 'BlockBlock'
