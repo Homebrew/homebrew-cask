39f195c666592172c450e27bf7fd8fbf2e3436c0
buo <buo@users.noreply.github.com>
2015-09-19 22:39:36 +0900

    Update Mactracker to 7.4.6

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'mactracker' do
-  version '7.4.5'
-  sha256 '1b49c5bb310eda23c746b650ba9a99423e9786be60e13b4e45d54c19ffb38bc3'
+  version '7.4.6'
+  sha256 'ae9448481736cc04a14aa031033064163569add6519ec58ee66876e7bffc83a1'
 
   url "http://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast 'http://update.mactracker.ca/appcast-b.xml',
-          :sha256 => '4792d02bbece6924e8ddb068327d0814b733475b1f95dddf33bb3cc9ecf0810c'
+          :sha256 => '48b66546001a2cf246a4240bd9ff7756978967ee9030ac1b56d2c58d595cca7b'
   name 'Mactracker'
   homepage 'http://mactracker.ca/'
   license :gratis
