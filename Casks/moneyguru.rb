ea7dda770bb4641c80d12132b4f984dbb86fda10
buo <buo@users.noreply.github.com>
2015-09-20 01:17:22 +0900

    Update moneyGuru to 2.9.3

--- Casks/moneyguru.rb
+++ Casks/moneyguru.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'moneyguru' do
-  version '2.8.2'
-  sha256 '65b8df411330593462fc0eb8377115cc0709c0e499af1876fda0baf2a0bc03f0'
+  version '2.9.3'
+  sha256 '5f47715fa02c5fcd6595a733b35b99bd86b3008dff2a566deb692a7abe0b21a6'
 
   url "http://download.hardcoded.net/moneyguru_osx_#{version.gsub('.', '_')}.dmg"
   name 'moneyGuru'
   appcast 'http://www.hardcoded.net/updates/moneyguru.appcast',
-          :sha256 => '8fac60f13b4295126926b07d42812d43166bb92912d0743def66d71c7a9c515b'
+          :sha256 => '32714462ad203d85285fd449dd472c609c8445384b579ff0dbac794e27bc8749'
   homepage 'http://www.hardcoded.net/moneyguru/'
   license :bsd
 
