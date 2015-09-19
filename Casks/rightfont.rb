54529349b2af8f741c42138d8b50aacc96ad0cc0
buo <buo@users.noreply.github.com>
2015-09-20 00:52:46 +0900

    Update RightFont to 1.2.0

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'rightfont' do
-  version '1.1.0'
-  sha256 '082ea61f0a798a45b66f84eaeaa7e40372b8ca0f042c91ccaf9a391a8914f607'
+  version '1.2.0'
+  sha256 '345f0d7b7d8eb834fd30287d0d81b7b98759ede682c50c6c8861fd9a0c12be19'
 
   url "http://rightfontapp.com/downloads/#{version}/rightfont.zip"
   name 'RightFont'
   appcast 'http://rightfontapp.com/update/appcast.xml',
-          :sha256 => '1b322e38363e6874a9664c670a6d892629c35a07ac39e65f983873d881df9188'
+          :sha256 => '39d29124d62ccaeca47650ba9c47796904b16cc24047892ec4addd71faa709d0'
   homepage 'http://rightfontapp.com/'
   license :commercial
 
