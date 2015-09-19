d1ae110bf35c058b5f6fff8c2f004a72077352e1
buo <buo@users.noreply.github.com>
2015-09-20 01:32:21 +0900

    Update PDF Converter Master to 4.0.0

--- Casks/pdf-converter-master.rb
+++ Casks/pdf-converter-master.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'pdf-converter-master' do
-  version '3.5.0'
-  sha256 'c927cea0b588dfc421c6f2feec6ab4a660c554a039e2bb160840b3bac17ed5ba'
+  version '4.0.0'
+  sha256 '00e74521645b2b744836352d6be342f95fd80f4c8e85966a2afb016ce89bcefd'
 
   url "http://www.lightenpdf.com/download/download.aspx?file=PDF-Converter-Master-#{version.delete('.')}.zip"
   appcast 'http://www.lightenpdf.com/xml/PDF-Converter-Mac.xml',
-          :sha256 => 'be7221e35d5e4e564f34bb364705595a9a0c73868627614e4629728f3bd3fdf8'
+          :sha256 => '727005e9425435c21808330d88b54bf4df4269db290f8c4ddcc9ed49b2d4ca53'
   name 'PDF Converter Master'
   homepage 'http://www.lightenpdf.com/pdf-converter-mac.html'
   license :commercial
