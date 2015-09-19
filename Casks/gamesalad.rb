73e2efd42816917addb451580eb9bf685f24768d
buo <buo@users.noreply.github.com>
2015-09-20 01:29:38 +0900

    Update GameSalad to 0.13.41

--- Casks/gamesalad.rb
+++ Casks/gamesalad.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'gamesalad' do
-  version '0.13.30'
-  sha256 '0c0d9ceaf7cc92c59d7be18fc785dfd00d25c5b05999f1f1fe1dfbdec62c646b'
+  version '0.13.41'
+  sha256 'ec19549deddb46e320354b9158f66586bcd5f43334c4cc272a45a702e2ecab14'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "http://gs.releases.s3.amazonaws.com/creator/#{version}/GameSalad-Creator-#{version}-beta.dmg"
   name 'GameSalad'
   appcast 'https://gamesalad.com/download/studioUpdates',
-          :sha256 => '2040ea4abaea2e9e089bfb35feefe6d35b7601b1ddd51c81117e2fde46f23a77'
+          :sha256 => '479889661b7dc4f242a2710a79b4a8a59651c13693cc06cbf3472713914dcabd'
   homepage 'https://gamesalad.com/'
   license :commercial
 
