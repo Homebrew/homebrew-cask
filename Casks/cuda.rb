6d74c1959ad4f03905766a22d9d681ee74381334
Alexander Altman <alexanderaltman@me.com>
2015-09-18 17:13:58 -0700

    Update CUDA to 7.5.18
    Needs a proper `uninstall` stanza

--- Casks/cuda.rb
+++ Casks/cuda.rb
@@ -1,19 +1,12 @@
 cask :v1 => 'cuda' do
-  version '7.0.29'
-  sha256 'e564b1f34a9079ff842b3a055016133347e3c5e8d7feea05b0b69e4ca090007a'
+  version '7.5.18'
+  sha256 '0a41f9abf0a96b9fa2d41b949054a63c916728bf320fd886d895f48f701d03ea'
 
-  url "http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_#{version}_mac.pkg"
+  url "http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_#{version}_mac.dmg"
   name 'CUDA'
   homepage 'https://developer.nvidia.com/cuda-zone'
   license :other
   tags :vendor => 'Nvidia'
 
-  pkg "cuda_#{version}_mac.pkg"
-
-  uninstall :pkgutil => 'com.nvidia.cuda.*',
-            :kext => 'com.nvidia.CUDA',
-            :delete => [
-                        '/Developer/NVIDIA/CUDA-7.0',
-                        '/usr/local/cuda',
-                       ]
+  installer :manual => "CUDAMacOSXInstaller.app"
 end
