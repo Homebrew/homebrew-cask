8aa3326b99114a888b397942b75a9f5e6464421b
buo <buo@users.noreply.github.com>
2015-09-20 01:22:38 +0900

    Update LaTeXiT to 2.8.0

--- Casks/latexit.rb
+++ Casks/latexit.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'latexit' do
-  version '2.7.5'
-  sha256 '2faef9682f1450d2a4b240bcf602a84ae187e58bf62787e2185af0ee05161e6f'
+  version '2.8.0'
+  sha256 '74258cbf8673ef17fb6ce8fbfdc630ea98002fb2a7012c251429299cf9b68489'
 
   url "http://www.chachatelier.fr/latexit/downloads/LaTeXiT-#{version.gsub('.', '_')}.dmg"
   appcast 'http://pierre.chachatelier.fr/latexit/downloads/latexit-sparkle-en.rss',
-          :sha256 => 'bc1bd88bf1d7a9770f0527652db2fc082214240a9b66684d9a95a0beaf2f260a'
+          :sha256 => '34cab0a120226e056f8ea3f24afa7e252429e487b522345adf862365986b2d66'
   name 'LaTeXiT'
   homepage 'http://www.chachatelier.fr/latexit'
   license :oss
