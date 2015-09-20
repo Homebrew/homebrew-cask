654dd169b041a2e4a0d46bba86330eaf9145bb06
buo <buo@users.noreply.github.com>
2015-09-20 17:28:42 +0900

    Update Simon to 4.1.1

--- Casks/simon.rb
+++ Casks/simon.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'simon' do
-  version '4.1'
-  sha256 'a7f6e3b7775afd404633e3a876071be554c4fecdd9efb1fcf3071c0d776f758c'
+  version '4.1.1'
+  sha256 'fa3f221882c769350647bd1904b95f22e4846ed582de1a99cb892828f4f6ba8c'
 
   url "http://www.dejal.com/download/simon-#{version}.zip"
   appcast 'http://www.dejal.com/appcast/?prod=simon',
-          :sha256 => '19ad2681be99141471d288d0b1547ed16161e160e265d29297c079bdf24f0eb3'
+          :sha256 => '7431a2e17604486863b071139da0665aec01e61a1447367b2f497080936e3f4b'
   name 'Simon'
   homepage 'http://www.dejal.com/simon/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
