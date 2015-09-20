83182b48b640a2d0993ff85a152baad73e517b4e
buo <buo@users.noreply.github.com>
2015-09-20 17:35:57 +0900

    Update Vienna to 3.0.8

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'vienna' do
-  version '3.0.6'
-  sha256 '9bc9e93a443dc03234949e7baa648a94d43c9e8b86fcba426d8e677eaa7e3e12'
+  version '3.0.8'
+  sha256 'dbb6510500539718955718567cb246901b0508c8bda804020e5026119c288c19'
 
-  # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/vienna-rss/Vienna#{version}.tgz"
+  # bintray.com is the official download host per the appcast feed
+  url "https://dl.bintray.com/viennarss/vienna-rss/Vienna#{version}.tgz"
   appcast 'http://vienna-rss.org/changelog.xml',
-          :sha256 => 'a8fa8150ae0a2f591e443b1fe50ad605243e2b6b76b2eeb08b019a79076b6ee4'
+          :sha256 => '15433a536e85cb65749a8e93baeffc56b8dec68610031eca0492dbddb5d0d74b'
   name 'Vienna'
   homepage 'http://www.vienna-rss.org'
   license :apache
