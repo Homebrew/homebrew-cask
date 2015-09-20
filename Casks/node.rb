72bf735f231630193c378ac24469a8c7830c0e62
prime8 <prime8@users.noreply.github.com>
2015-09-20 10:57:21 -0400

    Update org.nodejs pkg to 4.1.0

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'node' do
-  version '4.0.0'
-  sha256 '387dd01caf168267749b0ddc7702b2fef7c95fbe1b2dab9a41ded02c11f16889'
+  version '4.1.0'
+  sha256 '9df8830c12bd28a5c1a0e1355cad9f50546e40ce681061d62f7a16e351272764'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   name 'Node.js'
