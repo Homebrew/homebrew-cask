03e3c246d521bc45024d42cd6c33b3cc4a1bb81f
Koen Rouwhorst <info@koenrouwhorst.nl>
2015-09-20 15:20:30 +0200

    Upgrade QtPass.app to v1.0.1

--- Casks/qtpass.rb
+++ Casks/qtpass.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'qtpass' do
-  version :latest
-  sha256 :no_check
+  version '1.0.1'
+  sha256 '9a72a87a9f5408d50aa10ac11d8a5ec13453beb95b2a27ca46a6212132f4685b'
 
-  url 'https://annejan.com/media/qtpass.dmg'
+  url "https://github.com/IJHack/qtpass/releases/download/v#{version}/qtpass-#{version}.dmg"
+  appcast 'https://github.com/IJHack/qtpass/releases.atom'
   name 'QtPass'
   homepage 'https://qtpass.org/'
   license :gpl
