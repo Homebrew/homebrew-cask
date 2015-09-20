61ba638b608a114e7f6267ca0c93ed55b8daa8cf
buo <buo@users.noreply.github.com>
2015-09-21 06:01:32 +0900

    Update Slender to 2.2.2

--- Casks/slender.rb
+++ Casks/slender.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'slender' do
-  version '2.1.1'
-  sha256 '6235751e4510df2095ebae8e3588ed32d858d2f4612b5f020aea7efd4ff60cf0'
+  version '2.2.2'
+  sha256 '7d3486efb8d6b4cdce8179b423616642238081e510292d215a9c78210913afb5'
 
   url "http://downloads.dragonforged.com/slender#{version.delete('.')}.zip"
   appcast 'http://dragonforged.com/slender/sparkle/?bundleId=com.dfsw.Slender',
-          :sha256 => '05b8cc1837ad290486bac9bb9c1bf194b2e2c208c968f1e55bae47ce33c56141'
+          :sha256 => 'cc82ce3f2c0fc3a92911fba91438524543bee330e49e1d83613292b89ba5afbf'
   name 'Slender'
   homepage 'http://dragonforged.com/slender/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
