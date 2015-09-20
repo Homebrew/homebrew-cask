2b9660d945897d7cf5928bb3be50e49de19dda4c
Chris Ridgers <your@email.com>
2015-09-20 13:43:40 +0100

    New cask for minecraft-server
    I've created a new cask that pulls down the minecraft server jar and
    set's up a binary linking to it.  Some potential changes for anyone
    reviewing it include testing for 64bit architecture as part of
    the binary and if present passing '-d64' to the java call as an
    argument'.  Also, if there's a sane way to allow for the application to
    run in the background, that would be a nice default, along with the
    ability to pass options through to the java call.
    
    I've added in an extra line to the caveats.

---
+++ Casks/minecraft-server.rb
@@ -0,0 +1,40 @@
+cask :v1 => 'minecraft-server' do
+  version '1.8.8'
+  sha256 '39aef720dc5309476f56f2e96a516f3dd3041bbbf442cbfd47d63acbd06af31e'
+
+  # amazonaws.com is the official download host per the vendor homepage
+  url "https://s3.amazonaws.com/Minecraft.Download/versions/#{version}/minecraft_server.#{version}.jar"
+  name 'Minecraft Server'
+  homepage 'https://minecraft.net/'
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  container :type => :naked
+
+  preflight do
+    FileUtils.touch "#{staged_path}/minecraft-server"
+    minecraft_server = File.open "#{staged_path}/minecraft-server", 'w'
+    minecraft_server.puts '#!/bin/bash'
+    minecraft_server.puts 'BASEDIR=$(dirname "$(readlink -n $0)")'
+    minecraft_server.puts 'cd $BASEDIR'
+    minecraft_server.puts 'java -Xmx1024M -Xms1024M -jar minecraft_server.1.8.8.jar nogui'
+    minecraft_server.close
+  end
+
+  binary 'minecraft-server'
+
+  postflight do
+    set_permissions "#{staged_path}/minecraft-server", '+x'
+    system 'minecraft-server'
+
+    file_name = "#{staged_path}/EULA.txt"
+    contents = File.read(file_name).gsub(/false/, 'true')
+    File.open(file_name, 'w') {|file| file.puts contents }
+  end
+
+  caveats do
+    <<-EOS.undent
+      To run this app, type "#{token}" in terminal.
+      To configure the server take a look at the files staged at #{staged_path}
+    EOS
+  end
+end
