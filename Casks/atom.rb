adfb6d492c692616d3485e4ddd0845f4e373b953
Gabriel Augendre <gabriel@augendre.info>
2015-09-21 10:29:48 +0200

    Bump atom version and sha256 to 1.0.15 (latest)

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'atom' do
-  version '1.0.14'
-  sha256 'fee7efd0c41e33b6ff69c44b26a35572dd540c497166aa9e25d6c1fc58f21671'
+  version '1.0.15'
+  sha256 '5d6a10a93a13bc8e711e0896aff9196fd9d98b227b0e69fbce9a8c74fe3de58e'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
