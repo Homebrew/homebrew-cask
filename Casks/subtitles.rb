337aecc16aa085b41d21e39e03cf496e54310b8f
buo <buo@users.noreply.github.com>
2015-09-21 21:53:21 +0900

    Update Subtitles to 3.2.5

--- Casks/subtitles.rb
+++ Casks/subtitles.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'subtitles' do
-  version '3.2.4'
-  sha256 'ae2b2b0cb3e1229c1467a00fb5142dd18e454b4973d2b5cc61eff58c1c4c45c9'
+  version '3.2.5'
+  sha256 '12a7646b8f6150bd978ea878d3a7a7f2d7ccb586976c82c633cfb691c5ca32e7'
 
   url "http://subtitlesapp.com/download/Subtitles-mac-#{version}.zip"
   appcast 'http://subtitlesapp.com/updates.xml',
-          :sha256 => 'a93e1c4b9df7b46997212dc79ca999b5d521d308be82b910e92759511dd68ae4'
+          :sha256 => 'f4ce39ccdc98206b039b7241a127716f2282ea7e715a365a40b73bdf69d4e887'
   name 'Subtitles'
   homepage 'http://subtitlesapp.com'
   license :commercial
