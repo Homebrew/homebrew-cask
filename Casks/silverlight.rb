88f4781deedbd5b12b07fad25d767362f5de9bc2
Satoshi Ohtsuka <sohtsuka1317@gmail.com>
2015-09-22 02:37:20 +0900

    Upgrade Silverlight to v5.1.40728.0

--- Casks/silverlight.rb
+++ Casks/silverlight.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'silverlight' do
-  version '5.1.40416.0'
+  version '5.1.40728.0'
   sha256 :no_check    # required as upstream package is updated in-place
 
-  url 'http://silverlight.dlservice.microsoft.com/download/B/D/C/BDCE18B1-73C1-47BA-9B11-46A4C14CF7B0/40416.00/Silverlight.dmg'
+  url 'http://silverlight.dlservice.microsoft.com/download/8/E/7/8E7D9B4B-2088-4AED-8356-20E65BE3EC91/40728.00/Silverlight.dmg'
   name 'Silverlight'
   homepage 'https://www.microsoft.com/silverlight/'
   license :gratis
