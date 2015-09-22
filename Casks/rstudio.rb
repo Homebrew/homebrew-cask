3e8264e3462797d006d7ad5ba9099f506b8996c7
Shaun Jackman <sjackman@gmail.com>
2015-09-22 10:31:44 -0700

    rstudio 0.99.484

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rstudio' do
-  version '0.99.482'
-  sha256 '1d8ebc577c55bd3ad1472786cd8b86520f8e852113a6e816cefecaa8c93c475e'
+  version '0.99.484'
+  sha256 '5a7a9fc8559a653d38402aadbd7c1a8c726dfdf193ed5394c29fbfa9a4608b2b'
 
   # rstudio.org is the official download host per the vendor homepage
   url "http://download1.rstudio.org/RStudio-#{version}.dmg"
