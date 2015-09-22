5c7c08e1610d31164c93449add2bdaf8f91eaf00
tiagonbotelho <tiagonbotelho@hotmail.com>
2015-09-22 19:26:32 +0100

    fixes circle ci issues

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'google-chrome-canary' do
-    version :latest
-    sha256 :no_check
+  version :latest
+  sha256 :no_check
 
-    url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
-    name 'Google Chrome Canary'
-    homepage 'https://www.google.pt/chrome/browser/canary.html'
-    license :gratis
-    tags :vendor => 'Google'
+  url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
+  name 'Google Chrome Canary'
+  homepage 'https://www.google.pt/chrome/browser/canary.html'
+  license :gratis
+  tags :vendor => 'Google'
 
-    app 'Google Chrome Canary.app'
+  app 'Google Chrome Canary.app'
 end
