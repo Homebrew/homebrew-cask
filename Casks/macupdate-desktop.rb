ccadd2a7471b1b894a957d205682a70d07f526c0
Victor Popkov <victor@popkov.me>
2015-09-22 19:24:25 +0300

    macupdate-desktop.rb: add license

--- Casks/macupdate-desktop.rb
+++ Casks/macupdate-desktop.rb
@@ -7,7 +7,7 @@ cask :v1 => 'macupdate-desktop' do
           :sha256 => 'cecc073acecb3a7221854fae2f629759fb59de2de2e161bf68c7f1c9800d9ca2'
   name 'MacUpdate Desktop'
   homepage 'https://www.macupdate.com/desktop'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :freemium
 
   app 'MacUpdate Desktop.app'
 
