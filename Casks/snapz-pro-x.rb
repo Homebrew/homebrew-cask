8765435d8742890682d57211986b179eb686e0e9
buo <buo@users.noreply.github.com>
2015-09-22 19:32:23 +0900

    Update Snapz Pro X to 2.6.0

--- Casks/snapz-pro-x.rb
+++ Casks/snapz-pro-x.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'snapz-pro-x' do
-  version '2.5.4'
-  sha256 'b3b7dfd27222dfc45414cf820d83d14f12f6504af0b8e9009fd4b0a771b8894c'
+  version '2.6.0'
+  sha256 'f603dae7a8fe64633f01ca2a53a1eed0907d17e3733f125fedb6d2e96b491b64'
 
   url "http://downloads3.ambrosiasw.com/snapzprox/essentials/SnapzProX#{version.to_i}.dmg"
   appcast 'https://www.ambrosiasw.com/updates/profile.php/snapz_pro_x/release',
-          :sha256 => '9bb22b961c94283f380351f4748cc5cea9e443b9c082da51231995cbf3313987'
+          :sha256 => '8fbbd653283a84ce8541f1f58670cc2fe9da88ed32b75a153bbabd69709d7314'
   name 'Snapz Pro X'
   homepage 'http://www.ambrosiasw.com/utilities/snapzprox/'
   license :commercial
