a5ed06b427f170b4371f193aee75949c32ae37f8
Andrei Gliga <otzy_007@yahoo.com>
2015-09-24 00:43:32 +0300

    fixed the flux download link

--- Casks/flux.rb
+++ Casks/flux.rb
@@ -2,7 +2,7 @@ cask :v1 => 'flux' do
   version '36-3'
   sha256 '439b83c1886e8dee2fb79f9abb047cddf0fcd7dae7d6b68308a65e3954d63333'
 
-  url "https://justgetflux.com/mac/Flux#{version}.zip"
+  url "https://d38qbqfndhlqd2.cloudfront.net/Flux#{version}.zip"
   appcast 'https://justgetflux.com/mac/macflux.xml'
   name 'f.lux'
   homepage 'https://justgetflux.com/'
