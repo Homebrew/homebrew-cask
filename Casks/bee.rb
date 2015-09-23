b347301092d54bd9ee8d6ec802cc2226220e023e
Victor Popkov <victor@popkov.me>
2015-09-23 20:19:35 +0300

    bee.rb: upgrade to v2.2

--- Casks/bee.rb
+++ Casks/bee.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bee' do
-  version '2.1'
-  sha256 '3a3103938959d212ac9eb7c78bf83931d2876384ee13f5ffd3a9b3b2f0da3d58'
+  version '2.2'
+  sha256 'bdb31af7cd200a1faf0ba17c152775c108ca3c822931a0c672493a046041e0ca'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "http://bee-app.s3.amazonaws.com/public/Bee-#{version}.zip"
