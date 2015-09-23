fa904c4973309aee44aa588ff53f8e2ec18ef2f5
buo <buo@users.noreply.github.com>
2015-09-23 18:16:36 +0900

    Update Totals to 3.1

--- Casks/totals.rb
+++ Casks/totals.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'totals' do
-  version '3.0.17'
-  sha256 '23136083cdf528cf6883608c02c23d4881dd94a9e6c500a1d18e3626579d17d3'
+  version '3.1'
+  sha256 '35efc4f7521ecdc6baec8284d2e52fad4f461195416f5c4fc60bd35e969c8cb2'
 
   url "http://www.kedisoft.com/dl/totals/Totals-#{version}.dmg"
   name 'Totals'
   appcast 'http://kedisoft.com/appcasts/appcast.php?app=totals3',
-          :sha256 => 'bd168bbb85ad8a06d42295cb61ff9756c01572cdf90536d4f7401f5a43ed7f69'
+          :sha256 => '2355747b573a2d9143cfbc55529ac191a1ed949afed935d9e88ebdf3b54a08dd'
   homepage 'http://www.kedisoft.com/totals/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
