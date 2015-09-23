33fafbd9113e45b68baedcbe698d6b9b0e39b7ff
Franklin Chen <franklinchen@franklinchen.com>
2015-09-23 01:28:00 -0400

    beaker 1.3.4-4-g37d7759

--- Casks/beaker.rb
+++ Casks/beaker.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'beaker' do
-  version '1.3.3-0-g770609a'
-  sha256 '740e779069a42f62db22a0da7d3167fe1369c6b606c596f8bfe084fb0c1ddea1'
+  version '1.3.4-4-g37d7759'
+  sha256 '4bdb869ee4326098b6fbc2552b00b3308985a2b4eadf8ec37b68f2fa17ebad25'
 
   # cloudfront.net is the official download host per the vendor homepage
   url "http://d299yghl10frh5.cloudfront.net/beaker-notebook-v#{version}-mac.dmg"
