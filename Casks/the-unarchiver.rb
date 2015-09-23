4f94aebda18f8fd11f5ab9c0a7f94a799db4f9bb
Leonardo Fedalto <lfedalto@gmail.com>
2015-09-23 13:49:42 -0300

    Upgrade The Unarchiver to v3.10

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'the-unarchiver' do
-  version '3.9.1'
-  sha256 '4911c332df7f4bb23877652700e845fe097b793ae37450948319398009e923a3'
+  version '3.10'
+  sha256 'fa5fd1d3ec1c293b8fa8d60f6c3f36f441eeaf1237975249f7521eace430ea22'
 
-  url "http://unarchiver.c3.cx/downloads/TheUnarchiver#{version}_legacy.zip"
+  url "http://unarchiver.c3.cx/downloads/TheUnarchiver#{version}.dmg"
   name 'The Unarchiver'
   homepage 'http://unarchiver.c3.cx/unarchiver'
   license :oss
