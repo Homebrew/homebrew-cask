ad03432dc72186283caa4c26e77ca301635384d1
prime8 <prime8@users.noreply.github.com>
2015-09-23 07:20:28 -0400

    Update org.nodejs pkg to 4.1.1
      -- https://nodejs.org/en/blog/release/v4.1.1/

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'node' do
-  version '4.1.0'
-  sha256 '9df8830c12bd28a5c1a0e1355cad9f50546e40ce681061d62f7a16e351272764'
+  version '4.1.1'
+  sha256 '1d7ee48a3d66d895692ca8085470358306eb11f398564834c3030cf3fe9f77e0'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   name 'Node.js'
