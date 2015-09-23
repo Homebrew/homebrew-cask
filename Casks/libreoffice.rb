3f641717cb63833aa0949625a7fe8665fe310d21
Alexander Frolov <thephilosoft@gmail.com>
2015-09-23 21:08:43 +0300

    update libreoffice to 5.0.2

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -5,8 +5,8 @@ cask :v1 => 'libreoffice' do
     # documentfoundation.org is the official download host per the vendor homepage
     url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86/LibreOffice_#{version}_MacOS_x86.dmg"
   else
-    version '5.0.1'
-    sha256 'fe4857f275ed12cb6095cab7cd1c7ce44221dc5d97c36b478ffcc283624c6932'
+    version '5.0.2'
+    sha256 'ac06871f2627b2000ce61892c98633c97865927bb211a1a394aafa179abdb46f'
     # documentfoundation.org is the official download host per the vendor homepage
     url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   end
