5732bc932d0c183639ec4f1a1987cfefff5d00c7
buo <buo@users.noreply.github.com>
2015-09-24 18:22:23 +0900

    Update iMazing to 1.3-1442949224

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'imazing' do
-  version '1.2.4-1439985663'
-  sha256 '3c976aef00fd4fc29d224fd387baf7a8f18959a00fdb607e96a8d3fefe062d00'
+  version '1.3-1442949224'
+  sha256 '33365fae77122cf6a665da2240092ce1b4abfa8f69a34ba7461fa62372350674'
 
   # devmate.com is the official download host per the vendor homepage
   url "https://dl.devmate.com/com.DigiDNA.iMazingMac/#{version.sub(%r{-.*},'')}/#{version.sub(%r{.*?-},'')}/iMazingforMac-#{version.sub(%r{-.*},'')}.dmg"
   name 'iMazing'
   appcast 'http://updates.devmate.com/com.DigiDNA.iMazingMac.xml',
-          :sha256 => '097749ec17809265704df2aa4d7eb2fc936a7bab9f89e497dd79877812131dc9'
+          :sha256 => '396e01d8a69fa6a43886a90dd3a9e10f2febfc4397cf29aaacd7e9e0aa0aaeab'
   homepage 'https://imazing.com/'
   license :commercial
 
