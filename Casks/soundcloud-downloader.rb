a0a808ff82cbebe6d2fa33cde5dcf9870693fd06
buo <buo@users.noreply.github.com>
2015-09-26 00:01:54 +0900

    Update SoundCloud Downloader to 2.6.3

--- Casks/soundcloud-downloader.rb
+++ Casks/soundcloud-downloader.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'soundcloud-downloader' do
-  version '2.6.2'
-  sha256 'a68b69c3c1e523a1f93f8f9d774f8fa071e33cd0bbebdaaf7972103a58832005'
+  version '2.6.3'
+  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
   url "http://black-burn.ch/scd/downloads/#{version.delete('.')}/ex/b"
   name 'SoundCloud Downloader'
   appcast 'http://black-burn.ch/applications/scd/updates.php?hwni=1',
-          :sha256 => '3aec7755cdc3208b781ce41613d60f8e574f6c34e3fd819826e6734dd7aac70d'
+          :sha256 => '8b53694a85cec36bd12199a24d8efa7495029a9621fa74e28c29803293f5b1df'
   homepage 'http://black-burn.ch/scd/'
   license :mit
 
