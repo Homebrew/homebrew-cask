6b46864ef7b4335db4d9ed231ebc429951a7448b
bake <w8l@posteo.de>
2015-09-26 19:06:47 +0200

    Upgrade kyoku to v0.0.5

--- Casks/kyoku.rb
+++ Casks/kyoku.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'kyoku' do
-  version '0.0.4'
-  sha256 '5bac75ce062206a12c14d67411a8e802999fa31fb177e730b1ea913c293d6bd9'
+  version '0.0.5'
+  sha256 '2b291bd289ed4fbdaf37b211604a4d271890d9b6d57b4111167adedba37052ef'
 
   url "https://github.com/cheeaun/kyoku/releases/download/#{version}/Kyoku.app.zip"
   appcast 'https://github.com/cheeaun/kyoku/releases.atom'
