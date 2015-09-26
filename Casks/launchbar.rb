21138ba0fe9b13788737adcb24622b4fb2272714
Malo Bourgon <mbourgon@gmail.com>
2015-09-25 18:15:15 -0700

    Updated for version 6.5
    Updated version number to 6.5, and sha256 for new file.

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -5,8 +5,8 @@ cask :v1 => 'launchbar' do
     sha256 '22a1ec0c10de940e5efbcccd18b8b048d95fb7c63213a01c7976a76d6be69a4d'
     url "https://www.obdev.at/downloads/launchbar/legacy/LaunchBar-#{version}.dmg"
   else
-    version '6.4.1'
-    sha256 '9cc78874db65548cb642361fae997fb652623d0ab8fdd0128ce2e71f438d47c4'
+    version '6.5'
+    sha256 '6b92bfaadcb46956220343bd6b6741a3b30e66817a2060471cd1e5551209e071'
     url "https://www.obdev.at/downloads/launchbar/LaunchBar-#{version}.dmg"
   end
 
