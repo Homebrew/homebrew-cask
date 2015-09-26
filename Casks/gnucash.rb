223e86de9a8076e5f588706111cb4d39b1d18189
Sean Roberts <seanorama@gmail.com>
2015-09-26 12:38:36 +0100

    Fix URL in gnucash Cask

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -3,7 +3,7 @@ cask :v1 => 'gnucash' do
   sha256 'd0ed3339ac2f483219aab0b2c3317116b8ddb3a36e86722295899f2eeabbe74e'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/sourceforge/gnucash/Gnucash-Intel-#{version}.dmg"
+  url "http://downloads.sourceforge.net/project/gnucash/gnucash%20(stable)/#{version[/[^-]+/]}/Gnucash-Intel-#{version}.dmg"
   name 'GnuCash'
   homepage 'http://www.gnucash.org'
   license :gpl
