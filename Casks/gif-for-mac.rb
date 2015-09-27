cb7e97f5ecef6ec4828e91640556966705662224
Remko Tronçon <git@el-tramo.be>
2015-09-27 23:13:44 +0200

    Add GIF for Mac latest

---
+++ Casks/gif-for-mac.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'gif-for-mac' do
+  version :latest
+  sha256 :no_check
+
+  # cloudfront.net is the official download host per the vendor homepage
+  url 'https://d309cd6updicdi.cloudfront.net/mac/bin/GIFforMac.dmg'
+  name 'GIF for Mac'
+  homepage 'https://www.riffsy.com/Mac'
+  license :gratis
+
+  app 'GIF for Mac.app'
+end
