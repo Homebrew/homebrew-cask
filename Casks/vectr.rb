010b9229f1ae7bc8514be1103bfe53a93c94d675
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2015-09-29 12:59:57 -0500

    Add Vectr 0.1.7

--- Casks/vectr.rb
+++ Casks/vectr.rb
@@ -2,10 +2,17 @@ cask :v1 => 'vectr' do
   version '0.1.7'
   sha256 '8416736f29f1b2e72d8d4595386bce554bb4ccbc49b27f92a86c6febabfbc35e'
 
+<<<<<<< Updated upstream
   url "http://download.vectr.com/desktop/vectr-mac-#{version}.zip"
   name 'Vectr'
   homepage 'https://vectr.com'
   license :gratis
+=======
+  url 'http://download.vectr.com/desktop/vectr-mac-0.1.7.zip'
+  name 'Vectr'
+  homepage 'https://vectr.com/'
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+>>>>>>> Stashed changes
 
   app 'Vectr.app'
 end
