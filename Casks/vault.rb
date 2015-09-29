d3b6855bf147a02ce7eceaf83b7f8f43d4177b0c
Dieter De Meyer <dieterdemeyer@gmail.com>
2015-09-29 21:00:33 +0200

    Update vault to 0.3.0

--- Casks/vault.rb
+++ Casks/vault.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'vault' do
-  version '0.2.0'
-  sha256 '73dfa187a01fd4490b4c8a62a4316a4bd054538d4fd2df869415b0b00f37d654'
+  version '0.3.0'
+  sha256 'f6c7a30671a90df09d39197262e019a7dd7ad36a68d2f5895080b899aa81fc58'
 
   # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/mitchellh/vault/vault_#{version}_darwin_amd64.zip"
