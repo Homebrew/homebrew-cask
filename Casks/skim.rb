b8475245588a56cd4702c7fe867424cfe887de33
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-09-29 21:10:37 +0200

    update skim to 1.4.14

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'skim' do
-  version '1.4.13'
-  sha256 'bba918cf7a5f54ec1725399d5107ae1c861dd551000f78cf837af2347eff45b0'
+  version '1.4.14'
+  sha256 'b84ada7975a8f6c606d91b4b00871a41393b00dffea9b4928f1242f46def4c8d'
 
   url "http://downloads.sourceforge.net/project/skim-app/Skim/Skim-#{version}/Skim-#{version}.dmg"
   appcast 'http://skim-app.sourceforge.net/skim.xml',
-          :sha256 => '4aa953c1e52056552bcc8fa1609fcd80fe83fac66861de456760a5f7c8fe6f43'
+          :sha256 => '9cc2f44deaa4cbdc53665595a059de90428b3f558e91c5cc599d06e577af047c'
   name 'Skim'
   homepage 'http://skim-app.sourceforge.net/'
   license :bsd
