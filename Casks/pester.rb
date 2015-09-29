9f9f7a7b4ab0de16fea66636251bd1e7ef1c09c5
Victor Popkov <victor@popkov.me>
2015-09-29 23:13:04 +0300

    pester.rb: upgrade to v1.1b20

--- Casks/pester.rb
+++ Casks/pester.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'pester' do
-  version '1.1b19'
-  sha256 'ad8a8b9affcab8a1e30753383062d4e5fb117e3601e3138abae59142130227d9'
+  version '1.1b20'
+  sha256 'da6dc523e6c1df281ba99a947e30a55c8b2fbacb51110f67bd5a504edf53317e'
 
   url "http://sabi.net/nriley/software/Pester-#{version}.dmg"
   appcast 'http://sabi.net/nriley/software/Pester/updates.xml',
-          :sha256 => '05a547dabcb6df809f57695a38c88104b9d2df4ee290c7ba81b725af5bd1426f'
+          :sha256 => 'c9fdbf59afac8f0bdfd8daecb196155b8d194b199e906899ff17ab146b5fb4d5'
   name 'Pester'
   homepage 'http://sabi.net/nriley/software/index.html#pester'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
