8e4e70f8e1453a0f7cc1817e84ccd309b7a1498a
Lars Bilke <lars.bilke@ufz.de>
2015-09-29 08:32:48 +0200

    Otto 0.1.1

--- Casks/otto.rb
+++ Casks/otto.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'otto' do
-  version '0.1.0'
-  sha256 'a03ad303f85eac39dc913a928aedbaf07ca01fd7455b1638eafb7a0de2f03644'
+  version '0.1.1'
+  sha256 '91aaf90903d5decaf0f4317ba72a1d3f932289b429bdee02d2700e1d9137d5b5'
 
   # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/mitchellh/otto/otto_#{version}_darwin_amd64.zip"
