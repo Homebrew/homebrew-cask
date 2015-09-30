f88ea5c856be71aaf2c699221806b00f489e30b8
boydj <boyd_joshua@bah.com>
2015-09-30 10:54:06 -0400

    Version bump to 2.3.6

--- Casks/spyder.rb
+++ Casks/spyder.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'spyder' do
-  version '2.3.5.2'
-  sha256 '38c783b4f4ff83eee5a2ea9bf7a72b49cfc99252e8a7ad8e526621770d7b2c46'
+  version '2.3.6'
+  sha256 'bceb41c323f8910f6e3ad113bd4adbb4d52e996602de146a482b12d57a7bdf82'
 
   # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/spyder-ide/spyderlib/downloads/spyder-#{version}-py2.7.dmg"
