086e23f82dfb39c106f625a3e4fc9ff313ded62e
Fabian Krone <fkrone1993@gmail.com>
2015-10-01 00:26:22 +0200

    Updating ChitChat to version 1.4

--- Casks/chitchat.rb
+++ Casks/chitchat.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'chitchat' do
-  version '1.3'
-  sha256 '0851d0ed5e7b45f80d24c1d253eefd39f384624bd75d948dbb01a2994e47464c'
+  version '1.4'
+  sha256 '4c57de82dae89605b46ec361264237567156ca103541fa1f90e22c9f3636027a'
 
   url "https://github.com/stonesam92/ChitCHat/releases/download/v#{version}/ChitChat.zip"
   appcast 'https://github.com/stonesam92/ChitChat/releases.atom'
