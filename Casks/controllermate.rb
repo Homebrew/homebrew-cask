c66b4e3959f1365375ef1f51bbe575d23136b929
buo <buo@users.noreply.github.com>
2015-10-01 22:57:25 +0900

    Update ControllerMate to 4.9.8

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'controllermate' do
-  version '4.9.7'
-  sha256 'dad06ea5627a4848a3cf983977565fc36a1d3dd75702a2fda97a68ca06b23c14'
+  version '4.9.8'
+  sha256 'ab0e8bf540c04f691d71e39e6fd03350d96112780ee610f3d68fb284bad16765'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/orderedbytes/ControllerMate#{version.delete('.')}.zip"
   name 'ControllerMate'
   appcast 'http://www.orderedbytes.com/sparkle/appcast_cm460.xml',
-          :sha256 => '680a8928983f72e588e92a71d83517ceaec5701c5d6ee70cb4a13671d14e793c'
+          :sha256 => 'b75925e88055ccce64c3845876552c31dd3f1981a555747a0b52d3af9f75b72f'
   homepage 'http://www.orderedbytes.com/controllermate/'
   license :freemium
 
