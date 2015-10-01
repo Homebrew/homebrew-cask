c3c5cbd1b884184c963556827a49966ae9ab5835
Keith Smiley <keithbsmiley@gmail.com>
2015-10-01 09:09:44 -0700

    Update HyperDock
    It seems like this app's download URLs have become unversioned, this is
    the newest one along with its sha256.

--- Casks/hyperdock.rb
+++ Casks/hyperdock.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'hyperdock' do
-  version '1.5.2'
-  sha256 '83dcbc73ff3685487a80f665f30a44ef1d6f713afb3cf80aa3ba3656ac650a05'
+  version :latest
+  sha256 :no_check
 
-  url "https://bahoom.com/hyperdock/#{version}/HyperDock-#{version}.dmg"
+  url "https://bahoom.com/hyperdock/HyperDock.dmg"
   appcast 'https://bahoom.com/hyperdock/appcast.xml',
           :sha256 => '22d00db14142a74fce6466bde7dd590ee7f3c3ec277e59b31a637885f97c9542'
   name 'HyperDock'
