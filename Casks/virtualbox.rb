f62b1a92787fb5d558c013c0eb97cff2fcfee814
Scott Gorsuch <sgorsuch@gmail.com>
2015-10-02 15:23:23 -0400

    Update VirtualBox to 5.0.6

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'virtualbox' do
-  version '5.0.4-102546'   # virtualbox.rb and virtualbox-extension-pack.rb should be upgraded together
-  sha256 '6befb4fa52572416a5155cc6a98b93ad70d42123f1caf75c61bf0fc513ead2c5'
+  version '5.0.6-103037'   # virtualbox.rb and virtualbox-extension-pack.rb should be upgraded together
+  sha256 '612e73aafa3dcdcf99e69e8831fb526512a4e4f8d02a0f4ac9d05baee09bb452'
 
   url "http://download.virtualbox.org/virtualbox/#{version.sub(%r{-.*},'')}/VirtualBox-#{version}-OSX.dmg"
   name 'VirtualBox'
