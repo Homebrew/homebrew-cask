8a6cd47a9f3b72ab0299a62109f4c76edf4d8a26
Lou Rinaldi <ljrinaldi@alumni.albertus.edu>
2015-10-02 13:33:14 -0400

    bump valentina-studio to version 6

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -2,7 +2,7 @@ cask :v1 => 'valentina-studio' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.valentina-db.com/download/release/mac_32/vstudio_5_mac.dmg'
+  url 'http://www.valentina-db.com/download/release/mac_64/vstudio_x64_6_mac.dmg'
   name 'Valentina Studio'
   homepage 'http://www.valentina-db.com/en/valentina-studio-overview'
   license :freemium
