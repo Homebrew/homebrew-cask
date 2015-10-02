3e5f34ba4cb0cde83364c52c0094b875f17f8373
cabbarb <cabbarbolat@gmail.com>
2015-10-02 13:56:37 +0300

    Update iTerm2 cask

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -1,13 +1,13 @@
 cask :v1 => 'iterm2' do
   # note: "2" is not a version number, but indicates a different vendor
-  version '2.1.1'
-  sha256 'b8f1bbd11cdb3e26fd9fab6971c28ebeb422361b2cc5fd6e4a843836d5dedeb0'
+  version '2.1.3'
+  sha256 'f801d54d4dd49ba137fe0d9d5f50ced1916ab01766f4fa3c6132230ce8f3a52b'
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.gsub('.','_')}.zip"
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
   appcast 'https://iterm2.com/appcasts/final.xml',
-          :sha256 => 'c9d0c5800f6851fc55c07d95109f4cb8ef8dfc12a2c1713c1a4dcc5505fba100'
+          :sha256 => '02877596124c999d6e8783d634ed7970864094f4acc450ed34e31cf77dedcef1'
   license :gpl
 
   app 'iTerm.app'
