1bfdae1a6af0d0782e02a7d83039ca52dda053b3
Tommi Komulainen <tommi.komulainen@iki.fi>
2015-10-03 12:02:43 +0200

    mono-mdk: downgrade to 4.0.4.4 El Capitan hotfix
    Installs symlinks to /usr/local/bin so works with El Capitan.
    
    Also, 4.2 is still alpha and main repo is for the stable series.
    
    http://www.mono-project.com/docs/about-mono/releases/4.0.4.4/

--- Casks/mono-mdk.rb
+++ Casks/mono-mdk.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'mono-mdk' do
-  version '4.2.0'
+  version '4.0.4.4'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "http://download.mono-project.com/archive/#{version}/macos-10-x86/MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
+  url "http://download.mono-project.com/archive/4.0.4/macos-10-x86/MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
   name 'Mono'
   homepage 'http://mono-project.com'
   license :oss
