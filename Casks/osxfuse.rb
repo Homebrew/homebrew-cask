6d303218069c112db84178c01fad937974a0fb6d
Jussi Tuominen <me@jussituominen.me>
2015-10-03 19:01:08 +0300

    Update OSXFUSE to v2.8.1

--- Casks/osxfuse.rb
+++ Casks/osxfuse.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'osxfuse' do
-  version '2.8.0'
-  sha256 'ad339ffd6592c3808f63ffc5701993cc7209d29baa5cda1828ea93f6470bb273'
+  version '2.8.1'
+  sha256 '5bc1f3165bedb4395f7b1d1fd88ea25feeca9ee6f2c751c17249bafc6a5c66cd'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/osxfuse/osxfuse-#{version}/osxfuse-#{version}.dmg"
