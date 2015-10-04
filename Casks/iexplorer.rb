b108f0d237824861ca90d61d64a2542c9151ea88
buo <buo@users.noreply.github.com>
2015-10-04 22:16:52 +0900

    Update iExplorer to 3.8.3.0

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'iexplorer' do
-  version '3.8.2.0'
-  sha256 'b0248165c0f79cfd272679dee4a8b359416b98e55a7ad0d278a4ab8d5f1db2ed'
+  version '3.8.3.0'
+  sha256 'c2cab0509c3da45d6b36ef23f4a3a668eef48088c1af761d7fd4e2b05b9bc9f2'
 
   url "https://www.macroplant.com/release/iExplorer-#{version}.dmg"
   appcast 'https://www.macroplant.com/iexplorer/ie3-appcast.xml',
-          :sha256 => 'd846153745620098a0ba84a45b315c7be1a4b1f796adde0844de5ce521a2e88d'
+          :sha256 => '5d7e0fd033f910971b84a35cbc8ce99d0f15e5f45b3a62bd569ae8ba75ff58cb'
   name 'iExplorer'
   homepage 'https://www.macroplant.com/iexplorer/'
   license :freemium
