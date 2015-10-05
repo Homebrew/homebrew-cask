dd816626b8e123da79b97281641fdccb532b2c48
Justin Garrick <justingarrick@gmail.com>
2015-10-05 14:56:38 -0400

    Update hopper-disassembler to 3.10.9.1

--- Casks/hopper-disassembler.rb
+++ Casks/hopper-disassembler.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'hopper-disassembler' do
-  version '3.10.9'
-  sha256 'ed4aaad468b0bd8823c5ec6e68798aad69da4eac02c55222eca50441e7b615f3'
+  version '3.10.9.1'
+  sha256 '044ff7c40f6c760b7dab30f291a6d18483bdd472628d0406accb1099d5925928'
 
   url "http://www.hopperapp.com/HopperWeb/downloads/Hopper-#{version}.zip"
   appcast 'http://www.hopperapp.com/HopperWeb/appcast_v3.php',
-          :sha256 => '7fef169456987610e7e3c533d8629cfb29cfa38f442cbdd2401c8ac7acd0edd5'
+          :sha256 => 'ae81b57b450c7a0c99def5df3af9bf88994c1c265c1aa8d9fb2ab1bcdfcb7e59'
   name 'Hopper'
   name 'Hopper Disassembler'
   homepage 'http://www.hopperapp.com/'
