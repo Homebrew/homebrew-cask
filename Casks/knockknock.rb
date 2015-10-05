ad0c53a198680bdda84424b3ae469acd8d7fad1b
Koen Rouwhorst <info@koenrouwhorst.nl>
2015-10-06 01:34:42 +0200

    Upgrade KnockKnock.app to v1.6.0

--- Casks/knockknock.rb
+++ Casks/knockknock.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'knockknock' do
-  version '1.5.0'
-  sha256 'c0b4c3d8d7fb9981998f6b305aff792b3120c27c978c77cb7484f9a7799f793a'
+  version '1.6.0'
+  sha256 '55740aa84853eaee4da13fb860ee319f5b41f88102377e53f633a49982e7fbcb'
 
   # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/objective-see/deploy/downloads/KnockKnock_#{version}.zip"
