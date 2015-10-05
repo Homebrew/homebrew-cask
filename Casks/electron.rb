e33b0674e698e93170e116825bf579116376f3c1
Victor Popkov <victor@popkov.me>
2015-10-05 23:45:07 +0300

    electron.rb: upgrade to v0.33.6

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'electron' do
-  version '0.33.3'
-  sha256 'e83967d4f2aba573067ac10965a3bd188e90c24249e3a842e7728a293f5ad514'
+  version '0.33.6'
+  sha256 '31138f2a5d54d8e09c145dfb1bb3271a154ce9ec9b4ed29438e3b510cdb61cea'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
-          :sha256 => '4576d50562b71f9272476515e2a188042a0ab723a57930777ea7287f948a5488'
+          :sha256 => '1567ce0344fd8dade9394f810ffa1ee91dcb0d9d57227d8fc702f49c94a34bed'
   name 'Electron'
   homepage 'http://electron.atom.io/'
   license :mit
