08f6d3e9180fa4a94c53ea5c69e6cb83d2bc73ab
buo <buo@users.noreply.github.com>
2015-10-07 05:12:04 +0900

    Update MacSpice to 3.1.6

--- Casks/macspice.rb
+++ Casks/macspice.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'macspice' do
-  version '3.1.5'
-  sha256 '356d606dc8c5724db902b5290dcb3eca270601f1a91a00d0bd5847666237af2f'
+  version '3.1.6'
+  sha256 '9ea9206e18ecdbe71d2fa961badbbd1cb100bad16e30e0213446cf0cd13ab7ff'
 
   url "http://www.macspice.com/mirror/binaries/v#{version}/MacSpice3f5.dmg"
   appcast 'http://www.macspice.com/AppCast-v2.xml',
-          :sha256 => '31111c473934c550022a51f86fde12e5c9905515d188b3aa1f3b404b81173101'
+          :sha256 => '06453465656d258c11326d1f2cba6158a11ff9a939f557fb5da1b3b2d4402db0'
   name 'MacSpice'
   homepage 'http://www.macspice.com/'
   license :closed
