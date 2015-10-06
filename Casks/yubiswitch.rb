6cff264e67e0fde359d7b07c49954181f1c74122
graystevens <retrostyle@gmail.com>
2015-10-06 19:36:12 +0100

    Update yubiswitch.rb

--- Casks/yubiswitch.rb
+++ Casks/yubiswitch.rb
@@ -2,7 +2,7 @@ cask :v1 => 'yubiswitch' do
   version '0.12'
   sha256 '691ff7017c000211587217c60a6a9f352b3cb8d9ee2ce099f01d096d794a01ff'
 
-  url "https://github.com/pallotron/yubiswitch/releases/download/v#{version}_fix/yubiswitch_#{version}.dmg"
+  url "https://github.com/pallotron/yubiswitch/releases/download/v#{version}/yubiswitch_#{version}.dmg"
   appcast 'https://github.com/pallotron/yubiswitch/releases.atom'
   name 'yubiswitch'
   homepage 'https://github.com/pallotron/yubiswitch'
