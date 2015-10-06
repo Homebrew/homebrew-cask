db110959163bc574d634b0302703ed761807332f
buo <buo@users.noreply.github.com>
2015-10-06 22:18:32 +0900

    Update hypo to 0.5.3

--- Casks/hypo.rb
+++ Casks/hypo.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'hypo' do
-  version '0.5.2'
-  sha256 'af49d3c952c8d33e1427d2bd2556d981fbd8bf4957896fd71289c0d965003891'
+  version '0.5.3'
+  sha256 'fc32274ce0189358b28072a5ac612551481312aa21dfda8e69d9d6d1ec765916'
 
   # github.io is the official download host per the vendor homepage
   url "http://hypo.github.io/HypoAppPublic/hypo-#{version}.app.tbz"
   appcast 'http://hypo.github.io/HypoAppPublic/appcast.xml',
-          :sha256 => 'c27c043957cd78630d48a3829ce09b03997c84262845c18d2e1158b95e397b28'
+          :sha256 => '917c51dfaf3bc58c265e8f465813d82ace4f4d0765b0dfaa322fb26a2e545018'
   name 'hypo'
   homepage 'http://hypo.cc/mac.html'
   license :gratis
