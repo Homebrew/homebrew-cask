5f469f77c2ccf402a158c6194d39fd0ba15f7ed9
Arron Mabrey <arron@mabreys.com>
2015-10-06 08:24:25 -0400

    Update quicksilver.rb

--- Casks/quicksilver.rb
+++ Casks/quicksilver.rb
@@ -6,8 +6,8 @@ cask :v1 => 'quicksilver' do
     version '1.2.0-16399'
     sha256 '08c5aeaa7fe89427bcec30a9700eb0b7484bd68b647776b2c82c95355d1679d4'
   else
-    version '1.3.0-16402'
-    sha256 '1857c1a3a80ea5fb88958f12a934dbcc9726526989e815f478e6b0e4375147f6'
+    version '1.3.1-16403'
+    sha256 'c630c6fe386c653e17734a5def40e0d86afa47884602c810510cb180e3d2bcce'
   end
 
   url "http://cdn.qsapp.com/plugins/files/com.blacktree.Quicksilver__#{version.sub(%r{^.*?-},'')}.dmg"
