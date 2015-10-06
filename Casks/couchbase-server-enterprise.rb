08f93c5162f130990c7fc921b4249b28ef54b198
Brian Shumate <brian@couchbase.com>
2015-10-06 15:33:51 -0400

    working versions

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -2,16 +2,16 @@ cask :v1 => 'couchbase-server-enterprise' do
   version '4.0.0'
 
   if MacOS.release >= :el_capitan
-    sha256 '26edea946d845a96e21e887348f853dff0a632b7bf6d42e9d559b647855648f6'
+    sha256 'a49ca050bd47f7a59c871a0955c3682d6c4fbcbb83719038e54614f4da9012e9'
     url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-elcapitan_x86_64.zip"
   else
-    sha256 'ff994c89abf2efcc7dafa6753cfc25c821f3cc24935dccb7a979d170ced56162'
+    sha256 'dad9aa4fd5364576534bb3b765631b5be5f0e1a24303857dce6915d01a297b92'
     url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
   end
 
   name 'Couchbase Server'
   appcast 'http://appcast.couchbase.com/membasex.xml',
-          :sha256 => '26edea946d845a96e21e887348f853dff0a632b7bf6d42e9d559b647855648f6'
+          :sha256 => 'a49ca050bd47f7a59c871a0955c3682d6c4fbcbb83719038e54614f4da9012e9'
   homepage 'http://www.couchbase.com/'
   license :apache
 
