122eac95b963d22f2c9a2e737d36d6e6a775e0a6
Brian Shumate <brian@couchbase.com>
2015-10-06 11:24:55 -0400

    added conditional for 10.11 and removed unnecessary Casks

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -1,8 +1,14 @@
 cask :v1 => 'couchbase-server-enterprise' do
   version '4.0.0'
-  sha256 '1510e21f447f847f39509fdc9838a66e3a951ba8e2c1823848f573984bea480f'
 
-  url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
+  if MacOS.release >= :el_capitan
+    sha256 '1510e21f447f847f39509fdc9838a66e3a951ba8e2c1823848f573984bea480f'
+    url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-elcapitan_x86_64.zip"
+  else
+    sha256 '1510e21f447f847f39509fdc9838a66e3a951ba8e2c1823848f573984bea480f'
+    url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
+  end
+
   name 'Couchbase Server'
   appcast 'http://appcast.couchbase.com/membasex.xml',
           :sha256 => 'dad9aa4fd5364576534bb3b765631b5be5f0e1a24303857dce6915d01a297b92'
