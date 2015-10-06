ef2356959ba3afb62f994807ac6cd8f58a62dd44
Bruce Steedman <MatzFan@users.noreply.github.com>
2015-10-06 15:14:10 +0100

    Add sha256 for valentina-studio v6.0.5

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'valentina-studio' do
-  version :latest
-  sha256 :no_check
+  version '6.0.5'
+  sha256 'c9ae0cec456661c4b57825b55ac43f4b23c6e6f13d8e89a0bd90b2e07e41553a'
 
   url 'http://www.valentina-db.com/download/release/mac_64/vstudio_x64_6_mac.dmg'
   name 'Valentina Studio'
