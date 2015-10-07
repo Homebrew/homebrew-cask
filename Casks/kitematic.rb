7b076b8dcad54a9b9a80de5a18c46c8805867d02
Anthony Scalisi <scalisi.a@gmail.com>
2015-10-07 12:05:53 -0700

    bump Kitematic version

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'kitematic' do
-  version '0.8.5'
-  sha256 '7b84497919c8b2563b136bb73f248c09922e772f10d513e20e9edf4d697580ad'
+  version '0.8.6'
+  sha256 '9c88e7f3aea45495e30537e40eb70c6ca171b74367a453be42edf324c79b7862'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
