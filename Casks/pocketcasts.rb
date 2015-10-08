530892f0341f1bffc322a1cfd20fb5daee081e22
Christian Roth <cr0@users.noreply.github.com>
2015-10-08 14:42:08 +0200

    Update pocketcast to 1.2.1
    Fixed 404 error

--- Casks/pocketcasts.rb
+++ Casks/pocketcasts.rb
@@ -2,7 +2,7 @@ cask :v1 => 'pocketcasts' do
   version '1.2.1'
   sha256 '09837af1105cbb37ef3c74cdc0c000385686777d86c20f2601e49832d553713c'
 
-  url "https://github.com/mortenjust/PocketCastsOSX/releases/download/#{version}/PocketCasts#{version}.zip"
+  url "https://github.com/mortenjust/PocketCastsOSX/releases/download/#{version}/PocketCast#{version}.zip"
   appcast 'https://github.com/mortenjust/PocketCastsOSX/releases.atom'
   name 'Pocket Casts for Mac'
   homepage 'https://github.com/mortenjust/PocketCastsOSX'
