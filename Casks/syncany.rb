2adf9bc6228c520947f7f9679cdb67a07859ff82
Christian Roth <christian.roth@port17.de>
2015-10-08 11:25:57 +0200

    Update Syncany to 0.4.6-alpha
    see https://github.com/syncany/syncany/releases/tag/v0.4.6-alpha

--- Casks/syncany.rb
+++ Casks/syncany.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'syncany' do
-  version '0.4.5-alpha'
-  sha256 '00e03cd3788a438a82c20d5c4ae0d13e69b6eec865b96084095c18aee4470a67'
+  version '0.4.6-alpha'
+  sha256 '6ff3f4eadf716616e4f91402779d77734194071ff191bba4eb2f30352c161f90'
 
   url "https://get.syncany.org/dist/releases/syncany-#{version}-x86_64.app.zip"
   name 'Syncany'
