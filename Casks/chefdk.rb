ab20ae31ca9a13eef8f0e87b4e8c0a4c30e56141
Jeroen Jacobs <git@jeroenj.be>
2015-10-09 09:42:15 +0200

    Updates chefdk to v0.9.0

--- Casks/chefdk.rb
+++ Casks/chefdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'chefdk' do
-  version '0.8.0-1'
-  sha256 '4d4d6d29324aeed5331d7e573a6d658bae78b7bbf3e3502b52702476a8b747b4'
+  version '0.9.0-1'
+  sha256 '8e1d5e79976fc8242c3a1ef2dad728316fc19048c6bc1a68410786ab495b2acb'
 
   # amazonaws is the official download host per the vendor homepage
   url "https://opscode-omnibus-packages.s3.amazonaws.com/mac_os_x/10.8/x86_64/chefdk-#{version}.dmg"
