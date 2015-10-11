6ebc859ac0ce4c47d9dd1a1892b5cdb77db5ddab
Eitot <Tobias@MacBook.local>
2015-10-11 15:26:24 +0200

    Update spotify.rb
    • Change URL to https
    • Expand zap stanza

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -2,7 +2,7 @@ cask :v1 => 'spotify' do
   version :latest
   sha256 :no_check
 
-  url 'http://download.spotify.com/Spotify.dmg'
+  url 'https://download.spotify.com/Spotify.dmg'
   name 'Spotify'
   homepage 'https://www.spotify.com'
   license :gratis
@@ -14,7 +14,9 @@ cask :v1 => 'spotify' do
   zap :delete => [
                   '~/Library/Application Support/Spotify',
                   '~/Library/Preferences/com.spotify.client.plist',
+                  '~/Library/Preferences/com.spotify.client.helper.plist',
                   '~/Library/Caches/com.spotify.client',
+                  '~/Library/Saved Application State/com.spotify.client.savedState',
                   '~/Library/Logs/Spotify'
                  ]
 end
