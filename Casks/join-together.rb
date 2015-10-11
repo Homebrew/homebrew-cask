0d1dd79b2b9b009a8fd50113545419ee3bf44de3
buo <buo@users.noreply.github.com>
2015-10-12 03:20:41 +0900

    Update Join Together to 7.5.1

--- Casks/join-together.rb
+++ Casks/join-together.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'join-together' do
-  version '7.5.0'
-  sha256 'a6661c05b73fff78e1fcfc4e474e95f732ccc996a5bbae56f390e51a414e6867'
+  version '7.5.1'
+  sha256 '116365cb1dae0b3c3a00a90f357e1d154767e6d866e5e18cd32ab3d3f7698d30'
 
   url "http://dougscripts.com/itunes/scrx/jointogether#{version.delete('.')}.zip"
   name 'Join Together'
   appcast 'http://dougscripts.com/itunes/itinfo/jointogether_appcast.xml',
-          :sha256 => '4265e5826859579304b51216b5b675e853b25fda0b1293565a79ff8352a7e363'
+          :sha256 => '1522384c632e3336f4f3ffe3245135f9f7a7718619e564064139c45ad285ee5b'
   homepage 'http://dougscripts.com/itunes/itinfo/jointogether.php'
   license :commercial
 
