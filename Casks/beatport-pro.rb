9ad41142f03093680e3b7cdcf84fde81017b7834
buo <buo@users.noreply.github.com>
2015-10-12 03:01:07 +0900

    Update Beatport to 2.2.2_163

--- Casks/beatport-pro.rb
+++ Casks/beatport-pro.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'beatport-pro' do
-  version '2.2.1_161'
-  sha256 'f9a90e8c3d908dd8425b18a0eac9c72c54f02c456074d0551ea4bb83738fb100'
+  version '2.2.2_163'
+  sha256 '4f2803d53ce19720af93a97874590b6bad29b64b5bbb510d3a710f5e625f04f1'
 
-  url "https://pro.beatport.com/mac/#{version}/beatportpro_#{version}.dmg"
+  url "https://pro.beatport.com/mac/#{version}/beatportpro_#{version}.zip"
   name 'Beatport'
   name 'Beatport Pro'
   appcast 'https://pro.beatport.com/mac/appcast.xml',
-          :sha256 => '0cc995f01751cb46ef98862a5aa8d3d04530c96f961900c48e418875090cc4cc'
+          :sha256 => 'ddf5d7f399cc4cb49f48161f5d0e9f9c45b9def00c5d4bbce99f80ef244ba26c'
   homepage 'https://pro.beatport.com/'
   license :closed
 
