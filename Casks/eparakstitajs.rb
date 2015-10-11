10bc1599ff446d6c94198e11270eec786dbfc1ff
Ameriks <github@ameriks.lv>
2015-10-11 19:31:59 +0300

    Add eSigner - eParakstītājs Latest.
    This Cask installs middleware and signer which can be used to sign and verify documents with Latvian government issued eID card.

---
+++ Casks/eparakstitajs.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'eparakstitajs' do
+  version :latest
+  sha256 :no_check
+  
+  url 'https://www.eparaksts.lv/files/ep3/eparakstitajs3-distribution.pkg'
+  name 'eParakstītājs 3.0'
+  homepage 'https://www.eparaksts.lv'
+  license :closed
+
+  pkg 'eparakstitajs3-distribution.pkg'
+end
