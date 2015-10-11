424172c0d96f491bf69a260b147893813e6acb76
buo <buo@users.noreply.github.com>
2015-10-12 03:23:34 +0900

    Update CrossOver to 14.1.10

--- Casks/crossover.rb
+++ Casks/crossover.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'crossover' do
-  version '14.1.8'
-  sha256 'c5a4ce1fc7f36685bc0b2a982d1e4dbb2056076d13f2824d27713ad397233353'
+  version '14.1.10'
+  sha256 'd52b5200df3ba749709318bba4280d9e245f37583402c8f9e037dcbf08f63790'
 
   url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version}.zip"
   name 'CrossOver'
   appcast 'https://www.codeweavers.com/xml/versions/cxmac.xml',
-          :sha256 => '63ec2ef43db3f2029bd16cb1ef61a8899f568e6c22d2bb35b7361abbcd476edc'
+          :sha256 => 'bf7861be8ff858c5ff7b1d49056a6f4d3d43c8d3f4bd388d450c03e8a6c8e968'
   homepage 'https://www.codeweavers.com/products/crossover-mac/'
   license :commercial
 
