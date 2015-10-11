cf9a4c4cf7d661f1fb18f574999472a8d0ccc6f6
buo <buo@users.noreply.github.com>
2015-10-12 03:22:42 +0900

    Update Data Rescue 4 to 4.2.1

--- Casks/data-rescue.rb
+++ Casks/data-rescue.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'data-rescue' do
-  version '4.2'
-  sha256 '1a8bc1c530aadd192b40a2e8aae7d074952b394ccd89c4f1197312dee50392d8'
+  version '4.2.1'
+  sha256 '2f713f61dd40619029ccaaa3bcc85cc56e6e2cc099fd39649741c7ad069fb948'
 
-  url "https://downloads.prosofteng.com/drmac/Data_Rescue_#{version}_US.dmg"
+  url "https://downloads.prosofteng.com/drmac/Data_Rescue_#{version}.dmg"
   name 'Data Rescue 4'
   appcast 'https://www.prosofteng.com/resources/dr4/dr4_appcast.xml',
-          :sha256 => '7812fbe309ec361502dea912c99ce1668fa5a60891499de82b3c650f0b6cd19d'
+          :sha256 => 'bf199865d7e920ed125bacaaa7a180cc8f782f9bf4373f7f9d925d064d94338c'
   homepage 'https://www.prosofteng.com/products/data_rescue.php'
   license :commercial
 
