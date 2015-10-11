3a9d8b8e9cebe6de5b73aa6d5f5f99b6619990bc
buo <buo@users.noreply.github.com>
2015-10-12 03:23:41 +0900

    Update Cloak to 2.0.13

--- Casks/cloak.rb
+++ Casks/cloak.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'cloak' do
-  version '2.0.12b'
-  sha256 '5bf8290e1d81a55dd2deb0b997517f3c8017a610508eef426dc040c7586665bd'
+  version '2.0.13'
+  sha256 '6ea76b4d92d3bb3fcebab8c752a9dc2e9ac4c562983540fcefd20858118d0e43'
 
   url "https://static.getcloak.com/downloads/osx/updates/Release/Cloak-#{version}.dmg"
   appcast 'https://www.getcloak.com/updates/osx/public/',
-          :sha256 => '9420cd048a0216ef536e2b8242383d8b1c58a113f544e51c23bc14dfa0b85651'
+          :sha256 => '9188560a42923aa393c1a9795fb7ecb252c50b63f363222022b2f972da68aa9e'
   name 'Cloak'
   homepage 'https://www.getcloak.com'
   license :gratis
