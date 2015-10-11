41955999d1b3c8aa8c9f216660ce9edc862a7669
Jiege Chen <jiegec@qq.com>
2015-10-11 21:47:15 +0800

    Update bilibili to 2.14

--- Casks/bilibili.rb
+++ Casks/bilibili.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'bilibili' do
-  version '2.13'
-  sha256 '91b640374cd0f70c6b5da9bb542665d2b07d3f9149e2d8f22fb64958376cf024'
+  version '2.14'
+  sha256 '13cdf426bb3fe555cde886c7f840213c477c7a2b850428df50325367c7b71eee'
 
   url "https://github.com/typcn/bilibili-mac-client/releases/download/#{version}/Bilibili.dmg.zip"
   name 'Bilibili'
   appcast 'http://app.eqoe.cn/updates/bilimac.xml',
-          :sha256 => '48a288cedaf3f038ee0cb2117d6cf1f8d491bc41e59ed9b2766c26acef434176'
+          :sha256 => '57f3d58234c12ea9fbb733d8cba182e9511f5ab85c4fa72b76904492ee8e7154'
   homepage 'https://github.com/typcn/bilibili-mac-client/'
   license :gpl
 
