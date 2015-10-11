2138c2440727d24927cc0590911e8f650fe7875d
Eitot <t.tiemann@protonmail.ch>
2015-10-11 13:55:59 +0200

    Update openemu.rb
    • Add zap stanza
    • Add minimum OS

--- Casks/openemu.rb
+++ Casks/openemu.rb
@@ -8,6 +8,29 @@ cask :v1 => 'openemu' do
   name 'OpenEmu'
   homepage 'http://openemu.org/'
   license :oss
+  depends_on :macos => '>= :lion'
 
   app 'OpenEmu.app'
+  zap :delete => [
+    '~/Library/Preferences/org.openemu.OpenEmu.plist',
+    '~/Library/Caches/org.openemu.OpenEmu',
+    '~/Library/Application Support/OpenEmu',
+    '~/Library/Saved Application State/org.openemu.OpenEmu.savedState',
+    '~/Library/Application Support/org.openemu.OEXPCCAgent.Agents',
+    # Cores preferences
+    '~/Library/Preferences/org.openemu.CrabEmu.plist',
+    '~/Library/Preferences/org.openemu.desmume.plist',
+    '~/Library/Preferences/org.openemu.FCEU.plist',
+    '~/Library/Preferences/org.openemu.Gambatte.plist',
+    '~/Library/Preferences/org.openemu.GenesisPlus.plist',
+    '~/Library/Preferences/org.openemu.Higan.plist',
+    '~/Library/Preferences/org.openemu.Mednafen.plist',
+    '~/Library/Preferences/org.openemu.NeoPop.plist',
+    '~/Library/Preferences/org.openemu.Nestopia.plist',
+    '~/Library/Preferences/org.openemu.Picodrive.plist',
+    '~/Library/Preferences/org.openemu.SNES9x.plist',
+    '~/Library/Preferences/org.openemu.Stella.plist',
+    '~/Library/Preferences/org.openemu.TwoMbit.plist',
+    '~/Library/Preferences/org.openemu.VisualBoyAdvance.plist'
+  ]
 end
