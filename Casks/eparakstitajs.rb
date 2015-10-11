32f6d1512dc0e5e6082a368cfff1841d28757457
Ameriks <github@ameriks.lv>
2015-10-11 22:24:36 +0300

    Fixed trailing whitespaces

--- Casks/eparakstitajs.rb
+++ Casks/eparakstitajs.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'eparakstitajs' do
   version :latest
   sha256 :no_check
-  
+
   url 'https://www.eparaksts.lv/files/ep3/eparakstitajs3-distribution.pkg'
   name 'eParakstītājs 3.0'
   homepage 'https://www.eparaksts.lv'
