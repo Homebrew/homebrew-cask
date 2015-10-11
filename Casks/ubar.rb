60ac09633daaee9a40d5f3df9e38f4e7aa230375
buo <buo@users.noreply.github.com>
2015-10-12 03:26:55 +0900

    Update uBar to 3.0.9

--- Casks/ubar.rb
+++ Casks/ubar.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'ubar' do
-  version '3.0.5'
-  sha256 '302d6fcd0213ef2e8698e3ba54d52bf610b2f51745a3b4175b379e401d247e7c'
+  version '3.0.9'
+  sha256 'f05de1427fdf6cf4ca7d177e99da783bf2c499e99d2215539e69de3617a5536c'
 
   url "http://www.brawersoftware.com/downloads/ubar/ubar#{version.delete('.')}.zip"
   appcast "http://brawersoftware.com/appcasts/feeds/ubar/ubar#{version.to_i}.xml",
-          :sha256 => 'bc407a2ec6071e585a0a3843b49f7df32a7a38a77dafd79cc63e8ae197f21d13'
+          :sha256 => '7a2d319198f5fdc3e358a8238003cd87635ae0b47a5ddba5e598ac5f19bb7674'
   name 'uBar'
   homepage 'http://brawersoftware.com/products/ubar'
   license :commercial
