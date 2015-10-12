4515382dbd58ddaa9dbcc8a305740db0a7663580
Jiangge Zhang <tonyseek@gmail.com>
2015-10-12 16:24:35 +0800

    Upgrade Anvil to 1.1.6

--- Casks/anvil.rb
+++ Casks/anvil.rb
@@ -1,13 +1,13 @@
 cask :v1 => 'anvil' do
-  version '1.1.5'
-  sha256 '342c9041dea854d43fd0c9f78305c9e269e10b2f594079acb2af11675c53fa55'
+  version '2015-09-30_10-05-46'
+  sha256 '13def7101b594964501f3bf32d76ffd16e4b7f97c6bf9bc81c1b891d75624cbe'
 
-  # herokuapp.com is the official download host as per the vendor homepage
-  url 'https://sparkler.herokuapp.com/apps/3/versions/116/download'
+  # amazonaws.com is the official download host as per the vendor homepage
+  url "http://s3.amazonaws.com/sparkler_versions/versions/uploads/000/000/120/original/Anvil_#{version}.zip"
   name 'Anvil'
   appcast 'https://sparkler.herokuapp.com/apps/3/updates.xml'
   homepage 'http://anvilformac.com/'
   license :mit
 
-  app 'Anvil.app'
+  app "Anvil #{version}/Anvil.app"
 end
