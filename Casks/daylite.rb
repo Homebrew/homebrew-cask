cb44438abd89cc2a5007ed428c33beb5b9d11b77
Sven Vetsch <disenchant@users.noreply.github.com>
2015-10-12 09:00:47 +0200

    Update daylite.rb
    changes based on https://github.com/caskroom/homebrew-cask/pull/14337/files#r41715236 and https://github.com/caskroom/homebrew-cask/pull/14337/files#r41715259

--- Casks/daylite.rb
+++ Casks/daylite.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'daylite' do
-  version '5.0.9 (6632)'
+  version '5.0.9'
   sha256 '44f1f0fcf20d10581b7ee412ed83d8afced0b673087bf33e69f47ec0b6098f37'
 
-  url 'https://www.marketcircle.com/daylite/download/daylitedma/'
+  url "https://download.marketcircle.com/daylite/daylitedma#{version.delete('.')}.dmg"
   name 'Daylite'
   homepage 'https://www.marketcircle.com/daylite/'
   license :commercial
