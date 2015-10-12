3c02ee87ea1998fed70205e8201b4af99af6bb87
Nicolas Schwarzentrub <nicolas.schwarzentrub@gmail.comi>
2015-10-12 11:01:44 +0200

    Adding bitscope binaries for OSX 10'.6 or later.
    The casks included are bitscope-dso, bitscope-logic, bitscope-meter and bitscope-chart.
    All binaries for 64 bit
    bitscope-library is not (yet) included!

---
+++ Casks/bitscope-chart.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'bitscope-chart' do
+  version '1.1'
+  sha256 'e7c2900281cf289e6ae458a04c8435024aa526ef2df8d0b17874e71f9c92c3a8'
+
+  url 'http://bitscope.com/download/files/bitscope-chart_1.1.DC01G.app.tgz'
+  name 'Bitscope'
+  homepage 'http://www.bitscope.com/'
+  license :unknown
+
+  app 'bitscope-chart.app'
+
+end
