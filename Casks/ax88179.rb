30a65de1cdc67fd1ce3b214dce705e61c61c9c4a
Lorenzo Villani <lorenzo@villani.me>
2015-10-12 08:06:06 +0200

    Update ax88179 to version 2.6.0_20150820

--- Casks/ax88179.rb
+++ Casks/ax88179.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'ax88179' do
-  version '2.4.0_20141226'
-  sha256 '4de36d86600eef8374fa3161ee7a98980ab5faac47e4f3fbcdc9573af6975b97'
+  version '2.6.0_20150820'
+  sha256 '50b9754649cb9f67a1a911ff07e49c3fc5ac931c49f28dc3235adb95d31e3323'
 
   module Utils
     def self.basename
-      "AX88179_178A_Macintosh_10.6_to_10.10_Driver_Installer_v#{Module.nesting.last.version}"
+      "AX88179_178A_Macintosh_10.6_to_10.11_Driver_Installer_v#{Module.nesting.last.version}"
     end
   end
 
