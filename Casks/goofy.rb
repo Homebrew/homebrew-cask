d0da96d30cf5a6083d0c29fbc86a82eb911b084a
Simon Toft <toft1988@gmail.com>
2015-10-13 09:19:48 +0200

    Updated goofy
    Updated goofy to 2.2.3

--- Casks/goofy.rb
+++ Casks/goofy.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'goofy' do
-  version '2.2.1'
-  sha256 '74b95abb0b6a48048c3c5094b3a256bd48f20a9537bbf010a5a10c529b62bbdd'
+  version '2.2.3'
+  sha256 '6d57b7c6067c63e3096eb49120138642488a4ff33a902fd9adac44fcfe415700'
 
   # github.com is the official download host per the appcast feed
   url "https://github.com/danielbuechele/goofy/releases/download/v#{version}/Goofy.app.zip"
   name 'Goofy'
   appcast 'https://github.com/danielbuechele/goofy/releases.atom',
-          :sha256 => '75d3451f50383b0a4a024ea5937bdf01582d73185e7f63cdebdc62e277815b31'
+          :sha256 => 'fb33fcb13e310a06a718c63cffc9ef70f18859d4c058d3714dd5a50c56080e91'
   homepage 'http://www.goofyapp.com/'
   license :mit
 
