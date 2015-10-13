58664d4a8cf5c8abb125c0bc5506fec2865067f7
John Gerrard Holland <office@jgholland.de>
2015-10-13 15:30:11 +0200

    change panoply to use the version number, which is now part of the URL.

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'panoply' do
-  version :latest
+  version '4.3.1'
   sha256 :no_check
 
-  url 'http://www.giss.nasa.gov/tools/panoply/PanoplyOSX.dmg'
+  url "http://www.giss.nasa.gov/tools/panoply/PanoplyOSX-#{version}.dmg"
   name 'Panoply netCDF, HDF and GRIB Data Viewer'
   name 'Panoply'
   homepage 'http://www.giss.nasa.gov/tools/panoply/'
