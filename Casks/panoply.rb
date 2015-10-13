c9f03da6192b1a599c21852114248e02edcb8340
John Gerrard Holland <office@jgholland.de>
2015-10-13 15:56:59 +0200

    add SHA256

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'panoply' do
   version '4.3.1'
-  sha256 :no_check
+  sha256 '6a700118d7a1c15e4aad50f978be2733f49dbfa051c7901ed791b4d6a8361e67'
 
   url "http://www.giss.nasa.gov/tools/panoply/PanoplyOSX-#{version}.dmg"
   name 'Panoply netCDF, HDF and GRIB Data Viewer'
