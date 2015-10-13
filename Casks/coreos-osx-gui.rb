8a1bcdac215e7ae538b8d638276600394b2ed8d1
Victor Popkov <victor@popkov.me>
2015-10-13 23:25:28 +0300

    coreos-osx-gui.rb: upgrade to v0.9.5

--- Casks/coreos-osx-gui.rb
+++ Casks/coreos-osx-gui.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'coreos-osx-gui' do
-  version '0.9.4'
-  sha256 'c306dd7b7233adaaac242a7da416b2f7186324349e792c946452be9ed1ad5c21'
+  version '0.9.5'
+  sha256 'd9f01eeb7442f42ba13ea56d12c3dcfad3a478d6375d6d65feb08adb4af07306'
 
   url "https://github.com/rimusz/coreos-osx-gui/releases/download/v#{version}/CoreOS_GUI_v#{version}.dmg"
   appcast 'https://github.com/rimusz/coreos-osx-gui/releases.atom'
