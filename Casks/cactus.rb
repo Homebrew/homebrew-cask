b8a815d5dc09497d236314a83e06e2ae703ea2b4
Victor Popkov <victor@popkov.me>
2015-10-13 23:24:08 +0300

    cactus.rb: upgrade to v1.1.18

--- Casks/cactus.rb
+++ Casks/cactus.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'cactus' do
-  version '1.1.15'
-  sha256 'afde5be210ca70a4488fa5d6086a7c25a1fc74208b88a56096c4e355de4b4c37'
+  version '1.1.18'
+  sha256 'ae19cfaaf1995da09041e30b53061f6d3572c7dd35a607090118a54c502adb90'
 
   url "http://update.cactusformac.com/static/downloads/Cactus-#{version}.zip"
   name 'Cactus'
   appcast 'http://update.cactusformac.com.s3-website-us-east-1.amazonaws.com/appcast.xml',
-          :sha256 => 'a8ebf23005cc09a8471aa253f2e45359ab337cf2e0f2c1db2fc8d304e78348df'
+          :sha256 => '5fa42be56b7d6ace4fd7697a70f2b03daa33ad57bcc3178b2b28f106de8508a8'
   homepage 'http://cactusformac.com/'
   license :bsd
 
