7acae2f145a7b6322d6cdb4c60384039d66e1679
Josh Tilles <josh@signafire.com>
2015-10-13 16:36:36 -0400

    VirtualBox: remove vestigial symlink commands.
    At some point, VirtualBox started installing these for us, as the
    following shell interactions demonstrate.
    
    ```
    $ pkgutil --pkg-info org.virtualbox.pkg.virtualboxcli
    package-id: org.virtualbox.pkg.virtualboxcli
    version: 5.0.6
    volume: /
    location: usr/local/bin
    install-time: 1444768345
    $ pkgutil --files org.virtualbox.pkg.virtualboxcli
    VBoxAutostart
    VBoxBalloonCtrl
    VBoxDTrace
    VBoxHeadless
    VBoxManage
    VBoxVRDP
    VirtualBox
    vbox-img
    vboxwebsrv
    ```

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -9,8 +9,6 @@ cask :v1 => 'virtualbox' do
   tags :vendor => 'Oracle'
 
   pkg 'VirtualBox.pkg'
-  binary '/Applications/VirtualBox.app/Contents/MacOS/VBoxManage'
-  binary '/Applications/VirtualBox.app/Contents/MacOS/VBoxHeadless'
 
   uninstall :script => { :executable => 'VirtualBox_Uninstall.tool', :args => %w[--unattended] },
             :pkgutil => 'org.virtualbox.pkg.*'
