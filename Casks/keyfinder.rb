f4c72cf0df421e5a2070267aeeb3f267c875483f
Janic Mikes <janicmikes@users.noreply.github.com>
2015-10-14 19:01:24 +0200

    Keyfinder: Updated download path of archive
    The zip Archive moved to a bins/ folder

--- Casks/keyfinder.rb
+++ Casks/keyfinder.rb
@@ -2,7 +2,7 @@ cask :v1 => 'keyfinder' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.ibrahimshaath.co.uk/keyfinder/KeyFinder-OSX.zip'
+  url 'http://www.ibrahimshaath.co.uk/keyfinder/bins/KeyFinder-OSX.zip'
   name 'KeyFinder'
   homepage 'http://www.ibrahimshaath.co.uk/keyfinder/'
   license :gpl
