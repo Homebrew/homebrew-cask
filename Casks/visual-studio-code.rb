df6f60fe0dd5ec28007cfb836a791fccb118f971
markhuber <mark.huber@dealertrack.com>
2015-10-13 23:19:26 -0500

    Updating visual-studio-code to 0.9.1

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'visual-studio-code' do
-  version '0.8.0'
-  sha256 '9105964f4b7b95fd3831165bd78a534896f22020a1d9c607c86d14532dbd2911'
+  version '0.9.1'
+  sha256 'a8e10d35710f0de0d7cafebbebdb0f7a6db645bf3762ea233e2306882f2fd343'
 
   # vo.msecnd.net is the official download host per the vendor homepage
   url "https://az764295.vo.msecnd.net/public/#{version}/VSCode-darwin.zip"
