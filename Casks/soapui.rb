06081177a6d3de7fcaf318c95715e67df0a7aa7f
Jim LoVerde <loverde@nvisia.com>
2015-10-14 13:43:05 -0500

    removed hard coded version, added the official download host comment, moved installer input lines to a single source line, added description of the installer options chosen, and removed the comment about HermesJMS from the uninstaller

--- Casks/soapui.rb
+++ Casks/soapui.rb
@@ -2,36 +2,26 @@ cask :v1 => 'soapui' do
   version '5.2.1'
   sha256 '773cb3aaa269e14ed34f06d931b6898e6a8ad16d9d08d67cc4935e137bfbe37c'
 
-  url 'http://cdn01.downloads.smartbear.com/soapui/5.2.1/SoapUI-5.2.1.dmg'
+  # smartbear.com is the official download host per the vendor homepage
+  url "http://cdn01.downloads.smartbear.com/soapui/#{version}/SoapUI-#{version}.dmg"
   name 'SmartBear SoapUI'
   homepage 'http://www.soapui.org'
   license :oss
 
-  installer :script => 'SoapUI 5.2.1 Installer.app/Contents/MacOS/JavaApplicationStub',
+  # Run install4j from the distribution in CLI mode, choosing the options to:
+  #   accept the license agreement
+  #   install only SoapUI (not HermesJMS, the # tutorials, or source)
+  #   install to /Applications
+  #   don't create a desktop icon
+  #   don't view the release notes
+  #   don't launch SoapUI after installing.
+  installer :script => "SoapUI #{version} Installer.app/Contents/MacOS/JavaApplicationStub",
             :args => %w[-c],
             :input => [
-              'o',
-              '',
-              '',
-              '',
-              '',
-              '',
-              '',
-              '',
-              '',
-              '',
-              '',
-              '1',
-              '/Applications',
-              '1',
-              'n',
-              'n',
-              'n'
+              'o', '', '', '', '', '', '', '', '', '', '', '1', '/Applications', '1', 'n', 'n', 'n'
             ]
 
-  # Not installing HermesJMS above, but if we did, add:
-  # '/Applications/HermesJMS'
   uninstall :delete => [
-    '/Applications/SoapUI-5.2.1.app'
+    "/Applications/SoapUI-#{version}.app"
   ]
 end
