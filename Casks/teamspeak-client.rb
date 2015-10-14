6a98c4b39c72c875185d22e010f60476603d2e55
Wyatt J. Brown <sushidudeteam@gmail.com>
2015-10-14 01:04:46 -0400

    Update teamspeak-client to 3.0.18.1.

--- Casks/teamspeak-client.rb
+++ Casks/teamspeak-client.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'teamspeak-client' do
-  version '3.0.17'
-  sha256 '0ceec488700527472b16d92b3a73dc2f6170865106687535bcbf589b19b7d556'
+  version '3.0.18.1'
+  sha256 '9a2dc5e348aeed85f8e9630ffdfbe9c1e770594c81892beb8b2d73f4b118e4b2'
 
   # 4players.de is the official download host per the vendor homepage
   url "http://dl.4players.de/ts/releases/#{version}/TeamSpeak3-Client-macosx-#{version}.dmg"
