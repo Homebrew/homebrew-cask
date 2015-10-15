7e41bd2478993e9a4f6403016fe95bb56318770d
Victor Popkov <victor@popkov.me>
2015-10-15 21:36:14 +0300

    witgui.rb: upgrade to v2.2.2

--- Casks/witgui.rb
+++ Casks/witgui.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'witgui' do
-  version '2.2.1'
-  sha256 'c4714f9c689e113bee85bba56b4ec0e38eef7a84dbd7b168d69efdd493e992c4'
+  version '2.2.2'
+  sha256 '6f74c3ea43e24adad2da743963769671b096f329a883a35bd238ab217ad4d35d'
 
   url "http://desairem.altervista.org/witgui/download.php?version=#{version}"
   name 'Witgui'
   appcast 'http://desairem.altervista.org/witgui/appcast.xml',
-          :sha256 => '00acad0f41df33566b4fca4ad153b0658bb864ecdafc298c5a5e972e74f4862f'
+          :sha256 => '6cac8130e4e785ebf497329b1fb01f43ef340bb619bc3115f623f1522f4a3166'
   homepage 'http://desairem.altervista.org/witgui/wordpress/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
