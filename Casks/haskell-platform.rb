d09e301a9d80d4281b66635cfb12fa9f40c4d1c8
Joscha Feth <joscha@feth.com>
2015-10-15 12:03:38 +1100

    Update for OSX 10.11 (El Capitan)

--- Casks/haskell-platform.rb
+++ Casks/haskell-platform.rb
@@ -1,6 +1,11 @@
 cask :v1 => 'haskell-platform' do
-  version '7.10.2-a'
-  sha256 'dd1b64ecec95178044e12a08d9038f1e2156bbd51537da07b18832531b637672'
+  if MacOS.release <= :yosemite
+    version '7.10.2-a'
+    sha256 'dd1b64ecec95178044e12a08d9038f1e2156bbd51537da07b18832531b637672'
+  else
+    version '7.10.2-a1'
+    sha256 '80b1c550e8c6f0a0ffbced9aee8d77956a1246e74934c9baeff355fd4cc9b8a8'
+  end
 
   url "https://www.haskell.org/platform/download/#{version.sub(%r{-.*},'')}/Haskell%20Platform%20#{version}%2064bit-signed.pkg"
   name 'Haskell Platform'
