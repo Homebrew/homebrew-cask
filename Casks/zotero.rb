3945645e47432f6488412b7186a652d898faa3f0
Tim D. Smith <git@tim-smith.us>
2015-10-16 09:11:19 -0700

    zotero 4.0.28.8

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'zotero' do
-  version '4.0.28'
-  sha256 '49fccdf461063cac3233c9472ce03cde690b8a6628fe964d17b269128b3e6c50'
+  version '4.0.28.8'
+  sha256 'c7e9aa8e8dbd4b447ec291ce0d0814740a3b1e42f9241fe0bdc0298ee8d7645e'
 
   url "https://download.zotero.org/standalone/#{version}/Zotero-#{version}.dmg"
   name 'Zotero'
