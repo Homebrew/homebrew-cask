6e98e522c0c0aa9930078b63137299b87efae359
Vítor Galvão <info@vitorgalvao.com>
2015-10-16 22:37:11 +0100

    fix remoteviewer.rb

--- Casks/remoteviewer.rb
+++ Casks/remoteviewer.rb
@@ -3,7 +3,7 @@ cask :v1 => 'remoteviewer' do
   sha256 '96ec9eb33422ac8a717238a7f99fded7c50503198d7302028a09f4e536b02849'
 
   # freedesktop.org is the official download host per the vendor homepage
-  url "http://people.freedesktop.org/~teuf/spice-gtk-osx/dmg/#{version}/RemoteViewer-#{version}.dmg"
+  url "http://people.freedesktop.org/~teuf/spice-gtk-osx/dmg/#{version.sub(%r{-.*},'')}/RemoteViewer-#{version}.dmg"
   name 'Remote Viewer'
   homepage 'http://www.ovirt.org/SPICE_Remote-Viewer_on_OS_X'
   license :gratis
