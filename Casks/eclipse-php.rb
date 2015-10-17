f11066bc2cedeb70394b7214850c8bea31278e0b
Wyatt J. Brown <sushidudeteam@gmail.com>
2015-10-17 02:14:50 -0400

    Update eclipse-{cpp,ide,java,jee,modeling,php,ptp,rcp} to 4.5.1.

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'eclipse-php' do
-  version '4.5'
-  sha256 'e41dd9ffb8b6297beae731bae83c1d5e4606889183b380faf37f15dc85fb3230'
+  version '4.5.1'
+  sha256 '142fdeeedf091149a6d72e0aacf28f6937fe6bef01d83cbf141be4d847172492'
 
-  url 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/R/eclipse-php-mars-R-macosx-cocoa-x86_64.tar.gz&r=1'
+  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/1/eclipse-php-mars-1-macosx-cocoa-x86_64.tar.gz&r=1'
   name 'Eclipse'
   name 'Eclipse for PHP Developers'
   homepage 'https://eclipse.org/'
