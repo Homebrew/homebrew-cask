4a655a4dbdcb1088e9256066d048adce88dcc204
Victor Popkov <victor@popkov.me>
2015-10-18 22:59:48 +0300

    bandage.rb: upgrade to v0.7.0

--- Casks/bandage.rb
+++ Casks/bandage.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bandage' do
-  version '0.6.4'
-  sha256 '29407d60fbe847184bc55eeb66d1a5437e8d65559bc1c39bea7cf38b8e6c8584'
+  version '0.7.0'
+  sha256 '0da51065f7ac38cd3bdad354d8cda427ba4e7e17a5658407d4a8729b7464935d'
 
   url "https://github.com/rrwick/Bandage/releases/download/v#{version}/Bandage_Mac_v#{version}.zip"
   appcast 'https://github.com/rrwick/Bandage/releases.atom'
