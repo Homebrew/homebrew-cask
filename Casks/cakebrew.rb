243353bc3305f819a9a915e05933ffe06976046b
Victor Popkov <victor@popkov.me>
2015-10-18 23:05:43 +0300

    cakebrew.rb: upgrade to v1.1.4

--- Casks/cakebrew.rb
+++ Casks/cakebrew.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'cakebrew' do
-  version '1.1.3'
-  sha256 '5061af3f82dab82fe9c50250cd670743aa2401ec97658361ff509a9418e30edd'
+  version '1.1.4'
+  sha256 '072a399ccbed4e08892a9e64a85db51383deec951f29f66a5bd28cee9360347d'
 
   url "https://www.cakebrew.com/files/cakebrew-#{version}.dmg"
   appcast 'https://www.cakebrew.com/appcast/profileInfo.php',
-          :sha256 => '6f332be542626eaf275dd7a58d473772c9e954615b5d669a183ef5485b54463e'
+          :sha256 => '1c147ec8de9b02df0eab2bbcae96b65d6a5e6ee76a865a3f6ac081c6b4b8f65b'
   name 'Cakebrew'
   homepage 'https://www.cakebrew.com/'
   license :gpl
