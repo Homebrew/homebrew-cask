3fa65c9956dfeeb484c96c29f06b52b6aed0296f
Victor Popkov <victor@popkov.me>
2015-10-18 23:54:07 +0300

    totals.rb: upgrade to v3.1.1

--- Casks/totals.rb
+++ Casks/totals.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'totals' do
-  version '3.1'
-  sha256 '35efc4f7521ecdc6baec8284d2e52fad4f461195416f5c4fc60bd35e969c8cb2'
+  version '3.1.1'
+  sha256 '06739b005d95d319ce8884fc5ad89b1ed84b01a4ccd586dc2aaec4719155c8d3'
 
   url "http://www.kedisoft.com/dl/totals/Totals-#{version}.dmg"
   name 'Totals'
   appcast 'http://kedisoft.com/appcasts/appcast.php?app=totals3',
-          :sha256 => '2355747b573a2d9143cfbc55529ac191a1ed949afed935d9e88ebdf3b54a08dd'
+          :sha256 => '0d0dff1b24438235cec9f4c0da31c410ab3b172fa79a32bab4ea0de325dda89c'
   homepage 'http://www.kedisoft.com/totals/'
   license :commercial
 
