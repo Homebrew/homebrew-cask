38fb31f4983cf7b9ee831c0b51bcf0d4623aee7c
linguofeng <linguofeng@msn.com>
2015-10-20 22:48:55 +0800

    Upgrade PSequel to Latest

--- Casks/psequel.rb
+++ Casks/psequel.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'psequel' do
-  version '1.1.2'
-  sha256 '19c2721002ed35e899bfdc9fd1a52f1d8cbc42e869b407eaf536ce82a32763f5'
+  version :latest
+  sha256 :no_check
 
-  url "http://www.psequel.com/download?version=#{version}"
+  url "http://www.psequel.com/download?version=latest"
   name 'PSequel'
   homepage 'http://www.psequel.com'
   license :closed
