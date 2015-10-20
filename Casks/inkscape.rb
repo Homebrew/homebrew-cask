fadb0a24644c68874c91e421e2af0fe3a0c73787
Julien Lavergne <lavergne.julien@gmail.com>
2015-10-20 21:30:31 +0200

    fixed shasum 256

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'inkscape' do
   version '0.91-1'
-  sha256 'a9f6ec70d9fcd16ee2e5ac3539d0ba6daefc6a87923f1dc3c2dfb45b22d4720b'
+  sha256 '66c9af880f87d5b64006391fc8dd2d3f546d93d6d91235eba7856deacba29862'
 
   # fastly.net is the official download host per the vendor homepage
   url "https://inkscape.global.ssl.fastly.net/media/resources/file/Inkscape-#{version}-x11-10.7-x86_64.dmg"
