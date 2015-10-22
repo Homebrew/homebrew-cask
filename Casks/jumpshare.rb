b2e309d10530f3b8b7ce33f53723493c0a433b06
Vincent Klaiber <vincentklaiber@gmail.com>
2015-10-22 08:57:07 +0200

    Update Jumpshare.app to version 2.1.1

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'jumpshare' do
-  version '1.0.25-3'
-  sha256 'a2a2d44d858616965c8271dcfc177ec299e9592fed3abb795ddb81b533f6d818'
+  version '2.1.1'
+  sha256 '8181e5b35bd02dda770f81ba8f39b83e3c1cde0b934e2e563d962ce4ea16fced'
 
-  url "https://jumpshare.com/desktop/mac/Jumpshare_#{version}.dmg"
+  url "https://jumpshare.com/apps/mac/Jumpshare-#{version}.zip"
   name 'Jumpshare'
   homepage 'https://jumpshare.com/'
   license :gratis
