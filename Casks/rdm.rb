f20f99a1a4db83b4a24f03bd441a4873ab8e2617
Edward Samson <edward@samson.ph>
2015-10-23 10:59:03 +0800

    rdm.rb: upgrade to v0.8.0-2544

--- Casks/rdm.rb
+++ Casks/rdm.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'rdm' do
-  version '0.8.0-2540'
-  sha256 'c9b52b65e11b5a7eb240fdd5741b274cf461fc54a3e32b684e157ec713b69436'
+  version '0.8.0-2544'
+  sha256 '40d0e37a3af9084294b8638ba4f1f5a198ac8e77708f4da31754bb96a33bdd83'
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com/uglide/RedisDesktopManager/releases/download/#{version.sub(%r{-.*},'')}-rc4/redis-desktop-manager-#{version}.dmg"
+  url "https://github.com/uglide/RedisDesktopManager/releases/download/#{version.sub(%r{-.*},'')}-release/redis-desktop-manager-#{version}.dmg"
   appcast 'https://github.com/uglide/RedisDesktopManager/releases.atom'
   name 'Redis Desktop Manager'
   homepage 'http://redisdesktop.com'
