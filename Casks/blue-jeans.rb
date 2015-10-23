9a93c639640991934df2774a5548bed7e349078e
Larry Gilbert <larry@L2G.to>
2015-10-23 15:47:58 -0700

    Add Blue Jeans app installer
    The version in the file name is 152, but it actually downloads the
    latest version (1.6.153 at this writing).

---
+++ Casks/blue-jeans.rb
@@ -0,0 +1,13 @@
+cask :v1 => 'blue-jeans' do
+  version :latest
+  sha256 :no_check
+
+  # Download page: https://bluejeans.com/downloads
+  url 'https://swdl.bluejeans.com/desktop/mac/launchers/BlueJeansLauncher_live_152.dmg'
+  name 'Blue Jeans videoconferencing'
+  homepage 'https://bluejeans.com/'
+  license :gratis
+  depends_on :macos => '>= 10.6'
+
+  installer :manual => 'Blue Jeans Launcher.app'
+end
