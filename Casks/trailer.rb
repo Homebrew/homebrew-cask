fa9769ccb10c29107beab009c20c1011aac2e1f0
Maurice Kelly <osv-github@chatswood.org.uk>
2015-10-23 11:55:10 +0100

    Update Trailer to version 1.3.9
    This commit updates the version and sha256 stanzas.

--- Casks/trailer.rb
+++ Casks/trailer.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'trailer' do
-  version '1.3.7'
-  sha256 '9f022093051d6512a888cb1a0760afebed51e7c9f33dd991a81218f492491e55'
+  version '1.3.9'
+  sha256 '4f81aee67ecb732aaeaed39190ba64ab06e67a07bef95963d207117fc6313334'
 
   url "https://ptsochantaris.github.io/trailer/trailer#{version.delete('.')}.zip"
   appcast 'https://ptsochantaris.github.io/trailer/appcast.xml',
