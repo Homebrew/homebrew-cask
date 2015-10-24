34a3b78f21d8d3c3906ff2fcc0bb00c63e61a63b
Fuad Saud <fuadsaud@users.noreply.github.com>
2015-10-24 19:47:58 -0200

    Bump BlueJ to version 3.1.6

--- Casks/bluej.rb
+++ Casks/bluej.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'bluej' do
-  version '3.1.5'
-  sha256 'a9b30c1dcfd8f4245f22fb000a64ec09c1a8c353d4880cb818cc14b1f6fdf6ef'
+  version '3.1.6'
+  sha256 '925edfd6201167f6f04a04fe95980f8c800b9a8c0d27fe72b6eea3a709071a32'
 
   url "http://www.bluej.org/download/files/BlueJ-#{version.delete('.')}.zip"
   name 'BlueJ'
