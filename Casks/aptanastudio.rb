2198b5a40258687ad6d26823edc4d1cf32727245
Jean-Pierre Matsumoto <jpmat@apu.matsusoft.com>
2015-10-25 02:55:10 +0200

    Revert aptanastudio.rb change before pull request

--- Casks/aptanastudio.rb
+++ Casks/aptanastudio.rb
@@ -1,14 +1,10 @@
 cask :v1 => 'aptanastudio' do
   version '3.6.1'
   sha256 '1eed9a94ce9b9bf03743f0bfdc5efd752b3470842170ba571ee25d2810c3c8f0'
-<<<<<<< HEAD
-  url "https://github.com/aptana/studio3/releases/download/v#{version}/Aptana_Studio_3_Setup_#{version}.dmg"
-=======
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/aptana/studio3/releases/download/v#{version}/Aptana_Studio_3_Setup_#{version}.dmg"
   appcast 'https://github.com/aptana/studio3/releases.atom'
->>>>>>> upstream/master
   name 'Aptana Studio'
   homepage 'http://www.aptana.com/'
   license :gpl
