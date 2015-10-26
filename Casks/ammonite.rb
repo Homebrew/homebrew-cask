ef93b10b63d49f6e72d3f8655fb6ec7db1425275
Victor Popkov <victor@popkov.me>
2015-10-26 22:21:01 +0200

    ammonite.rb: upgrade to v1.13

--- Casks/ammonite.rb
+++ Casks/ammonite.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'ammonite' do
-  version '1.12'
-  sha256 '4bc4dd3c77efea03c324d7e671549ca9a7b23946f421c8bcbca5269c76d692be'
+  version '1.13'
+  sha256 '1a76406380fe633b475539b8238fdb4dd7667572caa44544bcd0dc110d636962'
 
   url "http://www.soma-zone.com/download/files/Ammonite_#{version}.tbz"
   name 'Ammonite'
   appcast 'http://www.soma-zone.com/Ammonite/a/appcast.xml',
-          :sha256 => '7c92236e3cb60afca3fa7e1a635a801e1c26c59efeec3c57b76c5c30a1946baf'
+          :sha256 => 'd8a28c27fff6cd47329846952eae3ac7d79f51139a222111f7b007794be795e8'
   homepage 'http://www.soma-zone.com/Ammonite/'
   license :closed
 
