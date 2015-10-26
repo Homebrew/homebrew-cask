256f30a6340ecee8004f559fc84d5144796dfcc8
Joel Castillo <Joel.Castillo@nyu.edu>
2015-10-25 22:16:30 -0400

    Upgrade OpenLP.app to v2.2
    Upgrading OpenLP to version 2.2 from version 2.05

--- Casks/openlp.rb
+++ Casks/openlp.rb
@@ -1,9 +1,8 @@
 cask :v1 => 'openlp' do
-  version '2.0.5'
-  sha256 '0ba5a37c359394c277e221f8ca74191d3425cf3cfbd70699d81346c77c93d746'
+  version '2.2'
+  sha256 :no_check
 
-  # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/project/openlp/openlp/#{version}/OpenLP-#{version}.dmg"
+  url "https://get.openlp.org/2.2/OpenLP-2.2.dmg"
   name 'OpenLP'
   homepage 'http://openlp.org'
   license :gpl
