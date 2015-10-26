664c9fc49e893cddd50b68a3d268ba9606eae87b
Honza Dvorsky <jan.dvorsky@yahoo.com>
2015-10-26 11:25:00 +0100

    Add Buildasaur v0.6.3

---
+++ Casks/Buildasaur.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'Buildasaur' do
+  version '0.6.3'
+  sha256 '347edecdade78a3959bf6c00f43d0d5b7eb33e78b1f53363b36cc8b61f3593f5'
+
+  url 'https://github.com/czechboy0/Buildasaur/releases/download/v0.6.3/Buildasaur.app.zip'
+  name 'Buildasaur'
+  homepage 'https://github.com/czechboy0/Buildasaur'
+  license :mit
+
+  app 'Buildasaur.app'
+end
