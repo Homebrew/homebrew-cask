dd3c4d83fa5a4fad12ce8bde8aa0804f378f297b
forkbomber <forkbomber@users.noreply.github.com>
2015-10-26 04:45:49 -0400

    update license

--- Casks/proxifier.rb
+++ Casks/proxifier.rb
@@ -5,7 +5,7 @@ cask :v1 => 'proxifier' do
   url 'https://www.proxifier.com/distr/ProxifierMac.zip'
   name 'Proxifier'
   homepage 'https://www.proxifier.com/mac/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'Proxifier.app'
 end
