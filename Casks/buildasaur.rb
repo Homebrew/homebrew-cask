00fdad2904f76f8204160383298a94f5f620581d
Vítor Galvão <info@vitorgalvao.com>
2015-10-26 13:27:21 +0000

    buildasaur.rb: use version interpolation

--- Casks/buildasaur.rb
+++ Casks/buildasaur.rb
@@ -2,7 +2,7 @@ cask :v1 => 'buildasaur' do
   version '0.6.3'
   sha256 '347edecdade78a3959bf6c00f43d0d5b7eb33e78b1f53363b36cc8b61f3593f5'
 
-  url 'https://github.com/czechboy0/Buildasaur/releases/download/v0.6.3/Buildasaur.app.zip'
+  url "https://github.com/czechboy0/Buildasaur/releases/download/v#{version}/Buildasaur.app.zip"
   appcast 'https://github.com/czechboy0/Buildasaur/releases.atom'
   name 'Buildasaur'
   homepage 'https://github.com/czechboy0/Buildasaur'
