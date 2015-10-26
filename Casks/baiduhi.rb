1b98170cdff8d8eda9360197ea6adaa17c7b61c8
DanChau <github@danc.co>
2015-10-26 11:56:53 +0800

    update baiduhi to 1.5.3.1

--- Casks/baiduhi.rb
+++ Casks/baiduhi.rb
@@ -2,7 +2,7 @@ cask :v1 => 'baiduhi' do
   version '1.5.3.1'
   sha256 'f76d6ca22df49631d40cb257494428616f2635079823885296e8d95b6afe7efb'
 
-  url "http://im.baidu.com/download/machi_setup.dmg"
+  url 'http://im.baidu.com/download/machi_setup.dmg'
   name '百度 Hi'
   name 'Baidu Hi'
   homepage 'http://im.baidu.com/'
