558f12235a4cc9de7447b4f3aaee079b7dad97e4
mawie81 <mawie@googlemail.com>
2015-10-26 17:49:19 +0100

    Upgrade WhatsDesktop to 1.0.1

--- Casks/whatsdesktop.rb
+++ Casks/whatsdesktop.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'whatsdesktop' do
-  version '1.0.0'
-  sha256 'bf76f620ad1f4e495eaf1f36f5300245c85afb232f7c2bcff741fe26d7b77d1b'
+  version '1.0.1'
+  sha256 '8d3930a9d7ed6173a57f185d72aec45a3269f17062c5ce423be2e4db60340d67'
 
   url "https://github.com/mawie81/whatsdesktop/releases/download/#{version}/WhatsDesktop-osx-#{version}.zip"
   appcast 'https://github.com/mawie81/whatsdesktop/releases.atom'
