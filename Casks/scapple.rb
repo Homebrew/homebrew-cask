20b3b66ea2c400a291bfcf8af034a38a776eb1f9
adityadalal924 <github@adityadalal.com>
2015-10-27 15:32:20 -0400

    updated scapple (1.2)

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'scapple' do
-  version :latest
-  sha256 :no_check
+  version '1.2'
+  sha256 '1b9968a7927c29b5b4fa4d2c9e5ce78c56e5c3cfbb42847836c2e732018d92c1'
 
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://scrivener.s3.amazonaws.com/Scapple.dmg'
