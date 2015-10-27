5009ba263f94addbb95bf32a1876844dc0a900e7
jekuno <jekuno@users.noreply.github.com>
2015-10-27 11:51:32 +0100

    Updates to tiddlywiki 0.0.5

--- Casks/tiddlywiki.rb
+++ Casks/tiddlywiki.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'tiddlywiki' do
-  version '0.0.4'
+  version '0.0.5'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '7d19e330c10c28694dbfef2ba09a66422d22f77e532b51831ab4404b7d9d23c6'
-    url "https://github.com/Jermolene/TiddlyDesktop/releases/download/v#{version}/tiddlydesktop-mac32-#{version}.zip"
+    sha256 '42512d123afe38a1ad40562fcb34c7556bf76930a96ed47ba30e98ff7fe8f76e'
+    url "https://github.com/Jermolene/TiddlyDesktop/releases/download/v#{version}/tiddlydesktop-mac32-v#{version}.zip"
   else
-    sha256 'e7ec36897c60c40f56ac0e1c66395e1c517ca403d9d1c0188738be6a7a0b8400'
-    url "https://github.com/Jermolene/TiddlyDesktop/releases/download/v#{version}/tiddlydesktop-mac64-#{version}.zip"
+    sha256 '59d34e2fb142d776dc33d9738a8f5032274f8a580ee28bd37b35b6f288c05e65'
+    url "https://github.com/Jermolene/TiddlyDesktop/releases/download/v#{version}/tiddlydesktop-mac64-v#{version}.zip"
     appcast 'https://github.com/Jermolene/TiddlyDesktop/releases.atom'
   end
 
