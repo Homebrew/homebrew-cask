826ed2dd19ab80a4c0c2148ca5917db156ce945f
Victor Popkov <victor@popkov.me>
2015-10-27 20:31:42 +0200

    amethyst.rb: upgrade to v0.9.6

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'amethyst' do
-  version '0.9.5'
-  sha256 'c4e67df3e8c2c9e885f1b5d230d87462dcfc8a50a81cffc62d1096fda589d9a8'
+  version '0.9.6'
+  sha256 '62f62c915539dd5d9fda7b062286d929eca84db6b7a889cdf91cc538d58c7106'
 
   url "https://ianyh.com/amethyst/versions/Amethyst-#{version}.zip"
   name 'Amethyst'
   appcast 'https://ianyh.com/amethyst/appcast.xml',
-          :sha256 => '7256809d4b302019f17b91ba4209ba02cbd3450758a5d17f37aaf71aa2e50306'
+          :sha256 => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   homepage 'https://ianyh.com/amethyst'
   license :mit
 
