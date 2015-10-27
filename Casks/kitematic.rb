2b7b27376e62efa40249614448b38f3d2b960ca7
David Routhieau <root-io@users.noreply.github.com>
2015-10-27 12:15:57 +0000

    Update Kitematic

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'kitematic' do
-  version '0.8.7'
-  sha256 '4512becd9422b1a377fdd932bb7178e5901c29d2e45532a5c0244477ddd76ff8'
+  version '0.9.2'
+  sha256 '7cb8f179938f2b87f597bdeea87edb0d6d74b2b7e11c61481e3b6075f7f7f278'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
