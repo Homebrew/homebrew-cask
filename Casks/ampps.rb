5426fd646c528867af17dbffa2008adb5d935c2d
Mike Branderhorst <mikebranderhorst@gmail.com>
2015-10-27 13:11:18 +0100

    Update ampps.rb
    Update AMPPS shasum (v3.3)

--- Casks/ampps.rb
+++ Casks/ampps.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'ampps' do
   version '3.3'
-  sha256 '2304de68a6de293f1b36c72ac8763414da4397c5f80a3ccaebc5039afab55dae'
+  sha256 '46a28cfda2478a9e7d8af789a28723cba48b51a4da772a371c245714b8a0532f'
 
   url "http://files.ampps.com/AMPPS-#{version}.dmg"
   name 'AMPPS'
