db6e730b1ef706b2eda217991101f0ac0e78e860
dnsfox <atanas.pepechkov@gmail.com>
2015-10-29 00:28:33 +0200

    Upgrade The Archive Browser to 1.10.1

--- Casks/the-archive-browser.rb
+++ Casks/the-archive-browser.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'the-archive-browser' do
-  version '1.9.1'
-  sha256 'c658cc67fde3773b20731394c3ca2829285b3e02efa10c4efb360b2bf053f26e'
+  version '1.10.1'
+  sha256 '6bdb2ff4af904bd5228b0c399ce6d285807a3674e54bbc3a887a26ab1282d0cc'
 
-  url "http://wakaba.c3.cx/releases/mac/TheArchiveBrowser#{version}.zip"
+  url "http://wakaba.c3.cx/releases/TheArchiveBrowser/TheArchiveBrowser#{version}.zip"
   name 'The Archive Browser'
   homepage 'http://archivebrowser.c3.cx'
   license :commercial
