e83188cabdca3b1d14f2bfa00cd694e49e88f7f8
Vítor Galvão <info@vitorgalvao.com>
2015-10-28 16:13:17 +0000

    thunderbird.rb: fixed missing comma

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -13,7 +13,7 @@ cask :v1 => 'thunderbird' do
   zap :delete => [
         '~/Library/Thunderbird',
         '~/Library/Caches/Thunderbird',
-        '~/Library/Saved Application State/org.mozilla.thunderbird.savedState'
-        '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.mozilla.thunderbird.sfl',
+        '~/Library/Saved Application State/org.mozilla.thunderbird.savedState',
+        '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.mozilla.thunderbird.sfl'
       ]
 end
