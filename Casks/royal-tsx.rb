5012f18857f5fb2e3653741529df01e3ad63de94
buo <buo@users.noreply.github.com>
2015-10-30 02:35:37 +0900

    Update Royal TSX to 2.2.0.1000

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'royal-tsx' do
-  version '2.1.0.1000'
-  sha256 '69af55cbb53244649d84535a7340d6453c021b9d192f5482fbc4fed0f2af2cb1'
+  version '2.2.0.1000'
+  sha256 'e547054b6ce818d1429701804069d0aec7c5e494f9b757e2068093bb6ca4c9a2'
 
   url "http://v2.royaltsx.com/updates/royaltsx_#{version}.dmg"
   appcast 'http://v2.royaltsx.com/updates_beta.php',
-          :sha256 => '035b9ce2264b942108386aa40456ebb87ab772df35569898867ffdfd64b95901'
+          :sha256 => '326f26d7c0d628d5fbccfdb4986593a5da5288b26d71bbfa7e504747cb08d309'
   name 'Royal TSX'
   homepage 'http://www.royaltsx.com'
   license :freemium
