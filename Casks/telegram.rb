abc5dc38a76a8883c3de446ef1e3d4c41ebc0b02
buo <buo@users.noreply.github.com>
2015-10-30 02:40:48 +0900

    Update Telegram to 0.9.6

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'telegram' do
-  version '0.9.2'
-  sha256 'fd6654a8f9d99fe04efb25ef239233f7ee500d1dc113c2e494e6b92c9897ec5b'
+  version '0.9.6'
+  sha256 '65dea3c795114b8c71fc045f67cb5bb77c7fa197b4035bca90362a1731bd66ec'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
