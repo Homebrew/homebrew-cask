e5b936e2f6dbfcce4e25362903d912444663fe13
buo <buo@users.noreply.github.com>
2015-10-30 02:31:36 +0900

    Update iPhoto Library Manager to 4.2.1

--- Casks/iphoto-library-manager.rb
+++ Casks/iphoto-library-manager.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'iphoto-library-manager' do
-  version '4.1.11'
-  sha256 '901741ef76ba58458821f83f37ca02b6d55b3dacdf4c8ad26899f0ea3f77c5d5'
+  version '4.2.1'
+  sha256 'fc4c70e5fd54d73e2f73c0acc6bfcd3c744dfd7ccb99119afaf824467cb0c015'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/fatcatsoftware/iplm/iPhotoLibraryManager_#{version.delete('.')}.zip"
   name 'iPhoto Library Manager'
   appcast 'http://www.fatcatsoftware.com/iplm/iplm4_appcast.xml',
-          :sha256 => 'bdb187324ac7a3eb0ac3a261c68672a17b12b8e9fa675b2426fc943875c4cf1a'
+          :sha256 => 'bdfe38601eeb932f7865021ad6da5c68af3c5c92c84f3cc598b04b9178348388'
   homepage 'http://www.fatcatsoftware.com/iplm/'
   license :commercial
 
