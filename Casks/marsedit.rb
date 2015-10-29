0942a01dedbb3ef72209512be09f3112db684e68
buo <buo@users.noreply.github.com>
2015-10-30 02:32:12 +0900

    Update MarsEdit to 3.7.3

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'marsedit' do
-  version '3.7.1'
-  sha256 'c0ff0b59aae840b53a54b7515bfa4b13e5a3b38275a281ab9d5b5a097367a474'
+  version '3.7.3'
+  sha256 'b7e9d15d2b7e07442742900b11362a9d7b28d34557e530dd23282d8e8f62b6c3'
 
   url "http://www.red-sweater.com/marsedit/MarsEdit#{version}.zip"
   appcast 'http://www.red-sweater.com/marsedit/appcast3.php',
-          :sha256 => '02f5ae0eddaa596db862bda36a4c84c47848cfd15cfe384b0d0322d8f73a80ad'
+          :sha256 => '3250869e30276202cdc41279ba3a7936114a87ed9d157c836362b0f3add9aa62'
   name 'MarsEdit'
   homepage 'http://www.red-sweater.com/marsedit/'
   license :commercial
