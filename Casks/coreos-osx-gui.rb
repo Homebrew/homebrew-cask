7d9ab9a94be1cd7be59514ea21b973dcdbff47cc
buo <buo@users.noreply.github.com>
2015-10-30 02:22:00 +0900

    Update CoreOS GUI to 1.0.2

--- Casks/coreos-osx-gui.rb
+++ Casks/coreos-osx-gui.rb
@@ -1,9 +1,10 @@
 cask :v1 => 'coreos-osx-gui' do
-  version '1.0.2'
-  sha256 '8ecab2abe4958836b1c3f9cb1ad8bfa7215e3a4a1623f49b0183c520a625be5a'
+  version '1.0.4'
+  sha256 '38b6bc5473b5537299cac4d8bd5de18d38a86ccf86f26e3c1540d0b3ab26c71e'
 
   url "https://github.com/rimusz/coreos-osx-gui/releases/download/v#{version}/CoreOS_GUI_v#{version}.dmg"
-  appcast 'https://github.com/rimusz/coreos-osx-gui/releases.atom'
+  appcast 'https://github.com/rimusz/coreos-osx-gui/releases.atom',
+          :sha256 => '031922a6fff5c76e477c857f376dbcbaf694c85df690aa37ec2a38072673709c'
   name 'CoreOS GUI'
   name 'CoreOS-Vagrant GUI'
   homepage 'https://github.com/rimusz/coreos-osx-gui'
