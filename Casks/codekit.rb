6648d8281715a4b7d286305f039a6a0f415afe78
buo <buo@users.noreply.github.com>
2015-10-30 02:21:34 +0900

    Update CodeKit to 2.4-19051

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'codekit' do
-  version '2.4-19051'
-  sha256 '358c185cd0c124679ddacc7e941e1bfe9ed80dd3120ec5552ac64cf2ebfcd6f5'
+  version '2.5.1-19076'
+  sha256 '7e41f7a0e3b36607fb33a758d21f1f51ea3902888448facd26ddf009b4c79f70'
 
   url "https://incident57.com/codekit/files/codekit-#{version.sub(%r{.*-},'')}.zip"
   appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml',
-          :sha256 => '2bd6ea91ec93ad5667a21bf7ccf12039270e811d352bacbc55c9e74868262cc6'
+          :sha256 => '72fc08fe815cb3faf6a6fb19dfcb4f0c4f0494bbdb7b1e8da8b87772db5dd88c'
   name 'CodeKit'
   homepage 'https://incident57.com/codekit/'
   license :commercial
