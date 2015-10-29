e9b03c5cfeb2389415013c6cb95a16b7a31df0e6
Nathan Toone <nathan@toonetown.com>
2015-10-29 13:24:00 -0600

    Update Charles to 3.11.1

--- Casks/charles.rb
+++ Casks/charles.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'charles' do
-  version '3.11'
-  sha256 'b31efe7c80464a92984d7a76e8a41df0d766c4047695b083f7278a9668585592'
+  version '3.11.1'
+  sha256 'cdbacc5372851fe69613b676eaf0639dc7e7f82f321479f32efa6dcbecfa6913'
 
   url "http://www.charlesproxy.com/assets/release/#{version}/charles-proxy-#{version}.dmg"
   name 'Charles'
