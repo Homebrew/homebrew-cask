ff1a154946287db791deac4fa79d16980b03f5d8
buo <buo@users.noreply.github.com>
2015-10-30 02:34:57 +0900

    Update Shapes to 4.4.1

--- Casks/shapes.rb
+++ Casks/shapes.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'shapes' do
-  version '4.4'
-  sha256 'b32ef0bdb153a170b0cfdd477b7055baa642635f8301e6c088c0babd91f65282'
+  version '4.4.1'
+  sha256 'ed59bfc41e4dc9902d9209e832544fda42b51325d127e4dd2aea40aa1344f663'
 
   url "http://shapesapp.com/dist/Shapes_#{version}.zip"
   name 'Shapes'
   appcast 'http://shapesapp.com/appcast/shapes4.rss',
-          :sha256 => '128a50bf57ca205718efbc69076f800da79dfd6d08e4705da216e757c064fd76'
+          :sha256 => '4d2d9f479749c429d8267d540d6835591cb53f47abf22d6363a6f58e83d6d150'
   homepage 'http://shapesapp.com'
   license :commercial
 
