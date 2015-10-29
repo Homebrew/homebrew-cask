9a9a2b032f5f0180d3a2c229a7d0b632479b1838
buo <buo@users.noreply.github.com>
2015-10-30 02:39:43 +0900

    Update TG Pro to 2.8.8

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'tg-pro' do
-  version '2.8.7'
-  sha256 '32b622cec40f4cfe0cd5455dd110696b8284524aadd92c552a769c130bbc88e7'
+  version '2.8.8'
+  sha256 'dcb4221d4b72960c306e248a0be947107ab3622b0c38570684b2f12c8ef87a44'
 
   url "http://www.tunabellysoftware.com/resources/TGPro_#{version.gsub('.','_')}.zip"
   name 'TG Pro'
   appcast 'http://tunabellysoftware.com/resources/sparkle/tgpro/profileInfo.php',
-          :sha256 => 'e276cc14d86471bc7c416faefc7e8bcffe94da4458c87c71c2f14287414df5fa'
+          :sha256 => 'ae55143d14a7a75093439c723db19e8672952efff4e38de0e0682a5037c455de'
   homepage 'http://www.tunabellysoftware.com/tgpro/'
   license :commercial
 
