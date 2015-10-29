2771ef98a5c4f2b5af8322355d0706b0f0eb128b
buo <buo@users.noreply.github.com>
2015-10-30 02:37:07 +0900

    Update TeX Live Utility to 1.23

--- Casks/tex-live-utility.rb
+++ Casks/tex-live-utility.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'tex-live-utility' do
-  version '1.21'
-  sha256 '51fb396fa1bcb73af575c653415d16f422d842a6f8029f75c85ce1a6e0f92cf0'
+  version '1.23'
+  sha256 'a1641f61259268bc92a16ffeffd0260a6c93b6c70d095433c3b722b448cf9b6a'
 
   url "https://github.com/amaxwell/tlutility/releases/download/#{version}/TeX.Live.Utility.app-#{version}.tar.gz"
   appcast 'https://raw.githubusercontent.com/amaxwell/tlutility/master/appcast/tlu_appcast.xml',
-          :sha256 => '9ca9e537751be33f6757984dd7d1d28a84d680bddb53c37fc7942adcd72b0eca'
+          :sha256 => '6e65fee1213ce00e4a2b29db2a24596e12390dd4edefda060f65fc1d78007e09'
   name 'TeX Live Utility'
   homepage 'https://github.com/amaxwell/tlutility'
   license :oss
