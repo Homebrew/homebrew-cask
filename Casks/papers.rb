5ba80a6dfcbca992f74191fec371b35819ea2109
buo <buo@users.noreply.github.com>
2015-10-30 02:32:43 +0900

    Update Papers to 3.4.0-386

--- Casks/papers.rb
+++ Casks/papers.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'papers' do
-  version '3.3.3-361'
-  sha256 '0f5279a77f7fd1560b77f9ae81ecebf531906900318b45b3b2746b1c6d87fb5b'
+  version '3.4.0-386'
+  sha256 'f0e31f58b2d1085c35e12a5fd3f8e67fde619cc84f7097fe5d86d731736a1f85'
 
   url "http://appcaster.papersapp.com/apps/mac/production/download/#{version.sub(%r{^.*-},'')}/papers_#{version.delete('.').sub('-','_')}.dmg"
   appcast 'http://appcaster.papersapp.com/apps/mac/production/appcast.xml',
-          :sha256 => '0b58ec3ddcf8f5080a8d6e149e92a1c273e668bd4e04f7f865d4aaa6a347370a'
+          :sha256 => '681469fc04bc4690c6af360d9cc4d7af1c03626b9c26e1af105b2f4290114caf'
   name 'Papers'
   homepage 'http://www.papersapp.com/'
   license :commercial
