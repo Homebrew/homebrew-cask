93ec125a8601d9aa0009d8d664130a63a6ed8716
buo <buo@users.noreply.github.com>
2015-10-30 02:38:43 +0900

    Update Synalyze It! Pro to 1.13

--- Casks/synalyze-it-pro.rb
+++ Casks/synalyze-it-pro.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'synalyze-it-pro' do
-  version '1.12'
-  sha256 '9bdfdc78affdc795cd44ce57be8be501dc8ea6041a4f3c70463053c307dfd40d'
+  version '1.13'
+  sha256 '64cdee281ae9c5829e5e4cf6734088414a776d46d282b6edae6a7e08ce7290f3'
 
   # it.com is the official download host per the vendor homepage
   url "http://www.synalyze-it.com/Downloads/SynalyzeItProTA_#{version}.zip"
   name 'Synalyze It! Pro'
   appcast 'http://www.synalyze-it.com/SynalyzeItPro/appcast.xml',
-          :sha256 => '3c92a4517b3ca565f4ea21644d96041b73a8b121808ce55e0fe5b13a8c6c4f1a'
+          :sha256 => 'eb77fa31623bdba61dfa091dd8a191ae46d9cc10a5c93a0e3cd28bb151b7d887'
   homepage 'https://www.synalysis.net/'
   license :commercial
 
