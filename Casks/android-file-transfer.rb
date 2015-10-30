bbf0dd2b60051abe0ced4d19194598a9c282d162
Ondrej Galbavy <ondrej.galbavy@gmail.com>
2015-10-30 17:09:08 +0100

    Fix Android File Transfer license

--- Casks/android-file-transfer.rb
+++ Casks/android-file-transfer.rb
@@ -6,7 +6,7 @@ cask :v1 => 'android-file-transfer' do
   url 'https://dl.google.com/dl/androidjumper/mtp/current/androidfiletransfer.dmg'
   name 'Android File Transfer'
   homepage 'https://www.android.com/filetransfer/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'Android File Transfer.app'
 
