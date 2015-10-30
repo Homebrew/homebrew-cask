e11e2d1621929b4a31a2993fa4c0a49077ba7daf
Ondrej Galbavy <ondrej.galbavy@gmail.com>
2015-10-30 17:04:48 +0100

    Fix RecordIt license

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -6,7 +6,7 @@ cask :v1 => 'recordit' do
   appcast 'https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790'
   name 'RecordIt'
   homepage 'http://recordit.co/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'RecordIt.app'
 end
