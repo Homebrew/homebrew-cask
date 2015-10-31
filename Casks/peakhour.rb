ca1d434f7bbda55713d2c21be33c99574eaefce8
Steven Nikolic <steven@nindoo.de>
2015-10-31 18:08:30 +0100

    Add PeakHour.app v3.1.1

---
+++ Casks/peakhour.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'peakhour' do
+  version '3.1.1'
+  sha256 'fc26e821ba7bdb54103ba85cbc757b991ead197a67defd515d9de0c6b25e4530'
+
+  url "https://updates.peakhourapp.com/releases/PeakHour%20#{version}.zip"
+  name 'PeakHour'
+  homepage 'http://www.peakhourapp.com'
+  license :commercial
+
+  app "PeakHour #{version.to_i}.app"
+end
