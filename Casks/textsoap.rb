19897342839239367e91e70dd945e6a7377c07e5
Victor Popkov <victor@popkov.me>
2015-11-01 22:48:24 +0200

    textsoap.rb: add license

--- Casks/textsoap.rb
+++ Casks/textsoap.rb
@@ -8,7 +8,7 @@ cask :v1 => 'textsoap' do
   appcast 'https://unmarked.s3.amazonaws.com/appcast/textsoap7.xml',
           :sha256 => '37211786d3aca6f891665256f383906a337bea31958b4b7a2ff0fbbaae98709e'
   homepage 'http://www.unmarked.com/textsoap/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'textsoap7.app'
 
