405d47c31c2cc3d9b3bec52d4745a6a958c4a93f
Victor Popkov <victor@popkov.me>
2015-11-01 22:42:40 +0200

    tangerine.rb: add license

--- Casks/tangerine.rb
+++ Casks/tangerine.rb
@@ -7,7 +7,7 @@ cask :v1 => 'tangerine' do
   appcast 'http://launch.karelia.com/appcast.php?version=0&product=13&appname=Tangerine!',
           :sha256 => 'c8e3f2ce6c968bd670dd33e2be1412a3a7f7614256737c05e7f3c3e5414e2bc9'
   homepage 'https://www.karelia.com/products/tangerine/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'Tangerine!.app'
 
