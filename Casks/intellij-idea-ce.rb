27470390607c4b1bb255ea82ca9c55062610d1cc
Jochen Schalanda <jochen@schalanda.name>
2015-11-02 15:56:50 +0100

    Upgrade to IntelliJ IDEA 15.0

--- Casks/intellij-idea-ce.rb
+++ Casks/intellij-idea-ce.rb
@@ -1,18 +1,18 @@
 cask :v1 => 'intellij-idea-ce' do
-  version '14.1.5'
-  sha256 '595448c3c85e83587f9059403bad155b97477780c11843763b4646b59592f000'
+  version '15.0'
+  sha256 'e9d0f479e692463cda6ab81deda93d4145fb01968bb802a0d7be35e3e0eee73d'
 
-  url "https://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
+  url "https://download.jetbrains.com/idea/ideaIC-#{version}-custom-jdk-bundled.dmg"
   name 'IntelliJ IDEA Community Edition'
   homepage 'https://www.jetbrains.com/idea/'
   license :apache
 
-  app 'IntelliJ IDEA 14 CE.app'
+  app 'IntelliJ IDEA 15 CE.app'
 
   zap :delete => [
-                  '~/Library/Application Support/IdeaIC14',
-                  '~/Library/Preferences/IdeaIC14',
-                  '~/Library/Caches/IdeaIC14',
-                  '~/Library/Logs/IdeaIC14',
+                  '~/Library/Application Support/IdeaIC15',
+                  '~/Library/Preferences/IdeaIC15',
+                  '~/Library/Caches/IdeaIC15',
+                  '~/Library/Logs/IdeaIC15',
                  ]
 end
