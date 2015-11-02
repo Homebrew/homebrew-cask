77179f037b5167536ef315df489d07d833547471
Sean Smith <ssmith@missiondata.com>
2015-11-02 14:31:45 -0500

    Terraform: Hashicorp Changes Official Download Service to Fastly

--- Casks/terraform.rb
+++ Casks/terraform.rb
@@ -2,8 +2,8 @@ cask :v1 => 'terraform' do
   version '0.6.6'
   sha256 '43912f5d3eac34a73eaa182a78e13e8392ff4b81f053be4a61cd78db53c505a7'
 
-  # bintray.com is the official download host per the vendor homepage
-  url "https://dl.bintray.com/mitchellh/terraform/terraform_#{version}_darwin_amd64.zip"
+  # hashicorp.com is the official download host per the vendor homepage
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
   name 'Terraform'
   homepage 'https://www.terraform.io/'
   license :mpl
