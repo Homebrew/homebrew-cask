66121c291f86466d3b80b8a8aa6d0b2e67229c7a
Christoph Keller <github@christophkeller.cc>
2015-11-02 20:28:41 +0100

    Upgrade pycharm to 5.0
    Caveats section removed since this version bundles its jdk.

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,22 +1,11 @@
 cask :v1 => 'pycharm' do
-  version '4.5.4'
-  sha256 '4bc042c0726fbe4ac738dd34cc3a59d52b14404ae5d32420b736ada57737017b'
+  version '5.0'
+  sha256 '21a1e8079d000580b5016e6459a87e29c413bdb6b1100308cfe815c1346913e9'
 
-  url "https://download.jetbrains.com/python/pycharm-professional-#{version}.dmg"
+  url "https://download.jetbrains.com/python/pycharm-professional-#{version}-jdk-bundled.dmg"
   name 'PyCharm'
   homepage 'https://www.jetbrains.com/pycharm/'
   license :commercial
 
   app 'PyCharm.app'
-
-  caveats <<-EOS.undent
-    #{token} requires Java 6 like any other IntelliJ-based IDE.
-    You can install it with
-
-      brew cask install caskroom/homebrew-versions/java6
-
-    The vendor (JetBrains) doesn't support newer versions of Java (yet)
-    due to several critical issues, see details at
-    https://intellij-support.jetbrains.com/entries/27854363
-  EOS
 end
