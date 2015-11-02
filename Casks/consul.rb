6d66306dca1f79fdac1e99a9fd1556e407025f5e
Sean Smith <ssmith@missiondata.com>
2015-11-02 14:19:52 -0500

    Consul: Hashicorp Changes Official Download Service to Fastly

--- Casks/consul.rb
+++ Casks/consul.rb
@@ -3,7 +3,7 @@ cask :v1 => 'consul' do
   sha256 '87be515d7dbab760a61a359626a734f738d46ece367f68422b7dec9197d9eeea'
 
   # bintray.com is the official download host per the vendor homepage
-  url "https://dl.bintray.com/mitchellh/consul/#{version}_darwin_amd64.zip"
+  url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_darwin_amd64.zip"
   name 'Consul'
   homepage 'https://www.consul.io/'
   license :mpl
