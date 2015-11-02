f90516e5801a8fd02ccd2a470d21d717909f773c
crzrcn <fernanlink@gmail.com>
2015-11-02 06:13:23 -0800

    Fix jadengeller-helium url stanza

--- Casks/jadengeller-helium.rb
+++ Casks/jadengeller-helium.rb
@@ -2,7 +2,7 @@ cask :v1 => 'jadengeller-helium' do
   version '1.9'
   sha256 '32bede939e1dcbbfe6ea879888456fbff30f1db2c764478fc7ba08d52e2f69fb'
 
-  url "https://github.com/JadenGeller/Helium/releases/download/v#{version}/Helium.app.zip"
+  url "https://github.com/JadenGeller/Helium/releases/download/#{version}/Helium.app.zip"
   appcast 'https://github.com/JadenGeller/Helium/releases.atom'
   name 'Helium'
   homepage 'https://jadengeller.github.io/Helium/'
