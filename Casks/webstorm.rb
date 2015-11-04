ffd7927a9ff72681a983d037d2fecbd61ae9129a
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2015-11-04 10:27:19 +0100

    Download Webstorm over https

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -2,7 +2,7 @@ cask :v1 => 'webstorm' do
   version '11.0.0'
   sha256 '1a60604e7e5b131f41fd7536db0594245697134896ee4054652f0dee09a85ba9'
 
-  url "http://download-cf.jetbrains.com/webstorm/WebStorm-#{version}.dmg"
+  url "https://download.jetbrains.com/webstorm/WebStorm-#{version}-custom-jdk-bundled.dmg"
   name 'WebStorm'
   homepage 'http://www.jetbrains.com/webstorm/'
   license :commercial
