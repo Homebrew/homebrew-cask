42b792b517a2769beda52d4cad507cbdad167d9b
buo <buo@users.noreply.github.com>
2015-11-04 19:25:07 +0900

    Update Deploymate to 1.2.9

--- Casks/deploymate.rb
+++ Casks/deploymate.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'deploymate' do
-  version '1.2.8'
-  sha256 '7e990c4ce807510ccfed544b968f049c28aec6a7988d068b5412fc93466b79f6'
+  version '1.2.9'
+  sha256 '0e2be785a35b9264791ad8bf5a3580b61cff446c0f0fa498ff3e12faaf9ce10e'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/deploymate/download/Deploymate#{version.delete('.')}.zip"
