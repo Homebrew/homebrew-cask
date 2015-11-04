6ed0547eac08eb867e3bb4db7bf677f304cea24f
buo <buo@users.noreply.github.com>
2015-11-04 14:21:55 +0900

    Update Ridibooks to versioned url

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'ridibooks' do
-  version :latest
-  sha256 :no_check
+  version '1.5.9b'
+  sha256 '27e06ef0625f8958a909c7801bfd0108d975164fec9b241d1a12ab083edea2e0'
 
-  url 'http://ridibooks.com/getapp?os=mac'
+  # ridicorp.com is the official download host per the vendor homepage
+  url "http://cdn.ridicorp.com/app/mac/ridibooks-#{version}.dmg"
   name 'Ridibooks'
   homepage 'https://ridibooks.com/support/app/download'
   license :gratis
