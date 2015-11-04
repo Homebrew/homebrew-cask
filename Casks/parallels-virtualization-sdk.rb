955d1fdf75aedb5cf313d4b87e13a5e9cda14239
buo <buo@users.noreply.github.com>
2015-11-04 19:38:59 +0900

    Update Parallels Virtualization SDK to 11.0.2-31348

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'parallels-virtualization-sdk' do
-  version '11.0.0-31193'
-  sha256 '5966961fa96e2124f7f4e8454620880f3fa618f46cb9cb46fcc7d6ec55e10718'
+  version '11.0.2-31348'
+  sha256 '159c31e7ef013cb51e10cb9c904dc7409ceee597b4ede28417067a26126cb94d'
 
   url "http://download.parallels.com/desktop/v#{version[/^\w+/]}/#{version.sub(/-.*$/, '')}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name 'Parallels Virtualization SDK'
