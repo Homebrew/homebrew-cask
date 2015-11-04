4b61f0c9651b0abe7d08064cd7254248e6b34db8
Honza Dvorsky <jan.dvorsky@yahoo.com>
2015-11-04 18:38:51 +0000

    conflicts

--- Casks/qtpass.rb
+++ Casks/qtpass.rb
@@ -1,11 +1,6 @@
 cask :v1 => 'qtpass' do
-<<<<<<< HEAD
   version '1.0.4'
   sha256 'cb5eb74ba37ded2a36d3362cc85e4a3c45aad095be7fe77252eab885f7d32c86'
-=======
-  version '1.0.3'
-  sha256 '404dad56342547d274aeb746f92039050eb3a4e67ae2164d3a141fb095ff8a75'
->>>>>>> 4f2a29140a31bdc3765c01586082613de3023b6b
 
   url "https://github.com/IJHack/qtpass/releases/download/v#{version}/qtpass-#{version}.dmg"
   appcast 'https://github.com/IJHack/qtpass/releases.atom'
