ec2d4d0a06b8499f00aeef08346e842cbc7eec9e
Honza Dvorsky <jan.dvorsky@yahoo.com>
2015-11-04 18:42:02 +0000

    Update buildasaur.rb

--- Casks/buildasaur.rb
+++ Casks/buildasaur.rb
@@ -2,7 +2,8 @@ cask :v1 => 'buildasaur' do
   version '0.6.4'
   sha256 'e851d5bc9ce7fe13ad65f559617d9130040e444aeaf35191013069e96eae8fc6'
 
-  url 'https://github.com/czechboy0/Buildasaur/releases/download/v0.6.3/Buildasaur.app.zip'
+  url 'https://github.com/czechboy0/Buildasaur/releases/download/v0.6.4/Buildasaur.app.zip'
+  appcast 'https://github.com/czechboy0/Buildasaur/releases.atom'
   name 'Buildasaur'
   homepage 'https://github.com/czechboy0/Buildasaur'
   license :mit
