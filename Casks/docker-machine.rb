a1b82a20d1c1727bee340ce5db09c56a4dac2415
Sergey Novikov <novikov.su@gmail.com>
2015-11-05 12:00:28 +0500

    Fix docker-machine.rb
    Docker Machine now is an zip-archive contains bunch of files. So old cask didn't work.

--- Casks/docker-machine.rb
+++ Casks/docker-machine.rb
@@ -9,12 +9,22 @@ cask :v1_1 => 'docker-machine' do
   homepage 'https://docs.docker.com/machine'
   license :apache
 
-  container :type => :naked
-  binary 'docker-machine_darwin-amd64', :target => 'docker-machine'
-
-  postflight do
-    set_permissions "#{staged_path}/docker-machine_darwin-amd64", '0755'
-  end
+  binary 'docker-machine'
+  binary 'docker-machine-driver-amazonec2'
+  binary 'docker-machine-driver-azure'
+  binary 'docker-machine-driver-digitalocean'
+  binary 'docker-machine-driver-exoscale'
+  binary 'docker-machine-driver-generic'
+  binary 'docker-machine-driver-google'
+  binary 'docker-machine-driver-hyperv'
+  binary 'docker-machine-driver-none'
+  binary 'docker-machine-driver-openstack'
+  binary 'docker-machine-driver-rackspace'
+  binary 'docker-machine-driver-softlayer'
+  binary 'docker-machine-driver-virtualbox'
+  binary 'docker-machine-driver-vmwarefusion'
+  binary 'docker-machine-driver-vmwarevcloudair'
+  binary 'docker-machine-driver-vmwarevsphere'
 
   depends_on :formula => 'docker'
   depends_on :arch => :x86_64
