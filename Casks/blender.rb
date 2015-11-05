6e30e63d9014fb0133c06dd8ee63a6258bb923d4
Michael <fanquake@users.noreply.github.com>
2015-11-05 15:41:45 +0800

    Blender 2.76b

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'blender' do
-  version '2.76a'
-  sha256 '37b583d19eb16123065b62a7c05c574d9ebee2ff7497c1180466447ce6dab383'
+  version '2.76b'
+  sha256 '60d0e6372782578f95c34ee0003a18df20d88c4c4df47e7b5e8814553c83c39a'
 
   url "https://download.blender.org/release/Blender#{version.to_f}/blender-#{version}-OSX_10.6-x86_64.zip"
   name 'Blender'
