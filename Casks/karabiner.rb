174aaf1d21562ecbc8e34dff472f47bb6617388b
chuanxd <timmy5519@gmail.com>
2015-11-06 10:55:07 +0800

    Update Karabiner.app to 10.14.0

--- Casks/karabiner.rb
+++ Casks/karabiner.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'karabiner' do
-  version '10.13.0'
-  sha256 '0bed7dedea819bd16ffc87ee21a8a11b9d5d8b30d7c76030309b2ea0e9713392'
+  version '10.14.0'
+  sha256 '429e88ba2e1c6a4d4f40cef9e17ff0b646e54fda3e7bc02ca7f4ed5eda7ca275'
 
   url "https://pqrs.org/osx/karabiner/files/Karabiner-#{version}.dmg"
   appcast 'https://pqrs.org/osx/karabiner/files/appcast.xml',
-          :sha256 => 'c356e845f6fc89139319b3b504f5a549ce4f77ad2091a9c60514077efbbb1dd3'
+          :sha256 => 'c68771d2943486830f431e279b2b42ebc3baf18b17fc1c174efcc2b61c8d34aa'
   name 'Karabiner'
   name 'KeyRemap4MacBook'
   homepage 'https://pqrs.org/osx/karabiner/'
