b26a443107d58865ac5e6ae32c7df6b8844c9297
Julien Lavergne <lavergne.julien@gmail.com>
2015-11-06 20:00:15 +0100

    servetome-beta: added beta version

---
+++ Casks/servetome-beta.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'servetome-beta' do
+  version '4.0.0123'
+  sha256 'afde55a5a5da1e1b02df4c06a3881140ba1ecdcc7d06d7cbe6a638d0f885388b'
+
+  url "http://downloads.zqueue.com/ServeToMe-v#{version}.dmg"
+  name 'ServeToMe'
+  homepage 'http://zqueue.com/servetome/'
+  license :gratis
+
+  app 'ServeToMe.app'
+end
