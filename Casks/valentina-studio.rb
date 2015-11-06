61aa92a7188c5c23e78e1cb1f5b4b3e2ad20a767
Bruce Steedman <MatzFan@users.noreply.github.com>
2015-11-06 15:11:52 +0000

    Update valentina-studio.rb

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'valentina-studio' do
-  version '6.0.5'
-  sha256 '6d8cf45e3efa8ceb246069076d8a83f030b66097d3d37490405e7cbae0c07481'
+  version :latest
+  sha256 :no_check
 
-  url 'http://www.valentina-db.com/download/release/mac_64/vstudio_x64_6_mac.dmg'
+  url 'https://www.valentina-db.com/en/all-downloads/vstudio/current/vstudio_mac_64?format=raw'
   name 'Valentina Studio'
-  homepage 'http://www.valentina-db.com/en/valentina-studio-overview'
+  homepage 'https://www.valentina-db.com/en/valentina-studio-overview'
   license :freemium
 
   app 'Valentina Studio.app'
