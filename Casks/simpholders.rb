3e39a2bb6730604dfbb722a31e2995c736814de3
Maurice Kelly <osv-github@chatswood.org.uk>
2015-11-06 19:22:49 +0000

    Update Simpholders to version 2.0
    This commit updates the version and sha256 stanzas.

--- Casks/simpholders.rb
+++ Casks/simpholders.rb
@@ -1,13 +1,13 @@
 cask :v1 => 'simpholders' do
-  version '1.5.0'
-  sha256 '2f4fb58a20d94a858c4d53648ee37fd082e23f50ef3f367fbaef4d6cea840cd6'
+  version '2.0'
+  sha256 'f98517e2dc3eee2a564409d1bb0e59239a4221048102b67477c3f754436596bb'
 
-  url "http://simpholders.com/site/assets/files/1007/simpholders-#{version.gsub('.','_')}.dmg"
+  url "http://simpholders.com/site/assets/files/1115/simpholders_#{version.gsub('.','_')}.dmg"
   appcast 'http://kfi-apps.com/appcasts/simpholders/',
           :sha256 => 'baa9148ebfb168d1c86480da0863b89a9eeb7b70e8d8e1e5806c7f7e1a0fdec2'
   name 'SimPholders'
   homepage 'http://simpholders.com/'
-  license :gratis
+  license :commercial
 
   app 'SimPholders.app'
 end
