5396eae7547794024824daf2b8031f12b97c5813
buo <buo@users.noreply.github.com>
2015-11-06 18:52:11 +0900

    Update Bilibili to 2.18

--- Casks/bilibili.rb
+++ Casks/bilibili.rb
@@ -1,14 +1,13 @@
 cask :v1 => 'bilibili' do
-  version '2.16'
-  sha256 'ee545dbfd027d206118955a8f079a8c8a161d77878083605ed38dc0076d32055'
+  version '2.18'
+  sha256 '4827034c5c9ecea1f5cfbdb0ea7a02e8a5061925a3bb50e35f5822240b323617'
 
-  url "https://github.com/typcn/bilibili-mac-client/releases/download/#{version}/Bilibili.dmg.zip"
+  url "https://github.com/typcn/bilibili-mac-client/releases/download/#{version}/Bilibili.dmg"
   name 'Bilibili'
   appcast 'http://app.eqoe.cn/updates/bilimac.xml',
           :sha256 => '6ced15daa9b5ebe2f55e62c721063b8d0f9b45290e99f4776e1a109489c23e53'
   homepage 'https://github.com/typcn/bilibili-mac-client/'
   license :gpl
 
-  container :nested => 'Bilibili.dmg'
   app 'Bilibili.app'
 end
