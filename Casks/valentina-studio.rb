26926ce57b73d2ab9d0d17da6801951e9c8d7d86
Victor Popkov <victor@popkov.me>
2015-11-06 16:10:41 +0200

    valentina-studio.rb: update sha256 checksum

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'valentina-studio' do
   version '6.0.5'
-  sha256 'c9ae0cec456661c4b57825b55ac43f4b23c6e6f13d8e89a0bd90b2e07e41553a'
+  sha256 '6d8cf45e3efa8ceb246069076d8a83f030b66097d3d37490405e7cbae0c07481'
 
   url 'http://www.valentina-db.com/download/release/mac_64/vstudio_x64_6_mac.dmg'
   name 'Valentina Studio'
