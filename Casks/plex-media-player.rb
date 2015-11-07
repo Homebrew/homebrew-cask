21d8e8603a431a398f7dceb1594fd34171501b8a
Patryk Prus <patryk.prus@gmail.com>
2015-11-07 14:31:59 -0500

    Update plex media player to 1.0.1.42

--- Casks/plex-media-player.rb
+++ Casks/plex-media-player.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'plex-media-player' do
-  version '1.0.0.5-53192cb0'
-  sha256 '82e11dde797d36d8e68e7aa2b5b25c70a05207f200737317810b8303ca54c329'
+  version '1.0.1.42-56cc6ef9'
+  sha256 'b5eb64a5d4668252fe26d3e4dba612b2ebd60197d42d9d8782273969c22be598'
 
   url "https://downloads.plex.tv/plexmediaplayer/#{version}/PlexMediaPlayer-#{version}-macosx-x86_64.zip"
   name 'Plex Media Player'
