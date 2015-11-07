39fede5946c04d96483075cf7b673b7828d63c73
Vítor Galvão <info@vitorgalvao.com>
2015-11-07 01:50:57 +0000

    updated aseprite (1.1.1)

--- Casks/aseprite.rb
+++ Casks/aseprite.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'aseprite' do
-  version '1.1.0.1'
-  sha256 '3d3bde4c64ea95865d78618c1f6665e42e4071bf84d3cf15bbcad21c27bf4eeb'
+  version '1.1.1'
+  sha256 '197b203968acb36210d15b7069b3fcfd8a4673dbbcb0eb9606e4ca95cd2afaba'
 
   url "http://www.aseprite.org/downloads/Aseprite-v#{version}-trial-MacOSX.dmg"
   name 'Aseprite'
