f08baf80a0e5e1a8fbfa92b4c1939a517572dae2
Vítor Galvão <info@vitorgalvao.com>
2015-11-07 15:59:30 +0000

    updated cycling74-max (7.0.6_150930)

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'cycling74-max' do
-  version '7.0.5_150727'
-  sha256 '61d5d5e0a4ca4d56a1bffbc49343b5624c02f0a707bfed943e8c0eb15c91c557'
+  version '7.0.6_150930'
+  sha256 '8498d56b174e214808e8ddfa05fa02c0e5b7f51b34182ab6cfe5eb9d60ef1697'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.delete('.')}.dmg"
