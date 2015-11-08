b5accb74ecb145b7bf0b51a287cab0ace0f289ae
buo <buo@users.noreply.github.com>
2015-11-09 07:53:56 +0900

    Update Syncthing to 0.12.1

--- Casks/syncthing.rb
+++ Casks/syncthing.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'syncthing' do
-  version '0.12.0'
-  sha256 '6e475352acb7ec501b38884464ab9978433df586d09ceb9bde7f435b4985b4e5'
+  version '0.12.1'
+  sha256 '6ecbdd3d4eea35deaad55317ea7b86984c8d09cf6f0b2a2432cb0c53e44d5f14'
 
   url "https://github.com/syncthing/syncthing/releases/download/v#{version}/syncthing-macosx-amd64-v#{version}.tar.gz"
   appcast 'https://github.com/syncthing/syncthing/releases.atom'
