25817f96d808d673ad74db6e93e06816c7e3854f
Vítor Galvão <info@vitorgalvao.com>
2015-11-09 13:43:19 +0000

    updated orange (3-3.2.dev0+2fc556d)

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'orange' do
-  version '2.7.8-7887ad2'
-  sha256 '312274a81c510227696dd655cc14a1fedd7029f58b67118a97ac7526af18608d'
+  version '3-3.2.dev0+2fc556d'
+  sha256 '9811008e071590c66abb2ac65355d55e466a59de2cd392b76fd328114094cf70'
 
-  url "http://orange.biolab.si/download/files/Orange-#{version}.dmg"
+  url "http://orange.biolab.si/download/files/Orange#{version}.dmg"
   name 'Orange'
   homepage 'http://orange.biolab.si/'
   license :gpl
