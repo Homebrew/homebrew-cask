f914872a11ee95195048d9ad78dfdd7b19a86315
Amory Meltzer <Amorymeltzer@gmail.com>
2015-11-09 15:20:36 -0800

    Update mendeley-desktop to v15.1

--- Casks/mendeley-desktop.rb
+++ Casks/mendeley-desktop.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mendeley-desktop' do
-  version '1.15'
-  sha256 '57d8338649d6efcc7817be31ce69e2633810a3352c78cf95eb5cdf84dee64dae'
+  version '1.15.1'
+  sha256 '5d579da0c478b4a02949abcb1a0028f64d10c4810040f5c5d342325fd665caba'
 
   url "http://desktop-download.mendeley.com/download/Mendeley-Desktop-#{version}-OSX-Universal.dmg"
   name 'Mendeley'
