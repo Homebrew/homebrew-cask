9d0193bee0613eae943c1cbc7e585e9f87b94690
Tony Rieker <tony@plenary.org>
2015-11-10 14:12:26 -0800

    Update pl2303 to support El Capitan

--- Casks/pl2303.rb
+++ Casks/pl2303.rb
@@ -1,17 +1,15 @@
 cask :v1 => 'pl2303' do
-  version '1.5.1'
-  sha256 '101058b71da6a6bd038b05abef0e556bd3203fcd04f35d545fd711c5d34d46c2'
+  version '1.6.0_20151022'
+  sha256 'ed0d7965c57e6d855e1909e0af7228372bedb0ae47247a00c5eea519b5dde6bb'
 
-  url "http://prolificusa.com/files/md_PL2303_MacOSX_10.6-10.10_v#{version}.zip"
+  url "http://www.prolific.com.tw/UserFiles/files/PL2303_MacOSX_#{version.gsub('.','_')}.zip"
   name 'Prolific USB-Serial Cable driver'
-  homepage 'http://www.prolificusa.com'
+  homepage 'http://www.prolific.com.tw'
   license :closed
 
   pkg "PL2303_MacOSX_v#{version}.pkg"
 
-  depends_on :macos => [:snow_leopard, :lion, :mountain_lion, :mavericks, :yosemite]
-
-  uninstall :pkgutil => "com.prolific.prolificUsbserialCableDriverV#{version.delete('.')}.ProlificUsbSerial.pkg",
+  uninstall :pkgutil => "com.prolific.prolificUsbserialCableDriverV#{version.gsub('.','_')}.ProlificUsbSerial.pkg",
             :kext => 'com.prolific.driver.PL2303',
             :delete => [
                         '/System/Library/Extensions/ProlificUsbSerial.kext',
