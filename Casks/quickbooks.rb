9ec509d6704c24694fb1babb72ba8ccdfaf05b75
Victor Popkov <victor@popkov.me>
2015-11-10 23:56:30 +0200

    quickbooks.rb: upgrade to v2.2.0

--- Casks/quickbooks.rb
+++ Casks/quickbooks.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'quickbooks' do
-  version '2.1.0'
-  sha256 'a95cc6e008ac63af2a055cfc10ea99e19ec2186f04e1ee503c77456314226746'
+  version '2.2.0'
+  sha256 '0fb97f313e33cbb8e4ee7158d53bf9316868ef92a12cb98c62971fe74f8842fd'
 
   url "https://http-download.intuit.com/http.intuit/CMO/apple_web/sample/static/qbomac/releases/QuickBooks_#{version}/QuickBooks.QBOWeb.dmg"
   appcast 'https://http-download.intuit.com/http.intuit/CMO/apple_web/sample/static/qbomac/rssfeeds/Staged/QuickBooks.QBOWeb.Release.xml',
-          :sha256 => '92a13f415527e55040b29e7c2a168bac262b503d0cc2fb78c2eae3a9a4e0eb47'
+          :sha256 => 'c24ad4d5c7a3a8ca4e5922d652b2d4a83eb42cc3d1ef8ff5e6c9dfd4668af10d'
   name 'QuickBooks Mac App'
   name 'QuickBooks'
   homepage 'https://qbo.intuit.com/'
