6f879841240958e40f98bb9b57417648351ac1a7
Victor Popkov <victor@popkov.me>
2015-11-11 22:40:00 +0200

    wasted.rb: add license

--- Casks/wasted.rb
+++ Casks/wasted.rb
@@ -6,7 +6,7 @@ cask :v1 => 'wasted' do
   name 'WASTED'
   appcast 'http://wasted.werk01.de/appcast.xml'
   homepage 'http://wasted.werk01.de'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :mit
 
   app 'Wasted.app'
 end
