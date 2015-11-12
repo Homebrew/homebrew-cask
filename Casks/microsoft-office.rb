afa989679e95aa8e36763e994c8fb6621b8e587d
adityadalal924 <github@adityadalal.com>
2015-11-12 13:42:13 -0500

    updated microsoft office to 2016

--- Casks/microsoft-office.rb
+++ Casks/microsoft-office.rb
@@ -2,52 +2,27 @@ cask :v1 => 'microsoft-office' do
   version :latest
   sha256 :no_check
 
-  url 'http://officecdn.microsoft.com/pr/MacOffice2011/en-US/MicrosoftOffice2011.dmg'
-  name 'Microsoft Office'
+  url 'http://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/Microsoft_Office_2016_Installer.pkg'
+  name 'Microsoft Office 2016'
   homepage 'https://www.microsoft.com/mac'
   license :commercial
 
-  pkg 'Office Installer.pkg'
+  pkg 'Microsoft_Office_2016_Installer.pkg'
 
-  uninstall :pkgutil   => 'com.microsoft.office.*',
-            :launchctl => 'com.microsoft.office.licensing.helper'
-  zap       :pkgutil   => [
-                           'com.microsoft.mau.all.autoupdate.*',
-                           'com.microsoft.merp.all.errorreporting.*'
+  uninstall :pkgutil   => [
+                           'com.microsoft.package.*',
+                           'com.microsoft.pkg.licensing'
                           ],
-            :delete    => [
-                           '/Library/LaunchDaemons/com.microsoft.office.licensing.helper.plist',
-                           '/Library/PrivilegedHelperTools/com.microsoft.office.licensing.helper',
-                           '/Library/Application Support/Microsoft/MAU2.0',
-                           '/Library/Application Support/Microsoft/MERP2.0',
-                           '/Library/Preferences/com.microsoft.Excel.plist',
-                           '/Library/Preferences/com.microsoft.Outlook.plist',
-                           '/Library/Preferences/com.microsoft.PlayReady.plist',
-                           '/Library/Preferences/com.microsoft.Powerpoint.plist',
-                           '/Library/Preferences/com.microsoft.Word.plist',
-                           '/Library/Preferences/com.microsoft.office.licensing.plist',
-                           '/Library/Preferences/com.microsoft.outlook.databasedaemon.plist',
-                           '/Library/Preferences/com.microsoft.outlook.officereminders.plist',
-                           '~/Library/Application Support/Microsoft/Office',
-                           '~/Library/Preferences/com.microsoft.Excel.plist',
-                           '~/Library/Preferences/com.microsoft.Outlook.plist',
-                           '~/Library/Preferences/com.microsoft.Powerpoint.plist',
-                           '~/Library/Preferences/com.microsoft.Word.plist',
-                           '~/Library/Preferences/com.microsoft.autoupdate2.plist',
-                           '~/Library/Preferences/com.microsoft.error_reporting.plist',
-                           '~/Library/Preferences/com.microsoft.office.plist',
-                           '~/Library/Preferences/com.microsoft.office.setupassistant.plist',
-                           '~/Library/Preferences/com.microsoft.outlook.databasedaemon.plist',
-                           '~/Library/Preferences/com.microsoft.outlook.office_reminders.plist',
-                           '~/Library/Preferences/com.microsoft.outlook.officereminders.plist',
-                           '~/Documents/Microsoft User Data/Microsoft',
-                           '~/Documents/Microsoft User Data/Office 2011 Identities',
-                           '~/Documents/Microsoft User Data/Outlook Sound Sets',
-                           '~/Documents/Microsoft User Data/Saved Attachments'
+            :launchctl => 'com.microsoft.office.licensingV2.helper.plist'
+
+  zap       :delete    => [
+                           '/Library/LaunchDaemons/com.microsoft.office.licensingV2.helper.plist',
+                           '/Library/PrivilegedHelperTools/com.microsoft.office.licensingV2.helper',
+                           '/Library/Preferences/com.microsoft.office.licensingV2.plist',
+                           '~/Library/Preferences/com.microsoft.autoupdate2.plist'
                           ],
             :rmdir     => [
-                           '/Library/Application Support/Microsoft',
-                           '~/Library/Application Support/Microsoft',
-                           '~/Documents/Microsoft User Data'
+                           '~/Library/Containers/com.microsoft.Office365ServiceV2',
+                           '~/Library/Group Containers/UBF8T346G9.*'
                           ]
 end
