f92a0098c28494415c774e4536006a33c7b5bb9d
Steven Hsu <admin@stevenhsu.ca>
2015-11-12 13:51:53 -0800

    Fixed URL to use #{version}

--- Casks/stackato.rb
+++ Casks/stackato.rb
@@ -2,7 +2,7 @@ cask :v1 => 'stackato' do
   version '3.2.3'
   sha256 '000d43816fc9b65616c234c6fc01c02ff626fa2f4bc6d57dc5c84b433cc22c50'
 
-  url "https://downloads.stackato.com/client/v3.2.3/stackato-3.2.3-macosx10.5-i386-x86_64.zip"
+  url "https://downloads.stackato.com/client/v#{version}/stackato-#{version}-macosx10.5-i386-x86_64.zip"
   name 'Stackato'
   homepage 'https://docs.stackato.com/user/client/'
   license :apache
