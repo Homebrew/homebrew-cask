644c775e24b688c6456e14ef304aa2fd0d258b66
Michael Ford <fanquake@users.noreply.github.com>
2015-11-13 12:39:36 +0800

    PyCharm 5.0.1

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pycharm' do
-  version '5.0'
-  sha256 '21a1e8079d000580b5016e6459a87e29c413bdb6b1100308cfe815c1346913e9'
+  version '5.0.1'
+  sha256 'e0c06f0b3a60334ea85ad8ed2d6845b3f12967a1ae75a9c4596a9325287b9439'
 
   url "https://download.jetbrains.com/python/pycharm-professional-#{version}-jdk-bundled.dmg"
   name 'PyCharm'
