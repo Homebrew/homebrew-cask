1b1d65a660aec2d0c209872f703d52f05440e789
Sébastien Gross <sebchezwamorg>
2015-11-13 16:54:37 +0100

    Remove iMazing configuration files.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -13,4 +13,13 @@ cask :v1 => 'imazing' do
   app 'iMazing.app'
 
   depends_on :macos => '>= :lion'
+
+  zap :delete => [
+        '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.digidna.imazingmac.sfl',
+        '~/Library/Application Support/iMazing',
+        '~/Library/Caches/com.DigiDNA.iMazingMac',
+        '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.DigiDNA.iMazingMac',
+        '~/Library/Caches/iMazing',
+        '~/Library/Preferences/com.DigiDNA.iMazingMac.plist'
+      ]
 end
