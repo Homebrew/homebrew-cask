b1b1b6fbff011f2fb1b724e174a55ed864509482
buo <buo@users.noreply.github.com>
2015-11-14 07:58:15 +0900

    Update IUEditor to 1.1.1.6

--- Casks/iueditor.rb
+++ Casks/iueditor.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'iueditor' do
-  version '1.1.0.16'
-  sha256 '0e904d4ae187d785cd33b0ff9a07aa810ee4ae5ff48dc36b61bef31c87ca7e5c'
+  version '1.1.1.6'
+  sha256 'f644d8e8a4f0aab420cf76ce724fb8e680e4f592de1bdad54399f938342fa641'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://iueditor.s3.amazonaws.com/Release/IUEditorV#{version}.zip"
