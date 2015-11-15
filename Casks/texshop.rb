4475b0aeb287bf69773588f47c9687df2444e7f7
adityadalal924 <github@adityadalal.com>
2015-11-15 12:29:59 -0500

    updated texshop (3.58)

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'texshop' do
-  version :latest
-  sha256 :no_check
+  version '3.58'
+  sha256 '182d257d00344c4e03523668f5c94cb23e2a32791efa7e46d5c1bf908a11d423'
 
-  url 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshop.zip'
+  url "http://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.sub('.','')}.zip"
   appcast 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml',
-          :sha256 => 'c40ae2cd48a38ab90fb5e3e9ec92e8769be03e13dfc2a6246c63ab8f94f51b10'
+          :sha256 => '9fa66294d1e61895b71299377546698427ef6e91a3cb1732c3544095850334ca'
   name 'TeXShop'
   homepage 'http://pages.uoregon.edu/koch/texshop'
   license :gpl
