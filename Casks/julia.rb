e46fb5ee9c58d62b7ea6c91eaf44a5ede4603cd5
Jens Schleede <jens.schleede@gmail.com>
2015-11-16 10:59:33 +0100

    Upgrade Julia to v0.4.1

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'julia' do
-  version '0.4.0'
-  sha256 '39a2765c7a0e03d798d3cfcce2ca265a1e612a9d8108a87315b040b14f2debff'
+  version '0.4.1'
+  sha256 'bbfbcb5abbe8fd2bbe3a61ae4db1651fb43b6d017316ff70b75a2fd5aa5db9bd'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/julialang/bin/osx/x64/#{version.sub(/\.\d+$/, '')}/julia-#{version}-osx10.7+.dmg"
