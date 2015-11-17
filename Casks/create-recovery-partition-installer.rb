aa9c657aada1cdd9e9e30ca326219a6da12dad88
Timothy Sutton <tim@synthist.net>
2015-11-17 10:28:00 -0500

    Add create-recovery-partition-installer appcast

--- Casks/create-recovery-partition-installer.rb
+++ Casks/create-recovery-partition-installer.rb
@@ -3,6 +3,7 @@ cask :v1 => 'create-recovery-partition-installer' do
   sha256 '19dfb873cfa574bc0692e58b0c96554e8f59c8406bb720889da490a548400882'
 
   url "https://github.com/MagerValp/Create-Recovery-Partition-Installer/releases/download/v#{version}/Create.Recovery.Partition.Installer-#{version}.dmg"
+  appcast 'https://github.com/MagerValp/Create-Recovery-Partition-Installer/releases.atom'
   name 'Create Recovery Partition Installer'
   homepage 'https://github.com/MagerValp/Create-Recovery-Partition-Installer/'
   license :unknown
