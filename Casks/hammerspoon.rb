5fb28c245c337040167123a6dadb8dfab3408121
buo <buo@users.noreply.github.com>
2015-11-18 04:41:54 +0900

    Update Hammerspoon to 0.9.43

--- Casks/hammerspoon.rb
+++ Casks/hammerspoon.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'hammerspoon' do
-  version '0.9.42'
-  sha256 '6b96a76c4c1927ef27bab9cf76c27de7da1f0a4b3c269eab79cebd82e06970bd'
+  version '0.9.43'
+  sha256 'c0ae3646f944c3f0f38561dfd54a4d9cdfdc512c3e980c2ea3053d2b732c5f15'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/Hammerspoon/hammerspoon/releases/download/#{version}/Hammerspoon-#{version}.zip"
