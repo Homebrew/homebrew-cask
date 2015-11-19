8ef24c0ae79696fc078bd7eaefb3034ca3d5d947
Sam Doran <github@samdoran.com>
2015-11-19 11:39:51 -0500

    Update download URL for Box Sync

--- Casks/box-sync.rb
+++ Casks/box-sync.rb
@@ -2,7 +2,8 @@ cask :v1 => 'box-sync' do
   version :latest
   sha256 :no_check
 
-  url 'https://box.com/sync4mac'
+  # boxcdn.net is the official download host per the vendor homepage
+  url 'https://e3.boxcdn.net/box-installers/sync/Sync+4+External/Box%20Sync%20Installer.dmg'
   name 'Box Sync'
   homepage 'https://sites.box.com/sync4/'
   license :gratis
