60efa4fcd2807b118f4ee70670512b9bd70cbefe
buo <buo@users.noreply.github.com>
2015-11-20 23:55:39 +0900

    Update AirDroid to 3.2.1

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'airdroid' do
-  version '3.2.0'
-  sha256 'd114417d7b27498fdc4100bd7dd46722cf604683c98538ccf06cb50611a61821'
+  version '3.2.1'
+  sha256 '6384f670d374f40e0559ad962bf7992ec310f974bdab469ed021bae69e932a59'
 
   url "http://dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
   name 'AirDroid'
