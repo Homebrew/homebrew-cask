adf2324176b6c9f02e5f45b7747c58dcb8e17d97
Graham Berks <graham@fatlazycat.com>
2015-11-21 09:02:30 +0000

    bump to version 0.1.8.0 of haskell stack

--- Casks/stack.rb
+++ Casks/stack.rb
@@ -1,6 +1,6 @@
 cask :v1_1 => 'stack' do
-  version '0.1.6.0'
-  sha256 '608ef6cf9c7b37c0d964deaf071558c8381fc4c3564a5f3f6685da3f1be8455f'
+  version '0.1.8.0'
+  sha256 '16e574595046a5cf2ecc395506c800f1eb194587173c74e374732d776c060f57'
 
   url "https://github.com/commercialhaskell/stack/releases/download/v#{version}/stack-#{version}-osx-x86_64.tar.gz"
   appcast 'https://github.com/commercialhaskell/stack/releases.atom'
