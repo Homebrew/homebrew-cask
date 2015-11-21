a10f638f06bd3e8b4e9746975a69c06adfc7e2a6
Ahmed El Gabri <ahmed@gabri.me>
2015-11-21 15:55:37 +0100

    Update Chrome devtools to 1.1.0

--- Casks/chrome-devtools.rb
+++ Casks/chrome-devtools.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'chrome-devtools' do
-  version :latest
-  sha256 :no_check
+  version '1.1.0'
+  sha256 'decb98cf06ed9dd65301449347e788dd757315460cf3c77ad91ceb3ef503831a'
 
-  url 'https://github.com/auchenberg/chrome-devtools-app/raw/master/build/Chrome%20DevTools/osx/Chrome-DevTools.app.zip'
+  url "https://github.com/auchenberg/chrome-devtools-app/releases/download/v#{version}/chrome-devtools-app_#{version}.dmg"
+  appcast 'https://github.com/auchenberg/chrome-devtools-app/releases.atom'
   name 'Chrome DevTools'
   homepage 'https://github.com/auchenberg/chrome-devtools-app'
   license :mit
