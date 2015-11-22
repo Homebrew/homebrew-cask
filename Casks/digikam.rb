bcdb31e225c99f544c28618be124d50be06b7afc
Rafal <kenorb@gmail.com>
2015-11-22 11:45:48 +0000

    Updating sha256 as per `cat digikam-4.13.0-Yosemite.pkg | openssl dgst -sha256` [GH: #15350]

--- Casks/digikam.rb
+++ Casks/digikam.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'digikam' do
   version '4.13.0'
-  sha256 'd06c37484bf80b1fe8e55302951606588c9f731277f62931698d324f15ae40aa'
+  sha256 'badc0d7f4f3c7092f0722cfcd546cea277e11ac97f948e4e1a23e749f3c400a0'
 
   url "http://download.kde.org/stable/digikam/digikam-#{version}-Yosemite.pkg"
   name 'digiKam'
