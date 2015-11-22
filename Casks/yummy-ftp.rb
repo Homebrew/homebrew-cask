d521eb5f6d0cfa08eac3ca91e58952d65230bd60
Victor Popkov <victor@popkov.me>
2015-11-22 23:50:18 +0200

    yummy-ftp.rb: add license

--- Casks/yummy-ftp.rb
+++ Casks/yummy-ftp.rb
@@ -7,7 +7,7 @@ cask :v1 => 'yummy-ftp' do
   appcast 'http://www.yummysoftware.com/su/yummyftp/feed.xml',
           :sha256 => 'fac71ab836dffdc1ae5b0b4acb03d63d2d93fb2639dd38d57b32d691a08d1ea7'
   homepage 'http://www.yummysoftware.com'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
   tags :vendor => 'Yummy Software'
 
   app 'Yummy FTP.app'
