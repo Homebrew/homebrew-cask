17f31380e74ac885cbc95990640c16279bbfb65b
Daniel Hopper <daniel.hopper@cdk.com>
2015-11-23 11:11:25 -0500

    Updating nomad to 0.2.0

--- Casks/nomad.rb
+++ Casks/nomad.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'nomad' do
-  version '0.1.2'
-  sha256 '8fcff1029c6cc9bf24ee441d749ef1039a6132e62ef486561a8a8d9e6e519fbf'
+  version '0.2.0'
+  sha256 '1606cc268298852b0e413d3799843597a9aa0c47c53313ef2a523cc595ccb2f1'
 
   # hashicorp.com is the official download host per the vendor homepage
   url "https://releases.hashicorp.com/nomad/#{version}/nomad_#{version}_darwin_amd64.zip"
