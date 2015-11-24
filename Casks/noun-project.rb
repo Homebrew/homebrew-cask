d73d055eb4112fd07fb80e3422a1dc257abf6e70
Victor Popkov <victor@popkov.me>
2015-11-24 23:51:23 +0200

    noun-project.rb: upgrade to v1.1.1

--- Casks/noun-project.rb
+++ Casks/noun-project.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'noun-project' do
-  version '1.1'
-  sha256 '7129db91ce7801d131241bf5fa8e918d4a2820fbbf709cb567054d9aca5f231e'
+  version '1.1.1'
+  sha256 '2c2dde80173af2d739688e47e74562b751bc138fffef68ae765e19597fd0ae0d'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/nounproject/mac/Noun-Project-#{version}.dmg"
   name 'Noun Project'
   appcast 'https://thenounproject.com/for-mac/feed/',
-          :sha256 => '6e6d98cbd80300c45eafafe88c4320e9838d07023beb2b0d6cc9f1c3bc08e045'
+          :sha256 => 'd5805a76644fde728873403ae7df2f16049908904db69b7af2e9650773a55cba'
   homepage 'https://thenounproject.com'
   license :commercial
 
