508ff68d5f96f7b1de8aa390c0e965476b9daa95
kazuminn <e145702@ie.u-ryukyu.ac.jp>
2015-11-24 21:29:40 +0900

    Update card.rb

--- card.rb
+++ card.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'card' do
-  version '1.2'
-  sha256 '5bcdbfe44adb3c4f77229a2c7fd7e0534d6fe8200773de0d6ee8eba69a76147f'
-
+  version :latest
+  sha256 :no_check
+  
   url 'https://github.com/Code-Hex/Card/blob/master/Card/Card.dmg?raw=true'
   name 'Card'
   homepage 'https://github.com/Code-Hex/Card'
