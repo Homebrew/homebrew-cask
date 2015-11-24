e8d3bafcfdcfdfed92da5554d6b5e563f3ebc0b5
Eduardo Russo <russoedu@gmail.com>
2015-11-24 12:39:45 -0200

    Add Trello.App
    Trello.app is a Trello web app created with Epichrome.

---
+++ Casks/trello.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'trello' do
+  version '1.0'
+  sha256 '9dcf1cbe7dfac72d4fa73e441e7c152888c840edf50179ff36f6325d9e493f4a'
+
+  url 'http://cl.ly/dtuB/Trello.dmg'
+  name 'Trello'
+  homepage 'http://trello.com'
+  license :gratis
+
+  app 'Trello.app'
+end
