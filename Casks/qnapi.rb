1451191772183a8bd3c30a64dad16b448f13e453
Victor Popkov <victor@popkov.me>
2015-11-24 23:53:11 +0200

    qnapi.rb: upgrade to v0.2.0

--- Casks/qnapi.rb
+++ Casks/qnapi.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qnapi' do
-  version '0.1.9'
-  sha256 '0c8c1ef1ca74af0abc37d5af6b31374e4aad93e16261c488bb6e581b2946606a'
+  version '0.2.0'
+  sha256 'a6eb52a4f70c6c22816a442741a7d8c44c62d793461596876e31c72453048d81'
 
   url "https://github.com/QNapi/qnapi/releases/download/#{version}/QNapi-#{version}.dmg"
   appcast 'https://github.com/QNapi/qnapi/releases.atom'
