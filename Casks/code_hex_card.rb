e384ff5960e2886606be862713cb6f8a4131b4d2
kazuminn <warugaki_kazumi@docomo.ne.jp>
2015-11-24 20:23:38 +0900

    add code_hex_card.rb

---
+++ Casks/code_hex_card.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'code_hex_card' do
+  version '1.2'
+  sha256 '5bcdbfe44adb3c4f77229a2c7fd7e0534d6fe8200773de0d6ee8eba69a76147f'
+
+  url 'https://github.com/Code-Hex/Card/blob/master/Card/Card.dmg?raw=true'
+  name 'Card'
+  homepage 'https://github.com/Code-Hex/Card'
+  license :other
+
+  app 'Card.app'
+end
