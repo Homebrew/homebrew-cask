8da89bcea4fcf49fe1348facdb52ddbf4d534023
kazuminn <e145702@ie.u-ryukyu.ac.jp>
2015-11-25 09:08:30 +0900

    Update card.rb

--- card.rb
+++ card.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'card' do
   version :latest
   sha256 :no_check
-  
-  url 'https://github.com/Code-Hex/Card/blob/master/Card/Card.dmg?raw=true'
+
+　url 'https://github.com/Code-Hex/Card/blob/master/Card/Card.dmg?raw=true'
   name 'Card'
   homepage 'https://github.com/Code-Hex/Card'
   license :gpl
