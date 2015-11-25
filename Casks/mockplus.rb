47136e6b4aef71efa438ce9cfc5a4c24f348de74
wujingke <hi.laow@gmail.com>
2015-11-25 09:39:45 +0800

    Update mockplus.rb

--- Casks/mockplus.rb
+++ Casks/mockplus.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'mockplus' do
-  version '2.0.9.8'
-  sha256 'e21fcab3a9c8a1c60f88cb5d2dfc2000ca7f443a665ab3614972f796d1478fc2'
+  version '2.0.9.9'
+  sha256 '288eb26d15f972d0caae0903d3d7025c577ded1e8e5957e9feffd72ce3989d44'
 
   url "http://cdn.mockplus.cn/software/osx/Mockplus_v#{version}.dmg"
   name 'Mockplus'
