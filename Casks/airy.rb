455e3e867b6682c48f70e25e07e24c78db89f7c6
Victor Popkov <victor@popkov.me>
2015-11-27 23:58:55 +0200

    airy.rb: upgrade to v2.2.100

--- Casks/airy.rb
+++ Casks/airy.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'airy' do
-  version '2.2.98'
-  sha256 'abfa6b5dd1998fac5709873697e22abc732fa7816868ec8024512ecbf47c840c'
+  version '2.2.100'
+  sha256 '5e0895a336c0eab4c15b422b5e0034a5d31ab90d1d4d63bdf8cd99ebc59f4868'
 
   url "http://www.eltima.com/download/airy-update/airy_#{version}.dmg"
   appcast 'http://mac.eltima.com/download/airy-update/airy.xml',
-          :sha256 => '7cf4ae5c7282eb89662dcfc7a40449e88fad97e191669f26f3dfe2e4e5eb9644'
+          :sha256 => '98f576a9852e419e0fd84bfac51a059345f3b34e190311c8f9f681e5e57d9da3'
   name 'Airy'
   homepage 'http://mac.eltima.com/youtube-downloader-mac.html'
   license :commercial
