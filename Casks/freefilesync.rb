82672b355f453a7cee04ca57c324a8c1c55394f1
Markus Reiter <me@reitermark.us>
2015-11-28 22:24:06 +0100

    FreeFileSync 7.6: fix URL

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,8 +1,10 @@
 cask :v1 => 'freefilesync' do
   version '7.6'
-  sha256 'e75e292d9464daf53c2051c87f682d8bf83685f0a6532a16fa8253b2c69bb266'
+  sha256 '8785195124b1c1f33fd91513ab9cdef098ff9f9fafe0880eca187803838f528a'
 
-  url "http://www.freefilesync.org/download/m9fd99e69p2a6j6/FreeFileSync_#{version}_Mac_OS_X_64-bit.zip", :referer => 'http://www.freefilesync.org/download.php'
+  url "http://www.freefilesync.org/download/p4blb1h5my6tw5f/FreeFileSync_#{version}_Mac_OS_X_64-bit.zip",
+      :referer => 'http://www.freefilesync.org/download.php'
+
   name 'FreeFileSync'
   homepage 'http://www.freefilesync.org'
   license :gpl
