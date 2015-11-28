06f7d18b798e267ca391d84efe300e5e0203ca09
JaHIY <jaklsy@gmail.com>
2015-11-28 22:56:15 +0800

    qlmarkdown upgrade to 1.3.4

--- Casks/qlmarkdown.rb
+++ Casks/qlmarkdown.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'qlmarkdown' do
-  version '1.3.3'
-  sha256 '045712562665673924397bbbef1ee1157b44e23c9744feda6feda27e107802d3'
+  version '1.3.4'
+  sha256 'a290edf5b6124cbd4e526217e0979a9011c8ef3b964a33458f5063d51a9b15f2'
 
   url "https://github.com/toland/qlmarkdown/releases/download/v#{version}/QLMarkdown.qlgenerator.zip"
   appcast 'https://github.com/toland/qlmarkdown/releases.atom'
