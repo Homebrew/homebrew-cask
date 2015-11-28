265ec83b000337f6e88a2982bf414d56421e7330
miccal <miccal.matthews@gmail.com>
2015-11-28 20:58:07 +0800

    Update Ipe.rb

--- Casks/Ipe.rb
+++ Casks/Ipe.rb
@@ -2,10 +2,11 @@ cask :v1 => 'ipe' do
   version '7.1.10'
   sha256 '5e14f13d213905af34561a0d169e9f1f9eed91ae8f85f163df951af2c2392198'
 
-  url 'https://dl.bintray.com/otfried/generic/ipe/7.1/ipe-7.1.10-mac.dmg'
+  # bintray.com is the official download host per the vendor homepage
+  url "https://dl.bintray.com/otfried/generic/ipe/#{version.to_f}/ipe-#{version}-mac.dmg"
   name 'Ipe'
   homepage 'http://ipe.otfried.org'
-  license :oss    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :oss
 
   app 'Ipe.app'
 end
