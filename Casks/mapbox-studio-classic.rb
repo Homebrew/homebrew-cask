652ade8b49e990fe24071e004ebbe04b0d6deb74
Corey Edwards <corey@cedwardsmedia.com>
2015-11-27 19:38:31 -0600

    Renamed Mapbox Studio to Mapbox Studio Classic
    Vendor now refers to the desktop version as Mapbox Studio Classic
    and maintains "Mapbox Studio" for the new web-based version of the
    app. Thus, I have updated the Cask to reflect this decision.

--- Casks/mapbox-studio-classic.rb
+++ Casks/mapbox-studio-classic.rb
@@ -1,12 +1,13 @@
-cask :v1 => 'mapbox-studio' do
+cask :v1 => 'mapbox-studio-classic' do
   version '0.3.3'
   sha256 'e88ec6986fd0b3c05923cf0e647863137cf28a4246f8822949d934c873f050cb'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://mapbox.s3.amazonaws.com/mapbox-studio/mapbox-studio-darwin-x64-v#{version}.zip"
   name 'Mapbox Studio'
-  homepage 'https://www.mapbox.com/mapbox-studio/'
+  homepage 'https://www.mapbox.com/mapbox-studio-classic/'
   license :bsd
 
+  # App name is still Mapbox Studio.app, per vendor, despite "classic" designation
   app "mapbox-studio-darwin-x64-v#{version}/Mapbox Studio.app"
 end
