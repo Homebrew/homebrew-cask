22969d3b5cae7eaf43cddabadaf13ec0b4f72d1e
Victor Popkov <victor@popkov.me>
2015-11-29 23:48:55 +0200

    praat.rb: upgrade to v6.0.06

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'praat' do
-  version '6.0.05'
+  version '6.0.06'
 
   if Hardware::CPU.is_32_bit? || MacOS.release <= :snow_leopard
-    sha256 'f243824c5122f3520989331ae710535728355f23fb6625423a7bf5fdce570ac1'
+    sha256 'f82242cc2fbc07ae5b9218e7707f842f0a428b0db019db04d0a0936dcb4c0298'
     url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.delete('.')}_mac32.dmg"
   else
-    sha256 'a9f5b0bc7192b4f8d9af19c3c788c5ebf9c81d0b63651b410bf520a3e5562753'
+    sha256 '8a5e2cbc79b5ef999231f0f108f864e3fdacf020942e166a930c40353eff1b06'
     url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.delete('.')}_mac64.dmg"
   end
 
