4cbe3070e2a87fcd28300d4061d78f88035a66d6
Samuel Dillow <tarwich@gmail.com>
2015-11-29 20:03:32 -0600

    Updated qlmarkdown to version 1.3.5

--- Casks/qlmarkdown.rb
+++ Casks/qlmarkdown.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'qlmarkdown' do
-  version '1.3.4'
+  version '1.3.5'
   sha256 'a290edf5b6124cbd4e526217e0979a9011c8ef3b964a33458f5063d51a9b15f2'
 
   url "https://github.com/toland/qlmarkdown/releases/download/v#{version}/QLMarkdown.qlgenerator.zip"
