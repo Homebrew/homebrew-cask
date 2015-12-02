54d0c5cb7e91d2324fedee61f3082efd1fa77b6c
Cuong Tran <ctran@users.noreply.github.com>
2015-12-01 22:08:16 -0800

    Update visual-studio-code to v0.10.3
    The 0.10.3 release includes a fix for a severe startup performance regression introduced in v0.10.1 (https://code.visualstudio.com/updates)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'visual-studio-code' do
-  version '0.10.2'
-  sha256 '3371bff1e11daa49ad77ad1eed683d1baaf073477c1baff20626250d5ee6153e'
+  version '0.10.3'
+  sha256 '90e4d35f176d045e12647c603a23eb270d47ede91b5fa933f26bb800ccf4ad04'
 
   # vo.msecnd.net is the official download host per the vendor homepage
   url "https://az764295.vo.msecnd.net/public/#{version}/VSCode-darwin.zip"
