6e6727e9f9d4bf0cb367fe6f33ef2a3e96b9fc3b
Francis T. O'Donovan <francis.odonovan@bostonfusion.com>
2015-12-03 21:45:27 -0500

    Update Casks to avoid hardcoding app version
    Fix #15564 (https://github.com/caskroom/homebrew-cask/pull/15564)
    changed CONTRIBUTING.md to emphasize the use of versioned
    URLs. Applied this recommended style to existing Casks.

--- Casks/techstoreclub-simple-comic.rb
+++ Casks/techstoreclub-simple-comic.rb
@@ -2,7 +2,7 @@ cask :v1 => 'techstoreclub-simple-comic' do
   version '1.7_252'
   sha256 '4ddd18a02a79fc8201824e6ab99291c6d4c8680f79f94bc372bf71f0535def35'
 
-  url 'https://github.com/techstoreclub/Simple-Comic/releases/download/1.7_252/SimpleComic_1.7_252.zip'
+  url "https://github.com/techstoreclub/Simple-Comic/releases/download/#{version}/SimpleComic_#{version}.zip"
   name 'Simple Comic'
   appcast 'http://www.dancingtortoise.com/simplecomic/simplecomic.xml',
           :sha256 => 'a60ac75aa2656f347cb8554c411d44dcc6500dd74e666aee5da1e54fb5928bf3'
--- Casks/witch.rb
+++ Casks/witch.rb
@@ -2,7 +2,7 @@ cask :v1 => 'witch' do
   if MacOS.release == :snow_leopard
     version '3.9.1'
     sha256 '5e46508e150ff16be14b9955abdcd15098376230ef71e3de6f15a056eec75e45'
-    url 'http://manytricks.com/download/witch/3.9.1'
+    url "http://manytricks.com/download/witch/#{version}"
   else
     version :latest
     sha256 :no_check
