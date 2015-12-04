4d6935b40851139a7b6a097b57362ed00156f07b
Vítor Galvão <info@vitorgalvao.com>
2015-12-04 12:25:41 +0000

    letterfix.rb: improve interpolation

--- Casks/letterfix.rb
+++ Casks/letterfix.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'letterfix' do
-  version '2.3.2'
+  version '2.3.2a'
   sha256 '0830e868adc90e71ddfb42ce3f4a2cd5592b0bb5e9df0caf3d45213e083cc9c0'
 
-  url "http://dl.sourceforge.jp/letter-fix/62451/LetterFix-#{version}a.dmg"
+  url "http://dl.sourceforge.jp/letter-fix/62451/LetterFix-#{version}.dmg"
   name 'LetterFix'
   homepage 'http://sourceforge.jp/projects/letter-fix/'
   license :mit
