beaecff8c638c3c726a750b12933356ed67c9b45
Francis T. O'Donovan <francis.odonovan@bostonfusion.com>
2015-12-03 21:45:27 -0500

    Update Casks to avoid hardcoding app version
    Fix #15564 (https://github.com/caskroom/homebrew-cask/pull/15564)
    changed CONTRIBUTING.md to emphasize the use of versioned
    URLs. Applied this recommended style to existing Casks.

--- Casks/polycom-realpresence-desktop.rb
+++ Casks/polycom-realpresence-desktop.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'polycom-realpresence-desktop' do
-  version '3.3'
+  version '3.3_50871'
   sha256 '75cc90740112f055ae85955f16a9da255bed60819e00a6343d42648d155b8c0c'
 
-  url 'http://downloads.polycom.com/video/realpresence_desktop/RPDMac-release_3.3_50871.dmg'
+  url "http://downloads.polycom.com/video/realpresence_desktop/RPDMac-release_#{version}.dmg"
   name 'Polycom RealPresence Desktop'
   homepage 'http://www.polycom.com/products-services/hd-telepresence-video-conferencing/realpresence-desktop/realpresence-desktop.html'
   license :commercial
