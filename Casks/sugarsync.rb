f02020d76f91ea367b054c05af02dd582fcc5a0f
Francis T. O'Donovan <francis.odonovan@bostonfusion.com>
2015-12-04 15:04:28 -0500

    Add SugarSync.app v3.7.0
    Switch to no_check for latest version

---
+++ Casks/sugarsync.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'sugarsync' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.sugarsync.com/downloads/p/InstallSugarSync.dmg'
+  name 'SugarSync'
+  homepage 'https://www.sugarsync.com/'
+  license :commercial
+
+  app 'SugarSync.app'
+end
