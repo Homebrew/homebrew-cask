d70398d8e807e82416166b332e22454ce64dafca
adityadalal924 <github@adityadalal.com>
2015-12-04 17:39:58 -0500

    change anaconda2 to anaconda(3)

--- Casks/anaconda.rb
+++ Casks/anaconda.rb
@@ -1,9 +1,9 @@
-cask :v1_1 => 'anaconda2' do
+cask :v1_1 => 'anaconda' do
   version '2.4.0'
-  sha256 '53c9123c9d508555100805fdb44d9845511c937e7a34f237beb19168d655e070'
+  sha256 '6d41e4cace78d9ed3cc4151898236ee8688cf8135cffd21de4727199c96c4737'
 
   url "https://repo.continuum.io/archive/Anaconda2-#{version}-MacOSX-x86_64.sh"
-  name 'Anaconda2'
+  name 'Anaconda'
   homepage 'https://www.continuum.io/why-anaconda'
   license :gratis
   tags :vendor => 'Continuum Analytics'
@@ -11,17 +11,17 @@ cask :v1_1 => 'anaconda2' do
   container :type => :naked
 
   preflight do
-    set_permissions "#{staged_path}/Anaconda2-#{version}-MacOSX-x86_64.sh", '+x'
+    set_permissions "#{staged_path}/Anaconda3-#{version}-MacOSX-x86_64.sh", '+x'
   end
 
-  installer :script => "Anaconda2-#{version}-MacOSX-x86_64.sh",
+  installer :script => "Anaconda3-#{version}-MacOSX-x86_64.sh",
             :args => [ '-b' ],
             :sudo => false
 
   uninstall :delete => '~/anaconda2'
 
   caveats do
-    path_environment_variable '~/anaconda2/bin'
+    path_environment_variable '~/anaconda3/bin'
   end
 
   depends_on :macos => '>= :lion'
