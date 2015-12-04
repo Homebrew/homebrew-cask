513147e7f3e87a281a2f2cf6ee73f5a8ccfbc4d6
cclauss <cclauss@bluewin.ch>
2015-12-05 00:21:53 +0100

    Really anaconda3, not anaconda2

--- Casks/anaconda.rb
+++ Casks/anaconda.rb
@@ -2,7 +2,7 @@ cask :v1_1 => 'anaconda' do
   version '2.4.0'
   sha256 '6d41e4cace78d9ed3cc4151898236ee8688cf8135cffd21de4727199c96c4737'
 
-  url "https://repo.continuum.io/archive/Anaconda2-#{version}-MacOSX-x86_64.sh"
+  url "https://repo.continuum.io/archive/Anaconda3-#{version}-MacOSX-x86_64.sh"
   name 'Anaconda'
   homepage 'https://www.continuum.io/why-anaconda'
   license :gratis
@@ -18,7 +18,7 @@ cask :v1_1 => 'anaconda' do
             :args => [ '-b' ],
             :sudo => false
 
-  uninstall :delete => '~/anaconda2'
+  uninstall :delete => '~/anaconda3'
 
   caveats do
     path_environment_variable '~/anaconda3/bin'
