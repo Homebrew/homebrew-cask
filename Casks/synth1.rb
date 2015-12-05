4ed8c410c53799951cdc22a6481ea4705e5c991d
Joshua McKinney <joshka.github.fake@joshka.net.fake>
2015-12-05 16:18:53 -0600

    Add Synth1 VST

---
+++ Casks/synth1.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'synth1' do
+  version '1.13beta8'
+  sha256 '6b172b9433358bce21f0af75a28505c3365d934fadee04a738336e7f5f601675'
+
+  url 'http://www.geocities.jp/daichi1969/softsynth/Synth1macvst113beta8.zip'
+  name 'Synth1'
+  name 'Synth 1'
+  homepage 'http://www.geocities.jp/daichi1969/softsynth/'
+  license :gratis
+
+  artifact 'Synth1.vst', :target => '../Library/Audio/Plug-Ins/VST/Synth1.vst'
+end
