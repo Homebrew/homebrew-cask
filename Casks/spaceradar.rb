9fc269f5a7a038e829d7f00e62c62bc5c1de4584
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-07 16:04:20 -0800

    Add spaceradar, a disk visualization app
    https://github.com/zz85/space-radar
    by @zz85

---
+++ Casks/spaceradar.rb
@@ -0,0 +1,21 @@
+cask :v1 => 'spaceradar' do
+  version '4.4.0'
+  sha256 'de93d62bb2845fa815b7010a5f42960ce849487489370fae3636177944b083ff'
+
+  url "https://github.com/zz85/space-radar/releases/download/v#{version}/SpaceRadar-darwin-x64.zip"
+  appcast 'https://github.com/zz85/space-radar/releases.atom'
+  name 'SpaceRadar'
+  homepage 'https://github.com/zz85/space-radar'
+  license :mit
+
+  app 'SpaceRadar.app'
+
+  uninstall :quit => 'com.electron.spaceradar'
+
+  zap :delete => [
+                  '~/Library/Application Support/space-radar',
+                  '~/Library/Caches/space-radar',
+                  '~/Library/Preferences/com.electron.spaceradar.plist',
+                  '~/Library/Saved Application State/com.electron.spaceradar.savedState'
+                 ]
+end
