fa8769b9d59c42383b8bbd269025f8e416e9f399
adityadalal924 <github@adityadalal.com>
2015-12-08 00:23:44 -0500

    updated 4k-video-downloader (3.7)

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,6 +1,6 @@
 cask :v1 => '4k-video-downloader' do
-  version '3.6'
-  sha256 '9adac4eaff8478be06dacd995e54c70bddc1571795e095003a298e52479aea21'
+  version '3.7'
+  sha256 '4fc9d184adb4fa348c9008fbe81e59c24d706a6cb1d2e3d5619f4f7a3b01ca8b'
 
   url "http://downloads.4kdownload.com/app/4kvideodownloader_#{version}.dmg"
   name '4K Video Downloader'
