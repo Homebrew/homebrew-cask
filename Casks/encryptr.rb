a1e4103d01cf69b7381d82d2a8f9ba7342286fe9
Felipe Bueno <bueno.felipe@gmail.com>
2015-12-08 15:19:38 -0200

    comment removed

--- Casks/encryptr.rb
+++ Casks/encryptr.rb
@@ -2,7 +2,6 @@ cask :v1 => 'encryptr' do
   version '2.0.0'
   sha256 '54e2ef8f8c4d22a20793b68fd35c65f8af34ffd783c48140b84c1a9b445589ea'
 
-  # github.com is the official download host per the vendor homepage
   url "https://spideroak.com/dist/encryptr/signed/osx/Encryptr-v#{version}-mac.dmg"
   appcast 'https://github.com/SpiderOak/Encryptr/releases.atom'
   name 'Encryptr'
