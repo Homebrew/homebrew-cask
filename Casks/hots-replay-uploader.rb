3e7b74d95b0ca61da7ff9eb7d0f0011b78bee55a
Philipp Gruhn <philipp.gruhn@googlemail.com>
2015-12-08 01:59:13 +0100

    upgrade HotS Replay Uploader to v2.0.1

--- Casks/hots-replay-uploader.rb
+++ Casks/hots-replay-uploader.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'hots-replay-uploader' do
-  version '2.0.0'
-  sha256 'c0d5775c47b2c57a2b63bddbbb7f4c380874cb37c277b4477491fe4d7be875e2'
+  version '2.0.1'
+  sha256 '800b6c3f6a1648815a66dfdbfdd4012436c455bf17cbc1ad26d30030877d6d6b'
 
   url "https://github.com/eivindveg/HotSUploader/releases/download/v#{version}/HotS.Replay.Uploader-#{version}.dmg"
   appcast 'https://github.com/eivindveg/HotSUploader/releases.atom'
