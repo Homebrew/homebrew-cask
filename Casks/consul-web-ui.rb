1c7f0226fad8db02daa189263c387e5f44abaa0f
Daryl Robbins <daryl@robbins.name>
2015-12-08 08:10:40 -0500

    Consul: Upgrade to 0.6

--- Casks/consul-web-ui.rb
+++ Casks/consul-web-ui.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'consul-web-ui' do
-  version '0.5.2'
-  sha256 'ad883aa52e1c0136ab1492bbcedad1210235f26d59719fb6de3ef6464f1ff3b1'
+  version '0.6.0'
+  sha256 '73c5e7ee50bb4a2efe56331d330e6d7dbf46335599c028344ccc4031c0c32eb0'
 
   # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/mitchellh/consul/#{version}_web_ui.zip"
