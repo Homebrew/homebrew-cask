7f12afdac0655c8c7e6f1f119c9971b3a68fb049
Vítor Galvão <info@vitorgalvao.com>
2015-12-08 13:11:56 +0000

    mattr-slate.rb: removed comments

--- Casks/mattr-slate.rb
+++ Casks/mattr-slate.rb
@@ -2,8 +2,6 @@ cask :v1 => 'mattr-slate' do
   version '1.2.0'
   sha256 'd409ccda9ed09f5647175f8834650e141a7375ced9665bf6af237525665d4966'
 
-  # github.com is the official download host for this fork
-  # see https://github.com/mattr-/slate/commit/148a51e50174c946c07c801a9f2b92222a4c0276
   url "https://github.com/mattr-/slate/releases/download/v#{version}/Slate.zip"
   appcast 'https://github.com/mattr-/slate/releases.atom'
   name 'Slate'
