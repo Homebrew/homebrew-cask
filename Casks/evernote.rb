1ec989b6e4cc29b5f3abce444e8c75101e13841a
Victor Popkov <victor@popkov.me>
2015-12-09 22:20:13 +0200

    evernote.rb: upgrade to v6.3_452832

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -5,8 +5,8 @@ cask :v1 => 'evernote' do
     sha256 '06b6da6d74ccab08deabfdd4c9519b9bc7f7ef0f0db2a0e8b0cd72e781f2e0ed'
     url 'https://cdn1.evernote.com/mac/release/Evernote_402634.dmg'
   else
-    version '6.2_452688'
-    sha256 '0d9cd5b678358cd9d5c57319d1cf45c3f5697c0c105af0fe2515e34af38bf8fd'
+    version '6.3_452832'
+    sha256 'ba6282c57bee35506f3ac8dc35d12398a4e1879b88f1ecc3fa0f158e75a503e8'
     url "https://cdn1.evernote.com/mac-smd/public/Evernote_RELEASE_#{version}.dmg"
     appcast 'https://update.evernote.com/public/ENMac/EvernoteMacUpdate.xml'
   end
