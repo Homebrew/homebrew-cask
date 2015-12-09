dd30bcdc292bffebd3a6979cc7a77cd86dc6a18a
Keloran <keloran@nordicarts.net>
2015-12-09 10:39:26 +0000

    updated alcatraz to 1.1.15

--- Casks/alcatraz.rb
+++ Casks/alcatraz.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'alcatraz' do
-  version '1.1.13'
-  sha256 'be5484b4571dedcceda25908daba70ba1cee85ce71e5b4a5172dac6ee4246a7e'
+  version '1.1.15'
+  sha256 'e28bfd9fcc22cdfde0c7c8f31e3468a4847e7bda9cb69f309f24257eee3dd41'
 
   url "https://github.com/alcatraz/Alcatraz/releases/download/#{version}/Alcatraz.tar.gz"
   appcast 'https://github.com/alcatraz/Alcatraz/releases.atom'
