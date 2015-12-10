01ef361f50244d237d14b10a3d90521fcdfd3866
Sergey Parhomenko <sparhomenko@thunderhead.com>
2015-12-10 10:00:34 +0100

    Switching to the default distribution of DbVisualizer, based on Java 8

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'dbvisualizer' do
   version '9.2.13'
-  sha256 '8a1687aa293c94ae283ef6912e60756440ba1a157a99c5f5c3a78cc106f0c9a5'
+  sha256 '9e7af8891b4e8218f28415db0a3680e63b68cf814d3b3fb566659ed4f6005404'
 
-  url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.gsub('.', '_')}_java7.dmg"
+  url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.gsub('.', '_')}.dmg"
   name 'DbVisualizer'
   homepage 'https://www.dbvis.com/'
   license :commercial
@@ -17,7 +17,7 @@ cask :v1 => 'dbvisualizer' do
   zap :delete => '~/.dbvis'
 
   caveats <<-EOS.undent
-    #{token} requires Java 7. You can install the latest version with
-      brew cask install caskroom/versions/java7
+    #{token} requires Java 8. You can install the latest version with
+      brew cask install java
   EOS
 end
