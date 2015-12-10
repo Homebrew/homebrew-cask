9ba20f226de7720f839162d94ac754c7a471a890
Vieira <vieirasantosn@gmail.com>
2015-12-10 16:35:12 -0200

    Update phpstorm.rb

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'phpstorm' do
-  version '10.0.1'
+  version '10.0.2'
   sha256 'f5ec2acf1d15fcf06ff32c415fad76b3d2e60b9a034e6709ebf933ef60574b01'
 
   url "https://download.jetbrains.com/webide/PhpStorm-#{version}-custom-jdk-bundled.dmg"
