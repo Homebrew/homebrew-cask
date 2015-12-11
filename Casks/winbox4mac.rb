50e26cf634d3adc20683c8d4de96ceab3f986e88
Nathaniel Varona <nathaniel.varona@gmail.com>
2015-12-11 15:25:29 +0800

    Complied version of “Winbox” by MikroTik with Wine in order to make it usable on Mac.

---
+++ Casks/winbox4mac.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'winbox4mac' do
+  version '3.0'
+  sha256 '2924d7629524f835ae9b49e125edb44f9e8cda890e41609fab5db66a8d63f595'
+
+  url 'http://joshaven.com/Winbox4Mac_3.0.dmg'
+  name 'Winbox for Mac'
+  homepage 'http://joshaven.com/resources/tools/winbox-for-mac/'
+  license :unknown
+
+  app 'Winbox4Mac.app'
+end
