30a8b99f764fe0bcc6c49642e13d8c533b6a35ae
Jeffrey C <spaz926@users.noreply.github.com>
2015-12-12 02:54:59 -0600

    Upgrade to version 0.93.3

--- Casks/armory.rb
+++ Casks/armory.rb
@@ -1,6 +1,5 @@
 cask :v1 => 'armory' do
-  version '0.93.2'
-  sha256 '1b8c36433802087e772594335c55f1a08acfc9a0571157d874c13268eb4362f6'
+  version '0.93.3'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/bitcoinarmory-releases/armory_#{version}_osx.tar.gz"
