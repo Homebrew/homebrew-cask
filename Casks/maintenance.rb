7c8430f026c4c25a265b1d920af6ae5ceca60e6b
Tin H Kyaw <tinhkyaw@gmail.com>
2015-12-13 15:44:22 -0800

    El Capitan support for Deeper and Maintenance
    added El Capitan support for Deeper and Maintenance

--- Casks/maintenance.rb
+++ Casks/maintenance.rb
@@ -16,6 +16,8 @@ cask :v1 => 'maintenance' do
     url 'http://www.titanium.free.fr/download/109/Maintenance.dmg'
   elsif MacOS.release == :yosemite
     url 'http://www.titanium.free.fr/download/1010/Maintenance.dmg'
+  elsif MacOS.release == :el_capitan
+    url 'http://joel.barriere.pagesperso-orange.fr/download/1011/Maintenance.dmg'
   else
     # Unusual case: there is no fall-through.  The software will stop
     # working, or is dangerous to run, on the next OS X release.
@@ -35,6 +37,7 @@ cask :v1 => 'maintenance' do
                           :mountain_lion
                           :mavericks
                           :yosemite
+                          :el_capitan
                          }
 
   caveats do
