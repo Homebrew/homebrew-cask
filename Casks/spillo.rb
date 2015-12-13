b9299be5c68fd6d04bcfafe1e53385935e7681d3
Victor Popkov <victor@popkov.me>
2015-12-13 21:20:44 +0200

    spillo.rb: upgrade to v1.6.3

--- Casks/spillo.rb
+++ Casks/spillo.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'spillo' do
-  version '1.6.2'
-  sha256 '65937ac2f298cd59fda46d000fd41c6a11be2afb91a8863cb9dffbef29167c6b'
+  version '1.6.3'
+  sha256 'a6193dd3b3cd8283fbeb1d5b92255e4c82073b5befc8638ba95397f98b4a2ecf'
 
   url "http://bananafishsoftware.com/software/spillo/app/spillo-#{version}.zip"
   name 'Spillo'
   appcast 'http://bananafishsoftware.com/feeds/spillo.xml',
-          :sha256 => 'a42ee543950ca5bccc862f3fd492a33456fd0dff44dfc70de1ce13da7e894144'
+          :sha256 => '1e7e2f8bbaede766bef1157ee3d33758b2a09a10de2fb00aa1a5157cdc0f9035'
   homepage 'http://bananafishsoftware.com/products/spillo/'
   license :commercial
 
