5a0d36dac5008283703eb38f9f314d475dece0e2
Victor Popkov <victor@popkov.me>
2015-12-14 23:32:08 +0200

    launchcontrol.rb: upgrade to v1.25

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'launchcontrol' do
-  version '1.24.1'
-  sha256 '7c8f8f9070fe01cbf9b6ee06939aec938a9f364e82b0c1bd882fa9ea50ddb92e'
+  version '1.25'
+  sha256 'cafa8454acba01fadc5710c2f395ca288e9eacf92ad6725593c1f7466da46e79'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
-          :sha256 => 'b169efc42ac688c9a6be365da4eb9d0f24595c7bc4239f02721271edbf7b3aa7'
+          :sha256 => '1d7bfd545b9be977aa0ebb3bde5e171e716d7875123b0dfd8bfc8689b259d2f9'
   name 'LaunchControl'
   homepage 'http://www.soma-zone.com/LaunchControl/'
   license :commercial
