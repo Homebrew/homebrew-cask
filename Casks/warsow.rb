434fb79d6efd136c0e70aaec4939d6c17f7e2db7
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-12-14 18:09:42 +0100

    upgrade Warsow cask to 2.0

--- Casks/warsow.rb
+++ Casks/warsow.rb
@@ -1,13 +1,11 @@
 cask :v1 => 'warsow' do
-  version '1.51'
-  sha256 '4d1a95f94769e0ec9dc70a000a3b461a0968cb5d8e45f5b6850607a99a058449'
+  version '2.0'
+  sha256 '07a4975c5c5abeb96ee9c11b53b363fb00aeba5d2a016563862538d28acf3763'
 
-  url "http://update.warsow.net/mirror/warsow_#{version}.dmg.zip"
+  url "https://www.warsow.gg/download?dl=warsow#{version.delete('.')}mac"
   name 'Warsow'
   homepage 'http://www.warsow.net/'
   license :gpl
 
-  container :nested => "Warsow #{version}.dmg"
-
-  app "Warsow #{version}.app"
+  app 'Warsow.app'
 end
