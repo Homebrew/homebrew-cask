a50187f03d95c6884d0e1b7baa70718bfdd51fbd
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-14 14:58:03 -0800

    Update inboard to v1.0.6-279

--- Casks/inboard.rb
+++ Casks/inboard.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'inboard' do
-  version '1.0.5-277'
-  sha256 'ee9ffe0abbb9730cbf63f90be1ebb335a1cc0a90e2c5c527fb0542f5a1c8d296'
+  version '1.0.6-279'
+  sha256 '071a85c42b26d15387d6ab03b110823871c20234b5cf05b464829bfdb40eb7b1'
 
   url "http://inboardapp.com/trial/Inboard-#{version}.zip"
   name 'Inboard'
   appcast 'http://inboardapp.com/appcast.xml',
-          :sha256 => 'e0481d21c66c6b33513f8b4d8916a6f8c08a105fbfc50a0e6040644f3868a00a'
+          :sha256 => 'cf1fe2fce047c5a5f9434de1e314a791ddc0d9c9c960dc1608902a8c7780835a'
   homepage 'http://inboardapp.com'
   license :commercial
 
