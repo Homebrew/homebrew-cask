2d8bb58250e6f8190fd9c7accc227a778898e48b
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-14 15:33:47 -0800

    Update googleappengine to v1.9.30

--- Casks/googleappengine.rb
+++ Casks/googleappengine.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'googleappengine' do
-  version '1.9.28'
-  sha256 'bc63f29a0fab4521975107db66031bc9a60cb74f1c5c1b815febc89f3d690de9'
+  version '1.9.30'
+  sha256 '11f28ae1f343ad44102e79e94887465c7a7ed9daa1c2cc749bb3b921e64a447f'
 
   # googleapis.com is the official download host per the vendor homepage
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
   appcast 'https://storage.googleapis.com/appengine-sdks',
-          :sha256 => '4981d218798ee2a124921c980c8c8a71b3538ddeb6f8b1d56ab97e4d6b1cca69'
+          :sha256 => '4d1c93104230e3d96a7dffe78261d6e48e103a6972a59f22610e75b2c855871c'
   name 'Google App Engine'
   homepage 'https://developers.google.com/appengine/'
   license :apache
