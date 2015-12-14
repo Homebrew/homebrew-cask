a396f687d88dc64ded55a1b6a84c32f7f0f676b4
Shaun Walbridge <shaun.walbridge@gmail.com>
2015-12-13 21:53:56 -0500

    Update RStudio download to use HTTPS.

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -3,7 +3,7 @@ cask :v1 => 'rstudio' do
   sha256 'e211c0645540b8c668824b6fecdfd19820fe937c4db4432abc7aba06e7efd9e6'
 
   # rstudio.org is the official download host per the vendor homepage
-  url "http://download1.rstudio.org/RStudio-#{version}.dmg"
+  url "https://download1.rstudio.org/RStudio-#{version}.dmg"
   name 'RStudio'
   homepage 'http://www.rstudio.com/'
   license :affero
