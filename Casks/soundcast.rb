24ff18d991e80fa1a1ce28ac6da50f497b807df6
Vítor Galvão <info@vitorgalvao.com>
2015-12-14 12:39:32 +0000

    soundcast.rb: added appcast

--- Casks/soundcast.rb
+++ Casks/soundcast.rb
@@ -3,6 +3,7 @@ cask :v1 => 'soundcast' do
   sha256 '19c53bac9900904ed3773360036ecc04f0c3cc24d09f908e3692b4ac9c422505'
 
   url "https://github.com/andresgottlieb/soundcast/releases/download/v#{version}/Soundcast_v#{version}.zip"
+  appcast 'https://github.com/andresgottlieb/soundcast/releases.atom'
   name 'Soundcast'
   homepage 'https://github.com/andresgottlieb/soundcast'
   license :mit
