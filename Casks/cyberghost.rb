e49840b5045a5d7884b9d3b0b5fbb54feb38a120
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-14 10:55:52 -0800

    Update cyberghost to v5.0.14.12

--- Casks/cyberghost.rb
+++ Casks/cyberghost.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'cyberghost' do
-  version '5.0.14.11'
-  sha256 'c1f10d6734482b319d66ef546cf31c35f9386b1befd80de6495e48f47c9d610e'
+  version '5.0.14.12'
+  sha256 'c1ed2c4837be99e0c2fef9c957d9e798a359f1b1bacb03dd5850cc9f779855be'
 
   url "https://download.cyberghostvpn.com/mac/cg5mac_#{version}.dmg"
   name 'CyberGhost'
   appcast 'https://download.cyberghostvpn.com/mac/updates/cyberghost_mac_update.inf',
-          :sha256 => '47b058275557f590e834a9ca94f67f697d05565b020a437251377df35564c9fc'
+          :sha256 => 'c006b554455103fc01c48c491d5d4d3dcc35e16656d3224e48a43d4f70bc9508'
   homepage 'https://www.cyberghostvpn.com/'
   license :gratis
 
