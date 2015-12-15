87242cc601261aabd5910ec673b7bee1cac506b4
Victor Popkov <victor@popkov.me>
2015-12-15 21:35:39 +0200

    firebird.rb: upgrade to v0.30

--- Casks/firebird.rb
+++ Casks/firebird.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firebird' do
-  version '0.20'
-  sha256 '834d62c4c7cf7bf2fa895bdbdf8c54da863411e930c5412cd3df9ad664830c38'
+  version '0.30'
+  sha256 'ec23275a9007fa180dacc7ae23348f9af7afa048df1c9c84b85ed8524447456b'
 
   url "https://github.com/nspire-emus/firebird/releases/download/v#{version}/firebird-mac.dmg"
   appcast 'https://github.com/nspire-emus/firebird/releases.atom'
