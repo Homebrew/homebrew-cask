1478d2fb92872513125363d812e43ad49f1e9dc3
Victor Popkov <victor@popkov.me>
2015-12-15 21:43:27 +0200

    stack.rb: upgrade to v0.1.10.1

--- Casks/stack.rb
+++ Casks/stack.rb
@@ -1,6 +1,6 @@
 cask :v1_1 => 'stack' do
-  version '0.1.10.0'
-  sha256 'd3f98aef526b8037dc5bcdea2a8944378c47e1e7c44b39eaefb4f55e7cb48703'
+  version '0.1.10.1'
+  sha256 '79abca83450e4c312b4c48ceb32bdf233dea9b67efb95cf2ced4534066c43494'
 
   url "https://github.com/commercialhaskell/stack/releases/download/v#{version}/stack-#{version}-osx-x86_64.tar.gz"
   appcast 'https://github.com/commercialhaskell/stack/releases.atom'
