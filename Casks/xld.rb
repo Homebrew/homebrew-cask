575d4131ee29eff33d4ae6e80107c0cda4c4d3b5
littleneko <876608906@qq.com>
2015-12-15 12:56:10 +0800

    Update xld.rb
    update xld to version 20151214

--- Casks/xld.rb
+++ Casks/xld.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'xld' do
-  version '20151128'
-  sha256 '75571c82a3d3b5f943cbf72774cb52e724fcca64bb63b251ee57d675435c6949'
+  version '20151214'
+  sha256 '08a03d72cf0ff2cc9e8810dc76604fd527270b0b6d35563d133e537a3ea30117'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/xld/xld-#{version}.dmg"
   appcast 'https://svn.code.sf.net/p/xld/code/appcast/xld-appcast_e.xml',
-          :sha256 => '2bf53dc840b0b037921e1ccc3b4b3244ff1a84f306c3ce749ec9da39f220b6d0'
+          :sha256 => 'bfcf5c644e119533677462c8d4f50670aa37aea7947170be7adb375a040f2fd4'
   name 'X Lossless Decoder'
   name 'XLD'
   homepage 'http://tmkk.undo.jp/xld/index_e.html'
