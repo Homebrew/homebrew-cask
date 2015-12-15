e022dedc55e6cf590e8adf6b3093164bbed7d790
Victor Popkov <victor@popkov.me>
2015-12-15 21:41:23 +0200

    rightfont.rb: upgrade to v2.4.0

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'rightfont' do
-  version '2.3.0'
-  sha256 '98b8003f3cb83e57ffe1da76b91300b2e33fd621d45d7d8950af7e29f65065d2'
+  version '2.4.0'
+  sha256 '05207bac5dbfc059a0e6d7f106d42367a66c6184309b285a72711ab1137747ee'
 
   url "http://rightfontapp.com/downloads/#{version}/rightfont.zip"
   appcast 'http://rightfontapp.com/update/appcast.xml',
-          :sha256 => '1c7fe82ec54aa8b1c2ed3cc0fff5f95702a2cc55c061735777943eb0db442816'
+          :sha256 => '47fb2a9e852f2370bd8eca7c93505c56b3444a213a5efffd47acb842d50d17f4'
   name 'RightFont'
   homepage 'http://rightfontapp.com/'
   license :commercial
