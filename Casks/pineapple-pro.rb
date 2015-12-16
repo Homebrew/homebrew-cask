7f9fee676f7d67e89d0cbc7e114b9aec20215928
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-12-16 14:20:01 +0100

    upgrading pineapple

--- Casks/pineapple-pro.rb
+++ Casks/pineapple-pro.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'pineapple-pro' do
-  version '0.7.4'
-  sha256 'ba8d623b4221cf64770fe6a843349763496fc6e44a15e1f7756047eba1b1ebc8'
+  version '0.8.0'
+  sha256 '9ad7c2371ff5c82be5ad236c17085261dba80f462f19fafd33de6a718c162881'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "https://s3.amazonaws.com/com.nathansuniversity/Pineapple/Releases/Pineapple-Pro-#{version}-Python3.5.dmg"
+  url "https://s3.amazonaws.com/com.nathansuniversity/Pineapple/Releases/Pineapple-#{version}-Python3.5.dmg"
   name 'Pineapple'
   homepage 'http://nwhitehead.github.io/pineapple/'
   license :gpl
