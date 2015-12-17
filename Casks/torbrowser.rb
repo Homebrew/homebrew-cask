d0c27db24ba70934e90d38b688ca631319811134
Michael Ford <fanquake@users.noreply.github.com>
2015-12-17 11:25:15 +0800

    TorBrowser 5.0.5

--- Casks/torbrowser.rb
+++ Casks/torbrowser.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser' do
-  version '5.0.4'
-  sha256 '168a116c55c56126a27521c28d64b1a13f8ea73d7350dabb8dd29059a8f22734'
+  version '5.0.5'
+  sha256 '7559cbe1268435d790495cb544ce0936dd0d13d92a12a3d7adb081374f809444'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
   gpg "#{url}.asc",
