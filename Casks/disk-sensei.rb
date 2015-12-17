fc727e8a9cb7faba61d2276e2fa94072e69b4e53
Josh Hagins <hagins.josh@gmail.com>
2015-12-16 23:57:04 -0500

    disk-sensei: fix audit error ('latest' -> :latest)

--- Casks/disk-sensei.rb
+++ Casks/disk-sensei.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'disk-sensei' do
-  version 'latest'
+  version :latest
   sha256 '3b17d9d02006530a176e51da312d9782825a660305dba874706d7a1462f10288'
 
   # amazonaws.com is the official download host per the vendor homepage
