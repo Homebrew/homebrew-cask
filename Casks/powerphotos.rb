20f0c70d2ba39d5d6e968d91465940d9d1c27ffb
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-18 15:21:04 -0800

    updated powerphotos (1.1.3)

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -1,12 +1,12 @@
 cask 'powerphotos' do
-  version '1.1.2'
-  sha256 '2822c28f1aab47fe5063e0527afe80fff3d044d181493068f9177e0ef8d8e31c'
+  version '1.1.3'
+  sha256 '8b1513c3c6fdd6f99a4ce436a906de774ee5717318c2fb4570455d539fb6a1bd'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/fatcatsoftware/powerphotos/PowerPhotos_#{version.delete('.')}.zip"
   name 'PowerPhotos'
   appcast 'http://www.fatcatsoftware.com/powerphotos/powerphotos_appcast.xml',
-          :sha256 => '36cfe73bd7332b8fd6a9dcd662a4569e930ef8f2a1f212d8714631988301c64e'
+          :sha256 => '67c166e96bc9f51af923100a133ef6d743e599ef83d4e19a48ba68f30d6a9334'
   homepage 'http://www.fatcatsoftware.com/powerphotos/'
   license :commercial
 
