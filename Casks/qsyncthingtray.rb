4fc4df40da4e7cdf6303edf5a8df01111e5ecbc9
adityadalal924 <github@adityadalal.com>
2015-12-18 01:45:19 -0500

    add qsyncthingtray 0.2.7

---
+++ Casks/qsyncthingtray.rb
@@ -0,0 +1,16 @@
+cask 'qsyncthingtray' do
+  version '0.2.7'
+  sha256 'b006a0d723560a953e27f5096084d1a57602241e1b60679892b0e65bed261dba'
+
+  url "https://github.com/sieren/QSyncthingTray/releases/download/#{version}/QSyncthingTray_OSX.dmg"
+  appcast 'https://github.com/sieren/QSyncthingTray/releases.atom'
+  name 'QSyncthingTray'
+  homepage 'https://github.com/sieren/QSyncthingTray'
+  license :gpl
+
+  depends_on :formula => 'syncthing'
+
+  app 'QSyncthingTray.app'
+
+  zap :delete => '~/Library/Preferences/com.sieren.QSyncthingTray.plist'
+end
