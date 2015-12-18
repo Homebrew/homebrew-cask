75311e48b74021ce69b9416f4ddf6949344c7dbc
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-18 14:58:13 -0800

    updated macspice (3.1.8)

--- Casks/macspice.rb
+++ Casks/macspice.rb
@@ -1,10 +1,10 @@
 cask 'macspice' do
-  version '3.1.7'
-  sha256 '80e1824e710467115700f377311f5ac287be7154caf228d87ab2ffab3a52b69c'
+  version '3.1.8'
+  sha256 'bf93677e91ea00e9604a347611c70d13312ccf479b42d215cce530e50e979082'
 
   url "http://www.macspice.com/mirror/binaries/v#{version}/MacSpice3f5.dmg"
   appcast 'http://www.macspice.com/AppCast-v2.xml',
-          :sha256 => '06453465656d258c11326d1f2cba6158a11ff9a939f557fb5da1b3b2d4402db0'
+          :sha256 => '79d1e2a5a03f4bf6cbf99da2055d762f9607579a40db686f9e283993d3569a70'
   name 'MacSpice'
   homepage 'http://www.macspice.com/'
   license :closed
