b79c32f5109748ee5b61dcde6c659f51209fe4af
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-18 15:54:24 -0800

    Add el capitan-specific version of sente
    http://www.thirdstreetsoftware.com/blog/2015/09/sente-679-for-os-x-el-capitan-prerelease.html

--- Casks/sente.rb
+++ Casks/sente.rb
@@ -2,6 +2,9 @@ cask 'sente' do
   if MacOS.release <= :snow_leopard
     version '6.6.5.11312'
     sha256 '0a848a2eada32651707e2deec991f06cc068a4c78b23447d38453853d434c2c8'
+  elsif MacOS.release == :el_capitan
+    version '6.7.9.11901'
+    sha256 'e7ce17c006598b446952e92ae4fbfb784ea029d61bb7c8a4abad6db68990c901'
   else
     version '6.7.8.11870'
     sha256 'f75bb32db9f0dbfa3d5dbf7639371b5892f6071e740de38e768a203899bb4b5b'
