1c237729fdfd57675d052a28000815e1489ff7d8
Vítor Galvão <info@vitorgalvao.com>
2015-12-18 18:06:56 +0000

    quickbooks-desktop.rb: remove empty line

--- Casks/quickbooks-desktop.rb
+++ Casks/quickbooks-desktop.rb
@@ -3,7 +3,6 @@ cask 'quickbooks-desktop' do
   sha256 :no_check # required as upstream package is updated in-place
 
   url "http://http-download.intuit.com/http.intuit/Downloads/#{version}/Latest/QuickBooksMac#{version}.dmg"
-
   name 'QuickBooks Desktop'
   homepage 'http://quickbooks.intuit.com/mac/'
   license :commercial
