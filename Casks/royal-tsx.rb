bc8f14869dea0af391ce2f1f25efa130ae584b87
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-18 15:23:54 -0800

    updated royal-tsx (2.2.1.1000)

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -1,10 +1,10 @@
 cask 'royal-tsx' do
-  version '2.2.1.1'
-  sha256 '5041be5501b87612070677efe0b3752e6051833033a95c9f0ba99a72833ef34e'
+  version '2.2.1.1000'
+  sha256 '5622c6d7f3593c5b0f6056614f1afe45bceb9143ca05cb14867f8581de017a47'
 
   url "http://v2.royaltsx.com/updates/royaltsx_#{version}.dmg"
   appcast 'http://v2.royaltsx.com/updates_beta.php',
-          :sha256 => '326f26d7c0d628d5fbccfdb4986593a5da5288b26d71bbfa7e504747cb08d309'
+          :sha256 => '51ac7cd5b6f296d5b28d6d8f959e4e01d29ca28d4742eebe25c061d18f5d649c'
   name 'Royal TSX'
   homepage 'http://www.royaltsx.com'
   license :freemium
