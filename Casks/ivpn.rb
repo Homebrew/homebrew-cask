951635ccd99e237f76fab6d8652eaf868fc119d4
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-18 14:56:02 -0800

    updated ivpn (7.4.1)

--- Casks/ivpn.rb
+++ Casks/ivpn.rb
@@ -1,10 +1,10 @@
 cask 'ivpn' do
-  version '7.3.1'
-  sha256 '85f5e4fb9d09fabf13c08841aa1f35e64d44792b09fd521f49a5281a675d17f5'
+  version '7.4.1'
+  sha256 'b1e1803fe7b2f1232a3bfc4c5fddd682cc92d147ab57cab48f6391358716df2c'
 
   url "http://macserve.org.uk/downloads/ivpn/iVPN_#{version}.zip"
   appcast 'http://macserve.org.uk:8090/profileInfo.php',
-          :sha256 => '6a4c9162b1b516b99e274b6c5cb2760a1b615de163395e639280639a785e0315'
+          :sha256 => 'fcaa1830a2c5a2731b724ac9dfdd25dc5bd9a7bfd10f56b4b48fd79d6e8d87f3'
   name 'iVPN'
   homepage 'http://macserve.org.uk/projects/ivpn/'
   license :commercial
