83dcfa8c0a654c4a2b927fd9c31b69e274a13f64
Andres Gottlieb <agottlieb@gmail.com>
2015-12-18 22:23:12 -0300

    Upgrade soundcast.app to v1.6

--- Casks/soundcast.rb
+++ Casks/soundcast.rb
@@ -1,6 +1,6 @@
 cask 'soundcast' do
-  version '1.5'
-  sha256 '19c53bac9900904ed3773360036ecc04f0c3cc24d09f908e3692b4ac9c422505'
+  version '1.6'
+  sha256 '470098b66a9d48c7c0fada09cc8bf27190d09015eaee9be74c8c4f01d29f84ef'
 
   url "https://github.com/andresgottlieb/soundcast/releases/download/v#{version}/Soundcast_v#{version}.zip"
   appcast 'https://github.com/andresgottlieb/soundcast/releases.atom'
