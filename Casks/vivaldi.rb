30eb6efad275826df087b9e56def1e6706f76cd4
Andrea Passaglia <gurghet@gmail.com>
2015-12-19 22:22:55 +0100

    Updated vivaldi.rb
    Updated vivaldi.rb
    
    Updated vivaldi.rb
    
    Updated vivaldi.rb

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,8 +1,8 @@
 cask 'vivaldi' do
-  version '1.0.303.52'
+  version '1.0.344.37'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://vivaldi.com/download/Vivaldi_Beta.#{version}.dmg"
+  url "https://vivaldi.com/download/stable/Vivaldi.#{version}.dmg"
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
   license :gratis
