900c06b136da1bbfc6c5b1a2581feac2b2249426
Vítor Galvão <info@vitorgalvao.com>
2015-12-20 18:48:53 +0000

    updated shotcut (15.12.02)

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,6 +1,6 @@
 cask 'shotcut' do
-  version '15.11.01'
-  sha256 '1a4dc85a01a877e5a68ef8b35aa937e469c808fae6471f22bfc38fe7b8814de4'
+  version '15.12.02'
+  sha256 'bcfc2355debb8082839d79f51c9722b9217f88e513320462b6a2298163b84a17'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/mltframework/shotcut/releases/download/v#{version.sub(/\.\d+$/, '')}/shotcut-osx-x86_64-#{version.delete('.')}.dmg"
