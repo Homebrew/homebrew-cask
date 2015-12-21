e4da9afb7a8583fb9cdad2b5ad843dda5820c5de
Shawn Stratton <sstratton@php.net>
2015-12-21 13:36:19 -0800

    Update to vagrant 1.8.0
    Update to vagrant 1.8.0

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,6 +1,6 @@
 cask 'vagrant' do
-  version '1.7.4'
-  sha256 '3d2e680cc206ac1d480726052e42e193eabce56ed65fc79b91bc85e4c7d2deb8'
+  version '1.8.0'
+  sha256 'ed9415e6d55a3c44863eb75817219fda752ff8ca087e7980fbb6539e8a9077dd'
 
   # hashicorp.com is the official download host per the vendor homepage
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}.dmg"
