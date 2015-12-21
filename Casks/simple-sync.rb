daeef12f2178ce5b2828bb8728533b382bce8064
Markus Reiter <me@reitermark.us>
2015-12-21 20:32:02 +0100

    Simple Sync 3.0.1

---
+++ Casks/simple-sync.rb
@@ -0,0 +1,19 @@
+cask 'simple-sync' do
+  version '3.0.1-232'
+  sha256 'afb31f53f92dc4187a0cc91542561aa006870d5e5f906eae70b4be0a406c8f1d'
+
+  # roomieremote.com is the official download host per the vendor homepage
+  url "https://www.roomieremote.com/b/SimpleSync-#{version.delete('.')}.zip"
+  appcast 'http://www.roomieremote.com/b/Simple-SyncAppcast.xml',
+          :sha256 => '3968102cbd303c83db82ef0a73170e0891d5ff681996f94b69c8c80120659d51'
+  name 'Simple Sync'
+  homepage 'https://simplecontrol.com/simple-sync'
+  license :commercial
+
+  app 'Simple Sync.app'
+
+  zap :delete => [
+                   '~/Library/Application Support/Roomie Agent',
+                   '~/Library/Preferences/com.roomie.agent.plist',
+                 ]
+end
