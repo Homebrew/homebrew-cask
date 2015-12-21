648b8f7cfa11dd4cbe66d83c468884cd237c8c10
Eitot <Eitot@users.noreply.github.com>
2015-12-21 10:12:03 +0100

    Update ImageOptim to 1.6.1

--- Casks/imageoptim.rb
+++ Casks/imageoptim.rb
@@ -1,6 +1,6 @@
 cask 'imageoptim' do
-  version '1.6.0'
-  sha256 'b319e97fb69631c29e5e67caf93ed1e2d000bcbd6bc1cbb33301710c381eaf66'
+  version '1.6.1'
+  sha256 'cfa43f7d9dbd3f231a95feb534c8862ed6770920a6f03a713a1a5556a26a9978'
 
   url "https://imageoptim.com/ImageOptim#{version}.tar.bz2"
   appcast 'https://imageoptim.com/appcast.xml'
@@ -9,4 +9,11 @@ cask 'imageoptim' do
   license :gpl
 
   app 'ImageOptim.app'
+
+  zap :delete => [
+                  '~/Library/Preferences/net.pornel.ImageOptim.plist',
+                  '~/Library/Caches/net.pornel.ImageOptim',
+                  '~/Library/Caches/ImageOptimResults.db',
+                  '~/Library/Saved Application State/net.pornel.ImageOptim.savedState'
+                 ]
 end
