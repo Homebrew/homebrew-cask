9a3836e0564f9d9291efd4f4f600fc316b34d8ba
Lee Hanxue <hanxue.developer@gmail.com>
2015-12-22 12:42:42 +0800

    Update Pacifist to 3.5.7

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -6,13 +6,13 @@ cask 'pacifist' do
     version '3.2.17'
     sha256 'd38e12293bc6087ddb09275e3c5ab34faa670e87e9dd41e04a587dd387f7b1d3'
   else
-    version '3.5.6'
-    sha256 'e730c3d7e1c57819e43c461a6acdc09cbad3cc295533e9a30e22e3a633246496'
+    version '3.5.7'
+    sha256 'd7caa20ce185152dbd6c868b90df07085748c2c1b791e3fd133d287a8161ac79'
   end
 
   url "https://www.charlessoft.com/pacifist_download/Pacifist_#{version}.dmg"
   appcast 'https://www.charlessoft.com/cgi-bin/pacifist_sparkle.cgi',
-          :sha256 => '695bd3911774d3f6dcba7111d9f31846a915da96a850cfceb19d10c916d0925f'
+          :sha256 => 'e68434065944cacad0c6831b989abfa8c16e873f3c07b9172925f0e2a73ab6f9'
   name 'Pacifist'
   homepage 'https://www.charlessoft.com/'
   license :commercial
