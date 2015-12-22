189fe6ae79727af44ebc6e2cb3df3401b1148d66
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-21 21:50:03 -0500

    Fix dsl and url in synth1-au and synth1-vst casks
    - Use and interpolate version
    - Remove dsl version erroneously introduced in abe5fa7096a26950b71eb53d567739ee58f0bf62

--- Casks/synth1-au.rb
+++ Casks/synth1-au.rb
@@ -1,8 +1,8 @@
-cask :v1 => 'synth1-au' do
+cask 'synth1-au' do
   version '1.13beta8'
   sha256 '78660848d78c19cdda2597bd050a2ec95fa915cc1a1ab598bde0192a1cbc9a6f'
 
-  url 'http://www.geocities.jp/daichi1969/softsynth/Synth1macau113beta8.zip'
+  url "http://www.geocities.jp/daichi1969/softsynth/Synth1macau#{version.delete('.')}.zip"
   name 'Synth1 (AU)'
   name 'Synth 1 (AU)'
   homepage 'http://www.geocities.jp/daichi1969/softsynth/'
