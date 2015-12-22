fd1a6b4f683b6f01b104e78f7e262b7d3d4df10d
Victor Popkov <victor@popkov.me>
2015-12-22 18:41:34 +0200

    biba.rb: upgrade to v3.7.3752

--- Casks/biba.rb
+++ Casks/biba.rb
@@ -1,11 +1,11 @@
 cask 'biba' do
-  version '3.6.3699'
-  sha256 '179b511ad7c6cbb26cfdf7ecffe3a145d49372c2bff397c38c76f4254dc46e80'
+  version '3.7.3752'
+  sha256 'fad2844cf812ee2b51c885e79d2a6fee9709ea4106e2e7ab49bcfa86b02ba7a4'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3-us-west-1.amazonaws.com/downloads.biba.com/builds/Biba-OSX-#{version}.dmg"
   appcast 'https://s3-us-west-1.amazonaws.com/downloads.biba.com/appcast/mac',
-          :sha256 => 'cfefc565fa942aa6d1da310db7a72a37e7ba4dcef26353fef020e35da2c39f77'
+          :sha256 => '85825cf0d081700ed109d27c790e51accabd952b99909fb4b1407a68157fc3a1'
   name 'Biba'
   homepage 'https://www.biba.com'
   license :closed
