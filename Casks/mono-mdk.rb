b4bd120fe0e6994029980c91c556613cceccced5
Markus Reiter <me@reitermark.us>
2015-12-22 23:13:28 +0100

    Revert Mono-MDK checksum to :no_check.

--- Casks/mono-mdk.rb
+++ Casks/mono-mdk.rb
@@ -1,6 +1,6 @@
 cask 'mono-mdk' do
   version '4.2.1.102'
-  sha256 'ce886f20e509544cfc4379d10a0e4c574b06f0f97ae09096a06aee0c769e1f48'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "http://download.mono-project.com/archive/#{version.sub(%r{\.[^.]*$},'')}/macos-10-universal/MonoFramework-MDK-#{version}.macos10.xamarin.universal.pkg"
   name 'Mono'
