ff96e040db2c1780e0a743a58fd1712f4939ddc4
Markus Reiter <me@reitermark.us>
2015-12-23 05:49:55 +0100

    Tower 2.3.3

--- Casks/tower.rb
+++ Casks/tower.rb
@@ -1,6 +1,6 @@
 cask 'tower' do
-  version '2.3.2-299-6e9850db'
-  sha256 '2778bb1c98417b694bf0b72908c3d88c3b055ac959b6a1366186c0b789a2f20a'
+  version '2.3.3-302-6f2959a3'
+  sha256 '5c23102b735172bc311cd811f7dec8a98a40c2c0cff2cefc9960f930c2515bc8'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://fournova-app-updates.s3.amazonaws.com/apps/tower#{version.to_i}-mac/#{version.sub(%r{^.*?-},'')}/Tower-#{version.to_i}-#{version.sub(%r{-[^-]*$},'')}.zip"
