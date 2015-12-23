5c84990dcd0ed9cf418270b581a5351a83523939
Aditya Dalal <adityadalal924@users.noreply.github.com>
2015-12-23 13:14:11 -0500

    Update virtualbox.rb
    Reverts https://github.com/caskroom/homebrew-cask/pull/16072

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -14,8 +14,6 @@ cask 'virtualbox' do
   tags :vendor => 'Oracle'
 
   pkg 'VirtualBox.pkg'
-  binary '/Applications/VirtualBox.app/Contents/MacOS/VBoxManage'
-  binary '/Applications/VirtualBox.app/Contents/MacOS/VBoxHeadless'
 
   uninstall :script => { :executable => 'VirtualBox_Uninstall.tool', :args => %w[--unattended] },
             :pkgutil => 'org.virtualbox.pkg.*'
