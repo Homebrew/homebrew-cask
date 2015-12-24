09158e6a48a1e795d8a71e4f873dfd2e9ce94ac6
gleox <gleox@live.com>
2015-12-24 10:18:54 +0800

    Update visual-studio-code to 0.10.6

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '0.10.5'
-  sha256 '0faafafed481652a059f04ab83c00161aed951a2414c6d5ef5363854ec4f0348'
+  version '0.10.6'
+  sha256 '78d333e1e7ae9bffc84fa1c6a0dbc3e8530d85b62c3b318e6687eefec2b3ddf9'
 
   # vo.msecnd.net is the official download host per the vendor homepage
   url "https://az764295.vo.msecnd.net/public/#{version}/VSCode-darwin.zip"
