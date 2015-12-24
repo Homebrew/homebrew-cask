e364c6ec1ace9ba3a565a22e0adbe605110372aa
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:04:01 +0000

    hype.rb: name changed to conform to new rules

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -3,7 +3,7 @@ cask 'hype' do
   sha256 '66d2df2b982aa4634583381046a7c3bbd93b9fe8e7232663d51fdc8bc52358f3'
 
   url "http://static.tumult.com/hype/download/Hype-#{version.sub(%r{^.*?-},'')}.app.zip"
-  name 'Hype'
+  name 'Tumult Hype'
   appcast 'https://tumult.com/hype/appcast_hype2.xml',
           :sha256 => '07aef09835937ee8af2d7938f30d9871faa6f935b36bc93850e021e32ebf6717'
   homepage 'http://tumult.com/hype/'
