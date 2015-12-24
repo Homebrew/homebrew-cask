8b42fdda2eda51d76364b5b1be0b5e68c08114b5
Markus Reiter <me@reitermark.us>
2015-12-24 21:58:25 +0100

    updated plex-media-server (0.9.12.19)

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-server' do
-  version '0.9.12.19.1537-f38ac80'
-  sha256 '047b08823d15013cc8daecbe8ba1da33fd21965aef7ae5be653fe3bb36116b69'
+  version '0.9.12.19'
+  sha256 ''
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   name 'Plex Media Server'
