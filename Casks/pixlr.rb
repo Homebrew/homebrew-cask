447131c28f9300d389c4af76d9a956319c194fd7
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:04:02 +0000

    pixlr.rb: name changed to conform to new rules

--- Casks/pixlr.rb
+++ Casks/pixlr.rb
@@ -3,7 +3,7 @@ cask 'pixlr' do
   sha256 'd4ee65c63fa307da05ba54d641f081c951581976655507efbcd466143a4afa92'
 
   url "https://cdn2.pixlr.com/mac/Autodesk_Pixlr_v#{version}_Mac_Installer.dmg"
-  name 'Pixlr'
+  name 'Autodesk Pixlr'
   appcast 'https://updatefeed.pixlr.com/pixlr-for-mac-appfeed.xml',
           :sha256 => '46d01575b9fd926b62093e9d5614544428f49a5f1013217b92c29b2dbed78966'
   homepage 'https://pixlr.com'
