fe92a052a0adf232b65c30d5128feade37b1c494
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:04:00 +0000

    aqua-data-studio.rb: name changed to conform to new rules

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -3,7 +3,7 @@ cask 'aqua-data-studio' do
   sha256 'cc30e96853ce0231bc1b1d917d6930ab578441371eb64bd56140a501dfb1f9b2'
 
   url "http://www.aquafold.com/download/v16.0.0/osx/ads-osx-#{version}.tar.gz"
-  name 'Aqua Data Studio'
+  name 'Aquafold Aqua Data Studio'
   homepage 'http://www.aquafold.com/aquadatastudio.html'
   license :commercial
 
