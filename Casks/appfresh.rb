fccd55e084736dcf5bbe9b1a9d499a8032dc3592
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:04:00 +0000

    appfresh.rb: name changed to conform to new rules

--- Casks/appfresh.rb
+++ Casks/appfresh.rb
@@ -3,7 +3,7 @@ cask 'appfresh' do
   sha256 'e4d4e719bebf17f0ea5e7336e5da84d032b26dfaefa2f5102250fe9a208674cc'
 
   url "http://backend.metaquark.de/download/appfresh/versions/#{version.sub(%r{.*_},'')}"
-  name 'AppFresh'
+  name 'metaquark AppFresh'
   appcast 'http://backend.metaquark.de/appcast/appfresh.xml'
   homepage 'http://metaquark.de/appfresh/mac'
   license :commercial
