e4df0b9ced7a4cc4f59e34f6e7ecfcc9ad461574
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:04:01 +0000

    heka.rb: name changed to conform to new rules

--- Casks/heka.rb
+++ Casks/heka.rb
@@ -3,7 +3,7 @@ cask 'heka' do
   sha256 '5fef4af06b97bf926fbec7ecb5c7983eed80601d56b2bedac02dad6b8677a094'
 
   url "https://github.com/mozilla-services/heka/releases/download/v#{version}/heka-#{version.gsub('.', '_')}-darwin-amd64.dmg"
-  name 'heka'
+  name 'Mozilla heka'
   homepage 'https://github.com/mozilla-services/heka'
   license :mpl
 
