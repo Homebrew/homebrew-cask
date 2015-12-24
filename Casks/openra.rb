a8c80ed61a3d7b389bf36b2bdf19951cd9f6a552
Victor Popkov <victor@popkov.me>
2015-12-24 23:57:34 +0200

    openra.rb: upgrade to v20151224

--- Casks/openra.rb
+++ Casks/openra.rb
@@ -1,6 +1,6 @@
 cask 'openra' do
-  version '20150919'
-  sha256 '395ab15e92cec263ca6fda3f97b3593fd7e42ab9e7b3ca23f302bc4809605761'
+  version '20151224'
+  sha256 '2c3f43839b6626e5c739d28c332e9f3b03c0ef4b0fd0c76980f3c55044c90dbb'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/OpenRA/OpenRA/releases/download/release-#{version}/OpenRA-release-#{version}.zip"
