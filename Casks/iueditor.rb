f84c1dfbc8f30d231973613d3f806b8efa82bff8
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:04:01 +0000

    iueditor.rb: name changed to conform to new rules

--- Casks/iueditor.rb
+++ Casks/iueditor.rb
@@ -4,7 +4,7 @@ cask 'iueditor' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://iueditor.s3.amazonaws.com/Release/IUEditorV#{version}.zip"
-  name 'IUEditor'
+  name 'JDLab IUEditor'
   homepage 'http://www.iueditor.org'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
