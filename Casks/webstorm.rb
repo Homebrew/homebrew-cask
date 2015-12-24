905d62d879df91da589d39684ee1fa6d24f91652
Michael Ford <fanquake@users.noreply.github.com>
2015-12-24 21:52:54 +0800

    WebStorm 11.0.3

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -1,6 +1,6 @@
 cask 'webstorm' do
-  version '11.0.2'
-  sha256 '43e0352fee07fa5b92dd22e557cb1d050ccde0cf97273e02f694930695b15134'
+  version '11.0.3'
+  sha256 'b868b926cdc8138de1de9a2b6665d532df9cc16c383ee28d124590e5c2ec406f'
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version}-custom-jdk-bundled.dmg"
   name 'WebStorm'
