806df9348de40214f639f7567c694c3fa3ec5241
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:04:00 +0000

    bleep.rb: name changed to conform to new rules

--- Casks/bleep.rb
+++ Casks/bleep.rb
@@ -4,7 +4,7 @@ cask 'bleep' do
 
   # utorrent.com is the official download host per the vendor homepage
   url 'https://download-new.utorrent.com/endpoint/bleep/os/osx/track/stable/'
-  name 'Bleep'
+  name 'BitTorrent Bleep'
   appcast 'https://bleeposx.bittorrent.com/appcast.xml',
           :sha256 => '266fc57b32cbbe67b46513eba330c1986681f767892d97ad66b0a61510d4aaf1'
   homepage 'http://www.bleep.pm/'
