c61996251fad29f2cd96e428853ee283a18550f9
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:04:01 +0000

    gyazo.rb: name changed to conform to new rules

--- Casks/gyazo.rb
+++ Casks/gyazo.rb
@@ -3,8 +3,7 @@ cask 'gyazo' do
   sha256 '08c9b90691267b0d2894d5ee0c37ce7eecb529526d59230e6430ab8c2721aa32'
 
   url "https://files.gyazo.com/setup/Gyazo-#{version}.dmg"
-  name 'Gyazo'
-  name 'Gyazo GIF'
+  name 'Nota Gyazo GIF'
   homepage 'https://gyazo.com/'
   license :other
 
