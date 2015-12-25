c0aa53eaf0d355e09449ab822259053e1cb2c983
dgellow <samuel.elborai@gmail.com>
2015-12-25 23:07:31 +0100

    upgrade VirtualC64 to 1.4

--- Casks/virtualc64.rb
+++ Casks/virtualc64.rb
@@ -1,12 +1,12 @@
 cask 'virtualc64' do
   # note: "64" is not a version number, but an intrinsic part of the product name
-  version '0.9.9.4'
-  sha256 'd5c1ed83a0ec81b45f8879920cbce41f851e593ba476de2cd29a267433a30f85'
+  version '1.4'
+  sha256 'c6b14eb714b7fa6abe8479d2a9b3a117fcbe8eb761756245b27831eed5402b61'
 
   url "http://www.dirkwhoffmann.de/virtualc64/VirtualC64_#{version}.zip"
   name 'Virtual C64'
   appcast 'http://dirkwhoffmann.de/virtualc64/VirtualC64Appcast.xml',
-          :sha256 => '1778a743b3ac853e07236243191d72876ab91f04a5d61088b0690ef205cb3e1b'
+          :sha256 => '817423383d30f9136816aa1cece794f24dd2503a0aca6750779bbad6d5bca2bd'
   homepage 'http://www.dirkwhoffmann.de/virtualc64/'
   license :gpl
 
