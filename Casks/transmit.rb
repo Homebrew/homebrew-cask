ecd7fa5f35d906ba200265bbdc99c00fa1ef561c
Vítor Galvão <info@vitorgalvao.com>
2015-12-25 21:00:26 +0000

    transmit.rb: delete appcast

--- Casks/transmit.rb
+++ Casks/transmit.rb
@@ -3,7 +3,6 @@ cask 'transmit' do
   sha256 '9255bff3e337d5c93e867a553d3889cf97aeecae70304f468648384b1990a257'
 
   url "https://www.panic.com/transmit/d/Transmit%20#{version}.zip"
-  appcast 'http://www.panic.com/updates/update.php'
   name 'Transmit'
   homepage 'http://panic.com/transmit'
   license :commercial
