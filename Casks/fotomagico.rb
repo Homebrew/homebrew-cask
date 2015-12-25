b87fc793c7ed38a37284b49aab30851de17b5365
Vítor Galvão <info@vitorgalvao.com>
2015-12-25 20:29:35 +0000

    fotomagico.rb: delete appcast

--- Casks/fotomagico.rb
+++ Casks/fotomagico.rb
@@ -3,7 +3,6 @@ cask 'fotomagico' do
   sha256 '7b92c77092a8467b1bcc2584e1d98c8e39819da0c3c37eeba53ad6b3e666d27c'
 
   url "https://cdn.boinx.com/software/fotomagico/Boinx_FotoMagico_#{version}.app.zip"
-  appcast 'https://sparkle.boinx.com/appcast.lasso'
   name 'FotoMagico'
   homepage 'http://www.boinx.com/fotomagico/'
   license :commercial
