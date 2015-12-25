08652e0d60ec64a93f1f4e4b534cce9a9f399139
adityadalal924 <github@adityadalal.com>
2015-12-24 20:21:00 -0500

    updated darktable (2.0.0)

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,6 +1,6 @@
 cask 'darktable' do
-  version '1.6.9'
-  sha256 'f79b0c4f317f87aab353c25216f2a3628efa2a072b1ce64c21d075a3dda54e9e'
+  version '2.0.0'
+  sha256 '1019646522c3fde81ce0de905220a88b506c7cec37afe010af7d458980dd08bd'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version}/darktable-#{version}.dmg"
