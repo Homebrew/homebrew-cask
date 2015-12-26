2caa397e32d1979128e75b93811bb8eed57cf16a
Jiajun Wang <amesists@gmail.com>
2015-12-26 16:19:45 +0800

    remove appcast of Manico

--- Casks/manico.rb
+++ Casks/manico.rb
@@ -3,8 +3,6 @@ cask 'manico' do
   sha256 '121458b99fbf3a11bc248e2922e73cd98d03409e343690d280a49903c38c79ce'
 
   url "http://manico.im/static/Manico_#{version}.dmg"
-  appcast 'http://manico.im/static/manico-appcast.xml',
-          :sha256 => 'cbdf604b5078b58c559ed17759f2db444a5ec417bc8e663e024df735bc9d96fb'
   name 'Manico'
   homepage 'http://manico.im/'
   license :commercial
