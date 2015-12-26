08ce71738081e72c8a1049dbbb2b633c57c1061c
Jiajun Wang <amesists@gmail.com>
2015-12-27 01:24:31 +0800

    add new appcast for Manico

--- Casks/manico.rb
+++ Casks/manico.rb
@@ -3,6 +3,8 @@ cask 'manico' do
   sha256 '121458b99fbf3a11bc248e2922e73cd98d03409e343690d280a49903c38c79ce'
 
   url "http://manico.im/static/Manico_#{version}.dmg"
+  appcast 'http://manico.im/static/manico-official-appcast.xml',
+          :sha256 => '6e15e440006e2717e5b0a1457bd0e97ec806fdc5d7398bf206d5a6a343036fb8'
   name 'Manico'
   homepage 'http://manico.im/'
   license :commercial
