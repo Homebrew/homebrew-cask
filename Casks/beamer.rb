939a5f464bc1bceb00da7f11c2fef97ff7dc4661
Victor Popkov <victor@popkov.me>
2015-12-26 23:32:44 +0200

    beamer.rb: upgrade to v3.0.3, update links to use SSL

--- Casks/beamer.rb
+++ Casks/beamer.rb
@@ -1,12 +1,12 @@
 cask 'beamer' do
-  version '3.0.2'
-  sha256 '3f3ce843b579e0b1aa9796bb0a2057b018a5afe0e719f0c143076f037ac194d4'
+  version '3.0.3'
+  sha256 '183a28b096415825e9e22816b25cdfbe4b6a55d53d0e1da89cae8d3a55c26f17'
 
   url "https://beamer-app.com/resources/downloads/Beamer-#{version}.zip"
-  name 'Beamer'
-  appcast 'http://beamer-app.com/beamer3-appcast.xml',
+  appcast 'https://beamer-app.com/beamer3-appcast.xml',
           :sha256 => '3b362b71ab9eaec3f9b0b347dece549b1d3c26ae39a064ce687c9fba3c091f83'
-  homepage 'http://beamer-app.com'
+  name 'Beamer'
+  homepage 'https://beamer-app.com/'
   license :commercial
 
   app 'Beamer.app'
