2d190e646e365fa4dd5f8695a27529295446712c
Steven Wallace <steven@thewallaces.ca>
2015-12-26 13:28:22 -0800

    Changed eve-online to eve-launcher

--- Casks/eve-launcher.rb
+++ Casks/eve-launcher.rb
@@ -1,11 +1,11 @@
-cask 'eve-online' do
+cask 'eve-launcher' do
   version '992878'
   sha256 '5c9a4ef2caedfaf2f5c109b323727cf14c45576d51c343489de4e2dba32cb945'
 
   url "http://binaries.eveonline.com/EveLauncher-#{version}.dmg"
   name 'Eve Online'
   homepage 'https://www.eveonline.com/'
-  license :closed
+  license :commercial
 
   app 'EVE Launcher.app'
 
