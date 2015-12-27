88e52c804a5467816ef06ee7a9becffe3e0732cc
Jason Gilman <axodys@gmail.com>
2015-12-27 09:48:50 -0800

    Upgrade Twine.app to 2.0.10
    bitbucket.org is now the official download host per vendor homepage.

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -2,6 +2,7 @@ cask 'twine' do
   version '2.0.10'
   sha256 '3d073a624bd56817438105732e0e808b2ae9ff5bfebb455b70133cff80b0134f'
 
+  # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/klembot/twinejs/downloads/twine_#{version}_osx.zip"
   name 'Twine'
   homepage 'http://twinery.org/'
