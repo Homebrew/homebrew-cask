ac31a721c15666efcd3d4c542be3a3edf151ab0c
Vítor Galvão <info@vitorgalvao.com>
2015-12-28 01:13:05 +0000

    bassjump.rb: remove name from conditional

--- Casks/bassjump.rb
+++ Casks/bassjump.rb
@@ -5,7 +5,6 @@ cask 'bassjump' do
 
     # rackcdn.com is the official download host per the vendor homepage
     url "http://ffe82a399885f9f28605-66638985576304cbe11c530b9b932f18.r24.cf2.rackcdn.com/BassJumpSoundSystem-#{version}-249-ML.mpkg.zip"
-    name 'BassJump'
 
     pkg "BassJump Sound System-#{version}-249-ML.mpkg"
   else
@@ -19,6 +18,7 @@ cask 'bassjump' do
     pkg 'BassJumpInstaller.pkg'
   end
 
+  name 'BassJump'
   homepage 'https://www.twelvesouth.com/product/bassjump-2-for-macbook'
   license :gratis
 
