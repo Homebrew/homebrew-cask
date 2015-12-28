3976beea96d3e0d1a5ee40ef5adca185167e0259
Vítor Galvão <info@vitorgalvao.com>
2015-12-28 00:43:04 +0000

    shimo.rb: remove appcast

--- Casks/shimo.rb
+++ Casks/shimo.rb
@@ -3,7 +3,6 @@ cask 'shimo' do
   sha256 'e413fedd9dcb0161986433eb26599de07428ef1a2c443b0e9448bb8bc86af504'
 
   url "https://www.feingeist.io/fg-library/appcasts/Shimo4/Shimo_#{version}.zip"
-  appcast 'http://www.feingeist.io/fg-library/appcast.php'
   name 'Shimo'
   homepage 'https://www.feingeist.io/shimo/'
   license :commercial
