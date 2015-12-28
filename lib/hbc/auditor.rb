da047832865ea4c05b15aa38303cf15d98589908
Josh Hagins <hagins.josh@gmail.com>
2015-12-28 01:41:14 -0500

    Fix audit bug introduced in #16166

--- lib/hbc/auditor.rb
+++ lib/hbc/auditor.rb
@@ -1,6 +1,6 @@
 class Hbc::Auditor
   def self.audit(cask, options = {})
-    download = options.fetch(:audit_download, false)
+    download = options.fetch(:audit_download, false) && Hbc::Download.new(cask)
     audit = Hbc::Audit.new(cask, download)
     audit.run!
     puts audit.summary
