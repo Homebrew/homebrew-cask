6267d212d823a736bc2916bcf0c411e8994ad460
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-28 09:45:57 -0800

    updated vimediamanager to v0.7a14c

--- Casks/vimediamanager.rb
+++ Casks/vimediamanager.rb
@@ -1,11 +1,11 @@
 cask 'vimediamanager' do
-  version '0.7a13c'
-  sha256 'da5a1f76d556bbdb4cff76295cd0e60a47d05f6073b35dae20a88387f4eb10ce'
+  version '0.7a14c'
+  sha256 '2fb3dc8b5d2c5ab40a34e18c0860da98d1b92bac673f8a134bfca82cf013714a'
 
   url "http://mariusth.channelwood.org/vimediamanager/files/vimediamanager_v#{version}.dmg"
   name 'ViMediaManager'
   appcast 'http://mariusth.heliohost.org/vimediamanager/appcast-cocoa.xml',
-          :sha256 => '595974a863e2502ca4cc038cf6f234d4e2606c93dd862bdfeacb50338fb6bf34'
+          :sha256 => '79c39ee503db054d15de98571385729eee7f9a2392ec14b54a2ac2e85a381992'
   homepage 'http://mariusth.heliohost.org/vimediamanager/'
   license :gratis
 
