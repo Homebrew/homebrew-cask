0fa35b62458e554db63b3be4e66f218b014b5402
Vítor Galvão <info@vitorgalvao.com>
2015-12-28 16:25:51 +0000

    bettertouchtool.rb: corrected stanza order

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -1,5 +1,4 @@
 cask 'bettertouchtool' do
-
   if MacOS.release <= :snow_leopard
     version '0.939'
     sha256 'fad5e9d36259c379bdb33188cf15d179fd9ff73023035c98f5734e7e3e13bb75'
@@ -9,20 +8,20 @@ cask 'bettertouchtool' do
   else
     version '1.47d'
     sha256 '81b15b17a13018e48edb796f972732d379f22b9f0601300899fea3d77162bc5e'
+
     url "http://boastr.net/releases/btt#{version}.zip"
+    appcast 'http://appcast.boastr.net',
+            :sha256 => 'd22e6a9b68ea9d83446ee27e24023fd734cb258b56f01162db52a0880bba6ef1'
   end
 
-  appcast 'http://appcast.boastr.net',
-          :sha256 => 'd22e6a9b68ea9d83446ee27e24023fd734cb258b56f01162db52a0880bba6ef1'
   name 'BetterTouchTool'
   homepage 'http://boastr.net/'
   license :gratis
 
   auto_updates true
+  accessibility_access true
 
   app 'BetterTouchTool.app'
-
-  accessibility_access true
 
   uninstall :login_item => 'BetterTouchTool'
 
