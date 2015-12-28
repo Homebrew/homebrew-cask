2cc5af5d78057978a69d17fb8aab74db72cce422
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-28 13:37:02 -0800

    updated texpad to v1.7.21 and cleaned up caskfile
    - No need for url to be duplicated
    - Order of stanzas

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -1,26 +1,23 @@
 cask 'texpad' do
-
   if MacOS.release >= :mavericks
-    version '1.7.19'
-    sha256 '7caba1e52fda572f32944fc52d37c2b1e2b0fb3b6794c930bc7cf247dd825316'
-    url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.gsub('.','_')}.zip"
+    version '1.7.21'
+    sha256 'cae312b89d25752917bf66572fd1c6b6722097b651abe9a1a1b399dc36493512'
   elsif MacOS.release <= :mountain_lion && MacOS.release >= :snow_leopard
     version '1.6.14'
     sha256 '18fcbe93e77e5b5bc848172546962fcde397a26fd543efcc1054004369192f7e'
-    url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.gsub('.','_')}.zip"
   else
     # If the app is used on MacOS lower than Snow Leopard,
     # unexpected behaviour or failures can occur.
   end
 
-  depends_on :macos => '>= :snow_leopard'
-
+  url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.gsub('.','_')}.zip"
   appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml',
-          :sha256 => 'e78733fb9529330962a6392283b7fdfc533adcf730cf66fbadb6d0445f2816a4'
-
+          :sha256 => 'd16b1bb80a8c38d6b82b26eb629cad0deaa15882a08db5d17b3484d63cd3e451'
   name 'Texpad'
   homepage 'https://www.texpadapp.com/osx'
   license :commercial
+
+  depends_on :macos => '>= :snow_leopard'
 
   app 'Texpad.app'
 end
