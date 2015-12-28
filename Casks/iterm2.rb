c85752f0b722335c3ef32e53546d36f14b57b665
Vítor Galvão <info@vitorgalvao.com>
2015-12-28 16:25:51 +0000

    iterm2.rb: corrected stanza order

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -4,10 +4,10 @@ cask 'iterm2' do
   sha256 '1062b83e7808dc1e13362f4a83ef770e1c24ea4ae090d1346b49f6196e9064cd'
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.gsub('.','_')}.zip"
-  name 'iTerm2'
-  homepage 'https://www.iterm2.com/'
   appcast 'https://iterm2.com/appcasts/final.xml',
           :sha256 => 'dbe44305b0b7e620fb020a60d6ca78042144a8e04b4b4f2fc3cec84e0df9fc44'
+  name 'iTerm2'
+  homepage 'https://www.iterm2.com/'
   license :gpl
 
   auto_updates true
