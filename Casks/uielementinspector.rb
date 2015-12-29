ff1b506787b427be860583ef513c26e2acecc414
Steven Willis <onlynone@gmail.com>
2015-12-29 16:17:10 -0500

    Add UIElementInspector.app v1.4

---
+++ Casks/uielementinspector.rb
@@ -0,0 +1,11 @@
+cask 'uielementinspector' do
+  version '1.4'
+  sha256 'd74693ca4a3bf410146de7f534fae92f5c7f6d618191b1e0e7288e1ad4116d97'
+
+  url 'http://developer.apple.com/library/mac/samplecode/UIElementInspector/UIElementInspector.zip'
+  name 'UIElementInspector'
+  homepage 'https://developer.apple.com/library/mac/samplecode/UIElementInspector/Introduction/Intro.html'
+  license :gratis
+
+  app 'UIElementInspector/UIElementInspector.app'
+end
