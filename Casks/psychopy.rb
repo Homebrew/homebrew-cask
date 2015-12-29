d871f10cdd6facc2c89b37c8887ca10cfbe52bf0
Chris Coleman <rot26@users.noreply.github.com>
2015-12-28 18:30:08 -0700

    corrected reference to version variable

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -2,7 +2,7 @@ cask 'psychopy' do
   version '1.83.03'
   sha256 'bfb5d1d07e7df089d3056e6f4fd9ff6847628d4ed7a24f508f7c79190d1bc753'
 
-  url "https://github.com/psychopy/psychopy/releases/download/1.83.03/StandalonePsychoPy-#{version}-OSX_64bit.dmg"
+  url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy-#{version}-OSX_64bit.dmg"
   name 'PsychoPy'
   homepage 'http://www.psychopy.org/'
   license :oss
