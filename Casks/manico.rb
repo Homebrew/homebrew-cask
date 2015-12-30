458a5650be5a73b4a82c36c9e1ceeac180abf333
Victor Popkov <victor@popkov.me>
2015-12-30 19:49:45 +0200

    manico.rb: upgrade to v2.0.1

--- Casks/manico.rb
+++ Casks/manico.rb
@@ -1,10 +1,10 @@
 cask 'manico' do
-  version '2.0'
-  sha256 '121458b99fbf3a11bc248e2922e73cd98d03409e343690d280a49903c38c79ce'
+  version '2.0.1'
+  sha256 '14171ca3d2b0dcebdbc63f2c3b905f54b1134455d835f3ba1dfebeca8bfa0f1b'
 
   url "http://manico.im/static/Manico_#{version}.dmg"
   appcast 'http://manico.im/static/manico-official-appcast.xml',
-          :sha256 => '6e15e440006e2717e5b0a1457bd0e97ec806fdc5d7398bf206d5a6a343036fb8'
+          :sha256 => '0a471a6f997a468135c993ac3e625227854c99c512f5a78460174436d15d0284'
   name 'Manico'
   homepage 'http://manico.im/'
   license :commercial
