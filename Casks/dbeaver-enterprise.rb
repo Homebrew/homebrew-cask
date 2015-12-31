94a72562cb42a50a110addcfcf55df8c1efcb5a6
Alberto A. Colon Viera (Albertico) <aacv@alberti.co>
2015-12-31 02:30:17 -0400

    Upgrade DBeaver-Enterprise to v3.5.8

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-enterprise' do
-  version '3.5.6'
-  sha256 '94af6166cc8624ca2e099a9382025340db25069d97f5dd023a036b3af75a9817'
+  version '3.5.8'
+  sha256 '08a8a2d419c2a6403fe485c43054d9aafd4c983d67a7029eb7d78bb7ec8c6ed6'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   name 'DBeaver Enterprise Edition'
