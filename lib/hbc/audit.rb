49fc58b76d39b3d96e37d21e7b7abe00c898154c
Vítor Galvão <info@vitorgalvao.com>
2015-12-31 14:38:39 +0000

    audit: made check for bad url formats more generic

--- lib/hbc/audit.rb
+++ lib/hbc/audit.rb
@@ -113,9 +113,13 @@ class Hbc::Audit
     end
   end
 
+  def bad_url_format?(regex, valid_formats_array)
+    return false unless cask.url.to_s =~ regex
+    valid_formats_array.none? { |format| cask.url.to_s =~ format }
+  end
+
   def bad_sourceforge_url?
-    return false unless cask.url.to_s =~ /sourceforge/
-    valid_url_formats = [
+    bad_url_format?(/sourceforge/, [
       %r{\Ahttps?://sourceforge\.net/projects/[^/]+/files/latest/download\Z},
       %r{\Ahttps?://downloads\.sourceforge\.net/},
       %r{\Ahttps?://[^/]+\.sourceforge\.jp/},
@@ -123,15 +127,12 @@ class Hbc::Audit
       %r{\Ahttps?://brushviewer\.sourceforge\.net/brushviewql\.zip\Z},
       %r{\Ahttps?://doublecommand\.sourceforge\.net/files/},
       %r{\Ahttps?://excalibur\.sourceforge\.net/get\.php\?id=},
-    ]
-    valid_url_formats.none? { |format| cask.url.to_s =~ format }
+    ])
   end
 
   def bad_osdn_url?
-    return false unless cask.url.to_s =~ /osdn/
-    valid_url_formats = [
+    bad_url_format?(/osd/, [
       %r{\Ahttps?://[^/]+\.osdn\.jp/},
-    ]
-    valid_url_formats.none? { |format| cask.url.to_s =~ format }
+    ])
   end
 end
