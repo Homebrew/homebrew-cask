59ca63143c271e905ddff8be1da39f742931ea4f
miccal <miccal.matthews@gmail.com>
2016-01-02 05:15:08 +0800

    Fix shasum Ipe 7.2.1

--- Casks/ipe.rb
+++ Casks/ipe.rb
@@ -1,6 +1,6 @@
 cask 'ipe' do
   version '7.2.1'
-  sha256 'd55877d6162bb49c27219ca5b299f9ce598c0600b61cec4c555a7b1f6cdc2e5f'
+  sha256 '5c7620898eadfe05ee7e5ec5ef43439ee41f8aaf1b3babaf128ca4396dc41009'
 
   # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/otfried/generic/ipe/#{version.to_f}/ipe-#{version}-mac.dmg"
