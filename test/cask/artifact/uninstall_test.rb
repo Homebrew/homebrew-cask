ba88ea2a70a1c06ecd1e0693e0834a7f3430218e
Josh Hagins <hagins.josh@gmail.com>
2016-01-02 18:37:10 -0500

    Clean up uninstall_test.rb

--- test/cask/artifact/uninstall_test.rb
+++ test/cask/artifact/uninstall_test.rb
@@ -1,16 +1,17 @@
 require 'test_helper'
 
 describe Hbc::Artifact::Uninstall do
+  let(:cask) { Hbc.load('with-installable') }
+  let(:uninstall_artifact) { Hbc::Artifact::Uninstall.new(cask, Hbc::FakeSystemCommand) }
+
   before {
-    @cask = Hbc.load('with-installable')
     shutup do
-      TestHelper.install_without_artifacts(@cask)
+      TestHelper.install_without_artifacts(cask)
     end
   }
 
   describe 'install_phase' do
     it 'does nothing, because the install_phase method is a no-op' do
-      uninstall_artifact = Hbc::Artifact::Uninstall.new(@cask, Hbc::FakeSystemCommand)
       shutup do
         uninstall_artifact.install_phase
       end
@@ -19,7 +20,6 @@ describe Hbc::Artifact::Uninstall do
 
   describe 'zap_phase' do
     it 'does nothing, because the zap_phase method is a no-op' do
-      uninstall_artifact = Hbc::Artifact::Uninstall.new(@cask, Hbc::FakeSystemCommand)
       shutup do
         uninstall_artifact.zap_phase
       end
@@ -27,168 +27,166 @@ describe Hbc::Artifact::Uninstall do
   end
 
   describe 'uninstall_phase' do
+    subject {
+      shutup do
+        uninstall_artifact.uninstall_phase
+      end
+    }
     # todo: uninstall tests for :signal (implementation does not use SystemCommand)
     it 'runs the specified uninstaller for the Cask' do
-      uninstall_artifact = Hbc::Artifact::Uninstall.new(@cask, Hbc::FakeSystemCommand)
-
-      Hbc::FakeSystemCommand.stubs_command(['/usr/bin/sudo', '-E', '--', '/usr/bin/osascript', '-e', 'tell application "System Events" to count processes whose bundle identifier is "my.fancy.package.app"'], '1')
-      Hbc::FakeSystemCommand.stubs_command(['/usr/bin/sudo', '-E', '--', '/usr/bin/osascript', '-e', 'tell application id "my.fancy.package.app" to quit'])
+      Hbc::FakeSystemCommand.stubs_command(sudo(['/usr/bin/osascript', '-e', 'tell application "System Events" to count processes whose bundle identifier is "my.fancy.package.app"']), '1')
+      Hbc::FakeSystemCommand.stubs_command(sudo(['/usr/bin/osascript', '-e', 'tell application id "my.fancy.package.app" to quit']))
 
       Hbc::FakeSystemCommand.expects_command(['/usr/bin/osascript', '-e', 'tell application "System Events" to delete every login item whose name is "Fancy"'])
 
-      Hbc::FakeSystemCommand.expects_command(['/usr/bin/sudo', '-E', '--', @cask.staged_path.join('MyFancyPkg','FancyUninstaller.tool'), '--please'])
-      Hbc::FakeSystemCommand.expects_command(['/usr/bin/sudo', '-E', '--', '/bin/rm', '-rf', '--', Pathname.new('/permissible/absolute/path'), Pathname.new('~/permissible/path/with/tilde').expand_path])
-      Hbc::FakeSystemCommand.expects_command(['/usr/bin/sudo', '-E', '--', '/bin/rm', '-f', '--', Pathname.new(TestHelper.local_binary_path('empty_directory')).join('.DS_Store')])
-      Hbc::FakeSystemCommand.expects_command(['/usr/bin/sudo', '-E', '--', '/bin/rmdir', '--', Pathname.new(TestHelper.local_binary_path('empty_directory'))])
+      Hbc::FakeSystemCommand.expects_command(sudo([cask.staged_path.join('MyFancyPkg','FancyUninstaller.tool'), '--please']))
+      Hbc::FakeSystemCommand.expects_command(sudo(['/bin/rm', '-rf', '--', Pathname.new('/permissible/absolute/path'), Pathname.new('~/permissible/path/with/tilde').expand_path]))
+      Hbc::FakeSystemCommand.expects_command(sudo(['/bin/rm', '-f', '--', Pathname.new(TestHelper.local_binary_path('empty_directory')).join('.DS_Store')]))
+      Hbc::FakeSystemCommand.expects_command(sudo(['/bin/rmdir', '--', Pathname.new(TestHelper.local_binary_path('empty_directory'))]))
 
-      shutup do
-        uninstall_artifact.uninstall_phase
-      end
+      subject
     end
 
-    it 'can uninstall using pkgutil, launchctl, and file lists' do
-      cask = Hbc.load('with-pkgutil-uninstall')
-      uninstall_artifact = Hbc::Artifact::Uninstall.new(cask, Hbc::FakeSystemCommand)
+    describe 'when using pkgutil, launchutil, and kext' do
+      let(:cask) { Hbc.load('with-pkgutil-uninstall') }
 
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--pkgs=my.fancy.package.*'],
-        [
-          'my.fancy.package.main',
-          'my.fancy.package.agent',
-        ].join("\n")
-      )
+      it 'can uninstall' do
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--pkgs=my.fancy.package.*'],
+          <<-FILES.undent
+            my.fancy.package.main
+            my.fancy.package.agent
+          FILES
+        )
 
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--only-files', '--files', 'my.fancy.package.main'],
-        [
-          'fancy/bin/fancy.exe',
-          'fancy/var/fancy.data',
-        ].join("\n")
-      )
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--only-dirs', '--files', 'my.fancy.package.main'],
-        [
-          'fancy',
-          'fancy/bin',
-          'fancy/var',
-        ].join("\n")
-      )
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--files', 'my.fancy.package.main'],
-        [
-          'fancy',
-          'fancy/bin',
-          'fancy/var',
-          'fancy/bin/fancy.exe',
-          'fancy/var/fancy.data',
-        ].join("\n")
-      )
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--pkg-info-plist', 'my.fancy.package.main'],
-        <<-PLIST
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-        <key>install-location</key>
-        <string>tmp</string>
-        <key>volume</key>
-        <string>/</string>
-</dict>
-</plist>
-        PLIST
-      )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--only-files', '--files', 'my.fancy.package.main'],
+          <<-FILES.undent
+            fancy/bin/fancy.exe
+            fancy/var/fancy.data
+          FILES
+        )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--only-dirs', '--files', 'my.fancy.package.main'],
+          <<-FILES.undent
+            fancy
+            fancy/bin
+            fancy/var
+          FILES
+        )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--files', 'my.fancy.package.main'],
+          <<-FILES.undent
+            fancy
+            fancy/bin
+            fancy/var
+            fancy/bin/fancy.exe
+            fancy/var/fancy.data
+          FILES
+        )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--pkg-info-plist', 'my.fancy.package.main'],
+          <<-PLIST.undent
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+            <plist version="1.0">
+            <dict>
+                    <key>install-location</key>
+                    <string>tmp</string>
+                    <key>volume</key>
+                    <string>/</string>
+            </dict>
+            </plist>
+          PLIST
+        )
 
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/bin/launchctl', 'list', 'my.fancy.package.service'],
-        "launchctl list returned unknown response\n"
-      )
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/bin/sudo', '-E', '--', '/bin/launchctl', 'list', 'my.fancy.package.service'],
-        <<-"PLIST"
-{
-        "LimitLoadToSessionType" = "Aqua";
-        "Label" = "my.fancy.package.service";
-        "TimeOut" = 30;
-        "OnDemand" = true;
-        "LastExitStatus" = 0;
-        "ProgramArguments" = (
-                "argument";
-        );
-};
-        PLIST
-      )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/bin/launchctl', 'list', 'my.fancy.package.service'],
+          "launchctl list returned unknown response\n"
+        )
+        Hbc::FakeSystemCommand.stubs_command(
+          sudo(['/bin/launchctl', 'list', 'my.fancy.package.service']),
+          <<-PLIST.undent
+            {
+                    "LimitLoadToSessionType" = "Aqua";
+                    "Label" = "my.fancy.package.service";
+                    "TimeOut" = 30;
+                    "OnDemand" = true;
+                    "LastExitStatus" = 0;
+                    "ProgramArguments" = (
+                            "argument";
+                    );
+            };
+          PLIST
+        )
 
-      Hbc::FakeSystemCommand.expects_command(['/usr/bin/sudo', '-E', '--', '/bin/launchctl', 'remove', 'my.fancy.package.service'])
+        Hbc::FakeSystemCommand.expects_command(sudo(['/bin/launchctl', 'remove', 'my.fancy.package.service']))
 
-      Hbc::FakeSystemCommand.stubs_command(['/usr/bin/sudo', '-E', '--', '/usr/sbin/kextstat', '-l', '-b', 'my.fancy.package.kernelextension'], 'loaded')
-      Hbc::FakeSystemCommand.expects_command(['/usr/bin/sudo', '-E', '--', '/sbin/kextunload', '-b', 'my.fancy.package.kernelextension'])
-      Hbc::FakeSystemCommand.stubs_command(['/usr/bin/sudo', '-E', '--', '/usr/sbin/pkgutil', '--forget', 'my.fancy.package.main'])
+        Hbc::FakeSystemCommand.stubs_command(sudo(['/usr/sbin/kextstat', '-l', '-b', 'my.fancy.package.kernelextension']), 'loaded')
+        Hbc::FakeSystemCommand.expects_command(sudo(['/sbin/kextunload', '-b', 'my.fancy.package.kernelextension']))
+        Hbc::FakeSystemCommand.stubs_command(sudo(['/usr/sbin/pkgutil', '--forget', 'my.fancy.package.main']))
 
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--only-files', '--files', 'my.fancy.package.agent'],
-        [
-          'fancy/agent/fancy-agent.exe',
-          'fancy/agent/fancy-agent.pid',
-          'fancy/agent/fancy-agent.log',
-        ].join("\n")
-      )
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--only-dirs', '--files', 'my.fancy.package.agent'],
-        [
-          'fancy',
-          'fancy/agent',
-        ].join("\n")
-      )
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--files', 'my.fancy.package.agent'],
-        [
-          'fancy',
-          'fancy/agent',
-          'fancy/agent/fancy-agent.exe',
-          'fancy/agent/fancy-agent.pid',
-          'fancy/agent/fancy-agent.log',
-        ].join("\n")
-      )
-      Hbc::FakeSystemCommand.stubs_command(
-        ['/usr/sbin/pkgutil', '--pkg-info-plist', 'my.fancy.package.agent'],
-        <<-PLIST
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-        <key>install-location</key>
-        <string>tmp</string>
-        <key>volume</key>
-        <string>/</string>
-</dict>
-</plist>
-        PLIST
-      )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--only-files', '--files', 'my.fancy.package.agent'],
+          <<-FILES.undent
+            fancy/agent/fancy-agent.exe
+            fancy/agent/fancy-agent.pid
+            fancy/agent/fancy-agent.log
+          FILES
+        )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--only-dirs', '--files', 'my.fancy.package.agent'],
+          <<-FILES.undent
+            fancy
+            fancy/agent
+          FILES
+        )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--files', 'my.fancy.package.agent'],
+          <<-FILES.undent
+            fancy
+            fancy/agent
+            fancy/agent/fancy-agent.exe
+            fancy/agent/fancy-agent.pid
+            fancy/agent/fancy-agent.log
+          FILES
+        )
+        Hbc::FakeSystemCommand.stubs_command(
+          ['/usr/sbin/pkgutil', '--pkg-info-plist', 'my.fancy.package.agent'],
+          <<-PLIST.undent
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+            <plist version="1.0">
+            <dict>
+                    <key>install-location</key>
+                    <string>tmp</string>
+                    <key>volume</key>
+                    <string>/</string>
+            </dict>
+            </plist>
+          PLIST
+        )
 
-      %w[
-        /tmp/fancy
-        /tmp/fancy/agent
-        /tmp/fancy/bin
-        /tmp/fancy/var
-      ].each do |dir|
-        Hbc::FakeSystemCommand.stubs_command(['/usr/bin/sudo', '-E', '--', '/bin/chmod', '--', '777', '#{dir}'])
-      end
+        %w[
+          /tmp/fancy
+          /tmp/fancy/agent
+          /tmp/fancy/bin
+          /tmp/fancy/var
+        ].each do |dir|
+          Hbc::FakeSystemCommand.stubs_command(sudo(['/bin/chmod', '--', '777', '#{dir}']))
+        end
 
-      Hbc::FakeSystemCommand.stubs_command(['/usr/bin/sudo', '-E', '--', '/usr/sbin/pkgutil', '--forget', 'my.fancy.package.agent'])
+        Hbc::FakeSystemCommand.stubs_command(sudo(['/usr/sbin/pkgutil', '--forget', 'my.fancy.package.agent']))
 
-      Hbc::FakeSystemCommand.stubs_command(['/usr/bin/sudo', '-E', '--', '/bin/rm', '-f', '--',
-                                             Pathname.new('/tmp/fancy/bin/fancy.exe'),
-                                             Pathname.new('/tmp/fancy/var/fancy.data')])
-      Hbc::FakeSystemCommand.stubs_command(['/usr/bin/sudo', '-E', '--', '/bin/rm', '-f', '--',
-                                             Pathname.new('/tmp/fancy/agent/fancy-agent.exe'),
-                                             Pathname.new('/tmp/fancy/agent/fancy-agent.pid'),
-                                             Pathname.new('/tmp/fancy/agent/fancy-agent.log')])
+        Hbc::FakeSystemCommand.stubs_command(sudo(['/bin/rm', '-f', '--',
+                                               Pathname.new('/tmp/fancy/bin/fancy.exe'),
+                                               Pathname.new('/tmp/fancy/var/fancy.data')]))
+        Hbc::FakeSystemCommand.stubs_command(sudo(['/bin/rm', '-f', '--',
+                                               Pathname.new('/tmp/fancy/agent/fancy-agent.exe'),
+                                               Pathname.new('/tmp/fancy/agent/fancy-agent.pid'),
+                                               Pathname.new('/tmp/fancy/agent/fancy-agent.log')]))
 
-      # No assertions after call since all assertions are implicit from the interactions setup above.
-      # TODO: verify rmdir commands (requires setting up actual file tree or faking out .exists?
-      shutup do
-        uninstall_artifact.uninstall_phase
+        subject
       end
     end
   end
