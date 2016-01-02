bc9f0dd8b8f358a04dbf190a6d81a81eb5cf328f
Carlos Precioso <kalprestito@gmail.com>
2016-01-02 14:36:23 +0100

    Update to 3.8.0v4

--- Casks/sqlitebrowser.rb
+++ Casks/sqlitebrowser.rb
@@ -1,11 +1,11 @@
 cask 'sqlitebrowser' do
-  version '3.7.0'
-  sha256 '421dc0d64a61f393bee6a482bc32fbdce4b4cdab6f5be10e9214624604079bbb'
+  version '3.8.0v4'
+  sha256 '95ef39263a7923dbfb7e659a52cbe6a51eb4139396536d5919864b3b8e1660a5'
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version}/sqlitebrowser-#{version}.dmg"
+  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version.major_minor_patch}/sqlitebrowser-#{version}.dmg"
   appcast 'https://github.com/sqlitebrowser/sqlitebrowser/releases.atom',
-          :sha256 => '650c126c7a2ffb3b5d0dbe378c1f592bfe24f4c541ead3a12828d0b2d7ea2144'
+          :sha256 => '3cc0f910259b1dd0eca32998a46a8832b594dec1dadbaa7c6d03a1c01ebcc349'
   name 'DB Browser for SQLite'
   name 'SQLite Database Browser'
   homepage 'http://sqlitebrowser.org'
