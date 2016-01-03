2fa38859e9802dd2bf4a49f7061c16c91de80483
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:36 -0500

    instabridge.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/instabridge.rb
+++ Casks/instabridge.rb
@@ -3,9 +3,9 @@ cask 'instabridge' do
   sha256 '7f316d42b649eb8d5dcde590540de4c135554026144577c680214b4e082b0a50'
 
   url "http://cdn.instabridge.com/mac/Instabridge-#{version}.zip"
-  name 'Instabridge'
   appcast 'http://cdn.instabridge.com/mac/updates.xml',
           :sha256 => '9974b9ea1d1701dd12aa42235de7bf2b4b2f1966c92e435ec92eae53e241c058'
+  name 'Instabridge'
   homepage 'https://instabridge.com/'
   license :gratis
 
