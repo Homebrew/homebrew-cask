2a7eed1eb83d02cd33c66176a156ed4216c1c1ab
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:27 -0500

    goofy.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/goofy.rb
+++ Casks/goofy.rb
@@ -4,9 +4,9 @@ cask 'goofy' do
 
   # github.com is the official download host per the appcast feed
   url "https://github.com/danielbuechele/goofy/releases/download/v#{version}/Goofy.app.zip"
-  name 'Goofy'
   appcast 'https://github.com/danielbuechele/goofy/releases.atom',
           :sha256 => 'fb33fcb13e310a06a718c63cffc9ef70f18859d4c058d3714dd5a50c56080e91'
+  name 'Goofy'
   homepage 'http://www.goofyapp.com/'
   license :mit
 
