a927bc988c7f6791ada467c088bd9d8a9ef64c69
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:20:01 -0500

    yummy-ftp.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/yummy-ftp.rb
+++ Casks/yummy-ftp.rb
@@ -3,9 +3,9 @@ cask 'yummy-ftp' do
   sha256 :no_check
 
   url 'http://www.yummysoftware.com/downloads/YummyFTP.zip'
-  name 'Yummy FTP'
   appcast 'http://www.yummysoftware.com/su/yummyftp/feed.xml',
           :sha256 => 'fac71ab836dffdc1ae5b0b4acb03d63d2d93fb2639dd38d57b32d691a08d1ea7'
+  name 'Yummy FTP'
   homepage 'http://www.yummysoftware.com'
   license :commercial
 
