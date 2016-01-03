f11cd3e48f2ee2fd90fee8fc9b09fe08a9d4d455
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:46 -0500

    app-tamer.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/app-tamer.rb
+++ Casks/app-tamer.rb
@@ -3,9 +3,9 @@ cask 'app-tamer' do
   sha256 'a8786527526f3b9e21975f70d93c0538b017b60667a29c15291ae62df2d74168'
 
   url "https://www.stclairsoft.com/download/AppTamer-#{version}.dmg"
-  name 'AppTamer'
   appcast 'https://www.stclairsoft.com/cgi-bin/sparkle.cgi?AT',
           :sha256 => 'c60e74c3da756c958bc961ad711759d7904473e75d26e0df410e2e83c34268ae'
+  name 'AppTamer'
   homepage 'https://www.stclairsoft.com/AppTamer/'
   license :commercial
 
