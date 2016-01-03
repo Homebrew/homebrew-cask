03ca96f8d86bf3829616d6619b37e4e8f4a7e239
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:16 -0500

    espionage.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/espionage.rb
+++ Casks/espionage.rb
@@ -3,13 +3,13 @@ cask 'espionage' do
   sha256 :no_check
 
   url 'https://www.espionageapp.com/Espionage.dmg'
-  gpg "#{url}.sig",
-      :key_url => 'https://www.taoeffect.com/other/A884B988.asc'
-  name 'Espionage'
   appcast 'https://updates.taoeffect.com/espionage3/appcast.xml',
           :sha256 => '47bca65264d0dce5ac4f065c2f6312727123a5b6cb83e5d1bfdedea2a0b3a80e'
+  name 'Espionage'
   homepage 'https://www.espionageapp.com/'
   license :commercial
+  gpg "#{url}.sig",
+      :key_url => 'https://www.taoeffect.com/other/A884B988.asc'
 
   app 'Espionage.app'
 end
