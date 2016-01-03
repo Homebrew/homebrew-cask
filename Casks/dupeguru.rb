1a0a3e8f7733cc41d797819d222ce55910b6fc24
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:12 -0500

    dupeguru.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/dupeguru.rb
+++ Casks/dupeguru.rb
@@ -3,9 +3,9 @@ cask 'dupeguru' do
   sha256 '844a929ebb60f3a43a466d3ce3e5ac25b1a79c1a81ae63e19bd9356391589d56'
 
   url "https://download.hardcoded.net/dupeguru_osx_#{version.gsub('.', '_')}.dmg"
-  name 'dupeGuru'
   appcast 'https://www.hardcoded.net/updates/dupeguru.appcast',
           :sha256 => '3abf01d99fb21c356cb439132cab9c734f6d514b4a19e7fcc1b4c408fcd08dbb'
+  name 'dupeGuru'
   homepage 'https://www.hardcoded.net/dupeguru/'
   license :bsd
 
