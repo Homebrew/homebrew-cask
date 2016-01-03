1dbef1b776d38963af7f924a0483f54168736c94
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:01 -0500

    cmake.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -2,10 +2,12 @@ cask 'cmake' do
   version '3.4.1'
   sha256 '3c6d84b32d3e787a7161244db310f36441c015f6f39345887d65cacbcfb9107d'
 
-  url "https://www.cmake.org/files/v#{version.sub(%r{\.\d+$},'')}/cmake-#{version}-Darwin-x86_64.dmg"
+  url "https://www.cmake.org/files/v#{version.sub(%r{\.\d+$}, '')}/cmake-#{version}-Darwin-x86_64.dmg"
   name 'CMake'
   homepage 'https://cmake.org'
   license :bsd
+
+  conflicts_with :formula => 'cmake'
 
   app 'CMake.app'
   binary 'CMake.app/Contents/bin/cmake'
@@ -14,6 +16,4 @@ cask 'cmake' do
   binary 'CMake.app/Contents/bin/cpack'
   binary 'CMake.app/Contents/bin/ctest'
   binary 'CMake.app/Contents/bin/cmake-gui'
-
-  conflicts_with :formula => 'cmake'
 end
