b1f709dbf1619c0702544ab163e8698d46565516
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:41 -0500

    jaksta.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/jaksta.rb
+++ Casks/jaksta.rb
@@ -3,9 +3,9 @@ cask 'jaksta' do
   sha256 '8f54cdba4ab42aec0430cdc15f9fd62370391bd2c42e0b734d6d5adea0355f69'
 
   url "http://downloads.jaksta.com/release/mac/Jaksta-#{version}.dmg"
-  name 'Jaksta'
   appcast 'http://downloads.jaksta.com/release/mac/sparkle/JakstaforMac.xml',
           :sha256 => '17d58d78dc008d18226c67348f377fc60b1dc84f42cc3fae438094c6e81c3072'
+  name 'Jaksta'
   homepage 'https://www.jaksta.com/Products#JakstaMediaRecorderMac'
   license :commercial
 
