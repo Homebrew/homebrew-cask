d3b4242ce8a0543e88b190d1a4f9f5ff6b88e7fa
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:04 -0500

    noiz2sa.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/noiz2sa.rb
+++ Casks/noiz2sa.rb
@@ -5,7 +5,7 @@ cask 'noiz2sa' do
   url 'https://workram.com/downloads.php?f=Noiz2sa', :referer => 'https://workram.com/games/noiz2sa'
   name 'Noiz2sa'
   homepage 'https://workram.com/games/noiz2sa/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Noiz2sa.app'
 end
