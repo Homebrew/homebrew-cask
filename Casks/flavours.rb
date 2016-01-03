8476d85bfc5eef7ed214ed0272d85f9a59073c27
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:20 -0500

    flavours.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/flavours.rb
+++ Casks/flavours.rb
@@ -12,14 +12,14 @@ cask 'flavours' do
   app 'Flavours.app'
 
   uninstall :launchctl => 'net.interacto.flavours.helper',
-            :quit  => 'net.interacto.Flavours',
-            :delete => [
-                        '/usr/local/bin/flavours.agent',
-                        '/usr/local/bin/flavours.ignitor',
-                        '/usr/local/bin/restoreui',
-                        '/usr/local/lib/libflavours.dylib',
-                        '/usr/local/lib/libflavoursui.dylib',
-                       ]
+            :quit      => 'net.interacto.Flavours',
+            :delete    => [
+                            '/usr/local/bin/flavours.agent',
+                            '/usr/local/bin/flavours.ignitor',
+                            '/usr/local/bin/restoreui',
+                            '/usr/local/lib/libflavours.dylib',
+                            '/usr/local/lib/libflavoursui.dylib',
+                          ]
 
   caveats do
     files_in_usr_local
