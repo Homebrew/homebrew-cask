de1198634912024cec6c5a245835b3b9d67a1197
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:55 -0500

    bricksmith.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/bricksmith.rb
+++ Casks/bricksmith.rb
@@ -3,9 +3,9 @@ cask 'bricksmith' do
   sha256 'bf7dcd7508d2d6e37f6079080e8ae51095e14d05eb507ad85ac4bbaf5b9ae7c5'
 
   url "http://downloads.sourceforge.net/sourceforge/bricksmith/bricksmith/Bricksmith%20#{version}/BricksmithComplete#{version}.zip"
-  name 'Bricksmith'
   appcast 'http://bricksmith.sourceforge.net/files/bricksmith_appcast.xml',
           :sha256 => '75f5c572a0db8373093ba2308c423086f6fc50069a79808b40c4f3d496ab256f'
+  name 'Bricksmith'
   homepage 'http://bricksmith.sourceforge.net/'
   license :bsd
 
