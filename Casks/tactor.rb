067e1591755e3f625fa8ea6e6a21472454f48a36
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:39 -0500

    tactor.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/tactor.rb
+++ Casks/tactor.rb
@@ -4,11 +4,11 @@ cask 'tactor' do
 
   # dropbox is the official download host per the vendor homepage
   url 'https://dl.dropboxusercontent.com/u/7614970/Tactor.zip'
-  name 'Tactor'
   appcast 'https://onflapp.appspot.com/tactor',
           :sha256 => 'de6620c8fd971681e356f2c24280ae9d38d09b78cafd2f25593fe50a235fd997'
+  name 'Tactor'
   homepage 'https://onflapp.wordpress.com/tactor/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Tactor.app'
 end
