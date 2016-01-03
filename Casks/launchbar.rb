1721ad49f55130fc4f86b63433a96c77f10dca2c
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:46 -0500

    launchbar.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -1,5 +1,4 @@
 cask 'launchbar' do
-
   if MacOS.release <= :mountain_lion
     version '5.6.4'
     sha256 '22a1ec0c10de940e5efbcccd18b8b048d95fb7c63213a01c7976a76d6be69a4d'
@@ -17,7 +16,7 @@ cask 'launchbar' do
   app 'LaunchBar.app'
 
   zap :delete => [
-                  '~/Library/Preferences/at.obdev.LaunchBar.plist',
-                  '~/Library/Application Support/LaunchBar',
+                   '~/Library/Preferences/at.obdev.LaunchBar.plist',
+                   '~/Library/Application Support/LaunchBar',
                  ]
 end
