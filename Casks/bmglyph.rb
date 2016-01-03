27119c8272582b5d7bd3aca5d8927ba924c206fa
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:54 -0500

    bmglyph.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/bmglyph.rb
+++ Casks/bmglyph.rb
@@ -3,9 +3,9 @@ cask 'bmglyph' do
   sha256 'b4e42720274ec4825651d5e6376c7e9d80d62770edd5d9f4dc5dc8e24f3fe3fe'
 
   url "http://www.bmglyph.com/application/bmGlyph.#{version}.zip"
-  name 'bmGlyph'
   appcast 'http://www.bmglyph.com/application/bmGlyphVersion.xml',
           :sha256 => '43c86d82a5f63b1316913b06399598e792ed2ae23e6bdc2e6e8ad98b3d7e4178'
+  name 'bmGlyph'
   homepage 'http://www.bmglyph.com/'
   license :commercial
 
