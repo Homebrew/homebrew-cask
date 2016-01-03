a32a2351e6b03d58079179e7c36453a943bc2fbf
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:14 -0500

    pollev-presenter.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/pollev-presenter.rb
+++ Casks/pollev-presenter.rb
@@ -4,9 +4,9 @@ cask 'pollev-presenter' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/polleverywhere-app/mac-beta/pollev.dmg'
-  name 'PollEv Presenter'
   appcast 'https://polleverywhere-app.s3.amazonaws.com/mac-beta/appcast.xml',
           :sha256 => 'dbfce32dced0e0370f6757d34aa89e176cab2495dcaaafc4a4fba85090e35bf6'
+  name 'PollEv Presenter'
   homepage 'https://www.polleverywhere.com/'
   license :gratis
 
