0d8b731d9071984f9995ccfdbdbdb1d22f6ed526
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:02 -0500

    codebug.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/codebug.rb
+++ Casks/codebug.rb
@@ -2,7 +2,7 @@ cask 'codebug' do
   version '0.9.4'
   sha256 '2a568b2eff8145823b552668d0b1caa75a98077afe7692ccb69ca030508c86b9'
 
-  url "http://codebugapp.com/downloads/Codebug#{version.gsub('.','-')}.zip"
+  url "http://codebugapp.com/downloads/Codebug#{version.gsub('.', '-')}.zip"
   appcast 'http://codebugapp.com/update.xml',
           :sha256 => '70782ae334e666a9ce810ce2d4f60df63fdb02fade4a266eca0f66341a299788'
   name 'Codebug'
