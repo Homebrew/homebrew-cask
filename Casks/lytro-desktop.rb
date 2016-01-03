436a8524483947ad8cdc169f242a0c1800ee9ce8
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:50 -0500

    lytro-desktop.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/lytro-desktop.rb
+++ Casks/lytro-desktop.rb
@@ -3,9 +3,9 @@ cask 'lytro-desktop' do
   sha256 '0646b7515e5bf591c6b75d646a1d98d1f31be949add736b11e7f4c2282487854'
 
   # amazonaws.com is the official download host per the appcast feed
-  url "https://s3.amazonaws.com/lytro-distro/lytro-#{version.sub(%r{.*-},'')}.dmg"
+  url "https://s3.amazonaws.com/lytro-distro/lytro-#{version.sub(%r{.*-}, '')}.dmg"
   appcast 'https://pictures.lytro.com/support/software_update',
-    :sha256 => 'a24271bb26982429e8e42ff2f3e1ab3d1690192cc2acc2ce36346ed4302e5904'
+          :sha256 => 'a24271bb26982429e8e42ff2f3e1ab3d1690192cc2acc2ce36346ed4302e5904'
   name 'Lytro Desktop'
   homepage 'https://www.lytro.com/'
   license :gratis
