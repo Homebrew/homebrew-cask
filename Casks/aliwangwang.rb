b1032312f7fd495ecefbc56c474e497fba4d2fa7
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:44 -0500

    aliwangwang.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/aliwangwang.rb
+++ Casks/aliwangwang.rb
@@ -4,21 +4,21 @@ cask 'aliwangwang' do
 
   # alicdn.com is the official download host per the vendor homepage
   url "https://dbison.alicdn.com/updates/macww-nosandbox-#{version}.dmg"
-  name 'Ali Wangwang'
   appcast 'https://update.labs.etao.com/macww/updates.xml',
           :sha256 => 'be61920aa7408e95378893f2829a8292d19bf06df1d95e854425df31ff49cc0f'
+  name 'Ali Wangwang'
   homepage 'https://wangwang.taobao.com'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'AliWangwang.app'
 
   uninstall :quit => 'com.taobao.aliwangwang'
 
   zap :delete => [
-                  '~/Library/Caches/com.taobao.aliwangwang',
-                  '~/Library/Containers/com.taobao.aliwangwang',
-                  '~/Library/Internet Plug-Ins/WangwangPlugin.plugin',
-                  '~/Library/Preferences/com.taobao.aliwangwang.plist',
-                  '~/Library/Saved Application State/com.taobao.aliwangwang.savedState'
+                   '~/Library/Caches/com.taobao.aliwangwang',
+                   '~/Library/Containers/com.taobao.aliwangwang',
+                   '~/Library/Internet Plug-Ins/WangwangPlugin.plugin',
+                   '~/Library/Preferences/com.taobao.aliwangwang.plist',
+                   '~/Library/Saved Application State/com.taobao.aliwangwang.savedState',
                  ]
 end
