80ab2419ebd1eb28d1ffceef95dbaacbfc76f974
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:59 -0500

    chessx.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/chessx.rb
+++ Casks/chessx.rb
@@ -1,8 +1,8 @@
 cask 'chessx' do
   version '1.3.2'
   sha256 '7719eaf448d0b974d524fb3c180fb748b18494afe9a36c34b17ea138dbdad00f'
-  url "http://downloads.sourceforge.net/project/chessx/chessx/#{version}/chessx-#{version}.dmg"
 
+  url "http://downloads.sourceforge.net/project/chessx/chessx/#{version}/chessx-#{version}.dmg"
   name 'ChessX'
   homepage 'http://chessx.sourceforge.net/'
   license :gpl
