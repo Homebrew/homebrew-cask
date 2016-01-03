c7296e8bcd9cfbce6fb559c6019d6d4f7bfd73df
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:17 -0500

    protonet.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/protonet.rb
+++ Casks/protonet.rb
@@ -3,11 +3,11 @@ cask 'protonet' do
   sha256 :no_check
 
   url 'http://clients.protonet.info/osx/Protonet.dmg'
-  name 'Protonet'
   appcast 'http://clients.protonet.info/osx/stable/appcast.xml',
           :sha256 => '592f270de11c6ef85081388c2c23d89808654db75a3cd144373c2f656c308e2e'
+  name 'Protonet'
   homepage 'http://clients.protonet.info/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Protonet.app'
 end
