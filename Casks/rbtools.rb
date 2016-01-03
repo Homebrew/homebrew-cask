9503f63cc3ac01e21c8b71ec40c56ad87588db6f
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:20 -0500

    rbtools.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/rbtools.rb
+++ Casks/rbtools.rb
@@ -2,7 +2,7 @@ cask 'rbtools' do
   version '0.7.5'
   sha256 'fd910542e24d067df3fe635e982ae5e3526a0036f79ff129c29cf66f02751bd6'
 
-  url "https://downloads.reviewboard.org/releases/RBTools/#{version.sub(%r{\.\d+$},'')}/RBTools-#{version}.pkg"
+  url "https://downloads.reviewboard.org/releases/RBTools/#{version.sub(%r{\.\d+$}, '')}/RBTools-#{version}.pkg"
   name 'RBTools'
   homepage 'https://www.reviewboard.org/docs/rbtools/0.7/'
   license :mit
