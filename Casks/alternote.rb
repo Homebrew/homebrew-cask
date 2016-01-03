5ce50fbcb0f6fb8c0aa67d5a30fb4a27112408cc
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:45 -0500

    alternote.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/alternote.rb
+++ Casks/alternote.rb
@@ -2,7 +2,7 @@ cask 'alternote' do
   version '1.0.6_1006'
   sha256 '31ff36a3a02daba52b3363d9f8b75e1183df488ff844f8f7f919f67ba57bb4d2'
 
-  url "http://alternoteapp.com/assets/downloads/Alternote#{version.sub(%r{.*_},'')}.zip"
+  url "http://alternoteapp.com/assets/downloads/Alternote#{version.sub(%r{.*_}, '')}.zip"
   appcast 'http://alternoteapp.com/assets/appcast.xml',
           :sha256 => '0cbfc55527716eeab37a3cd8b2ce4fcb91209fdb4a6ecc238a4a44046ce6bfd4'
   name 'Alternote'
