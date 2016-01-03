912a143cd32499d8f4fdc85ca8fcadf33b424570
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:44 -0500

    alfred.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -16,15 +16,15 @@ cask 'alfred' do
     suppress_move_to_applications :key => 'suppressMoveToApplications'
   end
 
-  uninstall :quit => 'com.runningwithcrayons.Alfred-2',
+  uninstall :quit       => 'com.runningwithcrayons.Alfred-2',
             :login_item => 'Alfred 2'
 
   zap :delete => [
-                  '~/Library/Application Support/Alfred 2',
-                  '~/Library/Caches/com.runningwithcrayons.Alfred-2',
-                  '~/Library/Caches/com.runningwithcrayons.Alfred-Preferences',
-                  '~/Library/Preferences/com.runningwithcrayons.Alfred-2.plist',
-                  '~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist',
-                  '~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState'
+                   '~/Library/Application Support/Alfred 2',
+                   '~/Library/Caches/com.runningwithcrayons.Alfred-2',
+                   '~/Library/Caches/com.runningwithcrayons.Alfred-Preferences',
+                   '~/Library/Preferences/com.runningwithcrayons.Alfred-2.plist',
+                   '~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist',
+                   '~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState',
                  ]
 end
