3ecd345634f7d4a393bee151f96d403e2933f5d1
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:57 -0500

    wondershare-player.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/wondershare-player.rb
+++ Casks/wondershare-player.rb
@@ -5,7 +5,7 @@ cask 'wondershare-player' do
   url 'http://download.wondershare.com/mac-player_full1479.dmg'
   name 'Wondershare Player'
   homepage 'http://www.wondershare.com/video-player/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Wondershare Player.app'
 end
