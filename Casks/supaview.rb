4dbe60ea63be16e374a44a1d715af56b4ecdd0ec
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:37 -0500

    supaview.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/supaview.rb
+++ Casks/supaview.rb
@@ -3,9 +3,9 @@ cask 'supaview' do
   sha256 'd0940ea48223ce138260a25a8a070365d619acb0adbe96288e70e5b19ed10ff0'
 
   url "http://twinside.free.fr/supaview/SupaView.#{version}.zip"
-  name 'SupaView'
   appcast 'http://twinside.free.fr/supaview/supaview-appcast.xml',
           :sha256 => 'd8c47e3f827e1f85702713e72012cc62e0398cbf5a91d86122785bcce448425e'
+  name 'SupaView'
   homepage 'http://twinside.free.fr/supaview/'
   license :gratis
 
