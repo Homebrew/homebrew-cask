c66b9e40ecda3d0f128e08329101c3c246cb7fe9
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:17 -0500

    proxifier.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/proxifier.rb
+++ Casks/proxifier.rb
@@ -3,7 +3,6 @@ cask 'proxifier' do
   sha256 :no_check
 
   url 'https://www.proxifier.com/distr/ProxifierMac.zip'
-
   name 'Proxifier'
   homepage 'https://www.proxifier.com/mac/'
   license :commercial
