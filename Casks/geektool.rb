0ec49823d339945179bfce499166a2d0b1677add
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:24 -0500

    geektool.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/geektool.rb
+++ Casks/geektool.rb
@@ -10,10 +10,10 @@ cask 'geektool' do
   app 'GeekTool.app'
 
   zap :delete => [
-                  '~/Library/Preferences/org.tynsoe.GeekTool.plist',
-                  '~/Library/Preferences/org.tynsoe.geeklet.file.plist',
-                  '~/Library/Preferences/org.tynsoe.geeklet.image.plist',
-                  '~/Library/Preferences/org.tynsoe.geeklet.shell.plist',
-                  '~/Library/Preferences/org.tynsoe.geektool3.plist',
+                   '~/Library/Preferences/org.tynsoe.GeekTool.plist',
+                   '~/Library/Preferences/org.tynsoe.geeklet.file.plist',
+                   '~/Library/Preferences/org.tynsoe.geeklet.image.plist',
+                   '~/Library/Preferences/org.tynsoe.geeklet.shell.plist',
+                   '~/Library/Preferences/org.tynsoe.geektool3.plist',
                  ]
 end
