0d86beed93975f05c2e84d7114052a2e366d02e6
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:32 -0500

    sophos-anti-virus-home-edition.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/sophos-anti-virus-home-edition.rb
+++ Casks/sophos-anti-virus-home-edition.rb
@@ -9,41 +9,41 @@ cask 'sophos-anti-virus-home-edition' do
 
   installer :script => 'Sophos Anti-Virus Home Edition.app/Contents/MacOS/tools/InstallationDeployer', :args => %w[--install]
 
-  uninstall :script => {
-                        :executable => 'Sophos Anti-Virus Home Edition.app/Contents/MacOS/tools/InstallationDeployer',
-                        :args => %w[--remove]
-                       },
+  uninstall :script    => {
+                            :executable => 'Sophos Anti-Virus Home Edition.app/Contents/MacOS/tools/InstallationDeployer',
+                            :args       => %w[--remove],
+                          },
             :launchctl => [
-                           'com.sophos.common.servicemanager',
-                           'com.sophos.uiserver',
+                            'com.sophos.common.servicemanager',
+                            'com.sophos.uiserver',
                           ],
-            :kext => [
-                      'com.sophos.kext.sav',
-                      'com.sophos.nke.swi',
-                     ],
-            :quit => 'com.sophos.ui',
-            :delete => [
-                        '/Applications/Remove Sophos Anti-Virus.app',
-                        '/Applications/Sophos Anti-Virus.app',
-                        '/Library/Extensions/SophosNetworkInterceptor.kext',
-                        '/Library/Extensions/SophosOnAccessInterceptor.kext',
-                        '/Library/Frameworks/SAVI.framework',
-                        '/Library/Frameworks/SUMScanKit.framework',
-                        '/Library/Frameworks/SophosGenericsCommon.framework',
-                        '/Library/Frameworks/SophosGenericsCore.framework',
-                        '/Library/LaunchAgents/com.sophos.uiserver.plist',
-                        '/Library/Sophos Anti-Virus',
-                        '/usr/bin/sweep',
-                        '/usr/share/man/man1/sweep.1',
-                       ]
+            :kext      => [
+                            'com.sophos.kext.sav',
+                            'com.sophos.nke.swi',
+                          ],
+            :quit      => 'com.sophos.ui',
+            :delete    => [
+                            '/Applications/Remove Sophos Anti-Virus.app',
+                            '/Applications/Sophos Anti-Virus.app',
+                            '/Library/Extensions/SophosNetworkInterceptor.kext',
+                            '/Library/Extensions/SophosOnAccessInterceptor.kext',
+                            '/Library/Frameworks/SAVI.framework',
+                            '/Library/Frameworks/SUMScanKit.framework',
+                            '/Library/Frameworks/SophosGenericsCommon.framework',
+                            '/Library/Frameworks/SophosGenericsCore.framework',
+                            '/Library/LaunchAgents/com.sophos.uiserver.plist',
+                            '/Library/Sophos Anti-Virus',
+                            '/usr/bin/sweep',
+                            '/usr/share/man/man1/sweep.1',
+                          ]
 
   zap :delete => [
-                  '/Library/Application Support/Sophos',
-                  '/Library/Preferences/com.sophos.ac.plist',
-                  '/Library/Preferences/com.sophos.dc.plist',
-                  '/Library/Preferences/com.sophos.mcs.plist',
-                  '/Library/Preferences/com.sophos.sau.plist',
-                  '/Library/Preferences/com.sophos.sav.plist',
-                  '/Library/Preferences/com.sophos.swc.plist',
+                   '/Library/Application Support/Sophos',
+                   '/Library/Preferences/com.sophos.ac.plist',
+                   '/Library/Preferences/com.sophos.dc.plist',
+                   '/Library/Preferences/com.sophos.mcs.plist',
+                   '/Library/Preferences/com.sophos.sau.plist',
+                   '/Library/Preferences/com.sophos.sav.plist',
+                   '/Library/Preferences/com.sophos.swc.plist',
                  ]
 end
