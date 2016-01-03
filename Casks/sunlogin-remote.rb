39fb0974d0f919ecc46413bbc81d45625acc6c6a
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:37 -0500

    sunlogin-remote.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/sunlogin-remote.rb
+++ Casks/sunlogin-remote.rb
@@ -6,7 +6,7 @@ cask 'sunlogin-remote' do
   name 'Sunlogin Remote'
   name '向日葵控制端'
   homepage 'https://sunlogin.oray.com'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Sunlogin Remote.app'
 end
