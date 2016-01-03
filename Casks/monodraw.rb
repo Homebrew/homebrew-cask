8aba43f04de6346082fe16ee76d19a58a91e45c3
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:57 -0500

    monodraw.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/monodraw.rb
+++ Casks/monodraw.rb
@@ -3,9 +3,9 @@ cask 'monodraw' do
   sha256 :no_check
 
   url 'http://updates.helftone.com/monodraw/downloads/Monodraw-latest.zip'
-  name 'Monodraw'
   appcast 'http://updates.helftone.com/monodraw/appcast-beta.xml',
           :sha256 => 'b15571cccfb97339f5762eff8a74a4f6fd4a0a1006f41071ab05a7d5e65193cd'
+  name 'Monodraw'
   homepage 'http://monodraw.helftone.com'
   license :commercial
 
@@ -16,7 +16,7 @@ cask 'monodraw' do
   end
 
   zap :delete => [
-                  '~/Library/Preferences/com.helftone.monodraw.plist',
-                  '~/Library/Caches/com.helftone.monodraw',
+                   '~/Library/Preferences/com.helftone.monodraw.plist',
+                   '~/Library/Caches/com.helftone.monodraw',
                  ]
 end
