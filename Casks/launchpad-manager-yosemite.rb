d6730ebb908a37382afdaa49e59ab7d8748cb87a
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:46 -0500

    launchpad-manager-yosemite.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/launchpad-manager-yosemite.rb
+++ Casks/launchpad-manager-yosemite.rb
@@ -3,9 +3,9 @@ cask 'launchpad-manager-yosemite' do
   sha256 :no_check
 
   url 'http://launchpadmanager.com/download_yosemite.php/LaunchpadManagerYosemite.dmg'
-  name 'Launchpad Manager'
   appcast 'http://launchpadmanager.com/appyos/sparkle.rss',
           :sha256 => 'f0e662903635a480bb1cb08122f4d1f743e694442c508e5f71c522d99128abb3'
+  name 'Launchpad Manager'
   homepage 'http://launchpadmanager.com/'
   license :commercial
 
