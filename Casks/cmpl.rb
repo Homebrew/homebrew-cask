536abfa6f75c0154c3a24eef0dac14c0170582fa
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:01 -0500

    cmpl.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/cmpl.rb
+++ Casks/cmpl.rb
@@ -2,14 +2,13 @@ cask 'cmpl' do
   version '1.10.0'
   sha256 'f893ca2537d971682df9a4a08d34505732b40f58ee58037ad4fe736607d8953e'
 
-  url "http://www.coliop.org/_download/Cmpl-#{version.gsub('.','-')}-osx.tar.gz"
+  url "http://www.coliop.org/_download/Cmpl-#{version.gsub('.', '-')}-osx.tar.gz"
   name 'CMPL'
   homepage 'http://www.coliop.org/'
   license :gpl
 
   # linking into ~/Applications breaks Cmpl, due to hardcoded paths
   suite 'Cmpl', :target => '/Applications/Cmpl'
-
   binary 'Cmpl/bin/cmpl'
   binary 'Cmpl/coliop'
   binary 'Cmpl/pyCmpl/scripts/Unix/cmplServer'
