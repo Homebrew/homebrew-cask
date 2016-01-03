cf59b2247be4ed1f55644f02a271308ec78acc45
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:18 -0500

    fbreader.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/fbreader.rb
+++ Casks/fbreader.rb
@@ -2,7 +2,7 @@ cask 'fbreader' do
   version '0.99.5-alpha'
   sha256 'c05e23d66942b49533e0eabc9d39e2062c9a11086369c31b417a1937ac5886f9'
 
-  url "https://fbreader.org/files/macos/FBReader%20#{version.gsub(/-/, '%20')}.dmg"
+  url "https://fbreader.org/files/macos/FBReader%20#{version.gsub(%r{-}, '%20')}.dmg"
   name 'FBReader'
   homepage 'https://fbreader.org/content/macos'
   license :gpl
@@ -10,6 +10,6 @@ cask 'fbreader' do
   app 'FBReader.app'
 
   zap :delete => [
-                  '~/.FBReader',
+                   '~/.FBReader'
                  ]
 end
