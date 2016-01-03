d7d1f49f3bbf9af421e5b1eab118c9a69a2e261b
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:46 -0500

    leech.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/leech.rb
+++ Casks/leech.rb
@@ -3,9 +3,9 @@ cask 'leech' do
   sha256 :no_check
 
   url 'http://manytricks.com/download/leech'
-  name 'Leech'
   appcast 'https://manytricks.com/leech/appcast.xml',
           :sha256 => 'ef4c85840f37dc92c0999f88e5e64acc5eb76b3e3933582710cdb703583473a1'
+  name 'Leech'
   homepage 'https://manytricks.com/leech/'
   license :commercial
 
