8aed09dea3863af622301ade1c09599dedc00a15
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:09 -0500

    djay-pro.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/djay-pro.rb
+++ Casks/djay-pro.rb
@@ -2,10 +2,10 @@ cask 'djay-pro' do
   version '1.2-201508141249'
   sha256 '1abf8861828d6233cf0254b0fe8621f2fb8ce1780201b2d1af1cdb1c3c8463af'
 
-  url "http://download.algoriddim.com/djay/#{version.sub(%r{.*?-},'')}/djay_Pro_#{version.sub(%r{-\d+$},'')}.zip"
-  name 'Algoriddim djay Pro'
+  url "http://download.algoriddim.com/djay/#{version.sub(%r{.*?-}, '')}/djay_Pro_#{version.sub(%r{-\d+$}, '')}.zip"
   appcast 'https://www.algoriddim.com/djay-mac/releasenotes/appcast',
           :sha256 => 'b4289029993baacd8d95e458b65227fdb1c449713efe1696c064e6e2032717e3'
+  name 'Algoriddim djay Pro'
   homepage 'http://algoriddim.com/djay-mac'
   license :commercial
 
