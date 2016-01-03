5fb5ca28359b682fd1973eaba5b399206df13805
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:54 -0500

    wasted.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/wasted.rb
+++ Casks/wasted.rb
@@ -3,9 +3,9 @@ cask 'wasted' do
   sha256 :no_check
 
   url 'http://wasted.werk01.de/Wasted.zip'
-  name 'WASTED'
   appcast 'http://wasted.werk01.de/appcast.xml',
           :sha256 => '9fb3139c5061c58d62e25ffce233c28f774439c7f9d09276ce5470e891f959d7'
+  name 'WASTED'
   homepage 'http://wasted.werk01.de'
   license :mit
 
