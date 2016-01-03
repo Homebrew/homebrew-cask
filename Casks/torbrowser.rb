293c99181f1d701879bf363376d9057ba0672c5f
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:44 -0500

    torbrowser.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/torbrowser.rb
+++ Casks/torbrowser.rb
@@ -3,11 +3,11 @@ cask 'torbrowser' do
   sha256 '11476ec321523f7e8eaf1e697e2b4745ea53aaadd4080197ee6d7a335a4323a8'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
-  gpg "#{url}.asc",
-      :key_id => 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
   name 'Tor Browser'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   license :oss
+  gpg "#{url}.asc",
+      :key_id => 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
 
   app 'TorBrowser.app'
 end
