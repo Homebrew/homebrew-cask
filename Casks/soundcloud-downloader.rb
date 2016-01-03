78555857b16ff74a50f70c8ecc49ad44a0bb3145
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:32 -0500

    soundcloud-downloader.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/soundcloud-downloader.rb
+++ Casks/soundcloud-downloader.rb
@@ -3,9 +3,9 @@ cask 'soundcloud-downloader' do
   sha256 'eabb5f3f7ef0db45a804a720069fa98160ff51ca6ec6e0184423f1f4ef98e0af'
 
   url "http://black-burn.ch/scd/downloads/#{version.delete('.')}/in/b"
-  name 'SoundCloud Downloader'
   appcast 'http://black-burn.ch/applications/scd/updates.php?hwni=1',
           :sha256 => '3aec7755cdc3208b781ce41613d60f8e574f6c34e3fd819826e6734dd7aac70d'
+  name 'SoundCloud Downloader'
   homepage 'http://black-burn.ch/scd/'
   license :mit
 
