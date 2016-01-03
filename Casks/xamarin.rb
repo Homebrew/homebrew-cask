6e474cfe626fc967c0ccbf7cc64ceacf0e288458
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:59 -0500

    xamarin.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/xamarin.rb
+++ Casks/xamarin.rb
@@ -5,11 +5,12 @@ cask 'xamarin' do
   url 'https://download.xamarin.com/Installer/Mac/XamarinInstaller.dmg'
   name 'Xamarin Platform'
   homepage 'https://xamarin.com/platform'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   installer :manual => 'Install Xamarin.app'
 
   uninstall :delete => '/Applications/Xamarin Studio.app'
+
   zap       :delete => '~/Library/Developer/Xamarin'
 
   caveats <<-EOS.undent
