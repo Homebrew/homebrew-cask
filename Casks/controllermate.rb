de20414b6d43fae26a218b867860603c262b5288
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:03 -0500

    controllermate.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -4,19 +4,20 @@ cask 'controllermate' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/orderedbytes/ControllerMate#{version.delete('.')}.zip"
-  name 'ControllerMate'
   appcast 'http://www.orderedbytes.com/sparkle/appcast_cm460.xml',
           :sha256 => 'b75925e88055ccce64c3845876552c31dd3f1981a555747a0b52d3af9f75b72f'
+  name 'ControllerMate'
   homepage 'http://www.orderedbytes.com/controllermate/'
   license :freemium
 
   pkg '#temp#/ControllerMate.pkg'
 
   uninstall :pkgutil => 'com.orderedbytes.controllermate.*'
+
   zap       :delete => [
-                        '~/Library/Application Support/ControllerMate',
-                        '~/Library/Caches/com.orderedbytes.ControllerMate4',
-                        '~/Library/Logs/ControllerMate MIDI',
-                        '~/Library/Logs/ControllerMate',
+                         '~/Library/Application Support/ControllerMate',
+                         '~/Library/Caches/com.orderedbytes.ControllerMate4',
+                         '~/Library/Logs/ControllerMate MIDI',
+                         '~/Library/Logs/ControllerMate',
                        ]
 end
