9585a9e4d9c8f3d111d1d7c671bd9abda35cdc64
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:01 -0500

    cocos-code-ide.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/cocos-code-ide.rb
+++ Casks/cocos-code-ide.rb
@@ -10,8 +10,8 @@ cask 'cocos-code-ide' do
   app 'Cocos Code IDE.app'
 
   zap :delete => [
-    '~/Library/Caches/org.cocos.platform.ide',
-    '~/Library/Saved Application State/org.cocos.platform.ide.savedState',
-    '~/Library/Preferences/org.cocos.platform.ide.plist',
-  ]
+                   '~/Library/Caches/org.cocos.platform.ide',
+                   '~/Library/Saved Application State/org.cocos.platform.ide.savedState',
+                   '~/Library/Preferences/org.cocos.platform.ide.plist',
+                 ]
 end
