379e62427226533a4c8497135d9cb6febe6d6266
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:53 -0500

    bittorrent.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/bittorrent.rb
+++ Casks/bittorrent.rb
@@ -12,10 +12,10 @@ cask 'bittorrent' do
   uninstall :delete => '/Applications/BitTorrent.app'
 
   zap :delete => [
-                  '~/Library/Preferences/com.com.bittorrent.BitTorrent.plist',
-                  '~/Library/Application Support/BitTorrent',
-                  '~/Library/Saved Application State/com.com.bittorrent.BitTorrent.savedState',
-                  '~/Library/Caches/com.com.bittorrent.BitTorrent',
-                  '~/Library/LaunchAgents/com.bittorrent.BitTorrent.plist'
+                   '~/Library/Preferences/com.com.bittorrent.BitTorrent.plist',
+                   '~/Library/Application Support/BitTorrent',
+                   '~/Library/Saved Application State/com.com.bittorrent.BitTorrent.savedState',
+                   '~/Library/Caches/com.com.bittorrent.BitTorrent',
+                   '~/Library/LaunchAgents/com.bittorrent.BitTorrent.plist',
                  ]
 end
