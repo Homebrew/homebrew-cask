6371b4066be524363ff5d2b4595cdd0f91fd6995
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:01 -0500

    navicat-for-mysql.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -2,7 +2,7 @@ cask 'navicat-for-mysql' do
   version '11.2.5'
   sha256 'b0dfdb85c79fd5d84f8604db37f6a8375e063e84acb468e84468764042bb6340'
 
-  url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}_mysql_en.dmg"
+  url "http://download.navicat.com/download/navicat#{version.sub(%r{^(\d+)\.(\d+).*}, '\1\2')}_mysql_en.dmg"
   name 'Navicat for MySQL'
   homepage 'http://www.navicat.com/products/navicat-for-mysql'
   license :commercial
