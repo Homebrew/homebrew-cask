433a90522ab5ced223a306fa1ea8b23a63fd8589
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:27 -0500

    sequential.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/sequential.rb
+++ Casks/sequential.rb
@@ -7,7 +7,7 @@ cask 'sequential' do
           :sha256 => '447db3c7237bdd750ad128556b1d3ff6f3f3c26f8c2bd0131b9993b5365f7d6b'
   name 'Sequential'
   homepage 'http://sequentialx.com'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Sequential.app'
 end
