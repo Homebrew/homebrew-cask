3479f1605b8481dd6d1c73445c6f8da00726dff9
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:20:00 -0500

    yasu.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/yasu.rb
+++ Casks/yasu.rb
@@ -1,5 +1,4 @@
 cask 'yasu' do
-
   if MacOS.release <= :snow_leopard
     version '2.8.2'
     sha256 '427672a45b8315c2f38d968ea5e0c35c21b91091a1fe0e750fcd2b0078644336'
@@ -10,20 +9,20 @@ cask 'yasu' do
     url "http://yasuapp.net/files/yasu_#{version}.zip"
   end
 
-  name 'Yasu'
   appcast 'http://yasuapp.net/appcasts/yasu.xml',
           :sha256 => 'e79812a2ecb9a4df2b1484f5e544d43d997ad539e5d8c1a3e058cd12d11b646e'
+  name 'Yasu'
   homepage 'http://yasuapp.net'
   license :gratis
 
   app 'Yasu.app'
 
   zap :delete => [
-                  '~/Library/Caches/com.apple.helpd/Generated/net.yasuapp.yasu.help',
-                  '~/Library/Caches/net.yasuapp.yasu',
-                  '~/Library/Logs/Yasu.log',
-                  '~/Library/Preferences/net.yasuapp.yasu.plist',
-                  '~/Library/Preferences/org.jimmitchell.yasu.plist',
+                   '~/Library/Caches/com.apple.helpd/Generated/net.yasuapp.yasu.help',
+                   '~/Library/Caches/net.yasuapp.yasu',
+                   '~/Library/Logs/Yasu.log',
+                   '~/Library/Preferences/net.yasuapp.yasu.plist',
+                   '~/Library/Preferences/org.jimmitchell.yasu.plist',
                  ]
 
   caveats do
