599dd56ad15ec3d3746e5045400db7498da05344
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:37 -0500

    swift-publisher.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -4,9 +4,9 @@ cask 'swift-publisher' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/belightsoft/SwiftPublisher.dmg'
-  name 'Swift Publisher'
   appcast 'https://www.belightsoft.com/download/updates/appcast_SwiftPublisher3.xml',
           :sha256 => '73bf29fcb6fa689fa32e5a1666ffb8db22401a6033a91e2866fc95d1be7ffd96'
+  name 'Swift Publisher'
   homepage 'https://www.belightsoft.com/products/swiftpublisher/overview.php'
   license :closed
 
