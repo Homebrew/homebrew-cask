de21e764ae3a1d74b1aca7004e38199d8194eb6c
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:24 -0500

    schnapps.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/schnapps.rb
+++ Casks/schnapps.rb
@@ -3,9 +3,9 @@ cask 'schnapps' do
   sha256 :no_check
 
   url 'https://d15xn61otjv90c.cloudfront.net/download/Schnapps_latest.zip'
-  name 'Schnapps'
   appcast 'http://schnappsformac.com/download/appcast.xml',
           :sha256 => '1fa261b1b268b99f43353523c760d602ae10e41ae1a7daeadeb98e9d9fae4f4b'
+  name 'Schnapps'
   homepage 'http://schnappsformac.com/'
   license :commercial
 
