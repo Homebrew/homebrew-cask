ed7cead6b02f5c98d214b2060a9e0b4d2464707f
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:11 -0500

    drobo-dashboard.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -2,14 +2,14 @@ cask 'drobo-dashboard' do
   version '2.7.0_77097'
   sha256 'c56a2fd01be607971a8846c7f762ba63169340d73616d84250183daa462d108f'
 
-  url "http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version.sub(%r{_.*},'')}.dmg"
+  url "http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version.sub(%r{_.*}, '')}.dmg"
   name 'Drobo Dashboard'
   homepage 'http://www.drobo.com'
   license :gratis
 
   pkg "Install.app/Contents/Drobo_Dashboard_Installer_#{version}.pkg"
 
-  uninstall :script => { :executable => 'Uninstall.app/Contents/Resources/Scripts/Drobo_Dashboard_uninstall.sh',
-                         :sudo => true },
+  uninstall :script  => { :executable => 'Uninstall.app/Contents/Resources/Scripts/Drobo_Dashboard_uninstall.sh',
+                          :sudo       => true },
             :pkgutil => 'com.datarobotics.drobodashboard'
 end
