bb3cedf064073352990aacaf33e66b72f422916c
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:57 -0500

    workamajig.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/workamajig.rb
+++ Casks/workamajig.rb
@@ -6,9 +6,9 @@ cask 'workamajig' do
   url 'https://s3.amazonaws.com/Workamajig/AIR/Workamajig.air'
   name 'Workamajig'
   homepage 'http://www.workamajig.com/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
-
-  app 'Workamajig.app'
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   depends_on :cask => 'adobe-air'
+
+  app 'Workamajig.app'
 end
