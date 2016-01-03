83f0e6a87dba7501c5ddadb9ab39430e74106118
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:47 -0500

    appzapper.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/appzapper.rb
+++ Casks/appzapper.rb
@@ -3,9 +3,9 @@ cask 'appzapper' do
   sha256 'b7d0bdd05cf246a2f2ab18145b052824860cb74a6ae665fba9d403f6bb79fac4'
 
   url "http://www.appzapper.com/downloads/AppZapper#{version}.zip"
-  name 'AppZapper'
   appcast 'http://www.appzapper.com/az2appcast.xml',
           :sha256 => 'e405c8598881a8351072efe6dd0596a3fd9bbb604fb4ea63c7b5bfa067a3b303'
+  name 'AppZapper'
   homepage 'http://www.appzapper.com/'
   license :commercial
 
