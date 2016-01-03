23a4dcc586686b9dcf557e0f9c77a1856bf4a7f6
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:04 -0500

    couchbase-server-enterprise.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -1,11 +1,11 @@
 cask 'couchbase-server-enterprise' do
   version '4.1.0'
   sha256 '237837598a1bf663debaea5c909bce36a39fbc673139cf4d4ebc55fb5c276313'
-  url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
 
-  name 'Couchbase Server'
+  url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
   appcast 'http://appcast.couchbase.com/membasex.xml',
           :sha256 => 'f05312af8660b51401dd70cd55ddedeb6de551d5b92372ab155906bc597c9ec7'
+  name 'Couchbase Server'
   homepage 'http://www.couchbase.com/'
   license :apache
 
