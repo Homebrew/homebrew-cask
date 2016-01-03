7dfa44d2d25ce0a2451bc37d3c4af0038ceaabac
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:40 -0500

    a-better-finder-rename.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/a-better-finder-rename.rb
+++ Casks/a-better-finder-rename.rb
@@ -3,9 +3,9 @@ cask 'a-better-finder-rename' do
   sha256 :no_check
 
   url 'http://www.publicspace.net/download/ABFRX.dmg'
-  name 'A Better Finder Rename'
   appcast 'http://www.publicspace.net/app/signed_abfr10.xml',
           :sha256 => '1f0457b0a280d86194117e2fd093cbcec0e56e50fdd80e5dc286a292506998d8'
+  name 'A Better Finder Rename'
   homepage 'http://www.publicspace.net/ABetterFinderRename/'
   license :commercial
 
