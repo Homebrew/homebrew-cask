ed973d81f51baabfef63341bbb108720c4ca7678
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:04 -0500

    crossover.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/crossover.rb
+++ Casks/crossover.rb
@@ -3,9 +3,9 @@ cask 'crossover' do
   sha256 '62fb015ebd1b90cc53ee447b73b74665bbb09dd8e04fc76fe10667d78100916a'
 
   url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version}.zip"
-  name 'CrossOver'
   appcast 'https://www.codeweavers.com/xml/versions/cxmac.xml',
           :sha256 => '5b27ca654f165ff1e3003a4565a5c155787f7356c4bd603554a13e72cf9aeeed'
+  name 'CrossOver'
   homepage 'https://www.codeweavers.com/products/crossover-mac/'
   license :commercial
 
