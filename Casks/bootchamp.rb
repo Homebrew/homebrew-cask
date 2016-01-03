a8466dd293313aec18616e4b1179243ca327eb53
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:54 -0500

    bootchamp.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/bootchamp.rb
+++ Casks/bootchamp.rb
@@ -3,9 +3,9 @@ cask 'bootchamp' do
   sha256 :no_check
 
   url 'https://www.kainjow.com/downloads/BootChamp.zip'
-  name 'BootChamp'
   appcast 'https://kainjow.com/updates/bootchamp.xml',
           :sha256 => 'b117cae73e7f24153c05549dab19cbd82013a410b5db89dbc6f9e36d5fd67f4f'
+  name 'BootChamp'
   homepage 'https://www.kainjow.com/'
   license :oss
 
