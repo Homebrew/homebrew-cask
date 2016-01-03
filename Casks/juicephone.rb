2c5db4eedbc1618e96fab84b7ec1d2deb8082343
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:42 -0500

    juicephone.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/juicephone.rb
+++ Casks/juicephone.rb
@@ -3,9 +3,9 @@ cask 'juicephone' do
   sha256 '4c5edb9197f7ae093681e810692b81e447a063cc0a59c025afc59cea0f59d33e'
 
   url "http://download.addpod.de/juicephone/#{version}"
-  name 'JuicePhone'
   appcast 'http://update.addpod.com/juicephone.xml',
           :sha256 => '771453d462d524ff73c53318489aa7719ee63a2a84a41a4c049cc12bc195c1e7'
+  name 'JuicePhone'
   homepage 'http://addpod.com/juicephone'
   license :gratis
 
