a994bf2e51ec9ddaeaf79ad7218d497a51f12c53
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:03 -0500

    commandq.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/commandq.rb
+++ Casks/commandq.rb
@@ -3,9 +3,9 @@ cask 'commandq' do
   sha256 '1183a2baf3775bd47851c94636ebe41356e4b53fce1dd35b4b70b9298294a107'
 
   url "http://dl.clickontyler.com/commandq/commandq_#{version}.zip"
-  name 'CommandQ'
   appcast 'https://shine.clickontyler.com/appcast.php?id=16',
           :sha256 => 'aded9f4d5543c963c0989f53ba18c35b2053703b543b10a54c61a6b42b53dd50'
+  name 'CommandQ'
   homepage 'https://clickontyler.com/commandq/'
   license :commercial
 
