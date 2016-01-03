4d2c8ad3126b67fe210fa03662179292bc0dcf3f
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:00 -0500

    myphotostream.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/myphotostream.rb
+++ Casks/myphotostream.rb
@@ -3,9 +3,9 @@ cask 'myphotostream' do
   sha256 :no_check
 
   url 'http://www.weareyeah.com/MyPhotostream/download'
-  name 'MyPhotostream'
   appcast 'http://www.weareyeah.com/MyPhotostream/feeds/update.appcast',
           :sha256 => 'a310ba257cb8c82100ad68ea5332ace6b8204629d671622691cec559e783f686'
+  name 'MyPhotostream'
   homepage 'http://www.weareyeah.com/MyPhotostream/'
   license :commercial
 
