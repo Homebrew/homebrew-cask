00cc36794e45389a0fd2538210cbd31b3a9617c2
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:41 -0500

    jameica.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/jameica.rb
+++ Casks/jameica.rb
@@ -3,11 +3,11 @@ cask 'jameica' do
   sha256 :no_check
 
   url 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-macos64.zip'
-  gpg "#{url}.asc",
-      :key_id => '5a8ed9cfc0db6c70'
   name 'Jameica'
   homepage 'https://www.willuhn.de/products/jameica/'
   license :gpl
+  gpg "#{url}.asc",
+      :key_id => '5a8ed9cfc0db6c70'
 
   app 'Jameica.app'
 end
