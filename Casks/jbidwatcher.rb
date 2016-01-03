96c176d2a00e6a36415ff2a276adfcf1edeff1ca
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:41 -0500

    jbidwatcher.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/jbidwatcher.rb
+++ Casks/jbidwatcher.rb
@@ -3,9 +3,9 @@ cask 'jbidwatcher' do
   sha256 '659de073d9e0d71fa86e21db1524b7df57b36ff7826600a01ed93da6df5f5c9f'
 
   url "https://www.jbidwatcher.com/download/JBidwatcher-#{version}.dmg"
-  name 'JBidwatcher'
   appcast 'https://www.jbidwatcher.com/sparkle/updates.xml',
           :sha256 => 'aa8cc693e2f0047a2214f990d5f418f026acf473dcb1f0f76204f7fee730077b'
+  name 'JBidwatcher'
   homepage 'https://www.jbidwatcher.com'
   license :cc
 
