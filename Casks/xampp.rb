2e8039ac4a32bab5738cc2544ab19f0b9f4a78c9
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:59 -0500

    xampp.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -3,14 +3,14 @@ cask 'xampp' do
   sha256 '23cd1b990397958b31e5904a340f416aed98b767a868bbcad7be123f53878828'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/project/xampp/XAMPP%20Mac%20OS%20X/#{version.sub(%r{-\d+$},'')}/xampp-osx-#{version}-installer.dmg"
+  url "http://downloads.sourceforge.net/project/xampp/XAMPP%20Mac%20OS%20X/#{version.sub(%r{-\d+$}, '')}/xampp-osx-#{version}-installer.dmg"
   name 'XAMPP'
   homepage 'https://www.apachefriends.org/'
   license :gpl
 
   installer :script => 'XAMPP.app/Contents/MacOS/osx-intel',
-            :args => [ '--mode', 'unattended' ]
+            :args   => ['--mode', 'unattended']
 
-  uninstall :quit => 'com.bitnami.manager',
+  uninstall :quit   => 'com.bitnami.manager',
             :delete => '/Applications/XAMPP/'
 end
