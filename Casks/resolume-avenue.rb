565880b1f1ec48ddb936cdda6494f0ed5767282d
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:21 -0500

    resolume-avenue.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/resolume-avenue.rb
+++ Casks/resolume-avenue.rb
@@ -3,7 +3,7 @@ cask 'resolume-avenue' do
   sha256 '996306b9ff0b4ec99e8b5338e8153c07e5ff194ae3ec7f350d791b8b336deb95'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "http://download.resolume.com.s3.amazonaws.com/Resolume_Avenue_#{version.gsub('.','_')}_Installer.dmg"
+  url "http://download.resolume.com.s3.amazonaws.com/Resolume_Avenue_#{version.gsub('.', '_')}_Installer.dmg"
   name 'Resolume'
   name 'Resolume Avenue'
   homepage 'https://resolume.com'
