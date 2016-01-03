8e384efcf25a99b841e95a4d1a9284ffadb416b4
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:05 -0500

    curb.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/curb.rb
+++ Casks/curb.rb
@@ -2,7 +2,7 @@ cask 'curb' do
   version '1.1.1'
   sha256 '832750d2a75272763c5c2f681b11670584626c9d93bf993d6b3af96234558f68'
 
-  url "https://mrrsoftware.com/Downloads/Curb/Curb-#{version.gsub('.','_')}.zip"
+  url "https://mrrsoftware.com/Downloads/Curb/Curb-#{version.gsub('.', '_')}.zip"
   appcast 'https://www.mrrsoftware.com/Downloads/Curb/CurbSoftwareUpdates.xml',
           :sha256 => '2025c4fc40a80f7ca7fb4076deb357a133990ef0e9576ceb5a5dca5f03a7c500'
   name 'Curb'
