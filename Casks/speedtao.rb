2808f01e3e1e3aa42f210578a75082267604d93b
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:33 -0500

    speedtao.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/speedtao.rb
+++ Casks/speedtao.rb
@@ -3,11 +3,11 @@ cask 'speedtao' do
   sha256 :no_check
 
   url 'http://www.speedtao.net/beta_latest'
-  name 'SpeedTao'
   appcast 'http://speedtao.net/su/su.xml',
           :sha256 => '335236df2877584cd9515ecd53a9c1dcc680182511665455a1cffd505f8769fe'
+  name 'SpeedTao'
   homepage 'http://www.speedtao.net/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'SpeedTao.app'
 end
