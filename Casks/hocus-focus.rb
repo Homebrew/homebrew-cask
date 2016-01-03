d08c362d673fc5e3154e85c9f7abb67d2f1e1b61
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:31 -0500

    hocus-focus.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/hocus-focus.rb
+++ Casks/hocus-focus.rb
@@ -3,15 +3,17 @@ cask 'hocus-focus' do
   sha256 '8a51419112d9626497217a9953460c075480a139e1b1a47b175454be8a04f85a'
 
   url "http://hocusfoc.us/static/releases/hocusfocus-#{version}.zip"
-  name 'Hocus Focus'
   appcast 'http://hocusfoc.us/updates.xml',
           :sha256 => 'afdeac54f1465741719415aa1db437888cad07486fe6bc90f5a8cee959ce39c6'
+  name 'Hocus Focus'
   homepage 'http://hocusfoc.us/'
   license :gratis
+
   app 'Hocus Focus.app'
+
   zap :delete => [
-                  '~/Library/Caches/com.uglyapps.HocusFocus',
-                  '~/Library/Application Support/com.uglyapps.HocusFocus',
-                  '~/Library/Preferences/com.uglyapps.HocusFocus.plist',
+                   '~/Library/Caches/com.uglyapps.HocusFocus',
+                   '~/Library/Application Support/com.uglyapps.HocusFocus',
+                   '~/Library/Preferences/com.uglyapps.HocusFocus.plist',
                  ]
 end
