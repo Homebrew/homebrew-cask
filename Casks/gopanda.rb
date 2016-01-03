1b6db20a49e00d44610630ed0502143f241b3a29
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:28 -0500

    gopanda.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/gopanda.rb
+++ Casks/gopanda.rb
@@ -5,7 +5,7 @@ cask 'gopanda' do
   url 'http://pandanet-igs.com/gopanda2/installer/stable/mac-32/gopanda2-mac-32.zip'
   name 'GoPanda'
   homepage 'http://pandanet-igs.com/communities/gopanda2'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'GoPanda2.app'
 end
