8ef814d13cfe821715fe18a48f451559aef20447
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:10 -0500

    pdfpenpro.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/pdfpenpro.rb
+++ Casks/pdfpenpro.rb
@@ -3,9 +3,9 @@ cask 'pdfpenpro' do
   sha256 :no_check
 
   url 'https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/PDFpenPro.zip'
-  name 'PDFpenPro'
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml',
           :sha256 => '20c1ab602462b7fc0d5b4cbd555cacf127b69a07a737579598ebcbc0f5b21319'
+  name 'PDFpenPro'
   homepage 'https://www.smilesoftware.com/PDFpenPro/'
   license :commercial
 
