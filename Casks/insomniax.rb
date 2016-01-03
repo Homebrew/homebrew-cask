33e73e08d3b426ce5e704fd77f8914e0b305f7bc
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:36 -0500

    insomniax.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/insomniax.rb
+++ Casks/insomniax.rb
@@ -3,9 +3,9 @@ cask 'insomniax' do
   sha256 'abbf4b27e86da934042a1dcedd339ffb59e16c4aa8e0c0d44534533be42e3efb'
 
   url "http://insomniax.semaja2.net/InsomniaX-#{version}.tgz"
-  name 'InsomniaX'
   appcast 'http://insomniax.semaja2.net/profile/profileInfo.php',
           :sha256 => '834f8bedbd0037f7e65a47c62939f2073f6b1efc5014ddd3f3d6e37650c246cb'
+  name 'InsomniaX'
   homepage 'https://semaja2.net/projects/insomniaxinfo/'
   license :gratis
 
