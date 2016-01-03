a29c834c0f42e6c84d46bd802b74a1741046e29f
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:56 -0500

    midi-monitor.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/midi-monitor.rb
+++ Casks/midi-monitor.rb
@@ -9,20 +9,20 @@ cask 'midi-monitor' do
   homepage 'https://www.snoize.com/MIDIMonitor/'
   license :bsd
 
+  depends_on :macos => '>= :lion'
+
   app 'MIDI Monitor.app'
 
   uninstall :quit => [
-                      'com.snoize.MIDIMonitor',
-                      'com.snoize.MIDIMonitorDriver',
-                      'com.snoize.MIDISpyFramework',
-                      'com.snoize.SnoizeMIDI'
+                       'com.snoize.MIDIMonitor',
+                       'com.snoize.MIDIMonitorDriver',
+                       'com.snoize.MIDISpyFramework',
+                       'com.snoize.SnoizeMIDI',
                      ]
 
   zap :delete => [
-                  '~/Library/Preferences/com.snoize.MIDIMonitor.plist',
-                  '~/Library/Caches/com.snoize.MIDIMonitor',
-                  '~/Library/Saved Application State/com.snoize.MIDIMonitor.savedState'
+                   '~/Library/Preferences/com.snoize.MIDIMonitor.plist',
+                   '~/Library/Caches/com.snoize.MIDIMonitor',
+                   '~/Library/Saved Application State/com.snoize.MIDIMonitor.savedState',
                  ]
-
-  depends_on :macos => '>= :lion'
 end
