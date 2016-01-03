d5cae04ba84e915b51bdba30094ea9d4dd9bf39a
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:31 -0500

    hiss.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/hiss.rb
+++ Casks/hiss.rb
@@ -7,7 +7,7 @@ cask 'hiss' do
           :sha256 => 'de44a2a8044a10edde7cdccc8a14419ac243fb2e8b66702677e7052416fd711f'
   name 'Hiss'
   homepage 'http://collect3.com.au/hiss/'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Hiss.app'
 end
