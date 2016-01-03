884491bd5f1ff33accae428c93c3527bef5e9343
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:17:58 -0500

    cave-story.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/cave-story.rb
+++ Casks/cave-story.rb
@@ -3,7 +3,7 @@ cask 'cave-story' do
   sha256 '305e9ec85442e79e92c0e4fd2d8e6566c5e0ad8db46b50aad172f3e0c384a479'
 
   # nakiwo.com is the official download host per the vendor homepage
-  url "http://www.nakiwo.com/attachments/doukutsu#{version.gsub('.','_')}.dmg"
+  url "http://www.nakiwo.com/attachments/doukutsu#{version.gsub('.', '_')}.dmg"
   name 'Pixel Cave Story'
   name 'Doukutsu'
   name '洞窟物語'
