6a42efabbf4bbf6b5fa768c8d59c2f1a73403c1b
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:13 -0500

    eagle.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -3,7 +3,7 @@ cask 'eagle' do
   sha256 '1abad61ebd8dd42dca64c289dccb38f39c2fe9c67d3313d53ec31e6bad74870b'
 
   # cadsoft.de is the official download host per the vendor homepage
-  url "http://web.cadsoft.de/ftp/eagle/program/#{version.sub(%r{\.\d+$},'')}/eagle-mac64-#{version}.zip"
+  url "http://web.cadsoft.de/ftp/eagle/program/#{version.sub(%r{\.\d+$}, '')}/eagle-mac64-#{version}.zip"
   name 'CadSoft EAGLE'
   homepage 'https://www.cadsoftusa.com/'
   license :freemium
@@ -11,5 +11,5 @@ cask 'eagle' do
   pkg "eagle-mac64-#{version}.pkg"
 
   uninstall :pkgutil => 'com.CadSoftComputerGmbH.EAGLE',
-            :delete => "/Applications/EAGLE-#{version}"
+            :delete  => "/Applications/EAGLE-#{version}"
 end
