7b81caca05b811d65002c51f0fd9c453c6ae7dfb
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:13 -0500

    pixlr.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/pixlr.rb
+++ Casks/pixlr.rb
@@ -3,11 +3,11 @@ cask 'pixlr' do
   sha256 'd4ee65c63fa307da05ba54d641f081c951581976655507efbcd466143a4afa92'
 
   url "https://cdn2.pixlr.com/mac/Autodesk_Pixlr_v#{version}_Mac_Installer.dmg"
-  name 'Autodesk Pixlr'
   appcast 'https://updatefeed.pixlr.com/pixlr-for-mac-appfeed.xml',
           :sha256 => '46d01575b9fd926b62093e9d5614544428f49a5f1013217b92c29b2dbed78966'
+  name 'Autodesk Pixlr'
   homepage 'https://pixlr.com'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Pixlr.app'
 end
