e0edd57e79ea52e333a5af91235b3789ceed7369
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:41 -0500

    textsoap.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/textsoap.rb
+++ Casks/textsoap.rb
@@ -4,9 +4,9 @@ cask 'textsoap' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://unmarked.s3.amazonaws.com/textsoap7.zip'
-  name 'TextSoap'
   appcast 'https://unmarked.s3.amazonaws.com/appcast/textsoap7.xml',
           :sha256 => '37211786d3aca6f891665256f383906a337bea31958b4b7a2ff0fbbaae98709e'
+  name 'TextSoap'
   homepage 'http://www.unmarked.com/textsoap/'
   license :commercial
 
