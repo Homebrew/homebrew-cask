ec549e708f8e1d9e324ba033e2b7baa94531407b
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:54 -0500

    mdrp.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/mdrp.rb
+++ Casks/mdrp.rb
@@ -3,9 +3,9 @@ cask 'mdrp' do
   sha256 :no_check
 
   url 'http://www.macdvdripperpro.com/download/'
-  name 'Mac DVDRipper Pro'
   appcast 'https://www.macdvdripperpro.com/mdrp_sparkle5.xml',
           :sha256 => '8e4f7c395dda29438cf1f4a931a626d484886109a01ea9ee733fa6d7c24f4634'
+  name 'Mac DVDRipper Pro'
   homepage 'https://www.macdvdripperpro.com/'
   license :commercial
 
