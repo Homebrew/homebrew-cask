2e3786e786bca2fd0cc6d9dfedd0d13214003251
Victor Popkov <victor@popkov.me>
2016-01-03 19:58:25 +0200

    cdock.rb: upgrade to v0.9.11

--- Casks/cdock.rb
+++ Casks/cdock.rb
@@ -1,10 +1,10 @@
 cask 'cdock' do
-  version '0.9.10'
-  sha256 '92e231a8d16117acd1687c169dff19caecc9cdeaa5997dd65b5de8ea3007b527'
+  version '0.9.11'
+  sha256 'da1657f0c92dac0b3d12c129f420b53471231da180d2ef2b6019ce5665f91b18'
 
   url "https://github.com/w0lfschild/cDock2/releases/download/v#{version}/cDock.zip"
   appcast 'https://github.com/w0lfschild/cDock2/releases.atom',
-          :sha256 => '14a8f05bfc3b3b6ad7c068700ffef35f4c25f8554c7ddeb4114c4f894c61ee5c'
+          :sha256 => '8b23380512d766d9773b145642876ab353987f2efe47efa2b42168ba7154aab3'
   name 'cDock2'
   name 'cDock'
   homepage 'https://w0lfschild.github.io/cdock'
