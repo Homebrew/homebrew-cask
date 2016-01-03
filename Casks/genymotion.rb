3d8adc26c512ac7eda3795f0a40321dd44071c53
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:25 -0500

    genymotion.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -2,12 +2,12 @@ cask 'genymotion' do
   version '2.6.0'
   sha256 '9d12ae904761d76b15a556262d7eb32d1f5031fe60690224d7b0a70303cf8d39'
 
-  depends_on :cask => 'virtualbox'
-
   url "http://files2.genymotion.com/genymotion/genymotion-#{version}/genymotion-#{version}.dmg"
   name 'Genymotion'
   homepage 'https://www.genymotion.com/'
   license :commercial
+
+  depends_on :cask => 'virtualbox'
 
   app 'Genymotion.app'
   app 'Genymotion Shell.app'
