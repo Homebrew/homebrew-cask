6a37e9cb5dfb37a0e75959cd081ac0e370eeb7d8
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:18:57 -0500

    moneywell.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/moneywell.rb
+++ Casks/moneywell.rb
@@ -8,7 +8,7 @@ cask 'moneywell' do
     version '1.7.3'
     sha256 '6f34a57999fcc09147035f62caff7efede3a385c0f2e7b6f3f90e9a64e826410'
 
-    url "http://downloads.nothirst.com/MoneyWell_#{version.sub(%r{^(\d+)\.(\d+).*},'\1\2')}.zip"
+    url "http://downloads.nothirst.com/MoneyWell_#{version.sub(%r{^(\d+)\.(\d+).*}, '\1\2')}.zip"
   else
     version '2.3.9'
     sha256 '0d262ee3c816e30088746d56f71475c46da82362d1edb3d50cf5dd416bd35a6f'
