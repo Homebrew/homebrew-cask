c6a3667fba0aee51e89d291ebecee9f671b361ff
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:57 -0500

    woodhouse.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/woodhouse.rb
+++ Casks/woodhouse.rb
@@ -3,9 +3,9 @@ cask 'woodhouse' do
   sha256 '3ed1b6711eb9ec862e34addc141d681d9b0fed2950b9d9b7189734087eeb7541'
 
   url "https://github.com/downloads/phinze/woodhouse/Woodhouse-#{version}.dmg"
-  name 'Woodhouse'
   appcast 'https://phinze.github.com/woodhouse/appcast.xml',
           :sha256 => '60e19f2463f90a57417229593f456df23d1ba55781d305db7ac5d47d0ecbcf79'
+  name 'Woodhouse'
   homepage 'https://github.com/phinze/woodhouse/'
   license :mit
 
