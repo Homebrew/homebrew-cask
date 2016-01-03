50120b63b6ff2bc061eb2a2a1f9e25912cb53255
Josh Hagins <hagins.josh@gmail.com>
2016-01-03 14:19:44 -0500

    totals.rb: RuboCop (master), RuboCop-cask (master) auto-correct

--- Casks/totals.rb
+++ Casks/totals.rb
@@ -3,9 +3,9 @@ cask 'totals' do
   sha256 'f3577a93423d228e8c6e640f5c87e1d57f40300e73d33a64aa28b68cc984e8c7'
 
   url "http://www.kedisoft.com/dl/totals/Totals-#{version}.dmg"
-  name 'Totals'
   appcast 'http://kedisoft.com/appcasts/appcast.php?app=totals3',
           :sha256 => 'd0a003df02921bfbeb93a0f225df86d706b648a76ca8ec01bca4063b27f3127b'
+  name 'Totals'
   homepage 'http://www.kedisoft.com/totals/'
   license :commercial
 
