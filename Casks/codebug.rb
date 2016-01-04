a25a5dba7bdf93d8733ae1d77d1c593f0ad665f1
Bradyn Poulsen <bradyn@actinium.local>
2016-01-04 12:43:55 -0700

    Upgrade Codebug.app to latest

--- Casks/codebug.rb
+++ Casks/codebug.rb
@@ -1,10 +1,8 @@
 cask 'codebug' do
-  version '0.9.4'
-  sha256 '2a568b2eff8145823b552668d0b1caa75a98077afe7692ccb69ca030508c86b9'
+  version :latest
+  sha256 :no_check
 
-  url "http://codebugapp.com/downloads/Codebug#{version.gsub('.', '-')}.zip"
-  appcast 'http://codebugapp.com/update.xml',
-          :sha256 => '70782ae334e666a9ce810ce2d4f60df63fdb02fade4a266eca0f66341a299788'
+  url 'http://codebugapp.com/downloads/Codebug.dmg'
   name 'Codebug'
   homepage 'http://www.codebugapp.com/'
   license :commercial
