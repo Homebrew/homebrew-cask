ca13e2d49833146034e67cb0de79b815bdddc21b
Victor Popkov <victor@popkov.me>
2016-01-04 20:49:44 +0200

    pritunl.rb: upgrade to v0.10.16

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,11 +1,11 @@
 cask 'pritunl' do
-  version '0.10.14'
-  sha256 'afa302d8e95d1635584673a3c206d370c93846f519735d9898e75af43d9bd3a8'
+  version '0.10.16'
+  sha256 '3179f88bf28d0a245cb140414f90f9d5ad13fdef3cefc88764f984e1ca484732'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip"
   appcast 'https://github.com/pritunl/pritunl-client-electron/releases.atom',
-          :sha256 => 'bc21320479679d2db525581edfaf33e0f8abab534e658aa190eb973a0050ca98'
+          :sha256 => '851b79be1bdadb1d745c10793fb705fca96a49f185894f660aacc7d5ef5182fe'
   name 'Pritunl OpenVPN Client'
   homepage 'https://client.pritunl.com'
   license :gpl
