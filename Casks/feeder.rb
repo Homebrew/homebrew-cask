1d5cb4f31d0076c234840d32f18336a282f7eec8
Victor Popkov <victor@popkov.me>
2016-01-04 20:40:19 +0200

    feeder.rb: upgrade to v3.1.4

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -1,10 +1,10 @@
 cask 'feeder' do
-  version '3.1.3'
-  sha256 '39205ffc12e40e447d7cabc758d3a1872fdb066aab5a13b0d21f7c4ab54c1c9c'
+  version '3.1.4'
+  sha256 '925cbf17149dc84632be10a142a0f238231ce72d8736528a872c9d44f47e451d'
 
   url "https://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
   appcast 'https://reinventedsoftware.com/feeder/downloads/Feeder3.xml',
-          :sha256 => '02f785d157d509a1c10911b3e108ed41fbb8590ad2d51b4039e070bf6f1a2081'
+          :sha256 => '4daf79a0cb6b322065af5776464cb1d034561cfde3d6d7e482b90b968ad33fd4'
   name 'Feeder'
   homepage 'https://reinventedsoftware.com/feeder/'
   license :commercial
