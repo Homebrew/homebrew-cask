23855668b2ad88ea9f73327bfc9a6950d9fe1d4a
Thibault Milan <hello@thibaultmilan.com>
2016-01-05 09:43:37 +0100

    Add Google Chrome Beta v48.0.2564.48

---
+++ Casks/google-chrome-beta.rb
@@ -0,0 +1,12 @@
+cask 'google-chrome-beta' do
+  version '48.0.2564.48'
+  sha256 'd5e9b79bc9e944dbe624c054196e5f62129b2ba2f2368b5c266e979a8986effd'
+
+  url 'https://dl.google.com/chrome/mac/beta/GoogleChrome.dmg'
+  name 'Google Chrome Beta'
+  homepage 'https://www.google.fr/chrome/browser/beta.html'
+  license :gratis
+
+  app 'Google Chrome'
+end
+
