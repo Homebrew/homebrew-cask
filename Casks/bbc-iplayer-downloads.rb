49a4b2ac08fa76790f15e752d89e44c1ae7e986c
Vítor Galvão <info@vitorgalvao.com>
2016-01-06 23:23:22 +0000

    updated bbc-iplayer-downloads (1.14.2)

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -1,11 +1,11 @@
 cask 'bbc-iplayer-downloads' do
-  version '1.14.1'
-  sha256 'a895f1473d9f0d8e9f185bb2a98c0cd56ccf0c4c9b05b8fe3ae705bcf88fcf40'
+  version '1.14.2'
+  sha256 'feb02a935380805598b211a94c28466f0e064bfc938767ddc084a37a8c4719ee'
 
   # bbci.co.uk is the official download host per the vendor homepage
   url "https://a.files.bbci.co.uk/iplayer/downloads/BBC-iPlayer-Downloads-#{version}.dmg"
   appcast 'http://ipd-hq.cloud.bbc.co.uk/downloads/update.xml',
-          :sha256 => '58f93571ce9fbcbf08fc7f6b14e587cbdacd985c844cf3767876af0a8bce1de2'
+          :sha256 => 'dbdc992225590fb6e644c720af73d60a5c821347bc43ddeb2681ec30935b2cdb'
   name 'BBC iPlayer Downloads'
   homepage 'http://www.bbc.co.uk/iplayer/install'
   license :gratis
