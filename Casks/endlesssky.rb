8114edf3e31c8f14a2829ae33c48659ce2fb1b7e
Victor Popkov <victor@popkov.me>
2016-01-06 20:44:44 +0200

    endlesssky.rb: upgrade to v0.8.10

--- Casks/endlesssky.rb
+++ Casks/endlesssky.rb
@@ -1,10 +1,10 @@
 cask 'endlesssky' do
-  version '0.8.9'
-  sha256 'adb12878ba4bc97eae7fb387ecb206d297540a8cb21da793955c854781911cc0'
+  version '0.8.10'
+  sha256 'b13e6742ed17cc504e08edbaeb9ff8bcbf0e7b6c09715c03f6443bd2bbf8aa2f'
 
   url "https://github.com/endless-sky/endless-sky/releases/download/v#{version}/endless-sky-macosx-#{version}.dmg"
   appcast 'https://github.com/endless-sky/endless-sky/releases.atom',
-          :sha256 => 'a0de0f323e8945e33cbf3a06aab1f2b265db372480f5f2db92c93e479c63de02'
+          :sha256 => '7a27a907fc6892c617c767f6919375755746d07333c32f92aa513f1ac3749d66'
   name 'Endless Sky'
   homepage 'https://endless-sky.github.io/'
   license :gpl
