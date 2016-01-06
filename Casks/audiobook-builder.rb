7dda2d66ebadd278d96d788c4f9c9810cb1570af
Vítor Galvão <info@vitorgalvao.com>
2016-01-06 23:20:28 +0000

    updated audiobook-builder (1.5.4)

--- Casks/audiobook-builder.rb
+++ Casks/audiobook-builder.rb
@@ -1,10 +1,10 @@
 cask 'audiobook-builder' do
-  version '1.5.3'
-  sha256 '8ae30fcc3811292c84a23febb50c78e110bc7514f0d3085ce21b427f45be5d4c'
+  version '1.5.4'
+  sha256 '8ba09725221c44c991d934334d8e6562bcf7408a2faafdb42236ef4c9244def3'
 
-  url "http://www.splasm.com/downloads/audiobookbuilder/Audiobook%20Builder%20#{version}.zip"
+  url "http://www.splasm.com/downloads/audiobookbuilder/Audiobook%20Builder%20#{version}.dmg"
   appcast 'http://www.splasm.com/versions/audiobookbuilder_sparkle.xml',
-          :sha256 => '0fa38f9d15f10be75de37b6ea556a486e9073b9a176fae5a2f6e8602b596ed5c'
+          :sha256 => '368a969cda2bd66baa7b8c24c7b861cee29c86d2fb7a8e9392bcddc87ac65e2b'
   name 'Audiobook Builder'
   homepage 'http://www.splasm.com/audiobookbuilder/'
   license :commercial
