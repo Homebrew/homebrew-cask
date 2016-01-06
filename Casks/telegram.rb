75dbbbc82ebb2cee0d2a8c5b23a0dd37a338ecd4
mikuhime <miku@miku.io>
2016-01-06 17:43:01 +0100

    Update telegram to 0.9.17

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '0.9.15'
-  sha256 '68716e849af816a76c4ca91b27635fbc927d5952055e3813f8429b6f8d1a4512'
+  version '0.9.17'
+  sha256 'e78f36628426bb4e842d7643ab1f2b46844cb1a65766393e0ffcdb73139f7a04'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
