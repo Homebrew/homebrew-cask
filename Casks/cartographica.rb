afb4cd157d19e6756747a09551e2c9fb5726b69b
Vítor Galvão <info@vitorgalvao.com>
2016-01-06 23:46:06 +0000

    updated cartographica (1.4.6,610)

--- Casks/cartographica.rb
+++ Casks/cartographica.rb
@@ -1,11 +1,11 @@
 cask 'cartographica' do
-  version '1.4.6_610'
+  version '1.4.6,610'
   sha256 '8f8db101e6bfce04aaea71a70c3eef210fc12284ca26f963db883a2f62d86ebe'
 
   # cluetrust.com is the official download host per the vendor homepage
-  url "https://www.cluetrust.com/Downloads/Cartographica_#{version.sub(%r{.*_}, '')}.dmg"
+  url "https://www.cluetrust.com/Downloads/Cartographica_#{version.after_comma}.dmg"
   appcast 'https://www.cluetrust.com/AppCasts/Cartographica.xml',
-          :sha256 => '9bb9075d8da457886cb86043d4807cbdd210e9f6c883f99ade7898911f90dd35'
+          :sha256 => '836d6694d66c60a7c151b0859f59532d9ebc2803030f7bd6d05f04c2faea5deb'
   name 'Cartographica'
   homepage 'https://www.macgis.com/'
   license :commercial
