cf5e776896cb7a2cfc7bca90e57c87ff1831ba0a
Vítor Galvão <info@vitorgalvao.com>
2016-01-06 22:45:41 +0000

    updated airdisplay (3.0.1)

--- Casks/airdisplay.rb
+++ Casks/airdisplay.rb
@@ -3,8 +3,8 @@ cask 'airdisplay' do
   sha256 '2da921517a94872b6909b225d8964081a1bad9d2b93be12592bd264665f4b769'
 
   url "https://www.avatron.com/updates/software/airdisplay/ad#{version.delete('.')}.zip"
-  appcast 'https://www.avatron.com/updates/software/airdisplay/appcast.xml',
-          :sha256 => '5318742e7d9f7f4da9498e3100d8b5f92abc18a574988f1d5fa5a551ad0af062'
+  appcast 'https://avatron.com/updates/software/airdisplay/appcast.xml',
+          :sha256 => '0cbe95610889c4f22939d50e9b0e1d1117cc3559a7ca0a679e7f7adbc7b262b2'
   name 'Air Display'
   homepage 'https://avatron.com/apps/air-display/'
   license :commercial
