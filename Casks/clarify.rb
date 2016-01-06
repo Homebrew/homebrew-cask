452542911e964d5dbb350667fc69cd0b20ade003
Vítor Galvão <info@vitorgalvao.com>
2016-01-06 23:51:04 +0000

    updated clarify (2.0.5r4)

--- Casks/clarify.rb
+++ Casks/clarify.rb
@@ -2,9 +2,9 @@ cask 'clarify' do
   version '2.0.5r4'
   sha256 '454bff1e655bd8aee1c5bcb3c85339b2d09a386637b1cdb0246ab849c6283654'
 
-  url "http://files.clarify-it.com/v2/updaters/#{version}/Clarify.app.zip"
+  url "http://files.clarify-it.com/v#{version.major}/updaters/#{version}/Clarify.app.zip"
   appcast 'http://www.bluemangolearning.com/download/clarify/2_0/auto_update/release/clarify_appcast.xml',
-          :sha256 => '4e03cb35a628946e3ea21ba547be2488f792f74364198d365f8f69d9ce4c93cb'
+          :sha256 => 'a00fef4fcfea2fc544d10b47553f7c69524afd0b8be165d2373477e016811e3e'
   name 'Clarify'
   homepage 'http://www.clarify-it.com/'
   license :commercial
