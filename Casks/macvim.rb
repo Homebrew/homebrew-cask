5c7cebb85d55e4402889b077e8e0c6133632e6e1
Jonas Haag <jonas@lophus.org>
2016-01-07 22:11:26 +0100

    Updated MacVim

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,6 +1,6 @@
 cask 'macvim' do
-  version '7.4-85'
-  sha256 'b83c8f584a2a7928edb7e9c5ce931ed66f9265c18f014ca4e377e0fa8062edc3'
+  version '7.4-89'
+  sha256 '779928de48eabc3f90a830e441affb0f28e370881737d2b3f4bf173c41a1798f'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/macvim-dev/macvim/releases/download/snapshot-#{version.sub(%r{^.*-}, '')}/MacVim-#{version.sub(%r{-.*$}, '')}.dmg"
