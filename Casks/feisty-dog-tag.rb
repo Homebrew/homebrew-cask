0125a4573a9ba2b0f903b140d609d5ef962de7be
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 20:10:19 +0000

    updated feisty-dog-tag (1.0.4,010)

--- Casks/feisty-dog-tag.rb
+++ Casks/feisty-dog-tag.rb
@@ -1,11 +1,11 @@
 cask 'feisty-dog-tag' do
-  version :latest
-  sha256 :no_check
+  version '1.0.4,010'
+  sha256 '1bfc83427aa474d9df3c5413ee5645a0e5b134ceef642bf99a717af1798e72df'
 
-  # amazonaws.com is the official download host per the vendor homepage
-  url 'https://s3.amazonaws.com/feisty-dog/Tag.zip'
+  # amazonaws.com is the official download host per the appcast feed
+  url "http://s3.amazonaws.com/feisty-dog/versions/binaries/000/000/#{version.after_comma}/Tag-#{version.before_comma}.zip"
   appcast 'https://www.feisty-dog.com/panel/updates/Tag',
-          :sha256 => '9e3d6f54c3bca3928a340c96fece9866131d77b8723dc86cc898fab3264d5807'
+          :sha256 => '775f68c198856332ee89dfbe2f115d3a9cc72cb44d1bf701acfdc43eca0c6a02'
   name 'Tag'
   homepage 'https://www.feisty-dog.com/tag/'
   license :commercial
