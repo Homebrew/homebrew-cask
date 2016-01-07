4efba1d305530329cb35d57d0685f55dacd66966
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 19:25:22 +0000

    updated ember (1.8.3)

--- Casks/ember.rb
+++ Casks/ember.rb
@@ -2,9 +2,9 @@ cask 'ember' do
   version '1.8.3'
   sha256 'cbd61a207b87a4668907b187198c62bc0b89dfdc31896cb5cbefcf95e2e8f1d1'
 
-  url "http://downloads.realmacmedia.com/ember/ember-#{version.delete('.')}.zip"
-  appcast 'http://www.realmacsoftware.com/feeds/ember.xml',
-          :sha256 => '5e37758cec9f0dd8b619f8cc40a257bd1ac1635fd1b2d28e0bd651bf2864dd6d'
+  url "http://downloads.realmacmedia.com/ember/ember-#{version.no_dots}.zip"
+  appcast 'http://realmacsoftware.com/feeds/ember.xml',
+          :sha256 => '37fd752918a4a295e9bf07a2b4634a61bac1f07123585a4a35c8ec988ac354c1'
   name 'Ember'
   homepage 'http://realmacsoftware.com/ember'
   license :commercial
