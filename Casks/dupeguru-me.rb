a69d2fa3115c573c1aecb578b8f049aae6f00aba
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 01:18:49 +0000

    updated dupeguru-me (6.8.1)

--- Casks/dupeguru-me.rb
+++ Casks/dupeguru-me.rb
@@ -2,9 +2,9 @@ cask 'dupeguru-me' do
   version '6.8.1'
   sha256 '65720a2cdd17242c30ce848133a6c53e018ea0088f1990fee8d45114c921a118'
 
-  url "https://download.hardcoded.net/dupeguru_me_osx_#{version.gsub('.', '_')}.dmg"
+  url "https://download.hardcoded.net/dupeguru_me_osx_#{version.dots_to_underscores}.dmg"
   appcast 'https://www.hardcoded.net/updates/dupeguru_me.appcast',
-          :sha256 => '06048bd2a5358e315d2e53c7110d593d2e6d1565fb742e53f2c7ac3cb07a8cc3'
+          :sha256 => '63c7ff19e3f4f83bd4bdf75c42e9ece7b3a5a0552b7b13160f62712a67551309'
   name 'dupeGuru Music Edition'
   homepage 'https://www.hardcoded.net/dupeguru_me/'
   license :bsd
