1a9d49a696651d9fd6ac8e42dfdd2021f3846e73
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 01:02:29 +0000

    updated docker-machine (0.5.5)

--- Casks/docker-machine.rb
+++ Casks/docker-machine.rb
@@ -1,11 +1,11 @@
 cask 'docker-machine' do
-  version '0.5.2'
-  sha256 '298a9d51d00c3b01c7b74853f19638467a07b6cf954d3116c0252232879de42e'
+  version '0.5.5'
+  sha256 'e19a74e4d9cce8dc50ab16a1f478fe5d6953bda9f794939032b3a1e84cbf6eb2'
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com/docker/machine/releases/download/v#{version}/docker-machine_darwin-amd64.zip"
+  url "https://github.com/docker/machine/releases/download/v#{version}/docker-machine_darwin-amd64"
   appcast 'https://github.com/docker/machine/releases.atom',
-          :sha256 => '2840ae55fa5e849ab523eae9f9d6c2a2cd055d42503caa055cdb43120baee94c'
+          :sha256 => '1db29b8a10da6c5b1781bb56048a30358e7e3979d6eba0102ed813ca1ccab2fb'
   name 'Docker Machine'
   homepage 'https://docs.docker.com/machine'
   license :apache
