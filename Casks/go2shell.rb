237dcf58cbe75fcb5c105819ec6f64409581deb8
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 21:05:29 +0000

    updated go2shell (2.3)

--- Casks/go2shell.rb
+++ Casks/go2shell.rb
@@ -1,10 +1,10 @@
 cask 'go2shell' do
-  version :latest
-  sha256 :no_check
+  version '2.3'
+  sha256 '8cdfbe43a9e0a6728cb2bc5f74bdf0c4087e6d033b829acb68cb5ceaddda44a3'
 
   url 'http://zipzapmac.com/download/Go2Shell'
   appcast 'http://appcast.zipzapmac.com/go2shell.xml',
-          :sha256 => '2a824962eb4bf47c11f69ce020dc07ad423d1c1d47df49e754fceb8b70ec2b1d'
+          :sha256 => '8f3455bdf8903acfce6957f3e91f639dd378d989ceb951c69a8e919b7170ffb7'
   name 'Go2Shell'
   homepage 'http://zipzapmac.com/go2shell'
   license :gratis
