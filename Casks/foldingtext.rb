b83da71cbce2a7cdd06ce2f3771c541ad6c23fe6
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 20:16:22 +0000

    updated foldingtext (2.1.1)

--- Casks/foldingtext.rb
+++ Casks/foldingtext.rb
@@ -1,11 +1,11 @@
 cask 'foldingtext' do
-  version '2.1'
-  sha256 '007e882d1d09273accf84d95ed15e89db056da9bebf81258098d3efb38ce6df2'
+  version '2.1.1'
+  sha256 '7cdef677ccf9df331baf1650d2fded5ca305116c8cf74c7d8969318f2f7b9666'
 
   # amazonaws.com is the official download host per the appcast feed
-  url "https://foldingtext.s3.amazonaws.com/FoldingText-#{version}.dmg"
+  url "https://FoldingText.s3.amazonaws.com/FoldingText-#{version}.dmg"
   appcast 'https://foldingtext.s3.amazonaws.com/FoldingText.rss',
-          :sha256 => 'edb29d497e4b36ad241ff05f35fcafb4c3712c8b1062b22a1dbd3ed8cb0354df'
+          :sha256 => '837cd66a00b3d55670ae49ff0d786b980be5ed2517aab352c285e04c189a646a'
   name 'FoldingText'
   homepage 'http://www.foldingtext.com'
   license :commercial
