f889c42dee38cea3cd18ca283818ea01e946c8e9
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 21:50:13 +0000

    updated houdahspot (4.1.2)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,10 +1,10 @@
 cask 'houdahspot' do
-  version '4.1'
-  sha256 'ecd30e68eb8a19e70887701b24b978c82b2498df46873bd2a08882b5484b8630'
+  version '4.1.2'
+  sha256 'bc3162424af7f82364202593257fe6771de5fa8f97d37ead4256cfc6f8f45e94'
 
-  url "https://www.houdah.com/houdahSpot/download_assets/HoudahSpot#{version}.zip"
-  appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.to_i}.xml",
-          :sha256 => 'c726d86606a58e564c077246d0e6f7238a0ebf286c047aadcd9affa1162bda71'
+  url "https://www.houdah.com/houdahSpot/updates/cast4_assets/HoudahSpot#{version}.zip"
+  appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml",
+          :sha256 => 'cbaea676d2684a8b2f5709cedc63ac66e462e079d8519c9117bc2ebf5200579d'
   name 'HoudahSpot'
   homepage 'https://www.houdah.com/houdahSpot/'
   license :commercial
