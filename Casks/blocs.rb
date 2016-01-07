b4bdf1aa8b982867c3b21a8c8350c4284b5e2017
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 16:56:54 +0000

    updated blocs (1.5.3)

--- Casks/blocs.rb
+++ Casks/blocs.rb
@@ -1,10 +1,10 @@
 cask 'blocs' do
-  version :latest
-  sha256 :no_check
+  version '1.5.3'
+  sha256 'd0263d88c2c17a84c431435ac1fb8f82b5ecb459b4967530a16466e1721360c5'
 
   url 'http://blocsapp.com/trial-app/BlocsApp.zip'
   appcast 'https://uistore.io/blocs/info.xml',
-          :sha256 => '42e6c855cf37dad70f357a117b08eb02b6c3c165c99f73caa9f9e814505ef8dc'
+          :sha256 => 'a2f5e79a1f8f39936a6c888300c93bc6e333ba215479d18d60263e7e7c9c8711'
   name 'Blocs'
   homepage 'http://blocsapp.com/'
   license :commercial
