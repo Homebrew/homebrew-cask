9f7f9e389cc358ad34c6e7b0c4e4ac890424268c
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 00:45:15 +0000

    updated devonthink-pro (2.8.8)

--- Casks/devonthink-pro.rb
+++ Casks/devonthink-pro.rb
@@ -1,11 +1,11 @@
 cask 'devonthink-pro' do
-  version '2.8.7'
-  sha256 '49f5ebd10dfb79c4610f3042d04ce49801160e69d37c9efd6da651c9e375be35'
+  version '2.8.8'
+  sha256 'f96de948ad5437a32dae136a0cbf115fafb82fce5355af1132d78470eb4883a7'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_Pro.dmg.zip"
-  appcast 'http://www.devon-technologies.com/Sparkle/DEVONthinkPro2.xml',
-          :sha256 => 'd1c3526d9d9d9f0c81fa3348ef39c26118be13e3b8bad33724897ea49058470f'
+  appcast 'http://www.devon-technologies.com/fileadmin/templates/filemaker/sparkle.php?product=300030707&format=xml',
+          :sha256 => '096d660d32614a2d4b80eb1fec7fe7bd478be3513af653c8bd79350878744a02'
   name 'DEVONthink Pro'
   homepage 'http://www.devontechnologies.com/products/devonthink/devonthink-pro.html'
   license :commercial
