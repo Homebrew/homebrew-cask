45d2f4f95260522fb7229451ecead37e0e503fc3
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 20:56:50 +0000

    updated gas-mask (0.8.3)

--- Casks/gas-mask.rb
+++ Casks/gas-mask.rb
@@ -4,7 +4,7 @@ cask 'gas-mask' do
 
   url "http://gmask.clockwise.ee/files/gas_mask_#{version}.zip"
   appcast 'http://gmask.clockwise.ee/check_update/',
-          :sha256 => '2e4f5292999bddfc25245a9c10f98d7ac23d0717a1dd45436a00cf09be7f8d9b'
+          :sha256 => '10e2f577b32aca6867982f3907803067452fef40aed44bcfee42de4e65b4dfd9'
   name 'Gas Mask'
   homepage 'http://www.clockwise.ee/'
   license :gpl
