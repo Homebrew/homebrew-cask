9aab2bb4fb12e50b5a349456f28e3fb6ccf79a84
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 21:09:56 +0000

    updated handbrake (0.10.2)

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -4,7 +4,7 @@ cask 'handbrake' do
 
   url "http://download.handbrake.fr/releases/#{version}/HandBrake-#{version}-MacOSX.6_GUI_x86_64.dmg"
   appcast 'https://handbrake.fr/appcast.x86_64.xml',
-          :sha256 => 'f0e700c39b76c16dba12ff8b931ae75ae4d764f1e8d1f5b2deb9231e5a445390'
+          :sha256 => '53220e13986dd207db920b7d91277b38fb475390120142dbb293d0cfef0fedbf'
   name 'HandBrake'
   homepage 'https://handbrake.fr'
   license :oss
