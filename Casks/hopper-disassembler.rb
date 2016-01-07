aa0bcdc79051726b4ff34a6b51fd500e17d4b297
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 21:46:42 +0000

    updated hopper-disassembler (3.11.5)

--- Casks/hopper-disassembler.rb
+++ Casks/hopper-disassembler.rb
@@ -1,10 +1,10 @@
 cask 'hopper-disassembler' do
-  version '3.11.2'
-  sha256 'b41e49bf60d49d855429bcd34fe04a05420e4975a1fcb9c01a8039ab37121259'
+  version '3.11.5'
+  sha256 'bd42626b51d4d8d26d22a1dca159a052722d89b99859388a0accfeaa32edffce'
 
   url "http://www.hopperapp.com/HopperWeb/downloads/Hopper-#{version}.zip"
-  appcast 'http://www.hopperapp.com/HopperWeb/appcast_v3.php',
-          :sha256 => '5e182ee160372ff0b9de30b9fcdc8641254e545a4d7a000fa6157a2e86b4764a'
+  appcast "http://www.hopperapp.com/HopperWeb/appcast_v#{version.major}.php",
+          :sha256 => '1ae3256f85304ac725327968ca30f04a3c77f0f508c6dc5ac93fba145b6984a5'
   name 'Hopper'
   name 'Hopper Disassembler'
   homepage 'http://www.hopperapp.com/'
@@ -12,14 +12,14 @@ cask 'hopper-disassembler' do
 
   depends_on :macos => '>= :lion'
 
-  app 'Hopper Disassembler v3.app'
-  binary 'Hopper Disassembler v3.app/Contents/MacOS/hopper'
+  app "Hopper Disassembler v#{version.major}.app"
+  binary "Hopper Disassembler v#{version.major}.app/Contents/MacOS/hopper"
 
   zap :delete => [
                    '~/Library/Application Support/Hopper',
-                   '~/Library/Application Support/Hopper Disassembler v3',
-                   '~/Library/Caches/com.cryptic-apps.hopper-web-3',
-                   '~/Library/Preferences/com.cryptic-apps.hopper-web-3.plist',
-                   '~/Library/Saved Application State/com.cryptic-apps.hopper-web-3.savedState',
+                   "~/Library/Application Support/Hopper Disassembler v#{version.major}",
+                   "~/Library/Caches/com.cryptic-apps.hopper-web-#{version.major}",
+                   "~/Library/Preferences/com.cryptic-apps.hopper-web-#{version.major}.plist",
+                   "~/Library/Saved Application State/com.cryptic-apps.hopper-web-#{version.major}.savedState",
                  ]
 end
