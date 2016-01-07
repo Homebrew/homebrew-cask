19f7cd0f04c8645cdf188f6e43c41e2e72156a7b
Vítor Galvão <info@vitorgalvao.com>
2016-01-07 22:34:01 +0000

    updated ipvanish-vpn (2.2.1_234)

--- Casks/ipvanish-vpn.rb
+++ Casks/ipvanish-vpn.rb
@@ -1,10 +1,10 @@
 cask 'ipvanish-vpn' do
-  version '2.2.0-231'
-  sha256 '35b3140ef065e99d22b4491ffd51cda6eb51f360d9c38e7447b9f64350fb54eb'
+  version '2.2.1_234'
+  sha256 '0f44ac51d46c5fa09131719548dc73760a4db7005a999b1f9cc4c295f37e15b8'
 
-  url "https://www.ipvanish.com/software/osx/IPVanish_v#{version.sub('-', '_')}.zip"
+  url "https://www.ipvanish.com/software/osx/IPVanish_v#{version}.zip"
   appcast 'https://www.ipvanish.com/software/osx/updates.xml',
-          :sha256 => '35b73a715d5e2061b44fe594a658b84cbde8c903352e8ace25283256a2945839'
+          :sha256 => 'ef99b4b91ee1ed96f0ac1b95d3150e516949eabff600e2ef7d57afab71f71a2c'
   name 'IPVanish'
   homepage 'https://www.ipvanish.com/'
   license :gratis
