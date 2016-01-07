bd9226aef48ea78eea0e53db73e9bb610d129f28
Victor Popkov <victor@popkov.me>
2016-01-07 20:50:39 +0200

    screenflow.rb: upgrade to v5.0.4

--- Casks/screenflow.rb
+++ Casks/screenflow.rb
@@ -1,10 +1,10 @@
 cask 'screenflow' do
-  version '5.0.3'
-  sha256 '5f729b09b04c29e20af3048aaccd8e60ad0527c4e6e5f0cdb07e82d73da3af88'
+  version '5.0.4'
+  sha256 '50b6c7e5b37d57ed2c5707e8cd9542372cda0ad64c7f2c543df0f0bb1ccb86d8'
 
   url "https://www.telestream.net/download-files/screenflow/5-0/ScreenFlow-#{version}.dmg"
   appcast 'https://www.telestream.net/updater/screenflow/appcast.xml',
-          :sha256 => '349eb93b3e0822e316cfa1d29f151ac14de555a6cdbfa962c8d6b774be44f037'
+          :sha256 => 'f206ba1d1eb804613e3997c0885491f01685107ff1a0483467904fe3e81049ce'
   name 'ScreenFlow'
   homepage 'https://www.telestream.net/screenflow/'
   license :commercial
