f16dc45551e4f731516f6b1f9f6b840e7dee814b
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 13:51:00 +0000

    updated macscale (1.6.4)

--- Casks/macscale.rb
+++ Casks/macscale.rb
@@ -1,11 +1,11 @@
 cask 'macscale' do
-  version :latest
-  sha256 :no_check
+  version '1.6.4'
+  sha256 'bf817f7d4f1b589d3134303b2a66d1cd3076f42c8c29e5e4cd0dabd844c5d26b'
 
   # brinscall.com is the official download host per the vendor homepage
   url 'https://www.brinscall.com/MacScale.zip'
-  appcast 'https://www.brinscall.com/updates/macscale.xml',
-          :sha256 => 'd5796f727a8ed5e9ed5d59dce484c66a38219edad80b9fb5f6343a3857ed7c1b'
+  appcast 'https://www.brinscall.com/updates/MacScale.xml',
+          :sha256 => '8b100a46b7173663058865a3c9cc94dac117260ae5dccc1142b334aea7cbf531'
   name 'MacScale'
   homepage 'http://www.macscale.com'
   license :closed
