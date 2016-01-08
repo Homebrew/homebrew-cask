1e7db78a5181c99e1b3a29d70881d79ac9c3c733
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 13:40:08 +0000

    updated lingon-x (2.3.1)

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,10 +1,10 @@
 cask 'lingon-x' do
-  version :latest
-  sha256 :no_check
+  version '2.3.1'
+  sha256 'a5276a87aa8fdcf35fbacabf1401cf414680db37730bc6461dd71fbdf1a678b4'
 
-  url 'https://www.peterborgapps.com/downloads/LingonX2.zip'
-  appcast 'https://www.peterborgapps.com/updates/lingonx2-appcast.xml',
-          :sha256 => 'd95b4b7d3aa0c0d5eb574bd5630994f591a2bf7d9cffc1fe3c320efc63b1b936'
+  url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
+  appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}-appcast.xml",
+          :sha256 => 'df1cb1f00c045efa8292ffd1918faeb3c6ae6186db76b8e5b6d524233bee7352'
   name 'Lingon X'
   homepage 'https://www.peterborgapps.com/lingon/'
   license :commercial
