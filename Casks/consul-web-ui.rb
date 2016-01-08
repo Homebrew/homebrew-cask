37408574a09f31770b9b601b684898ebb269e434
Joe Beda <joe.github@bedafamily.com>
2016-01-08 10:02:35 -0800

    Update Consul to 0.6.1

--- Casks/consul-web-ui.rb
+++ Casks/consul-web-ui.rb
@@ -1,6 +1,6 @@
 cask 'consul-web-ui' do
-  version '0.6.0'
-  sha256 '73c5e7ee50bb4a2efe56331d330e6d7dbf46335599c028344ccc4031c0c32eb0'
+  version '0.6.1'
+  sha256 'afccdd540b166b778c7c0483becc5e282bbbb1ee52335bfe94bf757df8c55efc'
 
   # hashicorp.com is the official download host per the vendor homepage
   url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_web_ui.zip"
