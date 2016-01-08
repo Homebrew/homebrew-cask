e405d4f17f1df4c0e27edd6522e187fd27e33610
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 16:20:41 +0000

    updated moneyguru (2.9.4)

--- Casks/moneyguru.rb
+++ Casks/moneyguru.rb
@@ -1,10 +1,10 @@
 cask 'moneyguru' do
-  version '2.9.3'
-  sha256 '5f47715fa02c5fcd6595a733b35b99bd86b3008dff2a566deb692a7abe0b21a6'
+  version '2.9.4'
+  sha256 'df9e113239f230d195f93aa51ad081f3aaea4c86df515b18a3eb1c67d66e9530'
 
   url "https://download.hardcoded.net/moneyguru_osx_#{version.gsub('.', '_')}.dmg"
   appcast 'https://www.hardcoded.net/updates/moneyguru.appcast',
-          :sha256 => '32714462ad203d85285fd449dd472c609c8445384b579ff0dbac794e27bc8749'
+          :sha256 => '3ff1173f8c5384118a3886952937ff8c21b9779c0a7cf592c985851a1e74c130'
   name 'moneyGuru'
   homepage 'https://www.hardcoded.net/moneyguru/'
   license :bsd
