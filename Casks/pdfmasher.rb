f074fd6859eaa0d416739a2c23924aaede05f355
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 23:39:09 +0000

    updated pdfmasher (0.7.4)

--- Casks/pdfmasher.rb
+++ Casks/pdfmasher.rb
@@ -2,9 +2,9 @@ cask 'pdfmasher' do
   version '0.7.4'
   sha256 '09940d6730d90bcf873e4d54262625ad566371258d14b0a6ac07a3b6dbcb9c30'
 
-  url "https://download.hardcoded.net/pdfmasher_osx_#{version.gsub('.', '_')}.dmg"
+  url "https://download.hardcoded.net/pdfmasher_osx_#{version.dots_to_underscores}.dmg"
   appcast 'https://www.hardcoded.net/updates/pdfmasher.appcast',
-          :sha256 => '2007b9ce4044a444b80a80ec40016ae267a910f37880f0bed4dc881f2c3b18af'
+          :sha256 => '400556f9c424326d59f9e3c571f86b93c40e34671c2eb1fb513668a6d1dd7553'
   name 'PdfMasher'
   homepage 'https://www.hardcoded.net/pdfmasher/'
   license :gpl
