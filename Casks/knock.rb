12c6596826afb1a2971c2e23e4ca0a6c273bdaec
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 01:17:51 +0000

    updated knock (2.1.5)

--- Casks/knock.rb
+++ Casks/knock.rb
@@ -1,10 +1,11 @@
 cask 'knock' do
-  version :latest
-  sha256 :no_check
+  version '2.1.5'
+  sha256 '6dc272cc18e44ef06e27c4a7fc5a37f231904f3a436fdfd9e455c74829117426'
 
-  url 'http://knocktounlock.com/download'
+  # amazonaws.com is the official download host per the vendor homepage
+  url 'http://knock-updates.s3.amazonaws.com/Knock.zip'
   appcast 'https://knock-updates.s3.amazonaws.com/Knock.xml',
-          :sha256 => 'c1976fb467cc420bf5e6d5137f01ca70baa3b758dcf8c23860c019bf6dd882c6'
+          :sha256 => '45d49b2a76c79ef7838798a975f9322135c7d3ffaf9af13c271fa597aee8b2f1'
   name 'Knock'
   homepage 'http://knocktounlock.com'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
