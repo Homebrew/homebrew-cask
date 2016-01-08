7ff1458e95e9cfbb01d1e32c841bfcb662453e0f
Patrick Kennedy <ppaine@uoregon.edu>
2016-01-08 01:09:54 -0800

    updated rstudio (0.99.491)

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -1,6 +1,6 @@
 cask 'rstudio' do
-  version '0.99.489'
-  sha256 'e211c0645540b8c668824b6fecdfd19820fe937c4db4432abc7aba06e7efd9e6'
+  version '0.99.491'
+  sha256 '6aef41467ba25889d49a23ebe274123b15bc5949b60db69540fc4276486eaea5'
 
   # rstudio.org is the official download host per the vendor homepage
   url "https://download1.rstudio.org/RStudio-#{version}.dmg"
