a4256ba9deb60aacc43e655709161a7f985edfe5
Daniel Bayley <daniel.bayley@me.com>
2016-01-08 10:30:02 +0000

    Add QuadroSync v1.0.0
    QuadroSync to make your computer compatible with Quadro.
    
    Signed-off-by: Daniel Bayley <daniel.bayley@me.com>

---
+++ Casks/quadrosync.rb
@@ -0,0 +1,21 @@
+cask 'quadrosync' do
+  version :latest
+  sha256 :no_check
+
+  # amazonaws.com is the official download host per the vendor homepage.
+  url 'https://s3-eu-west-1.amazonaws.com/quadro-downloads/daemon/mac/QuadroSync_mac.dmg'
+  name 'QuadroSync'
+  homepage 'http://quadro.me/sync'
+  license :gratis
+
+  installer :manual => 'QuadroInstaller.app'
+  app 'QuadroSync.app'
+
+  uninstall :quit       => [
+                             'com.quadro.QuadroSync',
+                             'com.quadro.QuadroNGui'
+                           ],
+            :launchctl  => 'com.quadro.QuadroInstaller.HelperTool',
+            :login_item => 'QuadroSync'
+  zap :delete => '/Library/Application Support/Quadro'
+end
