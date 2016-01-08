651753ad7345c118390ed6d151eecb51052519ef
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 16:35:12 +0000

    updated oolite (1.82)

--- Casks/oolite.rb
+++ Casks/oolite.rb
@@ -4,8 +4,8 @@ cask 'oolite' do
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/OoliteProject/oolite/releases/download/#{version}/Oolite-#{version}.zip"
-  appcast 'http://www.oolite.org/updates/oolite-mac-appcast.xml',
-          :sha256 => 'b1f324355dfb531102fd6e407d7e0fbb4547c720ffe833d31fcc5e26565b29a0'
+  appcast 'https://github.com/OoliteProject/oolite/releases.atom',
+          :sha256 => '4fe29bcb8ba413dec4fdd02a53ea20ad4f8dea6aa7594b5808f2bc6562234c3d'
   name 'oolite'
   homepage 'http://www.oolite.org/'
   license :gpl
