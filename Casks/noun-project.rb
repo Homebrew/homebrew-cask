7d156a52f4d112805272d674bd5f077122e6e4aa
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 16:28:20 +0000

    updated noun-project (1.1.3)

--- Casks/noun-project.rb
+++ Casks/noun-project.rb
@@ -2,10 +2,10 @@ cask 'noun-project' do
   version '1.1.3'
   sha256 '48cccb6c6e96f98eb3e31f10ec5a9478c1448637aa5c6e689d98f7d5b4c86931'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com is the official download host per the appcast feed
   url "https://s3.amazonaws.com/nounproject/mac/Noun-Project-#{version}.dmg"
   appcast 'https://thenounproject.com/for-mac/feed/',
-          :sha256 => '94c3d8509a7bedcd74e33e756a39a9e21f81c3753c3a61f29d453d1ef4b38dd3'
+          :sha256 => '16aae36066bb6a112fb3b835b7d6c8a4d5dd20105f9fe313ddc99fbc8cc19321'
   name 'Noun Project'
   homepage 'https://thenounproject.com'
   license :commercial
