56670406ee1336d78fe6679fd1bd646b49112ecc
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 23:45:53 +0000

    updated pdfpen (734.0,1451347971)

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -1,10 +1,10 @@
 cask 'pdfpen' do
-  version :latest
-  sha256 :no_check
+  version '734.0,1451347971'
+  sha256 '265eec35fa5f45c9623b7ee87bbfbc90f0afa2c146ca18a2e2da10774bee7389'
 
-  url 'https://dl.smilesoftware.com/com.smileonmymac.PDFpen/PDFpen.zip'
+  url "https://dl.smilesoftware.com/com.smileonmymac.PDFpen/#{version.before_comma}/#{version.after_comma}/PDFpen-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml',
-          :sha256 => '20c1ab602462b7fc0d5b4cbd555cacf127b69a07a737579598ebcbc0f5b21319'
+          :sha256 => '48ee58e738d901c4e78daef48e231ce4acf25d20501e892c6b36e86b63f31217'
   name 'PDFpen'
   homepage 'https://smilesoftware.com/PDFpen/'
   license :commercial
