ecebeac7785a9ed3604378ef025f07b020abf5da
Vítor Galvão <info@vitorgalvao.com>
2016-01-08 13:35:00 +0000

    updated lifeslice (2.3)

--- Casks/lifeslice.rb
+++ Casks/lifeslice.rb
@@ -1,11 +1,11 @@
 cask 'lifeslice' do
-  version :latest
-  sha256 :no_check
+  version '2.3'
+  sha256 '0d95ba8c7e1211fa8668b28a323f98aa486c08ce21f1720641ea5f405c1ff056'
 
   # wanderingstan.com is the official download host per the vendor homepage
-  url 'http://wanderingstan.com/apps/lifeslice/LifeSlice.dmg'
-  appcast 'http://wanderingstan.com/apps/lifeslice/sparkle-lifeslice-appcast-xml.php',
-          :sha256 => '90bd0939501fdc8a5b52bb24532919424ee1201a1b6281761b9e91d3b9068c61'
+  url 'http://www.wanderingstan.com/apps/lifeslice/LifeSlice_701.zip'
+  appcast 'http://wanderingstan.com/apps/lifeslice/lifeslice_appcast.xml',
+          :sha256 => '57aa727b84ce1ac250eb00aa1eb3954f6e733e9d7b7baee9221c49efc8ddaf06'
   name 'LifeSlice'
   homepage 'https://wanderingstan.github.io/Lifeslice/'
   license :oss
