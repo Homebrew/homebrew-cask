ba96dcab1e4407c491c516db47928d0668609912
Vítor Galvão <info@vitorgalvao.com>
2016-01-09 01:11:03 +0000

    updated robofont (1.6,1410152315)

--- Casks/robofont.rb
+++ Casks/robofont.rb
@@ -1,10 +1,10 @@
 cask 'robofont' do
-  version '1.6'
+  version '1.6,1410152315'
   sha256 'fb2fd390868a32fb9e89490081f3f052e65c1156ee32b39cac1c799b87abc4c7'
 
-  url 'http://robofont.com/downloads/RoboFont_1410152315.dmg'
-  appcast 'http://doc.robofont.com/appcast',
-          :sha256 => 'c235649bb973653a99e043bab89681dcd0407976ae1cdc56237e963492bd8562'
+  url "http://robofont.com/downloads/RoboFont_#{version.after_comma}.dmg"
+  appcast 'http://doc.robofont.com/appcast/',
+          :sha256 => 'ed39de07f36dbaf3d622c4d2172f36deea14ef3cf84214c6f2c9f4b0fb06e5ab'
   name 'RoboFont'
   homepage 'http://robofont.com'
   license :other # See http://doc.robofont.com/license-agreement/
