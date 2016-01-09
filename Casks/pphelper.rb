df8f6a6d7f8b6d06ca6a23ccfbe4e54afa630337
Vítor Galvão <info@vitorgalvao.com>
2016-01-09 00:46:29 +0000

    updated pphelper (2.3.4)

--- Casks/pphelper.rb
+++ Casks/pphelper.rb
@@ -1,10 +1,10 @@
 cask 'pphelper' do
-  version :latest
-  sha256 :no_check
+  version '2.3.4'
+  sha256 'c308c7733bab38342229826b2d6e7cdfca425876bdd794732d1a05e9a0ddc410'
 
   url 'https://ghost.25pp.com/soft/pp_mac.dmg'
   appcast 'https://liveupdate.25pp.com/macpc/Appcast.xml',
-          :sha256 => 'fcffad714bc89ca0670d8bc0f8cc3371bbf4f078459bc219e6ee7db92ab2e180'
+          :sha256 => '96e55462ba3629ff18e88c08d83fd5e202aec637eafed6856179ca214c1ddab4'
   name 'pp助手'
   name 'pphelper'
   homepage 'http://pro.25pp.com/pp_mac_ios'
