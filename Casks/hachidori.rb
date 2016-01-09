4ba114143d7ce442b885bae276db19b29718a50a
Victor Popkov <victor@popkov.me>
2016-01-09 23:29:25 +0200

    hachidori.rb: upgrade to v1.1.7.2

--- Casks/hachidori.rb
+++ Casks/hachidori.rb
@@ -1,11 +1,11 @@
 cask 'hachidori' do
-  version '1.1.7.1'
-  sha256 '28da36312ade8d1e4369d4229c9b319c274259b50987644b48c2c164f711c2c7'
+  version '1.1.7.2'
+  sha256 '65f11c9d94c68e7e46aa89a9923dbddb95c2c7abb0c183dc26aafa5ec6caf827'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/chikorita157/hachidori/releases/download/#{version}/Hachidori-#{version}.zip"
   appcast 'https://github.com/chikorita157/hachidori/releases.atom',
-          :sha256 => '3767d387127253d1f5fd3fe553a5f1d6d6e9858bbd4f2d02a2cd053cc31831bd'
+          :sha256 => 'b77dc40831d44d712e81693e73217f9f6d43accb5ae7552709a3bf4b8be1b9a6'
   name 'Hachidori'
   homepage 'http://hachidori.ateliershiori.moe'
   license :bsd
