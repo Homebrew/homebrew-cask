03ad893c52d041423da34c7f1e0e10ef1ee63dc3
Vítor Galvão <info@vitorgalvao.com>
2016-01-09 01:13:41 +0000

    updated rowmote-helper (4.1.4)

--- Casks/rowmote-helper.rb
+++ Casks/rowmote-helper.rb
@@ -3,8 +3,8 @@ cask 'rowmote-helper' do
   sha256 '7f58d3596d2ffd6310c5cdb9fef89d0f55e60e45deb3fade04990ed73e9c0bc9'
 
   url "http://regularrateandrhythm.com/rowmote-pro/rh/rowmote-helper-#{version}.zip"
-  appcast 'http://rowmote.com/rowmote-appcast.xml',
-          :sha256 => '6cd34bfdef0bcca248b37e96e7598e09f5ab6a990dc97fc50b0cea7f7adeab7f'
+  appcast 'http://www.regularrateandrhythm.com/apps/rowmote-pro/rowmote-appcast.xml',
+          :sha256 => 'b24f00bd9ef1fafb0f5266d8649741f2da65ea5be361013971dedaaf00790447'
   name 'Rowmote Helper'
   homepage 'http://regularrateandrhythm.com/apps/rowmote-pro/'
   license :commercial
