9030147c6c7745a2cce0293b36e6430a37c75f5c
Vítor Galvão <info@vitorgalvao.com>
2016-01-09 00:50:53 +0000

    updated praat (6.0.10)

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,16 +1,16 @@
 cask 'praat' do
-  version '6.0.06'
+  version '6.0.10'
 
   if Hardware::CPU.is_32_bit? || MacOS.release <= :snow_leopard
     sha256 'f82242cc2fbc07ae5b9218e7707f842f0a428b0db019db04d0a0936dcb4c0298'
-    url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.delete('.')}_mac32.dmg"
+    url "http://www.fon.hum.uva.nl/praat/praat#{version.no_dots}_mac32.dmg"
   else
-    sha256 '8a5e2cbc79b5ef999231f0f108f864e3fdacf020942e166a930c40353eff1b06'
-    url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.delete('.')}_mac64.dmg"
+    sha256 '966428494147c9c14e55fe77b6304e9ce400b2b4cb0866aec26ab79e851aec23'
+    url "http://www.fon.hum.uva.nl/praat/praat#{version.no_dots}_mac64.dmg"
   end
 
   appcast 'https://github.com/praat/praat/releases.atom',
-          :sha256 => '59a4fe43d795b61c7efa9ce61503f13a731878a422b93b22c49f8b9e05f09a39'
+          :sha256 => '4770bede37ef657126871d91158340e623e54ad2ab8a4da030529893541a3fb1'
   name 'Praat'
   homepage 'http://www.fon.hum.uva.nl/praat/'
   license :gpl
