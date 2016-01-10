749e51f202fea6f4416341c0b4b6b04b941fec35
katoquro <katoquro@gmail.com>
2016-01-10 13:23:33 +0300

    fix comment message

--- Casks/eiskaltdcpp-qt5.rb
+++ Casks/eiskaltdcpp-qt5.rb
@@ -1,6 +1,6 @@
 cask 'eiskaltdcpp-qt5' do
-  #EiskaltDC++ from git master for Mac OS X 10.9 64-bit
-  #Built on Mac OS X 10.9 with Qt 5.5.0
+  # EiskaltDC++ from git master for Mac OS X 10.9 64-bit
+  # Built on Mac OS X 10.9 with Qt 5.5.0
   version '2.3.0-v2.2.10-62-ge42c04e'
   sha256 '1463fd447ffd16d9f3af96b0d2306a728d580e59bc7b9a45fcbe895c99c5432c'
 
