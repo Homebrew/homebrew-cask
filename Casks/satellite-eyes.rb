e4e5a279192bf94daaca71c392e99cdee53017dc
Vítor Galvão <info@vitorgalvao.com>
2016-01-10 16:19:31 +0000

    updated satellite-eyes (1.4.1)

--- Casks/satellite-eyes.rb
+++ Casks/satellite-eyes.rb
@@ -5,7 +5,7 @@ cask 'satellite-eyes' do
   # amazonaws.com is the official download host per the vendor homepage
   url "https://satellite-eyes.s3.amazonaws.com/satellite-eyes-#{version}.zip"
   appcast 'https://satellite-eyes.s3.amazonaws.com/appcast.xml',
-          :sha256 => '5853ce38284dd959729ea64a423c359d6f2e383be3f062b38825762dfb97860b'
+          :sha256 => '4f8442b010054afde7b0f1545623b0ada25c16af803353af1a1254e6d5857339'
   name 'Satellite Eyes'
   homepage 'http://satelliteeyes.tomtaylor.co.uk/'
   license :apache
