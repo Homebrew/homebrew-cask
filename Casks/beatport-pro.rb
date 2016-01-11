7ec252d26ae6c308e0757ea638d78784a67e6f5e
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:37:03 +0000

    beatport-pro.rb: fixed name stanzas

--- Casks/beatport-pro.rb
+++ Casks/beatport-pro.rb
@@ -5,7 +5,6 @@ cask 'beatport-pro' do
   url "https://pro.beatport.com/mac/#{version}/beatportpro_#{version}.zip"
   appcast 'https://pro.beatport.com/mac/appcast.xml',
           :sha256 => 'b855defa3a7f0add216a0568c6c8922857954e61786dcb30fce4c1b565ad5716'
-  name 'Beatport'
   name 'Beatport Pro'
   homepage 'https://pro.beatport.com/'
   license :closed
