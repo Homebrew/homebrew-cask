984d820a017122a502707da241f184f7cbba09e8
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:42:06 +0000

    ynab.rb: fixed name stanzas

--- Casks/ynab.rb
+++ Casks/ynab.rb
@@ -3,8 +3,8 @@ cask 'ynab' do
   sha256 '0635c56582a1367c2ca3a8ab88805ec89625f2c5042497e7f13081726b2238a3'
 
   url "https://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_#{version}.dmg"
-  name 'YNAB'
   name 'You Need A Budget'
+  name 'YNAB'
   homepage 'https://www.youneedabudget.com/'
   license :commercial
 
