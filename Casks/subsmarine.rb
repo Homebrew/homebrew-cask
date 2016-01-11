29c347574f52c962ad6827eb235d715de9b54192
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 19:08:27 +0000

    updated subsmarine (1.2.2)

--- Casks/subsmarine.rb
+++ Casks/subsmarine.rb
@@ -4,7 +4,7 @@ cask 'subsmarine' do
 
   url "http://www.cocoawithchurros.com/downloads/SubsMarine.#{version}.zip"
   appcast 'http://www.cocoawithchurros.com/shine/appcast.php?id=7',
-          :sha256 => '9659830b134ac12326639e1b3f7ba06945cc18db343890e06a4c7f9d9dce8912'
+          :sha256 => 'bbb2b6362544e5e4a23e0716592fb151005054a2f9dc13903139fb541dbd082d'
   name 'SubsMarine'
   homepage 'http://www.cocoawithchurros.com/subsmarine.php'
   license :commercial
