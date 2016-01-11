64c7c086d8aa998a5472216f2cbeac78d1bd2ac8
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:40:20 +0000

    sqlitebrowser.rb: fixed name stanzas

--- Casks/sqlitebrowser.rb
+++ Casks/sqlitebrowser.rb
@@ -6,7 +6,6 @@ cask 'sqlitebrowser' do
   url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version.major_minor_patch}/sqlitebrowser-#{version}.dmg"
   appcast 'https://github.com/sqlitebrowser/sqlitebrowser/releases.atom',
           :sha256 => '9ca8a0972b220d498013af3dd25125a370d2da38a9d8487f6e76ed55f4e8c68f'
-  name 'DB Browser for SQLite'
   name 'SQLite Database Browser'
   homepage 'http://sqlitebrowser.org'
   license :oss
