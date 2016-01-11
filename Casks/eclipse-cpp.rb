3042a3d9d58565369e3d94e1cddbc0b732ae2b40
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:37:25 +0000

    eclipse-cpp.rb: fixed name stanzas

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -3,7 +3,6 @@ cask 'eclipse-cpp' do
   sha256 'd485444edbd6761da697dd55e974f8a45691f6bce515880eeb4216ea5ef6cbb5'
 
   url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/1/eclipse-cpp-mars-1-macosx-cocoa-x86_64.tar.gz&r=1'
-  name 'Eclipse'
   name 'Eclipse IDE for C/C++ Developers'
   homepage 'https://eclipse.org/'
   license :eclipse
