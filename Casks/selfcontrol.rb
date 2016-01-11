5abd6489b61afe476efed04f6f03d34c1dc86c75
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 16:56:18 +0000

    updated selfcontrol (2.1)

--- Casks/selfcontrol.rb
+++ Casks/selfcontrol.rb
@@ -3,13 +3,13 @@ cask 'selfcontrol' do
     version '1.5.1'
     sha256 'd3823a1e9ba0b47dc2cb39c93cd51837c2dafc7d5a5a564825f4a440fd2ab9ad'
   else
-    version '2.0.2'
-    sha256 'cd1fb7bd5524d81e784ad67f8639cfb836261f07d7e6db75458a398b17f9a1f9'
+    version '2.1'
+    sha256 '6a2b463e1c8b7b49b72b194f0160851738c83b09c543df931209f24e32b41526'
   end
 
   url "http://downloads.selfcontrolapp.com/SelfControl-#{version}.zip"
   appcast 'https://selfcontrolapp.com/SelfControlAppcast.xml',
-          :sha256 => '05d48c097c072ffd92d4d938b6984c8a09f446a556dc30acf25e3896cc3e0826'
+          :sha256 => '459ee4aef328efbefef3c2ee721b373a483ad11ee4b6d6b4b68bbbc018a05272'
   name 'SelfControl'
   homepage 'https://selfcontrolapp.com/'
   license :gpl
