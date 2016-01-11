ebb2a9394b8aef96a28d1ef4354b451104455d7e
Takeru Naito <takeru.naito@gmail.com>
2016-01-11 12:02:58 +0900

    updated aquaskk (4.3.4)

--- Casks/aquaskk.rb
+++ Casks/aquaskk.rb
@@ -1,6 +1,6 @@
 cask 'aquaskk' do
-  version '4.3.3'
-  sha256 '9a09d2fb8dcbf59494926fc1762a1dc16dfb94f27827659a0fdc68620a65adae'
+  version '4.3.4'
+  sha256 'fd29284c76ddc361506ad887be3868bc8e020d6424d818e5365d7d9148d19fcc'
 
   url "https://github.com/codefirst/aquaskk/releases/download/#{version}/AquaSKK-#{version}.dmg"
   name 'AquaSKK'
