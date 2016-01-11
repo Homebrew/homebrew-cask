3b372c7ce4d09e51f0bca6cf92036af930bc79d9
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:37:27 +0000

    eclipse-ide.rb: fixed name stanzas

--- Casks/eclipse-ide.rb
+++ Casks/eclipse-ide.rb
@@ -3,7 +3,6 @@ cask 'eclipse-ide' do
   sha256 'ec2a963367f3ee99429f151d0af3bb6d70d0550f27e2d7ab65730b429b54c9a9'
 
   url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/1/eclipse-committers-mars-1-macosx-cocoa-x86_64.tar.gz&r=1'
-  name 'Eclipse'
   name 'Eclipse IDE for Eclipse Committers'
   homepage 'https://eclipse.org/'
   license :eclipse
