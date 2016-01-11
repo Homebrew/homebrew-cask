6bd6fd5b90df13bb8339b7dc0b16e127261a14a4
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:37:35 +0000

    eclipse-php.rb: fixed name stanzas

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -3,7 +3,6 @@ cask 'eclipse-php' do
   sha256 '142fdeeedf091149a6d72e0aacf28f6937fe6bef01d83cbf141be4d847172492'
 
   url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/1/eclipse-php-mars-1-macosx-cocoa-x86_64.tar.gz&r=1'
-  name 'Eclipse'
   name 'Eclipse for PHP Developers'
   homepage 'https://eclipse.org/'
   license :eclipse
