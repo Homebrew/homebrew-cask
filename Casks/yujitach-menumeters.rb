8582ea55dc0fb0104f1857827cd72e555104db67
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:42:10 +0000

    yujitach-menumeters.rb: fixed name stanzas

--- Casks/yujitach-menumeters.rb
+++ Casks/yujitach-menumeters.rb
@@ -3,7 +3,6 @@ cask 'yujitach-menumeters' do
   sha256 'cffa931287fe0b4f7b05e400b96bead4317702524233bafc25460687ed20aebf'
 
   url "http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/zips/MenuMeters_#{version}.zip"
-  name 'MenuMeters'
   name 'MenuMeters El Capitan Port'
   homepage 'http://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/'
   license :gpl
