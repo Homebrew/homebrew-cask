ffa15b522512bb942871c925f244a0d89dd42693
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:37:45 +0000

    eudic.rb: fixed name stanzas

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -4,8 +4,8 @@ cask 'eudic' do
 
   # frdic.com is the official download host per the vendor homepage
   url 'http://static.frdic.com/pkg/eudicmac.dmg'
-  name '欧路词典'
   name 'EuDic'
+  name '欧路词典'
   homepage 'http://www.eudic.net/eudic/mac_dictionary.aspx'
   license :closed
 
