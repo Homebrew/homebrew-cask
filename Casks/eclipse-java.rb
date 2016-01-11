790b537f96d0534dedbd9d1da356d4335f8fd23b
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:37:28 +0000

    eclipse-java.rb: fixed name stanzas

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -3,7 +3,6 @@ cask 'eclipse-java' do
   sha256 '557070cc428715ae32915d7eb72f1720ef01d3979b5b5d6dd3f627710970d6f5'
 
   url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/1/eclipse-java-mars-1-macosx-cocoa-x86_64.tar.gz&r=1'
-  name 'Eclipse'
   name 'Eclipse IDE for Java Developers'
   homepage 'https://eclipse.org/'
   license :eclipse
