dc871707548717ca049da570a2f7349e2f88a5d4
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 19:14:03 +0000

    updated switchup (1.7)

--- Casks/switchup.rb
+++ Casks/switchup.rb
@@ -1,13 +1,13 @@
 cask 'switchup' do
   version '1.7'
-  sha256 'e35b6cbd212b501e615fa2c678c65d9539cbb72f9d913ccd9748d9a645527ebe'
+  sha256 '69d6b40162c9b033cbec21264322900555abcc25c2a78716de4d17439dfdb2d4'
 
   url "https://www.irradiatedsoftware.com/downloads/SwitchUp_#{version}.zip"
   appcast 'https://www.irradiatedsoftware.com/updates/profiles/switchup.php',
-          :sha256 => 'a9feeb5f7dcb832042ad2d8083844e6f26c0537628820870b27e1d8a8d5abb82'
+          :sha256 => '0d20321633384c6d1c15cb7f4b4f7e67242981d84ebd4dc971429cc46f76c43d'
   name 'SwitchUp'
   homepage 'https://www.irradiatedsoftware.com/switchup/'
-  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'SwitchUp.app'
 end
