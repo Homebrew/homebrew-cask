4e7e263fa158fdc411d80272afd79ddd28ae92d2
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:38:29 +0000

    itunesscrobbler.rb: fixed name stanzas

--- Casks/itunesscrobbler.rb
+++ Casks/itunesscrobbler.rb
@@ -3,7 +3,6 @@ cask 'itunesscrobbler' do
   sha256 '01dcacb1738e824d853dab7825cee88b017f47a65f8e22fdbd7bf762802cc893'
 
   url "http://www.easyclasspage.de/lastfm/files/iTunesScrobbler_#{version.delete('.')}.zip"
-  name 'iTunes (Radio) Scrobbler'
   name 'iTunesScrobbler'
   homepage 'http://www.easyclasspage.de/lastfm/seite-19.html'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
