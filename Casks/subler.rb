9f72a14d4bff30c63c4e21ea9bc91cc077cc5038
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 19:08:11 +0000

    updated subler (1.0.8)

--- Casks/subler.rb
+++ Casks/subler.rb
@@ -1,11 +1,11 @@
 cask 'subler' do
-  version '1.0.6'
-  sha256 '84e09cb84fd262c23b9ed61298ea98f1b6a611e4d355f8f95ed6a6890510b840'
+  version '1.0.8'
+  sha256 'ca4e5de56cddac494936916bfcf837cb4ab181bdcc1abcf1e6093699eaa514e4'
 
   # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/galad87/subler/downloads/Subler-#{version}.zip"
   appcast 'https://subler.org/appcast/appcast.xml',
-          :sha256 => '84e09cb84fd262c23b9ed61298ea98f1b6a611e4d355f8f95ed6a6890510b840'
+          :sha256 => '521d5efab5192750dfdc6dad5fa3ee0a5a563ed4658fdcc9c640e5aa11202a45'
   name 'Subler'
   homepage 'https://subler.org/'
   license :gpl
