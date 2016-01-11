d21c7883ccd9d9f6b7c9b6ad87bd1d24df007f54
Victor Popkov <victor@popkov.me>
2016-01-11 21:12:54 +0200

    tunnelbear.rb: upgrade to v2.5.7

--- Casks/tunnelbear.rb
+++ Casks/tunnelbear.rb
@@ -1,11 +1,11 @@
 cask 'tunnelbear' do
-  version '2.5.6'
-  sha256 '28f77ee1387999d4deb82983b78ca825b9d1caab143a683af19df025333a0376'
+  version '2.5.7'
+  sha256 'a18c1dacb5773079a190e6e2bdf1f48d23f3248719f9cc33fcd565c022e5f1a1'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://tunnelbear.s3.amazonaws.com/downloads/mac/TunnelBear-#{version}.zip"
   appcast 'https://s3.amazonaws.com/tunnelbear/downloads/mac/appcast.xml',
-          :sha256 => '9a437f3b3c02ce7947c7563ce9fa1f0f431d0f8fc00b398513e0f2d33312ae89'
+          :sha256 => '800dcdba4bf51951a84b9b9914d0d330f19ce67d6dcf6c103525e818cc4febf7'
   name 'TunnelBear'
   homepage 'https://www.tunnelbear.com/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
