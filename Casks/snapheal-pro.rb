0168883546720fe2ed91d7bcb6af4f93ba25035e
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 17:18:21 +0000

    updated snapheal-pro (1.5.1118)

--- Casks/snapheal-pro.rb
+++ Casks/snapheal-pro.rb
@@ -1,10 +1,11 @@
 cask 'snapheal-pro' do
-  version '1.4.980'
-  sha256 'c9beb872e8639395d996a2582e37bec305c12bfbe5ca036bffc6ffd4c9654801'
+  version '1.5.1118'
+  sha256 'b6b46a70f270872a5b9cd32f8366070227af4e70d37b054b3b79280d425b2aa2'
 
-  url "http://cdn.macphun.com/updates/SnaphealMacPRO/SnaphealPRONA_Distribution_v#{version.gsub('.', '_')}.zip"
+  # amazonaws.com is the official download host per the appcast feed
+  url "http://creativekit.s3.amazonaws.com/SnaphealCK/SnaphealCKNA_Distribution_v#{version.dots_to_underscores}.zip"
   appcast 'http://cdn.macphun.com/updates/SnaphealMacPRO/appcast.xml',
-          :sha256 => '58c31104e33c7a557eaeaf83ee0e614012916349de19760737fdf60924c8286b'
+          :sha256 => '3c29cd133345294e0f073a1d02df5339dd69facee830d3bfc77fa7f3be79739d'
   name 'Macphun Snapheal Pro'
   homepage 'https://macphun.com/snapheal/'
   license :commercial
