816a541eeeeed0f578b2217e64c3915d1fde10bb
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 18:32:12 +0000

    updated rightfont (3.1.1)

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -1,10 +1,10 @@
 cask 'rightfont' do
-  version '3.1.0'
-  sha256 '6353937ff1d4a62f616ec6a83836d3db78f48d4fff609280ede7ebd451d5c569'
+  version '3.1.1'
+  sha256 '517cfd4d7c0d1aee0bf52bd22127608361594236ddf588a8ff7a79537047097e'
 
   url "http://rightfontapp.com/downloads/#{version}/rightfont.zip"
   appcast 'http://rightfontapp.com/update/appcast.xml',
-          :sha256 => '561651befa2757a64e9f08aebd0313c16fd015678403921c06299490ef85a4ad'
+          :sha256 => '12ac031027eaf72848d72ebdc46b68ccfee00563475ac6d7a196aabbbf600242'
   name 'RightFont'
   homepage 'http://rightfontapp.com/'
   license :commercial
