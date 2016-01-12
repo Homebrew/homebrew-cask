df096ce3653bc82c26752bd4af0a08839e981735
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 18:14:39 +0000

    updated launchcontrol (1.25.2)

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,10 +1,10 @@
 cask 'launchcontrol' do
-  version '1.25.1'
-  sha256 '9c02c644c904a9e7f10e075296daa1a0d839c888f2930ecfd49b16acc2d6b708'
+  version '1.25.2'
+  sha256 'f58123c7aee8fe8438bbd1c3f977f13f359261afe30f902d799f721989ce33af'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
-          :sha256 => '9c13a78b71ade43bc4831128d1ade73c48cc2162efbbd8e4a9fc39c3f603dc10'
+          :sha256 => '8b828cd89bae76476056fcc88800bb5ca2c7c7d950e7b966e876e8c87de2f804'
   name 'LaunchControl'
   homepage 'http://www.soma-zone.com/LaunchControl/'
   license :commercial
