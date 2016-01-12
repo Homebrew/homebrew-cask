4614266e097fdd0afd327b3ea7167908de029cff
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 15:50:43 +0000

    updated ubar (3.1.2)

--- Casks/ubar.rb
+++ Casks/ubar.rb
@@ -1,10 +1,10 @@
 cask 'ubar' do
-  version '3.1.1'
-  sha256 'f5470e803ba75977099a9582a6d2eabd39aaad9baf6028bbc60b53a1c9bcc409'
+  version '3.1.2'
+  sha256 '899d7530958ae78d315a25ebb2bc2614c572cbd0bb8e1057e1725f602b415ac0'
 
   url "http://www.brawersoftware.com/downloads/ubar/ubar#{version.delete('.')}.zip"
   appcast "http://brawersoftware.com/appcasts/feeds/ubar/ubar#{version.to_i}.xml",
-          :sha256 => 'd7b751ea9c7cbbf04f549bfc24c8be2ea2901e5e0157b25216f0789a52620d1f'
+          :sha256 => '766a1635b0f2fe66ebb27a6f4813c70688d5d99792b023dcf6762721663bc977'
   name 'uBar'
   homepage 'http://brawersoftware.com/products/ubar'
   license :commercial
