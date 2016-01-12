4c84492a3a5aa77093a7badf59e29f251649cee6
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:59:28 +0000

    evom.rb: appcast sha256 without pubDate

--- Casks/evom.rb
+++ Casks/evom.rb
@@ -4,7 +4,7 @@ cask 'evom' do
 
   url 'https://files.thelittleappfactory.com/evom/Evom.zip'
   appcast 'https://files.thelittleappfactory.com/evom/appcast.xml',
-          :sha256 => 'ed7a09825022f8643d2ca67c0deccb727a45b95306b83bf5b3ea8df989195991'
+          :sha256 => '835e7059cd2bec30e3e3330fe2931fa8095392e063b7a5c9d497a2351e1abb49'
   name 'Evom'
   homepage 'http://thelittleappfactory.com/evom/'
   license :gratis
