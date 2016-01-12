51f59831356e8b91414c8e246e587c46931d27fb
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 16:44:04 +0000

    updated yummy-ftp (1.11.1)

--- Casks/yummy-ftp.rb
+++ Casks/yummy-ftp.rb
@@ -1,10 +1,10 @@
 cask 'yummy-ftp' do
-  version :latest
-  sha256 :no_check
+  version '1.11.1'
+  sha256 '43e11a1ebdeee8c9d8f49b2d5077382d81fa4140f363061137d652e4bed708af'
 
   url 'http://www.yummysoftware.com/downloads/YummyFTP.zip'
   appcast 'http://www.yummysoftware.com/su/yummyftp/feed.xml',
-          :sha256 => 'fac71ab836dffdc1ae5b0b4acb03d63d2d93fb2639dd38d57b32d691a08d1ea7'
+          :sha256 => '558301e72be50bd62a1e7f0ff8e15f9649759b213326451ad1ff6984c6bdf12a'
   name 'Yummy FTP'
   homepage 'http://www.yummysoftware.com'
   license :commercial
