476ba64b3b385c47bb025c3edb7d38fe80188636
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:18:45 +0000

    sublime-text.rb: appcast sha256 without pubDate

--- Casks/sublime-text.rb
+++ Casks/sublime-text.rb
@@ -5,7 +5,7 @@ cask 'sublime-text' do
   # rackcdn.com is the official download host per the vendor homepage
   url "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20#{version}.dmg"
   appcast 'https://www.sublimetext.com/updates/2/stable/appcast_osx.xml',
-          :sha256 => 'e11769f18c577d4cb189c6f6485119a66db5e5d3ba4df99326080f193c1f74b3'
+          :sha256 => '4f73b97733c713f3d76a9752061b4fe19587c780f87709681d913368ea667e6f'
   name 'Sublime Text'
   homepage 'https://www.sublimetext.com/2'
   license :closed
