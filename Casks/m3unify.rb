7d5022e95bfdc2dff5031a8fdee99268ffa37818
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 18:15:38 +0000

    updated m3unify (1.4.2)

--- Casks/m3unify.rb
+++ Casks/m3unify.rb
@@ -1,10 +1,10 @@
 cask 'm3unify' do
-  version '1.4.1'
-  sha256 'aceda65ecb588fd51380e4dc77cd6c1b95070b60fd30e65b50ba093b11efcc1f'
+  version '1.4.2'
+  sha256 '91bc8e53a10f670d3f939f19a1404a060c83fb47c52b7729e59023e915d66085'
 
   url "http://dougscripts.com/itunes/scrx/m3unifyv#{version.delete('.')}.zip"
   appcast 'http://dougscripts.com/itunes/itinfo/m3unify_appcast.xml',
-          :sha256 => '96e87ec4a0281bcd55ebf18024b68ac65b5f6a4ad46de2522ebdac72c9be7094'
+          :sha256 => '522e2d09c204f4e0c5e99d132a4903ba97c86296c382a4089888667dc41b19b7'
   name 'M3Unify'
   homepage 'http://dougscripts.com/itunes/itinfo/m3unify.php'
   license :commercial
