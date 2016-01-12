fbd97aaab03e139946777a857a8206cea5fa4c68
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 16:15:04 +0000

    updated wordcounter (1.3.3)

--- Casks/wordcounter.rb
+++ Casks/wordcounter.rb
@@ -1,10 +1,11 @@
 cask 'wordcounter' do
-  version :latest
-  sha256 :no_check
+  version '1.3.3'
+  sha256 '68219430aef534160b3c9264778bcac04c78a6ffe3c30c06a1f0ebfe622d034a'
 
-  url 'http://wordcounterapp.com/download'
-  appcast 'http://appcasts.christiantietze.de/wordcounter/v1/beta.xml',
-          :sha256 => '43d85462e4502f62fc4dcc39f2df83b6c1cfdbddb801a8cc30c7c9bace1e13c5'
+  # amazonaws.com is the official download host per the vendor homepage
+  url "https://s3.amazonaws.com/wordcounterapp/WordCounter-v#{version}.zip"
+  appcast "https://update.christiantietze.de/wordcounter/v#{version.major}/beta.xml",
+          :sha256 => '96b36ea1ddf1b899710c77a2d5d2ad6ce5b0943aa1cbc4f92f00a8b350f95b6a'
   name 'WordCounter'
   homepage 'http://wordcounterapp.com/'
   license :commercial
