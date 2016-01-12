e492abed4939935c3171fa96acef70c76eb7e623
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:24:44 +0000

    xquartz.rb: appcast sha256 without pubDate

--- Casks/xquartz.rb
+++ Casks/xquartz.rb
@@ -5,7 +5,7 @@ cask 'xquartz' do
   # macosforge.org is the official download host per the vendor homepage
   url "https://xquartz.macosforge.org/downloads/SL/XQuartz-#{version}.dmg"
   appcast 'https://xquartz-dl.macosforge.org/sparkle/release.xml',
-          :sha256 => '0bbfb27ff8686b3ce35e40a9f6d307e1568bda2db48e6a2a840d4e8d29f5f768'
+          :sha256 => 'f88f13a39d87f3b9d2dcc2cf8678aac277f644ab1ffbf75577e1bba54fe1eb43'
   name 'XQuartz'
   homepage 'http://www.xquartz.org/'
   license :oss
