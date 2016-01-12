9fbdfa969e5455b02e95e21a0c264bbb6e2fdb87
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 18:03:51 +0000

    updated gear-player (2.2.21)

--- Casks/gear-player.rb
+++ Casks/gear-player.rb
@@ -1,10 +1,10 @@
 cask 'gear-player' do
-  version :latest
-  sha256 :no_check
+  version '2.2.21'
+  sha256 '95461d52a45019460afee7d0e3a32102755ff60bd75d9c86d2db1941a4c688c4'
 
-  url 'https://dl.gearmusicplayer.com/gear.zip'
+  url 'https://dl.gearmusicplayer.com/gearupdate.zip'
   appcast 'https://dl.gearmusicplayer.com/gearcast.xml',
-          :sha256 => '48497b373d97e37f2bdf93aa4ff730f415a8731acec07083175d538343e5404e'
+          :sha256 => 'f2bb7dff6cb9810cc336fd29fb845a2b9e97ff30db7ea2fd713ac645af17d4c7'
   name 'Gear Player'
   homepage 'https://www.gearmusicplayer.com'
   license :commercial
