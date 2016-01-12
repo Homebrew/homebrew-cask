a26db8245ef7aad0b33d5a5fcd5e096491d6d16a
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:14:58 +1100

    Updated ember to use HTTPS

--- Casks/ember.rb
+++ Casks/ember.rb
@@ -3,7 +3,7 @@ cask 'ember' do
   sha256 'cbd61a207b87a4668907b187198c62bc0b89dfdc31896cb5cbefcf95e2e8f1d1'
 
   url "http://downloads.realmacmedia.com/ember/ember-#{version.no_dots}.zip"
-  appcast 'http://realmacsoftware.com/feeds/ember.xml',
+  appcast 'https://realmacsoftware.com/feeds/ember.xml',
           :sha256 => '37fd752918a4a295e9bf07a2b4634a61bac1f07123585a4a35c8ec988ac354c1'
   name 'Ember'
   homepage 'http://realmacsoftware.com/ember'
