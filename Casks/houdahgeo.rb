8fa4985f39a0bfa203bb0d3d2d23395101751e60
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 18:07:49 +0000

    updated houdahgeo (4.4.1)

--- Casks/houdahgeo.rb
+++ Casks/houdahgeo.rb
@@ -1,10 +1,10 @@
 cask 'houdahgeo' do
-  version '4.4'
-  sha256 '065c215e8c61081d4ff6c925ea20a56ba87d08633d2796aa423066ba9cf1f041'
+  version '4.4.1'
+  sha256 '176e173246803325f3864607244729aadc707d8100c9731a6a2fcc19739b6694'
 
   url "https://www.houdah.com/houdahGeo/updates4/cast_assets/HoudahGeo#{version}.zip"
   appcast "https://www.houdah.com/houdahGeo/updates#{version.major}/cast.xml",
-          :sha256 => '9f98899dd18d866a1d919eed043878e79528a8fe677cf56e8f7cb93aaefd3610'
+          :sha256 => '6a894387c4db638e52d6b1057a619e688eef0e222d78df50af7e5948a0b8e594'
   name 'HoudahGeo'
   homepage 'https://houdah.com/houdahGeo/'
   license :commercial
