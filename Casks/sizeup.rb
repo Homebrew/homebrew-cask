b7ca9cf554854aa706afa959be7716d415b5c6cf
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:20:07 +1100

    Updated sizeup to use HTTPS

--- Casks/sizeup.rb
+++ Casks/sizeup.rb
@@ -2,7 +2,7 @@ cask 'sizeup' do
   version '1.7.1'
   sha256 'e722b4c96256fcea252a92c798d03a0927b2826a1d6752446ed4f4f20817743a'
 
-  url "http://www.irradiatedsoftware.com/downloads/SizeUp_#{version}.zip"
+  url "https://www.irradiatedsoftware.com/downloads/SizeUp_#{version}.zip"
   appcast 'https://www.irradiatedsoftware.com/updates/profiles/sizeup.php',
           :sha256 => '563a7a8eb8623b0af56921f32407cfbc83752edc5a0f2dfe1eff73be7525c2d6'
   name 'SizeUp'
