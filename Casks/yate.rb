4a1a7a3e7e9d35c78f59598c89232f6e8c78b58f
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 16:42:47 +0000

    updated yate (3.10)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,10 +1,10 @@
 cask 'yate' do
-  version :latest
-  sha256 :no_check
+  version '3.10'
+  sha256 '0e91a895d4b5615fe65ce6a1a5b2e928e73de41e3b1c1750bf4b12e6106ad8ba'
 
   url 'https://2manyrobots.com/Builds/Yate/Yate.dmg'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          :sha256 => 'dcffb9d2f0b19ef6427906fa1f742d546166f214fcc6d0ae0b423e19a146d8c0'
+          :sha256 => 'c679b9ea8aa4d7634e5b8104c6db125ea223dd814736bca8e2f5053965ac4b9a'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
   license :commercial
