128ca9db628215f2d110fb8ae6d63e822056bc45
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:21:33 +0000

    trailrunner.rb: appcast sha256 without pubDate

--- Casks/trailrunner.rb
+++ Casks/trailrunner.rb
@@ -4,7 +4,7 @@ cask 'trailrunner' do
 
   url 'http://downloads.trailrunnerx.com/TrailRunner.app.zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/83c4086e3f968b874757ba689e71f610',
-          :sha256 => 'eb7ebed6ca392b7676a6424376b8f82748a6478d2a4f2b23b102f3b7b05ab28c'
+          :sha256 => 'db9e4fb35a05986f30c8f82290d17b584984b7bf548bf2a10d551138798cd674'
   name 'TrailRunner'
   homepage 'http://trailrunnerx.com/'
   license :gratis
