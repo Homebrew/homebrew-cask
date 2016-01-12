29e16d93241f0c620a037d128373d2ff83547227
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:05:06 +1100

    Updated bootchamp to use HTTPS

--- Casks/bootchamp.rb
+++ Casks/bootchamp.rb
@@ -2,7 +2,7 @@ cask 'bootchamp' do
   version '1.7'
   sha256 'a6494fbc9efd057d8f25ba3aa8122e2b037145cc78e1bba12c69401afc06d0bc'
 
-  url 'http://kainjow.com/downloads/BootChamp.zip'
+  url 'https://kainjow.com/downloads/BootChamp.zip'
   appcast 'https://kainjow.com/updates/bootchamp.xml',
           :sha256 => 'e3304e693f6f12a77ff2560785f7b7f35ac90013595d3bfbe349e199b6d5ad61'
   name 'BootChamp'
