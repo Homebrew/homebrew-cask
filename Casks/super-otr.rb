931bbc9805ebd5b4cd1c5ab05e07e9adebdb6433
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:18:52 +0000

    super-otr.rb: appcast sha256 without pubDate

--- Casks/super-otr.rb
+++ Casks/super-otr.rb
@@ -4,7 +4,7 @@ cask 'super-otr' do
 
   url "http://apfel-a.macbay.de/wordpress/wp-content/plugins/download-monitor/download.php?id=Super-OTR-#{version}.zip"
   appcast 'http://apfel-a.macbay.de/software/superotr/appcast-superotr.xml',
-          :sha256 => '191fa832fae581006bbc419d37acbb59121a3aefcdebefe0364a6f8e216bcca2'
+          :sha256 => 'd77a6f352c47713792161c4dadd7cec6b3f720989a215cdbf1930ff1b64bf46c'
   name 'Super OTR'
   homepage 'http://apfel-a.macbay.de/super-otr/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
