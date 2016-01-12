8bd031d72f75b45ca67f89a44e48a847f9551224
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:03:58 +0000

    kamakiri.rb: appcast sha256 without pubDate

--- Casks/kamakiri.rb
+++ Casks/kamakiri.rb
@@ -4,7 +4,7 @@ cask 'kamakiri' do
 
   url "https://mochidev.com/appresources/downloads/Kamakiri%20#{version.sub(%r{-.*$}, '')}%20(#{version.sub(%r{^.*-}, '')}).zip"
   appcast 'https://mochidev.com/appresources/updates/kamakiridd.xml',
-          :sha256 => 'cdaff5053910c08c9cf39d1e967c6581d157b564cac26a5f285f51d4ef6fae66'
+          :sha256 => 'b3f7fdf1557e3f988a036ac2d9ed0dc9b97d151c1a30cf90420bc20cd25d2e9a'
   name 'Kamakiri'
   homepage 'https://mochidev.com/apps/kamakiri/'
   license :commercial
