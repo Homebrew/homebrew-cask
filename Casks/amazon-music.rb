7d026a35353d74b93e542a6fa4c772c22d61f645
Tommy Sparber <tommy@sparber.net>
2016-01-12 18:54:22 +1100

    Updated amazon-music to use HTTPS

--- Casks/amazon-music.rb
+++ Casks/amazon-music.rb
@@ -4,7 +4,7 @@ cask 'amazon-music' do
 
   url "https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/#{version.sub(%r{_.*}, '')}/#{version.sub(%r{.*_}, '')}/AmazonMusicInstaller.dmg"
   name 'Amazon Music'
-  homepage 'http://www.amazon.com/gp/feature.html/?ie=UTF8&docId=1001067901'
+  homepage 'https://www.amazon.com/gp/feature.html/?ie=UTF8&docId=1001067901'
   license :gratis
 
   installer :script => 'Amazon Music Installer.app/Contents/MacOS/osx-intel',
