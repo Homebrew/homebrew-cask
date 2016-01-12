12ecf6f69cae9c96aa6a04eb41d59d08b1be560b
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:15:11 +1100

    Updated flixster-desktop to use HTTPS

--- Casks/flixster-desktop.rb
+++ Casks/flixster-desktop.rb
@@ -3,7 +3,7 @@ cask 'flixster-desktop' do
   sha256 '15cb9c5eb8ebac0d994114394c163afb97e5db8d22e7f256bb3a4dea29e86e58'
 
   # cloudfront.net is the official download host per the vendor homepage
-  url 'http://dtmmt9rxsy2no.cloudfront.net/desktop/mac/FlixsterDesktop.zip'
+  url 'https://dtmmt9rxsy2no.cloudfront.net/desktop/mac/FlixsterDesktop.zip'
   appcast 'https://dtmmt9rxsy2no.cloudfront.net/desktop/mac/FlixsterDesktopMacAppcast.xml',
           :sha256 => 'd2d08f23061e1d0bcb3ece14d5023ca5c372e1948a46bc63f14aa3e3466e7485'
   name 'Flixster Desktop for Mac'
