07d06858269fe9fc2775fa5a18c27d6efb30582f
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 16:08:43 +0000

    updated whiskey (0.4.0)

--- Casks/whiskey.rb
+++ Casks/whiskey.rb
@@ -1,10 +1,10 @@
 cask 'whiskey' do
-  version :latest
-  sha256 :no_check
+  version '0.4.0'
+  sha256 '27fa0798ac0c10821d5066ccc1f240714964d931548f61bca5da355277af6402'
 
   url 'http://usewhiskey.com/latest'
   appcast 'http://usewhiskey.com/appcast.xml',
-          :sha256 => '96dea545ba04bfa604924f6e2d995d3a71ca2ef16997f494724543f8c0b60ef4'
+          :sha256 => '7ed688377dff74817a7de04a52fb93b41a97f0ebd87cb6db92752f9fa7d5962c'
   name 'Whiskey'
   homepage 'http://usewhiskey.com/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
