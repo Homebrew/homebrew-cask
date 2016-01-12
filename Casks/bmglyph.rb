ec4a238a7693500b6788beb90711287ffef24677
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:54:52 +0000

    bmglyph.rb: appcast sha256 without pubDate

--- Casks/bmglyph.rb
+++ Casks/bmglyph.rb
@@ -4,7 +4,7 @@ cask 'bmglyph' do
 
   url "http://www.bmglyph.com/application/bmGlyph.#{version}.zip"
   appcast 'http://www.bmglyph.com/application/bmGlyphVersion.xml',
-          :sha256 => '43c86d82a5f63b1316913b06399598e792ed2ae23e6bdc2e6e8ad98b3d7e4178'
+          :sha256 => '43981d6f100374c4a14c750865f0479407101aea3288fb0ae3ee93d9e54dfc6a'
   name 'bmGlyph'
   homepage 'http://www.bmglyph.com/'
   license :commercial
