a7ad4c6da82561745faaa03f3ad91b58b096d661
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:12:29 +0000

    openscad.rb: appcast sha256 without pubDate

--- Casks/openscad.rb
+++ Casks/openscad.rb
@@ -4,7 +4,7 @@ cask 'openscad' do
 
   url "http://files.openscad.org/OpenSCAD-#{version}.dmg"
   appcast 'http://files.openscad.org/appcast.xml',
-          :sha256 => '554ac467d9f15e58b0857187b44bb63fefca247a2adc83e6fc5510b6e92699dd'
+          :sha256 => '6bf06ee7274ba17f1253aef8b5d87c8f3b383c52fb0421bda7ef7505a08890c2'
   name 'OpenSCAD'
   homepage 'http://www.openscad.org/'
   license :gpl
