dff669fe45ac4f66161fd2c2ca51c514da20d057
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:13:31 +0000

    pomotodo.rb: appcast sha256 without pubDate

--- Casks/pomotodo.rb
+++ Casks/pomotodo.rb
@@ -5,7 +5,7 @@ cask 'pomotodo' do
   # hackplan.com is the official download host per the vendor homepage
   url "http://cdn.hackplan.com/theair/#{version.after_comma}/Pomotodo_v#{version.before_comma}.dmg"
   appcast 'http://air.hackplan.com/projects/5455f382437315386000d4d5/versions/latest.xml',
-          :sha256 => 'af39a05955cef58ad8eec7cf0b767752c803875f03c6fc43bcf64d3152a29985'
+          :sha256 => '573e904ed563b7b51b7dbf384ba0359923de23a0041e867e664594ab3699a181'
   name 'Pomodoro'
   homepage 'https://pomotodo.com'
   license :gratis
