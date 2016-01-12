c24c04432dd61b02e12fe04e1245b6b935a02751
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:20:13 +1100

    Updated snapheal-pro to use HTTPS

--- Casks/snapheal-pro.rb
+++ Casks/snapheal-pro.rb
@@ -3,7 +3,7 @@ cask 'snapheal-pro' do
   sha256 'b6b46a70f270872a5b9cd32f8366070227af4e70d37b054b3b79280d425b2aa2'
 
   # amazonaws.com is the official download host per the appcast feed
-  url "http://creativekit.s3.amazonaws.com/SnaphealCK/SnaphealCKNA_Distribution_v#{version.dots_to_underscores}.zip"
+  url "https://creativekit.s3.amazonaws.com/SnaphealCK/SnaphealCKNA_Distribution_v#{version.dots_to_underscores}.zip"
   appcast 'http://cdn.macphun.com/updates/SnaphealMacPRO/appcast.xml',
           :sha256 => '3c29cd133345294e0f073a1d02df5339dd69facee830d3bfc77fa7f3be79739d'
   name 'Macphun Snapheal Pro'
