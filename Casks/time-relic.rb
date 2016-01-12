1bdf7aec84ab348ba3d283648f065699b644413f
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 15:17:00 +0000

    updated time-relic (2.1.0)

--- Casks/time-relic.rb
+++ Casks/time-relic.rb
@@ -1,11 +1,11 @@
 cask 'time-relic' do
-  version :latest
-  sha256 :no_check
+  version '2.1.0'
+  sha256 '95d881617a83e3e9787c06608fa56191151522f4f0224f09ce8300db32f73c48'
 
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://timerelic-download.s3.amazonaws.com/Time%20Relic.app.zip'
   appcast 'https://timerelic-download.s3.amazonaws.com/appcast.xml',
-          :sha256 => '671ff3726eab6e24fdafb47d2b0db7fc70dc09525bc3d5b15be7c12f1f5df196'
+          :sha256 => 'f2f31bebc730e67f64b31bd00eac77d0e45cfe58d1b503185fa42f5ff8d1a443'
   name 'Time Relic'
   homepage 'http://timerelic.com/'
   license :gratis
