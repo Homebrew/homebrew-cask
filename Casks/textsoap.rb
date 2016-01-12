520ba6ae077433d8e81b8c95d1b3679c09a2f387
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:19:32 +0000

    textsoap.rb: appcast sha256 without pubDate

--- Casks/textsoap.rb
+++ Casks/textsoap.rb
@@ -5,7 +5,7 @@ cask 'textsoap' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://unmarked.s3.amazonaws.com/textsoap7.zip'
   appcast 'https://unmarked.s3.amazonaws.com/appcast/textsoap7.xml',
-          :sha256 => '37211786d3aca6f891665256f383906a337bea31958b4b7a2ff0fbbaae98709e'
+          :sha256 => '0265f6c4621f39f0b0934b84fd54304f7348e24d49a3145e35b5514d7a622819'
   name 'TextSoap'
   homepage 'http://www.unmarked.com/textsoap/'
   license :commercial
