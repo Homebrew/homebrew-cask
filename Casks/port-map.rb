434e3374fe88a8cafb101e22162f06301993f578
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:18:15 +1100

    Updated port-map to use HTTPS

--- Casks/port-map.rb
+++ Casks/port-map.rb
@@ -3,10 +3,10 @@ cask 'port-map' do
   sha256 :no_check
 
   url 'http://www.codingmonkeys.de/portmap/download'
-  appcast 'http://www.codingmonkeys.de/portmap/appcast.rss',
+  appcast 'https://www.codingmonkeys.de/portmap/appcast.rss',
           :sha256 => 'c74b6bc407fe3a5daea1f3be09deabef3590fa5dc63c8ed32062f97d8bf5aa79'
   name 'Port Map'
-  homepage 'http://www.codingmonkeys.de/portmap'
+  homepage 'https://www.codingmonkeys.de/portmap'
   license :mit
 
   depends_on :macos => '>= :tiger'
