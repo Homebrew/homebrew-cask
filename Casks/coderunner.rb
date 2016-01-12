5ff03b301dbc90687c22d3ca10f886d83f2484ed
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:57:03 +0000

    coderunner.rb: appcast sha256 without pubDate

--- Casks/coderunner.rb
+++ Casks/coderunner.rb
@@ -5,7 +5,7 @@ cask 'coderunner' do
   # cloudfront.net is the official download host per the vendor homepage
   url "https://dktfof1z89xc1.cloudfront.net/CodeRunner%20#{version}.zip"
   appcast 'https://coderunnerapp.com/appcast.xml',
-          :sha256 => '9b2e8558235267858dfc4af85fc8c5e7f24c41f0535256ea5300409c5571866c'
+          :sha256 => '96376821ad5f0fb6a193e5af27632f2410294268afd895602a4e22f533a86a76'
   name 'CodeRunner'
   homepage 'https://coderunnerapp.com/'
   license :commercial
