2c90fb91b18648ad4a599b64f7525a9d750eaa2b
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 16:38:58 +0000

    updated xtabulator (2.002)

--- Casks/xtabulator.rb
+++ Casks/xtabulator.rb
@@ -1,13 +1,13 @@
 cask 'xtabulator' do
-  version :latest
-  sha256 :no_check
+  version '2.002'
+  sha256 'a4ec6d302f233599034c51cbc5e09ff42964022e694487f1793185c5105d5ddc'
 
-  url 'http://www.bartastechnologies.com/products/xtabulator/XTabulator2.zip'
+  url "http://www.bartastechnologies.com/products/xtabulator/sparkleupdates/#{version}.zip"
   appcast 'http://www.bartastechnologies.com/products/xtabulator/sparkleupdates/xtappcast.php',
-          :sha256 => '1ee23eeaded78d9f1c9bbc1cffa6db165fb11e06e33e120db1f3c928d582132c'
-  name 'XTabulator 2'
+          :sha256 => '11a351944e066600a05b69abb8c697be616bf6a2eabf50a8e92d2f28b519093b'
+  name 'XTabulator'
   homepage 'http://www.bartastechnologies.com/products/xtabulator/'
   license :commercial
 
-  app 'XTabulator 2/XTabulator.app'
+  app "XTabulator #{version.major}/XTabulator.app"
 end
