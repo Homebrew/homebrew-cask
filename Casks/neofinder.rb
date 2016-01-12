5426f744228684092196748fff8aa4f21f316885
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:11:53 +0000

    neofinder.rb: appcast sha256 without pubDate

--- Casks/neofinder.rb
+++ Casks/neofinder.rb
@@ -4,7 +4,7 @@ cask 'neofinder' do
 
   url 'http://www.cdfinder.de/neofinder.zip'
   appcast 'http://www.wfs-apps.de/updates/neofinder-appcast-64.xml',
-          :sha256 => '3434aae6fbc46a5d36590690c03a938e4cdda3af262bab47f4cfb3e60db42832'
+          :sha256 => 'd65ad22d2fb3e748acdd40108e411435870f96e8f7db4a7bd16f8642fcf2b69d'
   name 'NeoFinder'
   homepage 'http://www.cdfinder.de'
   license :commercial
