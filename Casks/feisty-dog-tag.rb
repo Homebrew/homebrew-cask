000143a3c6554d3fa345c3f46115a8966163bcd3
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:15:05 +1100

    Updated feisty-dog-tag to use HTTPS

--- Casks/feisty-dog-tag.rb
+++ Casks/feisty-dog-tag.rb
@@ -3,7 +3,7 @@ cask 'feisty-dog-tag' do
   sha256 '1bfc83427aa474d9df3c5413ee5645a0e5b134ceef642bf99a717af1798e72df'
 
   # amazonaws.com is the official download host per the appcast feed
-  url "http://s3.amazonaws.com/feisty-dog/versions/binaries/000/000/#{version.after_comma}/Tag-#{version.before_comma}.zip"
+  url "https://s3.amazonaws.com/feisty-dog/versions/binaries/000/000/#{version.after_comma}/Tag-#{version.before_comma}.zip"
   appcast 'https://www.feisty-dog.com/panel/updates/Tag',
           :sha256 => '775f68c198856332ee89dfbe2f115d3a9cc72cb44d1bf701acfdc43eca0c6a02'
   name 'Tag'
