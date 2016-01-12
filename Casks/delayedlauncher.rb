419fbac85ec6b75d3699bd897a1f608d4f3f24d3
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:58:03 +0000

    delayedlauncher.rb: appcast sha256 without pubDate

--- Casks/delayedlauncher.rb
+++ Casks/delayedlauncher.rb
@@ -4,7 +4,7 @@ cask 'delayedlauncher' do
 
   url 'https://www.taoeffect.com/delayedlauncher/DelayedLauncher.zip'
   appcast 'https://www.taoeffect.com/delayedlauncher/appcast.xml',
-          :sha256 => '7890874b09454101e7af04a798fea7a6daea3c18f820b628ce51c12c9d4b2f00'
+          :sha256 => 'a8d8ca4e2e5aad56bdd75ae853f7fdd57fbbdb0850a200d9f35542ad8738b493'
   name 'DelayedLauncher'
   homepage 'https://www.taoeffect.com/blog/2010/03/delayedlauncher-2-0/'
   license :bsd
