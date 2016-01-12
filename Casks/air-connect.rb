283793733a1576870683e98e99bdaa21e1eeff99
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:52:41 +0000

    air-connect.rb: appcast sha256 without pubDate

--- Casks/air-connect.rb
+++ Casks/air-connect.rb
@@ -4,7 +4,7 @@ cask 'air-connect' do
 
   url "https://www.avatron.com/updates/software/airconnect_mac/acmac#{version.delete('.')}.zip"
   appcast 'https://avatron.com/updates/software/airconnect_mac/appcast.xml',
-          :sha256 => 'af9bc6dc41bc632995c4e49b958a5623bc091ac0fe1fb337fbc9a571cfc1e85b'
+          :sha256 => 'a7590adc2917c5206de2b6d94aff3c78da38be43e8273900111a6904b973a8bb'
   name 'Air Connect'
   homepage 'https://avatron.com/get-air-connect/'
   license :gratis
