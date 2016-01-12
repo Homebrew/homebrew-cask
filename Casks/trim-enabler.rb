3a56867b39be1095f1247e4bad3f77eb4eb52ede
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:21:30 +0000

    trim-enabler.rb: latest version

--- Casks/trim-enabler.rb
+++ Casks/trim-enabler.rb
@@ -4,8 +4,8 @@ cask 'trim-enabler' do
     sha256 '3d9a7ad184435c05c4d8d5bf74484dcd9ac0f6eeb6a7a78e22709ca1557ff108'
     url 'https://cindori.org/trimenabler/TrimEnabler_old.dmg'
   else
-    version '3.4.2'
-    sha256 '423339ad1806701a87c50859e8b54c1011c4d9753c163f4b12d265424c55d314'
+    version :latest
+    sha256 :no_check
     # amazonaws.com is the official download host per the vendor homepage
     url 'https://s3.amazonaws.com/cindori/TrimEnabler.dmg'
   end
