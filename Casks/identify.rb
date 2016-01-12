f63e0a13b818102e8e4d51ed8c8d25c4aa5100b3
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:02:57 +0000

    identify.rb: appcast sha256 without pubDate

--- Casks/identify.rb
+++ Casks/identify.rb
@@ -5,7 +5,7 @@ cask 'identify' do
   # dropbox.com is the official download host per the appcast feed
   url "http://dl.dropbox.com/u/1495067/iDentifySparkle/iDentifyLite#{version}.zip"
   appcast 'https://dl.dropboxusercontent.com/u/1495067/iDentifySparkle/iDentifyAppcast.xml',
-          :sha256 => 'db8eb3671673ae557911e5678b70738b07672dd30e93eec806532448ce22c28e'
+          :sha256 => '08555291e78d4205a2b41679cd2de41f65464804c72269e3d008314d8eab3754'
   name 'iDentify'
   homepage 'http://identify2.arrmihardies.com/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
