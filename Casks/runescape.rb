603a25c0610bffeb6b9bf0016700b49aba5e8949
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:18:23 +1100

    Updated runescape to use HTTPS

--- Casks/runescape.rb
+++ Casks/runescape.rb
@@ -2,7 +2,7 @@ cask 'runescape' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.runescape.com/downloads/runescape.dmg'
+  url 'https://www.runescape.com/downloads/runescape.dmg'
   name 'RuneScape'
   homepage 'https://www.runescape.com'
   license :gratis
