1c082b6081827a11efc4a52435b6d0c26545ff80
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:18:12 +1100

    Updated piezo to use HTTPS

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -2,7 +2,7 @@ cask 'piezo' do
   version '1.2.2'
   sha256 '6be59b8b525ddb049a4ee24f40bce06beba5d98c2a6ada6826b9783bad28fb3b'
 
-  url "http://rogueamoeba.com/legacy/downloads/Piezo-#{version.no_dots}.zip"
+  url "https://rogueamoeba.com/legacy/downloads/Piezo-#{version.no_dots}.zip"
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo',
           :sha256 => 'f9a8903cc1e07056cd541257e6d61e65fc4fe653b91d5e732ce8870a46e39f45'
   name 'Piezo'
