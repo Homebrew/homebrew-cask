5fb8a6341fd3907984082362786c0a7117f44975
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 18:16:44 +0000

    updated moneymoney (2.2.9)

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,10 +1,10 @@
 cask 'moneymoney' do
-  version :latest
-  sha256 :no_check
+  version '2.2.9'
+  sha256 'dcdb1f70267328ac281ceb7ab3ddb87e17f0e736741653a0ea45f3a2db32c5e3'
 
-  url 'https://moneymoney-app.com/download/MoneyMoney.zip'
-  appcast 'https://moneymoney-app.com/update/appcast.xml',
-          :sha256 => '6f64a33786f0a3b469dfb4ff70ff1241eb678f049590dd79fccd98fe17087a37'
+  url 'https://service.moneymoney-app.com/1/MoneyMoney.zip'
+  appcast 'https://service.moneymoney-app.com/1/Appcast.xml',
+          :sha256 => '3172028de57f260078a002c04a2126aec4dc3d4b5a0c9c9528a1b4ffeba39bea'
   name 'MoneyMoney'
   homepage 'https://moneymoney-app.com/'
   license :commercial
