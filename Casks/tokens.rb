a82094d4828f8c83fde5bc11ed404cd22c8c4b84
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 15:23:41 +0000

    updated tokens (1.5.1)

--- Casks/tokens.rb
+++ Casks/tokens.rb
@@ -1,10 +1,10 @@
 cask 'tokens' do
-  version :latest
-  sha256 :no_check
+  version '1.5.1'
+  sha256 '650e664084555b3b2d7cef8dcdb5fd0f471520ac8a1bcde42645e11593a51a52'
 
-  url 'http://usetokens.com/latest'
+  url "http://resources.usetokens.com/download/#{version}/Tokens.app.zip"
   appcast 'http://api.usetokens.com/updates/stable',
-          :sha256 => '87feb75f006cd8570bf7d11102e43576e7e6c9a3558454ebaf19376d587e3bc3'
+          :sha256 => '51d915b42a68a23d614eaae84a85dad1067414f0d1af9833569dc4936bd10d92'
   name 'Tokens'
   homepage 'http://usetokens.com/'
   license :closed
