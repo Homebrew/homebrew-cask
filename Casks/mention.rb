eda488744bce2266218f4cc99e248d3820416a59
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:08:16 +0000

    mention.rb: appcast sha256 without pubDate

--- Casks/mention.rb
+++ Casks/mention.rb
@@ -4,7 +4,7 @@ cask 'mention' do
 
   url 'https://en.mention.com/downloads/mac/mention.dmg'
   appcast 'https://web.mention.com/uploads/appcast-mention.xml',
-          :sha256 => 'e3ee4b1624ab193b8236392da1c2051ca1ad5d8bf1c6baae76fed80cfd9d59c4'
+          :sha256 => '35cd1aa9dfff04fc5b0252496eb603992623c0f7c983b4b6c98b7f345d38c2d5'
   name 'Mention'
   homepage 'https://en.mention.com/'
   license :gratis
