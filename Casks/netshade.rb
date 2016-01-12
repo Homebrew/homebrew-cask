84b9b5c376b0fb315614fc2e0bd0beaa6f49f626
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:11:56 +0000

    netshade.rb: appcast sha256 without pubDate

--- Casks/netshade.rb
+++ Casks/netshade.rb
@@ -4,7 +4,7 @@ cask 'netshade' do
 
   url 'http://raynersoftware.com/downloads/NetShade.app.zip'
   appcast 'https://secure.raynersw.com/appcast.php',
-          :sha256 => 'b55277abdc1326bf4e8b4e978b9d9b29b1b384ecbbc7f6024f1a280194634eba'
+          :sha256 => 'a41186f3b0469ae6128af14b783c3710d6d07d0aed581e2cb3e5e0ae0d058bbb'
   name 'NetShade'
   homepage 'http://raynersoftware.com/netshade/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
