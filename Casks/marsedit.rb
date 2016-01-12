254bbf73d8e980b2f59825c5e59ee087db69a4de
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:08:04 +0000

    marsedit.rb: appcast sha256 without pubDate

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -4,7 +4,7 @@ cask 'marsedit' do
 
   url "http://www.red-sweater.com/marsedit/MarsEdit#{version}.zip"
   appcast 'https://www.red-sweater.com/marsedit/appcast3.php',
-          :sha256 => '3250869e30276202cdc41279ba3a7936114a87ed9d157c836362b0f3add9aa62'
+          :sha256 => 'dbeebc057471f81ce47f00a5cb8b8dbf8ffc44f7aeabf65d16fe3083f1cdb10b'
   name 'MarsEdit'
   homepage 'https://www.red-sweater.com/marsedit/'
   license :commercial
