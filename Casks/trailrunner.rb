b6626dd6bb1a1f11598b55a741a314b41575051f
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 15:33:21 +0000

    updated trailrunner (3.8.791)

--- Casks/trailrunner.rb
+++ Casks/trailrunner.rb
@@ -1,10 +1,10 @@
 cask 'trailrunner' do
-  version :latest
-  sha256 :no_check
+  version '3.8.791'
+  sha256 'd9658293fb783bac58067249958acf2476f3c498d281274be53e93cc5ab9f585'
 
   url 'http://downloads.trailrunnerx.com/TrailRunner.app.zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/83c4086e3f968b874757ba689e71f610',
-          :sha256 => '479cce6567dc1c7a8b4409c8ab4df678a373111c98f5d2d977ff2c5f9eddd7d0'
+          :sha256 => 'eb7ebed6ca392b7676a6424376b8f82748a6478d2a4f2b23b102f3b7b05ab28c'
   name 'TrailRunner'
   homepage 'http://trailrunnerx.com/'
   license :gratis
