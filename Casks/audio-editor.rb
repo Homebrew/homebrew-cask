4f9d0028543e61045f88d5e79b9376eeca2e3137
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:53:58 +0000

    audio-editor.rb: appcast sha256 without pubDate

--- Casks/audio-editor.rb
+++ Casks/audio-editor.rb
@@ -4,7 +4,7 @@ cask 'audio-editor' do
 
   url 'http://www.macsome.com/AudioEditor.dmg'
   appcast 'http://www.macsome.com/audio-editor-mac/su_feed.xml',
-          :sha256 => '562863ab96acd8e542cfa83a63ffb7bab65f8ac91327c537567d8128b3fab30e'
+          :sha256 => 'eae94846cf703af4153e2a3d7627b6a94f057a6bdf31d0c25a2e959108d749c8'
   name 'Macsome Audio Editor'
   homepage 'http://www.macsome.com/audio-editor-mac/'
   license :gratis
