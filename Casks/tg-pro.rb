2065c47925e56a511131ed0020f6584e932134a1
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 15:11:36 +0000

    updated tg-pro (2.9.5)

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,10 +1,10 @@
 cask 'tg-pro' do
-  version '2.8.8'
-  sha256 'dcb4221d4b72960c306e248a0be947107ab3622b0c38570684b2f12c8ef87a44'
+  version '2.9.5'
+  sha256 '6f8cb96bf91ee3a69a5f91c47232427826e5516355d45656c4be9118d5c5b78a'
 
-  url "https://www.tunabellysoftware.com/resources/TGPro_#{version.gsub('.', '_')}.zip"
-  appcast 'https://tunabellysoftware.com/resources/sparkle/tgpro/profileInfo.php',
-          :sha256 => 'ae55143d14a7a75093439c723db19e8672952efff4e38de0e0682a5037c455de'
+  url "https://www.tunabellysoftware.com/resources/TGPro_#{version.dots_to_underscores}.zip"
+  appcast 'https://www.tunabellysoftware.com/resources/sparkle/tgpro/profileInfo.php',
+          :sha256 => 'd762e608760132bf8da8e269488d231a7f582e536dfca65752f4d82e46c0beab'
   name 'TG Pro'
   homepage 'https://www.tunabellysoftware.com/tgpro/'
   license :commercial
