99143edfae8d720849551147c644872e301d074e
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 18:31:40 +0000

    updated recordit (1.6.7)

--- Casks/recordit.rb
+++ Casks/recordit.rb
@@ -1,10 +1,10 @@
 cask 'recordit' do
-  version '1.6.6'
-  sha256 'c7a665123b14aade0bec5bb1a29709cfe71573fb2a44c0db9777bcedd3d6ee0f'
+  version '1.6.7'
+  sha256 '1a18f527a10c0f9ba463fd747f96b5b863cbf19e9b90639181f614006307aeaa'
 
   url 'http://recordit.co/latest'
   appcast 'https://rink.hockeyapp.net/api/2/apps/5fcda0b48f1dcf0c938b289b9ab57790',
-          :sha256 => '61cb6dc1538206b6e522dab1db07e101a6ade1e013ad4af9748815305d32750d'
+          :sha256 => 'ca5eee51204e7ead23764b4dc67087e9f0292d727d361efaa44bd51f489a18e4'
   name 'RecordIt'
   homepage 'http://recordit.co/'
   license :gratis
