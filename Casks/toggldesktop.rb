6b98ce11822a960c9e7a19455b8cafb7bb4ecae9
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:21:20 +0000

    toggldesktop.rb: appcast sha256 without pubDate

--- Casks/toggldesktop.rb
+++ Casks/toggldesktop.rb
@@ -5,7 +5,7 @@ cask 'toggldesktop' do
   # github.com is the official download host per the appcast feed
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_stable_appcast.xml',
-          :sha256 => '6641c717bb95709107b3a9dadf73db7c5aee2fda32bb7ba83ce3ee10bec61531'
+          :sha256 => '16f316c6bb6565170ee35aa2c146b8be5b3a7062e7f70d2fd2e97eac6311ad64'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com'
   license :bsd
