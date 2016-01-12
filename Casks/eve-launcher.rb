399d14c256943e3cf4b29b7451573a9554f62f58
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:15:01 +1100

    Updated eve-launcher to use HTTPS

--- Casks/eve-launcher.rb
+++ Casks/eve-launcher.rb
@@ -2,7 +2,7 @@ cask 'eve-launcher' do
   version '995234'
   sha256 'a4d39ded415823c9fd7ed74c95f111d98ff7c44f00fc0d76cb40e2942831a68e'
 
-  url "http://binaries.eveonline.com/EveLauncher-#{version}.dmg"
+  url "https://binaries.eveonline.com/EveLauncher-#{version}.dmg"
   name 'Eve Online'
   homepage 'https://www.eveonline.com/'
   license :commercial
