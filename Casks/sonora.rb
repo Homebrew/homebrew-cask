c41b28df0ea07b807d74b094ac20d3c2e0520d39
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:17:51 +0000

    sonora.rb: appcast sha256 without pubDate

--- Casks/sonora.rb
+++ Casks/sonora.rb
@@ -5,7 +5,7 @@ cask 'sonora' do
   # github.com is the official download host per the vendor homepage
   url 'https://github.com/downloads/sonoramac/Sonora/Sonora.zip'
   appcast 'https://getsonora.com/updates/sonora2.xml',
-          :sha256 => '961a6fecdddf1f5e1dba7360a697a09781d202b61bc48369240bcef226f7f441'
+          :sha256 => '1f1bc37f92e4e02d1370733d50c9460bb0266e79cd2efe50303381f2769f50b7'
   name 'Sonora'
   homepage 'https://getsonora.com/'
   license :gratis
