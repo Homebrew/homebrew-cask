55061698bc7ed928793cf0319c5c0ced88cd52f2
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:17:46 +0000

    snippet-edit.rb: appcast sha256 without pubDate

--- Casks/snippet-edit.rb
+++ Casks/snippet-edit.rb
@@ -4,7 +4,7 @@ cask 'snippet-edit' do
 
   url "http://cocoaholic.com/downloads/snippet_edit/Snippet_Edit_#{version}.zip"
   appcast 'http://cocoaholic.com/sparkle/snippet_edit/sparkle.xml',
-          :sha256 => 'a32af024b5e43f5bd6fef837808d8130359000c2f7f975f38abc46aecf17e113'
+          :sha256 => '8c0c6598cb4cfe00b28449c7b28a717f46b0cb2072a5e39cce090352e87b60a4'
   name 'Snippet Edit'
   homepage 'http://cocoaholic.com/snippet_edit/'
   license :gratis
