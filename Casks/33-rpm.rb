7e6dc36a1c7502be5eb70e8a07dfc7e37411f815
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:52:26 +0000

    33-rpm.rb: appcast sha256 without pubDate

--- Casks/33-rpm.rb
+++ Casks/33-rpm.rb
@@ -4,7 +4,7 @@ cask '33-rpm' do
 
   url 'http://www.edenwaith.com/downloads/33rpm.dmg'
   appcast 'http://www.edenwaith.com/xml/33rpm.xml',
-          :sha256 => '2b0e3f1d3f0221e6c59c703e07366003bd8608adf10e4374c4d4b41ec1273276'
+          :sha256 => '0e07936b3b36c0c116773b53907c6ad1fa9a0f0b993342843bf866e6cc5c8433'
   name '33 RPM'
   homepage 'http://www.edenwaith.com/products/33rpm/'
   license :gratis
