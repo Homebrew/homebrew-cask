55b43f447d5e9418c3722bc67327c871daaafffd
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:56:44 +0000

    cleanmymac.rb: appcast sha256 without pubDate

--- Casks/cleanmymac.rb
+++ Casks/cleanmymac.rb
@@ -6,7 +6,7 @@ cask 'cleanmymac' do
     # devmate.com is the official download host per the appcast feed
     url "https://dl.devmate.com/com.macpaw.CleanMyMac/#{version.major_minor}/#{version.after_comma}/CleanMyMacClassic-#{version.major_minor}.zip"
     appcast 'https://updates.devmate.com/com.macpaw.CleanMyMac.xml',
-            :sha256 => '13bbf950696a9410fec848e80652f2826209b347fdb329b117371e25445951f5'
+            :sha256 => '15d977eac4369d98243e283f5dd65ac906ec79311bca0cc58a502219d2a90ea2'
     app 'CleanMyMac.app'
     # TODO: add uninstall and zap stanzas for legacy app
   elsif MacOS.release <= :lion
@@ -16,7 +16,7 @@ cask 'cleanmymac' do
     # devmate.com is the official download host per the appcast feed
     url "https://dl.devmate.com/com.macpaw.CleanMyMac2/#{version.major_minor_patch}/#{version.after_comma}/CleanMyMac#{version.to_i}-#{version.major_minor_patch}.zip"
     appcast "https://updates.devmate.com/com.macpaw.CleanMyMac#{version.major}.xml",
-            :sha256 => '612be3e443f49b6b1d5539611693714b2897b9b774896ac15c048daec76a34cb'
+            :sha256 => '15d977eac4369d98243e283f5dd65ac906ec79311bca0cc58a502219d2a90ea2'
     app "CleanMyMac #{version.major}.app"
 
     uninstall :launchctl => "com.macpaw.CleanMyMac#{version.major}.Agent"
@@ -40,7 +40,7 @@ cask 'cleanmymac' do
     # devmate.com is the official download host per the appcast feed
     url "https://dl.devmate.com/com.macpaw.CleanMyMac#{version.major}/#{version.major_minor_patch}/#{version.after_comma}/CleanMyMac3-#{version.major_minor_patch}.zip"
     appcast "https://updates.devmate.com/com.macpaw.CleanMyMac#{version.major}.xml",
-            :sha256 => 'e62a4026c55f52a3c31a0070e8fb9d49c63244697dff9332a3ab37f74d02c5aa'
+            :sha256 => '15d977eac4369d98243e283f5dd65ac906ec79311bca0cc58a502219d2a90ea2'
     app "CleanMyMac #{version.major}.app"
 
     postflight do
