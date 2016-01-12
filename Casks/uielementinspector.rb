fa2ad4a171f825f21064c71c8e4ec6614411306b
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:20:16 +1100

    Updated uielementinspector to use HTTPS

--- Casks/uielementinspector.rb
+++ Casks/uielementinspector.rb
@@ -2,7 +2,7 @@ cask 'uielementinspector' do
   version :latest
   sha256 :no_check
 
-  url 'http://developer.apple.com/library/mac/samplecode/UIElementInspector/UIElementInspector.zip'
+  url 'https://developer.apple.com/library/mac/samplecode/UIElementInspector/UIElementInspector.zip'
   name 'Apple UIElementInspector'
   homepage 'https://developer.apple.com/library/mac/samplecode/UIElementInspector/Introduction/Intro.html'
   license :gratis
