5ca7e93b871f60c6f54a3c5ad67df9ec7ba77d8d
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:02:31 +0000

    hot-shots.rb: appcast sha256 without pubDate

--- Casks/hot-shots.rb
+++ Casks/hot-shots.rb
@@ -4,7 +4,7 @@ cask 'hot-shots' do
 
   url "https://gngrwzrd.com/HotShots#{version.to_i}.zip"
   appcast 'https://www.gngrwzrd.com/hotshots2-appcast.xml',
-          :sha256 => '33b5ced270f0b6f9d1989f1b6a6d86c62f88c7b5448ed93c86d079c0640fc845'
+          :sha256 => 'd0ad8f060c3d574688563b4b44e71962258846889035c0d62cf3a5f1a6d60ec5'
   name 'Hot Shots'
   homepage 'https://gngrwzrd.com/hotshots/'
   license :commercial
