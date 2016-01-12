440cc885be72fec3c03cb837abda7d0a7b2cfe99
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:08:24 +0000

    metadatics.rb: appcast sha256 without pubDate

--- Casks/metadatics.rb
+++ Casks/metadatics.rb
@@ -4,7 +4,7 @@ cask 'metadatics' do
 
   url 'http://www.markvapps.com/applications/metadatics/Metadatics.zip'
   appcast 'http://www.markvapps.com/applications/metadatics/metadatics_appcast.xml',
-          :sha256 => 'be165a851e1f29743a79ad4252ccda747ed2a5a5ceb11a096e83134594919eb0'
+          :sha256 => 'dd15e8dbbc8fa5479224534b19f6cb3166f5aed388f7caebad521e1f98b47317'
   name 'Metadatics'
   homepage 'http://markvapps.com/metadatics'
   license :commercial
