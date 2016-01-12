f0b817376e7675afac108ad032d9733e15b97977
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 18:05:29 +0000

    updated grandtotal (4.1.5)

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,10 +1,10 @@
 cask 'grandtotal' do
-  version '4.1.4'
-  sha256 '136786ac36d96c543bd3bf0c3cdc714a4c806aa1ec6d95f6f8c1e10f1fbad2d2'
+  version '4.1.5'
+  sha256 '743cc137ab369c8f214fab85753f5b4b0e75a7e82a90995dbfc51e7ff0270a70'
 
   url "https://mediaatelier.com/GrandTotal4/GrandTotal_#{version}.zip"
   appcast 'https://mediaatelier.com/GrandTotal4/feed.php',
-          :sha256 => '59121a6b1a2a5ec64aa6df38e804614c20fab951ff9f195211285844d7366f08'
+          :sha256 => 'e0021023a0e17d686d6dc52f57766f3bf8266cea9f2086dbcdd58252ec5724fb'
   name 'GrandTotal'
   homepage 'https://www.mediaatelier.com/GrandTotal4/'
   license :commercial
