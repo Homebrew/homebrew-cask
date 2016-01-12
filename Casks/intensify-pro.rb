2741ca85020b2c9f67fc1c88ebdfc685e99080e7
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:15:14 +1100

    Updated intensify-pro to use HTTPS

--- Casks/intensify-pro.rb
+++ Casks/intensify-pro.rb
@@ -3,7 +3,7 @@ cask 'intensify-pro' do
   sha256 '6f259029ea928f92efdafe98706d341bd6418d8195374183758ab4cbc6a665c5'
 
   # amazonaws.com is the official download host per the appcast feed
-  url "http://creativekit.s3.amazonaws.com/IntensifyCK/IntensifyCK_Distribution_v#{version.dots_to_underscores}.zip"
+  url "https://creativekit.s3.amazonaws.com/IntensifyCK/IntensifyCK_Distribution_v#{version.dots_to_underscores}.zip"
   appcast 'http://cdn.macphun.com/updates/IntensifyPro/appcast.xml',
           :sha256 => '70bf0d5286ced8a50518463ad7580371f86360767feeaf94a694b73632da7049'
   name 'Intensify Pro'
