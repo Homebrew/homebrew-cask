2947a2bcfe7663bad29464f9cc2aa244660bccf9
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:17:58 +0000

    soundcloud-downloader.rb: appcast sha256 without pubDate

--- Casks/soundcloud-downloader.rb
+++ Casks/soundcloud-downloader.rb
@@ -4,7 +4,7 @@ cask 'soundcloud-downloader' do
 
   url "http://black-burn.ch/scd/downloads/#{version.delete('.')}/in/b"
   appcast 'http://black-burn.ch/applications/scd/updates.php?hwni=1',
-          :sha256 => '5e5e504fd45e7ed4dfcdca80bc8ffa0d7b466b092bbafd8988d0d38884052d26'
+          :sha256 => '69efb39511184a77b6cb9e240c70dad689dd57d5a8d26867af426eb0084e10ed'
   name 'SoundCloud Downloader'
   homepage 'http://black-burn.ch/scd/'
   license :mit
