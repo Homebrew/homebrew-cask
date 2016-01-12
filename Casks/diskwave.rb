9d111a75541a28c0296b2aad821ed753fd779823
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:14:49 +1100

    Updated diskwave to use HTTPS

--- Casks/diskwave.rb
+++ Casks/diskwave.rb
@@ -3,10 +3,10 @@ cask 'diskwave' do
   sha256 '976324e46e4ca4d54240de13cf2c6f0db9afdb703b0e6ef78e2b5b5d36d63e75'
 
   url "http://diskwave.barthe.ph/download/DiskWave_#{version}.dmg"
-  appcast 'http://diskwave.barthe.ph/sparkle/appcast_64bit.php',
+  appcast 'https://diskwave.barthe.ph/sparkle/appcast_64bit.php',
           :sha256 => 'e15fa8d673bc5f0a991d0bf4eddd196fa9d766e82e132bebe2f54f26b7a9a886'
   name 'DiskWave'
-  homepage 'http://diskwave.barthe.ph/'
+  homepage 'https://diskwave.barthe.ph/'
   license :gratis
 
   app 'DiskWave.app'
