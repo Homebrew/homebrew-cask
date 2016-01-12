8859711ad8dcd95fbf3caae3e4f1d7fa0d7f5df0
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:12:53 +0000

    pdfpen.rb: appcast sha256 without pubDate

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -4,7 +4,7 @@ cask 'pdfpen' do
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpen/#{version.before_comma}/#{version.after_comma}/PDFpen-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml',
-          :sha256 => '48ee58e738d901c4e78daef48e231ce4acf25d20501e892c6b36e86b63f31217'
+          :sha256 => '5df0b2298faad1440ce2686145b6678843b9d195358d234d39a46eb99fce7fc1'
   name 'PDFpen'
   homepage 'https://smilesoftware.com/PDFpen/'
   license :commercial
