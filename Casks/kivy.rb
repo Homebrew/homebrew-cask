088cac9e86817eed0b50160dd17fef88662ba08f
Tommy Sparber <tommy@sparber.net>
2016-01-12 19:17:50 +1100

    Updated kivy to use HTTPS

--- Casks/kivy.rb
+++ Casks/kivy.rb
@@ -2,7 +2,7 @@ cask 'kivy' do
   version '1.9.0-rev3'
   sha256 'c142114acd4859665cb7daa238194e09984519120d5756e8ab23e3ba7893ce44'
 
-  url "http://kivy.org/downloads/#{version.split('-').first}/Kivy-#{version}-osx.dmg"
+  url "https://kivy.org/downloads/#{version.split('-').first}/Kivy-#{version}-osx.dmg"
   name 'Kivy'
   homepage 'http://kivy.org'
   license :mit
