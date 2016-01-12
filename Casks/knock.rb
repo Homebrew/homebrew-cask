a3f5a940750803a7e7f931b7e547faa902fb6d92
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:05:34 +0000

    knock.rb: appcast sha256 without pubDate

--- Casks/knock.rb
+++ Casks/knock.rb
@@ -5,7 +5,7 @@ cask 'knock' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://knock-updates.s3.amazonaws.com/Knock.zip'
   appcast 'https://knock-updates.s3.amazonaws.com/Knock.xml',
-          :sha256 => '45d49b2a76c79ef7838798a975f9322135c7d3ffaf9af13c271fa597aee8b2f1'
+          :sha256 => '4adcd10cf5e9eba273b4c751f0fcb922c030d3235ee677e87547838ad73d3e55'
   name 'Knock'
   homepage 'http://knocktounlock.com'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
