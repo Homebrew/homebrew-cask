a4964e1bebf9f27495ff0802f2affe44ced3fff5
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 15:09:45 +0000

    updated texturepacker (4.0.1)

--- Casks/texturepacker.rb
+++ Casks/texturepacker.rb
@@ -4,7 +4,7 @@ cask 'texturepacker' do
 
   url "https://www.codeandweb.com/download/texturepacker/#{version}/TexturePacker-#{version}-uni.dmg"
   appcast 'https://www.codeandweb.com/releases/TexturePacker/appcast-mac-release.xml',
-          :sha256 => 'c4b65445ed1c68f6c024cb672f1915b156402c213e514595942c0c6096d8cd65'
+          :sha256 => '4971b06b26f5ba7d54b8fbf633ac614f614969b9eb2d549078b37e0193e8740b'
   name 'TexturePacker'
   homepage 'https://www.codeandweb.com/texturepacker'
   license :freemium
