c4c21ebadadbc4ebb0a3bcfb16e6d344cd144ce7
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 16:36:19 +0000

    updated xmplify (1.7.5)

--- Casks/xmplify.rb
+++ Casks/xmplify.rb
@@ -1,10 +1,10 @@
 cask 'xmplify' do
-  version :latest
-  sha256 :no_check
+  version '1.7.5'
+  sha256 '924e57fb83241b95950a1f6fae94341004b54c58fdb339aa137f26b1248176ee'
 
-  url 'http://xmplifyapp.com/releases/Xmplify.dmg'
+  url "http://xmplifyapp.com/releases/Xmplify-#{version}.dmg"
   appcast 'http://xmplifyapp.com/appcast.xml',
-          :sha256 => 'c07056253091ae2c4955af290992f23042c356c3ac33fc23e86604f07349c83d'
+          :sha256 => 'ebf9790e9d752b0decf447b8ae74b00c8709f15fde0df062353cb849094d4b57'
   name 'Xmplify'
   homepage 'http://xmplifyapp.com/'
   license :commercial
