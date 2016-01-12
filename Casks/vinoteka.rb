52aba806e94771d99c08cc1503eb257702c55796
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:22:26 +0000

    vinoteka.rb: appcast sha256 without pubDate

--- Casks/vinoteka.rb
+++ Casks/vinoteka.rb
@@ -4,7 +4,7 @@ cask 'vinoteka' do
 
   url 'http://download.vinotekasoft.com/Vinoteka.zip'
   appcast 'http://download.vinotekasoft.com/vinoteka_update.xml',
-          :sha256 => '648e28d49c9b38e04f623f44b4ee27d4c872e5e8302bc333c733bbd258dfde44'
+          :sha256 => '2a8645eec168b391ae9b09837dc1c92ea61b527518f43b6b09c01b9c032ed0bc'
   name 'Vinoteka'
   homepage 'http://www.vinotekasoft.com/'
   license :commercial
