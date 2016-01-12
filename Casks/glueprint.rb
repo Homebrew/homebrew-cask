ab78eb4b82f18ba2304c43a8ea8eac99e765c64d
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:01:20 +0000

    glueprint.rb: appcast sha256 without pubDate

--- Casks/glueprint.rb
+++ Casks/glueprint.rb
@@ -4,7 +4,7 @@ cask 'glueprint' do
 
   url "http://glueprintapp.com/static/download/GluePrint-#{version}.app.zip"
   appcast 'http://glueprint-update.s3-website-us-east-1.amazonaws.com/appcast.xml',
-          :sha256 => 'd356a0f32007b1fbd5238e1c5cff91b61f9f847ba4efe166badf468c5d7d76d1'
+          :sha256 => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   name 'GluePrint'
   homepage 'http://glueprintapp.com/'
   license :gratis
