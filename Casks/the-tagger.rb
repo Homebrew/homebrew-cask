f3109109bde99f7a226644ccd5091ed8d44ab5d7
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 15:12:24 +0000

    updated the-tagger (1.6.1)

--- Casks/the-tagger.rb
+++ Casks/the-tagger.rb
@@ -1,10 +1,10 @@
 cask 'the-tagger' do
-  version :latest
-  sha256 :no_check
+  version '1.6.1'
+  sha256 '51ef8424ae4f6c7e53159e665d87067ce43dd9be3cfc2e7c18efd0f717375b9c'
 
   url 'http://deadbeatsw.com/thetagger/TheTaggerLatest.zip'
   appcast 'http://deadbeatsw.com/thetagger/appcast.xml',
-          :sha256 => 'a705c4355bc47841b153faa39280aa42dcac99d919e82417ce409cdb2a70db92'
+          :sha256 => '41ea65403b2a02ddda76e9e43bb0bdc510113d6988572c2cb83860f9ba747291'
   name 'The Tagger'
   homepage 'http://deadbeatsw.com/thetagger/'
   license :commercial
