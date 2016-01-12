09e49221803487e9726a238deb62c833b7eddff3
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:13:12 +0000

    pixlr.rb: appcast sha256 without pubDate

--- Casks/pixlr.rb
+++ Casks/pixlr.rb
@@ -4,7 +4,7 @@ cask 'pixlr' do
 
   url "https://cdn2.pixlr.com/mac/Autodesk_Pixlr_v#{version}_Mac_Installer.dmg"
   appcast 'https://updatefeed.pixlr.com/pixlr-for-mac-appfeed.xml',
-          :sha256 => '46d01575b9fd926b62093e9d5614544428f49a5f1013217b92c29b2dbed78966'
+          :sha256 => '82cada86fe1b784868a714ea6085f295c45deb7c11299adc6acb4cf2372547de'
   name 'Autodesk Pixlr'
   homepage 'https://pixlr.com'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
