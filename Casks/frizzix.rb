3c7d83b4c8bd80107a94ce1353cbcad61b7a4491
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:00:51 +0000

    frizzix.rb: appcast sha256 without pubDate

--- Casks/frizzix.rb
+++ Casks/frizzix.rb
@@ -4,7 +4,7 @@ cask 'frizzix' do
 
   url "http://frizzix.de/downloads/Frizzix_#{version}.dmg"
   appcast 'http://frizzix.de/downloads/FrizzixUpdate.xml',
-          :sha256 => '424843fa7b6b19b0ca3ba17ff4042e751a56cbef122b6dc008b9cb903164606d'
+          :sha256 => 'b8d70191fa62dc97d7d78348d5771c9b9451ac04c6c7eb1c089fce14877cfdd1'
   name 'Frizzix'
   homepage 'http://mac.frizzix.de/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
