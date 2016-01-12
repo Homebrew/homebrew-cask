1932fa6d6c16af3a2a28d5b7809f8e1264740b65
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:07:13 +0000

    limechat.rb: appcast sha256 without pubDate

--- Casks/limechat.rb
+++ Casks/limechat.rb
@@ -10,7 +10,7 @@ cask 'limechat' do
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/limechat/limechat/LimeChat_#{version}.tbz"
   appcast 'http://limechat.net/mac/appcast.xml',
-          :sha256 => '32dd2f6795c4f19940a8bb8a093b5b297167c28a4b6712cb94d6de14f1002e4f'
+          :sha256 => '90b1b494b411d180994bbfb9eb6b90a7563f18a4f9b6d0874c9a37689a4d73e6'
   name 'LimeChat'
   homepage 'http://limechat.net/mac/'
   license :gpl
