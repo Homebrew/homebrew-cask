d922910eff605af231d6612b74531d427e1761f0
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 15:03:11 +0000

    updated tagspaces (2.0.4)

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,14 +1,14 @@
 cask 'tagspaces' do
-  version '2.0.1'
+  version '2.0.4'
 
   if Hardware::CPU.is_32_bit?
     sha256 'be88d60769da6af846f8d84037bcbf4917dbef3735c460cfc8727c8c17ace488'
     url "http://www.tagspaces.org/downloads/tagspaces-#{version}-osx32.zip"
   else
-    sha256 'c0dc6600fe6cdf5ea55972b6d6dd6b0ae922450f0db1e3d834e0e6aee831327b'
+    sha256 '1c28f143e28b167a586ed815ca4b87c3e65f237d8a5dd7ec2946d60afad15577'
     url "http://www.tagspaces.org/downloads/tagspaces-#{version}-osx64.zip"
     appcast 'https://github.com/tagspaces/tagspaces/releases.atom',
-            :sha256 => '209777f2eb0715d375ca8e97f21ffaa48f51055e3b59b2dab19c60e5b040a163'
+            :sha256 => 'eedc9c531f539694698591f7befc49737f84fe2245d9cc3cf0edc23976b9c80a'
   end
 
   name 'TagSpaces'
