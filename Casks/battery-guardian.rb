874c6d9dc17d50d012707d71c413969669ee295a
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:54:18 +0000

    battery-guardian.rb: appcast sha256 without pubDate

--- Casks/battery-guardian.rb
+++ Casks/battery-guardian.rb
@@ -4,7 +4,7 @@ cask 'battery-guardian' do
 
   url 'https://www.dssw.co.uk/batteryguardian/dsswbatteryguardian.dmg'
   appcast 'https://version.dssw.co.uk/batteryguardian/standard',
-          :sha256 => '6e86f2691ab9d2c1486b54e60b9528fb4f85c689e86a150300233d8485ce3306'
+          :sha256 => 'a55927bf0b9bd76de157564f1cae7412c16a0d94433d14a6abca983528543c23'
   name 'Battery Guardian'
   homepage 'https://www.dssw.co.uk/batteryguardian'
   license :gratis
