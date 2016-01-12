7f3c21c1b5b2f3b06508421c323bd44918eea820
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 21:59:26 +0000

    eve.rb: appcast sha256 without pubDate

--- Casks/eve.rb
+++ Casks/eve.rb
@@ -5,7 +5,7 @@ cask 'eve' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://s3.amazonaws.com/hotkeyeve/Downloads/EVE-latest.zip'
   appcast 'https://s3.amazonaws.com/hotkeyeve/hotkeyEVEappcast.xml',
-          :sha256 => '771a6b166a2752502899a4c5117b08aae56b36a72122a34dbb304ab7d7c8b0e5'
+          :sha256 => '27d2ee414a2155ae538724f43e251515cc0ae1b97662faa4aabbc0f36277fabd'
   name 'EVE'
   homepage 'http://www.hotkey-eve.com/'
   license :gratis
