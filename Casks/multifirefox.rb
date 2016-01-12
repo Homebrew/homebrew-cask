f45fe64ad0c1cc182f5c58de17e76f09f288fb4b
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:10:22 +0000

    multifirefox.rb: appcast sha256 without pubDate

--- Casks/multifirefox.rb
+++ Casks/multifirefox.rb
@@ -5,7 +5,7 @@ cask 'multifirefox' do
   # amazonaws.com is the official download host per the vendor homepage
   url 'https://mff.s3.amazonaws.com/MFF2_latest.dmg'
   appcast 'https://s3.amazonaws.com/mff_sparkle/MultiFirefoxAppcast2.xml',
-          :sha256 => 'adf4ecc69fb5ee5e81512db866e5955a0879eaa1924318c66c008132a7220ba8'
+          :sha256 => '33df7b0b7df0e738860e6ed36fac5cb6499b590c35c80ae0c94efd0a34e90717'
   name 'MultiFirefox'
   homepage 'http://davemartorana.com/multifirefox'
   license :mit
