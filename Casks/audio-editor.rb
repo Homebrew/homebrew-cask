c9980afc34adec62a393980c606190f0c0ed62ec
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:21:55 +0000

    updated audio-editor (1.0)

--- Casks/audio-editor.rb
+++ Casks/audio-editor.rb
@@ -1,6 +1,6 @@
 cask 'audio-editor' do
-  version :latest
-  sha256 :no_check
+  version '1.0'
+  sha256 '35beff4bbbfbce77c2aa1f2f4c044f8845a0a28cfac4c8f95409022348e31c25'
 
   url 'http://www.macsome.com/AudioEditor.dmg'
   appcast 'http://www.macsome.com/audio-editor-mac/su_feed.xml',
