6ca5b08637ccc365017d8e082437ad82bde55faf
Victor Popkov <victor@popkov.me>
2016-01-13 17:33:31 +0200

    kitematic.rb: upgrade to v0.9.5.1

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,11 +1,11 @@
 cask 'kitematic' do
-  version '0.9.5'
-  sha256 'a8b078cea9f77129fee63df810bc80faf3f56f4c1e6b724397c06ef77fae5e19'
+  version '0.9.5.1'
+  sha256 '70974b164f74d7dbaa393526a736066820379c07f3a91fe98fa042ad4cc91ee4'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
   appcast 'https://github.com/kitematic/kitematic/releases.atom',
-          :sha256 => '44025e7a855b6afc4c645971e90cd49b576aa4772ce1de8bec35f7b092596594'
+          :sha256 => 'bc2e4c8b498952d720949fc2d7c6d44ca641a9d6bdf6f2e88827817f5c0595a7'
   name 'Kitematic'
   homepage 'https://kitematic.com/'
   license :apache
