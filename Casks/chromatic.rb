b614b3219a20e99171b403f0dc1eedeecf5b0956
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:28:47 +0000

    updated chromatic (0.2.4)

--- Casks/chromatic.rb
+++ Casks/chromatic.rb
@@ -1,10 +1,10 @@
 cask 'chromatic' do
-  version :latest
-  sha256 :no_check
+  version '0.2.4'
+  sha256 '3185ff51209c67fb25555642cb0b88da83ece27f378e4025284368fd8f54fe36'
 
   url 'https://download.mrgeckosmedia.com/Chromatic.zip'
-  appcast 'https://mrgeckosmedia.com/applications/appcast/Chromatic',
-          :checkpoint => '375b5711b9e11f71af5e3f81f2fbb166541874f304a94b2f57db447e45e1837b'
+  appcast 'https://mrgeckosmedia.com/applications/releasenotes/Chromatic',
+          :checkpoint => '36c8f273a93367b4fe2284c0d279dbca49009584b368bbf35cac0250234b1afe'
   name 'Chromatic'
   homepage 'https://mrgeckosmedia.com/applications/info/Chromatic'
   license :isc
