84837958e16ba9713ee11f16aa089fa59a717ba3
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:16:45 +0000

    updated adventure (2.1)

--- Casks/adventure.rb
+++ Casks/adventure.rb
@@ -1,6 +1,6 @@
 cask 'adventure' do
-  version :latest
-  sha256 :no_check
+  version '2.1'
+  sha256 '743c6912a29cb225a4e44bdf59f65286fd00ada32e7747e83c8379bb532f9f5d'
 
   url 'http://www.lobotomo.com/products/downloads/Adventure.dmg'
   appcast 'http://www.lobotomo.com/products/Adventure/profileInfo.php',
