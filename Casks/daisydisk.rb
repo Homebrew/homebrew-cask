1d5eb8f58a9e13da7b35f32b809585b9dc152d84
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:33:24 +0000

    updated daisydisk (latest)

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -1,5 +1,5 @@
 cask 'daisydisk' do
-  if MacOS.release == :snow_leopard
+  if MacOS.release <= :snow_leopard
     version '2.1.2'
     sha256 'd0a606dee19e524d6fa7b79fd48b3b9865123ca4126fb8805f8e96c317b57b31'
     url "https://www.daisydiskapp.com/downloads/DaisyDisk_#{version}.dmg"
@@ -9,8 +9,6 @@ cask 'daisydisk' do
     url 'https://www.daisydiskapp.com/downloads/DaisyDisk.zip'
   end
 
-  appcast 'https://www.daisydiskapp.com/downloads/appcastFeed.php',
-          :checkpoint => '6d4ff00bbcb357beeb22859d931c098aad448192f0ff93376a31cbb23e2d294f'
   name 'DaisyDisk'
   homepage 'https://www.daisydiskapp.com'
   license :freemium
