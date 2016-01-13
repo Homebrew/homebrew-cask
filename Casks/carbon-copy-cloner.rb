18fcc064dcf411d592449b566698494f585f62f3
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:27:21 +0000

    updated carbon-copy-cloner (3.4.7)

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -1,10 +1,10 @@
 cask 'carbon-copy-cloner' do
-  version :latest
-  sha256 :no_check
+  version '3.4.7'
+  sha256 '4ea19d1c9111fb67b0f7567844a413794bd2d3b6a908a8921cabd10f21101247'
 
-  url 'https://www.bombich.com/software/download_ccc.php?v=latest&l=alternate'
-  appcast 'https://www.bombich.com/software/updates/ccc.php',
-          :checkpoint => '06cfc29f8c4b697139c437a95092890dc87cbab6ef3e9bc4e1561f70f7e15af4'
+  url "http://www.bombich.com/software/download_ccc_update.php?v=#{version}"
+  appcast 'https://bombich.com/software/updates/ccc.php',
+          :checkpoint => 'eec027f0d0aa576e292b4fe57441f1821409c6295330787dd82533b0d483cb26'
   name 'Carbon Copy Cloner'
   homepage 'https://bombich.com/'
   license :commercial
