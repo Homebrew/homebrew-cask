cf7cd789c201a3fcc624a01c49bc08ec126c97ca
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 21:35:53 +0000

    updated soundcleod (0.18)

--- Casks/soundcleod.rb
+++ Casks/soundcleod.rb
@@ -1,10 +1,10 @@
 cask 'soundcleod' do
-  version :latest
-  sha256 :no_check
+  version '0.18'
+  sha256 '467761e18935787f2b1b89179236a31162521975b5104dfecb0869aec15403c9'
 
   url 'https://github.com/salomvary/soundcleod/raw/master/dist/SoundCleod.dmg'
-  appcast 'https://raw.github.com/salomvary/soundcleod/master/appcast.xml',
-          :checkpoint => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b856'
+  appcast 'https://github.com/salomvary/soundcleod/releases.atom',
+          :checkpoint => 'ef94d659ad0d1367225799c76d689f2f7231e9f9b05bdb1097052a8c6ab91f7f'
   name 'SoundCleod'
   homepage 'https://salomvary.github.io/soundcleod/'
   license :mit
