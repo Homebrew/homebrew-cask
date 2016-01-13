f0e22e4f00a380defb23f0e557f0c92e29ca71d1
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 21:31:11 +0000

    updated sequential (2.1.2)

--- Casks/sequential.rb
+++ Casks/sequential.rb
@@ -3,8 +3,8 @@ cask 'sequential' do
   sha256 '7a9247e8623da5b6c74b65ad6d9e0be7667b832816134393e871e584c00eee64'
 
   url "http://sequentialx.com/Sequential#{version}.zip"
-  appcast 'http://www.sequentialx.com/sequential.xml',
-          :checkpoint => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b856'
+  appcast 'http://sequentialx.com/sequential.xml',
+          :checkpoint => '1fb8416920c90af57525b72c518fd89b0e6e29775d576720adf7535268ea510d'
   name 'Sequential'
   homepage 'http://sequentialx.com'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
