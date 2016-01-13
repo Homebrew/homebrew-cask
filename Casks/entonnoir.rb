92c08d443f5cf353551bf2c33d79bae0e4a62c30
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:35:34 +0000

    updated entonnoir (1.01)

--- Casks/entonnoir.rb
+++ Casks/entonnoir.rb
@@ -1,6 +1,6 @@
 cask 'entonnoir' do
-  version :latest
-  sha256 :no_check
+  version '1.01'
+  sha256 '99c69e443c4f710ef1322f3a872b8e57cb31bc7219e353cac5a57f3334cb1771'
 
   url 'http://tools.chocoflop.com/downloads/Entonnoir_latest.php'
   appcast 'http://tools.chocoflop.com/appcasts/entonappcast.xml',
