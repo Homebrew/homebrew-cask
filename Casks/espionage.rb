7764ede713500b502ca25c3be383c40062d79a38
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:36:35 +0000

    updated espionage (3.62)

--- Casks/espionage.rb
+++ Casks/espionage.rb
@@ -1,9 +1,9 @@
 cask 'espionage' do
-  version :latest
-  sha256 :no_check
+  version '3.62'
+  sha256 '4e6ddb3a51adca6a81a42d56d855eea35ca15590bc41c92d41067d5537eaf129'
 
   url 'https://www.espionageapp.com/Espionage.dmg'
-  appcast 'https://updates.taoeffect.com/espionage3/appcast.xml',
+  appcast "https://updates.taoeffect.com/espionage#{version.major}/appcast.xml",
           :checkpoint => '52e10849eb03fa552293aca733797366418af5fb28eb7da166d557f3fec41abf'
   name 'Espionage'
   homepage 'https://www.espionageapp.com/'
