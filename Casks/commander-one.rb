6f3f115cc219640ca1d0c288f2a1f50f5432290b
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:31:07 +0000

    updated commander-one (1.2)

--- Casks/commander-one.rb
+++ Casks/commander-one.rb
@@ -1,6 +1,6 @@
 cask 'commander-one' do
-  version :latest
-  sha256 :no_check
+  version '1.2'
+  sha256 'af0e92a611e6ed2e091ff44a20bc8dee23f09de5efa66880ff58823e51959225'
 
   url 'http://mac.eltima.com/download/commander.dmg'
   appcast 'http://www.eltima.com/download/commander-update/settings.xml',
