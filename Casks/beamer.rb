945d75e17c91b3ab6d6666cbbdda403e1dc06f1f
Victor Popkov <victor@popkov.me>
2016-01-13 17:28:21 +0200

    beamer.rb: upgrade to v3.0.4

--- Casks/beamer.rb
+++ Casks/beamer.rb
@@ -1,10 +1,10 @@
 cask 'beamer' do
-  version '3.0.3'
-  sha256 '183a28b096415825e9e22816b25cdfbe4b6a55d53d0e1da89cae8d3a55c26f17'
+  version '3.0.4'
+  sha256 '7620e46cff5e55241f1fc37d60ba82a5599ac172a494d1f5236176f82ddb69f2'
 
   url "https://beamer-app.com/resources/downloads/Beamer-#{version}.zip"
   appcast 'https://beamer-app.com/beamer3-appcast.xml',
-          :sha256 => '3b362b71ab9eaec3f9b0b347dece549b1d3c26ae39a064ce687c9fba3c091f83'
+          :sha256 => '806668a0e14c04a737b00da05d80036fdce4fae3c4b7a80a587f2fc211f58174'
   name 'Beamer'
   homepage 'https://beamer-app.com/'
   license :commercial
