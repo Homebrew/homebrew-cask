c146cf1685619b1ad3d674eff0e003556143fbd6
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 22:14:06 +0000

    updated 33-rpm (1.1.8)

--- Casks/33-rpm.rb
+++ Casks/33-rpm.rb
@@ -1,6 +1,6 @@
 cask '33-rpm' do
-  version :latest
-  sha256 :no_check
+  version '1.1.8'
+  sha256 '44937d70c41c20dd72ddfef6266c288beac66eeb01914cdc7901ae8111a4aa90'
 
   url 'http://www.edenwaith.com/downloads/33rpm.dmg'
   appcast 'http://www.edenwaith.com/xml/33rpm.xml',
