4670084ed140ebc59950952b3b7eaeccfd865d9f
Vítor Galvão <info@vitorgalvao.com>
2016-01-13 21:20:45 +0000

    updated camtasia (latest)

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -3,8 +3,6 @@ cask 'camtasia' do
   sha256 :no_check
 
   url 'http://download.techsmith.com/camtasiamac/enu/Camtasia.dmg'
-  appcast 'https://techsmithredirect.appspot.com/cmac?target=sparkleappcast&product=camtasiamac&lang=enu&ver=2.7.1&os=mac&code=none',
-          :checkpoint => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b856'
   name 'Camtasia'
   homepage 'https://www.techsmith.com/camtasia.html'
   license :commercial
