52fa938bd0ec4deb154f9ca5a2e0db074af3d583
raffaele messuti <raffaele@atomotic.com>
2016-01-14 09:20:13 +0100

    update josm, version 9329

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,5 +1,5 @@
 cask 'josm' do
-  version '9060'
+  version '9329'
   sha256 '4f4c3ae8a7cc1631d26698b7212d9d1acb179949edc4cb027a637747490309f2'
 
   url "https://josm.openstreetmap.de/download/macosx/josm-macosx-#{version}.zip"
