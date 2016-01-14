d2cca6d58670653ada27d2b9d85da7600afdde53
Victor Popkov <victor@popkov.me>
2016-01-14 21:54:54 +0200

    updated fantastical (2.1.5)

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,10 +1,10 @@
 cask 'fantastical' do
-  version '2.1.4'
-  sha256 '7227933605ca7ef5618d8402ddff54cf19b83737a37d63c31d8c9ced2a6e76f4'
+  version '2.1.5'
+  sha256 '816e780ead978f15fd1bb762c80f8c8a5abf9551c121ba502f61a528d962fb60'
 
   url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast 'https://flexibits.com/fantastical/appcast2.php',
-          :checkpoint => 'fb313838d6d63df09c48977ed263b7f293bcfc1084b00f823f38bb7244905c92'
+          :checkpoint => '891edbc3657ab7f08d356377092aa01beef808c6e7d5a19a99ef3d471786908a'
   name 'Fantastical'
   homepage 'https://flexibits.com/fantastical'
   license :freemium
