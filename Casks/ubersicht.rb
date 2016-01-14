6344a4e9c91bf74da1b898619c1033b86229cc73
Victor Popkov <victor@popkov.me>
2016-01-14 22:21:45 +0200

    ubersicht.rb: upgrade to v0.7.23

--- Casks/ubersicht.rb
+++ Casks/ubersicht.rb
@@ -1,10 +1,10 @@
 cask 'ubersicht' do
-  version '0.7'
-  sha256 '3ac9cb5b8e488029a50f216ff39f370f75b71a27d46c5d5b0a7df49732f3d334'
+  version '0.7.23'
+  sha256 '9d0caa3cfe7f58c091f4398fe7180eb789aa1df1d22085703163a7d97f0c0327'
 
   url "http://tracesof.net/uebersicht/releases/Uebersicht-#{version}.app.zip"
   appcast 'http://tracesof.net/uebersicht/updates.xml.rss',
-          :checkpoint => '07ec6b517d4be5bd4cb83cb0e4cd7b486feddcc4720254077bce026df34ef0b6'
+          :checkpoint => '3b2a0cb984b7a7e6d81f34e01b72b9df17479015d6b7ed82c33cadbf67880adb'
   name 'Übersicht'
   homepage 'http://tracesof.net/uebersicht'
   license :gpl
