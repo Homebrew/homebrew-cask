62872e4aa0a37b3df4fa2a8abd9e8e115bb21f5e
Vítor Galvão <info@vitorgalvao.com>
2016-01-15 01:14:07 +0000

    updated fivedetails-flow (1.7.2)

--- Casks/fivedetails-flow.rb
+++ Casks/fivedetails-flow.rb
@@ -1,6 +1,6 @@
 cask 'fivedetails-flow' do
-  version :latest
-  sha256 :no_check
+  version '1.7.2'
+  sha256 '8337d4f2152d7b00bb18cde16475c47a3a2608a7d773ee7ce0dcc425ccfbc5b8'
 
   url 'http://fivedetails.com/flow/download'
   appcast 'http://extendmac.com/flow/updates/update.php',
