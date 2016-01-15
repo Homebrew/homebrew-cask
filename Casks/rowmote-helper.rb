92021185f00ff05dacf2ffad25791aa47c646d64
Tommy Sparber <tommy@sparber.net>
2016-01-15 19:26:42 +1100

    Updated rowmote-helper to 4.1.5

--- Casks/rowmote-helper.rb
+++ Casks/rowmote-helper.rb
@@ -1,6 +1,6 @@
 cask 'rowmote-helper' do
-  version '4.1.4'
-  sha256 '7f58d3596d2ffd6310c5cdb9fef89d0f55e60e45deb3fade04990ed73e9c0bc9'
+  version '4.1.5'
+  sha256 'a9fb0fbc0adf90ff45a095f3d097910cc4d990105a960a6d1c567fde519b14e9'
 
   url "http://regularrateandrhythm.com/rowmote-pro/rh/rowmote-helper-#{version}.zip"
   appcast 'http://www.regularrateandrhythm.com/apps/rowmote-pro/rowmote-appcast.xml',
@@ -8,6 +8,8 @@ cask 'rowmote-helper' do
   name 'Rowmote Helper'
   homepage 'http://regularrateandrhythm.com/apps/rowmote-pro/'
   license :commercial
+
+  auto_updates true
 
   app 'Rowmote Helper.app'
 end
