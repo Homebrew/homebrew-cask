060f2b1fdc13d4fa491b7272b7db4695f131c6c1
adityadalal924 <github@adityadalal.com>
2016-01-15 16:55:45 -0500

    updated balsamiq-mockups (3.3.6)

--- Casks/balsamiq-mockups.rb
+++ Casks/balsamiq-mockups.rb
@@ -1,6 +1,6 @@
 cask 'balsamiq-mockups' do
-  version '3.3.5'
-  sha256 '67cded025ca358b60e8a31c8efe62ed4c2009fedbea86e4038a0eeb157bdf2ca'
+  version '3.3.6'
+  sha256 '3b48a23dcca8cf3e2b1b73973ae79f29564c78534bc91787e2720b203cc12430'
 
   url "https://builds.balsamiq.com/mockups-desktop/Balsamiq_Mockups_#{version}.dmg"
   name 'Balsamiq Mockups'
