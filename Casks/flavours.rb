93b20c0d8211b7a6ebaf57e7181a3df0cb3b8ba5
Vítor Galvão <info@vitorgalvao.com>
2016-01-15 01:18:15 +0000

    updated flavours (1.1.16)

--- Casks/flavours.rb
+++ Casks/flavours.rb
@@ -1,8 +1,8 @@
 cask 'flavours' do
-  version :latest
-  sha256 :no_check
+  version '1.1.16'
+  sha256 '54b9b06733ee9f58bf767ad101e3822b15884ac94cdfc37aa895381b6b8258b0'
 
-  url 'http://flavours-updates.interacto.net/Flavours.dmg'
+  url "http://flavours-updates.interacto.net/Flavours_#{version}.dmg"
   appcast 'http://flavours-updates.interacto.net/appcast.xml',
           :checkpoint => '89d3f77f9add647db73a55227497e09f585b556a93d5545de361a95bb40f9a47'
   name 'Flavours'
