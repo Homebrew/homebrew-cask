82b6335a35fd28d1fbe43bb3940efa604737c1ec
Vítor Galvão <info@vitorgalvao.com>
2016-01-15 01:10:54 +0000

    updated evom (1.0.2)

--- Casks/evom.rb
+++ Casks/evom.rb
@@ -1,6 +1,6 @@
 cask 'evom' do
-  version :latest
-  sha256 :no_check
+  version '1.0.2'
+  sha256 '5ac95d871f2cbe5918bb7a353161a0b58449b3680ea0dc69e2bd704c1ac05039'
 
   url 'https://files.thelittleappfactory.com/evom/Evom.zip'
   appcast 'https://files.thelittleappfactory.com/evom/appcast.xml',
