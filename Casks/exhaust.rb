76bf58704ee540dd8ca2c56f79b30c976814277f
Vítor Galvão <info@vitorgalvao.com>
2016-01-15 01:11:45 +0000

    updated exhaust (0.3)

--- Casks/exhaust.rb
+++ Casks/exhaust.rb
@@ -1,6 +1,6 @@
 cask 'exhaust' do
-  version :latest
-  sha256 :no_check
+  version '0.3'
+  sha256 '0276c4633c0ab31a16e9f5bd8b55cef4e3d5d880e98b1c29bbe53a37877adc77'
 
   url 'https://download.mrgeckosmedia.com/Exhaust.zip'
   appcast 'https://mrgeckosmedia.com/applications/appcast/Exhaust',
