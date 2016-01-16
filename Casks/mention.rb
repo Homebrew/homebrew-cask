48955b148c07921f8a44c4c5d460a73da8d60201
Vítor Galvão <info@vitorgalvao.com>
2016-01-16 19:10:12 +0000

    updated mention (2.0.14)

--- Casks/mention.rb
+++ Casks/mention.rb
@@ -1,6 +1,6 @@
 cask 'mention' do
-  version :latest
-  sha256 :no_check
+  version '2.0.14'
+  sha256 'bf9c951efb3a5b6a08e52769f5387e322138465ca6b9deac5305babc25be6521'
 
   url 'https://en.mention.com/downloads/mac/mention.dmg'
   appcast 'https://web.mention.com/uploads/appcast-mention.xml',
