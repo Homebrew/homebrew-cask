8b1e8155dff209721913aa72f3411cb84e0a9382
Vítor Galvão <info@vitorgalvao.com>
2016-01-16 12:50:16 +0000

    updated iphone-backup-extractor (1.2.4)

--- Casks/iphone-backup-extractor.rb
+++ Casks/iphone-backup-extractor.rb
@@ -1,6 +1,6 @@
 cask 'iphone-backup-extractor' do
-  version :latest
-  sha256 :no_check
+  version '1.2.4'
+  sha256 '23243aaf390e20729c39aa449cc4c6fa45892ece2685483c40f88d68fbbb73d5'
 
   url 'http://supercrazyawesome.com/downloads/iPhone%20Backup%20Extractor.app.zip'
   appcast 'http://supercrazyawesome.com/sparkle.xml',
