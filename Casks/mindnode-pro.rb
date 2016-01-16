dcc07164c1c54287e04bc8d6f8413746c49f8358
Vítor Galvão <info@vitorgalvao.com>
2016-01-16 19:13:01 +0000

    updated mindnode-pro (1.11.4)

--- Casks/mindnode-pro.rb
+++ Casks/mindnode-pro.rb
@@ -1,10 +1,10 @@
 cask 'mindnode-pro' do
-  version :latest
-  sha256 :no_check
+  version '1.11.4'
+  sha256 'db8b1f6266f343c3ad85d6733cdce00138ecd044200073a5616d040bd594794a'
 
   url 'https://www.mindnode.com/download/MindNodePro.zip'
   appcast 'https://www.mindnode.com/softwareupdate/mindnodepro.xml',
-          :checkpoint => '3de4f9ae0f4f5879d1c03d9cc90164ae7799b534c368b0e5af0aecefe04b9ed9'
+          :checkpoint => 'appcast is probably incorrect, as a non-200 (OK) HTTP response code was returned (301)'
   name 'MindNode Pro'
   homepage 'https://mindnode.com/'
   license :commercial
