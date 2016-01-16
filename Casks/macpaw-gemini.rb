b0745c34a480974bf37d2b722709fa235a368e25
Vítor Galvão <info@vitorgalvao.com>
2016-01-16 19:08:08 +0000

    updated macpaw-gemini (1.5.14,1448360907)

--- Casks/macpaw-gemini.rb
+++ Casks/macpaw-gemini.rb
@@ -1,9 +1,9 @@
 cask 'macpaw-gemini' do
-  version :latest
-  sha256 :no_check
+  version '1.5.14,1448360907'
+  sha256 '07779ae4175a18c821bed55ab2776e403f8c50e0a94d3b44e5dae4552aa9753e'
 
-  # devmate.com is the official download host per the vendor homepage
-  url 'https://dl.devmate.com/com.macpaw.site.Gemini/MacPawGemini.dmg'
+  # devmate.com is the official download host per the appcast feed
+  url "https://dl.devmate.com/com.macpaw.site.Gemini/#{version.before_comma}/#{version.after_comma}/MacPawGemini-#{version.before_comma}.zip"
   appcast 'https://updates.devmate.com/com.macpaw.site.Gemini.xml',
           :checkpoint => 'c5e98cb5ad6b531a34850e48b6cb29611cd504e6ee3c7d970cf651c6fe571baf'
   name 'MacPaw Gemini'
