0826d9fa5ba4c16428eb681a0e8efaeb5f65bf90
Vítor Galvão <info@vitorgalvao.com>
2016-01-16 19:27:25 +0000

    updated nicecast (1.9.7)

--- Casks/nicecast.rb
+++ Casks/nicecast.rb
@@ -1,8 +1,8 @@
 cask 'nicecast' do
-  version :latest
-  sha256 :no_check
+  version '1.9.7'
+  sha256 '4bc25f67f9d6dcd832846e6c563edfe9763b8d1fc57e50961049d6889219d47d'
 
-  url 'https://rogueamoeba.com/nicecast/download/Nicecast.zip'
+  url "http://rogueamoeba.com/legacy/downloads/Nicecast-#{version.no_dots}.zip"
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Nicecast',
           :checkpoint => '8f6d50cbc369b27a014c2393eaf9f9c7fb35e5467adba4233f5aead1b8dfaeca'
   name 'Nicecast'
