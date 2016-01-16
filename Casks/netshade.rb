65861df9a254e7659e5e4bb95667cd115c689548
Vítor Galvão <info@vitorgalvao.com>
2016-01-16 19:26:15 +0000

    updated netshade (6.3.1)

--- Casks/netshade.rb
+++ Casks/netshade.rb
@@ -1,10 +1,10 @@
 cask 'netshade' do
-  version :latest
-  sha256 :no_check
+  version '6.3.1'
+  sha256 'e4cf41a28a97a58ef4c3cae97a6e5bb7e039acf740d457e0e46ae5eff8735ffb'
 
   url 'http://raynersoftware.com/downloads/NetShade.app.zip'
-  appcast 'https://secure.raynersw.com/appcast.php',
-          :checkpoint => 'e25679e34ce737098f3c565e408bc6fb54d876ca10938a03099edc46e12229ab'
+  appcast 'https://secure.raynersw.com/changelog.php?prod=ns&format=std&warnpay=0',
+          :checkpoint => '7ad23be85e4561f896bdba0743e1086d6b8f7b195ce0b7cea349cd210b0ce351'
   name 'NetShade'
   homepage 'http://raynersoftware.com/netshade/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
