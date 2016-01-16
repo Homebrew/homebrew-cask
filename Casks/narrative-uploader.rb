a06f99cc44f268f78cde5e1c06dcbb1094f967c8
Vítor Galvão <info@vitorgalvao.com>
2016-01-16 19:23:11 +0000

    updated narrative-uploader (201510131242)

--- Casks/narrative-uploader.rb
+++ Casks/narrative-uploader.rb
@@ -1,8 +1,8 @@
 cask 'narrative-uploader' do
-  version :latest
-  sha256 :no_check
+  version '201510131242'
+  sha256 '1cc4cae7a1cfe43e3e35a2d49aa51a220925298f9628b2e2f84e900a1a82943b'
 
-  url 'https://dl.getnarrative.com/appcast/installers/NarrativeUploader.dmg'
+  url "http://dl.getnarrative.com/appcast/osx/#{version}.zip"
   appcast 'https://dl.getnarrative.com/appcast/osx.xml',
           :checkpoint => 'db3fe560e55b1735cfeb457552ec724d95c75b72699bb28e2590c12b58972919'
   name 'Narrative Uploader'
