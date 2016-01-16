b465a50845a574768356bb1188db3ff3e1b0459a
adidalal <adidalal@users.noreply.github.com>
2016-01-16 15:48:51 -0500

    updated resolutionator (1.0.2)

--- Casks/resolutionator.rb
+++ Casks/resolutionator.rb
@@ -1,10 +1,10 @@
 cask 'resolutionator' do
-  version :latest
-  sha256 :no_check
+  version '1.0.2'
+  sha256 '61c5c3d527bc0718b90347bf9a123e00813920ca612a11f755a9281791edd470'
 
-  url 'http://manytricks.com/download/resolutionator'
+  url 'https://manytricks.com/download/resolutionator'
   appcast 'https://manytricks.com/resolutionator/appcast.xml',
-          :checkpoint => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b856'
+          :checkpoint => 'daf1d476cf2235d77b3f0303b5f2672cafff7b713914a50a28d68c6747fece2f'
   name 'Resolutionator'
   homepage 'https://manytricks.com/resolutionator/'
   license :gratis
