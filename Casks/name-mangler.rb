bcb6626ba1c1de1013838d35a839c120672c21c6
adidalal <adidalal@users.noreply.github.com>
2016-01-16 15:47:37 -0500

    updated name-mangler (3.3.4)

--- Casks/name-mangler.rb
+++ Casks/name-mangler.rb
@@ -1,10 +1,10 @@
 cask 'name-mangler' do
-  version :latest
-  sha256 :no_check
+  version '3.3.4'
+  sha256 '7cef3b35a15f6a33a227870342adff64a4aedcd842a5de9d8c9ecfde8217a068'
 
-  url 'http://manytricks.com/download/namemangler'
+  url 'https://manytricks.com/download/namemangler'
   appcast 'https://manytricks.com/namemangler/appcast.xml',
-          :checkpoint => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b856'
+          :checkpoint => '07b3d34b96bfbc008a7d1df5314abf97b404cf5d92a379ec13269585c7a7d30c'
   name 'Name Mangler'
   homepage 'https://manytricks.com/namemangler/'
   license :freemium
