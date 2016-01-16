41a9ce5097f22632c66b9367b30a491090ea8212
Vítor Galvão <info@vitorgalvao.com>
2016-01-16 19:19:43 +0000

    updated mou (0.8.7)

--- Casks/mou.rb
+++ Casks/mou.rb
@@ -1,6 +1,6 @@
 cask 'mou' do
-  version :latest
-  sha256 :no_check
+  version '0.8.7'
+  sha256 '939a8916038287e5f69883d43738add238a8588a668be0c67079f3cecfa621dc'
 
   url 'http://25.io/mou/download/Mou.zip'
   appcast 'http://25.io/mou/up/updates.xml',
