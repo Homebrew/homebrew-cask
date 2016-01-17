f6c205204292fcf102b886884c0c4c8f1d3f422a
Vítor Galvão <info@vitorgalvao.com>
2016-01-17 22:29:22 +0000

    updated orbit (1.1.1)

--- Casks/orbit.rb
+++ Casks/orbit.rb
@@ -1,6 +1,6 @@
 cask 'orbit' do
-  version :latest
-  sha256 :no_check
+  version '1.1.1'
+  sha256 '9d62f432db392a972b0248bd41e87c95c95e3a731d5d63d479f741b71debbda5'
 
   url 'http://orbitapp.net/updates/Orbit.zip'
   appcast 'http://orbitapp.net/updates/appcast.xml',
