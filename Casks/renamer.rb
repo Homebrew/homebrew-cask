515c8bf805d1dfd68402e0a2717cc4d0b0d076d9
Vítor Galvão <info@vitorgalvao.com>
2016-01-17 22:47:19 +0000

    updated renamer (4.3.3)

--- Casks/renamer.rb
+++ Casks/renamer.rb
@@ -1,6 +1,6 @@
 cask 'renamer' do
-  version :latest
-  sha256 :no_check
+  version '4.3.3'
+  sha256 'cd4308d8555ad7e17851c45a71904b5695e2467f9ad499d64de36a9be0f522b0'
 
   # creativebe.com is the official download host per the vendor homepage
   url 'http://creativebe.com/download/renamer'
