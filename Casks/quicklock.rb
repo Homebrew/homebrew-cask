f62086bb5260eb5ad3775c99c6180f782de527a6
Vítor Galvão <info@vitorgalvao.com>
2016-01-17 22:44:18 +0000

    updated quicklock (1.5)

--- Casks/quicklock.rb
+++ Casks/quicklock.rb
@@ -1,9 +1,9 @@
 cask 'quicklock' do
-  version :latest
-  sha256 :no_check
+  version '1.5'
+  sha256 '8d3847602419040f860964bad937abddbd1b7b3ccd7f0dcd3bc0d083f16d3295'
 
-  # cl.ly is the official download host per the vendor homepage
-  url 'https://cl.ly/2C1b3Y0u3E2Y/download/QuickLock.app.zip'
+  # amazonaws.com is the official download host per the vendor homepage
+  url 'https://s3.amazonaws.com/f.cl.ly/items/1e3G0g2b1g0z201m3j1f/QuickLock.app.zip'
   appcast 'http://quicklockapp.com/appcast.xml',
           :checkpoint => '582556c374e09cc083dc8b3085787ce8323bb38ba6396ccf0327d08f35fc7a94'
   name 'QuickLock'
