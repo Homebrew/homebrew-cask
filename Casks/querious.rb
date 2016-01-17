ef3ac7688b3d4994dfa2f3589ca8e47e0f200cf3
Vítor Galvão <info@vitorgalvao.com>
2016-01-17 22:39:50 +0000

    updated querious (1.1.4)

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -1,10 +1,10 @@
 cask 'querious' do
-  version :latest
-  sha256 :no_check
+  version '1.1.4'
+  sha256 'c8553c7a81c777201cd4c155329ae0682edcac2d6fa8e21c2949da7bd7023ad2'
 
   url 'http://www.araelium.com/querious/downloads/Querious.dmg'
   appcast 'https://store.araelium.com/updates/querious',
-          :checkpoint => 'd80fc2dda3e645bb622abd2bf72e02dce2e7266859e2cc11c54812f30a5f0c54'
+          :checkpoint => 'appcast is probably incorrect, as a non-200 (OK) HTTP response code was returned (302)'
   name 'Querious'
   homepage 'http://www.araelium.com/querious/'
   license :commercial
