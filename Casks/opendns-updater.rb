e52fcaea93ea11049ce433cfcaffc26e3e785b5a
Vítor Galvão <info@vitorgalvao.com>
2016-01-17 22:28:02 +0000

    updated opendns-updater (3.0)

--- Casks/opendns-updater.rb
+++ Casks/opendns-updater.rb
@@ -1,6 +1,6 @@
 cask 'opendns-updater' do
-  version :latest
-  sha256 :no_check
+  version '3.0'
+  sha256 '64ad7b55cc8a62739815a806c077df0751ecbbc6c4a71c01792c2f71dc7fd8cc'
 
   url 'https://www.opendns.com/download/mac/'
   appcast 'https://opendnsupdate.appspot.com/macupdatecheck/ipupdater/AppCast.xml',
