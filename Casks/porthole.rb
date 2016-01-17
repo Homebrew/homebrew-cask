5f0ec2ce3584c1bd65e54057b3d044f0891e885e
Vítor Galvão <info@vitorgalvao.com>
2016-01-17 22:36:14 +0000

    updated porthole (1.7.7)

--- Casks/porthole.rb
+++ Casks/porthole.rb
@@ -1,8 +1,8 @@
 cask 'porthole' do
-  version :latest
-  sha256 :no_check
+  version '1.7.7'
+  sha256 '3a183805d58a042ddf455780c7ae204f15d7a0158fd9ba24d7e13fa92aa58451'
 
-  url 'http://getporthole.com/downloads/trial'
+  url "http://download.getporthole.com/Porthole-v#{version}.zip"
   appcast 'https://update.getporthole.com/appcast.rss',
           :checkpoint => '1eedb1650d2ddbbac106b5b65a88b1f07ee239264ed8d4f6c303637cc1b65a55'
   name 'Porthole'
