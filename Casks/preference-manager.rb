4efd7ff3ffb05da3502d97d5229e5b3b29ae2666
Vítor Galvão <info@vitorgalvao.com>
2016-01-17 22:37:41 +0000

    updated preference-manager (4.3.2)

--- Casks/preference-manager.rb
+++ Casks/preference-manager.rb
@@ -1,10 +1,10 @@
 cask 'preference-manager' do
-  version :latest
-  sha256 :no_check
+  version '4.3.2'
+  sha256 '39ba1f2e92cef296c8dc811dce7d7a972de3bda7838e9fad8c9c67e1e188dd75'
 
   url 'http://download.digitalrebellion.com/Pref_Man.dmg'
   appcast 'https://www.digitalrebellion.com/rss/appcasts/pref_man_appcast.xml',
-          :checkpoint => '5b9f5dd63a282c298d473e8fac4c5ca4c23de56b15defc2ef05350bcfb3d0c27'
+          :checkpoint => 'appcast is probably incorrect, as a non-200 (OK) HTTP response code was returned (301)'
   name 'Preference Manager'
   homepage 'https://www.digitalrebellion.com/prefman'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
