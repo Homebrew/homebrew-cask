5206de1997c3ebdb8c28e47416273aab563bbe40
Tommy Sparber <tommy@sparber.net>
2016-01-18 16:13:33 +1100

    updated things (1.4.8a)

--- Casks/things.rb
+++ Casks/things.rb
@@ -1,8 +1,10 @@
 cask 'things' do
-  version :latest
-  sha256 :no_check
+  version '1.4.8a'
+  sha256 'a489f0071b011e6459672df84a54d39f4169e414b581cfd286dd602c14c96034'
 
   url "http://culturedcode.cachefly.net/things/Things_#{version}.zip"
+  appcast 'https://downloads.culturedcode.com/things/download/Things_Updates.php',
+          checkpoint: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b856'
   name 'Things'
   homepage 'https://culturedcode.com/things/'
   license :commercial
