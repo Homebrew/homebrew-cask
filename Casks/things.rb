27dcf4929b4655361e035159148ec8fbc876288c
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 01:03:12 +0000

    updated things (latest)

--- Casks/things.rb
+++ Casks/things.rb
@@ -2,9 +2,7 @@ cask 'things' do
   version :latest
   sha256 :no_check
 
-  url 'https://culturedcode.com/things/download/'
-  appcast 'https://downloads.culturedcode.com/things/download/Things_Updates.php',
-          :checkpoint => 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b856'
+  url "http://culturedcode.cachefly.net/things/Things_#{version}.zip"
   name 'Things'
   homepage 'https://culturedcode.com/things/'
   license :commercial
