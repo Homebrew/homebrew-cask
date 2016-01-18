15695f64d3a4f745172b6c9b4db1b92adb52510f
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 01:13:42 +0000

    updated vlc-remote (3.60)

--- Casks/vlc-remote.rb
+++ Casks/vlc-remote.rb
@@ -1,8 +1,8 @@
 cask 'vlc-remote' do
-  version :latest
-  sha256 :no_check
+  version '3.60'
+  sha256 'd2a1755f056cb6f492061d52a538ad7f801859348cc9a0f5453e94209082cb64'
 
-  url 'http://hobbyistsoftware.com/Downloads/VLCRemote/latest-mac.php?cdn'
+  url "http://hobbyistsoftware.com/Downloads/VLCRemote/Versions/VLCSetup_#{version}.dmg"
   appcast 'http://hobbyistsoftware.com/Downloads/VLCRemote/vlcSetupHelperVersions.xml',
           :checkpoint => 'dbc6822df1776a697c456f13c7185db772ae288ba8c0b4062737b53584c7ac94'
   name 'VLC Remote'
