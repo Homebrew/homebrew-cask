cbfd89a69a2ef519a9a2171f81a7dd35787c69f5
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 00:21:54 +0000

    updated sketch-toolbox (0.5.1)

--- Casks/sketch-toolbox.rb
+++ Casks/sketch-toolbox.rb
@@ -1,6 +1,6 @@
 cask 'sketch-toolbox' do
-  version :latest
-  sha256 :no_check
+  version '0.5.1'
+  sha256 'e8e06d8caf87af2334655de49cc0e4a11e057ea7907e380f216476be52576065'
 
   url 'http://sketchtoolbox.com/Sketch%20Toolbox.zip'
   appcast 'http://sketchtoolbox.com/updates/appcast.xml',
