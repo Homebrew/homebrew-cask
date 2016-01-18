285101a742c78424c6c7c310c9e168b0d78d78ea
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 01:07:09 +0000

    updated tubbler (1.8.5)

--- Casks/tubbler.rb
+++ Casks/tubbler.rb
@@ -1,6 +1,6 @@
 cask 'tubbler' do
-  version :latest
-  sha256 :no_check
+  version '1.8.5'
+  sha256 'ac3696503fcab15b5bdf343854b5a8cafa799702c727fb57adb6d0dae9934981'
 
   url 'http://www.celmaro.com/files/tubbler/Tubbler.zip'
   appcast 'https://ssl.webpack.de/celmaro.com/updates/tubbler/tubbler.xml',
