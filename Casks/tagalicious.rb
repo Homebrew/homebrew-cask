fe5488d2bf85d1d1d60883cbe751818c9cdf22e5
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 00:53:55 +0000

    updated tagalicious (1.5.5)

--- Casks/tagalicious.rb
+++ Casks/tagalicious.rb
@@ -1,6 +1,6 @@
 cask 'tagalicious' do
-  version :latest
-  sha256 :no_check
+  version '1.5.5'
+  sha256 'dc3f3bbdc986f9b5b3dc698a466155273eb85483ed7708a9b2a1db898d053ba0'
 
   url 'https://files.thelittleappfactory.com/tagalicious/Tagalicious.zip'
   appcast 'https://files.thelittleappfactory.com/tagalicious/appcast.xml',
