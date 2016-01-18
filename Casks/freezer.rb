785e4898dfb77b967a571dc921b6a4f159159fec
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 14:40:50 +0000

    updated freezer (0.3)

--- Casks/freezer.rb
+++ Casks/freezer.rb
@@ -3,8 +3,8 @@ cask 'freezer' do
   sha256 'ad5882b867c1ace5255f6ac3b3ec2ebd0098c64047adcbdebc273cd0beff3ff3'
 
   url 'https://download.mrgeckosmedia.com/Freezer.zip'
-  appcast 'https://mrgeckosmedia.com/applications/appcast/Freezer',
-          checkpoint: 'bf2c35ae8fc05a99936a511abf58b75876fac648b025e1005c2135e446a4af3d'
+  appcast 'https://mrgeckosmedia.com/applications/releasenotes/Freezer',
+          checkpoint: '4f5c4a2130291c9e756110919390cb5aedc5bbd0e037160508f4eec2af0b926e'
   name 'Freezer'
   homepage 'https://mrgeckosmedia.com/applications/info/Freezer'
   license :gratis
