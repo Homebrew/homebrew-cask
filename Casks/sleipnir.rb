97607b43290256fe91fe941d432415920ff563c5
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 00:32:41 +0000

    updated sleipnir (4.5.1)

--- Casks/sleipnir.rb
+++ Casks/sleipnir.rb
@@ -1,6 +1,6 @@
 cask 'sleipnir' do
-  version :latest
-  sha256 :no_check
+  version '4.5.1'
+  sha256 'e0f59e6f3ecc59fbec4cea6b9295f7f5639769845d0a877c9cca5bb329af9327'
 
   url 'http://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
   appcast 'http://update.fenrir.co.jp/smartupdate/mac/sleipnir/appcast.xml',
