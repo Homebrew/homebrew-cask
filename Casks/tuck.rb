56981d503d6a267b3aee358e8a126772e34e1e43
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 01:07:50 +0000

    updated tuck (1.0)

--- Casks/tuck.rb
+++ Casks/tuck.rb
@@ -1,8 +1,8 @@
 cask 'tuck' do
-  version :latest
-  sha256 :no_check
+  version '1.0'
+  sha256 '7e7822e3a7d7eb932f58e770aee31f778164784e151dbc36ab997be4107804ed'
 
-  url 'https://www.irradiatedsoftware.com/downloads/Tuck.zip'
+  url "http://www.irradiatedsoftware.com/downloads/Tuck_#{version}.zip"
   appcast 'https://www.irradiatedsoftware.com/updates/profiles/tuck.php',
           :checkpoint => '9b7c8e54b14cf0c58f6c55fccdc7a553552045eb93fbfdda32311cf4c51c6084'
   name 'Tuck'
