c90d7090e603a0b7de0902524ded15cac8abe966
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 01:13:13 +0000

    updated viscosity (1.5.11)

--- Casks/viscosity.rb
+++ Casks/viscosity.rb
@@ -1,10 +1,8 @@
 cask 'viscosity' do
-  version :latest
-  sha256 :no_check
+  version '1.5.11'
+  sha256 'f235d2db655151d15976beee293bc2f6bc85025fdfe83c1b015e0d413547ba95'
 
   url 'https://www.sparklabs.com/downloads/Viscosity.dmg'
-  appcast 'http://www.viscosityvpn.com/update/viscosity.xml',
-          :checkpoint => '1dba070850ab55b9c94b8287e3f67a167c502ccf4da6a84088cb38fb0278e571'
   name 'Viscosity'
   homepage 'https://www.sparklabs.com/viscosity/'
   license :commercial
