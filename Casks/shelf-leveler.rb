6f00f54153ff2b0d2d568002bf2633017060eea6
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 00:15:07 +0000

    updated shelf-leveler (0.1)

--- Casks/shelf-leveler.rb
+++ Casks/shelf-leveler.rb
@@ -1,10 +1,8 @@
 cask 'shelf-leveler' do
-  version :latest
-  sha256 :no_check
+  version '0.1'
+  sha256 '47616c8fbbd1a2132f84ffa311cc34e1b198897cbdf7c24e4358012b12ee9989'
 
   url 'https://download.mrgeckosmedia.com/ShelfLeveler.zip'
-  appcast 'https://mrgeckosmedia.com/applications/appcast/Shelf-Leveler',
-          :checkpoint => '89acfa3ea13123d8624ef733dfa5e37d9e9e72aaa63d43f4866346b96cef4965'
   name 'Shelf Leveler'
   homepage 'https://mrgeckosmedia.com/applications/info/Shelf-Leveler'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
