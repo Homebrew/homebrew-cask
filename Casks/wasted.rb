8d9e3b0f5b30cf4bf2585399a39d7da4a2a64535
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 01:17:02 +0000

    updated wasted (3.0)

--- Casks/wasted.rb
+++ Casks/wasted.rb
@@ -1,6 +1,6 @@
 cask 'wasted' do
-  version :latest
-  sha256 :no_check
+  version '3.0'
+  sha256 '9802d9d9674145881f82ccd127ee49d64af4fd09f27a1199fe354a5fe57881ba'
 
   url 'http://wasted.werk01.de/Wasted.zip'
   appcast 'http://wasted.werk01.de/appcast.xml',
