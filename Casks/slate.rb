9afe033e549b07ebe221190b6f3f9b5bf8d024c0
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 00:30:52 +0000

    updated slate (1.0.25)

--- Casks/slate.rb
+++ Casks/slate.rb
@@ -1,6 +1,6 @@
 cask 'slate' do
-  version :latest
-  sha256 :no_check
+  version '1.0.25'
+  sha256 '428e375d5b1c64f79f1536acb309e4414c3178051c6fe0b2f01a94a0803e223f'
 
   # ninjamonkeysoftware.com is the official download host per the vendor homepage
   url 'http://slate.ninjamonkeysoftware.com/Slate.dmg'
