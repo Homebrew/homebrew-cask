f19781e5caa4eb195dae698e0f51f854489f84c5
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 14:45:22 +0000

    updated voicemac (0.4.1)

--- Casks/voicemac.rb
+++ Casks/voicemac.rb
@@ -3,6 +3,8 @@ cask 'voicemac' do
   sha256 '527b3356c11d1ef94787445829769983e9ac5c99e9cea724ad829148c78bcd7a'
 
   url 'https://download.mrgeckosmedia.com/VoiceMac.zip'
+  appcast 'https://mrgeckosmedia.com/applications/releasenotes/VoiceMac',
+          checkpoint: '1ed2eb4bc4f9580fcde443fe31ca61bf20430c7560386f3fa9250cca497ef9a9'
   name 'VoiceMac'
   homepage 'https://mrgeckosmedia.com/applications/info/VoiceMac'
   license :isc
