8607e180f9375f890971e2b40649f8b80ec3d1be
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 01:34:30 +0000

    updated youview (0.6.1)

--- Casks/youview.rb
+++ Casks/youview.rb
@@ -1,10 +1,8 @@
 cask 'youview' do
-  version :latest
-  sha256 :no_check
+  version '0.6.1'
+  sha256 '95538a324d9344ccd1eb46f4b4dbdf9cb2dc6d1259794b14e02683c35660d5dd'
 
   url 'https://download.mrgeckosmedia.com/YouView.zip'
-  appcast 'https://mrgeckosmedia.com/applications/appcast/YouView',
-          :checkpoint => '1e311e146e7a80cd0488baef8f007610552c6450fc89011811e01ecedc8ce37a'
   name 'YouView'
   homepage 'https://mrgeckosmedia.com/applications/info/YouView'
   license :oss
