40a239c86e8cc39299e18d83e211f1716010adee
Vítor Galvão <info@vitorgalvao.com>
2016-01-18 14:35:42 +0000

    updated sketch (3.4.4)

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -3,6 +3,8 @@ cask 'sketch' do
   sha256 '428f5d507f68a7b86cf5d97d3b0fc5c23297cd2048463dbf4e222375154cef79'
 
   url 'http://www.sketchapp.com/static/download/sketch.zip'
+  appcast "http://www.sketchapp.com/appcast#{version.major}.xml",
+          checkpoint: 'e415e751ba2bf73cff61fb5e3cba52982df96e3d61039102c17096145b6323ee'
   name 'Sketch'
   homepage 'http://www.sketchapp.com/'
   license :commercial
