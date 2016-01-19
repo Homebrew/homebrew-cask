f664bfe3e3a3d2b4e75ca8925b96ddd4e6c7cc19
Vladimir Dolzhenko <vladimir.dolzhenko@gmail.com>
2016-01-19 21:22:49 +0100

    updated intellij-idea (15.0.3)

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea' do
-  version '15.0.2'
-  sha256 'b1c30d9416f46045856573709035e4ab2abf472284d2fd010b40387f91286812'
+  version '15.0.3'
+  sha256 '22f8d63d9c332bc3f60c1316da3a0c70218b2508619713cb7268ff7286baf92b'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}-custom-jdk-bundled.dmg"
   name 'IntelliJ IDEA'
