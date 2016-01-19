6f88c076d2c49495b5c3b431092d0662ff6c890c
Tasuku OKUDA <tasuku.okuda@linecorp.com>
2016-01-19 19:07:54 +0900

    updated kitematic (0.9.6)

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,11 +1,11 @@
 cask 'kitematic' do
-  version '0.9.5.1'
-  sha256 '70974b164f74d7dbaa393526a736066820379c07f3a91fe98fa042ad4cc91ee4'
+  version '0.9.6'
+  sha256 '70572ca6a203acd49f357d9d6c946528a133333a95326b8342d00b5125c2b352'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
   appcast 'https://github.com/kitematic/kitematic/releases.atom',
-          checkpoint: '99a114dd6c566e0d39d4fc1fa2715588c0d405427555613f696e53d7847e12a6'
+          checkpoint: '0c57902f605889c8dc67f30fd04645a7c8f1bd5faf2241267b346199fd20570b'
   name 'Kitematic'
   homepage 'https://kitematic.com/'
   license :apache
