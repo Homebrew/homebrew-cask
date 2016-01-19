182633c00026d01bae3335842b9bb37628df4d7c
Vítor Galvão <info@vitorgalvao.com>
2016-01-19 00:52:04 +0000

    updated viscosity (1.5.11)

--- Casks/viscosity.rb
+++ Casks/viscosity.rb
@@ -3,6 +3,8 @@ cask 'viscosity' do
   sha256 'f235d2db655151d15976beee293bc2f6bc85025fdfe83c1b015e0d413547ba95'
 
   url 'https://www.sparklabs.com/downloads/Viscosity.dmg'
+  appcast 'http://swupdate.sparklabs.com/appcast/mac/release/viscosity/',
+          checkpoint: 'a7729c3828c350e03c1e27f3bf2c8f5c55bd7d2533d91b1359a28db34635036f'
   name 'Viscosity'
   homepage 'https://www.sparklabs.com/viscosity/'
   license :commercial
