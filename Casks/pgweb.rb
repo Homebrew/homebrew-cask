90e263f7bbb0425549ba86b8d7c5fa0f3f6a4537
Dan Sosedoff <dan.sosedoff@gmail.com>
2016-01-19 13:50:40 -0600

    Update pgweb to 0.9.0

--- Casks/pgweb.rb
+++ Casks/pgweb.rb
@@ -1,10 +1,10 @@
 cask 'pgweb' do
-  version '0.8.0'
-  sha256 '259ceaa19e0359af8090fa805a8fbf80633bdcfecbcbd2c472614b8b533fd20b'
+  version '0.9.0'
+  sha256 '7d077aa6d4bed88ba8dc867654369aaddc477ce4e222a8eb3495d57c0dcad9ed'
 
   url "https://github.com/sosedoff/pgweb/releases/download/v#{version}/pgweb_darwin_amd64.zip"
   appcast 'https://github.com/sosedoff/pgweb/releases.atom',
-          checkpoint: 'fbf5b1df511c3f5856afab78693a9b8536cd8560184ad80167549e6625953d01'
+          checkpoint: '108721904f9db9935331f4299f220c340b3086b4126d2330acea53e02b79ed76'
   name 'pgweb'
   homepage 'https://github.com/sosedoff/pgweb'
   license :mit
