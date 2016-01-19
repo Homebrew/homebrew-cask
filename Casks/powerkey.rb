1cefcd7118f1c21de9845b2983087ed920eccbaf
Benjamin Snow <benjamin.d.snow@gmail.com>
2016-01-19 14:36:57 -0800

    update download uri with version bump

--- Casks/powerkey.rb
+++ Casks/powerkey.rb
@@ -1,8 +1,8 @@
 cask 'powerkey' do
-  version '1.3'
+  version '1.5'
   sha256 '5f6246133953a65709252880738de7f69198ed3f23dc61bbfc062887e2313102'
 
-  url "https://github.com/pkamb/PowerKey/releases/download/v#{version}/PowerKey#{version}.zip"
+  url "https://github.com/pkamb/PowerKey/archive/v#{version}.zip"
   appcast 'https://github.com/pkamb/PowerKey/releases.atom',
           checkpoint: '776592bb04be331a5189385dcf83e0fabf7f46a415b2996a242465f3f3ebcb73'
   name 'PowerKey'
