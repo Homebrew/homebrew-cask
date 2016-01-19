aa152b3d07f507983f1f40cc3369bb193b2ae47f
Vítor Galvão <info@vitorgalvao.com>
2016-01-19 00:58:05 +0000

    updated sonora (2.0)

--- Casks/sonora.rb
+++ Casks/sonora.rb
@@ -4,6 +4,8 @@ cask 'sonora' do
 
   # github.com is the official download host per the vendor homepage
   url 'https://github.com/downloads/sonoramac/Sonora/Sonora.zip'
+  appcast 'https://getsonora.com/updates/sonora2.xml',
+          checkpoint: '6bc10e0ee1e63b8d453557ffba549974fcca1e0847f7b5ecd7d96e4595e1458d'
   name 'Sonora'
   homepage 'https://getsonora.com/'
   license :gratis
