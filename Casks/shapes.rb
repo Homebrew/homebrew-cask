7d5cb2fddaed068b7ea888ccdd61e5afcd212145
Victor Popkov <victor@popkov.me>
2016-01-20 20:34:00 +0200

    shapes.rb: upgrade to v4.5.5

--- Casks/shapes.rb
+++ Casks/shapes.rb
@@ -1,10 +1,10 @@
 cask 'shapes' do
-  version '4.5.4'
-  sha256 'f99a11a0d98bbc07f16408659c7487d91236e65284ee579da346e9baf524ed9e'
+  version '4.5.5'
+  sha256 '8650580e540fe07c654d9d183dbe7883468ae2aa2226c13d49f3a28246824244'
 
   url "http://shapesapp.com/dist/Shapes_#{version}.zip"
   appcast 'http://shapesapp.com/appcast/shapes4.rss',
-          checkpoint: 'a733a8f85532786e295b0eaf33489490623775a710426bc2e5a3b03cecab75ca'
+          checkpoint: '9ffd949e787523019d9985bf81542347cb767b5964da81b85b7c82f1551ff656'
   name 'Shapes'
   homepage 'http://shapesapp.com'
   license :commercial
