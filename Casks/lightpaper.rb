8a2c34475ab245abcf6a9389b68f406b06750e87
Johnathan Conley <johnathan.conley@gmail.com>
2016-01-22 11:41:11 -0600

    update lightpaper to :latest

--- Casks/lightpaper.rb
+++ Casks/lightpaper.rb
@@ -1,14 +1,14 @@
 cask 'lightpaper' do
-  version '1.1.4'
-  sha256 '583afc22a29e0bd4f241b5476a1aec8291e35ebf080e4161b3f8552bf948f483'
+  version '1.2.2'
+  sha256 'ed88ad17b53ae160af0f235aea0b10da75eeab606a83acb620ff201ebacc68bf'
 
   # hockeyapp.net is the official download host per the vendor homepage
   url 'https://rink.hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8?format=zip'
-  appcast 'https://dl.dropboxusercontent.com/u/83257/LpMacUpdates/lightpaper_appcast.xml',
+  appcast 'http://links.ashokgelal.com/lp-mac-update-feed',
           checkpoint: 'efc1776aec492aef87a267155c72c88ca67c15378171fed3c35199e1522ee036'
   name 'LightPaper'
-  homepage 'http://www.ashokgelal.com/lightpaper-for-mac/'
-  license :gratis
+  homepage 'http://lightpaper.42squares.in/'
+  license :commercial
 
   app 'LightPaper.app'
 end
