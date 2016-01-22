c66ef7302d6d5a87684013c198380613411336f4
Pavel <lamiska@Pavels-MacBook-Pro.local>
2016-01-22 14:49:29 +0100

    updated waterfox to 43.0.4

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,6 +1,6 @@
 cask 'waterfox' do
   version '43.0.4'
-  sha256 '49720ec39019cc7991531677014bbd140062fb72f0d5dbd963f7f14b75da37b6'
+  sha256 'f18ecbb17af5d186ab97c85ce226c29c001cbe83d4a5d565aaf4ec435027be6a'
 
   url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20#{version}%20Setup.dmg"
   name 'Waterfox'
