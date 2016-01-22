3ec7b5a2373edf4f2c582b194685c7d1e518b947
Marc Riera <mrc2407@gmail.com>
2016-01-22 12:41:41 +0100

    --amend

--- Casks/wmail.rb
+++ Casks/wmail.rb
@@ -2,7 +2,7 @@ cask 'wmail' do
   version '1.1.3'
   sha256 'dd34f54ac6ddf9ba880684c0c4a167a5c6431cddedc08450580efb25d3c68925'
 
-  url 'https://github.com/Thomas101/wmail/releases/download/v1.1.3/WMail_1_1_3.zip'
+  url "https://github.com/Thomas101/wmail/releases/download/v#{version}/WMail_#{version.dots_to_underscores}.zip"
   name 'WMail'
   homepage 'https://github.com/Thomas101/wmail'
   license :mit
