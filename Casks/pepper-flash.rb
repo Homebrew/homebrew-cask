cd4891ffaece2b7b79f659bf3ffd8b9173b6d39b
Yang Jiaqi <tarawa@users.noreply.github.com>
2016-01-23 14:47:58 +0800

    Update sha256 sum for pepper-flash

--- Casks/pepper-flash.rb
+++ Casks/pepper-flash.rb
@@ -1,6 +1,6 @@
 cask 'pepper-flash' do
   version '20.0.0.228'
-  sha256 'c94600c9b85f5e96f21580ac8aaa4be2aa8511c18379d1cb488a3d813bcbcce1'
+  sha256 'c8eb2fde34a1b886de9fe68b299ec85b0acf83012fce5d3cef0e5e6e351fcddd'
 
   url "https://admdownload.adobe.com/bin/live/AdobeFlashPlayer_#{version.to_i}ppau_a_install.dmg"
   name 'Adobe Pepper Flash Player'
