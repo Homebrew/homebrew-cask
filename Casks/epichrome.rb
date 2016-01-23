1a6e7eb9742dd38041640b4435a5f94cc3606829
Victor Popkov <victor@popkov.me>
2016-01-23 21:40:45 +0200

    epichrome.rb: upgrade to v2.1.7

--- Casks/epichrome.rb
+++ Casks/epichrome.rb
@@ -1,10 +1,10 @@
 cask 'epichrome' do
-  version '2.1.6'
-  sha256 '136f9cc1289510a33b572161bddf1ec3b1f1a7344e60bde3700bd208b6beaa3e'
+  version '2.1.7'
+  sha256 '29114049f8ec080e0d01dc761df17a9b396f7371888ef3adab441a99dd1142e7'
 
   url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.dmg"
   appcast 'https://github.com/dmarmor/epichrome/releases.atom',
-          checkpoint: 'c22d897666fdcea32a260fe0c361d4bd76d146ab8c26756535514427d65150f0'
+          checkpoint: '3ef839a6c1236a50f53c6c28f487a7bb053790d0a97bbdbc08ec1f9a528511e9'
   name 'Epichrome'
   homepage 'https://github.com/dmarmor/epichrome'
   license :gpl
