73ed52c76c8215415244547b63a736c411ffa815
Victor Popkov <victor@popkov.me>
2016-01-24 21:17:51 +0200

    handbrake.rb: upgrade to v0.10.3

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -1,10 +1,10 @@
 cask 'handbrake' do
-  version '0.10.2'
-  sha256 '9591cefbfcbe97ad4e039b456f82ea0e76035b5db9c9aa2ebca044fb171d4dd5'
+  version '0.10.3'
+  sha256 '5bf327f6d42901668490338c32eab8cc2d4db3e09ea1cb9b8c44573ed8fb5dda'
 
   url "http://download.handbrake.fr/releases/#{version}/HandBrake-#{version}-MacOSX.6_GUI_x86_64.dmg"
   appcast 'https://handbrake.fr/appcast.x86_64.xml',
-          checkpoint: '751918e991147ecd36ac29782bb0f0eda5a3fde1301e4727ef5e5e02d63b1d44'
+          checkpoint: 'fd37c7d6741c19ae69270c1af2c2adb084d66f064e1edabe249e1925eb7cda1f'
   name 'HandBrake'
   homepage 'https://handbrake.fr'
   license :oss
