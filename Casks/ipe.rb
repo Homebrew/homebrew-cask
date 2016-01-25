6810e09a4e6757b620bc3c6d2bdf4ab0888dc7d1
miccal <miccal.matthews@gmail.com>
2016-01-24 21:25:31 -0700

    Update ipe.rb (7.2.2)

--- Casks/ipe.rb
+++ Casks/ipe.rb
@@ -1,6 +1,6 @@
 cask 'ipe' do
-  version '7.2.1'
-  sha256 '5c7620898eadfe05ee7e5ec5ef43439ee41f8aaf1b3babaf128ca4396dc41009'
+  version '7.2.2'
+  sha256 'ba4f9aa51a98a5fe6b495b4840ed8d0c8df1d40912b54998866bad2e63031270'
 
   # bintray.com is the official download host per the vendor homepage
   url "https://dl.bintray.com/otfried/generic/ipe/#{version.to_f}/ipe-#{version}-mac.dmg"
