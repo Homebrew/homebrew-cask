2a7b192b72e1cfaee8b11e152e26913d458a423c
Victor Popkov <victor@popkov.me>
2016-01-25 20:49:45 +0200

    qtpass.rb: upgrade to v1.1.0

--- Casks/qtpass.rb
+++ Casks/qtpass.rb
@@ -1,10 +1,10 @@
 cask 'qtpass' do
-  version '1.0.6'
-  sha256 'f024f277fce4a069a49f60b03c85e45e84a49faad145538bd468c606e3468939'
+  version '1.1.0'
+  sha256 '3425bd777af74b116c071f7b04dabfda788844cbf01f6bdf435015e586e6bbfa'
 
   url "https://github.com/IJHack/qtpass/releases/download/v#{version}/qtpass-#{version}.dmg"
   appcast 'https://github.com/IJHack/qtpass/releases.atom',
-          checkpoint: '45611201958e1504409a00156dd0c2339f8330fd24630df9c0105caaad022b61'
+          checkpoint: '4d3beffb4da142bc793177247060f7c3f8712a09ccb43f08de930b80c8f2eb98'
   name 'QtPass'
   homepage 'https://qtpass.org/'
   license :gpl
