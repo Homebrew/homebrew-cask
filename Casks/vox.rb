f16ad01d2f7de6c21cd31157f304417f0f50180b
Alex Samorukov <samm@os2.kiev.ua>
2016-01-26 00:48:56 +0100

    vox.rb: update to the build 2710.1, fix installation on case-sensitive filesystems

--- Casks/vox.rb
+++ Casks/vox.rb
@@ -1,14 +1,14 @@
 cask 'vox' do
-  version '2700.1,1452622352'
-  sha256 '0fa143060e43411ed28a8cc9fbc4e17b6553a4a73a28a8b7d362e625108ea97a'
+  version '2710.1,1452877541'
+  sha256 '0a24e5b119636cc441a91fe88c628e4d3fde249a5b7ee219fdb324d0c956c534'
 
   # devmate.com is the official download host per the vendor homepage
   url "https://dl.devmate.com/com.coppertino.Vox/#{version.before_comma}/#{version.after_comma}/Vox-#{version.before_comma}.zip"
   appcast 'https://updates.devmate.com/com.coppertino.Vox.xml',
-          checkpoint: 'c1cb1cf6ccd4174f3371313c0eb8517f1810e98ffaab9ae239826c129e68ba82'
+          checkpoint: '4f0f130b5ad7360e3a3b128ab955493e6a2019a6cdb0b1bd6003ed6dfa211b30'
   name 'VOX'
   homepage 'https://coppertino.com/vox/mac'
   license :freemium
 
-  app 'VOX.app'
+  app 'Vox.app'
 end
