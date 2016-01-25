7a876d19ac9982788ade490e58a9fb2584eb02f4
Victor Popkov <victor@popkov.me>
2016-01-25 20:42:39 +0200

    contexts.rb: upgrade to v2.0.3

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -1,10 +1,10 @@
 cask 'contexts' do
-  version '1.9.2'
-  sha256 'cf01b2c7f8ff061071f8352794dbbc8f385bc0c2089a5df30792627d587baa59'
+  version '2.0.3'
+  sha256 '3cadaf3f5c77a62f1181282bffd37ea8eaf60501b8ece5fce45ca6af1b65dd62'
 
   url "https://contexts.co/releases/Contexts-#{version}.zip"
   appcast 'https://contexts.co/appcasts/stable.xml',
-          checkpoint: 'c3c271dcc8fad21e821c702b5da65b1a32b641219c0629b88063e20aa243960d'
+          checkpoint: 'be92aad194057e5602d48ebaf62647980c07afd21f79cd1d95b982c0c0985c62'
   name 'Contexts'
   homepage 'https://contexts.co'
   license :commercial
