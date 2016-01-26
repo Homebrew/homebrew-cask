83b080cf1742761a0ad852c8ab09923aeec25494
Aleksandr Bukhalo <evilsprut@users.noreply.github.com>
2016-01-26 17:16:47 +0300

    Upgrade avocode.app (2.5.2)

--- Casks/avocode.rb
+++ Casks/avocode.rb
@@ -1,6 +1,6 @@
 cask 'avocode' do
-  version '2.4.3'
-  sha256 '6f53e78a5fa60a80e103168fce8e3aca22013d20369f2d42b8155aee560d1b0b'
+  version '2.5.2'
+  sha256 '19087366dd81afc569abb9fca45738da5defbda5a2d2976e3fbaa5246182d10b'
 
   url "http://mediacdn.avocode.com/download/avocode-app/#{version}/avocode-app-mac-#{version}.zip"
   name 'Avocode'
