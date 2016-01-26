d7cfca9179729a91438bae6bd30baa7f7800a416
Spencer Mathews <spencer.mathews@gmail.com>
2016-01-25 23:17:39 -0800

    updated postman (3.2.10.1)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,6 +1,6 @@
 cask 'postman' do
-  version '3.2.9.1'
-  sha256 '1e62914e81fdb9ea5e00520777cee48570182a77a6771be5f63ca1681454cac4'
+  version '3.2.10.1'
+  sha256 'b6ab622c6e54f87c3d1222b28fd2dbc1f39cbd4add65638aef1f34cc29991e64'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/postman-electron-builds/mac/postman-osx-#{version}.zip"
