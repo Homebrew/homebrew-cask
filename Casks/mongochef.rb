2a739c0e7d699507f83836c0c1ea27dee1fcd22c
Dmitry Labutin <dmitry@labutin.net>
2016-01-27 12:07:47 +0300

    New version 3.3.0

--- Casks/mongochef.rb
+++ Casks/mongochef.rb
@@ -1,6 +1,6 @@
 cask 'mongochef' do
-  version '3.2.4'
-  sha256 'bda8abdfd005d6b5dce469e3652cc38a798c44c70e6368fdc7da5c1e8046a9a2'
+  version '3.3.0'
+  sha256 '9d193c1e1488485a7e130d5a319387fe8835f79b9c8614981a2fa80c49e2ec29'
 
   url "https://cdn.3t.io/mongochef/mac/#{version}/MongoChef.dmg"
   name 'MongoChef'
