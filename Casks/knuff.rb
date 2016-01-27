3f9861a02ecf82466283e798d446688649b1e6fe
Herb <i@4leaf.me>
2016-01-27 15:20:32 +0800

    add knuff

---
+++ Casks/knuff.rb
@@ -0,0 +1,13 @@
+cask 'knuff' do
+  version '1.0'
+  sha256 '4b5e88736e5d9443a84a204ba6c2a35faa7dfa6a6f94fb15286c70f07ef94744'
+
+  url "https://github.com/KnuffApp/Knuff/releases/download/v#{version}/Knuff.app.zip"
+  appcast 'https://github.com/KnuffApp/Knuff/releases.atom',
+          checkpoint: 'https://github.com/KnuffApp/Knuff/releases'
+  name 'Knuff'
+  homepage 'https://github.com/KnuffApp/Knuff'
+  license :mit
+
+  app 'Knuff.app'
+end
