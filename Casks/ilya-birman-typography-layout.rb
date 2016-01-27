4066dff52b6c54dfb99dd98a7692004333171151
Aleksandr Bukhalo <inbox@evilsprut.com>
2016-01-27 18:51:26 +0300

    Add Ilya Birman typography layout

---
+++ Casks/ilya-birman-typography-layout.rb
@@ -0,0 +1,13 @@
+cask 'ilya-birman-typography-layout' do
+  version '3.4.1'
+  sha256 'a4d830a490e2dbdc82c8f38e613391aeb62856bae3ed85774e3d939224de8af8'
+
+  url "http://ilyabirman.ru/download/ilya-birman-typolayout-#{version}-mac.dmg"
+  name 'Ilya Birman typography layout'
+  homepage 'http://ilyabirman.ru/projects/typography-layout/'
+  license :oss
+
+  artifact 'Install Ilya Birman Typography Layout.app/Contents/Resources/Layout/Ilya Birman Typography Layout.bundle', target: Pathname.new(File.expand_path('~')).join('Library/Keyboard Layouts/Ilya Birman Typography Layout.bundle')
+
+  caveats "To enable new keyboard layouts, you must add them manually in System Preferences. More info on #{homepage}."
+end
