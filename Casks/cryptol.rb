86e31f9005e4fdf59e155dbecc24214e75ff0b58
Vítor Galvão <info@vitorgalvao.com>
2016-01-28 23:45:39 +0000

    updated cryptol (2.3.0)

--- Casks/cryptol.rb
+++ Casks/cryptol.rb
@@ -1,11 +1,11 @@
 cask 'cryptol' do
-  version '2.2.6'
-  sha256 'ab6fc7167772f7a403df559626360c58b0162a48545657162758103ea0c86f3c'
+  version '2.3.0'
+  sha256 ''
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/GaloisInc/cryptol/releases/download/v#{version}/cryptol-#{version}-MacOSX-64.tar.gz"
   appcast 'https://github.com/GaloisInc/cryptol/releases.atom',
-          checkpoint: 'a297a208be4d6a81e6bf0675fd8d204eb99ad3254b4c886d382d0707824c0a03'
+          checkpoint: '58a2b57ae6c2c9775b4025228bbf92caad8f9b489b4fc36d185ffe0bc43d4b85'
   name 'Cryptol'
   homepage 'http://cryptol.net/'
   license :bsd
