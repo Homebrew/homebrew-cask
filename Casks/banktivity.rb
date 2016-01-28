55b1259a364906092940bd5831d3f1299d9fea9a
Maurice Kelly <osv-github@chatswood.org.uk>
2016-01-28 21:34:16 +0000

    Rename the cask for iBank to Banktivity
    The version number, 5, remains the same, but the name and URL has
    been updated. Additionally, I tested the URLs for the old versions
    of the app (for Leopard and Lion), and they now report 404 Not Found.

---
+++ Casks/banktivity.rb
@@ -0,0 +1,11 @@
+cask 'banktivity' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.iggsoft.com/banktivity/Banktivity5_Web.dmg'
+  name 'Banktivity'
+  homepage 'https://www.iggsoftware.com/banktivity'
+  license :commercial
+
+  app 'Banktivity 5.app'
+end
