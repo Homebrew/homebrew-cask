08675624092ce90a848b2421e21b25b51f0118c6
Vítor Galvão <info@vitorgalvao.com>
2016-01-28 23:41:13 +0000

    updated buildasaur (0.8.0)

--- Casks/buildasaur.rb
+++ Casks/buildasaur.rb
@@ -1,10 +1,10 @@
 cask 'buildasaur' do
-  version '0.6.5'
-  sha256 'f153d41bb99222fdad7407588af41fd32cf5ed54463cbdb52490e27446b629e8'
+  version '0.8.0'
+  sha256 'c8f16247d78a3c404d585082bfae829b2a8c59f88b21bd5c831638f9be5ea0fc'
 
   url "https://github.com/czechboy0/Buildasaur/releases/download/v#{version}/Buildasaur.app.zip"
   appcast 'https://github.com/czechboy0/Buildasaur/releases.atom',
-          checkpoint: 'fcd6687f2486601c8950858fb44d1a515ced2e8399366360b36c3c5f72017afd'
+          checkpoint: '29ab69a55849cc65744f71c471baf1f9ea61cc66041207b8a726810d49b653be'
   name 'Buildasaur'
   homepage 'https://github.com/czechboy0/Buildasaur'
   license :mit
