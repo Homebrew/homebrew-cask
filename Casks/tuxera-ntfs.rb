c61eaa2d59409e2e729786c5db1791e237bbfccb
Vincent Hamilton <vincent@magnesium.local>
2016-01-28 16:41:00 -0500

    updated tuxera-ntfs (latest)

--- Casks/tuxera-ntfs.rb
+++ Casks/tuxera-ntfs.rb
@@ -2,7 +2,7 @@ cask 'tuxera-ntfs' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.tuxera.com/mac/tuxerantfs_2015.dmg'
+  url 'https://www.tuxera.com/mac/tuxerantfs_2015.1.dmg'
   name 'Tuxera NTFS'
   homepage 'https://www.tuxera.com/products/tuxera-ntfs-for-mac/'
   license :closed
