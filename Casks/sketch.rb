93845209b562b7c89e177bd6c156607e753864cf
Ugo Arangino <ugo@arangino.de>
2016-01-28 14:30:19 +0100

    Update sketch.rb
    update version (to 3.5.1) and sha256

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -1,6 +1,6 @@
 cask 'sketch' do
-  version '3.4.4'
-  sha256 '428f5d507f68a7b86cf5d97d3b0fc5c23297cd2048463dbf4e222375154cef79'
+  version '3.5.1'
+  sha256 'f5ee1e689c941bf480fcc7a462347ca45d9ec94dde8395e79a879e12882f643e'
 
   url 'http://www.sketchapp.com/static/download/sketch.zip'
   appcast "http://www.sketchapp.com/appcast#{version.major}.xml",
