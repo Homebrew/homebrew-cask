00998214f1ba954f4f121df6a39ed144a1f3a6a2
Vítor Galvão <info@vitorgalvao.com>
2016-01-29 00:26:09 +0000

    updated dropzone (3.5.4)

--- Casks/dropzone.rb
+++ Casks/dropzone.rb
@@ -4,10 +4,10 @@ cask 'dropzone' do
 
   url "https://aptonic.com/dropzone3/sparkle/Dropzone-#{version}.zip"
   appcast 'https://aptonic.com/dropzone3/sparkle/updates.xml',
-          checkpoint: 'bcf0cbe0e40b87a1111c8bc0254588ab7ab4c9c4db77db4cf94ee46aa3c1db3e'
+          checkpoint: '3369ac9be8ca4bcea11a1827b776eb3da857ee7666b45cac40ede350ba95d006'
   name 'Dropzone'
   homepage 'https://aptonic.com'
   license :commercial
 
-  app 'Dropzone 3.app'
+  app "Dropzone #{version.major}.app"
 end
