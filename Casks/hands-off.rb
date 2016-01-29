b74546761d5aff042a9e8a876bd4b8634ae5aa11
Vítor Galvão <info@vitorgalvao.com>
2016-01-29 00:11:51 +0000

    updated hands-off (3.0.3)

--- Casks/hands-off.rb
+++ Casks/hands-off.rb
@@ -1,10 +1,10 @@
 cask 'hands-off' do
-  version '3.0.2'
-  sha256 'e954858da72a6f05194feb8cc193e3a2a10d33de38381f461b8894be95bde56e'
+  version '3.0.3'
+  sha256 '73eb32b2251816d138defaca5005a6943880bf1906f054bb39eefe341f03e830'
 
   url "https://www.oneperiodic.com/files/Hands%20Off!%20v#{version}.dmg"
   appcast "http://www.oneperiodic.com/handsoff#{version.major}.xml",
-          checkpoint: '3a5f3878cc4cd3a658d3969d4778222e027aefe2dd65a4836cd64ff4f671cd0e'
+          checkpoint: '3ffa499a6a810a091bb717765e7232c35a3071bc15533a42af01e2d8cb818155'
   name 'Hands Off!'
   homepage 'https://www.oneperiodic.com/products/handsoff/'
   license :commercial
