dd2d7e63760ace934f7cf9d61d39f7af18ea760e
Vítor Galvão <info@vitorgalvao.com>
2016-01-29 23:20:43 +0000

    updated metabase (0.14.0)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,10 +1,10 @@
 cask 'metabase' do
-  version '0.13.0'
-  sha256 '35b741061928eac96d4ba53b584dd0c99fd7ddb59cc9812fb802fe53843c28c3'
+  version '0.14.0'
+  sha256 '081caf591a5038ce233d40118de127ec0a38b28b38a25e3e38b341641fb78929'
 
   url "http://downloads.metabase.com/v#{version}/Metabase.dmg"
   appcast 'http://downloads.metabase.com/appcast.xml',
-          checkpoint: '54adac98a301a104408fc7983d4e466d86e6ba830ca5431479ed425fa6759827'
+          checkpoint: '72054dfbcbd85089bf0199b85e0398512676ae4ec990398c46c4ad2e131d4fa7'
   name 'Metabase'
   homepage 'http://www.metabase.com/'
   license :affero
