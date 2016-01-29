5c8c155ce23d8c802c2edae8594af4294948c8c4
Vítor Galvão <info@vitorgalvao.com>
2016-01-29 23:28:30 +0000

    updated optionspace (1.0.4)

--- Casks/optionspace.rb
+++ Casks/optionspace.rb
@@ -1,10 +1,10 @@
 cask 'optionspace' do
-  version '1.0.3'
-  sha256 '826d2ec636044c466d49dd0aa72b8a3a9cc674aea9988d7c0eb3b91274c9800f'
+  version '1.0.4'
+  sha256 'e03cc07a334db896f677f3b77b3b1aca6cbdab80f75614a3e03cdf4519e35355'
 
   url "https://optionspace.co/download/OptionSpace_v#{version}.dmg"
   appcast 'https://optionspace.co/app/appcast.xml',
-          checkpoint: '858fff1f446633e2143699f290e0f267ab3b2414ec319f101ba3bb9cac8f7d63'
+          checkpoint: '1e660b15eb8800467a96484a1b6a3725a749db714f14dc9b159258adaad59934'
   name 'OptionSpace'
   homepage 'https://optionspace.co'
   license :freemium
