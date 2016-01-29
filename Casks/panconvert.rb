2dacc941a355b2b86ae92dca33e6d2aa1a166268
Nathan A. Paxton <napaxton@gmail.com>
2016-01-29 09:34:06 -0500

    change url; add newline at end of file

--- Casks/panconvert.rb
+++ Casks/panconvert.rb
@@ -2,7 +2,7 @@ cask 'panconvert' do
   version :latest
   sha256 :no_check
 
-  url "http://sourceforge.net/projects/panconvert/files/latest/download?source=files"
+  url "http://downloads.sourceforge.net/sourceforge/panconvert/PanConvert-0.1.5.dmg"
   appcast 'https://github.com/apaeffgen/PanConvert/blob/master/docs/changelog.md',
       checkpoint: 'e2333a9cd663739863c8306193960401977aaa88'
 
