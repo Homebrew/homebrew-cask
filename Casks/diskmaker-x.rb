cb300f27606d834ed6a377c692954c24d1aa8aec
Yves Desgagne <yves@khome.org>
2016-01-29 09:23:19 -0500

    Fix symlink source is not there in diskmaker-x cask.

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -1,11 +1,11 @@
 cask 'diskmaker-x' do
-  version :latest
-  sha256 :no_check
+  version '5.0.2'
+  sha256 '040a21bdef0c2682c518a9e9572f7547b5f1fb02b8930a8a084ae85b12e70518'
 
   url 'http://diskmakerx.com/downloads/DiskMaker_X.dmg'
   name 'DiskMaker X'
   homepage 'http://diskmakerx.com/'
   license :gratis
 
-  app 'DiskMaker X.app'
+  app 'DiskMaker X 5.app'
 end
