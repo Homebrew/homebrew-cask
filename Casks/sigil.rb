d1e4eb01864b051011870f5ca5ecc249526cc89a
Vítor Galvão <info@vitorgalvao.com>
2016-01-29 23:50:29 +0000

    updated sigil (0.9.2)

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -3,9 +3,9 @@ cask 'sigil' do
   sha256 'ea9aca6e9c1c4d7931982fd6a3c5bcc4e1d35986e728fe3522dc92e9a6200f25'
 
   # github.com is the official download host per the vendor homepage
-  url "https://github.com/user-none/Sigil/releases/download/#{version}/Sigil-#{version}-Mac-Package.dmg"
-  appcast 'https://github.com/user-none/Sigil/releases.atom',
-          checkpoint: '23751416ae96455509d00cb3e55beedbf6047c089c5f0d3c4663ca4faf6e64e8'
+  url "https://github.com/Sigil-Ebook/Sigil/releases/download/#{version}/Sigil-#{version}-Mac-Package.dmg"
+  appcast 'https://github.com/Sigil-Ebook/Sigil/releases.atom',
+          checkpoint: '7db982b4c4be6c05da7438161c8f844a32203c5a3bf76b2ab14f5eddc2ad69ce'
   name 'Sigil'
   homepage 'http://sigil-ebook.com/'
   license :gpl
