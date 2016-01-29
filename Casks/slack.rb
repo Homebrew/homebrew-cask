085024aa1c07e10797c2d1c47142c48626362cc0
Vítor Galvão <info@vitorgalvao.com>
2016-01-29 23:57:38 +0000

    updated slack (1.1.10)

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -5,7 +5,7 @@ cask 'slack' do
   # fastly.net is the official download host per the vendor homepage
   url "https://slack-ssb-updates.global.ssl.fastly.net/mac_public_releases/slack-#{version}.zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1',
-          checkpoint: 'c2d90475179172f1e067b7b948da6c536a25c19568c2b1085ba67751fe75933c'
+          checkpoint: '0505bf9a7d75c00c27fd1e13eabc8aa946c1f3b6e21a3e9f0dfc4db8bcabdf54'
   name 'Slack'
   homepage 'https://slack.com/'
   license :gratis
