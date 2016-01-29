07e917b98a74be21c5cd05a2727e1f2ec9088e4b
Ryan Hendrickson <ryan.hendrickson@alum.mit.edu>
2016-01-28 19:08:16 -0500

    freesmug-chromium 48.0.2564.97

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -1,11 +1,11 @@
 cask 'freesmug-chromium' do
-  version '47.0.2526.111'
-  sha256 '71d47d02f868684774b70f4243b1030f7a3efb5557e4684acab246b541bf68ab'
+  version '48.0.2564.97'
+  sha256 '6ec54b8583be90545519b02cc13e119ba8ff69ad272bb7770628764aef496c56'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/sourceforge/osxportableapps/Chromium_OSX_#{version}.dmg"
   appcast 'http://sourceforge.net/projects/osxportableapps/rss?path=/Chromium',
-          checkpoint: '366664e93ad47982687fdff3c41e93017f85bf7e18bd88ad2d1d7bba9621f6be'
+          checkpoint: 'c5a0a7f86793407ea41cc046748eeb16760a5cf3bdbb01b4052610d442258707'
   name 'Chromium'
   homepage 'http://www.freesmug.org/chromium'
   license :gpl
