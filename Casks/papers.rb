18673e006b86cd6287d46da1f0321ea46a14610e
Vítor Galvão <info@vitorgalvao.com>
2016-01-29 23:30:50 +0000

    updated papers (3.4.3,458)

--- Casks/papers.rb
+++ Casks/papers.rb
@@ -1,10 +1,10 @@
 cask 'papers' do
-  version '3.4.2,439'
-  sha256 '6e22159c30cab70f9157ead8097bff8ec06bdad0e966e4c6bd33d4229b04ffce'
+  version '3.4.3,458'
+  sha256 '0bea17fde869cb3ee8057e888989abe2c39e35b1d0208d62fc9d7a14d636cc77'
 
   url "http://appcaster.papersapp.com/apps/mac/production/download/#{version.after_comma}/papers_#{version.before_comma.no_dots}_#{version.after_comma}.dmg"
   appcast 'http://appcaster.papersapp.com/apps/mac/production/appcast.xml',
-          checkpoint: '700e9e2f2a2b4a90b0c3bc5c346fb8aaf6c1ddee1367571cbdc972522ed1824f'
+          checkpoint: '2fff083f0bbfada12a7855d93193879700be699a4a18288a3d2264ec75ed0f85'
   name 'Papers'
   homepage 'http://www.papersapp.com/'
   license :commercial
