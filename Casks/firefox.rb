789fb381368508544a976a6243b5e2e877eb05e7
Vítor Galvão <info@vitorgalvao.com>
2016-01-29 23:09:56 +0000

    updated firefox (44.0)

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -3,8 +3,6 @@ cask 'firefox' do
   sha256 '74f7d4041f2582553a460fd8249575dfc501b57bdee37f43083e8d7277d2d722'
 
   url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
-  appcast 'https://www.mozilla.org/en-US/firefox/releases/',
-          checkpoint: '4a7b21fc652376f84ccc008bb33f7b59cd90aed812ee9c2eccf6c3e7a450601e'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/'
   license :mpl
