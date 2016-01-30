52f59e4a9a5db101368735680a6d03d90e99e506
Victor Popkov <victor@popkov.me>
2016-01-30 19:37:40 +0200

    multidoge.rb: upgrade to v0.1.7

--- Casks/multidoge.rb
+++ Casks/multidoge.rb
@@ -1,11 +1,11 @@
 cask 'multidoge' do
-  version '0.1.6'
-  sha256 'ca6ff07ecb1ecaad3040969270b963edae22875fa92e8695bf4ebcc068061441'
+  version '0.1.7'
+  sha256 '648751ce2598d838b07b65dd5ff3e4136b0c369ddc38efedd9329a63365506f5'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/langerhans/multidoge/releases/download/v#{version}/multidoge-#{version}.dmg"
   appcast 'https://github.com/langerhans/multidoge/releases.atom',
-          checkpoint: '2b5354ba83e7545f41b1f875823af53da215be0e152bcc195e788d7540baf49b'
+          checkpoint: 'e53665cab01a3c7751ec2c0f0aceb2fd15623e2627252f8260a91679f586d785'
   name 'MultiDoge'
   homepage 'http://multidoge.org/'
   license :mit
