849a271e422d50cb03648ea82bca8b90b3c8c64e
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 18:48:20 +0000

    updated macaw (1.6.1)

--- Casks/macaw.rb
+++ Casks/macaw.rb
@@ -1,10 +1,10 @@
 cask 'macaw' do
-  version '1.6'
-  sha256 'd9c547da61e4a4755361b2c44f3643d15530e3ae4edb4262a42afbeac0c6380e'
+  version '1.6.1'
+  sha256 '11aac6e57bbb71fb9ea5e3e3dfc5d991e36e266635df0c0c95e52efeb8a5e942'
 
   url "http://download.macaw.co/#{version}/Macaw#{version}.dmg"
   appcast 'http://download.macaw.co/appcast.xml',
-          checkpoint: '66f3ff26ff5246ed4d742d9ec02dd02f58044370e2d1238adb15ee0a6f2879db'
+          checkpoint: '965a05c732a828d55db7d563cb01d1f746c425e3c672233ba3f67ec998e7bf2e'
   name 'Macaw'
   homepage 'http://macaw.co/'
   license :commercial
