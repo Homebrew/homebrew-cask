921bbee2fadbb7ec88d4e96ba3f4ed58b4eb98f9
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 00:19:26 +0000

    updated studio-link-plugin (15.12.2-beta)

--- Casks/studio-link-plugin.rb
+++ Casks/studio-link-plugin.rb
@@ -1,11 +1,11 @@
 cask 'studio-link-plugin' do
-  version '15.12.1-beta'
-  sha256 '12145c5d054527a1460038166f5cf2d18219e3b614e01391af9b8b6d7a3d765f'
+  version '15.12.2-beta'
+  sha256 '3b83c27e7b91764ad11c8cf2a0a6ac6d55132123e8a8594f38ce25c83b0f3342'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/Studio-Link-v2/backend/releases/download/#{version}/studio-link-osx.zip"
   appcast 'https://github.com/Studio-Link-v2/backend/releases.atom',
-          checkpoint: 'a11fcbad52d77a933c9566c083325007b9a652ed95bc5d56a6a40207d46cb6ab'
+          checkpoint: '284425a11bad52cf7f6ea23d36abe94058fdda30871a2af95d292b43c173dc01'
   name 'Studio Link Plugin'
   homepage 'https://doku.studio-link.de/plugin/installation-plugin.html'
   license :bsd
