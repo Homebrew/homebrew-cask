fa5a556df6d219a318c4b72ba2aea02640c570b7
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 00:22:43 +0000

    updated tabula (1.0.1)

--- Casks/tabula.rb
+++ Casks/tabula.rb
@@ -2,9 +2,10 @@ cask 'tabula' do
   version '1.0.1'
   sha256 '85e75f120c450bc702c01aa88a4ca9c2d227ef2906ec7020e892e0f174a7d70b'
 
-  url "https://github.com/jazzido/tabula/releases/download/v#{version}/tabula-mac-#{version}.zip"
-  appcast 'https://github.com/jazzido/tabula/releases.atom',
-          checkpoint: '7f31dc12d85d261d05d3b57f1e432f24a3d8dacc0986f16e266db6cd4941a57d'
+  # github.com is the official download host per the vendor homepage
+  url "https://github.com/tabulapdf/tabula/releases/download/v#{version}/tabula-mac-#{version}.zip"
+  appcast 'https://github.com/tabulapdf/tabula/releases.atom',
+          checkpoint: '2e20dc186a93930ca4b59fe694ba278c4171776a19c6942273d1b6510d05755d'
   name 'Tabula'
   homepage 'http://tabula.technology/'
   license :mit
