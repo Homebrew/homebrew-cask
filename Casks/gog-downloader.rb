7aed7d4eeee872db0466c0172a36edfba32000db
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 12:02:12 +0000

    updated gog-downloader (1.2_512)

--- Casks/gog-downloader.rb
+++ Casks/gog-downloader.rb
@@ -3,6 +3,8 @@ cask 'gog-downloader' do
   sha256 '353604a2123feacf438c6586b3ec20967dca637b0a97d36203adbc3e1dfdefce'
 
   url "https://static.gog.com/download/d3/mac-stable/GOG_Downloader_#{version}.zip"
+  appcast 'https://api.gog.com/en/downloader2/status/mac-stable',
+          checkpoint: 'a3c972e56f751b534b16c9c63930e6e663c0643123a09f9cc6ec921a7b4d1afb'
   name 'GOG Downloader'
   homepage 'https://www.gog.com/downloader'
   license :gratis
