952f097bdea69276ae6c6a47647d491989b4af8e
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 18:51:57 +0000

    updated simple-sync (3.2.1-283)

--- Casks/simple-sync.rb
+++ Casks/simple-sync.rb
@@ -1,11 +1,11 @@
 cask 'simple-sync' do
-  version '3.2.0-281'
-  sha256 'fa993287dbd272f69d08b7a60b671af5c8be2f2bdceed9b171255fabcd1fef41'
+  version '3.2.1-283'
+  sha256 '18108c06846933f22281d060db129e45194d2341e43f9b68e5fab793b0f48217'
 
   # roomieremote.com is the official download host per the vendor homepage
   url "https://www.roomieremote.com/b/SimpleSync-#{version.no_dots}.zip"
   appcast 'https://www.roomieremote.com/b/Simple-SyncAppcast.xml',
-          checkpoint: '4287e03e4806162ae0baf238745843e6c724cf5b5756d259e17c3569117eb30f'
+          checkpoint: '163f6cac5b18c68cc26f53cce7cbf4f1bff4cf2bed17c7583cd4c295928ac102'
   name 'Simple Sync'
   homepage 'https://simplecontrol.com/simple-sync'
   license :commercial
