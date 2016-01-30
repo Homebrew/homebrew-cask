846380b61a35800075b4d785d1a422242931c816
Tommy Sparber <tommy@sparber.net>
2016-01-30 20:13:38 +1100

    Updated firefox (44.0)

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,8 +1,10 @@
 cask 'firefox' do
-  version '44.0'
+  version '44.0.0'
   sha256 '74f7d4041f2582553a460fd8249575dfc501b57bdee37f43083e8d7277d2d722'
 
-  url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
+  url "https://ftp.mozilla.org/pub/firefox/releases/#{version.major_minor}/mac/en-US/Firefox%20#{version.major_minor}.dmg"
+  appcast "https://aus5.mozilla.org/update/3/Firefox/#{version}/0/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/Darwin%2015.3.0/default/default/update.xml?force=1",
+          checkpoint: '91a080855e9ee207992c7f5eebf2c4fe8bb7221b983c77f0385e973d34155cea'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/'
   license :mpl
