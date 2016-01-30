94b449ebb2c25c90a5502ad3f455de6773174419
Nathan A. Paxton <napaxton@gmail.com>
2016-01-30 13:59:44 -0500

    Changes made with respect to suggestions at https://github.com/caskroom/homebrew-cask/pull/17670/files#diff-cb9efe614a43dfa56ff40a8b98522770

--- Casks/panconvert.rb
+++ Casks/panconvert.rb
@@ -1,11 +1,10 @@
 cask 'panconvert' do
-  version :latest
-  sha256 :no_check
+  version 0.1.5
+  sha256 98ab46e9ac375b155d9361480e2d180fc2caf9905d55a64ea2faca3873b80ff9
 
-  url "http://downloads.sourceforge.net/sourceforge/panconvert/PanConvert-0.1.5.dmg"
+  url "http://downloads.sourceforge.net/sourceforge/panconvert/PanConvert-v#{version}.dmg"
   appcast 'https://github.com/apaeffgen/PanConvert/blob/master/docs/changelog.md',
       checkpoint: 'e2333a9cd663739863c8306193960401977aaa88'
-
   name 'PanConvert'
   homepage 'http://panconvert.sourceforge.net'
   license :gpl
