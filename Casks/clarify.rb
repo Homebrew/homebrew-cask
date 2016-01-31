c055f88ef51248097eb7b72f5cfd5e39e5b853f9
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 22:24:57 +0000

    clarify.rb: added appcast verified comment

--- Casks/clarify.rb
+++ Casks/clarify.rb
@@ -3,6 +3,7 @@ cask 'clarify' do
   sha256 '454bff1e655bd8aee1c5bcb3c85339b2d09a386637b1cdb0246ab849c6283654'
 
   url "http://files.clarify-it.com/v#{version.major}/updaters/#{version}/Clarify.app.zip"
+  # bluemangolearning.com verified as official when first introduced to the cask
   appcast 'http://www.bluemangolearning.com/download/clarify/2_0/auto_update/release/clarify_appcast.xml',
           checkpoint: 'f72c5dca89003ee282774a04865e98f0f8c02ba3e13913aa428ea3fa6b9a9adc'
   name 'Clarify'
