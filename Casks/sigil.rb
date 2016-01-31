17d0dc1bb01a735b954de907a43157920adc4e22
Victor Popkov <victor@popkov.me>
2016-01-31 17:28:16 +0200

    sigil.rb: upgrade to v0.9.3

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -1,11 +1,11 @@
 cask 'sigil' do
-  version '0.9.2'
-  sha256 'ea9aca6e9c1c4d7931982fd6a3c5bcc4e1d35986e728fe3522dc92e9a6200f25'
+  version '0.9.3'
+  sha256 '83164918449ceee1080e1491c1d33ef9afabcf4d8866a4c738d24e58e27a89d0'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/Sigil-Ebook/Sigil/releases/download/#{version}/Sigil-#{version}-Mac-Package.dmg"
   appcast 'https://github.com/Sigil-Ebook/Sigil/releases.atom',
-          checkpoint: '960ce58effb6621ecc47451a3f95f6d1ef212198a6224b459be55778ecaee4aa'
+          checkpoint: '0b17e785f19bbdb331a4f141e00804eaaafd210a64f4a1b84097abbeaba4c860'
   name 'Sigil'
   homepage 'http://sigil-ebook.com/'
   license :gpl
