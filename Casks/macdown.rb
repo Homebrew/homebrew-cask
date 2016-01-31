400fcb8fe9a7d9ec1a7fc0bba88f7595e436a106
Victor Popkov <victor@popkov.me>
2016-01-31 17:27:09 +0200

    macdown.rb: upgrade to v0.5.5.1

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -1,11 +1,11 @@
 cask 'macdown' do
-  version '0.5.5'
-  sha256 '79d051fb03e953cdc27ae2a45f32cc956fa1471b8031e7284dee01fd19ad22c5'
+  version '0.5.5.1'
+  sha256 '3fba5cbe2e0e7a489e5262c7ef73da500895cef3881889ef961c95f5c3811216'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/uranusjr/macdown/releases/download/v#{version}/MacDown.app.zip"
   appcast 'https://github.com/uranusjr/macdown/releases.atom',
-          checkpoint: 'f9e7f793b7bce3bbdbf6c8fc69036c0fad9d5dd30f934733f449df820fd23c8e'
+          checkpoint: '29f4a235beeda5f90f4c8c133aa02cebd355de416beae8e074ad1b7f86942866'
   name 'MacDown'
   homepage 'http://macdown.uranusjr.com/'
   license :mit
