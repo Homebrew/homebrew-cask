0dbf2964417da62efd2b2e1d43c44b21fc5a1a73
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 22:55:13 +0000

    updated ibettercharge (1.0.10,1443542332)

--- Casks/ibettercharge.rb
+++ Casks/ibettercharge.rb
@@ -4,7 +4,7 @@ cask 'ibettercharge' do
 
   # dl.devmate.com is the official download host per the appcast feed
   url "https://dl.devmate.com/com.softorino.iBetterCharge/#{version.before_comma}/#{version.after_comma}/iBetterCharge-#{version.before_comma}.zip"
-  appcast 'http://updates.devmate.com/com.softorino.iBetterCharge.xml',
+  appcast 'http://hook.softorino.com/ibc/appcast.xml',
           checkpoint: '928b6c64636011e7c75492c6cd2adbaa7e15d38c7773d30bac6aca1745a72cb2'
   name 'iBetterCharge'
   homepage 'http://softorino.com/ibettercharge/'
