ac5e708e6a4e1fd06bc6568e62ea6ee849abb740
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 22:47:31 +0000

    flowdock.rb: added appcast verified comment

--- Casks/flowdock.rb
+++ Casks/flowdock.rb
@@ -4,6 +4,7 @@ cask 'flowdock' do
 
   # amazonaws.com is the official download host per the appcast feed
   url "https://s3.amazonaws.com/flowdock-resources/mac/#{version}/Flowdock.zip"
+  # amazonaws.com verified as official when first introduced to the cask
   appcast 'https://s3.amazonaws.com/flowdock-resources/mac/appcast.xml',
           checkpoint: '05c0917ea74ed402ec6f41dac60a62549f062e7a2f983f410a860d324cdcf976'
   name 'Flowdock'
