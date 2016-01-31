04e179571fa9b715d1a019f834a336455b42dc26
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 21:11:41 +0000

    batchmod.rb: added appcast verified comment

--- Casks/batchmod.rb
+++ Casks/batchmod.rb
@@ -3,6 +3,7 @@ cask 'batchmod' do
   sha256 '6b42b4226414ff3dbd48395e77f97901ccef0ddc04984014b5cf6c39cc3dd249'
 
   url "https://www.lagentesoft.com/resources/batchmod_#{version.delete('.')}.zip"
+  # macchampion.com verified as official when first introduced to the cask
   appcast 'http://www.macchampion.com/arbysoft/batchmodappcast.xml',
           checkpoint: '7cfd47a5e18fba5b39592454a7b97f8feef2239a33b7c15450b4ba73903dfaaf'
   name 'BatChmod'
