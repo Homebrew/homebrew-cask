a0f454c49b6e5c4d6149a451e7a9c215e5dc8221
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 21:07:11 +0000

    antetype.rb: added appcast verified comment

--- Casks/antetype.rb
+++ Casks/antetype.rb
@@ -4,6 +4,7 @@ cask 'antetype' do
 
   # rink.hockeyapp.net is the official download host per the appcast feed
   url 'https://rink.hockeyapp.net/api/2/apps/6ab08fb043a94f51c9109c216e295a50/app_versions/17?format=zip&avtoken=80aaf809156fdbf4f99e6efe71695d9d8f077580'
+  # hockeyapp.net verified as official when first introduced to the cask
   appcast 'https://rink.hockeyapp.net/api/2/apps/6ab08fb043a94f51c9109c216e295a50',
           checkpoint: '1c34ded90e4db5bd7cbec65b1e55fce4a28f0d014478f7a73fa7b60212eee20e'
   name 'Antetype'
