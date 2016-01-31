a4c4b436906cb4e21faa66d31d23edc2d22feb95
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 21:20:55 +0000

    blocs.rb: added appcast verified comment

--- Casks/blocs.rb
+++ Casks/blocs.rb
@@ -3,6 +3,7 @@ cask 'blocs' do
   sha256 'd0263d88c2c17a84c431435ac1fb8f82b5ecb459b4967530a16466e1721360c5'
 
   url 'http://blocsapp.com/trial-app/BlocsApp.zip'
+  # uistore.io verified as official when first introduced to the cask
   appcast 'https://uistore.io/blocs/info.xml',
           checkpoint: '9b50145fe34ffdaa2bd3aaba7dca5e94463dbdc4cbc8db252553036ff8f904e0'
   name 'Blocs'
