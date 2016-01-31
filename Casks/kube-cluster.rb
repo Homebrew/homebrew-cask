ae66e0be448100090d2e939fa8279be2f40083f0
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 03:05:21 +0000

    updated kube-cluster (0.1.7)

--- Casks/kube-cluster.rb
+++ Casks/kube-cluster.rb
@@ -3,7 +3,7 @@ cask 'kube-cluster' do
   sha256 '6f49f6d245ea3ce079ef6d05b4da11d9ce12f931eabce7fb76ae173a2eab50b2'
 
   url "https://github.com/TheNewNormal/kube-cluster-osx/releases/download/v#{version}/Kube-Cluster_v#{version}.dmg"
-  appcast 'https://github.com/TheNewNormal/kube-solo-osx/releases.atom',
+  appcast 'https://github.com/TheNewNormal/kube-cluster-osx/releases.atom',
           checkpoint: 'fbb812d1e79b0647b13beebc76bf01e44dac0b21f9c65ac4dc65acf75ce914be'
   name 'Kube-Cluster'
   homepage 'https://github.com/TheNewNormal/kube-cluster-osx'
