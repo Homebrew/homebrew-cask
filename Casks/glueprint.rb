e5f2f3b8733fc0e5d094c35b32f9f6278ad05b29
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 22:48:55 +0000

    glueprint.rb: added appcast verified comment

--- Casks/glueprint.rb
+++ Casks/glueprint.rb
@@ -3,6 +3,7 @@ cask 'glueprint' do
   sha256 '3a11d1150dd00b0e880ea0543aafc1bba5756a27f896a5e151a10d920595e747'
 
   url "http://glueprintapp.com/static/download/GluePrint-#{version}.app.zip"
+  # amazonaws.com verified as official when first introduced to the cask
   appcast 'http://glueprint-update.s3-website-us-east-1.amazonaws.com/appcast.xml',
           checkpoint: '8028e5a3bf0df90d01fedb100130816570a7baa44dbd34163df95ff1b5f1fd87'
   name 'GluePrint'
