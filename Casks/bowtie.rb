c234a4dd149211399f7394a016773c7f0e298280
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 21:22:13 +0000

    bowtie.rb: added appcast verified comment

--- Casks/bowtie.rb
+++ Casks/bowtie.rb
@@ -3,6 +3,7 @@ cask 'bowtie' do
   sha256 'd8406b066851c0730ca052036bedd5ded82019403de1fd58b579da34cfa4a948'
 
   url "http://bowtieapp.com/bowtie-#{version}.zip"
+  # 13bold.com verified as official when first introduced to the cask
   appcast 'http://updates.13bold.com/appcasts/bowtie',
           checkpoint: '801fdd497d1912b1446627c35d400a6a26bf5ffe430fb10ae8ec0695984bc98c'
   name 'Bowtie'
