abd2d81f5c4e0a2ed84781b243b01bcd8d7f92e2
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 22:31:32 +0000

    duet.rb: added appcast verified comment

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -4,7 +4,7 @@ cask 'duet' do
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3-us-west-1.amazonaws.com/duetmac/#{version.dots_to_underscores}/duet_#{version.dots_to_underscores}.zip"
-  appcast "https://s3-us-west-1.amazonaws.com/duetmac/#{version.dots_to_underscores}/DuetDisplayAppcast.xml",
+  appcast 'http://updates.duetdisplay.com/checkMacUpdates',
           checkpoint: '01a04fe0630a818640a6e9fa9040113791192369529cb757f570a03e19b257f0'
   name 'Duet'
   homepage 'http://www.duetdisplay.com/'
