15cf07e88f091dd36451823aa6be53502097b63e
Lauren <lauren@tomasello.me>
2016-02-01 20:31:11 +0000

    Don't need to use a caveat block, can just use string

--- Casks/mnemosyne.rb
+++ Casks/mnemosyne.rb
@@ -10,24 +10,22 @@ cask 'mnemosyne' do
 
   app 'Mnemosyne.app'
 
-  caveats do
-    puts <<-EOS.undent
-      If you want to use LaTeX inside your flashcards, you need a LaTeX installation. You can
-      get this via Homebrew Cask:
+  caveats <<-EOS.undent
+    If you want to use LaTeX inside your flashcards, you need a LaTeX installation. You can
+    get this via Homebrew Cask:
 
-        brew cask install mactex
+      brew cask install mactex
 
-      Mnemosyne looks for LaTeX in the $PATH environment variable, however OS X does not set
-      $PATH for applications launched via Finder.
+    Mnemosyne looks for LaTeX in the $PATH environment variable, however OS X does not set
+    $PATH for applications launched via Finder.
 
-      You can set environment variables for specific apps via Launch Services, by setting the
-      LSEnvironment key in the app's Info.plist file within the app bundle. You must update the
-      Launch Services registry after doing this, or the changes will not be recognised.
+    You can set environment variables for specific apps via Launch Services, by setting the
+    LSEnvironment key in the app's Info.plist file within the app bundle. You must update the
+    Launch Services registry after doing this, or the changes will not be recognised.
 
-      For example, (substituting for the location of your LaTeX installation):
+    For example, (substituting for the location of your LaTeX installation):
 
-        plutil -insert LSEnvironment -json "{\\"PATH\\":\\"$PATH:/Library/TeX/texbin\\"}" #{staged_path}/Mnemosyne.app/Contents/Info.plist
-        /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -v -f #{staged_path}/Mnemosyne.app
-      EOS
-  end
+      plutil -insert LSEnvironment -json "{\\"PATH\\":\\"$PATH:/Library/TeX/texbin\\"}" #{staged_path}/Mnemosyne.app/Contents/Info.plist
+      /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -v -f #{staged_path}/Mnemosyne.app
+    EOS
 end
