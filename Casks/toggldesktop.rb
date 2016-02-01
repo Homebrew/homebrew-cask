9cd0f20328a81578574cad26a5d31e29bfc5d7a7
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 17:06:42 +0000

    updated toggldesktop (7.3.197)

--- Casks/toggldesktop.rb
+++ Casks/toggldesktop.rb
@@ -2,7 +2,7 @@ cask 'toggldesktop' do
   version '7.3.197'
   sha256 '7cda91d92fc313640d0c06fafc8d0599f7b60c5b99a9dba38516f75ad196c8f2'
 
-  # github.com is the official download host per the appcast feed
+  # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_stable_appcast.xml',
           checkpoint: 'cd00113cfce04e368dc0209d73d9bb07e46a97622eaf2611de02e2c6d8e21f31'
