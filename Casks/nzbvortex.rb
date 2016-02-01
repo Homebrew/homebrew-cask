0b638d8705ddb1cfa226bcf7e6109c4979e015a0
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 16:40:09 +0000

    updated nzbvortex (2.11.10)

--- Casks/nzbvortex.rb
+++ Casks/nzbvortex.rb
@@ -1,9 +1,8 @@
 cask 'nzbvortex' do
   version '2.11.10'
-  sha256 'dc4280c1bf4925a38e8609ce4c794e9d6a415645b285b085faccc59ddae55f59'
+  sha256 'bef3d0a6297bb4fff98361d4108061b7fff0c8d6ab1f2e2d88a56e67d9fa00b8'
 
-  # cloudfront.net is the official download host per the appcast feed
-  url "http://d3qv7p1umdiwb4.cloudfront.net/NZBVortex-#{version}.zip"
+  url 'http://www.nzbvortex.com/downloads/NZBVortex.dmg'
   appcast 'http://www.nzbvortex.com/update/appcast.xml',
           checkpoint: '3a90a126c430ed60df9ecf9cc2ae1cc2691bee4ab2c4ed3bafb402d725537416'
   name 'NZBVortex'
