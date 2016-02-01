e1d60a8509e65c55250848cccb672a25d9e1e708
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 00:17:55 +0000

    netnewswire.rb: added appcast verified comment

--- Casks/netnewswire.rb
+++ Casks/netnewswire.rb
@@ -3,6 +3,7 @@ cask 'netnewswire' do
   sha256 'b5240f104c23f93eacf05a900992db2cd4b3fcb8c2dbdc7a0a504dd4cb8fcb7b'
 
   url "http://cdn.netnewswireapp.com/releases/NetNewsWire-#{version}.zip"
+  # blackpixel.com verified as official when first introduced to the cask
   appcast 'https://updates.blackpixel.com/updates?app=nnw',
           checkpoint: 'cee1d039a2ffc06b54fb22d46af75f49ecbf8c48fd84068ff92aabcab48744c8'
   name 'NetNewsWire'
