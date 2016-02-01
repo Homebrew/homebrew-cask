6fd0f4151122feee482399b5b20f2a952c80b4d9
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 17:39:37 +0000

    updated daemon-tools-lite (4.1.227)

--- Casks/daemon-tools-lite.rb
+++ Casks/daemon-tools-lite.rb
@@ -1,9 +1,9 @@
 cask 'daemon-tools-lite' do
   version '4.1.227'
-  sha256 '5beeeaea798781c83470c88f0404c8156efe47597d254bc7f5c1edac894aa5ad'
+  sha256 '8ab533761f8acbfda91a1e5d9e25865a10e98d3c5bc0d61552a47e4247e0c4d0'
 
-  # web-search-home.com is the official download host per the appcast feed
-  url 'http://web-search-home.com/download/dtLiteMac'
+  # disk-tools.com was verified as official when first introduced to the cask
+  url "http://eu-uk7.disk-tools.com/56ce8f647dedb536a1b5106ff40d88ba/DAEMONTools-#{version.dots_to_hyphens}.dmg"
   appcast 'http://resources.web-search-home.com/xml/DAEMONToolsLite-appcast.xml',
           checkpoint: '48410ce70ce912cd51c297e22da0ebdb2cf4b97c6f330b61a9fe91d389ed2e9b'
   name 'DAEMON Tools Lite'
