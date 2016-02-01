bcbdd7b4b7e95dc422e5e6ce9f42276c4d7ee08f
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 17:04:16 +0000

    updated syncmate (6.2.279)

--- Casks/syncmate.rb
+++ Casks/syncmate.rb
@@ -1,9 +1,8 @@
 cask 'syncmate' do
   version '6.2.279'
-  sha256 '8c77958b8be6a903b393773c59694ee1aa14a4f1aea7dc86447f8b7f305015ef'
+  sha256 'afc1bc6a1ac0d3aa0784eec271c76e31ea8c885f68ce6ffc1aa6220376c93b98'
 
-  # eltima.com is the official download host per the appcast feed
-  url "http://www.eltima.com/download/syncmate-update/SyncMate_#{version}.zip"
+  url 'http://www.sync-mac.com/download/syncmate.dmg'
   appcast 'http://www.eltima.com/download/syncmate-update/syncmate6.xml',
           checkpoint: '116cf7df50ef73545c5dac6323e8f32d068ebd37740601d9bd4037b21b3538ff'
   name 'SyncMate'
