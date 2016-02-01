528247ed51415538c9f98c92917906c257d338c5
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 15:38:35 +0000

    updated foldingtext (2.1.1)

--- Casks/foldingtext.rb
+++ Casks/foldingtext.rb
@@ -2,8 +2,8 @@ cask 'foldingtext' do
   version '2.1.1'
   sha256 '7cdef677ccf9df331baf1650d2fded5ca305116c8cf74c7d8969318f2f7b9666'
 
-  # amazonaws.com is the official download host per the appcast feed
-  url "https://FoldingText.s3.amazonaws.com/FoldingText-#{version}.dmg"
+  # amazonaws.com/foldingtext was verified as official when first introduced to the cask
+  url 'https://s3.amazonaws.com/foldingtext/FoldingText.dmg'
   appcast 'https://foldingtext.s3.amazonaws.com/FoldingText.rss',
           checkpoint: 'bc16b2b5bf258bd70244286243231f6487248b676f708cc95e6d81d333df416d'
   name 'FoldingText'
