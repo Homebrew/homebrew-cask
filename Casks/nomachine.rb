06004b8fce20995b7330b336dcfc9a437f29c8f5
Daehyun You <daehyun9176@icloud.com>
2016-02-01 21:22:43 +0900

    update nomachine

--- Casks/nomachine.rb
+++ Casks/nomachine.rb
@@ -1,6 +1,6 @@
 cask 'nomachine' do
-  version '5.0.58_1'
-  sha256 '736fae5bf03f437ae3b518e12ab359a8be1fe73de037ea881b31cf5028883b85'
+  version '5.0.63_1'
+  sha256 'feb4261486ff13ba859239a41b096b2a32c335b7f7decc45d99b12b718e3c028'
 
   url "http://download.nomachine.com/download/#{version.split('.')[0..1].join('.')}/MacOSX/nomachine_#{version}.dmg"
   name 'NoMachine'
