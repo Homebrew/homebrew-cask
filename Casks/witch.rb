42777bff831e2b370096e8dd2dfef0bfa6a54158
Victor Popkov <victor@popkov.me>
2016-02-01 20:10:03 +0200

    witch.rb: upgrade to v3.9.7

--- Casks/witch.rb
+++ Casks/witch.rb
@@ -4,12 +4,12 @@ cask 'witch' do
     sha256 '5e46508e150ff16be14b9955abdcd15098376230ef71e3de6f15a056eec75e45'
     url "http://manytricks.com/download/witch/#{version}"
   else
-    version '3.9.6'
-    sha256 'b7f62879e740984a4515038f56cf706d9bcba617791bf21800edc72a5c3aeb08'
+    version '3.9.7'
+    sha256 '27862ad4de6c403c64849a834f3fde7efabe29e942d01e2204d4ee75d01fef5b'
 
     url 'http://manytricks.com/download/witch'
     appcast 'https://manytricks.com/witch/appcast.xml',
-            checkpoint: '36fc6fa7454af97645ec12d1ee76fed2ff3a4bf6339e931b257fe75bd46f5027'
+            checkpoint: '290d0c4ebb54bb54ef1bcb174b587ba31d2bdd43c59f22189b2aae7a3d0321d3'
   end
 
   name 'Witch'
