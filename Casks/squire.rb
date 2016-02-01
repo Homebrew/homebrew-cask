5197ca08d26cc91915880fe83f89539b04dfbabf
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 17:01:05 +0000

    updated squire (1.5.7)

--- Casks/squire.rb
+++ Casks/squire.rb
@@ -1,9 +1,9 @@
 cask 'squire' do
   version '1.5.7'
-  sha256 '3cfa37fa0a5cdf64d4a936d044b5fa35ef39c728db439d5e34d83795415c6297'
+  sha256 'a543f006ee04630397f1316a5599d5944445f0bf232239fa50c791a33cc387c9'
 
-  # amazonaws.com is the official download host per the appcast feed
-  url "https://s3.amazonaws.com/Squire_Mac_Builds/Squire_Mac_#{version.dots_to_underscores}.zip"
+  # amazonaws.com/squire was verified as official when first introduced to the cask
+  url 'https://s3.amazonaws.com/squire/builds/Squire.dmg'
   appcast 'http://www.sylion.com/squireapp/sparkle/SquireMac/appcastSquireMac.xml',
           checkpoint: '4f7ea01207cca2fa14f1b6c82e6eb35500715ddeaa15258a5dccdbb05f0edb72'
   name 'Squire'
