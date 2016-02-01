1a89bcd86722b0b3809db7f91207d3cc42aaaee4
adidalal <adidalal@users.noreply.github.com>
2016-01-31 23:29:29 -0500

    updated rowanj-gitx (0.15.1964)

--- Casks/rowanj-gitx.rb
+++ Casks/rowanj-gitx.rb
@@ -10,7 +10,7 @@ cask 'rowanj-gitx' do
     sha256 'd88bcb7f92ca1cdf31cb3f1d2e24c03e2091ab330319aeef2e770c0dbd6f7817'
   end
 
-  url "https://github.com/rowanj/gitx/releases/download/builds%2F#{version.sub(%r{\.\d+$}, '')}%2F#{version.sub(%r{.+\.}, '')}/GitX-dev-#{version.sub(%r{.+\.}, '')}.dmg"
+  url "https://github.com/rowanj/gitx/releases/download/builds/#{version.major_minor}/#{version.patch}/GitX-dev-#{version.patch}.dmg"
   appcast 'https://github.com/rowanj/gitx/releases.atom',
           checkpoint: 'b1e11ef3e13e74f84a51bfd6169db61f9c37fe5df851bb8c5f131c1981f8fdc0'
   name 'GitX-dev'
