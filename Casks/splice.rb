31f9f1ccf61d1588b67cbf921611c3e2cd2027b9
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 16:57:33 +0000

    updated splice (1.6.17-201601271754)

--- Casks/splice.rb
+++ Casks/splice.rb
@@ -2,7 +2,7 @@ cask 'splice' do
   version '1.6.17-201601271754'
   sha256 '5fd77ef925712240c083a0e38bb058dae946e466ab92c8fd991bd31f9214a5c4'
 
-  # amazonaws.com is the official download host per the appcast feed
+  # amazonaws.com/spliceosx was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/spliceosx/Splice.app-#{version}.zip"
   appcast 'https://splice.com/appcast.xml',
           checkpoint: '1b15ef5d54407968a29801a818b9ad0536de2fe9fd96e2bbcbe114171b44f2b3'
