357404d3bf70f442444ed97a8dd6ce45aa2929d3
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 15:30:59 +0000

    updated enjoy (1.2.1)

--- Casks/enjoy.rb
+++ Casks/enjoy.rb
@@ -2,7 +2,7 @@ cask 'enjoy' do
   version '1.2.1'
   sha256 '95f37aae546392697a55c3e72add68dadff67663ceba0b9f86815c867cc9e85d'
 
-  # amazonaws.com is the official download host per the appcast feed
+  # amazonaws.com/nongraphical was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/nongraphical/releases/Enjoy2-#{version}.zip"
   appcast 'https://s3.amazonaws.com/nongraphical/enjoy2/Enjoy2Appcast.xml',
           checkpoint: 'cf19c8a0dc17a555a5dc370bc77e8eafd5f3adfb5aeb33406e06fc8e638e0984'
