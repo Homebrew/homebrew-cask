edc354291285300a1bf8e9925b8d354538a065d1
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 17:07:46 +0000

    updated vienna (3.0.9)

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -2,7 +2,7 @@ cask 'vienna' do
   version '3.0.9'
   sha256 'f819bf8db030b03c1ca176d5e0927efe9aa3e42e3679039e878214c215276ba6'
 
-  # bintray.com is the official download host per the appcast feed
+  # bintray.com/viennarss was verified as official when first introduced to the cask
   url "https://dl.bintray.com/viennarss/vienna-rss/Vienna#{version}.tgz"
   appcast 'http://vienna-rss.org/changelog.xml',
           checkpoint: '13ba3fe7e53bffe51f7131fde39621b9a57ab2fca7c4dc4bbbb7c6a05035b364'
