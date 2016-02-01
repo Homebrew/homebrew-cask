83b866cca4d92788e3ffa2ee8a984a0e6db99406
Vítor Galvão <info@vitorgalvao.com>
2016-02-01 15:55:29 +0000

    updated identify (551)

--- Casks/identify.rb
+++ Casks/identify.rb
@@ -2,7 +2,7 @@ cask 'identify' do
   version '551'
   sha256 'b3691e56912882deb9d8021afa30643cc3e7dd9e4758d550a83649f491b9285a'
 
-  # dropbox.com is the official download host per the appcast feed
+  # dropboxusercontent.com/u/1495067 was verified as official when first introduced to the cask
   url "http://dl.dropbox.com/u/1495067/iDentifySparkle/iDentifyLite#{version}.zip"
   appcast 'https://dl.dropboxusercontent.com/u/1495067/iDentifySparkle/iDentifyAppcast.xml',
           checkpoint: '703a63799a0a8ed68a772bf9514779c45d02707690ac45fb4355a3fd666ddcba'
