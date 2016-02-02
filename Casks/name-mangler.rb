4791246ab637688cdfb5c00c4947c7b649f3bf7d
Victor Popkov <victor@popkov.me>
2016-02-02 19:43:08 +0200

    name-mangler.rb: upgrade to v3.3.5

--- Casks/name-mangler.rb
+++ Casks/name-mangler.rb
@@ -1,10 +1,10 @@
 cask 'name-mangler' do
-  version '3.3.4'
-  sha256 '7cef3b35a15f6a33a227870342adff64a4aedcd842a5de9d8c9ecfde8217a068'
+  version '3.3.5'
+  sha256 'db608bd7aef53d605b08da92b7167aba536686347c2972394d024f3fa480982c'
 
   url 'https://manytricks.com/download/namemangler'
   appcast 'https://manytricks.com/namemangler/appcast.xml',
-          checkpoint: '07b3d34b96bfbc008a7d1df5314abf97b404cf5d92a379ec13269585c7a7d30c'
+          checkpoint: '3f57c34d228dfa2a3aa6d9de76defd2e3d1b8075b6b71a22a98cc727eddbb231'
   name 'Name Mangler'
   homepage 'https://manytricks.com/namemangler/'
   license :freemium
