40a5e15e2a5546ad76997af65cc1ff0597b855a0
Vítor Galvão <info@vitorgalvao.com>
2016-02-02 23:42:59 +0000

    updated atom (1.4.3)

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,11 +1,11 @@
 cask 'atom' do
-  version '1.4.2'
-  sha256 'cd078a94ad61888f355f2c241f0531f23a5765bbc1f2b45607ce8f878e19ce99'
+  version '1.4.3'
+  sha256 '8ede5588b4760e120c546d5a4d61fe8d21059b31ea6fcc4eae7932b5cb09964b'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
   appcast 'https://github.com/atom/atom/releases.atom',
-          checkpoint: '03180e9807e794c9216564a5608130af24c5ec72f964c47957551f7d0819cbc4'
+          checkpoint: '8e5c44d6f35060f74bf352ef898fea7c394fea554ac1e89cf33bef3cc9624f1f'
   name 'Github Atom'
   homepage 'https://atom.io/'
   license :mit
