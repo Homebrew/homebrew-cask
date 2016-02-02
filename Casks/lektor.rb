b312bf06f9004291d48e0bf595247b1da766df1b
Victor Popkov <victor@popkov.me>
2016-02-02 19:41:50 +0200

    lektor.rb: upgrade to v1.2

--- Casks/lektor.rb
+++ Casks/lektor.rb
@@ -1,11 +1,11 @@
 cask 'lektor' do
-  version '1.1'
-  sha256 'bb284bd3f92830ff0603de057a8897f5b575a8ed0bd1cb937b2be95f15cf6262'
+  version '1.2'
+  sha256 'f27aa608d015d233788f76faeb478554b7a01e4d9c2ab68359a4d969507d34b3'
 
   # github.com/lektor/lektor verified as official when first introduced to the cask
   url "https://github.com/lektor/lektor/releases/download/#{version}/Lektor-#{version}.dmg"
   appcast 'https://github.com/lektor/lektor/releases.atom',
-          checkpoint: 'eeb07104f9eda7a2740fe15a4b2d5bb2cf1b08ae335240cacc4cf3accc5e1083'
+          checkpoint: '628010063189f2778707f86a70f6eac4bf101600e98fd9fef1e35e05f5c89751'
   name 'Lektor'
   homepage 'https://www.getlektor.com/'
   license :oss
