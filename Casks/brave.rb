cfed92a0acecd08a30a04db52d71fd9f37551e2c
Vítor Galvão <info@vitorgalvao.com>
2016-02-02 23:47:53 +0000

    updated brave (0.7.12dev)

--- Casks/brave.rb
+++ Casks/brave.rb
@@ -1,11 +1,11 @@
 cask 'brave' do
-  version '0.7.10dev'
-  sha256 '92d21d5223369424baff859b5a38bb72d8b5a7c0b9b4ecf7242455047da64d5a'
+  version '0.7.12dev'
+  sha256 'd021bafa91e7f22e6465a69ca248d69a61e5b5565a0e44d56038ab7ea8326742'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/brave/browser-laptop/releases/download/v#{version}/Brave.dmg"
   appcast 'https://github.com/brave/browser-laptop/releases.atom',
-          checkpoint: '8daf6593e19465b868b0bc708d9536bf48cc1d1a3c77469f24971d9c482f4708'
+          checkpoint: '00ecd696c5bd898b8f8db7e829b0a37c5b97778382f5aebd1d0cad16aa789a8b'
   name 'Brave'
   homepage 'http://brave.com'
   license :mpl
