b433618814d9e2aae41843c8db57bd1fe5e95ae7
adidalal <adidalal@users.noreply.github.com>
2016-02-02 14:47:39 -0500

    updated navicat-for-sqlite (11.2.7)

--- Casks/navicat-for-sqlite.rb
+++ Casks/navicat-for-sqlite.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-sqlite' do
-  version '11.2.6'
-  sha256 '0e28fc07c862413576ad55b97cbafee25109ae62a0ee5cf233794ad5fb5f148a'
+  version '11.2.7'
+  sha256 'cb90b94433efcd58da3d5c1c0738d5ec888d7b4f6ec483d2d0e3a793e1d39a92'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlite_en.dmg"
   name 'Navicat for SQLite'
