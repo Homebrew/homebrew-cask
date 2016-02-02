228396ad4cbe5c6b10af74a938244e4c6c60fe04
Vítor Galvão <info@vitorgalvao.com>
2016-02-02 19:55:59 +0000

    updated navicat-for-oracle (11.2.7)

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-oracle' do
-  version '11.2.6'
-  sha256 '1e29b0d87ac45e9a6328ac92008b15c2311aca87a0ab1f6d2949db64af07a736'
+  version '11.2.7'
+  sha256 '0cc4ec43d3f0f05af62cd9ffa28eda120e977bd1de65e866c1870d8196aafdd5'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
   name 'Navicat for Oracle'
