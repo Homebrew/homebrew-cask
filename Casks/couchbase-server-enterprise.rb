17619022cbd35e83859fc60c5807c62cba74d8e1
Vítor Galvão <info@vitorgalvao.com>
2016-02-02 21:58:41 +0000

    updated couchbase-server-enterprise (4.1.0)

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -3,8 +3,6 @@ cask 'couchbase-server-enterprise' do
   sha256 '237837598a1bf663debaea5c909bce36a39fbc673139cf4d4ebc55fb5c276313'
 
   url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
-  appcast 'http://appcast.couchbase.com/membasex.xml',
-          checkpoint: '92ed75092bb887de84b8e2204a44794e18992f8191383cc32d60eae68bf44cd3'
   name 'Couchbase Server'
   homepage 'http://www.couchbase.com/'
   license :apache
