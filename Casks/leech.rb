7a2c69b37dbdf995be4c46b0959706dfea41e1fd
Victor Popkov <victor@popkov.me>
2016-02-02 19:40:46 +0200

    leech.rb: upgrade to v2.2.3

--- Casks/leech.rb
+++ Casks/leech.rb
@@ -1,10 +1,10 @@
 cask 'leech' do
-  version '2.2.2'
-  sha256 '5759223aa3ece0ed844975a470fa347a1e231471912a48d6c826070595074a08'
+  version '2.2.3'
+  sha256 '83ee1e8ddf62d5d462e7c953eff3f36220053518c7edfee1fcef78a8d13c47f0'
 
   url 'http://manytricks.com/download/leech'
   appcast 'https://manytricks.com/leech/appcast.xml',
-          checkpoint: 'ef4c85840f37dc92c0999f88e5e64acc5eb76b3e3933582710cdb703583473a1'
+          checkpoint: 'b24601aedbebbdc4d51f6bec40cfa567413a6cb34c83653af358f33f825b3de8'
   name 'Leech'
   homepage 'https://manytricks.com/leech/'
   license :commercial
