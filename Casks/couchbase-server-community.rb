15b43b9cc7ee9737214e6f109ade35be4f398133
Vítor Galvão <info@vitorgalvao.com>
2016-02-02 21:56:08 +0000

    updated couchbase-server-community (4.0.0)

--- Casks/couchbase-server-community.rb
+++ Casks/couchbase-server-community.rb
@@ -5,12 +5,10 @@ cask 'couchbase-server-community' do
     sha256 'ff994c89abf2efcc7dafa6753cfc25c821f3cc24935dccb7a979d170ced56162'
     url "http://packages.couchbase.com/releases/#{version}/couchbase-server-community_#{version}-macos_x86_64.zip"
   else
-    sha256 '26edea946d845a96e21e887348f853dff0a632b7bf6d42e9d559b647855648f6'
+    sha256 'ce39892b66c06d153004474e273d5775515710e7b073269f75637bf2c4f1f2c4'
     url "http://packages.couchbase.com/releases/#{version}/couchbase-server-community_#{version}-elcapitan_x86_64.zip"
   end
 
-  appcast 'http://appcast.couchbase.com/membasex.xml',
-          checkpoint: '92ed75092bb887de84b8e2204a44794e18992f8191383cc32d60eae68bf44cd3'
   name 'Couchbase Server'
   homepage 'http://www.couchbase.com/'
   license :apache
