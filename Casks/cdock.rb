f6693142c8087d070c297ecc7b9933b96e5ebbe0
Vítor Galvão <info@vitorgalvao.com>
2016-02-02 23:46:35 +0000

    updated cdock (0.10.1)

--- Casks/cdock.rb
+++ Casks/cdock.rb
@@ -1,10 +1,10 @@
 cask 'cdock' do
-  version '0.9.11'
-  sha256 'da1657f0c92dac0b3d12c129f420b53471231da180d2ef2b6019ce5665f91b18'
+  version '0.10.1'
+  sha256 'b3e9504303ce97560c131a36795a357b5823d46a1b22e3b3fbea5a10bcf6a99a'
 
-  url "https://github.com/w0lfschild/cDock2/releases/download/v#{version}/cDock.zip"
+  url "https://github.com/w0lfschild/cDock2/releases/download/v#{version}/release.zip"
   appcast 'https://github.com/w0lfschild/cDock2/releases.atom',
-          checkpoint: '8b23380512d766d9773b145642876ab353987f2efe47efa2b42168ba7154aab3'
+          checkpoint: 'c4ba3d7000a0db21d5dd9486abd3b52047438aa2b3a39b914c9c3edc8c0dbe5c'
   name 'cDock2'
   homepage 'https://w0lfschild.github.io/cdock'
   license :bsd
