289fd150d2d0eaa3eaa92a44f3bba68dfdf16e38
Victor Popkov <victor@popkov.me>
2016-02-02 20:14:37 +0200

    tg-pro.rb: upgrade to v2.9.7

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,10 +1,10 @@
 cask 'tg-pro' do
-  version '2.9.5'
-  sha256 '6f8cb96bf91ee3a69a5f91c47232427826e5516355d45656c4be9118d5c5b78a'
+  version '2.9.7'
+  sha256 '86a3414079d543738e2664f4707d6fd774dc8de07d1b5451a4f40159d10367f3'
 
   url "https://www.tunabellysoftware.com/resources/TGPro_#{version.dots_to_underscores}.zip"
   appcast 'https://www.tunabellysoftware.com/resources/sparkle/tgpro/profileInfo.php',
-          checkpoint: 'b36f84edfaf65508dba094649cac418bf527ee486552f2d0f5a883df1a23227e'
+          checkpoint: '4041f67a14d80be892907254862110161ca3a9f487b292b07b192faedeb0d9ae'
   name 'TG Pro'
   homepage 'https://www.tunabellysoftware.com/tgpro/'
   license :commercial
