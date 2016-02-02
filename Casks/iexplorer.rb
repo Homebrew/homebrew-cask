ff11e79aa423140b33abfed4270e2a9b6c8adf9a
Victor Popkov <victor@popkov.me>
2016-02-02 19:38:31 +0200

    iexplorer.rb: upgrade to v3.9.2.0

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,10 +1,10 @@
 cask 'iexplorer' do
-  version '3.9.1.0'
-  sha256 '45c0785f2c0929124a9a70fd371adbcee7632610377a8ade91e260c3492fd675'
+  version '3.9.2.0'
+  sha256 'f66a2df781dcfc6b607392fe989b8bf62d4ef7a00984f692be844eb899593f86'
 
   url "https://www.macroplant.com/release/iExplorer-#{version}.dmg"
   appcast 'https://www.macroplant.com/iexplorer/ie3-appcast.xml',
-          checkpoint: '889b61b57a876c2091077fe79fe2eca1683cf0c8a7649d66238e1662e9cbdc94'
+          checkpoint: 'e7ca248391679847b7995f7a2b23be7fd26947f0da53a12eaf6792e44c91d19e'
   name 'iExplorer'
   homepage 'https://www.macroplant.com/iexplorer/'
   license :freemium
