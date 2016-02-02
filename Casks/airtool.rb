91e507fcd20e6eba538d35eedb88757ac24235ae
Vítor Galvão <info@vitorgalvao.com>
2016-02-02 23:40:25 +0000

    updated airtool (1.2.2)

--- Casks/airtool.rb
+++ Casks/airtool.rb
@@ -1,11 +1,11 @@
 cask 'airtool' do
-  version '1.2.1'
-  sha256 '3bd3677cdf65b20d93e24c10908c5cdf1231f59696baa72af0919cf489a70f9e'
+  version '1.2.2'
+  sha256 'faa93364724f3544be52f4bec04836a58cc63fd94b6b6f03424058c4e61ee076'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "https://s3.amazonaws.com/airtool/airtool_#{version}.pkg"
-  appcast 'http://www.adriangranados.com/appcasts/airtoolcast.xml',
-          checkpoint: 'b8d00edbe68c31a79b2ba4ae74f810619544df39b460dad5570e5f3aaae485f9'
+  url "https://s3.amazonaws.com/adriangranados/airtool_#{version}.pkg"
+  appcast 'https://www.adriangranados.com/appcasts/airtoolcast.xml',
+          checkpoint: '9744ed49ec6bf20d5eb90bdc0fffdc79383f4660a6ba22effd4399b5297feb36'
   name 'Airtool'
   homepage 'http://www.adriangranados.com/apps/airtool'
   license :gratis
