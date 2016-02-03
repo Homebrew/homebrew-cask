f41c6b6569f946f9b4d060a51bc52d58d939401d
Victor Popkov <victor@popkov.me>
2016-02-03 19:25:57 +0200

    screenhero.rb: upgrade to v2.3.5.0

--- Casks/screenhero.rb
+++ Casks/screenhero.rb
@@ -1,10 +1,10 @@
 cask 'screenhero' do
-  version '2.3.4.0'
-  sha256 '6f9e5efae3b37fb34488e3a817b9e0ad7613401332e45bec86e1c5068f4aef4d'
+  version '2.3.5.0'
+  sha256 '2d87dfa787abf96e1a398e9bf7fb3034cd95d583cb28d82b41a2a95c668474a4'
 
   url "https://secure.screenhero.com/update/screenhero/Screenhero-#{version}.dmg"
   appcast 'https://d3hb26arjl8wb7.cloudfront.net/jsherwani/public/update/mac/screenhero/sparkle.xml',
-          checkpoint: '06562363da8ceb90c9dd977171c836330c88372d06927b6940fe59264f37d87f'
+          checkpoint: '317b6317e3fd44380b3ddb989cf561c043af0c23168ec033d60607c20601b2aa'
   name 'Screenhero'
   homepage 'https://screenhero.com/'
   license :commercial
