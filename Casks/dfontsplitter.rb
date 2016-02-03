38194f20079fbce8017d4b73c3b35c5d725f4dfc
Vítor Galvão <info@vitorgalvao.com>
2016-02-03 00:13:30 +0000

    updated dfontsplitter (0.4.2)

--- Casks/dfontsplitter.rb
+++ Casks/dfontsplitter.rb
@@ -1,10 +1,10 @@
 cask 'dfontsplitter' do
-  version '0.4.1'
-  sha256 'd9345ffe0e9be0c096e65114be21fc3aac676a9a3a53e84f1d6cebff0b7796b3'
+  version '0.4.2'
+  sha256 '6297da5d4f6d2f8d92eb35364178ea7972a54df7576dbd801cc483a88a7d49ad'
 
   url "https://peter.upfold.org.uk/files/dfontsplitter/dfontsplitter-#{version}-mac.zip"
   appcast 'https://apps.upfold.org.uk/appupdate/dfontsplitter.xml',
-          checkpoint: '8dfb0eca113f48d162fc12aae8c7b2c77d62f9958491bd8d6a3c5c9881567aba'
+          checkpoint: 'ead408294b7d718d1e30c6963492889a5b4cc07fef47fcc8c7c30f913c092664'
   name 'DfontSplitter'
   homepage 'https://peter.upfold.org.uk/projects/dfontsplitter'
   license :gpl
