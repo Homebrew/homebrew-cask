a3908b7bc3f61384ef7a3883f96e44f2bb52d55c
Vítor Galvão <info@vitorgalvao.com>
2016-02-03 00:22:29 +0000

    updated macbreakz (5.25)

--- Casks/macbreakz.rb
+++ Casks/macbreakz.rb
@@ -1,10 +1,10 @@
 cask 'macbreakz' do
-  version '5.24'
-  sha256 '854d33c9bf5db181d433106e5972df019f414c34b7e1bb87fd1b2381eef88b56'
+  version '5.25'
+  sha256 '75f7ab47d5b956f89d1bf1dac85e7f021b66c0ff9bbbb47c6f05203923c95b3e'
 
   url "http://www.publicspace.net/download/MacBreakZ#{version.major}.dmg"
   appcast "http://www.publicspace.net/app/signed_mb#{version.major}.xml",
-          checkpoint: '17d2ef73b30c1e2bc01f2287888d2a8cdd61f552822b205b59e9783c8b470143'
+          checkpoint: '49d25a687e02902434f65518b2d2c5a81b22175bf3835eebd155ba4eac591a95'
   name 'MacBreakZ'
   homepage 'http://www.publicspace.net/MacBreakZ/'
   license :commercial
