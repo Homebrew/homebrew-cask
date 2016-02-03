815cfecb4ad4baed427de60ca12271092d7fa4ec
Vítor Galvão <info@vitorgalvao.com>
2016-02-03 00:44:32 +0000

    updated xamarin-ios (9.4.1.25)

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-ios' do
-  version '9.4.1.24'
-  sha256 'ae740f835ac963fab80858fddbd382c5f4b747352180782da1d09aec6a9316f6'
+  version '9.4.1.25'
+  sha256 'b47d0e42bebfc02e7c85d7cedbedbe83d31af9d8b90d38839c745c98fde2c4ab'
 
   url "https://download.xamarin.com/MonoTouch/Mac/monotouch-#{version}.pkg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '477b5e7a151c72b77987ea715278ff32bf7139a1829ff4deb20e67e2d35c7dd3'
+          checkpoint: '855d95a330efdd9cc13cbaabc6e49dfcda111db3b16cbc6345e3a0d17e769972'
   name 'Xamarin.iOS'
   homepage 'https://xamarin.com/ios'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
