5d53e2eec31b5b9326bc07c0cbc103a6b5f606ce
Vítor Galvão <info@vitorgalvao.com>
2016-02-03 00:35:11 +0000

    updated raptor (1.0)

--- Casks/raptor.rb
+++ Casks/raptor.rb
@@ -1,10 +1,10 @@
 cask 'raptor' do
-  version '1.0rc6'
-  sha256 '52f405602b25d9fa490e6aefac545d3d42cbad025959bce687c79c9d76410960'
+  version '1.0'
+  sha256 '66ec547bfb6eb8ecc2fcb2932631c75c9142515201e4079152b51ed005657f3a'
 
-  url "https://github.com/Raptor-Fics-Interface/Raptor/releases/download/#{version}/Raptor_v#{version}_OS_X_Cocoa_x86_64.dmg"
+  url "https://github.com/Raptor-Fics-Interface/Raptor/releases/download/v#{version}/Raptor_v#{version}_OS_X_Cocoa_x86_64.dmg"
   appcast 'https://github.com/Raptor-Fics-Interface/Raptor/releases.atom',
-          checkpoint: '8546dbd018f4cd6a639f3dd82702b926341034dfa8f1d00ed3d872b1ad10a954'
+          checkpoint: '0f631ed3d1dafea70b65dc0427b4e2ec3d6743eb98e8be187d7fc4abaef39ff0'
   name 'Raptor Chess Interface'
   homepage 'https://github.com/raptor-fics-interface/raptor'
   license :bsd
