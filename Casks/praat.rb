4464d2215e5698e10c55918df7c09bd8e51bc777
Vítor Galvão <info@vitorgalvao.com>
2016-02-03 00:29:58 +0000

    updated praat (6.0.13)

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,16 +1,16 @@
 cask 'praat' do
-  version '6.0.12'
+  version '6.0.13'
 
   if Hardware::CPU.is_32_bit? || MacOS.release <= :snow_leopard
     sha256 'f82242cc2fbc07ae5b9218e7707f842f0a428b0db019db04d0a0936dcb4c0298'
     url "http://www.fon.hum.uva.nl/praat/praat#{version.no_dots}_mac32.dmg"
   else
-    sha256 'aa2033c39efd1c7076e949564e156b6cb2e805a6f5aba4e3710cd2e6b0a83dfb'
+    sha256 'c57346104bb04902315c578bcd10c226853dc0e760ac1d09d4a7ba596f0c4ebc'
     url "http://www.fon.hum.uva.nl/praat/praat#{version.no_dots}_mac64.dmg"
   end
 
   appcast 'https://github.com/praat/praat/releases.atom',
-          checkpoint: '63dab7096cf6d3de630dc8a7affb49da35128b2e54e2e02e16c76ded3975f005'
+          checkpoint: '962c0be2b39dbe200c8f7c2c2c46c63e08f7a5581b5dd3f92b08193e841e48c8'
   name 'Praat'
   homepage 'http://www.fon.hum.uva.nl/praat/'
   license :gpl
