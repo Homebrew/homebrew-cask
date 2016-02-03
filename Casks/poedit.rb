35f4c34b6d059950cd29ffb6482fbcc38f08ec85
Victor Popkov <victor@popkov.me>
2016-02-03 19:19:54 +0200

    poedit.rb: upgrade to v1.8.7

--- Casks/poedit.rb
+++ Casks/poedit.rb
@@ -5,12 +5,12 @@ cask 'poedit' do
 
     url "https://poedit.net/dl/poedit-#{version}.dmg"
   else
-    version '1.8.6'
-    sha256 'ca51faff5a818e4bb1fab96055d1f1b5e45ca1c5e84110400397b6f57dc49dcf'
+    version '1.8.7'
+    sha256 '10abc4201db285e0772e165e263ffd82e799b28f5a807d27a54884306b8f9845'
 
     url "https://download.poedit.net/Poedit-#{version}.zip"
     appcast 'https://poedit.net/updates/osx/appcast',
-            checkpoint: '5a83d975d617f0ffb39355c7cda82976b464f604cb45e58e512dca22b0f97589'
+            checkpoint: '77d211625afe4d76bcc78f1a79fa2e9c975f41275ad4259b1eea10ca9485542f'
   end
 
   name 'Poedit'
