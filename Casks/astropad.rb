06ea88868e69c73671866e8a882aee8fdccd8e2e
Victor Popkov <victor@popkov.me>
2016-02-04 23:02:47 +0200

    astropad.rb: upgrade to v1.3.6

--- Casks/astropad.rb
+++ Casks/astropad.rb
@@ -1,10 +1,10 @@
 cask 'astropad' do
-  version '1.3.5'
-  sha256 'd3f85f8a565d8335928ae1f4ea20be525de0f203ac0b323576d34d7958d7e41a'
+  version '1.3.6'
+  sha256 '850f32f183e1c48d0456becfa57e7ae266676b05283327440ee67ece08d6282a'
 
   url "http://astropad.com/downloads/Astropad-#{version}.zip"
   appcast 'http://astropad.com/downloads/sparkle.xml',
-          checkpoint: '9863ff6ac45b686f673c9eb2ffa4c65eb4ec0370e9fd0904c36d1c2b79f9d804'
+          checkpoint: '4f4ee6ec91eede8c8d405cdeda30f406374fe9e3a264958ea7e0612e10291021'
   name 'Astropad'
   homepage 'http://astropad.com/'
   license :gratis
