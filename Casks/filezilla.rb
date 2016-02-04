aef0ad1da4a17d0901a03d5b0c69b551e466d34f
Florian Harbecke <florian@harbecke.me>
2016-02-04 14:31:05 +0100

    Updated Filezilla.app to v3.15.0.1

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -3,8 +3,8 @@ cask 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.15.0'
-    sha256 '05372ae7f8e88bcb7a9514a288ca2afbab25960fb538278c6c75b606ad8cc2ff'
+    version '3.15.0.1'
+    sha256 '26aba2d8098096bcef32fa48b2ba48c1c6a3f16560f59a7b951f5a9d6cf98f7e'
   end
 
   # sourceforge.net is the official download host per the vendor homepage
