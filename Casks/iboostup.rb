8257256ac638eddf6101f13550817333024ad52d
Victor Popkov <victor@popkov.me>
2016-02-05 20:18:50 +0200

    iboostup.rb: upgrade to v5.3.2

--- Casks/iboostup.rb
+++ Casks/iboostup.rb
@@ -1,10 +1,10 @@
 cask 'iboostup' do
-  version '5.2'
-  sha256 'aede9188f3e7bebecd6a9fd56485d7727de0bfdeef143af0a62aadc03e25de2d'
+  version '5.3.2'
+  sha256 'd1356b575836690b69a81ce592b442aedda9bdc09f56852830fe14ed46ff1802'
 
   url 'https://www.iboostup.com/iboostup.dmg'
   appcast 'https://www.iboostup.com/updates',
-          checkpoint: 'ba17c4c865240d48cb077deec1d33dbf279f85f02693352baa39908c18b15f9f'
+          checkpoint: '63fb737ac730a7bc770a2ba5a335612ce5eb93bec8421d4a13fa403d438e605a'
   name 'iBoostUp'
   homepage 'https://www.iboostup.com/'
   license :freemium
