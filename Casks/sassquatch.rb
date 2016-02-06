836e121f9ce0eaed47c48764dca84f185bd588c0
adidalal <adidalal@users.noreply.github.com>
2016-02-06 18:16:46 -0500

    sassquatch: add appcast

--- Casks/sassquatch.rb
+++ Casks/sassquatch.rb
@@ -3,6 +3,8 @@ cask 'sassquatch' do
   sha256 '9f9c688e473cc8fdffbee38c5d8f876726620a42dacdd6c2996faadfab714612'
 
   url "https://thoughtbot.github.io/sassquatch/download/v#{version}/Sassquatch.zip"
+  appcast 'https://thoughtbot.github.io/sassquatch/feed.xml',
+          checkpoint: 'fd579aaf6f76be00a8d491628e99027fa0a4b033eb8d759fda2837c5536b4258'
   name 'Sassquatch'
   homepage 'http://sassquatch.thoughtbot.com'
   license :commercial
