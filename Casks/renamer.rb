52c55d5b900c9bea57fe255050a8df1f5f81933d
Victor Popkov <victor@popkov.me>
2016-02-07 21:21:52 +0200

    renamer.rb: upgrade to v4.4.1

--- Casks/renamer.rb
+++ Casks/renamer.rb
@@ -1,11 +1,11 @@
 cask 'renamer' do
-  version '4.4.0'
-  sha256 '9a8fa35e77831b81646b2e84a1b80448362aacb41e6e4be797a88933fb746974'
+  version '4.4.1'
+  sha256 'cc2e4ab9c2074311c8e6cb10662f7e7d2d95f9a8d1bfcd5dc712ab4cc81b782a'
 
   # creativebe.com is the official download host per the vendor homepage
   url 'http://creativebe.com/download/renamer'
   appcast 'https://storage.googleapis.com/incrediblebee/appcasts/renamer.xml',
-          checkpoint: '8b69a9063721e4cd8998e2cfc5701d7c72093b914f6d072c177f1c69c393f412'
+          checkpoint: '1bfd4de2d52080574682e11a22060fbfd7cccfb978e48ab1b996133629cfd500'
   name 'Renamer'
   homepage 'http://renamer.com'
   license :commercial
