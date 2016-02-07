7499529ba1742363e8e40e40233c88e52253f9d2
Victor Popkov <victor@popkov.me>
2016-02-07 20:18:51 +0200

    controlplane.rb: upgrade to v1.6.2

--- Casks/controlplane.rb
+++ Casks/controlplane.rb
@@ -3,11 +3,11 @@ cask 'controlplane' do
     version '1.2.3'
     sha256 '37f93d3a3a17a6e2f24447f0bc74c7e89ec1581ca52e5970960544858c86f909'
   else
-    version '1.6.1'
-    sha256 '945ae102b81b838df65edf6f83292d33399218113e67bdfdaaa088c0f219ea47'
+    version '1.6.2'
+    sha256 '3464571449fa95776c79e32c9f5b20d7528d8969a9905b04e4248ce505143168'
 
     appcast 'http://www.controlplaneapp.com/appcast.xml',
-            checkpoint: 'c4b1f509972a926c73775eed1feab651348c557fe7cb383a3f50bd7cd390aa9f'
+            checkpoint: 'a3abb9d2ae2daed7551c70a2953a235ee847055afaa2b0e7aa1471060f2c44e6'
   end
 
   url "http://www.controlplaneapp.com/download/#{version}"
