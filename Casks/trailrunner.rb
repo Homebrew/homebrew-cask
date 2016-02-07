4086344d3729c90d8783ef0cb1d88d7f19b37f60
Victor Popkov <victor@popkov.me>
2016-02-07 21:23:34 +0200

    trailrunner.rb: upgrade to v3.8.802

--- Casks/trailrunner.rb
+++ Casks/trailrunner.rb
@@ -1,10 +1,10 @@
 cask 'trailrunner' do
-  version '3.8.800'
-  sha256 '3c7ee91e11e5c870096352c71b2c6b6cb8369fdcf617282e343045551ba34f54'
+  version '3.8.802'
+  sha256 '55d45bacf2b432aab4bc9fb2f4b34a7afd33604a08e615270ab06b824708416a'
 
   url 'http://downloads.trailrunnerx.com/TrailRunner.app.zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/83c4086e3f968b874757ba689e71f610',
-          checkpoint: '589a8da19b6d9fc89778ff9dbff68be4797160ea2b5b9d053a524ac1aebd516c'
+          checkpoint: 'b4e1ce1a679064f600b91a862cf80390c1f90366983d5d72f8da606570a40369'
   name 'TrailRunner'
   homepage 'http://trailrunnerx.com/'
   license :gratis
