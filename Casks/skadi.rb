28f513bbc47735377c8218a0a9b6870eaf3bf6df
Victor Popkov <victor@popkov.me>
2016-02-07 21:23:10 +0200

    skadi.rb: upgrade to v1.1.0

--- Casks/skadi.rb
+++ Casks/skadi.rb
@@ -1,10 +1,10 @@
 cask 'skadi' do
-  version '1.0.0'
-  sha256 '26c08d7061bbca81bb8f23d83563465ae92422d51d2d63b73d20ad945a43b1d4'
+  version '1.1.0'
+  sha256 '32a76f2d48d613c98dbedafd2c4904dde582d843100e6dfb1e860407e9c5392c'
 
   url "https://github.com/redcapital/skadi/releases/download/v#{version}/skadi-v#{version}.zip"
   appcast 'https://github.com/redcapital/skadi/releases.atom',
-          checkpoint: 'b00910f5e8c35f0a86770fe939be395d38e2d59375882d7d52e240abfcb6d829'
+          checkpoint: '40d6e8f3a8d9382565bff1dc740497f5ccbad208962a6810b9a232e071e259d2'
   name 'skadi'
   homepage 'https://github.com/redcapital/skadi'
   license :gpl
