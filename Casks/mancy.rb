ccdc5ed5949b23036f18aac86d85d1c1cc266cfb
Victor Popkov <victor@popkov.me>
2016-02-07 21:14:44 +0200

    mancy.rb: upgrade to v2.2.1

--- Casks/mancy.rb
+++ Casks/mancy.rb
@@ -1,11 +1,11 @@
 cask 'mancy' do
-  version '2.2.0'
-  sha256 '50ad7a9016dc2436e70f86612b270c79795982eafe21a58071702f6313471f5a'
+  version '2.2.1'
+  sha256 '23e7f2c611a7762fd091e594ad8b559c02f111e13a5153cf68f47c3805840ed6'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/princejwesley/Mancy/releases/download/v#{version}/Mancy-darwin-x64.zip"
   appcast 'https://github.com/princejwesley/Mancy/releases.atom',
-          checkpoint: '013fc463156d88a8def8a80ff5468cf7c267d465f6c9456ea66d3bf2a9777c87'
+          checkpoint: '5c0778c413211456808d9f12028237ccae54c593f6264caea916ac4020cf2db7'
   name 'Mancy'
   homepage 'http://www.mancy-re.pl/'
   license :mit
