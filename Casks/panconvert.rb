56838369d18380654e63b9c168238461574aea62
adidalal <adidalal@users.noreply.github.com>
2016-02-08 18:59:04 -0500

    updated panconvert (0.1.5)

--- Casks/panconvert.rb
+++ Casks/panconvert.rb
@@ -2,9 +2,9 @@ cask 'panconvert' do
   version '0.1.5'
   sha256 '98ab46e9ac375b155d9361480e2d180fc2caf9905d55a64ea2faca3873b80ff9'
 
-  url "http://downloads.sourceforge.net/sourceforge/panconvert/PanConvert-v#{version}.dmg"
+  url "http://downloads.sourceforge.net/sourceforge/panconvert/PanConvert-#{version}.dmg"
   appcast 'https://raw.githubusercontent.com/apaeffgen/PanConvert/master/docs/changelog.md',
-          checkpoint: 'cbe74ea166203c4e72776556b49c667b33f38e5d23260cb606549d593c432a7f'
+          checkpoint: '619da3f895d1553f9a34f92385be79c158a6418ef8b3650af915586f9d7f81f5'
   name 'PanConvert'
   homepage 'http://panconvert.sourceforge.net'
   license :gpl
