d7ae0a3f283869e0da10537d1260ab03e77cb898
adidalal <adidalal@users.noreply.github.com>
2016-02-08 18:47:02 -0500

    updated mojibar (2.1.1)

--- Casks/mojibar.rb
+++ Casks/mojibar.rb
@@ -4,7 +4,7 @@ cask 'mojibar' do
 
   url "https://github.com/muan/mojibar/releases/download/#{version}/mojibar.zip"
   appcast 'https://github.com/muan/mojibar/releases.atom',
-          checkpoint: 'd00ef87055fa8a77db8d8a5829a6e7780555a71e59d71111ffc51cc54ee93f50'
+          checkpoint: '3186e269c95fc59d7b0b39785e6608ba35a4a399796be0ed22e8a08b11ee0922'
   name 'Mojibar'
   homepage 'https://github.com/muan/mojibar'
   license :mit
