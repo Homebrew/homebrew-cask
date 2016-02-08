7891709857654cec9ef6c8aa7c359a71c90bc3c1
adidalal <adidalal@users.noreply.github.com>
2016-02-08 17:37:58 -0500

    updated macvim (7.4.96)

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,10 +1,10 @@
 cask 'macvim' do
-  version '7.4.95'
-  sha256 '224f1381ee7763e946db6b009a2f5e722272f8e5227731ebf0b4f3b13373787b'
+  version '7.4.96'
+  sha256 'd9160cf89793c052af6cfd7dbc7ac168ad8e9d4c0adf054e9d5a400454924191'
 
   url "https://github.com/macvim-dev/macvim/releases/download/snapshot-#{version.patch}/MacVim.dmg"
   appcast 'https://github.com/macvim-dev/macvim/releases.atom',
-          checkpoint: '56c8cb7d9735e720b49aa9c2a25cc0fe5346d7026382d4d72310cb02284c6392'
+          checkpoint: 'f37ea7e39f94394d5c4912faf474ecf57998ba2a1ac8f9b159850e3808e562d6'
   name 'MacVim'
   homepage 'https://github.com/macvim-dev/macvim'
   license :oss
