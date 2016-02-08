fd901cf543ff2413bc7b335562490011a9e06bd1
adidalal <adidalal@users.noreply.github.com>
2016-02-08 17:33:43 -0500

    updated kitematic (0.10.0)

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,11 +1,11 @@
 cask 'kitematic' do
-  version '0.9.6'
-  sha256 '70572ca6a203acd49f357d9d6c946528a133333a95326b8342d00b5125c2b352'
+  version '0.10.0'
+  sha256 '1076567a11a4f5ae3e22b3fcfaa1c07b9e9198741d591df03a06dcbcaaef95d8'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/kitematic/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
   appcast 'https://github.com/kitematic/kitematic/releases.atom',
-          checkpoint: '0c57902f605889c8dc67f30fd04645a7c8f1bd5faf2241267b346199fd20570b'
+          checkpoint: 'd11c62240f141319388bd7b94e8148c83e00f2497350ed5a5d30b3ebbb7baf7f'
   name 'Kitematic'
   homepage 'https://kitematic.com/'
   license :apache
