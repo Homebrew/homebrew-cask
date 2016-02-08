381618b9751e897eee9389a6532ce3acac4fad8b
adidalal <adidalal@users.noreply.github.com>
2016-02-08 16:59:20 -0500

    updated boxcryptor (2.1.465.708)

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -5,7 +5,7 @@ cask 'boxcryptor' do
   # cloudfront.net is the official download host per the vendor homepage
   url "https://d3k3ih5otj72mn.cloudfront.net/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8',
-          checkpoint: '055743b0219d9f5bbbf4e54c5f248098083e400f5df09717b2ca351db3a3d343'
+          checkpoint: '74dfc32d84ff9747d3f35ca753390c1c4aec6a4b59b583e338eb9fb3ea6542d1'
   name 'Boxcryptor'
   homepage 'https://www.boxcryptor.com/en'
   license :commercial
