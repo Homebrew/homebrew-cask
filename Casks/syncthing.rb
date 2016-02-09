7c9738282fde9ce4760481f961938f6bcede17f7
adidalal <adidalal@users.noreply.github.com>
2016-02-08 19:13:50 -0500

    updated syncthing (0.12.18)

--- Casks/syncthing.rb
+++ Casks/syncthing.rb
@@ -1,10 +1,10 @@
 cask 'syncthing' do
-  version '0.12.17'
-  sha256 'c5ab37deb53a76449fc2f6a3b02631100c07bf306940958096ee92745dbc90f3'
+  version '0.12.18'
+  sha256 '3dabfb52bafbd081ddc04de15b8af059df6192028683d749612a15acf3927c74'
 
   url "https://github.com/syncthing/syncthing/releases/download/v#{version}/syncthing-macosx-amd64-v#{version}.tar.gz"
   appcast 'https://github.com/syncthing/syncthing/releases.atom',
-          checkpoint: 'c9a81193ec221e8c9e88cd610d6d624ef4f4ecc28f83a68f1cbb148c4f16e71e'
+          checkpoint: '81037fa3b5bde910d6f358c47365d8a083c28535b87574169ab6aae351e5e521'
   name 'Syncthing'
   homepage 'https://syncthing.net/'
   license :mpl
