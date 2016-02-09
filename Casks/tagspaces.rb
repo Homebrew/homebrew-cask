752669c96a03a97a1409315990cfe6433ae2e5fc
adidalal <adidalal@users.noreply.github.com>
2016-02-08 19:15:52 -0500

    updated tagspaces (2.1.1)

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,14 +1,14 @@
 cask 'tagspaces' do
-  version '2.1.0'
+  version '2.1.1'
 
   if Hardware::CPU.is_32_bit?
     sha256 'be88d60769da6af846f8d84037bcbf4917dbef3735c460cfc8727c8c17ace488'
     url "http://www.tagspaces.org/downloads/tagspaces-#{version}-osx32.zip"
   else
-    sha256 'a7f7e3854b5f65f2e9c76ab3b8a91c925dcf730e77377e56b14d0347fff9c98b'
+    sha256 '912e97396cec629a0ed1bd8e50204bafd699e13c18d927c6ce9489648cf17d41'
     url "http://www.tagspaces.org/downloads/tagspaces-#{version}-osx64.zip"
     appcast 'https://github.com/tagspaces/tagspaces/releases.atom',
-            checkpoint: 'da1993c795ae304b4fd5b0e43d7caa050932d43f4f8f892cde4268947b7667ee'
+            checkpoint: 'b0f0a3390df3bbbf67b4f1f9252263d932fdbe1c54eb80ab11dc87b96495543e'
   end
 
   name 'TagSpaces'
