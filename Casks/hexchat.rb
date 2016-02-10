8aea69ed09eca9ae0f8d84fd3b2c86a8636fb57d
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 21:50:39 +0000

    hexchat.rb: updated vendor comment

--- Casks/hexchat.rb
+++ Casks/hexchat.rb
@@ -2,10 +2,9 @@ cask 'hexchat' do
   version '2.10.2'
   sha256 'd4b20b528c741de69a41afab057ce935f1f8ef19e3ecda6dc7a56d7b829cf3a1'
 
-  # hexchat.net is the official download host per the vendor homepage
   url "https://dl.hexchat.net/hexchat/osx/HexChat-#{version}.app.zip"
   name 'HexChat'
-  homepage 'https://hexchat.github.io'
+  homepage 'https://hexchat.net'
   license :gpl
 
   app 'HexChat.app'
