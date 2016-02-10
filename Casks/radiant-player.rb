a2b7ac4f91673361ac936eb708169d9a1862ba6f
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 22:20:54 +0000

    radiant-player.rb: updated vendor comment

--- Casks/radiant-player.rb
+++ Casks/radiant-player.rb
@@ -2,6 +2,7 @@ cask 'radiant-player' do
   version '1.6.2'
   sha256 '41fe94faeb571c0393a80751b15fa86b73906ada515b3c12dbd71353b4426429'
 
+  # github.com/radiant-player/radiant-player-mac was verified as official when first introduced to the cask
   url "https://github.com/radiant-player/radiant-player-mac/releases/download/v#{version}/radiant-player-v#{version}.zip"
   appcast 'https://github.com/radiant-player/radiant-player-mac/releases.atom',
           checkpoint: 'fd476d1c9ce75b9b33de45259ccd6f45095ef5e0702219333edccd6aac6a06ab'
