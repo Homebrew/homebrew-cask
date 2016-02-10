b78315ca2e3c8737b84829d89c56ebb63e943e33
Jean-Sébastien Beaulieu <folkrav@gmail.com>
2016-02-10 15:30:58 -0500

    SHAs updated and fixed for good now.
    That's what I get for editing on mobile. Now all good...

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -1,12 +1,12 @@
 cask 'libreoffice' do
   if Hardware::CPU.is_32_bit? || MacOS.release < :mountain_lion
     version '4.3.7'
-    sha256 'b0537db77524c0b6fa27d2b1b8715658fda56396bd28593d6a75302e38ff6d12'
+    sha256 '2964a952ab633426df402de2f128cf788354ac622b7c30b25209d185d17617ec'
     # documentfoundation.org is the official download host per the vendor homepage
     url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86/LibreOffice_#{version}_MacOS_x86.dmg"
   else
     version '5.0.4'
-    sha256 '36d3a99dde9fd5bb845f93a24c091a36c97ff1adedf878ed2188704f3dfeb2ae'
+    sha256 'b0537db77524c0b6fa27d2b1b8715658fda56396bd28593d6a75302e38ff6d12'
     # documentfoundation.org is the official download host per the vendor homepage
     url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   end
