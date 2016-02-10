d12762d67fb45ad09e36eaf643fba0d9204b1090
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 22:26:08 +0000

    sqlectron.rb: updated vendor comment

--- Casks/sqlectron.rb
+++ Casks/sqlectron.rb
@@ -2,6 +2,7 @@ cask 'sqlectron' do
   version '1.4.0'
   sha256 '36c4a65b7692a72bb173e4d3bad6ec1a01a975e7a6208a398f40d14e94376e6c'
 
+  # github.com/sqlectron/sqlectron-gui was verified as official when first introduced to the cask
   url "https://github.com/sqlectron/sqlectron-gui/releases/download/v#{version}/Sqlectron-darwin-x64.zip"
   appcast 'https://github.com/sqlectron/sqlectron-gui/releases.atom',
           checkpoint: '8ea6aab4c949435627b0b44813069b26ae13377373ec57efca9fe7bfe24ec79b'
