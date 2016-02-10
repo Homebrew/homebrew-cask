8e0819d18e5c9c4421aaac504d35064b9c2edbee
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 22:31:55 +0000

    unetbootin.rb: updated vendor comment

--- Casks/unetbootin.rb
+++ Casks/unetbootin.rb
@@ -1,9 +1,9 @@
 cask 'unetbootin' do
-  version '613'
+  version '613,212083654'
   sha256 'b9356ad2ef3cfa6881690ddc1136c45e6d3f25063fdeaeeb477633500b0c7684'
 
-  # launchpad.net is the official download host per the vendor homepage
-  url "https://launchpad.net/unetbootin/trunk/#{version}/+download/unetbootin-mac-#{version}.zip"
+  # launchpadlibrarian.net/212083654/unetbootin-mac-613.zip was verified as official when first introduced to the cask
+  url "https://launchpadlibrarian.net/#{version.after_comma}/unetbootin-mac-#{version.before_comma}.zip"
   name 'UNetbootin'
   homepage 'https://unetbootin.github.io/'
   license :gpl
