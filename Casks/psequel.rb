327a84e41123611a2950e34c12c9c5af9900a7a5
Raúl Acuña <reacuna@crac.cl>
2016-02-10 08:06:11 -0300

    PSequel 1.4.1

--- Casks/psequel.rb
+++ Casks/psequel.rb
@@ -1,6 +1,6 @@
 cask 'psequel' do
-  version '1.4.0'
-  sha256 '39b47595ba58f1519469cfcc223989d262d6439d171d7e900c928a6f28449b74'
+  version '1.4.1'
+  sha256 '06bf68f7019a037c9a17a84d4f18df071afb1cfce789d828037f1961a727aae5'
 
   url "http://www.psequel.com/download?version=#{version}"
   name 'PSequel'
