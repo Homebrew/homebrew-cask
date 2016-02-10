63a0586c44dba24a1eeb2d64b524c276a87b7c76
kTitan <alexander.schneider@gmail.com>
2016-02-10 07:49:47 +0100

    Upgrade flash-player to v20.0.0.306

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,6 +1,6 @@
 cask 'flash-player' do
-  version '20.0.0.286'
-  sha256 '3c361a4680cac2f63a5f26c9e77332eb5f69dcdee5298efdf576e09bc17f5967'
+  version '20.0.0.306'
+  sha256 'a289a1ab838864a73070c324d882734ea6f592419d4b13d4c793caa706c625a2'
 
   # macromedia.com is the official download host per the vendor homepage
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.to_i}/flashplayer_#{version.to_i}_sa.dmg"
