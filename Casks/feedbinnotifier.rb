78611251b68493d11e8cef22de9eb17267536cc1
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 21:45:37 +0000

    feedbinnotifier.rb: updated vendor comment

--- Casks/feedbinnotifier.rb
+++ Casks/feedbinnotifier.rb
@@ -2,6 +2,7 @@ cask 'feedbinnotifier' do
   version '2.0.0'
   sha256 '82234ee8992e5180cfc3dac8624eb8e75047c80bb4e7e2360c71b226a211c40c'
 
+  # github.com/kmikael/FeedbinNotifier was verified as official when first introduced to the cask
   url "https://github.com/kmikael/FeedbinNotifier/releases/download/v#{version}/FeedbinNotifier.zip"
   appcast 'https://github.com/kmikael/FeedbinNotifier/releases.atom',
           checkpoint: '1dc76597191bab28d4832ea7845cd6c274743d19f531336fec55a2e7c1ea35b6'
