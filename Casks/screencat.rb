11d474b8a99c4734816ee032b12225383752a931
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 22:24:25 +0000

    screencat.rb: updated vendor comment

--- Casks/screencat.rb
+++ Casks/screencat.rb
@@ -2,6 +2,7 @@ cask 'screencat' do
   version '2.0.0'
   sha256 'd0587d3c7c926825019d296ad677c4469fa6cdf47ab434f556d74c372e5a18e6'
 
+  # github.com/maxogden/screencat was verified as official when first introduced to the cask
   url "https://github.com/maxogden/screencat/releases/download/#{version}/ScreenCat.zip"
   name 'ScreenCat'
   homepage 'https://maxogden.github.io/screencat/'
