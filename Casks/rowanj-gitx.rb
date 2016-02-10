edd5fc7834440ec2578e86b054ae1ca52f48cb07
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 22:21:50 +0000

    rowanj-gitx.rb: updated vendor comment

--- Casks/rowanj-gitx.rb
+++ Casks/rowanj-gitx.rb
@@ -10,6 +10,7 @@ cask 'rowanj-gitx' do
     sha256 'd88bcb7f92ca1cdf31cb3f1d2e24c03e2091ab330319aeef2e770c0dbd6f7817'
   end
 
+  # github.com/rowanj/gitx was verified as official when first introduced to the cask
   url "https://github.com/rowanj/gitx/releases/download/builds/#{version.major_minor}/#{version.patch}/GitX-dev-#{version.patch}.dmg"
   appcast 'https://github.com/rowanj/gitx/releases.atom',
           checkpoint: 'b1e11ef3e13e74f84a51bfd6169db61f9c37fe5df851bb8c5f131c1981f8fdc0'
