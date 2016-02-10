5bf471337a21a85c4ff8ae320584d073cdab3f77
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 21:59:10 +0000

    linear.rb: updated vendor comment

--- Casks/linear.rb
+++ Casks/linear.rb
@@ -2,6 +2,7 @@ cask 'linear' do
   version '1.2.0'
   sha256 '2bb17d6c529d4a8b8a8443803e44d976271ca298bef3f22b43ca9d5b41dc8327'
 
+  # github.com/mikaa123/linear was verified as official when first introduced to the cask
   url "https://github.com/mikaa123/linear/releases/download/v#{version}/linear.zip"
   appcast 'https://github.com/mikaa123/linear/releases.atom',
           checkpoint: '604b2fdad9d513e82c80ae1efccab182aa36335a7488b14eb753158969a6b571'
