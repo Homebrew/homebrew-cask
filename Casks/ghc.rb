758224f70d1f3b0de7b35a46671ce72cc60442ee
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 21:48:59 +0000

    ghc.rb: updated vendor comment

--- Casks/ghc.rb
+++ Casks/ghc.rb
@@ -2,6 +2,7 @@ cask 'ghc' do
   version '7.10.2-r0'
   sha256 'b581a90ca6b6cf3c6f94304aac8b9018d95fa15e7b18c168efa434def1a249e5'
 
+  # github.com/ghcformacosx/ghc-dot-app was verified as official when first introduced to the cask
   url "https://github.com/ghcformacosx/ghc-dot-app/releases/download/v#{version}/ghc-#{version}.zip"
   appcast 'https://github.com/ghcformacosx/ghc-dot-app/releases.atom',
           checkpoint: '135574a1f7a6be02316f69400e55e467d1b9daec428ec380942efd791f387c23'
