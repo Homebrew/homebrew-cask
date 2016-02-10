87d8b1feedfdca05de81c8bbeebfa443df5859ff
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 21:46:40 +0000

    fontforge.rb: updated vendor comment

--- Casks/fontforge.rb
+++ Casks/fontforge.rb
@@ -2,6 +2,7 @@ cask 'fontforge' do
   version '2015-08-24'
   sha256 '49d7f59350155b29ac0979987e35066beb8d4f30c6b79a87bc79b9b1f87c5bca'
 
+  # github.com/fontforge/fontforge was verified as official when first introduced to the cask
   url "https://github.com/fontforge/fontforge/releases/download/#{version.delete('-')}/Fontforge-#{version}-Terminal_only.dmg"
   appcast 'https://github.com/fontforge/fontforge/releases.atom',
           checkpoint: '798b4528144c6f8a7bb3ae6644029f45c3006382cbcdcdd49e95fb5e6bfba96f'
