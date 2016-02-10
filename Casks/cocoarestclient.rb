dc1e9b4d3799a9ebce7252a98b6aac50c6098499
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 21:40:06 +0000

    cocoarestclient.rb: updated vendor comment

--- Casks/cocoarestclient.rb
+++ Casks/cocoarestclient.rb
@@ -2,6 +2,7 @@ cask 'cocoarestclient' do
   version '1.3.10'
   sha256 'a57933614eb8758fc4ef6d8f24ba8c27d545763c57f396c65260d421aa24b8c0'
 
+  # github.com/mmattozzi/cocoa-rest-client was verified as official when first introduced to the cask
   url "https://github.com/mmattozzi/cocoa-rest-client/releases/download/#{version}/CocoaRestClient-#{version}.dmg"
   appcast 'https://github.com/mmattozzi/cocoa-rest-client/releases.atom',
           checkpoint: 'cff443ebe4b177855551ffa7f9cca8afe0ec367ba29f2cdbc70b8b3df030cca4'
