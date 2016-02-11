bfd249a698af00b3823e36ed2ae4bf3e23be526a
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:20:32 +0000

    avidemux.rb: updated vendor comment

--- Casks/avidemux.rb
+++ Casks/avidemux.rb
@@ -2,7 +2,7 @@ cask 'avidemux' do
   version '2.6.10'
   sha256 '25b8dec3474b9e6a5907b19643e0052e2cda5d1029191b20cf8fda43574a6a0f'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # sourceforge.net/avidemux was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/avidemux/Avidemux_#{version}_64bits_Qt4.dmg"
   name 'Avidemux'
   homepage 'http://www.avidemux.org/'
