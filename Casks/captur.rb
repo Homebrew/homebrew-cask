a850ef70de8d52f6c5decf5f0f9f89a5734d3c17
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 22:04:27 +0000

    captur.rb: updated vendor comment

--- Casks/captur.rb
+++ Casks/captur.rb
@@ -2,7 +2,7 @@ cask 'captur' do
   version '2.4'
   sha256 '1f6f19806290c1c366701a8391fdc26cae3b1a54e3d54d715e420346eeb66648'
 
-  # dropboxusercontent.com is the official download host per the vendor homepage
+  # dropboxusercontent.com/u/27027504 was verified as official when first introduced to the cask
   url "https://dl.dropboxusercontent.com/u/27027504/captur-#{version}.dmg.zip"
   name 'Captur'
   homepage 'http://cambhlumbulunk.blogspot.co.uk/p/captur.html'
