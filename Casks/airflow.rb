578913a4e4aa9ee530dd1b94db28ba52cb5d37ed
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 15:41:27 +0000

    airflow.rb: updated vendor comment

--- Casks/airflow.rb
+++ Casks/airflow.rb
@@ -2,7 +2,7 @@ cask 'airflow' do
   version '1.0.0-beta6'
   sha256 'fc97c76c90232ed1a5f18134a4f4d5be1da622ea9c823b5f5290204269f077fa'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/Airflow was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Airflow/Download/Airflow%20#{version}.dmg"
   name 'Airflow'
   homepage 'http://airflowapp.com/'
