2fe40d22f88412662808b95d27b0c4c871974e35
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:40:32 +0000

    bimedesktop.rb: updated vendor comment

--- Casks/bimedesktop.rb
+++ Casks/bimedesktop.rb
@@ -2,7 +2,7 @@ cask 'bimedesktop' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/bimeio was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/bimeio/bimedesktop/BimeDesktop-mac.zip'
   name 'BIME Desktop'
   homepage 'https://www.bimeanalytics.com/'
