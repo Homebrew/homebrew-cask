d37fc2677bee5fd94751c7f297bb2eb753fe17d0
Victor Popkov <victor@popkov.me>
2016-02-11 19:11:17 +0200

    porthole.rb: upgrade to v1.7.8

--- Casks/porthole.rb
+++ Casks/porthole.rb
@@ -1,10 +1,10 @@
 cask 'porthole' do
-  version '1.7.7'
-  sha256 '3a183805d58a042ddf455780c7ae204f15d7a0158fd9ba24d7e13fa92aa58451'
+  version '1.7.8'
+  sha256 'f59b618fcbd8135fdbbe58da6c87e9bc457d7e7a8f9aee11f460c5a86b49e09d'
 
   url "http://download.getporthole.com/Porthole-v#{version}.zip"
   appcast 'https://update.getporthole.com/appcast.rss',
-          checkpoint: '1eedb1650d2ddbbac106b5b65a88b1f07ee239264ed8d4f6c303637cc1b65a55'
+          checkpoint: '832694ca1715fbd74cac68b0c543084fdf67e7c1ebffc7e38be8f0f4d4f285cd'
   name 'Porthole'
   homepage 'http://getporthole.com/'
   license :commercial
