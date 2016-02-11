5424f93d1f045539f8e8c13cddcd9d95d7fb9a45
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 22:55:49 +0000

    updated cloud (3.8.2)

--- Casks/cloud.rb
+++ Casks/cloud.rb
@@ -1,6 +1,6 @@
 cask 'cloud' do
-  version '3.6.1'
-  sha256 '8a877c5503d5c390d01c029c1f8a2e8510455b9871da738ac957c6821cc1bdb4'
+  version '3.8.2'
+  sha256 'e23e9ec5859a90d013e7f5eecfb0ae89d50efb47d2c5c6a2b36642f3c3aaecb1'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.dmg"
