5f93b75a73f96bf7481a480d3b2f1f2934c2d0de
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 15:39:29 +0000

    adafruit-arduino.rb: updated vendor comment

--- Casks/adafruit-arduino.rb
+++ Casks/adafruit-arduino.rb
@@ -2,7 +2,7 @@ cask 'adafruit-arduino' do
   version '1.6.3'
   sha256 'c964acbe728b616f0595517b21bbf97c88b8ee92379c06943a2862399d0196e2'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/adafruit-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/adafruit-download/adafruit-arduino-#{version}-macosx.zip"
   name 'Adafruit Arduino'
   homepage 'https://adafruit.com'
