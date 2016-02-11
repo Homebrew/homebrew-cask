ab9801ccd70251c426fc3042aff0b69faa1039a0
Victor Popkov <victor@popkov.me>
2016-02-11 19:12:21 +0200

    postgres.rb: upgrade to v9.5.1.0

--- Casks/postgres.rb
+++ Casks/postgres.rb
@@ -1,11 +1,11 @@
 cask 'postgres' do
-  version '9.5.0.0'
-  sha256 '2c8abeeac2a2569d70b394afe76aca4714bc021aa6ac2af2b58cd9ac20db4eb1'
+  version '9.5.1.0'
+  sha256 'a315143b6ca5126043519c7d4cf79866bce0d00dd9356d6ee429af20a1e68ce0'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/PostgresApp/PostgresApp/releases/download/#{version}/Postgres-#{version}.zip"
   appcast 'https://github.com/PostgresApp/PostgresApp/releases.atom',
-          checkpoint: '82d9410d35e28afa763963fbed2e2b845328e2ce82a665adbf5f9473d8d885e3'
+          checkpoint: '77c7508718e6c58152b12f7e7254dc09fe2a7f243d69eb50ab91a27c404a83cd'
   name 'Postgres'
   homepage 'http://postgresapp.com/'
   license :oss
