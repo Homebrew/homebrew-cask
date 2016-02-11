332895b27684550679965bff934bf4d6d6095187
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:18:14 +0000

    audacity.rb: updated vendor comment

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -2,7 +2,7 @@ cask 'audacity' do
   version '2.1.2-1453294898'
   sha256 '2e4b7d608ecc0d2f79bf16663f085d383075e488f7d50bf7d74c0b69173defe7'
 
-  # oldfoss.com is the official download host per the vendor homepage
+  # oldfoss.com:81/download/Audacity was verified as official when first introduced to the cask
   url "http://app.oldfoss.com:81/download/Audacity/audacity_macosx_ub_#{version.dots_to_underscores}.dmg"
   name 'Audacity'
   homepage 'http://audacityteam.org'
