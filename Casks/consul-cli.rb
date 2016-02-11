7f0f5489195b65e093e7c14f8847e23679913dee
Victor Popkov <victor@popkov.me>
2016-02-11 18:48:03 +0200

    consul-cli.rb: upgrade to v0.2.0

--- Casks/consul-cli.rb
+++ Casks/consul-cli.rb
@@ -1,10 +1,10 @@
 cask 'consul-cli' do
-  version '0.1.1'
-  sha256 'cf4eada7fcd1207f06d957e43db6dbbcacbd046e80cf57b1a3621f306f3ac7f8'
+  version '0.2.0'
+  sha256 '2a048dceb47ec4df9ed6ddf26c945f8557790723a9194aee40ac6c5019c73a17'
 
   url "https://github.com/CiscoCloud/consul-cli/releases/download/v#{version}/consul-cli_#{version}_darwin_amd64.tar.gz"
   appcast 'https://github.com/CiscoCloud/consul-cli/releases.atom',
-          checkpoint: '0b6589d1ae99511a97a71d911ac44f99f979d69f89a34823727bbd586426af4b'
+          checkpoint: 'c460875673bda22f2bd9251e51bdb2fbf08c07b8136df9e54fd0ba1f9ee192e9'
   name 'Consul CLI'
   homepage 'https://github.com/CiscoCloud/consul-cli/wiki'
   license :apache
