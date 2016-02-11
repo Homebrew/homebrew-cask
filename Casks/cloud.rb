47f53a875427ec54bdf77708b209e7512d0b2910
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 23:48:58 +0000

    cloud.rb: updated vendor comment

--- Casks/cloud.rb
+++ Casks/cloud.rb
@@ -2,7 +2,7 @@ cask 'cloud' do
   version '3.8.2'
   sha256 'e23e9ec5859a90d013e7f5eecfb0ae89d50efb47d2c5c6a2b36642f3c3aaecb1'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/downloads.getcloudapp.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.dmg"
   name 'CloudApp'
   homepage 'https://getcloudapp.com/'
