1eee95236ba94233454868e23770d0b5de59b19c
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:45:42 +0000

    bleep.rb: updated vendor comment

--- Casks/bleep.rb
+++ Casks/bleep.rb
@@ -2,7 +2,7 @@ cask 'bleep' do
   version '1.1.2'
   sha256 'c5155e585ae2583095dd29155f419c8ecc4e7f862144c36fd7e55be8e32c5216'
 
-  # utorrent.com is the official download host per the vendor homepage
+  # utorrent.com was verified as official when first introduced to the cask
   url 'https://download-new.utorrent.com/endpoint/bleep/os/osx/track/stable/'
   appcast 'https://bleeposx.bittorrent.com/appcast.xml',
           checkpoint: 'aa00fc13f371df0ebc108d087610dd5527734bb86c671956e0d19894c813eec2'
