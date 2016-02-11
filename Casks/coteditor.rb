d8b85552983480218a98818fb05d3b91b21bc1c5
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 23:07:40 +0000

    coteditor.rb: updated vendor comment

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -2,17 +2,17 @@ cask 'coteditor' do
   if MacOS.release <= :snow_leopard
     version '1.3.1'
     sha256 '5c871bd9de30fc3c76fc66acb4ea258d4d3762ae341181d65a7ef1f8de4751c5'
-    # github.com is the official download host per the vendor homepage
+    # github.com/coteditor/CotEditor was verified as official when first introduced to the cask
     url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}_For10.4.dmg"
   elsif MacOS.release <= :lion
     version '1.5.4'
     sha256 '444133083698c7c94c2b029644f39a0e36982ae34c24745789fa890626188347'
-    # github.com is the official download host per the vendor homepage
+    # github.com/coteditor/CotEditor was verified as official when first introduced to the cask
     url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg"
   else
     version '2.4.0'
     sha256 '21cd83228b4e87967cf881fe60826a5990341760bf9c9cf0dc1efe23ef1d4883'
-    # github.com is the official download host per the vendor homepage
+    # github.com/coteditor/CotEditor was verified as official when first introduced to the cask
     url "https://github.com/coteditor/CotEditor/releases/download/#{version}/CotEditor_#{version}.dmg"
   end
 
