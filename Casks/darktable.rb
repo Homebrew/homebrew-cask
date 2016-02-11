c90b3f656a603f4ad3d3c380c76c50a0f15dbb66
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 23:15:49 +0000

    darktable.rb: updated vendor comment

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -2,7 +2,7 @@ cask 'darktable' do
   version '2.0.1'
   sha256 '580d1feb356e05d206eb74d7c134f0ffca4202943388147385c5b8466fc1eada'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/darktable-org/darktable was verified as official when first introduced to the cask
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version}/darktable-#{version}.dmg"
   appcast 'https://github.com/darktable-org/darktable/releases.atom',
           checkpoint: '2e8a1bcf85f0aeb99f456964160657ad8f24f9ad57ef324c9a651b840c231543'
