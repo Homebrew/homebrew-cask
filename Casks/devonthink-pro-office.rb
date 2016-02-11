285d2a45238a271e6129edeaf9de18f7cdb3fe47
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 23:53:52 +0000

    devonthink-pro-office.rb: updated vendor comment

--- Casks/devonthink-pro-office.rb
+++ Casks/devonthink-pro-office.rb
@@ -2,7 +2,7 @@ cask 'devonthink-pro-office' do
   version '2.8.9'
   sha256 '3fd65975ceed5abc3df0248d3c2b8c5df1979caf3c01b5cf41e5548b1de140c8'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_Pro_Office.dmg.zip"
   appcast 'http://www.devon-technologies.com/fileadmin/templates/filemaker/sparkle.php?product=300125739&format=xml',
           checkpoint: '1a9c3a6d6aabe6024e955966375b49be4b6af4e097bd7ed70b17764a19807428'
