c4167a70c3f18ca330b5bf1379e2c9d9a3434f5b
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:33:00 +0000

    beautune.rb: updated vendor comment

--- Casks/beautune.rb
+++ Casks/beautune.rb
@@ -2,7 +2,7 @@ cask 'beautune' do
   version '1.0.5'
   sha256 'bd73f38bbb5053522671b9772b033bfa94da7b046d0705d60ec08d994a335f6d'
 
-  # amzonaws.com is the official download host per the vendor homepage
+  # download.fotor.com.s3.amazonaws.com was verified as official when first introduced to the cask
   url "http://download.fotor.com.s3.amazonaws.com/BeautuneMacInstaller_v#{version}_en.dmg"
   name 'Beautune'
   homepage 'http://www.everimaging.com/software/beautune/'
