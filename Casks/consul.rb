4e04254700f52ece5acfe81d62e2259356ae772a
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 23:06:34 +0000

    updated consul (0.6.3)

--- Casks/consul.rb
+++ Casks/consul.rb
@@ -2,8 +2,10 @@ cask 'consul' do
   version '0.6.3'
   sha256 '6dff4ffc61d66aacd627a176737b8725624718a9e68cc81460a3df9b241c7932'
 
-  # hashicorp.com is the official download host per the vendor homepage
+  # hashicorp.com was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_darwin_amd64.zip"
+  appcast 'https://github.com/hashicorp/consul/releases.atom',
+          checkpoint: 'd38501c339d670687ee0932f150bb1fce20249745583dba1b3fc4a39623fcd03'
   name 'Consul'
   homepage 'https://www.consul.io/'
   license :mpl
