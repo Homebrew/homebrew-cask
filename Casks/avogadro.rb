ca7b0c7264f7873729d8d01fda68448579528089
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:21:34 +0000

    avogadro.rb: updated vendor comment

--- Casks/avogadro.rb
+++ Casks/avogadro.rb
@@ -2,7 +2,7 @@ cask 'avogadro' do
   version '1.1.1'
   sha256 '8e63b7ec07555fd30ea2c22ed7f070e1b692fd2c9fbb60a3c8e0ddd411bb6477'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # sourceforge.net/avogadro was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/avogadro/Avogadro-#{version}.dmg.zip"
   name 'Avogadro'
   homepage 'http://avogadro.openmolecules.net/'
