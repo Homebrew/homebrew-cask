627797ab8b0bcd5cb22dbc1fc36ebd1a56be907d
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 21:58:36 +0000

    boxcryptor.rb: updated vendor comment

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -2,7 +2,7 @@ cask 'boxcryptor' do
   version '2.1.465.708'
   sha256 'ff554c1f4bb759f34216402ff7aab6cf5a8cc08df25e33d6f7aa2e3149d053f7'
 
-  # cloudfront.net is the official download host per the vendor homepage
+  # d3k3ih5otj72mn.cloudfront.net was verified as official when first introduced to the cask
   url "https://d3k3ih5otj72mn.cloudfront.net/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8',
           checkpoint: '74dfc32d84ff9747d3f35ca753390c1c4aec6a4b59b583e338eb9fb3ea6542d1'
