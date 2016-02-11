dede92571a94610e2df4cf277085393bc7cb3e9c
gustavoavellar <gustavoavellar@gmail.com>
2016-02-11 02:25:14 -0200

    updated itsycal (0.10.10)

--- Casks/itsycal.rb
+++ Casks/itsycal.rb
@@ -1,11 +1,11 @@
 cask 'itsycal' do
-  version '0.10.9'
-  sha256 '59a8c18ea0443a43a25a5858211abaaca1f269215c011fcc94715a171d986b88'
+  version '0.10.10'
+  sha256 '3452b19d10e91c441ef0f218629961bbb98c41ba2b29bff14759306967551253'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://s3.amazonaws.com/itsycal/Itsycal-#{version}.zip"
   appcast 'https://s3.amazonaws.com/itsycal/itsycal.xml',
-          checkpoint: 'd7fa19383d434d948de626620d167f19692999ffe7cd57c46627abf6a6e19747'
+          checkpoint: '98c525b55275fb02b4fdb4896ae2198f89fff858679b67cf031691bbd157ba72'
   name 'Itsycal'
   homepage 'http://www.mowglii.com/itsycal/'
   license :gratis
