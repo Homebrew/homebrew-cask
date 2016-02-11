beab58838b5619af7f1cc0019676993155cbc21b
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:29:15 +0000

    updated basictex (latest)

--- Casks/basictex.rb
+++ Casks/basictex.rb
@@ -2,8 +2,7 @@ cask 'basictex' do
   version :latest
   sha256 :no_check
 
-  # ctan.org is the official download host per the vendor homepage
-  url 'http://mirror.ctan.org/systems/mac/mactex/BasicTeX.pkg'
+  url 'http://tug.org/cgi-bin/mactex-download/BasicTeX.pkg'
   name 'BasicTeX'
   homepage 'https://www.tug.org/mactex/morepackages.html'
   license :oss
