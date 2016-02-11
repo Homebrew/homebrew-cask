935c4357451b84f1e9c79da17013f8be719c36e7
Victor Popkov <victor@popkov.me>
2016-02-11 19:14:41 +0200

    sony-ericsson-bridge.rb: upgrade to v4.4

--- Casks/sony-ericsson-bridge.rb
+++ Casks/sony-ericsson-bridge.rb
@@ -1,10 +1,10 @@
 cask 'sony-ericsson-bridge' do
-  version '4.3'
-  sha256 '90a1a352febb48e0673e412140be9bc681d348a77a73acab102f0036ed61ab1a'
+  version '4.4'
+  sha256 '6ec2f2ac9ce4f8f0d66f4a19ab180fc55b748b26e5f6d9d3e62abec07275e67e'
 
   url 'https://www-support-downloads.sonymobile.com/Software%20Downloads/Bridge%20for%20Mac/SonyBridgeForMac_web.dmg'
   appcast 'https://dl-desktop-macapps.sonyericsson.com/production/Bridge/Databases_Prod/appcast.xml',
-          checkpoint: 'dbd8fa7003f229c76f9c10110c50bc723b32eae77b243b3472e00e1ddbf983c1'
+          checkpoint: '19afba9490a22b44de4cf61bfc08b98aeec6dbb8cf1be89456ae03e0ddf90782'
   name 'Sony Ericsson Bridge for Mac'
   homepage 'https://support.sonymobile.com/au/tools/bridge-for-mac/'
   license :gratis
