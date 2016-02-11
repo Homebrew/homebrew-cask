6cf9e71133f5c720ffe52e8b267df858f8dbeb8f
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:44:08 +0000

    bitcasa.rb: updated vendor comment

--- Casks/bitcasa.rb
+++ Casks/bitcasa.rb
@@ -2,7 +2,6 @@ cask 'bitcasa' do
   version '3.0.18.2'
   sha256 '09fcece3ac576ee9e8157445655e0571ec9b3aee1174ffe22b15852ac7107cde'
 
-  # cloudfront.net is the official download host per the vendor homepage
   url "https://dist.bitcasa.com/update/InstallBitcasa.#{version}.pkg"
   name 'Bitcasa CloudFS'
   homepage 'https://www.bitcasa.com'
