f1e0bb66b738f8e3bd995a5623b8dccb82bd2952
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 15:53:55 +0000

    andy.rb: updated vendor comment

--- Casks/andy.rb
+++ Casks/andy.rb
@@ -2,7 +2,7 @@ cask 'andy' do
   version '43'
   sha256 'ed55fb3a389a0afa62043fbdbe31e418aa96b789a743d987903a817fe4a8c75b'
 
-  # edgesuite.net is the official download host per the vendor homepage
+  # andyroid.vonetize.com.edgesuite.net was verified as official when first introduced to the cask
   url "http://andyroid.vonetize.com.edgesuite.net/Mac/v#{version}/Andy_OSX_v#{version}.dmg"
   name 'Andy'
   homepage 'http://www.andyroid.net/'
