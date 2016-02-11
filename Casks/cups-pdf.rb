ad6212e4d4ee5d77418ad2e86855a66bb4f06d04
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 23:14:38 +0000

    cups-pdf.rb: updated vendor comment

--- Casks/cups-pdf.rb
+++ Casks/cups-pdf.rb
@@ -2,7 +2,7 @@ cask 'cups-pdf' do
   version '2.6.1'
   sha256 '24bf65884dda2d3fd60266f96bf76791a61ec9095f11073047d880de692784b9'
 
-  # bitbucket.org is the official download host per the vendor homepage
+  # bitbucket.org/codepoet/cups-pdf-for-mac-os-x was verified as official when first introduced to the cask
   url "https://bitbucket.org/codepoet/cups-pdf-for-mac-os-x/downloads/CUPS-PDF%20#{version}%20Installer.pkg"
   name 'CUPS-PDF'
   homepage 'http://www.cups-pdf.de'
