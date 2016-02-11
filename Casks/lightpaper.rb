95769938efde87d49aa0ecbdf1534bc083273fe6
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 00:51:34 +0000

    updated lightpaper (1.2.2)

--- Casks/lightpaper.rb
+++ Casks/lightpaper.rb
@@ -2,9 +2,9 @@ cask 'lightpaper' do
   version '1.2.2'
   sha256 'ed88ad17b53ae160af0f235aea0b10da75eeab606a83acb620ff201ebacc68bf'
 
-  # hockeyapp.net is the official download host per the vendor homepage
+  # hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8 was verified as official when first introduced to the cask
   url 'https://rink.hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8?format=zip'
-  appcast 'http://links.ashokgelal.com/lp-mac-update-feed',
+  appcast 'https://dl.dropboxusercontent.com/u/83257/LpMacUpdates/lightpaper_appcast.xml',
           checkpoint: '5bb2bc21c40cb1c424eb1fcdf8def3489a5195c1e1dc75f7f4d8eca22d6c957c'
   name 'LightPaper'
   homepage 'http://lightpaper.42squares.in/'
