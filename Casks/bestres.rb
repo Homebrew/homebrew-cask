ff425fa6ae316aac735e21e4c3de9a052a9443e5
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:36:31 +0000

    bestres.rb: updated vendor comment

--- Casks/bestres.rb
+++ Casks/bestres.rb
@@ -2,7 +2,7 @@ cask 'bestres' do
   version :latest
   sha256 :no_check
 
-  # devmate.com is the official download host per the vendor homepage
+  # devmate.com/com.icyberon.BestRes was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.icyberon.BestRes/BestRes.zip'
   name 'BestRes'
   homepage 'http://icyberon.com/bestres/'
