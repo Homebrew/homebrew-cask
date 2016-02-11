30f180bcafc758b7351d9a8621a8d8c2ef0bc37a
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:35:01 +0000

    updated bee (2.3.2)

--- Casks/bee.rb
+++ Casks/bee.rb
@@ -2,8 +2,8 @@ cask 'bee' do
   version '2.3.2'
   sha256 'f2e448b389063f3be306705553807b8b424e90b0808f0e6525d8b353a42f90c7'
 
-  # amazonaws.com is the official download host per the vendor homepage
-  url "https://bee-app.s3.amazonaws.com/public/Bee-#{version}.zip"
+  # bee-app.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "http://bee-app.s3.amazonaws.com/public/Bee-#{version}.zip"
   appcast 'http://www.neat.io/appcasts/bee-appcast.xml',
           checkpoint: 'a8bcc6df65c976671ab7a3bbba0864e35a146cfe5bd48cf3638d2b07a361afbe'
   name 'Bee'
