57e9b0f5915c828fcf62a70c0d2f2a4d8775c91f
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:43:56 +0000

    updated bit-slicer (1.7.6)

--- Casks/bit-slicer.rb
+++ Casks/bit-slicer.rb
@@ -2,8 +2,8 @@ cask 'bit-slicer' do
   version '1.7.6'
   sha256 '03e9125481bd4c6459e379b3b0df69a2eecbde80f7cb11d9be8dfc9c0f8d3a58'
 
-  # bitbucket.org is the official download host per the vendor homepage
-  url "https://bitbucket.org/zorgiepoo/bit-slicer/downloads/Bit%20Slicer%20#{version}.zip"
+  # zgcoder.net was verified as official when first introduced to the cask
+  url "https://zgcoder.net/software/bitslicer/dist/stable/Bit_Slicer_#{version}.zip"
   appcast 'https://zgcoder.net/bitslicer/update/appcast.xml',
           checkpoint: '98f4404442be353240a8e8f1c6172d008119e9d0ab1987cd973e9ebec3480bfa'
   name 'Bit Slicer'
