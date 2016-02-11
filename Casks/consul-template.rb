8b40cb39970bb9c6a4b0d08cd1af515ac644f729
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 23:05:22 +0000

    updated consul-template (0.12.1)

--- Casks/consul-template.rb
+++ Casks/consul-template.rb
@@ -1,9 +1,11 @@
 cask 'consul-template' do
-  version '0.12.2'
-  sha256 'a9ab8e16cb02729153ec72a53f9f9f73efa0259521200467482fb34bd3e893b1'
+  version '0.12.1'
+  sha256 '9ccdfd5a99ed69cd694bbaa10ff45d1a660eda2d74ba010dc0400641959e5e73'
 
-  # hashicorp.com is the official download host per the vendor homepage
+  # hashicorp.com was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/consul-template/#{version}/consul-template_#{version}_darwin_amd64.zip"
+  appcast 'https://github.com/hashicorp/consul-template/releases.atom',
+          checkpoint: '2b7b817bf811db4cc66b7eeecbde3594bf0fc5e888aa02563a81e249e263e715'
   name 'Consul Template'
   homepage 'https://github.com/hashicorp/consul-template'
   license :mpl
