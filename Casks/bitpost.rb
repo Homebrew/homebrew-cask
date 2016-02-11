aec13afc742e36482d27d135ad195629e6e4a6e2
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:45:09 +0000

    bitpost.rb: updated vendor comment

--- Casks/bitpost.rb
+++ Casks/bitpost.rb
@@ -2,7 +2,7 @@ cask 'bitpost' do
   version '0.9.8.3'
   sha256 '8e58ddf022eae721bfca6ecb456edf40baa23ea31076ee9c1ad75e947b0cea2b'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # voluntary.net.s3.amazonaws.com was verified as official when first introduced to the cask
   url "http://voluntary.net.s3.amazonaws.com/Bitpost.#{version}.zip"
   name 'Bitpost'
   homepage 'https://voluntary.net/bitpost/'
