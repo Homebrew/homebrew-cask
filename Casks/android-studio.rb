bd155e7158b03b492554d171e0da3b69e116767f
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 15:52:54 +0000

    android-studio.rb: updated vendor comment

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -2,7 +2,7 @@ cask 'android-studio' do
   version '1.5.1.0_2456560'
   sha256 '2fe2ab35d9be1780269f6ade2cf097c5b5dc494ef44fbdd6ce84008609576430'
 
-  # google.com is the official download host per the vendor homepage
+  # google.com/dl/android/studio was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.sub(%r{_.*}, '')}/android-studio-ide-141.#{version.sub(%r{.*_}, '')}-mac.zip"
   name 'Android Studio'
   homepage 'https://developer.android.com/sdk/'
