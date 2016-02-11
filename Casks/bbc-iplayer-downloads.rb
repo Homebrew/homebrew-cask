3934b28338cf6ed1790613895d2f74ce44844cb0
Vítor Galvão <info@vitorgalvao.com>
2016-02-11 17:30:52 +0000

    bbc-iplayer-downloads.rb: updated vendor comment

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -2,7 +2,7 @@ cask 'bbc-iplayer-downloads' do
   version '1.14.2'
   sha256 'feb02a935380805598b211a94c28466f0e064bfc938767ddc084a37a8c4719ee'
 
-  # bbci.co.uk is the official download host per the vendor homepage
+  # bbci.co.uk was verified as official when first introduced to the cask
   url "https://a.files.bbci.co.uk/iplayer/downloads/BBC-iPlayer-Downloads-#{version}.dmg"
   name 'BBC iPlayer Downloads'
   homepage 'http://www.bbc.co.uk/iplayer/install'
