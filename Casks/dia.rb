6a5209e0cd431031e34923a5c092f48bf96ccede
Vítor Galvão <info@vitorgalvao.com>
2016-02-12 00:22:43 +0000

    Update dia.rb

--- Casks/dia.rb
+++ Casks/dia.rb
@@ -2,7 +2,7 @@ cask 'dia' do
   version '0.97.2-7'
   sha256 '9d3038c01347716800688830eaf52204deb78affe74a5f0c6e0a48fd414d44be'
 
-  # sourceforge.net/dia-installer was verified as official when first introduced to the cask
+  # sourceforge.net/dia-installer/dia was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/dia-installer/dia/#{version.sub(%r{-.*}, '')}/Dia-#{version}.dmg"
   name 'Dia'
   homepage 'http://dia-installer.de/'
