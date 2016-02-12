bf427bde97d27af30f1cb316184ad7f3f686523d
Vítor Galvão <info@vitorgalvao.com>
2016-02-12 16:16:31 +0000

    citrix-receiver.rb: fixed url

--- Casks/citrix-receiver.rb
+++ Casks/citrix-receiver.rb
@@ -2,7 +2,8 @@ cask 'citrix-receiver' do
   version '12.1'
   sha256 '86d376b1b3fdf3a5b53c1c9129b2cbf11087e0c190514a3da24c4f69aa8fa150'
 
-  url "https://downloads.citrix.com/10820/CitrixReceiver#{version}.dmg?__gda__=1455257516_b9db841c760bc0f1922a35e39581352e"
+  # downloadplugins.citrix.com.edgesuite.net was verified as official when first introduced to the cask
+  url 'http://downloadplugins.citrix.com.edgesuite.net/Mac/CitrixReceiverWeb.dmg'
   name 'Citrix Receiver'
   homepage 'https://www.citrix.com/receiver'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
