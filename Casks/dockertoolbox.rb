7074e0d4ef01b9884ed7584ca12c67903a2226a5
Vítor Galvão <info@vitorgalvao.com>
2016-02-12 16:17:59 +0000

    dockertoolbox.rb: added vendor comment

--- Casks/dockertoolbox.rb
+++ Casks/dockertoolbox.rb
@@ -2,6 +2,7 @@ cask 'dockertoolbox' do
   version '1.10.1'
   sha256 'cc1358d28011b80721bd377c3b8f45e6eeda490a49ace8e23ab0ac263eaaedb8'
 
+  # github.com/docker/toolbox was verified as official when first introduced to the cask
   url "https://github.com/docker/toolbox/releases/download/v#{version}/DockerToolbox-#{version}.pkg"
   appcast 'https://github.com/docker/toolbox/releases.atom',
           checkpoint: '06c615666bc65c9572fc672bf94513db3cbfb1a083d4e7abae461aaf8c8aee79'
