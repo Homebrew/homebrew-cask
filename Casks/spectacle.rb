87201bed1589990d8f67838f907f0530ab2619a1
Lasse Bang Mikkelsen <lbm@lbm.dk>
2016-02-12 17:13:28 +0100

    Bump Spectacle to 1.0.2

--- Casks/spectacle.rb
+++ Casks/spectacle.rb
@@ -3,11 +3,11 @@ cask 'spectacle' do
     version '0.8.6'
     sha256 '3e367d2d7e6fe7d5f41d717d49cb087ba7432624b71ddd91c0cfa9d5a5459b7c'
   else
-    version '1.0.1'
-    sha256 '8b41469acb8ae5bc845f1441c2cf630f72f0fc14ad324e78336b0cb4268216d7'
+    version '1.0.2'
+    sha256 '7c63ebe77cb39fdde04c5bdfb822972e06b4715356127886367822ed5829c4b2'
 
     appcast 'https://www.spectacleapp.com/updates/appcast.xml',
-            checkpoint: '226e49c2c3fe9bd13f9e0aebe5a687134443107cd542cea2ede86ff41d081943'
+            checkpoint: 'd7f2ed9a0414484b687dede58fdb8c2d774f50a01c35bad593aead7a5fc4dcc3'
   end
 
   # amazonaws.com is the official download host per the vendor homepage
