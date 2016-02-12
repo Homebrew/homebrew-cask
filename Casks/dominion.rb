0bae4949025a8a4c685051d90c8008698142039f
Vítor Galvão <info@vitorgalvao.com>
2016-02-12 00:10:56 +0000

    dominion.rb: updated vendor comment

--- Casks/dominion.rb
+++ Casks/dominion.rb
@@ -2,7 +2,7 @@ cask 'dominion' do
   version '2.0.44.2'
   sha256 '3a6fecab499a5b1d1f4a637d2418a5c8bf5f627eafb59416c288124aeabc55a1'
 
-  # makingfun.co is the official download host per the vendor homepage
+  # dominion.makingfun.co was verified as official when first introduced to the cask
   url "https://dominion.makingfun.co/Dominion%20#{version}.pkg"
   name 'Dominion'
   homepage 'https://www.playdominion.com/'
