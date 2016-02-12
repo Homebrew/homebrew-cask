10bbc6d927196443aad7e98562f1095f82f8fdf7
Vítor Galvão <info@vitorgalvao.com>
2016-02-12 00:10:19 +0000

    dogecoin.rb: updated vendor comment

--- Casks/dogecoin.rb
+++ Casks/dogecoin.rb
@@ -2,7 +2,7 @@ cask 'dogecoin' do
   version '1.10.0'
   sha256 'be854af97efecf30ee18ed846a3bf3a780a0eb0e459a49377d7a8261c212b322'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/dogecoin/dogecoin was verified as official when first introduced to the cask
   url "https://github.com/dogecoin/dogecoin/releases/download/v#{version}/dogecoin-#{version}-osx-signed.dmg"
   appcast 'https://github.com/dogecoin/dogecoin/releases.atom',
           checkpoint: '3d22468ec5e9fe768efce8293cb1af298a6332ee4e1018768257418a64f99f0b'
