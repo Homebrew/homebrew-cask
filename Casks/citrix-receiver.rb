1ee2cd374fae02bde1e341b4a71d379fdd6ca30f
adidalal <adidalal@users.noreply.github.com>
2016-02-12 00:14:40 -0500

    updated citrix-receiver (12.1)

--- Casks/citrix-receiver.rb
+++ Casks/citrix-receiver.rb
@@ -2,7 +2,7 @@ cask 'citrix-receiver' do
   version '12.1'
   sha256 '86d376b1b3fdf3a5b53c1c9129b2cbf11087e0c190514a3da24c4f69aa8fa150'
 
-  url "https://downloads.citrix.com/10820/CitrixReceiver#{version}.dmg?__gda__=1455232474_2e6dd1faf0ad4d99a5361e3ea75af357"
+  url "https://downloads.citrix.com/10820/CitrixReceiver#{version}.dmg?__gda__=1455257516_b9db841c760bc0f1922a35e39581352e"
   name 'Citrix Receiver'
   homepage 'https://www.citrix.com/receiver'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
