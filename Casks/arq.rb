3408bad8f5f76f7d05c99d27394bc0b7fc2a00d6
Kridsada Thanabulpong <sirn@ogsite.net>
2016-02-12 23:39:21 +0700

    arq.rb: use version (4.15) instead of latest.

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,8 +1,10 @@
 cask 'arq' do
-  version :latest
-  sha256 :no_check
+  version '4.15'
+  sha256 '72ebd8ce6afaabbede487ed99f2b88f9418d741a3bceb685c1288a9f71d4a12f'
 
-  url 'https://www.arqbackup.com/download/Arq_OSX.zip'
+  url "https://www.arqbackup.com/download/Arq_#{version}.zip"
+  appcast 'https://www.arqbackup.com/download/arq4.xml',
+          checkpoint: '48c791e8e25fef54b1f2ebd4405ecedd36aa7f7b7b197f04de85627a2ebf1b02'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
   license :commercial
