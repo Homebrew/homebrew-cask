080a1eafdd90591595252693aa70cf7b8f51e47f
Antelle <dmitryvit@gmail.com>
2016-02-12 23:09:44 +0300

    Added KeeWeb.app v1.0

---
+++ Casks/keeweb.rb
@@ -0,0 +1,16 @@
+cask 'keeweb' do
+  version '1.0.0'
+  sha256 '48fd9c20169e28c699306782daf6bc6592ea94e54d65ab32e1f38fa3384e6295'
+
+  # github.com/antelle/keeweb was verified as official when first introduced to the cask
+  url "https://github.com/antelle/keeweb/releases/download/v#{version}/KeeWeb.mac.dmg"
+  appcast 'https://github.com/antelle/keeweb/releases.atom',
+          checkpoint: '72151b698499fafca069338cfecad3d0c3044da3409f538646af68d76f4d6bd3'
+  name 'KeeWeb'
+  homepage 'https://keeweb.info'
+  license :mit
+
+  auto_updates true
+
+  app 'KeeWeb.app'
+end
