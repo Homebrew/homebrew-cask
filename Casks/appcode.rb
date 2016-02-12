1c439ece32446b0db546c26c0f28b87ca557b92a
Greg Rynkowski <dev@rynkowski.info>
2016-02-12 20:55:30 +0000

    Update AppCode to v3.3.3

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,6 +1,6 @@
 cask 'appcode' do
-  version '3.3.2'
-  sha256 '8a49a3942f396717db09e8fd757ffc50de88b072ddb63e41c1ee895fc2cd68d1'
+  version '3.3.3'
+  sha256 'bbc586779ec7c082eca46e266ac3799c6bebdbd8205250f075f04973caafaf54'
 
   url "https://download.jetbrains.com/objc/AppCode-#{version}-custom-jdk-bundled.dmg"
   name 'AppCode'
