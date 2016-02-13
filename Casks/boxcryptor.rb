549f1208c955f700db7bba3c7a287ebb2f9598b4
Victor Popkov <victor@popkov.me>
2016-02-13 21:43:39 +0200

    boxcryptor.rb: upgrade to v2.1.467.718

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,11 +1,11 @@
 cask 'boxcryptor' do
-  version '2.1.465.708'
-  sha256 'ff554c1f4bb759f34216402ff7aab6cf5a8cc08df25e33d6f7aa2e3149d053f7'
+  version '2.1.467.718'
+  sha256 '3859a2c0976a2fa1c280e4cea26a2037f125e066041f64cdbf4c47861aa39997'
 
   # d3k3ih5otj72mn.cloudfront.net was verified as official when first introduced to the cask
   url "https://d3k3ih5otj72mn.cloudfront.net/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8',
-          checkpoint: '74dfc32d84ff9747d3f35ca753390c1c4aec6a4b59b583e338eb9fb3ea6542d1'
+          checkpoint: 'c85906deda472e312cdd45f1892eb42db3f5c4a7adbbf737a407afbf756e8e9b'
   name 'Boxcryptor'
   homepage 'https://www.boxcryptor.com/en'
   license :commercial
