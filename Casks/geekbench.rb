2a439518a1b2af83b081ae09dca3e8e7cf0f944b
Victor Popkov <victor@popkov.me>
2016-02-13 21:51:44 +0200

    geekbench.rb: upgrade to v3.4.1

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -1,10 +1,10 @@
 cask 'geekbench' do
-  version '3.3.2'
-  sha256 '17ccf365001cb25dd39d49c3b6a5fbaa3db056336819e6eeab75da96fed64f9a'
+  version '3.4.1'
+  sha256 '9f2730472bba9fd39554290f465d37c32792debc5b20c9840efd1f79d40ca94c'
 
   url "http://cdn.primatelabs.com/Geekbench-#{version}-Mac.zip"
   appcast 'http://www.primatelabs.com/appcast/geekbench3.xml',
-          checkpoint: '584e688b7c3b295d704606385636a39c83078ba00e9b5fd7330eaa7bb81577cb'
+          checkpoint: '23877fa30558eada7e7beeefee4fa307c7a7ac3fad79b3c250fa09f4c68081c9'
   name 'Geekbench'
   homepage 'http://www.primatelabs.com/geekbench/'
   license :commercial
