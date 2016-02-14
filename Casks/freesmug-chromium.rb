112e7c224fec2bcf3c65e7ca15fa21e0d8679ed3
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:56:27 +0000

    freesmug-chromium.rb: updated vendor comment

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -2,7 +2,7 @@ cask 'freesmug-chromium' do
   version '48.0.2564.103'
   sha256 'dc555206efc4daf0cea328412e49c50ba2324454feb9cf20aaa48a4104aadbe5'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # sourceforge.net/sourceforge/osxportableapps was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/sourceforge/osxportableapps/Chromium_OSX_#{version}.dmg"
   appcast 'http://sourceforge.net/projects/osxportableapps/rss?path=/Chromium',
           checkpoint: '61f5bdf8dec83406cdc862c2c73214f54b6ba1e3f64d8cb2f55da9741aa91fe8'
