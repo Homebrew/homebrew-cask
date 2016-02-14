944d04c5bcbb57b1689e8d59b803b9956fe9247a
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:42:27 +0000

    filebot.rb: updated vendor comment

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -2,7 +2,7 @@ cask 'filebot' do
   version '4.6.1'
   sha256 'eed87622fe833a53fed2db816c08b5b5d68f663ef4356835e7b8f9ff3ad1d471'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # sourceforge.net/project/filebot was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/filebot/filebot/FileBot_#{version}/FileBot_#{version}-brew.tar.bz2"
   name 'FileBot'
   homepage 'https://www.filebot.net/'
