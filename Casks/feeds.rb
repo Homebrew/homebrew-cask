9450dd3892d6bd5dacf9d5f90b1b57bdfc264b25
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:34:17 +0000

    feeds.rb: updated vendor comment

--- Casks/feeds.rb
+++ Casks/feeds.rb
@@ -2,7 +2,7 @@ cask 'feeds' do
   version '2.1.1'
   sha256 '098dc0ceec2ca1ef26e6ff9f3a7ab4fd7c71877a7fc698584c986525a2204fdf'
 
-  # googleapis.com is the official download host per the vendor homepage
+  # storage.googleapis.com/feeds-releases was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/feeds-releases/Feeds-#{version}.zip"
   appcast 'https://storage.googleapis.com/feeds-releases/appcast.xml',
           checkpoint: '7af76d6014097647a2c5bf89bf8fc50019bd86c68c3eea60288eaf20c1a5c7f5'
