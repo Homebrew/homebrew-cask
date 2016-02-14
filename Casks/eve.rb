68a98492b1b67238fd0b135c454bea448be36301
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:28:21 +0000

    eve.rb: updated vendor comment

--- Casks/eve.rb
+++ Casks/eve.rb
@@ -2,7 +2,7 @@ cask 'eve' do
   version '1.4.3'
   sha256 '52ee822e2a931e03aa6f54f96453ceb3574490b4f5a9b2bafbc9c008e783f550'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/hotkeyeve was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/hotkeyeve/Downloads/EVE-latest.zip'
   appcast 'https://s3.amazonaws.com/hotkeyeve/hotkeyEVEappcast.xml',
           checkpoint: '93943cfb0ee6329879ca6aa563be493c2c30a8a461ae839c02de678463e01d53'
