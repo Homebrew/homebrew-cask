41f31cc87171c272bddc7a230dc6fecfe6e4fc04
Victor Popkov <victor@popkov.me>
2016-02-14 23:06:05 +0200

    edgeview.rb: upgrade to v0.9998

--- Casks/edgeview.rb
+++ Casks/edgeview.rb
@@ -1,11 +1,11 @@
 cask 'edgeview' do
-  version '0.9997'
-  sha256 '5972e4db1508de88b838dd9287c56b20eea3dda9b5b52d88490897020368a6aa'
+  version '0.9998'
+  sha256 'ee59450920a169ff64b6ff8c258648f1e4ee294242401d1769bd7fd36dc228e4'
 
   # dropboxusercontent.com/u/168552 was verified as official when first introduced to the cask
   url "https://dl.dropboxusercontent.com/u/168552/edgeViewUpdate/files/EdgeView_#{version.minor}.zip"
   appcast 'https://dl.dropboxusercontent.com/u/168552/edgeViewUpdate/edgeView_update.xml',
-          checkpoint: '006594c1812dbf8a14cf909f05508dd965e4af1d58b3a27cd9fcffdf8752220e'
+          checkpoint: '3e1af95c9d64ba5e39f004d61ddb4da416171a04bc45a2fa5cf9e3489e1771ab'
   name 'EdgeView'
   homepage 'https://edgehigh.wordpress.com'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
