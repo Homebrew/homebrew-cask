7a3a0176bbc792d7ed8442840135170848100356
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:14:25 +0000

    electric-sheep.rb: updated vendor comment

--- Casks/electric-sheep.rb
+++ Casks/electric-sheep.rb
@@ -2,7 +2,7 @@ cask 'electric-sheep' do
   version '2.7b36'
   sha256 'e8b35e203264adc6e8801d2e7c725453cca2c3adadda29640144ea3b50d37869'
 
-  # d100rc88eim93q.cloudfront.net is the official download host per the vendor homepage
+  # d100rc88eim93q.cloudfront.net was verified as official when first introduced to the cask
   url "https://d100rc88eim93q.cloudfront.net/electricsheep-#{version}.dmg"
   name 'Electric Sheep'
   homepage 'http://www.electricsheep.org'
