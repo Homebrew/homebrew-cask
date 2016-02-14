463fad64b9a89ff9a15a3ce494d3d8242f2fbb01
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:42:49 +0000

    filezilla.rb: updated vendor comment

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -7,7 +7,7 @@ cask 'filezilla' do
     sha256 'f86d9479026a731ef682707fe4a1372d0c1aae09bd81f5c1a564c3fa934d2858'
   end
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # sourceforge.net/project/filezilla was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   name 'FileZilla'
   homepage 'https://filezilla-project.org/'
