beb438dfd699d7f88d8f4b5203278e69867bc205
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:43:58 +0000

    flash-player.rb: updated vendor comment

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -2,7 +2,7 @@ cask 'flash-player' do
   version '20.0.0.306'
   sha256 'a289a1ab838864a73070c324d882734ea6f592419d4b13d4c793caa706c625a2'
 
-  # macromedia.com is the official download host per the vendor homepage
+  # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.to_i}/flashplayer_#{version.to_i}_sa.dmg"
   name 'Adobe Flash Player'
   homepage 'https://www.adobe.com/support/flashplayer/downloads.html'
