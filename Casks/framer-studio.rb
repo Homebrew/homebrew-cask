57b6d4ffaf4735fc0fb21e59c66aa89c72d608c1
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:54:33 +0000

    framer-studio.rb: updated vendor comment

--- Casks/framer-studio.rb
+++ Casks/framer-studio.rb
@@ -2,7 +2,7 @@ cask 'framer-studio' do
   version '50'
   sha256 '1649c2768d66c0e0c45345a05f43e095852bc26a2c54bc0df7ef6b08d023bdc1'
 
-  # devmate.com is the official download host per the vendor homepage
+  # devmate.com/com.motif.framer was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.motif.framer/FramerStudio.zip'
   name 'Framer Studio'
   homepage 'http://framerjs.com/'
