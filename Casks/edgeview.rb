de60eb82da6c2ee059ee6a7a4db9cd5b1e6acf61
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:12:59 +0000

    edgeview.rb: updated vendor comment

--- Casks/edgeview.rb
+++ Casks/edgeview.rb
@@ -2,8 +2,8 @@ cask 'edgeview' do
   version '0.9997'
   sha256 '5972e4db1508de88b838dd9287c56b20eea3dda9b5b52d88490897020368a6aa'
 
-  # dropboxusercontent.com is the official download host per the vendor homepage
-  url "https://dl.dropboxusercontent.com/u/168552/edgeViewUpdate/files/EdgeView_#{version.sub(%r{^\d+\.}, '')}.zip"
+  # dropboxusercontent.com/u/168552 was verified as official when first introduced to the cask
+  url "https://dl.dropboxusercontent.com/u/168552/edgeViewUpdate/files/EdgeView_#{version.minor}.zip"
   appcast 'https://dl.dropboxusercontent.com/u/168552/edgeViewUpdate/edgeView_update.xml',
           checkpoint: '006594c1812dbf8a14cf909f05508dd965e4af1d58b3a27cd9fcffdf8752220e'
   name 'EdgeView'
