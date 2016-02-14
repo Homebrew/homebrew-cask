5a92c7fc92ca980f7599395c50608c8d62dc0ad7
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:49:39 +0000

    flixster-desktop.rb: update vendor comment

--- Casks/flixster-desktop.rb
+++ Casks/flixster-desktop.rb
@@ -2,7 +2,7 @@ cask 'flixster-desktop' do
   version '2.5.12.335'
   sha256 '9b884410c07dd2f7f96b35b5dd0fcd9322b13b448cae69a494324cafa24deeb6'
 
-  # cloudfront.net is the official download host per the vendor homepage
+  # d1rtylazwb77ux.cloudfront.net was verified as official when first introduced to the cask
   url 'https://d1rtylazwb77ux.cloudfront.net/desktop/mac/FlixsterDesktop.zip'
   appcast 'https://d1rtylazwb77ux.cloudfront.net/desktop/mac/FlixsterDesktopMacAppcast.xml',
           checkpoint: 'ed42b9935d8dab11e2acb5502ccd1076c9c1e3d1d477a99c9dc569cfbd3a9706'
