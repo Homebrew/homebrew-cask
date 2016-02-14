72f8d61f8113333cdcedda7d3acf303b9530542d
David Baumgold <david@davidbaumgold.com>
2016-02-14 14:33:55 -0500

    GURPS Character Sheet 4.3

--- Casks/gurps-character-sheet.rb
+++ Casks/gurps-character-sheet.rb
@@ -1,6 +1,6 @@
 cask 'gurps-character-sheet' do
-  version '4.2.0'
-  sha256 '52b927a303e8c3bf1c551e7a41c1af72b946eadd246f8704cc91d3ffeeff4514'
+  version '4.3.0'
+  sha256 'f1a4d9ee526b57ab0bb41b07de2c140a1be212d6f00c8a7aeafe78e552af572c'
 
   url "http://gurpscharactersheet.com/downloads/#{version}/gcs-#{version}-mac.zip"
   name 'GURPS Character Sheet'
