685b3666a5755396bcaa5c3bb653dc06666c892e
Vítor Galvão <info@vitorgalvao.com>
2016-02-14 00:08:46 +0000

    eagle.rb: updated vendor comment

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -2,7 +2,7 @@ cask 'eagle' do
   version '7.5.0'
   sha256 '1abad61ebd8dd42dca64c289dccb38f39c2fe9c67d3313d53ec31e6bad74870b'
 
-  # cadsoft.de is the official download host per the vendor homepage
+  # cadsoft.de was verified as official when first introduced to the cask
   url "http://web.cadsoft.de/ftp/eagle/program/#{version.sub(%r{\.\d+$}, '')}/eagle-mac64-#{version}.zip"
   name 'CadSoft EAGLE'
   homepage 'https://www.cadsoftusa.com/'
