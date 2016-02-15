842b70f2ed443244260eeab76af4e20b024f1d18
Hao Dong <haodong@Haos-iMac.local>
2016-02-15 19:15:24 +0800

    updated launchcontrol (1.26.1)

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,10 +1,10 @@
 cask 'launchcontrol' do
-  version '1.26'
-  sha256 '3a8773ef89110a8fef4b8a5eae78d5ba1c7eaed53f1ad6ea48f2d32f44c8959f'
+  version '1.26.1'
+  sha256 '1fa73795989184bdeeeaa5b4439efaec4c6da90ac4a78cf2ef2c59386928c258'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
-          checkpoint: '498db658a45dba7caecbff61836a59c7eb70cb77ae5ac81ff9b49c68f47023e4'
+          checkpoint: 'ebdd759fb2259691b4714c91576999b3659e13f2c21934064893d30d5a2a85d5'
   name 'LaunchControl'
   homepage 'http://www.soma-zone.com/LaunchControl/'
   license :commercial
