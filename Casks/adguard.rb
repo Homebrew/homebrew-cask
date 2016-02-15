67bb26a30ccefe1b2cb245fd85cd3978d1984437
Victor Popkov <victor@popkov.me>
2016-02-15 21:56:27 +0200

    adguard.rb: upgrade to v1.2.0

--- Casks/adguard.rb
+++ Casks/adguard.rb
@@ -1,10 +1,10 @@
 cask 'adguard' do
-  version '1.1.0'
-  sha256 '4d48a7c048e36d002b371e22765e68fef31dd011db18635bef35063e456c27f8'
+  version '1.2.0'
+  sha256 'd25f2857fea39c9489546e518225ebf1c772f7d032239bc4f7d341ba2ba1f848'
 
   url "https://static.adguard.com/mac/Adguard-#{version}.release.dmg"
   appcast 'https://static.adguard.com/mac/adguard-release-appcast.xml',
-          checkpoint: '6d78234d82be433e81311e85d5b78fd915323bb32fb59c0b17891561367c3c5b'
+          checkpoint: '50d247400968bd4da37b7ed0c871ccbf101643c4637453bdfb4babfb30c6eb50'
   name 'Adguard for Mac'
   homepage 'https://adguard.com/'
   license :commercial
