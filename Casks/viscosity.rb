39a513025936588431b3fc06005c9562ddf1783d
Victor Popkov <victor@popkov.me>
2016-02-16 23:42:14 +0200

    viscosity.rb: upgrade to v1.6

--- Casks/viscosity.rb
+++ Casks/viscosity.rb
@@ -1,10 +1,10 @@
 cask 'viscosity' do
-  version '1.5.11'
-  sha256 'f235d2db655151d15976beee293bc2f6bc85025fdfe83c1b015e0d413547ba95'
+  version '1.6'
+  sha256 '0eb457a5c9ee1ec4b4311402afaef4c6aa1cc19b9beaedacc09afab0edb225bf'
 
   url 'https://www.sparklabs.com/downloads/Viscosity.dmg'
   appcast 'http://swupdate.sparklabs.com/appcast/mac/release/viscosity/',
-          checkpoint: 'a7729c3828c350e03c1e27f3bf2c8f5c55bd7d2533d91b1359a28db34635036f'
+          checkpoint: '36941cdde0505e4bb6619175c9c7109e651b1b91ecdf004758f840738d234951'
   name 'Viscosity'
   homepage 'https://www.sparklabs.com/viscosity/'
   license :commercial
