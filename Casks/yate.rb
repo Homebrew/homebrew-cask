5045f247cb7eb3ee247e90f5ea4d40c6f9a7495c
Victor Popkov <victor@popkov.me>
2016-02-16 23:42:45 +0200

    yate.rb: upgrade to v3.10.1

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,10 +1,10 @@
 cask 'yate' do
-  version '3.10'
-  sha256 '0e91a895d4b5615fe65ce6a1a5b2e928e73de41e3b1c1750bf4b12e6106ad8ba'
+  version '3.10.1'
+  sha256 'd07883ff13bd9f9807d002358434b6704d4b6572ff85839c42a3f944cc8ba072'
 
   url 'https://2manyrobots.com/Builds/Yate/Yate.dmg'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '2ee3046d2801acdb58793da6c666563ff1d66aa1be506967bd8817be9bc1e7c8'
+          checkpoint: '803920ff3437490bc7f2793a41c3fad309522f22a73a6ebb3c2f207537924c98'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
   license :commercial
