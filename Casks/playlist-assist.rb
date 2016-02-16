1ef4c68b65c66befb49bf7a2b6fd245d88e4021f
Victor Popkov <victor@popkov.me>
2016-02-16 23:37:06 +0200

    playlist-assist.rb: upgrade to v1.2.4

--- Casks/playlist-assist.rb
+++ Casks/playlist-assist.rb
@@ -1,10 +1,10 @@
 cask 'playlist-assist' do
-  version '1.2.3'
-  sha256 '8dbc5557919c6b227d560cf5668dd5bc11a4254522d22d72da4816f9b5ffbb58'
+  version '1.2.4'
+  sha256 '85606254173bddf0c282affa24c9db3bce8c15d754a1d9ba3912932878260958'
 
   url "http://dougscripts.com/itunes/scrx/playlistassistv#{version.delete('.')}.zip"
   appcast 'http://dougscripts.com/itunes/itinfo/playlistassist_appcast.xml',
-          checkpoint: 'e9f93840c8cdd03e3d467931a6f73b6d3a00a125c1217452e332b1e8900ea264'
+          checkpoint: '7324b771b62991def6574252bab947e3df630beae5351b7365ad8c9046f6fe29'
   name 'Playlist Assist'
   homepage 'http://dougscripts.com/apps/playlistassistapp.php'
   license :commercial
