e3981c184885fdb54e2f6b69f63cd928c17bdc81
J. Andres Rocha <j.andres.rochagmail.com>
2016-02-15 21:38:48 -0600

    updated gns3 (1.4.1)

--- Casks/gns3.rb
+++ Casks/gns3.rb
@@ -1,6 +1,6 @@
 cask 'gns3' do
-  version '1.3.11'
-  sha256 'baece9951303bca6e887be5fdce730fcc238350446203e227f39906dbd9010f8'
+  version '1.4.1'
+  sha256 'd85924e331d480b5ae1f32ae2ba2980f3a9ccce2d928ee5141fbef753750813f'
 
   url "https://github.com/GNS3/gns3-gui/releases/download/v#{version}/GNS3-#{version}.dmg"
   name 'GNS3'
