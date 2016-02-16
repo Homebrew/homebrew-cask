db4a666977e5a06fd91078dbf9c48cbe45862bae
Jiajun Wang <amesists@gmail.com>
2016-02-16 17:21:01 +0800

    Upgrade MacPass to 0.6.1-alpha

--- Casks/macpass.rb
+++ Casks/macpass.rb
@@ -1,11 +1,11 @@
 cask 'macpass' do
-  version '0.6-alpha'
-  sha256 '36e603a0ee310ccf341b885948910b0d05a0a29fe2aac19b0e12a47a74159e3a'
+  version '0.6.1-alpha'
+  sha256 'f3e4944cb8ebc1da5f605482781f664c4fb7d3815c8354760d37dbc036e07701'
 
   # github.com/mstarke/MacPass was verified as official when first introduced to the cask
   url "https://github.com/mstarke/MacPass/releases/download/#{version}/MacPass-#{version}.zip"
   appcast 'https://github.com/mstarke/MacPass/releases.atom',
-          checkpoint: '81b5266c94e2bf6910661686962296be947bd1a07de612c36b22d8ba53de1ab4'
+          checkpoint: 'fb3d2bfb6c2843f72b910f087c7d56b90c1ff3f8d38b717e469d79cb09093462'
   name 'MacPass'
   homepage 'https://mstarke.github.io/MacPass/'
   license :gpl
