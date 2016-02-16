e3066ad6dc24a4599bddd9d6146ffebfb43d0ae9
Miccal Matthews <miccal@Miccals-iMac.local>
2016-02-16 08:07:02 +0800

    updated djview (4.10.5)

--- Casks/djview.rb
+++ Casks/djview.rb
@@ -1,5 +1,5 @@
 cask 'djview' do
-  version '4.10.4.b'
+  version '4.10.5'
   sha256 'c33178d5ae44a6f43fc878ff458735cd7c825ea56b4a9d589748766184f09cee'
 
   url "https://downloads.sourceforge.net/sourceforge/djvu/DjVuLibre-3.5.27%2BDjView-#{version}-intel64.dmg"
