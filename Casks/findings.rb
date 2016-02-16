b8558055436f1ca0d3091697e3a371985b5218ef
Dustin Wheeler <mskblackbelt@me.com>
2016-02-16 15:38:00 -0500

    Update findings.rb for current download link.
    Update shasum, version number, url string interpolation for current download link.

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -1,8 +1,8 @@
 cask 'findings' do
-  version '1.3.2'
-  sha256 '2e9eb603adeb4b05481ae25af300588a438970f7cd9fd43e3d0721de8b216801'
+  version '1.3.2,3077'
+  sha256 'e76a1c67b0dfaf5807e45c2d0276385afb875612f2693b945cf1e97939b2256f'
 
-  url "http://downloads.findingsapp.com/Findings_#{version.sub(%r{^.*-}, '')}_#{version.sub(%r{-.*$}, '')}.zip"
+  url "http://downloads.findingsapp.com/Findings_#{version.after_comma}_#{version.before_comma}.zip"
   appcast 'http://downloads.findingsapp.com/appcast.xml',
           checkpoint: 'b3f02668f6b91ffb956f52de362e118e296aa0cd6486f8aa0195017fa5a878bb'
   name 'Findings'
