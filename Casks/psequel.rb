3227006aebc214e8a55403567f2d2fb26c14fa15
Brian Wigginton <brianwigginton@gmail.com>
2016-02-16 16:09:36 -0600

    updated psequel (1.4.2)

--- Casks/psequel.rb
+++ Casks/psequel.rb
@@ -1,6 +1,6 @@
 cask 'psequel' do
-  version '1.4.1'
-  sha256 '06bf68f7019a037c9a17a84d4f18df071afb1cfce789d828037f1961a727aae5'
+  version '1.4.2'
+  sha256 '3c9919b67de1f152cd9442c37a7f6a33cad38c268d40c439fd749c65f9461eb7'
 
   url "http://www.psequel.com/download?version=#{version}"
   name 'PSequel'
