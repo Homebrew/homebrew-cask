e236f2958a181f6d05a3e21fc3650f6a99a55e47
Victor Popkov <victor@popkov.me>
2016-02-16 23:39:15 +0200

    sourcetree.rb: upgrade to v2.2

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -3,14 +3,14 @@ cask 'sourcetree' do
     version '1.8.1'
     sha256 '37a42f2d83940cc7e1fbd573a70c3c74a44134c956ac3305f6b153935dc01b80'
   else
-    version '2.1'
-    sha256 '5c50f2ae7d9a4c26af6f1af4b3d30f81a4bfcf6b1386bf8ede73947c1447e305'
+    version '2.2'
+    sha256 'bec6a371c144c7733e3b706b12b7533459fd8884021339c636e37e17a9d6ad22'
   end
 
   # atlassian.com is the official download host per the vendor homepage
   url "https://downloads.atlassian.com/software/sourcetree/SourceTree_#{version}.dmg"
   appcast 'https://www.sourcetreeapp.com/update/SparkleAppcast.xml',
-          checkpoint: 'ef1f286dd1194d6715994ce99332e1db203de7f83555f15c5442773a9de44bf3'
+          checkpoint: 'd1b4f4cb63aafd67821535900087ded0bc91e5261c9f2291c2b5904ca451ae6b'
   name 'Atlassian SourceTree'
   homepage 'https://www.sourcetreeapp.com/'
   license :gratis
