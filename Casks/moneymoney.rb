44751b5c6adde5356800fdad6d7c7f15abb60467
Victor Popkov <victor@popkov.me>
2016-02-16 23:36:44 +0200

    moneymoney.rb: upgrade to v2.2.10

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,10 +1,10 @@
 cask 'moneymoney' do
-  version '2.2.9'
-  sha256 'e9e110bd6c958612505115298ce856c8b99da773990897333aa26c94e3834134'
+  version '2.2.10'
+  sha256 'b2b15d4b68c48d3e967c67bdbc5da70e686893eb1cdd1a00d547c5840dfa3e54'
 
   url 'https://service.moneymoney-app.com/1/MoneyMoney.zip'
   appcast 'https://service.moneymoney-app.com/1/Appcast.xml',
-          checkpoint: '3a7caaacc477862bf85d5551355cf298d86d2ff1838a1bb4c9448b6f99794ffe'
+          checkpoint: 'd7c9a17c81ae68c2aba319c16c9555184f22a5ed4bfc09c95aaf9ab7280c5530'
   name 'MoneyMoney'
   homepage 'https://moneymoney-app.com/'
   license :commercial
