1493aa21cd3b9d232a5267df809264e7d593e470
shian48263 <shian48263@gmail.com>
2016-02-17 09:04:08 +0800

    Update opera.rb

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,5 +1,5 @@
 cask 'opera' do
-  version '	35.0.2066.68'
+  version '35.0.2066.68'
   sha256 '51b7663a5beb458e91ee0815c067756fa6c51dd18f4ffb235c5b62df20494d2f'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
