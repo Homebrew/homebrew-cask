dcd43d06918df502b93d1bc14408b4f85e6e2375
Victor Popkov <victor@popkov.me>
2016-02-17 22:02:19 +0200

    namechanger.rb: upgrade to v3.2.1

--- Casks/namechanger.rb
+++ Casks/namechanger.rb
@@ -1,10 +1,10 @@
 cask 'namechanger' do
-  version '3.1.1'
-  sha256 '5ce678736e3e6fd2eb33bf2a9901e582b53612b5705ac7882ec86bc5a4a188cb'
+  version '3.2.1'
+  sha256 '3866310c60ed224d15d8cd6de864978e11d84d203e2bae6b9db4d77a46d4420f'
 
   url "https://www.mrrsoftware.com/Downloads/NameChanger/Updates/NameChanger-#{version.dots_to_underscores}.zip"
   appcast 'https://mrrsoftware.com/Downloads/NameChanger/Updates/NameChangerSoftwareUpdates.xml',
-          checkpoint: '29fac0db826f5168a5171a31a8c32b6d714e5b9673b03dab21325325ee1b18c2'
+          checkpoint: '427e9fe31bbd34d9ee9d6fb064f6d6c10b0634ec8a7a18ddfc8453b0498f2496'
   name 'NameChanger'
   homepage 'https://mrrsoftware.com/namechanger/'
   license :gratis
