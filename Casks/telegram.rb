a22918b9f6c595bd095c707684a9c38c99b6168f
BruceSun <okampfer@outlook.com>
2016-02-17 03:23:15 +0000

    Upgrade telegram to version 0.9.24

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '0.9.18'
-  sha256 '5faefc8544f5f6aa0b680fc6276183f8c12f9247ad1bfaf78e2e31e812513f3e'
+  version '0.9.24'
+  sha256 '93ef4ba0e84b6b76f9ab26a8a54085c21091667ee20f548b63be2512ab692a36'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
