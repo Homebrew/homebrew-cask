419aa84a8ea252c05dd826093c86f37e531dab43
Victor Popkov <victor@popkov.me>
2016-02-18 23:10:07 +0200

    keeweb.rb: upgrade to v1.0.2

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,11 +1,11 @@
 cask 'keeweb' do
-  version '1.0.1'
-  sha256 'be0cddeb74e5a4f611d449781dad6c43f5aa0c8b3098a1651bbcf37cb38f1dd1'
+  version '1.0.2'
+  sha256 'f19f544c01b8c5d9941f4f23207fde492698dc81024b62420c7896a50d75e311'
 
   # github.com/antelle/keeweb was verified as official when first introduced to the cask
   url "https://github.com/antelle/keeweb/releases/download/v#{version}/KeeWeb.mac.dmg"
   appcast 'https://github.com/antelle/keeweb/releases.atom',
-          checkpoint: '7151b46f830090ec0ca0092e067e62f33fb0e8787b4686e9d6f5e80d6bb2d2e2'
+          checkpoint: '7d11b6e586d40a9395794e22c8bda0440f20039c18f867be4bfe7ac60528a7be'
   name 'KeeWeb'
   homepage 'https://keeweb.info'
   license :mit
