b7df09d1f283a123826d668fa2b7abfb66829d9a
Julio Napurí <julionc@gmail.com>
2016-02-17 22:08:56 -0500

    Upgrade Lacona.app to v.0.4.2

--- Casks/lacona.rb
+++ Casks/lacona.rb
@@ -1,6 +1,6 @@
 cask 'lacona' do
-  version '0.3'
-  sha256 'e45fb6f855ffca635b96f79bdc262d46d1e384157163694f8686c29095c5e450'
+  version '0.4.2'
+  sha256 'b66796e3a489cbfb3ecd01a433da9af5806dcc48048f7175ee3407f2fb3d9d45'
 
   url "http://lacona-download.firebaseapp.com/packages/#{version}/LaconaBeta.zip"
   name 'Lacona'
