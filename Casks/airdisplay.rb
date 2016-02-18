33045f082153e2806134d1f8c06d155a8d2131cd
Victor Popkov <victor@popkov.me>
2016-02-18 22:47:17 +0200

    airdisplay.rb: upgrade to v3.0.3

--- Casks/airdisplay.rb
+++ Casks/airdisplay.rb
@@ -1,10 +1,10 @@
 cask 'airdisplay' do
-  version '3.0.1'
-  sha256 '2da921517a94872b6909b225d8964081a1bad9d2b93be12592bd264665f4b769'
+  version '3.0.3'
+  sha256 'db84a66fe3522929a0afa58a4fe0189977baded89df0035ead1ccd334f7b8126'
 
   url "https://www.avatron.com/updates/software/airdisplay/ad#{version.delete('.')}.zip"
   appcast 'https://avatron.com/updates/software/airdisplay/appcast.xml',
-          checkpoint: '6dd2c05fcf9bbeaa171569d791345e0131af37ec3fca83fe7e80e2135ceee0b8'
+          checkpoint: '938bdb9fbee793dce92818366cb2c19ba84c5b0cd6853fd893897d4a40689bc2'
   name 'Air Display'
   homepage 'https://avatron.com/apps/air-display/'
   license :commercial
