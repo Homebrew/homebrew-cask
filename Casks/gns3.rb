f8c6e0971e2dd7ab054a1b0f2a34bc84e3d873e0
Victor Popkov <victor@popkov.me>
2016-02-18 23:04:22 +0200

    gns3.rb: upgrade to v1.4.2

--- Casks/gns3.rb
+++ Casks/gns3.rb
@@ -1,11 +1,11 @@
 cask 'gns3' do
-  version '1.4.1'
-  sha256 'd85924e331d480b5ae1f32ae2ba2980f3a9ccce2d928ee5141fbef753750813f'
+  version '1.4.2'
+  sha256 'd65728ef2d4eece9aa2c20cff621557b057cc33fa9203e6b1c182b36089cb123'
 
   # github.com/GNS3/gns3-gui was verified as official when first introduced to the cask
   url "https://github.com/GNS3/gns3-gui/releases/download/v#{version}/GNS3-#{version}.dmg"
   appcast 'https://github.com/GNS3/gns3-gui/releases.atom',
-          checkpoint: 'ac780fcc3e3ad41cb9698e14125c2b7703cb11d292a74bf096348ad71cd93ab7'
+          checkpoint: '9f3b5878b56e227b1e25651ebf0824e7e95bcfda6e1c3a19f88b109e0ac8b128'
   name 'GNS3'
   homepage 'https://www.gns3.com/'
   license :gpl
