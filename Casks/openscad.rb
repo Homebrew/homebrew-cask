e5129c2703049ef4e8b8128ab9267ae17746d0f2
Victor Popkov <victor@popkov.me>
2016-02-18 23:17:58 +0200

    openscad.rb: upgrade to v2015.03-3

--- Casks/openscad.rb
+++ Casks/openscad.rb
@@ -1,10 +1,10 @@
 cask 'openscad' do
-  version '2015.03-2'
-  sha256 '5bd717b100f9a23da4539d2b8771d3817b9066aa4e3e0d63e5a8068d2ecaeb86'
+  version '2015.03-3'
+  sha256 'f7de286f2f3e5b625946e93ea27f65ecb59b552da1f33b5e29f29e5ddcbce633'
 
   url "http://files.openscad.org/OpenSCAD-#{version}.dmg"
   appcast 'http://files.openscad.org/appcast.xml',
-          checkpoint: '78064960619c06c2589b2c5d36e7e5ae896e31a88ca633f31083179daf4bf805'
+          checkpoint: 'af4d9b1bfa3e626d8a4916afbc3db629a00080c26d36e0453c46e0a0f7c21595'
   name 'OpenSCAD'
   homepage 'http://www.openscad.org/'
   license :gpl
