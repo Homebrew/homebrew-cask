d1705733d4e1930e2f2858c6f1f6f0c5620cdda7
adidalal <adidalal@users.noreply.github.com>
2016-02-18 01:32:25 -0500

    updated notifyr (1.0.6)

--- Casks/notifyr.rb
+++ Casks/notifyr.rb
@@ -1,6 +1,6 @@
 cask 'notifyr' do
-  version '1.0.5'
-  sha256 '6ca121656f22d079c5f45a617d16a31a40ce3137d9e2a11f05a620719de90500'
+  version '1.0.6'
+  sha256 '2f4dbb12682172de48494f8d70f7ad5216ba7e60c65528d743d20243ffa46b3f'
 
   url "http://getnotifyr.com/app/Notifyr#{version.no_dots}.zip"
   appcast 'http://getnotifyr.com/app/appcast.xml',
