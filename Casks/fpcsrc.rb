8961d4fe4419f3a9e361557c456babeb130628dc
adidalal <adidalal@users.noreply.github.com>
2016-02-19 12:28:46 -0500

    updated fpcsrc (3.0.0-20151207)

--- Casks/fpcsrc.rb
+++ Casks/fpcsrc.rb
@@ -1,9 +1,9 @@
 cask 'fpcsrc' do
-  version '2.6.4-20140421'
-  sha256 'fa7fdfff220241de9e11222e159bcc1b6f3c82588d437191666b5b0ec5fb51cf'
+  version '3.0.0-20151207'
+  sha256 '42184722c8836cce6e4c57e9f9b43b0d0cd1f4f3cbccc5cf2ceb07d193ac6092'
 
   # sourceforge.net/lazarus was verified as official when first introduced to the cask
-  url "http://downloads.sourceforge.net/lazarus/fpcsrc-#{version}-i386-macosx.dmg"
+  url "http://downloads.sourceforge.net/lazarus/fpcsrc-#{version}-macosx.dmg"
   name 'Free Pascal Compiler Source'
   homepage 'http://www.freepascal.org/'
   license :gpl
