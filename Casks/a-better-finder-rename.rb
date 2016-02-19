22d9069a10d7db56ab8cfd45eac2134f7f89be57
adidalal <adidalal@users.noreply.github.com>
2016-02-19 00:24:34 -0500

    updated a-better-finder-rename (10.05)

--- Casks/a-better-finder-rename.rb
+++ Casks/a-better-finder-rename.rb
@@ -1,10 +1,10 @@
 cask 'a-better-finder-rename' do
-  version '10.04'
-  sha256 'a391b5b2f26aef589239472c7de4f0c404939dad237bdbb226fa0317f1037f05'
+  version '10.05'
+  sha256 'da00a1148cca43cafd3f1fe9e5f5909ec90d1f0398ba02e00a834e22693170cf'
 
   url "http://www.publicspace.net/download/ABFRX#{version.major}.dmg"
   appcast "http://www.publicspace.net/app/signed_abfr#{version.major}.xml",
-          checkpoint: 'd45f55620fe76452f8f1a6459c41132092b8c6c4c9af5fbcf1a241ab0221ea04'
+          checkpoint: '96c3d39067ab206d72e66de98bc4e31379cc6a245d891bda4e8a04e133688c13'
   name 'A Better Finder Rename'
   homepage 'http://www.publicspace.net/ABetterFinderRename/'
   license :commercial
