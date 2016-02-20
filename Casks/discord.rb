6be681cad4cf456c2b8f0633b1b79249229333a6
Jerzerak <jerzerak@gmail.com>
2016-02-19 20:41:37 -0500

    Update Discord to 0.0.235

--- Casks/discord.rb
+++ Casks/discord.rb
@@ -1,6 +1,6 @@
 cask 'discord' do
-  version '0.0.233'
-  sha256 '902e6b34733270b54dd4d91d955d2cd61ed9b52a8a6863ea1af2864926ffc65d'
+  version '0.0.235'
+  sha256 'c98b2bd4681719d5faa2f7c17d92d6e712b7f64257ff6bfa00d8ae2b59aea7d0'
 
   url "https://cdn.discordapp.com/apps/osx/#{version}/Discord.dmg"
   name 'Discord'
