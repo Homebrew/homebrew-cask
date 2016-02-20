23f8c7188753bc783dff92f4d61f6983d6d02b30
adidalal <adidalal@users.noreply.github.com>
2016-02-20 15:58:17 -0500

    updated flux (36.6)

--- Casks/flux.rb
+++ Casks/flux.rb
@@ -1,8 +1,8 @@
 cask 'flux' do
-  version '36-6'
+  version '36.6'
   sha256 '613cdb8deee91dd50f266cecc1f10c859d8ba9aa6b0cf2a25156849eec46bb1a'
 
-  url "https://justgetflux.com/mac/Flux#{version}.zip"
+  url "https://justgetflux.com/mac/Flux#{version.dots_to_hyphens}.zip"
   appcast 'https://justgetflux.com/mac/macflux.xml',
           checkpoint: '8c96fbbbea24614bde904646ac83ebd852995bab8268c0c0e5297860c7a0f7e5'
   name 'f.lux'
