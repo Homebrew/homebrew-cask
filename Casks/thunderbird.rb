da8cf8a86abbc057acd8f3ea5bc31505cc996577
Jussi Tuominen <me@jussituominen.me>
2016-02-21 16:41:16 +0200

    Update Thunderbird.app to v38.6.0

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,10 +1,10 @@
 cask 'thunderbird' do
-  version '38.5.1'
-  sha256 '00d84b2dcd3189f42e2a5200bb556c2ac2216437834cf17cc758214ef2c49ef6'
+  version '38.6.0'
+  sha256 'ad6f9a2727c97b9435584eb1588715fde075c92f3ede559685d955a1212f9bf1'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   appcast "https://aus5.mozilla.org/update/3/Thunderbird/#{version}/0/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/Darwin%2015.3.0/default/default/update.xml?force=1",
-          checkpoint: '01d2d46414ba8b8783d0de7d96ae2fb3e0d4e06830c9714fa7be41e1c692f824'
+          checkpoint: 'c829c340e6218d3e47a1044b592feb68c7499625103ae34ba27f94e5ea62eacb'
   name 'Mozilla Thunderbird'
   homepage 'https://www.mozilla.org/en-US/thunderbird/'
   license :mpl
