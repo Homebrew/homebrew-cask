49733d8d9b7e03773c2166ab1eadc4817d78c748
Lasse Bang Mikkelsen <lbm@lbm.dk>
2016-02-21 13:12:11 +0100

    Bump Kodi to 16.0

--- Casks/kodi.rb
+++ Casks/kodi.rb
@@ -1,8 +1,8 @@
 cask 'kodi' do
-  version '15.2'
-  sha256 '9529729cf84bcb5f014ba030bf71f41cd2cb6b325ddd07f8605f37eef66c5760'
+  version '16.0'
+  sha256 '65766b8835a85a48b1d81c9578dfc4c1a1ad2dcdc5450447ebbb44976ebb29bd'
 
-  url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-Isengard-x86_64.dmg"
+  url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-Jarvis-x86_64.dmg"
   name 'Kodi'
   homepage 'https://kodi.tv/'
   license :gpl
