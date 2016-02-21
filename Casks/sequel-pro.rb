e8c9093a4fbb7407475f04134c8c8d4534601595
Victor Popkov <victor@popkov.me>
2016-02-21 14:32:46 +0200

    sequel-pro.rb: upgrade to v1.1.1

--- Casks/sequel-pro.rb
+++ Casks/sequel-pro.rb
@@ -1,11 +1,11 @@
 cask 'sequel-pro' do
-  version '1.1.0.1'
-  sha256 '9b9de84daefb258e57695d035971c2a8fb985c206a8f136de57d3badbf51718d'
+  version '1.1.1'
+  sha256 'e76c20d52d559157deca648c6ca9cf590af0eb3b08313fd0817b19e7fd3a1550'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/sequelpro/sequelpro/releases/download/#{version}/sequel-pro-#{version}.dmg"
   appcast 'https://github.com/sequelpro/sequelpro/releases.atom',
-          checkpoint: 'b34582c47c8472e700a45d398a7e373ce613568fd29016d45703f425aa3a74b6'
+          checkpoint: 'ed6e4b83bcd0ca3b51fec9aa6e7663a0392314cecc40bb3cdae1a2fc18f07d69'
   name 'Sequel Pro'
   homepage 'http://www.sequelpro.com/'
   license :mit
