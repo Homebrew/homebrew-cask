b56d89cf7fea94a0f989b9db8873f0199fb94beb
Yurii Kolesnykov <yurikoles@gmail.com>
2016-02-22 23:43:10 +0200

    galaxy up

--- Casks/gog-galaxy.rb
+++ Casks/gog-galaxy.rb
@@ -1,6 +1,6 @@
 cask 'gog-galaxy' do
-  version '1.1.5.27'
-  sha256 '8dd609a4aa7b9174f3083d83888e5d9016d2874fa05bf6b5b904c4cce1bd74f3'
+  version '1.1.5.27b'
+  sha256 '5e9f58723bd7b7a56fe3fdfc72b0b67e98ba367d3ee1abcc216e1981bd8c7fe2'
 
   url "http://cdn.gog.com/open/galaxy/client/galaxy_client_#{version}.pkg"
   name 'GOG Galaxy Client'
