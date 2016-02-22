d0a90ac760ead7085c5c24debdca5febaf651cbc
iosdeveloper <max.iphone.dev@gmail.com>
2016-02-23 00:25:15 +0100

    updated bitbar (1.6.0)

--- Casks/bitbar.rb
+++ Casks/bitbar.rb
@@ -1,10 +1,10 @@
 cask 'bitbar' do
-  version '1.5.1'
-  sha256 '28a99ad999567fb4399712d78c7592bb78bfd7cf634f0179b129810930959594'
+  version '1.6.0'
+  sha256 '1e177e1b86ed641c9bdd1a537177286447f2085d2f609955a53209fdd16e70bd'
 
   url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar-v#{version}.zip"
   appcast 'https://github.com/matryer/bitbar/releases.atom',
-          checkpoint: '26dc35c3b62a045f9026676618fbf7bb3528333ff3c144c4cd2e001ea046b0d4'
+          checkpoint: '9ca686f6e007eb1db6698cafcd6caad548e74943e102f8ed9c9b5355b7cd988a'
   name 'BitBar'
   homepage 'https://github.com/matryer/bitbar/'
   license :mit
