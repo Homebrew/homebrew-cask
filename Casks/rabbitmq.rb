6d0fdf58de7f0c5551c34b13fcea97da8ade2fb6
José Padilla <jpadilla@webapplicate.com>
2016-02-22 16:41:50 -0400

    Update RabbitMQ.app to v3.6.0-build.3

--- Casks/rabbitmq.rb
+++ Casks/rabbitmq.rb
@@ -1,11 +1,11 @@
 cask 'rabbitmq' do
-  version '3.6.0-build.2'
-  sha256 '25b9bc90bd511285fdbac4be6f6ea44a039e246960b3d70d9e0cb46d27095d18'
+  version '3.6.0-build.3'
+  sha256 '19c9166bad6846cdee92b29f8e22e868a9db41e18565cb6117b20f2b217188ae'
 
   # github.com/jpadilla/rabbitmqapp was verified as official when first introduced to the cask
   url "https://github.com/jpadilla/rabbitmqapp/releases/download/#{version}/RabbitMQ.zip"
   appcast 'https://github.com/jpadilla/rabbitmqapp/releases.atom',
-          checkpoint: '987cafe8e1c71f6932ff80183d2536a32beefad3f382fe0ca5c943fcc7b54e01'
+          checkpoint: '7515bf959b73b956ceb967351c7e299cbb3668a53d35f9c770eb72e00d93ced6'
   name 'RabbitMQ'
   homepage 'https://jpadilla.github.io/rabbitmqapp/'
   license :mit
