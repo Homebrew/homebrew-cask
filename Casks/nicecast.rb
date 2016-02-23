13dbab4cc618870f2bb0ca4ca25a15c3643e8708
Gustavo Barron <cicloid@42fu.com>
2016-02-22 21:49:08 -0600

    updated nicecast (1.11.7)

--- Casks/nicecast.rb
+++ Casks/nicecast.rb
@@ -1,10 +1,8 @@
 cask 'nicecast' do
-  version '1.9.7'
-  sha256 '4bc25f67f9d6dcd832846e6c563edfe9763b8d1fc57e50961049d6889219d47d'
+  version '1.11.7'
+  sha256 'c840bcb98574fd919eae79acd97f89f0a1eac489313edf940f3efce61cfab908'
 
-  url "http://rogueamoeba.com/legacy/downloads/Nicecast-#{version.no_dots}.zip"
-  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Nicecast',
-          checkpoint: '8f6d50cbc369b27a014c2393eaf9f9c7fb35e5467adba4233f5aead1b8dfaeca'
+  url 'https://www.rogueamoeba.com/nicecast/download.php'
   name 'Nicecast'
   homepage 'https://rogueamoeba.com/nicecast'
   license :commercial
