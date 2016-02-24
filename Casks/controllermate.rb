913709e872c43b5b634b88a52c6d2d59fc292028
Victor Popkov <victor@popkov.me>
2016-02-24 07:34:19 +0200

    updated controllermate (4.9.9)

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -1,11 +1,11 @@
 cask 'controllermate' do
-  version '4.9.8'
-  sha256 'ab0e8bf540c04f691d71e39e6fd03350d96112780ee610f3d68fb284bad16765'
+  version '4.9.9'
+  sha256 '24f3dd907df22222b90804db545c998533c86add36f4892292125ad4ccdacc08'
 
   # amazonaws.com/orderedbytes was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/orderedbytes/ControllerMate#{version.delete('.')}.zip"
   appcast 'http://www.orderedbytes.com/sparkle/appcast_cm460.xml',
-          checkpoint: '076cef001c4959fb88e96a163257098feb78ac8ac3435e3ebb92b249a33469dc'
+          checkpoint: '46aa2ba667280e37d3a6deb3bf561d6c2cde2e0bcebda1da9d560f58e4b3834a'
   name 'ControllerMate'
   homepage 'http://www.orderedbytes.com/controllermate/'
   license :freemium
