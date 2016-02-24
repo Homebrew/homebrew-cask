b1eb9603f8e652203e634604f70b4a72cc77c319
Victor Popkov <victor@popkov.me>
2016-02-24 07:28:35 +0200

    updated cakebrew (1.2)

--- Casks/cakebrew.rb
+++ Casks/cakebrew.rb
@@ -1,10 +1,10 @@
 cask 'cakebrew' do
-  version '1.1.5'
-  sha256 '8a49e2bd6172ba7466dbd760242f037da99bf0b327c0ea0ea25f2ef099e89aa9'
+  version '1.2'
+  sha256 '2d9de51fbca270abaf69a7b7fd822fe52790478508ab71401430d13d2bbcd098'
 
   url "https://www.cakebrew.com/files/cakebrew-#{version}.zip"
   appcast 'https://www.cakebrew.com/appcast/profileInfo.php',
-          checkpoint: '73aec6bf287ccff58f6e34ca35798a606b11a5665d52da829f7b5bd0573b4088'
+          checkpoint: 'b21892747ea8b7924813c3c08cd52da136332886961dfef3d52cf53eb5b0de2a'
   name 'Cakebrew'
   homepage 'https://www.cakebrew.com/'
   license :gpl
