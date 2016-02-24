ce45b86d1b7b927ab4ef057431cd5b5a8d4e535e
CODYQX4 <admin@codyqx4.com>
2016-02-24 15:16:06 -0500

    Updated iMazing to 1.5.1

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -1,11 +1,11 @@
 cask 'imazing' do
-  version '1.5.0,1456145979'
-  sha256 '564105da1a6882514044e6d2436571974ee904fa68ee893f6fb9c7cd953ed37e'
+  version '1.5.1,1456163160'
+  sha256 'fdc3789af394e63ebebf835a769a4e0d53b70f753e141d2f1bfe2e02455d4599'
 
   # devmate.com is the official download host per the vendor homepage
   url "https://dl.devmate.com/com.DigiDNA.iMazingMac/#{version.before_comma}/#{version.after_comma}/iMazingforMac-#{version.before_comma}.dmg"
   appcast 'https://updates.devmate.com/com.DigiDNA.iMazingMac.xml',
-          checkpoint: '2bdc038612b28e4323aea8569b14ec457e9c4a20a61a608062bd99c228da94d0'
+          checkpoint: '70f3943034bdd43262a134222225e1267749f36d68a34c13bace974a1d58bc65'
   name 'iMazing'
   homepage 'https://imazing.com/'
   license :commercial
