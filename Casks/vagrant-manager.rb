b7ce8fc9d4b3c3d1f98cd164201b9620b38db012
Chuah Chee Shian <shian48263@gmail.com>
2016-02-25 06:53:04 +0800

    Updated vagrant-manager to 2.5.3

--- Casks/vagrant-manager.rb
+++ Casks/vagrant-manager.rb
@@ -1,11 +1,11 @@
 cask 'vagrant-manager' do
-  version '2.5.1'
-  sha256 '3056e833b9399fbb0b1d2b834ee19597f56c492ab79d222ec4a607f75d566fb6'
+  version '2.5.3'
+  sha256 'c2780c3560f7f27a0a87fa01983ec329ed13b7cb139515cf70415ae959b99562'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/lanayotech/vagrant-manager/releases/download/#{version}/vagrant-manager-#{version}.dmg"
   appcast 'https://github.com/lanayotech/vagrant-manager/releases.atom',
-          checkpoint: '999bd2fd5a8eeae2a819f71523843c2ad139b4ca5576832ed1f4f9a18e547472'
+          checkpoint: '1a416ac6a40fa013b7876d9d02acbe17c7a0988b780bca0baafd8d6fcba45099'
   name 'Vagrant Manager'
   homepage 'http://vagrantmanager.com/'
   license :mit
