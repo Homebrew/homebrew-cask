39d23cbfd10206b1fdb79e30f99ed8fc11d4353f
cooclsee <cooclsee@qq.com>
2016-02-24 10:34:12 +0800

    updated quip (4.5.9)

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -1,6 +1,6 @@
 cask 'quip' do
-  version '4.4'
-  sha256 'ce1e192a74faaf19990db00bbe224217ea59f172abefe8b39259c7e3c04d4042'
+  version '4.5.9'
+  sha256 '16a9e06cbe1b204fefc3f095853da4d600904e0a2b8d109e8e31fd2550cbb079'
 
   # cloudfront.net is the official download host per the vendor homepage
   url "https://d2i1pl9gz4hwa7.cloudfront.net/macosx_#{version}.dmg"
