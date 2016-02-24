2e57385d1bc377ceb8c2c536c9135b94bec08d86
slack4u <slack4u@users.noreply.github.com>
2016-02-24 10:00:11 +0900

    Update Typora (0.9.8.6)

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,10 +1,10 @@
 cask 'typora' do
-  version '0.9.8.5'
-  sha256 'a89895dc2bb55e9b5d85caa8fdc9008e76a16d04c7eb0bc2673ecfad1fa4e20d'
+  version '0.9.8.6'
+  sha256 '94ab9b5656af6dc93b22d19d31c6d16e74e081108d48e7c868b095bd01753c20'
 
   url "https://www.typora.io/download/typora_#{version}.zip"
   appcast 'https://www.typora.io/download/dev_update.xml',
-          checkpoint: 'e3c717ecc8179492764c5f0017451c68f3b4bc8605cac28fd2818de684c8e62f'
+          checkpoint: '7090287277b4a81713299d470bbfc1bb34f3b8985067b4bb981f50c44de0d178'
   name 'Typora'
   homepage 'http://typora.io'
   license :gratis
