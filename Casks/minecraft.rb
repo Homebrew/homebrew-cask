533868ac6d97d4720ae77bbeb0131c6fff31d9c9
Tomohito Yamazaki <t07840ty@gmail.com>
2016-02-25 19:01:54 +0900

    Cannot install minecraft.dmg
    Though I've just tried to install the minecraft, I couldn't install it.
    The file path to minecraft.dmg would be wrong, isn't it?
    Please fixing this bug..

--- Casks/minecraft.rb
+++ Casks/minecraft.rb
@@ -3,7 +3,7 @@ cask 'minecraft' do
   sha256 :no_check
 
   # mojang.com was verified as official when first introduced to the cask
-  url 'https://launcher.mojang.com/download/Minecraft.dmgg'
+  url 'https://launcher.mojang.com/download/Minecraft.dmg'
   name 'Minecraft'
   homepage 'https://minecraft.net/'
   license :commercial
