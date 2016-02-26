443985229ba16e74433e6aa99e288b83df82bb3e
Victor Popkov <Dragon.vctr@gmail.com>
2016-02-26 20:51:36 +0200

    updated marsedit (3.7.5)

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,10 +1,10 @@
 cask 'marsedit' do
-  version '3.7.4'
-  sha256 '2133ccfacd3243f96cb4417733e028762f68e91fd2a0ebf8f88d8f014590a877'
+  version '3.7.5'
+  sha256 '881040881f06dedbddc56e81e0b33179eecf50344f2fdcd10770782887aa9507'
 
   url "http://www.red-sweater.com/marsedit/MarsEdit#{version}.zip"
   appcast 'https://www.red-sweater.com/marsedit/appcast3.php',
-          checkpoint: 'a5300508cfcfea4d58209bd488c0eb6dbf1ccb20a9fef344430993eeb8a87692'
+          checkpoint: 'f56ddc257b50c98b0eb8f37bb5002f48acfa07a4bad0b831c56581a5939c83bb'
   name 'MarsEdit'
   homepage 'https://www.red-sweater.com/marsedit/'
   license :commercial
