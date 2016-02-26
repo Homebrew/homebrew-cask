8f24e08fc4974e3a3405c1a8cb62b0d02804f4bc
seanzxx <seanzxx@me.com>
2016-02-26 15:01:34 -0800

    Update data grip to latest version 1.0.3

--- Casks/datagrip.rb
+++ Casks/datagrip.rb
@@ -1,6 +1,6 @@
 cask 'datagrip' do
-  version '1.0.2'
-  sha256 '91585f6d83fb0f5673f9f6b3748c0201b14501e4cd23681eb10ef7d7a6ed565c'
+  version '1.0.3'
+  sha256 '8aa557c3eef9749f82ada203af3f8f51e2601230cb169589da26c2d2449534bd'
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version}-custom-jdk-bundled.dmg"
   name 'DataGrip'
