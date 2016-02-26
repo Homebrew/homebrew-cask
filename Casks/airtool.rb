2528a1d62d668d23a0d7ccf8d21f99ebea23e29a
Victor Popkov <Dragon.vctr@gmail.com>
2016-02-26 20:11:26 +0200

    updated airtool (1.3)

--- Casks/airtool.rb
+++ Casks/airtool.rb
@@ -1,11 +1,11 @@
 cask 'airtool' do
-  version '1.2.2'
-  sha256 'faa93364724f3544be52f4bec04836a58cc63fd94b6b6f03424058c4e61ee076'
+  version '1.3'
+  sha256 '9f436539883a4a7318bc71ea648eb2d790134efdd32186397af74c497316c07e'
 
   # amazonaws.com/adriangranados was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/adriangranados/airtool_#{version}.pkg"
   appcast 'https://www.adriangranados.com/appcasts/airtoolcast.xml',
-          checkpoint: '9744ed49ec6bf20d5eb90bdc0fffdc79383f4660a6ba22effd4399b5297feb36'
+          checkpoint: 'b9b8f918d1872a3f4c4230ce9c20b765f82d9c468ec4cdb988ad01e9ca386a38'
   name 'Airtool'
   homepage 'http://www.adriangranados.com/apps/airtool'
   license :gratis
