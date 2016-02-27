1e0d2786fd74f481cafd5cbc823abcc3d00e126a
Vítor Galvão <info@vitorgalvao.com>
2016-02-27 15:49:15 +0000

    hammerspoon.rb: updated vendor comment

--- Casks/hammerspoon.rb
+++ Casks/hammerspoon.rb
@@ -2,7 +2,7 @@ cask 'hammerspoon' do
   version '0.9.45'
   sha256 'b6cdb1ebe49126d0cc2c7ba380b19ee824297c87b404a04f5236d1b631cf45b9'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/Hammerspoon/hammerspoon was verified as official when first introduced to the cask
   url "https://github.com/Hammerspoon/hammerspoon/releases/download/#{version}/Hammerspoon-#{version}.zip"
   appcast 'https://github.com/Hammerspoon/hammerspoon/releases.atom',
           checkpoint: 'e977818cc6c0525dc00dcc8302d517143659c2deac33dde70e850090f148bed0'
