62a1eaeaded17b1ae68d3b1b9e9fccc0991e7f6a
Vítor Galvão <info@vitorgalvao.com>
2016-02-27 15:31:49 +0000

    glo-bible.rb: updated vendor comment

--- Casks/glo-bible.rb
+++ Casks/glo-bible.rb
@@ -2,7 +2,7 @@ cask 'glo-bible' do
   version :latest
   sha256 :no_check
 
-  # llnwd.net is the official download host per the vendor homepage
+  # immersion.vo.llnwd.net was verified as official when first introduced to the cask
   url 'http://immersion.vo.llnwd.net/o42/pub/glo/mac/en-us/Glo.dmg'
   name 'Glo Bible'
   homepage 'https://globible.com/gloformac/'
