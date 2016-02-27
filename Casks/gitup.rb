4f30c37a6771e0bc3747e48642d3ddfde701bc72
Vítor Galvão <info@vitorgalvao.com>
2016-02-27 15:30:02 +0000

    gitup.rb: updated vendor comment

--- Casks/gitup.rb
+++ Casks/gitup.rb
@@ -2,7 +2,7 @@ cask 'gitup' do
   version '1.0.3'
   sha256 '58c51e521a51e4b92bbc9d1922b315d5d2086fa0f7e0e5b0186f1ee2c2aececc'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # s3-us-west-2.amazonaws.com/gitup-builds was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/gitup-builds/stable/GitUp.zip'
   appcast 'https://github.com/git-up/GitUp/releases.atom',
           checkpoint: 'c24e47e6c6f5c4e7c4945c43a07b6c2fbfe2c040d11efbd978d261f609452e36'
