4a6f2692e044c170330f463fbe3f0a7b7e72ed2b
Vítor Galvão <info@vitorgalvao.com>
2016-02-27 12:24:44 +0000

    updated eloquent (2.5.1)

--- Casks/eloquent.rb
+++ Casks/eloquent.rb
@@ -1,12 +1,12 @@
 cask 'eloquent' do
-  version '2.4.9'
-  sha256 'c792bc3f4685f49052d105c89c9edc830ccc27f1bec9bbe3b841c976b0d0b249'
+  version '2.5.1'
+  sha256 'f70d242b0afaa2c2067383ea51ccb0e43b93cc1013a779030150af8d35a82f45'
 
-  url "https://launchpad.net/eloquent/trunk/#{version}/+download/Eloquent-#{version}.app.zip"
-  appcast 'http://www.eloquent-bible-study.eu/Eloquent_L2_Appcast.xml',
-          checkpoint: '88014a9ab9d76ad70e66bc91fb9ed9931310d2e46810cd01b759b8266c02e9c9'
+  url "https://github.com/mdbergmann/Eloquent/releases/download/#{version}/Eloquent-#{version}.app.zip"
+  appcast 'https://github.com/mdbergmann/Eloquent/releases.atom',
+          checkpoint: '6e60338304dbce613859c0883d191d7ec2dc81e8fec7bb431c6f05818601c994'
   name 'Eloquent'
-  homepage 'https://launchpad.net/eloquent'
+  homepage 'https://github.com/mdbergmann/Eloquent'
   license :gratis
 
   app 'Eloquent.app'
