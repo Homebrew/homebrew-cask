1f49f195165590bb3b5790700a12311c09cba561
System Administrator <root@GuanMBP.local>
2016-02-28 17:44:43 +0800

    updated quip (4.5.13)

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -1,6 +1,6 @@
 cask 'quip' do
-  version '4.5.9'
-  sha256 '16a9e06cbe1b204fefc3f095853da4d600904e0a2b8d109e8e31fd2550cbb079'
+  version '4.5.13'
+  sha256 'a8a7db6d32156d4d3a5acd43894ec6fda743688057da5eed3f5f2804afb806ff'
 
   # cloudfront.net is the official download host per the vendor homepage
   url "https://d2i1pl9gz4hwa7.cloudfront.net/macosx_#{version}.dmg"
