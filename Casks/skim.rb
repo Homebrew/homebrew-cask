1c174068166049b3ecf5155900f0fdb90619abd8
Victor Popkov <Dragon.vctr@gmail.com>
2016-02-29 23:26:26 +0200

    updated skim (1.4.18)

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,10 +1,10 @@
 cask 'skim' do
-  version '1.4.17'
-  sha256 '6d48d2bdec04ccd778a2b173f8d7173dd3d474b32fc4f3c260120304d598795a'
+  version '1.4.18'
+  sha256 'be371b4d4cc6c94818a5396883d5d9f9fe5fde32f61734b70c454009cf85590e'
 
   url "http://downloads.sourceforge.net/project/skim-app/Skim/Skim-#{version}/Skim-#{version}.dmg"
   appcast 'http://skim-app.sourceforge.net/skim.xml',
-          checkpoint: '9441300e6f5beb35c617348432832f602295984e4a06dd071edab85b892af2e1'
+          checkpoint: '4e90b49f9dd366419174763d95bc55d5f5236bd29662c9c5fd23c6dc9aa0d019'
   name 'Skim'
   homepage 'http://skim-app.sourceforge.net/'
   license :bsd
