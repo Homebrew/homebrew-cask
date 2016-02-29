4c5e25e9081cf008b1467099b8ab091fa1df60a1
Victor Popkov <Dragon.vctr@gmail.com>
2016-02-29 23:29:16 +0200

    updated swinsian (1.11.8)

--- Casks/swinsian.rb
+++ Casks/swinsian.rb
@@ -1,10 +1,10 @@
 cask 'swinsian' do
-  version '1.11.7'
-  sha256 'c8d40f7486ea465adb571f390fe25c4a3fcedce978684e2d4872836c928efdbc'
+  version '1.11.8'
+  sha256 'aead098039dffaeabb9a88367ee5f60366c09b6e09a20e3d1e41d5a43e0a0d31'
 
   url "https://www.swinsian.com/sparkle/Swinsian_#{version}.zip"
   appcast 'https://www.swinsian.com/sparkle/sparklecast.xml',
-          checkpoint: 'a36dbca294e33d5450d0a3f04ad3e8bc7e1b2869d4f1ebfefa7a48067866b5bc'
+          checkpoint: '97e2fd4d039255f95503061d36a36c6df370374e8f6fc2b60bcd3d05458b5191'
   name 'Swinsian'
   homepage 'https://swinsian.com'
   license :commercial
