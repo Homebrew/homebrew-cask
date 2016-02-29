25611b0abfc4501f6acfba6547fd538d8c401a98
Victor Popkov <Dragon.vctr@gmail.com>
2016-02-29 23:28:38 +0200

    updated sqleditor (2.9.16)

--- Casks/sqleditor.rb
+++ Casks/sqleditor.rb
@@ -1,10 +1,10 @@
 cask 'sqleditor' do
-  version '2.9.15'
-  sha256 'a007075ca5acd4e79ea47bc5cee41de3cc5684e0074c0b863d0b4a41b8e96606'
+  version '2.9.16'
+  sha256 '68ebeb3620a304fe2aa24e96f00abc8a97f77a16b8df1042b9ba50df2ec849e6'
 
   url "https://www.malcolmhardie.com/sqleditor/releases/#{version}/SQLEditor-#{version.gsub('.', '-')}.zip"
   appcast 'https://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml',
-          checkpoint: '8187506d61afd22bbad51c21b9b838383fb6d475d9878a7d6b8ca9bd91f9f577'
+          checkpoint: '70bb55d075576012f45d3b420d13aece144e8c876f93b169adbf9dc70be324a8'
   name 'SQLEditor'
   homepage 'https://www.malcolmhardie.com/sqleditor/'
   license :commercial
