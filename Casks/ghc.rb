e8779c168e6b4f20c27b12e62ca4697a6df4b0fc
Victor Popkov <Dragon.vctr@gmail.com>
2016-02-29 23:23:34 +0200

    updated ghc (7.10.3-r1)

--- Casks/ghc.rb
+++ Casks/ghc.rb
@@ -1,11 +1,11 @@
 cask 'ghc' do
-  version '7.10.3-r0'
-  sha256 '51080b4c4df722497ec5e48138fff2f461e2eee32539889fdedf99b16f485096'
+  version '7.10.3-r1'
+  sha256 '4daf43e58da7288dbbc90b5a9b4cfcdab8b3b1d7f09941fab61d45a660e837c4'
 
   # github.com/ghcformacosx/ghc-dot-app was verified as official when first introduced to the cask
   url "https://github.com/ghcformacosx/ghc-dot-app/releases/download/v#{version}/ghc-#{version}.zip"
   appcast 'https://github.com/ghcformacosx/ghc-dot-app/releases.atom',
-          checkpoint: '9de76eb2e6ad24851c66c3f4c1ecd28a484755d2712630ebff0d957b7e1d60e5'
+          checkpoint: 'b58f9a3b2f5855a120d5fc42d42faea139f4da6c6c550b912b0e6868899c6664'
   name 'GHC'
   homepage 'https://ghcformacosx.github.io/'
   license :oss
