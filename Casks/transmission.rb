95d80411b7598c6c71577b61e2be10c656ba3e2b
miccal <miccal.matthews@gmail.com>
2016-02-29 08:31:13 +0800

    updated transmission (2.90)

--- Casks/transmission.rb
+++ Casks/transmission.rb
@@ -1,11 +1,11 @@
 cask 'transmission' do
-  version '2.84'
-  sha256 '53d08a55a5ca55010d409acb10f0285a649b8879085cad83f2cbcb7faa489ad5'
+  version '2.90'
+  sha256 '29a27fa1905c7a52d76f1ce96ae45a736786cd0230e0bd9413508bcf52af7794'
 
   # cachefly.net is the official download host per the vendor homepage
   url "https://transmission.cachefly.net/Transmission-#{version}.dmg"
   appcast 'https://update.transmissionbt.com/appcast.xml',
-          checkpoint: '430a5151b1e38fd2d6f544c8877b2cdd4deda6dc4b3cc6a6620550de4a8fdfec'
+          checkpoint: 'bb7200054491881444262022b8c6fee1786ccac4d2bcaabd52ac0d066d7020c4'
   name 'Transmission'
   homepage 'http://www.transmissionbt.com/'
   license :gpl
