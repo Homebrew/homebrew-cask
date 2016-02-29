29a85a6fa566363c4174cdf1b467fe6594fca6b9
Victor Popkov <Dragon.vctr@gmail.com>
2016-02-29 23:24:04 +0200

    updated hype (3.5.1-526)

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -1,10 +1,10 @@
 cask 'hype' do
-  version '3.5.0-518'
-  sha256 '66d2df2b982aa4634583381046a7c3bbd93b9fe8e7232663d51fdc8bc52358f3'
+  version '3.5.1-526'
+  sha256 'a6bbfef822d08c94b02a715c1f76bb95174cc2be92edcb141ca9c0563478de65'
 
   url "http://static.tumult.com/hype/download/Hype-#{version.sub(%r{^.*?-}, '')}.app.zip"
   appcast 'https://tumult.com/hype/appcast_hype2.xml',
-          checkpoint: '1afa2513499a16bd79404c548e5be9c0f5abba40553d4d231236eb504ac31ef7'
+          checkpoint: '6dacb16cbcb5a3bc2790d81740a18ecded789c0b72257063b710a8c8a105c0b6'
   name 'Tumult Hype'
   homepage 'https://tumult.com/hype/'
   license :commercial
