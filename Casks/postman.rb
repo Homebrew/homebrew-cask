13e6b5abba8592850674160aa3cbd7210aae65cd
Robert Burns <burnsra@gmail.com>
2016-02-29 14:14:09 -0500

    Update postman to 4.0.3

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,11 +1,11 @@
 cask 'postman' do
-  version '4.0.2'
-  sha256 '8223cb89f6ebf937ea9a3237612b4ea52742c39e8f8745264e29ec4ef070467d'
+  version '4.0.3'
+  sha256 '465c0ed0e07be1aea755c489d1d62f8fa5caf76f10ff15564e4707bd6a24f6d3'
 
   # s3.amazonaws.com/postman-electron-builds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/postman-electron-builds/mac/Postman-osx-#{version}.zip"
   appcast 'https://app.getpostman.com/api/electron_updates_auto',
-          checkpoint: 'cd3facb38b5b966e444fcb92e1fa3908e8c3d7be8290328d23a6596c9692bddb'
+          checkpoint: 'b87bee4e410346a559c18ea9f31fac39af357c05e2be0771827f4784b8db6772'
   name 'Postman'
   homepage 'https://www.getpostman.com/'
   license :closed
