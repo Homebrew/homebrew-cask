15020b762f2da5879330ef8b26da029592c27fa4
Victor Popkov <Dragon.vctr@gmail.com>
2016-02-29 23:16:20 +0200

    updated lightpaper (1.3.1)

--- Casks/lightpaper.rb
+++ Casks/lightpaper.rb
@@ -1,11 +1,11 @@
 cask 'lightpaper' do
-  version '1.3'
-  sha256 '0d30ea2e13c3ac7e541b234a97e14cb3d2ffb4afbf233dc25722710d81816916'
+  version '1.3.1'
+  sha256 '4df2fa5a481fc7ed9e9b3afc548a78199b12c7a6006011f0642e93339d06bcc3'
 
   # hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8 was verified as official when first introduced to the cask
   url 'https://rink.hockeyapp.net/api/2/apps/789cfa8846464727ae0fdb176ec8d3c8?format=zip'
   appcast 'https://dl.dropboxusercontent.com/u/83257/LpMacUpdates/lightpaper_appcast.xml',
-          checkpoint: 'd84e000883189b7e33f229aa8a97a325467f9f4fb205fe53786affbafa6a9565'
+          checkpoint: '82bbbb41cd12179fd15583927586a2a4a60d0237be095f30c795aad9a7aa85cd'
   name 'LightPaper'
   homepage 'http://lightpaper.42squares.in/'
   license :commercial
