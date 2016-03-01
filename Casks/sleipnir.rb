7fc1c1de36a3c300d0a3724d3c286129dceae041
Victor Popkov <Dragon.vctr@gmail.com>
2016-03-01 22:05:41 +0200

    updated sleipnir (4.5.2)

--- Casks/sleipnir.rb
+++ Casks/sleipnir.rb
@@ -1,10 +1,10 @@
 cask 'sleipnir' do
-  version '4.5.1'
-  sha256 'e0f59e6f3ecc59fbec4cea6b9295f7f5639769845d0a877c9cca5bb329af9327'
+  version '4.5.2'
+  sha256 '0b0871f813884c15ecea1e990fb6e1bda7cc5d3ae1fedf9de997db83036924c1'
 
   url 'http://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
   appcast 'http://update.fenrir.co.jp/smartupdate/mac/sleipnir/appcast.xml',
-          checkpoint: '8110c0f3ca1df9c454f9e936b49bd09de2d673a3e6be8700e1e9a83fc5ca0498'
+          checkpoint: 'c9bd4c2f4630e9390d1adea5ae2847499e1ae81fee5672d2c6167349a3418482'
   name 'Sleipnir'
   homepage 'http://www.fenrir-inc.com/sleipnir/'
   license :closed
