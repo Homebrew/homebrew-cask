c5b912b16a29fa6d9efcbdd4cccc120eccc66620
Victor Popkov <Dragon.vctr@gmail.com>
2016-03-01 22:29:04 +0200

    updated icons8 (5.2)

--- Casks/icons8.rb
+++ Casks/icons8.rb
@@ -1,14 +1,14 @@
 cask 'icons8' do
   # note: "8" is not a version number, but an intrinsic part of the product name
-  version '5.1'
-  sha256 '1096a96c63af0b1993994364c8d17c9f2f1d353da4ae927775c835e6b63081f0'
+  version '5.2'
+  sha256 '05f9a271624e3e1321067ee549110329431c609c07ddde00b568955e0318dd8c'
 
-  url 'http://icons8.com/icons8_mac_app'
+  url 'https://icons8.com/icons8_mac_app'
   appcast 'https://maxcdn.icons8.com/download/icons8_cast.xml',
-          checkpoint: '1f0768b3ef85dc432e746e7cd33e9d0c473c38caeeda900ed09131116d92c00b'
+          checkpoint: 'e913bd010e996305fd490a300b73149330ae378988097ddc81c88f40547f1453'
   name 'Icons8 App'
   homepage 'https://icons8.com/'
   license :freemium
 
-  app 'Icons8 App.app'
+  app 'Icons8.app'
 end
