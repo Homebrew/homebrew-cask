908c6b29557ffcc191372ee82288151279b0d8ae
Ioannis Poulakas <giannis.p@gmail.com>
2016-03-01 19:11:56 +0200

    Update sketch to 3.6.0

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -1,10 +1,10 @@
 cask 'sketch' do
-  version '3.5.2'
-  sha256 'fac9182c50a96cbc144bddb3984c82195980d6a4dc92fa3c2e40a59574f40a81'
+  version '3.6.0'
+  sha256 '26325abe8b9384d4214892c61a7f541c1e738482af4aa80131f4f43fce34c9cf'
 
   url 'http://www.sketchapp.com/static/download/sketch.zip'
   appcast "http://www.sketchapp.com/appcast#{version.major}.xml",
-          checkpoint: 'c9f08a2d76447a3f3fac479f053fb2612441c214454f151b85c460b9f726390d'
+          checkpoint: 'd3e09bb9a6c5f670bceda6ffcc6568c3aabf299e07a300125fdab18dda9b7c53'
   name 'Sketch'
   homepage 'http://www.sketchapp.com/'
   license :commercial
