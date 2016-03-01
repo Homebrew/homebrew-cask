7fafc329663c90b4c119a3c1633e3b672bb0fdc9
Victor Popkov <Dragon.vctr@gmail.com>
2016-03-01 22:00:45 +0200

    updated purescript (0.8.2)

--- Casks/purescript.rb
+++ Casks/purescript.rb
@@ -1,11 +1,11 @@
 cask 'purescript' do
-  version '0.8.0'
-  sha256 'a4eb3e55f141e6bebd2e148797d525ea1afe053831a6f0972c49f3bbf8a7243f'
+  version '0.8.2'
+  sha256 '74d1c270fa7b47222c50b8937563348b129c81c237aeb35a01a5fa54aff90867'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/purescript/purescript/releases/download/v#{version}/macos.tar.gz"
   appcast 'https://github.com/purescript/purescript/releases.atom',
-          checkpoint: '3e28bec2e4051e846cd4778884f9d690464aeae3b4535270a8812a9480c53c17'
+          checkpoint: '4af76b3b642bad5a57a49ecafc167e3ea928be5bd441c8269479c9e31754300f'
   name 'PureScript'
   homepage 'http://purescript.org'
   license :mit
