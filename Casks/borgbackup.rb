9bd9f752334ffe4ea06725e06482731074dfe38d
Markus Hubig <mhubig@gmail.com>
2016-03-02 11:54:52 +0100

    updated borgbackup (1.0.0rc2)

--- Casks/borgbackup.rb
+++ Casks/borgbackup.rb
@@ -1,10 +1,10 @@
 cask 'borgbackup' do
-  version '0.30.0'
-  sha256 'fe8dc32f89f56f96ee350a7989bc761520db911669e4b08cedcc8da60a4bd9dd'
+  version '1.0.0rc2'
+  sha256 'cb8659f2264903431eab9d4d4e80b989dd0a77fa222d5d5c3e62729091d87b5a'
 
   url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
   appcast 'https://github.com/borgbackup/borg/releases.atom',
-          checkpoint: 'aa2777f9a7969845e2719c9b8d50ca70819486c59701b9c23035dfabddc62832'
+          checkpoint: 'da8ffee5662c227726da623e83527719b01a97ed3e0b7c2344c7b1c4baaa9dfa'
   name 'BorgBackup'
   homepage 'https://borgbackup.readthedocs.org/en/stable/'
   license :bsd
