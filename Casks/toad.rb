7d0b3a45db2688a79b7b8e2e4cfb656bbeb997fe
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-03-02 12:41:49 +0700

    Add Toad.app v2.3.6

---
+++ Casks/toad.rb
@@ -0,0 +1,12 @@
+cask 'toad' do
+  version '2.3.6'
+  sha256 '012b866e16a33293dc3ff73a4864f5ae19213b7e6f3032b37df1f712a2c0f898'
+
+  # http://community-downloads.quest.com/toadsoft/ was verified as official when first introduced to the cask
+  url "http://community-downloads.quest.com/toadsoft/toadmacedition/ToadMacEdition_#{version.no_dots}.zip"
+  name 'Toad'
+  homepage 'https://www.toadworld.com/products/toad-mac-edition'
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'Toad.app'
+end
