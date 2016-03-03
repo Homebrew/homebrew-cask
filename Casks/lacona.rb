a899f85ea479951282d1ae3e6c53a7bbcb2caa0f
Julio Napurí <julionc@gmail.com>
2016-03-03 15:37:17 -0500

    Upgrade Lacona.app to v.0.5.0

--- Casks/lacona.rb
+++ Casks/lacona.rb
@@ -1,14 +1,18 @@
 cask 'lacona' do
-  version '0.4.2'
-  sha256 'b66796e3a489cbfb3ecd01a433da9af5806dcc48048f7175ee3407f2fb3d9d45'
+  version '0.5.0'
+  sha256 '6c541158e36d41d4cb81b7dbdb4c33703391fd085bd5084b6bd9fc78ff47bb2a'
 
   # lacona-download.firebaseapp.com was verified as official when first introduced to the cask
   url "http://lacona-download.firebaseapp.com/packages/#{version}/LaconaBeta.zip"
-  appcast 'https://raw.githubusercontent.com/lacona/LaconaApp/master/CHANGELOG.md',
-          checkpoint: 'b3c79253c0d5213a75f56ff72f35634cac1805d37b5cab160d11fa8c23bd39f1'
+  appcast 'https://lacona-download.firebaseapp.com/appcast.xml',
+          checkpoint: 'f760c7e4bd14dee76ba4fec36661e187999f8b61b3f41d3b3ff07e887c9c39c7'
   name 'Lacona'
   homepage 'http://www.lacona.io'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Lacona.app'
+
+  postflight do
+    suppress_move_to_applications
+  end
 end
