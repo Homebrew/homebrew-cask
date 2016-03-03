5ed04dd761a19cd8a2b3c33aa987dc62985a28b4
Alexander Frolov <thephilosoft@gmail.com>
2016-03-03 12:42:40 +0300

    update filezilla to 3.16.0

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -3,8 +3,8 @@ cask 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.15.0.2'
-    sha256 'f86d9479026a731ef682707fe4a1372d0c1aae09bd81f5c1a564c3fa934d2858'
+    version '3.16.0'
+    sha256 'bbf7fbb3b16ee0669352bb2087fcc039ac038f09f6e4f3c87a4b0f4b8bd70118'
   end
 
   # sourceforge.net/project/filezilla was verified as official when first introduced to the cask
