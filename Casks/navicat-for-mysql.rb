4b772e0d5486ff4f585008dcd0d91ea16e4e50dd
adidalal <adidalal@users.noreply.github.com>
2016-03-03 17:29:41 -0500

    updated navicat-for-mysql (11.2.9)

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-mysql' do
-  version '11.2.7'
-  sha256 '82d3f2b3f52c4af7698339170675141582080bf6501c2b6c7b4197be2ea0ca8e'
+  version '11.2.9'
+  sha256 '1f5f4169b7de642ad3ae05189486edbab5e3850f9e83d9e01901f715198ea818'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
   name 'Navicat for MySQL'
