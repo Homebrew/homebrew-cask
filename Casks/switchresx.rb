90cb015fb41967506647832f613baaa1d2012d96
Hao Dong <haodong@Haos-iMac.local>
2016-03-03 23:33:22 +0800

    updated switchresx (4.5.3)

--- Casks/switchresx.rb
+++ Casks/switchresx.rb
@@ -1,5 +1,5 @@
 cask 'switchresx' do
-  version '4.5.2'
+  version '4.5.3'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "http://www.madrau.com/data/switchresx/SwitchResX#{version.to_i}.zip"
