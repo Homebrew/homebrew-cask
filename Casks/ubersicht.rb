29e754e8753bc3e3de5c7aa1d26105e9fe9509ca
miccal <miccal.matthews@gmail.com>
2016-03-03 20:26:11 +0800

    updated ubersicht (0.8.36)

--- Casks/ubersicht.rb
+++ Casks/ubersicht.rb
@@ -1,10 +1,10 @@
 cask 'ubersicht' do
-  version '0.8.35'
-  sha256 'b2336618b06372da601d1f10f1b4db369f61bd1aadd0fcc3d349525656a56cb3'
+  version '0.8.36'
+  sha256 'd22eb580578e30b3353fef05c35b37e0fac95b986b9bd7d268485e5ed83ec752'
 
   url "http://tracesof.net/uebersicht/releases/Uebersicht-#{version}.app.zip"
   appcast 'http://tracesof.net/uebersicht/updates.xml.rss',
-          checkpoint: '275f6dc25a559f9677b1c81dd8a60825695342f1a5f00aadfe015b91e3e069bf'
+          checkpoint: '584d7e2f46b097a52df3d036e7ea15b49546e9ba325ca1ae6a55c4e50a477104'
   name 'Übersicht'
   homepage 'http://tracesof.net/uebersicht'
   license :gpl
