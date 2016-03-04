34d01bbff3d7cadc597e63c394ced066b05c3390
Victor Popkov <Dragon.vctr@gmail.com>
2016-03-04 19:19:56 +0200

    icab.rb: fix vendor comment

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -2,7 +2,7 @@ cask 'icab' do
   version '5.6.2'
   sha256 'b764f8cd53f8f6807a40e2fa959129d96b84f44055783b0db551221996ef11ba'
 
-  # clauss-net.de is the official download host per the vendor homepage
+  # icab.clauss-net.de was verified as official when first introduced to the cask
   url "http://icab.clauss-net.de/icab/iCab_#{version}_Intel.zip"
   name 'iCab'
   homepage 'http://www.icab.de/'
