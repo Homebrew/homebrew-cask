1d8cd9b61027141ed9b2e286ef7cf8bf1eea53a3
Vítor Galvão <info@vitorgalvao.com>
2016-03-04 17:28:08 +0000

    updated arq (4.15.1)

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,10 +1,10 @@
 cask 'arq' do
-  version '4.15'
-  sha256 '72ebd8ce6afaabbede487ed99f2b88f9418d741a3bceb685c1288a9f71d4a12f'
+  version '4.15.1'
+  sha256 '537d7872e2843a191bb1090e708bb48aedfa83dd2db616da25b792c013b8eb33'
 
   url "https://www.arqbackup.com/download/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arq#{version.major}.xml",
-          checkpoint: '48c791e8e25fef54b1f2ebd4405ecedd36aa7f7b7b197f04de85627a2ebf1b02'
+          checkpoint: '537720fc6b4b69b44ccc36ff81198d378fedae8a2783589cbbd8d730a98396ea'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
   license :commercial
