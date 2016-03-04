9b88d83f6ffa5525408d745b505b0f106e1083b9
adidalal <adidalal@users.noreply.github.com>
2016-03-04 15:19:23 -0500

    updated unity-android-support-for-editor (5.3.3f1,910d71450a97)

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -1,8 +1,8 @@
 cask 'unity-android-support-for-editor' do
-  version '5.3.1f1'
-  sha256 'c805d5ea4bf3ab089909427bc1ba6af82e05fd04cac832b3f154f27821b6d54a'
+  version '5.3.3f1,910d71450a97'
+  sha256 '06b9efa7e2f01c91b4ef64535e0d1a9ef87bbfca9e6064b71ebdfce999aa7e0a'
 
-  url "http://netstorage.unity3d.com/unity/cc9cbbcc37b4/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version}.pkg"
+  url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity Android Build Support'
   homepage 'https://unity3d.com/unity/'
   license :commercial
