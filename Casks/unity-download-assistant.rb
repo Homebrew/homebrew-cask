2dba1ede87a7d1b0baa02bd22f0c302b75e9d3f8
adidalal <adidalal@users.noreply.github.com>
2016-03-04 15:07:14 -0500

    updated unity-download-assistant (5.3.3f1,910d71450a97)

--- Casks/unity-download-assistant.rb
+++ Casks/unity-download-assistant.rb
@@ -1,8 +1,8 @@
 cask 'unity-download-assistant' do
-  version '5.3.1f1'
-  sha256 '6c2a31e6241d2b511a53b9cb88713a38ded25b7f29de540eaf281f44441287f5'
+  version '5.3.3f1,910d71450a97'
+  sha256 'e57a5a6af3250a09a113187114375461ca97612e8265818582f8815398bc17ec'
 
-  url "http://netstorage.unity3d.com/unity/cc9cbbcc37b4/UnityDownloadAssistant-#{version}.dmg"
+  url "http://netstorage.unity3d.com/unity/#{version.after_comma}/UnityDownloadAssistant-#{version.before_comma}.dmg"
   name 'Unity'
   homepage 'https://unity3d.com/unity/'
   license :commercial
