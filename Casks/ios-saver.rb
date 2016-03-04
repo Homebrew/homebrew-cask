3bdbf0da4ad3328e358392dba145690dcd12e308
Victor Popkov <Dragon.vctr@gmail.com>
2016-03-04 19:52:32 +0200

    ios-saver.rb: fix vendor comment

--- Casks/ios-saver.rb
+++ Casks/ios-saver.rb
@@ -2,7 +2,7 @@ cask 'ios-saver' do
   version :latest
   sha256 :no_check
 
-  # site90.net is the official download host per the vendor homepage
+  # dlstatsios7.site90.net was verified as official when first introduced to the cask
   url 'http://dlstatsios7.site90.net/download.php',
       referer: 'http://littleendiangamestudios.com/project/ios-8-screen-saver/'
   name 'iOS 8 Lockscreen for OSX'
