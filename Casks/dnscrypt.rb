69ad73a42db13136806bbc871d602fba1be02fb2
Royce Dy <rady182@me.com>
2016-03-04 10:26:12 +0800

    updated dnscrypt (1.0.11)

--- Casks/dnscrypt.rb
+++ Casks/dnscrypt.rb
@@ -1,10 +1,10 @@
 cask 'dnscrypt' do
-  version '1.0.10'
-  sha256 'd092d9151035a0853902261e883d5ba7c50cb93beea9834db41303b2ec4276e0'
+  version '1.0.11'
+  sha256 '91610e90474cf788430f820f9a13f59d84449773133ff11df1c5ee58a9dfe82e'
 
   url "https://github.com/alterstep/dnscrypt-osxclient/releases/download/#{version}/dnscrypt-osxclient-#{version}.dmg"
   appcast 'https://github.com/alterstep/dnscrypt-osxclient/releases.atom',
-          checkpoint: 'c90ef83cd8f91e7a9f7cda90a4ad2ed9ceab0807afe6deeeb39a8a2a75d56360'
+          checkpoint: '656d405a7df005ff301315ca7320a0831dd53873429e6d771fd2e239d538d09c'
   name 'DNSCrypt'
   homepage 'https://github.com/alterstep/dnscrypt-osxclient'
   license :oss
