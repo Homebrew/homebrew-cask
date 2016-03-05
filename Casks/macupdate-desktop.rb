492b34c7b4d3c9c31d9d4edbbae64295c3fbf374
Beng Hee Eu <beng@benghee.eu>
2016-03-05 10:56:17 -0500

    updated macupdate-desktop (6.1)

--- Casks/macupdate-desktop.rb
+++ Casks/macupdate-desktop.rb
@@ -1,10 +1,10 @@
 cask 'macupdate-desktop' do
-  version '6.0.9'
-  sha256 '82f8b1bc8d9ed5d3881a8ca618df146fbb82634ecca4e5aac8f96ad65389e7c4'
+  version '6.1'
+  sha256 '0f93e249074b7e49e6a7aacc4e69bc8c2a9a7e41e0004277299c74bd89f75bf9'
 
   url "http://cdn.macupdate.com/MacUpdateDesktop#{version}.zip"
   appcast 'http://www.macupdate.com/desktop/updates.xml',
-          checkpoint: '2d1167e6e25b5c477fe0e20c1bcdb18b9eed13f989aa025a0aa5611f3fe7c678'
+          checkpoint: '53c5732b515e636440732d8a150eb071e609869ca1a09fe8a93d74fadd71adec'
   name 'MacUpdate Desktop'
   homepage 'https://www.macupdate.com/desktop'
   license :freemium
