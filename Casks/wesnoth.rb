5c04f10b862620ee5453b4dbd2c6ae1fafa8cc1d
Victor Popkov <victor@popkov.me>
2016-03-05 20:31:58 +0200

    wesnoth.rb: fix vendor comment

--- Casks/wesnoth.rb
+++ Casks/wesnoth.rb
@@ -2,10 +2,10 @@ cask 'wesnoth' do
   version '1.12.4'
   sha256 '1773d6d6441e121ed8e1a219acaa6cf0c7b24aefe58c5253fc71a16a165e350b'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # sourceforge.net/sourceforge/wesnoth was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/sourceforge/wesnoth/Wesnoth_#{version}.dmg"
   name 'The Battle for Wesnoth'
-  homepage 'https://wesnoth.org'
+  homepage 'https://wesnoth.org/'
   license :gpl
 
   app 'Wesnoth.app'
