5ec4c1a5ab83760ef6b7efae3ded5087ca3b1efc
Victor Popkov <Dragon.vctr@gmail.com>
2016-03-06 16:16:44 +0200

    updated vivaldi (1.0.403.24)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,5 +1,5 @@
 cask 'vivaldi' do
-  version '1.0.344.37'
+  version '1.0.403.24'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://vivaldi.com/download/stable/Vivaldi.#{version}.dmg"
