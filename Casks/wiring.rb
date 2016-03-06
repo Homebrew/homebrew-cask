f89d1930eb8e5a2a615a9b3fd59fcd48ae94ab17
Bret Comnes <bcomnes@gmail.com>
2016-03-06 15:52:00 -0800

    Add Wiring 1.0

---
+++ Casks/wiring.rb
@@ -0,0 +1,12 @@
+cask 'wiring' do
+  version '1.0-101'
+  sha256 'ff593ccfc8a1ef988b86f7610141c9bd0e5d3763b64e04f28257a374042830a8'
+
+  url 'http://wiring.org.co/download/wiring-0101-macosx.zip'
+  name 'Wiring'
+  homepage 'http://wiring.org.co/'
+  license :gpl 
+
+  app 'Wiring.app'
+end
+
