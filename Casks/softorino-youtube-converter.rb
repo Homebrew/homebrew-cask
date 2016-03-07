480163c2279cf2324913fa851167e0f47f6b9859
Victor Popkov <victor@popkov.me>
2016-03-07 20:13:23 +0200

    softorino-youtube-converter.rb: fix vendor comment, update homepage to use SSL

--- Casks/softorino-youtube-converter.rb
+++ Casks/softorino-youtube-converter.rb
@@ -2,10 +2,10 @@ cask 'softorino-youtube-converter' do
   version :latest
   sha256 :no_check
 
-  # devmate.com is the official download host per the vendor homepage
+  # devmate.com/com.softorino.converter was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.softorino.converter/SoftorinoConverter.dmg'
   name 'Softorino YouTube Converter'
-  homepage 'http://softorino.com/youtube-converter'
+  homepage 'https://softorino.com/youtube-converter'
   license :gratis
 
   app 'Softorino YouTube Converter.app'
