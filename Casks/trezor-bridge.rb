3cbfd67ed4a5087218089ccc9c2061544cc463fa
Victor Popkov <victor@popkov.me>
2016-03-07 20:16:53 +0200

    trezor-bridge.rb: fix vendor comment

--- Casks/trezor-bridge.rb
+++ Casks/trezor-bridge.rb
@@ -2,7 +2,7 @@ cask 'trezor-bridge' do
   version '1.1.2'
   sha256 '95780f24cd7a4a6a30a3f715502ad436f036571b38e693edd88d347abc4348ab'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # mytrezor.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://mytrezor.s3.amazonaws.com/bridge/#{version}/trezor-bridge-#{version}.pkg"
   name 'TREZOR Bridge'
   homepage 'https://mytrezor.com/'
