a55243f7b4bb33d8401e206845810a4450920e37
Victor Popkov <victor@popkov.me>
2016-03-07 20:17:55 +0200

    tvrenamer.rb: fix vendor comment

--- Casks/tvrenamer.rb
+++ Casks/tvrenamer.rb
@@ -2,12 +2,12 @@ cask 'tvrenamer' do
   version '0.7.1'
   sha256 '51c56fca7ce361aed823d4b943a438f4d0b1d516620c4f8548cbefbff0ecf309'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/tvrenamer/tvrenamer was verified as official when first introduced to the cask
   url "https://github.com/tvrenamer/tvrenamer/releases/download/#{version}/TVRenamer-#{version}-osx64.zip"
   appcast 'https://github.com/tvrenamer/tvrenamer/releases.atom',
           checkpoint: '4b7e78cab3a5dcf2fc2067ab6daa594740deb345929b1d77c8035cb454e19861'
   name 'TVRenamer'
-  homepage 'http://tvrenamer.org'
+  homepage 'http://tvrenamer.org/'
   license :gpl
 
   app "TVRenamer-#{version}.app"
