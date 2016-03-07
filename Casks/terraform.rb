f880c3f83d214f37ac56cbd7efb122dd46ad109f
Victor Popkov <victor@popkov.me>
2016-03-07 20:15:31 +0200

    terraform.rb: fix vendor comment

--- Casks/terraform.rb
+++ Casks/terraform.rb
@@ -2,7 +2,7 @@ cask 'terraform' do
   version '0.6.12'
   sha256 'eaa50e05a88ef83a9ba18a3768932f4d530ce1b710b29ae29992f94addac0bfb'
 
-  # hashicorp.com is the official download host per the vendor homepage
+  # hashicorp.com/terraform was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
   name 'Terraform'
   homepage 'https://www.terraform.io/'
