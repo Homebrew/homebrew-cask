63050333cc0feafdf7f96348e9fd531629b070b3
Victor Popkov <victor@popkov.me>
2016-03-07 20:15:52 +0200

    the-escapers-flux.rb: fix vendor comment

--- Casks/the-escapers-flux.rb
+++ Casks/the-escapers-flux.rb
@@ -2,7 +2,7 @@ cask 'the-escapers-flux' do
   version '5.6.15'
   sha256 '01e2eeaa6259df5d0443d589fb0cb989d4fdfb79d55a535352f9deac534b9466'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/Flux was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Flux/FluxV#{version.major}.zip"
   appcast 'http://www.theescapers.com/flux/flux.xml',
           checkpoint: '5186709410d51bcd39f1fc569ecee474c97846173e483871eb8d1fd7ddf06099'
