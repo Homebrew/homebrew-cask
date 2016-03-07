b77b2caa447b99822284d25cb651260a482ee6ed
Victor Popkov <victor@popkov.me>
2016-03-07 20:12:36 +0200

    scapple.rb: fix vendor comment

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -2,7 +2,7 @@ cask 'scapple' do
   version '1.2'
   sha256 '1b9968a7927c29b5b4fa4d2c9e5ce78c56e5c3cfbb42847836c2e732018d92c1'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # scrivener.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://scrivener.s3.amazonaws.com/Scapple.dmg'
   appcast 'https://www.literatureandlatte.com/downloads/scapple/scapple.xml',
           checkpoint: '81c37dc0b445e6e8a5a1dbcdeff51f710598208bd78d31dfb79744ecf9b2f00a'
