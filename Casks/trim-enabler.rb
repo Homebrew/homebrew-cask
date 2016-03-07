cabe958f69f05a5521658c598f90d6739351fd30
Victor Popkov <victor@popkov.me>
2016-03-07 20:17:15 +0200

    trim-enabler.rb: fix vendor comment

--- Casks/trim-enabler.rb
+++ Casks/trim-enabler.rb
@@ -6,7 +6,7 @@ cask 'trim-enabler' do
   else
     version :latest
     sha256 :no_check
-    # amazonaws.com is the official download host per the vendor homepage
+    # amazonaws.com/cindori was verified as official when first introduced to the cask
     url 'https://s3.amazonaws.com/cindori/TrimEnabler.dmg'
   end
 
