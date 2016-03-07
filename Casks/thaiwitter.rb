1b5a38699d1a02bcaccb4e710f5d6df2483fa6a3
Victor Popkov <victor@popkov.me>
2016-03-07 20:15:41 +0200

    thaiwitter.rb: fix vendor comment

--- Casks/thaiwitter.rb
+++ Casks/thaiwitter.rb
@@ -2,7 +2,7 @@ cask 'thaiwitter' do
   version :latest
   sha256 :no_check
 
-  # dropbox.com is the official download host per the vendor homepage
+  # dropbox.com/u/25097375 was verified as official when first introduced to the cask
   url 'https://dl.dropbox.com/u/25097375/thaiWitter/Builds/thaiWitter3.app.zip'
   name 'thaiWitter'
   homepage 'https://tw3.herokuapp.com/'
