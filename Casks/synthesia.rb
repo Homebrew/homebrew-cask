8e40c0143b6e138a119744be22b594ab7912a1d1
Victor Popkov <victor@popkov.me>
2016-03-07 20:14:51 +0200

    synthesia.rb: fix vendor comment

--- Casks/synthesia.rb
+++ Casks/synthesia.rb
@@ -2,10 +2,10 @@ cask 'synthesia' do
   version '10.2'
   sha256 'e36602bc84d1b923c7879f0e9716b30ba8f5892ee7bd062bd57bbcb8dc38d920'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # synthesia.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://synthesia.s3.amazonaws.com/files/Synthesia-#{version}.dmg"
   name 'Synthesia'
-  homepage 'https://www.synthesiagame.com'
+  homepage 'https://www.synthesiagame.com/'
   license :freemium
 
   app 'Synthesia.app'
