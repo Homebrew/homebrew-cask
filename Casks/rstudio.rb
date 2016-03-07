f32576daca40614a9585b118df05c15f2d14c359
Victor Popkov <victor@popkov.me>
2016-03-07 20:12:24 +0200

    rstudio.rb: fix vendor comment

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -2,7 +2,7 @@ cask 'rstudio' do
   version '0.99.891'
   sha256 '1c9b77d2e7a858257bd3875c5e5f419bf077f7f1d403f6c26cdf5a7f1f7e8d60'
 
-  # rstudio.org is the official download host per the vendor homepage
+  # rstudio.org was verified as official when first introduced to the cask
   url "https://download1.rstudio.org/RStudio-#{version}.dmg"
   name 'RStudio'
   homepage 'https://www.rstudio.com/'
