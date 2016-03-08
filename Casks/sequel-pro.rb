401453003912d3bf667adbdeb01b23463fff0b3b
Victor Popkov <victor@popkov.me>
2016-03-08 23:02:38 +0200

    sequel-pro.rb: fix vendor comment

--- Casks/sequel-pro.rb
+++ Casks/sequel-pro.rb
@@ -2,7 +2,7 @@ cask 'sequel-pro' do
   version '1.1.1'
   sha256 'e76c20d52d559157deca648c6ca9cf590af0eb3b08313fd0817b19e7fd3a1550'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/sequelpro/sequelpro was verified as official when first introduced to the cask
   url "https://github.com/sequelpro/sequelpro/releases/download/#{version}/sequel-pro-#{version}.dmg"
   appcast 'https://github.com/sequelpro/sequelpro/releases.atom',
           checkpoint: 'ed6e4b83bcd0ca3b51fec9aa6e7663a0392314cecc40bb3cdae1a2fc18f07d69'
