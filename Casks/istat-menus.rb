ac14e6eb7f633ef5b2db3c0bd8b010ef7bb13c0a
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2016-03-08 09:39:52 +0100

    Fix SHA for iStat Menus

--- Casks/istat-menus.rb
+++ Casks/istat-menus.rb
@@ -1,6 +1,6 @@
 cask 'istat-menus' do
   version '5.11'
-  sha256 '5931ff8a024ea54f9268e66c650ae4a33b399ae25ca36586b7814d960ed77d19'
+  sha256 '21ccfe981ac8beafeda0f6e782767239cc9202bc44596d6e5e3c956dc7f57232'
 
   # amazonaws.com/bjango was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/bjango/files/istatmenus5/istatmenus#{version}.zip"
