7cbf8d6abb73b18cb0ad5919e672a0ce930ed54d
Victor Popkov <victor@popkov.me>
2016-03-08 23:01:27 +0200

    odrive.rb: fix vendor comment

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -3,7 +3,7 @@ cask 'odrive' do
   sha256 '3f655ccfab6a04bbcf6d5660b03ea0532184ee0840f6f5edec71d65340ebb3fe'
 
   # downloads can be found at https://www.odrive.com/downloaddesktop
-  # cloudfront.com is the official download host per the vendor homepage
+  # d3huse1s6vwzq6.cloudfront.net was verified as official when first introduced to the cask
   url "https://d3huse1s6vwzq6.cloudfront.net/odrivesync.#{version}.dmg"
   name 'odrive'
   homepage 'https://app.odrive.com/'
