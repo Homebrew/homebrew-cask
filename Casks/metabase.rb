5f68ed9485f041953000aa6de11ead4c4b722c80
Tom Robinson <tlrobinson@gmail.com>
2016-03-08 10:58:23 -0800

    updated metabase (0.15.0)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,10 +1,10 @@
 cask 'metabase' do
-  version '0.14.1'
-  sha256 'ab6d3f67abd034ef2f5abec4300f84ee122d9a12c6313dfacf531b476288e3b9'
+  version '0.15.0'
+  sha256 '4d15ef9b9eb943c27ae234a971d441ed48eedf77f48c12c72d7cfb7a3642d502'
 
   url "http://downloads.metabase.com/v#{version}/Metabase.dmg"
   appcast 'http://downloads.metabase.com/appcast.xml',
-          checkpoint: '00dcd919677f67ed38999f3196172a96bab3a626cc8fdb319144c3d68f45c8ae'
+          checkpoint: '55607ec03b49eee1ef45ecfd05854bed83e6985c409fc9cc520ac809bb29a7c9'
   name 'Metabase'
   homepage 'http://www.metabase.com/'
   license :affero
