402a32dad133edca3f6c6ca8030ff50feea29e18
Leonid Komarovsky <shpoont@users.noreply.github.com>
2016-03-08 14:13:46 +0200

    Update geppetto to 4.3.1

--- Casks/geppetto.rb
+++ Casks/geppetto.rb
@@ -1,12 +1,12 @@
 cask 'geppetto' do
-  version '4.3.0-R201409300140'
+  version '4.3.1-R201501182354'
 
   if Hardware::CPU.is_32_bit?
-    sha256 'd470ca583fd7be26fc0f2b24ee8ee35f101ec215bafc1cbc5b3455040eb0d2b3'
+    sha256 '8b7e3d9782c5c863185393fcd9f00b9b487af7f778fb2e5289567ace6f1e3866'
     # puppetlabs.com was verified as official when first introduced to the cask
     url "https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86-#{version}.zip"
   else
-    sha256 'c21a3522eead31643985108c6467de1956b21626e03e9c30050b31f9df0a9bd7'
+    sha256 'e01eba5861ecedd963528c3bc24e3b8ad6810aafd25e98080bff9f45f724e756'
     # puppetlabs.com was verified as official when first introduced to the cask
     url "https://downloads.puppetlabs.com/geppetto/4.x/geppetto-macosx.cocoa.x86_64-#{version}.zip"
   end
