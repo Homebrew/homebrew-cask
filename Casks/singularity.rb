2768d08fc27eb0b50333e2debee137e0f00d97e4
Victor Popkov <victor@popkov.me>
2016-03-08 23:03:42 +0200

    singularity.rb: fix vendor comment

--- Casks/singularity.rb
+++ Casks/singularity.rb
@@ -2,7 +2,7 @@ cask 'singularity' do
   version '1.8.6(6156)'
   sha256 '002597a1d7386ff1b505416476fce812037a38c61bd152212fd758db943de40c'
 
-  # bitbucket.org is the official download host per the vendor homepage
+  # bitbucket.org/SingularityViewer/singularityviewer was verified as official when first introduced to the cask
   url "https://bitbucket.org/SingularityViewer/singularityviewer/downloads/Singularity_#{version.gsub(%r{[.(]}, '_').delete(')')}.dmg"
   name 'Singularity Viewer'
   homepage 'http://www.singularityviewer.org/'
