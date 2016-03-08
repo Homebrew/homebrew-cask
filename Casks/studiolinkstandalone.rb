84f28cf92bf4321fd11b517326b722fa9400ba6a
Victor Popkov <victor@popkov.me>
2016-03-08 23:04:37 +0200

    studiolinkstandalone.rb: fix vendor comment

--- Casks/studiolinkstandalone.rb
+++ Casks/studiolinkstandalone.rb
@@ -2,7 +2,7 @@ cask 'studiolinkstandalone' do
   version '16.02.0-beta'
   sha256 '8427a2013295e02fa8679f57a3a9b991bafe864861179c3fd719ec6f5ea32869'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/Studio-Link-v2/backend was verified as official when first introduced to the cask
   url "https://github.com/Studio-Link-v2/backend/releases/download/v#{version}/studio-link-standalone-osx.zip"
   appcast 'https://github.com/Studio-Link-v2/backend/releases.atom',
           checkpoint: '9afde3059f8be79f95dee629fe161573f616d6eb9f39e22f13ac2ec9bd9f56f0'
