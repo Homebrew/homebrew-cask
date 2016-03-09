cb3904019d2f815f3cf957660e2620c5b741e547
Tej Chajed <tchajed@gmail.com>
2016-03-09 09:07:48 -0500

    twine: update to 2.0.11

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -1,6 +1,6 @@
 cask 'twine' do
-  version '2.0.10'
-  sha256 '3d073a624bd56817438105732e0e808b2ae9ff5bfebb455b70133cff80b0134f'
+  version '2.0.11'
+  sha256 '360d7b72b538d622e01d5fdac059bf6ddeac97619fdf5002fae736ca4b4eca19'
 
   # bitbucket.org is the official download host per the vendor homepage
   url "https://bitbucket.org/klembot/twinejs/downloads/twine_#{version}_osx.zip"
