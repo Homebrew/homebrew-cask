1183934c2c72fc014cd8dd5dc0041b49822b88cb
Jeff Ramnani <jramnani@8thlight.com>
2016-03-09 09:30:57 -0600

    Upgrade Acorn to v5.3.1.

--- Casks/acorn.rb
+++ Casks/acorn.rb
@@ -1,10 +1,10 @@
 cask 'acorn' do
-  version '5.3'
-  sha256 'c602778bbc0b44f164dbaf9c6c7ee48d3be9305d1522efdb4b561290e8649652'
+  version '5.3.1'
+  sha256 'b85eb989ccce80c9aac47ae9775c45ee3de83c65fb9021a887b308d3a27eb16e'
 
   url 'https://secure.flyingmeat.com/download/Acorn.zip'
   appcast 'http://www.flyingmeat.com/download/acorn5update.xml',
-          checkpoint: 'a00b7a314d4e3c1c29748aef856eff8fea178278e1129b5521bc044e6bc6d4d8'
+          checkpoint: '611f52dd08022d2692f9ca7b1f6b2a74572440ae665a9f026adfabbaea536c73'
   name 'Acorn'
   homepage 'http://flyingmeat.com/acorn/'
   license :commercial
