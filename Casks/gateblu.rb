59066fb56c1f3e45b003e37eef1fb56f5f75646d
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 18:54:10 -0500

    updated gateblu (5.5.8)

--- Casks/gateblu.rb
+++ Casks/gateblu.rb
@@ -1,11 +1,11 @@
 cask 'gateblu' do
   version '5.5.8'
-  sha256 'a0a29d7cf158747bcd1e722de23430db71605c32395b51835a066255766e4f94'
+  sha256 '38c7af1d6cec55a1c57c684c2c2152b954bddd2dcfd05d3b8b938d2f17f29f8f'
 
   # amazonaws.com/gateblu was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/gateblu/gateblu-ui/latest/Gateblu.dmg'
   appcast 'https://github.com/octoblu/gateblu/releases.atom',
-          checkpoint: '4402fe333a20866d098d55224fe6a4e33ba54505a1e900ae8c91e80cf5d69257'
+          checkpoint: '731ed0c4826ff40f5d1fe66300ff871883b32cbfa74bacdad74133bc82a72801'
   name 'Gateblu'
   homepage 'https://gateblu.octoblu.com'
   license :mit
