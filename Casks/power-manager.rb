782f70043fcf641ba482fb9516d85673563dbfe7
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:32:37 -0500

    updated power-manager (4.5.1)

--- Casks/power-manager.rb
+++ Casks/power-manager.rb
@@ -1,10 +1,10 @@
 cask 'power-manager' do
-  version '4.5.0'
-  sha256 '63beadb6631f76646214d90570d78704f4a54c7f129ebea259a232b29b94d296'
+  version '4.5.1'
+  sha256 '154bbf674e2aee6f482d5df6952460f7dc6d01e04a45ab6ddd481f50daa8d7ca'
 
   url "http://www.dssw.co.uk/powermanager/dsswpowermanager-#{version.no_dots}.dmg"
   appcast 'https://version.dssw.co.uk/powermanager/application',
-          checkpoint: '968a2262cc5e5bc817682cceb4d75d18ea2f8efc8e829b2ef812b2f9fedda081'
+          checkpoint: '603e0ad871c89bb1370a33575da47a700878348af1c74107e3c26d4d8efa5924'
   name 'Power Manager'
   homepage 'https://www.dssw.co.uk/powermanager'
   license :commercial
