e0e413c3bb7c0308e6a88fa7e6105348096b9087
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:20:40 -0500

    updated mnemosyne (2.3.5)

--- Casks/mnemosyne.rb
+++ Casks/mnemosyne.rb
@@ -5,7 +5,7 @@ cask 'mnemosyne' do
   # sourceforge.net/project/mnemosyne-proj was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/mnemosyne-proj/mnemosyne/mnemosyne-#{version}/Mnemosyne-#{version}.dmg"
   appcast 'http://sourceforge.net/p/mnemosyne-proj/activity/feed?source=project_activity',
-          checkpoint: 'a8f3a1d3ec76cfbc00c91c8957abb728e742e61860bc694c355421e3f8f0d8bc'
+          checkpoint: '6dde6afd7066743b7d40e0e0a57f21df12bbcf67a95f24b25fb2271f5c4a48dc'
   name 'Mnemosyne'
   homepage 'http://mnemosyne-proj.org/'
   license :gpl
