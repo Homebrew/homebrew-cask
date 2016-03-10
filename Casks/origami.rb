ab350fe016a554f2c9470a4b3b2803077034bac0
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:27:49 -0500

    updated origami (2.1.4)

--- Casks/origami.rb
+++ Casks/origami.rb
@@ -1,11 +1,11 @@
 cask 'origami' do
-  version '2.1.3'
-  sha256 'e97e9106d14c1beeb7a05019040b0a6f31691e4b0d1c8d3fb94e0de1b8110f72'
+  version '2.1.4'
+  sha256 '2aa49f683208157a4750fdf6fa07248c17709036d94a0e2708eac19e9dae54fd'
 
   # origami.facebook.com was verified as official when first introduced to the cask
   url "https://origami.facebook.com/download/Origami-#{version}.zip"
   appcast 'https://facebook.github.io/origami/update/updates.xml.rss',
-          checkpoint: '3545a46eb237f2628d404bfe845987a4411e937e95bd6d6cd3b1e679a0e332b3'
+          checkpoint: 'e965420f57412402ec5d8728f92a132291a9a3997bd54f718e175f83027443c1'
   name 'Origami'
   homepage 'https://facebook.github.io/origami'
   license :gratis
