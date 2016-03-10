c612a02eadcd56944f411185a4dcea124f566dd1
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:45:01 -0500

    updated shapes (4.6)

--- Casks/shapes.rb
+++ Casks/shapes.rb
@@ -1,10 +1,10 @@
 cask 'shapes' do
-  version '4.5.7'
-  sha256 '918bd2ff2ce86dd2d1438afd6b4c17f149468c6028986238933da18ec8948a0b'
+  version '4.6'
+  sha256 '132da27932917b01c12735c566c05ce90f810e5ae68408bcd04514a166de71dd'
 
   url "http://shapesapp.com/dist/Shapes_#{version}.zip"
   appcast 'http://shapesapp.com/appcast/shapes4.rss',
-          checkpoint: 'baa99161a61b6084b1941e8080300d3e9c1ec75cd8307019a3c3e430f836b087'
+          checkpoint: 'd9fd8cc6d2650b59bf1c8220804be5e0f73e572d2d1fd958ec9418897f8f28aa'
   name 'Shapes'
   homepage 'http://shapesapp.com'
   license :commercial
