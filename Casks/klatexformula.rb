80f67ddcb98ff1bb1fd523408b9e74b207eb6246
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:17:01 -0500

    updated klatexformula (3.3.0beta)

--- Casks/klatexformula.rb
+++ Casks/klatexformula.rb
@@ -3,8 +3,8 @@ cask 'klatexformula' do
   sha256 '7b1ac67dec83010acce4da00d1a10c6bab3921e9eedb7e88322f8b3bf987c104'
 
   url "http://downloads.sourceforge.net/project/klatexformula/klatexformula/klatexformula-#{version}/klatexformula-#{version}-macosx.dmg"
-  appcast 'http://sourceforge.net/projects/klatexformula/rss',
-          checkpoint: '26903d4e3edba1a44ffa69ded4012a1a4679522493fcd8b79b54382241b28b86'
+  appcast 'https://sourceforge.net/projects/klatexformula/rss?path=/klatexformula',
+          checkpoint: 'cd0d0e88a5c1fe433a1ade7d4bb6d052271db9496204353e77ca2f5307a871a5'
   name 'KLatexFormula'
   homepage 'http://klatexformula.sourceforge.net'
   license :gpl
