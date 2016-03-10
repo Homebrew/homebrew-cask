af9286a75a47c768164a7955e4aaaba1345a1155
Victor Popkov <victor@popkov.me>
2016-03-10 23:58:35 +0200

    wercker-cli.rb: fix vendor comment

--- Casks/wercker-cli.rb
+++ Casks/wercker-cli.rb
@@ -2,7 +2,7 @@ cask 'wercker-cli' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/downloads.wercker.com was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/downloads.wercker.com/cli/stable/darwin_amd64/wercker'
   name 'wercker'
   homepage 'http://wercker.com/'
