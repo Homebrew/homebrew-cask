a6b38d3986fc115418714f8d617e76ab5a962a7d
Victor Popkov <victor@popkov.me>
2016-03-10 23:58:13 +0200

    screenstagram.rb: fix vendor comment

--- Casks/screenstagram.rb
+++ Casks/screenstagram.rb
@@ -2,7 +2,7 @@ cask 'screenstagram' do
   version '2.01'
   sha256 '5cb9f7be247fdec897248f5091818bf7b25153fa99782e96f9d9b2c2431cdbb1'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # screenstagram.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://screenstagram.s3.amazonaws.com/screenstagram_#{version}.dmg"
   name 'Screenstagram'
   homepage 'https://screenstagram.s3.amazonaws.com/download.html'
