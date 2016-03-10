07220cb111a0a71a39e9e143fd428648700745bc
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:53:41 -0500

    updated syncmate (6.3.294)

--- Casks/syncmate.rb
+++ Casks/syncmate.rb
@@ -1,10 +1,10 @@
 cask 'syncmate' do
-  version '6.2.279'
-  sha256 'afc1bc6a1ac0d3aa0784eec271c76e31ea8c885f68ce6ffc1aa6220376c93b98'
+  version '6.3.294'
+  sha256 '2fe3c18a3cf60c880dfbb9a4cf99318177134b3b113f081072ca37e450cc3048'
 
   url 'http://www.sync-mac.com/download/syncmate.dmg'
   appcast 'http://www.eltima.com/download/syncmate-update/syncmate6.xml',
-          checkpoint: '116cf7df50ef73545c5dac6323e8f32d068ebd37740601d9bd4037b21b3538ff'
+          checkpoint: 'b5b107fc9a6a1e1d35f105ec2a451cb339e3e5834c1cece8acb46185e4e87e6c'
   name 'SyncMate'
   homepage 'http://www.sync-mac.com'
   license :commercial
