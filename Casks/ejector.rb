4d12feb3fa92b9f046b42176bd0c85e66e744230
Brendan E Mahon <styx1776@gmail.com>
2016-03-10 18:57:09 -0500

    Update ejector.rb
    Added sha256 checksum.

--- Casks/ejector.rb
+++ Casks/ejector.rb
@@ -1,6 +1,6 @@
 cask 'ejector' do
   version '0.8.1'
-  sha256 :c6fefa1788a1a67793456e9620e18b9613c19634b649ebf6578e9c082ff4946e
+  sha256 'c6fefa1788a1a67793456e9620e18b9613c19634b649ebf6578e9c082ff4946e'
 
   url "http://www.jeb.com.fr/soft/Ejector-v#{version}.dmg"
   name 'Ejector'
