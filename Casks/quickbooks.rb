2c1bea8b9d8996e75cd3d4a25572aea99acbb9e2
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:35:20 -0500

    updated quickbooks (2.4.3)

--- Casks/quickbooks.rb
+++ Casks/quickbooks.rb
@@ -1,10 +1,10 @@
 cask 'quickbooks' do
-  version '2.4.2'
-  sha256 '884aff0cbe93b11ca809196927ae6af340cfe4cbf5c5095be347e795495db703'
+  version '2.4.3'
+  sha256 'f405fac0e881a9b31fc061081a544c1720d67af27d513b9d1727f7090cdfb1a8'
 
   url "https://http-download.intuit.com/http.intuit/CMO/apple_web/sample/static/qbomac/releases/QuickBooks_#{version}/QuickBooks.QBOWeb.dmg"
   appcast 'https://http-download.intuit.com/http.intuit/CMO/apple_web/sample/static/qbomac/rssfeeds/Staged/QuickBooks.QBOWeb.Release.xml',
-          checkpoint: 'd65910422ce81187363f5da516ecb7b9ad5b2df4b64252442c020e3c346f9e4d'
+          checkpoint: '46e98a117c49e5bcdccd52dc0df7a7912da652232eb4b97b7aff1604ac2deb77'
   name 'QuickBooks'
   homepage 'https://qbo.intuit.com/'
   license :gratis
