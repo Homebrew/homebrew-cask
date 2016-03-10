21516aafac00b4720e86acfbe4fc0c0a24f559dc
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:57:14 -0500

    updated texpad (1.7.21)

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -3,7 +3,7 @@ cask 'texpad' do
     version '1.7.21'
     sha256 'cae312b89d25752917bf66572fd1c6b6722097b651abe9a1a1b399dc36493512'
   elsif MacOS.release <= :mountain_lion && MacOS.release >= :snow_leopard
-    version '1.7.23'
+    version '1.7.21'
     sha256 'cae312b89d25752917bf66572fd1c6b6722097b651abe9a1a1b399dc36493512'
   else
     # If the app is used on MacOS lower than Snow Leopard,
@@ -12,7 +12,7 @@ cask 'texpad' do
 
   url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.gsub('.', '_')}.zip"
   appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml',
-          checkpoint: '9271b5b259bc459316233532c993e5f95fa772253d5245befa080235dca3e7a2'
+          checkpoint: '4842788b8dd25bb010c19f6ad6230e0288c371185fd4f581003213c0b64b3d5a'
   name 'Texpad'
   homepage 'https://www.texpadapp.com/osx'
   license :commercial
