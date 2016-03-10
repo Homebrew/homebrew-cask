b8b09cb9b66bb711a46e119f743149a02b95a1f3
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:06:39 -0500

    updated hirundo (0.8.5)

--- Casks/hirundo.rb
+++ Casks/hirundo.rb
@@ -1,10 +1,10 @@
 cask 'hirundo' do
-  version '0.8.4'
-  sha256 'c4e7e9d0d699776bb497312d63e0c12bacde4e11c4317505f243ff9351915890'
+  version '0.8.5'
+  sha256 '9fc8400b4e8764eec913b89dd99f3ed4668abfa77886672063c068bdb6f4564f'
 
   url "https://stylemac.com/hirundo/versions/#{version}/Hirundo-#{version}.tgz"
   appcast 'https://stylemac.com/hirundo/versions/current.json',
-          checkpoint: 'fee92e8ac7d73014d177cc23740c872c504cae9021ebd2eae19033ffab7f43c1'
+          checkpoint: '5ec54e3e8a65e5d4709d7fd085049e98bcfb5aafdf67a9a454611b98b3a3f92f'
   name 'Hirundo'
   homepage 'https://stylemac.com/hirundo/'
   license :gratis
