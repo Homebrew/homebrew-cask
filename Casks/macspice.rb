3f977412de0403cd7d982ef310a17ab5e0c0b05a
Vítor Galvão <info@vitorgalvao.com>
2016-03-09 19:18:02 -0500

    updated macspice (3.1.10)

--- Casks/macspice.rb
+++ Casks/macspice.rb
@@ -1,10 +1,10 @@
 cask 'macspice' do
-  version '3.1.9'
-  sha256 'f0231aacc7aa881ed286015af599d5f550ab8b054aca80ce04c8d7a3abf1305c'
+  version '3.1.10'
+  sha256 '60f5157a715e17832c858020d9651116cb1b678a7a443ef4cbb05e8c535b4cbf'
 
   url "http://www.macspice.com/mirror/binaries/v#{version}/MacSpice3f5.dmg"
   appcast 'http://www.macspice.com/AppCast-v2.xml',
-          checkpoint: 'ad5ccb1f63ee40273acf297e470ea23af20cd194e2e148ec663824ed9d2ec3b8'
+          checkpoint: '3906261d637484b9652d5ebf4fc792e5b870bbe0337032c6016bf42c6472c192'
   name 'MacSpice'
   homepage 'http://www.macspice.com/'
   license :closed
