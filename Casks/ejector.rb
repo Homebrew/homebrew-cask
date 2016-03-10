2092753820c644afea43e66a88c37bae2dae49d5
Brendan E Mahon <styx1776@gmail.com>
2016-03-09 23:44:22 -0500

    Update ejector.rb
    The current version number is 0.8.1.  The version number indicated (0.9.0) results in a non-functional link.

--- Casks/ejector.rb
+++ Casks/ejector.rb
@@ -1,6 +1,6 @@
 cask 'ejector' do
-  version '0.9.0'
-  sha256 '78e588ba2f9b02c753af7e4f432c0dc980d3745c3b8d3978b6b11e113e109f19'
+  version '0.8.1'
+  sha256 :no_check
 
   url "http://www.jeb.com.fr/soft/Ejector-v#{version}.dmg"
   name 'Ejector'
