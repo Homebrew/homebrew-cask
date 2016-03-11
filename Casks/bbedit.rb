bc068d60a6d706a0b8808d017f5efab13392c822
Vítor Galvão <info@vitorgalvao.com>
2016-03-10 20:27:08 -0500

    updated bbedit (11.5.1)

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -1,10 +1,10 @@
 cask 'bbedit' do
-  version '11.5'
-  sha256 'ea8924e8a275a2c6ef19937d0874f0be5ac932a2a56718217a84b0c711bf8d12'
+  version '11.5.1'
+  sha256 '348fcbe39f1d35bb54db649cddfab4ed5bc1ee734b7a5bfb85c61f0f410f4224'
 
   url "http://pine.barebones.com/files/BBEdit_#{version}.dmg"
   appcast 'https://versioncheck.barebones.com/BBEdit.xml',
-          checkpoint: '38540ae3740f30447d6470ba25669380229a56f1d663720ba21641162d88dad7'
+          checkpoint: 'ade9d68d48f4d024416457a346fdbd109c584c50a096e6dc1fe74c6a41ef65b6'
   name 'BBEdit'
   homepage 'http://www.barebones.com/products/bbedit/'
   license :commercial
