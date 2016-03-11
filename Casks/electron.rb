ad25203d90c4c244b52458f7f04311c1a1f014a2
Vítor Galvão <info@vitorgalvao.com>
2016-03-10 20:33:49 -0500

    updated electron (0.36.10)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,10 +1,10 @@
 cask 'electron' do
-  version '0.36.9'
-  sha256 '889fbb4494f73117e44c5f11542d26c3411bd7fc8a970c8f0aa728c1eabc5587'
+  version '0.36.10'
+  sha256 '8692d812f70f75659c0f79ab1d376cd47a69d7fc9c5d3317f68acfdf97108ad7'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
-          checkpoint: '257ff5a0eaaae3eaeadcf4c0b900f0ec93d81e807d514e466ae0163fbbd55797'
+          checkpoint: '6bb06ab56edd0a0c555fa6d19f1b4ed851f8e318050df1c531ce73e21bf4ff3c'
   name 'Electron'
   homepage 'http://electron.atom.io/'
   license :mit
