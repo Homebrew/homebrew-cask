6097e30a30a1ae451a9dddf0d3ee1cbc90f908b3
Vítor Galvão <info@vitorgalvao.com>
2016-03-10 20:53:45 -0500

    updated xamarin-jdk (7.71)

--- Casks/xamarin-jdk.rb
+++ Casks/xamarin-jdk.rb
@@ -4,7 +4,7 @@ cask 'xamarin-jdk' do
 
   url "https://download.xamarin.com/Installer/MonoForAndroid/jdk-#{version.major}u#{version.minor}-macosx-x64.dmg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '855d95a330efdd9cc13cbaabc6e49dfcda111db3b16cbc6345e3a0d17e769972'
+          checkpoint: '14727c4cd976ca3d96a7a9c362bebd064a49c4f24d32296d33767e3a76b4a685'
   name 'Xamarin Java JDK'
   homepage 'https://xamarin.com/platform'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
