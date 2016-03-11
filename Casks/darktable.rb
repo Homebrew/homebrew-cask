b4eb9525b84ec76421a7074691e143041b3e660f
Vítor Galvão <info@vitorgalvao.com>
2016-03-10 20:31:38 -0500

    updated darktable (2.0.2)

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,11 +1,11 @@
 cask 'darktable' do
-  version '2.0.1'
-  sha256 '580d1feb356e05d206eb74d7c134f0ffca4202943388147385c5b8466fc1eada'
+  version '2.0.2'
+  sha256 '33789b5a791770f9308cc653aaf50b0e9c054a0fbdd5e4a1d2e48e2bf6553f95'
 
   # github.com/darktable-org/darktable was verified as official when first introduced to the cask
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version}/darktable-#{version}.dmg"
   appcast 'https://github.com/darktable-org/darktable/releases.atom',
-          checkpoint: '2e8a1bcf85f0aeb99f456964160657ad8f24f9ad57ef324c9a651b840c231543'
+          checkpoint: 'b34f3cd0eb6a273b1e0353a0f9e9332b42b06e36014083088048cdfefa5a631e'
   name 'darktable'
   homepage 'https://www.darktable.org/'
   license :gpl
