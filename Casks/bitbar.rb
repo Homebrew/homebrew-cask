e9cab615d07f6c0a35ca6ff890cd17f62d214580
Victor Popkov <victor@popkov.me>
2016-03-12 23:31:17 +0200

    updated bitbar (1.7.0)

--- Casks/bitbar.rb
+++ Casks/bitbar.rb
@@ -1,10 +1,10 @@
 cask 'bitbar' do
-  version '1.6.0'
-  sha256 '1e177e1b86ed641c9bdd1a537177286447f2085d2f609955a53209fdd16e70bd'
+  version '1.7.0'
+  sha256 '0396bcbb3b8ae81b5d8b299aa689253dfeaad08ce5a66cd88f6c1191d8ed0c18'
 
   url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar-v#{version}.zip"
   appcast 'https://github.com/matryer/bitbar/releases.atom',
-          checkpoint: '9ca686f6e007eb1db6698cafcd6caad548e74943e102f8ed9c9b5355b7cd988a'
+          checkpoint: 'd6b93aae146cc1403f4e5ff37041eab3aca98444dd567a808bd348d3a7137ffc'
   name 'BitBar'
   homepage 'https://github.com/matryer/bitbar/'
   license :mit
