20cca98019f845cc329412d6ae656357b5ca9cc9
mahori <mahori@users.noreply.github.com>
2016-03-12 16:47:43 +0900

    Upgrade Acrobat Reader DC to 2015.010.20060

--- Casks/adobe-reader.rb
+++ Casks/adobe-reader.rb
@@ -1,6 +1,6 @@
 cask 'adobe-reader' do
-  version '2015.010.20056'
-  sha256 'bc6c00e34aea417cfe6e3b4c556a798b749d904d0da7ea837edad2fa1584c022'
+  version '2015.010.20060'
+  sha256 '4a19212e418a20db4da733f12a1d284e536b27e4eb0f3cd2db1c381344bbb7b8'
 
   url "http://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.gsub('.', '')[2..-1]}/AcroRdrDC_#{version.gsub('.', '')[2..-1]}_MUI.dmg"
   name 'Adobe Acrobat Reader DC'
