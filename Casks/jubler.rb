ae5b367a468c3bed96e9fc57d3a5208a0d8e8fd0
Vítor Galvão <info@vitorgalvao.com>
2016-03-12 10:17:19 -0500

    jubler.rb: updated vendor comment

--- Casks/jubler.rb
+++ Casks/jubler.rb
@@ -2,7 +2,7 @@ cask 'jubler' do
   version '5.0.5'
   sha256 '363f272cc1e15e02cf3e28935b9b2fdd91c43b93b2d1afef281e500c158b8bcd'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # downloads.sourceforge.net/sourceforge/jubler was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/sourceforge/jubler/Jubler-#{version}.dmg"
   name 'Jubler'
   homepage 'http://www.jubler.org/'
