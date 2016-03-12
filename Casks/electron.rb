4b247691b602e626b651aac484aae7695412d46e
Victor Popkov <victor@popkov.me>
2016-03-12 23:35:34 +0200

    updated electron (0.37.0)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,10 +1,10 @@
 cask 'electron' do
-  version '0.36.10'
-  sha256 '8692d812f70f75659c0f79ab1d376cd47a69d7fc9c5d3317f68acfdf97108ad7'
+  version '0.37.0'
+  sha256 '4bce5b8e227b8d2a2589be434915b406438cdaa7abafbae51ec860215f56efe1'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
-          checkpoint: '6bb06ab56edd0a0c555fa6d19f1b4ed851f8e318050df1c531ce73e21bf4ff3c'
+          checkpoint: 'ba9aff4f10f2596ecb902f45762eade82823b6c1c6000ee6ad57cd2098f16280'
   name 'Electron'
   homepage 'http://electron.atom.io/'
   license :mit
