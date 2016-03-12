46616a2f66aa89e98aab03164902e23bfd9ffce5
Vítor Galvão <info@vitorgalvao.com>
2016-03-12 09:58:33 -0500

    hsang.rb: updated vendor comment

--- Casks/hsang.rb
+++ Casks/hsang.rb
@@ -2,7 +2,7 @@ cask 'hsang' do
   version '1.6.1'
   sha256 'e747d0549630c24a81b3b74b9382cd47ecb594af123f7471a4f9753c3bf4b1ba'
 
-  # netease.com is the official download host per the vendor homepage
+  # nie.gdl.netease.com/lushi was verified as official when first introduced to the cask
   url "http://nie.gdl.netease.com/lushi/HSAng_#{version}.dmg"
   name 'HSAng'
   homepage 'http://lushi.163.com/'
