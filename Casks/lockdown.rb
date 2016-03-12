f5daace7475c613dbd992a02c08e44252852e970
Troy McCall <troyxmccall@gmail.com>
2016-03-12 16:16:45 -0500

    Add Lockdown.app v1.0.0

---
+++ Casks/lockdown.rb
@@ -0,0 +1,12 @@
+cask 'lockdown' do
+  version '1.0.0'
+  sha256 '047f377e2a9495361084268c86cc80719e123bd8958d69fe51cd2be0d7ffd764'
+
+  # bitbucket.org/objective-see/deploy was verified as official when first introduced to the cask
+  url "https://bitbucket.org/objective-see/deploy/downloads/Lockdown_#{version}.zip"
+  name 'Lockdown'
+  homepage 'https://objective-see.com/products/lockdown.html'
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'Lockdown.app'
+end
