df8759dd1d87a9d166cb23ff44d48e03dbfcbb2a
Victor Popkov <victor@popkov.me>
2016-03-12 23:36:01 +0200

    updated fluid (1.8.5)

--- Casks/fluid.rb
+++ Casks/fluid.rb
@@ -1,10 +1,10 @@
 cask 'fluid' do
-  version '1.8.4'
-  sha256 'abf5c99d45b23be3df1b62da798beab7a4fed176d28d508c2629d2d089a491ce'
+  version '1.8.5'
+  sha256 'aa644629b2f3ccf79e0fc88cd59d747d84e8ad330e5d6fc3d7e09f68214b67b5'
 
   url "http://fluidapp.com/dist/Fluid_#{version}.zip"
   appcast 'http://fluidapp.com/appcast/fluid1.rss',
-          checkpoint: 'c90b9c819d4a6269f177bd496b6a3b43045e9b25c3d66973763d5b01af4fff53'
+          checkpoint: '0db21720ba6bb7d289f452e7719f48e5ad0deb9e52ee9efc521857838e58416c'
   name 'Fluid'
   homepage 'http://fluidapp.com/'
   license :freemium
