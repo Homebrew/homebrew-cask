210bc276857c905748d4a53650272f1138b7f43d
Vítor Galvão <info@vitorgalvao.com>
2016-03-12 10:05:50 -0500

    updated itools (2.8.7)

--- Casks/itools.rb
+++ Casks/itools.rb
@@ -1,8 +1,8 @@
 cask 'itools' do
-  version '2.8.6'
-  sha256 '928a154c60a01a13c79273007a15403e7c0db50d40afb73e7691b7016c85ce6e'
+  version '2.8.7'
+  sha256 'f49b075991705f57de4e46cdb6ae9ca5f7d995dba8d528c5c48f1cc31ff23dc3'
 
-  # itools.hk is the official download host per the vendor homepage
+  # dl2.itools.hk/dl was verified as official when first introduced to the cask
   url "http://dl2.itools.hk/dl/iTools64_#{version}.dmg"
   name 'iTools'
   homepage 'http://pro.itools.cn/mac/english'
