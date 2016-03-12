826f190791b2589a55e7b017306efed30df50b57
Jeffrey C <spaz926@me.com>
2016-03-12 11:25:23 -0600

    updated tunnelbear (2.5.9)

--- Casks/tunnelbear.rb
+++ Casks/tunnelbear.rb
@@ -1,11 +1,11 @@
 cask 'tunnelbear' do
-  version '2.5.8'
-  sha256 '5c79dc8478572d4abbc8867e3badc0639a1f3468a8ca6e8f2bdeeb83453cd803'
+  version '2.5.9'
+  sha256 '469ac53e2ddefe2573a1231c22709a548ff8cb76ec4dae2cac02acacfac30316'
 
   # tunnelbear.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://tunnelbear.s3.amazonaws.com/downloads/mac/TunnelBear-#{version}.zip"
   appcast 'https://s3.amazonaws.com/tunnelbear/downloads/mac/appcast.xml',
-          checkpoint: '544799b7dc65c96cfcc5ef0f92cbcfcdedb687e7570c25080aa9491c3352e0cd'
+          checkpoint: '527f5dcf48d78bcb6c36f65df0a48fd03538be4c63bb6dff6b47823542a63432'
   name 'TunnelBear'
   homepage 'https://www.tunnelbear.com/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
