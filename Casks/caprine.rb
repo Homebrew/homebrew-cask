2cb0c73abceb623568b1987a04bfdd3d1641913c
Victor Popkov <victor@popkov.me>
2016-03-14 21:40:02 +0200

    updated caprine (1.2.0)

--- Casks/caprine.rb
+++ Casks/caprine.rb
@@ -1,10 +1,10 @@
 cask 'caprine' do
-  version '1.1.0'
-  sha256 'b853864169111509fe6329d8a9a6f78542a6f024ee086146f92ee978755a8ff8'
+  version '1.2.0'
+  sha256 '878a72e265eaf012276b1352c800fd86ac75b8543279984df417e23e4a6f1f0f'
 
   url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/Caprine-osx-#{version}.zip"
   appcast 'https://github.com/sindresorhus/caprine/releases.atom',
-          checkpoint: 'ffe1b6632b19a9538447f4258fd465c82b91e0ff6d026d7762912df785ec7fe9'
+          checkpoint: 'd754bf331577ee08513ae88a40dae9d8b48ae58c68db88d7530434af1d88090e'
   name 'Caprine'
   homepage 'https://github.com/sindresorhus/caprine'
   license :mit
