9a5ace153aca7f5edc7d40f4f954435a5317fa1e
ishu3101 <ishu3101@users.noreply.github.com>
2016-03-14 15:13:21 +1300

    Add sha256

--- Casks/popclip.rb
+++ Casks/popclip.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'popclip' do
   version '1.5.3'
-  sha256 :no_check
+  sha256 'e32bb0ecdd4b5a992d5b18d34dd929c3f237585c0bd52835bdd46fa907bdf089'
 
   url "https://d20vhy8jiniubf.cloudfront.net/downloads/PopClip-#{version}.zip"
   name 'Popclip'
