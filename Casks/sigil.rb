8fe80422a1e8d7122082cfd205ffe0582a2de98a
John Jelinek IV <john@johnjelinek.com>
2016-03-14 08:06:44 -0500

    updated sigil (0.9.4)

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -1,11 +1,11 @@
 cask 'sigil' do
-  version '0.9.3'
-  sha256 '83164918449ceee1080e1491c1d33ef9afabcf4d8866a4c738d24e58e27a89d0'
+  version '0.9.4'
+  sha256 '0324c2209000f1d27c9b233fa2f1e811046306a4ce29504ff1c4c9fd0d971f09'
 
   # github.com/Sigil-Ebook/Sigil was verified as official when first introduced to the cask
   url "https://github.com/Sigil-Ebook/Sigil/releases/download/#{version}/Sigil-#{version}-Mac-Package.dmg"
   appcast 'https://github.com/Sigil-Ebook/Sigil/releases.atom',
-          checkpoint: '0b17e785f19bbdb331a4f141e00804eaaafd210a64f4a1b84097abbeaba4c860'
+          checkpoint: 'c30813f46cccc3943608c882efcb688d6f9a879b217f815651adb798b8b9d0a2'
   name 'Sigil'
   homepage 'https://sigil-ebook.com/'
   license :gpl
