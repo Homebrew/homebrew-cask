1e244fe90c5164601e647e98d455ae71f8549126
Mark Castle <mark@castle.sx>
2016-03-14 11:36:08 -0700

    updated paw (2.3.2)

--- Casks/paw.rb
+++ Casks/paw.rb
@@ -1,10 +1,10 @@
 cask 'paw' do
-  version '2.3.1'
-  sha256 '0d357eb96c28ee17114ad09dc82370f9d1620e98ea9ae8a7641afc369751e585'
+  version '2.3.2'
+  sha256 '392d31d9ffacf769b1b94ac172c3e61478a740247e020db90f7bf5d70c5c073f'
 
   url "https://luckymarmot.com/paw/download/#{version}"
   appcast "https://luckymarmot.com/api/v#{version.major}/updates/appcast",
-          checkpoint: '125bec971b3e45197be8cd1d2f2c6c2df13ef36d175ffed53c9021f8865a7575'
+          checkpoint: 'd4e55fea3c983fecad58e295d2ba789a8e532338022483dcac6dc9d22b85f251'
   name 'Paw'
   homepage 'https://luckymarmot.com/paw'
   license :commercial
