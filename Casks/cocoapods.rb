ff6a0715951f4b17442557ed6bd7c11b512ca7bd
Vítor Galvão <info@vitorgalvao.com>
2016-03-14 11:08:31 -0400

    updated cocoapods (1.0.0.beta.5)

--- Casks/cocoapods.rb
+++ Casks/cocoapods.rb
@@ -1,11 +1,11 @@
 cask 'cocoapods' do
-  version '0.39.0'
-  sha256 '51c5d0117272a9b3c30c15dd0a9776574a23c5d810ec0c4b298b0ce4c483f27f'
+  version '1.0.0.beta.5'
+  sha256 '953f5ac33908b97fb5a370d6ab3db0b82e61f455875315bde95c6be7f45dda5a'
 
   # github.com/CocoaPods/CocoaPods was verified as official when first introduced to the cask
   url "https://github.com/CocoaPods/CocoaPods-app/releases/download/#{version}/CocoaPods.app-#{version}.tar.bz2"
   appcast 'https://app.cocoapods.org/sparkle',
-          checkpoint: 'd6f6b86a010ba244a2a323737cb3c7c5e3904dbb6e3c7edc302c514b0fe5bc5f'
+          checkpoint: '86d4ca53997c3bcbfb74629080800a32769c96fc5c186e8d89bd34c0ae9f6633'
   name 'CocoaPods.app'
   homepage 'https://cocoapods.org/'
   license :mit
