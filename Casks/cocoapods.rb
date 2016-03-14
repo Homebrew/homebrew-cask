225a0aab3429ce082dfc4cc60a060751140fcfbf
Orta <orta.therox@gmail.com>
2016-03-14 08:01:59 -0400

    update sha

--- Casks/cocoapods.rb
+++ Casks/cocoapods.rb
@@ -1,6 +1,6 @@
 cask 'cocoapods' do
   version '1.0.0.beta.5'
-  sha256 ''
+  sha256 '953f5ac33908b97fb5a370d6ab3db0b82e61f455875315bde95c6be7f45dda5a'
 
   # github.com/CocoaPods/CocoaPods was verified as official when first introduced to the cask
   url "https://github.com/CocoaPods/CocoaPods-app/releases/download/#{version}/CocoaPods.app-#{version}.tar.bz2"
