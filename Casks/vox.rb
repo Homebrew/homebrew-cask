7ec0d0e4a80b51a4e35cebc556cd8a317a5e3db8
Victor Popkov <victor@popkov.me>
2016-03-14 23:06:37 +0200

    updated vox (2740.4,1457956405)

--- Casks/vox.rb
+++ Casks/vox.rb
@@ -1,11 +1,11 @@
 cask 'vox' do
-  version '2730.1,1455479596'
-  sha256 '73afdc0f72a00b1b3721ace45acff7302ed79f08b593d61eb51f9a8823b4d6f2'
+  version '2740.4,1457956405'
+  sha256 '2a07ea6902d32e488c32f639a9c441bb4be1b089cb23d2edaad95ccf1d6d0830'
 
   # devmate.com/com.coppertino.Vox was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.coppertino.Vox/#{version.before_comma}/#{version.after_comma}/Vox-#{version.before_comma}.zip"
   appcast 'https://updates.devmate.com/com.coppertino.Vox.xml',
-          checkpoint: '4fae7195b8a22398ac7b750bb4fa404a600b5c0655d8ac8466018dd23810ad7c'
+          checkpoint: 'a78481e3d555e5225c088dca4744e3f023b4edc69f0171a9cfcafbdf25cb7f15'
   name 'VOX'
   homepage 'https://coppertino.com/vox/mac'
   license :freemium
