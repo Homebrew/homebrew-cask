4ca6511c87b88dbac5bd3ccdf9fe55e64d1bb232
Victor Popkov <victor@popkov.me>
2016-03-14 22:59:11 +0200

    updated p5 (0.5.10)

--- Casks/p5.rb
+++ Casks/p5.rb
@@ -1,12 +1,12 @@
 cask 'p5' do
   # note: "5" is not a version number, but an intrinsic part of the product name
-  version '0.5.9'
-  sha256 'a5e8b4e577c6a10af4a57819e51a1c7478eb0d8b5f8b67b1a0bfc7c04ae96961'
+  version '0.5.10'
+  sha256 '52d7abe71a9613c9ef694adeede7a0a9dfa78ff0a11602783fe0187c476bc25b'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/processing/p5.js-editor/releases/download/v#{version}/p5-mac.zip"
   appcast 'https://github.com/processing/p5.js-editor/releases.atom',
-          checkpoint: 'c991cb2dc7268711191ca1f99570141aeefbaac6f619821d972b328dcbc03530'
+          checkpoint: '15242dba0ba4ed40a22e23cdfc0b93d64781d229fcb05b0f2517aa2ef97588f3'
   name 'p5.js Editor'
   homepage 'http://p5js.org/download/#editor'
   license :mit
