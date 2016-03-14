db9549649d0c28123c462983b18d3d8c549d2bf2
ishu3101 <ishu3101@users.noreply.github.com>
2016-03-14 15:29:41 +1300

    Update version to 1.5.5 and its sha256

--- Casks/popclip.rb
+++ Casks/popclip.rb
@@ -1,6 +1,6 @@
 cask 'popclip' do
-  version '1.5.3'
-  sha256 'e32bb0ecdd4b5a992d5b18d34dd929c3f237585c0bd52835bdd46fa907bdf089'
+  version '1.5.5'
+  sha256 'e028c21c93c6632c85b63c5ea9b365ae76dd375455baec804506f0091b93a4be'
 
   # d20vhy8jiniubf.cloudfront.net was verified as official when first introduced to the cask
   url "https://d20vhy8jiniubf.cloudfront.net/downloads/PopClip-#{version}.zip"
