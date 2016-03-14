1cd725f3f18d881845ed638d8e1ae1f7f3668c9e
Victor Popkov <victor@popkov.me>
2016-03-14 21:40:48 +0200

    updated airparrot (2.4.0)

--- Casks/airparrot.rb
+++ Casks/airparrot.rb
@@ -1,10 +1,10 @@
 cask 'airparrot' do
-  version '2.3.2'
-  sha256 'e15e2e868d7398c56ab0ad3e06c249e1e5b08da0979b78e8211dfb2f61a319dc'
+  version '2.4.0'
+  sha256 'b0259013fa9f2a6f0c0d2458884a7812eb407b02e658cee2128faafe8907d5b6'
 
   url "https://download.airsquirrels.com/AirParrot2/Mac/AirParrot-#{version}.dmg"
   appcast 'https://updates.airsquirrels.com/AirParrot2/Mac/AirParrot2.xml',
-          checkpoint: '0ed53a274ffa088d2157d7e37544d2b097f2cd961190d327a3b39c915c42dfa2'
+          checkpoint: '89ba917b619eb0ee38572711cc522b4bd9e15aa263ac73a34836f872db30a9d1'
   name 'AirParrot'
   homepage 'http://www.airsquirrels.com/airparrot/'
   license :commercial
