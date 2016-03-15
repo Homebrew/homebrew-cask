3556ef708dcab3f3ef576e91cc4f349dcfade507
Alvaro Miranda <kikitux@gmail.com>
2016-03-15 17:47:01 +1300

    updated packer (0.10.0)

--- Casks/packer.rb
+++ Casks/packer.rb
@@ -1,6 +1,6 @@
 cask 'packer' do
-  version '0.9.0'
-  sha256 'bd5f8ce38ceb866c3f1db2eb3d51091184dd021dd785d05bb47177b223fea9df'
+  version '0.10.0'
+  sha256 'cb1d9768306466d566abc79d83911983ace6a9a6c0a6657dc7eaab03cabd2e21'
 
   # hashicorp.com is the official download host per the vendor homepage
   url "https://releases.hashicorp.com/packer/#{version}/packer_#{version}_darwin_amd64.zip"
