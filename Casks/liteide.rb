00078efc72a6c081c88de73d64fe1f75c5a7a1a4
Marius Karnauskas <marius.karnauskas@gmail.com>
2016-03-15 06:20:39 +0000

    Bump LiteIDE to v28

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,9 +1,9 @@
 cask 'liteide' do
-  version '27.2.1'
-  sha256 '4f142d79cd7161ff7e51cb38a0c5e60242315456bebe23b3ecaaf54227cd01ff'
+  version '28'
+  sha256 '5e5f4eea936bb9b3e9fa4b21fdd55a0a7de6e9c2ca53af5ddaaf863efa06ffd2'
 
   # sourceforge.net is the official download host per the vendor homepage
-  url "http://downloads.sourceforge.net/project/liteide/X#{version}/liteidex#{version}.macosx-qt5.zip"
+  url "http://downloads.sourceforge.net/project/liteide/X#{version}/liteidex#{version}.macosx-qt4.zip"
   name 'LiteIDE'
   homepage 'https://github.com/visualfc/liteide'
   license :gpl
