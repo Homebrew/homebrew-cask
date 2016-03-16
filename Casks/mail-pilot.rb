5a2947dcc1d59ce20d2af337b5c28cd7906f68f2
Anthony Green <anthony.green@bbc.co.uk>
2016-03-16 13:42:42 +0000

    Add Mail Pilot v2.3

---
+++ Casks/mail-pilot.rb
@@ -0,0 +1,11 @@
+cask 'mail-pilot' do
+  version '2.3'
+  sha256 '51a8bd123aa390625451f8fb73029bf67c639e7e182a8422f8d7b762defc6e43'
+
+  url 'https://vendors.paddle.com/download/product/497805'
+  name 'Mail mail-pilot'
+  homepage 'http://mindsense.co/mailpilot/'
+  license :commercial
+
+  app 'Mail Pilot 2.app'
+end
