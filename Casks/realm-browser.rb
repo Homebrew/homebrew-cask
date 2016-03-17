b6b76a66897559601a63d92b90a4dc3dda572bfb
Thi <t@thi.im>
2016-03-17 16:13:09 +0900

    updated realm-browser (0.98.5)

--- Casks/realm-browser.rb
+++ Casks/realm-browser.rb
@@ -1,10 +1,10 @@
 cask 'realm-browser' do
-  version '0.98.2'
-  sha256 '529d31d375a0a09b77d6c80531e3add99473e2b33ec457587698e148cee86abe'
+  version '0.98.5'
+  sha256 'b2cc181e498ddd6b36612862bfe548717bc433bbe0f3a20615f3c68ce79e7325'
 
-  url "https://github.com/realm/realm-browser-osx/releases/download/#{version}/RealmBrowser.app.zip"
+  url "https://github.com/realm/realm-browser-osx/releases/download/#{version}/RealmBrowser#{version}.zip"
   appcast 'https://github.com/realm/realm-browser-osx/releases.atom',
-          checkpoint: 'a82f1e55a88ba8c3502d57e3c5ff18a77557fbd4770734de07652c119d38e30d'
+          checkpoint: '59593c0313a9646ea54db68af510d33eed1054b18411ebbd3ff3441639b622ae'
   name 'Realm Browser'
   homepage 'https://github.com/realm/realm-browser-osx/'
   license :apache
