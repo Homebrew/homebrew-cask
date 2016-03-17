987eb907ae83310933c2828996f8839e1e89bffd
Pei-Tang Huang <tangtheone@gmail.com>
2016-03-17 23:59:33 +0800

    updated intellij-idea (2016.1)

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,20 +1,20 @@
 cask 'intellij-idea' do
-  version '15.0.4'
-  sha256 'fddfa3edcf966daee1b8e9b1ac0cd22c2ffc8bcea429342a599e31d411b773de'
+  version '2016.1'
+  sha256 '551f9696facb94bf3b593bfdb8ba59423acd7df9241b3b8e8cf93db75098196c'
 
-  url "https://download.jetbrains.com/idea/ideaIU-#{version}-custom-jdk-bundled.dmg"
+  url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   name 'IntelliJ IDEA'
   homepage 'https://www.jetbrains.com/idea/'
   license :commercial
 
-  app 'IntelliJ IDEA 15.app'
+  app 'IntelliJ IDEA.app'
 
   zap delete: [
                 "~/.IntelliJIdea#{version.major}",
-                "~/Library/Caches/IntelliJIdea#{version.major}",
-                "~/Library/Logs/IntelliJIdea#{version.major}",
-                "~/Library/Application Support/IntelliJIdea#{version.major}",
-                "~/Library/Preferences/IntelliJIdea#{version.major}",
+                "~/Library/Caches/IntelliJIdea#{version}",
+                "~/Library/Logs/IntelliJIdea#{version}",
+                "~/Library/Application Support/IntelliJIdea#{version}",
+                "~/Library/Preferences/IntelliJIdea#{version}",
                 '~/Library/Preferences/com.jetbrains.intellij.plist',
               ]
 end
