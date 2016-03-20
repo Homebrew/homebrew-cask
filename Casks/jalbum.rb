1bef70dd89f13677eb2cf0b9b34026a463885a3d
Gino Filicetti <gino@filicetti.net>
2016-03-20 18:32:45 -0400

    Add jAlbum.app v13 Initial creation of jAlbum.app First attempt at jAlbum cask Passed install / uninstall test Passed audit Passed style check
    Removed superfluous uninstall line and removed template comment left over
    
    Added a sha256 but iffy about it due to continuous release

---
+++ Casks/jalbum.rb
@@ -0,0 +1,11 @@
+cask 'jalbum' do
+  version '13'
+  sha256 '1b6466ad6b2272addeaa23b70ac3714b4df3357d0c168ea58ff6060c8cf33e4c'
+
+  url "http://download.jalbum.net/download/#{version}/MacOSX/jAlbum.dmg"
+  name 'jAlbum'
+  homepage 'https://jalbum.net'
+  license :freemium
+
+  app 'jAlbum.app'
+end
