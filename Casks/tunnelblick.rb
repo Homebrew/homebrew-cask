625da7f0ed9b75e8f0831d31346adba14c7577c9
shonjir <shonjir@gmail.com>
2016-03-19 17:39:39 -0700

    updated tunnelblick (3.6.0a_build_4543.4546)
    Tunneblick 3.6.0 no longer functions on OSX prior to 10.6.  Version 3.5 continues to work on 10.4 and 10.5.

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -1,6 +1,11 @@
 cask 'tunnelblick' do
-  version '3.5.8_build_4270.4530'
-  sha256 'd634a9a5fd06f95c7325987197fb7890119bf0a40a3958284814958a14c20ce3'
+  if MacOS.release <= :snow_leopard
+    version '3.5.8_build_4270.4530'
+    sha256 'd634a9a5fd06f95c7325987197fb7890119bf0a40a3958284814958a14c20ce3'
+  else
+    version '3.6.0a_build_4543.4546'
+    sha256 '0850dffd539cc60097d29fdb8a8e6b1cf83eeec7ea8bc7c4a821df69fe817441'
+  end
 
   url "https://www.tunnelblick.net/release/Tunnelblick_#{version}.dmg"
   name 'Tunnelblick'
