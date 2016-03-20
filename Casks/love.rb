05a0a90736cec67e9c2b401e0d16e9f34176728e
Victor Popkov <victor@popkov.me>
2016-03-20 22:17:26 +0200

    love.rb: fix vendor comment

--- Casks/love.rb
+++ Casks/love.rb
@@ -2,10 +2,10 @@ cask 'love' do
   version '0.10.1'
   sha256 '8412f6c451a60118527a42423a99e4583da79b5ff4dd18900755f97527ac1725'
 
-  # bitbucket.org is the official download host per the vendor homepage
+  # bitbucket.org/rude/love was verified as official when first introduced to the cask
   url "https://bitbucket.org/rude/love/downloads/love-#{version}-macosx-x64.zip"
   name 'LÖVE'
-  homepage 'https://love2d.org'
+  homepage 'https://love2d.org/'
   license :oss
 
   app 'love.app'
