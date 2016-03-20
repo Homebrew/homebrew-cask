b139f76699f3412feea0f0b53e2d0fc4219557c3
Victor Popkov <victor@popkov.me>
2016-03-20 22:18:50 +0200

    munki.rb: fix vendor comment

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -2,7 +2,7 @@ cask 'munki' do
   version '2.5.1.2630'
   sha256 'cf2c50078f1bf02225ac886536cca0448455a0926bc3aa5e53ea0bac2261f7fa'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/munki/munki was verified as official when first introduced to the cask
   url "https://github.com/munki/munki/releases/download/v#{version.sub(%r{^(\d+\.\d+.\d+).*}, '\1')}/munkitools-#{version}.pkg"
   appcast 'https://github.com/munki/munki/releases.atom',
           checkpoint: 'babf25ad250efca9b32499be3b45401d6ae3a2358614a87dc5278f5f8c4cd244'
