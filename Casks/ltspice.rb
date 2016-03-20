4bfc884b3a708e75bf5f8082c43d79772b6c2520
Victor Popkov <victor@popkov.me>
2016-03-20 22:17:43 +0200

    ltspice.rb: fix vendor comment

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -2,7 +2,7 @@ cask 'ltspice' do
   version :latest
   sha256 :no_check
 
-  # tech.com is the official download host per the vendor homepage
+  # ltspice.linear-tech.com was verified as official when first introduced to the cask
   url 'http://ltspice.linear-tech.com/LTspiceIV.dmg'
   name 'LTspice'
   homepage 'http://www.linear.com/designtools/software/'
