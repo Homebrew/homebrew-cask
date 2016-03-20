0063de544b3a0b55fcca4beca8e8931faf44ceae
Victor Popkov <victor@popkov.me>
2016-03-20 22:18:23 +0200

    menubar-countdown.rb: fix vendor comment

--- Casks/menubar-countdown.rb
+++ Casks/menubar-countdown.rb
@@ -2,7 +2,7 @@ cask 'menubar-countdown' do
   version '1.2'
   sha256 '4ee0a7a87dbd4013c461b59316c749a5f9a92160bdf6d90afb1ff029f9381c01'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/capablehands was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/capablehands/downloads/MenubarCountdown-#{version}.zip"
   name 'Menubar Countdown'
   homepage 'http://capablehands.net/menubarcountdown'
