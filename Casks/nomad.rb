2a0b577d3203554e1a98332907bbbb9d2b1c9de5
Victor Popkov <victor@popkov.me>
2016-03-20 22:21:08 +0200

    nomad.rb: fix vendor comment

--- Casks/nomad.rb
+++ Casks/nomad.rb
@@ -2,7 +2,7 @@ cask 'nomad' do
   version '0.3.1'
   sha256 'c300a3b5c195fbccf26dfcdb756b4c953ca0e5e566a41fce1d10ef1947ec4279'
 
-  # hashicorp.com is the official download host per the vendor homepage
+  # hashicorp.com/nomad was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/nomad/#{version}/nomad_#{version}_darwin_amd64.zip"
   name 'Nomad'
   homepage 'https://nomadproject.io/'
