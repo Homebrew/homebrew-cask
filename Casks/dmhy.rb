987940d3a84c118cd94bc1ce3ba8e73fb43add1a
yaqinking <yaqinking@yahoo.co.jp>
2016-03-21 14:39:59 +0800

    Upgrade DMHY.app to v1.3

--- Casks/dmhy.rb
+++ Casks/dmhy.rb
@@ -1,6 +1,6 @@
 cask 'dmhy' do
-  version '1.2'
-  sha256 '70bccf20c1ac24796ccb077e4589aaca603553a394ab0e5f16414788dcbebdec'
+  version '1.3'
+  sha256 '8c41c2b0a28f846495e550d0ffbade5d23e295491f4d04a26daa0d6f2cb9d153'
 
   url "https://github.com/yaqinking/DMHY/releases/download/v#{version}/DMHY.app.zip"
   name 'DMHY'
