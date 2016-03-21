fcc74e04b4732668bce415dd0dea9ba09ad9046f
Victor Popkov <victor@popkov.me>
2016-03-22 00:21:19 +0200

    updated wwdc (3.2)

--- Casks/wwdc.rb
+++ Casks/wwdc.rb
@@ -1,10 +1,10 @@
 cask 'wwdc' do
-  version '3.1'
-  sha256 '06ea52cfe18f8f72a79d9c944af7b32f9da0aa2e96eb662d5aa28047ec606044'
+  version '3.2'
+  sha256 '601afec4cc5b51a24a8776a5c2267e585e669db638fc2a6c72c2ef1218d9580e'
 
   url "https://github.com/insidegui/WWDC/releases/download/#{version}/WWDC_v#{version}.zip"
   appcast 'https://github.com/insidegui/WWDC/releases.atom',
-          checkpoint: '8a21858bad2dadf7f686251993c918df986a5a66c3e46230e7a442c1d87cefb0'
+          checkpoint: 'dc5e956921c4797b837fd740637655d99252e8f1e6f18c00188ad7b422ad3b72'
   name 'WWDC'
   homepage 'https://github.com/insidegui/WWDC'
   license :bsd
