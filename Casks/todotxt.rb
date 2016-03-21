938f1b805b8f1bd06cfe2e23e96b3a4f93a56f9c
Victor Popkov <victor@popkov.me>
2016-03-22 00:15:40 +0200

    updated todotxt (2.1.0)

--- Casks/todotxt.rb
+++ Casks/todotxt.rb
@@ -1,11 +1,11 @@
 cask 'todotxt' do
-  version '2.0.0'
-  sha256 'b859e8fa3a2fabe3743ce85a29bf1243b29b1552271a5660b2c6c1571576c601'
+  version '2.1.0'
+  sha256 '9f58cd655a4f8711ac6419038328a99a631944a17417b457254412996749a393'
 
   # github.com/mjdescy/TodoTxtMac was verified as official when first introduced to the cask
   url "https://github.com/mjdescy/TodoTxtMac/releases/download/#{version}/TodoTxtMac.app.zip"
   appcast 'https://github.com/mjdescy/TodoTxtMac/releases.atom',
-          checkpoint: '5490bd8ddb040bc53852d5d4ef1719015e3182442431bbf0523dbc49843ea739'
+          checkpoint: 'ef8102fe3bd7e8f151c407cf11250a8b942080a22e9effe1c229a47fad5fe222'
   name 'TodoTxtMac'
   homepage 'https://mjdescy.github.io/TodoTxtMac/'
   license :mit
