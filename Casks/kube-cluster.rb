349f23444344c6cfa2580a8b26967d3064c4c931
Victor Popkov <victor@popkov.me>
2016-03-21 23:07:47 +0200

    updated kube-cluster (0.2.1)

--- Casks/kube-cluster.rb
+++ Casks/kube-cluster.rb
@@ -1,10 +1,10 @@
 cask 'kube-cluster' do
-  version '0.2.0'
-  sha256 '44f7456a5a1bb004e2b4ae6694c3758df3f5ac748d6bf5e6f22a7914a3670ee7'
+  version '0.2.1'
+  sha256 'e9c2f10617feeb6c49d12b65014a5d38396478f96f66c659ad71c284230ebde5'
 
   url "https://github.com/TheNewNormal/kube-cluster-osx/releases/download/v#{version}/Kube-Cluster_v#{version}.dmg"
   appcast 'https://github.com/TheNewNormal/kube-cluster-osx/releases.atom',
-          checkpoint: '113ada88eaf58bcf9f988b5702d686a10e06b891d6af5adf953ccce54d26a48a'
+          checkpoint: '0a2e3e70f57dbc700045ca469c87939e74c40f1fbe0e4d4bb883095e74f8fe2c'
   name 'Kube-Cluster'
   homepage 'https://github.com/TheNewNormal/kube-cluster-osx'
   license :apache
