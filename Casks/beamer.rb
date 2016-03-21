c4efacf595ebc8cc8fc04cf444b8f24faeb90694
Victor Popkov <victor@popkov.me>
2016-03-21 22:50:38 +0200

    updated beamer (3.1.2)

--- Casks/beamer.rb
+++ Casks/beamer.rb
@@ -1,10 +1,10 @@
 cask 'beamer' do
-  version '3.1.1'
-  sha256 '5d18d26964c6810a68304b34cba9e7438cbf0589fc39f29d1b02be7d47d93d4f'
+  version '3.1.2'
+  sha256 '3190c7e4e6902a1a9a2d36227c9265a17d946bb9b7acd62517af7b5ec33266b0'
 
   url "https://beamer-app.com/resources/downloads/Beamer-#{version}.zip"
   appcast 'https://beamer-app.com/beamer3-appcast.xml',
-          checkpoint: 'b6f0db65f5f61ed54e1d4c8de62f58f087ed6b3f1fd109fb5425290bf1800457'
+          checkpoint: 'efc534002867fa76b4e190e0614f7d4b3e35fcd27a33814716309bf6d3477f7d'
   name 'Beamer'
   homepage 'https://beamer-app.com/'
   license :commercial
