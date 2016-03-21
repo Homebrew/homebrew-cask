4489160fc33f872f2a629feac2cb6779edad7009
Victor Popkov <victor@popkov.me>
2016-03-22 00:19:52 +0200

    updated tvrenamer (0.7.2)

--- Casks/tvrenamer.rb
+++ Casks/tvrenamer.rb
@@ -1,11 +1,11 @@
 cask 'tvrenamer' do
-  version '0.7.1'
-  sha256 '51c56fca7ce361aed823d4b943a438f4d0b1d516620c4f8548cbefbff0ecf309'
+  version '0.7.2'
+  sha256 '6d7ba27e26cbad16287f85db0d1b2b621f5ffd5c9a2dad73f1b63421eb2f0373'
 
   # github.com/tvrenamer/tvrenamer was verified as official when first introduced to the cask
   url "https://github.com/tvrenamer/tvrenamer/releases/download/#{version}/TVRenamer-#{version}-osx64.zip"
   appcast 'https://github.com/tvrenamer/tvrenamer/releases.atom',
-          checkpoint: '4b7e78cab3a5dcf2fc2067ab6daa594740deb345929b1d77c8035cb454e19861'
+          checkpoint: '2bf2f27122580b34bb9c5d1c3c06ca9d6a629e09365b4db6ad5244573a6b8c18'
   name 'TVRenamer'
   homepage 'http://tvrenamer.org/'
   license :gpl
