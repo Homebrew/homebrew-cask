0da15980234ad61c48cd8138f49b3ba33fabd843
Vítor Galvão <info@vitorgalvao.com>
2016-03-21 09:54:38 -0400

    icompta.rb: fix app stanza

--- Casks/icompta.rb
+++ Casks/icompta.rb
@@ -2,11 +2,15 @@ cask 'icompta' do
   if MacOS.release <= :lion
     version '4.0.13'
     sha256 '8c7dcd69f4d38f2532a32ad7e14b5032811ac5b893566f46fef38b8c61a1f261'
+
     url "http://www.icompta-app.com/files/iCompta_#{version}.dmg"
+    app 'iCompta.app'
   else
     version :latest
     sha256 :no_check
+
     url 'http://www.icompta-app.com/files/iCompta_Demo.zip'
+    app 'iCompta Demo.app'
   end
 
   name 'iCompta'
@@ -14,6 +18,4 @@ cask 'icompta' do
   license :commercial
 
   depends_on macos: '>= :leopard'
-
-  app 'iCompta.app'
 end
