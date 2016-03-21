8200edd985ff199be879047af5bc1fd333f0d683
Victor Popkov <victor@popkov.me>
2016-03-21 22:58:57 +0200

    updated gear-player (2.2.23)

--- Casks/gear-player.rb
+++ Casks/gear-player.rb
@@ -1,10 +1,10 @@
 cask 'gear-player' do
-  version '2.2.22'
-  sha256 'd08a938272441c2c62d7678e732b56330a910db495119fe3fc25d2250806583f'
+  version '2.2.23'
+  sha256 '40d4eb262730c9ed3663530aa1b78fb79a01eab2bdc65f19fb77e1786a9f6e85'
 
   url 'https://dl.gearmusicplayer.com/gearupdate.zip'
   appcast 'https://dl.gearmusicplayer.com/gearcast.xml',
-          checkpoint: 'b21b8393fec67538bba244585ede91e5d2efa42d89b98f0bd7754f332beed212'
+          checkpoint: 'aba787ebd4984d4ddeb44747f660af4153f28f73c0ab1aac7a3ec3495f6f7518'
   name 'Gear Player'
   homepage 'https://www.gearmusicplayer.com'
   license :commercial
