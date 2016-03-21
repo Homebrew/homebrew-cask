aa74c27bf7bc9d020f61a266663b4b08bd9536b9
Victor Popkov <victor@popkov.me>
2016-03-22 00:26:13 +0200

    updated xamarin-studio (5.10.3.27-0)

--- Casks/xamarin-studio.rb
+++ Casks/xamarin-studio.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-studio' do
-  version '5.10.3.26-0'
-  sha256 'edd4339e029f7dce825d1a6d6506d8fdcb907e9c907ce9de36d82e40bf27e02e'
+  version '5.10.3.27-0'
+  sha256 'bd8d37eda0797575e2f6206d430271cdd3025d206fe084d1d8526ec5b289bb8a'
 
   url "https://download.xamarin.com/studio/Mac/XamarinStudio-#{version}.dmg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '14727c4cd976ca3d96a7a9c362bebd064a49c4f24d32296d33767e3a76b4a685'
+          checkpoint: '7fd7fddb12ef8962a108073eaf8d0751fc200266439aae050ac318d7c242c940'
   name 'Xamarin Studio'
   homepage 'https://xamarin.com/studio'
   license :gpl
