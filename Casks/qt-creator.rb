3c22c31ae79533370307419662214d6ef0e86ee9
Hervé Yviquel <hyviquel@gmail.com>
2016-03-23 14:48:49 -0300

    Update qt-creator to 3.6.1

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator' do
-  version '3.6.0'
-  sha256 '6aedd15f0e57dcf8211892aba04853ecb43f2a142d856e74e26dd3c88d95639f'
+  version '3.6.1'
+  sha256 '0e4806e69105d141f767908694d0c59739fb55bfd01f5e72b83a6abad87203ae'
 
   url "http://download.qt.io/official_releases/qtcreator/#{version.to_f}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
