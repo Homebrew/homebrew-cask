056a5096505b670088f8382b94c63d246a9d95fe
Victor Popkov <victor@popkov.me>
2016-03-25 00:19:14 +0200

    updated slack (2.0.1)

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,11 +1,11 @@
 cask 'slack' do
-  version '2.0.0'
-  sha256 'd142f6ed26252c40ca56218aa95675be94788aea1516088a687ad535539dc3e0'
+  version '2.0.1'
+  sha256 'a903dd21f72c7808803de0f59705411602d3411750eee829a26d3834dad7694b'
 
   # slack-ssb-updates.global.ssl.fastly.net was verified as official when first introduced to the cask
   url "https://slack-ssb-updates.global.ssl.fastly.net/mac_public_releases/slack-#{version}.zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/38e415752d573e7e78e06be8daf5acc1',
-          checkpoint: '489190b6860e056a8bcdb44914659205e03ef59bbf761efcc39fe01cd1de6449'
+          checkpoint: '586e0bc6cce7c1196b18e88f6b70474b03fd04812e9e5eb99dde3b27ada65e75'
   name 'Slack'
   homepage 'https://slack.com/'
   license :gratis
