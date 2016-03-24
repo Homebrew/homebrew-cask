e5edde8afd6fbf27dacd236be4ad3aee1d22342b
Victor Popkov <victor@popkov.me>
2016-03-24 23:56:24 +0200

    updated pandoc (1.17.0.2)

--- Casks/pandoc.rb
+++ Casks/pandoc.rb
@@ -1,11 +1,11 @@
 cask 'pandoc' do
-  version '1.17'
-  sha256 '8be59e86e2438d8071f9033ac3adf0319d70ff9bd3a944b3b5e92769b8f0b787'
+  version '1.17.0.2'
+  sha256 '79aa0bdbcc5df8c87041a5497f1dc090ea6d993a9788f650d53abf96c68228bd'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/jgm/pandoc/releases/download/#{version}/pandoc-#{version}-osx.pkg"
   appcast 'https://github.com/jgm/pandoc/releases.atom',
-          checkpoint: '70aaecdeba6ac0d132bef954b808b1a64edaf9f7f3a7919147035668091b8ebf'
+          checkpoint: '2b4cb56d54977995bf215de6b8caf7964b9d5dbbeb79ef19c971e398e345b49b'
   name 'Pandoc'
   homepage 'http://pandoc.org/'
   license :gpl
