93e7a809f5337f2097980511ef6c7ac7e61b1a98
Victor Popkov <victor@popkov.me>
2016-03-25 00:22:44 +0200

    updated spriteilluminator (1.3.0)

--- Casks/spriteilluminator.rb
+++ Casks/spriteilluminator.rb
@@ -1,10 +1,10 @@
 cask 'spriteilluminator' do
-  version '1.2.2'
-  sha256 '30b9e2f76e1d37a367c39770412f2673e90983d52321050b5c8c2ee75f8ce79b'
+  version '1.3.0'
+  sha256 'f8eb5acc5abc7ecc5edcfc795f1018bf1ca974360f4dfa814cc829a036596f77'
 
   url "https://www.codeandweb.com/download/spriteilluminator/#{version}/SpriteIlluminator-#{version}-uni.dmg"
   appcast 'https://www.codeandweb.com/releases/SpriteIlluminator/appcast-mac-release.xml',
-          checkpoint: '6eecc1343d3aebcd37e00cb8ea5954c0830e0959d6c542a45abf124b9533396b'
+          checkpoint: 'b295ea112a460f8f2217d40710412ba10b9756eae2d2579f39990949a982f0cf'
   name 'SpriteIlluminator'
   homepage 'https://www.codeandweb.com/spriteilluminator'
   license :commercial
