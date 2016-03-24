397afcdcc2cdbfeb40aa31c96b22571a49eb454c
Victor Popkov <victor@popkov.me>
2016-03-24 23:55:14 +0200

    updated near-lock (3.5.1)

--- Casks/near-lock.rb
+++ Casks/near-lock.rb
@@ -1,10 +1,10 @@
 cask 'near-lock' do
-  version '3.4.0'
-  sha256 'df699f4244f86e827da2e24d547a8413061e8ad8b3451fb759acaef3f7e1886e'
+  version '3.5.1'
+  sha256 'd65459f0f06f094e9ac83c1aafb2b774889e6485174e56b3a73a34ea8875a810'
 
   url 'http://nearlock.me/downloads/nearlock.dmg'
   appcast 'http://nearlock.me/downloads/nearlock.xml',
-          checkpoint: '4a70ed81b4ef1f93921752ef010374ad0beae717b08c52651c00ed4503a70b47'
+          checkpoint: '2f2118e5fcf9de4548cdcf251bfc9425887ebf3e2727e9c8f50a2ed4e5754368'
   name 'Near Lock'
   homepage 'http://nearlock.me/'
   license :gratis
