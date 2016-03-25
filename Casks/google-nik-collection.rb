9090c6178284c3698348e309b628a798f292f3b1
Ben <BenjaminHCCarr@users.noreply.github.com>
2016-03-24 22:22:26 -0400

    Update google-nik-collection.rb

--- Casks/google-nik-collection.rb
+++ Casks/google-nik-collection.rb
@@ -1,8 +1,8 @@
 cask 'google-nik-collection' do
-  version '1.2.1'
+  version '1.2.11'
   sha256 '7a06f90c8c6a9f09dc882dfb7a84cd4d97dae39e064540ecc2f5daa87569df2b'
 
-  url 'https://dl.google.com/edgedl/photos/nikcollection-full-1.2.11.dmg'
+  url 'https://dl.google.com/edgedl/photos/nikcollection-full-#{version}.dmg'
   name 'Google Nik Collection'
   homepage 'https://www.google.com/nikcollection/'
   license :commercial
