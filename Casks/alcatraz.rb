4fe59cc2de778cba4038f282dba0706321966123
Tommy Sparber <tommy@sparber.net>
2016-03-26 23:10:51 +0100

    updated alcatraz (1.1.18)

--- Casks/alcatraz.rb
+++ Casks/alcatraz.rb
@@ -1,10 +1,10 @@
 cask 'alcatraz' do
-  version '1.1.16'
-  sha256 '7733bccb7af18f4e2092f0fbd9370b8082a24550521956b08935e7834e4493ee'
+  version '1.1.18'
+  sha256 'ac76e9db045099cf87e24c9a29b69bf64952d2681bb1c45a9f7eb677b8c69cd2'
 
   url "https://github.com/alcatraz/Alcatraz/releases/download/#{version}/Alcatraz.tar.gz"
   appcast 'https://github.com/alcatraz/Alcatraz/releases.atom',
-          checkpoint: '5d0705177737f6b9d875c0b7fe91797177286f60db76b5ed513a973862bbb066'
+          checkpoint: '0c5cc9b751a09bc2dacee65b80a328b4e73782c6853cabd97be6913accfb0317'
   name 'alcatraz'
   homepage 'http://alcatraz.io'
   license :mit
