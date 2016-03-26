b9dc684aebef2afef6e167906b5239cf7f339332
Steven <SKN@localhost.local>
2016-03-26 16:06:42 +0100

    Add bitcoin-xt v0.11E Fixed the comments

--- Casks/bitcoin-xt.rb
+++ Casks/bitcoin-xt.rb
@@ -2,7 +2,8 @@ cask 'bitcoin-xt' do
   version '0.11E'
   sha256 '1a1dcfcaef0d3cf87b8994eca25aa53789650220c783488e10eb74eac287d40a'
 
-  url 'https://github.com/bitcoinxt/bitcoinxt/releases/download/v0.11E/BitcoinXT-0.11E.dmg',
+  # github.com/bitcoinxt/bitcoinxt was verified as official when first introduced to the cask
+  url "https://github.com/bitcoinxt/bitcoinxt/releases/download/v#{version}/BitcoinXT-#{version}.dmg",
       referer: 'https://github.com/bitcoinxt/bitcoinxt/releases/',
       cookies: {
                  'i_follow_redirects' => 'yes',
@@ -10,9 +11,7 @@ cask 'bitcoin-xt' do
                }
   appcast 'https://github.com/bitcoinxt/bitcoinxt/releases.atom',
           checkpoint: '843d6ef3717eaf20b2e88f78e3eacae10dc745a0c134f34187acfdbfcfdb4d8d'
-  # Vendor dev and release is on github official homepage is hosted separatly
-  name 'Bitcoin XT',
-       'Bitcoin-XT'
+  name 'Bitcoin XT'
   homepage 'https://bitcoinxt.software/'
   license :oss
 
