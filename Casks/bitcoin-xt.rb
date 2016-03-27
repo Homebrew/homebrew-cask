36b26c694d50a21d78431ccecc8520bc68a21531
Steven <SKN@localhost.local>
2016-03-27 08:27:46 +0200

    Fixed the addressed issues - container type removed - zap delete is array now - redundant caveats removed - streamlined the url parameters

--- Casks/bitcoin-xt.rb
+++ Casks/bitcoin-xt.rb
@@ -4,10 +4,8 @@ cask 'bitcoin-xt' do
 
   # github.com/bitcoinxt/bitcoinxt was verified as official when first introduced to the cask
   url "https://github.com/bitcoinxt/bitcoinxt/releases/download/v#{version}/BitcoinXT-#{version}.dmg",
-      referer: 'https://github.com/bitcoinxt/bitcoinxt/releases/',
       cookies: {
                  'i_follow_redirects' => 'yes',
-                 'logged_in'          => 'no',
                }
   appcast 'https://github.com/bitcoinxt/bitcoinxt/releases.atom',
           checkpoint: '843d6ef3717eaf20b2e88f78e3eacae10dc745a0c134f34187acfdbfcfdb4d8d'
@@ -16,13 +14,13 @@ cask 'bitcoin-xt' do
   license :mit
 
   conflicts_with cask: 'bitcoin-core'
-  container type: :dmg
-
-  zap delete: '~/Library/Application Support/Bitcoin/'
-      delete: '~/Library/Preferences/org.bitcoin.Bitcoin-Qt.plist'
-
-  caveats "#{token} and Bitcoin Core will try to operate on the same Blockchain, it is not advised to run them simulatneously."
-          "Your Wallet will be deleted when use brew cask zap #{token} OR bitcoin-core. Please move wallet.dat out of ~/Library/Application Support/Bitcoin/ or use File>Backup Wallet... beforehand. If wallet.dat got lost try to recover it with Time Machine or any other System Backup existend."
 
   app 'Bitcoin-XT.app'
+
+  zap delete: [
+                '~/Library/Application Support/Bitcoin/',
+                '~/Library/Preferences/org.bitcoin.Bitcoin-Qt.plist',
+              ]
+
+  caveats "Your Wallet will be deleted when use brew cask zap #{token} OR bitcoin-core. Please move wallet.dat out of ~/Library/Application Support/Bitcoin/ or use File>Backup Wallet... beforehand. If wallet.dat got lost try to recover it with Time Machine or any other System Backup existend."
 end
