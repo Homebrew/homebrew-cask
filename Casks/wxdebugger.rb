ec5a3424f112be58824ec02ad5d2ddd9a74486c7
Vítor Galvão <info@vitorgalvao.com>
2016-03-28 15:00:54 -0400

    wxdebugger.rb: fix version

--- Casks/wxdebugger.rb
+++ Casks/wxdebugger.rb
@@ -1,8 +1,8 @@
 cask 'wxdebugger' do
-  version '0.4.0,0.4.0'
+  version '0.4.0'
   sha256 'f9c91dd30c89bcc9b376a89dab2ea7e129fca54cf1889ecb6d24f13f5d565f44'
 
-  url "http://dldir1.qq.com/WechatWebDev/#{version.after_comma}/wxdebugger_#{version.before_comma}.dmg"
+  url "http://dldir1.qq.com/WechatWebDev/#{version}/wxdebugger_#{version}.dmg"
   name 'wxdebugger'
   name '微信web开发者工具'
   homepage 'http://mp.weixin.qq.com/wiki/10/e5f772f4521da17fa0d7304f68b97d7e.html'
