f95842dd21f6809a35d17bf38508ea3a30ce31c5
Sean <sean@tox.im>
2016-03-28 13:15:05 -0700

    Seamonkey 2.39 -> 2.40

--- Casks/seamonkey.rb
+++ Casks/seamonkey.rb
@@ -1,6 +1,6 @@
 cask 'seamonkey' do
-  version '2.39'
-  sha256 '43bc6ad087242055db6ae6a3f14ef83729f4b455d7a2e10f7935a9304d84dcf8'
+  version '2.40'
+  sha256 '51eb9ebc5578e81692d8c03cd2e7723f2279a4462c2afd2779b38c29bf12bd75'
 
   # mozilla.org is the official download host per the vendor homepage
   url "https://download.mozilla.org/?product=seamonkey-#{version}&os=osx&lang=en-US"
