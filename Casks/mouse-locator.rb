7fb7e8212645607639c37240e756e97767046e59
Paul  Cav <pacav69@gmail.com>
2016-03-28 18:59:46 +0800

    updated mouse-locater to v1.1
    Passed audit and style checks

--- Casks/mouse-locator.rb
+++ Casks/mouse-locator.rb
@@ -1,6 +1,6 @@
 cask 'mouse-locator' do
-  version :'1.1'
-  sha256 :‘1809760210e5afb80f9be34dc930c0c6fb84efee91747640d2d9717561149645’
+  version '1.1'
+  sha256 '1809760210e5afb80f9be34dc930c0c6fb84efee91747640d2d9717561149645'
 
   url 'http://www.2point5fish.com/files/MouseLocator.dmg'
   name 'Mouse Locator'
