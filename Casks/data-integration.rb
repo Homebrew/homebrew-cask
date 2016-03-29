a72f26de5090828f48449bbceccbada6d163be24
Joey Smith <joeysmith@gmail.com>
2016-03-29 09:21:05 -0600

    Update hash to 6.0.1.0-386 value

--- Casks/data-integration.rb
+++ Casks/data-integration.rb
@@ -1,6 +1,6 @@
 cask 'data-integration' do
   version '6.0.1.0-386'
-  sha256 '240e72e2227f1e3e4c7b7173a42157a1ba0ef0e2055ffa3122d2f633ca9e14c6'
+  sha256 '98a1877977cfba5c2ea40baf23921d418068d3ff19d5f949b85b0b036a45cb85'
 
   # sourceforge.net/sourceforge/pentaho was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/sourceforge/pentaho/pdi-ce-#{version}.zip"
