81e0cee0e2eae5e2e81f7a210eb136bdc5a2d8e8
Viktor Eriksson <viktor.eriksson2@gmail.com>
2016-03-29 16:22:58 +0200

    updated bitbar (1.9.1)

--- Casks/bitbar.rb
+++ Casks/bitbar.rb
@@ -1,10 +1,10 @@
 cask 'bitbar' do
   version '1.9.1'
-  sha256 '86f255d4f426159ec5b184dfdc27c06d6aa1a11ed3ce16cf08b5289115967467'
+  sha256 '72350013b295ef1fa4beb1374bd3d2bdc5a7335c42666e8592b574799f9ced65'
 
   url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar-v#{version}.zip"
   appcast 'https://github.com/matryer/bitbar/releases.atom',
-          checkpoint: '39dc809ebfebcca0c7456d19f9991f07a386ada0fa92f964a897007114f4963c'
+          checkpoint: 'e3e891b60d43bff854ddc78d7639323b42ce10c1a012a0b80b6b16dfc98e177e'
   name 'BitBar'
   homepage 'https://github.com/matryer/bitbar/'
   license :mit
