3f0c3581b3ce6d3faf68a995893748d460871c6c
Joey Smith <joeysmith@gmail.com>
2016-03-29 09:08:32 -0600

    Update data-integration to version 6.0.1.0-386

--- Casks/data-integration.rb
+++ Casks/data-integration.rb
@@ -1,5 +1,5 @@
 cask 'data-integration' do
-  version '5.4.0.1-130'
+  version '6.0.1.0-386'
   sha256 '240e72e2227f1e3e4c7b7173a42157a1ba0ef0e2055ffa3122d2f633ca9e14c6'
 
   # sourceforge.net/sourceforge/pentaho was verified as official when first introduced to the cask
