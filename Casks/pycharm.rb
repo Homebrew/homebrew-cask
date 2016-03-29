132703320610ca98f9cb150b341f7359f2416a17
tty1 <tty1@users.noreply.github.com>
2016-03-29 14:55:23 +0000

    Fix PyCharm 2016.1

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -9,11 +9,14 @@ cask 'pycharm' do
 
   app 'PyCharm.app'
 
+  uninstall delete: '/usr/local/bin/charm'
+
   zap delete: [
-                '~/Library/Preferences/com.jetbrains.PyCharm.plist',
-                "~/Library/Preferences/PyCharm#{version}",
-                "~/Library/Application Support/PyCharm#{version}",
-                "~/Library/Caches/PyCharm#{version}",
-                "~/Library/Logs/PyCharm#{version}",
+                "~/.PyCharm#{version.major_minor}",
+                # TODO: expand/glob for '~/Library/Preferences/jetbrains.pycharm.*.plist',
+                "~/Library/Preferences/PyCharm#{version.major_minor}",
+                "~/Library/Application Support/PyCharm#{version.major_minor}",
+                "~/Library/Caches/PyCharm#{version.major_minor}",
+                "~/Library/Logs/PyCharm#{version.major_minor}",
               ]
 end
