64758d240213cde154f1fb19ac033e128cc311e4
Zhiming Wang <zmwangx@gmail.com>
2016-03-30 10:55:21 -0700

    Add Safari Technology Preview latest
    https://developer.apple.com/safari/technology-preview/
    https://webkit.org/blog/6017/introducing-safari-technology-preview/

---
+++ Casks/safari-technology-preview.rb
@@ -0,0 +1,25 @@
+cask 'safari-technology-preview' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://secure-appldnld.apple.com/STP/SafariTechnologyPreview.dmg'
+  name 'Safari Technology Preview'
+  homepage 'https://developer.apple.com/safari/technology-preview/'
+  license :gratis
+
+  pkg 'Safari Technology Preview for OS X El Capitan.pkg'
+
+  uninstall delete: '/Applications/Safari Technology Preview.app'
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.apple.safaritechnologypreview.sfl',
+                '~/Library/Caches/com.apple.SafariTechnologyPreview',
+                '~/Library/Preferences/com.apple.SafariTechnologyPreview.plist',
+                '~/Library/SafariTechnologyPreview',
+                '~/Library/Saved Application State/com.apple.SafariTechnologyPreview.savedState',
+                '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview-com.apple.Safari.UserRequests.plist',
+                '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview-com.apple.Safari.WebFeedSubscriptions.plist',
+                '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview.plist',
+                '~/Library/WebKit/com.apple.SafariTechnologyPreview',
+              ]
+end
