e153f8248694a8561e643248b968a2aa457fe2cc
Afriza N. Arief <afriza.na@gmail.com>
2016-03-30 11:50:35 +0800

    Update TortoiseHg to version 3.7.2
    Update TortoiseHg to version 3.7.2

--- Casks/tortoisehg.rb
+++ Casks/tortoisehg.rb
@@ -1,8 +1,8 @@
 cask 'tortoisehg' do
-  version '3.2.4'
-  sha256 'c529539e20f4ff2f6d181a5c7f64ad5dcd9346605011ab4c3725c7cedf6b5a8c'
+  version '3.7.2'
+  sha256 'e429dadbc8d2f0c784795ce9f8df88cdd0d66b26a139b4283f5b4524aa926108'
 
-  url "https://bitbucket.org/tortoisehg/files/downloads/TortoiseHg-#{version}-mac-x64.zip"
+  url "https://bitbucket.org/tortoisehg/files/downloads/TortoiseHg-#{version}-mac-x64.dmg"
   name 'TortoiseHg'
   homepage 'http://tortoisehg.bitbucket.org/'
   license :gpl
