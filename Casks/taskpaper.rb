c39fbad582b9e93b6edaee65b4e5e05ccc272464
forkbomber <forkbomber@users.noreply.github.com>
2016-03-30 04:07:51 +0000

    taskpaper.rb: fix download url

--- Casks/taskpaper.rb
+++ Casks/taskpaper.rb
@@ -3,7 +3,7 @@ cask 'taskpaper' do
   sha256 '28c4a9b66f94d618c08e406b943fdcaf5fb1a851d1d79f6b5c72d1dc091d5b95'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "https://taskpaper.s3.amazonaws.com/TaskPaper-#{version}.dmg"
+  url "https://taskpaper.s3.amazonaws.com/assets/app/TaskPaper-#{version}.dmg"
   appcast 'https://taskpaper.s3.amazonaws.com/TaskPaper.rss',
           checkpoint: 'b6c1c82c5dd0f12f207e4634ba6d59cfcd169e3750e8964367f272782dc13272'
   name 'TaskPaper'
