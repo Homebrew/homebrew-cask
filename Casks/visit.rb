71cdf2e7fc46730e85d2e0dc365106fb0ade9f45
Geoffrey Oxberry <oxberry1@llnl.gov>
2016-03-29 17:44:19 -0700

    Upgrade VisIt to version 2.10.1

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -1,6 +1,6 @@
 cask 'visit' do
-  version '2.10.0'
-  sha256 'afc83987a37ab1e330b81f21e123de1f98809057717f25c305331440a940e985'
+  version '2.10.1'
+  sha256 '29304fabd934da5f6445962c3ee32de725d2c0e6ef693525c4d97cdfa2d9c4a7'
 
   # nersc.gov is the official download host per the vendor homepage
   url "https://portal.nersc.gov/project/visit/releases/#{version}/VisIt-#{version}.dmg"
