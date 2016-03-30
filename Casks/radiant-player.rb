450e088a5addc87b747077576a20bc10f3b3074d
Tom Janson <tom.janson@rwth-aachen.de>
2016-03-30 19:01:56 +0200

    updated radiant-player (1.7.5)

--- Casks/radiant-player.rb
+++ Casks/radiant-player.rb
@@ -1,11 +1,11 @@
 cask 'radiant-player' do
-  version '1.7.4'
-  sha256 '26614f4d040b97d1d0e45ac0431f1ae19ba1ea90b850b622521f1e0191e34166'
+  version '1.7.5'
+  sha256 'ccb072e2af93035587cdd8335d8ed51db2855059466ae96fc337a4e1613c56c9'
 
   # github.com/radiant-player/radiant-player-mac was verified as official when first introduced to the cask
   url "https://github.com/radiant-player/radiant-player-mac/releases/download/v#{version}/radiant-player-v#{version}.zip"
   appcast 'https://github.com/radiant-player/radiant-player-mac/releases.atom',
-          checkpoint: 'cb1555401a1ea3acf15841dec4c156f9d0ff7ed4ba507c03c113418a28c40107'
+          checkpoint: '7eb559212fdd0d3785cd1d3742093e46158e0fc25cefea1821d5a93f4e1cc156'
   name 'Radiant Player'
   homepage 'https://radiant-player.github.io/radiant-player-mac/'
   license :mit
