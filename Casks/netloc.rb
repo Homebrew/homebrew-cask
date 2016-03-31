adae2061c75ec84e7eaecf64ae4340f53d8a81d4
Yusuke SHIBATA <shibata@fata.io>
2016-04-01 02:38:04 +0900

    Update netloc url

--- Casks/netloc.rb
+++ Casks/netloc.rb
@@ -2,9 +2,9 @@ cask 'netloc' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/panefordope/netloc/blob/master/dist/netloc.zip?raw=true'
+  url 'https://github.com/yusukeshibata/netloc/blob/master/dist/netloc.zip?raw=true'
   name 'netloc'
-  homepage 'https://github.com/panefordope/netloc/'
+  homepage 'https://github.com/yusukeshibata/netloc/'
   license :mit
 
   app 'netloc.app'
