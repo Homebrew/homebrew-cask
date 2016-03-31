4c032f55fe599f1092f35e0e166706e6258bceec
Victor Popkov <victor@popkov.me>
2016-03-31 22:59:37 +0300

    updated opera (36.0.2130.46)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '36.0.2130.32'
-  sha256 '6088cfaea4adcfef204da8bde8747c738ee715f5766ebb5168bd07c2227ee2a4'
+  version '36.0.2130.46'
+  sha256 '16babd1ccad88d8598b3615645b6711eabe208afe8b535df10eeca7c09166659'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
