aa28d006e7c6acd56438e752b110384453368aca
Vítor Galvão <info@vitorgalvao.com>
2016-04-01 11:56:34 -0400

    webex-nbr-player.rb: fixed quotes

--- Casks/webex-nbr-player.rb
+++ Casks/webex-nbr-player.rb
@@ -2,7 +2,7 @@ cask 'webex-nbr-player' do
   version 'T29L10NSP13EP71-10003'
   sha256 '242ad4c65afee9fc2d7f5608fe3f959e71eb20b22ac33af4ae125b9c670642f0'
 
-  url 'https://welcome.webex.com/client/WBXclient-#{version}/mac/intel/webexnbrplayer_intel.dmg'
+  url "https://welcome.webex.com/client/WBXclient-#{version}/mac/intel/webexnbrplayer_intel.dmg"
   name 'Webex Network Recording player'
   homepage 'https://www.webex.com/play-webex-recording.html'
   license :gratis
