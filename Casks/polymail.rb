54970ae9bbaadcb7688661190b0c785c77853522
Victor Popkov <victor@popkov.me>
2016-04-01 23:18:46 +0300

    updated polymail (0.77)

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,10 +1,10 @@
 cask 'polymail' do
-  version '0.76'
-  sha256 'd02d70700c4bc56d983505c1f69b32f0329cfe0a4c357d37170ffb912331af8a'
+  version '0.77'
+  sha256 '19727725b78137ddbc8d44746baaf22e98d0153e5feb96862ec35003b634af0c'
 
   url "https://sparkle-updater.polymail.io/osx/builds/Polymail-v#{version.no_dots}.zip"
   appcast 'https://sparkle-updater.polymail.io/cast.xml',
-          checkpoint: '35ea7eeaa1ce7a2f8e109b2cfbf53f83ceb28277625d7cfb2c1c191269453b81'
+          checkpoint: 'f88614b715199b7a7dc9329e7ee0226b4a4fa4fc225368f0636796c7862cd41a'
   name 'Polymail'
   homepage 'https://polymail.io/'
   license :closed
