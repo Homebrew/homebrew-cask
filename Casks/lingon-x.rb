8bb9fd07b90d5f1896f7c9934f92d529b67ed5bb
adidalal <adidalal@users.noreply.github.com>
2016-04-01 11:41:39 -0400

    updated lingon-x (4.0)

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,10 +1,10 @@
 cask 'lingon-x' do
-  version '2.3.5'
-  sha256 '7dc1e230b36c4309397f5fad5270050dea4295ea4779b7b40e528d6185e1f09f'
+  version '4.0'
+  sha256 'f10e3d765c95daa1eeca7144d59b81b356bd8da06f43406276b1c5f9cd62afb4'
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
   appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}-appcast.xml",
-          checkpoint: '3289b42891c6366693b3f74d6abb895de7c296d4c398d7e3e47b35ae85ed62ba'
+          checkpoint: '48f22e57cfe79d4173dd2d36a7d787852be0fd6d18bc766a6545cff17e22c032'
   name 'Lingon X'
   homepage 'https://www.peterborgapps.com/lingon/'
   license :commercial
