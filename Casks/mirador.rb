41dd606749cb0276cfeb13e386186ead4d45581b
Victor Popkov <victor@popkov.me>
2016-04-01 23:16:28 +0300

    updated mirador (1.4)

--- Casks/mirador.rb
+++ Casks/mirador.rb
@@ -1,11 +1,11 @@
 cask 'mirador' do
-  version '1.3'
-  sha256 'bc9e8a6f3de948a8674de3d35b9f9074be15421f24f4bcce5591899dc828a9c2'
+  version '1.4'
+  sha256 '4131c4114fba598e86b803d20ba1ef9732b7b92897b6c4b753ae62e892d9bdf1'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/mirador/mirador/releases/download/#{version}/mirador-macosx-#{version}.zip"
   appcast 'https://github.com/mirador/mirador/releases.atom',
-          checkpoint: 'ca493a1c606996ad3328e3b8ad40b5df85a935706fb79522d64ddf2965c0bfea'
+          checkpoint: 'f170ed80b82bb18d60baca9c3310a9039ce8b88ee1c023f02a6a676bd0f6cdf6'
   name 'Mirador'
   homepage 'http://fathom.info/mirador/'
   license :gpl
