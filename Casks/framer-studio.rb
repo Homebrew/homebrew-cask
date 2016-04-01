c51b6466efcbaef246e44bf757fccde6d5952f0c
Victor Popkov <victor@popkov.me>
2016-04-01 22:40:30 +0300

    updated framer-studio (59)

--- Casks/framer-studio.rb
+++ Casks/framer-studio.rb
@@ -1,11 +1,11 @@
 cask 'framer-studio' do
-  version '58'
-  sha256 'd6eda94e3815764844e5447b773a7d2f43e7f3ded966f0a091fa5e9680c8bcb2'
+  version '59'
+  sha256 '4de9bde1afe58ed9ec31f2c7a7d0c368ad1772dcfcdc63b1dfb7f01e0699bc51'
 
   # devmate.com/com.motif.framer was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.motif.framer/FramerStudio.zip'
   appcast 'https://updates.devmate.com/com.motif.framer.xml',
-          checkpoint: 'f70404104dfdf7fd4646d9bfd8db4d596081bb5c8faa5e05bf11af6577450e11'
+          checkpoint: 'bf25e7d0f0008356aa12806ebd8330dcf0fc20af2064bcd59dd52ac3d05ef4e3'
   name 'Framer Studio'
   homepage 'http://framerjs.com/'
   license :commercial
