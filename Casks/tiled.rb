f676688ea0a1c2a2dc8b2ee0e36affa50d99e149
Victor Popkov <victor@popkov.me>
2016-04-02 00:32:45 +0300

    updated tiled (0.16.0)

--- Casks/tiled.rb
+++ Casks/tiled.rb
@@ -1,11 +1,11 @@
 cask 'tiled' do
-  version '0.15.2'
-  sha256 '70089f438f80d56bf3820478cca36ccb2b4adacace0240125a4d0ecf165e6d88'
+  version '0.16.0'
+  sha256 '6d2ea8026ea20f5d449670ab70194f649135b5452fd6ebbb88ea5ab25f5d14b3'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/bjorn/tiled/releases/download/v#{version}/tiled-#{version}.dmg"
   appcast 'https://github.com/bjorn/tiled/releases.atom',
-          checkpoint: '18cff847ce24d6dcb02d42be46f419a600d78a3382bd8d0bbd3c1ca05d85b2c0'
+          checkpoint: '355ff763b7fce255b981fa745bcf58e72c3de809d37f955ad8a4f37a59be488e'
   name 'Tiled'
   homepage 'http://www.mapeditor.org/'
   license :gpl
