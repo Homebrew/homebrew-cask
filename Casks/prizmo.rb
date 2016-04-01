f92fbb20a1645042bbaf54c111796762886c5309
Victor Popkov <victor@popkov.me>
2016-04-01 23:20:45 +0300

    updated prizmo (3.1.9)

--- Casks/prizmo.rb
+++ Casks/prizmo.rb
@@ -1,10 +1,10 @@
 cask 'prizmo' do
-  version '3.1.8'
-  sha256 '2a57ec332d5094248aa6ce5e0a6c3ea50d22acd05afb765f0ece31a04012409d'
+  version '3.1.9'
+  sha256 '4dc9d09655121ca6b5b6f08273524804bb9ddda345bd3fafb5f4e881b3bc43e1'
 
   url "http://www.creaceed.com/downloads/prizmo#{version.to_i}_#{version}.zip"
   appcast 'http://www.creaceed.com/appcasts/prizmo3.xml',
-          checkpoint: '16e7f3d4e35494389f7582fd8ca8ed8b196aeb88e4a2bfd4a4dab7899d4ea510'
+          checkpoint: '67f7f81fe7504cd71f51d9707e6f7fb76b054a21560db94ebe401d3c6205e304'
   name 'Prizmo'
   homepage 'http://www.creaceed.com/prizmo'
   license :freemium
