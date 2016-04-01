39cf4698236e21b3afff2bab79ec2c3fd9e0e98a
Victor Popkov <victor@popkov.me>
2016-04-02 00:32:11 +0300

    updated textbar (1.9.6)

--- Casks/textbar.rb
+++ Casks/textbar.rb
@@ -1,10 +1,10 @@
 cask 'textbar' do
-  version '1.7.95'
-  sha256 '7fe84b1aa092bf37c51165c247b73e2e94e0ba0c6464f9d522666ff37dc35c3b'
+  version '1.9.6'
+  sha256 '014d37b9d4f7a1d9920776f998cb29b7642d397e6f5a2cb469ce5aea2776deb2'
 
   url "http://www.richsomerfield.com/apps/textbar/TextBar.app-#{version}.zip"
   appcast 'http://www.richsomerfield.com/apps/textbar/sparkle_textbar.xml',
-          checkpoint: '639930c95ad623671b7f3eb3f7a330eaa2c2cf456c31109ecdac03685881833b'
+          checkpoint: 'e45d9a5e9c6b0a2924821279ce93169b7d598d778a2e2fede9f630cc56b8b84a'
   name 'TextBar'
   homepage 'http://www.richsomerfield.com/apps/'
   license :closed
