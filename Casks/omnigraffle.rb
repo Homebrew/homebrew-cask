51f43f08a9051e43be992d3a9c95f5983f444a0e
Vítor Galvão <info@vitorgalvao.com>
2016-04-01 12:14:47 -0400

    updated omnigraffle (6.5.2)

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -8,8 +8,8 @@ cask 'omnigraffle' do
     sha256 'a2eff19909d1ba38a4f01b2beecbde2f31f4af43d30e06d2c6921ae8880f85bc'
     url "http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.8/OmniGraffle-#{version}.dmg"
   else
-    version '6.5.1'
-    sha256 '5591e52b21f97507a4521adaec74655e14266d10bb21974f0e0b3a6a1e3945e7'
+    version '6.5.2'
+    sha256 '0696232cd0ad3d8183ae2a452ca00b399994a89011afa7f63d229562292376b5'
     url "http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.10/OmniGraffle-#{version}.dmg"
   end
 
