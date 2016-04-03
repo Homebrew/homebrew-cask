81950beb3d525eca5b9496e3cf69ed39e9b295a0
Vítor Galvão <info@vitorgalvao.com>
2016-04-03 17:13:29 -0400

    geektool.rb: fix indentation

--- Casks/geektool.rb
+++ Casks/geektool.rb
@@ -5,7 +5,7 @@ cask 'geektool' do
   # dl.devmate.com/org.tynsoe.GeekTool was verified as official when first introduced to the cask
   url "https://dl.devmate.com/org.tynsoe.GeekTool/#{version.before_comma}/#{version.after_comma}/GeekTool-#{version.before_comma}.zip"
   appcast 'https://updates.devmate.com/org.tynsoe.GeekTool.xml',
-	  checkpoint: '6834d4c329b703053afe025d07b19c06d916081b0df5509e98bec6d9a16ed343'
+          checkpoint: '6834d4c329b703053afe025d07b19c06d916081b0df5509e98bec6d9a16ed343'
   name 'GeekTool'
   homepage 'http://projects.tynsoe.org/en/geektool/'
   license :mit
