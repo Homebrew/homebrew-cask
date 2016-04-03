5bc02785e3773e2bc7b6c5b0c0d4b5bb46499ae8
Matt Newman <mdjnewman@gmail.com>
2016-04-03 11:26:36 +1000

    Use new transmission-remote-gui Sourceforge URL

--- Casks/transmission-remote-gui.rb
+++ Casks/transmission-remote-gui.rb
@@ -2,7 +2,7 @@ cask 'transmission-remote-gui' do
   version '5.0.1'
   sha256 'b961aeb244b2519563837745f3475d21379e3da32bae2b3cbb20ca91d1a90d75'
 
-  url "http://downloads.sourceforge.net/sourceforge/transgui/transgui-#{version}.dmg"
+  url "http://downloads.sourceforge.net/project/transgui/#{version}/transgui-#{version}.dmg"
   name 'Transmission Remote GUI'
   homepage 'http://sourceforge.net/projects/transgui/'
   license :oss
