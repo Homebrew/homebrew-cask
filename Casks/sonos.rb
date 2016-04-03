a78360337fcf4758b481ea98c0ff6e04c8ab80e5
Ryan Welton <rwelton24@gmail.com>
2016-04-03 14:17:21 -0400

    Add file hash and version for Sonos

--- Casks/sonos.rb
+++ Casks/sonos.rb
@@ -1,7 +1,6 @@
 cask 'sonos' do
-  version :latest
-  sha256 :no_check
-
+  version '6.2.2'
+  sha256 '5a505fb59645e82a9adff21b5e6231edcb5a5763546a2682b9c49bffe1ed92c3'
   url 'http://www.sonos.com/redir/controller_software_mac'
   name 'Sonos'
   homepage 'http://www.sonos.com/'
