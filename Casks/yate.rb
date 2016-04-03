0ead48c5b17b76ea05566b6f5cd609b33e26b6f4
Jussi Tuominen <wickedsp1d3r@COLDwithin.local>
2016-04-03 17:35:58 +0300

    updated yate (3.11.0.2)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,10 +1,10 @@
 cask 'yate' do
-  version '3.11.0.1'
-  sha256 'f48c3a8bbfceb1f50026f04336146060a5f424632655b21010962bf705ff1d9a'
+  version '3.11.0.2'
+  sha256 '30fbb8e175ef5c2b0d70338229ac0999e6e05d7d911d2bd0d7ce8ae72d7e0291'
 
   url 'https://2manyrobots.com/Builds/Yate/Yate.dmg'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '720585f616f141965c7d0ae605b891ddfa255abd0bf9dd20bacddb22579d9812'
+          checkpoint: 'ae0423024ab5e89d7dfcaab0078d3351e45e6e63a332e34a99c04b201e48881f'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
   license :commercial
