ba0fe110b7c0d147e27ea10713cfe06684661468
Dan Hansen <dan.hansen@readytalk.com>
2016-04-04 09:03:00 -0600

    Add verified url comment

--- Casks/readytalk-desktop.rb
+++ Casks/readytalk-desktop.rb
@@ -2,6 +2,7 @@ cask 'readytalk-desktop' do
   version :latest
   sha256 :no_check
 
+  # callinfo.com was verified as official when first introduced to the cask
   url 'https://core.callinfo.com/packages/runtime/rtd-readytalk/darwin-x86_64/setup.dmg'
   name 'ReadyTalk Desktop'
   homepage 'https://www.readytalk.com/products-services/integrations/productivity/readytalk-desktop'
