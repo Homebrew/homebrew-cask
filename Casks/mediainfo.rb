4d244d4260f514d278bc0b73305b7d16615b694a
matteocng <matteocng@users.noreply.github.com>
2016-04-04 22:29:08 +0200

    updated mediainfo (0.7.84)

--- Casks/mediainfo.rb
+++ Casks/mediainfo.rb
@@ -1,6 +1,6 @@
 cask 'mediainfo' do
-  version '0.7.82'
-  sha256 'b35594a95adbe11636db9bdc1a2ed122de1a5f6cfcda1591845e477b6e3142f8'
+  version '0.7.84'
+  sha256 '33719ef8301bed5ce7f3d6acd1058363449877ee2e48f4da2dcdee81e6fd7596'
 
   url "https://mediaarea.net/download/binary/mediainfo-gui/#{version}/MediaInfo_GUI_#{version}_Mac.dmg"
   name 'MediaInfo'
