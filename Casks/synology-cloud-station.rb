4f4cc10f70d0a40339e88be6edd6be843173458e
Joe Palmieri <jpalmieri@lumoslabs.com>
2016-04-04 09:36:25 -0700

    updated synology-cloud-station (4.0-4203)

--- Casks/synology-cloud-station.rb
+++ Casks/synology-cloud-station.rb
@@ -1,13 +1,13 @@
 cask 'synology-cloud-station' do
-  version '3.2-3487'
-  sha256 'c2446bb15ce0e113253635a3457643c260f9c92cf9aec5e4d69b5d49c2592631'
+  version '4.0-4203'
+  sha256 '4e4d705e77faec58d04d816d3d6eeb0fd081345490c0fd338184e06243cd22ca'
 
-  url "https://global.download.synology.com/download/Tools/CloudStation/#{version}/Mac/Installer/synology-cloud-station-#{version.sub(%r{.*-}, '')}.dmg"
-  name 'Synology Cloud Station'
+  url "https://global.download.synology.com/download/Tools/CloudStationDrive/#{version}/Mac/Installer/synology-cloud-station-drive-#{version.sub(%r{.*-}, '')}.dmg"
+  name 'Synology Cloud Station Drive'
   homepage 'https://www.synology.com/'
   license :gratis
 
-  pkg "synology-cloud-station-#{version.sub(%r{.*-}, '')}.pkg"
+  pkg "Install\ Cloud\ Station\ Drive.pkg"
 
   uninstall pkgutil:   'com.synology.CloudStation',
             launchctl: 'com.synology.Synology Cloud Station'
