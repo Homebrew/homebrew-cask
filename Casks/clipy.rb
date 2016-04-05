fa01497f88ee56476689145730072e6a324d819f
Victor Popkov <victor@popkov.me>
2016-04-05 23:14:04 +0300

    updated clipy (1.0.9)

--- Casks/clipy.rb
+++ Casks/clipy.rb
@@ -1,10 +1,10 @@
 cask 'clipy' do
-  version '1.0.8'
-  sha256 '26ff09347612c45fa623557d5f8fef1dcbfc72b467a8ba012e1720ab9c8eacef'
+  version '1.0.9'
+  sha256 '088e3f221ae3445913e81bed4ac9cc05dd15274463604e382583cbd4f9b787b2'
 
   url "https://github.com/Clipy/Clipy/releases/download/#{version}/Clipy_#{version}.dmg"
   appcast 'https://clipy-app.com/appcast.xml',
-          checkpoint: '191ea5bf32fcdd8dd8bba1ccdae8bc988619c4658fb31089fc24441093b47ca8'
+          checkpoint: '1c0febceb417ff7abcb857febad0947c665d63c69e65eda24bd035ed7a14b7b7'
   name 'Clipy'
   homepage 'https://clipy-app.com/'
   license :mit
