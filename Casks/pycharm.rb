5364b9a4b1855f164beb59693f2575091c635de6
Mateusz Jagiełło <spam@sigo.pl>
2016-04-05 22:03:38 +0200

    updated pycharm (2016.1.1)

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,6 +1,6 @@
 cask 'pycharm' do
-  version '2016.1'
-  sha256 'a07eab762373b5e119157eb3ffbfc6f8f7f754f80f01cf6249c997694c85cb1e'
+  version '2016.1.1'
+  sha256 '675ab0aeb861d06c11f3e57c8b642e3e6bf19dee5413379fd3fa4dc3b26c067c'
 
   url "https://download.jetbrains.com/python/pycharm-professional-#{version}.dmg"
   name 'PyCharm'
