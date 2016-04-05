9471eeffdf2513b8a4d7b0c51f63436dd526424a
Victor Popkov <victor@popkov.me>
2016-04-05 23:28:59 +0300

    updated coreos (1.2.3)

--- Casks/coreos.rb
+++ Casks/coreos.rb
@@ -1,10 +1,10 @@
 cask 'coreos' do
-  version '1.2.2'
-  sha256 '152426f6cd6c46b298c2560a96a85328441de3628685f9a1fc744b6e0ed2baee'
+  version '1.2.3'
+  sha256 '493c9963bff1f8601fb93e583893dcee653072cc13c8a1688196781e1b303f4e'
 
   url "https://github.com/TheNewNormal/coreos-osx/releases/download/v#{version}/CoreOS_v#{version}.dmg"
   appcast 'https://github.com/TheNewNormal/coreos-osx/releases.atom',
-          checkpoint: 'c4f07738018b304dcd4d138b413564a83c35a0e3f5efa4faa041086985c5d2d0'
+          checkpoint: '6aad70ea992600e746a2e13980decb8726e68998702cc15ad4195708fdec5a04'
   name 'CoreOS VM'
   homepage 'https://github.com/TheNewNormal/coreos-osx'
   license :apache
