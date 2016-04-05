50b71fb1b4eefa3d1f03af0129e158807d6d09a0
Victor Popkov <victor@popkov.me>
2016-04-06 00:16:48 +0300

    updated hachidori (1.1.7.3)

--- Casks/hachidori.rb
+++ Casks/hachidori.rb
@@ -1,11 +1,11 @@
 cask 'hachidori' do
-  version '1.1.7.2'
-  sha256 '65f11c9d94c68e7e46aa89a9923dbddb95c2c7abb0c183dc26aafa5ec6caf827'
+  version '1.1.7.3'
+  sha256 'dd9e9966e5c89392aaffb5a39d47c469918a20637d5d3b778d0de2f1bb1d64ee'
 
   # github.com/chikorita157/hachidori was verified as official when first introduced to the cask
   url "https://github.com/chikorita157/hachidori/releases/download/#{version}/Hachidori-#{version}.zip"
   appcast 'https://github.com/chikorita157/hachidori/releases.atom',
-          checkpoint: 'b77dc40831d44d712e81693e73217f9f6d43accb5ae7552709a3bf4b8be1b9a6'
+          checkpoint: 'b17eb5f0d15a34a5edcfb570a9fd1927ee3ad62e695243cd93a2661dd1e74d42'
   name 'Hachidori'
   homepage 'http://hachidori.ateliershiori.moe'
   license :bsd
