74d36479deb277003711a85c59e7c598e3945c58
Vítor Galvão <info@vitorgalvao.com>
2016-04-05 10:49:25 -0400

    updated nicecast (1.11.7)

--- Casks/nicecast.rb
+++ Casks/nicecast.rb
@@ -1,8 +1,9 @@
 cask 'nicecast' do
   version '1.11.7'
-  sha256 'c840bcb98574fd919eae79acd97f89f0a1eac489313edf940f3efce61cfab908'
+  sha256 'b75fe78837d8edab0cf35f0f7e42418c7b9cb7cf2beaccfc519caca4be942310'
 
-  url 'https://www.rogueamoeba.com/nicecast/download.php'
+  # d2oxtzozd38ts8.cloudfront.net was verified as official when first introduced to the cask
+  url 'https://d2oxtzozd38ts8.cloudfront.net/nicecast/download/Nicecast.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Nicecast&system=10113&platform=osx',
           checkpoint: '4c5793276ba39f8b4e98c00e89e21e209e60fd4e4dd5c4b99f21ae7d01f4df0a'
   name 'Nicecast'
