1fa6a565776165e54eab91a5da8f798004928803
matteocng <matteocng@users.noreply.github.com>
2016-04-05 07:14:02 +0200

    updated google-adwords-editor (11.3.3.0)

--- Casks/google-adwords-editor.rb
+++ Casks/google-adwords-editor.rb
@@ -1,6 +1,6 @@
 cask 'google-adwords-editor' do
-  version '11.1.3'
-  sha256 '3fe702bb8edb10a214d0c2feefe4a0da8a2e7a8555581f6414de9a2807a0440a'
+  version '11.3.3.0'
+  sha256 'ec3725c96d6e1b9f4b8d59ee9d68d0fea22acfc1926f405436ed6175b7957f0b'
 
   url "https://dl.google.com/adwords_editor/#{version}/Google_AdWords_Editor.dmg"
   name 'Google AdWords Editor'
