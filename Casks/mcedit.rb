05094644be88d9e9271c9c8e3b3bdf0b308765a9
Victor Popkov <victor@popkov.me>
2016-04-06 00:25:50 +0300

    updated mcedit (1.5.2.0)

--- Casks/mcedit.rb
+++ Casks/mcedit.rb
@@ -1,11 +1,11 @@
 cask 'mcedit' do
-  version '1.5.1.0'
-  sha256 'cbe893c076415ae269b723aed1d0294e2648a93444309e23d3767fa8c05990c8'
+  version '1.5.2.0'
+  sha256 '93fd86b1cbe59965bfc246cead9de77271a01c2129109734032a2e29e05f6116'
 
   # github.com/Khroki/MCEdit-Unified was verified as official when first introduced to the cask
   url "https://github.com/Khroki/MCEdit-Unified/releases/download/#{version}/MCEdit.v#{version}.OSX.64bit.zip"
   appcast 'https://github.com/Khroki/MCEdit-Unified/releases.atom',
-          checkpoint: '08be06910483a496c714fc1e41805694c1839311d9685cc185ffb7a60e50eff5'
+          checkpoint: '8a77353aef92a298a272281246815a9f9a460a18f482986fb59875564508ad91'
   name 'MCEdit-Unified'
   homepage 'https://khroki.github.io/MCEdit-Unified/'
   license :mit
