349fa0e305eeecc4045a46c8fd2fd82a8672229b
George Gooden <gecgooden@gmail.com>
2016-04-06 13:46:54 +0800

    Added nScope.app v0.6

---
+++ Casks/nscope.rb
@@ -0,0 +1,11 @@
+cask 'nscope' do
+  version '0.6'
+  sha256 'bbee7f4acb848617cadf84d164a3f8c58d426528c56e96a4018d16aeac49409d'
+
+  url 'http://www.nscope.org/prerelease2015/v06/mac/nScope.dmg'
+  name 'nScope'
+  homepage 'http://www.nscope.org/'
+  license :closed
+
+  app 'nScope.app'
+end
