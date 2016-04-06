6b0281c452e21f9fee736418b0a7d8a7c36ddd79
Joshua McKinney <joshka@users.noreply.github.com>
2016-04-06 15:39:37 -0500

    Add Bovada Poker cask

---
+++ Casks/bovada-poker.rb
@@ -0,0 +1,19 @@
+cask 'bovada-poker' do
+  version :latest
+  sha256 :no_check
+
+  # https://cdn3-c.imagestore.lv was verified as official when first introduced to the cask
+  url 'https://cdn3-c.imagestore.lv/static/poker-client-installer-delta/installer/BovadaPoker.pkg'
+  name 'Bovada Poker'
+  homepage 'https://poker.bovada.lv'
+  license :gratis
+
+  auto_updates true
+
+  pkg 'BovadaPoker.pkg'
+
+  uninstall pkgutil: '2110',
+            quit:    'com.BovadaPoker.Lobby'
+
+  zap delete: '~/Library/Application Support/Bovada'
+end
