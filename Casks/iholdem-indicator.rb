d57deedf43b7822760bfa1b1d478cd649c254934
Joshua McKinney <joshka@users.noreply.github.com>
2016-04-06 16:20:35 -0500

    Add iHoldem Indicator cask

---
+++ Casks/iholdem-indicator.rb
@@ -0,0 +1,21 @@
+cask 'iholdem-indicator' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.iholdemindicator.com/download/iHoldemIndicatorInstaller.pkg'
+  name 'iHoldem Indicator'
+  homepage 'http://www.iholdemindicator.com'
+  license :commercial
+
+  pkg 'iHoldemIndicatorInstaller.pkg'
+
+  uninstall launchctl: 'com.iholdemindicator.setenv',
+            quit:      [
+                         'com.ckmn.iHoldemIndicator',
+                         'com.ckmn.IndicatorHelper',
+                       ],
+            pkgutil:   'iHoldemIndicatorInstaller',
+            delete:    '/Applications/iHoldemIndicator.app'
+
+  zap delete: '~/Library/Application Support/iHoldemIndicator'
+end
