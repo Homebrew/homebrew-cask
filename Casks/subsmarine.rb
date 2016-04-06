dd62fd2558dac74369c92b7051fc667b06e0a1f9
matteocng <matteocng@users.noreply.github.com>
2016-04-06 20:24:41 +0200

    updated subsmarine (1.2.3)
    Signed-off-by: matteocng <matteocng@users.noreply.github.com>

--- Casks/subsmarine.rb
+++ Casks/subsmarine.rb
@@ -1,8 +1,8 @@
 cask 'subsmarine' do
-  version '1.2.2'
-  sha256 'b6533f4fa1f3d477a7474a7c5f3baeb8c0c06b8da0d83ca8acddd138cfd1aeb1'
+  version '1.2.3'
+  sha256 '8cdaec741029dec8a45fe55679009fa4a30ede35704b313888388618c331f000'
 
-  url "http://www.cocoawithchurros.com/downloads/SubsMarine.#{version}.zip"
+  url "https://s3-us-west-2.amazonaws.com/cwcbucket/subsmarine/subsmarine.#{version}.zip"
   appcast 'http://www.cocoawithchurros.com/shine/appcast.php?id=7',
           checkpoint: 'a6076937b5f95403d0d867e409b7d833f5f1d56cd0c060be4ea6ae3546a3554a'
   name 'SubsMarine'
