a4e73606c37a86540f10bf9fa6943278a32c1e55
Thomas Willson <thomas.willson@me.com>
2016-04-06 10:36:27 -0700

    updated liclipse (2.5.4)

--- Casks/liclipse.rb
+++ Casks/liclipse.rb
@@ -1,12 +1,12 @@
 cask 'liclipse' do
-  version '2.1.0'
-  sha256 '3b6077e3208d92ac313f685f05fd20aee6705df9461db88a9141899969a7b623'
+  version '2.5.4'
+  sha256 '4056c3282455f84dc6ceab9127df1123539bc63820e054d362e16fd617d297c2'
 
-  # googledrive.com is the official download host per the vendor homepage
+  # googledrive.com/host/0BwwQN8QrgsRpLVlDeHRNemw3S1E was verified as official when first introduced to the cask
   url "https://googledrive.com/host/0BwwQN8QrgsRpLVlDeHRNemw3S1E/LiClipse%20#{version}/liclipse_#{version}_macosx.cocoa.x86_64.dmg"
   name 'LiClipse'
   homepage 'http://www.liclipse.com/'
   license :commercial
 
-  app 'liclipse/LiClipse.app'
+  app 'LiClipse.app'
 end
