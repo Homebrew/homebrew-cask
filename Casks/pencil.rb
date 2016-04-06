3a4a52f6fc3c2612c23c7e274454c9b565b7461f
Aditya Dalal <adidalal@users.noreply.github.com>
2016-04-06 00:30:20 -0400

    add pencil appcast

--- Casks/pencil.rb
+++ Casks/pencil.rb
@@ -3,6 +3,8 @@ cask 'pencil' do
   sha256 'dfddc7a5b7b781eed0cd8dc998656421e2d86eddb1068fe227cc85e04c7ab520'
 
   url "https://github.com/prikhi/pencil/releases/download/v#{version}/Pencil-#{version}-mac-osx.zip"
+  appcast 'https://github.com/prikhi/pencil/releases.atom',
+          checkpoint: '78c1eac95e9b45745b6a5e2590e8023626ca9928b8f650f0362c62ab18da8a00'
   name 'Pencil'
   homepage 'https://github.com/prikhi/pencil'
   license :gpl
