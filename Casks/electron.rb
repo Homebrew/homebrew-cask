25a574f8975879b14bdb2c1335ca7809f06b0cfc
Jerzerak <jerzerak@gmail.com>
2016-04-07 16:11:43 -0400

    Update electron (0.37.5) (#20368)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,10 +1,10 @@
 cask 'electron' do
-  version '0.37.4'
-  sha256 '492405a25b8471c71b8994b68e96a8a47e8b3421688affdb24de6449c91f316e'
+  version '0.37.5'
+  sha256 '410ecfa4319adc63fc980ffa06c39dc5e29065bc313744ff3e1ec40caedaeb17'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
-          checkpoint: '9b2b977016fd0b739b70b716bab8bbde5b56b6f5a925e1b99570f9a992eb91b1'
+          checkpoint: '4b692564c630075833d2086222cd3123baf1732b0b2bde9895f70677b60df5f3'
   name 'Electron'
   homepage 'http://electron.atom.io/'
   license :mit
