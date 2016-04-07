4eeb4332a45ae66c36a27e12ed230ca1b7c1aff3
Cameron J Roe <cameronjroe@gmail.com>
2016-04-07 17:02:05 -0400

    begin PR for adding Plug for OSX 10.9.5

--- Casks/plug.rb
+++ Casks/plug.rb
@@ -1,10 +1,15 @@
 cask 'plug' do
-  version '2.0.9'
-  sha256 'bead86a9880ae8eb63296b48d6aef30d80d6057a11ca8491dc85a27b296f5e36'
+  if MacOS.release <= :lion
+    version '?'
+    sha256 '?'
+  else
+    version '2.0.9'
+    sha256 'bead86a9880ae8eb63296b48d6aef30d80d6057a11ca8491dc85a27b296f5e36'
+    appcast 'https://www.plugformac.com/updates/plug2/sparklecast.xml',
+	  checkpoint: '7092bd0eb0a14a478018d36a44addfa4a8dfc871e214f79d66b6a5e1ce216815'
+  end
 
   url 'https://www.plugformac.com/updates/plug2/Plug-latest.dmg'
-  appcast 'https://www.plugformac.com/updates/plug2/sparklecast.xml',
-          checkpoint: '7092bd0eb0a14a478018d36a44addfa4a8dfc871e214f79d66b6a5e1ce216815'
   name 'Plug'
   homepage 'https://www.plugformac.com/'
   license :gratis
