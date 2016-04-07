c5f070ac5b14cd574ffe0433a69377af0781c7ec
Scott Humphries <git@sscotth.io>
2016-04-07 15:29:11 -0500

    Readd Epic Browser

---
+++ Casks/epic.rb
@@ -0,0 +1,14 @@
+cask 'epic' do
+  version '2462'
+  sha256 'fc8fadf78816eaad050d514b3fc9a5c524b7fe5bf5a4b779e7b373ba2c240d1f'
+
+  # kxcdn.com is the official download host per the vendor homepage
+  url "https://macepic-cbe.kxcdn.com/#{version}/sign/Epic.dmg"
+  appcast 'https://updates.epicbrowser.com/mac_updates/appcast.xml',
+          checkpoint: 'aac3853720066e62a3398b67df8e2250a9d0163459f1513918899bad06b566d4'
+  name 'Epic Privacy Browser'
+  homepage 'https://www.epicbrowser.com/'
+  license :gratis
+
+  app 'Epic.app'
+end
