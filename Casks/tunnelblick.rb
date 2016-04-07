0480e7044724200f751d36e23548faaa5ee0cddc
nowherecat <dennis@rodewyk.net>
2016-04-07 15:18:14 +0200

    updated tunnelblick (3.6.1_build_4543.4551)

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -3,8 +3,8 @@ cask 'tunnelblick' do
     version '3.5.8_build_4270.4530'
     sha256 'd634a9a5fd06f95c7325987197fb7890119bf0a40a3958284814958a14c20ce3'
   else
-    version '3.6.0a_build_4543.4546'
-    sha256 '0850dffd539cc60097d29fdb8a8e6b1cf83eeec7ea8bc7c4a821df69fe817441'
+    version '3.6.1_build_4543.4551'
+    sha256 '6a1ae217e58a91fa6ffcb165758a65043e6fddbae875a2262f8f71d11dfd1b85'
   end
 
   url "https://www.tunnelblick.net/release/Tunnelblick_#{version}.dmg"
