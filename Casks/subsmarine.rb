7af95594b78475de034980fa9ae441b704cd7726
matteocng <matteocng@users.noreply.github.com>
2016-04-07 06:00:26 +0200

    add vendor comment and update appcast checkpoint

--- Casks/subsmarine.rb
+++ Casks/subsmarine.rb
@@ -2,9 +2,10 @@ cask 'subsmarine' do
   version '1.2.3'
   sha256 '8cdaec741029dec8a45fe55679009fa4a30ede35704b313888388618c331f000'
 
+  # amazonaws.com/cwcbucket/subsmarine was verified as official when first introduced to the cask
   url "https://s3-us-west-2.amazonaws.com/cwcbucket/subsmarine/subsmarine.#{version}.zip"
-  appcast 'http://www.cocoawithchurros.com/shine/appcast.php?id=7',
-          checkpoint: 'a6076937b5f95403d0d867e409b7d833f5f1d56cd0c060be4ea6ae3546a3554a'
+  appcast 'https://www.cocoawithchurros.com/shine/appcast.php?id=7',
+          checkpoint: '3f99aeebb0ac8611031f46340560ecf733977801f85d3d663ad2e812b3846701'
   name 'SubsMarine'
   homepage 'http://www.cocoawithchurros.com/subsmarine.php'
   license :commercial
