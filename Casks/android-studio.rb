613911d8884973a839b6e01ecf536e889c5ad0e1
Cuong Tran <ctran@users.noreply.github.com>
2016-04-07 11:28:37 -0700

    Update Android Studio 2.0.0
    The download url has change slightly.

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,9 +1,9 @@
 cask 'android-studio' do
-  version '1.5.1.0_2456560'
-  sha256 '2fe2ab35d9be1780269f6ade2cf097c5b5dc494ef44fbdd6ce84008609576430'
+  version '2.0.0.20_143.2739321'
+  sha256 'b5311fdc2b16e8244ab96c3bc4c4ef166f501be8348c26e7d9dce34c24a2ae42'
 
   # google.com/dl/android/studio was verified as official when first introduced to the cask
-  url "https://dl.google.com/dl/android/studio/ide-zips/#{version.sub(%r{_.*}, '')}/android-studio-ide-141.#{version.sub(%r{.*_}, '')}-mac.zip"
+  url "https://dl.google.com/dl/android/studio/install/#{version.sub(%r{_.*}, '')}/android-studio-ide-#{version.sub(%r{.*_}, '')}-mac.dmg"
   name 'Android Studio'
   homepage 'https://developer.android.com/sdk/'
   license :apache
