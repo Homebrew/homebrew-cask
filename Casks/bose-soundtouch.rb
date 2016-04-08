7d5dd296a8e8c7a3c4867ee1c2bc8fd6dbbebc06
Christian Inzinger <christian@inz.io>
2016-04-08 09:37:38 +0200

    updated bose-soundtouch (11.0.12.13555) (#20379)

--- Casks/bose-soundtouch.rb
+++ Casks/bose-soundtouch.rb
@@ -1,6 +1,6 @@
 cask 'bose-soundtouch' do
-  version '10.0.13.12393'
-  sha256 'b2bfaee92c30bada8d3e7c28cd829996415b0b0a1560536c8b1caf56b4141f5e'
+  version '11.0.12.13555'
+  sha256 '58357776167de6c7c603bde71ce157dedb68392f5fb03a07cef921f4f4bc8f35'
 
   url "https://worldwide.bose.com/downloads/assets/updates/soundtouch_app-m/SoundTouch-#{version}-osx-10.9-installer.app.dmg"
   name 'Bose Soundtouch Controller App'
