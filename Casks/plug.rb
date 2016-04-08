80c36c90b2842118512c6365f97c6e3f496d5e2c
Cameron J Roe <cameronjroe@gmail.com>
2016-04-08 16:31:37 -0400

    Plug for OSX 10.9.5

--- Casks/plug.rb
+++ Casks/plug.rb
@@ -1,8 +1,8 @@
 cask 'plug' do
-  if MacOS.release <= :lion
+  if MacOS.release <= :mavericks
     version '0.10.5'
     sha256 'd8eed07bd1f84d6f1daa7a0699617f2e21c01df2e68924945bcb4889d1251d01'
-    url 'http://plugformac.com/files/Plug-#{version}.dmg'
+    url 'http://plugformac.com/files/Plug-0.10.5.dmg'
   else
     version :latest
     sha256 :no_check
