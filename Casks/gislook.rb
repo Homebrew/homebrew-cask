bd4959172ca6aa93ff9be04c8dbd4a135f3cfa1a
Jeffrey Munowitch <jmunowitch@gmail.com>
2016-04-08 10:56:43 -0700

    Create gislook.rb

---
+++ Casks/gislook.rb
@@ -0,0 +1,14 @@
+cask 'gislook' do
+  version :1.0.2
+  sha256 :no_check
+
+  url 'http://cartography.oregonstate.edu/gislook/GISLook_GISMeta.pkg'
+  name 'GISLook'
+  homepage 'http://cartography.oregonstate.edu/GISLook.html'
+  license :unknown
+
+  pkg 'GISLook_GISMeta.pkg'
+
+  uninstall delete: '/Library/QuickLook/GISLook.qlgenerator',
+            delete: '/Library/Spotlight/GISMeta.mdimporter'
+end
