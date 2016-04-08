7c545122a1c95b1b2e3d2b030438f0931cc90dca
Chris Metcalf <chris@chrismetcalf.net>
2016-04-07 22:39:30 -0700

    Add OCRKit v15.11.24

---
+++ Casks/ocrkit.rb
@@ -0,0 +1,11 @@
+cask 'ocrkit' do
+  version '15.11.24'
+  sha256 '39502f49af57eea167232cdb7cc71b9f2c102aea611dd11777ba95166368bb7a'
+
+  url 'http://dl.exactcode.de/tmp/62c3a0fc70ed4e5c64527ba781d634bc/OCRKit-15.11.24.dmg'
+  name 'OCRKit'
+  homepage 'http://ocrkit.com/'
+  license :freemium
+
+  app 'OCRKit.app'
+end
