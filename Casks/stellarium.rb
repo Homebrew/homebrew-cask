131330a27a990a197293a0fcf84b7c9ba4f39029
matteocng <matteocng@users.noreply.github.com>
2016-04-08 02:08:12 +0200

    updated stellarium (0.14.3) (#20376)

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,6 +1,6 @@
 cask 'stellarium' do
-  version '0.14.2'
-  sha256 '6367e9d77743312fe638fa12965afbfb520dbe64228f9d24f952928bcaaa0fe3'
+  version '0.14.3'
+  sha256 'f8d0c80abbde2d9e76a222ff3fc0e0bd63b4cdb661688794fb241d055e274acf'
 
   # sourceforge.net/sourceforge/stellarium was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/sourceforge/stellarium/Stellarium-#{version}.dmg"
