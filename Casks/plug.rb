3d42ffa658e3076fe0207e7c66eaa59da2fc5dee
Vítor Galvão <info@vitorgalvao.com>
2016-04-08 17:03:37 -0400

    plug.rb: readd version

--- Casks/plug.rb
+++ Casks/plug.rb
@@ -2,11 +2,11 @@ cask 'plug' do
   if MacOS.release <= :mavericks
     version '0.10.5'
     sha256 'd8eed07bd1f84d6f1daa7a0699617f2e21c01df2e68924945bcb4889d1251d01'
-    url 'http://plugformac.com/files/Plug-0.10.5.dmg'
+    url "http://plugformac.com/files/Plug-#{version}.dmg"
   else
-    version :latest
-    sha256 :no_check
-    appcast 'https://www.plugformac.com/updates/plug2/sparklecast.xml',
+    version '2.0.9'
+    sha256 'bead86a9880ae8eb63296b48d6aef30d80d6057a11ca8491dc85a27b296f5e36'
+    appcast "https://www.plugformac.com/updates/plug#{version.major}/sparklecast.xml",
             checkpoint: '7092bd0eb0a14a478018d36a44addfa4a8dfc871e214f79d66b6a5e1ce216815'
     url 'https://www.plugformac.com/updates/plug2/Plug-latest.dmg'
   end
