225709f078f24d8db04d0ac06c7a24d140c0c412
Scott Humphries <git@sscotth.io>
2016-04-08 10:34:57 -0500

    Add Stremio v3.5.9

---
+++ Casks/stremio.rb
@@ -0,0 +1,11 @@
+cask 'stremio' do
+  version '3.5.9'
+  sha256 'eae503c5c93b09bc19ce363f28dc055127ab0f76c1faa1a5a9d741de885ae527'
+
+  url 'http://dl.strem.io/Stremio%203.5.9.dmg'
+  name 'Stremio'
+  homepage 'http://www.strem.io/'
+  license :gratis
+
+  app 'Stremio.app'
+end
