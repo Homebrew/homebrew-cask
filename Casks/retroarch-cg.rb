99fe7f224cb0dac9fcf621656fac4734f25fe512
Adam Scott <ascott.ca@gmail.com>
2016-04-08 21:56:08 -0400

    Rename retroarchcg.rb to retroarch-cg.rb
    Upgrade RetroArch CG.app to v1.3.3
    
    * Correct the url pattern
    * Rename the cask
    * Rename the .app for clarity
    
    Add RetroArch.app v1.3.3

---
+++ Casks/retroarch-cg.rb
@@ -0,0 +1,13 @@
+cask 'retroarch-cg' do
+  version '1.3.3'
+  sha256 '59410f57518353076fa17afa8b3c1a8076c5fd25829f52f2ff3712bdb0513abe'
+
+  url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch_CG.dmg"
+  name 'RetroArch CG'
+  homepage 'http://www.libretro.com/'
+  license :gpl
+
+  # Renamed for clarity: 2 versions of RetroArch exists for MacOS X.
+  # There is RetroArch (non-CG) and this version.
+  app 'RetroArch.app', target: 'RetroArch CG.app'
+end
