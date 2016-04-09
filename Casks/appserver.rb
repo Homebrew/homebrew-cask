22b55bd4d1ab7bc5e81b62ffd4e9589309642b77
Vítor Galvão <info@vitorgalvao.com>
2016-04-09 16:54:05 -0400

    updated appserver (1.1.0-100)

--- Casks/appserver.rb
+++ Casks/appserver.rb
@@ -5,7 +5,7 @@ cask 'appserver' do
   # github.com/appserver-io/appserver was verified as official when first introduced to the cask
   url "https://github.com/appserver-io/appserver/releases/download/#{version.sub(%r{-.*}, '')}/appserver-dist_#{version}_x86_64.pkg"
   appcast 'https://github.com/appserver-io/appserver/releases.atom',
-          checkpoint: '342e0d4feda247b4d628f55ef0b2dfb7139ca2221a806e0ac1c1f8aa3cac0d0f'
+          checkpoint: 'a20e2c28531fe78b1ff33bace086a04a05c76d54602736c18185bb6101d67a35'
   name 'appserver.io'
   homepage 'http://www.appserver.io'
   license :oss
