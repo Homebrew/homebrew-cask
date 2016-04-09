cf6ef6b3ea69314ad77573e19262be1c155e712c
Janek Thomaschewski <janek@jbbr.net>
2016-04-09 18:52:20 +0200

    updated mediathekview (11)

--- Casks/mediathekview.rb
+++ Casks/mediathekview.rb
@@ -1,6 +1,6 @@
 cask 'mediathekview' do
-  version '10'
-  sha256 '6118fb5362f08f8204c56f52864ae030c05e4212de96330e8bcc40c5cea5d319'
+  version '11'
+  sha256 'f8a40daeefb37bd835c03674561957414d866dea1ba31e8bb81cd633a0c1a38f'
 
   url "http://downloads.sourceforge.net/project/zdfmediathk/Mediathek/Mediathek%20#{version}/MediathekView_#{version}.dmg"
   name 'MediathekView'
