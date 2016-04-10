1ca2d87c93ebfe0710932985bfd96f01db9d686b
Victor Popkov <victor@popkov.me>
2016-04-10 23:46:51 +0300

    updated m3unify (1.4.4) (#20457)

--- Casks/m3unify.rb
+++ Casks/m3unify.rb
@@ -1,10 +1,10 @@
 cask 'm3unify' do
-  version '1.4.3'
-  sha256 '1ac7fd5535bfa6ea42bd4c38771744c7ea98ac4d7af604c159e5a5bbe04c05da'
+  version '1.4.4'
+  sha256 'f31041a4664b8158b3514571a9221fa8aa55d0af31817bca72f141fb86d5ed84'
 
   url "http://dougscripts.com/itunes/scrx/m3unifyv#{version.delete('.')}.zip"
   appcast 'http://dougscripts.com/itunes/itinfo/m3unify_appcast.xml',
-          checkpoint: '5425159de028abd0cb8fb88dbf2dcdd1173d399ee449438fa9fe6ef93e33318c'
+          checkpoint: '18a6929c547c19989a8ead8ead2bc8da131f45fe1e22639e6b2a8493b4ac30eb'
   name 'M3Unify'
   homepage 'http://dougscripts.com/itunes/itinfo/m3unify.php'
   license :commercial
