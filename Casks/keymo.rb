11675170325dfcf181e187a9b76c9b94fa60a5af
Victor Popkov <victor@popkov.me>
2016-04-10 23:35:48 +0300

    updated keymo (1.2.5)

--- Casks/keymo.rb
+++ Casks/keymo.rb
@@ -1,10 +1,10 @@
 cask 'keymo' do
-  version '1.2.4'
-  sha256 '583a7ac1a42d804883e5d71d352a96de26128ca1c01c9caaa175fb085b5042ec'
+  version '1.2.5'
+  sha256 '7eb685ae806e0bcf251cf74d18890ef07d75dc31bc92323ff66efdf8b56476a0'
 
   url 'https://manytricks.com/download/keymo'
   appcast 'https://manytricks.com/keymo/appcast.xml',
-          checkpoint: '92314cc0908a53935d85123d9dbbb063d3aa8c0311c27c66ccc1692a8fe43701'
+          checkpoint: '382c704be6dbdc0bbeff5b01a661c227a9beabc487363bea91d63cf55ec0f8c7'
   name 'Keymo'
   homepage 'https://manytricks.com/keymo'
   license :commercial
