4b8dada0a4844c586f76d8d6ecb4cb4aac53a3d6
Victor Popkov <victor@popkov.me>
2016-04-10 23:45:07 +0300

    updated recd (1.5.0)

--- Casks/recd.rb
+++ Casks/recd.rb
@@ -1,10 +1,10 @@
 cask 'recd' do
-  version '1.4.1'
-  sha256 '11f149609cf3996d850a33939c4b6de24470f200bd0ac9fd3719b408ec8f49d6'
+  version '1.5.0'
+  sha256 '08785d5bd3e9e37c28f334b5d1a5dc33220e11c6f10e285faaa2bfa29510a457'
 
   url "https://github.com/aeewhite/Recd/releases/download/v#{version}/Rec.d-Mac-v#{version}.dmg"
   appcast 'https://github.com/aeewhite/recd/releases.atom',
-          checkpoint: 'b5c956ce0cd2d888aa2c69b537f05ad47b4601ee57a753a592006e820c3927cb'
+          checkpoint: '89f7f967ccc169b05f8de72236c0c360c8516635ce710aa3d52c0c345486d207'
   name "Rec'd"
   homepage 'https://github.com/aeewhite/Recd'
   license :oss
