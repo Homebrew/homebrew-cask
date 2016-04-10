04c9dd0ff1fbb88ed1e6bd113f735533e48d7388
Clemens Giuliani <clemens@inailuig.it>
2016-04-10 19:14:48 +0200

    updated telegram (0.9.40)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '0.9.32'
-  sha256 '812136ba30ca788c0b85ed6f2a0a232e50731fd64acbb55f17b5693eb49c3584'
+  version '0.9.40'
+  sha256 'a55ac6141b5ff76036d7ed7472ffb822cd09ec3f56f72feafe4dc7dcfed924fb'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
