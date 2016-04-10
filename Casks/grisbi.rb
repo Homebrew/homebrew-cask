5850b989190d68dd90f9902a33d79e806d9de5be
Fräntz Miccoli <frantz.miccoli@fakedomain.com>
2016-04-10 13:41:15 +0200

    Use the grisbi standard source forge download url + mention to the official url

--- Casks/grisbi.rb
+++ Casks/grisbi.rb
@@ -2,7 +2,8 @@ cask 'grisbi' do
   version '1.0.0'
   sha256 'c7a84e8737468e3c1dc08dec5a34330158c767b3ef5a64716d6ec3a0b2c879c2'
 
-  url 'http://downloads.sourceforge.net/project/grisbi/grisbi%20stable/1.0.x/Grisbi-1.0.0.dmg?r=&ts=1460142325&use_mirror=heanet'
+  # http://downloads.sourceforge.net/project/grisbi/grisbi%20stable/1.0.x/Grisbi-1.0.0.dmg was verified as official when first introduced to the cask
+  url 'http://downloads.sourceforge.net/project/grisbi/grisbi%20stable/1.0.x/Grisbi-1.0.0.dmg'
   name 'Grisbi'
   homepage 'http://www.grisbi.org/'
   license :oss
