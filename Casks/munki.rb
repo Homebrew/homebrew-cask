6b21d47c0ae09d85a6d2b599cb6d77b0e65b2533
Victor Popkov <victor@popkov.me>
2016-04-10 23:40:04 +0300

    updated munki (2.6.1.2684)

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -1,11 +1,11 @@
 cask 'munki' do
-  version '2.5.1.2630'
-  sha256 'cf2c50078f1bf02225ac886536cca0448455a0926bc3aa5e53ea0bac2261f7fa'
+  version '2.6.1.2684'
+  sha256 'ad177547c2ebfb8332bfb929bb976b1a61d802d57a4a4d701a606506e48c2b21'
 
   # github.com/munki/munki was verified as official when first introduced to the cask
   url "https://github.com/munki/munki/releases/download/v#{version.sub(%r{^(\d+\.\d+.\d+).*}, '\1')}/munkitools-#{version}.pkg"
   appcast 'https://github.com/munki/munki/releases.atom',
-          checkpoint: 'babf25ad250efca9b32499be3b45401d6ae3a2358614a87dc5278f5f8c4cd244'
+          checkpoint: 'c737daa2eb0ed7ac574771833788ee62a12869aa1f53c516bc07f1ecffb5391c'
   name 'Munki'
   homepage 'https://www.munki.org/munki/'
   license :apache
