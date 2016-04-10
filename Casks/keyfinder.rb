244527d582ba556a0b323cc32a87486fa9df8070
Vítor Galvão <info@vitorgalvao.com>
2016-04-10 09:59:44 -0400

    updated keyfinder (2.1)

--- Casks/keyfinder.rb
+++ Casks/keyfinder.rb
@@ -1,8 +1,8 @@
 cask 'keyfinder' do
-  version :latest
-  sha256 :no_check
+  version '2.1'
+  sha256 '79a8d55ffdabb676c2166f0921dc191154f74881e13fc6b0071955a8a1e32b00'
 
-  url 'http://www.ibrahimshaath.co.uk/keyfinder/bins/KeyFinder-OSX.zip'
+  url "http://www.ibrahimshaath.co.uk/keyfinder/bins/KeyFinder-OSX-#{version.dots_to_hyphens}.zip"
   name 'KeyFinder'
   homepage 'http://www.ibrahimshaath.co.uk/keyfinder/'
   license :gpl
