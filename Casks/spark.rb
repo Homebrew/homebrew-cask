b6e9350d1e996d784ff32354e8cb14cea0562980
Seijiro <seijiro4751@gmail.com>
2016-04-12 00:10:09 +0900

    Update spark.rb

--- Casks/spark.rb
+++ Casks/spark.rb
@@ -2,7 +2,7 @@ cask 'spark' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.shadowlab.org/softwares/Spark/Spark.dmg'
+  url 'http://www.shadowlab.org/softwares/Spark/Spark.zip'
   name 'Spark'
   homepage 'http://www.shadowlab.org/softwares/spark.php'
   license :mit
