aea8db4fbb190c631707fdd64122ac3ca1ad5d4a
Sam Shadwell <smshdwll@gmail.com>
2016-04-11 20:57:08 -0500

    vlc, added shimscript (#20478)
    Fixes #20476

--- Casks/vlc.rb
+++ Casks/vlc.rb
@@ -12,6 +12,16 @@ cask 'vlc' do
       key_id: '65f7c6b4206bd057a7eb73787180713be58d1adc'
 
   app 'VLC.app'
+  shimscript = "#{staged_path}/vlcwrapper"
+  binary shimscript, target: 'vlc'
+
+  preflight do
+    File.open(shimscript, 'w') do |f|
+      f.puts '#! /bin/bash'
+      f.puts "#{staged_path}/VLC.app/Contents/MacOS/VLC \"$@\""
+      FileUtils.chmod '+x', f
+    end
+  end
 
   zap delete: [
                 '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.videolan.vlc.sfl',
