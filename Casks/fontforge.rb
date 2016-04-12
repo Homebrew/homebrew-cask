0959072d7a63463b787e3d032e44d22f4ecd20ad
Dan Poggi <dan.poggi@detroitlabs.com>
2016-04-12 16:56:10 -0400

    fontforge: String#delete('-') -> String#no_hyphens

--- Casks/fontforge.rb
+++ Casks/fontforge.rb
@@ -3,7 +3,7 @@ cask 'fontforge' do
   sha256 '71da3bfad5c20b6746b5e619484b45f208b9e699924de82e8cc9ab709f5e13bb'
 
   # github.com/fontforge/fontforge was verified as official when first introduced to the cask
-  url "https://github.com/fontforge/fontforge/releases/download/#{version.delete('-')}/FontForge-#{version}-Mac.app.dmg.zip"
+  url "https://github.com/fontforge/fontforge/releases/download/#{version.no_hyphens}/FontForge-#{version}-Mac.app.dmg.zip"
   appcast 'https://github.com/fontforge/fontforge/releases.atom',
           checkpoint: 'dcb2587675266e30c2ac1530982c66f170a27a962d77708f8862d5f469ff8102'
   name 'FontForge'
