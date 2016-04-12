210f4bf974849b4cb504485691a232bf8004efe0
Dustin Wheeler <dustin.wheeler@hunter.cuny.edu>
2016-04-12 11:26:56 -0400

    updated launchbar (6.6.2)

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -4,8 +4,8 @@ cask 'launchbar' do
     sha256 '22a1ec0c10de940e5efbcccd18b8b048d95fb7c63213a01c7976a76d6be69a4d'
     url "https://www.obdev.at/downloads/launchbar/legacy/LaunchBar-#{version}.dmg"
   else
-    version '6.6'
-    sha256 '119e006dcbcfb6a4f26076c46905005bb8a00bd8955cf57f254ffd0dddd8fae9'
+    version '6.6.2'
+    sha256 'cd9fe62776b8fdd185e4f176def83dd5f41c9ea2dc0d3fcdc1dc8b860bca21f3'
     url "https://www.obdev.at/downloads/launchbar/LaunchBar-#{version}.dmg"
   end
 
