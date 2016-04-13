91f0a7cf8842d5cc3dcbe12236249116921bf48e
Johan Carlquist <johan@rymdvarel.se>
2016-04-13 15:10:20 +0200

    Don't check hash on bineries without version.

--- Casks/xccello.rb
+++ Casks/xccello.rb
@@ -1,6 +1,6 @@
 cask 'xccello' do
-  version '1.2.3'
-  sha256 'a03595566bb7400acd1404bf3633b2fd9fedfeb0357c2041b5d859392d9f4b7e'
+  version ':latest'
+  sha256 ':no_check'
 
   url 'http://lingsdesigns.com/xccello/files/Xccello.dmg'
   name 'Xccello'
