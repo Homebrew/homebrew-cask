0d9c584c1db2dbc3b723622fc4a1f88c9ae33f4b
Janek Thomaschewski <janek@jbbr.net>
2016-04-13 04:05:05 +0200

    updated nylas-n1 (0.4.25-a22631a)

--- Casks/nylas-n1.rb
+++ Casks/nylas-n1.rb
@@ -1,11 +1,11 @@
 cask 'nylas-n1' do
-  version '0.4.19-d41e72c'
-  sha256 '523e549896679cfe7fb3e6005fb3803212a47ea97a3d24a56a4318764c4b61c6'
+  version '0.4.25-a22631a'
+  sha256 '7a5ed91b7f3a0ed32046d4d620d0611cdc11840817d6dbc05395498070850415'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://edgehill.s3-us-west-2.amazonaws.com/#{version}/darwin/x64/N1.dmg"
   appcast 'https://edgehill.nylas.com/update-check?platform=darwin&arch=64',
-          checkpoint: '34e9fb0ff67ade6e19873479cf88249205e5bdcf9d6751dbb53e7bf63d0d0bb9'
+          checkpoint: 'e8a851a576fccb67277a1459a1fbb265d7d17f5c02b1c530e9ffde2c29f40b35'
   name 'Nylas N1'
   homepage 'https://www.nylas.com/n1'
   license :gpl
