037c0ec5f02d5e541757fd99f05d2702ff24e35d
Tom Janson <tom.janson@rwth-aachen.de>
2016-04-13 02:19:08 +0200

    updated firefox (45.0.2)

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,10 +1,10 @@
 cask 'firefox' do
-  version '45.0.1'
-  sha256 '4ffe9b87c5474555d6ca29e4b44725b2d1f71a873b84e4883119516d8657cac4'
+  version '45.0.2'
+  sha256 'd50dfd5b118dfceef48f3b761ac898782931a1216bf16d0e551c3c79f2347eb2'
 
   url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
   appcast "https://aus5.mozilla.org/update/3/Firefox/#{version}/0/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/Darwin%2015.3.0/default/default/update.xml?force=1",
-          checkpoint: '9aa94b34be9931f59c0d24a5b9088a886f6fae79095a6ba880c673a236f22457'
+          checkpoint: '5fadf45bb7e48de60e94d9886c4fffa9fae70979232b8db88bcb9b55a67f889f'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/'
   license :mpl
