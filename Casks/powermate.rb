dfc5c87095a13afec0fac93b56f404c255712d2a
nvmd <kazenyuk@gmail.com>
2016-04-13 17:23:35 +0300

    Minor name style fix

--- Casks/powermate.rb
+++ Casks/powermate.rb
@@ -3,7 +3,7 @@ cask 'powermate' do
   sha256 '9f59f5d5bdce076cd9d0e86375fc9d935f54fb4fc0b18178c22a0fe16e9027b3'
 
   url "https://support.griffintechnology.com/wp-content/uploads/sites/#{version.after_comma.dots_to_slashes}/PowerMate_v#{version.before_comma}.zip"
-  name 'Griffin Powermate'
+  name 'Griffin PowerMate'
   homepage 'https://support.griffintechnology.com/support/powermate/'
   license :gratis
 
