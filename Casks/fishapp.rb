d4f805026fe2cb4f6b2f526636bd1702cf4cc1e0
Jerzerak <jerzerak@gmail.com>
2016-04-13 13:14:24 -0400

    Update fishapp (2.2.0)

--- Casks/fishapp.rb
+++ Casks/fishapp.rb
@@ -1,6 +1,6 @@
 cask 'fishapp' do
-  version '2.1.1'
-  sha256 '92af7a82e6f46b35867ca160e42ca55a65eb86404bff47b8db489952dab488fc'
+  version '2.2.0'
+  sha256 'e07d30687eff3a66f17384e3d870f42e4eea722f24f8e742c69b2d5f4f3ce61d'
 
   url "http://fishshell.com/files/#{version}/fish.app.zip"
   name 'Fish App'
