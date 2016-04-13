b84a0581de91df1cbdd9d7252d66f4dd4d9dd23e
Johan Carlquist <johan@rymdvarel.se>
2016-04-13 09:29:18 +0200

    Trello X has been renamed to Xccello.

---
+++ Casks/xccello.rb
@@ -0,0 +1,11 @@
+cask 'xccello' do
+  version '1.2.3'
+  sha256 'a03595566bb7400acd1404bf3633b2fd9fedfeb0357c2041b5d859392d9f4b7e'
+
+  url 'http://lingsdesigns.com/xccello/files/Xccello.dmg'
+  name 'Xccello'
+  homepage 'http://lingsdesigns.com/xccello/'
+  license :unknown
+
+  app 'Xccello.app'
+end
