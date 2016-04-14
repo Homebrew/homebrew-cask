ff03bdb0c704f7c121b25d7dc7a9b33b45bd3fdc
Aditya Dalal <adidalal@users.noreply.github.com>
2016-04-14 14:10:49 -0400

    updated bilibili (2.40)

--- Casks/bilibili.rb
+++ Casks/bilibili.rb
@@ -1,10 +1,10 @@
 cask 'bilibili' do
   version '2.40'
-  sha256 'ac201a1913d5daf472ab159cfb3e45e4ce6e3bd8e2435261a72aac3a3582c59e'
+  sha256 'a4965d6368d80456047e4d8ecf79ee12768b7a7d4ec5d4b50b1f6f4ce9590250'
 
   url "https://github.com/typcn/bilibili-mac-client/releases/download/#{version}/Bilibili.dmg.zip"
   appcast 'https://github.com/typcn/bilibili-mac-client/releases.atom',
-          checkpoint: '187363a48ee039afa1a19779a9f2b7d8167da922845fe65c4c078afecc16419d'
+          checkpoint: 'b728fd42ebb035ff90f01b2171f46b9fc0e42b5aa15117dfd4d04840e85a98c8'
   name 'Bilibili'
   homepage 'https://github.com/typcn/bilibili-mac-client/'
   license :gpl
