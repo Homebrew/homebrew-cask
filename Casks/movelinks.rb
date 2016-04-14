02bc4b41c11e05f59446094c6b2506fa6a627ad2
Victor Popkov <victor@popkov.me>
2016-04-14 23:53:55 +0300

    updated movelinks (1.3.27) (#20560)

--- Casks/movelinks.rb
+++ Casks/movelinks.rb
@@ -1,11 +1,11 @@
 cask 'movelinks' do
-  version '1.3.25'
-  sha256 '193a484db300af323a358bb7e8eb528c7a9f5c534df810bb63b5e178e768cf72'
+  version '1.3.27'
+  sha256 '8ba8a3f7847914d8ab3f01d87118dd5f88f9e836c5159ef98a3436349d9fc205'
 
   # d1c229iib3zm7m.cloudfront.net was verified as official when first introduced to the cask
   url "https://d1c229iib3zm7m.cloudfront.net/mac/Moveslink2_#{version.dots_to_underscores}.dmg"
   appcast 'https://d1c229iib3zm7m.cloudfront.net/mac/appcast.xml',
-          checkpoint: 'fb749b7e304f3bda8311b2eb06f1b272f945638225272fe9cf664d587704de39'
+          checkpoint: '322993293bf498eeb206ead2aae2308cd6ad5d7f62bda511f74749ca9defac91'
   name 'Movelinks'
   homepage 'http://www.movescount.com/connect/moveslinkmac/Suunto_Ambit'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
