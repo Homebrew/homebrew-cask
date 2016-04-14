e5008076c4e7303a3161ee2f37be436aeabe3794
Victor Popkov <victor@popkov.me>
2016-04-14 23:24:45 +0300

    updated blueharvest (6.3.7)

--- Casks/blueharvest.rb
+++ Casks/blueharvest.rb
@@ -1,10 +1,10 @@
 cask 'blueharvest' do
-  version '6.3.6'
-  sha256 '5ed3e785966145de5b2bb78ca5ac2b602444a739b54668f24666c848046ba198'
+  version '6.3.7'
+  sha256 '78dcc3bbad7426a40150984dc1573157fbfaf894d8084f60cf055c5b90b49f51'
 
   url "http://zeroonetwenty.com/downloads/BlueHarvest#{version.delete('.')}.dmg"
   appcast 'https://cp37.ezyreg.com/~zeroonet/downloads/versioninfo/sparkle/blueharvest6.xml',
-          checkpoint: 'e2a9fc64ee0869c31bdc120e458afd4b5db82390c6fde6c71fcf1f7389633d8e'
+          checkpoint: 'b495c1d2c2a1609bba50a038a6bcf03b936cddfd222aaa231595de9068d12a7c'
   name 'BlueHarvest'
   homepage 'http://zeroonetwenty.com/blueharvest/'
   license :commercial
