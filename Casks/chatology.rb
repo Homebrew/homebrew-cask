6148f0fece13881bf953db9f79e0de1146177f76
Victor Popkov <victor@popkov.me>
2016-04-14 23:34:17 +0300

    updated chatology (1.1) (#20551)

--- Casks/chatology.rb
+++ Casks/chatology.rb
@@ -1,10 +1,10 @@
 cask 'chatology' do
-  version '1.0.7'
-  sha256 '5cb80a22b3e9cbd654591606fcea41d9cf2484ca4cd528736118a94c6034158d'
+  version '1.1'
+  sha256 'cf5e9bf958b9cb62e3e194578a43d8c2d9c7bcb3138f0205c3b421f111566c73'
 
   url "http://cdn.flexibits.com/Chatology_#{version}.zip"
   appcast 'https://flexibits.com/chatology/appcast.php',
-          checkpoint: 'ea2ca59301336db753a723fff0fe7680a2ea52abf163a5450163b5a15ed245b2'
+          checkpoint: '8de7c950c0f46d3649bd3b0b78c1e787fa302e048da00b0dc384f9cb86821357'
   name 'Chatology'
   homepage 'https://flexibits.com/chatology'
   license :commercial
