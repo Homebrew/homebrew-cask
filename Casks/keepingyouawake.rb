adc30f5245ee1be38d970b1c816a775cc7bd22f9
Eitot <Eitot@users.noreply.github.com>
2016-04-16 18:28:42 +0200

    Update KeepingYouAwake to 1.4.0 (#20586)

--- Casks/keepingyouawake.rb
+++ Casks/keepingyouawake.rb
@@ -1,13 +1,23 @@
 cask 'keepingyouawake' do
-  version '1.3.1'
-  sha256 '8a4277fa63d6088f676a34fa82415552eef2122fa49e811ac18a0916689ae1f1'
+  version '1.4.0'
+  sha256 '98eb5025525548be6eaf1da4bfc374dbe73dc2fcd17610e6703f849eba3b986b'
 
   url "https://github.com/newmarcel/KeepingYouAwake/releases/download/#{version}/KeepingYouAwake-#{version}.zip"
   appcast 'https://github.com/newmarcel/KeepingYouAwake/releases.atom',
-          checkpoint: '326990ba0fde4a02bc22f08ad4cc30d3c14e94fd7ee59b9b5d393d3c211bb6ec'
+          checkpoint: 'ba4df62788e318184eb2c3d4c185243315ce52a16d559191dbf31fcbf27e1842'
   name 'KeepingYouAwake'
   homepage 'https://github.com/newmarcel/KeepingYouAwake'
   license :mit
 
   app 'KeepingYouAwake.app'
+
+  uninstall login_item: 'KeepingYouAwake'
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/info.marcel-dierkes.keepingyouawake.sfl',
+                '~/Library/Application Support/info.marcel-dierkes.KeepingYouAwake',
+                '~/Library/Caches/info.marcel-dierkes.KeepingYouAwake',
+                '~/Library/Preferences/info.marcel-dierkes.KeepingYouAwake.plist',
+                '~/Library/Saved Application State/info.marcel-dierkes.KeepingYouAwake.savedState',
+              ]
 end
