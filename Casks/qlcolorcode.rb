7392c344f4b291350b3bbbeb1dec233381affe72
Saagar Jha <saagarjha28@gmail.com>
2016-04-15 18:35:06 -0700

    updated qlcolorcode (2.0.6) (#20574)

--- Casks/qlcolorcode.rb
+++ Casks/qlcolorcode.rb
@@ -4,7 +4,7 @@ cask 'qlcolorcode' do
 
   url "https://github.com/anthonygelibert/QLColorCode/releases/download/release-#{version}/QLColorCode.qlgenerator.zip"
   appcast 'https://github.com/anthonygelibert/QLColorCode/releases.atom',
-          checkpoint: '0fdfdf4ab501124f84cfce27623a4b5d23e2b5a22c04fc50b74bbf20120ea3ae'
+          checkpoint: 'de01b6e253ec929a50c21e36414d29e7d33dd5c90a5416e2a414745902399bc8'
   name 'QLColorCode'
   homepage 'https://github.com/anthonygelibert/QLColorCode'
   license :oss
@@ -12,6 +12,11 @@ cask 'qlcolorcode' do
   depends_on formula: 'highlight'
 
   qlplugin 'QLColorCode.qlgenerator'
+
+  postflight do
+    # This sets the path to the highlight binary to the preferred one found in $PATH.
+    system 'defaults write org.n8gray.QLColorCode pathHL "$(which highlight)"'
+  end
 
   zap trash: '~/Library/Preferences/org.n8gray.QLColorCode.plist'
 end
