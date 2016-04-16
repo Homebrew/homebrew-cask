048e35e6626241ce7b667c20df3e8fee32759796
Samuel D. Leslie <sdl@nexiom.net>
2016-04-16 20:43:15 +1000

    updated remote-desktop-manager (3.5.2.0)

--- Casks/remote-desktop-manager.rb
+++ Casks/remote-desktop-manager.rb
@@ -1,6 +1,6 @@
 cask 'remote-desktop-manager' do
-  version '3.0.6.0'
-  sha256 'd0b6351831e81009cb115a23166f883eacedfef920c732c3525119d02d0a6a07'
+  version '3.5.2.0'
+  sha256 '8f4fb5737686f74ad01010a31dc93c259c48ae2babe07951652200f024debda1'
 
   # devolutions.net is the official download host per the vendor homepage
   url "http://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Mac.#{version}.dmg"
