88285e4236476e82abf6e897ba93fab7adba8290
Julio Napurí <julionc@gmail.com>
2016-04-16 00:02:17 -0500

    Upgrade Mattermost.app to v1.1.1

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,11 +1,11 @@
 cask 'mattermost' do
-  version '1.1.0'
-  sha256 '9f97b131209bb2daad6cf55be0faa20a461d5644284e556e48d1ca0707aa93c6'
+  version '1.1.1'
+  sha256 'afc6cce8205466d93089b4ae183364a8fc76b2d36cb96017b248c7e750b7dd30'
 
   # github.com/mattermost/desktop was verified as official when first introduced to the cask
   url "https://github.com/mattermost/desktop/releases/download/v#{version}/mattermost-desktop-#{version}-osx.tar.gz"
   appcast 'https://github.com/mattermost/desktop/releases.atom',
-          checkpoint: 'e5e9e6d55127eda2abfcfd624ba352acefd23af6750419f324f4149806305d97'
+          checkpoint: '1f48974001908cf0e7c934bbe95e3427581feff4fe261849cc4a015911894ef5'
   name 'Mattermost'
   homepage 'http://www.mattermost.org/'
   license :mit
