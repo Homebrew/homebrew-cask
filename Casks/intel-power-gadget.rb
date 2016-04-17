fb9e586c16c7151082cb15ec278e145bd658c245
Ken Sato <ksato9700@gmail.com>
2016-04-17 11:28:33 +0900

    fix a cask style offense

--- Casks/intel-power-gadget.rb
+++ Casks/intel-power-gadget.rb
@@ -3,7 +3,7 @@ cask 'intel-power-gadget' do
   version '3.0.3'
   sha256 '93f052f5c1306272fceab7af5740d2837656242ab879436a6ce9e573ed9a274e'
 
-  url "https://software.intel.com/file/501089/download"
+  url 'https://software.intel.com/file/501089/download'
   name 'Intel Power Gadget'
   homepage 'https://software.intel.com/en-us/articles/intel-power-gadget-20'
   license :gratis
