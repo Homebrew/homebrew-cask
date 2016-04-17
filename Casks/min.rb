37ad36a4f12f8668bbd4d2724e4f3e428f4c2ee6
Jerzerak <jerzerak@gmail.com>
2016-04-17 19:38:03 -0400

    Update min (1.1.2) (#20605)

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,11 +1,11 @@
 cask 'min' do
-  version '1.1.1'
-  sha256 '841da2609660a6fbb4c3cad70cf4170b3251cd78a708e4c1a53a5715007db61b'
+  version '1.1.2'
+  sha256 'fe353c6808018485eed39c0e138ab3ed443055f648d0694889713f54d0835545'
 
   # github.com/PalmerAL/min was verified as official when first introduced to the cask
   url "https://github.com/PalmerAL/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/PalmerAL/min/releases.atom',
-          checkpoint: '550a199aeefd280611b3eb03afe317920f25e220bb200134d3c1766b9ee03898'
+          checkpoint: 'b02a6d4a9ac21cce8119a6ff6064cfd82becb17c2ce713410d9c27ccc4780555'
   name 'Min'
   homepage 'https://palmeral.github.io/min'
   license :oss
