c13e9e7d63a57c6cc5f8c0399581f91e539bcfd0
Karel Rank <karel.rank@gmail.com>
2016-04-16 20:55:03 -0400

    updated franz (0.9.10)

--- Casks/franz.rb
+++ Casks/franz.rb
@@ -1,9 +1,11 @@
 cask 'franz' do
-  version :latest
-  sha256 :no_check
+  version '0.9.10'
+  sha256 'bac5896e5cad84d4018e9486cd580a04548c0a0b350ade506b82192175306570'
 
-  # bit.ly/22yh2b6 was verified as official when first introduced to the cask
-  url 'http://bit.ly/22yh2b6'
+  # github.com/imprecision/franz-app was verified as official when first introduced to the cask
+  url "https://github.com/imprecision/franz-app/releases/download/#{version}/Franz-darwin-x64-#{version}.dmg"
+  appcast 'https://github.com/imprecision/franz-app/releases.atom',
+          checkpoint: 'bfe803c6957cf9d530b954061370b590edebfc24eb3c6dd0afdce97fc1642e68'
   name 'Franz'
   homepage 'http://meetfranz.com'
   license :gratis
