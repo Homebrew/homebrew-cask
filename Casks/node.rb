7075972e3bf97d27e667cf4c9890d24b6d315448
Julio Napurí <julionc@gmail.com>
2016-04-17 23:11:31 -0500

    Upgrade Node to v.4.4.3 LTS

--- Casks/node.rb
+++ Casks/node.rb
@@ -1,6 +1,6 @@
 cask 'node' do
-  version '4.4.1'
-  sha256 '02e77d4ccd6d2a97124f61fc264f0e51268fe395c5de7f7528897eb7d31873e3'
+  version '4.4.3'
+  sha256 '2e7cfa53c916524add24d7407572ede6d199a0a7b539ba6da67fd5a97c7ebab2'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   name 'Node.js'
