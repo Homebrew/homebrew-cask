8915da744ff677e8f5c7e85231f3380b8c0bd45b
Aditya Dalal <adidalal@users.noreply.github.com>
2016-04-18 01:28:29 -0400

    updated commander-one (1.5.1)

--- Casks/commander-one.rb
+++ Casks/commander-one.rb
@@ -1,10 +1,10 @@
 cask 'commander-one' do
-  version '1.5'
-  sha256 '76b7b4e877bf2cfef92439741bf63f00d9aa151a9b99101c9e2db985c24d6abc'
+  version '1.5.1'
+  sha256 'b6c1759887ae9081395a9f437a415f52a9adcaa4f62cd273bd4b38c775ebf12d'
 
   url 'http://mac.eltima.com/download/commander.dmg'
   appcast 'http://www.eltima.com/download/commander-update/settings.xml',
-          checkpoint: 'f70b5070ef747f116c85198f1cbbbdd04d031fd3f36e9774b1c55cc66d2fc1b7'
+          checkpoint: '04e6c4477c910342e13172d66f73c5235488129fe10dc73faa525bb49a063772'
   name 'Commander One'
   homepage 'http://mac.eltima.com/file-manager.html'
   license :freemium
