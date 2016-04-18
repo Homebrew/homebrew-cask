a7a2ed578906361c814d70eba7f8e68d5f40d466
Aditya Dalal <adidalal@users.noreply.github.com>
2016-04-18 16:10:50 -0400

    updated sqlectron (1.6.1)

--- Casks/sqlectron.rb
+++ Casks/sqlectron.rb
@@ -1,11 +1,11 @@
 cask 'sqlectron' do
-  version '1.6.0'
-  sha256 '134917373edee38fea54e96eebfed92ead2f388abe589b4e92dac1c46f8aeeb9'
+  version '1.6.1'
+  sha256 '1eb4ed03b6add6ba34749d34bf786d0abc5f9867bfaa7be9fbadc0b5db245dcc'
 
   # github.com/sqlectron/sqlectron-gui was verified as official when first introduced to the cask
   url "https://github.com/sqlectron/sqlectron-gui/releases/download/v#{version}/Sqlectron-#{version}-mac.zip"
   appcast 'https://github.com/sqlectron/sqlectron-gui/releases.atom',
-          checkpoint: 'fe52c6cf05144ef4b470cf423e51826ad133cf4ba78f3a901457d10a7c2efdaf'
+          checkpoint: '5356a7133ca1819c8dc20630a3a274f45e222519e2f35015b392f94ce1761de8'
   name 'Sqlectron'
   homepage 'https://sqlectron.github.io/'
   license :mit
