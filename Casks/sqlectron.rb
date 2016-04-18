8371ac68830043d32a78b4aaa4bc70145136d9a5
Max Claus Nunes <maxcnunes@gmail.com>
2016-04-17 21:34:10 -0300

    Upgrade sqlectron to version 1.6.0
    In this version the installation file format has changed.

--- Casks/sqlectron.rb
+++ Casks/sqlectron.rb
@@ -1,17 +1,16 @@
 cask 'sqlectron' do
-  version '1.5.0'
-  sha256 'f5c2960cdbec32c8c8d4803b1d09e01be9e75ac1169030c3eac99b7d25e02fec'
+  version '1.6.0'
+  sha256 '134917373edee38fea54e96eebfed92ead2f388abe589b4e92dac1c46f8aeeb9'
 
   # github.com/sqlectron/sqlectron-gui was verified as official when first introduced to the cask
-  url "https://github.com/sqlectron/sqlectron-gui/releases/download/v#{version}/Sqlectron-darwin-x64.zip"
+  url "https://github.com/sqlectron/sqlectron-gui/releases/download/v#{version}/Sqlectron-#{version}-mac.zip"
   appcast 'https://github.com/sqlectron/sqlectron-gui/releases.atom',
-          checkpoint: 'a81a8512c35464dc1c1167fd27a15fa561251c88f7a991ef097f2d150f821eec'
+          checkpoint: 'fe52c6cf05144ef4b470cf423e51826ad133cf4ba78f3a901457d10a7c2efdaf'
   name 'Sqlectron'
   homepage 'https://sqlectron.github.io/'
   license :mit
 
   depends_on macos: '>= :mountain_lion'
-  container nested: 'Sqlectron.dmg'
 
   app 'Sqlectron.app'
 
