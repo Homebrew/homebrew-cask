83c7f9a9edea6427dde7333f93554395f4ab1bdb
Victor Popkov <victor@popkov.me>
2016-04-18 22:38:03 +0300

    updated mongodb (3.2.5-build.1)

--- Casks/mongodb.rb
+++ Casks/mongodb.rb
@@ -1,11 +1,11 @@
 cask 'mongodb' do
-  version '3.2.4-build.1'
-  sha256 'baae264c3dd2620b399681d7e1819e766239fe21da87bda66e780ad092a474af'
+  version '3.2.5-build.1'
+  sha256 '4b81fe65510f39aa244191ae5df3d083040d84b319de9a13d51eff67ce2a6488'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/gcollazo/mongodbapp/releases/download/#{version}/MongoDB.zip"
   appcast 'https://github.com/gcollazo/mongodbapp/releases.atom',
-          checkpoint: '21f3d7df471a8d6ebafe2bb113cb0b35e730298dddbd077db581a4049cec612c'
+          checkpoint: '6c6512a4e0cbd16c4e25553a2d06eb5467cf087f59746fb111dd214a73768e7a'
   name 'MongoDB'
   homepage 'https://elweb.co/mongodb-app/'
   license :mit
