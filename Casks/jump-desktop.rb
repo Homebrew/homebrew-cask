4ea19bbc00ca01be4de0863f3fcc4aaaaff00855
haseebq <haseebq@jumpdesktop.com>
2016-04-20 14:16:09 -0400

    Fix Jump Desktop appcast url (#20665)

--- Casks/jump-desktop.rb
+++ Casks/jump-desktop.rb
@@ -3,8 +3,8 @@ cask 'jump-desktop' do
   sha256 'b4776b0bf2dc2932e899690dc041a6e8ee7337f50ff39783f3c40964c6c1e010'
 
   url 'https://jumpdesktop.com/downloads/jdmac'
-  appcast 'https://service.jumpdesktop.com/update/jdmac-web/appcast.xml',
-          checkpoint: '4ff82f62c8503ac4342a7193ee1198936aa2ed4e3446727eec889a4e40a3c446'
+  appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml',
+          checkpoint: 'c1ec2782d66c15a6128dc3506d0f2d52d3959ac7a7f4632e295649e76ad6b2ab'
   name 'Jump Desktop'
   homepage 'https://jumpdesktop.com/#jdmac'
   license :commercial
