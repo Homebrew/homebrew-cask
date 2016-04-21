511d5f2f17ca884bbd15309a6dbb34e2d4b05337
Victor Popkov <victor@popkov.me>
2016-04-21 21:13:59 +0300

    cloud.rb: upgrade to v3.9.0, add appcast

--- Casks/cloud.rb
+++ Casks/cloud.rb
@@ -1,9 +1,11 @@
 cask 'cloud' do
-  version '3.8.2'
-  sha256 'e23e9ec5859a90d013e7f5eecfb0ae89d50efb47d2c5c6a2b36642f3c3aaecb1'
+  version '3.9.0'
+  sha256 '1d44e1c653f022f4a0bd5235484f37628f4c7f840019d24431598b67b0a444d9'
 
   # amazonaws.com/downloads.getcloudapp.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.dmg"
+  appcast 'http://updates.getcloudapp.com/appcast.xml',
+          checkpoint: 'd34ad1d87a2969a49dea721d91587d191c08c47fa435db2a95c12cfae525f61a'
   name 'CloudApp'
   homepage 'https://getcloudapp.com/'
   license :gratis
