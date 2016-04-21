3b8d50a1b01a83e5ad33aeaf4230b74d74aac8ff
Chris Metcalf <chris@chrismetcalf.net>
2016-04-21 11:37:12 -0700

    Fixing my hash syntax on the DeckHub cask

--- Casks/deckhub.rb
+++ Casks/deckhub.rb
@@ -2,7 +2,7 @@ cask 'deckhub' do
   version '1.0.1'
   sha256 '3d7fcb5039fce81f7ade31f25a0dd6fb721cbd4cf076a2e27dc79990c30caa71'
 
-  url 'http://download.getdeckhub.com/', :user_agent => :fake
+  url 'http://download.getdeckhub.com/', user_agent: :fake
   name 'DeckHub'
   homepage 'https://getdeckhub.com/'
   license :commercial
