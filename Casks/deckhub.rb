9cc24cae8bbd87275cec647ec15dc8ee5d48c790
Chris Metcalf <chris@chrismetcalf.net>
2016-04-21 11:37:12 -0700

    Syntax and style corrections to the deckhub cask

--- Casks/deckhub.rb
+++ Casks/deckhub.rb
@@ -1,8 +1,8 @@
 cask 'deckhub' do
-  version '1.0.1'
-  sha256 '3d7fcb5039fce81f7ade31f25a0dd6fb721cbd4cf076a2e27dc79990c30caa71'
+  version :latest
+  sha256 :no_check
 
-  url 'http://download.getdeckhub.com/', :user_agent => :fake
+  url 'http://download.getdeckhub.com/', user_agent: :fake
   name 'DeckHub'
   homepage 'https://getdeckhub.com/'
   license :commercial
