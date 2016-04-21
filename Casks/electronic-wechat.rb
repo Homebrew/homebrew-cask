63ec20fb4799e07bee815bfd031a24d360eae767
Zhongyi Tong <zhongyi.tong@foxmail.com>
2016-04-21 23:35:26 +0800

    Add `Electronic WeChat.app` v1.2.0.
    You can find the app here:
    https://github.com/geeeeeeeeek/electronic-wechat

---
+++ Casks/electronic-wechat.rb
@@ -0,0 +1,10 @@
+cask 'electronic-wechat' do
+  version '1.2.0'
+  sha256 :no_check
+
+  url 'https://github.com/geeeeeeeeek/electronic-wechat/releases/download/v1.2.0/mac-osx.tar.gz'
+  name 'Electronic WeChat'
+  homepage 'https://github.com/geeeeeeeeek/electronic-wechat'
+  license :mit
+  app 'Electronic WeChat-darwin-x64/Electronic WeChat.app'
+end
