f452af39a3dabdcde5821d3fb9092816ca9c46f7
Julio Napurí <julionc@gmail.com>
2016-04-21 17:06:10 -0500

    Upgrade Evernote.app to v6.6.1_453372

--- Casks/evernote.rb
+++ Casks/evernote.rb
@@ -4,11 +4,11 @@ cask 'evernote' do
     sha256 '06b6da6d74ccab08deabfdd4c9519b9bc7f7ef0f0db2a0e8b0cd72e781f2e0ed'
     url 'https://cdn1.evernote.com/mac/release/Evernote_402634.dmg'
   else
-    version '6.6_453293'
-    sha256 '4aca519788b4f0a4f9fd5637499836cd967d432148703f7014f32b289f688ef4'
+    version '6.6.1_453372'
+    sha256 'cd1cde41172c0db0b70212c2c902cb950ef872474b339cfd147dc626c3bdf0ec'
     url "https://cdn1.evernote.com/mac-smd/public/Evernote_RELEASE_#{version}.dmg"
     appcast 'https://update.evernote.com/public/ENMac/EvernoteMacUpdate.xml',
-            checkpoint: 'abf7888f90998b0fd516b14c3fc61c69f7bdec9b325fd890da276c7001c1fa60'
+            checkpoint: '0aa0e83defff628a87eb69245552fc87c947a86def8ad4213c625261ab382b2e'
   end
 
   name 'Evernote'
