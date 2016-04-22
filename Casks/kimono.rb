e75c6e70294b61d81c0c910963ccf61fed91f974
Tennison Chan <tennison.chan@gmail.com>
2016-04-22 12:36:57 -0400

    Version 1.0.5

--- Casks/kimono.rb
+++ Casks/kimono.rb
@@ -1,5 +1,5 @@
 cask 'kimono' do
-  version '0.35.2'
+  version '1.0.5'
   sha256 '30c6b83661573e10e1afabad0ef7f5c9ead60c4b2cedd4454160736c6c91a55f'
 
   # s3.amazonaws.com/kimono-desktop-releases was verified as official when first introduced to the cask
