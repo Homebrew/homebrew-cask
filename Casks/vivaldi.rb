6236d6db2ad35e5483e31ab7f63ebdd99d8a4081
William Tsoi <william@williamtsoi.net>
2016-04-22 14:11:33 +1000

    updating URL for vivaldi

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -2,7 +2,7 @@ cask 'vivaldi' do
   version '1.0.435.42'
   sha256 'd8b3149b77bc7ea1c8d9192de7fdec101eaa1b92ae0104bf86c62c9f22f0dde0'
 
-  url "https://download.vivaldi.com/stable/Vivaldi.#{version}.dmg"
+  url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.dmg"
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
   license :gratis
