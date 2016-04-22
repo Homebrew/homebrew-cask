d5ccd55b8b397e01e378d2445ecbe346e92b68cc
kryhear <kryhear@msn.cn>
2016-04-22 22:45:33 +0800

    Update tongbu.rb
    update to 1.2.1.0

--- Casks/tongbu.rb
+++ Casks/tongbu.rb
@@ -1,6 +1,6 @@
 cask 'tongbu' do
-  version '1.1.7'
-  sha256 'e9ff364db18915318367bc8ba359f963ce9c274bb9e27be10f2ce87192ac8a68'
+  version '1.2.1.0'
+  sha256 '1b5b0717424d8cef7a949f11f12f000b33a895b5f353ceb321247770a92e531b'
 
   # leaderhero.com is the official download host per the vendor homepage
   url "http://qd.appdown.info/qd/zsmacqd/Tongbu_mac_v#{version}_baidupinzhuan.dmg"
