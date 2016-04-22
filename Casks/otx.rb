8755d9fcc500035ddc06cfb41a7bfbfb46f02a3c
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2016-04-22 17:01:52 +0200

    Minor improvements * Removed caveats text about symlinking in favor of using the native 'binary' property * Added build auto-versioning * Enabled auto-updates

--- Casks/otx.rb
+++ Casks/otx.rb
@@ -1,19 +1,16 @@
 cask 'otx' do
-  version '1.7'
+  version '1.7,b566'
   sha256 '116a9441cfed31c28e0f9b3aa26b82f2a7186d3c8ec4afd2173c2ad460e51ab8'
 
-  url "https://github.com/x43x61x69/otx/releases/download/v#{version}/otx_b566.zip"
+  url "https://github.com/x43x61x69/otx/releases/download/v#{version.before_comma}/otx_#{version.after_comma}.zip"
   appcast 'https://github.com/x43x61x69/otx/releases.atom',
           checkpoint: 'd15c0290c67946c961ad885829d82d9e455cbd94e1be3892120711f4d89778e1'
   name 'otx'
   homepage 'https://github.com/x43x61x69/otx'
   license :oss
 
+  auto_updates true
+
   app 'otx.app'
-
-  caveats <<-EOS.undent
-    If you would like to install the #{token} CLI tool as well, please run the following command:
-
-      ln -s '#{staged_path}/#{token}' '/usr/local/bin/#{token}'
-  EOS
+  binary 'otx'
 end
