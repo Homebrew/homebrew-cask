125e76a560e1072f716ed0cabb71029825c996cb
Victor Popkov <victor@popkov.me>
2016-04-22 22:54:27 +0300

    updated biba (3.11.3891)

--- Casks/biba.rb
+++ Casks/biba.rb
@@ -1,11 +1,11 @@
 cask 'biba' do
-  version '3.10.3851'
-  sha256 '30c0522162f6c4f6be21cd9e78996fff0afa9c7c46eb7c00e48e09695d35ce85'
+  version '3.11.3891'
+  sha256 '875f6ab66d27f7650139716c0c65505d6f009f22d0c835c250a1a76384695b19'
 
   # amazonaws.com/downloads.biba.com was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/downloads.biba.com/builds/Biba-OSX-#{version}.dmg"
   appcast 'https://api.biba.com/osx_downloads/appcast',
-          checkpoint: 'be79a8d3ae09302eaf59df2f74758655df08a15497685adc66daaf1c93989547'
+          checkpoint: '4447cdca98d92db56e3dfb2f94674fbccf5dc34e487eb677d860d486dc068d0c'
   name 'Biba'
   homepage 'https://www.biba.com'
   license :closed
