c6d07e0e0d6d0fe428cc7f0938e686c4fdbfbb3c
Aditya Dalal <adidalal@users.noreply.github.com>
2016-04-23 14:46:25 -0400

    updated veracrypt (1.17)

--- Casks/veracrypt.rb
+++ Casks/veracrypt.rb
@@ -2,7 +2,7 @@ cask 'veracrypt' do
   version '1.17'
   sha256 'fa62b0e84f993dc2c58966e4eef91d8fdd3710913f78d62aa352e12187259f89'
 
-  # https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt
+  # download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt was verified as official when first introduced to the cask
   url 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt&DownloadId=1537180&FileTime=130999181375130000&Build=21031'
   name 'VeraCrypt'
   homepage 'https://veracrypt.codeplex.com/'
