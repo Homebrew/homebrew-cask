187f5c2ae0c00ae26aa39fc9f4bbf9c1632014c2
Michael Ford <fanquake@users.noreply.github.com>
2016-04-23 20:06:28 +0800

    PrivateTunnel 2.6

--- Casks/privatetunnel.rb
+++ Casks/privatetunnel.rb
@@ -1,6 +1,6 @@
 cask 'privatetunnel' do
-  version '2.5'
-  sha256 'a5ffd8a9d13fc20180b102212e3036d7a59210162bba647f207405ed03b14812'
+  version '2.6'
+  sha256 'a71e30cf59bbbb0ef95f5ce8099ac3f758cab8b9bc3d8727596df4a635efeb5b'
 
   url "https://swupdate.openvpn.org/privatetunnel/client/privatetunnel-mac-#{version}.dmg"
   name 'Private Tunnel'
