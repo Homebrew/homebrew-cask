cff20d638d9808cf7c43991b3cc10504fa4f026c
Surajram Kumaravel <me@surajr.am>
2016-04-23 15:47:16 +0530

    Update URL to HTTPS

--- Casks/canon-imagerunner-printer-driver-ufrii.rb
+++ Casks/canon-imagerunner-printer-driver-ufrii.rb
@@ -2,7 +2,7 @@ cask 'canon-imagerunner-printer-driver-ufrii' do
   version '10.09.00'
   sha256 '2811eb7c46a0011394d030a31d819466283aa3a5fb4dc79ce5e8c57d29820f6a'
 
-  url "http://downloads.canon.com/bisg2016/drivers/mac/UFRII_v#{version}_Mac.zip"
+  url "https://downloads.canon.com/bisg2016/drivers/mac/UFRII_v#{version}_Mac.zip"
   name 'Canon imageRUNNER UFRII Printer Driver'
   homepage 'https://www.usa.canon.com/internet/portal/us/home/support/details/copiers-mfps-fax-machines/support-multifunction/imagerunner-2018'
   license :gratis
