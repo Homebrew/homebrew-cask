eed404788fa189fbc4e5a72072e439ea6c6a568f
Victor Popkov <victor@popkov.me>
2016-04-23 23:36:25 +0300

    updated imazing (1.5.8,1461148384) (#20735)

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -1,11 +1,11 @@
 cask 'imazing' do
-  version '1.5.7,1460039697'
-  sha256 '65dee95097f4fb9b32fbf581bd227cf4fe956a54ad8f1512b29804d65db1d273'
+  version '1.5.8,1461148384'
+  sha256 '3f6ae09675f6370a013c9afe03e4bbf8d49578bd12755be8eba08d0ddb5aa844'
 
   # devmate.com/com.DigiDNA.iMazingMac was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.DigiDNA.iMazingMac/#{version.before_comma}/#{version.after_comma}/iMazingforMac-#{version.before_comma}.dmg"
   appcast 'https://updates.devmate.com/com.DigiDNA.iMazingMac.xml',
-          checkpoint: '067adde4ab4b95ddcf39a12afce955b90bb0153a15132cf353a8c20f0fdb5bd6'
+          checkpoint: '8df1e11389c211c0bcd71f3477b6244a3e51c4548f5a3a485179283be1e199f2'
   name 'iMazing'
   homepage 'https://imazing.com/'
   license :commercial
