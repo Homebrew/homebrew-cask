c295ccf5343aba5bd08967ce1b57bc3ff1c568c8
Aditya Dalal <adidalal@users.noreply.github.com>
2016-04-23 09:36:41 -0400

    updated privatetunnel (2.6)

--- Casks/privatetunnel.rb
+++ Casks/privatetunnel.rb
@@ -2,12 +2,13 @@ cask 'privatetunnel' do
   version '2.6'
   sha256 'a71e30cf59bbbb0ef95f5ce8099ac3f758cab8b9bc3d8727596df4a635efeb5b'
 
+  # swupdate.openvpn.org/privatetunnel was verified as official when first introduced to the cask
   url "https://swupdate.openvpn.org/privatetunnel/client/privatetunnel-mac-#{version}.dmg"
   name 'Private Tunnel'
   homepage 'https://www.privatetunnel.com'
   license :gratis
 
-  pkg 'PrivateTunnel.mpkg'
+  pkg 'PrivateTunnel.pkg'
 
   uninstall script: 'Uninstaller.command'
 end
