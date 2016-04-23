3348e81ae4e326332173a11359381313fdc059e6
Jussi Tuominen <me@jussituominen.me>
2016-04-23 21:34:16 +0300

    updated a-better-finder-rename (10.06) (#20728)

--- Casks/a-better-finder-rename.rb
+++ Casks/a-better-finder-rename.rb
@@ -1,10 +1,10 @@
 cask 'a-better-finder-rename' do
-  version '10.05'
-  sha256 'da00a1148cca43cafd3f1fe9e5f5909ec90d1f0398ba02e00a834e22693170cf'
+  version '10.06'
+  sha256 '0ac7a888709ed4799c001af38a9a84abf7a5777f97413a034bbeaa7593197f9c'
 
   url "http://www.publicspace.net/download/ABFRX#{version.major}.dmg"
   appcast "http://www.publicspace.net/app/signed_abfr#{version.major}.xml",
-          checkpoint: '96c3d39067ab206d72e66de98bc4e31379cc6a245d891bda4e8a04e133688c13'
+          checkpoint: '785224b7a90e2089df11e5c3f29d09943a11da42bb75c0a587a7981592773523'
   name 'A Better Finder Rename'
   homepage 'http://www.publicspace.net/ABetterFinderRename/'
   license :commercial
