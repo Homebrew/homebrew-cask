80f22345fa9a071fe7d4a27c037af8d6284c95f7
zhenglaizhang <zhenglaizhang@hotmail.com>
2016-04-23 18:18:11 +0800

    Add AliWorkBench.app V1.01.35-35

---
+++ Casks/aliworkbench.rb
@@ -0,0 +1,14 @@
+cask 'aliworkbench' do
+  version '1.01.35-35'
+  sha256 'd42b830d1a5b1eafba095ea0f70c96b09fff608d3bdef83f03de6bedf76f4609'
+
+  # dbison.alicdn.com was verified as official when first introduced to the cask
+  url "http://dbison.alicdn.com/updates/MacQN-#{version}.dmg"
+  name 'AliWorkBench'
+  name 'Qian Niu'
+  name '千牛'
+  homepage 'https://qianniu.1688.com'
+  license :gratis
+
+  app 'AliWorkBench.app'
+end
