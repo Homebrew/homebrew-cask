58ba2a0bb49375adf05b6284558a51a30c89499f
Yutaro Horikawa <yutaro.ver05@gmail.com>
2016-04-24 22:49:45 +0900

    Add DOTEC-AUDIO DeeSpeaker (AU)

---
+++ Casks/deespeaker-au.rb
@@ -0,0 +1,11 @@
+cask 'deespeaker-au' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://dotec-audio.com/release/DeeSpeaker/latest/au/DeeSpeakerAU.zip'
+  name 'DOTEC-AUDIO DeeSpeaker (AU)'
+  homepage 'http://dotec-audio.com/deespeaker.html'
+  license :gratis
+
+  audio_unit_plugin 'DeeSpeaker.component'
+end
