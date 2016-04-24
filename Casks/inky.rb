a3ddecf006166be1a10c9196ba465a7834c2c2e0
qkdreyer <quentin.dreyer@triotech.fr>
2016-04-24 14:23:14 +0200

    updated inky (latest)

--- Casks/inky.rb
+++ Casks/inky.rb
@@ -2,7 +2,7 @@ cask 'inky' do
   version :latest
   sha256 :no_check
 
-  url 'https://inky.com/mail/InkyInstall.dmg'
+  url 'https://download.inky.com/InkyInstall_osx_3.0.7_38.dmg'
   name 'Inky'
   homepage 'http://inky.com'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
