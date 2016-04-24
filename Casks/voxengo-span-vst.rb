e3db209fe3cdcd6fa7234a1b32a5ebcd53b66273
Sergio Sánchez <mofeing@gmail.com>
2016-04-24 22:08:09 +0200

    Update and rename voxengo-span.rb to voxengo-span-vst.rb

--- Casks/voxengo-span-vst.rb
+++ Casks/voxengo-span-vst.rb
@@ -1,9 +1,9 @@
-cask 'voxengo-span' do
+cask 'voxengo-span-vst' do
   version '2.10'
   sha256 '09f5a195145b5dff81b095bae8af9f3d98e301debe8a0f7732684fd479b20185'
 
   url "http://www.voxengo.com/files/VoxengoSPAN_#{version.delete('.')}_Mac_VST_VST3_setup.dmg"
-  name 'Voxengo SPAN'
+  name 'Voxengo SPAN (VST)'
   homepage 'http://www.voxengo.com/product/span/'
   license :gratis
 
