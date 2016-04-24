4c04f15a8a4433711cbf4b0d2eafe6652803c27a
howie <howie.yu@gmail.com>
2016-04-24 12:08:40 +0800

    updated filezilla (3.17.0) (#20754)

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -3,8 +3,8 @@ cask 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.16.1'
-    sha256 '61c47e2d3e76f6c38e93a2a85d2bfe62c309e887188c5f07d1e8cf1ca6947692'
+    version '3.17.0'
+    sha256 '16814c7c98a8ce4102eb27323f373a4f3009c63a76d04da19066b8a3e04ac7da'
   end
 
   # sourceforge.net/project/filezilla was verified as official when first introduced to the cask
