8c21c075b5bff9d40d5033276077c50a96c13e8b
Jerzerak <jerzerak@gmail.com>
2016-04-25 19:53:06 -0400

    Update curse.rb stanza's
    - Add new download url
    - Rename from Curse Voice to Curse
    - Link new homepage
    - Add zap stanza

--- Casks/curse.rb
+++ Casks/curse.rb
@@ -2,10 +2,22 @@ cask 'curse' do
   version :latest
   sha256 :no_check
 
-  url 'http://beta.cursevoice.com/download?platform=mac'
-  name 'Curse Voice'
-  homepage 'http://beta.cursevoice.com/'
+  # curseapp.net was verified as official when first introduced to the cask
+  url 'https://updates.curseapp.net/osx/Curse.dmg'
+  name 'Curse'
+  homepage 'https://www.curse.com'
   license :freemium
 
+  auto_updates true
+
   app 'Curse.app'
+
+  zap delete: [
+                '~/Library/Application Support/Curse',
+                '~/Library/Curse',
+                '~/Library/Saved Application State/com.curse.curseclient.savedState',
+                '~/Library/Caches/com.curse.radium',
+                '~/Library/Caches/curse',
+                '~/Library/Preferences/com.curse.curseclient.plist',
+              ]
 end
