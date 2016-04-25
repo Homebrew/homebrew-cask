306aeda91c8ad029981854cc0fdd22389351f59d
Aditya Dalal <adidalal@users.noreply.github.com>
2016-04-25 13:32:41 -0400

    updated sketch (3.7.2) (#20790)

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -1,10 +1,10 @@
 cask 'sketch' do
-  version '3.7.1'
-  sha256 'a6aae446bb5b0a1ac883f55ef01a5e32ef7dc21000b861f114200025cfc96e8d'
+  version '3.7.2'
+  sha256 'cf07466d5c8aaffb3e0c6d6242c3f2367b4f84d293d01064e1a7ee1a503c3429'
 
   url 'http://www.sketchapp.com/static/download/sketch.zip'
   appcast "http://www.sketchapp.com/appcast#{version.major}.xml",
-          checkpoint: 'b8181f62ece181db9c1d4558cbf0907e35d70dad0c82ddc297d45ad0572fd688'
+          checkpoint: 'fd79b655317e763fc7f11cbd084f0810f8c7f7c56438636207211b64f395048a'
   name 'Sketch'
   homepage 'http://www.sketchapp.com/'
   license :commercial
