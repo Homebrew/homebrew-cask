0b2b72d7a592d38190151802c3e10e9100c1cee2
Victor Popkov <victor@popkov.me>
2016-04-26 00:28:37 +0300

    updated openbazaar (1.1.4) (#20802)

--- Casks/openbazaar.rb
+++ Casks/openbazaar.rb
@@ -1,11 +1,11 @@
 cask 'openbazaar' do
-  version '1.1.3'
-  sha256 '9cc1e8548634ef272a74c153580363008eaeebfee35189776d33da1816a6a111'
+  version '1.1.4'
+  sha256 'ec5367ce0b9c8c4601432745848507591671f75a51595c0f402163d8fcf0efca'
 
   # github.com/OpenBazaar/OpenBazaar-Installer was verified as official when first introduced to the cask
   url "https://github.com/OpenBazaar/OpenBazaar-Installer/releases/download/v#{version}/OpenBazaar-#{version}.dmg"
   appcast 'https://github.com/OpenBazaar/OpenBazaar-Installer/releases.atom',
-          checkpoint: '5806b280d2073c5eb749d55daed0ffbd3ac9254209770ad5cb56b81d260bb0da'
+          checkpoint: 'abb16a0a9005fdaa8c78bd8baacccc4ce9cea4574d7d028516cb8451f1b08653'
   name 'OpenBazaar'
   homepage 'https://openbazaar.org/'
   license :mit
