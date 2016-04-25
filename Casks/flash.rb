613e02c0811cfa7a82dda379b088cad65f58885f
Aditya Dalal <adidalal@users.noreply.github.com>
2016-04-25 13:32:49 -0400

    updated flash (21.0.0.226) (#20791)

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -1,11 +1,11 @@
 cask 'flash' do
-  version '21.0.0.213'
-  sha256 '12c96c3133e1a214ae8a1c7a3a88982ae670517ba1f1c5bed94c5dedfa2b3c09'
+  version '21.0.0.226'
+  sha256 'a28cb25ec9c53396d9abe3a57da44e7fbe8e6f63e45fb01eabc4aa7e5ef7fab7'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "http://fpdownload.macromedia.com/get/flashplayer/pdc/#{version}/install_flash_player_osx.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
-          checkpoint: '32a4cb768f58a0ef3b080ca467ef06046eeead4b3d122e8e57646d1fef6829cf'
+          checkpoint: 'ea4cd9e1fae84e72772ddec64f7fe85a22b7a89f12f7d45f450e50d830078d06'
   name 'Adobe Flash Player'
   homepage 'https://get.adobe.com/flashplayer'
   license :gratis
