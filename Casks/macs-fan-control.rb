892852a4b2b1cbcc928be3c03e1349e849a1a2c8
Jerzerak <jerzerak@gmail.com>
2016-04-25 00:57:27 -0400

    Update macs-fan-control
    - Add zap stanza
    - Add legacy version 1.2.1.0

--- Casks/macs-fan-control.rb
+++ Casks/macs-fan-control.rb
@@ -1,11 +1,19 @@
 cask 'macs-fan-control' do
-  version :latest
-  sha256 :no_check
+  if MacOS.release <= :snow_leopard
+    version '1.2.1.0'
+    sha256 '6e47db508e2fbb54c626a9ba37baeeda0d6acc920a9f07b6d7c3526fc11d898e'
+    url 'http://www.crystalidea.com/downloads/legacy/macsfancontrol_10.6.zip'
+  else
+    version :latest
+    sha256 :no_check
+    url 'http://www.crystalidea.com/downloads/macsfancontrol.zip'
+  end
 
-  url 'http://www.crystalidea.com/downloads/macsfancontrol.zip'
   name 'Macs Fan Control'
   homepage 'http://www.crystalidea.com/macs-fan-control'
   license :gratis
 
   app 'Macs Fan Control.app'
+
+  zap delete: '~/Libary/Preferences/com.crystalidea.MacsFanControl.plist'
 end
