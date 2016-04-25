dca4c2dd18f913ed1c040e8b0f3c985d072edcbe
Michael Guillory <mike@knotis.com>
2016-04-25 02:11:27 -0700

    Update Kodi to 16.1 (#20778)

--- Casks/kodi.rb
+++ Casks/kodi.rb
@@ -1,6 +1,6 @@
 cask 'kodi' do
-  version '16.0'
-  sha256 '65766b8835a85a48b1d81c9578dfc4c1a1ad2dcdc5450447ebbb44976ebb29bd'
+  version '16.1'
+  sha256 '315ec1f3d64f6d5d7b55ecd80f839d0702326f6bbe80b64e94da4ae21ae44bca'
 
   url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-Jarvis-x86_64.dmg"
   name 'Kodi'
