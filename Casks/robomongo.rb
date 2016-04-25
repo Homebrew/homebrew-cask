6939c8047aaa21d256dd8549c8ece95bedbdc9c6
zhengdai <798484315@qq.com>
2016-04-25 15:10:51 +0800

    fix robomongo url

--- Casks/robomongo.rb
+++ Casks/robomongo.rb
@@ -2,7 +2,7 @@ cask 'robomongo' do
   version '0.9.0-rc7,2b7a8ca'
   sha256 '50cb4499796130e62b37f808790c22599059ec0d1d8d14dbc2094a836665828f'
 
-  url "https://download.robomongo.org/#{version.before_comma}/osx/robomongo-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg”
+  url "https://download.robomongo.org/#{version.before_comma}/osx/robomongo-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg"
   name 'Robomongo'
   homepage 'https://robomongo.org'
   license :gpl
