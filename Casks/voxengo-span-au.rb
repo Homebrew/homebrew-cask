e5a80548d8c5edecd776a593920161b81cb0f2a8
Sergio Sánchez <mofeing@gmail.com>
2016-04-25 18:10:52 +0200

    add Voxengo SPAN 2.10 (AU) (#20771)

---
+++ Casks/voxengo-span-au.rb
@@ -0,0 +1,11 @@
+cask 'voxengo-span-au' do
+  version '2.10'
+  sha256 '48cf467a2d3b3be87b4b6ab725dd10f6ecf98cb8bceb090c4b2920989f33d413'
+
+  url "http://www.voxengo.com/files/VoxengoSPAN_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
+  name 'Voxengo SPAN (AU)'
+  homepage 'http://www.voxengo.com/product/span/'
+  license :gratis
+
+  audio_unit_plugin 'SPAN.component'
+end
