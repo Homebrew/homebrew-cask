d339946060bbb9feea2d21eceb9237374bc6eed7
Sergio Sánchez <mofeing@gmail.com>
2016-04-25 11:33:51 +0200

    fix #{version.delete('.')} to #{version.no_dots}

--- Casks/voxengo-span-vst.rb
+++ Casks/voxengo-span-vst.rb
@@ -2,7 +2,7 @@ cask 'voxengo-span-vst' do
   version '2.10'
   sha256 '09f5a195145b5dff81b095bae8af9f3d98e301debe8a0f7732684fd479b20185'
 
-  url "http://www.voxengo.com/files/VoxengoSPAN_#{version.delete('.')}_Mac_VST_VST3_setup.dmg"
+  url "http://www.voxengo.com/files/VoxengoSPAN_#{version.no_dots}_Mac_VST_VST3_setup.dmg"
   name 'Voxengo SPAN (VST)'
   homepage 'http://www.voxengo.com/product/span/'
   license :gratis
