a279a43e0d396a92f2a44d664985dc4b867b82c3
Victor Popkov <victor@popkov.me>
2016-04-25 23:35:49 +0300

    updated jabref (3.3)

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -1,11 +1,11 @@
 cask 'jabref' do
-  version '3.2'
-  sha256 'ddde9938c3d5092ffe2ba2ecd127439de4ae304d783b5e33056951f449a185b5'
+  version '3.3'
+  sha256 '949bd9a1a3bb0c71dccfa73e6f9c62627071534250db79bdd051f79f75a3c2a8'
 
   # sourceforge.net/project/jabref was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/jabref/v#{version}/JabRef_macos_#{version.dots_to_underscores}.dmg"
   appcast 'https://github.com/JabRef/jabref/releases.atom',
-          checkpoint: '397228862c29af39b57e97e0a4337508d56fadd96289ecf54a8369955c9d2e21'
+          checkpoint: 'd4d445db221bf7ac2889bb5abbec4ca933cf2c533bdebcdf716d2c22c11776e0'
   name 'JabRef'
   homepage 'http://www.jabref.org/'
   license :gpl
