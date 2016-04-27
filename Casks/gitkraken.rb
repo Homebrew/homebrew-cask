6aab474170a35099b4d259836d171c30f57a65d7
Wirianto Djunaidi <wirianto_djunaidi@westpac.co.nz>
2016-04-27 14:39:39 +1200

    updated gitkraken (1.2.0)

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,10 +1,10 @@
 cask 'gitkraken' do
-  version '1.1.0'
-  sha256 '3c0c567d5103eec52d1568ce1b92b7fa91ff4d44e3500d0585cf8ecd496bb86d'
+  version '1.2.0'
+  sha256 'c9a472dddddd955f922f04310905c6fef0a955e2ea839a5674ee3a8b9276d5c4'
 
   url "http://release.gitkraken.com/darwin/v#{version}.zip"
   appcast 'https://release.gitkraken.com/darwin/RELEASES',
-          checkpoint: 'fd4ba279b253e5cda9f70d34269e88fb66d2f63230edf3b175ad6c6846a8a8d2'
+          checkpoint: '2a2d6e27b0409353931315f1789814124bb8015330737433085d1a1be0955bd5'
   name 'GitKraken'
   homepage 'http://www.gitkraken.com/'
   license :gratis
