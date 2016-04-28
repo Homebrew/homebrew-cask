556b800278d1bd41c784da8c0a2b8f71dfb4c10f
Tin H Kyaw <tinhkyaw@gmail.com>
2016-04-27 21:25:18 -0700

    updated typora cask to reflect the latest distro style

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,10 +1,8 @@
 cask 'typora' do
-  version '0.9.9.2.1'
-  sha256 '126eb546437987e57a9f5a7d5901be26e50554cf195f383b080bc0facbdfd98f'
+  version :latest
+  sha256 :no_check
 
   url "https://www.typora.io/download/typora_#{version}.zip"
-  appcast 'https://www.typora.io/download/dev_update.xml',
-          checkpoint: 'cee44a4f873585f26e13d859a69e234e9a2bace830797143c1078ef03f6da2e2'
   name 'Typora'
   homepage 'http://typora.io'
   license :gratis
