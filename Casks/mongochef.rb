8f45c0b039f7050b13214d55d4f78e5135a8f832
Vítor Galvão <info@vitorgalvao.com>
2016-04-28 14:59:12 -0400

    mongochef.rb: remove license comment

--- Casks/mongochef.rb
+++ Casks/mongochef.rb
@@ -5,7 +5,6 @@ cask 'mongochef' do
   url "https://cdn.3t.io/mongochef/mac/#{version}/MongoChef.dmg"
   name 'MongoChef'
   homepage 'https://3t.io/mongochef/'
-  # License is free for personal use (gratis) and paid for business use (commercial)
   license :other
 
   app '3T MongoChef.app'
