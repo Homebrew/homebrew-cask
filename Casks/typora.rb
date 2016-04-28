84b0ff0cac04c525b9be2d5ffc27146c4c622a29
Vítor Galvão <info@vitorgalvao.com>
2016-04-28 15:14:21 -0400

    updated typora (0.9.9.2.5) (#20853)

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,10 +1,10 @@
 cask 'typora' do
-  version '0.9.9.2.1'
-  sha256 '126eb546437987e57a9f5a7d5901be26e50554cf195f383b080bc0facbdfd98f'
+  version '0.9.9.2.5'
+  sha256 'd0925f78c43c18a61b63ead0cbd6fcec80c3e5ab081366d85492b6badc570527'
 
   url "https://www.typora.io/download/typora_#{version}.zip"
   appcast 'https://www.typora.io/download/dev_update.xml',
-          checkpoint: 'cee44a4f873585f26e13d859a69e234e9a2bace830797143c1078ef03f6da2e2'
+          checkpoint: 'adacf54f5c1b70945361714fdaf149d80bf079796ed3891255cc3e5c99553959'
   name 'Typora'
   homepage 'http://typora.io'
   license :gratis
