35cb336aa866dd7785f7e28875ff9f34afa2db33
Vítor Galvão <info@vitorgalvao.com>
2016-04-29 10:47:51 -0400

    updated cockatrice (2016-02-03,Gatewatchers) (#20874)

--- Casks/cockatrice.rb
+++ Casks/cockatrice.rb
@@ -1,18 +1,14 @@
 cask 'cockatrice' do
   version '2016-02-03,Gatewatchers'
-  sha256 'bddcd36c4517ee60fa342496488ca1d4678e22dc07f6abe50a4f139194b6ec1a'
+  sha256 '5934cc1688b5d27e922929ef513ec523955774f31332d8da223e802c116c27c1'
 
   # github.com/Cockatrice/Cockatrice was verified as official when first introduced to the cask
-  url "https://github.com/Cockatrice/Cockatrice/releases/download/#{version.before_comma}-Release/Cockatrice-#{version.after_comma}-osx.dmg.zip"
+  url "https://github.com/Cockatrice/Cockatrice/releases/download/#{version.before_comma}-Release/Cockatrice-#{version.after_comma}-osx.dmg"
   appcast 'https://github.com/Cockatrice/Cockatrice/releases.atom',
           checkpoint: '5a119ae4ed3c642e334614763aabe442631ddfa3594e99f5a3419518805d5a90'
   name 'Cockatrice'
   homepage 'http://www.woogerworks.com/'
   license :gpl
-
-  #container nested: "Cockatrice-#{version.after_comma}-osx.dmg"
-  # This is a typo on their part, and needs to be corrected upstream
-  container nested: 'Cockatrice-Gatewaterchers-osx.dmg'
 
   app 'cockatrice.app'
   app 'oracle.app'
