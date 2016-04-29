8ce476b305a8f5a9e06dfdbfc18571a77ce554e8
ilovezfs <ilovezfs@icloud.com>
2016-04-29 06:21:06 -0700

    openzfs 1.5.2 (#20871)

--- Casks/openzfs.rb
+++ Casks/openzfs.rb
@@ -1,13 +1,13 @@
 cask 'openzfs' do
-  version '1.4.5'
-  sha256 '9ad0b21bc54c5453a90427d478533f530ff9bb98744d17f2fcb127a81af18169'
+  version '1.5.2'
+  sha256 '185d20242bacd14cd609ccfb8f89736e1ea0ca6dec6475fd9eb7703c17ab5413'
 
-  url "https://openzfsonosx.org/w/images/9/9f/OpenZFS_on_OS_X_#{version}.dmg"
+  url "https://openzfsonosx.org/w/images/6/6b/OpenZFS_on_OS_X_#{version}.dmg"
   name 'OpenZFS on OS X'
   homepage 'https://openzfsonosx.org'
   license :oss
 
-  # OpenZFS on OSX has no version below Mountain Lion.
+  # OpenZFS on OS X has no version below Mountain Lion.
   if MacOS.release == :mountain_lion
     pkg "OpenZFS on OS X #{version.sub(%r{-.*}, '')} Mountain Lion.pkg"
   elsif MacOS.release == :mavericks
