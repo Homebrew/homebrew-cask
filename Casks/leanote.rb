fe5b32d17aac7466bcf29d9ec82e2971a6b94e5a
Gavin Hu <haozhonghu@hotmail.com>
2016-04-29 22:16:07 +0800

    Fix style check failed

--- Casks/leanote.rb
+++ Casks/leanote.rb
@@ -1,11 +1,11 @@
 cask 'leanote' do
   version '1.0.7'
-  sha256 :no_check 
+  sha256 :no_check
 
   url "http://downloads.sourceforge.net/sourceforge/leanote-desktop-app/leanote-desktop-mac-v#{version}.zip"
   name 'Leanote'
   homepage 'http://leanote.org'
-  license :gpl 
+  license :gpl
 
   app 'Leanote.app'
 end
