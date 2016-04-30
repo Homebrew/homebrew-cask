d5308b7821805605e03ec6f334b5f7b106046f92
miccal <miccal.matthews@gmail.com>
2016-04-30 16:21:02 +0800

    updated nimble (1.1.1) (#20882)

--- Casks/nimble.rb
+++ Casks/nimble.rb
@@ -1,11 +1,11 @@
 cask 'nimble' do
-  version '1.1.0'
-  sha256 '755367d441dc1065ec0d70c52455ec5b785c4659da0dd352ed9de2a447fa3682'
+  version '1.1.1'
+  sha256 '7b106f527adab8fa8fc4054a8dea6e6944cb3b3dbb8c128dbc0a18552e5be88b'
 
   # github.com/madebybright/Nimble was verified as official when first introduced to the cask
   url "https://github.com/madebybright/Nimble/releases/download/#{version}/Nimble-v#{version}.dmg"
   appcast 'https://github.com/madebybright/Nimble/releases.atom',
-          checkpoint: '368ffc1fbe8ae6fa67affe2c125232dd0329d0ba1e822a435acd515d936183a9'
+          checkpoint: '295a85ecdef78a59314a243a57e2af14f5f13365f6ae3e8ac0042dde017355cc'
   name 'Nimble'
   homepage 'http://madebybright.com'
   license :mit
