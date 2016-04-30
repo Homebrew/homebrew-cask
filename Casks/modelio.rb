86709fcbbf30de23bcbf1a6ba4af9eb06b7733d6
Michael Piffret <michael.piffret@gmail.com>
2016-04-30 18:56:39 +0200

    App Modelio.app v3.4.1

---
+++ Casks/modelio.rb
@@ -0,0 +1,13 @@
+cask 'modelio' do
+  version '3.4.1,118'
+  sha256 'ad31345e88b287cce90b21cf35ca801bc24befcfb4bd2dfc25a3781e4fc415fb'
+
+  url "https://www.modelio.org/modelio-download-archive/doc_download/#{version.after_comma}-modelio-#{version.before_comma.delete('.')}-macos-x.html"
+  name 'Modelio'
+  homepage 'https://www.modelio.org/'
+  license :oss
+
+  app "Modelio #{version.major_minor}/modelio.app"
+
+  zap delete: '~/.modelio'
+end
