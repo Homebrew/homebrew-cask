971fd38152b43c66b0834515ba56aa513432d184
miccal <miccal.matthews@gmail.com>
2016-04-30 15:26:51 +0800

    Nimble (1.1.0) (#20881)

--- Casks/nimble.rb
+++ Casks/nimble.rb
@@ -5,10 +5,18 @@ cask 'nimble' do
   # github.com/madebybright/Nimble was verified as official when first introduced to the cask
   url "https://github.com/madebybright/Nimble/releases/download/#{version}/Nimble-v#{version}.dmg"
   appcast 'https://github.com/madebybright/Nimble/releases.atom',
-          checkpoint: '755367d441dc1065ec0d70c52455ec5b785c4659da0dd352ed9de2a447fa3682'
+          checkpoint: '368ffc1fbe8ae6fa67affe2c125232dd0329d0ba1e822a435acd515d936183a9'
   name 'Nimble'
   homepage 'http://madebybright.com'
   license :mit
 
   app 'Nimble.app'
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.electron.nimble.sfl',
+                '~/Library/Application Support/Nimble',
+                '~/Library/Caches/Nimble',
+                '~/Library/Preferences/com.electron.nimble.plist',
+                '~/Library/Saved Application State/com.electron.nimble.savedState',
+              ]
 end
