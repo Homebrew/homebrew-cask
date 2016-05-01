c2fe2deb43e88d3affb0acfb41ad339942bd329f
Victor Popkov <victor@popkov.me>
2016-05-01 23:50:31 +0300

    updated freeze (2.1) (#20914)

--- Casks/freeze.rb
+++ Casks/freeze.rb
@@ -1,10 +1,10 @@
 cask 'freeze' do
-  version '2.0'
-  sha256 'a64aa70671daf02816b537ed5ebd3b02e00efec9bba2b25b4eb209217e1372fb'
+  version '2.1'
+  sha256 'cfa2858e21d3e338ec55a783e13b01391364a7db8e911a851b081355ccff781f'
 
   url 'https://freezeapp.net/download/Freeze.zip'
   appcast 'https://freezeapp.net/appcast.xml',
-          checkpoint: 'a3f4b527e5af6a1922640f344ca63897caf31b278e879624a75e5563a2895e12'
+          checkpoint: '1dd16638e8718507eb9a781feeb673c5be14079dc5024fcc74cd7c626c082dd1'
   name 'Freeze'
   homepage 'https://freezeapp.net/'
   license :freemium
