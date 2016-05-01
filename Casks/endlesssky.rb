34db38ff18f74d713913fd459e7a601bc777b2eb
Victor Popkov <victor@popkov.me>
2016-05-01 23:47:02 +0300

    updated endlesssky (0.9.0) (#20913)

--- Casks/endlesssky.rb
+++ Casks/endlesssky.rb
@@ -1,11 +1,11 @@
 cask 'endlesssky' do
-  version '0.8.11'
-  sha256 'c6a9a20bac8ef9bd4813f590d3125381a90bf15d41bdfda4e78e74c57654de9f'
+  version '0.9.0'
+  sha256 '708cbfaad8448eb5208f5b85408545054c1b8744f87a4208a848e7452c1b337e'
 
   # github.com/endless-sky/endless-sky was verified as official when first introduced to the cask
   url "https://github.com/endless-sky/endless-sky/releases/download/v#{version}/endless-sky-macosx-#{version}.dmg"
   appcast 'https://github.com/endless-sky/endless-sky/releases.atom',
-          checkpoint: '2802b6e9a84a8f2f9b246df12a3807d8c493d9730a24b49f838f14deae257186'
+          checkpoint: '4bc2dcbd402ab4172d925a06522a9bc764129d4fd36825485b9f75fbee441793'
   name 'Endless Sky'
   homepage 'https://endless-sky.github.io/'
   license :gpl
