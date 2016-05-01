c6c108ae9434244b6e9b7c25772c978cde7d1ba5
Victor Popkov <victor@popkov.me>
2016-05-01 23:13:11 +0300

    updated swiftybeaver (0.7.2)

--- Casks/swiftybeaver.rb
+++ Casks/swiftybeaver.rb
@@ -1,10 +1,10 @@
 cask 'swiftybeaver' do
-  version '0.7.0'
-  sha256 'ecb636b58fde06307a3875c3221b53f122e53c9b460db20bc88865211330a25a'
+  version '0.7.2'
+  sha256 'e8e87a73438e92a70a6026d36ca66b317dbdee822b0159dba89e6a06c759ee4b'
 
   url "https://cdn.swiftybeaver.com/SBMac-#{version}.zip"
   appcast 'https://cdn.swiftybeaver.com/sbmac-appcast.xml',
-          checkpoint: 'a98626de43749ccb471858f2fd7155728dae1474de37083b39f2d8c8590a3c0e'
+          checkpoint: 'acfbdeb4d7e2bd30ad758e72631c4ef60f173c22302577ca1f05a1bf836dc98e'
   name 'SwiftyBeaver'
   homepage 'https://swiftybeaver.com/'
   license :mit
