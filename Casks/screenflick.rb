7dc4785ca2eaffb00fade8bce0646a8d013c01bd
Victor Popkov <victor@popkov.me>
2016-05-02 00:10:09 +0300

    updated screenflick (2.7.13) (#20918)

--- Casks/screenflick.rb
+++ Casks/screenflick.rb
@@ -1,10 +1,10 @@
 cask 'screenflick' do
-  version '2.7.12'
-  sha256 '314d855c59a1f3124d4867b984f7ca28a27b61164096b9c0f69c0020ef4f565d'
+  version '2.7.13'
+  sha256 'ea833fbbb12d3bcc2d767e3171f5fc7c8b562ed65b12f9878009d685dc73f3cf'
 
   url "https://store.araelium.com/screenflick/downloads/versions/Screenflick#{version}.zip"
   appcast 'https://arweb-assets.s3.amazonaws.com/downloads/screenflick/screenflick2.xml',
-          checkpoint: 'abf7983542c28399d22d860295c97779f401e4dc792634d0648370c69a5cd484'
+          checkpoint: 'f8edb9b54c1d2374b43ef9a374ae11fe7d3001a05b177c9c5671ee7c303b18d0'
   name 'Screenflick'
   homepage 'http://www.araelium.com/screenflick/'
   license :commercial
