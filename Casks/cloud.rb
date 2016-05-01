61a983d61aabd225c2243b2cf84d4be9b8b818fe
Victor Popkov <victor@popkov.me>
2016-05-01 23:44:05 +0300

    updated cloud (3.10.0) (#20906)

--- Casks/cloud.rb
+++ Casks/cloud.rb
@@ -1,11 +1,11 @@
 cask 'cloud' do
-  version '3.9.0'
-  sha256 '1d44e1c653f022f4a0bd5235484f37628f4c7f840019d24431598b67b0a444d9'
+  version '3.10.0'
+  sha256 '6cc47e17ff8de0144187b0d21f193db412455549e044371d7d047455ec292918'
 
   # amazonaws.com/downloads.getcloudapp.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.dmg"
   appcast 'http://updates.getcloudapp.com/appcast.xml',
-          checkpoint: 'd34ad1d87a2969a49dea721d91587d191c08c47fa435db2a95c12cfae525f61a'
+          checkpoint: 'df9f3bf904fde11673f20af284a83661e5ae8e5cf3ce593928b8594a8b09c51f'
   name 'CloudApp'
   homepage 'https://getcloudapp.com/'
   license :gratis
