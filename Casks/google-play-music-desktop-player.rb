dd5830d4ed9a0dcc3b8030f43082d0d0470708d0
Jerzerak <jerzerak@gmail.com>
2016-05-01 18:01:20 -0400

    Update google-play-music-desktop-player (3.2.4)

--- Casks/google-play-music-desktop-player.rb
+++ Casks/google-play-music-desktop-player.rb
@@ -1,11 +1,11 @@
 cask 'google-play-music-desktop-player' do
-  version '3.2.1'
-  sha256 'f44aec5699f78063fed465925bcfbaf4ccc440701f98a2a74afa9ed36f523169'
+  version '3.2.4'
+  sha256 '041b855b91a4ee889ed4dbdf87c202a0f972eb058b36720a6319a3fba83938b2'
 
   # github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL- was verified as official when first introduced to the cask
   url "https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v#{version}/Google.Play.Music.Desktop.Player.OSX.zip"
   appcast 'https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases.atom',
-          checkpoint: '6503449150af8cec13380c68fde4a0cd776fec554c150c12d9c140390a61830e'
+          checkpoint: 'ce7ebdd1679b95b71287b7d13fae3f9a555a446ac4e5d51927cc220177fa2734'
   name 'Google Play Music Desktop Player'
   homepage 'http://www.googleplaymusicdesktopplayer.com/'
   license :mit
