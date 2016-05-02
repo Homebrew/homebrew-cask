b53cc73f42561a89ddce9ce0c522fe70f725e3c9
Norio Nomura <norio.nomura@gmail.com>
2016-05-02 18:59:38 +0900

    Add PlaygroundMDImporter 0.1.0

---
+++ Casks/playgroundmdimporter.rb
@@ -0,0 +1,13 @@
+cask 'playgroundmdimporter' do
+  version '0.1.0'
+  sha256 '15724e51ec59e647b1688618f418b549006c58f5a1d8fc04866ee48114aa2c85'
+
+  url "https://github.com/norio-nomura/PlaygroundMDImporter/releases/download/#{version}/Playground.mdimporter-#{version}.zip"
+  appcast 'https://github.com/norio-nomura/PlaygroundMDImporter/releases.atom',
+          checkpoint: '7220165f1235ccc639906c3a34b75e4a1b49bb3a2d8aa07b22b4fbdbf1bf3f65'
+  name 'PlaygroundMDImporter'
+  homepage 'https://github.com/norio-nomura/PlaygroundMDImporter'
+  license :mit
+
+  artifact 'Playground.mdimporter', target: "#{ENV['HOME']}/Library/Spotlight/Playground.mdimporter"
+end
