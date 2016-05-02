983950e6619be56d9be5c3c69e9237133f3281db
Matthew Herrmann <herrmann.matthew@abc.net.au>
2016-05-02 13:31:07 +1000

    Modifications to Datadog Agent.app v5.7.4-1 cask
    Modifications to Datadog Agent cask as per comments on pull request.

--- Casks/datadog-agent.rb
+++ Casks/datadog-agent.rb
@@ -1,8 +1,8 @@
 cask 'datadog-agent' do
-  version :latest
+  version :5.7.4-1
   sha256 :no_check
 
-  # amazonaws.com is the official download host as per vendor install scripts/documentation
+  # amazonaws.com/dd-agent is the official download host as per vendor documentation/install script (https://github.com/DataDog/dd-agent/blob/master/packaging/osx/install.sh)
   url 'https://s3.amazonaws.com/dd-agent/datadogagent.dmg'
   name 'Datadog Agent'
   homepage 'https://www.datadoghq.com/'
@@ -19,5 +19,13 @@ cask 'datadog-agent' do
 
   zap delete: '/opt/datadog-agent'
 
-  caveats 'You will need to update /opt/datadog-agent/etc/datadog.conf and replace APIKEY with your api key.'
+  caveats <<-EOS.undent
+    You will need to update /opt/datadog-agent/etc/datadog.conf and replace APIKEY with your api key
+
+    If you ever want to start/stop the Agent, please use the Datadog Agent App or datadog-agent command.
+    It will start automatically at login, if you want to enable it at startup, run these commands:
+
+    sudo cp '/opt/datadog-agent/etc/com.datadoghq.agent.plist' /Library/LaunchDaemons
+    sudo launchctl load -w /Library/LaunchDaemons/com.datadoghq.agent.plist
+  EOS
 end
