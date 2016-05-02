043bf84ff558d3314abe0107e24c63ab30e136d0
yutarody <yutaro.ver05+github@gmail.com>
2016-05-03 01:43:45 +0900

    update dmm 2.0.4 & rename to dmmbookviwer
    rename dmm>dmmbookviewer
    
    name dmmviwer>dmmbookviwer
    
    change uninstall method

---
+++ Casks/dmmbookviewer.rb
@@ -0,0 +1,14 @@
+cask 'dmmbookviewer' do
+  version '2.0.4'
+  sha256 'ac794dc71761a8a891dc237c11665866cfe76f7bcce68c20f7afb24a1a4a39b9'
+
+  url "http://dl.aka.dmm.co.jp/dmmviewer/mac/DMMViewerSetup_Mac_#{version}.pkg"
+  name 'DMMbookviewer'
+  homepage 'http://book.dmm.com/info_bookviewer.html'
+  license :gratis
+
+  pkg "DMMViewerSetup_Mac_#{version}.pkg"
+
+  uninstall pkgutil: 'com.dmm.DMMbookviewer',
+            rmdir: '/Applications/DMMbookviewer.app'
+end
