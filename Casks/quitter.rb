28f5e9422f7db4c20ca121c11bdc6a7da980e247
Chris Farmer <farmerchris@gmail.com>
2016-05-02 14:15:35 -0500

    Add Quitter.app v1.0

---
+++ Casks/quitter.rb
@@ -0,0 +1,21 @@
+cask 'quitter' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://marco.org/appcasts/Quitter.zip'
+  name 'Quitter'
+  homepage 'https://marco.org/apps#quitter'
+  license :gratis
+
+  app 'Quitter.app'
+
+  postflight do
+    suppress_move_to_applications
+  end
+
+  zap delete: [
+                '~/Library/Preferences/com.marcoarment.quitter.plist',
+                '~/Library/Caches/com.marcoarment.quitter',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.marcoarment.quitter.sfl',
+              ]
+end
