69cb90f3f8f46c54aa54b915df753db7866e22f1
yutarody <yutaro.ver05+github@gmail.com>
2016-05-03 05:49:50 +0900

    fix version
    fix url

--- Casks/dmm-player.rb
+++ Casks/dmm-player.rb
@@ -1,13 +1,13 @@
 cask 'dmm-player' do
-  version '1_6_0_2'
+  version '1.6.0.2'
   sha256 '3acbcf81b439c60013685c3243820e4906a3f2eff176d63b55b31ad0fd06abdf'
 
-  url "http://portalapp.dmm.com/silverlightplayer/dmm/m/#{version}/DMMPlayerInstaller_#{version}.pkg"
+  url "http://portalapp.dmm.com/silverlightplayer/dmm/m/#{version.dots_to_underscores}/DMMPlayerInstaller_#{version.dots_to_underscores}.pkg"
   name 'DMM Player'
   homepage 'http://www.dmm.com/digital/howto_dmmplayer_html/'
   license :gratis
 
-  pkg "DMMPlayerInstaller_#{version}.pkg"
+  pkg "DMMPlayerInstaller_#{version.dots_to_underscores}.pkg"
 
   uninstall pkgutil: 'com.apple.ScriptEditor.id.DMMPlayer*'
 end
