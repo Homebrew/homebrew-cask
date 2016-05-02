8cf345f07b598119645da779e90e52e50788f8d9
Jerzerak <jerzerak@gmail.com>
2016-05-01 20:43:16 -0400

    Update trailer (1.3.19)

--- Casks/trailer.rb
+++ Casks/trailer.rb
@@ -1,10 +1,10 @@
 cask 'trailer' do
-  version '1.3.17'
-  sha256 '3c556eb1e68751818346e3468ca5d779c7f38f1ad3cd9b8d14a3baddc7deb2be'
+  version '1.3.19'
+  sha256 '838d0c9245e4672584bc2e7a1044f86bd17409793491ce1f142a2784943d6128'
 
   url "https://ptsochantaris.github.io/trailer/trailer#{version.no_dots}.zip"
   appcast 'https://ptsochantaris.github.io/trailer/appcast.xml',
-          checkpoint: 'eeded65b93ba8a391c14e4b5e60187bc56c56cdbe197005ef1288236aee3bd91'
+          checkpoint: '4a2bf008f35c5dca70d840d1e05ac1fae8e079b1797dbca9c7c2a63a5faf2021'
   name 'Trailer'
   homepage 'https://ptsochantaris.github.io/trailer/'
   license :mit
