0dddd4c896ef795a6ef23df0c5ed05d1eb7239e4
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-05-03 16:37:26 +0300

    updated utox (0.8.1) (#20947)

--- Casks/utox.rb
+++ Casks/utox.rb
@@ -1,11 +1,11 @@
 cask 'utox' do
-  version '0.8.0'
-  sha256 '334a26e2979229239fc620c70b61c9283b96a68e5233dd06c5e4d8fc635142f7'
+  version '0.8.1'
+  sha256 '54060e95ef6ffb37cad6aee5dfa83c487d06b84b159dabb703e32c3960931b05'
 
   # github.com/GrayHatter/uTox was verified as official when first introduced to the cask
   url "https://github.com/GrayHatter/uTox/releases/download/v#{version}/uTox-#{version}.dmg"
   appcast 'https://github.com/GrayHatter/uTox/releases.atom',
-          checkpoint: '4f964099551dbd1db6d551f08b421862b185a3c54a5b91f503408c0c8289d331'
+          checkpoint: '23224efe8b2246ef173745955e1cdc806e1f9dcf6703b33b74058aa0a0af84d3'
   name 'uTox'
   homepage 'https://www.tox.chat'
   license :oss
