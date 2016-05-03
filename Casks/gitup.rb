19a72d8ee7f2aa458817d209dcbc5f87d69af857
Matteo Castellini <mcastellini@users.noreply.github.com>
2016-05-03 12:05:13 +0200

    updated gitup (1.0.5) (#20944)

--- Casks/gitup.rb
+++ Casks/gitup.rb
@@ -1,11 +1,11 @@
 cask 'gitup' do
-  version '1.0.4'
-  sha256 '756222e40bb125e5e0e3231b7a5ada035c73a0461e67ce09475fb0324dd7de76'
+  version '1.0.5'
+  sha256 'ca230886c4e808518b918530392ef2eb6aea5d11dcf4ea23a89a271d3d0ee3f1'
 
   # s3-us-west-2.amazonaws.com/gitup-builds was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/gitup-builds/stable/GitUp.zip'
   appcast 'https://github.com/git-up/GitUp/releases.atom',
-          checkpoint: '6d7967f3e3e93fce6596c760ed4ca3989b8c02077c7b0b18c4ff127e5723fda3'
+          checkpoint: '70b0baa99c73973227fc6a3a361e0b7bf6df59dbd77972400a028604e4d21ef9'
   name 'GitUp'
   homepage 'http://gitup.co'
   license :gpl
