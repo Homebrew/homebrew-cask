a2134e99d29053853b09a232d04c4ec006f91b66
ftrossbach <florian.trossbach@codecentric.de>
2016-05-03 16:10:15 +0200

    Updated mqttfx (1.1.0) (#20948)

--- Casks/mqttfx.rb
+++ Casks/mqttfx.rb
@@ -1,6 +1,6 @@
 cask 'mqttfx' do
-  version '1.0.0'
-  sha256 'ffa6d389c5e39e9ce2613598d0ab9e1c187a374889c21f188dbef7aeda9c5177'
+  version '1.1.0'
+  sha256 'd5e05708e6ac5c4884cac9e36828352ded4cce5d87a1cbf093ee19af7e44f95c'
 
   # jensd.de/apps/mqttfx was verified as official when first introduced to the cask
   url "http://www.jensd.de/apps/mqttfx/#{version}/MQTT.fx-#{version}.dmg"
