7e65157ef3e4c82ebd74128ec18a0a991731b3cb
Nicholas E. Rabenau <nerab@gmx.at>
2016-05-03 21:36:00 +0200

    Add PosteRazor 1.5

---
+++ Casks/posterazor.rb
@@ -0,0 +1,11 @@
+cask 'posterazor' do
+  version '1.5'
+  sha256 'dbecf2dc00fe2bf106df2e9fd9bc66014015989771db6c369a86d755f516df49'
+
+  url "http://downloads.sourceforge.net/sourceforge/posterazor/PosteRazor-#{version}-OSX.dmg"
+  name 'PosteRazor'
+  homepage 'http://posterazor.sourceforge.net'
+  license :gpl
+
+  app 'PosteRazor.app'
+end
