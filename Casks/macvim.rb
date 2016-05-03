5784d98dc845cddd17745b21b7137a9a7efc689a
Register <registe@users.noreply.github.com>
2016-05-03 10:06:17 +0800

    updated macvim (7.4.102) (#20941)

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,10 +1,10 @@
 cask 'macvim' do
-  version '7.4.101'
-  sha256 'daecfbc7f060ae4916d2da78e4592f3468a98f3598e34314bffa9daab1c63848'
+  version '7.4.102'
+  sha256 'b4a32d1404fc1209aaff4ee11aed00bb7e3a4518cd7d3669a03a6edb2aa63b75'
 
   url "https://github.com/macvim-dev/macvim/releases/download/snapshot-#{version.patch}/MacVim.dmg"
   appcast 'https://github.com/macvim-dev/macvim/releases.atom',
-          checkpoint: '31489a1a1cc86637be6ffa7efe535e9679383a49eeacb54dc38870941f8bcd56'
+          checkpoint: '26f570c5620a93e81cdf2fa94b52c280170a42d55b9db35b191135fdbeaa23f4'
   name 'MacVim'
   homepage 'https://github.com/macvim-dev/macvim'
   license :oss
