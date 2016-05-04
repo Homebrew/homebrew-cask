f245965e26143570551ba2db91915e1164925f66
Aditya Dalal <adidalal@users.noreply.github.com>
2016-05-04 09:28:14 -0400

    layout_test.rb: Add vendor to ignored dirs (#20972)
    Add `vendor` to ignore dirs, allowing tests to pass

--- test/layout_test.rb
+++ test/layout_test.rb
@@ -18,6 +18,7 @@ describe "Repo layout" do
                     .rubocop_todo.yml
                     .ruby-version
                     coverage
+                    vendor
                    }
 
   # the developer has hopefully gitignored these
