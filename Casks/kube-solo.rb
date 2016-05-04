c5a073f7ab42fa58326fe7f38aa8fd365aec37f1
Victor Popkov <victor@popkov.me>
2016-05-04 23:48:51 +0300

    updated kube-solo (0.5.8) (#20994)

--- Casks/kube-solo.rb
+++ Casks/kube-solo.rb
@@ -1,10 +1,10 @@
 cask 'kube-solo' do
-  version '0.5.7'
-  sha256 'e8b4dac67b873a9722cbe89ceb14a036e79569bc243cf251ce18476afbe5d5e8'
+  version '0.5.8'
+  sha256 '8c4f995344ab4dcfa26dde05149aa3b17f30ccb195330b894fdf8d6a1de0ad2a'
 
   url "https://github.com/TheNewNormal/kube-solo-osx/releases/download/v#{version}/Kube-Solo_v#{version}.dmg"
   appcast 'https://github.com/TheNewNormal/kube-solo-osx/releases.atom',
-          checkpoint: '5de84b2d1fd2e430503060e563d12f09db6e27a0291b93d033a2a21af915fdb7'
+          checkpoint: '07408eb4128be011cec274d7b89b78cb3cd56d67ab92eec199b90f94f3a98ecf'
   name 'Kube-Solo'
   homepage 'https://github.com/TheNewNormal/kube-solo-osx'
   license :apache
