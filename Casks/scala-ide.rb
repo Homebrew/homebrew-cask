a1cf82404e298c0c53b522f1e26be835d33a5b1f
Alex Varju <alex@varju.ca>
2016-05-04 12:29:01 -0700

    Upgrade to Scala IDE 4.4.0

--- Casks/scala-ide.rb
+++ Casks/scala-ide.rb
@@ -1,14 +1,14 @@
 cask 'scala-ide' do
-  version '4.3.0'
+  version '4.4.0'
 
   if Hardware::CPU.is_32_bit?
     # typesafe.com is the official download host per the vendor homepage
-    url "https://downloads.typesafe.com/scalaide-pack/#{version}-vfinal-luna-211-20150928/scala-SDK-#{version}-vfinal-2.11-macosx.cocoa.x86.zip"
-    sha256 '3f43fef5ccb7f01b6e539c9270e26799361c006b3bfdba2140a9af8e8eafe197'
+    url "http://downloads.typesafe.com/scalaide-pack/#{version}-vfinal-luna-211-20160401/scala-SDK-#{version}-vfinal-2.11-macosx.cocoa.x86.zip"
+    sha256 '271edecd517fe11cb34ca55fb2e654616daeb2cd84a1d7e47d9d622583182012'
   else
     # typesafe.com is the official download host per the vendor homepage
-    url "https://downloads.typesafe.com/scalaide-pack/#{version}-vfinal-luna-211-20151201/scala-SDK-#{version}-vfinal-2.11-macosx.cocoa.x86_64.zip"
-    sha256 '3a7ad459cf2257da2cb8694b47c6fdb30ff3c3bcc020e03636fc49635a476ef6'
+    url "http://downloads.typesafe.com/scalaide-pack/#{version}-vfinal-luna-211-20160401/scala-SDK-#{version}-vfinal-2.11-macosx.cocoa.x86_64.zip"
+    sha256 'c2b8a2d02fb6dd8586427cd171e3e3e8b55cf50afbb126e2bbf936052775e4b8'
   end
 
   name 'Scala IDE'
