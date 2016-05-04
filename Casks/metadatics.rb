a8c0132893e4cca5195aa4d81ee4ad47314bdc98
Victor Popkov <victor@popkov.me>
2016-05-04 23:53:48 +0300

    updated metadatics (1.5.7) (#20996)

--- Casks/metadatics.rb
+++ Casks/metadatics.rb
@@ -1,10 +1,10 @@
 cask 'metadatics' do
-  version '1.5.5.1'
-  sha256 'a9f9cfc137c6e0db6d1886fed67b6ba936e803158fa20682b1907ab77eb44514'
+  version '1.5.7'
+  sha256 'c8baea464a742d35ca0793128d7d2606e59358b7f3e156ac7e7a27694db67927'
 
   url 'http://www.markvapps.com/applications/metadatics/Metadatics.zip'
   appcast 'http://www.markvapps.com/applications/metadatics/metadatics_appcast.xml',
-          checkpoint: '30a574ab51ded0b5aa8d3fb4a211a8980bafb19b601a8e2b17be9cf6e0e9258a'
+          checkpoint: '19bdc4e350aa8e16f5170031425181eb88a465441a12816798533d34b6261814'
   name 'Metadatics'
   homepage 'http://markvapps.com/metadatics'
   license :commercial
