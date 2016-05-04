a33e3508688902dfc2ca9101115345d68ae6d6c3
Karsten Silkenbäumer <ksi@kluks.de>
2016-05-04 20:45:51 +0200

    updated cryptomator (1.0.4) (#20983)

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask 'cryptomator' do
-  version '1.0.3'
-  sha256 'f880cf16920e1d0adffa549514f8f419fda4b0146341aea67cbcea2a4a589690'
+  version '1.0.4'
+  sha256 '4750dadfe9facd7465b07fccdb41b22a77c56da6a721f8b3506183070ea0a228'
 
   # bintray.com/artifact/download/cryptomator was verified as official when first introduced to the cask
   url "http://bintray.com/artifact/download/cryptomator/cryptomator/Cryptomator-#{version}.dmg"
