29a73b3ba90b913d9c80ffb52d53f91f483f1701
Ernesto Navarro <ernesto.navarro@gmail.com>
2016-05-04 23:03:17 +0200

    updated lightworks (12.6.0)

--- Casks/lightworks.rb
+++ Casks/lightworks.rb
@@ -2,7 +2,7 @@ cask 'lightworks' do
   version '12.6.0'
   sha256 'b6d3a534063f12dba90beb63676d2e168facb95fead668b6f06b900c2cbf5279'
 
-  url "https://www.lwks.com/dmpub/lightworks_v#{version}.dmg"
+  url "http://downloads.lwks.com/lightworks_v#{version}.dmg"
   name 'Lightworks'
   homepage 'https://www.lwks.com'
   license :freemium
