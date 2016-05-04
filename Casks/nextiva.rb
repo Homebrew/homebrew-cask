b3fb5e0c7cbf83d1d8934a96531e77cf9d2669d4
Scott Brickner <scottb@brickner.net>
2016-05-03 21:58:23 -0400

    Add Nextiva (21.3.0.1311)

---
+++ Casks/nextiva.rb
@@ -0,0 +1,11 @@
+cask 'nextiva' do
+  version '21.3.0.1311'
+  sha256 '1e5de7747db38e99cdc76e6b6194d24f44078d89eacc88843250a8fa13ce1466'
+
+  url "https://dm.nextiva.com/dms/bc/pc/Nextiva_App.bc-uc.osx-#{version}.dmg"
+  name 'Nextiva'
+  homepage 'https://www.nextiva.com/support/nextiva-app-mac-setup.html'
+  license :closed
+
+  app 'Nextiva App.app'
+end
