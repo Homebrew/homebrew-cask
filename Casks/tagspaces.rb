75587cc397ec839be7d9b1d63f99334c0ab76133
Benjamin Asbach <asbachb@users.noreply.github.com>
2016-05-05 12:31:43 +0200

    removed 32bit version since it's not supported anymore

--- Casks/tagspaces.rb
+++ Casks/tagspaces.rb
@@ -1,15 +1,10 @@
 cask 'tagspaces' do
   version '2.1.1'
 
-  if Hardware::CPU.is_32_bit?
-    sha256 'be88d60769da6af846f8d84037bcbf4917dbef3735c460cfc8727c8c17ace488'
-    url "http://www.tagspaces.org/downloads/tagspaces-#{version}-osx32.zip"
-  else
-    sha256 '912e97396cec629a0ed1bd8e50204bafd699e13c18d927c6ce9489648cf17d41'
-    url "http://www.tagspaces.org/downloads/tagspaces-#{version}-osx64.zip"
-    appcast 'https://github.com/tagspaces/tagspaces/releases.atom',
-            checkpoint: '131248cf9fab7c55e92223fdc1b4ad4eb5b44229f6fdbb133d485d7717f507af'
-  end
+  sha256 '912e97396cec629a0ed1bd8e50204bafd699e13c18d927c6ce9489648cf17d41'
+  url "https://github.com/tagspaces/tagspaces/releases/download/v#{version}/tagspaces-#{version}-osx64.zip"
+  appcast 'https://github.com/tagspaces/tagspaces/releases.atom',
+    checkpoint: '131248cf9fab7c55e92223fdc1b4ad4eb5b44229f6fdbb133d485d7717f507af'
 
   name 'TagSpaces'
   homepage 'http://www.tagspaces.org'
