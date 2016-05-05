a0a1fe500ea35969ecbd002b7b25bde053145709
miccal <miccal.matthews@gmail.com>
2016-05-06 06:41:08 +0800

    updated xquartz (2.7.9) (#21017)

--- Casks/xquartz.rb
+++ Casks/xquartz.rb
@@ -1,9 +1,9 @@
 cask 'xquartz' do
-  version '2.7.8'
-  sha256 '17a4c2da1ab3b676fdf80d1a0714c6bfd22183a604e823b4fd8329fdb4432c2a'
+  version '2.7.9'
+  sha256 'f1a8c055e37f6c508ec471dd94d0aadc0fc69601cbbc97abe78abbb2dbee2f5c'
 
   # bintray.com/xquartz was verified as official when first introduced to the cask
-  url "https://dl.bintray.com/xquartz/legacy-downloads/SL/XQuartz-#{version}.dmg"
+  url "https://dl.bintray.com/xquartz/downloads/XQuartz-#{version}.dmg"
   appcast 'https://xquartz-dl.macosforge.org/sparkle/release.xml',
           checkpoint: 'b0acd11079391e97a302227b526f39a60050b7ccd79f59e232ff6079737f889c'
   name 'XQuartz'
