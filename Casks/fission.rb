56541c48918c6205f19a8c5ef0c18e4f8edfdef3
Chris Nuccitelli <slothdog@air.local>
2016-05-06 16:09:48 -0600

    updated fission (2.3.0)

--- Casks/fission.rb
+++ Casks/fission.rb
@@ -1,10 +1,10 @@
 cask 'fission' do
-  version '1.6.8'
-  sha256 '99e41fd6a8cd5e729ebb5098a80fc9d8dcd7d7ed3a6f2b9d2b6347af2470ab20'
+  version '2.3.0'
+  sha256 'bef0622d1868f443257d537a64f2ff77d4c0b1db81764ea9901821b2f31b1405'
 
-  url "http://rogueamoeba.com/legacy/downloads/Fission-#{version.no_dots}.zip"
-  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Fission',
-          checkpoint: '56a9412e5dffad3e7c87fd2d32c70d7f1a28fa1cc851a6ad8c462bdab3614088'
+  url 'http://rogueamoeba.com/fission/download/Fission.zip'
+  appcast 'http://rogueamoeba.com/fission/releasenotes.php',
+          checkpoint: 'ba8812044685f30aa4e04052d0a3222b731b7f60f818cd25897461f67468760d'
   name 'Fission'
   homepage 'https://rogueamoeba.com/fission/'
   license :commercial
