2b0d83ae6bdaa3862d584d0786c0037e1c515809
MoA <honomoa@users.noreply.github.com>
2016-05-06 09:02:10 +0800

    updated robomongo (0.9.0-rc8) (#21019)

--- Casks/robomongo.rb
+++ Casks/robomongo.rb
@@ -1,6 +1,6 @@
 cask 'robomongo' do
-  version '0.9.0-rc7,2b7a8ca'
-  sha256 '50cb4499796130e62b37f808790c22599059ec0d1d8d14dbc2094a836665828f'
+  version '0.9.0-rc8,c113244'
+  sha256 '30f7666e0e25bfd2504e018221fa7686922aeaf116ee61a06a41f24adf3b5e81'
 
   url "https://download.robomongo.org/#{version.before_comma}/osx/robomongo-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg"
   name 'Robomongo'
