510cbbdeeb6f7363f80ffd99ba7c3c59f2941ed9
Aditya Dalal <adidalal@users.noreply.github.com>
2016-05-07 11:08:17 -0400

    updated radio-silence (2.1) (#21039)

--- Casks/radio-silence.rb
+++ Casks/radio-silence.rb
@@ -1,13 +1,15 @@
 cask 'radio-silence' do
-  version :latest
-  sha256 :no_check
+  version '2.1'
+  sha256 '12194f8729ada834f3d187154e94c157765bdd7ca21d6fc171e5a14671aa8dba'
 
-  url 'http://radiosilenceapp.com/downloads/Radio_Silence_for_OS_X_10.9_and_later.pkg'
+  url "https://radiosilenceapp.com/downloads/Radio_Silence_#{version}.pkg"
+  appcast 'https://radiosilenceapp.com/update',
+          checkpoint: 'b499bb832ce12d4e641ab1d69468dd1ddfc7d3871056a357a06d13610e9d2de8'
   name 'Radio Silence'
   homepage 'http://radiosilenceapp.com'
   license :commercial
 
-  pkg 'Radio_Silence_for_OS_X_10.9_and_later.pkg'
+  pkg "Radio_Silence_#{version}.pkg"
 
   # We intentionally unload the kext twice as a workaround
   # See https://github.com/caskroom/homebrew-cask/pull/1802#issuecomment-34171151
