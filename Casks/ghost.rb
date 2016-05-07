891c47bc5ba7541f41423743c9de02b5219b7481
Jerzerak <jerzerak@gmail.com>
2016-05-06 23:25:18 -0400

    Update ghost (0.4.0) (#21032)

--- Casks/ghost.rb
+++ Casks/ghost.rb
@@ -1,11 +1,11 @@
 cask 'ghost' do
-  version '0.3.2'
-  sha256 '5c167e665a208847ffb5e01bb6e8927c6569bca00997b0d50314ef703cb89afe'
+  version '0.4.0'
+  sha256 '787575d11d10ef7221aa98eca641309e99f7f764fcee5d5caf51c0676f8d994f'
 
   # github.com/TryGhost/Ghost-Desktop was verified as official when first introduced to the cask
   url "https://github.com/TryGhost/Ghost-Desktop/releases/download/v#{version}/ghost-desktop-#{version}-osx.zip"
   appcast 'https://github.com/TryGhost/Ghost-Desktop/releases.atom',
-          checkpoint: '2a175c290a3bce66d51fccf278d8bd697ed876fbdb8380e78cdb410afe62f38f'
+          checkpoint: 'e0239429496de27b9a841dd7676d869e6ad8783d991921a4d64ab5499a90d257'
   name 'Ghost Desktop'
   homepage 'https://ghost.org/downloads'
   license :mit
