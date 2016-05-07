456204d61c352302d0a95e43713c941b660009e1
Victor Popkov <victor@popkov.me>
2016-05-07 23:50:08 +0300

    updated nocturn (1.1.2) (#21051)

--- Casks/nocturn.rb
+++ Casks/nocturn.rb
@@ -1,10 +1,10 @@
 cask 'nocturn' do
-  version '1.1.1'
-  sha256 '3818849494489e312c4dccd545b44624e33f7db90458bd3c2535c3b7971991e0'
+  version '1.1.2'
+  sha256 '9eb96dc4f999f2b33d62a05638ccc665418e330222b97d0d85eb6c677c023c17'
 
   url "https://github.com/k0kubun/Nocturn/releases/download/v#{version}/Nocturn-darwin-x64.zip"
   appcast 'https://github.com/k0kubun/Nocturn/releases.atom',
-          checkpoint: '1165bb4b926f0d7271545062a0785d462b547212c68d7e961d69b8ed3281619b'
+          checkpoint: '58e68e03917fca9073c983eff37442d5a01dddbaf22e49bd3bba1846037bb8a7'
   name 'Nocturn'
   homepage 'https://github.com/k0kubun/Nocturn'
   license :mit
