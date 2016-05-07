b02b66317240ccbd5ba677fbd5020acd790d4386
Aditya Dalal <adidalal@users.noreply.github.com>
2016-05-07 01:36:51 -0400

    airfoil: change back to official URL

--- Casks/airfoil.rb
+++ Casks/airfoil.rb
@@ -2,8 +2,7 @@ cask 'airfoil' do
   version '5.0.2'
   sha256 '87aebc5284eaced85a3441efc57d1d4a51dd65dc00b068c6e21988c776f1f6d9'
 
-  # d2oxtzozd38ts8.cloudfront.net was verified as official when first introduced to the cask
-  url 'https://d2oxtzozd38ts8.cloudfront.net/airfoil/download/Airfoil.zip'
+  url 'https://rogueamoeba.com/airfoil/download/Airfoil.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Airfoil&platform=osx',
           checkpoint: 'aaf36799bceac10c5b282aed1c7eec0c25606f236e27243b19b196f1be85b74d'
   name 'Airfoil'
