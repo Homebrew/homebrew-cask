50d8770b12c44ece3ef61498f990705332ec382d
Aditya Dalal <adidalal@users.noreply.github.com>
2016-05-07 01:44:46 -0400

    fission: update appcast URL

--- Casks/fission.rb
+++ Casks/fission.rb
@@ -2,9 +2,9 @@ cask 'fission' do
   version '2.3.0'
   sha256 'bef0622d1868f443257d537a64f2ff77d4c0b1db81764ea9901821b2f31b1405'
 
-  url 'http://rogueamoeba.com/fission/download/Fission.zip'
-  appcast 'http://rogueamoeba.com/fission/releasenotes.php',
-          checkpoint: 'ba8812044685f30aa4e04052d0a3222b731b7f60f818cd25897461f67468760d'
+  url 'https://rogueamoeba.com/fission/download/Fission.zip'
+  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Fission&version=2000000',
+          checkpoint: 'a2734a4521c4b7f8a41af8407445a129a993152cc69c5c9cbd096be0060d41ab'
   name 'Fission'
   homepage 'https://rogueamoeba.com/fission/'
   license :commercial
