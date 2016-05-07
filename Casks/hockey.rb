ee5a2711feb49f7675320f005407a2e111fb04c5
Victor Popkov <victor@popkov.me>
2016-05-07 23:35:00 +0300

    updated hockey (2.0.16)

--- Casks/hockey.rb
+++ Casks/hockey.rb
@@ -1,10 +1,10 @@
 cask 'hockey' do
-  version '2.0.15'
-  sha256 'f2a5f3e1b6ff047dc030e78a93572e54d6afb2901c190b1841489e1289dc22d3'
+  version '2.0.16'
+  sha256 '782b2355341251989b28021d07c28c44ac3d9c388547fc5a06b777bbd4a536d6'
 
   url 'https://rink.hockeyapp.net/api/2/apps/67503a7926431872c4b6c1549f5bd6b1/app_versions/396?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/67503a7926431872c4b6c1549f5bd6b1',
-          checkpoint: '924c8019247e3ab7c5c039aefa46eaff3353473ec3d3760650a39c62c6f20781'
+          checkpoint: '8802b612ed27a271b0c50c4b56c4882c6b24a9914b39d7b6058ec8b40ffc943f'
   name 'HockeyApp'
   homepage 'http://hockeyapp.net/releases/mac/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
