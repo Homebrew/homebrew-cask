22d199319e49a1b9ba3823af416e74e13a1aa75a
Pierre-Antoine Delnatte <info@flatland.be>
2016-05-08 14:23:54 +0200

    updated teseve (0.5.0) (#21061)

--- Casks/teseve.rb
+++ Casks/teseve.rb
@@ -1,11 +1,11 @@
 cask 'teseve' do
-  version '0.4.0'
-  sha256 'd5f1713b6f3155b46b4552bcfd777175b293990c0280bee1ebf4675be423fa3b'
+  version '0.5.0'
+  sha256 'cd92eea036b83fa4733ec43a1ce69f3f3a562c153d9841348bdb5e75d43cdb4d'
 
   # github.com/teseve/teseve was verified as official when first introduced to the cask
   url "https://github.com/teseve/teseve/releases/download/#{version}/Teseve-#{version}-mac-x64.zip"
   appcast 'https://github.com/teseve/teseve/releases.atom',
-          checkpoint: '4824632ace63e44e8b87947b761015f9911165f4204ccd61bdbf34608afa9635'
+          checkpoint: 'ee303125528b6dce68a182b0cc5e5b08a97907f39c46cd6da11bb41a0cd0a205'
   name 'Tèsèvè'
   homepage 'http://teseve.github.io/'
   license :public_domain
