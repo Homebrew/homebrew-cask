93c6be1b786fb5066a0a919d6cfc86a2f36e34db
Tom Janson <priv.tom.janson@gmail.com>
2016-05-09 00:02:42 +0200

    updated keyboard-maestro (7.1.1) (#21063)

--- Casks/keyboard-maestro.rb
+++ Casks/keyboard-maestro.rb
@@ -1,6 +1,6 @@
 cask 'keyboard-maestro' do
-  version '7.1'
-  sha256 'ef3f422e594f6cffa09284d6672c2c0cf6067eeff8f38f7be9c2922fc4c5a7e7'
+  version '7.1.1'
+  sha256 '332525f85f6cb4e487edd6edf16b87855d2907d367bd806efda2e3bc0326422e'
 
   # stairways.com is the official download host per the vendor homepage
   url "https://files.stairways.com/keyboardmaestro-#{version.delete('.')}.zip"
