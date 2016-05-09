53f75f3a546a228184e248535e4958a8e03eea40
Aditya Dalal <adidalal@users.noreply.github.com>
2016-05-09 19:33:04 -0400

    updated beatunes (4.6.0)

--- Casks/beatunes.rb
+++ Casks/beatunes.rb
@@ -1,8 +1,8 @@
 cask 'beatunes' do
-  version '4.0.26'
-  sha256 '18ccbdf745c6b062fb41c4ebb08038e122aaef466946d55d38ec5a2152251515'
+  version '4.6.0'
+  sha256 'ba84ef2e7f1a2dabb96cc27b4bba286c9f0e209812599c54973b9ec8cf97176c'
 
-  url "http://coxy.beatunes.com/download/beaTunes-#{version.gsub('.', '-')}-osx.dmg"
+  url "http://coxy.beatunes.com/download/beaTunes-#{version.dots_to_hyphens}.dmg"
   name 'beaTunes'
   homepage 'https://www.beatunes.com'
   license :closed
@@ -10,5 +10,5 @@ cask 'beatunes' do
   depends_on macos: '>= :lion'
   depends_on arch: :x86_64
 
-  app "beaTunes#{version.to_i}.app"
+  app "beaTunes#{version.major}.app"
 end
