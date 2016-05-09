260c13bdcaf93b4bc894cdb7467e7a405ea2fbc9
Lukas Drbal <lukas.drbal@merck.com>
2016-05-09 17:45:21 +0200

    Added ODBC driver for Hive from Hortoworks

---
+++ Casks/hortonworks-hive-odbc.rb
@@ -0,0 +1,19 @@
+cask 'hortonworks-hive-odbc' do
+  version '2.1.2'
+  sha256 'eca2b3fc4a1f220f8fc34ad00b9a49a85703204a22dc5451dfb61dc792090fff'
+
+  url "http://public-repo-1.hortonworks.com/HDP/hive-odbc/#{version}.1002/Mac_OS_X/hive-odbc-native.dmg"
+  name 'Hortoworks ODBC Driver for Hive'
+  homepage 'http://www.hortoworks.com'
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  pkg 'hive-odbc-native.pkg'
+
+  uninstall pkgutil: 'hortonworks.hiveodbc'
+
+  caveats <<-EOS.undent
+    For configuration instructions, see
+
+      /opt/hortonworks/hiveodbc/Readme.txt
+  EOS
+end
