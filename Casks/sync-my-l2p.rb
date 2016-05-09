39f255559c71fb6aa5acbd1d39077dd8b869cc41
SirWindfield <svenforgithub@gmail.com>
2016-05-09 23:35:22 +0200

    Fix download URL to use version field Add appcast and checkpoint

--- Casks/sync-my-l2p.rb
+++ Casks/sync-my-l2p.rb
@@ -2,7 +2,10 @@ cask 'sync-my-l2p' do
   version '2.2.0'
   sha256 '76344b27cda005a3e60135d880f482eef08bc0f1db84480e39677dd230823886'
 
-  url 'https://github.com/Sync-my-L2P/Sync-my-L2P/releases/download/v2.2.0/SyncMyL2P-2.2.0-osx.dmg'
+  # github.com/Sync-my-L2P/Sync-my-L2P was verified as official when first introduced to the cask
+  url "https://github.com/Sync-my-L2P/Sync-my-L2P/releases/download/v#{version}/SyncMyL2P-#{version}-osx.dmg"
+  appcast 'https://github.com/Sync-my-L2P/Sync-my-L2P/releases.atom',
+          checkpoint: '131d35c756edf86c9a5197973c41651749402e4a948699a98d1d7ff8bae6b3c2'
   name 'Sync-my-L2P'
   homepage 'http://www.sync-my-l2p.de'
   license :oss
