00c229ed74b2047b25b327d3609c0c67a8edd29b
Steven Ametjan <merlin903@users.noreply.github.com>
2016-05-09 22:33:46 -0700

    updated plex-media-player (1.1.1.293-cc2cc067) (#21084)

--- Casks/plex-media-player.rb
+++ Casks/plex-media-player.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-player' do
-  version '1.0.6.229-1ce41570'
-  sha256 '7bb4a6a25da551753f859487c9823687cd47f73152e53ece44c25e135afe2499'
+  version '1.1.1.293-cc2cc067'
+  sha256 '265f67ba311a11db672286797023ceea34eb205a67ca53b17a41fee84f9c5b75'
 
   url "https://downloads.plex.tv/plexmediaplayer/#{version}/PlexMediaPlayer-#{version}-macosx-x86_64.zip"
   name 'Plex Media Player'
