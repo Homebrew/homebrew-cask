51e51759a7b0748ac5478e5e9ef6ea7201a3fdae
Di Xu <dixudx@users.noreply.github.com>
2016-05-10 15:48:09 +0800

    updated downie (2.3.1225) (#21085)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,10 +1,10 @@
 cask 'downie' do
-  version '2.3.1222'
-  sha256 'fd31564706c422308074fa7961d76047b5eae3276cbc107d90d934a964ad73fd'
+  version '2.3.1225'
+  sha256 'a1b78160e316a4df97c2098a84676ccc78ccbd72c0b326137aea3d2591866ab0'
 
   url "http://trial.charliemonroe.net/downie/Downie_#{version.patch}.zip"
   appcast "http://trial.charliemonroe.net/downie/updates_#{version.major_minor}.xml",
-          checkpoint: '3ed0de120361da341e4a2a10875e001094a1be61c30a9f82d11eee555fe8c5b0'
+          checkpoint: '3cf70765eb1df94e772f0c96d756bbd79c58aa44fba0bd57c3b486eafe66e61d'
   name 'Downie'
   homepage 'http://software.charliemonroe.net/downie.php'
   license :commercial
