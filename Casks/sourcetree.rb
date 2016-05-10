95b8bdf2dec3c302cfa0c1e55ea75a5431521cb5
Karsten Silkenbäumer <ksi@kluks.de>
2016-05-11 01:50:06 +0200

    updated sourcetree (2.3)

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -6,12 +6,12 @@ cask 'sourcetree' do
     version '2.0.5.5'
     sha256 'f23129587703a706a37d5fdd9b2390875305b482a2b4e4b0e34bd49cba9b63c9'
   else
-    version '2.2.4'
-    sha256 '068b594ca1612a08c575545baa363741a1024b67e3d3a86747c84f52a0c4e032'
+    version '2.3'
+    sha256 '944ea6157c76c10b6745e26d2531235e34fd8f400cbdc71818045583b3adf13c'
   end
 
   # atlassian.com was verified as official when first introduced to the cask
-  url "https://downloads.atlassian.com/software/sourcetree/SourceTree_#{version}.dmg"
+  url "https://downloads.atlassian.com/software/sourcetree/SourceTree_#{version}.zip"
   appcast 'https://www.sourcetreeapp.com/update/SparkleAppcast.xml',
           checkpoint: '281824c0f503c7db534e0ba7684606ab2e3810787154f91096bcf203d344596b'
   name 'Atlassian SourceTree'
