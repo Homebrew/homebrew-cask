b5aaa288a473aad3be445f1b6bfc30ec78eaed8d
Justin Hall <justin.r.hall@gmail.com>
2016-05-10 16:26:09 -0600

    Upgrade vmware-horizon-client to 4.0.1-3708485 (#21106)

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -1,6 +1,6 @@
 cask 'vmware-horizon-client' do
-  version '3.5.2-3151577'
-  sha256 '52254a1203706ed8a7700b888f9d88b0754ff053698d21313d4f7683fcfff48c'
+  version '4.0.1-3708485'
+  sha256 '28d5a75194b57754391ebecc08311b3c81a587b9ed22a64685c1e0a51c136e48'
 
   url "https://download3.vmware.com/software/view/viewclients/CART15Q4_3/VMware-Horizon-Client-#{version}.dmg"
   name 'VMware Horizon Client'
