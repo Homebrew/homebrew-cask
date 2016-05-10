5d3951efefdebb1a1d3e3bc574a7a33665a8fb54
Victor Popkov <victor@popkov.me>
2016-05-10 23:25:04 +0300

    updated clamxav (2.8.9.3)

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -6,11 +6,11 @@ cask 'clamxav' do
     version '2.5.1'
     sha256 '02a7529c74d11724e2d0e8226ac83a0d3cfb599afb354d02f6609632d69d9eb1'
   else
-    version '2.8.9.2'
-    sha256 '2b08b617561ba91a7ec329205a0bbdb204c16f8fd520fab3e96e95bc602c50f5'
+    version '2.8.9.3'
+    sha256 '06cfdd93a27f6a58dd30f2e268761e2956eedb88af120b7932f1d73442e00526'
 
     appcast 'https://www.clamxav.com/sparkle/appcast.xml',
-            checkpoint: '3ce536cc2a59eb67325e564279eedc747111b2c8774874b5a6ecceb6319c5fdd'
+            checkpoint: '3174407536e67a24c265ba98e7c1e6fe78558ef28ce2e3defde2c30bac1f6270'
   end
 
   url "https://www.clamxav.com/downloads/ClamXav_#{version}.dmg"
