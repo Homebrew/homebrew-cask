42f6ce72f8cc2dc77216d6ac32281df85dbc8f42
John Sockwell <jsockwell@spies.ws>
2016-05-09 21:14:59 -0400

    Add kk7ds-python-runtime v10.1 (#21077)
    The KK7DS Python Runtime is required by CHIRP for programming radios.

---
+++ Casks/kk7ds-python-runtime.rb
@@ -0,0 +1,14 @@
+cask 'kk7ds-python-runtime' do
+  version '10.1'
+  sha256 '5cee8acb941e39f93a4df6a99ed29a14c48da0bc5beb3b31068852b1fad8b009'
+
+  url "http://www.d-rats.com/download/OSX_Runtime/KK7DS_Python_Runtime_R#{version.major}.pkg"
+  name 'KK7DS Python Runtime'
+  homepage 'http://www.d-rats.com/download/OSX_Runtime/'
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  pkg "KK7DS_Python_Runtime_R#{version.major}.pkg"
+
+  uninstall pkgutil: 'com.danplanet.python_runtime',
+            delete:  '/opt/kk7ds'
+end
