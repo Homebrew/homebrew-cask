adb23a55b0d560a4deef7f01d52d4fbf1a751fa6
Yurii Kolesnykov <yurikoles@gmail.com>
2016-05-10 22:05:18 +0300

    updated epic-games-launcher (2.11.9-2970807) (#21091)

--- Casks/epic-games-launcher.rb
+++ Casks/epic-games-launcher.rb
@@ -1,6 +1,6 @@
 cask 'epic-games-launcher' do
-  version '2.11.0-2904489'
-  sha256 '807348dc48bbbca7e46504035e9e0ceb7f36f2b3eba91e0a521acc0b87ea0c19'
+  version '2.11.9-2970807'
+  sha256 'cf73e0ae7ecd03c415a7134984ad7b0775127cb490c71744d674ef1d2090c372'
 
   url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicGamesLauncher-#{version}.dmg"
   name 'Epic Games Launcher'
