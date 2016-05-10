ee08404dc119d54313730c6e05159af433351df8
Victor Popkov <victor@popkov.me>
2016-05-11 00:06:00 +0300

    updated dropshare (4-4217) (#21099)

--- Casks/dropshare.rb
+++ Casks/dropshare.rb
@@ -1,11 +1,11 @@
 cask 'dropshare' do
-  version '4-4118'
-  sha256 '1488eaa5354ff8d8cc30ea97e5de28183e835aa644fad61e457baee0e9853219'
+  version '4-4217'
+  sha256 '53220d5594eb007d52232bcae4b2e3eead06c8cf16d6e8dd009a647d40936152'
 
   # d2wvuuix8c9e48.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2wvuuix8c9e48.cloudfront.net/Dropshare#{version}.app.zip"
   appcast 'https://getdropsha.re/sparkle/Dropshare4.xml',
-          checkpoint: '3a91ee0fe4f7c602fc7ee8729991ae5158446cafa1c6088b94397a4f2bb9e99c'
+          checkpoint: '6d88c460423ed3615a00b5c5761626265fe544b8b37635ffaf052340c712cea0'
   name 'Dropshare'
   homepage 'https://getdropsha.re/'
   license :commercial
