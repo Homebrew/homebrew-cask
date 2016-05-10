73c3744d1ce54cb5248430a4cd465d786b136ad3
Victor Popkov <victor@popkov.me>
2016-05-10 23:41:52 +0300

    updated trailer (1.3.20)

--- Casks/trailer.rb
+++ Casks/trailer.rb
@@ -1,10 +1,10 @@
 cask 'trailer' do
-  version '1.3.19'
-  sha256 '838d0c9245e4672584bc2e7a1044f86bd17409793491ce1f142a2784943d6128'
+  version '1.3.20'
+  sha256 '17d49464f6c4636b0c58c69e4b16f0132fb2e2a2cc3164f50a9ed57a4d54a890'
 
   url "https://ptsochantaris.github.io/trailer/trailer#{version.no_dots}.zip"
   appcast 'https://ptsochantaris.github.io/trailer/appcast.xml',
-          checkpoint: '4a2bf008f35c5dca70d840d1e05ac1fae8e079b1797dbca9c7c2a63a5faf2021'
+          checkpoint: 'ac98df74286154a41c934ddd5b8012683a6f3cf250937a73a932593adb9c2f48'
   name 'Trailer'
   homepage 'https://ptsochantaris.github.io/trailer/'
   license :mit
