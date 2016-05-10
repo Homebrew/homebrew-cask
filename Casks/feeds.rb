3a3210cc70a2c65cd58d3704425cc3c5da6f1639
Victor Popkov <victor@popkov.me>
2016-05-10 23:27:46 +0300

    updated feeds (2.1.2)

--- Casks/feeds.rb
+++ Casks/feeds.rb
@@ -1,11 +1,11 @@
 cask 'feeds' do
-  version '2.1.1'
-  sha256 '098dc0ceec2ca1ef26e6ff9f3a7ab4fd7c71877a7fc698584c986525a2204fdf'
+  version '2.1.2'
+  sha256 'a9b1476dae52fef2abc9f8427c106412ca726b873c469ae23553818660eb7fdc'
 
   # storage.googleapis.com/feeds-releases was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/feeds-releases/Feeds-#{version}.zip"
   appcast 'https://storage.googleapis.com/feeds-releases/appcast.xml',
-          checkpoint: '7af76d6014097647a2c5bf89bf8fc50019bd86c68c3eea60288eaf20c1a5c7f5'
+          checkpoint: '4450c36d30006bb4b85fa047d9062a3ce22d3d170824eea73db3db01acb0a880'
   name 'Feeds'
   homepage 'http://www.feedsapp.com/'
   license :mit
