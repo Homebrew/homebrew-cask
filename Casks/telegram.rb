8c95a088e63f47d4683d6c71ec333d5878f56233
inailuig <inailuig@users.noreply.github.com>
2016-05-11 13:25:38 +0200

    updated telegram (0.9.48) (#21116)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '0.9.44'
-  sha256 'adc458677c4d7f6a80087e5f7025c972348ee01a766fbc6e9251da930e35fe68'
+  version '0.9.48'
+  sha256 '34fd43368f13507f954ed42a382f28305e38f2a6643c90d621519184b44399c4'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
