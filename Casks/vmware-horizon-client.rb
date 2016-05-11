b22d001910bc05823b2a3e2aa11b38a39eb76ad8
Justin Hall <justin.r.hall@gmail.com>
2016-05-11 11:00:25 -0600

    Fix vmware-horizon-client 4.0.1-3708485 download link (#21123)

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -2,7 +2,7 @@ cask 'vmware-horizon-client' do
   version '4.0.1-3708485'
   sha256 '28d5a75194b57754391ebecc08311b3c81a587b9ed22a64685c1e0a51c136e48'
 
-  url "https://download3.vmware.com/software/view/viewclients/CART15Q4_3/VMware-Horizon-Client-#{version}.dmg"
+  url "https://download3.vmware.com/software/view/viewclients/CART16Q1_2/VMware-Horizon-Client-#{version}.dmg"
   name 'VMware Horizon Client'
   homepage 'https://www.vmware.com/'
   license :gratis
