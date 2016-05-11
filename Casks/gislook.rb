962f2880c5ebe59e308828b5adde6490aaf5e973
Jeffrey Munowitch <jmunowitch@fargeo.com>
2016-05-11 13:37:27 -0700

    gislook - update sha256 value

--- Casks/gislook.rb
+++ Casks/gislook.rb
@@ -1,6 +1,6 @@
 cask 'gislook' do
-  version :1.0.2
-  sha256 :no_check
+  version '1.0.2'
+  sha256 'ac1e8a48bce216b17d573783a647cac2fb06a20c6c002ea45f7439341a0a10a1'
 
   url 'http://cartography.oregonstate.edu/gislook/GISLook_GISMeta.pkg'
   name 'GISLook'
