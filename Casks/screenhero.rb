c42926a6e68c71abb74a74bab46965fed505d5fd
Vítor Galvão <info@vitorgalvao.com>
2016-05-11 15:43:58 -0400

    updated screenhero (2.3.8.0) (#21137)

--- Casks/screenhero.rb
+++ Casks/screenhero.rb
@@ -1,10 +1,10 @@
 cask 'screenhero' do
-  version '2.3.7.0'
-  sha256 '7f642ca6294c136192d4afbc52bf95f843b4c10749b2ee6dc39cf871c49f125a'
+  version '2.3.8.0'
+  sha256 '4a6dde7b647afb898680a4456908f32769b31d8ac36b290916427981b4bc76ec'
 
   url "https://secure.screenhero.com/update/screenhero/Screenhero-#{version}.dmg"
   appcast 'https://d3hb26arjl8wb7.cloudfront.net/jsherwani/public/update/mac/screenhero/sparkle.xml',
-          checkpoint: 'deae936563576f81f73b4ab3e74b3843a71dd5cdf50b8540632ea2520c1263b7'
+          checkpoint: '324493ad3c8a18b98321f7286781c7b907910c53aaf459f88f5194c4897606ca'
   name 'Screenhero'
   homepage 'https://screenhero.com/'
   license :commercial
