a0bdb443dc470e7a83275b43fe24d793d7deb344
Hervé Yviquel <hyviquel@gmail.com>
2016-05-11 11:01:08 -0300

    updated qt-creator (4.0.0) (#21121)

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator' do
-  version '3.6.1'
-  sha256 '0e4806e69105d141f767908694d0c59739fb55bfd01f5e72b83a6abad87203ae'
+  version '4.0.0'
+  sha256 'd470f00c944cf7d8c69af99386dd1b11e912c9fb5b3302cf83a5549f21abfdc6'
 
   url "http://download.qt.io/official_releases/qtcreator/#{version.to_f}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
