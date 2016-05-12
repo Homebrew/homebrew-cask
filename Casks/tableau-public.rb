1f8addeefc76560df756f68173b45be01277fe35
Vítor Galvão <info@vitorgalvao.com>
2016-05-12 00:23:27 -0400

    updated tableau-public (9.3.0)

--- Casks/tableau-public.rb
+++ Casks/tableau-public.rb
@@ -1,10 +1,10 @@
 cask 'tableau-public' do
-  version :latest
-  sha256 :no_check
+  version '9.3.0'
+  sha256 '73f82fb7eac3f99a52ed53f48457b031db2d38b9cf1a30462dffa655a4618012'
 
-  url 'https://downloads.tableausoftware.com/public/TableauPublic.dmg'
+  url "https://downloads.tableau.com/public/TableauPublic-#{version.dots_to_hyphens}.dmg"
   name 'Tableau Public'
-  homepage 'https://www.tableausoftware.com/public'
+  homepage 'https://public.tableau.com/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Tableau Public.app'
