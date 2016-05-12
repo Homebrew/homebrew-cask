6f246014ec50ae515b35f7fb48d356b1d0c47687
Andy Li <andy@onthewings.net>
2016-05-12 16:23:45 +0800

    flash, flash-player, and flash-player-debugger 21.0.0.242

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger' do
-  version '21.0.0.213'
-  sha256 '892ceaccb40dc69e49ae0ec4922e6108555d5a28b2ffec2d9e51e0a1e5b3aab2'
+  version '21.0.0.242'
+  sha256 '322a6b4cd5920726b12b40c2d28b116e07c6a691ae5926b202b17cfb391f48a4'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.to_i}/flashplayer_#{version.to_i}_sa_debug.dmg"
