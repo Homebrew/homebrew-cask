a08a5ee6765d91f1b1cbe26b751dbdf2719436a4
Victor Popkov <victor@popkov.me>
2016-05-12 22:43:46 +0300

    updated propresenter (6.0.19_b15114) (#21170)

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -1,10 +1,10 @@
 cask 'propresenter' do
-  version '6.0.18_b15107'
-  sha256 '16b935c1902626a5711604bf96bedbeccb859b9b4a7ce9e8ce57ff586fe906eb'
+  version '6.0.19_b15114'
+  sha256 'c3749e53f8bbb41e7b56e43b0e5c7be25fe6a37be264a016f66437a08126d7c4'
 
   url "https://www.renewedvision.com/downloads/ProPresenter6_#{version}.dmg"
   appcast 'https://www.renewedvision.com/update/ProPresenter6.php',
-          checkpoint: '3838dbec375e3bc56a26ba2340a5ec4a278c302b92b0c54f3c02e9333020f3a2'
+          checkpoint: '14e44977ad86b9ecbddfc1062aa36fc5f54ca2795fb626dd0d4c6abf40e51b5b'
   name 'ProPresenter'
   homepage 'https://www.renewedvision.com/propresenter.php'
   license :commercial
