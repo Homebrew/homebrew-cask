30ceea5641382f2dcc9ac9a52e6ac10a4c5ed032
Victor Popkov <victor@popkov.me>
2016-05-12 19:56:49 +0300

    updated cashculator (1.3.7)

--- Casks/cashculator.rb
+++ Casks/cashculator.rb
@@ -1,10 +1,10 @@
 cask 'cashculator' do
-  version '1.3.6'
-  sha256 '6b7985e902b79ea444ba911d7f4c688b1c059a98c893c5c5dbe3de0a319233bd'
+  version '1.3.7'
+  sha256 '81b9be9131bcabd87a2c740028e5511b2f3db25abc15acc401cf7df62df12e30'
 
   url "http://dl.apparentsoft.com/Cashculator_#{version}.dmg"
   appcast 'http://dl.apparentsoft.com/cashculator.rss',
-          checkpoint: '56b9baae5a54c6fd4d83472ab3f33a9f2a7d555bd6b18bff4dbb3502962fbd30'
+          checkpoint: 'c5f684fdba54e32ba054459ec44f052bc76db4943bd284086516f351f50f902d'
   name 'Cashculator'
   homepage 'http://www.apparentsoft.com/cashculator'
   license :freemium
