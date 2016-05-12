1f3f4a98f906a3cc5f389656c74e992e418bbc96
sudosun <ipservisman@gmail.com>
2016-05-12 11:51:15 +0600

    Update opera.rb
    Update opera url & sha

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
   version '37.0.2178.43'
-  sha256 '3c1165e5604fb93079615501c47e8df41d8efa8ee52f236bb4dfab0a44aee409'
+  sha256 '0facd8ad0003fe7c9f6115ebb6b1d6354c2ba477164c229b19ea61464170e476'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
