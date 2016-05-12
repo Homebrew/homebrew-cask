a15c5125c46bff6970955873c7813219ed2c65e4
Victor Popkov <victor@popkov.me>
2016-05-12 21:31:13 +0300

    updated chronicle (6.0.1) (#21165)

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -1,10 +1,10 @@
 cask 'chronicle' do
-  version '5.7.0'
-  sha256 '1171b77577cdfabc5cf1a140ba909a0697fcb157fc0ba2a0a8fca7ed639ab687'
+  version '6.0.1'
+  sha256 '3e3b57158d701fafe7e75747a45b31e1ac3ae8519b8136b6aa9ce5acd5da10e1'
 
   url 'http://chronicleapp.com/static/downloads/chronicle.zip'
   appcast 'http://www.littlefin.com/downloads/chronicle3.xml',
-          checkpoint: '143465ba0dda54d7de226e50fe24966c6faed507cf3dde641ffc25d809a89567'
+          checkpoint: '72d729bd9e17b839fd2f74c2f741cc02ada06e6c427e59135954c42bff0b89f5'
   name 'Chronicle'
   homepage 'http://chronicleapp.com/'
   license :commercial
