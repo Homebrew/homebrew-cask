482a9ca5964211bfb66794954241eb92cbd94418
Tommy Sparber <tsparber@users.noreply.github.com>
2016-05-13 15:45:50 +0200

    updated flash (21.0.0.242) (#21187)

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -5,7 +5,7 @@ cask 'flash' do
   # macromedia.com was verified as official when first introduced to the cask
   url "http://fpdownload.macromedia.com/get/flashplayer/pdc/#{version}/install_flash_player_osx.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
-          checkpoint: 'ea4cd9e1fae84e72772ddec64f7fe85a22b7a89f12f7d45f450e50d830078d06'
+          checkpoint: '3a7ddc4c641926867aa42c761bbd080df6083003861b2012f021fe0fd03d2a1d'
   name 'Adobe Flash Player'
   homepage 'https://get.adobe.com/flashplayer'
   license :gratis
