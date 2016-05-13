becb58e68f016ad08bba6e4bbd97c8fc46f4c4ad
Ernesto Navarro <ernesto.navarro@gmail.com>
2016-05-13 09:42:03 +0200

    updated mactracker (7.5.5)

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,10 +1,10 @@
 cask 'mactracker' do
   version '7.5.5'
-  sha256 '9febbad5da43f601983ee93a3900ed63de95632b1dde1a4516a334060d9da763'
+  sha256 '9eabc8dcc9eb86bed6ddf9b6d3867b69a5fb5fc57c5111b741ec7f98f7019731'
 
   url "http://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast 'http://update.mactracker.ca/appcast-b.xml',
-          checkpoint: '37639fbdfd36fe9a4341dc08c919db3ac1cfaee4d54461669fd45acb25b8e99d'
+          checkpoint: 'ca0c3fae4628bfa2ea2dea2f685bd67c70638eb0214dfd1e3735753a0034aea0'
   name 'Mactracker'
   homepage 'http://mactracker.ca/'
   license :gratis
