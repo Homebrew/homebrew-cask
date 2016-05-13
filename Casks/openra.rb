d5c71b21513cfc37f97c30976610f73013458313
Johannes <jojow@users.noreply.github.com>
2016-05-13 16:43:43 +0200

    OpenRA updated to 20160508 (#21181)

--- Casks/openra.rb
+++ Casks/openra.rb
@@ -1,13 +1,13 @@
 cask 'openra' do
-  version '20151224'
-  sha256 '2c3f43839b6626e5c739d28c332e9f3b03c0ef4b0fd0c76980f3c55044c90dbb'
+  version '20160508'
+  sha256 '2992357757c8882ebda97ae5239a4e8e435e06c58575f4230de45647414c6bd1'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/OpenRA/OpenRA/releases/download/release-#{version}/OpenRA-release-#{version}.zip"
   appcast 'https://github.com/OpenRA/OpenRA/releases.atom',
-          checkpoint: '6375065250a8ecd493ac7e19b37e60c18f57c28883a74458455fe751ac155bf0'
+          checkpoint: 'e6787d8d0cafc3da4c192de3f532e331173134ac2cb043ba67be65e09cb66462'
   name 'OpenRA'
-  homepage 'http://www.openra.net/'
+  homepage 'http://www.openra.net'
   license :gpl
 
   depends_on cask: 'mono-mdk'
