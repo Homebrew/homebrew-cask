e54b8947c3942748101fd1cffe3ddb46f54a9e20
Jerzerak <jerzerak@gmail.com>
2016-05-13 18:25:40 -0400

    trailer 1.3.21 (#21195)

--- Casks/trailer.rb
+++ Casks/trailer.rb
@@ -1,13 +1,19 @@
 cask 'trailer' do
-  version '1.3.20'
-  sha256 '17d49464f6c4636b0c58c69e4b16f0132fb2e2a2cc3164f50a9ed57a4d54a890'
+  version '1.3.21'
+  sha256 '8243e1509146b38b76d5d71772936cf1665367b884bf814f2200f96a903600c1'
 
   url "https://ptsochantaris.github.io/trailer/trailer#{version.no_dots}.zip"
   appcast 'https://ptsochantaris.github.io/trailer/appcast.xml',
-          checkpoint: 'ac98df74286154a41c934ddd5b8012683a6f3cf250937a73a932593adb9c2f48'
+          checkpoint: '6f675e6432be10165ed8fc78692c79bc4c291e34215e44da0ef59f692ba2893c'
   name 'Trailer'
   homepage 'https://ptsochantaris.github.io/trailer/'
   license :mit
 
   app 'Trailer.app'
+
+  zap delete: [
+                '~/Library/Application Support/com.housetrip.Trailer',
+                '~/Library/Caches/com.housetrip.Trailer',
+                '~/Library/Preferences/com.housetrip.Trailer.plist',
+              ]
 end
