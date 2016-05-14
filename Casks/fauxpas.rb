ef37735fc83333958df93243c716074a566518f2
Victor Popkov <victor@popkov.me>
2016-05-14 23:00:20 +0300

    updated fauxpas (1.6.2) (#21216)

--- Casks/fauxpas.rb
+++ Casks/fauxpas.rb
@@ -1,10 +1,10 @@
 cask 'fauxpas' do
-  version '1.6.1'
-  sha256 '8fa9f4e02e58f1916730dea2c074775af2c7b873328f7a61cda31d91d2817e45'
+  version '1.6.2'
+  sha256 '2723b5a420d2ec37debe1511ef058370f356ec3decfb6bd74c10e28de192028a'
 
   url "http://files.fauxpasapp.com/FauxPas-#{version}.tar.bz2"
   appcast 'http://files.fauxpasapp.com/appcast.xml',
-          checkpoint: '30c973f4af6720f24f73003d2d4acfda48c20263b45159edac57df8cc92c0a26'
+          checkpoint: '4ed08adb98070de7a9889ed57106241b073b01fb107d2a22adfd86e995b1feb9'
   name 'Faux Pas'
   homepage 'http://fauxpasapp.com'
   license :commercial
