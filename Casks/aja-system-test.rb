d39e72740dda32f559d4e6b6418a58a5cabba796
Timothy Sutton <tim@synthist.net>
2016-05-14 10:53:54 -0400

    Use interpolated version

--- Casks/aja-system-test.rb
+++ Casks/aja-system-test.rb
@@ -1,8 +1,8 @@
 cask 'aja-system-test' do
-  version '2.1'
+  version '2.1.0'
   sha256 '53fa38382d73a0e70a88f5f23fdb384612fd57dd8ae1bb28fe49a7896b321bac'
 
-  url 'https://www.aja.com/assets/products/files/90/AJA_SystemTest-2.1.0.app.zip'
+  url "https://www.aja.com/assets/products/files/90/AJA_SystemTest-#{version}.app.zip"
   name 'AJA System Test'
   homepage 'https://www.aja.com/en/products/aja-system-test'
   license :gratis
