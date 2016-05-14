d272f8938fce401a357ca797c0ac7de239e3a99f
Jerzerak <jerzerak@gmail.com>
2016-05-14 16:08:37 -0400

    electron 1.1.0 (#21209)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '1.0.2'
-  sha256 'eed3bc4c56ed25bb2b57be9c3c4f0eca3565b4c9a8ffff70c77d7058cf4e320e'
+  version '1.1.0'
+  sha256 '1f9a6a93d20b156ef9011b42fbc3b38c054271578312858ef30b7b6db3b60846'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
