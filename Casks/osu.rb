e6ccd43f789f5a0000dfda3265bc35aa6d4c53f8
Numbermaniac <Numbermaniac@users.noreply.github.com>
2016-05-14 14:37:42 +1000

    Update osu.rb
    fix quotes

--- Casks/osu.rb
+++ Casks/osu.rb
@@ -2,7 +2,7 @@ cask 'osu' do
   version '20151017'
   sha256 '5677ee299ea9e271546e6ab56b9d3abe002d4295c9529a5b917f5af167dcd477'
 
-  url 'http://m1.ppy.sh/osu%21-#{version}.dmg'
+  url "http://m1.ppy.sh/osu%21-#{version}.dmg"
   name 'osu!'
   homepage 'https://osu.ppy.sh'
   license :closed
