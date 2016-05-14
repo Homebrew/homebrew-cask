d19de551c593369b7d3f32691a78a865232e3439
Victor Popkov <victor@popkov.me>
2016-05-14 23:09:33 +0300

    updated pomotodo (0.13.3,1458914986) (#21220)

--- Casks/pomotodo.rb
+++ Casks/pomotodo.rb
@@ -1,11 +1,11 @@
 cask 'pomotodo' do
-  version '0.13.2,1453112814'
-  sha256 '652d25d75d4f2ad73ad987da78a1486f5ea270916653f07a5aaf4369e11b29d8'
+  version '0.13.3,1458914986'
+  sha256 '8ff2279ffd9e7efa7d3018df36ece019b2b6ca74d1e926f5c7a5a1ca12f436c6'
 
   # hackplan.com is the official download host per the vendor homepage
   url "http://cdn.hackplan.com/theair/#{version.after_comma}/Pomotodo_v#{version.before_comma}.dmg"
   appcast 'http://air.hackplan.com/projects/5455f382437315386000d4d5/versions/latest.xml',
-          checkpoint: 'af014dc3310a984d9da77c960ae6364103cde1f9691991748e9678e3a1a9a9be'
+          checkpoint: '30f76b576eba5c4a718e3284dc3bbab396f9f384ea5b21f9349b5c56e02cc104'
   name 'Pomodoro'
   homepage 'https://pomotodo.com'
   license :gratis
