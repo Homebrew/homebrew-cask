8cba85738ec05de566e86f761a0ee77fe50548d0
Alex <alexchandel@gmail.com>
2016-05-14 04:22:37 -0500

    cockatrice 2016-05-06,The_Shadows (#21204)
    - added container

--- Casks/cockatrice.rb
+++ Casks/cockatrice.rb
@@ -1,14 +1,16 @@
 cask 'cockatrice' do
-  version '2016-02-03,Gatewatchers'
-  sha256 '5934cc1688b5d27e922929ef513ec523955774f31332d8da223e802c116c27c1'
+  version '2016-05-06,The_Shadows'
+  sha256 'd59f7778f6ad153478716209f480e79ada3b0fd4a3ec3902a9650dff82e4ada0'
 
   # github.com/Cockatrice/Cockatrice was verified as official when first introduced to the cask
-  url "https://github.com/Cockatrice/Cockatrice/releases/download/#{version.before_comma}-Release/Cockatrice-#{version.after_comma}-osx.dmg"
+  url "https://github.com/Cockatrice/Cockatrice/releases/download/#{version.before_comma}-Release/Cockatrice_#{version.after_comma}_osx.dmg.zip"
   appcast 'https://github.com/Cockatrice/Cockatrice/releases.atom',
           checkpoint: '5a119ae4ed3c642e334614763aabe442631ddfa3594e99f5a3419518805d5a90'
   name 'Cockatrice'
   homepage 'http://www.woogerworks.com/'
   license :gpl
+
+  container nested: "Cockatrice_#{version.after_comma}_osx.dmg"
 
   app 'cockatrice.app'
   app 'oracle.app'
