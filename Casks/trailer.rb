0ed0a10ca4a0eb1c38b911ad33f39cd31fa6059f
Jerzerak <jerzerak@gmail.com>
2016-05-14 00:19:35 -0400

    fix trailer sha256 & checkpoint

--- Casks/trailer.rb
+++ Casks/trailer.rb
@@ -1,10 +1,10 @@
 cask 'trailer' do
   version '1.3.21'
-  sha256 '8243e1509146b38b76d5d71772936cf1665367b884bf814f2200f96a903600c1'
+  sha256 '703f89819b57a7f5588b733f60ca823b5df5ea63ae95204c2036b2c17b752e9b'
 
   url "https://ptsochantaris.github.io/trailer/trailer#{version.no_dots}.zip"
   appcast 'https://ptsochantaris.github.io/trailer/appcast.xml',
-          checkpoint: '6f675e6432be10165ed8fc78692c79bc4c291e34215e44da0ef59f692ba2893c'
+          checkpoint: '9c1590632ab790e2b84a2237613b1a01da61cc0459c5ca4b58e37e05f19a32f4'
   name 'Trailer'
   homepage 'https://ptsochantaris.github.io/trailer/'
   license :mit
