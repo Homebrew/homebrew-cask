1eaf5799d174d06ca83fdc8c1c101c8451da8a7f
Timothy Sutton <tim@synthist.net>
2016-05-14 08:36:38 -0400

    updated aja-system-test (2.1)

--- Casks/aja-system-test.rb
+++ Casks/aja-system-test.rb
@@ -1,11 +1,11 @@
 cask 'aja-system-test' do
-  version :latest
-  sha256 :no_check
+  version '2.1'
+  sha256 '53fa38382d73a0e70a88f5f23fdb384612fd57dd8ae1bb28fe49a7896b321bac'
 
-  url 'https://www.aja.com/assets/products/files/90/AJASystemTestMac.zip'
+  url 'https://www.aja.com/assets/products/files/90/AJA_SystemTest-2.1.0.app.zip'
   name 'AJA System Test'
   homepage 'https://www.aja.com/en/products/aja-system-test'
-  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :gratis
 
   app 'AJA System Test.app'
 end
