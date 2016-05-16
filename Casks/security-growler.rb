2268dde033a4fc16ec0ffb56dd5fc3de28b74468
Ben <BenjaminHCCarr@users.noreply.github.com>
2016-05-16 11:45:45 -0400

    Cleanup Based on Pull Request

--- Casks/security-growler.rb
+++ Casks/security-growler.rb
@@ -1,8 +1,6 @@
-cask 'securitygrowler' do
+cask 'security-growler' do
   version '2.2'
   sha256 '6e0a7a36745e1003177db4d419ec4502fb4a1ccbbf6766ba7ea8a69cad863593'
-
-  # github.com/pirate/security-growler/ was verified as official when first introduced to the cask
   url "https://github.com/pirate/security-growler/releases/download/v#{version.major_minor}/Security.Growler.app.zip"
   appcast 'https://github.com/pirate/security-growler/releases.atom',
           checkpoint: 'f871b12f5e054e01460055a896e6fce7117c2c647b5cfef8acd7a1ae39ec25be'
