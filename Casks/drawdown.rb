a4e1ee673327d68a36f357257b1a6715c9ee6cfc
Victor Popkov <victor@popkov.me>
2016-05-17 00:07:52 +0300

    updated drawdown (1.1.1) (#21250)

--- Casks/drawdown.rb
+++ Casks/drawdown.rb
@@ -1,10 +1,10 @@
 cask 'drawdown' do
-  version '1.1.0'
-  sha256 '4b5bf6c5e3cbf9809594457e6d7b4580de5babe65143a0d9097b7ad6538d7e1b'
+  version '1.1.1'
+  sha256 '83a48e16ad89f9acd2050c804978a4374cc8bd030b8768c8903a9cc7d6095b09'
 
   url "https://www.rwe-uk.com/uploads/updates/DrawDown_#{version}.zip"
   appcast 'https://www.rwe-uk.com/sparkle/drawdown',
-          checkpoint: '5eeb0fe53ebe57c149c2235010d7fed60ef8e2a098306bb3b010ec6f647d6b08'
+          checkpoint: 'f23f65b11c6005f28f84ed5e23cb82cf88f8c11d15e25fc46178346d7176dce0'
   name 'DrawDown'
   homepage 'https://www.rwe-uk.com/app/drawdown'
   license :commercial
