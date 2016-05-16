db20b37238458aae913fb1a2b05fbec2a555d301
Weihang <weihanglo@users.noreply.github.com>
2016-05-16 23:35:55 +0800

    updated framer (63) (#21243)

--- Casks/framer.rb
+++ Casks/framer.rb
@@ -1,4 +1,4 @@
-cask 'framer-studio' do
+cask 'framer' do
   version '63'
   sha256 '52e8086787d292cdb2089d9aa6a49cacc2c8d4705c4719ef9173e8b0ecb11e1d'
 
@@ -6,9 +6,9 @@ cask 'framer-studio' do
   url 'https://dl.devmate.com/com.motif.framer/FramerStudio.zip'
   appcast 'https://updates.devmate.com/com.motif.framer.xml',
           checkpoint: '0e24229b8de88317b1166cbbd112eb97fcbc01d80db2b95b5278d9999f09edeb'
-  name 'Framer Studio'
+  name 'Framer'
   homepage 'http://framerjs.com/'
   license :commercial
 
-  app 'Framer Studio.app'
+  app 'Framer.app'
 end
