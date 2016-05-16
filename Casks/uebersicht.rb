9983a7bc0b4a1b822f42aafb7f47f9834dbcb27c
JR Boyens <jboyens@fooninja.org>
2016-05-16 16:37:06 -0700

    Add Uebersicht v0.8.36

---
+++ Casks/uebersicht.rb
@@ -0,0 +1,11 @@
+cask 'uebersicht' do
+  version '0.8.36'
+  sha256 'd22eb580578e30b3353fef05c35b37e0fac95b986b9bd7d268485e5ed83ec752'
+
+  url 'http://tracesof.net/uebersicht/releases/Uebersicht-0.8.36.app.zip'
+  name 'Übersicht'
+  homepage 'http://tracesof.net/uebersicht/'
+  license :gpl
+
+  app 'Übersicht.app'
+end
