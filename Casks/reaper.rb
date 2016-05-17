a0b3d30ff3c35efee0bca275691b2ad23eb6e336
Christian Fillion <cfillion@users.noreply.github.com>
2016-05-17 17:52:11 -0400

    update reaper (5.20) (#21272)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,13 +1,13 @@
 cask 'reaper' do
-  version '5.18'
+  version '5.20'
 
   if Hardware::CPU.is_32_bit?
-    sha256 'c794c34e4254f344f34740a0addffdca0756c8354dccf9800fe079cbcd30f377'
+    sha256 '275d7c0b87c82dca5d8c49af233c64cd9f873cea4f81a8ef424f9f5047444e65'
     url "http://www.reaper.fm/files/#{version.to_i}.x/reaper#{version.no_dots}_i386.dmg"
     app 'REAPER.app'
     app 'ReaMote.app'
   else
-    sha256 'af2086c9c3d2be323781cfe9439374cc411aa6061509af95a0b52c7f5215a3fe'
+    sha256 '4de3cf1636d0074ef20f9f1fbeba154503ae395995b53bc32f4d309338e2f9c4'
     url "http://www.reaper.fm/files/#{version.to_i}.x/reaper#{version.no_dots}_x86_64.dmg"
     app 'REAPER64.app'
     app 'ReaMote64.app'
