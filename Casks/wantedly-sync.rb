fecb9f7ce07868c9bda55f7905043417cd32c175
Yuki Iwanaga <yuki@creasty.com>
2016-05-17 14:04:08 +0900

    Remove descriptive text from name

--- Casks/wantedly-sync.rb
+++ Casks/wantedly-sync.rb
@@ -3,7 +3,7 @@ cask 'wantedly-sync' do
   sha256 'fb9f550d60fa21e91489f735f0c3f2fc3049118c698b2b3df35c4dad632fdb38'
 
   url 'https://sync-download.s3-ap-northeast-1.amazonaws.com/releases/sync-darwin-production-1456814986.zip'
-  name 'Sync - a group chat app for business'
+  name 'Sync'
   homepage 'https://www.wantedly.com/sync'
   license :commercial
 
