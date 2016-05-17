4f3ab4fd69b2c96addf60d0ed38ad9289ca0c27d
niki-timofe <niki-timofe@ya.ru>
2016-05-18 02:43:48 +0300

    updated narodmon (1.5.3) (#21276)

--- Casks/narodmon.rb
+++ Casks/narodmon.rb
@@ -1,11 +1,11 @@
 cask 'narodmon' do
-  version '1.5.2'
-  sha256 '25bdc9c9414ef2b6330553d3ad769642eee61d2dd564159663a63c96f2769d6d'
+  version '1.5.3'
+  sha256 'ba8a5bf63525ff075603b203a5d327d78857530e47fff192ec37cab83c549292'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/niki-timofe/NarodMonOSX/releases/download/#{version}/NarodMon.zip"
   appcast 'https://github.com/niki-timofe/NarodMonOSX/releases.atom',
-          checkpoint: 'cf50ae01f26bbe62dec5bb0391504c816fd45639402a0319a28d9f4dc7023183'
+          checkpoint: 'b82c47d9286acc23f2c6dce439901c2433d2cb0fb9c2856d1fb78ffc96ce3f1e'
   name 'NarodMon'
   homepage 'http://narodmon.ru/'
   license :mit
