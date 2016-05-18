4fce884ff07ec17185437c76c6df6f2c9a59659f
Josh Hagins <hagins.josh@gmail.com>
2016-05-18 12:39:46 -0400

    fixup! Add gpg tests

--- test/support/Casks/bad-gpg-signature.rb
+++ test/support/Casks/bad-gpg-signature.rb
@@ -1,11 +1,11 @@
-cask :v1test => 'bad-gpg-signature' do
+test_cask 'bad-gpg-signature' do
   version '2.1.1_tampered'
   sha256 '2be45cd67fc081b9e9cdbcc5b43a74a010ae2ae0a8ee9efdf7295e7a1e144143'
 
   url TestHelper.local_binary_url('gnupg-2.1.1_tampered.tar.bz2')
   homepage 'http://example.com/bad-gpg-ignature'
   gpg TestHelper.local_support_url('gnupg-2.1.1.tar.bz2.sig'),
-      :key_url => 'https://www.gnupg.org/signature_key.html'
+      key_url: 'https://www.gnupg.org/signature_key.html'
 
   stage_only true
 end
--- test/support/Casks/good-gpg-signature.rb
+++ test/support/Casks/good-gpg-signature.rb
@@ -1,11 +1,11 @@
-cask :v1test => 'good-gpg-signature' do
+test_cask 'good-gpg-signature' do
   version '2.1.1'
   sha256 '70ecd01d2875db62624c911c2fd815742f50aef5492698eb3bfc09a08690ce49'
 
   url TestHelper.local_binary_url('gnupg-2.1.1.tar.bz2')
   homepage 'http://example.com/good-gpg-signature'
   gpg TestHelper.local_support_url('gnupg-2.1.1.tar.bz2.sig'),
-      :key_url => 'https://www.gnupg.org/signature_key.html'
+      key_url: 'https://www.gnupg.org/signature_key.html'
 
   stage_only true
 end
