49b56166cc08554f196661f0fcf59ede230518e7
Alex <alex@brewingcode.net>
2016-05-17 17:26:51 -0700

    get dwarf-fortress runnable: just install and type `df_osx`
    1. Replaced `suite` stanza with `binary`, DF doesn't belong in ~/Applications.
    
    2. The DF binary requires launching from the directory it is in, so we
    build a shimscript in preflight.
    
    3. The binary's name is `df`, but this conflicts with the OSX `df` command,
    so the symlink is `df_osx` instead.
    
    4. Part of the installation on modern OSX requires replacing 2 of the
    framework libraries that are included in the DF tarball. This is now
    done automatically in postflight with a series of `system` calls.

--- Casks/dwarf-fortress.rb
+++ Casks/dwarf-fortress.rb
@@ -1,4 +1,15 @@
 cask 'dwarf-fortress' do
+  module Utils
+    def self.update_sdl(name, url, path)
+      ohai "Replacing #{name}.framework from libsdl.org"
+      system 'curl', '-ksL', "https://www.libsdl.org/#{url}", '-o', "/tmp/#{name}.dmg"
+      system 'hdiutil', 'attach', "/tmp/#{name}.dmg"
+      system 'rm', '-rf', "#{path}/df_osx/libs/#{name}.framework"
+      system 'cp', '-r', "/Volumes/#{name}/#{name}.framework", "#{path}/df_osx/libs"
+      system 'hdiutil', 'detach', "/Volumes/#{name}"
+    end
+  end
+
   version '0.43.01'
   sha256 '7fe378b7aeee67f10a1f88a2341b8724edbf91795fd928506f01dfb403304d43'
 
@@ -7,5 +18,20 @@ cask 'dwarf-fortress' do
   homepage 'http://www.bay12games.com/dwarves/'
   license :gratis
 
-  suite 'df_osx', target: 'Dwarf Fortress'
+  shimscript = "#{staged_path}/df_wrapper"
+  binary shimscript, target: Hbc.homebrew_prefix.join('bin/df_osx')
+
+  preflight do
+    File.open(shimscript, 'w') do |f|
+      f.puts '#!/bin/sh'
+      f.puts "cd '#{staged_path}/df_osx' && ./df \"$@\""
+      FileUtils.chmod '+x', f
+    end
+  end
+
+  postflight do
+    # http://dwarffortresswiki.org/index.php/DF2014:Installation#Mac
+    Utils.update_sdl 'SDL_ttf', 'projects/SDL_ttf/release/SDL_ttf-2.0.11.dmg', staged_path
+    Utils.update_sdl 'SDL', 'release/SDL-1.2.15.dmg', staged_path
+  end
 end
