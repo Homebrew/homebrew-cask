15d0d57d731a1e37c021d5441dbeb1ee18cb0eb4
Jerzerak <jerzerak@gmail.com>
2016-05-18 23:06:04 -0400

    kube-cluster 0.2.5 (#21293)
    - Add zap stanza

--- Casks/kube-cluster.rb
+++ Casks/kube-cluster.rb
@@ -1,13 +1,15 @@
 cask 'kube-cluster' do
-  version '0.2.4'
-  sha256 'b9822b4cfeff7bdc80036620bdc59bd207e90a72bb040aa70694a1c5400c7f20'
+  version '0.2.5'
+  sha256 '478f4f9b7d21510d0754282628c23c90c3a8991e8f80c66609302bbc18ed182d'
 
   url "https://github.com/TheNewNormal/kube-cluster-osx/releases/download/v#{version}/Kube-Cluster_v#{version}.dmg"
   appcast 'https://github.com/TheNewNormal/kube-cluster-osx/releases.atom',
-          checkpoint: '30ad7d4dd23479855215f48b324b936db270189304f93fe59c7282802dfa1bd6'
+          checkpoint: 'c39ada164152288db5e78598a2fe2c6e55a305bfe71311b94a7cdbe82a83b775'
   name 'Kube-Cluster'
   homepage 'https://github.com/TheNewNormal/kube-cluster-osx'
   license :apache
 
   app 'Kube-Cluster.app'
+
+  zap delete: '~/kube-cluster'
 end
