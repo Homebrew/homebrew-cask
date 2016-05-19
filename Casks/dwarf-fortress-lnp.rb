e00c1a6bff31c9a537fbd6e40791a8999179aed6
Alex <alex@brewingcode.net>
2016-05-19 08:43:41 -0700

    add "verified official" to url stanza

--- Casks/dwarf-fortress-lnp.rb
+++ Casks/dwarf-fortress-lnp.rb
@@ -2,6 +2,7 @@ cask 'dwarf-fortress-lnp' do
   version '0.10.3a'
   sha256 '12809a2cda51b451039785070c575f137f0cc096a04eef861b90706cfda93c4e'
 
+  # dffd.bay12games.com was verified as official when first introduced to the cask
   url "http://dffd.bay12games.com/download.php?id=11412&f=Macnewbie_#{version.minor}.#{version.patch}.dmg"
   name 'Dwarf Fortress: Macnewbie Pack v0.10.3a w/ DF 42.04'
   homepage 'http://www.bay12forums.com/smf/index.php?topic=128960.0'
