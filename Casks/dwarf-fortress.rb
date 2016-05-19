0bd409aa190b438c2482859ca5a03b145c5cd9d2
Alex <alex@brewingcode.net>
2016-05-19 08:59:02 -0700

    make better use of `version` helpers in url stanza

--- Casks/dwarf-fortress.rb
+++ Casks/dwarf-fortress.rb
@@ -18,7 +18,7 @@ hdiutil detach /Volumes/#{name}
   version '0.43.01'
   sha256 '7fe378b7aeee67f10a1f88a2341b8724edbf91795fd928506f01dfb403304d43'
 
-  url "http://www.bay12games.com/dwarves/df_#{version.sub(%r{^0+\.}, '').gsub('.', '_')}_osx.tar.bz2"
+  url "http://www.bay12games.com/dwarves/df_#{version.minor}_#{version.patch}_osx.tar.bz2"
   name 'Dwarf Fortress'
   homepage 'http://www.bay12games.com/dwarves/'
   license :gratis
