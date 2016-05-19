c03d4421bfb1ce5ace4487f0a5af723da933ae28
Roman <romazhuzha@gmail.com>
2016-05-19 14:25:58 +0200

    Add Porting Kit (#21266)

---
+++ Casks/porting-kit.rb
@@ -0,0 +1,25 @@
+cask 'porting-kit' do
+  version '2.1.0'
+  sha256 '162ec84e03f2098bacf6568378d06b87b333ed2b59bd2bd273e0be77c3b48811'
+
+  url "http://portingkit.com/kit/Porting%20Kit%20#{version}.zip"
+  appcast 'http://portingkit.com/kit/updatecast.xml',
+          checkpoint: 'a5e30702e697aa2bf969da15c901e99b0517851fe9bee9b306c11a032e95e55b'
+  name 'Porting Kit'
+  homepage 'http://portingkit.com/'
+  license :gratis
+
+  auto_updates true
+  conflicts_with cask: 'caskroom/versions/porting-kit-legacy'
+  depends_on macos: '>= :snow_leopard'
+
+  app 'Porting Kit.app'
+
+  zap delete: [
+                '~/Library/Preferences/edu.ufrj.vitormm.Porting-Kit.plist',
+                '~/Library/Application Support/Porting-Kit',
+                '~/Library/Saved Application State/edu.ufrj.vitormm.Porting-Kit.savedState',
+                '~/Library/Caches/edu.ufrj.vitormm.Porting-Kit',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/edu.ufrj.vitormm.porting-kit.sfl',
+              ]
+end
