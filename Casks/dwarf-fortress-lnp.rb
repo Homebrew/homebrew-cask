c304acee93f4bb320691c7a2a73a0d2e610eb2f6
Alex <alex@brewingcode.net>
2016-05-18 17:04:28 -0700

    Add dwarf-fortress-lnp v0.10.3a
    This is the Lazy Newb Pack for Dwarf Fortress v42.04.

---
+++ Casks/dwarf-fortress-lnp.rb
@@ -0,0 +1,11 @@
+cask 'dwarf-fortress-lnp' do
+  version '0.10.3a'
+  sha256 '12809a2cda51b451039785070c575f137f0cc096a04eef861b90706cfda93c4e'
+
+  url 'http://dffd.bay12games.com/download.php?id=11412&f=Macnewbie_10.3a.dmg'
+  name 'Dwarf Fortress: Macnewbie Pack v0.10.3a w/ DF 42.04'
+  homepage 'http://www.bay12forums.com/smf/index.php?topic=128960.0'
+  license :gratis
+
+  suite 'Macnewbie'
+end
