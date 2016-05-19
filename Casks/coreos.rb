d2866e5e2d591de239ffd3f7880eb22efbb802e7
Jerzerak <jerzerak@gmail.com>
2016-05-18 23:04:57 -0400

    coreos 1.2.8 (#21295)
    - Add zap stanza

--- Casks/coreos.rb
+++ Casks/coreos.rb
@@ -1,13 +1,15 @@
 cask 'coreos' do
-  version '1.2.5'
-  sha256 '80643deab5dabdb89b312def48beb97f72160163f8b23e9d969c2640d36cc961'
+  version '1.2.8'
+  sha256 '846c2b7b96ec897b603383051e36bf4b06dc95f07a5cdbd9c1e429f04f82a4f4'
 
   url "https://github.com/TheNewNormal/coreos-osx/releases/download/v#{version}/CoreOS_v#{version}.dmg"
   appcast 'https://github.com/TheNewNormal/coreos-osx/releases.atom',
-          checkpoint: '3a4d0db68c5e771c51254919040fd4908904f23b3f7a68e1bedc2bee05087a99'
+          checkpoint: '74f6ba0620644f379f489e50c1c83a4c3b5e9946ec87c91c0d27e4b6269847c8'
   name 'CoreOS VM'
   homepage 'https://github.com/TheNewNormal/coreos-osx'
   license :apache
 
   app 'CoreOS.app'
+
+  zap delete: '~/coreos-osx'
 end
