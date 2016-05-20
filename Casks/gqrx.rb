0314b9cd87c70309709dbd0cb13c08aef5993070
Yuval Adam <yuval@y3xz.com>
2016-05-20 19:17:10 +0300

    Add gqrx.rb cask

---
+++ Casks/gqrx.rb
@@ -0,0 +1,13 @@
+cask 'gqrx' do
+  version '2.5.3'
+  sha256 'b5053b9a86c13d856137143a8215f48a85d3e57b4d867c317a666b1bc0e97d10'
+
+  url "https://github.com/csete/gqrx/releases/download/v#{version}/Gqrx-#{version}-2.dmg"
+  appcast 'https://github.com/csete/gqrx/releases.atom',
+          checkpoint: '686d4a577ad5eea69ae06269c07f215f0c8c90a6cfe89777808e5813d54b53cd'
+  name 'Gqrx'
+  homepage 'http://gqrx.dk/'
+  license :gpl
+
+  app 'Gqrx.app'
+end
