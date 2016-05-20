2c3c8604de0f234a72cb2873fae448d2b09ffbfe
Victor Popkov <victor@popkov.me>
2016-05-20 22:38:41 +0300

    updated iexplorer (3.9.9.1) (#21332)

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,10 +1,10 @@
 cask 'iexplorer' do
-  version '3.9.8.0'
-  sha256 '93a9a6e9ba083dcffc22c8f02aa17576d456a6cccce2a423f6c41497bdd9135f'
+  version '3.9.9.1'
+  sha256 '636d6f22e2844677c7f76440e1d5094b2a4365a6e026094d1f6eb7feefb62139'
 
   url "https://www.macroplant.com/release/iExplorer-#{version}.dmg"
   appcast 'https://www.macroplant.com/iexplorer/ie3-appcast.xml',
-          checkpoint: '5681a4d7b14edacfc86a43ad3929b4b798e6ecc30db80f7f58e40bac0746ec65'
+          checkpoint: '8b87ff2576664b75296e953006fe6fc236afc218e783d46dd8314c30ab13c097'
   name 'iExplorer'
   homepage 'https://www.macroplant.com/iexplorer/'
   license :freemium
