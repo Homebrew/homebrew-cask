0116a72e35a66b9e328dc8d0b7d3a1bdd2cf03f6
Victor Popkov <victor@popkov.me>
2016-05-20 22:23:22 +0300

    updated airserver (6.0.7) (#21328)

--- Casks/airserver.rb
+++ Casks/airserver.rb
@@ -1,10 +1,10 @@
 cask 'airserver' do
-  version '6.0.6'
-  sha256 'd69453c83016d420cd9222dba0ecb0a756e58d647ded2ed2927f062cb8d12a82'
+  version '6.0.7'
+  sha256 'ed0e11a9ea1966a26e94f292ba96755d5cb3d1816d5a87deaaa327ed16b5bc4c'
 
   url "http://dl.airserver.com/mac/AirServer-#{version}.dmg"
   appcast 'https://www.airserver.com/downloads/mac/appcast.xml',
-          checkpoint: '32c0941369a342febbd49d830f796f8cc0c7c8d4a78c4668ee0e81dc9feb5802'
+          checkpoint: 'cb5082a38ce39fa9429532e3a9de301e7eff86d430f1c0f37bbcbd29239b489f'
   name 'AirServer'
   homepage 'https://www.airserver.com'
   license :commercial
