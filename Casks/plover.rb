98a1a77b9d70df04e2c6c66d5235a5bf88fda667
Waleed Khan <arxanas@gmail.com>
2016-05-19 17:12:11 -0700

    Update plover to version 3.0.0 (#21312)

--- Casks/plover.rb
+++ Casks/plover.rb
@@ -1,11 +1,11 @@
 cask 'plover' do
-  version '2.5.8'
-  sha256 'a8bbeddb5d6fb8d4499844257035edd62e431a4f1eb7959bdf17a15c1cbe12de'
+  version '3.0.0'
+  sha256 '89505ebad2700e38d57b5c710550dac0ad4b9a44bd1e15ba8b1f162f0b464694'
 
-  # github.com is the official download host per the vendor homepage
-  url "https://github.com/openstenoproject/plover/releases/download/v#{version}/Plover.dmg"
+  # github.com/openstenoproject/plover was verified as official when first introduced to the cask
+  url "https://github.com/openstenoproject/plover/releases/download/v#{version}/Plover-#{version}.dmg"
   appcast 'https://github.com/openstenoproject/plover/releases.atom',
-          checkpoint: 'f94793545aa832111ad2fb2ec54728f1a2dcaf8915f691a50f42a4705c01e776'
+          checkpoint: '8eb7487c0243890078c45e7f0a3ca5b856f3d3b980ec9737f5aa87607037b91e'
   name 'Plover'
   homepage 'http://stenoknight.com/wiki/Main_Page'
   license :gpl
@@ -13,4 +13,6 @@ cask 'plover' do
   accessibility_access true
 
   app 'plover.app'
+
+  zap delete: '~/Library/Application Support/plover/'
 end
