cb60c8db953ed5c5d4be0acea9d6b90509ebf190
Victor Popkov <victor@popkov.me>
2016-05-20 22:23:31 +0300

    updated aseprite (1.1.5.6) (#21329)

--- Casks/aseprite.rb
+++ Casks/aseprite.rb
@@ -1,10 +1,10 @@
 cask 'aseprite' do
-  version '1.1.5.4'
-  sha256 '9b69ad5860cf4c5eeacbb141512158f8e6271de1eecd1d64d51a1ea5c7ce38f5'
+  version '1.1.5.6'
+  sha256 '8e177409221bec7587d823881ddd7706e99fc89eae85d319ab6c5abf0e0e29af'
 
   url "http://www.aseprite.org/downloads/Aseprite-v#{version}-trial-MacOSX.dmg"
   appcast 'https://github.com/aseprite/aseprite/releases.atom',
-          checkpoint: 'bab35c4491700ed85cf263e6365ccba0a39f1950234259cb63d748f236ee894e'
+          checkpoint: 'b4479716a52906161873b78c22b1e6c0033dbab076fb93630273d0ec108625c0'
   name 'Aseprite'
   homepage 'http://www.aseprite.org'
   license :gpl
