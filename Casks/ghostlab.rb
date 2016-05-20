c311808dbceda43b8311322e3f72a26de0a6e912
Victor Popkov <victor@popkov.me>
2016-05-20 22:23:51 +0300

    updated ghostlab (2.1.2) (#21331)

--- Casks/ghostlab.rb
+++ Casks/ghostlab.rb
@@ -1,10 +1,10 @@
 cask 'ghostlab' do
-  version '2.1.1'
-  sha256 '3f62bcfa28d556183a6578f884769d9b5ecf1c5b391e15aed01fa7bcbda907b7'
+  version '2.1.2'
+  sha256 'ce2a1a6189fe226c59cd923717a5ae6f71621fcdd8a57e28f70efaaaf81f8d13'
 
   url "https://awesome.vanamco.com/Ghostlab2/update/packages/mac/Ghostlab2-#{version}.zip"
   appcast 'https://awesome.vanamco.com/Ghostlab2/update/ghostlab2-cast.xml?vco=trkd',
-          checkpoint: '97bf0f608f618d26c958a6b8214c3d05073b2da61cc88c70e9c4ece3b3f1a696'
+          checkpoint: '243c77ba04b68fb6af58aed4467f7664d5bf79e88eff4726c48ca157d98a7490'
   name 'Ghostlab'
   homepage 'https://vanamco.com/ghostlab/'
   license :commercial
