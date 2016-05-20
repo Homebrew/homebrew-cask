b1e7364ef78670f5e03e5b2cb51ef75259dcd798
Thomas Anquetin <thomas_anquetin@hotmail.com>
2016-05-21 00:57:59 +0200

    updated borgbackup (1.0.3) (#21340)

--- Casks/borgbackup.rb
+++ Casks/borgbackup.rb
@@ -1,17 +1,17 @@
 cask 'borgbackup' do
-  version '1.0.2'
-  sha256 '2880f17df99dfdf6a16c206c78cd6d853a8c140c4d09f9b6c83a43094b4d34c7'
+  version '1.0.3'
+  sha256 '72c13b51be987560159cb8801e1a7bdfa31d50225c2691a6f129859dc103f04e'
 
-  url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
+  url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-darwin64"
   appcast 'https://github.com/borgbackup/borg/releases.atom',
-          checkpoint: '92766ba1c792610c63086621a1a149f39d52e338e7993b44812bfa63fcdc93f8'
+          checkpoint: 'bc816a4618996630e59876dc6590959a5c0275e42b1c652930348d24be4ff6be'
   name 'BorgBackup'
   homepage 'https://borgbackup.readthedocs.org/en/stable/'
   license :bsd
   gpg "#{url}.asc",
-      key_id: '243ACFA951F78E01'
+      key_id: '51F78E01'
 
   container type: :naked
 
-  binary 'borg-macosx64', target: 'borg'
+  binary 'borg-darwin64', target: 'borg'
 end
