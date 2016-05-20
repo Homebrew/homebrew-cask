0fa5a1194bc07a0c7a02d9b9851f8449b61d329c
Pengcheng Li <lpc1983@gmail.com>
2016-05-20 09:05:30 +0800

    Update electronic wechat to 1.3.0 (#21314)

--- Casks/electronic-wechat.rb
+++ Casks/electronic-wechat.rb
@@ -1,6 +1,6 @@
 cask 'electronic-wechat' do
-  version '1.2.0'
-  sha256 '9c31710fc280604a441a12258475edad8d82893b19e5525ca6187b9b08dbfff6'
+  version '1.3.0'
+  sha256 '103fbc03c3d12727938ddfc55fec842ada6cc959be94a55838b9bddf66abeb91'
 
   url "https://github.com/geeeeeeeeek/electronic-wechat/releases/download/v#{version}/mac-osx.tar.gz"
   appcast 'https://github.com/geeeeeeeeek/electronic-wechat/releases.atom',
