3d343f9a81d9abc270129613b8ad53c87820f6d0
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-05-21 14:47:58 +0200

    updated mkchromecast (0.2.4) (#21350)

--- Casks/mkchromecast.rb
+++ Casks/mkchromecast.rb
@@ -1,10 +1,10 @@
 cask 'mkchromecast' do
-  version '0.2.3.1'
-  sha256 '2f16b83a2a132c9aa49ead8d55553070137fbdf4419799a15f2e33b637c902fa'
+  version '0.2.4'
+  sha256 '4e02180d77c3ebf8b01e0f5b37d45895944749411c4ad60425b981b1fa38f333'
 
   url "https://github.com/muammar/mkchromecast/releases/download/#{version}/mkchromecast_v#{version}.dmg"
   appcast 'https://github.com/muammar/mkchromecast/releases.atom',
-          checkpoint: 'e2d625225881d00fd307f506d6e4b6268dedd70c2245c1e02ca1d798abc14331'
+          checkpoint: '77b8bba9ad445ce5a571178858d94d134755f7b1be455f8da468eb5bd2e4ef06'
   name 'mkchromecast'
   homepage 'http://mkchromecast.com'
   license :mit
