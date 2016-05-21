2060abf7389253a46bb2f09687775bc9f8568231
Aditya Dalal <adidalal@users.noreply.github.com>
2016-05-21 10:05:50 -0400

    facebook-ios-sdk: Add URL comment

--- Casks/facebook-ios-sdk.rb
+++ Casks/facebook-ios-sdk.rb
@@ -2,6 +2,7 @@ cask 'facebook-ios-sdk' do
   version '3.24.4'
   sha256 'dc9ae3e6c9969a03b15e041dd24d7f0d0bdfcd35f8a7eb8d35c2e21434a3a0b3'
 
+  # origincache.facebook.com/developers was verified as official when first introduced to the cask
   url "https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-#{version}.pkg"
   name 'Facebook SDK for iOS'
   homepage 'https://developers.facebook.com/docs/ios'
