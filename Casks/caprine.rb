87822ac3823ac80a640aba1376c4d5f1b9677078
Sindre Sorhus <sindresorhus@gmail.com>
2016-05-21 16:03:16 +0700

    updated caprine (1.4.0)

--- Casks/caprine.rb
+++ Casks/caprine.rb
@@ -1,10 +1,10 @@
 cask 'caprine' do
-  version '1.3.1'
-  sha256 '19e048e2457ade0712b8fac73f6b0c928993fe791dfd38ec23abe6d96a62ec15'
+  version '1.4.0'
+  sha256 '51cc1dab3c7e02c13ffef14f446bad9af417be62fb64cafa5bdced0f6ffd163a'
 
-  url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/Caprine-osx-#{version}.zip"
+  url "https://github.com/sindresorhus/caprine/releases/download/#{version}/Caprine-osx-#{version}.zip"
   appcast 'https://github.com/sindresorhus/caprine/releases.atom',
-          checkpoint: '8cda3c72e9bac26fac211a79baf29de04d5442ba5c905a65fc8637943ea2d550'
+          checkpoint: '39cd7443804cee6199df82380723094ab6c0413e605765353cef6545533e3a8f'
   name 'Caprine'
   homepage 'https://github.com/sindresorhus/caprine'
   license :mit
