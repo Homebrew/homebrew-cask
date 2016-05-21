6199aea52127ab69fee89be0f51371f6046e4bb4
Claudia <claui@users.noreply.github.com>
2016-05-21 21:24:15 +0200

    sage.rb: Switch mirror; update to v7.2; fix URLs (#21353)
    The latest stable version of SageMath is [7.2](http://mirrors.mit.edu/sage/osx/intel/index.html) and can be found on the MIT mirror.
    
    Currently, the El Capitan branch of this cask is broken because it points to [go-parts.com](http://mirrors-usa.go-parts.com/sage/sagemath/osx/intel/), a mirror which seems not to carry neither the stable 7.1 version nor the current stable v7.2.
    
    The site is also not part of the [list of download servers on sagemath.org](http://www.sagemath.org/download-mac.html). Because of this, I switched the El Capitan branch to point to mirrors.mit.edu/sage just like the other branches do.
    
    This commit also updates broken URLs for the Mavericks and Yosemite branches.
    
    http://mirrors.mit.edu/sage/osx/intel/sage-7.2-OSX_10.11.4-x86_64.tar.bz2
    
    This cask also happens to symlink a binary which lives inside the moved artifact. So this is one of the cases where we might want to use the new `appdir` method that is being introduced in #13966.

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -3,17 +3,17 @@ cask 'sage' do
     version '6.9'
     sha256 '21f460c90db2a9ee83d196937587de5daab8c6712b0f24366b6a1c15a8dd592b'
     # mit.edu/sage was verified as official when first introduced to the cask
-    url "http://mirrors.mit.edu/sage/osx/intel/sage-#{version}-x86_64-Darwin-OSX-10.7-app.dmg"
+    url "http://mirrors.mit.edu/sage/osx/intel/old/sage-#{version}-x86_64-Darwin-OSX-10.7-app.dmg"
   elsif MacOS.release <= :yosemite
     version '6.9'
     sha256 '03112bf747cf807f308d518f34c1982ca3c9599e65bf64a6782efc78136198a4'
     # mit.edu/sage was verified as official when first introduced to the cask
-    url "http://mirrors.mit.edu/sage/osx/intel/sage-#{version}-x86_64-Darwin-OSX_10.10_x86_64-app.dmg"
+    url "http://mirrors.mit.edu/sage/osx/intel/old/sage-#{version}-x86_64-Darwin-OSX_10.10_x86_64-app.dmg"
   else
-    version '7.1'
-    sha256 'ce900a39dd1a4c33cec40a3c2fde81cf4a4446a7d8b3985041b501b9d88f8471'
-    # go-parts.com/sage was verified as official when first introduced to the cask
-    url "http://mirrors-usa.go-parts.com/sage/sagemath/osx/intel/sage-#{version}-OSX_10.11.3-x86_64.app.dmg"
+    version '7.2'
+    sha256 '4ede3cac113778e120d233baf702426ffad10905d43afe239c1ea8fd1068697e'
+    # mit.edu/sage was verified as official when first introduced to the cask
+    url "http://mirrors.mit.edu/sage/osx/intel/sage-#{version}-OSX_10.11.4-x86_64.app.dmg"
   end
 
   name 'Sage'
