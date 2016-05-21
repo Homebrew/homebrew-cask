59ec464f9e10d7f2a1853ac16874206473268ecb
Oleksii Taran <oleksii.taran@gmail.com>
2016-05-20 17:11:24 -0700

    avidemux 2.6.12

--- Casks/avidemux.rb
+++ Casks/avidemux.rb
@@ -1,9 +1,9 @@
 cask 'avidemux' do
-  version '2.6.10'
-  sha256 '25b8dec3474b9e6a5907b19643e0052e2cda5d1029191b20cf8fda43574a6a0f'
+  version '2.6.12'
+  sha256 'df95610c560b4bd4df8fbc4285c64161ddf7f987c70a6ef073b4adc5b197b1e5'
 
   # sourceforge.net/avidemux was verified as official when first introduced to the cask
-  url "http://downloads.sourceforge.net/avidemux/Avidemux_#{version}_64bits_Qt4.dmg"
+  url "https://downloads.sourceforge.net/project/avidemux/avidemux/#{version}/Avidemux_#{version}_ML_64Bits_Qt5.dmg"
   name 'Avidemux'
   homepage 'http://www.avidemux.org/'
   license :gpl
