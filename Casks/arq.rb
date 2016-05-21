5ae604b44456e97181658f37e07ee779371cd3e3
Leonid Komarovsky <shpoont@users.noreply.github.com>
2016-05-21 19:28:22 +0300

    updated arq (5.1.2) (#21352)

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,10 +1,10 @@
 cask 'arq' do
-  version '5.0.11'
-  sha256 '079a8d98077a2c2da9bd46f7bd71dc02c52dd1e0cd289fdf82444dbaa7f3914c'
+  version '5.1.2'
+  sha256 'b2a096544e56d7269605b34e36166282a34b2e088fb3c1f1e553e67016a128a7'
 
   url "https://www.arqbackup.com/download/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arq#{version.major}.xml",
-          checkpoint: '1427c156bca99a4062b440416a9b16c84b2df6cdd923a3c18665b78c30555c92'
+          checkpoint: 'ed5cbf74be385005e87425fd87445915e47ebb676da1ef7b762b68ec3f87c750'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
   license :commercial
