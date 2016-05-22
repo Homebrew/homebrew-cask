35286f4f4fa2d9d222533fecb40df70efb7c1add
Jacob True <jmtrue1@gmail.com>
2016-05-23 08:48:58 +0900

    updated anki (2.0.36) (#21380)

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -3,8 +3,8 @@ cask 'anki' do
     version '2.0.33.qt47'
     sha256 '85a49099b37160c12b1ceb18d4728e99d87ed7d0e6ee978f237794a14b3b53d6'
   else
-    version '2.0.33'
-    sha256 '1a18498dff0bb4b9d6f493dc63a55d0b4f3e99ebfc122d83cd2ceec34fe9268b'
+    version '2.0.36'
+    sha256 '4e229ace72b11b93186af31183ae365bb24c5f14d098b7c4bb564d16c3dfbc3c'
   end
 
   url "http://ankisrs.net/download/mirror/anki-#{version}.dmg"
