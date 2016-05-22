a7660ddc35b7631bf64c912d706c30a8d629fc65
Claudia <claui@users.noreply.github.com>
2016-05-22 04:32:39 +0200

    Add `uninstall` stanza to omnifocus.rb, amend `zap` (#21358)

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -20,19 +20,22 @@ cask 'omnifocus' do
   app 'OmniFocus.app'
 
   if MacOS.release <= :mountain_lion
+    uninstall quit: 'com.omnigroup.OmniFocus'
     zap delete: [
                   '~/Library/Application Support/OmniFocus/Plug-Ins',
                   '~/Library/Application Support/OmniFocus/Themes',
                   '~/Library/Preferences/com.omnigroup.OmniFocus.plist',
                 ]
   else
+    uninstall quit: 'com.omnigroup.OmniFocus2'
     zap delete: [
-                  '~/Library/containers/com.omnigroup.omnifocus2',
+                  '~/Library/Containers/com.omnigroup.OmniFocus2',
                   '~/Library/Preferences/com.omnigroup.OmniFocus2.LSSharedFileList.plist',
                   '~/Library/Preferences/com.omnigroup.OmniSoftwareUpdate.plist',
                   '~/Library/Caches/Metadata/com.omnigroup.OmniFocus2',
                   '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.omnigroup.omnifocus2.sfl',
                   '~/Library/Group Containers/34YW5XSRB7.com.omnigroup.OmniFocus',
+                  '~/Library/Saved Application State/com.omnigroup.OmniFocus2.savedState',
                 ]
   end
 end
