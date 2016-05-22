c5717b28fa114e481ff566bc2605f2d94d1e0456
mike-u <mpu2@pitt.edu>
2016-05-22 14:36:10 -0400

    Added CellProfiler and made changes requested

---
+++ Casks/cellprofiler.rb
@@ -0,0 +1,10 @@
+cask 'cellprofiler' do
+  version :latest
+  sha256 :no_check
+  # cellprofiler-org.s3.amazonaws.com was verified as official when first introduced to the cask
+  url 'http://cellprofiler-org.s3.amazonaws.com/CellProfiler.dmg'
+  name 'CellProfiler'
+  homepage 'http://cellprofiler.org'
+  license :oss
+  app 'CellProfiler.app'
+end
