08674082ae00bfa01fe1bd83a5c6f6459e0215d4
Sindre Sorhus <sindresorhus@gmail.com>
2016-05-22 16:43:57 +0700

    updated anatine (0.4.1) (#21364)

--- Casks/anatine.rb
+++ Casks/anatine.rb
@@ -1,10 +1,10 @@
 cask 'anatine' do
-  version '0.4.0'
-  sha256 '3f6405ce04d7753fc52bd207f20070a08db589473ab7e79b1201199194978544'
+  version '0.4.1'
+  sha256 '8eaf61e86d2466ae5a9b8d64811665aa80c0190c6e9a7c3fbf4e20c3d482ba2d'
 
   url "https://github.com/sindresorhus/anatine/releases/download/#{version}/Anatine-osx-#{version}.zip"
   appcast 'https://github.com/sindresorhus/anatine/releases.atom',
-          checkpoint: '2c2c1e14ccc0d076d5c016feeed840da2d0354e8ce2f54c798819702216e0354'
+          checkpoint: '437dc043ae8a936d5f4e26507749801b9d83af27863267cb05ca227cb373e480'
   name 'Anatine'
   homepage 'https://github.com/sindresorhus/anatine'
   license :mit
