ad3f8e80b5eb3632bee38d9ce5f799b210d07faa
Lorelei Aurora <iam@loreleiaurora.me>
2016-05-22 01:29:08 +0100

    Update omnioutliner to 4.5.3

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -1,5 +1,5 @@
 cask 'omnioutliner' do
-  version '4.5.2'
+  version '4.5.3'
   sha256 '3e17b59171a3a75ee8f4dcf412c7001e0ffdcfefffeec5b613749a9113216b7f'
 
   url "http://downloads.omnigroup.com/software/MacOSX/10.10/OmniOutliner-#{version}.dmg"
