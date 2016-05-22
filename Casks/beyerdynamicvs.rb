515a416449fcfdb8e53e72078e54c744d3339439
yutarody <yutaro.ver05+github@gmail.com>
2016-05-22 16:12:41 +0900

    add beyerdynamic-virtual-studio
    Rename beyerdynamic-virtual-studio to beyerdynamicvs

---
+++ Casks/beyerdynamicvs.rb
@@ -0,0 +1,14 @@
+cask 'beyerdynamicvs' do
+  version '1.011'
+  sha256 '639cfe1fa96026042030455afbaaa518f11522c3ebd86cdbde16e61af7477e15'
+
+  url 'http://www.beyerdynamic.com/fileadmin/editors/downloads/virtualstudio/beyerdynamic-virtual-studio_04-13.zip'
+  name 'beyerdynamic Virtual Studio'
+  homepage 'http://www.beyerdynamic.de/virtual-studio.html'
+  license :gratis
+
+  container nested: "mac/beyerdynamicVS_Mac_#{version}_Plugin.dmg"
+
+  audio_unit_plugin 'beyerdynamicVS.component'
+  vst_plugin 'beyerdynamicVS.vst'
+end
