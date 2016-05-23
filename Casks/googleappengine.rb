090b33e72a56a668b58a35b81cf6fca7deff3e8b
Joseph Chiocchi <joseph@gopangea.com>
2016-05-23 12:05:36 -0500

    updated googleappengine (1.9.37)

--- Casks/googleappengine.rb
+++ Casks/googleappengine.rb
@@ -1,11 +1,11 @@
 cask 'googleappengine' do
-  version '1.9.35'
-  sha256 '7b835604e0687c74414d499647a426c7f164a20eb03963670a76c044c4f29afc'
+  version '1.9.37'
+  sha256 'ccc9a88010b8bc62660365395eab9113f73ee9800a528f62464b7f8ee8750c71'
 
   # storage.googleapis.com/appengine-sdks was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
   appcast 'https://storage.googleapis.com/appengine-sdks',
-          checkpoint: '289c3a9dbfcbc652417fedbb839249684f6bc65d735c18025f269a2685d81676'
+          checkpoint: '43853083c43deaedd850a967039980722f7299a5eb9423e1634a78f90dc40d34'
   name 'Google App Engine'
   homepage 'https://developers.google.com/appengine/'
   license :apache
