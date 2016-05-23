b0abe877d5108a05a2a10472717fee3c6b601bf4
Gavin Hu <haozhonghu@hotmail.com>
2016-05-23 16:55:48 +0800

    updated leanote (2.0)

--- Casks/leanote.rb
+++ Casks/leanote.rb
@@ -1,11 +1,11 @@
 cask 'leanote' do
-  version '1.0.7'
-  sha256 'bdcaf67fb40f1b2477843ea2a51580ab9c1520bff8ee04e0b4a929a57e67cbf9'
+  version '2.0'
+  sha256 '61894f7a905b59aa04c92f1a6767fcb8e3313614e7bc59cfa4ff9ad816b2a2a0'
 
   # download.sourceforge.net/sourceforge/leanote-desktop-app was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/sourceforge/leanote-desktop-app/leanote-desktop-mac-v#{version}.zip"
   appcast 'https://github.com/leanote/desktop-app/releases.atom',
-          checkpoint: '9309addba49ca9a33306f607d259cd793d4dfe92f98761a36ab4405a1d146666'
+          checkpoint: '845826b5a9b926a23e193e62560a980485d02ad0a527b23cf24d42d562048598'
   name 'Leanote'
   homepage 'http://leanote.org'
   license :gpl
