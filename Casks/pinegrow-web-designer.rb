658bc77246f1725f75b9e808eb1c0dee4a524317
Madalin Ignisca <madalin.ignisca@gmail.com>
2016-05-23 21:05:58 +0100

    update pinegrow web designer to 2.81 (#21398)

--- Casks/pinegrow-web-designer.rb
+++ Casks/pinegrow-web-designer.rb
@@ -1,6 +1,6 @@
 cask 'pinegrow-web-designer' do
-  version '2.2'
-  sha256 'c0d1b9595963b8080298261c4d9744237427e5d2ed02d38d38fbdbcbb43a7f1a'
+  version '2.81'
+  sha256 '76d14cc1947d97cfb58f9879956ffc8ee04582fbdff3131a6b11f4dabe7c2e25'
 
   # pinegrow.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://pinegrow.s3.amazonaws.com/PinegrowMac.#{version}.dmg"
