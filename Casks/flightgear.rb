f1c0c6130b339d889b8b53e19c5988a1e5ca46ee
Steffen Hansen <sgnh@users.noreply.github.com>
2016-05-23 05:22:04 +0200

    updated flightgear (2016.2.1) (#21382)

--- Casks/flightgear.rb
+++ Casks/flightgear.rb
@@ -1,6 +1,6 @@
 cask 'flightgear' do
-  version '2016.1.1'
-  sha256 '223b8a50ff050e45b7039b6537a2ef62ea1f9efc78ce214b15d05be49c78a688'
+  version '2016.2.1'
+  sha256 '86e0bdbe06a7e9660380060fc3e48f3e2d1ee5cf838af3573805db2560a5c720'
 
   # sourceforge.net/sourceforge/flightgear was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/sourceforge/flightgear/FlightGear-#{version}.dmg"
