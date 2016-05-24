1f0c3edd018cda8b7476dd477b0beca3f12eb1a8
Manu <m@nuelriel.com>
2016-05-25 03:31:10 +0800

    updated Shimo (4.1_8209) (#21406)

--- Casks/shimo.rb
+++ Casks/shimo.rb
@@ -1,8 +1,9 @@
 cask 'shimo' do
-  version '4.0.5_7199'
-  sha256 'e413fedd9dcb0161986433eb26599de07428ef1a2c443b0e9448bb8bc86af504'
+  version '4.1_8209'
+  sha256 '6b2183851540a31fcbf9c887c90195ed08eaa2ed76d23a5cfd8294d8f2499dd8'
 
-  url "https://www.feingeist.io/fg-library/appcasts/Shimo4/Shimo_#{version}.zip"
+  # shimo.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://shimo.s3.amazonaws.com/Shimo_#{version}.zip"
   name 'Shimo'
   homepage 'https://www.feingeist.io/shimo/'
   license :commercial
