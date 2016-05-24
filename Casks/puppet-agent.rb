93651b3c9721c3f396d54822f72f93cc459cffce
Jerzerak <jerzerak@gmail.com>
2016-05-24 18:54:17 -0400

    puppet-agent 1.5.0 (#21418)

--- Casks/puppet-agent.rb
+++ Casks/puppet-agent.rb
@@ -1,21 +1,21 @@
 cask 'puppet-agent' do
-  version '1.4.2'
+  version '1.5.0'
 
   if MacOS.release == :el_capitan
-    sha256 'bb94f785c253614edd2e82e89fdbb4dabe800f0dc009b5658e0d42e11e875ff9'
+    sha256 'd92a12ed3ab7aede17d21333c3bb92c3987559932291b4760210cf1e2ca64b07'
   elsif MacOS.release == :yosemite
-    sha256 '90d9188dc75748171cc920159724877a60d6e3d9c8792d288cbac0acebe1af43'
+    sha256 'ee4ee7004c63a8a2cc4680bff6d76ee5bfab22449b60a05505a576e9a8042257'
   elsif MacOS.release == :mavericks
-    sha256 'c119526e30fa93917c327109e45d0297bec2c2f443ede1f80d5ce694db1d64b7'
+    sha256 'a56f1360616ab91bbd024658b536ada07083391747fb67c9874aa33effae5f80'
   end
 
   # downloads.puppetlabs.com was verified as official when first introduced to the cask
   url "https://downloads.puppetlabs.com/mac/#{MacOS.release}/PC1/x86_64/puppet-agent-#{version}-1.osx#{MacOS.release}.dmg"
   name 'Puppet Agent'
-  homepage 'https://docs.puppet.com/puppet/4.4/reference/about_agent.html'
+  homepage 'https://docs.puppet.com/puppet/4.5/reference/about_agent.html'
   license :oss # all Apache 2 except for the vendored OpenSSL + Ruby
 
-  pkg 'puppet-agent-1.4.2-1-installer.pkg'
+  pkg 'puppet-agent-1.5.0-1-installer.pkg'
 
   uninstall launchctl: %w[puppet pxp-agent mcollective],
             pkgutil:   'com.puppetlabs.puppet-agent'
