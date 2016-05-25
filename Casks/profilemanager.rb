041dae6975f11c1dee05cd1b7b339a798333cfc6
Hal Wine <h.alwine@gmail.com>
2016-05-25 03:32:31 -0700

    Update source URL (#21421)

--- Casks/profilemanager.rb
+++ Casks/profilemanager.rb
@@ -2,7 +2,7 @@ cask 'profilemanager' do
   version '1.0'
   sha256 'a46295851063d8a0630cace6720813e571e86a66734a8765f9706bab939b3f48'
 
-  url "https://ftp.mozilla.org/pub/mozilla.org/utilities/profilemanager/#{version}/profilemanager.mac.dmg"
+  url "https://archive.mozilla.org/pub/mozilla.org/utilities/profilemanager/#{version}/profilemanager.mac.dmg"
   name 'Mozilla Profile Manager'
   homepage 'https://developer.mozilla.org/en-US/docs/Profile_Manager'
   license :mpl
