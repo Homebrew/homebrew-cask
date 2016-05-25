a36b362c6ee72d88a60d62b1f8bf0bc1a7ab3070
Victor Popkov <victor@popkov.me>
2016-05-25 21:40:08 +0300

    updated debookee (5.1.0)

--- Casks/debookee.rb
+++ Casks/debookee.rb
@@ -1,10 +1,10 @@
 cask 'debookee' do
-  version '5.0.7'
-  sha256 '6f3da60452783c00f1c12a2023619983291e0cea940995ba033c1525d6694049'
+  version '5.1.0'
+  sha256 '221a98590a367c3dda96facba62deac6046730324a09978491ff2dd404126afd'
 
   url 'https://www.iwaxx.com/debookee/debookee.zip'
   appcast 'https://www.iwaxx.com/debookee/appcast.php',
-          checkpoint: '6ce459f965fab48a53e113594cc0af5af8c4c43e098f569d260669fad583ff29'
+          checkpoint: '3034696e3dea16c771e0a95d20ce6dc83991105d0c39c6522576723d6f86abb2'
   name 'Debookee'
   homepage 'https://www.iwaxx.com/debookee/'
   license :commercial
