d500c45b05e010528c94047ccec5c4aa637678ef
Eugene Nikolsky <eunikolsky@gmail.com>
2016-05-25 09:11:44 -0700

    Add RPN Scientific Calculator v1.2.5

---
+++ Casks/rpn-scientific.rb
@@ -0,0 +1,25 @@
+cask 'rpn-scientific' do
+  version '1.2.5'
+  sha256 '96e72827fe0214eb9cc279484a718814b356f2e3e1cd6653314ed213548e83d9'
+
+  url "http://rpnscientific.freehostia.com/RPNScientificV#{version}.dmg"
+  name 'RPN Scientific Calculator'
+  homepage 'http://rpnscientific.freehostia.com/'
+  license :gratis
+
+  app 'RPN Scientific.app'
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.industriouse!.rpnscientific.sfl',
+                '~/Library/Caches/com.IndustriousE!.RPNScientific',
+                '~/Library/Preferences/com.IndustriousE!.RPNScientific.plist',
+                "~/Library/Preferences/com.IndustriousE!.RPNScientificV#{version.major_minor}.data.plist",
+              ]
+
+  caveats <<-EOS.undent
+    Installing the Cask means you have AGREED to the Freeware License Agreement
+    for RPN Scientific Calculator at
+
+      http://rpnscientific.freehostia.com/About.html
+  EOS
+end
