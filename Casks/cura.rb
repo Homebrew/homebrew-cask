760daf831c17dccc5ffabcfbbf62c4e46f04aedb
Alex Alonso <alex4551@gmail.com>
2016-05-25 22:03:28 +0800

    update cura version

--- Casks/cura.rb
+++ Casks/cura.rb
@@ -1,6 +1,6 @@
 cask 'cura' do
-  version '15.04.2'
-  sha256 '48157d41c03d1a8d19edb4c2a7e2356f57d0a6d6a64a50c087962d64b1841a6b'
+  version '15.04.6'
+  sha256 'a3ce29da4d024187e6db1cc2f5643d2084160a2d09be738fe83e1a18d8456ef5'
 
   url "https://software.ultimaker.com/current/Cura-#{version}-MacOS.dmg"
   name 'Cura'
