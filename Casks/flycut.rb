bba1f5506464ded03da19e8c34109dfea02a459c
Eugene <eunikolsky@gmail.com>
2016-05-25 08:54:29 -0700

    Add appcast to flycut (#21430)

--- Casks/flycut.rb
+++ Casks/flycut.rb
@@ -3,6 +3,8 @@ cask 'flycut' do
   sha256 'f7fa218a4fee31208476ff400cd1044f3356bf2200a06e32731572498c7ce42a'
 
   url "https://github.com/TermiT/Flycut/releases/download/#{version}/Flycut.app.#{version}.zip"
+  appcast 'https://github.com/TermiT/Flycut/releases.atom',
+          checkpoint: '02bb24f0420441a85701adebe5e145497146bd9c4923b321df6974042cbbffa4'
   name 'Flycut'
   homepage 'https://github.com/TermiT/Flycut'
   license :mit
