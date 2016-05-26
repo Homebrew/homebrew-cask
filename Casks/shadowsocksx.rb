e35824b6d507e0201f165eb1bd4b2a76b3106857
Register <registerdedicated+github@gmail.com>
2016-05-26 10:59:06 +0800

    updated shadowsocksx (2.6.4)

--- Casks/shadowsocksx.rb
+++ Casks/shadowsocksx.rb
@@ -1,11 +1,15 @@
 cask 'shadowsocksx' do
-  version '2.6.3'
-  sha256 'f0e263dd1d74b0b6977389f2b8b28c524bceccbc5ad24ca8a8164b92ede1c45a'
+  version '2.6.4'
+  sha256 'fe76027d34fcf87e6c9ad9dcf7ba77827dbeb66ac994d9fb27e1cd010045db5a'
 
-  url "https://github.com/shadowsocks/shadowsocks-iOS/releases/download/#{version}/ShadowsocksX-#{version}.dmg"
+  url "https://github.com/RobertYan/ShadowsocksX/releases/download/#{version}/ShadowsocksX.#{version}.zip"
+  appcast 'https://github.com/RobertYan/ShadowsocksX/releases/releases.atom',
+          checkpoint: 'e94cd1ccc01488e99b07a372162ea9e555b6aef0e0911cbb198281f03b975467'
   name 'ShadowsocksX'
   homepage 'https://github.com/shadowsocks/shadowsocks-iOS/wiki/Shadowsocks-for-OSX-Help'
   license :gpl
+
+  depends_on formula: 'libsodium'
 
   app 'ShadowsocksX.app'
 end
