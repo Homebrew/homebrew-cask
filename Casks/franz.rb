3e28d9e1ffd6b6034affe1b16c75371b13162ab1
Jose Salcido <eljose152@gmail.com>
2016-05-26 02:43:35 -0700

    updated franz (0.9.10) (#21454)

--- Casks/franz.rb
+++ Casks/franz.rb
@@ -2,7 +2,7 @@ cask 'franz' do
   version '0.9.10'
   sha256 'bac5896e5cad84d4018e9486cd580a04548c0a0b350ade506b82192175306570'
 
-  url "http://downloads.meetfranz.com/releases/#{version}/Franz-darwin-x64-#{version}.dmg"
+  url "https://github.com/imprecision/franz-app/releases/download/2.0/Franz-darwin-x64-#{version}.dmg"
   appcast 'https://github.com/imprecision/franz-app/releases.atom',
           checkpoint: 'e8032c13996ef3f39bda32345034a2c8d2ba4b03be678c20126e361e9d019f47'
   name 'Franz'
