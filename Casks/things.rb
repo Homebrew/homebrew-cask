90fbce5006acf0876b05024a988282879d13ebe7
Andy Buckingham <andy@andybee.com>
2016-05-26 20:47:57 +0100

    Update things to 2.8.4 (#21450)
    * Add things2 :latest
    
    I've created a new cask for the :latest version of Things, as the
    current 'things' cask seems to be pinned to 1.x releases. Not sure
    if there's an intentional reason for this, hence playing safe with
    a new token.
    
    Please advise if this is not correct.
    
    * Update things to 2.8.4 and remove things2
    
    Based on feedback, replacing "new" things2 cask with modifications to
    existing things cask.
    
    * Updated version, checksum, url, appcast and checkpoint
    * Added verification comment for url/app hostname mismatch
    * Added suppress_move_to_applications

--- Casks/things.rb
+++ Casks/things.rb
@@ -1,13 +1,31 @@
 cask 'things' do
-  version '1.4.8a'
-  sha256 'a489f0071b011e6459672df84a54d39f4169e414b581cfd286dd602c14c96034'
+  version '2.8.4'
+  sha256 'a46b7bd62c39a381a13cdcfc2a9523ac90c0b12a6e7d5c239ef0f042a9457028'
 
-  url "http://culturedcode.cachefly.net/things/Things_#{version}.zip"
-  appcast 'https://downloads.culturedcode.com/things/download/Things_Updates.php',
-          checkpoint: 'cd06fe3a9cbcc04efdaa8d0fb4184a50ceb7e42e5b647e66b13ac9f9eca922f9'
+  # culturedcode.cachefly.net was verified as official when first introduced to the cask
+  url "https://culturedcode.cachefly.net/things/Things_#{version}.zip"
+  appcast 'https://culturedcode.cachefly.net/things/sparkle/sparkle_en.xml',
+          checkpoint: '9b918aa150cd88fe2bf30879792bff1e232d20460db5f2cacb9a132924b552bc'
   name 'Things'
   homepage 'https://culturedcode.com/things/'
   license :commercial
 
   app 'Things.app'
+
+  postflight do
+    suppress_move_to_applications key: 'suppressMoveToApplications'
+  end
+
+  zap delete: [
+                '~/Library/Preferences/com.culturedcode.things.plist',
+                '~/Library/Application Support/Cultured Code',
+                '~/Library/Containers/com.culturedcode.things',
+                '~/Library/Containers/com.culturedcode.things.ActionExtension',
+                '~/Library/Containers/com.culturedcode.things.ShareExtension',
+                '~/Library/Containers/com.culturedcode.things.TodayExtension',
+                '~/Library/Containers/com.culturedcode.thingsreminders',
+                '~/Library/Metadata/com.culturedcode.things',
+                '~/Library/Caches/com.culturedcode.things',
+                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.culturedcode.things',
+              ]
 end
