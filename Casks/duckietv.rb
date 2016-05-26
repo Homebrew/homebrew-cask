3322856873993e3169f7d0421e9df9486c5fbd78
bibnumber <revorot+git@gmail.com>
2016-05-26 17:39:32 +0200

    Adding duckieTV v1.1.3 standalone pkg

---
+++ Casks/duckietv.rb
@@ -0,0 +1,21 @@
+cask 'duckietv' do
+  version '1.1.3'
+  sha256 '4c4ee965cf39db6333dad65fe2b56ebf412af490a1b091487ef5d1fe8989b359'
+
+  url "https://github.com/SchizoDuckie/DuckieTV/releases/download/#{version}/DuckieTV-#{version}-OSX-x64.pkg"
+  appcast 'https://github.com/SchizoDuckie/DuckieTV/releases.atom',
+          checkpoint: 'f11c61dde34cfe2a9f3e7150122340aa3f6680a634e00851066fccd4668e0103'
+  name 'duckieTV'
+  homepage 'https://schizoduckie.github.io/DuckieTV/'
+  license :oss
+
+  container type: :pkg
+
+  pkg "DuckieTV-#{version}-OSX-x64.pkg", allow_untrusted: true
+
+  uninstall pkgutil: 'tv.duckie.base.pkg',
+            delete:  [
+                       '/Applicatons/duckieTV.app',
+                       '~/Library/Application Support/DuckieTV-Standalone',
+                     ]
+end
