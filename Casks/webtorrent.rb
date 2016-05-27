73b3c75e9288b74189f12e7a25f8a97fcb7866c6
Jerzerak <jerzerak@gmail.com>
2016-05-27 14:15:16 -0400

    webtorrent 0.6.1

--- Casks/webtorrent.rb
+++ Casks/webtorrent.rb
@@ -1,11 +1,11 @@
 cask 'webtorrent' do
-  version '0.5.1'
-  sha256 'eeda30d9c4dc8dca7bb2b3fb6fd2c7c2c1aa0eb0bc39b1eb576fd4546dcbe049'
+  version '0.6.1'
+  sha256 'ea8c74a587577765ff892e267268e25df75d24f4b4c39625522aeab553e690d6'
 
   # github.com/feross/webtorrent-desktop was verified as official when first introduced to the cask
   url "https://github.com/feross/webtorrent-desktop/releases/download/v#{version}/WebTorrent-v#{version}.dmg"
   appcast 'https://github.com/feross/webtorrent-desktop/releases.atom',
-          checkpoint: '05338003baf4669cc187b21b917b5025060d5653dda794995f9e2a85c8f4476f'
+          checkpoint: '07c24fd9845dba6bd10ad24fac4a93904b25e4cd87ed1b08f6ce74df608ccf5b'
   name 'WebTorrent Desktop'
   homepage 'https://webtorrent.io/desktop'
   license :mit
