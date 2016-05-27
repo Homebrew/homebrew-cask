c06c127bd0d9f8b771b5fac2ed8eff85a7896a7a
Victor Popkov <victor@popkov.me>
2016-05-27 23:19:45 +0300

    updated taskpaper (3.3.1)

--- Casks/taskpaper.rb
+++ Casks/taskpaper.rb
@@ -1,11 +1,11 @@
 cask 'taskpaper' do
-  version '3.2'
-  sha256 'a91aa7b05992abb229bd9c281ced4be0ed46d2a34d638f6e22a2bbf51eae7511'
+  version '3.3.1'
+  sha256 '94c57b9b34753595ba91e1cdfa454aa1db67b639e03f8237c3d6de83d48f6796'
 
   # amazonaws.com is the official download host per the vendor homepage
   url "https://taskpaper.s3.amazonaws.com/assets/app/TaskPaper-#{version}.dmg"
   appcast 'https://taskpaper.s3.amazonaws.com/TaskPaper.rss',
-          checkpoint: '84bf613325a2eaaf72581c23f6ee9b6c890a4bf47c50fbcf7ac17853206c8acb'
+          checkpoint: 'd7cdc5831a950d1a6f3694e5f7cb241980fdf3535856ff35ade154abd441de43'
   name 'TaskPaper'
   homepage 'http://www.hogbaysoftware.com/products/taskpaper'
   license :commercial
