0da685d86a9ef5d1d1c63e8de4f6c949da6a0a6f
shonjir <shonjir@gmail.com>
2016-05-27 15:37:02 -0700

    updated adobe-dng-converter (9.5.1)

--- Casks/adobe-dng-converter.rb
+++ Casks/adobe-dng-converter.rb
@@ -1,6 +1,6 @@
 cask 'adobe-dng-converter' do
-  version '9.5'
-  sha256 'bd6f69810927d59d687ebe88b08edcf5763acbf471b74f14a30f8677a26c5b45'
+  version '9.5.1'
+  sha256 '4465231586b5a55f7cf1b56843ec7fb30f73a0f9aff177d0e9a65e951796a348'
 
   url "http://download.adobe.com/pub/adobe/dng/mac/DNGConverter_#{version.dots_to_underscores}.dmg"
   name 'Adobe Camera Raw and DNG Converter'
