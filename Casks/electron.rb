b62f8ed30444f77b25ecbb06db6d834889d960cb
Jerzerak <jerzerak@gmail.com>
2016-05-27 12:36:33 -0400

    electron 1.2.0
    - Add zap stanza

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,13 +1,20 @@
 cask 'electron' do
-  version '1.1.1'
-  sha256 'f0a3d8bef18e6ae7016648196f4ce86dc42a6a09ca94b94e2386ecbbcbab5d40'
+  version '1.2.0'
+  sha256 '1ec63d384eaa3494bba8f3c18b9dc764b680764664749f8a1f3f2da6d8198fa4'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
-          checkpoint: '185073c6b84473777f338f55d5e8430d410af64fd116fd78fa9e32f9746c8b00'
+          checkpoint: '0002ed6bec765224b555b4c1214eec1756a99aabcf9d49c430d19f5d433c0473'
   name 'Electron'
   homepage 'http://electron.atom.io/'
   license :mit
 
   app 'Electron.app'
+
+  zap delete: [
+                '~/Library/Application Support/Electron',
+                '~/Library/Caches/Electron',
+                '~/Library/Preferences/com.github.electron.helper.plist',
+                '~/Library/Preferences/com.github.electron.plist',
+              ]
 end
