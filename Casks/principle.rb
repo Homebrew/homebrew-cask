da18aaf4f14766274fd4cfd992d3a88c77214a16
Victor Popkov <victor@popkov.me>
2016-05-27 23:27:55 +0300

    updated principle (1.2.3) (#21492)

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,11 +1,11 @@
 cask 'principle' do
-  version '1.2.1'
-  sha256 '721901792558c4367509b44f4a58c6631617781ab46da7f8a2496b6c157bc84e'
+  version '1.2.3'
+  sha256 '58e0c8ddb9814b009e2e0665a8305c638ed468a1c577f542a6628623fcbce823'
 
   # dropboxusercontent.com is the official download host per the vendor homepage
   url "https://dl.dropboxusercontent.com/u/13897407/Principle_#{version.dots_to_underscores}.zip"
   appcast 'https://dl.dropboxusercontent.com/u/13897407/buildTrain-601A6666-57A4-4C19-BDD3-1387B3CB9719.xml',
-          checkpoint: '6919925da47287b45e2d9748b8bc41e6b5f38098c2311093496783b662a7e838'
+          checkpoint: '94a66544a40b22a629c69320f8d2520ea1a15ab14a9431b45eebfd4de612d556'
   name 'Principle'
   homepage 'http://principleformac.com/'
   license :commercial
