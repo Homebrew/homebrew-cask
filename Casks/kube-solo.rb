9f25614c5c22178652dd07c2565d4ffaff25e9fa
Jerzerak <jerzerak@gmail.com>
2016-05-28 01:04:52 -0400

    kube-solo 0.6.3 (#21510)

--- Casks/kube-solo.rb
+++ Casks/kube-solo.rb
@@ -1,10 +1,10 @@
 cask 'kube-solo' do
-  version '0.6.2'
-  sha256 'f9477950f7365b73704ac8a87e9cdcafe73717eff7feb16fe38cac4e2f7c258d'
+  version '0.6.3'
+  sha256 '3b9ced3e21b82a0982acb4258263083b6441cf30179377ed7a95d57bab2320b9'
 
   url "https://github.com/TheNewNormal/kube-solo-osx/releases/download/v#{version}/Kube-Solo_v#{version}.dmg"
   appcast 'https://github.com/TheNewNormal/kube-solo-osx/releases.atom',
-          checkpoint: '11ab423dcd94e79e2919ccfc81188a71fc410c5643a2fcc896eade0fbdd4cbf7'
+          checkpoint: '129a51caa8e617adbc554fc2f7140facf7b44691103debea7341316243f0421d'
   name 'Kube-Solo'
   homepage 'https://github.com/TheNewNormal/kube-solo-osx'
   license :apache
