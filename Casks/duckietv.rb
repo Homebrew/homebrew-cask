7c36995fe5b7d1edc007a6365fe55f585d6057c7
bibnumber <revorot+git@gmail.com>
2016-05-28 19:34:47 +0200

    Update duckietv
    added comment for first introducing to cask.

--- Casks/duckietv.rb
+++ Casks/duckietv.rb
@@ -2,6 +2,7 @@ cask 'duckietv' do
   version '1.1.3'
   sha256 '4c4ee965cf39db6333dad65fe2b56ebf412af490a1b091487ef5d1fe8989b359'
 
+  # schizoduckie.github.io/DuckieTV was verified as official homepage when first introduced to the cask
   url "https://github.com/SchizoDuckie/DuckieTV/releases/download/#{version}/DuckieTV-#{version}-OSX-x64.pkg"
   appcast 'https://github.com/SchizoDuckie/DuckieTV/releases.atom',
           checkpoint: 'f11c61dde34cfe2a9f3e7150122340aa3f6680a634e00851066fccd4668e0103'
