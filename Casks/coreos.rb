465f00186eee37427aba8e89cf26120a9dfddc02
Jerzerak <jerzerak@gmail.com>
2016-05-28 01:04:34 -0400

    coreos 1.2.9 (#21509)

--- Casks/coreos.rb
+++ Casks/coreos.rb
@@ -1,10 +1,10 @@
 cask 'coreos' do
-  version '1.2.8'
-  sha256 '846c2b7b96ec897b603383051e36bf4b06dc95f07a5cdbd9c1e429f04f82a4f4'
+  version '1.2.9'
+  sha256 '3d59b7af835f2d643a0d0ec56d21be1d4b0822afb5be3f8c5a3bb86ffa71bf03'
 
   url "https://github.com/TheNewNormal/coreos-osx/releases/download/v#{version}/CoreOS_v#{version}.dmg"
   appcast 'https://github.com/TheNewNormal/coreos-osx/releases.atom',
-          checkpoint: '74f6ba0620644f379f489e50c1c83a4c3b5e9946ec87c91c0d27e4b6269847c8'
+          checkpoint: '4702a0df004b01e142f337970ce12439b3558866923bdbb60a64e900909ea1e0'
   name 'CoreOS VM'
   homepage 'https://github.com/TheNewNormal/coreos-osx'
   license :apache
