7492b309bde93dc140ce5570d620875285f9f199
Chris 'sh0shin' Frage <git@sh0shin.org>
2016-05-28 16:35:01 +0200

    updated seafile-client (5.1.1)

--- Casks/seafile-client.rb
+++ Casks/seafile-client.rb
@@ -1,6 +1,6 @@
 cask 'seafile-client' do
-  version '5.0.7'
-  sha256 '06be9e54b022ff5ed7e5221bd4d3218eb50cd74a4f0e549c8adc04af36b6500e'
+  version '5.1.1'
+  sha256 '9b24efc4e26cc587fd7e69f5d2dc9f3f447071460996d511b132c68e5521b019'
 
   # bintray.com/artifact/download/seafile-org was verified as official when first introduced to the cask
   url "https://bintray.com/artifact/download/seafile-org/seafile/seafile-client-#{version}.dmg"
