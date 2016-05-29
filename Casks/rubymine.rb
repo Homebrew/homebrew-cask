ec8314229ad34f49930665ab083be80976a3d39c
Jerzerak <jerzerak@gmail.com>
2016-05-29 00:37:56 -0400

    RubyMine 2016.1.1b (#21522)
    - Add `conflicts_with` stanza
    - Add `uninstall` stanza
    - Amend `zap` stanza

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -1,19 +1,22 @@
 cask 'rubymine' do
-  version '2016.1.1'
-  sha256 '9fa6bb7a95acd5821bab2c43f9e83a95e68508be250f1fae05288414716094c5'
+  version '2016.1.1b'
+  sha256 '86ada622827c32bb8dc646f7696bfa63a33e9f0386fb6ab30c89391b3fd5e67b'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version}.dmg"
   name 'RubyMine'
   homepage 'https://www.jetbrains.com/ruby/'
   license :commercial
 
+  conflicts_with cask: 'rubymine-eap'
+
   app 'RubyMine.app'
+
+  uninstall delete: '/usr/local/bin/mine'
 
   zap delete: [
                 "~/Library/Application Support/RubyMine#{version.major_minor}",
                 "~/Library/Preferences/RubyMine#{version.major_minor}",
                 "~/.RubyMine#{version.major_minor}",
-                '~/Library/Preferences/com.jetbrains.RubyMine.plist',
                 "~/Library/Caches/RubyMine#{version.major_minor}",
                 "~/Library/Logs/RubyMine#{version.major_minor}",
               ]
