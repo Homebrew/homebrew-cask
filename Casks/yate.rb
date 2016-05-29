7cfce1ece4203f313f68a0dc2ab6346d9c479aee
Aditya Dalal <adidalal@users.noreply.github.com>
2016-05-29 01:22:11 -0400

    updated yate (3.12) (#21529)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,8 +1,9 @@
 cask 'yate' do
   version '3.12'
-  sha256 'f62d235272434ac206ae347392d82f2c8ff0cb52314343bb0ac0794703e880d9'
+  sha256 '6611b12ef7336e1add86e35345aa2605fde79248c23354736a14384482a72eff'
 
-  url 'https://2manyrobots.com/Builds/Yate/Yate.dmg'
+  url 'https://2manyrobots.com/Updates/Yate/Yate.zip',
+      user_agent: :fake
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
           checkpoint: '50770d2e20a73b0d2cdbd4ad663650e3ee1d963431dfb23b803912df950573b8'
   name 'Yate'
