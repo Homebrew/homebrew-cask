52851480cef1c407845c074c4db93e7a45995a7a
Victor Popkov <victor@popkov.me>
2016-05-29 23:04:03 +0300

    updated postgres (9.5.3) (#21534)

--- Casks/postgres.rb
+++ Casks/postgres.rb
@@ -1,11 +1,11 @@
 cask 'postgres' do
-  version '9.5.2'
-  sha256 'c0efe0a0d975c5b1fb5bff1f3b5b6c16cb516beb3024413cabaa8c6fa562fa4a'
+  version '9.5.3'
+  sha256 '751b72b270cac9fcd35abbd761829b132e646d664fa1acca28a00feb11a7b26c'
 
   # github.com/PostgresApp/PostgresApp was verified as official when first introduced to the cask
   url "https://github.com/PostgresApp/PostgresApp/releases/download/#{version}/Postgres-#{version}.zip"
   appcast 'https://github.com/PostgresApp/PostgresApp/releases.atom',
-          checkpoint: 'c511f629f055cdad9d882f957a26349cbb0c9f85f7274d92bf97e1e639ba35e2'
+          checkpoint: '8f75d7d7fd811ee6135147a9caf6d4ed9807945cb83d2074aac283b458d3d1c9'
   name 'Postgres'
   homepage 'http://postgresapp.com/'
   license :oss
