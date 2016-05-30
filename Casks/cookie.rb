fa12f2285848dd4e04361a0b52c14cb07c6f57b2
Jussi Tuominen <me@jussituominen.me>
2016-05-30 22:14:28 +0300

    updated cookie (5.0.8) (#21543)

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.0.7'
-  sha256 '736bc774b3fd73f439be4a33fbec67a7d6aed6e971df8c40b0f7315db7c4954f'
+  version '5.0.8'
+  sha256 '39507c5e8d63def19e57b66c1e95eaa92c4b59c5619a1641292788697784c564'
 
-  url 'https://sweetpproductions.com/products/cookie5/Cookie5.dmg'
+  url 'https://sweetpproductions.com/products/cookie5/Cookie5.zip'
   appcast 'https://sweetpproductions.com/products/cookie5/appcast.xml',
-          checkpoint: '1f5dd90f3fde985de192b92eda0440c70d29e5eabd22c9565bbad44bcc110e6a'
+          checkpoint: '585e29b6e2cd3552b1b5958dfab6bc607e07f133d7098734b5bbad425b0a85a3'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
   license :commercial
