31bd3bc67246dec13d3d2b79ec97a504d798d3ce
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2016-05-30 21:37:20 +0200

    Add Workcraft v3.1.0

---
+++ Casks/workcraft.rb
@@ -0,0 +1,11 @@
+cask 'workcraft' do
+  version '3.1.0'
+  sha256 '90a82bd05eec11c118b92078cf1e03f23b5bd148e019c9e0f61a341be04d516f'
+
+  url "http://www.workcraft.org/_media/download/workcraft-v#{version}-osx.tar.gz"
+  name 'Workcraft'
+  homepage 'http://www.workcraft.org/'
+  license :oss
+
+  app 'Workcraft.app'
+end
