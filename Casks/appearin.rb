5b88a7a9babd056dec6055643f8f6e4abbcc2bc8
Vítor Galvão <info@vitorgalvao.com>
2016-05-31 21:26:34 +0100

    added appear.in (unofficial app) 1.0.0 (#21568)

---
+++ Casks/appearin.rb
@@ -0,0 +1,22 @@
+cask 'appearin' do
+  version '1.0.0'
+  sha256 'eacd73dad26341235eba953f533cd1bc59f2d6e1e07d249b56e709f00aca5e82'
+
+  url "https://github.com/vitorgalvao/appear.in/releases/download/#{version}/appear.in-osx-#{version}.zip"
+  appcast 'https://github.com/vitorgalvao/appear.in/releases.atom',
+          checkpoint: '4640526b621a7f622b852eebb3940bb7c3ce6773e4679130efde1ba1fc5296a2'
+  name 'Fog'
+  homepage 'https://github.com/vitorgalvao/appear.in'
+  license :public_domain
+
+  app 'appear.in.app'
+
+  uninstall quit: 'com.vitorgalvao.appear.in'
+
+  zap delete: [
+                '~/Library/Application Support/appear.in',
+                '~/Library/Caches/appear.in',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.vitorgalvao.appear.in.sfl',
+                '~/Library/Saved Application State/com.vitorgalvao.appear.in.savedState',
+              ]
+end
