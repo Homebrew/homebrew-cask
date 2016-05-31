dc09992ec3a4f9bbe5a2fb97ed2ab55e64cbccff
D. Kress <kressnerd@users.noreply.github.com>
2016-05-31 17:54:15 +0200

    Thunderbird 45.1.1 released

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird' do
-  version '45.1.0'
-  sha256 'f298ed10a0989523e7884bbe712d56c1b15e080c1928dcbf195932af9b49ae8b'
+  version '45.1.1'
+  sha256 'bf0a169794938900e607c293b261f2b9a327bfd2801ae6967ffa33cac508e38a'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   appcast "https://aus5.mozilla.org/update/3/Thunderbird/#{version}/0/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/Darwin%2015.3.0/default/default/update.xml?force=1",
