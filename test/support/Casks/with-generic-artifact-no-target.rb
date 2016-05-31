2b5b575da20688a4d0af8c5dbfc64580dfbaee22
Josh Hagins <hagins.josh@gmail.com>
2016-05-31 07:30:08 -0400

    Require target for generic artifact (#21549)
    * Require target for generic artifact
    
    * Add audit check for generic artifacts

--- test/support/Casks/with-generic-artifact-no-target.rb
+++ test/support/Casks/with-generic-artifact-no-target.rb
@@ -1,9 +1,9 @@
-test_cask 'with-generic-artifact' do
+test_cask 'with-generic-artifact-no-target' do
   version '1.2.3'
   sha256 '9203c30951f9aab41ac294bbeb1dcef7bed401ff0b353dcb34d68af32ea51853'
 
   url TestHelper.local_binary_url('caffeine.zip')
   homepage 'http://example.com/with-generic-artifact'
 
-  artifact 'Caffeine.app', target: "#{Hbc.appdir}/Caffeine.app"
+  artifact 'Caffeine.app'
 end
