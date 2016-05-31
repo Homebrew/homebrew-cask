7e98fa1d7bcdbafc977875514b277c14610bbf93
Donny Kurnia <donnykurnia@gmail.com>
2016-05-31 16:26:07 +0700

    Upgrade Opera.app to version 37.0.2178.54 (#21550)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '37.0.2178.43'
-  sha256 '0facd8ad0003fe7c9f6115ebb6b1d6354c2ba477164c229b19ea61464170e476'
+  version '37.0.2178.54'
+  sha256 'a9d0a986bf9eea3ba6c781bbaf34e3ccb774c16b8066c59923fc95bb79de20de'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
