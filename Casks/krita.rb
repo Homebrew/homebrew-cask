94107edd112817a2640fefdc668022cf49eaf984
Steffen Hansen <sgnh@users.noreply.github.com>
2016-05-31 18:17:14 +0200

    updated krita (3.0) (#21553)

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -1,9 +1,9 @@
 cask 'krita' do
-  version '2.9.11.0'
-  sha256 '0be9babe1c08678780c66b9039c6fdde1e67875ddb662c17eaf87ff52fe314c2'
+  version '3.0'
+  sha256 '8c678bdf6804c5b8decf08093fce809f2c78f664604dfe91b4ded91b3b5e4e00'
 
-  # kde.org is the official download host per the vendor homepage
-  url "https://files.kde.org/krita/osx/krita-#{version}.dmg"
+  # kde.org/krita was verified as official when first introduced to the cask
+  url "https://files.kde.org/krita/#{version.major}/osx/krita-#{version}.dmg"
   name 'Krita'
   homepage 'https://krita.org/'
   license :gpl
