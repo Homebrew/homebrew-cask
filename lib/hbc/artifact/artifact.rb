eeee6f705301db9adaf697abc76a41d00d807da8
Josh Hagins <jhagins@palantir.com>
2016-05-31 02:11:33 -0400

    Require target for generic artifact

--- lib/hbc/artifact/artifact.rb
+++ lib/hbc/artifact/artifact.rb
@@ -6,4 +6,13 @@ class Hbc::Artifact::Artifact < Hbc::Artifact::Symlinked
   def self.artifact_dirmethod
     :appdir
   end
+
+  def load_specification(artifact_spec)
+    source_string, target_hash = artifact_spec
+    raise Hbc::CaskInvalidError.new(@cask.token, 'no source given for artifact') if source_string.nil?
+    @source = @cask.staged_path.join(source_string)
+    raise Hbc::CaskInvalidError.new(@cask.token, "target required for generic artifact #{source_string}") unless target_hash.is_a?(Hash)
+    target_hash.assert_valid_keys(:target)
+    @target = Pathname.new(target_hash[:target])
+  end
 end
