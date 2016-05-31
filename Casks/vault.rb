9160015bab3fe3c74580241e68783cbcfbdc2042
MansM <cybermans@gmail.com>
2016-05-31 19:03:09 +0200

    0.5.2 -> 0.5.3
    0.5.2 -> 0.5.3

--- Casks/vault.rb
+++ Casks/vault.rb
@@ -1,6 +1,6 @@
 cask 'vault' do
-  version '0.5.2'
-  sha256 '48bf1d66cc3b81293186fd458f63fc2b02344aec5f1490c9b9a2915831c13d33'
+  version '0.5.3'
+  sha256 '31e7eff07c202cf2166ac63457054da59a1f4f49e7ad079b38316efadbb79e32'
 
   # hashicorp.com/vault was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vault/#{version}/vault_#{version}_darwin_amd64.zip"
