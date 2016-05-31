40c0c1f95f25c7c2668291ec03e9901aec1767a1
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-05-30 22:49:43 -0300

    updated fantastical (2.2.3) (#21547)

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,10 +1,10 @@
 cask 'fantastical' do
-  version '2.2.2'
-  sha256 '53fc7655c095616424a2b7e89d3a6f48edf6119acf676af33f2c3bf01d3febb2'
+  version '2.2.3'
+  sha256 'd8adf22932a33acccce187fb48805beb509679e8298ceb00941f2f37398f0aa7'
 
   url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast 'https://flexibits.com/fantastical/appcast2.php',
-          checkpoint: '872e73fcc7c824dfe59f9daa3e9958a8bdb69c238d48d4fed9ca3a32e6932acd'
+          checkpoint: '9b6991140a00b5a096228d1ca8b1f378422a361d18aff0432814949e60a5a169'
   name 'Fantastical'
   homepage 'https://flexibits.com/fantastical'
   license :freemium
