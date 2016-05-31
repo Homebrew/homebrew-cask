ca9803ff29b1529b7287299316f4a2175353b4c9
Dave Lowe <dlowe@iinet.net.au>
2016-06-01 06:49:02 +1000

    Update maltego-classic.rb
    Changed the app and url to use the builtin version methods. @vitorgalvao

--- Casks/maltego-classic.rb
+++ Casks/maltego-classic.rb
@@ -2,10 +2,11 @@ cask 'maltego-classic' do
   version '4.0.0.8846'
   sha256 '1fd90f884f40a6b2a7530b8e081b6b9f7d21cdaf53a52a6dc800034a85af84be'
 
-  url "https://www.paterva.com/malv#{version.sub(%r{^(\d+)\..*}, '\1\2')}/classic/Maltego.v#{version}.dmg"
+  url "https://www.paterva.com/malv#{version.major}/classic/Maltego.v#{version}.dmg"
   name 'Paterva Maltego'
   homepage 'https://www.paterva.com/web7/buy/maltego-clients/maltego.php'
   license :closed
 
-  app "Maltego Classic v#{version.sub(%r{\.\d+$}, '')}.app"
+  app "Maltego Classic v#{version.major_minor_patch}.app"
+
 end
