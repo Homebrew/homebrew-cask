661c06310f15ee151b525ecd199d800038a73565
D. Kress <kressnerd@users.noreply.github.com>
2016-06-01 12:23:29 +0200

    Update thunderbird.rb 45.1.1 checkpoint

--- Casks/thunderbird.rb
+++ Casks/thunderbird.rb
@@ -4,7 +4,7 @@ cask 'thunderbird' do
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   appcast "https://aus5.mozilla.org/update/3/Thunderbird/#{version}/0/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/Darwin%2015.3.0/default/default/update.xml?force=1",
-          checkpoint: '45a3b54afef941e2843951f10ad5663235598d6fd256a72054b79a9871e9f7dc'
+          checkpoint: '121eb72f0664730809c856d3afecb9eefc7682b809881a8f0131250c947ef9ca'
   name 'Mozilla Thunderbird'
   homepage 'https://www.mozilla.org/en-US/thunderbird/'
   license :mpl
