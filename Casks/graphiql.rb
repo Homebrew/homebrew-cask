43c0a44a98ffce5d75a394d5381650549fb462db
Garen Torikian <gjtorikian@users.noreply.github.com>
2016-06-01 16:58:54 -0700

    Bump GraphiQL to 0.4.1 (#21612)

--- Casks/graphiql.rb
+++ Casks/graphiql.rb
@@ -1,10 +1,10 @@
 cask 'graphiql' do
-  version '0.4.0'
-  sha256 '7f4c775a0f740363c3dd2e3881cdaf8456b5c5a0b6dc517e44fc2f598c5fed86'
+  version '0.4.1'
+  sha256 '34946a4503a193d6ec8d9fc7c84324d1fd585cd86605f36c7701c809fd398d58'
 
   url "https://github.com/skevy/graphiql-app/releases/download/v#{version}/GraphiQL.app.zip"
   appcast 'https://github.com/skevy/graphiql-app/releases.atom',
-          checkpoint: '7f4c775a0f740363c3dd2e3881cdaf8456b5c5a0b6dc517e44fc2f598c5fed86'
+          checkpoint: '34946a4503a193d6ec8d9fc7c84324d1fd585cd86605f36c7701c809fd398d58'
   name 'GraphiQL App'
   homepage 'https://github.com/skevy/graphiql-app'
   license :mit
