Homebrew/homebrew-cask

commit 6500fa1fd916fcf45f2f4820b03e348012734d7f

Force overwrite artifacts on `--force` reinstall
This commit changes the behavior of a `Moved` artifact such that if the target already exists, `brew cask install --force` will remove the existing target before moving the staged artifact.

In that case, the warning message will say *overwriting* instead of *not moving*.

The behavior of plain `brew cask install` remains unchanged; the same goes for the warning message for that case.

lib/hbc/installer.rb
@@ -125,7 +125,8 @@ class Hbc::Installer
     odebug "#{artifacts.length} artifact/s defined", artifacts
     artifacts.each do |artifact|
       odebug "Installing artifact of class #{artifact}"
-      artifact.new(@cask, @command).install_phase
+      options = { command: @command, force: force }
+      artifact.new(@cask, options).install_phase
     end
   end
 
@@ -293,7 +294,8 @@ class Hbc::Installer
     odebug "#{artifacts.length} artifact/s defined", artifacts
     artifacts.each do |artifact|
       odebug "Un-installing artifact of class #{artifact}"
-      artifact.new(@cask, @command).uninstall_phase
+      options = { command: @command, force: force }
+      artifact.new(@cask, options).uninstall_phase
     end
   end
 

lib/hbc/artifact/base.rb
@@ -24,6 +24,8 @@ class Hbc::Artifact::Base
      cask.artifacts[self.artifact_dsl_key].any?
   end
 
+  attr_reader :force
+
   def zap_phase
     odebug "Nothing to do. The #{self.class.artifact_name} artifact has no zap phase."
   end
@@ -76,8 +78,9 @@ class Hbc::Artifact::Base
     {}
   end
 
-  def initialize(cask, command=Hbc::SystemCommand)
+  def initialize(cask, options={})
     @cask = cask
-    @command = command
+    @command = options.fetch(:command, Hbc::SystemCommand)
+    @force = options.fetch(:force, false)
   end
 end

lib/hbc/artifact/moved.rb
@@ -22,6 +22,7 @@ class Hbc::Artifact::Moved < Hbc::Artifact::Base
     each_artifact do |artifact|
       load_specification(artifact)
       next unless preflight_checks
+      delete if File.exist?(target)
       move
     end
   end
@@ -64,14 +65,28 @@ class Hbc::Artifact::Moved < Hbc::Artifact::Base
 
   def preflight_checks
     if target.exist?
-      ohai "It seems there is already #{self.class.artifact_english_article} #{english_name} at '#{target}'; not moving."
-      return false
+      if force
+        ohai(warning_target_exists { |s| s << 'overwriting.' })
+      else
+        ohai(warning_target_exists { |s| s << 'not moving.' })
+        return false
+      end
     end
     unless source.exist?
       message = "It seems the #{english_name} source is not there: '#{source}'"
       raise Hbc::CaskError.new(message)
     end
     true
+  end
+
+  def warning_target_exists
+    message_parts = [
+      "It seems there is already #{
+        self.class.artifact_english_article} #{english_name
+        } at '#{target}'"
+    ]
+    yield(message_parts) if block_given?
+    message_parts.join('; ')
   end
 
   def delete

test/cask/artifact/pkg_test.rb
@@ -10,7 +10,8 @@ describe Hbc::Artifact::Pkg do
 
   describe 'install_phase' do
     it 'runs the system installer on the specified pkgs' do
-      pkg = Hbc::Artifact::Pkg.new(@cask, Hbc::FakeSystemCommand)
+      pkg = Hbc::Artifact::Pkg.new(@cask,
+        command: Hbc::FakeSystemCommand)
 
       Hbc::FakeSystemCommand.expects_command(['/usr/bin/sudo', '-E', '--', '/usr/sbin/installer', '-pkg', @cask.staged_path.join('MyFancyPkg','Fancy.pkg'), '-target', '/'])
 
@@ -22,7 +23,8 @@ describe Hbc::Artifact::Pkg do
 
   describe 'uninstall_phase' do
     it 'does nothing, because the uninstall_phase method is a no-op' do
-      pkg = Hbc::Artifact::Pkg.new(@cask, Hbc::FakeSystemCommand)
+      pkg = Hbc::Artifact::Pkg.new(@cask,
+        command: Hbc::FakeSystemCommand)
       shutup do
         pkg.uninstall_phase
       end

test/cask/artifact/uninstall_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 describe Hbc::Artifact::Uninstall do
   let(:cask) { Hbc.load('with-installable') }
-  let(:uninstall_artifact) { Hbc::Artifact::Uninstall.new(cask, Hbc::FakeSystemCommand) }
+
+  let(:uninstall_artifact) do
+    Hbc::Artifact::Uninstall.new(cask, command: Hbc::FakeSystemCommand)
+  end
 
   before {
     shutup do

test/cask/artifact/zap_test.rb
@@ -4,7 +4,10 @@ require 'test_helper'
 # - test that zap removes an alternate version of the same Cask
 describe Hbc::Artifact::Zap do
   let(:cask) { Hbc.load('with-installable') }
-  let(:zap_artifact) { Hbc::Artifact::Zap.new(cask, Hbc::FakeSystemCommand) }
+
+  let(:zap_artifact) do
+    Hbc::Artifact::Zap.new(cask, command: Hbc::FakeSystemCommand)
+  end
 
   before {
     shutup do

test/cask/cli/install_test.rb
@@ -39,7 +39,7 @@ describe Hbc::CLI::Install do
 
     TestHelper.must_output(self, lambda {
       Hbc::CLI::Install.run('local-transmission', '--force')
-    }, %r{==> Success! local-transmission staged at '#{Hbc.caskroom}/local-transmission/2.61' \(487 files, 11M\)})
+    }, %r{==> Success! local-transmission staged at '#{Hbc.caskroom}/local-transmission/2.61' \(0B\)})
   end
 
   it "properly handles Casks that are not present" do

commit 82386c9c205cef355bea8e174c27184fe36a98c6

Fix `zap` regression introduced in 6500fa1; ref #21583
This PR fixes an issue with 6500fa1, in which the (internal) installer API was refactored in preparation for the big change. That refactoring missed one case, causing `zap` to break; see #21583.

There are **no tests** included with this fix due to pre-existing coverage issues. Test will follow in a separate commit.

lib/hbc/installer.rb
@@ -304,7 +304,7 @@ class Hbc::Installer
     uninstall_artifacts
     if Hbc::Artifact::Zap.me?(@cask)
       ohai "Dispatching zap stanza"
-      Hbc::Artifact::Zap.new(@cask, @command).zap_phase
+      Hbc::Artifact::Zap.new(@cask, command: @command).zap_phase
     else
       opoo "No zap stanza present for Cask '#{@cask}'"
     end