6aff13aeb95ee9455d31097c6a43813a6fe5d546
Andreas Zeidler <az@zitc.de>
2016-06-02 15:41:00 +0200

    Add Crypho.app v2.7.4 (#21623)

---
+++ Casks/crypho.rb
@@ -0,0 +1,11 @@
+cask 'crypho' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.crypho.com/downloads/Crypho.dmg'
+  name 'Crypho'
+  homepage 'https://www.crypho.com'
+  license :closed
+
+  app 'Crypho.app'
+end
