7ccef589c0dbd2971072c1bb574bfee5732cd298
Koen Rouwhorst <koenrh@users.noreply.github.com>
2016-06-02 08:39:02 +0200

    updated kextviewr (1.1.0) (#21620)

--- Casks/kextviewr.rb
+++ Casks/kextviewr.rb
@@ -1,6 +1,6 @@
 cask 'kextviewr' do
-  version '1.0.1'
-  sha256 '19eaddfcce40c7608bd0f9b4df1e61277c57896dc7e666e237866260e2b17073'
+  version '1.1.0'
+  sha256 'bf57dc55adc7179914a452eeacf553952e86b4e1ff2406875a9a0b21932684ca'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/KextViewr_#{version}.zip"
