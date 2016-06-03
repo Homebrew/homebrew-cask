f9002683f28efe664fe619c76b5d419b9d2a473e
Luke Adams <luke4450@gmail.com>
2016-06-03 17:16:19 -0500

    [Steam cask] add HTTPS download url
    payload is identical

--- Casks/steam.rb
+++ Casks/steam.rb
@@ -2,7 +2,7 @@ cask 'steam' do
   version :latest
   sha256 :no_check
 
-  url 'http://media.steampowered.com/client/installer/steam.dmg'
+  url 'https://steamcdn-a.akamaihd.net/client/installer/steam.dmg'
   name 'Steam'
   homepage 'http://store.steampowered.com/about/'
   license :gratis
