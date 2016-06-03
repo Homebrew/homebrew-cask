5061a319ee26fbecba8da0a9136829852c961da6
John Jelinek IV <johnjelinek@users.noreply.github.com>
2016-06-03 10:04:45 -0500

    updated vivaldi (1.2.490.39) (#21652)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,6 +1,6 @@
 cask 'vivaldi' do
-  version '1.1.453.59'
-  sha256 'bf6baf4c3e9068e79a29413453f2d2ff48f0809e9874a7bbd647afbded92889f'
+  version '1.2.490.39'
+  sha256 'dc159a6f3cbf6d32796646d0746e7dfabd9869b9ae18c83b2357c218d68816b0'
 
   url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.dmg"
   name 'Vivaldi'
