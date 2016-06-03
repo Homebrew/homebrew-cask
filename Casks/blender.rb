a00f4be3fdb42c22bcc0af05b22578e27ec0dbc0
Vítor Galvão <info@vitorgalvao.com>
2016-06-03 15:13:12 +0100

    blender.rb: Hbc.appdir to appdir

--- Casks/blender.rb
+++ Casks/blender.rb
@@ -18,8 +18,8 @@ cask 'blender' do
     pythonversion = '3.4'
     File.open(shimscript, 'w') do |f|
       f.puts '#!/bin/bash'
-      f.puts "export PYTHONHOME=#{Hbc.appdir}/blender.app/Contents/Resources/#{version}/python/lib/python#{pythonversion}"
-      f.puts "#{Hbc.appdir}/blender.app/Contents/MacOS/blender $@"
+      f.puts "export PYTHONHOME=#{appdir}/blender.app/Contents/Resources/#{version}/python/lib/python#{pythonversion}"
+      f.puts "#{appdir}/blender.app/Contents/MacOS/blender $@"
       FileUtils.chmod '+x', f
     end
   end
