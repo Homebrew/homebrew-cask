e494603765c5485817d059aba630a6561953d5c9
Hervé Yviquel <hyviquel@gmail.com>
2016-06-03 16:39:29 -0300

    updated kid3 (3.4.0) (#21661)

--- Casks/kid3.rb
+++ Casks/kid3.rb
@@ -1,7 +1,7 @@
 cask 'kid3' do
   # note: "3" is not a version number, but an intrinsic part of the product name (ID3 tags)
-  version '3.3.1'
-  sha256 '2bd2349679f6cfc81b1b2484a7ad85263df9c130b90e0d70dad93e16947a7348'
+  version '3.4.0'
+  sha256 'ab96cf402d1546e5d3c8adb28e49832107501f15a65c78d7472016d9b5565632'
 
   url "http://downloads.sourceforge.net/sourceforge/kid3/kid3-#{version}-Darwin.dmg"
   name 'Kid3'
