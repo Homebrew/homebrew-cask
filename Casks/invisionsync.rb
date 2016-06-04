fad6cae912572207c49c4135d31e76b57d4d4b4b
Victor Popkov <victor@popkov.me>
2016-06-04 23:32:30 +0300

    updated invisionsync (1.7.24-678)

--- Casks/invisionsync.rb
+++ Casks/invisionsync.rb
@@ -1,10 +1,10 @@
 cask 'invisionsync' do
-  version '1.7.23-676'
-  sha256 'e99f7275d0b6e5c97b7de2d3c554e6058f11636c65d94ddf0765f9d62face21d'
+  version '1.7.24-678'
+  sha256 '8a7eedfb87f0e8d5c092d054e4bb0fee17c0dee3cde74d2a96628b99687569c2'
 
   url "https://projects.invisionapp.com/native_app/mac/sparkle/#{version.sub(%r{^.*?-}, '')}.zip"
   appcast 'https://projects.invisionapp.com/native_app/mac/sparkle/appcast_v2.xml',
-          checkpoint: '497c1f37a3bcc4ab7527e479069b9c8eddf8e7db036dc1dbae69668075554750'
+          checkpoint: '6131c1e35f72ea3e993b5054ffec8298ee85ddf8bdb3ca105a590eb45583e0e8'
   name 'InVision Sync'
   homepage 'http://invisionapp.com/'
   license :gratis
