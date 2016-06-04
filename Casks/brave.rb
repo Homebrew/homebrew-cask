c315e1561fa44c3213a80fc59e5a37fef39f2493
Koen Rouwhorst <info@koenrouwhorst.nl>
2016-06-04 15:05:35 +0200

    updated brave (0.10.2dev)

--- Casks/brave.rb
+++ Casks/brave.rb
@@ -1,6 +1,6 @@
 cask 'brave' do
-  version '0.10.1dev'
-  sha256 '5f314f403fcd4924ae32e34617db0bd80ab29c14090368efc78f38b816668752'
+  version '0.10.2dev'
+  sha256 'a51fba336acf34005a236aeaf4ef4ba0b63fc0b4e04304c20af79fc83bfe880f'
 
   # github.com/brave/browser-laptop was verified as official when first introduced to the cask
   url "https://github.com/brave/browser-laptop/releases/download/v#{version}/Brave.dmg"
