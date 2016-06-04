de311428b78a3f8dbb0fe5208f039da62f3f155e
Luke Adams <luke4450@gmail.com>
2016-06-03 19:04:16 -0500

    [Steam cask] add HTTPS download url (#21668)

--- Casks/steam.rb
+++ Casks/steam.rb
@@ -2,7 +2,8 @@ cask 'steam' do
   version :latest
   sha256 :no_check
 
-  url 'http://media.steampowered.com/client/installer/steam.dmg'
+  # steamcdn-a.akamaihd.net was verified as official when first introduced to the cask
+  url 'https://steamcdn-a.akamaihd.net/client/installer/steam.dmg'
   name 'Steam'
   homepage 'http://store.steampowered.com/about/'
   license :gratis
