a982f4fbc9c78c678c6097a1f5d37a1dd4d8b0ef
Victor Popkov <victor@popkov.me>
2016-06-04 23:41:24 +0300

    updated catch (1.9.4) (#21688)

--- Casks/catch.rb
+++ Casks/catch.rb
@@ -1,11 +1,11 @@
 cask 'catch' do
-  version '1.9.3'
-  sha256 'a68ec939abe55d25b992047a6f88b448f9aad25b545c2e9b137e00296c40eec1'
+  version '1.9.4'
+  sha256 'a57c48fabe764c3bfe06747f669912b7e9787fa830e30b6d31640d44540c2414'
 
   # github.com/mipstian/catch was verified as official when first introduced to the cask
   url "https://github.com/mipstian/catch/releases/download/#{version}/Catch-#{version}.zip"
   appcast 'https://github.com/mipstian/catch/releases.atom',
-          checkpoint: '1ec55b0baef1cc8729e826f130133bb1fd3c291bb87251111a0a2ed1d7289a0a'
+          checkpoint: '0b4257bc1e21f6c4ccc16c4b3f7f16da6106cfae5bd22b18c960a6991b0463dc'
   name 'Catch'
   homepage 'http://www.giorgiocalderolla.com/index.html#catch'
   license :oss
