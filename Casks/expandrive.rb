925ee2f10bf9dc2ccad126ffce0d6b743fd98d01
Victor Popkov <victor@popkov.me>
2016-06-04 23:29:25 +0300

    updated expandrive (5.3.0)

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,10 +1,10 @@
 cask 'expandrive' do
-  version '5.1.9'
-  sha256 '424d8ebceeddf54ede46c39310723cfba6335e553daa2a148fae1de1e9a301aa'
+  version '5.3.0'
+  sha256 '4cc93c9c71b295d43e2482f1a16e286747c7cafaf7d8725ee15e37ed17268aa5'
 
   url "http://updates.expandrive.com/apps/expandrive/v/#{version.dots_to_hyphens}/download.dmg"
   appcast 'http://updates.expandrive.com/appcast/expandrive.xml?version=3',
-          checkpoint: 'ff6d5e798375459a2ffc3a7996108c6f78b6ce05637728213854b705be4dc70a'
+          checkpoint: '9d4c58aeb0bf291ce46cce2008bdcbd7e8f1f840efd4d9d993cd6c07901f1f5d'
   name 'ExpanDrive'
   homepage 'https://www.expandrive.com/expandrive'
   license :commercial
