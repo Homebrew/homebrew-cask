d6d1b7a6e762a95187683cf7d6bd7f52b6bbf05f
Victor Popkov <victor@popkov.me>
2016-06-04 23:33:49 +0300

    updated mountain-duck (1.4.0.4007)

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,10 +1,10 @@
 cask 'mountain-duck' do
-  version '1.3.1.3743'
-  sha256 '735bd8444c37b7f72c8d36aee5d49873319a63e0a619fd2d645acab81a166c7e'
+  version '1.4.0.4007'
+  sha256 '8f940abdea35f10fed133f5740385d7355f64bd08af25ba33d87e2823e4d5f9d'
 
   url "https://dist.mountainduck.io/Mountain%20Duck-#{version}.zip"
   appcast 'https://version.mountainduck.io/changelog.rss',
-          checkpoint: '148b8fdc7730b288726b2a0c86d706567af586331859e227505d16cf9a20ab44'
+          checkpoint: '41ed60c4059267cd1bd3a182e4bccd0d2c16233175c79cacde6945efdb1cae07'
   name 'Mountain Duck'
   homepage 'https://mountainduck.io/'
   license :commercial
