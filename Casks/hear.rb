3e2fb4d95e00dfccaed721292ede13ae33cdd1df
Victor Popkov <victor@popkov.me>
2016-06-04 23:30:45 +0300

    updated hear (1.3)

--- Casks/hear.rb
+++ Casks/hear.rb
@@ -1,10 +1,10 @@
 cask 'hear' do
-  version '1.2.4'
-  sha256 '54bd28a771cd26813a9177bbd41879694a3998bf712ca6b94b18c45895bc79e6'
+  version '1.3'
+  sha256 'f89c07d5b6c10da35159647ec46c0060bf38b53fad41acfa5a3ff5d0231b2799'
 
   url "https://downloads.prosofteng.com/hear/Hear_#{version}.dmg"
   appcast 'https://www.prosofteng.com/resources/sparkle/sparkle.php?psProduct=Hear',
-          checkpoint: '70719bdb2692b7d14ac2e410c4c9b75a2849519169b819fd85f6cf99a8caaf13'
+          checkpoint: '70573fc0b85e2ea2a06bd05bc616534d534181ae3dfd0010bbeac0980273a41b'
   name 'Hear'
   homepage 'https://www.prosofteng.com/hear/'
   license :commercial
