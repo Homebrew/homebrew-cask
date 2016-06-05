d2fb66762712c5cd2635019492b4fbd703697436
Vítor Galvão <info@vitorgalvao.com>
2016-06-05 22:28:04 +0100

    ax88179.rb: fix missing comma

--- Casks/ax88179.rb
+++ Casks/ax88179.rb
@@ -18,7 +18,7 @@ cask 'ax88179' do
   pkg "AX88179_178A_v#{version}.pkg"
 
   uninstall script: {
-                      executable: 'AX88179_178A_Uninstall_v140'
+                      executable: 'AX88179_178A_Uninstall_v140',
                       sudo:       false,
                     }
 end
