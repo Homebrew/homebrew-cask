9d3b188a91f97251a5949f7c47d4098d813f6b6e
Dave Wikoff <dgsims@gmail.com>
2016-06-04 21:40:25 -0400

    Renaming cask back to standard, updating comment about official url

--- Casks/sqlpro-for-postgres.rb
+++ Casks/sqlpro-for-postgres.rb
@@ -1,8 +1,8 @@
-cask 'sqlpropostgres' do
+cask 'sqlpro-for-postgres' do
   version '43'
   sha256 'ef510f2a7f69d0071872e9942f5f3cb967dadf1840e6ef978b38e8db3b019daf'
 
-  # cloudfront.net is the official download host per the vendor homepage
+  # d3fwkemdw8spx3.cloudfront.net was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/postgres/SQLProPostgres#{version}.app.zip"
   name 'SQLPro for Postgres'
   homepage 'http://www.macpostgresclient.com/SQLProPostgres'
