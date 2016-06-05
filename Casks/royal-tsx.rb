4b5b314bd71b0aa5c11edc51d45aaf441784f7ce
howie <howie.yu@tenmax.io>
2016-06-05 16:38:07 +0800

    updated royal-tsx (2.2.4.1000)

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -1,10 +1,10 @@
 cask 'royal-tsx' do
-  version '2.2.3.1000'
-  sha256 '601ada5103428f2bca12941e68be90c5089c4c7b4e2f5a5eef62273f256df19a'
+  version '2.2.4.1000'
+  sha256 '1efd0a5e6605f451f913c02595967e2dc08ad86b1b394e794c1ac81361ac2f7f'
 
   url "http://v2.royaltsx.com/updates/royaltsx_#{version}.dmg"
   appcast "https://royaltsx-v#{version.major}.royalapplications.com/updates_stable.php",
-          checkpoint: '9c50ae75ff555c2f557ea16163da581d392c2c9d660edfcb7d9b52c7162c7b7c'
+          checkpoint: '0451af62eb020e454e2b920e6f8aaa6e2883f9b990d68e44e31539765d8a3ea8'
   name 'Royal TSX'
   homepage 'http://www.royaltsx.com'
   license :freemium
