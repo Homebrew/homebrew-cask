faa3fcb72c1d3a925e14cb14c2c8f0a34ed1f527
Andrei Krasnitski <xaaabk@gmail.com>
2016-06-05 11:13:37 +0300

    Fix Stackato URL (#21701)

--- Casks/stackato.rb
+++ Casks/stackato.rb
@@ -2,7 +2,7 @@ cask 'stackato' do
   version '3.2.4'
   sha256 'e1d940509d46bc1ec4829998c736b802f77c01971e661d43456fe7574b16e48f'
 
-  url "https://downloads.stackato.com/client/v#{version}/stackato-#{version}-macosx10.5-i386-x86_64.zip"
+  url "http://downloads.stackato.com/client/v#{version}/stackato-#{version}-macosx10.5-i386-x86_64.zip"
   name 'Stackato'
   homepage 'https://docs.stackato.com/user/client/'
   license :apache
