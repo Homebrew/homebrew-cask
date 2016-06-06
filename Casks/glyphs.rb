c58f269be9a41885e7dd35163aee299623db0875
Dylan Arbour <arbourd@users.noreply.github.com>
2016-06-05 22:41:02 -0400

    updated glyphs (2.3-895) (#21733)

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,13 +1,15 @@
 cask 'glyphs' do
-  version '2.2.2-827'
-  sha256 'd568094e9061c76cc84a11f271f33abf0e3b4d135e57a4430ac972f996722732'
+  version '2.3-895'
+  sha256 'f44eaaad438db4e0f854386c8d8259381a0338a2877c3f4c1cfb4aedb20b3f46'
 
   url "https://updates.glyphsapp.com/Glyphs#{version}.zip"
   appcast "https://updates.glyphsapp.com/appcast#{version.major}.xml",
-          checkpoint: '2980985be44b73e6c8e61916f1b56bf2c431a4376ca71a38474dd3ce9db5697b'
+          checkpoint: '1499e8ac48652748e2a2f919c1241542cc058269e8cc3341c550b143f25c6c7a'
   name 'Glyphs'
   homepage 'https://www.glyphsapp.com'
   license :commercial
+
+  auto_updates true
 
   app 'Glyphs.app'
 end
