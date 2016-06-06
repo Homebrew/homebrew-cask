27ea3caa7fd09aadf3bf549e643abbfbb3d0a0f4
Jerzerak <jerzerak@gmail.com>
2016-06-05 22:41:31 -0400

    Discord 0.0.240 (#21728)

--- Casks/discord.rb
+++ Casks/discord.rb
@@ -1,6 +1,6 @@
 cask 'discord' do
-  version '0.0.239'
-  sha256 'a4a262322e98785273b5774ebfb054d89f0e8170be8bde55f086596f4d32191c'
+  version '0.0.240'
+  sha256 'e6d62060d6e4d9ca88513e9ec039d803b8b472b82e5c06424a2a62c6a675932d'
 
   url "https://cdn.discordapp.com/apps/osx/#{version}/Discord.dmg"
   name 'Discord'
