cf8a75b7add848339c003c297f67aa082c2f1f2d
Kyle <kyle@hankinsoft.com>
2016-06-06 12:43:37 -0300

    SQLPro for MSSQL cask now uses a specific version
    Proper sha256, updated url and added a verified comment.

--- Casks/sqlpro-for-mssql.rb
+++ Casks/sqlpro-for-mssql.rb
@@ -1,8 +1,9 @@
 cask 'sqlpro-for-mssql' do
-  version :latest
-  sha256 :no_check
+  version '1.0.73'
+  sha256 'a7737293009e0d458359ed53340d125f0aadf16122b020879b624e7312c18811'
 
-  url 'http://macsqlclient.com/download.php'
+  # d3fwkemdw8spx3.cloudfront.net/mssql was verified as official when first introduced to the cask
+  url "https://d3fwkemdw8spx3.cloudfront.net/mssql/SQLProMSSQL.#{version}.app.zip"
   name 'SQLPro for MSSQL'
   homepage 'http://www.macsqlclient.com'
   license :commercial
