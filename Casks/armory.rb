badcb0909b2a14ddfcb0720c7b1db528d8ea779e
Michael Ford <fanquake@users.noreply.github.com>
2016-06-06 10:58:58 +0800

    Armory: Add appcast

--- Casks/armory.rb
+++ Casks/armory.rb
@@ -3,6 +3,8 @@ cask 'armory' do
   sha256 'e1a977de35840d969bdf4039a0d5ddb466866ee793e9c178b0650ebbbf93af5c'
 
   url "https://github.com/goatpig/BitcoinArmory/releases/download/v#{version}/armory_#{version}_osx.tar.gz"
+  appcast 'https://github.com/goatpig/BitcoinArmory/releases.atom',
+          checkpoint: '50b50e2071c2cb4f05c0564cc0019d7fdd621ccc225953cdf4c1d93e40abba12'
   name 'Armory'
   homepage 'https://github.com/goatpig/BitcoinArmory'
   license :oss
