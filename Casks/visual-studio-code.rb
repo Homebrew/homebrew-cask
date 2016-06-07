044018df6bef91a1013707317881767135a3279c
Aditya Dalal <adidalal@users.noreply.github.com>
2016-06-07 08:39:42 -0400

    updated visual-studio-code (1.2.0,809e7b30e928e0c430141b3e6abf1f63aaf55589) (#21762)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.1.1,def9e32467ad6e4f48787d38caf190acbfee5880'
-  sha256 'd0e0e40cca55fb4c84bc868530d6dddbd5fdc7d8e24cad39cf777ec99edceb85'
+  version '1.2.0,809e7b30e928e0c430141b3e6abf1f63aaf55589'
+  sha256 '16e94495938ea0bcb137e4c91eb1766a801d3be015d96b1915aa2e0cee3f3a11'
 
   # az764295.vo.msecnd.net was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
