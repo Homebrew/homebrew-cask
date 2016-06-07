3abcaaebc3d1f74f61dc2ca19c93057adee3e033
Sascha Hagedorn <mail@shagedorn.net>
2016-06-07 14:07:41 +0200

    updated ynab (4.3.855)

--- Casks/ynab.rb
+++ Casks/ynab.rb
@@ -1,6 +1,6 @@
 cask 'ynab' do
-  version '4.3.817'
-  sha256 '66ff8fd735b69fa16fe884f91913fd4ef867fb03980631bb7d32b18cf68e400a'
+  version '4.3.855'
+  sha256 '05dd388122e49a131c76adf265620b2fafac589f9b92ad8b622720e0752a1eb6'
 
   url "https://www.youneedabudget.com/CDNOrigin/download/ynab4/liveCaptive/Mac/YNAB4_LiveCaptive_#{version}.dmg"
   name 'You Need A Budget'
