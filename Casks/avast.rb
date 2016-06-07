d07d616607c99cdb611eb73b4fe895537af718ad
Vítor Galvão <info@vitorgalvao.com>
2016-06-07 14:20:46 +0100

    avast.rb: added must_succeed false (#21760)

--- Casks/avast.rb
+++ Casks/avast.rb
@@ -10,8 +10,9 @@ cask 'avast' do
   pkg 'Avast Mac Security.pkg'
 
   uninstall script: {
-                      executable: '/Library/Application Support/Avast/hub/uninstall.sh',
-                      sudo:       true,
+                      executable:   '/Library/Application Support/Avast/hub/uninstall.sh',
+                      must_succeed: false, # A non-0 exit code may be given even if the uninstall succeeds (https://github.com/caskroom/homebrew-cask/issues/21740#issuecomment-224094946).
+                      sudo:         true,
                     }
 
   zap delete: '~/Library/Preferences/com.avast.avast!.plist'
