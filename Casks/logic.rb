ed45bcfb0074b6fc1d305d4e7b3e6feb820a8a6b
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2016-06-07 18:02:41 -0500

    updated logic (1.2.9) (#21769)

--- Casks/logic.rb
+++ Casks/logic.rb
@@ -1,8 +1,8 @@
 cask 'logic' do
-  version '1.1.34'
-  sha256 'a30535480c38d88c023d5fe83dc53f8e97aa20f3b98fc5c6ecf08bf2ffc50eaf'
+  version '1.2.9'
+  sha256 '8c85ad2aa05b86974351b0b573f41f68c42bb1835d531c2a2e288df5a335fe55'
 
-  url "http://downloads.saleae.com/betas/#{version}/Logic-#{version}-Darwin.dmg"
+  url "http://downloads.saleae.com/logic/#{version}/Logic-#{version}-Darwin.dmg"
   name 'Logic'
   homepage 'https://www.saleae.com/'
   license :commercial
