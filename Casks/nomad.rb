43e0aa300526e3690088319ba1ff8435dad96fc9
Justin Lecher <jlec@gentoo.org>
2016-06-08 21:14:52 +0100

    updated nomad (0.3.2)

--- Casks/nomad.rb
+++ Casks/nomad.rb
@@ -1,13 +1,13 @@
 cask 'nomad' do
-  version '0.3.1'
-  sha256 'c300a3b5c195fbccf26dfcdb756b4c953ca0e5e566a41fce1d10ef1947ec4279'
+  version '0.3.2'
+  sha256 '96808b9b8056490026327bdc6614e1813cee7d42978676e0024085709fd3fcac'
 
   # hashicorp.com/nomad was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/nomad/#{version}/nomad_#{version}_darwin_amd64.zip"
   appcast 'https://github.com/hashicorp/nomad/releases.atom',
-          checkpoint: '7dbb430f88706ed39867506dc3da084838475e27de7570f524a202b3f73fa4c1'
+          checkpoint: 'b65196b74ce907895afc4e8fc5bc4e4545d431edb2477cf743a21bb2b953b140'
   name 'Nomad'
-  homepage 'https://nomadproject.io/'
+  homepage 'https://www.nomadproject.io/'
   license :mpl
 
   binary 'nomad'
