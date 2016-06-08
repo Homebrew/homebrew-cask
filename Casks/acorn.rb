83b98b6658220183ef49cb905524832960999704
Jonny Bylsma <jbylsma@quotient-inc.com>
2016-06-08 11:11:42 -0400

    updated acorn (5.4.1)

--- Casks/acorn.rb
+++ Casks/acorn.rb
@@ -1,10 +1,10 @@
 cask 'acorn' do
-  version '5.4'
-  sha256 '9065ae8493fa73bfdf5d29ffcd0012cd343475cf3d550ae526407b9910eb35b7'
+  version '5.4.1'
+  sha256 '71d46ff25e739f15819aee8ad4a9e021ecf5b31122fd96788c923810b63ea05b'
 
   url 'https://secure.flyingmeat.com/download/Acorn.zip'
   appcast 'http://www.flyingmeat.com/download/acorn5update.xml',
-          checkpoint: '95ffe5b581434db6284ed8dfe0cddead69a5d3f7269ca488baba3bd1218e43f7'
+          checkpoint: '51d2bff7118dd2d82883732f119b9036e937471bac4f50d9510903e5bd24d9b4'
   name 'Acorn'
   homepage 'http://flyingmeat.com/acorn/'
   license :commercial
