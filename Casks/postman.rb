30e63fcf6c67123efb1546d7adb01c7d738471d5
IPconfig <justin.haan92@gmail.com>
2016-06-09 13:39:37 +0200

    updated postman (4.2.2) (#21799)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,11 +1,11 @@
 cask 'postman' do
-  version '4.2.0'
-  sha256 '04bc591667fcf9201876a1ab79d2002c4f1964b417915ae36c24cc6961c054cf'
+  version '4.2.2'
+  sha256 'b76ce5673980c747f2bf7bd2cb62d40dd31b09a9eb6c3b2479e3f597e24cc38d'
 
   # s3.amazonaws.com/postman-electron-builds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/postman-electron-builds/mac/Postman-osx-#{version}.zip"
   appcast 'https://app.getpostman.com/api/electron_updates_auto',
-          checkpoint: '834663ff1cf079d09854c43a903098736fdbeb37c070cf48323f54a4fb8f5720'
+          checkpoint: 'a312488dc3f9730394c518527f622f93c652a9fb58dc48bdea2cce56993a4757'
   name 'Postman'
   homepage 'https://www.getpostman.com/'
   license :closed
