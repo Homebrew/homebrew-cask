d1b59268555ac3538d75cac531ec10840a5e374c
György Frivolt <g.frivolt@netronixgroup.com>
2016-06-09 10:38:31 +0200

    yakyak update to v1.2.3

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,10 +1,10 @@
 cask 'yakyak' do
-  version '1.2.0'
-  sha256 'f568fb4858e5f824541e789b9c2d2963eec3b5227807b58cbebcd9d9afa3eeb5'
+  version '1.2.3'
+  sha256 'a76eb88acecdea7de4e7ecdc993df8c425f6e1fcd80341c0738491aaf108de9a'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-osx.app.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom',
-          checkpoint: '9635251571e242909a1bf0922ca65aea949ff6028c7bf222cb453072836a2c0e'
+          checkpoint: 'a59249625267737a094b89e158d322642038b6269819dc1fc11c296cdef32b4e'
   name 'Yakyak'
   homepage 'https://github.com/yakyak/yakyak'
   license :mit
