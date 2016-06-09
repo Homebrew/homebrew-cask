f2137735d66ba5777710e45e9d6bb2a3eea52129
ptb <ptb@ioutime.com>
2016-06-09 18:13:09 -0400

    update steermouse (4.2.6f1) (#21827)

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,5 +1,5 @@
 cask 'steermouse' do
-  version '4.2.6'
+  version '4.2.6f1'
   sha256 '4bbe779fcbfc69d9ebb196e888789cb088459cd3f944c3978a55c061d7d9f819'
 
   url "http://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
