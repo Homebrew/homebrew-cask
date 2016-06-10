0b9299004fe4eb70cb08b681a904292a84237235
Alex Alonso <alex4551@gmail.com>
2016-06-10 16:32:37 +0800

    update firefox version 47.0

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,6 +1,6 @@
 cask 'firefox' do
-  version '46.0.1'
-  sha256 '7ab6508b098023840d63ec622beb9f8ff4c68922f11aa769acf351dae5baa231'
+  version '47.0'
+  sha256 'e8e068a8f87126d1e252a51bbd0d4b20314fef8dc015c70c21468deaab9c4d9d'
 
   url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
   appcast "https://aus5.mozilla.org/update/3/Firefox/#{version}/0/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/Darwin%2015.3.0/default/default/update.xml?force=1",
