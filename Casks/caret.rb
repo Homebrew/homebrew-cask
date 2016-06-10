ffaf0fdb5d63d426d0de7e266691b8f3e9e00b38
Vítor Galvão <info@vitorgalvao.com>
2016-06-10 15:28:48 +0100

    updated caret (1.3.2)

--- Casks/caret.rb
+++ Casks/caret.rb
@@ -1,8 +1,11 @@
 cask 'caret' do
-  version :latest
-  sha256 :no_check
+  version '1.3.2'
+  sha256 '439d6fdb5f8c461312e83e55ceefe880c6eabdaaf4c52b0384a35f1f0b981f83'
 
-  url 'http://cdn.caret.io/Caret.dmg'
+  # github.com/careteditor/caret was verified as official when first introduced to the cask
+  url "https://github.com/careteditor/caret/releases/download/v#{version}/Caret.dmg"
+  appcast 'https://github.com/careteditor/caret/releases.atom',
+          checkpoint: '8ca9ddb2c318252727dec99eaf1a539a390738bed95f5e088d24c2d4923a8dd5'
   name 'Caret'
   homepage 'http://caret.io/'
   license :commercial
