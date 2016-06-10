d74291eec1fa7fc47647dd82c466fc248d55ccb1
Alex Alonso <alex4551@gmail.com>
2016-06-10 16:56:34 +0800

    update whatsapp to version 0.2.777

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,6 +1,6 @@
 cask 'whatsapp' do
-  version '0.2.684'
-  sha256 '728e1761d81e5d84a3fa9bc5f63383780f2830c7b10103c5791ffeccf016b842'
+  version '0.2.777'
+  sha256 '05c015f0ad7a51f62b341ce006827a7bc5c5962e8303859e13de3f49a59c6312'
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64',
