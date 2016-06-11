26f35973d6a6d5761e9d4ce26bfd9eeec6386396
Yuuta Sakai <ysakai.g@gmail.com>
2016-06-12 00:34:19 +0900

    overture.rb: add appcast

--- Casks/overture.rb
+++ Casks/overture.rb
@@ -4,6 +4,8 @@ cask 'overture' do
 
   # github.com/overturetool/overture was verified as official when first introduced to the cask
   url "https://github.com/overturetool/overture/releases/download/Release%2F#{version}/Overture-#{version}-macosx.cocoa.x86_64.zip"
+  appcast 'https://github.com/overturetool/overture/releases.atom',
+          checkpoint: 'bc8eff785ac9f781aeeee6f94a37447ff529439b53918de3d8487bdca855c046'
   name 'Overture Tool'
   homepage 'http://overturetool.org/'
   license :gpl
