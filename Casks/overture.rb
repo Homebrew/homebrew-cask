e9b4cb2d94ae6afb970b12d4b3fe1e3dd3135fe2
Yuuta Sakai <ysakai.g@gmail.com>
2016-06-11 22:50:18 +0900

    overture.rb: add appcast and url comment

--- Casks/overture.rb
+++ Casks/overture.rb
@@ -2,7 +2,8 @@ cask 'overture' do
   version '2.3.6'
   sha256 '623355be84033257f3434d2f85a2a632f28f4adbeee4a897b92be53e147846a4'
 
-  url 'https://github.com/overturetool/overture/releases/download/Release%2F2.3.6/Overture-2.3.6-macosx.cocoa.x86_64.zip'
+  # github.com/overturetool/overture was verified as official when first introduced to the cask
+  url "https://github.com/overturetool/overture/releases/download/Release%2F#{version}/Overture-#{version}-macosx.cocoa.x86_64.zip"
   name 'Overture Tool'
   homepage 'http://overturetool.org/'
   license :gpl
