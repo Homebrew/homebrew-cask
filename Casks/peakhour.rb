d0d6b86cf72b31d723d6cfb4387eb8ff8e01dec4
Victor Popkov <victor@popkov.me>
2016-06-11 23:23:36 +0300

    updated peakhour (3.1.5) (#21867)

--- Casks/peakhour.rb
+++ Casks/peakhour.rb
@@ -1,10 +1,10 @@
 cask 'peakhour' do
-  version '3.1.4'
-  sha256 'f9e9d86a4e5e6122fa87f7dc1e1baa750aac4b7160a243dba117dbe98d213ad6'
+  version '3.1.5'
+  sha256 '619a550162a76111b5f7023d7a8f905bd234193530d599b4a9d47a4e2ebeb6de'
 
   url "https://updates.peakhourapp.com/releases/PeakHour%20#{version}.zip"
   appcast 'https://updates.peakhourapp.com/PeakHourAppcast.xml',
-          checkpoint: '842d16e2dcf126c6253b19ce838a9f8c0a90b6052db42b858f2c462899cc9c57'
+          checkpoint: '25ab3c1e8f3bf95ffd15d0d5b5d32c082ff782ec4c0dfb20325d46af771dd8c2'
   name 'PeakHour'
   homepage 'https://www.peakhourapp.com'
   license :commercial
