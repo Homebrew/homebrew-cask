88ae4686a3f1b12a25c772a79b2c993ff4a58831
Koen Rouwhorst <koenrh@users.noreply.github.com>
2016-06-11 15:15:12 +0200

    updated knockknock (1.8.0) (#21856)

--- Casks/knockknock.rb
+++ Casks/knockknock.rb
@@ -1,6 +1,6 @@
 cask 'knockknock' do
-  version '1.7.1'
-  sha256 'ff15f02d4b1a4f5fc53dfe2cb3be6e548362d943d9b6336eb6b549e6a87dd873'
+  version '1.8.0'
+  sha256 'e9639d5782da6bc8796815fe57bbb7a5598b15cc7725db1a84b88b8cb47f44b8'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/KnockKnock_#{version}.zip"
