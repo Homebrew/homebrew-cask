f8c6dd0ee0ee946693127f14143c927c9440efd6
Victor Popkov <victor@popkov.me>
2016-06-11 23:48:24 +0300

    updated ukelele (3.0.4) (#21873)

--- Casks/ukelele.rb
+++ Casks/ukelele.rb
@@ -1,10 +1,10 @@
 cask 'ukelele' do
-  version '3.0.3'
-  sha256 'a4c0d0c2154f00acff922a42c7d6c558c77682fae33a2f9baf89df2fb6518a4b'
+  version '3.0.4'
+  sha256 '4a6c53aee7fb6bab4d497a56c6d84e4bbe3dff3b1107380efd5a9a1ada2ebe0d'
 
   url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Ukelele_#{version}&filename=Ukelele_#{version}.dmg"
   appcast 'https://dl.dropboxusercontent.com/u/60565698/Ukelele/Ukelele_appcast.xml',
-          checkpoint: 'e20fee0b23e4741c8e0dce26e16f7aadddc99c80a0a5d5cc6f14136b19301f9e'
+          checkpoint: '3dae9e233ecbd20e59e99a93abbf71ccb2660f2d6a330eacf708d598ea395810'
   name 'Ukelele'
   homepage 'https://scripts.sil.org/ukelele'
   license :gratis
