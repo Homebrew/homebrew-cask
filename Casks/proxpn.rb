f86a02694cf0f11e6085861ec5ed1a5440f3f7ae
Victor Popkov <victor@popkov.me>
2016-06-12 17:59:36 +0300

    proxpn.rb: add depends_on, upgrade to v4.3.6.2

--- Casks/proxpn.rb
+++ Casks/proxpn.rb
@@ -1,13 +1,15 @@
 cask 'proxpn' do
-  version '4.3.5.2'
-  sha256 '754d91b6e927cbc3eea0279accf2d7b41b0d548b98e04a2922663992d7774789'
+  version '4.3.6.2'
+  sha256 '5f4bf12371ccdf694a222ec8d966f6796e8d93a484934e674798385d60891370'
 
-  url "https://www.proxpn.com/updater/proXPN-MacOSX-10.5-#{version}.dmg"
+  url "https://www.proxpn.com/updater/proXPN-MacOSX-10.7-#{version}.dmg"
   appcast 'http://www.proxpn.com/updater/appcast.rss',
-          checkpoint: 'bea9ff2d33380580e343857de18e45f61a7b45848d72280a8914408acbbcb850'
+          checkpoint: 'ec65d9d281487212b1aba6ff55e4ec7802dff549d692941fb2f81a996b8485ab'
   name 'proXPN'
   homepage 'https://www.proxpn.com/'
   license :freemium
+
+  depends_on macos: '>= :leopard'
 
   app 'ProXPN.app'
 end
