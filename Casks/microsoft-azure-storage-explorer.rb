7a361c3ef1b4dd6847048096acf8f1a889060702
jazzwang <jazzwang.tw@gmail.com>
2016-06-12 22:25:09 +0800

    updated microsoft-azure-storage-explorer (0.8.0)

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,9 +1,9 @@
 cask 'microsoft-azure-storage-explorer' do
-  version :latest
-  sha256 :no_check
+  version '0.8.0'
+  sha256 'aeeb6d463367f9d31a31553d676af0382c62f1befdbd02c2a651da0affcfba6b'
 
   # microsoft.com was verified as official when first introduced to the cask
-  url 'https://go.microsoft.com/fwlink/?LinkId=708342'
+  url 'http://go.microsoft.com/fwlink/?LinkId=708342'
   name 'Microsoft Azure Storage Explorer'
   homepage 'http://storageexplorer.com/'
   license :gratis
