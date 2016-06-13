71e40276e02727b3110a17d77c8ba30e8d356dc4
Vítor Galvão <info@vitorgalvao.com>
2016-06-13 22:51:43 +0100

    updated linear (1.4.1) (#21941)

--- Casks/linear.rb
+++ Casks/linear.rb
@@ -1,11 +1,11 @@
 cask 'linear' do
-  version '1.3.0'
-  sha256 'f93d9115e5173375b529c836cf6727936f40cd364b12314c42dc80564444ca10'
+  version '1.4.1'
+  sha256 '57576222fba52a5fcaf35360c28a81d36a6b2abd3938a11574a7a041dea3cbdc'
 
   # github.com/mikaa123/linear was verified as official when first introduced to the cask
   url "https://github.com/mikaa123/linear/releases/download/#{version}/linear.zip"
   appcast 'https://github.com/mikaa123/linear/releases.atom',
-          checkpoint: '5dff0089adc2d1dd2a8c8617f4b87587ed306c4e3fd8beb87549e37bbc51305e'
+          checkpoint: '7abff407ef82bea4ee878441a10ff6172341414d866b46d3fda6da5533c1d92d'
   name 'Linear'
   homepage 'https://mikaa123.github.io/linear-website/'
   license :mit
