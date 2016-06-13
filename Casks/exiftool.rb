9bb6eb47c4011d7e9fa8cd4ad0e8751446f4a805
Victor Popkov <victor@popkov.me>
2016-06-13 23:13:30 +0300

    updated exiftool (10.20) (#21931)

--- Casks/exiftool.rb
+++ Casks/exiftool.rb
@@ -1,10 +1,10 @@
 cask 'exiftool' do
-  version '10.19'
-  sha256 'e43e22e676ef58ba20a10eaca6fae83e2768848fa949d11fe0554de0a2b8f220'
+  version '10.20'
+  sha256 '8b45837e9f72a3da8726c130e7597377e5507f880fe4fa8eedac103e7ccf33c9'
 
   url "http://owl.phy.queensu.ca/~phil/exiftool/ExifTool-#{version}.dmg"
   appcast 'http://owl.phy.queensu.ca/~phil/exiftool/rss.xml',
-          checkpoint: 'b447d3e609e77648a346d554fe2ee3e1c0963a79c393622cda34915dea422039'
+          checkpoint: '01830791e3904c8bb1f955a14ae86c639fac5e2742dbb3ba875e6baa041feab4'
   name 'ExifTool by Phil Harvey'
   homepage 'http://www.sno.phy.queensu.ca/~phil/exiftool/'
   license :gpl
