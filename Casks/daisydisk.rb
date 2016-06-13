10fd44acec5c3d0abb0d49cf91ad259cc8183eb7
Amory Meltzer <Amorymeltzer@gmail.com>
2016-06-13 14:29:08 -0700

    daisydisk: Add lion through mavericks version
    Version 3.0.3.1
    Fix url for snow leopard version
    Remove outdated and nonspecific appcasts for old versions

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -2,9 +2,11 @@ cask 'daisydisk' do
   if MacOS.release <= :snow_leopard
     version '2.1.2'
     sha256 'd0a606dee19e524d6fa7b79fd48b3b9865123ca4126fb8805f8e96c317b57b31'
-    url "https://www.daisydiskapp.com/downloads/DaisyDisk_#{version}.dmg"
-    appcast 'https://daisydiskapp.com/downloads/appcastFeed.php',
-            checkpoint: '5f9960cd3d158636268aeae712f959bde3407efc4db59b7615e4ab08938d566a'
+    url "https://www.daisydiskapp.com/downloads/DaisyDisk_#{version.dots_to_underscores}.dmg"
+  elsif MacOS.release <= :mavericks
+    version '3.0.3.1'
+    sha256 'fe2aa86f2ea8a1f0c4791857a5b7991ecad295b5b969849bb7b15a890ab54b86'
+    url "https://www.daisydiskapp.com/downloads/DaisyDisk_#{version.dots_to_underscores}.zip"
   else
     version '4.2'
     sha256 'e60b9643d33fc0f10da0446f101b1ffb196c668f313ffeaf24ff5589a6cb2978'
