9c0aa191cc2c227aba3cbb2d7af3a86c15fd813e
Thomas Obadia <thomas.obadia@gmail.com>
2016-06-14 11:25:22 +0200

    Update audacity URL, now grabbed from an embeded iframe within the protected download URL

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -1,9 +1,17 @@
+require 'open-uri'
 cask 'audacity' do
   version '2.1.2'
   sha256 '2e4b7d608ecc0d2f79bf16663f085d383075e488f7d50bf7d74c0b69173defe7'
 
   # Current official URL as proposed on http://www.audacityteam.org/download/mac/
-  url "http://www.fosshub.com/Audacity.html/audacity-macosx-ub-#{version}.dmg"
+  # must be parsed to extract temporary url embedded in iframe
+  iframe_src_protected_url = nil
+  open("http://www.fosshub.com/Audacity.html/audacity-macosx-ub-#{version}.dmg") { |io|
+    content = io.read
+    iframe_src_protected_url = /^\<iframe.*src=\"(http.*\.dmg)\".*>/.match(content)[1].to_s
+  }
+  url iframe_src_protected_url
+  
   name 'Audacity'
   homepage 'http://audacityteam.org'
   license :gpl
