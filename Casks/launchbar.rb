406b5ecd061b7b6c8ac547618993ea1f85228370
Marc Gähwiler <marc@marcg.ch>
2016-06-14 13:02:04 +0200

    updated launchbar (6.7)

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -4,8 +4,8 @@ cask 'launchbar' do
     sha256 '22a1ec0c10de940e5efbcccd18b8b048d95fb7c63213a01c7976a76d6be69a4d'
     url "https://www.obdev.at/downloads/launchbar/legacy/LaunchBar-#{version}.dmg"
   else
-    version '6.6.2'
-    sha256 'cd9fe62776b8fdd185e4f176def83dd5f41c9ea2dc0d3fcdc1dc8b860bca21f3'
+    version '6.7'
+    sha256 '2ecee4522b6761925090e228d56e04f29c2c90c8760d98fc7bd9fd27f2f7ebb5'
     url "https://www.obdev.at/downloads/launchbar/LaunchBar-#{version}.dmg"
   end
 
