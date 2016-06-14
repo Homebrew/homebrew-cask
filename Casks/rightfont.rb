74b1bdeb2623528335d656b49730c2ff230fc95b
Victor Popkov <victor@popkov.me>
2016-06-14 23:49:47 +0300

    updated rightfont (3.2.3) (#21973)

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -1,10 +1,10 @@
 cask 'rightfont' do
-  version '3.2.2'
-  sha256 '682a5ac169e68382f6396dabaee26d08e50bb7cca2af9bd9d650f0fc31b6c4a9'
+  version '3.2.3'
+  sha256 'a1b8e65a19ef6a913b12864e7b075e50e33318bf56f459e9ee27f0945f9f5db9'
 
   url "http://rightfontapp.com/downloads/#{version}/rightfont.zip"
   appcast 'http://rightfontapp.com/update/appcast.xml',
-          checkpoint: '9f4f97ef30386fba224b58e8fbaa18addf36729ad50f4226480fcf9413cc3b79'
+          checkpoint: '5869af418c2eb753f19d2d5506dd99048ccdcf752459fbcd5603cbe7cb6b7a7a'
   name 'RightFont'
   homepage 'http://rightfontapp.com/'
   license :commercial
