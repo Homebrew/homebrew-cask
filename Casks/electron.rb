a56dc4cbe45ab74a0bfcc60b717984473a7f2974
Timmy Chang <timmy5519@gmail.com>
2016-06-14 16:03:06 +0800

    Update electron to v1.2.2 (#21948)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,10 +1,10 @@
 cask 'electron' do
-  version '1.2.1'
-  sha256 '0527d6a4218fb5ef2f6555e84ac458d13d40bdc071f9d8a00546434359f2165f'
+  version '1.2.2'
+  sha256 '3f5cbe242edc99718faa7adf0c2b728696cdb3549c9289a4d312034086bc1467'
 
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
-          checkpoint: 'a95a384ab00b00a10085153618b966e400f3225988f748fd2a4636a812f2ad29'
+          checkpoint: 'cdfab1240683476cdd4082636e363366a2b1fa5805567a7592f9bd9ad7b4124e'
   name 'Electron'
   homepage 'http://electron.atom.io/'
   license :mit
