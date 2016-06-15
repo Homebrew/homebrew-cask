17ea6506004c171c4a4f9c8dfbfa276fc186fe4c
flother <flother@users.noreply.github.com>
2016-06-15 22:39:41 +0000

    Update Rodeo to version 2.0.0

--- Casks/rodeo.rb
+++ Casks/rodeo.rb
@@ -1,11 +1,11 @@
 cask 'rodeo' do
-  version '1.4.9'
-  sha256 '0e590fd0cc91153f971c56562cb4c2ae9b5209547c9e4043371ca03f53155b4c'
+  version '2.0.0'
+  sha256 'd9dfba1f62934e4ec7c80c4bfd0d4b77ddec63254f9d14b7cc867c0c20d5baf1'
 
   # github.com/yhat/rodeo was verified as official when first introduced to the cask
   url "https://github.com/yhat/rodeo/releases/download/v#{version}/Rodeo-#{version}.dmg"
   appcast 'https://github.com/yhat/rodeo/releases.atom',
-          checkpoint: '8a2cc3b31afd75edc1c9b3bb738bfb2f761c7a8d7f87c1f111550a6499ef1038'
+          checkpoint: '900acf0852da14c0d38a73345f82cfa37a951123af8dac9dce0cacb8a35b61fc'
   name 'Rodeo'
   homepage 'http://rodeo.yhat.com/'
   license :affero
