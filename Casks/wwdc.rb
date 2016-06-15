af3d1e543f5f064752c2f221308a9555f6405654
Vítor Galvão <info@vitorgalvao.com>
2016-06-15 15:44:18 +0100

    updated wwdc (4.2.2)

--- Casks/wwdc.rb
+++ Casks/wwdc.rb
@@ -1,10 +1,10 @@
 cask 'wwdc' do
-  version '4.2.1'
-  sha256 'd1d75a70885bbf9da7b6c39aae146692672b7912f995a8d3b18fb5f2b5c23b33'
+  version '4.2.2'
+  sha256 '27245ef28e03380ff8f8d404c9d7a45b7b1fd23ed2f7d754aef37fd08ddc4f5e'
 
   url "https://github.com/insidegui/WWDC/releases/download/#{version}/WWDC_v#{version}.zip"
   appcast 'https://github.com/insidegui/WWDC/releases.atom',
-          checkpoint: '9925c90252904041fa07484b743008a8d1d7426df6bf10b9c9f36b87486461ed'
+          checkpoint: 'bfbe7e28c0b7184bfe89d438011faef35b1b22154127dea76eb456c07f79c17e'
   name 'WWDC'
   homepage 'https://github.com/insidegui/WWDC'
   license :bsd
