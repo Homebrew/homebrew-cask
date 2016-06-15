217789f658790a2cd4521f1c91401c6803fa05d9
Vítor Galvão <info@vitorgalvao.com>
2016-06-15 03:26:20 +0100

    updated cubicsdr (0.2.0,rc2) (#21979)

--- Casks/cubicsdr.rb
+++ Casks/cubicsdr.rb
@@ -1,8 +1,11 @@
 cask 'cubicsdr' do
-  version '0.1.27'
-  sha256 '51e7629880e8f061f4089c73482df91d02649633c2f9ccca6256e9b64f7eca22'
+  version '0.2.0,rc2'
+  sha256 '14bc59bcfc1ceb04532bf89ec99199d17f9bcbff75a38c22d33920c866a828e6'
 
-  url "https://github.com/cjcliffe/CubicSDR/releases/download/#{version}-alpha/CubicSDR-#{version}-Darwin.dmg"
+  # github.com/cjcliffe/CubicSDR was verified as official when first introduced to the cask
+  url "https://github.com/cjcliffe/CubicSDR/releases/download/#{version.before_comma}-beta-#{version.after_comma}/CubicSDR-#{version.before_comma}-Darwin-#{version.after_comma}.dmg"
+  appcast 'https://github.com/cjcliffe/CubicSDR/releases.atom',
+          checkpoint: '253b4817796c7e5ccc2f7934e6ebe0397b62e4dbcfc03336454c5a96d753f09c'
   name 'CubicSDR'
   homepage 'http://cubicsdr.com/'
   license :gpl
