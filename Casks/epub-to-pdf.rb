cb82c75512dcd74671acad2c163f2aef8fa8e741
Jean Regisser <jean.regisser@gmail.com>
2016-06-15 20:52:06 +0200

    Fix incorrect google code archive urls (#21996)

--- Casks/epub-to-pdf.rb
+++ Casks/epub-to-pdf.rb
@@ -2,7 +2,7 @@ cask 'epub-to-pdf' do
   version '3.1'
   sha256 'dcfc59d57f756802e844614b7dae43bca67284ec85fe6b909f244e41f20987b3'
 
-  url "https://storage.googleapis.com/google-code-archive-downloads/v1/code.google.com/epub-2-pdf/e2p-#{version.major}.dmg"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/epub-2-pdf/e2p-#{version.major}.dmg"
   name 'epub-2-pdf'
   homepage 'https://code.google.com/archive/p/epub-2-pdf'
   license :oss
