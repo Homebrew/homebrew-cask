967d60c06320831f51ad5a219cd60753671d5a20
Marko Locher <marko@codeship.com>
2016-06-15 09:43:58 +0200

    Update Near Lock to version 3.6 (again) (#21985)

--- Casks/near-lock.rb
+++ Casks/near-lock.rb
@@ -1,10 +1,10 @@
 cask 'near-lock' do
   version '3.6'
-  sha256 '8118ae08ef0dd414244dfc436abad5f3648bc36e18a7ace5d8587dde896b52b3'
+  sha256 'd5f15c5da6e818ed467dead23a070ffd99af48d6265f5dbf3319ef6f336df530'
 
   url 'http://nearlock.me/downloads/nearlock.dmg'
   appcast 'http://nearlock.me/downloads/nearlock.xml',
-          checkpoint: '0d6cb95559b472c3e0b68f322310199311a74ebf915419607644b70e1a3d3917'
+          checkpoint: 'e0ca65379137858b4f565a9abd40195cd834829143575e800465096bf86a6a7c'
   name 'Near Lock'
   homepage 'http://nearlock.me/'
   license :gratis
