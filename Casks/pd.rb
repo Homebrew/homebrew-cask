a6cc80a27bf8c7372bcaeb17f861bdd77970a993
Vítor Galvão <info@vitorgalvao.com>
2016-06-16 11:58:09 +0100

    pd.rb: appdir to Hbc.appdir in blocks

--- Casks/pd.rb
+++ Casks/pd.rb
@@ -10,6 +10,6 @@ cask 'pd' do
   app "Pd-#{version}-64bit.app"
 
   postflight do
-    set_permissions "#{appdir}/Pd-#{version}-64bit.app", 'u+w'
+    set_permissions "#{Hbc.appdir}/Pd-#{version}-64bit.app", 'u+w'
   end
 end
