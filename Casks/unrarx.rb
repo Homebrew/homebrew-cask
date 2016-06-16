565856492fe40eeebb0096ad40c67c580cdc1a1d
Scott Gorsuch <sgorsuch@gmail.com>
2016-06-16 13:52:32 -0400

    update sha256 for unrarx (#22016)

--- Casks/unrarx.rb
+++ Casks/unrarx.rb
@@ -1,10 +1,10 @@
 cask 'unrarx' do
   version '2.2'
-  sha256 '616c5c536efb29a35fe45c8171874592cc28b269e5d7ed6947c19c8cbb686955'
+  sha256 '17fa2f3324d45c27a318ed51dab739c7f09b573185b76889b955ad2c9ad1d7b8'
 
   url "http://www.unrarx.com/files/UnRarX_#{version}.zip"
   appcast 'http://www.unrarx.com/update.xml',
-          checkpoint: 'c42c5affe2dd688136eb43599d6bc91e2a6816a7be9cf063e58d81004b359ba6'
+          checkpoint: '17fa2f3324d45c27a318ed51dab739c7f09b573185b76889b955ad2c9ad1d7b8'
   name 'UnRarX'
   homepage 'http://www.unrarx.com'
   license :gratis
