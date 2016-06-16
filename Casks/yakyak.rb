c48ed78be2b54598a94e29ddf3605c300126f58d
Troy McCall <troyxmccall@gmail.com>
2016-06-16 15:39:33 -0400

    updated yakyak (1.3.0)

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,10 +1,10 @@
 cask 'yakyak' do
-  version '1.2.3'
-  sha256 'a76eb88acecdea7de4e7ecdc993df8c425f6e1fcd80341c0738491aaf108de9a'
+  version '1.3.0'
+  sha256 '620727f86960d4b59e25f71e0b8ef0580b10c49c98a688f91e442356578c9cfe'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-osx.app.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom',
-          checkpoint: 'a59249625267737a094b89e158d322642038b6269819dc1fc11c296cdef32b4e'
+          checkpoint: 'a37c0808ae1e3fe67de41c58701085dd5868e6378b0f9ec4ba02ffd3f75e2b85'
   name 'Yakyak'
   homepage 'https://github.com/yakyak/yakyak'
   license :mit
