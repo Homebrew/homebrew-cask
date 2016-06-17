bdba2a0534ef229ddc4f4bb991b4bc9ec19ec5b1
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 23:33:06 +0100

    nylas-n1.rb: updated vendor comment

--- Casks/nylas-n1.rb
+++ Casks/nylas-n1.rb
@@ -2,7 +2,7 @@ cask 'nylas-n1' do
   version '0.4.40-85cf726'
   sha256 '668ff7119a51c5aff01dddfead4cfd9befbf51157af54a3d719ffdb0024956c1'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # edgehill.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
   url "https://edgehill.s3-us-west-2.amazonaws.com/#{version}/darwin/x64/N1.dmg"
   appcast 'https://edgehill.nylas.com/update-check?platform=darwin&arch=64',
           checkpoint: 'fae87f180132f0c60fa400a632ae7d5ef5f13d3c8db924603d78483df0a96dc8'
