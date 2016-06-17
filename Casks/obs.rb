8e5685369ac3e00bde962fd9bde4f272b2ce48c4
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 23:33:42 +0100

    obs.rb: updated vendor comment

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -2,7 +2,7 @@ cask 'obs' do
   version '0.14.2'
   sha256 '9324c8cea44c385f19dfb5dcf740090334c49d450089bd108fca95c1728b58b5'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/jp9000/obs-studio was verified as official when first introduced to the cask
   url "https://github.com/jp9000/obs-studio/releases/download/#{version}/obs-#{version}-installer.dmg"
   appcast 'https://github.com/jp9000/obs-studio/releases.atom',
           checkpoint: '72e576ac4cb5b68742e19d5adc69390d4500c2eed8c74aaa11449de7f3a677d6'
