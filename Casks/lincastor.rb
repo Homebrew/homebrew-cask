5275364ed9b3d3081a3e8323426dcb42d3bf197a
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 20:49:36 +0100

    lincastor.rb: updated vendor comment

--- Casks/lincastor.rb
+++ Casks/lincastor.rb
@@ -2,7 +2,7 @@ cask 'lincastor' do
   version '1.4'
   sha256 'fbe3af69e932cebdd0ddb76460018a4cc9194d60bb9476c2a42c2ccf59bdbba2'
 
-  # dropboxusercontent.com is the official download host per the vendor homepage
+  # dl.dropboxusercontent.com/u/7614970 was verified as official when first introduced to the cask
   url 'https://dl.dropboxusercontent.com/u/7614970/LinCastor.zip'
   appcast 'https://onflapp.appspot.com/lincastor',
           checkpoint: 'f5624589dfecb9b5fba5fab9e92a816e01cb7b4f172e94d5e462a62e643a4bea'
