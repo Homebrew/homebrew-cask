9f89776d896d405372dcad499b0c8a8af2629b81
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 23:27:18 +0100

    kollaborate-folder-watcher.rb: updated vendor comment (#22080)

--- Casks/kollaborate-folder-watcher.rb
+++ Casks/kollaborate-folder-watcher.rb
@@ -2,7 +2,7 @@ cask 'kollaborate-folder-watcher' do
   version '1.0.0.0'
   sha256 '31652e3b275a566157da2fcca19740f1451d8bff24a1fdf4ce75c0c69b4ed6b5'
 
-  # digitalrebellion.com is the official download host per the vendor homepage
+  # digitalrebellion.com was verified as official when first introduced to the cask
   url "http://www.digitalrebellion.com/download/kollabfolderwatcher?version=#{version.no_dots}"
   name 'Kollaborate Folder Watcher'
   homepage 'https://www.kollaborate.tv/resources'
