368ca4faff85d39a442a25b75403e1f0b8e6e4a9
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 17:39:49 +0100

    updated etcher (1.0.0-beta.8)

--- Casks/etcher.rb
+++ Casks/etcher.rb
@@ -1,6 +1,6 @@
 cask 'etcher' do
-  version '1.0.0-beta.2'
-  sha256 '558a961efcbf601349dfebea7129cf6cfceec90137c6bb80c4ed7649459c91d2'
+  version '1.0.0-beta.8'
+  sha256 'fa8489ad9d51de8884b862afed054b3d8597a2aa7c21e7cc5e28894868fb2466'
 
   # resin-production-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://resin-production-downloads.s3.amazonaws.com/etcher/#{version}/Etcher-darwin-x64.dmg"
