feafb519277c62d8c8840a9681128613bcf42372
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 23:08:49 +0100

    mmex.rb: updated vendor comment

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -2,7 +2,7 @@ cask 'mmex' do
   version '1.2.2'
   sha256 'c9da531544af256490ec5d1a62080535c1440b10f80f3600c250c7cbd0afd269'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # downloads.sourceforge.net/project/moneymanagerex was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/moneymanagerex/moneymanagerex/Version_#{version}/mmex_#{version}_mac10.10.dmg"
   name 'Money Manager Ex'
   homepage 'http://www.moneymanagerex.org'
