b32c98d47d5bf77d98dcfb58b540044be85a3025
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 18:13:47 +0100

    kensington-trackball-works.rb: fix vendor comment (#22069)

--- Casks/kensington-trackball-works.rb
+++ Casks/kensington-trackball-works.rb
@@ -2,7 +2,7 @@ cask 'kensington-trackball-works' do
   version '1.3'
   sha256 '6c2271211f71fc80e237dad2a86f253b408e985d5aea58e574ac89f54e4ce1fc'
 
-  # windows.net is the official download host per the vendor homepage
+  # accoblobstorageus.blob.core.windows.net was verified as official when first introduced to the cask
   url 'https://accoblobstorageus.blob.core.windows.net/software/38c915e6-ca92-406e-bd65-84e9692d4493.dmg'
   name 'Kensington TrackballWorks'
   homepage 'http://www.kensington.com/'
