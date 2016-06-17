da669b4efeb5765f202cf943e6ffe88a8b676c6a
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 22:55:52 +0100

    mancy.rb: updated vendor comment

--- Casks/mancy.rb
+++ Casks/mancy.rb
@@ -2,7 +2,7 @@ cask 'mancy' do
   version '3.0.0'
   sha256 '1e0df709ea909d1e2ec88f5a2bedb4b71d4c75ce0d1e67b909d0b02671af6ee6'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/princejwesley/Mancy was verified as official when first introduced to the cask
   url "https://github.com/princejwesley/Mancy/releases/download/v#{version}/Mancy-darwin-x64.zip"
   appcast 'https://github.com/princejwesley/Mancy/releases.atom',
           checkpoint: '2060ba97dc18075843a5d0908b73674a8f81412982e6a08b7d3db616dcc963f5'
