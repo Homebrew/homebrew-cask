a6b5be2b88c481e3d1a031a591261ca6fcd8cec2
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 23:31:26 +0100

    numi.rb: updated vendor comment

--- Casks/numi.rb
+++ Casks/numi.rb
@@ -2,7 +2,7 @@ cask 'numi' do
   version :latest
   sha256 :no_check
 
-  # devmate.com is the official download host per the vendor homepage
+  # dl.devmate.com/com.dmitrynikolaev.numi was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.dmitrynikolaev.numi/Numi.zip'
   name 'Numi'
   homepage 'https://numi.io/'
