e50e95e59616b32c7881e27966dfe1620314f242
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 23:03:18 +0100

    marker-import.rb: updated vendor comment

--- Casks/marker-import.rb
+++ Casks/marker-import.rb
@@ -2,7 +2,7 @@ cask 'marker-import' do
   version '2.1.2.0'
   sha256 '82654c0b409af3ce609840e196c71989aa7a5c477488c9c99e773f01b8462917'
 
-  # digitalrebellion.com is the official download host per the vendor homepage
+  # digitalrebellion.com was verified as official when first introduced to the cask
   url "http://www.digitalrebellion.com/download/markerimport?version=#{version.no_dots}"
   name 'Kollaborate Marker Import'
   homepage 'https://www.kollaborate.tv/resources'
