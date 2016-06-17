416165aeb5156852287f863d7de5cd5153400b95
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 23:34:18 +0100

    oclint.rb: updated vendor comment

--- Casks/oclint.rb
+++ Casks/oclint.rb
@@ -2,7 +2,7 @@ cask 'oclint' do
   version '0.10.2'
   sha256 '908ff716be831f1842daf81f899d86b1ee56109656fc1b6ccd69f8806ffdb75c'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/oclint/oclint was verified as official when first introduced to the cask
   url "https://github.com/oclint/oclint/releases/download/v#{version}/oclint-#{version}-x86_64-darwin-15.2.0.tar.gz"
   appcast 'https://github.com/oclint/oclint/releases.atom',
           checkpoint: '776ee0710ae369b0995578c4c0b28bf420012975b70703014fe69e943c481f46'
