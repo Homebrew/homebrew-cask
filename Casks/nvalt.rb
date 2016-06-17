48d4af6eee7ec6813715d08685cb29cfbe57e063
Vítor Galvão <info@vitorgalvao.com>
2016-06-17 23:31:55 +0100

    nvalt.rb: updated vendor comment

--- Casks/nvalt.rb
+++ Casks/nvalt.rb
@@ -2,7 +2,7 @@ cask 'nvalt' do
   version '2.2b111'
   sha256 'd787ddf92730bb03ba084e72bc6fb5f4fbd42731fa3531476af9eb3ce39e1cd0'
 
-  # designheresy.com is the official download host per the vendor homepage
+  # abyss.designheresy.com/nvaltb was verified as official when first introduced to the cask
   url "http://abyss.designheresy.com/nvaltb/nvalt#{version}.zip"
   appcast 'http://abyss.designheresy.com/nvalt2/nvalt2main.xml',
           checkpoint: '0a7a6a0a27508d2de6ffcd7ebd5be5db54986ea9ba6694daacbe01c59cdea89d'
