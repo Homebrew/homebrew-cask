23823ce5958380a7052faeb5061a36c0f2c40474
Pieter Nicolai <pnicolai@users.noreply.github.com>
2016-06-18 12:09:41 +0200

    updated telegram (2.15-47337) (#22154)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,11 +1,15 @@
 cask 'telegram' do
-  version :latest
-  sha256 :no_check
+  version '2.15-47337'
+  sha256 '53166186b69d789f94d9153963ff9b20791a83d9e3f73ae644e8772ae911b234'
 
-  url 'https://osx.telegram.org/updates/telegram.dmg'
+  url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
+  appcast 'http://osx.telegram.org/updates/versions.xml',
+          checkpoint: 'fabc96e1a66b355a7e6dc59c5eb00b0d1ae06ffa26857bf7edd949ed1bc17fcc'
   name 'Telegram for macOS'
   homepage 'https://macos.telegram.org'
   license :gpl
+
+  auto_updates true
 
   app 'Telegram.app'
 end
