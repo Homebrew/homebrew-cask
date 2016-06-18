e0d2ada0c1eb298183aa995a1f6dd50100dd20ec
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 00:14:02 +0100

    updated renamer (5)

--- Casks/renamer.rb
+++ Casks/renamer.rb
@@ -1,11 +1,9 @@
 cask 'renamer' do
-  version '4.4.1'
-  sha256 'cc2e4ab9c2074311c8e6cb10662f7e7d2d95f9a8d1bfcd5dc712ab4cc81b782a'
+  version '5'
+  sha256 'cd63c92343eb68e62f3873959758c0b5fb6ab0b5583a1a8112ac6b2fd36ce494'
 
-  # creativebe.com is the official download host per the vendor homepage
-  url 'http://creativebe.com/download/renamer'
-  appcast 'https://storage.googleapis.com/incrediblebee/appcasts/renamer.xml',
-          checkpoint: '1bfd4de2d52080574682e11a22060fbfd7cccfb978e48ab1b996133629cfd500'
+  # storage.googleapis.com/incrediblebee was verified as official when first introduced to the cask
+  url "https://storage.googleapis.com/incrediblebee/apps/Renamer-#{version}/Renamer.zip"
   name 'Renamer'
   homepage 'http://renamer.com'
   license :commercial
