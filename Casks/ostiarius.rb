8bba84e55a142a79257c7f4a2de68a5a02dd23f6
Vítor Galvão <info@vitorgalvao.com>
2016-06-18 21:54:46 +0100

    ostiarius.rb: updated vendor comment

--- Casks/ostiarius.rb
+++ Casks/ostiarius.rb
@@ -2,7 +2,7 @@ cask 'ostiarius' do
   version '1.1.0'
   sha256 '32b1e0071985ba804a6ca3131504f0d8d789e89950110a8919995c607e447dce'
 
-  # bitbucket.org is the official download host per the vendor homepage
+  # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/Ostiarius_#{version}.zip"
   name 'Ostiarius'
   homepage 'https://objective-see.com/products/ostiarius.html'
