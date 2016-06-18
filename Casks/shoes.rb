29d73b68b1c12b12ba29df1008310375b2c7f832
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 00:41:17 +0100

    shoes.rb: updated vendor comment

--- Casks/shoes.rb
+++ Casks/shoes.rb
@@ -2,7 +2,7 @@ cask 'shoes' do
   version '3.3.1'
   sha256 'f30a01d1e5dadc7dd775d140de9f04bf12a5046e4f2d5931ea7b67372ebe56fa'
 
-  # mvmanila.com is the official download host per the vendor homepage
+  # shoes.mvmanila.com/public/shoes was verified as official when first introduced to the cask
   url "http://shoes.mvmanila.com/public/shoes/shoes-#{version}-osx-10.9.tgz"
   name 'Shoes'
   homepage 'http://shoesrb.com/'
