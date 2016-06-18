5296f8fa6638eb24f0cbef558750210efc39b2af
Vítor Galvão <info@vitorgalvao.com>
2016-06-18 22:47:37 +0100

    pomotodo.rb: updated vendor comment

--- Casks/pomotodo.rb
+++ Casks/pomotodo.rb
@@ -2,7 +2,7 @@ cask 'pomotodo' do
   version '0.13.3,1458914986'
   sha256 '8ff2279ffd9e7efa7d3018df36ece019b2b6ca74d1e926f5c7a5a1ca12f436c6'
 
-  # hackplan.com is the official download host per the vendor homepage
+  # cdn.hackplan.com/theair was verified as official when first introduced to the cask
   url "http://cdn.hackplan.com/theair/#{version.after_comma}/Pomotodo_v#{version.before_comma}.dmg"
   appcast 'http://air.hackplan.com/projects/5455f382437315386000d4d5/versions/latest.xml',
           checkpoint: '30f76b576eba5c4a718e3284dc3bbab396f9f384ea5b21f9349b5c56e02cc104'
