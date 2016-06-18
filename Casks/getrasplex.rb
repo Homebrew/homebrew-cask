bc20d46a96c0246e06aa89a9c61194bb98556193
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 00:01:04 +0100

    Update and rename rasplex.rb to getrasplex.rb

--- Casks/getrasplex.rb
+++ Casks/getrasplex.rb
@@ -1,4 +1,4 @@
-cask 'rasplex' do
+cask 'getrasplex' do
   version '1.0.1'
   sha256 '78ffbeb70754495e1dd686e3c351e89d2cf76d64e13b9339e03063c9e942b2ad'
 
@@ -6,9 +6,9 @@ cask 'rasplex' do
   url "https://github.com/RasPlex/rasplex-installer/releases/download/#{version}/GetRasplex-#{version}.dmg"
   appcast 'https://github.com/RasPlex/rasplex-installer/releases.atom',
           checkpoint: '370b7b8c278eb603e593b76b5c0cff1afc97cd592b189403492d3f19f3987f91'
-  name 'Rasplex'
+  name 'Rasplex Installer'
   homepage 'http://www.rasplex.com/'
   license :commercial
 
-  app 'Rasplex.app'
+  app 'GetRasplex.app'
 end
