7d7461042598c9dab236aec04a7008f3380eb971
Vítor Galvão <info@vitorgalvao.com>
2016-06-18 22:48:57 +0100

    preform.rb: updated vendor comment

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -2,7 +2,7 @@ cask 'preform' do
   version '1.8.1_1'
   sha256 '51ed43f83821c9ba10a50d315990aa59ede13a64e46bda438a3e113a93385209'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # s3.amazonaws.com/FormlabsReleases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.sub(%r{_.*}, '')}/PreForm_#{version}.dmg"
   name 'PreForm'
   homepage 'http://formlabs.com/en/products/preform/'
