cdee07f23d499571227a16ca7e9d8391cb28d02d
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 00:44:15 +0100

    sketchupviewer.rb: updated vendor comment

--- Casks/sketchupviewer.rb
+++ Casks/sketchupviewer.rb
@@ -2,8 +2,8 @@ cask 'sketchupviewer' do
   version :latest
   sha256 :no_check
 
-  # trimble.com is the official download host per the vendor homepage
-  # downloads can be found at http://www.sketchup.com/download/all
+  # downloads can be found at https://www.sketchup.com/download/all
+  # dl.trimble.com/sketchup was verified as official when first introduced to the cask
   url 'https://dl.trimble.com/sketchup/SketchUpViewer-en.dmg'
   name 'SketchUpViewer'
   homepage 'https://www.sketchup.com/products/sketchup-viewer'
