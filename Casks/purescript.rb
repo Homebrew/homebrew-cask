ce34728e2d2823764b6c49c8fd61f7eae63a6926
Vítor Galvão <info@vitorgalvao.com>
2016-06-18 23:24:01 +0100

    purescript.rb: updated vendor comment

--- Casks/purescript.rb
+++ Casks/purescript.rb
@@ -2,7 +2,7 @@ cask 'purescript' do
   version '0.8.5'
   sha256 'c80d786e92fd274204a45f8422fc06f5feb2c5db145d117bf1a694135990e59b'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/purescript/purescript was verified as official when first introduced to the cask
   url "https://github.com/purescript/purescript/releases/download/v#{version}/macos.tar.gz"
   appcast 'https://github.com/purescript/purescript/releases.atom',
           checkpoint: 'e96922ccbd64436109b67cf8f9760e261efff65989d4c3a947b5e966677047d0'
