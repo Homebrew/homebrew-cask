686cf435964dc6df9ada7652ef1f39096a8ef391
Vítor Galvão <info@vitorgalvao.com>
2016-06-18 23:12:09 +0100

    projectlibre.rb: updated vendor comment

--- Casks/projectlibre.rb
+++ Casks/projectlibre.rb
@@ -2,7 +2,7 @@ cask 'projectlibre' do
   version '1.6.2'
   sha256 '6136fd390d357ec7f92f56619520ffff7a1255155ee20b25b2df8b96a7208f12'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # downloads.sourceforge.net/project/projectlibre was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/projectlibre/ProjectLibre/#{version}/projectlibre-#{version}.dmg"
   name 'ProjectLibre'
   homepage 'http://www.projectlibre.org/'
