c9e318abb950bfe635f024c3246b373c43188750
specter119 <specter119@gmail.com>
2016-06-18 21:08:29 +0800

    change name to "ZeroNet ZeroBundle"

--- Casks/zeronet.rb
+++ Casks/zeronet.rb
@@ -4,7 +4,7 @@ cask 'zeronet' do
 
   # zeronet.io and https://github.com/HelloZeroNet/ZeroBundle were verified as official when first introduced to the cask
   url 'https://github.com/HelloZeroNet/ZeroBundle/raw/master/dist/ZeroBundle-mac-osx.zip'
-  name 'zeronet'
+  name 'ZeroNet ZeroBundle'
   homepage 'https://zeronet.io/'
   license :gpl # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
