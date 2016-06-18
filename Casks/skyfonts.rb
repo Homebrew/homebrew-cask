3235704c3cb2aea358f75b271ef936eeb1fa48bd
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 00:46:22 +0100

    skyfonts.rb: updated vendor comment

--- Casks/skyfonts.rb
+++ Casks/skyfonts.rb
@@ -2,7 +2,7 @@ cask 'skyfonts' do
   version '5.5.0.0'
   sha256 '8b79798e79f2529a9e30438a9126ab577859a6ee27c56d733cbbd793f9eabfb0'
 
-  # skyfonts.com is the official download host per the vendor homepage
+  # cdn1.skyfonts.com was verified as official when first introduced to the cask
   url "http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_#{version}.dmg"
   name 'SkyFonts'
   homepage 'https://www.fonts.com/web-fonts/google'
