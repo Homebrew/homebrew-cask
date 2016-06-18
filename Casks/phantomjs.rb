cad2b73dd97c1e96fe45809d8c57ec9a4b711334
Vítor Galvão <info@vitorgalvao.com>
2016-06-18 22:40:48 +0100

    phantomjs.rb: updated vendor comment

--- Casks/phantomjs.rb
+++ Casks/phantomjs.rb
@@ -2,7 +2,7 @@ cask 'phantomjs' do
   version '2.1.1'
   sha256 '538cf488219ab27e309eafc629e2bcee9976990fe90b1ec334f541779150f8c1'
 
-  # bitbucket.org is the official download host per the vendor homepage
+  # bitbucket.org/ariya/phantomjs was verified as official when first introduced to the cask
   url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-#{version}-macosx.zip"
   name 'PhantomJS'
   homepage 'http://phantomjs.org/'
