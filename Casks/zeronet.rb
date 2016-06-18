5f348c67798dc64e10b9bac279e8a52258f0cbaf
specter119 <specter119@gmail.com>
2016-06-18 17:12:21 +0800

    add zeronet

---
+++ Casks/zeronet.rb
@@ -0,0 +1,11 @@
+cask 'zeronet' do
+  version :latest
+  sha256 'aab07d27cd8f3fd307a3438a953af3bdfc7de62592c375a9463028ab7c7331e3'
+
+  url 'https://github.com/HelloZeroNet/ZeroBundle/raw/master/dist/ZeroBundle-mac-osx.zip'
+  name 'zeronet'
+  homepage 'https://zeronet.io/'
+  license :gpl # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  suite 'ZeroBundle'
+end
