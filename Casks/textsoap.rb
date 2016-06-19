f27a36873e2381dd6199c183db9d8423fa272db2
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 19:20:32 +0100

    updated textsoap (8.0.9)

--- Casks/textsoap.rb
+++ Casks/textsoap.rb
@@ -1,11 +1,11 @@
 cask 'textsoap' do
-  version '7.4.3'
-  sha256 'ecb88cb6c2cc6fe873a1fccdd38fb6040f2721e3440f98f513b376a360d91f20'
+  version '8.0.9'
+  sha256 'e98eef3b2d7ecda91c800bd9d8c9f01f1d0bd9faa896865b72d8245cce2fe097'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # unmarked.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://unmarked.s3.amazonaws.com/textsoap#{version.major}.zip"
   appcast "https://unmarked.s3.amazonaws.com/appcast/textsoap#{version.major}.xml",
-          checkpoint: 'ddbb0073e1116c64c75620c2fe43bd7af1befc07e82459a3cb614c92a294579d'
+          checkpoint: '36101ecd320abc8e5e48d221645187dfc983ef893b1d98d749ed8d0d5e4e52ad'
   name 'TextSoap'
   homepage 'http://www.unmarked.com/textsoap/'
   license :commercial
