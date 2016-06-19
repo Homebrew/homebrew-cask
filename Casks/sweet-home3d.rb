5146a337b7456b64ba495ce214e0da3d42844ab7
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 19:06:01 +0100

    sweet-home3d.rb: updated vendor comment

--- Casks/sweet-home3d.rb
+++ Casks/sweet-home3d.rb
@@ -2,7 +2,7 @@ cask 'sweet-home3d' do
   version '5.2'
   sha256 'd9450ac52ead59cd24a69616aa7d613a568d841295f56bee3a993c4ed066e2da'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # downloads.sourceforge.net/project/sweethome3d was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/sweethome3d/SweetHome3D/SweetHome3D-#{version}/SweetHome3D-#{version}-macosx.dmg"
   name 'Sweet Home 3D'
   homepage 'http://www.sweethome3d.com/'
