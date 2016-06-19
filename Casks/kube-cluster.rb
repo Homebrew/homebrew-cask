bf0965653bdb1e7421da674b3c559d046ffa857a
Jerzerak <jerzerak@gmail.com>
2016-06-18 22:29:18 -0400

    Kube-Cluster 0.2.9

--- Casks/kube-cluster.rb
+++ Casks/kube-cluster.rb
@@ -1,10 +1,10 @@
 cask 'kube-cluster' do
-  version '0.2.6'
-  sha256 'b959baa8577a01fa637f524eb2f95e80fe49e60c3e4abfb98d75b823815cc1a5'
+  version '0.2.9'
+  sha256 '8d9faabcef5422fb5e3f2ab6701d3897049dbf90dfbca0e4b206f767476ac894'
 
   url "https://github.com/TheNewNormal/kube-cluster-osx/releases/download/v#{version}/Kube-Cluster_v#{version}.dmg"
   appcast 'https://github.com/TheNewNormal/kube-cluster-osx/releases.atom',
-          checkpoint: '72eadab6b8a706ce13ff74385cc620802526ed036d74ec7039b2b284d6e558d8'
+          checkpoint: '5f847b18b166d61b36d8dd14279fb50d84b54873690c2ce878eaad7f425d668a'
   name 'Kube-Cluster'
   homepage 'https://github.com/TheNewNormal/kube-cluster-osx'
   license :apache
