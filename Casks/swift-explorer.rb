a5a35d087d97f00daf3928f6b244fe1686d7b0b0
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 19:06:19 +0100

    swift-explorer.rb: updated vendor comment

--- Casks/swift-explorer.rb
+++ Casks/swift-explorer.rb
@@ -2,7 +2,7 @@ cask 'swift-explorer' do
   version '1.0.7'
   sha256 '24b73711f5555aff19cf2b48edfb781488778fca1d0568c4d9b56b829e70f395'
 
-  # bintray.com is the official download host per the vendor homepage
+  # bintray.com/artifact/download/roikku was verified as official when first introduced to the cask
   url "https://bintray.com/artifact/download/roikku/generic/SwiftExplorer-#{version}-1.0.dmg"
   name 'Swift Explorer'
   homepage 'http://www.619.io/swift-explorer'
