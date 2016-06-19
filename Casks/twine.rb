cb607f1189c792f059a9d280f68aaf6fd048b4c4
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 19:47:44 +0100

    twine.rb: updated vendor comment

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -2,7 +2,7 @@ cask 'twine' do
   version '2.0.11'
   sha256 '360d7b72b538d622e01d5fdac059bf6ddeac97619fdf5002fae736ca4b4eca19'
 
-  # bitbucket.org is the official download host per the vendor homepage
+  # bitbucket.org/klembot/twinejs was verified as official when first introduced to the cask
   url "https://bitbucket.org/klembot/twinejs/downloads/twine_#{version}_osx.zip"
   name 'Twine'
   homepage 'http://twinery.org/'
