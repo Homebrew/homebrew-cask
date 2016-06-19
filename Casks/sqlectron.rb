82e638268e709c2445d0f8129ec0b7f6d0e8d8dc
Max Claus Nunes <maxcnunes@gmail.com>
2016-06-19 01:06:08 -0300

    updated sqlectron (1.11.1)

--- Casks/sqlectron.rb
+++ Casks/sqlectron.rb
@@ -1,11 +1,11 @@
 cask 'sqlectron' do
-  version '1.11.0'
-  sha256 'dbdc7f33953ab3ae386301a75716417f284c23184291f686a3c39026ef321581'
+  version '1.11.1'
+  sha256 '43947827af0f0ce5c648fdfab34e62d785592878fa08f05524dd17ed968dae9a'
 
   # github.com/sqlectron/sqlectron-gui was verified as official when first introduced to the cask
   url "https://github.com/sqlectron/sqlectron-gui/releases/download/v#{version}/Sqlectron-#{version}-mac.zip"
   appcast 'https://github.com/sqlectron/sqlectron-gui/releases.atom',
-          checkpoint: '24706dd691a4d50bcfbbaf3bd04da6d4b4da1761539737fdb196feafbaf9cdec'
+          checkpoint: '55cb6da44252c98d325251dc4bc524d71c8317c468a74c318e95b56d9eab2da3'
   name 'Sqlectron'
   homepage 'https://sqlectron.github.io/'
   license :mit
