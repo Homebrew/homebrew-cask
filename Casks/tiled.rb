c30904ac5c16328f6d8e086b1ff3979806952b4c
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 19:32:26 +0100

    tiled.rb: updated vendor comment

--- Casks/tiled.rb
+++ Casks/tiled.rb
@@ -2,7 +2,7 @@ cask 'tiled' do
   version '0.16.0'
   sha256 '6d2ea8026ea20f5d449670ab70194f649135b5452fd6ebbb88ea5ab25f5d14b3'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/bjorn/tiled was verified as official when first introduced to the cask
   url "https://github.com/bjorn/tiled/releases/download/v#{version}/tiled-#{version}.dmg"
   appcast 'https://github.com/bjorn/tiled/releases.atom',
           checkpoint: '355ff763b7fce255b981fa745bcf58e72c3de809d37f955ad8a4f37a59be488e'
