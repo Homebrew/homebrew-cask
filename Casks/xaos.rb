7a68c84f06d540ffc4efcbbebf597d732cbf9cbb
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 20:12:21 +0100

    xaos.rb: updated vendor comment

--- Casks/xaos.rb
+++ Casks/xaos.rb
@@ -2,7 +2,7 @@ cask 'xaos' do
   version '3.6'
   sha256 '1a3864f354759c03c13150ef541b48d06cf74351360a85a8b46e73a45edc7054'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # downloads.sourceforge.net/xaos was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/xaos/xaos-#{version}-macosx.dmg"
   name 'GNU XaoS'
   homepage 'http://matek.hu/xaos'
