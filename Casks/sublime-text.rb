549f614a2d27dad8c400b5eb237e3d9353187f6d
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 19:02:04 +0100

    updated sublime-text (2.0.2)

--- Casks/sublime-text.rb
+++ Casks/sublime-text.rb
@@ -2,8 +2,7 @@ cask 'sublime-text' do
   version '2.0.2'
   sha256 '906e71e19ae5321f80e7cf42eab8355146d8f2c3fd55be1f7fe5c62c57165add'
 
-  # rackcdn.com is the official download host per the vendor homepage
-  url "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20#{version}.dmg"
+  url "https://download.sublimetext.com/Sublime%20Text%20#{version}.dmg"
   appcast 'https://www.sublimetext.com/updates/2/stable/appcast_osx.xml',
           checkpoint: '1f88d22b2ed5ba5a9213accf500ca8f1a9723b391fbbbc45ffb23cd98c98519c'
   name 'Sublime Text'
