fae2a3e3e23d20e5fbb48619ec4008e9e26674d7
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 19:05:44 +0100

    subtitle-master.rb: updated vendor comment

--- Casks/subtitle-master.rb
+++ Casks/subtitle-master.rb
@@ -2,7 +2,7 @@ cask 'subtitle-master' do
   version '2.0.1'
   sha256 '8936495ef4aefe9bf59c28dd4d5a92574c194062067c318667e3f55428245304'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/subtitle-master/subtitlemaster was verified as official when first introduced to the cask
   url "https://github.com/subtitle-master/subtitlemaster/releases/download/v#{version}-SNAPSHOT/Subtitle.Master-osx-v#{version}-SNAPSHOT.zip"
   appcast 'https://github.com/subtitle-master/subtitlemaster/releases.atom',
           checkpoint: 'e12b1034eeb5c53a908d9ddaea48e3e58d408853b4d0836bbbc50334e7a70878'
