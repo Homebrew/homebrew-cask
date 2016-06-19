53d64efa18af874992f82a6b8f21e6b386b4c774
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 18:37:37 +0100

    sonic-visualiser.rb: updated vendor comment

--- Casks/sonic-visualiser.rb
+++ Casks/sonic-visualiser.rb
@@ -2,7 +2,7 @@ cask 'sonic-visualiser' do
   version '2.5.0.1'
   sha256 'f271f685c2198924bb90355b856673cb8c526368aa2ce1251e37b4bab6c1870b'
 
-  # ac.uk is the official download host per the vendor homepage
+  # code.soundsoftware.ac.uk was verified as official when first introduced to the cask
   url "https://code.soundsoftware.ac.uk/attachments/download/1680/Sonic%20Visualiser-#{version}.dmg"
   name 'Sonic Visualiser'
   homepage 'http://www.sonicvisualiser.org/'
