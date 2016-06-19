72eb83741fa51374745643007ec74b2e16f841ed
Vítor Galvão <info@vitorgalvao.com>
2016-06-19 20:04:46 +0100

    workamajig.rb: updated vendor comment

--- Casks/workamajig.rb
+++ Casks/workamajig.rb
@@ -2,7 +2,7 @@ cask 'workamajig' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # s3.amazonaws.com/Workamajig was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/Workamajig/AIR/Workamajig.air'
   name 'Workamajig'
   homepage 'http://www.workamajig.com/'
