960ed478f1120006002b38f12347335d70fbad92
Vítor Galvão <info@vitorgalvao.com>
2016-06-20 14:38:34 +0100

    updated rodeo (2.0.2) (#22262)

--- Casks/rodeo.rb
+++ Casks/rodeo.rb
@@ -1,11 +1,11 @@
 cask 'rodeo' do
-  version '2.0.0'
-  sha256 'd9dfba1f62934e4ec7c80c4bfd0d4b77ddec63254f9d14b7cc867c0c20d5baf1'
+  version '2.0.2'
+  sha256 '26bda274e10111db1ca60995552dd3f955de49dcc2c3907a1033c7ac34286df3'
 
   # github.com/yhat/rodeo was verified as official when first introduced to the cask
   url "https://github.com/yhat/rodeo/releases/download/v#{version}/Rodeo-#{version}.dmg"
   appcast 'https://github.com/yhat/rodeo/releases.atom',
-          checkpoint: '900acf0852da14c0d38a73345f82cfa37a951123af8dac9dce0cacb8a35b61fc'
+          checkpoint: '83e57ee8349297372a8315a725b8782734863e8a92a05ac10ac0e9fc66e3807c'
   name 'Rodeo'
   homepage 'http://rodeo.yhat.com/'
   license :affero
