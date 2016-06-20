fe0d8f6279bc2f52f8cad9a47dc05f80f116d080
Victor Popkov <victor@popkov.me>
2016-06-21 01:36:48 +0300

    updated simple-hub (3.5.4-341) (#22286)

--- Casks/simple-hub.rb
+++ Casks/simple-hub.rb
@@ -1,10 +1,10 @@
 cask 'simple-hub' do
-  version '3.5.3-326'
-  sha256 'bb5e1c74fc27b5f87471e77e7a7d24bbd83a17ccbd555a011260dd6689fa7e61'
+  version '3.5.4-341'
+  sha256 '80442edb0db21b247ead0a56a645453ceb23bc361f26dba3d2d1359e2fe384c0'
 
   url "https://www.simplecontrol.com/b/SimpleHub-#{version.no_dots}.zip"
   appcast 'https://www.simplecontrol.com/b/Simple-HubAppcast.xml',
-          checkpoint: '4ad96e6a4b2f55058311506f0dd3b6020fcbf4c7ce954ee216707bf8ecd207c9'
+          checkpoint: 'e14bfc0223b32aad19294df061da3a5690a559088db6522e976dec38b5740018'
   name 'Simple Hub'
   homepage 'https://store.simplecontrol.com/simple-sync.html'
   license :commercial
