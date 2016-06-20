075acfc486b3409dcf7f4e159e297e2531a1c779
Danijel-James Wynyard <danijel.james@mgail.com>
2016-06-20 14:01:11 +0930

    removed appcast url as no longer working

--- Casks/unrarx.rb
+++ Casks/unrarx.rb
@@ -3,8 +3,6 @@ cask 'unrarx' do
   sha256 'd371ae58bfae5269575cf0822a5432a371b2f1b4e710e4b08b465aebf88ce1ae'
 
   url "https://www.danijelj.com/files/unrarx/UnRarX_#{version}.zip"
-  appcast 'http://www.unrarx.com/update.xml',
-          checkpoint: '17fa2f3324d45c27a318ed51dab739c7f09b573185b76889b955ad2c9ad1d7b8'
   name 'UnRarX'
   homepage 'https://www.danijelj.com/unrarx/'
   license :gratis
