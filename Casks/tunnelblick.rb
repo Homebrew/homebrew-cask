76d666c3df1f41af6d7d5aa1c5b2ea1ed929a4c6
Alex Morega <alex@grep.ro>
2016-06-23 18:21:27 +0300

    update tunnelblick to 3.6.4a_build_4561

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -3,8 +3,8 @@ cask 'tunnelblick' do
     version '3.5.9_build_4270.4560'
     sha256 '7651754cab92c5f61fc22b55448875cf14fcf8b6f5b3ba469899740c49b6fae3'
   else
-    version '3.6.3_build_4560'
-    sha256 '0240c92c2b27670f25319a9e083ad6be318fac6ad5a87357cc2b4d7874381066'
+    version '3.6.4a_build_4561'
+    sha256 'a2ed7027109d3edc34998abe72623e64d811e9c2a6c03c8c29ed0cce24617943'
   end
 
   url "https://www.tunnelblick.net/release/Tunnelblick_#{version}.dmg"
