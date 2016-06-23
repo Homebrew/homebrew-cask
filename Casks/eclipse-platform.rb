e0628455a075840f605e014b9326bee0ed5391fe
Fabian Vogler <fabian@equivalence.ch>
2016-06-23 16:54:20 +0200

    Update Eclipse platform to 4.6
    Neon release

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-platform' do
-  version '4.5-201506032000'
-  sha256 '953b7ecacb3c84667c616e1b640240de8cf5c045f475d0aebc6179316ed083d6'
+  version '4.6-201606061100'
+  sha256 'd8c0b7b970581bbfab047375c0bf44243f5f4e528e950be2313e275451d684b6'
 
   url "http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.to_i}/R-#{version}/eclipse-SDK-#{version.sub(%r{-.*}, '')}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse SDK'
