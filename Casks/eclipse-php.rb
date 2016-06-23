5cde48a83756b33919912e1dbea081535a3c3f88
Fabian Vogler <fabian@equivalence.ch>
2016-06-23 16:36:31 +0200

    Update Eclipse packages to 4.6
    Neon release

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-php' do
-  version '4.5.2'
-  sha256 '8198b5826bba50ed93ed7dfbaa8f46bd30bbe06ec9817a4c0ebbcd2c196eea91'
+  version '4.6.0'
+  sha256 '763b2e6f41277f61d377ed15aa7c6277f0a308c9b162ff00ffab46a823fc0865'
 
-  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/2/eclipse-php-mars-2-macosx-cocoa-x86_64.tar.gz&r=1'
+  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/R/eclipse-php-neon-R-macosx-cocoa-x86_64.tar.gz&r=1'
   name 'Eclipse for PHP Developers'
   homepage 'https://eclipse.org/'
   license :eclipse
