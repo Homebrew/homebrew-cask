8b3bd4525f042dd81cfb986a9d602449ce0c5133
Markus Reiter <me@reitermark.us>
2016-06-23 03:33:48 +0200

    Update Plex Media Server to 0.9.17.3 (#22328)

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,13 +1,13 @@
 cask 'plex-media-server' do
-  version '0.9.16.6.1993-5089475'
-  sha256 'b788d6879920e26574956ef8d177fd4663a8df89c71250c07d1a3deece110af2'
+  version '0.9.17.3.2239-fe07491'
+  sha256 '19560ff62e0f8645131cea87b59f19d503c56a25210eddd02244ef1f2484bd02'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   name 'Plex Media Server'
   homepage 'https://plex.tv/'
   license :freemium
 
-  depends_on macos: '>= :snow_leopard'
+  depends_on macos: '>= :mountain_lion'
 
   app 'Plex Media Server.app'
 end
