664b07fe33bdce7907d7e8f8f36cf71d5a2af29f
Victor Popkov <victor@popkov.me>
2016-06-23 20:11:41 +0300

    updated splice (2.0.9-201606161636)

--- Casks/splice.rb
+++ Casks/splice.rb
@@ -1,11 +1,11 @@
 cask 'splice' do
-  version '2.0.8-201606151628'
-  sha256 'e71a29b02fd4c96f4ada9696395cfcd60d2aa9e19780064894fb1e1adae387ff'
+  version '2.0.9-201606161636'
+  sha256 '60e05085b77f25d70a1e6a4c854fb9cfbfe26e28a62519817d28ce8c3ce75416'
 
   # amazonaws.com/spliceosx was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/spliceosx/Splice.app-#{version}.zip"
   appcast 'https://splice.com/appcast.xml',
-          checkpoint: '4b16f14fb56ba110be13e90e7d0e3bebcafb1a9729ec770b27cc430e8a34e941'
+          checkpoint: '52e8c0268cffb6ba6479abfde683fb87d4fddae430b5c5f8441dc5459c6e9b5b'
   name 'Splice'
   homepage 'https://splice.com/'
   license :gratis
