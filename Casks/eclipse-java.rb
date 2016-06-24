48bcaa5b64752098d1e1e7e1701150c766ccffc3
Fabian Vogler <fabian@equivalence.ch>
2016-06-24 07:58:12 +0200

    Update Eclipse packages to 4.6 (#22339)
    Neon release

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-java' do
-  version '4.5.2'
-  sha256 'ecb79ad20548d65643d6c1b24992f900c7192cf8f3c54a1b38ed575835f2a6fe'
+  version '4.6.0'
+  sha256 'cc776030aac93ca313c2e7791f3368af399d595c6979e421ca7c36c180de3c96'
 
-  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/2/eclipse-java-mars-2-macosx-cocoa-x86_64.tar.gz&r=1'
+  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/R/eclipse-java-neon-R-macosx-cocoa-x86_64.tar.gz&r=1'
   name 'Eclipse IDE for Java Developers'
   homepage 'https://eclipse.org/'
   license :eclipse
--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-jee' do
-  version '4.5.2'
-  sha256 'e5989046a9518b691b38fb718f7e60a4c4f593176115807579979e14ee7c15ad'
+  version '4.6.0'
+  sha256 'ec62c9734396fb99ae5fc8af8731bb87d19d4d40579aeac69b8fe1447c51a614'
 
-  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-macosx-cocoa-x86_64.tar.gz&r=1'
+  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/R/eclipse-jee-neon-R-macosx-cocoa-x86_64.tar.gz&r=1'
   name 'Eclipse IDE for Java EE Developers'
   homepage 'https://eclipse.org/'
   license :eclipse
