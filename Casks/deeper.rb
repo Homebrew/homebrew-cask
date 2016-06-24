fd3ad0a3fc18935c278d22dde0234854110e0a5f
Markus Reiter <me@reitermark.us>
2016-06-24 23:10:41 +0200

    Simplify "Titanium Software" casks. (#22363)

--- Casks/deeper.rb
+++ Casks/deeper.rb
@@ -2,32 +2,20 @@ cask 'deeper' do
   version :latest
   sha256 :no_check
 
-  if MacOS.release == :tiger
-    url 'http://www.titanium.free.fr/download/104/Deeper.dmg'
-  elsif MacOS.release == :leopard
-    url 'http://www.titanium.free.fr/download/105/Deeper.dmg'
-  elsif MacOS.release == :snow_leopard
-    url 'http://www.titanium.free.fr/download/106/Deeper.dmg'
-  elsif MacOS.release == :lion
-    url 'http://www.titanium.free.fr/download/107/Deeper.dmg'
-  elsif MacOS.release == :mountain_lion
-    url 'http://www.titanium.free.fr/download/108/Deeper.dmg'
-  elsif MacOS.release == :mavericks
-    url 'http://www.titanium.free.fr/download/109/Deeper.dmg'
-  elsif MacOS.release == :yosemite
-    url 'http://www.titanium.free.fr/download/1010/Deeper.dmg'
-  elsif MacOS.release == :el_capitan
-    # joel.barriere.pagesperso-orange.fr was verified as official when first introduced to the cask
-    url 'http://joel.barriere.pagesperso-orange.fr/download/1011/Deeper.dmg'
+  macos_release = MacOS.release.to_s.delete('.')
+
+  if MacOS.release <= :yosemite
+    url "http://www.titanium.free.fr/download/#{macos_release}/Deeper.dmg"
   else
-    # Unusual case: there is no fall-through.  The software will stop
-    # working, or is dangerous to run, on the next macOS release.
+    # joel.barriere.pagesperso-orange.fr was verified as official when first introduced to the cask
+    url "http://joel.barriere.pagesperso-orange.fr/download/#{macos_release}/Deeper.dmg"
   end
 
   name 'Deeper'
   homepage 'http://www.titanium.free.fr/deeper.html'
   license :gratis
 
+  # Unusual case: The software will stop working, or is dangerous to run, on the next macOS release.
   depends_on macos: %w[
                       :tiger
                       :leopard
