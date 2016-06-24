985e6b38b9ef5f8d0a6c633637a4dac9e7b760f5
specter119 <specter119@users.noreply.github.com>
2016-06-24 19:58:11 +0800

    Update Julia to 0.4.6
    Update Julia to 0.4.6

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask 'julia' do
-  version '0.4.5'
-  sha256 'b253c035b846bbf120695306a5d8bc5d204e8287a5d3d3721a12408e1c990539'
+  version '0.4.6'
+  sha256 '81d6d73f3519788164431c996a563663fe002ce901e55de80d3f72b4f9bc473e'
 
   # s3.amazonaws.com/julialang was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/julialang/bin/osx/x64/#{version.sub(%r{\.\d+$}, '')}/julia-#{version}-osx10.7+.dmg"
