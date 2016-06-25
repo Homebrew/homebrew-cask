ecec093ab9b99cab5dda327dcedd2e628641e141
Koen Rouwhorst <koenrh@users.noreply.github.com>
2016-06-25 02:03:47 +0200

    updated brave (0.10.4dev) (#22374)

--- Casks/brave.rb
+++ Casks/brave.rb
@@ -1,11 +1,11 @@
 cask 'brave' do
-  version '0.10.3dev'
-  sha256 '61d5ffde1f6fd4975230a98181f3f789f5fccb9c54906071d882bde2c0c9c9be'
+  version '0.10.4dev'
+  sha256 '170fc2cb685d11320644aa7ff02cce8658425513f53536702e8ef3773ee79a54'
 
   # github.com/brave/browser-laptop was verified as official when first introduced to the cask
   url "https://github.com/brave/browser-laptop/releases/download/v#{version}/Brave.dmg"
   appcast 'https://github.com/brave/browser-laptop/releases.atom',
-          checkpoint: 'bb4e23a982c897c40b5d8d5add2717a6d8c344965fab31e5a8c173d31ac9e494'
+          checkpoint: 'e402a34e51ceaa5074836f924770bfbbadd69cfa7ebff74e57d49497a61df187'
   name 'Brave'
   homepage 'https://brave.com'
   license :mpl
