bc7393c047e5f92e20f95080df14d2b9b5674f35
Haroen Viaene <fingebimus@me.com>
2016-06-25 12:31:27 +0200

    Create emojify.rb
    This adds the cask emojify which is <https://github.com/Haroenv/emojify-service>

---
+++ Casks/emojify.rb
@@ -0,0 +1,11 @@
+cask 'emojify' do
+  version 'latest'
+  sha256 :no_check
+
+  url "https://github.com/Haroenv/emojify-service/archive/#{version}.tar.gz"
+  name 'Emojify Service'
+  homepage 'https://github.com/Haroenv/emojify-service'
+  license :apache
+
+  service 'Emojify.workflow'
+end
