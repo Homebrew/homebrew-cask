ab99848b4a6bac5eeb083a2452dfb876e3ef2095
Haroen Viaene <fingebimus@me.com>
2016-06-25 13:31:16 +0200

    use versioned link

--- Casks/emojify.rb
+++ Casks/emojify.rb
@@ -1,8 +1,8 @@
 cask 'emojify' do
-  version :latest
-  sha256 :no_check
+  version '0.1.0'
+  sha256 '2ca93bf5707dfa67e4be8f1f27384ac1cb0d2644154999c7904b5d8b303ba18c'
 
-  url "https://github.com/Haroenv/emojify-service/archive/#{version}.tar.gz"
+  url "https://github.com/Haroenv/emojify-service/archive/v#{version}.zip"
   appcast 'https://github.com/Haroenv/emojify-service/releases.atom',
           checkpoint: 'b53826c2bbf31c9ee4a668ebed4f370458e3d3f08fb2dc527242e4fff0f56d04'
   name 'Emojify Service'
