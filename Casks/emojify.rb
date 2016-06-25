158935428ab6f2b3c9187bd65e6c8f7e7c348c4d
Haroen Viaene <fingebimus@me.com>
2016-06-25 12:38:43 +0200

    add appcast

--- Casks/emojify.rb
+++ Casks/emojify.rb
@@ -3,6 +3,8 @@ cask 'emojify' do
   sha256 :no_check
 
   url "https://github.com/Haroenv/emojify-service/archive/#{version}.tar.gz"
+  appcast  appcast 'https://github.com/Haroenv/emojify-service/releases.atom',
+          checkpoint: 'b53826c2bbf31c9ee4a668ebed4f370458e3d3f08fb2dc527242e4fff0f56d04'
   name 'Emojify Service'
   homepage 'https://github.com/Haroenv/emojify-service'
   license :apache
