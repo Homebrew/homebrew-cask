46bc06486a62c65b58730793a87147c354a66dbd
asins <asinsimple@gmail.com>
2016-06-26 02:26:59 +0800

    firefoxdeveloperedition-cn

---
+++ Casks/firefoxdeveloperedition-cn.rb
@@ -0,0 +1,11 @@
+cask 'firefoxdeveloperedition' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://download.mozilla.org/?product=firefox-aurora-latest-l10n&os=osx&lang=zh-CN'
+  name 'Mozilla Firefox Developer Edition'
+  homepage 'https://www.mozilla.org/zh-CN/firefox/developer/'
+  license :mpl
+
+  app 'FirefoxDeveloperEdition.app'
+end
