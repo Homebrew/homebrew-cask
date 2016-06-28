f07aeabd94802bd969f8445790193ba8c907098d
Zhang Cheng <stephenpcg@gmail.com>
2016-06-28 19:32:56 +0800

    qiniu-devtools: remove 'v' from version, and put it in the url

--- Casks/qiniu-devtools.rb
+++ Casks/qiniu-devtools.rb
@@ -1,9 +1,9 @@
 cask 'qiniu-devtools' do
-  version 'v3.1.20151125'
+  version '3.1.20151125'
   sha256 '4a1ebcee5ea681e7d7e9f5544d7fe22e27240f70de9c0cd7cb790e8868a2b5f8'
 
   # devtools.qiniu.io was verified as official when first introduced to the cask
-  url "http://devtools.qiniu.io/qiniu-devtools-darwin_amd64-#{version}.tar.gz"
+  url "http://devtools.qiniu.io/qiniu-devtools-darwin_amd64-v#{version}.tar.gz"
   name 'Qiniu Developer Tools'
   homepage 'http://developer.qiniu.com/'
   license :unknown
