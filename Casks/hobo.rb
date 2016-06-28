582dacac340f1021719611202ed048cbf4aa6b19
Victor Popkov <victor@popkov.me>
2016-06-29 01:05:51 +0300

    updated hobo (1.1.7) (#22452)

--- Casks/hobo.rb
+++ Casks/hobo.rb
@@ -1,11 +1,11 @@
 cask 'hobo' do
-  version '1.1.6'
-  sha256 'dd22f3c4cc83e62a099303cbbae4777cc892de6b7ebe0c83ea7b0d682216f7ee'
+  version '1.1.7'
+  sha256 '82aa973997f1e3655d6d9f8545e99824e64f2f1fad0ec45600bcdac42c0e648d'
 
   # downloads-clickonideas.netdna-ssl.com/hobo was verified as official when first introduced to the cask
   url "https://downloads-clickonideas.netdna-ssl.com/hobo/hobo_#{version}.zip"
   appcast 'https://shine.clickontyler.com/appcast.php?id=31',
-          checkpoint: 'a71e0ca6be6ba4ae10e657c168b118f7d991385249dbaa76ea19a7ddf9a7edac'
+          checkpoint: '7e1bee541054caae98209bdb7039350806173a1baf9ffff81e659f12bc8d1460'
   name 'Hobo'
   homepage 'https://clickontyler.com/hobo/'
   license :commercial
