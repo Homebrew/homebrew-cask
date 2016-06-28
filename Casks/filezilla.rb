509676d2ea22486c18df22e100b773584aa4eac1
Steven Roddis <fuzzyroddis@users.noreply.github.com>
2016-06-28 16:28:59 +1000

    Update FileZilla cask to 3.19.0 (#22438)

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -3,8 +3,8 @@ cask 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.18.0'
-    sha256 '3003240707506959175938ab81de43d784caf9ed3cb95a6e068b09848008ecf5'
+    version '3.19.0'
+    sha256 'e92f48055131f5b3cea24a0862e319be158feac27f1c918e3c1e17c2665aeccf'
   end
 
   # sourceforge.net/project/filezilla was verified as official when first introduced to the cask
