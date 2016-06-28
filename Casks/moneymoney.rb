98e9e7e215c578a47252f3f4a35841fc1c544a67
Victor Popkov <victor@popkov.me>
2016-06-28 20:22:57 +0300

    updated moneymoney (2.2.12)

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,10 +1,10 @@
 cask 'moneymoney' do
-  version '2.2.11'
-  sha256 'db1f03f71cf150888e504c958341c8f5d191592e1b6d21bff336af0dac0e3892'
+  version '2.2.12'
+  sha256 'def3e59b19d5acd7f3acf568c09fccab9b6dd63ef0a1bc8125d44d32f9d13ea6'
 
   url 'https://service.moneymoney-app.com/1/MoneyMoney.zip'
   appcast 'https://service.moneymoney-app.com/1/Appcast.xml',
-          checkpoint: 'bc13fef965482dbaa024e5cedb7af7a81ce6be91f7748e1bba84e4491c62ebc3'
+          checkpoint: 'c34e4d0540fbcf09f6f1bd9419be82867f8832c523f324a10f736bc010267d58'
   name 'MoneyMoney'
   homepage 'https://moneymoney-app.com/'
   license :commercial
