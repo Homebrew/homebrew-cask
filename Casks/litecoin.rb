28d5166272832e297f9f41651f79cd649f8dc4ad
Aditya Dalal <adidalal@users.noreply.github.com>
2016-06-28 13:53:09 -0400

    updated litecoin (0.10.4.0)

--- Casks/litecoin.rb
+++ Casks/litecoin.rb
@@ -1,6 +1,6 @@
 cask 'litecoin' do
-  version '0.10.2.2'
-  sha256 'f37c813319504dcb50883eaf9c77795df1be1fe748b5e452ca19c89c2b7543da'
+  version '0.10.4.0'
+  sha256 '165452e7eefb36771002fdfc8e35b436f4af5280683c33ee6919282be5078f01'
 
   url "https://download.litecoin.org/litecoin-#{version}/osx/litecoin-#{version}-osx.dmg"
   name 'Litecoin'
