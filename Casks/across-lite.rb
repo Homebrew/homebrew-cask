f9119bb1ae538ec12a08d6c85e92598a2ba212b8
AVP <aakpat6@gmail.com>
2016-06-28 11:21:36 -0500

    Add Across Lite 2.3.1. (#22403)

---
+++ Casks/across-lite.rb
@@ -0,0 +1,11 @@
+cask 'across-lite' do
+  version '2.3.1'
+  sha256 '49720648b8eb766fa0fbdebfd98213cdec442565f36b62f6406f0ff1f1462c13'
+
+  url "http://www.litsoft.com/across/alite/download/download.php/al#{version.no_dots}osx.dmg?os=macosx"
+  name 'Across Lite'
+  homepage 'http://www.litsoft.com/across/alite/download/'
+  license :unknown
+
+  app 'Across Lite.app'
+end
