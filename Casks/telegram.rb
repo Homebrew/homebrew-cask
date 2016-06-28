018c03df6ac5ff02cc9ad578e74a4a7fe6ed1951
Patryk Soika <patryk@superrenaissanceman.com>
2016-06-27 22:16:37 -0400

    updated telegram (2.16-47508)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,11 +1,11 @@
 cask 'telegram' do
   version '2.16-47508'
-  sha256 '395911b884c65259e596c7ef7b6fee278578231279cb0e71f22dc5788b0a850b'
+  sha256 '5ffbd2f76054fc47aafa6f2596ef9c6a85a69412a3a1f227f789b1832e7518d5'
 
   # telegram.org was verified as official when first introduced to the cask
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'http://osx.telegram.org/updates/versions.xml',
-          checkpoint: '90b88fae996b8f22c2df063127b60547643044dfdc84b7f775d4e171f5618718'
+          checkpoint: '07caec5ebbe1b7081cb2e873cb5321d6f736b6ecbe7421968ba2f69745ac1a6e'
   name 'Telegram for macOS'
   homepage 'https://macos.telegram.org'
   license :gpl
