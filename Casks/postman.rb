0180bcbfaf0d2b6312d5706184ad32ed4a419c94
GyuYong Jung <Ephemera@users.noreply.github.com>
2016-06-28 22:02:33 +0900

    update postman (4.4.1) (#22441)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,11 +1,11 @@
 cask 'postman' do
-  version '4.3.2'
-  sha256 '4716c4979fe5470f46af423f5fa8cea663778bf9c3f1ef7a78d94ffaedfd56a2'
+  version '4.4.1'
+  sha256 '6d2f114e6c56d23e0fa6e6c1d1fe263be55a7b4a9a4da53e7ff49512c390d68b'
 
   # s3.amazonaws.com/postman-electron-builds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/postman-electron-builds/mac/Postman-osx-#{version}.zip"
   appcast 'https://app.getpostman.com/api/electron_updates_auto',
-          checkpoint: 'dfcdcac83f9a2d2afef9c6e6252f1468913c55135e27e0fa5fc87848d63cfc0a'
+          checkpoint: '3d361f2a04add4047aea0c7a4c221f8e1c5f016af94d6746716136a640886946'
   name 'Postman'
   homepage 'https://www.getpostman.com/'
   license :closed
