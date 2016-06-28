191d270e6cc132ca608153addc3b1e91bdb7942e
Haroen Viaene <fingebimus@me.com>
2016-06-28 15:42:58 +0200

    Update emojify.rb
    change appcast checkpoint

--- Casks/emojify.rb
+++ Casks/emojify.rb
@@ -4,7 +4,7 @@ cask 'emojify' do
 
   url "https://github.com/Haroenv/emojify-service/archive/v#{version}.zip"
   appcast 'https://github.com/Haroenv/emojify-service/releases.atom',
-          checkpoint: 'b53826c2bbf31c9ee4a668ebed4f370458e3d3f08fb2dc527242e4fff0f56d04'
+          checkpoint: 'd19588f51bfb615eb77bead4deb04eb8871dc320bc3d934538f52d212f394ad1'
   name 'Emojify Service'
   homepage 'https://github.com/Haroenv/emojify-service'
   license :apache
