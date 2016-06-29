bbf4bf849628fe20fe9b830b363bb38712b0f022
Dmitry Labutin <dmitry@labutin.net>
2016-06-29 12:44:05 +0300

    Fix Download URI

--- Casks/mongochef.rb
+++ Casks/mongochef.rb
@@ -2,7 +2,7 @@ cask 'mongochef' do
   version '4.0.1'
   sha256 'f9668ae195dafd27ae206c3f1033e7352d9cbdf85b2f32b86071978226bd259b'
 
-  url "https://cdn.3t.io/mongochef/mac/#{version}/MongoChef.dmg"
+  url "https://cdn.3t.io/mongochef-core/mac/#{version}/MongoChef.dmg"
   name 'MongoChef'
   homepage 'https://3t.io/mongochef/'
   license :other
