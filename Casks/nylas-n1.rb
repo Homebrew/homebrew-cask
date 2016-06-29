98f707275b3f938bc04d69d8e0dfe08b317117f0
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-06-29 14:20:22 -0300

    updated nylas-n1 (0.4.45-7637265) (#22482)

--- Casks/nylas-n1.rb
+++ Casks/nylas-n1.rb
@@ -1,11 +1,11 @@
 cask 'nylas-n1' do
-  version '0.4.40-85cf726'
-  sha256 '668ff7119a51c5aff01dddfead4cfd9befbf51157af54a3d719ffdb0024956c1'
+  version '0.4.45-7637265'
+  sha256 '550867275f1b72256989409ea9ef2fcfbb1eb4aa920b91b1cd0283a6bd0e173f'
 
   # edgehill.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
   url "https://edgehill.s3-us-west-2.amazonaws.com/#{version}/darwin/x64/N1.dmg"
   appcast 'https://edgehill.nylas.com/update-check?platform=darwin&arch=64',
-          checkpoint: 'fae87f180132f0c60fa400a632ae7d5ef5f13d3c8db924603d78483df0a96dc8'
+          checkpoint: '2c50861ecacabfac6e9bcd47940dd523da2f94825466f1ff06f4f0d794c81264'
   name 'Nylas N1'
   homepage 'https://www.nylas.com/n1'
   license :gpl
