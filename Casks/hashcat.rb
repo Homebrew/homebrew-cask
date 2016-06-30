938f9d0f804b2137d5bd40c9cd0e87f1856a793d
Mateusz Kowalski <mkowalski@users.noreply.github.com>
2016-06-30 09:40:37 +0200

    updated hashcat (3.00) (#22498)

--- Casks/hashcat.rb
+++ Casks/hashcat.rb
@@ -1,6 +1,6 @@
 cask 'hashcat' do
-  version '2.00'
-  sha256 '142c2b419bedbec36c19ee14d09a3fe49e876f1a1d6064fab6595eca83e920cd'
+  version '3.00'
+  sha256 '54fe501ecc7b5246aaf92eef3f6afc23f985f721ef8d53e5ce5fda7d680f46a2'
 
   url "https://hashcat.net/files/hashcat-#{version}.7z"
   name 'hashcat'
