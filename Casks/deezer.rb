f911774708cfdcf3329f688fe4adf69e434670c2
Loïc Froidmont <lfroidmo@gmail.com>
2016-06-30 11:14:37 +0200

    updated deezer (1.1.3_4308)

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -1,6 +1,6 @@
 cask 'deezer' do
-  version '1.1.3_4211'
-  sha256 '2ac3bc4c631773b2de9214eff5fc0a0b67362d08d4fc1237e673e67aa9aa388e'
+  version '1.1.3_4308'
+  sha256 '244530cb29c232c8464660153ddd052d81c7c6e0a6317288544f3225f55d79e5'
 
   url "http://e-cdn-content.deezer.com/builds/mac/Deezer_#{version.sub(%r{.*_}, '')}.dmg"
   name 'Deezer'
