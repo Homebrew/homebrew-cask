241b5b00d248b669485a5a380cce3bacaf761c7e
Alexander Schneider <alexander.schneider@gmail.com>
2016-07-03 22:05:27 +0200

    updated freefilesync (8.2) (#22559)

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,8 +1,8 @@
 cask 'freefilesync' do
-  version '8.0'
-  sha256 '8d7579f7e1d006417158b73310b82d15bf83daf54526ad63448b406505d26f3c'
+  version '8.2'
+  sha256 '6d89c15ebdb1be2c1d26d1103e549e4c6dc4805a6d897202bb4bb9715a033690'
 
-  url "http://www.freefilesync.org/download/2d658dgdjcnbthd/FreeFileSync_#{version}_Mac_OS_X_64-bit.zip",
+  url "http://www.freefilesync.org/download/FreeFileSync_#{version}_Mac_OS_X.zip",
       referer: 'http://www.freefilesync.org/download.php'
   name 'FreeFileSync'
   homepage 'http://www.freefilesync.org'
