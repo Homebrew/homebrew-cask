c1d4f7baa6f1ed87da04d3911d2d603e34b71ae9
shonjir <shonjir@gmail.com>
2016-07-03 12:37:27 -0700

    Rename wacom-tablet to reflect Intuos Pro specificity and add conflict

--- Casks/wacom-intuos-pro-tablet.rb
+++ Casks/wacom-intuos-pro-tablet.rb
@@ -1,11 +1,13 @@
-cask 'wacom-tablet' do
+cask 'wacom-intuos-pro-tablet' do
   version '6.3.16-12'
   sha256 'd466685d873739a8e714aef5eb5a01f5c2e586bedc308e5b32eda5275af58004'
 
   url "http://cdn.wacom.com/u/productsupport/drivers/mac/professional/WacomTablet_#{version}.dmg"
-  name 'Wacom Tablet'
+  name 'Wacom Intuos Pro Tablet'
   homepage 'http://www.wacom.com/'
   license :gratis
+
+  conflicts_with cask: 'wacom-intuos-tablet'
 
   pkg 'Install Wacom Tablet.pkg'
 
