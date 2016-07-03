294172b343fe6c25ea3c095e22a1249b77afc67d
Victor Popkov <victor@popkov.me>
2016-07-03 23:19:01 +0300

    updated ccmenu (12.0) (#22561)

--- Casks/ccmenu.rb
+++ Casks/ccmenu.rb
@@ -1,10 +1,10 @@
 cask 'ccmenu' do
-  version '11.0'
-  sha256 '5e1a7aff0fc9e4c8520f55f38081979593544f7e29ec1e159ae2be000278a28c'
+  version '12.0'
+  sha256 'b7fde5ece32172e31da448bcf75efeb8858a439e8885a8dcbd1082ffc51b08bd'
 
   url "https://github.com/erikdoe/ccmenu/releases/download/v#{version}/CCMenu.app.zip"
   appcast 'https://github.com/erikdoe/ccmenu/releases.atom',
-          checkpoint: 'b3dee74295d4e8ed3c770726803b45a09731f09bd73164ebebfa723836d663b6'
+          checkpoint: 'c65d96892edc5dc76f09b4a2db610009e6ea1131b73e66cc1b7f4bcd653695b6'
   name 'CCMenu'
   homepage 'http://ccmenu.org/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
