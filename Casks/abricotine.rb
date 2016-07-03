4cf66f2ae58844c8c18bcb72eca28f842d8afd7b
Victor Popkov <victor@popkov.me>
2016-07-03 23:08:01 +0300

    updated abricotine (0.3.3)

--- Casks/abricotine.rb
+++ Casks/abricotine.rb
@@ -1,11 +1,11 @@
 cask 'abricotine' do
-  version '0.3.2'
-  sha256 'c247842d744180d7879e1d9c9f1c066fbd3606dd9fa142835f85206ce99ab57e'
+  version '0.3.3'
+  sha256 '3097ce48260bcacbd5ec9df6a4c77ed6d63f31fda2b7db75e5b8ad04afb95d8b'
 
   # github.com/brrd/Abricotine was verified as official when first introduced to the cask
   url "https://github.com/brrd/Abricotine/releases/download/#{version}/Abricotine-osx-x64.zip"
   appcast 'https://github.com/brrd/Abricotine/releases.atom',
-          checkpoint: '370f31370e2a0c860542b55301517bbde98588bd190722b49d44daa93c910610'
+          checkpoint: '1ea84fd733feb1f4bf458b884fa6c3ec46240ee159f002877cc3c1242882e6b6'
   name 'abricotine'
   homepage 'http://abricotine.brrd.fr'
   license :gpl
