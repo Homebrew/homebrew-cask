8f23d24befe61cea53b6092587a9dcdad8819430
Victor Popkov <victor@popkov.me>
2016-07-03 23:21:20 +0300

    updated qbserve (1.4) (#22563)

--- Casks/qbserve.rb
+++ Casks/qbserve.rb
@@ -1,10 +1,10 @@
 cask 'qbserve' do
-  version '1.3'
-  sha256 'f563e10f984d54f6a5b4454be36c80611101713c6e396d5cefb8d52cacf66bb4'
+  version '1.4'
+  sha256 'dde34f2b0b01004a258ca60dcfdda363a981fcd2937662e217d4aff47c2e83b0'
 
   url "https://qotoqot.com/qbserve/app/Qbserve-#{version}.zip"
   appcast 'https://qotoqot.com/qbserve/app/appcast.xml',
-          checkpoint: 'c14ff12a1abf66f3fec6fe19a86db03ce9e97cbc67bc8976bfe4b1d121145f47'
+          checkpoint: '20b2b68c17f9a93c7ed600d9b2d165bc1e24359ae352fe2035b150a08c8419d5'
   name 'Qbserve'
   homepage 'https://qotoqot.com/qbserve/'
   license :commercial
