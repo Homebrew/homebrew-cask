451a26b91b8ec871e9c8ee6603d06509a52e9496
Markus Reiter <me@reitermark.us>
2016-07-05 00:35:37 +0200

    Fix `url` stanza comment for mkchromecast.

--- Casks/mkchromecast.rb
+++ Casks/mkchromecast.rb
@@ -2,6 +2,7 @@ cask 'mkchromecast' do
   version '0.2.9.1'
   sha256 'aa31a493c83e5ca4c95d1867e63cc685340e4af795a4cb779d97cd0db14ed812'
 
+  # github.com/muammar/mkchromecast was verified as official when first introduced to the cask
   url "https://github.com/muammar/mkchromecast/releases/download/#{version}/mkchromecast_v#{version}.dmg"
   appcast 'https://github.com/muammar/mkchromecast/releases.atom',
           checkpoint: 'e09c8f379bd125ae97f49f074b143e96ca9f6f2c8a877c6d0b94070084fd33df'
