cba095b740580cad8969e259c67c62f7c273e991
Markus Reiter <me@reitermark.us>
2016-07-04 23:48:44 +0200

    Fix `url` stanza comment for LoadMyTracks.

--- Casks/loadmytracks.rb
+++ Casks/loadmytracks.rb
@@ -2,7 +2,7 @@ cask 'loadmytracks' do
   version '1.4.7,099'
   sha256 '5dcc85fd51e89a3792f0aad093bbfbac036e5c0a82a5d758b8352e6905ecfff2'
 
-  # cluetrust.com is the official download host per the product homepage
+  # cluetrust.com was verified as official when first introduced to the cask
   url "http://www.cluetrust.com/Downloads/LoadMyTracks_#{version.after_comma}.dmg"
   appcast 'https://www.cluetrust.com/AppCasts/LoadMyTracks.xml',
           checkpoint: 'bef86b055707d407eb2835db82e07d615968d344450417515cae5719fd97cefd'
