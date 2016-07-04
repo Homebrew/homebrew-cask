4335d5a3a2cb5500d5f464c2cdab3ba6ce9dc0b0
Thomas Willson <thomas.willson@me.com>
2016-07-03 19:06:46 -0700

    updated liclipse (3.0.6)

--- Casks/liclipse.rb
+++ Casks/liclipse.rb
@@ -1,6 +1,6 @@
 cask 'liclipse' do
-  version '2.5.4'
-  sha256 '4056c3282455f84dc6ceab9127df1123539bc63820e054d362e16fd617d297c2'
+  version '3.0.6'
+  sha256 'e5bf65644729267e436ee876cededabd79fedcdadfe43930ea45e72b359c31cf'
 
   # googledrive.com/host/0BwwQN8QrgsRpLVlDeHRNemw3S1E was verified as official when first introduced to the cask
   url "https://googledrive.com/host/0BwwQN8QrgsRpLVlDeHRNemw3S1E/LiClipse%20#{version}/liclipse_#{version}_macosx.cocoa.x86_64.dmg"
