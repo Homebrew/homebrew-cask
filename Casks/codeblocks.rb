cf9d17458703df255308820d7e05a16a4e121ac4
Markus Reiter <me@reitermark.us>
2016-07-05 00:32:57 +0200

    Fix `url` stanza comment for Code::Blocks.

--- Casks/codeblocks.rb
+++ Casks/codeblocks.rb
@@ -2,6 +2,7 @@ cask 'codeblocks' do
   version '13.12'
   sha256 'ef0b541a8897db3132494b899034019739ccee8b8add2a36f018922a82d08b84'
 
+  # sourceforge.net/project/codeblocks was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/codeblocks/Binaries/#{version}/MacOS/CodeBlocks-#{version}-mac.zip"
   name 'Code::Blocks'
   homepage 'http://www.codeblocks.org'
