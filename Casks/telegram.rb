0a0388420500eeed52da94c0d5f4f5400473d3f9
Vítor Galvão <info@vitorgalvao.com>
2016-07-04 18:57:27 +0100

    telegram.rb: remove vendor comment

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -2,7 +2,6 @@ cask 'telegram' do
   version '2.16-47508'
   sha256 '5ffbd2f76054fc47aafa6f2596ef9c6a85a69412a3a1f227f789b1832e7518d5'
 
-  # telegram.org was verified as official when first introduced to the cask
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'http://osx.telegram.org/updates/versions.xml',
           checkpoint: '07caec5ebbe1b7081cb2e873cb5321d6f736b6ecbe7421968ba2f69745ac1a6e'
