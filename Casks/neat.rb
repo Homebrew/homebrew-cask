8d14feabff16e3939c6ef51904aff165f06cd79f
Markus Reiter <me@reitermark.us>
2016-07-04 23:33:44 +0200

    Fix `url` stanza comment for Neat for Mac.

--- Casks/neat.rb
+++ Casks/neat.rb
@@ -2,6 +2,7 @@ cask 'neat' do
   version '4.3.0.36'
   sha256 '4db396a5e9e6dbf1d03670830959fbba5753caeb77d279a60f6f251f376f3f1d'
 
+  # neatco.com was verified as official when first introduced to the cask
   url "http://cdn.neatco.com/Neat-#{version}-Release.dmg"
   name 'Neat for Mac'
   homepage 'https://www.neat.com'
