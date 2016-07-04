0ff18dfbfeff60269f22a7fcf22c0b4e7aef370d
Markus Reiter <me@reitermark.us>
2016-07-05 00:31:37 +0200

    Fix `url` stanza comment for Avidemux.

--- Casks/avidemux.rb
+++ Casks/avidemux.rb
@@ -2,7 +2,7 @@ cask 'avidemux' do
   version '2.6.12'
   sha256 'df95610c560b4bd4df8fbc4285c64161ddf7f987c70a6ef073b4adc5b197b1e5'
 
-  # sourceforge.net/avidemux was verified as official when first introduced to the cask
+  # sourceforge.net/project/avidemux was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/project/avidemux/avidemux/#{version}/Avidemux_#{version}_ML_64Bits_Qt5.dmg"
   name 'Avidemux'
   homepage 'http://www.avidemux.org/'
