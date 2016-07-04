e33c7615b0c9039580f2a2afc3b610586d40a82d
Markus Reiter <me@reitermark.us>
2016-07-05 00:33:28 +0200

    Fix `url` stanza comment for digiKam.

--- Casks/digikam.rb
+++ Casks/digikam.rb
@@ -2,6 +2,7 @@ cask 'digikam' do
   version '4.13.0'
   sha256 'badc0d7f4f3c7092f0722cfcd546cea277e11ac97f948e4e1a23e749f3c400a0'
 
+  # kde.org/stable/digikam was verified as official when first introduced to the cask
   url "http://download.kde.org/stable/digikam/digikam-#{version}-Yosemite.pkg"
   name 'digiKam'
   homepage 'https://www.digikam.org'
