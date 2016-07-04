b2bfa49fdd209e7397903d5050d0a53e3bbc0d86
Vítor Galvão <info@vitorgalvao.com>
2016-07-04 13:41:18 +0100

    updated ryver (1.1.6)

--- Casks/ryver.rb
+++ Casks/ryver.rb
@@ -1,7 +1,8 @@
 cask 'ryver' do
-  version '1.1.3'
-  sha256 '50a67e30aa8069afc869e2e7545e45d255b447ba67f8bc06e32a1185a02a7396'
+  version '1.1.6'
+  sha256 '8176bc4612e52e53bb14cffb50d576762fb556668a8e0d2b251b59c9a3ed7bd0'
 
+  # d3vkb1nw20iqfq.cloudfront.net was verified as official when first introduced to the cask
   url "https://d3vkb1nw20iqfq.cloudfront.net/mac/Ryver-#{version}.dmg"
   name 'Ryver'
   homepage 'http://www.ryver.com'
