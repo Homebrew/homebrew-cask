f80889519f39ceb163162e086cd472ddf014e6ca
Markus Reiter <me@reitermark.us>
2016-07-05 00:31:18 +0200

    Fix `url` stanza comment for BitTorrent.

--- Casks/bittorrent.rb
+++ Casks/bittorrent.rb
@@ -2,6 +2,7 @@ cask 'bittorrent' do
   version :latest
   sha256 :no_check
 
+  # utorrent.com was verified as official when first introduced to the cask
   url 'https://download-new.utorrent.com/os/osx/track/stable/endpoint/btmac'
   name 'BitTorrent'
   homepage 'https://www.bittorrent.com'
