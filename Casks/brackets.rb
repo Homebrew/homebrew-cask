266a6bbaa00bd53f75bc9560588120e49c1363c3
Markus Reiter <me@reitermark.us>
2016-07-05 00:32:26 +0200

    Fix `url` stanza comment for Brackets.

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -2,6 +2,7 @@ cask 'brackets' do
   version '1.7'
   sha256 '1631dd2603f69488171d29776fa6fad09266a6d22b4263b59ef7d1bc39d64ea8'
 
+  # github.com/adobe/brackets was verified as official when first introduced to the cask
   url "https://github.com/adobe/brackets/releases/download/release-#{version}/Brackets.Release.#{version}.dmg"
   appcast 'https://github.com/adobe/brackets/releases.atom',
           checkpoint: 'aab65b5d8b065a452af59622b1e46def37290e1abe05600cd3539a60c6a67c96'
