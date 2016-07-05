ab2ded3b16b20bd949bf3c924081d2382f79fa52
Aditya Dalal <adidalal@users.noreply.github.com>
2016-07-04 22:27:24 -0400

    Fix `url` for rapidweaver
    Fixes edge cases for https://github.com/caskroom/rubocop-cask/pull/26

--- Casks/rapidweaver.rb
+++ Casks/rapidweaver.rb
@@ -3,7 +3,7 @@ cask 'rapidweaver' do
   sha256 '780e2894ad450f7743c3e05a10c79308412098a738a098ef5dea55007122149b'
 
   # devmate.com/com.realmacsoftware.rapidweaver6 was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.realmacsoftware.rapidweaver#{version.major}/RapidWeaver#{version.major}.zip"
+  url "https://dl.devmate.com/com.realmacsoftware.rapidweaver6/RapidWeaver#{version.major}.zip"
   appcast "https://updates.devmate.com/com.realmacsoftware.rapidweaver#{version.major}.xml",
           checkpoint: '5462a78b61a3834b1a5323c3b969acc231dff40cc403b9f5fb9422fffcd7bc81'
   name 'RapidWeaver'
