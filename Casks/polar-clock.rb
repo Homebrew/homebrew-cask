fd3fa998239dc5fc9c63c175373a18041d4a8e51
Ryan Rishi <rdrishi12@gmail.com>
2016-07-06 17:47:12 -0600

    used version in screen_saver declaration (#22645)

--- Casks/polar-clock.rb
+++ Casks/polar-clock.rb
@@ -2,10 +2,10 @@ cask 'polar-clock' do
   version '3.0'
   sha256 '3260e4bfb6ecd2ffe0267d11b65e7698773f0d12b8480dcd8aedafa30762e853'
 
-  url 'http://blog.pixelbreaker.com/downloads/polarclock/PolarClock3_osx_snow_leopard.zip'
+  url "http://blog.pixelbreaker.com/downloads/polarclock/PolarClock#{version.major}_osx_snow_leopard.zip"
   name 'PolarClock Screensaver'
   homepage 'http://blog.pixelbreaker.com/polarclock'
   license :unknown
 
-  screen_saver 'PolarClock 3.0.saver'
+  screen_saver 'PolarClock #{version}.saver'
 end
