10a0a7328492cad92078856055ab97e692dd444c
Victor Popkov <victor@popkov.me>
2016-07-07 02:39:20 +0300

    updated virtualhostx (7.1.8,7_17) (#22641)

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -1,11 +1,11 @@
 cask 'virtualhostx' do
-  version '7.1.7,7_16'
-  sha256 'fac4e42140d642e442a528d634f04fe0025560f1d908db2de12609621cdd1991'
+  version '7.1.8,7_17'
+  sha256 '2a65b4084ab2441086c68d27bde1b379283fe692a3e62ab8c5d2ec873db76bd8'
 
   # downloads-clickonideas.netdna-ssl.com/virtualhostx was verified as official when first introduced to the cask
   url "https://downloads-clickonideas.netdna-ssl.com/virtualhostx/virtualhostx#{version.after_comma}.zip"
   appcast 'https://shine.clickontyler.com/appcast.php?id=33',
-          checkpoint: 'cf50646d61515de31d60fc2ba6b7030f91f5dff586bde77c19f8197bdfa90b33'
+          checkpoint: 'cd403bc5d3f6916355bffb40a544dff83241b66391b0b2bc54f0dac4fa91ee7b'
   name 'VirtualHostX'
   homepage 'https://clickontyler.com/virtualhostx/'
   license :commercial
