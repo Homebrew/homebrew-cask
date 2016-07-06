140c949bda618d2d9d87529d58b2209b917e0d66
Jonathan Harris <Marginal@users.noreply.github.com>
2016-07-06 14:25:00 +0100

    updated qlvideo (1.86) (#22624)

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -1,10 +1,10 @@
 cask 'qlvideo' do
-  version '1.84'
-  sha256 '5f055f2e152e352217350712ed483f11e48a4966f49d30d42591c8cc7342c90a'
+  version '1.86'
+  sha256 '62dc1e1870a3d939fbbd2b75632fbc96cd408a7e99fba75d9ac4b2d66abf857e'
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.delete('.')}/QLVideo_#{version.delete('.')}.pkg"
   appcast 'https://github.com/Marginal/QLVideo/releases.atom',
-          checkpoint: 'c199d34c99e519d672963a4de75f75a7b1a0cc12333f21382e09c4297670737f'
+          checkpoint: '48a93dd1fbc6e44b588225e3e9208d3edf44f37a6cda773f19a87d24e1416e41'
   name 'QuickLook Video'
   homepage 'https://github.com/Marginal/QLVideo'
   license :gpl
