addfb9af66e26d7e21cd3d7019c0622208a33174
Philip Corliss <pcorliss@50projects.com>
2016-07-08 10:52:05 -0500

    real-vnc package locations modified. Removing uneccessary preflight step.

--- Casks/real-vnc.rb
+++ Casks/real-vnc.rb
@@ -2,18 +2,14 @@ cask 'real-vnc' do
   version '5.3.0'
   sha256 '940d1871e587e9135f957ab879ac5fc479721e47b4d8122152e5d2e92c30e231'
 
-  url 'https://www.realvnc.com/download/binary/1775/'
+  url "https://www.realvnc.com/download/file/vnc.files/VNC-#{version}-MacOSX-x86_64.pkg"
   name 'Real VNC'
   homepage 'https://www.realvnc.com'
   license :freemium
 
   container type: :naked
 
-  pkg "VNC-#{version}-MacOSX.pkg"
-
-  preflight do
-    system '/bin/mv', '--', staged_path.join('1775'), staged_path.join("VNC-#{version}-MacOSX.pkg")
-  end
+  pkg "VNC-#{version}-MacOSX-x86_64.pkg"
 
   uninstall script: '/Applications/RealVNC/Advanced.localized/Uninstall VNC Viewer.app/Contents/Resources/uninstaller.sh'
 end
