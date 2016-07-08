af6ecf8dcf5113b16471db7797d28e53403d4b6d
Aditya Dalal <adidalal@users.noreply.github.com>
2016-07-08 17:15:57 -0400

    updated beatunes (4.6.3) (#22672)

--- Casks/beatunes.rb
+++ Casks/beatunes.rb
@@ -1,6 +1,6 @@
 cask 'beatunes' do
-  version '4.6.1'
-  sha256 '2e0563a976e30af50b219bff0b522b2506e55380259fced6451e514f68bc1061'
+  version '4.6.3'
+  sha256 '55e67e197bc47ea46f5f42c52600621716ae2854221013c77fa3cdab653b325a'
 
   url "http://coxy.beatunes.com/download/beaTunes-#{version.dots_to_hyphens}.dmg"
   name 'beaTunes'
