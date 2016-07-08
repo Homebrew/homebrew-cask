22f7ab3c8818d1bb9acf77d46b66b1f8262621b5
李春奕 <373852389@qq.com>
2016-07-08 13:49:12 +0800

    update QQ -> 5.0.1

--- Casks/qq.rb
+++ Casks/qq.rb
@@ -1,6 +1,6 @@
 cask 'qq' do
-  version '5.0.0'
-  sha256 '520c3250ee330235949fffbd0ebb34338195ff9fa14d57e7e35b6d529faca239'
+  version '5.0.1'
+  sha256 'dc942a782a8f2ef49b8fbd83e46af63b495af3dc06ece1594181932b4f030fa0'
 
   url "http://dldir1.qq.com/qqfile/QQforMac/QQ_V#{version}.dmg"
   name 'QQ'
