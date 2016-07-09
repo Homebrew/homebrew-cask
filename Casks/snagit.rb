572bb52372686e97e7d449928688e0670f823982
My2ndAngelic <My2ndAngelic@users.noreply.github.com>
2016-07-09 20:58:40 +0200

    Update snagit.rb

--- Casks/snagit.rb
+++ Casks/snagit.rb
@@ -1,27 +1,13 @@
 cask 'snagit' do
-#Mac OS version check
-=begin
-  if MacOS.release <= :lion #for 10.6 and 10.7 (10.6 and before cannot run brew)
-    version '2.1.7'
+  if MacOS.release == :mavericks
+    version '3.3.7'
     sha256 :no_check
-    url "http://download.techsmith.com/snagitmac/enu/217/snagit.dmg"
+    url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
   else
-=end
-
-cask 'appcleaner' do
-  if MacOS.release <= :lion
-    version '3.2.3'
-    url "http://download.techsmith.com/snagitmac/enu/323/snagit.dmg"
-  else
-    if MacOS.release <= :mavericks
-      version '3.3.7'
-      sha256 '69da212e2972e23e361c93049e4b4505d7f226aff8652192125f078be7eecf7f'
-      url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
-    else
-      version :latest
-      sha256 :no_check
-      url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
-  end
+    version :latest
+    sha256 :no_check
+    url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
+end
 
     name 'Snagit'
     homepage 'https://www.techsmith.com/snagit.html'
