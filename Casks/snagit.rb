43c9d96f69bef7b440c481fa5ccc8c70032821ce
My2ndAngelic <My2ndAngelic@users.noreply.github.com>
2016-07-10 01:15:12 +0200

    Update snagit.rb

--- Casks/snagit.rb
+++ Casks/snagit.rb
@@ -2,27 +2,27 @@ cask 'snagit' do
   if MacOS.release == :leopard
     version '1.0.4'
     sha256 :no_check
-    url 'http://download.techsmith.com/snagitmac/enu/104/snagit.dmg'
+    url 'http://download.techsmith.com/snagitmac/enu/#{version.no_dots}/snagit.dmg'
   elsif MacOS.release == :snow_leopard
     version '2.1.7'
     sha256 :no_check
-    url 'http://download.techsmith.com/snagitmac/enu/217/snagit.dmg'
+    url 'http://download.techsmith.com/snagitmac/enu/#{version.no_dots}/snagit.dmg'
   elsif MacOS.release == :lion
     version '2.1.7'
     sha256 :no_check
-    url 'http://download.techsmith.com/snagitmac/enu/217/snagit.dmg'
+    url 'http://download.techsmith.com/snagitmac/enu/#{version.no_dots}/snagit.dmg'
   elsif MacOS.release == :mountain_lion
     version '3.2.3'
     sha256 :no_check
-    url 'http://download.techsmith.com/snagitmac/enu/323/snagit.dmg'
+    url 'http://download.techsmith.com/snagitmac/enu/#{version.no_dots}/snagit.dmg'
   elsif MacOS.release == :mavericks
     version '3.3.7'
     sha256 :no_check
-    url 'http://download.techsmith.com/snagitmac/enu/337/snagit.dmg'
+    url 'http://download.techsmith.com/snagitmac/enu/#{version.no_dots}/snagit.dmg'
   elsif MacOS.release == :yosemite
     version '4.0.3'
     sha256 :no_check
-    url 'http://download.techsmith.com/snagitmac/enu/403/snagit.dmg'
+    url 'http://download.techsmith.com/snagitmac/enu/#{version.no_dots}/snagit.dmg'
   else
     version :latest
     sha256 :no_check
