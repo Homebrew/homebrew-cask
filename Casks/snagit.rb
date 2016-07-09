b1bd5ba2444ff43934c9e45da3961c8c1ddf0b5f
My2ndAngelic <My2ndAngelic@users.noreply.github.com>
2016-07-09 20:48:24 +0200

    Update snagit.rb

--- Casks/snagit.rb
+++ Casks/snagit.rb
@@ -10,12 +10,12 @@ cask 'snagit' do
     if MacOS.release <= :mountain_lion #for 10.8
       version '3.2.3'
       sha256 :no_check
-      url "http://download.techsmith.com/snagitmac/enu/323/snagit.dmg"
+      url 'http://download.techsmith.com/snagitmac/enu/323/snagit.dmg'
     else
       if MacOS.release <= :mavericks #for 10.9
         version '3.3.7'
         sha256 :no_check
-        url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
+        url 'http://download.techsmith.com/snagitmac/enu/337/snagit.dmg'
       else #for 10.10 and above (soon, 10.10 will be removed)
         version :latest
         sha256 :no_check
