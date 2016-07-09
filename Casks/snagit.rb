9c97231ae1ef7b95aee33b2680245427f36cda8e
My2ndAngelic <My2ndAngelic@users.noreply.github.com>
2016-07-09 21:15:29 +0200

    Update snagit.rb

--- Casks/snagit.rb
+++ Casks/snagit.rb
@@ -3,25 +3,22 @@ cask 'snagit' do
     version '2.1.7'
     sha256 :no_check
     url "http://download.techsmith.com/snagitmac/enu/217/snagit.dmg"
-  else
-    if MacOS.release == lion
+  elsif MacOS.release == lion
       version '2.1.7'
       sha256 :no_check
       url "http://download.techsmith.com/snagitmac/enu/217/snagit.dmg"
-    else
-      if MacOS.release == mountain_lion
-        version '3.2.3'
-        sha256 :no_check
-        url "http://download.techsmith.com/snagitmac/enu/323/snagit.dmg"
-      else
-        if MacOS.release == :mavericks
-          version '3.3.7'
-          sha256 :no_check
-          url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
-        else
-          version :latest
-          sha256 :no_check
-          url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
+  elsif MacOS.release == mountain_lion
+    version '3.2.3'
+    sha256 :no_check
+    url "http://download.techsmith.com/snagitmac/enu/323/snagit.dmg"
+  elsif MacOS.release == :mavericks
+    version '3.3.7'
+    sha256 :no_check
+    url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
+  else
+    version :latest
+    sha256 :no_check
+    url "http://download.techsmith.com/snagitmac/enu/337/snagit.dmg"
   end
 
     name 'Snagit'
