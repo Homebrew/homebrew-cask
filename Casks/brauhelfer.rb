218069f4986dec223b7e568c05c78abee6470646
Alexander Seith <alexander.seith@gmail.com>
2016-07-10 14:53:13 +0200

    Use version variable for url in brauhelfer.app cask

--- Casks/brauhelfer.rb
+++ Casks/brauhelfer.rb
@@ -2,7 +2,7 @@ cask 'brauhelfer' do
   version '1.4.2.2'
   sha256 '3c2173b7fc241a278b000b3e51ef4145408dfa6fdaf1db09374f9f15c94a996a'
 
-  url 'http://www.joerum.de/kleiner-brauhelfer/lib/exe/fetch.php?media=download:01_04_02_02:kb_osx_lion_v1.4.2.2.dmg'
+  url "http://www.joerum.de/kleiner-brauhelfer/lib/exe/fetch.php?media=download:01_04_02_02:kb_osx_lion_v#{version}.dmg"
   name 'Kleiner Brauhelfer'
   homepage 'http://www.joerum.de/kleiner-brauhelfer'
   license :gpl
