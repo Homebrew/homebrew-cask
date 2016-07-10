e9dc7aff0d5e63431b70a2c857fd8e5e08b782ae
Rbcafe <Webmaster@rbcafe.com>
2016-07-10 19:14:57 +0200

    Update outguess.rb

--- Casks/outguess.rb
+++ Casks/outguess.rb
@@ -1,6 +1,6 @@
 cask 'outguess' do
-  version '1.1.1'
-  sha256 '13255302a624cec644ea402cff3507f69588755b5ea99ee178d428b1a3c500df'
+  version 'latest'
+  sha256 'no_check'
 
   url 'http://www.rbcafe.com/download/outguess.zip'
   name 'Outguess'
