4b5defa6dfa683bcbca9f81c83c1690a27eaf945
dejavuzhang <dejavuzhang@gmail.com>
2016-07-10 15:49:07 -0400

    Update diumoo.rb

--- Casks/diumoo.rb
+++ Casks/diumoo.rb
@@ -2,6 +2,7 @@ cask 'diumoo' do
   version '1.5.0'
   sha256 'f6a6628dab6c64752f6827e453368c18dcd06962ee0823d51d51c439cf7e04b6'
 
+  # github.com/shanzi/diumoo was verified as official when first introduced to the cask
   url 'https://github.com/shanzi/diumoo/releases/download/v#{version}/diumoo.zip'
   name 'diumoo'
   homepage 'http://diumoo.net/'
