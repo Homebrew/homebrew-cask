a4501ed998631c0ead393e0ecadb2b3d81fb74d6
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-07-11 14:49:28 -0400

    updated polymail (0.93) (#22729)

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,10 +1,10 @@
 cask 'polymail' do
-  version '0.92'
-  sha256 '257aeebbddff10b1048f4c717aa7f49f4b7d658eb8cf26a4bab60bda7b55b82a'
+  version '0.93'
+  sha256 'bbe564c6bf1bba85a609eda557e336b9c97ca9051a7c6c8a81e3619cf8d2d0db'
 
   url "https://sparkle-updater.polymail.io/osx/builds/Polymail-v#{version.no_dots}.zip"
   appcast 'https://sparkle-updater.polymail.io/cast.xml',
-          checkpoint: 'b34f147a02724f6ed1f01a14a0f7ea3dc009341e78f405e81854006c82a84a37'
+          checkpoint: '1548a373fb11748e6c02108ba5c925b874912fd9687cdfdb9c8789af0f51d151'
   name 'Polymail'
   homepage 'https://polymail.io/'
   license :closed
