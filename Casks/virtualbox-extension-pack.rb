f3c7e4fb6edd4a98dba482315e56c96ccc7774f0
Steffen Hansen <sgnh@users.noreply.github.com>
2016-07-13 00:45:36 +0200

    updated virtualbox-extension-pack (5.1.0-108711)

--- Casks/virtualbox-extension-pack.rb
+++ Casks/virtualbox-extension-pack.rb
@@ -1,6 +1,6 @@
 cask 'virtualbox-extension-pack' do
-  version '5.0.24-108355'
-  sha256 'aeee163c3d1bea8d3de9647d2ce6771fbf0e873bd20de6b39f84ef9d2de4b0c9'
+  version '5.1.0-108711'
+  sha256 '7e9802dbbdf56dcd963f8ef368fe1f9ff9accbd74d9fb297b44af3d4af757ec7'
 
   url "http://download.virtualbox.org/virtualbox/#{version.sub(%r{-.*}, '')}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
   name 'Oracle VirtualBox Extension Pack'
