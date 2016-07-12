141ba87437632eb5dc36a6a9a3bd07a71f094c93
mathbunnyru <mathbunnyru@users.noreply.github.com>
2016-07-12 14:23:58 +0400

    Update pepper-flash.rb
    New 22.0.0.209 version

--- Casks/pepper-flash.rb
+++ Casks/pepper-flash.rb
@@ -1,6 +1,6 @@
 cask 'pepper-flash' do
-  version '22.0.0.192'
-  sha256 '63858fde11525e0f2de2120da449b8576576057604374ad32c5c9da40af3f27c'
+  version '22.0.0.209'
+  sha256 'c208880e734a11bc00cf4eb973a5ced394ab545508d476f7136ea208e50b7bcb'
 
   url "https://admdownload.adobe.com/bin/live/AdobeFlashPlayer_#{version.to_i}ppau_a_install.dmg"
   name 'Adobe Pepper Flash Player'
