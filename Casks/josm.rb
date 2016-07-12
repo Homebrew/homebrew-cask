9a181bcabd0620da939539c4e79b81271dd60a4e
Andrey Golovin <Andygol@users.noreply.github.com>
2016-07-12 23:07:35 +0300

    fixed sha checksum

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask 'josm' do
   version '10526'
-  sha256 '33d1705634c554327bde3d3689723cc950ad0c42e8c47914bae92e3efce460e2'
+  sha256 'b6e1eb86af6ea953af8d50770aeea3cedfe8ef47cbea7256ff9e715a1729cb81'
 
   url "https://josm.openstreetmap.de/download/macosx/josm-macosx-#{version}.zip"
   name 'JOSM'
