6a0ef8ce36772748ed13251bc04ac0ed1aafca02
Caius Durling <dev@caius.name>
2016-07-12 17:38:34 +0100

    Update Marp to state download URL is official

--- Casks/marp.rb
+++ Casks/marp.rb
@@ -2,6 +2,7 @@ cask 'marp' do
   version '0.0.8'
   sha256 '6db7310232bf9abb4629f5a1600ebb9b912ffd750aa44eeaaa7c7ebdf5f563c6'
 
+  # github.com was verified as official when first introduced to the cask
   url 'https://github.com/yhatt/marp/releases/download/v0.0.8/0.0.8-Marp-darwin-x64.dmg'
   name 'Marp'
   homepage 'https://yhatt.github.io/marp/'
