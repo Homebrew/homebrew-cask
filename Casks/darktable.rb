beac61f7c1cb1f2218e53bd2bcda272e225fdf65
Amit Kotlovski <amitbk@gmail.com>
2016-07-13 16:10:29 +0300

    updated darktable (2.0.5)

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,11 +1,11 @@
 cask 'darktable' do
-  version '2.0.4'
-  sha256 '1e6306f623c3743fabe88312d34376feae94480eb5a38858f21751da04ac4550'
+  version '2.0.5'
+  sha256 'e0ae0e5e19771810a80d6851e022ad5e51fb7da75dcbb98d96ab5120b38955fd'
 
   # github.com/darktable-org/darktable was verified as official when first introduced to the cask
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version}/darktable-#{version}.dmg"
   appcast 'https://github.com/darktable-org/darktable/releases.atom',
-          checkpoint: '7983f9d630d521bc530069d06be63b971685d9ed81b181d70bba95c788cdd978'
+          checkpoint: '097513cfe481beb354fc63002a3255646cbf5c5b2e23b9f6e60d58e9a7c5b45d'
   name 'darktable'
   homepage 'https://www.darktable.org/'
   license :gpl
