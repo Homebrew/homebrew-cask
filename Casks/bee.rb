541c697e245915ee7867cc8cee056c55922b7a19
Victor Popkov <victor@popkov.me>
2016-07-14 01:16:43 +0300

    updated bee (2.3.5) (#22784)

--- Casks/bee.rb
+++ Casks/bee.rb
@@ -1,11 +1,11 @@
 cask 'bee' do
-  version '2.3.4'
-  sha256 '674ca80554b5d706598643fb1ea40ce36f7f3515fe426472d938889abc80e8eb'
+  version '2.3.5'
+  sha256 'b2b0289b009a9da5db5350eed89ef63f9ab0000a4112bfb0084140160eb3a771'
 
   # bee-app.s3.amazonaws.com was verified as official when first introduced to the cask
   url "http://bee-app.s3.amazonaws.com/public/Bee-#{version}.zip"
   appcast 'http://www.neat.io/appcasts/bee-appcast.xml',
-          checkpoint: 'e4f0b8fd6492cd7c346721895b845c947c5cc132afb3d90591cbd6169b3ae453'
+          checkpoint: '0035153546057fb21a72ee030271f765eb2b25964ed2637c1ecc0f1db00b4738'
   name 'Bee'
   homepage 'http://neat.io/bee/'
   license :commercial
