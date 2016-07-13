1b42aa69f063c9e5dbd93436feabf94a9e113a60
Premkumar Subramanian <prem_x87@outlook.com>
2016-07-13 13:47:49 +0530

    Updated intellij-idea 2016.2

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea' do
-  version '2016.1.3'
-  sha256 '6da532f34b77a285f91756740cd548ab985bd5350f15e8de87c5bdfb6cc590dd'
+  version '2016.2'
+  sha256 'ebdcb34b8d3bddc4dfedd71dd24f5273f63941eed480e6063acf910f1a10b593'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   name 'IntelliJ IDEA'
