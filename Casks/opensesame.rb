5d3038357acc9663fdcc00c65178b06d8144d760
Matthieu PERREIRA DA SILVA <matthieu@perreira.net>
2016-07-13 16:00:59 +0200

    Update opensesame.rb

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -4,9 +4,9 @@ cask 'opensesame' do
     sha256 'b2a37cfd1c514b2ae8ddd0be09a274844420bfa432318ef87df308fdd3b6a770'
     url "http://files.cogsci.nl/software/opensesame/opensesame_#{version}-macos-2.zip"
   else
-    version '0.27.4'
-    sha256 '8814da8fe5e638cb7db18b4e8188fc97028bd98f1603ceae006aff13745fc739'
-    url "http://www.cogsci.nl/dschreij/opensesame-mac/opensesame-#{version}-macos-x86_64-1.dmg"
+    version '3.1.0'
+    sha256 ''
+    url "https://github.com/smathot/OpenSesame/releases/download/release%2F#{version}/opensesame_#{version}-py2.7-macos-2.dmg"
   end
 
   name 'OpenSesame'
