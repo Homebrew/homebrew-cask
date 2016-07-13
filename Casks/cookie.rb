4d41e95976d38b5c85515b2dcb5e66a0dd2ae226
Victor Popkov <victor@popkov.me>
2016-07-13 23:16:11 +0300

    updated cookie (5.0.10)

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.0.9'
-  sha256 '638dfb131e7d662c8e1846e90588314dc63e4cd435e52c0d060930845cd70059'
+  version '5.0.10'
+  sha256 '9f50869effeeb23b99cff8e187f138ab4d1c0d29b0672833316c53a775ccf734'
 
   url 'https://sweetpproductions.com/products/cookie5/Cookie5.zip'
   appcast 'https://sweetpproductions.com/products/cookie5/appcast.xml',
-          checkpoint: '68f0054f8a9a5c23ddfe506f86d2f30f63962b5b417f29b5dd0ed430d7d8b0a5'
+          checkpoint: 'faaa97a800da4e62f559fdbdeb9851f2dc985e3453ed5fbee27b2c3c0cd7c845'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
   license :commercial
