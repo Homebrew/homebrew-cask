6b28e550d7280cca804890d46aa6dd987be4357c
Dmitry Labutin <dmitry@labutin.net>
2016-07-14 15:33:50 +0300

    Fixed sha256

--- Casks/mongodb-compass.rb
+++ Casks/mongodb-compass.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass' do
   version '1.2.0'
-  sha256 '58fae4d0aae13ce1bd9a39610773d877b3163baa151cb6536bcb4b2005cc929d'
+  sha256 '64eedf4595b89413590a943dc16088fea8423f72fb98f6cf00f32261a129a1c3'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-#{version}-darwin-x64.dmg"
   name 'MongoDB Compass'
