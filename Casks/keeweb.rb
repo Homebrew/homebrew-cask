f49e11f4bd16624dad4d90daf66384a69f96ab91
Eric <eric@amalgamar.com.br>
2016-07-13 21:21:42 -0500

    Upgrade KeeWeb.app to v1.2.1

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,11 +1,11 @@
 cask 'keeweb' do
-  version '1.1.4'
-  sha256 '9d383d2d962e774c89b7981ab0a53f22921ad3c0558abd4136cd7b1f020cde92'
+  version '1.2.1'
+  sha256 'a92974c54477f98782c52562593b10e78672d12f0a18e6140fe880afbc5bbde7'
 
   # github.com/keeweb/keeweb was verified as official when first introduced to the cask
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb.mac.dmg"
   appcast 'https://github.com/keeweb/keeweb/releases.atom',
-          checkpoint: 'ab8b788f35d9afce9b5d3d44b58c38d679a96a8497e23e8a2fb4ea9b137ddac4'
+          checkpoint: '6c3848370e941f0180a41cd3d8e66da69c2d2c601b2f3661c9a3537deede98b6'
   name 'KeeWeb'
   homepage 'https://keeweb.info'
   license :mit
