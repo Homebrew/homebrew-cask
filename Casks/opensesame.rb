0bbfdcbd53ff7724d658d572e5df02c92531b74b
Matthieu PERREIRA DA SILVA <matthieu@perreira.net>
2016-07-14 15:07:45 +0200

    Update opensesame.rb

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -6,6 +6,7 @@ cask 'opensesame' do
   else
     version '3.1.0'
     sha256 '7224d05e72a91d1514c64f1c1d1e417ac28ead8cf62ae06c3d1e9486448302bf'
+    # github.com/smathot/OpenSesame was verified as official when first introduced to the cask
     url "https://github.com/smathot/OpenSesame/releases/download/release/#{version}/opensesame_#{version}-py2.7-macos-2.dmg"
     appcast 'https://github.com/smathot/OpenSesame/releases.atom',
           checkpoint: '976508aa8adac9f95d190cdea3dfc78745fe68834d9d8da36eab694e884b49df'
