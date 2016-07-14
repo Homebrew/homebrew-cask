4e850ae6cf843c917789be28ce6e0581f32b5f01
Sven <Sven@Svens-MacBook-Pro.local>
2016-07-14 10:42:56 +0200

    Removed varify comment

--- Casks/devrantio.rb
+++ Casks/devrantio.rb
@@ -2,7 +2,6 @@ cask 'devrantio' do
     version '1.0.2'
     sha256 '440a39b8e16cde4e8fc55eb387526bc74c5176dcad1c80820a1582b350ac870e'
     
-    # github.com was verified as official when first introduced to the cask
     url "https://github.com/Meadowcottage/devRant/releases/download/#{version}/devRant.io-darwin-x64.zip"
     appcast 'https://github.com/Meadowcottage/devRant/releases.atom',
     checkpoint: '1292b2981cd6f3e90fc0a5b0defaffc373113dda3455c0b8b02e6aceaeaff0a1'
