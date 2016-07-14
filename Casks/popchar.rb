881dc0d02525ce5e8acf031d17b3c4875f0b5dfa
Markus Reiter <me@reitermark.us>
2016-07-14 02:23:51 +0200

    Update PopChar X to 7.5 (#22789)

--- Casks/popchar.rb
+++ Casks/popchar.rb
@@ -1,8 +1,8 @@
 cask 'popchar' do
-  version '7.3.1'
-  sha256 '1f7d06897b165613f564e34409ff42e20e383f831b45c86d6e4bd71bc1c1b670'
+  version '7.5'
+  sha256 'f991f2be60e1409209cc1921c1a2c2a7f2e6281a7275e52c6e1bbb8597a5c83a'
 
-  url "http://www.ergonis.com/downloads/products/popcharx/PopCharX#{version.gsub(%r{\.}, '')}-Install.dmg"
+  url "http://www.ergonis.com/downloads/products/popcharx/PopCharX#{version.no_dots}-Install.dmg"
   name 'PopChar X'
   homepage 'http://www.ergonis.com/products/popcharx/'
   license :commercial
