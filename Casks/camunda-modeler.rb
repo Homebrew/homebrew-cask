b19fe7aad8e7f45bf0dc1354a04fccb9f85d8cc8
Jan Galinski <jan.galinski@holisticon.de>
2016-07-15 15:12:51 +0200

    fix checksum

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask 'camunda-modeler' do
   version '1.2.0'
-  sha256 f4d8e6a8738b167cb952476720ae42082c310fe6828798b265fcd5f699259c5c
+  sha256 'f4d8e6a8738b167cb952476720ae42082c310fe6828798b265fcd5f699259c5c'
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-darwin-x64.tar.gz"
   name 'Camunda Modeler'
