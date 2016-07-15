9abda6d865da74977b67fe3b2eb60791f37d31de
Vítor Galvão <info@vitorgalvao.com>
2016-07-15 19:00:34 +0100

    updated keeweb (1.2.2)

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,11 +1,11 @@
 cask 'keeweb' do
-  version '1.2.1'
-  sha256 'a92974c54477f98782c52562593b10e78672d12f0a18e6140fe880afbc5bbde7'
+  version '1.2.2'
+  sha256 '8b4a629a9be8781864302f2bf204f96449383eaed16bd5b6930369057d3fa2b1'
 
   # github.com/keeweb/keeweb was verified as official when first introduced to the cask
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb.mac.dmg"
   appcast 'https://github.com/keeweb/keeweb/releases.atom',
-          checkpoint: '6c3848370e941f0180a41cd3d8e66da69c2d2c601b2f3661c9a3537deede98b6'
+          checkpoint: '552205c4775461633d4ce431b3593b2a1f9b52cf35733a512964acd24bfb1c80'
   name 'KeeWeb'
   homepage 'https://keeweb.info'
   license :mit
