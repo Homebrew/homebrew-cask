f367ccad902e0bbf2faa42dd41237664b698eb9a
Victor Popkov <victor@popkov.me>
2016-07-15 21:49:17 +0300

    updated betterzip (3.1.2)

--- Casks/betterzip.rb
+++ Casks/betterzip.rb
@@ -1,10 +1,10 @@
 cask 'betterzip' do
-  version '3.1.1'
-  sha256 '0406e9849c048b744924444c2419fcc1bcfde529c3fce9344d72b875a484586b'
+  version '3.1.2'
+  sha256 'eaf686e95876fd2b064cc1a6aba984c7b40b74710c5894c28130ed53a428f049'
 
   url "http://macitbetter.com/dl/BetterZip-#{version}.zip"
   appcast 'http://macitbetter.com/BetterZip3.rss',
-          checkpoint: '92ccd21015545a5e42d93643ec5e7d394a10bc4caafeaffcfdfe6fc4943107b3'
+          checkpoint: 'cc8f405304c66c96a64702a7806bef9e5c5a3f0b393560c95130e1730a38d79f'
   name 'BetterZip'
   homepage 'http://macitbetter.com'
   license :commercial
