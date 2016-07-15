da5f88621020dd6a09b15453b43d3fdfa807adec
Vítor Galvão <info@vitorgalvao.com>
2016-07-15 12:01:26 +0100

    soundtoys.rb: fixed vendor comment / interpolation

--- Casks/soundtoys.rb
+++ Casks/soundtoys.rb
@@ -2,8 +2,8 @@ cask 'soundtoys' do
   version '5.0.4.11752'
   sha256 'da8bc65685bbc8cdbe45956cc668a4c8a2ccac0a16c11aba073ee215cc9a40a2'
 
-  # s3.amazonaws.com/soundtoys was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/soundtoys#{version.major}/#{version.sub(%r{.*\.}, '')}/Soundtoys#{version.major}_#{version}.dmg"
+  # s3.amazonaws.com/soundtoys5 was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/soundtoys5/#{version.sub(%r{.*\.}, '')}/Soundtoys#{version.major}_#{version}.dmg"
   name 'Soundtoys Bundle'
   homepage "http://www.soundtoys.com/product/soundtoys-#{version.major}/"
   license :gratis
