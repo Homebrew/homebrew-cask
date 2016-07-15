2fffff2d8bdf26272fd8e3062287c8b4b1323c89
Vítor Galvão <info@vitorgalvao.com>
2016-07-15 12:02:47 +0100

    soundtoys: clearer interpolation

--- Casks/soundtoys.rb
+++ Casks/soundtoys.rb
@@ -1,9 +1,9 @@
 cask 'soundtoys' do
-  version '5.0.4.11752'
+  version '5.0.4,11752'
   sha256 'da8bc65685bbc8cdbe45956cc668a4c8a2ccac0a16c11aba073ee215cc9a40a2'
 
   # s3.amazonaws.com/soundtoys5 was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/soundtoys5/#{version.sub(%r{.*\.}, '')}/Soundtoys#{version.major}_#{version}.dmg"
+  url "https://s3.amazonaws.com/soundtoys5/#{version.after_comma}/Soundtoys#{version.major}_#{version.before_comma}.#{version.after_comma}.dmg"
   name 'Soundtoys Bundle'
   homepage "http://www.soundtoys.com/product/soundtoys-#{version.major}/"
   license :gratis
