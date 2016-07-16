82cc6bacb702116c0cdd682353f3e288db86cfda
Michael Ford <fanquake@users.noreply.github.com>
2016-07-16 19:42:12 +0800

    dbeaver enterprise 3.7.1

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-enterprise' do
-  version '3.6.10'
-  sha256 '5c19c5f38a7dd8575a6fc3a4d97c2df80e305338057e2e870d2527c3a46ebe40'
+  version '3.7.1'
+  sha256 'd3f5022d223ca212341cba2b4513b5089498c177ece38ac4ac85da8ffc1ec122'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   name 'DBeaver Enterprise Edition'
