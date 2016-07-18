d6be46d7402a6274824c5411719b6a68fe6d2861
Jeffrey Cafferata <jeffrey@jcid.nl>
2016-07-18 20:02:49 +0200

    Updated Pashua homepage.

--- Casks/pashua.rb
+++ Casks/pashua.rb
@@ -4,7 +4,7 @@ cask 'pashua' do
 
   url 'https://www.bluem.net/files/Pashua.dmg'
   name 'Pashua'
-  homepage 'https://www.bluem.net/en/mac/pashua/'
+  homepage 'http://www.bluem.net/en/mac/pashua/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Pashua.app'
