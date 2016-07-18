05be6562601770de9b87536a179661ac4877bcca
Lucian Fratila <lucianf@gmail.com>
2016-07-18 17:33:44 +0300

    X-Lite 4.9.4 (#22862)

--- Casks/x-lite.rb
+++ Casks/x-lite.rb
@@ -1,6 +1,6 @@
 cask 'x-lite' do
-  version '4.9.2_79049'
-  sha256 'd0fedd71c37d7a6a12b7d63bbb9b2495f58ec45270e31cb5ea60bdf8947b7bc6'
+  version '4.9.4_80103'
+  sha256 '86f00a98b8b842b65ec01c9e2cc1eb41903e187616b5f98100a9d58f313c8f46'
 
   # counterpath.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://counterpath.s3.amazonaws.com/downloads/X-Lite_#{version}.dmg"
