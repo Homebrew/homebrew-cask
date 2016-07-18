c1ca700f1641db100ece6adebe874826eaae7a26
Victor Popkov <victor@popkov.me>
2016-07-18 18:40:04 +0300

    updated appdelete (4.2.7)

--- Casks/appdelete.rb
+++ Casks/appdelete.rb
@@ -1,10 +1,10 @@
 cask 'appdelete' do
-  version '4.2.6'
-  sha256 'c7d74df75671b76109accc720f0e3b58c505854a6c33a3d6fd18833753e8f239'
+  version '4.2.7'
+  sha256 'b72e95d804446f33fa297a088edb6f65b6b72c9c2c4d3c6718004d04f331e612'
 
   url 'http://www.reggieashworth.com/downloads/AppDelete.dmg'
   appcast 'http://www.reggieashworth.com/AD4Appcast.xml',
-          checkpoint: 'f71c04fce74afd1a123fefbc70f88bd5d05ec4b5f2ab37b908519b1317ea603f'
+          checkpoint: '52e80243071ab32d1cd4809b750116cfedc41e07925c3958c71bc12576520418'
   name 'AppDelete'
   homepage 'http://www.reggieashworth.com/appdelete'
   license :commercial
