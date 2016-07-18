4a4dfdb569e637c6444554a9b5321e24ef8967e9
Jeffrey Cafferata <jeffrey@jcid.nl>
2016-07-18 20:17:30 +0200

    Updated Valentina Studio homepage.

--- Casks/valentina-studio.rb
+++ Casks/valentina-studio.rb
@@ -2,9 +2,9 @@ cask 'valentina-studio' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.valentina-db.com/en/all-downloads/vstudio/current/vstudio_mac_64?format=raw'
+  url 'https://www.valentina-db.net/en/all-downloads/vstudio/current/vstudio_mac_64?format=raw'
   name 'Valentina Studio'
-  homepage 'https://www.valentina-db.com/en/valentina-studio-overview'
+  homepage 'https://www.valentina-db.net/en/valentina-studio-overview'
   license :freemium
 
   app 'Valentina Studio.app'
