61996491417d2f26df7d6ee988570225ee62bc6e
Victor Popkov <victor@popkov.me>
2016-07-18 19:19:30 +0300

    updated timings (2.4.7) (#22876)

--- Casks/timings.rb
+++ Casks/timings.rb
@@ -1,10 +1,10 @@
 cask 'timings' do
-  version '2.4.6'
-  sha256 '71e6d52238da8606005ac47299c40be6138e4de0f1374cfe76ea3ac3ee5ca6c1'
+  version '2.4.7'
+  sha256 '5c014e6e2b2ed85641bd4f4bef37b9267e41b17ac93171cfb8d56be46e005774'
 
   url "https://mediaatelier.com/Timings2/Timings_#{version}.zip"
   appcast 'https://mediaatelier.com/Timings2/feed.php',
-          checkpoint: '252f5a8a3972ea6b0e6c494bf310df48898b3404b9506ac2e518775bb452f7cb'
+          checkpoint: 'd9a2c34883a1e0247824d454a8a539ef8be8c9b03494e642368e7ed06c048e6b'
   name 'Timings'
   homepage 'http://mediaatelier.com/Timings'
   license :commercial
