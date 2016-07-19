be93d04a3050a53279ff16d022073ad6b6383688
Joshua Hagins <jhagins@palantir.com>
2016-07-19 14:20:36 -0400

    updated adobe-reader (15.017.20050)

--- Casks/adobe-reader.rb
+++ Casks/adobe-reader.rb
@@ -1,15 +1,15 @@
 cask 'adobe-reader' do
-  version '2015.017.20050'
+  version '15.017.20050'
   sha256 '38509de0ceda1b51d44f247098d2933455e6c5a34cc977132ae9264dca0ebecf'
 
-  url "http://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots[2..-1]}/AcroRdrDC_#{version.no_dots[2..-1]}_MUI.dmg"
+  url "http://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots}/AcroRdrDC_#{version.no_dots}_MUI.dmg"
   name 'Adobe Acrobat Reader DC'
   homepage 'https://www.adobe.com/products/reader.html'
   license :gratis
 
   depends_on macos: '>= 10.9'
 
-  pkg "AcroRdrDC_#{version.no_dots[2..-1]}_MUI.pkg"
+  pkg "AcroRdrDC_#{version.no_dots}_MUI.pkg"
 
   uninstall pkgutil: 'com.adobe.acrobat.DC.reader.*',
             delete:  '/Applications/Adobe Acrobat Reader DC.app'
