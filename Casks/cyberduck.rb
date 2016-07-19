56b69f19a59735b3ae4c0ecd711b487cbd6f70fa
Victor Popkov <victor@popkov.me>
2016-07-19 17:49:12 +0300

    updated cyberduck (5.0.9.20675) (#22934)

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -1,10 +1,10 @@
 cask 'cyberduck' do
-  version '5.0.3.20504'
-  sha256 'ce1f20d96d6112f2ba1c5c5144ee07109a6ffd7da6124a66b88a1e06e63bfc32'
+  version '5.0.9.20675'
+  sha256 '003c26cff08f9a7709bebb33bfa41371d83987334c04681d89aa1f259f1b9c17'
 
   url "https://update.cyberduck.io/Cyberduck-#{version}.zip"
   appcast 'https://version.cyberduck.io/changelog.rss',
-          checkpoint: '16537c2a31ea9937848da56b27c89433bbaeb5722c5a744cc351fcd97eae3373'
+          checkpoint: 'a6e08dce08837bf5ab933d24a158fbe634f58009b0c2556a45f339ed8c054675'
   name 'Cyberduck'
   homepage 'https://cyberduck.io/'
   license :gpl
