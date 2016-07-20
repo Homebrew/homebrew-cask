ed4548e342f86156f484e87f707531fa6edcf296
Wes Baker <wes@wesbaker.com>
2016-07-20 10:32:35 -0400

    updated vassal (3.2.16) (#22954)

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -1,6 +1,6 @@
 cask 'vassal' do
-  version '3.2.15'
-  sha256 '936f7eeda9ee19c78708102e647aa43b91bfd70690da2552c72a28d5fbd10541'
+  version '3.2.16'
+  sha256 '13a6a556a4c4d87a8c40556b72b8c703f919da19f3144ddf48d61be947e9bb02'
 
   # sourceforge.net/project/vassalengine was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/vassalengine/VASSAL-current/VASSAL-#{version}/VASSAL-#{version}-macosx.dmg"
