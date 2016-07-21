b2aef9eeca5fe168f2a5509a648c98ecd157e013
Norman Walker <norman.walker@avira.com>
2016-07-21 13:58:24 +0200

    Avira-VPN: Finally fixed the comment issue.

--- Casks/avira-vpn.rb
+++ Casks/avira-vpn.rb
@@ -2,7 +2,7 @@ cask 'avira-vpn' do
   version :latest
   sha256 :no_check
 
-  # Verified install.avira-update.com/package/vpn/osx/int/vpn.pkg as the official download link.
+  # avira-update.com was verified as official when first introduced to the cask
   url 'https://install.avira-update.com/package/vpn/osx/int/vpn.pkg'
   name 'Avira VPN'
   homepage 'http://www.avira.com/en/avira-phantom-vpn'
