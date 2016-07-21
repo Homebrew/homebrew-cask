8f430804c94254ba1bb14b55d6863149985ff902
Victor Popkov <victor@popkov.me>
2016-07-21 20:28:00 +0300

    updated mactracker (7.5.6)

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,10 +1,10 @@
 cask 'mactracker' do
-  version '7.5.5'
-  sha256 '9eabc8dcc9eb86bed6ddf9b6d3867b69a5fb5fc57c5111b741ec7f98f7019731'
+  version '7.5.6'
+  sha256 'd09ca4f15a2d610a670111b0e6913ab27bc6a75f174f08cfd8583cedeff174e2'
 
   url "http://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast 'http://update.mactracker.ca/appcast-b.xml',
-          checkpoint: 'ca0c3fae4628bfa2ea2dea2f685bd67c70638eb0214dfd1e3735753a0034aea0'
+          checkpoint: '6b79b75ef700dca673faab2ac7df022b7f170012a5c0080793049b909fa1efc0'
   name 'Mactracker'
   homepage 'http://mactracker.ca/'
   license :gratis
