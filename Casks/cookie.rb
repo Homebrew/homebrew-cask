75ed4c838ecc9e615fc6ffe5b96043a20a877653
Jussi Tuominen <wickedsp1d3r@COLDwithin.local>
2016-07-21 19:43:10 +0300

    updated cookie (5.0.11)

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.0.10'
-  sha256 '9f50869effeeb23b99cff8e187f138ab4d1c0d29b0672833316c53a775ccf734'
+  version '5.0.11'
+  sha256 '9688c455f7a8f1b5373619136bb31d9cc40de335a630e80b8a914e7f53715eb5'
 
   url 'https://sweetpproductions.com/products/cookie5/Cookie5.zip'
   appcast 'https://sweetpproductions.com/products/cookie5/appcast.xml',
-          checkpoint: 'faaa97a800da4e62f559fdbdeb9851f2dc985e3453ed5fbee27b2c3c0cd7c845'
+          checkpoint: 'dff4962f6d60dfb9c0ab54bdc1a7ee4baddd4f622c5ac091b17173367bc5dc0f'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
   license :commercial
