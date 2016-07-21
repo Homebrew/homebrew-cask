ab2921e6bcd9420da559589b700cddbe17280558
Philipp Weißmann <github@philipp-weissmann.de>
2016-07-21 09:54:21 +0200

    Update etcher to version 1.0.0-beta.11

--- Casks/etcher.rb
+++ Casks/etcher.rb
@@ -1,6 +1,6 @@
 cask 'etcher' do
-  version '1.0.0-beta.8'
-  sha256 'fa8489ad9d51de8884b862afed054b3d8597a2aa7c21e7cc5e28894868fb2466'
+  version '1.0.0-beta.11'
+  sha256 'a70a986971a00865bb036ba629a46de5e49efee5a2bc8da87412fbdbc01d0e42'
 
   # resin-production-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://resin-production-downloads.s3.amazonaws.com/etcher/#{version}/Etcher-darwin-x64.dmg"
