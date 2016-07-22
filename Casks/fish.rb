25c2ae24582d0495cc8b09e4bd77ca5b136b9fd9
mathbunnyru <mathbunnyru@users.noreply.github.com>
2016-07-23 01:22:46 +0400

    Update fish.rb to v2.3.1 (#22998)
    New 2.3.1 version

--- Casks/fish.rb
+++ Casks/fish.rb
@@ -1,8 +1,8 @@
 cask 'fish' do
-  version '2.2.0'
-  sha256 '8e430d1cadec82aa0c3c757ec6ee24abc18aa5d432ccf435dd9f8a58588970bc'
+  version '2.3.1'
+  sha256 '0993fcb0b10b1d2609f0d4533836f43f66ae2cba2f59f279208f603cdf7fd770'
 
-  url "http://fishshell.com/files/#{version}/fish.pkg"
+  url "http://fishshell.com/files/#{version}/fish-#{version}.pkg"
   name 'Fish Shell'
   homepage 'http://fishshell.com'
   license :gpl
