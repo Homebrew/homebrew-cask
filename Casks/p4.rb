a3ba437a7f2e6dbe1068f993d36d78774709fcc1
Morten Bøgh <morten@justabeech.com>
2016-07-22 16:16:11 +0200

    Moved url to 32/64 bit check in p4

--- Casks/p4.rb
+++ Casks/p4.rb
@@ -3,11 +3,12 @@ cask 'p4' do
 
   if Hardware::CPU.is_32_bit?
     sha256 'ddc3272687038b1b507017f6a702e9bdf41b8faf6d5b89ca27e0fc9b1879ad4f'
+    url "http://filehost.perforce.com/perforce/r#{version.sub(%r{\A20(\d\d\.\d+).*}, '\1')}/bin.macosx105x86/p4"
   else
     sha256 '85153a60d75d84b172320e0d9348d0da67979f24a2e63650e5febf005afac778'
+    url "http://filehost.perforce.com/perforce/r#{version.sub(%r{\A20(\d\d\.\d+).*}, '\1')}/bin.macosx105x86_64/p4"
   end
 
-  url "http://filehost.perforce.com/perforce/r#{version.sub(%r{\A20(\d\d\.\d+).*}, '\1')}/bin.macosx105x86#{'_64' unless Hardware::CPU.is_32_bit?}/p4"
   name 'Perforce Command-Line Client'
   name 'p4'
   homepage 'https://www.perforce.com/product/components/perforce-commandline-client'
