a26006b0c3fe3048597773e7a7e1155bfc6df7c3
Jürgen Jatzkowski <jj@patagonics.de>
2016-07-23 14:43:11 +0200

    Add 'Qobuz Desktop' v1.5.0

---
+++ Casks/qobuz-desktop.rb
@@ -0,0 +1,14 @@
+cask 'qobuz-desktop' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.qobuz.com/apps/qobuz-desktop/osx/QobuzDesktopInstaller.pkg'
+  name 'Qobuz Desktop'
+  homepage 'http://www.qobuz.com/de-de/applications#app-desktop'
+  license :unknown
+
+  pkg 'QobuzDesktopInstaller.pkg'
+
+  uninstall pkgutil: 'com.qobuz.QobuzDesktop.*',
+            delete: '/Applications/Qobuz Desktop.app'
+end
