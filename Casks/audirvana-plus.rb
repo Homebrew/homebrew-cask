46fe2d89ac203ae133bccaff32b70040355bf194
Markus Reiter <me@reitermark.us>
2016-07-25 00:09:09 +0200

    updated audirvana-plus (2.5.3)

--- Casks/audirvana-plus.rb
+++ Casks/audirvana-plus.rb
@@ -1,10 +1,10 @@
 cask 'audirvana-plus' do
-  version '2.5'
-  sha256 '1ec64f1e0743cb0c5a6084d33db9851bd920d3c95cfefb0f61d73c5a66a2755f'
+  version '2.5.3'
+  sha256 'a7581b3d13bad7ba5d7590b446221775c7085e20fed0188a76ba10c60e17c87f'
 
   url "https://audirvana.com/delivery/AudirvanaPlus_#{version}.dmg"
   appcast 'https://audirvana.com/delivery/audirvanaplus2_appcast.xml',
-          checkpoint: '57992a3bc8fc20129af011a952eb1c04815c5aca01a9875b50683f63eedfd5a3'
+          checkpoint: '0b388d211a8cba93df931399c07fd3b464fc7e5332c6e08b0959e102d62cf57a'
   name 'Audirvana Plus 2'
   homepage 'https://audirvana.com/'
   license :commercial
