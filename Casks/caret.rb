0d4f39a5fb80f20159d35b1093b51260a5e042c4
Markus Reiter <me@reitermark.us>
2016-07-25 01:30:39 +0200

    updated caret (1.4.0) (#23077)

--- Casks/caret.rb
+++ Casks/caret.rb
@@ -1,11 +1,11 @@
 cask 'caret' do
-  version '1.3.3'
-  sha256 'cd47d5beed2deb1a55d3ddb65b4ec9ac1cefb6d50f1b5e1bde3bd3ea2c96aace'
+  version '1.4.0'
+  sha256 '43949b3897584cfc44912fb91725f458b0db8fffccdd0898baf20b8c28fe0fd4'
 
   # github.com/careteditor/caret was verified as official when first introduced to the cask
-  url "https://github.com/careteditor/caret/releases/download/v#{version}/Caret.dmg"
+  url "https://github.com/careteditor/caret/releases/download/#{version}/Caret.dmg"
   appcast 'https://github.com/careteditor/caret/releases.atom',
-          checkpoint: '3d91c89c2f2bb2c594d07c48d93222758146c1bae16e13fd91bf20c9ebe8b2bc'
+          checkpoint: '77800db3d65041806baecdbccd71859a96ad9c7a2098ba28dccd3cb17a70e9fb'
   name 'Caret'
   homepage 'http://caret.io/'
   license :commercial
