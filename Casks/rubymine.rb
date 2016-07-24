afcdf90424bfcc61f4fec9e0e139d32fd9ae1486
bluehallu <bluehallu@gmail.com>
2016-07-24 18:02:06 +0100

    Update Rubymine to 2016.2

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -1,6 +1,6 @@
 cask 'rubymine' do
-  version '2016.1.2'
-  sha256 '947b87ab8425ef0d5e075baab68bd522111a034361d6cca756c1df42c99f76bf'
+  version '2016.2'
+  sha256 '0c5f92a343fddf8f6e3c694d122c255c2b1cc6405c1a027d6b1777c67eb04091'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version}.dmg"
   name 'RubyMine'
