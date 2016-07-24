8a507e2d28401e7857280f0bb1ba25de49ce16f3
Markus Reiter <me@reitermark.us>
2016-07-24 16:48:52 +0200

    Fix Soundnode URL. (#23037)

--- Casks/soundnode.rb
+++ Casks/soundnode.rb
@@ -2,8 +2,8 @@ cask 'soundnode' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.soundnodeapp.com/downloads/mac/Soundnode-App.zip'
-  name 'Soundnode App'
+  url 'http://www.soundnodeapp.com/downloads/mac/Soundnode.zip'
+  name 'Soundnode'
   homepage 'http://www.soundnodeapp.com/'
   license :oss
 
