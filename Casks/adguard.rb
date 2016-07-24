623b12556a4ea410d6b8d9c5c13bb97976d20f4a
Markus Reiter <me@reitermark.us>
2016-07-24 23:33:26 +0200

    updated adguard (1.3.0)

--- Casks/adguard.rb
+++ Casks/adguard.rb
@@ -1,10 +1,10 @@
 cask 'adguard' do
   version '1.3.0'
-  sha256 '5c3dacdb060475a98998ab7d86397cd041c574904b3055f422f262c84f9a6b21'
+  sha256 'eb8f1fbb8e0ddf31f46eae39a571bee6d1237a1cdda0fa245bb21a85c2e32c25'
 
   url "https://static.adguard.com/mac/Adguard-#{version}.release.dmg"
   appcast 'https://static.adguard.com/mac/adguard-release-appcast.xml',
-          checkpoint: '1ca05e88a34737cde6c0c9c2292e45e30b0b243db9b256477f568da7b5687d0c'
+          checkpoint: 'a934745bff30fa64ca202744b91a9c11dad69e60e84bf905aa4e118754fa993c'
   name 'Adguard for Mac'
   homepage 'https://adguard.com/'
   license :commercial
