135b88fa8d411238eb5f9d694b39797c83127e2a
Markus Reiter <me@reitermark.us>
2016-07-25 00:45:02 +0200

    updated blocs (2.1.1) (#23071)

--- Casks/blocs.rb
+++ Casks/blocs.rb
@@ -1,11 +1,11 @@
 cask 'blocs' do
   version '2.1.1'
-  sha256 '2ad3558f020144eddbd0b7df532eaa71ea54318a4c4b3e6bf5747091c8824a5b'
+  sha256 '132a8b9fff71d8c83f15b9568f746d44b988ea49216be35a05779c8e91ab7d5f'
 
   # uistore.io was verified as official when first introduced to the cask
   url 'http://downloads.uistore.io/blocs/version-2/Blocs.zip'
   appcast 'https://uistore.io/blocs/2.0/info.xml',
-          checkpoint: '91f7f658121e3c25a18a89570bc7595b67ed7389e8ecebf1f523f6b0e9f560c5'
+          checkpoint: '586bab690dd9bb90834a896b5afe05cd5ac0d662f93b0c7b07a9b3bd8f39b25a'
   name 'Blocs'
   homepage 'http://blocsapp.com/'
   license :commercial
