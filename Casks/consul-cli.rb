730499906fc64adfe01091f8a4a71ea638a71d5b
Markus Reiter <me@reitermark.us>
2016-07-25 02:08:23 +0200

    updated consul-cli (0.3.1) (#23087)

--- Casks/consul-cli.rb
+++ Casks/consul-cli.rb
@@ -1,10 +1,10 @@
 cask 'consul-cli' do
-  version '0.2.0'
-  sha256 '2a048dceb47ec4df9ed6ddf26c945f8557790723a9194aee40ac6c5019c73a17'
+  version '0.3.1'
+  sha256 '245915c510547e37ad19ee3eb934d9981e9e39cf4db55c89fff82ca13555bb1c'
 
   url "https://github.com/CiscoCloud/consul-cli/releases/download/v#{version}/consul-cli_#{version}_darwin_amd64.tar.gz"
   appcast 'https://github.com/CiscoCloud/consul-cli/releases.atom',
-          checkpoint: 'c460875673bda22f2bd9251e51bdb2fbf08c07b8136df9e54fd0ba1f9ee192e9'
+          checkpoint: '18cefe6107b5f47bdbc9430028a7708f1abfd4fd266379f2ffa7cbbc1eed51f6'
   name 'Consul CLI'
   homepage 'https://github.com/CiscoCloud/consul-cli/wiki'
   license :apache
