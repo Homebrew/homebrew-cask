c05552be9dd9736b345a23691a552142e4085c41
Markus Reiter <me@reitermark.us>
2016-07-25 22:57:49 +0200

    updated hazel (4.0.4) (#23120)

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -1,11 +1,11 @@
 cask 'hazel' do
-  version '4.0.3'
-  sha256 'fd50798cb2e738ddbbf2d9f5dcb53e0b74aa5802386e858c80f80db1ce3fba69'
+  version '4.0.4'
+  sha256 'de01730a13bae394036f732de7438dfd59c6bd90ca80df4db1b3059cb854b327'
 
   # s3.amazonaws.com/Noodlesoft was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Noodlesoft/Hazel-#{version}.dmg"
   appcast 'https://www.noodlesoft.com/release_notes',
-          checkpoint: '0e65e37801a2a162bd8b1c020061c546a0d6064915d08f48dc4f7beffcfcdc8d'
+          checkpoint: 'e388f88a192ef6a147671ef825bc960f1dbfbd5bd0756ee3f1cf4f45257ef56a'
   name 'Hazel'
   homepage 'https://www.noodlesoft.com/hazel.php'
   license :freemium
