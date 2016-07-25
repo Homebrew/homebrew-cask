bae897eccdfcbd184158857d063b3107cfc39651
Arthur Tazhitdinov <osteotek@gmail.com>
2016-07-25 15:29:50 +0300

    Cask for Master Pasword app (latest non-MAS version)

---
+++ Casks/master-password.rb
@@ -0,0 +1,16 @@
+cask 'master-password' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://ssl.masterpasswordapp.com/masterpassword-mac.zip'
+  name 'Master Password'
+  homepage 'http://masterpasswordapp.com'
+  license :gpl
+
+  app 'Master Password.app'
+
+  uninstall quit:      'com.lyndir.lhunath.MasterPassword.Mac',
+            launchctl: 'com.lyndir.lhunath.MasterPassword.Mac.LoginHelper',
+            pkgutil:   'com.lyndir.lhunath.MasterPassword.*',
+            delete:    '/Applications/Master Password.app'
+end
