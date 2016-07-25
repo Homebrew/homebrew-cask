496b1a212cfc3b0905fa2f8977a5dfb7e786697b
Markus Reiter <me@reitermark.us>
2016-07-25 23:18:57 +0200

    updated mac-linux-usb-loader (3.2.2) (#23127)

--- Casks/mac-linux-usb-loader.rb
+++ Casks/mac-linux-usb-loader.rb
@@ -5,7 +5,7 @@ cask 'mac-linux-usb-loader' do
   # github.com/SevenBits/Mac-Linux-USB-Loader was verified as official when first introduced to the cask
   url "https://github.com/SevenBits/Mac-Linux-USB-Loader/releases/download/v#{version}/Mac.Linux.USB.Loader.zip"
   appcast 'https://github.com/SevenBits/Mac-Linux-USB-Loader/releases.atom',
-          checkpoint: '096f9376cf6d7c78f599d86f29e5a9d5e99ff4505a99082056180ea5e6fadf36'
+          checkpoint: '2f828e4edbab5a5814f8e4e7be45c68ded9ed0ea9316c362830ff96d647647b2'
   name 'Mac Linux USB Loader'
   homepage 'https://sevenbits.github.io/Mac-Linux-USB-Loader/'
   license :bsd
