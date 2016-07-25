9b1b6c6da470527dd2c28f10467c16addc23c146
Markus Reiter <me@reitermark.us>
2016-07-25 02:49:56 +0200

    updated cryptol (2.4.0)

--- Casks/cryptol.rb
+++ Casks/cryptol.rb
@@ -1,11 +1,11 @@
 cask 'cryptol' do
-  version '2.3.0'
-  sha256 '734e2781aaa3d5cceb59cad12aa7592c90db9920785b71d052761b0899841641'
+  version '2.4.0'
+  sha256 'a643ba63920cb4a7f119cc9432ac1de6e8c309c0656f984dd5826b19c7b48963'
 
   # github.com/GaloisInc/cryptol was verified as official when first introduced to the cask
   url "https://github.com/GaloisInc/cryptol/releases/download/#{version}/cryptol-#{version}-MacOSX-64.tar.gz"
   appcast 'https://github.com/GaloisInc/cryptol/releases.atom',
-          checkpoint: '58a2b57ae6c2c9775b4025228bbf92caad8f9b489b4fc36d185ffe0bc43d4b85'
+          checkpoint: '40789a770eae4e9c8ddde82cc49f2f9c9a0117192dd478249acf7e12cf588275'
   name 'Cryptol'
   homepage 'http://cryptol.net/'
   license :bsd
