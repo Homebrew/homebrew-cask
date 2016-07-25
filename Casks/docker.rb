7c1f5f6a75624f24e63eb03586eb32e6348e117f
Sean Smith <sean@seansith.com>
2016-07-24 22:12:49 -0400

    updated docker (1.12.0.10404)

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -1,10 +1,10 @@
 cask 'docker' do
-  version '1.12.0.9996'
-  sha256 '7ac7c061b135f821fac39e53f7b74233dcaf8f926e4c6d28031acc8717b85107'
+  version '1.12.0.10404'
+  sha256 'ba458f4b568ca9f8395469f1cdc3030f3924bcfc166658ec3e29284af1a1d633'
 
   url "https://download.docker.com/mac/beta/#{version}/Docker.dmg"
   appcast 'https://download.docker.com/mac/beta/appcast.xml',
-          checkpoint: 'd0c1276909ef816d6fee039c1aa4a5584c1bea4354ad6d5607f9a4e600304c94'
+          checkpoint: 'dbdd029c3d6576232188b33148ff0aca12c68b329c6646fb3628e2c8e4bdb6cb'
   name 'Docker for Mac'
   homepage 'https://www.docker.com/products/docker'
   license :mit
