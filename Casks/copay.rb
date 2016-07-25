67a0d88878d0008d41a9186024939162d4d5fce8
Markus Reiter <me@reitermark.us>
2016-07-25 02:12:41 +0200

    updated copay (2.3.0) (#23089)

--- Casks/copay.rb
+++ Casks/copay.rb
@@ -1,11 +1,11 @@
 cask 'copay' do
-  version '1.12.1'
-  sha256 '2c3181c5985dd7165e563c2702ec927947a8e0f601583204bdecaf897fa3ebdc'
+  version '2.3.0'
+  sha256 'f76e49dda5d17903ec2e11ba1c0a004fd260d2cdf69886a64173ae0b6487ed48'
 
   # github.com/bitpay/copay was verified as official when first introduced to the cask
   url "https://github.com/bitpay/copay/releases/download/v#{version}/Copay-osx.dmg"
   appcast 'https://github.com/bitpay/copay/releases.atom',
-          checkpoint: 'b28d35e6e29c6811be0e5d9531a95fed474b69d0cb783c1bcf48c7b22977f71e'
+          checkpoint: 'cc185d20343e47f5545b5b9a26a6839abc645555b70234170403a995ef047a2a'
   name 'Copay'
   homepage 'https://copay.io'
   license :oss
