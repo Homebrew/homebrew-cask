2724ab0a304b7cabe9a30bfa65478cd6b2f65463
Markus Reiter <me@reitermark.us>
2016-07-25 19:30:20 +0200

    Use `version.major_minor` where possible.

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -2,7 +2,7 @@ cask 'qt-creator' do
   version '4.0.2'
   sha256 'cf651ae9dca43578baff53ed4e4793aa54a2c5693b752db13d703beec0d85814'
 
-  url "http://download.qt.io/official_releases/qtcreator/#{version.to_f}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
+  url "http://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
   homepage 'https://www.qt.io/developers/'
   license :gpl
