e751d2abc2667628605f018d3142ea1f03f8b4fc
Victor Popkov <victor@popkov.me>
2016-07-26 22:08:37 +0300

    updated geektool (330.006,1469399034)

--- Casks/geektool.rb
+++ Casks/geektool.rb
@@ -1,11 +1,11 @@
 cask 'geektool' do
-  version '320.23,1468354898'
-  sha256 '21f51ea73c5e552613a432f534dafe8eb31d118db18a4f3094adaceedbd827f5'
+  version '330.006,1469399034'
+  sha256 '2f8cae65837dd349c8a0a057e2dd6532e5c62a4868af8c13b8ad5597e9d84500'
 
   # dl.devmate.com/org.tynsoe.GeekTool was verified as official when first introduced to the cask
   url "https://dl.devmate.com/org.tynsoe.GeekTool/#{version.before_comma}/#{version.after_comma}/GeekTool-#{version.before_comma}.zip"
   appcast 'https://updates.devmate.com/org.tynsoe.GeekTool.xml',
-          checkpoint: 'e32e33786907cd3857f0694a84f0c5ea916b921e03413b16c8711575336042bd'
+          checkpoint: '760c8a6d2b95cd5d981f539efe396e03dcaac358bc0f8a86d52a67aaa2be5003'
   name 'GeekTool'
   homepage 'http://projects.tynsoe.org/en/geektool/'
   license :mit
