b55e0f455886a0d583cbcba41c74348cc8efecde
Victor Popkov <victor@popkov.me>
2016-07-26 22:02:32 +0300

    updated caret (1.5.1)

--- Casks/caret.rb
+++ Casks/caret.rb
@@ -1,11 +1,11 @@
 cask 'caret' do
-  version '1.4.0'
-  sha256 '43949b3897584cfc44912fb91725f458b0db8fffccdd0898baf20b8c28fe0fd4'
+  version '1.5.1'
+  sha256 '145b93a80ff5c1d170d762beb98c62dfb1a97ab54cc9d49303fd8384def11443'
 
   # github.com/careteditor/caret was verified as official when first introduced to the cask
   url "https://github.com/careteditor/caret/releases/download/#{version}/Caret.dmg"
   appcast 'https://github.com/careteditor/caret/releases.atom',
-          checkpoint: '77800db3d65041806baecdbccd71859a96ad9c7a2098ba28dccd3cb17a70e9fb'
+          checkpoint: '6d765756107e24d5c217e4f4269899ada41a802467ef887d3c568012c3d59b44'
   name 'Caret'
   homepage 'http://caret.io/'
   license :commercial
