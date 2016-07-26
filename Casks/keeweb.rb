3eeaaf6e8c223d5755af2311408877b86997cd99
Luís Bianchin <labianchin@users.noreply.github.com>
2016-07-26 11:08:38 +0200

    Update KeeWeb to 1.2.4 (#23171)

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,11 +1,11 @@
 cask 'keeweb' do
-  version '1.2.2'
-  sha256 '8b4a629a9be8781864302f2bf204f96449383eaed16bd5b6930369057d3fa2b1'
+  version '1.2.4'
+  sha256 '1862260a658985d43121e504944c2ff9c6758e58f72e06c47694568e745f7242'
 
   # github.com/keeweb/keeweb was verified as official when first introduced to the cask
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb.mac.dmg"
   appcast 'https://github.com/keeweb/keeweb/releases.atom',
-          checkpoint: '552205c4775461633d4ce431b3593b2a1f9b52cf35733a512964acd24bfb1c80'
+          checkpoint: '866cc9e0efe19e5e00be5ee1063c0c50890fe5f967c22939135b23f8217b100e'
   name 'KeeWeb'
   homepage 'https://keeweb.info'
   license :mit
