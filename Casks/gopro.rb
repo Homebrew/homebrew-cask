a3d82d11361914bc536be3cd1510a2f9cbda5827
Johnathan Conley <johnathan.conley@gmail.com>
2016-07-27 11:47:17 -0500

    updated gopro (1.4.0)

--- Casks/gopro.rb
+++ Casks/gopro.rb
@@ -1,6 +1,6 @@
 cask 'gopro' do
-  version '1.3.0.3078'
-  sha256 'b4926626362cbd12f20c782fcba945e2b5670fe175d592fb501dd84b471a4d95'
+  version '1.4.0.3411'
+  sha256 'a7df7d8f0e509a0411fc8a1e9df95da5099523aa9edf283f2c45b65308c7ecf8'
 
   url "https://software.gopro.com/Mac/GoPro-MacInstaller-#{version}.dmg"
   name 'GoPro Desktop'
