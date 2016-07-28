eed1b140da62b536b1d016102a4755bdfb936c5d
cooclsee <cooclsee@qq.com>
2016-07-28 16:21:24 +0800

    updated electron (1.3.1)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,11 +1,11 @@
 cask 'electron' do
-  version '1.2.8'
-  sha256 '53ab867364a785656773214ba8ea0e0096702811f46a38e8d3c3769f9c2f7c4d'
+  version '1.3.1'
+  sha256 '1b83459022cbb338ce0d618024d584a08d7648f4a62e54f12fbf3d7d8fe6edc5'
 
   # github.com/atom/electron was verified as official when first introduced to the cask
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
-          checkpoint: '8b2ceb9a58f2b34bf3305cb5875b2f6334260efe7e0b21df5d75780ee8b1bc1c'
+          checkpoint: 'd541fe97f88f8f9a57aa8228cf0f3c01fd7b0a905e92a69ed0a41edf0c90a42f'
   name 'Electron'
   homepage 'http://electron.atom.io/'
   license :mit
