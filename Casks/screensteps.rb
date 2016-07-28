a48bd77fa854efd71d895c6403eb2e857ce31b6f
Rutger Meekers <rutger@meekers.eu>
2016-07-28 11:50:22 +0200

    Revert "Fix screensteps.rb URL comment"
    This reverts commit 64a9c501f1b459f005f49dbde50c221835c0f7d4.

--- Casks/screensteps.rb
+++ Casks/screensteps.rb
@@ -2,7 +2,6 @@ cask 'screensteps' do
   version '4.0 build 108'
   sha256 '40678ddc531032950108e47976e9f54cd4e1660b22bfd4e5d16c06e15ac52c5b'
 
-  # bluemangolearning.com was verified as official when first introduced to the cask
   url 'https://www.bluemangolearning.com/download/screensteps/4_0/beta/ScreenSteps.dmg'
   name 'ScreenSteps'
   homepage 'http://www.screensteps.com/'
