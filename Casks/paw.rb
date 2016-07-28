50c50ec599a4cc40aec5a1e87ea8542727db5306
Richard Lee <dlackty@gmail.com>
2016-07-28 23:11:03 +0800

    updated paw (3.0)

--- Casks/paw.rb
+++ Casks/paw.rb
@@ -1,12 +1,13 @@
 cask 'paw' do
-  version '2.3.4'
-  sha256 '3b998b1733cd0c2ad4aff5b9f4106ec8a2fb98bced896f9117c25f5d9f999fdf'
+  version '3.0'
+  sha256 '4dd023fec7c14ce36258a8860d957680f9b90ff720771e0f3bec2108de8e5748'
 
-  url "https://luckymarmot.com/paw/download/#{version}"
-  appcast "https://luckymarmot.com/api/v#{version.major}/updates/appcast",
-          checkpoint: '8067da8efe004063dbfa99bd0c85e9b6cb5ee8bc3ea1fb7b8e6001a707029ade'
+  # d3hevc2w7wq7nj.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d3hevc2w7wq7nj.cloudfront.net/paw/Paw-#{version}-3000000000.zip"
+  appcast "https://paw.cloud/api/v2/updates/appcast",
+          checkpoint: 'f7c2809abde7f659df292edc087d11678731f645ebc5eaf4d8a118e021398a82'
   name 'Paw'
-  homepage 'https://luckymarmot.com/paw'
+  homepage 'https://paw.cloud'
   license :commercial
 
   app 'Paw.app'
