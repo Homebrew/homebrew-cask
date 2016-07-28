1cc941184b3f0b9dbcaef34413f127929875be95
Matt <matt@communicatehealth.com>
2016-07-28 13:47:20 -0400

    Updated Colour contrast analyzer to v2.3

--- Casks/colour-contrast-analyser.rb
+++ Casks/colour-contrast-analyser.rb
@@ -1,8 +1,9 @@
 cask 'colour-contrast-analyser' do
-  version '1.5.13'
+  version '2.3'
   sha256 '2e737dde56179725ccbad1306bfa8db2711bbc910d046cdb4c567e6a5ad86bb4'
 
-  url "http://files.paciellogroup.com/resources/CCA_#{version}.dmg"
+  # Downloads are hosted on Github account now
+  url 'https://github.com/ThePacielloGroup/CCA-OSX/releases/download/{version}/Colour.Contrast.Analyser.app.zip'
   name 'Colour Contrast Analyser'
   homepage 'https://www.paciellogroup.com/resources/contrastanalyser/'
   license :gpl
