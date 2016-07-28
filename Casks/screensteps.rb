bd12463fd8ecc0e01cbe521e4da6aa718f6ab08b
Rutger Meekers <rutger@meekers.eu>
2016-07-28 12:01:10 +0200

    Fix screensteps.rb URL comment (#23216)

--- Casks/screensteps.rb
+++ Casks/screensteps.rb
@@ -1,8 +1,9 @@
 cask 'screensteps' do
-  version '4.0 build 108'
+  version '4.0.108'
   sha256 '40678ddc531032950108e47976e9f54cd4e1660b22bfd4e5d16c06e15ac52c5b'
 
-  url 'https://www.bluemangolearning.com/download/screensteps/4_0/beta/ScreenSteps.dmg'
+  # bluemangolearning.com was verified as official when first introduced to the cask
+  url "https://www.bluemangolearning.com/download/screensteps/#{version.major_minor.dots_to_underscores}/beta/ScreenSteps.dmg"
   name 'ScreenSteps'
   homepage 'http://www.screensteps.com/'
   license :commercial
