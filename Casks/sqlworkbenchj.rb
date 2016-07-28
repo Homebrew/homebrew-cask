1aa2c92da562f888996b476ea6094f44e3b8af97
Dean Morin <morin.dean@gmail.com>
2016-07-28 15:32:59 -0700

    Fix sqlworkbenchj URL
    The download fails because a newer version was released and this version
    was moved to the `archive` folder.
    
    I've asked the maintainer for more durable download urls. In the
    meantime, I figured it's better to have a working url than the latest
    url which will break again when a new version is released.

--- Casks/sqlworkbenchj.rb
+++ Casks/sqlworkbenchj.rb
@@ -2,7 +2,7 @@ cask 'sqlworkbenchj' do
   version '119'
   sha256 '8565105504e517ca972b4f3c99f4436b13e7a3caaf8f53a97f44a71a7c71efc6'
 
-  url "http://www.sql-workbench.net/Workbench-Build#{version}-MacJava8.tgz"
+  url "http://www.sql-workbench.net/archive/Workbench-Build#{version}-MacJava8.tgz"
   name 'SQL Workbench/J'
   homepage 'http://www.sql-workbench.net'
   license :apache
