24ea2643b077ddb684b37a0c7d3901cdd0383e1e
Marius Karnauskas <marius.karnauskas@gmail.com>
2016-07-28 23:26:23 +0100

    Bump LiteIDE to X30.2

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask 'liteide' do
-  version '29'
-  sha256 '4a21386696edbbf8464d22539bc1d8cae0d1ecb694965a977c0206af64abc20d'
+  version '30.2'
+  sha256 'f4269b59169a342de125d566db39c6c96ba75366865b670f16ab2d910bd0bc1a'
 
   # sourceforge.net/project/liteide was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/project/liteide/X#{version}/liteidex#{version}.macosx-qt4.zip"
