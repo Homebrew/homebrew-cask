efd90db60bf4f27e0354356473e3377be69b2ed0
Pelle ten Cate <pelle@technoot.nl>
2016-07-29 17:05:23 +0200

    updated time-out (2.0.2) (#23256)

--- Casks/time-out.rb
+++ Casks/time-out.rb
@@ -6,8 +6,8 @@ cask 'time-out' do
     version '1.7.1'
     sha256 '3c9892344c8313b8ccf0a76cceb00834ddbe26e5114bcd674c4fd53aeb44e310'
   else
-    version '2.0.1'
-    sha256 '2748f22d74cab6c468b2f6bed4d6ba06e702d34cbc2778b687262f27cf9e5350'
+    version '2.0.2'
+    sha256 'f28b6cf73fc51801495b75e94108969700d06492d81d17ba7244040c251182cc'
   end
 
   url "http://www.dejal.com/download/timeout-#{version}.zip"
