21fbd2b46ac84d66bc013881277472199a3bfdfa
Markus Reiter <me@reitermark.us>
2016-07-29 02:00:11 +0200

    Add `appcast` to applications hosted on Github.

--- Casks/wavtap.rb
+++ Casks/wavtap.rb
@@ -3,6 +3,8 @@ cask 'wavtap' do
   sha256 'b114703e407ce269070165421a81f74e64ac1730a85d829c148325b4ac1a18f6'
 
   url "https://github.com/pje/WavTap/releases/download/#{version}/WavTap.#{version}.pkg"
+  appcast 'https://github.com/pje/WavTap/releases.atom',
+          checkpoint: '952af9b0ee8389aa44bf90fa72aa4b0e83a271341ff69930d0024a80cca40454'
   name 'WavTap'
   homepage 'https://github.com/pje/wavtap'
   license :mit
