aad529bcb87bed47280777fc58ca2c7163865f80
Joshua McKinney <joshka@users.noreply.github.com>
2016-07-28 21:57:32 -0500

    Update Docker to stable channel and v1.12.0.10871 (#23250)

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -1,10 +1,10 @@
 cask 'docker' do
-  version '1.12.0.10404'
-  sha256 'ba458f4b568ca9f8395469f1cdc3030f3924bcfc166658ec3e29284af1a1d633'
+  version '1.12.0.10871'
+  sha256 'f170610d95c188dee8433eff33c84696c1c8a39421de548a71a1258a458e1b21'
 
-  url "https://download.docker.com/mac/beta/#{version}/Docker.dmg"
-  appcast 'https://download.docker.com/mac/beta/appcast.xml',
-          checkpoint: 'dbdd029c3d6576232188b33148ff0aca12c68b329c6646fb3628e2c8e4bdb6cb'
+  url "https://download.docker.com/mac/stable/#{version}/Docker.dmg"
+  appcast 'https://download.docker.com/mac/stable/appcast.xml',
+          checkpoint: 'ee03f7c36b4192a64ba30e49a0fa1f8358cc3fe4a8a3af3def066c80f36b18bf'
   name 'Docker for Mac'
   homepage 'https://www.docker.com/products/docker'
   license :mit
