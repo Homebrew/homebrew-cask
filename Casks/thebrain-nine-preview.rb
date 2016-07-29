ee47d09350c5df234362241b7276d45db07a27fa
Roberto Nogueira <enogrob@gmail.com>
2016-07-29 17:29:23 -0300

    Adding thebrain-nine-preview cask

--- Casks/thebrain-nine-preview.rb
+++ Casks/thebrain-nine-preview.rb
@@ -1,9 +1,9 @@
-cask 'thebrain9' do
+cask 'thebrain-nine-preview' do
   version '9.0.97.0'
   sha256 '0b72955083b96b4f3a3f3597ee8431d5f26815b72a11b847aeb411cd089e0e94'
 
   url "http://api.thebrain.com/?a=doDirectDownload&id=78"
-  name 'TheBrain9'
+  name 'TheBrain 9 preview'
   homepage 'http://www.thebrain.com/products/thebrain/thebrain9/'
   license :commercial
 
