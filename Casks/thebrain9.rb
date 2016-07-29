77b63e73be922f21e2599d676c7414f65c603bec
Roberto Nogueira <enogrob@gmail.com>
2016-07-29 15:56:23 -0300

    Adding TheBrain9 version 9.0.97.0

---
+++ Casks/thebrain9.rb
@@ -0,0 +1,13 @@
+cask 'thebrain' do
+  version '9.0.97.0'
+  sha256 '0b72955083b96b4f3a3f3597ee8431d5f26815b72a11b847aeb411cd089e0e94'
+
+  url "http://api.thebrain.com/?a=doDirectDownload&id=78"
+  name 'TheBrain'
+  homepage 'http://www.thebrain.com/products/thebrain/thebrain9/'
+  license :commercial
+
+  app 'TheBrain 9.app'
+
+  uninstall delete: '/Applications/TheBrain.app'
+end
