7241fac8adb68a5f8858d68e78a6ab15a0388823
Roberto Nogueira <enogrob@gmail.com>
2016-07-29 18:12:24 -0300

    Adding thebrain-nine-preview Cask

--- Casks/thebrain-nine-preview.rb
+++ Casks/thebrain-nine-preview.rb
@@ -2,12 +2,12 @@ cask 'thebrain-nine-preview' do
   version '9.0.97.0'
   sha256 '0b72955083b96b4f3a3f3597ee8431d5f26815b72a11b847aeb411cd089e0e94'
 
-  url "http://api.thebrain.com/?a=doDirectDownload&id=78"
+  url 'http://api.thebrain.com/?a=doDirectDownload&id=78'
   name 'TheBrain 9 preview'
   homepage 'http://www.thebrain.com/products/thebrain/thebrain9/'
   license :commercial
 
   app 'TheBrain 9.app'
 
-  uninstall delete: '/Applications/TheBrain.app'
+  uninstall delete: '/Applications/TheBrain 9.app'
 end
