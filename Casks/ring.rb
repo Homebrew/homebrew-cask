ef628dcdf776fbeff004356baf8e158b2796534d
Anthony Angel <anthony.angel@unrulymedia.com>
2016-07-29 11:13:01 +0100

    Add Ring.app v0.55

--- Casks/ring.rb
+++ Casks/ring.rb
@@ -1,12 +1,16 @@
 cask 'ring' do
-  version :latest
-  sha256 :no_check
+  version '0.55'
+  sha256 'ea0a97d692866e9dbc09f228b6b098b3e3ecc1cc0d8ed04c0d649204035b7acd'
 
-  # s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/ring-mac-app-assets/RingMacLatest.zip'
+  # ring-mac-app-assets.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://ring-mac-app-assets.s3.amazonaws.com/production/Ring_#{version}.zip"
+  appcast 'https://ring-mac-app-assets.s3.amazonaws.com/production/ring-appcast.xml',
+          checkpoint: '56d4addc5e0302953537dfaff3cbd87283cc333e73d3b129bf251aa48a271f52'
   name 'Ring'
   homepage 'https://ring.com/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  app 'ring.app'
+  auto_updates 'True'
+
+  app 'Ring.app'
 end
