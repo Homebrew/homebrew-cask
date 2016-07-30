01de02f9eaed204dbbbd405d27789c5507d667af
Brice Dutheil <brice.dutheil@gmail.com>
2016-07-31 00:07:16 +0200

    Upgrade Eclipse Memory Analyzer to 1.6.0 (#23009)
    and rename memoryanalizer to mat

---
+++ Casks/mat.rb
@@ -0,0 +1,11 @@
+cask 'mat' do
+  version '1.6.0.20160531'
+  sha256 '300e151bc6bc7e34b196fc50803269f3ce23ad49b0cda9d8335d8861d13ccb64'
+
+  url "http://www.eclipse.org/downloads/download.php?r=1&file=/mat/#{version.major_minor}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.x86_64.zip"
+  name 'Eclipse Memory Analyzer'
+  homepage 'https://www.eclipse.org/mat/'
+  license :eclipse
+
+  app 'mat.app'
+end
