59e8d9d628776e24d2e7a146b3ed6ca527c2090a
Mabel Xu <mabelrxu@gmail.com>
2016-07-30 17:12:39 -0400

    update to runescape NXT client
    change URL to grab Runescape NXT v2.2.2 (13 Apr 2016) client

--- Casks/runescape.rb
+++ Casks/runescape.rb
@@ -2,7 +2,7 @@ cask 'runescape' do
   version :latest
   sha256 :no_check
 
-  url 'https://content.runescape.com/downloads/osx/RuneScape.dmg'
+  url 'https://content.runescape.com/downloads/osx/RuneScape.dmg?crc=2901131314'
   name 'RuneScape'
   homepage 'https://www.runescape.com'
   license :gratis
