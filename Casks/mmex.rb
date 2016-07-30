f7110a6f9a68b05b91e190dd373efc183d4f5400
chyiz <chyi.zhao@gmail.com>
2016-07-30 18:34:28 -0400

    Add appcast and fix the url stanza comment.

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -2,8 +2,10 @@ cask 'mmex' do
   version '1.2.7'
   sha256 '48b780000a5b153ce58b49d7ba03c45d9eed1311b3984d79f4f3080fb05d99d0'
 
-  # https://github.com/moneymanagerex/moneymanagerex seems to be the default repository now. See http://www.moneymanagerex.org/.
+  # github.com/moneymanagerex/moneymanagerex was verified as official when first introduced to the cask
   url "https://github.com/moneymanagerex/moneymanagerex/releases/download/v#{version}/mmex-v#{version}.dmg"
+  appcast 'https://github.com/moneymanagerex/moneymanagerex/releases.atom',
+          checkpoint: '18d49df544c4efd41e6f85a164250408437a9081fb6f3901c62f318e1c3e7e6c'
   name 'Money Manager Ex'
   homepage 'http://www.moneymanagerex.org'
   license :gpl
