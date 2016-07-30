dee902833ccbddba18f4eaa851e9e19580c09829
Victor Popkov <victor@popkov.me>
2016-07-30 21:46:26 +0300

    updated witch (3.9.8)

--- Casks/witch.rb
+++ Casks/witch.rb
@@ -4,12 +4,12 @@ cask 'witch' do
     sha256 '5e46508e150ff16be14b9955abdcd15098376230ef71e3de6f15a056eec75e45'
     url "http://manytricks.com/download/witch/#{version}"
   else
-    version '3.9.7'
-    sha256 '27862ad4de6c403c64849a834f3fde7efabe29e942d01e2204d4ee75d01fef5b'
+    version '3.9.8'
+    sha256 '6a290c9fcc6354044fb077d8ae3a6a7221cd5d4e19bf3a872ff09da95b9a7509'
 
     url 'http://manytricks.com/download/witch'
     appcast 'https://manytricks.com/witch/appcast.xml',
-            checkpoint: '290d0c4ebb54bb54ef1bcb174b587ba31d2bdd43c59f22189b2aae7a3d0321d3'
+            checkpoint: '239d72b3fac6a2c8ba69e8028cb3bed7de7d4065c5335f907183457a8db2e114'
   end
 
   name 'Witch'
