a5bf6fcfd63d1f2b87945d7d72fa00cab68d7462
Markus Reiter <me@reitermark.us>
2016-07-31 21:33:03 +0200

    SourceForge /project/ URLs P-Z (#23304)

--- Casks/scribus.rb
+++ Casks/scribus.rb
@@ -2,10 +2,10 @@ cask 'scribus' do
   version '1.4.6'
   sha256 'db818ae3a69ca16c1b3fb873b55903062f7b81f42d7adcc64a61ee93bf95727e'
 
-  # downloads.sourceforge.net/project/scribus was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/project/scribus/scribus/#{version}/scribus-#{version}.dmg"
+  # downloads.sourceforge.net/scribus was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/scribus/scribus/#{version}/scribus-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/scribus/rss',
-          checkpoint: 'd097b602655dd8b18a3012f406b325f0279c643c8b43f155927eabf14decd7e1'
+          checkpoint: '1f49e634e8be3cad53eef763364d5436821f1984cd2a9a9b96e8f5963cb34c79'
   name 'Scribus'
   homepage 'http://www.scribus.net/canvas/Scribus'
   license :gpl
--- Casks/zeroinstall.rb
+++ Casks/zeroinstall.rb
@@ -2,8 +2,8 @@ cask 'zeroinstall' do
   version '2.8'
   sha256 'ab0fbf7fc43c5ff6429d5686bde3ecef01c78894c46c8bd554c2f4a1f0e10e66'
 
-  # downloads.sourceforge.net/project/zero-install was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/project/zero-install/0install/#{version}/ZeroInstall.pkg"
+  # downloads.sourceforge.net/zero-install was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/zero-install/0install/#{version}/ZeroInstall.pkg"
   appcast 'https://sourceforge.net/projects/zero-install/rss',
           checkpoint: '7c02e36690dd4e096e40898ee54d59916110c3f3128862ba327857585daa7e57'
   name 'Zero Install'
