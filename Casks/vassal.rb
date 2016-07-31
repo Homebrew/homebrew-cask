ca08d3f7cd3ce93158890ea705a7dd6cded721d4
Markus Reiter <me@reitermark.us>
2016-07-31 02:20:24 +0200

    Add appcast to sourceforge URLs.

--- Casks/vassal.rb
+++ Casks/vassal.rb
@@ -3,7 +3,9 @@ cask 'vassal' do
   sha256 '13a6a556a4c4d87a8c40556b72b8c703f919da19f3144ddf48d61be947e9bb02'
 
   # sourceforge.net/project/vassalengine was verified as official when first introduced to the cask
-  url "http://downloads.sourceforge.net/project/vassalengine/VASSAL-current/VASSAL-#{version}/VASSAL-#{version}-macosx.dmg"
+  url "https://downloads.sourceforge.net/project/vassalengine/VASSAL-current/VASSAL-#{version}/VASSAL-#{version}-macosx.dmg"
+  appcast 'https://sourceforge.net/projects/vassalengine/rss',
+          checkpoint: '441baba4b477cdd3a872591cb19e084a7b3c9ac52da2c8efdd948a6dd671b3dd'
   name 'VASSAL'
   homepage 'http://www.vassalengine.org/'
   license :gpl
--- Casks/vistrails.rb
+++ Casks/vistrails.rb
@@ -3,7 +3,9 @@ cask 'vistrails' do
   sha256 'efee1669d3ba39985079e44a423f6e7b6b17493669c5945a3bbd70ec8659fc22'
 
   # sourceforge.net/project/vistrails was verified as official when first introduced to the cask
-  url "http://downloads.sourceforge.net/project/vistrails/vistrails/v#{version.sub(%r{-.*}, '')}/vistrails-mac-10.6-intel-#{version}.dmg"
+  url "https://downloads.sourceforge.net/project/vistrails/vistrails/v#{version.sub(%r{-.*}, '')}/vistrails-mac-10.6-intel-#{version}.dmg"
+  appcast 'https://sourceforge.net/projects/vistrails/rss',
+          checkpoint: '5b5470cbdb0773a980d33bdb4c56d44d4d1ccbc56961988faaccc9104174569c'
   name 'VisTrails'
   homepage 'http://www.vistrails.org/'
   license :bsd
