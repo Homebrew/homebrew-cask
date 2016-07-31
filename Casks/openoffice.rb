159fea5b8ed0b917a9c0f81a81852520cc4e048e
Markus Reiter <me@reitermark.us>
2016-07-31 06:40:06 +0200

    SourceForge URLs M-S

--- Casks/openoffice.rb
+++ Casks/openoffice.rb
@@ -2,8 +2,8 @@ cask 'openoffice' do
   version '4.1.2'
   sha256 '4fe8e4b30989d0476fe3afc6d9d4374af57d38bd04f4a5e1947462bf5dde7699'
 
-  # downloads.sourceforge.net/sourceforge/openofficeorg.mirror was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/sourceforge/openofficeorg.mirror/Apache_OpenOffice_#{version}_MacOS_x86-64_install_en-US.dmg"
+  # downloads.sourceforge.net/openofficeorg.mirror was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/openofficeorg.mirror/Apache_OpenOffice_#{version}_MacOS_x86-64_install_en-US.dmg"
   name 'Apache OpenOffice'
   homepage 'https://www.openoffice.org/'
   license :apache
--- Casks/psi.rb
+++ Casks/psi.rb
@@ -2,8 +2,8 @@ cask 'psi' do
   version '0.15'
   sha256 '76d50b5314d0a7d9f9a0a71d90e0f806f2da25436135dbd90bd1959286747742'
 
-  # downloads.sourceforge.net/sourceforge/psi was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/sourceforge/psi/Psi-#{version}.dmg"
+  # downloads.sourceforge.net/psi was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/psi/Psi-#{version}.dmg"
   name 'Psi'
   homepage 'http://psi-im.org/'
   license :gpl
