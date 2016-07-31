b86d7d064403677956239b2ea27668b590bfe07a
Markus Reiter <me@reitermark.us>
2016-07-31 07:45:28 +0200

    SourceForge URLs T-W (#23295)

--- Casks/tag.rb
+++ Casks/tag.rb
@@ -2,8 +2,8 @@ cask 'tag' do
   version '0.4.1'
   sha256 '165631a76db33cbe5200ff64a64f5e1dad6606ae815c7609d59b7367f9344360'
 
-  # downloads.sourceforge.net/sourceforge/tagosx was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/sourceforge/tagosx/Tag-#{version}.zip"
+  # downloads.sourceforge.net/tagosx was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/tagosx/Tag-#{version}.zip"
   name 'Tag'
   homepage 'http://sbooth.org/Tag/'
   license :gpl
--- Casks/tv-browser.rb
+++ Casks/tv-browser.rb
@@ -2,8 +2,8 @@ cask 'tv-browser' do
   version '3.4.4'
   sha256 '53128cbebb2432ca254893453ea4f13fb16abdaf74d7d66355588d523162a086'
 
-  # downloads.sourceforge.net/sourceforge/tvbrowser was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/sourceforge/tvbrowser/tvbrowser_#{version}_macjava.dmg"
+  # downloads.sourceforge.net/tvbrowser was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/tvbrowser/tvbrowser_#{version}_macjava.dmg"
   name 'TV-Browser'
   homepage 'http://www.tvbrowser.org/'
   license :gpl
