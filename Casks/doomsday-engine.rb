03880ef8f6f62bdd7c58decdd2d4fbf32961cda5
Markus Reiter <me@reitermark.us>
2016-07-31 08:09:48 +0200

    SourceForge URLs A-D (#23298)

--- Casks/doomsday-engine.rb
+++ Casks/doomsday-engine.rb
@@ -2,8 +2,8 @@ cask 'doomsday-engine' do
   version '1.15.8'
   sha256 '6882d3ff2aa855096e003ec19b1ccca4e9b2b4576d2c667abf8e48310f08f098'
 
-  # sourceforge.net/sourceforge/deng was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/sourceforge/deng/doomsday_#{version}.dmg"
+  # sourceforge.net/deng was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/deng/doomsday_#{version}.dmg"
   name 'Doomsday Engine'
   homepage 'http://dengine.net'
   license :gpl
