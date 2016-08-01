6ec1a191e8cd253caf7a9efe2c963135b3989853
Markus Reiter <me@reitermark.us>
2016-08-01 06:30:18 +0200

    Fix BankID URL.

--- Casks/bankid.rb
+++ Casks/bankid.rb
@@ -1,15 +1,13 @@
 cask 'bankid' do
   version '7.2.0'
-  sha256 '0f862bd59ac52349a016745ac098a9aa8861ecb9b3032fc677aca719da40ff12'
+  sha256 '9a3768cb9ea240faee5d4958679e17b73e5ffeb62765f3fdb2e34275ca0beb1a'
 
-  url "https://install.bankid.com/Repository/BankID_installation_#{version}.pkg"
+  url "https://install.bankid.com/Repository/BankID_installation_#{version.dots_to_underscores}.pkg"
   name 'BankID'
   homepage 'https://install.bankid.com/'
   license :gratis
 
-  container type: :naked
-
-  pkg "BankID_installation_#{version}.pkg"
+  pkg "BankID_installation_#{version.dots_to_underscores}.pkg"
 
   uninstall pkgutil: 'com.bankid.bankid.BankID.pkg'
 end
