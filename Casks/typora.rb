dca54218a447867f780fcf79a165e74ab9b09cbd
RoCky Wu <rwu@cepave.com>
2016-08-02 06:47:50 +0800

    Fixed typora version to latest

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,5 +1,5 @@
 cask 'typora' do
-  version '0.9.9.5.1'
+  version 'latest'
   sha256 '75fcc47ff7078f728c0b51422106b5aa0661dc579e0e5b7fb6c2f323283e2d06'
 
   url "https://www.typora.io/download/typora_#{version}.zip"
