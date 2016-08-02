7f9006f7e334311f5b2b3f967674eb2aba8f8da3
Jussi Tuominen <wickedsp1d3r@COLDwithin.local>
2016-08-02 17:05:26 +0300

    updated swinsian (1.12.0)

--- Casks/swinsian.rb
+++ Casks/swinsian.rb
@@ -1,10 +1,10 @@
 cask 'swinsian' do
-  version '1.11.8'
-  sha256 'aead098039dffaeabb9a88367ee5f60366c09b6e09a20e3d1e41d5a43e0a0d31'
+  version '1.12.0'
+  sha256 '733e2579d6a8da3ac096091de7885d60825cd1d5eb973d17b894fa93b53315ce'
 
   url "https://www.swinsian.com/sparkle/Swinsian_#{version}.zip"
   appcast 'https://www.swinsian.com/sparkle/sparklecast.xml',
-          checkpoint: '97e2fd4d039255f95503061d36a36c6df370374e8f6fc2b60bcd3d05458b5191'
+          checkpoint: '7b33fa250c9bcbf5c0d8d7df4be44624d9afb45b3e7bd31e31d4f38d15f2843d'
   name 'Swinsian'
   homepage 'https://swinsian.com'
   license :commercial
