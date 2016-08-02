bb020a5f5fdf649846321ec82db1dd673c3a8862
Markus Reiter <me@reitermark.us>
2016-08-02 12:50:24 +0200

    Auto-detect nested containers.

--- lib/hbc/container/zip.rb
+++ lib/hbc/container/zip.rb
@@ -4,6 +4,23 @@ class Hbc::Container::Zip < Hbc::Container::Base
   end
 
   def extract
-    @command.run!("/usr/bin/ditto", args: ["-x", "-k", "--", @path, @cask.staged_path])
+    Dir.mktmpdir do |unpack_dir|
+      @command.run!("/usr/bin/ditto", args: ["-x", "-k", "--", @path, unpack_dir])
+
+      children = Pathname.new(unpack_dir).children
+
+      nested_container = children[0]
+
+      unless children.count == 1 &&
+             !nested_container.directory? &&
+             @cask.artifacts[:nested_container].empty? &&
+             extract_nested(nested_container)
+        children.each do |src|
+          dest = @cask.staged_path.join(src.relative_path_from(Pathname.new(unpack_dir)))
+          FileUtils.rm_r(dest) if dest.exist?
+          FileUtils.mv(src, dest, force: true)
+        end
+      end
+    end
   end
 end
