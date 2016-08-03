3d150c913c4331264e497bd72da93c91f11ee817
Eduardo Sebastian <esebastian@altiora.es>
2016-08-03 17:20:13 +0200

    updated polymail (1.03)

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,10 +1,10 @@
 cask 'polymail' do
-  version '1.02.11'
-  sha256 '98de9e8c5c5aa31cf70e13101a46fa14550e71f75eab234116ccdd6e3e744d4c'
+  version '1.03'
+  sha256 '7cabd2294bd4dfb3f85a1684f6cf657ad530f4d2d3210697ca4405dcaf9f0495'
 
   url "https://sparkle-updater.polymail.io/osx/builds/Polymail-v#{version.major_minor.no_dots}.zip"
   appcast 'https://sparkle-updater.polymail.io/cast.xml',
-          checkpoint: '4f1137789b56fab835c88e48f0ceb8afb8f37078bfb1f87eaf19fa881a474a64'
+          checkpoint: '19115bcd562746116c6e618190073d0233c80f65990665b0c2912aa672b8bc20'
   name 'Polymail'
   homepage 'https://polymail.io/'
   license :closed
