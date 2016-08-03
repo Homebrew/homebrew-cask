1b374c0e50881477c78530922cf390fc2309bb28
Chuah Chee Shian <shian48263@gmail.com>
2016-08-02 22:44:35 -0500

    solved logitech-options Error: pkg source file not found (#23386)

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -9,7 +9,7 @@ cask 'logitech-options' do
 
   depends_on macos: '>= :mavericks'
 
-  pkg 'LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg'
+  pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
 
   uninstall script:  {
                        executable: '/Applications/Utilities/Logitech Options Uninstaller.app/Contents/Resources/Uninstaller',
