52baf755de339a64b3ff0192a8b54af8d27c1d93
David Guyon <dguyon@gmail.com>
2016-08-03 11:43:36 +0200

    Add Molotov to v0.9.3

---
+++ Casks/molotov.rb
@@ -0,0 +1,11 @@
+cask 'molotov' do
+  version '0.9.3'
+  sha256 '5063a62f8a7fe6a92c7fec9d739f5e0dd7516bb499cb0e109b22ef8f6915d438'
+
+  url "http://desktop-auto-upgrade.molotov.tv/mac/Molotov-v#{version}.dmg"
+  name 'Molotov'
+  homepage 'http://www.molotov.tv/'
+  license :unknown
+
+  app 'Molotov.app'
+end
