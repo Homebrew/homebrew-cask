229698de86995d2e13dc432dfec1b80b0382103a
Patryk Soika <patryk@superrenaissanceman.com>
2016-08-04 19:19:59 -0400

    updated telegram (2.19-48910)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,10 +1,10 @@
 cask 'telegram' do
-  version '2.18-48803'
-  sha256 'c9dcf0c5b25e90d9dccbc3b47fc9f1c03cc18937ebb4a47e75a5c1c8227e17cc'
+  version '2.19-48910'
+  sha256 'b59451effb2c0dbd599e03f21f27d8b31786b6f532ba9a4cbb1cf663f45ccb04'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'http://osx.telegram.org/updates/versions.xml',
-          checkpoint: 'f314d17ab2620e4595f67ffaaa708d6c93e385cc2b8c8ba5a92c6eb602644557'
+          checkpoint: '2e48e6b684fac637e997175a549dac1c214ddd11c4251f7213258b1d2476506c'
   name 'Telegram for macOS'
   homepage 'https://macos.telegram.org'
   license :gpl
