527056c890d4b26874f1cc9d2fbca06338763c0c
Sebastian <sebastian@yo.lk>
2016-08-04 23:53:59 +0200

    Add dynamite v3.2.2
    Controls your mite timers directly from your menu bar.
    https://www.mediaatelier.com/DynaMite3/

---
+++ Casks/dynamite.rb
@@ -0,0 +1,13 @@
+cask 'dynamite' do
+  version '3.2.2'
+  sha256 'e1ac339c37844bcbe82f9cc1718aa7cd7c7232d36761dbf3a0a84d500899a06c'
+
+  url "https://mediaatelier.com/DynaMite3/DynaMite_#{version}.zip"
+  name 'DynaMite'
+  homepage 'https://www.mediaatelier.com/DynaMite3/'
+  license :commercial
+
+  depends_on macos: '>= :mavericks'
+
+  app 'DynaMite.app'
+end
