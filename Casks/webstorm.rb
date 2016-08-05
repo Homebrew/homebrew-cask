40db0ab0debebfa03d05b2b01147a6eec134006f
Justin Hall <justin.r.hall@gmail.com>
2016-08-05 17:51:59 -0600

    Update webstorm to 2016.2.1 (#23489)

--- Casks/webstorm.rb
+++ Casks/webstorm.rb
@@ -1,6 +1,6 @@
 cask 'webstorm' do
-  version '2016.2'
-  sha256 '1023408ea1cffe4af984d651ae2d10fb514410b1d4baec6bbca68ae87fef7184'
+  version '2016.2.1'
+  sha256 '46de59244f97e6cbe9c378cd11402fcf0fbf41b2f7e12a1f877894c6d1d8f820'
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version}.dmg"
   name 'WebStorm'
