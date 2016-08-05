90f3c439e67b7d527c8ad912ae0d7547d47e1716
Joshua Hagins <jhagins@palantir.com>
2016-08-05 00:42:03 -0400

    updated ocenaudio (latest)

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -2,7 +2,7 @@ cask 'ocenaudio' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.ocenaudio.com.br/downloads/ocenaudio64.dmg'
+  url 'https://www.ocenaudio.com/downloads/ocenaudio64.dmg'
   name 'ocenaudio'
   homepage 'https://www.ocenaudio.com/en'
   license :gratis
