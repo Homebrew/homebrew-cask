dbbceb61bb158c15d48ef1fac816ce77238c937e
Andrew Thauer <andrewthauer@users.noreply.github.com>
2016-08-05 11:33:53 -0400

    updated postico (1.0.9) (#23470)

--- Casks/postico.rb
+++ Casks/postico.rb
@@ -1,6 +1,6 @@
 cask 'postico' do
-  version '1.0.8'
-  sha256 '44633d2c10932eef09eb6497ef50aace82cf126efef13e079a4a5b1dd9f26181'
+  version '1.0.9'
+  sha256 '19ef3bab73b5764e327e3d62c1c11894915ab9f00019dd6e6bd4427eb18812a1'
 
   # amazonaws.com/eggerapps-downloads was verified as official when first introduced to the cask
   url "https://s3-eu-west-1.amazonaws.com/eggerapps-downloads/postico-#{version}.zip"
