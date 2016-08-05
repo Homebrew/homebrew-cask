2e4a6f06114a9df1e97abae4fbe3fb219b0df055
Viktor Szakats <vszakats@users.noreply.github.com>
2016-08-05 11:38:34 +0200

    4k-youtube-to-mp3: fix checksum, use secure url (#23460)

--- Casks/4k-youtube-to-mp3.rb
+++ Casks/4k-youtube-to-mp3.rb
@@ -1,9 +1,9 @@
 cask '4k-youtube-to-mp3' do
   # note: "3" is not a version number, but an intrinsic part of the product name
   version '3.0'
-  sha256 '29b65a99f64ba5d6a1924df989b3cc1e0b7354fface7b4cbedc02eac17f2bd33'
+  sha256 '8b077edb8a01f14c1aafe9c4a4d3eb4366a821946e18daa8441d479e82aa14d6'
 
-  url "http://downloads.4kdownload.com/app/4kyoutubetomp3_#{version}.dmg"
+  url "https://downloads.4kdownload.com/app/4kyoutubetomp3_#{version}.dmg"
   name '4K YouTube to MP3'
   homepage 'https://www.4kdownload.com/products/product-youtubetomp3'
   license :oss
