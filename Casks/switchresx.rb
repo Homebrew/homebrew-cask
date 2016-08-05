5218bee4aba80e83ec9649f897c4b3e3bca129ad
Trevor D. Miller <trevordmiller@icloud.com>
2016-08-05 17:52:37 -0600

    Update switchresx to latest 4.5.5 version (#23485)

--- Casks/switchresx.rb
+++ Casks/switchresx.rb
@@ -1,5 +1,5 @@
 cask 'switchresx' do
-  version '4.5.3'
+  version '4.5.5'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.madrau.com/data/switchresx/SwitchResX#{version.major}.zip"
