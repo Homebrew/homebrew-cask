a3540c5b4e59395144500d78840bf0317b5a15e5
Victor Popkov <victor@popkov.me>
2016-08-07 21:39:37 +0300

    updated vinoteka (3.5.1)

--- Casks/vinoteka.rb
+++ Casks/vinoteka.rb
@@ -1,10 +1,10 @@
 cask 'vinoteka' do
-  version '3.5.0'
-  sha256 '75581dc007b6bf047ea11093436ac78254de36005b58e770a9076f838056399b'
+  version '3.5.1'
+  sha256 '21b85c94f51d77e56a0dea29f619ee1a12191cfaa04c1d81b7b7113ae85cad19'
 
   url 'http://download.vinotekasoft.com/Vinoteka.zip'
   appcast 'http://download.vinotekasoft.com/vinoteka_update.xml',
-          checkpoint: '5664b44e3a2ae8d493a8a4693dde30d14430cbd193b47d2c844a30840489bc8c'
+          checkpoint: '7e65d66ceafa9deb3af5758197ede0f893b2858b1dd9272e870b64a24c887842'
   name 'Vinoteka'
   homepage 'http://www.vinotekasoft.com/'
   license :commercial
