a5b1b8323ba4f45492f4a81863555f353c0bf0b7
Viktor Szakats <vszakats@users.noreply.github.com>
2016-08-07 11:18:25 +0200

    scrivener 2.80.3 and secure url (#23517)

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -1,11 +1,11 @@
 cask 'scrivener' do
-  version '2.7'
-  sha256 '4b85b723448410e0903424749a568590c1007f324ef09d7b97b67b4d1c287214'
+  version '2.80.3'
+  sha256 '9ea0910d950b36159d5e722ebcf04e8f1fff48ad667382a01e05edbbfb02fdb0'
 
   # scrivener.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "http://scrivener.s3.amazonaws.com/mac_updates/Scrivener_106_#{version}.zip"
+  url "https://scrivener.s3.amazonaws.com/mac_updates/Scrivener_106_#{version}.zip"
   appcast 'https://www.literatureandlatte.com/downloads/scrivener-2.xml',
-          checkpoint: 'a72d55b3c4787fd7bc602a9c5bfbb36b3103faf4a97ff12cd1d987c9e065eaa0'
+          checkpoint: '79e3d31dd436e68082f47d8d3690967b250f6f810eca34b4e057f3255003cdde'
   name 'Scrivener'
   homepage 'https://literatureandlatte.com/scrivener.php'
   license :commercial
