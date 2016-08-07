ff3bbbc52553be1dd7f6b156dfcbee35e0ac7af3
Viktor Szakats <vszakats@users.noreply.github.com>
2016-08-07 10:44:47 +0200

    editready 1.4.2 and secure url

--- Casks/editready.rb
+++ Casks/editready.rb
@@ -1,8 +1,8 @@
 cask 'editready' do
-  version '1.3.8'
-  sha256 '74eda021699b29c7922ed1ddfa61556e2433df271fce1c7bc619a69b99a0d5db'
+  version '1.4.2'
+  sha256 'ed0f73788709bc68e28936f50a164727eafe7a56ce4f39ebcd8c8c784103f0ea'
 
-  url "http://www.divergentmedia.com/filedownload/editready%20#{version}.dmg"
+  url "https://www.divergentmedia.com/filedownload/editready%20#{version}.dmg"
   name 'divergent media EditReady'
   homepage 'https://www.divergentmedia.com/editready'
   license :commercial
