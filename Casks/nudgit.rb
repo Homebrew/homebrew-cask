30b1d7a92aeab9468a15912c8ebdf4c28ee7352a
Thibault Milan <thibault.milan@kpmg.lu>
2016-08-08 11:05:39 +0200

    Add Nudg.it v 1.1

---
+++ Casks/nudgit.rb
@@ -0,0 +1,11 @@
+cask 'nudgit' do
+  version '1.1'
+  sha256 '7abeabd7d7029aa20c76495b20ec3aeafff171be5a1c6d58baed90565c062776'
+
+  url 'http://nudg.it/nudg.it%201.1.zip'
+  name 'Nudg.it'
+  homepage 'http://nudg.it/'
+  license :gratis # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'nudgit 1.1/nudg.it.app'
+end
