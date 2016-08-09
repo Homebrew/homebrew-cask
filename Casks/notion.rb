20f6125db6b2269bcb454900a235ea429278fc81
Jesse Claven <jesse.claven@gmail.com>
2016-08-10 00:40:15 +0100

    Add Notion 0.1.6 cask.

---
+++ Casks/notion.rb
@@ -0,0 +1,12 @@
+cask 'notion' do
+  version '0.1.6'
+  sha256 :no_check
+
+  url 'https://www.notion.so/desktop/Notion-0.1.6.dmg'
+  name 'Notion'
+  homepage 'https://www.notion.so/'
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'Notion.app'
+end
+
