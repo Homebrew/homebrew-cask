446f1dcf444e74165fa1fadcdeda7cb04da8545f
Victor Popkov <victor@popkov.me>
2016-08-09 23:13:49 +0300

    updated atom (1.9.7)

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,11 +1,11 @@
 cask 'atom' do
-  version '1.9.6'
-  sha256 '1e799ad7b2b50081a397fb74d90c59f8b1473e3bdd11df771e0ada54ceaba57e'
+  version '1.9.7'
+  sha256 'a07e1823482c393801cb860ad3c21cdd95649c34c4bc147cf65105bd978bfee0'
 
   # github.com/atom/atom was verified as official when first introduced to the cask
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
   appcast 'https://github.com/atom/atom/releases.atom',
-          checkpoint: '2342858bafc32a24c7ccb6a314288611d543189a8eec331d023127b4fd183ab6'
+          checkpoint: 'fb7b5cbf83f562fa57ca81cd45b758e1a2d9c42469a4094e632040996c7979ac'
   name 'Github Atom'
   homepage 'https://atom.io/'
   license :mit
