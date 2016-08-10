4b3a4a1df18d628854deffb039e5e1ff220e0cb3
Alex Edelman <alexoedelman@gmail.com>
2016-08-10 14:21:45 -0500

    updated unity-ios-support-for-editor (5.4.0f3,a6d8d714de6f) (#23600)

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-ios-support-for-editor' do
-  version '5.3.5f1,960ebf59018a'
-  sha256 '61f8773248aabfd4659d2b9aebe0c31a120f82d8dbd0b18b5376aca2276a5df6'
+  version '5.4.0f3,a6d8d714de6f'
+  sha256 '334cb86bb71a0fdf92455c9bb07506f94ed81a6b9b36b6a9dd0a4dfd15ef65c8'
 
   url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity iOS Build Support'
