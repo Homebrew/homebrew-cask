14a588ffcd33caddfd4cabca01775983e0c567c8
Anne Celestino <hubwub@tagspace.me>
2016-08-10 14:20:40 -0500

    updated polymail (1.04) (#23602)

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,10 +1,10 @@
 cask 'polymail' do
-  version '1.03'
-  sha256 '7cabd2294bd4dfb3f85a1684f6cf657ad530f4d2d3210697ca4405dcaf9f0495'
+  version '1.04'
+  sha256 '4a061b3b4761ea8661c7a270d0d966b95c9e50915448b228de9d75b3a4fd17d1'
 
   url "https://sparkle-updater.polymail.io/osx/builds/Polymail-v#{version.major_minor.no_dots}.zip"
   appcast 'https://sparkle-updater.polymail.io/cast.xml',
-          checkpoint: '19115bcd562746116c6e618190073d0233c80f65990665b0c2912aa672b8bc20'
+          checkpoint: 'a8b8d11ae029aebe21d1ff3612cb6b9c6b5237df089319bdceccb951535e0cf9'
   name 'Polymail'
   homepage 'https://polymail.io/'
   license :closed
