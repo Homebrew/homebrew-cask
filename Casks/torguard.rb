78bd9baf1f6ca6fe38529c0a9abad933ef166bfd
Markus Reiter <me@reitermark.us>
2016-08-11 13:53:40 +0200

    updated torguard (0.3.50)

--- Casks/torguard.rb
+++ Casks/torguard.rb
@@ -1,11 +1,11 @@
 cask 'torguard' do
-  version '0.3.48'
-  sha256 'eb141c3c55b0f109196b8a449de5f23bbe03ffd345ebc6770b0e9fd7b31eeac8'
+  version '0.3.50'
+  sha256 '79bc2ffb6bf04412429e3dff17c1eee82e35f11845298e06e9b90e5bf305e320'
 
   # torguard.biz was verified as official when first introduced to the cask
   url "https://updates.torguard.biz/Software/MacOSX/TorGuard-v#{version}.dmg"
   appcast 'https://updates.torguard.biz/Software/MacOSX/checksums.sha256',
-          checkpoint: '1bd7449dd8fae62cb0c1dc4d1c18bd34dbe798dc7a4af5b23aa3b836a6f47770'
+          checkpoint: '990152a87c456dac5141d16f607d21128c52079affb49aaecddcce516e42cf30'
   name 'TorGuard'
   homepage 'https://torguard.net/'
   license :unknown
