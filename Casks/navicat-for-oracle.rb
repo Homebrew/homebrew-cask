301b831f36bff2205a24ed3f97ebaf87bb613847
Aditya Dalal <adidalal@users.noreply.github.com>
2016-08-10 21:54:09 -0400

    updated navicat-for-oracle (11.2.13) (#23625)

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-oracle' do
-  version '11.2.11'
-  sha256 '54c358540d855cf182d294a7dd0935b30d0e18ac8fe9fa7460525a86c0218606'
+  version '11.2.13'
+  sha256 '658d835ff94d333b189b22708e6cb5c028dc8fd28a837331bb6ca7a23d8a0faa'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
   name 'Navicat for Oracle'
