8c77d76930589bb007588c12db8557d851f77b1c
Markus Reiter <me@reitermark.us>
2016-08-11 13:04:53 +0200

    updated handbrake (0.10.5)

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -2,9 +2,9 @@ cask 'handbrake' do
   version '0.10.5'
   sha256 'a2f1cacfb508e5ebe2489c64fa15204f15578aaf5e7f8a5ae9ab1fdccde51222'
 
-  url "http://download.handbrake.fr/releases/#{version}/HandBrake-#{version}-MacOSX.6_GUI_x86_64.dmg"
+  url "http://download.handbrake.fr/handbrake/releases/#{version}/HandBrake-#{version}-MacOSX.6_GUI_x86_64.dmg"
   appcast 'https://handbrake.fr/appcast.x86_64.xml',
-          checkpoint: '4a7758883f991b677ada503d54925f0182da7b9c66557495e1bd204a859d9b03'
+          checkpoint: '7518bedc8a1da492621dd67d681e072a4071d0a0b0bb3623909dbd2a9092cc86'
   name 'HandBrake'
   homepage 'https://handbrake.fr'
   license :oss
