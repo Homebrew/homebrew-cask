3249e2afeecbb79b0421bf9bb58e591651f7469d
Markus Reiter <me@reitermark.us>
2016-08-11 16:44:53 +0200

    updated xamarin-android (6.1.2-21) (#23675)

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-android' do
-  version '6.1.1-1'
-  sha256 'c89e6a390ef9d25e932b0cb4043831912cb67a1c335f19b7ddd654c8f2846c12'
+  version '6.1.2-21'
+  sha256 'bd1c37bb2dff6ad0056351e1faef1519d30bf96a1575ce292a5da28e719fc578'
 
   url "https://download.xamarin.com/MonoforAndroid/Mac/xamarin.android-#{version}.pkg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '2cb6260bf1f5348e125fe9cab2effe9862fe76f0706f146dd91c827827cdda6b'
+          checkpoint: '773227b41218a95c3404b764d09eed828cc248aa4bb3aaabbe75c6ad95022b22'
   name 'Xamarin.Android'
   homepage 'https://xamarin.com/android'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
