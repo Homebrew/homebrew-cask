4732e6262e4b0d5d775bc770385e607429ab7928
Markus Reiter <me@reitermark.us>
2016-08-12 00:08:03 +0200

    updated devonthink-pro-office (2.9.2) (#23713)

--- Casks/devonthink-pro-office.rb
+++ Casks/devonthink-pro-office.rb
@@ -1,11 +1,11 @@
 cask 'devonthink-pro-office' do
-  version '2.9.1'
-  sha256 '93535563f2a8dab5a69a5e7d705b0f20ef3df77ea77e63b9a679ef3c730d3618'
+  version '2.9.2'
+  sha256 'a33b06bc6f504c987f708e688fa8eb9fdf2c70150950558ee6b18a9f81a2a4bc'
 
   # amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_Pro_Office.app.zip"
   appcast 'http://www.devon-technologies.com/fileadmin/templates/filemaker/sparkle.php?product=300125739&format=xml',
-          checkpoint: '81771f9d3deb54ab8d298d7cce32439296570a060e7b2bf6e0955ac0cb7807d9'
+          checkpoint: 'b210d90a515c3ccef2fd28d238f96792a764b6d72091af965df118f906995fd0'
   name 'DEVONthink Pro Office'
   homepage 'http://www.devontechnologies.com/products/devonthink/devonthink-pro-office.html'
   license :commercial
