0e12d06012dc6473f0fd392683f69dc40b6f03a2
Markus Reiter <me@reitermark.us>
2016-08-11 13:32:59 +0200

    updated rhinoceros (5.2.1) (#23650)

--- Casks/rhinoceros.rb
+++ Casks/rhinoceros.rb
@@ -1,11 +1,11 @@
 cask 'rhinoceros' do
-  version '5.2'
-  sha256 '2c2fd9553031c5f9709a9ddda188c32806f8293c30e2ac77b19d7fc34b607aa2'
+  version '5.2.1'
+  sha256 'af6c6142d6c6f3bec9d72dd9111a7150693357cf60d150d44fe5681f13a1f19f'
 
   # mcneel.com was verified as official when first introduced to the cask
   url "https://files.mcneel.com/Releases/Rhino/#{version.major}.0/Mac/Rhinoceros_#{version}.dmg"
   appcast 'https://files.mcneel.com/rhino/5.0/mac/5CcommercialUpdates.xml',
-          checkpoint: '259ba86e3ece12b204264854a73bf381e530ded1110e85bf623e7b75297d9f2e'
+          checkpoint: '91467e2ee25ef36dd48c226b54c6cdd7d39c5f0e21e7658531ff885b4cf777a3'
   name 'Rhinoceros'
   homepage 'https://www.rhino3d.com/'
   license :commercial
