e27ae06e2a648fad5483fcb1c7dfa0c7cb154160
Markus Reiter <me@reitermark.us>
2016-08-11 14:30:08 +0200

    updated querious (2.0_beta45) (#23655)
    * updated houdahgeo (4.5.2)
    
    * updated querious (2.0_beta45)

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -1,10 +1,10 @@
 cask 'querious' do
-  version '2.0_beta44'
-  sha256 '005784b7055bcf4252eb575d27e0bf799e52b829677f42e8281d44fbb5b1796c'
+  version '2.0_beta45'
+  sha256 '1b47da63c835e48a2d7b10bca0811cd3f7aad01f738057ba977226048281bcd5'
 
   url "https://store.araelium.com/querious/downloads/versions/Querious#{version}.zip"
   appcast 'https://arweb-assets.s3.amazonaws.com/downloads/querious/prerelease-updates.xml',
-          checkpoint: '2e570e2810574f79930e093f50af8682cfc7b43d1b84c0118ae1b2c9122e2fc5'
+          checkpoint: '0cc75e58d3d5232065e87f075f69ea3904d01427a4088d0bcdfa3b60bded0246'
   name 'Querious 2'
   homepage 'http://www.araelium.com/querious/'
   license :commercial
