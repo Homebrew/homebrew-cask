16aaf75814977398d049d2e52a8b854d9faf1479
Markus Reiter <me@reitermark.us>
2016-08-11 15:17:22 +0200

    updated sleep-monitor (3.0.2)

--- Casks/sleep-monitor.rb
+++ Casks/sleep-monitor.rb
@@ -1,10 +1,10 @@
 cask 'sleep-monitor' do
   version '3.0.2'
-  sha256 '673481aa7fcbf3781074e962bb41c59fdc4a18cf96f4cd9333ad231649bc95e6'
+  sha256 '1c16e85c46726bca60cbe1c3840ccd99d66af383c921d45f190c4deea1dfeafd'
 
   url 'https://www.dssw.co.uk/sleepmonitor/dsswsleepmonitor.dmg'
   appcast 'https://version.dssw.co.uk/sleepmonitor/standard',
-          checkpoint: '495f3bf7b2abb0728d57f5e1a7648e0a55d8a38f0084591c25c944401f7ce31e'
+          checkpoint: 'ce2f60062ef44a4e9ca411b903641bbc5b08568f6418a10c79f7409191fcd60f'
   name 'Sleep Monitor'
   homepage 'https://www.dssw.co.uk/sleepmonitor'
   license :commercial
