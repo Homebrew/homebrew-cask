ba766bd91896c92afefbe1ce1fd962e697b030f0
Markus Reiter <me@reitermark.us>
2016-08-11 14:29:53 +0200

    updated xamarin-mdk (4.4.2.11) (#23657)
    * updated houdahgeo (4.5.2)
    
    * updated xamarin-mdk (4.4.2.11)

--- Casks/xamarin-mdk.rb
+++ Casks/xamarin-mdk.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-mdk' do
-  version '4.4.1.0'
-  sha256 '451602bbb5f06442c4fc11d8593ebc3904a814260bba312a033ef31395459491'
+  version '4.4.2.11'
+  sha256 'd8bfbee7ae4d0d1facaf0ddfb70c0de4b1a3d94bb1b4c38e8fa4884539f54e23'
 
   url "https://download.xamarin.com/MonoFrameworkMDK/Macx86/MonoFramework-MDK-#{version}.macos10.xamarin.universal.pkg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '2cb6260bf1f5348e125fe9cab2effe9862fe76f0706f146dd91c827827cdda6b'
+          checkpoint: '773227b41218a95c3404b764d09eed828cc248aa4bb3aaabbe75c6ad95022b22'
   name 'Xamarin Mono MDK'
   homepage 'https://xamarin.com/platform'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
