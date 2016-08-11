e56a7a52a180ab42c1929f77389b42d330c025d8
Markus Reiter <me@reitermark.us>
2016-08-11 15:37:19 +0200

    updated subler (1.1.1) (#23668)

--- Casks/subler.rb
+++ Casks/subler.rb
@@ -1,11 +1,11 @@
 cask 'subler' do
-  version '1.1'
-  sha256 'adcffb181ce632c209b63e6490e44d4d518dd681f603fc0b0e0f3883cff9934a'
+  version '1.1.1'
+  sha256 '8f96512cacf3f99d17697201d43f53efd387f01a15e09d7798e6f0fdc062772d'
 
   # bitbucket.org/galad87/subler was verified as official when first introduced to the cask
   url "https://bitbucket.org/galad87/subler/downloads/Subler-#{version}.zip"
   appcast 'https://subler.org/appcast/appcast.xml',
-          checkpoint: '18085cd6bb7aee775bd4a96cab7aaa4b5029576fad0129813fa4a58ce58805d8'
+          checkpoint: 'a48a3b2a60d5baebde8a06045b79bf317c41b3796f607549b510222f4df013bb'
   name 'Subler'
   homepage 'https://subler.org/'
   license :gpl
