b76b182406a2b89ffe2ecdd1dbae7bcdf4dd3fbf
Markus Reiter <me@reitermark.us>
2016-08-11 13:30:45 +0200

    updated metabase (0.19.2)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,10 +1,10 @@
 cask 'metabase' do
-  version '0.17.1'
-  sha256 'eab08c264bfc18cc107f1a7ee690ee30c9ba9261f055cde154a06eceb08a6e35'
+  version '0.19.2'
+  sha256 '7f7db7097f5400e2592139dbc1ba86cee225102487267408ae899bd2755fbc34'
 
   url "http://downloads.metabase.com/v#{version}/Metabase.dmg"
   appcast 'http://downloads.metabase.com/appcast.xml',
-          checkpoint: '396102b87b795047a9b38507b5db87bc4bd93ddc2284f9434177c06fc073378e'
+          checkpoint: '382c2f29e728fe2d9ddf69879807f8949bc4fd3b6b93ca0863412e77a69dc1e8'
   name 'Metabase'
   homepage 'http://www.metabase.com/'
   license :affero
