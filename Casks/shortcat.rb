9121e1aba3c623a8bf485623e2e9aee4b3245584
Markus Reiter <me@reitermark.us>
2016-08-11 15:11:51 +0200

    updated shortcat (0.7.7)

--- Casks/shortcat.rb
+++ Casks/shortcat.rb
@@ -1,10 +1,10 @@
 cask 'shortcat' do
-  version '0.7.6'
-  sha256 'b78f20a94e0270ea1b0376677c07559db0e91c4ac53227b588adee46f971be1b'
+  version '0.7.7'
+  sha256 '272b7d2c4f2025e693d3ebfd8c3276e992676196040d1b79dc26ac91cee22edf'
 
   url "https://files.shortcatapp.com/v#{version}/Shortcat.zip"
-  appcast 'https://shortcatapp.com/changelog.html',
-          checkpoint: 'c63b2b590d976d5fb83ebacb974a19052c4e56d21627fdf773685bcad192816f'
+  appcast 'https://shortcatapp.com/updates/appcast.xml',
+          checkpoint: '708f387b4618c28c21a68722ee5ecaefbcbe82b9bb898ffdb74a7c1e0f6157a3'
   name 'Sproutcube Shortcat'
   homepage 'https://shortcatapp.com/'
   license :commercial
