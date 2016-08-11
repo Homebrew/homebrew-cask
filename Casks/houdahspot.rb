63c094f74e76c67b4850462b8cf4ffdc4c5fe6c8
Markus Reiter <me@reitermark.us>
2016-08-11 13:16:39 +0200

    updated houdahspot (4.2.3)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,10 +1,10 @@
 cask 'houdahspot' do
-  version '4.2.2'
-  sha256 '609e002a403b6d561e883d7901cfc585d7845ac46419faa04bcce7590a53b0d3'
+  version '4.2.3'
+  sha256 '84fd159f36012569c10ab2fb62a774b39ea32a2b4ae00634388441d28b82ce6e'
 
   url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml",
-          checkpoint: '58dd82d6c9de805828c06f22655e4ea2b8bdebddc33174a70b9e6ddfd5284bbd'
+          checkpoint: '5c99b50c655ceef16c50d5056d4bd749a89f12beb701b6057732fdaf94fe2026'
   name 'HoudahSpot'
   homepage 'https://www.houdah.com/houdahSpot/'
   license :commercial
