5cada37f7554a0598382bb61196dd017e949c86a
Markus Reiter <me@reitermark.us>
2016-08-11 13:42:12 +0200

    updated xamarin-ios (9.8.2.22)

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-ios' do
-  version '9.8.1.4'
-  sha256 '492bcc12ca2e5677bdd84eda98bcbd5132bee9d8d74bb10705f0e001f7cc1720'
+  version '9.8.2.22'
+  sha256 'f67eef6234a7e53cda3bf716e36166d5ecc226425de4b66177a5acbb1a5fab03'
 
   url "https://download.xamarin.com/MonoTouch/Mac/xamarin.ios-#{version}.pkg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '2cb6260bf1f5348e125fe9cab2effe9862fe76f0706f146dd91c827827cdda6b'
+          checkpoint: '773227b41218a95c3404b764d09eed828cc248aa4bb3aaabbe75c6ad95022b22'
   name 'Xamarin.iOS'
   homepage 'https://xamarin.com/ios'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
