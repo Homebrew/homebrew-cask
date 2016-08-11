a6b8acd3862d37f5c079947338afd100489daa87
Markus Reiter <me@reitermark.us>
2016-08-11 15:55:33 +0200

    updated wizard101 (1.10) (#23673)

--- Casks/wizard101.rb
+++ Casks/wizard101.rb
@@ -1,10 +1,10 @@
 cask 'wizard101' do
   version '1.10'
-  sha256 'ec6029ad2d20067dee3f57bb8c8e7683650656c9baa77b44db9f8eb49fe497ae'
+  sha256 '524694a96f33e98e6b63be1804c9fc2ad173fc324b1928b5d36dbcfe47ed8908'
 
   url 'http://versionec.us.wizard101.com//Wizard101.dmg'
   appcast 'http://versionec.us.wizard101.com//Wizard101.xml',
-          checkpoint: 'f752460bac62d1798d4faffc8ade196e703e3c70677427dca7f6f4b23bf358d4'
+          checkpoint: 'aad04168f49ab14661ac4ff399796d54095fcb2d91cf379affed3c8ccda273a5'
   name 'Wizard101'
   homepage 'https://www.wizard101.com/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
