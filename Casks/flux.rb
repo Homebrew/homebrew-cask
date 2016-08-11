23e203c2ac0acedaf89754e1059d9e10ac8006f1
Markus Reiter <me@reitermark.us>
2016-08-11 13:01:13 +0200

    updated flux (37.5)

--- Casks/flux.rb
+++ Casks/flux.rb
@@ -2,9 +2,9 @@ cask 'flux' do
   version '37.5'
   sha256 '082c213931a8e9594d34042372aef2f2c1baefecf9474b89fedc3aadf67ba1f8'
 
-  url 'https://justgetflux.com/mac/Flux.zip'
+  url "https://justgetflux.com/mac/Flux#{version}.zip"
   appcast 'https://justgetflux.com/mac/macflux.xml',
-          checkpoint: 'f2b14fbd27f0d4e43931c5d9826668aac4797fdc494460189aa6de1c7d892cff'
+          checkpoint: 'a83636ef44a20631d8a58ed06cbd8078b8349a15339bbe20dfd7d719a918bf89'
   name 'f.lux'
   homepage 'https://justgetflux.com/'
   license :gratis
