7d207d1cba54a937ade8c94ef68a82fdacc22930
Markus Reiter <me@reitermark.us>
2016-08-11 15:37:07 +0200

    updated soqlxplorer (2.50) (#23666)

--- Casks/soqlxplorer.rb
+++ Casks/soqlxplorer.rb
@@ -1,10 +1,10 @@
 cask 'soqlxplorer' do
-  version '2.40'
-  sha256 'c85897a120bd8d5f6afa7b7c1c1917c3b08a971135a652b21d9a52c7d7cf2641'
+  version '2.50'
+  sha256 '8267f37c6fa88c7a521830a0790d0bc528d78e07b40b048cbaa6403b9eb93455'
 
   url "http://www.pocketsoap.com/osx/soqlx/soqlXplorer_v#{version}.zip"
   appcast 'http://www.pocketsoap.com/osx/soqlx/appcast.xml',
-          checkpoint: 'd86d13c4a2e045f00bfd6e98bf78dd6ffc4a631c706d022be840178a1c83e2d2'
+          checkpoint: 'f7b6c34d4e4425891096a5d3911f9b69ff452f799721d08ced6bebf2f5125300'
   name 'SoqlXplorer'
   homepage 'http://www.pocketsoap.com/osx/soqlx/'
   license :mit
