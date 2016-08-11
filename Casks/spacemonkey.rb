cfb8725030dd0fa4e7e239bc5ab39469346acef3
Markus Reiter <me@reitermark.us>
2016-08-11 15:20:21 +0200

    updated spacemonkey (0.7.17)

--- Casks/spacemonkey.rb
+++ Casks/spacemonkey.rb
@@ -4,7 +4,7 @@ cask 'spacemonkey' do
 
   url 'http://downloads.spacemonkey.com/client/mac/latest'
   appcast 'https://rink.hockeyapp.net/api/2/apps/aa33b6780fdfc71247b2995fa47b5d7c',
-          checkpoint: 'f2b7a87fc442d49534923f392be6d2fa9b78b75fe1bafb3843835c3681cd8f39'
+          checkpoint: 'cfdb1a65ee5826ac97cb25af5d3cf55fa1af43c95ae3490f2737cd7063c3819d'
   name 'Space Monkey'
   homepage 'https://www.spacemonkey.com'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
