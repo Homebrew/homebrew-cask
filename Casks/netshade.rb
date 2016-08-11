f41b32c3385774de04e5acd112b0c7212dabbe9b
Markus Reiter <me@reitermark.us>
2016-08-11 16:54:08 +0200

    updated netshade (7.0.1)

--- Casks/netshade.rb
+++ Casks/netshade.rb
@@ -1,12 +1,12 @@
 cask 'netshade' do
-  version '6.3.1'
-  sha256 'e4cf41a28a97a58ef4c3cae97a6e5bb7e039acf740d457e0e46ae5eff8735ffb'
+  version '7.0.1'
+  sha256 '834d0010f410144714775919c6299b8f9da63e9d8d7955bde9f294fbf42e4b4e'
 
-  url 'http://raynersoftware.com/downloads/NetShade.app.zip'
-  appcast 'https://secure.raynersw.com/changelog.php?prod=ns&format=std&warnpay=0',
-          checkpoint: '7ad23be85e4561f896bdba0743e1086d6b8f7b195ce0b7cea349cd210b0ce351'
+  url "https://secure.raynersw.com/downloads/NetShade-#{version.dots_to_hyphens}.app.zip"
+  appcast 'https://secure.raynersw.com/appcast.php',
+          checkpoint: '23df6d2434d0053ad584d388ac124b9d7a89a1cf36ef9ead9cc540a64bc7ef0f'
   name 'NetShade'
-  homepage 'http://raynersoftware.com/netshade/'
+  homepage 'http://raynersw.com/netshade.php'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'NetShade.app'
