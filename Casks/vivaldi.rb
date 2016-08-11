ad3ea558d2bc6ab8b7ff4e55326cb825a72ba143
Yutaro Horikawa <yutaro.ver05+github@gmail.com>
2016-08-12 04:57:06 +0900

    update vivaldi 1.3.551.30 (#23706)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,6 +1,6 @@
 cask 'vivaldi' do
-  version '1.2.490.43'
-  sha256 'a148f149f43f1273dd4b0010e8c86184415ebee1d69728f2111c4386ac4f36c6'
+  version '1.3.551.30'
+  sha256 'aabfdf21bd29eaeae2266e6b190c53100fc1df9259ef2b6d3475548797aa06db'
 
   url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.dmg"
   name 'Vivaldi'
