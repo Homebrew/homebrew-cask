f942692ee843d2dc01d89f527ea9ff98e2aa1622
Brian Morin <bdmorin@gmail.com>
2016-08-11 15:35:09 -0500

    Fix download url in rainlender.rb Cask
    Accidentally added https - audit didn't catch it because it was already downloaded, I assume.

--- Casks/rainlender.rb
+++ Casks/rainlender.rb
@@ -2,7 +2,7 @@ cask 'rainlender' do
   version '2.13.1'
   sha256 '22fc298423a0bc9c2e3ab8b0752ba353eed2cdd38695cda24d542b138ad511a9'
 
-  url "https://http://www.rainlendar.net/download/Rainlendar-Pro-#{version}.dmg"
+  url "http://www.rainlendar.net/download/Rainlendar-Pro-#{version}.dmg"
   name 'rainlender'
   homepage 'http://www.rainlendar.net/'
   license :commercial
