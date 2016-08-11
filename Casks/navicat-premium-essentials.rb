4c42323d6f3ed4ea8a64ceee9bbe75ca480fe641
Aditya Dalal <adidalal@users.noreply.github.com>
2016-08-10 22:02:55 -0400

    updated navicat-premium-essentials (11.2.13) (#23629)

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium-essentials' do
-  version '11.2.11'
-  sha256 '556705265af0b09cb1bde5542c74dff9ea5b3bb5abfbfea040c3755b7edfaa7d'
+  version '11.2.13'
+  sha256 'ec738c5a01da5f69a24bc9c72adc08e15b746c2a3c3122035f374f9bf33b139b'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
   name 'Navicat Premium Essentials'
