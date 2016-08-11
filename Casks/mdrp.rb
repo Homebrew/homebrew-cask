34da4eb55addc7492dfc7e98196b74d80be1720d
Markus Reiter <me@reitermark.us>
2016-08-11 16:49:16 +0200

    updated mdrp (6.0.5)

--- Casks/mdrp.rb
+++ Casks/mdrp.rb
@@ -1,10 +1,10 @@
 cask 'mdrp' do
-  version '6.0.2'
-  sha256 'da63b71ef31ba9d705e6a65e3edb3cd5108dfbeb9abc8f8322c6078e4d30c9ce'
+  version '6.0.5'
+  sha256 '4a8bb95527530d144b42a857617a3d34f56a7521931fcc6a069fc75af4793cb2'
 
   url "http://www.macdvdripperpro.com/MDRP_v#{version.no_dots}.zip"
   appcast 'https://www.macdvdripperpro.com/mdrp_sparkle5.xml',
-          checkpoint: '011bc89c3f06f1dfdcfdaff97822eac93434f1cb4188391aa5b0abd7eb0c6e83'
+          checkpoint: '182bbd4878cff5cef58fdbb700f6dd0ae625aca284407d49aab56afad85f4b66'
   name 'Mac DVDRipper Pro'
   homepage 'https://www.macdvdripperpro.com/'
   license :commercial
