49d46e242a4dfd89cbcc3aed62c9f5f201d8260f
Markus Reiter <me@reitermark.us>
2016-08-12 00:52:22 +0200

    updated wordpresscom (1.4.0) (#23720)

--- Casks/wordpresscom.rb
+++ Casks/wordpresscom.rb
@@ -1,10 +1,10 @@
 cask 'wordpresscom' do
-  version '1.3.4'
-  sha256 '39c8c38a35d7e764d4ebd6e2f42c01b13549b307f476bd3dbafac55e1a08d426'
+  version '1.4.0'
+  sha256 'c1bcedae572caf6b5aaedeb9448f0ac4155b338d308f99d12ea449b9d9e43550'
 
   url "https://public-api.wordpress.com/rest/v1.1/desktop/osx/download?type=app&ref=update&version=#{version}"
   appcast 'https://public-api.wordpress.com/rest/v1.1/desktop/osx/version?compare=0.1.0&channel=stable',
-          checkpoint: '984154ebbf1cd47c12c87a4620a463050c6e42cc3046f8568502cdfe124804ab'
+          checkpoint: '67c301ee7c79b1c161c3d0a8691d267032489baf850ba8aecc4b9f5f3dcc3d87'
   name 'WordPress.com'
   homepage 'https://desktop.wordpress.com/'
   license :gpl
