f4ae30f2bc6262167ad698dbafa08ce07ecbeed6
Tushar Jain <tusharjain.champ@gmail.com>
2016-08-12 04:22:18 +0200

    Editing younity's cask for version 1.12.6 in stanza

--- Casks/younity.rb
+++ Casks/younity.rb
@@ -1,5 +1,5 @@
 cask 'younity' do
-  version :latest
+  version '1.12.6'
   sha256 :no_check
 
   url 'http://downloads.getyounity.com/files/younityInstaller.dmg'
@@ -7,7 +7,7 @@ cask 'younity' do
   homepage 'https://getyounity.com'
   license :gratis # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  pkg 'younityInstaller-1.12.6.pkg'
+  pkg 'younityInstaller-#{version}.pkg'
 
   uninstall pkgutil: 'net.entangledmedia.younity.client.YounityClient'
 end
