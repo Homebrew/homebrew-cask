48567a9f92376f683647c7bd04e73c7522db6ec7
Tushar Jain <tusharjain.champ@gmail.com>
2016-08-12 03:36:42 +0200

    Adding initial cask for Younity

---
+++ Casks/younity.rb
@@ -0,0 +1,15 @@
+cask 'younity' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://downloads.getyounity.com/files/younityInstaller.dmg'
+  name 'Younity'
+  homepage 'https://getyounity.com'
+  license :gratis # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  pkg 'younityInstaller-1.12.6.pkg'
+
+  uninstall pkgutil: 'net.entangledmedia.younity.client.YounityClient'
+
+  # app 'Younity.app'
+end
