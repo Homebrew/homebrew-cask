de99d07bc13c6f07a548b02f3d774c3e92912667
Ryan Scott Lewis <ryan@rynet.us>
2016-08-12 17:33:16 -0400

    Fix style error, make sure style and audit pass

--- Casks/whichspace.rb
+++ Casks/whichspace.rb
@@ -5,7 +5,6 @@ cask 'whichspace' do
   url "https://github.com/gechr/WhichSpace/releases/download/v#{version}/WhichSpace.app.zip"
   appcast 'https://github.com/gechr/WhichSpace/releases.atom',
           checkpoint: '100d2a27e92d419f26342e7c2389388e0812a59640d21203db0c8c81a8aa9b82'
-
   name 'WhichSpace'
   homepage 'https://github.com/gechr/WhichSpace'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
