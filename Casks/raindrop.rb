c4d805c15c0477c3243de8b95f02f77e3699b4e4
Artem Medvedev <amedvedev@outlook.com>
2016-08-12 17:33:05 +0300

    Update Raindrop (4.0.4)

--- Casks/raindrop.rb
+++ Casks/raindrop.rb
@@ -2,7 +2,7 @@ cask 'raindrop' do
   version :latest
   sha256 :no_check
 
-  url 'https://raindrop.io/releases/Raindrop.app.zip'
+  url 'https://raindrop.io/releases/mac/Raindrop.dmg'
   name 'Raindrop'
   homepage 'https://raindrop.io/'
   license :gratis
