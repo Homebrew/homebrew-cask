2f30c82a13f67f6646ec6f649b98b022b65d71c4
Muammar El Khatib <muammar@Muammars-iMac.local>
2016-08-12 19:21:59 +0200

    updated mkchromecast (0.3.1)

--- Casks/mkchromecast.rb
+++ Casks/mkchromecast.rb
@@ -1,11 +1,11 @@
 cask 'mkchromecast' do
-  version '0.3.0'
-  sha256 '5b9be6891471d7eeefeea1001a1f89b5d8d4103cd29c6aabba34ed1ca8973f15'
+  version '0.3.1'
+  sha256 '51c3fea62c07f16e9ec2b9d729c42b34f541da25927fffcaf1932c320ffb637e'
 
   # github.com/muammar/mkchromecast was verified as official when first introduced to the cask
   url "https://github.com/muammar/mkchromecast/releases/download/#{version}/mkchromecast_v#{version}.dmg"
   appcast 'https://github.com/muammar/mkchromecast/releases.atom',
-          checkpoint: '78e68174ea99b8c1bb2508fd9cdc0e611bb742d92b9927ed90b0b0ab2c6667c1'
+          checkpoint: 'f71659a2fa7e5c02fff3457cb46c5977a44a07b5dad887d24242fd49a039199c'
   name 'mkchromecast'
   homepage 'http://mkchromecast.com'
   license :mit
