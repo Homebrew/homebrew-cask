8f671644d329734ded14d61c1296cf4ec3b74eb5
Ozzie Sabina <oz@sabina.org>
2016-08-13 17:12:26 -0400

    Update makemkv (1.10.1)

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,6 +1,6 @@
 cask 'makemkv' do
-  version '1.10.0'
-  sha256 'fc0f403c24b687d741780578be50832e14b199ba5ae9119020ec997804e65dd2'
+  version '1.10.1'
+  sha256 '39e6bfb950dc2edce333f145b2e69aca26f8215004868790d0343fe45a9e4de6'
 
   url "http://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   name 'MakeMKV'
