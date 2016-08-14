3e8d52f543bd21b42739c3f54749aee312ef03e3
Tushar Jain <tusharjain.champ@gmail.com>
2016-08-14 11:36:09 +0200

    Using double quote for interpolating version of Younity cask

--- Casks/younity.rb
+++ Casks/younity.rb
@@ -7,7 +7,7 @@ cask 'younity' do
   homepage 'https://getyounity.com'
   license :gratis # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  pkg 'younityInstaller-#{version}.pkg'
+  pkg "younityInstaller-#{version}.pkg"
 
   uninstall pkgutil: 'net.entangledmedia.younity.client.YounityClient'
 end
