9bb4f229dc1a490bed9a6adb989334df7c9e3307
Victor Popkov <victor@popkov.me>
2016-08-14 21:47:24 +0300

    updated boostnote (0.6.3) (#23776)

--- Casks/boostnote.rb
+++ Casks/boostnote.rb
@@ -1,11 +1,11 @@
 cask 'boostnote' do
-  version '0.6.2'
-  sha256 '726304f6ab66bb9d9b6b8b3f83b349c6a795608a123805800775f025ce96114d'
+  version '0.6.3'
+  sha256 '3d4c30dd556d89566e660577519038efa4d9db67884531a09187c595214a5864'
 
   # github.com/BoostIO/boost-releases was verified as official when first introduced to the cask
   url "https://github.com/BoostIO/boost-releases/releases/download/v#{version}/Boostnote-mac.dmg"
   appcast 'https://github.com/BoostIO/boost-releases/releases.atom',
-          checkpoint: '2a15c71dff8e004261c7ab4459cd559db74bdbdf79d2ef3bdc913e116ea305ef'
+          checkpoint: 'd315659adf44b92b9565ace9a60f9750c0e769b2ef019bcab326b717d6029d14'
   name 'Boostnote'
   homepage 'https://b00st.io'
   license :gpl
