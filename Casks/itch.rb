c7a5f6aba627e795958a79447396846304419ccd
Victor Popkov <victor@popkov.me>
2016-08-14 22:48:47 +0300

    updated itch (18.6.1)

--- Casks/itch.rb
+++ Casks/itch.rb
@@ -1,11 +1,11 @@
 cask 'itch' do
-  version '17.6.0'
-  sha256 'f5ec3f4c9c45a4811f11b79962d84c1e12ac0c42458653c68c02051f555c6671'
+  version '18.6.1'
+  sha256 '4edf40fa7475a7b79805270d132af7448a6531879d85d8f2a4f5a73e4abbc750'
 
   # github.com/itchio/itch was verified as official when first introduced to the cask
   url "https://github.com/itchio/itch/releases/download/v#{version}/itch-mac.dmg"
   appcast 'https://github.com/itchio/itch/releases.atom',
-          checkpoint: 'e2d69aeae8b736debddab73e78dcef0e4af45db40fefbc6bc3881b94dbbf6d87'
+          checkpoint: '26a87915c719ebdc7a62a82e821b0af586024887069621aefcb1896052ea44e2'
   name 'itch'
   homepage 'https://itch.io/app'
   license :mit
