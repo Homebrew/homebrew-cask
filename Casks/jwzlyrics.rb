3e74f909a60215da44ef8c6cf87d507c90fabb1e
Victor Popkov <victor@popkov.me>
2016-08-15 21:49:52 +0300

    updated jwzlyrics (1.14) (#23810)

--- Casks/jwzlyrics.rb
+++ Casks/jwzlyrics.rb
@@ -1,10 +1,10 @@
 cask 'jwzlyrics' do
-  version '1.13'
-  sha256 '33661b9ec28f38be48fcd7bb53c9b90a0d1dc4e32eafc69b1896a15c44c0b776'
+  version '1.14'
+  sha256 'a009eb1a42d6684dfb743df2d6742093d1a719367bfcd8cea56c0a6546a0018d'
 
   url "https://www.jwz.org/jwzlyrics/jwzlyrics-#{version}.zip"
   appcast 'https://www.jwz.org/jwzlyrics/updates.xml',
-          checkpoint: 'c48af683455fe3c72bc0aabc1c2e1d7dfa74c06dc5b41cb928b91709c5b01768'
+          checkpoint: 'ba186147bb99f7727d9db34e09181952613a20f607536614468eff3bee1d4b83'
   name 'jwzlyrics'
   homepage 'https://www.jwz.org/jwzlyrics/'
   license :unknown
