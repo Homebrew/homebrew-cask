fd3f0c45a807278d875a1d16cf3ddf3e11127f04
Yurii Kolesnykov <yurikoles@gmail.com>
2016-08-15 04:53:44 +0300

    updated free-download-manager (5.1.15) (#23800)

--- Casks/free-download-manager.rb
+++ Casks/free-download-manager.rb
@@ -1,8 +1,8 @@
 cask 'free-download-manager' do
-  version '5.1'
+  version '5.1.15'
   sha256 '7ad5ef681371f290bef80ef1fd1f2856e5873c252183da7066601f79d6fa45c9'
 
-  url "http://files2.freedownloadmanager.org/#{version.major}/#{version}-latest/fdm.dmg"
+  url "http://files2.freedownloadmanager.org/#{version.major}/#{version.major_minor}-latest/fdm.dmg"
   name 'Free Download Manager'
   homepage 'http://www.freedownloadmanager.org/landing5.htm'
   license :gratis
