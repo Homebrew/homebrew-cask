db17a8c4a87d5e097d738cb51de920b651192163
Victor Popkov <victor@popkov.me>
2016-08-15 21:41:58 +0300

    updated shapes (4.7.2)

--- Casks/shapes.rb
+++ Casks/shapes.rb
@@ -1,10 +1,10 @@
 cask 'shapes' do
-  version '4.7.1'
-  sha256 '748d74770eee714eb0d9fb7ac5f464bb9ffb5f5994b3a1bd7a90b3aa900ca88d'
+  version '4.7.2'
+  sha256 'a0eb7db19ce502f1a29015a922a6ff18035349c257a19cbe9536f11c93674ee9'
 
   url "http://shapesapp.com/dist/Shapes_#{version}.zip"
   appcast 'http://shapesapp.com/appcast/shapes4.rss',
-          checkpoint: '2760c553c75b46569b0d5203dec4a6f02777390de4cf495a5abba62beba5012d'
+          checkpoint: '4e3a90784913f1477df9fd594cc9a7cbd69e9b7d85873cdbe123e4ba618c4cb6'
   name 'Shapes'
   homepage 'http://shapesapp.com'
   license :commercial
