493ac16d3ee75f1e9b2a7e75a3df558b567d2195
optikfluffel <optik@fluffel.io>
2016-08-15 22:42:47 +0200

    updated etcher (1.0.0-beta.13)

--- Casks/etcher.rb
+++ Casks/etcher.rb
@@ -1,6 +1,6 @@
 cask 'etcher' do
-  version '1.0.0-beta.12'
-  sha256 '929d1ab9bfe7d301e03d0ee6933805f1366e6008f31b06ed1f3e95fb1c146a17'
+  version '1.0.0-beta.13'
+  sha256 '0a7b9f7b8b2a21478957e2815f430444285736761652f9b3c7f4fbefea7db356'
 
   # resin-production-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://resin-production-downloads.s3.amazonaws.com/etcher/#{version}/Etcher-darwin-x64.dmg"
