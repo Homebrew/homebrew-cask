b5d79a3b913090fde43e7f8d3c76744085c20eba
Dylan Arbour <dylan.arbour@gmail.com>
2016-08-15 18:04:15 -0400

    Update Logitech Gaming Software to 8.83.55

--- Casks/logitech-gaming-software.rb
+++ Casks/logitech-gaming-software.rb
@@ -1,6 +1,6 @@
 cask 'logitech-gaming-software' do
-  version '8.79.45'
-  sha256 '7385e438a7bf3956f05e8420b723b5e1a0b0502c308b075419dde5552c593f55'
+  version '8.83.55'
+  sha256 '74da2cd277651ba28a591374d3076842d16d464cf8665598bba5fd271355effc'
 
   url "http://download01.logitech.com/web/ftp/pub/techsupport/gaming/LogitechSetup_#{version}.zip"
   name 'Logitech Gaming Software'
