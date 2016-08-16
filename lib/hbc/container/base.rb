f3790b417fe4e84c546c70266514362686d307ad
Markus Reiter <me@reitermark.us>
2016-08-17 01:20:25 +0200

    Refactor nested containers.

--- lib/hbc/container/base.rb
+++ lib/hbc/container/base.rb
@@ -6,7 +6,25 @@ class Hbc::Container::Base
     @nested = nested
   end
 
-  def extract_nested(source)
+  def extract_nested_inside(dir)
+    children = Pathname.new(dir).children
+
+    nested_container = children[0]
+
+    unless children.count == 1 &&
+           !nested_container.directory? &&
+           @cask.artifacts[:nested_container].empty? &&
+           extract_nested_container(nested_container)
+
+      children.each do |src|
+        dest = @cask.staged_path.join(src.basename)
+        FileUtils.rm_r(dest) if dest.exist?
+        FileUtils.mv(src, dest)
+      end
+    end
+  end
+
+  def extract_nested_container(source)
     container = Hbc::Container.for_path(source, @command)
 
     return false unless container
--- lib/hbc/container/bzip2.rb
+++ lib/hbc/container/bzip2.rb
@@ -12,13 +12,7 @@ class Hbc::Container::Bzip2 < Hbc::Container::Base
       @command.run!("/usr/bin/ditto",   args: ["--", @path, unpack_dir])
       @command.run!("/usr/bin/bunzip2", args: ["--quiet", "--", Pathname.new(unpack_dir).join(@path.basename)])
 
-      src = Pathname.new(unpack_dir).children[0]
-
-      unless @cask.artifacts[:nested_container].empty? &&
-             extract_nested(src)
-        dest = @cask.staged_path.join(src.relative_path_from(Pathname.new(unpack_dir)))
-        FileUtils.mv(src, dest, force: true)
-      end
+      extract_nested_inside(unpack_dir)
     end
   end
 end
