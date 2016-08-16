c73256f7aaad333e4a663fcc7b12f4b12c24747b
Victor Popkov <victor@popkov.me>
2016-08-16 21:59:34 +0300

    updated powerphotos (1.1.9) (#23839)

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -1,11 +1,11 @@
 cask 'powerphotos' do
-  version '1.1.8'
-  sha256 '6996bd3834f9275d16d141bc12cefa83be85487a513f3b6b424f91a7e0e3ff7a'
+  version '1.1.9'
+  sha256 '2517c6911789a816ae28d6986d7cdce57fe3341853f2200e1a13e02f0e9424c5'
 
   # s3.amazonaws.com/fatcatsoftware/powerphotos was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/fatcatsoftware/powerphotos/PowerPhotos_#{version.no_dots}.zip"
   appcast 'https://www.fatcatsoftware.com/powerphotos/powerphotos_appcast.xml',
-          checkpoint: '8e20c09bbffb0e07d275479375bc9ad7d851eec1377d397074bb6f427585e4ad'
+          checkpoint: '844a4522eac5692df0422d57288d6fb852d0e31a064041089e1416f240820a94'
   name 'PowerPhotos'
   homepage 'https://www.fatcatsoftware.com/powerphotos/'
   license :commercial
