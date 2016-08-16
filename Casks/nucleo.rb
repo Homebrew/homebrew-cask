40e3ee18583144951c8903ecdd8534bc4112ef1e
Aditya Dalal <adidalal@users.noreply.github.com>
2016-08-15 21:29:36 -0400

    updated nucleo (1.0.3) (#23820)

--- Casks/nucleo.rb
+++ Casks/nucleo.rb
@@ -1,9 +1,9 @@
 cask 'nucleo' do
-  version '1.0.3,26'
+  version '1.0.3'
   sha256 'e550489f76166fddf5e9267823d01560d00df4abce960541f42252caea2ff8b7'
 
   # dl.devmate.com was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/co.ambercreative.Nucleoapp/#{version.after_comma}/1467733372/Nucleo-#{version.after_comma}.zip"
+  url 'https://dl.devmate.com/co.ambercreative.Nucleoapp/Nucleo.zip'
   appcast 'https://updates.devmate.com/co.ambercreative.Nucleoapp.xml',
           checkpoint: 'c895bfe129830eb1d85bc6bd0727c321eda667aea63a116222dbd3f242815a3d'
   name 'Nucleo'
