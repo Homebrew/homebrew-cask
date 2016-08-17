4c10b3d03b5fe23f7727359156cf26d67f0a5688
Gregory Pakosz <gregory.pakosz@gmail.com>
2016-08-17 14:12:32 +0200

    updated virtualbox (5.1.4-110228) (#23864)

--- Casks/virtualbox-extension-pack.rb
+++ Casks/virtualbox-extension-pack.rb
@@ -6,8 +6,8 @@ cask 'virtualbox-extension-pack' do
     version '5.0.26-108824'
     sha256 '2f2302c7ba3d00a1258fe8e7767a6eb08dccdc3c31f6e3eeb74063c2c268b104'
   else
-    version '5.1.2-108956'
-    sha256 '394d933be4022b2b15225670fe6e2a4486f34e5a565a719e0de5984089ccd975'
+    version '5.1.4-110228'
+    sha256 '9462ff1b567c37ad9a33c0c7ca1925776615ec89b5a72563f29a8cc8514cf316'
   end
 
   url "http://download.virtualbox.org/virtualbox/#{version.sub(%r{-.*}, '')}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
