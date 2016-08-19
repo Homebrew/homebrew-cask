960a269990bc0e46facf630e9e7014ef27621065
Victor Popkov <victor@popkov.me>
2016-08-19 22:47:07 +0300

    updated texshop (3.68) (#23927)

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,10 +1,10 @@
 cask 'texshop' do
-  version '3.66'
-  sha256 '14e058a114568491d0da7bb94e4bb5b906bc570e6102e1b82547886bed95e710'
+  version '3.68'
+  sha256 '32fd24cdd544b7cd1c72403fefaba24483388fe180283e63e290803bf08e75fc'
 
   url "http://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.sub('.', '')}.zip"
   appcast 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml',
-          checkpoint: 'aed778ab781bd3a5906f983f8b8c06a5cfad95e0fbbc4bccb7eacc5bb30d7585'
+          checkpoint: '5d86649f1b35d11de653458f126b161afd828614d728554dc195f1eee66b8442'
   name 'TeXShop'
   homepage 'http://pages.uoregon.edu/koch/texshop'
   license :gpl
