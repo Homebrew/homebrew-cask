70bb541276a5000f80191abdd84d3cdc770fd863
Victor Popkov <victor@popkov.me>
2016-08-20 16:20:02 +0300

    screaming-frog-seo-spider.rb: fix sha256 checksum

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -4,7 +4,7 @@ cask 'screaming-frog-seo-spider' do
     sha256 'f37a517cb1ddb13a0621ae2ef98eba148027b3a2b5ce56b6e6b4ca756e40329b'
   else
     version '6.1'
-    sha256 'c5c1aa22d3074249f0285b72cb2e9196ffb0dbc462632433d5b323b6d907ab70'
+    sha256 '4ac1924c2be29bac37b730ba0a406e7d5367a07a72e2dfc9f0ac02b7db879b06'
   end
 
   url "https://www.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}.dmg"
