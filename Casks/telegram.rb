5e20963a728725f3eda884b416300a2721c3730d
friendimaginary <patryk@superrenaissanceman.com>
2016-08-21 15:17:02 -0400

    updated telegram (2.22-50247) (#23958)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,10 +1,10 @@
 cask 'telegram' do
   version '2.22-50247'
-  sha256 'a18b39ec44ab64a1e14fd9c9cdf96001672c71059d41ae78b764bf4828969c1e'
+  sha256 '9b301d2963cafc4673d2956c069e7c4ad7286e4e6b6bb3e5314490cea89cf439'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml',
-          checkpoint: '0ff8543aa9f74b9c07e6eda9edd816c204ecad2d35812f7bcc7398d4591c7da5'
+          checkpoint: '3449c4ddba2c2140f4e7614b26ca2e4a841a90083e0a5beda9e956a432783607'
   name 'Telegram for macOS'
   homepage 'https://macos.telegram.org'
   license :gpl
