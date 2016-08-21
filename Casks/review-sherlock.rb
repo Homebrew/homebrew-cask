a5cf8e9e74fe62070d30f400d70d35626b5b9a1c
Rbcafe <Webmaster@rbcafe.com>
2016-08-21 19:10:32 +0200

    Update review-sherlock.rb (#23952)

--- Casks/review-sherlock.rb
+++ Casks/review-sherlock.rb
@@ -2,9 +2,9 @@ cask 'review-sherlock' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.rbcafe.com/download/reviewsherlock.zip'
+  url 'http://www.rbcafe.com/download/reviewsherlock.zip'
   name 'Review Sherlock'
-  homepage 'https://www.rbcafe.com/softwares/review-sherlock/'
+  homepage 'http://www.rbcafe.com/softwares/review-sherlock/'
   license :commercial
 
   depends_on macos: '>= :mountain_lion'
