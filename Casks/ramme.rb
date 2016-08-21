5e3ed117469fbf55f25f3f6a6d6657c6939590b1
Terkel Gjervig <terkel@terkel.com>
2016-08-21 19:01:43 +0200

    Add Ramme.app v1.0 Unofficial Instagram Desktop Client build with Electron.

---
+++ Casks/ramme.rb
@@ -0,0 +1,11 @@
+cask 'ramme' do
+  version '1.0.0'
+  sha256 '1101f9a671547192580c5898b1a22e07ff0c143fb7934d1e53c4a2d587317e38'
+
+  url 'https://github.com/terkelg/ramme/releases/download/1.0.0/Ramme-osx-1.0.0.zip'
+  name 'Ramme'
+  homepage 'https://github.com/terkelg/ramme/'
+  license :mit
+
+  app 'Ramme.app'
+end
