f1721ec287b2b187f34446bf1179b94dd5151355
Victor Popkov <victor@popkov.me>
2016-08-23 23:46:49 +0300

    updated the-hit-list (1.1.26,345)

--- Casks/the-hit-list.rb
+++ Casks/the-hit-list.rb
@@ -1,10 +1,10 @@
 cask 'the-hit-list' do
-  version '1.1.25,339'
-  sha256 '4fd622f27700ad5fad591fa6d8d3a5f4ecd9a190d0f20e2466f6640bdca73ac9'
+  version '1.1.26,345'
+  sha256 'a3b843d6156456330fce9746064996885b021cd4a876f0e6230c3e0ee5766ff0'
 
   url "https://distrib.karelia.com/downloads/TheHitList-#{version.after_comma}.zip"
   appcast 'https://launch.karelia.com/appcast.php?product=9&appname=The+Hit+List',
-          checkpoint: '304ba77de025071738623905825ea8f0e4a07221a2449a9672daa38e660cadb1'
+          checkpoint: '484546055de385df594a995cb3738b178f95a28a0da2a00b02c5a81612507b5c'
   name 'The Hit List'
   homepage 'https://www.karelia.com/products/the-hit-list/mac.html'
   license :commercial
