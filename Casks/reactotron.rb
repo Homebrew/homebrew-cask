3d890b86b51bfe5201c005d6b491286048151682
Jakub Synowiec <jakub.synowiec@gmail.com>
2016-08-24 13:32:58 +0200

    Add Reactotron.app v1.0.0
    Reactotron is an OS X application for inspecting your React JS and React Native apps.

---
+++ Casks/reactotron.rb
@@ -0,0 +1,15 @@
+cask 'reactotron' do
+  version '1.0.0'
+  sha256 '31ef4f1291c5d05090cf2dd493da4dcfa081cb462a7f5d2774d287ae1b0436cb'
+
+  url "https://github.com/reactotron/reactotron/releases/download/v#{version}/Reactotron.app.zip"
+  name 'Reactotron'
+  homepage 'https://github.com/reactotron/reactotron'
+  license :mit
+
+  app 'Reactotron.app'
+
+  zap delete: [
+                '~/Library/Application Support/Reactotron'
+              ]
+end
