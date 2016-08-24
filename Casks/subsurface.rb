5d116a3885f5ff0f29716144491973f4a48cfb88
pseudoku <decrement@users.noreply.github.com>
2016-08-24 11:19:50 -0700

    updated subsurface (4.5.6)

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,6 +1,6 @@
 cask 'subsurface' do
-  version '4.5.3'
-  sha256 '498629d001cefc6b54c4b604a9fdebb4cff33bef457df509566d07c99fbfcc3a'
+  version '4.5.6'
+  sha256 '9c56465cbfcfe28b23d3c05a168d46c974731c06023a3dd9b08bae6a8b0ae50c'
 
   url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
   name 'Subsurface'
