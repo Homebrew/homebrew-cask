2d1d63794fc515da7a4c1a99667a4c1861f1f331
Victor Popkov <victor@popkov.me>
2016-08-25 23:53:25 +0300

    updated synalyze-it-pro (1.17)

--- Casks/synalyze-it-pro.rb
+++ Casks/synalyze-it-pro.rb
@@ -1,11 +1,11 @@
 cask 'synalyze-it-pro' do
-  version '1.16'
-  sha256 'e134b88cad3f07441307bae72c6abe386118769d614c0df3f0b6ccb51233c8de'
+  version '1.17'
+  sha256 '14e589698e9086174cb0019340fd9fecb550b538d701e342f06d387f8ddd7bb3'
 
   # synalyze-it.com/Downloads was verified as official when first introduced to the cask
   url "https://www.synalyze-it.com/Downloads/SynalyzeItProTA_#{version}.zip"
   appcast 'https://www.synalyze-it.com/SynalyzeItPro/appcast.xml',
-          checkpoint: '5c226dbef95724d894242452b0bb521f5b1128739d6cbf064aea687b32187f94'
+          checkpoint: 'd80e558a73b1568aec8f48ee91c9a438209c6bf84d64eda684c03569caa7d5a7'
   name 'Synalyze It! Pro'
   homepage 'https://www.synalysis.net/'
   license :commercial
