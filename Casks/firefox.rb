1ca663ffb8b1ba02a938adc0c7638df8819693c6
Jussi T <wickedsp1d3r@users.noreply.github.com>
2016-08-25 17:36:55 +0300

    updated firefox (48.0.2) (#24046)

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,10 +1,10 @@
 cask 'firefox' do
-  version '48.0.1'
-  sha256 '535b8eff6ad4fd94e383ec61b961735c84b9c351f97c62c7518300a9a433c3a3'
+  version '48.0.2'
+  sha256 '9328b33d62a2e95ffd2cc04ca0271c5cf137bc9a09860c527e8608c6ec715445'
 
   url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
   appcast "https://aus5.mozilla.org/update/3/Firefox/#{version}/0/Darwin_x86_64-gcc3-u-i386-x86_64/en-US/release/Darwin%2015.3.0/default/default/update.xml?force=1",
-          checkpoint: 'c8682bea4b5db86382a1f039712c45c6d95493c66e950d723edc1b6106a20621'
+          checkpoint: '9585e7dd7d365afd92f4514fc02f9ca4532ece60d44d04251426ecd1094bd6c5'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/'
   license :mpl
