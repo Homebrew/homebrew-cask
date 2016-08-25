7b168b15db2e91c9083a0582b6c4d125b8a8f47c
Mehdi Zakaria Benadel <mehdi.benadel@gmail.com>
2016-08-25 10:42:27 +0200

    Update docker-toolbox to 1.12.1 (docker-toolbox.rb) (#24016)

--- Casks/docker-toolbox.rb
+++ Casks/docker-toolbox.rb
@@ -1,11 +1,11 @@
 cask 'docker-toolbox' do
-  version '1.12.0'
-  sha256 'b06cfd858b805dd5e2bfd88cf780953ba6f326d69c4e689ad69c6d49e6cc694f'
+  version '1.12.1'
+  sha256 '43b8073295bb8c9c81963bbd3cb7f8821dc2139c22f5a26deb979a93519e0b2b'
 
   # github.com/docker/toolbox was verified as official when first introduced to the cask
   url "https://github.com/docker/toolbox/releases/download/v#{version}/DockerToolbox-#{version}.pkg"
   appcast 'https://github.com/docker/toolbox/releases.atom',
-          checkpoint: '8f110f6dffb3a494ec8b037bbab4c761c56879a2364c6b52cd1c8ffdd8551b72'
+          checkpoint: '0fbea9f72534c0621f9ec6559776c324c4cf333c3403f1a77da426f8cd35f68d'
   name 'Docker Toolbox'
   homepage 'https://www.docker.com/toolbox'
   license :apache
