96accd85bef30d48b66955d99213608a0124c4d9
Numbermaniac <Numbermaniac@users.noreply.github.com>
2016-08-25 14:01:14 +1000

    Update vlc.rb
    add zap

--- Casks/vlc.rb
+++ Casks/vlc.rb
@@ -27,6 +27,7 @@ cask 'vlc' do
   zap delete: [
                 '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.videolan.vlc.sfl',
                 '~/Library/Application Support/org.videolan.vlc',
+                '~/Library/Application Support/VLC',
                 '~/Library/Preferences/org.videolan.vlc',
                 '~/Library/Preferences/org.videolan.vlc.plist',
                 '~/Library/Saved Application State/org.videolan.vlc.savedState',
