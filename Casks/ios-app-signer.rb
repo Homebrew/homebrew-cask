a7553b36b0858a1cdef0166dbea425a1c676465c
Jordan Bondo <jordanbondo@icloud.com>
2016-08-26 08:19:29 -0700

    Added #{version} to URL stanza

--- Casks/ios-app-signer.rb
+++ Casks/ios-app-signer.rb
@@ -3,7 +3,7 @@ cask 'ios-app-signer' do
   sha256 '5ae5e9ad1a5d97c858fdeda06ae7eb764f5abf1df75a7a8e0028c49689de8877'
 
   # github.com/DanTheMan827/ios-app-signer was verified as official when first introduced to the cask
-  url 'https://github.com/DanTheMan827/ios-app-signer/releases/download/1.8.2/iOS.App.Signer.app.zip'
+  url "https://github.com/DanTheMan827/ios-app-signer/releases/download/#{version}/iOS.App.Signer.app.zip"
   name 'iOS App Signer'
   homepage 'http://dantheman827.github.io/ios-app-signer/'
   license :unknown
