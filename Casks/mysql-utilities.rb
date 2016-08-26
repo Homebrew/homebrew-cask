3a79804000146d388186adb2405490a4955a9fa3
Brian Menges <mengesb@gmail.com>
2016-08-26 13:13:45 -0700

    mysql-utilities-1.6.4 - Addresses [https://github.com/caskroom/homebrew-cask/issues/24076](caskroom/homebrew-cask/issues/24076)

--- Casks/mysql-utilities.rb
+++ Casks/mysql-utilities.rb
@@ -1,13 +1,13 @@
 cask 'mysql-utilities' do
-  version '1.5.6'
-  sha256 '2553ae12323abe28ab3161ebdb6aff991fbb738dc6ef03946c0fae86aadac230'
+  version '1.6.4'
+  sha256 '31253329cbecefd376da59f4a6c0c7f71200c323585dedf8320f008cb41e1265'
 
-  url "http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities-#{version}-osx10.9.dmg"
+  url "http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities-#{version}-osx10.11.dmg"
   name 'MySQL Utilities'
   homepage 'https://dev.mysql.com/downloads/utilities/'
   license :gpl
 
-  depends_on macos: '<= :yosemite'
+  depends_on macos: '<= :elcapitan'
 
   pkg "mysql-utilities-#{version}.pkg"
 
