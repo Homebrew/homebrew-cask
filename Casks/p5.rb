400e55985a5ce9b7925c24ee34c2c094494e7740
Mert Akengin <mert@n0pe.me>
2016-08-26 04:33:57 +0300

    updated p5 (0.6.1)

--- Casks/p5.rb
+++ Casks/p5.rb
@@ -1,12 +1,12 @@
 cask 'p5' do
   # note: "5" is not a version number, but an intrinsic part of the product name
-  version '0.6.0'
-  sha256 '0422a61a0b24a3ba3ac03c89e7b57cf1de58be5cbd9163d673a81882bc1cae58'
+  version '0.6.1'
+  sha256 '608d54e6e18aee78b6292fdd56d7968821cb6b5a23567df9f452f9ce7e16231c'
 
   # github.com/processing/p5.js-editor was verified as official when first introduced to the cask
   url "https://github.com/processing/p5.js-editor/releases/download/v#{version}/p5-mac.zip"
   appcast 'https://github.com/processing/p5.js-editor/releases.atom',
-          checkpoint: '06236c5ce5c6731b52368f4d7263eff97624f9c102781805daa202a7f3b46a94'
+          checkpoint: '219df1fa9cba52c93906de9018afc7657c809190a9abda6c0715a7aea28fa099'
   name 'p5.js Editor'
   homepage 'https://p5js.org/download/#editor'
   license :mit
