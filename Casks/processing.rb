495f2a86cb488db80d6ebbf658ebcc8b918d537f
Mert Akengin <mert@n0pe.me>
2016-08-27 20:54:52 +0300

    updated processing (3.2.1)

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -1,10 +1,10 @@
 cask 'processing' do
-  version '3.2'
-  sha256 'f6e94847b9be044efec353c64857a2534bc8e40e2f40f96354c0bcb8f8541cfd'
+  version '3.2.1'
+  sha256 '5fd2310a39da1791a7a3b2a91f7cb8239dd575f1993380df1a2143cc8a134ca6'
 
   url "http://download.processing.org/processing-#{version}-macosx.zip"
   appcast 'https://github.com/processing/processing/releases.atom',
-          checkpoint: '1ba288228624b5994069e28efc5c2ae46ea3f0bd8c8ac21167412933d355f92c'
+          checkpoint: '02ad612ff327c10b61eda7c06329a9ba5d80db902ba673131ecce4c6d1505b88'
   name 'Processing'
   homepage 'https://processing.org/'
   license :gpl
