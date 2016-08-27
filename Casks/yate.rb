79202866830b0a9f394b8a18bad7cf104cf5e792
pseudoku <decrement@users.noreply.github.com>
2016-08-26 18:22:40 -0700

    updated yate (3.13.0.2) (#24080)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,11 +1,11 @@
 cask 'yate' do
-  version '3.12'
-  sha256 '6611b12ef7336e1add86e35345aa2605fde79248c23354736a14384482a72eff'
+  version '3.13.0.2'
+  sha256 '28aafeefae414c179d1f22866865f87cfe1ff034079ba8fd8825d74d6c48d01f'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip',
-      user_agent: :fake
+      using: :post
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '50770d2e20a73b0d2cdbd4ad663650e3ee1d963431dfb23b803912df950573b8'
+          checkpoint: '454bc6ab0a129b84b251793bae9fff91de0ad0d59ab9c26027dfd096fac2b87f'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
   license :commercial
