f6af451be83425cadea16d210e7defeb29cb713b
J Ma <codejma@gmail.com>
2016-08-28 12:51:01 +1000

    Update electronic wechat to 1.4.0 (#24109)
    * Update electronic wechat to 1.4.0
    
    * fix checkpoint

--- Casks/electronic-wechat.rb
+++ Casks/electronic-wechat.rb
@@ -1,10 +1,10 @@
 cask 'electronic-wechat' do
-  version '1.3.0'
-  sha256 '103fbc03c3d12727938ddfc55fec842ada6cc959be94a55838b9bddf66abeb91'
+  version '1.4.0'
+  sha256 '332720d23feba392fc4feb3bb70416d79c88b24f79f175fa79571bf8257e0317'
 
   url "https://github.com/geeeeeeeeek/electronic-wechat/releases/download/v#{version}/mac-osx.tar.gz"
   appcast 'https://github.com/geeeeeeeeek/electronic-wechat/releases.atom',
-          checkpoint: 'e9899eac08894c445325bd0ac5195b280920157c95efcc8267aeb8316f08563f'
+          checkpoint: 'aed794ae6ae043f36a95dc23636cf9a8259e1f7702b948f26c23362b35ccceda'
   name 'Electronic WeChat'
   homepage 'https://github.com/geeeeeeeeek/electronic-wechat'
   license :mit
