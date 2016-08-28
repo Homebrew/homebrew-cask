02d7f86df7d651ee954b5dce3b7648c87a1bd1d5
Aditya Dalal <adidalal@users.noreply.github.com>
2016-08-28 18:36:49 -0400

    updated eaglefiler (1.7)

--- Casks/eaglefiler.rb
+++ Casks/eaglefiler.rb
@@ -1,5 +1,5 @@
 cask 'eaglefiler' do
-  version '1.6.9'
+  version '1.7'
   sha256 '400bac07fc8d9699814cbf7bc06b1d36742225ad59d6c12ddab6017780276f60'
 
   url "https://c-command.com/downloads/EagleFiler-#{version}.dmg"
