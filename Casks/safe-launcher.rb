8195ecb9534aa52e67275ae2cf4fc552e2268bf6
Christian Wesselhoeft <christian.wesselhoeft@gmail.com>
2016-08-27 20:50:41 -0600

    Update SAFE Launcher to 0.8.1 (#24093)

--- Casks/safe-launcher.rb
+++ Casks/safe-launcher.rb
@@ -1,18 +1,18 @@
 cask 'safe-launcher' do
-  version '0.8.0'
-  sha256 'f1ddf65d02cd4b70297b2994ae8af028647399e51cd2e029fea8dfe200a7c15b'
+  version '0.8.1'
+  sha256 'd0f009761915b8ac32718137ad774213fdd2719e022d5480fcec3caf339eeb5a'
 
   # github.com/maidsafe/safe_launcher was verified as official when first introduced to the cask
-  url "https://github.com/maidsafe/safe_launcher/releases/download/#{version}/safe_launcher-v#{version}-osx.pkg"
+  url "https://github.com/maidsafe/safe_launcher/releases/download/#{version}/safe_launcher-v#{version}-osx-x64.zip"
   appcast 'https://github.com/maidsafe/safe_launcher/releases.atom',
-          checkpoint: '064e67282ae3f5468679b75c9b0ac8b2efbbcbbbd1076231bc56f6cb277ca5b7'
+          checkpoint: 'd9786c58d9c64973c510f793d84185cb1cfeeb04c2828c65370b0ec659df27a7'
   name 'SAFE Launcher'
   homepage 'https://www.maidsafe.net/'
   license :unknown
 
-  pkg "safe_launcher-v#{version}-osx.pkg"
+  app 'SAFE Launcher.app'
 
-  uninstall pkgutil: 'com.maidsafe.safelauncher',
+  uninstall pkgutil: 'net.maidsafe.safelauncher',
             delete:  [
                        '~/Library/Application Support/SAFE Launcher/',
                        '~/Library/Application Support/SAFE Launcher Helper/',
