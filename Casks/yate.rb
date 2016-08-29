94d65e19e4768f383adcd46e76b78b3e4f152b69
Victor Popkov <victor@popkov.me>
2016-08-29 22:11:50 +0300

    updated yate (3.14) (#24142)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,11 +1,11 @@
 cask 'yate' do
-  version '3.13.0.2'
-  sha256 '28aafeefae414c179d1f22866865f87cfe1ff034079ba8fd8825d74d6c48d01f'
+  version '3.14'
+  sha256 'bb810ba3634d5d10b2a2994c1d4fa4e3826570bbe5a66dc07c2345a195562276'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip',
       using: :post
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '454bc6ab0a129b84b251793bae9fff91de0ad0d59ab9c26027dfd096fac2b87f'
+          checkpoint: 'e3617051442a1b251f1a9f5cff351eabbc3c001e637aff924a0d767311d59c27'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
   license :commercial
