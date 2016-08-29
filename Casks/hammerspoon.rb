41835762b31726652a827d23dae9bc86557cc480
Alex Guerrero <alexguerrero1092@gmail.com>
2016-08-29 19:46:53 +0200

    updated hammerspoon (0.9.47)

--- Casks/hammerspoon.rb
+++ Casks/hammerspoon.rb
@@ -1,11 +1,11 @@
 cask 'hammerspoon' do
-  version '0.9.46'
-  sha256 '20f7e81624b6f007d6fdd8944cab3d9ba48c36fd0b4f1405a590526b5d4859bc'
+  version '0.9.47'
+  sha256 '666077b428899d3e9796c8f2637d3209f822bcb52f7be796b78c2fd884557daa'
 
   # github.com/Hammerspoon/hammerspoon was verified as official when first introduced to the cask
   url "https://github.com/Hammerspoon/hammerspoon/releases/download/#{version}/Hammerspoon-#{version}.zip"
   appcast 'https://github.com/Hammerspoon/hammerspoon/releases.atom',
-          checkpoint: '05766d2bc98e45972168b97d57c250b60c6450a49fba54ea55e8ea74cab6cdbf'
+          checkpoint: '285cfb09cf55440da21cd2f0a478a0261722a1701b58d0de6e20e7d376b6c138'
   name 'Hammerspoon'
   homepage 'http://www.hammerspoon.org/'
   license :mit
