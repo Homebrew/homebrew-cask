ddad9ddd552b29e3fcac59359de078ee17e4cdf0
Tobias Erthal <android@akgbensheim.de>
2016-08-29 19:16:23 +0200

    Merged 32bit version url in "Vega.app" cask
    Cask now contains the download url to both the 64bit and the 32bit
    version.

--- Casks/vega.rb
+++ Casks/vega.rb
@@ -2,9 +2,13 @@ cask 'vega' do
   version :latest
   sha256 :no_check
 
-  url 'https://dist.subgraph.com/downloads/Vega64.dmg'
+  if Hardware::CPU.is_32_bit?
+    url 'https://dist.subgraph.com/downloads/Vega.dmg'
+  else
+    url 'https://dist.subgraph.com/downloads/Vega64.dmg'
+  end
+
   name 'Vega'
-  name 'Vega (64bit)'
   homepage 'https://subgraph.com/vega/index.en.html'
   license :unknown
 
