ce4b891308dc36d5cc6571a1f04707d3fedbd209
Tobias Erthal <android@akgbensheim.de>
2016-08-29 13:23:54 +0200

    Added "Vega.app" cask
    Added 64bit version of the latest release of "Vega.app" as new cask
    vega.rb

---
+++ Casks/vega.rb
@@ -0,0 +1,12 @@
+cask 'vega' do
+  version :latest
+  sha256 '14f7eee1f41ef9241efeb2321731f8dda9ac0716805132c1a02f07994d86415f'
+
+  url 'https://dist.subgraph.com/downloads/Vega64.dmg'
+  name 'Vega'
+  name 'Vega (64bit)'
+  homepage 'https://subgraph.com/vega/index.en.html'
+  license :unknown
+
+  app 'Vega.app'
+end
