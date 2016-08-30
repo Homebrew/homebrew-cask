c9fc1326de52c9f145aaf00e2659c7e6eac21aff
Victor Popkov <victor@popkov.me>
2016-08-30 20:50:48 +0300

    updated power-manager-pro (4.5.4)

--- Casks/power-manager-pro.rb
+++ Casks/power-manager-pro.rb
@@ -1,10 +1,10 @@
 cask 'power-manager-pro' do
-  version '4.5.3'
-  sha256 '9ba78659549e9750b05dd89b42ca4e5cc9ee5af0b564c9f0e80f392bcdb3d4d6'
+  version '4.5.4'
+  sha256 'd69879539d2af1f9cd78dc4b3297ed2d7722eccf4237fdc9f65922a11c11828f'
 
   url "https://www.dssw.co.uk/powermanager/dsswpowermanagerpro-#{version.no_dots}.dmg"
   appcast 'https://version.dssw.co.uk/powermanager/professional',
-          checkpoint: 'f85a40c960f6bb324e87ede66a5a742d38c3089fbaf753a5bab420aa72139ed6'
+          checkpoint: '1a8584ecd6238e6fdc7e152eb979ac94bb34d3a5583e4eec9957af7394b14d39'
   name 'Power Manager Pro'
   homepage 'https://www.dssw.co.uk/powermanager/professional/'
   license :commercial
