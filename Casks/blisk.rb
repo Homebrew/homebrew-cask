8b7ff13627a9b526905bdfa3c6dc6477de899ef1
Patrik Affentranger <patrik@thefrontiergroup.com.au>
2016-08-30 10:29:34 +0800

    updated blisk (0.61.2743.166) (#24147)

--- Casks/blisk.rb
+++ Casks/blisk.rb
@@ -1,6 +1,6 @@
 cask 'blisk' do
-  version '0.60.2491.72'
-  sha256 '7b8a74cd36aec00b87870914b20833ef0e58fff7bb81fd0e15369726d27abf78'
+  version '0.61.2743.166'
+  sha256 '023d977bb17f54057c4b32986c9541af787058b2be7b7c5d7d93d5441f2a2616'
 
   # bliskcloudstorage.blob.core.windows.net was verified as official when first introduced to the cask
   url "https://bliskcloudstorage.blob.core.windows.net/mac-installers/BliskInstaller_#{version}.dmg"
