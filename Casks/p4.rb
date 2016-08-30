6d6f46eab629fe4022c8a882cfbd84c6ee96807b
Morten Bøgh <morten@justabeech.com>
2016-08-30 08:47:44 +0200

    updated p4 (2016.1.1429894)

--- Casks/p4.rb
+++ Casks/p4.rb
@@ -1,11 +1,11 @@
 cask 'p4' do
-  version '2016.1.1411799'
+  version '2016.1.1429894'
 
   if Hardware::CPU.is_32_bit?
-    sha256 'ddc3272687038b1b507017f6a702e9bdf41b8faf6d5b89ca27e0fc9b1879ad4f'
+    sha256 '7e68c05e9822d36f7d77609b2e127baab16ec9d72c74c8367b78333ec9d244bb'
     url "http://filehost.perforce.com/perforce/r#{version.major_minor[2..-1]}/bin.macosx105x86/p4"
   else
-    sha256 '85153a60d75d84b172320e0d9348d0da67979f24a2e63650e5febf005afac778'
+    sha256 'bdd53babf08e487997a6a5bd9ee8da3241136f556d6f3a72dd0cadb04d2929d1'
     url "http://filehost.perforce.com/perforce/r#{version.major_minor[2..-1]}/bin.macosx105x86_64/p4"
   end
 
