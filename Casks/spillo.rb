bf9a6101750b7d2e8c4a4ad94bbde8563ff59553
Victor Popkov <victor@popkov.me>
2016-08-31 00:44:40 +0300

    updated spillo (127_1.8.6) (#24167)

--- Casks/spillo.rb
+++ Casks/spillo.rb
@@ -1,11 +1,11 @@
 cask 'spillo' do
-  version '126_1.8.5'
-  sha256 '37172b7716fab717c15212653964cf2579690f40713663c3eb07113e68cfb0c5'
+  version '127_1.8.6'
+  sha256 'a65f413252cf5629d002b2c90577a72a9069c0bf78132e9a5b23067a8ef06a60'
 
   # s3.amazonaws.com/bananafish-builds/spillo was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/bananafish-builds/spillo/spillo_#{version}.zip"
   appcast 'https://bananafishsoftware.com/feeds/spillo.xml',
-          checkpoint: '3cba1573938d8c1653cfe6d2ffaf4d78a2cd2d0b49a53195a67ead0e84a9a1d7'
+          checkpoint: '690d354ae7a3e078b6d8fcf5c6ab576b8b2b6eeded51d8d68e9f1ab18038dd2b'
   name 'Spillo'
   homepage 'https://bananafishsoftware.com/products/spillo/'
   license :commercial
