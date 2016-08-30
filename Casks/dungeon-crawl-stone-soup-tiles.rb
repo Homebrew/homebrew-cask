227cd4dd8775ad0a02220aed817ad06a15d260f3
Daniel James McMahon <mcmahoniel@users.noreply.github.com>
2016-08-29 23:47:32 -0700

    Update download extension (#24149)

--- Casks/dungeon-crawl-stone-soup-tiles.rb
+++ Casks/dungeon-crawl-stone-soup-tiles.rb
@@ -2,7 +2,7 @@ cask 'dungeon-crawl-stone-soup-tiles' do
   version '0.18.1'
   sha256 'b6d8b39f1940b71e16c6e2e9cd1a7cf51bfd4306296c3b421bd6b3e0a0104336'
 
-  url "https://crawl.develz.org/release/stone_soup-#{version}-tiles-macosx.zip"
+  url "https://crawl.develz.org/release/stone_soup-#{version}-tiles-macosx.dmg"
   name 'Dungeon Crawl Stone Soup'
   homepage 'https://crawl.develz.org'
   license :gpl
