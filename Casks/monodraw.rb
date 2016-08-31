a91847a6cc797328e030066bf8fae279aa1c69f5
Viktor Szakats <vszakats@users.noreply.github.com>
2016-08-31 09:21:16 +0200

    use https URLs

--- Casks/monodraw.rb
+++ Casks/monodraw.rb
@@ -2,8 +2,8 @@ cask 'monodraw' do
   version 'b88'
   sha256 '233cc3b96681bb3dc6358ca846ad1b938b5fa1d77834f7fca149fa15d5deec96'
 
-  url "http://updates.helftone.com/monodraw/downloads/Monodraw-#{version}.zip"
-  appcast 'http://updates.helftone.com/monodraw/appcast-beta.xml',
+  url "https://updates.helftone.com/monodraw/downloads/Monodraw-#{version}.zip"
+  appcast 'https://updates.helftone.com/monodraw/appcast-beta.xml',
           checkpoint: 'd8023e46e9ab868fd198e0c403776749e74a2d1dd431d9c4af503f3c405b5402'
   name 'Monodraw'
   homepage 'http://monodraw.helftone.com'
