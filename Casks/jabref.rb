d9939d5241072cdf5654320c21b78138b4d9cf86
Christian Landsiedel <christian.landsiedel@tum.de>
2016-08-31 14:55:59 +0200

    Update JabRef to v3.6

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -1,11 +1,11 @@
 cask 'jabref' do
-  version '3.5'
-  sha256 'fea9b90668c94c31e18c0a6593adacb84e1247aa4f937ddeb8719deb2a900e4c'
+  version '3.6'
+  sha256 '1eabadccc6eca0c4a7157870509e8d5702351cf9f438d126e25859c92edae52a'
 
   # github.com/JabRef/jabref was verified as official when first introduced to the cask
   url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef_macos_#{version.dots_to_underscores}.dmg"
   appcast 'https://github.com/JabRef/jabref/releases.atom',
-          checkpoint: '5529a3b6efe55446a7621c7de9727fdcdcebf06cf4a078738ce12b70b1ba1312'
+          checkpoint: '0cff3ed91fec9ffd92eb9592882c38f7f630fbb57c17e50fc1bc70f997b77729'
   name 'JabRef'
   homepage 'https://www.jabref.org/'
   license :gpl
