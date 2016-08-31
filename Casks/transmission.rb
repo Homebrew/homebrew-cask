36b709c2a034957c769fe7c0a6f0d2a81cfd565f
Bryan Black <reelsense@users.noreply.github.com>
2016-08-31 05:18:51 -0700

    transmission style
    separated by a single empty
    and tab...

--- Casks/transmission.rb
+++ Casks/transmission.rb
@@ -1,7 +1,9 @@
 cask 'transmission' do
   version '2.92'
   sha256 '926a878cac007e591cfcea987048abc0689d77e7729a28255b9ea7b73f22d693'
-# github.com was verified as official
+  
+  # https://github.com/transmission/transmission/releases/download/ was verified as official
+
   url "https://github.com/transmission/transmission/releases/download/#{version}/transmission-#{version}.dmg"
   appcast 'https://transmissionbt.com/appcast.xml',
           checkpoint: '78fb3a9cfd536d80d5a9a64da89e2e7e6281c18b8787b565450e501098bef83b'
