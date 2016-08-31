dcc4cc9c3f5b8a8939312610d46e436e5d855308
Bryan Black <reelsense@users.noreply.github.com>
2016-08-30 18:55:59 -0700

    url switched to github
    Safer download url because the project maintainers are incapable of securing a file server and don't know how to sign files with PGP.

--- Casks/transmission.rb
+++ Casks/transmission.rb
@@ -2,7 +2,7 @@ cask 'transmission' do
   version '2.92'
   sha256 '926a878cac007e591cfcea987048abc0689d77e7729a28255b9ea7b73f22d693'
 
-  url "https://download.transmissionbt.com/files/Transmission-#{version}.dmg"
+  url "https://github.com/transmission/transmission/releases/download/#{version}/transmission-#{version}.dmg"
   appcast 'https://update.transmissionbt.com/appcast.xml',
           checkpoint: '24dcf232666db1aed41dae45c6a4fa9e7f52b98c10e69207cdd48baf83e114ac'
   name 'Transmission'
