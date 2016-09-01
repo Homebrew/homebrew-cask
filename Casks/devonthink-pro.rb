7e37084da1faf00d70333558978a11494bbb589b
Victor Popkov <victor@popkov.me>
2016-09-01 21:24:52 +0300

    updated devonthink-pro (2.9.3)

--- Casks/devonthink-pro.rb
+++ Casks/devonthink-pro.rb
@@ -1,11 +1,11 @@
 cask 'devonthink-pro' do
-  version '2.9.2'
-  sha256 '4ffd5ab148321e358c429667b66f49f8766c12568147fdc2f9d3ed18764494c3'
+  version '2.9.3'
+  sha256 '96061d924afd0cb6acb5232c94a81e65cb8ebc43d018aace067cbeffd1d197c9'
 
   # amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_Pro.app.zip"
   appcast 'http://www.devon-technologies.com/fileadmin/templates/filemaker/sparkle.php?product=300030707&format=xml',
-          checkpoint: 'e16f6b6e725b7a92413182cb57bd84496b411607fef21ba66cb5b98333195f7b'
+          checkpoint: 'a47c6d82099bf170f6cfae81d576a3bad3639b6f333fe39da9346fc66276576c'
   name 'DEVONthink Pro'
   homepage 'http://www.devontechnologies.com/products/devonthink/devonthink-pro.html'
   license :commercial
