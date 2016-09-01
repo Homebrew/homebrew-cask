34df51787c6abd59ba3f7f8e65fef430fd92192f
Michael Steinbaugh <mike@steinbaugh.com>
2016-09-01 11:11:03 -0400

    updated bbedit (11.6.1)

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -1,10 +1,10 @@
 cask 'bbedit' do
-  version '11.6'
-  sha256 'ba67eed37a7d712b1f408369eec1776086c87421a0e4458fc351961979ee4e46'
+  version '11.6.1'
+  sha256 'aa881cc8afc3006acee8ebab9d22feb011844a15b1aeb717587cf9d9c33aff0e'
 
   url "http://pine.barebones.com/files/BBEdit_#{version}.dmg"
   appcast 'https://versioncheck.barebones.com/BBEdit.xml',
-          checkpoint: '834b32a7873db793203a072d902b4037b99d0650739934c387953dc71f411719'
+          checkpoint: '25a635ee2d2a6c67de7bd49b2341106c4480c0ef8399aa4f158df1aaef3dd25c'
   name 'BBEdit'
   homepage 'http://www.barebones.com/products/bbedit/'
   license :commercial
