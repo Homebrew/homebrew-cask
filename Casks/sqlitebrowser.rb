f087c5c3e70fcfa1600032258978f32891380760
screendriver <screendriver@users.noreply.github.com>
2016-09-01 15:43:26 +0200

    updated sqlitebrowser (3.9.0) (#24197)

--- Casks/sqlitebrowser.rb
+++ Casks/sqlitebrowser.rb
@@ -1,11 +1,11 @@
 cask 'sqlitebrowser' do
-  version '3.8.0v5'
-  sha256 'b32597066279442e67e363dab9d89cf2533a6624f3a769b0d2681e22552ed3eb'
+  version '3.9.0'
+  sha256 '6d82db8924c3ff7d9abda45a844fe000db9679255722fb2ebf07b93d9c2f0e11'
 
   # github.com/sqlitebrowser/sqlitebrowser was verified as official when first introduced to the cask
-  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version.major_minor_patch}/sqlitebrowser-#{version}.dmg"
+  url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version.major_minor_patch}/DB.Browser.for.SQLite-#{version}.dmg"
   appcast 'https://github.com/sqlitebrowser/sqlitebrowser/releases.atom',
-          checkpoint: '5e7433998dd925ed7fc0bd2764c56019af0a930323a7fefe18ef240145a64522'
+          checkpoint: 'bb9bd0784daca32be3d60b56a2f48c1a7891e5c579d3ac96dce4f26fed434ac1'
   name 'SQLite Database Browser'
   homepage 'http://sqlitebrowser.org'
   license :oss
