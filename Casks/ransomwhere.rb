de646d41c55150f66025eada158ff0b0b0e84bae
ment4t <ment4t@users.noreply.github.com>
2016-09-01 22:34:01 +0100

    Add RansomWhere.app v1.1.0 (#20722)

--- Casks/ransomwhere.rb
+++ Casks/ransomwhere.rb
@@ -1,11 +1,22 @@
 cask 'ransomwhere' do
-  version '1.0.0'
-  sha256 '412a22a22342235f08bbe34512cc612ddeee11e79e1a89ba00e17b346c99a05c'
+  version '1.1.0'
+  sha256 '7b18e17abd8fb40d7c25f29f65d64a1f65c758d61c9f67c11d9722d1d7486ea9'
 
+  # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/RansomWhere_#{version}.zip"
   name 'RansomWhere'
   homepage 'https://objective-see.com/products/ransomwhere.html'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  app 'RansomWhere.app'
+  installer script: {
+                      executable: "#{staged_path}/RansomWhere.app/Contents/MacOS/RansomWhere",
+                      args:       ['-install'],
+                      sudo:       true,
+                    }
+
+  uninstall script: {
+                      executable: "#{staged_path}/RansomWhere.app/Contents/MacOS/RansomWhere",
+                      args:       ['-uninstall'],
+                      sudo:       true,
+                    }
 end
