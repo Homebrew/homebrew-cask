176101968674fb0f40f8894509c5a1ee99f9fba3
Mert Akengin <pvtmert@users.noreply.github.com>
2016-09-03 20:18:08 +0300

    update nwjs (#24241)
    * update nwjs
    
    * Update nwjs.rb

--- Casks/nwjs.rb
+++ Casks/nwjs.rb
@@ -1,5 +1,5 @@
 cask 'nwjs' do
-  version '0.12.3'
+  version '0.17.0'
 
   if Hardware::CPU.is_32_bit?
     sha256 '0170cb6acccb2329b4c31494a4bdb7e336e201204eba96e227e7b02546cc9ebb'
@@ -9,7 +9,7 @@ cask 'nwjs' do
     binary "nwjs-v#{version}-osx-ia32/nwjc"
     app "nwjs-v#{version}-osx-ia32/nwjs.app"
   else
-    sha256 '416747be34ad3bbae008ac6121551fda1505dfac55169cbd025558b29a40faf4'
+    sha256 '35e2bf24c31d27da96f32f00bfe2d4c74a83c43b58a7ac61222e128c494f7e5e'
 
     url "https://dl.nwjs.io/v#{version}/nwjs-v#{version}-osx-x64.zip"
 
