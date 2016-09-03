60574ea70143a8ffa24c4e18bc82ce0772ab2e1c
Mert Akengin <pvtmert@users.noreply.github.com>
2016-09-03 19:43:31 +0300

    Update nwjs.rb

--- Casks/nwjs.rb
+++ Casks/nwjs.rb
@@ -9,7 +9,7 @@ cask 'nwjs' do
     binary "nwjs-v#{version}-osx-ia32/nwjc"
     app "nwjs-v#{version}-osx-ia32/nwjs.app"
   else
-    sha256 '5e2bf24c31d27da96f32f00bfe2d4c74a83c43b58a7ac61222e128c494f7e5e'
+    sha256 '35e2bf24c31d27da96f32f00bfe2d4c74a83c43b58a7ac61222e128c494f7e5e'
 
     url "https://dl.nwjs.io/v#{version}/nwjs-v#{version}-osx-x64.zip"
 
