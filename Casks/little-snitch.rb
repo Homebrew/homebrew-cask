8bf99c01f36359319c2db2f6efd9f3dc24d87c9c
Toni Förster <toni.foerster@gmail.com>
2016-09-04 14:30:52 +0900

    Little Snitch: appcast added

--- Casks/little-snitch.rb
+++ Casks/little-snitch.rb
@@ -3,6 +3,8 @@ cask 'little-snitch' do
   sha256 '143070b3d8fd7370aa9c7881d3239efe33f05f4d4413a46e22988dd64f5b5223'
 
   url "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-#{version}.dmg"
+  appcast 'https://www.obdev.at/products/littlesnitch/releasenotes.html',
+          checkpoint: 'f66b1e72e3c1a7d245b5748579656eb0deabbc73cecf355a6c0cc901305b4dab'
   name 'Little Snitch'
   homepage 'https://www.obdev.at/products/littlesnitch/'
   license :commercial
