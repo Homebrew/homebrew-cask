c3bb414b1827f0941650a5617b8031e2739480da
Toni Förster <toni.foerster@gmail.com>
2016-09-04 18:04:41 +0900

    The Unarchiver: appcast added

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -3,9 +3,13 @@ cask 'the-unarchiver' do
   sha256 '2b9e1c0f6bcad958c19bfa0b5b1c59ad0681574100918004c0bcad2e43dc0761'
 
   url "https://unarchiver.c3.cx/downloads/TheUnarchiver#{version}.dmg"
+  appcast 'https://unarchiver.c3.cx/updates.rss',
+          checkpoint: '12b3e96559528c84b7353ce25f66d685b3813a4f86b3802835988f9b90084228'
   name 'The Unarchiver'
   homepage 'https://unarchiver.c3.cx/unarchiver'
   license :oss
+
+  auto_updates true
 
   app 'The Unarchiver.app'
 end
