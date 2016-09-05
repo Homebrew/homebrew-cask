debfddafe3657724e02ac16bf082d582b162588c
Viktor Szakats <vszakats@users.noreply.github.com>
2016-09-06 01:09:58 +0200

    use https URLs

--- Casks/smart-scroll.rb
+++ Casks/smart-scroll.rb
@@ -2,9 +2,9 @@ cask 'smart-scroll' do
   version '4.2'
   sha256 '5ebe15dd73b708770263bb9aae1e22702539aeeae269656f72410e563fcf7ca4'
 
-  url 'http://www.marcmoini.com/SmartScroll.dmg'
+  url 'https://www.marcmoini.com/SmartScroll.dmg'
   name 'Smart Scroll'
-  homepage 'http://www.marcmoini.com/sx_en.html'
+  homepage 'https://www.marcmoini.com/sx_en.html'
   license :commercial
 
   depends_on macos: '>= :snow_leopard'
