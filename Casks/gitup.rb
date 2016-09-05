da7a6459527fb16b67ee5e3e55786762b0721421
Amar Sood <mail@tekacs.com>
2016-09-04 21:34:31 -0700

    Update GitUp to version 1.0.6 (#24269)

--- Casks/gitup.rb
+++ Casks/gitup.rb
@@ -1,11 +1,11 @@
 cask 'gitup' do
-  version '1.0.5'
-  sha256 'ca230886c4e808518b918530392ef2eb6aea5d11dcf4ea23a89a271d3d0ee3f1'
+  version '1.0.6'
+  sha256 '420127192b63adfbd6b907c17a4934ffacffe856409dfb53b0696346cfb091d2'
 
   # s3-us-west-2.amazonaws.com/gitup-builds was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/gitup-builds/stable/GitUp.zip'
   appcast 'https://github.com/git-up/GitUp/releases.atom',
-          checkpoint: '70b0baa99c73973227fc6a3a361e0b7bf6df59dbd77972400a028604e4d21ef9'
+          checkpoint: '3fc88622548fe98ef117005b96e4284fcb544e8d52f58b5f8329ebd99cc4a9a2'
   name 'GitUp'
   homepage 'http://gitup.co'
   license :gpl
