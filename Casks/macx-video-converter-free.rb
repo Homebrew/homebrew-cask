1c933f11dc817683458befc6c7b41a45829861f6
Fred Lackey <fred.lackey@gmail.com>
2016-09-05 17:20:34 -0400

    MacX DVD Ripper Mac Free Edition
    Created new cask for MacX Video Converter / DVD Ripper (Free Edition).

---
+++ Casks/macx-video-converter-free.rb
@@ -0,0 +1,11 @@
+cask 'macx-video-converter-free' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.macxdvd.com/download/converter_ripper_org/macx-dvd-ripper-mac-free-edition.dmg'
+  name 'MacX DVD Ripper Mac Free Edition'
+  homepage 'https://www.macxdvd.com/mac-video-converter-free/'
+  license :gratis
+
+  app 'MacX DVD Ripper Mac Free Edition.app'
+end
