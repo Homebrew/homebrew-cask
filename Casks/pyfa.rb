edc972762b7c67afb60a6d371ccca278e2b1f943
Otto Strassen <otto@tostr.io>
2016-09-06 10:53:55 +0200

    updated appcast checkpoint (#24293)

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,10 +1,10 @@
 cask 'pyfa' do
-  version '1.23.1'
+  version '1.23.1,yc.118.7-1.4'
   sha256 '4e0b4641a0824e99729278b9f077376a17628f685125bef5007587066e580c3f'
 
-  url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-#{version}-yc.118.7-1.4-mac.zip"
+  url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version.before_comma}/pyfa-#{version.before_comma}-#{version.after_comma}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom',
-          checkpoint: '443b0342f7a4b5b342de546ecdc50a3145ac6191640a65b04ef1d0e5fcec3298'
+          checkpoint: 'eea8bb24a425814f3a83bd27bf8a690c77ac4bf058ebeb9765ac1b4415abe13d'
   name 'pyfa'
   homepage 'https://github.com/pyfa-org/Pyfa'
   license :gpl
