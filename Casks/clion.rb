7022dcc8c0c9a7ddf88ff3180c5214743f77cfe8
Eric Nielsen <eric@amalgamar.com.br>
2016-09-06 14:13:30 -0500

    Update clion to 2016.2.2

--- Casks/clion.rb
+++ Casks/clion.rb
@@ -1,6 +1,6 @@
 cask 'clion' do
-  version '2016.2.1'
-  sha256 'bc411574a2bc0666e4ebfd0a2bfb798dded012a428e8470e2508ed180ea6f529'
+  version '2016.2.2'
+  sha256 '9c7667bf180352e2af9ceb521f3fcd260c30bb99cd7d7d7b90b058997dd6f8dc'
 
   url "https://download.jetbrains.com/cpp/CLion-#{version}.dmg"
   name 'CLion'
