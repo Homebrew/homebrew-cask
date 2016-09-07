31b9670fdfd4e33f6493b87a103289d7799a79a9
Claire Furney <infuerno@fastmail.fm>
2016-09-07 23:12:21 +0100

    Update serviio to 1.6.1

--- Casks/serviio.rb
+++ Casks/serviio.rb
@@ -1,6 +1,6 @@
 cask 'serviio' do
-  version '1.6'
-  sha256 'c6305ef40bb39653a46adb35341511a15a17133c6145e8cce74bae9a96010ebd'
+  version '1.6.1'
+  sha256 '3f5110543c1336eabce255f45aed28bd3b66b1752714e5826654df232b91e747'
 
   url "http://download.serviio.org/releases/serviio-#{version}-osx.tar.gz"
   name 'Serviio'
