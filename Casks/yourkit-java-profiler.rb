b5013e39e571a9bb872c255ec1d69b32f6e9fb3f
Grant Hutchins <ghutchins@pivotal.io>
2016-09-07 10:20:39 -0500

    Update yourkit-java-profiler to 2016.02-b41
    Signed-off-by: Michael Stuart <mistuart@corelogic.com>

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask 'yourkit-java-profiler' do
-  version '2016.02-b40'
-  sha256 '7682d3fa37bf3ee6226e30147845a1297ec6758e21b0ac473c57f363ab7459f8'
+  version '2016.02-b41'
+  sha256 '2778fe509870597b615a883664b3503c85d93089bd851676d26d5b54dbda0348'
 
   url "https://www.yourkit.com/download/yjp-#{version}-mac.zip"
   name 'YourKit Java Profiler'
