51de20b25a09111cbade78872b1f3f4d16fb1fc8
Daniel Bayley <daniel.bayley@me.com>
2016-09-08 03:39:14 +0100

    Fix url
    Correct `url` to Adobe official link.
    
    Signed-off-by: Daniel Bayley <daniel.bayley@me.com>

--- Casks/adobe-animate-cc.rb
+++ Casks/adobe-animate-cc.rb
@@ -2,19 +2,18 @@ cask 'adobe-animate-cc' do
   version '2015'
   sha256 'c4a0affca7c37884471b495a0c242ddb110c8591bc41da91ecab81d12da60590'
 
-  # prodesigntools.com was verified as official when first introduced to the cask
-  url 'http://prodesigntools.com/trials3/AdobeProducts/FLPR/15_1/osx10-64/Animate_15_LS20.dmg',
+  url 'http://trials3.adobe.com/AdobeProducts/FLPR/15_1/osx10-64/Animate_15_LS20.dmg',
       user_agent: :fake,
       cookies:    { 'MM_TRIALS' => '1234' }
   name 'Adobe Animate CC'
-  homepage 'https://www.adobe.com/products/animate.html'
+  homepage 'https://adobe.com/products/animate'
   license :commercial
 
-  installer script: "#{staged_path}/Adobe Animate CC 2015/Install.app/Contents/MacOS/Install",
-            args:   ['--mode=silent', "--deploymentFile=#{staged_path}/Adobe Animate CC 2015/deploy/en_US_Deployment.xml"]
+  installer script: "#{staged_path}/Adobe Animate CC #{version}/Install.app/Contents/MacOS/Install",
+            args:   ['--mode=silent', "--deploymentFile=#{staged_path}/Adobe Animate CC #{version}/deploy/en_US_Deployment.xml"]
 
   uninstall script: {
-                      executable: "#{staged_path}/Adobe Animate CC 2015/Install.app/Contents/MacOS/Install",
-                      args:       ['--mode=silent', "--deploymentFile=#{staged_path}/Adobe Animate CC 2015/deploy/uninstall.xml"],
+                      executable: "#{staged_path}/Adobe Animate CC #{version}/Install.app/Contents/MacOS/Install",
+                      args:       ['--mode=silent', "--deploymentFile=#{staged_path}/Adobe Animate CC #{version}/deploy/uninstall.xml"],
                     }
 end
