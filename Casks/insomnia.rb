7b03fbe0a6092905f0c187894ae025f539bfade6
Roman <romazhuzha@gmail.com>
2016-09-08 12:09:26 +0200

    Update insomnia to 3.3.2 (#24336)

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask 'insomnia' do
-  version '3.3.1'
-  sha256 '8136fdff55c0f3894f47e0a22de7290c0b8c9b1173f78f61f91e271a52ae448b'
+  version '3.3.2'
+  sha256 'd80949618e87c29f96d38d896317e022f79311e89c601ae71f78c42bd4651311'
 
   # s3.amazonaws.com/builds-insomnia-rest was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/builds-insomnia-rest/mac/Insomnia-#{version}.dmg"
