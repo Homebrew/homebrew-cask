a598834c7befaa7761b62ddd4c19f0256026fa86
Victor Popkov <victor@popkov.me>
2016-09-11 02:29:50 +0300

    updated toggldesktop (7.3.326)

--- Casks/toggldesktop.rb
+++ Casks/toggldesktop.rb
@@ -1,11 +1,11 @@
 cask 'toggldesktop' do
-  version '7.3.319'
-  sha256 'ed5ce0d8db3819cb516a7900b18c93fc07ef826aa197fccb753ab376a1dc1079'
+  version '7.3.326'
+  sha256 'b508d813961395286f8b56522d549f7c585ed265b1114be4cdcab120ebce1a00'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_stable_appcast.xml',
-          checkpoint: '88464c3266e1339f31945eb311d060bb83529049d22350daa43afba53d13cd79'
+          checkpoint: '460f65668197a03b0b95a17d1a1f7d2c617321281eca6c7b6dd136de9511189d'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com'
   license :bsd
