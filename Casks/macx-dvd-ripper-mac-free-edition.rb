b09fc6387ca83387ce326c7ecae8a6b28bcfee2a
Viktor Szakats <vszakats@users.noreply.github.com>
2016-09-10 18:11:25 +0200

    use secure URLs (#24376)

--- Casks/macx-dvd-ripper-mac-free-edition.rb
+++ Casks/macx-dvd-ripper-mac-free-edition.rb
@@ -2,7 +2,7 @@ cask 'macx-dvd-ripper-mac-free-edition' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.macxdvd.com/download/converter_ripper_org/macx-dvd-ripper-mac-free-edition.dmg'
+  url 'https://www.macxdvd.com/download/converter_ripper_org/macx-dvd-ripper-mac-free-edition.dmg'
   name 'MacX DVD Ripper Mac Free Edition'
   homepage 'https://www.macxdvd.com/dvd-ripper-mac-free/'
   license :gratis
