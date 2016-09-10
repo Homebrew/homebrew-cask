5e9950852614b374412a71fa7ba06e765a3cb3c4
Nathan Toone <nathan@toonetown.com>
2016-09-09 20:42:34 -0600

    Fixed cask for boom (1.4.3)

--- Casks/boom.rb
+++ Casks/boom.rb
@@ -1,11 +1,11 @@
 cask 'boom' do
-  version '1.4.3,1469022046'
+  version '1.4.3,1471949433'
   sha256 'ecefe52dda2ff20df283260b676532ed2431bfdace550db4ecd08cb9bccddfd1'
 
   # devmate.com/com.globaldelight.Boom2 was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.globaldelight.Boom2/#{version.before_comma}/#{version.after_comma}/Boom2-#{version.before_comma}.dmg"
   appcast 'https://updates.devmate.com/com.globaldelight.Boom2.xml',
-          checkpoint: '46243f73857722dd91616328b8ab0812704495f6f8305af29187b6f78f6d68cc'
+          checkpoint: 'bd0fb47e7addd9347315fe8679e089bcb5e33e56993d8bc7e3fa645f8596f9b0'
   name 'Boom'
   homepage 'http://www.globaldelight.com/boom/'
   license :commercial
