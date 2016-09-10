de6df041dd398235a3a25db4b6a40610854a900c
Yves Desgagne <yves@khome.org>
2016-09-10 15:59:45 -0400

    Update texmacs to 1.99.5

--- Casks/texmacs.rb
+++ Casks/texmacs.rb
@@ -1,6 +1,6 @@
 cask 'texmacs' do
   version '1.99.5'
-  sha256 '1e753a68bc0dd3dc6d8b5bfe1a0b4a7346408eeae8c76bf60609026c20b7be75'
+  sha256 '6f560861a25439a3a4e314098ed096a3416581eab90ace89c7055e2eb34f7e2e'
 
   url "http://www.texmacs.org/Download/ftp/tmftp/macos/TeXmacs-#{version}.dmg"
   name 'GNU TeXmacs'
