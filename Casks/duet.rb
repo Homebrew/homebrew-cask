63ed1d59d9122d38f173f094ba6fca903494aa41
Alexander Zayats <z4y4ts@users.noreply.github.com>
2016-09-12 00:19:44 +0300

    Revert appcast checkpoint

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -5,7 +5,7 @@ cask 'duet' do
   # d2ycb980mbr5lq.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2ycb980mbr5lq.cloudfront.net/#{version.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip"
   appcast 'https://updates.duetdisplay.com/checkMacUpdates',
-          checkpoint: '6ee2c3da802374c5f8b2d5867d1838a19e91c985a89c09b950b972959faf51b1'
+          checkpoint: '01a04fe0630a818640a6e9fa9040113791192369529cb757f570a03e19b257f0'
   name 'Duet'
   homepage 'http://www.duetdisplay.com/'
   license :gratis
