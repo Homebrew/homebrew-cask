eb240bf2464990ab8e5e1c2d1cde2e52bd7f3809
Victor Popkov <victor@popkov.me>
2016-09-13 03:09:34 +0300

    updated papers (3.4.6,523) (#24425)

--- Casks/papers.rb
+++ Casks/papers.rb
@@ -1,10 +1,10 @@
 cask 'papers' do
-  version '3.4.5,512'
-  sha256 'dcaba19531a7954e9b8f396ec0587eab490aced7d51dfa5a7be146008b829ec3'
+  version '3.4.6,523'
+  sha256 '5cbc3a60a944d622bd40b9cc1424543e873fa340631d0b67c77cc8500ec0f164'
 
   url "http://appcaster.papersapp.com/apps/mac/production/download/#{version.after_comma}/papers_#{version.before_comma.no_dots}_#{version.after_comma}.dmg"
   appcast 'https://appcaster.papersapp.com/apps/mac/production/appcast.xml',
-          checkpoint: '99c2b980d6909a50dc3d9656604f908c796507d6861805e8bf3fb04136abfe6a'
+          checkpoint: '5628cd0bf9c6b9bf57861d595da2d9dbce63ad8fd0629fe22c622f06c1c10398'
   name 'Papers'
   homepage 'https://www.papersapp.com/'
   license :commercial
