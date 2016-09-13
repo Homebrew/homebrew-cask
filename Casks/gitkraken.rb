59a8622a9a2e9356f8754a4cd17dfb719a576c93
Christoph <c.schnieder@snap-agentur.de>
2016-09-13 17:19:34 +0200

    Update gitkraken to 1.6.0

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,10 +1,10 @@
 cask 'gitkraken' do
-  version '1.5.4'
-  sha256 'a5555de7218313851447c43e5f5c86ce0a42d6e8dfd3315f0cb4d688c1506a4e'
+  version '1.6.0'
+  sha256 '9c51476d4ba6cb79cb07910b11d1f76b0915515ff42228ddf82f7f845523d8ac'
 
   url "https://release.gitkraken.com/darwin/v#{version}.zip"
   appcast 'https://release.gitkraken.com/darwin/RELEASES',
-          checkpoint: 'f7b9df24ffb1d26c58aa07e33244a4f508db46a3138bfb5766f3eaf9dfdd3e98'
+          checkpoint: '7ed015411320d1d784e60d1e4ed5819f1f129a7e37e0deedcfc8f932cfe7badf'
   name 'GitKraken'
   homepage 'https://www.gitkraken.com/'
   license :gratis
