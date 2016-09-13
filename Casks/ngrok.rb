583e2a89fff762f75c0a1474b1a2e23020f9c23a
Tim Oram <mitmaro@gmail.com>
2016-09-13 13:14:27 -0230

    updated ngrok (2.1.12,4VmDzA7iaHb)

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -1,6 +1,6 @@
 cask 'ngrok' do
-  version '2.1.3,4VmDzA7iaHb'
-  sha256 '863ebc05824329d288977d275d2ab90e554780a053019ae2bbadb317a1abf607'
+  version '2.1.12,4VmDzA7iaHb'
+  sha256 '10812c02fb8450bb3b5726a50251e2d2615727875fb584764bbe38cfcc5dd68a'
 
   # bin.equinox.io was verified as official when first introduced to the cask
   url "https://bin.equinox.io/c/#{version.after_comma}/ngrok-stable-darwin-amd64.zip"
