1bcb17044600ed2181282c5f0c66d836a6830bd8
mchlrmrz <ramirez3494@gmail.com>
2016-09-13 07:09:17 -0400

    Update razorsql to 7.0.5 (#24439)

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,11 +1,11 @@
 cask 'razorsql' do
-  version '6.3.26'
+  version '7.0.5'
 
   if Hardware::CPU.is_32_bit? || MacOS.version <= :snow_leopard
-    sha256 '820deccfc41be160db3158f9ed69570bf054d4306a4a0180bf8d89d70376a3db'
+    sha256 '69e7e492ecdf0313464617269e98b5289599ce9d161671af4d98b166110726ee'
     url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}.dmg"
   else
-    sha256 '1ac51415955b7d72197184763fba707d675cdf9c9c65cac1bee91fe925aca677'
+    sha256 '2885e6542028a743579ad1f69427a1e254933e47f925d5d7b22c057b1b73794a'
     url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   end
 
