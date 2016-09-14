abd90d5e4cc7809e828130a5eb98eae7e3cc616b
Victor Popkov <victor@popkov.me>
2016-09-14 23:28:52 +0300

    Update yummy-ftp to 1.11.10b5

--- Casks/yummy-ftp.rb
+++ Casks/yummy-ftp.rb
@@ -1,10 +1,10 @@
 cask 'yummy-ftp' do
-  version '1.11.9'
+  version '1.11.10b5'
   sha256 '70f721e1ef51d1f04ba546999bb408bdf21ad6629c7cc244be8b4a6e8196e72e'
 
   url 'http://www.yummysoftware.com/downloads/YummyFTP.zip'
   appcast 'http://www.yummysoftware.com/su/yummyftp/feed.xml',
-          checkpoint: '5482dca1c4307092a8323659a1e2ae6fa2a5120cfe7510e673d0deac01fa2550'
+          checkpoint: '9293514587965d3fbe0d84b6eae0f5ddef9b2447286caab1d9ce8623f4e4fb15'
   name 'Yummy FTP'
   homepage 'http://www.yummysoftware.com'
   license :commercial
