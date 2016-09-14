74d704c1723297cc519705dc99386acf4a35bfc7
pseudoku <decrement@users.noreply.github.com>
2016-09-13 17:47:49 -0700

    Update osxfuse to 3.5.0

--- Casks/osxfuse.rb
+++ Casks/osxfuse.rb
@@ -1,11 +1,11 @@
 cask 'osxfuse' do
-  version '2.8.3'
-  sha256 'b0f05fa6e74372b73417e89bfd103a46a0b968239f03832755e424bec09051e6'
+  version '3.5.0'
+  sha256 '6d331236ee45782e404f46324a5e6d444943641d3fb62c4d84ba7fab8f4534a5'
 
-  # sourceforge.net/osxfuse was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/osxfuse/osxfuse-#{version}.dmg"
-  appcast 'https://sourceforge.net/projects/osxfuse/rss',
-          checkpoint: 'd137ba68265d71c31ce6124b0db2918543b38d6deb4afdf42f13097420c00b6e'
+  # github.com/osxfuse was verified as official when first introduced to the cask
+  url "https://github.com/osxfuse/osxfuse/releases/download/osxfuse-#{version}/osxfuse-#{version}.dmg"
+  appcast 'https://github.com/osxfuse/osxfuse/releases.atom',
+          checkpoint: 'd682c42b621aa74feebc066c018b47e4b6356f5f4d8604810c16d9d6bb1f1598'
   name 'OSXFUSE'
   homepage 'https://osxfuse.github.io/'
   license :bsd
