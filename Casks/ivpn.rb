d71712bd810f2591a0126658754e2cf4cc934a76
Victor Popkov <victor@popkov.me>
2016-09-15 04:04:53 +0300

    Update ivpn to 7.4.3 (#24497)

--- Casks/ivpn.rb
+++ Casks/ivpn.rb
@@ -1,10 +1,10 @@
 cask 'ivpn' do
-  version '7.4.2'
-  sha256 '1b3bf872999aa88f98451420f0fc00f6c3926fa7e58bea99d37508d89b915a4d'
+  version '7.4.3'
+  sha256 '4c8ed0b22e13ae23c0afd1307c690d9bb60011b5336c6b1eb5a2a42371f3b7e0'
 
   url "https://macserve.org.uk/downloads/ivpn/iVPN_#{version}.zip"
   appcast 'http://macserve.org.uk:8090/profileInfo.php',
-          checkpoint: 'fe87e733a838d6bfe7bc43484f914dc489ea595ad40c61e85bd376cf3ad8945a'
+          checkpoint: 'ef5a831947d62a9c82baa03e50e1044ef065f4bc80e30fd35c543f0eb6087414'
   name 'iVPN'
   homepage 'https://macserve.org.uk/projects/ivpn/'
   license :commercial
