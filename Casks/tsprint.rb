11b294b13dcd0be4607ae6294e7ec22986607c9f
Mark Anderson <marksolomon3@me.com>
2016-09-15 08:32:03 -0500

    Update tsprint.rb

--- Casks/tsprint.rb
+++ Casks/tsprint.rb
@@ -9,6 +9,6 @@ cask 'tsprint' do
 
   pkg 'TSPrintClient.pkg'
 
-  uninstall quit: 'com.terminalworks.TSPrintClient'
-  uninstall trash: '/Applications/TSPrintClient.app'
+  uninstall quit: 'com.terminalworks.TSPrintClient', 
+            trash: '/Applications/TSPrintClient.app'
 end
