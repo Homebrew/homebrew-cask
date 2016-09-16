fa1e6fc0a77ffbc877f2fc1b14d910de6ed90e56
Joe Wicentowski <joewiz@gmail.com>
2016-09-16 12:21:51 -0400

    Update MakeMKV to 1.10.2

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,6 +1,6 @@
 cask 'makemkv' do
-  version '1.10.1'
-  sha256 '39e6bfb950dc2edce333f145b2e69aca26f8215004868790d0343fe45a9e4de6'
+  version '1.10.2'
+  sha256 'cb2dd115242e2ab0c11aa1e167aa44337094410a621855326c90f6b5dd6f40c7'
 
   url "https://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   name 'MakeMKV'
