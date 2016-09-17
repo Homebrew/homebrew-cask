2b1e93dc4b230994b365b995659e32b9f396361d
Sasikumar <sasi.madhav2005@gmail.com>
2016-09-17 15:00:39 +0530

    Update Gramps to 4.2.4-3 (#24567)

--- Casks/gramps.rb
+++ Casks/gramps.rb
@@ -1,6 +1,6 @@
 cask 'gramps' do
-  version '4.2.4-1'
-  sha256 '58c655936e4ca021ada3d44e32f52c84b3eac2442b1e2fe4834bd78d3cefc4c6'
+  version '4.2.4-3'
+  sha256 '0b422840552e5da933feb69dd7964ed54002ecff8ef4813196fdb74c94cb72b3'
 
   # github.com/gramps-project/gramps was verified as official when first introduced to the cask
   url "https://github.com/gramps-project/gramps/releases/download/v#{version.major_minor_patch}/Gramps-Intel-#{version}.dmg"
