2f82380abb629abc9eacea34cc8b3ab026753f4d
Victor Popkov <victor@popkov.me>
2016-09-17 20:55:31 +0300

    Update github-desktop to 221

--- Casks/github-desktop.rb
+++ Casks/github-desktop.rb
@@ -1,10 +1,10 @@
 cask 'github-desktop' do
-  version '220'
-  sha256 '497182f4cf85f0c674f599cec22986b76dd2fdb6de3e54ee5efee86025be314c'
+  version '221'
+  sha256 'cbb12a07ab9e219a5e335479f35fda9af6cb76c13d19ccb6149f186ea2295d4a'
 
   url "https://mac-installer.github.com/mac/GitHub%20Desktop%20#{version}.zip"
   appcast 'https://central.github.com/mac/appcast.xml',
-          checkpoint: '1052f76811839a303d1e477a4834589ecf6ce2b5b30e680e03c8f61506e7db63'
+          checkpoint: '9c34ddf221f3943b28ced05edd3997cceb50631abc2a68111ff14cfad21b9017'
   name 'GitHub Desktop'
   homepage 'https://desktop.github.com/'
   license :gratis
