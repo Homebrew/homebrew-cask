9314840d09efae6bc176563d00f294866d7891f2
Mert Akengin <mert@n0pe.me>
2016-09-17 17:04:34 +0300

    added paths to end

--- Casks/facebook-ios-sdk.rb
+++ Casks/facebook-ios-sdk.rb
@@ -7,21 +7,20 @@ cask 'facebook-ios-sdk' do
   homepage 'https://developers.facebook.com/docs/ios'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
+  artifact 'AccountKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/AccountKit.framework"
+  artifact 'AccountKitStrings.bundle', target: "#{ENV['HOME']}/Documents/FacebookSDK/AccountKitStrings.bundle"
+  artifact 'Bolts.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/Bolts.framework"
+  artifact 'DocSets', target: "#{ENV['HOME']}/Documents/FacebookSDK/DocSets"
+  artifact 'FBAudienceNetwork.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/FBAudienceNetwork.framework"
+  artifact 'FBNotifications.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/FBNotifications.framework"
+  artifact 'FBSDKCoreKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/FBSDKCoreKit.framework"
+  artifact 'FBSDKLoginKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/FBSDKLoginKit.framework"
+  artifact 'FBSDKMessengerShareKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/FBSDKMessengerShareKit.framework"
+  artifact 'FBSDKShareKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/FBSDKShareKit.framework"
+  artifact 'FacebookSDKStrings.bundle', target: "#{ENV['HOME']}/Documents/FacebookSDK/FacebookSDKStrings.bundle"
+  artifact 'Samples', target: "#{ENV['HOME']}/Documents/FacebookSDK/Samples"
+
   preflight do
     system_command '/bin/mkdir', args: ['-p', '--', "#{ENV['HOME']}/Documents/FacebookSDK"]
   end
-
-  artifact 'AccountKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'AccountKitStrings.bundle', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'Bolts.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'DocSets', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'FBAudienceNetwork.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'FBNotifications.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'FBSDKCoreKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'FBSDKLoginKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'FBSDKMessengerShareKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'FBSDKShareKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'FacebookSDKStrings.bundle', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-  artifact 'Samples', target: "#{ENV['HOME']}/Documents/FacebookSDK"
-
 end
