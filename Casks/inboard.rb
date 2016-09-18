0d55d1f51d05d904fb37d0e46ff2fcd8c7ed7719
Victor Popkov <victor@popkov.me>
2016-09-18 19:49:51 +0300

    Update inboard to 1.0.9-288

--- Casks/inboard.rb
+++ Casks/inboard.rb
@@ -1,10 +1,10 @@
 cask 'inboard' do
-  version '1.0.8-285'
-  sha256 'aa787c9a3c75788499ba530bc5377523b59ea76e109899c843ba9def7f37cf46'
+  version '1.0.9-288'
+  sha256 '02d9ec80b5aeaa57f1775823e53ed4d55007369ab2d487ae9d840c0f0089fa73'
 
   url "http://inboardapp.com/trial/Inboard-#{version}.zip"
   appcast 'http://inboardapp.com/appcast.xml',
-          checkpoint: 'e53ad34aa629142245d50f2d35f51cf5c262013afe9007337ec9613b972e8d8c'
+          checkpoint: '2af763dba5809aa09932b379c652d04a372b1ededb7b8203db2bb65dc8115dd3'
   name 'Inboard'
   homepage 'http://inboardapp.com'
   license :commercial
