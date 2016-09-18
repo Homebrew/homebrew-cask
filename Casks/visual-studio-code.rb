aa2182e5f2510efc75ffafa2159c06bc075db5b9
Seiji Kohara <sage.0118@gmail.com>
2016-09-18 19:15:38 +0900

    Update visual-studio-code to 1.5.2,66f37fd2a99eb9d628dd374d81d78835b410c39b (#24589)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.5.1,07d663dc1bd848161edf4cd4ce30cce410d3d877'
-  sha256 '5ff7f21c2e519442466e91d5cfe44f81b16bffb53f9beed1de29985a6da304a3'
+  version '1.5.2,66f37fd2a99eb9d628dd374d81d78835b410c39b'
+  sha256 '478104e63d5c687ac03f59cbbb86e7ae20cd3b8ac47ca780dae1aaecc84f9400'
 
   # az764295.vo.msecnd.net was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
