08417dbf7343b2cf31d1e0dd05aa48631c0ec6e5
Victor Popkov <victor@popkov.me>
2016-09-18 20:07:47 +0300

    Update game-capture-hd to 2.0.5_983 (#24595)

--- Casks/game-capture-hd.rb
+++ Casks/game-capture-hd.rb
@@ -3,13 +3,13 @@ cask 'game-capture-hd' do
     version '2.0.3_970'
     sha256 '9eeb0bb7c00f29aa17a0bf4ff958f43e7ddae035b1390a003b0a43b6585c3542'
   else
-    version '2.0.4_981'
-    sha256 '21b741d7e64f0f14e2f230cd3b461657cd70ed7b2adc7ec731bb2b8994589f4f'
+    version '2.0.5_983'
+    sha256 '4803bcac9069e1e63a89e9053fdf2285487acf9e608e84f7610555075489ad5a'
   end
 
   url "http://files.elgato.com/gamecapture/gchdm_#{version.no_dots}.dmg"
   appcast 'http://updates.elgato.com/autoupdate/gameCapture20.rss?lang=English',
-          checkpoint: 'b3967a3eb2b8a46d7705d00bfb81cac8a553bef4bba09893856cd8f895e6878b'
+          checkpoint: 'a1d6aa74e2d6182ae3b0be8b40b72c550824922fc6b97f3a2b01ab88ad9e5606'
   name 'Game Capture HD'
   homepage 'https://www.elgato.com/en/gaming/gamecapture-hd'
   license :commercial
