2699cc8eee2dc78ccb8a89e7b34b8e8cc7ef386e
Grettir Asmundarson <grettir@users.noreply.github.com>
2016-09-19 01:19:29 -0600

    Update cookie to 5.1.1 (#24608)

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.1'
-  sha256 'c378e590c49e88b9a67ce193307d1fc40e5de19c3510bc1356c2b9c2eacc8e0f'
+  version '5.1.1'
+  sha256 '7768f4071a5566b8294090a6200096b7a95b4bddc4ad9eba0943aebc190a9ecb'
 
   url "https://sweetpproductions.com/products/cookie#{version.major}/Cookie#{version.major}.zip"
   appcast "https://sweetpproductions.com/products/cookie#{version.major}/appcast.xml",
-          checkpoint: '80a4b2de1ba33195ad07d3436d29e4e920f0161f67fe786f1d4eddb48e09d1ad'
+          checkpoint: '4f75c39159206a3dca1992fdf4d6e24101e5ebcdf0519b6bdeceea4df0af1d54'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
   license :commercial
