854c70e4778cec0848cf982275cee4f1221dbd06
Roman Zhuzha <romazhuzha@gmail.com>
2016-09-19 17:27:42 +0200

    replace spark appcast with github.atom

--- Casks/screencast.rb
+++ Casks/screencast.rb
@@ -3,8 +3,8 @@ cask 'screencast' do
   sha256 'ce662e83464e94495f782e5656d3a541b7dd4095eb400622e0e7e0cc04ecae7d'
 
   url "https://github.com/soh335/Screencast/releases/download/#{version}/Screencast.dmg"
-  appcast 'https://raw.githubusercontent.com/soh335/Screencast/master/appcast.xml',
-          checkpoint: 'cfbce27e2809dfc24831d2976fef1c9b9ce0da37876cc16ba5e0e62269d97254'
+  appcast 'https://github.com/soh335/Screencast/releases.atom',
+          checkpoint: '888ce555dd77a6e70f588ddf421ec6c5e1f7778be0312dc5f43a9f415ee93e97'
   name 'Screencast'
   homepage 'https://github.com/soh335/Screencast'
   license :closed
