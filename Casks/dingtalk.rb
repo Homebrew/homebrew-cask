9189b9dd9581f40a3ca1446b5c0e1ca2cfd7701a
死水微澜 <564311873@qq.com>
2016-09-19 21:14:15 +0800

    Update download url
    Extract new url from official site.

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -3,7 +3,7 @@ cask 'dingtalk' do
   sha256 '770205d6af3e46a9ac9746dcda645d8125cd1b275c7c759bcc11451ecf8828e2'
 
   # taobaocdn.com/dingtalk-desktop was verified as official when first introduced to the cask
-  url "http://download.taobaocdn.com/dingtalk-desktop/Release/install/DingTalk_v#{version}.dmg"
+  url "https://download.alicdn.com/dingtalk-desktop/Release/install/DingTalk_v#{version}.dmg"
   name 'DingTalk'
   name '钉钉'
   homepage 'https://www.dingtalk.com/'
