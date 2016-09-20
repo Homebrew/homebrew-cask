8aa6fb0b5535b880902216b48a876318bfe3e302
Victor Popkov <victor@popkov.me>
2016-09-20 03:33:51 +0300

    Update xbox360-controller-driver to 0.16.4 (#24635)

--- Casks/xbox360-controller-driver.rb
+++ Casks/xbox360-controller-driver.rb
@@ -1,10 +1,10 @@
 cask 'xbox360-controller-driver' do
-  version '0.16.3'
-  sha256 '43ddccb21801088a3bfe745f0b1c8be17296e83dc93813da68ab9b6662dcd3bd'
+  version '0.16.4'
+  sha256 'e72232947be69b9483349ffa9bc4d8fe7e74c5ea453f3df14a6d86489489f4c3'
 
   url "https://github.com/360Controller/360Controller/releases/download/v#{version}/360ControllerInstall_#{version}.dmg"
   appcast 'https://github.com/360Controller/360Controller/releases.atom',
-          checkpoint: '00296f3a9712ec64c07be2eef0c28f968a83ddb683dcbd1f3f3b73d0f9050634'
+          checkpoint: 'ea89ea749b89a985b663d1bee677a06acc0b1afebbd58258d62ea5d92c5f4f7f'
   name 'TattieBogle Xbox 360 Controller Driver (with improvements)'
   homepage 'https://github.com/360Controller/360Controller'
   license :gpl
