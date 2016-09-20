8fcf02b2b646fc192a594c72dadb43249a30ebe2
Kayvan Sylvan <KayvanSylvan@gmail.com>
2016-09-20 09:49:02 -0700

    Update lima to 1.2.1 (#24656)

--- Casks/lima.rb
+++ Casks/lima.rb
@@ -1,6 +1,6 @@
 cask 'lima' do
-  version '1.2.0'
-  sha256 '648f02b70560b84b161dd8cc2a2813d595e2b092dca23185dc273d7d1ca359ad'
+  version '1.2.1'
+  sha256 '6863bdadb85ebfeb96d52d9b695329a0b4e907ac82220ed448be2ce972ec2739'
 
   url "https://update.api.meetlima.com/downloads/osx/dist/Lima_#{version}.dmg"
   name 'Lima'
