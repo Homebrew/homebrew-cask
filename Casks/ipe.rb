9b8cb8098361d3932a3bc65fc5a262aefad8b685
miccal <miccal.matthews@gmail.com>
2016-09-20 08:34:21 +0800

    Update ipe to 7.2.6 (#24639)

--- Casks/ipe.rb
+++ Casks/ipe.rb
@@ -1,6 +1,6 @@
 cask 'ipe' do
-  version '7.2.5'
-  sha256 '5fd9adc4e3114bed178eb533223afeb6f60bf3c7fec564c984afecf36cac9cc5'
+  version '7.2.6'
+  sha256 '35e90434d635bb0c085c4447767ad746e61fc276b1f1dbe43a9d671327d5cfa1'
 
   # bintray.com/otfried was verified as official when first introduced to the cask
   url "https://dl.bintray.com/otfried/generic/ipe/#{version.major_minor}/ipe-#{version}-mac.dmg"
