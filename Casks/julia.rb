0869c67f806d8ba1150d5c07d2666bee50b83072
specter119 <specter119@gmail.com>
2016-09-20 09:18:29 +0800

    Update julia to 0.4.7

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask 'julia' do
-  version '0.4.6'
-  sha256 '81d6d73f3519788164431c996a563663fe002ce901e55de80d3f72b4f9bc473e'
+  version '0.4.7'
+  sha256 'ff6ed2950e7f8908818ec6ca953c9f0777c52999058f5dc5c1eb8793166b9253'
 
   # s3.amazonaws.com/julialang was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/julialang/bin/osx/x64/#{version.sub(%r{\.\d+$}, '')}/julia-#{version}-osx10.7+.dmg"
