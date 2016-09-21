a897239d8b591ccad25da1602951f54102ed3fa4
Clemens Giuliani <clemens@inailuig.it>
2016-09-21 21:53:24 +0200

    updated telegram-desktop (0.10.7)

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask 'telegram-desktop' do
-  version '0.10.6'
-  sha256 'f3eeed343d21a03ad6c047564ee9ac249872291962de926c4376ed2e0de46c54'
+  version '0.10.7'
+  sha256 '899c0de13274c8fe7b8cf187cb6b94addd5ead1568affa8b72e45900b88f4d06'
 
   # tdesktop.com was verified as official when first introduced to the cask
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
