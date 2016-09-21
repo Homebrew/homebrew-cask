d41cf2183d9a22bcd34db1a906ebe00b2982ab24
Jussi Tuominen <wickedsp1d3r@COLDwithin.local>
2016-09-21 17:46:37 +0300

    Update xld to 2016.09.20

--- Casks/xld.rb
+++ Casks/xld.rb
@@ -1,11 +1,11 @@
 cask 'xld' do
-  version '2015.12.14'
-  sha256 '08a03d72cf0ff2cc9e8810dc76604fd527270b0b6d35563d133e537a3ea30117'
+  version '2016.09.20'
+  sha256 '8a7c894cb28f10c08ab1d376da2c8ea7c454687a4cd5938af2d5159876ee10c4'
 
   # sourceforge.net/xld was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/xld/xld-#{version.no_dots}.dmg"
   appcast 'https://svn.code.sf.net/p/xld/code/appcast/xld-appcast_e.xml',
-          checkpoint: 'c4658828a87e26aad584edf9c0d6414ca45037c7219fb3d5889cac3e2578da33'
+          checkpoint: '5fbc9772cb9c0c56244a42432f39d340d4d4aec49cc9e11c4b3c014a65329b29'
   name 'X Lossless Decoder'
   name 'XLD'
   homepage 'http://tmkk.undo.jp/xld/index_e.html'
