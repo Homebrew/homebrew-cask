a7e5a705c8185dbd96cc0d9506517cf06607e058
Michał Ordon <designorant@users.noreply.github.com>
2016-09-21 13:54:32 +0100

    Update facebook-ios-sdk to 4.15.1 (#24688)

--- Casks/facebook-ios-sdk.rb
+++ Casks/facebook-ios-sdk.rb
@@ -1,11 +1,11 @@
 cask 'facebook-ios-sdk' do
-  version :latest
-  sha256 :no_check
+  version '4.15.1'
+  sha256 '255b0b47cbad187dbb8834ed49e917106fb1d38815a8d88d57c816ac741a4ed5'
 
-  url 'https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip'
+  url "https://origincache.facebook.com/developers/resources/?id=FacebookSDKs-iOS-#{version}.zip"
   name 'Facebook SDK for iOS'
   homepage 'https://developers.facebook.com/docs/ios'
-  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :other # https://github.com/facebook/facebook-ios-sdk/blob/master/LICENSE
 
   artifact 'AccountKit.framework', target: "#{ENV['HOME']}/Documents/FacebookSDK/AccountKit.framework"
   artifact 'AccountKitStrings.bundle', target: "#{ENV['HOME']}/Documents/FacebookSDK/AccountKitStrings.bundle"
