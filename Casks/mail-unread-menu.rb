ef6d9efc63a7b5012dec2be2da8811605e4bd42a
Artem Medvedev <amedvedev@outlook.com>
2016-09-21 13:36:08 +0300

    Fixed url version
    Sorry, fixed

--- Casks/mail-unread-menu.rb
+++ Casks/mail-unread-menu.rb
@@ -2,7 +2,7 @@ cask 'mail-unread-menu' do
   version '3.4.6'
   sha256 '869527e4a7ef34cd116856537f69f57a5c5775798c54615a9aae3e2060f166e9'
 
-  url 'http://loganrockmore.com/MailUnreadMenu/MailUnreadMenu_3.4.6.zip'
+  url "http://loganrockmore.com/MailUnreadMenu/MailUnreadMenu_#{version}.zip"
   name 'Mail Unread Menu'
   homepage 'http://loganrockmore.com/MailUnreadMenu/'
   license :gratis
