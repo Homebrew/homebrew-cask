d4c4e5c8c7f4b21e6f62a0a4c9011bf7fce1522e
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-09-22 13:01:07 -0300

    Added appcast

--- Casks/opensim.rb
+++ Casks/opensim.rb
@@ -3,8 +3,10 @@ cask 'opensim' do
   sha256 '79cc8a041f470da4f463e8d0087d928dce9c2519b5ed5a6e221d47a205bc2a42'
 
   url "https://github.com/luosheng/OpenSim/releases/download/#{version}/OpenSim.app.zip"
+  appcast 'https://github.com/luosheng/OpenSim/releases.atom',
+          checkpoint: '8e6ab4bdbed55df1cba0cf229007cee1bebb550bbb4fbf252a6ea92d5997fa82'
   name 'OpenSim'
-  homepage 'https://github.com/luosheng/OpenSim'
+  homepage 'https://github.com/luosheng/OpenSim/'
   license :mit
 
   app 'OpenSim.app'
