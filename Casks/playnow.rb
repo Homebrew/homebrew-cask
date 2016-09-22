9d9cf0680f60b34a6a9752f38e6bd3914d93094c
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-09-22 13:10:17 -0300

    Updated url, version, homepage and inserted appcast

--- Casks/playnow.rb
+++ Casks/playnow.rb
@@ -1,10 +1,12 @@
 cask 'playnow' do
-  version '1.0'
+  version '0.4'
   sha256 'c082bc144ca1991bf045ee5f416a0066c549e6adb35d49edf881a58f8dbec801'
 
-  url 'https://raw.githubusercontent.com/apalancat/PlayNow/gh/PlayNow.tgz'
+  url "https://github.com/marcboquet/PlayNow/releases/download/#{version}/PlayNow.tgz"
+  appcast 'https://github.com/marcboquet/PlayNow/releases.atom',
+          checkpoint: '8be659cc2770e4252406cd5e58c800a1de4774dfd8f5e10b7c496350e273dd51'
   name 'PlayNow'
-  homepage 'https://raw.githubusercontent.com/apalancat/PlayNow'
+  homepage 'https://github.com/marcboquet/PlayNow/'
   license :mit
 
   app 'PlayNow.app'
