613bade79e2232c3a5427239566b23e50f365cd2
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2016-09-22 23:04:29 +0200

    Also update Tunnelblick for MacOS <= Snow Leopard to version 3.5.10

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -1,7 +1,7 @@
 cask 'tunnelblick' do
   if MacOS.version <= :snow_leopard
-    version '3.5.9_build_4270.4560'
-    sha256 '7651754cab92c5f61fc22b55448875cf14fcf8b6f5b3ba469899740c49b6fae3'
+    version '3.5.10_build_4270.4563'
+    sha256 '2219f7ffcf5a5be7fb5f55945a19f6b3966e73d500feb03d8c376a0e00640ade'
   else
     version '3.6.7a_build_4603'
     sha256 '29c79907550a8077008eb4a4d71877d97dd5ede4c43272bb151908df5ac6e0b1'
