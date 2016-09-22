751543f3fba7ed5a0aa214099792b2906189efa1
Paul Jordan <paullj1@users.noreply.github.com>
2016-09-22 03:09:01 -0400

    Update hermes to 1.3.0 (#24717)

--- Casks/hermes.rb
+++ Casks/hermes.rb
@@ -1,11 +1,11 @@
 cask 'hermes' do
-  version '1.2.8'
-  sha256 '62cf30dd4f02a0e2f3d29d63b4051963724140c4f098e527ee465277d9a7a275'
+  version '1.3.0'
+  sha256 '661544619346c83c586890a93e6b68af6e86e09b8605acc92d53921c2c160911'
 
   # github.com/HermesApp/Hermes was verified as official when first introduced to the cask
   url "https://github.com/HermesApp/Hermes/releases/download/v#{version}/Hermes-#{version}.zip"
   appcast 'https://raw.githubusercontent.com/HermesApp/HermesApp.github.io/master/versions.xml',
-          checkpoint: 'f02de008d4f279cd956d828a0b36386af81947a2b6cba42f5a79915c978318ab'
+          checkpoint: 'b50dd357b9bc29039a83b1eba22065a300ee1a3d3bcb17933507ce1a2086dfd9'
   name 'Hermes'
   homepage 'http://hermesapp.org/'
   license :mit
