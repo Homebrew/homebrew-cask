032701a34d52ce6cf81448ea70c9d7f03b14d00d
Jack Barclay <jack@jacks-mbp.local>
2016-09-22 17:35:19 +0100

    Update royal-tsx to 2.2.6.1000

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -1,10 +1,10 @@
 cask 'royal-tsx' do
-  version '2.2.4.1000'
-  sha256 '1efd0a5e6605f451f913c02595967e2dc08ad86b1b394e794c1ac81361ac2f7f'
+  version '2.2.6.1000'
+  sha256 '31b6c0b325076e0fdf67d5d2e684bf789bf73396fd53d4b51feb61cec22a2b7b'
 
   url "http://v2.royaltsx.com/updates/royaltsx_#{version}.dmg"
   appcast "https://royaltsx-v#{version.major}.royalapplications.com/updates_stable.php",
-          checkpoint: '0451af62eb020e454e2b920e6f8aaa6e2883f9b990d68e44e31539765d8a3ea8'
+          checkpoint: '1c70f653f26c956862062b7dd9cf6c1ce65b3065d707edbe0abb511a1ea21d12'
   name 'Royal TSX'
   homepage 'http://www.royaltsx.com'
   license :freemium
