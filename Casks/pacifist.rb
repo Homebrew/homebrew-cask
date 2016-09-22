6dfe98ce9748f7e45aedcca1d2d5156df96e22ec
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-09-22 13:11:26 +0700

    Update pacifist (3.5.10)

--- Casks/pacifist.rb
+++ Casks/pacifist.rb
@@ -6,13 +6,13 @@ cask 'pacifist' do
     version '3.2.17'
     sha256 'd38e12293bc6087ddb09275e3c5ab34faa670e87e9dd41e04a587dd387f7b1d3'
   else
-    version '3.5.9'
-    sha256 '30effe2572c3e4538c766c06d3dea3c03176216a8a8f9c3ce13052a913990829'
+    version '3.5.10'
+    sha256 '8905a66adcc75d33475cfa1e6789f83a1fd6da1952056671e95725d1b534143e'
   end
 
   url "https://www.charlessoft.com/pacifist_download/Pacifist_#{version}.dmg"
   appcast 'https://www.charlessoft.com/cgi-bin/pacifist_sparkle.cgi',
-          checkpoint: '10a07c4a5369252470ab87e571d98d69352bf7e0826eae63c5fd4cc8349d2e08'
+          checkpoint: '11541f9dbefb04099d97228a7104e7fa290ffe0db0c703c7c89f66a8220f85de'
   name 'Pacifist'
   homepage 'https://www.charlessoft.com/'
   license :commercial
