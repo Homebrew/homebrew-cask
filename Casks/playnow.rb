253fc6da712781aca35b5a908ff9789d44f66698
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-09-22 11:01:34 -0300

    Add PlayNow.app v1.0

---
+++ Casks/playnow.rb
@@ -0,0 +1,11 @@
+cask 'playnow' do
+  version '1.0'
+  sha256 'c082bc144ca1991bf045ee5f416a0066c549e6adb35d49edf881a58f8dbec801'
+
+  url 'https://raw.githubusercontent.com/apalancat/PlayNow/gh/PlayNow.tgz'
+  name 'PlayNow'
+  homepage 'https://raw.githubusercontent.com/apalancat/PlayNow'
+  license :mit
+
+  app 'PlayNow.app'
+end
