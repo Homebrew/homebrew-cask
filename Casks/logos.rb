ebc6c972dc3fffc44c4b7ebd528f8626753379eb
ickc <ickc@users.noreply.github.com>
2016-09-22 01:03:27 -0700

    Add Logos Bible Software 6.14.0.65 (#24705)

---
+++ Casks/logos.rb
@@ -0,0 +1,11 @@
+cask 'logos' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.logos.com/cgi/DownloadFile.aspx?ID=194'
+  name 'Logos Bible Software'
+  homepage 'https://www.logos.com'
+  license :commercial
+
+  app 'Logos.app'
+end
