bbbc179df10a3b99bdb4e82c3b669e5c3e0535aa
filosfino <filosfino@gmail.com>
2016-09-23 13:44:36 +0800

    fix homepage

--- Casks/maxiang.rb
+++ Casks/maxiang.rb
@@ -7,7 +7,7 @@ cask 'maxiang' do
   name 'ma ke fei xiang'
   name 'marxico'
   name 'maxiang'
-  homepage 'https://maxiang.io/'
+  homepage 'https://github.com/filosfino/apps/raw/master/Maxiang.dmg'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app '马克飞象.app'
