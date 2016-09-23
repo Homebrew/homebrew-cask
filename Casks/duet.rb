da266d51b0f673674b18bf5b32e43c9650a42efd
Victor Popkov <victor@popkov.me>
2016-09-23 20:29:44 +0300

    Update duet to 1.6.0.2

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -1,11 +1,11 @@
 cask 'duet' do
-  version '1.5.9.6'
-  sha256 '54464c6c20a0b208fea8096e41aafe9aa4a30887f5327df0f4bd34ee46e152cb'
+  version '1.6.0.2'
+  sha256 'ea56fecb6b7a0af97836e138009f905ae394c4be832fedac4b44542972707bc7'
 
   # d2ycb980mbr5lq.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2ycb980mbr5lq.cloudfront.net/#{version.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip"
   appcast 'https://updates.duetdisplay.com/checkMacUpdates',
-          checkpoint: '0fd19ed1b7880600902c308a5fbf1f727d745b87540a00e2c7da902ed3dc44e9'
+          checkpoint: '742afd0dee11d3c6ed50531ceb6b7230d9b3a1331aea6e553b9a0dba9f68347f'
   name 'Duet'
   homepage 'http://www.duetdisplay.com/'
   license :gratis
