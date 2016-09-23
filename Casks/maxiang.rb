621b8a6f4f4944ce603367f22ff3440ca8a803dc
filosfino <filosfino@gmail.com>
2016-09-23 13:41:20 +0800

    fix name, version, sha256

--- Casks/maxiang.rb
+++ Casks/maxiang.rb
@@ -1,9 +1,11 @@
 cask 'maxiang' do
-  version ':latest'
-  sha256 'ba9f2f32bc6c68ac732d0131d1e2a74aae2125a44012a0f6c1e975f7a4676419'
+  version :latest
+  sha256 :no_check
 
   # github.com/filosfino was verified as official when first introduced to the cask
   url 'https://github.com/filosfino/apps/raw/master/Maxiang.dmg'
+  name 'ma ke fei xiang'
+  name 'marxico'
   name 'maxiang'
   homepage 'https://maxiang.io/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
