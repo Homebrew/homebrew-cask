26f4a1eac4261b24ff7e7270461a60eeb4518084
Toni Suter <tonisuter@users.noreply.github.com>
2016-09-23 10:38:20 +0200

    Updated tifig to v0.4.0 (#24761)

--- Casks/tifig.rb
+++ Casks/tifig.rb
@@ -1,6 +1,6 @@
 cask 'tifig' do
-  version '0.3.0-201607151151'
-  sha256 '694a1ae486b716e5afaf2ba5244987a672d7aa04432de82383002a43d74164c6'
+  version '0.4.0-201609230646'
+  sha256 '9835efb6d4e7931a83fe0eb860ab45cbdb8d3178815d7076212a6bc70af4ba0e'
 
   # tifig-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://tifig-downloads.s3.amazonaws.com/tifig-#{version}-macosx.cocoa.x86_64.tar.gz"
