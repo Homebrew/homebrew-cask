6294e2b5f3569c058e291365cf7a644651e762c9
Ben Chapman <ben.chapman@hbo.com>
2016-09-23 15:07:26 -0400

    Updates based upon PR feedback

--- Casks/nvalt.rb
+++ Casks/nvalt.rb
@@ -7,12 +7,12 @@ cask 'nvalt' do
   else
     version '2.2b120'
     sha256 'e3ebdc012d5bba6e0e46173daa7c7548eca947958355b37d6ae6040e85b79153'
-    # abyss.designheresy.com/ was verified as official when first introduced to the cask
-    url "http://abyss.designheresy.com/nvALT#{version.sub(%r{b}, '')}.zip"
+    # abyss.designheresy.com was verified as official when first introduced to the cask
+    url "http://abyss.designheresy.com/nvALT#{version.delete('b')}.zip"
+    appcast "http://abyss.designheresy.com/nvalt#{version.major}/nvalt#{version.major}main.xml",
+            checkpoint: 'e9855b4e389ddce8ad13f5c9a325cd728c85a030f43b633e36bc31303910d34e'
   end
 
-  appcast 'http://abyss.designheresy.com/nvalt2/nvalt2main.xml',
-          checkpoint: 'e9855b4e389ddce8ad13f5c9a325cd728c85a030f43b633e36bc31303910d34e'
   name 'nvALT'
   homepage 'http://brettterpstra.com/project/nvalt/'
   license :bsd
