eb08f63728848569ec6b08999f33e6e5eb6fdde0
Danny Korpan <danny.korpan@mailbox.org>
2016-09-24 08:51:45 +0200

    Update mucommander.rb

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -2,7 +2,7 @@ cask 'mucommander' do
   version '0.9.0'
   sha256 '24665a248bd0f027b399277c2e7096a57e75120048302f3ac2f89ce4f7f1acae'
 
-  url "http://www.mucommander.com/download/mucommander-#{version.dots_to_underscores}.dmg"
+  url "https://github.com/mucommander/mucommander/releases/download/{version}/mucommander-#{version.dots_to_underscores}.dmg"
   name 'muCommander'
   homepage 'http://www.mucommander.com/'
   license :gpl
