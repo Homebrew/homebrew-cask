a8e488711148cdb23d6654ec6762db6be63df2be
Vítor Galvão <info@vitorgalvao.com>
2016-09-24 14:18:45 +0100

    Update private-internet-access to latest (#24814)

--- Casks/private-internet-access.rb
+++ Casks/private-internet-access.rb
@@ -7,8 +7,7 @@ cask 'private-internet-access' do
   homepage 'https://www.privateinternetaccess.com'
   license :gratis
 
-  installer script: 'Private Internet Access Installer.app/Contents/MacOS/runner.sh',
-            sudo:   true
+  installer manual: 'Private Internet Access Installer.app'
 
   postflight do
     set_ownership '~/.pia_manager'
