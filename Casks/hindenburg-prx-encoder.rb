fb92f6e080baf1995bc5d95579e78879e52172c4
Chris Kalafarski <chris@farski.com>
2016-09-23 20:39:02 -0400

    Add Hindenburg PRX Encoder Latest

---
+++ Casks/hindenburg-prx-encoder.rb
@@ -0,0 +1,11 @@
+cask 'hindenburg-prx-encoder' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://hindenburg.com/support/downloads/HindenburgPRXEncoder.dmg'
+  name 'Hindenburg PRX Encoder'
+  homepage 'https://hindenburg.com/prx-encoder'
+  license :gratis
+
+  app 'Hindenburg PRX Encoder.app'
+end
