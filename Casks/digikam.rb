d56446baf9c15bf1d3f2a0e01d8ebb3eac888ae9
Tobias Erthal <tobias.erthal@gmail.com>
2016-09-24 21:06:18 +0200

    Updated digikam to 5.2.0-02

--- Casks/digikam.rb
+++ Casks/digikam.rb
@@ -1,6 +1,6 @@
 cask 'digikam' do
-  version '5.1.0-01'
-  sha256 '39c7f55f9c3c8b02cc56488607f42acb8c611b5dbcc8a3e7099eb651e18563f1'
+  version '5.2.0-02'
+  sha256 '78f2f57f4a49c99792c1c656a7bdcaef3404c85df019d8b031c99b364087e5a5'
 
   # kde.org/stable/digikam was verified as official when first introduced to the cask
   url "http://download.kde.org/stable/digikam/digiKam-#{version}-MacOS-x86-64.pkg"
