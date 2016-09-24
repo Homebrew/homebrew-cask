63e5897316d85829989fd94f717980399ebe338e
Victor Popkov <victor@popkov.me>
2016-09-24 22:55:03 +0300

    Update pollev-presenter to 2.8.1

--- Casks/pollev-presenter.rb
+++ Casks/pollev-presenter.rb
@@ -1,11 +1,11 @@
 cask 'pollev-presenter' do
-  version '2.8.0'
-  sha256 '20e9222a0a109bb7588b1fd34848402c2a64350ac3e9bc020b8bc97772a33d06'
+  version '2.8.1'
+  sha256 '93f74cca3589a16ab202705de684df00795ab6455ce7b7dd00730be35176e30c'
 
   # amazonaws.com/polleverywhere-app was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/polleverywhere-app/mac-beta/pollev.dmg'
   appcast 'https://polleverywhere-app.s3.amazonaws.com/mac-beta/appcast.xml',
-          checkpoint: 'f46feb4920dbbba86d8385db1b4094c8ddb2da092258a09cca705d285df6316d'
+          checkpoint: '6ebc1c92694e53bf219a540880fe16b2cb280c2a8e38084144076029d2b74d55'
   name 'PollEv Presenter'
   homepage 'https://www.polleverywhere.com/'
   license :gratis
