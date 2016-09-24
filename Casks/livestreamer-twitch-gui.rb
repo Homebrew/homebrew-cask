e2b25272c08d67b431b873409712d6d418ae25dd
Nick Maher <nick.maher@appearhere.co.uk>
2016-09-24 02:27:08 +0100

    Update livestreamer-twitch-gui to 0.15.2

--- Casks/livestreamer-twitch-gui.rb
+++ Casks/livestreamer-twitch-gui.rb
@@ -1,10 +1,10 @@
 cask 'livestreamer-twitch-gui' do
-  version '0.14.2'
-  sha256 '8f23cdea09c3ab5f54dfab2f34304dd1ee9af1174dd4f227c6bdaf0d3847ddb5'
+  version '0.15.2'
+  sha256 'a7f650e72c05e4fb13a66184a2d46524aeea671b172ceba2db62efe63b5a09ae'
 
   url "https://github.com/bastimeyer/livestreamer-twitch-gui/releases/download/v#{version}/livestreamer-twitch-gui-v#{version}-osx64.tar.gz"
   appcast 'https://github.com/bastimeyer/livestreamer-twitch-gui/releases.atom',
-          checkpoint: '62fad10fd3c6c0fc19b9a7438314fd8950045a6ba3886e48819905cee12814be'
+          checkpoint: 'f22b92d8a4c51bac99ac09386bcfd4a763b5c68298987d30bf97455ec0572182'
   name 'Livestreamer Twitch GUI'
   homepage 'https://github.com/bastimeyer/livestreamer-twitch-gui'
   license :mit
