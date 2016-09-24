1b856dbfc465c8ca1802e8e460c7d0c541b021b8
Artem Medvedev <amedvedev@outlook.com>
2016-09-24 07:55:49 +0300

    Update folx to 5.1

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -1,10 +1,10 @@
 cask 'folx' do
-  version '5.0.13493'
-  sha256 'bfc084391da36755747300645fc0e69d5dd04e064436c2a0d462219918a5e500'
+  version '5.1'
+  sha256 '22a9d4a37444b91ad2f66625839eea706ba5310c66d8d796e7f5b1691ab10f3a'
 
   url "http://www.eltima.com/download/folx-update/downloader_mac_#{version}.dmg"
   appcast 'http://mac.eltima.com/download/folx-updater/folx.xml',
-          checkpoint: '36884a9a0153ed9afdfb7e41383a14789ac27d951102626909057b2ae209c45f'
+          checkpoint: 'af8da469e29f9348247d26748f9ba6cadafee62fbc09606bf766974c1da920ae'
   name 'Folx'
   homepage 'http://mac.eltima.com/download-manager.html'
   license :freemium
