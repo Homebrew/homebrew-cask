c0df9c99dbcccf01dbbc57607690b4a7f9c6f2f6
chincheta0815 <Rouven_Weiler@gmx.net>
2016-09-24 12:14:15 +0200

    Corrected version number string.

--- Casks/mp3tag.rb
+++ Casks/mp3tag.rb
@@ -1,5 +1,5 @@
 cask 'mp3tag' do
-  version '2.7.9'
+  version '2.79'
   sha256 '15301427664658f3cd38f7156b1ff95891198d637a298baea68b236440581e80'
 
   url "http://download.mp3tag.de/mp3tagv#{version.delete('.')}-MacOSX-Wine.zip"
