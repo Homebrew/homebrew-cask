bbce8a4fb203192af9f556f6c7fa04eba47a6126
HuangKai <mail.huangkai@qq.com>
2016-09-24 22:18:20 +0800

    Update sketch.rb
    Update sketch.rb version to 40

--- Casks/sketch.rb
+++ Casks/sketch.rb
@@ -1,24 +1,11 @@
 cask 'sketch' do
-  version '39.1'
-  sha256 'da8582b6d67b0189d98dfa9aad441e6b68aa0048389aee44fa5acad4d014b127'
-
-  url "https://download.sketchapp.com/sketch-#{version}.zip"
-  appcast 'https://www.sketchapp.com/appcast3.xml',
-          checkpoint: 'b258be21946179f9b6e867585d139668af663428b80b5b0a84f21e35471543c9'
+  version latest
+  sha256 no_check
+  
+  url "https://sketchapp.com/static/download/sketch.zip"
   name 'Sketch'
   homepage 'https://www.sketchapp.com/'
   license :commercial
 
   app 'Sketch.app'
-
-  zap delete: [
-                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl',
-                '~/Library/Application Support/com.bohemiancoding.sketch3',
-                '~/Library/Caches/com.bohemiancoding.sketch3',
-                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3',
-                '~/Library/Logs/com.bohemiancoding.sketch3',
-                '~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist',
-                '~/Library/Preferences/com.bohemiancoding.sketch3.plist',
-                '~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies',
-              ]
 end
