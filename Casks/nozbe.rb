7a587454cab7b8563f649d264dff043ebef0f908
Igor Kapkov <igasgeek@me.com>
2016-09-25 22:19:05 +1000

    Update Nozbe to 3.2.3

--- Casks/nozbe.rb
+++ Casks/nozbe.rb
@@ -1,6 +1,6 @@
 cask 'nozbe' do
-  version '3.2.2'
-  sha256 '9bed6eb0736cef1ff8749189cc21760c11edbeded238b1ac3056575963716f27'
+  version '3.2.3'
+  sha256 '0befb34fd98fe763417dc9809bf9a778b41d54603b5e51930d0c9523400cff66'
 
   url "https://files.nozbe.com/#{version.no_dots}/Nozbe.app.zip"
   name 'Nozbe'
