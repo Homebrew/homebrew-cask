69ffd55219afc00476b2fe7e6453d8af90ea73f0
Victor Popkov <victor@popkov.me>
2016-09-25 21:41:49 +0300

    Update commander-one to 1.6

--- Casks/commander-one.rb
+++ Casks/commander-one.rb
@@ -1,10 +1,10 @@
 cask 'commander-one' do
-  version '1.5.2'
-  sha256 '9994e89a5821714741b421051d05cc5cc760439fe8ea66381338f82c842ecb73'
+  version '1.6'
+  sha256 'e5474e14be688ed98ec3d78c47f7fefb1c7f2b8e392cb27e5e04295573aabbe5'
 
   url 'http://mac.eltima.com/download/commander.dmg'
   appcast 'http://www.eltima.com/download/commander-update/settings.xml',
-          checkpoint: '618d262bf47c8b972b7d8878549b6ee978e30d474b1546676946c35aa688d60e'
+          checkpoint: 'ad715e45624b992a2cd8c2ca6f1c5598d7527a68cffcd9b748d8360eb31ffb46'
   name 'Commander One'
   homepage 'http://mac.eltima.com/file-manager.html'
   license :freemium
