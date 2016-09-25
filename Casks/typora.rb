edcb407927c475f93b96654b74647530a2e04faa
zhuyue <zhuyue.sut@icloud.com>
2016-09-26 06:09:38 +0800

    Update typora.rb

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,10 +1,10 @@
 cask 'typora' do
-  version '0.9.9.7.6'
-  sha256 '2daae8b119c99b48c4b2d7199622af5b733b51270d0d46b8795fb5e3b32ff44b'
+  version '0.9.9.7.8'
+  sha256 '878835b97dc53f51b8bd0c934e9c2511eafc6a607e1e3b7792c388620430690b'
 
   url "https://www.typora.io/download/typora_#{version}.zip"
   appcast 'https://www.typora.io/download/dev_update.xml',
-          checkpoint: 'a856ddacf753bd32120c79adbd680c2d063e35bf8d2f95ea672d0ca6f7ab4e2b'
+          checkpoint: '38c50230fe61d7093ae1fd8b1763db4f7df37a1269dd5b29ef7257a9bd07e0fd'
   name 'Typora'
   homepage 'https://typora.io'
   license :gratis
