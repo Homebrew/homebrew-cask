cbfaa9dc690927d8ca91b1ee338d2fc4ad4748e3
slack4u <slack4u@users.noreply.github.com>
2016-09-25 16:33:23 +0900

    Update visual-studio-code to 1.5.3

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.5.2,66f37fd2a99eb9d628dd374d81d78835b410c39b'
-  sha256 '478104e63d5c687ac03f59cbbb86e7ae20cd3b8ac47ca780dae1aaecc84f9400'
+  version '1.5.3,5be4091987a98e3870d89d630eb87be6d9bafd27'
+  sha256 '78993c1bb27b76d2bd60c01f85b8b5f297bb8671a1c2799d7ab678b9841d20e1'
 
   # az764295.vo.msecnd.net was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
