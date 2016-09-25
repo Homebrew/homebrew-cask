a4ad72c69f6f0d18e40d1465374040b08ebee883
Aditya Dalal <adidalal@users.noreply.github.com>
2016-09-25 17:29:38 -0400

    Update black-screen to 0.2.48,0.2.53

--- Casks/black-screen.rb
+++ Casks/black-screen.rb
@@ -1,10 +1,10 @@
 cask 'black-screen' do
-  version '0.2.48'
+  version '0.2.48,0.2.53'
   sha256 '38f5c7c252e14874d4c7d5202774e108c6b6519fbed00f0c719d3a6438a66a8f'
 
-  url "https://github.com/shockone/black-screen/releases/download/v#{version}/black-screen-#{version}-mac.zip"
+  url "https://github.com/shockone/black-screen/releases/download/v#{version.after_comma}/black-screen-#{version.before_comma}-mac.zip"
   appcast 'https://github.com/shockone/black-screen/releases.atom',
-          checkpoint: '8a7cd91f6e9f869c06663e31e22e65770b2f2efa1860b53f377274d4f650acc9'
+          checkpoint: 'ed0760f5f8966c444931340edfc7cd8945eb5a7521ca8fd71c21d71c4b483f51'
   name 'Black Screen'
   homepage 'https://github.com/shockone/black-screen'
   license :mit
