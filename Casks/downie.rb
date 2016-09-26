28526da6387f31efe7aac90d68153833eb1f07fb
Victor Popkov <victor@popkov.me>
2016-09-26 20:10:33 +0300

    Update downie to 2.5.2,1318

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,10 +1,10 @@
 cask 'downie' do
-  version '2.5.1,1315'
-  sha256 '85bc2bde072ff09dd8e60c466603d7db53b27559574dafd1c5f14883d9194b9a'
+  version '2.5.2,1318'
+  sha256 'c459c394108d18c09b8616e77f7540f130d1c0e210d587cbcbbedb63de908d94'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.after_comma}.zip"
   appcast 'https://trial.charliemonroe.net/downie/updates_2.3.xml',
-          checkpoint: '0afdfd5ab1bcd1944bc28d27b462f05a2f5b29fd7a18795bae68cc7a9fad9cd9'
+          checkpoint: '1f68df6f83f4eac6e5da43e455ccb39150e1c21b38da915b01f0bab7c6e927c1'
   name 'Downie'
   homepage 'https://software.charliemonroe.net/downie.php'
   license :commercial
