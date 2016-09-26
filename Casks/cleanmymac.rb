b2748705ae6d8e51ba4b0020a4bb78273009f3fe
Victor Popkov <victor@popkov.me>
2016-09-26 20:07:37 +0300

    Update cleanmymac to 1.12,1474717668 and 2.5.0,1473949812

--- Casks/cleanmymac.rb
+++ Casks/cleanmymac.rb
@@ -1,22 +1,22 @@
 cask 'cleanmymac' do
   if MacOS.version <= :snow_leopard
-    version '1.11,1417522595'
-    sha256 'ac5d4bf36882dd34bdb0a68eb384a6b3aba355be896d03dfa40a120c6bef4a0d'
+    version '1.12,1474717668'
+    sha256 '8cdc59584eff5b7662bdb069168bbbed76a3de8988db9635163c13df468d812f'
 
     # devmate.com/com.macpaw.CleanMyMac was verified as official when first introduced to the cask
     url "https://dl.devmate.com/com.macpaw.CleanMyMac/#{version.major_minor}/#{version.after_comma}/CleanMyMacClassic-#{version.major_minor}.zip"
     appcast 'https://updates.devmate.com/com.macpaw.CleanMyMac.xml',
-            checkpoint: 'd9ac93bf9f995dc0903a668b357dadbe7cb187bc1382f6183bc40e5d79a26802'
+            checkpoint: '0dc743954653b4623c9ff60ec7965b09e2f36ed2bfacd6c2563c7844fa347138'
     app 'CleanMyMac.app'
     # TODO: add uninstall and zap stanzas for legacy app
   elsif MacOS.version <= :lion
-    version '2.4,1443544143'
-    sha256 '0d08f4d9b36493359f6ca3ff2f96a9b769a8eed4ea017ecbb2d5644f75aafad0'
+    version '2.5.0,1473949812'
+    sha256 '9f8513d39e3b2f21ac76e94c370561fe582f9d50937070ef6fc58ed4506353bf'
 
     # devmate.com/com.macpaw.CleanMyMac2 was verified as official when first introduced to the cask
     url "https://dl.devmate.com/com.macpaw.CleanMyMac2/#{version.major_minor_patch}/#{version.after_comma}/CleanMyMac#{version.major}-#{version.major_minor_patch}.zip"
     appcast "https://updates.devmate.com/com.macpaw.CleanMyMac#{version.major}.xml",
-            checkpoint: 'd9ac93bf9f995dc0903a668b357dadbe7cb187bc1382f6183bc40e5d79a26802'
+            checkpoint: 'e29bde675d18732b441d8bb7e0850356a9ef2192efc68276ec1c146904ae1c13'
     app "CleanMyMac #{version.major}.app"
 
     uninstall launchctl: "com.macpaw.CleanMyMac#{version.major}.Agent"
