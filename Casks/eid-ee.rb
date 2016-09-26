09be1b5a166fb07c4ff17b65aabddd2c82f47852
Jaan Toots <jaantoots@gmail.com>
2016-09-26 19:04:30 +0300

    Add Estonian Open-EID 3.12.4 as eid-ee
    https://github.com/Homebrew/homebrew-cask-eid/pull/14

---
+++ Casks/eid-ee.rb
@@ -0,0 +1,22 @@
+cask 'eid-ee' do
+  version '3.12.4.1666'
+  sha256 'a5a7b2824da4efb9479e4b70a952981d41c50a75b00839d0216133b04e32b5b5'
+
+  url "https://installer.id.ee/media/osx/Open-EID_#{version}.dmg"
+  name 'Electronic identity card software for Estonia'
+  name 'eID Estonia'
+  homepage 'https://installer.id.ee/?lang=eng'
+  license :gpl
+
+  pkg 'Open-EID.pkg'
+
+  uninstall script: {
+                      executable: 'uninstall.sh',
+                      input:      %w[y],
+                    }
+
+  caveats <<-EOS.undent
+    DigiDoc3 Client and ID-card Utility are available in the App Store:
+      http://appstore.com/mac/ria
+  EOS
+end
