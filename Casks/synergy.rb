001f9b419ebb4441122d9c7e8a64daa795c32774
Adrian Petrescu <apetresc@gmail.com>
2016-09-26 10:19:05 -0400

    Update synergy to 1.8.3 (#24876)

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,6 +1,6 @@
 cask 'synergy' do
-  version '1.8.2,36cd521'
-  sha256 '07851d85a7cbb5875c8be7111880d3ab5213b0ef48c63ab1b4dfef8defdd6bcc'
+  version '1.8.3,db9181b'
+  sha256 '9c20556b79691ab45d4cd96ed56e4f67f223fee5d7763e29001e705b44c21b97'
 
   url "https://symless.com/files/packages/synergy-v#{version.before_comma}-stable-#{version.after_comma}-MacOSX1011-x86_64.dmg"
   name 'Synergy'
