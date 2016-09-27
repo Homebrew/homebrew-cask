c16da7788f0eaa953cd7fc6e4255596cc0271ace
Victor Popkov <victor@popkov.me>
2016-09-27 23:01:31 +0300

    Update cocoapacketanalyzer to 1.50 (#24907)

--- Casks/cocoapacketanalyzer.rb
+++ Casks/cocoapacketanalyzer.rb
@@ -1,10 +1,10 @@
 cask 'cocoapacketanalyzer' do
-  version '1.31'
-  sha256 'fc3586868fcf90e3ca766c31dbf184de78e5c907210dfdabbd13a831712fc864'
+  version '1.50'
+  sha256 'cd6549fb466fb73f5e22c82112d708513dfa903b151167f2d6112d9117efb017'
 
   url "http://tastycocoabytes.com/_downloads/CPA_#{version.no_dots}.dmg"
   appcast 'http://www.tastycocoabytes.com/cpa/updates/appcasting.php',
-          checkpoint: '9813fa52c69e2e54f5ba108352aa7d4a570a136a66dd4d94fb9b10ff959430e3'
+          checkpoint: '7de1cd4c3d499c5ece7441cdc66c9c729ff12648c3528cd2fa7da907660f443e'
   name 'Cocoa Packet Analyzer'
   homepage 'http://www.tastycocoabytes.com/cpa/'
   license :gratis
