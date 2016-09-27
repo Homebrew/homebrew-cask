025b1b9ce01844724be882e126b56d86b5dc5f3c
Victor Popkov <victor@popkov.me>
2016-09-28 00:05:57 +0300

    Update xtabulator to 2.003 (#24916)

--- Casks/xtabulator.rb
+++ Casks/xtabulator.rb
@@ -1,10 +1,10 @@
 cask 'xtabulator' do
-  version '2.002'
-  sha256 'a4ec6d302f233599034c51cbc5e09ff42964022e694487f1793185c5105d5ddc'
+  version '2.003'
+  sha256 '041d8a415ebc4e2bba615d887f6ba3dcb17f34b59a779db2c592dbb45ad2037d'
 
   url "http://www.bartastechnologies.com/products/xtabulator/sparkleupdates/#{version}.zip"
   appcast 'http://www.bartastechnologies.com/products/xtabulator/sparkleupdates/xtappcast.php',
-          checkpoint: '6d9bec45bf8d4214eb1973876728c622ca8cd476c3b16430f63433693443359f'
+          checkpoint: '9107d1169ca979ff224be09bf6981df865ad2d3666313afe2e72716eb882712d'
   name 'XTabulator'
   homepage 'http://www.bartastechnologies.com/products/xtabulator/'
   license :commercial
