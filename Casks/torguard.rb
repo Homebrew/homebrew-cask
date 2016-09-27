d01edce40c0a74c3cfac8be3b06de5e43783776c
Eddie Gurnee <pegurnee@users.noreply.github.com>
2016-09-27 18:47:21 -0400

    Update torguard to 0.3.53 (#24922)

--- Casks/torguard.rb
+++ Casks/torguard.rb
@@ -1,11 +1,11 @@
 cask 'torguard' do
-  version '0.3.50'
-  sha256 '79bc2ffb6bf04412429e3dff17c1eee82e35f11845298e06e9b90e5bf305e320'
+  version '0.3.53'
+  sha256 'dea26a748d3ed25e389690539e79f15d6815c89d2dd7b20a80265ded45822847'
 
   # torguard.biz was verified as official when first introduced to the cask
   url "https://updates.torguard.biz/Software/MacOSX/TorGuard-v#{version}.dmg"
   appcast 'https://updates.torguard.biz/Software/MacOSX/checksums.sha256',
-          checkpoint: '990152a87c456dac5141d16f607d21128c52079affb49aaecddcce516e42cf30'
+          checkpoint: '0df2c241fd91173c70abe2fb8900c39b75e1fe19e91df5d4eba6c59e1c32f48b'
   name 'TorGuard'
   homepage 'https://torguard.net/'
   license :unknown
