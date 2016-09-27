d209287e0fbba8d91ab4321b19107b9d4df2f38d
Jeremy Saklad <saklad5@me.com>
2016-09-27 14:28:57 -0500

    Update graphicconverter.rb

--- Casks/graphicconverter.rb
+++ Casks/graphicconverter.rb
@@ -4,7 +4,7 @@ cask 'graphicconverter' do
 
   # lemkesoft.info was verified as official when first introduced to the cask
   url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.minor}.zip"
-  appcast "http://www.lemkesoft.org/files/graphicconverter/graphicconverter{version.major}.xml",
+  appcast "http://www.lemkesoft.org/files/graphicconverter/graphicconverter#{version.major}.xml",
           checkpoint: '621f0918d123dd684b30e03995c383469a893408dc180aba7a760e87c0d1cf6f'
   name 'GraphicConverter'
   homepage 'https://www.lemkesoft.de/en/products/graphicconverter/'
