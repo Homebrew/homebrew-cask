24b7276cb70521a652f4063de6e12ac2c03fc791
Liu Xinan <xinan@me.com>
2016-09-27 18:38:15 +0800

    Update dripcap to 0.3.10 (#24892)

--- Casks/dripcap.rb
+++ Casks/dripcap.rb
@@ -1,11 +1,11 @@
 cask 'dripcap' do
   version '0.3.10'
-  sha256 '7680f1aaa75c8c16a4e749383a608e892ae418cf2d9854823342ecb3c3627f0e'
+  sha256 'da431b636ba7fa7d5d286ccfac2a940e4c7a426f436fe9f17e140531e70c5274'
 
   # github.com/dripcap was verified as official when first introduced to the cask
   url "https://github.com/dripcap/dripcap/releases/download/v#{version}/dripcap-darwin-amd64.dmg"
   appcast 'https://github.com/dripcap/dripcap/releases.atom',
-          checkpoint: '2ece84a697f186a233ed4858669fbbcf76fc0b8223eac787a405868eed8be66a'
+          checkpoint: 'eaf3729dcdc2a4e441028b803b20829963ef63b4809d0c753b148dd4f5bcf833'
   name 'Dripcap'
   homepage 'https://dripcap.org/'
   license :mit
