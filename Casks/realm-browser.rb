72de967c822f60972d64712b340717e3081a2de2
Ali Ashoor <ali.ash.kh@gmail.com>
2016-09-27 12:34:43 +0300

    Update realm-browser.rb

--- Casks/realm-browser.rb
+++ Casks/realm-browser.rb
@@ -1,8 +1,8 @@
 cask 'realm-browser' do
-  version '0.101.0'
+  version '0.1.2'
   sha256 '2bda0e80feaa09d799822d0893498d697a159fe1393c6e426d9c01f61b09ca5d'
 
-  url "https://github.com/realm/realm-browser-osx/releases/download/#{version}/RealmBrowser#{version}.zip"
+  url "https://github.com/realm/realm-browser-osx/releases/download/#{version}/RealmBrowser-#{version}.zip"
   appcast 'https://github.com/realm/realm-browser-osx/releases.atom',
           checkpoint: '29539a7cfaea5e2c001b8e4d42a5430184493b5f5be8316225f04818f05b2e79'
   name 'Realm Browser'
