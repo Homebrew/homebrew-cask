8ccaad1ed99f3621c86b9b25be0a59542aa7119f
Victor Popkov <victor@popkov.me>
2016-09-27 21:34:31 +0300

    Update data-rescue to 4.3

--- Casks/data-rescue.rb
+++ Casks/data-rescue.rb
@@ -1,10 +1,10 @@
 cask 'data-rescue' do
-  version '4.2.3'
-  sha256 '1b2502bcba662ee01f59b9f118e7332d0d4f9d59a0b44f00abe0bdbfb37bb53f'
+  version '4.3'
+  sha256 '64b9571df129a5f9a1a48b4b7c69a1f590e25e38ef6ea7a46952463d0317f45a'
 
   url "https://downloads.prosofteng.com/drmac/Data_Rescue_#{version}.dmg"
   appcast 'https://www.prosofteng.com/resources/dr4/dr4_appcast.xml',
-          checkpoint: '178efedbf100f9e99fcc8ca74d53a6017033559af1ab62b14a82b2ee620aa226'
+          checkpoint: 'b21030a529148a9ad0dd85bd6b3a075709e6386b53f40eeab31e33daf119d768'
   name 'Data Rescue 4'
   homepage 'https://www.prosofteng.com/products/data_rescue.php'
   license :commercial
