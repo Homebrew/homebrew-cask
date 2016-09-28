f64deede56cec5b4d557c6d3f3401ff932cc9e6c
Artem Medvedev <amedvedev@outlook.com>
2016-09-28 16:30:21 +0300

    Update vlc-webplugin to 2.2.4 (#24945)

--- Casks/vlc-webplugin.rb
+++ Casks/vlc-webplugin.rb
@@ -1,6 +1,6 @@
 cask 'vlc-webplugin' do
-  version '2.2.2'
-  sha256 '6ff4272c0a28b18e388e17841535a2d719d54c98a4558c212a393d5d269a91ca'
+  version '2.2.4'
+  sha256 'ceeece6670427eca2499133f6b4d066aa0d20058d0526a42462bee8de819a563'
 
   url "https://get.videolan.org/vlc/#{version}/macosx/VLC-webplugin-#{version}.dmg"
   name 'VLC for OS X web plugin'
