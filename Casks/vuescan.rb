f6c676a6623ac0f15894b10adefa9783e28e12d8
Vítor Galvão <info@vitorgalvao.com>
2016-09-28 10:51:24 +0100

    vuescan.rb: use version methods

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -3,9 +3,9 @@ cask 'vuescan' do
   sha256 :no_check # required as upstream package is updated in-place
 
   if Hardware::CPU.is_32_bit?
-    url "https://www.hamrick.com/files/vuex32#{version.sub(%r{^(\d+)\.(\d+).*}, '\1\2')}.dmg"
+    url "https://www.hamrick.com/files/vuex32#{version.major_minor.no_dots}.dmg"
   else
-    url "https://www.hamrick.com/files/vuex64#{version.sub(%r{^(\d+)\.(\d+).*}, '\1\2')}.dmg"
+    url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   end
 
   name 'VueScan'
