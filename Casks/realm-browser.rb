da38d370d9b6ed658589419a8a4dc70748f84b41
Aditya Dalal <adidalal@users.noreply.github.com>
2016-09-27 22:04:50 -0400

    Update realm-browser to 2.0.0

--- Casks/realm-browser.rb
+++ Casks/realm-browser.rb
@@ -1,10 +1,10 @@
 cask 'realm-browser' do
-  version '0.101.0'
-  sha256 '2bda0e80feaa09d799822d0893498d697a159fe1393c6e426d9c01f61b09ca5d'
+  version '2.0.0'
+  sha256 '9e1e6493829e960282d877e8a82eae244370422c578c46ba7dbd975a79f07772'
 
-  url "https://github.com/realm/realm-browser-osx/releases/download/#{version}/RealmBrowser#{version}.zip"
+  url "https://github.com/realm/realm-browser-osx/releases/download/v#{version}/RealmBrowser-#{version}.zip"
   appcast 'https://github.com/realm/realm-browser-osx/releases.atom',
-          checkpoint: '29539a7cfaea5e2c001b8e4d42a5430184493b5f5be8316225f04818f05b2e79'
+          checkpoint: '47f78f20dbd0320e1f9f584b86fcac736f14e2c100eb8bd4bfc2f9000552b64f'
   name 'Realm Browser'
   homepage 'https://github.com/realm/realm-browser-osx/'
   license :apache
