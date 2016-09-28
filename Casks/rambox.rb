405beb69063cdd94f02b3371b19bf0b34e090168
kuma yuta <kinisinakuteiiyo@gmail.com>
2016-09-28 16:43:18 +0900

    Update rambox to 0.4.4 (#24932)

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -1,11 +1,11 @@
 cask 'rambox' do
-  version '0.4.2'
-  sha256 '329b0c4618cd2d77bbea3b751fee485b0be7954bc1b5b0615ec3c9c1d1f021b5'
+  version '0.4.4'
+  sha256 '3c14ac937e9f81983175cdfc1e978fe7070799844f1d3916841b54a74b61c3c4'
 
   # github.com/saenzramiro/rambox was verified as official when first introduced to the cask
   url "https://github.com/saenzramiro/rambox/releases/download/#{version}/Rambox-#{version}-mac.zip"
   appcast 'https://github.com/saenzramiro/rambox/releases.atom',
-          checkpoint: '63cd52a314b6b9aa5b965b49fd04f64d534db32dd179635a54517a76341ca231'
+          checkpoint: 'e20b6c46e8ccf41ab8b6cedf643d42e1cc92759f803910872ee20d9a42e36528'
   name 'Rambox'
   homepage 'http://rambox.pro/'
   license :oss
