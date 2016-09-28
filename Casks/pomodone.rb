b8c231711ab6eb776a80cf573eff2a12528707c7
Serge Bibauw <inyarr@gmail.com>
2016-09-28 18:03:30 +0200

    Use a version-flexible URL

--- Casks/pomodone.rb
+++ Casks/pomodone.rb
@@ -2,7 +2,7 @@ cask 'pomodone' do
   version '1.5.806'
   sha256 '0e934efef2f9832ff27616153c1d61962f321c16912794271c49ef5672f8516c'
 
-  url 'https://app.pomodoneapp.com/PomoDoneApp-1.5.806.dmg'
+  url "https://app.pomodoneapp.com/PomoDoneApp-#{version}.dmg"
   name 'PomoDone'
   homepage 'https://pomodoneapp.com/'
   license :gratis
