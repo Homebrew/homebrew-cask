6e711daa14bdd0b15d715918454d360f383de6d0
Alexandre Bouffard <code@alexandrebouffard.ca>
2016-09-28 20:36:49 -0400

    Convert to inclusive version matching

--- Casks/usb-overdrive.rb
+++ Casks/usb-overdrive.rb
@@ -1,8 +1,8 @@
 cask 'usb-overdrive' do
-  if MacOS.version < :mountain_lion
+  if MacOS.version <= :lion
     version '3.0.2'
     sha256 'd6bed831521a5ab2b82d7eede7be46953e1c010cc6e02ff11f3dd2532d7b0b13'
-  elsif MacOS.version < :mavericks
+  elsif MacOS.version <= :mountain_lion
     version '3.2'
     sha256 '5be1d46596f1e98e29a5634655f36fc3070ac08542c36cef77d8f516c883f6d7'
   else
