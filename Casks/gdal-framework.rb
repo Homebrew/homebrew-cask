e15c32c3d830e95bc2de466c724627d4b32de3dc
Lifepillar <lifepillar@lifepillar.me>
2016-09-29 10:45:40 +0200

    Update GDAL framework to 2.1

--- Casks/gdal-framework.rb
+++ Casks/gdal-framework.rb
@@ -1,5 +1,5 @@
 cask 'gdal-framework' do
-  version '1.11.1-1'
+  version '2.1'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "http://www.kyngchaos.com/files/software/frameworks/GDAL_Complete-#{version.sub(%r{^(\d+\.\d+).*}, '\1')}.dmg"
@@ -11,13 +11,12 @@ cask 'gdal-framework' do
   pkg 'Numpy.pkg'
 
   uninstall pkgutil: [
-                       'org.maptools.proj-framework-snow',
-                       'org.sqlite.sqlite3b-framework-snow',
-                       'com.kyngchaos.UnixImageIO-framework-E-lion',
-                       'org.gdal.gdal111-framework-lion',
-                       'net.refractions.geos-framework-snow',
+                       'org.maptools.proj-framework-mtlion',
+                       'org.sqlite.sqlite3c-framework-mtlion',
+                       'com.kyngchaos.UnixImageIO-framework-E-mtlion',
+                       'org.gdal.gdal21-framework-mtlion',
+                       'net.refractions.geos-framework-mtlion',
                        'org.scipy.numpy-lion',
-                       'org.scipy.numpy-snow',
                      ]
 
   caveats do
