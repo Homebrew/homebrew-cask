11e744255812adb48c6b167b4170569166119709
Numbermaniac <Numbermaniac@users.noreply.github.com>
2016-09-29 11:26:15 +1000

    add zxpinstaller 1.5.0

--- Casks/zxpinstaller.rb
+++ Casks/zxpinstaller.rb
@@ -2,6 +2,7 @@ cask 'zxpinstaller' do
   version '1.5.0'
   sha256 'fe54a02b891c99d4452f723447166e2d1da0c3a47a3ebe7b9762ed2243963ba4'
 
+  # github.com/CreativeDo was verified as official when first introduced to the cask
   url "https://github.com/CreativeDo/ZXPInstaller/releases/download/#{version}/ZXPInstaller.dmg"
   name 'ZXPInstaller'
   homepage 'http://zxpinstaller.com'
