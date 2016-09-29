88479894616fbaf9b6f755e79849743bdad6133a
miccal <miccal.matthews@gmail.com>
2016-09-29 22:14:41 +0800

    Update vuescan to 9.5.41

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -2,12 +2,7 @@ cask 'vuescan' do
   version '9.5.41'
   sha256 :no_check # required as upstream package is updated in-place
 
-  if Hardware::CPU.is_32_bit?
-    url "https://www.hamrick.com/files/vuex32#{version.major_minor.no_dots}.dmg"
-  else
-    url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
-  end
-
+  url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   name 'VueScan'
   homepage 'https://www.hamrick.com/'
   license :commercial
