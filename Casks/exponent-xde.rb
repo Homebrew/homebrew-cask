81dbe9cf8efa90c4000e2dc2f6950f667826a7d9
Adam Ouellette <adamouellette57@gmail.com>
2016-09-28 23:15:22 -0400

    Added comment above url for TravisCI

--- Casks/exponent-xde.rb
+++ Casks/exponent-xde.rb
@@ -2,6 +2,7 @@ cask 'exponent-xde' do
   version '2.8.0'
   sha256 'a9eb36f75a9f971afc923e0866b7359e33d7409bbfdfa9e5715d0213d7749ff8'
 
+  # https://xde-updates.exponentjs.com/download/mac was verified as official when first introduced to the cask
   url 'https://xde-updates.exponentjs.com/download/mac'
   name 'Exponent Development Environment (XDE)'
   homepage 'https://getexponent.com/'
