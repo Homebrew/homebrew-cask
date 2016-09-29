e511027ca38d3fc96e97f1322a5ac41aabad67f4
Victor Popkov <victor@popkov.me>
2016-09-29 09:57:20 +0300

    Update boxcryptor to 2.4.403.767 (#24965)

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,16 +1,16 @@
 cask 'boxcryptor' do
-  version '2.3.405.746'
-  sha256 '0067dad6e70041f38076fd26fa674f337e51b6532bb8e35a2ac534e7e4fb858b'
+  version '2.4.403.767'
+  sha256 '5a1fc1939a33b126a82bd2db59fdbc4e878552bdc3c04983e3904da912123ee6'
 
   # d3k3ih5otj72mn.cloudfront.net was verified as official when first introduced to the cask
-  url "https://d3k3ih5otj72mn.cloudfront.net/Boxcryptor_v#{version}_Installer.dmg"
+  url "https://d3k3ih5otj72mn.cloudfront.net/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8',
-          checkpoint: '5d0f6c73dd0c2a0fc578c87268187306baaa610adb6f4a1d9cc7976b657659fc'
+          checkpoint: 'e2a747a1e5641595b466da20a79f59e86255b63f4a0d3a75febc77c8177a4033'
   name 'Boxcryptor'
   homepage 'https://www.boxcryptor.com/en'
   license :commercial
 
-  depends_on macos: '>= :lion'
+  depends_on macos: '>= :mavericks'
 
   app 'Boxcryptor.app'
 end
