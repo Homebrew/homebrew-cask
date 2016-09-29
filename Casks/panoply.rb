6df83865d360cc7a9e5a5528a4db3e09fc19e63c
Bill Capehart <wjcapehart@yahoo.com>
2016-09-29 09:19:19 -0600

    Update panoply.rb
    Change to Panoply version file structure (changed from "PanoplyOSX-{version}.dmg to PanoplyMacOS-#{version}.dmg)

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,8 +1,8 @@
 cask 'panoply' do
   version '4.6.0'
   sha256 '5645381fd2317bc64d4d842d9b78cf499d3d8be2bd572557a3b0d1cd55d184ae'
-
-  url "http://www.giss.nasa.gov/tools/panoply/PanoplyOSX-#{version}.dmg"
+       
+  url "http://www.giss.nasa.gov/tools/panoply/PanoplyMacOS-#{version}.dmg"
   name 'Panoply netCDF, HDF and GRIB Data Viewer'
   homepage 'http://www.giss.nasa.gov/tools/panoply/'
   license :closed
