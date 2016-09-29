4d304b849062eb839283fb538fa4c614c676fa78
Adam Ouellette <adamouellette57@gmail.com>
2016-09-28 22:59:15 -0400

    Added Exponent XDE.app v2.8.0

---
+++ Casks/exponent-xde.rb
@@ -0,0 +1,11 @@
+cask 'exponent-xde' do
+  version '2.8.0'
+  sha256 'a9eb36f75a9f971afc923e0866b7359e33d7409bbfdfa9e5715d0213d7749ff8'
+
+  url 'https://xde-updates.exponentjs.com/download/mac'
+  name 'Exponent Development Environment (XDE)'
+  homepage 'https://getexponent.com/'
+  license :mit
+
+  app 'Exponent XDE.app'
+end
