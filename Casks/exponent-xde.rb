e44b0368419e58480b2ffcba9e559d6907de0a8e
Adam Ouellette <adamouellette57@gmail.com>
2016-09-29 00:44:44 -0400

    Fixed url scheme

--- Casks/exponent-xde.rb
+++ Casks/exponent-xde.rb
@@ -2,8 +2,8 @@ cask 'exponent-xde' do
   version '2.8.0'
   sha256 'a9eb36f75a9f971afc923e0866b7359e33d7409bbfdfa9e5715d0213d7749ff8'
 
-  # xde-updates.exponentjs.com/download/mac was verified as official when first introduced to the cask
-  url 'xde-updates.exponentjs.com/download/mac'
+  # exponentjs.com/download/mac was verified as official when first introduced to the cask
+  url 'https://xde-updates.exponentjs.com/download/mac'
   name 'Exponent Development Environment (XDE)'
   homepage 'https://getexponent.com/'
   license :mit
