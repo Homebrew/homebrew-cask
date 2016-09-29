cf44ed47752bef7e40921582e3d5d30aee8a43e5
miccal <miccal.matthews@gmail.com>
2016-09-30 03:49:06 +0800

    Update praat to 6.0.21 (#25000)

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,16 +1,10 @@
 cask 'praat' do
-  if Hardware::CPU.is_32_bit? || MacOS.version <= :leopard
-    version '6.0.16'
-    sha256 'cd9d7b8c5d48e2d7f0673e0aa13e82e198f66e958d173d679e38a94abb1b2435'
-    url "http://www.fon.hum.uva.nl/praat/old/#{version.no_dots}/praat#{version.no_dots}_mac32.dmg"
-  else
-    version '6.0.18'
-    sha256 'b28fc804c8561f72af1b51ed516b92cf2c39725733b8147ae84797a051d1c387'
-    url "http://www.fon.hum.uva.nl/praat/praat#{version.no_dots}_mac64.dmg"
-    appcast 'https://github.com/praat/praat/releases.atom',
-            checkpoint: 'e251609874e851ae122fac81ee103ca1184751fb8543bf5b83f27a991bcc300f'
-  end
+  version '6.0.21'
+  sha256 '4c6c35f2c72b5862ac79e29989ac244618e1997e87c79b1bf7828360314b5f05'
 
+  url "http://www.fon.hum.uva.nl/praat/praat#{version.no_dots}_mac64.dmg"
+  appcast 'https://github.com/praat/praat/releases.atom',
+          checkpoint: '3d74ff31449d5f37f3cca51c91e62ba828697e7e247ac60f8e62ca26f4a3ff8a'
   name 'Praat'
   homepage 'http://www.fon.hum.uva.nl/praat/'
   license :gpl
