c917e9f746405538bbe5cc039bb06d8167c3c8c9
miccal <miccal.matthews@gmail.com>
2016-09-29 22:45:28 +0800

    Update wkhtmltopdf to 0.12.3 (#25009)

--- Casks/wkhtmltopdf.rb
+++ Casks/wkhtmltopdf.rb
@@ -1,23 +1,16 @@
 cask 'wkhtmltopdf' do
   version '0.12.3'
+  sha256 'c8aa0f9456444847d08c6c772e2e0e4244813e6a2911eba6c940439df9abd0f0'
 
-  if Hardware::CPU.is_32_bit?
-    sha256 '6e4613c060eb9e5eb0bd05b0ccd85d09086ef7a1156300e53a9dfba7969b6fc0'
-    # download.gna.org/wkhtmltopdf was verified as official when first introduced to the cask
-    url "http://download.gna.org/wkhtmltopdf/#{version.major_minor}/#{version}/wkhtmltox-#{version}_osx-carbon-i386.pkg"
-    pkg "wkhtmltox-#{version}_osx-carbon-i386.pkg"
-  else
-    sha256 'c8aa0f9456444847d08c6c772e2e0e4244813e6a2911eba6c940439df9abd0f0'
-    # download.gna.org/wkhtmltopdf was verified as official when first introduced to the cask
-    url "http://download.gna.org/wkhtmltopdf/#{version.major_minor}/#{version}/wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
-    pkg "wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
-  end
-
+  # download.gna.org/wkhtmltopdf was verified as official when first introduced to the cask
+  url "http://download.gna.org/wkhtmltopdf/#{version.major_minor}/#{version}/wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
   name 'wkhtmltopdf'
   homepage 'http://wkhtmltopdf.org/'
   license :gpl
 
   depends_on macos: '>= :snow_leopard'
+
+  pkg "wkhtmltox-#{version}_osx-cocoa-x86-64.pkg"
 
   uninstall pkgutil: 'org.wkhtmltopdf.wkhtmltox',
             delete:  [
