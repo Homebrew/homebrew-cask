ed9ac551ae971e41eb8a5bb6e9d616e4d3b589a8
Artem Medvedev <amedvedev@outlook.com>
2016-09-30 03:57:13 +0300

    Update airfoil to 5.1.2 (#25024)

--- Casks/airfoil.rb
+++ Casks/airfoil.rb
@@ -1,10 +1,10 @@
 cask 'airfoil' do
-  version '5.1.1'
-  sha256 'f794708ad182fcc84cfc3ac77172f3b92bf239c60ef3b96a83607120dd08c67e'
+  version '5.1.2'
+  sha256 '34251eeb95d6faffc83005708feb79c95cf297bba68092f931ec6e511de3b3e0'
 
   url 'https://rogueamoeba.com/airfoil/download/Airfoil.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Airfoil&platform=osx',
-          checkpoint: '86a633e65b6d09d78bafd10af4ddf20b44910aae64da7599b8089a2e6da348fb'
+          checkpoint: 'f0aaf45c9b315219c2e453874e41524347bd9d6977e17f58322998029a9f0fe9'
   name 'Airfoil'
   homepage 'https://www.rogueamoeba.com/airfoil/mac/'
   license :commercial
