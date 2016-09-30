b03af872f516ef00c74c2a91e2488bd24cc5f932
Victor Popkov <victor@popkov.me>
2016-09-30 04:17:26 +0300

    Update totals to 3.1.4 (#25027)

--- Casks/totals.rb
+++ Casks/totals.rb
@@ -1,10 +1,10 @@
 cask 'totals' do
-  version '3.1.3'
-  sha256 '42921894ad7bb46a2698d0ad36d45bf4b504d6d331ba4999887e75caf0b032a6'
+  version '3.1.4'
+  sha256 '78aa952ae63b98e8373c006611aa5782d0a4688bbdbca441f7e0b6b9910f2e1d'
 
   url "http://www.kedisoft.com/dl/totals/Totals-#{version}.dmg"
   appcast 'http://kedisoft.com/appcasts/appcast.php?app=totals3',
-          checkpoint: '094f83ffb0b50c42613280b54c58179c2574e6fdad529ed41b7efd7ce3f3ed6b'
+          checkpoint: '29dbb4e349cb4a0234f7c0fa86807c92f029d574d21146ce5cbb10bd4d9777dd'
   name 'Totals'
   homepage 'http://www.kedisoft.com/totals/'
   license :commercial
