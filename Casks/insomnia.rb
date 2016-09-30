64266ca9ab6ee1ce03b8df0044edcff46b31b845
Sascha Bratton <sascha@brattonbratton.com>
2016-09-29 21:33:24 -0400

    update insomnia (3.5.3) (#25038)

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask 'insomnia' do
-  version '3.5.2'
-  sha256 '28ca233deca09e44e3c50b60a5ef1deac50ed918963e696ba4d6241a0da6c10f'
+  version '3.5.3'
+  sha256 '45380184649e3c3cf95a526c24de1e1afd569117e6a8ce287860785e341f9062'
 
   url "https://downloads.insomnia.rest/mac/Insomnia-#{version}.dmg"
   name 'Insomnia'
