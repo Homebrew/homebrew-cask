66242eed0ccaf3e2ffff46f7cd1b14b5da712bb6
Marius Karnauskas <nkts@users.noreply.github.com>
2016-09-30 16:18:01 +0100

    Liteide qt5 version (#25050)

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,9 +1,9 @@
 cask 'liteide' do
   version '30.2'
-  sha256 'f4269b59169a342de125d566db39c6c96ba75366865b670f16ab2d910bd0bc1a'
+  sha256 '7f4897029f2fc2671e3461ec7426b634c38b4d152c19d47cc255b0671e45907f'
 
   # sourceforge.net/liteide was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/liteide/X#{version}/liteidex#{version}.macosx-qt4.zip"
+  url "https://downloads.sourceforge.net/liteide/X#{version}/liteidex#{version}.macosx-qt5.zip"
   appcast 'https://sourceforge.net/projects/liteide/rss',
           checkpoint: 'c9055a3a1b71a9c59f74e33210a3674ba76282253f1989a1a46cdfa7ed5c5898'
   name 'LiteIDE'
