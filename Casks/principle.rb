d69f4deeef88550dbcda3ad09b69d9d2e0d3d9e8
Artem Medvedev <amedvedev@outlook.com>
2016-09-30 18:18:21 +0300

    Update principle to 2.0.3 (#25048)

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,11 +1,11 @@
 cask 'principle' do
-  version '2.0'
-  sha256 '42cd733ad462b4849fd6a5b8b50c5dab92e9668d113281c30db23203189aed4b'
+  version '2.0.3'
+  sha256 'e9ab0ed54f059002d58508441ff47ca0f7c88dc1d0d350c1d176931a156c5450'
 
   # dl.dropboxusercontent.com/u/13897407 was verified as official when first introduced to the cask
   url "https://dl.dropboxusercontent.com/u/13897407/Principle_#{version.dots_to_underscores}.zip"
   appcast 'https://dl.dropboxusercontent.com/u/13897407/buildTrain-601A6666-57A4-4C19-BDD3-1387B3CB9719.xml',
-          checkpoint: 'e0e66f6b03362eddc6fd32950c8989ef00031d613e2977c14f40ddc407bb9c93'
+          checkpoint: 'c6a2d3371fb08182c907316c18019718ccd82c099d366cb0a7e3153ac79e9bbb'
   name 'Principle'
   homepage 'http://principleformac.com/'
   license :commercial
