cadc400306e40c61cadc6a5c9d94aa86493373a2
Seiji Kohara <sage.0118@gmail.com>
2016-10-01 00:18:35 +0900

    Update rationalplan-viewer to 4.14.7306 (#25045)

--- Casks/rationalplan-viewer.rb
+++ Casks/rationalplan-viewer.rb
@@ -1,6 +1,6 @@
 cask 'rationalplan-viewer' do
-  version '4.13.0'
-  sha256 '5c0796a4c7d20b644cd0fb6dc6bfe7b81bd5d261da1d72ba08c6370072ef42fa'
+  version '4.14.7306'
+  sha256 '1e8926757687e3ca1e15241bfb1aacc82e07e2167c9c1cc0a5416a71585c3d3c'
 
   url "http://www.rationalplan.com/download/RationalPlan-Viewer-#{version}.dmg"
   name 'RationalPlan Viewer'
