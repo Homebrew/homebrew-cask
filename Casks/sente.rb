0cad8f82bf900e46228f112bb19a9050b0b149e5
Victor Popkov <victor@popkov.me>
2016-09-30 22:55:45 +0300

    Update sente to 6.7.11.11931 (#25069)

--- Casks/sente.rb
+++ Casks/sente.rb
@@ -6,13 +6,13 @@ cask 'sente' do
     version '6.7.8.11870'
     sha256 'f75bb32db9f0dbfa3d5dbf7639371b5892f6071e740de38e768a203899bb4b5b'
   else
-    version '6.7.11.11926'
-    sha256 '5ce3f5a23fa3dc532745a84bcbd5adfc990cd3ddb82bac58301221be28f6e9a8'
+    version '6.7.11.11931'
+    sha256 '578b05e715203694f53006b54bd9dd72386d7a4670629dea5cc59ab44f8a0a24'
   end
 
   url "https://www.thirdstreetsoftware.com/downloads/Sente-#{version}.zip"
   appcast 'https://www.thirdstreetsoftware.com/rss/Sente65.xml',
-          checkpoint: '6714309fa46724f80c4cafcb811d132ff0f1ac9c37dde306cf58fe084633aa47'
+          checkpoint: '8b4ffee4f0379d9e1565166a6cecfa6a73260640ee8070e6abd8f58f3d26f754'
   name 'Sente'
   homepage 'https://www.thirdstreetsoftware.com'
   license :freemium
