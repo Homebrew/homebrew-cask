538a47915ab98275fd3374ea3a636b4408de1dfa
Jeremy Saklad <saklad5@me.com>
2016-09-29 20:36:57 -0500

    Add 3MCloudLibrary.app v1.50.735564 (#25037)

---
+++ Casks/3mcloudlibrary.rb
@@ -0,0 +1,14 @@
+cask '3mcloudlibrary' do
+  version '1.50.735564'
+  sha256 '569b78d4f78054518b7ea87678637949408ca7cd1fd90baf2221d92fff35cdfc'
+
+  url "http://www.yourcloudlibrary.com/index.php/en-us/downloads/software/2-3mcloudlibrary-live-version-#{version.dots_to_hyphens}/file"
+  appcast 'http://www.yourcloudlibrary.com/index.php/en-us/downloads/software?format=rss',
+          checkpoint: '948716fbbe4a3af107c7e73424608615bf53623b0110dc24fa456a2ce06dad92'
+  name '3M Cloud Library'
+  name 'cloudLibrary'
+  homepage 'http://www.yourcloudlibrary.com/index.php/en-us/get-the-app/mac'
+  license :apache
+
+  app '3MCloudLibrary.app'
+end
