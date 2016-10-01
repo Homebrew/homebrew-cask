fffdfbcd3aba1c04a242840b053a592c6bfae2b6
Victor Popkov <victor@popkov.me>
2016-10-01 23:04:56 +0300

    Update bilibili to 2.47 (#25094)

--- Casks/bilibili.rb
+++ Casks/bilibili.rb
@@ -1,10 +1,10 @@
 cask 'bilibili' do
-  version '2.46'
-  sha256 'dfc07c9f76e497aec74c153ac832d6ee879bc72044cdb8982c62fc2173de2184'
+  version '2.47'
+  sha256 '234a1db450d1e9768d4d83b3dd90baaf47823d2d9dbd21d5cde17991969e702d'
 
   url "https://github.com/typcn/bilibili-mac-client/releases/download/#{version}/Bilibili.dmg"
   appcast 'https://github.com/typcn/bilibili-mac-client/releases.atom',
-          checkpoint: '1890efcced7351cf6531463bf7c87add4d2203c8d8d9b8ff153ce7c0e25d8139'
+          checkpoint: '33007f2ef2383f38337bc2c4948ece8bd3bf8972e8af8c841b93a0483e1ecfa8'
   name 'Bilibili'
   homepage 'https://github.com/typcn/bilibili-mac-client/'
   license :gpl
