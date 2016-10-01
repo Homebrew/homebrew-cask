28eff0f4c5a6cbf28699b5cdc77efbb0071c1334
Markus Reiter <me@reitermark.us>
2016-10-01 04:08:24 +0200

    OSXFUSE: Install MacFUSE compatibility layer by default. (#25044)

--- Casks/osxfuse.rb
+++ Casks/osxfuse.rb
@@ -10,13 +10,39 @@ cask 'osxfuse' do
   homepage 'https://osxfuse.github.io/'
   license :bsd
 
-  pkg "Extras/FUSE for macOS #{version}.pkg"
+  installer script: '/usr/sbin/installer',
+            args:   [
+                      '-pkg', "#{staged_path}/Extras/FUSE for macOS #{version}.pkg",
+                      '-target', '/',
+                      '-applyChoiceChangesXML', "#{staged_path}/Extras/Choices.xml"
+                    ]
+
+  preflight do
+    IO.write "#{staged_path}/Extras/Choices.xml", <<-EOS.undent
+      <plist>
+        <array>
+        	<dict>
+        		<key>attributeSetting</key>
+        		<integer>1</integer>
+        		<key>choiceAttribute</key>
+        		<string>selected</string>
+        		<key>choiceIdentifier</key>
+        		<string>com.github.osxfuse.pkg.MacFUSE</string>
+        	</dict>
+        </array>
+      </plist>
+    EOS
+  end
 
   postflight do
     set_ownership ['/usr/local/include', '/usr/local/lib']
   end
 
-  uninstall pkgutil: 'com.github.osxfuse.pkg.Core|com.github.osxfuse.pkg.PrefPane',
+  uninstall pkgutil: [
+                       'com.github.osxfuse.pkg.Core',
+                       'com.github.osxfuse.pkg.MacFUSE',
+                       'com.github.osxfuse.pkg.PrefPane',
+                     ],
             kext:    'com.github.osxfuse.filesystems.osxfusefs'
 
   caveats do
