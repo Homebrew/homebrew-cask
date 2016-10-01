a02f8a18a119dca892c82c7842f1d969fea78d4b
Maarten Paauw <maartenpaauw@gmail.com>
2016-10-01 16:34:41 +0200

    Update filezilla to 3.22.0 (#25082)

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -3,14 +3,14 @@ cask 'filezilla' do
     version '3.8.1'
     sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
   else
-    version '3.21.0'
-    sha256 'fe577a3e7115078d82272b617a1bbc46ec4c1ecfc0ffdbf9a23409e8b5e411f5'
+    version '3.22.0'
+    sha256 'f41e4df47a942802c93b05510f85a1f4b8d0e49b3b428b2341d6f84e37ebb144'
   end
 
   # sourceforge.net/filezilla was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   appcast 'https://sourceforge.net/projects/filezilla/rss?path=/FileZilla_Client',
-          checkpoint: 'b387097041e70182747d8d23af0269e642f93ec3d41683446a2338e73e15a676'
+          checkpoint: 'a9660b431c3cf580c515e5750cf6c7899b9b4b4790a8b5b649c3f0597bb2658a'
   name 'FileZilla'
   homepage 'https://filezilla-project.org/'
   license :gpl
