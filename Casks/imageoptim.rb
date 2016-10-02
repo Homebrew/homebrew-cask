35dc9214bf4e2a1d8c36934eab456c1a21a36156
Vítor Galvão <info@vitorgalvao.com>
2016-10-02 20:37:47 +0100

    Update imageoptim to 1.6.1 (#25107)

--- Casks/imageoptim.rb
+++ Casks/imageoptim.rb
@@ -6,7 +6,7 @@ cask 'imageoptim' do
   appcast 'https://imageoptim.com/appcast.xml',
           checkpoint: '1b321e043e90a78528070831a1f29d06f118cb82770193e1e86aff74508144ad'
   name 'ImageOptim'
-  homepage 'https://imageoptim.com/'
+  homepage 'https://imageoptim.com/mac'
   license :gpl
 
   auto_updates true
