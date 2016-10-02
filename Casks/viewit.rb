ec9f3ccf723092d192876876dca462180567eb14
Jussi Tuominen <wickedsp1d3r@COLDwithin.local>
2016-10-02 20:00:34 +0300

    Update viewit to 2.60

--- Casks/viewit.rb
+++ Casks/viewit.rb
@@ -1,6 +1,6 @@
 cask 'viewit' do
-  version '2.59'
-  sha256 'd128933b6e0b81bc30c5e6b31db523b80bd15094f7e03834335b6b4f566cc4c3'
+  version '2.60'
+  sha256 '478d052bd9b7ad0e83fd61765b5ee097e143da37769efdd856b582b26c26d5b8'
 
   url "http://www.hexcat.com/downloads/ViewIt-#{version}.zip"
   name 'ViewIt'
