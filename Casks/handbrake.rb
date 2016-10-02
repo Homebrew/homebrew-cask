9a86d2baa1927514b8128053d21e8ea6e4926708
Vítor Galvão <info@vitorgalvao.com>
2016-10-02 14:08:49 +0100

    Update handbrake to 0.10.5 (#25106)

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -3,8 +3,8 @@ cask 'handbrake' do
   sha256 'a2f1cacfb508e5ebe2489c64fa15204f15578aaf5e7f8a5ae9ab1fdccde51222'
 
   url "http://download.handbrake.fr/handbrake/releases/#{version}/HandBrake-#{version}-MacOSX.6_GUI_x86_64.dmg"
-  appcast 'https://handbrake.fr/appcast.x86_64.xml',
-          checkpoint: '7518bedc8a1da492621dd67d681e072a4071d0a0b0bb3623909dbd2a9092cc86'
+  appcast 'https://github.com/HandBrake/HandBrake/releases.atom',
+          checkpoint: '2b923cfd24bf46490955348df4961fde6deaa3b0e1345d7775adcea86176a34a'
   name 'HandBrake'
   homepage 'https://handbrake.fr'
   license :oss
