17daf3833ff35de988bc0471df00b3cc959ef810
m0ls <m0ls@users.noreply.github.com>
2016-10-02 20:30:42 +0800

    fix canon-capt-printer-driver version: v3.9.0 -> v3.90 (#25103)

--- Casks/canon-capt-printer-driver.rb
+++ Casks/canon-capt-printer-driver.rb
@@ -1,9 +1,9 @@
 cask 'canon-capt-printer-driver' do
-  version '3.9.0'
+  version '3.90'
   sha256 '75341c120a4cee3635780d875ee209afe6d175153596f36a176980870d6ca57e'
 
   # gdlp01.c-wss.com was verified as official when first introduced to the cask
-  url 'http://gdlp01.c-wss.com/gds/7/0100004207/14/Mac_CAPT_V390_uk_EN.dmg'
+  url "http://gdlp01.c-wss.com/gds/7/0100004207/14/Mac_CAPT_V#{version.no_dots}_uk_EN.dmg"
   name 'Canon CAPT Printer Driver'
   homepage 'http://support-au.canon.com.au/contents/AU/EN/0100420705.html'
   license :closed
