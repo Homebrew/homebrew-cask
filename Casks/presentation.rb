280d248f0f3cf4e1f276dac6dc4479646e805bf4
rndblnch <rndblnch@gmail.com>
2016-10-02 22:16:04 +0200

    Update presentation.rb to 1.7 (#25120)

--- Casks/presentation.rb
+++ Casks/presentation.rb
@@ -1,6 +1,6 @@
 cask 'presentation' do
-  version '1.6'
-  sha256 'ba6beab694d715b67a193338ffb3edca2e0dbfd7c546fcaf08265dbb403d760a'
+  version '1.7'
+  sha256 'ca13a2176a0d23f425c61c5d9172c160c4a0373b0080ca5d6b5e61355b31bb81'
 
   url "http://iihm.imag.fr/blanch/software/osx-presentation/releases/osx-presentation-#{version}.dmg"
   name 'Présentation'
