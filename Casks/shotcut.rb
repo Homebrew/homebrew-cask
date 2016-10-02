1218f032f77a18c6d820fe3eba0fdb3fd1619c88
Vítor Galvão <info@vitorgalvao.com>
2016-10-02 14:03:41 +0100

    Update shotcut to 16.10.01

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,11 +1,11 @@
 cask 'shotcut' do
-  version '16.09.01'
-  sha256 'a3cf4864fc7dc2f98f579fd2db5162d86f98c698bad1a260d5126562382a4f08'
+  version '16.10.01'
+  sha256 'b24bf7036df42be70b64f8905224568afaae04cd794d9898dfffda47ccad8db7'
 
   # github.com/mltframework/shotcut was verified as official when first introduced to the cask
   url "https://github.com/mltframework/shotcut/releases/download/v#{version.major_minor}/shotcut-osx-x86_64-#{version.no_dots}.dmg"
   appcast 'https://github.com/mltframework/shotcut/releases.atom',
-          checkpoint: '9fa80f1d2ae03b025c94d2ce046ac2f9186ba3dededec3ee361feb4e86e1ca9d'
+          checkpoint: '779aed1d63fae54c2801da15a9993d598e9cf7815b9328995ebbe1dac8debc66'
   name 'Shotcut'
   homepage 'https://www.shotcut.org/'
   license :gpl
