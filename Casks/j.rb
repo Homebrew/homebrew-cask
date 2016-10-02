11f8c3ca4315075e7b822150977a50b51aecf3af
Danielle McLean <gopsychonauts@gmail.com>
2016-10-02 16:37:50 +1100

    Repair provided apps and scripts for launching j 804 (#25084)
    * Additionally symlink j's jconsole under the name 'jcon'
    
    * Create symlinks on the path for j's jbrk and jhs scripts
    
    * Fix the app bundles installed by the J cask so they point to the installed J instance
    
    * Properly install and generate wrappers for launching to jqt, the graphical IDE for J
    
    * Use absolute path to echo and readlink in j cask to ensure we use the system versions

--- Casks/j.rb
+++ Casks/j.rb
@@ -7,11 +7,34 @@ cask 'j' do
   homepage 'http://www.jsoftware.com'
   license :gpl
 
-  %w[jbrk jcon jhs jqt].each do |a|
+  apps = %w[jbrk jcon jhs jqt]
+  apps.each do |a|
     app "j64-#{version}/#{a}.app"
   end
 
-  %w[jconsole].each do |b|
-    binary "j64-#{version}/bin/#{b}"
+  installer script: "j64-#{version}/updatejqt.sh",
+            sudo:   false
+
+  # target names according to readme.txt
+  %w[jcon jconsole].each do |b|
+    binary "j64-#{version}/bin/jconsole", target: b
+  end
+  commands = %w[jbrk jhs jqt]
+  commands.each do |b|
+    binary "j64-#{version}/bin/#{b}.command", target: b
+  end
+
+  postflight do
+    # Use `readlink` to get full path of symlinked commands.
+    commands.each do |c|
+      command = "#{staged_path}/j64-#{version}/bin/#{c}.command"
+      IO.write command, IO.read(command).gsub('$0', '$(/usr/bin/readlink "$0" || /bin/echo "$0")')
+    end
+
+    # Fix relative paths inside App bundles.
+    apps.each do |a|
+      apprun = "#{appdir}/#{a}.app/Contents/MacOS/apprun"
+      IO.write apprun, IO.read(apprun).gsub(%r{`dirname "\$0"`.*?/bin}, "#{staged_path}/j64-#{version}/bin")
+    end
   end
 end
