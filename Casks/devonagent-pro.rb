85755ae866016e9f8329cac9ac53faaae8b32386
Nelson Santos <nbsantos@gmail.com>
2016-10-02 20:35:55 -0400

    Fixed app name and added target

--- Casks/devonagent-pro.rb
+++ Casks/devonagent-pro.rb
@@ -7,8 +7,8 @@ cask 'devonagent-pro' do
   appcast 'http://www.devon-technologies.com/fileadmin/templates/filemaker/sparkle.php?product=300005193&format=xml',
           checkpoint: '9d696c00b3076dee96be1a5b441b3574fd0976031c0e5dcadeacf6fefbeedc84'
   name 'DEVONagent Pro'
-  homepage 'http://www.devontechnologies.com/products/devonagent/overview.html'
+  homepage 'http://www.devontechnologies.com/products/devonagent/devonagent-pro.html'
   license :commercial
 
-  app 'DEVONagent Pro.app'
+  app 'DEVONagent.app', target: 'DEVONagent Pro.app'
 end
