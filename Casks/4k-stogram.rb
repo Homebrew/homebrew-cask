7ca17bb0245b5c3a700ddd0d200a52c7f9e922d8
Juraj Bednar <juraj@bednar.sk>
2016-10-03 12:47:25 +0200

    Update to 4k-stogram 2.1
    Update to the new version of 4k-stogram

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -1,6 +1,6 @@
 cask '4k-stogram' do
-  version '2.0'
-  sha256 'a4ff472fcf5c46c7ca82f9ad5d85d45048fe8d0553588bedf8e41c2564b968cf'
+  version '2.1'
+  sha256 '5413a4ce5caca32dc6e60cb8b9388635ce3c882fe26011621e4ff3a3cd3e59d0'
 
   url "https://downloads.4kdownload.com/app/4kstogram_#{version}.dmg"
   name '4K Stogram'
