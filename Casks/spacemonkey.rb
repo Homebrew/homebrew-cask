0747dffc824836ba4817100b9f0dee10c82c7975
Victor Popkov <victor@popkov.me>
2016-10-03 20:32:51 +0300

    Update spacemonkey to 0.7.22,135

--- Casks/spacemonkey.rb
+++ Casks/spacemonkey.rb
@@ -1,11 +1,11 @@
 cask 'spacemonkey' do
-  version '0.7.21,134'
-  sha256 'cf84059b32286feb14ee9d73c1c015c74e33c800548df817507e82c0803bd049'
+  version '0.7.22,135'
+  sha256 '3890a4710782ff95b1f892548b9e7b95df06050b958c8c565e171d44752aec51'
 
   # hockeyapp.net/api/2/apps/aa33b6780fdfc71247b2995fa47b5d7c was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/aa33b6780fdfc71247b2995fa47b5d7c/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/aa33b6780fdfc71247b2995fa47b5d7c',
-          checkpoint: 'f6e6e143a5ec6f6a6d62e478f5e29ed08c1441ec73c3ed12a13ba498b0672b51'
+          checkpoint: 'f2b61b62eea11d2c1d970740b4c6c04437d31469ac29160100d3fba723fdad4d'
   name 'Space Monkey'
   homepage 'https://www.spacemonkey.com'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
