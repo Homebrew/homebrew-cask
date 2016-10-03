6d572d1a73b11e123b49d1e6a9d33e9cb522f076
Robert LU <robberphex@gmail.com>
2016-10-04 02:39:53 +0800

    Update flashs (#25150)
    * fix pepper-flash's info
    
    * fix download url
    * avoid using installer
    
    * update flash download url

--- Casks/flash.rb
+++ Casks/flash.rb
@@ -3,7 +3,7 @@ cask 'flash' do
   sha256 'f4c10d33c13fcee3f9c9cd2c742743c92269474f90f98bf47dff367932517e67'
 
   # macromedia.com was verified as official when first introduced to the cask
-  url "https://fpdownload.macromedia.com/get/flashplayer/pdc/#{version}/install_flash_player_osx.dmg"
+  url "https://fpdownload.adobe.com/get/flashplayer/pdc/#{version}/install_flash_player_osx.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
           checkpoint: 'e6bf656907bdee3e843e649f97de873aac9aa18e19d87c111d7fb658037c4047'
   name 'Adobe Flash Player'
