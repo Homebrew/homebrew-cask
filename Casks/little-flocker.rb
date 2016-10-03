bd1d7c20ee784a60201589019542c82362a5787c
Security Generation <info@securethoughts.net>
2016-10-03 20:05:27 +0100

    Little Flocker 1.0 RC3 (#25156)

--- Casks/little-flocker.rb
+++ Casks/little-flocker.rb
@@ -1,6 +1,6 @@
 cask 'little-flocker' do
-  version '0.99.92'
-  sha256 '1844837870ac215ce6dd2a05ebd76f8e71f159456aa1e04f8e28a5aeb2a69150'
+  version '0.99.93'
+  sha256 '3dd1daa8d94d8182ac6194e84d36f7cc70502e4c6db1b9a7e1508d484206f597'
 
   # zdziarski.com/littleflocker was verified as official when first introduced to the cask
   url "https://www.zdziarski.com/littleflocker/LittleFlocker-#{version}.dmg"
