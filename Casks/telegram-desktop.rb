1ae10e9ffabb0553b58d0984bd7104c00f6ce8cf
inailuig <inailuig@users.noreply.github.com>
2016-10-04 17:26:20 +0200

    Update telegram-desktop to 0.10.11 (#25178)

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask 'telegram-desktop' do
-  version '0.10.8'
-  sha256 '283709d0924381eeeb80e2a7d0e8551b80aaf5e2dbf86db307419029a01e1688'
+  version '0.10.11'
+  sha256 '19d3ebbfb49189fcab30a70616d1293a9711b5215341752252f91ccf888748f6'
 
   # tdesktop.com was verified as official when first introduced to the cask
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
