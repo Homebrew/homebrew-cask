80181f9f3182e6b9761de302c9d3fb79e88abbcb
Mike Fiedler <miketheman@gmail.com>
2016-10-04 11:26:08 -0400

    Update hab to 0.10.2-20160930234832 (#25177)

--- Casks/hab.rb
+++ Casks/hab.rb
@@ -1,6 +1,6 @@
 cask 'hab' do
-  version '0.9.1-20160901213157'
-  sha256 '760689f49e25f09cc8331b9dc1fdb74b5f24f53176232535a68d4b7e76e23893'
+  version '0.10.2-20160930234832'
+  sha256 '1a819160da57d80d313378b56989b2b101eca00d95e9f8ec7aad2d5b419bab39'
 
   # habitat.bintray.com was verified as official when first introduced to the cask
   url "https://habitat.bintray.com/stable/darwin/x86_64/hab-#{version}-x86_64-darwin.zip"
