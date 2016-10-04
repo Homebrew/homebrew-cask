194d6ac97c79d66e5ba396a50918abdf58de15f9
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2016-10-05 00:25:55 +0900

    Update cmd-eikana to 2.0.0 (#25176)

--- Casks/cmd-eikana.rb
+++ Casks/cmd-eikana.rb
@@ -1,10 +1,10 @@
 cask 'cmd-eikana' do
-  version '1.1.1'
-  sha256 'c61f82e1dfd6093272a1f441361c33da79ea29279d4de749dd31d2533d7ab18d'
+  version '2.0.0'
+  sha256 '3d2527476cbb992fb4c9fba7179b4a1bd04686ef3e66dd9c47f816d1145e1fbd'
 
   url "https://ei-kana.appspot.com/download/eikana-#{version}.app.zip"
   appcast 'https://ei-kana.appspot.com',
-          checkpoint: '72ae6ad7ab527ad4713dcca309d1485716a666ff8c2636434a2c17e2102e9092'
+          checkpoint: '2c08afc48520059b1f35a49c61abada3b9f8f2d1ac4000e308b3674ebc862c01'
   name 'cmd-eikana'
   name '⌘英かな'
   homepage 'https://ei-kana.appspot.com'
