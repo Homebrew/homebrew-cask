64a0c3561449ead9d16a2a6dbf867752912d5d95
garfi43 <garfi43@gmail.com>
2016-10-05 04:13:38 +0900

    NetBeans update [Installer,PHP,CPP] to 8.2 (#25184)

--- Casks/netbeans.rb
+++ Casks/netbeans.rb
@@ -1,6 +1,6 @@
 cask 'netbeans' do
-  version '8.1'
-  sha256 'adc3963891a62e7d716d4b2ee195bc57f7e42150dabfe684d0a8eedfc9b6c99a'
+  version '8.2'
+  sha256 'ddcf37e91d960cca6b6a37c95eabf2c6f15330ed708bfd79be796de00da20e5e'
 
   url "http://download.netbeans.org/netbeans/#{version}/final/bundles/netbeans-#{version}-macosx.dmg"
   name 'NetBeans IDE'
