e23358b32d178150a4ed0da2866ed583100adce4
chris <mail@chohner.com>
2016-10-04 14:15:07 +0200

    Update texpad to 1.7.32__104__a6eb552 (#25174)

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -3,13 +3,13 @@ cask 'texpad' do
     version '1.7.21'
     sha256 'cae312b89d25752917bf66572fd1c6b6722097b651abe9a1a1b399dc36493512'
   else
-    version '1.7.28'
-    sha256 'f59a83923c70dd60ee2ad0acb07814b04ccc9b2ecea6d33d2340f49c54df70cc'
+    version '1.7.32__104__a6eb552'
+    sha256 'f43883c2efd95d59a04e0bef0d3f847cdad8a779ee5f0f2f490fea12994d9fe2'
   end
 
   url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.dots_to_underscores}.zip"
   appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml',
-          checkpoint: 'cfcc73295708a553ca7a637d5d465c038cd1b547fc604a0cebb2d05df70816dc'
+          checkpoint: 'c71e243eb741d3b9fa44c7b649a78b564c79ea6038e98249b0905d11f2b1616e'
   name 'Texpad'
   homepage 'https://www.texpadapp.com/osx'
   license :commercial
