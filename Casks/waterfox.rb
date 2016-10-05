278b5a010740cc03bbbe5e3844628e04813bd102
Jean-Philippe Roemer <jp@roemer.im>
2016-10-05 04:28:53 +0200

    Update waterfox to 48.0.2 (#25203)

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,6 +1,6 @@
 cask 'waterfox' do
-  version '43.0.4'
-  sha256 'f18ecbb17af5d186ab97c85ce226c29c001cbe83d4a5d565aaf4ec435027be6a'
+  version '48.0.2'
+  sha256 '9eda2dfb17115059b4331d9ab15cb0e0bea490251bd9af63aaf488a4fc7c92bc'
 
   # storage-waterfox.netdna-ssl.com was verified as official when first introduced to the cask
   url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20#{version}%20Setup.dmg"
