b1d9dc52f2e9dc489148183c46218dd4ba205584
miccal <miccal.matthews@gmail.com>
2016-10-05 22:34:47 +0800

    Update dropbox to latest (#25215)

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -15,9 +15,12 @@ cask 'dropbox' do
 
   zap delete: [
                 '~/.dropbox',
+                '~/Library/Application Scripts/com.getdropbox.dropbox.garcon',
+                '~/Library/Caches/CloudKit/com.apple.bird/iCloud.com.getdropbox.Dropbox',
                 '~/Library/Caches/com.getdropbox.dropbox',
                 '~/Library/Containers/com.getdropbox.dropbox.garcon',
                 '~/Library/Group Containers/com.getdropbox.dropbox.garcon',
+                '~/Library/Logs/Dropbox_debug.log',
                 '/Library/DropboxHelperTools',
               ]
 end
