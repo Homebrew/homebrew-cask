3d48d611cd68f84451e0494d07f29620248fccf6
Victor Popkov <victor@popkov.me>
2016-10-05 23:13:33 +0300

    Update ipvanish-vpn to 3.0.2_442

--- Casks/ipvanish-vpn.rb
+++ Casks/ipvanish-vpn.rb
@@ -1,10 +1,10 @@
 cask 'ipvanish-vpn' do
-  version '3.0.1_440'
-  sha256 '45e98d0a9125c528288646d7a7a92b273a5b0d7cd7c601cfbe629714b3273a6b'
+  version '3.0.2_442'
+  sha256 'c1aabda78cdd2d02bbe4fdda22ac2f4c787392d70326c81d6dc74fdfb52278df'
 
   url "https://www.ipvanish.com/software/osx/IPVanish_v#{version}.zip"
   appcast 'https://www.ipvanish.com/software/osx/updates.xml',
-          checkpoint: 'd3d3749ec17e3b9e10fcd756711d90fea3dd6975c0fc3e0d3daff7b5cf018702'
+          checkpoint: '7d73708aaa5bb44de71aeb4b25e6b9e92a37267ef2eb0730cf1d34b538dc0543'
   name 'IPVanish'
   homepage 'https://www.ipvanish.com/'
   license :gratis
