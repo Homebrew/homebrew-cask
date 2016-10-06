44a7c5bef4245d4f1636c6dcf46f76a8f5041972
pragnesh <pg.radadia@gmail.com>
2016-10-06 12:13:55 +0530

    Update TortoiseHg to version 3.9.2

--- Casks/tortoisehg.rb
+++ Casks/tortoisehg.rb
@@ -1,6 +1,6 @@
 cask 'tortoisehg' do
-  version '3.8.1'
-  sha256 '0a2b781a108fc4f94f94352d8595047aeb5f9b7ee7c53069fa17293035fcb489'
+  version '3.9.2'
+  sha256 'f2be4c32f6a2084523d532c6bed35ace574c7862c17a3c4ec9128b3b31b1a5c5'
 
   url "https://bitbucket.org/tortoisehg/files/downloads/TortoiseHg-#{version}-mac-x64.dmg"
   name 'TortoiseHg'
