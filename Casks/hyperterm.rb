28ec1cac70503529badeb5f789a688d5ee897662
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-10-06 18:15:39 +0200

    Updated Hyperterm to Hyper
    See https://github.com/zeit/hyper/releases/tag/0.8.1

--- Casks/hyperterm.rb
+++ Casks/hyperterm.rb
@@ -1,26 +1,26 @@
 cask 'hyperterm' do
-  version '0.7.1'
-  sha256 '7add5e9f73c70a536b9a715c10410a45552f4586c90f75c0b07e8404e00f80b8'
+  version '0.8.1'
+  sha256 '9b39e2dc710845db99ad2002e107c0c7754b7718c6cc41c8793435e9d18c3e03'
 
-  # github.com/zeit/hyperterm was verified as official when first introduced to the cask
-  url "https://github.com/zeit/hyperterm/releases/download/#{version}/hyperterm-#{version}-mac.zip"
-  appcast 'https://github.com/zeit/hyperterm/releases.atom',
-          checkpoint: 'd7b5fed723c882c236104c7571d330fe81a4e17c5dc3a4793ac5829105c3933e'
-  name 'HyperTerm'
-  homepage 'https://hyperterm.org/'
+  # github.com/zeit/hyper was verified as official when first introduced to the cask
+  url "https://github.com/zeit/hyper/releases/download/#{version}/hyper-#{version}-mac.zip"
+  appcast 'https://github.com/zeit/hyper/releases.atom',
+          checkpoint: '5ccfd2a1a40f2a2c2ea39a554f122fc165a2815de4c2d864c11c2153981088e6'
+  name 'Hyper'
+  homepage 'https://hyper.is/'
   license :mit
 
-  app 'HyperTerm.app'
+  app 'Hyper.app'
 
   zap delete: [
-                '~/.hyperterm.js',
-                '~/.hyperterm_plugins',
-                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/co.zeit.hyperterm.sfl',
-                '~/Library/Application Support/HyperTerm',
-                '~/Library/Caches/co.zeit.hyperterm',
-                '~/Library/Caches/co.zeit.hyperterm.ShipIt',
-                '~/Library/Preferences/co.zeit.hyperterm.plist',
-                '~/Library/Preferences/co.zeit.hyperterm.helper.plist',
-                '~/Library/Saved Application State/co.zeit.hyperterm.savedState',
+                '~/.hyper.js',
+                '~/.hyper_modules',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/co.zeit.hyper.sfl',
+                '~/Library/Application Support/Hyper',
+                '~/Library/Caches/co.zeit.hyper',
+                '~/Library/Caches/co.zeit.hyper.ShipIt',
+                '~/Library/Preferences/co.zeit.hyper.plist',
+                '~/Library/Preferences/co.zeit.hyper.helper.plist',
+                '~/Library/Saved Application State/co.zeit.hyper.savedState',
               ]
 end
