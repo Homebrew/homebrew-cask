2c2bf1d2ee9a35626db766a9ecaf007b2599df43
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-10-06 21:09:26 +0200

    Fixed name of file (#25259)

--- Casks/hyper.rb
+++ Casks/hyper.rb
@@ -1,11 +1,11 @@
-cask 'hyperterm' do
+cask 'hyper' do
   version '0.8.1'
   sha256 '9b39e2dc710845db99ad2002e107c0c7754b7718c6cc41c8793435e9d18c3e03'
 
   # github.com/zeit/hyper was verified as official when first introduced to the cask
   url "https://github.com/zeit/hyper/releases/download/#{version}/hyper-#{version}-mac.zip"
   appcast 'https://github.com/zeit/hyper/releases.atom',
-          checkpoint: '5ccfd2a1a40f2a2c2ea39a554f122fc165a2815de4c2d864c11c2153981088e6'
+          checkpoint: '9cb83c950deb477b1d9c95ebecbdaba3637db29007b2ff279ec4301147426b49'
   name 'Hyper'
   homepage 'https://hyper.is/'
   license :mit
