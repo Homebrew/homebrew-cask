405ca30a6df769f6315ba996192557a2bc1f6602
mathbunnyru <mathbunnyru@users.noreply.github.com>
2016-10-06 11:26:27 +0300

    Update whatsapp to 0.2.1880 (#25254)

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,10 +1,10 @@
 cask 'whatsapp' do
-  version '0.2.1455'
-  sha256 'ef24b632945439529b687ac875c0c50c89c4dee0dc9aaaf7e1f73c5f2a2f1833'
+  version '0.2.1880'
+  sha256 '1f48d23ad16e068d5a8df60cccbb7b5b15e2dba74d33cd678590e261b268b928'
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64',
-          checkpoint: '854fc587ed285baf19a01e0c02a7a5f6854308732a0022ae238a5d1d4bb1855a'
+          checkpoint: '1ecfe85f9dd8c59c79ba751466317315e61101fad6c91b57a607cf20d76f3d08'
   name 'WhatsApp'
   homepage 'https://www.whatsapp.com/'
   license :gratis
