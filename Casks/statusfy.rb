7b1b811912a36493ec24b3e2168e7abb0134721e
Andrei Simionescu <ansimionescu@gmail.com>
2016-10-07 00:19:14 +0200

    url 1.4.0 -> #{version}

--- Casks/statusfy.rb
+++ Casks/statusfy.rb
@@ -2,7 +2,7 @@ cask 'statusfy' do
   version '1.4.0'
   sha256 '7fcd2bf27000bec59ef76343621fc4c551c68dd5ab338742ca824b2c5fe0771b'
 
-  url "https://github.com/paulyoung/Statusfy/releases/download/1.4.0/Statusfy.zip"
+  url "https://github.com/paulyoung/Statusfy/releases/download/#{version}/Statusfy.zip"
   appcast 'https://github.com/fitztrev/shuttle/releases.atom',
           checkpoint: '7c389e357ae08cb0f7d9e20108fe239da609514d09c1fbcb94192998f570b6c5'
   name 'Shuttle'
