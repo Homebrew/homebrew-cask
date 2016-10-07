61e89386aee24439c15495734208cf149f1cee1e
Artem Medvedev <amedvedev@outlook.com>
2016-10-07 14:13:10 +0300

    Removed outdated appcast

--- Casks/pins.rb
+++ Casks/pins.rb
@@ -3,8 +3,6 @@ cask 'pins' do
   sha256 :no_check
 
   url 'http://pinsapp.com/download/Pins.dmg'
-  appcast 'http://pinsapp.com/appcast.xml',
-          checkpoint: '5c64de791ef112795cdbd0b98eeb582a8c66d26e74ec0cdeb1ab8cbc52c163ef'
   name 'Pins'
   homepage 'http://pinsapp.com/'
   license :commercial
