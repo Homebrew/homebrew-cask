52a5a0ddc494bf2f4e0ed9bc9182169ec4a6f6e2
Jonathan Rehm <jonathan@rehm.me>
2016-10-06 20:36:22 -0700

    Update tiny download url (#25279)

--- Casks/tiny.rb
+++ Casks/tiny.rb
@@ -2,7 +2,8 @@ cask 'tiny' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.delightfuldev.com/assets/files/tiny/Tiny.zip'
+  # kaomojiformac.github.io was verified as official when first introduced to the cask
+  url 'https://kaomojiformac.github.io/tiny-download/Tiny.zip'
   name 'Tiny'
   homepage 'https://www.delightfuldev.com/tiny/'
   license :gratis
