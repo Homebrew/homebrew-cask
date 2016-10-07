2c22440daaa01e5f639e58337a84b0ca80353620
Noah Overcash <smileytechguy@smileytechguy.com>
2016-10-06 23:33:39 -0400

    Collection of crowbar brute-force tools for DMG, KeyChains, and PGP

---
+++ Casks/crowbarkc.rb
@@ -0,0 +1,14 @@
+cask 'crowbarkc' do
+  version '1.0'
+  sha256 :no_check
+
+  url "http://www.georgestarcher.com/crowbarKC/crowbarKC-v1.0.dmg"
+  name 'crowbarKC'
+  homepage 'http://www.georgestarcher.com/software/'
+  license :unknown
+
+  app 'crowbarKC.app'
+
+  # developer says that he doesn't plan to recreate for sierra
+  depends_on macos: '<= :el_capitan'
+end
