3f6413eac79125b622b30ea7c8638632bc684e4b
Artem Medvedev <amedvedev@outlook.com>
2016-10-07 11:04:20 +0300

    Update pins to latest

--- Casks/pins.rb
+++ Casks/pins.rb
@@ -1,8 +1,8 @@
 cask 'pins' do
-  version '1.8.0'
-  sha256 '63872cf0bee1287266823062e3d0d810b23bdcc9285412f2214bcf6a5d2d65aa'
+  version :latest
+  sha256 :no_check
 
-  url "http://pinsapp.com/download/Pins#{version}.zip"
+  url 'http://pinsapp.com/download/Pins.dmg'
   appcast 'http://pinsapp.com/appcast.xml',
           checkpoint: '5c64de791ef112795cdbd0b98eeb582a8c66d26e74ec0cdeb1ab8cbc52c163ef'
   name 'Pins'
@@ -10,4 +10,6 @@ cask 'pins' do
   license :commercial
 
   app 'Pins.app'
+
+  zap delete: '~/Library/Containers/com.pinsapp.pins'
 end
