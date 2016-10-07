6c6ce522fc17a20930e21fd01b22803d0d2ed642
Artem Medvedev <amedvedev@outlook.com>
2016-10-07 11:38:00 +0300

    Update razorsql to 7.0.8 (#25285)

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask 'razorsql' do
-  version '7.0.5'
-  sha256 '2885e6542028a743579ad1f69427a1e254933e47f925d5d7b22c057b1b73794a'
+  version '7.0.8'
+  sha256 '3f22f0be5eb0a4ff06d81b162c4c3a7a801bd9e73932ca7cba6245152fbdd56f'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   name 'RazorSQL'
