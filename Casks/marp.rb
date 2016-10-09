c5eef9d52a43938a7a80c88ec39fce897f78801e
Robert Lu <robberphex@gmail.com>
2016-10-09 09:36:10 +0800

    update marp to 0.0.9

--- Casks/marp.rb
+++ Casks/marp.rb
@@ -1,11 +1,11 @@
 cask 'marp' do
-  version '0.0.8'
-  sha256 '6db7310232bf9abb4629f5a1600ebb9b912ffd750aa44eeaaa7c7ebdf5f563c6'
+  version '0.0.9'
+  sha256 'd72603f232ac9a4dcef05132221262b961536ebb2cf115d007f017dc94030f7f'
 
   # github.com/yhatt/marp was verified as official when first introduced to the cask
   url "https://github.com/yhatt/marp/releases/download/v#{version}/#{version}-Marp-darwin-x64.dmg"
   appcast 'https://github.com/yhatt/marp/releases.atom',
-          checkpoint: '377f8d38c817a6ffb293c6de539ae7f4cb309cb76244f0f872c43e8eb8625c29'
+          checkpoint: 'c88ae7e0e0cce3ca564c0a263c2421f57202e52014773c0721b54904356a8432'
   name 'Marp'
   homepage 'https://yhatt.github.io/marp/'
   license :mit
