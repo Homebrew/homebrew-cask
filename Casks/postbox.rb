8ef6eb2af8ac996a4c6303e3a31bdb5cb0bc0c34
Artem Medvedev <amedvedev@outlook.com>
2016-10-09 08:38:51 +0300

    Update postbox to 5.0.4 (#25333)

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,9 +1,9 @@
 cask 'postbox' do
-  version '5.0.3'
-  sha256 '161828c7bb916dcd7ea77e853b61ed80d271d6ed1145e010e7c01110e048d882'
+  version '5.0.4'
+  sha256 '367b597f4b802034405e75efb33078c12e69428d8fbaafe45cd0a53e27f3b9a7'
 
   # amazonaws.com/download.getpostbox.com was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/download.getpostbox.com/installers/#{version}/1_1005b8e6ea15bdb4dff5245b833ebd7e27919eb8/postbox-#{version}-mac64.dmg"
+  url "https://s3.amazonaws.com/download.getpostbox.com/installers/#{version}/1_6ba7710e4b30693e0770fd893492f752db8b7fc7/postbox-#{version}-mac64.dmg"
   name 'Postbox'
   homepage 'https://www.postbox-inc.com/'
   license :commercial
