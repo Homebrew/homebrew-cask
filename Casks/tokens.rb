88873745aba0ac5a16b12f63659208c314d5c9a6
Victor Popkov <victor@popkov.me>
2016-10-09 20:52:19 +0300

    Update tokens to 1.5.2 (#25343)

--- Casks/tokens.rb
+++ Casks/tokens.rb
@@ -1,10 +1,10 @@
 cask 'tokens' do
-  version '1.5.1'
-  sha256 '650e664084555b3b2d7cef8dcdb5fd0f471520ac8a1bcde42645e11593a51a52'
+  version '1.5.2'
+  sha256 '9654eeb499951e463fac5f29adc633f5996116a444d6a99ca4c095bf2bd46a18'
 
   url "http://resources.usetokens.com/download/#{version}/Tokens.app.zip"
   appcast 'http://api.usetokens.com/updates/stable',
-          checkpoint: '2f4bd11922727463342201da26c6bbd71487f33774fd2d12b439c21238622a36'
+          checkpoint: '44bc6b825238fc5b6ae48b87719e2a21fc43ada018ddcd257fc28c7dd9ca2811'
   name 'Tokens'
   homepage 'http://usetokens.com/'
   license :closed
