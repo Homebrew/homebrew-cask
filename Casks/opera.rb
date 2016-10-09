5decf2a737fbd0b9dde8e3a8c49671587d714bff
Artem Medvedev <amedvedev@outlook.com>
2016-10-10 02:38:43 +0300

    Update opera to 40.0.2308.81

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '40.0.2308.75'
-  sha256 '130e2971bde479d9293a6e53914438c1e4cb3631e15141f227b0c6ac8c5878a1'
+  version '40.0.2308.81'
+  sha256 '8b21d54227c13e074859f7f48f108944976c444f02b3e5da07628dbaf3093bda'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
