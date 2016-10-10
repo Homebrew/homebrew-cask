e0f96160393b5a817f6bfcde69eae4a1cc5f96b1
syscrusher <syscrusher@users.noreply.github.com>
2016-10-10 17:09:03 -0500

    Update visual-studio-code to 1.6,e52fb0bc87e6f5c8f144e172639891d8d8c9aa55

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.5.3,5be4091987a98e3870d89d630eb87be6d9bafd27'
-  sha256 '78993c1bb27b76d2bd60c01f85b8b5f297bb8671a1c2799d7ab678b9841d20e1'
+  version '1.6,e52fb0bc87e6f5c8f144e172639891d8d8c9aa55'
+  sha256 '13f054d3971ab4547b4ed7297a7cbbdd1c4035556fe0dd0da9ce194be045f06e'
 
   # az764295.vo.msecnd.net was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
