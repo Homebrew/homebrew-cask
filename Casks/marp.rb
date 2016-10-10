ed9ac598f8607cbe24cf57b2cb082053701bbdd4
Kamil Wargula <kamil.wargula@allegrogroup.com>
2016-10-10 15:25:40 +0200

    Upgrade Marp to v0.0.9

--- Casks/marp.rb
+++ Casks/marp.rb
@@ -1,6 +1,6 @@
 cask 'marp' do
-  version '0.0.8'
-  sha256 '6db7310232bf9abb4629f5a1600ebb9b912ffd750aa44eeaaa7c7ebdf5f563c6'
+  version '0.0.9'
+  sha256 'd72603f232ac9a4dcef05132221262b961536ebb2cf115d007f017dc94030f7f'
 
   # github.com/yhatt/marp was verified as official when first introduced to the cask
   url "https://github.com/yhatt/marp/releases/download/v#{version}/#{version}-Marp-darwin-x64.dmg"
