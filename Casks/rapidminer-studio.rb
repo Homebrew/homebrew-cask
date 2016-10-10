83cbfacdf3426f6f9151c414bf9aee5156ddc07e
longstone <git@longstone.ch>
2016-10-10 15:10:52 +0200

    Add RapidMiner Studio 7.2.3

---
+++ Casks/rapidminer-studio.rb
@@ -0,0 +1,12 @@
+cask 'rapidminer-studio' do
+  version '7.2.3'
+  sha256 'b04cc77028bac7822d5aeead687a4cf1cca146cebc220cb4d55803aa86489d61'
+
+  url 'https://s3.amazonaws.com/rapidminer.releases/rapidminer-studio/7.2.3/rapidminer-studio-osx-7.2.3.dmg'
+  name 'RapidMiner Studio'
+  homepage 'https://rapidminer.com/products/studio/'
+  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+
+  app 'RapidMiner\ Studio.app'
+
+end
