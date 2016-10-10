d3b5f17ae4e51a5f80f178eb71394e18e63dd290
longstone <git@longstone.ch>
2016-10-11 00:23:56 +0200

    add :no_check and :latest according to review

--- Casks/rapidminer-studio.rb
+++ Casks/rapidminer-studio.rb
@@ -1,6 +1,6 @@
 cask 'rapidminer-studio' do
-  version ':latest'
-  sha256 'b04cc77028bac7822d5aeead687a4cf1cca146cebc220cb4d55803aa86489d61'
+  version :latest
+  sha256 :no_check
 
   url 'http://go.rapidminer.com/rm-studio-download-mac'
   name 'RapidMiner Studio'
