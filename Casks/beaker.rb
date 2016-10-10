cc1fbb9a8c151bb74c088d58c54a50eaea7e328b
Michael Ford <fanquake@users.noreply.github.com>
2016-10-10 17:36:48 +0200

    Beaker Notebook 1.6-0-gb7c81a9

--- Casks/beaker.rb
+++ Casks/beaker.rb
@@ -1,6 +1,6 @@
 cask 'beaker' do
-  version '1.5.1-0-g6e6bdd3'
-  sha256 '5ce78888b799b7b39d532b99d63a83301586ede2ccf91b7b2e8fe8ecb1ae7bbf'
+  version '1.6-0-gb7c81a9'
+  sha256 '9c101935a6a715e9e396c7a8945bef38abf26ebcd5d510ddf70a137aa927f3ac'
 
   # d299yghl10frh5.cloudfront.net was verified as official when first introduced to the cask
   url "https://d299yghl10frh5.cloudfront.net/beaker-notebook-#{version}-mac.dmg"
