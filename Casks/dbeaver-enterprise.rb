f1ec62b6c16a04ea6c5ae27b22f6d3f8cc8717d3
Colin Dunn <dunnc@amazon.com>
2016-10-11 09:08:53 +1100

    Update dbeaver-enterprise to 3.7.6

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-enterprise' do
-  version '3.7.5'
-  sha256 '2378701036d23b2b0962a89ab4f29bc58b5692693602d20050f060c2a2a05c25'
+  version '3.7.6'
+  sha256 '2077b2d487fbe5cf8fab1866097d37e4fe125e2d312c10050aed23fc09777c61'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   name 'DBeaver Enterprise Edition'
