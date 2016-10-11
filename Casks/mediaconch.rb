5ccc67d4f796a890f6434bb52439d5145164f8ca
Dave Rice <dave@dericed.com>
2016-10-11 11:31:11 -0400

    add MediaConch v16.09

---
+++ Casks/mediaconch.rb
@@ -0,0 +1,11 @@
+cask 'mediaconch' do
+  version '16.09'
+  sha256 '585b31fb6ec46bd32800ce2f392d2eec3ac0fba9da878be8f448091a5cc6a192'
+
+  url 'https://mediaarea.net/download/binary/mediaconch-gui/16.09/MediaConch_GUI_16.09_Mac.dmg'
+  name 'MediaConch'
+  homepage 'https://mediaarea.net/MediaConch'
+  license :gratis
+
+  app 'MediaConch.app'
+end
