6aa50e2438dbe7da805f65c582d8e5e4f00f5a36
Till Backhaus <till@backha.us>
2016-10-11 14:48:20 +0200

    Cleanup helm 2.0.0-alpha.5
    - correct URL
    - remove license

--- Casks/helm.rb
+++ Casks/helm.rb
@@ -2,14 +2,12 @@ cask 'helm' do
   version '2.0.0-alpha.5'
   sha256 '6bd540611042665c78923fd722e9cd25d08e7381e982535a3a720458da163811'
 
-  # storage.googleapis.com/kubernetes-helm/helm-v was verified as official when first introduced to the cask (2016-10-11)
+  # storage.googleapis.com/kubernetes-helm/ was verified as official when first introduced to the cask
   url "http://storage.googleapis.com/kubernetes-helm/helm-v#{version}-darwin-amd64.tar.gz"
   appcast 'https://github.com/kubernetes/helm/releases.atom',
           checkpoint: '89bc384e94e30b530e8afce2db165db31c3e18465077de889255d166a9110d44'
   name 'Helm'
   homepage 'https://github.com/kubernetes/helm'
-
-  license :apache
 
   binary 'darwin-amd64/helm'
 end
