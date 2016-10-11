438f050c0120d2b69b3539801be8a1ed780784f7
Artem Medvedev <amedvedev@outlook.com>
2016-10-11 14:29:48 +0300

    Update pastebot to 2.0.b7

--- Casks/pastebot.rb
+++ Casks/pastebot.rb
@@ -1,6 +1,6 @@
 cask 'pastebot' do
-  version '2.0.b6'
-  sha256 '3456ef932f2a701b9ac8c45f7eb4519b1976201c8ee6dead4fb3fd77bd288264'
+  version '2.0.b7'
+  sha256 '01b0bfccd1904b8b087fe4aeb0a09d0791f0cf176e5090893381d4c42fb6904e'
 
   # tapbots.net/pastebot was verified as official when first introduced to the cask
   url "https://tapbots.net/pastebot#{version.major}/PastebotBeta.zip"
