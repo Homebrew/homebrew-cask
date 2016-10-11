a164c9022798fb28636c93ada7371b0b7702618a
Anne Jan Brouwer <brouwer@annejan.com>
2016-10-11 02:44:25 +0200

    Update qtpass.rb

--- Casks/qtpass.rb
+++ Casks/qtpass.rb
@@ -5,7 +5,7 @@ cask 'qtpass' do
   # github.com/IJHack/qtpass was verified as official when first introduced to the cask
   url "https://github.com/IJHack/qtpass/releases/download/v#{version}/qtpass-#{version}.dmg"
   appcast 'https://github.com/IJHack/qtpass/releases.atom',
-          checkpoint: '96b00beb82f888bbee5f39f9251b59a3fda77de19cc5adfc52caa43711a97f46'
+          checkpoint: 'ff499dc0c1c79de926d52e62cfb7e2f4a8c07a27d69b1c15df140bec9b0cf52f'
   name 'QtPass'
   homepage 'https://qtpass.org/'
 
