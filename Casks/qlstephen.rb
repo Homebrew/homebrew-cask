cd739a979c812636d6bf5ae64856a00695504c55
Trevor Dorsey <tsdorsey@gmail.com>
2016-10-11 06:35:39 -0700

    updated qlstephen (1.4.4) (#25394)

--- Casks/qlstephen.rb
+++ Casks/qlstephen.rb
@@ -1,11 +1,11 @@
 cask 'qlstephen' do
-  version '1.4.3'
-  sha256 '73f9a467ba2eb4244ffe1dbf9be435d3481a438c8ec64d2bd9bb1ba60d576a66'
+  version '1.4.4'
+  sha256 'ef7323064fd5bff5c3e4b7fabe9ac832711535862cdf585f806e6962cd4a9119'
 
   # github.com/whomwah/qlstephen was verified as official when first introduced to the cask
   url "https://github.com/whomwah/qlstephen/releases/download/#{version}/QLStephen.qlgenerator.#{version}.zip"
   appcast 'https://github.com/whomwah/qlstephen/releases.atom',
-          checkpoint: '6cf8bb2decd2e475f96e024cdd7c265266361417b4d30520def92c31eb951c3b'
+          checkpoint: 'c7d3cacf504b6969a76ea0ebd067408b4d01efd62862db9f902cf810eefb4e5c'
   name 'QLStephen'
   homepage 'https://whomwah.github.io/qlstephen/'
 
