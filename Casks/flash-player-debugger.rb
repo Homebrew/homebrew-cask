dc77bc22361787b4d0d71ffb712158f4cc64f9f9
Vítor Galvão <info@vitorgalvao.com>
2016-10-12 07:33:14 +0100

    Update flash-player-debugger to 23.0.0.185 (#25412)

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger' do
-  version '23.0.0.162'
-  sha256 'b53cf7711fed9e7a90d3fa24fcc571478cbce29467b46206cec153a29c449272'
+  version '23.0.0.185'
+  sha256 '2d0faacfa637eef76c9c78f2b250e01f5f3d29cb4d09ea3bd493c0f70e3b9102'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
