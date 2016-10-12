3e8542768fe6826e86bc7a39e85cb8b536f49efb
mathbunnyru <mathbunnyru@users.noreply.github.com>
2016-10-12 09:30:35 +0300

    Update pepper-flash to 23.0.0.185 (#25409)

--- Casks/pepper-flash.rb
+++ Casks/pepper-flash.rb
@@ -1,10 +1,10 @@
 cask 'pepper-flash' do
-  version '23.0.0.162'
-  sha256 'b1c468b9f523e1b267115ad75e1074d89528b199bc9d21101d8b5be8fb13450c'
+  version '23.0.0.185'
+  sha256 'e94635e515bad36908313f605a07480c1759ba348842326dae35723cff6fedf4'
 
   url "http://fpdownload.adobe.com/pub/flashplayer/pdc/#{version}/install_flash_player_osx_ppapi.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
-          checkpoint: 'e6bf656907bdee3e843e649f97de873aac9aa18e19d87c111d7fb658037c4047'
+          checkpoint: '159582e9a7da281f6d866c000f4f2803c7306e89be44075e6793c142286077e5'
   name 'Adobe Pepper Flash Player'
   homepage 'https://get.adobe.com/flashplayer/otherversions'
 
