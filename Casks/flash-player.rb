22a3046b3026b4a64f7cd560871577b53ff2facb
Vítor Galvão <info@vitorgalvao.com>
2016-10-12 07:32:59 +0100

    Update flash-player to 23.0.0.185 (#25411)

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,6 +1,6 @@
 cask 'flash-player' do
-  version '23.0.0.162'
-  sha256 '88bc4b82d03195fe457b426d1ca73e0c493a7755a6fbff9e9d26155a1cb56117'
+  version '23.0.0.185'
+  sha256 'f1a7b29230bee1a8125d005d69becc9d6a63e7192291c5478b1f1f5e68d3e9b0'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
