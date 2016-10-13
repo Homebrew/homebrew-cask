f24abc1cec721c6d6d7068eaa6f5f2b3a514af0c
Vítor Galvão <info@vitorgalvao.com>
2016-10-13 19:38:59 +0100

    flash-ppapi.rb: reword `name`

--- Casks/flash-ppapi.rb
+++ Casks/flash-ppapi.rb
@@ -5,7 +5,7 @@ cask 'flash-ppapi' do
   url "https://fpdownload.adobe.com/pub/flashplayer/pdc/#{version}/install_flash_player_osx_ppapi.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
           checkpoint: '159582e9a7da281f6d866c000f4f2803c7306e89be44075e6793c142286077e5'
-  name 'Adobe Flash Player Plugin - PPAPI (for Opera and Chromium)'
+  name 'Adobe Flash Player PPAPI (plugin for Opera and Chromium)'
   homepage 'https://get.adobe.com/flashplayer/otherversions'
 
   pkg 'Install Adobe Pepper Flash Player.app/Contents/Resources/Adobe Flash Player.pkg'
