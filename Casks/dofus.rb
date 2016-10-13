1faa7f30183202cb63c2c1d8613126cf9bf785a1
Kevin Rambaud <kevin.rambaud@gmail.com>
2016-10-13 14:13:31 +0200

    Update Dofus version and sha256

--- Casks/dofus.rb
+++ Casks/dofus.rb
@@ -1,7 +1,7 @@
 cask 'dofus' do
-  version ':latest'
-  sha256 'e38cfa38c09a59867a8a9da1542caa7cf44e11805e7506ab689011059670b065'
-
+  version :latest
+  sha256 :no_check
+  
   # dl.ak.ankama.com was verified as official when first introduced to the cask
   url 'http://dl.ak.ankama.com/games/installers/dofus.dmg'
   name 'Dofus'
