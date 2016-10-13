f8681b1b5f3fafa9fe1e7d66f24e907a29482d60
S. Zachariah Sprackett <zac@sprackett.com>
2016-10-13 12:06:25 -0700

    Add Secure Pipes.app

---
+++ Casks/secure-pipes.rb
@@ -0,0 +1,10 @@
+cask 'secure-pipes' do
+  version '0.99.8'
+  sha256 'ea52611b251ecd03cf0ccb0127ed6a9032a4d48d6faf6053b1459cc1930cbaf8'
+
+  url 'https://www.opoet.com/pyro/index.php/files/download/15e8a5ef38260ea'
+  name 'Secure Pipes'
+  homepage 'https://www.opoet.com/pyro/index.php/'
+
+  app 'Secure Pipes.app'
+end
