3c4b38e39abf7ff413889c4842e94ae5e7d7c943
Eric Nielsen <eric@amalgamar.com.br>
2016-10-13 16:42:58 -0500

    Update macvim to 8.0.111

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,10 +1,10 @@
 cask 'macvim' do
-  version '8.0.110'
-  sha256 '273490172ae459b28fe0963d460c7d7936da48e865201b22e3ba419a506e1f22'
+  version '8.0.111'
+  sha256 'e91ad178c963df6f8ced8b61033a47aef290986fcf7b0e79e7b83b5aa7e5491c'
 
   url "https://github.com/macvim-dev/macvim/releases/download/snapshot-#{version.patch}/MacVim.dmg"
   appcast 'https://github.com/macvim-dev/macvim/releases.atom',
-          checkpoint: '12e3a13c18f5e90cf45659af3f295a40238384e46d18c931323f81e37506e5ac'
+          checkpoint: 'e4228aab58218059de7c0bf8ea6006ee4f936291bf482b3fe204d4a9d3061b40'
   name 'MacVim'
   homepage 'https://github.com/macvim-dev/macvim'
 
