4efd6930dcf2c7b9bc9759712b2b585b40b81d58
Fredrik Sävje <fsavje@users.noreply.github.com>
2016-10-13 03:15:21 -0700

    Update epichrome to 2.1.13 (#25451)

--- Casks/epichrome.rb
+++ Casks/epichrome.rb
@@ -1,10 +1,10 @@
 cask 'epichrome' do
-  version '2.1.12'
-  sha256 '3f0a39576cc1ca5f211346a3dfdcacfd1e87cd43a121e17c02baf585d9fade80'
+  version '2.1.13'
+  sha256 '60b4b207469e1f461e4979ce9c4e8ebe82d9e48813b4737484c932ade1e95812'
 
   url "https://github.com/dmarmor/epichrome/releases/download/v#{version}/epichrome-#{version}.dmg"
   appcast 'https://github.com/dmarmor/epichrome/releases.atom',
-          checkpoint: '60d7434a8f8762ed4d846778c9d9e6fc6964ac77bb2ac19786841dcfc3af983d'
+          checkpoint: '88fc19f8e6dcfcf25c8831b7df98ce8c820b9fdc2bd621096527044b8198d4cb'
   name 'Epichrome'
   homepage 'https://github.com/dmarmor/epichrome'
 
