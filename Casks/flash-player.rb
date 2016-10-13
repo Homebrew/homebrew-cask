0fc69f8ca084550b68e51d26a4cf2a2c263f0a7c
Vítor Galvão <info@vitorgalvao.com>
2016-10-13 19:40:38 +0100

    reverted flash-projector to flash-player

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,4 +1,4 @@
-cask 'flash-projector' do
+cask 'flash-player' do
   version '23.0.0.185'
   sha256 'f1a7b29230bee1a8125d005d69becc9d6a63e7192291c5478b1f1f5e68d3e9b0'
 
@@ -6,7 +6,7 @@ cask 'flash-projector' do
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
           checkpoint: '159582e9a7da281f6d866c000f4f2803c7306e89be44075e6793c142286077e5'
-  name 'Adobe Flash Player projector (standalone version)'
+  name 'Adobe Flash Player projector'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
   app 'Flash Player.app'
