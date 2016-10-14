0e48a4060f0c2b1c16465bdf8468ed1c03cf4637
Markus Reiter <me@reitermark.us>
2016-10-14 19:30:04 +0200

    Update calibre to v2.70.0 (#25491)

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -3,10 +3,10 @@ cask 'calibre' do
     version '1.48.0'
     sha256 '0533283965fbc9a6618d0b27c85bdf3671fe75ff0e89eeff406fe1457ee61b14'
   else
-    version '2.69.0'
-    sha256 '6588e8723ce53944b80ee6bbb264e912500c617f543330652b61a9875cbbbb07'
+    version 'v2.70.0'
+    sha256 'fe84a144561af99f8103ec5f1ea68b53da7bf22883490a95668db7e1964b504d'
     appcast 'https://github.com/kovidgoyal/calibre/releases.atom',
-            checkpoint: '2d506c52541e006aa3dbe59bfc386806a4eceaa87c44fd7152cc2916d41f104f'
+            checkpoint: '67980993effbfe0d585b55a6a7a5cbd6209db5df3032db65e819b43be038f803'
   end
 
   url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
