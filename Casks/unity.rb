c910e18b5b71d3a6361a31d63cbab0bcf4518ff4
Alex Edelman <alexoedelman@gmail.com>
2016-10-14 10:35:48 -0500

    Update unity to 5.4.2f1,94e3a60ee258

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask 'unity' do
-  version '5.4.1f1,649f48bbbf0f'
-  sha256 'aaeb770b200ded4dfca6a9dc5f4cdd5948841dd668cbad2a44b78f748f798203'
+  version '5.4.2f1,94e3a60ee258'
+  sha256 '02b7900d2f81be23bb3fbe60eeffef5573787ebe9a6695dc81435187f2a328df'
 
   url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity.pkg"
   name 'Unity Editor'
