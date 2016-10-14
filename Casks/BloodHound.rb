3aa4558ff1e6d3248e615b1b2c5232ee67dd1b94
ralphte <ralphte@gmail.com>
2016-10-14 17:03:24 -0400

    Add BloodHound.app v1.1

---
+++ Casks/BloodHound.rb
@@ -0,0 +1,10 @@
+cask 'BloodHound' do
+  version '1.1'
+  sha256 '399ab1b17f68128cc5a2c143f3795ac74876fa48a117c284a09a7f5a196c6a8e'
+
+  url 'https://github.com/adaptivethreat/BloodHound/releases/download/1.1/BloodHound-darwin-x64.zip'
+  name 'BloodHound'
+  homepage 'https://github.com/adaptivethreat/BloodHound'
+
+  app 'BloodHound-darwin-x64/BloodHound.app'
+end
