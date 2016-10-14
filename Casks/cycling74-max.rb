78359625f7d2731daedccede6a7b64bbc272012d
yuhki50 <yuhki50@gmail.com>
2016-10-15 00:37:15 +0900

    update cycling74 max (7.3.0 161012) (#25476)

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,6 +1,6 @@
 cask 'cycling74-max' do
-  version '7.2.5_160915'
-  sha256 '2bab53a1a5217c2150429472ed45c665472bd676ff23bb3624102adc34a77768'
+  version '7.3.0_161012'
+  sha256 '20893b1f26a41675ef565710eb7299f2e9ac946399430afc289eca3c02be580c'
 
   # akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.no_dots}.dmg"
