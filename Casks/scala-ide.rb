51976b6a0c275c1de4c2c25b4c6dbb8dabd5c6bc
Ryan Ovrevik <rovrevik@ovrevik.com>
2016-10-15 17:39:28 -0400

    Update scala-ide to 4.4.1

--- Casks/scala-ide.rb
+++ Casks/scala-ide.rb
@@ -1,9 +1,15 @@
 cask 'scala-ide' do
-  version '4.4.0'
-  sha256 'c2b8a2d02fb6dd8586427cd171e3e3e8b55cf50afbb126e2bbf936052775e4b8'
+  version '4.4.1'
+  sha256 'b3bd0907aec82e943de80df428839f08fca6823e99484bf28b50f878d1503d05'
+
+  module Utils
+    def self.suffix
+      '20160401'
+    end
+  end
 
   # downloads.typesafe.com/scalaide-pack was verified as official when first introduced to the cask
-  url "https://downloads.typesafe.com/scalaide-pack/#{version}-vfinal-luna-211-20160401/scala-SDK-#{version}-vfinal-2.11-macosx.cocoa.x86_64.zip"
+  url "https://downloads.typesafe.com/scalaide-pack/#{version}-vfinal-luna-211-#{Utils.suffix}/scala-SDK-#{version}-vfinal-2.11-macosx.cocoa.x86_64.zip"
   name 'Scala IDE'
   homepage 'http://scala-ide.org/'
 
