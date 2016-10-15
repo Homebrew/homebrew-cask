0db8860c0faf79bbf806d68ec8f066856850dd31
Tennison Chan <tennison.chan+gh@gmail.com>
2016-10-15 11:29:03 -0400

    Change into bitbucket.org/objective-see

--- Casks/oversight.rb
+++ Casks/oversight.rb
@@ -2,7 +2,7 @@ cask 'oversight' do
   version '1.0.0'
   sha256 'e20b7d91f4301f26236607e5c204286b1901cd138ae6c3a66865c09a95746bc5'
 
-  # bitbucket.org was verified as official when first introduced to the cask
+  # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url 'https://bitbucket.org/objective-see/deploy/downloads/OverSight_#{version}.zip'
   name 'Oversight'
   homepage 'https://objective-see.com/products/oversight.html'
