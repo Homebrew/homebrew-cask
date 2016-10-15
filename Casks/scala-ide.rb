cd3cf603a9ea8386d9099ab1148c476702fa49ed
Ryan Ovrevik <rovrevik@ovrevik.com>
2016-10-15 19:42:22 -0400

    update scala-ide: rename suffix to build

--- Casks/scala-ide.rb
+++ Casks/scala-ide.rb
@@ -3,13 +3,13 @@ cask 'scala-ide' do
   sha256 'b3bd0907aec82e943de80df428839f08fca6823e99484bf28b50f878d1503d05'
 
   module Utils
-    def self.suffix
+    def self.build
       '20160401'
     end
   end
 
   # downloads.typesafe.com/scalaide-pack was verified as official when first introduced to the cask
-  url "https://downloads.typesafe.com/scalaide-pack/#{version}-vfinal-luna-211-#{Utils.suffix}/scala-SDK-#{version}-vfinal-2.11-macosx.cocoa.x86_64.zip"
+  url "https://downloads.typesafe.com/scalaide-pack/#{version}-vfinal-luna-211-#{Utils.build}/scala-SDK-#{build}-vfinal-2.11-macosx.cocoa.x86_64.zip"
   name 'Scala IDE'
   homepage 'http://scala-ide.org/'
 
