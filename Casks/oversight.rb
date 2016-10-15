b9cecc6ccda12673c0dd27a20eac29ff23e6feb1
Tennison Chan <tennison.chan+gh@gmail.com>
2016-10-14 21:01:40 -0400

    Add OverSight Releases v1.0.0

---
+++ Casks/oversight.rb
@@ -0,0 +1,13 @@
+cask 'oversight' do
+  version '1.0.0'
+  sha256 'e20b7d91f4301f26236607e5c204286b1901cd138ae6c3a66865c09a95746bc5'
+
+  # bitbucket.org was verified as official when first introduced to the cask
+  url 'https://bitbucket.org/objective-see/deploy/downloads/OverSight_1.0.0.zip'
+  name 'Oversight'
+  homepage 'https://objective-see.com/products/oversight.html'
+
+  app 'OverSight_Installer.app'
+
+  uninstall delete: '/Applications/OverSight.app'
+end
