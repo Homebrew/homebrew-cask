4b684b4944190a96c0426a4712091e6b9dc81576
Markus Reiter <me@reitermark.us>
2016-10-15 17:22:47 +0200

    Update powershell to 6.0.0-alpha.10 (#25485)

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -5,7 +5,7 @@ cask 'powershell' do
   # github.com/PowerShell/PowerShell was verified as official when first introduced to the cask
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}.pkg"
   appcast 'https://github.com/PowerShell/PowerShell/releases.atom',
-          checkpoint: '11d86f299b326db2ca34aa771a5576da5438c28d30dbcba22c85878bf57298de'
+          checkpoint: '839967733ed3b1157f9767499e18fcdb5dffb68e3b4a551197a16916bc0f5f74'
   name 'PowerShell'
   homepage 'https://msdn.microsoft.com/powershell'
 
