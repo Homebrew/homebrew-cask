9fac24c529a75d298da27ed06f0e89ba8f18fc75
Ryan Ovrevik <rovrevik@ovrevik.com>
2016-10-16 01:13:04 -0400

    eclipse-jee: update to use standard version modifiers

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -1,20 +1,10 @@
 cask 'eclipse-jee' do
-  version '4.6.1'
+  version '4.6.1,neon:1a'
   sha256 'c64da846dfb762ce6650e4d842c566a61836b14c0a77b1f5395f4f9842a66aad'
 
-  module Utils
-    def self.release
-      'neon'
-    end
-
-    def self.build
-      '1a'
-    end
-  end
-
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{Utils.release}/#{Utils.build}/eclipse-jee-#{Utils.release}-#{Utils.build}-macosx-cocoa-x86_64.tar.gz&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-jee-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse IDE for Java EE Developers'
-  homepage "http://www.eclipse.org/downloads/packages/release/#{Utils.release}/#{Utils.build}"
+  homepage "http://www.eclipse.org/downloads/packages/release/#{version.after_comma.before_colon}/#{version.after_colon}"
 
   depends_on macos: '>= :leopard'
   depends_on arch: :x86_64
