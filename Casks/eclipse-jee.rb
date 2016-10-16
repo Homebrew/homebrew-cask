026f7a79268bffc337562da4cb8f487c382dadd8
Ryan Ovrevik <rovrevik@users.noreply.github.com>
2016-10-16 01:37:19 -0400

    Update eclipse-jee to 4.6.1 (#25531)
    * Update eclipse-jee to 4.6.1
    
    * eclipse-jee: update to use standard version modifiers

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -1,10 +1,10 @@
 cask 'eclipse-jee' do
-  version '4.6.0'
-  sha256 'ec62c9734396fb99ae5fc8af8731bb87d19d4d40579aeac69b8fe1447c51a614'
+  version '4.6.1,neon:1a'
+  sha256 'c64da846dfb762ce6650e4d842c566a61836b14c0a77b1f5395f4f9842a66aad'
 
-  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/R/eclipse-jee-neon-R-macosx-cocoa-x86_64.tar.gz&r=1'
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-jee-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse IDE for Java EE Developers'
-  homepage 'https://eclipse.org/'
+  homepage "http://www.eclipse.org/downloads/packages/release/#{version.after_comma.before_colon}/#{version.after_colon}"
 
   depends_on macos: '>= :leopard'
   depends_on arch: :x86_64
