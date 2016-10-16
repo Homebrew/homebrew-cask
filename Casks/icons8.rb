f76d2c8125b08e67df5f9479e8c049d24fa7cf23
Varun Yellina <varun@yellina.io>
2016-10-17 01:25:23 +0530

    Update icons8 to 5.4.2

--- Casks/icons8.rb
+++ Casks/icons8.rb
@@ -1,7 +1,7 @@
 cask 'icons8' do
   # note: "8" is not a version number, but an intrinsic part of the product name
-  version '5.4'
-  sha256 '69c1111ffc53b46083231e5942772af1d4dd31fc595e62a8e013d8f558db2299'
+  version '5.4.2'
+  sha256 'e17afaf20eef3e5aca378c5ed0630903692de58e089f1dc78c7ff1656aeb05e0'
 
   url 'https://icons8.com/icons8_mac_app'
   appcast 'https://maxcdn.icons8.com/download/icons8_cast.xml',
