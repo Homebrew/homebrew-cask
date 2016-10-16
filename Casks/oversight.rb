5bf704fa03492984b1bcccb7b1c494dc973f5563
Tennison Chan <tennison.chan+gh@gmail.com>
2016-10-16 19:00:32 -0400

    Add checkpoint to appcast stanza

--- Casks/oversight.rb
+++ Casks/oversight.rb
@@ -4,7 +4,8 @@ cask 'oversight' do
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/OverSight_#{version}.zip"
-  appcast 'https://objective-see.com/products/versions/oversight.json'
+  appcast 'https://objective-see.com/products/versions/oversight.json',
+          checkpoint: 'da8cba3af8fa83a106e23be6299fd286fe8377931906270bfdd885a609a1ae8b'
   name 'Oversight'
   homepage 'https://objective-see.com/products/oversight.html'
 
