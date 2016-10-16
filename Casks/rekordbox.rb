b6f2c2e4cde9ada832318f6c62c74343dd6fb5bd
Hervé Yviquel <hyviquel@gmail.com>
2016-10-16 17:17:26 -0200

    Update rekordbox to 4.2.4

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -1,6 +1,6 @@
 cask 'rekordbox' do
-  version '4.2.1'
-  sha256 '45edce6c77a273d000d19b9f2855088861ea36a2220b15219a99a92c1d8559aa'
+  version '4.2.4'
+  sha256 '00c094a64ba486f8230c4627bfb84f6036c7e501d8d936acd732aa5fd64648d5'
 
   url "https://rekordbox.com/_app/files/Install_rekordbox_#{version.dots_to_underscores}.pkg.zip"
   name 'rekordbox'
