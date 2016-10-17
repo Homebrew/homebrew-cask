d78fab7b0101262430a2645fdab63ca35d9fc437
kkshmz <kyeshimizu@gmail.com>
2016-10-17 10:37:41 +0900

    Adding Openframeworks v0.9.4
    Adding the stable OSX version of Openframeworks to homebrew as well as creating path for updates

---
+++ Casks/openframeworks.rb
@@ -0,0 +1,10 @@
+cask 'openframeworks' do
+  version '0.9.4'
+  sha256 '63f215d7107d77126f69608a5e2404faf4d5a8f23865fcf20834eac8c9e55884'
+
+  url 'http://openframeworks.cc/versions/v0.9.4/of_v0.9.4_osx_release.zip'
+  name 'Openframeworks 0.9.4 OSX'
+  homepage 'http://openframeworks.cc/'
+
+  app 'of_v0.9.4_osx_release'
+end
