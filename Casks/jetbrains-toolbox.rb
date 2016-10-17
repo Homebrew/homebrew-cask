68d79f64aa941d463dd4754b2060f40472fd310d
Ryan Ovrevik <rovrevik@users.noreply.github.com>
2016-10-17 12:56:00 -0400

    Update jetbrains-toolbox to 1.0.2095 (#25575)

--- Casks/jetbrains-toolbox.rb
+++ Casks/jetbrains-toolbox.rb
@@ -1,6 +1,6 @@
 cask 'jetbrains-toolbox' do
-  version '1.0.2002'
-  sha256 '3e6a71dbd768ac6ea277728a7bd0fe6c161d8f228260b2a46fc9cf2936216178'
+  version '1.0.2095'
+  sha256 '3776ee7fc13e86eefc180c463d8e7ae2df1862847473cc454780bf0f6e35aa0e'
 
   url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.dmg"
   name 'JetBrains Toolbox'
