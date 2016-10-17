df5b81f78afbb7d2bba5109a6f8e6eeda87fad76
Jeremy Saklad <saklad5@me.com>
2016-10-17 09:52:42 -0500

    Update smcfancontrol to 2.6 (#25574)

--- Casks/smcfancontrol.rb
+++ Casks/smcfancontrol.rb
@@ -1,12 +1,14 @@
 cask 'smcfancontrol' do
-  version '2.5.2'
-  sha256 'e543b6a9fdb1bda6612699692f4dbaa9305007d451c987f2ae586950cbdf852b'
+  version '2.6'
+  sha256 '7662058e618537eb466307e3b12e540b857e61392646a5b09df51bec9ad6da38'
 
   url "https://www.eidac.de/smcfancontrol/smcfancontrol_#{version.dots_to_underscores}.zip"
   appcast 'https://www.eidac.de/smcfancontrol/smcfancontrol.xml',
-          checkpoint: '02ee10ec262f518ad8e099fe4230b78235d48ed26729c5ccd2241fe2d6291881'
+          checkpoint: 'b39e3a345beddb5026f54e7cb9593c4eb7c83a3cb63de015d53c84c10f52bf26'
   name 'smcFanControl'
   homepage 'https://www.eidac.de/?cat=40'
+
+  depends_on macos: '>= :lion'
 
   app 'smcFanControl.app'
 
