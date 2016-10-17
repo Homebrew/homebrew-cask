1e1d66fd3906078e33494fb7cf5330f6b897a991
pseudoku <decrement@users.noreply.github.com>
2016-10-17 12:02:24 -0700

    Update lyx to 2.2.2

--- Casks/lyx.rb
+++ Casks/lyx.rb
@@ -1,8 +1,8 @@
 cask 'lyx' do
   version '2.2.2'
-  sha256 'b6cf43227bfa8c1fb6879bacf04b818d78b6368c7318b0391f2bdfab70858d69'
+  sha256 'eae722f4ef8057d37dcd9c118442922e5d9ba1c7460b28ef8bc7a4b264cf995b'
 
-  url "ftp://ftp.lyx.org/pub/lyx/bin/#{version}/LyX-#{version}+qt5-x86_64-cocoa.dmg"
+  url "ftp://ftp.lyx.org/pub/lyx/bin/#{version}/LyX-#{version}+qt5.6.2-x86_64-cocoa.dmg"
   name 'LyX'
   homepage 'https://www.lyx.org'
   gpg "#{url}.sig",
