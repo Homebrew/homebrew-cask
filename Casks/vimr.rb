0fee9500f255c82cfeffcbcd94fe45123c4c4b17
Philip Deuchler <pbdeuchler@gmail.com>
2016-10-17 14:57:32 -0600

    Update vimr to v0.9.0-SNAPSHOT-20161017.2152-107

--- Casks/vimr.rb
+++ Casks/vimr.rb
@@ -1,11 +1,11 @@
 cask 'vimr' do
-  version '0.8.0-32'
-  sha256 '8660e7d5157b3165faab8a228f67a1bd63016c51bc300af131b90edc18dda678'
+  version 'v0.9.0-SNAPSHOT-20161017.2152-107'
+  sha256 '0ef84f2f2dddce3d0a4ab1c7578bf28d3ecd24d12e9554b7cac610974e4af447'
 
   # github.com/qvacua/vimr was verified as official when first introduced to the cask
-  url "https://github.com/qvacua/vimr/releases/download/v#{version}/VimR-#{version.sub(%r{-.*}, '')}.tar.bz2"
+  url "https://github.com/qvacua/vimr/releases/download/snapshot%2f#{version}/VimR-#{version}.tar.bz2"
   appcast 'https://github.com/qvacua/vimr/releases.atom',
-          checkpoint: 'cfd99e9ff5feb321bde77647d980df524d1522c8203df977f7a245254020c250'
+          checkpoint: 'e4d44a53c967f8fc2e23d0d674bbc0e55875312431d6bc0944f5fff3a9cf7918'
   name 'VimR'
   homepage 'http://vimr.org/'
 
