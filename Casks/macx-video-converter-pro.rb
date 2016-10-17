2d6d41e8bb676ec2ac8c0f568a1f0a268b85f571
Mark Anderson <mark@PowerBook.local>
2016-10-17 11:32:47 -0500

    added MacX Video Converter Pro

---
+++ Casks/macx-video-converter-pro.rb
@@ -0,0 +1,12 @@
+cask 'macx-video-converter-pro' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.macxdvd.com/download/macx-video-converter-pro.dmg'
+  name 'MacX Video Converter Pro'
+  homepage 'https://www.macxdvd.com/mac-video-converter-pro/'
+
+  app 'MacX Video Converter Pro.app'
+
+  uninstall pkgutil: 'com.macxdvd.macxvdoconverterpro'
+end
