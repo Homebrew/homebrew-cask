9ee10f813f91bfc0f52c5ad3104673712dab9335
Philip Deuchler <pbdeuchler@gmail.com>
2016-10-17 16:14:24 -0600

    Fix styling and put "v" in the url instead of in the version string as per CR comments

--- Casks/vimr.rb
+++ Casks/vimr.rb
@@ -1,14 +1,15 @@
 cask 'vimr' do
-  version 'v0.9.0-SNAPSHOT-20161017.2152-107'
+  version '0.9.0-SNAPSHOT-20161017.2152-107'
   sha256 '0ef84f2f2dddce3d0a4ab1c7578bf28d3ecd24d12e9554b7cac610974e4af447'
 
   # github.com/qvacua/vimr was verified as official when first introduced to the cask
-  url "https://github.com/qvacua/vimr/releases/download/snapshot%2f#{version}/VimR-#{version}.tar.bz2"
+  url "https://github.com/qvacua/vimr/releases/download/snapshot%2fv#{version}/VimR-#v{version}.tar.bz2"
   appcast 'https://raw.githubusercontent.com/qvacua/vimr/master/appcast.xml',
           checkpoint: 'ee806d0b9294d6a886dc659f951a250fe3d4bbfcb75d919a8cc7768d922dd1b4'
-  auto_updates true
   name 'VimR'
   homepage 'http://vimr.org/'
+
+  auto_updates true
 
   app 'VimR.app'
   binary "#{appdir}/VimR.app/Contents/Resources/vimr"
