1ce789af5973fcc458e4cbc0638fb7c6a5288747
Philip Deuchler <pbdeuchler@gmail.com>
2016-10-18 10:17:04 -0600

    Typo 😔

--- Casks/vimr.rb
+++ Casks/vimr.rb
@@ -3,7 +3,7 @@ cask 'vimr' do
   sha256 '0ef84f2f2dddce3d0a4ab1c7578bf28d3ecd24d12e9554b7cac610974e4af447'
 
   # github.com/qvacua/vimr was verified as official when first introduced to the cask
-  url "https://github.com/qvacua/vimr/releases/download/snapshot%2fv#{version}/VimR-#v{version}.tar.bz2"
+  url "https://github.com/qvacua/vimr/releases/download/snapshot%2fv#{version}/VimR-v#{version}.tar.bz2"
   appcast 'https://github.com/qvacua/vimr/releases.atom',
           checkpoint: 'ee806d0b9294d6a886dc659f951a250fe3d4bbfcb75d919a8cc7768d922dd1b4'
   name 'VimR'
