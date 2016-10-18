8d24378347524d5def5409e5037ac0c98c99ab43
asmsuechan <suenagaryoutaabc@gmail.com>
2016-10-18 23:00:33 +0900

    Updates boostnote to v0.7.2

--- Casks/boostnote.rb
+++ Casks/boostnote.rb
@@ -1,11 +1,11 @@
 cask 'boostnote' do
-  version '0.7.1'
-  sha256 'f8fd7e2ec544a7fbc8bb627a7349555c00ee800f7ed7a522c1bacdf7af31962f'
+  version '0.7.2'
+  sha256 '33cca3abefbbb38d9ae3a72411092a890535c634c0e8430d558edc621617ff57'
 
   # github.com/BoostIO/boost-releases was verified as official when first introduced to the cask
   url "https://github.com/BoostIO/boost-releases/releases/download/v#{version}/Boostnote-mac.dmg"
   appcast 'https://github.com/BoostIO/boost-releases/releases.atom',
-          checkpoint: '2aff9d48ba3431e3b0e22758a0720e873168709f7508bd4b9b7abb4f7bd62840'
+          checkpoint: '07df80006c9cfcf019787ce4629a4e0ac522c56845c5c4338a2a1958aa5de4f7'
   name 'Boostnote'
   homepage 'https://b00st.io'
 
