2caf5339ce47de85a701f814dbde54fc9c204d12
Howard Chen <hhchen@andrew.cmu.edu>
2016-10-18 19:18:53 -0400

    fix the download link

--- Casks/league-of-legends.rb
+++ Casks/league-of-legends.rb
@@ -3,7 +3,7 @@ cask 'league-of-legends' do
   sha256 :no_check
 
   # riotgames.com is the official download host per the vendor homepage
-  url 'http://l3cdn.riotgames.com/Installer/NA_Mac_Installer/League%20of%20Legends%20NA.dmg'
+  url 'https://riotgamespatcher-a.akamaihd.net/Mac_ShellInstaller/NA/League%20of%20Legends%20NA.dmg'
   name 'League of Legends'
   homepage 'http://leagueoflegends.com/'
   license :gratis
