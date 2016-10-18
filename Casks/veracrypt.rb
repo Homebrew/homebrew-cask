279f0881c44d55ae2bb2d9219ab0b2770ad65f94
Vítor Galvão <info@vitorgalvao.com>
2016-10-18 10:16:09 +0100

    Update veracrypt.rb

--- Casks/veracrypt.rb
+++ Casks/veracrypt.rb
@@ -6,7 +6,6 @@ cask 'veracrypt' do
   url 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=veracrypt&DownloadId=1614080&FileTime=131212015497000000&Build=21031'
   name 'VeraCrypt'
   homepage 'https://veracrypt.codeplex.com/'
-  license :oss
 
   depends_on cask: 'osxfuse'
 
