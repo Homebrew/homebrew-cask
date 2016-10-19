3fc2bfc7ee08c499bf088049751a7ec0d27a9907
Sihan Zhu <zshbleaker@gmail.com>
2016-10-19 12:36:14 +0800

    Update and rename baidunetdisk.rb to Caskroom/baidunetdisk.rb

--- Caskroom/baidunetdisk.rb
+++ Caskroom/baidunetdisk.rb
@@ -2,7 +2,7 @@ cask 'baidunetdisk' do
   version '2.0.0'
   sha256 '06a3477139a2c96fc8ceaa3a59c1a65dc0b57ec9a069f2f6356067778ae2014f'
 
-  # baidupcs.com is used to distribute static resources from Baidu
+  # baidupcs.com/issue/netdisk/MACguanjia was verified as official when first introduced to the cask
   url "http://issuecdn.baidupcs.com/issue/netdisk/MACguanjia/BaiduNetdisk_mac_#{version}.dmg"
   name 'Baidu NetDisk'
   name '百度网盘'
