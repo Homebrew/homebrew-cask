67271c44b207676369b2aab57078dc8956c5d382
Sihan Zhu <zshbleaker@gmail.com>
2016-10-19 10:53:02 +0800

    Update baiducloud-manager.rb

--- baiducloud-manager.rb
+++ baiducloud-manager.rb
@@ -1,6 +1,6 @@
 cask 'baiducloud-manager' do
   version '2.0.0'
-  sha256 :no_check
+  sha256 '06a3477139a2c96fc8ceaa3a59c1a65dc0b57ec9a069f2f6356067778ae2014f'
 
   url 'http://issuecdn.baidupcs.com/issue/netdisk/MACguanjia/BaiduNetdisk_mac_2.0.0.dmg'
   name '百度网盘'
