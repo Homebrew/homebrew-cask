565e2e3423f65b8676f7aafb6164f13300d62583
Leonardo Santos <leonardo.santos@unirio.br>
2016-10-20 13:08:41 -0200

    Correct url.

--- Casks/jaspersoft-studio.rb
+++ Casks/jaspersoft-studio.rb
@@ -3,7 +3,7 @@ cask 'jaspersoft-studio' do
   sha256 'e2c0e713d3c5b309e1944c3d472f9dcea9e29d4c0ba439942f377ba9786fcd6a'
 
   # sourceforge.net/jasperstudio was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/jasperstudio/JaspersoftStudio-#{version}/TIBCOJaspersoftStudio-#{version}.final-mac-x86_64.dmg"
+  url "https://downloads.sourceforge.net/project/jasperstudio/JaspersoftStudio-#{version}/TIBCOJaspersoftStudio-#{version}.final-mac-x86_64.dmg"
   appcast 'https://sourceforge.net/projects/jasperstudio/rss',
           checkpoint: '8119c62e016755bd697e8becfc82f9eb342766a8e6d89895b02242588482faab'
   name 'Jaspersoft Studio'
