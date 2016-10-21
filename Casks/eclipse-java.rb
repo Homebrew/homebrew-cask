db42842a10325476f93690f7ed4158061e8ed388
Miccal Matthews <miccal.matthews@gmail.com>
2016-10-21 18:34:18 +0800

    Update eclipse-java to 4.6.1,neon:1a (#25993)

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-java' do
-  version '4.6.0'
-  sha256 'cc776030aac93ca313c2e7791f3368af399d595c6979e421ca7c36c180de3c96'
+  version '4.6.1,neon:1a'
+  sha256 'b58ca0f98a391bc0856e313ec33f30168dc17dec94012d9760f9f33060f22187'
 
-  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/R/eclipse-java-neon-R-macosx-cocoa-x86_64.tar.gz&r=1'
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-java-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse IDE for Java Developers'
   homepage 'https://eclipse.org/'
 
