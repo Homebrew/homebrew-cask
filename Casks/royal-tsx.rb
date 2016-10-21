961b1df3e1008e401cc77e51ffc64daf4311a719
oaoit <oaoist@gmail.com>
2016-10-21 17:06:23 +0800

    update royal-tsx to 2.2.9.1000

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -1,6 +1,6 @@
 cask 'royal-tsx' do
-  version '2.2.6.1000'
-  sha256 '31b6c0b325076e0fdf67d5d2e684bf789bf73396fd53d4b51feb61cec22a2b7b'
+  version '2.2.9.1000'
+  sha256 '7fccf594f56fd2824efef0bc933669ad3ab2f3788224450a65fd2eb493423bf9'
 
   url "http://v2.royaltsx.com/updates/royaltsx_#{version}.dmg"
   appcast "https://royaltsx-v#{version.major}.royalapplications.com/updates_stable.php",
