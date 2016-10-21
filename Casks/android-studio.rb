c6687b35af65ae8dfe4bdcbc6a8424e82645ad68
Lingchao Xin <douglarek@users.noreply.github.com>
2016-10-21 08:49:07 +0800

    Update android-studio to 2.2.2.0,145.3360264 (#25981)

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,6 +1,6 @@
 cask 'android-studio' do
-  version '2.2.1.0,145.3330264'
-  sha256 '50f39bc08cdc3ce425828f7c50175dfe71e9d4cd07bcd44fde68107b3f694416'
+  version '2.2.2.0,145.3360264'
+  sha256 'c882c96643860a20ff1c4ced0e11ca72e613c9c2b9450a312c56f7c2678730b9'
 
   # google.com/dl/android/studio was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
