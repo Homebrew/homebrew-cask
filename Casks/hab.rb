3572382f08727f8097edb3ba3c08db6df102dcee
Mike Fiedler <miketheman@gmail.com>
2016-10-21 07:12:05 -0400

    Update hab to 0.11.0-20161013184001
    #### Checklist
    
    - [x] The commit message includes the cask’s name and version.
    - [x] `brew cask audit --download {{cask_file}}` is error-free.
    - [x] `brew cask style --fix {{cask_file}}` left no offenses.

--- Casks/hab.rb
+++ Casks/hab.rb
@@ -1,6 +1,6 @@
 cask 'hab' do
-  version '0.10.2-20160930234832'
-  sha256 '1a819160da57d80d313378b56989b2b101eca00d95e9f8ec7aad2d5b419bab39'
+  version '0.11.0-20161013184001'
+  sha256 'aa3264d8e94d454b39389f17d3be6e6121853c6e5d885f49d6d71df70bd95714'
 
   # habitat.bintray.com was verified as official when first introduced to the cask
   url "https://habitat.bintray.com/stable/darwin/x86_64/hab-#{version}-x86_64-darwin.zip"
