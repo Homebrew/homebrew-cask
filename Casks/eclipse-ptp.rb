80cceaee2df55bc50e29d176c162c7b24d3411e0
Miccal Matthews <miccal.matthews@gmail.com>
2016-10-21 19:46:36 +0800

    Update eclipse-ptp to 4.6.1,neon:1a (#26002)

--- Casks/eclipse-ptp.rb
+++ Casks/eclipse-ptp.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-ptp' do
-  version '4.6.0'
-  sha256 '7eab1605dce524abb872185d184e8033b02e8a2c9fd23005a4c15224b4712803'
+  version '4.6.1,neon:1a'
+  sha256 '6bf6d6ceac99616a621f30b63ac720299d6264de439603b259148d7a2f61a311'
 
-  url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/R/eclipse-parallel-neon-R-macosx-cocoa-x86_64.tar.gz&r=1'
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-parallel-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse for Parallel Application Developers'
   homepage 'https://eclipse.org/'
 
