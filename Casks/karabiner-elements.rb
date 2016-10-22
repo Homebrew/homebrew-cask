cdb7476ccdd3100bfcd59d42fae50a9b68126ed3
Artem Medvedev <amedvedev@outlook.com>
2016-10-22 16:34:48 +0300

    Update karabiner-elements to 0.90.55 (#26022)

--- Casks/karabiner-elements.rb
+++ Casks/karabiner-elements.rb
@@ -1,10 +1,10 @@
 cask 'karabiner-elements' do
-  version '0.90.54'
-  sha256 '43c2103a6b1644faefdc134a3e8bee01319869549f2cab5aa1e1fa0432ccaf6a'
+  version '0.90.55'
+  sha256 '8c4161d4fae2d281968a098a41524042e59b5017c419ccfcf4b65ebcf57fddbf'
 
   url "https://pqrs.org/osx/karabiner/files/Karabiner-Elements-#{version}.dmg"
   appcast 'https://pqrs.org/osx/karabiner/files/karabiner-elements-appcast.xml',
-          checkpoint: 'b8cb74876f4a73d66141e333605565a076d67e9f1f194e36231f2c7ff795fef1'
+          checkpoint: 'a8396c5686a705a05819f379108ff772b662626c1ec59b18396a089799c41e85'
   name 'Karabiner Elements'
   homepage 'https://pqrs.org/osx/karabiner/'
 
