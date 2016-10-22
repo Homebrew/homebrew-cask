a33844cd0068a9678059c56c46c7e129509cb37a
Timmy Chang <timmy5519@gmail.com>
2016-10-23 01:11:18 +0800

    Update electron to v1.4.4 (#26025)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,11 +1,11 @@
 cask 'electron' do
-  version '1.4.3'
-  sha256 '7bbd9260e499dc8acdc7f6056693ba6785d54778166f1ead93d339ea58c6eeaf'
+  version '1.4.4'
+  sha256 '87382e242592f295dfd9d53fc6a7c6906960b3cde870c73ad3a6ae12b8e6b20b'
 
   # github.com/atom/electron was verified as official when first introduced to the cask
   url "https://github.com/atom/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/atom/electron/releases.atom',
-          checkpoint: '40af55bc4cc58b48ab71fd46d5f17d6b09ec2078df3a77137ee6ccc7432db487'
+          checkpoint: '9203e03fb46b83e867f27c93da6013fdc4fd4f7fe071f4f6e314004edac6b906'
   name 'Electron'
   homepage 'http://electron.atom.io/'
 
