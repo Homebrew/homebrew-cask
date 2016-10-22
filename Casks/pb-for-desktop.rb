86db58980e202803ddc92739ed0d9378da31ca99
sidneys <sidneys.github.io@outlook.com>
2016-10-22 22:19:21 +0200

    Remove appcast

--- Casks/pb-for-desktop.rb
+++ Casks/pb-for-desktop.rb
@@ -3,8 +3,6 @@ cask 'pb-for-desktop' do
   sha256 'c28d023d1be83a04155f1c098c97c25777ce21ec740cd7807a96e76780081e42'
 
   url "https://github.com/sidneys/pb-for-desktop/releases/download/v#{version}/pb-for-desktop-v#{version}-darwin-x64.zip"
-  appcast 'https://github.com/sidneys/pb-for-desktop/releases.atom',
-          checkpoint: 'cc9b404807059f60c4c0f295d9caed3849813223ea927e635871ca0021fae3e7'
   name 'PB for Desktop'
   homepage 'https://github.com/sidneys/pb-for-desktop'
 
