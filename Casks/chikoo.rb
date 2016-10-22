5b4bc976b67c66017147bde693014a59333d321b
Rezart Qelibari <rezart.q+github@gmail.com>
2016-10-22 17:41:09 +0200

    Supply version, checksum and appcast stanza for Chikoo.app v.1.1
    Supply version stanza with a version of the app and therfor also
    the sha sum. Additionally @vitorgalvao pointed out, that there
    is a appcast url available => Add appcast stanza with checkpoint
    argument.

--- Casks/chikoo.rb
+++ Casks/chikoo.rb
@@ -1,10 +1,12 @@
 cask 'chikoo' do
-  version :latest
-  sha256 :no_check
+  version '1.1'
+  sha256 '05efeaf4bc2d00f5fc83df4d17a226d205bfe8cf895111a748a1ecb87fb686ff'
 
-  url 'https://codingturtle.com/chikoo/download'
+  url 'https://download.codingturtle.com/download-files/chikoo/chikoo-#{version}.zip'
   name 'Chikoo'
   homepage 'https://codingturtle.com/chikoo'
 
+  appcast 'https://download.codingturtle.com/appcast/chikoo/11000',
+          checkpoint: 'af050d033356ae59b5bfc002faeb289d7dc6632f6bd8ac6623c5ecd9af2ec6f8'
   app 'Chikoo.app'
 end
