8011d0285d0cbd79a9ed25ef8e08c0762b74717e
Aditya Dalal <adidalal@users.noreply.github.com>
2016-10-22 16:54:18 -0400

    Fix merge issues

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -13,7 +13,24 @@ header 'Running before_script.sh...'
 # work around travis xcode bug
 # https://github.com/direnv/direnv/issues/210
 run rvm get head
- 
+
+# capture system ruby and gem locations
+run export SYSTEM_RUBY_HOME="/System/Library/Frameworks/Ruby.framework/Versions/Current"
+run export SYSTEM_RUBY_BINDIR="${SYSTEM_RUBY_HOME}/usr/bin"
+run export SYSTEM_GEM_HOME="$(gem_homes="${SYSTEM_RUBY_HOME}/usr/lib/ruby/gems/"*; echo ${gem_homes[${#gem_homes[@]}-1]})"
+run export SYSTEM_GEM_BINDIR="${SYSTEM_GEM_HOME}/bin"
+
+# capture user gem locations
+run export GEM_HOME="${HOME}/.gem/ruby/${SYSTEM_GEM_HOME##*/}"
+run export GEM_BINDIR="${GEM_HOME}/bin"
+
+# ensure that the gems we install are used before system gems
+run export GEM_PATH="${GEM_HOME}:${SYSTEM_GEM_HOME}"
+run export PATH="${GEM_BINDIR}:${SYSTEM_GEM_BINDIR}:${SYSTEM_RUBY_BINDIR}:${PATH}"
+
+# ensure that brew uses the ruby we want it to
+run export HOMEBREW_RUBY_PATH="${SYSTEM_RUBY_BINDIR}/ruby"
+
 # update homebrew
 run brew update
 
