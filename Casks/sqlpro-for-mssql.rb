655f724258547798572809b3371124aad7b67183
Aditya Dalal <adidalal@users.noreply.github.com>
2016-10-22 15:58:05 -0400

    Testing that audit still works

--- Casks/sqlpro-for-mssql.rb
+++ Casks/sqlpro-for-mssql.rb
@@ -1,5 +1,5 @@
 cask 'sqlpro-for-mssql' do
-  version '1.0.110'
+  version '1.0.113'
   sha256 '5d7d7dd3a3472161f766316db262a8c3f245f9c0f6082cd4a86d382796b337d9'
 
   # d3fwkemdw8spx3.cloudfront.net/mssql was verified as official when first introduced to the cask
