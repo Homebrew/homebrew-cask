f0ded9a6477ee49977022bfa420f71311092f69c
Artem Medvedev <amedvedev@outlook.com>
2016-10-22 07:04:28 +0300

    Fixed url

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -2,7 +2,7 @@ cask 'diskmaker-x' do
   version '6rc3'
   sha256 '762681fff8576a345f2138df17bcb071565171e49de1c5b59e0abc0c8ccbc7d5'
 
-  url "http://diskmakerx.com/downloads/DiskMaker_X_#{version.no_dots}.dmg"
+  url "http://diskmakerx.com/downloads/DiskMaker_X_#{version}.dmg"
   appcast 'http://diskmakerx.com/feed/',
           checkpoint: '972a26f7e1c3d53eea31ed54d7572cedfae435af19d5010ccc0f54d9d072dd44'
   name 'DiskMaker X'
