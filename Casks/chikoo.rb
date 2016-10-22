6413c5bf5a179c575d511719b38c743f894ae23b
Rezart Qelibari <rezart.q+github@gmail.com>
2016-10-22 16:37:52 +0200

    Add Chikoo.app Latest
    Add a cask for Chikoo app, a document/library management app.

---
+++ Casks/chikoo.rb
@@ -0,0 +1,10 @@
+cask 'chikoo' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://codingturtle.com/chikoo/download'
+  name 'Chikoo'
+  homepage 'https://codingturtle.com/chikoo'
+
+  app 'Chikoo.app'
+end
