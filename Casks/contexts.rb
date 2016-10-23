24c3b10a9721d1fe7cccbd01d840bd10459f8650
Colin Dunn <cgd@vikingdev.net>
2016-10-23 10:48:43 +0100

    Update contexts to 2.6.1 (#26043)

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -1,10 +1,10 @@
 cask 'contexts' do
-  version '2.5'
-  sha256 '08df69cd895314325cfc69dcea9f1f17b62d6cb212a62d02132eccea56bfdd88'
+  version '2.6.1'
+  sha256 'aa4051ec2a2e49e23c1dc621afb23938166063ccb230e4c24d996f53ecdc5f73'
 
   url "https://contexts.co/releases/Contexts-#{version}.zip"
   appcast 'https://contexts.co/appcasts/stable.xml',
-          checkpoint: 'a2449460fca0cf5f1259c4415d681ecdb2945329d9e90a517716fece4c5a05b4'
+          checkpoint: 'be56bd036b79f7d3b7d05685f151db835e51802547695bf78e639ed899f0ef89'
   name 'Contexts'
   homepage 'https://contexts.co'
 
