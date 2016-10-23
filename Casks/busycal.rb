e9b3186ad623b95928c091547c6135a9823a36e5
Josh Hagins <hagins.josh@gmail.com>
2016-10-23 11:59:24 -0400

    busycal: update uninstall and add zap

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -6,7 +6,21 @@ cask 'busycal' do
   name 'BusyCal'
   homepage 'https://busymac.com/busycal/index.html'
 
+  auto_updates true
+
   pkg 'BusyCal Installer.pkg'
 
-  uninstall pkgutil: 'com.busymac.busycal2.pkg'
+  uninstall pkgutil: 'com.busymac.busycal3.pkg',
+            quit:    [
+                       'com.busymac.busycal3',
+                       'N4RA379GBW.com.busymac.busycal3.alarm',
+                     ]
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.busymac.busycal3.sfl',
+                '~/Library/Containers/com.busymac.busycal3',
+                '~/Library/Containers/N4RA379GBW.com.busymac.busycal3.alarm',
+                '~/Library/Group Containers/com.busymac.busycal3',
+                '~/Library/Group Containers/N4RA379GBW.com.busymac.busycal3',
+              ]
 end
