b810eca2843b89a627b093c38afecf0c175a3595
वेणु गोपाल <vg@users.noreply.github.com>
2016-10-23 13:15:45 -0400

    Rename techstoreclub-simple-comic.rb to simple-comic.rb
    Reason: Previous Cask name was a fork of the original app

---
+++ Casks/simple-comic.rb
@@ -0,0 +1,12 @@
+cask 'simple-comic' do
+  version '1.7_252'
+  sha256 '4ddd18a02a79fc8201824e6ab99291c6d4c8680f79f94bc372bf71f0535def35'
+
+  url "https://github.com/dancingtortoise/Simple-Comic/releases/download/#{version}/SimpleComic_#{version}.zip"
+  appcast 'https://github.com/dancingtortoise/Simple-Comic/releases.atom',
+          checkpoint: '3bb3b59bebce4cb396c2f0debeb04510261b6d9e0d0a071bef6de9a8d076fdb5'
+  name 'Simple Comic'
+  homepage 'https://dancingtortoise.github.io/'
+
+  app 'Simple Comic.app'
+end
