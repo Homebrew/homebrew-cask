d68c61a9a942c04152612794d61d7ef622d02400
Josh Hagins <hagins.josh@gmail.com>
2016-10-23 13:46:22 -0400

    imagej, tinyumbrella: use depends_on_java caveat (#26050)

--- Casks/imagej.rb
+++ Casks/imagej.rb
@@ -6,8 +6,11 @@ cask 'imagej' do
   name 'ImageJ'
   homepage 'https://imagej.nih.gov/ij/index.html'
 
-  depends_on cask: 'java'
   depends_on macos: '>= :mountain_lion'
 
   suite 'ImageJ'
+
+  caveats do
+    depends_on_java
+  end
 end
