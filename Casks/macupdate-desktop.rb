2ab95e265c651f05d5e564a21ce2f86386cec0d1
Aditya Dalal <adidalal@users.noreply.github.com>
2016-10-22 21:08:56 -0400

    Update macupdate-desktop to 6.1.3 (#26037)

--- Casks/macupdate-desktop.rb
+++ Casks/macupdate-desktop.rb
@@ -2,7 +2,7 @@ cask 'macupdate-desktop' do
   version '6.1.3'
   sha256 'c1dac184d4b221981247fb173abc8610a00e0c0a57d7928708f8bdf709e46832'
 
-  url "https://cdn.macupdate.com/MacUpdateDesktop#{version}.zip"
+  url "http://cdn.macupdate.com/MacUpdateDesktop#{version}.zip"
   appcast 'https://www.macupdate.com/desktop/updates.xml',
           checkpoint: '81693bee3a3b6917465b714432f13730e767b2781eff2cab18a21ba46475187c'
   name 'MacUpdate Desktop'
