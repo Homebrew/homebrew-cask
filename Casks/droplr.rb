b081850ae17493bb558b0c013d6a7ec741e44504
Serge Bibauw <sbibauw@gmail.com>
2016-10-24 17:28:48 +0200

    Updated Droplr to 4.6.4 (Build 91)
    Previous link (http://droplr.com/homebrew) links currently to an error

--- Casks/droplr.rb
+++ Casks/droplr.rb
@@ -1,8 +1,8 @@
 cask 'droplr' do
-  version :latest
-  sha256 :no_check
+  version '4.6.4'
+  sha256 '41064cec1922fa76b15e940a747e3d12e720588fa3ab9a527b38ba3a683c531b'
 
-  url 'https://droplr.com/homebrew'
+  url 'https://s3.amazonaws.com/files.droplr.com/apps/mac/Droplr+91.zip'
   name 'Droplr'
   homepage 'https://droplr.com/'
 
