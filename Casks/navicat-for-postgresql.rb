a6f5ea80af72bdef1374bb04886cf20f79909bc4
Vítor Galvão <info@vitorgalvao.com>
2016-10-25 17:03:14 +0100

    Update navicat-for-postgresql to 11.2.15

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-postgresql' do
-  version '11.2.14'
-  sha256 '98fb9715f540ffb77bfe8de54cf99480b88434dd0ef077958b2be0ace5988c13'
+  version '11.2.15'
+  sha256 '36433d22ddfc9e463abe7b72ac4fb5a2e61e4e77c4a033e9f0365a8ca1ef6b5e'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
   name 'Navicat for PostgreSQL'
