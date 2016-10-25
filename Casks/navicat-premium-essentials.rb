eee558dc60649406012774a357b8b6eb62674f7b
Vítor Galvão <info@vitorgalvao.com>
2016-10-25 17:05:29 +0100

    Update navicat-premium-essentials to 11.2.15 (#26108)

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium-essentials' do
-  version '11.2.13'
-  sha256 'ec738c5a01da5f69a24bc9c72adc08e15b746c2a3c3122035f374f9bf33b139b'
+  version '11.2.15'
+  sha256 'f1b3ae67e28867740591635885f6562fc44d5f9be0242d4e813af452e3b4c381'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
   name 'Navicat Premium Essentials'
