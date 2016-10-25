f4a8eb2fc2f83205c9612b1144b25108a1f3dd2e
Vítor Galvão <info@vitorgalvao.com>
2016-10-25 12:30:48 +0100

    Update droplr.rb

--- Casks/droplr.rb
+++ Casks/droplr.rb
@@ -1,6 +1,6 @@
 cask 'droplr' do
-  version "4.6.4,91"
-  sha256 "41064cec1922fa76b15e940a747e3d12e720588fa3ab9a527b38ba3a683c531b"
+  version '4.6.4,91'
+  sha256 '41064cec1922fa76b15e940a747e3d12e720588fa3ab9a527b38ba3a683c531b'
 
   # files.droplr.com.s3.amazonaws.com was verified as official when first introduced to the cask
   url "http://files.droplr.com.s3.amazonaws.com/apps/mac/Droplr+#{version.after_comma}.zip"
