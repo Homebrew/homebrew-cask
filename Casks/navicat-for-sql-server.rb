a277ebde608dc4ab65bf15462003dc7cf46a3bb5
Vítor Galvão <info@vitorgalvao.com>
2016-10-25 17:03:51 +0100

    Update navicat-for-sql-server to 11.2.15

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-sql-server' do
-  version '11.2.14'
-  sha256 'c60d1d493429b80cc380ff7ad880ed707fb005d428d46739de019b0085de468a'
+  version '11.2.15'
+  sha256 '45a967a43ba4f95a17a80d8fff2579bc17c81e17bb992db4055f7009b9575e6b'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
   name 'Navicat for SQL Server'
