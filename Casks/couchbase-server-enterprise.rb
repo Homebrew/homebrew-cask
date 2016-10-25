5898fe84cc515c814e3527c94276239023c7a77b
Eddie Gurnee <pegurnee@users.noreply.github.com>
2016-10-25 08:50:35 -0400

    Update couchbase-server-enterprise to 4.5.1 (#26093)

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -1,6 +1,6 @@
 cask 'couchbase-server-enterprise' do
-  version '4.1.0'
-  sha256 '237837598a1bf663debaea5c909bce36a39fbc673139cf4d4ebc55fb5c276313'
+  version '4.5.1'
+  sha256 'de014c7c134eb97ff00be6b2e6f5d0da84295ce05bbb7bb3a4d3c747a365cd22'
 
   url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
   name 'Couchbase Server'
