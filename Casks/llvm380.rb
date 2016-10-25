44c7b9e93a010e39ba7d28b67e2f4813f88e55c3
Francois BERENGER <francois.berenger@inria.fr>
2016-10-25 13:42:30 +0200

    added Casks/llvm380.rb

---
+++ Casks/llvm380.rb
@@ -0,0 +1,8 @@
+cask 'llvm380' do
+  version '3.8.0'
+  sha256 'e5a961e04b0e1738bbb5b824886a34932dc13b0af699d1fe16519d814d7b776f'
+
+  url 'http://llvm.org/releases/3.8.0/clang+llvm-3.8.0-x86_64-apple-darwin.tar.xz'
+  name 'clang+llvm-3.8.0-x86_64'
+  homepage 'http://llvm.org/releases/download.html#3.8.0'
+end
