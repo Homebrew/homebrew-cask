1e8e52c95c4baa8b79458e5a4ecb0371c9fdbabe
Vítor Galvão <info@vitorgalvao.com>
2016-10-25 17:00:31 +0100

    Update navicat-for-mariadb to 11.2.15

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-mariadb' do
-  version '11.2.14'
-  sha256 '73f2b864cafbbeed2430d61b0463fdd9e68996215a72e1d900663a9922793803'
+  version '11.2.15'
+  sha256 '4d05fb39a67ab1a7f910e6a1215e1bcb29b0e54c834ed8a409a9e033858160cc'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
   name 'Navicat for MariaDB'
