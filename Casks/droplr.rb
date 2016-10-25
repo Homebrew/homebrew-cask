9fc4a1d281e822b10a97ac065a4e2d0ed58922e0
Serge Bibauw <sbibauw@gmail.com>
2016-10-25 13:21:17 +0200

    Update Droplr to 4.6.4,91
    URL provided in Droplr Auto-updater

--- Casks/droplr.rb
+++ Casks/droplr.rb
@@ -1,10 +1,11 @@
 cask 'droplr' do
-  version '4.6.4'
-  sha256 '41064cec1922fa76b15e940a747e3d12e720588fa3ab9a527b38ba3a683c531b'
+  version "4.6.4,91"
+  sha256 "41064cec1922fa76b15e940a747e3d12e720588fa3ab9a527b38ba3a683c531b"
 
-  url 'https://s3.amazonaws.com/files.droplr.com/apps/mac/Droplr+91.zip'
-  name 'Droplr'
-  homepage 'https://droplr.com/'
+  # files.droplr.com.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "http://files.droplr.com.s3.amazonaws.com/apps/mac/Droplr+#{version.after_comma}.zip"
+  name "Droplr"
+  homepage "https://droplr.com/"
 
   auto_updates true
 
