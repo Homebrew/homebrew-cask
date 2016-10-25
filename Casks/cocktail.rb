f69921e9c9dc018f10d85ccd03347238bd097114
Bo Link <knilob@gmail.com>
2016-10-25 10:57:14 -0500

    Update Cocktail.app for MacOS Sierra to v10.1 (#26099)

--- Casks/cocktail.rb
+++ Casks/cocktail.rb
@@ -42,12 +42,12 @@ cask 'cocktail' do
     appcast 'https://www.maintain.se/downloads/sparkle/elcapitan/elcapitan.xml',
             checkpoint: 'a4d0e1719641e583255e63fc6d20aea2d910d77cdf9cb51ff5402d9a519109a1'
   else
-    version '10.0.1'
-    sha256 'ea90cbbfe33ddfe8c789ce6bde1a23d8edc11ae305a7a57e40fa79543b73abdf'
+    version '10.1'
+    sha256 'c87671e42f20646a981e69c8c5b8b502aee808367fe85f6a5709da129ed8eba6'
 
     url "https://www.maintain.se/downloads/sparkle/sierra/Cocktail_#{version}.zip"
     appcast 'https://www.maintain.se/downloads/sparkle/sierra/sierra.xml',
-            checkpoint: '2ae2b8f19373912c47436bd4bdf89891c95739c90c299b8a414fa3c768ba21cc'
+            checkpoint: 'a3629f9c689818cf031fa873880eaff56a8a3a10dc70e60f59e6d67b81e993ae'
   end
 
   name 'Cocktail'
