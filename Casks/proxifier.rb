909e543ddba75ece066372d7584adc7b1565fe35
nuomi1 <nuomi1@qq.com>
2016-10-26 18:34:42 +0800

    Update proxifier.rb for appcast and zap (#26122)

--- Casks/proxifier.rb
+++ Casks/proxifier.rb
@@ -1,10 +1,19 @@
 cask 'proxifier' do
-  version :latest
-  sha256 :no_check
+  version '2.16'
+  sha256 'de592e28198deb22edc522c7ef6785d1a83b5f21e683c0dc6b7ab838ebda50c7'
 
   url 'https://www.proxifier.com/distr/ProxifierMac.zip'
+  appcast 'https://www.proxifier.com/distr/last_versions/ProxifierMac.txt',
+          checkpoint: '5c316b2043de3d51392e3b60fc0894167c9b2c8abb5d1fe91ccf9d9c0a2056c9'
   name 'Proxifier'
   homepage 'https://www.proxifier.com/mac/'
 
   app 'Proxifier.app'
+
+  zap delete: [
+                '~/Library/Application Support/Proxifier',
+                '~/Library/Caches/com.initex.proxifier.macosx',
+                '~/Library/Preferences/com.initex.proxifier.macosx.plist',
+                '~/Library/Saved Application State/com.initex.proxifier.macosx.savedState',
+              ]
 end
