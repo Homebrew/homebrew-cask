f1720183b42bca7f6ee22ffddb02f3208dd7b019
Matt Tagg <wamatt@gmail.com>
2016-10-25 22:58:45 -0700

    Update fs-uae to 2.8.0 (#26121)

--- Casks/fs-uae.rb
+++ Casks/fs-uae.rb
@@ -1,8 +1,8 @@
 cask 'fs-uae' do
-  version '2.6.2'
-  sha256 '1c4e285ac82828d004c900e05b29dc6363eeb5f705524bfaa2bd4e6f57101ace'
+  version '2.8.0'
+  sha256 '4988967ea61701dc0c2025a1d3765254142cecdd35ea84cc8a8a59d9198e99b0'
 
-  url "https://fs-uae.net/stable/#{version}/fs-uae-suite_#{version}_macosx.tar.gz"
+  url "https://fs-uae.net/stable/#{version}/fs-uae-suite_#{version}_macosx_x86-64.tar.gz"
   name 'FS-UAE'
   homepage 'https://fs-uae.net/'
 
