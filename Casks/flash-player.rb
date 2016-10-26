d473b7cb7deb97edab7ce1156d6b5a006290ceef
Andy Li <andy@onthewings.net>
2016-10-26 17:19:04 +0800

    flash-player, flash-npapi, flash-ppapi 23.0.0.205 (#26125)

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,11 +1,11 @@
 cask 'flash-player' do
-  version '23.0.0.185'
-  sha256 'f1a7b29230bee1a8125d005d69becc9d6a63e7192291c5478b1f1f5e68d3e9b0'
+  version '23.0.0.205'
+  sha256 '446bce8361362e87bdd9f3d4befb76a5a865c236412e8b755481962540077cdb'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
-          checkpoint: '159582e9a7da281f6d866c000f4f2803c7306e89be44075e6793c142286077e5'
+          checkpoint: '26a40ba2c668486155b9a3bce042a5105d158ddc8a0266f13614e264727ec5c3'
   name 'Adobe Flash Player projector'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
