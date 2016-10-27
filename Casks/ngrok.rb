6966643fa2a402bc67dc14b1da96115c7f9eaee5
Aditya Dalal <adidalal@users.noreply.github.com>
2016-10-26 21:57:11 -0400

    Update ngrok to 2.1.18,4VmDzA7iaHb (#26144)

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -1,5 +1,5 @@
 cask 'ngrok' do
-  version '2.1.14,4VmDzA7iaHb'
+  version '2.1.18,4VmDzA7iaHb'
   sha256 '379763f38394903d59c49842034e89861c641ce8449d97fa4317e1433b6c0a79'
 
   # bin.equinox.io was verified as official when first introduced to the cask
