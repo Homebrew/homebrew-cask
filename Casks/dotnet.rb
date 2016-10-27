74ad547e785601ead94b95b3f55f1af8432e4fc8
Romain Lespinasse <romain.lespinasse@gmail.com>
2016-10-27 03:50:49 +0200

    fix(dotnet): use the right internal version during postflight (#26133)
    * fix(dotnet): use the right internal version during postflight
    
    version 1.0.0-preview2-003148 use /usr/local/share/dotnet/shared/Microsoft.NETCore.App/1.0.2
    
    * Update dotnet.rb

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -1,19 +1,19 @@
 cask 'dotnet' do
-  version '1.0.0-preview2-003148'
+  version '1.0.0-preview2-003148,1.0.2'
   sha256 '1209b0c7c07f46cc402fd087f1135b780f298daafe2e690b9251f1eea3bc93a7'
 
-  url "https://download.microsoft.com/download/1/0/C/10C868F3-EF61-47A7-95CF-FF2AE042D65F/dotnet-dev-osx-x64.#{version}.pkg"
+  url "https://download.microsoft.com/download/1/0/C/10C868F3-EF61-47A7-95CF-FF2AE042D65F/dotnet-dev-osx-x64.#{version.before_comma}.pkg"
   name '.Net Core'
   homepage 'https://www.microsoft.com/net/core#macos'
 
   depends_on formula: 'openssl'
 
-  pkg "dotnet-dev-osx-x64.#{version}.pkg"
+  pkg "dotnet-dev-osx-x64.#{version.before_comma}.pkg"
 
   # Patch .NET Core to use the latest version of OpenSSL installed via Homebrew.
   # https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md#openssl
   postflight do
-    dotnet_core = '/usr/local/share/dotnet/shared/Microsoft.NETCore.App/1.0.1'
+    dotnet_core = "/usr/local/share/dotnet/shared/Microsoft.NETCore.App/#{version.after_comma}"
     system '/usr/bin/sudo', '-E', '--', '/usr/bin/install_name_tool', "#{dotnet_core}/System.Security.Cryptography.Native.dylib", '-add_rpath', "#{HOMEBREW_PREFIX}/opt/openssl/lib"
     system '/usr/bin/sudo', '-E', '--', '/usr/bin/install_name_tool', "#{dotnet_core}/System.Net.Http.Native.dylib", '-change', '/usr/lib/libcurl.4.dylib', "#{HOMEBREW_PREFIX}/opt/curl/lib/libcurl.4.dylib"
   end
