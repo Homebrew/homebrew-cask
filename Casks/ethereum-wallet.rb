e38d5c6bec0ad6f599168ee65db56800400bb093
Reed Riley <john.reed.riley@gmail.com>
2016-10-27 13:20:19 -0400

    Update ethereum-wallet to 0.8.7 (#26162)

--- Casks/ethereum-wallet.rb
+++ Casks/ethereum-wallet.rb
@@ -1,10 +1,10 @@
 cask 'ethereum-wallet' do
-  version '0.8.6'
-  sha256 '1b72cbeeb439517e7e20fabf9c39bb3263cca78c0074b11a76ea0a0586a51d30'
+  version '0.8.7'
+  sha256 'f9c89ba595ba6db9b976e1747c5355f01608dc09a44409fc1cd2c76b0660852c'
 
   url "https://github.com/ethereum/mist/releases/download/v#{version}/Ethereum-Wallet-macosx-#{version.dots_to_hyphens}.dmg"
   appcast 'https://github.com/ethereum/mist/releases.atom',
-          checkpoint: '4377f696a120458187946696684e3a39cac3e1ca9931069d36299c52f87e998e'
+          checkpoint: '4beac8a0d92cb0a142cb33444d28bc48d5634830725ccd95b9eb16820ca11548'
   name 'Ethereum Wallet'
   homepage 'https://github.com/ethereum/mist'
 
