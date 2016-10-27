40a3c680574aca7ef3cf839c649923e57f6be340
chaucerling <chaucerling@users.noreply.github.com>
2016-10-27 15:34:57 +0800

    Add appcast to spechtlite.rb

--- Casks/spechtlite.rb
+++ Casks/spechtlite.rb
@@ -3,6 +3,8 @@ cask 'spechtlite' do
   sha256 '9b302e73adab8ae39e51fee0b16803fee67c92add0d6b50b676a5fce50bfc527'
 
   url "https://github.com/zhuhaow/SpechtLite/releases/download/#{version}/SpechtLite.zip"
+  appcast 'https://github.com/zhuhaow/SpechtLite/releases.atom',
+          checkpoint: '02b227695791677acf7cd531eba12bc2514c7327cda9044aa9e07dcf0124541a'
   name 'SpechtLite'
   homepage 'https://github.com/zhuhaow/SpechtLite'
 
