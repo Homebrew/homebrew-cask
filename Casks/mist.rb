c73ef5c15c37cd2514976a1e161bfb44c1312c58
Reed Riley <john.reed.riley@gmail.com>
2016-10-27 13:20:27 -0400

    Update mist to 0.8.7 (#26161)

--- Casks/mist.rb
+++ Casks/mist.rb
@@ -1,10 +1,10 @@
 cask 'mist' do
-  version '0.8.6'
-  sha256 '3a66606aad465d8295e29e9711f5e76630c36bfd6064966ed6b7b4c194320acd'
+  version '0.8.7'
+  sha256 'd8f5a442292b7a5faf4423a765fd34d4f08913ed59950c12c0c0f4447f64c278'
 
   url "https://github.com/ethereum/mist/releases/download/v#{version}/Mist-macosx-#{version.dots_to_hyphens}.dmg"
   appcast 'https://github.com/ethereum/mist/releases.atom',
-          checkpoint: '4377f696a120458187946696684e3a39cac3e1ca9931069d36299c52f87e998e'
+          checkpoint: '4beac8a0d92cb0a142cb33444d28bc48d5634830725ccd95b9eb16820ca11548'
   name 'Mist'
   homepage 'https://github.com/ethereum/mist'
 
