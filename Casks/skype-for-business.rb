256d293e6df86df42a8bc8d76dcce60424a4a76d
Vítor Galvão <info@vitorgalvao.com>
2016-10-28 12:09:17 +0100

    Update skype-for-business.rb

--- Casks/skype-for-business.rb
+++ Casks/skype-for-business.rb
@@ -3,13 +3,13 @@ cask 'skype-for-business' do
   sha256 '57236471924a67973e66fbe4e2e53e86cfe5e054318950d183a4e0e4df68a2be'
 
   url "https://download.microsoft.com/download/D/0/5/D055DA17-C7B8-4257-89A1-78E7BBE3833F/SkypeForBusinessInstaller-#{version}.pkg"
-  name 'Skype for Business on Mac'
+  name 'Skype for Business'
   homepage 'https://www.microsoft.com/en-us/download/details.aspx?id=54108'
 
   pkg 'SkypeForBusinessInstaller-#{version}.pkg'
 
-  uninstall pkgutil:  'com.microsoft.SkypeForBusiness'
-  uninstall login_item: 'Skype for Business'
+  uninstall pkgutil:  'com.microsoft.SkypeForBusiness',
+            login_item: 'Skype for Business'
 
   zap delete: [
                 '/Library/Internet Plug-Ins/MeetingJoinPlugin.plugin',
