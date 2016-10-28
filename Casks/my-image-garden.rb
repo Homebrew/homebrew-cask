ad27c974aa341d1e46fcf73838a9630a8465dab5
Vítor Galvão <info@vitorgalvao.com>
2016-10-28 17:24:49 +0100

    Update my-image-garden.rb

--- Casks/my-image-garden.rb
+++ Casks/my-image-garden.rb
@@ -1,13 +1,13 @@
 cask 'my-image-garden' do
-  version '3.5.1,ea11'
+  version '3.5.1'
   sha256 '03c4f7a4ea0e39063f2f51fc51826ea53a60159cd0f0542d37a45367dddc8131'
 
-  # gdlp01.c-wss.com was verified as official when first introduced to the cask
-  url "http://gdlp01.c-wss.com/gds/3/0200004873/03/mmig-mac-#{version.before_comma.dots_to_underscores}-#{version.after_comma}.dmg"
+  # c-wss.com was verified as official when first introduced to the cask
+  url "http://gdlp01.c-wss.com/gds/3/0200004873/03/mmig-mac-#{version.dots_to_underscores}-ea11.dmg"
   name 'Canon My Image Garden'
   homepage 'http://support-asia.canon-asia.com/?personal'
 
-  pkg "My Image Garden V#{version.before_comma.no_dots}.pkg"
+  pkg "My Image Garden V#{version.no_dots}.pkg"
 
   uninstall pkgutil: 'jp.co.canon.MyImageGarden',
             quit:    'jp.co.canon.MyImageGarden'
