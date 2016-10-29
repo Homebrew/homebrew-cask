ce70260886061364626fee5158a3e18354bfa52f
Artem Medvedev <amedvedev@outlook.com>
2016-10-29 05:05:31 +0300

    Update bee to 2.4.4 (#26181)

--- Casks/bee.rb
+++ Casks/bee.rb
@@ -1,11 +1,11 @@
 cask 'bee' do
-  version '2.4.3'
-  sha256 '58235cb6e1e99cd04db936f4109eeea8ba7bdc9b35c482edff9d26036d8eed0c'
+  version '2.4.4'
+  sha256 'a6dcb6f1915903eb518f1011b4fa6297fc1ff318a3f22a481ebe398cf168720e'
 
   # bee-app.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://bee-app.s3.amazonaws.com/public/Bee-#{version}.zip"
   appcast 'https://s3.amazonaws.com/www.neat.io/appcasts/bee-appcast.xml',
-          checkpoint: 'dfc51f2e7bb64c9f8bc3766ae325fad6be7fe7d72319aef1befe3278c55d6c84'
+          checkpoint: '24d4f97bafc024fc43623d37ac49e69f1ee2db89dbae3c2fef10c39a3e6275dc'
   name 'Bee'
   homepage 'http://neat.io/bee/'
 
