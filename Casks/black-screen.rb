04bde8a68bc88e2cf749ccca7dafff0cfcc441a9
Bartek Igielski <igloczek@gmail.com>
2016-10-29 15:53:32 +0200

    Update black-screen to 0.2.71 (#26195)

--- Casks/black-screen.rb
+++ Casks/black-screen.rb
@@ -1,10 +1,10 @@
 cask 'black-screen' do
-  version '0.2.66'
-  sha256 '51843c889baf89adbfe8f812a62260a6eb45a46283c99719ac939d2d40bdd0a4'
+  version '0.2.71'
+  sha256 'da11aad8859d0206a7ed4b19a5415004a80787e59b627e3e1af5d65bb46449e1'
 
   url "https://github.com/shockone/black-screen/releases/download/v#{version}/black-screen-#{version}-mac.zip"
   appcast 'https://github.com/shockone/black-screen/releases.atom',
-          checkpoint: '27828c8ef817f5375e341c56c2ac3ed331553b963968f2b52c8f7e2dcb8caf7c'
+          checkpoint: 'bfef122940e6b57ee5ba7dbf5c4cf829f72bb55455fb2ed02dc2750a1918b18e'
   name 'Black Screen'
   homepage 'https://github.com/shockone/black-screen'
 
