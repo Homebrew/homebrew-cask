889c97229d30c148f647eab8cbc4700b0a0eeda5
Rafael Gaspar <rafael.gaspar@me.com>
2016-10-30 15:53:40 -0200

    Add ICQ for macOS v3.0.6475 (#26225)

---
+++ Casks/icq.rb
@@ -0,0 +1,18 @@
+cask 'icq' do
+  version :latest
+  sha256 :no_check
+
+  # r.mail.ru/clo12053316/ftp.icq.com was verified as official when first introduced to the cask
+  url 'https://r.mail.ru/clo12053316/ftp.icq.com/pub/ICQ_Mac/ICQ.dmg'
+  name 'ICQ for macOS'
+  homepage 'https://icq.com/mac/en'
+
+  app 'ICQ.app'
+
+  zap delete: [
+                '~/Library/Application Support/ICQ',
+                '~/Library/Caches/com.icq.macicq',
+                '~/Library/Preferences/com.icq.macicq.plist',
+                '~/Library/Saved Application State/com.icq.macicq.savedState',
+              ]
+end
