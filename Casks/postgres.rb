f256b5caa558da13fa057ab3346f40cd62fccf9b
Okke Timm <okke@timm.sh>
2016-10-30 16:14:31 +0100

    Update postgres to 9.6.1 (#26220)

--- Casks/postgres.rb
+++ Casks/postgres.rb
@@ -1,11 +1,11 @@
 cask 'postgres' do
-  version '9.6.0'
-  sha256 '5f3c00c67e40bf6c201e7aff6209946d4f14791bf1ac1e2ff0ca2e5a987640b3'
+  version '9.6.1'
+  sha256 '780ccd723baa52f7620471cb74d5cdd58040502f0f485ed667b5ccb6a649d3ac'
 
   # github.com/PostgresApp/PostgresApp was verified as official when first introduced to the cask
   url "https://github.com/PostgresApp/PostgresApp/releases/download/#{version}/Postgres-#{version}.zip"
   appcast 'https://github.com/PostgresApp/PostgresApp/releases.atom',
-          checkpoint: 'f917159368356ab5e54e22dfa7f13c728d1846fe2e5e3cdd8f21fb5c749681cf'
+          checkpoint: 'dd955be34d429304a39b3efa812eb87ad3e9df661aa5caf71443622765316397'
   name 'Postgres'
   homepage 'https://postgresapp.com/'
 
