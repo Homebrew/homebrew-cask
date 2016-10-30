5445f873fa5428445244f6993ab0114003adffac
Victor Popkov <victor@popkov.me>
2016-10-30 22:45:55 +0100

    Update metabase to 0.20.3.0 (#26236)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,10 +1,10 @@
 cask 'metabase' do
-  version '0.20.1.0'
-  sha256 'f5c8b105973d7e78a68cdc5ededb69b5a5399812816900160bf710b0d1eaec34'
+  version '0.20.3.0'
+  sha256 '45804c178bdf46e6c60f45c709bc5af8eb978571bf1a28c4b99168f57f91e2e6'
 
   url "http://downloads.metabase.com/v#{version.major_minor_patch}/Metabase.dmg"
   appcast 'http://downloads.metabase.com/appcast.xml',
-          checkpoint: 'f70b9e2350a6b65bdb9bed51fe1a8250947fd91680fa91eb6ce5717776eb74f7'
+          checkpoint: 'c93852307774349a390d08f5f5d688aa7bbf40c90eaaccc3d91368678e33143a'
   name 'Metabase'
   homepage 'http://www.metabase.com/'
 
