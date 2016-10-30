3a7c178adb8d9b8c3e2b159c12122ad5f0b5ce7b
Vinoth Ramiah <vin047@gmail.com>
2016-10-30 23:00:16 +0000

    Update plex-media-server to 1.2.3.2914-1ff0f18

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-server' do
-  version '1.1.4.2757-24ffd60'
-  sha256 '42731e23bbfb1ac76e68712b74002724d92fa50864db49e38c6460216080cc17'
+  version '1.2.3.2914-1ff0f18'
+  sha256 '6f929e61315cb08523aea1e2215b47aa873f0d5f5969595d78e0fc34cb9c9ed8'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   name 'Plex Media Server'
