4890474068c509781bec089a16f73f1c35e24772
Dirk Weise <code@dirk-weise.de>
2016-10-30 05:02:51 +0100

    Fix style issue: "Favor format over String#%."

--- Casks/tipp10.rb
+++ Casks/tipp10.rb
@@ -2,7 +2,7 @@ cask 'tipp10' do
   version '2.1.0'
   sha256 '023f9545b78a7ca35eea3b23597b6380a4f4d42cc75ace72dcadf645be9edb93'
 
-  url 'https://www.tipp10.com/en/download/getfile/1/%s/' % Time.now.to_i
+  url "https://www.tipp10.com/en/download/getfile/1/#{Time.now.to_i}/"
   name 'TIPP10'
   homepage 'https://www.tipp10.com/'
 
