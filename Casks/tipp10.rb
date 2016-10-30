194d3b57840844a1136bbdb98c50ff7b4c021702
Vítor Galvão <info@vitorgalvao.com>
2016-10-30 15:21:52 +0000

    tipp10.rb: made unversioned

--- Casks/tipp10.rb
+++ Casks/tipp10.rb
@@ -1,6 +1,7 @@
 cask 'tipp10' do
-  version '2.1.0'
-  sha256 '023f9545b78a7ca35eea3b23597b6380a4f4d42cc75ace72dcadf645be9edb93'
+  # note: "10" is not a version number, but an intrinsic part of the product name
+  version :latest
+  sha256 :no_check
 
   url "https://www.tipp10.com/en/download/getfile/1/#{Time.now.to_i}/"
   name 'TIPP10'
