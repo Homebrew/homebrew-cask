ab9a9b0dff05a3ef363989edaa56698b62edafbd
Takeru Naito <takeru.naito@gmail.com>
2016-10-31 10:23:57 +0900

    Update aquaskk to 4.4.3

--- Casks/aquaskk.rb
+++ Casks/aquaskk.rb
@@ -1,10 +1,10 @@
 cask 'aquaskk' do
-  version '4.4.2'
-  sha256 '745a864ac3682cfe9f94ad39f46ffbdd25c448f962647607c05bd3bb284bacef'
+  version '4.4.3'
+  sha256 'f9d481126c5b985ca80f74909da0c6c917747a8f391bcc27ec1b82e9b6cd3a00'
 
   url "https://github.com/codefirst/aquaskk/releases/download/#{version}/AquaSKK-#{version}.dmg"
   appcast 'https://github.com/codefirst/aquaskk/releases.atom',
-          checkpoint: '1525cf3a5ea8a2d9ee540bb3e9320cf512061b95683bc73353b6694c1f3ed2c2'
+          checkpoint: 'c4f7fd5793801a887b7c65f73472cda663b8d0aa30c33381fabe42518a905493'
   name 'AquaSKK'
   homepage 'https://github.com/codefirst/aquaskk'
 
