8690a908ec7735494d43fddb834a147a72fcf3c5
Taylor Blau <me@ttaylorr.com>
2016-11-01 10:20:48 -0600

    Add Aware.app v1.0.3

---
+++ Casks/aware.rb
@@ -0,0 +1,10 @@
+cask 'aware' do
+  version '1.0.3'
+  sha256 '65dcaef6735c649e2b6a406f48583609ca9d66e7680ca69eb61871502613dec3'
+
+  url 'https://github.com/josh/Aware/releases/download/v1.0.3/Aware.zip'
+  name 'Aware'
+  homepage 'http://awaremac.com'
+
+  app 'Aware.app'
+end
