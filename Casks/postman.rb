30080eb7589d87140db94ff264d219c2f55e53e0
Roman Zhuzha <romazhuzha@gmail.com>
2016-11-01 15:17:42 +0100

    Update postman to 4.8.1

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,11 +1,11 @@
 cask 'postman' do
-  version '4.8.0'
-  sha256 'b66d3f74b95d5116b04345eb358b81e5615957caa1794b012d175fb21ff252e4'
+  version '4.8.1'
+  sha256 'ed4e4c101b00eb43895d66d8510a98bfc21512542e393dde5346df105ac9b47c'
 
   # s3.amazonaws.com/postman-electron-builds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/postman-electron-builds/mac/Postman-osx-#{version}.zip"
   appcast 'https://app.getpostman.com/api/electron_updates_auto',
-          checkpoint: '5e127eb1fd7d0a137ddd33c6f7124e46af214964acf8837a432e4df63181b29d'
+          checkpoint: '21dcdef4cadfb47037b010020030e1dc8da8c0cca1ace5c328ffa3e74a26dde8'
   name 'Postman'
   homepage 'https://www.getpostman.com/'
 
