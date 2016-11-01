f2fbe5a3de191fcd277095e1dd435ef3d4202dfe
joniles <jon.iles@bcs.org.uk>
2016-11-01 21:01:03 +0000

    Merline Project.app Latest (#26298)

---
+++ Casks/merlin-project.rb
@@ -0,0 +1,10 @@
+cask 'merlin-project' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.projectwizards.net/downloads/MerlinProject.zip'
+  name 'Merlin Project'
+  homepage 'http://projectwizards.net/en/products/merlin-project'
+
+  app 'Merlin Project.app'
+end
