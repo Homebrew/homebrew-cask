1482ba602742652dffd263fad28b58c0f05fac09
Taylor Blau <me@ttaylorr.com>
2016-11-01 11:15:38 -0600

    Aware: interpolate version into URL

--- Casks/aware.rb
+++ Casks/aware.rb
@@ -3,7 +3,7 @@ cask 'aware' do
   sha256 '65dcaef6735c649e2b6a406f48583609ca9d66e7680ca69eb61871502613dec3'
 
   # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/josh/Aware/releases/download/v1.0.3/Aware.zip'
+  url "https://github.com/josh/Aware/releases/download/v#{version}/Aware.zip"
   appcast 'https://github.com/jos/Aware/releases.atom',
           checkpoint: '69d1a1fc0074d3e30dd3cb919be7a930907e690fdf733eb22d4c5bb4b9555986'
   name 'Aware'
