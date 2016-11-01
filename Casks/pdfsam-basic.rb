2a407ee30c3f13aa6128d4acca78228d3aeccf1a
longstone <longstone@users.noreply.github.com>
2016-11-01 18:23:42 +0100

    Update pdfsam-basic to 3.2.2 (#26297)

--- Casks/pdfsam-basic.rb
+++ Casks/pdfsam-basic.rb
@@ -1,11 +1,11 @@
 cask 'pdfsam-basic' do
-  version '3.1.0'
-  sha256 'ccb2a8dfe6509111b66d89cfd6c0404f6d674f5e6e319a732adbaae7957b6daf'
+  version '3.2.2'
+  sha256 'c57457fa03a943644ecf0b3ae189ccb1cdd4156157ac05be9d4846a29abcb08a'
 
   # github.com/torakiki/pdfsam was verified as official when first introduced to the cask
   url "https://github.com/torakiki/pdfsam/releases/download/v#{version}.RELEASE/PDFsam-#{version}.RELEASE.dmg"
   appcast 'https://github.com/torakiki/pdfsam/releases.atom',
-          checkpoint: '021f6c12c7e670d77fcd1497a728c8703fe069af99ecd3ce8cecdd0aba82e22a'
+          checkpoint: 'c50746704a30c294000a22a0acb4c7fea41e4b5f0f20fa8d961e4f0e5ca98893'
   name 'PDFsam Basic'
   homepage 'http://www.pdfsam.org/'
 
