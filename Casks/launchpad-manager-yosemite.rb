6ec54476db5c9c51a7622e15f2be07b3e2694db9
Numbermaniac <Numbermaniac@users.noreply.github.com>
2016-11-01 22:49:31 +1100

    add zap (#26286)

--- Casks/launchpad-manager-yosemite.rb
+++ Casks/launchpad-manager-yosemite.rb
@@ -20,5 +20,8 @@ cask 'launchpad-manager-yosemite' do
   name 'Launchpad Manager'
   homepage 'http://launchpadmanager.com/'
 
-  zap delete: '~/Library/Application Support/Launchpad Manager'
+  zap delete: [
+                '~/Library/Application Support/Launchpad Manager',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.attilamiklosi.yos.launchpadmanager.sfl',
+              ]
 end
