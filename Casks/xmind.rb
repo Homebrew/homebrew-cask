d03d545fb27a3add712f5ae8b3e03c6da64f3eb0
neutric <neutric@users.noreply.github.com>
2016-11-02 19:23:55 +0100

    xmind: Change version scheme

--- Casks/xmind.rb
+++ Casks/xmind.rb
@@ -1,8 +1,8 @@
 cask 'xmind' do
-  version '3.7.0'
+  version '8'
   sha256 'c4dc8e3eb47d19557fbe5ac837122768eded1321042b064fc3e25f7a2a66ce86'
 
-  url 'https://www.xmind.net/xmind/downloads/xmind-8-macosx.dmg'
+  url 'https://www.xmind.net/xmind/downloads/xmind-#{version}-macosx.dmg'
   name 'XMind'
   homepage 'https://www.xmind.net'
 
