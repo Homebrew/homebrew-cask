66688dcb1ba4abf577b300ef3b870a110ce84fb0
puffdad <puffdad@users.noreply.github.com>
2016-11-02 19:04:04 +0800

    Update tableau to 10.1.0 (#26317)

--- Casks/tableau.rb
+++ Casks/tableau.rb
@@ -1,6 +1,6 @@
 cask 'tableau' do
-  version '10.0.2'
-  sha256 'f023ae4778ec4d3579aef1d635be1b7f4086d28ff5244c7b681d2f0791d4d451'
+  version '10.1.0'
+  sha256 '2f29c64b1794030ceb33ad0c666c0334bc31add74154e1ba72b7f7d144cd6a4a'
 
   url "https://downloads.tableau.com/tssoftware/TableauDesktop-#{version.dots_to_hyphens}.dmg"
   name 'Tableau'
