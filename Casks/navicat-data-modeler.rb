ef06464d8c82d33c0d56e3d8688e4488817c3351
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-02 19:46:37 +0800

    Update navicat-data-modeler to 2.1.12 (#26333)

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -1,6 +1,6 @@
 cask 'navicat-data-modeler' do
-  version '2.1.5'
-  sha256 '4977dec5f0d5e18acf3bd075c6a725a16390c1900baf54f97b71200255024077'
+  version '2.1.12'
+  sha256 '84ed9e9ea5337dd15f36fceeb2e332168c1116ddb11ec66d0b6b658216761492'
 
   url "http://download.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
   name 'Navicat Data Modeler'
