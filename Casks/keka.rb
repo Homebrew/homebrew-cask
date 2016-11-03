7cb3d1cfdc3898acb1a5d602236b79ed27c9b3f7
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-04 04:40:08 +0800

    Update keka to 1.0.5 (#26386)

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -1,10 +1,10 @@
 cask 'keka' do
-  version '1.0.4'
-  sha256 '0075741ed52e2c86d7749dfe2baf54c8b6dad75a780b4b51ca5fb14337124701'
+  version '1.0.5'
+  sha256 'c27401253aad6de5d9d19c5a2b548042bda713049caab8a850fd0c3a5858f6dd'
 
   url "http://www.kekaosx.com/release/Keka-#{version}-intel.dmg"
   appcast 'http://www.kekaosx.com/update/keka.xml',
-          checkpoint: '40bf3dfa927e2b377a98177f0362d459b39e96c60c45c5f0cfd53d305f01cbe3'
+          checkpoint: 'b9ef692d877af3c3bb2e9f6dfc679786ed3cfad538f4fdb2485e592d55d600cb'
   name 'Keka'
   homepage 'http://kekaosx.com/'
 
