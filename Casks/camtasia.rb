b1b563670df823c4896c364382ffcc7a843fe20a
Luis López <ldotlopez@gmail.com>
2016-11-03 11:48:17 +0100

    Reuse version info.
    https://github.com/caskroom/homebrew-cask/pull/26366#pullrequestreview-6977259

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -2,9 +2,9 @@ cask 'camtasia' do
   version '3.0.2'
   sha256 '539aa3348178ce1614efff36cb26c3241cebc96516ee95b2c0da3abe622947e7'
 
-  url 'https://download.techsmith.com/camtasiamac/enu/302/camtasia.dmg'
+  url "https://download.techsmith.com/camtasiamac/enu/#{version.no_dots}/camtasia.dmg"
   name 'Camtasia'
   homepage 'https://www.techsmith.com/camtasia.html'
 
-  app 'Camtasia 3.app'
+  app "Camtasia #{version.major}.app"
 end
