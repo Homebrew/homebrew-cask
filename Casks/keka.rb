b509a4126ab5cbc2ad0e5fa3362e1269763fa089
Vítor Galvão <info@vitorgalvao.com>
2016-11-03 20:44:09 +0000

    keka.rb fix sha

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -1,6 +1,6 @@
 cask 'keka' do
   version '1.0.5'
-  sha256 'c27401253aad6de5d9d19c5a2b548042bda713049caab8a850fd0c3a5858f6dd'
+  sha256 '0075741ed52e2c86d7749dfe2baf54c8b6dad75a780b4b51ca5fb14337124701'
 
   url "http://www.kekaosx.com/release/Keka-#{version}-intel.dmg"
   appcast 'http://www.kekaosx.com/update/keka.xml',
