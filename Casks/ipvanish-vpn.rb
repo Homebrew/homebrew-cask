57c90382b4b6e6f57494a2212d8203de94973a57
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-03 09:28:17 +0800

    Update ipvanish-vpn.rb

--- Casks/ipvanish-vpn.rb
+++ Casks/ipvanish-vpn.rb
@@ -1,6 +1,6 @@
 cask 'ipvanish-vpn' do
   version '3.0.4'
-  sha256 '19465c0309e0b9ecabe2e371219e8ab9fe2a3b4210a1266ca57a4f40efe88bb0'
+  sha256 'cf934a6cf62bbc4268e438a294e1ae337b27125df46970046412c506d0d8b92a'
 
   url "https://www.ipvanish.com/software/osx/IPVanish_v#{version}.zip"
   appcast 'https://www.ipvanish.com/software/osx/updates.xml',
