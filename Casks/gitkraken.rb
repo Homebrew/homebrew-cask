fd1d610d5bc267ef8a8076e3104da41a91e87edc
Artem Medvedev <amedvedev@outlook.com>
2016-11-03 23:34:43 +0200

    Update gitkraken to 1.9.0 (#26387)

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,10 +1,10 @@
 cask 'gitkraken' do
-  version '1.8.1'
-  sha256 '808c8abf62c2b8b06dad06f4c544a0127144916e242fd6e7c18b254c3cb8bedf'
+  version '1.9.0'
+  sha256 '6e727515202bac6af5ab2d15427c984ee795de3647ade4c0e66f71d00f9685fc'
 
   url "https://release.gitkraken.com/darwin/v#{version}.zip"
   appcast 'https://release.gitkraken.com/darwin/RELEASES',
-          checkpoint: '422497775f56c8e33008f78e613abdd0d1f37a9588176c328a7207f2218e9218'
+          checkpoint: '6a9a0bd89cda61c0d833498f1f068c04970d4083246adde25d04bffef01a45df'
   name 'GitKraken'
   homepage 'https://www.gitkraken.com/'
 
