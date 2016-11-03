bec63f2b761fd999ac32a8137aa6a5227b1937be
Christian Inzinger <christian@inz.io>
2016-11-03 21:31:26 +0100

    Update bose-soundtouch to 13.0.13.16800 (#26381)

--- Casks/bose-soundtouch.rb
+++ Casks/bose-soundtouch.rb
@@ -1,6 +1,6 @@
 cask 'bose-soundtouch' do
-  version '13.0.11.16439'
-  sha256 'af75c51c01f004747d43cebf195fe337feeccaa6bd8e8e95b2b1ea019499c0da'
+  version '13.0.13.16800'
+  sha256 '8e0bf45c017f074c449e4031a920b5344bc82a8e1ffac32beadd0274b8197f8d'
 
   # bose.com was verified as official when first introduced to the cask
   url "https://worldwide.bose.com/downloads/assets/updates/soundtouch/SoundTouch-#{version}-osx-10.9-installer.app.dmg"
