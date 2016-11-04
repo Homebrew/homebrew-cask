43d7a414e523cf6f04acc16816df8a83b7c73dac
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-04 20:43:53 +0800

    Update abyss to x1 (#26405)

--- Casks/abyss.rb
+++ Casks/abyss.rb
@@ -1,8 +1,8 @@
 cask 'abyss' do
   version 'x1'
-  sha256 '56d2a51ad4a0b5788f89907ad1b4df6106bd2515906aee7e62466b8aa52bccd6'
+  sha256 '2130e3da4d97c71d2c932261f51ed31cbb3bcadfc600db5eb6b7f646ccf31707'
 
-  url 'https://aprelium.com/data/abwsx1.dmg'
+  url "https://aprelium.com/data/abws#{version}.dmg"
   name 'Abyss Web Server'
   homepage 'https://aprelium.com/abyssws/'
 
