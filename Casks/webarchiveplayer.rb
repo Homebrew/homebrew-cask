c8a9f29dc9f7677bc61399486930cfa4951f74da
Vítor Galvão <info@vitorgalvao.com>
2016-11-04 17:50:04 +0000

    Update webarchiveplayer to 1.4.5 (#26417)

--- Casks/webarchiveplayer.rb
+++ Casks/webarchiveplayer.rb
@@ -1,10 +1,10 @@
 cask 'webarchiveplayer' do
-  version '1.4.1'
+  version '1.4.5'
   sha256 'dc5bc702800c913c8781f9c23074156003bc991d4c3aeffdb5add0f51e05cdb6'
 
   url "https://github.com/ikreymer/webarchiveplayer/releases/download/#{version}/webarchiveplayer.dmg"
   appcast 'https://github.com/ikreymer/webarchiveplayer/releases.atom',
-          checkpoint: 'afa19ab62477ff8a21dde5e4704c78e1ae2d6ba027c43d53d13e6d970c3a38e6'
+          checkpoint: '83f4f76661fddb0fb06f946c43b1291ee57e51690430bbc77bd1f6d177ec2b70'
   name 'webarchiveplayer'
   homepage 'https://github.com/ikreymer/webarchiveplayer'
 
