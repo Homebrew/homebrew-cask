b7a72eb651abbc40899150ba1ee0be2d4a8bfd7e
Markus Reiter <me@reitermark.us>
2016-11-04 16:38:58 +0100

    updated svg-cleaner (0.7.1) (#26410)

--- Casks/svg-cleaner.rb
+++ Casks/svg-cleaner.rb
@@ -1,14 +1,12 @@
 cask 'svg-cleaner' do
-  version '0.7.0'
-  sha256 '702b4c5fa80b30aedb44f8e232a7cd06aa3f8d3d3ec57c931142c7fbdf687752'
+  version '0.7.1'
+  sha256 '3eae7e6a3738f291d93d6ab029db55ffacfc795ecca8d23e1fe66de00ba703d4'
 
-  url "https://github.com/RazrFalcon/svgcleaner-gui/releases/download/v#{version}/svgcleaner_macos_#{version}.7z"
+  url "https://github.com/RazrFalcon/svgcleaner-gui/releases/download/v#{version}/svgcleaner_macos_#{version}.zip"
   appcast 'https://github.com/RazrFalcon/svgcleaner-gui/releases.atom',
-          checkpoint: '57e1927fc72a17d28760328c3cffc6ee15fc56de1160334489aa72dc12304164'
-  name 'SVGCleaner'
+          checkpoint: 'c6d3fc15b92224928961f3d87218a1306d2bb47c44b9c793ea1cff40c6251b41'
+  name 'SVG Cleaner'
   homepage 'https://github.com/RazrFalcon/svgcleaner-gui/'
-
-  depends_on formula: 'unar'
 
   app 'SVG Cleaner.app'
 
