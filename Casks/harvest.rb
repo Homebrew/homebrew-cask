266c869e080acf0e2bdaa753e7de24406ab75e88
Victor Popkov <victor@popkov.me>
2016-11-04 22:33:45 +0200

    Update harvest to 2.0.2

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -1,10 +1,10 @@
 cask 'harvest' do
-  version '2.0.1'
-  sha256 '1addff925c7136a9e727af5f8d0dcc246ff37d7e71dbbac2e62baa630d676ad8'
+  version '2.0.2'
+  sha256 '51aaf90c4bae2b766ee2ab10158d3a0dd9a33e848075ac765d265be45250241e'
 
   url "https://www.getharvest.com/harvest/mac/Harvest.#{version}.zip"
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml',
-          checkpoint: '3ff20ca06678903097db234f916dff84419c76f954d64c4903f4210836974f6f'
+          checkpoint: '7c7882523447866331187227ff9c410d2e34bf68d6d00bf454676db71b8d76f0'
   name 'Harvest'
   homepage 'https://www.getharvest.com/mac'
 
