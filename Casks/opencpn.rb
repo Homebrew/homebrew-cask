7134bc113fd3fab3f0eab54e0743f45345f76dbf
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-05 07:49:21 +0800

    Update opencpn to 4.4.0 (#26426)

--- Casks/opencpn.rb
+++ Casks/opencpn.rb
@@ -3,7 +3,7 @@ cask 'opencpn' do
   sha256 'aedb42ed43a4f5776422410079b946dd87e05a117ba4289733e32c1e6657d6e7'
 
   # opencpn.navnux.org was verified as official when first introduced to the cask
-  url 'http://opencpn.navnux.org/4.4.0/OpenCPN_4.4.0.dmg'
+  url "http://opencpn.navnux.org/#{version}/OpenCPN_#{version}.dmg"
   name 'OpenCPN'
   homepage 'http://opencpn.org'
 
