620c196fc9d22ceae1a7756ca918682e7ae8cd8d
Laurent Perrinet <laurent.perrinet@gmail.com>
2016-11-04 14:05:12 +0100

    adding nteract v0.0.14

--- Casks/nteract.rb
+++ Casks/nteract.rb
@@ -2,7 +2,8 @@ cask 'nteract' do
   version '0.0.14'
   sha256 '0734cc687cdf64f499c760e951ea27810d8ddd05d5708f9384fcaeea82117108'
 
-  url "https://github.com/nteract/nteract/releases/download/v0.0.14/nteract-#{version}.dmg"
+  url "https://github.com/nteract/nteract/releases/download/v#{version}/nteract-#{version}.dmg"
+
   name 'nteract'
   homepage 'https://github.com/nteract'
 
