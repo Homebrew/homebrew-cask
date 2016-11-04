c9bfba0f3b665b681288e0fd319baaa7bdd68b82
Michael Ford <fanquake@users.noreply.github.com>
2016-11-04 12:32:50 +0800

    Anki 2.0.36.qt47

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,7 +1,7 @@
 cask 'anki' do
   if MacOS.version <= :snow_leopard
-    version '2.0.34.qt47'
-    sha256 '83b008ad8ebb20247a673de3e37908d96cbb8573e5257b826176f5649a4ef6b7'
+    version '2.0.36.qt47'
+    sha256 '365d11edfad2e828db58031af03b4af3fa95b18bbbb09e59c4ba97eb2fee7a97'
   else
     version '2.0.36'
     sha256 '4e229ace72b11b93186af31183ae365bb24c5f14d098b7c4bb564d16c3dfbc3c'
