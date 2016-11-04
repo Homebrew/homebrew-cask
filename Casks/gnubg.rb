b0487d62f8f6a86b941e45040bba60e8ee8b1ff4
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-05 01:42:49 +0800

    Update gnubg to 1.04.000,1010 (#26407)
    * Update gnubg to 1.04.000,1010
    
    * Update gnubg.rb

--- Casks/gnubg.rb
+++ Casks/gnubg.rb
@@ -1,12 +1,14 @@
 cask 'gnubg' do
   if MacOS.version <= :mavericks
-    version '1.03.000-x86_64'
+    version '1.03.000'
     sha256 '90614e940a94515d265ccccabf05ae6610121207b6ba0b7ae656ac2e68c0f6ff'
-    url 'http://gnubg.org/media/macos/gnubg-1_03_000-mac-x86_64.dmg'
+
+    url "http://gnubg.org/media/macos/gnubg-#{version.dots_to_underscores}-mac-x86_64.dmg"
   else
-    version '1.04.000-1010-x86_64'
+    version '1.04.000,1010'
     sha256 '3f093856f18b0d47373d3be4094a4f127e654326438e7172c877b775764d7422'
-    url 'http://gnubg.org/media/macos/gnubg-1_04_000-mac-1010-x86_64.dmg'
+
+    url "http://gnubg.org/media/macos/gnubg-#{version.before_comma.dots_to_underscores}-mac-#{version.after_comma}-x86_64.dmg"
   end
 
   name 'Gnu Backgammon'
