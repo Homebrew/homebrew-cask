8f76926186bb57aa6d4d8df47c86eee29a4a552b
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-06 03:02:01 +0800

    Update audio-hijack to 3.3.2 (#26457)

--- Casks/audio-hijack.rb
+++ Casks/audio-hijack.rb
@@ -3,7 +3,7 @@ cask 'audio-hijack' do
   sha256 '7339455831699d5c7b55f0bee2445c2a63c4c6f29e70791b384215a283f98a4b'
 
   url 'https://rogueamoeba.com/audiohijack/download/AudioHijack.zip'
-  appcast 'https://www.rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.audiohijack3',
+  appcast "https://www.rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.audiohijack#{version.major}",
           checkpoint: '6c8bca6747961bfd604191d57047623ed8162f4418b049919b11a83067e68719'
   name 'Audio Hijack'
   homepage 'https://www.rogueamoeba.com/audiohijack/'
