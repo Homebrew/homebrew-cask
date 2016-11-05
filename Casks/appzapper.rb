522af0a41e58489f1ac9640481af822f90b1d7c2
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-06 01:04:18 +0800

    Update appzapper to 2.0.1 (#26455)

--- Casks/appzapper.rb
+++ Casks/appzapper.rb
@@ -3,7 +3,7 @@ cask 'appzapper' do
   sha256 'b7d0bdd05cf246a2f2ab18145b052824860cb74a6ae665fba9d403f6bb79fac4'
 
   url "https://www.appzapper.com/downloads/AppZapper#{version}.zip"
-  appcast 'https://www.appzapper.com/az2appcast.xml',
+  appcast "https://www.appzapper.com/az#{version.major}appcast.xml",
           checkpoint: 'b91ae47cbd23159ada1e03357647dc3140cd35ef509dfa44ed13e369aad07e88'
   name 'AppZapper'
   homepage 'https://www.appzapper.com/'
