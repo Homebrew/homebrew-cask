7a279f3893eb207fa436ebab8c890eca61f428ca
Dirk Rademann <dirk.rademann@icloud.com>
2016-11-05 01:26:04 +0100

    Update iridient-developer to 3.1.2

--- Casks/iridient-developer.rb
+++ Casks/iridient-developer.rb
@@ -1,6 +1,6 @@
 cask 'iridient-developer' do
-  version '3.1.0'
-  sha256 'bf2dfeaa4f1c16eaa159566a6191d174b69e7e46f5b47375f44528eb0687bc88'
+  version '3.1.2'
+  sha256 '740aa73096d8729dcd0b457b6b76f5f8056347a3f8104112999283814a5bddc1'
 
   url "http://www.iridientdigital.com/downloads/IridientDeveloper_#{version.no_dots}.dmg"
   name 'Iridient Developer'
