6de2d9463825e7993b7850fc504a1ce57a781db0
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-06 01:06:46 +0800

    Update airparrot to 2.6.2 (#26453)

--- Casks/airparrot.rb
+++ Casks/airparrot.rb
@@ -2,11 +2,11 @@ cask 'airparrot' do
   version '2.6.2'
   sha256 'fdc491ab964ac19d4f2b62e9f1f5c827941ce1c207a9eec6003f8aeaf7dc81bd'
 
-  url "https://download.airsquirrels.com/AirParrot2/Mac/AirParrot-#{version}.dmg"
-  appcast 'https://updates.airsquirrels.com/AirParrot2/Mac/AirParrot2.xml',
+  url "https://download.airsquirrels.com/AirParrot#{version.major}/Mac/AirParrot-#{version}.dmg"
+  appcast "https://updates.airsquirrels.com/AirParrot#{version.major}/Mac/AirParrot#{version.major}.xml",
           checkpoint: '92dc351a84122af35a424579d65fa9c842fb0a26d66625a729b21e243a49bffa'
   name 'AirParrot'
   homepage 'https://www.airsquirrels.com/airparrot/'
 
-  app 'AirParrot 2.app'
+  app "AirParrot #{version.major}.app"
 end
