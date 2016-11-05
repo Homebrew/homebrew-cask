7d572753fac0f34cfe73a9354363c50551b606b2
miccal <miccal.matthews@gmail.com>
2016-11-06 02:48:54 +0800

    Update beamer to 3.1.2

--- Casks/beamer.rb
+++ Casks/beamer.rb
@@ -3,7 +3,7 @@ cask 'beamer' do
   sha256 '3190c7e4e6902a1a9a2d36227c9265a17d946bb9b7acd62517af7b5ec33266b0'
 
   url "https://beamer-app.com/resources/downloads/Beamer-#{version}.zip"
-  appcast 'https://beamer-app.com/beamer3-appcast.xml',
+  appcast "https://beamer-app.com/beamer#{version.major}-appcast.xml",
           checkpoint: 'efc534002867fa76b4e190e0614f7d4b3e35fcd27a33814716309bf6d3477f7d'
   name 'Beamer'
   homepage 'https://beamer-app.com/'
