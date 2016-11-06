9281c961acc868dc755b7184bae049e1cb05ada8
Jan van der Lugt <janlugt@gmail.com>
2016-11-06 13:54:42 -0800

    Update tuxera-ntfs to 2016

--- Casks/tuxera-ntfs.rb
+++ Casks/tuxera-ntfs.rb
@@ -1,6 +1,6 @@
 cask 'tuxera-ntfs' do
-  version '2015.3'
-  sha256 'a263f405f8f3a7950e6c29e8d0ac511823a8b2e58f76211190ec179fbd62947a'
+  version '2016'
+  sha256 '29ae78a41fb79f8bec6f3dfd7878098300c79d24f9acda55db293c82aa235d03'
 
   url "https://www.tuxera.com/mac/tuxerantfs_#{version}.dmg"
   name 'Tuxera NTFS'
