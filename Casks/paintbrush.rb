0636c8a87148fb0d776cd27e0078c7bb2229b38c
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-07 05:24:11 +0800

    Update paintbrush to 2.1.2 (#26527)

--- Casks/paintbrush.rb
+++ Casks/paintbrush.rb
@@ -3,7 +3,7 @@ cask 'paintbrush' do
   sha256 '3bf18908a191b65efec2a15af0c5f5e95f76e1ec00d71063e235a146b9f6c417'
 
   url "https://downloads.sourceforge.net/paintbrush/Paintbrush%202.x/Paintbrush%20#{version}/Paintbrush-#{version}.zip"
-  appcast 'http://paintbrush.sourceforge.net/updates2x.xml',
+  appcast "http://paintbrush.sourceforge.net/updates#{version.major}x.xml",
           checkpoint: 'e1e7f7afa9b02245fae55442f3feedddddf1a8d43730abd68425529b83fe37bd'
   name 'Paintbrush'
   homepage 'http://paintbrush.sourceforge.net/'
