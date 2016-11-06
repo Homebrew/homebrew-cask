3146648b6582fc4f1c94734d8d96ce53613be736
miccal <miccal.matthews@gmail.com>
2016-11-06 09:03:41 +0800

    Update hot-shots to 2.5

--- Casks/hot-shots.rb
+++ Casks/hot-shots.rb
@@ -3,7 +3,7 @@ cask 'hot-shots' do
   sha256 'ab8b108b6fba399e6adc6eb69b0964c3a86645a2c845d95817bbf03b61198a03'
 
   url "https://gngrwzrd.com/HotShots#{version.major}.zip"
-  appcast 'https://www.gngrwzrd.com/hotshots2-appcast.xml',
+  appcast "https://www.gngrwzrd.com/hotshots#{version.major}-appcast.xml",
           checkpoint: 'bc2389f3e28b96748c5f94508d125124f0af2d188b810d397c412e12f166eb59'
   name 'Hot Shots'
   homepage 'https://gngrwzrd.com/hotshots/'
