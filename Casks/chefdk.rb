c4ab6f5d5b06eccd5e8c348bf95543ed28b5506d
ed@flanagan.xyz <ed@flanagan.xyz>
2016-11-06 13:02:52 -0800

    chefdk: update to use macOS versions (#26531)
    * Add conditional for Mountain Lion package version
    * Replace static '10.11' in url/appcast urls with
      macOS version
    * Add case statement for appcast checkpoint
    * Add x86_64 dependency

--- Casks/chefdk.rb
+++ Casks/chefdk.rb
@@ -1,13 +1,20 @@
 cask 'chefdk' do
-  version '0.19.6-1'
-  sha256 '10018814d9abf1323ad7193c7950ec943b5c307e63f07de395604c09e96edce9'
+  if MacOS.version == :mountain_lion
+    version '0.11.2-1'
+    sha256 '56899eab322cacac7f445a24d3159af34fccb5910642f4535eff4ee47321fe56'
+  elsif MacOS.version >= :mavericks
+    version '0.19.6-1'
+    sha256 '10018814d9abf1323ad7193c7950ec943b5c307e63f07de395604c09e96edce9'
+  end
 
-  url "https://packages.chef.io/stable/mac_os_x/10.11/chefdk-#{version}.dmg"
-  appcast 'https://www.chef.io/chef/metadata-chefdk?p=mac_os_x&pv=10.11&m=x86_64&v=latest&prerelease=false',
+  url "https://packages.chef.io/stable/mac_os_x/#{MacOS.version}/chefdk-#{version}.dmg"
+  appcast "https://www.chef.io/chef/metadata-chefdk?p=mac_os_x&pv=#{MacOS.version}&m=x86_64&v=latest&prerelease=false",
           checkpoint: 'ca865b973720f39f692b0e80e226cb999633e973cb74f9d2df4399a457854564'
   name 'Chef Development Kit'
   name 'ChefDK'
   homepage 'https://downloads.chef.io/chef-dk/'
+
+  depends_on arch: :x86_64
 
   pkg "chefdk-#{version}.pkg"
 
