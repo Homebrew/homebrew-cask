529351be6d960e416608a73e7403072a93d8d771
nuomi1 <nuomi1@qq.com>
2016-11-06 20:15:02 +0800

    Update keka.rb for url and zap (#26499)
    Update keka.rb for url and zap

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -1,14 +1,21 @@
 cask 'keka' do
   version '1.0.5'
-  sha256 '0075741ed52e2c86d7749dfe2baf54c8b6dad75a780b4b51ca5fb14337124701'
+  sha256 'c27401253aad6de5d9d19c5a2b548042bda713049caab8a850fd0c3a5858f6dd'
 
-  url "http://www.kekaosx.com/release/Keka-#{version}-intel.dmg"
+  url "http://www.kekaosx.com/release/Keka-#{version}.dmg"
   appcast 'http://www.kekaosx.com/update/keka.xml',
           checkpoint: 'b9ef692d877af3c3bb2e9f6dfc679786ed3cfad538f4fdb2485e592d55d600cb'
   name 'Keka'
   homepage 'http://kekaosx.com/'
 
+  auto_updates true
+  depends_on macos: '>= :lion'
+
   app 'Keka.app'
 
-  zap delete: '~/Library/Preferences/com.aone.keka.plist'
+  zap delete: [
+                '~/Library/Application Support/Keka',
+                '~/Library/Caches/com.aone.keka',
+                '~/Library/Preferences/com.aone.keka.plist',
+              ]
 end
