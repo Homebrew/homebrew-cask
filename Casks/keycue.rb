e18a7820b397585c3f178e6d9f37db46a4a35c4a
Matt Tagg <wamatt@gmail.com>
2016-11-06 13:07:44 -0800

    Update keycue to 8.2 (#26520)

--- Casks/keycue.rb
+++ Casks/keycue.rb
@@ -1,6 +1,6 @@
 cask 'keycue' do
-  version '7.5'
-  sha256 '8d2ae27ac7a623e3cdcfe939caf996e81e39e3dbb7452f1a634b24dbe5050ad3'
+  version '8.2'
+  sha256 'd0a455b983ef966b6e1741972cce25745410b0e639f5482c4e4696c9c649844e'
 
   url "http://www.ergonis.com/downloads/products/keycue/KeyCue#{version.no_dots}-Install.dmg"
   name 'KeyCue'
