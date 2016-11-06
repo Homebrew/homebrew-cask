9c41a9a3a730adbaa37b6c36893525c86ef1906b
n0ts <me@n0ts.org>
2016-11-06 18:18:30 +0900

    Update archiver 3

--- Casks/archiver.rb
+++ Casks/archiver.rb
@@ -3,7 +3,7 @@ cask :v1 => 'archiver' do
   sha256 :no_check
 
   # googleapis.com is the official download host per the vendor homepage
-  url 'https://commondatastorage.googleapis.com/incrediblebee/apps/Archiver/Archiver.zip'
+  url 'https://commondatastorage.googleapis.com/incrediblebee/apps/Archiver-3/Archiver.zip'
   name 'Archiver'
   homepage 'http://archiverapp.com/'
   license :commercial
