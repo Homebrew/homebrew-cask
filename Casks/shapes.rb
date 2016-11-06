f788df900de8257540c83ab09fe2aa772f44fad6
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-06 18:37:54 +0800

    Update shapes to 4.8.2 (#26502)

--- Casks/shapes.rb
+++ Casks/shapes.rb
@@ -3,7 +3,7 @@ cask 'shapes' do
   sha256 '4185d0f604ea4e271e198b4166667ad76c2cdd4092fd685b53a2209fb294a0ed'
 
   url "http://shapesapp.com/dist/Shapes_#{version}.zip"
-  appcast 'http://shapesapp.com/appcast/shapes4.rss',
+  appcast "http://shapesapp.com/appcast/shapes#{version.major}.rss",
           checkpoint: '273c28c2c6030abfc48ef63eba711fcf86f36db4d31567b299ad95d5d90d4602'
   name 'Shapes'
   homepage 'http://shapesapp.com'
