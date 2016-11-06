e18357e47ec0da5ff0cc273aa2b4e77337c2dd84
miccal <miccal.matthews@gmail.com>
2016-11-06 17:54:51 +0800

    Update slicy to 1.1.7

--- Casks/slicy.rb
+++ Casks/slicy.rb
@@ -2,8 +2,9 @@ cask 'slicy' do
   version '1.1.7'
   sha256 '0a57becbcbb6d4de17ab27e7c011f07543c37277e2e6988f60f464c6483fee39'
 
-  url 'https://macrabbit.com/slicy/get/'
-  appcast 'https://update.macrabbit.com/slicy/1.1.3.xml',
+  # s3.amazonaws.com/macrabbit/downloads was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/macrabbit/downloads/Slicy+#{version}.zip"
+  appcast "https://update.macrabbit.com/slicy/#{version}.xml",
           checkpoint: '39d01f62bb6ab96fdee061a8077fd8cbffcafee6551d7be4bc0fe4dc20249e3d'
   name 'Slicy'
   homepage 'https://macrabbit.com/slicy/'
