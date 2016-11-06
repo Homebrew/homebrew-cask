1504e59aa9382edeb11b5373ac1a2fe4ceccd2c5
neutric <neutric@users.noreply.github.com>
2016-11-06 12:19:55 +0100

    Update privatetunnel to 2.7 (#26511)

--- Casks/privatetunnel.rb
+++ Casks/privatetunnel.rb
@@ -1,6 +1,6 @@
 cask 'privatetunnel' do
-  version '2.6'
-  sha256 '8a294d0bff57e0b5df41d613e1104a6f39483f3440dccd080022249f0b698460'
+  version '2.7'
+  sha256 '64ff83847f41cbdc857f616bff03477292c30e2283ce9617a59bb4bbba1058b4'
 
   # swupdate.openvpn.org/privatetunnel was verified as official when first introduced to the cask
   url "https://swupdate.openvpn.org/privatetunnel/client/privatetunnel-mac-#{version}.dmg"
