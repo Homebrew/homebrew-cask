30cd48bc53e5058d9e7e4587931f2c12fdaaa561
Jason Masker <jason@masker.net>
2016-11-06 05:40:53 -0500

    komodo-edit: upgrade to 10.1.1-17414 (#26497)

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -1,6 +1,6 @@
 cask 'komodo-edit' do
-  version '10.0.1-17276'
-  sha256 '6ff89c1c0a43b16900889da6d7c6b7988f9aa82d04623bfc15c0b95b6e7fc591'
+  version '10.1.1-17414'
+  sha256 'ff5f06fa202bb82b4e30f55f03a8ea657fcf4dbcc1af8dfd9c73660bf824a406'
 
   # activestate.com/Komodo was verified as official when first introduced to the cask
   url "https://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*}, '')}/Komodo-Edit-#{version}-macosx-x86_64.dmg"
