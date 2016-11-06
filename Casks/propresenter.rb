4874b4a756a02341b995c5c9755dd5c816b59d53
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-06 10:25:24 +0800

    Update propresenter to 6.1.2_b15157 (#26489)

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -2,11 +2,11 @@ cask 'propresenter' do
   version '6.1.2_b15157'
   sha256 '693a1ee058ed307a85ce0e7c09e4c9d44902bab52dc422465bc46ab7cd4a85c9'
 
-  url "https://www.renewedvision.com/downloads/ProPresenter6_#{version}.dmg"
-  appcast 'https://www.renewedvision.com/update/ProPresenter6.php',
+  url "https://www.renewedvision.com/downloads/ProPresenter#{version.major}_#{version}.dmg"
+  appcast "https://www.renewedvision.com/update/ProPresenter#{version.major}.php",
           checkpoint: '8fbd91f023565ce9cffe855793fe29849812fda7710cb0224b5de8020d524b8a'
   name 'ProPresenter'
   homepage 'https://www.renewedvision.com/propresenter.php'
 
-  app 'ProPresenter 6.app'
+  app "ProPresenter #{version.major}.app"
 end
