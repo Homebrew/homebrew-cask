6efc48db6a49357fe10868562b725cfb19a7ca84
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-06 10:25:10 +0800

    Update fluid to 1.8.6 (#26477)

--- Casks/fluid.rb
+++ Casks/fluid.rb
@@ -3,7 +3,7 @@ cask 'fluid' do
   sha256 '82a3e4d634383b2423adb16fbc15699c7c0927180f1ac58e223f6eaf524d99c0'
 
   url "http://fluidapp.com/dist/Fluid_#{version}.zip"
-  appcast 'http://fluidapp.com/appcast/fluid1.rss',
+  appcast "http://fluidapp.com/appcast/fluid#{version.major}.rss",
           checkpoint: 'fbc75fb1251ce981acce75c680073b0e360b27e67dcf869d63ab5437387e015b'
   name 'Fluid'
   homepage 'http://fluidapp.com/'
