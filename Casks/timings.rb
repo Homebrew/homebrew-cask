f5da226c2cf52539b51e1c4c66f14c42c2497c44
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-06 18:39:20 +0800

    Update timings to 3.0.2 (#26506)

--- Casks/timings.rb
+++ Casks/timings.rb
@@ -1,10 +1,10 @@
 cask 'timings' do
-  version '2.4.8'
-  sha256 'd569aaa064557fa31654826e083a295b3199a17f845f11b48da54b3d304e140b'
+  version '3.0.2'
+  sha256 '491616bdb7688317a44eaae9325d35a0fe2ce1fe6c58b3ac4610d4b5cc1fba65'
 
-  url "https://mediaatelier.com/Timings2/Timings_#{version}.zip"
-  appcast 'https://mediaatelier.com/Timings2/feed.php',
-          checkpoint: '849b39da0e118572475b2089f9e3d68b08b531c490b35a18f294a763a270e444'
+  url "https://mediaatelier.com/Timings#{version.major}/Timings_#{version}.zip"
+  appcast "https://mediaatelier.com/Timings#{version.major}/feed.php",
+          checkpoint: '0e383bab08e9fdc9e2e2ae99c8f63b2d5558991ea0adfb6f47b96fe14392feac'
   name 'Timings'
   homepage 'https://mediaatelier.com/Timings'
 
