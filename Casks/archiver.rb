99be863c92676417316e960a4abff5775cb546f8
Vítor Galvão <info@vitorgalvao.com>
2016-11-06 12:20:44 +0000

    Update archiver to latest (#26514)

--- Casks/archiver.rb
+++ Casks/archiver.rb
@@ -3,7 +3,7 @@ cask 'archiver' do
   sha256 :no_check
 
   # googleapis.com/incrediblebee was verified as official when first introduced to the cask
-  url 'https://commondatastorage.googleapis.com/incrediblebee/apps/Archiver/Archiver.zip'
+  url 'http://commondatastorage.googleapis.com/incrediblebee/apps/Archiver-3/Archiver.zip'
   name 'Archiver'
   homepage 'http://archiverapp.com/'
 
