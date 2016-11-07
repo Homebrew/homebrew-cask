9989aef326af6cec9a6481f5d7d7938a9f569bff
Alexander Mancevice <amancevice@cargometrics.com>
2016-11-07 10:52:05 -0500

    min v1.5.0 (#26550)
    updated to `1.5.0`

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,11 +1,11 @@
 cask 'min' do
-  version '1.4.1'
-  sha256 '2fa840f36c946b9a36c1ada237025f4345ad48d8309c79c0b9c4e63ea3f024a7'
+  version '1.5.0'
+  sha256 'b143004d34f2d1f22b3e1bffc38fa5a53fce491e5ffe6639bceeaeaf5f04ede2'
 
   # github.com/minbrowser/min was verified as official when first introduced to the cask
   url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/minbrowser/min/releases.atom',
-          checkpoint: '726031eecd9ac76c8a54b9270fefa4b4b5baf81e58bab8ae1d8dcf499f446f30'
+          checkpoint: 'd9fe31b89a107bec70ad419961cfba3e7dc971ac3946bf67894b9fb99b4a36cc'
   name 'Min'
   homepage 'https://minbrowser.github.io/min'
 
