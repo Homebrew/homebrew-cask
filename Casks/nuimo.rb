e97524ee56998c2685932478a3e39cbb0d56f13d
Anthony Angel <anthony.angel@unrulymedia.com>
2016-11-07 13:45:08 +0000

    Add Nuimo.app v1.7.0

---
+++ Casks/nuimo.rb
@@ -0,0 +1,12 @@
+cask 'nuimo' do
+  version '1.7.0'
+  sha256 'e50c8f751b6944bdbe64f1c01e61d9c4d939fd63850bd4d112883aa46db10f3b'
+
+  url 'https://files.senic.com/nuimo-app-osx-1-7-0.zip'
+  name 'Nuimo'
+  homepage 'https://www.senic.com/en/app'
+
+  auto_updates 'true'
+
+  app 'Nuimo.app'
+end
