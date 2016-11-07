c9f8c74e9ad97a99550a0cfefc6f514a9b5c88fa
Timothy Sutton <tim@synthist.net>
2016-11-07 11:18:38 -0500

    Adding new jtool Cask
    - no versioned download, using latest
    - symlinking manpage manually
    - previous context: https://github.com/Homebrew/homebrew-binary/pull/321

---
+++ Casks/jtool.rb
@@ -0,0 +1,13 @@
+cask 'jtool' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.newosxbook.com/files/jtool.tar'
+  name 'jtool'
+  homepage 'http://newosxbook.com/tools/jtool.html'
+
+  license :gratis
+
+  binary 'jtool'
+  artifact 'jtool.1', target: "#{HOMEBREW_PREFIX}/share/man/man1/jtool.1"
+end
