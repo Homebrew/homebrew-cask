5c10c91b2dba28ba422a2412b1a7b6c25e734692
Raphx <rcxng93@hotmail.com>
2016-11-08 06:44:21 +0800

    Add stanzas to folx v5.1.13629 (#26560)
    - Add auto_updates
    
    - Add zap

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -8,5 +8,18 @@ cask 'folx' do
   name 'Folx'
   homepage 'http://mac.eltima.com/download-manager.html'
 
+  auto_updates true
+
   app 'Folx.app'
+
+  zap delete: [
+                '~/Library/Application Support/Eltima Software/Folx3',
+                '~/Library/Caches/com.eltima.Folx3',
+                '~/Library/Internet Plug-Ins/Folx3Plugin.plugin',
+                '~/Library/Logs/Folx.log',
+                '~/Library/Logs/Folx3.log',
+                '~/Library/Preferences/com.eltima.Folx3.plist',
+                '~/Library/Preferences/com.eltima.FolxAgent.plist',
+                '~/Library/Saved Application State/com.eltima.Folx3.savedState',
+              ]
 end
