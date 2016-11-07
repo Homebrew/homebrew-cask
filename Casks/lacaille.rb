a7cfd902900f424589edd8f88024e223ee951985
Kaito Udagawa <umireon@gmail.com>
2016-11-07 23:32:35 +0900

    New formula: Lacaille 2.2.2 (#26545)

---
+++ Casks/lacaille.rb
@@ -0,0 +1,16 @@
+cask 'lacaille' do
+  version '2.2.2'
+  sha256 '3885a55a4fe1b80f9340271972372ab4b4fe234aad37c27f849e6d78fbd6fcb2'
+
+  url "http://lacaille.jpn.org/Lacaille_#{version}.dmg"
+  appcast 'http://lacaille.jpn.org',
+          checkpoint: '9278d72ed9b5179467c1cf8888fc8d981ba32fc085bad32cf09119cf836c3ec4'
+  name 'Lacaille'
+  homepage 'http://lacaille.jpn.org'
+
+  app 'Lacaille.app'
+
+  uninstall login_item: 'Lacaille'
+
+  zap delete: '~/Library/Preferences/org.jpn.lacaille.Lacaille.plist'
+end
