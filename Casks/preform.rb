70f94e6fc288c973486ab4045f59b273d26eb58a
Jackie Yang <valkjsaaa@users.noreply.github.com>
2016-11-07 01:58:15 -0500

    Upgrade Perform to 2.9.1 (#26542)

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,9 +1,9 @@
 cask 'preform' do
-  version '1.8.1_1'
-  sha256 '51ed43f83821c9ba10a50d315990aa59ede13a64e46bda438a3e113a93385209'
+  version '2.9.1,370'
+  sha256 '62c008c9529f396ae4ce35affd14316f45209e927eec6fad5b32a31b8881bc7e'
 
   # s3.amazonaws.com/FormlabsReleases was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.sub(%r{_.*}, '')}/PreForm_#{version}.dmg"
+  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release_origin_release_#{version.before_comma}_build_#{version.after_comma}.dmg"
   name 'PreForm'
   homepage 'https://formlabs.com/products/preform/'
 
