543d13e2b277ef6226d3eac5a9850113aaf59f28
Vítor Galvão <info@vitorgalvao.com>
2016-11-07 14:23:45 +0000

    Update nuimo.rb

--- Casks/nuimo.rb
+++ Casks/nuimo.rb
@@ -2,7 +2,7 @@ cask 'nuimo' do
   version '1.7.0'
   sha256 'e50c8f751b6944bdbe64f1c01e61d9c4d939fd63850bd4d112883aa46db10f3b'
 
-  url 'https://files.senic.com/nuimo-app-osx-1-7-0.zip'
+  url "https://files.senic.com/nuimo-app-osx-#{version.dots_to_dashes}.zip"
   name 'Nuimo'
   homepage 'https://www.senic.com/en/app'
 
