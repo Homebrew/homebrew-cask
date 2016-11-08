9fc682f9c1ebf7084de74dce9ab416181a128361
claucece <claucece@users.noreply.github.com>
2016-11-08 01:00:12 +0000

    Updated version of coyim cask. (#26568)

--- Casks/coyim.rb
+++ Casks/coyim.rb
@@ -1,6 +1,6 @@
 cask 'coyim' do
-  version '0.3.6'
-  sha256 '12e098ab05a33f9219acecb0eb4265ab35d7c535e33577cc70dc00685ca22417'
+  version '0.3.7_1'
+  sha256 '6e0647e455a2883739799b62bbeb5a9b7a3de6997e87548b525cc11b370a33df'
 
   # bintray.com/twstrike/coyim was verified as official when first introduced to the cask
   url "https://dl.bintray.com/twstrike/coyim/v#{version}/mac-bundle/coyim.dmg"
