7a4c916a6ab46288be039e58da05469b290a581d
Colin Dunn <cgd@vikingdev.net>
2016-11-10 09:42:01 +1100

    Update flash-player to 23.0.0.207 (#26626)

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,11 +1,11 @@
 cask 'flash-player' do
-  version '23.0.0.205'
-  sha256 '446bce8361362e87bdd9f3d4befb76a5a865c236412e8b755481962540077cdb'
+  version '23.0.0.207'
+  sha256 'c43a1bf05268144086b8459ee2ed5c58434d0f8e1b402ade33e532ae396e7089'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
-          checkpoint: '26a40ba2c668486155b9a3bce042a5105d158ddc8a0266f13614e264727ec5c3'
+          checkpoint: '50c4e19caa48710cf812e1549e5179124552b4bf760dc9aa719e09dd86d10fbe'
   name 'Adobe Flash Player projector'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
