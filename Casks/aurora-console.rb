a0b6e383550047210ba4fe07cb324979734418fa
Ri Xu <xuri.me@gmail.com>
2016-11-09 15:46:58 +0800

    Upgrade to version 1.4

--- Casks/aurora-console.rb
+++ Casks/aurora-console.rb
@@ -1,6 +1,6 @@
 cask 'aurora-console' do
-  version '1.2'
-  sha256 'c480e46592d6fb6f025572795bac80357f81b662cefdb3d149c77c723fb9a81f'
+  version '1.4'
+  sha256 '44d3de24bf6e3391b165effb039d5cca75fa428ec6d6cb1f6bddd7ad3bca0319'
 
   url "https://github.com/Luxurioust/aurora/releases/download/#{version}/aurora_darwin_amd64_v#{version}.tar.gz"
   name 'aurora'
