e2a5c586d1747c02f435ee474aa010db4e80abe0
Victor Popkov <victor@popkov.me>
2016-11-09 02:01:24 +0100

    Update screenflow to 6.2 (#26594)

--- Casks/screenflow.rb
+++ Casks/screenflow.rb
@@ -1,10 +1,10 @@
 cask 'screenflow' do
-  version '6.1'
-  sha256 'bb32f1f2eaff52e39d2f2fb9cef623cc2390af0606c9bd6497c12c91a4a6ec24'
+  version '6.2'
+  sha256 '55082a700ceee0e250664b1f15ee61fc2e1befb88bd7308a42ca1cbc9c9e3e96'
 
   url "https://www.telestream.net/download-files/screenflow/#{version.dots_to_hyphens}/ScreenFlow-#{version}.dmg"
   appcast 'https://www.telestream.net/updater/screenflow/appcast.xml',
-          checkpoint: 'aa20375740264cf5ecf4e6ed56a0f258f91631fc95f0e71783793840604ed2b9'
+          checkpoint: '84ddb2c3ddf8729712ae1cdee0f220946af0487b2d56d42e63dfa3edc82f58bd'
   name 'ScreenFlow'
   homepage 'https://www.telestream.net/screenflow/'
 
