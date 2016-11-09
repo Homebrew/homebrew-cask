699573b43486a9f15bb55e00f29b5bf61f86444a
Victor Popkov <victor@popkov.me>
2016-11-09 02:01:11 +0100

    Update proxpn to 4.3.6.7 (#26593)

--- Casks/proxpn.rb
+++ Casks/proxpn.rb
@@ -1,10 +1,10 @@
 cask 'proxpn' do
-  version '4.3.6.6'
-  sha256 'dcb62b781b84a2dabfe77fb4e3a00e00cddf07ecc7ff5c5fcbedd12d86beb8c2'
+  version '4.3.6.7'
+  sha256 '0bb86b819bf4b4ab5a3136b4ee4640a99f21f3cfc211f89dd07813030d7c8bfb'
 
   url "https://www.proxpn.com/updater/proXPN-MacOSX-10.7-#{version}.dmg"
   appcast 'https://www.proxpn.com/updater/appcast.rss',
-          checkpoint: '0e4f4aef85ffe583d9fe32124aa0d8f2ede276b5095b5b961a21039643996e54'
+          checkpoint: '873ccf8cdb7488f1231cb2810e7a4f19d1d2602f11fc6a5e5a9604ebd77383c6'
   name 'proXPN'
   homepage 'https://www.proxpn.com/'
 
