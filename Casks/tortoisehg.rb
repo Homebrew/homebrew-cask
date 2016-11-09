ad252044aa1a42149e5cf905fd76284877a83deb
Jordan Bondo <faithfracture@users.noreply.github.com>
2016-11-09 07:10:09 -0800

    Updated TortoiseHG to v. 4.0.0 (#26610)

--- Casks/tortoisehg.rb
+++ Casks/tortoisehg.rb
@@ -1,6 +1,6 @@
 cask 'tortoisehg' do
-  version '3.9.2'
-  sha256 'f2be4c32f6a2084523d532c6bed35ace574c7862c17a3c4ec9128b3b31b1a5c5'
+  version '4.0.0'
+  sha256 'e16795c608b775cf6d755d4a5273214b65211891c2ac5b76907ecac3ff170717'
 
   url "https://bitbucket.org/tortoisehg/files/downloads/TortoiseHg-#{version}-mac-x64.dmg"
   name 'TortoiseHg'
