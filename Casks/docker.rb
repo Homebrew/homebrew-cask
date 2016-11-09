8480d1b819a38c34efe909d918bfa2d38b36cf77
Victor Popkov <victor@popkov.me>
2016-11-09 21:58:54 +0200

    Update docker to 1.12.3.13776

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -1,10 +1,10 @@
 cask 'docker' do
-  version '1.12.1.12133'
-  sha256 '09570e4bca991cb5fc3b04d8f64835c910942c5b4b35366ffb6f1af90ad0f336'
+  version '1.12.3.13776'
+  sha256 '5e9676cedcd87dd0909bf7007bfba571354f61d728deb52a426a3be105ba3774'
 
   url "https://download.docker.com/mac/stable/#{version}/Docker.dmg"
   appcast 'https://download.docker.com/mac/stable/appcast.xml',
-          checkpoint: '70ba05cde55d1499e124b2a164ff0256294a2ec1793ea1d38462b3a80738b489'
+          checkpoint: '1104809b02843215c4b7acf1108fb958d1491811912c8ef20ab4bc718e82cce2'
   name 'Docker for Mac'
   homepage 'https://www.docker.com/products/docker'
 
