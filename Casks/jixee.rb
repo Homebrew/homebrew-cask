2b227cc7ba34a5ee309a8eed5c7c727432331238
Daniel Wittberger <dan@fredmansky.at>
2016-11-11 11:45:24 +0100

    Add Jixee.app

---
+++ Casks/jixee.rb
@@ -0,0 +1,10 @@
+cask 'jixee' do
+  version '1'
+  sha256 '4e7f37ab3d95f27833e3b18f63bbd6884625fdfe62d10467cb421da9e0d94326'
+
+  url 'https://jixee.me/site/desktop/jixee.dmg'
+  name 'Jixee'
+  homepage 'https://jixee.me/'
+
+  app 'Jixee.app'
+end
