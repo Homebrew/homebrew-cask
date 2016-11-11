bb61aaa8841bab0f4bd351f8461a88059acdd602
Victor Popkov <victor@popkov.me>
2016-11-11 01:12:09 +0100

    Update findings to 1.4.2,3539 (#26658)

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -1,10 +1,10 @@
 cask 'findings' do
-  version '1.4.1,3468'
-  sha256 '620a4fdf08cf31f1ba17e1fb4d941000834d38e3a72f5621bfbf90ca705b39cd'
+  version '1.4.2,3539'
+  sha256 'a85915da38ac0d1d835083f4908ee39b69895c36e1ed8650fb1d65db09abe63a'
 
   url "http://downloads.findingsapp.com/Findings_#{version.after_comma}_#{version.before_comma}.zip"
   appcast 'http://downloads.findingsapp.com/appcast.xml',
-          checkpoint: '03d2cbcf088818ad76fe749a5b87ec4d1231482593ea42020842034832a43ce5'
+          checkpoint: '51548c6d2506ba1413967c96f183853facfef0cf4369fab6e069948340653397'
   name 'Findings'
   homepage 'http://findingsapp.com'
 
