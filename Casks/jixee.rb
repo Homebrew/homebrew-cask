adcf3f1cc4b9f91c6d8b8d8669a28650c9307751
Daniel Wittberger <dan@fredmansky.at>
2016-11-11 13:55:33 +0100

    corrected versioning and hash

--- Casks/jixee.rb
+++ Casks/jixee.rb
@@ -1,6 +1,6 @@
 cask 'jixee' do
-  version '1'
-  sha256 '4e7f37ab3d95f27833e3b18f63bbd6884625fdfe62d10467cb421da9e0d94326'
+  version :latest
+  sha256 :no_check
 
   url 'https://jixee.me/site/desktop/jixee.dmg'
   name 'Jixee'
