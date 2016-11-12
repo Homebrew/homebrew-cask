15656bc916f06d3ea5185d6563218ab40f191cce
Sascha Hagedorn <saesh@users.noreply.github.com>
2016-11-12 13:25:00 +0100

    Update cryptomator to 1.2.1 (#26705)

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask 'cryptomator' do
-  version '1.2.0'
-  sha256 'fa743d7520467f6213e47a6fc058bb7ac7d28f8a3742d49f5bb01677b23df22f'
+  version '1.2.1'
+  sha256 '24aec5c60cc5c391eacf38c8c0096d3e6b5f743741331ad0c84c11c3d7e201e3'
 
   # bintray.com/artifact/download/cryptomator was verified as official when first introduced to the cask
   url "https://bintray.com/artifact/download/cryptomator/cryptomator/Cryptomator-#{version}.dmg"
