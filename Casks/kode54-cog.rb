670b6719d7c579c9d19af40b584999615e0da74f
Jussi T <wickedsp1d3r@users.noreply.github.com>
2016-11-12 19:45:14 +0200

    Update kode54-cog to 0.08,fbe9ae5 (#26708)

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,8 +1,9 @@
 cask 'kode54-cog' do
-  version '0.08,4fdbf35'
-  sha256 '3239ba84b1dd65edbfa816695ffaa0af7ad6dbf6fdc3d77d9807ec2454095cbf'
+  version '0.08,fbe9ae5'
+  sha256 '6a36632a4a0c390614f32b18ca1fd01943b130ee5029dc46ea3627b8b91bf1f6'
 
-  url "https://www.kode54.net/cog/mercury_builds/Cog-#{version.after_comma}.zip"
+  # losno.co/cog was verified as official when first introduced to the cask
+  url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
   name 'Cog'
   homepage 'https://kode54.net/cog/'
 
