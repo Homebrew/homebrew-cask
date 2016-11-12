ea68a299f47ecd50fbaafc3c05962101c538c20d
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-12 08:07:16 +0800

    Update scroll-reverser to 1.7.4 (#26686)

--- Casks/scroll-reverser.rb
+++ Casks/scroll-reverser.rb
@@ -3,10 +3,10 @@ cask 'scroll-reverser' do
     version '1.5.1'
     sha256 '7d31ac1f2a2fbdc06db200da0aeb6ce7fea9ffff712b70ddffbfb769d4fd0f25'
   else
-    version '1.7.3'
-    sha256 'e19f585a201bd8e1fa3ea3acdcf436835c2adfe7869f1888009fe5dab15b130a'
+    version '1.7.4'
+    sha256 '13b4355e8fa34c702cea1430287abffbfde879ad2f5c47c009b06d2353b7b3fd'
     appcast 'https://softwareupdate.pilotmoon.com/update/scrollreverser/appcast.xml',
-            checkpoint: '0ba5505031699c96c955d66f9a983447565f014fc9c0807d731a468069408790'
+            checkpoint: '831aba8ba0fd03adfc4e25444c0ebbe08e4208a62dfe0a34627de8a53dbaf67b'
   end
 
   url "https://pilotmoon.com/downloads/ScrollReverser-#{version}.zip"
