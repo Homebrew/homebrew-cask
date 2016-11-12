89fa9f85ff4f3879a80763459719141686ec79a2
Sascha Hagedorn <mail@shagedorn.net>
2016-11-12 08:52:46 +0100

    Remove trailing whitespace

--- Casks/eurkey.rb
+++ Casks/eurkey.rb
@@ -1,6 +1,6 @@
 cask 'eurkey' do
   version :latest
-  sha256 :no_check 
+  sha256 :no_check
 
   # github.com/jonasdiemer/EurKEY-Mac was verified as official when first introduced to the cask
   url 'https://github.com/jonasdiemer/EurKEY-Mac/archive/master.zip'
