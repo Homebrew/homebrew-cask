3a2a624b1c8f1558a3211478a1517177d0c76449
Christopher LIJLENSTOLPE <github@cdl.asgaard.org>
2016-11-13 03:44:28 -0800

    added indi-control-panel v2.0 (#26730)

---
+++ Casks/indi-control-panel.rb
@@ -0,0 +1,10 @@
+cask 'indi-control-panel' do
+  version '2.0'
+  sha256 '065a338ceba09aceb840666cf043082056dd7636859e23251f77c4e26b0a7980'
+
+  url "http://download.cloudmakers.eu/INDI_Control_Panel_#{version}.dmg"
+  name 'INDI Control Panel'
+  homepage 'http://www.cloudmakers.eu'
+
+  app 'INDI Control Panel.app'
+end
