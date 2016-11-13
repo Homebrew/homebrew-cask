33fc2549247bb041198042002b5a44975872c83d
derekedmond <derekedmond@gmail.com>
2016-11-13 21:59:40 +0000

    Update sabnzbd to 1.1.1 (#26744)

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -1,11 +1,11 @@
 cask 'sabnzbd' do
-  version '1.1.0'
-  sha256 'bad84ec37a2d9e9acd254e50d5a0746001b4d81b1e1fb5186ece2a7c078fe501'
+  version '1.1.1'
+  sha256 '85fee2a2f3f6ecbb7bd3e741710ed47c829e5a35443eaae05ae69a994d80eba8'
 
   # github.com/sabnzbd/sabnzbd was verified as official when first introduced to the cask
   url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-osx.dmg"
   appcast 'https://github.com/sabnzbd/sabnzbd/releases.atom',
-          checkpoint: 'ab70d22f87c7080c02ae3914b4a30563d72fb7c6a628e94ce92d40af60980968'
+          checkpoint: '563da92a4ed4ada3ea49a922a7f952891a06ebde7577dab9ad03ad5544a9f9e2'
   name 'SABnzbd'
   homepage 'https://sabnzbd.org/'
 
