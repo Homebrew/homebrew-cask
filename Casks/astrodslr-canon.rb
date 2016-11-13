b0cbc9c0bbd13c2d15eb25a97e26ca6a07ca080e
Christopher LILJENSTOLPE <cdl@asgaard.org>
2016-11-12 19:29:48 -0800

    Added AstroDSLR for Canon v1.2

---
+++ Casks/astrodslr-canon.rb
@@ -0,0 +1,10 @@
+cask 'astrodslr-canon' do
+  version '1.2'
+  sha256 'd7d3d35c773840ebe00a63821bcf2f925f0e66ad034ce9ee6cb3ff900d17237c'
+
+  url "http://download.cloudmakers.eu/AstroDSLR_#{version}dmg.dmg"
+  name 'AstroDSLR for Canon'
+  homepage 'http://www.cloudmakers.eu'
+
+  app 'AstroDSLR for Canon.app'
+end
