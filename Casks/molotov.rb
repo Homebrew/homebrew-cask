2feaf3632998cb0f62901e92394722ab011da8f8
David Guyon <dguyon@gmail.com>
2016-11-13 18:24:44 +0100

    update molotov to 0.9.6 (#26740)

--- Casks/molotov.rb
+++ Casks/molotov.rb
@@ -1,6 +1,6 @@
 cask 'molotov' do
-  version '0.9.5'
-  sha256 '969775a2a7369d491ee78ae76c5e2be760e47aacaf051cfb9c9633972c084f00'
+  version '0.9.6'
+  sha256 '5a0766725ac85507786a3027db48c9d361eb031933d8b464e85b68991e2128ae'
 
   url "http://desktop-auto-upgrade.molotov.tv/mac/Molotov-v#{version}.dmg"
   name 'Molotov'
