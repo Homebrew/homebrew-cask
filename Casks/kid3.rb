7db43b356f22409a845c934926cc07591451f88b
avinassh <avinassh@users.noreply.github.com>
2016-11-14 18:20:16 +0530

    Update kid3 to 3.4.3 (#26746)

--- Casks/kid3.rb
+++ Casks/kid3.rb
@@ -1,11 +1,11 @@
 cask 'kid3' do
   # note: "3" is not a version number, but an intrinsic part of the product name (ID3 tags)
-  version '3.4.1'
-  sha256 '607d2cdf682d7c8b7df20f08466d4fe1e9f0c27ced66dbe766668752d600173e'
+  version '3.4.3'
+  sha256 'ca36e6fea2e94bdce05c975104a1522270df10e0ef86a7533aad40ab85e9b174'
 
   url "https://downloads.sourceforge.net/kid3/kid3-#{version}-Darwin.dmg"
   appcast 'https://sourceforge.net/projects/kid3/rss',
-          checkpoint: 'f51a382a4f5278a1d43f17c95464ff63d65cb17d61e010a72dabe8c23c8ddd44'
+          checkpoint: '6f52b52de4f5db962effe8341ed4430a2e68adf87668442dcc5eb16b1cce65e0'
   name 'Kid3'
   homepage 'http://kid3.sourceforge.net/'
 
