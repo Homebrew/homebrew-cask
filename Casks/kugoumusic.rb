c1810f33e28ef0badbdb9ec22472837dd1f56fba
Victor Popkov <victor@popkov.me>
2016-11-15 01:18:58 +0200

    Update kugoumusic homepage (https, slash)

--- Casks/kugoumusic.rb
+++ Casks/kugoumusic.rb
@@ -4,7 +4,7 @@ cask 'kugoumusic' do
 
   url 'http://macservice.kugou.com/download/KugouMusicForMac.dmg'
   name 'Kugou Music'
-  homepage 'http://www.kugou.com'
+  homepage 'https://www.kugou.com/'
 
   app 'KugouMusic.app'
 end
