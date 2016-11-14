33ec408977dc3f7e82f43555bed9230f63e74a23
Victor Popkov <victor@popkov.me>
2016-11-15 01:18:46 +0200

    Update baiduhi homepage (https)

--- Casks/baiduhi.rb
+++ Casks/baiduhi.rb
@@ -5,7 +5,7 @@ cask 'baiduhi' do
   url 'http://hiupdate.baidu.com/machi_setup.dmg'
   name 'Baidu Hi'
   name '百度 Hi'
-  homepage 'http://hi.baidu.com/'
+  homepage 'https://hi.baidu.com/'
 
   app '百度Hi.app'
 end
