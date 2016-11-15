ebc7dfe00de2fb5df58dfda4a940bae402aaa9db
Andrey Listochkin <andrey.listochkin@gmail.com>
2016-11-15 16:15:35 +0200

    Add "What's Your Sign?" 1.2.1
    App uses same installer technique as all other Objective-See apps: zip contains an '*_Installer.app' that has to be manually run to install a helper for Finder.

---
+++ Casks/whats-your-sign.rb
@@ -0,0 +1,13 @@
+cask 'whats-your-sign' do
+  version '1.2.1'
+  sha256 'b05363ce2106e23eda95caef1f812fb6cb1dc8f983dee035a4730afe35fc13d6'
+
+  # bitbucket.org/objective-see was verified as official when first introduced to the cask
+  url "https://bitbucket.org/objective-see/deploy/downloads/WhatsYourSign_#{version}.zip"
+  name "What's YourSign?"
+  homepage 'https://objective-see.com/products/whatsyoursign.html'
+
+  installer manual: 'WhatsYourSign_Installer.app'
+
+  uninstall delete: '/Applications/WhatsYourSign.app'
+end
