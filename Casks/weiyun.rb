376017ea4d042ed697410ada2bfde01398b45003
Chiho Sin <ChihoSin@users.noreply.github.com>
2016-11-15 18:36:18 +0800

    Add Weiyun.app v2.0.002.20632 (#26762)

---
+++ Casks/weiyun.rb
@@ -0,0 +1,18 @@
+cask 'weiyun' do
+  version '2.0.002.20632'
+  sha256 'e219143abb8b5cbf1063a28f72398e8747bdec0cd95ad03369edbb16a8dd9043'
+
+  # dldir1.qq.com/weiyun was verified as official when first introduced to the cask
+  url "http://dldir1.qq.com/weiyun/Weiyun_Mac_#{version}.dmg"
+  name 'weiyun'
+  homepage 'http://www.weiyun.com'
+
+  app 'Weiyun.app'
+
+  uninstall quit: 'com.tencent.MacWeiyun'
+
+  zap delete: [
+                '~/Library/Preferences/com.tencent.MacWeiyun.plist',
+                '~/Library/Saved Application State/com.tencent.MacWeiyun.savedState',
+              ]
+end
