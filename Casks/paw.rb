0f7347a4c4f1a75de929e617aee9fc745a139f09
Victor Popkov <victor@popkov.me>
2016-11-17 00:31:25 +0200

    Update paw to 3.0.13,1 (#26829)

--- Casks/paw.rb
+++ Casks/paw.rb
@@ -1,10 +1,10 @@
 cask 'paw' do
-  version '3.0.12,1'
-  sha256 'e12e1d3233afdda71493238a7f45209248ac46d3a17e640e21de9367bb2ea117'
+  version '3.0.13,1'
+  sha256 '1a44e5fbbcdc4cf5e4543c4b980309b99402026359688a613b5aef8d394a5ff9'
 
   url "https://cdn-builds.paw.cloud/paw/Paw-#{version.major_minor_patch}-#{version.major}#{version.minor.rjust(3, '0')}#{version.patch.rjust(3, '0')}#{version.after_comma.rjust(3, '0')}.zip"
   appcast 'https://paw.cloud/api/v2/updates/appcast',
-          checkpoint: 'd8c013df9507469ea98347619d54678373ecd83291174eadbaa40ad9330368c2'
+          checkpoint: 'aa05f0eb9177ded8dd467d60b875f0f27347b93fcbcf0978d9de0d95f5d7b529'
   name 'Paw'
   homepage 'https://paw.cloud'
 
