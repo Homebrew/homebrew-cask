779bbf032209b2166b56a34b1e3e3f5b38715be0
miccal <miccal.matthews@gmail.com>
2016-11-17 05:21:28 +0800

    Update game-capture-hd to 2.5_1119

--- Casks/game-capture-hd.rb
+++ Casks/game-capture-hd.rb
@@ -1,15 +1,18 @@
 cask 'game-capture-hd' do
-  if MacOS.version <= '10.7'
-    version '2.0.3_970'
-    sha256 '9eeb0bb7c00f29aa17a0bf4ff958f43e7ddae035b1390a003b0a43b6585c3542'
-  else
+  if MacOS.version <= '10.10'
     version '2.0.5_983'
     sha256 '4803bcac9069e1e63a89e9053fdf2285487acf9e608e84f7610555075489ad5a'
+
+    url "http://files.elgato.com/gamecapture/gchdm_#{version.no_dots}.dmg"
+  else
+    version '2.5_1119'
+    sha256 '2d45145f1f86445847c07f349d525812cbc67cddce8f5fc3707164df359d6f87'
+
+    url "https://edge.elgato.com/egc/macos/egcm/#{version.major_minor}/final/gchdm_#{version.no_dots}.dmg"
   end
 
-  url "http://files.elgato.com/gamecapture/gchdm_#{version.no_dots}.dmg"
   appcast 'http://updates.elgato.com/autoupdate/gameCapture20.rss?lang=English',
-          checkpoint: 'a1d6aa74e2d6182ae3b0be8b40b72c550824922fc6b97f3a2b01ab88ad9e5606'
+          checkpoint: 'a397c88f22ee03e4616273d34d35b1cc1e01e0b9ffe3603ebf3e5f09cd439e6a'
   name 'Game Capture HD'
   homepage 'https://www.elgato.com/en/gaming/gamecapture-hd'
 
