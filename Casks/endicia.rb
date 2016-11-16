cdf634302fd4a51b4862306f4c2a47f9d5a5e300
Laen <laen@oshpark.com>
2016-11-16 14:32:28 -0800

    Endicia for Mac version 2.16.4.

---
+++ Casks/endicia.rb
@@ -0,0 +1,10 @@
+cask 'endicia' do
+  version '2.16.4'
+  sha256 '19e5944f2e9cef1e7d45b8d53ac386a9a46a6d20c043b6c9818b1388ee8b96c8'
+
+  url 'https://download.endiciaformac.com/EndiciaForMac2164v733.dmg'
+  name 'Endicia for Mac'
+  homepage 'http://mac.endicia.com/'
+
+  app 'Endicia.app'
+end
