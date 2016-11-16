e7f3280855e2b2c152dbcff169dee28c3b7b390f
nuomi1 <nuomi1@qq.com>
2016-11-16 22:26:57 +0800

    Update docker.rb for zap (#26814)
    * Update docker.rb for zap
    
    Update docker.rb for zap
    
    * Update docker.rb for uninstall and zap
    
    Update docker.rb for uninstall and zap
    
    * Update docker.rb for uninstall and zap
    
    Update docker.rb for uninstall and zap

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -11,4 +11,18 @@ cask 'docker' do
   auto_updates true
 
   app 'Docker.app'
+
+  uninstall launchctl: 'com.docker.vmnetd'
+
+  zap delete: [
+                '/Library/PrivilegedHelperTools/com.docker.vmnetd',
+                '~/Library/Application Scripts/com.docker.helper',
+                '~/Library/Caches/KSCrashReports/Docker',
+                '~/Library/Caches/com.docker.docker',
+                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.docker.docker',
+                '~/Library/Containers/com.docker.docker',
+                '~/Library/Containers/com.docker.helper',
+                '~/Library/Group Containers/group.com.docker',
+                '~/Library/Preferences/com.docker.docker.plist',
+              ]
 end
