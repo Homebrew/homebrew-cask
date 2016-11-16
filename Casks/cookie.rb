29f24a8c4db581915f9252679fb6c08bbb52d147
Jussi Tuominen <jussi@COLDwithin.local>
2016-11-16 18:36:53 +0200

    Update cookie to 5.2

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.1.3'
-  sha256 '2e6a6aa0bdf7d9c51c1a9acc644ddd0fd355f9d167d06e2727ba988d8364e70a'
+  version '5.2'
+  sha256 'bbcd265dfd5ae11f694481208c45cc67944e74d2af9a478cd9d3dd727d3969d9'
 
   url "https://sweetpproductions.com/products/cookie#{version.major}/Cookie#{version.major}.zip"
   appcast "https://sweetpproductions.com/products/cookie#{version.major}/appcast.xml",
-          checkpoint: '7a98d53f49fa67c618c421211a0fee137acc9158141f0fcb1d976e90900572de'
+          checkpoint: '6fed71094b02eb77b3fb683f0ee409b9288e56d9e23ff7a74c80b312ea0377ae'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
 
