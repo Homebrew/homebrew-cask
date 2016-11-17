0373fe26a54c516eb5725f49277df3f5e1b0c5db
Artem Medvedev <amedvedev@outlook.com>
2016-11-17 19:25:44 +0200

    Update lingon-x to 4.3 (#26861)

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,10 +1,10 @@
 cask 'lingon-x' do
-  version '4.2.4'
-  sha256 '8a8badc9b653ac59d6909df5fa782d0e2f04f2aacd7a44b70baadb7ce60596f0'
+  version '4.3'
+  sha256 '96d66d13b4f2461c3bfb4b737f519b9478829d232c2bdc33507bcab407f5b15d'
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
   appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}-appcast.xml",
-          checkpoint: '16253916417aebe2d398a6d7b93e102bb41bfde3dae2604291b07b7214527ae0'
+          checkpoint: 'a050e6a7fdbe083fd4f8195ccbeed19872e53f90586e96751384dd920ba80e69'
   name 'Lingon X'
   homepage 'https://www.peterborgapps.com/lingon/'
 
