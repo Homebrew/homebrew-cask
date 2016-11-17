4e962c64b9db83ad198bced1f8d1a3606c36b5ec
neutric <neutric@users.noreply.github.com>
2016-11-17 16:09:06 +0100

    Update syncmate to 6.6.334 (#26855)

--- Casks/syncmate.rb
+++ Casks/syncmate.rb
@@ -1,10 +1,10 @@
 cask 'syncmate' do
-  version '6.6.333'
-  sha256 '72cf1e5ea9a7f5b5c99a2be665109311ab5ec88c1ecfd23e973154b5221453a5'
+  version '6.6.334'
+  sha256 '701b9cbcbdf4899c82c7592816f47b0403b0df448185d3f33daa7d8ee31d3807'
 
   url 'http://www.sync-mac.com/download/syncmate.dmg'
   appcast "http://www.eltima.com/download/syncmate-update/syncmate#{version.major}.xml",
-          checkpoint: '3ed8ba500cf8973c675df4d8c57880a0c12231f3f9f3da7c398a923721158f29'
+          checkpoint: '36abb6bd0123ba9944961df59b58ef9de8fe72d6bc0d0ecc8daf5ff2d0f6a40f'
   name 'SyncMate'
   homepage 'http://www.sync-mac.com'
 
