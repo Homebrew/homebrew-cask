69264e0d452a7a8bad4efd7afee536420e891d10
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-18 12:53:59 +0800

    Update hydrogen.rb

--- Casks/hydrogen.rb
+++ Casks/hydrogen.rb
@@ -3,7 +3,7 @@ cask 'hydrogen' do
   sha256 'daeaa3e7a6fc5ee61a4c59116c90104d2393fd71c8782f136b48bed58d6082db'
 
   # downloads.sourceforge.net/projects/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/projects/hydrogen/files/Hydrogen/#{version.before_comma}%20Binaries/hydrogen-#{version.before_comma}-#{version.after_comma}.dmg/download"
+  url "https://sourceforge.net/projects/hydrogen/files/Hydrogen/#{version.before_comma}%20Binaries/hydrogen-#{version.before_comma}-#{version.after_comma}.dmg/download"
   name 'Hydrogen'
   homepage 'https://www.hydrogen-music.org/hcms/'
 
