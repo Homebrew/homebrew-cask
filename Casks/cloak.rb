2e4e1e5ac5632ba33130a58340fe37e17428fb60
Victor Popkov <victor@popkov.me>
2016-11-18 22:38:07 +0200

    Update cloak to 2.1.2 (#26915)

--- Casks/cloak.rb
+++ Casks/cloak.rb
@@ -1,10 +1,10 @@
 cask 'cloak' do
-  version '2.1.1'
-  sha256 '8c944596df81a20dca347f4b6d67d12e32acb91bfb4366d5e56904f01991bdac'
+  version '2.1.2'
+  sha256 '31386b801cd7677bb33446ab93259ca8ed4c330d2dd88918354360d34588cd09'
 
   url "https://static.getcloak.com/downloads/osx/updates/Release/Cloak-#{version}.dmg"
   appcast 'https://www.getcloak.com/updates/osx/public/',
-          checkpoint: 'd8ac973fcae18b4f4ff205bf22fff17ba40d839fb8afea2846e4e68974416447'
+          checkpoint: 'b3d53389e79d941e79f60fe8f76f6ab95b89e3e76694f5f23662fd897c6f257a'
   name 'Cloak'
   homepage 'https://www.getcloak.com'
 
