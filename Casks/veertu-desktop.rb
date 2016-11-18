c70bbf94880916acfb93651a1fd6f2a057cb249e
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-18 11:35:09 +0800

    Update veertu-desktop to 1.2.93 (#26879)

--- Casks/veertu-desktop.rb
+++ Casks/veertu-desktop.rb
@@ -1,11 +1,11 @@
 cask 'veertu-desktop' do
-  version '1.2.92'
-  sha256 '4c6cd46bc02472c3ef9a26fd2f9808191c0703057a60a7f7002681f471463bd0'
+  version '1.2.93'
+  sha256 '5904e5e82d147fd9b8c37ad14f508883ff62a817a168607f9d6b5ded14629834'
 
   # d2sje6b9huarvp.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2sje6b9huarvp.cloudfront.net/VeertuDesktop-#{version}.dmg"
   appcast 'https://d2sje6b9huarvp.cloudfront.net/vdoaupd.rss',
-          checkpoint: '75b13e0b0ab27a47b873057177b2afd0c6ca7e20466c09e64a60b13febfa3907'
+          checkpoint: '9532f4546c91a58d10ec7802d712ba70e855005ba5102f593ec1889c3ddc82a5'
   name 'Veertu'
   homepage 'https://veertu.com/'
 
