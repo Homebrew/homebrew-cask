805cb8a5187726a86f0f2a38810dc3172433de88
miccal <miccal.matthews@gmail.com>
2016-11-18 08:42:55 +0800

    Add hydrogen 0.9.7-rc1

---
+++ Casks/hydrogen.rb
@@ -0,0 +1,11 @@
+cask 'hydrogen' do
+  version '0.9.7,rc1'
+  sha256 '606a33ad624beecc849f8d651b267ee9cea28dbf7e3aea951b9ad4b48c73b0b3'
+
+  # downloads.sourceforge.net/projects/ was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/projects/hydrogen/files/Hydrogen/#{version.before_comma}%20Binaries/hydrogen-#{version.before_comma}-#{version.after_comma}.dmg"
+  name 'Hydrogen'
+  homepage 'http://www.hydrogen-music.org/hcms/'
+
+  app 'hydrogen.app'
+end
