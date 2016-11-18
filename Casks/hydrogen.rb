374ca2b53253cbf02b8566f46970dca2ef24f793
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-18 19:21:14 +0800

    Update hydrogen.rb

--- Casks/hydrogen.rb
+++ Casks/hydrogen.rb
@@ -4,6 +4,8 @@ cask 'hydrogen' do
 
   # sourceforge.net/projects/hydrogen was verified as official when first introduced to the cask
   url "https://sourceforge.net/projects/hydrogen/files/Hydrogen/#{version.before_comma}%20Binaries/hydrogen-#{version.before_comma}-#{version.after_comma}.dmg/download"
+  appcast "https://sourceforge.net/projects/hydrogen/rss?path=/Hydrogen/#{version.before_comma}%20Binaries",
+          checkpoint: '8800dd8de8f03917ff109f548879775f11b5ae7aceecd75089b91a92f9b28f80'
   name 'Hydrogen'
   homepage 'https://www.hydrogen-music.org/hcms/'
 
