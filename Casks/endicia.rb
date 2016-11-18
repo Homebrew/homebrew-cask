10b5720b725cf94bde0fd5636d3395866a2ef432
Vítor Galvão <info@vitorgalvao.com>
2016-11-18 13:50:30 +0000

    Update endicia.rb

--- Casks/endicia.rb
+++ Casks/endicia.rb
@@ -1,8 +1,8 @@
 cask 'endicia' do
-  version '2.16.4'
+  version '2.16.4v733'
   sha256 '19e5944f2e9cef1e7d45b8d53ac386a9a46a6d20c043b6c9818b1388ee8b96c8'
 
-  url 'https://download.endiciaformac.com/EndiciaForMac2164v733.dmg'
+  url "https://download.endiciaformac.com/EndiciaForMac#{version.no_dots}.dmg"
   name 'Endicia for Mac'
   homepage 'http://mac.endicia.com/'
 
