f6ce4da628c74d5167ff0dc61cbf28794e7bc500
Artem Medvedev <amedvedev@outlook.com>
2016-11-18 19:38:23 +0200

    Update textmate to 2.0-rc.4 (#26910)

--- Casks/textmate.rb
+++ Casks/textmate.rb
@@ -1,11 +1,11 @@
 cask 'textmate' do
-  version '2.0-rc.1'
-  sha256 '744228142e9359dd114233c6acb0b2bb87975b2adcbf073c3ada40117ccc5f0a'
+  version '2.0-rc.4'
+  sha256 '8d956f079494c73a04750fc64018f3ce3680c2f7c6519cc3f67d2baf3943f0ff'
 
   # github.com/textmate/textmate was verified as official when first introduced to the cask
   url "https://github.com/textmate/textmate/releases/download/v#{version}/TextMate_#{version}.tbz"
   appcast 'https://github.com/textmate/textmate/releases.atom',
-          checkpoint: 'ed404d0d0c4bb5895432f527ad5ecad1e000ae3aac1f4980eaf2af8246a2b3e4'
+          checkpoint: 'd844ca42e9860f5cd025dff173e586bb85a417e3eb5a45b87593d1936c179985'
   name 'TextMate'
   homepage 'https://macromates.com/'
 
