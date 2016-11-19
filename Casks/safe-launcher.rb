1d3e3963935c9674217bc9b98585c2843f48ef5e
Christian Wesselhoeft <christian.wesselhoeft@gmail.com>
2016-11-19 05:54:37 -0700

    Update SAFE Launcher to 0.10.0 (#26911)

--- Casks/safe-launcher.rb
+++ Casks/safe-launcher.rb
@@ -1,11 +1,11 @@
 cask 'safe-launcher' do
-  version '0.8.1'
-  sha256 'd0f009761915b8ac32718137ad774213fdd2719e022d5480fcec3caf339eeb5a'
+  version '0.10.0'
+  sha256 '68c4c59f3fa2bd3cbcd51fa109e827ade2fd2227289a46c8df6af487597e378d'
 
   # github.com/maidsafe/safe_launcher was verified as official when first introduced to the cask
-  url "https://github.com/maidsafe/safe_launcher/releases/download/#{version}/safe_launcher-v#{version}-osx-x64.zip"
+  url "https://github.com/maidsafe/safe_launcher/releases/download/#{version}/safe-launcher-v#{version}-osx-x64.zip"
   appcast 'https://github.com/maidsafe/safe_launcher/releases.atom',
-          checkpoint: 'd9786c58d9c64973c510f793d84185cb1cfeeb04c2828c65370b0ec659df27a7'
+          checkpoint: '2c16c1dc4b4a623edee889158be53fd2823181bd4d135e1aaac749e90af9f568'
   name 'SAFE Launcher'
   homepage 'https://www.maidsafe.net/'
 
