5336f15f58314b985ac504f4e232117ceb979539
Artem Medvedev <amedvedev@outlook.com>
2016-11-20 10:23:10 +0200

    Update cookie to 5.2.1 (#26948)

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.2'
-  sha256 'bbcd265dfd5ae11f694481208c45cc67944e74d2af9a478cd9d3dd727d3969d9'
+  version '5.2.1'
+  sha256 'cca57cc80e879297aab1490af0b2034fea56e31dc887c1377acc8c5962b5113b'
 
   url "https://sweetpproductions.com/products/cookie#{version.major}/Cookie#{version.major}.zip"
   appcast "https://sweetpproductions.com/products/cookie#{version.major}/appcast.xml",
-          checkpoint: '6fed71094b02eb77b3fb683f0ee409b9288e56d9e23ff7a74c80b312ea0377ae'
+          checkpoint: 'dc3ae52241c15fedeb65213cfb816a92436b22014b27c5bec708060c3aad71ad'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
 
