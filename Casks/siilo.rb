24bc3a654054c557edf5b5d2b5bd5075a020ca0a
ruuti <miikka.varri@gmail.com>
2016-11-20 23:30:06 +0100

    Add Siilo 0.3.2 Corrected application version number.

--- Casks/siilo.rb
+++ Casks/siilo.rb
@@ -1,5 +1,5 @@
 cask 'siilo' do
-  version '.0.3.2'
+  version '0.3.2'
   sha256 '848f87c8cca1704080ee165174b9362e117a0926c25b29f00f23bdeca4897f75'
 
   url "https://siiloapp.com/releases/siilo-#{version}.zip"
