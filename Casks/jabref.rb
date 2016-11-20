31ced7cf0d9926cd8c1ed92f8256a8eadd61695f
iamthad <thadeus.j.fleming@gmail.com>
2016-11-20 13:16:42 -0600

    JabRef 3.7 (#26961)

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -1,11 +1,11 @@
 cask 'jabref' do
-  version '3.6'
-  sha256 '1eabadccc6eca0c4a7157870509e8d5702351cf9f438d126e25859c92edae52a'
+  version '3.7'
+  sha256 'd5833d9624c2a6f71a41b56bf82b300a5ee58cefc62dc2c29d6fdbe3ad7029c5'
 
   # github.com/JabRef/jabref was verified as official when first introduced to the cask
   url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef_macos_#{version.dots_to_underscores}.dmg"
   appcast 'https://github.com/JabRef/jabref/releases.atom',
-          checkpoint: '0cff3ed91fec9ffd92eb9592882c38f7f630fbb57c17e50fc1bc70f997b77729'
+          checkpoint: 'e9c5d2fa45eb53b586ba0afd00392ba74c09ad8587c70f4588eae82ff35bf9b9'
   name 'JabRef'
   homepage 'https://www.jabref.org/'
 
