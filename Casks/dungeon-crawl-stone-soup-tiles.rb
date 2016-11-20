68295075ef320c8bf84571218da1cd6a326e4a45
Scott Centoni <scentoni@splunk.com>
2016-11-20 12:49:04 -0800

    Updated dungeon-crawl-stone-soup-tiles to version 0.19.0

--- Casks/dungeon-crawl-stone-soup-tiles.rb
+++ Casks/dungeon-crawl-stone-soup-tiles.rb
@@ -1,8 +1,8 @@
 cask 'dungeon-crawl-stone-soup-tiles' do
-  version '0.18.1'
-  sha256 'b6d8b39f1940b71e16c6e2e9cd1a7cf51bfd4306296c3b421bd6b3e0a0104336'
+  version '0.19.0'
+  sha256 'b2c9d2140c16069fd10d352162373180598a1b21dc493a8e3aa0843dec30af02'
 
-  url "https://crawl.develz.org/release/stone_soup-#{version}-tiles-macosx.dmg"
+  url "https://crawl.develz.org/release/stone_soup-#{version}-tiles-macosx.zip"
   name 'Dungeon Crawl Stone Soup'
   homepage 'https://crawl.develz.org'
 
