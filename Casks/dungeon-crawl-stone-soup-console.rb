1a103753ae4c353bb21c14c28a1329aff06862c9
scentoni <scentoni@hotmail.com>
2016-11-20 16:42:21 -0800

    Updated dungeon-crawl-stone-soup-tiles to version 0.19.0 (#26974)
    * Updated dungeon-crawl-stone-soup-console
    
    * Updated dungeon-crawl-stone-soup-tiles to version 0.19.0

--- Casks/dungeon-crawl-stone-soup-console.rb
+++ Casks/dungeon-crawl-stone-soup-console.rb
@@ -1,8 +1,8 @@
 cask 'dungeon-crawl-stone-soup-console' do
-  version '0.18.1'
-  sha256 'c7bb424dd09aed5110ff9d21185a4328ee4cd2ca1cd81c4f38594fd77d1e42ec'
+  version '0.19.0'
+  sha256 'd25d7dbb42cd5ba8e04592d431e7b405b4f22152af85a82580a7a610fcfa6639'
 
-  url "https://crawl.develz.org/release/stone_soup-#{version}-console-macosx.dmg"
+  url "https://crawl.develz.org/release/stone_soup-#{version}-console-macosx.zip"
   name 'Dungeon Crawl Stone Soup'
   homepage 'https://crawl.develz.org'
 
