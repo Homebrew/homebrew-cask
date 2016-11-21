7b7a20992016d4b9130a0891cbdd6c89006e1d96
au <au@nau.re>
2016-11-21 21:18:55 +0100

    Add appcast to deckard.rb

--- Casks/deckard.rb
+++ Casks/deckard.rb
@@ -3,6 +3,8 @@ cask 'deckard' do
   sha256 '005b20436efe21ee99a64ce691c7fbebeface030b3d56cb49282c1ead3da495b'
 
   url "https://www.deckard.ai/download/#{version}.dmg"
+  appcast 'https://www.deckard.ai/releases.html',
+          checkpoint: '3dda621f0da54302086fd3d786641150b5fe1f1feffefc3c4489532d43b220de'
   name 'Deckard Assist'
   homepage 'https://www.deckard.ai/'
 
