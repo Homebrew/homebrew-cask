fac5578114324f3f4bbbc5ea0f87fe6a07fa395d
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-23 13:57:27 +0800

    Update royal-tsx.rb

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -2,7 +2,6 @@ cask 'royal-tsx' do
   version '2.2.9.1000'
   sha256 '7fccf594f56fd2824efef0bc933669ad3ab2f3788224450a65fd2eb493423bf9'
 
-  # royaltsx-v2.royalapplications.com/updates/ was verified as official when first introduced to the cask
   url "https://royaltsx-v2.royalapplications.com/updates/royaltsx_#{version}.dmg"
   appcast "https://royaltsx-v#{version.major}.royalapplications.com/updates_stable.php",
           checkpoint: '24711c531c247279671199e4247695bcbc9c30d3e69254fc22c23df86cde5bcd'
