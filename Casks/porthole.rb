7fb4e7fb7610bf751cfab19b95242c46b2793e6f
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-23 13:03:37 +0800

    Update porthole.rb

--- Casks/porthole.rb
+++ Casks/porthole.rb
@@ -2,6 +2,7 @@ cask 'porthole' do
   version :latest
   sha256 :no_check
 
+  # download.getporthole.com/ was verified as official when first introduced to the cask
   url 'https://download.getporthole.com/Porthole-latest.zip'
   name 'Porthole'
   homepage 'https://www.dangercove.com/porthole/'
