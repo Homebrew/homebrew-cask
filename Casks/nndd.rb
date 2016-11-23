bfe6056305fda814bb92168d591064d6476f7e9a
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-23 13:02:58 +0800

    Update nndd.rb

--- Casks/nndd.rb
+++ Casks/nndd.rb
@@ -2,6 +2,7 @@ cask 'nndd' do
   version '2.4.3,62201'
   sha256 '6a73dcad2e73d877ad1503ed1162cae1a1c84f21d1abaa6aaf9b31bb2fbca531'
 
+  # dl.osdn.jp/nndd/ was verified as official when first introduced to the cask
   url "http://dl.osdn.jp/nndd/#{version.after_comma}/NNDD_v#{version.before_comma.dots_to_underscores}.dmg"
   appcast 'https://osdn.jp/projects/nndd/releases/rss',
           checkpoint: '4f1d23fd9ebe4b3abf7495cf512df2f72f2097b3b4ff51d21e5fa837bb88766b'
