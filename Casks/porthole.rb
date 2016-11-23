2f358e20d207f0c7fb1583e4e2ec4213875cc4d9
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-23 15:13:18 +0800

    Update porthole.rb

--- Casks/porthole.rb
+++ Casks/porthole.rb
@@ -2,8 +2,10 @@ cask 'porthole' do
   version '1.7.9'
   sha256 '805847cfec098073feabccd7f65f448c27c3db14574ac4a94b823458e525ebe3'
 
-  # download.getporthole.com/ was verified as official when first introduced to the cask
+  # download.getporthole.com was verified as official when first introduced to the cask
   url 'https://download.getporthole.com/Porthole-latest.zip'
+  appcast 'https://update.getporthole.com/appcast.rss',
+          checkpoint: 'a3602cb1b9271bafaa09b607760da032f2bedaa6315238a339ae22c607510197'
   name 'Porthole'
   homepage 'https://www.dangercove.com/porthole/'
 
