057008cafbc528fb5d2afd3b53cf557833b5cc10
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-23 15:06:07 +0800

    Update quantum.rb

--- Casks/quantum.rb
+++ Casks/quantum.rb
@@ -2,7 +2,7 @@ cask 'quantum' do
   version :latest
   sha256 :no_check
 
-  # triplecheck.net/download/ was verified as official when first introduced to the cask
+  # triplecheck.net was verified as official when first introduced to the cask
   url 'http://triplecheck.net/download/quantum-mac.zip'
   name 'Quantum'
   homepage 'http://triplecheck.tech/quantum/'
