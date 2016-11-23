e04ffbda9f4104755d69cd0502923a24a139b88b
Jussi <wickedsp1d3r@users.noreply.github.com>
2016-11-23 23:36:18 +0200

    Update yummy-ftp to 1.11.12 (#27071)

--- Casks/yummy-ftp.rb
+++ Casks/yummy-ftp.rb
@@ -1,10 +1,10 @@
 cask 'yummy-ftp' do
-  version '1.11.11'
-  sha256 'f35cf0b30d527a1bb117f72bad29f912a4bd26c779bc1285dd64ed254af14b60'
+  version '1.11.12'
+  sha256 '55f963ba70c2f08d7522f6b657710b07d900bf0fd5dadd5c7f104d40c6b9d5fb'
 
   url 'http://www.yummysoftware.com/downloads/YummyFTP.zip'
   appcast 'http://www.yummysoftware.com/su/yummyftp/feed.xml',
-          checkpoint: '27b108f2c1ba09db26e1dc121810772b0b1629e2177c6695fc1f85980b198962'
+          checkpoint: '70a3e488950656cf6b71fe10165d29b6af2c403e4023274dd084e544f61c49df'
   name 'Yummy FTP'
   homepage 'http://www.yummysoftware.com'
 
