a16adae500bd7e3fe6c5d4261ee39a869c151d0f
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-23 14:27:03 +0800

    Update namecoin.rb

--- Casks/namecoin.rb
+++ Casks/namecoin.rb
@@ -2,7 +2,7 @@ cask 'namecoin' do
   version '0.3.80-a00c33d'
   sha256 '5e2432e384b46615d34c00fbe2bcb0a9cbc4f1cc1a27cd72fbffceebcac7eb95'
 
-  # namecoin.info/files/ was verified as official when first introduced to the cask
+  # namecoin.info was verified as official when first introduced to the cask
   url "https://namecoin.info/files/Namecoin-Qt.app-#{version}.zip"
   name 'Namecoin'
   homepage 'https://namecoin.org/'
