4a217375b968e906b6fb28d56be301b265ab8f2a
Dimitri Jorge <jorge.dimitri@gmail.com>
2016-11-23 14:56:48 +0100

    Create aircall cask

---
+++ Casks/aircall.rb
@@ -0,0 +1,12 @@
+cask 'aircall' do
+  version '1.1.2'
+  sha256 '7c0afc31019dfaa6867bc47cdc8b84033b0d98a00eadefb19a78868d23e7c5dc'
+
+  url 'http://electron.aircall.io/download/latest/osx'
+  name 'Aircall'
+  homepage 'https://aircall.io'
+
+  app 'Aircall.app'
+
+  auto_updates true
+end
