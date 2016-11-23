a6a5d8df6fb6781419d037372227ab38bd6b3a58
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-23 15:07:18 +0800

    Update securid.rb

--- Casks/securid.rb
+++ Casks/securid.rb
@@ -2,7 +2,7 @@ cask 'securid' do
   version '4.1.2'
   sha256 'df0719121911cecb1917e1ca3f2a941b209db57c7585d057c00898bffefa0ec1'
 
-  # ftp.emc.com/pub/agents/ was verified as official when first introduced to the cask
+  # ftp.emc.com was verified as official when first introduced to the cask
   url "ftp://ftp.emc.com/pub/agents/RSASecurIDMac#{version.no_dots}.dmg"
   name 'RSA SecurID'
   homepage 'https://www.rsa.com/en-us/products-services/identity-access-management/securid/software-tokens/software-token-for-mac-os-x'
