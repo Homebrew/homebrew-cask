18c6af8d09f360261031e52af9d265bc0aea808d
Victor Popkov <victor@popkov.me>
2016-11-23 03:03:17 +0200

    Update expandrive to 5.4.2 (#27047)

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,10 +1,10 @@
 cask 'expandrive' do
-  version '5.4.1'
-  sha256 '22a71f16a7108e3090841d97023cfd5480d4e7a852ceda3d70d3566008df3cee'
+  version '5.4.2'
+  sha256 'defcfb288dc46f0ad3ae5543a9c84d20a8efd528374fc1e5f58be97a520b0860'
 
   url "https://updates.expandrive.com/apps/expandrive/v/#{version.dots_to_hyphens}/download.dmg"
   appcast 'https://updates.expandrive.com/appcast/expandrive.xml?version=3',
-          checkpoint: 'dda2d1d5d2533f15c9aa383e3995c94adce333061f6d06f76bba00800500b4c4'
+          checkpoint: '001bb695168f7cb477562f2656f535759fd5bc1e5be24def15d780a2141eb909'
   name 'ExpanDrive'
   homepage 'https://www.expandrive.com/expandrive'
 
