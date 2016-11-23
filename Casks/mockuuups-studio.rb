0455c13af6ecc29d1d7afc4f82f26e548f5c8a88
Thibault Milan <thibault.milan@kpmg.lu>
2016-11-23 12:00:41 +0100

    Add Mockuups Studio Latest

---
+++ Casks/mockuuups-studio.rb
@@ -0,0 +1,10 @@
+cask 'mockuuups-studio' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://mockuuups.studio/download/mac/'
+  name 'Mockuuups Studio'
+  homepage 'https://mockuuups.studio/'
+
+  app 'Mockuuups Studio.app'
+end
