b33e0603c40c4a0026f6b65b300ce38a8ff44e42
Thibault Milan <thibault.milan@kpmg.lu>
2016-11-23 13:53:34 +0100

    wrong type of quotes

--- Casks/mockuuups-studio.rb
+++ Casks/mockuuups-studio.rb
@@ -2,7 +2,7 @@ cask 'mockuuups-studio' do
   version '1.0.0'
   sha256 '1ad632921da0ea2a0443bd8a34aa9ad53b36f8b324303a9971ea3be84bbda1b4'
 
-  url 'https://binaries.mockuuups.com/Mockuuups%20Studio-#{version}.dmg'
+  url "https://binaries.mockuuups.com/Mockuuups%20Studio-#{version}.dmg"
   name 'Mockuuups Studio'
   homepage 'https://mockuuups.studio/'
 
