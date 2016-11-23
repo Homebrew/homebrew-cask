2ac4b2e2d2bb0f094008131071bfe4918fe1d392
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-23 14:28:00 +0800

    Update paymowidget.rb

--- Casks/paymowidget.rb
+++ Casks/paymowidget.rb
@@ -4,7 +4,7 @@ cask 'paymowidget' do
 
   url "https://app.paymoapp.com/desktop-apps/paymowidget/PaymoWidget_mac_#{version}.dmg"
   name 'Paymo Widget'
-  homepage 'https://app.paymoapp.com/auth/login/'
+  homepage 'https://app.paymoapp.com/'
 
   app 'PaymoWidget.app'
 end
