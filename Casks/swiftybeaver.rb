12dae8952860cbc5d08941a2c997c8fc9a0d3db9
Victor Popkov <victor@popkov.me>
2016-11-25 00:01:57 +0200

    Update swiftybeaver to 1.1.0 (#27127)

--- Casks/swiftybeaver.rb
+++ Casks/swiftybeaver.rb
@@ -1,10 +1,10 @@
 cask 'swiftybeaver' do
-  version '1.0.5'
-  sha256 '0d453136582f55737e5540a8929781513d7dd97ece5c120fa7df8a7d8d728b76'
+  version '1.1.0'
+  sha256 '1a12cf1061aac8cb455ebce2898b8165f89c5016b3befdbbe71ebbcef6680f0a'
 
   url "https://cdn.swiftybeaver.com/SBMac-#{version}.zip"
   appcast 'https://cdn.swiftybeaver.com/sbmac-appcast.xml',
-          checkpoint: '759c9911aab9aa3be2877094fdf332615dd32a50485209e5eaf8758a28fd2f29'
+          checkpoint: '565d43c26994964606e69037232c1b30e492e08ae871f3bb5cf576bb7736dd56'
   name 'SwiftyBeaver'
   homepage 'https://swiftybeaver.com/'
 
