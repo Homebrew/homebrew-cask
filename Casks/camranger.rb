9bd011b8c18768fff9bc2fd800839809debcfc74
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-24 23:56:22 +0800

    Update camranger to 1.9.3 (#27101)

--- Casks/camranger.rb
+++ Casks/camranger.rb
@@ -1,10 +1,11 @@
 cask 'camranger' do
-  version '1.8.7'
-  sha256 'da68eeb0a19edf5a1658aee119ca37443dbe9af5d39c45f5fa34c3956fcdf29b'
+  version '1.9.3'
+  sha256 'b8483e82f7d77557be54a7801775671956a93bff5140d7f65a11cfd471ea5cb6'
 
-  url "http://www.camranger.com/downloadFiles/CamRanger_#{version.dots_to_underscores}.dmg"
+  url "http://www.camranger.com/downloadFiles/CamRanger_Mac_#{version.dots_to_underscores}.dmg",
+      user_agent: :fake
   name 'CamRanger'
-  homepage 'https://camranger.com/mac-downloads/'
+  homepage 'http://camranger.com/mac-downloads/'
 
   app 'CamRanger.app'
 end
