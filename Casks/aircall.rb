2a24817b671d7474d4011533adb62bc542b44647
Dimitri Jorge <jorge.dimitri@gmail.com>
2016-11-24 10:36:32 +0100

    Aircall: drop auto-update, use version-specific package url

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -2,11 +2,9 @@ cask 'aircall' do
   version '1.1.2'
   sha256 '7c0afc31019dfaa6867bc47cdc8b84033b0d98a00eadefb19a78868d23e7c5dc'
 
-  url 'http://electron.aircall.io/download/latest/osx'
+  url "http://electron.aircall.io/download/#{version}/osx"
   name 'Aircall'
   homepage 'https://aircall.io'
-
-  auto_updates true
 
   app 'Aircall.app'
 end
