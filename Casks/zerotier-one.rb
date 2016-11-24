e141a35bc3ef7fdf4e26fbcf20c877bc946e20f4
Rusty Ross <consultant@rustyross.com>
2016-11-24 11:52:58 -0800

    Add appcast and update URLs for Zerotier One Cask
    - Add appcast and checkpoint
    - Update url
    - Update homepage
    - Make pkg id explicit (no wildcard)

--- Casks/zerotier-one.rb
+++ Casks/zerotier-one.rb
@@ -2,11 +2,13 @@ cask 'zerotier-one' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.zerotier.com/dist/dist/ZeroTier%20One.pkg'
+  url 'https://download.zerotier.com/dist/ZeroTier%20One.pkg'
+  appcast 'https://github.com/zerotier/ZeroTierOne/releases.atom',
+          checkpoint: '281d0b06265be988b641b1dce843cf3635059e8cd0e5fce9391be1e03c74827d'
   name 'ZeroTier One'
-  homepage 'https://www.zerotier.com'
+  homepage 'https://www.zerotier.com/product-one.shtml'
 
   pkg 'ZeroTier One.pkg'
 
-  uninstall pkgutil: 'com.zerotier.*'
+  uninstall pkgutil: 'com.zerotier.pkg.ZeroTierOne'
 end
