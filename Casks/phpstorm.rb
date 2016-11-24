aa20e1d950748ef2dc31c371b272ccc667dbfbef
Artem Berdyshev <berdartem@gmail.com>
2016-11-24 19:46:40 +0200

    Update PhpStorm version to 2016.3 (#27110)

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -1,6 +1,6 @@
 cask 'phpstorm' do
-  version '2016.2.2'
-  sha256 '9422a5d8733f82f8a77d7da7376a7bf58ca9d170c43dc1392c8377bda5e6711b'
+  version '2016.3'
+  sha256 'e7f4084dab9424822195c1ec5eb5ed7381dd1a9c5264584edb8d63cabbfc7931'
 
   url "https://download.jetbrains.com/webide/PhpStorm-#{version}.dmg"
   name 'JetBrains PhpStorm'
