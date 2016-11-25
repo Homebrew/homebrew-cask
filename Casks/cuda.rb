9e0cf685fea7f364fc58d8afdec273b00bda0d76
Vlad Drahnea <vlad.d@kuende.com>
2016-11-25 19:04:01 +0200

    update cuda version

--- Casks/cuda.rb
+++ Casks/cuda.rb
@@ -1,6 +1,6 @@
 cask 'cuda' do
-  version '8.0.47'
-  sha256 '6866465217e88f7d0c17c202c4aa21281bd56671b863d810efa870d2bbed0a54'
+  version '8.0.55'
+  sha256 'bd9f6a525916ab9c5774a67a89004bbd8e38334f66c37ff2ab4bc82003b7e57d'
 
   url "https://developer.nvidia.com/compute/cuda/#{version.major_minor}/Prod/local_installers/cuda_#{version}_mac-dmg"
   name 'Nvidia CUDA'
