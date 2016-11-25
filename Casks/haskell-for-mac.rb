6306135afac857a8714cad0299977cb3c5b4c81d
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-11-26 01:55:45 +0700

    Use a versioned url in Haskell for Mac

--- Casks/haskell-for-mac.rb
+++ Casks/haskell-for-mac.rb
@@ -1,9 +1,9 @@
 cask 'haskell-for-mac' do
   version '1.3.0'
-  sha256 'e3cc1ab01b4f799bab9e103a1677180caeff72441fb7c68e05c4d5761e5811c6'
+  sha256 '01086dac60c42f113cf25df0ea5ec4adc013722c7634795033ff49d195dfff8f'
 
   # dl.devmate.com/com.haskellformac.Haskell.basic was verified as official when first introduced to the cask
-  url 'https://dl.devmate.com/com.haskellformac.Haskell.basic/Haskell%E2%80%94FunctionalProgrammingLab.dmg'
+  url 'https://dl.devmate.com/com.haskellformac.Haskell.basic/1174/1473667413/Haskell%E2%80%94FunctionalProgrammingLab-1174.zip'
   appcast 'https://updates.devmate.com/com.haskellformac.Haskell.basic.xml',
           checkpoint: '967c75ea27da3a1955b3ca288fa52d4814e8f419770d008025a005ab0157ee83'
   name 'Haskell for Mac'
