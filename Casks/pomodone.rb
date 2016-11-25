853d0f8036654b453af239ab5d999416d35eda53
Stefano Guandalini <stefano.guandalini@buongiorno.com>
2016-11-25 14:41:18 +0100

    Update pomodone to 1.5.856

--- Casks/pomodone.rb
+++ Casks/pomodone.rb
@@ -1,10 +1,10 @@
 cask 'pomodone' do
-  version '1.5.806'
-  sha256 '0e934efef2f9832ff27616153c1d61962f321c16912794271c49ef5672f8516c'
+  version '1.5.856'
+  sha256 '4a51bf6f5b17493545d355101c2407e6b76be40a744a142d330c666636f7ee39'
 
   url "https://app.pomodoneapp.com/PomoDoneApp-#{version}.dmg"
   name 'PomoDone'
   homepage 'https://pomodoneapp.com/'
 
-  app 'PomoDone.app'
+  app 'PomoDoneApp.app'
 end
