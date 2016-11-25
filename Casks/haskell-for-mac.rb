36f4fa4d4755ef8de96b42eed8fa15bc0d53974f
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-11-26 01:46:52 +0700

    Add appcast for Haskell for Mac

--- Casks/haskell-for-mac.rb
+++ Casks/haskell-for-mac.rb
@@ -1,9 +1,11 @@
 cask 'haskell-for-mac' do
-  version :latest
-  sha256 :no_check
+  version '1.3.0'
+  sha256 'e3cc1ab01b4f799bab9e103a1677180caeff72441fb7c68e05c4d5761e5811c6'
 
   # dl.devmate.com/com.haskellformac.Haskell.basic was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.haskellformac.Haskell.basic/Haskell%E2%80%94FunctionalProgrammingLab.dmg'
+  appcast 'https://updates.devmate.com/com.haskellformac.Haskell.basic.xml',
+          checkpoint: '967c75ea27da3a1955b3ca288fa52d4814e8f419770d008025a005ab0157ee83'
   name 'Haskell for Mac'
   homepage 'http://haskellformac.com'
 
