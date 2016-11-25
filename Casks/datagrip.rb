2d78114d40489b118d9afa7a38e5b95139f1718b
Lippy Lee <lippy@lipnet.org>
2016-11-25 10:38:14 +0800

    Update DataGrip to 2016.3 (#27136)

--- Casks/datagrip.rb
+++ Casks/datagrip.rb
@@ -1,6 +1,6 @@
 cask 'datagrip' do
-  version '2016.2.6'
-  sha256 'a9466450233719fdbd83d6457533e72ad71e968da665066f7bb1878ff62651c1'
+  version '2016.3'
+  sha256 '998e037c295aa7849711163ef654d667cf1ef6e12f6530254f24d3d367df399c'
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version}.dmg"
   name 'DataGrip'
