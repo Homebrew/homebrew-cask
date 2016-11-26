99c01b2843ceda296176782e967c4650e274a223
Rezart Qelibari <rezart.q+github@gmail.com>
2016-11-26 08:05:05 +0100

    Add HMA! Pro VPN.app v. 3.2.11.2

---
+++ Casks/hma-pro-vpn.rb
@@ -0,0 +1,14 @@
+cask 'hma-pro-vpn' do
+  version '3.2.11.2'
+  sha256 '77ef3863352702697052dbbec5eff6087a5e2b5f18bde70827e98be748628978'
+
+  # d1llq9kjmh5kle.cloudfront.net/HMA-Pro-VPN-macOS- was verified as official when first introduced to the cask
+  url "https://d1llq9kjmh5kle.cloudfront.net/HMA-Pro-VPN-macOS-#{version}.dmg"
+  name 'HMA! Pro VPN'
+  name 'HideMyAss! Pro VPN'
+  homepage 'https://www.hidemyass.com'
+
+  installer manual: 'Install HMA! Pro VPN.app'
+
+  zap delete: '~/Library/Preferences/com.privax.hmaprovpn.plist'
+end
