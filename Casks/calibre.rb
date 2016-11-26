3bdfd6962a5aed2202ee229b503038b88e86cc27
Aditya Dalal <adidalal@users.noreply.github.com>
2016-11-26 00:59:50 -0500

    Update calibre to 2.73.0 (#27157)

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -3,10 +3,10 @@ cask 'calibre' do
     version '1.48.0'
     sha256 '0533283965fbc9a6618d0b27c85bdf3671fe75ff0e89eeff406fe1457ee61b14'
   else
-    version '2.72.0'
-    sha256 '442cc4865a8024675a8e9f1dcfb0ac791c6e40f3eed8d150acada361f9a9fd59'
+    version '2.73.0'
+    sha256 '00e00b8557d6e29b144538da929af445bd75a38969cf4b7439c3b5696a711f63'
     appcast 'https://github.com/kovidgoyal/calibre/releases.atom',
-            checkpoint: '019fff5e26e15866f4b157c5733abac5c489e7a12c673f60068ddf3a6c2c71c8'
+            checkpoint: '6fde85281bf6b944ea4e0c14e9c44850dc161a1d4519a7ecdab964d4e43777a8'
   end
 
   url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
