4dab2d110aad35690d6322673f1ce6c95fbc698c
Jhen-Jie Hong <iainst0409@gmail.com>
2016-11-26 21:57:12 +0800

    Update react-native-debugger (0.5.4) (#27166)

--- Casks/react-native-debugger.rb
+++ Casks/react-native-debugger.rb
@@ -1,10 +1,10 @@
 cask 'react-native-debugger' do
-  version '0.5.3'
-  sha256 'ba914aa11fef7a7753b5a8a827f13640bdd79935688830be6172586de42af761'
+  version '0.5.4'
+  sha256 'f4ed5d68f133a6db0ea80512136a3ed9b0bbebdad12ea94e35193b693fe66ed1'
 
   url "https://github.com/jhen0409/react-native-debugger/releases/download/v#{version}/rn-debugger-darwin-x64.zip"
   appcast 'https://github.com/jhen0409/react-native-debugger/releases.atom',
-          checkpoint: '7ee7d860def972a7602a341e191ca1367fa138c9946427fe219bdf7e542e3807'
+          checkpoint: '4c2817d0ea75ea72a3716cb4ead3ea890a201e2958099c997d611e7a529d40e8'
   name 'React Native Debugger'
   homepage 'https://github.com/jhen0409/react-native-debugger'
 
