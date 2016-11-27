2f27e40ab3cdef05e1934e8a8a3d64512e7fa5e5
Conor Randall <ibrokemypie@bastardi.net>
2016-11-28 09:24:00 +1100

    Add Pale Moon version 26.5.0
    Adds the app Pale Moon (Newmoon.app) for MacOS. Would like to add version 27 seperately as it is a major rebase that breaks some features.

---
+++ Casks/palemoon.rb
@@ -0,0 +1,10 @@
+cask 'palemoon' do
+  version '26.5.0'
+  sha256 '617581f55863b2ab0f55538791d2c82a06378febffebce14b8d1c1d625f465cb'
+
+  url 'https://mac.palemoon.org/dist/palemoon-26.5.0-131-gstreamer.en-US.mac64.dmg'
+  name 'Pale Moon for Mac'
+  homepage 'https://www.palemoon.org/'
+
+  app 'NewMoon.app'
+end
