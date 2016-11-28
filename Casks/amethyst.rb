d5262cbf0b2904a9053d9efaa4f412565f011a1c
Ian Ynda-Hummel <ianynda@gmail.com>
2016-11-28 15:18:30 -0500

    Update Amethyst to 0.11.0 (#27206)

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -1,15 +1,18 @@
 cask 'amethyst' do
-  if MacOS.version == :mavericks
+  if MacOS.version <= :mavericks
     version '0.9.10'
     sha256 '82adf42ce6031ab59a3072e607788e73f594ad5f21c7118aabc6c5dafe3d0b47'
-  else
+  elsif MacOS.version <= :el_capitan
     version '0.10.1'
     sha256 '9fd1ac2cfb8159b2945a4482046ee6d365353df617f4edbabc4e8cadc448c1e7'
+  else
+    version '0.11.0'
+    sha256 'd0c34de787ff631745b52002616d9ae20b45b845b277eb8af267f77bdbb4324c'
   end
 
   url "https://ianyh.com/amethyst/versions/Amethyst-#{version}.zip"
   appcast 'https://ianyh.com/amethyst/appcast.xml',
-          checkpoint: '92d4615302d5e3ed68a3a187b6709e78080691550517f9b171b3631a6ca45d1c'
+          checkpoint: 'a0e5e9650dbc7af90e7e2fc409d33f7f2ab5aca66e8e41959da6affda2c1dc5c'
   name 'Amethyst'
   homepage 'https://ianyh.com/amethyst/'
 
