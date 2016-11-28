313f186dad1884091790bb1895ff2dcf46c80914
Vítor Galvão <info@vitorgalvao.com>
2016-11-28 11:18:09 +0000

    Update newmoon.rb

--- Casks/newmoon.rb
+++ Casks/newmoon.rb
@@ -3,7 +3,7 @@ cask 'newmoon' do
   sha256 '617581f55863b2ab0f55538791d2c82a06378febffebce14b8d1c1d625f465cb'
 
   url "https://mac.palemoon.org/dist/palemoon-#{version}-gstreamer.en-US.mac64.dmg"
-  name 'Pale Moon for Mac'
+  name 'Pale Moon'
   homepage 'https://www.palemoon.org/'
 
   app 'NewMoon.app'
