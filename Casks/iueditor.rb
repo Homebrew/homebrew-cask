b7c07276d0bc4a53e31e1e50d6987e5c6f9f36ba
Victor Popkov <victor@popkov.me>
2016-11-28 09:28:08 +0200

    Update iueditor homepage (bare_slash)

--- Casks/iueditor.rb
+++ Casks/iueditor.rb
@@ -5,7 +5,7 @@ cask 'iueditor' do
   # iueditor.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://iueditor.s3.amazonaws.com/Release/IUEditorV#{version}.zip"
   name 'JDLab IUEditor'
-  homepage 'http://www.iueditor.org'
+  homepage 'http://www.iueditor.org/'
 
   app 'IUEditor.app'
 
