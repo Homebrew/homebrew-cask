1e0d0061b1c3b5aa2ed34cb5cb829a624ce49f2f
Victor Popkov <victor@popkov.me>
2016-11-28 09:26:33 +0200

    Update hachidori homepage (bare_slash)

--- Casks/hachidori.rb
+++ Casks/hachidori.rb
@@ -7,7 +7,7 @@ cask 'hachidori' do
   appcast 'https://github.com/chikorita157/hachidori/releases.atom',
           checkpoint: 'f72e2b4819b37d77f73496453f1d91398d2f677314c9492b9db3b3c927c8c0e5'
   name 'Hachidori'
-  homepage 'http://hachidori.ateliershiori.moe'
+  homepage 'http://hachidori.ateliershiori.moe/'
 
   app 'Hachidori.app'
 end
