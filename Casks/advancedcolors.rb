2a257b8d95ef83f750e50c0e4f899195bd7bfaeb
Bran Sorem <bransorem@gmail.com>
2016-11-28 22:37:33 -0800

    Add AdvancedColors.app v1.0.0

---
+++ Casks/advancedcolors.rb
@@ -0,0 +1,10 @@
+cask 'advancedcolors' do
+  version '1.0.0'
+  sha256 '1d96d600899130075734c6d65a65b6d7864a48e4308f7f305799fa56a9e42dcf'
+
+  url 'http://advancedcolors.com/AdvancedColors.zip'
+  name 'Advanced Colors'
+  homepage 'http://advancedcolors.com'
+
+  app 'AdvancedColors.app'
+end
