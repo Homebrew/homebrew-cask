cad55f42c14ef1de3b632d922ce375682ec56354
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-29 09:14:33 +0800

    Update trailrunner to 3.8.823 (#27226)

--- Casks/trailrunner.rb
+++ Casks/trailrunner.rb
@@ -1,10 +1,10 @@
 cask 'trailrunner' do
-  version '3.8.822'
-  sha256 '7274f7d396a154c53fd597cd941da39136d3ee15d98c36caab417237eae75c57'
+  version '3.8.823'
+  sha256 '02576d15331a42a8b65a3d824542f1bedc7a2b45f229feb56515cab1f4f910a3'
 
   url 'http://downloads.trailrunnerx.com/TrailRunner.app.zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/83c4086e3f968b874757ba689e71f610',
-          checkpoint: '17cf8c53e27cf35acad1aa9f5194822111255274916b7408c37f5544af0fdc26'
+          checkpoint: '4dbdc369f89142987943bc665218e99ca16d59de12e092a58368a387b0034118'
   name 'TrailRunner'
   homepage 'http://trailrunnerx.com/'
 
