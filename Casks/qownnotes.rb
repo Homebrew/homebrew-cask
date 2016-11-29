91808b7a30a117e1e56080058e22068abac41c04
Eamon Doyle <eamon@cornercase.net>
2016-11-29 13:25:23 -0500

    Create QOwnNotes Cask with v16.11.16

---
+++ Casks/qownnotes.rb
@@ -0,0 +1,11 @@
+cask 'qownnotes' do
+  version '16.11.16'
+  sha256 '98e6546870a8b1202d6839ba0d1819f27b9b7f3ab626840e50135f106f8fe099'
+
+  # github.com was verified as official when first introduced to the cask
+  url 'https://github.com/pbek/QOwnNotes/releases/download/macosx-b2545-185156/QOwnNotes-16.11.16.dmg'
+  name 'QOwnNotes'
+  homepage 'http://www.qownnotes.org/'
+
+  app 'QOwnNotes.app'
+end
