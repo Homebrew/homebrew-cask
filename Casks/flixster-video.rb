b4fb8eb65e7248097be450dbadad6936218bb871
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-30 07:59:23 +0800

    Update flixster-video to 2.7.0.608 (#27251)

--- Casks/flixster-video.rb
+++ Casks/flixster-video.rb
@@ -1,11 +1,11 @@
 cask 'flixster-video' do
-  version '2.7.0.606'
+  version '2.7.0.608'
   sha256 'f9b447fbb938d62eba39618c1f85473915fa20ca01154c10c57784010bb4ed72'
 
   # d1rtylazwb77ux.cloudfront.net was verified as official when first introduced to the cask
   url 'https://d1rtylazwb77ux.cloudfront.net/desktop/mac/FlixsterDesktop.zip'
   appcast 'https://d1rtylazwb77ux.cloudfront.net/desktop/mac/FlixsterDesktopMacAppcast.xml',
-          checkpoint: '70d0513d0f9ccb486129b0a4aa6b728877aa9849adc57a4f3083b953c2598dc1'
+          checkpoint: '0e5e4ab27bdd264491186aa849ab6c134f32316fcac060cb4d1039baac39c593'
   name 'Flixster Video'
   homepage 'https://www.flixster.com/about/ultraviolet/'
 
