bc0870db8b49780a65851bd9e119f7421299c316
Miccal Matthews <miccal.matthews@gmail.com>
2016-11-30 07:59:14 +0800

    Update tokens to 1.5.4 (#27252)

--- Casks/tokens.rb
+++ Casks/tokens.rb
@@ -1,10 +1,10 @@
 cask 'tokens' do
-  version '1.5.3'
-  sha256 'e7653f03f27c3d6053c51f40c79aff71e4ada4111aff43c3f80d2f41add54962'
+  version '1.5.4'
+  sha256 '9b33639cdd9037d7c385432c3e2aabc0cce066002b5f3b767b82257f3cda167b'
 
   url "http://resources.usetokens.com/download/#{version}/Tokens.app.zip"
   appcast 'http://api.usetokens.com/updates/stable',
-          checkpoint: 'c49298d7ef643e3d004164e1b8505ad2d170f387d46e179522ef574df3c1ed61'
+          checkpoint: 'e255134bf321cd7998ac78cb7bf87b29cca4134f840e11d502532b4faa765927'
   name 'Tokens'
   homepage 'http://usetokens.com/'
 
