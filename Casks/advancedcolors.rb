f81960887787f5d5b538f08b4658425f37cc6a12
Vítor Galvão <info@vitorgalvao.com>
2016-11-29 19:47:56 +0000

    Update advancedcolors.rb

--- Casks/advancedcolors.rb
+++ Casks/advancedcolors.rb
@@ -1,6 +1,6 @@
 cask 'advancedcolors' do
-  version '1.0.0'
-  sha256 '1d96d600899130075734c6d65a65b6d7864a48e4308f7f305799fa56a9e42dcf'
+  version :latest
+  sha256 :no_check
 
   url 'http://advancedcolors.com/AdvancedColors.zip'
   name 'Advanced Colors'
