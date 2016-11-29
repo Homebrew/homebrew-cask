a2cd29d394c8b3ca3139b16526e87a3ffa42516f
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2016-11-29 14:24:05 +0100

    Updated Etcher to 1.0.0-beta.17 (#27241)

--- Casks/etcher.rb
+++ Casks/etcher.rb
@@ -1,6 +1,6 @@
 cask 'etcher' do
-  version '1.0.0-beta.16'
-  sha256 '4c6e3ec337e871acbd7913d345e169aabff6b2865fa4cd386e66a0d4e38f060a'
+  version '1.0.0-beta.17'
+  sha256 '648ef2babbbaea6abdf7a680a76afe3f03dbc11780164e890d377c67aec124dc'
 
   # resin-production-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://resin-production-downloads.s3.amazonaws.com/etcher/#{version}/Etcher-#{version}-darwin-x64.dmg"
