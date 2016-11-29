fbf6677db8bca7f5594712880c05668909391ca5
Donny Kurnia <donnykurnia@gmail.com>
2016-11-29 16:44:50 +0700

    Update pinegrow-web-designer to 2.951

--- Casks/pinegrow-web-designer.rb
+++ Casks/pinegrow-web-designer.rb
@@ -1,6 +1,6 @@
 cask 'pinegrow-web-designer' do
-  version '2.81'
-  sha256 '76d14cc1947d97cfb58f9879956ffc8ee04582fbdff3131a6b11f4dabe7c2e25'
+  version '2.951'
+  sha256 'ebad933a306fcf0b0848ece55950e17653799da49ed01100d5d0e5da2c87f69c'
 
   # pinegrow.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://pinegrow.s3.amazonaws.com/PinegrowMac.#{version}.dmg"
