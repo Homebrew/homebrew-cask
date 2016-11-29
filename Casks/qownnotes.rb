9a66e8f5440e21af864abecfd9c31ca513cd5701
Vítor Galvão <info@vitorgalvao.com>
2016-11-29 19:44:17 +0000

    Update qownnotes.rb

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,9 +1,9 @@
 cask 'qownnotes' do
-  version '16.11.16'
+  version '16.11.16,b2545-185156'
   sha256 '98e6546870a8b1202d6839ba0d1819f27b9b7f3ab626840e50135f106f8fe099'
 
-  # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/pbek/QOwnNotes/releases/download/macosx-b2545-185156/QOwnNotes-16.11.16.dmg'
+  # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
+  url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.after_comma}.dmg"
   name 'QOwnNotes'
   homepage 'http://www.qownnotes.org/'
 
