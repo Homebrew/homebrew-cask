f645983cdde515344e5984b973d4cb4a4596b4d4
Jorge Paramo <jrgparamo@utexas.edu>
2016-11-30 17:05:52 -0600

    Fixed url (#27285)

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -2,7 +2,7 @@ cask 'eclipse-platform' do
   version '4.6.1,201609071200'
   sha256 'eb089d46d0b772228a49cc813f965d036a47e1f15a283bd46d2e27880214a7be'
 
-  url "http://download.eclipse.org/eclipse/downloads/drops#{version.major}/R-#{version.before_comma}-#{version.after_comma}/eclipse-SDK-#{version.before_comma}-macosx-cocoa-x86_64.tar.gz&r=1"
+  url "http://download.eclipse.org/eclipse/downloads/drops#{version.major}/R-#{version.before_comma}-#{version.after_comma}/eclipse-SDK-#{version.before_comma}-macosx-cocoa-x86_64.tar.gz"
   name 'Eclipse SDK'
   homepage 'https://eclipse.org/eclipse/'
 
