88f77cb767a2bb6c6a3c32e390bb5ce109741aa9
Artem Medvedev <amedvedev@outlook.com>
2016-11-30 05:44:35 +0200

    Fixed version

--- Casks/resxtreme.rb
+++ Casks/resxtreme.rb
@@ -1,6 +1,6 @@
 cask 'resxtreme' do
-  version '1.0'
-  sha256 'c19f6266412eaf77a7fff5c6e1ad8e1bb86c99160039100c928d1619d29a9b0e'
+  version :latest
+  sha256 :no_check
 
   url 'http://resxtreme.com/download/ResXtreme.zip'
   name 'ResXtreme'
