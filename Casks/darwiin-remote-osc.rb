37dd930942cc7e33ceb584411e3e413651499917
Leandro Facchinetti <me@leafac.com>
2016-11-30 15:16:40 -0500

    Add DarwiinRemoteOSC 0.6

---
+++ Casks/darwiin-remote-osc.rb
@@ -0,0 +1,10 @@
+cask 'darwiin-remote-osc' do
+  version '0.6'
+  sha256 'f98c6c982182ae899af0e1f729120242544e5b3231002802aaef8d565068fcb9'
+
+  url 'http://www.matthiaskronlachner.com/?download=1954'
+  name 'DarwiinRemoteOSC'
+  homepage 'http://www.matthiaskronlachner.com/?p=1574'
+
+  app 'DarwiinRemoteOSC.app'
+end
