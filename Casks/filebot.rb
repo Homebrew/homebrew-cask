8aca497bc149d9b164d5fdf0d59ed24e9428cf3d
Reinhard Pointner <rednoah@users.noreply.github.com>
2016-11-30 20:11:13 +0800

    FileBot 4.7.5 (#27220)

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -1,20 +1,18 @@
 cask 'filebot' do
-  version '4.7.2'
-  sha256 '888d98c63bc8f3a008cdb360f132822878c06dfa3f8fdec9fdacf2ba9855ba26'
+  version '4.7.5'
+  sha256 '51f0fc6a992ee82ee05a7596a36b68f95e5f0306d6b35f5a767ab5aeeecb5e64'
 
   # sourceforge.net/filebot was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/filebot/filebot/FileBot_#{version}/FileBot_#{version}-brew.tar.bz2"
-  appcast 'https://sourceforge.net/projects/filebot/rss?path=/filebot',
-          checkpoint: 'db948fcf10ec2bf554c46e33caaf26a190f642493d7790f7bb282beb6ec34b56'
+  url "https://downloads.sourceforge.net/filebot/filebot/FileBot_#{version}/FileBot_#{version}-darwin.tar.xz"
+  appcast 'https://app.filebot.net/update.xml',
+          checkpoint: '10b885da024848f3d58be9b54f44088aa2dd3f7e714a8b0e3bbef96f2824bb2c'
   name 'FileBot'
   homepage 'https://www.filebot.net/'
 
-  app "FileBot_#{version}-brew.app"
-  binary "#{appdir}/FileBot_#{version}-brew.app/Contents/MacOS/filebot.sh", target: 'filebot'
+  # The darwin package only includes the CLI tools. Launching the app bundle merely redirects to the Mac App Store.
+  binary 'FileBot.app/Contents/MacOS/filebot.sh', target: 'filebot'
 
-  zap delete: [
-                '~/Library/Preferences/net.filebot.ui.plist',
-              ]
+  zap delete: '~/Library/Preferences/net.filebot.ui.plist'
 
   caveats do
     depends_on_java('8')
