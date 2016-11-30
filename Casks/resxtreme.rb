58f8b6d573f85699105cd88de20f022a8de27ca5
Artem Medvedev <amedvedev@outlook.com>
2016-11-30 05:30:33 +0200

    Add ResXtreme.app v1.0

---
+++ Casks/resxtreme.rb
@@ -0,0 +1,10 @@
+cask 'resxtreme' do
+  version '1.0'
+  sha256 'c19f6266412eaf77a7fff5c6e1ad8e1bb86c99160039100c928d1619d29a9b0e'
+
+  url 'http://resxtreme.com/download/ResXtreme.zip'
+  name 'ResXtreme'
+  homepage 'http://resxtreme.com'
+
+  app 'ResXtreme.app'
+end
