58dd1442610f1914fa190ba0b8992e34887eb7e9
Victor Popkov <victor@popkov.me>
2016-12-01 22:35:00 +0200

    Update sketch-toolbox homepage (bare_slash)

--- Casks/sketch-toolbox.rb
+++ Casks/sketch-toolbox.rb
@@ -6,7 +6,7 @@ cask 'sketch-toolbox' do
   appcast 'http://sketchtoolbox.com/updates/appcast.xml',
           checkpoint: '61636d433e619f4b56206366764c85f000715f7741d393ca30c214f36aad6c91'
   name 'Sketch Toolbox'
-  homepage 'http://sketchtoolbox.com'
+  homepage 'http://sketchtoolbox.com/'
 
   app 'Sketch Toolbox.app'
 end
