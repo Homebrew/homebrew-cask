4d23579ab37c1e991fa39c5977f704fd57b9a3df
Artem Medvedev <amedvedev@outlook.com>
2016-12-03 17:10:54 +0200

    Update marked to 2.5.9 (#27345)

--- Casks/marked.rb
+++ Casks/marked.rb
@@ -1,10 +1,10 @@
 cask 'marked' do
-  version '2.5.8'
+  version '2.5.9'
   sha256 '58162745a1cc698611049d4cfdc9354d1b6b42c7f6fdb2a82bfa151c8d1e1931'
 
   url 'http://marked2app.com/download/Marked.zip'
   appcast 'https://updates.marked2app.com/marked.xml',
-          checkpoint: 'd3535eea0a5c3990d26b3ed474d2d8e32add3b6d14b3672b31a5682002db1295'
+          checkpoint: 'caf003f39ca2a87922e2d1f6afe17df55bd6bb6f36dcfd16b52e257cf629f5e0'
   name 'Marked'
   homepage 'http://marked2app.com/'
 
