6b17b43435793173baa6d3d95c8632ed0bb7aac0
Artem Medvedev <amedvedev@outlook.com>
2016-12-03 18:04:54 +0200

    Update murus to 1.4.7 (#27348)

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -1,6 +1,6 @@
 cask 'murus' do
-  version '1.4.4'
-  sha256 '991af45515b74457013f436403632ba97ef9d8755d4ab628966da144e8021a1c'
+  version '1.4.7'
+  sha256 '1e21e9b7cfe4a7c485b9120affe6579c1c7c4c34aa8b616a83e5a3bce48605ae'
 
   url "http://www.murusfirewall.com/downloads/murus-#{version}.zip"
   name 'Murus Firewall'
