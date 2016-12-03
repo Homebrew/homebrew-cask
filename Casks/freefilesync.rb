69f353e1af5f0a207a33bd402d2eed7fac3f2b23
Markus Reiter <me@reitermark.us>
2016-12-04 00:58:07 +0100

    Fix FreeFileSync 8.6.

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -2,8 +2,8 @@ cask 'freefilesync' do
   version '8.6'
   sha256 'aaa1dd1e2dfcc39bf4673d8280cc6561ee8dc9921c4deb926ed336dd5aa1d57b'
 
-  # download2114.mediafire.com/dl0o9adlw80g/bzdxny1n7cxozkh was verified as official when first introduced to the cask.
-  url "https://download2114.mediafire.com/dl0o9adlw80g/bzdxny1n7cxozkh/FreeFileSync_#{version}_macOS.zip"
+  # download1606.mediafire.com/hmk8823hmgqg/bzdxny1n7cxozkh was verified as official when first introduced to the cask.
+  url "https://download1606.mediafire.com/hmk8823hmgqg/bzdxny1n7cxozkh/FreeFileSync_#{version}_macOS.zip"
   name 'FreeFileSync'
   homepage 'http://www.freefilesync.org/'
 
