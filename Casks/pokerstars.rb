20a62afd667968004df36c49d2adf4057b1efa00
Vítor Galvão <info@vitorgalvao.com>
2016-12-04 20:56:19 +0000

    Update pokerstars.rb

--- Casks/pokerstars.rb
+++ Casks/pokerstars.rb
@@ -2,21 +2,25 @@ cask 'pokerstars' do
   version :latest
   sha256 :no_check
 
-  language 'en', default: true do
-    %w[com]
+  language 'US', default: true do
+    %w[.com]
   end
 
+  language 'AU' do
+    %w[.eu/de EU]
+  end
+
+  launguage 'BE' do
+    %w[.be BE]
+  end
+  
   language 'PT' do
-    %w[pt PT]
+    %w[.pt PT]
   end
 
-  language 'eu' do
-    %w[eu EU]
-  end
-
-  url "https://www.pokerstars.#{language[0]}/PokerStars#{language[1]}.app.zip"
+  url "https://www.pokerstars#{language[0]}/PokerStars#{language[1]}.app.zip"
   name 'PokerStars'
-  homepage "https://www.pokerstars.#{language[0]}"
+  homepage "https://www.pokerstars#{language[0]}"
 
   auto_updates true
   container nested: "PokerStars#{language[1]}/PokerStars#{language[1]}.dmg"
