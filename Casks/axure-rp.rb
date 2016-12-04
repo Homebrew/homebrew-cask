a19e8fe63bef7165472c7c7a912a02eb7e0c0516
nuomi1 <nuomi1@qq.com>
2016-12-04 18:13:45 +0800

    Update axure-rp.rb for version, sha256 and zap
    Update axure-rp.rb for version, sha256 and zap

--- Casks/axure-rp.rb
+++ Casks/axure-rp.rb
@@ -1,11 +1,17 @@
 cask 'axure-rp' do
-  version :latest
-  sha256 :no_check
+  version '8.0.0.3322'
+  sha256 'a6a1b836d602693616addbebe9c2bad36f258e256dadb611a685a17813fe643b'
 
   # axure.cachefly.net was verified as official when first introduced to the cask
   url 'https://axure.cachefly.net/AxureRP-Setup.dmg'
   name 'Axure RP'
   homepage 'https://www.axure.com/'
 
-  app 'Axure RP 8.app'
+  app "Axure RP #{version.major}.app"
+
+  zap delete: [
+                "~/Library/Preferences/com.axure.AxureRP#{version.major}.plist",
+                "~/Library/Saved Application State/com.axure.AxureRP#{version.major}.savedState",
+                "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.axure.axurerp#{version.major}.sfl",
+              ]
 end
