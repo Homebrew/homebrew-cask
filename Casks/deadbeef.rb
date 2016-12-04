ff108727875243bd9846cbb5109516ef1bd5eb10
Tommy Levi Morenos <tommylm@T-Mac-Pro.hitronhub.home>
2016-12-03 17:04:19 -0800

    Update deadbeef to 0.7.2 (#27363)

--- Casks/deadbeef.rb
+++ Casks/deadbeef.rb
@@ -1,8 +1,8 @@
 cask 'deadbeef' do
-  version '0.7.2'
-  sha256 '7dc688cca32bf1ef2efbfb4f93c7464b1f935ad71e3d2e35fb06513a615bd7e8'
+  version '0.7.2-2'
+  sha256 'cfac56c37d55ccebf934d3b8c0a2b0ae7acf809e9c5aa5dcf6ff0b5a281a4e5b'
 
-  url 'https://downloads.sourceforge.net/deadbeef/deadbeef-devel-osx-x86_64.zip'
+  url "https://downloads.sourceforge.net/deadbeef/deadbeef-static_#{version}_i686.tar.bz2"
   appcast 'https://sourceforge.net/projects/deadbeef/rss?path=/travis/osx/master',
           checkpoint: 'ed8178e68dd11ce0d03d31f8864e4eaa95cf45112764691f0b82793fcbd4dcb2'
   name 'DeaDBeeF'
