8c567e3768e7e2da3ae1c8a297d68ceae2e93d2d
Ronald Hötschl <rhoetsc@gmail.com>
2016-12-04 10:42:11 +0100

    Upgrade VyprVPN.app to v2.13.1.5133

--- Casks/vyprvpn.rb
+++ Casks/vyprvpn.rb
@@ -1,6 +1,6 @@
 cask 'vyprvpn' do
-  version '2.13.0.5056'
-  sha256 '1a3c76aa8970af5e6597324df2f924a82552e7b021e824980d5944b671e6d9da'
+  version '2.13.1.5133'
+  sha256 'a99bdc5bc316d8a19c3ddd9e51185503f08fe657e01699f49b7beee0f1e5d642'
 
   url "https://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/#{version}/VyprVPN_v#{version}.dmg"
   name 'VyprVPN'
