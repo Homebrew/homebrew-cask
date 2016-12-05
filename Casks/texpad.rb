99eac5b13188d1a5f56c3a30f8252229d4f0d6d4
Vítor Galvão <info@vitorgalvao.com>
2016-12-05 12:26:39 +0000

    Update texpad.rb

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -2,15 +2,17 @@ cask 'texpad' do
   if MacOS.version <= :mountain_lion
     version '1.6.14'
     sha256 '18fcbe93e77e5b5bc848172546962fcde397a26fd543efcc1054004369192f7e'
+
     url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.dots_to_underscores}.zip"
   else
     version '1.7.36,135,846a918'
     sha256 'c1d288cb91e33f90f5e578930a2ec54807ef2bd7a8a235bc92a8c055fcfd79a6'
+
     url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.before_comma.dots_to_underscores}__#{version.after_comma.before_comma}__#{version.after_comma.after_comma}.dmg"
+    appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml',
+            checkpoint: '8f551d3e4cc0b4c543413591e04943e6013b331073b16fc08209bcd3a24051e3'
   end
 
-  appcast 'https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml',
-          checkpoint: '8f551d3e4cc0b4c543413591e04943e6013b331073b16fc08209bcd3a24051e3'
   name 'Texpad'
   homepage 'https://www.texpadapp.com/osx'
 
