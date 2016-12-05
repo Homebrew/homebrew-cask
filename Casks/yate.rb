b9449f613f3c351732dc0c9637e4cb64893dac83
Artem Medvedev <amedvedev@outlook.com>
2016-12-05 14:20:13 +0200

    Update yate to 3.14.3.2 (#27399)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,11 +1,11 @@
 cask 'yate' do
-  version '3.14.3.1'
-  sha256 '4c7cc85e994788f0a427c6cf7ac571537ccc50a97232cdfc9e414142b30a4902'
+  version '3.14.3.2'
+  sha256 'c699e75e3f5990378e3bed4fcef6418eca519abdf93ae2b19a67f38d4209c942'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip',
       using: :post
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '9383d20a3a7eeaa3664ff1a2310aad4d47f60d9ab43fdb32160f0de6bd678162'
+          checkpoint: 'f46d8f0bc2b2a40390be064c76fad4dc73b16f7d9003744ce3431be26363270b'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
 
