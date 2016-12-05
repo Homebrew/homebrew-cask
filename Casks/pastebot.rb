00a63e85640824a2c11ea058bf46db2d5e6dec79
Artem Medvedev <amedvedev@outlook.com>
2016-12-06 00:43:10 +0200

    Update pastebot to 2.0.b15

--- Casks/pastebot.rb
+++ Casks/pastebot.rb
@@ -1,5 +1,5 @@
 cask 'pastebot' do
-  version '2.0.b14'
+  version '2.0.b15'
   sha256 'f20df8b31d2ef47858a0fff216e300edfe34730572f61f062d737e708f8189a2'
 
   # tapbots.net/pastebot was verified as official when first introduced to the cask
