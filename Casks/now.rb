938b9bfca2bad12015a29d3e86e56a58b32552b4
Leo Lamprecht <mindrun@icloud.com>
2016-12-05 14:30:47 +0100

    appcast added

--- Casks/now.rb
+++ Casks/now.rb
@@ -4,6 +4,8 @@ cask 'now' do
 
   # now-auto-updates.now.sh was verified as official when first introduced to the cask
   url 'https://now-auto-updates.now.sh/download/macos'
+  appcast 'https://github.com/zeit/now-desktop/releases.atom',
+          checkpoint: '2fdd5ed203f96a0d7c81f0c6694d2604dae52baf732dbf2a8f2aa1f2826d7761'
   name 'Now'
   homepage 'https://zeit.co/now'
 
