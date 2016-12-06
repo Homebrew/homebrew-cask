37cbeea34182dc7d8b86ce7fd1b65d180b8f4ca2
Yurii Kolesnykov <yurikoles@gmail.com>
2016-12-06 21:57:19 +0200

    Update appcode to 2016.3 (#27440)

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,6 +1,6 @@
 cask 'appcode' do
-  version '2016.2.3'
-  sha256 '2a4fe78e2f57b43dd2193db39538b5ca81945cc4c9b1774877c48c53e38ddfa5'
+  version '2016.3'
+  sha256 '6aaf95a86aaa77160fac5602659d7552aae4bbe525f55a40ae5e79d3b80dca6d'
 
   url "https://download.jetbrains.com/objc/AppCode-#{version}.dmg"
   name 'AppCode'
