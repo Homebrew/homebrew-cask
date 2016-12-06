da43f724561249adf05b322bcd88ae18ef7fd385
Colin Dunn <dunnc@amazon.com>
2016-12-07 09:14:27 +1100

    Update tuxguitar to 1.3.2

--- Casks/tuxguitar.rb
+++ Casks/tuxguitar.rb
@@ -2,7 +2,6 @@ cask 'tuxguitar' do
   version '1.3.2'
   sha256 'c66d038813bb58b8a0a69fc57c6834ac62ede8d47bce5e49e469e64bb6cc4706'
 
-  # sourceforge.net/tuxguitar was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/tuxguitar/tuxguitar-#{version}-macosx-cocoa-64.app.tar.gz"
   appcast 'https://sourceforge.net/projects/tuxguitar/rss?path=/TuxGuitar',
           checkpoint: '671e2f8853920191b1204bc8b14b73caa0dc033a054424787019744f7f1aaa10'
