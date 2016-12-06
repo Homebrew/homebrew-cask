50d83a439f5d666f2c9c80cb25049d9d4163fb8b
Colin Dunn <dunnc@amazon.com>
2016-12-07 09:48:18 +1100

    Update git-it to 4.2.2

--- Casks/git-it.rb
+++ Casks/git-it.rb
@@ -1,10 +1,10 @@
 cask 'git-it' do
-  version '4.2.1'
-  sha256 'b54945eff4fa95b21586b97c829957729f8502b63187f0521391c6316d8bd29b'
+  version '4.2.2'
+  sha256 '636efb126d3c55b1baf7ac15766019a12a4cb78416814205559e29ea1bdfaf94'
 
   url "https://github.com/jlord/git-it-electron/releases/download/#{version}/Git-it-Mac-x64.zip"
   appcast 'https://github.com/jlord/git-it-electron/releases.atom',
-          checkpoint: '0c373b2f4391f9cf2acf0f573f5e35fb60e5e8ab8333e6483ffe6e8e0a49dac5'
+          checkpoint: '6fafdd46968a5d2b551a83b17508a31f18b4a099f262874a61dca3f96c86b9b2'
   name 'Git-it'
   homepage 'https://github.com/jlord/git-it-electron'
 
