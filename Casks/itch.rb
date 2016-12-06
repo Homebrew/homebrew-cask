be0580b2a557f7d7ecdfcaa30be6e2c68a6c7e69
Colin Dunn <cgd@vikingdev.net>
2016-12-06 23:56:42 +1100

    Update itch to 21.0.4 (#27434)

--- Casks/itch.rb
+++ Casks/itch.rb
@@ -1,11 +1,11 @@
 cask 'itch' do
-  version '18.6.1'
-  sha256 '4edf40fa7475a7b79805270d132af7448a6531879d85d8f2a4f5a73e4abbc750'
+  version '21.0.4'
+  sha256 'e9f9534d1f5d546ae37f91be006e850e897a8dc8dcf3a7455ad02732d3935fae'
 
   # github.com/itchio/itch was verified as official when first introduced to the cask
   url "https://github.com/itchio/itch/releases/download/v#{version}/itch-mac.dmg"
   appcast 'https://github.com/itchio/itch/releases.atom',
-          checkpoint: '26a87915c719ebdc7a62a82e821b0af586024887069621aefcb1896052ea44e2'
+          checkpoint: 'ac9fe2cf1674b608e30748749a23d0a2acde99de33cb63f8e51ad18f56bc8cee'
   name 'itch'
   homepage 'https://itch.io/app'
 
