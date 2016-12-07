8a390a471b0d74f060c0542a853b196d8bf56880
Vinoth Ramiah <vin047@gmail.com>
2016-12-07 16:24:19 +0000

    Update polymail to 1.17

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,10 +1,10 @@
 cask 'polymail' do
-  version '1.16'
-  sha256 'd258a5069d60da0b1fcc50d2d5b221a266afcaf478557c0c2f95d9779b8bf1ce'
+  version '1.17'
+  sha256 '9a97fa9fbf5f618f848869251aaee1c55c72fead2aad49ffabbc1df35843c3fc'
 
   url "https://sparkle-updater.polymail.io/osx/builds/Polymail-v#{version.major_minor.no_dots}.zip"
   appcast 'https://sparkle-updater.polymail.io/cast.xml',
-          checkpoint: '054058fe06bf143c32ccac853ad4acd6146179b9d88a9c7aeece0b7f8a272fea'
+          checkpoint: 'c192a3e748b427c7309524623cec6997f67dc60b4e228d8f7c61dc9f5b4b729f'
   name 'Polymail'
   homepage 'https://polymail.io/'
 
