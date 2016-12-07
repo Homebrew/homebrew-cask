cbcd6d95093c88d1d28831bced1fb46247dfc95d
TAKAHASHI Shuuji <shuuji3@gmail.com>
2016-12-07 11:44:35 +0900

    Update brow to 1.0 (#27458)

--- Casks/brow.rb
+++ Casks/brow.rb
@@ -2,7 +2,7 @@ cask 'brow' do
   version '1.0'
   sha256 'f646b89e63853edab7dfae701f10074b0b966f624d2c1e412f0c07cdeeff2eeb'
 
-  url 'https://www.timschroeder.net/brow/brow.zip'
+  url 'https://www.timschroeder.net/files/brow.zip'
   name 'Brow'
   homepage 'https://www.timschroeder.net/brow/'
 
