8aa6603699bab00da45816f63e98a81b409c0cf6
Kamal Fariz Mahyuddin <kamal.fariz@gmail.com>
2016-12-07 18:36:45 -0800

    Update sqlpro-for-postgres to 1.0.99 (#27491)

--- Casks/sqlpro-for-postgres.rb
+++ Casks/sqlpro-for-postgres.rb
@@ -1,6 +1,6 @@
 cask 'sqlpro-for-postgres' do
-  version '1.0.76'
-  sha256 '9132f3e9a9c9df61e00a2bf1e8f8e1348faf73ae1efc0a68c5a64f15884cee1b'
+  version '1.0.99'
+  sha256 'ef61eec75ea9eeb84b34c750519f76503f5d442575cdae86e4725d695f41a329'
 
   # d3fwkemdw8spx3.cloudfront.net/postgres was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/postgres/SQLProPostgres.#{version}.app.zip"
