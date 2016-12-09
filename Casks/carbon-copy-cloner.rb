84df03eb9b5dfe804aeae028b5df5976e8ca30b8
Grettir Asmundarson <grettir@users.noreply.github.com>
2016-12-09 14:48:25 -0700

    Update carbon-copy-cloner to 4.1.12.4485 (#27564)

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -1,6 +1,6 @@
 cask 'carbon-copy-cloner' do
-  version '4.1.10.4428'
-  sha256 '1c590298b6d226dad891242f36a1af868a31ee36b2703aa9654c9768694d1178'
+  version '4.1.12.4485'
+  sha256 '7f3aec364bbe77a166207523d40d313fcf37133df6202e629efac19e2ab46395'
 
   url "https://bombich.com/software/download_ccc_update.php?v=#{version}"
   appcast "https://bombich.com/software/updates/ccc.php?os_minor=11&os_bugfix=#{version.major}&ccc=#{version.after_comma}&beta=0&locale=en",
