8f0463f8c16148e482cc04c7720f4e338d54bec8
Yurii Kolesnykov <yurikoles@gmail.com>
2016-12-09 18:03:44 +0200

    add wine-staging 1.9.23; fixes according to miccal.

---
+++ Casks/wine-staging.rb
@@ -0,0 +1,20 @@
+cask 'wine-staging' do
+  version '1.9.23'
+  sha256 '20b6f450642e3ec2ffc2a170ad5f6d8872b4389960500f1421534385c5ea2ae3'
+
+  url "https://repos.wine-staging.com/macosx/i686/winehq-staging-#{version}.pkg"
+  name 'WineHQ-staging'
+  homepage 'https://www.wine-staging.com/'
+
+  depends_on cask: 'xquartz'
+
+  pkg "winehq-staging-#{version}.pkg"
+
+  uninstall pkgutil: [
+                       'org.winehq.wine-staging',
+                       'org.winehq.wine-staging-deps',
+                       'org.winehq.wine-staging-deps64',
+                       'org.winehq.wine-staging32',
+                       'org.winehq.wine-staging64',
+                     ]
+end
