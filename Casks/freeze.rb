951669db01cda95662c1050465a9cd06c58852ce
Sebastian Volland <seb@baunz.net>
2016-12-10 15:07:14 +0100

    Update freeze to 2.8-235

--- Casks/freeze.rb
+++ Casks/freeze.rb
@@ -1,10 +1,10 @@
 cask 'freeze' do
-  version '2.7-233'
-  sha256 '9d995ff91896d523854879fca756347d329a7be7feb058dc277608f1fea8af42'
+  version '2.8-235'
+  sha256 'a7e0823f2698a3a2547d8a0958458da9de443e38df2079fbde1e6c641c29e289'
 
   url "https://www.freezeapp.net/download/Freeze-#{version}.zip"
   appcast 'https://www.freezeapp.net/appcast.xml',
-          checkpoint: '10dbe8d09826aa2540d2206ab4f3ee2b5ed69f4a115120a125cc1a234bcc5f83'
+          checkpoint: 'cbbe7f0a9ad40f27c60f8fab8806154cefebb18a56e0688e5c74aa7d346f3311'
   name 'Freeze'
   homepage 'https://www.freezeapp.net/'
 
