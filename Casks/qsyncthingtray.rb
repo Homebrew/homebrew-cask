e9674839384c474a6ad5b89f9eacdc34db311611
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-11 20:33:24 +0800

    Update qsyncthingtray to 0.5.6 (#27633)

--- Casks/qsyncthingtray.rb
+++ Casks/qsyncthingtray.rb
@@ -1,10 +1,10 @@
 cask 'qsyncthingtray' do
-  version '0.5.2'
-  sha256 '5419b2e4040f2fa347e9b173a54462a4885f9f145c72baefd988065cdd91fa7e'
+  version '0.5.6'
+  sha256 '30ffe15479173986c594677504eb6ba76cfdbf710cf7a2e52ad459f31d31d205'
 
   url "https://github.com/sieren/QSyncthingTray/releases/download/#{version}/QSyncthingTray_#{version}_MAC.dmg"
   appcast 'https://github.com/sieren/QSyncthingTray/releases.atom',
-          checkpoint: '05524e57a102be842841cb8f07504d742595fb63631bbef89e3dbe8c5fdfb251'
+          checkpoint: 'b71f1ff7715d8d10b2695d4202c33eb65da564c190a55e5601bb2f187a7500fb'
   name 'QSyncthingTray'
   homepage 'https://github.com/sieren/QSyncthingTray'
 
