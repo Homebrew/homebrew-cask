e2d79c0883ab9f44961c93132b32772e93e109ae
miccal <miccal.matthews@gmail.com>
2016-12-11 19:35:34 +0800

    Update psychopy to 1.84.2

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,10 +1,10 @@
 cask 'psychopy' do
-  version '1.83.04'
-  sha256 '01a4fdf1d087d408b195d7a10cc406030623bbfce72bd85e6b9647ace9084960'
+  version '1.84.2'
+  sha256 '61d56a3c894fd75a8e23b4ca59af0bb7c172ee9b3713b1dd27f0169ccf186adb'
 
   url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy-#{version}-OSX_64bit.dmg"
   appcast 'https://github.com/psychopy/psychopy/releases.atom',
-          checkpoint: 'cae3f1e417eeadfe1203dd34a150bca64bc78fea6ae9649491bbc113ed5800da'
+          checkpoint: 'f901e06f8e03c312d524c51a479c84e0939a1c6b75d62a7509af39ebab194a8a'
   name 'PsychoPy'
   homepage 'https://github.com/psychopy/psychopy'
 
