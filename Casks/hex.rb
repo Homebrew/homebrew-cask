4884be9990f79603ca3f2b96269733dff0492bf2
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-11 11:17:46 +0800

    Update hex.rb

--- Casks/hex.rb
+++ Casks/hex.rb
@@ -2,7 +2,6 @@ cask 'hex' do
   version :latest
   sha256 :no_check
 
-  # dl.hex.gameforge.com was verified as official when first introduced to the cask
   url 'http://dl.hex.gameforge.com/HexInstaller.dmg'
   name 'HEX'
   homepage 'https://www.hex.gameforge.com/'
