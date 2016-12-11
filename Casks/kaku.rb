1be5ace8b969553f55b23d60b2a4e8bd1a6fd553
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-11 16:12:32 +0800

    Update kaku to 1.8.5 (#27604)

--- Casks/kaku.rb
+++ Casks/kaku.rb
@@ -1,11 +1,11 @@
 cask 'kaku' do
-  version '1.8.0'
-  sha256 '2843de576b2e7d2647eddea53f1ca6ed0e5c1b9293f7aec0829bc400fdd81f08'
+  version '1.8.5'
+  sha256 '7610ba4a10126127b919bb917fb5d1854286a9ae76e22df8660284857cdf781f'
 
   # github.com/EragonJ/Kaku was verified as official when first introduced to the cask
   url "https://github.com/EragonJ/Kaku/releases/download/#{version}/Kaku-#{version}.dmg"
   appcast 'https://github.com/EragonJ/Kaku/releases.atom',
-          checkpoint: 'ba0caf830db975e86fb14a85a2400fa91de98666c12dcef59afacb4cbd5533aa'
+          checkpoint: '39cfe15ddd86b8120075c0afba93d5f8befc63aff093f6d486efea97df03528c'
   name 'Kaku'
   homepage 'http://kaku.rocks/'
 
