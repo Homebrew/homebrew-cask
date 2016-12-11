aac3f09526c823631dfafe27b2da108cd3cad570
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-11 12:08:43 +0800

    Update streamlink-twitch-gui.rb

--- Casks/streamlink-twitch-gui.rb
+++ Casks/streamlink-twitch-gui.rb
@@ -5,7 +5,7 @@ cask 'streamlink-twitch-gui' do
   url "https://github.com/streamlink/streamlink-twitch-gui/releases/download/v#{version}/streamlink-twitch-gui-v#{version}-macOS.tar.gz"
   appcast 'https://github.com/streamlink/streamlink-twitch-gui/releases.atom',
           checkpoint: '51a457a6201c9fde3d502b32e680f62befdf9d57db91ee87f1858b647d8870b9'
-  name 'Livestreamer Twitch GUI'
+  name 'Streamlink Twitch GUI'
   homepage 'https://github.com/streamlink/streamlink-twitch-gui/'
 
   depends_on cask: 'vlc'
