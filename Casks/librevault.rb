4298542514adbbb3518ba881426e0fbe2a50784a
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-11 16:41:17 +0800

    Update librevault to 0.1.19 (#27606)

--- Casks/librevault.rb
+++ Casks/librevault.rb
@@ -1,10 +1,10 @@
 cask 'librevault' do
-  version '0.1.18.7'
-  sha256 '9a825fbf1e40130bb95a7c26e7981fd5317442c2087e3031fda803f66fa8a97a'
+  version '0.1.19'
+  sha256 '3ddd7451a8220ee57a8e50cce076bf163c95911eb06e0930a57c902079bd9c23'
 
   url "https://releases.librevault.com/mac/librevault_v#{version}.dmg"
   appcast 'https://github.com/Librevault/librevault/releases.atom',
-          checkpoint: '7bb5563f93f7b3deed9a0875b008ab4d46b58000aca379dff5d8a40b9d90bc5d'
+          checkpoint: '477d27ceb07eee4ac513830da666a278c4e89557c1d5b143627b239a6b0396ba'
   name 'Librevault'
   homepage 'https://librevault.com/'
 
