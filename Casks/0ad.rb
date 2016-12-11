25dcc8136dbea4961f32d0f7f705b9ece61c4b17
Yurii Kolesnykov <yurikoles@gmail.com>
2016-12-12 01:57:16 +0200

    fix path for 0ad; fix bad download server.

--- Casks/0ad.rb
+++ Casks/0ad.rb
@@ -3,9 +3,9 @@ cask '0ad' do
   sha256 '4f616798252e4c814262e25b7d7e714619a6f0897c8a44a6a128766901d8b25d'
 
   # wildfiregames.com was verified as official when first introduced to the cask
-  url "https://releases.wildfiregames.com/0ad-#{version}-osx64.dmg"
+  url "http://releases.wildfiregames.com/0ad-#{version}-osx64.dmg"
   name '0 A.D.'
   homepage 'https://play0ad.com/'
 
-  app '0ad.app'
+  app '0 A.D..app'
 end
