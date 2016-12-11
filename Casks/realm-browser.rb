0cd013593f0df3e152298a9bcda8da3779b7f876
miccal <miccal.matthews@gmail.com>
2016-12-12 07:53:12 +0800

    Update realm-browser to 2.1.1

--- Casks/realm-browser.rb
+++ Casks/realm-browser.rb
@@ -1,10 +1,10 @@
 cask 'realm-browser' do
-  version '2.0.0'
-  sha256 '9e1e6493829e960282d877e8a82eae244370422c578c46ba7dbd975a79f07772'
+  version '2.1.1'
+  sha256 'e1daafd250cc4df1695e2f5809e78d383faef45d1c862905af0b3588b0ad90e4'
 
   url "https://github.com/realm/realm-browser-osx/releases/download/v#{version}/RealmBrowser-#{version}.zip"
   appcast 'https://github.com/realm/realm-browser-osx/releases.atom',
-          checkpoint: '47f78f20dbd0320e1f9f584b86fcac736f14e2c100eb8bd4bfc2f9000552b64f'
+          checkpoint: 'a5f183540d2e264640d13abd4f58a049198e0117f6543563715b5fc939da8a19'
   name 'Realm Browser'
   homepage 'https://github.com/realm/realm-browser-osx/'
 
