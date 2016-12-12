7e1e8c93f057d71bf8d7a37090319dde838925d5
Ivan Kukharchuk <ikukharchuk@gmail.com>
2016-12-12 15:01:36 +0900

    Added ScreenSleeves screensaver v5.3

---
+++ Casks/screensleeves.rb
@@ -0,0 +1,10 @@
+cask 'screensleeves' do
+  version '5.3'
+  sha256 'cc868a3ae9fcd6ddc2f47282a01823377ff3fa6696499ccbf0895423f2f542b8'
+
+  url 'http://peacockmedia.software/mac/screensleeves/screensleeves.dmg'
+  name 'ScreenSleeves screensaver (free version)'
+  homepage 'http://peacockmedia.software/mac/screensleeves/'
+
+  screen_saver 'ScreenSleeves.saver'
+end
