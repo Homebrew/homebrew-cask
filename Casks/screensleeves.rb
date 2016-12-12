da32348b879d8cc2e680d087b22d02423fa91fbe
Ivan Kukharchuk <ikukharchuk@gmail.com>
2016-12-12 15:37:13 +0900

    Fixing screensleeves v5.3
    Removed version and sha256 check, changed name.

--- Casks/screensleeves.rb
+++ Casks/screensleeves.rb
@@ -1,9 +1,9 @@
 cask 'screensleeves' do
-  version '5.3'
-  sha256 'cc868a3ae9fcd6ddc2f47282a01823377ff3fa6696499ccbf0895423f2f542b8'
+  version :latest
+  sha256 :no_check
 
   url 'http://peacockmedia.software/mac/screensleeves/screensleeves.dmg'
-  name 'ScreenSleeves screensaver (free version)'
+  name 'ScreenSleeves screensaver'
   homepage 'http://peacockmedia.software/mac/screensleeves/'
 
   screen_saver 'ScreenSleeves.saver'
