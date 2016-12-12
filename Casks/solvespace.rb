61589e1e92d227b560da227bc851ea088ca1f31c
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-12 08:16:07 +0800

    Update solvespace to 2.2 (#27649)

--- Casks/solvespace.rb
+++ Casks/solvespace.rb
@@ -1,11 +1,11 @@
 cask 'solvespace' do
-  version '2.1'
-  sha256 '68255640cc291b044cf69fa67650590583623a331b2b25d23649f6c638ae559e'
+  version '2.2'
+  sha256 '58b3577f27255a48f8cdb6e56efab54e6f8625137903d7af2aaf2a150b05d72b'
 
   # github.com was verified as official when first introduced to the cask
   url "https://github.com/solvespace/solvespace/releases/download/v#{version}/solvespace.dmg"
   appcast 'https://github.com/solvespace/solvespace/releases.atom',
-          checkpoint: 'f81fcca30d49cc7b8c442d10ed814b8fb12f8dfe60369b4c48294904d931ab69'
+          checkpoint: '14d5efb3a103d1ef45cbbf1911b99d91f849cd1cfc7354989e349b83940c0a97'
   name 'SolveSpace'
   homepage 'http://solvespace.com/index.pl'
 
