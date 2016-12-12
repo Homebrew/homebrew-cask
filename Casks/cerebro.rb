d73d52ef7120fb4907758842e693623f81b3525e
Alexandr Subbotin <kelionweb@gmail.com>
2016-12-12 18:14:43 +0100

    Add Cerebro.app v0.1.0

---
+++ Casks/cerebro.rb
@@ -0,0 +1,13 @@
+cask 'cerebro' do
+  version '0.1.0'
+  sha256 '0a2acec0d520cd632172c00cabd0a160184dcbc29302eae28478832c930021ca'
+
+  # github.com/KELiON/cerebro was verified as official when first introduced to the cask
+  url "https://github.com/KELiON/cerebro/releases/download/#{version}/Cerebro.dmg"
+  appcast 'https://github.com/KELiON/cerebro/releases.atom',
+          checkpoint: 'd0a372db626b46ceb8125c351c69a2733bc84fe82772bda007b91a0f649a1561'
+  name 'Cerebro'
+  homepage 'https://cerebroapp.com'
+
+  app 'Cerebro.app'
+end
