dcbe94489262c8408feb1a8379531c1ceadb93bd
miccal <miccal.matthews@gmail.com>
2016-12-12 08:01:05 +0800

    Update shuttle to 1.2.9

--- Casks/shuttle.rb
+++ Casks/shuttle.rb
@@ -1,11 +1,11 @@
 cask 'shuttle' do
-  version '1.2.7'
-  sha256 '529f0c33d272a112aac3c99d023c615fbe704e6132377599c1eb1c38aaad80da'
+  version '1.2.9'
+  sha256 '0b80bf62922291da391098f979683e69cc7b65c4bdb986a431e3f1d9175fba20'
 
   # github.com/fitztrev/shuttle was verified as official when first introduced to the cask
   url "https://github.com/fitztrev/shuttle/releases/download/v#{version}/Shuttle.zip"
   appcast 'https://github.com/fitztrev/shuttle/releases.atom',
-          checkpoint: '7c389e357ae08cb0f7d9e20108fe239da609514d09c1fbcb94192998f570b6c5'
+          checkpoint: '450d58e7a2032148d809afe3bc6dd4cb7b0fd0bb807855fa17f452c57ace3a3b'
   name 'Shuttle'
   homepage 'https://fitztrev.github.io/shuttle/'
 
