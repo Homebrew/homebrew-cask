96cd7ac7035bd0f4713da381b739e5734000be57
Nick Parsons <nparsons08@gmail.com>
2016-12-12 16:40:13 -0700

    Update winds.rb

--- Casks/winds.rb
+++ Casks/winds.rb
@@ -3,7 +3,7 @@ cask 'winds' do
   sha256 'f3d97376a9f060c6d4c1b3f32383332cfa4a96c294aed760306111759adc292b'
 
   # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/GetStream/Winds/releases/download/v0.3.0/Winds.zip'
+  url 'https://github.com/GetStream/Winds/releases/download/v#{version}/Winds.zip'
   name 'Winds'
   homepage 'https://winds.getstream.io'
 
