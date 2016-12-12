6d07c6eec88b69bbbb1f144accdc811e4f74c7a7
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-12 08:16:45 +0800

    Update spechtlite to 0.8.1 (#27651)

--- Casks/spechtlite.rb
+++ Casks/spechtlite.rb
@@ -1,10 +1,10 @@
 cask 'spechtlite' do
-  version '0.4.9'
-  sha256 '9b302e73adab8ae39e51fee0b16803fee67c92add0d6b50b676a5fce50bfc527'
+  version '0.8.1'
+  sha256 '302ca8194c2787eb8d89a6b7a7bd8624ce3f3fd844bf21005d0c8d2d0031241d'
 
   url "https://github.com/zhuhaow/SpechtLite/releases/download/#{version}/SpechtLite.zip"
   appcast 'https://github.com/zhuhaow/SpechtLite/releases.atom',
-          checkpoint: '02b227695791677acf7cd531eba12bc2514c7327cda9044aa9e07dcf0124541a'
+          checkpoint: 'fe837c2b13fd661be485639228ad98b9416fc3acea9ae1a91baaf31f289890f7'
   name 'SpechtLite'
   homepage 'https://github.com/zhuhaow/SpechtLite'
 
