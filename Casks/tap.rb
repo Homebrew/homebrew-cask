67a637fa1407ea4070102497cd66a378cf212bed
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-14 07:01:07 +0800

    Update tap.rb

--- Casks/tap.rb
+++ Casks/tap.rb
@@ -3,9 +3,10 @@ cask 'tap' do
   sha256 :no_check
 
   if MacOS.version <= :mavericks
-    # dl.devmate.com was verified as official when first introduced to the cask
+    # dl.devmate.com/com.pabix.tap-mountain was verified as official when first introduced to the cask
     url 'https://dl.devmate.com/com.pabix.tap-mountain/Tap-mountain.dmg'
   else
+    # dl.devmate.com/com.pabix.tap was verified as official when first introduced to the cask
     url 'https://dl.devmate.com/com.pabix.tap/Tap.dmg'
   end
 
