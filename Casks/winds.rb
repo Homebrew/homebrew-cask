031d22424153ee7a6797e5fc62cc040278e80b6c
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-13 13:34:09 +0800

    Update winds.rb

--- Casks/winds.rb
+++ Casks/winds.rb
@@ -4,7 +4,7 @@ cask 'winds' do
 
   # github.com/GetStream/Winds was verified as official when first introduced to the cask
   url "https://github.com/GetStream/Winds/releases/download/v#{version}/Winds.zip"
-  appcast 'https://github.com/GetStream/Winds/releases.atom'
+  appcast 'https://github.com/GetStream/Winds/releases.atom',
           checkpoint: '0bbe523d705c737a8fd063038d2fc299af8717a449c6b57fb644185824da2060'
   name 'Winds'
   homepage 'https://winds.getstream.io'
