ebccc7a131fcbd46138162ae2df7e6a7b4bc1dc7
Artem Medvedev <amedvedev@outlook.com>
2016-12-13 14:31:49 +0200

    Update inkdrop to 2.6.2

--- Casks/inkdrop.rb
+++ Casks/inkdrop.rb
@@ -1,6 +1,6 @@
 cask 'inkdrop' do
-  version '0.11.0'
-  sha256 '23b47aa14a6531b3daf1ce96244b88255e43627a9b450b84220e1bfb70f095f5'
+  version '2.6.2'
+  sha256 'b6fb511c488dc8ca4d03489b1198e07445164a6066998af136a926ad8e5fe666'
 
   url "https://www.inkdrop.info/api/update/Inkdrop-#{version}-Mac.zip"
   name 'Inkdrop'
