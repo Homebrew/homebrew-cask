529bf11b4e8a8837f6832517734c6d1dd8d0889f
Colin Dunn <dunnc@amazon.com>
2016-12-13 11:43:20 +1100

    Update 010-editor to 7.0.2

--- Casks/010-editor.rb
+++ Casks/010-editor.rb
@@ -1,8 +1,8 @@
 cask '010-editor' do
-  version :latest
-  sha256 :no_check
+  version '7.0.2'
+  sha256 'd83c7a34151eba65946ad6dcd9459cd1a83e618211ea8db99b5c35361b544f46'
 
-  url 'http://www.sweetscape.com/download/010EditorMacInstaller.dmg'
+  url "http://download.sweetscape.com/010EditorMac64Installer#{version.no_dots}.dmg"
   name '010 Editor'
   homepage 'https://www.sweetscape.com/'
 
