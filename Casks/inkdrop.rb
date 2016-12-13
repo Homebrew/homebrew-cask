a954136bf98e1cbeb0219186b6a7ca1d576608f0
Artem Medvedev <amedvedev@outlook.com>
2016-12-13 15:20:33 +0200

    Fixes

--- Casks/inkdrop.rb
+++ Casks/inkdrop.rb
@@ -2,9 +2,19 @@ cask 'inkdrop' do
   version '2.6.2'
   sha256 'b6fb511c488dc8ca4d03489b1198e07445164a6066998af136a926ad8e5fe666'
 
-  url "https://www.inkdrop.info/api/update/Inkdrop-#{version}-Mac.zip"
+  url "https://github.com/inkdropapp/releases/releases/download/v#{version}/Inkdrop-#{version}-Mac.zip"
+  appcast 'https://github.com/inkdropapp/releases/releases.atom',
+          checkpoint: '68889ce94c9b45b41713d4c2d85fce3a00590f0f1af10ff583b944582002172c'
   name 'Inkdrop'
   homepage 'https://www.inkdrop.info/'
 
-  app 'Inkdrop (Beta).app'
+  app 'Inkdrop.app'
+
+  zap delete: [
+                '~/Library/Application Support/inkdrop',
+                '~/Library/Saved Application State/info.pkpk.inkdrop.savedState',
+                '~/Library/Caches/info.pkpk.inkdrop',
+                '~/Library/Preferences/info.pkpk.inkdrop.plist',
+                '~/Library/Preferences/info.pkpk.inkdrop.helper.plist',
+              ]
 end
