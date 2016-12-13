a3106b4d8471f3b11f65d7ed6dbad9402a991589
Colin Dunn <cgd@vikingdev.net>
2016-12-14 10:54:18 +1100

    Update data-science-studio to 3.1.5 (#27754)

--- Casks/data-science-studio.rb
+++ Casks/data-science-studio.rb
@@ -1,6 +1,6 @@
 cask 'data-science-studio' do
-  version '3.1.2'
-  sha256 'fad81f78a87ef4900f586079a5bb434c069a1fe05b5daea8a8b78fc7b1828488'
+  version '3.1.5'
+  sha256 '7148eebfbd2dd92556da603b4c600a365548df9b2cc18eb02d2def0c7675da7d'
 
   url "https://downloads.dataiku.com/public/studio/#{version}/Data%20Science%20Studio%20#{version}.dmg"
   name 'Dataiku Data Science Studio'
