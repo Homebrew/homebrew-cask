0ce6e2983876093fe7a338217a9453a6bfc5ab21
Colin Dunn <dunnc@amazon.com>
2016-12-13 12:22:10 +1100

    Add appcast url to enpass.rb

--- Casks/enpass.rb
+++ Casks/enpass.rb
@@ -4,6 +4,8 @@ cask 'enpass' do
 
   # sinew.in was verified as official when first introduced to the cask
   url "https://dl.sinew.in/mac/setup/Enpass-#{version}.dmg"
+  appcast 'https://www.enpass.io/release-notes/macosx/',
+          checkpoint: 'e2fa3558bc9317f30f14428529f924a340d0501c69f5c6fdefaf7c9613780037'
   name 'Enpass'
   homepage 'https://www.enpass.io/'
 
