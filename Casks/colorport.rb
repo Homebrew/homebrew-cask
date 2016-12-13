f766a9697d77df9241d2deba6a0180797e61a15e
jeffrbauer <jeffrbauer@me.com>
2016-12-13 10:58:51 -0800

    Update colorport to 2.0.5

--- Casks/colorport.rb
+++ Casks/colorport.rb
@@ -2,7 +2,7 @@ cask 'colorport' do
   version '2.0.5'
   sha256 'ae176e3118ba55b4dd8a0716707d149a4e13827b30cdc3f1d29bc329a0ef4bd0'
 
-  url 'http://www.xrite.com/downloader.aspx?FileID=1168&Type=M&returnurl=%2fcolorport-utility-software%2fsupport%2fd1168'
+  url 'https://my.xrite.com/downloader.aspx?FileID=1168&Type=M'
   name 'ColorPort Utility Software'
   homepage 'http://www.xrite.com/colorport-utility-software/support/d1168'
 
