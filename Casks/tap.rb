5b730715fe8a0016f047172e5dfba14ea83ba8e9
miccal <miccal.matthews@gmail.com>
2016-12-13 21:07:25 +0800

    Add Tap (Latest)

---
+++ Casks/tap.rb
@@ -0,0 +1,18 @@
+cask 'tap' do
+  version :latest
+  sha256 :no_check
+
+  if MacOS.version <= :mavericks
+    # dl.devmate.com was verified as official when first introduced to the cask
+    url 'https://dl.devmate.com/com.pabix.tap-mountain/Tap-mountain.dmg'
+  else
+    url 'https://dl.devmate.com/com.pabix.tap/Tap.dmg'
+  end
+
+  name 'Tap'
+  homepage 'http://tap.thepabix.com/'
+
+  depends_on macos: '>= :mountain_lion'
+
+  app 'Tap.app'
+end
