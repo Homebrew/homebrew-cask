2ae249b7c5c4a180dd6948ae8559b09e0a39a6c8
Artem Medvedev <amedvedev@outlook.com>
2016-12-13 15:54:56 +0200

    Added url comment

--- Casks/inkdrop.rb
+++ Casks/inkdrop.rb
@@ -2,6 +2,7 @@ cask 'inkdrop' do
   version '2.6.2'
   sha256 'b6fb511c488dc8ca4d03489b1198e07445164a6066998af136a926ad8e5fe666'
 
+  # github.com/inkdropapp was verified as official when first introduced to the cask
   url "https://github.com/inkdropapp/releases/releases/download/v#{version}/Inkdrop-#{version}-Mac.zip"
   appcast 'https://github.com/inkdropapp/releases/releases.atom',
           checkpoint: '68889ce94c9b45b41713d4c2d85fce3a00590f0f1af10ff583b944582002172c'
