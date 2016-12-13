150316d835802e199f24219eafc6692053e5d8f8
Colin Dunn <dunnc@amazon.com>
2016-12-13 14:08:04 +1100

    Update ipartition to 3.5.1

--- Casks/ipartition.rb
+++ Casks/ipartition.rb
@@ -1,8 +1,8 @@
 cask 'ipartition' do
-  version '3.4.5'
-  sha256 '0b66c44dfb4b8525056b68bfd8f4d56fe4b78fd0e5e4705659bfd242e2722319'
+  version '3.5.1'
+  sha256 'c3400af48b24ea2f30d6d1f829c761a0542cc04ecbe87755223d95418ddeb40a'
 
-  url "https://coriolis-systems.com/downloads/iPartition-#{version}.zip"
+  url "https://coriolis-systems.com/downloads/iPartition-#{version}.dmg"
   name 'iPartition'
   homepage 'https://coriolis-systems.com/iPartition/'
 
