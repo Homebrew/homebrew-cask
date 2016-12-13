1a08b8571dc15bc88558b85b19eecc2ec36c38dd
Kuncara Adi Nugraha <pengen.makan.nasi@gmail.com>
2016-12-13 20:42:57 +0700

    Remove old comment

--- Casks/lacona.rb
+++ Casks/lacona.rb
@@ -2,7 +2,6 @@ cask 'lacona' do
   version '1.1.2'
   sha256 '07c410761091faad533073c10fbad7ad6802e0ce8aef6d1f1fa61f5b392fa2c9'
 
-  # lacona-download.firebaseapp.com was verified as official when first introduced to the cask
   url "https://download.lacona.io/packages/#{version}/Lacona.zip"
   appcast 'https://download.lacona.io/appcast.xml',
           checkpoint: 'fa0c84c376f2af8c8f9f7c87c42115bbd8e4c9304f6547282039146b2ed30898'
