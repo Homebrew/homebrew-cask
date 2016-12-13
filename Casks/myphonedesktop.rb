4a34a198a6b17e880ab27f9ac05291578062d767
Colin Dunn <cgd@vikingdev.net>
2016-12-13 22:27:33 +1100

    Update myphonedesktop to 2.2.0 (#27723)

--- Casks/myphonedesktop.rb
+++ Casks/myphonedesktop.rb
@@ -1,6 +1,6 @@
 cask 'myphonedesktop' do
-  version '2.0.3'
-  sha256 '89cab32dcb91ea67a6627dba0690d54ab01ec1f74d322995c9d289ca016578a0'
+  version '2.2.0'
+  sha256 'df30b2ed62c074c3ccd5118f1a42521a6b170069781c491a4c93b1b3a581d335'
 
   url "http://www.myphonedesktop.com/dwn/myPhoneDesktop_macos_#{version.dots_to_underscores}.dmg"
   name 'myPhoneDesktop'
