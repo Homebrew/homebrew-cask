1fd98a078a2105513611588d4ddd5fd5da08f159
Colin Dunn <dunnc@amazon.com>
2016-12-13 21:04:40 +1100

    Update invisorql to 2.3

--- Casks/invisorql.rb
+++ Casks/invisorql.rb
@@ -1,8 +1,10 @@
 cask 'invisorql' do
-  version '2.0'
-  sha256 '0ac603ab71b927d55a2580a498380b0cada46f3bc50c6439fb1879c793a6f543'
+  version '2.3'
+  sha256 '5c50a72af0edff9aa77e3209c3b9f48f36044c60ee83bc8f4afd993964ce4fa9'
 
   url "http://www.pozdeev.com/invisor/download/InvisorQL-#{version}.dmg"
+  appcast 'http://www.pozdeev.com/invisor/changelog-ql.php',
+          checkpoint: '7f58ba19ad1422d0d949fc6d56ae24c623033e8b2fbadf5a1dae8509ed527ee6'
   name 'Invisor QuickLook Plug-In'
   homepage 'http://www.pozdeev.com/invisor/'
 
