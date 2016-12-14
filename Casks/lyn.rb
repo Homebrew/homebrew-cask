e959ecc0c9173ba9e358abd5038bc46e7ad9a3de
Victor Popkov <victor@popkov.me>
2016-12-14 23:37:43 +0200

    Update lyn to 1.8.4

--- Casks/lyn.rb
+++ Casks/lyn.rb
@@ -1,10 +1,10 @@
 cask 'lyn' do
-  version '1.8.3'
-  sha256 'f76ec277f153766b3be787e3a2a7d355f593cbf9f95653e31b974b36a6d4033e'
+  version '1.8.4'
+  sha256 '899e6c37699f02ed0fe7c4abb09a27a5fdbf3f834ebfca1e661ec648495c4297'
 
   url "http://www.lynapp.com/downloads/Lyn-#{version}.dmg"
   appcast 'http://www.lynapp.com/lyn/update.xml',
-          checkpoint: '893d026155f21ace185be5b8672cac5a66a3db45d43f861a3e2bfd70a5215c20'
+          checkpoint: 'd4771771a079e5cfcb98e6767eee31349ea606837c4f5120ec19c2dab6fc9661'
   name 'Lyn'
   homepage 'http://www.lynapp.com/'
 
