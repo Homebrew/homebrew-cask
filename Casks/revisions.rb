0b7f13514a170b8b1a65f2bb2f51dc70eaa502a2
Colin Dunn <cgd@vikingdev.net>
2016-12-14 20:03:34 +1100

    Update revisions to 2.2.1 (#27767)

--- Casks/revisions.rb
+++ Casks/revisions.rb
@@ -1,6 +1,6 @@
 cask 'revisions' do
-  version '2.2'
-  sha256 '63b77499ee8663ee55bc33d90a46be4a7b35b132b23f429ce2478e31f6680558'
+  version '2.2.1'
+  sha256 '56622f5f034fa7d5a759ce2db56635bee6382a1eb39d8154db8c724bcc416f07'
 
   url "https://revisionsapp.com/downloads/revisions-#{version}.dmg"
   name 'Revisions'
