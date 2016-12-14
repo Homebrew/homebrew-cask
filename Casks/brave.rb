0d75068caf76bb556bc94bd3e409510335d14e33
Victor Popkov <victor@popkov.me>
2016-12-14 23:12:14 +0200

    Update brave to 0.12.15dev (#27790)

--- Casks/brave.rb
+++ Casks/brave.rb
@@ -1,11 +1,11 @@
 cask 'brave' do
-  version '0.12.14dev'
-  sha256 'd98370990a24307d68a568fcddc2535dcca8dc41d6a4603ee9e05834033ae9a2'
+  version '0.12.15dev'
+  sha256 '4a94c42a45f26ba7609a153039ae3926ebf3e682201b8ba655ed313962d454a5'
 
   # github.com/brave/browser-laptop was verified as official when first introduced to the cask
   url "https://github.com/brave/browser-laptop/releases/download/v#{version}/Brave.dmg"
   appcast 'https://github.com/brave/browser-laptop/releases.atom',
-          checkpoint: '4f147828870a2d71767f572621308d937e64ab2feed96f8301b48e0719ccbeb2'
+          checkpoint: '2a5456d6e2e49802347879f664f3203e0062c42ea3bfc8c61559bd0b2b2a1103'
   name 'Brave'
   homepage 'https://brave.com/'
 
