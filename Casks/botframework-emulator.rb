2e166c9980790d13760df9d14d6f2f8ef5a70422
Sertac Ozercan <sozercan@gmail.com>
2016-12-13 17:47:15 -0800

    Update botframework-emulator.rb

--- Casks/botframework-emulator.rb
+++ Casks/botframework-emulator.rb
@@ -3,6 +3,8 @@ cask 'botframework-emulator' do
   sha256 'eb658b3fd0825d3b332aeaafc8c3451a297a6018e1f48232dc84a8a0fb42aed8'
 
   url "https://github.com/Microsoft/BotFramework-Emulator/releases/download/v#{version}/botframework-emulator-#{version}-mac.zip"
+  appcast 'https://github.com/Microsoft/BotFramework-Emulator/releases.atom',
+          checkpoint: '3e730926a4e5ac193a0cebd8be9fc3f0de7a73d9fbe4c2470275a282c6c4df63'
   name 'Microsoft Bot Framework Emulator'
   homepage 'https://github.com/Microsoft/BotFramework-Emulator'
 
