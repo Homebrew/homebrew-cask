89881fcb033600d13f3d1213b769f107101c61a6
Sebastian Schawohl <s.schawohl@magnetic.coop>
2016-12-14 17:40:46 +0100

    Update mattermost to 3.5.0

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,11 +1,11 @@
 cask 'mattermost' do
-  version '3.4.1'
-  sha256 'f34bfaa88901cecbea1a1fa5df97ce842b1f188f6e0083fcf63c0d1aef376006'
+  version '3.5.0'
+  sha256 'fb5d843fadef0dc2a18f95049951d21f09d5c822f8664bb8995507094f942f6c'
 
   # releases.mattermost.com was verified as official when first introduced to the cask
   url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-osx.tar.gz"
   appcast 'https://github.com/mattermost/desktop/releases.atom',
-          checkpoint: '83fffe005c17ee483f8e297417c0166a075b26145fbf01466de6a04d5571ea48'
+          checkpoint: '9e1033d791d7bcb6de41b302229fb8c867aa527700323c988e24acc594fa89d1'
   name 'Mattermost'
   homepage 'https://about.mattermost.org/'
 
