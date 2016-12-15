f1ead388bf85df838d64ce7eaa23cd0969a4b27d
Colin Dunn <cgd@vikingdev.net>
2016-12-15 16:27:51 +1100

    Update bluegriffon to 2.1.2 (#27800)

--- Casks/bluegriffon.rb
+++ Casks/bluegriffon.rb
@@ -1,10 +1,12 @@
 cask 'bluegriffon' do
-  version '2.1.1'
-  sha256 'e2a44b453a1919243056615a3e7bdc9f8240f9681b7c071ffb33bf7f8a62f560'
+  version '2.1.2'
+  sha256 '8a7a0586c8f4ae002886c3c80f4ddbff65648429c56c8e45d4d08dca9ac85b66'
 
   url "http://bluegriffon.org/freshmeat/#{version}/bluegriffon-#{version}.mac.dmg"
   name 'BlueGriffon'
   homepage 'http://bluegriffon.org/'
+
+  depends_on macos: '>= :mountain_lion'
 
   app 'BlueGriffon.app'
 end
