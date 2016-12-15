ce3261f806070db6ccea5272d358c9d1abc04268
Koen Rouwhorst <info@koenrouwhorst.nl>
2016-12-15 13:37:02 +0100

    Update arq to 5.6.1

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,10 +1,10 @@
 cask 'arq' do
-  version '5.6'
-  sha256 '932503e3809f4b2259d11360dc23abf150c56e937f1bfb03ae65e66104999084'
+  version '5.6.1'
+  sha256 '5eb9aed9abd6e9ed074761c347e9b2b966683bca2b793dd6eb068ed4f11f16e7'
 
   url "https://www.arqbackup.com/download/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arq#{version.major}.xml",
-          checkpoint: '6be156e49b2e550bb82d1e56372ad34e0cd105c1f82fe9379419ac68a1381c95'
+          checkpoint: 'a19d0e330a7777261d066e85b666ffd3af7b92cbf9d8278701c8c92f92b69cde'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
