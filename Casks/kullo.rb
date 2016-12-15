37b55832c85116816b47c026d15b36912a7acb98
Colin Dunn <cgd@vikingdev.net>
2016-12-15 16:27:22 +1100

    Update kullo to 49.0.0 (#27797)
    * Update kullo to 49.0.0
    
    * Update kullo to 49.0.0

--- Casks/kullo.rb
+++ Casks/kullo.rb
@@ -1,10 +1,12 @@
 cask 'kullo' do
-  version '0.29.0'
-  sha256 'e2b4a24af038a5f1b675843c4752549399678e9f286429c4c05e572531c0cd6e'
+  version '49.0.0'
+  sha256 'e0193b6c2871bf09175baef5747d9758fff37aaf5d39869be77e1b95833a2fd2'
 
   url "https://www.kullo.net/download/files/osx/Kullo-#{version}.dmg"
   name 'Kullo'
   homepage 'https://www.kullo.net/'
+
+  depends_on macos: '>= :mavericks'
 
   app 'Kullo.app'
 end
