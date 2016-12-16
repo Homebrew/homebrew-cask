bd666cbc1680d5338067555ca0d5e1eceb4bc6da
Koen Rouwhorst <koenrh@users.noreply.github.com>
2016-12-16 19:22:12 +0100

    Update arq to 5.6.1 (#27826)
    * Update arq to 5.6.1
    
    * Update appcast signature

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,10 +1,10 @@
 cask 'arq' do
-  version '5.6'
-  sha256 '932503e3809f4b2259d11360dc23abf150c56e937f1bfb03ae65e66104999084'
+  version '5.6.1'
+  sha256 '5eb9aed9abd6e9ed074761c347e9b2b966683bca2b793dd6eb068ed4f11f16e7'
 
   url "https://www.arqbackup.com/download/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arq#{version.major}.xml",
-          checkpoint: '6be156e49b2e550bb82d1e56372ad34e0cd105c1f82fe9379419ac68a1381c95'
+          checkpoint: 'f94e21a3027ac1073eeaac6a984f2f7de7294a634cc5474f7941926da9c14b00'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
