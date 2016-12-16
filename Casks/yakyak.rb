06b6d279b8c0938dda7898013400f7f849a757d2
Ben Plessinger <plessbd@users.noreply.github.com>
2016-12-16 14:21:41 -0500

    fix yakyak v1.4.1 shasums (#27885)

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,10 +1,10 @@
 cask 'yakyak' do
   version '1.4.1'
-  sha256 'c16d4e5276a0a39742d5e9389967b5dcd180afb4db074ffa156a1f627b421671'
+  sha256 '25f8660c9987320e98f71ed741ff43a5565636ae8d2ac3cbb7666bd2b63eb7d3'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom',
-          checkpoint: '50ab8f3dd67c702ce239d8e25d71ff391c4a2fca3cbd87ab2511e7b166f1ebb4'
+          checkpoint: 'c211df9625e8e4dc399226a1d72fd8095c949d2c7f2036119ff1684ecbfa73f7'
   name 'Yakyak'
   homepage 'https://github.com/yakyak/yakyak'
 
