5ed77c9d0e487f2a2925c2b8c68b846fbd382109
Colin Dunn <cgd@vikingdev.net>
2016-12-16 13:02:14 +1100

    Update arduino to 1.6.13 (#27854)

--- Casks/arduino.rb
+++ Casks/arduino.rb
@@ -1,8 +1,10 @@
 cask 'arduino' do
-  version '1.6.12'
-  sha256 '5289e7a3284ae326f43f0d6bab48b9775b8ed8d2486193accff2f38937dd0e8a'
+  version '1.6.13'
+  sha256 '38eb07af336ab0224712017e5cbf316b7ffb3a2adb319a6d37810ce79ed56916'
 
   url "https://downloads.arduino.cc/arduino-#{version}-macosx.zip"
+  appcast 'https://www.arduino.cc/en/Main/ReleaseNotes',
+          checkpoint: 'd597f3628e775a229c0314707c12ac58cbac90508bb01d710ae1a43ef742de58'
   name 'Arduino'
   homepage 'https://www.arduino.cc/'
 
