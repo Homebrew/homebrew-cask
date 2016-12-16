a6beef708292334ae60ce7e7f29c66e4401c1e01
Joshua McKinney <joshka@users.noreply.github.com>
2016-12-16 15:29:56 -0600

    Bitwig Studio 1.3.15 (#27889)

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask 'bitwig-studio' do
-  version '1.3.13'
-  sha256 '1b83cbe5b52b72d39c719491a53ae98857e8d6a14d75dfd3f1c75b173e4b267a'
+  version '1.3.15'
+  sha256 '05ab5a7c93d2c917708ca4524151dc77e50252c56f0965a6b0fff5c2897783f8'
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   name 'Bitwig Studio'
