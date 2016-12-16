190c98214e699be43f00ab91780d9184a96c7525
Victor Popkov <victor@popkov.me>
2016-12-17 00:30:47 +0200

    Update iconjar to 0.9.5.2,103 (#27895)

--- Casks/iconjar.rb
+++ Casks/iconjar.rb
@@ -1,11 +1,11 @@
 cask 'iconjar' do
-  version '0.9.5.2,98'
-  sha256 '21c9d6d5b37eab2fc3f31c8adc12765765dd375192616a99e2db42381354ec96'
+  version '0.9.5.2,103'
+  sha256 'dc74b92f7e32e042ce72115a7caa262c7369d00f5bd4bf3832af0c6c0d11cc4a'
 
   # hockeyapp.net/api/2/apps/b91fcb22f1ac5013dad6ffde2dca7c79 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/b91fcb22f1ac5013dad6ffde2dca7c79/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/b91fcb22f1ac5013dad6ffde2dca7c79',
-          checkpoint: '6279ca41ca42667780f330ffd5d295466dcb1113b386de276e66951b97c3a0ac'
+          checkpoint: '5a30ef3b0be22eeb3f047ab72e997643a2e9b23f63b54ad409618c056b2d3efc'
   name 'IconJar'
   homepage 'http://geticonjar.com/'
 
