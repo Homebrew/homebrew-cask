59faba3293ca4e4a0d2aa2bbd8f3700affde4142
Colin Dunn <dunnc@amazon.com>
2016-12-17 15:18:36 +1100

    Update synology-photo-station-uploader to 1.4-080

--- Casks/synology-photo-station-uploader.rb
+++ Casks/synology-photo-station-uploader.rb
@@ -3,8 +3,6 @@ cask 'synology-photo-station-uploader' do
   sha256 '8b1e9477329d6f611df85ffcef715e289001a186e0f95ff0062b85104beaa83b'
 
   url "https://global.download.synology.com/download/Tools/PhotoStationUploader/#{version}/Mac/PhotoStationUploader-#{version.sub(%r{.*-}, '')}-Mac-Installer.dmg"
-  appcast 'https://www.synology.com/en-global/releaseNote/PhotoStationUploader',
-          checkpoint: 'e438d9e1c4b182f1c59d661122c1a3bb2bf09eb1d72442acfb71be9aa3cb3cf4'
   name 'Synology Photo Station Uploader'
   homepage 'https://www.synology.com/'
 
