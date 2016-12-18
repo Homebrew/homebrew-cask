3595dfb602873e90d79e87c9d44716fa5f8ee8de
Victor Popkov <victor@popkov.me>
2016-12-18 23:17:44 +0200

    Update itch to 22.2.0

--- Casks/itch.rb
+++ Casks/itch.rb
@@ -1,11 +1,11 @@
 cask 'itch' do
-  version '22.0.0'
-  sha256 '24c712f0e745d15439cfe4fe63af95f18af8bea5d8f6f4fb8b1431af44fc363d'
+  version '22.2.0'
+  sha256 '3c93a7b1c5d880c819e75c0bf31277261ea71b5585817a0966976867de8fecee'
 
   # github.com/itchio/itch was verified as official when first introduced to the cask
   url "https://github.com/itchio/itch/releases/download/v#{version}/itch-mac.dmg"
   appcast 'https://github.com/itchio/itch/releases.atom',
-          checkpoint: '0c9ddb4b7dae961ddf3ad7ab75ff16c6b91a4ed215a9170bcd2fffb843ef997a'
+          checkpoint: 'eca59b0f515cf26edd52c85607aa6469efff497affd10816103123a63259572e'
   name 'itch'
   homepage 'https://itch.io/app'
 
