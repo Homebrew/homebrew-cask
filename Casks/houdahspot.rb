45bdad0654fc629f2a1aaba60d3011c0746ffc74
Colin Dunn <dunnc@amazon.com>
2016-12-18 15:37:06 +1100

    Update houdahspot to 4.2.8

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,10 +1,10 @@
 cask 'houdahspot' do
-  version '4.2.7'
-  sha256 '3eb4d5bda5420fcc37b207e8fcc9e63bd22c7d09590653466f1e0c31bd53bf49'
+  version '4.2.8'
+  sha256 'f0601012a694e1f07dd5708ba4cb86bbbe7b085483c55a6a4ab6c45d68094a9e'
 
   url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml",
-          checkpoint: 'c0176bbeaa2d46dcba4b1e43d4f2df31d28ef7e29698759a1f3c297862c09372'
+          checkpoint: 'f2d8687b84885bc2caab04dcfc13d28618e0ec1bc9804f66025046ebf20d084c'
   name 'HoudahSpot'
   homepage 'https://www.houdah.com/houdahSpot/'
 
