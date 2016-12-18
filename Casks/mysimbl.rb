ced24b42b65d12a7e20c7b04ea3f3b1063d1bc2a
Victor Popkov <victor@popkov.me>
2016-12-18 23:22:29 +0200

    Update mysimbl to 0.4.2

--- Casks/mysimbl.rb
+++ Casks/mysimbl.rb
@@ -1,11 +1,11 @@
 cask 'mysimbl' do
-  version '0.4.1'
-  sha256 '0b233c33198d05c720b46a21fc5d197b283336d32f84e86edc08fa41f39d3e37'
+  version '0.4.2'
+  sha256 '1cc9211ad7e5eb65cb51c1c392400c5ab7209776fdfd7d0c2c7cfd39bb64aaa8'
 
   # githubusercontent.com/w0lfschild/app_updates/master/mySIMBL was verified as official when first introduced to the cask
   url "https://raw.githubusercontent.com/w0lfschild/app_updates/master/mySIMBL/mySIMBL_#{version}.zip"
   appcast 'https://raw.githubusercontent.com/w0lfschild/app_updates/master/mySIMBL/appcast.xml',
-          checkpoint: '940e1e47e97747928b70e6bb51d633efbd7a95fc0c26af5de470f29596351ae7'
+          checkpoint: 'b7a6a025e1a6d2af2f41f79a6642cb2e6384d0242d209a83116c7ab3c701e6ed'
   name 'mySIMBL'
   homepage 'https://github.com/w0lfschild/mySIMBL'
 
