34fbc34123d2b7476e3d3b72af93960674b26142
Vinoth Ramiah <vin047@users.noreply.github.com>
2016-12-18 06:39:01 +0000

    Update qbittorrent to 3.3.10 (#27926)

--- Casks/qbittorrent.rb
+++ Casks/qbittorrent.rb
@@ -1,11 +1,11 @@
 cask 'qbittorrent' do
-  version '3.3.9'
-  sha256 'e41aceeb2afde69cf97aaefb2dc2a40a75c85f7fbb87960ff07997a53eca706a'
+  version '3.3.10'
+  sha256 '2ceece42dbb7e84a88496028c09e34edac5be8d9583d0475d494883b97a11a5f'
 
   # sourceforge.net/qbittorrent was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-mac',
-          checkpoint: '136344e8dcb6fe394c3a0f00e58c0335043a6312e5ce776ad01455b27c45a290'
+          checkpoint: '80ba15210f3eb226e6f01848dd69298df7f42e7ee7dd849dfc2a2dc576dbb3e1'
   name 'qBittorrent'
   homepage 'http://www.qbittorrent.org/'
 
