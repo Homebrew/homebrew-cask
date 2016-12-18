01dde05bb04a83d6d33be36d7abfdcf571131a44
Colin Dunn <dunnc@amazon.com>
2016-12-18 20:29:13 +1100

    Add splashtop-personal.rb appcast

--- Casks/splashtop-personal.rb
+++ Casks/splashtop-personal.rb
@@ -4,6 +4,8 @@ cask 'splashtop-personal' do
 
   # d17kmd0va0f0mp.cloudfront.net was verified as official when first introduced to the cask
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STP/Splashtop_Personal_v#{version}.dmg"
+  appcast 'http://www.splashtop.com/wp-content/themes/responsive/downloadx.php?product=stp&platform=mac-client',
+          checkpoint: '5c62995b415044d924c96f2fbf50b11ee0884af98355d7d8e9f17c6ba1e878fd'
   name 'Splashtop Personal'
   homepage 'https://www.splashtop.com/personal'
 
