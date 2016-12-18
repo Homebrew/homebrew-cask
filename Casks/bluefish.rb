ab268ef36018d26478b1e624efcf1b85e6dbb48f
iamybj <iamybj@qq.com>
2016-12-18 15:25:33 +0800

    add bluefish.rb
    add bluefish.rb

---
+++ Casks/bluefish.rb
@@ -0,0 +1,10 @@
+cask 'bluefish' do
+  version '2.2.8'
+  sha256 '46e6202adc39d4759380ab603174f3549a9620e735c816c61392ac2900350f4e'
+
+  url "http://www.bennewitz.com/bluefish/stable/binaries/macosx/Bluefish-#{version}.dmg"
+  name 'Bluefish'
+  homepage 'http://bluefish.openoffice.nl/'
+
+  app 'Bluefish.app'
+end
