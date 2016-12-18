80c3c87d4f0eb507c9543fb643423a99cce23c65
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-18 21:20:34 +0800

    Update nextiva to 21.5.0.813 (#27946)

--- Casks/nextiva.rb
+++ Casks/nextiva.rb
@@ -1,10 +1,10 @@
 cask 'nextiva' do
-  version '21.3.0.1311'
-  sha256 '1e5de7747db38e99cdc76e6b6194d24f44078d89eacc88843250a8fa13ce1466'
+  version '21.5.0.813'
+  sha256 '84d566d2eb996f74a76041310165e6dbad5e6c00aa981199e0f532bc03ebcd98'
 
   url "https://dm.nextiva.com/dms/bc/pc/Nextiva_App.bc-uc.osx-#{version}.dmg"
   name 'Nextiva'
-  homepage 'https://www.nextiva.com/support/nextiva-app-mac-setup.html'
+  homepage 'https://www.nextiva.com/support/articles/nextiva-app-mac-setup.html'
 
   app 'Nextiva App.app'
 end
