b78cd37edfea93142691e0c7eb4c68bad1bbab1e
Michael Stringer <stringbean@users.noreply.github.com>
2016-12-19 11:09:49 +0000

    Update aerial to 1.2

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -1,10 +1,10 @@
 cask 'aerial' do
-  version '1.2beta5'
+  version '1.2'
   sha256 '5b128fb10a1e088ece7feafc3e58f923036dc73b1efed0b8ac74df33b3e0daff'
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.zip"
   appcast 'https://github.com/JohnCoates/Aerial/releases.atom',
-          checkpoint: '05c9cbb19ad4349b46d4aad95ec7e6391dc43dbebc378e2671c050f442058150'
+          checkpoint: '1bd34c3e417de6b81d1d876fed873f9fa45d0077dfecfeb8112b9050cd5d0f4b'
   name 'Aerial Screensaver'
   homepage 'https://github.com/JohnCoates/Aerial'
 
