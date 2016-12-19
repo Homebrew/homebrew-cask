5ee7d8d7620152df1b6628ed3f6a59fc690337c1
Colin Dunn <dunnc@amazon.com>
2016-12-19 21:01:02 +1100

    Update camerabag to 3.0.1

--- Casks/camerabag.rb
+++ Casks/camerabag.rb
@@ -1,11 +1,11 @@
 cask 'camerabag' do
-  version '2.7.01'
-  sha256 'e120208a17692ebcc5ad7c7c3fc27125fe9870530174b2872a2a3b3fdaff6163'
+  version '3.0.1'
+  sha256 '5fd4bad30ae1d84d179c0b10a38ea9df3f89d7c450e1c8f912da2f8f37949c9d'
 
   # downloads.nevercenter.com.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "http://downloads.nevercenter.com.s3.amazonaws.com/CameraBag_Mac_#{version.dots_to_underscores}.dmg"
+  url "http://downloads.nevercenter.com.s3.amazonaws.com/CameraBag_Photo_Mac_#{version.dots_to_underscores}.dmg"
   name 'CameraBag'
   homepage 'https://nevercenter.com/camerabag/photo/'
 
-  app "CameraBag #{version.major}.app"
+  app 'CameraBag Photo.app'
 end
