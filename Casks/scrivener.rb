008c1a03a842afe0f59c05b82ea4b8ca30709f50
Colin Dunn <dunnc@amazon.com>
2016-12-19 17:06:56 +1100

    Update scrivener to 2.81.2,106

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -1,9 +1,9 @@
 cask 'scrivener' do
-  version '2.8.1.2,106'
+  version '2.81.2,106'
   sha256 'b4a09047b4b7f7522d58fdd5736f7de9d7250447f3573fd993964bfb3b6be1e1'
 
   # scrivener.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "http://scrivener.s3.amazonaws.com/mac_updates/Scrivener_#{version.after_comma}_#{version.before_comma.gsub(%r{\.\d+\.}, '.' + version.minor)}.zip"
+  url "http://scrivener.s3.amazonaws.com/mac_updates/Scrivener_#{version.after_comma}_#{version.before_comma}.zip"
   appcast "https://www.literatureandlatte.com/downloads/scrivener-#{version.major}.xml",
           checkpoint: '5ebdbf2cd82e1d644ad1a63fac21cbc02ca8d180295eea9ab944d91700f8052c'
   name 'Scrivener'
