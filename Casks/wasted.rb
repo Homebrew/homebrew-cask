5cdd86dcbde48f3252302275de9c522651c7d180
Colin Dunn <dunnc@amazon.com>
2016-12-20 10:56:16 +1100

    Move wasted.rb links

--- Casks/wasted.rb
+++ Casks/wasted.rb
@@ -3,7 +3,7 @@ cask 'wasted' do
   sha256 '9802d9d9674145881f82ccd127ee49d64af4fd09f27a1199fe354a5fe57881ba'
 
   url 'http://wasted.werk01.de/Wasted.zip'
-  appcast 'http://werk01.de/wasted/appcast.xml',
+  appcast 'http://wasted.werk01.de/appcast.xml',
           checkpoint: '2798524a4eb590c537dd46085528bb94a3717d6dd07572e93cb2241ed682f722'
   name 'WASTED'
   homepage 'http://wasted.werk01.de'
