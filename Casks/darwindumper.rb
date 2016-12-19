615b86fe3c78e18ea5dba5e7ddf3023f3f0bccf3
Colin Dunn <dunnc@amazon.com>
2016-12-20 09:57:27 +1100

    Update darwindumper to 3.0.2

--- Casks/darwindumper.rb
+++ Casks/darwindumper.rb
@@ -1,10 +1,12 @@
 cask 'darwindumper' do
-  version '2.9.9.2'
-  sha256 '6971211171f5f562ad833ca530173d7783c5211222b2540cd599d47ac7f2576c'
+  version '3.0.2'
+  sha256 '17267226d3b7f6bec761566921cdba1e20c0885dcf3240eb5060fda1e073babc'
 
   url "https://bitbucket.org/blackosx/darwindumper/downloads/DarwinDumper_v#{version}.zip"
+  appcast 'https://api.bitbucket.org/2.0/repositories/blackosx/darwindumper/downloads',
+          checkpoint: '09770d7879d75643e398ae84951c81ae5bdbd5b51be70ae7514bf2d6744acb7d'
   name 'DarwinDumper'
   homepage 'https://bitbucket.org/blackosx/darwindumper'
 
-  app "DarwinDumper_v#{version}/DarwinDumper.app"
+  app 'DarwinDumper.app'
 end
