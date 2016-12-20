0ba4d723563ca234af362dc9b8581cd06471b5a7
Ben <BenjaminHCCarr@users.noreply.github.com>
2016-12-20 01:07:05 -0500

    New Cask Raven Lite 2.0 sound analysis software
    This adds the 64 bit Free version of the Cornell Lab of Ornithology Raven Lite 2.0 software used "for the acquisition, visualization, measurement, and analysis of sounds."

---
+++ Casks/raven-lite.rb
@@ -0,0 +1,19 @@
+cask 'raven-lite' do
+  version '2.0.0.0022'
+  sha256 '84c8d97df6119605ff645c93d896d472a814246fb87128611225168459423532'
+
+  url "http://www.birds.cornell.edu/brp/RavenLite/RavenLiteFullInstaller/InstData/MacOSX/RavenLite-#{version}_macosx.dmg"
+  name 'Raven Lite 2.0'
+  homepage 'http://www.birds.cornell.edu/brp/RavenLite/RavenLiteReadMe.htm'
+
+  depends_on macos: '>= :mountain_lion'
+  depends_on arch: :x86_64
+
+  installer manual: 'RavenLite-Installer.app'
+
+  uninstall trash: '/Applications/Raven Lite 2.0/'
+
+  caveats do
+    depends_on_java('6')
+  end
+end
