a90bd6d1d0d1cf4da55de8727f9af353e6cfa5af
Vítor Galvão <info@vitorgalvao.com>
2016-12-20 16:10:29 +0000

    Update macdjview.rb

--- Casks/macdjview.rb
+++ Casks/macdjview.rb
@@ -2,7 +2,7 @@ cask 'macdjview' do
   version '0.1.2'
   sha256 'a5869a740b0459ff2e048ff7350d0db12ea26090d6889a2fed903cd297ba7248'
 
-  url 'https://downloads.sourceforge.net/windjview/MacDjView/0.1.2/MacDjView-0.1.2.dmg.gz'
+  url "https://downloads.sourceforge.net/windjview/MacDjView/#{version}/MacDjView-#{version}.dmg.gz"
   name 'MacDjView'
   homepage 'http://windjview.sourceforge.net/'
 
