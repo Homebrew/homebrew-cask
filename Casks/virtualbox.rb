e736bb17c6e95593af95c306c99da64b720fa29d
Alexander Hamann <neutric@users.noreply.github.com>
2016-12-20 22:49:34 +0100

    Update virtualbox to 5.1.12-112440

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -6,13 +6,13 @@ cask 'virtualbox' do
     version '5.0.26-108824'
     sha256 'e8836a98adea9350917a41e754dfec4fe2df7c4a0224fd8beca72cbc5d778437'
   else
-    version '5.1.10-112026'
-    sha256 '3500f07be9f5e27a08c00cc626981230bdaf639f6fad0b82ecf731a0580cccef'
+    version '5.1.12-112440'
+    sha256 '74b10d9af01b8eb7d987088ffe52c74646a479a27cba4f98dc5149f1f6b93c76'
   end
 
   url "http://download.virtualbox.org/virtualbox/#{version.sub(%r{-.*}, '')}/VirtualBox-#{version}-OSX.dmg"
   appcast 'http://download.virtualbox.org/virtualbox/LATEST.TXT',
-          checkpoint: '779e6d24cb291ff605d93a3ec585cfa5589c1d4584ab7d9ee04a231d41ff5df3'
+          checkpoint: 'be267ff5adb254acf71b9e2dab4de6bd16d4b0e5bf085edd9156c2d7be1f585f'
   name 'Oracle VirtualBox'
   homepage 'https://www.virtualbox.org/'
 
