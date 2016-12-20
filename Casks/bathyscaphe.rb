fd00d27064f506e6e931cf3897fb211951a44972
Colin Dunn <dunnc@amazon.com>
2016-12-20 11:00:32 +1100

    Update bathyscaphe to 280-v988

--- Casks/bathyscaphe.rb
+++ Casks/bathyscaphe.rb
@@ -3,8 +3,6 @@ cask 'bathyscaphe' do
   sha256 '12f1036e7881a9a8bd61cfe0493fe7c4684e7b139438018790b219e04e7b1c30'
 
   url "https://bitbucket.org/bathyscaphe/public/downloads/BathyScaphe-#{version}.dmg"
-  appcast 'https://api.bitbucket.org/2.0/repositories/bathyscaphe/public/downloads',
-          checkpoint: 'ddb893bb169ce5bb42f0f3f0ece5315e53606ba03054ab78452db18eebd53084'
   name 'BathyScaphe'
   homepage 'http://bathyscaphe.bitbucket.org/'
 
