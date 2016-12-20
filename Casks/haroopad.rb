35efd7bce8f4485036f1a9389473756dc3acc4c7
Colin Dunn <dunnc@amazon.com>
2016-12-20 11:01:51 +1100

    Update haroopad to 0.13.2

--- Casks/haroopad.rb
+++ Casks/haroopad.rb
@@ -4,8 +4,6 @@ cask 'haroopad' do
 
   # bitbucket.org/rhiokim/haroopad-download was verified as official when first introduced to the cask
   url "https://bitbucket.org/rhiokim/haroopad-download/downloads/Haroopad-v#{version}-x64.dmg"
-  appcast 'https://api.bitbucket.org/2.0/repositories/rhiokim/haroopad-download/downloads',
-          checkpoint: '18a1125d12b7dfed1a60b96a8f978947c4ab6b63c130d62f2686e22fab03a3ee'
   name 'Haroopad'
   homepage 'http://pad.haroopress.com/'
 
