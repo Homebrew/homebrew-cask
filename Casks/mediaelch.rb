4b53469bc00c2510425b8b7dddf6d99d1a4d0d10
Colin Dunn <cgd@vikingdev.net>
2016-12-20 11:15:13 +1100

    Update mediaelch to 2.4.2 (#28018)
    * Update mediaelch to 2.4.2
    
    * Update mediaelch to 2.4.2

--- Casks/mediaelch.rb
+++ Casks/mediaelch.rb
@@ -1,6 +1,6 @@
 cask 'mediaelch' do
-  version '2.4'
-  sha256 '56697eaad8ee5000d83ee1c262abc381b4dae06b1352893b9223422167eb7744'
+  version '2.4.2'
+  sha256 'f82d4c157ef64e25529744fb962771ce678a9e2e3a94b18b3d398dfa5b8b9452'
 
   url "http://www.kvibes.de/releases/mediaelch/#{version}/MediaElch-#{version}.dmg"
   name 'MediaElch'
