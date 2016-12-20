b6ed1d19873fddb346e5a696e217194b8cf9a6cc
Mu-An Chiou <me@muanchiou.com>
2016-12-20 11:44:02 +0800

    updated mojibar (2.3.2)

--- Casks/mojibar.rb
+++ Casks/mojibar.rb
@@ -1,10 +1,10 @@
 cask 'mojibar' do
-  version '2.2.2'
-  sha256 '50de1520e88c393b28560d321d596ce279094eb39ff451658131346d22a9848e'
+  version '2.3.2'
+  sha256 '7c6faac950216475ca88600cdf07de0b92b5ce12c3331286bff85c9567d475d8'
 
   url "https://github.com/muan/mojibar/releases/download/#{version}/mojibar.zip"
   appcast 'https://github.com/muan/mojibar/releases.atom',
-          checkpoint: 'e33f7c268ba39988b426256954ef07f54165433a6835229278f4aafd87ed47c9'
+          checkpoint: '4aed1bf5a60456b1ff68d3b714027ea75ffeaff45074dd30be401b30de8852f5'
   name 'Mojibar'
   homepage 'https://github.com/muan/mojibar'
 
