57396d29ca8f6387d4e3039cbac4c3176761b015
Colin Dunn <cgd@vikingdev.net>
2016-12-21 10:34:00 +1100

    Update xmind to 8-update1 (#28053)

--- Casks/xmind.rb
+++ Casks/xmind.rb
@@ -1,6 +1,6 @@
 cask 'xmind' do
-  version '8'
-  sha256 'c4dc8e3eb47d19557fbe5ac837122768eded1321042b064fc3e25f7a2a66ce86'
+  version '8-update1'
+  sha256 'e0a8a534746c9a31be890afc429fb3d8059db79e52d9d205c054d9d9ef4c5ffb'
 
   url "https://www.xmind.net/xmind/downloads/xmind-#{version}-macosx.dmg"
   name 'XMind'
