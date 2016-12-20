f5404fff2f51aae0711cd7d75e966e42671a32d4
Jeremy Saklad <saklad5@me.com>
2016-12-20 07:20:17 -0600

    Update iexplorer to 4.0.11.0

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,10 +1,10 @@
 cask 'iexplorer' do
-  version '4.0.10.0'
-  sha256 '4d4ec33e065a7c1e9cf124516c69f7086f8c1419888f37d821919fd7bc4a095b'
+  version '4.0.11.0'
+  sha256 'b9d222d53cdcc53f0137140d48ab01ff782860c9872397c7462d23f6dbedcb8b'
 
   url "https://www.macroplant.com/release/iExplorer-#{version}.dmg"
   appcast "https://www.macroplant.com/iexplorer/ie#{version.major}-appcast.xml",
-          checkpoint: 'a2c5f6cb31d8d18b94527522631551e9959a1fb95745e1fa8cf088141fbcd649'
+          checkpoint: '48eaa402b874473b322a88d0af31c3642f1c2588a3337524a95a71c2d5fd564d'
   name 'iExplorer'
   homepage 'https://www.macroplant.com/iexplorer/'
 
