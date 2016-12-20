5ee890ce5c9114d6401a3f9f5fd149c2f94d2d6f
Matheus Araujo <matheus.araujo@e-strident.com>
2016-12-20 20:10:35 +0100

    Add Freeter Latest
    Freeter is an organizer for professionals that work with projects

---
+++ Casks/freeter.rb
@@ -0,0 +1,11 @@
+cask 'freeter' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://freeter.io/download/Freeter.dmg'
+  name 'Freeter'
+  homepage 'https://freeter.io/'
+
+  app 'Freeter.app'
+end
+
