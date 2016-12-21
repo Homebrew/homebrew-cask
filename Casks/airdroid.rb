035e27b51b16219180eaed4aada9ac8c5c245c15
Colin Dunn <cgd@vikingdev.net>
2016-12-21 19:57:20 +1100

    Update airdroid to 3.3.5.3 (#28079)

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask 'airdroid' do
-  version '3.3.5.1'
-  sha256 '6e2e6e988071b4eec64ddc1e3e30c7f372fe31554f35a3f36d01984fbc6c33cd'
+  version '3.3.5.3'
+  sha256 '325b282641410e2f7d62ca70d03a1d1de3c31519d3660cfd1e501eda596d8286'
 
   # s3.amazonaws.com/dl.airdroid.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
