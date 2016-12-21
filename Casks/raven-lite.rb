caaab6d1d61f01185853463afdbf2e9b62b90174
Ben <BenjaminHCCarr@users.noreply.github.com>
2016-12-20 22:28:45 -0500

    revert back to #{version} string
    Needed to revert the #{version} string:
    from `#{version-major_minor_patch}`
    back to: `#{version}
    
    didn't pass CI audit, failed path to download.

--- Casks/raven-lite.rb
+++ Casks/raven-lite.rb
@@ -2,7 +2,7 @@ cask 'raven-lite' do
   version '2.0.0.0022'
   sha256 '84c8d97df6119605ff645c93d896d472a814246fb87128611225168459423532'
 
-  url "http://www.birds.cornell.edu/brp/RavenLite/RavenLiteFullInstaller/InstData/MacOSX/RavenLite-#{version.major_minor_patch}_macosx.dmg"
+  url "http://www.birds.cornell.edu/brp/RavenLite/RavenLiteFullInstaller/InstData/MacOSX/RavenLite-#{version}_macosx.dmg"
   name 'Raven Lite'
   homepage 'http://www.birds.cornell.edu/brp/RavenLite/RavenLiteReadMe.htm'
 
