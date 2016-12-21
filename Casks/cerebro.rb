8ea3ec0a57c5c98b97c3081e447085ab3cbf0b88
Alexandr Subbotin <kelionweb@gmail.com>
2016-12-21 13:20:56 +0100

    Update Cerebro to 0.2.0

--- Casks/cerebro.rb
+++ Casks/cerebro.rb
@@ -1,11 +1,11 @@
 cask 'cerebro' do
-  version '0.1.0'
-  sha256 '0a2acec0d520cd632172c00cabd0a160184dcbc29302eae28478832c930021ca'
+  version '0.2.0'
+  sha256 'e5b57ef950be2472c56a5f466890a05f648e6bc67db30f30b7ca6d0521e34211'
 
   # github.com/KELiON/cerebro was verified as official when first introduced to the cask
   url "https://github.com/KELiON/cerebro/releases/download/#{version}/Cerebro.dmg"
   appcast 'https://github.com/KELiON/cerebro/releases.atom',
-          checkpoint: 'd0a372db626b46ceb8125c351c69a2733bc84fe82772bda007b91a0f649a1561'
+          checkpoint: '6f14d2366470f22932456ffa257857b78d8cdd39069b8cb38077099c677744ef'
   name 'Cerebro'
   homepage 'https://cerebroapp.com/'
 
