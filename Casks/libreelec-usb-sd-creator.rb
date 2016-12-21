8f640b054c806b97e15ccf0b3f8a8f078854885b
Ashish Vijayaram <avijayaram@mozilla.com>
2016-12-21 14:37:19 -0800

    Add LibreELEC USB-SD Creator v1.2

---
+++ Casks/libreelec-usb-sd-creator.rb
@@ -0,0 +1,10 @@
+cask 'libreelec-usb-sd-creator' do
+  version '1.2'
+  sha256 'b4e3bc1201f208679540d26ab03eee4aca6059b76571c7fb0568693f0faf2157'
+
+  url 'http://releases.libreelec.tv/LibreELEC.USB-SD.Creator.macOS.dmg'
+  name 'LibreELEC USB-SD Creator'
+  homepage 'https://libreelec.tv/'
+
+  app 'LibreELEC USB-SD Creator.app'
+end
