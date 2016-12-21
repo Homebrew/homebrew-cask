ec40a8f48d036c34bd52f65f1b2e91580e17dc44
Ashish Vijayaram <ashish@mozilla.com>
2016-12-21 15:16:57 -0800

    Update libreelec-usb-sd-creator.rb
    Remove version number and checksum

--- Casks/libreelec-usb-sd-creator.rb
+++ Casks/libreelec-usb-sd-creator.rb
@@ -1,6 +1,6 @@
 cask 'libreelec-usb-sd-creator' do
-  version '1.2'
-  sha256 'b4e3bc1201f208679540d26ab03eee4aca6059b76571c7fb0568693f0faf2157'
+  version :latest
+  sha256 :no_check
 
   url 'http://releases.libreelec.tv/LibreELEC.USB-SD.Creator.macOS.dmg'
   name 'LibreELEC USB-SD Creator'
