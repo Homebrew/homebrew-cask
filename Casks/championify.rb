d336c83c19852599449a2e0290b588a18f854659
Julian POIDEVIN <poidevin.julian@gmail.com>
2016-12-22 19:47:29 +0100

    Added appcast
    - added appcast and checkpoint

--- Casks/championify.rb
+++ Casks/championify.rb
@@ -3,6 +3,8 @@ cask 'championify' do
   sha256 '092332a6aaf06309180c82a93a1a9b7a874e125cfefe13337db5199899054624'
 
   url "https://github.com/dustinblackman/Championify/releases/download/#{version}/Championify-OSX-#{version}.dmg"
+  appcast 'https://github.com/dustinblackman/Championify/releases.atom',
+          checkpoint: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   name 'Championify'
   homepage 'https://github.com/dustinblackman/Championify/'
 
