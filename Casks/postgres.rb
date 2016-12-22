6f9ea937e7d7806c3bbbb4dac9d5ea9c8958b2f8
Gerry Power <gerrypower@Gerrys-MacBook-Pro.local>
2016-12-22 16:14:02 -0500

    Update to new v2 url

--- Casks/postgres.rb
+++ Casks/postgres.rb
@@ -3,7 +3,7 @@ cask 'postgres' do
   sha256 '4924b956c30d1e3191277dcd72834dc43fc76f74c1cae5be18729af771b0da45'
 
   # github.com/PostgresApp/PostgresApp was verified as official when first introduced to the cask
-  url "https://github.com/PostgresApp/PostgresApp/releases/download/#{version}/Postgres-#{version}.zip"
+  url "https://github.com/PostgresApp/PostgresApp/releases/download/v#{version}/Postgres-#{version}.dmg"
   appcast 'https://github.com/PostgresApp/PostgresApp/releases.atom',
           checkpoint: '84cfa62ebeaaa4cb156d4d89bcb392020c138f9838f29816685f563cb2b5454e'
   name 'Postgres'
