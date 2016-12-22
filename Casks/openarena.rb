acb8e0dcc61f93d1b24d02e948b9199c4fffa509
Vítor Galvão <info@vitorgalvao.com>
2016-12-22 16:41:08 +0000

    Update openarena.rb

--- Casks/openarena.rb
+++ Casks/openarena.rb
@@ -9,7 +9,7 @@ cask 'openarena' do
   app "openarena-#{version}/OpenArena.app"
 
   postflight do
-    system_command '/bin/mv', args: ["#{staged_path}/baseoa", "#{appdir}/OpenArena.app/Contents/MacOS"]
+    system_command '/bin/mv', args: ["#{staged_path}/openarena-#{version}/baseoa", "#{appdir}/OpenArena.app/Contents/MacOS"]
     set_permissions "#{appdir}/OpenArena.app/Contents/MacOS/openarena.ub", '755'
   end
 end
