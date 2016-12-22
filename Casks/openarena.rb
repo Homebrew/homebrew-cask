31a2565e402f029a5926245f9d85e4710924dcd2
Vítor Galvão <info@vitorgalvao.com>
2016-12-22 16:58:13 +0000

    openarena.rb: fix installation (#28174)

--- Casks/openarena.rb
+++ Casks/openarena.rb
@@ -1,14 +1,15 @@
 cask 'openarena' do
-  version '0.8.8'
-  sha256 '5a8faf7f5b51f351b0a1618c06b6b98a5f1a6758f1d39818de2c87df2a0bac4a'
+  version '0.8.8 r28'
+  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
-  url 'http://openarena.ws/request.php?4'
+  url "http://download.tuxfamily.org/openarena/rel/#{version.major_minor_patch.no_dots}/openarena-#{version.major_minor_patch}.zip"
   name 'OpenArena'
   homepage 'http://openarena.ws/smfnews.php'
 
-  app "openarena-#{version}/OpenArena #{version} r28.app"
+  app "openarena-#{version.major_minor_patch}/OpenArena #{version}.app"
 
   postflight do
-    set_permissions "#{appdir}/OpenArena.app/Contents/MacOS/openarena.ub", '755'
+    system_command '/bin/mv', args: ["#{staged_path}/openarena-#{version.major_minor_patch}/baseoa", "#{appdir}/OpenArena #{version}.app/Contents/MacOS"]
+    set_permissions "#{appdir}/OpenArena #{version}.app/Contents/MacOS/openarena.ub", '755'
   end
 end
