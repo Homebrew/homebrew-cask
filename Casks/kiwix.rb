2c9328cd0caa6f0340700eb7c022479434e58e58
Colin Dunn <dunnc@amazon.com>
2016-12-22 20:57:38 +1100

    Update kiwix.rb download url

--- Casks/kiwix.rb
+++ Casks/kiwix.rb
@@ -2,7 +2,6 @@ cask 'kiwix' do
   version '0.9'
   sha256 '98e677aee3c106c6ec5b16791d3be7c22274b3b32cc44a9cce1eeca6275b7bcc'
 
-  # mirror.netcologne.de/kiwix was verified as official when first introduced to the cask
   url "http://download.kiwix.org/bin/#{version}/kiwix-#{version}.dmg"
   name 'Kiwix'
   homepage 'http://www.kiwix.org/'
