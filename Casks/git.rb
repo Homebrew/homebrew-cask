2420ac1e9a9983418bedf9c2023b41d50036101a
Bruce Sun <okampfer@gmail.com>
2016-12-22 19:15:16 +0800

    Upgrade git to version 2.10.1, plus a fix to uninstall_preflight. (#28126)
    * Upgrade git to version 2.10.1, plus a fix to uninstall_preflight.
    
    * revert staged_path change
    
    staged_path is not the installation path, it’s where we keep the extracted archive pieces after download.

--- Casks/git.rb
+++ Casks/git.rb
@@ -6,12 +6,12 @@ cask 'git' do
     url "https://downloads.sourceforge.net/git-osx-installer/git-#{version}-intel-universal-snow-leopard.dmg"
     pkg "git-#{version}-intel-universal-snow-leopard.pkg"
   else
-    version '2.7.1'
-    sha256 '260b32e8877eb72d07807b26163aeec42e2d98c350f32051ab1ff0cc33626440'
+    version '2.10.1'
+    sha256 '6bad786bae098eaae0aa824e714d5a0b0fb3eb5d1412ed27c89743ddc1d3d209'
 
     url "https://downloads.sourceforge.net/git-osx-installer/git-#{version}-intel-universal-mavericks.dmg"
     appcast 'https://sourceforge.net/projects/git-osx-installer/rss',
-            checkpoint: '96f8b3facd8324ed75a2dc919d33519506b892f2b3bd8a84d16f2a6f93c9f6a5'
+            checkpoint: '56ba7834c0c58ff1b9f9ab9c7660d7a3228339e6114224a665b0285c5cc5d800'
 
     pkg "git-#{version}-intel-universal-mavericks.pkg"
   end
