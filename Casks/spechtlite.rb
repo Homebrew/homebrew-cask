66a317b267695484e3931b4b9defd65bb91cd7e4
Colin Dunn <dunnc@amazon.com>
2016-12-22 22:29:03 +1100

    Update spechtlite to 0.9.0

--- Casks/spechtlite.rb
+++ Casks/spechtlite.rb
@@ -1,10 +1,10 @@
 cask 'spechtlite' do
-  version '0.8.1'
-  sha256 '302ca8194c2787eb8d89a6b7a7bd8624ce3f3fd844bf21005d0c8d2d0031241d'
+  version '0.9.0'
+  sha256 '9d27de394ff10222a410d51bb98d894b82684276406ddcd219f00b0280cf7398'
 
   url "https://github.com/zhuhaow/SpechtLite/releases/download/#{version}/SpechtLite.zip"
   appcast 'https://github.com/zhuhaow/SpechtLite/releases.atom',
-          checkpoint: 'fe837c2b13fd661be485639228ad98b9416fc3acea9ae1a91baaf31f289890f7'
+          checkpoint: '2449df94491e7bb164efe98b5e2884e703d7b899ef2ea33b2834ffd4fbc3ccad'
   name 'SpechtLite'
   homepage 'https://github.com/zhuhaow/SpechtLite'
 
