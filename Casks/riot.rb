2285319754cca54c0270e8240025e73331abef4b
Ashish Vijayaram <avijayaram@mozilla.com>
2016-12-22 23:15:15 -0800

    Add Riot.app v0.9.2

---
+++ Casks/riot.rb
@@ -0,0 +1,10 @@
+cask 'riot' do
+  version '0.9.2'
+  sha256 'e7e9f8df7958f9c28705aef8bd70950eb192d8c23ed0bff203cdc08059859b60'
+
+  url "https://riot.im/download/desktop/install/macos/Riot-#{version}.dmg"
+  name 'Riot'
+  homepage 'https://riot.im'
+
+  app 'Riot.app'
+end
