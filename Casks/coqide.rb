941e6945c4523bb8f71920f6d0214cf948fb1b1f
alekar <manas.alekar@gmail.com>
2016-12-24 03:37:31 -0800

    Upgrade coqide to 8.6. (#28223)

--- Casks/coqide.rb
+++ Casks/coqide.rb
@@ -1,6 +1,6 @@
 cask 'coqide' do
-  version '8.5pl2'
-  sha256 'e02a24b87f2aa52d580239ac778b8ee9feee83273ebff49418818c77254fb901'
+  version '8.6'
+  sha256 'abd00fc22b7a99a214d8b6977c982d6be26060d769f79f42d3ddcc8761bb4f49'
 
   url "https://coq.inria.fr/distrib/V#{version}/files/CoqIDE_#{version}.dmg"
   name 'Coq'
