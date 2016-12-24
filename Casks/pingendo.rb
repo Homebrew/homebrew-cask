e0c62f75e825bc6d258b41c11a8a6244d68350b0
Vítor Galvão <info@vitorgalvao.com>
2016-12-24 03:13:21 +0000

    Update pingendo.rb

--- Casks/pingendo.rb
+++ Casks/pingendo.rb
@@ -1,10 +1,10 @@
 cask 'pingendo' do
-  version :latest
-  sha256 :no_check
+  version '3'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url 'http://download.pingendo.com/v3/Pingendo.dmg'
+  url "http://download.pingendo.com/v#{version}/Pingendo.dmg"
   name 'Pingendo'
   homepage 'https://pingendo.com/'
 
-  app 'Pingendo3.app'
+  app "Pingendo#{version}.app"
 end
