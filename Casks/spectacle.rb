92ac3210a82c742e646cfec299822ad55389d5d8
Vítor Galvão <info@vitorgalvao.com>
2016-12-24 17:42:06 +0000

    Update spectacle.rb

--- Casks/spectacle.rb
+++ Casks/spectacle.rb
@@ -5,12 +5,13 @@ cask 'spectacle' do
   else
     version '1.2'
     sha256 '766d5bf3b404ec567110a25de1d221290bc829302283b28ed0fbe73b9557f30c'
+
+    appcast 'https://www.spectacleapp.com/updates/appcast.xml',
+            checkpoint: 'f6b3d6282b56296885c8e0dc3ff3218f12b6c045dfc946379a9322323dd85fac'
   end
 
   # amazonaws.com/spectacle was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/spectacle/downloads/Spectacle+#{version}.zip"
-  appcast 'https://www.spectacleapp.com/updates/appcast.xml',
-          checkpoint: 'f6b3d6282b56296885c8e0dc3ff3218f12b6c045dfc946379a9322323dd85fac'
   name 'Spectacle'
   homepage 'https://www.spectacleapp.com/'
 
