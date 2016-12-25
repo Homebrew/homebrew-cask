5fff3c952646c137111821dac5d766a1cc0ea0c1
Lukas Bestle <account-github@lukasbestle.com>
2016-12-25 19:26:56 +0100

    Add Prince v11 (#28239)
    * Add Prince v11
    
    Prince was previously in [homebrew-binary](https://github.com/Homebrew/homebrew-binary/blob/master/prince.rb).
    Moving this here as homebrew-binary is deprecated and outdated.
    
    * Update prince.rb

---
+++ Casks/prince.rb
@@ -0,0 +1,20 @@
+cask 'prince' do
+  version '11'
+  sha256 'dac005d49c594b90b0933ffac0601997cee8c64c3e6190073dc1d024eb439553'
+
+  url "https://www.princexml.com/download/prince-#{version}-macosx.tar.gz"
+  name 'Prince'
+  homepage 'https://www.princexml.com'
+
+  installer script: "prince-#{version}-macosx/install.sh",
+            sudo:   false
+
+  uninstall delete: [
+                      '/usr/local/bin/prince',
+                      '/usr/local/lib/prince',
+                    ]
+
+  caveats do
+    files_in_usr_local
+  end
+end
