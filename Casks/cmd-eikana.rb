f68d0912e51340bfd9a1e16a4f52cf12ad4d3487
Victor Popkov <victor@popkov.me>
2016-12-25 19:54:00 +0200

    Update cmd-eikana to 2.2.0 (#28243)

--- Casks/cmd-eikana.rb
+++ Casks/cmd-eikana.rb
@@ -1,10 +1,10 @@
 cask 'cmd-eikana' do
-  version '2.1.2'
-  sha256 '994b228095eda7770e0292b8db7a72765a711433489d7de790721567eddcba80'
+  version '2.2.0'
+  sha256 'dde4d9633f3df10f96b08ce8d6f1155eb5cce45efd4c06048facd1abecffeca1'
 
   url "https://github.com/iMasanari/cmd-eikana/releases/download/v#{version}/eikana-#{version}.app.zip"
   appcast 'https://github.com/iMasanari/cmd-eikana/releases.atom',
-          checkpoint: 'a1cfd202f711379405b323239812fdca43b7f217c503bbcb952d52ba985eb832'
+          checkpoint: '5e5acb8bea7bb871976592f547fa40366fddeb5e82b90d54fb2930c2828ad8b9'
   name 'cmd-eikana'
   name '⌘英かな'
   homepage 'https://github.com/iMasanari/cmd-eikana'
