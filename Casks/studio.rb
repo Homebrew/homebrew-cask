a5922c701e80449c7c38c73ff156c9e0f4f076a8
Vítor Galvão <info@vitorgalvao.com>
2016-12-25 15:55:11 +0000

    Update studio.rb

--- Casks/studio.rb
+++ Casks/studio.rb
@@ -2,7 +2,7 @@ cask 'studio' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com/blstudio was verified as official when first introduced to the cask
+  # s3.amazonaws.com/blstudio was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/blstudio/Stud.io.pkg'
   name 'Stud.io'
   homepage 'http://studio.bricklink.com/v2/build/studio.page'
