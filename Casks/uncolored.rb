5798e37685e0f667f70eebb8d8a0192b3651dab3
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-26 01:25:02 +0800

    Remove space after url

--- Casks/uncolored.rb
+++ Casks/uncolored.rb
@@ -3,7 +3,7 @@ cask 'uncolored' do
   sha256 '7ac0c07fbe3c0b9b1bf575274e590c7070e9b30f46d16744464ae99d4aed9f07'
 
   # github.com/n457/Uncolored was verified as official when first introduced to the cask
-  url  "https://github.com/n457/Uncolored/releases/download/v.#{version}/Uncolored-v.#{version}-osx-x64.dmg"
+  url "https://github.com/n457/Uncolored/releases/download/v.#{version}/Uncolored-v.#{version}-osx-x64.dmg"
   appcast 'https://github.com/n457/Uncolored/releases.atom',
           checkpoint: '2edf99eeec394128ecb3272e6eb4e392abd044f4f3887a438fd48895fe392428'
   name '(Un)colored'
