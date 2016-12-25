624266e24ef82aee87e550f526ba4e2123800fe0
Victor Popkov <victor@popkov.me>
2016-12-25 20:10:29 +0200

    Update grandtotal to 4.3.3 (#28252)

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,10 +1,10 @@
 cask 'grandtotal' do
-  version '4.3.2'
-  sha256 'eb044df5241ce444466128e04797006bd5c89910bf33752cfa6129695bdac1ed'
+  version '4.3.3'
+  sha256 '350d93c0d76637be413f89831c98af0ad956bd6e00be66311e2946783c9a0d61'
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   appcast "https://mediaatelier.com/GrandTotal#{version.major}/feed.php",
-          checkpoint: 'a1233d2d6a52f00d24113ebab4243043071e86da1477a322e86d6d126bacfb03'
+          checkpoint: 'dc49981ad74f180e7d049c5131193855e58a352c3c6211765c08d4877036a9de'
   name 'GrandTotal'
   homepage 'https://www.mediaatelier.com/GrandTotal4/'
 
