bdf74bdf290d4956d30825a332b125834c16c263
Mui Kai En <muikaien1@gmail.com>
2016-12-26 17:14:48 +0800

    Fix url in BRL-CAD / MGED.app cask

--- Casks/brl-cad.rb
+++ Casks/brl-cad.rb
@@ -3,7 +3,7 @@ cask 'brl-cad' do
   sha256 '5fe9eb9f5a2eb5544cf459b02e7e880040e02d1312d04c2207b90a731ebd8f0f'
 
   # downloads.sourceforge.net/brlcad was verified as official when first introduced to the cask
-  url 'https://downloads.sourceforge.net/brlcad/BRL-CAD%20for%20Mac%20OS%20X/7.24.0/BRL-CAD%207.24.0.dmg'
+  url "https://downloads.sourceforge.net/brlcad/BRL-CAD%20for%20Mac%20OS%20X/#{version}/BRL-CAD%20#{version}.dmg"
   name 'BRL-CAD'
   homepage 'http://brlcad.org/'
 
