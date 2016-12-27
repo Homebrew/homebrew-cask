24afdd7c8ae9e5437f07670072f6d50f6a818adf
yyar <yymjar@gmail.com>
2016-12-27 13:46:43 +0900

    Update mat to 1.6.1.20161125 (#28289)

--- Casks/mat.rb
+++ Casks/mat.rb
@@ -1,6 +1,6 @@
 cask 'mat' do
-  version '1.6.0.20160531'
-  sha256 '300e151bc6bc7e34b196fc50803269f3ce23ad49b0cda9d8335d8861d13ccb64'
+  version '1.6.1.20161125'
+  sha256 'b2dd323c5d09db97e17ed2c3cf97b15b7f13fe434417fa4ea50dd57577ee78fd'
 
   url "http://www.eclipse.org/downloads/download.php?r=1&file=/mat/#{version.major_minor}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.x86_64.zip"
   name 'Eclipse Memory Analyzer'
