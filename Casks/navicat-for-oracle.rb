2ead91e498be076551d8b763fe9b2d7113b7cfb1
Nima Jalali <nimajalali@Nimas-MacBook-Pro.local>
2016-12-27 00:30:10 -0800

    Update navicat-for-oracle to 11.2.16

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-oracle' do
-  version '11.2.15'
-  sha256 'c8a607c38cc0b93cab70931cceca71a2d2d08c1f616dd851173c704b1f744f33'
+  version '11.2.16'
+  sha256 'bbc1d84a75d67b9dc29c2ea3030d109d0b0087fbabd68f08dd95a380ae397e73'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
   name 'Navicat for Oracle'
