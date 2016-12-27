de42f57c956b95a5c3187ece88e5d0f4d4128d8d
Chan Lee Siong <ahlee2326@me.com>
2016-12-27 13:34:03 +0800

    Update screaming-frog-seo-spider to 7.1

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -3,8 +3,8 @@ cask 'screaming-frog-seo-spider' do
     version '2.40'
     sha256 'f37a517cb1ddb13a0621ae2ef98eba148027b3a2b5ce56b6e6b4ca756e40329b'
   else
-    version '6.1'
-    sha256 '4ac1924c2be29bac37b730ba0a406e7d5367a07a72e2dfc9f0ac02b7db879b06'
+    version '7.1'
+    sha256 'ca9f3ef0287da969de9d35239773853fb8bca91418d61c27cf5cbb04a15601a4'
   end
 
   url "https://www.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}.dmg"
