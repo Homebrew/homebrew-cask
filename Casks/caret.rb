feefb7de37656120ca9989d79271fff04f17b1ab
Victor Popkov <victor@popkov.me>
2016-12-28 20:37:36 +0200

    Update caret to 1.12.1 (#28332)

--- Casks/caret.rb
+++ Casks/caret.rb
@@ -1,11 +1,11 @@
 cask 'caret' do
-  version '1.12.0'
-  sha256 '041d7fa5cb22d38749c9402557920d4519c2bbbbcd9fd2c602a507e75526c973'
+  version '1.12.1'
+  sha256 'cf730c7c6389714adb4fa5bbae1328d2a92824df07ff18408ef007d8996e83bc'
 
   # github.com/careteditor/caret was verified as official when first introduced to the cask
   url "https://github.com/careteditor/caret/releases/download/#{version}/Caret.dmg"
   appcast 'https://github.com/careteditor/caret/releases.atom',
-          checkpoint: '9145c50d1c3ae4b4d9628df42d59d85168e9e81cb89c423623b8e037917f28f6'
+          checkpoint: '203633db4b6b64d4c705aad237e4dbd2c6d9b51d67c0f307bb232cfbc049f7c2'
   name 'Caret'
   homepage 'https://caret.io/'
 
