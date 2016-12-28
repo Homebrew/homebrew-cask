094da1acf592188b0c47f83bf88058af6ec8ee13
Artem Medvedev <amedvedev@outlook.com>
2016-12-28 20:32:56 +0200

    Update blisk to 2.0.136.192 (#28330)

--- Casks/blisk.rb
+++ Casks/blisk.rb
@@ -1,6 +1,6 @@
 cask 'blisk' do
-  version '1.0.125.166'
-  sha256 '76c9b1c6f71524bef2a35c5055eb36c1b96f42945e4fb324447525a7e7ee1c59'
+  version '2.0.136.192'
+  sha256 '4760ef49a4fee184db5c6a9b28f17d282dead1ba936cd42cc32603f8a6bac10b'
 
   # bliskcloudstorage.blob.core.windows.net was verified as official when first introduced to the cask
   url "https://bliskcloudstorage.blob.core.windows.net/mac-installers/BliskInstaller_#{version}.dmg"
