8adf95053a8fe61a0133a5cc806c904fa564e97b
Bret Martin <bam@miranda.org>
2016-12-28 19:41:18 -0500

    Update logitech-myharmony to 1.0.0.87 (#28350)

--- Casks/logitech-myharmony.rb
+++ Casks/logitech-myharmony.rb
@@ -1,8 +1,8 @@
 cask 'logitech-myharmony' do
-  version '1.0'
-  sha256 'de3c4d3a6d71dcccf45acf92fca496558bab95f70461c8466ba4ade3f5986740'
+  version '1.0.0.87'
+  sha256 'e85bd9a1f65470df94c604b2671b60b47b0dbc07958b20796e93e7400e06cda2'
 
-  url "https://app.myharmony.com/prod/mac/#{version}/MyHarmony-App.dmg"
+  url "https://app.myharmony.com/prod/mac/#{version.major_minor}/MyHarmony-App.dmg"
   name 'MyHarmony'
   homepage 'https://setup.myharmony.com/'
 
