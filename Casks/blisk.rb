de57456454675a0c369ca03402eafd68cc561884
Eric McCormick <edm00se@users.noreply.github.com>
2016-12-29 16:27:25 -0600

    correct blisk sha256 for 2.0.136.192 (#28356)

--- Casks/blisk.rb
+++ Casks/blisk.rb
@@ -1,6 +1,6 @@
 cask 'blisk' do
   version '2.0.136.192'
-  sha256 '4760ef49a4fee184db5c6a9b28f17d282dead1ba936cd42cc32603f8a6bac10b'
+  sha256 'e4cf4958b6b6d7ae5b51375d0c3fc2e0b2adaafdffc93b0405ad4cd15d3f9d30'
 
   # bliskcloudstorage.blob.core.windows.net was verified as official when first introduced to the cask
   url "https://bliskcloudstorage.blob.core.windows.net/mac-installers/BliskInstaller_#{version}.dmg"
