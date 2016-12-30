979da50dab087562ca3e64cb9b7341ef491d0c43
Victor Popkov <victor@popkov.me>
2016-12-30 23:31:36 +0200

    Update spechtlite to 0.10.0 (#28413)

--- Casks/spechtlite.rb
+++ Casks/spechtlite.rb
@@ -1,10 +1,10 @@
 cask 'spechtlite' do
-  version '0.9.0'
-  sha256 '9d27de394ff10222a410d51bb98d894b82684276406ddcd219f00b0280cf7398'
+  version '0.10.0'
+  sha256 '9a51860d215e36162f63696e5b6aa7f18cb18349a20e27bc93fcada2cfede7a9'
 
   url "https://github.com/zhuhaow/SpechtLite/releases/download/#{version}/SpechtLite.zip"
   appcast 'https://github.com/zhuhaow/SpechtLite/releases.atom',
-          checkpoint: '2449df94491e7bb164efe98b5e2884e703d7b899ef2ea33b2834ffd4fbc3ccad'
+          checkpoint: '7274c1852fa70b69b22773d238418b7171af0e3857193febae5ec3bdd4f691b0'
   name 'SpechtLite'
   homepage 'https://github.com/zhuhaow/SpechtLite'
 
