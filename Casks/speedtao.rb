5e3409240b3029cf87adaadf46bb93821af824dc
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-30 21:01:03 +0800

    Update speedtao.rb
    The `homepage` is currently down but `brew cask fetch` is working.

--- Casks/speedtao.rb
+++ Casks/speedtao.rb
@@ -1,10 +1,10 @@
 cask 'speedtao' do
   version '0.9.98'
-  sha256 '61481dab9265c406cadd1658add93e713e6e90d7e8fd27350a125fcd60f7c40a'
+  sha256 '9ac32e6a26aa97aa62bc5322506b8873399a427ce78f44d896b574571e7b839a'
 
   url 'http://www.speedtao.net/beta_latest'
   appcast 'http://speedtao.net/su/su.xml',
-          checkpoint: '0aa02fc70783ceebc25a3bcc16d9480395f76f44d3488e0d2127f283ded753a7'
+          checkpoint: '83ab425aaa00d0b31ca2905a96aacfad59782f5ad5afe773d19346748503d0f5'
   name 'SpeedTao'
   homepage 'http://www.speedtao.net/'
 
