86d3e34f07aec9ec3bac3780f5233c1b2c2dbed1
nuomi1 <nuomi1@users.noreply.github.com>
2016-12-31 14:42:43 +0800

    Update youku to 1.1.8.12206 (#28427)
    * Update youku to 1.1.8.12206
    
    Update youku to 1.1.8.12206
    
    * Update youku to 1.1.8.12206
    
    Update youku to 1.1.8.12206

--- Casks/youku.rb
+++ Casks/youku.rb
@@ -1,16 +1,18 @@
 cask 'youku' do
-  if MacOS.version <= :mountain_lion
+  if MacOS.version == :mountain_lion
     version '1.0.5.5031'
     sha256 'b7d9ff08bd87150dfa9a52a891d57002e0cbc3f73d181240adfa86a076793d55'
   else
-    version '1.1.5.10190'
-    sha256 '621ace860acd18512fa34b453bb54be0b0ce9d9d2de8d8aef2d39338fedd9d63'
+    version '1.1.8.12206'
+    sha256 'bf26b321c77c1490b72476482dfef131a098fbb89fadcde5b5091d79e026aaaa'
   end
 
-  url "http://desktop.youku.com/ugc/youkumac_#{version}.dmg"
+  url "http://pcclient.download.youku.com/ugc/youkumac_#{version}.dmg"
   name 'Youku'
   name '优酷'
   homepage 'https://pd.youku.com/pc'
+
+  depends_on macos: '>= :mountain_lion'
 
   app '优酷.app'
 
