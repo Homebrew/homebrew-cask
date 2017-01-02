66a64bdc9e6ae55a217fed8e5acf1fa87f692f09
Vicent Tsai <vayn@vayn.de>
2017-01-03 01:11:55 +0800

    Add ExNihilo.app v1.5.1

---
+++ Casks/exnihilo.rb
@@ -0,0 +1,21 @@
+cask 'exnihilo' do
+  version '1.5.1'
+  sha256 'ec3a04fcc1792e8f3a7ae8664caa93ad0cfa04b286c5fee05d2a1f622b71dd94'
+
+  # github.com/Vayn/ex-nihilo was verified as official when first introduced to the cask
+  url "https://github.com/Vayn/ex-nihilo/releases/download/#{version}/ExNihilo.zip"
+  appcast 'https://vayn.github.io/ex-nihilo/release/appcast.xml',
+          checkpoint: 'd65a82d8aac3cb650e2a4ec0903d6f3b3577870ef59c7c64e16165df5773cd88'
+  name 'Ex nihilo'
+  homepage 'https://vayn.github.io/ex-nihilo/'
+
+  app 'ExNihilo.app'
+
+  uninstall  login_item: 'ExNihilo'
+
+  zap        delete: [
+                       '~/Library/Caches/com.soloinc.ExNihilo',
+                       '~/Library/Preferences/com.soloinc.ExNihilo.plist',
+                       '~/Library/Cookies/com.soloinc.ExNihilo.binarycookies',
+                     ]
+end
