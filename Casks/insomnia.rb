ddf88ffbaed5d9795e8e7597d73020fb6a9f729b
Kuncara Adi Nugraha <pengen.makan.nasi@gmail.com>
2017-01-02 14:50:57 +0700

    Update insomnia to 4.0.13 (#28473)

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask 'insomnia' do
-  version '4.0.12'
-  sha256 'ab1a1817f7dbf7cbf170a54f8caa8737c8d7f3497072c813307e5cfa04c3f629'
+  version '4.0.13'
+  sha256 '8cf4abe469302b025deb496db1179fcfc99907a74130d9ae74dde36417faa45f'
 
   url "https://downloads.insomnia.rest/mac/Insomnia-#{version}.dmg"
   name 'Insomnia'
