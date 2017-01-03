dc6e5dffea53fc1f8d9b112309bce7a211d47c9b
Colin Dunn <dunnc@amazon.com>
2017-01-04 09:11:06 +1100

    Update xampp to 5.6.28-1

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -1,6 +1,6 @@
 cask 'xampp' do
-  version '5.6.28-0'
-  sha256 '7243fddb861ac3780db665c7bf60008582379adf28ec6caaa146ae0206f6812c'
+  version '5.6.28-1'
+  sha256 '23ea25814a5f2bb38cf5d43d499bafbcfc303ec99f967b69d8f5b43e8f65fa0b'
 
   # downloadsapachefriends.global.ssl.fastly.net/xampp-files was verified as official when first introduced to the cask
   url "https://downloadsapachefriends.global.ssl.fastly.net/xampp-files/#{version.major_minor_patch}/xampp-osx-#{version}-installer.dmg"
