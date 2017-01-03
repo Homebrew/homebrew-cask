7e83f69685f274dcd9d54a6a7529194a3cc8b4f3
Sarah Seo <sarah.seo.0311@gmail.com>
2017-01-03 15:42:25 +0900

    Fix inpad.rb

--- Casks/inpad.rb
+++ Casks/inpad.rb
@@ -4,9 +4,10 @@ cask 'inpad' do
 
   # github.com/Sarah-Seo/Inpad was verified as official when first introduced to the cask
   url "https://github.com/Sarah-Seo/Inpad/releases/download/v#{version}/Inpad-#{version}.dmg"
+  appcase 'https://github.com/Sarah-Seo/Inpad/releases.atom',
+          checkpoint: '3a738e6fe75a048c190c52e31a4d6151af05928da27625e3b1e864f268175a05'
   name 'Inpad'
   homepage 'https://sarah-seo.github.io/Inpad/'
-  license :unknown
 
   app 'Inpad.app'
 end
