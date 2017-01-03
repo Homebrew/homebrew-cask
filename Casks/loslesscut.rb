22d724aecdd9087540a587f38bbfa1c1cb1a9417
Vítor Galvão <info@vitorgalvao.com>
2017-01-03 20:59:42 +0000

    Update loslesscut.rb

--- Casks/loslesscut.rb
+++ Casks/loslesscut.rb
@@ -2,7 +2,7 @@ cask 'loslesscut' do
   version '1.3.0'
   sha256 'd44affab76fc4538a7968207742a2324a2cffb60b14360519650b0989b4049fb'
 
-  url 'https://github.com/mifi/lossless-cut/releases/download/v1.3.0/LosslessCut-darwin-x64.zip'
+  url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-darwin-x64.zip"
   name 'Loslesscut'
   homepage 'https://github.com/mifi/lossless-cut'
 
