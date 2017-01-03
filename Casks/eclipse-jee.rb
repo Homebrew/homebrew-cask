f1d9ef527184beb02741473208910aa209000d8c
Colin Dunn <cgd@vikingdev.net>
2017-01-03 14:49:26 +1100

    Update eclipse casks to 4.6.2 (#28492)

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-jee' do
-  version '4.6.1,neon:1a'
-  sha256 'c64da846dfb762ce6650e4d842c566a61836b14c0a77b1f5395f4f9842a66aad'
+  version '4.6.2,neon:2'
+  sha256 '7a65903dd5ad2e3d32fedd76d3fd59beadd92a8144181eb198b786063f17c9b6'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-jee-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse IDE for Java EE Developers'
--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-php' do
-  version '4.6.1,neon:1a'
-  sha256 '4126090e0c88f97eac863e75a2657a4b6d29e7dac724dba28e95c4e32ce948d3'
+  version '4.6.2,neon:2'
+  sha256 '2dc3d7ae523f55dab1c64a2a30b0a8f6e42d4372cecfaeb4230ad2c63dfb7dbd'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-php-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse for PHP Developers'
