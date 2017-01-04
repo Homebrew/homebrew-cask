b85e817b95eff53831eddf927700e081731dc4d2
David Ohayon <ohayon.1@gmail.com>
2017-01-04 18:21:38 -0500

    Update fastlane to 1.4 (#28578)

--- Casks/fastlane.rb
+++ Casks/fastlane.rb
@@ -1,6 +1,6 @@
 cask 'fastlane' do
-  version '1.3'
-  sha256 '2645702993105b286b2f54b91058aaa3fcd482c400b507478a9fad42c273631b'
+  version '1.4'
+  sha256 'dba9444d7bfeb48c6c50ee9fd481f211ff1beb503092a1c5597d83c6c78eb823'
 
   # kits-crashlytics-com.s3.amazonaws.com/fastlane/ was verified as official when first introduced to the cask
   url "https://kits-crashlytics-com.s3.amazonaws.com/fastlane/standalone/bundle-#{version}.zip"
