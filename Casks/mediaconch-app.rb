1f71e2bd18d7a84acb2d7e66086fb94b2baa4680
Maxime Gervais <gervais.maxime@gmail.com>
2017-01-04 15:21:34 +0100

    Update mediaconch-app to 16.12

--- Casks/mediaconch-app.rb
+++ Casks/mediaconch-app.rb
@@ -1,10 +1,10 @@
 cask 'mediaconch-app' do
-  version '16.11'
-  sha256 '9cac189b13d85958eb39fed86df531858e6229033426b3d7b5502fe1d8741600'
+  version '16.12'
+  sha256 'f82803d8ce0514d6364d8d4742ecdf904b19d970e9d2a40177852f3d6a2a31fd'
 
   url "https://mediaarea.net/download/binary/mediaconch-gui/#{version}/MediaConch_GUI_#{version}_Mac.dmg"
   appcast 'https://github.com/MediaArea/MediaConch_SourceCode/releases.atom',
-          checkpoint: '2a3a15822cda61ed254ed20ee3561ee4d792ad86bff9307a1730af0e41d45fc7'
+          checkpoint: '08444b4bade984a3fdcb57207badcd8ff0f9a0f82bd4346bc544d2b6bd57d480'
   name 'MediaConch'
   homepage 'https://mediaarea.net/MediaConch/'
 
