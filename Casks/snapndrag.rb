4cce057459d47277bb08c991a4a3bf83c4ea31de
Vítor Galvão <info@vitorgalvao.com>
2017-01-04 20:51:49 +0000

    Update snapndrag.rb

--- Casks/snapndrag.rb
+++ Casks/snapndrag.rb
@@ -5,7 +5,6 @@ cask 'snapndrag' do
   url "http://yellowmug.com/download/SnapNDrag_#{version}.dmg"
   appcast 'http://yellowmug.com/snapndrag/appcast-1012.xml',
           checkpoint: 'e850ffe995147b3a8ad8f73caaf067e60390a3dc3aa2fa000b1f93d664dcc81b'
-
   name 'SnapNDrag'
   homepage 'http://www.yellowmug.com/snapndrag/'
 
