5e33f3f8b848eb7115852f9247a37da9b1162e86
Kayvan Sylvan <kayvansylvan@gmail.com>
2017-01-04 09:09:42 -0800

    Update lima to 1.2.4

--- Casks/lima.rb
+++ Casks/lima.rb
@@ -1,6 +1,6 @@
 cask 'lima' do
-  version '1.2.3'
-  sha256 '9b8ff2effd930921401d26c28303a2da61f44eb1b8638c3537b1644e42671d42'
+  version '1.2.4'
+  sha256 'caae6a8aa9209bd5ba5746a0406f3c1cf7f47adc89daa9c801f1830d1d127ee5'
 
   url "https://update.api.meetlima.com/downloads/osx/dist/Lima_#{version}.dmg"
   name 'Lima'
