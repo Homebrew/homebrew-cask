609036a9931f55972d6e0390e4595b9d2a9fb0c9
Gerry Power <gerry@thepowerhouse.com>
2017-01-03 21:05:57 -0500

    Add cask for SnapNDrag 4.2.1

---
+++ Casks/snapndrag.rb
@@ -0,0 +1,10 @@
+cask 'snapndrag' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.yellowmug.com/download/SnapNDrag.dmg'
+  name 'SnapNDrag'
+  homepage 'http://www.yellowmug.com/snapndrag/'
+
+  app 'SnapNDrag.app'
+end
