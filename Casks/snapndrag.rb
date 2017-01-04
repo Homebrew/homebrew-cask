bdfd748d983c2415459bb3ddc35a1caf6dcf32dc
Gerry Power <gerry@thepowerhouse.com>
2017-01-04 15:21:54 -0500

    Add appcast, version and sha256

--- Casks/snapndrag.rb
+++ Casks/snapndrag.rb
@@ -1,8 +1,11 @@
 cask 'snapndrag' do
-  version :latest
-  sha256 :no_check
+  version '4.2.1'
+  sha256 '4b768be224b6358b16f8555f3d5b82801a46afc87b1609d91108f71ed0ce93e2'
 
-  url 'http://www.yellowmug.com/download/SnapNDrag.dmg'
+  url "http://yellowmug.com/download/SnapNDrag_#{version}.dmg"
+  appcast 'http://yellowmug.com/snapndrag/appcast-1012.xml',
+          checkpoint: 'e850ffe995147b3a8ad8f73caaf067e60390a3dc3aa2fa000b1f93d664dcc81b'
+
   name 'SnapNDrag'
   homepage 'http://www.yellowmug.com/snapndrag/'
 
