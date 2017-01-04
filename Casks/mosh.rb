30ad03f155b776972ce6bc83f86eab2a98d9db57
Colin Dunn <dunnc@amazon.com>
2017-01-04 16:19:08 +1100

    Update mosh to 1.2.6

--- Casks/mosh.rb
+++ Casks/mosh.rb
@@ -1,6 +1,6 @@
 cask 'mosh' do
-  version '1.2.5'
-  sha256 '8a590ba81edd6f706f2d0afe1cb882bd8ff8860e395b7c6ac7285306f4f12209'
+  version '1.2.6'
+  sha256 '5eb7797b0c3a5423da1c62f80f8e6268acd55b1b10a850e58fd7bb8f6bdb520d'
 
   url "https://mosh.org/mosh-#{version}.pkg"
   name 'Mosh'
