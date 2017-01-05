067d90ccf236a9f94aa01f7dd97db0b75ebc3862
Colin Dunn <dunnc@amazon.com>
2017-01-05 11:01:26 +1100

    Update workcraft to 3.1.2

--- Casks/workcraft.rb
+++ Casks/workcraft.rb
@@ -1,6 +1,6 @@
 cask 'workcraft' do
-  version '3.1.0'
-  sha256 '90a82bd05eec11c118b92078cf1e03f23b5bd148e019c9e0f61a341be04d516f'
+  version '3.1.2'
+  sha256 '1d540df19e232eba0135c1293e6a74f60fdef26a9495e84baebce47c3c650f73'
 
   url "http://www.workcraft.org/_media/download/workcraft-v#{version}-osx.tar.gz"
   name 'Workcraft'
