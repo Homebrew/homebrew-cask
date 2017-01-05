0958427db869c7863feb9d3065b1839291ca3269
Colin Dunn <dunnc@amazon.com>
2017-01-05 14:31:25 +1100

    Update microsoft-teams to 0.6.00.34901

--- Casks/microsoft-teams.rb
+++ Casks/microsoft-teams.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-teams' do
-  version '0.6.00.29902'
-  sha256 'bbd22672782acb78daea001e847cd2254de47bf5b255487cce5758c3298105dd'
+  version '0.6.00.34901'
+  sha256 '2ae3687a2280117ff83f0bacbf8a08db1bee81ae26bc8eb82faa38436550a04b'
 
   url "https://statics.teams.microsoft.com/production-osx/#{version}/Teams_osx.dmg"
   name 'Microsoft Teams'
