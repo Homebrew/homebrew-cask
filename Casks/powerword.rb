888f71fb5a80dbee38f05357d6aac973f107f9a7
miccal <miccal.matthews@gmail.com>
2017-01-05 20:24:17 +0800

    Update powerword to 1.0.0

--- Casks/powerword.rb
+++ Casks/powerword.rb
@@ -2,7 +2,7 @@ cask 'powerword' do
   version '1.0.0'
   sha256 '8bee28fa52bc967f91f6700d9642e00fc5e275cc41e0c15e5bf288fcac537c07'
 
-  url "http://mac.iciba.com/download/powerword_macosx_beta_#{version}.dmg"
+  url "http://download.iciba.com/pc/powerword_macosx_beta_#{version}.dmg"
   name 'Powerword'
   name '词霸'
   homepage 'http://mac.iciba.com/'
