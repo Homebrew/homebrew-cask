9d22c488255c84678d64f89fc631a6b580cb7757
Colin Dunn <dunnc@amazon.com>
2017-01-06 10:20:24 +1100

    Update elektriktrickquicklook to 1.1.0

--- Casks/elektriktrickquicklook.rb
+++ Casks/elektriktrickquicklook.rb
@@ -1,10 +1,10 @@
 cask 'elektriktrickquicklook' do
-  version '1.0.3'
-  sha256 '9449543ff34b5d0cba1e330340e34e8bf046425e5bf83d28bcc55f7562f78e12'
+  version '1.1.0'
+  sha256 '91977be7e0e0c5929ca5ac4edf6a9430ca2e83db8ca71ef5c9ed9082c160ef54'
 
-  url "http://www.elektriktrick.com/Downloads/ElektriktrickQL_#{version}.dmg"
+  url "http://www.elektriktrick.com/Downloads/Elektriktrick_#{version}.dmg"
   name 'Elektriktrick STL and GCode Quicklook'
   homepage 'http://www.elektriktrick.com/sw_quicklook.html'
 
-  qlplugin 'ElektrikTrickQuickLook.qlgenerator'
+  app 'ElektrikTrick.app'
 end
