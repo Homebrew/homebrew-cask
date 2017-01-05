12ac53f31cbed54baca0ef4bcf1b4cc4eaed015f
Joe Wicentowski <joewiz@gmail.com>
2017-01-05 10:54:28 -0500

    Add Oxygen XML Editor.app v18.1

---
+++ Casks/oxygen-xml-editor.rb
@@ -0,0 +1,10 @@
+cask 'oxygen-xml-editor' do
+  version '18.1'
+  sha256 '9e94a7329818ad158fb62e335a381bcdee6e85d3c7848fa6020a87dc39a0bca4'
+
+  url 'http://mirror.oxygenxml.com/InstData/Editor/MacOSX/VM/oxygen.dmg'
+  name 'oXygen XML Editor'
+  homepage 'https://www.oxygenxml.com/xml_editor.html'
+
+  suite 'Oxygen XML Editor'
+end
