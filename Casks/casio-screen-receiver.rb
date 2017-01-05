70daf5ccedacbc93112ff3bf9bf9000f24177fe4
Numbermaniac <lleytonj73@gmail.com>
2017-01-05 18:12:14 +1100

    update version,url,pkg

--- Casks/casio-screen-receiver.rb
+++ Casks/casio-screen-receiver.rb
@@ -1,12 +1,12 @@
 cask 'casio-screen-receiver' do
-  version '3.02_b,4'
+  version '3.02,b:4'
   sha256 '5eaff7fd7894d483534d82e7fbc6b77a6d304afa580e10b1c7bce38a3a69e07a'
 
-  url "https://edu.casio.com/education/support_software/dl/screen_Receiver/screenrecv_inst_302b_#{version.after_comma}.zip" # 302b should be interpolated from the version
+  url "https://edu.casio.com/education/support_software/dl/screen_Receiver/screenrecv_inst_#{version.before_comma_no_dots}#{version.after_comma_before_colon}_#{version.after_colon}.zip" # 302b should be interpolated from the version
   name 'Casio Screen Receiver'
   homepage 'https://edu.casio.com/education/support_software/'
 
-  pkg "ScreenReceiver_Ver.#{version.before_comma}.pkg"
+  pkg "ScreenReceiver_Ver.#{version.before_comma}_#{version.after_comma_before_colon}.pkg"
 
   uninstall pkgutil: [
                        'com.casio.fx.ScreenReceiver.ScreenReceiver',
