34a9809b19e63654859132b9cce11d15684c9c47
Numbermaniac <lleytonj73@gmail.com>
2017-01-05 15:49:26 +1100

    delete app before directory

--- Casks/casio-screen-receiver.rb
+++ Casks/casio-screen-receiver.rb
@@ -13,6 +13,7 @@ cask 'casio-screen-receiver' do
                        'com.casio.fx.ScreenReceiver.fxASPIDriver',
                        'com.casio.fx.ScreenReceiver.fxSRManual',
                      ],
+            delete:  "#{appdir}/CASIO/ScreenReceiver.app",
             rmdir:   "#{appdir}/CASIO"
 
   zap delete: '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.casio.fx.screenreceiver.sfl'
