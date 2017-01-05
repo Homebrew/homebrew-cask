ea1592979191938d24b02c11255739c212d259fc
Colin Dunn <cgd@vikingdev.net>
2017-01-05 14:55:05 +1100

    Update magicavoxel to 0.98 (#28601)

--- Casks/magicavoxel.rb
+++ Casks/magicavoxel.rb
@@ -1,6 +1,6 @@
 cask 'magicavoxel' do
-  version '0.97.5'
-  sha256 'dc11b8ed8302557cdb2a62bc4dcbd041b4f9d106d80de502cb098eef8935d404'
+  version '0.98'
+  sha256 '8fb7dc065ede34fa64203faf6da8bd9c54d65950177e0601225880d94010f83d'
 
   # 23.98.147.40 was verified as official when first introduced to the cask
   url "http://23.98.147.40/uploads/MagicaVoxel-#{version}-win-mac.zip"
