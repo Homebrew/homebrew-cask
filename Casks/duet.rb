023fe0dac80ab5fc2bae4dee43804354d6842d33
Benny Schudel <bennyschudel@gmail.com>
2017-01-05 08:54:21 +0100

    Update duet to 1.6.3.4

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -5,7 +5,7 @@ cask 'duet' do
   # s3-us-west-1.amazonaws.com/duetmac/ was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/duetmac/#{version.major_minor_patch.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip"
   appcast 'https://updates.duetdisplay.com/checkMacUpdates',
-          checkpoint: '5ced264c2c1161b2226ab307ca8dd8a9a42ad3db2ea50f4283a8a4092f8cb882'
+          checkpoint: 'ead13c59711365f14dbf78817c8e9eac21c112735792ca1631166dc0ef3c5ea4'
   name 'Duet'
   homepage 'https://www.duetdisplay.com/'
 
