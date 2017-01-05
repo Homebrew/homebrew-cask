459fadbc00fd835da151b51acfbfbf15657933fe
Joe Wicentowski <joewiz@gmail.com>
2017-01-05 14:45:57 -0500

    Use latest version since url doesn't point to a specific version

--- Casks/oxygen-xml-editor.rb
+++ Casks/oxygen-xml-editor.rb
@@ -1,6 +1,6 @@
 cask 'oxygen-xml-editor' do
-  version '18.1'
-  sha256 '9e94a7329818ad158fb62e335a381bcdee6e85d3c7848fa6020a87dc39a0bca4'
+  version 'latest'
+  sha256 :no_check
 
   url 'http://mirror.oxygenxml.com/InstData/Editor/MacOSX/VM/oxygen.dmg'
   name 'oXygen XML Editor'
