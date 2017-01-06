ee787e5e396ff73b694f8d50cda28c501db07bf1
Pablo Varela <pablopunk@users.noreply.github.com>
2017-01-06 13:55:59 -0500

    Update healthi to 1.0.3 (#28682)
    * Update healthi to 1.0.3
    
    * Fix checkpoint in healthi app
    
    * Add # to {version}

--- Casks/healthi.rb
+++ Casks/healthi.rb
@@ -1,10 +1,10 @@
 cask 'healthi' do
-  version '1.0.2'
-  sha256 'e519636ba2424c91806d26eefcd6a37cfbb6af47cfa4e02902ddfc8379034d72'
+  version '1.0.3'
+  sha256 '06152346caae837a2fab3ab905de40e1919a88de73838c35bbcc5a31cf073dd4'
 
-  url "https://github.com/pablopunk/healthi/releases/download/v#{version}/healthi.app.zip"
+  url "https://github.com/pablopunk/healthi/releases/download/#{version}/healthi.app.zip"
   appcast 'https://github.com/pablopunk/healthi/releases.atom',
-          checkpoint: '9af2a5dd2ca01556a71989dab2eb83ef1eb62127a0289701fd481d3767fd9905'
+          checkpoint: 'c91f3f15824cf2db2652de3b3ce3cf49f649e9fe678c29d1989886c55f6acfc2'
   name 'healthi'
   homepage 'https://github.com/pablopunk/healthi'
 
