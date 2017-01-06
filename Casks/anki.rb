3e316705de4b494131c4604f40cfbd254abb5788
fcccbf8f8f1 <fcccbf8f8f1@users.noreply.github.com>
2017-01-05 23:02:33 -0500

    Update anki to 2.0.38 (#28667)

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,10 +1,10 @@
 cask 'anki' do
   if MacOS.version <= :snow_leopard
-    version '2.0.36.qt47'
-    sha256 '365d11edfad2e828db58031af03b4af3fa95b18bbbb09e59c4ba97eb2fee7a97'
+    version '2.0.38-alternate'
+    sha256 '75c3f0043c85dac19720ce27d81deda69544f0c0a6465bd4fb1970814b07b9b4'
   else
-    version '2.0.36'
-    sha256 '4e229ace72b11b93186af31183ae365bb24c5f14d098b7c4bb564d16c3dfbc3c'
+    version '2.0.38'
+    sha256 'aa5bf1494c10fbdc6ea89a1ed3026c03634a8babf3f3330ddca7b9a2f4a01b87'
   end
 
   url "http://ankisrs.net/download/mirror/anki-#{version}.dmg"
