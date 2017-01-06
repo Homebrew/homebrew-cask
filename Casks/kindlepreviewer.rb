1557a9c5ee1637d7ab6c48da05049646aebab10f
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-06 08:53:52 +0800

    Update kindlepreviewer.rb

--- Casks/kindlepreviewer.rb
+++ Casks/kindlepreviewer.rb
@@ -5,7 +5,7 @@ cask 'kindlepreviewer' do
   # s3.amazonaws.com/kindlepreviewer3 was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/kindlepreviewer3/KindlePreviewerInstaller.pkg'
   name 'Kindle Previewer'
-  homepage 'https://www.amazon.com/gp/feature.html/?docId=1000765261'
+  homepage 'https://www.amazon.com/gp/feature.html/?docId=1003018611'
 
   pkg 'KindlePreviewerInstaller.pkg'
 
