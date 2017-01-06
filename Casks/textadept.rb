7473c25b02cde50ce582904a9b1e9ca5e7c92a44
Colin Dunn <cgd@vikingdev.net>
2017-01-06 14:18:25 +1100

    Update textadept to 9.2 (#28656)

--- Casks/textadept.rb
+++ Casks/textadept.rb
@@ -1,8 +1,10 @@
 cask 'textadept' do
-  version '8.3'
-  sha256 '25215aa18ce279fc312dd2a8e5b88eca32ce551991f5475cb12dbd491b24619d'
+  version '9.2'
+  sha256 '8fbc8f05ebbcd0118d72a2d1044415778aadc7a4c9bfba578f2964383797f66e'
 
   url "https://foicica.com/textadept/download/textadept_#{version}.osx.zip"
+  appcast 'https://foicica.com/textadept/feed',
+          checkpoint: '62e7aa917e28ee129b3942114bd18e635a6c0ed081a1ec71eecc75f661e44be4'
   name 'Textadept'
   homepage 'https://foicica.com/textadept/'
 
