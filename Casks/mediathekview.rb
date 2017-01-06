35c2a88f689c1b76a4d05cdc3600f0a4a3c19ca7
neutric <neutric@users.noreply.github.com>
2017-01-06 16:27:49 +0100

    Update mediathekview to 13.0.1 (#28680)

--- Casks/mediathekview.rb
+++ Casks/mediathekview.rb
@@ -1,10 +1,10 @@
 cask 'mediathekview' do
-  version '13'
+  version '13.0.1'
   sha256 '96c8ba822f171dab19472ae4d8e2120014540c833ca28c767fc913b49a31144a'
 
-  url "https://downloads.sourceforge.net/zdfmediathk/Mediathek/Mediathek%20#{version}/MediathekView-#{version}.dmg"
+  url "https://downloads.sourceforge.net/zdfmediathk/Mediathek/Mediathek%20#{version.major}/MediathekView-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/zdfmediathk/rss?path=/Mediathek',
-          checkpoint: '74248bb4c74b544fa9cfc53eb13ce709ed7a937dd205691b5a808a459e622b86'
+          checkpoint: 'bec541062bdddfcea5b0ecd4f3e678f9fb0ecd0a00fb1c9b6ec6175b95b86f47'
   name 'MediathekView'
   homepage 'https://sourceforge.net/projects/zdfmediathk/'
 
