885bf73960d56ea7864e590e34679d1d77ad5cc4
Guido Serra <guido.serra@olx.com>
2017-01-06 21:27:16 +0100

    Update Goad v1.3.0

--- Casks/goad.rb
+++ Casks/goad.rb
@@ -1,6 +1,6 @@
 cask 'goad' do
-  version '1.2.0.ci6'
-  sha256 'bdd79b7c70a343c551e2d1903c46bda59b3d7426794b4ca9575a76eca2ef36ca'
+  version '1.3.0'
+  sha256 '62c76c90b9fd6d1a1dba9058b57a7ba3af8e09df38d3ea3dc7809422b8c7a8d3'
 
   # github.com/zeph/goad was verified as official when first introduced to the cask
   url "https://github.com/zeph/goad/releases/download/v#{version}/goad-osx-x86-64.zip"
