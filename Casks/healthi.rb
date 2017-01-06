b03a51e3759646c6f5ccce2113617095343ef15a
Pablo Varela <pablovarela182@gmail.com>
2017-01-06 12:23:52 -0500

    Fix checkpoint in healthi app

--- Casks/healthi.rb
+++ Casks/healthi.rb
@@ -2,9 +2,9 @@ cask 'healthi' do
   version '1.0.3'
   sha256 '06152346caae837a2fab3ab905de40e1919a88de73838c35bbcc5a31cf073dd4'
 
-  url "https://github.com/pablopunk/healthi/releases/download/#{version}/healthi.app.zip"
+  url "https://github.com/pablopunk/healthi/releases/download/{version}/healthi.app.zip"
   appcast 'https://github.com/pablopunk/healthi/releases.atom',
-          checkpoint: '3983b08ab2502f69485120bf646f9f5fa228cdd5e55f28c076681b3175af0295'
+          checkpoint: 'c91f3f15824cf2db2652de3b3ce3cf49f649e9fe678c29d1989886c55f6acfc2'
   name 'healthi'
   homepage 'https://github.com/pablopunk/healthi'
 
