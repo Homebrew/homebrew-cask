b85e81ad4e7ed72c4f86bc5f2adfe774044c3ac6
Ben <BenjaminHCCarr@users.noreply.github.com>
2017-01-07 14:57:30 -0500

    [New] Add Objective-See Dylib Hijack Scanner 1.3.1
    ```
    Dylib Hijack Scanner
    Dylib Hijack Scanner or DHS, is a simple utility that will scan your computer for applications that are either susceptible to dylib hijacking or have been hijacked.
    ```
    
    https://objective-see.com/products/dhs.html

---
+++ Casks/dylib-hijack-scanner.rb
@@ -0,0 +1,11 @@
+cask 'dylib-hijack-scanner' do
+  version '1.3.1'
+  sha256 '3fc293da9f4730790e8c07833e0225fa6b57d2455bec53dd8e5b1e50de41c8d4'
+
+  # bitbucket.org/objective-see was verified as official when first introduced to the cask
+  url "https://bitbucket.org/objective-see/deploy/downloads/DHS_#{version}.zip"
+  name 'Dylib Hijack Scanner'
+  homepage 'https://objective-see.com/products/dhs.html'
+
+  app 'DHS.app'
+end
