e10c17072a504848ee63f4b9fc1d27aba37014bc
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-01-07 15:43:07 +0200

    Add Buttercup v0.6.1

---
+++ Casks/buttercup.rb
@@ -0,0 +1,10 @@
+cask 'buttercup' do
+  version '0.6.1'
+  sha256 :no_check
+
+  url "https://github.com/buttercup-pw/buttercup/releases/download/0.6.1-alpha/Buttercup-0.6.1-mac.zip"
+  name 'Buttercup'
+  homepage 'https://buttercup.pw'
+
+  app 'Buttercup.app'
+end
