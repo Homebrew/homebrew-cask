1b1725d0eb173339b7afcd8778171485de1580ce
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-01-07 15:55:06 +0200

    Added Buttercup url verification

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -2,6 +2,7 @@ cask 'buttercup' do
   version '0.6.1'
   sha256 :no_check
 
+  # github.com/buttercup-pw/buttercup was verified as official when first introduced to the cask
   url "https://github.com/buttercup-pw/buttercup/releases/download/0.6.1-alpha/Buttercup-0.6.1-mac.zip"
   name 'Buttercup'
   homepage 'https://buttercup.pw'
