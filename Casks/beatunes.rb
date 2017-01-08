bba39f7bbcf61baa4300a2d587f5089afa0d5faa
Colin Dunn <cgd@vikingdev.net>
2017-01-09 09:39:21 +1100

    Update beatunes to 4.6.10 (#28766)

--- Casks/beatunes.rb
+++ Casks/beatunes.rb
@@ -1,6 +1,6 @@
 cask 'beatunes' do
-  version '4.6.8'
-  sha256 'a59f76de6fbc4a5eb93296961fa587094a4d898b326746cbb520e6d3fdeddfc8'
+  version '4.6.10'
+  sha256 '61044a00678fde4a718c1178871fb68d679076b230cabe9609c4c6741f516b75'
 
   url "http://coxy.beatunes.com/download/beaTunes-#{version.dots_to_hyphens}.dmg"
   name 'beaTunes'
