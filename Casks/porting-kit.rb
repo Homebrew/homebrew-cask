0c4964619ae2bc595fea61dc4ffd2d89867e8591
Victor Popkov <victor@popkov.me>
2017-01-08 22:31:39 +0200

    Update porting-kit to 2.4.185 (#28758)

--- Casks/porting-kit.rb
+++ Casks/porting-kit.rb
@@ -1,10 +1,10 @@
 cask 'porting-kit' do
-  version '2.4.171'
-  sha256 'cba53106d07992f06d8360e8390510958fc668041987063d8c66ddb12d1acd0a'
+  version '2.4.185'
+  sha256 '7b608cab1e7d15a11db769c3cfbd651fe61114a902168bb75d629ce18c3997b8'
 
   url "http://portingkit.com/kit/Porting%20Kit%20#{version}.zip"
   appcast 'http://portingkit.com/kit/updatecast.xml',
-          checkpoint: '777d576f0b8e31de1a9bf2484b52658a312085f89de7b1bb9fb32c16332e20cf'
+          checkpoint: '7b111a9b16065c844628623fe868293634dac11dab4af0dbd63c8d2c1ab913ab'
   name 'Porting Kit'
   homepage 'http://portingkit.com/en/'
 
