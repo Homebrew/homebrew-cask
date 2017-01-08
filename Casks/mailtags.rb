b712c009ad08c78d237d63dd3cf76802e93d594a
Vítor Galvão <info@vitorgalvao.com>
2017-01-08 00:08:45 +0000

    Update mailtags.rb

--- Casks/mailtags.rb
+++ Casks/mailtags.rb
@@ -2,11 +2,11 @@ cask 'mailtags' do
   version '5.0.3'
   sha256 '5c618299dcbc742d7bc2287209eb8d902d512381f6cded596b0612e645e15870'
 
-  url 'https://smallcubed.com/download/mt/MailTags5.0.3.dmg'
+  url "https://smallcubed.com/download/mt/MailTags#{version}.dmg"
   name 'MailTags'
   homepage 'https://smallcubed.com/mt/'
 
-  installer manual: 'Install MailTags 5.app'
+  installer manual: "Install MailTags #{version.major}.app"
 
   zap delete: [
                 '~/Library/Mail/Bundles/MailTags.mailbundle',
