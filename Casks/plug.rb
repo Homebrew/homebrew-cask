4fef4861531f2815aecfea99aa288a6450402c62
Victor Popkov <victor@popkov.me>
2017-01-08 22:31:23 +0200

    Update plug to 2.0.15 (#28757)

--- Casks/plug.rb
+++ Casks/plug.rb
@@ -4,11 +4,11 @@ cask 'plug' do
     sha256 'd8eed07bd1f84d6f1daa7a0699617f2e21c01df2e68924945bcb4889d1251d01'
     url "https://plugformac.com/files/Plug-#{version}.dmg"
   else
-    version '2.0.14'
-    sha256 'afab04697b729bf6fda4042b2292c2c84803d5d1cdf18a491d4025e699403240'
+    version '2.0.15'
+    sha256 'e1a1e3fcfe7aeb5319833f407396cebaa27f5e3a01f3f904e622697c4093167e'
     url "https://www.plugformac.com/updates/plug#{version.major}/Plug-latest.dmg"
     appcast "https://www.plugformac.com/updates/plug#{version.major}/sparklecast.xml",
-            checkpoint: '46430df9121b04a98a6ccb058ebbd97e4e396760c3cf70e5902b2c4acf95f88b'
+            checkpoint: '9ec6a8e0979e3e23bc1facf217ac2f9d868e512ecd25abfa20e5300556e05f62'
   end
 
   name 'Plug'
