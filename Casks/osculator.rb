82e845c738c3b97ff132b09c21f1d81018c6769a
Vítor Galvão <info@vitorgalvao.com>
2017-01-08 11:20:04 +0000

    Update osculator.rb

--- Casks/osculator.rb
+++ Casks/osculator.rb
@@ -1,9 +1,9 @@
 cask 'osculator' do
-  version '3.1.1'
+  version '3.1.1-7-g064b670,8f8c7fe3-196c-44c2-8b93-e7507b749fdf'
   sha256 '960bc3536cbf27fd0307d8e08d5d855f0f796f14b2016df4e6835818ffac2817'
 
   # distribution.wildora.net was verified as official when first introduced to the cask
-  url 'https://distribution.wildora.net/products/osculator-v3/revisions/8f8c7fe3-196c-44c2-8b93-e7507b749fdf/osculator-3.1.1-7-g064b670.dmg'
+  url "https://distribution.wildora.net/products/osculator-v#{version.major}/revisions/#{version.after_comma}/osculator-#{version.before_comma}.dmg"
   name 'OSCulator'
   homepage 'https://osculator.net/'
 
