ae2735029cee8af72a8f29b0d889c658a067ff52
Alexander Hamann <neutric@users.noreply.github.com>
2017-01-09 22:02:00 +0100

    Update hawk-eye to 0.2.0

--- Casks/hawk-eye.rb
+++ Casks/hawk-eye.rb
@@ -1,10 +1,10 @@
 cask 'hawk-eye' do
-  version '0.1.0'
-  sha256 'f3a499f056f3a7e1f40dd60eda67503ae70b23bfc52c3bddd1befb16f02f1f08'
+  version '0.2.0'
+  sha256 'f62dbef8556a8b9d698ca0e10fb7aa550fc1a8a854c39f9417ddb158db9d2a09'
 
   url "https://github.com/harksys/HawkEye/releases/download/#{version}/hawkeye-#{version}-osx-x64.dmg"
   appcast 'https://github.com/harksys/HawkEye/releases.atom',
-          checkpoint: '150c01ef0b020ff10b357c7835db5f41d92cf5f0673f7bc22b60663307a6dd61'
+          checkpoint: 'cdb54347126b720f323668e929ff42e88a2795a32a0cfc7a0a86c8ba5752e009'
   name 'Hawk Eye'
   homepage 'https://github.com/harksys/HawkEye'
 
