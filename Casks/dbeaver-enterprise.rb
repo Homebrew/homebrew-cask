97702d0ff612a91ee926ef3e0624def7c9aac077
Colin Dunn <dunnc@amazon.com>
2017-01-10 08:12:57 +1100

    Update dbeaver-enterprise to 3.8.3

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,10 +1,10 @@
 cask 'dbeaver-enterprise' do
-  version '3.8.2'
-  sha256 '8061dfcebf357586938e0f89bbd774a0fc9cd501afa1cef218e5754b529d9f70'
+  version '3.8.3'
+  sha256 '447d50d3615f5045f2c24c5cac64bbed55e3cfd792258e00abb427fcb90a8bb0'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   appcast 'http://dbeaver.jkiss.org/files/',
-          checkpoint: '248a14eb2cac6358f84b3043f226f9a06125ae2c4a3a497d59fd133652a56842'
+          checkpoint: '5b090f47cf3307e1a20ab8afdfdbb5e8374a9171445620ad85cdd0614cade71c'
   name 'DBeaver Enterprise Edition'
   homepage 'http://dbeaver.jkiss.org/'
 
