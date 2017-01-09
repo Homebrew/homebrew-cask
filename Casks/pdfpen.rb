55bd0c7f7662041d2c5284f2a6f4bc8a5c9163d2
Victor Popkov <victor@popkov.me>
2017-01-10 01:01:33 +0200

    Update pdfpen to 831.0,1483740279 (#28796)

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -1,10 +1,10 @@
 cask 'pdfpen' do
-  version '830.1,1481080041'
-  sha256 'a43aaac84cc66d58213356471622432c685e7076ba31889740e090fd9924d54f'
+  version '831.0,1483740279'
+  sha256 'fc3ffc67a4c072e99534c6d0b2d3f89beb8830353b88bc1f852ab9f597acf594'
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpen/#{version.before_comma}/#{version.after_comma}/PDFpen-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml',
-          checkpoint: 'cbe84f9d3e6f4f1be74c15b6f1e653db128807ea69e9ed0a9d9c22a0080c8c69'
+          checkpoint: 'e9eb182b8d662af31ef78554fd3ec20a7d94850a81f0763a5a3135abb754b51e'
   name 'PDFpen'
   homepage 'https://smilesoftware.com/PDFpen'
 
