fff598f215576175b8533ba4867cd0f1fc007986
Colin Dunn <dunnc@amazon.com>
2017-01-09 16:35:51 +1100

    Update vuescan to 9.5.64

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,5 +1,5 @@
 cask 'vuescan' do
-  version '9.5.41'
+  version '9.5.64'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
