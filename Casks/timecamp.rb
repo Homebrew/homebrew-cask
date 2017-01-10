6ad05b7bd201a707763f8cc11ca9d75b9edbfc22
Christian Boynton <cboynton@cweil.com>
2017-01-10 12:56:15 -0800

    Add TimeCamp v1.3.78

--- Casks/timecamp.rb
+++ Casks/timecamp.rb
@@ -2,7 +2,6 @@ cask 'timecamp' do
   version '1.3.78'
   sha256 '69049d5ca4b12166fa91724d01762310bc9d09326e4ad3f91822a3ac77b2fb46'
 
-  # downloads page is https://www.timecamp.com/helper/download
   url 'https://www.timecamp.com/downloadsoft/TimeCamp.dmg'
   name 'TimeCamp'
   homepage 'https://www.timecamp.com/'
@@ -10,6 +9,6 @@ cask 'timecamp' do
   app 'TimeCamp.app'
 
   zap rmdir: [
-               '~/Library/Application Support/TimeCamp'
+               '~/Library/Application Support/TimeCamp',
              ]
 end
