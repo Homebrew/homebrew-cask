fb1caaa7832eaf53fbf6593773ce39e9281a3959
Colin Dunn <dunnc@amazon.com>
2017-01-11 09:59:20 +1100

    Update fish to 2.4.0

--- Casks/fish.rb
+++ Casks/fish.rb
@@ -1,6 +1,6 @@
 cask 'fish' do
-  version '2.3.1'
-  sha256 'd42bfd0137043a7f372590b78354a6a0153987bad2ddfdd38401ef192b5bf6cd'
+  version '2.4.0'
+  sha256 '2736219f658c21d965adee3d4728b4d04d1948f67345871b2a33dd983eebee90'
 
   url "https://fishshell.com/files/#{version}/fish-#{version}.app.zip"
   name 'Fish App'
