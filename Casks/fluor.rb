8b3ed83fe06bdbbb7bba9db53944b1bff188bb17
Pierre TACCHI <pyroh@icloud.com>
2017-01-10 22:29:27 +0100

    Update fluor.rb

--- Casks/fluor.rb
+++ Casks/fluor.rb
@@ -1,6 +1,6 @@
 cask 'fluor' do
   version '1.0.1'
-  sha256 :no_check
+  sha256 b1f8e0d40e134f5cc3b321839e6fbfda0c4cabdd8876c0abd0befef20805444f
 
   url "https://github.com/Pyroh/Fluor/releases/download/#{version}/Fluor.#{version}.dmg"
   name 'Fluor'
