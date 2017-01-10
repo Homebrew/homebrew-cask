d198731c6cc0fa772f36e2781d8e3ecaa8d00341
Colin Dunn <cgd@vikingdev.net>
2017-01-10 22:27:58 +1100

    Update retro-virtual-machine to 1.1.4 (#28826)

--- Casks/retro-virtual-machine.rb
+++ Casks/retro-virtual-machine.rb
@@ -1,6 +1,6 @@
 cask 'retro-virtual-machine' do
-  version '1.0.1'
-  sha256 '2144c713889780fcc1b4f9e994f4466514f9434974c942406f966145adaf899d'
+  version '1.1.4'
+  sha256 '58e796a910a0f4079bd0189d92eced6b137ab55fe83ceff8e5b1fa587373031f'
 
   url "http://www.retrovirtualmachine.org/release/Retro%20Virtual%20Machine%20v#{version}.dmg"
   name 'Retro Virtual Machine'
