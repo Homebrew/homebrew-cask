e8e06af61d2cdb850a9e933ad2917308bfff2db3
Pyroh <pyroh@icloud.com>
2017-01-10 22:17:04 +0100

    Added Fluor.app

---
+++ Casks/fluor.rb
@@ -0,0 +1,10 @@
+cask 'fluor' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/Pyroh/Fluor/releases/download/1.0.1/Fluor.1.0.1.dmg'
+  name 'Fluor'
+  homepage 'https://github.com/Pyroh/Fluor/'
+
+  app 'Fluor.app'
+end
