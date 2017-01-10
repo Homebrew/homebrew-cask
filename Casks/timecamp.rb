d98ccb3b5a7f1167fcd7af96d4bd34aecf877e42
Vítor Galvão <info@vitorgalvao.com>
2017-01-10 21:24:23 +0000

    Update timecamp.rb

--- Casks/timecamp.rb
+++ Casks/timecamp.rb
@@ -1,6 +1,6 @@
 cask 'timecamp' do
-  version '1.3.78'
-  sha256 '69049d5ca4b12166fa91724d01762310bc9d09326e4ad3f91822a3ac77b2fb46'
+  version :latest
+  sha256 :no_check
 
   url 'https://www.timecamp.com/downloadsoft/TimeCamp.dmg'
   name 'TimeCamp'
@@ -8,7 +8,5 @@ cask 'timecamp' do
 
   app 'TimeCamp.app'
 
-  zap rmdir: [
-               '~/Library/Application Support/TimeCamp',
-             ]
+  zap rmdir: '~/Library/Application Support/TimeCamp'
 end
