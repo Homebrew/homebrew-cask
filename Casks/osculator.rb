99f60a7356069cc2e18b3bd60a16b3895845865d
Colin Dunn <dunnc@amazon.com>
2017-01-12 07:39:13 +1100

    Add discovered appcasts to 10 casks #10

--- Casks/osculator.rb
+++ Casks/osculator.rb
@@ -4,6 +4,8 @@ cask 'osculator' do
 
   # distribution.wildora.net was verified as official when first introduced to the cask
   url "https://distribution.wildora.net/products/osculator-v#{version.major}/revisions/#{version.after_comma}/osculator-#{version.before_comma}.dmg"
+  appcast 'https://osculator.net/downloadthanks.php?stable',
+          checkpoint: 'f05f25b4bdc6e493e45619036b4037e5fb2918a61803b7cfd27912b10dba8843'
   name 'OSCulator'
   homepage 'https://osculator.net/'
 
--- Casks/phoenix-slides.rb
+++ Casks/phoenix-slides.rb
@@ -2,7 +2,10 @@ cask 'phoenix-slides' do
   version '1.4.2'
   sha256 'bde43c827972b8539198a0838e59e5d8f0fb3376a767eb04e467b584c832d239'
 
-  url "http://blyt.net/phxslides/phoenix-slides-#{version.no_dots}.dmg"
+  # github.com/gobbledegook/creevey/releases/download was verified as official when first introduced to the cask
+  url "https://github.com/gobbledegook/creevey/releases/download/v#{version}/phoenix-slides-#{version.no_dots}.dmg"
+  appcast 'https://github.com/gobbledegook/creevey/releases.atom',
+          checkpoint: '6965560912b64694cbd614f48d59b66b2511cffd46005301d4467edb87c5b3cd'
   name 'Phoenix Slides'
   homepage 'https://blyt.net/phxslides/'
 
