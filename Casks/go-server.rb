cae7192cfd244d05a00288b7f95c2372640f067a
Colin Dunn <cgd@vikingdev.net>
2017-01-11 17:26:23 +1100

    Add discovered appcasts to 10 casks #4 (#28878)

--- Casks/go-server.rb
+++ Casks/go-server.rb
@@ -3,6 +3,8 @@ cask 'go-server' do
   sha256 'f0b7f7f401e1a11022978c908f02409c0deec9c1cd5d80a6ae334bd82a7749e9'
 
   url "https://download.gocd.io/binaries/#{version}/osx/go-server-#{version}-osx.zip"
+  appcast 'https://github.com/gocd/gocd/releases.atom',
+          checkpoint: '6539c8829706600fcd7c5b25ee83d46c90946cf06b66f053f789ad57ceb911e7'
   name 'Go Server'
   homepage 'https://www.gocd.io/'
 
