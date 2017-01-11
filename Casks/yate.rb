82bc010f7b30f082dd2c387d8ba57a1dc222406a
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-11 11:39:56 +0800

    sha updates

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,11 +1,11 @@
 cask 'yate' do
   version '3.14.3.4'
-  sha256 '716b87b1a54bd0f91aa48ee4bf3062d09c8fce0f19d0ca5f212c7635c6cb00f6'
+  sha256 'a6e7306de1b5fc485702ce2b1177c7f05ffab66ef26cace046777d7866edb17b'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip',
       using: :post
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '04e543a96a8bd7780b5b98c91546bae583d2fbffaf28068cf008e019119c7465'
+          checkpoint: '6ca07e56be7b8e2791e348e6157f1b7a02f002095e328c4462276572b8f34870'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
 
