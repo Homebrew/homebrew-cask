83535d2555c77ee89f59a1be5590903ee7bcdfbd
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-12 06:58:48 +0800

    Update kismac.rb

--- Casks/kismac.rb
+++ Casks/kismac.rb
@@ -1,6 +1,6 @@
 cask 'kismac' do
-  version '0.3.4'
-  sha256 '62408ccc503c104ede70f8ad1ee55998a6c60ad89411fc2a57ce6be7bab39db5'
+  version :latest
+  sha256 :no_check
 
   url 'http://downloads.igrsoft.com/beta/KisMac2.zip'
   name 'KisMac'
