848151cdf46ee6543f249cad2c912fd04ade3b18
Colin Dunn <dunnc@amazon.com>
2017-01-11 16:29:05 +1100

    Add discovered appcasts to 10 casks #2

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -3,6 +3,8 @@ cask 'aws-vault' do
   sha256 'aead4c87af8eb13fe9a28ba955d77760b774176f1b286dd5c4a94ebda7a071c3'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64"
+  appcast 'https://github.com/99designs/aws-vault/releases.atom',
+          checkpoint: '72632fdb5d8302f33360e4d3dd65c0255679be1fc53e0a59f1d812a24ce3c988'
   name 'aws-vault'
   homepage 'https://github.com/99designs/aws-vault'
 
--- Casks/carmaintenance.rb
+++ Casks/carmaintenance.rb
@@ -3,6 +3,8 @@ cask 'carmaintenance' do
   sha256 'df6cad8772ca8edcc18c721489b3e2c0c4aa0a61bb18acd443d621b76b6393aa'
 
   url "http://www.traxxsoftware.com/downloads/CarMaintenance_#{version.dots_to_underscores}.zip"
+  appcast 'http://www.traxxsoftware.com/download.html',
+          checkpoint: '99f9ae58fc6a4a6c4cf95f38f7531aa947b00aa21fb08ce6215f170b2232e13a'
   name 'CarMaintenance'
   homepage 'http://www.traxxsoftware.com/download.html'
 
