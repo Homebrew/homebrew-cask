951b54763d52bef3be14f88271cb984141a877ff
Artem Medvedev <amedvedev@outlook.com>
2017-01-11 17:13:36 +0200

    Update mockuuups-studio to 1.1.2 (#28888)

--- Casks/mockuuups-studio.rb
+++ Casks/mockuuups-studio.rb
@@ -1,6 +1,6 @@
 cask 'mockuuups-studio' do
-  version '1.1.0'
-  sha256 '93fb62e9cce1cb7bae025df86e0130865d0ed827f36db0b353820ba622f33275'
+  version '1.1.2'
+  sha256 'b030a95b698942967f83f6e83fb4aaa1514c6f96004228af28a23f7b8b27b315'
 
   # mockuuups.com was verified as official when first introduced to the cask
   url "https://binaries.mockuuups.com/Mockuuups%20Studio-#{version}.dmg"
