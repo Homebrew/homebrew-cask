eca97428c92fa1231cda2c4135ad5425ff4e4216
Colin Dunn <dunnc@amazon.com>
2017-01-11 11:59:24 +1100

    Update komodo-edit to 10.1.4-17456

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -1,6 +1,6 @@
 cask 'komodo-edit' do
-  version '10.1.1-17414'
-  sha256 'ff5f06fa202bb82b4e30f55f03a8ea657fcf4dbcc1af8dfd9c73660bf824a406'
+  version '10.1.4-17456'
+  sha256 'be6f046d0b12a6f5c5aa8badc730de25181d9f473f40f7f49dbd8e46057dc03a'
 
   # activestate.com/Komodo was verified as official when first introduced to the cask
   url "https://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*}, '')}/Komodo-Edit-#{version}-macosx-x86_64.dmg"
