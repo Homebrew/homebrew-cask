bd18ce35fc6390272aa9a75e8cc49d99eacbe421
Victor Popkov <victor@popkov.me>
2017-01-11 23:49:24 +0200

    Update lacona to 1.1.3 (#28905)

--- Casks/lacona.rb
+++ Casks/lacona.rb
@@ -1,10 +1,10 @@
 cask 'lacona' do
-  version '1.1.2'
-  sha256 '07c410761091faad533073c10fbad7ad6802e0ce8aef6d1f1fa61f5b392fa2c9'
+  version '1.1.3'
+  sha256 '7fe34e71a8854de24396ca0b3c0dbf26f71bdbb75394c292f53450db85083be2'
 
   url "https://download.lacona.io/packages/#{version}/Lacona.zip"
   appcast 'https://download.lacona.io/appcast.xml',
-          checkpoint: 'fa0c84c376f2af8c8f9f7c87c42115bbd8e4c9304f6547282039146b2ed30898'
+          checkpoint: 'c7fcb976766ce370cea8326d97b538a20fe6f6e2dc05c4f119664c9d3f352ee3'
   name 'Lacona'
   homepage 'https://www.lacona.io/'
 
