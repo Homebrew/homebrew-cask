b196d6289252aa1447b33411546cbd3d12ef0688
Colin Dunn <dunnc@amazon.com>
2017-01-11 12:00:08 +1100

    Update veertu-desktop to 1.2.97

--- Casks/veertu-desktop.rb
+++ Casks/veertu-desktop.rb
@@ -1,11 +1,11 @@
 cask 'veertu-desktop' do
-  version '1.2.96'
-  sha256 '3f1cd0a0bd94c5096ad2be817b925bd5ceaa269f9c15d0ab5230262d280b1b07'
+  version '1.2.97'
+  sha256 '982500c343119bda7d49eaa037d485937ee36b3bf51934b165931339d85a114c'
 
   # d2sje6b9huarvp.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2sje6b9huarvp.cloudfront.net/VeertuDesktop-#{version}.dmg"
   appcast 'https://d2sje6b9huarvp.cloudfront.net/vdoaupd.rss',
-          checkpoint: '2b10ff8c4c6b8eb6fc0614bddf3ad5984505edf01bed6415202432d353580c1d'
+          checkpoint: 'fadbb21aa91efd48ebc69bf71a09cf13134dd1f7ea454d2e4df98d2b3c655611'
   name 'Veertu'
   homepage 'https://veertu.com/'
 
