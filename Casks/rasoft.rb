71749353c1ea1413526b7243ae5281305f9891ae
Colin Dunn <dunnc@amazon.com>
2017-01-11 18:41:44 +1100

    Add discovered appcasts to 10 casks #8

--- Casks/rasoft.rb
+++ Casks/rasoft.rb
@@ -3,6 +3,8 @@ cask 'rasoft' do
   sha256 'a285edb402ea394b1044f441236751403dae6399d46c455833f658af18a287ec'
 
   url "https://www.rakocontrols.com/rasoftx/release/Rasoft%20Pro-#{version}.dmg"
+  appcast 'https://rakocontrols.com/useful-information/rasoftpro/',
+          checkpoint: '9a6ff15d831c7d722d2a318f085af5769e2f85e3be10ca6ba28e3c80caaa8468'
   name 'Rasoft Pro'
   homepage 'https://rakocontrols.com/useful-information/rasoftpro/'
 
