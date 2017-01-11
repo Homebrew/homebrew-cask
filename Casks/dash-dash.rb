2bd7a2f4d818b276b70101edaa6a268d21a54844
Vítor Galvão <info@vitorgalvao.com>
2017-01-11 15:05:45 +0000

    Update dash-dash to 0.12.0.58

--- Casks/dash-dash.rb
+++ Casks/dash-dash.rb
@@ -2,7 +2,8 @@ cask 'dash-dash' do
   version '0.12.0.58'
   sha256 '09af72ac52e3c5a62a4eb735a0cd3609c90808a54663b0fe1e5bd6a5f9a7745c'
 
-  url "https://www.dash.org/binaries/dash-#{version}-osx.dmg"
+  # github.com/dashpay/dash was verified as official when first introduced to the cask
+  url "https://github.com/dashpay/dash/releases/download/v#{version}/dash-#{version}-osx.dmg"
   appcast 'https://github.com/dashpay/dash/releases.atom',
           checkpoint: 'ae9b81edbf9bb5651ddd278a9b1952b173198b45592f82f8cfd9c26fad700758'
   name 'Dash'
