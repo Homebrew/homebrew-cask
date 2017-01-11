10b9d4ba96d81ee6b95727946d85ebc6ddbc9b99
Colin Dunn <dunnc@amazon.com>
2017-01-11 16:01:42 +1100

    Add discovered appcasts

--- Casks/antwordprofiler.rb
+++ Casks/antwordprofiler.rb
@@ -3,6 +3,8 @@ cask 'antwordprofiler' do
   sha256 'b7cea938e38001b54fce28d1efb1336bbfe4f5af37559c7b3ffd3a14a8aed608'
 
   url "http://www.laurenceanthony.net/software/antwordprofiler/releases/AntWordProfiler#{version.no_dots}/AntWordProfiler.zip"
+  appcast 'http://www.laurenceanthony.net/software/antwordprofiler/releases/',
+          checkpoint: 'e90c95c59a2d69fe1bc13b2a3b8a16adb72c6c3d767660c51eee418a24db20fd'
   name 'AntWordProfiler'
   homepage 'http://www.laurenceanthony.net/software/antwordprofiler/'
 
