b5900ae469067cd83703190f7edd4acde95e215c
Delfim Machado <delfim@telecom.pt>
2017-01-12 20:45:53 +0000

    new santa

--- Casks/santa.rb
+++ Casks/santa.rb
@@ -1,10 +1,10 @@
 cask 'santa' do
-  version '0.9.15'
-  sha256 '8dde83663dd5e80770483f364e51a1b106c92291f84e707a4b31903104651a27'
+  version '0.9.16'
+  sha256 '76287d015bbcd0aa0425cd0410a84ee237ef279b0c2145abe9c258665f5ea98a'
 
   url "https://github.com/google/santa/releases/download/#{version}/santa-#{version}.dmg"
   appcast 'https://github.com/google/santa/releases.atom',
-          checkpoint: 'd988bc77ef0f75f77209fa6d8602677b2d8c0c4f1d4bc56a9dba1ea7180fc174'
+          checkpoint: 'ec812bdf9e237614dd5ddb88ba6e4bf2d8a57bca084afbbe867110596e901186'
   name 'Santa'
   homepage 'https://github.com/google/santa'
 
