518dffba990b7752026713cacd4550f19446531e
Colin Dunn <cgd@vikingdev.net>
2017-01-12 18:01:36 +1100

    Add discovered appcasts to 10 casks #16 (#28937)

--- Casks/xrg.rb
+++ Casks/xrg.rb
@@ -3,6 +3,8 @@ cask 'xrg' do
   sha256 '9c963b1ab5ddd4dba49e0e085e63e84b31613eff3e155f5e3dbb55c0abcfbe46'
 
   url "http://download.gauchosoft.com/xrg/XRG-release-#{version}.zip"
+  appcast 'http://www.gauchosoft.com/Products/XRG/',
+          checkpoint: '28e09c94daac04fa2bb14cfd3bbf156fc20eaaa2bb1bc75cf274b38b9a9d0b01'
   name 'XRG'
   homepage 'http://www.gauchosoft.com/Products/XRG/'
 
--- Casks/youtrack-workflow.rb
+++ Casks/youtrack-workflow.rb
@@ -3,6 +3,8 @@ cask 'youtrack-workflow' do
   sha256 '331bd2745696507da7ee3a6a193587fc53739face966c12736c16235211cda14'
 
   url "https://download-cf.jetbrains.com/charisma/youtrack-workflow-editor-#{version}-macos.zip"
+  appcast 'https://data.services.jetbrains.com/products/releases?code=YTWE&latest=true&type=release',
+          checkpoint: '2db4960d99f2b90571518bdeca5ad360d3c6df46573661a344daed7f61622547'
   name 'JetBrains Youtrack Workflow Editor'
   homepage 'https://www.jetbrains.com/youtrack/download/get_youtrack.html#materials=workflow-editor'
 
