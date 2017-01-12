b38517368dcc27adfa38a3b0f5c87ff24126f981
Colin Dunn <cgd@vikingdev.net>
2017-01-12 18:01:10 +1100

    Add discovered appcasts to 10 casks #14 (#28935)

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -3,6 +3,8 @@ cask 'steermouse' do
   sha256 '1e918e321bf33cd0f678c8730d4ebc3da281bc59fc32420201ad831c4015a701'
 
   url "http://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
+  appcast 'http://plentycom.jp/en/steermouse/download.php',
+          checkpoint: '2f5eaebe70063f0559d233c46689671634e3ad16e8aece63410795101689b917'
   name 'SteerMouse'
   homepage 'http://plentycom.jp/en/steermouse/'
 
