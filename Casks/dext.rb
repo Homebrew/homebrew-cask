e584dd1d2f3c6f1865e49418ba76e0d1f0086018
akz92 <aride.moulin@gmail.com>
2017-01-12 14:42:27 -0200

    Add appcast to Dext.app v0.7.1

--- Casks/dext.rb
+++ Casks/dext.rb
@@ -3,6 +3,8 @@ cask 'dext' do
   sha256 'f80441fe7daaa854104297b59a8fbef29a52c0886d3497896ebef8f82aff9a7d'
 
   url 'https://github.com/vutran/dext/releases/download/v0.7.1/Dext-darwin-x64.zip'
+  appcast 'https://github.com/vutran/dext/releases.atom',
+          checkpoint: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   name 'Dext'
   homepage 'https://github.com/vutran/dext'
 
