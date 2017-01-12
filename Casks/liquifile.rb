f243451c2404302c07f9a7a007ca2fff72a67d51
Colin Dunn <cgd@vikingdev.net>
2017-01-12 14:31:47 +1100

    Add discovered appcasts to 10 casks #7 (#28912)

--- Casks/liquifile.rb
+++ Casks/liquifile.rb
@@ -3,6 +3,8 @@ cask 'liquifile' do
   sha256 '81e8a0583af77fe9a2fbafa3433e73a5cb8567fdd53e37119b431c4ea6ce242d'
 
   url 'http://www.liquifile.info/Liquifile.dmg'
+  appcast 'http://www.liquifile.info/',
+          checkpoint: 'c15e603a4ef343df45c22eea78b2035685b6b1f9a5f1ae0c884b8d1e01c24fba'
   name 'Liquifile'
   homepage 'http://www.liquifile.info/'
 
--- Casks/marble.rb
+++ Casks/marble.rb
@@ -3,6 +3,8 @@ cask 'marble' do
   sha256 '6d1bf3e02c34ef0df0d5d0311d580bfae1d5259e3db45036163a5330dc139c04'
 
   url "https://files.kde.org/marble/downloads/MacOSX/Marble-#{version}.dmg"
+  appcast 'https://marble.kde.org/install.php',
+          checkpoint: '2e380a4e00b3dd33424238e3f263c594baa4b5baf4c26ef51141bd9f94cf3fce'
   name 'Marble'
   homepage 'https://marble.kde.org/'
 
