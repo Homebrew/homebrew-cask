24cbd5bdd138d5f8447b5a13ce000f5498d5ddd9
Colin Dunn <cgd@vikingdev.net>
2017-01-12 16:59:41 +1100

    Add discovered appcasts to 10 casks #9 (#28929)

--- Casks/nisus-thesaurus.rb
+++ Casks/nisus-thesaurus.rb
@@ -3,6 +3,8 @@ cask 'nisus-thesaurus' do
   sha256 'ee9203ada1fa944ac4b7fc04f03ec58fd7c60ce1d73e6058321583f7dbf8ae5a'
 
   url "https://nisus.com/files/free/Thesaurus-v#{version.no_dots}.zip"
+  appcast 'https://nisus.com/Thesaurus/updates.php',
+          checkpoint: '08c437fa79d1ade081069f3d1bb0f8d0bce40abc66eeb3812e47f2f5259785fc'
   name 'Nisus Thesaurus'
   homepage 'https://nisus.com/Thesaurus/'
 
