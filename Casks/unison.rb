79c4c090330591c5b995beac9ab772e29e285af1
Colin Dunn <cgd@vikingdev.net>
2017-01-12 18:01:20 +1100

    Add discovered appcasts to 10 casks #15 (#28936)

--- Casks/unison.rb
+++ Casks/unison.rb
@@ -9,10 +9,12 @@ cask 'unison' do
     version '2.48.15'
     sha256 '89894d14c9ff3c4d6195cb6a8065a2849e6ad55951799eedf8879e1a257d3e11'
 
-    # unison-binaries.inria.fr was verified as official when first introduced to the cask
-    url "http://unison-binaries.inria.fr/files/Unison-OS-X-#{version}.zip"
+    # github.com/bcpierce00/unison/releases/download was verified as official when first introduced to the cask
+    url "https://github.com/bcpierce00/unison/releases/download/#{version}/Unison-OS-X-#{version}.zip"
   end
 
+  appcast 'https://github.com/bcpierce00/unison/releases.atom',
+          checkpoint: '03df713ad77a41b0df51dea0474894d2dfa7dc55a63474735497bde71509e763'
   name 'Panic Unison'
   homepage 'https://www.cis.upenn.edu/~bcpierce/unison/'
 
