a571b33718916bbd1606895eead62012d9ea7222
Vítor Galvão <info@vitorgalvao.com>
2017-01-12 16:32:16 +0000

    Update gpgtools.rb

--- Casks/gpgtools.rb
+++ Casks/gpgtools.rb
@@ -28,7 +28,7 @@ cask 'gpgtools' do
     system_command '/usr/bin/killall', args: ['-kill', 'gpg-agent']
 
     %w(gpg gpg2 gpg-agent).map { |exec_name| "/usr/local/bin/#{exec_name}" }.each do |exec|
-      File.rm(exec) if (File.exist?(exec) && File.readlink(exec))
+      File.rm(exec) if (File.exist?(exec) && File.readlink(exec).include?('MacGPG2'))
     end
   end
 
