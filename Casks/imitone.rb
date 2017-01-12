cce47827856c8f1e0ad63daddcadf11b9819c695
Vítor Galvão <info@vitorgalvao.com>
2017-01-12 22:51:22 +0000

    Update imitone to 0.9.0d

--- Casks/imitone.rb
+++ Casks/imitone.rb
@@ -1,8 +1,10 @@
 cask 'imitone' do
-  version '0.9.0c'
-  sha256 '654cd6a18bdb7bce708a715af54cf746b89421c440bbce6a957599fbd2f19f99'
+  version '0.9.0d'
+  sha256 'b88be27f7f2a349f387d0a01ba9c60ae9ac344b93d3f64995eed7c41dc35a432'
 
   url "https://imitone.com/beta/imitone-#{version}.dmg"
+  appcast 'https://imitone.com/beta/',
+          checkpoint: '375b3e8ebd03ac408f490ddf74846e3b8b00b60130639f933a78c9e08d3bc313'
   name 'imitone'
   homepage 'https://imitone.com/'
 
