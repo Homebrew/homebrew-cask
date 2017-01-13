5d608b61d81a175a29dbb6f7857a62fba6432a55
Ernesto Navarro <ernesto.navarro@gmail.com>
2017-01-13 13:38:38 +0100

    Update jaspersoft-studio to 6.3.1

--- Casks/jaspersoft-studio.rb
+++ Casks/jaspersoft-studio.rb
@@ -1,11 +1,11 @@
 cask 'jaspersoft-studio' do
   version '6.3.1'
-  sha256 'e2c0e713d3c5b309e1944c3d472f9dcea9e29d4c0ba439942f377ba9786fcd6a'
+  sha256 'c6bb50bd1f0e44e173db90e28080b6f08c63bd5fe0f62d5bc55b800ea85950a8'
 
   # sourceforge.net/jasperstudio was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/jasperstudio/TIBCOJaspersoftStudio-#{version}.final-mac-x86_64.dmg"
   appcast 'https://sourceforge.net/projects/jasperstudio/rss',
-          checkpoint: 'f90cd2b1b04de19ab2c8fcbdd5392018e55734b28ede1eaa3209c3957bc20707'
+          checkpoint: 'e46ce59c6a652c8bdca94d2dc96aa210b0cf003af2e445148497a229c4825a8a'
   name 'Jaspersoft Studio'
   homepage 'https://community.jaspersoft.com/project/jaspersoft-studio'
 
