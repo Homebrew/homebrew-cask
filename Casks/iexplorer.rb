ebab5de5e1a395a2f35d7f28c3bb6ea4b0b9b914
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-14 02:39:01 +0800

    Remove appcast

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -3,8 +3,6 @@ cask 'iexplorer' do
   sha256 'b9d222d53cdcc53f0137140d48ab01ff782860c9872397c7462d23f6dbedcb8b'
 
   url "https://assets.macroplant.com/downloads/iExplorer-#{version}.dmg"
-  appcast "https://macroplant.com/iexplorer/mac/v#{version.major}/appcast.xml",
-          checkpoint: '3088283e134ded801787373c3457f07a6ead123063150eba43f1358a0bf54d94'
   name 'iExplorer'
   homepage 'https://macroplant.com/iexplorer'
 
