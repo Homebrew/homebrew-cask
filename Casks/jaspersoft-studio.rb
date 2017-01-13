15a1cf31e98410f0ec7ec62ccbe36a9ffdb59140
Vítor Galvão <info@vitorgalvao.com>
2017-01-13 15:26:16 +0000

    Update jaspersoft-studio.rb

--- Casks/jaspersoft-studio.rb
+++ Casks/jaspersoft-studio.rb
@@ -5,7 +5,7 @@ cask 'jaspersoft-studio' do
   # sourceforge.net/jasperstudio was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/jasperstudio/TIBCOJaspersoftStudio-#{version}.final-mac-x86_64.dmg"
   appcast 'https://sourceforge.net/projects/jasperstudio/rss',
-          checkpoint: 'e46ce59c6a652c8bdca94d2dc96aa210b0cf003af2e445148497a229c4825a8a'
+          checkpoint: 'e2c0e713d3c5b309e1944c3d472f9dcea9e29d4c0ba439942f377ba9786fcd6a'
   name 'Jaspersoft Studio'
   homepage 'https://community.jaspersoft.com/project/jaspersoft-studio'
 
