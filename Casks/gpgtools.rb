814e4b9939b4af587f2871c0f187b02c69f6415b
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-14 02:18:55 +0800

    Update gpgtools.rb (#28939)
    * Update gpgtools.rb
    
    For reference: https://github.com/caskroom/homebrew-cask/issues/28911.
    
    * Update gpgtools.rb
    
    * Update
    
    * Update gpgtools.rb
    
    * Update gpgtools.rb
    
    * Update gpgtools.rb
    
    * Update gpgtools.rb

--- Casks/gpgtools.rb
+++ Casks/gpgtools.rb
@@ -25,39 +25,58 @@ cask 'gpgtools' do
   end
 
   uninstall_postflight do
-    system_command '/bin/bash', args: ['-c', '[[ "$(/usr/bin/readlink /usr/local/bin/gpg2)"      =~ MacGPG2 ]] && /bin/rm -- /usr/local/bin/gpg2']
-    system_command '/bin/bash', args: ['-c', '[[ "$(/usr/bin/readlink /usr/local/bin/gpg)"       =~ MacGPG2 ]] && /bin/rm -- /usr/local/bin/gpg']
-    system_command '/bin/bash', args: ['-c', '[[ "$(/usr/bin/readlink /usr/local/bin/gpg-agent)" =~ MacGPG2 ]] && /bin/rm -- /usr/local/bin/gpg-agent']
+    # TODO: add kill to uninstall quit
+    system_command '/usr/bin/killall', args: ['-kill', 'gpg-agent']
+
+    %w[gpg gpg2 gpg-agent].map { |exec_name| "/usr/local/bin/#{exec_name}" }.each do |exec|
+      File.rm(exec) if File.exist?(exec) && File.readlink(exec).include?('MacGPG2')
+    end
   end
 
-  uninstall pkgutil:   'org.gpgtools.*',
+  uninstall script:    {
+                         executable: "#{staged_path}/Uninstall.app/Contents/Resources/GPG Suite Uninstaller.app/Contents/Resources/uninstall.sh",
+                         sudo:       true,
+                       },
+            pkgutil:   'org.gpgtools.*',
             quit:      [
                          'com.apple.mail',
                          'org.gpgtools.gpgkeychainaccess',
+                         'org.gpgtools.gpgkeychain',
                          'org.gpgtools.gpgservices',
                        ],
             launchctl: [
-                         'org.gpgtools.macgpg2.shutdown-gpg-agent',
                          'org.gpgtools.Libmacgpg.xpc',
-                         'org.gpgtools.gpgmail.enable-bundles',
-                         'org.gpgtools.gpgmail.user-uuid-patcher',
-                         'org.gpgtools.gpgmail.uuid-patcher',
+                         'org.gpgtools.gpgmail.patch-uuid-user',
                          'org.gpgtools.macgpg2.fix',
+                         'org.gpgtools.macgpg2.shutdown-gpg-agent',
                          'org.gpgtools.macgpg2.updater',
+                         'org.gpgtools.macgpg2.gpg-agent',
                        ],
             delete:    [
-                         '/Applications/GPG Keychain Access.app',
-                         '/Applications/GPG Keychain.app',
-                         '/usr/local/MacGPG2',
                          '/Library/Services/GPGServices.service',
                          '/Library/Mail/Bundles/GPGMail.mailbundle',
+                         '/Network/Library/Mail/Bundles/GPGMail.mailbundle',
+                         '/usr/local/MacGPG2',
+                         '/private/etc/paths.d/MacGPG2',
+                         '/private/etc/manpaths.d/MacGPG2',
+                         '/private/tmp/gpg-agent',
                          '/Library/PreferencePanes/GPGPreferences.prefPane',
+                         '/Applications/GPG Keychain Access.app',
+                         '/Applications/GPG Keychain.app',
+                         '/Library/Application Support/GPGTools',
+                         '/Library/Frameworks/Libmacgpg.framework',
                        ]
 
   zap delete: [
                 '~/Library/Services/GPGServices.service',
                 '~/Library/Mail/Bundles/GPGMail.mailbundle',
                 '~/Library/PreferencePanes/GPGPreferences.prefPane',
+                '~/Library/LaunchAgents/org.gpgtools.*',
+                '~/Library/Preferences/org.gpgtools.*',
+                '~/Library/Containers/com.apple.mail/Data/Library/Preferences/org.gpgtools.*',
+                '~/Library/Application Support/GPGTools',
+                '~/Library/Frameworks/Libmacgpg.framework',
+                '~/Containers/com.apple.mail/Data/Library/Frameworks/Libmacgpg.framework',
                 # TODO: expand/glob for ~/Library/Caches/org.gpgtools.gpg*
               ]
 
