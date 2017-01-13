113be57742d39f067c556b96d6ca4be07791f04c
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-13 10:23:19 +0800

    Update appcast

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -3,8 +3,8 @@ cask 'iexplorer' do
   sha256 'b9d222d53cdcc53f0137140d48ab01ff782860c9872397c7462d23f6dbedcb8b'
 
   url "https://assets.macroplant.com/downloads/iExplorer-#{version}.dmg"
-  appcast "https://www.macroplant.com/iexplorer/ie#{version.major}-appcast.xml",
-          checkpoint: '48eaa402b874473b322a88d0af31c3642f1c2588a3337524a95a71c2d5fd564d'
+  appcast "https://macroplant.com/iexplorer/mac/v#{version.major}/appcast.xml",
+          checkpoint: '3088283e134ded801787373c3457f07a6ead123063150eba43f1358a0bf54d94'
   name 'iExplorer'
   homepage 'https://macroplant.com/iexplorer'
 
