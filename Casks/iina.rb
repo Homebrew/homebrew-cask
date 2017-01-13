93e1bbf3bbd781c4aa1b0f12a8349c28a06a1f65
Vítor Galvão <info@vitorgalvao.com>
2017-01-13 15:25:28 +0000

    Update iina.rb

--- Casks/iina.rb
+++ Casks/iina.rb
@@ -5,7 +5,7 @@ cask 'iina' do
   # github.com/lhc70000/iina was verified as official when first introduced to the cask
   url "https://github.com/lhc70000/iina/releases/download/v#{version}/IINA.v#{version}.zip"
   appcast 'https://github.com/lhc70000/iina/releases.atom',
-          checkpoint: '5ea6da7163d7181a2d86646edc7971f7fecb5841f9e5920de18487269f9da4b9'
+          checkpoint: 'c945776d76ba415244bd40a9cae05ea6e56772a7ce7a74bd05e3f050a6a7eba0'
   name 'IINA'
   homepage 'https://lhc70000.github.io/iina/'
 
