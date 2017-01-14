e3413e2c9341d4e1aacfb0214c872d69f659eb0e
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-15 00:33:44 +0800

    Migrate grenchman 0.2.0 from homebrew-binary (#29026)
    * Migrate grenchman 0.2.0 from homebrew-binary
    
    * Update grenchman.rb
    
    * Update grenchman.rb

---
+++ Casks/grenchman.rb
@@ -0,0 +1,13 @@
+cask 'grenchman' do
+  version '0.2.0'
+  sha256 'f70542dffc1f60e372b77582998570b02ff21c85f699fd1854c98e4359804bb6'
+
+  # technomancy.github.io/grenchman/downloads was verified as official when first introduced to the cask
+  url "https://technomancy.github.io/grenchman/downloads/grench-#{version}-mac"
+  name 'Grenchman'
+  homepage 'http://leiningen.org/grench.html'
+
+  depends_on formula: 'libffi'
+
+  binary "grench-#{version}-mac", target: 'grench'
+end
