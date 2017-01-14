f5988999c7c2e8ab702dea391ae4ed7f6300eb27
Christopher LIJLENSTOLPE <github@cdl.asgaard.org>
2017-01-13 19:16:08 -0800

    Update iridient-developer to 3.1.3 (#29012)

--- Casks/iridient-developer.rb
+++ Casks/iridient-developer.rb
@@ -1,6 +1,6 @@
 cask 'iridient-developer' do
-  version '3.1.2'
-  sha256 '740aa73096d8729dcd0b457b6b76f5f8056347a3f8104112999283814a5bddc1'
+  version '3.1.3'
+  sha256 '7b28bfad2541e32b497a8816d489099189ec9b24dc9bfafa958ccd0cfbfb6208'
 
   url "http://www.iridientdigital.com/downloads/IridientDeveloper_#{version.no_dots}.dmg"
   name 'Iridient Developer'
