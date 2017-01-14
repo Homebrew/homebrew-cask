16335ac2f317cd1aeca16f484da71b0e55bf59d6
Matthew Horton <mkhorton@users.noreply.github.com>
2017-01-13 23:00:51 -0800

    Add OVITO v2.8.1 (#29018)
    * Add OVITO v2.8.1
    
    * Update ovito.rb
    
    * Update ovito.rb
    
    Fixed url to include version

---
+++ Casks/ovito.rb
@@ -0,0 +1,10 @@
+cask 'ovito' do
+  version '2.8.1'
+  sha256 'ece52b1dc5b0b7e18380c4df105ecc6a6ffb9e3a8fe7cdef100744457b5a8d2a'
+
+  url "https://ovito.org/download/#{version}/ovito-#{version}-macos.dmg"
+  name 'OVITO'
+  homepage 'https://ovito.org'
+
+  app 'Ovito.app'
+end
