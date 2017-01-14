e8b408c4ec7a52245879a9b4bacc770c9de41aa9
mugukamil <mugukamil@gmail.com>
2017-01-14 16:08:37 +0300

    Update anki to 2.0.40

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -3,8 +3,8 @@ cask 'anki' do
     version '2.0.38-alternate'
     sha256 '75c3f0043c85dac19720ce27d81deda69544f0c0a6465bd4fb1970814b07b9b4'
   else
-    version '2.0.38'
-    sha256 'aa5bf1494c10fbdc6ea89a1ed3026c03634a8babf3f3330ddca7b9a2f4a01b87'
+    version '2.0.40'
+    sha256 '4089d1dfa5e94a161d9a2e8596f7622c9103046020b72590ab1d8be5f42687db'
   end
 
   url "http://ankisrs.net/download/mirror/anki-#{version}.dmg"
