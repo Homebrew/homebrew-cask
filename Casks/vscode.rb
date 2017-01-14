712c447a7742e8f899c4c7f9b55d14d2519ad40e
Microshit dot Not <microshit.dot.not@gmail.com>
2017-01-14 18:37:47 +0800

    Update vscode.rb

--- Casks/vscode.rb
+++ Casks/vscode.rb
@@ -3,7 +3,7 @@ cask 'vscode' do
   sha256 '9202c85669853b07d1cbac9e6bcb01e7c08e13fd2a2b759dd53994e0fa51e7a1'
 
   # azurewebsites.net was verified as official when first introduced to the cask
-  url 'https://vscode-update.azurewebsites.net/1.8.1/darwin/stable'
+  url "https://vscode-update.azurewebsites.net/#{version}/darwin/stable"
   name 'VSCode'
   name 'Visual Studio Code'
   homepage 'http://code.visualstudio.com/'
