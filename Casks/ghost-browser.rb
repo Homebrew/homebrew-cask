2562589f09d3bbbae245d323d205e912f4cf1d09
Deivide Oliveira <deivide.s.oliveira@gmail.com>
2017-01-13 21:30:25 -0800

    Add Ghost Browser.app v1.0.3.6

---
+++ Casks/ghost-browser.rb
@@ -0,0 +1,10 @@
+cask 'ghost-browser' do
+  version '1.0.3.6'
+  sha256 'c04f6f1e3c64c20d4d9fad06e1620454f64f04a5c1603664190c2189c947ed0e'
+
+  url "https://ghostbrowser.com/ghostdev/downloads/GhostBrowser-#{version}.dmg"
+  name 'Ghost Browser'
+  homepage 'https://ghostbrowser.com'
+
+  app 'Ghost Browser.app'
+end
