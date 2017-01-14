2e39618aad890b77ddec3e148964912cc3fb8bc7
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-14 14:33:49 +0800

    Fix url

--- Casks/pl2303.rb
+++ Casks/pl2303.rb
@@ -2,7 +2,7 @@ cask 'pl2303' do
   version '1.6.1_20160309'
   sha256 '14868e4a3c38904760d3445c37bbb5ca2f1024498547645147abbabfcb52eb87'
 
-  url "http://prolificusa.com/files/PL2303_MacOSX_#{version.dots_to_underscores}.zip"
+  url "http://prolificusa.com/files/PL2303_MacOSX_#{version}.zip"
   name 'Prolific USB-Serial Cable driver'
   homepage 'http://prolificusa.com/'
 
