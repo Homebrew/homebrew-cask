fe96f97e273be27238ea1813116b48228b90c354
Colin Dunn <dunnc@amazon.com>
2017-01-17 10:38:34 +1100

    Update ampps to 3.6

--- Casks/ampps.rb
+++ Casks/ampps.rb
@@ -1,10 +1,14 @@
 cask 'ampps' do
-  version '3.3'
-  sha256 '46a28cfda2478a9e7d8af789a28723cba48b51a4da772a371c245714b8a0532f'
+  version '3.6'
+  sha256 'e266be32a1b6206c95fd75c405451a53aee4319c1f62641cdb58e2f2dc809e94'
 
   url "http://files.ampps.com/AMPPS-#{version}.dmg"
   name 'AMPPS'
   homepage 'http://www.ampps.com/'
 
   suite 'AMPPS'
+
+  uninstall_preflight do
+    set_permissions "#{appdir}/AMPPS", '0777'
+  end
 end
