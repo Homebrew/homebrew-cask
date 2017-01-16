cf7f0a421e0324d41d5868a490344c3ae1a522f2
Shawn Hensley <sahcode@gmail.com>
2017-01-15 20:52:41 -0600

    Add SafariSort.app Latest

---
+++ Casks/safarisort.rb
@@ -0,0 +1,11 @@
+cask 'safarisort' do
+  version :latest
+  sha256 :no_check
+
+  # macupdate.com was verified as official when first introduced to the cask
+  url 'https://www.macupdate.com/download/34101/SafariSort.dmg'
+  name 'SafariSort'
+  homepage 'http://www.safarisort.com'
+
+  app 'SafariSort.app'
+end
