cec3e5955c5936418f81379b9a5b6c6b529a70a5
Artem Medvedev <amedvedev@outlook.com>
2017-01-16 16:40:01 +0200

    Update launchcontrol to 1.28.4

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,10 +1,10 @@
 cask 'launchcontrol' do
-  version '1.28.3'
-  sha256 'd5f08263c615846df220528e267e6b251627c894d6501ae4ad568756cc4ea17b'
+  version '1.28.4'
+  sha256 '57b274752809ddd3dcff65ab4df45425e27923937acfdabd7b51bbca5281fc47'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
-          checkpoint: '0fdbb5083496bdb5b2e4ccf1192af5be0316f24022d22c0b31aba7b9a6263505'
+          checkpoint: '542ebdb2f3c3a66d4431f493bb1dac1b455cfd306c92e3605cb1910e2b18602b'
   name 'LaunchControl'
   homepage 'http://www.soma-zone.com/LaunchControl/'
 
