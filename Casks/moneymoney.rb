4c54cfd256dd327d27affe5fcdca668819a21420
Andreas Zeidler <az@zitc.de>
2017-01-17 02:57:49 +0100

    Update moneymoney to 2.2.15 (#29088)

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,10 +1,10 @@
 cask 'moneymoney' do
   version '2.2.15'
-  sha256 'd9827e500af6e530eea02e92a73feb42200d262b5d6e427023f1c1bd95218515'
+  sha256 'be09bf954c7253d160b68dc775a5084c51b2440e590c033a96db728e9b08c052'
 
   url 'https://service.moneymoney-app.com/1/MoneyMoney.zip'
   appcast 'https://service.moneymoney-app.com/1/Appcast.xml',
-          checkpoint: 'fcdb0dab20cce8545360daaed1b47d85f67b25bcf2e7e3e87ea7618c3f6de337'
+          checkpoint: 'aa9b2c98eba69af2c5dbbcef1a84f07866e5c6fd38790fc043c31b19a2b3972a'
   name 'MoneyMoney'
   homepage 'https://moneymoney-app.com/'
 
