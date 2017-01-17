1de69aa8bffaf6fac53d510a62b45537b04379e5
Alexander Hamann <neutric@users.noreply.github.com>
2017-01-17 14:40:20 +0100

    Update virtualbox-extension-pack to 5.1.14-112924 and 5.0.32-112930

--- Casks/virtualbox-extension-pack.rb
+++ Casks/virtualbox-extension-pack.rb
@@ -3,14 +3,16 @@ cask 'virtualbox-extension-pack' do
     version '4.3.40-110317a'
     sha256 '829812f9a94204cd206968a8b8c3ca14f719f9cc8e3e1c6825b68c2c3da13033'
   elsif MacOS.version == :mountain_lion
-    version '5.0.30-112061'
-    sha256 'f0880b7948bdc185d7e7be0fc98b551296ed9578f21e38d01b43771323a71a3d'
+    version '5.0.32-112930'
+    sha256 '3a0c45eb2471566787def7d73f8c01b03a806e5b2042c21911c2142dafdf9a44'
   else
-    version '5.1.12-112440'
-    sha256 '03111380afb06122a494595e966dffe3e2779840e8698e27e80f87342b291286'
+    version '5.1.14-112924'
+    sha256 'baddb7cc49224ecc1147f82d77fce2685ac39941ac9b0aac83c270dd6570ea85'
   end
 
   url "http://download.virtualbox.org/virtualbox/#{version.sub(%r{-.*}, '')}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
+  appcast 'http://download.virtualbox.org/virtualbox/LATEST.TXT',
+          checkpoint: '69ff967697383e82adfccadcc6e493391648adb709c427d4fef9ded116c08798'
   name 'Oracle VirtualBox Extension Pack'
   homepage 'https://www.virtualbox.org/'
 
