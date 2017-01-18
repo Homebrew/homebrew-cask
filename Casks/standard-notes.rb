c602c2500d9f100111b376a439d009cbb963d1a4
greg <blackhole@flesh.space>
2017-01-18 09:18:48 -0500

    Add Standard Notes v0.1.1

---
+++ Casks/standard-notes.rb
@@ -0,0 +1,11 @@
+cask 'standard-notes' do
+  version '0.1.1'
+  sha256 '9b6a96ac9d00687f9fdd1ccd456ce36575228dd3a6b90662e8fe529bee04d955'
+
+  # github.com was verified as official when first introduced to the cask
+  url 'https://github.com/standardnotes/desktop/releases/download/v0.1.1/Standard.Notes-Mac-0.1.1.dmg'
+  name 'Standard Notes'
+  homepage 'https://standardnotes.org/'
+
+  app 'Standard Notes.app'
+end
