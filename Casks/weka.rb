4a04942af249e8204fe665632b1a7658dee1e3b2
SentinelWarren <warrenkalolo@gmail.com>
2017-01-18 21:14:33 +0300

    Update weka to 3.8.1

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -1,11 +1,11 @@
 cask 'weka' do
-  version '3.8.0'
-  sha256 '9d47fa04ff3b7d6ebc18ab79ee7da76cbbdabe2bf26f003d08317ae225fc3249'
+  version '3.8.1'
+  sha256 '7714dd0cddee3412d5092a3b439c9a06844134885c39f30f5ad63e89d852bd9b'
 
   # sourceforge.net/weka was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-oracle-jvm.dmg"
   appcast 'https://sourceforge.net/projects/weka/rss',
-          checkpoint: '63dc078aa81e58784909f2dd679e41b752c65403bea64221175ca73532190591'
+          checkpoint: '8a78728c5dd060f44cc26052dddc4c04ae07e3d5e8512b988178d09d16b910fa'
   name 'Weka'
   homepage 'http://www.cs.waikato.ac.nz/ml/weka/'
 
