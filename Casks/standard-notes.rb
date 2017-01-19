e841319a87f0906adca43913f0458e7d149de3e6
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-19 10:01:09 +0800

    Fix checkpoint

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -5,7 +5,7 @@ cask 'standard-notes' do
   # github.com was verified as official when first introduced to the cask
   url 'https://github.com/standardnotes/desktop/releases/download/v0.1.1/Standard.Notes-Mac-0.1.1.dmg'
   appcast 'https://github.com/standardnotes/desktop/releases.atom',
-    checkpoint: '727ba8f515d8c859bf0c05465a1be64d07951fd650b235460a8ef58e4f302954'
+          checkpoint: '727ba8f515d8c859bf0c05465a1be64d07951fd650b235460a8ef58e4f302954'
   name 'Standard Notes'
   homepage 'https://standardnotes.org/'
 
