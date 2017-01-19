237ee2047e452e0bf8c0f4eb6c65490b57af2a69
Jeffrey Armstrong <jeffrey.armstrong@approximatrix.com>
2017-01-19 13:13:33 -0500

    Update Simply Fortran to use version string interpolation in URL.

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -3,7 +3,7 @@ cask 'simply-fortran' do
   sha256 'bc35e918a62c605ba6a1a7ebbc8100a28f9250cd09a59346ab6dc78ed7238a5b'
 
   # download.approximatrix.com/sfortran/ was verified as official when first introduced to the cask
-  url 'https://download.approximatrix.com/sfortran/2.33/SimplyFortran-2.33.dmg'
+  url "https://download.approximatrix.com/sfortran/#{version}/SimplyFortran-#{version}.dmg"
   name 'Simply Fortran'
   homepage 'http://simplyfortran.com/'
 
