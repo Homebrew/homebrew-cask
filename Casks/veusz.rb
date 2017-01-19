43e99b361aa605508e2f2922a61ee6db64838d5c
SentinelWarren <warrenkalolo@gmail.com>
2017-01-19 18:36:17 +0300

    Update veusz to 1.25.1

--- Casks/veusz.rb
+++ Casks/veusz.rb
@@ -1,6 +1,6 @@
 cask 'veusz' do
-  version '1.23.1'
-  sha256 '67cada3e84e8a4f4629bb70ffe2bcbb5dc3836fc91e862054ffda98e91759bb5'
+  version '1.25.1'
+  sha256 '38d4144babf3a07845b018af610cdf4b21a951a9b7c200c904509dbbe1d2b7a7'
 
   url "http://download.gna.org/veusz/Binaries-AppleOSX/veusz-#{version}-AppleOSX.dmg"
   name 'Veusz'
