869ead858e62859b7b55f0669b5f2df15d67bb52
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-20 04:39:56 +0800

    Migrate jad 1.5.8g from homebrew-binary (#29179)
    * Migrate jad 1.5.8g from homebrew-binary
    
    * Update jad.rb

---
+++ Casks/jad.rb
@@ -0,0 +1,17 @@
+cask 'jad' do
+  version '1.5.8g'
+  sha256 '8e9e4ea6c4177acce6d27325a036f10a72c170ed60e48c37c3483335319d07b9'
+
+  url "http://www.varaneckas.com/jad/jad#{version.no_dots}.mac.intel.zip"
+  name 'Jad - the fast Java Decompiler'
+  homepage 'http://www.varaneckas.com/jad/'
+
+  binary 'jad'
+  artifact 'jad.1', target: '/usr/local/share/man/man1/jad.1'
+
+  caveats <<-EOS.undent
+    Instructions on using jad are available in
+
+      #{staged_path}/Readme.txt
+  EOS
+end
