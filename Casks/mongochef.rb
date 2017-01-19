bde7ec46482cded2d4187b77677a566866619233
RJ Hsiao <rj19860725@gmail.com>
2017-01-19 17:03:20 +0800

    Update mongochef to 4.5.2

--- Casks/mongochef.rb
+++ Casks/mongochef.rb
@@ -1,10 +1,10 @@
 cask 'mongochef' do
-  version '4.5.1'
-  sha256 '8221b179fef6946244ff439cdadb55f408481690bcaec996c1d59c664f966590'
+  version '4.5.2'
+  sha256 'd28c2c5c724cf0a6f46025a0adfa9d336ff89a6ff184a5d89fb483e292410928'
 
   url "https://cdn.3t.io/mongochef-core/mac/#{version}/MongoChef.dmg"
   appcast 'http://downloads.3t.io/mongochef-core/changelog.txt',
-          checkpoint: '464de2612fe807d96e7f66fd967ce86cea55ceef013d593862f580bf9df1852f'
+          checkpoint: '3bbdf1822749f7f469f6f01f416dad6e97eadde83a65b4a7db24af6b128e4470'
   name 'MongoChef'
   homepage 'https://3t.io/mongochef/'
 
