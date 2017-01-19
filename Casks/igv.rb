0b7c2b5671b69aa4a5ef616048cf2365c27d3f07
tevfik <dincer@ucla.edu>
2017-01-19 01:13:38 -0800

    Update igv to 2.3.90

--- Casks/igv.rb
+++ Casks/igv.rb
@@ -1,6 +1,6 @@
 cask 'igv' do
-  version '2.3.89'
-  sha256 'a3960c964fd96e069c3edbec0fd64bb103309745d184d0188202c54339ebb3eb'
+  version '2.3.90'
+  sha256 '42e445c23f386a06349aea1e57cfb1dcc4ed1c72a635698e282db4f953af3db1'
 
   url "https://data.broadinstitute.org/igv/projects/downloads/IGV_#{version}.app.zip"
   name 'Integrative Genomics Viewer (IGV)'
