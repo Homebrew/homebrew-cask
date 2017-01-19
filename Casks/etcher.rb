db71c767d603807d1de1ec0109455a2472118fc8
Sergio Sánchez <mofeing@users.noreply.github.com>
2017-01-19 07:03:30 +0100

    Update etcher to 1.0.0-beta.18 (#29152)

--- Casks/etcher.rb
+++ Casks/etcher.rb
@@ -1,11 +1,11 @@
 cask 'etcher' do
-  version '1.0.0-beta.17'
-  sha256 '648ef2babbbaea6abdf7a680a76afe3f03dbc11780164e890d377c67aec124dc'
+  version '1.0.0-beta.18'
+  sha256 '3573c73362557296b7dfa19604eeddfeecb4ee2baf4af9b60e46c49a253899e9'
 
   # resin-production-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://resin-production-downloads.s3.amazonaws.com/etcher/#{version}/Etcher-#{version}-darwin-x64.dmg"
   appcast 'https://github.com/resin-io/etcher/releases.atom',
-          checkpoint: 'd95cb8bd3494d8c9707618e1f111506f6f281ae6d5fe50694bac02e654f164da'
+          checkpoint: '0de4565cc036df2e3a1cf93eba12b96607a61265a0749fbdde37ef108f2d5418'
   name 'Etcher'
   homepage 'https://etcher.io/'
 
