407582f183b17b4869f015cec45be0fad23e8b5f
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-20 15:35:30 +0800

    Remove trailing whitespace

--- Casks/whatroute.rb
+++ Casks/whatroute.rb
@@ -1,7 +1,7 @@
 cask 'whatroute' do
   if MacOS.version <= :mavericks
     version '1.13.3'
-    sha256 'bc8a4612dcd2d64758dbb2714de60dd95907744cc59dd771a0be29bd56d81e5a' 	
+    sha256 'bc8a4612dcd2d64758dbb2714de60dd95907744cc59dd771a0be29bd56d81e5a'
 
   else
     version '2.0.12'
