f4d0e6106f2d879317140af90b558ef70859a63d
Jiashu Wang <wjs9889@gmail.com>
2017-01-19 16:16:05 -0800

    Update Nylas Mail to 1.0.10.

--- Casks/nylas-mail.rb
+++ Casks/nylas-mail.rb
@@ -1,12 +1,12 @@
 cask 'nylas-mail' do
-  version '1.0.8-a638f3e'
-  sha256 '5f7ecfe628370f67c22ed1587518b8266ee8653f833efab445997224189549cc'
+  version '1.0.10-5e144cf'
+  sha256 '2ac7f49a00c8a195e1b983b11e808f66e1399e69b46f6761b3470ab23293abb7'
 
   # edgehill.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
   url "https://edgehill.s3-us-west-2.amazonaws.com/#{version}/darwin/x64/NylasMail.zip"
   appcast 'https://edgehill.nylas.com/update-check?platform=darwin&arch=64',
-          checkpoint: '6bcf141c0be124c72423db9be099ba4b8e68b5b2735f0a2c78a180f2f01e3b3b'
-  name 'Nylas N1'
+          checkpoint: '19732a7204b4e5840befa53277795bce8dab0d6244e20c995e2dbdff7ff87155'
+  name 'Nylas Mail'
   homepage 'https://www.nylas.com/'
 
   auto_updates true
