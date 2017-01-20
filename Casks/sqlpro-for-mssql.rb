50b292c3347975c48ecca5e1ada0356452e5c683
Colin Dunn <dunnc@amazon.com>
2017-01-21 08:58:38 +1100

    Update sqlpro-for-mssql to 1.0.135

--- Casks/sqlpro-for-mssql.rb
+++ Casks/sqlpro-for-mssql.rb
@@ -1,6 +1,6 @@
 cask 'sqlpro-for-mssql' do
-  version '1.0.133'
-  sha256 '34d67ceca14902964663b3f680e2a9b407f50f3a9c67fe089a06a5cce5aba5af'
+  version '1.0.135'
+  sha256 '13ac27101abeba3b183d027cad97395ae2ab208227eb383ff7f311309daf453b'
 
   # d3fwkemdw8spx3.cloudfront.net/mssql was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/mssql/SQLProMSSQL.#{version}.app.zip"
