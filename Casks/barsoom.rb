b3cc46af899a0aaedaddecc6f4e3eef6725325ed
Mupat <dev@mupat.net>
2017-01-20 15:04:00 +0100

    Add Barsoom Latest

---
+++ Casks/barsoom.rb
@@ -0,0 +1,10 @@
+cask 'barsoom' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.seense.com/barsoom/macossierra/updateapp/barsoom_macossierra.zip'
+  name 'Barsoom'
+  homepage 'https://www.seense.com/barsoom/'
+
+  app 'Barsoom.app'
+end
