66cb5dd81647a5df445091c4073d52867349e85f
Vítor Galvão <info@vitorgalvao.com>
2017-01-20 18:31:33 +0000

    Update barsoom.rb

--- Casks/barsoom.rb
+++ Casks/barsoom.rb
@@ -1,6 +1,6 @@
 cask 'barsoom' do
-  version :latest
-  sha256 :no_check
+  version '2.5'
+  sha256 'e13f37ab2b0c7bac03decce50bd27bba22593c794c7b94b9fab1133e110b139c'
 
   url 'https://www.seense.com/barsoom/macossierra/updateapp/barsoom_macossierra.zip'
   appcast 'https://www.seense.com/barsoom/macossierra/updateapp/appcast.xml',
