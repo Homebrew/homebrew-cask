5e872c383cbd89ded69ada95311d005869798d5d
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-20 11:14:32 +0800

    Fix style

--- Casks/whatroute.rb
+++ Casks/whatroute.rb
@@ -1,16 +1,16 @@
 cask 'whatroute' do
   if MacOS.version <= :mavericks
-		version '1.13.3'
-		sha256 'bc8a4612dcd2d64758dbb2714de60dd95907744cc59dd771a0be29bd56d81e5a' 	
-		url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
-	else
-		version '2.0.12'
-		sha256 'd2186d8064eb851a9a6b1d3feafaf78959f0043e9a64317aa6c1a475d5d4125d'
-		url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
-	end
-  
+    version '1.13.3'
+    sha256 'bc8a4612dcd2d64758dbb2714de60dd95907744cc59dd771a0be29bd56d81e5a' 	
+
+  else
+    version '2.0.12'
+    sha256 'd2186d8064eb851a9a6b1d3feafaf78959f0043e9a64317aa6c1a475d5d4125d'
+  end
+
+  url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
   name 'WhatRoute'
   homepage 'https://www.whatroute.net/'
-  
+
   app 'WhatRoute.app'
 end
