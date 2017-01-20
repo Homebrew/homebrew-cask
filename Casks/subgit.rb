f7187e7c662f028672c7cea8d5c76b4606d1ef8d
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-20 19:28:00 +0800

    Migrate subgit.rb from homebrew/binary (#29223)

---
+++ Casks/subgit.rb
@@ -0,0 +1,14 @@
+cask 'subgit' do
+  version '3.2.3'
+  sha256 '61c1e65bf91a48b3532549f33099ef057e38b773e35c38814b4fd4f0c2b0fc42'
+
+  url "https://subgit.com/download/subgit-#{version}.zip"
+  name 'SubGit'
+  homepage 'http://subgit.com'
+
+  binary "subgit-#{version}/bin/subgit"
+
+  caveats do
+    depends_on_java('7+')
+  end
+end
