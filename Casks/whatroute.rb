bcca76aea6557a9e40ff0ef8cfec322f9cba0515
Warren Alex <warrenkalolo@gmail.com>
2017-01-20 12:27:49 +0300

    Update whatroute to 2.0.12 (#29182)
    * Update whatroute to 2.0.12
    
    * Update whatroute.rb
    
    * Fix style
    
    * Remove trailing whitespace

--- Casks/whatroute.rb
+++ Casks/whatroute.rb
@@ -1,8 +1,14 @@
 cask 'whatroute' do
-  version '1.13.1'
-  sha256 '34a37acea1285b6a2c28bc18b81974b1214fbbf019c0ef9fad32844bb780cc56'
+  if MacOS.version <= :mavericks
+    version '1.13.3'
+    sha256 'bc8a4612dcd2d64758dbb2714de60dd95907744cc59dd771a0be29bd56d81e5a'
 
-  url "https://www.whatroute.net/software/whatroute-#{version}.dmg"
+  else
+    version '2.0.12'
+    sha256 'd2186d8064eb851a9a6b1d3feafaf78959f0043e9a64317aa6c1a475d5d4125d'
+  end
+
+  url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
   name 'WhatRoute'
   homepage 'https://www.whatroute.net/'
 
