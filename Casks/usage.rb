65b0305f21cfb65fdc2cf692a3314d603dce6bf6
SentinelWarren <warrenkalolo@gmail.com>
2017-01-20 17:31:37 +0300

    Update usage to 1.3

--- Casks/usage.rb
+++ Casks/usage.rb
@@ -1,10 +1,10 @@
 cask 'usage' do
-  version '1.2'
-  sha256 '144edfdc45c7a92e66b4c8730026ef73dc3901b9516bdb084c05a126c9216ad3'
+  version '1.3'
+  sha256 'd976ad992a15cc8dc78ee8f5cfc68970fb654ae2ae6367020565ee14dd9a5521'
 
   url "https://mediaatelier.com/Usage/Usage_#{version}.zip"
   appcast 'https://mediaatelier.com/Usage/feed.php',
-          checkpoint: '81026be3b915fe9608c1e1787790b427f2e70a804a41b46cd045c8e9a00cf9b5'
+          checkpoint: 'a0294fb6625e0f98f3c988889a4ba77b4b8e96dd18f78a994c458a3b2762d160'
   name 'Usage'
   homepage 'https://www.mediaatelier.com/Usage/'
 
