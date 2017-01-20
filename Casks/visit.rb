b9a8232634f353d177b629ff73cee36a203e253c
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-20 13:32:34 +0800

    Update checkpoint

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -5,7 +5,7 @@ cask 'visit' do
   # portal.nersc.gov/project/visit was verified as official when first introduced to the cask
   url "https://portal.nersc.gov/project/visit/releases/#{version}/VisIt-#{version}.dmg"
   appcast 'https://wci.llnl.gov/simulation/computer-codes/visit/executables',
-          checkpoint: '732a92915a20a1edc31798b8b5a775761716a8665f28684b83ff517dedb9ad41'
+          checkpoint: '6d1f16f2a61085c04db7dea62820ffb8dd63306eb3930588b5a1c19a43b317cd'
   name 'VisIt'
   homepage 'https://wci.llnl.gov/simulation/computer-codes/visit'
 
