d8e7f0dfa68f965b873e3085b239edbe980f5492
Garen Torikian <gjtorikian@users.noreply.github.com>
2017-01-21 01:26:34 -0800

    Use the right ZIP sha 🙄 (#29263)

--- Casks/graphiql.rb
+++ Casks/graphiql.rb
@@ -1,6 +1,6 @@
 cask 'graphiql' do
   version '0.5.0'
-  sha256 '9a20a7a48067c486c2c8148b4112556b06d932aa49e656073c422122d276ec97'
+  sha256 '81bdfc1ef03210684cf42c7dfed770c0abf290f05596b29eb0ce85c08900b2d5'
 
   url "https://github.com/skevy/graphiql-app/releases/download/v#{version}/GraphiQL.app.zip"
   appcast 'https://github.com/skevy/graphiql-app/releases.atom',
