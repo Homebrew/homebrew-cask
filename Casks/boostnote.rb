738fb388296fa8aa946bc356a4733f6f0755a136
Artem Medvedev <amedvedev@outlook.com>
2017-01-21 11:16:19 +0200

    Update boostnote to 0.8.3 (#29266)

--- Casks/boostnote.rb
+++ Casks/boostnote.rb
@@ -1,11 +1,11 @@
 cask 'boostnote' do
-  version '0.8.2'
-  sha256 'a0eeddc8a16f43d31038e7f9261ae3cbef3b2acf19e5d2e3c9a1fd33f38f8093'
+  version '0.8.3'
+  sha256 '9f27d0451c94799d9cc05aaea52c1ef8a40892d5ddb180e4ab93e5f2c257f995'
 
   # github.com/BoostIO/boost-releases was verified as official when first introduced to the cask
   url "https://github.com/BoostIO/boost-releases/releases/download/v#{version}/Boostnote-mac.dmg"
   appcast 'https://github.com/BoostIO/boost-releases/releases.atom',
-          checkpoint: 'ed6319b0ee6d7d3e121f8475c9cb77f406fd791f7a21055886e8933350feceef'
+          checkpoint: 'd6168d65138979580968b99957ed7af91ce0eea0faff452b0bf553fd434d3ae2'
   name 'Boostnote'
   homepage 'https://b00st.io/'
 
