cf0c6a1eb2f3f85e1278455519a2718b7d461ab4
Daniel Seidler <daniel.seidler@gmail.com>
2017-01-21 21:44:05 +0100

    Update iina to 0.0.3 (#29276)

--- Casks/iina.rb
+++ Casks/iina.rb
@@ -1,11 +1,11 @@
 cask 'iina' do
-  version '0.0.2'
-  sha256 '786a44c6cc118dfc97d2c91684b8e28f05dcaa3c64401f0505e1db4e67224d9d'
+  version '0.0.3'
+  sha256 '7f0ba198f4122f458a0d7b8873fef3fa5d683217eb09b18559c1a73bd6fa5837'
 
   # github.com/lhc70000/iina was verified as official when first introduced to the cask
   url "https://github.com/lhc70000/iina/releases/download/v#{version}/IINA.v#{version}.zip"
   appcast 'https://github.com/lhc70000/iina/releases.atom',
-          checkpoint: 'c945776d76ba415244bd40a9cae05ea6e56772a7ce7a74bd05e3f050a6a7eba0'
+          checkpoint: '2e96b9eafe2cff0e8b802295435ef441389ab45a52fc1cf0838c127be01a4a0e'
   name 'IINA'
   homepage 'https://lhc70000.github.io/iina/'
 
