ca76ebbb453cb6ea8274114c5bdaa7b541c75513
Majid Fatemian <majidfn@yahoo.com>
2017-01-22 15:42:57 -0500

    Starleaf breeze (#29259)
    * Client for Satrleaf video conferencing
    
    Breeze is a client for Starleaf video conferencing application
    
    * Fixing the issue with pkg
    
    * Adding the pkg uninstall
    
    * Fixing the end line

---
+++ Casks/starleaf-breeze.rb
@@ -0,0 +1,14 @@
+cask 'starleaf-breeze' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://dl.starleaf.com/breeze_downloads/StarLeaf-Breeze-Installer-Mac-OS.pkg'
+  name 'Starleaf Breeze'
+  homepage 'http://www.starleaf.com/'
+
+  auto_updates true
+
+  pkg 'StarLeaf-Breeze-Installer-Mac-OS.pkg'
+
+  uninstall pkgutil: 'com.starleaf.breeze.mac'
+end
