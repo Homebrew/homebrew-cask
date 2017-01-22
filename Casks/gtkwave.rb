75934fc3dbc5b5a20ee89c74a5eced5c554225a4
Mike McQuaid <mike@mikemcquaid.com>
2017-01-22 18:40:00 +0000

    gtkwave 3.3.79 (new cask)
    Migrating this from homebrew/homebrew-gui.

---
+++ Casks/gtkwave.rb
@@ -0,0 +1,10 @@
+cask 'gtkwave' do
+  version '3.3.79'
+  sha256 'dcdc3e9d356c00c356183cee07269734e8763098bb4958c5a355b1583cecd7e5'
+
+  url 'https://downloads.sourceforge.net/project/gtkwave/gtkwave-3.3.79-osx-app/gtkwave.zip'
+  name 'GTKWave'
+  homepage 'http://gtkwave.sourceforge.net'
+
+  app 'gtkwave.app'
+end
