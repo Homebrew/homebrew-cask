85f8cdd617d63b33ff443f996c7322803051f37c
wastrachan <winston.astrachan@phobio.com>
2017-01-22 13:09:43 -0500

    Updated lulzbot-cura to 21.03

--- Casks/lulzbot-cura.rb
+++ Casks/lulzbot-cura.rb
@@ -1,6 +1,6 @@
 cask 'lulzbot-cura' do
-  version '21.02-1da4'
-  sha256 '104fd12da8e24cf8b23c084ba7f420def952a7ecc660e798e1de3c5193f95bcd'
+  version '21.03-69de'
+  sha256 'd250c7262d4a8c5c5d35895f5831066ec14f1883d906c77bf29c5cae86b3d9a7'
 
   # download.alephobjects.com/lulzbot was verified as official when first introduced to the cask
   url "http://download.alephobjects.com/lulzbot/Software/Cura/Packages/Cura_#{version.major_minor}/cura_#{version}.dmg"
