fd574be2d0ae6430e9a70d2fd4e138fc0e6bb906
SentinelWarren <warrenkalolo@gmail.com>
2017-01-23 18:03:08 +0300

    Update tikzit to 1.1

--- Casks/tikzit.rb
+++ Casks/tikzit.rb
@@ -1,8 +1,8 @@
 cask 'tikzit' do
-  version '0.8'
-  sha256 '6e2721d2b46d16219a0c7abc92c3abfb8a924d8413ac53fde9d10a7465f1559e'
+  version '1.1'
+  sha256 '069e322e675469a774ceba737e5ea1fbe821c5b6fd7573322a3a7699fb0b7fcf'
 
-  url "https://downloads.sourceforge.net/tikzit/TikZiT-#{version}.dmg"
+  url "https://downloads.sourceforge.net/tikzit/TikZiT-osx-#{version}.zip"
   appcast 'http://tikzit.sourceforge.net/appcast/tikzit.xml',
           checkpoint: '2fc201624061d05e5b3ae505e69611fd11824534f28ada3ab4150de98ebca5f3'
   name 'TikZiT'
