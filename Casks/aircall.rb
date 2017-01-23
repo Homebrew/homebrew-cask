292de9592d876e6ffe1df1a30543b833131a610d
Viktor Szakats <vszakats@users.noreply.github.com>
2017-01-23 23:58:11 +0100

    aircall 1.3.0 and secure URLs

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -1,10 +1,10 @@
 cask 'aircall' do
-  version '1.2.1'
-  sha256 'df416f8df0910d5d368b9de7cce05e3b78505b734f79eb489c7e58f6cfd0bc2d'
+  version '1.3.0'
+  sha256 '49c2aa6bbdd9c6c579545ab5f89dd83e57469040008452f500a735cae0c32d0d'
 
-  url "http://electron.aircall.io/download/#{version}/osx"
-  appcast 'http://electron.aircall.io/update/osx/1.1.0',
-          checkpoint: '925c9a8016d511c4e1290ab280143152f5d7871c3b1332a8cd5539eaca4541ec'
+  url "https://electron.aircall.io/download/#{version}/osx"
+  appcast 'https://electron.aircall.io/update/osx/1.1.0',
+          checkpoint: '43301a5ffe74fea9d4cf0271b110c80201e839a0af1b69c077057f3b192b898e'
   name 'Aircall'
   homepage 'https://aircall.io/'
 
