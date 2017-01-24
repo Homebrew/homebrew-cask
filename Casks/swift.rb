77356f6411bf413ac6b1073b65d7bea8ce592361
Vítor Galvão <info@vitorgalvao.com>
2017-01-24 23:54:15 +0000

    otherwise I’d see this be confused with the programming language

--- Casks/swift.rb
+++ Casks/swift.rb
@@ -3,7 +3,7 @@ cask 'swift' do
   sha256 '8849a59f0875f2d3e2c8a99a171b4f0177f98ecc2241deac92509056858f5024'
 
   url "https://swift.im/downloads/releases/swift-#{version}/Swift-#{version}.dmg"
-  name 'Swift'
+  name 'Swift XMPP Client'
   homepage 'https://swift.im/'
 
   app 'Swift.app'
