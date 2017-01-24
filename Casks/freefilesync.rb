072d6cf173b633f78c0bd8c0c22c2bf11ef5ea99
Viktor Szakats <vszakats@users.noreply.github.com>
2017-01-24 13:35:50 +0100

    freefilesync 8.8 (#29340)

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,11 +1,11 @@
 cask 'freefilesync' do
-  version '8.6'
-  sha256 'aaa1dd1e2dfcc39bf4673d8280cc6561ee8dc9921c4deb926ed336dd5aa1d57b'
+  version '8.8'
+  sha256 'f8a9878fed6b88eb7f0f48a105d2da771f2c203fae865776a3d581481b9a1f52'
 
-  # download1606.mediafire.com/hmk8823hmgqg/bzdxny1n7cxozkh was verified as official when first introduced to the cask.
-  url "https://download1606.mediafire.com/hmk8823hmgqg/bzdxny1n7cxozkh/FreeFileSync_#{version}_macOS.zip"
+  # download673.mediafire.com/kd71fs8nd0ng/x47b3389dwyx8k5 was verified as official when first introduced to the cask.
+  url "https://download673.mediafire.com/kd71fs8nd0ng/x47b3389dwyx8k5/FreeFileSync_#{version}_macOS.zip"
   name 'FreeFileSync'
-  homepage 'http://www.freefilesync.org/'
+  homepage 'https://www.freefilesync.org/'
 
   app 'FreeFileSync.app'
   app 'RealtimeSync.app'
