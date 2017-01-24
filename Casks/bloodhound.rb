622cddedd114deb350c3d371b2a31b36309fedb2
Victor Popkov <victor@popkov.me>
2017-01-25 01:12:33 +0200

    Update bloodhound to 1.2 (#29347)

--- Casks/bloodhound.rb
+++ Casks/bloodhound.rb
@@ -1,10 +1,10 @@
 cask 'bloodhound' do
-  version '1.1'
-  sha256 '399ab1b17f68128cc5a2c143f3795ac74876fa48a117c284a09a7f5a196c6a8e'
+  version '1.2'
+  sha256 '3873ae845a8ffefa3578142e2e264b6e8f510ca59be94c768b7d519d4b978828'
 
   url "https://github.com/adaptivethreat/BloodHound/releases/download/#{version}/BloodHound-darwin-x64.zip"
   appcast 'https://github.com/adaptivethreat/BloodHound/releases.atom',
-          checkpoint: '09212808c04cbcb7d651570160c1140bc05c7791889ac1b49eab147e1c787eb3'
+          checkpoint: '4c88fc90e6cf817a74f951f3a60fabd94ac19602348086b0bf72a070b1894eae'
   name 'bloodhound'
   homepage 'https://github.com/adaptivethreat/BloodHound'
 
