092782508ec4759f9b620ca08ab93544649995aa
Viktor Szakats <vszakats@users.noreply.github.com>
2017-01-24 05:32:07 +0100

    fluxcenter 1.2.4.44994 (#29328)

--- Casks/fluxcenter.rb
+++ Casks/fluxcenter.rb
@@ -1,11 +1,11 @@
 cask 'fluxcenter' do
-  version '1.2.3.44498'
-  sha256 '034d1da03515c23d2cf9d03873bc16f4e73658ed926fc24e33eec051b6398859'
+  version '1.2.4.44994'
+  sha256 'bdc036d665898263ea062336f928f61ae946cb745b6b3ca19f5acd2d3054f59a'
 
   # files.flux.to was verified as official when first introduced to the cask
   url "http://files.flux.to/files/Center/MacOS/Flux_FluxCenter_MacOSX_Installer_(#{version}).dmg"
   name 'FluxCenter'
-  homepage 'http://www.fluxhome.com/'
+  homepage 'https://www.fluxhome.com/'
 
   app 'FluxCenter.app'
 end
