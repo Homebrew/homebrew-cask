ca99ff987901f2ea0daf7517a74bafbbe4e25ae2
Vítor Galvão <info@vitorgalvao.com>
2017-01-24 18:56:13 +0000

    Update keka to 1.0.7 (#29342)

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -2,9 +2,10 @@ cask 'keka' do
   version '1.0.7'
   sha256 '92d05db46ba32e656992d2f9699e328b94dee058e573da3147865f3993bbe579'
 
-  url "http://www.kekaosx.com/release/Keka-#{version}.dmg"
-  appcast 'http://www.kekaosx.com/update/keka.xml',
-          checkpoint: '772e4f21cf3ba63c4b2d9a10a8419b680a74464fadaead8b70380ad677410546'
+  # github.com/aonez/Keka was verified as official when first introduced to the cask
+  url "https://github.com/aonez/Keka/releases/download/#{version}/Keka-#{version}.dmg"
+  appcast 'https://github.com/aonez/Keka/releases.atom',
+          checkpoint: '6239d57ff44cec2f16c5f8546b61b891daa96a358b4b99481fc4de516cf00e57'
   name 'Keka'
   homepage 'http://www.kekaosx.com/'
 
