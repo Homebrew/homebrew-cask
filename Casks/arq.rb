91778551bb59bd3165935038cb216b2b409ba37e
Victor Popkov <victor@popkov.me>
2017-01-24 19:33:38 +0200

    Update arq to 5.7.2

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,10 +1,10 @@
 cask 'arq' do
-  version '5.7'
-  sha256 '1cdd383ec198e41e54d1c16987a919428739beff845e7004c551188b6c5a74df'
+  version '5.7.2'
+  sha256 '9e9212c6cfc0fcd7dd73c35e280f742ac4ade80197c800875269e46e1dd9a77f'
 
   url "https://www.arqbackup.com/download/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arq#{version.major}.xml",
-          checkpoint: '04c87d9fc48f69006088ecbddadeb11ddf719c4817d935a8d73d070dfd1e0d9f'
+          checkpoint: '2b2f2e0e637fc60438bd683b55fa3bcfcaaf04a60ead31f864ae5e712c6c5ec0'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
