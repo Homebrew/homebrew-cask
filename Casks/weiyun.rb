c0410f5fb23fbcde6499f328f7b2b562307d7dca
yuzy <riccoyu@gmail.com>
2017-01-25 21:36:02 +0800

    Update weiyun to 3.0.1.315.28482 (#29385)

--- Casks/weiyun.rb
+++ Casks/weiyun.rb
@@ -1,6 +1,6 @@
 cask 'weiyun' do
-  version '2.0.002.20632'
-  sha256 'e219143abb8b5cbf1063a28f72398e8747bdec0cd95ad03369edbb16a8dd9043'
+  version '3.0.1.315.28482'
+  sha256 '80b7bd49f30bebcef5c501d78b7b2d4b118bb05cfc6fced73cffcce25ff9191c'
 
   # dldir1.qq.com/weiyun was verified as official when first introduced to the cask
   url "http://dldir1.qq.com/weiyun/Weiyun_Mac_#{version}.dmg"
