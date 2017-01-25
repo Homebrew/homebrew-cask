4a76469ff8f404d55dd6435ff9c94cb8d1dc5b88
Stanley Lai <laizhifei@laizhifeideMBPR2014M.local>
2017-01-25 13:05:20 +0800

    Add Stykz.

---
+++ Casks/stykz.rb
@@ -0,0 +1,10 @@
+cask 'stykz' do
+  version '1.0.2'
+  sha256 '5e48b2115f7d3406e5f218045813184f9dc65db39b973fa8ad71ecbf7b86cede'
+
+  url 'http://www.stykz.net/downloads/mac/Stykz.dmg.zip'
+  name 'Stykz'
+  homepage 'http://www.stykz.net'
+
+  app 'Stykz.app'
+end
