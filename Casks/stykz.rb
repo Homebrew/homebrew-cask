22c99165e20596efd1b0002158c5f97bd333c65d
laizhifei <laizhifei@users.noreply.github.com>
2017-01-25 20:18:27 +0800

    Add Stykz. (#29375)
    * Add Stykz.
    
    * Change the version to latest and no check the sha.

---
+++ Casks/stykz.rb
@@ -0,0 +1,10 @@
+cask 'stykz' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.stykz.net/downloads/mac/Stykz.dmg.zip'
+  name 'Stykz'
+  homepage 'http://www.stykz.net'
+
+  app 'Stykz.app'
+end
