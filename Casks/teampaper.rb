dbbe85f3bf67c84d6dc5908a125b5b549d29076c
Semyon Pisarev <cema.rus@gmail.com>
2017-01-26 12:44:58 +0300

    Add TeamPaper Latest
    Add TeamPaper Snap screen capturing tool

---
+++ Casks/teampaper.rb
@@ -0,0 +1,10 @@
+cask 'teampaper' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://teampaper.me/snap/download/teampapersnap.dmg'
+  name 'TeamPaper'
+  homepage 'https://teampaper.me'
+
+  app 'TeamPaper.app'
+end
