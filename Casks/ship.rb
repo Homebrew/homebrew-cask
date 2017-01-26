fdbc3bde44c7a4e420cea3b23783a67267198c72
JP Simard <jp@jpsim.com>
2017-01-26 11:18:32 -0800

    update ship to version 2.0
    just released today: https://www.realartists.com/blog/ship-20.html

--- Casks/ship.rb
+++ Casks/ship.rb
@@ -2,7 +2,7 @@ cask 'ship' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.realartists.com/Ship.app.zip'
+  url 'https://www.realartists.com/builds/2.0/Ship.app.zip'
   name 'Ship'
   homepage 'https://www.realartists.com/'
 
