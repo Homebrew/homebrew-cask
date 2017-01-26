b28cfd6a425fdcb9a5c1b9a7c3934ffa1d1783b1
Vítor Galvão <info@vitorgalvao.com>
2017-01-26 19:59:05 +0000

    Update ship.rb

--- Casks/ship.rb
+++ Casks/ship.rb
@@ -1,8 +1,10 @@
 cask 'ship' do
-  version :latest
-  sha256 :no_check
+  version '2.0'
+  sha256 '13be3718111f8deaf2fa65bac1a93315e48516b7a8e06b840c867e25e528b527'
 
-  url 'https://www.realartists.com/builds/2.0/Ship.app.zip'
+  url "https://www.realartists.com/builds/#{version}/Ship.app.zip"
+  appcast "https://www.realartists.com/builds/#{version}/sparkle.xml",
+          checkpoint: '0e91a5f2eebbcafa921bc98cbacddefebcd8ffcc6763691089381aec47e3b2a9'
   name 'Ship'
   homepage 'https://www.realartists.com/'
 
