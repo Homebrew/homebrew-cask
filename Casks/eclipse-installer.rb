aaf717f69fa8a535c459f00baf1ada0e9b7270a9
Flávio Camilo <flaviocamilo@gmail.com>
2017-01-26 05:19:55 -0200

    eclipse-installer.rb: Correct version and url

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-installer' do
-  version '4.6.2,neon:2'
+  version '4.6.2,neon:2a'
   sha256 '8ff79e95be4bc8f88d75723cd068bc37545eb78f297eed0c544f9830b008fc3b'
 
-  url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.after_comma.before_colon}/R2a/eclipse-inst-mac64.tar.gz&r=1"
+  url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.after_comma.before_colon}/R#{version.after_colon}/eclipse-inst-mac64.tar.gz&r=1"
   name 'Eclipse Installer'
   homepage 'https://eclipse.org/'
 
