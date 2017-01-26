25a88c8ef26378a0235972999374e70adf1aff44
Dmitry <condemil@users.noreply.github.com>
2017-01-26 14:44:55 +0100

    Set proper sha256
        $ brew cask install graphql-ide
        ==> Downloading https://github.com/redound/graphql-ide/releases/download/v1.1.0/GraphQL.IDE.app.zip
        ######################################################################## 100.0%
        ==> Verifying checksum for Cask graphql-ide
        ==> Note: running "brew update" may fix sha256 checksum errors
        Error: sha256 mismatch
        Expected: 697b1618d416c18a17bb6176eda88a1187588b6f95bafb0f559bb6b9fcca8775
        Actual: df90c88d0963987d51d766c956f1ef9221c5463b4c1358b1d0c1de90e5bd1500

--- Casks/graphql-ide.rb
+++ Casks/graphql-ide.rb
@@ -1,6 +1,6 @@
 cask 'graphql-ide' do
   version '1.1.0'
-  sha256 '697b1618d416c18a17bb6176eda88a1187588b6f95bafb0f559bb6b9fcca8775'
+  sha256 'df90c88d0963987d51d766c956f1ef9221c5463b4c1358b1d0c1de90e5bd1500'
 
   url "https://github.com/redound/graphql-ide/releases/download/v#{version}/GraphQL.IDE.app.zip"
   appcast 'https://github.com/redound/graphql-ide/releases.atom',
