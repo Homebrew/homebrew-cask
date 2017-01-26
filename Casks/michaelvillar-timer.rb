ec7e0bf833d3f83a2ed2e4dee4b3115f1a53edca
Vítor Galvão <info@vitorgalvao.com>
2017-01-26 17:28:54 +0000

    Update michaelvillar-timer.rb

--- Casks/michaelvillar-timer.rb
+++ Casks/michaelvillar-timer.rb
@@ -3,7 +3,7 @@ cask 'michaelvillar-timer' do
   sha256 'e0d75fbcd65b54127810b47ff06db4cb8be1b37585969155e3c00d2a7a482f24'
 
   url "https://github.com/michaelvillar/timer-app/releases/download/#{version}/Timer.app.zip"
-  name 'Timer App'
+  name 'Timer'
   homepage 'https://github.com/michaelvillar/timer-app'
 
   app 'Timer.app'
