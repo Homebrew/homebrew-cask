85e441e604a34a24bc554192ed1cf21a2667b06c
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-26 17:47:54 +0800

    Update freefilesync.rb

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -3,6 +3,7 @@ cask 'freefilesync' do
   sha256 'f8a9878fed6b88eb7f0f48a105d2da771f2c203fae865776a3d581481b9a1f52'
 
   url "http://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip"
+      user_agent: :fake
   name 'FreeFileSync'
   homepage 'https://www.freefilesync.org/'
 
