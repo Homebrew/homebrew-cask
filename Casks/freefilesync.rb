acb0872a1b97a5f542cd28133018f9520ca14e2d
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-27 00:34:13 +0800

    Fix freefilesync.rb url (#29426)

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -2,8 +2,8 @@ cask 'freefilesync' do
   version '8.8'
   sha256 'f8a9878fed6b88eb7f0f48a105d2da771f2c203fae865776a3d581481b9a1f52'
 
-  # download673.mediafire.com/kd71fs8nd0ng/x47b3389dwyx8k5 was verified as official when first introduced to the cask.
-  url "https://download673.mediafire.com/kd71fs8nd0ng/x47b3389dwyx8k5/FreeFileSync_#{version}_macOS.zip"
+  url "http://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
+      user_agent: :fake
   name 'FreeFileSync'
   homepage 'https://www.freefilesync.org/'
 
