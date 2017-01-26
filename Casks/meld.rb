26624b3330f13428469c721a23308b1987194890
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-26 15:51:26 +0800

    Update meld to 3.16.0-r1,osx-9 (#29401)
    * Update meld to 3.16.0-r1,osx-9
    
    * Update meld.rb
    
    * Update meld.rb

--- Casks/meld.rb
+++ Casks/meld.rb
@@ -5,9 +5,10 @@ cask 'meld' do
   # github.com/yousseb/meld was verified as official when first introduced to the cask
   url "https://github.com/yousseb/meld/releases/download/#{version.after_comma}/meldmerge.dmg"
   appcast 'https://github.com/yousseb/meld/releases.atom',
-          checkpoint: '4c9b17456ea505a7490456e40b96b980ae3295ad844fed7e71ac1c091787c250'
+          checkpoint: '4f8b54087e9218c1195f74c5a70bbcfc32803fbbd92a3a90257437d7300a4f27'
   name 'Meld for OSX'
   homepage 'https://yousseb.github.io/meld/'
 
   app 'Meld.app'
+  binary "#{appdir}/Meld.app/Contents/MacOS/Meld", target: 'meld'
 end
