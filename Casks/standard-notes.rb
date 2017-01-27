23089df8287f05855943b9c991dfb4d71788304a
SentinelWarren <warrenkalolo@gmail.com>
2017-01-27 15:50:57 +0300

    Update standard-notes to 0.1.2

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,11 +1,11 @@
 cask 'standard-notes' do
-  version '0.1.1'
+  version '0.1.2'
   sha256 '9b6a96ac9d00687f9fdd1ccd456ce36575228dd3a6b90662e8fe529bee04d955'
 
   # github.com was verified as official when first introduced to the cask
   url 'https://github.com/standardnotes/desktop/releases/download/v0.1.1/Standard.Notes-Mac-0.1.1.dmg'
   appcast 'https://github.com/standardnotes/desktop/releases.atom',
-          checkpoint: '727ba8f515d8c859bf0c05465a1be64d07951fd650b235460a8ef58e4f302954'
+          checkpoint: 'ba4ed2c7e86e4179f5cc0e7ef1db9e42cb048039921969e8ee2a7cfda25ef507'
   name 'Standard Notes'
   homepage 'https://standardnotes.org/'
 
