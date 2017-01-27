83b522fdbc02c335562b7d0deb721f9ed56e15c1
Viktor Szakats <vszakats@users.noreply.github.com>
2017-01-27 19:37:26 +0000

    secure URLs in multiple casks #3/5

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -2,7 +2,7 @@ cask 'panoply' do
   version '4.6.2'
   sha256 '4530da27271ae5819bbb03e17ebac83633f061d80d4ece67b1c7f1d1c210832b'
 
-  url "http://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
+  url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
   name 'Panoply netCDF, HDF and GRIB Data Viewer'
   homepage 'https://www.giss.nasa.gov/tools/panoply/'
 
--- Casks/pingplotter.rb
+++ Casks/pingplotter.rb
@@ -2,7 +2,7 @@ cask 'pingplotter' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.pingplotter.com/downloads/pingplotter_osx.zip'
+  url 'https://www.pingplotter.com/downloads/pingplotter_osx.zip'
   name 'PingPlotter'
   homepage 'https://www.pingplotter.com/'
 
