41d6e6ffd20861b3fa9e9345eccda31b045de1b7
JP Simard <jp@jpsim.com>
2017-01-27 00:59:57 -0800

    update ship to version 2.0 (#29453)
    * update ship to version 2.0
    
    just released today: https://www.realartists.com/blog/ship-20.html
    
    * Update ship.rb
    
    * Update sha's

--- Casks/ship.rb
+++ Casks/ship.rb
@@ -1,8 +1,10 @@
 cask 'ship' do
-  version :latest
-  sha256 :no_check
+  version '2.0'
+  sha256 '4cf4c0f7f369a1958a8e486b05fdd49dd575ad8f8a2ffc1be3b36d20e9c4b9a9'
 
-  url 'https://www.realartists.com/Ship.app.zip'
+  url "https://www.realartists.com/builds/#{version}/Ship.app.zip"
+  appcast "https://www.realartists.com/builds/#{version}/sparkle.xml",
+          checkpoint: 'add1ec24702d46c77f294866507b13c6bc105cc34968bb7150371a9e62a5c6a4'
   name 'Ship'
   homepage 'https://www.realartists.com/'
 
