a33fc7662ffe43040e1cc6791b19b585e11f72f0
Hervé Yviquel <hyviquel@gmail.com>
2017-01-27 10:39:26 -0200

    Update rambox to 0.5.3

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -1,6 +1,6 @@
 cask 'rambox' do
-  version '0.5.2'
-  sha256 'c396a408ec6645e4147fd526b10b3b8d9814394f39a74d5a5c6afb5a704a5894'
+  version '0.5.3'
+  sha256 '17b70119483ca85a274fdceacfaf1284e2e069a3dd17683f160f54861f2b8ff1'
 
   # github.com/saenzramiro/rambox was verified as official when first introduced to the cask
   url "https://github.com/saenzramiro/rambox/releases/download/#{version}/Rambox-#{version}-mac.zip"
