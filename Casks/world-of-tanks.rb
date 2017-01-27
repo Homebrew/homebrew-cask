05e63e2a0a97c7b8f931473580c2b4a9f2bab900
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-27 21:10:00 +0800

    Update checkpoints

--- Casks/world-of-tanks.rb
+++ Casks/world-of-tanks.rb
@@ -7,7 +7,7 @@ cask 'world-of-tanks' do
 
     url 'https://wot.gcdn.co/eu/files/osx/worldoftanks_eu.dmg'
     appcast 'https://wot.gcdn.co/eu/files/osx/WoT_OSX_update_eu.xml',
-            checkpoint: 'd54b5dc41164a6dcb009b5322cbda681b0a0b46337fe7ba4ba13a359f1348771'
+            checkpoint: 'ff33b34e2e24f607ee0ed1d61381f735f1549a5f97efd4e8c58d3b815e59920d'
     homepage 'http://worldoftanks.eu/'
   end
 
@@ -16,7 +16,7 @@ cask 'world-of-tanks' do
 
     url 'https://wot.gcdn.co/us/files/osx/worldoftanks_na.dmg'
     appcast 'https://wot.gcdn.co/us/files/osx/WoT_OSX_update_na.xml',
-            checkpoint: '8fcc538b937d2c3a4fe17b85e7e4c59b609afe77056f6ce5d229618a67b08d09'
+            checkpoint: 'd2ec97babd465efc465a53aa68449d36e7b6c113d4580b1e93772fcc159bb564'
     homepage 'http://worldoftanks.com/'
   end
 
@@ -25,7 +25,7 @@ cask 'world-of-tanks' do
 
     url 'https://wot.gcdn.co/sea/files/osx/worldoftanks_asia.dmg'
     appcast 'https://wot.gcdn.co/sea/files/osx/WoT_OSX_update_asia.xml',
-            checkpoint: '086cd744b3b602f69b32c016599a465b153bf28b67f8dc6a057addef45ba192f'
+            checkpoint: 'ac623285e49b86445a21fa1d1fa252db3c64b8eb404dea8612bbe65d85ca46c3'
     homepage 'http://worldoftanks.asia/'
   end
 
@@ -34,7 +34,7 @@ cask 'world-of-tanks' do
 
     url 'https://wot.gcdn.co/kr/files/osx/worldoftanks_kr.dmg'
     appcast 'https://wot.gcdn.co/kr/files/osx/WoT_OSX_update_kr.xml',
-            checkpoint: '5fa09a65e22f1d42b00d052acc4abbb3cf4ce71d946a549fb5904427069d8a6e'
+            checkpoint: 'eebcac213b0780034b8592a30584d0022edf3ce611d45eff5d91b39b51e7127e'
     homepage 'http://worldoftanks.kr/'
   end
 
@@ -43,7 +43,7 @@ cask 'world-of-tanks' do
 
     url 'https://wot.gcdn.co/ru/files/osx/worldoftanks_ru.dmg'
     appcast 'https://wot.gcdn.co/ru/files/osx/WoT_OSX_update_ru.xml',
-            checkpoint: 'ff33b34e2e24f607ee0ed1d61381f735f1549a5f97efd4e8c58d3b815e59920d'
+            checkpoint: '00dcde02245e1b02259316adf06235a0cd67b94c6449e3798a814f10180a8c5e'
     homepage 'http://worldoftanks.ru/'
   end
 
