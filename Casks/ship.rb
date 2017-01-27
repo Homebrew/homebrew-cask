f536ab07386ae76a1e8742136c120430549ce573
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-27 15:24:50 +0800

    Update sha's

--- Casks/ship.rb
+++ Casks/ship.rb
@@ -1,10 +1,10 @@
 cask 'ship' do
   version '2.0'
-  sha256 '13be3718111f8deaf2fa65bac1a93315e48516b7a8e06b840c867e25e528b527'
+  sha256 '4cf4c0f7f369a1958a8e486b05fdd49dd575ad8f8a2ffc1be3b36d20e9c4b9a9'
 
   url "https://www.realartists.com/builds/#{version}/Ship.app.zip"
   appcast "https://www.realartists.com/builds/#{version}/sparkle.xml",
-          checkpoint: '0e91a5f2eebbcafa921bc98cbacddefebcd8ffcc6763691089381aec47e3b2a9'
+          checkpoint: 'add1ec24702d46c77f294866507b13c6bc105cc34968bb7150371a9e62a5c6a4'
   name 'Ship'
   homepage 'https://www.realartists.com/'
 
