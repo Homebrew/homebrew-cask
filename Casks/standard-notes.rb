2f188f7bf651d3a78d4e652ed558bf3a69b2c66f
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-27 21:07:33 +0800

    Update sha

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,6 +1,6 @@
 cask 'standard-notes' do
   version '0.1.2'
-  sha256 '9b6a96ac9d00687f9fdd1ccd456ce36575228dd3a6b90662e8fe529bee04d955'
+  sha256 'c93bb4edafbe2b60e9b5cc78d261e6b5381772c2ff338af4e3c7d6ae462886e2'
 
   # github.com was verified as official when first introduced to the cask
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard.Notes-Mac-#{version}.dmg"
