17574a865aa3aaccf38a0e95739b94242e4fbd24
Dmitry <condemil@users.noreply.github.com>
2017-01-27 09:56:14 +0100

    graphql-ide 1.1.0: fix sha256 (#29431)
    * graphql-ide 1.1.0: fix sha256
    
    * Update checkpoint

--- Casks/graphql-ide.rb
+++ Casks/graphql-ide.rb
@@ -1,10 +1,10 @@
 cask 'graphql-ide' do
   version '1.1.0'
-  sha256 '697b1618d416c18a17bb6176eda88a1187588b6f95bafb0f559bb6b9fcca8775'
+  sha256 'df90c88d0963987d51d766c956f1ef9221c5463b4c1358b1d0c1de90e5bd1500'
 
   url "https://github.com/redound/graphql-ide/releases/download/v#{version}/GraphQL.IDE.app.zip"
   appcast 'https://github.com/redound/graphql-ide/releases.atom',
-          checkpoint: '303b735e0bceec72e00116ef27afd394686f2c1aa0cd133a4ec658b28d900555'
+          checkpoint: '85a07b3f3077bd2139c2b4d42942c1a724a6a27c5e55387489d86c6aeb4d2b17'
   name 'GraphQL IDE'
   homepage 'https://github.com/redound/graphql-ide'
 
