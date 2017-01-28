36a1f72184cba631b89d8628ecddc7f368bb667f
lagilaper <pengen.makan.nasi@gmail.com>
2017-01-28 15:07:56 +0700

    Update postico to 1.1

--- Casks/postico.rb
+++ Casks/postico.rb
@@ -1,6 +1,6 @@
 cask 'postico' do
-  version '1.0.10'
-  sha256 'c746450e6decef04ab8ee193dd69524c951743718ca38bdaedef6c6f149c7300'
+  version '1.1'
+  sha256 '42980661e881a3f15af787de881dff8f97a69d8ba34a3c0192345a8a6d333280'
 
   # amazonaws.com/eggerapps-downloads was verified as official when first introduced to the cask
   url "https://s3-eu-west-1.amazonaws.com/eggerapps-downloads/postico-#{version}.zip"
