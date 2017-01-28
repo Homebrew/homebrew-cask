72cf00e36748fdd5b3a14bea01563c97100c4401
Viktor Szakats <vszakats@users.noreply.github.com>
2017-01-28 01:59:19 +0100

    tinymediamanager 2.9.2

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,10 +1,10 @@
 cask 'tinymediamanager' do
-  version '2.9.1_520388f'
-  sha256 '84f62d867d74af7b13486a7ecc5f6e96f92bff290841f8bd956c7517befac551'
+  version '2.9.2_4c5fcb0'
+  sha256 '1521c06c6642befd76a76c93e0db0a08e84ae3a7ee4177eba134f77c64cd1a57'
 
-  url "http://release.tinymediamanager.org/dist/tmm_#{version}_mac.zip"
-  appcast 'http://release.tinymediamanager.org/',
-          checkpoint: '40c74bcce7552c2dd81cb5d5ba5445e7e376d9ea65f282ce146c234625b30d0b'
+  url "https://release.tinymediamanager.org/dist/tmm_#{version}_mac.zip"
+  appcast 'https://release.tinymediamanager.org/',
+          checkpoint: '3c8cca623dcb4eecf957048e8637839db0dbc597faae72987427a8ce218c4e08'
   name 'tinyMediaManager'
   homepage 'https://www.tinymediamanager.org/'
 
