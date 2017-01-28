8df151e470f0ebcf39d4203d5704e7dd9b532d80
Direlynx <logan@izenas.com>
2017-01-28 13:10:49 -0800

    Update google-drive.rb
    Now deletes the actual Google Drive folder

--- Casks/google-drive.rb
+++ Casks/google-drive.rb
@@ -13,5 +13,8 @@ cask 'google-drive' do
                 '~/Library/Application Support/Google/Drive',
                 '~/Library/Preferences/com.google.GoogleDrive.plist',
                 '~/Library/Caches/com.google.GoogleDrive',
+              ],
+      rmdir:  [
+                '~/Google Drive',
               ]
 end
