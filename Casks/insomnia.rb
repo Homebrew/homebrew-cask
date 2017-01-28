5e68a36203d91fa4bba2b73226b890165f208405
lagilaper <pengen.makan.nasi@gmail.com>
2017-01-28 15:11:36 +0700

    Update insomnia to 4.2.3

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,6 +1,6 @@
 cask 'insomnia' do
-  version '4.1.1'
-  sha256 '3874f7f5d5648bd3525c91077daf7f1f58aadb8758bbeb5e22e3d2462c7791da'
+  version '4.2.3'
+  sha256 'e6cb61a7b2a27f967cb99ae4220239a5acc92fde1bb5a63583d16df8f9d781ea'
 
   url "https://downloads.insomnia.rest/mac/Insomnia-#{version}.dmg"
   name 'Insomnia'
