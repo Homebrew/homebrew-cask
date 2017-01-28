87f6628bf462bf53b4f335d1207db9d157b53403
John Hoyt <jhoyt@JGH4.local>
2017-01-28 11:56:19 -0500

    v

--- Casks/mythfrontend.rb
+++ Casks/mythfrontend.rb
@@ -4,6 +4,9 @@ cask 'mythfrontend' do
 
   # downloads.sourceforge.net was verified as official when first introduced to the cask
   url 'https://downloads.sourceforge.net/mythtvformacosx/MythFrontend-0.28-intel-10.9-v0.28-52-ge6a60f7.dmg'
+  appcast 'https://sourceforge.net/projects/mythtvformacosx/rss?path=/',
+           checkpoint: 'ab4861cf53ab2a9d006aec31ed89378ad242bef1199eb1864436076f3e39c583'
+
   name 'MythFrontend'
   homepage 'https://www.mythtv.org/'
 
