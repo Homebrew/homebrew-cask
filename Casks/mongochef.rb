5aa0314543afdf09a50426a421c2eed0697002d6
Viktor Szakats <vszakats@users.noreply.github.com>
2017-01-28 01:42:48 +0100

    mongochef: TLS appcast (#29498)

--- Casks/mongochef.rb
+++ Casks/mongochef.rb
@@ -3,7 +3,7 @@ cask 'mongochef' do
   sha256 '862b18acdf433151413b3f8e66645e3ade55c828466d1b76a4babb7967398de7'
 
   url "https://cdn.3t.io/mongochef-core/mac/#{version}/MongoChef.dmg"
-  appcast 'http://downloads.3t.io/mongochef-core/changelog.txt',
+  appcast 'https://downloads.3t.io/mongochef-core/changelog.txt',
           checkpoint: 'e25999a0785eb04736917ee64e1e491eb4e5cca6beddac8a6e5a906af1553f95'
   name 'MongoChef'
   homepage 'https://3t.io/mongochef/'
