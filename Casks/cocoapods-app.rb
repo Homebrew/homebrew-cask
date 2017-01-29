cb93a29b441e21928cbf319a52200f7dc82b5525
Orta <orta.therox+github@gmail.com>
2017-01-29 14:44:14 -0500

    Update CocoaPods App to 1.2.0 (#29556)

--- Casks/cocoapods-app.rb
+++ Casks/cocoapods-app.rb
@@ -1,11 +1,11 @@
 cask 'cocoapods-app' do
-  version '1.1.1'
-  sha256 '04700e67478a626be75fd8e54990068c87a131620c3585438d7a95123a099e96'
+  version '1.2.0'
+  sha256 '189e40d887364027645d5471a8fc733a22dc006dec112ad6bc33cd3ea8f417a4'
 
   # github.com/CocoaPods/CocoaPods was verified as official when first introduced to the cask
   url "https://github.com/CocoaPods/CocoaPods-app/releases/download/#{version}/CocoaPods.app-#{version}.tar.bz2"
   appcast 'https://app.cocoapods.org/sparkle',
-          checkpoint: 'ad3ea79f59cc2ce3408b4945608d0691c6e569b363bfdd95c5a0677637487a92'
+          checkpoint: '083584b4914d17892691e80a248a15ee7f773bdd1677554eb4e13480d7c9e777'
   name 'CocoaPods.app'
   homepage 'https://cocoapods.org/'
 
