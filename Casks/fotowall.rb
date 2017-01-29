e96b79964247c7d7acb9212714df3e92a151b573
Colin Dunn <cgd@vikingdev.net>
2017-01-30 08:27:37 +1100

    Update fotowall download url (#29563)

--- Casks/fotowall.rb
+++ Casks/fotowall.rb
@@ -2,8 +2,8 @@ cask 'fotowall' do
   version '0.8.2'
   sha256 'f49ad020eb6d36b9ad5492edd24ce608aef4466b727b5d0811ed4218b35d0c8c'
 
-  # fotowall.googlecode.com was verified as official when first introduced to the cask
-  url "https://fotowall.googlecode.com/files/Fotowall-#{version}-OSX.dmg"
+  # storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/fotowall was verified as official when first introduced to the cask
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/fotowall/Fotowall-#{version}-OSX.dmg"
   name 'Fotowall'
   homepage 'http://www.enricoros.com/opensource/fotowall/'
 
