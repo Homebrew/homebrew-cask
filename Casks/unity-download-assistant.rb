da0203db8624c19e446b3b46ad5eacc29ee8d630
Brian <briandilley@briandilley.com>
2017-01-28 19:24:30 -0800

    Update unity-download-assistant.rb

--- Casks/unity-download-assistant.rb
+++ Casks/unity-download-assistant.rb
@@ -1,6 +1,6 @@
 cask 'unity-download-assistant' do
   version '5.5.1f1,88d00a7498cd'
-  sha256 'ae7f01b8406a5b57571ae49aca3e78e012a628d0ca83bfc2883dce492d4c88cc'
+  sha256 'b2e02f6ef46abb287f1a67a6c74d8c0c6df93ecd7a47ef8bd09998bd09dd5f7e'
 
   url "http://netstorage.unity3d.com/unity/#{version.after_comma}/UnityDownloadAssistant-#{version.before_comma}.dmg"
   name 'Unity'
