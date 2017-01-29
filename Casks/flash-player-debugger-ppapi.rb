c217aa1d23accadb4db6fe50bcad24db8ffb1517
Miccal Matthews <miccal.matthews@gmail.com>
2017-01-29 21:21:12 +0800

    Update flash-player-debugger-ppapi.rb

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -5,7 +5,7 @@ cask 'flash-player-debugger-ppapi' do
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
-          checkpoint: 'f621c439083bbb6bb5857e26c0d5e5c6b01a616617144f4c2ddf598c1ea6083b'
+          checkpoint: '25a5c623a510f8458e8f901ad1d9b62ac6d78cfe91cf231223ef8e48629e3a2b'
   name 'Adobe Flash Player PPAPI (plugin for Opera and chromium) content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
