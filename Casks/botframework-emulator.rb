5239cce48a161a13ae7392ec10d4864d25943255
Victor Popkov <victor@popkov.me>
2017-01-29 23:15:26 +0200

    Update botframework-emulator to 3.5.25 (#29560)

--- Casks/botframework-emulator.rb
+++ Casks/botframework-emulator.rb
@@ -1,10 +1,10 @@
 cask 'botframework-emulator' do
-  version '3.5.23'
-  sha256 '753a52046a7e581deb23a53a12572dd4b158ed79d704af2982326ee625724e98'
+  version '3.5.25'
+  sha256 '78fb6ea5289a6e8fb0d2d54d921bb582af03f733de71efbb1598c5110b8e4fda'
 
   url "https://github.com/Microsoft/BotFramework-Emulator/releases/download/v#{version}/botframework-emulator-#{version}-mac.zip"
   appcast 'https://github.com/Microsoft/BotFramework-Emulator/releases.atom',
-          checkpoint: '01b5ed10ba1fb2333bf424505438cdcab011aa1e54f7de5a9d2a79d4481c1e59'
+          checkpoint: '77b6108493d7f104e623f266e462f615530fec6480f1e0b07adac2148ec01bc9'
   name 'Microsoft Bot Framework Emulator'
   homepage 'https://github.com/Microsoft/BotFramework-Emulator'
 
