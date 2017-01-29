74f193c7797975bc33e2d0aa48795ff08f1e300c
Warren Alex <warrenkalolo@gmail.com>
2017-01-29 12:20:48 +0300

    update checkpoint

--- Casks/speedy.rb
+++ Casks/speedy.rb
@@ -4,7 +4,7 @@ cask 'speedy' do
 
   url "http://www.apimac.com/download/previous/Speedy-#{version.no_dots}.zip"
   appcast 'http://www.apimac.com/version_checking/speedy_mac.xml',
-          checkpoint: '6b0638d63dd078a73c3ffd8ae2a625123d52d9a30c4f220676aae8eb36718707'
+          checkpoint: '7dddd319b0035c46f79e445d1e91d260a50f46f51963ef494c3f9e5cbe76fe58'
   name 'Speedy'
   homepage 'http://www.apimac.com/'
 
