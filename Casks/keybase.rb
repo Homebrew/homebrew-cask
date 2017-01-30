cdf30ce9850bc0d5100a4c9ae6422dbd1d20b6e5
Vítor Galvão <info@vitorgalvao.com>
2017-01-30 01:58:28 +0000

    can’t have a sha256 without a version

--- Casks/keybase.rb
+++ Casks/keybase.rb
@@ -1,5 +1,5 @@
 cask 'keybase' do
-  version :latest
+  version '1.0.18-20170120152345'
   sha256 '896ebeab741c4f4abfc848aef771488d2b5bbb08388c2ff6237284e22e23bb82'
 
   url 'https://prerelease.keybase.io/Keybase.dmg'
