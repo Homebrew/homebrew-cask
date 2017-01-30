46c639c58efe8698ed168b8257f7d6cfcca3a050
Timothy Sutton <tim@synthist.net>
2017-01-30 11:52:04 -0500

    Update iina to 0.0.4

--- Casks/iina.rb
+++ Casks/iina.rb
@@ -1,11 +1,11 @@
 cask 'iina' do
-  version '0.0.3'
-  sha256 '7f0ba198f4122f458a0d7b8873fef3fa5d683217eb09b18559c1a73bd6fa5837'
+  version '0.0.4'
+  sha256 'c2847263dce7103d620be132c6004822f2197a330eb85b85bc49ecfd9eebf5f7'
 
   # github.com/lhc70000/iina was verified as official when first introduced to the cask
   url "https://github.com/lhc70000/iina/releases/download/v#{version}/IINA.v#{version}.zip"
   appcast 'https://github.com/lhc70000/iina/releases.atom',
-          checkpoint: '2e96b9eafe2cff0e8b802295435ef441389ab45a52fc1cf0838c127be01a4a0e'
+          checkpoint: '70901e8c6d313b114fe817b48aefa6e6aeac393b4aec577d562481f923a2b88e'
   name 'IINA'
   homepage 'https://lhc70000.github.io/iina/'
 
