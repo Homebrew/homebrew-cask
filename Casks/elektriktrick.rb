ca27c649bfa9ca6b1ea541ea8c04b7172371ad5a
Colin Dunn <cgd@vikingdev.net>
2017-01-30 21:05:06 +1100

    Add 9 discovered appcasts (#29590)

--- Casks/elektriktrick.rb
+++ Casks/elektriktrick.rb
@@ -3,6 +3,8 @@ cask 'elektriktrick' do
   sha256 '91977be7e0e0c5929ca5ac4edf6a9430ca2e83db8ca71ef5c9ed9082c160ef54'
 
   url "http://www.elektriktrick.com/Downloads/Elektriktrick_#{version}.dmg"
+  appcast 'http://www.elektriktrick.com/sw_quicklook.html',
+          checkpoint: '4d3c5e3800ecc5b8a0d98213b3cf2902587a1d658e3118ebb6950185e485438e'
   name 'Elektriktrick STL and GCode Quicklook'
   homepage 'http://www.elektriktrick.com/sw_quicklook.html'
 
