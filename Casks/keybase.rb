2d3270638ddaa00cc50ca9bddebee21a2096eeee
Bill Cromie <bill.cromie@gmail.com>
2017-01-30 15:10:51 -0500

    adding keybase checksum (#29582)
    * keybase, version latest
    
    Adding a checksum for security. It is crypto, after all.
    
    * can’t have a sha256 without a version
    
    * Update keybase.rb

--- Casks/keybase.rb
+++ Casks/keybase.rb
@@ -1,8 +1,10 @@
 cask 'keybase' do
-  version :latest
-  sha256 :no_check
+  version '1.0.18-20170120152345'
+  sha256 '896ebeab741c4f4abfc848aef771488d2b5bbb08388c2ff6237284e22e23bb82'
 
   url 'https://prerelease.keybase.io/Keybase.dmg'
+  appcast 'https://github.com/keybase/client/releases.atom',
+          checkpoint: 'e52341b2c47ad7ebf202812221116c9bb4eda1f938de97225584bf7ebeff0d6d'
   name 'Keybase'
   homepage 'https://keybase.io/'
 
