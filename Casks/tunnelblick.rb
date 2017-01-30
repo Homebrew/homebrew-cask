675710689c2e4a851d2d18f72fc7fd7e10ded791
Colin Dunn <dunnc@amazon.com>
2017-01-31 07:53:31 +1100

    Update tunnelblick to 3.7.0,4790

--- Casks/tunnelblick.rb
+++ Casks/tunnelblick.rb
@@ -1,11 +1,11 @@
 cask 'tunnelblick' do
-  version '3.6.10,build_4760'
-  sha256 '05f8f487a08f10f0b2558c27fde3aa37a8dbe609e0c7cf556f38d92cc49b18e0'
+  version '3.7.0,4790'
+  sha256 '5053038aa8caf7dea66dcab11d6d240672216e6546eff4c2622e216c61af85e5'
 
   # github.com/Tunnelblick/Tunnelblick/releases/download was verified as official when first introduced to the cask
-  url "https://github.com/Tunnelblick/Tunnelblick/releases/download/v#{version.before_comma}/Tunnelblick_#{version.before_comma}_#{version.after_comma}.dmg"
+  url "https://github.com/Tunnelblick/Tunnelblick/releases/download/v#{version.before_comma}/Tunnelblick_#{version.before_comma}_build_#{version.after_comma}.dmg"
   appcast 'https://github.com/Tunnelblick/Tunnelblick/releases.atom',
-          checkpoint: 'eaec16a7880bb2105e2a2de1a342b7d77f6ad5b11dca085c94f04e28b071ae78'
+          checkpoint: '2947732dcfc2fb59bf51991a4f07088055de40c37fa320e615951e528a60e07c'
   name 'Tunnelblick'
   homepage 'https://www.tunnelblick.net/'
 
