5888edc2c4d1b3dff7f2319b2ee6995e7b637a52
Jean-Philippe Roemer <jp@roemer.im>
2017-01-30 23:41:53 +0100

    Update keybase to 1.0.18-20170130165558,0fcd377

--- Casks/keybase.rb
+++ Casks/keybase.rb
@@ -1,8 +1,8 @@
 cask 'keybase' do
-  version '1.0.18-20170120152345'
-  sha256 '896ebeab741c4f4abfc848aef771488d2b5bbb08388c2ff6237284e22e23bb82'
+  version '1.0.18-20170130165558,0fcd377'
+  sha256 'b871ee5bf7af5f3194ae4c679e7cf98c260262638005b7851090a08093d5b103'
 
-  url 'https://prerelease.keybase.io/Keybase.dmg'
+  url "https://prerelease.keybase.io/darwin/Keybase-#{version.before_comma}%2B#{version.after_comma}.dmg"
   appcast 'https://github.com/keybase/client/releases.atom',
           checkpoint: 'e52341b2c47ad7ebf202812221116c9bb4eda1f938de97225584bf7ebeff0d6d'
   name 'Keybase'
