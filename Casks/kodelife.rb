08f976f6ad4aab0067946d2499043903f9d179b5
jzgdev <jzg.dev@gmail.com>
2017-01-29 18:34:01 -0800

    Add KodeLife.app v0.4.0

---
+++ Casks/kodelife.rb
@@ -0,0 +1,10 @@
+cask 'kodelife' do
+  version '0.4.0'
+  sha256 '5aa576d094a514cd9c519db01786fb31321133523ecd23e572412a3aa0cad2f3'
+
+  url 'http://hexler.net/mint/pepper/orderedlist/downloads/download.php?file=http%3A//hexler.net/pub/kodelife/kodelife-0.4.0-osx.zip'
+  name 'KodeLife'
+  homepage 'http://hexler.net/software/kodelife'
+
+  app 'KodeLife.app'
+end
