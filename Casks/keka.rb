2ec8bdabfc13c97815236813aab46fd3310d20f1
Sébastien OUDRY <sebastien.oudry@gmail.com>
2017-01-30 16:47:05 +0100

    Keka 1.0.7 fix url (#29605)

--- Casks/keka.rb
+++ Casks/keka.rb
@@ -3,9 +3,9 @@ cask 'keka' do
   sha256 '92d05db46ba32e656992d2f9699e328b94dee058e573da3147865f3993bbe579'
 
   # github.com/aonez/Keka was verified as official when first introduced to the cask
-  url "https://github.com/aonez/Keka/releases/download/#{version}/Keka-#{version}.dmg"
+  url "https://github.com/aonez/Keka/releases/download/v#{version}/Keka-#{version}.dmg"
   appcast 'https://github.com/aonez/Keka/releases.atom',
-          checkpoint: '6239d57ff44cec2f16c5f8546b61b891daa96a358b4b99481fc4de516cf00e57'
+          checkpoint: '2abc192f8bc1572c7654d6eb4121c9f638ce405de61229ad09ec770a7c91deb2'
   name 'Keka'
   homepage 'http://www.kekaosx.com/'
 
