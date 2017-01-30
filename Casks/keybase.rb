a4057e179a5726525866b1d0cb05a33bcf95639e
Vítor Galvão <info@vitorgalvao.com>
2017-01-30 15:46:00 +0000

    Update keybase.rb

--- Casks/keybase.rb
+++ Casks/keybase.rb
@@ -3,6 +3,8 @@ cask 'keybase' do
   sha256 '896ebeab741c4f4abfc848aef771488d2b5bbb08388c2ff6237284e22e23bb82'
 
   url 'https://prerelease.keybase.io/Keybase.dmg'
+  appcast 'https://github.com/keybase/client/releases.atom',
+          checkpoint: 'e52341b2c47ad7ebf202812221116c9bb4eda1f938de97225584bf7ebeff0d6d'
   name 'Keybase'
   homepage 'https://keybase.io/'
 
