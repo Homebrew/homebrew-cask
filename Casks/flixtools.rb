c45fbb274398e5140c6fadcbc6c0b542e15c7b2c
Serge Bibauw <sbibauw@gmail.com>
2017-01-30 18:14:12 -0500

    Updated Flixtools to 2.4

--- Casks/flixtools.rb
+++ Casks/flixtools.rb
@@ -1,6 +1,6 @@
 cask 'flixtools' do
-  version :latest
-  sha256 :no_check
+  version '2.4'
+  sha256 '86d1761dbc7a3e338a95dd860c468e94df36df028df0e481b224d338bf0cf49b'
 
   url 'http://www.flixtools.com/download/FlixTools.dmg'
   name 'OpenSubtitles FlixTools Lite'
