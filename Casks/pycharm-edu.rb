a870da2fa1373ae9188ea2b8f5981079dba434e8
Colin Dunn <cgd@vikingdev.net>
2017-01-30 23:11:09 +1100

    Update pycharm-edu to 3.5 (#29601)
    * Update pycharm-edu to 3.5
    
    * Update pycharm-edu to 3.5

--- Casks/pycharm-edu.rb
+++ Casks/pycharm-edu.rb
@@ -1,8 +1,10 @@
 cask 'pycharm-edu' do
-  version '3.0.1'
-  sha256 '34f6f1003cee6b2612c69b07dac1b0319899a93a6da928b15d3ab5c882bcd14e'
+  version '3.5'
+  sha256 '69b59880aa93e8329f7fee1b1696ba14d8554e3d6c3ddd9f5f150844b4258534'
 
   url "https://download.jetbrains.com/python/pycharm-edu-#{version}.dmg"
+  appcast 'https://data.services.jetbrains.com/products/releases?code=PCE&latest=true&type=release',
+          checkpoint: '996879c1ca05564f7b2c3b77abf406e7bfa5c5565b725aeb8b866c709d5d8303'
   name 'Jetbrains PyCharm Educational Edition'
   name 'PyCharm Edu'
   homepage 'https://www.jetbrains.com/pycharm-edu/'
