d9ea2bcb24207be06609ac78aaff2ed68f47437c
Colin Dunn <dunnc@amazon.com>
2017-01-31 21:31:27 +1100

    Update gurps-character-sheet to 4.8.0

--- Casks/gurps-character-sheet.rb
+++ Casks/gurps-character-sheet.rb
@@ -1,10 +1,10 @@
 cask 'gurps-character-sheet' do
-  version '4.7.0'
-  sha256 '5a633046dc78aaaef9e554f95cf16c141dceeb4b3b74daa4ece799e754acaaa7'
+  version '4.8.0'
+  sha256 '79b4e3dbad0216b3163eda9ee5336451eccec7e895636570da75f44d0f3dc86a'
 
   url "http://gurpscharactersheet.com/downloads/#{version}/gcs-#{version}-mac.zip"
   appcast 'http://gurpscharactersheet.com/releases.php',
-          checkpoint: 'e7828c3fbb3364f691effe4bd1b34caeafbde224f68a93721e7091da964c16c9'
+          checkpoint: 'c1154e7284a80073c5f6d8b72433bcdd4b07b3aa113e958bb8915c092fb35f8b'
   name 'GURPS Character Sheet'
   homepage 'http://gurpscharactersheet.com/'
 
