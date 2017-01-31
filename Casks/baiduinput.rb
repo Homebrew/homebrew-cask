3ad33421e4bcd7045e167e974c5c432979bfa45b
Gray Zhang <otakustay@icloud.com>
2017-01-31 18:14:51 +0800

    Update baiduinput.rb (#29629)
    * Update baiduinput.rb
    
    * Change to single quotes - no interpolation

--- Casks/baiduinput.rb
+++ Casks/baiduinput.rb
@@ -1,8 +1,8 @@
 cask 'baiduinput' do
-  version '3.4.1.5_1000e,5r1ZsjOhKgQFm2e88IuM_a'
-  sha256 '0d6b13ac06bfdca2cf3f1d6c84d09904da6baf8fdb9730feaa077b2a64eac69b'
+  version :latest
+  sha256 :no_check
 
-  url "https://gss0.baidu.com/#{version.after_comma}/srf/mac/baiduinput_mac_v#{version.before_comma}.dmg"
+  url 'https://srf.baidu.com/?c=j&e=d&platform=mac'
   name 'Baidu Input'
   name '安装百度输入法'
   homepage 'https://srf.baidu.com/input/mac.html'
