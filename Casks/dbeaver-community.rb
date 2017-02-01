110cc6c705415971637e002309fda869c66236ad
Ilya Medvedev <medved55rus@gmail.com>
2017-02-01 12:43:40 +0000

    dbeaver-community v3.8.5

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
-  version '3.8.4'
-  sha256 '134377b618cd782aa9ba86cd97c47f99f4da6739a6f27e1e4ec1b526fe21a984'
+  version '3.8.5'
+  sha256 '318cb06e707719bd6a182c079c783108ccc001bb0ad141a531a34d5cdde65a23'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name 'DBeaver Community Edition'
