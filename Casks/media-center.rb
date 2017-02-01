1223892661fa82d17e2dbbd5929ef996579a616e
Vítor Galvão <info@vitorgalvao.com>
2017-02-01 02:25:46 +0000

    Update media-center.rb

--- Casks/media-center.rb
+++ Casks/media-center.rb
@@ -2,7 +2,7 @@ cask 'media-center' do
   version '22.0.54'
   sha256 'fc85bf66f75ce640936f1e21fc11c0786f2b43811f2082a4a4984b42676c337b'
 
-  url "http://files.jriver.com/mediacenter/channels/v22/stable/MediaCenter#{version.no_dots}.dmg"
+  url "http://files.jriver.com/mediacenter/channels/v#{version.major}/stable/MediaCenter#{version.no_dots}.dmg"
   name 'JRiver Media Center'
   homepage 'https://www.jriver.com/'
 
