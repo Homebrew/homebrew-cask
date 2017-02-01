f35e59697a783e74eaa59735ecd65d2162cddba1
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-02 06:47:16 +0800

    Remove extra .0

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -3,7 +3,7 @@ cask 'krita' do
   sha256 '7d8e5b97615327122887f1114a009093e2974aa4fca01e56950fa25173b9600a'
 
   # kde.org/stable/krita was verified as official when first introduced to the cask
-  url "http://download.kde.org/stable/krita/#{version}/krita-#{version}.0.dmg"
+  url "http://download.kde.org/stable/krita/#{version}/krita-#{version}.dmg"
   name 'Krita'
   homepage 'https://krita.org/'
 
