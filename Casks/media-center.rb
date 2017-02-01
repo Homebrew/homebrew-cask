b8cbfed4a8d26bce1b72b944d98200e1ac657e12
Vítor Galvão <info@vitorgalvao.com>
2017-02-01 02:25:27 +0000

    Update and rename jriver-media-center.rb to media-center.rb

--- Casks/media-center.rb
+++ Casks/media-center.rb
@@ -1,14 +1,12 @@
-cask 'jriver-media-center' do
+cask 'media-center' do
   version '22.0.54'
   sha256 'fc85bf66f75ce640936f1e21fc11c0786f2b43811f2082a4a4984b42676c337b'
 
-  url 'http://files.jriver.com/mediacenter/channels/v22/stable/MediaCenter220054.dmg'
+  url "http://files.jriver.com/mediacenter/channels/v22/stable/MediaCenter#{version.no_dots}.dmg"
   name 'JRiver Media Center'
-  name 'JRiver'
-  name 'Media Center'
   homepage 'https://www.jriver.com/'
 
-  app 'Media Center 22.app'
+  app "Media Center #{vesion.major}.app"
 
   zap delete: '~/Library/Application Support/J River/'
 end
