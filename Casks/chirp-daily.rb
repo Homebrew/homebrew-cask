c0a8d20e7ee3f865cde80fcd6a62e8b9fd2791d9
Colin Dunn <cgd@vikingdev.net>
2017-02-02 21:01:39 +1100

    Update chirp to 20170126 and rename to chirp-daily (#29677)

---
+++ Casks/chirp-daily.rb
@@ -0,0 +1,12 @@
+cask 'chirp-daily' do
+  version '20170126'
+  sha256 '7fc48fa3752f828120b94a21c1df1612dbd285904bcff6886d0b843604cfcf31'
+
+  url "http://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
+  name 'CHIRP'
+  homepage 'http://chirp.danplanet.com/'
+
+  depends_on cask: 'kk7ds-python-runtime'
+
+  app "chirp-daily-#{version}.app"
+end
