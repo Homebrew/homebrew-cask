de1e8339fa744c72653b3cbd653e523e52c01440
Artem Medvedev <amedvedev@outlook.com>
2017-02-02 11:58:58 +0200

    Update vox to 2.8.16 (#29661)

--- Casks/vox.rb
+++ Casks/vox.rb
@@ -1,11 +1,11 @@
 cask 'vox' do
-  version '2.8.14'
-  sha256 'e7b63f52e4a2e977266a360b6a9f08a1d2f9e7e9638628d522f35d26a9548d73'
+  version '2.8.16'
+  sha256 '52d5bd6903675e1afc7163af36f05a231efc48804c8273a109990bf830714db3'
 
   # devmate.com/com.coppertino.Vox was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coppertino.Vox/Vox.dmg'
   appcast 'https://updates.devmate.com/com.coppertino.Vox.xml',
-          checkpoint: '18dfeb8851c0f7bb5379b3eb6aff25246705c01bbfad9c3ae8f986a2fe19d00f'
+          checkpoint: 'f7ae93b9daf4918507e9379dce4a568972c2ae7f609bfad9000da50e64e6cfca'
   name 'VOX'
   homepage 'https://vox.rocks/mac-music-player'
 
