8a95016544b8d12dc5919474942518ab241cd89b
neutric <neutric@users.noreply.github.com>
2017-02-02 19:55:13 +0100

    Update krita to 3.1.2 (#29681)

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -1,6 +1,6 @@
 cask 'krita' do
-  version '3.1.1'
-  sha256 '6a654ed36b24ad77ce6c55cf850234aa7db1f7bf1b6bddb54491097401ee90f8'
+  version '3.1.2'
+  sha256 '7d8e5b97615327122887f1114a009093e2974aa4fca01e56950fa25173b9600a'
 
   # kde.org/stable/krita was verified as official when first introduced to the cask
   url "http://download.kde.org/stable/krita/#{version}/krita-#{version}.dmg"
