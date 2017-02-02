603193e3cc082f269385a8ec34021b16a097d3f4
Colin Dunn <cgd@vikingdev.net>
2017-02-03 06:16:51 +1100

    Update preform to 2.10.3,489 (#29729)

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask 'preform' do
-  version '2.10.2,454'
-  sha256 '690bd4c2f17d64e8e984eee1aba8896845e5d0f422aab4c44b0ec4b3c5561465'
+  version '2.10.3,489'
+  sha256 '926dcc4992aa21a7f191f723f317f376e0d34b7e53bc3eaf3c439c9ece03f530'
 
   # s3.amazonaws.com/FormlabsReleases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release_origin_release_#{version.before_comma}_build_#{version.after_comma}.dmg"
