e23c20c3c254106e7e449953c4d03984da64adca
HobbinHood <nils.hobbensiefken@gmail.com>
2017-02-02 01:54:14 +0100

    Update tomtom-mysports-connect to 3.1.6 (#29632)

--- Casks/tomtom-mysports-connect.rb
+++ Casks/tomtom-mysports-connect.rb
@@ -2,7 +2,8 @@ cask 'tomtom-mysports-connect' do
   version :latest
   sha256 :no_check
 
-  url 'http://download.tomtom.com/sweet/fitness/MySportsConnectDesktop/current/TomTomMySportsConnectInstaller.pkg'
+  # sports.tomtom-static.com was verified as official when first introduced to the cask
+  url 'https://sports.tomtom-static.com/downloads/desktop/mysportsconnect/latest/TomTomMySportsConnectInstaller.pkg'
   name 'TomTom MySports Connect'
   homepage 'https://www.tomtom.com/mysports/getstarted'
 
