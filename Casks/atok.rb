d8dcbc0d9db2ff3b88e441aa3bb27e795cdd14c6
Tsutomu Ohkura <tsutomu+github@ohkura.com>
2017-02-04 19:57:34 +0900

    Add atok (#29783)
    * Add ATOK 2016 for Mac, a Japanese IME by JustSystem.  It's a package for both trial and normal use for monthly payment user.
    
    * Fix style by "brew cask style --fix"
    
    * Add interpolation fix style

---
+++ Casks/atok.rb
@@ -0,0 +1,25 @@
+cask 'atok' do
+  version '2016'
+  sha256 'bda513644013a35c352624f74b4b918f35650e9a88155ecc758d978cddb1a9ae'
+
+  url 'https://gate.justsystems.com/download/atok/ut/mac/at29try.dmg'
+  name 'ATOK'
+  homepage 'http://www.justsystems.com/jp/products/atokmac/'
+
+  pkg "ATOK #{version} インストーラ.pkg"
+
+  uninstall pkgutil:   [
+                         "com.justsystems.atok#{version}.doc.pkg",
+                         "com.justsystems.atok#{version}.pkg",
+                         "com.justsystems.atok#{version}.quicklook.pkg",
+                         "com.justsystems.atok#{version}.sync.pkg",
+                         'com.justsystems.JustOnlineUpdate.pkg',
+                         'com.justsystems.pkg.lma',
+                       ],
+            launchctl: [
+                         'com.justsystems.inputmethod.atok29',
+                         'com.justsystems.launchd.jslmaUI',
+                         'com.justsystems.launchd.UpdateChecker',
+                         'com.justsystems.launchd.Atok29.AlBg',
+                       ]
+end
