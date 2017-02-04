ed50c9fe86ec7ea3388a601d023f78ebc998546f
Colin Dunn <dunnc@amazon.com>
2017-02-05 10:38:00 +1100

    Add 9 discovered appcasts

--- Casks/bragi-updater.rb
+++ Casks/bragi-updater.rb
@@ -3,6 +3,8 @@ cask 'bragi-updater' do
   sha256 '305fd036fae31b765455c8dab9176f12b8f98a526d45ead0bb9edc41a2940c86'
 
   url "http://update.bragi.com/bin/Bragi%20Updater-#{version}.dmg"
+  appcast 'http://update.bragi.com/',
+          checkpoint: 'bd743631d7dd53de5b729500723b1b0ce47412b4ee8113dbb29a612362c1d49d'
   name 'Bragi Updater'
   homepage 'http://update.bragi.com/'
 
--- Casks/veusz.rb
+++ Casks/veusz.rb
@@ -3,6 +3,8 @@ cask 'veusz' do
   sha256 '38d4144babf3a07845b018af610cdf4b21a951a9b7c200c904509dbbe1d2b7a7'
 
   url "http://download.gna.org/veusz/Binaries-AppleOSX/veusz-#{version}-AppleOSX.dmg"
+  appcast 'http://download.gna.org/veusz/Binaries-AppleOSX/',
+          checkpoint: '7dcefe5737e3e8cbd0134289588e301755e351bc8db898e543c762ab8e01c972'
   name 'Veusz'
   homepage 'http://home.gna.org/veusz/'
 
