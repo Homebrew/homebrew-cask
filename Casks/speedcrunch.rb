e702acfbe7e8d833f5cadd4f71b0cbe064fea4ab
Colin Dunn <cgd@vikingdev.net>
2017-02-04 22:19:58 +1100

    Update speedcrunch to 0.12 (#29792)

--- Casks/speedcrunch.rb
+++ Casks/speedcrunch.rb
@@ -1,6 +1,6 @@
 cask 'speedcrunch' do
-  version '0.11'
-  sha256 '1ce5ef9d167614a2e63daad43a23bd8df60b8ea641df6be9aabdf826bbb5a826'
+  version '0.12'
+  sha256 '1c513acd0db375e079f47055f5903b538361fdd2ebf62d09bd3e876cb7513a94'
 
   # bitbucket.org/heldercorreia/speedcrunch was verified as official when first introduced to the cask
   url "https://bitbucket.org/heldercorreia/speedcrunch/downloads/SpeedCrunch-#{version}.dmg"
