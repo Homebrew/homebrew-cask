0fb3414fb45a322dbbe74379e0e145e82b2a4291
Colin Dunn <cgd@vikingdev.net>
2017-02-04 15:56:38 +1100

    Add versions and checksums to latest casks with appcasts (#29756)

--- Casks/carmetal.rb
+++ Casks/carmetal.rb
@@ -1,10 +1,10 @@
 cask 'carmetal' do
-  version :latest
-  sha256 :no_check
+  version '3.8.2'
+  sha256 'ca91624b708b50428d4390ae2c21176d689d73f5e432061fc8922f43bc4d33b3'
 
   url 'http://db-maths.nuxit.net/CaRMetal/download/carmetal.dmg'
   appcast 'http://db-maths.nuxit.net/CaRMetal/telechargement_en.html',
-          checkpoint: '4caaf30e8385702176b6cb1358bdcb91187e3e5f9427b15f1fb661d2b13dd30f'
+          checkpoint: '27dc224170e577131722b1fda1c86db8cbfeb4915dfbc9ca463cb0aa35d8eb8c'
   name 'CaRMetal'
   homepage 'http://db-maths.nuxit.net/CaRMetal/index_en.html'
 
--- Casks/launchpad-manager-yosemite.rb
+++ Casks/launchpad-manager-yosemite.rb
@@ -1,7 +1,7 @@
 cask 'launchpad-manager-yosemite' do
   if MacOS.version <= :mavericks
-    version :latest
-    sha256 :no_check
+    version '1.3.11'
+    sha256 'f686a9a332663a003e9fabd32a1d44fc98debda15225368f1e8aef181955bc72'
 
     url 'http://launchpadmanager.com/download.php/LaunchpadManager.dmg'
 
