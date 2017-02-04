93d2a45b91444d43fd18cad92a4efebde5586745
Jussi <wickedsp1d3r@users.noreply.github.com>
2017-02-04 13:48:43 +0200

    Update itsycal to 0.11.1 (#29800)

--- Casks/itsycal.rb
+++ Casks/itsycal.rb
@@ -6,13 +6,13 @@ cask 'itsycal' do
     # s3.amazonaws.com/itsycal was verified as official when first introduced to the cask
     url "https://s3.amazonaws.com/itsycal/Itsycal-#{version}.zip"
   else
-    version '0.11.0'
-    sha256 '60ff34640b0b18e61ffe84af20d048ca20213712cfcfedae944119c346222634'
+    version '0.11.1'
+    sha256 'd913b6be32f75c64e4b01e5086dc81656fceff4d3f5222a2c49a38289d7e2ad5'
 
     # s3.amazonaws.com/itsycal was verified as official when first introduced to the cask
     url "https://s3.amazonaws.com/itsycal/Itsycal-#{version}.zip"
     appcast 'https://s3.amazonaws.com/itsycal/itsycal.xml',
-            checkpoint: '37721ee9e4205b27db362d645a0a96ba8946f5de8b080b3b8e483c6dd981fc1a'
+            checkpoint: '213a375dd57d079d19796d2c258b1c88f840cdc12d50843b73c4b70c506d5c32'
   end
 
   name 'Itsycal'
