1ca22f9a53ec58d736e08edc4a148205a45297f3
Colin Dunn <dunnc@amazon.com>
2017-02-05 10:46:56 +1100

    Update filemaker-odbc sha and add appcast

--- Casks/filemaker-odbc.rb
+++ Casks/filemaker-odbc.rb
@@ -1,8 +1,10 @@
 cask 'filemaker-odbc' do
   version '14.0.10'
-  sha256 '68284d8556622081d90f2502b3785200090b8868f4b335b3512c5e0023102d8a'
+  sha256 '27aeca66ea4e82ac0dbdd538f42037611fefa4b7c6e461d3dba35cb10de20c18'
 
   url "http://fmdl.filemaker.com/UPDT/#{version.major}/FM#{version.major_minor}v1_xDBC_#{version}.dmg"
+  appcast 'https://www.filemaker.com/support/updaters/updater_json.txt',
+          checkpoint: '3936901fb3008befea96737250e1b2995894404938e83b1d431e1c7e6ec9f863'
   name 'FileMaker ODBC Client Drivers'
   homepage 'https://www.filemaker.com/'
 
