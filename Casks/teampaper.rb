9b57953cbd41d16bb19c0e1c6c977246c191f032
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-05 09:22:32 +0800

    Update teampaper.rb (#29836)
    * Update teampaper.rb
    
    * Update teampaper.rb

--- Casks/teampaper.rb
+++ Casks/teampaper.rb
@@ -1,12 +1,12 @@
 cask 'teampaper' do
-  version '2.0,8610'
-  sha256 '8acd953f91273b08da1511a72472bb947c37b0907ff80152de51e995c7db09e1'
+  version '2.0.8610'
+  sha256 'bb0911addcc22462ebf826137c049411a9b6215cf80917939e592d9e5065435c'
 
-  url "https://www.teampaper.me/sparkle/teampaper#{version.after_comma}.zip"
+  url 'http://teampaper.me/snap/download/teampapersnap.dmg'
   appcast 'https://www.teampaper.me/sparkle/sparkle.xml',
           checkpoint: '2f477deb9b9b1eb2911395024627d6cb5e5588cd2a9be54d980cfe4c6db7c95e'
   name 'TeamPaper'
-  homepage 'http://teampaper.me/'
+  homepage 'https://teampaper.me/snap/'
 
   app 'TeamPaper.app'
 end
