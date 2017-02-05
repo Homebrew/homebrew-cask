69a72a4bb170026c793ea740031db9d8160491a4
Tsutomu Ohkura <t-ohkura@futurer.net>
2017-02-05 12:04:30 +0900

    Add BOOKSCAN Downloader 2.6.8.

---
+++ Casks/bookscan-downloader.rb
@@ -0,0 +1,12 @@
+cask 'bookscan-downloader' do
+  version '2.6.8'
+  sha256 'f5c172a02803524fd0dec6b0a1556af5f9248f4eeb35a54caa3a1b3228fd616d'
+
+  url "http://system.bookscan.co.jp/downloader/bookscanDownloader#{version}.air"
+  name 'BOOKSCAN Downloader'
+  homepage 'http://bookscan.co.jp/'
+
+  depends_on cask: 'adobe-air'
+
+  app 'BOOKSCAN ダウンローダー 2.6β.app'
+end
