965c9fc921bc1e93bc6671b2c24b537d4e7def88
Victor Popkov <victor@popkov.me>
2017-02-05 22:54:04 +0200

    Update powerphotos to 1.2.2

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -1,11 +1,11 @@
 cask 'powerphotos' do
-  version '1.2.1'
-  sha256 'b69a97b5771ce2b7772adca381a0a584cdadc160cf520617e6ec101523f33925'
+  version '1.2.2'
+  sha256 '5d88d7bb8164c700b64ba22ab93ada0309a548ecc1a80f2ad2ca1d797fe07b76'
 
   # s3.amazonaws.com/fatcatsoftware/powerphotos was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/fatcatsoftware/powerphotos/PowerPhotos_#{version.no_dots}.zip"
   appcast 'https://www.fatcatsoftware.com/powerphotos/powerphotos_appcast.xml',
-          checkpoint: 'aa291cd226bfc68e1443736d6015a4018bc75d1bec691aa7a5a97ec5ba704c06'
+          checkpoint: '5f7b2f3a82cd8910441231320f6d3c631215c97245a6a5fd6a24da3abcb4854c'
   name 'PowerPhotos'
   homepage 'https://www.fatcatsoftware.com/powerphotos/'
 
