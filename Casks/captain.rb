661029fc83ee50a84c5ee0c4996005b871c47358
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-02-06 18:48:43 -0200

    Add Captain.app v6.0.0

---
+++ Casks/captain.rb
@@ -0,0 +1,11 @@
+cask 'captain' do
+  version '6.0.0'
+  sha256 '0d077781163d4a759d428218f6028829ee50e97f1d40303bb42020145a10be2b'
+
+  # dropbox.com was verified as official when first introduced to the cask
+  url 'https://www.dropbox.com/s/pkayzy14unr4r0a/Captain.dmg?dl=1'
+  name 'Captain'
+  homepage 'https://getcaptain.co/'
+
+  app 'Captain.app'
+end
