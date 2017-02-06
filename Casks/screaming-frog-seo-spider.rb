2f5e06481df75e89ee7608487acca5901cf397a0
Colin Dunn <cgd@vikingdev.net>
2017-02-06 18:01:20 +1100

    Update screaming-frog-seo-spider to 7.2 (#29873)

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -3,11 +3,13 @@ cask 'screaming-frog-seo-spider' do
     version '2.40'
     sha256 'f37a517cb1ddb13a0621ae2ef98eba148027b3a2b5ce56b6e6b4ca756e40329b'
   else
-    version '7.1'
-    sha256 'ca9f3ef0287da969de9d35239773853fb8bca91418d61c27cf5cbb04a15601a4'
+    version '7.2'
+    sha256 'd3654be1c66e4c8fb4a5530a35eff62582a5272a2064243679795cc7d2f1d3ad'
   end
 
   url "https://www.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}.dmg"
+  appcast 'https://www.screamingfrog.co.uk/wp-content/themes/screamingfrog/inc/download-modal.php',
+          checkpoint: '4741066c7036d2499b440c4528a3b2d1416c296e620bbb08fa3a942d628dc7ab'
   name 'Screaming Frog SEO Spider'
   homepage 'https://www.screamingfrog.co.uk/seo-spider/'
 
