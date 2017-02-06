90a6a5189a95148b41bf746adcb2c327dd2690b1
Victor Popkov <victor@popkov.me>
2017-02-07 01:27:58 +0200

    Update sylo to 3.6 (#29897)

--- Casks/sylo.rb
+++ Casks/sylo.rb
@@ -1,11 +1,11 @@
 cask 'sylo' do
-  version '3.5'
-  sha256 'e18335580e2a3905b383ab3c3cec28ee95a28528e9f0b8d9c0edfb9027357e90'
+  version '3.6'
+  sha256 'e64d30c961568b6113df63857710527eeae8cc9bb032afff139606da9847f4f4'
 
   # s3-us-west-2.amazonaws.com/unision was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/unision/Sylo.dmg'
   appcast 'http://admin.unisionmusic.com/sylo/update.xml',
-          checkpoint: '4bf7981f23b254b78788617f903ef03e00ced67d3872b08b23a3a96bf11c576f'
+          checkpoint: '79b1a897a6fb230ffe8da31b7eb90c457ead5130c4d9919cd44df08dacfd90c0'
   name 'Sylo'
   homepage 'http://www.sylomusic.com/'
 
