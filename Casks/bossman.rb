f1f3b906712b99707ae8bc43e4e4db8fcf71d248
imichaeldotorg <michael@imichael.org>
2017-02-05 21:05:48 -0700

    Fixed style of Bossman 0.0.1

--- Casks/bossman.rb
+++ Casks/bossman.rb
@@ -1,15 +1,15 @@
-cask 'bossman' do  
-  version "0.0.1"
-  sha256 "ae33e81729aa9f014bbb02a295f0bbd5d145042f7e98f2d30baa5af8c8752f03"
+cask 'bossman' do
+  version '0.0.1'
+  sha256 'ae33e81729aa9f014bbb02a295f0bbd5d145042f7e98f2d30baa5af8c8752f03'
 
-  name "Bossman"
-  app "Bossman.app"
-  homepage "https://github.com/ianclarksmith/Bossman"
-  url "https://github.com/ianclarksmith/Bossman/releases/download/0.0.1/Bossman.zip"
+  url 'https://github.com/ianclarksmith/Bossman/releases/download/0.0.1/Bossman.zip'
+  appcast 'https://github.com/ianclarksmith/Bossman/releases.atom',
+          checkpoint: 'fe7abd2bc606f39d8fb69029bd6bdc7e546598d1e7ceabd4c93496d6d52f72fb'
+  name 'Bossman'
+  homepage 'https://github.com/ianclarksmith/Bossman'
 
-  appcast "https://github.com/ianclarksmith/Bossman/releases.atom",
-          checkpoint: "fe7abd2bc606f39d8fb69029bd6bdc7e546598d1e7ceabd4c93496d6d52f72fb"
+  depends_on formula: 'tmux'
 
-  depends_on formula: "tmux"
-  depends_on macos: '>= :mavericks'
+  app 'Bossman.app'
+  app 'Bossman.app'
 end
