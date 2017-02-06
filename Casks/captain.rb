94bf0d91df3b098dd3c512b0117fe223a931c381
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-02-06 20:03:48 -0200

    Fix Captain Cask
    Set latest version, disable sha256 check

--- Casks/captain.rb
+++ Casks/captain.rb
@@ -1,6 +1,6 @@
 cask 'captain' do
-  version '6.0.0'
-  sha256 '0d077781163d4a759d428218f6028829ee50e97f1d40303bb42020145a10be2b'
+  version :latest
+  sha256 :no_check
 
   # dropbox.com was verified as official when first introduced to the cask
   url 'https://www.dropbox.com/s/pkayzy14unr4r0a/Captain.dmg?dl=1'
