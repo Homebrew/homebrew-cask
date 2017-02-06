620182563b94f36a4f997332d5a542695c0f1c7a
Vítor Galvão <info@vitorgalvao.com>
2017-02-06 19:57:10 +0000

    Update webarchiveextractor.rb

--- Casks/webarchiveextractor.rb
+++ Casks/webarchiveextractor.rb
@@ -3,6 +3,8 @@ cask 'webarchiveextractor' do
   sha256 'ff9d0debc9c718b58752c1c8163fea627e461238b7f315bdbe85329d427bd7f4'
 
   url "https://downloads.sourceforge.net/webarchivext/WebArchiveExtractor.#{version}.zip"
+  appcast 'https://sourceforge.net/projects/webarchivext/rss',
+          checkpoint: '6fac095518b79cf496e71a1929b8ea8a91aac73023e31a5c93d40605f56f916f'
   name 'WebArchiveExtractor'
   homepage 'http://webarchivext.sourceforge.net'
 
