0019c955fb2fc3f43b67c3c7ce734cb69f864076
Victor Popkov <victor@popkov.me>
2017-02-06 23:00:17 +0200

    Update jump-desktop to 7.0.3

--- Casks/jump-desktop.rb
+++ Casks/jump-desktop.rb
@@ -1,10 +1,10 @@
 cask 'jump-desktop' do
-  version '7.0.2'
-  sha256 'e74af344b252b4e7fc6ca3e083c14db55cab9adb433efdc93a5e7fd010e24966'
+  version '7.0.3'
+  sha256 '987763efc74b5bdefd8cdff7cf804d9fb3334a1b0a8d2acac489197a9f7ddf3d'
 
   url 'https://jumpdesktop.com/downloads/jdmac'
   appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml',
-          checkpoint: '16c78aa6cdc4ecfceb2cce490f374230bc7e15551023277e71c3a4514481aa6f'
+          checkpoint: '90719bf84fce814a49fbd8645b5bd7d4655ef6980353272ae5d083ef34791d1c'
   name 'Jump Desktop'
   homepage 'https://jumpdesktop.com/#jdmac'
 
