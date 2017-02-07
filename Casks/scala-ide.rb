de848d7f83fe2d865d75d4d38c05594cd0246ba1
Eric Nielsen <eric@amalgamar.com.br>
2017-02-07 17:33:31 -0500

    Update scala-ide to 4.5.0,2.11:20161213

--- Casks/scala-ide.rb
+++ Casks/scala-ide.rb
@@ -1,9 +1,9 @@
 cask 'scala-ide' do
-  version '4.4.1,2.11:20160504'
-  sha256 'b3bd0907aec82e943de80df428839f08fca6823e99484bf28b50f878d1503d05'
+  version '4.5.0,2.11:20161213'
+  sha256 'acfd393b0c4d88049b470e5187acc75c3c95c586834ed58c74fc9e379eae927e'
 
   # downloads.typesafe.com/scalaide-pack was verified as official when first introduced to the cask
-  url "https://downloads.typesafe.com/scalaide-pack/#{version.before_comma}-vfinal-luna-#{version.before_colon.after_comma.no_dots}-#{version.after_colon}/scala-SDK-#{version.before_comma}-vfinal-#{version.before_colon.after_comma}-macosx.cocoa.x86_64.zip"
+  url "https://downloads.typesafe.com/scalaide-pack/#{version.before_comma}-vfinal-neon-#{version.before_colon.after_comma.no_dots}-#{version.after_colon}/scala-SDK-#{version.before_comma}-vfinal-#{version.before_colon.after_comma}-macosx.cocoa.x86_64.zip"
   name 'Scala IDE'
   homepage 'http://scala-ide.org/'
 
