8b3bcb72924e60268bb0d644f5a3aa0abf51169b
Daniel Feitosa <feitosa-daniel@users.noreply.github.com>
2017-02-07 17:27:36 +0100

    Fixed download URL

--- Casks/cerebro.rb
+++ Casks/cerebro.rb
@@ -3,7 +3,7 @@ cask 'cerebro' do
   sha256 'e3d77b42b778c7b9f46b35846a2cc4d2149da45052d549456541a893d03a371e'
 
   # github.com/KELiON/cerebro was verified as official when first introduced to the cask
-  url "https://github.com/KELiON/cerebro/releases/download/v#{version}/cerebro-#{version}.dmg"
+  url "https://github.com/KELiON/cerebro/releases/download/#{version}/cerebro-#{version}.dmg"
   appcast 'https://github.com/KELiON/cerebro/releases.atom',
           checkpoint: 'da110ee4f7e47efe1d5c12ad5885433855dfce78f07935867d65abf06c2f52cc'
   name 'Cerebro'
