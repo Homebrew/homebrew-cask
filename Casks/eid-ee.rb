68752fd83afbd8567e8363e8cd1041cc3e70dccf
Jaan Toots <jaantoots@gmail.com>
2017-02-07 07:20:16 +0000

    Update eid-ee to 17.01.1686
    https://github.com/Homebrew/homebrew-cask-eid/pull/19

--- Casks/eid-ee.rb
+++ Casks/eid-ee.rb
@@ -1,6 +1,6 @@
 cask 'eid-ee' do
-  version '3.12.5.1673'
-  sha256 'f68f76ad009b661c3f1ad76856ddc39e430a72963a2d3dec3093781067caed66'
+  version '17.01.1686'
+  sha256 '1cae314556d643663ea13d79abd67ff84aade76d5b50b331faf42cfa6b785b02'
 
   url "https://installer.id.ee/media/osx/Open-EID_#{version}.dmg"
   name 'Electronic identity card software for Estonia'
