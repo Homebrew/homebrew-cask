755ea7a257d57be7396eb3e48d2f2885ff90ecc2
Grettir Asmundarson <grettir@tinypineapple.com>
2017-02-07 10:40:28 -0700

    Update cookie to 5.2.2

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.2.1'
-  sha256 'cca57cc80e879297aab1490af0b2034fea56e31dc887c1377acc8c5962b5113b'
+  version '5.2.2'
+  sha256 '292807d489db35f54497a9bbc38e5beb2106f57265e32dc5acea09f31986d86e'
 
   url "https://sweetpproductions.com/products/cookie#{version.major}/Cookie#{version.major}.zip"
   appcast "https://sweetpproductions.com/products/cookie#{version.major}/appcast.xml",
-          checkpoint: 'dc3ae52241c15fedeb65213cfb816a92436b22014b27c5bec708060c3aad71ad'
+          checkpoint: '6355be512e41644cde806c9d5945ce632e8b155c33837333531eeaf076c92993'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
 
