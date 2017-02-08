cdf1bef7b9f76a4244108a5daa52b861decf2685
Vítor Galvão <info@vitorgalvao.com>
2017-02-08 02:01:11 +0000

    Update whatroute.rb

--- Casks/whatroute.rb
+++ Casks/whatroute.rb
@@ -1,12 +1,6 @@
 cask 'whatroute' do
-  if MacOS.version <= :mavericks
-    version '1.13.3'
-    sha256 'bc8a4612dcd2d64758dbb2714de60dd95907744cc59dd771a0be29bd56d81e5a'
-
-  else
-    version '2.0.13'
-    sha256 '920c088f9c0b80b78eb632c64119c81096497536ca62e4ce428516c3471be5c5'
-  end
+  version '2.0.13'
+  sha256 '920c088f9c0b80b78eb632c64119c81096497536ca62e4ce428516c3471be5c5'
 
   url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
   name 'WhatRoute'
