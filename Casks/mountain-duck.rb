4241ab81b5bd19bf3e7baf88274a32982f733ac8
joshuroar <joshuroar@users.noreply.github.com>
2017-02-08 04:45:56 -0800

    Update mountain-duck to 1.7.0.6208 (#29942)

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,10 +1,10 @@
 cask 'mountain-duck' do
-  version '1.6.3.5079'
-  sha256 '652046c4003bdfdccb1dd8aebd68c4dc6e48eb16028a224a8f479e99eaa05f15'
+  version '1.7.0.6208'
+  sha256 'ca1df4e82f7d4e3487ec27f95e9346ae8b9b31e6d1b60e3da5ae99fef7578090'
 
   url "https://dist.mountainduck.io/Mountain%20Duck-#{version}.zip"
   appcast 'https://version.mountainduck.io/changelog.rss',
-          checkpoint: 'c5f444c8e14978645c62a3b30af904ec81a8ea1fcaf054ffdd3883c5aee95e6e'
+          checkpoint: '1d1fc124c6155b14322e8f742e3127b1dba94bb65679973c486f1cdcfc08ab7c'
   name 'Mountain Duck'
   homepage 'https://mountainduck.io/'
 
