ef56c02238cfda8ecac2f9a3b7765ccad30fbf70
Mendel Kramer <mendelk@users.noreply.github.com>
2017-02-09 16:22:59 -0500

    Fixes DevDogs v0.2.1 : Add checkpoint to appcast

--- Casks/devdogs.rb
+++ Casks/devdogs.rb
@@ -3,7 +3,8 @@ cask 'devdogs' do
   sha256 '004cb15be88bbef2516d0ed3d75f6e4e198b5a0700ecfeba4131f34cc1cb869f'
 
   url "https://github.com/ragingwind/devdogs/releases/download/v#{version}/Devdogs-#{version}.zip"
-  appcast 'https://github.com/ragingwind/devdogs/releases.atom'
+  appcast 'https://github.com/ragingwind/devdogs/releases.atom',
+          checkpoint: 'a7376c6ffe714969b60b94a478fb74e8eb0a34fed5a4e372d566257bfc35dd52'
   name 'Devdogs'
   homepage 'https://github.com/ragingwind/devdogs'
 
