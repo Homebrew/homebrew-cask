061ff6c9aedebcad19740dfa572e62cce6b9e8cb
Vítor Galvão <info@vitorgalvao.com>
2017-02-09 00:58:31 +0000

    Update ipsecuritas.rb

--- Casks/ipsecuritas.rb
+++ Casks/ipsecuritas.rb
@@ -3,6 +3,8 @@ cask 'ipsecuritas' do
   sha256 '42e85f68aa6a321fdaea7a352f5d85d7a987bb7e2a8067360d23633a9df3baba'
 
   url "http://www.lobotomo.com/products/downloads/IPSecuritas%20#{version}.dmg"
+  appcast "http://www.lobotomo.com/products/IPSecuritas/appcast#{version.major}.xml",
+          checkpoint '49fa4775fa217555c2f17f40ba71ec9deeeebba7097318bc83b2640937e8c73c'
   name 'IPSecuritas'
   homepage 'http://www.lobotomo.com/products/IPSecuritas/'
 
