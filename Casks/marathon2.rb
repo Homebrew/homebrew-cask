bfa509f663baddd938b646534d7afdb0278c437b
Jim Wise <jwise@draga.com>
2017-02-09 20:00:12 -0500

    Three style tweaks
    - add appcast
    - capitalize name
    - more detailed URL comment

--- Casks/marathon2.rb
+++ Casks/marathon2.rb
@@ -2,9 +2,10 @@ cask 'marathon2' do
   version '20150620'
   sha256 '5459d769d903a679b1d15be8aaf14660c47886e9221ec60fc8defd8cad8135ec'
 
-  # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/Aleph-One-Marathon/alephone/releases/download/release-20150620/Marathon2-20150620-Mac.dmg'
-  name 'marathon2'
+  # github.com/Aleph-One-Marathon/alephone was verified as official when first introduced to the cask
+  url "https://github.com/Aleph-One-Marathon/alephone/releases/download/release-#{version}/Marathon2-#{version}-Mac.dmg"
+  appcast 'https://github.com/Aleph-One-Marathon/alephone/releases.atom', checkpoint: '7960a0eee7bd6c594b7a6cf6ce093a71d31715522774224c9def5903390479d6'
+  name 'Marathon2'
   homepage 'https://alephone.lhowon.org'
 
   app 'Marathon 2.app'
