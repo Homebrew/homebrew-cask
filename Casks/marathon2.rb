380e8ded321cb9a946ef863b163d31fcec9ed221
Jim Wise <jwise@draga.com>
2017-02-09 19:35:22 -0500

    Marathon 2 port from AlephOne

---
+++ Casks/marathon2.rb
@@ -0,0 +1,11 @@
+cask 'marathon2' do
+  version '20150620'
+  sha256 '5459d769d903a679b1d15be8aaf14660c47886e9221ec60fc8defd8cad8135ec'
+
+  # github.com was verified as official when first introduced to the cask
+  url 'https://github.com/Aleph-One-Marathon/alephone/releases/download/release-20150620/Marathon2-20150620-Mac.dmg'
+  name 'marathon2'
+  homepage 'https://alephone.lhowon.org'
+
+  app 'Marathon 2.app'
+end
