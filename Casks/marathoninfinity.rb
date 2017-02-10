d2b9f433891186130b43b53939e6e06007814fa4
Jim Wise <jwise@draga.com>
2017-02-09 20:05:28 -0500

    Line break appcast

--- Casks/marathoninfinity.rb
+++ Casks/marathoninfinity.rb
@@ -4,7 +4,8 @@ cask 'marathoninfinity' do
 
   # github.com/Aleph-One-Marathon/alephone was verified as official when first introduced to the cask
   url 'https://github.com/Aleph-One-Marathon/alephone/releases/download/release-20150620/MarathonInfinity-20150620-Mac.dmg'
-  appcast 'https://github.com/Aleph-One-Marathon/alephone/releases.atom', checkpoint: '7960a0eee7bd6c594b7a6cf6ce093a71d31715522774224c9def5903390479d6'
+  appcast 'https://github.com/Aleph-One-Marathon/alephone/releases.atom',
+          checkpoint: '7960a0eee7bd6c594b7a6cf6ce093a71d31715522774224c9def5903390479d6'
   name 'Marathon Infinity'
   homepage 'https://alephone.lhowon.org'
 
