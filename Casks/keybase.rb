caf4b6624b0f150b35e82e4ee912e83ea3bd4813
Joshua McKinney <joshka@users.noreply.github.com>
2017-02-10 10:40:33 -0600

    Update Keybase to 1.0.18-20170208165022,dc66461 (#29956)

--- Casks/keybase.rb
+++ Casks/keybase.rb
@@ -1,14 +1,27 @@
 cask 'keybase' do
-  version '1.0.18-20170207120017,ce0f90d'
-  sha256 '791f510ed8440e9fa59e5dae2b632ce8427ad50a682e5c3792e4b127a410a638'
+  version '1.0.18-20170209165653,17b641d'
+  sha256 'a5a974089e94e68ac3a5113570e903d0b88fe243d04474107645a5c792d13693'
 
   url "https://prerelease.keybase.io/darwin/Keybase-#{version.before_comma}%2B#{version.after_comma}.dmg"
   appcast 'https://prerelease.keybase.io/update-darwin-prod-v2.json',
-          checkpoint: 'a8454aca49d7dbb496f5fb4e5dc646c3ddeb2fb3f253d561600ea0682f3052f1'
+          checkpoint: '37f720c5a5c9be9cbf429c0b755a4b024ab900f5758f64b195bdf00c672634b4'
   name 'Keybase'
   homepage 'https://keybase.io/'
 
   auto_updates true
 
   app 'Keybase.app'
+
+  postflight do
+    system_command "#{appdir}/Keybase.App/Contents/Resources/KeybaseInstaller.app/Contents/MacOS/Keybase",
+                   args: ["--app-path=#{appdir}/Keybase.app", '--run-mode=prod', '--timeout=10']
+  end
+
+  uninstall_preflight do
+    if system_command('launchctl', args: ['list']).stdout =~ %r{/^\d+.*keybase.Electron/}
+      system_command 'killall', args: ['-kill', 'Keybase']
+    end
+    system_command "#{appdir}/Keybase.app/Contents/SharedSupport/bin/keybase",
+                   args: ['uninstall']
+  end
 end
