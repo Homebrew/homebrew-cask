8baa5de94d19ee17faf0b299c3f3bd570dcf19e5
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-02-10 11:05:45 +0200

    Add kontena-cli 1.1.1

---
+++ Casks/kontena-cli.rb
@@ -0,0 +1,14 @@
+cask 'kontena-cli' do
+  version '1.1.1'
+  sha256 'a764f33f7b87ddbfcb844d09180956519c049b8b54c18967020741f17c39de96'
+
+  url "https://github.com/kontena/kontena/releases/download/v#{version}/kontena-cli-osx-v#{version}-amd64.pkg"
+  appcast 'https://github.com/kontena/kontena/releases.atom',
+          checkpoint: '85e8d0fc3b07695f9825ed57b6bf3269ea0ec624247fcc453a343e183395f332'
+  name 'kontena-cli'
+  homepage 'https://kontena.io/'
+
+  pkg "kontena-cli-osx-v#{version}-amd64.pkg"
+
+  uninstall pkgutil: 'io.kontena.pkg.cli'
+end
