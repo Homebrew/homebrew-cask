a4de9b3813846621a2d266ef393ce344933e050d
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-02-10 14:54:59 +0200

    Add the verified url comment

--- Casks/kontena-cli.rb
+++ Casks/kontena-cli.rb
@@ -2,6 +2,7 @@ cask 'kontena-cli' do
   version '1.1.1'
   sha256 'a764f33f7b87ddbfcb844d09180956519c049b8b54c18967020741f17c39de96'
 
+  # github.com was verified as official when first introduced to the cask
   url "https://github.com/kontena/kontena/releases/download/v#{version}/kontena-cli-osx-v#{version}-amd64.pkg"
   appcast 'https://github.com/kontena/kontena/releases.atom',
           checkpoint: '85e8d0fc3b07695f9825ed57b6bf3269ea0ec624247fcc453a343e183395f332'
