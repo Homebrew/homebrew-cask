8cb2349afbfdae8b1d081823dd940f7a4ec2c5cd
Artem Medvedev <amedvedev@outlook.com>
2017-02-10 02:24:52 +0200

    Update insomnia to 4.2.11 (#29976)

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,10 +1,10 @@
 cask 'insomnia' do
-  version '4.2.9'
-  sha256 '1e72f36c4f5fee44064250a9df86615dbcc3b1766d7f178413af1a7608579557'
+  version '4.2.11'
+  sha256 'b26d7090b6a97c40b8273c72b9721045f4ee228f537c58120f0bd87960f8d1c8'
 
   url "https://downloads.insomnia.rest/mac/Insomnia-#{version}.dmg"
   appcast 'https://insomnia.rest/changelog/index.xml',
-          checkpoint: '3ac85ff14c87e8a720bde9532aa5b32120ecb0c5694f3a799a11e70c23ed0898'
+          checkpoint: 'd45b2f6a4342268859ab01c0ae4d37dfb3cc8c53ccd3ace0dc183141281e7b7f'
   name 'Insomnia'
   homepage 'https://insomnia.rest/'
 
