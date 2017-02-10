a62750ebedd2329c911d77063450ea829379dc07
Vítor Galvão <info@vitorgalvao.com>
2017-02-10 01:43:31 +0000

    Update whale to 0.7.0 (#29982)

--- Casks/whale.rb
+++ Casks/whale.rb
@@ -1,12 +1,21 @@
 cask 'whale' do
-  version '0.6.0'
-  sha256 '8b904b654dd56502bdc08f33acb72c6647ed65735f24c6f7bc2434e0c0c8a006'
+  version '0.7.0'
+  sha256 'daf56f0211916041b8a83950571e492303f33f97c98b31c0b5286d4412d96465'
 
   url "https://github.com/1000ch/whale/releases/download/v#{version}/Whale-macos-v#{version}.zip"
   appcast 'https://github.com/1000ch/whale/releases.atom',
-          checkpoint: '14c344ca7f76d3fc7a7cf88a78871feac1fcbf41391b2d1e713e0acaf331c1e6'
+          checkpoint: 'a24eaea022aab78a11e83b8b4f0db850eb85e662dbef6870eb2df709dbafcaba'
   name 'Whale'
   homepage 'https://github.com/1000ch/whale'
 
   app 'Whale.app'
+
+  zap delete: [
+                '~/Library/Application Support/Whale',
+                '~/Library/Caches/net.1000ch.whale',
+                '~/Library/Caches/net.1000ch.whale.ShipIt',
+                '~/Library/Preferences/net.1000ch.whale.helper.plist',
+                '~/Library/Preferences/net.1000ch.whale.plist',
+                '~/Library/Saved Application State/net.1000ch.whale.savedState',
+              ]
 end
