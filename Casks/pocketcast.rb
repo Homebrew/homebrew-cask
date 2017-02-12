e41d0e5d67879946dd602e7732b0b02306b51243
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-13 00:15:06 +0100

    Update pocketcast to 1.30

--- Casks/pocketcast.rb
+++ Casks/pocketcast.rb
@@ -1,10 +1,10 @@
 cask 'pocketcast' do
-  version '1.26'
-  sha256 '9281179e3f1069470976dc8590e26e4f817142b1641094433e6b5c2806ee7d15'
+  version '1.30'
+  sha256 '9fee8fec0b5c5c3205a24c6b127064077dd6792ddf5d811b4df2dc84a34020da'
 
-  url "https://github.com/mortenjust/PocketCastsOSX/releases/download/#{version}/PocketCast.zip"
+  url "https://github.com/mortenjust/PocketCastsOSX/releases/download/#{version}/PocketCasts.zip"
   appcast 'https://github.com/mortenjust/PocketCastsOSX/releases.atom',
-          checkpoint: 'eecfbcc95b0d373b4c28aa39c3cd9dd8c9a4cb9f7acba7a8d1f597296b9ca4fd'
+          checkpoint: '503438969aa8948af77fdcf08d2ea57535301c837e4690413679f9c652015212'
   name 'Pocket Casts for Mac'
   homepage 'https://github.com/mortenjust/PocketCastsOSX'
 
