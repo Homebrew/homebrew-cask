e2ea4bf6e79eabf412b1f92c744750fa90da22d7
ment4t <gregd@barmail.ch>
2017-02-12 11:53:40 +0000

    Update eloston-chromium to 55.0.2883,95

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -1,10 +1,8 @@
 cask 'eloston-chromium' do
-  version '55.0.2883,75-1:9Morello'
-  sha256 'e19bedc98ec3b252a29468d1536a4f766570e60b68cdabf33cb158c140f26a90'
+  version '55.0.2883,95'
+  sha256 'ce3059098bc77dcc298bf2f81c08bcb541495bdddc3d6a1cc0651fd39ab6f271'
 
-  url "https://github.com/Eloston/ungoogled-chromium/releases/download/#{version.before_comma}.#{version.after_comma.before_colon}/ungoogled-chromium_#{version.before_comma}-#{version.after_comma.before_colon}_macos_#{version.after_colon}.dmg"
-  appcast 'https://github.com/Eloston/ungoogled-chromium/releases.atom',
-          checkpoint: 'e5d42e01d13de904ab33df77bb8d7ebd3432755fe13d7524bdcda988410a7cbe'
+  url "https://github.com/ungoogled-software/ungoogled-chromium-binaries/releases/download/#{version.before_comma}.#{version.after_comma}/ungoogled-chromium_#{version.before_comma}.#{version.after_comma}-1_macos.dmg"
   name 'Ungoogled Chromium'
   homepage 'https://github.com/Eloston/ungoogled-chromium'
 
