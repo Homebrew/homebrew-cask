7b2f3b9fc0af7508060f052bc2c673ac3b9bf6c1
Victor Popkov <victor@popkov.me>
2017-02-12 23:06:41 +0200

    Update losslesscut to 1.5.0 (#30058)

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,10 +1,10 @@
 cask 'losslesscut' do
-  version '1.4.0'
-  sha256 'f721a32381105f178f38c2daedb094244f4a72ba5ff5e84cb2a7e6be598fbb72'
+  version '1.5.0'
+  sha256 '98f972bd24c78142d847e5beb2529bcd0496c8c79ee919696329bf5bb2fbcb73'
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-darwin-x64.zip"
   appcast 'https://github.com/mifi/lossless-cut/releases.atom',
-          checkpoint: '7cb2aaac4723bba159f40fa4590daba8a1ff48ffe8832a5185d445d03654c722'
+          checkpoint: '4ef5a96c89e9f8f77a1c7902b4c3602bb3d96c06c32e4e66c093f1251ef46726'
   name 'Loslesscut'
   homepage 'https://github.com/mifi/lossless-cut'
 
