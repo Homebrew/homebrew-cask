28278d0302115fb876bf4ede511a6b058863a48b
Vítor Galvão <info@vitorgalvao.com>
2017-02-12 23:31:43 +0000

    Update and rename pocketcast.rb to pocketcasts.rb

--- Casks/pocketcasts.rb
+++ Casks/pocketcasts.rb
@@ -1,4 +1,4 @@
-cask 'pocketcast' do
+cask 'pocketcasts' do
   version '1.30'
   sha256 '9fee8fec0b5c5c3205a24c6b127064077dd6792ddf5d811b4df2dc84a34020da'
 
@@ -8,5 +8,5 @@ cask 'pocketcast' do
   name 'Pocket Casts for Mac'
   homepage 'https://github.com/mortenjust/PocketCastsOSX'
 
-  app 'PocketCast.app'
+  app 'PocketCasts.app'
 end
