9fddb278875db9159d16d199ab46f67b0e3a3203
Gianni Tam-McMillan <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-02-13 03:06:50 +0000

    * 'Add tankix.app :latest'

---
+++ Casks/tanki-x.rb
@@ -0,0 +1,10 @@
+cask 'tanki-x' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://static.tankix.com/app/StandaloneOSXIntel64/prod_523/TankiX.dmg'
+  name 'Tanki X'
+  homepage 'http://tankix.com/'
+
+  app 'tankix.app'
+end
