5976fda1b44eb73e675f5694358df516bbaef3b9
Mikael Kjaer <mkjaer@users.noreply.github.com>
2017-02-14 00:53:57 +0400

    Update textsoap to 8.3.1 (#30077)

--- Casks/textsoap.rb
+++ Casks/textsoap.rb
@@ -1,11 +1,11 @@
 cask 'textsoap' do
-  version '8.3'
-  sha256 '6d4fdc5a3866d95d06c28135ae4c602d1bf2572d9bfe59fe335f4a9b8fa8768d'
+  version '8.3.1'
+  sha256 '33f6c3015d6389ba38a6691b6d306586f544c283aa4a43071168528983848351'
 
   # unmarked.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://unmarked.s3.amazonaws.com/textsoap#{version.major}.zip"
   appcast "https://unmarked.s3.amazonaws.com/appcast/textsoap#{version.major}.xml",
-          checkpoint: '91b001a03e3322296844a7ac66a3b6d8a7aa4553f1bebe38b2123d2149993245'
+          checkpoint: 'b7a7dd38ee4f6205c9f0842089c6e8e52d3735ba6076b48290e0ce1f0656b90e'
   name 'TextSoap'
   homepage 'https://www.unmarked.com/textsoap/'
 
