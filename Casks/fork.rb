6c908d15574a60d9e8a2dac9f133a04bafd39b40
Artem Medvedev <amedvedev@outlook.com>
2017-02-14 00:59:59 +0200

    Add appcast and zap delete to Fork.app

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -3,8 +3,17 @@ cask 'fork' do
   sha256 :no_check
 
   url 'https://git-fork.com/update/files/Fork.dmg'
+  appcast 'https://git-fork.com/update/feed.xml',
+          checkpoint: 'f4209e679946a2ad3952aa5cea97c51b377b3f6c9ab78856d5a0c9c436306a42'
   name 'Fork'
   homepage 'https://git-fork.com/'
 
+  auto_updates true
+
   app 'Fork.app'
+
+  zap delete: [
+                '~/Library/Application Support/com.DanPristupov.Fork',
+                '~/Library/Preferences/com.DanPristupov.Fork.plist',
+              ]
 end
