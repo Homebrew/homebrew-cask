1965c33c60244292160ddd9a012d974bcebbb756
Gianni Tam-McMillan <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-02-14 22:15:48 +0000

    * Add Trusteer Rapport :latest

---
+++ Casks/trusteer-rapport.rb
@@ -0,0 +1,10 @@
+cask 'trusteer-rapport' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://download.trusteer.com/Gcur4Wtnu/leopard/Rapport.dmg'
+  name 'Trusteer Rapport'
+  homepage 'https://www.trusteer.com/ProtectYourMoney'
+
+  pkg 'Rapport.pkg'
+end
