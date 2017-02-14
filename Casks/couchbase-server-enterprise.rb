d734c89c1563829d445fbc8702ede379177c9e8f
Chris Griego <cgriego@gmail.com>
2017-02-14 09:20:32 -0600

    Update couchbase-server-enterprise to 4.6.0-DP

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -1,8 +1,8 @@
 cask 'couchbase-server-enterprise' do
-  version '4.5.1'
-  sha256 'de014c7c134eb97ff00be6b2e6f5d0da84295ce05bbb7bb3a4d3c747a365cd22'
+  version '4.6.0-DP'
+  sha256 '4b52ad87d703c235b2f9e4e63a8c1b6f4a720d36b28b81cbf593947256399a3f'
 
-  url "http://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
+  url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
   name 'Couchbase Server'
   homepage 'https://www.couchbase.com/'
 
