cd2d0c5e82490c741fd963cb5f4c49caa5d3b831
Ebrahim Byagowi <ebrahim@gnu.org>
2017-02-15 22:52:46 +0330

    Add MT32Emu 2.0.0

---
+++ Casks/mt32emu.rb
@@ -0,0 +1,10 @@
+cask 'mt32emu' do
+  version '2.0.0'
+  sha256 'b0a8434a6de660725723bd822c00a2bbb24c06ee9f2fd994961fa3dbb1b1bbd1'
+
+  url 'https://downloads.sourceforge.net/munt/munt/2.0.0/OS%20X/MT32Emu-qt-1.4.0.dmg'
+  name 'MT32Emu'
+  homepage 'https://sourceforge.net/projects/munt/'
+
+  app 'MT32Emu.app'
+end
