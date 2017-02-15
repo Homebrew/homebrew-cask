495da2e92952c24ecb78ccb5b61e1e261ba4fa55
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-16 06:41:05 +0800

    Update checkpoint

--- Casks/mt32emu.rb
+++ Casks/mt32emu.rb
@@ -3,8 +3,8 @@ cask 'mt32emu' do
   sha256 'b0a8434a6de660725723bd822c00a2bbb24c06ee9f2fd994961fa3dbb1b1bbd1'
 
   url "https://downloads.sourceforge.net/munt/munt/#{version}/OS%20X/MT32Emu-qt-1.4.0.dmg"
-  appcast "https://sourceforge.net/projects/munt/rss?path=/munt",
-          checkpoint: '84b7ecd3cafd0ff1e4ab1ec372e0e705bcde4eb9a9e3373d16a52c56d83c05a2'
+  appcast 'https://sourceforge.net/projects/munt/rss?path=/munt',
+          checkpoint: 'bec1a2eb6dd50e93a10a8f1e479860531cb1e61f7cd8f55a2992c774a995833c'
   name 'MT32Emu'
   homepage 'https://sourceforge.net/projects/munt/'
 
