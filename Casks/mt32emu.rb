cd1ed93d2672e56eb642f23bab7213111bf435fe
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-16 05:22:19 +0800

    Add appcast

--- Casks/mt32emu.rb
+++ Casks/mt32emu.rb
@@ -2,7 +2,9 @@ cask 'mt32emu' do
   version '2.0.0'
   sha256 'b0a8434a6de660725723bd822c00a2bbb24c06ee9f2fd994961fa3dbb1b1bbd1'
 
-  url 'https://downloads.sourceforge.net/munt/munt/2.0.0/OS%20X/MT32Emu-qt-1.4.0.dmg'
+  url "https://downloads.sourceforge.net/munt/munt/#{version}/OS%20X/MT32Emu-qt-1.4.0.dmg"
+  appcast "https://sourceforge.net/projects/munt/rss?path=/munt/#{version}",
+          checkpoint: '84b7ecd3cafd0ff1e4ab1ec372e0e705bcde4eb9a9e3373d16a52c56d83c05a2'
   name 'MT32Emu'
   homepage 'https://sourceforge.net/projects/munt/'
 
