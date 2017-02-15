d23b9e5a54543f3b695b6cfa5bb091d54b793a12
giannitm <giannitammcmillan@gmail.com>
2017-02-15 05:49:01 +0000

    Update flash-npapi to 24.0.0.221 (#30114)

--- Casks/flash-npapi.rb
+++ Casks/flash-npapi.rb
@@ -1,11 +1,11 @@
 cask 'flash-npapi' do
-  version '24.0.0.194'
-  sha256 '54d7f2ff29fb2271064d25992d4595df81b9227a1a2e7db3caa130404d2c56c0'
+  version '24.0.0.221'
+  sha256 '163f8717e40cdea98cc9c2984fc8522537b8fef6ca5d356c48d52d0e8a63c29d'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.adobe.com/get/flashplayer/pdc/#{version}/install_flash_player_osx.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
-          checkpoint: 'f621c439083bbb6bb5857e26c0d5e5c6b01a616617144f4c2ddf598c1ea6083b'
+          checkpoint: 'b07017a15c35b22a55ddc34fe87200e707c39d98facfdde2b331860d2fe7916f'
   name 'Adobe Flash Player NPAPI (plugin for Safari and Firefox)'
   homepage 'https://get.adobe.com/flashplayer/'
 
