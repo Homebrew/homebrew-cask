412a7a1270a9c542425197a5997d6fe6132caa72
Ben <BenjaminHCCarr@users.noreply.github.com>
2017-02-14 21:55:51 -0500

    Change to >=10.8, version 5.0 only going forward
    Updated to reflect focus on mountain_lion and newer support.
    
    Formula edited, audited, style'ed, and pulled.

--- Casks/rawtherapee.rb
+++ Casks/rawtherapee.rb
@@ -1,23 +1,12 @@
 cask 'rawtherapee' do
-  if MacOS.version <= :lion
-    version '4.2.1'
-    sha256 'd553f04cab4e09a1d66d8811be5ec8468171abe584bdb541ff97d9fa0841e32f'
+  version '5.0-r1_gtk3'
+  sha256 '4a8c7d527d06e674e9a3d61dac3a45760c5ff1314db06b81e63f74bc8a3dff16'
 
-    url "http://rawtherapee.com/releases_head/mac/RawTherapee_OSX_10.7_64_#{version}.zip"
-  else
-    version '5.0-r1_gtk3'
-    sha256 '4a8c7d527d06e674e9a3d61dac3a45760c5ff1314db06b81e63f74bc8a3dff16'
-    
-    url "http://www.rawtherapee.com/shared/builds/mac/RawTherapee_OSX_10.9_#{version}.app.zip"
-  end
-
+  url "http://www.rawtherapee.com/shared/builds/mac/RawTherapee_OSX_10.9_#{version}.app.zip"
   name 'RawTherapee'
   homepage 'http://rawtherapee.com/'
 
-  if MacOS.version <= :lion
-    container nested: "RawTherapee_OSX_10.7_64_#{version}/RawTherapee_OSX_10.7_64_#{version}__.dmg"
-    app 'RawTherapee.app'
-  else
-    app 'RawTherapee-gtk3.app'
-  end  
+  depends_on macos: '>= 10.8'
+
+  app 'RawTherapee-gtk3.app'
 end
