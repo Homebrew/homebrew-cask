cf72030668ba4dd3512534e1b1361029983077b3
Vítor Galvão <info@vitorgalvao.com>
2017-02-16 14:57:12 +0000

    Update waltr2.rb

--- Casks/waltr2.rb
+++ Casks/waltr2.rb
@@ -1,11 +1,10 @@
-cask 'waltr2' do
-  # note: "2" is not a version number, but an intrinsic part of the product name
+cask 'waltr' do
   version :latest
   sha256 :no_check
 
   # dl.devmate.com/com.softorino.waltr2 was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.softorino.waltr2/Waltr2.zip'
-  name 'WALTR2'
+  name 'WALTR 2'
   homepage 'https://softorino.com/w2'
 
   app 'Waltr 2.app'
