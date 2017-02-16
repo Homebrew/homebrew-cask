07357356eaf450e7a7e144eb71819c55369feea9
Andy Croll <andy@goodscary.com>
2017-02-16 19:56:23 +0000

    Add SoundSource 3.0.0 (latest)
    …by Rogue Amoeba

---
+++ Casks/soundsource.rb
@@ -0,0 +1,10 @@
+cask 'soundsource' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://rogueamoeba.com/soundsource/download.php'
+  name 'SoundSource'
+  homepage 'https://rogueamoeba.com/soundsource/'
+
+  app 'SoundSource.app'
+end
