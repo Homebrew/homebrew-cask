1720421ba75e8b226aa812f3e798decddd007152
Vítor Galvão <info@vitorgalvao.com>
2017-02-16 20:34:57 +0000

    Update soundsource.rb

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,8 +1,10 @@
 cask 'soundsource' do
-  version :latest
-  sha256 :no_check
+  version '3.0.0'
+  sha256 '00be2f147b624e27826417c40f2640014540c81b855e27d3c9c0353dd80edd3b'
 
-  url 'https://rogueamoeba.com/soundsource/download.php'
+  url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
+  appcast 'https://rogueamoeba.com/soundsource/releasenotes.php',
+          checkpoint: '726909dc10a44857765c053a83b007a35f21312aa14a2381d4a967e245e59296'
   name 'SoundSource'
   homepage 'https://rogueamoeba.com/soundsource/'
 
