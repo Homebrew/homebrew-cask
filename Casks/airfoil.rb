f59c12c23da45b92c2ab30814b6e767963c285c6
Robin Breathe <robin@isometry.net>
2017-02-16 23:54:18 +0000

    Enhance Airfoil 5.5.2 with zap stanza

--- Casks/airfoil.rb
+++ Casks/airfoil.rb
@@ -8,6 +8,26 @@ cask 'airfoil' do
   name 'Airfoil'
   homepage 'https://www.rogueamoeba.com/airfoil/mac/'
 
+  auto_updates true
+  depends_on macos: '>= :mavericks'
+
   app 'Airfoil/Airfoil Satellite.app'
   app 'Airfoil/Airfoil.app'
+
+  zap quit:       [
+                    'com.rogueamoeba.Airfoil',
+                    'com.rogueamoeba.AirfoilSpeakers',
+                  ],
+      login_item: 'Airfoil Satellite',
+      delete:     [
+                    '/Library/Audio/Plug-Ins/HAL/InstantOn.driver',
+                    '~/Library/Application Support/Airfoil',
+                    '~/Library/Application Support/Airfoil Satellite',
+                    '~/Library/Caches/com.rogueamoeba.Airfoil',
+                    '~/Library/Caches/com.rogueamoeba.AirfoilSpeakers',
+                    '~/Library/Preferences/com.rogueamoeba.Airfoil.plist',
+                    '~/Library/Preferences/com.rogueamoeba.AirfoilSpeakers.plist',
+                    '~/Library/Saved Application State/com.rogueamoeba.Airfoil.savedState',
+                    '~/Library/Saved Application State/com.rogueamoeba.AirfoilSpeakers.savedState',
+                  ]
 end
