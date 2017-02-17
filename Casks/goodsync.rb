ce92ca1927515ecd93a8bd95b31ee8dd0183e26b
Andrew Wade <awade@ligo.caltech.edu>
2017-02-16 17:25:13 -0800

    Update goodsync to v10.3.0

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,8 +1,8 @@
 cask 'goodsync' do
-  version :latest
-  sha256 :no_check
+  version 'v10.3.0'
+  sha256 'd262147c05c4d4968d85ea6fa09f2e78fd4b6a7fc1ab71446b757857efec805f'
 
-  url 'https://www.goodsync.com/download/goodsync-mac.dmg'
+  url 'https://www.goodsync.com/download/goodsync-v10-mac.dmg'
   name 'GoodSync'
   homepage 'https://www.goodsync.com/'
 
