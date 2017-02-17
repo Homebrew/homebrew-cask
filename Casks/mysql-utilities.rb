030ef214589bf328c913baa5bd64bdef5b8cb7a8
Mike Lewis <mike@mplew.is>
2017-02-17 12:35:00 -0500

    Restore mysql-utilities cask (v1.6.5)
    The latest update seems to have fixed the installer issues, meaning
    this cask can be re-added.

---
+++ Casks/mysql-utilities.rb
@@ -0,0 +1,16 @@
+cask 'mysql-utilities' do
+  version '1.6.5'
+  sha256 '114658256e846b4eeff141065e18ef3f779bcbe59e5d2828ab920d905e693db7'
+
+  url "https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities-#{version}-macos10.12.dmg"
+  name 'MySQL Utilities'
+  homepage 'https://dev.mysql.com/downloads/utilities/'
+
+  license :gpl
+
+  depends_on macos: '>= :sierra'
+
+  pkg "mysql-utilities-#{version}.pkg"
+
+  uninstall pkgutil: 'com.oracle.mysql.utilities'
+end
