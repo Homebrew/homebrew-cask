70bc0d7bba60a7ee68653315a867159f95a5c982
Vítor Galvão <info@vitorgalvao.com>
2017-02-17 01:41:44 +0000

    Update goodsync.rb

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,8 +1,8 @@
 cask 'goodsync' do
-  version 'v10.3.0'
+  version '10.3.0'
   sha256 'd262147c05c4d4968d85ea6fa09f2e78fd4b6a7fc1ab71446b757857efec805f'
 
-  url 'https://www.goodsync.com/download/goodsync-v10-mac.dmg'
+  url "https://www.goodsync.com/download/goodsync-#{version.major}-mac.dmg"
   name 'GoodSync'
   homepage 'https://www.goodsync.com/'
 
