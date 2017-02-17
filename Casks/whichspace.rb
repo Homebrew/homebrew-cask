ae6b8f5292ad998413991c61857fdae9d65a393a
Victor Popkov <victor@popkov.me>
2017-02-17 05:39:55 +0200

    Update whichspace to 0.2.0 (#30166)

--- Casks/whichspace.rb
+++ Casks/whichspace.rb
@@ -1,10 +1,10 @@
 cask 'whichspace' do
-  version '0.1.9'
-  sha256 '4c1c0fb5f036cb1e11e2126ea5caaafb2ea9526523ed3a36a160793910529b57'
+  version '0.2.0'
+  sha256 '8cbb5d9517b965e7757abb417aa4d0c27e13e15853574cfc2bcfcc2deeab0567'
 
   url "https://github.com/gechr/WhichSpace/releases/download/v#{version}/WhichSpace.app.zip"
   appcast 'https://github.com/gechr/WhichSpace/releases.atom',
-          checkpoint: '100d2a27e92d419f26342e7c2389388e0812a59640d21203db0c8c81a8aa9b82'
+          checkpoint: '86864d35045fab06c1997228f04b16b66c84d803291fb807d3f35f55ab060eef'
   name 'WhichSpace'
   homepage 'https://github.com/gechr/WhichSpace'
 
