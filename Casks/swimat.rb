89dd32dda1db2385c08cb76fa14505a12b16960a
Jintin <Jintin@users.noreply.github.com>
2017-02-18 20:24:47 +0800

    Update swimat to 1.3.1 (#30193)

--- Casks/swimat.rb
+++ Casks/swimat.rb
@@ -1,10 +1,10 @@
 cask 'swimat' do
-  version '1.3'
-  sha256 '5b0e017bec0de775cb38aeee6d15917e7211f21958f3ead44b56348971d5d31b'
+  version '1.3.1'
+  sha256 '88585d9c478e728838fb842dbd9187839bbc8e3e227a31e7bfb0502717da674c'
 
   url "https://github.com/Jintin/Swimat/releases/download/v#{version}/Swimat.zip"
   appcast 'https://github.com/Jintin/Swimat/releases.atom',
-          checkpoint: 'e8919722a14863c9c2b5f5e2205d8bb45dd9be01a81063a7cc337640e04a3260'
+          checkpoint: 'e2af280e708fb6d5e248f23240fed3968d52ac53c4d4b4023b027025ee254cfa'
   name 'Swimat'
   homepage 'https://github.com/Jintin/Swimat'
 
