b4a68f33c938c4a70a8c8cebe4f0eca3b87aacbc
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2017-02-19 11:53:08 +0100

    Musixmatch 0.3.15 Cask

---
+++ Casks/musixmatch.rb
@@ -0,0 +1,10 @@
+cask 'musixmatch' do
+  version '0.3.15'
+  sha256 '16346617e96641a62e5bd89437288903f95d0fbf04fb59273e249764a75a883c'
+
+  url 'https://download-app.musixmatch.com/download/osx'
+  name 'Musixmatch'
+  homepage 'https://www.musixmatch.com/'
+
+  app 'Musixmatch.app'
+end
