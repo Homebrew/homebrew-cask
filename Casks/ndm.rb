8b11819c810ec03d479c5c026d18d326e59607bf
Victor Popkov <victor@popkov.me>
2017-02-20 00:25:13 +0200

    Update ndm.rb

--- Casks/ndm.rb
+++ Casks/ndm.rb
@@ -1,11 +1,11 @@
 cask 'ndm' do
   version '1.1.0'
-  sha256 'a4da54e09c944cf288a91d999f932daa4fa8102d63974c57caadeca35a187d2c'
+  sha256 '18bb22469f2804f5533a4b87159dedaf570be3fb6cb38ae40dbe26ef5d275170'
 
   # github.com/720kb/ndm was verified as official when first introduced to the cask
   url "https://github.com/720kb/ndm/releases/download/v#{version}/ndm-#{version}.dmg"
   appcast 'https://github.com/720kb/ndm/releases.atom',
-          checkpoint: '31e8ea2c8c1748af0a3060313acf6319e313fc88ff5652d55de68c12910f67a9'
+          checkpoint: 'bc369e17fd3e6ef4421adfd4b86670c474f57ba37f1f9775427ac9b944fc6c46'
   name 'ndm'
   homepage 'https://720kb.github.io/ndm/'
 
