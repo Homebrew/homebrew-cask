0ba9a540a70071e7f37ee29c72ce96a802473cfc
Yurii Kolesnykov <yurikoles@gmail.com>
2017-02-19 12:31:45 -0800

    Update telegram-desktop to 1.0.12

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,11 +1,11 @@
 cask 'telegram-desktop' do
-  version '1.0.5'
-  sha256 'b8003d64cc850956a12aada5a02ea5d1c9f1d8e103ee34620ab380be0c61346c'
+  version '1.0.12'
+  sha256 '5a61daa1d30ea034d87114ba2ccb119207c87a1a946d7cfedf408f75f9ed35d5'
 
   # tdesktop.com was verified as official when first introduced to the cask
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
   appcast "https://tdesktop.com/mac/tupdates/current?version=#{version.no_dots}",
-          checkpoint: 'c5f1d715b3ce140fa3c5b465aa333c32d024a3fb992a72a52fe60da0196cdb00'
+          checkpoint: '98320bae1823c4dbdc9c3c33b54fca18adcb0ed48fa9ecddc13acd004c35f6e5'
   name 'Telegram Desktop'
   homepage 'https://desktop.telegram.org/'
 
