f8aeab0b47b6887390512d7d73f49605e20ab504
Vítor Galvão <info@vitorgalvao.com>
2017-02-19 00:54:36 +0000

    Update packetsender.rb

--- Casks/packetsender.rb
+++ Casks/packetsender.rb
@@ -3,7 +3,7 @@ cask 'packetsender' do
   sha256 '28f4f46fc0e8ec7584660376089f4f07f1626fac6775a954fc17e28154127418'
 
   # github.com/dannagle/PacketSender was verified as official when first introduced to the cask
-  url "https://github.com/dannagle/PacketSender/releases/download/v#{version.before_comma}/PacketSender_v#{version.before_comma.to_s.sub('.', '_')}_#{version.after_comma}.dmg"
+  url "https://github.com/dannagle/PacketSender/releases/download/v#{version.before_comma}/PacketSender_v#{version.before_comma.dots_to_underscores}_#{version.after_comma}.dmg"
   appcast 'https://github.com/dannagle/PacketSender/releases.atom',
           checkpoint: 'b035e6238a22dae4ef736bc40315fc2cd6b81e5ac4ee1a5f02f3debdf41325b5'
   name 'Packet Sender'
