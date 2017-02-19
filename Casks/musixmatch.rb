e042dae8b1afd89ccf4c6e2ce4c3e676160a44ab
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2017-02-19 18:54:04 +0100

    Musixmatch version and sha256 fixes

--- Casks/musixmatch.rb
+++ Casks/musixmatch.rb
@@ -1,6 +1,6 @@
 cask 'musixmatch' do
-  version '0.3.15'
-  sha256 '16346617e96641a62e5bd89437288903f95d0fbf04fb59273e249764a75a883c'
+  version :latest
+  sha256 :no_check
 
   url 'https://download-app.musixmatch.com/download/osx'
   name 'Musixmatch'
