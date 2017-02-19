cc2f524b6be594926f72ecb03d7c046b6002bdfd
Victor Popkov <victor@popkov.me>
2017-02-20 00:23:37 +0200

    Update copyq to 2.9.0 (#30233)

--- Casks/copyq.rb
+++ Casks/copyq.rb
@@ -1,11 +1,11 @@
 cask 'copyq' do
-  version '2.8.2'
-  sha256 '050dd898ffb2c68aad3d9a7163dd0b79ec126bded13249a69236985b69d01699'
+  version '2.9.0'
+  sha256 '4b784d3a7edab3fe4883175cd0fad44116e9db444865403ce0e7a08479016dfe'
 
   # github.com/hluk/CopyQ was verified as official when first introduced to the cask
   url "https://github.com/hluk/CopyQ/releases/download/v#{version}/CopyQ.dmg"
   appcast 'https://github.com/hluk/CopyQ/releases.atom',
-          checkpoint: '2bca0baf7bb9ede52cd8ceb271fea4937043ca427aed43c8378fbc3852a51cd7'
+          checkpoint: '076ee71758e41f3684c5225522c2c14c0c8a0e16ac9d59d8c6e30741a2cdb24c'
   name 'CopyQ'
   homepage 'https://hluk.github.io/CopyQ/'
 
