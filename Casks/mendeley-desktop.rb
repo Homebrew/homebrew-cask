77a83b221a10e84cd06fe25e7f4d6e8051a009c1
Andrew Wade <fincle@users.noreply.github.com>
2017-02-18 20:08:46 -0800

    Update mendeley-desktop to 1.17.8 (#30211)

--- Casks/mendeley-desktop.rb
+++ Casks/mendeley-desktop.rb
@@ -1,6 +1,6 @@
 cask 'mendeley-desktop' do
-  version '1.17.6'
-  sha256 'a42ab1332911a91e4d986b105dd0373e7380c0daf95f610941158c899cc842a3'
+  version '1.17.8'
+  sha256 '6e5c7a648324e389f5cdac52d3ea2255a568054c8bffdeb749c9388719e00687'
 
   url "http://desktop-download.mendeley.com/download/Mendeley-Desktop-#{version}-OSX-Universal.dmg"
   name 'Mendeley'
