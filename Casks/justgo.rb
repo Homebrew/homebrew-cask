46dbd8c662ef45b48868cb58def7e2db2544a546
Irakli N <inadarei@users.noreply.github.com>
2017-02-20 16:39:39 -0500

    Add justgo 1.0.1

--- Casks/justgo.rb
+++ Casks/justgo.rb
@@ -1,8 +1,8 @@
 cask 'justgo' do
   version '1.0.1'
-  sha256 '858217b6025a54e3cb3451903806d6853c6c3478a16c625c263794f29756c410'
+  sha256 '74864b00222d1e679d3673ada2d31e5c6ea8af6ca5572e42293481a862c193c6'
 
-  url 'https://github.com/inadarei/justgo/blob/master/cmd/justgo/build/justgo'
+  url 'https://github.com/inadarei/justgo/blob/master/cmd/justgo/build/justgo.zip?raw=true'
   name 'justgo'
   homepage 'https://github.com/inadarei/justgo'
 
