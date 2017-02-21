dd214182aa020e5d6b482509e15fb5ac886137a2
jbeagley52 <jonathan@jonathanbeagley.com>
2017-02-21 14:29:06 +1100

    Fixed Sourceforge URL for OmegaT again
    Changed build to update

--- Casks/omegat.rb
+++ Casks/omegat.rb
@@ -3,7 +3,7 @@ cask 'omegat' do
   sha256 '3047e5f43d2460901cf476b9b716c1f7c08c8ba0'
 
   # downloads.sourceforge.net/project/omegat was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/project/omegat/OmegaT%20-%20Standard/OmegaT%20#{version.major_minor}%20%build%20#{version.patch}/OmegaT_#{version}_Mac_Signed.zip"
+  url "https://downloads.sourceforge.net/project/omegat/OmegaT%20-%20Standard/OmegaT%20#{version.major_minor}%20%update%20#{version.patch}/OmegaT_#{version}_Mac_Signed.zip"
   appcast 'https://sourceforge.net/projects/omegat/rss',
           checkpoint: 'ee727bd624e9fc1e1358f540422afe8b6f5d917258e97db85d3cb9c8115f4519'
   name 'OmegaT'
