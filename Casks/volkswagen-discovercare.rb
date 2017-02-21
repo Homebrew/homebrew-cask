d836f373ad26c596e09a21c00b8f3ee524d0810a
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-02-21 20:54:40 +0100

    Added name stanza

--- Casks/volkswagen-discovercare.rb
+++ Casks/volkswagen-discovercare.rb
@@ -2,6 +2,7 @@ cask 'volkswagen-discovercare' do
   version :latest
   sha256 :no_check
 
+  name 'Volkswagen DiscoverCare'
   url 'http://en.volkswagen.com/content/medialib/vwd4/global/discovercare/files/discovercare_macen/_jcr_content/renditions/rendition.download_attachment.file/discovercare_mac_en.zip'
   homepage 'http://www.volkswagen.com/discovercare'
 
