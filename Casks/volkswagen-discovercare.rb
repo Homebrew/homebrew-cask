9ab91b65ee161f167026823bfc85b6c815220bab
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-02-21 20:56:33 +0100

    Rubocop-cask fixes

--- Casks/volkswagen-discovercare.rb
+++ Casks/volkswagen-discovercare.rb
@@ -2,8 +2,8 @@ cask 'volkswagen-discovercare' do
   version :latest
   sha256 :no_check
 
-  name 'Volkswagen DiscoverCare'
   url 'http://en.volkswagen.com/content/medialib/vwd4/global/discovercare/files/discovercare_macen/_jcr_content/renditions/rendition.download_attachment.file/discovercare_mac_en.zip'
+  name 'Volkswagen DiscoverCare'
   homepage 'http://www.volkswagen.com/discovercare'
 
   container nested: 'DiscoverCare_v.1.0.3.dmg'
