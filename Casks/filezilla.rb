66a2406f7595f84091f312f0aee89c47c7d1855b
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-22 16:21:18 +0800

    Remove old version

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,11 +1,6 @@
 cask 'filezilla' do
-  if MacOS.version <= :snow_leopard
-    version '3.8.1'
-    sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
-  else
-    version '3.24.1'
-    sha256 '79f28c13820ffcb9e6fa4446a1a317a45fd91d6a67fb1a562d30443f1abe186e'
-  end
+  version '3.24.1'
+  sha256 '79f28c13820ffcb9e6fa4446a1a317a45fd91d6a67fb1a562d30443f1abe186e'
 
   # sourceforge.net/filezilla was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
