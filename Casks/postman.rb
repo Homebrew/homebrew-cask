808cffc50552e206cf7689513b1f3a07c8aee593
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-22 09:05:07 +0800

    Update version and sha

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,8 +1,8 @@
 cask 'postman' do
-  version '4.10.2'
-  sha256 '7d5db210802c925bfd114516db200e3c48e801bb4adf3476526e9d8b84c940dd'
+  version :latest
+  sha256 :no_check
 
-  # dl.pstmn.io was verified as official when first introduced to the cask
+  # dl.pstmn.io/download/latest/osx was verified as official when first introduced to the cask
   url 'https://dl.pstmn.io/download/latest/osx'
   name 'Postman'
   homepage 'https://www.getpostman.com/'
