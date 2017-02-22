aa6c1f590a283dc7612e5493c9b25f6d41441c1a
Victor Popkov <victor@popkov.me>
2017-02-22 09:16:59 +0200

    Update amazon-chime to 4.0.5528 (#30290)

--- Casks/amazon-chime.rb
+++ Casks/amazon-chime.rb
@@ -1,10 +1,10 @@
 cask 'amazon-chime' do
-  version '4.0.5522'
-  sha256 '1e98838fcb722ab840cc3372a466ef7150c13772ea4a63fc600290df6b7d817e'
+  version '4.0.5528'
+  sha256 '832c40397be7386df16622936920eeba5fcf7a3722a3a78938b34bbcb6e940a6'
 
   url "https://clients.chime.aws/mac/releases/Chime-OSX-#{version}.dmg"
   appcast 'https://clients.chime.aws/mac/appcast',
-          checkpoint: '29229c5f4f18a6ddc2f6201678404eba63a95cc2cc19711d16033af03154e873'
+          checkpoint: '9086b58df40b65741389716b144114030c67f40f51a80ee3ff39eabd7c0f7673'
   name 'Amazon Chime'
   homepage 'https://chime.aws/'
 
