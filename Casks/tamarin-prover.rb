47899f7b3f8f9ce96e9e219190875982cda8f7d0
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-22 08:49:38 +0800

    Add interpolation

--- Casks/tamarin-prover.rb
+++ Casks/tamarin-prover.rb
@@ -3,7 +3,7 @@ cask 'tamarin-prover' do
   sha256 'f6178d6b3f7356d3c0d704339417bb413432f781f1ebf810e18ce32eed45ebda'
 
   # github.com/tamarin-prover/tamarin-prover was verified as official when first introduced to the cask
-  url 'https://github.com/tamarin-prover/tamarin-prover/releases/download/1.2.0/tamarin-prover-1.2-macOS-x86_64.zip'
+  url "https://github.com/tamarin-prover/tamarin-prover/releases/download/#{version}/tamarin-prover-#{version.major_minor}-macOS-x86_64.zip"
   appcast 'https://github.com/tamarin-prover/tamarin-prover/releases.atom',
           checkpoint: 'd53924084b55faacd8db4d3c65bbfbfc51583107708084b4e8189fb4ae4d9677'
   name 'tamarin-prover'
@@ -15,5 +15,5 @@ cask 'tamarin-prover' do
                         'graphviz',
                       ]
 
-  binary "tamarin-prover-"${version.major_minor}-macOS-x86_64", target: 'tamarin-prover'
+  binary "tamarin-prover-#{version.major_minor}-macOS-x86_64", target: 'tamarin-prover'
 end
