0c4975c3a8ee1306f0bda24cccdfa82b2c4f715d
Stephen Wade <stephen@stephenwade.me>
2017-02-22 10:52:46 -0500

    updating forklift to version 3.0

--- Casks/forklift.rb
+++ Casks/forklift.rb
@@ -1,17 +1,22 @@
 cask 'forklift' do
-  version '2.6.6'
-  sha256 '0fa2bb7927d07c0c13b964b517b500ea6a77a6a569becba3ea392b2f82c05fce'
+  version '3.0'
+  sha256 '2055f64398e09a61c07e42d74c8074d58739ccc1d1a228f719acde0ef865f2d8'
 
   url "http://download.binarynights.com/ForkLift#{version}.zip"
-  appcast "http://update.binarynights.com/ForkLift#{version.major}/update.xml",
-          checkpoint: '06635ed48d05ac9318bcb2f527d848934f7bf32f173cc9aa1a1ee6bbc6c98443'
+  appcast "https://updates.binarynights.com/ForkLift#{version.major}/update.xml",
+          checkpoint: 'e30a4752e0ef26d7b8aaf988ec151b083334d2bc04d3c6155ab04ae9f5e6623f'
   name 'ForkLift'
-  homepage 'https://www.binarynights.com/'
+  homepage 'https://binarynights.com/forklift/'
+
+  auto_updates true
+  depends_on macos: '>= :el_capitan'
 
   app 'ForkLift.app'
 
   zap delete: [
-                '~/Library/Preferences/com.binarynights.ForkLift2.plist',
-                '~/Library/Caches/com.binarynights.ForkLift2',
+                '~/Library/Application Support/ForkLift',
+                '~/Library/Caches/com.binarynights.ForkLift-3',
+                '~/Library/Preferences/com.binarynights.ForkLift-3.plist',
+                '~/Library/Saved Application State/com.binarynights.ForkLift-3.savedState',
               ]
 end
