9f8b4c1092f04eaf0e811bdd059d87007e53baac
Artem Medvedev <amedvedev@outlook.com>
2017-02-23 08:35:33 +0200

    Update ui-browser to 2.6.0 (#30314)

--- Casks/ui-browser.rb
+++ Casks/ui-browser.rb
@@ -1,6 +1,6 @@
 cask 'ui-browser' do
-  version '2.5.0'
-  sha256 '76a4d3c195d82c27d18b9477554fd1783ae72790b7671762310f8852a38ebc0a'
+  version '2.6.0'
+  sha256 '1240a8a181e896e9fd8ef360f2dbc879ac3566d68d3933c7f5a1e9485a3cbd75'
 
   url "http://pfiddlesoft.com/uibrowser/downloads/UIBrowser#{version.no_dots}.dmg"
   name 'UI Browser'
