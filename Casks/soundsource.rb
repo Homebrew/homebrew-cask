16a3ba24882114d851b37f7221510e552ee17f37
Grettir Asmundarson <grettir@users.noreply.github.com>
2017-02-23 00:57:05 -0700

    Update soundsource to 3.0.1 (#30316)

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,10 +1,10 @@
 cask 'soundsource' do
-  version '3.0.0'
-  sha256 '00be2f147b624e27826417c40f2640014540c81b855e27d3c9c0353dd80edd3b'
+  version '3.0.1'
+  sha256 '4d26415d718e7be8f8e0233be281b55e636330b6c581ea345796d8255470766d'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
   appcast 'https://rogueamoeba.com/soundsource/releasenotes.php',
-          checkpoint: '726909dc10a44857765c053a83b007a35f21312aa14a2381d4a967e245e59296'
+          checkpoint: '73af7680362997b2d1cf34422bda1e2adf8301fb08279fd5193bfdbec288e95f'
   name 'SoundSource'
   homepage 'https://rogueamoeba.com/soundsource/'
 
