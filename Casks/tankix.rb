26ab66d68ab9ac90d12e80affe1e5b58be01e9ce
giannitm <giannitammcmillan@gmail.com>
2017-02-23 23:01:35 +0000

    Update tankix to 532 (#30336)

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,8 +1,8 @@
 cask 'tankix' do
-  version :latest
-  sha256 :no_check
+  version '532'
+  sha256 'f545d6ef9e9127dd2d4a65e57a054286a12150cdee2c216deb50a28cd5e54c29'
 
-  url 'http://static.tankix.com/app/StandaloneOSXIntel64/prod_523/TankiX.dmg'
+  url "http://static.tankix.com/app/StandaloneOSXIntel64/prod_#{version}/TankiX.dmg"
   name 'Tanki X'
   homepage 'http://www.tankix.com/'
 
