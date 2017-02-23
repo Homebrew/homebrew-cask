3ff6f593937c94a39106256db6cdb2d084e3ff86
Vítor Galvão <info@vitorgalvao.com>
2017-02-23 23:49:48 +0000

    Update AppGate-Client.rb

--- Casks/AppGate-Client.rb
+++ Casks/AppGate-Client.rb
@@ -3,16 +3,17 @@ cask 'AppGate-Client' do
   sha256 'a9eb6ed2f46f33dfca21aea81ff163b99978aee27f78bbbc55200ed12728ae62'
 
   url "http://download.cryptzone.com/files/download/AppGate-#{version}/Clients/MacOSX/AppGate_Client.dmg"
-  name 'AppGate (Client)'
+  name 'AppGate (classic)'
   homepage 'https://www.cryptzone.com/downloadcenter/appgate-classic/'
 
   pkg 'AppGate Client.pkg'
 
+  uninstall pkgutil: [
+                       'com.appgate.pkg.appgateclient.component',
+                       'com.appgate.pkg.appgatefwd.component',
+                     ]
+
   caveats do
-    depends_on_java('6')
+    depends_on_java('6+')
   end
-
-  uninstall pkgutil: 'com.appgate.pkg.appgateclient.component'
-  uninstall pkgutil: 'com.appgate.pkg.appgatefwd.component'
-
 end
