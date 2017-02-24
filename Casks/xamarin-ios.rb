68b282419cfe3d4c5e70329f3c12c8f8da4ba19b
Alex Petin <axp@ngs.ru>
2017-02-24 14:54:52 +0700

    Update xamarin-ios to 10.4.0.123 (#30326)

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-ios' do
-  version '10.3.1.8'
-  sha256 '04a118218bd1d02650100a4123bceb2346d142262384c6d2172e074c0db8b398'
+  version '10.4.0.123'
+  sha256 'a836f786fa48bcaaaa72aa9bf55d9742732ead4918fcc43e0663b636eb8a69b6'
 
   url "https://dl.xamarin.com/MonoTouch/Mac/xamarin.ios-#{version}.pkg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: 'ef02a6e6134070526989d176bdc16871c6e00b5a4e546ce976d93e14a87b63fa'
+          checkpoint: '16d648bbecc3ec71d375dbbd193ee0f7a8116c562fedefa9c050f2fac1b4726d'
   name 'Xamarin.iOS'
   homepage 'https://www.xamarin.com/platform'
 
