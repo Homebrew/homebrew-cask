85b1e6a79c795ceaf7fe48a1df9f394af961fc9d
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-02-24 11:27:16 -0300

    Update wmail to 2.2.0

--- Casks/wmail.rb
+++ Casks/wmail.rb
@@ -1,11 +1,11 @@
 cask 'wmail' do
-  version '2.1.0'
-  sha256 '1bbab3c8d6c2c99120258e484df883aa90f7737f246ddaa1ffac4dfd56137677'
+  version '2.2.0'
+  sha256 'dfd33d0c40d600043f507b64c027cf0f2f34ae189c31c33667aa63ff5b366b5c'
 
   # github.com/Thomas101/wmail was verified as official when first introduced to the cask
-  url "https://github.com/Thomas101/wmail/releases/download/#{version}/WMail_#{version.dots_to_underscores}_osx.dmg"
+  url "https://github.com/Thomas101/wmail/releases/download/v#{version}/WMail_#{version.dots_to_underscores}_osx.dmg"
   appcast 'https://github.com/Thomas101/wmail/releases.atom',
-          checkpoint: '3f8d154cc47118f8070fb1a92f76b2c12928e5ad3a5d4019c788653209cedeec'
+          checkpoint: '1287488a9a4d771b86ecb667eef9b44eb0a50a558ccfcc9fb8e76c137c73e2bd'
   name 'WMail'
   homepage 'https://thomas101.github.io/wmail/'
 
