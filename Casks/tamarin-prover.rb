b554cf3bb5030f2a4db1486a82a4b9173f0b2e25
Vítor Galvão <info@vitorgalvao.com>
2017-02-25 17:30:48 +0000

    Update tamarin-prover.rb

--- Casks/tamarin-prover.rb
+++ Casks/tamarin-prover.rb
@@ -3,7 +3,7 @@ cask 'tamarin-prover' do
   sha256 'ab8719d7743dd0e2d3a99bd7233ea4f5fda985c15ed51498115bff1c90652d2c'
 
   # github.com/tamarin-prover/tamarin-prover was verified as official when first introduced to the cask
-  url "https://github.com/tamarin-prover/tamarin-prover/releases/download/#{version}/tamarin-prover-#{version.major_minor_patch}-macOS-x86_64.zip"
+  url "https://github.com/tamarin-prover/tamarin-prover/releases/download/#{version}/tamarin-prover-#{version}-macOS-x86_64.zip"
   appcast 'https://github.com/tamarin-prover/tamarin-prover/releases.atom',
           checkpoint: '8c17c539bdbd3fef91799b534be0dc5b2075d604dcb579be603d6c8dbfa40c4d'
   name 'tamarin-prover'
@@ -15,5 +15,5 @@ cask 'tamarin-prover' do
                         'graphviz',
                       ]
 
-  binary "tamarin-prover"
+  binary 'tamarin-prover'
 end
