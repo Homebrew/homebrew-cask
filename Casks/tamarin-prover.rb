5fe83fb5cd57e1f4f1716aa019c14bf290722021
Jorden Whitefield <j.whitefield@surrey.ac.uk>
2017-02-25 17:20:20 +0000

    URL and Binary name conventions changed in 1.2.1
    URL now requires the major_minor_patch and the binary contained in the
    zip is now just 'tamarin-prover'

--- Casks/tamarin-prover.rb
+++ Casks/tamarin-prover.rb
@@ -1,9 +1,9 @@
 cask 'tamarin-prover' do
   version '1.2.1'
-  sha256 'f8f2eb5ba1fc7ff1f3ed6db0dc4063c291f03c5dace442f0fb896ed5f783f80e'
+  sha256 'ab8719d7743dd0e2d3a99bd7233ea4f5fda985c15ed51498115bff1c90652d2c'
 
   # github.com/tamarin-prover/tamarin-prover was verified as official when first introduced to the cask
-  url "https://github.com/tamarin-prover/tamarin-prover/releases/download/#{version}/tamarin-prover-#{version.major_minor}-macOS-x86_64.zip"
+  url "https://github.com/tamarin-prover/tamarin-prover/releases/download/#{version}/tamarin-prover-#{version.major_minor_patch}-macOS-x86_64.zip"
   appcast 'https://github.com/tamarin-prover/tamarin-prover/releases.atom',
           checkpoint: '8c17c539bdbd3fef91799b534be0dc5b2075d604dcb579be603d6c8dbfa40c4d'
   name 'tamarin-prover'
@@ -15,5 +15,5 @@ cask 'tamarin-prover' do
                         'graphviz',
                       ]
 
-  binary "tamarin-prover-#{version.major_minor}-macOS-x86_64", target: 'tamarin-prover'
+  binary "tamarin-prover"
 end
