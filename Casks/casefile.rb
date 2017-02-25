5495150d2075360b5865c0cf04ab65f67733a2b5
Jorden Whitefield <j.whitefield@surrey.ac.uk>
2017-02-25 16:09:32 +0000

    Added new Cask for CaseFile version v2.1.1.8751
    CaseFile gives you the ability to quickly add, link and analyze data.

---
+++ Casks/casefile.rb
@@ -0,0 +1,10 @@
+cask 'casefile' do
+  version 'v2.1.1.8751'
+  sha256 'd9b330471e6d03a9af436fc45f5ab46b1038bc437cda3b753371733cb813c97c'
+
+  url "https://www.paterva.com/cf211/MaltegoCaseFile.#{version}.dmg"
+  name 'CaseFile'
+  homepage 'https://www.paterva.com/web7/buy/maltego-clients/casefile.php'
+
+  app 'Maltego CaseFile v2.1.1.app'
+end
