17ecc45c84389bf00309adfbdd3ae9ef8ca8d6bd
Vítor Galvão <info@vitorgalvao.com>
2017-02-26 23:10:31 +0000

    ship.rb: mroe specific version

--- Casks/ship.rb
+++ Casks/ship.rb
@@ -1,9 +1,9 @@
 cask 'ship' do
-  version '2.0'
+  version '2.0.2'
   sha256 '0422eef83b18e1f7e60f568b66750060857829b80d220e4ca9c96d50872f7649'
 
-  url "https://www.realartists.com/builds/#{version}/Ship.app.zip"
-  appcast "https://www.realartists.com/builds/#{version}/sparkle.xml",
+  url "https://www.realartists.com/builds/#{version.major_minor}/Ship.app.zip"
+  appcast "https://www.realartists.com/builds/#{version.major_minor}/sparkle.xml",
           checkpoint: 'dd8ffc08a7a2d0b1d5dc24ffdfbdd6820a8f827897e7f18cb1170267fa4fbe7a'
   name 'Ship'
   homepage 'https://www.realartists.com/'
