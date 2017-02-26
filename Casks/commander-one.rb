a0ba206a2df3a3b8828d41f90b8a93304938c0e5
Victor Popkov <victor@popkov.me>
2017-02-26 19:40:40 +0200

    Update commander-one to 1.7

--- Casks/commander-one.rb
+++ Casks/commander-one.rb
@@ -1,10 +1,10 @@
 cask 'commander-one' do
-  version '1.6'
-  sha256 'e5474e14be688ed98ec3d78c47f7fefb1c7f2b8e392cb27e5e04295573aabbe5'
+  version '1.7'
+  sha256 '8e33f2fca68b3b7e445f86ee3b4cae8c180b18f28d9a11faf7133843f81b0e0c'
 
   url 'http://mac.eltima.com/download/commander.dmg'
   appcast 'http://www.eltima.com/download/commander-update/settings.xml',
-          checkpoint: 'ad715e45624b992a2cd8c2ca6f1c5598d7527a68cffcd9b748d8360eb31ffb46'
+          checkpoint: '73f40a368b723490bad6cac5f8d02c133df810cbded3b51ff7d6dbe2eb7896e2'
   name 'Commander One'
   homepage 'http://mac.eltima.com/file-manager.html'
 
