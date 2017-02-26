0b6c9bb9c34cbc51dc5d5607a647e2d9ef1bce60
Steve Kellock <steve@kellock.ca>
2017-02-26 17:36:44 -0500

    Update reactotron to 1.8.0 (#30406)

--- Casks/reactotron.rb
+++ Casks/reactotron.rb
@@ -1,10 +1,10 @@
 cask 'reactotron' do
-  version '1.7.0'
-  sha256 '286663392d6f53f2de911380aee77c23fad04b05c285aed82d616b45f552dccd'
+  version '1.8.0'
+  sha256 '64790e7b199241b6923839709f5679c2c0a1e157f6fc85138a345facf3f90987'
 
   url "https://github.com/infinitered/reactotron/releases/download/v#{version}/Reactotron.app.zip"
   appcast 'https://github.com/infinitered/reactotron/releases.atom',
-          checkpoint: 'c2a65f49ff4345bf46eaa0cef6d59f94df72f9cd819f1bb1ddd254d9041edc70'
+          checkpoint: '5a7cf84b2695661da635e9687598a84881a18fae9a409180706932f4733170e5'
   name 'Reactotron'
   homepage 'https://github.com/infinitered/reactotron'
 
