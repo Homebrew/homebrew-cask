388a4b13aab906cf02f39a6a5472a1d9a53a0081
Andrew Wade <fincle@users.noreply.github.com>
2017-02-26 17:11:05 -0800

    Update goodsync to 10.3.3 (#30409)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.3.2'
-  sha256 '7ad18d08b6360368ba9d43d33dba47f49452411ae03a4d2d836bf50ce4aaca48'
+  version '10.3.3'
+  sha256 '655c34e60cd90ccda4a53920127ca1b31a9fc6770720275574f00e6a66b326b5'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   name 'GoodSync'
