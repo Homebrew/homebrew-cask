0d84bc9c173574436a83c2edec9a85924a2e6fb6
Sergei Kolobov <skolobov@SK-MacBook-Pro-15.local>
2017-02-28 01:20:05 +0300

    Add Emercoin-Qt.app 0.6.0

---
+++ Casks/emercoin.rb
@@ -0,0 +1,16 @@
+cask 'emercoin' do
+  version '0.6.0'
+  sha256 'c95d85ccb6bad6f60447ee882cc2d2b77751b43117ddf86ed0b9b7c00e4d95eb'
+
+  # sourceforge.net was verified as official when first introduced to the cask
+  url 'https://downloads.sourceforge.net/emercoin/emercoin-0.6.0-osx.dmg'
+  name 'Emercoin'
+  homepage 'http://emercoin.com'
+
+  app 'Emercoin-Qt.app'
+
+  preflight do
+    set_permissions "#{staged_path}/Emercoin-Qt.app", '0755'
+  end
+end
+
