ba9432f9c265271b1f0ca2ee07a177d34a1cf8f8
Maciej Matys <metyl@users.noreply.github.com>
2017-02-27 12:37:36 +0100

    Update intellij-idea to version 2016.3.5 build 163.13906.18

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,10 +1,10 @@
 cask 'intellij-idea' do
   version '2016.3.5'
-  sha256 '136e679a751c4a098b2c7195efa526f8b3b51b010a96f568c875986b8eb11a71'
+  sha256 '4417df3aa9fba7c75af371b51d734759b2d555930404b2063866b4e869ae3b79'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release',
-          checkpoint: 'a725581302012d1dba4f2f26834e14992d21db3f725ce3f4933f7d1b9d10852c'
+          checkpoint: '96c427261ad1bea66b78cead640f843bf3808acef31b4ec05d43044d21f53233'
   name 'IntelliJ IDEA Ultimate'
   homepage 'https://www.jetbrains.com/idea/'
 
