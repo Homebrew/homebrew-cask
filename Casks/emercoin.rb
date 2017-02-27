dbc1af9866b7bc347325e8660e30da321557fd2e
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-28 06:33:53 +0800

    Update emercoin.rb

--- Casks/emercoin.rb
+++ Casks/emercoin.rb
@@ -2,8 +2,8 @@ cask 'emercoin' do
   version '0.6.0'
   sha256 'c95d85ccb6bad6f60447ee882cc2d2b77751b43117ddf86ed0b9b7c00e4d95eb'
 
-  # sourceforge.net was verified as official when first introduced to the cask
-  url 'https://downloads.sourceforge.net/emercoin/emercoin-0.6.0-osx.dmg'
+  # sourceforge.net/emercoin was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/emercoin/emercoin-#{version}-osx.dmg"
   name 'Emercoin'
   homepage 'http://emercoin.com'
 
@@ -13,4 +13,3 @@ cask 'emercoin' do
     set_permissions "#{staged_path}/Emercoin-Qt.app", '0755'
   end
 end
-
