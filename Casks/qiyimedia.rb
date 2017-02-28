fa4f61fc4e08f75f42a062f2d3c4a005672e048a
nuomi1 <nuomi1@users.noreply.github.com>
2017-02-28 12:49:25 +0800

    Update qiyimedia.rb
    Update qiyimedia.rb

--- Casks/qiyimedia.rb
+++ Casks/qiyimedia.rb
@@ -1,14 +1,18 @@
 cask 'qiyimedia' do
-  version :latest
-  sha256 :no_check
+  version '4.12.17'
+  sha256 '39a9582a353472147865be23b6f4b4190f1aafa2941ebd8978b34bcafcc2e829'
 
-  url 'http://mbdapp.iqiyi.com/j/ot/iQIYIMedia_003.dmg'
+  url 'https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_003.dmg'
   name '爱奇艺视频'
   homepage 'https://www.iqiyi.com/'
+
+  depends_on macos: '>= :mountain_lion'
 
   app '爱奇艺.app'
 
   zap delete: [
-                '~/Library/Containers/com.iqiyi.yingyin',
+                '~/Library/Application Scripts/com.iqiyi.player',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.iqiyi.player.sfl',
+                '~/Library/Containers/com.iqiyi.player',
               ]
 end
