5929af0791867e0e1b8d66466ac70d39fb7a7f6a
Davanum Srinivas <davanum@gmail.com>
2017-02-28 16:36:43 -0500

    Switch minikube-darwin-amd64 download to GCS from AWS

--- Casks/minikube.rb
+++ Casks/minikube.rb
@@ -2,7 +2,8 @@ cask 'minikube' do
   version '0.16.0'
   sha256 '1de0dda591d23c01aa52f6e7b6c85bec7e4811a007a5a939eb2d1bed6fa84144'
 
-  url "https://github.com/kubernetes/minikube/releases/download/v#{version}/minikube-darwin-amd64"
+  # storage.googleapis.com/minikube was verified as official when first introduced to the cask
+  url "https://storage.googleapis.com/minikube/releases/v#{version}/minikube-darwin-amd64"
   appcast 'https://github.com/kubernetes/minikube/releases.atom',
           checkpoint: '30e9116a67824c129b5a28a458d8b2e57c01dbfb0912f8784d9d8c0d6712a250'
   name 'Minikube'
