07c5ea0676fc84077e4a9fbe5c523462c8db7e7f
Dmitry Chirkin <dmitry.chirkin@gmail.com>
2017-02-28 11:30:35 +0200

    Update robomongo to 1.0.0-rc1,496f5c2 (#30443)

--- Casks/robomongo.rb
+++ Casks/robomongo.rb
@@ -1,6 +1,6 @@
 cask 'robomongo' do
-  version '0.9.0,0786489'
-  sha256 '35862af46635e33d89d5af3164f7cf445862add199a2645b0f246102df93691c'
+  version '1.0.0-rc1,496f5c2'
+  sha256 '7b2e0ee1effb36d548945fe668c2e584c1e2dde1e3b7786f2ee134dc419ed1ef'
 
   url "https://download.robomongo.org/#{version.before_comma}/osx/robomongo-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg"
   name 'Robomongo'
