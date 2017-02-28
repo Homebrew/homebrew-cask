83d10bcf866332e421b07b59498468a3f5988f68
Victor Popkov <victor@popkov.me>
2017-02-28 23:46:06 +0200

    Update comma-chameleon to 0.5.0 (#30459)

--- Casks/comma-chameleon.rb
+++ Casks/comma-chameleon.rb
@@ -1,11 +1,11 @@
 cask 'comma-chameleon' do
-  version '0.4.8'
-  sha256 'f872ad9f95b9e303bac53636307374e591d2c61a7fd8600d7586c605065925ab'
+  version '0.5.0'
+  sha256 'b63d8a51ba8535555f28eda9ff82c9ff76afe9b4d649b3b8453a8802715220d7'
 
   # github.com/theodi/comma-chameleon was verified as official when first introduced to the cask
   url "https://github.com/theodi/comma-chameleon/releases/download/#{version}/comma-chameleon-darwin-x64.tar.gz"
   appcast 'https://github.com/theodi/comma-chameleon/releases.atom',
-          checkpoint: '6ec4b53671ef606c5cf28756cac4199a83c41e4d985b6b04bbbe01a1a7db922a'
+          checkpoint: '6db5403267c51a690ce811cc015391a3278eb8c8a062c537eb29a805a22a4c18'
   name 'Comma Chameleon'
   homepage 'https://comma-chameleon.io/'
 
