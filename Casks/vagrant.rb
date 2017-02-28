5b55280469dd5f65a6fcd5f428a71cafec0865ff
Christoph Blecker <admin@toph.ca>
2017-02-28 12:55:48 -0800

    Update vagrant to 1.9.2

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,11 +1,11 @@
 cask 'vagrant' do
-  version '1.9.1'
-  sha256 '17191ff7a1d796aa89c558b45c706288275dade6a65e2b219c2efa0e54c02ef7'
+  version '1.9.2'
+  sha256 'ee0549a16859042d39c08ae153895da6cdfbad7c587b3438349cdf01efe10703'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}.dmg"
   appcast 'https://github.com/mitchellh/vagrant/releases.atom',
-          checkpoint: 'd3d0452d55c76d71c91b71ff2026b16e6acb61b7662adf9f36c284bcc50d5726'
+          checkpoint: 'b66330f0e370b55df9091a59231978e239d897a65b4f0cebcecc5b4303cc622d'
   name 'Vagrant'
   homepage 'https://www.vagrantup.com/'
 
