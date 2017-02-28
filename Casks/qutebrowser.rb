56fa7696a3714ee611473076133fd8a15150cf62
André de Jager <andrethehunter@gmail.com>
2017-02-28 20:31:15 +1000

    Fix styles
    Fixes from `brew cask audit` and `brew cask style`

--- Casks/qutebrowser.rb
+++ Casks/qutebrowser.rb
@@ -1,6 +1,8 @@
 cask 'qutebrowser' do
   version '0.10.0'
   sha256 'ba0202df8af6905d5b823bafdc04415853f897c06cdbbb6e40af9ed5e602c1d2'
+
+  # github.com/qutebrowser/qutebrowser was verified as official when first introduced to the cask
   url "https://github.com/qutebrowser/qutebrowser/releases/download/v#{version}/qutebrowser-#{version}.dmg"
   appcast 'https://github.com/qutebrowser/qutebrowser/releases.atom',
           checkpoint: 'ee0a69fe4e86f27d0d7127cd7492c01f98ad1120d9fa0bdaa1791937199aba41'
