be9436980e73f34f0014b2c7d6fc8b72ba07a4b5
Alissa Shim <alissa@Alissas-MacBook-Pro.local>
2017-03-01 15:17:55 -0500

    Update cyberduck to 5.3.8.23611

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -1,10 +1,10 @@
 cask 'cyberduck' do
-  version '5.3.7.23440'
-  sha256 'd4893a669d413ff691167f0afdc73d6fe736753ace7c6990eda9d5ae00294d31'
+  version '5.3.8.23611'
+  sha256 '61242083d9a5c0116cccf87ef15ff919e7d93265701e8859beb049a56bf0ebee'
 
   url "https://update.cyberduck.io/Cyberduck-#{version}.zip"
   appcast 'https://version.cyberduck.io/changelog.rss',
-          checkpoint: '8df61d456c80d69a15188136d45bda41b0b5f4cd05077131868204bdba1ed46c'
+          checkpoint: '06933ed8443c84a7f47c5a243052cc902f07b7bae2553053a9da44780882bbb0'
   name 'Cyberduck'
   homepage 'https://cyberduck.io/'
 
