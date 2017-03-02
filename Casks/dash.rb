4dc114643cabaf3a590038418ebef4001c7cecf2
Artem Medvedev <amedvedev@outlook.com>
2017-03-02 20:07:59 +0200

    Update dash to 4.0.2 (#30511)

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -1,10 +1,10 @@
 cask 'dash' do
-  version '4.0.1'
-  sha256 '2ef4d0721a89feb680bb416fd7694cbec5441df442b64aabb8b7abbaa5c9b3f3'
+  version '4.0.2'
+  sha256 '8d4fdff79b808f5ddb4020e13d58f5c21c590c07a4a74c48ead477a17cb910c7'
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
   appcast "https://kapeli.com/Dash#{version.major}.xml",
-          checkpoint: 'ed127f51a54c198287e90ea386f6b746c3dc2b78bc8d061c5b52d6e9919588e4'
+          checkpoint: '3b5484d105f086b86f594ffc0793c9e7dec33737d13bb9d1c9c95f50002902a5'
   name 'Dash'
   homepage 'https://kapeli.com/dash'
 
