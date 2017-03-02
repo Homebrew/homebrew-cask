15a58b228ccc9a2290ffbb749aca7423a154bcf9
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-02 08:37:18 +0800

    Update tableau.rb

--- Casks/tableau.rb
+++ Casks/tableau.rb
@@ -3,7 +3,7 @@ cask 'tableau' do
   sha256 'ef8aa2045b5b1ca979daf6cefab79f34fa35d3bc07931e9b5699cd6a72f10986'
 
   url "https://downloads.tableau.com/tssoftware/TableauDesktop-#{version.dots_to_hyphens}.dmg"
-  name 'Tableau'
+  name 'Tableau Desktop'
   homepage 'https://public.tableau.com/s/'
 
   depends_on macos: '>= :yosemite'
