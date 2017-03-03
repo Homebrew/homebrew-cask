1f8845bfb10d49e5a041345e27ed47ffb8f22ef2
Sean Houghton <sean.houghton@gmail.com>
2017-03-02 18:39:23 -0800

    Fix style issues

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -2,7 +2,8 @@ cask 'kstars' do
   version ':latest'
   sha256 '514b1b3ac52fca4b4623e2feb98f2429683dd2e6bf883956674e7033abd6bfbc'
 
-  url "http://www.indilib.org/jdownloads/kstars/kstars-latest.dmg"
+  # indilib.org was verified as official when first introduced to the cask
+  url 'http://www.indilib.org/jdownloads/kstars/kstars-latest.dmg'
   name 'KStars'
   homepage 'https://edu.kde.org/kstars/'
 
