8e967455a177e0e6ddeeb3ec9bb4fd050160719a
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-03 10:47:10 +0800

    Update kstars.rb

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,8 +1,8 @@
 cask 'kstars' do
-  version ':latest'
-  sha256 '514b1b3ac52fca4b4623e2feb98f2429683dd2e6bf883956674e7033abd6bfbc'
+  version :latest
+  sha256 :no_check
 
-  # indilib.org was verified as official when first introduced to the cask
+  # indilib.org/jdownloads/kstars was verified as official when first introduced to the cask
   url 'http://www.indilib.org/jdownloads/kstars/kstars-latest.dmg'
   name 'KStars'
   homepage 'https://edu.kde.org/kstars/'
