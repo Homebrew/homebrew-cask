05b38887ea583ca3ebe95882e3c09afc7748dd4d
Mike Hodges <rmikehodges@gmail.com>
2017-03-03 13:40:58 -0500

    Update bloodhound to 1.2.1 (#30540)

--- Casks/bloodhound.rb
+++ Casks/bloodhound.rb
@@ -1,10 +1,10 @@
 cask 'bloodhound' do
   version '1.2.1'
-  sha256 'dadf9b6c692693c628e8b51cf7d0657b14ef2e669daede5ceb6144484edc0f5e'
+  sha256 '63abbfb483811a004171a8680402e613ca6129c7bd152d67321e65d90b70810b'
 
   url "https://github.com/BloodHoundAD/BloodHound/releases/download/#{version}/BloodHound-darwin-x64.zip"
   appcast 'https://github.com/BloodHoundAD/BloodHound/releases.atom',
-          checkpoint: '2356a4e6a8274454e11cd014ce98cf9ef4365ce3348f1b7c16f7b39258e4cbc2'
+          checkpoint: '7d191f2c56d164380082cbbbfa75908947fd45bb943ee405d50ed9194eece955'
   name 'bloodhound'
   homepage 'https://github.com/BloodHoundAD/BloodHound'
 
