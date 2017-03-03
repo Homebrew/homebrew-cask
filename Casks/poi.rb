c8f0e6fc149b05063dd6134fc55b3ef1593d7fe3
darg20127 <darg20127@gmail.com>
2017-03-03 16:16:00 +0800

    Update poi to v7.4.0 (#30530)
    * Update poi.app to v7.4.0
    
    - update poi.app to v7.4.0
    - add zap
    
    * Update poi.rb

--- Casks/poi.rb
+++ Casks/poi.rb
@@ -1,13 +1,15 @@
 cask 'poi' do
-  version '7.3.0'
-  sha256 '10a0150600c9bb95021c0e198057d6764bed796fc5b894c9ee4d4b0752fff6ab'
+  version '7.4.0'
+  sha256 '9033f7a00e439a1f4873dc9a949465a0bfb1bff559bddabe95271653e72cee1e'
 
   # github.com/poooi/poi was verified as official when first introduced to the cask
   url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}-macos-x64.dmg"
   appcast 'https://github.com/poooi/poi/releases.atom',
-          checkpoint: 'a7cfdeecffb231551946cdb08d50db8a84610d10321c81b3218c829cb90625fb'
+          checkpoint: '0c0d51a9d63e23eaa67dbf98dbbe7f131083aa0b8fe47330510fe381ed2d23f4'
   name 'poi'
   homepage 'https://poi.io/'
 
   app 'poi.app'
+
+  zap delete: '~/Library/Application Support/poi/'
 end
