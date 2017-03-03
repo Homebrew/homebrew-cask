bfb992fa25a401c799d3bf9cddf4c168c9bf2889
Maxwell Bernstein <tekknolagi@gmail.com>
2017-03-02 20:17:39 -0800

    fman 0.3.0

---
+++ Casks/fman.rb
@@ -0,0 +1,9 @@
+cask 'fman' do
+    version '0.3.0'
+    sha256 '9abd41e8032f4746aa058cfb7b5a8b6ad70b333da64277277624165182192594'
+
+    url 'http://download.fman.io/fman.dmg'
+    name 'fman'
+    homepage 'https://fman.io/'
+    app 'fman.app'
+end
