4ebddaaca3f25b9827ee2c7251bba6bb243ff261
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-03 14:45:58 +0800

    Update fman.rb

--- Casks/fman.rb
+++ Casks/fman.rb
@@ -1,9 +1,10 @@
 cask 'fman' do
-    version '0.3.0'
-    sha256 '9abd41e8032f4746aa058cfb7b5a8b6ad70b333da64277277624165182192594'
+  version :latest
+  sha256 :no_check
 
-    url 'http://download.fman.io/fman.dmg'
-    name 'fman'
-    homepage 'https://fman.io/'
-    app 'fman.app'
+  url 'http://download.fman.io/fman.dmg'
+  name 'fman'
+  homepage 'https://fman.io/'
+
+  app 'fman.app'
 end
