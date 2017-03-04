4f74313d1188ba2d6ba65b3cf06a7a8901b63b0b
Shane Liesegang <shane@techie.net>
2017-03-04 13:58:49 -0800

    Update unity-webgl-support-for-editor to 5.5.2f1,3829d7f588f3

--- Casks/unity-webgl-support-for-editor.rb
+++ Casks/unity-webgl-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-webgl-support-for-editor' do
-  version '5.5.0f3,38b4efef76f0'
-  sha256 '17532ebcf73d74dec9b1403e6649697dfd39aafac87806a601b70bf713a7a270'
+  version '5.5.2f1,3829d7f588f3'
+  sha256 '8b4dbca202a16270e614e2425d62d3fbe2b36c1fc46604dfdb25f3078a3f7590'
 
   url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity WebGL Build Support'
