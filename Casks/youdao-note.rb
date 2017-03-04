64c1ae03bcbc127dceb91a1323a44e66edbbf3ab
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-04 23:17:27 +0800

    Update youdao-note.rb

--- Casks/youdao-note.rb
+++ Casks/youdao-note.rb
@@ -1,15 +1,15 @@
 cask 'youdao-note' do
   version '2.5.1'
-  sha256 :no_check
+  sha256 '1ea5f4ebaacf658f712d1e2cd386c6eb90c8e4a82f577b9adbc44692b020a755'
 
-  # download.ydstatic.com was verified as official when first introduced to the cask
+  # download.ydstatic.com/notewebsite/downloads was verified as official when first introduced to the cask
   url 'http://download.ydstatic.com/notewebsite/downloads/YoudaoNote.dmg'
   name '有道云笔记'
   homepage 'https://note.youdao.com/index.html'
 
   depends_on macos: '>= :yosemite'
 
-  app 'YoudaoNote.app'
+  app 'Youdao Note.app'
 
   zap delete: [
                 '~/Library/Caches/com.crashlytics.data/com.youdao.YoudaoDict',
