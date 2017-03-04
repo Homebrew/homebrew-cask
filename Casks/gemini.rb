11d5ca83cbf1fc0f4e080ee4e9d68735f1822e90
Niko Felger <niko.felger@gmail.com>
2017-03-04 05:21:16 +0100

    Update gemini to 2.3.5 (#30549)

--- Casks/gemini.rb
+++ Casks/gemini.rb
@@ -1,11 +1,11 @@
 cask 'gemini' do
-  version '2.3.1'
-  sha256 'a55b7a90eeac893ce5b799e2182dd80713c9633742a326d1ab94e2d1be294d61'
+  version '2.3.5'
+  sha256 'f7efede4b8f358a73db0fa78c8b319381432b69517eebdca9072d1e2b893b83f'
 
   # dl.devmate.com/com.macpaw.site.Gemini was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.macpaw.site.Gemini#{version.major}/Gemini#{version.major}.dmg"
   appcast "https://updates.devmate.com/com.macpaw.site.Gemini#{version.major}.xml",
-          checkpoint: '8c05d7bdcc1807a2d20cbf508b64109c54c3ca35e7d19424b1dca2030ac012bb'
+          checkpoint: 'dd9ed864b5e7863293d8145f172707d55a8941750fa62847efb5fc2065146e0f'
   name 'Gemini'
   homepage 'https://macpaw.com/gemini'
 
