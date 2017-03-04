585cf77e0dbcb48f132ba2c3b3d383a0101bd4e7
Cihan Erkut <cihan.erkut@embl.de>
2017-03-04 16:44:26 +0100

    Update findings to 1.4.3.3551 (#30573)

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -1,5 +1,5 @@
 cask 'findings' do
-  version '1.4.4.3564'
+  version '1.4.3.3551'
   sha256 '55d94404139b7c0c3e303d208aba47b4f20bffcfef4cdcfc4535c55fbe2e021c'
 
   url "http://downloads.findingsapp.com/Findings_#{version.after_comma}_#{version.before_comma}.zip"
