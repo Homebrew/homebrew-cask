8156ef2169cc5e4d4cf6158e2830bd480d1a01d5
Gobinath Mallaiyan <i@gobinath.com>
2017-03-04 09:01:51 -0500

    Adding Scout-App.app V.2.12.12 - Correct URL Stanza
    - Updated URL Stanza to use version number instead of hardcoded number

--- Casks/scout.rb
+++ Casks/scout.rb
@@ -3,7 +3,7 @@ cask 'scout' do
   sha256 '33a59da631d45fe4abc4b2525cf8148c722eb3d87787a94c0eeee7d81be9b4b7'
 
   # github.com/scout-app/scout-app was verified as official when first introduced to the cask
-  url 'https://github.com/scout-app/scout-app/releases/download/v2.12.12/OSX_Scout-App_2.12.12.zip'
+  url "https://github.com/scout-app/scout-app/releases/download/v#{version}/OSX_Scout-App_#{version}.zip"
   appcast 'https://github.com/scout-app/scout-app/releases.atom',
           checkpoint: '75ad564c48c266066eab01ef7f06180e065bfd9ac0eb92261579bb0d9cd7b833'
   name 'Scout-App'
