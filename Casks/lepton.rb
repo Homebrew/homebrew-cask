be4a0329243ba71488a7d92ba67ce6a6c3cc8821
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-05 00:52:09 +0800

    Update and rename lepton-app.rb to lepton.rb

--- Casks/lepton.rb
+++ Casks/lepton.rb
@@ -1,4 +1,4 @@
-cask 'lepton-app' do
+cask 'lepton' do
   version '1.2.0'
   sha256 '93609204d33dcb284693886ef406cebf3ddd6fdb2c7dbd9320d38e4e59b98902'
 
@@ -6,7 +6,7 @@ cask 'lepton-app' do
   url "https://github.com/hackjutsu/Lepton/releases/download/v#{version}/Lepton-#{version}-mac.zip"
   appcast 'https://github.com/hackjutsu/Lepton/releases.atom',
           checkpoint: '8452f00ddc67c72cf97a555020f93b9e8bbe4ae73701b6b6cfa5dc93e533b2ac'
-  name 'lepton-app'
+  name 'Lepton'
   homepage 'http://hackjutsu.com/Lepton/'
 
   app 'Lepton.app'
