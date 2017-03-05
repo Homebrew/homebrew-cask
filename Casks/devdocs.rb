b59f829536f78145ac55d1cb0f00984b075a6215
Victor Popkov <victor@popkov.me>
2017-03-05 20:30:27 +0200

    Update devdocs to 0.2.1

--- Casks/devdocs.rb
+++ Casks/devdocs.rb
@@ -1,10 +1,10 @@
 cask 'devdocs' do
-  version '0.1.0'
-  sha256 'ede46d788775fbac7147dc45589f2de27762273962e4219730555896e00c576a'
+  version '0.2.1'
+  sha256 '554eb5cbe0051d10bb116dee406e7a093e7cb6ed386574217e82749fcda31bd5'
 
   url "https://github.com/egoist/devdocs-app/releases/download/v#{version}/DevDocs-#{version}.dmg"
   appcast 'https://github.com/egoist/devdocs-app/releases.atom',
-          checkpoint: 'c8b1dff1f79f31423f1cf14bd37c25f06e594afb97c340e98b3e218089cd8cc6'
+          checkpoint: '1c157ca1e9184b137f1f5d406b0f4841c0995389ef3a97f37b3fd0aef69a4aaf'
   name 'DevDocs App'
   homepage 'https://github.com/egoist/devdocs-app'
 
