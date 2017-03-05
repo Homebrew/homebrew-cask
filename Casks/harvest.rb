65353abede1f9c0e6e92c909b6b3ceff25858c64
Victor Popkov <victor@popkov.me>
2017-03-05 20:39:15 +0200

    Update harvest to 2.0.5

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -1,10 +1,10 @@
 cask 'harvest' do
-  version '2.0.4'
-  sha256 'fba658d0232ffddd0f961e461d6f8103acf74fffe981e5618439a7ac07010ae1'
+  version '2.0.5'
+  sha256 'b4a307f22d7f5fa4031cc1a8b1ed0d92f736837ccb99d429383932e00ce4a2f9'
 
   url "https://www.getharvest.com/harvest/mac/Harvest.#{version}.zip"
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml',
-          checkpoint: 'e1251a07581c4ead95b31d21d1c2c95426762392e1c2656aec4d61629c09c770'
+          checkpoint: '67f5102e0e5c50a03e51dfac1e829b1a839302f76bf4e65c2014b3460697ffbd'
   name 'Harvest'
   homepage 'https://www.getharvest.com/mac-time-tracking'
 
