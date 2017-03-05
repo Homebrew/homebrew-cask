e478387edfb8564b84fbc438e7604afbaf256166
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-06 00:22:39 +0800

    Update docker.rb

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -1,8 +1,10 @@
 cask 'docker' do
-  version :latest
-  sha256 :no_check
+  version '17.03.0'
+  sha256 '17a05db62267b175986536594b1a4ea0a393690031d0ae688f6db73e2399d30a'
 
   url 'https://download.docker.com/mac/stable/Docker.dmg'
+  appcast 'https://github.com/docker/docker/releases.atom',
+          checkpoint: '6a5a4e061449ba35f3a14a7f87c60826d35c2db6a93d864f966eb76420d0c0a8'
   name 'Docker Community Edition'
   name 'Docker CE'
   homepage 'https://www.docker.com/community-edition'
