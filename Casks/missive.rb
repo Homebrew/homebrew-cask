1aa3d4a63e84f2d8037832d11617c9dab0c7d240
Gobinath Mallaiyan <i@gobinath.com>
2017-03-06 11:11:03 -0500

    Adding Missive.App v3.5.0
    - updating version & sha256 as suggested in PR

--- Casks/missive.rb
+++ Casks/missive.rb
@@ -1,6 +1,6 @@
 cask 'missive' do
-  version '3.5.0'
-  sha256 '46f849271fe88e965399e361043d8969b26901577bc55ba73db7dad9c718572e'
+  version :latest
+  sha256 :no_check
 
   url 'https://download.missiveapp.com/download/osx'
   name 'missive'
