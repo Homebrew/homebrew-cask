aef4874f35dba0cac3d4c719dfd6793868d6067e
Ugo Arangino <ugo@arangino.de>
2017-03-06 11:24:11 +0100

    GitUp v1.0.6 (#30630)
    * Update gitup.rb
    
    Update the sha256 checksums for the latest GitUp release.
    
    * Update gitup.rb
    
    Update appcast checkpoint for the latest GitUp release.
    
    * Update gitup.rb
    
    Update version for the latest GitUp release.

--- Casks/gitup.rb
+++ Casks/gitup.rb
@@ -1,11 +1,11 @@
 cask 'gitup' do
-  version '1.0.6'
-  sha256 '420127192b63adfbd6b907c17a4934ffacffe856409dfb53b0696346cfb091d2'
+  version '1.0.7'
+  sha256 'b5bfe8c1802b1c7f72359214e58b2c768c5b7a573d6579a3101f8932b81d308f'
 
   # s3-us-west-2.amazonaws.com/gitup-builds was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/gitup-builds/stable/GitUp.zip'
   appcast 'https://github.com/git-up/GitUp/releases.atom',
-          checkpoint: '094085cc569cb42d40e12536f817c88934fd04cdea8cd1ad275f88d418b9298e'
+          checkpoint: 'e28488300af69d4eb5c2eb24d2d3e4e31a9fc5606b27e5a4c936216a7cc1022a'
   name 'GitUp'
   homepage 'http://gitup.co/'
 
