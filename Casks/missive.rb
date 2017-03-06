61c9417a0a656e97f9a0a695820c7baa0e7e5b5f
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-06 18:16:49 -0500

    Adding Missive.App v3.5.0 (#30625)
    * Adding Missive.App v3.5.0
    
    - Including new cask for Missive App ~ Meet the one app for team email and chat :speech_balloon: :incoming_envelope:
    
    * Adding Missive.App v3.5.0
    
    - updating version & sha256 as suggested in PR

---
+++ Casks/missive.rb
@@ -0,0 +1,10 @@
+cask 'missive' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://download.missiveapp.com/download/osx'
+  name 'missive'
+  homepage 'https://missiveapp.com'
+
+  app 'Missive.app'
+end
