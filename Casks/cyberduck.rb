8152c7a7f905611b3bd233ad58d9020003c83d8c
Michael Ledin <m.ledin@appheads.ru>
2017-03-06 10:32:56 +0300

    Update cyberduck to 5.3.9.23624

--- Casks/cyberduck.rb
+++ Casks/cyberduck.rb
@@ -1,10 +1,10 @@
 cask 'cyberduck' do
-  version '5.3.7.23440'
-  sha256 'd4893a669d413ff691167f0afdc73d6fe736753ace7c6990eda9d5ae00294d31'
+  version '5.3.9.23624'
+  sha256 'de2e7888c9e96c82996ac932e7a0075f86db1c5bda33cb626204e774d553e7e1'
 
   url "https://update.cyberduck.io/Cyberduck-#{version}.zip"
   appcast 'https://version.cyberduck.io/changelog.rss',
-          checkpoint: '8df61d456c80d69a15188136d45bda41b0b5f4cd05077131868204bdba1ed46c'
+          checkpoint: '06933ed8443c84a7f47c5a243052cc902f07b7bae2553053a9da44780882bbb0'
   name 'Cyberduck'
   homepage 'https://cyberduck.io/'
 
