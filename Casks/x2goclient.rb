2725d6e4d912e4a6202e544be0b60feba9914926
Vítor Galvão <info@vitorgalvao.com>
2017-03-06 13:35:23 +0000

    Update x2goclient.rb

--- Casks/x2goclient.rb
+++ Casks/x2goclient.rb
@@ -1,7 +1,7 @@
 cask 'x2goclient' do
   version '4.1.0.0'
 
-  if MacOS.version < '10.9'
+  if MacOS.version <= :mountain_lion
     sha256 '8fbb806167461af5cfc15b5662a6a6ab5a4459c7f3c8efc61087ade80f919a4c'
     url "https://code.x2go.org/releases/binary-macosx/x2goclient/releases/#{version}/x2goclient-#{version}-20170222_OSX_10_6.dmg"
   else
