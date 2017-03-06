a00522fe36fc874708c3b6fd1ee1afbee8f8dc9d
Gobinath Mallaiyan <i@gobinath.com>
2017-03-06 01:45:17 -0500

    Adding Catlight.app v2.4.2
    - Including a New Cask for Catlight ~ Status Notifier for Developers :construction:

---
+++ Casks/catlight.rb
@@ -0,0 +1,10 @@
+cask 'catlight' do
+  version '2.4.2'
+  sha256 'c9f561f54a0df5d329152cd69290b5b9afb56d121c3e29177efea53b57d3f843'
+
+  url 'https://catlight.io/downloads/mac/beta'
+  name 'catlight'
+  homepage 'https://catlight.io/'
+
+  app 'Catlight.app'
+end
