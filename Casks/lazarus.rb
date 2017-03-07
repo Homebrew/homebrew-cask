d98920194cdc48e791dce0c55893a3a4406d8688
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-07 11:31:12 +0800

    Update lazarus.rb

--- Casks/lazarus.rb
+++ Casks/lazarus.rb
@@ -3,7 +3,7 @@ cask 'lazarus' do
   sha256 '342c73a2481fb37b75ba95ef052b4a3bb734e19fd7fe7a2556f53b42e67f243c'
 
   # sourceforge.net/lazarus was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/lazarus/lazarus-#{version}-i686-macosx.dmg"
+  url "https://downloads.sourceforge.net/lazarus/lazarus-#{version}-i386-macosx.dmg"
   appcast 'https://sourceforge.net/projects/lazarus/rss',
           checkpoint: 'ce6a7706e0ea9631ad9bc2e4a3d8d712459c79ea239fee34b7aace8cbe5ecd94'
   name 'Lazarus'
