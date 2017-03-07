732c6adf6827ee6d5d57362b3b4436f504f98b62
Gobinath Mallaiyan <i@gobinath.com>
2017-03-06 19:06:47 -0500

    Update google-adwords-editor to 11.7
    - Corrected Version Number & sha256

--- Casks/google-adwords-editor.rb
+++ Casks/google-adwords-editor.rb
@@ -1,6 +1,6 @@
 cask 'google-adwords-editor' do
-  version '11.7'
-  sha256 'a87499d20ace9af9e47de5a91b6c34ea3b8c8918c2d691ad6f417f9952eecda9'
+  version :latest
+  sha256 :no_check
 
   url 'https://dl.google.com/adwords_editor/Google_AdWords_Editor.dmg'
   name 'Google AdWords Editor'
