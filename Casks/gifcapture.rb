7833cace4d2e1e808f831f75ea57c80ef5386c00
Vítor Galvão <info@vitorgalvao.com>
2017-03-07 22:41:32 +0000

    Update gifcapture.rb

--- Casks/gifcapture.rb
+++ Casks/gifcapture.rb
@@ -1,8 +1,8 @@
-cask 'GifCapture' do
-  version :latest
+cask 'gifcapture' do
+  version '1.0.3'
   sha256 :no_check
 
-  url 'https://github.com/onmyway133/GifCapture/releases/download/1.0.3/GifCapture.zip'
+  url "https://github.com/onmyway133/GifCapture/releases/download/#{version}/GifCapture.zip"
   name 'GifCapture'
   homepage 'https://github.com/onmyway133/GifCapture'
 
