207c8efc347f43d29ceecc3bf9fac12050534893
Khoa Pham <onmyway133@gmail.com>
2017-03-07 22:37:20 +0100

    Create gifcapture.rb

---
+++ Casks/gifcapture.rb
@@ -0,0 +1,10 @@
+cask 'GifCapture' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/onmyway133/GifCapture/releases/download/1.0.3/GifCapture.zip'
+  name 'GifCapture'
+  homepage 'https://github.com/onmyway133/GifCapture'
+
+  app 'GifCapture.app'
+end
