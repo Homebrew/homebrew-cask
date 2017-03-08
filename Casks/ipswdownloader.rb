4c42aaf70ef862f3be4e43c135952a3a7464c7c0
Gobinath Mallaiyan <i@gobinath.com>
2017-03-08 10:10:22 -0500

    Update ipswdownloader to 2.6.6
    - Remove Support for Mac version lower than snow leopard since ipswdownlader is no more available for download.

--- Casks/ipswdownloader.rb
+++ Casks/ipswdownloader.rb
@@ -1,14 +1,8 @@
 cask 'ipswdownloader' do
-  if MacOS.version <= :snow_leopard
-    version '1.6.0'
-    sha256 '82a236f69d2b7acfa9b0c801b9df76372005d5aab3f399f0d10db71320bb0af3'
-    url 'http://www.igrsoft.com/wp-content/plugins/download-monitor/download.php?id=48'
-  else
-    version '2.6.6'
-    sha256 '00b6901c0e3d18f98a8dace757a56ad248914a9f7aaab24902e631c8d9f9259d'
-    url "http://downloads.igrsoft.com/downloads/ipswDownloader/ipswDownloader_#{version.no_dots}.zip"
-  end
+  version '2.6.6'
+  sha256 '00b6901c0e3d18f98a8dace757a56ad248914a9f7aaab24902e631c8d9f9259d'
 
+  url "http://downloads.igrsoft.com/downloads/ipswDownloader/ipswDownloader_#{version.no_dots}.zip"
   name 'ipswDownloader'
   homepage 'https://igrsoft.com/ipswdownloader/'
 
