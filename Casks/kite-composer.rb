a3df7374aaa80f05d6774cd3926110ce56b1d348
Bert Devriese <bert@builtinbruges.com>
2017-03-09 20:13:46 +0100

    Add Kite.app (Kite Composer) v1.0

---
+++ Casks/kite-composer.rb
@@ -0,0 +1,10 @@
+cask 'kite-composer' do
+  version '1.0'
+  sha256 '2154c0278276e8828ca44b4d3185ede4e8ebb7317f23d5a9060f774c6cb7f2a6'
+
+  url 'https://kiteapp.co/downloads/KiteCompositor.dmg'
+  name 'Kite-Composer'
+  homepage 'https://kiteapp.co'
+
+  app 'Kite.app'
+end
