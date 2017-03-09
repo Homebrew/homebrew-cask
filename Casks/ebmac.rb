1716f9207b2dea8ba40a275f95c945c24469e12a
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-09 04:40:31 -0500

    Update ebmac to 1.39 (#30739)

--- Casks/ebmac.rb
+++ Casks/ebmac.rb
@@ -1,9 +1,9 @@
 cask 'ebmac' do
-  version '1.37'
-  sha256 '120b9e1e922d32caf60424c1561b3eb76d7eb8c9e82ad9cd976a53496e514bb7'
+  version '1.39'
+  sha256 'cf74bf3ee5a91f77d7bac0f3e140535f35180ac6a820bd102af96409f71d6e60'
 
   # ftp.vector.co.jp was verified as official when first introduced to the cask
-  url "http://ftp.vector.co.jp/67/18/938/EBMac#{version}.dmg"
+  url "http://ftp.vector.co.jp/68/53/938/EBMac#{version}.dmg"
   name 'EBMac'
   homepage 'http://ebstudio.info/manual/EBMac/'
 
