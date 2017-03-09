5ed27ae6ec37833beb0608f3a424a613695afefe
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-09 05:45:15 -0500

    Update ultimate to 3.0.9.222 (#30748)

--- Casks/ultimate.rb
+++ Casks/ultimate.rb
@@ -1,6 +1,6 @@
 cask 'ultimate' do
-  version '3.0.8.1129'
-  sha256 'a1f57a8a593f3d8b2502526d08d69a19431c60ee9923e9a3029e9556884dd943'
+  version '3.0.9.222'
+  sha256 '0cf3ec41852aa7578c4d673357aeea01ac531e4f8a867041cc7cf63dab23607a'
 
   url 'http://download.epubor.com/epubor_ultimate.zip'
   name 'Ultimate Converter'
