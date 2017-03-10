c728c334814541780bfb4f1388c830a06f1e88b3
Marius Karnauskas <marius@karnauskas.lt>
2017-03-10 23:41:24 +0000

    Bump LiteIDE to v31

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,9 +1,10 @@
 cask 'liteide' do
-  version '30.3'
-  sha256 'f88dc0a924e3e9ce55c771da97c559f5ba3b61af35b8cbba7bed5bfdd30426c4'
+  version '31'
+  sha256 '910a6d5c6a57b5681a1737e5bbb870628fc3dfb204d723576bd6196e6e74fdcd'
 
-  # sourceforge.net/liteide was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/liteide/X#{version}/liteidex#{version}.macosx-qt5.zip"
+  url 'https://github.com/visualfc/liteide/releases/download/'\
+    "x#{version}/liteidex#{version}.macosx-qt5.zip"
+
   appcast 'https://sourceforge.net/projects/liteide/rss',
           checkpoint: '13e80964ddf7a8deb2c0a657cb0fe0efaa47cc53799fd3cce506015d66f0f667'
   name 'LiteIDE'
