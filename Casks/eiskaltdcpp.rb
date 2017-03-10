c39a5f6a2316556b37e90a1c852c7ea88dcad293
Gobinath Mallaiyan <i@gobinath.com>
2017-03-10 17:16:40 -0500

    Update eiskaltdcpp to 2.3.0-v2.2.10-62-ge42c04e

--- Casks/eiskaltdcpp.rb
+++ Casks/eiskaltdcpp.rb
@@ -1,10 +1,10 @@
 cask 'eiskaltdcpp' do
   version '2.3.0-v2.2.10-62-ge42c04e'
-  sha256 '1463fd447ffd16d9f3af96b0d2306a728d580e59bc7b9a45fcbe895c99c5432c'
+  sha256 '59916ff385e2ee440a302740622f6d17f0fd006e446469c473cd09fcded794d5'
 
   url "https://downloads.sourceforge.net/eiskaltdcpp/EiskaltDC++-#{version}-x86_64-qt5.dmg"
   appcast 'https://sourceforge.net/projects/eiskaltdcpp/rss',
-          checkpoint: 'b4fb28f10cc3fbd688aed28a882b31447f7deac8ce29a5300090b71a0e45b63c'
+          checkpoint: '70a68d8316c37dbd3ed23be4aa4319d9d876a190d0206f037c629cd77bf88485'
   name 'EiskaltDC++'
   homepage 'https://sourceforge.net/projects/eiskaltdcpp/'
 
