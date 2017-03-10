5aeddc4ed4e4aa77524c6da84bfa64ca619a0263
Gobinath Mallaiyan <i@gobinath.com>
2017-03-09 20:50:23 -0500

    Adding paymoplus app v2.0.3
    - Including new cask for paymoplus app.

---
+++ Casks/paymoplus.rb
@@ -0,0 +1,10 @@
+cask 'paymoplus' do
+  version '2.0.3'
+  sha256 '9acddfbf87cc6f9209e81b359131b6575e09ad24bb6e3f4afafda84cc8015683'
+
+  url 'https://app.paymoapp.com/desktop-apps/paymoplus/PaymoPlus_latest.dmg'
+  name 'PaymoPlus'
+  homepage 'https://www.paymoapp.com'
+
+  app 'PaymoPlus.app'
+end
