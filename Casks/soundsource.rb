3ed82858b46eaf376ca2fab2636da053c215d05b
Stephen Wade <stephen@stephenwade.me>
2017-03-09 22:45:15 -0500

    Update soundsource to 3.0.2 (#30778)

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,10 +1,10 @@
 cask 'soundsource' do
-  version '3.0.1'
-  sha256 '4d26415d718e7be8f8e0233be281b55e636330b6c581ea345796d8255470766d'
+  version '3.0.2'
+  sha256 '04cf3d83102da384fad41ae0673250cb36ee4c15cdf6690b98dbf2cd88c2e84c'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
   appcast 'https://rogueamoeba.com/soundsource/releasenotes.php',
-          checkpoint: '73af7680362997b2d1cf34422bda1e2adf8301fb08279fd5193bfdbec288e95f'
+          checkpoint: '998b7b81e3a83bd741263661a535f6de541ed58a6020c76b69d250f084d2fc20'
   name 'SoundSource'
   homepage 'https://rogueamoeba.com/soundsource/'
 
