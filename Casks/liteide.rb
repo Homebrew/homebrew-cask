1cbb522bf7c29fd7002657c8d9a912118090267e
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-11 08:53:22 +0800

    Update liteide.rb

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,12 +1,10 @@
 cask 'liteide' do
-  version '31'
+  version '31.1'
   sha256 '910a6d5c6a57b5681a1737e5bbb870628fc3dfb204d723576bd6196e6e74fdcd'
 
-  url 'https://github.com/visualfc/liteide/releases/download/'\
-    "x#{version}/liteidex#{version}.macosx-qt5.zip"
-
+  url "https://github.com/visualfc/liteide/releases/download/x#{version.major}/liteidex#{version}.macosx-qt5.zip"
   appcast 'https://sourceforge.net/projects/liteide/rss',
-          checkpoint: '13e80964ddf7a8deb2c0a657cb0fe0efaa47cc53799fd3cce506015d66f0f667'
+          checkpoint: '7c0cae7a9bf833cb7cdee027d93232d17637a85cd0c2ec4aeec3fa998c315caa'
   name 'LiteIDE'
   homepage 'https://github.com/visualfc/liteide'
 
