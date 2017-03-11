02cfe5fba339eb36e69b1db020346cf814ea4ca7
Tim Sheridan <tghs@tghs.net>
2017-03-11 23:01:27 +0200

    affinic-debugger-gui: Fix app bundle name (#30821)

--- Casks/affinic-debugger.rb
+++ Casks/affinic-debugger.rb
@@ -1,11 +1,10 @@
-cask 'affinic-debugger-gui' do
+cask 'affinic-debugger' do
   version :latest
   sha256 :no_check
 
   url 'http://www.affinic.com/download/adg_macosx.dmg'
-  name 'Affinic Debugger GUI'
-  name 'ADG'
+  name 'Affinic Debugger'
   homepage 'http://www.affinic.com/?page_id=109'
 
-  app 'Affinic Debugger GUI.app'
+  app 'Affinic Debugger.app'
 end
