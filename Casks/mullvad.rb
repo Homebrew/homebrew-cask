c650aeb68f337c4da1b8a245d450abe170ad9f24
Pontus Malm <malmstenen@gmail.com>
2017-03-11 03:43:17 +0100

    Add Mullvad.app v61 (#30804)

---
+++ Casks/mullvad.rb
@@ -0,0 +1,10 @@
+cask 'mullvad' do
+  version '61'
+  sha256 '16769849f5370d406765d607a9c9d650c01aa48f0c1c4d99d4587ba7bcbfd83c'
+
+  url "https://www.mullvad.net/media/client/Mullvad-#{version}.dmg"
+  name 'Mullvad'
+  homepage 'https://mullvad.net'
+
+  app 'Mullvad.app'
+end
