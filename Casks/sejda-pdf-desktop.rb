635343331f313219dd52fe0164d6355a03f2c9f6
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-12 07:38:20 +0800

    Update sejda-pdf-desktop.rb

--- Casks/sejda-pdf-desktop.rb
+++ Casks/sejda-pdf-desktop.rb
@@ -4,7 +4,7 @@ cask 'sejda-pdf-desktop' do
 
   # bitbucket.org/sejdapdf/sejda-desktop-public was verified as official when first introduced to the cask
   url "https://bitbucket.org/sejdapdf/sejda-desktop-public/downloads/sejda-desktop_#{version}.dmg"
-  name 'sejda-pdf-desktop'
+  name 'Sejda PDF Desktop'
   homepage 'https://www.sejda.com/desktop'
 
   app 'Sejda PDF Desktop.app'
