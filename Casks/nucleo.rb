31931ce396a01ce4c57a97e23d4b0671a8fadf84
NAOTONE <me@naotone.com>
2017-03-11 10:57:40 -0500

    Update nucleo to 1.0.8

--- Casks/nucleo.rb
+++ Casks/nucleo.rb
@@ -1,11 +1,11 @@
 cask 'nucleo' do
-  version '1.0.6'
-  sha256 '12488166734d96bdc43faf6bfac156def05ad5701ee827dfd49245ba57c04b23'
+  version '1.0.8'
+  sha256 '29fc28950580397b369abadf18b399a41789bfbbac187dfd09d337f6205dd366'
 
   # dl.devmate.com was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/co.ambercreative.Nucleoapp/Nucleo.zip'
   appcast 'https://updates.devmate.com/co.ambercreative.Nucleoapp.xml',
-          checkpoint: '77915ed41d2083508e6fe6ea299afc5af542bef273b9fe521830e69c65d2db0f'
+          checkpoint: 'f9919b453efa59ddddbc230a3f9b10502021d56c4dc4fed059d17914acff44e5'
   name 'Nucleo'
   homepage 'https://nucleoapp.com/'
 
