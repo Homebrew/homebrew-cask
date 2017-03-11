ab1ab108086b298379f40ce47758cbdb7ae716e0
Gobinath Mallaiyan <i@gobinath.com>
2017-03-11 02:03:22 -0500

    Adding Beaker Browser V.0.6.1
    - New Cask to Include Beaker Browser v0.6.1

---
+++ Casks/beaker-browser.rb
@@ -0,0 +1,10 @@
+cask 'beaker-browser' do
+  version '0.6.1'
+  sha256 'd52887eff9877326940267b11f2bca1b48f7a1f601e82012e8847c844e92b91a'
+
+  url 'https://download.beakerbrowser.net/download/latest/osx'
+  name 'Beaker Browser'
+  homepage 'https://beakerbrowser.com'
+
+  app 'Beaker Browser.app'
+end
