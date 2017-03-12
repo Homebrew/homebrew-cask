c87503ae622d26973008631424cba8199280e815
Vítor Galvão <info@vitorgalvao.com>
2017-03-12 19:37:54 +0000

    Update fugio.rb

--- Casks/fugio.rb
+++ Casks/fugio.rb
@@ -2,7 +2,7 @@ cask 'fugio' do
   version '2.6.0'
   sha256 'ad7d00590a747d31bb35680414c8ff4cd6d39d597e52d677f896cb41bf22e6d7'
 
-  url 'http://www.bigfug.com/releases/Fugio-2.6.0-Cask.zip'
+  url "http://www.bigfug.com/releases/Fugio-#{version}-Cask.zip"
   name 'Fugio'
   homepage 'http://www.bigfug.com/software/fugio'
 
