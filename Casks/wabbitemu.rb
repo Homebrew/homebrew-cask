64ae321994df007171f24c36b7d06d3265589f22
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-13 03:17:19 +0800

    Update wabbitemu.rb

--- Casks/wabbitemu.rb
+++ Casks/wabbitemu.rb
@@ -1,9 +1,9 @@
 cask 'wabbitemu' do
-  version :latest
-  sha256 :no_check
+  version '21040'
+  sha256 '23fc164767da10389eec858d022967fb58fdca1debaa5b9e022f2e9280a4f4e8'
 
   # download-codeplex.sec.s-msft.com/Download was verified as official when first introduced to the cask
-  url 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wabbit&DownloadId=187234&FileTime=130242671530600000&Build=21040'
+  url "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wabbit&DownloadId=187234&FileTime=130242671530600000&Build=#{version}"
   name 'Wabbitemu TI Calculator Emulator'
   homepage 'https://wabbit.codeplex.com'
 
