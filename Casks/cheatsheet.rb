1ede2b1699c52e46d1de41dc5f3b0bdf22f5bb64
Victor Popkov <victor@popkov.me>
2017-03-12 23:14:12 +0200

    Update cheatsheet to 1.2.7

--- Casks/cheatsheet.rb
+++ Casks/cheatsheet.rb
@@ -1,11 +1,11 @@
 cask 'cheatsheet' do
-  version '1.2.6'
-  sha256 'cb4008869ea0dcb569a7c310c73a0bfe2192a7eb566f41b13c771a22adaea78c'
+  version '1.2.7'
+  sha256 'cfe0c699e04ea116965d13be02eee8358d5acd89c32144c388d0e28ee693fbd3'
 
   # mediaatelier.com/CheatSheet was verified as official when first introduced to the cask
   url "https://mediaatelier.com/CheatSheet/CheatSheet_#{version}.zip"
   appcast 'https://mediaatelier.com/CheatSheet/feed.php',
-          checkpoint: 'fa0b4ea2a2da30024eee3071e6cfcd4ebbf2fac502c2f7fe5f07e5039256b22c'
+          checkpoint: '59fa86defe758b8e0d85572f5a67e72cf559a8be0f0b912e4f4510dc50f85c0e'
   name 'CheatSheet'
   homepage 'https://www.cheatsheetapp.com/CheatSheet/'
 
