e0bdaba4562a655aa3bf7afd09d62d19b59cb93b
Jussi Tuominen <jussi@COLDwithin.local>
2017-03-12 16:29:36 +0200

    Update swinsian to 1.13.1

--- Casks/swinsian.rb
+++ Casks/swinsian.rb
@@ -1,10 +1,10 @@
 cask 'swinsian' do
-  version '1.13.0'
-  sha256 '1c31a8c22a4cfd2779df23c48c9f5cb2d45d0fd0d4be664ddc244e21a21a6300'
+  version '1.13.1'
+  sha256 'f8947ce7c3a52879c66f0927680be705b8013c46a407cc5246880fbf03fcd981'
 
   url "https://www.swinsian.com/sparkle/Swinsian_#{version}.zip"
   appcast 'https://www.swinsian.com/sparkle/sparklecast.xml',
-          checkpoint: '1f1539d5f95c42862a816b7b1a7ea137166fd2b4a2e974257121ef6f38c860fc'
+          checkpoint: 'e7da1b004c95e4f8a4872f51dfd35db169828a0cd2a2ab9ddf677576bfabc5ba'
   name 'Swinsian'
   homepage 'https://swinsian.com/'
 
