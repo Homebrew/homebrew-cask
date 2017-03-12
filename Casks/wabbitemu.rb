6da3083d67f6adf017c48fba1a2b7f319bd520ca
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-13 03:08:13 +0800

    Update wabbitemu.rb

--- Casks/wabbitemu.rb
+++ Casks/wabbitemu.rb
@@ -1,8 +1,8 @@
 cask 'wabbitemu' do
   version '187234'
-  sha256 'cbae40f1041d5e7b084931e329f1bd81528b8024c9a5138d874cf7940af28cb2'
+  sha256 '23fc164767da10389eec858d022967fb58fdca1debaa5b9e022f2e9280a4f4e8'
 
-  url "https://wabbit.codeplex.com/downloads/get/#{version}"
+  url "https://wabbit.codeplex.com/downloads/get/#{version}#"
   name 'Wabbitemu TI Calculator Emulator'
   homepage 'https://wabbit.codeplex.com'
 
