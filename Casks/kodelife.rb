6f87b384e2d793b09e40313e104bfbf33be29c2a
Victor Popkov <victor@popkov.me>
2017-03-12 23:26:59 +0200

    Update kodelife to 0.4.2 (#30853)

--- Casks/kodelife.rb
+++ Casks/kodelife.rb
@@ -1,10 +1,10 @@
 cask 'kodelife' do
-  version '0.4.1'
-  sha256 'cd75661db4496b160109589ab05a83c6808c4d810778c573584f70f24229c429'
+  version '0.4.2'
+  sha256 '60e12db0d2da09d0efac2f1c345367bb0be15dfa58a07a91f1edef87341e8683'
 
   url "http://hexler.net/pub/kodelife/kodelife-#{version}-osx.zip"
   appcast 'http://hexler.net/pub/kodelife/appcast.hex',
-          checkpoint: '72aaf0fc5ec5c8f865026db8a44ad6f574a6a5444986c64375a9bb95d3961853'
+          checkpoint: '34e61d28b9c31c6dba034cb39f05493685ce86e4d5372159d86eaf9ec2dfed5e'
   name 'KodeLife'
   homepage 'https://hexler.net/software/kodelife'
 
