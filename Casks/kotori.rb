7b1f3faed6000d5e4d84c4011038b8f67c9f3fe4
Victor Popkov <victor@popkov.me>
2017-03-12 23:31:39 +0200

    Update kotori to 0.22 (#30854)

--- Casks/kotori.rb
+++ Casks/kotori.rb
@@ -1,10 +1,10 @@
 cask 'kotori' do
-  version '0.20'
-  sha256 '719f7a87f72654c1760787ec998665622f0b48bddb033b18d7eaa9886b5505a3'
+  version '0.22'
+  sha256 'b8fbb9e7d35eb85ea82db0ebda6b491acea063d95e8475b412f2a65ef6bf3786'
 
   url "https://github.com/Watson1978/kotori/releases/download/v#{version}/kotori_#{version}.dmg"
   appcast 'https://github.com/Watson1978/kotori/releases.atom',
-          checkpoint: 'b68136ca01e145656496a64bb64821b8f506d6d34f9ec14b48162a93ce8a1594'
+          checkpoint: '5707982aa7fa0e030f1cfd58ad975da1adb6d49e32b1f97b6db9ca1541128f2a'
   name 'kotori'
   name '小鳥'
   homepage 'https://github.com/Watson1978/kotori'
