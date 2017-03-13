42dbc01d8777a0eab655b18a88a11c474a11576e
Gobinath Mallaiyan <i@gobinath.com>
2017-03-13 10:30:04 -0400

    Update mikogo to latest

--- Casks/mikogo.rb
+++ Casks/mikogo.rb
@@ -3,7 +3,7 @@ cask 'mikogo' do
   sha256 :no_check
 
   # mikogo4.com was verified as official when first introduced to the cask
-  url 'https://download.mikogo4.com/mikogo.dmg'
+  url 'http://download.mikogo4.com/Mikogo-installer.signed.pkg'
   name 'Mikogo'
   homepage 'https://www.mikogo.com/'
 
