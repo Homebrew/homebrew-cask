cb502aeab86b6d7f99d3d70bd5148543432972d2
Vítor Galvão <info@vitorgalvao.com>
2017-03-13 23:18:34 +0000

    Casks/logitech-options.rb: remove sudo: false

--- Casks/logitech-options.rb
+++ Casks/logitech-options.rb
@@ -11,8 +11,7 @@ cask 'logitech-options' do
   pkg "LogiMgr Installer #{version}.app/Contents/Resources/LogiMgr.mpkg"
 
   uninstall script:  {
-                       executable: '/Applications/Utilities/LogiMgr Uninstaller.app/Contents/Resources/Uninstaller',
-                       sudo:       false,
+                       executable: '/Applications/Utilities/LogiMgr Uninstaller.app/Contents/Resources/Uninstaller'
                      },
             pkgutil: [
                        'com.logitech.manager.pkg',
