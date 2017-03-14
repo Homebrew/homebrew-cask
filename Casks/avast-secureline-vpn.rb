568f00bb97c2dedec27370e8be692cb0436cebfe
Gianni Tam-McMillan <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-03-14 21:05:36 +0000

    Add cask avast-secureline-vpn

---
+++ Casks/avast-secureline-vpn.rb
@@ -0,0 +1,23 @@
+cask 'avast-secureline-vpn' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://mac.sl.d.avast.com/mac/avast_secureline.dmg'
+  name 'Avast SecureLine VPN'
+  homepage 'https://www.avast.com/secureline-vpn#mac'
+
+  pkg 'Avast SecureLine VPN.pkg'
+
+  uninstall script:   '/Applications/AvastSecureLine.app/Contents/Applications/AvastSecureLineUninstaller.app/Contents/MacOS/AvastSecureLineUninstaller',
+            pkgutil:   'com.avast.secureline',
+            launchctl: [
+                         'com.avast.secureline.userinit',
+                         'com.avast.secureline.update-agent',
+                         '*.com.avast.osx.secureline.avastsecurelinehelper',
+                       ]
+
+  zap delete: [
+                '~/Library/Cookies/com.avast.osx.secureline.binarycookies',
+                '~/Library/Caches/com.avast.osx.secureline',
+              ]
+end
