55f33f319e8866f4ee3b58b4817a5c9780915293
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-13 23:34:50 -0400

    Update mikogo to latest (#30877)
    * Update mikogo to latest
    
    * Update mikogo to latest
    
    - Corrected PKG usage

--- Casks/mikogo.rb
+++ Casks/mikogo.rb
@@ -3,10 +3,12 @@ cask 'mikogo' do
   sha256 :no_check
 
   # mikogo4.com was verified as official when first introduced to the cask
-  url 'https://download.mikogo4.com/mikogo.dmg'
+  url 'http://download.mikogo4.com/Mikogo-installer.signed.pkg'
   name 'Mikogo'
   homepage 'https://www.mikogo.com/'
 
-  # Renamed for clarity: app name is inconsistent with its branding
-  app 'Mikogo-host.app', target: 'Mikogo.app'
+  pkg 'Mikogo-installer.signed.pkg'
+
+  uninstall pkgutil: 'com.mikogo.pkg.open-beta',
+            delete:  '/Applications/Mikogo-host.app'
 end
