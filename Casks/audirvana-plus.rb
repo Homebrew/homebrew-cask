42c77b6d7c90b46014738064724225ebf9ea6886
Artem Medvedev <amedvedev@outlook.com>
2017-03-15 08:46:29 +0200

    Update audirvana-plus to 3.0.2

--- Casks/audirvana-plus.rb
+++ Casks/audirvana-plus.rb
@@ -1,12 +1,20 @@
 cask 'audirvana-plus' do
-  version '2.6.6'
-  sha256 'fd2d0832f1257459991cc1e4008b2c154d86b7b518f66064096d78e885a5ac92'
+  version '3.0.2'
+  sha256 'bd0b19314e567ba0ca0f38eee9286925c3d31315d762598c7105bfe83ca096d8'
 
   url "https://audirvana.com/delivery/AudirvanaPlus_#{version}.dmg"
   appcast "https://audirvana.com/delivery/audirvanaplus#{version.major}_appcast.xml",
-          checkpoint: '487036025712b4e5d8681666fb319408faa4177c749f3185386bf9d329d98f08'
+          checkpoint: '377d6d49236c794f858eddd352cc726ba03c4a7cf0aa9f351cc09a1a93081d3d'
   name "Audirvana Plus #{version.major}"
   homepage 'https://audirvana.com/'
 
   app 'Audirvana Plus.app'
+
+  zap delete: [
+                '/Library/LaunchDaemons/com.audirvana.Audirvana-Plus.plist',
+                '/Library/PrivilegedHelperTools/com.audirvana.Audirvana-Plus',
+                '~/Library/Caches/com.audirvana.Audirvana-Plus',
+                '~/Library/Cookies/com.audirvana.Audirvana-Plus.binarycookies',
+                '~/Library/Preferences/com.audirvana.Audirvana-Plus.plist',
+              ]
 end
