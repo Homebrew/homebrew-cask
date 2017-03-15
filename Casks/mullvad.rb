cb6c9cfeeb6d79742d6e627a6a135dbfaea7ba8e
Pontus Malm <malm@chalmers.it>
2017-03-15 12:30:01 +0100

    Update Mullvad to v62

--- Casks/mullvad.rb
+++ Casks/mullvad.rb
@@ -1,6 +1,6 @@
 cask 'mullvad' do
-  version '61'
-  sha256 '16769849f5370d406765d607a9c9d650c01aa48f0c1c4d99d4587ba7bcbfd83c'
+  version '62'
+  sha256 '980a638842f43a8466c7a856152b65efb7a9aa88721c218194a4ac6653ff3448'
 
   url "https://www.mullvad.net/media/client/Mullvad-#{version}.dmg"
   name 'Mullvad'
