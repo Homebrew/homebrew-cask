e49a3626e3b3cf7f40fb299f740494c229582f26
Mikael Kjaer <mkjaer@users.noreply.github.com>
2017-03-15 18:24:37 +0400

    Update filezilla to 3.25.0 (#30907)

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,16 +1,16 @@
 cask 'filezilla' do
-  if MacOS.version <= :snow_leopard
-    version '3.8.1'
-    sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
-  else
+  if MacOS.version <= :mavericks
     version '3.24.1'
     sha256 '79f28c13820ffcb9e6fa4446a1a317a45fd91d6a67fb1a562d30443f1abe186e'
+  else
+    version '3.25.0'
+    sha256 '37b2b623d481e472e7d0a31a44aee7e2ffdfff1fbddd231963cd373176026e51'
   end
 
   # sourceforge.net/filezilla was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   appcast 'https://sourceforge.net/projects/filezilla/rss?path=/FileZilla_Client',
-          checkpoint: '9aef92172bbc03270c5097accc8aa56af29a23ba59a8a7e5d19073f002f7824e'
+          checkpoint: '95412095423095b55575ff398e510f6bc6970b1a61e109423e5da8928682061f'
   name 'FileZilla'
   homepage 'https://filezilla-project.org/'
 
