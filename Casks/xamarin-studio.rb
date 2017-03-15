47cbf3017809b7029f1e803b6fdaa3595bce6518
Alex Petin <axp@ngs.ru>
2017-03-15 19:11:17 +0700

    Update xamarin-studio to 6.2.0.1829 (#30946)

--- Casks/xamarin-studio.rb
+++ Casks/xamarin-studio.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-studio' do
-  version '6.2.0.1821'
-  sha256 'c2532647409bb6d60ab4c0b61e7bc1459502d6a1931da3aeb642130ef460383f'
+  version '6.2.0.1829'
+  sha256 'ab7558419af831e5b56023edfef5ae110b44c91a99b8ff595c3a7867592700f7'
 
   url "https://dl.xamarin.com/MonoDevelop/Mac/XamarinStudio-#{version}.dmg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '16d648bbecc3ec71d375dbbd193ee0f7a8116c562fedefa9c050f2fac1b4726d'
+          checkpoint: '08fff1d7a6a76fad3c5bddf85eac1be682963000aff8a1c355284dd7361fd02d'
   name 'Xamarin Studio'
   homepage 'https://www.xamarin.com/studio'
 
