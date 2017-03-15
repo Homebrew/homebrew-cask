918da36c3941a349156abd11d7443af8250858eb
Vítor Galvão <info@vitorgalvao.com>
2017-03-15 00:31:42 +0000

    Update filezilla.rb

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,14 +1,10 @@
 cask 'filezilla' do
-  if MacOS.version <= :snow_leopard
-    version '3.8.1'
-    sha256 '86c725246e2190b04193ce8e7e5ea89d5b9318e9f20f5b6f9cdd45b6f5c2d283'
+  if MacOS.version <= :mavericks
+    version '3.24.1'
+    sha256 '79f28c13820ffcb9e6fa4446a1a317a45fd91d6a67fb1a562d30443f1abe186e'
   else
-    if MacOS.version <= :mavericks
-      version '3.24.1'
-      sha256 '79f28c13820ffcb9e6fa4446a1a317a45fd91d6a67fb1a562d30443f1abe186e'
-    else
-      version '3.25.0'
-      sha256 '37b2b623d481e472e7d0a31a44aee7e2ffdfff1fbddd231963cd373176026e51'
+    version '3.25.0'
+    sha256 '37b2b623d481e472e7d0a31a44aee7e2ffdfff1fbddd231963cd373176026e51'
   end
 
   # sourceforge.net/filezilla was verified as official when first introduced to the cask
