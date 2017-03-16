50b009609885faf8964c1b31fac877a77a79d527
Gobinath Mallaiyan <i@gobinath.com>
2017-03-15 22:44:39 -0400

    Update auristor-client to 0.149

--- Casks/auristor-client.rb
+++ Casks/auristor-client.rb
@@ -1,5 +1,5 @@
 cask 'auristor-client' do
-  version '0.128'
+  version '0.149'
 
   if MacOS.version == :mavericks
     sha256 'e79579c9ac2cd609bedd2e01104c113a3417e082e6b5a9f8d333d74e8a6d5030'
@@ -11,7 +11,7 @@ cask 'auristor-client' do
     sha256 'b0a7eee25398e265304a7adfe672f8daa942b916ed7e069d45c3b871d023086f'
     url "https://www.auristor.com/downloads/auristor/osx/macos-10.11/AuriStor-client-#{version}-ElCapitan.dmg"
   else
-    sha256 'bd10ac755255e30b887cb8a534ddbf19801ffbec7a40a831a2580302bd3a6646'
+    sha256 'a5b0cb5e14654e7e0558b779f3c5c32e03a691be10f3f88ea7857cebea2f66f1'
     url "https://www.auristor.com/downloads/auristor/osx/macos-10.12/AuriStor-client-#{version}-Sierra.dmg"
   end
 
