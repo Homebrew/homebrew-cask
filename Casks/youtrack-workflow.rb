6cda36e644d1561bce2b0c3bbdaa266ae80c2d01
jcbot <jcb@leipert.io>
2017-03-17 15:10:33 +0100

    Update youtrack-workflow to 3385

--- Casks/youtrack-workflow.rb
+++ Casks/youtrack-workflow.rb
@@ -1,10 +1,10 @@
 cask 'youtrack-workflow' do
-  version '3384fix'
-  sha256 '331bd2745696507da7ee3a6a193587fc53739face966c12736c16235211cda14'
+  version '3385'
+  sha256 '113749a54abd6c537bf1838a91cf35a70b2fdf04cfc4880eb906448b3156053e'
 
   url "https://download.jetbrains.com/charisma/youtrack-workflow-editor-#{version}-macos.zip"
   appcast 'https://data.services.jetbrains.com/products/releases?code=YTWE&latest=true&type=release',
-          checkpoint: '595ed7ce2488d2700a26c9709c4b9c1b9e5795c8afbd0a3d3c597999d06a30df'
+          checkpoint: 'df24f300b6c16a313b60874679a1b78546a854cd1fef8250de7968c6f6fefe1c'
   name 'JetBrains Youtrack Workflow Editor'
   homepage 'https://www.jetbrains.com/youtrack/download/get_youtrack.html#materials=workflow-editor'
 
