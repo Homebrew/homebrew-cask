91f056947bc635bd9d7b20728e379265086a1c9b
Lawrence <lawrencews@jhu.edu>
2017-03-17 13:06:42 -0400

    Add `IPA Manager.app` v2.2.1
    IPA Palette is a palette-class input method for OS X 10.5 and later.
    It makes possible point-and-click input of International Phonetic
    Alphabet symbols into Unicode-savvy applications.

---
+++ Casks/ipa-manager.rb
@@ -0,0 +1,10 @@
+cask 'ipa-manager' do
+  version '2.2.1'
+  sha256 '5cd8d2ae311f2d99b0e83e7e861ccdaf7156dab874b2e170d645791031509ffd'
+
+  url 'https://www.blugs.com/Downloads/IPAManager.dmg'
+  name 'IPA Manager'
+  homepage 'https://www.blugs.com/IPA/'
+
+  app 'IPA Manager.app'
+end
