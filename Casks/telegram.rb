6e7ae5a9bd00ff471189091b70cb04a69c44201d
cryomi <yulik_86@mail.ru>
2017-03-17 22:39:30 +0300

    Renewed Telegram.app version to 2.91-90199 (#31023)
    From 2.91-90072 to 2.91-90199.

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,10 +1,10 @@
 cask 'telegram' do
-  version '2.91-90072'
-  sha256 '01ed53901dba0696eee09f688e53dfef6fc7728690b48f803eb145bda7537341'
+  version '2.91-90199'
+  sha256 '4f1806e7c72c6d2494612ae0972b348e85b8caeb5ee9406846a4d6af84096b9d'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml',
-          checkpoint: '03c3982bc1b87fad4260d7a3ad79b54555871861662a234306cbf03d1ca9ce3a'
+          checkpoint: 'cff68418229da26d5f6bbd5fe46764e4878fe183c4a4d02eedb77feabeba0a3b'
   name 'Telegram for macOS'
   homepage 'https://macos.telegram.org/'
 
