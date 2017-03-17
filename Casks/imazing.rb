d0ae0c809233172ec6a16c3a707685c3232c058b
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-17 05:42:28 -0400

    Update imazing to 2.2.0 (#30937)
    * Update imazing to 2.2.0
    
    * Update imazing.rb

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -1,11 +1,9 @@
 cask 'imazing' do
-  version '1.5.10,1462359797'
-  sha256 '6f6eb0b238a3837bc0899b54ff20b1373af0d73a95e9c4d3230c5f827011b65a'
+  version '2.2.0'
+  sha256 '19dae23b93617a8f3815ed1cde9537754af58b22bfafc8cf490638145a76c5d6'
 
-  # devmate.com/com.DigiDNA.iMazingMac was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.DigiDNA.iMazingMac/#{version.before_comma}/#{version.after_comma}/iMazingforMac-#{version.before_comma}.dmg"
-  appcast 'https://updates.devmate.com/com.DigiDNA.iMazingMac.xml',
-          checkpoint: '8753345cab0ae9c735b346860854dc4806d9f90dbb21a61cf7fa83a6d5aabdb1'
+  # dl.devmate.com was verified as official when first introduced to the cask
+  url "https://dl.devmate.com/com.DigiDNA.iMazing#{version.major}Mac/iMazing#{version.major}forMac.dmg"
   name 'iMazing'
   homepage 'https://imazing.com/'
 
