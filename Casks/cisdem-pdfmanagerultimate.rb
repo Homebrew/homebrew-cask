59f182da91efda81ae213bad6cf350eb11ade3b9
Chaz Larson <chaz@chazlarson.com>
2017-03-17 04:13:25 -0500

    Create cisdem-pdfmanagerultimate (#30990)
    * Create cisdem-pdfmanagerultimate
    
    * Rename cisdem-pdfmanagerultimate to cisdem-pdfmanagerultimate.rb

---
+++ Casks/cisdem-pdfmanagerultimate.rb
@@ -0,0 +1,10 @@
+cask 'cisdem-pdfmanagerultimate' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.cisdem.com/download/cisdem-pdfmanagerultimate.dmg'
+  name 'Cisdem PDFManagerUltimate'
+  homepage 'https://www.cisdem.com/pdf-manager-ultimate-mac.html'
+
+  app 'Cisdem PDFManagerUltimate.app'
+end
