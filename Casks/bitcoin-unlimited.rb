8a12b415961b4f099de09595a9eefd8e4965b01d
Justin Aiken <60tonangel@gmail.com>
2017-03-17 14:34:09 -0600

    Add Bitcoin Unlimited (1.0.1.1, current stable)

---
+++ Casks/bitcoin-unlimited.rb
@@ -0,0 +1,22 @@
+cask 'bitcoin-unlimited' do
+  version '1.0.1.1'
+  sha256  '47aa38b237d15112ea41e70341be3d2cbbc0edc431a2ddf2533bb34cbd359db9'
+
+  url      "https://www.bitcoinunlimited.info/downloads/bitcoinUnlimited-#{version}-osx.dmg"
+  name     'Bitcoin Unlimited'
+  homepage 'https://www.bitcoinunlimited.info'
+
+  conflicts_with cask: 'bitcoin-core',
+                 cask: 'bitcoin-xt'
+
+  depends_on     macos: '>= :mountain_lion'
+
+  # Renamed for consistency: app name is different in the Finder and in a shell.
+  app 'Bitcoin-Qt.app', target: 'Bitcoin Unlimited.app'
+
+  preflight do
+    set_permissions "#{staged_path}/Bitcoin-Qt.app", '0755'
+  end
+
+  zap delete: '~/Library/Preferences/info.bitcoinunlimited.Bitcoin-Qt.plist'
+end
