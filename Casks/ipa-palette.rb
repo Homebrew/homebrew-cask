40bdaa96912b6fc8a9cb49c577a9b0dd9e7a3128
Lawrence <lawrencews@jhu.edu>
2017-03-17 14:29:42 -0400

    renamed ipa-manager to ipa-palette to reflect the way the application is referred to on the website and in general use (IPA Manager is just the app installed that manages the IPA Palette tool)

--- Casks/ipa-palette.rb
+++ Casks/ipa-palette.rb
@@ -1,11 +1,11 @@
-cask 'ipa-manager' do
+cask 'ipa-palette' do
   version '2.2.1'
   sha256 '5cd8d2ae311f2d99b0e83e7e861ccdaf7156dab874b2e170d645791031509ffd'
 
   url 'https://www.blugs.com/Downloads/IPAManager.dmg'
   appcast 'https://www.blugs.com/Appcasts/IPAPalette.xml',
           checkpoint: 'ea8fde774a46f69a2c4adac6e60f9ab4ccf4e4f5bc52c5cf706787a59999c0bf'
-  name 'IPA Manager'
+  name 'IPA Palette'
   homepage 'https://www.blugs.com/IPA/'
 
   auto_updates true
