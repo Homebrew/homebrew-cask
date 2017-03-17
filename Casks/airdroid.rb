0ce41f1ef31b8d1c76abae43f2b0f2042410b64d
Gianni Tam-McMillan <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-03-17 23:27:42 +0000

    Update airdroid to 3.4.1.0

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask 'airdroid' do
-  version '3.4.0.0'
-  sha256 '06f58f86309fe9d34ca94063ccb5445b08b7487e5651f90984ddfcb47ce5bcee'
+  version '3.4.1.0'
+  sha256 '9691ab3e83c4167f76dcf5c9b6b8993f425f34ff4fd317f3b632168ebcfc4515'
 
   # s3.amazonaws.com/dl.airdroid.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
