d64cb7c139f5da8a8853526e05251ec5b946ae8a
giannitm <giannitammcmillan@gmail.com>
2017-03-18 14:34:05 +0000

    messenger-for-desktop: Add zap delete stanza and update appcast checkpoint (#31078)

--- Casks/messenger-for-desktop.rb
+++ Casks/messenger-for-desktop.rb
@@ -5,9 +5,21 @@ cask 'messenger-for-desktop' do
   # github.com/Aluxian/Facebook-Messenger-Desktop was verified as official when first introduced to the cask
   url "https://github.com/Aluxian/Facebook-Messenger-Desktop/releases/download/v#{version}/messengerfordesktop-#{version}-osx.dmg"
   appcast 'https://github.com/Aluxian/Facebook-Messenger-Desktop/releases.atom',
-          checkpoint: 'b26b01cc4a54ceae8f024c5e157b1bb4327338faf5768b4bc38707f02e9fc662'
+          checkpoint: '3f77603c5d2b79e63a66044fcbc9a11c309c692282e911eb2e209c418b72e56c'
   name 'Messenger for Desktop'
   homepage 'https://messengerfordesktop.com/'
 
   app 'Messenger for Desktop.app'
+
+  zap delete: [
+                '/Library/Logs/DiagnosticReports/Messenger For Desktop*',
+                '~/Library/Application Support/Messenger For Desktop',
+                '~/Library/Caches/com.aluxian.messengerfd',
+                '~/Library/Caches/com.aluxian.messengerfd.ShipIt',
+                '~/Library/Cookies/com.aluxian.messengerfd.binarycookies',
+                '~/Library/Preferences/ByHost/com.aluxian.messengerfd.ShipIt*',
+                '~/Library/Preferences/com.aluxian.messengerfd.helper.plist',
+                '~/Library/Preferences/com.aluxian.messengerfd.plist',
+                '~/Library/Saved Application State/com.aluxian.messengerfd.savedState',
+              ]
 end
