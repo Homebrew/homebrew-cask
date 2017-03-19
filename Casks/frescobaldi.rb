723c909d1179463e00efa86f9c73e35f38b43934
Vítor Galvão <info@vitorgalvao.com>
2017-03-19 18:39:38 +0000

    Update frescobaldi.rb

--- Casks/frescobaldi.rb
+++ Casks/frescobaldi.rb
@@ -4,13 +4,14 @@ cask 'frescobaldi' do
 
   # github.com/wbsoft/frescobaldi was verified as official when first introduced to the cask
   url 'https://github.com/wbsoft/frescobaldi/releases/download/v2.20.0/Frescobaldi-2.20.0-x86_64.dmg'
-  appcast 'https://github.com/wbsoft/frescobaldi/releases.atom', checkpoint: '1edb6cbd5767024e224f7aecda697d206b49a180066180dd3201359cf4f6a598'
+  appcast 'https://github.com/wbsoft/frescobaldi/releases.atom',
+          checkpoint: '1edb6cbd5767024e224f7aecda697d206b49a180066180dd3201359cf4f6a598'
   name 'Frescobaldi'
   homepage 'http://frescobaldi.org/'
+  
+  depends_on cask: 'lilypond'
 
   app 'Frescobaldi.app'
 
   zap delete: '~/Library/Preferences/org.frescobaldi.frescobaldi.plist'
-
-  caveats "If you don't already have LilyPond installed, download it from http://lilypond.org or install it with `brew cask install lilypond`."
 end
