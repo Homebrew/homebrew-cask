839850a452aadc972fb8a4cbfed304c063974eb0
Vítor Galvão <info@vitorgalvao.com>
2017-03-19 13:32:24 +0000

    Update exfalso.rb

--- Casks/exfalso.rb
+++ Casks/exfalso.rb
@@ -3,7 +3,7 @@ cask 'exfalso' do
   sha256 '8184024910a6d2914cb30bf662f31a69a1bad077f6c0c909269f9dd4e11462e3'
 
   # bitbucket.org was verified as official when first introduced to the cask
-  url 'https://bitbucket.org/lazka/quodlibet/downloads/ExFalso-3.8.1.dmg'
+  url "https://bitbucket.org/lazka/quodlibet/downloads/ExFalso-#{version}.dmg"
   name 'Ex Falso'
   homepage 'http://quodlibet.readthedocs.io/'
 
