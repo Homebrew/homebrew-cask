a8c292e30e1b832196e85572bc71325dc2a3c268
Artem Medvedev <amedvedev@outlook.com>
2017-03-19 03:17:52 +0200

    Update url and appcast in Iconjar.app

--- Casks/iconjar.rb
+++ Casks/iconjar.rb
@@ -1,13 +1,21 @@
 cask 'iconjar' do
-  version '0.9.5.2,103'
-  sha256 'dc74b92f7e32e042ce72115a7caa262c7369d00f5bd4bf3832af0c6c0d11cc4a'
+  version :latest
+  sha256 :no_check
 
-  # hockeyapp.net/api/2/apps/b91fcb22f1ac5013dad6ffde2dca7c79 was verified as official when first introduced to the cask
-  url "https://rink.hockeyapp.net/api/2/apps/b91fcb22f1ac5013dad6ffde2dca7c79/app_versions/#{version.after_comma}?format=zip"
-  appcast 'https://rink.hockeyapp.net/api/2/apps/b91fcb22f1ac5013dad6ffde2dca7c79',
-          checkpoint: '5a30ef3b0be22eeb3f047ab72e997643a2e9b23f63b54ad409618c056b2d3efc'
+  # dl.devmate.com/com.iconjar.iconjar was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.iconjar.iconjar/Iconjar.dmg'
+  appcast 'https://updates.devmate.com/com.iconjar.iconjar.xml',
+          checkpoint: 'ac2cdc02fcba9c9f5e38b379c93a17a86b5fd618d52ef9c1a7b0527dd1531997'
   name 'IconJar'
   homepage 'https://geticonjar.com/'
 
+  auto_updates true
+
   app 'IconJar.app'
+
+  zap delete: [
+                '/Users/Shared/IconJar',
+                '~/Library/Application Support/IconJar',
+                '~/Library/Preferences/com.iconjar.iconjar.plist',
+              ]
 end
