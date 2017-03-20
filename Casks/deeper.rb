bd6a5fbe053406f4be16d9bc22190562ff88d4cd
Tin H Kyaw <tinhkyaw@users.noreply.github.com>
2017-03-20 11:20:44 -0700

    Update urls for Deeper latest, Maintenance latest and Onyx latest (#31136)

--- Casks/deeper.rb
+++ Casks/deeper.rb
@@ -4,15 +4,9 @@ cask 'deeper' do
 
   macos_release = MacOS.version.to_s.delete('.')
 
-  if MacOS.version <= :yosemite
-    url "http://www.titanium.free.fr/download/#{macos_release}/Deeper.dmg"
-  else
-    # joel.barriere.pagesperso-orange.fr was verified as official when first introduced to the cask
-    url "http://joel.barriere.pagesperso-orange.fr/download/#{macos_release}/Deeper.dmg"
-  end
-
+  url "https://www.titanium-software.fr/download/#{macos_release}/Deeper.dmg"
   name 'Deeper'
-  homepage 'http://www.titanium.free.fr/deeper.html'
+  homepage 'http://www.titanium-software.fr/en/deeper.html'
 
   # Unusual case: The software will stop working, or is dangerous to run, on the next macOS release.
   depends_on macos: [
