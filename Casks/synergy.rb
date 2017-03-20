298b1a2503606d0af880cb9e3a2758847866514d
Jonathan Wan <jnthnwn@users.noreply.github.com>
2017-03-19 22:43:32 -0600

    Update synergy.rb (#31124)
    Reflect the new URL for Synergy binaries.
    Synergy 1.8.8

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -2,7 +2,7 @@ cask 'synergy' do
   version '1.8.8,c30301e'
   sha256 '9f9019b3c558bf0c05e558828ed282b29c316d9477c5f5169d3461e32dadb9b6'
 
-  url "https://symless.com/files/packages/synergy-v#{version.before_comma}-stable-#{version.after_comma}-MacOSX-x86_64.dmg"
+  url "https://binaries.symless.com/v#{version.before_comma}/synergy-v#{version.before_comma}-stable-#{version.after_comma}-MacOSX-x86_64.dmg"
   name 'Synergy'
   homepage 'https://symless.com/synergy/'
 
