cb6750686875a225480e89a2d2c5eb7e5ea5518a
davidcondrey <davidcondrey@me.com>
2017-03-20 14:06:43 -0700

    Update Adobe Digital Editions version 4.5.4

--- Casks/adobe-digital-editions.rb
+++ Casks/adobe-digital-editions.rb
@@ -1,8 +1,8 @@
 cask 'adobe-digital-editions' do
-  version '4.5'
+  version '4.5.4'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "http://download.adobe.com/pub/adobe/digitaleditions/ADE_#{version}_Installer.dmg"
+  url "http://download.adobe.com/pub/adobe/digitaleditions/ADE_#{version.major_minor}_Installer.dmg"
   name 'Adobe Digital Editions'
   homepage 'https://www.adobe.com/solutions/ebook/digital-editions.html'
 
