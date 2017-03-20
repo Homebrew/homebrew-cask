8c2f1796f76ba904326c06b913d7fbeb4c904383
Vítor Galvão <info@vitorgalvao.com>
2017-03-20 17:16:09 +0000

    Update onyx.rb

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -7,7 +7,6 @@ cask 'onyx' do
     sha256 '10da0a9126080bdf36e3b0a8a7a47e1fdf9eadfe6282e4b502150e020d1fc4d5'
   end
 
-  # joel.barriere.pagesperso-orange.fr/download was verified as official when first introduced to the cask
   url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete('.')}/OnyX.dmg"
   appcast 'http://www.titanium-software.fr/en/release_onyx.html',
           checkpoint: 'ae7c39cba93b9d724adc82dadd2ca421ec39551c7bb7e42487aee9a6e7f49fb9'
