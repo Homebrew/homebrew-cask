bf5c9e623c236863bbf3e6e5cad74caed93c7049
Vítor Galvão <info@vitorgalvao.com>
2017-03-21 13:02:08 +0000

    Update geogebra.rb

--- Casks/geogebra.rb
+++ Casks/geogebra.rb
@@ -1,8 +1,8 @@
 cask 'geogebra' do
-  version :latest
-  sha256 :no_check
+  version '6.0.295.0'
+  sha256 '597bd732b3096defd129501b4dcd0282cb257cb82c495a4006cef3c8cdc30a72'
 
-  url 'https://download.geogebra.org/package/mac-port'
+  url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-MacOS-Portable-#{version.dots_to_hyphens}.zip"
   name 'GeoGebra'
   homepage 'https://www.geogebra.org/'
 
