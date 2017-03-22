9600bb00683bb9aa7c41f7f208425ca0b77ab826
jcbot <jcb@leipert.io>
2017-03-22 23:32:18 +0100

    Update clion to 2016.3.5

--- Casks/clion.rb
+++ Casks/clion.rb
@@ -1,10 +1,10 @@
 cask 'clion' do
-  version '2016.3.4'
-  sha256 '9cc76ac67c5dd25dee63fe47bbf3131b73214e390d3406fc0327bceb725546c7'
+  version '2016.3.5'
+  sha256 'bc4d29ca8ba6b54f37f81372cfc3492d476246b39783dc89c5bbc33e99f2e86c'
 
   url "https://download.jetbrains.com/cpp/CLion-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=CL&latest=true&type=release',
-          checkpoint: 'f48a8b3797cd281e6f578341c83bf6d0914a5fa9291f499f66dc8680dea3216e'
+          checkpoint: '17803b3e7a50a31d23334273bea66d9d8ebe8d1ba638cd7d722db5f3adc168e0'
   name 'CLion'
   homepage 'https://www.jetbrains.com/clion/'
 
