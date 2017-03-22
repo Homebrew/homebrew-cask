1779ef2ff33f67186c3e3e52e9d13eb76d6b82f5
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-22 09:58:46 +0800

    Update marcedit.rb

--- Casks/marcedit.rb
+++ Casks/marcedit.rb
@@ -1,6 +1,6 @@
 cask 'marcedit' do
-  version '6.2'
-  sha256 'ab0543d683fbf7b59cc5564cf1231e831e6dd326ba83db0efa34a15cadb5f4b4'
+  version :latest
+  sha256 :no_check
 
   url 'http://marcedit.reeset.net/software/MarcEdit.dmg'
   name 'MarcEdit.app'
