ec656cb0991e0944956b672fb07891b5c860ae5d
davidcondrey <davidcondrey@me.com>
2017-03-22 01:43:02 -0700

    update uninstall

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -5,17 +5,17 @@ cask 'metasploit' do
   url 'https://osx.metasploit.com/metasploitframework-latest.pkg'
   appcast 'https://github.com/rapid7/metasploit-framework/releases.atom',
           checkpoint: '5de0fa55d535d005b078370eaaf3e50b4428a25041fdae604e4e0d66413595a6'
-  name 'Metasploit'
+  name 'Metasploit Framework'
   homepage 'https://www.metasploit.com'
   license :bsd
 
   pkg 'metasploitframework-latest.pkg'
 
-  uninstall script:  {
-                        executable: 'uninstall.tool',
-                        input:      %w[Yes] 
-                     },
-            pkgutil: 'test.rapid7releaseengineeringr7rerapid7com.pkg.metasploit-framework'
-
-  zap delete: '~/.msf4'
+  uninstall  script:  {
+                       executable: '/opt/metasploit-framework/bin/msfremove',
+                       input:      %w[Yes],
+                       sudo:       true
+                      },
+            pkgutil:  '.*metasploit.*',
+              rmdir:  '/opt/metasploit-framework'
 end
