40064a9354f32deba349b4f269247fb46aabc524
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-23 05:16:11 +0800

    Update metasploit.rb

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,10 +1,10 @@
 cask 'metasploit' do
-  version '4.4.13'
+  version '4.14.3'
   sha256 '1adb8fc8911ed229c8c3d86e5de680f39054ec7a08e1b0794899260b20ca6e2b'
 
   url 'https://osx.metasploit.com/metasploitframework-latest.pkg'
   appcast 'https://github.com/rapid7/metasploit-framework/releases.atom',
-          checkpoint: '5de0fa55d535d005b078370eaaf3e50b4428a25041fdae604e4e0d66413595a6'
+          checkpoint: '73b285460f9e7304a63a269de121e039ad3e71150a3d9351199398fb882bd505'
   name 'Metasploit Framework'
   homepage 'https://www.metasploit.com/'
 
