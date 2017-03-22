295384328078609bda54578c86ea3ff3f0bb9a90
Gobinath Mallaiyan <i@gobinath.com>
2017-03-21 21:53:50 -0400

    Adding Marcedit.app v6.2
    - Including new app marcedit
    - Closes #31172

---
+++ Casks/marcedit.rb
@@ -0,0 +1,10 @@
+cask 'marcedit' do
+  version '6.2'
+  sha256 'ab0543d683fbf7b59cc5564cf1231e831e6dd326ba83db0efa34a15cadb5f4b4'
+
+  url 'http://marcedit.reeset.net/software/MarcEdit.dmg'
+  name 'MarcEdit.app'
+  homepage 'http://marcedit.reeset.net/'
+
+  app 'MarcEdit.app'
+end
