e2a067c8ad2b4120215dce0d3ba1bedc4d2216bc
Paulo Romeira <paulo@pauloromeira.com>
2017-03-22 19:58:43 -0300

    Update datagrip to 2017.1

--- Casks/datagrip.rb
+++ Casks/datagrip.rb
@@ -1,10 +1,10 @@
 cask 'datagrip' do
-  version '2016.3.4'
-  sha256 '9a5fd58be9fb9e9144ee13df83590a869dda33f89a4b5cd7c0dc0a94b59e01ad'
+  version '2017.1'
+  sha256 '1ebc833e3bd9acdb3837c393bd993cb0bd03a09b681cfeeb5fa2d3bb523be9da'
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release',
-          checkpoint: '5a5654769c64b370838a9e2b1412d12a417c8988e6c0e1324bc3013d0ca616df'
+          checkpoint: '8f904c3a1e9958b6da7a6a087001c23f46a60a7159635220ef73c42887172f5f'
   name 'DataGrip'
   homepage 'https://www.jetbrains.com/datagrip/'
 
