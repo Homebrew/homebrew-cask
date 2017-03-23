7952103fe0562f57c0333dbb2affc736e30fafd8
Barijaona Ramaholimihaso <github.com@barijaona.com>
2017-03-23 05:18:32 +0300

    Update vienna to 3.1.9 (#31211)

--- Casks/vienna.rb
+++ Casks/vienna.rb
@@ -1,11 +1,11 @@
 cask 'vienna' do
-  version '3.1.8'
-  sha256 '8916188c2eaeb6107224961f0b7dbe9e458f59e4db27aafb9065988fea9af0aa'
+  version '3.1.9'
+  sha256 '4b0bb5b1ad4574c540fd1104f04d5a3d83ccb03d2c95cd354b272758e7d1c2a0'
 
   # bintray.com/viennarss was verified as official when first introduced to the cask
   url "https://dl.bintray.com/viennarss/vienna-rss/Vienna#{version}.tar.gz"
   appcast 'https://viennarss.github.io/sparkle-files/changelog.xml',
-          checkpoint: 'e16e5fb62571e6f28b5054a973716b9bfb2e253f319259f1a22156cd42883f72'
+          checkpoint: '0ba8a2208a3f5797f63ed0f6e3225aa9af6276ce57efbd182364d8f7c29a709d'
   name 'Vienna'
   homepage 'http://www.vienna-rss.org/'
 
