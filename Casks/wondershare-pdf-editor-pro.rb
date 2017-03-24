85ac219eadfdd80cf80eef4ebd52dfb65392faac
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-25 05:21:36 +0800

    Update wondershare-pdf-editor-pro.rb

--- Casks/wondershare-pdf-editor-pro.rb
+++ Casks/wondershare-pdf-editor-pro.rb
@@ -1,8 +1,8 @@
 cask 'wondershare-pdf-editor-pro' do
-  version '5.7.1'
+  version '738'
   sha256 '6ea68d6ff1a27c2080fed4944a64396db84f8b1c9aa5e89a4a2c6087109cab14'
 
-  url 'http://download.wondershare.com/mac-pdf-editor_full738.dmg'
+  url "http://download.wondershare.com/mac-pdf-editor_full#{version}.dmg"
   name 'Wondershare PDF Editor for Mac'
   homepage 'https://pdf.wondershare.com/pdf-editor-mac/'
 
