f95bfd11b5739b1f6928c8f3850671ec9b3b4e5e
Adrian Petrescu <apetresc@gmail.com>
2017-03-24 16:01:20 -0400

    Update PyCharm CE to 2017.1

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,10 +1,10 @@
 cask 'pycharm-ce' do
-  version '2016.3.3'
-  sha256 'a088b656e3e493d865512511873a7b8eec8621456c81cad500b95b5c9cc862f9'
+  version '2017.1'
+  sha256 '248bbed16874f42d2f75bebc237a572eb8a43fef4b1fc778efabc549d1aed159'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=PCC&latest=true&type=release',
-          checkpoint: '6ec0db4129aea6c15fb0534a2e8f3343783bf29bae23930166a4bb6c1ad373ef'
+          checkpoint: 'd56902f0f9917a35f04f2e538d9b2f432f18550bbd8eb05acff59cbadd90894d'
   name 'Jetbrains PyCharm Community Edition'
   name 'PyCharm CE'
   homepage 'https://www.jetbrains.com/pycharm/'
