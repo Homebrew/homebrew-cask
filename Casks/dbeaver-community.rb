d08a09fbf893f045b9b9a58a0697939aafbcac61
Tarun <eztarun@gmail.com>
2017-03-24 10:59:16 -0400

    Update dbeaver-community to 4.0.3 (#31251)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
-  version '4.0.2'
-  sha256 '8545d888a908996a4b84ac87a47b3943684f8ce9339a86260bba8b8b66578296'
+  version '4.0.3'
+  sha256 '6324a964024653241ba41a25b2415a2c67ddeb8e4892740b6d78a3907207094d'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name 'DBeaver Community Edition'
