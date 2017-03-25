00087512ef2597974c86e52a052a0b0287f57c9c
commitay <commitay@users.noreply.github.com>
2017-03-26 09:21:27 +1000

    Create metasploit.rb

---
+++ Casks/metasploit.rb
@@ -0,0 +1,35 @@
+cask 'metasploit' do
+  version '4.14.6,20170325092307'
+  sha256 '8367198a6e5290f88fdfbb6ff71c59ed1e20827d3fc7df2d5a4ae32cda82b315'
+
+  url "https://osx.metasploit.com/metasploit-framework-#{version.major_minor_patch}+#{version.after_comma}-1rapid7-1.pkg"
+  appcast 'https://osx.metasploit.com/LATEST',
+          checkpoint: '29508eb54b33f9120fb31e02a6b3f7397927dd98b3b857a7613c402e2d223e0c'
+  name 'Metasploit Framework'
+  homepage 'https://www.metasploit.com/'
+  gpg "#{url}.asc", key_id: '2007B954'
+
+  depends_on formula: %w[
+                        nmap
+                        postgresql
+                      ]
+
+  pkg "metasploit-framework-#{version.major_minor_patch}+#{version.after_comma}-1rapid7-1.pkg"
+  binary '/opt/metasploit-framework/bin/metasploit-aggregator'
+  binary '/opt/metasploit-framework/bin/msfbinscan'
+  binary '/opt/metasploit-framework/bin/msfconsole'
+  binary '/opt/metasploit-framework/bin/msfd'
+  binary '/opt/metasploit-framework/bin/msfdb'
+  binary '/opt/metasploit-framework/bin/msfelfscan'
+  binary '/opt/metasploit-framework/bin/msfmachscan'
+  binary '/opt/metasploit-framework/bin/msfpescan'
+  binary '/opt/metasploit-framework/bin/msfremove'
+  binary '/opt/metasploit-framework/bin/msfrop'
+  binary '/opt/metasploit-framework/bin/msfrpc'
+  binary '/opt/metasploit-framework/bin/msfrpcd'
+  binary '/opt/metasploit-framework/bin/msfupdate'
+  binary '/opt/metasploit-framework/bin/msfvenom'
+
+  uninstall pkgutil: '.*metasploit.*',
+            delete:  '/opt/metasploit-framework'
+end
