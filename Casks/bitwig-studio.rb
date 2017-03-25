36bbf1f66859de562bcdc80724df10b024716b44
Joshua McKinney <joshka@users.noreply.github.com>
2017-03-25 04:11:27 -0500

    Update bitwig-studio to 1.3.16

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask 'bitwig-studio' do
-  version '1.3.15'
-  sha256 '05ab5a7c93d2c917708ca4524151dc77e50252c56f0965a6b0fff5c2897783f8'
+  version '1.3.16'
+  sha256 '2429416edb2ea704eb1165c5c3bf5c876c8000a551a53c952f00f0da762c8aa0'
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   name 'Bitwig Studio'
