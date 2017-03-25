3ee2ab53da5680369cb6369b13568fb9a32b645a
Joshua McKinney <joshka@users.noreply.github.com>
2017-03-25 17:39:54 -0500

    Update bitwig-studio to 2.0

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,10 +1,16 @@
 cask 'bitwig-studio' do
-  version '1.3.16'
-  sha256 '2429416edb2ea704eb1165c5c3bf5c876c8000a551a53c952f00f0da762c8aa0'
+  version '2.0'
+  sha256 'e7ca7ddaadf1fda21ea6dcef01afa33c012e5278150b773ee87091ac268f3d6e'
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   name 'Bitwig Studio'
   homepage 'https://www.bitwig.com/'
 
   app 'Bitwig Studio.app'
+
+  zap delete: [
+                '~/Library/Application Support/Bitwig',
+                '~/Library/Caches/Bitwig',
+                '~/Library/Logs/Bitwig',
+              ]
 end
