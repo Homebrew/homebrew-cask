c3b56ea2775b9e7ecea7f828ac46501f3602116d
Maarten Paauw <maartenpaauw@gmail.com>
2017-03-25 14:20:18 +0100

    Update teamspeak-client to 3.1.3

--- Casks/teamspeak-client.rb
+++ Casks/teamspeak-client.rb
@@ -1,6 +1,6 @@
 cask 'teamspeak-client' do
-  version '3.1.2'
-  sha256 '56e834e6eac5a700790d7611b8fe782d146b1a75ad802cc7d2ccc1d871165fab'
+  version '3.1.3'
+  sha256 '881d14f171411354208766ed3ab7b81f2ea8ffdb7c595f283dbb37db144dd039'
 
   # dl.4players.de/ts was verified as official when first introduced to the cask
   url "http://dl.4players.de/ts/releases/#{version}/TeamSpeak#{version.major}-Client-macosx-#{version}.dmg"
