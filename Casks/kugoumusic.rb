5a60e8be37d5d11a6868236cedd4e2b29189b92b
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-26 12:04:37 +0800

    Update kugoumusic.rb

--- Casks/kugoumusic.rb
+++ Casks/kugoumusic.rb
@@ -4,7 +4,7 @@ cask 'kugoumusic' do
 
   url 'http://macservice.kugou.com/download/KugouMusicForMac.dmg'
   name 'Kugou Music'
-  homepage 'https://www.kugou.com/'
+  homepage 'http://www.kugou.com/'
 
   app 'KugouMusic.app'
 end
