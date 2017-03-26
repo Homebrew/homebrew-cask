cfb3c06203ab49c0abd53df2256918b7cc5362eb
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-26 11:36:30 +0800

    Update capture-one.rb

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -4,7 +4,7 @@ cask 'capture-one' do
 
   url "http://downloads.phaseone.com/9c7cb7b7-1525-4cfc-86a8-7d8f5a51f38e/International/CaptureOne.Mac.#{version}.dmg"
   name 'Capture One'
-  homepage 'https://www.phaseone.com/en/Products/Software/Capture-One-Pro/dot-release.aspx'
+  homepage 'https://www.phaseone.com/en/Products/Software/Capture-One-Pro/Whats-new.aspx'
 
   app "Capture One #{version.major}.app"
 end
