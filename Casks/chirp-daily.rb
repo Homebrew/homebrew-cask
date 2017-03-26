d2025cd97be78b7bf1614da87356a6a86836c4b3
Stephen Walker-Weinshenker <sww1235@users.noreply.github.com>
2017-03-26 13:54:12 -0600

    Update chirp-daily to latest version 20170324

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,5 +1,5 @@
 cask 'chirp-daily' do
-  version '20170222'
+  version '20170324'
   sha256 'ec3d82f04e88922fb89b1ee4515623aa3aabbb2a1c4184797315ac4db126fdb7'
 
   url "http://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
