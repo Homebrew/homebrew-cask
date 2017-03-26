ad9ca8ce76e6d63bb2f0b7755b5e7e0e85061786
Anthony Garand <garand@garand.me>
2017-03-25 23:24:21 -0400

    Update capture-one 10.0.2 SHA256

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -1,6 +1,6 @@
 cask 'capture-one' do
   version '10.0.2'
-  sha256 ''
+  sha256 'ffd01fa5ee4a5ddfdcf74db0156f6ea8ea983c6b7b382d008b6da5204cdebe2b'
 
   url "http://downloads.phaseone.com/9c7cb7b7-1525-4cfc-86a8-7d8f5a51f38e/International/CaptureOne.Mac.#{version}.dmg"
   name 'Capture One'
