b2a653d01d357075281beb4acee9f04ea41d8ce5
Gobinath Mallaiyan <i@gobinath.com>
2017-03-26 09:51:56 -0400

    Update eagle to 8.1.0

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -1,6 +1,6 @@
 cask 'eagle' do
-  version '8.0.2'
-  sha256 '4fea0669d2eafc39dcdb60ca60d7fa2763970f5efd358fd85458b4a0699bdd7a'
+  version '8.1.0'
+  sha256 'a3abd48794e545b51925f2bce5f5a81303f7917c97c2749d9b0e23f351901d01'
 
   url "http://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   name 'CadSoft EAGLE'
