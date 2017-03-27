550be4d0abc6fea72c2149b5b54122fb99edc008
alswl <alswlx@gmail.com>
2017-03-27 14:29:26 +0800

    fix: url homepage differ

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -2,6 +2,7 @@ cask 'cctalk' do
   version '0.8.1-176'
   sha256 '701f597df2d21ff5c71b12fdb9311d2661dfc28c4782106cd55533519e7c4a9d'
 
+  # f1.ct.hjfile.cn was verified as official when first introduced to the cask
   url 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/archive/0.8.1.176/CCtalk-0.8.1-176-hujiang-2017-01-12.23.37.dmg'
   name 'CCTalk'
   homepage 'http://www.cctalk.com/download/'
