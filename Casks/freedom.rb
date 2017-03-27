02eff4e3adcfe682b50f616ed7e0d212fb194003
Sergei Kolobov <sergei@kolobov.com>
2017-03-27 11:26:19 +0300

    Update freedom to 1.4.6 (#31345)

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,10 +1,10 @@
 cask 'freedom' do
-  version '1.4.2'
-  sha256 'cf1e9c1174c3e1d5cd98ee1d8598d4e6a209edc4ce4aa3e618f23230ef98de3d'
+  version '1.4.6'
+  sha256 'de857dbbcf3f1f65dc8a44380977ed7dc3a7da6bda1e5616f3555767b459384e'
 
   url 'https://cdn.freedom.to/installers/FreedomSetup.dmg'
   appcast 'https://cdn.freedom.to/installers/updates/mac/Appcast.xml',
-          checkpoint: '0605cda5d267294eabb156a9e268be230367b7153d48638a32ef89cc4d789e93'
+          checkpoint: 'd5b94d6d470c8ecc7f77bca718d6641ea5de9188fa4db354bad5fa700e1d7fd5'
   name 'Freedom'
   homepage 'https://freedom.to/'
 
