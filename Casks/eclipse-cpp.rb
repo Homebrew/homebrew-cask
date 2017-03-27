2d57ede945d518d01e8010b93b1f22025081dfd0
neutric <neutric@users.noreply.github.com>
2017-03-27 18:32:41 +0200

    Update eclipse-cpp to 4.6.3,neon:3 (#31362)

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-cpp' do
-  version '4.6.2,neon:2'
-  sha256 '34a655e0acf7ffd720a3071a3ee1dcc44e844f9388d77809b5aa7868027a5e20'
+  version '4.6.3,neon:3'
+  sha256 '60d1aead8f620c0805c678840bf5b9ea08a28c9be96b9012477be741d0b23c13'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-cpp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse IDE for C/C++ Developers'
