253a7b0760d3aafb70fa8ce09518769ea566c6d0
alswl <alswlx@gmail.com>
2017-03-27 14:14:47 +0800

    Add CCTalk.app v0.8.1-176

---
+++ Casks/cctalk.rb
@@ -0,0 +1,10 @@
+cask 'cctalk' do
+  version '0.8.1-176'
+  sha256 '701f597df2d21ff5c71b12fdb9311d2661dfc28c4782106cd55533519e7c4a9d'
+
+  url 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/archive/0.8.1.176/CCtalk-0.8.1-176-hujiang-2017-01-12.23.37.dmg'
+  name 'CCTalk'
+  homepage 'http://www.cctalk.com/download/'
+
+  app 'CCTalk.app'
+end
