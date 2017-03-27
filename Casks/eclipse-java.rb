27cb19ad99c72434e6733bd656f90236a7d9a9ff
neutric <neutric@users.noreply.github.com>
2017-03-27 20:19:54 +0200

    Update eclipse-java to 4.6.3,neon:3 (#31380)

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-java' do
-  version '4.6.2,neon:2'
-  sha256 '6fb58cdfe12c9ea0d36839405ac64b99997ae1a2280cfe6ea0ca6fcc756430dd'
+  version '4.6.3,neon:3'
+  sha256 '886bc0d38a1cb9c3fab17d6f4a9f28266763894e690e1bcce9098ca65cbf1a77'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-java-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
   name 'Eclipse IDE for Java Developers'
