83bd4c12bfe078417ab9f83b4e17e41ea07bcfaf
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-27 13:31:31 -0400

    Update vectoraster to 7.0.9 (#31367)

--- Casks/vectoraster.rb
+++ Casks/vectoraster.rb
@@ -1,6 +1,6 @@
 cask 'vectoraster' do
-  version '7.0.8'
-  sha256 'e4089f97ff86d5296886150dcfc2878cf999432c99085aa424632e801d345964'
+  version '7.0.9'
+  sha256 '3c38d964266f47d148fbe1b0dac58fa75b44c1562420555a07d7185db00a2244'
 
   url "https://www.lostminds.com/downloads/dl.php?vectoraster#{version.major}.zip"
   name 'Vectoraster'
