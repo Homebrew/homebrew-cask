17ff13c22dac996543e70a2bdc8a0d68a2be423d
Yurii Kolesnykov <yurikoles@gmail.com>
2017-03-27 18:20:12 +0300

    Update upwork to 4_2_115_0_7n66kq4suk1ud047

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -1,6 +1,6 @@
 cask 'upwork' do
-  version '4_2_95_0_9xgmle488qryoawe'
-  sha256 '64f674c63ea281006891570a941c1f3412d42d1696af544a6a4b8cdb1aefee37'
+  version '4_2_115_0_7n66kq4suk1ud047'
+  sha256 '7c63fb5e93518ae0b4d47d01d92b1f4455120472b2a2a3dc17e48809de6c1669'
 
   url "https://updates-desktopapp.upwork.com/binaries/v#{version}/Upwork.dmg"
   name 'Upwork'
