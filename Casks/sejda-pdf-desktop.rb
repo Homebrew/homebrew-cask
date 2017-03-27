1443655b659dd19c87a5f8c6981a0d00faed20d6
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-26 20:57:20 -0400

    Update sejda-pdf-desktop to 2.5.0 (#31324)

--- Casks/sejda-pdf-desktop.rb
+++ Casks/sejda-pdf-desktop.rb
@@ -1,6 +1,6 @@
 cask 'sejda-pdf-desktop' do
-  version '2.4.0'
-  sha256 '6ce9f2294310c0a663bd99564375948bec2e9984332cb1c5091068db31e23e80'
+  version '2.5.0'
+  sha256 'ec2dc5dcfea0dad3ef620ebdbd6d54b314b186cecd8ee7612431f14d18f06c83'
 
   # bitbucket.org/sejdapdf/sejda-desktop-public was verified as official when first introduced to the cask
   url "https://bitbucket.org/sejdapdf/sejda-desktop-public/downloads/sejda-desktop_#{version}.dmg"
