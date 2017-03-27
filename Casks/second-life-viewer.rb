04c4ebcd31f25280495200c6d526606857562f7b
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-28 07:08:51 +0800

    Update second-life-viewer.rb

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -2,7 +2,7 @@ cask 'second-life-viewer' do
   version '5.0.3.324435'
   sha256 'f34b7a99fef9939265611aaa2470a54a985406b3b7955a85505c3c8cea6a8f75'
 
-  url "http://download.cloud.secondlife.com/Viewer_5/Second_Life_#{version.dots_to_underscores}_i386.dmg"
+  url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_i386.dmg"
   name 'Linden Lab Second Life Viewer'
   homepage 'https://secondlife.com/'
 
