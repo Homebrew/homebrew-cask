f51377f32a7c60d82126ecd6fc5b95049803725f
Kirito <simonlassau@users.noreply.github.com>
2017-03-28 13:27:49 +0200

    Update skim to 1.4.28 (#31471)

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,10 +1,10 @@
 cask 'skim' do
-  version '1.4.27'
-  sha256 'a3ce0577014059ee7ac3ea6bc298a442eea28ce13d0a6e26a624e4b82b11789c'
+  version '1.4.28'
+  sha256 'fc3b7213c22f5ddf899f3b120aa79e82640db483943558f7ab3f2ad3fc23e5c6'
 
   url "https://downloads.sourceforge.net/skim-app/Skim/Skim-#{version}/Skim-#{version}.dmg"
   appcast 'http://skim-app.sourceforge.net/skim.xml',
-          checkpoint: '6e81b6dfd587b2205c9b2500a62d9a04c14134e4481eadf3c84e279ba16e9913'
+          checkpoint: 'dacac0f8ae3453e85eb2d67045c2f0dd518e6a9ee3dc90a018294391a3dbf91b'
   name 'Skim'
   homepage 'http://skim-app.sourceforge.net/'
 
