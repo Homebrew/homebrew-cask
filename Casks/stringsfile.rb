e34888c8348b7e0dc7faae9f21b59769db7bd620
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-28 17:50:50 -0400

    Update stringsfile to latest (#31535)

--- Casks/stringsfile.rb
+++ Casks/stringsfile.rb
@@ -2,7 +2,7 @@ cask 'stringsfile' do
   version :latest
   sha256 :no_check
 
-  url 'http://blog.timac.org/post-images/StringsFileQuickLook/StringsFile.qlgenerator.zip'
+  url 'http://blog.timac.org/wp-content/post-images/StringsFileQuickLook/StringsFile.qlgenerator.zip'
   name 'StringsFile QuickLook plugin'
   homepage 'http://blog.timac.org/?p=933'
 
