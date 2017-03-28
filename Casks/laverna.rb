0e11182d17ead7fb54a6890ece711b321e7c237d
Gobinath Mallaiyan <i@gobinath.com>
2017-03-28 08:53:24 -0400

    Update laverna to 0.7.3

--- Casks/laverna.rb
+++ Casks/laverna.rb
@@ -5,7 +5,7 @@ cask 'laverna' do
   # github.com/Laverna/laverna was verified as official when first introduced to the cask
   url "https://github.com/Laverna/laverna/releases/download/#{version}/laverna-#{version}-darwin-x64.zip"
   appcast 'https://github.com/Laverna/laverna/releases.atom',
-          checkpoint: 'f76275b249b08d1b525da20e6391570e4c687ab32e8be78b9d88473fc086772a'
+          checkpoint: '9237abe7697aa7294e5ffbbdd8ff70f6ded0e4657bfd26ae4d46140dc609262d'
   name 'Laverna'
   homepage 'https://laverna.cc/'
 
