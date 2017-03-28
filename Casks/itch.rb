61e44bffcca922edfee1a76642539bfebd431ddc
Victor Popkov <victor@popkov.me>
2017-03-28 21:56:41 +0300

    Update itch to 23.4.0

--- Casks/itch.rb
+++ Casks/itch.rb
@@ -1,11 +1,11 @@
 cask 'itch' do
-  version '23.3.1'
-  sha256 'eb963879e17f26c67bda3c382401478b4f4e36c70b3f3308a6dc643864dee2f2'
+  version '23.4.0'
+  sha256 'c20cc689fcc5bf97eb55a5f7f53c4ab11cb2a276bd0ee70803f9ae36736e33ac'
 
   # github.com/itchio/itch was verified as official when first introduced to the cask
   url "https://github.com/itchio/itch/releases/download/v#{version}/itch-mac.dmg"
   appcast 'https://github.com/itchio/itch/releases.atom',
-          checkpoint: 'd981c31d952f0152ed0fcaccd21e9b3ac1858a3d0bcaa96e01fd71051f5b883e'
+          checkpoint: '69c016cf3ba40f8b3ebdc90bc38d05d7482d1591a597151784290b654e807ec4'
   name 'itch'
   homepage 'https://itch.io/app'
 
