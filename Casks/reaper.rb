6f0bb46d9f9bd3a06e0ba2bfb62bc14150cb8a88
cfillion <cfillion@users.noreply.github.com>
2017-03-28 14:09:27 -0400

    update REAPER to 5.40

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,10 +1,10 @@
 cask 'reaper' do
-  version '5.35'
-  sha256 'acba5b3bcfdb11bbd3a4c5626232d32ce6d21ae3449d5eb0b0d3a7cc49a9103d'
+  version '5.40'
+  sha256 '7b4d257b3716a3d2bcefcb64a863115ae221c25da3be3088ca7f1b1af80b8195'
 
   url "http://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots}_x86_64.dmg"
   appcast 'http://www.reaper.fm/whatsnew.txt',
-          checkpoint: '2f949b46ffa9a609e1adb53d1884efcd54e779465ab33e0473e1fce712d2327f'
+          checkpoint: 'adf9905d3a534285a4c1394dd9d40a7b857071ab98cd0cc1a88df296be2674cf'
   name 'REAPER'
   homepage 'http://www.reaper.fm/'
 
