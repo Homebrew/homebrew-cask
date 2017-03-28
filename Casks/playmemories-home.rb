a55efb0e2e349baeafb731db68319d3b284683e9
naru0504 <naru.kubota@gmail.com>
2017-03-28 14:32:12 +0900

    add PlayMemories Home

---
+++ Casks/playmemories-home.rb
@@ -0,0 +1,10 @@
+cask 'playmemories-home' do
+  version '3.3.00'
+  sha256 '3b0755157bd0d5d4fcae5f6199eac3f6d158bafe018fcd15dd79f56aabfac98b'
+
+  url 'http://support.d-imaging.sony.co.jp/disoft_DL/PMHMAC_DL/mac?fm=ttl'
+  name 'PlayMemories Home'
+  homepage 'http://support.d-imaging.sony.co.jp/www/disoft/int/download/playmemories-home/mac/en/'
+
+  app 'PlayMemories Home.app'
+end
