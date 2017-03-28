2275170f5578b1a28e1c3f0b3b75036ab19893c0
Gobinath Mallaiyan <i@gobinath.com>
2017-03-28 03:44:24 -0400

    Adding Opale Messenger Latest Version
    - New Cask to add Opale Messenger

---
+++ Casks/opale-messenger.rb
@@ -0,0 +1,10 @@
+cask 'opale-messenger' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://dmware.fr/opale/opale.dmg'
+  name 'Opale'
+  homepage 'http://dmware.fr/'
+
+  app 'Opale.app'
+end
