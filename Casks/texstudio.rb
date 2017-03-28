a90ddccade63f50abd646ad8466a29fd8b1075b3
neutric <neutric@users.noreply.github.com>
2017-03-28 18:10:26 +0200

    Update texstudio to 2.12.4 (#31499)

--- Casks/texstudio.rb
+++ Casks/texstudio.rb
@@ -1,10 +1,10 @@
 cask 'texstudio' do
-  version '2.12.2'
-  sha256 'cd8fa98b4884a2fb8db5b2a66222fdc9393e4dfe2df38677619930d66813af94'
+  version '2.12.4'
+  sha256 'b594f4c72454a5d56732c8e4c74690a77f72d99440db5bbbead6cf9640c37666'
 
   url "https://downloads.sourceforge.net/texstudio/texstudio-#{version}-osx-qt5.7.1.zip"
   appcast 'https://sourceforge.net/projects/texstudio/rss',
-          checkpoint: 'bbddafbb347b89e35154f84d725273f11e5d62bec60c128375e9d4a94b283b09'
+          checkpoint: '2a3e20d8bee14961e3de7989ac3cfe31439918b57fe5b6395aac134cbe561dff'
   name 'TeXstudio'
   homepage 'http://texstudio.sourceforge.net/'
 
