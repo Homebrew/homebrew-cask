c5b3d3e30eb2c850d9cc40294fe677480b1bda72
Vítor Galvão <info@vitorgalvao.com>
2017-03-28 23:07:18 +0100

    Update mal-updater.rb

--- Casks/mal-updater.rb
+++ Casks/mal-updater.rb
@@ -2,9 +2,10 @@ cask 'mal-updater' do
   version '2.3'
   sha256 'b6ceb1d8f2ca60da81bd8ff728d0e07c5074bce76e5b857c666ff5f44db68ffa'
 
-  url 'https://malupdaterosx.ateliershiori.moe/download.php'
-  appcast 'https://updates.ateliershiori.moe/malupdaterosx/profileInfo.php',
-          checkpoint: 'c98b2bdd9a563d9cf3027763b4da79530c76ddd2afadda3cfa91f4bd99e37a13'
+  # github.com/Atelier-Shiori/malupdaterosx-cocoa was verified as official when first introduced to the cask
+  url "https://github.com/Atelier-Shiori/malupdaterosx-cocoa/releases/download/#{version}/malupdaterosx-#{version}.dmg"
+  appcast 'https://github.com/Atelier-Shiori/malupdaterosx-cocoa/releases.atom',
+          checkpoint: '3dfc84e2a863b64eeef960dd5403d78a7ba078741f7a6d9bc25b0914993f2edf'
   name 'MAL Updater OS X'
   homepage 'https://malupdaterosx.ateliershiori.moe/'
 
