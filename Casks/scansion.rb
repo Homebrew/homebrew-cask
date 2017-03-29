db2bd0c3d57560b3ef95be990296b23e360636ac
gigaflw <a_gigantic_flower@yeah.net>
2017-03-29 17:42:58 +0800

    Add Scansion.app v1.12
    vcd (Value Change Dump) file viewer

---
+++ Casks/scansion.rb
@@ -0,0 +1,10 @@
+cask 'scansion' do
+  version '1.12'
+  sha256 '5c85eed5db1d3b1bd95152fa155862194e467c7ac4bacdb41f9f3c11052060d4'
+
+  url 'http://www.logicpoet.com/releases/scansion/Scansion_1.12.dmg'
+  name 'scansion'
+  homepage 'http://www.logicpoet.com/scansion/'
+
+  app 'Scansion.app'
+end
