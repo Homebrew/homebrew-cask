79a05c6f6f7dff953ab8b95778ea3d4f3061ce00
Jonathan 'J5' Cook <jonathan.cook@concur.com>
2017-03-29 13:01:26 -0400

    Adding Webex Player
    Currently at version 205

---
+++ Casks/webex-player.rb
@@ -0,0 +1,13 @@
+cask 'webex-player' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://welcome.webex.com/client/T31L/mac/intel/webexplayer_intel.dmg'
+  name 'WebEx Player'
+  homepage 'https://www.webex.com/play-webex-recording.html'
+  license :gratis
+
+  pkg 'WebEx Player.pkg'
+
+  uninstall pkgutil: 'playback.mac.webex.com'
+end
