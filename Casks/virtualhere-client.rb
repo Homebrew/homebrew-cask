096f56bb90cdf64a4c6b6e757cc3c15e054d3c36
Gobinath Mallaiyan <i@gobinath.com>
2017-03-29 09:13:16 -0400

    Adding Virtualhere Client Version 4.0.3
    - Including New Cask for VirtualHere

---
+++ Casks/virtualhere-client.rb
@@ -0,0 +1,10 @@
+cask 'virtualhere-client' do
+  version '4.0.3'
+  sha256 'ecbfa51fb67db4f6cbc5b9345141ef5860333abc1dfba99d578d8360f357e115'
+
+  url 'https://www.virtualhere.com/sites/default/files/usbclient/VirtualHere.dmg'
+  name 'VirtualHere'
+  homepage 'https://www.virtualhere.com/usb_client_software'
+
+  app 'VirtualHere.app'
+end
