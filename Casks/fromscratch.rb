7bd185a160bd4b122f7dc9b1fece486370aac6f0
Gobinath Mallaiyan <i@gobinath.com>
2017-03-29 09:16:51 -0400

    Update fromscratch to 1.3.0

--- Casks/fromscratch.rb
+++ Casks/fromscratch.rb
@@ -5,7 +5,7 @@ cask 'fromscratch' do
   # github.com/Kilian/fromscratch was verified as official when first introduced to the cask
   url "https://github.com/Kilian/fromscratch/releases/download/v#{version}/FromScratch-darwin-x64-#{version}.zip"
   appcast 'https://github.com/Kilian/fromscratch/releases.atom',
-          checkpoint: '38871d364c41971303d97e39099cfa9b850d82a66fa9dcfbe91d356f046df95f'
+          checkpoint: 'ca952c2266a9b03dc53565c0c8f5ab6bd50b16a06122935fadaee887781e4c56'
   name 'FromScratch'
   homepage 'https://fromscratch.rocks/'
 
