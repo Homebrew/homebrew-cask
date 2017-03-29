26421ff5a66dfc06dad452de0c94b98ec7aaae78
Colin Dunn <cgd@vikingdev.net>
2017-03-29 14:12:15 +1100

    Update visual-paradigm-ce to 14.0,20170302 (#31545)

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm-ce' do
-  version '14.0,20170121'
-  sha256 '49f75a7d8fba8bd085e637ce74ac7857a4a13de49ecc33284473a20bb77ae2dd'
+  version '14.0,20170302'
+  sha256 'b3242ac173645d0d844eba14f0fdf38ff158daf4f096bb91175b065aa32d0101'
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vpce#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   name 'Visual Paradigm'
