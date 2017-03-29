453c252c7d995e73959a2a2a5cd4c3420c6cacb7
giannitm <giannitammcmillan@gmail.com>
2017-03-29 19:33:48 +0100

    Update tankix to 571 (#31601)

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version '570'
-  sha256 '5a79216945ca564b720d360689d8037adddc6262aa4b5a31b223594fbb2a7521'
+  version '571'
+  sha256 'b4e07174d8b5dbb65ed521188e63317835e66529b7e7a3198e4e65099d846bcb'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/prod_#{version}/TankiX.dmg"
   name 'Tanki X'
