656ff505f3d8b4b5d3e8093a43d9ce7c1fc4281e
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-29 08:55:36 -0400

    Update virtualhere-server.rb

--- Casks/virtualhere-server.rb
+++ Casks/virtualhere-server.rb
@@ -1,6 +1,6 @@
 cask 'virtualhere-server' do
-  version :latest
-  sha256 :no_check
+  version '3.2.8'
+  sha256 '829060fc7b82cc03ff76e6313dad4e1158c4ed2904896c4dcd20965493462c3d'
 
   url 'https://www.virtualhere.com/sites/default/files/usbserver/VirtualHereServer.dmg'
   name 'VirtualHereServer'
