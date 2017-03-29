52329942beed5675e503105f18262061dda15ba1
Colin Dunn <cgd@vikingdev.net>
2017-03-29 21:20:20 +1100

    Update synology-assistant to 6.1-15030 (#31556)

--- Casks/synology-assistant.rb
+++ Casks/synology-assistant.rb
@@ -1,8 +1,8 @@
 cask 'synology-assistant' do
-  version '6.0-7319'
-  sha256 '8ac000d51671b7d1293d449f496e66ba029a1c7e537610ca65778239e4edcb70'
+  version '6.1-15030'
+  sha256 '534ea2de08744fa8ae9a1bac99ffe40219f14d5f6d3df8a207a3032c53a49bbe'
 
-  url "https://global.download.synology.com/download/Tools/Assistant/#{version}/Mac/Synology-Assistant-#{version}.dmg"
+  url "https://global.download.synology.com/download/Tools/Assistant/#{version}/Mac/synology-assistant-#{version}.dmg"
   name 'Synology Assistant'
   homepage 'https://www.synology.com/'
 
