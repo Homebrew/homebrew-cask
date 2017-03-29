01c25bdec94f9d43d6dcaf754c148f1a83209a34
Gobinath Mallaiyan <i@gobinath.com>
2017-03-29 17:28:00 -0400

    Update sleipnir to 4.5.7

--- Casks/sleipnir.rb
+++ Casks/sleipnir.rb
@@ -1,10 +1,10 @@
 cask 'sleipnir' do
-  version '4.5.6'
-  sha256 'a4d03e6c264274881157a04209ebbb3eabbf23c80197b6ecad8b04384be6e1d5'
+  version '4.5.7'
+  sha256 '4a68918732e5365cb0ca3b38b5731f6dfe103380a671cb1f6c8a3ba1a20ec480'
 
   url 'http://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
   appcast 'https://update.fenrir.co.jp/smartupdate/mac/sleipnir/appcast.xml',
-          checkpoint: '2bcec747977792c3e31ab44d1cb71b1db52f2015ef3c2459b09112c7dd20cc3b'
+          checkpoint: '02fb658905c9f306799788db97c23a0fc403862a866e65c231d9614eda72f50b'
   name 'Sleipnir'
   homepage 'http://www.fenrir-inc.com/jp/sleipnir/'
 
