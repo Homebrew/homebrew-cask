2b75ec419514862b76b2b48c153d9132b69b8a17
Gobinath Mallaiyan <i@gobinath.com>
2017-03-29 11:20:56 -0400

    Update keycue to 8.3

--- Casks/keycue.rb
+++ Casks/keycue.rb
@@ -2,7 +2,8 @@ cask 'keycue' do
   version '8.3'
   sha256 'a261dad0a1f0dde6624d4824a3d7843422d3466ad605ea2fbcf831ecd10d578e'
 
-  url "http://www.ergonis.com/downloads/products/keycue/KeyCue#{version.no_dots}-Install.dmg"
+  url "http://www.ergonis.com/downloads/products/keycue/KeyCue#{version.no_dots}-Install.dmg",
+      user_agent: :fake
   name 'KeyCue'
   homepage 'http://www.ergonis.com/products/keycue/'
 
