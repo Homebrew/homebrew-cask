85f2ce30878284013eab2bc672ae8e2d30d891c0
Simon Bennetts <psiinon@gmail.com>
2017-03-29 12:00:43 +0100

    Used .dots_to_underscores

--- Casks/owasp-zap.rb
+++ Casks/owasp-zap.rb
@@ -1,10 +1,9 @@
 cask 'owasp-zap' do
-  version_dot '2.6.0'
-  version_under '2_6_0'
+  version '2.6.0'
   sha256 '7133593b91d0d7925436adc79370b654cb1d4d642a9bb85cc3880bfaf2683b87'
 
   # github.com/zaproxy/zaproxy was verified as official when first introduced to the cask
-  url "https://github.com/zaproxy/zaproxy/releases/download/#{version_dot}/ZAP_#{version_under}_macos.dmg"
+  url "https://github.com/zaproxy/zaproxy/releases/download/#{version}/ZAP_#{version.dots_to_underscores}_macos.dmg"
   appcast 'https://github.com/zaproxy/zaproxy/releases.atom',
           checkpoint: '7b84ad46090667a859271d665f06266f2397582390c78a92ce3b974c0229e7f4'
   name 'OWASP Zed Attack Proxy'
