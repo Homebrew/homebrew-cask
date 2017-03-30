78b9a2248af49095a6255476ef423bbb61241173
Rick Fletcher <fletch@pobox.com>
2017-03-30 10:48:54 -0400

    Update url comment

--- Casks/rclone-browser.rb
+++ Casks/rclone-browser.rb
@@ -2,7 +2,7 @@ cask 'rclone-browser' do
   version '1.2,a1156a0'
   sha256 '542cd23eea128736999a7e512a9f2ff89be081c688d1581e6c78ab3d3ca118dd'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/mmozeiko/RcloneBrowser was verified as official when first introduced to the cask
   url "https://github.com/mmozeiko/RcloneBrowser/releases/download/#{version.before_comma}/#{version.before_comma}-#{version.after_comma}.zip"
   name 'Rclone Browser'
   homepage 'https://mmozeiko.github.io/RcloneBrowser/'
