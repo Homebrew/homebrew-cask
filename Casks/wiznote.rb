d0be25cb74368ea0d587ede1f6982e4d28f87dd1
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-30 01:13:23 -0400

    Update wiznote to 2.4.4,2017-03-14 (#31632)

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,6 +1,6 @@
 cask 'wiznote' do
-  version '2.4.3,2017-01-03'
-  sha256 '399b7989bdc0e8ba3a646f409dd9ddd301cb34bb4a1cff8fc25d1f861909560d'
+  version '2.4.4,2017-03-14'
+  sha256 '35090576e94ad67f84c213571818fe44cf45ebc0ad1a977a8a53b2a0d1f95ea6'
 
   url "http://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
   name 'WizNote'
