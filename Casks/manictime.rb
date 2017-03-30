7789378ac145b4e264f118e569c82e5fa411394a
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-30 10:56:36 +0800

    Update manictime.rb

--- Casks/manictime.rb
+++ Casks/manictime.rb
@@ -9,8 +9,4 @@ cask 'manictime' do
   pkg "ManicTime-#{version}.pkg"
 
   uninstall pkgutil: 'com.finkit.manictime.tracker'
-
-  caveats 'This is not a full ManicTime Windows port to Mac. '\
-          'It is only a tracker, and  requires ManicTime Server v3.2 or later '\
-          'installed on a Windows machine to receive and display the reports.'
 end
