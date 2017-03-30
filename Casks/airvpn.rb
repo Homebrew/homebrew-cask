caf10d399dcca77c567eb19cba7000f795e2e5d0
Karl Noss <n055@users.noreply.github.com>
2017-03-29 19:49:28 -0700

    Fix incorrect sha256 for cask airvpn (#31625)
    * Fix incorrect sha256 for cask airvpn
    
    See https://airvpn.org/macosx/
    
    * Update airvpn.rb

--- Casks/airvpn.rb
+++ Casks/airvpn.rb
@@ -1,6 +1,6 @@
 cask 'airvpn' do
   version '2.12.4'
-  sha256 '8e7eda78f51756f5da3cd903b6b2d7e1c6c8cf7363793266772b2a5f07c0b462'
+  sha256 'ab52cafc027378c8e37c4807b00377a903d82009d5c5afc53a09da5afff20311'
 
   # eddie.website was verified as official when first introduced to the cask
   url "https://eddie.website/download/?platform=macos&arch=x64&ui=ui&format=installer.pkg&version=#{version}"
