285e58660efaa32e22ef5ddddb0c8e602f195e56
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-31 03:55:58 +0800

    Update colormunki-display.rb

--- Casks/colormunki-display.rb
+++ Casks/colormunki-display.rb
@@ -3,7 +3,7 @@ cask 'colormunki-display' do
   sha256 '763a6b9e8e704452de79ec07d96853c6a9adbb204fe59376754d79eac5487b92'
 
   # downloads.xrite.com was verified as official when first introduced to the cask
-  url 'http://downloads.xrite.com/Downloads/Software/ColorMunki/Display/1.1.3/Mac/ColorMunki_Display.zip'
+  url "http://downloads.xrite.com/Downloads/Software/ColorMunki/Display/#{version}/Mac/ColorMunki_Display.zip"
   name 'ColorMunki Display'
   homepage 'http://xritephoto.com/colormunki-display'
 
