1aa4b53a4d35c120029670ade024c1fdbf54376c
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-30 13:33:32 -0400

    Update macvim to 8.0.130 (#31669)

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,10 +1,10 @@
 cask 'macvim' do
-  version '8.0.129'
-  sha256 'a75d4999986f65b12b5adecde9dbdcf30ae04a19545a65b0131783c547897cf2'
+  version '8.0.130'
+  sha256 'fd70339c60ca2cb8b8729f2cbb618812f232bb4987e7f17079cbfb2e5eb0cea9'
 
   url "https://github.com/macvim-dev/macvim/releases/download/snapshot-#{version.patch}/MacVim.dmg"
   appcast 'https://github.com/macvim-dev/macvim/releases.atom',
-          checkpoint: 'b94f207852bed3a9ece4d15be8163ed9a9b6abd18a751e0ae02123f42bb8b8c2'
+          checkpoint: 'a9a6c47c52a67d06d0fc2c805c1065289e9c07d95c1e1e9718471d75caa012e3'
   name 'MacVim'
   homepage 'https://github.com/macvim-dev/macvim'
 
