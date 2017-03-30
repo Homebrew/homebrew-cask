8d6c4b2020fc4aa3785e2c8d5912737d8625faf1
Vítor Galvão <info@vitorgalvao.com>
2017-03-30 15:42:50 +0100

    Update rclone-browser.rb

--- Casks/rclone-browser.rb
+++ Casks/rclone-browser.rb
@@ -1,11 +1,9 @@
 cask 'rclone-browser' do
-  version '1.2'
+  version '1.2,a1156a0'
   sha256 '542cd23eea128736999a7e512a9f2ff89be081c688d1581e6c78ab3d3ca118dd'
 
-  archive_name = 'rclone-browser-1.2-a1156a0-macOS'
-
   # github.com was verified as official when first introduced to the cask
-  url "https://github.com/mmozeiko/RcloneBrowser/releases/download/#{version}/#{archive_name}.zip"
+  url "https://github.com/mmozeiko/RcloneBrowser/releases/download/#{version.before_comma}/#{version.before_comma}-#{version.after_comma}.zip"
   name 'Rclone Browser'
   homepage 'https://mmozeiko.github.io/RcloneBrowser/'
 
