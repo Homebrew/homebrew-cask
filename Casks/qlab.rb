236926abab57772e9556cd72d89b47a300473202
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-30 13:15:19 +0800

    Update qlab.rb

--- Casks/qlab.rb
+++ Casks/qlab.rb
@@ -3,7 +3,7 @@ cask 'qlab' do
   sha256 'ea36f0df90934ebf6cd6f9f34286304d27a7b727f7e26aa8365fc807afe78466'
 
   url "https://figure53.com/qlab/downloads/QLab-#{version}.zip"
-  appcast 'https://figure53.com/qlab/downloads/appcast-v4/',
+  appcast "https://figure53.com/qlab/downloads/appcast-v#{version.major}/",
           checkpoint: '10cc503ea8aaf0c37b367ca299f6a3bd84553656ba4d69cc3a89813b45c8490a'
   name 'QLab'
   homepage 'https://figure53.com/qlab/'
