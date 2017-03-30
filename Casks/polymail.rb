90107cdcba0c97a6c03a082a2bf330c20533bf80
Gobinath Mallaiyan <i@gobinath.com>
2017-03-29 20:36:11 -0400

    Update polymail to 1.28

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,10 +1,10 @@
 cask 'polymail' do
-  version '1.26'
-  sha256 'c95fefaf4cf3d76e627074dabf0dee2a8c8c42706a1716caead434729018934c'
+  version '1.28'
+  sha256 '7ef0e504fbea42e1f7bdc52a26bf2f17845982f1282a4faaeb9468bfd1c9463c'
 
   url "https://sparkle-updater.polymail.io/osx/builds/Polymail-v#{version.major_minor.no_dots}.zip"
   appcast 'https://sparkle-updater.polymail.io/cast.xml',
-          checkpoint: '46aed69be030447451264ec7b4ee0e284f7933bb629ade7e495d38be0e6b55de'
+          checkpoint: '5cc48c3a3382f7c9c63f2456e20081277ca6b2cc96cb8ad2809580e6cc3789e5'
   name 'Polymail'
   homepage 'https://polymail.io/'
 
