6fe1077ef94739a4ed9992d8083a4e3b6de6f102
giannitm <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-04-01 00:19:59 +0100

    Update flinto to 2.1.2

--- Casks/flinto.rb
+++ Casks/flinto.rb
@@ -1,6 +1,6 @@
 cask 'flinto' do
-  version '2.1.1'
-  sha256 '4616f6183f2b439be32f7c6e1d4fd922527e69b6d9af647758d6f95b7058438f'
+  version '2.1.2'
+  sha256 'a9aa8a8809e5cc76af9c77efe11cb58b377a39bce307105904761e9d5c21a112'
 
   url "https://www.flinto.com/assets/Flinto-#{version}.dmg"
   name 'Flinto'
