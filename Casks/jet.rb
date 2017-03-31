1ab02537cbcdfe56ffd23fad71b5215cbfb08001
Perry <perry@dailyhotel.com>
2017-03-31 10:41:40 +0900

    Update jet to 1.17.0 (#31685)

--- Casks/jet.rb
+++ Casks/jet.rb
@@ -1,6 +1,6 @@
 cask 'jet' do
-  version '1.16.0'
-  sha256 '392ee5660e9c436465bdcf71610482300528dc9cfaa1e9c54e391b2781d48441'
+  version '1.17.0'
+  sha256 'b728fb6070081d81e6c1c58a504426c3cd554d408ab80779b18b133873f413fe'
 
   # s3.amazonaws.com/codeship-jet-releases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/codeship-jet-releases/#{version}/jet-darwin_amd64_#{version}.tar.gz"
