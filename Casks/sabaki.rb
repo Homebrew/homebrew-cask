12d447f6cfe15bb64480355e81df9a399941cc24
Vítor Galvão <info@vitorgalvao.com>
2017-03-31 01:14:41 +0100

    comment was not specific enough

--- Casks/sabaki.rb
+++ Casks/sabaki.rb
@@ -2,7 +2,7 @@ cask 'sabaki' do
   version '0.21.0'
   sha256 'e464ccb75867ca920b842a254231042489d9650a04cc782e2955b5ced4b086ea'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/yishn/Sabaki was verified as official when first introduced to the cask
   url "https://github.com/yishn/Sabaki/releases/download/v#{version}/sabaki-v#{version}-mac-x64.7z"
   name 'Sabaki'
   homepage 'http://sabaki.yichuanshen.de/'
