cf4e5cd37a276107b6f6e48c2d1405b4caaa41be
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-31 11:56:51 -0400

    Update subsmarine to 1.2.3 (#31711)

--- Casks/subsmarine.rb
+++ Casks/subsmarine.rb
@@ -5,7 +5,7 @@ cask 'subsmarine' do
   # amazonaws.com/cwcbucket/subsmarine was verified as official when first introduced to the cask
   url "https://s3-us-west-2.amazonaws.com/cwcbucket/subsmarine/subsmarine.#{version}.zip"
   appcast 'https://www.cocoawithchurros.com/shine/appcast.php?id=7',
-          checkpoint: '3f99aeebb0ac8611031f46340560ecf733977801f85d3d663ad2e812b3846701'
+          checkpoint: '40d2a45c63120bcfe9b0ec15c60c2f2b5b6753c98daa58c3042c6660761b57d5'
   name 'SubsMarine'
   homepage 'https://www.cocoawithchurros.com/subsmarine.php'
 
