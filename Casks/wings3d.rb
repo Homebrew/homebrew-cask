aac761b5b67ff571a2ab75bea5d015987cc25050
Gobinath Mallaiyan <i@gobinath.com>
2017-03-31 11:12:04 -0400

    Update wings3d to 2.1.5

--- Casks/wings3d.rb
+++ Casks/wings3d.rb
@@ -5,7 +5,7 @@ cask 'wings3d' do
   # sourceforge.net/wings was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/wings/wings-#{version}-macosx.dmg"
   appcast 'https://sourceforge.net/projects/wings/rss',
-          checkpoint: '38311ee2ef58ca6eabc963cef085649faf357fd90662208308a572048d64920c'
+          checkpoint: 'ecd54f678a1790ea55882e7ac25e82ae1f42e310e7553f774a26f0471e9284db'
   name 'Wings 3D'
   homepage 'http://www.wings3d.com/'
 
