0bbf40f5f8cbc0d553f1e15a5be8b59e94f0361b
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-03-31 10:06:33 -0400

    Update caret to 2.0.6 (#31700)

--- Casks/caret.rb
+++ Casks/caret.rb
@@ -1,11 +1,11 @@
 cask 'caret' do
-  version '2.0.5'
-  sha256 '84e3dcb448150261d54cdc14da0e62072bd89b12e93ae94f75736f11e96016a7'
+  version '2.0.6'
+  sha256 'db80c98d737e0ff2d07d962388f03c12ba59d1d52cb6ee7f74ce49b03571130e'
 
   # github.com/careteditor/caret was verified as official when first introduced to the cask
   url "https://github.com/careteditor/caret/releases/download/#{version}/Caret.dmg"
   appcast 'https://github.com/careteditor/caret/releases.atom',
-          checkpoint: 'f76e2cd28ec1936d2da7456efa809cb2eadb8ce93752e55a6512162316bc63ec'
+          checkpoint: '134b8dbcbef7340c1699775e26a3e388765960f4b29418e09c3536c1b5d1d363'
   name 'Caret'
   homepage 'https://caret.io/'
 
