58823aaff2e2fa39bb56a1d8bb5896c76f63ada9
Miccal Matthews <miccal.matthews@gmail.com>
2017-03-31 22:24:33 +0800

    Update huamim.rb

--- Casks/huamim.rb
+++ Casks/huamim.rb
@@ -2,6 +2,7 @@ cask 'huamim' do
   version :latest
   sha256 :no_check
 
+  # kisexu-file.b0.upaiyun.com/fp/app was verified as official when first introduced to the cask
   url 'https://kisexu-file.b0.upaiyun.com/fp/app/huami-mac.zip'
   name 'HuaMiM'
   name 'Flower Password'
