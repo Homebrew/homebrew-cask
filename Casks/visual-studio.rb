922494b7226fe9f8c6f7f3835fc4afacc2a7d8ee
Vítor Galvão <info@vitorgalvao.com>
2017-04-01 03:04:20 +0100

    Update visual-studio.rb

--- Casks/visual-studio.rb
+++ Casks/visual-studio.rb
@@ -2,7 +2,7 @@ cask 'visual-studio' do
   version '2017-01-20-1'
   sha256 '5c33d771e8c43668023d8df2ce31f6977c33b36a38af1c39b68a8b0d2b384a8b'
 
-  # xamarin.azureedge.net was verified as official when first introduced to the cask\
+  # xamarin.azureedge.net was verified as official when first introduced to the cask
   url "https://xamarin.azureedge.net/VsMacInstaller/#{version}/VisualStudioforMacPreviewInstaller.dmg"
   name 'Visual Studio for Mac'
   homepage 'https://www.visualstudio.com/vs/visual-studio-mac/'
