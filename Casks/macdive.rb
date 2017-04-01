459dd97e1c79992ae36521e87b8dbfe31dc32b9f
Remo Koch <remkoch@gmail.com>
2017-04-01 18:01:40 +0700

    Add MacDive.app v2.8.1

---
+++ Casks/macdive.rb
@@ -0,0 +1,10 @@
+cask 'macdive' do
+  version '2.8.1'
+  sha256 '715b20c9633a9459f1e7154e99094b0f306033c46468bc36cee65252628c9d17'
+
+  url 'http://mac-dive.com/shimmer/?download&appName=MacDive&appVariant=&appVersion=2.8.1'
+  name '715b20c9633a9459f1e7154e99094b0f306033c46468bc36cee65252628c9d17'
+  homepage 'http://www.mac-dive.com/'
+
+  app 'MacDive.app'
+end
