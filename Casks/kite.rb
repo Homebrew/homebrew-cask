8a8c3205abcca765de2e1725ece88dd8898938fa
Timo Sand <timo.sand@iki.fi>
2017-04-01 14:21:57 +0300

    Renames casks kite to rhodes-kite and kite-copilot to kite

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,12 +1,15 @@
+  version '0.20170330.0'
+  sha256 'ffeef839d7bdf591134ec56f060e0305a4a6e1fc8f1c0cd1714c787eb57ab0db'
 cask 'kite' do
-  version '1.1'
-  sha256 '0f80766eb315d6e2188ea2c62ea86478f838719d34b6e70d1e42d07d8bde5519'
 
-  url 'https://kiteapp.co/downloads/KiteCompositor.dmg'
-  appcast 'https://api.kiteapp.co/kite_appcast.xml',
-          checkpoint: '4bf1b9e1686160f292d1de9e714d5fe74003df2be7a3c06ffa7870a4eb90ea69'
-  name 'Kite Compositor'
-  homepage 'https://kiteapp.co/'
+  # s3-us-west-1.amazonaws.com/kite-downloads was verified as official when first introduced to the cask
+  url "https://s3-us-west-1.amazonaws.com/kite-downloads/Kite-#{version}.dmg"
+  appcast 'https://release.kite.com/appcast.xml',
+          checkpoint: '7611b119c633ab1402d8348f29d1aaa9743b2dadf815a3d984fcb6942e807686'
+  name 'Kite'
+  homepage 'https://kite.com/'
+
+  auto_updates true
 
   app 'Kite.app'
 end
