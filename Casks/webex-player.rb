723be3034d86838461a5f32b2fce5a32cba19636
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-02 17:21:38 +0800

    Update webex-player.rb

--- Casks/webex-player.rb
+++ Casks/webex-player.rb
@@ -1,8 +1,8 @@
 cask 'webex-player' do
-  version :latest
-  sha256 :no_check
+  version 'T31L'
+  sha256 'fcaeaa30414b3ba1bec41ec75940321b13cd738c32ed2f350810265fa2c8cb2f'
 
-  url 'https://welcome.webex.com/client/T31L/mac/intel/webexplayer_intel.dmg'
+  url "https://welcome.webex.com/client/#{version}/mac/intel/webexplayer_intel.dmg"
   name 'WebEx Player'
   homepage 'https://www.webex.com/play-webex-recording.html'
 
