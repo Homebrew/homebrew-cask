aa964bf64bea90b31bc10917a02e329c44d6a077
ibrokemypie <ibrokemypie@bastardi.net>
2017-04-02 16:49:49 +1000

    Update deadbeef to 1.0

--- Casks/deadbeef.rb
+++ Casks/deadbeef.rb
@@ -1,10 +1,10 @@
 cask 'deadbeef' do
   version '1.0'
-  sha256 '57b1c473f4897e6ad2f20f81d53cf9441c1d50a23162d999d7eec7ee171141db'
+  sha256 '0357c19172d560c7e8eb24f12ca77fffaf29b1608cde812f145c39ede7086364'
 
   url 'https://downloads.sourceforge.net/deadbeef/travis/osx/master/deadbeef-devel-osx-x86_64.zip'
   appcast 'https://sourceforge.net/projects/deadbeef/rss?path=/travis/osx/master',
-          checkpoint: 'd5242a9bc079e0b285c8b12c69a5c2aab17c610f8683294554382f69a6ab5288'
+          checkpoint: '4ad50dde5f5017cee4a8df60104ecf48c5c4b72a8d2550d77f0b1c047c568d27'
   name 'DeaDBeeF'
   homepage 'http://deadbeef.sourceforge.net/'
 
