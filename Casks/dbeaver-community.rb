c3613afdbae3a38855f421036449022761c1bced
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-03 16:16:14 +0200

    Update dbeaver-community to 4.0.4

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,11 +1,11 @@
 cask 'dbeaver-community' do
-  version '4.0.3'
-  sha256 '6324a964024653241ba41a25b2415a2c67ddeb8e4892740b6d78a3907207094d'
+  version '4.0.4'
+  sha256 '720a743e33155b45739c294fdee8e811083e7c21bdb88cef95fefaa8b167ba53'
 
   # github.com/serge-rider/dbeaver was verified as official when first introduced to the cask
   url "https://github.com/serge-rider/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
   appcast 'https://github.com/serge-rider/dbeaver/releases.atom',
-          checkpoint: '15b3eb893d99309501e278bc76741e27d078d82e523e346b2f3d101595f26e87'
+          checkpoint: '97b301b86c5667378c45b712a0d49efb0135165de6868d5fe091ccb96f6d1971'
   name 'DBeaver Community Edition'
   homepage 'http://dbeaver.jkiss.org/'
 
