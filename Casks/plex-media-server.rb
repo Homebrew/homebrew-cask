b3b5b2277acd6c39d4f223f5b5513fff987a3d75
Vinoth Ramiah <vin047@users.noreply.github.com>
2017-04-03 16:55:12 +0100

    Update plex-media-server to 1.5.3.3580-4b377d295 (#31811)

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-server' do
-  version '1.5.2.3557-029f6ffbf'
-  sha256 '436e31d5e21866d30801e361219353355c231c1c95afd4d09eed4ad5862424be'
+  version '1.5.3.3580-4b377d295'
+  sha256 'e80ecc839b44ea2a8336091c3689f80b26681a5c09e3280b4ce7197aba655021'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   appcast 'https://plex.tv/api/downloads/1.json',
