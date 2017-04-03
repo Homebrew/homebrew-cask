f23e3fe2e575a62173cbe750cc47d9d4f4b4daf7
Reinhard Pointner <rednoah@users.noreply.github.com>
2017-04-03 10:43:53 +0000

    FileBot 4.7.9 (#31796)

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -1,11 +1,11 @@
 cask 'filebot' do
-  version '4.7.8'
-  sha256 'd58c699ecd47a6f9cec2226acf11a01bb57c07ac093eaaed31c5bc4cdda7c7b7'
+  version '4.7.9'
+  sha256 'bae8ec7ee51f3c11b8c190c14199c42a299d72755dd9e14763ca87189dd1d687'
 
   # sourceforge.net/filebot was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/filebot/filebot/FileBot_#{version}/FileBot_#{version}-darwin.tar.xz"
   appcast 'https://app.filebot.net/update.xml',
-          checkpoint: '47b0253c28bac7d5182b615cd80c8cec9f67d59b697d9331e1f1c74cdae78c6c'
+          checkpoint: 'cfc707d5378dd6e9ca479a4d20f92801e4a18ce326909ff31b99d417649a4fae'
   name 'FileBot'
   homepage 'https://www.filebot.net/'
 
