5f5326766fdcd1c1cbd90c82ef9f9e2771a67533
Vinoth Ramiah <vin047@gmail.com>
2017-04-03 16:51:23 +0100

    Add Clover Configurator.app v4.41.1
    Add Clover Configurator.app v4.41.1 to homebrew cask

---
+++ Casks/clover-configurator.rb
@@ -0,0 +1,10 @@
+cask 'clover-configurator' do
+  version '4.41.1'
+  sha256 'b766115c850e0d3b05d525ec4a8b13938693624d6bd5eb16c1f82ef16e923a8f'
+
+  url 'http://mackie100projects.altervista.org/download-mac.php?version=vibrant'
+  name 'Clover Configurator'
+  homepage 'http://mackie100projects.altervista.org/'
+
+  app 'Clover Configurator.app'
+end
