71ad2535a6f22fa1fe36c6821e7b50b2019661b9
Philip Rosenberg-Watt <p.rosenberg-watt@cablelabs.com>
2017-04-03 13:03:44 -0600

    Update yubico-authenticator to 4.0.1

--- Casks/yubico-authenticator.rb
+++ Casks/yubico-authenticator.rb
@@ -1,10 +1,10 @@
 cask 'yubico-authenticator' do
-  version '4.0.0'
-  sha256 'a87f4a341563bb06e13439a20081e616349bd89b22c1c9ba9cac34df18ae491a'
+  version '4.0.1'
+  sha256 '62d1bd5ceeefcb03c03f92c7b8d3dd8adc9c93d5d71b397e69b5def92852f6c6'
 
   url "https://developers.yubico.com/yubioath-desktop/Releases/yubioath-desktop-#{version}-mac.pkg"
   appcast 'https://developers.yubico.com/yubioath-desktop/Release_Notes.html',
-          checkpoint: '6eaf7c2a30e3991e661269db6dd6d66824023067bbbe5eb6fd4194732a49c3cd'
+          checkpoint: '05aa040249f31c3ad1d871a5663e72aa9546e7914c8a8f8e425403378434bdc0'
   name 'Yubico Authenticator'
   homepage 'https://developers.yubico.com/yubioath-desktop/'
 
