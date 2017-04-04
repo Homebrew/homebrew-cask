4fe3a583372ac2e37a96321698f5a4bdd147cf9d
Sepehr Lajevardi <lajevardi@gmail.com>
2017-04-04 12:01:11 +0430

    Add latest devcolor.

---
+++ Casks/devcolor.rb
@@ -0,0 +1,10 @@
+cask 'devcolor' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/downloads/chrisladd/DevColor/DevColor.zip'
+  name 'DevColor'
+  homepage 'https://github.com/chrisladd/DevColor'
+
+  app 'DevColor.app'
+end
