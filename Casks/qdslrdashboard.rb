6a870327c659f7782e5af88811a6bc9d98764c8b
Colin Dunn <cgd@vikingdev.net>
2017-04-04 18:28:00 +1000

    Update qdslrdashboard to 3.5.3 (#31839)

--- Casks/qdslrdashboard.rb
+++ Casks/qdslrdashboard.rb
@@ -1,6 +1,6 @@
 cask 'qdslrdashboard' do
-  version '3.5.1'
-  sha256 '7ac36ad7ce8daefa7bbddfd2b1297537716cc83e5ed0bf5cd369fe388159a9db'
+  version '3.5.3'
+  sha256 '2af31578a4b1c4203ce15a252d30ef8001aabbe9db3aa84d66ff27799eca86c7'
 
   # files.lrtimelapse.com/dslrdashboard was verified as official when first introduced to the cask
   url "http://files.lrtimelapse.com/dslrdashboard/V#{version}/qDslrDashboard_V#{version}_OSX_x64.dmg"
