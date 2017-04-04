b0222f99687516f19a7ecb4456ef50cb335a58b3
William Entriken <github.com@phor.net>
2017-04-03 22:18:37 -0400

    brew cask style --fix corelocationcli

--- Casks/corelocationcli.rb
+++ Casks/corelocationcli.rb
@@ -4,7 +4,7 @@ cask 'corelocationcli' do
 
   url "https://github.com/fulldecent/corelocationcli/releases/download/#{version}/CoreLocationCLI.zip"
   appcast 'https://github.com/fulldecent/corelocationcli/releases.atom',
-    checkpoint: '1d7c93f8ee6d06431a759358e4be2ff1578003d9e5eaa7d6f0845af773feb7ac'
+          checkpoint: '1d7c93f8ee6d06431a759358e4be2ff1578003d9e5eaa7d6f0845af773feb7ac'
   name 'Core Location CLI'
   homepage 'https://github.com/fulldecent/corelocationcli'
 
