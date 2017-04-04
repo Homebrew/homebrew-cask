13aa50a057a7c7d47f4f792c93eec36f3bc39d1b
Colin Dunn <cgd@vikingdev.net>
2017-04-05 06:52:18 +1000

    Update drawbot to 3.95 (#31853)

--- Casks/drawbot.rb
+++ Casks/drawbot.rb
@@ -1,11 +1,11 @@
 cask 'drawbot' do
-  version '3.94'
-  sha256 '6c8e43816ea05efa53439d2f2e147443d4e45aec06d7fc5676c05d49d17a2764'
+  version '3.95'
+  sha256 'cd92dd2f4f6eb886c752401623ab039adba93561dbe797e8af4bd99911d399a6'
 
   # typemytype.com/drawBot was verified as official when first introduced to the cask
   url 'http://static.typemytype.com/drawBot/DrawBot.dmg'
   appcast 'https://raw.githubusercontent.com/typemytype/drawbot/master/drawBot/drawBotSettings.py',
-          checkpoint: 'b2f58ba11dcf9e3fef254e84aed69e185e72b18236ef63be6cdf86a86ae8bb53'
+          checkpoint: '7a626a14ccca184ac5e5085005db611b6cf8f878f82dbaf02fabb24e46dfed74'
   name 'DrawBot'
   homepage 'http://www.drawbot.com/'
 
