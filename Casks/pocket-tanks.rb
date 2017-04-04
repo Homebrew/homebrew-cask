5968daaf0ead8b37632a44461d91b3e462b010ef
Steve Dignam <steve@dignam.xyz>
2017-04-03 22:12:25 -0400

    Add Pocket Tanks v1.6
    Currently there is no version in the URL however on the download page it
    states that this is version 1.6.
    
    http://www.blitwise.com/downloads.html

---
+++ Casks/pocket-tanks.rb
@@ -0,0 +1,10 @@
+cask 'pocket-tanks' do
+  version '1.6'
+  sha256 'b78aee08d32b073749f5039323a82343e85cf1dfa6ac1348a5611ab400505440'
+
+  url 'http://www.blitwise.com/ptanks_mac.dmg'
+  name 'Pocket Tanks'
+  homepage 'http://www.blitwise.com/index.html'
+
+  app 'Pocket Tanks.app'
+end
