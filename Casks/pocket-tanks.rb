259d9abce05ac6efbfc2ba932c37d0b3e293bb2e
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-04 10:16:51 +0800

    Update pocket-tanks.rb

--- Casks/pocket-tanks.rb
+++ Casks/pocket-tanks.rb
@@ -1,6 +1,6 @@
 cask 'pocket-tanks' do
-  version '1.6'
-  sha256 'b78aee08d32b073749f5039323a82343e85cf1dfa6ac1348a5611ab400505440'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.blitwise.com/ptanks_mac.dmg'
   name 'Pocket Tanks'
