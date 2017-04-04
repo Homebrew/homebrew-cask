06e16f3da5a8bd1928133848ca9fd22dfb4587a3
William Entriken <github.com@phor.net>
2017-04-03 22:08:37 -0400

    Remove appcast checkpoint

--- Casks/corelocationcli.rb
+++ Casks/corelocationcli.rb
@@ -3,8 +3,7 @@ cask 'corelocationcli' do
   sha256 'fb59410ee9182c2616303c21f19c783a8afe852b16170dfe3894b0c461bf541e'
 
   url "https://github.com/fulldecent/corelocationcli/releases/download/#{version}/CoreLocationCLI.zip"
-  appcast 'https://github.com/fulldecent/corelocationcli/releases.atom',
-          checkpoint: 'c31d218a45a4f8e685e573c441ac72aa5c53f68e9966736e95f4f3aab51b927d'
+  appcast 'https://github.com/fulldecent/corelocationcli/releases.atom'
   name 'Core Location CLI'
   homepage 'https://github.com/fulldecent/corelocationcli'
 
