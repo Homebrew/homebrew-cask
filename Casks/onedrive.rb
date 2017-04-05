7d785da6369cf13f880f1c2b0a6d03bc5a17f787
giannitm <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-04-05 18:44:30 +0100

    Update onedrive to 17.3.6798.0207

--- Casks/onedrive.rb
+++ Casks/onedrive.rb
@@ -9,5 +9,26 @@ cask 'onedrive' do
 
   pkg 'OneDrive.pkg'
 
-  uninstall pkgutil: 'com.microsoft.OneDrive'
+  uninstall pkgutil: 'com.microsoft.OneDrive',
+            quit:    [
+                       'com.microsoft.OneDrive',
+                       'com.microsoft.OneDrive.FinderSync',
+                     ],
+            delete:  '/Library/LaunchDaemons/com.microsoft.OneDriveUpdaterDaemon.plist'
+
+  zap delete: [
+                '~/Library/Caches/com.microsoft.OneDrive',
+                '~/Library/Application Scripts/com.microsoft.OneDrive.FinderSync',
+                '~/Library/Application Scripts/com.microsoft.OneDriveLauncher',
+                '~/Library/Application Support/OneDrive',
+                '~/Library/Application Support/com.microsoft.OneDrive',
+                '~/Library/Caches/com.microsoft.OneDrive',
+                '~/Library/Containers/com.microsoft.OneDrive.FinderSync',
+                '~/Library/Containers/com.microsoft.OneDriveLauncher',
+                '~/Library/Cookies/com.microsoft.OneDrive.binarycookies',
+                '~/Library/Group Containers/*.OneDriveStandaloneSuite',
+                '~/Library/Logs/OneDrive',
+                '~/Library/Preferences/com.microsoft.OneDrive.plist',
+                '~/Library/Preferences/com.microsoft.OneDriveUpdater.plist',
+              ]
 end
