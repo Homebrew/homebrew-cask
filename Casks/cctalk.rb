04accb9aa3dfafb3c0a9082de1c84569b13f1b4e
alswl <alswlx@gmail.com>
2017-04-05 13:18:19 +0800

    Update cctalk to 0.8.1-190

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,6 +1,6 @@
 cask 'cctalk' do
-  version '0.8.1-176,2017-01-12.23.37'
-  sha256 '701f597df2d21ff5c71b12fdb9311d2661dfc28c4782106cd55533519e7c4a9d'
+  version '0.8.1-190,2017-03-16.15.35'
+  sha256 'a9375653b644f2c4ea46e726939c15c4810c1dae3b290e80878b20fc89f8696e'
 
   # f1.ct.hjfile.cn was verified as official when first introduced to the cask
   url "http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/archive/#{version.before_comma.hyphens_to_dots}/CCtalk-#{version.before_comma}-hujiang-#{version.after_comma}.dmg"
