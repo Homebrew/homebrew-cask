8707e3e3d3c29b52b3ca58c15db500be52413464
Colin Dunn <dunnc@amazon.com>
2017-04-05 13:24:05 +1000

    Update electrum to 2.8.2

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,10 +1,10 @@
 cask 'electrum' do
-  version '2.8.1'
-  sha256 '2f1fa3e4e9ced6aa3836c8400ea05b7d53c240ace68d7e732fdc1930ac8a7f1d'
+  version '2.8.2'
+  sha256 '1ebe0772fcfa1d04761d23174d1793009cf16b75b90034d8e384f9cc3fd29077'
 
   url "https://download.electrum.org/#{version}/electrum-#{version}.dmg"
   appcast 'https://github.com/spesmilo/electrum/releases.atom',
-          checkpoint: '0fc587b748542e8b997b002ebe2c107145458d7f0412bb9f057a6413035b045a'
+          checkpoint: '3f13cfabbecbd6b6804f125c966e40a8c2dc733a9e9e8e7530fc13cbedbdcbae'
   name 'Electrum'
   homepage 'https://electrum.org/'
   gpg "#{url}.asc", key_id: '6694d8de7be8ee5631bed9502bd5824b7f9470e6'
