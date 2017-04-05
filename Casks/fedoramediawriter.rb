3f23c6c080dc3d7c9b15ab00362e81a1279f173f
Yurii Kolesnykov <yurikoles@gmail.com>
2017-04-05 19:07:37 +0300

    fedoramediawriter: new formulae

---
+++ Casks/fedoramediawriter.rb
@@ -0,0 +1,11 @@
+cask 'fedoramediawriter' do
+  version '4.0.8'
+  sha256 '6f64f2ba333033f839981cb4435662fc299a8bdc45e895bc33ffd9753f675f62'
+
+  # getfedora.org/fmw/ was verified as official when first introduced to the cask
+  url "https://getfedora.org/fmw/FedoraMediaWriter-osx-#{version}.dmg"
+  name 'Fedora Media Writer'
+  homepage 'https://fedoraproject.org/wiki/How_to_create_and_use_Live_USB'
+
+  app 'Fedora Media Writer.app'
+end
