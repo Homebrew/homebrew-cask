4a0dd14f07492bd421927cf61cca6d1d60337ed4
Aoshi <deepblue.will@gmail.com>
2017-04-05 10:05:57 +0900

    Change url of prott (#31887)
    * Change url of prott latest
    
    * Update prott.rb

--- Casks/prott.rb
+++ Casks/prott.rb
@@ -1,8 +1,10 @@
 cask 'prott' do
-  version :latest
-  sha256 :no_check
+  version '1.0.0'
+  sha256 'c1c3c76608f669791edfddb7237fd672d8c52d4301cbd9b9fbc505c707a7afd7'
 
-  url 'https://prottapp.com/app/gadgets/prott.dmg'
+  url 'https://dl.prottapp.com/apps/prott.dmg'
+  appcast 'https://dl.prottapp.com/apps/appcast.xml',
+          checkpoint: 'a6878656337db7d1542331666345fb8b080780c1d2618b9143369759187b0227'
   name 'Prott'
   homepage 'https://prottapp.com/'
 
