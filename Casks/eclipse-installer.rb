0ec5a109cc7cc420f6a4438cc98f11f0b346dac4
Colin Dunn <cgd@vikingdev.net>
2017-04-05 14:51:57 +1000

    Update eclipse-installer to 4.6.3,neon:3 (#31897)

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-installer' do
-  version '4.6.2,neon:2a'
-  sha256 '8ff79e95be4bc8f88d75723cd068bc37545eb78f297eed0c544f9830b008fc3b'
+  version '4.6.3,neon:3'
+  sha256 '4b40a694b998c1b8f5819837ae50bdd9f7113d9c410760d966eb47d2aa2b4ddf'
 
   url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.after_comma.before_colon}/R#{version.after_colon}/eclipse-inst-mac64.tar.gz&r=1"
   name 'Eclipse Installer'
