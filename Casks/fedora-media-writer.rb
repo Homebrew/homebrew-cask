2a19a44b1a882fc3de8ba912ee9b50cb9a441cc0
Vítor Galvão <info@vitorgalvao.com>
2017-04-05 18:03:22 +0100

    Update and rename fedoramediawriter.rb to fedora-media-writer.rb

--- Casks/fedora-media-writer.rb
+++ Casks/fedora-media-writer.rb
@@ -1,8 +1,8 @@
-cask 'fedoramediawriter' do
+cask 'fedora-media-writer' do
   version '4.0.8'
   sha256 '6f64f2ba333033f839981cb4435662fc299a8bdc45e895bc33ffd9753f675f62'
 
-  # getfedora.org/fmw/ was verified as official when first introduced to the cask
+  # getfedora.org/fmw was verified as official when first introduced to the cask
   url "https://getfedora.org/fmw/FedoraMediaWriter-osx-#{version}.dmg"
   name 'Fedora Media Writer'
   homepage 'https://fedoraproject.org/wiki/How_to_create_and_use_Live_USB'
