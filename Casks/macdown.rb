412f4887bd45bdaebb9c8383fd55852f048cde0f
Jean-Philippe Roemer <jp@roemer.im>
2017-04-06 00:08:55 +0200

    macdown: add artifact to zap & update appcast url

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -4,18 +4,22 @@ cask 'macdown' do
 
   # github.com/MacDownApp/macdown was verified as official when first introduced to the cask
   url "https://github.com/MacDownApp/macdown/releases/download/v#{version}/MacDown.app.zip"
-  appcast 'https://github.com/MacDownApp/macdown/releases.atom',
-          checkpoint: '494e6ec6883fd528a9b1905aa3270dfd0361143bfae5bec6b50284b22ef1f966'
+  appcast 'http://macdown.uranusjr.com/sparkle/macdown/stable/appcast.xml',
+          checkpoint: 'b5d3dd1f4690778a82675d110ff4699aecbd29a6597214492756d1e8d38310f6'
   name 'MacDown'
   homepage 'https://macdown.uranusjr.com/'
+
+  auto_updates true
 
   app 'MacDown.app'
   binary "#{appdir}/MacDown.app/Contents/SharedSupport/bin/macdown"
 
   zap delete: [
                 '~/Library/Caches/com.uranusjr.macdown',
+                '~/Library/Cookies/com.uranusjr.macdown.binarycookies',
                 '~/Library/Preferences/com.uranusjr.macdown.plist',
                 '~/Library/Preferences/com.uranusjr.macdown.LSSharedFileList.plist',
                 '~/Library/Application Support/MacDown',
+                '~/Library/WebKit/com.uranusjr.macdown',
               ]
 end
