b0a04f2400c6fa0f4ba17ecd351c520940a293fa
Vítor Galvão <info@vitorgalvao.com>
2017-04-05 12:27:14 +0100

    travis: rvm stable now suffices

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -13,7 +13,7 @@ header 'Running before_script.sh...'
 # Required workarounds
 if ! run command -v gpg2 &>/dev/null; then run brew unlink gnupg && run brew install gpg2; fi # After El Capitan is no longer needed, this can likely be removed
 run gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 # Required by RVM: https://rvm.io/rvm/security
-run rvm get head # Required due to Travis bug: https://github.com/travis-ci/travis-ci/issues/6307#issuecomment-233315824
+run rvm get stable # Required due to Travis bug: https://github.com/travis-ci/travis-ci/issues/6307#issuecomment-233315824
 
 # see https://github.com/travis-ci/travis-ci/issues/2666
 run export BRANCH_COMMIT="${TRAVIS_COMMIT_RANGE##*.}"
