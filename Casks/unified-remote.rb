c262d7301912f083174f1212e50adf249d130236
tiiiecherle <tiiiecherle@gmail.com>
2017-04-06 15:20:35 +0200

    Update unified-remote to 3.6.0.674

--- Casks/unified-remote.rb
+++ Casks/unified-remote.rb
@@ -1,6 +1,6 @@
 cask 'unified-remote' do
-  version :latest
-  sha256 :no_check
+  version '3.6.0.674'
+  sha256 '3e22e900ab54f7b0b602b8858669e1ddcd0414db15008ecb0d0186425dd8438d'
 
   url 'https://www.unifiedremote.com/download/mac'
   name 'Unified Remote'
