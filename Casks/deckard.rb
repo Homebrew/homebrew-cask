fcd0b90279c1eee4ed13e18bd1139afc8eace26e
Victor Popkov <victor@popkov.me>
2017-04-06 22:46:09 +0300

    Update deckard to not use SSL

--- Casks/deckard.rb
+++ Casks/deckard.rb
@@ -2,11 +2,11 @@ cask 'deckard' do
   version '0.6.2'
   sha256 '053aef4eb4cf0774f43dbc87349580a2c03cc6bbcb0f81f50f7081bdda0e05a5'
 
-  url "https://deckard.ai/download/#{version}.dmg"
-  appcast 'https://deckard.ai/releases.html',
+  url "http://deckard.ai/download/#{version}.dmg"
+  appcast 'http://deckard.ai/releases.html',
           checkpoint: '9ba55546518333367d58ae14849ee1a4a4a401416b798dde553bf40e837a7c9a'
   name 'Deckard Assist'
-  homepage 'https://deckard.ai/'
+  homepage 'http://deckard.ai/'
 
   auto_updates true
 
