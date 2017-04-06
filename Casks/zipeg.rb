c9a17a4d168f341967daa3372d0dd64ca5a015ed
tiiiecherle <tiiiecherle@gmail.com>
2017-04-06 15:21:41 +0200

    Update zipeg to 2.9.4.1316

--- Casks/zipeg.rb
+++ Casks/zipeg.rb
@@ -1,6 +1,6 @@
 cask 'zipeg' do
-  version :latest
-  sha256 :no_check
+  version '2.9.4.1316'
+  sha256 '2d619ed6b7b82035bd31fa1ffbcae3d11f0e6e90878f826bf0ba50d158f38c14'
 
   url 'http://www.zipeg.com/downloads/zipeg_mac.dmg'
   name 'Zipeg'
