1800a1ac0652a9c32a4560059cc3d59382c829e0
Gobinath Mallaiyan <i@gobinath.com>
2017-04-06 13:05:13 -0400

    Update bordertool to latest

--- Casks/bordertool.rb
+++ Casks/bordertool.rb
@@ -2,9 +2,12 @@ cask 'bordertool' do
   version :latest
   sha256 :no_check
 
-  url 'http://xvi.rpc1.org/BorderTool.zip'
+  # xvi.rpc1.free.fr was verified as official when first introduced to the cask
+  url 'http://xvi.rpc1.free.fr/BorderTool.zip'
   name 'BorderTool'
   homepage 'http://xvi.rpc1.org/'
+
+  auto_updates true
 
   app 'BorderTool.app'
 end
