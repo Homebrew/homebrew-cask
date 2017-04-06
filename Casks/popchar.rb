82d7d5ff8231272dafff76bf26e16071754d7965
Alissa Shim <alissa@Alissas-MacBook-Pro.local>
2017-04-06 07:16:33 -0400

    Update popchar to 7.7

--- Casks/popchar.rb
+++ Casks/popchar.rb
@@ -1,8 +1,11 @@
 cask 'popchar' do
-  version '7.5'
-  sha256 'f991f2be60e1409209cc1921c1a2c2a7f2e6281a7275e52c6e1bbb8597a5c83a'
+  version '7.7'
+  sha256 'b0428f4ffa6630bdc3f3a54ea3c872934caf3631b99c47d92264382d82ccafd2'
 
-  url "http://www.ergonis.com/downloads/products/popcharx/PopCharX#{version.no_dots}-Install.dmg"
+  url "http://www.ergonis.com/downloads/products/popcharx/PopCharX#{version.no_dots}-Install.dmg",
+      user_agent: :fake
+  appcast 'http://www.ergonis.com/products/popcharx/history.html',
+          checkpoint: '84690fe42a9e8ef619366b9a0dad6b774f43c76171b7d88028aed91670f45bce'
   name 'PopChar X'
   homepage 'http://www.ergonis.com/products/popcharx/'
 
