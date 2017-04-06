2b49044e7b53f04a095a674f109c63f4ddeeaa78
neutric <neutric@users.noreply.github.com>
2017-04-06 20:08:37 +0200

    dropbox: remove appcast
    What hasn't been updated for months is now finally gone.

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -4,8 +4,6 @@ cask 'dropbox' do
 
   # clientupdates.dropboxstatic.com was verified as official when first introduced to the cask
   url "https://clientupdates.dropboxstatic.com/client/Dropbox%20#{version}.dmg"
-  appcast 'https://www.dropbox.com/release_notes/rss.xml',
-          checkpoint: '2794637a8b028d903f0d27432291dce968886923a86325994156cf1206edec80'
   name 'Dropbox'
   homepage 'https://www.dropbox.com/'
 
