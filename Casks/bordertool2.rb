556b7220ff73da426cab1e6e07a7b5dc15f4841d
Vítor Galvão <info@vitorgalvao.com>
2017-04-06 22:35:42 +0100

    Update bordertool2.rb

--- Casks/bordertool2.rb
+++ Casks/bordertool2.rb
@@ -3,10 +3,9 @@ cask 'bordertool2' do
   version :latest
   sha256 :no_check
 
-  # xvi.rpc1.free.fr was verified as official when first introduced to the cask
   url 'http://xvi.rpc1.free.fr/BorderTool%202.zip'
   name 'BorderTool 2'
-  homepage 'http://xvi.rpc1.org/'
+  homepage 'http://xvi.rpc1.free.fr/'
   
   auto_updates true
 
