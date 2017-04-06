c31b801ff0ea9cd728c6194ba47335597dc29a50
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-04-06 19:14:52 +0900

    Add honto v6.20.0

---
+++ Casks/honto.rb
@@ -0,0 +1,10 @@
+cask 'honto' do
+  version '6.20.0'
+  sha256 'ae8ba586a0e9125498bd0337744bb6a66bdd281f45dd6f1d54a3044a06df43c2'
+
+  url 'https://dl.honto.jp/macapp/honto.dmg'
+  name 'honto view app'
+  homepage 'https://honto.jp/ebook/dlinfo.html'
+
+  app 'honto.app'
+end
