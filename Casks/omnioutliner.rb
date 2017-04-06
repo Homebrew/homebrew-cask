48277238da6b1dfcc43915d2810a2bab272835e4
Chris Finazzo <chris.finazzo@gmail.com>
2017-04-05 20:12:37 -0400

    Use the correct URL

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -2,7 +2,7 @@ cask 'omnioutliner' do
   version '5.0.1'
   sha256 '6b9e85db62565e804562701b16dfa30e51469d54e6c8faacb7dd1aee6b293c95'
 
-  url "https://downloads.omnigroup.com/software/MacOSX/10.10/OmniOutliner-#{version}.dmg"
+  url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniOutliner-#{version}.dmg"
   name 'OmniOutliner'
   homepage 'https://www.omnigroup.com/omnioutliner/'
 
