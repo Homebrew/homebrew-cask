b70e5a3d37b71eaf78316910bb69e752190d3c94
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-07 19:35:10 -0400

    Update espresso to 3.0 (#32079)
    * Update espresso to 3.0
    
    * Update espresso.rb

--- Casks/espresso.rb
+++ Casks/espresso.rb
@@ -1,8 +1,9 @@
 cask 'espresso' do
   version '3.0'
-  sha256 '6d93eb9648413eeaac91d4237570a24554f1b3104c3e272a31188e2ebdd864e1'
+  sha256 '48f4af866e75f163770ee3edc48af1a8e2d214ad49a32d94a198ee6e8c0a5104'
 
-  url 'http://presto.espressoapp.com/downloads/Espresso%20v3.zip'
+  # static.macrabbit.com was verified as official when first introduced to the cask
+  url "https://static.macrabbit.com/downloads/Espresso%20v#{version.major}.zip"
   appcast "https://update.macrabbit.com/espresso/#{version}.xml",
           checkpoint: '900b7d97f5d419eb7cd390b6416043fa57763a233ea2482227d39764fc17acb1'
   name 'Espresso'
