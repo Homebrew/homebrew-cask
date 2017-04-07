4c62dc31352a0bf54fa4dfdfff774e23b6dd7826
Teppei Fukuda <teppei@elab.ic.i.u-tokyo.ac.jp>
2017-04-07 19:23:01 +0900

    Update burp-suite to 1.7.20 (#32075)

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -1,6 +1,6 @@
 cask 'burp-suite' do
-  version '1.7.19'
-  sha256 '0ee69e71319c6279f7c47baf1b9a3000350b3349a854374191bd76cd7ad7e6fa'
+  version '1.7.20'
+  sha256 'ac576e85e7e02b6b4bd182198dac29289c171d6e5bf0dffafe1321b67a066364'
 
   url "https://portswigger.net/Burp/Releases/Download?productId=100&version=#{version}&type=MacOsx"
   name 'Burp Suite'
