a7b4af39b5322dba4ff1fbd271dc1fd2683f243a
Gobinath Mallaiyan <i@gobinath.com>
2017-04-07 12:25:35 -0400

    Update Eclipse Platform Cask
    - Updating the cask to avoid conflicts with other eclipse casks

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -8,5 +8,6 @@ cask 'eclipse-platform' do
 
   depends_on macos: '>= :leopard'
 
-  app 'Eclipse.app'
+  # Renamed to avoid conflict with other Eclipse.
+  app 'Eclipse.app', target: 'Eclipse Platform.app'
 end
