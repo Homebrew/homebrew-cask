75520f2ab4ff8c2df40c754063fc1e1a6dbfd64d
Colin Dunn <cgd@vikingdev.net>
2017-04-07 11:55:44 +1000

    Update makehuman to 1.1.1 (#32041)

--- Casks/makehuman.rb
+++ Casks/makehuman.rb
@@ -1,9 +1,11 @@
 cask 'makehuman' do
-  version '1.1.0'
-  sha256 'a16f3aa66b553a60c57e3e973dba5be559493a111251db057c38cf300a3d762b'
+  version '1.1.1'
+  sha256 '0f5626b1878f8f4233be431917e740806b141522f9f4b3da7809e8e7798ee2d8'
 
   # tuxfamily.org/makehuman was verified as official when first introduced to the cask
   url "https://download.tuxfamily.org/makehuman/releases/#{version}/makehuman-#{version}-osx.dmg"
+  appcast 'https://download.tuxfamily.org/makehuman/releases/',
+          checkpoint: '75064672d3c4afcf777057fba4176be9c6ccd36c15f52ca4e736abd273c24068'
   name 'MakeHuman'
   homepage 'http://www.makehuman.org/'
 
