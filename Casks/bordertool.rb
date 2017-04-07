62e44e11c6a12f329f78185332b6e4d2cb516baf
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-06 20:02:40 -0400

    Update bordertool to latest (#32006)
    * Update bordertool to latest
    
    * Update bordertool.rb

--- Casks/bordertool.rb
+++ Casks/bordertool.rb
@@ -2,9 +2,11 @@ cask 'bordertool' do
   version :latest
   sha256 :no_check
 
-  url 'http://xvi.rpc1.org/BorderTool.zip'
+  url 'http://xvi.rpc1.free.fr/BorderTool.zip'
   name 'BorderTool'
-  homepage 'http://xvi.rpc1.org/'
+  homepage 'http://xvi.rpc1.free.fr/'
+
+  auto_updates true
 
   app 'BorderTool.app'
 end
