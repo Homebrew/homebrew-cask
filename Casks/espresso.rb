274ff2a77b1b23944d2d8f99cca31f24c8c36882
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-08 06:47:02 +0800

    Update espresso.rb

--- Casks/espresso.rb
+++ Casks/espresso.rb
@@ -3,7 +3,7 @@ cask 'espresso' do
   sha256 '48f4af866e75f163770ee3edc48af1a8e2d214ad49a32d94a198ee6e8c0a5104'
 
   # static.macrabbit.com was verified as official when first introduced to the cask
-  url 'https://static.macrabbit.com/downloads/Espresso%20v3.zip'
+  url "https://static.macrabbit.com/downloads/Espresso%20v#{version.major}.zip"
   appcast "https://update.macrabbit.com/espresso/#{version}.xml",
           checkpoint: '900b7d97f5d419eb7cd390b6416043fa57763a233ea2482227d39764fc17acb1'
   name 'Espresso'
