494fa5221cbfcf94b284c9139144007d7541d9bc
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-07 18:48:04 -0400

    Update operadriver to 2.27 (#32081)

--- Casks/operadriver.rb
+++ Casks/operadriver.rb
@@ -1,10 +1,10 @@
 cask 'operadriver' do
-  version '0.2.2'
-  sha256 '6584ea2b0ebb06a2c5639570fdac0a28de90bb88982ce9e74d531147106988bd'
+  version '2.27'
+  sha256 '2e30a9ab0c05329db8181d8dbc17b89491f7b2504a49a32d4ae2caa907b51be4'
 
-  url "https://github.com/operasoftware/operachromiumdriver/releases/download/v#{version}/operadriver_mac64.zip"
+  url "https://github.com/operasoftware/operachromiumdriver/releases/download/v.#{version}/operadriver_mac64.zip"
   appcast 'https://github.com/operasoftware/operachromiumdriver/releases.atom',
-          checkpoint: 'a17c9b85ec515b393f9b29c6bf27e7f3103d92e7fa3e079a9ba41ce145d3ddf3'
+          checkpoint: '4ea8096bf0d539fcbadd869cd5acdaf8efdccee94553218863a903c439f1099e'
   name 'operachromiumdriver'
   homepage 'https://github.com/operasoftware/operachromiumdriver'
 
