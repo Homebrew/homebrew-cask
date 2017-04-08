109525db43b62b1a2b1ae39c3e60819384b5e813
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-09 06:47:25 +0800

    Update autopano-giga.rb

--- Casks/autopano-giga.rb
+++ Casks/autopano-giga.rb
@@ -2,7 +2,7 @@ cask 'autopano-giga' do
   version '4.4.0.400,2017-03-25'
   sha256 '836a0b67cd4a6553efc6f19d7440561ea4c521952949551a4229f13583588682'
 
-  url "http://cdn-download.kolor.com/apg/#{version.before_comma}_#{version.after_comma}/AutopanoGiga_Mac_#{version.major_minor_patch_no_dots}_#{version.after_comma}.dmg"
+  url "http://cdn-download.kolor.com/apg/#{version.before_comma}_#{version.after_comma}/AutopanoGiga_Mac_#{version.major_minor_patch.no_dots}_#{version.after_comma}.dmg"
   name 'Autopano Giga'
   homepage 'http://www.kolor.com/autopano/'
 
