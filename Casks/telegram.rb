dce755e805d93d1c54e044c872b4a333a95536d6
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-08 18:54:31 -0400

    Update telegram to latest version

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '2.30-51442'
-  sha256 'db28d47e8337eb417e4c75e6cc1650ccd3f34cd0ed6cffcebafd489f88336cac'
+  version '2.92-91700'
+  sha256 'e4039991e0bc7aa29276d70a01423649439b30fd9a338a2fa9fe4f96d3450cea'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml',
