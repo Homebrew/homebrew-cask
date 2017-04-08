eccd649c34b97af3a1176bf231dcf44abb6f8ace
Weilu Jia <weilu.jia@gusto.com>
2017-04-08 00:20:58 -0700

    Update openttd to 1.7.0

--- Casks/openttd.rb
+++ Casks/openttd.rb
@@ -1,6 +1,6 @@
 cask 'openttd' do
-  version '1.6.1'
-  sha256 'b6faabfdaa9aba3f82537fd93ea128e45f735308e675c455657eb3fcb96265d7'
+  version '1.7.0'
+  sha256 '208e2f876450fdac4caf4e5f5b16714cef3e9ac3047f465cdddbf8d03567dcb2'
 
   url "http://binaries.openttd.org/releases/#{version}/openttd-#{version}-macosx-universal.zip"
   name 'OpenTTD'
