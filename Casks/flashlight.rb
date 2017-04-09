d1eefbd87143aad6e6f76f45d9cea4d2166bc9c7
Vinoth Ramiah <vin047@gmail.com>
2017-04-09 15:56:36 +0100

    Update flashlight to 1.0.6

--- Casks/flashlight.rb
+++ Casks/flashlight.rb
@@ -1,11 +1,11 @@
 cask 'flashlight' do
-  version '1.0.4'
-  sha256 '2040a268be730cc5cff603341d13b58a671132c74bee8dc8cc5662ea95466822'
+  version '1.0.6'
+  sha256 'b324c20ed13afae8e060c2e4ed09a2ffd7617b022ed4da271b4e2a793ad6fa52'
 
   # githubusercontent.com/w0lfschild/app_updates/master/Flashlight was verified as official when first introduced to the cask
   url "https://raw.githubusercontent.com/w0lfschild/app_updates/master/Flashlight/Flashlight_#{version}.zip"
   appcast 'https://raw.githubusercontent.com/w0lfschild/app_updates/master/Flashlight/appcast.xml',
-          checkpoint: 'd81c3766258fcb1f424cb93ae3ceb5938d157aa3c4b90bf88ab9304c92aaf794'
+          checkpoint: '33b12bbb0c79e43759ffece9a1bf4168e65fbf89178c214fd16647e579951f3c'
   name 'Flashlight'
   homepage 'https://github.com/w0lfschild/Flashlight'
 
