c57bcbde8e194430a2b659c09f71633f9c6386fb
commitay <commitay@users.noreply.github.com>
2017-04-10 12:53:07 +1000

    miniconda

--- Casks/miniconda.rb
+++ Casks/miniconda.rb
@@ -10,8 +10,10 @@ cask 'miniconda' do
   depends_on macos: '>= :lion'
   container type: :naked
 
-  installer script: 'Miniconda3-latest-MacOSX-x86_64.sh',
-            args:   ['-b']
+  installer script: {
+                      executable: 'Miniconda3-latest-MacOSX-x86_64.sh',
+                      args:       ['-b'],
+                    }
 
   uninstall delete: '~/miniconda3'
 
