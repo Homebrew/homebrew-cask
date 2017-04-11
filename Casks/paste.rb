1bf526558c165a3fe0af0f942b9873119fee2126
Vítor Galvão <info@vitorgalvao.com>
2017-04-11 19:58:59 +0100

    Update paste.rb

--- Casks/paste.rb
+++ Casks/paste.rb
@@ -3,8 +3,8 @@ cask 'paste' do
   sha256 '131e1a2c06e88c1532570e4342073b043023fd049508b31be9d90a93f6defab1'
 
   # rink.hockeyapp.net/api was verified as official when first introduced to the cask
-  url "https://rink.hockeyapp.net/api/#{version.major}/apps/ee24d1a939cd4ff8b2861eb8c788a995/app_versions/6?format=zip"
-  appcast "https://rink.hockeyapp.net/api/#{version.major}/apps/ee24d1a939cd4ff8b2861eb8c788a995",
+  url "https://rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995/app_versions/6?format=zip"
+  appcast "https://rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995",
           checkpoint: 'c1c8499b83a3f44b49f8985d7c508cea576cca12c8619720491c33f58458d06f'
   name 'Paste'
   homepage 'http://pasteapp.me/'
