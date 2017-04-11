4ae025bb77effece628cac1dc2743be972e58b40
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-11 09:23:38 -0400

    Update deepanpot-au to 2.0.0 (#32200)

--- Casks/deepanpot-au.rb
+++ Casks/deepanpot-au.rb
@@ -1,8 +1,8 @@
 cask 'deepanpot-au' do
-  version :latest
-  sha256 :no_check
+  version '2.0.0'
+  sha256 '4a63172f715cd912cbc7f3439855f0878a8579e27c798b89513cc10315e6bb60'
 
-  url 'http://dotec-audio.com/release/DeePanpot/latest/au/DeePanpotAU.zip'
+  url "https://dotec-audio.com/release/DeePanpot/DeePanpotMac_#{version}.zip"
   name 'DOTEC-AUDIO DeePanpot (AU)'
   homepage 'https://dotec-audio.com/deepanpot.html'
 
