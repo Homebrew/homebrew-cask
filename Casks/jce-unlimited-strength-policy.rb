372ec5bd7d143c9b466d3e192acc800bffbda2cc
Indrajit Raychaudhuri <irc+code@indrajit.com>
2017-04-10 22:09:30 -0500

    jce-unlimited-strength-policy: simplify version matching
    No need for `version.split('.')[1]`, when handy `version.minor` is around.

--- Casks/jce-unlimited-strength-policy.rb
+++ Casks/jce-unlimited-strength-policy.rb
@@ -2,10 +2,10 @@ cask 'jce-unlimited-strength-policy' do
   version '1.8'
   sha256 'f3020a3922efd6626c2fff45695d527f34a8020e938a49292561f18ad1320b59'
 
-  url "http://download.oracle.com/otn-pub/java/jce/#{version.split('.')[1]}/jce_policy-#{version.split('.')[1]}.zip",
+  url "http://download.oracle.com/otn-pub/java/jce/#{version.minor}/jce_policy-#{version.minor}.zip",
       cookies: { 'oraclelicense' => 'accept-securebackup-cookie' }
   name 'Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files'
-  homepage "https://www.oracle.com/technetwork/java/javase/downloads/jce#{version.split('.')[1]}-download-2133166.html"
+  homepage "https://www.oracle.com/technetwork/java/javase/downloads/jce#{version.minor}-download-2133166.html"
 
   postflight do
     `/usr/libexec/java_home -v #{version} -X | grep -B0 -A1 JVMHomePath | sed -n -e 's/[[:space:]]*<string>\\(.*\\)<\\/string>/\\1/p'`.split("\n").each do |path|
@@ -18,11 +18,11 @@ cask 'jce-unlimited-strength-policy' do
                      sudo: true
 
       system_command '/bin/ln',
-                     args: ['-nsf', "#{staged_path}/UnlimitedJCEPolicyJDK#{version.split('.')[1]}/US_export_policy.jar", "#{path}/jre/lib/security/US_export_policy.jar"],
+                     args: ['-nsf', "#{staged_path}/UnlimitedJCEPolicyJDK#{version.minor}/US_export_policy.jar", "#{path}/jre/lib/security/US_export_policy.jar"],
                      sudo: true
 
       system_command '/bin/ln',
-                     args: ['-nsf', "#{staged_path}/UnlimitedJCEPolicyJDK#{version.split('.')[1]}/local_policy.jar", "#{path}/jre/lib/security/local_policy.jar"],
+                     args: ['-nsf', "#{staged_path}/UnlimitedJCEPolicyJDK#{version.minor}/local_policy.jar", "#{path}/jre/lib/security/local_policy.jar"],
                      sudo: true
     end
   end
