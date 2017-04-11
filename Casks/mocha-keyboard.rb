5370c833e802836addf7fa117c20741309adfa55
Jannes Jeising <jannes@jeising.net>
2017-04-11 15:29:25 +0200

    Add mocha-keyboard (latest)

---
+++ Casks/mocha-keyboard.rb
@@ -0,0 +1,10 @@
+cask 'mocha-keyboard' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.mochasoft.dk/mochakeyboard.dmg.zip'
+  name 'Mocha Keyboard'
+  homepage 'https://www.mochasoft.dk/mochakeyboard.htm'
+
+  app 'Mocha Keyboard.app'
+end
