73a7525a49a66a286b05ec49c038a04dcc4aa23b
Sergei Vorobev <svorobev@dropbox.com>
2017-04-11 12:23:51 -0700

    Update powershell.rb to alpha 18 release

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -1,17 +1,17 @@
 cask 'powershell' do
-  version '6.0.0-alpha.17'
-  sha256 'b25b4a9e40ad8d036931ef1f4292805fec048c1ab26f5d9983650784f87d7bc3'
+  version '6.0.0-alpha.18'
+  sha256 '486be09e5f20fbbbda8a1a35b3959630f805dc3d5ab1c1b77d7af3e2bd234d7b'
 
   # github.com/PowerShell/PowerShell was verified as official when first introduced to the cask
-  url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}.pkg"
+  url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx.10.11-x64.pkg"
   appcast 'https://github.com/PowerShell/PowerShell/releases.atom',
-          checkpoint: 'b29c39526841c258d0476c9de9beae54c589f2cf337af93d02e55d3d9722f74f'
+          checkpoint: 'edd757730a98d4e41ea656cfa009ef80b2a3223f13dc745af633e3c850de1c4f'
   name 'PowerShell'
   homepage 'https://msdn.microsoft.com/powershell'
 
   depends_on formula: 'openssl'
 
-  pkg "powershell-#{version}.pkg"
+  pkg "powershell-#{version}-osx.10.11-x64.pkg"
 
   uninstall pkgutil: 'powershell'
 
