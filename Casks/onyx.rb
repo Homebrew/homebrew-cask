518a5b206429c002fe76952fb390c7d06b511f54
Jeremy Saklad <saklad5@me.com>
2017-04-11 14:58:09 -0500

    Update onyx to 3.2.5

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -3,13 +3,13 @@ cask 'onyx' do
     version '3.1.8'
     sha256 '9d46d09346666a4dcecd0c92eb1bebd76c76f145744926ebda594831d45724ab'
   else
-    version '3.2.4'
+    version '3.2.5'
     sha256 '068b9d4df199727ecf750879b943efc9b5813aab318cca05f937be7f9a075d81'
   end
 
   url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete('.')}/OnyX.dmg"
   appcast 'http://www.titanium-software.fr/en/release_onyx.html',
-          checkpoint: '91046a29eec018e16d37414affd7d2fd898e650934f51918324b3e163ef1978f'
+          checkpoint: 'dbf3a781dd00731b9d9e00d72580d1b32c2caefdac2b846b1540bf1d4d97cda2'
   name 'OnyX'
   homepage 'https://www.titanium-software.fr/en/onyx.html'
 
