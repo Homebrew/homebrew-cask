07957a66a01cf3fdc86dfff8ceb6fa2cb22526dc
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-11 15:25:02 -0400

    Update zulip download url (#32223)

--- Casks/zulip.rb
+++ Casks/zulip.rb
@@ -2,8 +2,7 @@ cask 'zulip' do
   version :latest
   sha256 :no_check
 
-  # zulip.com was verified as official when first introduced to the cask
-  url 'https://zulip.com/dist/apps/mac/Zulip-latest.dmg'
+  url 'https://zulip.org/dist/apps/mac/Zulip-latest.dmg'
   name 'Zulip'
   homepage 'https://www.zulip.org/'
 
