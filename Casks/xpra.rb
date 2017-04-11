cf8676efadd816fc6ee6cce1398de146af8803b0
Gobinath Mallaiyan <i@gobinath.com>
2017-04-11 12:28:35 -0400

    Update xpra to latest

--- Casks/xpra.rb
+++ Casks/xpra.rb
@@ -2,7 +2,7 @@ cask 'xpra' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.xpra.org/dists/osx/x86/Xpra.dmg'
+  url 'https://www.xpra.org/dists/osx/x86_64/Xpra-x86_64.pkg'
   name 'Xpra'
   homepage 'https://www.xpra.org/'
 
