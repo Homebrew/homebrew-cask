50195a0ad88399631b729a7bc6922d1f64c1f825
Victor Popkov <victor@popkov.me>
2017-04-12 00:11:58 +0300

    Update caption to 1.2.1 (#32232)

--- Casks/caption.rb
+++ Casks/caption.rb
@@ -1,11 +1,11 @@
 cask 'caption' do
-  version '1.1.0'
-  sha256 '311142b4aa1418bfc680b9de4287f89ce731a55a2df16b3cd6624a0d284fe40a'
+  version '1.2.1'
+  sha256 '1751a982812a76e9c0a250659f326b5bb677ad4701aa97b4b0a04b069f72e465'
 
   # github.com/gielcobben/Caption was verified as official when first introduced to the cask
   url "https://github.com/gielcobben/Caption/releases/download/v#{version}/Caption-v#{version}-darwin_x64.dmg"
   appcast 'https://github.com/gielcobben/Caption/releases.atom',
-          checkpoint: '33b62b058b92a2bcf0f91ab3a42d62b787fa41ed54ff7a518fc4418ebc22552a'
+          checkpoint: '281fc39c69bbfec6c43e7008592939292753b83e29a52ec5aa3821ef2b737c51'
   name 'Caption'
   homepage 'https://getcaption.co/'
 
