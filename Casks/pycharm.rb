7e580bb76dd03be30dab8cddce94bbd3d36bda10
jcbot <jcb@leipert.io>
2017-04-12 15:50:57 +0200

    Update pycharm to 2017.1.1,171.4163.6

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,10 +1,10 @@
 cask 'pycharm' do
-  version '2017.1,171.3780.115'
-  sha256 '9890a90274acde0bdfa0a559d47ca3d4f59a08035333a60875fbf0b57ab29570'
+  version '2017.1.1,171.4163.6'
+  sha256 '5056d7d2fc0148f02909302e07e37b44e09f186a19f806e26cca0f5b3912652a'
 
   url "https://download.jetbrains.com/python/pycharm-professional-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=PCP&latest=true&type=release',
-          checkpoint: 'fee227c74f9450b3192675ce9fa25a276437d14585aa31cf74911546658adf8a'
+          checkpoint: 'bb9679dbc3a7a9d25a9691e1ffb03531ca63bf26a8167bdc2dd5247fc61c5332'
   name 'PyCharm'
   homepage 'https://www.jetbrains.com/pycharm/'
 
