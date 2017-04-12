f31f5bc6919da473018b97f0501165a121942fb0
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-12 02:41:11 -0400

    Update paste to 2.2.1 (#32226)
    * Update paste to 2.2.1
    
    * Update paste.rb
    
    * Update paste.rb
    
    * Update paste cask
    
    - Roll Back Vendor Comment

--- Casks/paste.rb
+++ Casks/paste.rb
@@ -1,11 +1,11 @@
 cask 'paste' do
-  version '2.0.0'
-  sha256 '9e6983c2d6d6e1146a6f2ea5c3cdd7afe309fa7026882be159ef64cd052566df'
+  version '2.2.1'
+  sha256 '131e1a2c06e88c1532570e4342073b043023fd049508b31be9d90a93f6defab1'
 
-  # hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995 was verified as official when first introduced to the cask
-  url 'https://rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995?format=zip'
+  # rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995was verified as official when first introduced to the cask
+  url 'https://rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995/app_versions/6?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995',
-          checkpoint: '0618aaad6d8f42b92351aedc35feb8873f7b7cd6e38427d13b7cfe56518d8d59'
+          checkpoint: 'c1c8499b83a3f44b49f8985d7c508cea576cca12c8619720491c33f58458d06f'
   name 'Paste'
   homepage 'http://pasteapp.me/'
 
