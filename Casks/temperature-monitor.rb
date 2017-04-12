c9c0adaf9a5184adc425e03d6efcfdf958f7ac4f
Gobinath Mallaiyan <i@gobinath.com>
2017-04-12 01:19:57 -0400

    Update temperature-monitor to latest

--- Casks/temperature-monitor.rb
+++ Casks/temperature-monitor.rb
@@ -2,8 +2,8 @@ cask 'temperature-monitor' do
   version :latest
   sha256 :no_check
 
-  # bresink.eu was verified as official when first introduced to the cask
-  url 'https://www.bresink.eu/Downloads/TemperatureMonitor.dmg'
+  # ssl.webpack.de/bresink.eu was verified as official when first introduced to the cask
+  url 'https://ssl.webpack.de/bresink.eu/Downloads/TemperatureMonitor.dmg'
   name 'Temperature Monitor'
   homepage 'https://www.bresink.com/osx/LegacyProducts.html'
 
