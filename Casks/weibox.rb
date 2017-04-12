5b1f0f202ca1392c8a9a1a0d8cc3fd7969f1513a
Vítor Galvão <info@vitorgalvao.com>
2017-04-12 22:27:22 +0100

    Update weibox.rb

--- Casks/weibox.rb
+++ Casks/weibox.rb
@@ -4,7 +4,7 @@ cask 'weibox' do
 
   url "http://2.weiboformac.sinaapp.com/downloads/#{version}.release.zip"
   appcast 'https://weiboformac.sinaapp.com/appcast/wm2.xml',
-          checkpoint: '2aefd31728fb15674f7afd1bc2fd4b47f42b6720227f2ee81b8a6d80344815c3'
+          checkpoint: '416e2159d0ca83f66359cf3278d300a55367ee573867de71f31c4d926b97df6e'
   name 'WeiboX'
   homepage 'https://weiboformac.sinaapp.com/'
 
