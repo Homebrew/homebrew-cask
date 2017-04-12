885d072678b2191cb4659600ea45771c98513297
jcbot <jcb@leipert.io>
2017-04-12 15:48:32 +0200

    Update intellij-idea to 2017.1.1,171.4073.35

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,10 +1,10 @@
 cask 'intellij-idea' do
-  version '2017.1.1,171.4073.28'
+  version '2017.1.1,171.4073.35'
   sha256 '6b3f89ce7a2f393552382b05d52efc779980fae6a478f8609ce90fa72bf159c7'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release',
-          checkpoint: 'bad4959cda49504f99325d1aff1095ec912c342fb6ca389750b729c63d4e5a6a'
+          checkpoint: '2cc7fb1f9e1f0e528c0507d623542605382fb16952bf95f0d1a6282ac807cc98'
   name 'IntelliJ IDEA Ultimate'
   homepage 'https://www.jetbrains.com/idea/'
 
