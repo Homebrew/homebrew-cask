30327736039c2e71a8aadd6b1b2a7ab7a4ea5eb2
Matt Coster <matthew@thecosters.co.uk>
2017-04-12 20:36:33 +0100

    Documented direct link source for future updates

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -2,6 +2,7 @@ cask 'gotomeeting' do
   version '8.2.6634'
   sha256 'd8de55400362f9913017dc4a82373c1148adf4ff24c4b4f82b5d40dadbb4f8dc'
 
+  # Retrieved from https://support.citrixonline.com/en_US/meeting/all_files/G2M010014 via "Install DMG" link
   url 'https://support.citrixonline.com/servlet/fileField?entityId=ka3380000000FRYAA2&field=Content__Body__s'
   name 'GoToMeeting'
   homepage 'https://www.gotomeeting.com/'
