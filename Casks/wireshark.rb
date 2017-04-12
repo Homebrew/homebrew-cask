c06fcfef7811792206c6057df30aa1dfa8903073
Vítor Galvão <info@vitorgalvao.com>
2017-04-12 11:33:50 +0100

    wireshark.rb: refactor uninstall

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -23,12 +23,17 @@ cask 'wireshark' do
                    sudo: true
   end
 
-  uninstall script:  {
-                       executable: '/usr/sbin/dseditgroup',
-                       args:       ['-o', 'delete', 'access_bpf'],
-                       sudo:       true,
-                     },
-            pkgutil: 'org.wireshark.*',
+  uninstall_preflight do
+    system_command '/usr/sbin/dseditgroup',
+                   args: [
+                           '-o',
+                           'delete',
+                           'access_bpf',
+                         ],
+                   sudo: true
+  end
+
+  uninstall pkgutil: 'org.wireshark.*',
             delete:  [
                        '/usr/local/bin/capinfos',
                        '/usr/local/bin/dftest',
