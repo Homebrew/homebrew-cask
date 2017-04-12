084597e5d690863e271073e8a7f4785bab4a12f0
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-04-12 17:18:33 +0100

    Update paste to 2.2.1

--- Casks/paste.rb
+++ Casks/paste.rb
@@ -2,7 +2,7 @@ cask 'paste' do
   version '2.2.1'
   sha256 '131e1a2c06e88c1532570e4342073b043023fd049508b31be9d90a93f6defab1'
 
-  # rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995was verified as official when first introduced to the cask
+  # rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995 was verified as official when first introduced to the cask
   url 'https://rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995/app_versions/6?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/ee24d1a939cd4ff8b2861eb8c788a995',
           checkpoint: 'c1c8499b83a3f44b49f8985d7c508cea576cca12c8619720491c33f58458d06f'
