8954fc9ae917a61ae6b622ba72132e7480f2a836
giannitm <giannitammcmillan@gmail.com>
2017-04-13 00:24:43 +0100

    Update microsoft-office to 15.33.17040900 (#32298)

--- Casks/microsoft-office.rb
+++ Casks/microsoft-office.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-office' do
-  version '15.32.17030901'
-  sha256 '6fb8dd28e36e350f40199f90c0095d9f252182b75842a964f3a2714e77ee0520'
+  version '15.33.17040900'
+  sha256 '4c97ac4103f9f1e7738d47f50b8d0b228f0e74c222d7446bee5e42131256690c'
 
   # officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/ was verified as official when first introduced to the cask
   url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/Microsoft_Office_2016_#{version}_Installer.pkg"
