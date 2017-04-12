2daacb27efcb9a545d95bd4a8835edf899da8ab2
Matt Coster <matthew@thecosters.co.uk>
2017-04-12 20:40:51 +0100

    Added URL verification comment

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -2,7 +2,8 @@ cask 'gotomeeting' do
   version '8.2.6634'
   sha256 'd8de55400362f9913017dc4a82373c1148adf4ff24c4b4f82b5d40dadbb4f8dc'
 
-  # Retrieved from https://support.citrixonline.com/en_US/meeting/all_files/G2M010014 via "Install DMG" link
+  # URL retrieved from https://support.citrixonline.com/en_US/meeting/all_files/G2M010014 via "Install DMG" link
+  # support.citrixonline.com was verified as official when first introduced to the cask
   url 'https://support.citrixonline.com/servlet/fileField?entityId=ka3380000000FRYAA2&field=Content__Body__s'
   name 'GoToMeeting'
   homepage 'https://www.gotomeeting.com/'
