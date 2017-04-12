9b90e40d76a4393b289e706ece1f465c2376ce2d
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-04-13 00:20:09 +0100

    Update deepanpot-vst to 2.0.0

--- Casks/deepanpot-vst.rb
+++ Casks/deepanpot-vst.rb
@@ -1,8 +1,8 @@
 cask 'deepanpot-vst' do
-  version :latest
-  sha256 :no_check
+  version '2.0.0'
+  sha256 '4a63172f715cd912cbc7f3439855f0878a8579e27c798b89513cc10315e6bb60'
 
-  url 'http://dotec-audio.com/release/DeePanpot/latest/mac/DeePanpotMac.zip'
+  url "https://dotec-audio.com/release/DeePanpot/DeePanpotMac_#{version}.zip"
   name 'DOTEC-AUDIO DeePanpot (VST)'
   homepage 'https://dotec-audio.com/deepanpot.html'
 
