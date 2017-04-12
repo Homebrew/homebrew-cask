e1b1c448d1eb483d05a8849f5c4264f9acbd39eb
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-12 16:34:52 +0800

    Update portfolioperformance.rb

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,6 +1,6 @@
 cask 'portfolioperformance' do
   version '0.26.3'
-  sha256 :no_check
+  sha256 '542c7893acf298373260785d219b80b342a7037681079125c204e02b6e6da004'
 
   # s3.amazonaws.com/name.abuchen.portfolio was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/name.abuchen.portfolio/#{version}/PortfolioPerformance-#{version}-macosx.cocoa.x86_64.tar.gz"
