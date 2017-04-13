f29d359ecabfa5de8c73b2601c749057ad8b752a
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-13 15:50:27 +0800

    Update newmoon.rb

--- Casks/newmoon.rb
+++ Casks/newmoon.rb
@@ -1,6 +1,6 @@
 cask 'newmoon' do
   version '26.5.0-131'
-  sha256 '617581f55863b2ab0f55538791d2c82a06378febffebce14b8d1c1d625f465cb'
+  sha256 'cfb774a32ec323829a5a03fefd6a07fd9242d32856aee5c22bbfdf0d44abe8dd'
 
   url "https://mac.palemoon.org/dist/palemoon-#{version}-gstreamer.en-US.mac64.dmg"
   name 'Pale Moon'
