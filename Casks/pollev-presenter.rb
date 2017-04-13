00239280bdfaac03fd2e037a7673464ed1d292be
Gobinath Mallaiyan <i@gobinath.com>
2017-04-12 23:51:58 -0400

    Update pollev-presenter to 2.9.0

--- Casks/pollev-presenter.rb
+++ Casks/pollev-presenter.rb
@@ -1,9 +1,9 @@
 cask 'pollev-presenter' do
   version '2.9.0'
-  sha256 'fdd711dc48d6cebdbb5d4d4951d586180bacc3ce2227d48b7a31d6380f3efe8b'
+  sha256 'fb0e363f4665efbe79082024417afaaa03f48174e3bcda573c2f513c79f4c89e'
 
   # amazonaws.com/polleverywhere-app was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/polleverywhere-app/mac-stable/pollev.dmg'
+  url "https://s3.amazonaws.com/polleverywhere-app/mac-stable/#{version}/pollev.dmg"
   appcast 'https://polleverywhere-app.s3.amazonaws.com/mac-beta/appcast.xml',
           checkpoint: '007c5488e8d9d53c03b5966338fd42186477b29d5a21789c156c9afad87f237e'
   name 'PollEv Presenter'
