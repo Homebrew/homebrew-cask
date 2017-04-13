e0f7ba7a51baa6e17c3362e034eabf7c67835d5f
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-13 09:51:14 +0800

    Update gotomeeting.rb

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -1,8 +1,7 @@
 cask 'gotomeeting' do
-  version '8.2.6634'
-  sha256 'd8de55400362f9913017dc4a82373c1148adf4ff24c4b4f82b5d40dadbb4f8dc'
+  version :latest
+  sha256 :no_check
 
-  # URL retrieved from https://support.citrixonline.com/en_US/meeting/all_files/G2M010014 via "Install DMG" link
   # support.citrixonline.com was verified as official when first introduced to the cask
   url 'https://support.citrixonline.com/servlet/fileField?entityId=ka3380000000FRYAA2&field=Content__Body__s'
   name 'GoToMeeting'
