804f22757c5630b219baffde065141c11dba467d
Gobinath Mallaiyan <i@gobinath.com>
2017-04-13 00:00:53 -0400

    Update hbuilder to 8.1.0

--- Casks/hbuilder.rb
+++ Casks/hbuilder.rb
@@ -1,6 +1,6 @@
 cask 'hbuilder' do
-  version '8.0.2'
-  sha256 '972e5614d66624306e52726e339842155064bc744148ef7eb54c8441c4c7a828'
+  version '8.1.0'
+  sha256 '2ca373a1cf9afb1de3d227946be41545c4bceca223b4f9f97e7b74480b158c15'
 
   # download.dcloud.net.cn was verified as official when first introduced to the cask
   url "http://download.dcloud.net.cn/HBuilder.#{version}.macosx_64.dmg"
