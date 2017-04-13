5f0ed291ffb66be0f8e71b371c8807988fcc2d1f
Anton Petrov <givemeletter@gmail.com>
2017-04-13 22:42:59 +0300

    Update genymotion to 2.9.0

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -1,6 +1,6 @@
 cask 'genymotion' do
-  version '2.8.1'
-  sha256 'e6bc3a4a4b1570644bbfe0c98ce192cad1517b1ae47b1ca3240a2ec2b3874b4f'
+  version '2.9.0'
+  sha256 '6351aaf2cbca439957b7bca40685253d0c1743dee28f6ab056fd59e1055906fc'
 
   url "https://dl.genymotion.com/releases/genymotion-#{version}/genymotion-#{version}.dmg"
   name 'Genymotion'
