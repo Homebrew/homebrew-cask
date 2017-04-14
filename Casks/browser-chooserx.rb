314539233a897e4010f6c4117eeed74624624614
Artem Medvedev <amedvedev@outlook.com>
2017-04-14 03:18:32 +0300

    Update browser-chooserx to 1.3.1 (#32343)

--- Casks/browser-chooserx.rb
+++ Casks/browser-chooserx.rb
@@ -1,10 +1,10 @@
 cask 'browser-chooserx' do
-  version '1.3.0'
-  sha256 'b29dd4bc246163345e7f4fb76557ccfa3eed91cceac5758ae1455a7a87e2cefe'
+  version '1.3.1'
+  sha256 'e75b1c49ed79c5e10887b076d99c371fa45350bee6c9c81e8ecfd3bfccc7fa1f'
 
   url 'https://www.bdevapps.com/files/downloads/Browser%20ChooserX.zip'
   appcast "https://www.bdevapps.com/files/downloads/BrowserChooserXAppCast#{version.major}.xml",
-          checkpoint: '02e3700015a76c64b69e5343f0cde0164896906ad8d61289995a5a9b070dec52'
+          checkpoint: 'b5524e0d8bce8f7943d58dec86feecc9bb2134da5e3f675a43a363f09b9dc66a'
   name 'Browser ChooserX'
   homepage 'https://bdevapps.com/'
 
