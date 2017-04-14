8843d1c0341c5cba11872287ffa029c66981729b
Nicholas <nick@Nicholass-MacBook-Pro.local>
2017-04-14 17:57:04 -0400

    Update yakyak to 1.4.3

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,10 +1,10 @@
 cask 'yakyak' do
-  version '1.4.2'
-  sha256 '3e83358dd23a8bd6739b10e8599d55b5664a99c40f30aac3bea8eb50fc9f6c2a'
+  version '1.4.3'
+  sha256 'a8e220046279f2794087c8609bcc643e1171e83a84b93967b2c169362289a411'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom',
-          checkpoint: '613e22af7e08b46183349d4f22f5f8780d66f6c26441ddc725edcf39742b1add'
+          checkpoint: 'ad612c55efbfbb9d72738e2af20f00ce5fe3fa913a5225f215d606deaf8e345e'
   name 'Yakyak'
   homepage 'https://github.com/yakyak/yakyak'
 
