77dc9522a856da3052572ac3cf2a10b5fc4c7d56
Nick Wind-McJetters <nick@elearningexperts.net>
2017-04-13 20:20:07 -0400

    Update yakyak to 1.4.2 (#32349)

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,10 +1,10 @@
 cask 'yakyak' do
-  version '1.4.1'
-  sha256 '8a65fbb2b15d9805341571a2d7aed60f68e2839932b77706fb6363b9bfc1c5e9'
+  version '1.4.2'
+  sha256 '3e83358dd23a8bd6739b10e8599d55b5664a99c40f30aac3bea8eb50fc9f6c2a'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom',
-          checkpoint: 'e6b4c60c3e7bc88c55862e05282c35444e42e003d2e912372fb6d0ff3e69685a'
+          checkpoint: '613e22af7e08b46183349d4f22f5f8780d66f6c26441ddc725edcf39742b1add'
   name 'Yakyak'
   homepage 'https://github.com/yakyak/yakyak'
 
