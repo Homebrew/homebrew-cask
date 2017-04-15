933322aa26565c9ffbd0c0e52544907909fff90e
Lex Tang <lexrus@gmail.com>
2017-04-15 22:55:16 +0800

    Update qlswift to 0.0.2

--- Casks/qlswift.rb
+++ Casks/qlswift.rb
@@ -1,10 +1,10 @@
 cask 'qlswift' do
-  version '0.0.1'
-  sha256 '9541767fb3f381791cd5b98a84c681da6607179c8da32677f73972c9a0836728'
+  version '0.0.2'
+  sha256 '97ceafb35519c49793e96fbdb865013d3cf044e88b5ccf259856141723c54613'
 
   url "https://github.com/lexrus/QLSwift/releases/download/#{version}/QLSwift.qlgenerator.zip"
   appcast 'https://github.com/lexrus/QLSwift/releases.atom',
-          checkpoint: '5dc3ff40c67e2c7625aed0ad377a35dea553b437939372d784ef5b94080e8104'
+          checkpoint: 'ac6706c2f6fc75663df60ea0809447cfdabe41acb40a7870207f78bd88ad92ed'
   name 'QLSwift'
   homepage 'https://github.com/lexrus/QLSwift'
 
