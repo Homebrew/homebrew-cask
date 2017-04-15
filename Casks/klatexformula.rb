02c9e593ca5d0f93af7512f64b34c1c08dd97543
Victor Popkov <victor@popkov.me>
2017-04-15 23:57:18 +0300

    Update klatexformula to 4.0.0 (#32408)

--- Casks/klatexformula.rb
+++ Casks/klatexformula.rb
@@ -1,12 +1,12 @@
 cask 'klatexformula' do
-  version '3.3.0beta'
-  sha256 '7b1ac67dec83010acce4da00d1a10c6bab3921e9eedb7e88322f8b3bf987c104'
+  version '4.0.0'
+  sha256 '2204da5aef0e30a3a4fd4d4e8d372a9a139589b99c12254c6f30f2553f1f0d94'
 
   url "https://downloads.sourceforge.net/klatexformula/klatexformula/klatexformula-#{version}/klatexformula-#{version}-macosx.dmg"
   appcast 'https://sourceforge.net/projects/klatexformula/rss?path=/klatexformula',
-          checkpoint: 'd03dfb386990dae34c456202e67d3d0976ef346c05a59e8c0c3c68a37d05ad2d'
+          checkpoint: '089e768852081144675a63ccf60590358830e77bcd765063b09c5777ced7b031'
   name 'KLatexFormula'
   homepage 'http://klatexformula.sourceforge.net/'
 
-  app 'KLatexFormula.app'
+  app 'klatexformula.app'
 end
