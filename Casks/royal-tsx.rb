dc42fd1be9b5724fb53e2c1dee2d188f00fa473d
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-14 20:42:54 -0400

    Update royal-tsx to 3.1.3.1000 (#32377)

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -1,11 +1,11 @@
 cask 'royal-tsx' do
-  version '3.0.0.1000'
-  sha256 'cd2fce3697f9b414285b2631587d4c2449e0f45a686f1869288e2ac11de958ff'
+  version '3.1.3.1000'
+  sha256 '2dc27df75186d9d9ba7757a74dafe312594601f98d4f8b24aa65227ab6ee9fa4'
 
   # https://royaltsx-v3.royalapplications.com was verified as official when first introduced to the cask
   url "https://royaltsx-v3.royalapplications.com/updates/royaltsx_#{version}.dmg"
   appcast "https://royaltsx-v#{version.major}.royalapplications.com/updates_stable.php",
-          checkpoint: '0e3d44af016fd5446a0e853ce4dda3991f79239564f50c25a44defea75df6045'
+          checkpoint: '508638cd99782fb30906199b7df01464daae78f698513c803e14df8338120a08'
   name 'Royal TSX'
   homepage 'https://www.royalapplications.com/ts/osx/features'
 
