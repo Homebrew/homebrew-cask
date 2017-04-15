9bdf13f22d0fafec81594afb666fe085a15afc72
Victor Popkov <victor@popkov.me>
2017-04-15 23:42:29 +0300

    Update djay-pro to 1.4.3,201703200957 (#32402)

--- Casks/djay-pro.rb
+++ Casks/djay-pro.rb
@@ -1,10 +1,10 @@
 cask 'djay-pro' do
-  version '1.4.2,201611211720'
-  sha256 '5c19c6b1d8bda8f339b3df29f11d467e40f200a39e528950da25144e59db51c5'
+  version '1.4.3,201703200957'
+  sha256 'a2d1af6130a5e2de76c3abe85fafff8047f4160b98d7a55ba5df39b30c6e13e0'
 
   url "http://download.algoriddim.com/djay/#{version.after_comma}/djay_Pro_#{version.before_comma}.zip"
   appcast 'https://www.algoriddim.com/djay-pro-mac/releasenotes/appcast',
-          checkpoint: 'e7a64c7779d10c61c11ce60783065df86adcadd6190b6d0a2a8c32687f1a345f'
+          checkpoint: '6c669e49621bc10770238d0207b157789a534ba3b952a463354cc8d1e340fdcb'
   name 'Algoriddim djay Pro'
   homepage 'https://www.algoriddim.com/djay-pro-mac'
 
