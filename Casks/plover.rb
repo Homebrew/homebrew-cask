3f8527d703dcaf9f3ed2ec5e61b01326160a2e09
Victor Popkov <victor@popkov.me>
2017-04-16 00:23:23 +0300

    Update plover to 3.1.1 (#32417)

--- Casks/plover.rb
+++ Casks/plover.rb
@@ -1,17 +1,17 @@
 cask 'plover' do
-  version '3.1.0'
-  sha256 '6e325d70bab78de5c6846f9ba83e66b3ce3c59d61a267eaeb9183dc26edac9cf'
+  version '3.1.1'
+  sha256 'c45c1c6cf797de62923d20e6e46aec0d946027b6b3cca4df2fc2db4e3183516b'
 
   # github.com/openstenoproject/plover was verified as official when first introduced to the cask
-  url "https://github.com/openstenoproject/plover/releases/download/v#{version}/Plover-#{version}-macOS.dmg"
+  url "https://github.com/openstenoproject/plover/releases/download/v#{version}/plover-#{version}-py2-mac.dmg"
   appcast 'https://github.com/openstenoproject/plover/releases.atom',
-          checkpoint: '00552cb3716e36085af2c8ad44d0774d3e404e9ad86656bee7edb57721cd1d8c'
+          checkpoint: '040e2f8a084202788d5e6d1e68a38214d14cac854ebca97c7d99e42d8855d40b'
   name 'Plover'
   homepage 'https://stenoknight.com/wiki/Main_Page'
 
   accessibility_access true
 
-  app 'plover.app'
+  app 'Plover.app'
 
   zap delete: '~/Library/Application Support/plover/'
 end
