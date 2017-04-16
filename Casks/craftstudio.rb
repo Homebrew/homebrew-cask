81166029fb3250a3f85b574e88bf265c74c3e92f
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-16 14:45:26 +0800

    Update craftstudio.rb

--- Casks/craftstudio.rb
+++ Casks/craftstudio.rb
@@ -2,9 +2,10 @@ cask 'craftstudio' do
   version :latest
   sha256 :no_check
 
+  # craftstud.io/files/OSX was verified as official when first introduced to the cask
   url 'http://craftstud.io/files/OSX/CraftStudio.pkg'
   name 'CraftStudio'
-  homepage 'http://craftstud.io/'
+  homepage 'https://sparklinlabs.itch.io/craftstudio'
 
   depends_on cask: 'mono-mre'
 
