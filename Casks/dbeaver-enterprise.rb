1b0ce02643b36c6a8c0a014f4c44b8042582c75d
Colin Dunn <dunnc@amazon.com>
2017-04-18 09:52:52 +1000

    Update dbeaver-enterprise to 4.0.5

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,10 +1,10 @@
 cask 'dbeaver-enterprise' do
-  version '4.0.4'
-  sha256 'bad3df2ca3ad63c5fb177269fddc0f24cf89d3a51012c789626ace26f4d26f96'
+  version '4.0.5'
+  sha256 'c58b03056e0a845f2d7fff7ca37dcdf8b4dc2ded74e1932bc2654f8676510bf0'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   appcast 'http://dbeaver.jkiss.org/files/',
-          checkpoint: '2ab25ff4d79c97015ef4672cb3a94677f23e63b96b8a592952370846f0148622'
+          checkpoint: 'f3e43d131961916e864abaad038206aaaa29c89f571145b632cec27e1976bc2b'
   name 'DBeaver Enterprise Edition'
   homepage 'http://dbeaver.jkiss.org/'
 
