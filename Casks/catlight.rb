3df34744cd11b5f4e8fb7f65c1b1d36b87db821f
Gobinath Mallaiyan <i@gobinath.com>
2017-04-17 13:03:49 -0400

    Update catlight to 2.8.5

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -1,8 +1,8 @@
 cask 'catlight' do
-  version :latest
-  sha256 :no_check
+  version '2.8.5'
+  sha256 'f1387c0876e78361213ac2ca291cdf750916ad93a67fa9acc00accae92ae5b6d'
 
-  url 'https://catlight.io/downloads/mac/beta'
+  url "https://www.catlight.io/dl/mac/beta/setup-#{version}.zip"
   name 'catlight'
   homepage 'https://catlight.io/'
 
