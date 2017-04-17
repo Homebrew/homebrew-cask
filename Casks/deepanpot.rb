b6750bd594cbc08431f8b4e0246e41cb57bb460c
Gobinath Mallaiyan <i@gobinath.com>
2017-04-17 14:54:29 -0400

    Update deepanpot cask to single
    - Merging deepanpot-au & deepanpot-vst to make it a single cask.

--- Casks/deepanpot.rb
+++ Casks/deepanpot.rb
@@ -1,10 +1,11 @@
-cask 'deepanpot-vst' do
+cask 'deepanpot' do
   version '2.0.0'
   sha256 '4a63172f715cd912cbc7f3439855f0878a8579e27c798b89513cc10315e6bb60'
 
   url "https://dotec-audio.com/release/DeePanpot/DeePanpotMac_#{version}.zip"
-  name 'DOTEC-AUDIO DeePanpot (VST)'
+  name 'DOTEC-AUDIO DeePanpot'
   homepage 'https://dotec-audio.com/deepanpot.html'
 
+  audio_unit_plugin 'DeePanpot.component'
   vst_plugin 'DeePanpot.vst'
 end
