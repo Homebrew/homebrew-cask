d57805656b19121e96c2563889e0df36e2b61c59
Gobinath Mallaiyan <i@gobinath.com>
2017-04-17 14:50:32 -0400

    Update Deegain cask to single
    - Merging deegain-au & deegain-vst to make it a single cask.

--- Casks/deegain.rb
+++ Casks/deegain.rb
@@ -1,10 +1,11 @@
-cask 'deegain-au' do
+cask 'deegain' do
   version '1.0.5'
   sha256 '57e60cb7f0baf3e52622edfd8a61997aac7256a63b7d16f65700649206b2c437'
 
   url "https://dotec-audio.com/release/DeeGain/DeeGainMac_#{version}.zip"
-  name 'DOTEC-AUDIO DeeGain (AU)'
+  name 'DOTEC-AUDIO DeeGain'
   homepage 'https://dotec-audio.com/deegain.html'
 
   audio_unit_plugin 'DeeGain.component'
+  vst_plugin 'DeeGain.vst'
 end
