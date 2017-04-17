d1809fb130e951527df1591d66d5920b9b17a878
Andrew Barchuk <robots@raindev.io>
2017-04-17 19:44:11 +0200

    Use HTTPS to download adobe-digital-editions (#32452)

--- Casks/adobe-digital-editions.rb
+++ Casks/adobe-digital-editions.rb
@@ -2,7 +2,7 @@ cask 'adobe-digital-editions' do
   version '4.5.4'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "http://download.adobe.com/pub/adobe/digitaleditions/ADE_#{version.major_minor}_Installer.dmg"
+  url "https://download.adobe.com/pub/adobe/digitaleditions/ADE_#{version.major_minor}_Installer.dmg"
   name 'Adobe Digital Editions'
   homepage 'https://www.adobe.com/solutions/ebook/digital-editions.html'
 
