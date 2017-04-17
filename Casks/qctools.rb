1ce2b8e13a66f2098a199a0cd17be5a154da951e
Dave Rice <dave@dericed.com>
2017-04-17 13:13:22 -0300

    Update qctools 0.8 (#32445)

--- Casks/qctools.rb
+++ Casks/qctools.rb
@@ -1,10 +1,10 @@
 cask 'qctools' do
-  version '0.7.3.20161011'
-  sha256 '7a515466f541c07d37be94fbdeaca2669351e01a78ecb433b6cdf750b53a9342'
+  version '0.8'
+  sha256 'd3cfaeeaf74fe2bdc90d37e3ae22d3c429c2fdeb89fb1f0466150f9ad0c23d51'
 
   url "https://github.com/bavc/qctools/releases/download/v#{version.major_minor_patch}/QCTools_#{version}_mac.dmg"
   appcast 'https://github.com/bavc/qctools/releases.atom',
-          checkpoint: '19f1602531af9b2de0b5b8ca76714b9869d9a66abcdee863517d6a14fe2d5907'
+          checkpoint: '85e48dfbc70a96cf01b636dd382f2e7c28cc788d128a5009072c1a5829822e6c'
   name 'QCTools'
   homepage 'https://github.com/bavc/qctools'
 
