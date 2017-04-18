ce5be6156f9c3cbbe6cdc17674493663390cc843
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-17 22:37:30 -0400

    Update nylas-mail to 2.0.15-c770c80 (#32487)

--- Casks/nylas-mail.rb
+++ Casks/nylas-mail.rb
@@ -1,11 +1,11 @@
 cask 'nylas-mail' do
-  version '2.0.14-8c3dc81'
-  sha256 '352144bbef94b531713521caa016ffecd484181e17bd2a5b20760842aa103f32'
+  version '2.0.15-c770c80'
+  sha256 '10abf7eeb07ba5c795b20825ee9d10e551bc0f8c4ef01b0128191e8968e1f430'
 
   # edgehill.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
   url "https://edgehill.s3-us-west-2.amazonaws.com/#{version}/darwin/x64/NylasMail.dmg"
   appcast 'https://edgehill.nylas.com/update-check?platform=darwin&arch=64',
-          checkpoint: '2ab2ec2c3a568f7d1f5ab3569a2638cbaa5fea816ee5a41dcd05b496343cfa07'
+          checkpoint: 'e750c17e30543aafd5e6fd29db40150a57daa51e41e1b79af23befe04767c13a'
   name 'Nylas Mail'
   homepage 'https://www.nylas.com/'
 
