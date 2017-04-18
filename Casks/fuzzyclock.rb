badcd5180be79ddc078754dcebc8a2d8c8e3ae71
giannitm <giannitammcmillan@gmail.com>
2017-04-18 01:01:54 +0100

    Update fuzzyclock to latest (#32478)

--- Casks/fuzzyclock.rb
+++ Casks/fuzzyclock.rb
@@ -2,9 +2,9 @@ cask 'fuzzyclock' do
   version :latest
   sha256 :no_check
 
-  url 'https://mschmidt.me/macOS/FuzzyClock.zip'
+  url 'http://mschmidt.me/macOS/FuzzyClock.zip'
   name 'FuzzyClock'
-  homepage 'https://mschmidt.me/macos.html'
+  homepage 'http://mschmidt.me/macos.html'
 
   app 'FuzzyClock.app'
 end
