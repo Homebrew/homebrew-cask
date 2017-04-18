ee800914c9979a4fdebf2e43e029f3ccb2ac6357
fcccbf8f8f1 <fcccbf8f8f1@users.noreply.github.com>
2017-04-17 20:07:08 -0400

    Update graphicconverter to 10.4.1,2758 (#32477)

--- Casks/graphicconverter.rb
+++ Casks/graphicconverter.rb
@@ -1,13 +1,16 @@
 cask 'graphicconverter' do
-  version '10.2743'
-  sha256 'afb9db2618b6d89f954604ceb52091cb9cc03b8b219ad8979bcf06100044aeaf'
+  version '10.4.1,2758'
+  sha256 '92c0ad87c3d70f231ad2be28d6613651872d53be10a5a1debb31d1ddbf83c3d2'
 
   # lemkesoft.info was verified as official when first introduced to the cask
-  url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.minor}.zip"
-  appcast "http://www.lemkesoft.org/files/graphicconverter/graphicconverter#{version.major}.xml",
-          checkpoint: '621f0918d123dd684b30e03995c383469a893408dc180aba7a760e87c0d1cf6f'
+  url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.after_comma}.zip"
+  appcast "https://www.lemkesoft.info/sparkle/graphicconverter/graphicconverter#{version.major}.xml",
+          checkpoint: 'c030fb7c2b96c05f50b1afb09419d4ab58da157ff5c58cd7c8a30141bd9bedc3'
   name 'GraphicConverter'
   homepage 'https://www.lemkesoft.de/en/products/graphicconverter/'
+
+  auto_updates true
+  depends_on macos: '>= :mavericks'
 
   app "GraphicConverter #{version.major}.app"
 end
