c9f67f4ce145a3ecea5e751f18e44b9f8e07883d
Maarten Paauw <maartenpaauw@gmail.com>
2017-04-18 12:29:11 +0200

    Update bytecoinwallet to 1.1.1

--- Casks/bytecoinwallet.rb
+++ Casks/bytecoinwallet.rb
@@ -1,6 +1,6 @@
 cask 'bytecoinwallet' do
-  version '1.1.0'
-  sha256 '6964b94e3ee09ce3d093833e131e30d7c0b6c2c4997752c985de7ea8c480b28d'
+  version '1.1.1'
+  sha256 '6b1266a761c037fc4156c7e4f9df1fa225d12666f20f71d4aa1f0296f0a26c0d'
 
   url "https://bytecoin.org/static/files/BytecoinWallet-#{version}.dmg"
   name 'Bytecoin Wallet'
