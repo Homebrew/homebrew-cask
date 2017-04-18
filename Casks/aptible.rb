3fb8dd84453f7e29b0d60c99eb00ca6596350f54
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-18 18:37:58 +0800

    Update aptible.rb

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -3,7 +3,7 @@ cask 'aptible' do
   sha256 'dc8d5f356ab3aaa9e532e63704021aadab2f2c82212aab57c13dfa922e3c3bd6'
 
   # omnibus-aptible-toolbelt.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.after_comma}/pkg/aptible-toolbelt-#{version.sub('+', '%2B')}-mac-os-x.10.11.6-1.pkg"
+  url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.after_comma}/pkg/aptible-toolbelt-#{version.before_comma.sub('+', '%2B')}-mac-os-x.10.11.6-1.pkg"
   name 'Aptible Toolbelt'
   homepage 'https://www.aptible.com/support/toolbelt/'
 
