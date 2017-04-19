dbef192cc6180b1413c6a7fa8f0fea81fd856bbc
Lingchao Xin <douglarek@gmail.com>
2017-04-19 12:01:03 +0800

    Update java to 1.8.0_131-b11,d54c1d3a095b4ff2b6607d096fa80163

--- Casks/java.rb
+++ Casks/java.rb
@@ -1,6 +1,6 @@
 cask 'java' do
-  version '1.8.0_121-b13,e9e7ea248e2c4826b92b3f075a80e441'
-  sha256 '82ff2493cd4b9ebdaeb9135abaffc9a37b71d341b007a83f73aa6ff3df1b6a3a'
+  version '1.8.0_131-b11,d54c1d3a095b4ff2b6607d096fa80163'
+  sha256 '642aca454e10bea70a36a36f54cc5bac22267de78bf85c2d019b1fefbc023c43'
 
   java_update = version.sub(%r{.*_(\d+)-.*}, '\1')
   url "http://download.oracle.com/otn-pub/java/jdk/#{version.minor}u#{version.before_comma.split('_').last}/#{version.after_comma}/jdk-#{version.minor}u#{java_update}-macosx-x64.dmg",
