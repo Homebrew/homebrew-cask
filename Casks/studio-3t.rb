ce02c8396f513c37f06e623ad098193c516f629e
Colin Dunn <cgd@vikingdev.net>
2017-04-20 00:36:01 +1000

    Update studio-3t to 5.2.1 (#32582)

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,10 +1,10 @@
 cask 'studio-3t' do
-  version '5.1.1'
-  sha256 '730a7fb8d8da605f44696bbc5dd34e3b1fcc2d3e74e8eaa308d806f9081ef96f'
+  version '5.2.1'
+  sha256 '694b5ee3a4617af3eea1a36703ab541e096aba07e453dab424862d788c4d82c2'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'http://files.studio3t.com/changelog/changelog.txt',
-          checkpoint: '9fbd97055bde402bbeea72d66c4b38ef1a12dd7dbc7d9c91bbb63f47e7e103bd'
+          checkpoint: '627e9f98d4c76f038172125a0f95ac92c3db47e73f8ed1ded8505a8983928935'
   name 'Studio 3T'
   homepage 'https://studio3t.com/'
 
