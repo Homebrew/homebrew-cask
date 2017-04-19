4257303615c4e4c5600c7d1bce617d6e16fd490b
Victor Popkov <victor@popkov.me>
2017-04-19 23:22:27 +0300

    Update boxcryptor download url

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -2,8 +2,7 @@ cask 'boxcryptor' do
   version '2.10.820'
   sha256 '4e4d2ad6c5d00011edcee1cbed149eedd5546dd273d3f3f03ce6ab4ebb70a914'
 
-  # d3k3ih5otj72mn.cloudfront.net was verified as official when first introduced to the cask
-  url "https://d3k3ih5otj72mn.cloudfront.net/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
+  url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8',
           checkpoint: '96de80d6dc8c7dfbe069a33e4dda791feaf7158fa2b681c3fc42e83459367720'
   name 'Boxcryptor'
