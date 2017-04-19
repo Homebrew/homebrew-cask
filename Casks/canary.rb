1224e2daeca65c80cf09677e55f29b84e71625d9
Artem Medvedev <amedvedev@outlook.com>
2017-04-20 00:20:06 +0300

    Update url

--- Casks/canary.rb
+++ Casks/canary.rb
@@ -3,7 +3,7 @@ cask 'canary' do
   sha256 'efd57e00bd616d3e1cf10447e14d7a91ed8075f25a99bccc0263298fb01fed2c'
 
   # rink.hockeyapp.net/api was verified as official when first introduced to the cask
-  url 'https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050/app_versions/236?format=zip'
+  url 'https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050/app_versions/304?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050',
           checkpoint: '6f149aba00dc0b5ee994e8fc95401aa9ebeefd760ed3a9fc0b51f5b3ddc45f46'
   name 'Canary'
