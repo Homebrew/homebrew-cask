aee3d1fbcad4285585a9ea3692ebb66d2c05e0d9
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-18 21:49:51 -0400

    Update mixlr to latest (#32538)
    * Update mixlr to latest
    
    * Update mixlr.rb

--- Casks/mixlr.rb
+++ Casks/mixlr.rb
@@ -2,9 +2,17 @@ cask 'mixlr' do
   version :latest
   sha256 :no_check
 
-  url 'http://cdn.mixlr.com/Mixlr.dmg'
+  url 'http://cdn.mixlr.com/Mixlr.pkg'
   name 'Mixlr'
   homepage 'http://mixlr.com/'
 
-  app 'Mixlr.app'
+  pkg 'Mixlr.pkg'
+
+  uninstall pkgutil: 'com.mixlr.pkg.Mixlr'
+
+  zap delete: [
+                '~/Library/Application Support/mixlr',
+                '~/Library/Preferences/com.mixlr.Broadcaster.plist',
+                '~/Library/Saved Application State/com.mixlr.Broadcaster.savedState',
+              ]
 end
