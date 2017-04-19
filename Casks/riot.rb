29f260fcc340f86cab3f39f852a72cb797c3b71e
giannitm <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-04-19 21:57:51 +0100

    Update riot to 0.9.8

--- Casks/riot.rb
+++ Casks/riot.rb
@@ -1,10 +1,10 @@
 cask 'riot' do
-  version '0.9.7'
-  sha256 '8586274e4e3a48b227884e170b9b626b4c662ddf6d808e189bf07f86fd86ae53'
+  version '0.9.8'
+  sha256 'd5611b1ef8fd811c6fc2b20fc76c1788e3008b89a3566521ad860f8012e5dbe9'
 
   url "https://riot.im/download/desktop/install/macos/Riot-#{version}.dmg"
   appcast 'https://github.com/vector-im/riot-web/releases.atom',
-          checkpoint: '16f57186dedac3c4c36adbfbe3464af71770d017b5d8fbc864634a0b4cb86f1b'
+          checkpoint: 'bf8534d3a086f932106b41609e1d388212e999e2c9ae6cfd3c27a02f51a76afc'
   name 'Riot'
   homepage 'https://riot.im/'
 
