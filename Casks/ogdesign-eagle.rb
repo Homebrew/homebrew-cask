d727e7033020e8f6486266f92df0dc8b3f517417
Gobinath Mallaiyan <i@gobinath.com>
2017-04-19 19:00:59 -0400

    Update ogdesign-eagle to 1.0.0

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,6 +1,6 @@
 cask 'ogdesign-eagle' do
-  version '0.9.46'
-  sha256 'd208e17ebf419ab064a793f264a0de20addd57a62eb0307ac3d947029a98693a'
+  version '1.0.0'
+  sha256 'ccb8e013980b2e58f20a0fd09189dba5a545f333533d92666155aeaf12c4c067'
 
   # eagle-1253434826.file.myqcloud.com was verified as official when first introduced to the cask
   url "http://eagle-1253434826.file.myqcloud.com/releases/darwin/#{version}/Eagle.zip"
