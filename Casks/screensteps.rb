3046c8f54d15991a83798551ba30b99ca35b2bb0
Gobinath Mallaiyan <i@gobinath.com>
2017-04-18 23:27:43 -0400

    Update screensteps to 4.0.106

--- Casks/screensteps.rb
+++ Casks/screensteps.rb
@@ -1,6 +1,6 @@
 cask 'screensteps' do
   version '4.0.106'
-  sha256 '6fad22899e703f89d5dd2dbcdfdd83fbaebdcaa83ccb04da2280cfb4279c7a92'
+  sha256 '992fd72ff00294180bc0dff8d6d520313069fe0e1307a15a3b7e4939b6fbd3eb'
 
   # bluemangolearning.com was verified as official when first introduced to the cask
   url "https://www.bluemangolearning.com/download/screensteps/#{version.major_minor.dots_to_underscores}/release/ScreenSteps.dmg"
