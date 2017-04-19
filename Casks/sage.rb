0d1607823d9693bf861c98ba229ec03cdb415293
Jonatas Miguel <jonatas.df.miguel@gmail.com>
2017-04-19 01:48:47 -0400

    Fix El Capitan support for SageMath 7.6 (#32561)
    * Fix El Capitan support for SageMath 7.6
    
    * Update sage.rb

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -1,14 +1,14 @@
 cask 'sage' do
-  if MacOS.version <= :lion
-    version '7.2'
-    sha256 'f88e0686ae8fe31f2684163a57bea938d93f732842cab7c263ee6e4cdeb271cc'
-    # mit.edu/sage was verified as official when first introduced to the cask
-    url "http://mirrors.mit.edu/sage/osx/intel/sage-#{version}-OSX_10.7.5-x86_64.app.dmg"
-  elsif MacOS.version <= :mavericks
+  if MacOS.version <= :mavericks
     version '7.2'
     sha256 'a4cd5c6f3207cd9c429642bb58a6310ba05e6da9fddbf36dc1aa5e47c5904c96'
     # mit.edu/sage was verified as official when first introduced to the cask
     url "http://mirrors.mit.edu/sage/osx/intel/sage-#{version}-OSX_10.9.5-x86_64.app.dmg"
+  elsif MacOS.version <= :el_capitan
+    version '7.6'
+    sha256 'ba9ffba5dea394dc808c31a7b71af4d0db9759d9440b4dc2e35c921bd03e916f'
+    # mit.edu/sage was verified as official when first introduced to the cask
+    url "http://mirrors.mit.edu/sage/osx/intel/sage-#{version}-OSX_10.11.6-x86_64.app.dmg"
   else
     version '7.6'
     sha256 'dbd5e79825ad505dbf852c8c794cd604a3bd560151fb3ec08a9df7f9c5626ab2'
