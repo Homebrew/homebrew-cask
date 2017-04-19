87a87781cfb50c7a0638cab60f3696fd049ad45e
Colin Dunn <cgd@vikingdev.net>
2017-04-19 22:47:37 +1000

    Update mt32emu to 2.1.0 (#32575)

--- Casks/mt32emu.rb
+++ Casks/mt32emu.rb
@@ -1,10 +1,10 @@
 cask 'mt32emu' do
-  version '2.0.0'
-  sha256 'b0a8434a6de660725723bd822c00a2bbb24c06ee9f2fd994961fa3dbb1b1bbd1'
+  version '2.1.0'
+  sha256 'c9c39386ac04a97a29d7ec661021e43b2b365bbd7dcecb1edd7de135182b20bc'
 
-  url "https://downloads.sourceforge.net/munt/munt/#{version}/OS%20X/MT32Emu-qt-1.4.0.dmg"
+  url "https://downloads.sourceforge.net/munt/munt/#{version}/OS%20X/MT32Emu-qt-1.5.0.dmg"
   appcast 'https://sourceforge.net/projects/munt/rss?path=/munt',
-          checkpoint: 'bec1a2eb6dd50e93a10a8f1e479860531cb1e61f7cd8f55a2992c774a995833c'
+          checkpoint: '372b2206914571d17f480d2cc11dc718bbf4d134d9992f1bd592c91396288eba'
   name 'MT32Emu'
   homepage 'https://sourceforge.net/projects/munt/'
 
