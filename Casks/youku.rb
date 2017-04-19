86bc5671ea104b9f4e0650c4554cd3804aee6bea
Gobinath Mallaiyan <i@gobinath.com>
2017-04-18 22:03:11 -0400

    Update youku to 1.2.2.04189

--- Casks/youku.rb
+++ Casks/youku.rb
@@ -3,8 +3,8 @@ cask 'youku' do
     version '1.0.6.01094'
     sha256 'ab17b83f88f1035bda7d20c064bfd59c35925c373aed510f1612f3473ec0c37e'
   else
-    version '1.2.1.03315'
-    sha256 'b7a80f361ee1cf0c5160e079623d8f25769b3b6ce8ad29e2ce8fa64c63de917e'
+    version '1.2.2.04189'
+    sha256 '6fe78dea2f1c292775b1ab5d0eb2102ec2d70eef5f6b6f37d850a08b9ba65e7b'
   end
 
   url "http://pcclient.download.youku.com/ikumac/youkumac_#{version}.dmg"
