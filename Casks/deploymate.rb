4861e2284ccb4d10e6a110c5f7d8c8991f15c103
Colin Dunn <dunnc@amazon.com>
2017-04-19 23:29:26 +1000

    Update deploymate to 1.3.6

--- Casks/deploymate.rb
+++ Casks/deploymate.rb
@@ -1,11 +1,11 @@
 cask 'deploymate' do
-  version '1.3.5'
-  sha256 '1f8b91e74a2c02edee21b4a2bbf938b4dfc308f771d7ef794cb54cb1c4d34f1c'
+  version '1.3.6'
+  sha256 'c1bf5bdbbb0f3fce95a7afdfb578ff821f1b41c6232f4cf9f4857072159055b8'
 
   # amazonaws.com/deploymate was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/deploymate/download/Deploymate#{version.no_dots}.zip"
   appcast 'http://www.deploymateapp.com/deploymate-updates.xml',
-          checkpoint: 'cdb5d425339c3776d0eb2ef61069180434a2ab04040734362e626d3097ef7e63'
+          checkpoint: '2557bac19dee7963a93093aeb21c2dd2820a07c780275e3c3358f07534b24312'
   name 'Deploymate'
   homepage 'http://www.deploymateapp.com/'
 
