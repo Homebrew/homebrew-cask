cf69d5fd08c499f70cde375b4ffba6cb964853d9
Colin Dunn <dunnc@amazon.com>
2017-04-19 21:50:30 +1000

    Update utopia to 3.0.2

--- Casks/utopia.rb
+++ Casks/utopia.rb
@@ -1,6 +1,6 @@
 cask 'utopia' do
-  version '3.0.0'
-  sha256 '2fc2f0ffae6ef37f803b30bee0dcd62237949fc27e29de51920c2b32fd82ab7c'
+  version '3.0.2'
+  sha256 'aec6ce346940c6e58825b7d90c0f1364743a40ff34c78a9186f1ab4f25625191'
 
   url "http://utopiadocs.com/files/Utopia-#{version}-Mac.dmg"
   name 'Utopia Documents'
