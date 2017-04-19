469f226060cabaf646343b9d61abcf2fbb25b861
Artem Medvedev <amedvedev@outlook.com>
2017-04-20 01:31:12 +0300

    Update canary to 0.965 (#32607)

--- Casks/canary.rb
+++ Casks/canary.rb
@@ -1,15 +1,20 @@
 cask 'canary' do
-  version '0.916'
-  sha256 'efd57e00bd616d3e1cf10447e14d7a91ed8075f25a99bccc0263298fb01fed2c'
+  version '0.965'
+  sha256 '6e38dc9cbf416dbca72c7331ec4026de533f9e36408d7161a2363fdfb8b7e04b'
 
   # rink.hockeyapp.net/api was verified as official when first introduced to the cask
-  url 'https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050/app_versions/236?format=zip'
+  url 'https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050/app_versions/305?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050',
-          checkpoint: '066146a5d4973df03299e08672af916e90e61954d9bd9dbf5a5201c87e84103d'
+          checkpoint: '6f149aba00dc0b5ee994e8fc95401aa9ebeefd760ed3a9fc0b51f5b3ddc45f46'
   name 'Canary'
   homepage 'https://canarymail.io/'
 
   auto_updates true
 
-  app 'canary.app'
+  app 'Canary.app'
+
+  zap delete: [
+                '~/Library/Application Scripts/Sanghani.Canary',
+                '~/Library/Containers/Sanghani.Canary',
+              ]
 end
