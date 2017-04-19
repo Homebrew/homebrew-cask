03163512bf8cb91d1fcbbd8c4b85245ed6bd4f18
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-19 00:19:16 -0400

    Update tuneinstructor to 3.6 (#32548)

--- Casks/tuneinstructor.rb
+++ Casks/tuneinstructor.rb
@@ -1,6 +1,6 @@
 cask 'tuneinstructor' do
   version '3.6'
-  sha256 '9f68e5d1d31a7a7502f84e21f1d26cd8fd3a8eba7708f07386aa641551592d14'
+  sha256 '04921ad3d8d458725baefb98d7c9a192d7e41e7ba9db3469b44dc749c292893a'
 
   url "https://www.tune-instructor.de/resources/downloads/TuneInstructor#{version}.dmg"
   name 'Tune•Instructor'
