be2163ebd85dceed3218bbe16c4482a308b21f31
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-20 07:28:39 +0800

    Update siilo.rb

--- Casks/siilo.rb
+++ Casks/siilo.rb
@@ -2,7 +2,7 @@ cask 'siilo' do
   version '0.3.2'
   sha256 '848f87c8cca1704080ee165174b9362e117a0926c25b29f00f23bdeca4897f75'
 
-  url 'http://siiloapp.com/releases/siilo-0.3.2.zip'
+  url "http://siiloapp.com/releases/siilo-#{version}.zip"
   name 'Siilo'
   homepage 'http://siiloapp.com/'
 
