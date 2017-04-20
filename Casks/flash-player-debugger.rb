67b526b272f2bfbcc1b94d35cecd434a3bbc970e
Colin Dunn <cgd@vikingdev.net>
2017-04-20 13:07:31 -0700

    Update flash-player-debugger to 24.0.0.221

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger' do
-  version '25.0.0.148'
-  sha256 '324e169d34ea9d848b51dd4e4d37bd3877e5960e8fc93115559c331e8892a27a'
+  version '24.0.0.221'
+  sha256 'f5df68ed57d7104cac8eff3542ac923150273361c097de18206f80f13a2dac93'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
