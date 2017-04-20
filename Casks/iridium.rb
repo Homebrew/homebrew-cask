b45a571bf1d7d269dd76807c1883b994aa07d863
Vítor Galvão <info@vitorgalvao.com>
2017-04-20 21:14:01 +0100

    Update iridium.rb

--- Casks/iridium.rb
+++ Casks/iridium.rb
@@ -1,8 +1,8 @@
 cask 'iridium' do
-  version '57.0'
+  version '57.0.0'
   sha256 '3fec33a6426d1ee1a52371a721b4760fa8d0bd355cf5e0060f84201859210b66'
 
-  url 'https://downloads.iridiumbrowser.de/macosx/iridium_browser_osx_latest.dmg'
+  url "https://downloads.iridiumbrowser.de/macosx/#{version}/iridium_browser_#{version}_osx_x64.dmg"
   appcast 'https://downloads.iridiumbrowser.de/macosx/',
           checkpoint: '8eecacd9cc8db0fca9979234468787faedff9a7a902f0740a23aa70a5cceca95'
   name 'Iridium Browser'
