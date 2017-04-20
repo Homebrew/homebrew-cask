b168fd7b5fa3a8319f4d14b86116c4d155fc89d7
Colin Dunn <cgd@vikingdev.net>
2017-04-20 14:58:08 -0700

    Update tunnelbear to 3.0.13

--- Casks/tunnelbear.rb
+++ Casks/tunnelbear.rb
@@ -1,11 +1,11 @@
 cask 'tunnelbear' do
-  version '3.0.12'
-  sha256 'da491e766e1dcf2fe1d36a09b9f4a32700be72810b9bb886483c40d37d547922'
+  version '3.0.13'
+  sha256 '8fad03cb8161cb0143eea16a7bec0d97a2acbc05bdfad2fd7df47e69342ca16e'
 
   # tunnelbear.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://tunnelbear.s3.amazonaws.com/downloads/mac/TunnelBear-#{version}.zip"
   appcast 'https://s3.amazonaws.com/tunnelbear/downloads/mac/appcast.xml',
-          checkpoint: '490edd55a26cb4f900f09a280cba8953228a0925c9018a2dcf7da8fe05c6497d'
+          checkpoint: '523208858b04f642c818710fe5bb2cfee6687e0c6b2df9c5885019c901894d8c'
   name 'TunnelBear'
   homepage 'https://www.tunnelbear.com/'
 
