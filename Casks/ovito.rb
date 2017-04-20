caa524b05d0ae91f87e9c1934fe4e1085f50aa91
Colin Dunn <cgd@vikingdev.net>
2017-04-20 17:25:25 +1000

    Update ovito to 2.8.2 (#32660)

--- Casks/ovito.rb
+++ Casks/ovito.rb
@@ -1,6 +1,6 @@
 cask 'ovito' do
-  version '2.8.1'
-  sha256 'ece52b1dc5b0b7e18380c4df105ecc6a6ffb9e3a8fe7cdef100744457b5a8d2a'
+  version '2.8.2'
+  sha256 'b0baaa0709aec816d87f3877a417f26d289981f60d004d7c894174e4f5b890d6'
 
   url "https://ovito.org/download/#{version}/ovito-#{version}-macos.dmg"
   name 'OVITO'
