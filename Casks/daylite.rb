08ae87d01ce6b2aef93617c7746a310f7f0f6eb2
Colin Dunn <cgd@vikingdev.net>
2017-04-21 00:34:58 +1000

    Update daylite to 6.1.1 (#32677)

--- Casks/daylite.rb
+++ Casks/daylite.rb
@@ -1,6 +1,6 @@
 cask 'daylite' do
-  version '6.0.8'
-  sha256 '564325ecd8931b8b8e69bdad473b1de68440011195fdd5944d963afe0ac76b17'
+  version '6.1.1'
+  sha256 '35d1f2a1e78e0eab29218b163c1c06dd2fed63c87e5eb64166e8009b2ea23227'
 
   url "https://download.marketcircle.com/daylite/daylitedma#{version.no_dots}.dmg"
   name 'Daylite'
@@ -8,5 +8,8 @@ cask 'daylite' do
 
   pkg 'Install Daylite & Mail Assistant.pkg'
 
-  uninstall pkgutil: 'com.marketcircle.Daylite4.*'
+  uninstall pkgutil: [
+                       'com.marketcircle.pkg.DLBase',
+                       'com.marketcircle.pkg.DMABase',
+                     ]
 end
