0faedd321782beeec4625265bec2d362969850f6
Colin Dunn <cgd@vikingdev.net>
2017-04-20 11:55:20 +1000

    Update twine to 2.1.1 (#32627)

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -1,6 +1,6 @@
 cask 'twine' do
-  version '2.1.0b5'
-  sha256 '355b2ef4ecf82ff5e696f3b7337e9d8939d0625b5e35c0a9a2889f98d25cd435'
+  version '2.1.1'
+  sha256 '4aad95da8b4fc2d6bb961ea48baeb78fdf81dd5ef9f0e5e085c31fdb66bc79f3'
 
   # bitbucket.org/klembot/twinejs was verified as official when first introduced to the cask
   url "https://bitbucket.org/klembot/twinejs/downloads/twine_#{version}_osx.zip"
