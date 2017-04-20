44d7c26588fa122e91fb00fe33ba2156dc415531
Colin Dunn <cgd@vikingdev.net>
2017-04-20 15:12:39 -0700

    Update ipvanish-vpn to 3.0.7_451

--- Casks/ipvanish-vpn.rb
+++ Casks/ipvanish-vpn.rb
@@ -1,10 +1,10 @@
 cask 'ipvanish-vpn' do
-  version '3.0.6_450'
-  sha256 '8d53d319b51bf80e02d9e37745c0119c3e5d029ac5f033d912a5ccff8dce10a0'
+  version '3.0.7_451'
+  sha256 '9cebe70f906b8ca72db40d95f68f020f300643dc975b08af6ee60a83c88bd93d'
 
   url "https://www.ipvanish.com/software/osx/IPVanish_v#{version}.zip"
   appcast 'https://www.ipvanish.com/software/osx/updates.xml',
-          checkpoint: 'd4ec0e0ee484046d05488c92e70e53b5bee194e7102d8b6097b74765700c3895'
+          checkpoint: '501e0775ce7dc3287971307dfb212a97281de85fe13428c60b69159b61178e01'
   name 'IPVanish'
   homepage 'https://www.ipvanish.com/'
 
