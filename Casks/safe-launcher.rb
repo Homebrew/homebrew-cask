f2afdc8974c14c2408b3bee2f97d9bf5ba515fda
Colin Dunn <cgd@vikingdev.net>
2017-04-20 15:39:38 +1000

    Update safe-launcher to 0.11.0 (#32651)

--- Casks/safe-launcher.rb
+++ Casks/safe-launcher.rb
@@ -1,15 +1,15 @@
 cask 'safe-launcher' do
-  version '0.10.1'
-  sha256 '55d3a8931ab43281350140361f2e2c6c2f4723397d134f965b2488763aad57bd'
+  version '0.11.0'
+  sha256 '4dd918574e835e32aaeb4a0fc23e2572df8641a432e82e201dd0c0b0c47fba3d'
 
   # github.com/maidsafe/safe_launcher was verified as official when first introduced to the cask
   url "https://github.com/maidsafe/safe_launcher/releases/download/#{version}/safe-launcher-v#{version}-osx-x64.zip"
   appcast 'https://github.com/maidsafe/safe_launcher/releases.atom',
-          checkpoint: 'a3aedc075368b7584b445a49cd7b7be66a57775452260248245a586bb317c57f'
+          checkpoint: '48376dbfa4b984bb2d6cc68d443be9147d1c440664899d8f914d3c6634c9e3dc'
   name 'SAFE Launcher'
   homepage 'https://www.maidsafe.net/'
 
-  app 'SAFE Launcher.app'
+  app "safe-launcher-v#{version}-osx-x64/SAFE Launcher.app"
 
   uninstall pkgutil: 'net.maidsafe.safelauncher',
             delete:  [
