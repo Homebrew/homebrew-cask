dedd2ccd598022cbb20d2209f42b6c440daf5bed
Colin Dunn <cgd@vikingdev.net>
2017-04-20 16:19:40 -0700

    Update growlvoice to 1.7.1

--- Casks/growlvoice.rb
+++ Casks/growlvoice.rb
@@ -1,10 +1,10 @@
 cask 'growlvoice' do
-  version '2.0.3'
-  sha256 '6c9e15b83894746e2bf9488228e95eb2f47520b1fdac728da52d16532e2ee979'
+  version '1.7.1'
+  sha256 '23c78d76060730e7f252cad0111841e1198247697b6ba7e6dea76907ef361194'
 
   url "http://www.growlvoice.com/trials/growlvoice-#{version}.zip"
   appcast 'http://www.growlvoice.com/trials/appcast.xml',
-          checkpoint: '415d7e6dcc19144b6f776d979521815c7d3768764d32a16c408890454d66ab83'
+          checkpoint: '50b95db0510b841fc1cbfe7110d043b1a1eb56239a2284636e399b6f2d436d4f'
   name 'GrowlVoice'
   homepage 'http://www.growlvoice.com/'
 
