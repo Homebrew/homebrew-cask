c9de7a4352bf01327ede7bc344b346fda9c17341
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-20 11:05:13 +0800

    Update lincastor.rb

--- Casks/lincastor.rb
+++ Casks/lincastor.rb
@@ -1,9 +1,9 @@
 cask 'lincastor' do
   version '1.4'
-  sha256 '2102986e6a7600ca18c0510ad395f027c4bb4d10beec229b3d73d806b6dfd378'
+  sha256 'fbe3af69e932cebdd0ddb76460018a4cc9194d60bb9476c2a42c2ccf59bdbba2'
 
   # dropbox.com/s/43fuhic0mhvhb6f was verified as official when first introduced to the cask
-  url 'https://www.dropbox.com/s/43fuhic0mhvhb6f/LinCastor.zip'
+  url 'https://www.dropbox.com/s/43fuhic0mhvhb6f/LinCastor.zip?dl=1'
   appcast 'https://onflapp.appspot.com/lincastor',
           checkpoint: 'dc2c8150acb3ef4d786d65bf538edd9bdef45c0a4b2ec39fa8c1dd99ffaf3f42'
   name 'LinCastor'
