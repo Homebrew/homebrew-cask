4568a9a223141f7b7c3ca304616f1da3a4d658ae
Colin Dunn <dunnc@amazon.com>
2017-04-20 10:53:14 +1000

    Update second-life-viewer to 5.0.4.325124

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,6 +1,6 @@
 cask 'second-life-viewer' do
-  version '5.0.3.324435'
-  sha256 'f34b7a99fef9939265611aaa2470a54a985406b3b7955a85505c3c8cea6a8f75'
+  version '5.0.4.325124'
+  sha256 '1b58247d166f70272080cc45dbede4fa9ea2c15e3b2d7352923e2ac2a4f3988c'
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_i386.dmg"
   name 'Linden Lab Second Life Viewer'
