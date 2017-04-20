15fc3d882110c3b02d2693fc2b36ff39583e9b8e
Colin Dunn <cgd@vikingdev.net>
2017-04-20 11:55:05 -0700

    Update jaxx to 1.2.4

--- Casks/jaxx.rb
+++ Casks/jaxx.rb
@@ -1,8 +1,8 @@
 cask 'jaxx' do
-  version '1.1.7'
-  sha256 'eaf1ffe03982d596d5d5ab570e9bc35ec5dd2dffa14b880aa58b1bdb7d71927e'
+  version '1.2.4'
+  sha256 'dd2e5fbb0164ec5dc1702513f27834331f381d3386e423608ee7ce7a73bfe470'
 
-  url "https://jaxx.io/files/#{version}/Jaxx-v#{version}-osx.dmg"
+  url "https://jaxx.io/files/#{version}/Jaxx-Installer-v#{version.major_minor}.osx.dmg"
   name 'Jaxx Blockchain Wallet'
   homepage 'https://jaxx.io/'
 
