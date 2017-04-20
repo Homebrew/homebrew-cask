ed4a49a49b98668812f97040f59497fa47d2709c
Colin Dunn <dunnc@amazon.com>
2017-04-20 13:52:55 +1000

    Update pokertracker to 4.14.24

--- Casks/pokertracker.rb
+++ Casks/pokertracker.rb
@@ -1,6 +1,6 @@
 cask 'pokertracker' do
-  version '4.14.23'
-  sha256 '3273d1eeef990e4141b69fee577deee303eafd019b8e4f9bf45843a29a8299ea'
+  version '4.14.24'
+  sha256 '6ee042c28475b475f622e5d2593bf204ced6eba0a3491302b5fd421d2e06b469'
 
   # s3-us1.ptrackupdate.com was verified as official when first introduced to the cask
   url "http://s3-us1.ptrackupdate.com/releases/PT-Install-v#{version}.dmg"
