458dcc828551f3b62294d832f63c0b9c2fad3627
Colin Dunn <dunnc@amazon.com>
2017-04-20 11:53:52 +1000

    Update multipatch to 1.6

--- Casks/multipatch.rb
+++ Casks/multipatch.rb
@@ -1,6 +1,6 @@
 cask 'multipatch' do
-  version '1.5'
-  sha256 'd69af49969f035ae0eb4bebe55bb6bca78ebe816ee32eccdf0060e8bd3d1c893'
+  version '1.6'
+  sha256 '608e745c6672b0fa8119d4e8e4e9585f72adc958d5c2632ae6fbd7ad1666421a'
 
   url "http://projects.sappharad.com/tools/multipatch#{version.no_dots}.zip"
   name 'MultiPatch'
