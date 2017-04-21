37ac7f302eaa92e706513857f2093a1f31976221
Colin Dunn <dunnc@amazon.com>
2017-04-21 17:56:19 +1000

    Update digital-power-station download link

--- Casks/digital-power-station.rb
+++ Casks/digital-power-station.rb
@@ -2,7 +2,7 @@ cask 'digital-power-station' do
   version '2.1.0.8,2017.04'
   sha256 '2432917002f4aca81a51939d281e643bae64241053dc06284f3f4b5396507760'
 
-  url "https://bongiovidps.com/wp-content/uploads/#{version.after_comma.dots_to_slashes}/Bongiovi_DPS_Mac_#{version.before_comma}.zip"
+  url "https://bongiovidps.com/wp-content/uploads/#{version.after_comma.dots_to_slashes}/Bongiovi_DPS_Mac_Setup.zip"
   name 'Bongiovi Digital Power Station Plugin'
   homepage 'https://bongiovidps.com/'
 
