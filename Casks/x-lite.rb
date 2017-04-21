81bcc7ecc87ea5c76e99361ae9b020a886c8e0c4
Colin Dunn <cgd@vikingdev.net>
2017-04-21 15:21:40 +1000

    Update x-lite to 4.9.8_84254 (#32756)

--- Casks/x-lite.rb
+++ Casks/x-lite.rb
@@ -1,6 +1,6 @@
 cask 'x-lite' do
-  version '4.9.7.1_83369'
-  sha256 '5440cfb882df118cfe8f3f934a03221cf963bb1b429e655002e36eda6c52f3c8'
+  version '4.9.8_84254'
+  sha256 '7a33cf0ea6f30e1373a71002dcd1102bc56b4e5a665f50ede00219bb9788f154'
 
   # counterpath.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://counterpath.s3.amazonaws.com/downloads/X-Lite_#{version}.dmg"
