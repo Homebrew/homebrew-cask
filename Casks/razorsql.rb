fba224ee4b6e4dd6353ea333d4296d50a0210204
Colin Dunn <cgd@vikingdev.net>
2017-04-21 00:39:25 -0700

    Update razorsql to 7.2.5

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask 'razorsql' do
-  version '7.1.0'
-  sha256 'cabc57ed66062e03250b321ac486a4b484f22bb502af80a5fe7c491dd4e9a9d0'
+  version '7.2.5'
+  sha256 '39d8f50c6d185e5643db1ce8827361ce25feaf37ee26288f459a0bcaf17644cd'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   name 'RazorSQL'
