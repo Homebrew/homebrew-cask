9b4fc76d70c99e3d82f72bb3833645ac0ac68793
Toni Suter <tonisuter@me.com>
2017-04-21 16:07:38 +0200

    removed appcast

--- Casks/cevelop.rb
+++ Casks/cevelop.rb
@@ -3,8 +3,6 @@ cask 'cevelop' do
   sha256 '5561659187360aa635ce04220544eb841e2e245f1a757c9b90f5f7a8ecef228f'
 
   url "https://www.cevelop.com/cevelop/downloads/cevelop-#{version}-macosx.cocoa.x86_64.tar.gz"
-  appcast 'https://www.cevelop.com/download/',
-          checkpoint: '913de30886b050e25b8e0996cb593a18412d2dec9d5664fac00b668cbd7e0ef9'
   name 'Cevelop'
   homepage 'https://www.cevelop.com/'
 
