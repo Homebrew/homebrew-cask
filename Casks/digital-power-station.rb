a86271d288b794ffc5e7e51f0d3205a42c33cee1
Colin Dunn <cgd@vikingdev.net>
2017-04-21 19:15:16 +1000

    Update digital-power-station to 2.1.0.8,2017.04 (#32758)
    * Update digital-power-station to 2.1.0.8,2017.04
    
    * Update digital-power-station download link

--- Casks/digital-power-station.rb
+++ Casks/digital-power-station.rb
@@ -1,14 +1,17 @@
 cask 'digital-power-station' do
-  version '2.0.0.20,2017.01'
-  sha256 '337dc9ed04906c4460a89b537907bdc5cd1ea9beb1ee42a1bd7fbfa4bd617727'
+  version '2.1.0.8,2017.04'
+  sha256 '2432917002f4aca81a51939d281e643bae64241053dc06284f3f4b5396507760'
 
-  url "https://bongiovidps.com/wp-content/uploads/#{version.after_comma.dots_to_slashes}/Bongiovi_DPS_Mac_#{version.before_comma}.zip"
+  url "https://bongiovidps.com/wp-content/uploads/#{version.after_comma.dots_to_slashes}/Bongiovi_DPS_Mac_Setup.zip"
   name 'Bongiovi Digital Power Station Plugin'
   homepage 'https://bongiovidps.com/'
 
   pkg 'Bongiovi DPS.pkg'
 
-  uninstall pkgutil: 'com.bongiovi.pkg.BongioviDPS.*',
+  uninstall pkgutil: [
+                       'com.bongiovi.pkg.BongioviDPS.*',
+                       'com.bongiovi.pkg.DigitalPowerStation.tmp.pkg',
+                     ],
             kext:    'com.bongiovi.DPSReflector',
             quit:    'com.bongiovi.DPS20'
 end
