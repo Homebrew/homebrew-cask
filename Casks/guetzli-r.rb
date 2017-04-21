136ecc9c815dbd8f78cd05c879cbb5969801be81
Andros Fenollosa <andros@fenollosa.email>
2017-04-21 18:50:15 +0200

    Fixbug indetation

--- Casks/guetzli-r.rb
+++ Casks/guetzli-r.rb
@@ -1,5 +1,5 @@
 cask 'guetzli-r' do
-	version '1.0.0'
+  version '1.0.0'
   sha256 'c050ff89f430a87c6aba55a55299db70f0a68e41eab695eecdbf4f5e6b316096'
 
   url 'https://github.com/tanrax/guetzli-recursively-gui/raw/master/dist/guetzli-r.zip'
