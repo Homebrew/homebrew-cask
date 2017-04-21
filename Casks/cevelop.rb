4ae3de247b1df5086a7ec4fc4c2d588cf6db2ee3
Toni Suter <tonisuter@users.noreply.github.com>
2017-04-21 17:39:17 +0200

    Updated cevelop to v1.7.1 (#32772)

--- Casks/cevelop.rb
+++ Casks/cevelop.rb
@@ -1,10 +1,10 @@
 cask 'cevelop' do
-  version '1.7.0-201704121451'
-  sha256 '80cf1c8b2a922e62fbdd23f7bc231feebc869b237ffc39764b1f74c04c622876'
+  version '1.7.1-201704211123'
+  sha256 '5561659187360aa635ce04220544eb841e2e245f1a757c9b90f5f7a8ecef228f'
 
   url "https://www.cevelop.com/cevelop/downloads/cevelop-#{version}-macosx.cocoa.x86_64.tar.gz"
   appcast 'https://www.cevelop.com/download/',
-          checkpoint: '8906632c2c715dce25d629c7b82321201e66614ba5631f3fbd7b1f5ac3e61686'
+          checkpoint: '913de30886b050e25b8e0996cb593a18412d2dec9d5664fac00b668cbd7e0ef9'
   name 'Cevelop'
   homepage 'https://www.cevelop.com/'
 
