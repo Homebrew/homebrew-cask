01f186a4d0eb990e7ae740ed4466f7f4e3fff85a
Colin Dunn <cgd@vikingdev.net>
2017-04-22 21:27:11 +1000

    Update gyazmail to 1.5.18 (#32814)

--- Casks/gyazmail.rb
+++ Casks/gyazmail.rb
@@ -1,10 +1,10 @@
 cask 'gyazmail' do
-  version '1.5.17'
-  sha256 '44660863538177d323702e2b952a38246dfe3ff82ed3899df0a5162fdd375c96'
+  version '1.5.18'
+  sha256 '0c0d89b8c92f158ecb2042c6e310b60de269f1e17d27e5d2140f675f40b4ac6f'
 
   url "http://gyazsquare.com/gyazmail/GyazMail-#{version.no_dots}.dmg"
   appcast 'http://gyazsquare.com/gyazmail/',
-          checkpoint: '30d8f8109aec27359a161c6b65a10d5f3d60371dae11ed32494a6e8eb1eea050'
+          checkpoint: '5f3d0523f6b92d529892e8910b161de323130ae2d3b833c4dbc52b0b7ef3ae72'
   name 'GyazMail'
   homepage 'http://gyazsquare.com/gyazmail/'
 
