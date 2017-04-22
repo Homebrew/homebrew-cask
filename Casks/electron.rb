d07829b5e6b3bba76833dafc15c739d5bbed676f
Stephane Paquet <spaquet@up4b.com>
2017-04-21 23:39:13 -0700

    Update electron to 1.6.5

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '1.3.15'
-  sha256 '660d01eeaef9c13dd47bd756b593a91eb6083163bb63c0a9eba7405dd7ebb341'
+  version '1.6.5'
+  sha256 'ad2d76b03ecb92cea828968e63c5c93042becd1adfcfebc39db594a7893817d5'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
