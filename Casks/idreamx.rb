f96b95c2c5a6cbfd0635e368227c3057d2063d25
Simon Pucher <simon.pucher@creativo.at>
2017-04-22 20:29:29 +0200

    Add iDreamX v2.7.0b
    After making all changes to the cask:
    
    - [x] brew cask audit --download {{cask_file}} is error-free.
    - [x] brew cask style --fix {{cask_file}} left no offenses.
    - [x] The commit message includes the cask’s name and version.

---
+++ Casks/idreamx.rb
@@ -0,0 +1,10 @@
+cask 'idreamx' do
+  version '2.7.0b'
+  sha256 '4acf5a39e9a9933e06c2c6a5fce887129af71eb23642b2dd13fb2821875a738e'
+
+  url 'http://tooloosoft.com/apps/iDreamX/mac/iDreamX_2.7.dmg'
+  name 'iDreamX'
+  homepage 'http://tooloosoft.com/tooloosoft/features_EN.html'
+
+  app 'iDreamx/iDreamX.app'
+end
