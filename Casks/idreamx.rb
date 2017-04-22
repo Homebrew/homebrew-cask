130f411be1ba163fbd9d69adfcc312a7c9656530
Vítor Galvão <info@vitorgalvao.com>
2017-04-22 21:28:50 +0100

    Update idreamx.rb

--- Casks/idreamx.rb
+++ Casks/idreamx.rb
@@ -1,8 +1,8 @@
 cask 'idreamx' do
-  version '2.7.0b'
+  version '2.7'
   sha256 '4acf5a39e9a9933e06c2c6a5fce887129af71eb23642b2dd13fb2821875a738e'
 
-  url 'http://tooloosoft.com/apps/iDreamX/mac/iDreamX_2.7.dmg'
+  url "http://tooloosoft.com/apps/iDreamX/mac/iDreamX_#{version}.dmg"
   name 'iDreamX'
   homepage 'http://tooloosoft.com/tooloosoft/features_EN.html'
 
