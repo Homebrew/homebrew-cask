969c86aef92a89e5177427beab75c4d2d0443688
Colin Dunn <cgd@vikingdev.net>
2017-04-22 23:15:48 +1000

    Update tweeten to 3.3.5 (#32821)

--- Casks/tweeten.rb
+++ Casks/tweeten.rb
@@ -1,11 +1,11 @@
 cask 'tweeten' do
-  version '3.3.4'
-  sha256 'a7b57365fa4b94fe85b1a8ac290de38915ae7101fa01361ce56a16316ceee21b'
+  version '3.3.5'
+  sha256 '4ec2ee9d144ea944ebab61f812618c19cd03dc6fde9d46d8d3bc9ae82a0e7ff7'
 
   # github.com/MehediH/TweetenSite was verified as official when first introduced to the cask
   url "https://github.com/MehediH/TweetenSite/releases/download/#{version}/tweeten-darwin-x64.zip"
   appcast 'https://github.com/MehediH/TweetenSite/releases.atom',
-          checkpoint: 'a8b9da6f8223ebe8cb92846b17b9d165a80d870812fdaa6db3c021458c0725e8'
+          checkpoint: 'fecfcb09ea664c7eac19e82c5dff08477ae643c1a539457418c7dd2a132275dc'
   name 'Tweeten'
   homepage 'http://tweeten.xyz/'
 
