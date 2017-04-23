a50ea8712da68ff71ee1c0375acbd8bc9d4c76c0
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-24 07:18:26 +0800

    Update anydesk.rb

--- Casks/anydesk.rb
+++ Casks/anydesk.rb
@@ -1,10 +1,10 @@
 cask 'anydesk' do
-  version '2.7.1'
-  sha256 '7d0355bb5fcbc2ae188174f5d01bcb89aa898996ed42e1667acfe7ba834770df'
+  version :latest
+  sha256 :no_check
 
-  url 'https://download.anydesk.com/anydesk.dmg?_ga=1.209100562.1850019479.1492867609'
-  name 'anydesk'
+  url 'https://download.anydesk.com/anydesk.dmg'
+  name 'AnyDesk'
   homepage 'https://anydesk.com/'
 
-  app 'anydesk.app'
+  app 'AnyDesk.app'
 end
