18e15393cde574d3df909a6a6761c44ed1607102
Artem Medvedev <amedvedev@outlook.com>
2017-04-23 03:43:08 +0300

    Fixed url

--- Casks/tablauncher.rb
+++ Casks/tablauncher.rb
@@ -2,7 +2,7 @@ cask 'tablauncher' do
   version '2.9.5'
   sha256 '97aaf949e292cfabe16b7f59cceb7cf58780a23eddcad6bf875732f16d588b3d'
 
-  url "https://tablauncher.com/updates/TabLauncher#{version}.dmg"
+  url "https://tablauncher.com/updates/TabLauncherV#{version}.dmg"
   appcast 'http://tablauncher.com/updates/appcast.xml',
           checkpoint: '45077fea58ccf4053e3921f72cd56eada2f2956bfe794cb76ab08cfe97447711'
   name 'TabLauncher'
