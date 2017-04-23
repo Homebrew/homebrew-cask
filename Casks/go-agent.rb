7186ba9746df41f89d5611ad671dbff989623114
Colin Dunn <cgd@vikingdev.net>
2017-04-23 10:07:36 +1000

    Update go-agent to 17.3.0-4704 (#32848)

--- Casks/go-agent.rb
+++ Casks/go-agent.rb
@@ -1,10 +1,10 @@
 cask 'go-agent' do
-  version '17.1.0-4511'
-  sha256 '66cad16c00d89113c7bd7b31a19cf56721334911fcdb9b98de786f2cfdcc8912'
+  version '17.3.0-4704'
+  sha256 'a3f4fc0bf61e6a75c86fe7a7651d4125fca337d4ace25bde289c7fa959d9c789'
 
   url "https://download.gocd.io/binaries/#{version}/osx/go-agent-#{version}-osx.zip"
   appcast 'https://github.com/gocd/gocd/releases.atom',
-          checkpoint: 'a03aba1085c1682f406b2a113414328bb9d40231b0adde4b945ca81676fbec23'
+          checkpoint: 'ce17e0124c329cc9c755c014579480f4ac0ffb78543db6da611c86ba09cd74b7'
   name 'Go Agent'
   homepage 'https://www.gocd.io/'
 
