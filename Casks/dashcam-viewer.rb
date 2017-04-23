c2446df8c88d43d4b1080fcacd4a26997ff1310a
Colin Dunn <cgd@vikingdev.net>
2017-04-23 20:00:58 +1000

    Update dashcam-viewer to 2.6.1 (#32870)

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -1,6 +1,6 @@
 cask 'dashcam-viewer' do
-  version '2.5.9'
-  sha256 '374fdcb918c91df5ca553f8baeffcb512112ec36975354d63510ef4855cee960'
+  version '2.6.1'
+  sha256 '314e75b533ebd690b019d4e940b9a99bd5f4fde60874cfbdddb61a9dbcd5dcde'
 
   # dropboxusercontent.com/u/25150850 was verified as official when first introduced to the cask
   url "https://dl.dropboxusercontent.com/u/25150850/Dashcam_Viewer_v#{version}.dmg"
