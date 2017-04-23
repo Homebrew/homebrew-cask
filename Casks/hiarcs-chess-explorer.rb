54486cbb65e3f2975f70625b32144533aff5250f
Colin Dunn <dunnc@amazon.com>
2017-04-23 21:36:24 +1000

    Remove 1 appcast

--- Casks/hiarcs-chess-explorer.rb
+++ Casks/hiarcs-chess-explorer.rb
@@ -3,8 +3,6 @@ cask 'hiarcs-chess-explorer' do
   sha256 'b08b328f767db0cf2aa552ede7251b69175ed78be30da23ca6a16de1d4dbaa62'
 
   url "http://www.hiarcs.com/mhce/HIARCS-Chess-Explorer-Installer-v#{version}.pkg"
-  appcast 'http://www.hiarcs.com/mac-chess-explorer.htm',
-          checkpoint: '4d2bb8ad7cfb48c48d6051f91bb9b524023f378690ee7e6166f8dc4e2e07a5b5'
   name '(Deep) HIARCS Chess Explorer'
   homepage 'http://www.hiarcs.com/mac-chess-explorer.htm'
 
