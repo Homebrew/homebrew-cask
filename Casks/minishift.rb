78fb474b8f37acb3bd960593d42c9d621ab21b73
Vítor Galvão <info@vitorgalvao.com>
2017-04-23 16:14:24 +0100

    Update minishift.rb

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -2,7 +2,7 @@ cask 'minishift' do
   version '1.0.0-rc.2'
   sha256 'e2570cf193704311d99a4f4e189e8eb115678bc15803b702b9f6228d0da26d08'
 
-  url "https://github.com/minishift/minishift/releases/download/v1.0.0-rc.2/minishift-#{version}-darwin-amd64.tgz"
+  url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom',
           checkpoint: '58e3db1b1ba4204b6df8b5f0ecb55526352884c63921f23762c349aba62f1f92'
   name 'Minishift'
