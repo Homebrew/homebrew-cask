88d6dd699b17749ee5a94524b15098679597353f
Colin Dunn <cgd@vikingdev.net>
2017-04-23 22:13:57 +1000

    Add 8 discovered appcasts (#32844)

--- Casks/isabelle.rb
+++ Casks/isabelle.rb
@@ -3,6 +3,8 @@ cask 'isabelle' do
   sha256 '99669bf27bff93be4f860993883848c49a007308ef69b363954a8c2feaa1a314'
 
   url "https://www.cl.cam.ac.uk/research/hvg/Isabelle/dist/Isabelle#{version}.dmg"
+  appcast 'http://mirror.cse.unsw.edu.au/pub/isabelle/dist/',
+          checkpoint: '3f075bb979d605106e33c69c1a27e1df72218db14093b6ff1917a6079f2c221e'
   name 'Isabelle'
   homepage 'https://www.cl.cam.ac.uk/research/hvg/Isabelle/'
 
--- Casks/jsl.rb
+++ Casks/jsl.rb
@@ -3,6 +3,8 @@ cask 'jsl' do
   sha256 'e7304d03fdb28165370e2859722c4821366e784dd86329bd246231cab610268e'
 
   url "http://www.javascriptlint.com/download/jsl-#{version}-mac.tar.gz"
+  appcast 'http://www.javascriptlint.com/download.htm',
+          checkpoint: 'd9ce005903d8a141e77b642da58e5b5ac9aa9b163849238d3a376467060ab825'
   name 'JavaScript Lint'
   homepage 'http://www.javascriptlint.com/'
 
