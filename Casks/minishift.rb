35084a92afee6ad972682f122f35b1f36f7e347d
Paul <pavel.finkelshtein@gmail.com>
2017-04-23 19:44:54 +0300

    Update minishift.rb (#32891)

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -1,14 +1,12 @@
 cask 'minishift' do
-  version '0.9.0'
-  sha256 '885251315ba9e3e7e3cb2687c8cbf534d8cfffd6c860140c739bbc37a6f78621'
+  version '1.0.0-rc.2'
+  sha256 'e2570cf193704311d99a4f4e189e8eb115678bc15803b702b9f6228d0da26d08'
 
-  url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-darwin-amd64"
+  url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom',
-          checkpoint: '98ed8bd13a92b9ef0c2179154a9ba6d7347a2d883c455068f3b3f5343b9d22d2'
+          checkpoint: '58e3db1b1ba4204b6df8b5f0ecb55526352884c63921f23762c349aba62f1f92'
   name 'Minishift'
   homepage 'https://github.com/minishift/minishift'
 
-  container type: :naked
-
-  binary 'minishift-darwin-amd64', target: 'minishift'
+  binary 'minishift'
 end
