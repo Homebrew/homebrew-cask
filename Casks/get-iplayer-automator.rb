c0a7b25a9cfdee4fe6e5e4034307c44ded5b0a5d
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-23 22:39:42 +0800

    Update get-iplayer-automator.rb

--- Casks/get-iplayer-automator.rb
+++ Casks/get-iplayer-automator.rb
@@ -2,7 +2,7 @@ cask 'get-iplayer-automator' do
   version '1.8.7'
   sha256 'd4b3cf156cb0d101a4a4c4799b92c0bdd276adcb5af73b9eeb6b7e2f75a87acb'
 
-  url "https://github.com/GetiPlayerAutomator/get-iplayer-automator/releases/download/1.8.7/GetiPlayerAutomator_#{version}.zip"
+  url "https://github.com/GetiPlayerAutomator/get-iplayer-automator/releases/download/#{version}/GetiPlayerAutomator_#{version}.zip"
   appcast 'https://github.com/GetiPlayerAutomator/get-iplayer-automator/releases.atom',
           checkpoint: '076b539e0eae2dd1a2d7ef485b1293eeeeaaaa456856e44b27374566835ef077'
   name 'Get iPlayer Automator'
