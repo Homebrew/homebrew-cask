892168853671a03d46b3c8ecac5560143b871d58
Paul <pavel.finkelshtein@gmail.com>
2017-04-23 18:12:19 +0300

    Update minishift.rb

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -8,5 +8,5 @@ cask 'minishift' do
   name 'Minishift'
   homepage 'https://github.com/minishift/minishift'
 
-  binary 'minishift-darwin-amd64', target: 'minishift'
+  binary 'minishift-#{version}-darwin-amd64.tgz', target: 'minishift'
 end
