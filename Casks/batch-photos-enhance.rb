49cc1e82b349c3cdc0080090cee9312eaddaef8e
Benjamin Troller <bctroller@gmail.com>
2017-04-23 18:02:28 -0700

    Add Batch Photos Enhance.app v2.0

---
+++ Casks/batch-photos-enhance.rb
@@ -0,0 +1,10 @@
+cask 'batch-photos-enhance' do
+  version '2.0'
+  sha256 '01ad43caeb717ad30e135f8ef10e3f7f7adb79212a47fb751c39ca4eedf374de'
+
+  url 'https://github.com/invener/Hacky-Apple-Photos-batch-enhance/releases/download/2.0/Batch.Photos.Enhance.app.zip'
+  name 'Batch Photos Enhance'
+  homepage 'https://github.com/invener/Hacky-Apple-Photos-batch-enhance'
+
+  app 'Batch Photos Enhance.app'
+end
