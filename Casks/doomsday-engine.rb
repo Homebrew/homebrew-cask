48b55012a428535afc917e6cee3ee9db6c24fa5d
Colin Dunn <cgd@vikingdev.net>
2017-04-24 00:41:25 -0700

    Update doomsday-engine to 2.0.0

--- Casks/doomsday-engine.rb
+++ Casks/doomsday-engine.rb
@@ -1,15 +1,12 @@
 cask 'doomsday-engine' do
-  version '1.15.8'
-  sha256 '6882d3ff2aa855096e003ec19b1ccca4e9b2b4576d2c667abf8e48310f08f098'
+  version '2.0.0'
+  sha256 '4694061347d8f14880de8e5973816424263552b28773596116a410c7e3c721f1'
 
   # sourceforge.net/deng was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/deng/doomsday_#{version}.dmg"
-  appcast 'https://sourceforge.net/projects/deng/rss',
-          checkpoint: '889f9570d9b2cdc64198a4766473d44f8c923affd73ff6879fed3f5feec6f87c'
+  url "https://downloads.sourceforge.net/deng/doomsday_#{version}_x86_64.dmg"
   name 'Doomsday Engine'
   homepage 'http://dengine.net/'
 
-  pkg 'Doomsday.pkg'
-
-  uninstall pkgutil: 'net.dengine.doomsday.*'
+  app 'Doomsday.app'
+  app 'Doomsday Shell.app'
 end
