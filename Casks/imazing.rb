de27be95e8cfa9c9e8f130433c1db72b7a34d11b
Colin Dunn <cgd@vikingdev.net>
2017-04-23 23:22:09 -0700

    Update imazing to 2.2.5

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -1,6 +1,6 @@
 cask 'imazing' do
-  version '2.2.4'
-  sha256 '516280f766eaa9e947ae0a60f9c7fbab5da7898c04a62b88cdcf15ac44e074b3'
+  version '2.2.5'
+  sha256 '9fd7d4f36ae53ed550d39c8f4bca207431943d8410cd5fbd0cbae4c7202809f8'
 
   # dl.devmate.com was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.DigiDNA.iMazing#{version.major}Mac/iMazing#{version.major}forMac.dmg"
