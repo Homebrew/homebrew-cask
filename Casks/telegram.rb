07372347138695d43191defa275ebd20fbb40c39
RJ Hsiao <rj19860725@gmail.com>
2017-04-25 04:06:32 +0800

    Update telegram to 2.96-94323

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,10 +1,10 @@
 cask 'telegram' do
   version '2.96-94323'
-  sha256 'e44612746753ec14c4616e5a71c50768a0d544611ba14f449cd5dc9ab7feb29a'
+  sha256 'afc6196f064368e6b6084d2b844d83f15295bc51101e0d5addc3839ddb510a70'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml',
-          checkpoint: '9b95cfd73f79a97cfd393ba6f8f689f07049afb9b2d802f0ce762af1f0720a9b'
+          checkpoint: 'bfce3755e325aa52c76a07035d4fe44bb89067a94ae1d8a25dae4c0bd1bbc698'
   name 'Telegram for macOS'
   homepage 'https://macos.telegram.org/'
 
