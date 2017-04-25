33b44c1d0ed637c30f4915832c896ba1d0a75b57
Stephen <stephengroat@users.noreply.github.com>
2017-04-24 17:46:57 -0700

    Create propelleride.rb

---
+++ Casks/propelleride.rb
@@ -0,0 +1,10 @@
+cask 'propelleride' do
+  version '0.38.5'
+  sha256 'ea7788c356dc53143156307278867a020ebbf806e87ac2540f37ebbb61b6c3e9'
+
+  url "https://github.com/parallaxinc/PropellerIDE/releases/download/#{version}/propelleride-#{version}-amd64.dmg"
+  name 'PropellerIDE'
+  homepage 'https://github.com/parallaxinc/PropellerIDE'
+
+  app 'PropellerIDE.app'
+end
