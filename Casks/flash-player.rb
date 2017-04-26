ee8471c1ecf546c056198f258fba3f0a7b25b803
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2017-04-26 18:51:11 +0200

    Fixed sha256

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,6 +1,6 @@
 cask 'flash-player' do
-  version '24.0.0.221'
-  sha256 '59b6913619a18e4e2c48068c4c11d078d2b84a8032fe3f86bf8515e384b5a3dd'
+  version '25.0.0.163'
+  sha256 '2ceeb89cf6c52802d7adb2af01dc73ca68c343280ce4563f4f19af755c132940'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
