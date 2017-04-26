3943b522bfa7e26de709241e0e5a2082e26a7972
Amit Kumar Swami <amitiitm2009@gmail.com>
2017-04-26 16:40:36 +0530

    Update fastlane to 2.28.4 (#33000)

--- Casks/fastlane.rb
+++ Casks/fastlane.rb
@@ -1,9 +1,9 @@
 cask 'fastlane' do
-  version '1.8'
+  version '2.28.4'
   sha256 '74bf78e033b64036b80832f92ece77fb12831f50380e60d5c9c7677aa433ff4b'
 
   # kits-crashlytics-com.s3.amazonaws.com/fastlane/ was verified as official when first introduced to the cask
-  url "https://kits-crashlytics-com.s3.amazonaws.com/fastlane/standalone/bundle-#{version}.zip"
+  url 'https://kits-crashlytics-com.s3.amazonaws.com/fastlane/standalone/fastlane.zip'
   name 'fastlane'
   homepage 'https://fastlane.tools/'
 
