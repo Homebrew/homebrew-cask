89506b983cafd485f993da8c8561ed2038643740
Amit <amit@moldedbits.com>
2017-04-28 01:19:21 +0530

    Update fastlane to latest

--- Casks/fastlane.rb
+++ Casks/fastlane.rb
@@ -1,6 +1,6 @@
 cask 'fastlane' do
-  version '2.28.4'
-  sha256 '74bf78e033b64036b80832f92ece77fb12831f50380e60d5c9c7677aa433ff4b'
+  version :latest
+  sha256 :no_check
 
   # kits-crashlytics-com.s3.amazonaws.com/fastlane/ was verified as official when first introduced to the cask
   url 'https://kits-crashlytics-com.s3.amazonaws.com/fastlane/standalone/fastlane.zip'
