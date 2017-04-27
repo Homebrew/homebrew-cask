dc3a3967cb66c837c255c64247ea98938d0d1456
Jussi <wickedsp1d3r@users.noreply.github.com>
2017-04-27 16:44:08 +0300

    Update steermouse to 5.1 (#33033)

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,10 +1,10 @@
 cask 'steermouse' do
-  version '5.0.8'
-  sha256 '08c6bf95b949817ab9508145c4ed714f9e39dc1617a8d013d7d0b5ca0e6567d4'
+  version '5.1'
+  sha256 '00a32b26a3ce04543ef820e9985b4b1d0514b83164be876b1b4f79cdb1dd29ac'
 
   url "http://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
   appcast 'http://plentycom.jp/en/steermouse/download.php',
-          checkpoint: '3f73e634800e6c8f79101896b7a1c6d222aa075055694185467909415ba81d26'
+          checkpoint: '20ae76fd5154d1e2ebaa6892644e202e1eb7b7607c6a4412d73a32b74b9d2a0c'
   name 'SteerMouse'
   homepage 'http://plentycom.jp/en/steermouse/'
 
