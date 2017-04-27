428e48dac2e6eed2aca76a2202f50e5059dfeb3c
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-27 20:06:10 +0800

    Update cakebrew.rb

--- Casks/cakebrew.rb
+++ Casks/cakebrew.rb
@@ -2,6 +2,7 @@ cask 'cakebrew' do
   version '1.2.3'
   sha256 '12b35753178ebb73cb71631c363ab8bdee1e06597015780c259e4483c1fa522a'
 
+  # cakebrew-377a.kxcdn.com was verified as official when first introduced to the cask
   url "https://cakebrew-377a.kxcdn.com/cakebrew-#{version}.dmg"
   appcast 'https://www.cakebrew.com/appcast/profileInfo.php',
           checkpoint: 'a1aaa9cb869aa3740faf459cf4fdbed932d5bd00375854ad6ca58a1b562624b4'
