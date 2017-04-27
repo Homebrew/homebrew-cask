db423bf85b9ceb62d039262973deedc68c4fc787
Vítor Galvão <info@vitorgalvao.com>
2017-04-27 23:19:31 +0100

    Update adapter.rb

--- Casks/adapter.rb
+++ Casks/adapter.rb
@@ -3,7 +3,7 @@ cask 'adapter' do
   sha256 'ef2de9f0795cd446d26a4de1180b2580402abe8e5f13368e42d8da39eadde729'
 
   url "http://downloads.macroplant.com/Adapter-#{version}.dmg"
-  appcast 'https://macroplant.com/adapter/mac/v2/appcast',
+  appcast "https://macroplant.com/adapter/mac/v#{version.major}/appcast",
           checkpoint: '75a11da44c802486bc6f65640aa48a730f0f684c5c07a42ba3cd1735eb3fb070'
   name 'Adapter'
   homepage 'https://macroplant.com/adapter'
