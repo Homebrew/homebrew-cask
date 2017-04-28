831334d39f60e4892a11b34e4e8e614465af43b6
giannitm <giannitammcmillan@gmail.com>
2017-04-28 07:44:12 +0100

    Update adapter to 2.1.6 (#33048)
    * Update adapter to 2.1.6
    
    * Update adapter.rb

--- Casks/adapter.rb
+++ Casks/adapter.rb
@@ -3,8 +3,8 @@ cask 'adapter' do
   sha256 'ef2de9f0795cd446d26a4de1180b2580402abe8e5f13368e42d8da39eadde729'
 
   url "http://downloads.macroplant.com/Adapter-#{version}.dmg"
-  appcast 'https://www.macroplant.com/adapter/adapterAppcast.xml',
-          checkpoint: '8198e12480f9a0c5b45c10610bf50966e8cfbe1f11dbe0ece2e5661c7fcf5306'
+  appcast "https://macroplant.com/adapter/mac/v#{version.major}/appcast",
+          checkpoint: '75a11da44c802486bc6f65640aa48a730f0f684c5c07a42ba3cd1735eb3fb070'
   name 'Adapter'
   homepage 'https://macroplant.com/adapter'
 
