e83411a164a45d10508249463d33a02595c9de17
tiiiecherle <tiiiecherle@gmail.com>
2017-04-28 09:22:35 +0200

    Update whatsapp to 0.2.4238

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,10 +1,10 @@
 cask 'whatsapp' do
-  version '0.2.3698'
-  sha256 'a780c03b102cbd14aedb0299b1a44b7115db02164ff959b1672b471e73935310'
+  version '0.2.4238'
+  sha256 '882cef96febb3756b1af32cd8568643989c04b260eeeb62e2b17ceee172d923e'
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64',
-          checkpoint: '09646589f8c45cd9ba7d0d05a0fa0535d51ac782d6f7cd88b4afa97ba373e59b'
+          checkpoint: 'e9b28780b3aae614a21b24f76008d1a3124f4fce2d39f6a9d01cd1bc81c13820'
   name 'WhatsApp'
   homepage 'https://www.whatsapp.com/'
 
