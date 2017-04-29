15baa480dd81cdb8e5aeaeda2ae7d93a6504a86e
Colin Dunn <cgd@vikingdev.net>
2017-04-29 05:46:17 -0700

    Update texturepacker to 4.4.0 (#33133)

--- Casks/texturepacker.rb
+++ Casks/texturepacker.rb
@@ -1,10 +1,10 @@
 cask 'texturepacker' do
-  version '4.3.3'
-  sha256 'cebfc0bd741015f9c6c43562c1d17e41b224c700a3a6fa67f6b8044fb68efaa4'
+  version '4.4.0'
+  sha256 '1177a22edee36cd200a52950f7ca06a3bb933a2931f8fad496047e21d42268d6'
 
   url "https://www.codeandweb.com/download/texturepacker/#{version}/TexturePacker-#{version}-uni.dmg"
   appcast 'https://www.codeandweb.com/releases/TexturePacker/appcast-mac-release.xml',
-          checkpoint: '5599a20179c04e0935117d247be0a8da982cd2d540cba2dd5da5465f350a0eef'
+          checkpoint: '5c51a647cb446128326169b77fb82d9d38c974d046f308fb9c9b556e8f6b4dac'
   name 'TexturePacker'
   homepage 'https://www.codeandweb.com/texturepacker'
 
