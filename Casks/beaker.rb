a16aa5402b1070aac97141fb40008261006f253c
giannitm <giannitammcmillan@gmail.com>
2017-04-29 18:55:02 +0100

    Update beaker to 1.7.1-0-g6dac09a (#33175)

--- Casks/beaker.rb
+++ Casks/beaker.rb
@@ -4,8 +4,8 @@ cask 'beaker' do
 
   # d299yghl10frh5.cloudfront.net was verified as official when first introduced to the cask
   url "https://d299yghl10frh5.cloudfront.net/beaker-notebook-#{version}-mac.dmg"
-  appcast 'https://github.com/twosigma/beaker-notebook/releases.atom',
-          checkpoint: 'aae840a5dd88e2e8f246c62d4310809d2b7c93fedeffbe55175106175e4c01c6'
+  appcast 'https://github.com/twosigma/beakerx/releases.atom',
+          checkpoint: '31c52c046703cf1719ff51b52b7f886834f46f7dfb17b3ee648c66786e978263'
   name 'Beaker'
   homepage 'http://beakernotebook.com/'
 
