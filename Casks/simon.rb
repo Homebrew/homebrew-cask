7cc40019565165e73dd98952af2156fc053b558f
Colin Dunn <cgd@vikingdev.net>
2017-04-29 08:08:28 -0700

    Update simon to 4.2.2

--- Casks/simon.rb
+++ Casks/simon.rb
@@ -1,10 +1,10 @@
 cask 'simon' do
-  version '4.2'
-  sha256 '9471c1d2fc4dfe0957603795bef2bcb831385ca7088d5bb956d435320fa3e1dc'
+  version '4.2.2'
+  sha256 '611a50df550d2a446616be1c794a886047c34d79b58c5701e9b7cc06f3e327ac'
 
   url "http://www.dejal.com/download/simon-#{version}.zip"
   appcast 'http://www.dejal.com/appcast/?prod=simon',
-          checkpoint: '91c57296f0f57d0c74d4c196fa4306c7630e3f701aa9166367a65366261c95d3'
+          checkpoint: 'fbf3de413753b075311558115a9bbc72579dd8048316bd816c35d923581d01d0'
   name 'Simon'
   homepage 'http://www.dejal.com/simon/'
 
