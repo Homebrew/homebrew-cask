1b677d63d35e3661e35f5781b6e44efe97f76274
Colin Dunn <cgd@vikingdev.net>
2017-04-29 05:31:46 -0700

    Update microsoft-teams to 1.0.00.10951

--- Casks/microsoft-teams.rb
+++ Casks/microsoft-teams.rb
@@ -1,10 +1,10 @@
 cask 'microsoft-teams' do
-  version '1.0.00.8201'
-  sha256 '01e8d6d19a1d50e7fe06dbfb1c4cd78df1657b361cec3751fa83a885bfef6909'
+  version '1.0.00.10951'
+  sha256 'ed19b0efcb0e68ae3d8b6966e60e2090b20da09137462c94a15981ab022874f5'
 
   url "https://statics.teams.microsoft.com/production-osx/#{version}/Teams_osx.dmg"
   appcast 'https://teams.microsoft.com/downloads/DesktopUrl?env=production&plat=osx',
-          checkpoint: 'd93cfc3109bab479ca409d6a1d9623acd4e33e7d116db6f693d19134b77c1d95'
+          checkpoint: 'd249664a8f614c9f7ee3f7fb4743dc11eb451c64963b5e7ac0a48a8bd496e07b'
   name 'Microsoft Teams'
   homepage 'https://teams.microsoft.com/downloads'
 
