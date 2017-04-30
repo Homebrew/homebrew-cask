dac1085aa8932a42b1fe57cbc869e55ea9dceda4
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-04-30 13:36:51 +0100

    Update tankix to 599

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -3,6 +3,8 @@ cask 'tankix' do
   sha256 'ea0ad56ad9f767947817930d4278118534629de39ec1c2fca69adc83b941b2a9'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/prod_#{version}/TankiX.dmg"
+  appcast 'http://www.tankix.com/en/download/',
+          checkpoint: '8ea7ac137f631e79f4d1a2d461d6864ff072ba6e78d62be32bb28db1e7fca6fb'
   name 'Tanki X'
   homepage 'https://www.tankix.com/'
 
