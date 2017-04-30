a70717829b63ec39d3676a9598327afec2a72253
Colin Dunn <cgd@vikingdev.net>
2017-04-30 15:58:58 +1000

    Update remote-desktop-manager to 4.2.1.0 (#33239)
    Also, add appcast.

--- Casks/remote-desktop-manager.rb
+++ Casks/remote-desktop-manager.rb
@@ -1,9 +1,11 @@
 cask 'remote-desktop-manager' do
-  version '4.0.1.0'
-  sha256 '490ba72bc58cd4a2b916ae730dc744f462312dce2c0d3ef4f2a9ea1f1dc9078e'
+  version '4.2.1.0'
+  sha256 'e2f5665372d9324fac8c1cda8fc7f5c3ecddb243be7bf50a57629ccfcc0ad560'
 
   # devolutions.net was verified as official when first introduced to the cask
   url "http://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Mac.#{version}.dmg"
+  appcast 'http://cdn.devolutions.net/download/Mac/RemoteDesktopManager.xml',
+          checkpoint: '37ddc1327f069c984feb9ec72ee2c22aba11da3db52fad59174bea276ecd6597'
   name 'Remote Desktop Manager'
   homepage 'https://mac.remotedesktopmanager.com/'
 
