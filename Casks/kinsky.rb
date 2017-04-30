aa0f5114f58d09f26f765dcce56a5a88a6247601
Colin Dunn <cgd@vikingdev.net>
2017-04-30 15:58:28 +1000

    Add 10 discovered appcasts (#33235)
    * apache-directory-studio
    * araxis-merge
    * axure-rp
    * digital-power-station
    * dynamite
    * elyse
    * renchman
    * kinsky
    * kivy
    * ncmeta

--- Casks/kinsky.rb
+++ Casks/kinsky.rb
@@ -3,6 +3,8 @@ cask 'kinsky' do
   sha256 '2f4f04e76863eebe1da3d77e2fbd8eee71d57cdd3950f3bac64e0d569fa104de'
 
   url "https://oss.linn.co.uk/Releases/Kinsky/Davaar/Kinsky_#{version}_osx.pkg"
+  appcast 'http://oss.linn.co.uk/Releases/Kinsky/Davaar/',
+          checkpoint: 'd9c7986a2d999b4287e34178abfa434029eda9ceeca6b327051397a01a42f017'
   name 'Kinsky'
   homepage 'https://oss.linn.co.uk/trac/wiki/DownloadKinsky'
 
