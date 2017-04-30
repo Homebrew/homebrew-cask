12efe957669a36e70fd5b71e54c6a5b06cce22ff
Gobinath Mallaiyan <i@gobinath.com>
2017-04-30 01:30:24 -0400

    Adding sparkleapp latest version
       - including new cask to add sparkleapp
    
    Closes #33245

---
+++ Casks/sparkleapp.rb
@@ -0,0 +1,10 @@
+cask 'sparkleapp' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://sparkleapp.com/download'
+  name 'sparkle'
+  homepage 'https://sparkleapp.com/'
+
+  app 'sparkle.app'
+end
