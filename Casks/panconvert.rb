f9261eba473a06b5f151ec57e018578a55c5831c
Colin Dunn <cgd@vikingdev.net>
2017-04-29 22:05:37 -0700

    Correct panconvert download path

--- Casks/panconvert.rb
+++ Casks/panconvert.rb
@@ -2,7 +2,7 @@ cask 'panconvert' do
   version '0.2.7'
   sha256 '1533540b3a28fb2ba263e9a79a813e0968c674a5ad6d370ee0163fd828ba7c42'
 
-  url "https://downloads.sourceforge.net/panconvert/PanConvert-#{version}.app.dmg"
+  url "https://downloads.sourceforge.net/panconvert/Panconvert-#{version}-osx-installer.app.dmg"
   appcast 'https://sourceforge.net/projects/panconvert/rss?path=/Newest',
           checkpoint: '5464a597ae81046b9000a9ab4e0ec722579f68262cea6e13625770b87af67a7e'
   name 'PanConvert'
