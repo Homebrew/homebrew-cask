df1bb23ba1e9d5f5c968c3c6117ba1704a8eb304
Vítor Galvão <info@vitorgalvao.com>
2017-05-01 11:55:48 +0100

    Update lrtimelapse.rb

--- Casks/lrtimelapse.rb
+++ Casks/lrtimelapse.rb
@@ -2,7 +2,7 @@ cask 'lrtimelapse' do
   version '4.7.5'
   sha256 'dde02d27b0bbc9e75868c878e6511a9af676624261c93728ebdff8bc62928313'
 
-  url "https://lrtimelapse.com/files/lrtimelapse-#{version.gsub('.', '-')}-mac/"
+  url "https://lrtimelapse.com/files/lrtimelapse-#{version.dots_to_hyphens}-mac/"
   name 'LRTimelapse'
   homepage 'https://lrtimelapse.com/'
 
