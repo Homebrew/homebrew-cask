57de9ee648fab6172038bd58d02467ecdb46f342
Victor Popkov <victor@popkov.me>
2017-05-01 20:56:25 +0300

    Update clover-configurator to 4.42.0

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,10 +1,10 @@
 cask 'clover-configurator' do
-  version '4.41.1'
-  sha256 'b766115c850e0d3b05d525ec4a8b13938693624d6bd5eb16c1f82ef16e923a8f'
+  version '4.42.0'
+  sha256 'af50e2fafa77ffd8b6a7d63b712bb1b1384531d78c763e120f4764814986c6f6'
 
   url 'http://mackie100projects.altervista.org/download-mac.php?version=vibrant'
   appcast 'http://mackie100projects.altervista.org/apps/cloverconf/10.10/update.xml',
-          checkpoint: 'a6b68978dc7d98c6d163fd22ddd185db41cdc56ef1670fb4b892655ae578b5a5'
+          checkpoint: '0e5579375fa6e060aca52d8d86e8ce4846d56e232e38e3b9a9c76695cbfb48a8'
   name 'Clover Configurator'
   homepage 'http://mackie100projects.altervista.org/'
 
