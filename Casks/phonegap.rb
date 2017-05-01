7369ad5af9263af35ae43b0911ac047c138adaf2
Victor Popkov <victor@popkov.me>
2017-05-01 21:22:38 +0300

    Update phonegap to 0.4.4 (#33300)

--- Casks/phonegap.rb
+++ Casks/phonegap.rb
@@ -1,11 +1,11 @@
 cask 'phonegap' do
-  version '0.4.3'
-  sha256 '43076b27d96f2ed31fbbdc0a6e23331caf1583a8cb3c2ae2026f9fa3d94d7944'
+  version '0.4.4'
+  sha256 'de491f77e3e55c01329e4d48c9ff02003058e6824e1a4cf971f92c128a94c145'
 
   # github.com/phonegap/phonegap-app-desktop was verified as official when first introduced to the cask
   url "https://github.com/phonegap/phonegap-app-desktop/releases/download/#{version}/PhoneGapDesktop.dmg"
   appcast 'https://github.com/phonegap/phonegap-app-desktop/releases.atom',
-          checkpoint: '60009a43e006c53790bfce55051e3bdb7f410cdfd042922a70c49d80ad4b8029'
+          checkpoint: 'd7d06c24e40da4ca8aa8ea1891b6f659edae4c8390778a4c4d2b9894f3d5f84c'
   name 'PhoneGap Desktop App'
   homepage 'http://phonegap.com/'
 
