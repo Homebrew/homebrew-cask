f76ca22ee0d26ae7066d5eb2a3cf6b13d19554f3
Vítor Galvão <info@vitorgalvao.com>
2017-05-02 10:51:33 +0100

    Update zekr.rb

--- Casks/zekr.rb
+++ Casks/zekr.rb
@@ -2,7 +2,7 @@ cask 'zekr' do
   version '1.1.0'
   sha256 '95c1fc030f250e29ab7227170d8a670d269810f2281ccd3def6ba81604672518'
 
-  # downloads.sourceforge.net/zekr/ was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/zekr was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/zekr/zekr-#{version}-mac_64.tgz"
   name 'Zekr'
   homepage 'http://zekr.org/'
