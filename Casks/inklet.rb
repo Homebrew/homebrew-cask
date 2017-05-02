d809346e7aa61bc1602d36fe7efe5f4720049892
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-02 00:50:50 -0400

    Update inklet to 2.0.3 (#33312)

--- Casks/inklet.rb
+++ Casks/inklet.rb
@@ -1,10 +1,10 @@
 cask 'inklet' do
-  version '2.0.2'
-  sha256 'd93711f48f7b1be4a0de325e91b7419ac493de89f1aa20c644814ad4bd881b0f'
+  version '2.0.3'
+  sha256 '3215946ed73d6e9156c6e809def90dc181cb60c389f2f0ff1d5ed66cc5051d84'
 
   url "https://tenonedesign.com/apps/inklet/Inklet-#{version}.zip"
   appcast 'https://tenonedesign.com/apps/inklet/profileInfo2.php',
-          checkpoint: 'ecce46a697e8866e96668802e64be7360885e323fe1c2e9f5e9260279c14e042'
+          checkpoint: 'cb9c08731198482d2bb5a0a3f7e934502dee7ab462ab546bf5414ab3317c8ce4'
   name 'Inklet'
   homepage 'https://tenonedesign.com/inklet.php'
 
