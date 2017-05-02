3e447e512078934bc09d6875ec462c8ba5a21f3f
commitay <commitay@users.noreply.github.com>
2017-05-02 18:54:30 +1000

    noise-machine

--- Casks/noise-machine.rb
+++ Casks/noise-machine.rb
@@ -1,9 +1,5 @@
 cask 'noise-machine' do
-  if MacOS.version <= :leopard
-    version '1.0'
-    sha256 '8b8d531688c259ffe4cb31fb75ec77f55bca16405fc75c1c77393f3b7e8bd447'
-    url 'http://www.publicspace.net/download/NoiseMachine_Leopard.dmg'
-  elsif MacOS.version <= :snow_leopard
+  if MacOS.version <= :snow_leopard
     version '1.05'
     sha256 '501415e60204b77e8e15b46ea08c3f8526cfeb28e6ca11c647b7bffb1f3b9d28'
     url 'http://www.publicspace.net/download/NoiseMachine_Snow_Leopard.dmg'
