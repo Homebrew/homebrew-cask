94fd90ca8cfed203f0e9ad4e76c0a9abe6edd418
commitay <commitay@users.noreply.github.com>
2017-05-02 18:49:36 +1000

    cleanapp

--- Casks/cleanapp.rb
+++ Casks/cleanapp.rb
@@ -1,8 +1,5 @@
 cask 'cleanapp' do
-  if MacOS.version <= :leopard
-    version '3.4.12'
-    sha256 'e6020a82949a55ae1eb5b7e45f736bc8b51c7d5e94503f0e4a88b5f8940ab269'
-  elsif MacOS.version <= :lion
+  if MacOS.version <= :lion
     version '4.0.8'
     sha256 'e77359586731e1ae863351846afc9fd34a7b9774625122001c0d92593a340ff1'
   else
