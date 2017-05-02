008c4c512d9ab6ae167e9dce81960ad912978e9d
Brian Dwyer <bdwyer@ieee.org>
2017-05-02 01:05:38 -0400

    OWASP-ZAP 2.6.0 Checksum (#33311)
    * OWASP-ZAP Checksum
    
    Fix the checksum
    
    * Update owasp-zap.rb

--- Casks/owasp-zap.rb
+++ Casks/owasp-zap.rb
@@ -1,11 +1,11 @@
 cask 'owasp-zap' do
   version '2.6.0'
-  sha256 '7133593b91d0d7925436adc79370b654cb1d4d642a9bb85cc3880bfaf2683b87'
+  sha256 'ed8f58c07bee746439879649a3b13bd7bffbccd6b1163c335f5eb0711ec3563d'
 
   # github.com/zaproxy/zaproxy was verified as official when first introduced to the cask
   url "https://github.com/zaproxy/zaproxy/releases/download/#{version}/ZAP_#{version.dots_to_underscores}_macos.dmg"
   appcast 'https://github.com/zaproxy/zaproxy/releases.atom',
-          checkpoint: '205f9bd6402fa384e38f5536187cda3380802698aa2e18ffdafa889a260545e3'
+          checkpoint: 'e8e7e116b1f7d63a1e484f81e5a0599cc02d8354ef3738c7d158da2000e7084d'
   name 'OWASP Zed Attack Proxy'
   name 'ZAP'
   homepage 'https://www.owasp.org/index.php/OWASP_Zed_Attack_Proxy_Project'
