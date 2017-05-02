d18977573f74dd54c58df932ecf5916bc727882d
Vincent Klaiber <vinkla@users.noreply.github.com>
2017-05-02 02:08:50 +0200

    Add Vanilla.app latest version (#33301)
    Hide menu bar icons on your Mac. http://matthewpalmer.net/vanilla/

---
+++ Casks/vanilla.rb
@@ -0,0 +1,11 @@
+cask 'vanilla' do
+  version :latest
+  sha256 :no_check
+
+  # devmate.com was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/net.matthewpalmer.Vanilla/Vanilla.dmg'
+  name 'Vanilla'
+  homepage 'http://matthewpalmer.net/vanilla'
+
+  app 'Vanilla.app'
+end
