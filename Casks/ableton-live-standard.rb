a73be42888e63793198f17c254ab3895caf151c7
decorate <ehlojames@gmail.com>
2017-05-03 00:16:29 +0100

    Update ableton-live-standard to 9.7.2 (#33340)

--- Casks/ableton-live-standard.rb
+++ Casks/ableton-live-standard.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-standard' do
-  version '9.5'
-  sha256 '5935fae913d2759e474f3c606ca247d90af419a4c24903de3f2f91f7959c3abe'
+  version '9.7.2'
+  sha256 '3f2a424dcc6680246f27bf1f759455426005321229cfb03f32787df8b5d39211'
 
   url "http://cdn2-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_64.dmg"
   name 'Ableton Live Standard'
