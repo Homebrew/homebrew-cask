6da6cd5673d2a6dac215d9a891b26e3d74c75e98
Victor Popkov <victor@popkov.me>
2017-05-03 23:17:33 +0300

    Update jwzlyrics to 1.16

--- Casks/jwzlyrics.rb
+++ Casks/jwzlyrics.rb
@@ -1,10 +1,10 @@
 cask 'jwzlyrics' do
-  version '1.15'
-  sha256 '515dac45a710739685a77fc09f70bf880a73a9cb635b099ea8e54b51c3dc2584'
+  version '1.16'
+  sha256 'c3718e9529218b1e38c1521d288dcff53d004e332cff9a4db73b4d1b293369e3'
 
   url "https://www.jwz.org/jwzlyrics/jwzlyrics-#{version}.zip"
   appcast 'https://www.jwz.org/jwzlyrics/updates.xml',
-          checkpoint: '20f0e3e4b12b9ea8436de3d36fad445f44e7271af14416ea9b2a5a7b9d7b779f'
+          checkpoint: '4b8c1b97c8e23b88424dc870b22a30c7541bc1bb76f14ed4157eab6498b253e5'
   name 'jwzlyrics'
   homepage 'https://www.jwz.org/jwzlyrics/'
 
