bd8d22152ff6986b0116ece0fd3843c11701abcf
Vítor Galvão <info@vitorgalvao.com>
2017-05-03 09:28:50 +0100

    Update fontbase to 2.0.1 (#33347)

--- Casks/fontbase.rb
+++ Casks/fontbase.rb
@@ -1,6 +1,6 @@
 cask 'fontbase' do
-  version '2.0.0'
-  sha256 '5a9169db4e5609539a3a8de0dc091ddd49a4698d47b4845c74650c8e3e328a9d'
+  version '2.0.1'
+  sha256 '012f88b19dba05bb700f0bd9e29409c1d2da30aa6355d84324eb439b9832f742'
 
   url "http://releases.fontba.se/mac/#{version}/FontBase-#{version}.dmg"
   name 'FontBase'
