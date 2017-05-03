47771e73a58383d757e0c1dbb5f821e7520dd37d
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-03 10:59:46 +0800

    Update scrabble3d.rb

--- Casks/scrabble3d.rb
+++ Casks/scrabble3d.rb
@@ -2,8 +2,8 @@ cask 'scrabble3d' do
   version :latest
   sha256 :no_check
 
+  # sourceforge.net/projects/scrabble was verified as official when first introduced to the cask
   url 'https://sourceforge.net/projects/scrabble/files/latest/download'
-  # url 'https://downloads.sourceforge.net/project/scrabble/Main_Program/MacOS/Scrabble3D-darwin.pkg'
   name 'Scrabble3D'
   homepage 'https://scrabble.sourceforge.io/'
 
