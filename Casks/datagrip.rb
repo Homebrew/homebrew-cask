96f3cc56a0714fc0e792514a1b956cc0059f8996
Alexander Hamann <neutric@users.noreply.github.com>
2017-05-03 13:14:19 +0200

    Update datagrip to 2017.1.3,171.4424.16

--- Casks/datagrip.rb
+++ Casks/datagrip.rb
@@ -1,10 +1,10 @@
 cask 'datagrip' do
-  version '2017.1.2,171.4249.43'
-  sha256 '64b60b9370ff8fcd654f0f985e2ff019644aa3abf527015b867754a98a549346'
+  version '2017.1.3,171.4424.16'
+  sha256 'c48aa6c6bc991e598298148cb8e05554e88f2aba153e9a6f31be6fe2fa0e7cb5'
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release',
-          checkpoint: '0e67d03cc3b2560bbae6df734d62cf84e989cbe9dadd632482e7df9c28320731'
+          checkpoint: '705366acab66bbf5ec73fd1da6e6c30fe49428f6a76ae235ba363613beeacf86'
   name 'DataGrip'
   homepage 'https://www.jetbrains.com/datagrip/'
 
