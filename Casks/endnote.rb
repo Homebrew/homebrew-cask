f34f91581c2721aacde02ee090f0896e7068c645
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-04 18:03:56 -0400

    Update endnote to latest

--- Casks/endnote.rb
+++ Casks/endnote.rb
@@ -2,7 +2,7 @@ cask 'endnote' do
   version :latest
   sha256 :no_check
 
-  url 'http://endnote.com/x7/EndNoteX7Installer.dmg'
+  url 'http://download.endnote.com/downloads/X8/EndNoteX8Installer.dmg'
   name 'EndNote'
   homepage 'http://endnote.com/'
 
