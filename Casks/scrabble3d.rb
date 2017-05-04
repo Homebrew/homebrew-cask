fa5797ad1830cb3fa1608dd0a32fd7862950557f
DrStrangepork <rickkasten@gmail.com>
2017-05-03 22:39:24 -0400

    Added appcast

--- Casks/scrabble3d.rb
+++ Casks/scrabble3d.rb
@@ -4,6 +4,8 @@ cask 'scrabble3d' do
 
   # downloads.sourceforge.net/scrabble was verified as official when first introduced to the cask
   url 'https://downloads.sourceforge.net/scrabble/Scrabble3D-darwin.pkg'
+  appcast 'https://sourceforge.net/projects/scrabble/rss?path=/Main_Program/MacOS',
+          checkpoint: 'd126154155cdbe7ad8c285e739a8a902e69bded1b6b980d9280495796c123351'
   name 'Scrabble3D'
   homepage 'https://scrabble.sourceforge.io/'
 
