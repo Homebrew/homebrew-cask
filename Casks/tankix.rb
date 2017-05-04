269d09ece8411e61709947c678e06c14f0dcd3f1
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-04 08:40:53 +0800

    Update tankix.rb

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -3,8 +3,6 @@ cask 'tankix' do
   sha256 'c1567c756f6f8cc3c4baf05205e584988c1153cf9180ef4a5988c663613cd3ab'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/prod_#{version}/TankiX.dmg"
-  appcast 'http://www.tankix.com/en/download/',
-          checkpoint: 'bb54941ed2be33d14982a7ef12b3353a480503b151842bebf20d8a3e429ac66f'
   name 'Tanki X'
   homepage 'https://www.tankix.com/'
 
