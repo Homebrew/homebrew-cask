3d8515deb404abdd77b43f37c8a143ad744e2f56
Francesco Di Lorenzo <fd@bendingspoons.com>
2017-05-04 10:10:07 +0200

    Add cask for Vanilla.

---
+++ Casks/vanilla.rb
@@ -0,0 +1,10 @@
+cask 'vanilla' do
+  version '1.0'
+  sha256 '5c1e4513b32544aaeeefb9e6e92d8cd816c63270c8567bceb481bd8a4a772382'
+
+  url 'https://dl.devmate.com/net.matthewpalmer.Vanilla/Vanilla.dmg'
+  name 'Vanilla'
+  homepage 'http://matthewpalmer.net/vanilla/'
+
+  app 'Vanilla.app'
+end
