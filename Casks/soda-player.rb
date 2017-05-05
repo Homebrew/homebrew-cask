9a1eb951e56e05633fcfef5b78332bc54f806622
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2017-05-05 02:53:05 -0300

    Add 'Soda Player' latest

---
+++ Casks/soda-player.rb
@@ -0,0 +1,19 @@
+cask 'soda-player' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://releases.sodaplayer.com/mac/Soda%20Player.dmg'
+  name 'Soda Player'
+  homepage 'https://www.sodaplayer.com/'
+
+  auto_updates true
+
+  app 'Soda Player.app'
+
+  zap delete: [
+                '~/Library/Application Support/Soda Player',
+                '~/Library/Preferences/com.sodaplayer.sodaplayer.plist',
+                '~/Library/Preferences/com.sodaplayer.sodaplayer-helper.plist',
+                '~/Library/Saved Application State/com.sodaplayer.sodaplayer.savedState',
+              ]
+end
