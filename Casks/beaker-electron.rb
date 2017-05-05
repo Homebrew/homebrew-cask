a242a6dd424f8e570c16fb128d30e1fa2029cc0b
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-05-05 19:24:52 +0100

    Update beaker-electron to 1.7.1-0-g6dac09a

--- Casks/beaker-electron.rb
+++ Casks/beaker-electron.rb
@@ -5,7 +5,7 @@ cask 'beaker-electron' do
   # d299yghl10frh5.cloudfront.net was verified as official when first introduced to the cask
   url "https://d299yghl10frh5.cloudfront.net/beaker-notebook-#{version}-electron-mac.dmg"
   appcast 'https://github.com/twosigma/beakerx/releases.atom',
-          checkpoint: '4fec60a4331b6add7fb39b4819cdd8989a4898ece8c16debe3b1b584a17d1399'
+          checkpoint: 'a66b88ef479e3a664e0591c30517386bc4d5e4e14a4b0294be303f6595189072'
   name 'Beaker Electron'
   homepage 'http://beakernotebook.com/'
 
