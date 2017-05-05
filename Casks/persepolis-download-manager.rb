02de8ceb9eaec3ff3f544a91570672258f919f10
Vítor Galvão <info@vitorgalvao.com>
2017-05-05 11:24:35 +0100

    Update and rename persepolis.rb to persepolis-download-manager.rb

--- Casks/persepolis-download-manager.rb
+++ Casks/persepolis-download-manager.rb
@@ -1,4 +1,4 @@
-cask 'persepolis' do
+cask 'persepolis-download-manager' do
   version '2.4.2'
   sha256 '855756ae23ff672890f05c38bd1e146ee2d4ec218a920026ac26f286a98422a4'
 
@@ -6,7 +6,7 @@ cask 'persepolis' do
   url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version.dots_to_underscores}_mac.dmg"
   appcast 'https://github.com/persepolisdm/persepolis/releases.atom',
           checkpoint: '691d6a7e5af0135e95912ee8119af7f23a1293b2b4337920dc3c63b2ac146737'
-  name 'persepolis'
+  name 'Persepolis'
   homepage 'https://persepolisdm.github.io/'
 
   app 'Persepolis Download Manager.app'
