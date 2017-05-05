9f86167518c98b7940a49ff066ca8fb3f0099838
Vítor Galvão <info@vitorgalvao.com>
2017-05-05 10:36:47 +0100

    Update soda-player.rb

--- Casks/soda-player.rb
+++ Casks/soda-player.rb
@@ -1,8 +1,10 @@
 cask 'soda-player' do
-  version :latest
-  sha256 :no_check
+  version '1.1.2'
+  sha256 'd7ba85c4afa1631f1df809b34da7e79193b88ecfbd2961d9c2f0f92ec8f9db8a'
 
   url 'https://releases.sodaplayer.com/mac/Soda%20Player.dmg'
+  appcast 'http://changelog.sodaplayer.com/',
+          checkpoint: '9dee7ffbaa9330c8909e950a4f51e167e5327ceea4cbbaa54d7b5cdc5d6999e8'
   name 'Soda Player'
   homepage 'https://www.sodaplayer.com/'
 
