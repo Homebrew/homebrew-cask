eb1ce183d98b2e6c52c932a3d5e440ca010ce4d0
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-05-05 19:55:42 +0100

    Update dragthing to 5.9.17

--- Casks/dragthing.rb
+++ Casks/dragthing.rb
@@ -5,7 +5,7 @@ cask 'dragthing' do
   # amazonaws.com/tlasystems was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/tlasystems/DragThing-#{version}.dmg"
   appcast 'http://www.dragthing.com/english/download.html',
-          checkpoint: '193085839afd838f3b81674f261e32fd6d2790e938834c245d4ca1a365501446'
+          checkpoint: '2f4f10b7778cbc2ce8198bafdc1e63cd0edb626054926cf25eb20325ea206c6d'
   name 'DragThing'
   homepage 'http://www.dragthing.com/'
 
