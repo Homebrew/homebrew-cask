3d3f2c5c5bd512f29651b03c4997686bd8a9b818
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-05-05 18:57:48 +0100

    Update atom to 1.16.0 (#33462)

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -5,7 +5,7 @@ cask 'atom' do
   # github.com/atom/atom was verified as official when first introduced to the cask
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
   appcast 'https://github.com/atom/atom/releases.atom',
-          checkpoint: '813445927f99bd25a0d7c8597822d01570d8685ec39e9089be8868925900c239'
+          checkpoint: 'bf9aa9c80548e089b764df3a386788c25a8836567a8de1271f4b939839a4df28'
   name 'Github Atom'
   homepage 'https://atom.io/'
 
