d25a31549e927edf57c2282724efddd7bacf321b
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-07 00:36:48 +0800

    Update soda-player.rb

--- Casks/soda-player.rb
+++ Casks/soda-player.rb
@@ -3,8 +3,6 @@ cask 'soda-player' do
   sha256 '759793eb97409fde0063bf954792598063457b5ea73d1ce17b477b480d2d3913'
 
   url "https://releases.sodaplayer.com/mac/Soda%20Player.dmg?version=#{version}"
-  appcast 'http://changelog.sodaplayer.com/',
-          checkpoint: '9dee7ffbaa9330c8909e950a4f51e167e5327ceea4cbbaa54d7b5cdc5d6999e8'
   name 'Soda Player'
   homepage 'https://www.sodaplayer.com/'
 
