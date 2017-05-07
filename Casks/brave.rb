6930b417c989208cef5b81b02a630e2d13c92551
giannitm <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-05-07 18:24:03 +0100

    Update brave to 0.15.2

--- Casks/brave.rb
+++ Casks/brave.rb
@@ -5,7 +5,7 @@ cask 'brave' do
   # github.com/brave/browser-laptop was verified as official when first introduced to the cask
   url "https://github.com/brave/browser-laptop/releases/download/v#{version}dev/Brave-#{version}.dmg"
   appcast 'https://github.com/brave/browser-laptop/releases.atom',
-          checkpoint: '28faec1a4f4f33af8ebe244ad22994557e875ae3a9531fadc29e80f6bbd3a110'
+          checkpoint: '38a0a5315f394fb6939a2ab26b25553cdb713c9ef315dc455076e2baee36ad3b'
   name 'Brave'
   homepage 'https://brave.com/'
 
