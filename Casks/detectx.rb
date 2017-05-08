6b2192da85366e35c77f055d21dc88d1f836a2b4
commitay <commitay1@gmail.com>
2017-05-08 10:23:49 +1000

    Update detectx to 2.60 (#33552)

--- Casks/detectx.rb
+++ Casks/detectx.rb
@@ -1,11 +1,11 @@
 cask 'detectx' do
-  version '2.59'
-  sha256 '34153586bc1f646ea84404176a14a228a08cc8c61dee152445faa27b16a5d517'
+  version '2.60'
+  sha256 '6d8dfcb67ede9ec83d71aedd1904c2b7bcf44a5208d78c5723d91c26bdbe3db6'
 
   # amazonaws.com/sqwarq.com was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/sqwarq.com/PublicZips/DetectX.app.zip'
   appcast 'https://s3.amazonaws.com/sqwarq.com/AppCasts/detectx.xml',
-          checkpoint: 'f71f9f5be2178ca9d9b70fcea4723e2b83abed7c8b724f0687f44ada006d8b37'
+          checkpoint: '325c94bb3f6bd49c85b596c5f2efe03c141047b8da94e774c84b372c115c5d85'
   name 'DetectX'
   homepage 'https://sqwarq.com/detectx/'
 
