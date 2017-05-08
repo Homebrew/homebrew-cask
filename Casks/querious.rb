2163739f94999c557de32a6febafa2ab4addd0b9
Gobinath Mallaiyan <i@gobinath.com>
2017-05-08 16:15:55 -0400

    Update querious to 2.0

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -4,7 +4,7 @@ cask 'querious' do
 
   url "https://www.araelium.com/querious/downloads/versions/Querious#{version}.zip"
   appcast 'https://arweb-assets.s3.amazonaws.com/downloads/querious/prerelease-updates.xml',
-          checkpoint: 'e3a304a3acb9e95a070530f27b13dc96dd404927fa3e8e94ee0596b647e924bf'
+          checkpoint: '72b9051269861bacefd1f4614e34f4edf22b6176c576b250c7f07290b3916135'
   name "Querious #{version.major}"
   homepage 'https://www.araelium.com/querious/'
 
