92f81ce2284099858ec35c26264b97f5956208ab
Gobinath Mallaiyan <i@gobinath.com>
2017-05-08 18:24:41 -0400

    Update zxpinstaller to 1.5.0

--- Casks/zxpinstaller.rb
+++ Casks/zxpinstaller.rb
@@ -5,7 +5,7 @@ cask 'zxpinstaller' do
   # github.com/CreativeDo/ZXPInstaller was verified as official when first introduced to the cask
   url "https://github.com/CreativeDo/ZXPInstaller/releases/download/#{version}/ZXPInstaller.dmg"
   appcast 'https://github.com/CreativeDo/ZXPInstaller/releases.atom',
-          checkpoint: 'b90c69ba778b9321c00765c0d04e0e1d6aa38db8a88b49cf4ba86928108b020a'
+          checkpoint: '184cceb310724f604287b91c59d0781983209a653ebef88ba5c285ea4c624b3b'
   name 'ZXPInstaller'
   homepage 'http://zxpinstaller.com/'
 
