b529de616b46209b75008d3d0b6c7e4317bccfa4
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-08 19:49:14 -0400

    Update rambox to 0.5.3 (#33604)

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -5,7 +5,7 @@ cask 'rambox' do
   # github.com/saenzramiro/rambox was verified as official when first introduced to the cask
   url "https://github.com/saenzramiro/rambox/releases/download/#{version}/Rambox-#{version}-mac.zip"
   appcast 'https://github.com/saenzramiro/rambox/releases.atom',
-          checkpoint: 'cee34001a896d6de679c3bdca37743246ab2d6dce5158fb6910316418d18d4d1'
+          checkpoint: 'e4787ac17d9f63f29a2ec2646b7819e0b154116bd3e248df43f18fb8e008f640'
   name 'Rambox'
   homepage 'http://rambox.pro/'
 
