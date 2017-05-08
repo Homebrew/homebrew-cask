a63a6a16291b03ddc7b4e85c7457f0d26a2b552b
Gobinath Mallaiyan <i@gobinath.com>
2017-05-08 18:17:30 -0400

    Update fiji to latest

--- Casks/fiji.rb
+++ Casks/fiji.rb
@@ -2,8 +2,8 @@ cask 'fiji' do
   version :latest
   sha256 :no_check
 
-  # jenkins.imagej.net/job/Stable-Fiji-MacOSX was verified as official when first introduced to the cask
-  url 'https://jenkins.imagej.net/job/Stable-Fiji-MacOSX/lastSuccessfulBuild/artifact/fiji-macosx.dmg'
+  # downloads.imagej.net/fiji was verified as official when first introduced to the cask
+  url 'https://downloads.imagej.net/fiji/latest/fiji-macosx.dmg'
   name 'Fiji'
   homepage 'https://fiji.sc/'
 
