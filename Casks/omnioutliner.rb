e959853510b35d8f5a0445b0c9886667d048dfd8
Vítor Galvão <info@vitorgalvao.com>
2017-05-09 00:37:40 +0100

    Update omnioutliner.rb

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -3,7 +3,7 @@ cask 'omnioutliner' do
   sha256 'e0d9e543b457efc059eba9875729ddd9196cd1c9edfc1092118ce38a0e2c4c0d'
 
   url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniOutliner-#{version}.dmg"
-  appcast 'https://update.omnigroup.com/appcast/com.omnigroup.OmniOutliner5',
+  appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniOutliner#{version.major}",
           checkpoint: '1d608a6b2dbbabbcfdbe92ddfbfc4ac531a5a4b9d8cdef0862dbda406784a8b0'
   name 'OmniOutliner'
   homepage 'https://www.omnigroup.com/omnioutliner/'
