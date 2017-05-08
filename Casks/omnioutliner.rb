82cfa3d8661e0551250ba97b0c90d4d2322d8cda
Rajiv Aaron Manglani <rajiv@alum.mit.edu>
2017-05-08 19:21:54 -0400

    update omnioutliner to 5.0.3.

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -1,6 +1,6 @@
 cask 'omnioutliner' do
-  version '5.0.2'
-  sha256 'be66619497a8c31f21ea790c24a53e41b9968957740d9f1c66ad774c57d71779'
+  version '5.0.3'
+  sha256 'e0d9e543b457efc059eba9875729ddd9196cd1c9edfc1092118ce38a0e2c4c0d'
 
   url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniOutliner-#{version}.dmg"
   name 'OmniOutliner'
