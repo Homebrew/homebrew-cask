27ab6143ee775ebb86406db0330518c61b2fdd32
Hanxue <hanxue@outlook.com>
2017-05-09 17:11:23 +0800

    Update xiami to 3.0.5 (#33678)

--- Casks/xiami.rb
+++ Casks/xiami.rb
@@ -1,9 +1,9 @@
 cask 'xiami' do
-  version '3.0.2'
-  sha256 '6fe366c25bf53597dfc955a2581f22992ba0fef766461c23964c72ade9b6a9fb'
+  version '3.0.5'
+  sha256 'af32206caeb848b65eb0e16f16aa1c94c3b1b6044eb14f99413b82947d1f10f3'
 
   # gxiami.alicdn.com/xiami-desktop was verified as official when first introduced to the cask
-  url "http://gxiami.alicdn.com/xiami-desktop/update/%E8%99%BE%E7%B1%B3%E9%9F%B3%E4%B9%90-#{version}.dmg"
+  url "http://gxiami.alicdn.com/xiami-desktop%2fupdate%2f%e8%99%be%e7%b1%b3%e9%9f%b3%e4%b9%90-#{version}.dmg"
   name 'Xiami'
   name '虾米音乐'
   homepage 'http://www.xiami.com/'
