94552fdd633a787b28094d265c4799d26780c0f3
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-09 00:21:01 -0400

    Update file-juicer.rb
    - Using versioned URL

--- Casks/file-juicer.rb
+++ Casks/file-juicer.rb
@@ -2,7 +2,7 @@ cask 'file-juicer' do
   version '4.58'
   sha256 'd5534d38a0e263b08b6376072ed739171526f2ca160a9bb64e5df60ebd282356'
 
-  url 'https://echoone.com/filejuicer/FileJuicer-4.58.zip'
+  url "https://echoone.com/filejuicer/FileJuicer-#{version}.zip"
   name 'File Juicer'
   homepage 'https://echoone.com/filejuicer/'
 
