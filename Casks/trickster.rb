3e9bc21e740918bcaa5589be7271c4a3f74c95cd
Gobinath Mallaiyan <i@gobinath.com>
2017-05-09 00:22:36 -0400

    Update trickster to 2.6

--- Casks/trickster.rb
+++ Casks/trickster.rb
@@ -4,12 +4,12 @@ cask 'trickster' do
     sha256 'cddc4a27c3c2a016f86d1688ef9708d3e8c605cfe06302470471309ccdc241db'
   else
     version '2.6'
-    sha256 'e3e3e93da2f9e1743cf597633c5443cb59f75ac8938db4dcc14b2554126cb986'
+    sha256 '73ddedc3e3622284f5111f9f85f0b8068355eeafe1f47c61990de1a3c0d86d91'
   end
 
   url "https://dl.apparentsoft.com/Trickster_#{version}.zip"
   appcast 'https://dl.apparentsoft.com/trickster.rss',
-          checkpoint: '83ad3c8336c908829948296e96ad2095d0655965080a72aafdf110129e2dfd98'
+          checkpoint: 'fd7806111308b4b506d92fbdbad2fb90661b0933a3e3c40896927eb7f1a693cf'
   name 'Trickster'
   homepage 'https://www.apparentsoft.com/trickster/'
 
