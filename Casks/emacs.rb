1605501d99890cd1e0662ac99958cadc18588cd7
midchildan <midchildan+git@gmail.com>
2017-05-09 12:38:33 +0900

    Make Emacs 25.2 cask conflict with the emacs formula

--- Casks/emacs.rb
+++ Casks/emacs.rb
@@ -8,6 +8,8 @@ cask 'emacs' do
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
 
+  conflicts_with formula: 'emacs'
+
   app 'Emacs.app'
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
