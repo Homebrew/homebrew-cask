1fb33e7afc5cd63cdebe4f63f0ab45312dacb06a
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-08 20:36:31 -0400

    Adding asciidocfx version 1.5.5 (#33625)
    * Adding asciidocfx version 1.5.5
    
    - New cask for asciidocfx
    
    ---
    Closes #33620
    
    * Update asciidocfx.rb

---
+++ Casks/asciidocfx.rb
@@ -0,0 +1,17 @@
+cask 'asciidocfx' do
+  version '1.5.5'
+  sha256 '368756a759175ff5e0a321db8b5665928bcc5b18eadeba4a788ce76ff6ab3c75'
+
+  # github.com/asciidocfx/AsciidocFX was verified as official when first introduced to the cask
+  url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac.dmg"
+  appcast 'https://github.com/asciidocfx/AsciidocFX/releases.atom',
+          checkpoint: '71e3d7c0e829ecf95331d9c9a5576bea320ea45c520516fa5ac8b9e9d738a7fe'
+  name 'AsciidocFX'
+  homepage 'http://www.asciidocfx.com/'
+
+  installer manual: 'AsciidocFX Installer.app'
+
+  uninstall script: {
+                      executable: '/Applications/AsciidocFX/AsciidocFX Uninstaller.app',
+                    }
+end
