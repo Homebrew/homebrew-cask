e39ec37c590498f6e0e2197d33cf6dd305808606
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-09 08:28:24 +0800

    Update asciidocfx.rb

--- Casks/asciidocfx.rb
+++ Casks/asciidocfx.rb
@@ -4,12 +4,14 @@ cask 'asciidocfx' do
 
   # github.com/asciidocfx/AsciidocFX was verified as official when first introduced to the cask
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac.dmg"
+  appcast 'https://github.com/asciidocfx/AsciidocFX/releases.atom',
+          checkpoint: '71e3d7c0e829ecf95331d9c9a5576bea320ea45c520516fa5ac8b9e9d738a7fe'
   name 'AsciidocFX'
   homepage 'http://www.asciidocfx.com/'
 
   installer manual: 'AsciidocFX Installer.app'
 
   uninstall script: {
-                      executable: "/Applications/AsciidocFX/AsciidocFX Uninstaller.app",
+                      executable: '/Applications/AsciidocFX/AsciidocFX Uninstaller.app',
                     }
 end
