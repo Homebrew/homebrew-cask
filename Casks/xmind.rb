2de85611b6d352884bbd270b7ca2b3c892c01ec2
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-09 00:54:48 -0400

    Update xmind to 8-update2 (#33673)

--- Casks/xmind.rb
+++ Casks/xmind.rb
@@ -1,6 +1,6 @@
 cask 'xmind' do
-  version '8-update1'
-  sha256 'e0a8a534746c9a31be890afc429fb3d8059db79e52d9d205c054d9d9ef4c5ffb'
+  version '8-update2'
+  sha256 '4c2d3a367c84754ff969a1a9e28725af85e79121648555ed99fd60a94d9ea5ab'
 
   url "https://www.xmind.net/xmind/downloads/xmind-#{version}-macosx.dmg"
   name 'XMind'
