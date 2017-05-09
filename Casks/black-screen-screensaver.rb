46e206bcd1b1c2b5dda7225c817f118f93ef152e
Simon Lundström <simlu@su.se>
2017-05-09 13:42:03 +0200

    Add Black Screen screensaver 1.1

---
+++ Casks/black-screen-screensaver.rb
@@ -0,0 +1,10 @@
+cask 'black-screen-screensaver' do
+  version '1.1'
+  sha256 'ce4fbc714a4808947304f40874a9a67458076408131a7fce66dc1417ab4c5c2d'
+
+  url 'http://www.dr-lex.be/software/download/BlackScreen1_1.zip'
+  name 'Black Screen screensaver'
+  homepage 'http://www.dr-lex.be/software/screensavers.html'
+
+  screen_saver 'Black Screen/BlackScreen.saver'
+end
