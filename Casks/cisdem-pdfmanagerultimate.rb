a02cd49f5aa4cdc6db5f12cebd7900a6145ea4eb
Gobinath Mallaiyan <i@gobinath.com>
2017-05-09 22:49:33 -0400

    Update cisdem-pdfmanagerultimate to 2.5.0

--- Casks/cisdem-pdfmanagerultimate.rb
+++ Casks/cisdem-pdfmanagerultimate.rb
@@ -1,8 +1,8 @@
 cask 'cisdem-pdfmanagerultimate' do
-  version :latest
-  sha256 :no_check
+  version '2.5.0'
+  sha256 '572ed99e8ea18474a0dfe7fd42e0fe704e40607f12a979ada704054fc81d05de'
 
-  url 'https://www.cisdem.com/download/cisdem-pdfmanagerultimate.dmg'
+  url 'https://www.cisdem.com/downloads/cisdem-pdfmanagerultimate-15.dmg'
   name 'Cisdem PDFManagerUltimate'
   homepage 'https://www.cisdem.com/pdf-manager-ultimate-mac.html'
 
