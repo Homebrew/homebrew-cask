1bff14f0fd05b25f20304f0c262191b41ddb4b2e
Gobinath Mallaiyan <i@gobinath.com>
2017-05-10 09:48:39 -0400

    Update solvespace to 2.3 (#33786)

--- Casks/solvespace.rb
+++ Casks/solvespace.rb
@@ -5,7 +5,7 @@ cask 'solvespace' do
   # github.com was verified as official when first introduced to the cask
   url "https://github.com/solvespace/solvespace/releases/download/v#{version}/solvespace.dmg"
   appcast 'https://github.com/solvespace/solvespace/releases.atom',
-          checkpoint: '9941a53da74844d3eb4f36a905494f70ed3498e6fdbb18370792370e60ad2468'
+          checkpoint: '80948af9a3b9048b69f498307fe2cd184953586768ac2cdb9d4d24d589bf5ff1'
   name 'SolveSpace'
   homepage 'http://solvespace.com/index.pl/'
 
