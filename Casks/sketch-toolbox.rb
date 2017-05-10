ff1a1926ccfe207185670a51653423d4a0cb70fd
Gobinath Mallaiyan <i@gobinath.com>
2017-05-10 09:35:29 -0400

    Update sketch-toolbox to 0.5.1

--- Casks/sketch-toolbox.rb
+++ Casks/sketch-toolbox.rb
@@ -4,7 +4,7 @@ cask 'sketch-toolbox' do
 
   url 'http://sketchtoolbox.com/Sketch%20Toolbox.zip'
   appcast 'http://sketchtoolbox.com/updates/appcast.xml',
-          checkpoint: '61636d433e619f4b56206366764c85f000715f7741d393ca30c214f36aad6c91'
+          checkpoint: '9e5898e1e9af4f4d92072ff4bd57fb3726db309c1cee55ea3552c8a71c9b8963'
   name 'Sketch Toolbox'
   homepage 'http://sketchtoolbox.com/'
 
