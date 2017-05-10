1acacfcbfdf804b4b3c98f8855603c7ffb78157a
Gobinath Mallaiyan <i@gobinath.com>
2017-05-10 09:51:27 -0400

    Update sonic-pi to z

--- Casks/sonic-pi.rb
+++ Casks/sonic-pi.rb
@@ -1,10 +1,10 @@
 cask 'sonic-pi' do
-  version '2.11.1'
-  sha256 'fbe9d5939d296b9ffae69432dffb6a6b1b4e06d5d89754d3c110a5cfdd5cd954'
+  version 'z'
+  sha256 '9f0e93247f8bea1c41e37a78b000b610ca3d8df45ce93c53bc2bcf305c9abf93'
 
   url "http://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-v#{version}.dmg"
   appcast 'https://github.com/samaaron/sonic-pi/releases.atom',
-          checkpoint: 'de0a077ee24d16baa40620e5cabc991c2b8dcfabf71893b4d014c384a442ed9c'
+          checkpoint: 'eb4ec1bd1e5fabf1dca795111628996bb3eef2696fd2ca52d866ca4f0337ded9'
   name 'Sonic Pi'
   homepage 'http://sonic-pi.net/'
 
