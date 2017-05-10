2195ac4ae8d561054650ee72b3d076dfb869fa6b
Gobinath Mallaiyan <i@gobinath.com>
2017-05-09 23:41:20 -0400

    Update comictagger to 1.1.16-beta-rc2

--- Casks/comictagger.rb
+++ Casks/comictagger.rb
@@ -1,6 +1,6 @@
 cask 'comictagger' do
-  version '1.1.15-beta'
-  sha256 '6640834d966c1cc760de6aa32729bf139bf06727f8d650a4534cdf780f084960'
+  version '1.1.16-beta-rc2'
+  sha256 '3d2d883371a22074205ef96f98fdd0ccc35399f9439bc235b6f4c1c21e856bc5'
 
   url "https://github.com/davide-romanini/comictagger/releases/download/#{version}/ComicTagger-#{version}.dmg"
   appcast 'https://github.com/davide-romanini/comictagger/releases.atom',
