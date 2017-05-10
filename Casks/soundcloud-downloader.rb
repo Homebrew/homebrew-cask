34f3f09ae03d04e649181eb42fad9ff555d659bf
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-10 15:09:05 -0400

    Update soundcloud-downloader to 2.7.7 (#33791)
    * Update soundcloud-downloader to 2.7.2
    
    * Update soundcloud-downloader.rb

--- Casks/soundcloud-downloader.rb
+++ Casks/soundcloud-downloader.rb
@@ -1,10 +1,10 @@
 cask 'soundcloud-downloader' do
-  version '2.7.2'
-  sha256 '0272b1ebd6c6d28e4737ec08cb6ef560cc4c9f2b9d626806d4bef58fb37e278b'
+  version '2.7.7'
+  sha256 'fa43678ebedfd71edf3ee6972f824aa5f39b55f640e011b408a6e9b69878703b'
 
   url "http://black-burn.ch/scd/downloads/#{version.no_dots}/in/b"
-  appcast 'http://black-burn.ch/applications/scd/updates.php?hwni=1',
-          checkpoint: '18614c69ab696d1af1aeee1edf18c87ebc29aa81af2c33d1ab283e8f869aafb8'
+  appcast 'https://black-burn.ch/apps/SCD2/updates/gold.xml?hwni=1',
+          checkpoint: '26e98b02b515fa664bd395dfe5ecaac54a8dba3507953bf245a3524f7099ea46'
   name 'SoundCloud Downloader'
   homepage 'https://black-burn.ch/scd/'
 
