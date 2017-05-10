95f263ef5ab774da5f32bd03960a2265055cd030
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-10 00:15:12 -0400

    Update screencat to 4.2.0 (#33744)

--- Casks/screencat.rb
+++ Casks/screencat.rb
@@ -5,7 +5,7 @@ cask 'screencat' do
   # github.com/maxogden/screencat was verified as official when first introduced to the cask
   url "https://github.com/maxogden/screencat/releases/download/v#{version}/ScreenCat.zip"
   appcast 'https://github.com/maxogden/screencat/releases.atom',
-          checkpoint: '5fdac9979072880ea612a383a066a2eaf6844181f578221d71d10a1604e69ead'
+          checkpoint: '3e8451b689135fa55033b526c18366ec0b295af0896ccc8f53e978d29d715fd4'
   name 'ScreenCat'
   homepage 'https://maxogden.github.io/screencat/'
 
