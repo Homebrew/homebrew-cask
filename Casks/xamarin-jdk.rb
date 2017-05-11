59cb165007f197783f227c7a2341ab6bae3f8af6
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-10 21:05:13 -0400

    Update xamarin-jdk to 8.101 (#33837)

--- Casks/xamarin-jdk.rb
+++ Casks/xamarin-jdk.rb
@@ -3,8 +3,8 @@ cask 'xamarin-jdk' do
   sha256 '680de8ddead3867fc34e7ff380f437c7ddb8dc75eb606186a3e8ae7e3b8c7fbc'
 
   url "https://download.xamarin.com/Installer/MonoForAndroid/jdk-#{version.major}u#{version.minor}-macosx-x64.dmg"
-  appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: 'c93e56fc4aef0c3daf9964c91543d76d9c68f557ad8b9e8254a9b11e66ed4bb8'
+  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
+          checkpoint: '477c85901ee0146c2f34467c8cfc1f3a94bcb96c5467bb8b5cfc1ed765b3676e'
   name 'Xamarin Java JDK'
   homepage 'https://www.xamarin.com/platform'
 
