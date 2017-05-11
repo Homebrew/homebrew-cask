c64cb1e16e069c86e5f41555d1a81349d4215aaa
commitay <commitay@users.noreply.github.com>
2017-05-12 09:57:27 +1000

    Update mono-mdk - add appcast

--- Casks/mono-mdk.rb
+++ Casks/mono-mdk.rb
@@ -4,6 +4,8 @@ cask 'mono-mdk' do
 
   # mono-project.azureedge.net/archive was verified as official when first introduced to the cask
   url "https://mono-project.azureedge.net/archive/#{version.major_minor_patch}/macos-10-universal/MonoFramework-MDK-#{version}.macos10.xamarin.universal.pkg"
+  appcast 'http://www.mono-project.com/docs/about-mono/releases/index.html',
+          checkpoint: 'e4491dc51d494c0af9739a68d2da0582718146aaff098ff94578189ca7bbb22f'
   name 'Mono'
   homepage 'http://www.mono-project.com/'
 
