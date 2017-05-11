6a17483556915bf64e79099f9a75bba418d9d372
Gobinath Mallaiyan <i@gobinath.com>
2017-05-10 23:53:03 -0400

    Update fldigi to 4.0.4

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -1,10 +1,10 @@
 cask 'fldigi' do
-  version '4.0.3'
-  sha256 '10ede0d058dc1c1887a6cfc9f8ffa6845dd02be52b76cc622937f0e5be2b6bc0'
+  version '4.0.4'
+  sha256 '0a9399fe18aa730a475e312e81329c8379f9abc5320da3a52b29b560ae144a8f'
 
   url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}_i386.dmg"
   appcast 'https://sourceforge.net/projects/fldigi/rss?path=/fldigi',
-          checkpoint: '48e0dbed30c72fd68197d712b0771560ece6e2cbb259ec80e9d7782528b0da43'
+          checkpoint: 'bf711ab3dc2e996d846fc5222ba9e8d9abedc9443be4626013f626dc412faf0f'
   name 'fldigi'
   homepage 'https://sourceforge.net/projects/fldigi/files/fldigi/'
 
