6bee51d17125782e3a110045f6fcd6448dda40ae
giannitm <giannitm@users.noreply.github.com>
2017-05-12 20:30:40 +0100

    Update grids to 4.5.6

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,10 +1,10 @@
 cask 'grids' do
-  version '4.5.2'
-  sha256 '6e8a669b804caebce0c25206c1a4825d03f5f42e58480f223f69a0621d5b8541'
+  version '4.5.6'
+  sha256 '3d80066672470cef0b889ed540384a9733daf6fdab189f023f9753a54fca6d10'
 
   url "http://thegridsapp.com/bin/Grids_#{version}.dmg"
   appcast 'http://thegridsapp.com/appcast.json',
-          checkpoint: 'b2179adb47b181e99b7e55a1c3cf8b9978f92ed9baaf7816b96507e070ce8627'
+          checkpoint: '90d59bd1f2925bf6592c01f6e43df7daf5f23f71e69c044fe17f2b021b67126a'
   name 'Grids'
   homepage 'http://thegridsapp.com/'
 
