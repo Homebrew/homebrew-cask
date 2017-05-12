5ca066b89a12511bfe60e12d53085ffe9cd73db7
commitay <commitay1@gmail.com>
2017-05-12 18:19:08 +1000

    Update etcher to 1.0.0 (#33908)

--- Casks/etcher.rb
+++ Casks/etcher.rb
@@ -1,11 +1,11 @@
 cask 'etcher' do
-  version '1.0.0-rc.5'
-  sha256 '119840577a54057d4893b27e8ea67e700949097ca09ea2b9b9070a7f43d10f50'
+  version '1.0.0'
+  sha256 '11259ae4a3b36a60092bdf365fd3f54eba854e9f0a88e661d5a7969d26d51fae'
 
   # resin-production-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://resin-production-downloads.s3.amazonaws.com/etcher/#{version}/Etcher-#{version}-darwin-x64.dmg"
   appcast 'https://github.com/resin-io/etcher/releases.atom',
-          checkpoint: '8768b21701a9b4540cd06c243bbe06dea6b13b1a7b974f7b59cab632dd9733d6'
+          checkpoint: '0c2518d73ad6620d615e52dbf6dffe9edb525915330ecd020e92b54dabbe4f30'
   name 'Etcher'
   homepage 'https://etcher.io/'
 
