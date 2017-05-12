6946193e68fd11761955fdbfe3f119eff894262d
giannitm <giannitm@users.noreply.github.com>
2017-05-12 21:03:15 +0100

    Update headset to 1.5.2

--- Casks/headset.rb
+++ Casks/headset.rb
@@ -5,7 +5,7 @@ cask 'headset' do
   # github.com/headsetapp/headset-electron was verified as official when first introduced to the cask
   url "https://github.com/headsetapp/headset-electron/releases/download/v#{version}/Headset-#{version}.dmg"
   appcast 'https://github.com/headsetapp/headset-electron/releases.atom',
-          checkpoint: 'c618b5228f592eff07978e49e299392740903fb884edca467af30eea60acb79b'
+          checkpoint: 'ede19e767bd5b40cfa82ab5012ada8870d1c66fd80a6a890705b567655b9b8d0'
   name 'Heasdset'
   homepage 'http://headsetapp.co/'
 
