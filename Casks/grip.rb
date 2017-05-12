90b415e354dc973dbefa712fd4405364b5df0944
giannitm <giannitm@users.noreply.github.com>
2017-05-12 20:37:00 +0100

    Update grip to 1.5.2

--- Casks/grip.rb
+++ Casks/grip.rb
@@ -5,7 +5,7 @@ cask 'grip' do
   # github.com/WPIRoboticsProjects/GRIP was verified as official when first introduced to the cask
   url "https://github.com/WPIRoboticsProjects/GRIP/releases/download/v#{version}/GRIP-v#{version}-x64.dmg"
   appcast 'https://github.com/WPIRoboticsProjects/GRIP/releases.atom',
-          checkpoint: '705846f8990f130fa5fd9faa0163f758b28b73653050a490f7d2a06f83ef2141'
+          checkpoint: '626b1f5a2092d8db1682e2abf1137cbc0fc7f98e1f025e29a190effab438b3dd'
   name 'GRIP Computer Vision Engine'
   name 'GRIP'
   homepage 'https://wpiroboticsprojects.github.io/GRIP/'
