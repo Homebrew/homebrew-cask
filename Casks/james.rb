697f3706b6de4999419817ae16c5c654e026c62e
Laurent Goussard <loranger@free.fr>
2017-05-13 23:14:03 +0200

    Upg Version number as variable

--- Casks/james.rb
+++ Casks/james.rb
@@ -2,7 +2,7 @@ cask 'james' do
   version '1.5.0'
   sha256 '6c0dae2282913d11527d302e13f7a0b08d6a892fcdc0e665a222f811e127e61d'
 
-  url 'https://github.com/james-proxy/james/releases/download/v1.5.0/james-1.5.0.dmg'
+  url 'https://github.com/james-proxy/james/releases/download/v#{version}/james-#{version}.dmg'
   name 'James'
   homepage 'https://github.com/james-proxy/james'
 
