d5a0386adb3a9883a8e9825459a6319889aaaac4
Laurent Goussard <loranger@free.fr>
2017-05-14 00:29:01 +0200

    Fix style guidelines

--- Casks/james.rb
+++ Casks/james.rb
@@ -3,7 +3,8 @@ cask 'james' do
   sha256 '6c0dae2282913d11527d302e13f7a0b08d6a892fcdc0e665a222f811e127e61d'
 
   url "https://github.com/james-proxy/james/releases/download/v#{version}/james-#{version}.dmg"
-  appcast 'https://github.com/james-proxy/james/releases.atom', checkpoint: '59daa019d6198f4d7fa68e1c8fec2dd8075f293a9103ce0d75611d618fdf5f9a'
+  appcast 'https://github.com/james-proxy/james/releases.atom',
+          checkpoint: '59daa019d6198f4d7fa68e1c8fec2dd8075f293a9103ce0d75611d618fdf5f9a'
   name 'James'
   homepage 'https://github.com/james-proxy/james'
 
