0277cff5958453f0884298fd3ea6cca8f2d0ca7d
guoc <guochen42@gmail.com>
2017-05-13 17:30:09 +1000

    Add SpaceLauncher.app :latest

---
+++ Casks/spacelauncher.rb
@@ -0,0 +1,10 @@
+cask 'spacelauncher' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://spacelauncherapp.com/download/SpaceLauncher.zip'
+  name 'SpaceLauncher'
+  homepage 'https://spacelauncherapp.com/'
+
+  app 'SpaceLauncher.app'
+end
