cdbd6fc4154d4063a3db07d3189e6474f3ed1ede
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-05-13 18:50:00 +0100

    Update loadmytracks to 1.4.7,099 (#34013)

--- Casks/loadmytracks.rb
+++ Casks/loadmytracks.rb
@@ -5,7 +5,7 @@ cask 'loadmytracks' do
   # cluetrust.com was verified as official when first introduced to the cask
   url "https://www.cluetrust.com/Downloads/LoadMyTracks_#{version.after_comma}.dmg"
   appcast 'https://www.cluetrust.com/AppCasts/LoadMyTracks.xml',
-          checkpoint: 'bef86b055707d407eb2835db82e07d615968d344450417515cae5719fd97cefd'
+          checkpoint: '9f1b54bf491bc03b35b45499e4cf4c3599ae3bdc6363f5ce3d14f420bd7f84d7'
   name 'LoadMyTracks'
   homepage 'https://www.loadmytracks.com/'
 
