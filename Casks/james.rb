7454c9f5419f3eb9514d3e968ae01c233aa1d74e
Laurent Goussard <loranger@free.fr>
2017-05-13 22:51:39 +0200

    Add James v1.5.0

---
+++ Casks/james.rb
@@ -0,0 +1,10 @@
+cask 'james' do
+  version '1.5.0'
+  sha256 '6c0dae2282913d11527d302e13f7a0b08d6a892fcdc0e665a222f811e127e61d'
+
+  url 'https://github.com/james-proxy/james/releases/download/v1.5.0/james-1.5.0.dmg'
+  name 'James'
+  homepage 'https://github.com/james-proxy/james'
+
+  app 'James.app'
+end
