87a2eab84fd88fa3a4c3b2a831fba63244aeb750
Kevin Hwang <kevinhwang@eng.ucsd.edu>
2017-05-13 12:50:55 -0700

    Update hyper to 1.3.3 (#34054)

--- Casks/hyper.rb
+++ Casks/hyper.rb
@@ -1,11 +1,11 @@
 cask 'hyper' do
-  version '1.3.2'
-  sha256 '719576676f5c5037c04e77a6a5c4aac82df4fd132a5a3521293f619f67114257'
+  version '1.3.3'
+  sha256 'b99e11d38f389bc759ce14d00b5665e4dceb4da05d2a563e7ba20d2cd2a71d53'
 
   # github.com/zeit/hyper was verified as official when first introduced to the cask
   url "https://github.com/zeit/hyper/releases/download/#{version}/hyper-#{version}-mac.zip"
   appcast 'https://github.com/zeit/hyper/releases.atom',
-          checkpoint: '65da4620f6941662e33be4c09b49c8ea87a8644d99cbc1d375c6ead0aa02bdd8'
+          checkpoint: '7fd30ef5c924d11ad85525e7be3dd41d4b821ae7bd675771fd97d95067357a36'
   name 'Hyper'
   homepage 'https://hyper.is/'
 
