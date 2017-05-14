ec7259b3bf297c0e5759e95e7fe1ed35f93795f6
giannitm <giannitm@users.noreply.github.com>
2017-05-14 10:32:08 +0100

    Update moom to 3.2.6

--- Casks/moom.rb
+++ Casks/moom.rb
@@ -2,9 +2,9 @@ cask 'moom' do
   version '3.2.6'
   sha256 '482dd69788ac707f13483631eae5aaef7fcc356b688d51e359a3b69fc531a143'
 
-  url 'https://manytricks.com/download/moom'
-  appcast 'https://manytricks.com/moom/appcast.xml',
-          checkpoint: 'ea75ccd05a0a5ffbff3da7692f5fb60315b6a13dc055a44562c07c38588edb41'
+  url "https://manytricks.com/download/_do_not_hotlink_/moom#{version.no_dots}.dmg"
+  appcast 'https://manytricks.com/moom/appcast',
+          checkpoint: 'bc2bee38bf81ffd05241e99899af816ba4807a96864e84ec0c89aa50b9e3d93d'
   name 'Moom'
   homepage 'https://manytricks.com/moom/'
 
