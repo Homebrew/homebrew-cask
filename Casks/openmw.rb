1127c1cfaaccf500beaeea56a4c857df97462b1a
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-05-14 23:40:46 +0100

    Update openmw to 0.41.0 (#34138)

--- Casks/openmw.rb
+++ Casks/openmw.rb
@@ -5,7 +5,7 @@ cask 'openmw' do
   # github.com/OpenMW/openmw was verified as official when first introduced to the cask
   url "https://github.com/OpenMW/openmw/releases/download/openmw-#{version}/OpenMW-#{version}.dmg"
   appcast 'https://github.com/OpenMW/openmw/releases.atom',
-          checkpoint: 'b6c39884fbdf3cc3a0c83691442bd8575994a25f5f45ce4dc1ad6d39f38111fb'
+          checkpoint: '37100067008b122d88a15d3b253f9fbeb371eb6b3be5764660105cd7332e74ab'
   name 'OpenMW'
   homepage 'https://openmw.org/'
 
