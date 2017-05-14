37c1c3ae3fc0800750e220a988aca0e2589bc0ba
giannitm <giannitm@users.noreply.github.com>
2017-05-14 14:11:32 +0100

    Update ndm to 1.1.1

--- Casks/ndm.rb
+++ Casks/ndm.rb
@@ -5,7 +5,7 @@ cask 'ndm' do
   # github.com/720kb/ndm was verified as official when first introduced to the cask
   url "https://github.com/720kb/ndm/releases/download/v#{version}/ndm-#{version}.dmg"
   appcast 'https://github.com/720kb/ndm/releases.atom',
-          checkpoint: '5eb0a91feca304dc4980ed708a2db3096dfd822b4c5ebc630222b4293bb5b1af'
+          checkpoint: '8c1048c46c654a57cac40d4d076070577f15f3a263690909c07dafe236858c78'
   name 'ndm'
   homepage 'https://720kb.github.io/ndm/'
 
