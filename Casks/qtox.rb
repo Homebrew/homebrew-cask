a4f3be3311757fa598f7b184bbbe0d777a85cd7b
commitay <commitay1@gmail.com>
2017-05-14 23:09:17 +1000

    Update qtox to 1.10.1 (#34098)

--- Casks/qtox.rb
+++ Casks/qtox.rb
@@ -1,11 +1,11 @@
 cask 'qtox' do
-  version '1.10.0'
-  sha256 'f9adc318ea4b89c089ad11ef6a74dec7fa83499a232c1ebe9fdebdcf4fc58265'
+  version '1.10.1'
+  sha256 '611b4ce4792e5b99dac4095cc8ec9dde7641f3985acaf74151721c2e6523616b'
 
   # github.com/tux3/qTox was verified as official when first introduced to the cask
   url "https://github.com/tux3/qTox/releases/download/v#{version}/qTox.dmg"
   appcast 'https://github.com/tux3/qtox/releases.atom',
-          checkpoint: 'd74c248f0c50c446be1676b1fc490a027c0e997d42d0320b4e652a00ee0b967b'
+          checkpoint: '3d680b77edfd26458214ee7774bbb733bad55f6ef9ab5c10e1e2289b2e897d58'
   name 'qTox'
   homepage 'https://qtox.github.io/'
 
