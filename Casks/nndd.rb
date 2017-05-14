2e9e8ae42bdc77034efe732ad47e6f9b511d8393
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-05-14 23:12:49 +0100

    Update nndd to 2.4.3,62201 (#34121)

--- Casks/nndd.rb
+++ Casks/nndd.rb
@@ -3,8 +3,8 @@ cask 'nndd' do
   sha256 '6a73dcad2e73d877ad1503ed1162cae1a1c84f21d1abaa6aaf9b31bb2fbca531'
 
   url "http://dl.osdn.jp/nndd/#{version.after_comma}/NNDD_v#{version.before_comma.dots_to_underscores}.dmg"
-  appcast 'https://osdn.jp/projects/nndd/releases/rss',
-          checkpoint: '688d962d47c3a84d474ddbb25c37fbe44240bba4f792bcfdb8cb6f1134fef678'
+  appcast 'https://ja.osdn.net/projects/nndd/releases/rss',
+          checkpoint: '63fae7ea9a0a42bd34091f032cdce3fe8a26c3c9a9866f1e29a45555ada52fc3'
   name 'NNDD'
   homepage 'https://osdn.jp/projects/nndd/'
 
