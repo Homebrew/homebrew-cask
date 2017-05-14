1bfc4dd47e247cd65805e98c43cc609c455933d6
Timothy Sutton <tim@synthist.net>
2017-05-14 15:38:55 -0400

    style fixes for new process-monitor cask

--- Casks/process-monitor.rb
+++ Casks/process-monitor.rb
@@ -2,15 +2,15 @@ cask 'process-monitor' do
   version '1.1'
   sha256 :no_check
 
-  # s3.amazonaws.com/sqwarq.com/AppCasts was verified as official when first introduced to the cask
+  # s3.amazonaws.com/sqwarq.com/PublicZips/ProcessMonitor.app.zip was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/sqwarq.com/PublicZips/ProcessMonitor.app.zip'
   appcast 'https://s3.amazonaws.com/sqwarq.com/AppCasts/procmon-updates.xml',
           checkpoint: '3961558b7f0ee17bdb52704fa2237bcd161f4812d01690f7579e7612fc84db95'
   name 'Process Monitor'
   homepage 'https://sqwarq.com/process-monitor/'
 
-  depends_on macos: '>= :mountain_lion'
   auto_updates true
+  depends_on macos: '>= :mountain_lion'
 
   app 'Process Monitor.app'
 end
