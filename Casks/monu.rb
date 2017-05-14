45cc96e4d57247c3b920eae1dda416004f22956c
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-05-14 22:57:04 +0100

    Update monu to 1.3.0 (#34087)

--- Casks/monu.rb
+++ Casks/monu.rb
@@ -4,7 +4,7 @@ cask 'monu' do
 
   url "https://github.com/maxogden/monu/releases/download/#{version}/Monu.zip"
   appcast 'https://github.com/maxogden/monu/releases.atom',
-          checkpoint: '862cec6bc7b0b779f7c8266de779bd270ae08f39b39b79912d2b616e269115f1'
+          checkpoint: '98c0f47dcb64d3643fa2b3e50888d9cd91c19b5c014f6343d7f1243240388cf8'
   name 'Monu'
   homepage 'https://github.com/maxogden/monu'
 
