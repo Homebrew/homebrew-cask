48533ca22d9c75c44d2bebc59778886e2f0adf6f
nuomi1 <nuomi1@users.noreply.github.com>
2017-05-15 20:41:06 +0800

    Update pphelper.rb for depens_on and zap (#34170)

--- Casks/pphelper.rb
+++ Casks/pphelper.rb
@@ -9,5 +9,14 @@ cask 'pphelper' do
   name 'pp助手'
   homepage 'https://pro.25pp.com/pp_mac_ios'
 
+  depends_on macos: '>= :lion'
+
   app 'pp助手.app'
+
+  zap delete: [
+                '~/Library/Application Support/PPHelper',
+                '~/Library/Caches/com.teiron.pphelper.mac',
+                '~/Library/Preferences/com.teiron.pphelper.mac.plist',
+                '~/Library/Saved Application State/com.teiron.pphelper.mac.savedState',
+              ]
 end
