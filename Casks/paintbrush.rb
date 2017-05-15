9ef4b89d566696f9fe24f7cc1087f510446312cd
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-15 08:29:46 +0800

    Update paintbrush.rb

--- Casks/paintbrush.rb
+++ Casks/paintbrush.rb
@@ -3,7 +3,7 @@ cask 'paintbrush' do
   sha256 '3bf18908a191b65efec2a15af0c5f5e95f76e1ec00d71063e235a146b9f6c417'
 
   url "https://downloads.sourceforge.net/paintbrush/Paintbrush%202.x/Paintbrush%20#{version}/Paintbrush-#{version}.zip"
-  appcast 'https://paintbrush.sourceforge.io/updates2x.xml',
+  appcast "https://paintbrush.sourceforge.io/updates#{version.major}x.xml",
           checkpoint: '776d288198003cdbdc854c58ecf5a89afc6676c56aaa2c66fdfc4ea9646e8fd6'
   name 'Paintbrush'
   homepage 'http://paintbrush.sourceforge.net/'
