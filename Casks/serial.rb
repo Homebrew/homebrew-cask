055222af0c43b7d47c9a3bbe1f3ad59b3b2138e2
commitay <commitay@users.noreply.github.com>
2017-05-16 06:50:36 +1000

    Update serial to 1.3.2

--- Casks/serial.rb
+++ Casks/serial.rb
@@ -1,6 +1,6 @@
 cask 'serial' do
-  version '1.3.1'
-  sha256 '2062f72115ba84fe107f6da71c99405355b0cd5557a940668048418938aa9ec5'
+  version '1.3.2'
+  sha256 '7af8a6662699c76e6646e662701e5b5ec88dd3ec12c0fc08087ad854ee3f78a2'
 
   url "https://download.decisivetactics.com/products/serial/dl/Serial_#{version}.zip"
   name 'Serial'
