773f61cc6127aec757f26bca9db5c6082a63a666
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-05-16 04:04:44 +0100

    Update postbird to 0.6.2 (#34203)

--- Casks/postbird.rb
+++ Casks/postbird.rb
@@ -1,10 +1,10 @@
 cask 'postbird' do
-  version '0.6.1'
-  sha256 '1cd2d31b0f316a1abfb112d795b6072b0baa98a54b1ae18733e2c00673db8244'
+  version '0.6.2'
+  sha256 'a6ffb7b8aeef43a96c920c829f810001686946e144e50ede856f7091b90361c0'
 
   url "https://github.com/Paxa/postbird/releases/download/#{version}/Postbird-#{version}.dmg"
   appcast 'https://github.com/Paxa/postbird/releases.atom',
-          checkpoint: '4241621f2803ef1ba254b8cb5a5c436eb6359bc173bc2b0ec64389eb87f25c8d'
+          checkpoint: '0c830468537007820e3caa4c9e22f12ae8db7f4528dbb91b1460e5b3e1840621'
   name 'Postbird'
   homepage 'https://github.com/Paxa/postbird'
 
