530f1a132620743504e04fada74b37fabbc84fa8
Gobinath Mallaiyan <i@gobinath.com>
2017-05-16 11:27:55 -0400

    Update openshot-video-editor to 2.3.1

--- Casks/openshot-video-editor.rb
+++ Casks/openshot-video-editor.rb
@@ -5,7 +5,7 @@ cask 'openshot-video-editor' do
   # github.com/OpenShot/openshot-qt was verified as official when first introduced to the cask
   url "https://github.com/OpenShot/openshot-qt/releases/download/v#{version}/OpenShot-v#{version}-x86_64.dmg"
   appcast 'https://github.com/OpenShot/openshot-qt/releases.atom',
-          checkpoint: '150d0dc218d3d53e4f30e45accc245d8b12c9951e55dd68f48f0bc445be0eee8'
+          checkpoint: 'b9d68ef0727965693320bced947420b4cff9ecc8386e43fdc46fe98ce4405662'
   name 'OpenShot Video Editor'
   homepage 'https://openshot.org/'
 
