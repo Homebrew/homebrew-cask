76453d045751c75a0bd7dae5907cda139a2240a9
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-16 16:07:37 -0400

    Update navicat-premium-essentials.rb

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,5 +1,5 @@
 cask 'navicat-premium-essentials' do
-  version '11.2.17'
+  version '11.2.18'
   sha256 '8e5da83150d3e93a85b634447b55e944a1058ef15250b569aa1ec7a6d987a865'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
