3bbb52c0df9ba151c8f50af9236efb48f84b4be9
Gobinath Mallaiyan <i@gobinath.com>
2017-05-16 16:20:11 -0400

    Update navicat-for-mysql to 11.2.18

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-mysql' do
-  version '11.2.17'
-  sha256 '8e328346efddb8b0b2f8a3a8df5ab595c5ded8c7b774aa764812bdfaa1888229'
+  version '11.2.18'
+  sha256 'bd2e1f93e2e2512f8298498b1f40b632a206f8b8d0fa4fd9566787ba634cea4d'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
   name 'Navicat for MySQL'
