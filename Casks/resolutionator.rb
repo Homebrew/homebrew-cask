63b99ce624a620b2cc10f59be5c5a1960e70c7c3
giannitm <giannitm@users.noreply.github.com>
2017-05-16 21:09:50 +0100

    Update resolutionator to 1.1.1

--- Casks/resolutionator.rb
+++ Casks/resolutionator.rb
@@ -2,9 +2,9 @@ cask 'resolutionator' do
   version '1.1.1'
   sha256 'b4e0ba696aeee124c8d701371af71c4d1702fe0333fb955e3e5492d10f6ef679'
 
-  url 'https://manytricks.com/download/resolutionator'
-  appcast 'https://manytricks.com/resolutionator/appcast.xml',
-          checkpoint: '6a6a79eceb2b0920958fb46b211bcac8ead6a3cb7c4e5e50e2b4b1655fefc34b'
+  url "https://manytricks.com/download/_do_not_hotlink_/resolutionator#{version.no_dots}.dmg"
+  appcast 'https://manytricks.com/resolutionator/appcast',
+          checkpoint: 'e3bb2ac06a8762d3b746b16e68e6d762a5b7d175bb64be260831246b742e8962'
   name 'Resolutionator'
   homepage 'https://manytricks.com/resolutionator/'
 
