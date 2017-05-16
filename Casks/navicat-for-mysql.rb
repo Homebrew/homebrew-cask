9364d398e1ce7c586ec25156d855d6cf513c2c04
Gobinath Mallaiyan <i@gobinath.com>
2017-05-16 18:01:13 -0400

    Update navicat-for-mysql to 11.2.18
    - whitespace fix

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -4,7 +4,7 @@ cask 'navicat-for-mysql' do
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-mysql-release-note',
-          checkpoint: '3b1d2837be33fd080b74cf7a37d6a2213eb5915979af2abc0660c6f858834c45'  
+          checkpoint: '3b1d2837be33fd080b74cf7a37d6a2213eb5915979af2abc0660c6f858834c45'
   name 'Navicat for MySQL'
   homepage 'https://www.navicat.com/products/navicat-for-mysql'
 
