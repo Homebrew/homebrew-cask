b1eeb16ebd50adb29085112a00889c48a110cabf
Gobinath Mallaiyan <i@gobinath.com>
2017-05-16 18:02:21 -0400

    Update navicat-for-sql-server to 11.2.18
    - whitespace fix

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -4,7 +4,7 @@ cask 'navicat-for-sql-server' do
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-sqlserver-release-note',
-          checkpoint: '11f0134465eb9c7f0526a03e4fcab01760bfa43352b0678e1d78364361a62625'  
+          checkpoint: '11f0134465eb9c7f0526a03e4fcab01760bfa43352b0678e1d78364361a62625'
   name 'Navicat for SQL Server'
   homepage 'https://www.navicat.com/products/navicat-for-sqlserver'
 
