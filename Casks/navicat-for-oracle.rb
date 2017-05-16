43e361ca6c51c8aa7caa9d2e38847182a6165a91
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-16 19:04:02 -0400

    Update navicat-for-oracle to 11.2.18 (#34274)
    * Update navicat-for-oracle to 11.2.18
    
    * Update navicat-for-oracle.rb
    
    * Update navicat-for-oracle to 11.2.18
    
    - fix whitespace issu

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,8 +1,10 @@
 cask 'navicat-for-oracle' do
-  version '11.2.17'
-  sha256 '8a22eb43edba11055f38144be9ed96c858354aeb5c77ac682aaf979b490b625f'
+  version '11.2.18'
+  sha256 'cd957470c76ebdd9ac2ba625746298650d9356640acbda5141f8515b8f1dc321'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
+  appcast 'https://www.navicat.com/products/navicat-for-oracle-release-note',
+          checkpoint: '976ee6551c762789ddd6b656ddad35a11809e83a66cd3e7327fa7e1d2c384105'
   name 'Navicat for Oracle'
   homepage 'https://www.navicat.com/products/navicat-for-oracle'
 
