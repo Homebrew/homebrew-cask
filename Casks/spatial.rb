7a8e41e69ab42abba27671c14afc9a8850d5b2aa
Peter Mounce <petermounce@improbable.io>
2017-05-18 12:16:43 +0100

    spatial:latest - After installation, set the command structure to v2 which is current.
    This eliminates a manual step on our setup guide for new users: https://spatialos.improbable.io/docs/reference/10.3/setup-spatialos/mac#2-set-up-the-spatialos-cli

--- Casks/spatial.rb
+++ Casks/spatial.rb
@@ -10,4 +10,9 @@ cask 'spatial' do
   container type: :naked
 
   binary 'mac', target: 'spatial'
+
+  postflight do
+    puts 'Setting cli-structure to v2 (current).'
+    `spatial config set cli-structure v2`
+  end
 end
