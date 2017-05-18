015e3e4cf603be7e3350fa88544957d962afec9b
Gobinath Mallaiyan <i@gobinath.com>
2017-05-18 14:01:48 -0400

    Update remote-desktop-manager to 4.2.1.0

--- Casks/remote-desktop-manager.rb
+++ Casks/remote-desktop-manager.rb
@@ -5,7 +5,7 @@ cask 'remote-desktop-manager' do
   # devolutions.net was verified as official when first introduced to the cask
   url "http://cdn.devolutions.net/download/Mac/Devolutions.RemoteDesktopManager.Mac.#{version}.dmg"
   appcast 'http://cdn.devolutions.net/download/Mac/RemoteDesktopManager.xml',
-          checkpoint: '37ddc1327f069c984feb9ec72ee2c22aba11da3db52fad59174bea276ecd6597'
+          checkpoint: '0f20dc31c6a641a3c4dee2a135bba73e432a1a8b00329cf2d63c54bcf720dde0'
   name 'Remote Desktop Manager'
   homepage 'https://mac.remotedesktopmanager.com/'
 
