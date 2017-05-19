63ef10953df9d13c80ca2198520435135182ecce
Colin Dunn <cgd@vikingdev.net>
2017-05-18 21:32:00 -0700

    Update stamp to 4.6.3

--- Casks/stamp.rb
+++ Casks/stamp.rb
@@ -1,6 +1,6 @@
 cask 'stamp' do
-  version '4.5.1'
-  sha256 '2b87577793b0b8a721963e1c85df930ba7773a1b17030e88979243bffc4558e7'
+  version '4.6.3'
+  sha256 '02c26def4301964cc1a884d520842e1e67445328e2923c1e2ca3995a1e7b92ee'
 
   url "https://freeyourmusic.com/media/STAMP#{version.no_dots}.dmg"
   name 'Stamp'
