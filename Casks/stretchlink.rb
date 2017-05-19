c6c6c2ca4b9e1a3e23b96259057c21f53e4d2c9e
giannitm <giannitm@users.noreply.github.com>
2017-05-19 22:35:50 +0100

    Update stretchlink to 1.0.3-105

--- Casks/stretchlink.rb
+++ Casks/stretchlink.rb
@@ -2,9 +2,10 @@ cask 'stretchlink' do
   version '1.0.3-105'
   sha256 'a12b8199207009a4906d5bc54992bdccc91ff9c877c4d2b8730a2153d04c8180'
 
-  url 'http://stretchlinkapp.com/download/StretchLink.zip'
+  # abyss.designheresy.com/stretchlink was verified as official when first introduced to the cask
+  url "http://abyss.designheresy.com/stretchlink/stretchlink#{version.no_dots}.zip"
   appcast 'http://abyss.designheresy.com/stretchlink/stretchlink.xml',
-          checkpoint: '50db6bf7a27a9a4fe0b3c7f802913be1dfd54cbb85c9f42c7513184c9014f0c9'
+          checkpoint: '24ee72f9dc64631fda556cda283c474f9acc0e976d6650e5f95e5270daff1496'
   name 'StretchLink'
   homepage 'http://stretchlinkapp.com/'
 
