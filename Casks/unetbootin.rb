883d9359a43d19463ff87adaaf0992b4ff44ec5a
David Beitey <david@davidjb.com>
2017-05-19 17:03:12 +1000

    Download unetbootin 627 over HTTPS

--- Casks/unetbootin.rb
+++ Casks/unetbootin.rb
@@ -3,7 +3,7 @@ cask 'unetbootin' do
   sha256 '6f0abd2ab696501dbddde135d4709259d6d4ca2de33a54bfb10f51b4d5ffd22a'
 
   # launchpad.net/unetbootin was verified as official when first introduced to the cask
-  url "http://launchpad.net/unetbootin/trunk/#{version}/+download/unetbootin-mac-#{version}.dmg"
+  url "https://launchpad.net/unetbootin/trunk/#{version}/+download/unetbootin-mac-#{version}.dmg"
   appcast 'https://github.com/unetbootin/unetbootin/releases.atom',
           checkpoint: 'fd308855106d78c0ad2e793b760b6ee07abeef5456d0cbcc2f87236a314bc06f'
   name 'UNetbootin'
