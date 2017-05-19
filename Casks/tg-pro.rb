7c90e059e5cd1191b8e201e8fabd41b97216075c
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-05-20 00:07:00 +0100

    Update tg-pro to 2.20 (#34443)

--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -1,10 +1,10 @@
 cask 'tg-pro' do
-  version '2.18'
-  sha256 '6d41d38cdaa479eeb1958f1e9cca762a66fa3b3c127c772d746855721a2116a9'
+  version '2.20'
+  sha256 'c3717b50786c1cf2f12e903cfce3b3b4733f735dcb010c1272e3b82e1302d188'
 
   url "https://www.tunabellysoftware.com/resources/TGPro_#{version.dots_to_underscores}.zip"
   appcast 'https://www.tunabellysoftware.com/resources/sparkle/tgpro/profileInfo.php',
-          checkpoint: '0699744b368b56e0c0dbe43f5fa68a228418b4b7c752f4e52c41008ba7eb91ae'
+          checkpoint: '9408248bf5b6df77c918f5f41ab143fc67a6f3d3e6b3d4b61cf830ed1d95a823'
   name 'TG Pro'
   homepage 'https://www.tunabellysoftware.com/tgpro/'
 
