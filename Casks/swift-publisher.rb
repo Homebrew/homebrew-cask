de12319a8cf70f53496dcb7c07285f0fe87239e8
giannitm <giannitm@users.noreply.github.com>
2017-05-19 22:56:59 +0100

    Update swift-publisher to 4.0.5

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -1,11 +1,11 @@
 cask 'swift-publisher' do
-  version '4.0.4'
-  sha256 '743204d13b5945eda94a1b2d7b4589b522f680a1d4f4b5f83c0163c2409ec801'
+  version '4.0.5'
+  sha256 '387bcfac5c3c1c45bb3ac3e0a88bae99b1388d621aa14d58896fe19c08dd889f'
 
   # s3.amazonaws.com/belightsoft was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/belightsoft/SwiftPublisher.dmg'
   appcast "https://www.belightsoft.com/download/updates/appcast_SwiftPublisher#{version.major}.xml",
-          checkpoint: 'd6a4ec3a83b57ec88a5618ce44c518faa4618caa03e00234afd1d8da972af4ec'
+          checkpoint: '0af71d81b2ef429cb3aec5f5ecf204d371a145c79a80afc848376b2016fcbe38'
   name 'Swift Publisher'
   homepage 'https://www.belightsoft.com/products/swiftpublisher/'
 
