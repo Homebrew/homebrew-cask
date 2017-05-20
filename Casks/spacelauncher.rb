a4f9e643ebd3046e35f54bc24962d993f69e64d6
Colin Dunn <cgd@vikingdev.net>
2017-05-20 01:29:57 -0700

    Update spacelauncher to 1.3

--- Casks/spacelauncher.rb
+++ Casks/spacelauncher.rb
@@ -1,10 +1,10 @@
 cask 'spacelauncher' do
-  version '1.0.2'
-  sha256 'cda767f28b39f3590d94a0a1266f54694d932f37de34bfc9cdd2a002e9eadff4'
+  version '1.3'
+  sha256 '415b54074155845563bb5d72a636dca0acc2f631d6e12842f8f6dcba1995a73e'
 
   url 'https://spacelauncherapp.com/download/SpaceLauncher.zip'
   appcast 'https://spacelauncherapp.com/download/appcast.xml',
-          checkpoint: '18a42d05d1659eb4638491c2d9e4f99cbf7bac61aeb18bcab136164ed5f408f6'
+          checkpoint: '0b2ae8e0e9978c3e1565c10b1f0d0ad79e727d107844b7a7670ad79194e216fc'
   name 'SpaceLauncher'
   homepage 'https://spacelauncherapp.com/'
 
