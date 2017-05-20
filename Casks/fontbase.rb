2e60e153b58092697911dd022a33133fab32e918
Colin Dunn <cgd@vikingdev.net>
2017-05-19 20:13:37 -0700

    Update fontbase to 2.0.2 (#34470)

--- Casks/fontbase.rb
+++ Casks/fontbase.rb
@@ -1,6 +1,6 @@
 cask 'fontbase' do
-  version '2.0.1'
-  sha256 '012f88b19dba05bb700f0bd9e29409c1d2da30aa6355d84324eb439b9832f742'
+  version '2.0.2'
+  sha256 'bbbf9d6d9919c4816e312fa975696b3fba07819b5813a6c39ac914b5a8fdf59d'
 
   url "http://releases.fontba.se/mac/#{version}/FontBase-#{version}.dmg"
   name 'FontBase'
