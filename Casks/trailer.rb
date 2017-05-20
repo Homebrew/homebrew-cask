3e1438ba4ad932877f7f449fa05696adc4c60517
giannitm <giannitm@users.noreply.github.com>
2017-05-20 10:48:29 +0100

    Update trailer to 1.5.7

--- Casks/trailer.rb
+++ Casks/trailer.rb
@@ -1,10 +1,10 @@
 cask 'trailer' do
-  version '1.5.5'
-  sha256 '5af1ed7d576bb79c0b964b0a2b2597286f7a170118512f19cbf0f9ef9227394d'
+  version '1.5.7'
+  sha256 'cbe5d00f52f156d670bb75824d34a36aec2e43a833f57c9e8bfe1303aef4a047'
 
   url "https://ptsochantaris.github.io/trailer/trailer#{version.no_dots}.zip"
   appcast 'https://ptsochantaris.github.io/trailer/appcast.xml',
-          checkpoint: '7cf2e300d7276c1b825e4d69c3d1cd925e82575cec89a7035e2ae287488f75cb'
+          checkpoint: 'f0426f92decd3afc5828b6bb48dc479f0746938b3a14d87e9a0b852bbf906b54'
   name 'Trailer'
   homepage 'https://ptsochantaris.github.io/trailer/'
 
