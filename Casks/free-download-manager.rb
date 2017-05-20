61c69edd50ff23035fd5002ca2a8db4a3143e2ba
commitay <commitay@users.noreply.github.com>
2017-05-20 19:46:06 +1000

    Update free-download-manager to 5.1.28 (#34476)

--- Casks/free-download-manager.rb
+++ Casks/free-download-manager.rb
@@ -1,6 +1,6 @@
 cask 'free-download-manager' do
-  version '5.1.27'
-  sha256 '05c969ff67df393afec1b456005a5d8b52e7d14942bc0b1640ccc59a2ea4a759'
+  version '5.1.28'
+  sha256 'c92674e88a741cec39a77d1ae03ce6cee017e065b13dc920b9d73549a2462305'
 
   url "http://files2.freedownloadmanager.org/#{version.major}/#{version.major_minor}-latest/fdm.dmg"
   name 'Free Download Manager'
