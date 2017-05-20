363d38dabd2e15fadf6acbd61cd0f7acd2d4f3ba
Colin Dunn <cgd@vikingdev.net>
2017-05-20 10:54:29 -0700

    Update omnioutliner to 5.0.4 (#34565)

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -1,10 +1,10 @@
 cask 'omnioutliner' do
-  version '5.0.3'
-  sha256 'e0d9e543b457efc059eba9875729ddd9196cd1c9edfc1092118ce38a0e2c4c0d'
+  version '5.0.4'
+  sha256 '1cc528b984b98eb2a17d2c10d943d0941b6834008d304002c0ecf9c3c1e36de3'
 
   url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniOutliner-#{version}.dmg"
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniOutliner#{version.major}",
-          checkpoint: '1d608a6b2dbbabbcfdbe92ddfbfc4ac531a5a4b9d8cdef0862dbda406784a8b0'
+          checkpoint: 'a2b591b031821d53be2d40ca50b1dd23fcb3076cd3a2efd1dad8928969593498'
   name 'OmniOutliner'
   homepage 'https://www.omnigroup.com/omnioutliner/'
 
