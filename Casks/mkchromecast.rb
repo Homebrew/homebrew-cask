ce0f63c4df6ceaf18c6e21e303da883c0256a597
Colin Dunn <cgd@vikingdev.net>
2017-05-20 11:21:09 -0700

    Update mkchromecast to 0.3.7

--- Casks/mkchromecast.rb
+++ Casks/mkchromecast.rb
@@ -1,11 +1,11 @@
 cask 'mkchromecast' do
-  version '0.3.6'
-  sha256 'cf167c5770a5a5b03b81865ceab0cbc50c9d5f93ff826bc1b4c204e5f692b112'
+  version '0.3.7'
+  sha256 'bba745669d7ff4555aa4895706ac7808eb24c007581cb055bc72591a043198ad'
 
   # github.com/muammar/mkchromecast was verified as official when first introduced to the cask
   url "https://github.com/muammar/mkchromecast/releases/download/#{version}/mkchromecast_v#{version}.dmg"
   appcast 'https://github.com/muammar/mkchromecast/releases.atom',
-          checkpoint: '35e0610f7ee3541075f5468d5043e1165ef558216c7e6123bbfc5006fb3e076c'
+          checkpoint: '5046a770995fba5330b5f89d100750c38f35ab8fee1a8d0012c2ffe83141a7bb'
   name 'mkchromecast'
   homepage 'http://mkchromecast.com/'
 
