d1082facc0850db13e9754cdf4e6bdba23273899
Colin Dunn <cgd@vikingdev.net>
2017-05-21 03:19:46 -0700

    Update mblock to 3.4.8

--- Casks/mblock.rb
+++ Casks/mblock.rb
@@ -1,11 +1,11 @@
 cask 'mblock' do
-  version '3.4.6'
-  sha256 '9078302780a4203e51a7033307a524ae8411a02cea3f0a118c8f3bdc7ca9e4df'
+  version '3.4.8'
+  sha256 'cb836071d38039fb09340e8456022cbf20a26d98f4536f9fd96ba572f572e50e'
 
   # mblock.makeblock.com was verified as official when first introduced to the cask
   url "http://mblock.makeblock.com/mBlock_mac_V#{version}.zip"
   appcast 'http://www.mblock.cc/release-logs',
-          checkpoint: 'b93daf44a38ae3b47cdf76d3cee2b004a4fb12fd6dcb1cd9faa86f0e2cd15800'
+          checkpoint: '0ae993a841cd4cc7341e765ddfe8f4f84b1559915c9b564eb888afd0cb48bf8a'
   name 'mBlock'
   homepage 'http://www.mblock.cc/'
 
