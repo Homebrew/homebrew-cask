211f918e2ecc6c9f00f00f3480ed2e84cbaba935
Colin Dunn <cgd@vikingdev.net>
2017-05-21 06:11:27 -0700

    Update resolume-avenue to 4.6.4 (#34659)

--- Casks/resolume-avenue.rb
+++ Casks/resolume-avenue.rb
@@ -1,6 +1,6 @@
 cask 'resolume-avenue' do
-  version '4.6.2'
-  sha256 '0dfe3f28308c05dd180619071b87256333f00b2f3a19e1a2b9ea1fa649c74451'
+  version '4.6.4'
+  sha256 'e6adfd60d07cfa025bd61c1047b4451c07407e1bf67746fa741952c3efbde7ae'
 
   # d19j6z4lvv1vde.cloudfront.net was verified as official when first introduced to the cask
   url "https://d19j6z4lvv1vde.cloudfront.net/Resolume_Avenue_#{version.dots_to_underscores}_Installer.dmg"
