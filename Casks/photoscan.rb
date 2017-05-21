b1a61a4c67cf8252e4fce82d57c7beaab30ef5c3
Colin Dunn <cgd@vikingdev.net>
2017-05-21 16:16:51 +1000

    Update photoscan to 1.3.2 (#34616)

--- Casks/photoscan.rb
+++ Casks/photoscan.rb
@@ -1,10 +1,10 @@
 cask 'photoscan' do
-  version '1.3.1'
-  sha256 '13f7cffc29c611145b3901ef3a2c2e73998bbc2dc33fa5fd09c04a952c7021c2'
+  version '1.3.2'
+  sha256 'da70c79272aed723ee6c5b0ae49fa9c0394ad823b33827068aad300f08414cf7'
 
   url "http://download.agisoft.com/photoscan_#{version.dots_to_underscores}.dmg"
   appcast 'http://www.agisoft.com/downloads/installer/',
-          checkpoint: '16e519a5ed47b4fc58e39177d41f3815f69f2b835f2ef1eafb7116bc8ebc4593'
+          checkpoint: 'a6e525e8551969d5ddd61a36f2993d8f75f764a904bf0925b21c7f4be47b493e'
   name 'Agisoft Photoscan'
   homepage 'http://www.agisoft.com/'
 
