eef84f9b60b49efa184097cb0b03f339fe405e85
Colin Dunn <cgd@vikingdev.net>
2017-05-21 06:04:31 -0700

    Update flinto to 2.2.3

--- Casks/flinto.rb
+++ Casks/flinto.rb
@@ -1,6 +1,6 @@
 cask 'flinto' do
-  version '2.1.2'
-  sha256 'a9aa8a8809e5cc76af9c77efe11cb58b377a39bce307105904761e9d5c21a112'
+  version '2.2.3'
+  sha256 '6682b9929cd8adaf28423b4aba9c29c90af9e0e2395888008db2fd54a996c509'
 
   url "https://www.flinto.com/assets/Flinto-#{version}.dmg"
   name 'Flinto'
