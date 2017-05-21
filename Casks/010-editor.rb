f55c7e999f55c77a859e942e1c9e3e6ad51194df
giannitm <giannitm@users.noreply.github.com>
2017-05-21 17:17:15 +0100

    Update 010-editor to 8.0

--- Casks/010-editor.rb
+++ Casks/010-editor.rb
@@ -3,6 +3,8 @@ cask '010-editor' do
   sha256 '2553272d930cd1c8067426d570ebf34bb8dab28632ee3184f2313e303ce6e17d'
 
   url "https://download.sweetscape.com/010EditorMac64Installer#{version.no_dots}.dmg"
+  appcast 'https://www.sweetscape.com/010editor/',
+          checkpoint: '1063eeb41e5cf5cc8891d5f7744d91233f482721eee6ff1016c6fbb238d1b278'
   name '010 Editor'
   homepage 'https://www.sweetscape.com/'
 
