cca3ed078086c2d4c18b668d8cc5b89197b68ca4
Colin Dunn <cgd@vikingdev.net>
2017-05-21 16:01:00 +1000

    Update bankid to 7.4.0 (#34600)

--- Casks/bankid.rb
+++ Casks/bankid.rb
@@ -1,6 +1,6 @@
 cask 'bankid' do
-  version '7.3.0'
-  sha256 '6f0045c06c88a7c56f08ccf7aa6ccb036fd6552dfbf8860ec5f8cbc9fb48f2ad'
+  version '7.4.0'
+  sha256 'c014b5580b848cd649671b2e0e00c3eacba1e3c78e5fb8ac2166ea0a02f5d0d8'
 
   url "https://install.bankid.com/Repository/BankID_installation_#{version.dots_to_underscores}.pkg"
   name 'BankID'
