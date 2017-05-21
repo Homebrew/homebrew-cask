4ca4d2fb83bbc21d194213e7950a9dbf3419c7fe
Vítor Galvão <info@vitorgalvao.com>
2017-05-21 12:02:39 +0100

    Update saoimage-ds9.rb

--- Casks/saoimage-ds9.rb
+++ Casks/saoimage-ds9.rb
@@ -1,10 +1,10 @@
 cask 'saoimage-ds9' do
   version '7.5'
 
-  if MacOS.version.eql? :yosemite
+  if MacOS.version == :yosemite
     sha256 '2b8df99204f6d739da4e4dc784056350ee7670ac896baa16be8f2fb0847a9755'
     url "http://ds9.si.edu/download/macosxyosemite/SAOImage%20DS9%20#{version}.dmg"
-  elsif MacOS.version.eql? :el_capitan
+  elsif MacOS.version == :el_capitan
     sha256 'bfb80ed0311034ac060fc7ef455fae4056870c93f06f90606da4fd49eb551b4b'
     url "http://ds9.si.edu/download/macosxelcapitan/SAOImage%20DS9%20#{version}.dmg"
   else
@@ -15,7 +15,7 @@ cask 'saoimage-ds9' do
   name 'SAOImage DS9'
   homepage 'http://ds9.si.edu/site/Home.html'
 
-  depends_on macos: '> :yosemite'
+  depends_on macos: '=> :yosemite'
 
   app 'SAOImage DS9.app'
 end
