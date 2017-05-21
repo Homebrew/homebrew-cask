3ff6484d56f6cc269d462f1da215285705c5fbf5
Colin Dunn <cgd@vikingdev.net>
2017-05-21 14:03:33 +1000

    Update xrg to 2.5.0 (#34592)

--- Casks/xrg.rb
+++ Casks/xrg.rb
@@ -1,10 +1,10 @@
 cask 'xrg' do
-  version '2.0.0'
-  sha256 '9c963b1ab5ddd4dba49e0e085e63e84b31613eff3e155f5e3dbb55c0abcfbe46'
+  version '2.5.0'
+  sha256 'c9cc0e808a87376c838fa6e4a9c04fef9143a8ddedcfce9f7187258d7eac9ec9'
 
   url "https://download.gauchosoft.com/xrg/XRG-release-#{version}.zip"
   appcast 'http://www.gauchosoft.com/Products/XRG/',
-          checkpoint: '28e09c94daac04fa2bb14cfd3bbf156fc20eaaa2bb1bc75cf274b38b9a9d0b01'
+          checkpoint: '050bc1403bec43c7d0514065447ed9af949752ea0d6b38a6012aea4c3b550fb8'
   name 'XRG'
   homepage 'http://www.gauchosoft.com/Products/XRG/'
 
