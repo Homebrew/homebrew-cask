5805ee9f517782346bc50a0d3f2bfc6927c7d599
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-23 07:35:27 +0800

    Update dbeaver-enterprise.rb

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -3,8 +3,6 @@ cask 'dbeaver-enterprise' do
   sha256 'c58b03056e0a845f2d7fff7ca37dcdf8b4dc2ded74e1932bc2654f8676510bf0'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
-  appcast 'http://dbeaver.jkiss.org/files/',
-          checkpoint: 'ffef67bd5da18feb5a7143c0365679819ee456b260ec7ccec9b1b9199cef5bd9'
   name 'DBeaver Enterprise Edition'
   homepage 'http://dbeaver.jkiss.org/'
 
