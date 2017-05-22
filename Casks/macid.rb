26f45e733c32625d5153962aab92338041eb6ad4
rubedozen <rubedozen@users.noreply.github.com>
2017-05-22 13:28:29 +0800

    Update MacID to 1.3.6

--- Casks/macid.rb
+++ Casks/macid.rb
@@ -1,6 +1,6 @@
 cask 'macid' do
-  version '1.3.5'
-  sha256 'fdb37485748ad774e6e78e5d9134ce355aa5227e2926206a0e2314cc660ae452'
+  version '1.3.6'
+  sha256 'b13c7018e073007ec5ad2e2b8573b0d1dae7d7be77fb380ac0055ba269fdf80b'
 
   url "https://macid.co/app/#{version}/MacID%20for%20macOS.zip"
   name 'MacID'
