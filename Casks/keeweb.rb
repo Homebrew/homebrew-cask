1900a3bedcc9be0756315ad29bf05190665292f7
Gobinath Mallaiyan <i@gobinath.com>
2017-05-23 16:47:36 -0400

    Update keeweb to 1.5.1

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,11 +1,11 @@
 cask 'keeweb' do
-  version '1.5.0'
-  sha256 '4b8476149c4da15e1b973ceb151bab08533f01431235572dfd8d9e2fc907b263'
+  version '1.5.1'
+  sha256 '732ff8a7b8098beed69c459ca5334ae818b6946a89d0508d827b546663465930'
 
   # github.com/keeweb/keeweb was verified as official when first introduced to the cask
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.dmg"
   appcast 'https://github.com/keeweb/keeweb/releases.atom',
-          checkpoint: '8e1d8bfc9ca84496fb5d12e234a599dc2d965cb04743dfa9e554a8590045960f'
+          checkpoint: 'fa88335e6baefeff0651dbf4c49911a218c2bb280f9c22edc33a9c0176587cd5'
   name 'KeeWeb'
   homepage 'https://keeweb.info/'
 
