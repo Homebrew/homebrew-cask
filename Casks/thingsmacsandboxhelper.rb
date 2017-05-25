e14a5a91ff81d78f7622b1363b3d2ab2cddf21fb
S. Zachariah Sprackett <zac@sprackett.com>
2017-05-24 20:51:51 -0400

    Add thingsmacsandboxhelper for Things3 (#34860)

---
+++ Casks/thingsmacsandboxhelper.rb
@@ -0,0 +1,15 @@
+cask 'thingsmacsandboxhelper' do
+  version '3.4'
+  sha256 'cfc9123c3489a56c3807fe311c41618039690085e1023fc888d119e6415bdb80'
+
+  # culturedcode.cachefly.net was verified as official when first introduced to the cask
+  url "https://culturedcode.cachefly.net/things/thingssandboxhelper/#{version}/ThingsHelper.zip"
+  name 'ThingsMacSandboxHelper'
+  homepage 'https://culturedcode.com/things/mac/help/things-sandboxing-helper-download/'
+
+  app 'ThingsMacSandboxHelper.app'
+
+  postflight do
+    suppress_move_to_applications key: 'suppressMoveToApplications'
+  end
+end
