690cfc605f70394503e62f3dbeddece1bb5de48c
Colin Dunn <cgd@vikingdev.net>
2017-05-26 05:54:35 +1000

    Update houdahgeo to 5.1.9 (#34890)

--- Casks/houdahgeo.rb
+++ Casks/houdahgeo.rb
@@ -1,6 +1,6 @@
 cask 'houdahgeo' do
-  version '5.1.8'
-  sha256 '151b4c5ec060a4c9493a1b235b60595a2e2a1a01b22da1818c91f83a99efd27c'
+  version '5.1.9'
+  sha256 '281fecfc41209f36b34d37ba730a3d03c1aac07c27b6eb3b8e085bef36c82358'
 
   url "https://www.houdah.com/houdahGeo/download_assets/HoudahGeo#{version}.zip"
   name 'HoudahGeo'
