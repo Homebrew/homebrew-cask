b71930f2afe6de410ba8cc450914321686f2a0db
Lukas Eipert <git@leipert.io>
2017-05-26 09:25:09 +0200

    adjust rider version. now it can be used in download url and during zap

--- Casks/rider-eap.rb
+++ Casks/rider-eap.rb
@@ -1,8 +1,9 @@
 cask 'rider-eap' do
-  version 'EAP build 22'
+  # Rider is EAP only for now
+  version '1.0,171.4456.199'
   sha256 'dc995c3d9483c1b033a2de38fa81982e125ebafe478559f23f058989d90092dd'
 
-  url 'https://download.jetbrains.com/resharper/riderRS-171.4456.199.dmg'
+  url "https://download.jetbrains.com/resharper/riderRS-#{version.after_comma}.dmg"
   name 'Jetbrains Rider'
   homepage 'https://www.jetbrains.com/rider/'
 
@@ -13,10 +14,10 @@ cask 'rider-eap' do
   uninstall delete: ENV['PATH'].split(File::PATH_SEPARATOR).map { |p| "#{p}/rider" }.select { |f| File.exist?(f) }
 
   zap delete: [
-                '~/Library/Caches/Rider10',
-                '~/Library/Logs/Rider10',
-                '~/Library/Application Support/Rider10',
-                '~/Library/Preferences/Rider10',
+                "~/Library/Caches/Rider#{version.major_minor.no_dots}",
+                "~/Library/Logs/Rider#{version.major_minor.no_dots}",
+                "~/Library/Application Support/Rider#{version.major_minor.no_dots}",
+                "~/Library/Preferences/Rider#{version.major_minor.no_dots}",
                 '~/Library/Preferences/com.jetbrains.rider-EAP.plist',
                 '~/Library/Saved Application State/com.jetbrains.rider-EAP.savedState',
               ]
