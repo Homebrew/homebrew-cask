3734034d49a96de0a634aa4a8a8cb9b5714e1ae1
commitay <commitay1@gmail.com>
2017-05-27 11:48:25 +1000

    Update lingon-x to 5.1 (#34935)

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,10 +1,10 @@
 cask 'lingon-x' do
-  version '5.0.4'
-  sha256 'f73eb4921eb4d777bb15ec43e731b63e7be6f61ca4a6d1e03b4ac7547b3ed22c'
+  version '5.1'
+  sha256 '043b375547cfb1dfe32f060f7c97550bca7564f62630b01ca1b64f0ced1c03c8'
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
   appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}-appcast.xml",
-          checkpoint: '0b5269a2e8bd3ec3fba8ddee593e1a408af9b08bd2594822f0734d90fb855be3'
+          checkpoint: '845dff97c061da8b79ceeb289b59c6160759cd76d5dc9c0a55742961c09f8393'
   name 'Lingon X'
   homepage 'https://www.peterborgapps.com/lingon/'
 
