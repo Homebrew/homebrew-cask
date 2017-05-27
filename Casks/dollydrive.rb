fa1a4e0338589f70a0d8613cbac8c7445390da94
Vítor Galvão <info@vitorgalvao.com>
2017-05-27 21:26:03 +0100

    Update dollydrive.rb

--- Casks/dollydrive.rb
+++ Casks/dollydrive.rb
@@ -3,7 +3,7 @@ cask 'dollydrive' do
   sha256 '654c2b0d5a6eab71e6bf60769ace36f5c24b287d00ce8f5b6fcb47ac904825ee'
 
   url "http://dollydrive.com/download-center/dollydrive/DollyDrive_#{version.major_minor}_#{version.no_dots}_CERTIFIED.zip"
-  appcast 'http://www.dollydrive.com/dolly3.xml',
+  appcast "http://www.dollydrive.com/dolly#{version.major}.xml",
           checkpoint:
                       '3ff4fe0e5a38053b5179774599043aa76b718d6af4286d28ace8a8bce93524d1'
   name 'DollyDrive'
