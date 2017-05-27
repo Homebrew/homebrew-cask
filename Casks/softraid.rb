4e601f1c115b14b632bdcf53ea51a12556fb2c17
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-27 13:53:12 +0800

    Update softraid.rb

--- Casks/softraid.rb
+++ Casks/softraid.rb
@@ -3,8 +3,8 @@ cask 'softraid' do
   sha256 'a255295d5e98fa47556f6aa54ef22ef7756fc5cd5256dc34cf312c41a520396f'
 
   url "https://softraid.com/updates/SoftRAID%20#{version}.dmg"
-  appcast 'https://www.softraid.com/updates/Latest_SoftRAID_Release.html',
-          checkpoint: '72a2fdf31da4cc525e869bc11eeb6e927d8ec294060ea8078f65f8f58a500dfe'
+  appcast 'https://www.softraid.com/pages/support/download_latest_version.html',
+          checkpoint: 'ac9af2488e40b380b2d51ba04a56a2d7cee70de9ac9ca040d385e2356bb9dea1'
   name 'SoftRAID'
   homepage 'https://www.softraid.com/'
 
