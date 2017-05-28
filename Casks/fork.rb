f0bac661cfd62c9cd0628ceb89b04e421d63173a
commitay <commitay1@gmail.com>
2017-05-28 12:11:43 +1000

    Update fork to 1.0.47.1 (#34972)

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,10 +1,10 @@
 cask 'fork' do
-  version '1.0.47'
-  sha256 'f874389adfb54302c87bd59817f0791ccd85c815c6af0f415c976c4ff32b502a'
+  version '1.0.47.1'
+  sha256 '8d3d7dd9bd20d00c3e777993a9ed501eb6016acadfba8943fef47e4cc169b7de'
 
   url 'https://git-fork.com/update/files/Fork.dmg'
   appcast 'https://git-fork.com/update/feed.xml',
-          checkpoint: '975d193698a70575f9479efa108b78464b28df67c4c7e04184428ce1b2bbb982'
+          checkpoint: '2fed1e0f792b7cc858ea0c5a7e199b7eb44f49b6bf611e2d01378f0823021f75'
   name 'Fork'
   homepage 'https://git-fork.com/'
 
