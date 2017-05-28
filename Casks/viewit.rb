de992c713aca4c062f96f51189ae236889c4e5a0
Jussi Tuominen <jussi@COLDwithin.local>
2017-05-28 18:06:42 +0300

    Update viewit to 2.63

--- Casks/viewit.rb
+++ Casks/viewit.rb
@@ -1,10 +1,10 @@
 cask 'viewit' do
-  version '2.62'
-  sha256 'fe1a19f6ff6a714d07255a0968810d309c63daa68dcded5e5f0017c7b38f5728'
+  version '2.63'
+  sha256 '41dfaa4904b2cf454bc9f7d3a36df2a28382e109e4be43eb53024bd26cc2d16e'
 
   url "http://www.hexcat.com/downloads/ViewIt-#{version}.zip"
   appcast 'http://www.hexcat.com/downloads.html',
-          checkpoint: '0d3fb4664613e977b6a1d4aa94fc13056c8df51e8c7b1b50ad4ef9238aba8beb'
+          checkpoint: '9dcddf7d3f126e39c3901e73b388f79b3c2aafdefd3ef198120c0592dd9fd91f'
   name 'ViewIt'
   homepage 'http://www.hexcat.com/viewit/'
 
