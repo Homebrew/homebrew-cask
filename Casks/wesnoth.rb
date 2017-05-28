9254e09ca17d44c4ba55ab7beb81c41869bc411b
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-28 12:25:54 +0800

    Update wesnoth.rb

--- Casks/wesnoth.rb
+++ Casks/wesnoth.rb
@@ -1,11 +1,11 @@
 cask 'wesnoth' do
-  version '1.13.8'
-  sha256 'df98a343ac2d83fb5f4144fb79379afa827c57cb8c68c3eaa29a2afb706352b5'
+  version '1.12.6'
+  sha256 'c94fe7880b1ed0f52e31ad570d3f082412ffbeb9d7ddadf0c64d501dfc1f0589'
 
   # sourceforge.net/wesnoth was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/wesnoth/Wesnoth_#{version}.dmg"
   appcast 'https://sourceforge.net/projects/wesnoth/rss',
-          checkpoint: '23497792e09d8ffeec2329c807e16a429f9694dc496649add9e23c753f369829'
+          checkpoint: '6ab872944613cebe3c54f7922e42f01e3122841a526699ccea41373f258c2ea0'
   name 'The Battle for Wesnoth'
   homepage 'https://wesnoth.org/'
 
