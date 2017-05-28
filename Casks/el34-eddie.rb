521d9d3da06656e6982ff84095096fd13aad3294
Vítor Galvão <info@vitorgalvao.com>
2017-05-28 23:23:02 +0100

    Update and rename eddie.rb to el34-eddie.rb

--- Casks/el34-eddie.rb
+++ Casks/el34-eddie.rb
@@ -3,7 +3,7 @@ cask 'el34-eddie' do
   sha256 'f9fe7cfd6b4d1fa8c6d6c89a9bd527525908979dc65ae96bae91e456fd1fae2b'
 
   # el34software.com was verified as official when first introduced to the cask
-  url 'http://el34software.com/latest/Eddie%20OSX%203.3.1.zip'
+  url "http://el34software.com/latest/Eddie%20OSX%20#{version}.zip"
   name 'Eddie'
   homepage 'http://www.el34.com/'
 
