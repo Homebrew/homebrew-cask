a30b14bd3e35c3c8ae56b3429e6f79ba0189d778
Alex Petin <axp@ngs.ru>
2017-05-29 18:33:24 +0700

    Update xamarin-android to 7.3.1-2 (#34992)

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-android' do
-  version '7.3.1-1'
-  sha256 '4e2b76eb4a830d718b0ab292aa747d100bc2ba834af58b779afe655a027d8bcb'
+  version '7.3.1-2'
+  sha256 'dd57b13b38323fed00c385c47aaa8adbffb2a60ff1fa0a96d13421e1376857ea'
 
   url "https://dl.xamarin.com/MonoforAndroid/Mac/xamarin.android-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '45f2c1416f8321cfd68ffc388ecedae744d12cf4345bbb9bac1e36dca3ce6214'
+          checkpoint: '08fef545d50bd40a08d194b082bba6752cb27453d3381fd4c0504bdf18dd6ab2'
   name 'Xamarin.Android'
   homepage 'https://www.xamarin.com/platform'
 
