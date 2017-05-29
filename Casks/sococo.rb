59e18c6370afe0d2b1252c88a0c9a8bd408ca220
Colin Dunn <cgd@vikingdev.net>
2017-05-29 07:21:55 -0700

    Update sococo to 0.5.4-14344 (#35115)

--- Casks/sococo.rb
+++ Casks/sococo.rb
@@ -1,10 +1,10 @@
 cask 'sococo' do
-  version '0.5.3-13375'
-  sha256 'da51bad80212950811ded38cdb3fc24258df838eae318a5ca7865403a8840cc9'
+  version '0.5.4-14344'
+  sha256 '6430e60970265f334a9f5c998c4e05809804119b002169d0d231e2bb3e896464'
 
   url "https://s.sococo.com/rs/client/mac/Sococo-#{version}.dmg"
   appcast 'https://s.sococo.com/rs/client/latest.json',
-          checkpoint: '4fa435c8784708ee384ae2af6fbc2602327abdefdf393ff0db428c34eed55383'
+          checkpoint: '8b399c9b48f81ae7340f4d391dee398f4d3533671154f94f85da342c323da45a'
   name 'Sococo'
   homepage 'https://www.sococo.com/'
 
