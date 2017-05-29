332ef0e6654b332ce792e948939498a576187fe4
Miikka Koskinen <miikka.koskinen@iki.fi>
2017-05-30 00:05:11 +0300

    Update patchwork to 3.4.0 (#34997)

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,10 +1,10 @@
 cask 'patchwork' do
-  version '3.3.3'
-  sha256 '45d620ef7aed08d41db20f99852367912b4613d9d66afc3a22475a75679d4348'
+  version '3.4.0'
+  sha256 '9be5a32dbabcdd3ca046afb0032c3de47b3d56bfea269d53d0655445c6ffc2db'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}-mac.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom',
-          checkpoint: '740e7dea88a08437987f135603fe8450050912320439acf35b2bf12a3b68075a'
+          checkpoint: '4a97057f15b9aff08e08356369003363d3c965ded15a763aa80058c4ce79bb1d'
   name 'Patchwork'
   homepage 'https://github.com/ssbc/patchwork'
 
