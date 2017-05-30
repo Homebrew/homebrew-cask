1d2e8f2f474ed2e2c6ed3b01aca69fb1cb8de0d2
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-30 08:34:26 -0400

    Update fantastical to 2.3.8 (#35014)

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,10 +1,10 @@
 cask 'fantastical' do
-  version '2.3.7'
-  sha256 '333f8f326dc3fa19d615e9dbb624d9123f0a9fdec4a852cd9002b1ebae420733'
+  version '2.3.8'
+  sha256 '7d0f3783b0548bfda3abe7155dbfdbc664d5da82323c8733a48851041e676163'
 
   url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast 'https://flexibits.com/fantastical/appcast2.php',
-          checkpoint: 'dac576c04fa7dba27ad64783f7c11853cd393f49ac7afa7da6f43d27a6cacc7d'
+          checkpoint: '7cdb92212898bcafc053c64655e714286925ed629a9f22e6462736f07befa8e5'
   name 'Fantastical'
   homepage 'https://flexibits.com/fantastical'
 
