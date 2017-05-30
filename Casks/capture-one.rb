0316d6bf86351e980ef81fe286d8adaf9a14ff26
Anthony Garand <garand@garand.me>
2017-05-30 14:02:22 -0400

    Update capture-one to 10.1.1 (#35020)

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -1,10 +1,10 @@
 cask 'capture-one' do
-  version '10.0.2'
-  sha256 'ffd01fa5ee4a5ddfdcf74db0156f6ea8ea983c6b7b382d008b6da5204cdebe2b'
+  version '10.1.1'
+  sha256 'c5c37621481c4f49ff2a91cc3648cadd814d8f6fe95d5633e9d2b506acd936d5'
 
   url "http://downloads.phaseone.com/9c7cb7b7-1525-4cfc-86a8-7d8f5a51f38e/International/CaptureOne.Mac.#{version}.dmg"
   name 'Capture One'
   homepage 'https://www.phaseone.com/en/Products/Software/Capture-One-Pro/Whats-new.aspx'
 
-  app "Capture One #{version.major}.app"
+  app 'Capture One.app'
 end
