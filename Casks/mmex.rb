dc5bb1edf7bfa801957ad35266057b27c80c1413
Gobinath Mallaiyan <i@gobinath.com>
2017-05-30 15:30:17 -0400

    Update mmex to 1.3.1 (#35040)

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -5,7 +5,7 @@ cask 'mmex' do
   # github.com/moneymanagerex/moneymanagerex was verified as official when first introduced to the cask
   url "https://github.com/moneymanagerex/moneymanagerex/releases/download/v#{version}/mmex-v#{version}-macOS10.9.dmg"
   appcast 'https://github.com/moneymanagerex/moneymanagerex/releases.atom',
-          checkpoint: '1a1324a603e532ca85978619a8852fa2d91dfdb1f7cfb9a8b5c51de1c673283e'
+          checkpoint: 'd64a84e883064903f617f33b3c01c2925d239b9f630314e24a32795109802d87'
   name 'Money Manager Ex'
   homepage 'http://www.moneymanagerex.org/'
 
