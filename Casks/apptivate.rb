a48becdfd7e3162487d240be9cb7b33d0439d759
Bruno Pinheiro <bspinheiro@me.com>
2017-05-31 12:13:32 +0200

    Add Apptivate Latest

---
+++ Casks/apptivate.rb
@@ -0,0 +1,10 @@
+cask 'apptivate' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.apptivateapp.com/resources/Apptivate.app.zip'
+  name 'Apptivate'
+  homepage 'http://www.apptivateapp.com/'
+
+  app 'Apptivate.app'
+end
