2644c859832b9316bd395eb9fddf4602a247d956
Eden S.-G <eden@edensg.me>
2017-05-31 02:50:33 -0400

    Update tigervnc-viewer to 1.8.0 (#35053)

--- Casks/tigervnc-viewer.rb
+++ Casks/tigervnc-viewer.rb
@@ -1,6 +1,6 @@
 cask 'tigervnc-viewer' do
   version '1.8.0'
-  sha256 'b816efe3f80844d63450bd80abf537d8d19c84c182ea59767c12de6aa27bd48a'
+  sha256 'fdff189c864788a174066da081e9bd8991628df46a3a2b69bd7c593d3e62d426'
 
   # bintray.com/tigervnc was verified as official when first introduced to the cask
   url "https://bintray.com/tigervnc/stable/download_file?file_path=TigerVNC-#{version}.dmg"
