6c6a4ac0f4d03a70d09d4fa9dbe765860a87106d
commitay <commitay1@gmail.com>
2017-05-31 20:37:50 +1000

    Update rambox - remove empty line (#35064)

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -3,7 +3,6 @@ cask 'rambox' do
   sha256 'cb0d0263a5bfb7f3ded3762f6ccc057e2ac9c1a14efa6152f7ee1aff71039093'
 
   # github.com/saenzramiro/rambox was verified as official when first introduced to the cask
-
   url "https://github.com/saenzramiro/rambox/releases/download/#{version}/Rambox-#{version}.dmg"
   appcast 'https://github.com/saenzramiro/rambox/releases.atom',
           checkpoint: 'e1e360e15afdef800ae546ff7658e2f8271e2091beb0275e6369b81ba85c8f70'
