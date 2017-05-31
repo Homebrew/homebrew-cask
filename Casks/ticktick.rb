429014e89678d3a72a8e448d99e3437d68d66116
Jean-Philippe Roemer <jp@roemer.im>
2017-05-31 11:21:00 +0100

    Update ticktick to 1.6.80,29

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -1,6 +1,6 @@
 cask 'ticktick' do
-  version '1.6.70,28'
-  sha256 '6196c9652c7aa57ea88a0dd6a0f577c449bf8512f997c563aca81864f664c31c'
+  version '1.6.80,29'
+  sha256 '227369aa701493da448fa4dc1b9fc0cae70a54ea57887d63787b3a3553f89c8a'
 
   # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
