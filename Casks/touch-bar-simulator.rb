71fb24b3a3c961e85162878576c783b2cd90e717
Victor Popkov <victor@popkov.me>
2017-05-31 22:49:05 +0300

    Update touch-bar-simulator to 1.2.0 (#35072)

--- Casks/touch-bar-simulator.rb
+++ Casks/touch-bar-simulator.rb
@@ -1,10 +1,10 @@
 cask 'touch-bar-simulator' do
-  version '1.1.0'
-  sha256 'c90569301b783022372a2ab298c3c66422f6342ce7394f859fb8e357c40d81b2'
+  version '1.2.0'
+  sha256 '4abe55de716ae56a41031cdb1d3b27bf6b1efae18b33b80bb0419669a9a76aa1'
 
   url "https://github.com/sindresorhus/touch-bar-simulator/releases/download/v#{version}/Touch-Bar-Simulator-#{version}.dmg"
   appcast 'https://github.com/sindresorhus/touch-bar-simulator/releases.atom',
-          checkpoint: 'fa23b23a47d65ba73e79edb980c77ae8d7f1b0fb5fc26010616503105444583b'
+          checkpoint: '833a29126dc3932077c6cb389e93ce615df633db17d0644b820f707c870e85f5'
   name 'Touch Bar Simulator'
   homepage 'https://github.com/sindresorhus/touch-bar-simulator'
 
