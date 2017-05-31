74bb5e66443412c3eee8259c2937c0719f2e86b0
Vítor Galvão <info@vitorgalvao.com>
2017-05-31 01:08:49 +0100

    Update wwdc.rb

--- Casks/wwdc.rb
+++ Casks/wwdc.rb
@@ -2,6 +2,7 @@ cask 'wwdc' do
   version '5.0.3'
   sha256 '75121262fe2a50cc6e4046160f85b7f508d9c5486d6d125cfe780db868d55fe7'
 
+  # github.com/insidegui/WWDC was verified as official when first introduced to the cask
   url "https://github.com/insidegui/WWDC/releases/download/#{version}/WWDC_v#{version}.zip"
   appcast 'https://github.com/insidegui/WWDC/releases.atom',
           checkpoint: '50d8b2567a563c1e9c5e2e19730731f19445f61777988ff626aa7b91a01709e0'
