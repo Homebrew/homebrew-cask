f62dc642124a82d74b526fc61bb581a5e2b77ee4
Jesús Urrutia <jesus.urrutia@gmail.com>
2017-06-02 15:26:40 -0400

    Add appcast option Forrest.app Cask

--- Casks/forrest.rb
+++ Casks/forrest.rb
@@ -3,6 +3,8 @@ cask 'forrest' do
   sha256 'cdff5441ca9d3db3dbcc6be29732ac2f05896135059f70a7dd49be5eede86991'
 
   url "https://github.com/stefanjudis/forrest/releases/download/v#{version}-zeta/Forrest-#{version}-zeta-mac.zip"
+  appcast 'https://github.com/stefanjudis/forrest/releases.atom',
+          checkpoint: 'd30d71d5161285042781090adc70289b20f75350fec6256564b93ac1e49d1aa8'
   name 'Forrest'
   homepage 'https://github.com/stefanjudis/forrest'
 
