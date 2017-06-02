db1a26aded7209d82a3cafba6b6035b1cae8cf84
dk <tkisme2013@gmail.com>
2017-06-02 03:51:13 -0500

    Update tripmode to 2.0.1-492 (#35101)

--- Casks/tripmode.rb
+++ Casks/tripmode.rb
@@ -1,8 +1,8 @@
 cask 'tripmode' do
-  version '1.0.7-252'
-  sha256 'de12167df44b18463fb1a85d487b857137f89fbc481a72d9d8272510f4773795'
+  version '2.0.1-492'
+  sha256 '03ff98645a6512b9f2dba8f05d4e6437340b1a2eeea0701dafdea2213d511317'
 
-  url "https://www.tripmode.ch/app/TripMode-#{version}.dmg"
+  url "https://www.tripmode.ch/app/TripMode-#{version}-app.dmg"
   appcast 'http://updates.tripmode.ch/app/appcast.xml',
           checkpoint: '1c589ebad8ed5f9c36a702b070882b73612030eb42bd23587d2106c1399d5e8e'
   name 'TripMode'
