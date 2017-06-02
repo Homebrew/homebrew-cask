2b38bcd431e6ce8d688ad3604c1d24409fab37ca
Vítor Galvão <info@vitorgalvao.com>
2017-06-02 20:28:42 +0100

    Update forrest.rb

--- Casks/forrest.rb
+++ Casks/forrest.rb
@@ -1,8 +1,8 @@
 cask 'forrest' do
-  version '1.0.0'
+  version '1.0.0-zeta'
   sha256 'cdff5441ca9d3db3dbcc6be29732ac2f05896135059f70a7dd49be5eede86991'
 
-  url "https://github.com/stefanjudis/forrest/releases/download/v#{version}-zeta/Forrest-#{version}-zeta-mac.zip"
+  url "https://github.com/stefanjudis/forrest/releases/download/v#{version}/Forrest-#{version}-mac.zip"
   appcast 'https://github.com/stefanjudis/forrest/releases.atom',
           checkpoint: 'd30d71d5161285042781090adc70289b20f75350fec6256564b93ac1e49d1aa8'
   name 'Forrest'
