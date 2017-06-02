28bdcd7b5436f1872982fcbd563e827daed0f355
Grettir Asmundarson <grettir@tinypineapple.com>
2017-06-02 12:44:54 -0600

    Update hazel to 4.1.3

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -1,11 +1,11 @@
 cask 'hazel' do
-  version '4.1.2'
-  sha256 'f736ee3529d14ea86d707941d27e17260cf94f4d3bf8d780b1bb1a3377688d5e'
+  version '4.1.3'
+  sha256 '764fd94c873b21f481920e8f796c0dc988714a16eff1092884347f39c1302d07'
 
   # s3.amazonaws.com/Noodlesoft was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Noodlesoft/Hazel-#{version}.dmg"
   appcast 'https://www.noodlesoft.com/Products/Hazel/generate-appcast.php',
-          checkpoint: '7ddf8244c4d2a4ed87b6d1e17bb7f94c8534d1d47af408c5af9f2ac199dd3aea'
+          checkpoint: '589e9a1726a6032b89939410b6a8ce29a5a0da2fa73943c0522b6972c34e016b'
   name 'Hazel'
   homepage 'https://www.noodlesoft.com/'
 
