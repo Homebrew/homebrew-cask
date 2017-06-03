f457f7c7b79d204697e89dfaa2bdd066431b7822
Vítor Galvão <info@vitorgalvao.com>
2017-06-04 00:20:55 +0100

    Update binary-ninja.rb

--- Casks/binary-ninja.rb
+++ Casks/binary-ninja.rb
@@ -1,6 +1,6 @@
 cask 'binary-ninja' do
-  version '10.0.20 demo'
-  sha256 '7c35c3d7561e9905bfc1d838b2a9648538da21076db9e2e08c1d4d0606aae3eb'
+  version :latest
+  sha256 :no_check
 
   url 'https://cdn.binary.ninja/installers/BinaryNinja-demo.dmg'
   name 'Binary Ninja'
