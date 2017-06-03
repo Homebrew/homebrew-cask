0153c6ba0efae0961d2b8d861ec3aa829649bdeb
Victor Popkov <victor@popkov.me>
2017-06-03 21:47:30 +0300

    Update displaycal to 3.3.0.0

--- Casks/displaycal.rb
+++ Casks/displaycal.rb
@@ -1,11 +1,11 @@
 cask 'displaycal' do
-  version '3.2.4.0'
-  sha256 'a0d31130473637422fef0c4c2f56de43439f5dd820e879fad5bfa97385f352cf'
+  version '3.3.0.0'
+  sha256 'c450db7db2ed8e444c51ab07c1c41cbe4f8e330afa88cea65f830c68b84c3413'
 
   # sourceforge.net/dispcalgui was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/dispcalgui/release/#{version}/DisplayCAL-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/dispcalgui/rss?path=/release',
-          checkpoint: 'd5b7860c827fe9f1d2187148610cfd32927d46710d8d14498212a134c91e6902'
+          checkpoint: 'de7a888bc8af79eb88d5b03651aaae239c7c09912358127e32b08d4fb05269b9'
   name 'DisplayCAL'
   homepage 'https://displaycal.net/'
 
