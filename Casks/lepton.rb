0931fc6469014975da2fc96e60f6dda723207c35
Victor Popkov <victor@popkov.me>
2017-06-03 22:30:59 +0300

    Update lepton to 1.3.0 (#35177)

--- Casks/lepton.rb
+++ Casks/lepton.rb
@@ -1,11 +1,11 @@
 cask 'lepton' do
-  version '1.2.2'
-  sha256 'f829674b3c4bc37b429c50cb4a1ebd3a44e2aa4a198c10fb68344a24c496a263'
+  version '1.3.0'
+  sha256 '0d8396d775feb474dd747addd0d2672253a295921fb7cf64f4efbc5d7a73afd7'
 
   # github.com/hackjutsu/Lepton was verified as official when first introduced to the cask
   url "https://github.com/hackjutsu/Lepton/releases/download/v#{version}/Lepton-#{version}-mac.zip"
   appcast 'https://github.com/hackjutsu/Lepton/releases.atom',
-          checkpoint: '77ddd48b815605224488b1015e11da14d82a3481cf4c14018f9b008aa0c2324a'
+          checkpoint: 'dea9c256fbcb39688d097f628a61acc3a916ddebfa320781c29cae8b18f70711'
   name 'Lepton'
   homepage 'http://hackjutsu.com/Lepton/'
 
