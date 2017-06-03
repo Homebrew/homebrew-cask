48eaffe4a6b5fef34c71d2bacc0327679089be6b
Yurii Kolesnykov <yurikoles@gmail.com>
2017-06-03 06:52:19 +0300

    telegram-desktop: set auto_updates true

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -9,6 +9,7 @@ cask 'telegram-desktop' do
   name 'Telegram Desktop'
   homepage 'https://desktop.telegram.org/'
 
+  auto_updates true
   conflicts_with cask: 'telegram-desktop-dev'
 
   # Renamed to avoid conflict with telegram
