d505ef60660490ddbf0da7e28cb0772a83320c54
commitay <commitay@users.noreply.github.com>
2017-06-04 07:04:25 +1000

    Update qownnotes to 17.06.2,b3039-094931

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,11 +1,11 @@
 cask 'qownnotes' do
-  version '17.06.1,b3034-151538'
-  sha256 '0355e9986be0b31a480255a9bc762b9c5a4113af551f3f8caa5a79cbf8d6decb'
+  version '17.06.2,b3039-094931'
+  sha256 '75fba5fb7254483bc5dd112e665fff0ae9196b70e3240ce18d1ab3e4295a652e'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
   appcast 'https://github.com/pbek/QOwnNotes/releases.atom',
-          checkpoint: '322b61637084f1c1185bcdb28b10f80f6d62ef9cb5e9c40d640759122d6cfdc5'
+          checkpoint: '2a5723eb27ec292017442db4b7b65f9ae09a473035c8d629e8330aaf83e0660a'
   name 'QOwnNotes'
   homepage 'https://www.qownnotes.org/'
 
