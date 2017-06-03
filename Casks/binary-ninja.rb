f3a0ac89e80ea7641412388a55cbdb67fe085862
Kamil Boratyński <kamil.boratynski@icloud.com>
2017-06-03 23:02:46 +0200

    Added BinaryNinja v10.0.20 demo.

---
+++ Casks/binary-ninja.rb
@@ -0,0 +1,10 @@
+cask 'binary-ninja' do
+  version '10.0.20 demo'
+  sha256 '7c35c3d7561e9905bfc1d838b2a9648538da21076db9e2e08c1d4d0606aae3eb'
+
+  url 'https://cdn.binary.ninja/installers/BinaryNinja-demo.dmg'
+  name 'Binary Ninja'
+  homepage 'https://binary.ninja/'
+
+  app 'Binary Ninja.app'
+end
