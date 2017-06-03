641e5dc84388b4cf7ac5577b9159a87bd7b0e066
Victor Popkov <victor@popkov.me>
2017-06-03 21:49:49 +0300

    Update ghost to 1.3.0

--- Casks/ghost.rb
+++ Casks/ghost.rb
@@ -1,11 +1,11 @@
 cask 'ghost' do
-  version '1.2.1'
-  sha256 'a40c46a597160cdb253bfa2866e3047eb2e4c132622597fd23f6e1be3ed7ebb7'
+  version '1.3.0'
+  sha256 '0c871ed93f9d0536e910d7cbce22ad0eac7a84fd131f19d7b75aca7671dee041'
 
   # github.com/TryGhost/Ghost-Desktop was verified as official when first introduced to the cask
   url "https://github.com/TryGhost/Ghost-Desktop/releases/download/v#{version}/ghost-desktop-#{version}-osx.zip"
   appcast 'https://github.com/TryGhost/Ghost-Desktop/releases.atom',
-          checkpoint: '197e1e03c5ca68fa356fb9ab76f03b5e9e0e01e5e73d85e9d5c2fed8705b3472'
+          checkpoint: '63116eb7bdc8c0f15dda0e4819c65b06dd3f17081d41076a18442a7e57c8455f'
   name 'Ghost Desktop'
   homepage 'https://ghost.org/downloads/'
 
