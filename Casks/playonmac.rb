58075e5220a66088ff45fc9b26d0607723360243
Colin Dunn <cgd@vikingdev.net>
2017-06-03 12:14:26 +1000

    Update playonmac to 4.2.11 (#35152)

--- Casks/playonmac.rb
+++ Casks/playonmac.rb
@@ -1,10 +1,10 @@
 cask 'playonmac' do
-  version '4.2.10'
-  sha256 '6d95f5e4b6b89b6f38d873738759a708304f528c8696c080043328457d9b3f24'
+  version '4.2.11'
+  sha256 '489e6806b2952cad193abd4918ce597c27b105ffe645d044219c766862193d64'
 
   url "http://repository.playonmac.com/PlayOnMac/PlayOnMac_#{version}.dmg"
   appcast "https://github.com/PlayOnLinux/POL-POM-#{version.major}/releases.atom",
-          checkpoint: 'b939d1657277128da61461be04c876c32a8f88e48fa46ed0f002230ede1c190e'
+          checkpoint: 'a4859d74522483388025f71b5bb45ea53ca9f6a5892163a92fa7168c010e7fd9'
   name 'PlayOnMac'
   homepage 'https://www.playonmac.com/en'
 
