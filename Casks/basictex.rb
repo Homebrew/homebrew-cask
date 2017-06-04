89f8a940562503f31deb22a7cf4e560da847e806
Conan1985 <youwei_lu@hotmail.com>
2017-06-04 14:32:42 -0400

    Update basictex to 20170523

--- Casks/basictex.rb
+++ Casks/basictex.rb
@@ -1,6 +1,6 @@
 cask 'basictex' do
-  version '2016.1009'
-  sha256 '17f27a9c05f108b889b39d8ea2ff6236c1265f244e8e0376e9d14af4acf680bd'
+  version '20170523'
+  sha256 '024fc0e2bff8d28074a25ee8adc12c2ab37202cfea7229678d92a7b5d1debba2'
 
   # mirror.ctan.org/systems/mac/mactex was verified as official when first introduced to the cask
   url "http://mirror.ctan.org/systems/mac/mactex/mactex-basictex-#{version.no_dots}.pkg"
