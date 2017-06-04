6b4d002d9df58e012c72e752362dae256c73b8c4
Vítor Galvão <info@vitorgalvao.com>
2017-06-04 21:36:17 +0100

    Update ios-saver.rb

--- Casks/ios-saver.rb
+++ Casks/ios-saver.rb
@@ -2,7 +2,6 @@ cask 'ios-saver' do
   version :latest
   sha256 :no_check
 
-  # dlstatsios7.site90.net was verified as official when first introduced to the cask
   url 'http://littleendiangamestudios.com/download/ios_screensaver/',
       referer: 'http://littleendiangamestudios.com/project/ios-8-screen-saver/'
   name 'iOS 8 Lockscreen for OSX'
