73a403f4490cd6a2de8fb9266db5c1a78a30b386
Colin Dunn <cgd@vikingdev.net>
2017-06-05 22:58:36 +1000

    Update visual-paradigm to 14.1,20170601 (#35213)

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm' do
-  version '14.1,20170519'
-  sha256 '5f70171ec54b2c15d5ab740e89f88e83a5145e8299cdd3d1358ad2ce3f4cc5e5'
+  version '14.1,20170601'
+  sha256 '5580528f7c61176d4ee44f0c6bec009983f6031a1fb12735c3a44fc67704338b'
 
   url "https://eu6.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.dots_to_underscores.gsub(',', '_')}_OSX_WithJRE.dmg"
   name 'Visual Paradigm'
