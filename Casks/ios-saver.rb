71708dc0d0ef80139f40de0375c5a23bd1a16b50
Joey Hoer <me@joeyhoer.com>
2017-06-04 22:50:57 -0400

    Updating iOS-Saver URL (latest) (#35193)
    * Updating iOS-Saver URL (latest)
    
    * Update ios-saver.rb

--- Casks/ios-saver.rb
+++ Casks/ios-saver.rb
@@ -2,8 +2,7 @@ cask 'ios-saver' do
   version :latest
   sha256 :no_check
 
-  # dlstatsios7.site90.net was verified as official when first introduced to the cask
-  url 'http://dlstatsios7.site90.net/download.php',
+  url 'http://littleendiangamestudios.com/download/ios_screensaver/',
       referer: 'http://littleendiangamestudios.com/project/ios-8-screen-saver/'
   name 'iOS 8 Lockscreen for OSX'
   homepage 'http://littleendiangamestudios.com/project/ios-8-screen-saver/'
