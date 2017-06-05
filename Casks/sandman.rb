58d69bf6e1c2ba92f38e340ebc733a3bf291c94e
Vítor Galvão <info@vitorgalvao.com>
2017-06-05 21:08:54 +0100

    verified link was not specific enough

--- Casks/sandman.rb
+++ Casks/sandman.rb
@@ -2,7 +2,7 @@ cask 'sandman' do
   version '1.8.0'
   sha256 'faff9c1e068de8a69764a7dff8fa582b688e99855953ca88bc78a667586f5ba4'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/alexanderepstein/Sandman was verified as official when first introduced to the cask
   url "https://github.com/alexanderepstein/Sandman/releases/download/v#{version}/Sandman-#{version}.dmg"
   name 'Sandman'
   homepage 'https://alexanderepstein.github.io/Sandman/'
