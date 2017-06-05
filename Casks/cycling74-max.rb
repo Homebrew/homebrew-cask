4745e836e80db89c5be9570e763050bb5c5bc43e
Colin Dunn <cgd@vikingdev.net>
2017-06-05 22:59:05 +1000

    Update cycling74-max to 7.3.4_170530 (#35215)

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,6 +1,6 @@
 cask 'cycling74-max' do
-  version '7.3.3_170301'
-  sha256 '87095679f673b2d29c24a0229358f5c9153370d1a80b163497b749c932c6210b'
+  version '7.3.4_170530'
+  sha256 '355fe6e81d37dffcb6dc478f213ab37191c9e931350d45d76e13763628c645e2'
 
   # akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.no_dots}.dmg"
