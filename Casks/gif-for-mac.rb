2d95511ee6748ac45b5f4d8c9a3d44029b5e75d7
Miccal Matthews <miccal.matthews@gmail.com>
2017-06-05 20:35:54 +0800

    Update gif-for-mac.rb

--- Casks/gif-for-mac.rb
+++ Casks/gif-for-mac.rb
@@ -2,6 +2,7 @@ cask 'gif-for-mac' do
   version :latest
   sha256 :no_check
 
+  # media.tenor.co/mac/bin was verified as official when first introduced to the cask
   url 'https://media.tenor.co/mac/bin/GIFforMac.dmg'
   name 'GIF for Mac'
   homepage 'https://tenor.com/mac'
