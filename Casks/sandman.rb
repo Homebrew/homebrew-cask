271c877ea1c3592a50276c63ac78bebdf8a49a65
Alex Epstein <epsteina@wit.edu>
2017-06-05 16:36:32 -0400

    Adding appcast for Sandman 1.8.0

--- Casks/sandman.rb
+++ Casks/sandman.rb
@@ -4,8 +4,10 @@ cask 'sandman' do
 
   # github.com/alexanderepstein/Sandman was verified as official when first introduced to the cask
   url "https://github.com/alexanderepstein/Sandman/releases/download/v#{version}/Sandman-#{version}.dmg"
+  appcast 'https://github.com/alexanderepstein/Sandman/releases.atom',
+          checkpoint: '1412a9607110f348f3f0bac66adc917cf16046c2a48affeb392140914937c6c0'
   name 'Sandman'
   homepage 'https://alexanderepstein.github.io/Sandman/'
-
+  
   app 'Sandman.app'
 end
