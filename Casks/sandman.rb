3cab81eda28950588d994d464dd93b647ae1b4e8
Alex Epstein <epsteina@wit.edu>
2017-06-05 13:39:03 -0400

    Adding The Sandman cask

---
+++ Casks/sandman.rb
@@ -0,0 +1,10 @@
+cask 'sandman' do
+  version '1.8.0'
+  sha256 'faff9c1e068de8a69764a7dff8fa582b688e99855953ca88bc78a667586f5ba4'
+  # github.com was verified as official when first introduced to the cask
+  url "https://github.com/alexanderepstein/Sandman/releases/download/v#{version}/Sandman-#{version}.dmg"
+  name 'Sandman'
+  homepage 'https://alexanderepstein.github.io/sandman'
+  license 'MIT'
+  app 'Sandman.app'
+end
