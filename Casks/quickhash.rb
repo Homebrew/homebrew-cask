583c6aa9795ebe0a61cea313fe12a518be7cb0f2
Colin Dunn <cgd@vikingdev.net>
2017-06-07 10:33:39 +1000

    Update quickhash to 2.8.2,396 (#35272)

--- Casks/quickhash.rb
+++ Casks/quickhash.rb
@@ -1,10 +1,10 @@
 cask 'quickhash' do
-  version '2.8.1,379'
-  sha256 '64afa9be2625ff0d0544cf944faa467f412777457c102fe0dd789b4566afb0e3'
+  version '2.8.2,396'
+  sha256 '527670f17fdfec451c8808e62baeb44d2c831efb019f195bbaebad8e0addec34'
 
   url "http://quickhash-gui.org/download/quickhash-v#{version.before_comma.dots_to_hyphens}-for-apple-mac-osx/?wpdmdl=#{version.after_comma}"
   name 'Quickhash'
   homepage 'http://quickhash-gui.org/'
 
-  app 'OSX/QuickHash.app'
+  app "QuickHash-v#{version.before_comma}-OSX"
 end
