7bba4d6d2c88b5c0d36d694a8cd349074cc65dd5
jcbot <jcb@leipert.io>
2017-06-07 18:01:33 +0200

    Update appcode to 2017.1.3,171.4694.27

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,10 +1,10 @@
 cask 'appcode' do
-  version '2017.1.2,171.4249.57'
-  sha256 '4e78fe0115de5f2e4c7086963ba76753c5b2a93845d17fb501488c328f9b8b58'
+  version '2017.1.3,171.4694.27'
+  sha256 '85461480392290fa2fd941196d5677e5c2869fe03fbe95a8cda1d452f61f5b28'
 
   url "https://download.jetbrains.com/objc/AppCode-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=AC&latest=true&type=release',
-          checkpoint: 'b51c84e37f40e057398625e16f127b4c9e032efba48e42360033a023951e0ff8'
+          checkpoint: '51c512573d9be7ba04caca7995da23017b02fb9f5f0536b7662b775214ed1279'
   name 'AppCode'
   homepage 'https://www.jetbrains.com/objc/'
 
