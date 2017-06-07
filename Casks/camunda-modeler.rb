c88215e6ea680587ec6008be9b5c01002a50a01b
Jan Galinski <jan.galinski@holisticon.de>
2017-06-07 12:51:01 +0200

    Update camunda-modeler to 1.8.1

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask 'camunda-modeler' do
-  version '1.8.0'
-  sha256 '325f33cb325e2ae1ab1bca204804fe6429589d8cc1ffbe45bcf9f8f74f1e5fab'
+  version '1.8.1'
+  sha256 'b3a13bb3ce4783c68cad274a10a154766bdc4de27d87e28ff5d203be538d5743'
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-darwin-x64.tar.gz"
   name 'Camunda Modeler'
