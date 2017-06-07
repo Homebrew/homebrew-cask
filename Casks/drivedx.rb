ac3b2f6a13494bce95c54eb678bc925d5b40318e
Bert Devriese <bert@builtinbruges.com>
2017-06-07 13:36:33 +0200

    Add DriveDX.app v1.5.1

---
+++ Casks/drivedx.rb
@@ -0,0 +1,10 @@
+cask 'drivedx' do
+  version '1.5.1'
+  sha256 '32c9cc2cc5c28ce8a1800d4dc805fcdea040c0aab86f8ae9f658d94c5ff13668'
+
+  url "https://binaryfruit.com/download/drivedx/mac/1/bin/DriveDx.#{version}.zip"
+  name 'DriveDX'
+  homepage 'https://binaryfruit.com/drivedx/'
+
+  app 'DriveDX.app'
+end
