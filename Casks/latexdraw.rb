2c50a553888ef937ec5c11f3bffe233c8b1bd682
Vítor Galvão <info@vitorgalvao.com>
2017-06-08 12:10:46 +0100

    Create latexdraw.rb

--- Casks/latexdraw.rb
+++ Casks/latexdraw.rb
@@ -2,9 +2,9 @@ cask 'latexdraw' do
   version '3.3.5'
   sha256 '905da2d258baf777dff9cb14468207dff04094049f9bc82f050d8a8541967965'
 
-  url 'https://downloads.sourceforge.net/latexdraw/LaTeXDraw-3.3.5.app.zip'
+  url "https://downloads.sourceforge.net/latexdraw/LaTeXDraw-#{version}.app.zip"
   name 'LaTexDraw'
   homepage 'http://latexdraw.sourceforge.net/'
 
-  app 'LaTexDraw-3.3.5.app', target: 'LaTexDraw.app'
+  app 'LaTexDraw-3.3.5.app'
 end
