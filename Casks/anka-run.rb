3ff2b92026fa2eb768a914c620ab10c9a59f372c
commitay <commitay@users.noreply.github.com>
2017-06-09 07:30:28 +1000

    Add anka-run 1.0_34
    remove empty line

---
+++ Casks/anka-run.rb
@@ -0,0 +1,26 @@
+cask 'anka-run' do
+  version '1.0_34'
+  sha256 'b1d5edd68a20964298c3ff41c48b5b0fe7ce0974e57f666a1de087919167ed2d'
+
+  # d1efqjhnhbvc57.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d1efqjhnhbvc57.cloudfront.net/AnkaRun-#{version}.pkg"
+  name 'Veertu Anka Run'
+  homepage 'https://veertu.com/'
+
+  pkg "AnkaRun-#{version}.pkg"
+
+  uninstall script: {
+                      executable: '/Library/Application Support/Veertu/Anka/tools/uninstall.sh',
+                      args:       ['-f'],
+                      sudo:       true,
+                    }
+
+  zap delete: [
+                '~/Library/Application Support/Veertu/Anka',
+                '/Library/Application Support/Veertu/Anka',
+              ],
+      rmdir:  [
+                '~/Library/Application Support/Veertu',
+                '/Library/Application Support/Veertu',
+              ]
+end
