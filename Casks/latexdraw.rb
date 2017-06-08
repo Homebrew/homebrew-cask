5adc6f7280c764fa1bbaefd0a675aa3a33edbe40
Aurelien Noce <aurelien.noce@imagine-app.fr>
2017-06-08 15:20:47 +0200

    Add latexdraw.rb

--- Casks/latexdraw.rb
+++ Casks/latexdraw.rb
@@ -3,6 +3,8 @@ cask 'latexdraw' do
   sha256 '905da2d258baf777dff9cb14468207dff04094049f9bc82f050d8a8541967965'
 
   url "https://downloads.sourceforge.net/latexdraw/LaTeXDraw-#{version}.app.zip"
+  appcast 'https://sourceforge.net/projects/latexdraw/rss',
+          checkpoint: '359991ab747f45b0157314d4826dc2cb3f311e222e6f3dca4fff361cd9d35144'
   name 'LaTexDraw'
   homepage 'http://latexdraw.sourceforge.net/'
 
