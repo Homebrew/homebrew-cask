2cd1fb4ff3d3d7571fba98a9cabf49f02c82d41c
commitay <commitay1@gmail.com>
2017-06-08 12:07:06 +1000

    Update arq to 5.8.6 (#35304)

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,10 +1,10 @@
 cask 'arq' do
-  version '5.8.5'
-  sha256 '430eb506fdab80cd949b527e9798355acd79c1e89de1c5dae828be5e421af7dc'
+  version '5.8.6'
+  sha256 '0f488137af538e8761b670ee7e69f033572b3a5800e43dac8a40b8ebeafad562'
 
   url "https://www.arqbackup.com/download/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arq#{version.major}.xml",
-          checkpoint: '9b0ad0dccb0580e19d23b211847f6109a059861083f7e7ce74bfc3b8a26f7abe'
+          checkpoint: 'c4d184bbab0eb181cec46d0343bc6e3bec0619546ef12a306ad8a9ddbde59bd6'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
