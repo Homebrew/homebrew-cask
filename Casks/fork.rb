c699544b40b783710320706ea2cc49a67b5c3492
commitay <commitay@users.noreply.github.com>
2017-06-09 06:37:50 +1000

    Update fork to 1.0.48

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,10 +1,10 @@
 cask 'fork' do
-  version '1.0.47.1'
-  sha256 '8d3d7dd9bd20d00c3e777993a9ed501eb6016acadfba8943fef47e4cc169b7de'
+  version '1.0.48'
+  sha256 'fd04d47ecf5d6ededa567c1c7b61c020563143b90f5062610a9734ef7ab32ce1'
 
   url 'https://git-fork.com/update/files/Fork.dmg'
   appcast 'https://git-fork.com/update/feed.xml',
-          checkpoint: '2fed1e0f792b7cc858ea0c5a7e199b7eb44f49b6bf611e2d01378f0823021f75'
+          checkpoint: '56947e5aa7585accb5a2601206c5eaa1f1957b4cd11fe978146a76428e75be90'
   name 'Fork'
   homepage 'https://git-fork.com/'
 
