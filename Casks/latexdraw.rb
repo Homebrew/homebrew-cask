46bc4fc1f31a42e1732f0762764d519db6d185de
Vítor Galvão <info@vitorgalvao.com>
2017-06-09 22:18:13 +0100

    Create latexdraw.rb

--- Casks/latexdraw.rb
+++ Casks/latexdraw.rb
@@ -3,8 +3,8 @@ cask 'latexdraw' do
   sha256 '905da2d258baf777dff9cb14468207dff04094049f9bc82f050d8a8541967965'
 
   url "https://downloads.sourceforge.net/latexdraw/LaTeXDraw-#{version}.app.zip"
-  appcast 'https://sourceforge.net/projects/latexdraw/rss',
-          checkpoint: '359991ab747f45b0157314d4826dc2cb3f311e222e6f3dca4fff361cd9d35144'
+  appcast 'https://sourceforge.net/projects/latexdraw/rss?path=/latexdraw',
+          checkpoint: 'e032f06eb8bb7ed367670947673f763ff1d9b4a1cc662a969c28c29dcbcabe56'
   name 'LaTexDraw'
   homepage 'http://latexdraw.sourceforge.net/'
 
