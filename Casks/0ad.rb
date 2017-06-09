80ac2460e291fa2cf22b335440af0fe4264b333c
giannitm <giannitm@users.noreply.github.com>
2017-06-09 22:12:26 +0100

    Update 0ad to 0.0.21-alpha (#35345)

--- Casks/0ad.rb
+++ Casks/0ad.rb
@@ -5,7 +5,7 @@ cask '0ad' do
   # wildfiregames.com was verified as official when first introduced to the cask
   url "http://releases.wildfiregames.com/0ad-#{version}-osx64.dmg"
   appcast 'https://play0ad.com/download/mac/',
-          checkpoint: 'f79d0c015cf663ad3a72ca1f6e2b9512c4c0402887670d31c80450e12aec6bf0'
+          checkpoint: 'd540eba952d8c3871d4c12fd7690a5a276dab4f627e601e77f1c78091d3a5c86'
   name '0 A.D.'
   homepage 'https://play0ad.com/'
 
