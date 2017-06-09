9273822c86f0a242403bf50f13c29a57009a2907
Benedikt Müller <bemueller@posteo.de>
2017-06-09 10:28:10 +0200

    Add Todour.app v2.06
    Todour is an application for handling todo.txt files (as made famous by Gina Trapani of Lifehacker) on Windows, Mac and Linux.

---
+++ Casks/todour.rb
@@ -0,0 +1,10 @@
+cask 'todour' do
+  version '2.06'
+  sha256 '94651ae0c9aebd1b389c7c21799ac3c7053a9ab46eaeb8a6d492d21ce3eab93d'
+
+  url "http://nerdur.com/Todour-#{version}.dmg"
+  name 'Todour'
+  homepage 'http://nerdur.com/todour-pl/'
+
+  app 'Todour.app'
+end
