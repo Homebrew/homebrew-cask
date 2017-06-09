f92c632ee15f5aa8de30be2c3e21e76bbfb4e931
commitay <commitay1@gmail.com>
2017-06-10 06:06:36 +1000

    Update anka-run - update zap / add appcast (#35326)
    * Update anka-run - add appcast
    
    
    add folder to zap
    
    
    fix zap mistake
    
    * update appcast checkpoint

--- Casks/anka-run.rb
+++ Casks/anka-run.rb
@@ -4,6 +4,8 @@ cask 'anka-run' do
 
   # d1efqjhnhbvc57.cloudfront.net was verified as official when first introduced to the cask
   url "https://d1efqjhnhbvc57.cloudfront.net/AnkaRun-#{version}.pkg"
+  appcast 'https://ankadoc.bitbucket.io/release-notes/index.html',
+          checkpoint: 'e8fdd43a7c0fea864e6c24360b5a8aa87c014f5e8c22768e41a2c7da52f8a563'
   name 'Veertu Anka Run'
   homepage 'https://veertu.com/'
 
@@ -21,6 +23,7 @@ cask 'anka-run' do
                 '~/.anka.mac_prefix.txt',
                 '~/Library/Application Support/Veertu/Anka',
                 '~/Library/Logs/com.veertu.anka',
+                '~/vm_lib',
                 '/Library/Application Support/Veertu/Anka',
               ],
       rmdir:  [
