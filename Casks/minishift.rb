c35221330505e45e16a135ef8c9dbda2b61f8295
Donovan Muller <donovan.muller@gmail.com>
2017-06-09 07:57:50 +0200

    Update minishift to 1.1.0

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -1,10 +1,10 @@
 cask 'minishift' do
-  version '1.0.1'
-  sha256 '1bf40df9909a7a44fbdb0f229eaff86a803e080a365c7d05f54bf6af1c19f58d'
+  version '1.1.0'
+  sha256 '6c516e11c8b69995df76e39100cda98906259ea5f5ceb860cac51a451bc39d0a'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom',
-          checkpoint: '80a24dd49bb10aa5a8ef21dc492eb487f4931343224662049b2118250b4ab49a'
+          checkpoint: 'b6068bca3356018f3bddba72eb7b6ee45caa29846c3284956504d13603963111'
   name 'Minishift'
   homepage 'https://github.com/minishift/minishift'
 
