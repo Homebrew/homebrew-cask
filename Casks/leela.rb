37f72e4d0e1c9be985927ab38283e2cbd205c4d9
Adrian Petrescu <apetresc@gmail.com>
2017-06-10 07:38:51 -0700

    leela cask: remove opencl cask and use version string

--- Casks/leela.rb
+++ Casks/leela.rb
@@ -2,9 +2,10 @@ cask 'leela' do
   version '0.10.0'
   sha256 '567ce9969675d04a4373a2604b8e9c84b2b2d25509edf62b317316e12b78f4a6'
 
-  url 'https://sjeng.org/dl/Leela_0.10.0.dmg'
+  url "https://sjeng.org/dl/Leela_#{version}.dmg"
   name 'leela'
   homepage 'https://sjeng.org/leela.html'
 
   app 'Leela.app'
+  app 'Leela OpenCL.app'
 end
