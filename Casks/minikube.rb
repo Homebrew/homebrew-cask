67ef9b3ba9e97e755266730585e0e5f693e5cc68
Phil Smith <phil.h.smith@gmail.com>
2017-06-09 22:37:35 -0700

    Update appcast checkpoint for minikube 0.19.1
    I'm not sure why the hash changed, but
    `brew cask audit minikube --download` revealed the issue.  Calculating
    the hash manually (as detailed in `appcast.md`) produces the new hash as
    well.

--- Casks/minikube.rb
+++ Casks/minikube.rb
@@ -5,7 +5,7 @@ cask 'minikube' do
   # storage.googleapis.com/minikube was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/minikube/releases/v#{version}/minikube-darwin-amd64"
   appcast 'https://github.com/kubernetes/minikube/releases.atom',
-          checkpoint: 'acf23bcf8c82a0010c2bd9821ac9d3a9a10e03bd3158da0f3a69127286093f46'
+          checkpoint: 'b72d7dd99f9e92b115c459ce532db88b70490a5eae48b189947c4881281ac5e0'
   name 'Minikube'
   homepage 'https://github.com/kubernetes/minikube'
 
