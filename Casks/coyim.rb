d838121c5a268901fbb44bb1f8d9233bf9486916
commitay <commitay1@gmail.com>
2017-06-10 13:23:13 +1000

    Update coyim to 0.3.8 (#35359)

--- Casks/coyim.rb
+++ Casks/coyim.rb
@@ -1,6 +1,6 @@
 cask 'coyim' do
   version '0.3.8'
-  sha256 '80ef836252349f867cd48a2532b65505aceac480c25b02472a1416b88d3e9494'
+  sha256 '1238d6d67087c38a003eafa54286b492342e2f62ddcb0d4a0fcacde07591fdf1'
 
   # bintray.com/twstrike/coyim was verified as official when first introduced to the cask
   url "https://dl.bintray.com/twstrike/coyim/v#{version}/mac-bundle/coyim.dmg"
