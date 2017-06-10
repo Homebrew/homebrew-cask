aa250f61da3e06070849a11fbb52eb1deb96b5cd
Adrian Petrescu <apetresc@gmail.com>
2017-06-09 22:38:51 -0700

    New formulas: leela and leela-opencl 0.10.0

---
+++ Casks/leela.rb
@@ -0,0 +1,10 @@
+cask 'leela' do
+  version '0.10.0'
+  sha256 '567ce9969675d04a4373a2604b8e9c84b2b2d25509edf62b317316e12b78f4a6'
+
+  url 'https://sjeng.org/dl/Leela_0.10.0.dmg'
+  name 'leela'
+  homepage 'https://sjeng.org/leela.html'
+
+  app 'Leela.app'
+end
