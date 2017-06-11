3f81159c304c6887a09faeb319be486bd124fd84
Miccal Matthews <miccal.matthews@gmail.com>
2017-06-11 15:01:09 +0800

    Create triumph.rb

--- Casks/triumph.rb
+++ Casks/triumph.rb
@@ -2,7 +2,7 @@ cask 'triumph' do
   version :latest
   sha256 :no_check
 
-  # www.audiofile-engineering.com/triumph/download was verified as official when first introduced to the cask
+  # audiofile-engineering.com/triumph/download was verified as official when first introduced to the cask
   url 'https://www.audiofile-engineering.com/triumph/download/Triumph.dmg'
   name 'Triumph'
   homepage 'http://triumph.aurchitect.com/'
