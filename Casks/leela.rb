501b46da1c4a82c76aab0d68124305f74df7aba8
Markus Reiter <me@reitermark.us>
2017-06-11 02:48:59 +0200

    Capitalize name.

--- Casks/leela.rb
+++ Casks/leela.rb
@@ -3,7 +3,7 @@ cask 'leela' do
   sha256 '567ce9969675d04a4373a2604b8e9c84b2b2d25509edf62b317316e12b78f4a6'
 
   url "https://sjeng.org/dl/Leela_#{version}.dmg"
-  name 'leela'
+  name 'Leela'
   homepage 'https://sjeng.org/leela.html'
 
   app 'Leela.app'
