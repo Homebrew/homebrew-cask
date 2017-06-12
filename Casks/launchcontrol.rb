880084902255feea0109f14cacabb14eef71e9d8
Grettir Asmundarson <grettir@tinypineapple.com>
2017-06-12 16:21:16 -0600

    Update launchcontrol to 1.32 (#35455)

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,10 +1,10 @@
 cask 'launchcontrol' do
-  version '1.31'
-  sha256 '7484957adbd92efb55ef29a9b1c08b4c79e02e026372c4c500922d64b926a9ed'
+  version '1.32'
+  sha256 '1375cd061e2c38531c7851e31471d46f8ecc3617cc5011dfedfdcbd40c69ba57'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
-          checkpoint: 'db96982f7e4175fea6807be01d913a9e2354bb4a02ddbd16d457b9143f0ec074'
+          checkpoint: '1fd999fc5135dfe9037c94454ec590284d4c81df86f5092c50642443a9f41a15'
   name 'LaunchControl'
   homepage 'http://www.soma-zone.com/LaunchControl/'
 
