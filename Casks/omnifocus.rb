fc11a758b912590ab83e7bfee5c141c22c4c88ed
Rajiv Aaron Manglani <rajiv@alum.mit.edu>
2017-06-12 19:01:49 -0400

    Update omnifocus to 2.10

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -12,13 +12,13 @@ cask 'omnifocus' do
     sha256 'a273e55c15f82540fe305344f9e49ad7d0d9c326ba2c37c312076ffd73780f80'
     url "https://downloads.omnigroup.com/software/MacOSX/10.10/OmniFocus-#{version}.dmg"
   else
-    version '2.9.1'
-    sha256 '4866356bbc3bd5f3d31bd64d528fe1ddc1c0db3a5400be05ca2c443508d996ea'
+    version '2.10'
+    sha256 'e808a72e60cdff9ff5aa1046d856bf62d6418e4915248816c4640e32e52fd8e8'
     url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniFocus-#{version}.dmg"
   end
 
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniFocus#{version.major}",
-          checkpoint: '269ee59938e4785ce50bcaefb9c84c7f5407dc03ca9703fb259620e4870fc99e'
+          checkpoint: 'de61d32697bb5a1b8b927df2adb90ebcc505979d73424f1b6631a8eccd9f3347'
   name 'OmniFocus'
   homepage 'https://www.omnigroup.com/omnifocus/'
 
