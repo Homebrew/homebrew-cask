89abba522d9d626e322a7d837542c83050f52f38
Gobinath Mallaiyan <i@gobinath.com>
2017-06-12 12:34:17 -0400

    Update qlvideo to 1.88

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -1,10 +1,10 @@
 cask 'qlvideo' do
-  version '1.87'
-  sha256 '0f21d994715fa409a858b5f04a76708975b4109dd9ac4e820dbccb7caaf61ded'
+  version '1.88'
+  sha256 '7c59d0ae79a64bbe0ffe233df474d765a7682614ee8339c19bae49e1f6856316'
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.no_dots}/QLVideo_#{version.no_dots}.pkg"
   appcast 'https://github.com/Marginal/QLVideo/releases.atom',
-          checkpoint: '2dbbdfc46712b462753a2d0a0c54c74c98a746eaf63896c0db4d1b09463d8266'
+          checkpoint: 'a9013818ec62665be6f6a1a6d398d7e931492f0f6ac06a3f0ff8d3d872e3c8b0'
   name 'QuickLook Video'
   homepage 'https://github.com/Marginal/QLVideo'
 
