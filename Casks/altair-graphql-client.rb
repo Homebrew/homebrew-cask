c2e435547f2f63dbafaf091ef689d0195cf78bb8
Samuel Imolorhe <samuelimolo4real@gmail.com>
2017-06-12 13:40:40 +0100

    Added altair graphql client - altair.app v1.3.0

---
+++ Casks/altair-graphql-client.rb
@@ -0,0 +1,10 @@
+cask 'altair-graphql-client' do
+  version '1.3.0'
+  sha256 '93fe9b190c97c572ec75efbfec735bf53bcef3242daa16ffa94d6115681438d0'
+
+  url 'https://github.com/imolorhe/altair/releases/download/v1.3.0/Altair.app.zip'
+  name 'Altair GraphQL Client'
+  homepage 'https://github.com/imolorhe/altair/'
+
+  app 'Altair.app'
+end
