1ddae7aed14b93c92a8520b24544c7765ae37458
Colin Dunn <cgd@vikingdev.net>
2017-06-12 18:44:36 -0700

    Update yourkit-java-profiler to 2017.02-b61

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask 'yourkit-java-profiler' do
-  version '2017.02-b59'
-  sha256 '6715fe863e7d484d3690f1294bfa335350191f2f13a93801617615bd79add16e'
+  version '2017.02-b61'
+  sha256 '683bc0b0ab9513410fb1a544673c926f44b2b1ca640a7174dc54cc2e020efb91'
 
   url "https://www.yourkit.com/download/yjp-#{version}-mac.zip"
   name 'YourKit Java Profiler'
