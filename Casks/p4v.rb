1cdb161aab507d0843400fe773ae94f33560f0b2
Colin Dunn <cgd@vikingdev.net>
2017-06-12 18:20:45 -0700

    Update p4v to 2017.2

--- Casks/p4v.rb
+++ Casks/p4v.rb
@@ -1,11 +1,11 @@
 cask 'p4v' do
-  version '2017.1'
-  sha256 'b015a82a7c0bd492edb972dc542b377a7633bad2148e4b3e2d800f7df0c9297a'
+  version '2017.2'
+  sha256 '1c758ce705703e1ce8f643308b44105699f5f871aa4fcb554ce0c77ed78a15f5'
 
   url "http://cdist2.perforce.com/perforce/r#{version.sub(%r{\A20(\d\d\.\d+).*}, '\1')}/bin.macosx1011x86_64/P4V.dmg"
   name 'Perforce Visual Client'
   name 'P4V'
-  homepage 'https://www.perforce.com/helix-visual-client'
+  homepage 'https://www.perforce.com/products/helix-core-apps/helix-visual-client-p4v'
 
   app 'p4v.app'
   app 'p4admin.app'
