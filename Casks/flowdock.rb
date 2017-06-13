e1230e3e6922e9f516d5f997c6f2f5083e36e7dc
Miikka Koskinen <miikka@metosin.fi>
2017-06-13 14:51:08 +0300

    Update flowdock to 1.2.9

--- Casks/flowdock.rb
+++ Casks/flowdock.rb
@@ -1,11 +1,11 @@
 cask 'flowdock' do
-  version '1.2.7'
-  sha256 '80cce5d7062ca5e24189d268bb6b0c08ce732c5039156bbe393c0bdecec5eadd'
+  version '1.2.9'
+  sha256 '803c42d393f91f2b8f08ad890f7603c9607d9adcac5acaf495e9ba66441657e7'
 
   # amazonaws.com/flowdock-resources was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/flowdock-resources/mac/#{version}/Flowdock.zip"
   appcast 'https://s3.amazonaws.com/flowdock-resources/mac/appcast.xml',
-          checkpoint: '05c0917ea74ed402ec6f41dac60a62549f062e7a2f983f410a860d324cdcf976'
+          checkpoint: 'e032b539857ff609e45db3de3e095c2d9334781bb6a29e6cf0192a63bd41a134'
   name 'Flowdock'
   homepage 'https://www.flowdock.com/'
 
