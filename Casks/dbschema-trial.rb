9af4fbaa10d9171168bcaff819291c5a71cd2ba7
Fred Lackey <fred.lackey@gmail.com>
2017-06-13 19:52:19 -0400

    Add DbSchema.app v7.5.1

---
+++ Casks/dbschema-trial.rb
@@ -0,0 +1,10 @@
+cask 'dbschema-trial' do
+  version '7.5.1'
+  sha256 'cf7450e331f80edeb56f1f4365ddcdf577a6694bd3b37e8ff4003815a65359ef'
+
+  url 'http://www.dbschema.com/download/DbSchema_macos_7_5_1.tgz'
+  name 'DbSchema'
+  homepage 'http://www.dbschema.com/'
+
+  app 'DbSchema.app'
+end
