e789fa9876b047fb865cb6ac434c12141553f406
Eric Nielsen <eric@amalgamar.com.br>
2017-06-14 17:15:20 -0500

    Update ngrok to 2.2.4,4VmDzA7iaHb

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -1,5 +1,5 @@
 cask 'ngrok' do
-  version '2.2.2,4VmDzA7iaHb'
+  version '2.2.4,4VmDzA7iaHb'
   sha256 '9c170076a1f4f08a09532ce6cf9791c4c4f946886cdbf2a8917b569a9d5457f6'
 
   # bin.equinox.io was verified as official when first introduced to the cask
