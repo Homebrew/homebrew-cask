7d20964e4adf4f5b3307ed8ee5caf84981f8a036
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-06-14 02:50:47 -0400

    Update insomnia to 5.3.3 (#35506)

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,10 +1,10 @@
 cask 'insomnia' do
-  version '5.2.0'
-  sha256 'db8bb86c9468b34e7216be6387d46a85e22ef35365a9996616b03499cec0c014'
+  version '5.3.3'
+  sha256 'a266082405fa42e42e3ad7be9a83de69dd042460bbbde5d13012799772723318'
 
   url 'https://builds.insomnia.rest/downloads/mac/latest'
   appcast 'https://insomnia.rest/changelog/index.xml',
-          checkpoint: '305e888c6e5c6b1ff1fb975bfc3d0933d73be17e1e4696827885daaf7fad7f69'
+          checkpoint: '04e8beea33c6395bf40b02fa8f1aa05632d9a3e1c0edd8de6b8bc43b4930c371'
   name 'Insomnia'
   homepage 'https://insomnia.rest/'
 
