eb4071abd2b066db430fc5e459abe06ffd9f428e
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-06-14 01:58:56 +0100

    Update brave 0.16.9 checkpoint (#35499)

--- Casks/brave.rb
+++ Casks/brave.rb
@@ -5,7 +5,7 @@ cask 'brave' do
   # github.com/brave/browser-laptop was verified as official when first introduced to the cask
   url "https://github.com/brave/browser-laptop/releases/download/v#{version}dev/Brave-#{version}.dmg"
   appcast 'https://github.com/brave/browser-laptop/releases.atom',
-          checkpoint: '02e2bb69de5bf1dc1fe59863b936a095afb67044e659898cfdf0a458ae2cb308'
+          checkpoint: 'c9c138db615f85760adda67731eea1f36d2d0c6a795d195d9e289018867f6795'
   name 'Brave'
   homepage 'https://brave.com/'
 
