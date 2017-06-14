4d21317cebc735457feb228dba5929f74a2fb53f
Colin Dunn <cgd@vikingdev.net>
2017-06-14 16:49:41 +1000

    Update daylite to 6.2 (#35458)
    * Update daylite to 6.2
    
    * Update daylite to 6.2
    
    * Update daylite to 6.2

--- Casks/daylite.rb
+++ Casks/daylite.rb
@@ -1,6 +1,6 @@
 cask 'daylite' do
-  version '6.1.3'
-  sha256 'dde46c27686d60d27d39d123116f15c7c851dcbeda4f7c652669de22d98104b6'
+  version '6.2'
+  sha256 'e4229c3cacabddb74008e774420c2e3912138bce16f95b84d63ae1178ad3d696'
 
   url "https://download.marketcircle.com/daylite/daylitedma#{version.no_dots}.dmg"
   name 'Daylite'
@@ -8,8 +8,26 @@ cask 'daylite' do
 
   pkg 'Install Daylite & Mail Assistant.pkg'
 
-  uninstall pkgutil: [
-                       'com.marketcircle.pkg.DLBase',
-                       'com.marketcircle.pkg.DMABase',
-                     ]
+  uninstall pkgutil:   [
+                         'com.marketcircle.pkg.DLBase',
+                         'com.marketcircle.pkg.DMABase',
+                       ],
+            launchctl: [
+                         'com.marketcircle.persistence.autoupdate',
+                         '*.com.marketcircle.Daylite-Helper',
+                       ],
+            signal:    [
+                         ['TERM', 'com.marketcircle.daylite'],
+                         ['TERM', 'com.marketcircle.persistence.autoupdate'],
+                         ['KILL', '*.com.marketcircle.Daylite-Helper'],
+                       ]
+
+  zap delete: [
+                '~/Library/Application Support/com.marketcircle.daylite',
+                '~/Library/Caches/com.marketcircle.daylite',
+                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.marketcircle.daylite',
+                '~/Library/Group Containers/*.com.marketcircle.daylite',
+                '~/Library/Preferences/com.marketcircle.daylite.plist',
+                '~/Library/Saved Application State/com.marketcircle.daylite.savedState',
+              ]
 end
