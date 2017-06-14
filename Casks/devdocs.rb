4e5a6dcc3006a54422f15c43ff7db74a6a52acd0
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-06-13 20:50:11 -0400

    Update devdocs to 0.4.0 (#35497)

--- Casks/devdocs.rb
+++ Casks/devdocs.rb
@@ -1,10 +1,10 @@
 cask 'devdocs' do
-  version '0.3.0'
-  sha256 '6518829267c552f5d3a46785f641fe0d0e994432eba00f84585d433b59357db2'
+  version '0.4.0'
+  sha256 'ff8f0bdf1c6ad677139b1798717c0cfe58ab41fc2857b1155b196887e05d88ea'
 
   url "https://github.com/egoist/devdocs-app/releases/download/v#{version}/DevDocs-#{version}.dmg"
   appcast 'https://github.com/egoist/devdocs-app/releases.atom',
-          checkpoint: 'ea9adaccb2e41cd783b224d8562a2dc1f112d64e0c68511dff1c6eb0855738c2'
+          checkpoint: '83ad81c3338a3ae9fc7ea32b3522d32a67f93f56d6bd7f1266e8340d13d59711'
   name 'DevDocs App'
   homepage 'https://github.com/egoist/devdocs-app'
 
