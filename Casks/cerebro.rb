81cb51efbc6b4568b82a2a64439e138eebbf380d
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-06-14 01:59:15 +0100

    Update cerebro 0.3.0 checkpoint (#35501)

--- Casks/cerebro.rb
+++ Casks/cerebro.rb
@@ -1,11 +1,11 @@
 cask 'cerebro' do
-  version '0.2.8'
-  sha256 'c54a6e6aba93d9573551e3c09cd24589ec8e974ec1d9e2bca7a28eaeacd20408'
+  version '0.3.0'
+  sha256 '191f79987bbffbe1686ae5044ee1fe3964688139981de4ff26b4039525451db5'
 
   # github.com/KELiON/cerebro was verified as official when first introduced to the cask
   url "https://github.com/KELiON/cerebro/releases/download/v#{version}/cerebro-#{version}.dmg"
   appcast 'https://github.com/KELiON/cerebro/releases.atom',
-          checkpoint: '64da05a53fdbb1da2ee4530039fd90e0712da764c2c9d4abfee1f65971f728aa'
+          checkpoint: '4017af9f5c5b47224aff0785eb825ac4bb3be799367ef157cef67299a7e18b2f'
   name 'Cerebro'
   homepage 'https://cerebroapp.com/'
 
