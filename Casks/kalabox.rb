465a26e1c4a8248a8c4a599d4d8f7dafa35df49d
Vítor Galvão <info@vitorgalvao.com>
2017-06-15 10:24:51 +0100

    Update kalabox.rb

--- Casks/kalabox.rb
+++ Casks/kalabox.rb
@@ -2,7 +2,7 @@ cask 'kalabox' do
   version '2.1.3'
   sha256 'fd02aa0fb6459270f050d2f7708aca2edb4ebb49f88fa9083f3db2fb44963c54'
 
-  # github.com/kalabox/kalabox/releases/download/ was verified as official when first introduced to the cask
+  # github.com/kalabox/kalabox was verified as official when first introduced to the cask
   url "https://github.com/kalabox/kalabox/releases/download/v#{version}/kalabox-v#{version}.dmg"
   name 'Kalabox'
   homepage 'http://kalabox.io/'
