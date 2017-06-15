66ec1a06975e5f932d2b9a32a5a6d3233ad598dd
giannitm <giannitm@users.noreply.github.com>
2017-06-15 15:20:40 +0100

    Update diumoo to 1.6.0beta (#35576)

--- Casks/diumoo.rb
+++ Casks/diumoo.rb
@@ -1,11 +1,11 @@
 cask 'diumoo' do
-  version '1.5.0'
-  sha256 'f6a6628dab6c64752f6827e453368c18dcd06962ee0823d51d51c439cf7e04b6'
+  version '1.6.0beta'
+  sha256 '6747c250af4a3ad6ea022725b0183e968f4be3f0333130de55f5795095c3759a'
 
   # github.com/shanzi/diumoo was verified as official when first introduced to the cask
-  url "https://github.com/shanzi/diumoo/releases/download/v#{version}/diumoo.zip"
+  url "https://github.com/shanzi/diumoo/releases/download/#{version}/diumoo.zip"
   appcast 'https://github.com/shanzi/diumoo/releases.atom',
-          checkpoint: 'a821ab089dadfb4dbaa63b68c5c7725588f40a1e144488519f80fa67b05e5eb9'
+          checkpoint: '5e4e7dc23c4461c9f091a583139d7abc5fd7a78a826c4bab526eb7d21374684d'
   name 'diumoo'
   homepage 'http://diumoo.net/'
 
