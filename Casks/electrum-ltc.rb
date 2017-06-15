25a45bd8fde2c4110e64d49ab0583c7c76f6c93f
giannitm <giannitm@users.noreply.github.com>
2017-06-15 16:14:19 +0100

    Update electrum-ltc to 2.6.4.2 (#35587)

--- Casks/electrum-ltc.rb
+++ Casks/electrum-ltc.rb
@@ -4,7 +4,7 @@ cask 'electrum-ltc' do
 
   url "https://electrum-ltc.org/download/Electrum-LTC-#{version}.dmg"
   appcast 'https://electrum-ltc.org/',
-          checkpoint: 'c83f747474638c99418c76cc80263d06de665096ec1c99b9b2e959b83cbece1f'
+          checkpoint: 'b72a5a867e5ecaf73e47bdfdda36160b13659d10719894c2fbf74dda79fdaab0'
   name 'Electrum-LTC'
   homepage 'https://electrum-ltc.org/'
   gpg "#{url}.asc", key_id: '9914864dfc33499c6ca2beea22453004695506fd'
