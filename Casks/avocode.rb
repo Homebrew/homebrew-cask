c4081b860b539c4c0181c3319ef8148dca383976
zalishchuk <zalishchuk.me@ya.ru>
2017-06-16 00:40:00 +0300

    Update avocode to 2.14.4

--- Casks/avocode.rb
+++ Casks/avocode.rb
@@ -1,6 +1,6 @@
 cask 'avocode' do
-  version '2.14.2'
-  sha256 'fe5fe595443eb540bd3c13635366fee9c139fb821b11e6e08ba588a2dbfa443d'
+  version '2.14.4'
+  sha256 '13a9b25339048cb3228d399239ec4e1f30a816be0278a62edcdb28d7f75f6824'
 
   url "http://media.avocode.com/download/avocode-app/#{version}/Avocode-#{version}-mac.zip"
   name 'Avocode'
