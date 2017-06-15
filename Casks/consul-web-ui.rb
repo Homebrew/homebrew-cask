39d1f3037103d1ba24d2ba59d94b7c4adbbb19d2
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-06-15 15:34:50 +0100

    Update consul-web-ui to 0.8.4 (#35568)

--- Casks/consul-web-ui.rb
+++ Casks/consul-web-ui.rb
@@ -1,11 +1,11 @@
 cask 'consul-web-ui' do
-  version '0.8.3'
-  sha256 '9212c44c3ee4acaeca88d794225a2858a558be531a3cd44c741990c4493d6f12'
+  version '0.8.4'
+  sha256 '7a49924a872205002b2bf72af8c82d5560d4a7f4a58b2f65ee284dd254ebd063'
 
   # hashicorp.com was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_web_ui.zip"
   appcast 'https://github.com/hashicorp/consul/releases.atom',
-          checkpoint: '19d042490f2195267f70b959ab2ebb811f007c1ae67d282cda105c4dc2371fa7'
+          checkpoint: '22fc8401281f172bab0a46da05282477b7ecd237bdd9d34eabf94ca2aad84645'
   name 'Consul Web UI'
   homepage 'https://www.consul.io/intro/getting-started/ui.html'
 
