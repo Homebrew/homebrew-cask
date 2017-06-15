777bbd07adbecf2edc1f9b24a04c5491fb579cf8
giannitm <giannitm@users.noreply.github.com>
2017-06-15 15:48:33 +0100

    Update earthcoin to 1.3.3

--- Casks/earthcoin.rb
+++ Casks/earthcoin.rb
@@ -4,7 +4,7 @@ cask 'earthcoin' do
 
   url "https://github.com/earthcoinproject/earthcoin/releases/download/v#{version}/EarthCoin-Qt-#{version}.dmg"
   appcast 'https://github.com/earthcoinproject/earthcoin/releases.atom',
-          checkpoint: '406ba1e1d3df7414a784dca7e33c088126de1f21be61e1cf99e179d4a277bfba'
+          checkpoint: 'c6a519dbfd324fdb63513e876e5e2ee54f07eb29a19607de911ca77af7f228e7'
   name 'Earthcoin'
   homepage 'https://github.com/earthcoinproject/earthcoin'
 
