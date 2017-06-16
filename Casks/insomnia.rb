984e509ec44ac74cfc7648e6b1a9e371c39543b1
Gregory Schier <gschier1990@gmail.com>
2017-06-15 19:17:11 -0700

    Update Insomnia to 5.3.6 and fix URL (#35600)

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,12 +1,14 @@
 cask 'insomnia' do
-  version '5.3.3'
-  sha256 'a266082405fa42e42e3ad7be9a83de69dd042460bbbde5d13012799772723318'
+  version '5.3.6'
+  sha256 '072fd1f7b234cd4e69c8e9b622c3e436e0f860f64696bde3d43ab95dacfc2028'
 
-  url 'https://builds.insomnia.rest/downloads/mac/latest'
+  url "https://builds.insomnia.rest/downloads/mac/#{version}"
   appcast 'https://insomnia.rest/changelog/index.xml',
-          checkpoint: '04e8beea33c6395bf40b02fa8f1aa05632d9a3e1c0edd8de6b8bc43b4930c371'
+          checkpoint: 'c4d09787cd2cf8113c7bce7c0c794625b49cc67835862644e734f968c7b68d52'
   name 'Insomnia'
   homepage 'https://insomnia.rest/'
+
+  auto_updates true
 
   app 'Insomnia.app'
 
