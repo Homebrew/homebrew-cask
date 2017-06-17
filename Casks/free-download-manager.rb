f23217f0415ef7ebf830e91bfb41c97672bebe64
Yurii Kolesnykov <yurikoles@gmail.com>
2017-06-18 00:47:05 +0300

    Update free-download-manager to 5.1.29 and include zap (#35647)

--- Casks/free-download-manager.rb
+++ Casks/free-download-manager.rb
@@ -1,10 +1,18 @@
 cask 'free-download-manager' do
-  version '5.1.28'
-  sha256 'c92674e88a741cec39a77d1ae03ce6cee017e065b13dc920b9d73549a2462305'
+  version '5.1.29'
+  sha256 '272ba51600440ba5c9c57d2eb8331bb5219821625514bca8c497b84e595c150e'
 
   url "http://files2.freedownloadmanager.org/#{version.major}/#{version.major_minor}-latest/fdm.dmg"
   name 'Free Download Manager'
   homepage 'http://www.freedownloadmanager.org/landing5.htm'
 
   app 'Free Download Manager.app'
+
+  zap delete: [
+                '~/Library/Application Support/Free Download Manager',
+                "~/Library/Caches/org.freedownloadmanager.fdm#{version.major}",
+                "~/Library/Preferences/org.freedownloadmanager.fdm#{version.major}.plist",
+                "~/Library/Saved Application State/org.freedownloadmanager.fdm#{version.major}.savedState",
+                "~/Library/LaunchAgents/org.freedownloadmanager.fdm#{version.major}.helper.plist",
+              ]
 end
