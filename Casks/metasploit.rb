321adb88fa130fd69a2421bd9fbc7914971b55e5
commitay <commitay1@gmail.com>
2017-06-18 05:28:40 +1000

    Update metasploit to 4.14.28+20170617092236 - binary / uninstall (#35639)
    * Update metasploit to 4.14.28+20170617092236
    
    * binary / uninstall_preflight

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,10 +1,10 @@
 cask 'metasploit' do
-  version '4.14.27+20170616092238'
-  sha256 '863433f668b109459916bde9c0a26cf2a397f07ec19c75806cdcce30b698af8d'
+  version '4.14.28+20170617092236'
+  sha256 '8a76196f8b57373a7a6a11ef37b5e98fd28dd1ea5d327cb252e3cb7b4fa4a316'
 
   url "https://osx.metasploit.com/metasploit-framework-#{version}-1rapid7-1.pkg"
   appcast 'https://osx.metasploit.com/LATEST',
-          checkpoint: '9157836d08ef48a551281bdb568c87dc086c6cbb8a8f3767bfaa293ad9bc1df1'
+          checkpoint: 'f218cb29c407e42fb5acc3e0010ad217ad7969ee87fdee3bac056a1376f9e5ba'
   name 'Metasploit Framework'
   homepage 'https://www.metasploit.com/'
   gpg "#{url}.asc", key_id: '2007B954'
@@ -20,13 +20,17 @@ cask 'metasploit' do
   binary '/opt/metasploit-framework/bin/msfelfscan'
   binary '/opt/metasploit-framework/bin/msfmachscan'
   binary '/opt/metasploit-framework/bin/msfpescan'
-  binary '/opt/metasploit-framework/bin/msfremove'
   binary '/opt/metasploit-framework/bin/msfrop'
   binary '/opt/metasploit-framework/bin/msfrpc'
   binary '/opt/metasploit-framework/bin/msfrpcd'
   binary '/opt/metasploit-framework/bin/msfupdate'
   binary '/opt/metasploit-framework/bin/msfvenom'
 
-  uninstall pkgutil: '.*metasploit.*',
-            delete:  '/opt/metasploit-framework'
+  uninstall_preflight do
+    system_command '/bin/rm',
+                   args: ['-rf', '/opt/metasploit-framework'],
+                   sudo: true
+  end
+
+  uninstall pkgutil: '.*metasploit.*'
 end
