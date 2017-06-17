ef55306830e26b10dd6fefb29d77ec0e597e1b9f
Jörg Eichhorn <jeichhorn@me.com>
2017-06-17 08:35:32 +0200

    Update aqua-data-studio to 18.0.14 (#35616)
    * Update aqua-data-studio to 18.0.14
    
    * Update aqua-data-studio.rb

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -1,10 +1,10 @@
 cask 'aqua-data-studio' do
-  version '18.0.13'
-  sha256 '879181aeb6237118ed040a9095c1558ca182fb958a671783da9ae4f67e845084'
+  version '18.0.14'
+  sha256 '6e5f72dba68a120d75b59652aadfe2da283f0922d15e9d69df5903b987753533'
 
   url "http://www.aquafold.com/download/v#{version.major}.0.0/osx/ads-osx-#{version}.tar.gz"
   appcast 'http://www.aquafold.com/download/',
-          checkpoint: 'ef91e22d2d66c511d7063eeaedf35b5cd7f0b3429b2c3724801b36e004923ef0'
+          checkpoint: 'c5dde3e0fbd7d9d67b3c1e13612dfa5c8c7a6705652f88599472b4a97b1a2620'
   name 'Aquafold Aqua Data Studio'
   homepage 'http://www.aquafold.com/aquadatastudio.html'
 
