ba065043499def6085fcc1750c22b64f02a3d236
commitay <commitay1@gmail.com>
2017-06-18 07:46:26 +1000

    Update brisk to 0.2.1 (#35646)

--- Casks/brisk.rb
+++ Casks/brisk.rb
@@ -1,10 +1,10 @@
 cask 'brisk' do
-  version '0.2.0'
-  sha256 'fa466a06e20374c091ed299367b6e0b58525fcad74a36419ca06f8685d49da76'
+  version '0.2.1'
+  sha256 'a1c5022d4c8234586cc8d11df181f9584f4d7ad28c1d872e8e25b90a7a6df9f0'
 
   url "https://github.com/br1sk/brisk/releases/download/#{version}/Brisk.app.tar.gz"
   appcast 'https://github.com/br1sk/brisk/releases.atom',
-          checkpoint: 'fbc52e647fb83bf6b4856de0d0c9a60c4cfc12c5ebce6c782c1625a1b03cb72f'
+          checkpoint: 'f5b88f40bcbb907c8de2ef984a86158e869e4a69eb09b418d9a0276e669907f7'
   name 'Brisk'
   homepage 'https://github.com/br1sk/brisk'
 
