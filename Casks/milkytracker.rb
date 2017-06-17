464e4f8fb12e81c7946f8d40effbe42987d9d32d
Miccal Matthews <miccal.matthews@gmail.com>
2017-06-17 14:15:38 +0800

    Update milkytracker.rb

--- Casks/milkytracker.rb
+++ Casks/milkytracker.rb
@@ -1,9 +1,9 @@
 cask 'milkytracker' do
-  version '1.00.00'
+  version '1.0.0,1.00.00'
   sha256 '8b8c4ba5e5459b7c83959990d302dce780c01faea7786b47ca0e2b3100b5b364'
 
-  # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/milkytracker/MilkyTracker/releases/download/v1.0.0/milkytracker-1.00.00-osx.dmg'
+  # github.com/milkytracker/MilkyTracker was verified as official when first introduced to the cask
+  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version.before_comma}/milkytracker-#{version.after_comma}-osx.dmg"
   name 'MilkyTracker'
   homepage 'http://milkytracker.titandemo.org/'
 
