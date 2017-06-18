fa0428d980f653e8267730a8e57e82dda76e35c0
nuomi1 <nuomi1@users.noreply.github.com>
2017-06-18 22:21:04 +0800

    Update qqlive to 1.0.4.30128 (#35658)
    Update qqlive to 1.0.4.30128

--- Casks/qqlive.rb
+++ Casks/qqlive.rb
@@ -1,11 +1,24 @@
 cask 'qqlive' do
-  version '1.0.2.30100'
-  sha256 '2499f0a3f8e4d12f847ae45891d7a701001c57dba7ecc79736ef2431303d0801'
+  version '1.0.4.30128'
+  sha256 '8742b11967be5c2177f2bc2de72f628b0df092d81a0815ba5c2f2a899ddde0b0'
 
-  url "http://dldir1.qq.com/qqtv/mac/TencentVideo_V#{version}.dmg"
+  url "https://dldir1.qq.com/qqtv/mac/TencentVideo_V#{version}.dmg"
   name 'QQLive'
   name '腾讯视频'
-  homepage 'https://v.qq.com/'
+  homepage 'https://v.qq.com/download.html#mac'
+
+  auto_updates true
+  depends_on macos: '>= :yosemite'
 
   app 'QQLive.app'
+
+  zap delete: [
+                '~/Library/Caches/com.tencent.tenvideo',
+                '~/Library/Containers/com.tencent.tenvideo',
+                '~/Library/Cookies/com.tencent.tenvideo.binarycookies',
+                '~/Library/Logs/QQLive',
+                '~/Library/Preferences/com.tencent.tenvideo.plist',
+                '~/Library/Saved Application State/com.tencent.tenvideo.savedState',
+                '~/Library/WebKit/com.tencent.tenvideo',
+              ]
 end
