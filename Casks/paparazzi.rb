f1487314041b1f0b254723f085fd68ec967f0f8c
Grettir Asmundarson <grettir@tinypineapple.com>
2017-06-19 10:02:57 -0600

    Update paparazzi to 1.0b6

--- Casks/paparazzi.rb
+++ Casks/paparazzi.rb
@@ -1,10 +1,10 @@
 cask 'paparazzi' do
-  version '1.0b5'
-  sha256 'f65571f660e100ac9531fab9edef6413e1f23a5a952e6f2553380cecc3fc0b7b'
+  version '1.0b6'
+  sha256 'f5362e23ba840247a03dea426e3d3f4fdf9b3f832cf3cf4e27498564da7af0c9'
 
   url "https://derailer.org/paparazzi/Paparazzi!%20#{version}.dmg"
   appcast 'https://derailer.org/paparazzi/appcast/',
-          checkpoint: '1673362065a3a18036832bdc850b40b24b44bfb1188739b134f9dcabf48c6fa1'
+          checkpoint: 'ca001383f08caeaa53ecf37407f5ec589aecd2dc1a2cfd705bffbaf9416a3628'
   name 'Paparazzi!'
   homepage 'https://derailer.org/paparazzi/'
 
