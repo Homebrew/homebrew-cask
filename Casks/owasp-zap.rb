2ac5dbcb37b270b499f7e0d7571f79797fd43b56
Gobinath Mallaiyan <i@gobinath.com>
2017-06-19 09:03:25 -0400

    Update owasp-zap to 2.6.0

--- Casks/owasp-zap.rb
+++ Casks/owasp-zap.rb
@@ -5,7 +5,7 @@ cask 'owasp-zap' do
   # github.com/zaproxy/zaproxy was verified as official when first introduced to the cask
   url "https://github.com/zaproxy/zaproxy/releases/download/#{version}/ZAP_#{version.dots_to_underscores}_macos.dmg"
   appcast 'https://github.com/zaproxy/zaproxy/releases.atom',
-          checkpoint: '5cd63080ce020a8422379ad9ac25c02405cdb6e996a580020b53e6b163285324'
+          checkpoint: '23a240ee8d41b3316270c51928f36ae58e530dc80cbf2440da799d569130dc20'
   name 'OWASP Zed Attack Proxy'
   name 'ZAP'
   homepage 'https://www.owasp.org/index.php/OWASP_Zed_Attack_Proxy_Project'
