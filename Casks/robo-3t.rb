e6f85d4af84812867f277fec4595eab65d64a789
Vitaliy Meshchaninov <glukki.spb.ru@gmail.com>
2017-06-20 02:31:42 +0300

    Replace Robomongo v1.0.0 with Robo 3T v1.1.1 (#35694)
    * Replace Robomongo v1.0.0 with Robo 3T v1.1.1
    * Update and rename robo3t.rb to robo-3t.rb

---
+++ Casks/robo-3t.rb
@@ -0,0 +1,17 @@
+cask 'robo-3t' do
+  version '1.1.1,c93c6b0'
+  sha256 'b536ca099c200d80a4173c4aa2726fa3eaeaef8ddd17968e2326bd8d2ec9f3b4'
+
+  url "https://download.robomongo.org/#{version.before_comma}/osx/robo3t-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg"
+  name 'Robo 3T (formerly Robomongo)'
+  homepage 'https://robomongo.org/'
+
+  app 'Robo 3T.app'
+
+  uninstall quit: 'Robo 3T'
+
+  zap delete: [
+                '~/.3T/robo-3t/',
+                '~/Library/Saved Application State/Robo 3T.savedState',
+              ]
+end
