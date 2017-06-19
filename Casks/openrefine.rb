67f3ba3a2d0f3ab2b906281878d8b47db884e229
Miccal Matthews <miccal.matthews@gmail.com>
2017-06-19 08:51:09 +0800

    Update openrefine.rb

--- Casks/openrefine.rb
+++ Casks/openrefine.rb
@@ -4,6 +4,8 @@ cask 'openrefine' do
 
   # github.com/OpenRefine/OpenRefine was verified as official when first introduced to the cask
   url "https://github.com/OpenRefine/OpenRefine/releases/download/#{version}/openrefine-mac-#{version}.dmg"
+  appcast 'https://github.com/OpenRefine/OpenRefine/releases.atom',
+          checkpoint: '770da085d3ec3b93f915b53d1a6132ad15b1ff8d0d0388915fcd5aa83f7a119b'
   name 'OpenRefine'
   homepage 'http://openrefine.org/'
 
