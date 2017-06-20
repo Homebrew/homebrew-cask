af9c72a415f5330b0888f1f49c74eaaa21b1b2a1
Jussi Tuominen <jussi@COLDwithin.local>
2017-06-20 20:09:36 +0300

    Update smart-scroll to 4.3.4

--- Casks/smart-scroll.rb
+++ Casks/smart-scroll.rb
@@ -1,6 +1,6 @@
 cask 'smart-scroll' do
-  version '4.3.3'
-  sha256 'aa0c7c1a19b50e03c3cb8e78c4f3529351fe9108d98d710afb6f02a51cce0822'
+  version '4.3.4'
+  sha256 '8dd4843ef7bdf39525dc8f0ea00b28fd6aebdcacf831802151a233cca8da58df'
 
   url 'https://www.marcmoini.com/SmartScroll.dmg'
   name 'Smart Scroll'
