51e3a471f3b1a08ef198b46a8b063e9f0c221f64
commitay <commitay1@gmail.com>
2017-06-21 09:17:06 +1000

    Update imagej.rb

--- Casks/imagej.rb
+++ Casks/imagej.rb
@@ -2,7 +2,7 @@ cask 'imagej' do
   version '1.50'
   sha256 '681bb1af1cc1797e2fe5eddd5828a38128d3fc5cf9ea05a268650466e2eb6b93'
 
-  # wsr.imagej.net was verified as official when first introduced to the cask
+  # wsr.imagej.net/distros/osx was verified as official when first introduced to the cask
   url "https://wsr.imagej.net/distros/osx/ImageJ#{version.no_dots}.zip"
   name 'ImageJ'
   homepage 'https://imagej.nih.gov/ij/index.html'
