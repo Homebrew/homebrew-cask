86c5272a44707a9f9e1388a23296f7ec9404806b
commitay <commitay1@gmail.com>
2017-06-21 15:18:47 +1000

    Update pages-data-merge.rb

--- Casks/pages-data-merge.rb
+++ Casks/pages-data-merge.rb
@@ -1,6 +1,6 @@
 cask 'pages-data-merge' do
-  version '1.2 (1)'
-  sha256 'd651f7de30033d5fb7ebf785da982ebd4c190fd8eeb7bf28cb5d6583fc4b6bb0'
+  version :latest
+  sha256 :no_check
 
   url 'http://iworkautomation.com/pages/PagesDataMergeApp.zip'
   name 'Pages Data Merge'
