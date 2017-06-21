7d5761fafc79153f7495cbfa5907c967b6ae2a44
Yassine El Badaoui <yassine@jp.ibm.com>
2017-06-21 14:00:43 +0900

    Initial Commit for Pages Data Merge.app version 1.2 (1)

---
+++ Casks/pages-data-merge.rb
@@ -0,0 +1,10 @@
+cask 'pages-data-merge' do
+  version '1.2 (1)'
+  sha256 'd651f7de30033d5fb7ebf785da982ebd4c190fd8eeb7bf28cb5d6583fc4b6bb0'
+
+  url 'http://iworkautomation.com/pages/PagesDataMergeApp.zip'
+  name 'Pages Data Merge'
+  homepage 'http://iworkautomation.com/pages/script-tags-data-merge.html'
+
+  app 'Pages Data Merge.app'
+end
