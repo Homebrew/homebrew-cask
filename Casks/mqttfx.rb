17b7d3852a76c747d7b247988785705b7eaebd82
Jens Strobel <strobel@pixeldreamz.com>
2017-06-21 22:44:53 +0200

    Update mqttfx to 1.5.0 (#35746)

--- Casks/mqttfx.rb
+++ Casks/mqttfx.rb
@@ -1,6 +1,6 @@
 cask 'mqttfx' do
-  version '1.4.2'
-  sha256 'c1805128ee4cea11edd0b26535aaa15529d3246e3d804cc1642fe1e4e22406e6'
+  version '1.5.0'
+  sha256 '894708779e1db41a7cd8f26d9e3d0776558921be1c10dba6a32f46df5b41e2f9'
 
   # jensd.de/apps/mqttfx was verified as official when first introduced to the cask
   url "http://www.jensd.de/apps/mqttfx/#{version}/mqttfx-#{version}-macos.dmg"
