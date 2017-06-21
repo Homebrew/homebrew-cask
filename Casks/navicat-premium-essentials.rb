b813d310ea4b5bb79cbfbf93d1b86aafe8c45448
commitay <commitay@users.noreply.github.com>
2017-06-22 07:25:59 +1000

    Update navicat-premium-essentials to 12.0.5

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium-essentials' do
-  version '12.0.2'
-  sha256 'da28c951697a15356a4034845313e4fa1b19a609b559cebc3091f7281f87b3b3'
+  version '12.0.5'
+  sha256 '52c2d87a6ede4e4c1689a88b44e833d50aa3377827a7bfdfbb113ade7f9c44af'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
   name 'Navicat Premium Essentials'
