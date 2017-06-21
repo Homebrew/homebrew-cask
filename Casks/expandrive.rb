b560593506797158291a4c34f1e1613d8231a32a
commitay <commitay@users.noreply.github.com>
2017-06-21 22:51:30 +1000

    Update expandrive to 5.5.4

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,10 +1,10 @@
 cask 'expandrive' do
-  version '5.5.0'
-  sha256 '537a2dd653d7e5442e0cd0eb2a07bae24e18bdf84a22df546423595b49a755ba'
+  version '5.5.4'
+  sha256 'cd315e227a1fc4719a77760de6b7a27f6b5dd9036a58319352e72fbbaa60a623'
 
   url "https://updates.expandrive.com/apps/expandrive/v/#{version.dots_to_hyphens}/download.dmg"
   appcast 'https://updates.expandrive.com/appcast/expandrive.xml?version=3',
-          checkpoint: 'ef7b60656f23aecb19ed13fa535bf8f85c8642312a3500ab0d2cdee19c974e6f'
+          checkpoint: '5ef57ba7165a2afc67ab96fd08e68aed3a2a90a273fa717b887c0c34040b2ca7'
   name 'ExpanDrive'
   homepage 'https://www.expandrive.com/apps/expandrive/'
 
