59a59b99daa5bd21485ead2c627cda377a1140b9
Christoph Blecker <admin@toph.ca>
2017-06-21 13:45:13 -0700

    Update minikube to 0.20.0

--- Casks/minikube.rb
+++ Casks/minikube.rb
@@ -1,11 +1,11 @@
 cask 'minikube' do
-  version '0.19.1'
-  sha256 '1689ff1686c07da8b72a4dd2d472ec4d6cf3347eb33804024bb368f455fb4214'
+  version '0.20.0'
+  sha256 'c09b3ff9045a9b4c2bc9dab85b981f7846b77417bcfeb3248fc50cd985a5c5fe'
 
   # storage.googleapis.com/minikube was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/minikube/releases/v#{version}/minikube-darwin-amd64"
   appcast 'https://github.com/kubernetes/minikube/releases.atom',
-          checkpoint: 'b72d7dd99f9e92b115c459ce532db88b70490a5eae48b189947c4881281ac5e0'
+          checkpoint: 'c6be88cbd2d7e87f50c33d6be3ecd34a32eb07415659d27a8e745349357fedb4'
   name 'Minikube'
   homepage 'https://github.com/kubernetes/minikube'
 
