79cebec2bfcbe8f2feb590c50e22b009b753902e
Mees Boeijen <mees@amees.me>
2017-06-21 10:57:58 +0200

    Update keypad-layout to 1.4 (#35735)

--- Casks/keypad-layout.rb
+++ Casks/keypad-layout.rb
@@ -1,10 +1,10 @@
 cask 'keypad-layout' do
-  version '1.3'
-  sha256 '0999d545298233fb2c175ddce8777b13bcb82eef626203e3130481566509951e'
+  version '1.4'
+  sha256 '588be349ee8ad5806f40467f512381b8ea295aac5d9548c895a220b48168de3d'
 
   url "https://github.com/janten/keypad-layout/releases/download/#{version}/Keypad-Layout.zip"
   appcast 'https://github.com/janten/keypad-layout/releases.atom',
-          checkpoint: 'ff85da6ee18dde5cd668d99db3e80765ec2fb158202325bbe4f62f5e505a9004'
+          checkpoint: '993ed24bc82fea12c8f7fa3f36de0e6c3276b663d8887c41342f6d9c05d61a0e'
   name 'Keypad Layout'
   homepage 'https://github.com/janten/keypad-layout'
 
