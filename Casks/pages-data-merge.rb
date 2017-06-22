d64e9e0bead1be375f8045031400341f9d3a3791
Yassine EL BADAOUI <byassine52@yahoo.fr>
2017-06-22 10:56:54 +0900

    Initial Commit for Pages Data Merge.app version 1.2 (1) (#35729)
    * Initial Commit for Pages Data Merge.app version 1.2 (1)
    
    * Update pages-data-merge.rb

---
+++ Casks/pages-data-merge.rb
@@ -0,0 +1,10 @@
+cask 'pages-data-merge' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://iworkautomation.com/pages/PagesDataMergeApp.zip'
+  name 'Pages Data Merge'
+  homepage 'http://iworkautomation.com/pages/script-tags-data-merge.html'
+
+  app 'Pages Data Merge.app'
+end
