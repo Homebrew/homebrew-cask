e39fb88c0189d66783de5c1e0374a91e73cf907b
Jannes Jeising <jannes@jeising.net>
2017-06-22 02:25:49 +0200

    Update waves-central to 1.3.2.4

--- Casks/waves-central.rb
+++ Casks/waves-central.rb
@@ -1,6 +1,6 @@
 cask 'waves-central' do
-  version '1.3.1.4'
-  sha256 '7df74304ee460539fceac42fd02c6c83b8d7e06e1b3215946a938c8acddb2c41'
+  version '1.3.2.4'
+  sha256 'a51c3cdc4c7c2d72920717b90349a139768cb2a09a8fd57a686cae9fb615482f'
 
   url "http://installers.waves.com/WavesCentral/V#{version}/Install_Waves_Central.dmg"
   name 'Waves Central'
