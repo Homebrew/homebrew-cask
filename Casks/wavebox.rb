e0a0597a9b485eb9855c4bb3604cfe11c839c6be
Gobinath Mallaiyan <i@gobinath.com>
2017-06-22 10:05:33 -0400

    Update wavebox to 3.1.10

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,11 +1,11 @@
 cask 'wavebox' do
-  version '3.1.8'
-  sha256 '3fc09c5e63b63f0fd883cb0ba292e664b8c30c14a038e84157bddc85001c8fa2'
+  version '3.1.10'
+  sha256 'c0f62b3ac82eab2f30ad58b7d54f37f26643caa3f17ef56bcbbb8f615b7c994e'
 
   # github.com/wavebox/waveboxapp was verified as official when first introduced to the cask
   url "https://github.com/wavebox/waveboxapp/releases/download/v#{version}/Wavebox_#{version.dots_to_underscores}_osx.dmg"
   appcast 'https://github.com/wavebox/waveboxapp/releases.atom',
-          checkpoint: 'a7870102187e882bc2ddf9ecbb0443cc58b12ded784d4c6f08f56bc20f35db8d'
+          checkpoint: '0d5ad69e13293c53fa59ff4cd1059f84510729cbe3a72fc88aa48e0fe3417ea2'
   name 'Wavebox'
   homepage 'https://wavebox.io/'
 
