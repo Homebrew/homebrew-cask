307a28d88a9ac61b8d54f52e85ce4b9d93ed3d68
Gobinath Mallaiyan <i@gobinath.com>
2017-06-22 09:08:08 -0400

    Update electorrent to 2.1.5 (#35785)

--- Casks/electorrent.rb
+++ Casks/electorrent.rb
@@ -1,10 +1,10 @@
 cask 'electorrent' do
-  version '2.1.4'
-  sha256 'a33c7ee0652dec678659e4712745024c8016a827c6d22e53745335165074be2b'
+  version '2.1.5'
+  sha256 '2e54d9522624200e7d15a00662f9d440383b4d3d018378464689b7a37865cd04'
 
   url "https://github.com/Tympanix/Electorrent/releases/download/v#{version}/electorrent-#{version}.dmg"
   appcast 'https://github.com/Tympanix/Electorrent/releases.atom',
-          checkpoint: 'f365bad3325db723c51c5b3ff1d3879d6ca0a83acb866e75a95630ee320e0783'
+          checkpoint: '97a48528bbbbf226deccbafb52d9dab3ec370ee0a71e5678c827c8a49cb4eb67'
   name 'Electorrent'
   homepage 'https://github.com/Tympanix/Electorrent'
 
