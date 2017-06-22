096e53f20f9352ae51114a5e0bc9466d78e821bd
Kun Zhou <kun-zhou@users.noreply.github.com>
2017-06-22 13:10:53 +0800

    Update neteasemusic to 1.5.6_566

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -1,6 +1,6 @@
 cask 'neteasemusic' do
-  version '1.5.5_552'
-  sha256 '0d6e225356c95d0a5129fcc5515110f241c5395a60254a54f1bc1f3dd7466f5e'
+  version '1.5.6_566'
+  sha256 'ac7b3196b16509186e4d434c5cbc63f036c26051d69b61969d810a00dfc725cc'
 
   # s1.music.126.net was verified as official when first introduced to the cask
   url "http://s1.music.126.net/download/osx/NeteaseMusic_#{version}_web.dmg"
