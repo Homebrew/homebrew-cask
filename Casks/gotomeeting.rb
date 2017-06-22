f74289ed5c7c13eed243bb4cb1a63c982a4f9ada
Jeremy Saklad <saklad5@me.com>
2017-06-22 09:58:23 -0500

    Update gotomeeting to 8.6.0.7107,ka338000000CjdPAAS (#35790)

--- Casks/gotomeeting.rb
+++ Casks/gotomeeting.rb
@@ -1,6 +1,6 @@
 cask 'gotomeeting' do
-  version '8.5.0.6956,ka338000000CjPXAA0'
-  sha256 '5722fb63a00c320a20d071f8cb00d2fe93a1c4f73cbf8b80602021a8f8fb82de'
+  version '8.6.0.7107,ka338000000CjdPAAS'
+  sha256 'a731facb69b4c5b5f5bb59365e0765d24849ed9ee9adc114825083f41b8bee4d'
 
   # support.citrixonline.com was verified as official when first introduced to the cask
   url "https://support.citrixonline.com/servlet/fileField?entityId=#{version.after_comma}&field=Content__Body__s"
