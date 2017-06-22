5e9c272df94749316d102306e57816bed89891a1
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-06-22 09:49:04 -0400

    Update jprofiler to 10.0.2 (#35780)

--- Casks/jprofiler.rb
+++ Casks/jprofiler.rb
@@ -1,10 +1,10 @@
 cask 'jprofiler' do
-  version '10.0.1'
-  sha256 'b908d2a31b7b49358bef6f093cfe6c805f5deacaaa64822747892a1c9b80a033'
+  version '10.0.2'
+  sha256 'b4518d8728c0b17ca1b7da2a56a487b058bafd74985918e55c0f024a38e5a3c1'
 
   url "https://download-keycdn.ej-technologies.com/jprofiler/jprofiler_macos_#{version.dots_to_underscores}.dmg"
   appcast 'http://feeds.ej-technologies.com/jprofiler',
-          checkpoint: 'ae69c3b96d219020d98efb9453feb09e58f44682ccc870e475589d7cc4e77f29'
+          checkpoint: '9c52a48e88f3189ff64ec3fae53efd98bd2feec1c70ae0afdffceedd76bc480c'
   name 'JProfiler'
   homepage 'https://www.ej-technologies.com/products/jprofiler/overview.html'
 
