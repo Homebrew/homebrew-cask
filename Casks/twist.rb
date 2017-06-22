321c312efdd1d3d1c138bfaed194b0d3164bda29
Andreas Zeidler <andi@pyfidelity.com>
2017-06-22 08:23:11 +0200

    Add Twist.app v1.0.10

---
+++ Casks/twist.rb
@@ -0,0 +1,10 @@
+cask 'twist' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://downloads.twistapp.com/mac/Twist.dmg'
+  name 'Twist'
+  homepage 'https://twistapp.com/'
+
+  app 'Twist.app'
+end
