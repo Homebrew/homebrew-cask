758221a63fddd9c2c66e861845c2a834ad78387f
Niklas <me@sodapop.se>
2017-06-24 23:43:40 +0200

    Update airflow to 2.0.0-beta4 (#35838)

--- Casks/airflow.rb
+++ Casks/airflow.rb
@@ -1,6 +1,6 @@
 cask 'airflow' do
-  version '2.0.0-beta3'
-  sha256 '8440e6fa315f416342e68b34ea076bfcf071d9ea33f19f19c7434446cf393cfe'
+  version '2.0.0-beta4'
+  sha256 'c0e0fb3aa79e8aafba6b5371a7968115e36e828c048e43ec56ea795a20bce85d'
 
   # amazonaws.com/Airflow was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Airflow/Download/Airflow%20#{version}.dmg"
