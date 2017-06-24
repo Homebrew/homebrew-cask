d861d3af4a75405fba7a2f3c4b57f2a616473cdd
commitay <commitay@users.noreply.github.com>
2017-06-24 22:22:29 +1000

    cask test processing 3.3.5

--- Casks/processing.rb
+++ Casks/processing.rb
@@ -1,10 +1,10 @@
 cask 'processing' do
-  version '3.3.4'
-  sha256 '65aaa629b04b37976b456e8834de1c5c7f19fb92cb94b6496b0a8222f627e6b2'
+  version '3.3.5'
+  sha256 '8fae957b6ccb62254e3e4cdf04b025bee238c3c56da609ce22206b37122f3501'
 
   url "http://download.processing.org/processing-#{version}-macosx.zip"
   appcast 'https://github.com/processing/processing/releases.atom',
-          checkpoint: '60699238aa599f5c023d6f8d485a2f72f6c85e468b95183d069eb79a9361a7c9'
+          checkpoint: '72f9a6ee7ab08b4edf447de994d23c57220d2a3ef04f48d5cd25b0dd57430796'
   name 'Processing'
   homepage 'https://processing.org/'
 
