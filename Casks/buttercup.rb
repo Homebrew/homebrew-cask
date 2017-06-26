2809437d0dd59e0bb7f92ccf0a4f1937c602fe70
Victor Popkov <victor@popkov.me>
2017-06-26 08:32:22 +0300

    Update buttercup to 0.18.0

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -2,9 +2,9 @@ cask 'buttercup' do
   version '0.18.0'
   sha256 '79fecced3534194604839048625b35e585742016a6c79df115663d63c27817f4'
 
-  # github.com/buttercup/buttercup was verified as official when first introduced to the cask
-  url "https://github.com/buttercup/buttercup/releases/download/v#{version}/Buttercup-#{version}-mac.zip"
-  appcast 'https://github.com/buttercup/buttercup/releases.atom',
+  # github.com/buttercup/buttercup-desktop was verified as official when first introduced to the cask
+  url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-#{version}-mac.zip"
+  appcast 'https://github.com/buttercup/buttercup-desktop/releases.atom',
           checkpoint: '9db5a0812078c2c86d1930e592fabb5d3c137a4e024ca7c4e9d6e9c40e4a4f5d'
   name 'Buttercup'
   homepage 'https://buttercup.pw/'
