7e16e39326da6be55e08cde14a06bad962bde7a5
commitay <commitay1@gmail.com>
2017-06-26 20:04:42 +1000

    add verified comment

--- Casks/iris.rb
+++ Casks/iris.rb
@@ -2,6 +2,7 @@ cask 'iris' do
   version '0.9.0.7'
   sha256 '6074a029736dcc25f393d3bc28babdcc0973bef1fe15eda0ee95b0d12ebc7241'
 
+  # raw.github.com/danielng01/Iris-Builds/master/OSX was verified as official when first introduced to the cask
   url "https://raw.github.com/danielng01/Iris-Builds/master/OSX/Iris-#{version}-OSX.zip"
   name 'Iris'
   homepage 'https://iristech.co/iris/'
