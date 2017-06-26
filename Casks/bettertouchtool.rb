8621acb347704660667299446c9fa26978f54f22
Victor Popkov <victor@popkov.me>
2017-06-26 08:24:05 +0300

    Update bettertouchtool to 2.25 (#35873)

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -14,11 +14,11 @@ cask 'bettertouchtool' do
     sha256 '41013cfeffee286a038363651db3dd315ff3a1e0cf07774d9ce852111be50a5a'
     url "https://bettertouchtool.net/releases/btt#{version}_final_10_9.zip"
   else
-    version '2.20'
-    sha256 '4ec647454cdecc5588e46498747cacc0f9cadee77c422e36934ce79f53c59590'
+    version '2.25'
+    sha256 '8a75f660ca7894b9b4634f2dd0d246e35404617a7d2dd8d5e909bb69e4da248e'
     url "https://bettertouchtool.net/releases/btt#{version}.zip"
     appcast 'https://updates.bettertouchtool.net/appcast.xml',
-            checkpoint: 'cc5b5c51ae98754ada25d0f2f0d6c53743d7668bf51eb14edd7ed8d8959aeae3'
+            checkpoint: 'e484259ae0fc58124658a318ace74e8b133d6ff808f65339d462ea6c56a191cd'
   end
 
   name 'BetterTouchTool'
