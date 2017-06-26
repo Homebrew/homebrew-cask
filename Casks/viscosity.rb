73a54b38ce8ea902e5bbad509da74d166bce7efb
Jussi <wickedsp1d3r@users.noreply.github.com>
2017-06-26 16:55:39 +0300

    Update viscosity to 1.7.3 (#35897)

--- Casks/viscosity.rb
+++ Casks/viscosity.rb
@@ -1,10 +1,10 @@
 cask 'viscosity' do
-  version '1.7.2'
-  sha256 '153b4813af9ef36e600eb8977332e93e3d005548753334377bd335bbecb5bbb1'
+  version '1.7.3'
+  sha256 'e28e9a3e0f02198eee105d73cc8b867cef6b05a2239974349d1a13e4ece6eadb'
 
   url "https://swupdate.sparklabs.com/download/mac/release/viscosity/Viscosity%20#{version}.dmg"
   appcast 'https://swupdate.sparklabs.com/appcast/mac/release/viscosity/',
-          checkpoint: 'cf130dbbf468fb5762016540c43c013ad5780861b8ff291040f1f4d8e61f48f0'
+          checkpoint: '328ccdcdabed28c790cb1de6439b175dad597bb36ec6b2df19b52aba63f34a43'
   name 'Viscosity'
   homepage 'https://www.sparklabs.com/viscosity/'
 
