cdfa1b156806c526a8ac830d26f58153c1ed67e3
commitay <commitay1@gmail.com>
2017-06-28 08:21:07 +1000

    change to versioned

--- Casks/autofirma.rb
+++ Casks/autofirma.rb
@@ -1,13 +1,13 @@
 cask 'autofirma' do
-  version :latest
-  sha256 :no_check
+  version '1.5'
+  sha256 'c39ad866df3e655a3bff390ae98a768af1c26d51bffef2fd92a507335d1c98c4'
 
   # estaticos.redsara.es/comunes/autofirma was verified as official when first introduced to the cask
   url 'https://estaticos.redsara.es/comunes/autofirma/currentversion/AutoFirma_Mac.zip'
   name 'AutoFirma'
   homepage 'https://administracionelectronica.gob.es/ctt/clienteafirma'
 
-  pkg 'AutoFirma_1_5_signed.pkg'
+  pkg "AutoFirma_#{version.dots_to_underscores}_signed.pkg"
 
   # remove 'Autofirma ROOT' and '127.0.0.1' certificates from keychain (these were installed by pkg)
   uninstall_postflight do
