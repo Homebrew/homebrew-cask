f0062f10209b5ec3aa54e8a045467c44bbbde693
Jason Masker <masker@post.harvard.edu>
2017-06-28 14:48:03 -0400

    Add MurusLogsVisualizer.app v1.4.2

---
+++ Casks/muruslogsvisualizer.rb
@@ -0,0 +1,16 @@
+cask 'muruslogsvisualizer' do
+  version '1.4.2'
+  sha256 'c36fc392a741588b9e270d45f0a791cf869473d5842e2d799d32a25b1a3a64e9'
+
+  url "https://www.murusfirewall.com/downloads/muruslogsvisualizer-#{version}.zip"
+  name 'Murus Logs Visualizer'
+  homepage 'https://www.murusfirewall.com/'
+
+  depends_on macos: '>= :mavericks'
+
+  app 'MurusLogsVisualizer.app'
+
+  zap delete: [
+                '~/Library/Preferences/it.murus.MurusLogsVisualizer.plist',
+              ]
+end
