0c45ee1f2b1d53ee9b99ae9f892556414b9ebd8a
jottr <jottr@users.noreply.github.com>
2017-06-28 12:49:52 +0200

    Update webcatalog to 6.0.0 (#35941)

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,11 +1,11 @@
 cask 'webcatalog' do
-  version '5.2.1'
-  sha256 'adf48096578e5467677c9998ab4b5d1d2c3153b258b7abb14910a062fd421d91'
+  version '6.0.0'
+  sha256 '698bad59ab8e978bf7a7a8aaf8e8e62b84c91132db827b6572dac1ee0b3812a0'
 
   # github.com/webcatalog/desktop/releases/download/ was verified as official when first introduced to the cask
   url "https://github.com/webcatalog/desktop/releases/download/v#{version}/WebCatalog-#{version}.dmg"
   appcast 'https://github.com/webcatalog/desktop/releases.atom',
-          checkpoint: '53e7c05bc0d381483de96614d4d2ae18c9028cc9610aee6151f7317cc402cf01'
+          checkpoint: '80fe4496b798a929b6d98a8e15d75a14267dbe6d8a58ffdcbd96663d7d252a02'
   name 'WebCatalog'
   homepage 'https://getwebcatalog.com/download/mac'
 
