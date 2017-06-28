e8f977ea4c1cd97d7438df1c6744e3edd9ade354
commitay <commitay1@gmail.com>
2017-06-28 22:31:16 +1000

    update appcast

--- Casks/icons8.rb
+++ Casks/icons8.rb
@@ -4,8 +4,8 @@ cask 'icons8' do
   sha256 '3bf80f88d0ffa08a96f4747453f534f4ad058a6a76e2348bad7c85d07a25c6c1'
 
   url 'https://desktop.icons8.com/updates/mac/Icons8App_for_Mac_OS.dmg'
-  appcast 'https://maxcdn.icons8.com/download/icons8_cast.xml',
-          checkpoint: 'ac1d98b11528f0f3417ed8e84ce258c06c726874e6b35ae2ca882a60969c5f7d'
+  appcast 'https://desktop.icons8.com/updates/mac/icons8_cast.xml',
+          checkpoint: '9688fb2b390f16363062bc105b7bdbf88840a771d3d4070d000c673f74d22c13'
   name 'Icons8 App'
   homepage 'https://icons8.com/'
 
