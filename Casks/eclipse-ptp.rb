e1f1590e0e95804dc51b43f122f76eb24ea5551a
Willian Mitsuda <wmitsuda@gmail.com>
2017-06-28 18:45:23 -0300

    Update eclipse packages to 4.7 (oxygen release)

--- Casks/eclipse-ptp.rb
+++ Casks/eclipse-ptp.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-ptp' do
-  version '4.6.3,neon:3'
-  sha256 'c19e0698efb27d87533cdf92433be1c6a52335226a03bf5583e6ffe34ab62589'
+  version '4.7.0,oxygen:R'
+  sha256 'af09c7d670ae12fb2c41de996028a516ab35d3285e07fff45845191c166f8754'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-parallel-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.tar.gz&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-parallel-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Parallel Application Developers'
   homepage 'https://eclipse.org/'
 
