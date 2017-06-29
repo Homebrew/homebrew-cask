e084ade24c4410d68784120f875487d0e52f1238
Victor Popkov <victor@popkov.me>
2017-06-29 22:47:23 +0300

    Update codekit to 3.2-25849

--- Casks/codekit.rb
+++ Casks/codekit.rb
@@ -1,10 +1,10 @@
 cask 'codekit' do
-  version '3.1-25813'
-  sha256 '5856af9a5035c25fac01875efc053f33f4da282c5f089b8dca9ef800fed47bd9'
+  version '3.2-25849'
+  sha256 'ef41362ab6a1f22f0d80bf674598614f99d264a6b9f5f7efd481dfd057a18bed'
 
   url "https://codekitapp.com/binaries/codekit-#{version.sub(%r{.*-}, '')}.zip"
   appcast "https://codekitapp.com/api/#{version.major}/appcast.xml",
-          checkpoint: '8a4e8e10d0d73b681d18252c8c8bd8cdd0a7c1a0ac18921b7f2596605c51e489'
+          checkpoint: '7a4fb00acf71139248a8178bd45c1fea94abd674cc58482e0092fe71a6274387'
   name 'CodeKit'
   homepage 'https://codekitapp.com/'
 
