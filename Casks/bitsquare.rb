61da72bb6a5b8bb5e9e03aaac2b6c07a60ce3cb4
Gobinath Mallaiyan <i@gobinath.com>
2017-06-29 00:00:34 -0400

    Update bitsquare to 0.5.0

--- Casks/bitsquare.rb
+++ Casks/bitsquare.rb
@@ -1,11 +1,11 @@
 cask 'bitsquare' do
-  version '0.4.9.9.3'
-  sha256 '4e44b70dd12cc2ef2d3cc641c84a6bc8c38518fba5af327acf0de8985cccdcd1'
+  version '0.5.0'
+  sha256 'c8d7b040f6f1e506741054b19677671e99ae3265eb879aa5d2c902667b81c63b'
 
   # github.com/bitsquare/bitsquare was verified as official when first introduced to the cask
-  url "https://github.com/bitsquare/bitsquare/releases/download/v#{version}/Bitsquare-#{version}.dmg"
+  url "https://github.com/bitsquare/bitsquare/releases/download/v#{version}/Bisq-#{version}.dmg"
   appcast 'https://github.com/bitsquare/bitsquare/releases.atom',
-          checkpoint: 'f1ce7f5db8512545a60123e5fa567ad8737f3ffb8672d17b298002f5faba156f'
+          checkpoint: 'e17cb7c3259e7e08aeeeb391d2d7ad4b5d0c8c535b32891f238379551f955c56'
   name 'Bitsquare'
   homepage 'https://bitsquare.io/'
   gpg "#{url}.asc", key_id: '1dc3c8c4316a698ac494039cf5b84436f379a1c6'
