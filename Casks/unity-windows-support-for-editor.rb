8379c0aad64283e75bfd9ff760d1607a88a1dc71
Bradley McCrorey <brad@projectpixelpress.com>
2017-06-29 14:43:00 -0500

    Update unity-windows-support-for-editor to 5.6.2f1,a2913c821e27 (#35988)

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-windows-support-for-editor' do
-  version '5.6.0f3,497a0f351392'
-  sha256 '5958941e2b13fa96f42e28c2413f9b2b709ea1db8a79355652ee62c30de82713'
+  version '5.6.2f1,a2913c821e27'
+  sha256 '47c3fd017bf7099e16db043666f88cd6f89f9814b8b1881202a9339240bf20d1'
 
   url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity Windows Build Support'
