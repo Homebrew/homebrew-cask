3a8b37113271e9c9e98e13c5569d9643e509e188
commitay <commitay@users.noreply.github.com>
2017-06-30 12:15:06 +1000

    Update endnote

--- Casks/endnote.rb
+++ Casks/endnote.rb
@@ -1,12 +1,21 @@
 cask 'endnote' do
-  version :latest
-  sha256 :no_check
+  version '8'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url 'http://download.endnote.com/downloads/X8/EndNoteX8Installer.dmg'
+  url "http://download.endnote.com/downloads/X#{version}/EndNoteX#{version}Installer.dmg"
   name 'EndNote'
   homepage 'http://endnote.com/'
 
-  container nested: 'Install EndNote X8.app/Contents/Resources/EndNote.zip'
+  container nested: "Install EndNote X#{version}.app/Contents/Resources/EndNote.zip"
 
-  suite 'EndNote X8'
+  suite "EndNote X#{version}"
+
+  zap delete: [
+                '/Library/Application Support/ResearchSoft/EndNote',
+                '~/Library/Application Support/EndNote',
+                '~/Library/Caches/com.ThomsonResearchSoft.EndNote',
+                '~/Library/Services/ENService.app/Contents/Resources/EndNote.icns',
+                '~/Library/Spotlight/EndNote.mdimporter',
+              ],
+      trash:  '~/Library/Preferences/com.ThomsonResearchSoft.EndNote.plist'
 end
