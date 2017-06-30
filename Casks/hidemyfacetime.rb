65db892b26a93b59313c355cd4f32c87e664d00e
qkdreyer <quentin.dreyer@triotech.fr>
2017-06-30 13:44:29 +0200

    Add HideMyFaceTime.app v1.0

---
+++ Casks/hidemyfacetime.rb
@@ -0,0 +1,10 @@
+cask 'hidemyfacetime' do
+  version '1.0'
+  sha256 '82059c37bece329ab5bd293615b209deb893c390513410b7cd2d199485513c75'
+
+  url 'https://andrewdeniszczyc.com/hidemyfacetime/builds/HideMyFaceTime.zip'
+  name 'HideMyFaceTime'
+  homepage 'https://andrewdeniszczyc.com/hidemyfacetime/'
+
+  app 'HideMyFaceTime.app'
+end
