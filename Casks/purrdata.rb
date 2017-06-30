bc0b25a68617369fa3d5f224088437fbed1389fa
commitay <commitay1@gmail.com>
2017-06-30 20:28:48 +1000

    Update purrdata.rb

--- Casks/purrdata.rb
+++ Casks/purrdata.rb
@@ -1,11 +1,14 @@
-cask 'purrdata' do
-  version ':latest'
+cask 'pd-l2ork' do
+  version '2.2.1'
   sha256 'f11c122dcabfdec9acbfd78e4ca4654f6fe6fe61bb7e612d54825a0289908753'
 
   # github.com/agraef/purr-data was verified as official when first introduced to the cask
-  url 'https://github.com/agraef/purr-data/releases/download/2.2.1/pd-l2ork-2.2.1-osx_10.8-amd64.dmg'
-  name 'purrdata'
+  url "https://github.com/agraef/purr-data/releases/download/#{version}/pd-l2ork-#{version}-osx_10.11-amd64.dmg"
+  appcast 'https://github.com/agraef/purr-data/releases.atom',
+          checkpoint: '97a5f064e9efe23c3ef5b95afac77b5398e5f9464fb52d886b75b85bd198ae5e'
+  name 'Pd-l2ork'
+  name 'Purr Data'
   homepage 'https://agraef.github.io/purr-data/'
 
-  app 'pd-l2ork.app'
+  app 'Pd-l2ork.app'
 end
