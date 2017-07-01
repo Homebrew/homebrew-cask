402a11c2a5fa36b2a964602d4082d0d9408fa50e
Yasser Fadl <yasser.fadl@gmail.com>
2017-07-01 21:52:42 +0200

    Making version number interpolate for deepstream.io

--- Casks/deepstream.rb
+++ Casks/deepstream.rb
@@ -2,10 +2,12 @@ cask 'deepstream' do
   version '2.4.0'
   sha256 '3b4a03a560373493859802b0a802331ddc91fe95bd51bdfa26baa882d2967b8e'
 
-  # s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/ds-server-artifacts/deepstreamIO/deepstream.io/2.4.0/deepstream.io-mac-2.4.0-5bca621.pkg'
-  name 'deepstream.io'
-  homepage 'https://deepstreamhub.com/'
+  # github.com/deepstreamIO/deepstream.io was verified as official when first introduced to the cask
+  url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg"
+  appcast 'https://github.com/deepstreamIO/deepstream.io/releases.atom',
+          checkpoint: 'e018eef5240d2eaf2158a9c9dad2ba7d5bfe26d33a42762008f15b3bcb66d6aa'
+  name 'deepstream'
+  homepage 'https://deepstream.io/'
 
-  pkg 'deepstream.io-mac-2.4.0-5bca621.pkg'
+  pkg "deepstream.io-mac-#{version}.pkg"
 end
