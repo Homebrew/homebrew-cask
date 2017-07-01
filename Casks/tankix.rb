79570ccf54b7a404482c0969f593d36ecf35d8f5
giannitm <giannitm@users.noreply.github.com>
2017-07-01 21:55:15 +0100

    Update tankix to 670

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version '668'
-  sha256 '45786e0409371581f596111a2e163bcf4cf7ecc2889e7548abd672a46d8b553a'
+  version '670'
+  sha256 '8f6057bf784dfd27dd89f38bcab23021883b8665b111b0aa6997b3c5271bf3f1'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/prod_#{version}/TankiX.dmg"
   name 'Tanki X'
