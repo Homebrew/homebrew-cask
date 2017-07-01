e3085e38843446a57c6e26d7c0f79e1dedef4fce
commitay <commitay1@gmail.com>
2017-07-02 09:21:05 +1000

    update sha256 / add uninstall and caveat

--- Casks/deepstream.rb
+++ Casks/deepstream.rb
@@ -1,6 +1,6 @@
 cask 'deepstream' do
   version '2.4.0'
-  sha256 '3b4a03a560373493859802b0a802331ddc91fe95bd51bdfa26baa882d2967b8e'
+  sha256 '53cde5333b3a32cc626467880bc45748f92a5543613ef3625100e31569c06e69'
 
   # github.com/deepstreamIO/deepstream.io was verified as official when first introduced to the cask
   url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg"
@@ -10,4 +10,10 @@ cask 'deepstream' do
   homepage 'https://deepstream.io/'
 
   pkg "deepstream.io-mac-#{version}.pkg"
+
+  uninstall pkgutil: 'deepstream.io'
+
+  caveats do
+    files_in_usr_local
+  end
 end
