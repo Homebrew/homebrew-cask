7d04834f2feff696f08a412c57c00e8bd7d940ea
Oliver Erxleben <oliver.erxleben@me.com>
2017-07-01 08:10:37 +0200

    update Gravit Designer URL

--- Casks/gravitdesigner.rb
+++ Casks/gravitdesigner.rb
@@ -2,7 +2,7 @@ cask 'gravitdesigner' do
   version :latest
   sha256 :no_check
 
-  url 'https://app.designer.io/_downloads/mac/GravitDesigner.zip'
+  url 'https://designer.gravit.io/_downloads/mac/GravitDesigner.zip'
   name 'Gravit Designer'
   homepage 'https://designer.io/'
 
