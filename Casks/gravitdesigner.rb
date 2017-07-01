15cb9b2f94f4227dea3340fda0ea0ffec6e5f51c
commitay <commitay1@gmail.com>
2017-07-01 16:28:34 +1000

    add comment

--- Casks/gravitdesigner.rb
+++ Casks/gravitdesigner.rb
@@ -2,6 +2,7 @@ cask 'gravitdesigner' do
   version :latest
   sha256 :no_check
 
+  # designer.gravit.io/_downloads/mac was verified as official when first introduced to the cask
   url 'https://designer.gravit.io/_downloads/mac/GravitDesigner.zip'
   name 'Gravit Designer'
   homepage 'https://designer.io/'
