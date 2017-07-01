fae58b599ea95a6c251d20aab4caf2dc044fb112
Aki Ariga <chezou+github@gmail.com>
2017-07-02 03:11:17 +0900

    Upgrade Impala ODBC Driver to v2.5.39.1020 (#36061)

--- Casks/cloudera-impala-odbc.rb
+++ Casks/cloudera-impala-odbc.rb
@@ -1,6 +1,6 @@
 cask 'cloudera-impala-odbc' do
-  version '2.5.37.1014'
-  sha256 '9ef149fe28a4e0fa4c7b18dda5f078cfb10fb1e261d461af7fa7b38c6815f4fe'
+  version '2.5.39.1020'
+  sha256 '12135956a72f0bd8ec74367996cd6c2a8a065975b80cd4394b20fb366a8b8a13'
 
   url "https://downloads.cloudera.com/connectors/impala_odbc_#{version}/OSX/ClouderaImpalaODBC.dmg"
   name 'Cloudera ODBC Driver for Impala'
