b9d3602f2fbdd14b231c8b1411fcddc233c035a9
Mike Mattozzi <mike.mattozzi@gmail.com>
2017-07-01 15:05:17 -0400

    Update cocoarestclient to 1.4.1

--- Casks/cocoarestclient.rb
+++ Casks/cocoarestclient.rb
@@ -1,11 +1,11 @@
 cask 'cocoarestclient' do
-  version '1.3.16'
-  sha256 '330d075ef8e7937aff4f583fc47c4f5dd094b07360946602f0790cfde958dfd9'
+  version '1.4.1'
+  sha256 '6dd1268e1c23efc6938d5615a691255c356d317918e89346e86caefec8d599bf'
 
   # github.com/mmattozzi/cocoa-rest-client was verified as official when first introduced to the cask
   url "https://github.com/mmattozzi/cocoa-rest-client/releases/download/#{version}/CocoaRestClient-#{version}.dmg"
   appcast 'https://github.com/mmattozzi/cocoa-rest-client/releases.atom',
-          checkpoint: 'e227c188c94e718eab556b3d1a97ea037ae78e43b2f62e00009e5d99a1145a59'
+          checkpoint: '10c6f9905497ea558e43425388e947af9c1a8ade867a311b503b1e69a69f2511'
   name 'CocoaRestClient'
   homepage 'https://mmattozzi.github.io/cocoa-rest-client/'
 
