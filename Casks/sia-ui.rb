03d9367cce00e2c182b1a7bced8fe1662a007a62
Miccal Matthews <miccal.matthews@gmail.com>
2017-07-02 09:05:05 +0800

    Update sia-ui.rb

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -7,7 +7,7 @@ cask 'sia-ui' do
   appcast 'https://github.com/NebulousLabs/Sia-UI/releases.atom',
           checkpoint: '121f1e8775f906861a4ca376e5cb440d289411511313243d7b891982bea6cdb5'
   name 'Sia-UI'
-  homepage 'http://sia.tech/'
+  homepage 'https://sia.tech/'
 
   app 'Sia-UI.app'
 end
