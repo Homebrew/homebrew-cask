80d2881f0bb5c3c95b30b9a7ee43fa9825c8fd0a
Vicent Tsai <vayn@vayn.de>
2017-07-02 12:40:28 +0800

    Update exnihilo to 1.7.2

--- Casks/exnihilo.rb
+++ Casks/exnihilo.rb
@@ -1,11 +1,11 @@
 cask 'exnihilo' do
-  version '1.7.0'
-  sha256 '83e4d902fe2b6bada07473a2ac2252dceed9464a95675769e217679fdb2f4d27'
+  version '1.7.2'
+  sha256 'd7666bc354a194096cd4b80b80128c51eeb73e73436e58f149046eb1fbf2a186'
 
   # github.com/Vayn/ex-nihilo was verified as official when first introduced to the cask
   url "https://github.com/Vayn/ex-nihilo/releases/download/#{version}/ExNihilo.zip"
   appcast 'https://github.com/vayn/ex-nihilo/releases.atom',
-          checkpoint: '2aa8f081814ba2342b2e1252445c23c6499c139ba8d321056e44e49917e42fa8'
+          checkpoint: '52bb112245d4732b99ef24a844d8aec9e5bab9705f7beac7933fd130e882e09b'
   name 'Ex nihilo'
   homepage 'https://vayn.github.io/ex-nihilo/'
 
