445ff92af0d01e37239669eada951b1750b16829
Victor Popkov <victor@popkov.me>
2017-07-02 22:21:02 +0300

    Update bisq to 0.5.1 (#36089)

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,11 +1,11 @@
 cask 'bisq' do
-  version '0.5.0'
-  sha256 'c8d7b040f6f1e506741054b19677671e99ae3265eb879aa5d2c902667b81c63b'
+  version '0.5.1'
+  sha256 '68c7fdbead8b723c9a887576a4d9be497a1c525db029ccec8af62001a7a42d6a'
 
   # github.com/bitsquare/bitsquare was verified as official when first introduced to the cask
   url "https://github.com/bitsquare/bitsquare/releases/download/v#{version}/Bisq-#{version}.dmg"
   appcast 'https://github.com/bitsquare/bitsquare/releases.atom',
-          checkpoint: 'e17cb7c3259e7e08aeeeb391d2d7ad4b5d0c8c535b32891f238379551f955c56'
+          checkpoint: '09835a205339c4157693f75fcbe7e0ca6725a3b82008f8180bc25d6e5a6c21d5'
   name 'Bisq'
   homepage 'https://bitsquare.io/'
   gpg "#{url}.asc", key_id: '1dc3c8c4316a698ac494039cf5b84436f379a1c6'
