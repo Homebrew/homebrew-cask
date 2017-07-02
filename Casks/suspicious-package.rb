b79d502cc8d06a82e3a471ae3e723e8913623581
commitay <commitay1@gmail.com>
2017-07-02 23:28:22 +1000

    Update suspicious-package - add zap (#36082)
    * Update suspicious-package - add zap
    
    * zap trash

--- Casks/suspicious-package.rb
+++ Casks/suspicious-package.rb
@@ -7,4 +7,15 @@ cask 'suspicious-package' do
   homepage 'http://www.mothersruin.com/software/SuspiciousPackage/'
 
   app 'Suspicious Package.app'
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.mothersruin.suspiciouspackageapp.sfl',
+                '~/Library/Caches/com.mothersruin.SuspiciousPackageApp',
+                '~/Library/Caches/com.mothersruin.XPCService.UpdateChecker',
+                '~/Library/WebKit/com.mothersruin.SuspiciousPackageApp',
+              ],
+      trash:  [
+                '~/Library/Preferences/com.mothersruin.SuspiciousPackage.plist',
+                '~/Library/Preferences/com.mothersruin.SuspiciousPackageApp.plist',
+              ]
 end
