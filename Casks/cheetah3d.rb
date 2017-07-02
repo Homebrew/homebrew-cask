9d4333f0f978c37b5c52745d5f98641eb106c369
Miccal Matthews <miccal.matthews@gmail.com>
2017-07-02 09:02:37 +0800

    Update cheetah3d.rb

--- Casks/cheetah3d.rb
+++ Casks/cheetah3d.rb
@@ -4,7 +4,7 @@ cask 'cheetah3d' do
 
   url 'http://cheetah3d.com/download/Cheetah3D.dmg'
   name 'Cheetah3D'
-  homepage 'http://www.cheetah3d.com/'
+  homepage 'https://www.cheetah3d.com/'
 
   app 'Cheetah3D.app'
 end
