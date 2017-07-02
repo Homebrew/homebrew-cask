523e2de9c8c2c073fa99050e1d3eb0ec5edaccdb
Lance M <mightyfree@users.noreply.github.com>
2017-07-02 05:46:23 -0400

    Update Authy Desktop version to 1.0.12, replace version checksum. (#36076)

--- Casks/authy-desktop.rb
+++ Casks/authy-desktop.rb
@@ -1,9 +1,9 @@
 cask 'authy-desktop' do
-  version '1.0.10'
-  sha256 '64c76377fe993eca459bc0f470cd0eaef26e16f7b4ced75adfd46eaf2e37ae89'
+  version '1.0.12'
+  sha256 '3f500b84a876cadc3ff3b8cbdf1fc122ea25b557aebb812a54a0312c595ba6a8'
 
   # s3.amazonaws.com/authy-electron-repository-production was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/authy-electron-repository-production/stable/#{version}/darwin/x64/installer.dmg"
+  url "https://s3.amazonaws.com/authy-electron-repository-production/stable/#{version}/darwin/x64/authy-installer.dmg"
   name 'Authy Desktop'
   homepage 'https://authy.com/'
 
