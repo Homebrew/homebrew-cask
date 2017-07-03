72117d251e8e7c8efd711a741c336b37a0d49e11
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 00:22:32 -0400

    Update puppet-agent to version 1.10.4-1
    Updated puppet-agent to version 1.10.4-1, modifying SHA256 checksums for Yosemite, El Capitan & MacOS. Additionally, modified appcast checkpoint checksum to reflects recent updates on page.

--- Casks/puppet-agent.rb
+++ Casks/puppet-agent.rb
@@ -1,18 +1,18 @@
 cask 'puppet-agent' do
-  version '1.10.1-1'
+  version '1.10.4-1'
 
   if MacOS.version == :yosemite
-    sha256 '03c9a5c43cd5e865967381d5ff51a4fa84d0241814148b323d479e0593d95ebe'
+    sha256 'e9c2dd30c2a81cf004f168782e2cc49161e2bd4ea041e43116a7d0be5b854dfb'
   elsif MacOS.version == :el_capitan
-    sha256 '80973fed3afb8527568d2d07f7b3ed8db15dbe814320e36922f95532b76381a4'
+    sha256 '37067d3a337492021895ca14a60e50be4eea82e0b9c431b63ecc95b63bf4876'
   else
-    sha256 '64c72aeeab9fa3e06b5317ea3d32145ee34be88df4f8b5d667b5fead2a863679'
+    sha256 '32ff2b3dafbc9e98df979d4a3bb0ebc42963277430a5f5be7ae07e5123bbd143'
   end
 
   # downloads.puppetlabs.com was verified as official when first introduced to the cask
   url "https://downloads.puppetlabs.com/mac/#{MacOS.version}/PC1/x86_64/puppet-agent-#{version}.osx#{MacOS.version}.dmg"
   appcast 'https://downloads.puppetlabs.com/mac/10.12/PC1/x86_64/',
-          checkpoint: '3cb97715d7f9e1b0afe3d2a7736ab8390b6b02bd1d7db79db6d9e4e41ad74640'
+          checkpoint: 'e161485d3f785c7e10323b4a82db0034ac4b7b5899cfa077a55dd5401255c494'
   name 'Puppet Agent'
   homepage 'https://docs.puppet.com/puppet/4.5/about_agent.html'
 
