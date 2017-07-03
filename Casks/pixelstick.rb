8a23838ae4cbd9cfca78402ddb19ac6c8b616e02
Miccal Matthews <miccal.matthews@gmail.com>
2017-07-03 16:24:12 +0800

    Update pixelstick.rb

--- Casks/pixelstick.rb
+++ Casks/pixelstick.rb
@@ -1,6 +1,6 @@
 cask 'pixelstick' do
-  version '2.10.1'
-  sha256 '7a28198028e432f89a8edd5e9d9b4174ba65fe88b234c3bebec89058ff155e53'
+  version :latest
+  sha256 :no_check
 
   url 'https://plumamazing.com/bin/pixelstick/pixelstick.zip'
   name 'PixelStick'
