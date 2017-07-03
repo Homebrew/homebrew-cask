de6924b7796ef933c5091a56e6df4ad1a35f28b3
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 06:33:24 -0400

    Update panoply.rb to version 4.8.1 (#36130)
    Updated panoply.rb to version 4.8.1 and adjusted SHA256 checksum accordingly.

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,6 @@
 cask 'panoply' do
-  version '4.7.0'
-  sha256 '17467c84ba9e8e359da0fd9d636710675b7c0b9637faaac5489f4faed64f89a2'
+  version '4.8.1'
+  sha256 'e6142a1c1d83313381ab33b9faa2fe85aa455fc816d55fb5d02231b484eaf520'
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
   name 'Panoply netCDF, HDF and GRIB Data Viewer'
