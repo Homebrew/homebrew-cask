c46a52547f088d908e6a525084f891ae022347e8
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 02:45:18 -0400

    Update portfolioperformance to version 0.27.3 (#36113)
    Updated portfolioperformance to version 0.27.3 and adjusted SHA256 checksum accordingly. Additionally, updated appcast checkpoint due to updates.

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,11 +1,11 @@
 cask 'portfolioperformance' do
-  version '0.27.1'
-  sha256 '6cb8c9c334eca5dc0fd0fe56a875ab61d1da7b768716b505d74f17edf568eb08'
+  version '0.27.3'
+  sha256 'ff9993e00dd5bcb448e39758e91470727461b816556e37371993182780fc7a3f'
 
   # s3.amazonaws.com/name.abuchen.portfolio was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/name.abuchen.portfolio/#{version}/PortfolioPerformance-#{version}-macosx.cocoa.x86_64.tar.gz"
   appcast 'https://github.com/buchen/portfolio/releases.atom',
-          checkpoint: '44817467c163087f005fa958f5240b3d3662dfd33f0be799d2d4ebd9f2e1d485'
+          checkpoint: '72a8026a0bd28f1ca392bc18730b8f922933439efda89135c777ea661ce693b5'
   name 'Portfolio Performance'
   homepage 'http://www.portfolio-performance.info/portfolio/'
 
