fde59e22d77bb79a7c5858b12decf27085d416e8
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 02:33:27 -0400

    Update postage SHA256 checksum

--- Casks/postage.rb
+++ Casks/postage.rb
@@ -1,6 +1,6 @@
 cask 'postage' do
   version '3.2.15'
-  sha256 'de36ba3d4eb97dac51d0ca8181ced8d022111f640f409ab5b0b84847653b5352'
+  sha256 'acc41d713a645db0b38d1b0b5fbe59b575f965cb9faa18ea144bac9d64d19674'
 
   # github.com/workflowproducts/postage was verified as official when first introduced to the cask
   url "https://github.com/workflowproducts/postage/releases/download/eV#{version}/Postage-#{version}.dmg"
