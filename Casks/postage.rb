1cf82f73f7d64c533fb88e4e29df33e5e3d22da2
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 02:25:45 -0400

    Update postage to version 3.2.15
    Updated postage to version 3.2.15 and adjusted SHA256 checksum accordingly. Additionally, updated appcast checkpoint to reflect version updates on page.

--- Casks/postage.rb
+++ Casks/postage.rb
@@ -1,11 +1,11 @@
 cask 'postage' do
-  version '3.2.14'
-  sha256 '4d644afc5181498cf4a0859f763fda0d0d89ad95e3a3ed21856d30580fc56c26'
+  version '3.2.15'
+  sha256 'de36ba3d4eb97dac51d0ca8181ced8d022111f640f409ab5b0b84847653b5352'
 
   # github.com/workflowproducts/postage was verified as official when first introduced to the cask
   url "https://github.com/workflowproducts/postage/releases/download/eV#{version}/Postage-#{version}.dmg"
   appcast 'https://github.com/workflowproducts/postage/releases.atom',
-          checkpoint: '136a805a46e7098a7ff6dcdae3b7d2e02464e5206693d15c450fc19531310915'
+          checkpoint: 'de36ba3d4eb97dac51d0ca8181ced8d022111f640f409ab5b0b84847653b5352'
   name 'Postage'
   homepage 'https://www.workflowproducts.com/postage.html'
 
