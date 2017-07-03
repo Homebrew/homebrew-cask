174f87687ec03039d14a3458b175b3b9595dca06
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 01:10:33 -0400

    Update propresenter to version 6.2.3_b16029
    Update propresenter to version 6.2.3_b16029 and updated SHA256 checksum. Additionally, updated appcast checkpoint to reflect updated page.

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -1,10 +1,10 @@
 cask 'propresenter' do
-  version '6.2.1_b16020'
-  sha256 '2be4c6a776ff9b12e626374ccaeabde139709c130cbfa19de4ec67bd942f1a2d'
+  version '6.2.3_b16029'
+  sha256 'e674617802f95af5d92ac700c3997c4d95b30398215729cdfb134e188d3f3421'
 
   url "https://www.renewedvision.com/downloads/ProPresenter#{version.major}_#{version}.dmg"
   appcast "https://www.renewedvision.com/update/ProPresenter#{version.major}.php",
-          checkpoint: 'cf0ea3d657a0eaa3a688eede5b8caf0a2e151bf731c6ff724bf9d453445e9ad0'
+          checkpoint: 'd072eac69d484da1355a1a9072240ee59c8dedfc83e974929d050c667097f5f0'
   name 'ProPresenter'
   homepage 'https://www.renewedvision.com/propresenter.php'
 
