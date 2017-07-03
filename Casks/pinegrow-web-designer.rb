4ab9f2b355cca858665469c3162f99c6eef741f2
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 03:45:47 -0400

    Update pinegrow-web-designer to version 3.01
    Updated pinegrow-web-designer to version 3.01 and adjusted SHA256 checksum.

--- Casks/pinegrow-web-designer.rb
+++ Casks/pinegrow-web-designer.rb
@@ -1,6 +1,6 @@
 cask 'pinegrow-web-designer' do
-  version '2.951'
-  sha256 'ebad933a306fcf0b0848ece55950e17653799da49ed01100d5d0e5da2c87f69c'
+  version '3.01'
+  sha256 '4939f9df33471a4390fdcf3b49ab3d9bb20311b2d5eab860a9ce845929958092'
 
   # pinegrow.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://pinegrow.s3.amazonaws.com/PinegrowMac.#{version}.dmg"
