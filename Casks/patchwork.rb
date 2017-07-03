de2e0a1bc979508dd5176aeadb742352587b763f
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 05:41:09 -0400

    Update patchwork.rb to version 3.6.5
    Updated patchwork.rb to version 3.6.5 and adjusted SHA256 checksum accordingly. Additionally, updated appcast checkpoint to reflect recent updates.

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,10 +1,10 @@
 cask 'patchwork' do
-  version '3.6.4'
-  sha256 '36a1a240348a9fe580f7f8c42bf7b3139328b4b62894f700fe95661bf29b1fc2'
+  version '3.6.5'
+  sha256 'bb95d84ae6f33d3d32eb53fb58ed2acd62dbb6c533d5f166a32ee27bb6b6188e'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}-mac.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom',
-          checkpoint: 'b8dbd728b0a113a57572f4860cea17ba39daa10b722e767ed0cf376085821793'
+          checkpoint: '548805d36a44614dd7d98344a0061b6ca068cc3f81447382380256ae3c782a3f'
   name 'Patchwork'
   homepage 'https://github.com/ssbc/patchwork'
 
