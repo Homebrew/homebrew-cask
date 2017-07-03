96ebb9090a1149d02d45d2f60c35418e9c3eeb9a
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 08:17:00 -0400

    Update paintcode.rb version to 3, update url (#36131)
    * Update pixelcut.rb version to 3, update url
    
    Update pixelcut.rb version to 3 and adjust SHA256 checksum accordingly. Update url path for download to pixelcut.com (official developer download).
    
    * add verified comment
    
    * Update paintcode.rb
    
    * Update paintcode.rb version to 3.3.1, url path
    
    Updated paintcode.rb version to 3.3.1, url path to version major.

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -1,8 +1,11 @@
 cask 'paintcode' do
-  version '2.4.2'
-  sha256 '8e052831ba1ea257f265ac7248d560f4872bb71fcaccb214f048c8907c319381'
+  version '3.3.1'
+  sha256 '3b736b8e29eed8573d285123ea3fe3f5d9dd258549e1c78d667233736651cba9'
 
-  url "https://www.paintcodeapp.com/content/versions/#{version}/paintcode-trial.zip"
+  # pixelcut.com/paintcode was verified as official when first introduced to the cask
+  url "https://www.pixelcut.com/paintcode#{version.major}/paintcode.zip"
+  appcast "https://www.pixelcut.com/paintcode#{version.major}/appcast.xml",
+          checkpoint: '29d13ab38d6162342c8b3f3da11df15655632a706ad849bf901a92ff7441d891'
   name 'PaintCode'
   homepage 'https://www.paintcodeapp.com/'
 
