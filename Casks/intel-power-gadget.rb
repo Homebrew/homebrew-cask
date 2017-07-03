a5d132b4c2790f624a3ec7f5458ef21c3f1c96da
Miccal Matthews <miccal.matthews@gmail.com>
2017-07-03 20:21:15 +0800

    Update intel-power-gadget.rb

--- Casks/intel-power-gadget.rb
+++ Casks/intel-power-gadget.rb
@@ -1,8 +1,8 @@
 cask 'intel-power-gadget' do
-  version '3.0.7'
+  version '3.0.7,553992'
   sha256 '4474360bf027a76b69a5803a681ff9cded1e0a92c9e03c57bfc127d29eea0319'
 
-  url 'https://software.intel.com/file/553992/download'
+  url "https://software.intel.com/file/#{version.after_comma}/download"
   name 'Intel Power Gadget'
   homepage 'https://software.intel.com/en-us/articles/intel-power-gadget-20'
 
