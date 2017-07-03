346af4a431958dd1f18c53ddc287a1e39ef9d6c3
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 00:07:16 -0400

    Update pyfa version to 1.29.4,yc119.5-1.0
    Updated pya to version to 1.29.4,yc119.5-1.0 and replaced SHA256 for new zip file. Additionally, updated checkpoint for existing appcast to reflect recent changes.

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,10 +1,10 @@
 cask 'pyfa' do
-  version '1.29.3,yc119.5-1.0'
-  sha256 '1c9de1119d9e7cfc1b004e5196747c1d59c767a88f8e1d3fd9e6ecaa388cc1a9'
+  version '1.29.4,yc119.5-1.0'
+  sha256 '14cfdaaf480b98e08b88cf6015064ac4f7873c276cc96ae6770b5e56c6e05e2a'
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version.before_comma}/pyfa-#{version.before_comma}-#{version.after_comma}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom',
-          checkpoint: '2940e61ab40bee484b4a1f0d27286492140b35bd65ce2c58e662cd3748e875bc'
+          checkpoint: '839cab356a4b1c4a0ad7e6f8a33b4f68ba479973494540f22261104f438c8e78'
   name 'pyfa'
   homepage 'https://github.com/pyfa-org/Pyfa'
 
