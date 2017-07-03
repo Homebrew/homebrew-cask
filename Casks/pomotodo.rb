cf6134a9271d955326b8e01665bb1fdecd1b903f
commitay <commitay1@gmail.com>
2017-07-03 17:07:42 +1000

    update appcast

--- Casks/pomotodo.rb
+++ Casks/pomotodo.rb
@@ -4,8 +4,8 @@ cask 'pomotodo' do
 
   # cdn.hackplan.com/theair was verified as official when first introduced to the cask
   url "http://cdn.hackplan.com/theair/#{version.after_comma}/Pomotodo_v#{version.before_comma}.dmg"
-  appcast 'http://air.hackplan.com/projects/5455f382437315386000d4d5/versions/latest.xml',
-          checkpoint: '8f67471dd199a1fdaeda4e5aad44226e85e40c556f946bd141e7e691e45b7fcc'
+  appcast "https://air.pomotodo.com/v1/p/com.pomotodo.PomotodoMac#{version.major}/latest.xml",
+          checkpoint: 'd901ecb9af3414888d4acec8f61eebe4cf4640e4ef298934ece09fe67ab0ce99'
   name 'Pomodoro'
   homepage 'https://pomotodo.com/'
 
