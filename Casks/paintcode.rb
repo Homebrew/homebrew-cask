0c9f4b5d4fa29d4e11863bd2b5cea115e807a103
commitay <commitay1@gmail.com>
2017-07-03 20:42:33 +1000

    add verified comment

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -2,6 +2,7 @@ cask 'paintcode' do
   version '3'
   sha256 '3b736b8e29eed8573d285123ea3fe3f5d9dd258549e1c78d667233736651cba9'
 
+  # pixelcut.com/paintcode was verified as official when first introduced to the cask
   url "https://www.pixelcut.com/paintcode#{version}/paintcode.zip"
   name 'PaintCode'
   homepage 'https://www.paintcodeapp.com/'
