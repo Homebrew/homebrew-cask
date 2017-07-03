42582d3df91b63e2835cb19c830134181e27aa39
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 04:28:28 -0400

    Update pharo to version 6.0
    Updated pharo to version 6.0, now serving x64 on same domain.

--- Casks/pharo.rb
+++ Casks/pharo.rb
@@ -1,8 +1,8 @@
 cask 'pharo' do
-  version '5.0'
+  version '6.0'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "http://files.pharo.org/platform/Pharo#{version}-mac.zip"
+  url "http://files.pharo.org/platform/Pharo#{version}-64-mac.zip"
   name 'Pharo'
   homepage 'https://pharo.org/'
 
