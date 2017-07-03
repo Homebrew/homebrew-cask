2a2b71f6ff406549c0b7069255118169d9f7c6f8
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 07:32:04 -0400

    Update paintcode.rb version to 3.3.1, url path
    Updated paintcode.rb version to 3.3.1, url path to version major.

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -1,10 +1,10 @@
 cask 'paintcode' do
-  version '3'
+  version '3.3.1'
   sha256 '3b736b8e29eed8573d285123ea3fe3f5d9dd258549e1c78d667233736651cba9'
 
   # pixelcut.com/paintcode was verified as official when first introduced to the cask
-  url "https://www.pixelcut.com/paintcode#{version}/paintcode.zip"
-  appcast "https://www.pixelcut.com/paintcode#{version}/appcast.xml",
+  url "https://www.pixelcut.com/paintcode#{version.major}/paintcode.zip"
+  appcast "https://www.pixelcut.com/paintcode#{version.major}/appcast.xml",
           checkpoint: '29d13ab38d6162342c8b3f3da11df15655632a706ad849bf901a92ff7441d891'
   name 'PaintCode'
   homepage 'https://www.paintcodeapp.com/'
