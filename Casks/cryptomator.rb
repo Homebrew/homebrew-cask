5a7afd432ac3457b25596047c6af8bdd427d2126
Leo Lou <louyuhong@gmail.com>
2017-07-03 10:48:09 +0800

    Update cryptomator to 1.3.0 (#36098)

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,11 +1,11 @@
 cask 'cryptomator' do
-  version '1.2.4'
-  sha256 'f3753716ee69240d2904740c38a0c280bfca1e3b89fa5111b98db484a896d090'
+  version '1.3.0'
+  sha256 '6fa678f0bdc15af485664d18cb6a3381733d97d5222581fb01087ed712a9fbce'
 
   # bintray.com/artifact/download/cryptomator was verified as official when first introduced to the cask
   url "https://bintray.com/artifact/download/cryptomator/cryptomator/Cryptomator-#{version}.dmg"
   appcast 'https://github.com/cryptomator/cryptomator/releases.atom',
-          checkpoint: 'a46bed0d8c85da9b93bd70b17091b30098a68186751ec92fe65bd54e5407432f'
+          checkpoint: 'a5785478203f4bd2e500ee0d62b0077fb27437aa138fb78f805304022315ce48'
   name 'Cryptomator'
   homepage 'https://cryptomator.org/'
 
