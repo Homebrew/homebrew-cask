09ae5a2a114a80d12c402d95156eba7c68eea525
Lance M <mightyfree@users.noreply.github.com>
2017-07-03 06:03:14 -0400

    Update parallels-virtualization-sdk.rb to version 12.2.1-41615 (#36128)
    Updated parallels-virtualization-sdk.rb to version 12.2.1-41615 and update SHA256 checksum accordingly.

--- Casks/parallels-virtualization-sdk.rb
+++ Casks/parallels-virtualization-sdk.rb
@@ -1,6 +1,6 @@
 cask 'parallels-virtualization-sdk' do
-  version '12.2.0-41591'
-  sha256 '7294fb05e5750025acecde57fb516170c266bb437a42cfea626f79b2bbb8bfd1'
+  version '12.2.1-41615'
+  sha256 'dce000083affb654bdf3ae1cd837d7437686ac3a5683a00a7d9e0d8f13d537b8'
 
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsVirtualizationSDK-#{version}-mac.dmg"
   name 'Parallels Virtualization SDK'
