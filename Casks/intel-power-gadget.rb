76c1b72940672ebab949e5bd5409138c272392e3
commitay <commitay1@gmail.com>
2017-07-04 15:49:10 +1000

    Update intel-power-gadget - url fix (#36161)

--- Casks/intel-power-gadget.rb
+++ Casks/intel-power-gadget.rb
@@ -2,7 +2,7 @@ cask 'intel-power-gadget' do
   version '3.0.7,553992'
   sha256 '4474360bf027a76b69a5803a681ff9cded1e0a92c9e03c57bfc127d29eea0319'
 
-  url "https://software.intel.com/file/#{version.after_comma}/download"
+  url "https://software.intel.com/file/#{version.after_comma}/download?#.dmg"
   name 'Intel Power Gadget'
   homepage 'https://software.intel.com/en-us/articles/intel-power-gadget-20'
 
