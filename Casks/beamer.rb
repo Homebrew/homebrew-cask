ded7812763e0fffb3da732883f9a7cbe3a2d93bc
Victor Popkov <victor@popkov.me>
2017-07-04 21:22:40 +0300

    Update beamer to 3.3.2 (#36170)

--- Casks/beamer.rb
+++ Casks/beamer.rb
@@ -1,10 +1,10 @@
 cask 'beamer' do
-  version '3.3.1'
-  sha256 '53d712e1037ccdb0334caf6615c2da97c239f1a4f3eeb65d0547a018d6447d26'
+  version '3.3.2'
+  sha256 'da58fa737a303f9d3bbe3885faae77c41678d94bb776f292fc6a8206f6d2f4bb'
 
   url "https://beamer-app.com/resources/downloads/Beamer-#{version}.zip"
   appcast "https://beamer-app.com/beamer#{version.major}-appcast.xml",
-          checkpoint: 'a91f0d52dfbeb5060670038c147c7cfccb7d4b7aba027dcf9e0f87962319ae06'
+          checkpoint: '68804545e82b376c6fb408754630523804f671c9cb1296cba5502597a404ef55'
   name 'Beamer'
   homepage 'https://beamer-app.com/'
 
