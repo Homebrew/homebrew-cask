cc2b773c97c909164c8ce3a99b4da6280cb11591
commitay <commitay@users.noreply.github.com>
2017-07-04 10:53:40 +1000

    Update intel power gadget - url / appcast
    remove appcast
    version

--- Casks/intel-power-gadget.rb
+++ Casks/intel-power-gadget.rb
@@ -1,8 +1,8 @@
 cask 'intel-power-gadget' do
-  version '3.0.7,553992'
+  version '3.0.7'
   sha256 '4474360bf027a76b69a5803a681ff9cded1e0a92c9e03c57bfc127d29eea0319'
 
-  url "https://software.intel.com/file/#{version.after_comma}/download"
+  url 'https://software.intel.com/sites/default/files/managed/10/45/Intel%C2%AE%20Power%20Gadget.dmg'
   name 'Intel Power Gadget'
   homepage 'https://software.intel.com/en-us/articles/intel-power-gadget-20'
 
