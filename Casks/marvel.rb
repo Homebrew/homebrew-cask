62f1ea11b198ac5a72d384bb7867a9733b84b86a
Victor Popkov <victor@popkov.me>
2017-07-04 21:33:15 +0300

    Update marvel to 5.2.1

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,11 +1,11 @@
 cask 'marvel' do
-  version '5.1'
-  sha256 '1bf9c4cdbe7a871cb12137216a01242d5e2d702a294f0baf6961d6feec31e477'
+  version '5.2.1'
+  sha256 '7a2c392c8023362d2b719c10421fbda7f794016e0b47fc0fc904c4ff39eee1eb'
 
   # storage.googleapis.com/sketch-plugin was verified as official when first introduced to the cask
   url 'https://storage.googleapis.com/sketch-plugin/Marvel.zip'
   appcast 'https://storage.googleapis.com/sketch-plugin/app.xml',
-          checkpoint: '82a6c7f0e343d95178e0e64360b84fe0338c6e80894d47fb85d8fc05322c8544'
+          checkpoint: '3358a74dd7c0688ef3d40d1340051455e70a9a8adf9b0ecaf93e443e80375925'
   name 'Marvel'
   homepage 'https://marvelapp.com/'
 
