69573a7d2b3724c09593dc270285f94a4b639f45
byelipk <byelipk@gmail.com>
2017-07-05 12:16:16 -0400

    Update framer to 9475

--- Casks/framer.rb
+++ Casks/framer.rb
@@ -1,11 +1,11 @@
 cask 'framer' do
-  version '9386'
-  sha256 '485fbe07bfaf8f7675b2c50075f5b51ff9293f6125246db69735ad58feb30487'
+  version '9475'
+  sha256 '89271d51287506d18c03d308a5eaf33c80fbd27dbf18694bc852789b474a37b9'
 
   # devmate.com/com.motif.framer was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.motif.framer/FramerStudio.zip'
   appcast 'https://updates.devmate.com/com.motif.framer.xml',
-          checkpoint: '2daebfdeb9371eb119eff4ce446ef8d931bd5db5b1fca2ffaf20a04a1077e78b'
+          checkpoint: 'cfa0c4116ced02b627e998cbfba1daf3a73939d9ad115c7854cbd0e3e89fe4ac'
   name 'Framer'
   homepage 'https://framer.com/'
 
