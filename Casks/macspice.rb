fbbfd244b677d84de4ca0a749f20a2c9961fd54e
commitay <commitay@users.noreply.github.com>
2017-07-05 22:28:16 +1000

    Update macspice to 3.1.16

--- Casks/macspice.rb
+++ Casks/macspice.rb
@@ -1,10 +1,10 @@
 cask 'macspice' do
-  version '3.1.15'
-  sha256 'c12699e694d415ef7711e45d3f348e84202188014e679d505a2d5f126c84f77c'
+  version '3.1.16'
+  sha256 '5ef9430b956b8a3795a7e605fb0d5ce235d55dbcd886f06fc4959ff61427cd39'
 
   url "http://www.macspice.com/mirror/binaries/v#{version}/MacSpice3f5.dmg"
   appcast 'http://www.macspice.com/AppCast-v2.xml',
-          checkpoint: '0d57f4a640d1d4991aab10d13a744be9d2afcec1b9ee1621ea5668f4964eb6a9'
+          checkpoint: '52a03cddd8a97bbec18832c2bbebccb8e8edfefc67b80c03f30b48662a007f6c'
   name 'MacSpice'
   homepage 'https://www.macspice.com/'
 
