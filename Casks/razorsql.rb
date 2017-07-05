c1921d7a6635edfb763eb84308c920c9daa93a26
Lance M <mightyfree@users.noreply.github.com>
2017-07-04 20:46:21 -0400

    Update razorsql
    Updated razorsql to version 7.3.0, replacing the SHA256 checksum. Added appcast and checkpoint.

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,8 +1,10 @@
 cask 'razorsql' do
-  version '7.2.6'
-  sha256 'cb106a25f05676741bc6b7dc92c009c0b863120a529c541268e06553e924c24c'
+  version '7.3.0'
+  sha256 'e1e3ae86d7496c4908cd7498eae8dc839f4e03b26e903518d9b0cff47974d106'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
+  appcast 'https://razorsql.com/updates.html',
+          checkpoint: '317f8f8bf00910f05a870e27c1bd6a529c6839b318d4b90e7538885a987b0df6'
   name 'RazorSQL'
   homepage 'https://razorsql.com/download_mac.html'
 
