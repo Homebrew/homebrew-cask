5ec047cb55bcb7a4648b3039da8505bc8906cf95
Lance M <mightyfree@users.noreply.github.com>
2017-07-06 04:26:05 -0400

    Update url verification

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -2,7 +2,7 @@ cask 'natron' do
   version '2.3.0'
   sha256 '882e7869e65f29fbe0b2c97adb0065a86bfe47f9f01622dbddd64deeba89a32a'
 
-  # ayera.dl.sourceforge.net/project/natron was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/natron was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/natron/Natron-#{version}.dmg"  
   appcast 'https://github.com/MrKepzie/Natron/releases.atom',
           checkpoint: '4d89bf0b2b8449a032f002135c79c38bba5110d46a8262f7c8a8fbd44254cfcb'
