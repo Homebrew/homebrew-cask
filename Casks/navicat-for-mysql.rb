5b3de81a7f674e374de6cf994612972840daba24
Lance M <mightyfree@users.noreply.github.com>
2017-07-06 03:58:23 -0400

    Update navicat-for-mysql to version 12.0.7
    Updated navicat-for-mysql to version 12.0.7 and replaced sha256 checksum. Modified appcast url and replaced checkpoint.

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,10 +1,10 @@
 cask 'navicat-for-mysql' do
-  version '12.0.5'
-  sha256 'a05751caa4469ac1e82586af1254964ebde1ceaedafe7f5e766ad45a022a7ab8'
+  version '12.0.7'
+  sha256 'b1083c4a4727879619cf62fb8259e1810909e6b58235ce5aaba843fa62c7ef69'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
-  appcast 'https://www.navicat.com/products/navicat-for-mysql-release-note',
-          checkpoint: '7ca5fe14f57dab0ae1e497c6a7f6b0b63a346c8e5aad929460796fc8709a81b9'
+  appcast 'https://www.navicat.com/products/navicat-for-mysql-release-note#M',
+          checkpoint: '59ee399fc001a0b69cad9f3d54f12c09649e14eedf97f3eded2010c0c9407f17'
   name 'Navicat for MySQL'
   homepage 'https://www.navicat.com/products/navicat-for-mysql'
 
