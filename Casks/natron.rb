1050860ee794be44e3191c8583fc3cfd4dbb6003
Lance M <mightyfree@users.noreply.github.com>
2017-07-06 04:21:15 -0400

    Update natron download url per sourceforge specs
    Updated natron download url per sourceforge specs

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -3,7 +3,7 @@ cask 'natron' do
   sha256 '882e7869e65f29fbe0b2c97adb0065a86bfe47f9f01622dbddd64deeba89a32a'
 
   # ayera.dl.sourceforge.net/project/natron was verified as official when first introduced to the cask
-  url "https://ayera.dl.sourceforge.net/project/natron/OSX/Universal/releases/Natron-#{version}.dmg"  
+  url "https://downloads.sourceforge.net/natron/Natron-#{version}.dmg"  
   appcast 'https://github.com/MrKepzie/Natron/releases.atom',
           checkpoint: '4d89bf0b2b8449a032f002135c79c38bba5110d46a8262f7c8a8fbd44254cfcb'
   name 'Natron'
