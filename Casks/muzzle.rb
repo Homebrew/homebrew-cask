0c7f3d985e10851c0c991e88a241c2c78663cd4d
Mark Phelps <mark.aaron.phelps@gmail.com>
2017-07-06 11:03:10 -0400

    Add Muzzle :latest
    Adding [Muzzle](https://muzzleapp.com/)

---
+++ Casks/muzzle.rb
@@ -0,0 +1,10 @@
+cask 'muzzle' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://muzzleapp.com/binaries/muzzle.zip'
+  name 'Muzzle'
+  homepage 'https://muzzleapp.com/'
+
+  app 'Muzzle.app'
+end
