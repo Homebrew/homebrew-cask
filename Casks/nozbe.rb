dfdddf087be18ff17e220bace058323f4bd1050e
ito <ito_ma@ap-com.co.jp>
2017-07-07 16:23:59 +0900

    update nozbe to v3.5.2

--- Casks/nozbe.rb
+++ Casks/nozbe.rb
@@ -1,6 +1,6 @@
 cask 'nozbe' do
-  version '3.4.0'
-  sha256 '32fbadbe6185c819696ac971ba840fa2b0cd638d3ddea5725623a0c3dfa7e1ee'
+  version '3.5.2'
+  sha256 '957a8b9d750dcfe07cf33d2d4d1a026b7c09c276688993e7d253179534f880e7'
 
   url "https://files.nozbe.com/#{version.no_dots}/Nozbe.app.zip"
   name 'Nozbe'
