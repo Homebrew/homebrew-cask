33aad187e42d364506d54d658fbaf443637221b4
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-07-07 09:06:15 -0400

    Update brackets to 1.10 (#36279)

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -1,11 +1,11 @@
 cask 'brackets' do
-  version '1.9'
-  sha256 '489846205d13eb5abe156e0ffd171f0dfaf2183d216e6165f26a84caae1002d5'
+  version '1.10'
+  sha256 'a4faac726671439590e462af46d228e97ed70ede2e101bbd195b1f03143b00db'
 
   # github.com/adobe/brackets was verified as official when first introduced to the cask
   url "https://github.com/adobe/brackets/releases/download/release-#{version}/Brackets.Release.#{version}.dmg"
   appcast 'https://github.com/adobe/brackets/releases.atom',
-          checkpoint: 'cf3c947a5903174f207a1251c0536d66f304d1af0a2e173f7e235496d866fc40'
+          checkpoint: '3559e75e79f9cbb64679435add8870e45f2c7c15daca08ef9e890a46920e38ee'
   name 'Brackets'
   homepage 'http://brackets.io/'
 
