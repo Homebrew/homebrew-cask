9b2e86cfa6d2b6c6caf02695073a3fd381a05658
Andrew Wade <awade@andrews-air.attlocal.net>
2017-07-07 00:09:14 -0700

    Update goodsync to 10.5.1

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.4.7'
-  sha256 'c62332e8c3c355cf0dce864fe023cc8a31b3fa22f88f4a9414b5966812d86c1e'
+  version '10.5.1'
+  sha256 '40eb93ddd65fb153b2c6fc332326381a198c9ad3c80de223a7102e840ef4b068'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   name 'GoodSync'
