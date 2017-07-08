306fd0766ac06be6b61fe60a7881107fa025b830
commitay <commitay@users.noreply.github.com>
2017-07-08 10:16:30 +1000

    Update vagrant to 1.9.7

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,11 +1,11 @@
 cask 'vagrant' do
-  version '1.9.6'
-  sha256 'bf3ed196cc12f9906d1d1dbe38c297d42e1de318f1ac0f7eb720894af9519141'
+  version '1.9.7'
+  sha256 '3ebb9bf1f7d7bb947da52c33cb540750156f4a9ba1c1697c07bb9317d42ea503'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
   appcast 'https://github.com/mitchellh/vagrant/releases.atom',
-          checkpoint: 'c459223d93951b2bdbdd1ff9b8ed09e098dc6377e60f90d241e138b0b8ffd0c1'
+          checkpoint: 'e2c4859e876a81fa46e17359d452a26f250ee859e43c5265c2a19ab88b376644'
   name 'Vagrant'
   homepage 'https://www.vagrantup.com/'
 
