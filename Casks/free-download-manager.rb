8011e29141fc8b7c0fadf2655e395cd0c0ddcf4f
commitay <commitay1@gmail.com>
2017-07-10 07:27:34 +1000

    Update free-download-manager.rb

--- Casks/free-download-manager.rb
+++ Casks/free-download-manager.rb
@@ -1,8 +1,8 @@
 cask 'free-download-manager' do
-  version :latest
-  sha256 :no_check
+  version '5.1'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url 'http://dn3.freedownloadmanager.org/5/5.1-latest/fdm.dmg'
+  url "http://dn3.freedownloadmanager.org/#{version.major}/#{version}-latest/fdm.dmg"
   name 'Free Download Manager'
   homepage 'http://www.freedownloadmanager.org/'
 
@@ -12,9 +12,9 @@ cask 'free-download-manager' do
 
   zap delete: [
                 '~/Library/Application Support/Free Download Manager',
-                '~/Library/Caches/org.freedownloadmanager.fdm*',
-                '~/Library/Preferences/org.freedownloadmanager.fdm5.plist',
-                '~/Library/Saved Application State/org.freedownloadmanager.fdm5.savedState',
-                '~/Library/LaunchAgents/org.freedownloadmanager.fdm5.helper.plist',
+                "~/Library/Caches/org.freedownloadmanager.fdm#{version.major}",
+                "~/Library/Preferences/org.freedownloadmanager.fdm#{version.major}.plist",
+                "~/Library/Saved Application State/org.freedownloadmanager.fdm#{version.major}.savedState",
+                "~/Library/LaunchAgents/org.freedownloadmanager.fdm#{version.major}.helper.plist",
               ]
 end
