e9d62fdc94b41f52e33b1783ec1acc05ab157f60
Victor Popkov <victor@popkov.me>
2017-07-09 21:44:18 +0300

    Update devonthink to 2.9.13

--- Casks/devonthink.rb
+++ Casks/devonthink.rb
@@ -1,11 +1,11 @@
 cask 'devonthink' do
-  version '2.9.12'
-  sha256 '3aa6866089f91b1d9d14598954443218d731f443627a2ac102649b51acbdf168'
+  version '2.9.13'
+  sha256 '77f9d48d262d9b96b3406b5ce2b346c7e437e579ad5bd693e21c56a024258c6d'
 
   # amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_Personal.app.zip"
   appcast 'http://www.devon-technologies.com/fileadmin/templates/filemaker/sparkle.php?product=217255&format=xml',
-          checkpoint: '3f5321f92ad6779d131217fde8396e1a7816c16abe15447ec4e58eecad9c0693'
+          checkpoint: '86968e1f3082d5c31f153dbbbbcef6986278f2ec63a8fb1168b7e4ef3e8273ef'
   name 'DEVONthink Personal'
   homepage 'https://www.devontechnologies.com/products/devonthink/devonthink-personal.html'
 
