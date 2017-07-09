e46a03b54a2482d4847ecafebd152ce0988d3c76
Jason Masker <jason@masker.net>
2017-07-09 19:42:49 -0400

    Update ngrok to latest (#36305)
    This URL actually points to the latest version of ngrok and has for
    every release after 2.0.19 from around March 2016.

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -1,9 +1,9 @@
 cask 'ngrok' do
-  version '2.2.6,4VmDzA7iaHb'
-  sha256 '54dd3706f628213931ae3566b63ef740ce97ef68351e51fcf55f08a8c9ae33d2'
+  version :latest
+  sha256 :no_check
 
   # bin.equinox.io was verified as official when first introduced to the cask
-  url "https://bin.equinox.io/c/#{version.after_comma}/ngrok-stable-darwin-amd64.zip"
+  url 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip'
   name 'ngrok'
   homepage 'https://ngrok.com/'
 
