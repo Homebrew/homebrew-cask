9e41220ad5551583a380c9f68c68ab14017bb3fb
lunaticare <lunaticare@users.noreply.github.com>
2017-07-09 17:44:40 +0300

    Update gnucash to 2.6.17-1 (#36303)

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,11 +1,11 @@
 cask 'gnucash' do
-  version '2.6.16-1'
-  sha256 'a1e0fe408b9bc34a9d7ecff1fdaf66846289032636e44b37f6ef18b71867aec8'
+  version '2.6.17-1'
+  sha256 'f60238bc7bced79bc50e223ba0d4047d3235e71a7f815a375745242977ecfdeb'
 
   # github.com/Gnucash/gnucash was verified as official when first introduced to the cask
-  url "https://github.com/Gnucash/gnucash/releases/download/#{version.major_minor_patch}/Gnucash-Intel-#{version}.dmg"
+  url "https://github.com/Gnucash/gnucash/releases/download/#{version.major_minor_patch}b/Gnucash-Intel-#{version}.dmg"
   appcast 'https://github.com/Gnucash/gnucash/releases.atom',
-          checkpoint: '4dc7df87ea304f84d38f6df81fc229f8168ee07de5694b1c0ad63edf06b553cc'
+          checkpoint: '3cf2598a33e81e50ebd801d8fb0c72b0a80cf921137e28f5e61e3b719d45a41f'
   name 'GnuCash'
   homepage 'https://www.gnucash.org/'
 
