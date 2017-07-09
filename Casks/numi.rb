46880f59a8170f9e27c92e8449bc3e17699824e5
commitay <commitay@users.noreply.github.com>
2017-07-09 22:45:02 +1000

    Update numi - version / url

--- Casks/numi.rb
+++ Casks/numi.rb
@@ -1,9 +1,9 @@
 cask 'numi' do
-  version '3.15.2,122:1496059169'
+  version '3.15.2'
   sha256 '16d6c2c0b8879d44217f0154f7cf5152b286c4c4a1049f9dbee76946719407b2'
 
   # dl.devmate.com/com.dmitrynikolaev.numi was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.dmitrynikolaev.numi/#{version.after_comma.before_colon}/#{version.after_colon}/Numi-#{version.after_comma.before_colon}.zip"
+  url 'http://dl.devmate.com/com.dmitrynikolaev.numi/Numi.zip'
   appcast 'http://updates.devmate.com/com.dmitrynikolaev.numi.xml',
           checkpoint: 'c36849ccf43d30f885d718ad155168bc999023253585672d1be1b5795e48236c'
   name 'Numi'
