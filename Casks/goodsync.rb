004fd429f5f805a1cf4f809fff20ecf6d3eff5a7
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-07-10 18:49:20 -0400

    Update goodsync to 10.5.2 (#36344)
    * Update goodsync to 10.5.2
    
    * Update goodsync.rb
    
    Introduce an appcast

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,8 +1,10 @@
 cask 'goodsync' do
-  version '10.5.1'
-  sha256 '40eb93ddd65fb153b2c6fc332326381a198c9ad3c80de223a7102e840ef4b068'
+  version '10.5.2'
+  sha256 'ee94b9715c6f7b8785f8c24c0da8f08426c1824b27eb6a140fb81c771414d050'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
+  appcast 'https://www.goodsync.com/download',
+          checkpoint: '7a1dbc58e629aba3f17544a7adeac0db1546fe3cf276b9a041cea10fb6a3b770'
   name 'GoodSync'
   homepage 'https://www.goodsync.com/'
 
