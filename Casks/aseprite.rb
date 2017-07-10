6aac610fe7ac76d71733b63e0f5e37a83115fcb4
commitay <commitay1@gmail.com>
2017-07-11 09:13:34 +1000

    version and url

--- Casks/aseprite.rb
+++ Casks/aseprite.rb
@@ -1,8 +1,8 @@
 cask 'aseprite' do
-  version '1.1.13'
-  sha256 '90939bc06f5694a512d233018ae12ce7ce3bed409e7164d4160dbfe16afc4609'
+  version '1.2-beta12'
+  sha256 'f142d30ea43cd4ac0c176e400b109627a772df48b54a3b4aa884567c93063ff0'
 
-  url "https://www.aseprite.org/downloads/Aseprite-v#{version}-trial-MacOSX.dmg"
+  url "https://www.aseprite.org/downloads/trial/Aseprite-v#{version}-trial-MacOSX.dmg"
   appcast 'https://github.com/aseprite/aseprite/releases.atom',
           checkpoint: '69daacf9be253265a763583cb67dc3c9e92078d6c056c1c736ec2d457444b594'
   name 'Aseprite'
