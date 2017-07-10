ce70f9aa085dd0367c01f68e366529d84f6ce493
Steven Conaway <steven.conaway@icloud.com>
2017-07-10 16:37:10 -0700

    Update Free Download Manager to use Latest Link (#36311)
    * Update Free Download Manager to use Latest Link
    
    Update Free Download Manager to use Latest Link, not a specific version. Also fixes errors with sha256 mismatches.
    
    * Fix Free Download Manager's homepage
    
    * Update free-download-manager.rb
    
    * update uninstall

--- Casks/free-download-manager.rb
+++ Casks/free-download-manager.rb
@@ -1,20 +1,30 @@
 cask 'free-download-manager' do
-  version '5.1.31'
-  sha256 '845964e7172ac10e297b6734270bfbee1c7263bd8c0a6f9688bb63c1c9e000bb'
+  version '5.1'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "http://files2.freedownloadmanager.org/#{version.major}/#{version.major_minor}-latest/fdm.dmg"
+  url "http://dn3.freedownloadmanager.org/#{version.major}/#{version}-latest/fdm.dmg"
   name 'Free Download Manager'
-  homepage "http://www.freedownloadmanager.org/landing#{version.major}.htm"
+  homepage 'http://www.freedownloadmanager.org/'
 
   depends_on macos: '>= 10.9'
 
   app 'Free Download Manager.app'
 
+  uninstall launchctl: [
+                         "org.freedownloadmanager.fdm#{version.major}",
+                         "org.freedownloadmanager.fdm#{version.major}.helper",
+                       ],
+            quit:      [
+                         "org.freedownloadmanager.fdm#{version.major}",
+                         "org.freedownloadmanager.fdm#{version.major}.launcher",
+                       ]
+
   zap delete: [
-                '~/Library/Application Support/Free Download Manager',
                 "~/Library/Caches/org.freedownloadmanager.fdm#{version.major}",
-                "~/Library/Preferences/org.freedownloadmanager.fdm#{version.major}.plist",
                 "~/Library/Saved Application State/org.freedownloadmanager.fdm#{version.major}.savedState",
-                "~/Library/LaunchAgents/org.freedownloadmanager.fdm#{version.major}.helper.plist",
+              ],
+      trash:  [
+                '~/Library/Application Support/Free Download Manager',
+                "~/Library/Preferences/org.freedownloadmanager.fdm#{version.major}.plist",
               ]
 end
