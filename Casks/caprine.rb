3a717f590e53255b9352a405d4b86e796e28eb4d
Gobinath Mallaiyan <i@gobinath.com>
2017-07-10 11:22:18 -0400

    Update caprine to 2.5.0

--- Casks/caprine.rb
+++ Casks/caprine.rb
@@ -1,10 +1,10 @@
 cask 'caprine' do
-  version '2.4.0'
-  sha256 'ec0dc7d6e5eff8e8569e501b34bd2c590525d1b02918ae7b18ece4881f356c98'
+  version '2.5.0'
+  sha256 'fef69ef1f09623ba429bf90b940aaea59f93e23225e0cb53f23702b7d3c14062'
 
   url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/caprine-#{version}-mac.zip"
   appcast 'https://github.com/sindresorhus/caprine/releases.atom',
-          checkpoint: '6790d11ddca8d12233616beef6353dac6374b3901539faadfdceec24009c9a83'
+          checkpoint: 'eeadbb7e6220e50e1140bac345bdbad0f31832d5e143af1839eb113d23f29541'
   name 'Caprine'
   homepage 'https://github.com/sindresorhus/caprine'
 
