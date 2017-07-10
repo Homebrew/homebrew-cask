49bcf78a90c42adedf0b5d4750223f92c0450207
bvgastel <bvgastel@bitpowder.com>
2017-07-11 00:48:04 +0200

    qt-creator: 4.3.0 -> 4.3.1 (#36340)

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator' do
-  version '4.3.0'
-  sha256 '37118c49b724d5986fdd052b1098e3d648224837fdc4a8ba4d8fd842dfd561e6'
+  version '4.3.1'
+  sha256 '0098a420d43578b064382a73a706bf145ed04e352277a9609ee2970b6146d546'
 
   url "http://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
