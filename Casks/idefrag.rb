346f67aa82e315bcdf9240598719691cdc62a52a
wickedsp1d3r <jussi@juss.it>
2017-07-10 10:08:09 +0300

    Update idefrag to 5.1.9

--- Casks/idefrag.rb
+++ Casks/idefrag.rb
@@ -3,8 +3,8 @@ cask 'idefrag' do
     version '5.1.3'
     sha256 '4b695c04f491b8f9f60a1fb43836164960f7d95f82aaa38d1e3b7dd4eacd7d5c'
   else
-    version '5.1.8'
-    sha256 '2e072ee9a7e3469bbb93b749ae01cc562a0b404dc46daa349edeeaeaede03ecc'
+    version '5.1.9'
+    sha256 '494c0e0be7340cdfeff3015db96dfd9d541c861a814d8ba8ded990b8c30b684a'
   end
 
   url "https://coriolis-systems.com/downloads/iDefrag-#{version}.dmg"
