e4b6fd3209c2eae33e4c78f65bd3a1fa46c3bc78
Thomas Orozco <thomas@orozco.fr>
2017-07-10 21:31:00 +0200

    Update aptible to 0.11.1 (#36329)

--- Casks/aptible.rb
+++ Casks/aptible.rb
@@ -1,6 +1,6 @@
 cask 'aptible' do
-  version '0.11.0+20170530075306,143'
-  sha256 '41b1fdd464c049105e0fadfc18b6041aa88ba17ab903c84280e584c74e9ecea1'
+  version '0.11.1+20170706115813,147'
+  sha256 'd7a54df30ac4423e35c7c9769a699c9d1eac04bd5438b163691014e20ffe3e08'
 
   # omnibus-aptible-toolbelt.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://omnibus-aptible-toolbelt.s3.amazonaws.com/aptible/omnibus-aptible-toolbelt/master/#{version.after_comma}/pkg/aptible-toolbelt-#{version.before_comma.sub('+', '%2B')}-mac-os-x.10.11.6-1.pkg"
