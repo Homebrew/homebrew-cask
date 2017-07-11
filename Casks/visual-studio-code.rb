576fe11eb6cee0f9ea17f65774e952decd8510dd
Mateusz Paluszkiewicz <aifam96@gmail.com>
2017-07-11 22:45:09 +0200

    Update visual-studio-code to 1.14.0,b8cd5b9556d8b70ea560d35b903422363f6c5c40

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.13.1,379d2efb5539b09112c793d3d9a413017d736f89'
-  sha256 '2f22d4e15987f59cd1b224d26e9e9668b305165a42b0ad0a7c9093d9403255bb'
+  version '1.14.0,b8cd5b9556d8b70ea560d35b903422363f6c5c40'
+  sha256 '16ddda8ba1172ea7c221fe25a4eb09a823c31fd3649691f0e2356f986d6e92c3'
 
   # az764295.vo.msecnd.net was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
