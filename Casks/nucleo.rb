5c34b47076e1e9ccdb5e6b4cad78e71e12f56c4c
naotone <me@naotone.com>
2017-07-11 17:43:13 -0400

    Update nucleo to 2.0.5

--- Casks/nucleo.rb
+++ Casks/nucleo.rb
@@ -1,11 +1,11 @@
 cask 'nucleo' do
-  version '2.0.2'
-  sha256 '89641365b6de691d6761cda630d1ffe034c39cadbcdf99a6a02154a229ed2683'
+  version '2.0.5'
+  sha256 '0a8a26891d938f9406e71126407e7d277a1dcd6429aff9d842cd6a4c390f5d91'
 
   # s3-us-west-2.amazonaws.com/nucleo-app-releases was verified as official when first introduced to the cask
   url "https://s3-us-west-2.amazonaws.com/nucleo-app-releases/mac/Nucleo_#{version}.zip"
   appcast 'https://nucleoapp.com/updates',
-          checkpoint: 'd083d6ad0f542ccabc982f8f5fe19c6bfb86d513eac739302235d6d0bf4f49e2'
+          checkpoint: '5182b46d00306264b2047e1f207c2a22c5226cc20e8d122bde8bd6bb018ade31'
   name 'Nucleo'
   homepage 'https://nucleoapp.com/'
 
