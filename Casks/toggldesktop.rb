90c816682feb43268ba8c1c636d805f7f8009af9
Gobinath Mallaiyan <i@gobinath.com>
2017-07-12 18:42:43 -0400

    Update toggldesktop to 7.4.53

--- Casks/toggldesktop.rb
+++ Casks/toggldesktop.rb
@@ -1,11 +1,11 @@
 cask 'toggldesktop' do
-  version '7.4.47'
-  sha256 '1dd41946486763c767aa25ecf425e3cbf529c7bd95060a88776655581df627f7'
+  version '7.4.53'
+  sha256 'be99aec956a5c0c462abb0aeac86433c0d6f7b9fd6cf52de3b30d58453c04c40'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_stable_appcast.xml',
-          checkpoint: '981b83d74b75daa47e4f07aedd1bf08fadef51542bec3a512b8ad31761ec3db0'
+          checkpoint: '9454d90a2bdf976c0ad4cf88495e6324e1c36c4a4c564335d2d4df2580b0c8a1'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com/'
 
