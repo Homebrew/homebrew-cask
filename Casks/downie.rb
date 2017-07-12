c8a01106503e4977fa5793cc8375fd545e05cfd5
commitay <commitay@users.noreply.github.com>
2017-07-12 13:16:13 +1000

    Update downie to 2.9.5,1505

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,10 +1,10 @@
 cask 'downie' do
-  version '2.9.4,1496'
-  sha256 '723e0d9d0dfe8dc553f80c870818a0f3a0e6045997c12178217cf7d463e528f2'
+  version '2.9.5,1505'
+  sha256 '41025d496ab9eafcd1f1edb9e5c9aa0fcbad32e444030333339d75df905b986e'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.after_comma}.zip"
   appcast 'https://trial.charliemonroe.net/downie/updates_2.0.xml',
-          checkpoint: '824fcfcc1368b40fc75e67b73f4b669f787fc74f0b82c59b2fed8d555c2489e2'
+          checkpoint: '8e63f501445ba7b6dedc74f1724d1a23c091bc45111002edc7bfc660fc311b6e'
   name 'Downie'
   homepage 'https://software.charliemonroe.net/downie.php'
 
