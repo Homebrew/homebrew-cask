53e92ef7d4085cf315698014a9d6794b7a3678df
Victor Popkov <victor@popkov.me>
2017-07-13 22:01:26 +0300

    Update dollydrive to 3.96,39625

--- Casks/dollydrive.rb
+++ Casks/dollydrive.rb
@@ -1,10 +1,10 @@
 cask 'dollydrive' do
-  version '3.94.12'
-  sha256 '654c2b0d5a6eab71e6bf60769ace36f5c24b287d00ce8f5b6fcb47ac904825ee'
+  version '3.96,39625'
+  sha256 '87d3925035f8af2a410c035f426d1e47115d119fb08f4a5e14e4972399263a71'
 
-  url "http://dollydrive.com/download-center/dollydrive/DollyDrive_#{version.major_minor}_#{version.no_dots}_CERTIFIED.zip"
+  url "http://dollydrive.com/download-center/dollydrive/DollyDrive_#{version.before_comma}_#{version.after_comma}_CERTIFIED.zip"
   appcast "http://www.dollydrive.com/dolly#{version.major}.xml",
-          checkpoint: '3ff4fe0e5a38053b5179774599043aa76b718d6af4286d28ace8a8bce93524d1'
+          checkpoint: '4e4d8683c5b445f35a83f963c82963d70b68bc16b8235d0db37788eda253de88'
   name 'DollyDrive'
   homepage 'http://www.dollydrive.com/'
 
