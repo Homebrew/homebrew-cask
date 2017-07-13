94bbeace05045b25567f1409a0537743a3c915f1
Victor Popkov <victor@popkov.me>
2017-07-13 21:19:45 +0300

    Update championify to 2.0.5

--- Casks/championify.rb
+++ Casks/championify.rb
@@ -1,10 +1,10 @@
 cask 'championify' do
-  version '2.0.4'
-  sha256 '9209be663ef541b49012356c2797b6b499235fa3ec1ee27d2eaa0f47cbfe6a64'
+  version '2.0.5'
+  sha256 '535a166078e499789db6ed714d552620e9558e8152e3caf2a9a43a036fae8070'
 
   url "https://github.com/dustinblackman/Championify/releases/download/#{version}/Championify-OSX-#{version}.dmg"
   appcast 'https://github.com/dustinblackman/Championify/releases.atom',
-          checkpoint: 'abac4f5d49633034571c75bf18900ad2df0197de54eebab5ef4a0afcc6fd33c6'
+          checkpoint: 'e7d723dec6246c502e37fc8e4257c5d96b191601392afa8c47f2982ce6a697e8'
   name 'Championify'
   homepage 'https://github.com/dustinblackman/Championify/'
 
