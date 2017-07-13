fd4b91e8e8451b636d6d4cbb40e1845c75c91b0b
Victor Popkov <victor@popkov.me>
2017-07-14 02:50:05 +0300

    Update nordvpn to 3.2.0 (#36484)

--- Casks/nordvpn.rb
+++ Casks/nordvpn.rb
@@ -1,10 +1,10 @@
 cask 'nordvpn' do
-  version '3.1.5'
-  sha256 '54cc1de9e07d41a487bd175645a70a625e2eefb59202821bbde4bc0a54c9700f'
+  version '3.2.0'
+  sha256 '2b588d73fbd73331b3049564f8fdd33f1ff735fb6bc48536633d452eac068f66'
 
   url 'https://nordvpn.com/api/osxapp/latest'
   appcast 'https://downloads.nordvpn.com/apps/osx/update.xml',
-          checkpoint: '6c4384ac9a8b8c669849489f6fdbc3b0a664aa9e483e81aa9b78db3f3befe318'
+          checkpoint: 'e25bfa25342846e846f42f10b60d7a5923175d6580fd33e93feed626a18f78b1'
   name 'NordVPN'
   homepage 'https://nordvpn.com/'
 
