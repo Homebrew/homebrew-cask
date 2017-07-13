2414e9258bad1ba745e12e1f62b8193fe8db7844
Victor Popkov <victor@popkov.me>
2017-07-14 01:19:08 +0300

    Update hockey to 2.0.19,503 (#36459)

--- Casks/hockey.rb
+++ Casks/hockey.rb
@@ -1,10 +1,10 @@
 cask 'hockey' do
-  version '2.0.18,501'
-  sha256 'ff1ecf90b86ead33fc84d9c9957b8be53eff441663ab10916c5f11adc5aca3ee'
+  version '2.0.19,503'
+  sha256 '4f4b4c12921242c61e9c9bf12414a4bbb550bd1539973adbc93c5c60fb6181bb'
 
   url "https://rink.hockeyapp.net/api/2/apps/67503a7926431872c4b6c1549f5bd6b1/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/67503a7926431872c4b6c1549f5bd6b1',
-          checkpoint: '956fa50f0b2eba951921dee825f4fb4fdf4ebe661386dc1e4c2fc4f906b8bb10'
+          checkpoint: 'a9d73e714a0199766bf9860903ab810b84e5f4c03c4606a970660108cf2e1346'
   name 'HockeyApp'
   homepage 'https://hockeyapp.net/releases/mac/'
 
