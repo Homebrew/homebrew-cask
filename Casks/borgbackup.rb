85d6e9135d5c956b1c08950c74a0e67e6b6e3eec
giannitm <giannitm@users.noreply.github.com>
2017-07-13 23:03:15 +0100

    Update borgbackup to 1.0.10

--- Casks/borgbackup.rb
+++ Casks/borgbackup.rb
@@ -5,7 +5,7 @@ cask 'borgbackup' do
   # github.com/borgbackup/borg was verified as official when first introduced to the cask
   url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
   appcast 'https://github.com/borgbackup/borg/releases.atom',
-          checkpoint: '8a9d7902806cc859f45c0d9de3fb00fbbeda6b72e69c9578311f3201a8eff6cd'
+          checkpoint: '5d4f3de5d1e113eb2663fcd107fcafa262a2b74ae2cbd36a61d73beb4ab87aa5'
   name 'BorgBackup'
   homepage 'https://borgbackup.readthedocs.io/en/stable/'
   gpg "#{url}.asc", key_id: '51F78E01'
