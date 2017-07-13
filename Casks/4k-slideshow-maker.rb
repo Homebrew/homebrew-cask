0fb289738b21ef1072876dc420340eb9e9017b35
giannitm <giannitm@users.noreply.github.com>
2017-07-13 15:17:17 +0100

    Update 4k-slideshow-maker to 1.6.2.948

--- Casks/4k-slideshow-maker.rb
+++ Casks/4k-slideshow-maker.rb
@@ -1,10 +1,10 @@
 cask '4k-slideshow-maker' do
-  version '1.6.1.938'
-  sha256 '86f893e8b2e5e120e8c01fff359c73c3d58f8cc576d3bdefb016301c0c4812ca'
+  version '1.6.2.948'
+  sha256 '46b789c27de3f65bddd475b79d59d18e2b86229d622e3b3ec446fceca5731466'
 
   url "https://downloads2.4kdownload.com/app/4kslideshowmaker_#{version.major_minor}.dmg"
   appcast 'https://www.4kdownload.com/download',
-          checkpoint: '9fe8dcb8ad4126c3e296b842be3e03d76a8eb9957f00881354c93d663cc4e77d'
+          checkpoint: '49ff507a887439ff101f36b84c58788834a7a4a7638127609c8a49fa6278b627'
   name '4K Slideshow Maker'
   homepage 'https://www.4kdownload.com/products/product-slideshowmaker'
 
