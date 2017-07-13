3c7884045c0498e2ee3790cc55c57083f1329d86
Victor Popkov <victor@popkov.me>
2017-07-13 22:04:02 +0300

    Update fly to 3.3.2

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,10 +1,10 @@
 cask 'fly' do
-  version '3.3.1'
-  sha256 'c33c381bb675fc2e777ecbe1f7dff08f30e3ffec639727ef57916983b3f9f702'
+  version '3.3.2'
+  sha256 '97f3ec170dce41739b9cf8002b25402457410ccbfd057e378f8776a7f1bf290d'
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly_darwin_amd64"
   appcast 'https://github.com/concourse/concourse/releases.atom',
-          checkpoint: '36f927788e8f81fdbf54d8d6efde0e41c4e69bdfdf8daba0e680acffe5c2fed6'
+          checkpoint: '288168c1a6cb51917aa4bf9f93cab31f14e99629891c8f3b18f09bd1f632fe94'
   name 'fly'
   homepage 'https://github.com/concourse/fly'
 
