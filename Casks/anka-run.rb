002f32a8d6d155ff44926eec55dfe35c5b79ae41
giannitm <giannitm@users.noreply.github.com>
2017-07-13 19:55:00 +0100

    Update anka-run to 1.0_40

--- Casks/anka-run.rb
+++ Casks/anka-run.rb
@@ -1,12 +1,12 @@
 cask 'anka-run' do
-  version '1.0_39'
-  sha256 '76e6ed733927e79a150d4ee6a301d61aab450dc93aeae3c68feba4b7e0884f65'
+  version '1.0_40'
+  sha256 '7bf52d013a36e7d50dbf5eec94ef6b675b5a02470282e758ef8ca796f57d29dd'
 
   # d1efqjhnhbvc57.cloudfront.net was verified as official when first introduced to the cask
   url "https://d1efqjhnhbvc57.cloudfront.net/AnkaRun-#{version}.pkg",
       referer: 'https://veertu.com/download-anka-run/'
   appcast 'https://ankadoc.bitbucket.io/release-notes/index.html',
-          checkpoint: '09509132b813d53fb3367506c9e71750765c7df4ba121d7b82dafaee6fcc2c9d'
+          checkpoint: '422bcfb1601ba1d65be38017b12b058d1fc2f333d8f8d5f0f4a0a1c3dff36344'
   name 'Veertu Anka Run'
   homepage 'https://veertu.com/'
 
