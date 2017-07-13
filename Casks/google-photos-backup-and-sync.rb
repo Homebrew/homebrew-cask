355a2718b97783a758347ba9254c0bfa9e0e51b8
Lee Hanxue <hanxue.developer@gmail.com>
2017-07-13 08:23:27 +0800

    New Cask Google Photos Backup and Sync

---
+++ Casks/google-photos-backup-and-sync.rb
@@ -0,0 +1,12 @@
+cask 'google-photos-backup-and-sync' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://dl.google.com/drive/InstallPhotosBackupAndSync.dmg'
+  name 'Google Photos Backup And Sync'
+  homepage 'https://photos.google.com/apps'
+
+  depends_on macos: '>= :mavericks'
+
+  app 'Backup and Sync.app'
+end
