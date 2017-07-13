e1e3f127dfca20500a93ed362751fbb422cbe59f
Victor Popkov <victor@popkov.me>
2017-07-13 23:42:14 +0300

    Update join-together to 7.7.3

--- Casks/join-together.rb
+++ Casks/join-together.rb
@@ -1,10 +1,10 @@
 cask 'join-together' do
-  version '7.7.2'
-  sha256 '3c674140d33f80dcf3a3748faa876deac4e05183d5ffbe3e483a917a9281d336'
+  version '7.7.3'
+  sha256 '8b8771a048096ba27d01fe334673b990483cb957d7836895ca18f295dd222e30'
 
   url "https://dougscripts.com/itunes/scrx/jointogether#{version.no_dots}.zip"
   appcast 'https://dougscripts.com/itunes/itinfo/jointogether_appcast.xml',
-          checkpoint: '617ab4b1fd88e0ef7bf9fab0bbac4982b7d58a80a5a37115c724e39ec0390e2c'
+          checkpoint: '2b85a0d3b4ca1b61d84c4c397d738f81864682781eba8adb1f5b8d0f86b5af7d'
   name 'Join Together'
   homepage 'https://dougscripts.com/apps/jointogetherapp.php'
 
