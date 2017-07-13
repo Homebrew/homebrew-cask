ce7f6d08e0d06b4026aae32b4ec0925220def545
commitay <commitay@users.noreply.github.com>
2017-07-13 11:19:42 +1000

    update comment url

--- Casks/gpxsee.rb
+++ Casks/gpxsee.rb
@@ -2,7 +2,7 @@ cask 'gpxsee' do
   version '4.9'
   sha256 '60bd08e8d1097ecc6d61cda58ba0b0ef8ccae1154366ddc9a91446596b3922f8'
 
-  # sourceforge.net was verified as official when first introduced to the cask
+  # sourceforge.net/gpxsee/Mac%20OS%20X was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/gpxsee/rss?path=/Mac%20OS%20X',
           checkpoint: 'a3f47ed0f7fe4eb7781b0259c5c38efa39775bf0f21e9a4948b1ebdbc44e8053'
