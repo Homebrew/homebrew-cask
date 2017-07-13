e3f605b6f17d1b1b83e17c39da6a75b026131b02
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-07-13 01:00:46 -0300

    Update tweeten to 4.0.3

--- Casks/tweeten.rb
+++ Casks/tweeten.rb
@@ -1,11 +1,11 @@
 cask 'tweeten' do
-  version '4.0.2'
-  sha256 '1700d35f74ad3fcb6f557ef8d2a08be8cd4a066f17ed3409f78932edef7894e2'
+  version '4.0.3'
+  sha256 '4aebecef707c27f63491dca8f1c44b7ed7f72fd4dfab23213b8e2cd1dafb2498'
 
   # github.com/MehediH/Tweeten was verified as official when first introduced to the cask
   url "https://github.com/MehediH/Tweeten/releases/download/#{version}/tweeten-darwin-x64.zip"
   appcast 'https://github.com/MehediH/Tweeten/releases.atom',
-          checkpoint: 'd8922d634d40f461260552dabb6922a97f68a185dea5e6633493cccb5511d69a'
+          checkpoint: '5070efadbd26c4d6793514a961f6f282f6c2c6f0539d99c654dd026f67d03a68'
   name 'Tweeten'
   homepage 'http://tweetenapp.com/'
 
