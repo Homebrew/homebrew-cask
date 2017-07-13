55236708a5259bcc2834a20546c67c2f02ca99ba
commitay <commitay@users.noreply.github.com>
2017-07-13 12:05:28 +1000

    Update google-backup-and-sync (#36419)

--- Casks/google-backup-and-sync.rb
+++ Casks/google-backup-and-sync.rb
@@ -6,5 +6,8 @@ cask 'google-backup-and-sync' do
   name 'Google Backup and Sync'
   homepage 'https://www.google.com/drive/download/'
 
+  conflicts_with cask: 'google-photos-backup-and-sync'
+  depends_on macos: '>= :mavericks'
+
   app 'Backup and Sync.app'
 end
