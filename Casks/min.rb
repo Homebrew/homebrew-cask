18940663201852c6d2839fddff103417112fdab6
Victor Popkov <victor@popkov.me>
2017-07-14 03:18:51 +0300

    Update min to 1.6.0 (#36478)
    * Update min to 1.6.0
    
    * update checkpoint

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,11 +1,11 @@
 cask 'min' do
-  version '1.5.1'
-  sha256 '702adc38b323ce68032fca433126248af55d08f6d73098a3b1ce89f4285528d0'
+  version '1.6.0'
+  sha256 '33259c28da7eb86a50df2a7fc7e890060dabc6f69956da2480e06adbbc6df327'
 
   # github.com/minbrowser/min was verified as official when first introduced to the cask
   url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/minbrowser/min/releases.atom',
-          checkpoint: 'cb4a0fa6ec7b562c31aa9d218ca819db68a8904184cdddd9205ecad33349e8b3'
+          checkpoint: '30c8d1934776833221158a4b0e2ffb1f0e38e21806309f3315119c0597c53939'
   name 'Min'
   homepage 'https://minbrowser.github.io/min/'
 
