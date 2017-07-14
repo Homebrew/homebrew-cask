59db2ac2cd4a321e6d03dd062587b0ec89ebb359
giannitm <giannitm@users.noreply.github.com>
2017-07-15 00:02:42 +0100

    Update context-free to 3.0.11.3

--- Casks/context-free.rb
+++ Casks/context-free.rb
@@ -1,10 +1,10 @@
 cask 'context-free' do
-  version '3.0.11'
-  sha256 'ecc71fe3b9f7f589c1bd55fa551a4b0f5caaa9660770eb516fa148ce5d04ef87'
+  version '3.0.11.3'
+  sha256 '5baae1cf7487ea0902f781f0891d3f7dc2c01071ab9546a1e2523676ea45c0da'
 
   url "http://www.contextfreeart.org/download/ContextFree#{version}.dmg"
   appcast 'https://github.com/MtnViewJohn/context-free/releases.atom',
-          checkpoint: '6b36b78fb8b7843415e0c8daff96faf420896c32516cc364269f0263dba19080'
+          checkpoint: 'e870c4c9ad2c4e7dcd36b196f0008000a4f821cf6f86a55237067f37769f809b'
   name 'Context Free'
   homepage 'https://www.contextfreeart.org/'
 
