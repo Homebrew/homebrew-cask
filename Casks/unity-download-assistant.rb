8c36d6aca8d52fbcac24f9c08b1b33b1cc400922
Oran Shuster <thristle666@gmail.com>
2017-07-15 18:24:37 +0300

    Update unity-download-assistant to 2017.1.0f3,472613c02cf7 (#36553)

--- Casks/unity-download-assistant.rb
+++ Casks/unity-download-assistant.rb
@@ -1,6 +1,6 @@
 cask 'unity-download-assistant' do
-  version '5.6.0f3,497a0f351392'
-  sha256 '5dcd1f56ce7d32c84238354a1cd40f797f224e4012d3ea8ab9ee6e3bac4c1f1c'
+  version '2017.1.0f3,472613c02cf7'
+  sha256 'b0f30d984884f1ac90072334a5e98f97f0e42ecb93dac7d592b26ec750769674'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/UnityDownloadAssistant-#{version.before_comma}.dmg"
   name 'Unity'
