351c27030c5bb02994db455e86269e3ddc9d1f23
giannitm <giannitm@users.noreply.github.com>
2017-07-15 16:10:07 +0100

    Update doomsday-engine to 2.0.2

--- Casks/doomsday-engine.rb
+++ Casks/doomsday-engine.rb
@@ -1,11 +1,11 @@
 cask 'doomsday-engine' do
-  version '2.0.1'
-  sha256 'c4dfbfdc452bb6d1552a3ab9c5da0ab74352ed044d783ed0e7729668a2a12f83'
+  version '2.0.2'
+  sha256 'dfd6e8a0399bc5a1a828b43254160c5523edbe74077fd0668b9f3bc6637be45a'
 
   # sourceforge.net/deng was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/deng/doomsday_#{version}_x86_64.dmg"
   appcast 'https://sourceforge.net/projects/deng/rss',
-          checkpoint: '4df660f7418c2dfd1be9d923695708b44d2a0353e240c4d25f2930f88cb01e73'
+          checkpoint: 'cc90b05250432b8e38a3637fe15917cd4cc66662916e04fea7ad7f0c21db76db'
   name 'Doomsday Engine'
   homepage 'https://dengine.net/'
 
