96d77fa8bf152b4dfbb8cf87bf9800fe8cb94fae
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-07-15 19:30:40 -0400

    Update sabaki to 0.31.2 (#36583)

--- Casks/sabaki.rb
+++ Casks/sabaki.rb
@@ -1,11 +1,11 @@
 cask 'sabaki' do
-  version '0.31.1'
-  sha256 'c25e245dd68154e4c47e6db23908b98e4113720d200b825f64eb656fbe8f336a'
+  version '0.31.2'
+  sha256 'b99b480ede7bb352d12fd0789a791ffb691dcba435ee54203d1de3c766dfbf2c'
 
   # github.com/yishn/Sabaki was verified as official when first introduced to the cask
   url "https://github.com/yishn/Sabaki/releases/download/v#{version}/sabaki-v#{version}-mac-x64.7z"
   appcast 'https://github.com/yishn/Sabaki/releases.atom',
-          checkpoint: '4b128c81b2addc5ba26e3fd6d174abe5f4db05f41d693f3d77c88163c24b3c81'
+          checkpoint: '98d940460e29b4092294cc759f5c917e51d474ee6ddd8a67ae7404400c05f6a0'
   name 'Sabaki'
   homepage 'http://sabaki.yichuanshen.de/'
 
