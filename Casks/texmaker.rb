f096622477032e9a81663f2be48b622725d249ea
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2017-07-15 16:11:14 +0000

    Update texmaker to 5.0

--- Casks/texmaker.rb
+++ Casks/texmaker.rb
@@ -1,10 +1,10 @@
 cask 'texmaker' do
-  version :latest
-  sha256 :no_check
+  version '5.0'
+  sha256 '9e8705cee04ff515c2daaf4b26b9bd918cf666836ec25a370456e35d2b0cc829'
 
-  url 'http://www.xm1math.net/texmaker/TexmakerMacosxLion.zip'
+  url "http://www.xm1math.net/texmaker/assets/files/texmaker-#{version}.dmg"
   name 'Texmaker'
   homepage 'http://www.xm1math.net/texmaker/'
 
-  app 'TexmakerMacosxLion/texmaker.app'
+  app 'texmaker.app'
 end
