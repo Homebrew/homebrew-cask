9ba45b48d86abe0293b1065b00dc7f84f8fdfc5f
commitay <commitay@users.noreply.github.com>
2017-07-15 23:13:25 +1000

    Update sdformatter.rb

--- Casks/sdformatter.rb
+++ Casks/sdformatter.rb
@@ -1,14 +1,14 @@
 cask 'sdformatter' do
-  version '5.0.0'
+  version '0.5.0.0'
   sha256 '4dba087a5a639d12b9e34666834c1b9f09914e92dc0aec20bd9f43e2fffb1ba9'
 
-  url 'https://www.sdcard.org/downloads/formatter_4/eula_mac/InstallSD_CardFormatter0500.mpkg'
+  url "https://www.sdcard.org/downloads/formatter_4/eula_mac/InstallSD_CardFormatter#{version.no_dots}.mpkg"
   name 'SD Formatter'
   homepage 'https://www.sdcard.org/'
 
-  depends_on macos: '>= :leopard'
-
-  pkg 'InstallSD_CardFormatter0500.mpkg'
+  pkg "InstallSD_CardFormatter#{version.no_dots}.mpkg"
 
   uninstall pkgutil: 'com.tuxera.pkg.SD_Card_Formatter'
+
+  zap delete: '~/Library/Caches/com.tuxera.SDCardFormatter'
 end
