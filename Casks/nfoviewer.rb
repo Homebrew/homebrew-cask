e1412864267f6b173d8504cfb4a8604789427573
nrlquaker <ustynov.vy@gmail.com>
2017-07-16 11:15:12 +0300

    Add NFOviewer 0.3 beta 3

---
+++ Casks/nfoviewer.rb
@@ -0,0 +1,11 @@
+cask 'nfoviewer' do
+  version '0.3 beta 3'
+  sha256 'f0f3bc2879bd9952c85c538cb9b85aee90bd66c91464269dd820053129b60a3b'
+
+  url 'https://downloads.sourceforge.net/blockart/NFOViewer%20(0.3%20beta%203).zip'
+  name 'NFOViewer'
+  homepage 'http://blockart.sourceforge.net/'
+
+  app 'NFOViewer.app'
+  font 'ProFont/ProFontWindows.ttf'
+end
