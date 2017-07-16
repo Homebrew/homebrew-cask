c613103800ac00abfcf2bc513f81ac58a094b5a4
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-07-16 20:08:54 +0100

    Update go-server to 17.7.0-5147 (#36611)

--- Casks/go-server.rb
+++ Casks/go-server.rb
@@ -1,11 +1,11 @@
 cask 'go-server' do
-  version '17.5.0-5095'
-  sha256 '8f2ed15fa4e20918a2540f45678beb0d7f956d8586524ed58ac9f0fedaecf7d6'
+  version '17.7.0-5147'
+  sha256 '4c79237b7ba597d2c0bd3f1663965dac0ea31838d73a40bc9040e47e9fd1c269'
 
   # download.gocd.io/binaries was verified as official when first introduced to the cask
   url "https://download.gocd.io/binaries/#{version}/osx/go-server-#{version}-osx.zip"
   appcast 'https://github.com/gocd/gocd/releases.atom',
-          checkpoint: '4e4f162d063d75ea1ff1642a0c0544cf6c628b1a915329e8c5fbf88da2dd6ce8'
+          checkpoint: 'f1cba4692ca4f0878fd95f276a55c43a4c2a435a96eae547070ac430689e19f1'
   name 'Go Server'
   homepage 'https://www.gocd.org/'
 
