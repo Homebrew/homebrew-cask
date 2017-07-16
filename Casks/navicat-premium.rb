af284d7d06053910ca65541f7fc76710d35d16ee
commitay <commitay@users.noreply.github.com>
2017-07-16 11:21:34 +1000

    Update navicat-premium to 12.0.9 (#36579)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium' do
-  version '12.0.7'
-  sha256 'f9618284898d29e5eafc4fc0636900a74a60c4d95e37382bb48112f56788b3d3'
+  version '12.0.9'
+  sha256 '3c940b7855a29260f19b4fe51f2bc16a78295900631259fd639ad1ebbe972167'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   name 'Navicat Premium'
