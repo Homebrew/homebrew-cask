8587d8846a512e7bd746765531864a3437142bc7
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-07-17 16:41:31 +0100

    Update imitone to 0.9.1 (#36654)

--- Casks/imitone.rb
+++ Casks/imitone.rb
@@ -1,10 +1,10 @@
 cask 'imitone' do
-  version '0.9.0d'
-  sha256 'b88be27f7f2a349f387d0a01ba9c60ae9ac344b93d3f64995eed7c41dc35a432'
+  version '0.9.1'
+  sha256 '6a999d3c4f134fb265b61278a24a3bb4bd849dc749f58ee9f538e1214b8869a7'
 
   url "https://imitone.com/beta/imitone-#{version}.dmg"
   appcast 'https://imitone.com/beta/',
-          checkpoint: '8d9ae178bbba1ed3dc26b07fc9d16a907eefc2827f9e091a8c21cd3085b69544'
+          checkpoint: 'd24a5abc0f3ffb9751b922e53746147d66983677043b85221560b3951d0a83f7'
   name 'imitone'
   homepage 'https://imitone.com/'
 
