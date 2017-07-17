8a3157aa4a6c13eb86396f1b2c0364fbde8ef561
commitay <commitay@users.noreply.github.com>
2017-07-17 11:21:49 +1000

    Update google-earth-pro to 7.1.8.3036

--- Casks/google-earth-pro.rb
+++ Casks/google-earth-pro.rb
@@ -1,6 +1,6 @@
 cask 'google-earth-pro' do
-  version '7.3.0.3827'
-  sha256 '85e49d4a377b148a4529f53624114647d6da9714074cc8b2c147ed152f2d25a0'
+  version '7.1.8.3036'
+  sha256 '6eff03463b4ae435d7e27a5501352e85ed7b92eadafbe341c3b88784f6d4fd4e'
 
   url 'https://dl.google.com/earth/client/advanced/current/GoogleEarthProMac-Intel.dmg'
   name 'Google Earth Pro'
