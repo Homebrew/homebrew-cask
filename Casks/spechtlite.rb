8fe99f3a20d8746b86abc2880ec63229269ffc10
ZEO <nieyujiang@gmail.com>
2017-07-18 18:50:24 +0800

    Update spechtlite.rb

--- Casks/spechtlite.rb
+++ Casks/spechtlite.rb
@@ -1,10 +1,10 @@
 cask 'spechtlite' do
-  version '0.10.3'
-  sha256 '72c33be92eb3b111f62264403bfe56233f3a6777cbad9e9ea392630080915384'
+  version '0.10.6'
+  sha256 '03c25351b7a10dc6f8e09e39293f9b2b54b3720caaba03a863dbc3f324611b1c'
 
   url "https://github.com/zhuhaow/SpechtLite/releases/download/#{version}/SpechtLite.zip"
   appcast 'https://github.com/zhuhaow/SpechtLite/releases.atom',
-          checkpoint: 'b378177850d4adc0f083de9d3b44bcc2ff71cd043ea66090b5a9ebf0859c6a9a'
+          checkpoint: '100e05cc7e529b90e2ad52a5a0deb4cf35ad525514028f200253b9271db887f7'
   name 'SpechtLite'
   homepage 'https://github.com/zhuhaow/SpechtLite'
 
