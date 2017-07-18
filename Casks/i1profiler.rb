320ba8fb30fca9eb0930632c204b8be302188f42
commitay <commitay@users.noreply.github.com>
2017-07-19 08:43:39 +1000

    update version and url

--- Casks/i1profiler.rb
+++ Casks/i1profiler.rb
@@ -1,12 +1,12 @@
 cask 'i1profiler' do
-  version '1.7.1'
+  version '1.7.1,1808'
   sha256 '8b6a4c2171e16abc44708c58868fa462f3a86ab8a1911957bdddd3985eb2ca12'
 
-  url 'https://my.xrite.com/downloader.aspx?FileID=1808&Type=M'
+  url "https://my.xrite.com/downloader.aspx?FileID=#{version.after_comma}&Type=M"
   name 'i1Profiler'
   name 'Eye-One Profiler'
   name 'i1Publish'
-  homepage "http://www.xrite.com/service-support/downloads/I/i1Profiler-i1Publish_V#{version.dots_to_underscores}"
+  homepage "http://www.xrite.com/service-support/downloads/I/i1Profiler-i1Publish_V#{version.before_comma.dots_to_underscores}"
 
   pkg 'i1Profiler.pkg'
 
