76a4f6e4209c5c996ff845508819577bc1820456
commitay <commitay@users.noreply.github.com>
2017-07-18 23:54:08 +1000

    update name, uninstall and zap

--- Casks/kubernetic.rb
+++ Casks/kubernetic.rb
@@ -4,15 +4,23 @@ cask 'kubernetic' do
 
   # s3-eu-west-1.amazonaws.com/kubernetic was verified as official when first introduced to the cask
   url "https://s3-eu-west-1.amazonaws.com/kubernetic/Kubernetic-#{version}.dmg"
-  name 'Kubernetic - The Kubernetes Desktop Client'
+  name 'Kubernetic'
   homepage 'https://kubernetic.com/'
 
   app 'Kubernetic.app'
 
+  uninstall signal: [
+                      ['TERM', 'com.kubernetic.desktop.helper'],
+                      ['TERM', 'com.kubernetic.desktop'],
+                    ]
+
   zap delete: [
-                '~/Library/Application Support/Kubernetic',
                 '~/Library/Logs/Kubernetic',
-                '~/Library/Preferences/com.kubernetic.desktop.*',
                 '~/Library/Saved Application State/com.kubernetic.desktop.*',
+              ],
+      trash:  [
+                '~/.kubernetic',
+                '~/Library/Application Support/Kubernetic',
+                '~/Library/Preferences/com.kubernetic.desktop.*',
               ]
 end
