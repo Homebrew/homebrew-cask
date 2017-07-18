b26da4e2ddee96841787ca1f063edc027a273ad6
Jean-Philippe Roemer <jp@roemer.im>
2017-07-18 11:20:31 +0100

    Update waterfox to 54.0.1

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,6 +1,6 @@
 cask 'waterfox' do
-  version '54.0.0.1'
-  sha256 '0e603d8ce2f3258267b41ac441056cf64a66db5aab1268cdc4278142a5e17338'
+  version '54.0.1'
+  sha256 'd33158ea714652002a6ead9f94d4f049521031e2a32d53c25ec57921c69b3367'
 
   # storage-waterfox.netdna-ssl.com was verified as official when first introduced to the cask
   url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20#{version.before_comma}%20Setup.dmg"
