ba20dd738813539df7dad3762b319623f4081fde
Birger J. Nordølum <MindTooth@users.noreply.github.com>
2017-07-19 03:08:06 +0200

    Update i1profiler to 1.7.1 (#36734)
    * Update i1profiler to 1.7.1
    
    * update version and url
    
    * fix url and version
    
    * homepage

--- Casks/i1profiler.rb
+++ Casks/i1profiler.rb
@@ -1,8 +1,8 @@
 cask 'i1profiler' do
-  version '1.7.0'
-  sha256 '7a256c428c429140dea8e86eb685be5d6231bf1dc029f65973d653b7232dcb0a'
+  version '1.7.1'
+  sha256 '8b6a4c2171e16abc44708c58868fa462f3a86ab8a1911957bdddd3985eb2ca12'
 
-  url 'https://my.xrite.com/downloader.aspx?FileID=1802&Type=M'
+  url "https://downloads.xrite.com/downloads/software/i1Profiler/#{version}/Mac/i1Profiler.zip"
   name 'i1Profiler'
   name 'Eye-One Profiler'
   name 'i1Publish'
