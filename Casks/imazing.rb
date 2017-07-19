104c05e3b9480f7adceb0b8f7c2d1ae57a8da521
commitay <commitay@users.noreply.github.com>
2017-07-19 13:14:59 +1000

    Update imazing

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -7,16 +7,33 @@ cask 'imazing' do
   name 'iMazing'
   homepage 'https://imazing.com/'
 
-  depends_on macos: '>= :lion'
+  conflicts_with cask: 'imazing-mini'
+  depends_on macos: '>= 10.8'
 
   app 'iMazing.app'
 
-  zap delete: [
-                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.digidna.imazingmac.sfl',
-                '~/Library/Application Support/iMazing',
-                '~/Library/Caches/com.DigiDNA.iMazingMac',
-                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.DigiDNA.iMazingMac',
-                '~/Library/Caches/iMazing',
-                '~/Library/Preferences/com.DigiDNA.iMazingMac.plist',
-              ]
+  uninstall login_item: 'iMazing Mini',
+            quit:       [
+                          "com.DigiDNA.iMazing#{version.major}Mac",
+                          "com.DigiDNA.iMazing#{version.major}Mac.Mini",
+                        ]
+
+  zap trash: [
+               '~/Library/Application Support/iMazing',
+               '~/Library/Application Support/iMazing Mini',
+               '~/Library/Application Support/MobileSync/Backup/iMazing.Versions',
+               "~/Library/Caches/com.DigiDNA.iMazing#{version.major}Mac",
+               "~/Library/Caches/com.DigiDNA.iMazing#{version.major}Mac.Mini",
+               "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.DigiDNA.iMazing#{version.major}Mac.Mini",
+               '~/Library/Caches/iMazing',
+               "~/Library/Preferences/com.DigiDNA.iMazing#{version.major}Mac.plist",
+               "~/Library/Preferences/com.DigiDNA.iMazing#{version.major}Mac.Mini.plist",
+               '/Users/Shared/iMazing Mini',
+               '/Users/Shared/iMazing',
+             ]
+
+  caveats <<-EOS.undent
+    Performing a zap on this cask removes files pertaining to both #{token}
+    and imazing-mini, so it should not be done if you only want to uninstall one of them.
+  EOS
 end
