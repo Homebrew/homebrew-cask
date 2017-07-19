486e51d787ade0a25636fb3f07667ecc18eed376
commitay <commitay@users.noreply.github.com>
2017-07-20 07:51:53 +1000

    Update caret to 3.3.0

--- Casks/caret.rb
+++ Casks/caret.rb
@@ -1,6 +1,6 @@
 cask 'caret' do
-  version '3.2.2'
-  sha256 '208eb97c5649d7b9a3722ffc4979f8e9cdf89f48923ba142c1745b28df541738'
+  version '3.3.0'
+  sha256 '43cf2cd024ac09b8d09b46529e3bbe7132876809e88930199858644c09d47f7d'
 
   # github.com/careteditor/caret was verified as official when first introduced to the cask
   url "https://github.com/careteditor/caret/releases/download/#{version}/Caret.dmg"
