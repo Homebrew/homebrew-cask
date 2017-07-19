b0aadf6d23dc13fc7a320343296847261463009c
Gobinath Mallaiyan <i@gobinath.com>
2017-07-19 08:59:21 -0400

    Update psi to 0.15

--- Casks/psi.rb
+++ Casks/psi.rb
@@ -5,7 +5,7 @@ cask 'psi' do
   # sourceforge.net/psi was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/psi/Psi-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/psi/rss',
-          checkpoint: '176bb001a1e0f9c7847213e638e1daffe5d636ec1f2b396d2b9bb9013cf58e8a'
+          checkpoint: '892f3ae46a1c0f761b3b97451cb94081ff45a0796f8821725994f5f6009d495f'
   name 'Psi'
   homepage 'http://psi-im.org/'
 
