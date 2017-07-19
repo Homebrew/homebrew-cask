182b1823bcb3cd8ac82683400166e5d7cac96cd3
Miccal Matthews <miccal.matthews@gmail.com>
2017-07-19 14:36:02 +0800

    Update xiami.rb

--- Casks/xiami.rb
+++ Casks/xiami.rb
@@ -1,9 +1,9 @@
 cask 'xiami' do
-  version '3.0.6'
+  version '3.0.6-18A5068'
   sha256 '58ff58af020d59d630018a0f43ccceb4bec92b1a848cb2d9c9647866c95752e9'
 
   # gxiami.alicdn.com/xiami-desktop was verified as official when first introduced to the cask
-  url "http://gxiami.alicdn.com/xiami-desktop/update/%E8%99%BE%E7%B1%B3%E9%9F%B3%E4%B9%90-#{version}-18A5068.dmg"
+  url "http://gxiami.alicdn.com/xiami-desktop/update/%E8%99%BE%E7%B1%B3%E9%9F%B3%E4%B9%90-#{version}.dmg"
   name 'Xiami'
   name '虾米音乐'
   homepage 'http://www.xiami.com/'
