67642b35e2b25dd5cfc76173f6fa6e290350cafe
commitay <commitay@users.noreply.github.com>
2017-07-21 00:17:52 +1000

    Update visual-paradigm to 14.1,20170723 (#36810)

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm' do
-  version '14.1,20170606'
-  sha256 '0daad5802dd41790fe9b36c0b96e77429ec18a45eeb735c457057246e639ad54'
+  version '14.1,20170723'
+  sha256 'cffc7c64354a791135cd5d9032621fa7d5a65b87b66ef5f568dce84cd30c74f4'
 
   url "https://eu6.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.dots_to_underscores.gsub(',', '_')}_OSX_WithJRE.dmg"
   name 'Visual Paradigm'
