45a5e49b4981d6afc9dd1560ba6ee194d290e967
Miccal Matthews <miccal.matthews@gmail.com>
2017-07-20 13:10:50 +0800

    Update speedy.rb

--- Casks/speedy.rb
+++ Casks/speedy.rb
@@ -6,7 +6,7 @@ cask 'speedy' do
   appcast 'http://www.apimac.com/version_checking/speedy_mac.xml',
           checkpoint: '7dddd319b0035c46f79e445d1e91d260a50f46f51963ef494c3f9e5cbe76fe58'
   name 'Speedy'
-  homepage 'http://www.apimac.com/'
+  homepage 'https://www.apimac.com/'
 
   app 'Speedy.app'
 end
