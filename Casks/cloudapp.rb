75211884bef1c34741cce114226433148451aa85
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-07-20 10:18:51 -0400

    Update cloudapp to 4.2.5 (#36814)

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -5,7 +5,7 @@ cask 'cloudapp' do
   # amazonaws.com/downloads.getcloudapp.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.zip"
   appcast 'https://updates.getcloudapp.com/appcast.xml',
-          checkpoint: '13392ccf2006b0acd00b48ef20307c9acb2e39fe269bfb84469b59d3c59a2470'
+          checkpoint: '1c250b628b810cb5c3cdbf38ec793c8c53e4ad47d51c69d7a5ac33082720cda0'
   name 'CloudApp'
   homepage 'https://www.getcloudapp.com/'
 
