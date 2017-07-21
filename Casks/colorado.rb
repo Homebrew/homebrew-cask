8705a6897adbe95c1a63632324b4bc319bc971ef
Nathaniel Hammond <nathanielhammond@gmail.com>
2017-07-21 10:08:40 +0100

    Add colorado Latest

---
+++ Casks/colorado.rb
@@ -0,0 +1,11 @@
+cask 'colorado' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.moesaid.com/colorado/app/colorado-mac-x64.zip'
+  name 'Colorado'
+  homepage 'http://www.moesaid.com/colorado/'
+
+  app 'colorado-mac/colorado.app'
+
+end
