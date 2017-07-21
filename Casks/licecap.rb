784afe9528489db624238b8661a1c60a213ef1ba
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-07-21 20:30:58 +0100

    Update licecap checkpoint and use https (#36844)

--- Casks/licecap.rb
+++ Casks/licecap.rb
@@ -2,9 +2,9 @@ cask 'licecap' do
   version '1.25'
   sha256 '0ed33667b3e19ee47fc09b3619499816e229bc678884fd5c27e24e785472f6ba'
 
-  url "http://www.cockos.com/licecap/licecap#{version.no_dots}.dmg"
-  appcast 'http://www.cockos.com/licecap/',
-          checkpoint: '507aa808788cad8ecfa938a3a839c67164a25d37d166cb7972c7cf7016a37a13'
+  url "https://www.cockos.com/licecap/licecap#{version.no_dots}.dmg"
+  appcast 'https://www.cockos.com/licecap/',
+          checkpoint: '3213076943b13521614da283c34456831dc9cf09f74bbf204bb08921dc8aa581'
   name 'LICEcap'
   homepage 'https://www.cockos.com/licecap/'
 
