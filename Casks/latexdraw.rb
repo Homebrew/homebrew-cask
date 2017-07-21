13cf744483f5940e4e897081efcc594741ea6cdb
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-07-21 20:29:12 +0100

    Update latexdraw 3.3.6 url and checkpoint (#36840)

--- Casks/latexdraw.rb
+++ Casks/latexdraw.rb
@@ -2,9 +2,9 @@ cask 'latexdraw' do
   version '3.3.6'
   sha256 '01a29a4886cf71ad914724d38eb5d470fae707eac40f5f1e70d45673780e4b07'
 
-  url "https://downloads.sourceforge.net/latexdraw/LaTeXDraw-#{version}.app"
+  url "https://downloads.sourceforge.net/latexdraw/LaTeXDraw-#{version}.app.zip"
   appcast 'https://sourceforge.net/projects/latexdraw/rss?path=/latexdraw',
-          checkpoint: '88d1f7154a75ba13476ee3ce6498452c54c23bb4d0a4aa5561c93c3bcda239ec'
+          checkpoint: '652dd1b1e3145652e548c5eb84e68888ede7a61c52008fbd68be19c884a2fbdd'
   name 'LaTexDraw'
   homepage 'http://latexdraw.sourceforge.net/'
 
