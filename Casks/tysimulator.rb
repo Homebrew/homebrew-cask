db6e1069ca244b014982fe02c6d60e76b3c249c7
luckytianyiyan <luckytianyiyan@gmail.com>
2017-07-22 17:08:15 +0800

    Add TySimulator.app v0.8.0

---
+++ Casks/tysimulator.rb
@@ -0,0 +1,11 @@
+cask 'tysimulator' do
+  version '0.8.0'
+  sha256 '0bd46b70215ddc53ad349fb7d53d2bdd550b5481b37d62bedeef87d1298b831e'
+
+  # dl.devmate.com/com.tianyiyan.TySimulator was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.tianyiyan.TySimulator/0.8.0.0/1500711183/TySimulator-0.8.0.0.dmg'
+  name 'TySimulator'
+  homepage 'https://tysimulator.com/'
+
+  app 'TySimulator.app'
+end
