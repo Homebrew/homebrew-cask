275653545fc8c1898f45995548f329c1683ee7ca
commitay <commitay@users.noreply.github.com>
2017-07-24 08:33:59 +1000

    Update mimolive.rb

--- Casks/mimolive.rb
+++ Casks/mimolive.rb
@@ -3,7 +3,7 @@ cask 'mimolive' do
   sha256 '590fad1c33bf30a105ca78557a6c7defc376735acafb373970a2772cf72c8fb4'
 
   url "https://cdn.boinx.com/software/mimolive/Boinx_mimoLive_#{version}.app.zip"
-  appcast 'https://boinx.com/d/connect//histories/mimolive',
+  appcast 'https://boinx.com/d/connect/histories/mimolive',
           checkpoint: '5b569a3ae5aaf8986b02d1e0188de9bf6f6c2ecc7b58991feea3298b2fe9b8a0'
   name 'mimoLive'
   homepage 'https://boinx.com/mimolive/'
