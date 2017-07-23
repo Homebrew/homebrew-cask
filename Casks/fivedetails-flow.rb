fdc8b1379962234647ce6fd08662d48d6fbc13af
commitay <commitay@users.noreply.github.com>
2017-07-23 22:03:57 +1000

    Remove appcast from 17 discontinued casks

--- Casks/fivedetails-flow.rb
+++ Casks/fivedetails-flow.rb
@@ -3,8 +3,6 @@ cask 'fivedetails-flow' do
   sha256 '8337d4f2152d7b00bb18cde16475c47a3a2608a7d773ee7ce0dcc425ccfbc5b8'
 
   url 'http://fivedetails.com/flow/download'
-  appcast 'http://extendmac.com/flow/updates/update.php',
-          checkpoint: '19ace39f9820f653f5a1af52764589637db1b15503b0c245aeb78b55b101dd68'
   name 'Flow'
   homepage 'http://fivedetails.com/flow/'
 
