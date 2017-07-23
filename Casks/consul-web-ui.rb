06895f9f9c973fbf5445b853fcccec48add7fe62
commitay <commitay@users.noreply.github.com>
2017-07-24 09:41:25 +1000

    Update consul-web-ui (#36888)

--- Casks/consul-web-ui.rb
+++ Casks/consul-web-ui.rb
@@ -4,8 +4,6 @@ cask 'consul-web-ui' do
 
   # hashicorp.com was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_web_ui.zip"
-  appcast 'https://github.com/hashicorp/consul/releases.atom',
-          checkpoint: '63c6dc0162381b6e93453e2140df07e88e98c1da12c5052a6c6cf98865f557f8'
   name 'Consul Web UI'
   homepage 'https://www.consul.io/intro/getting-started/ui.html'
 
@@ -14,6 +12,6 @@ cask 'consul-web-ui' do
   stage_only true
 
   caveats do
-    "Invoke consul with '-ui-dir #{staged_path}'"
+    discontinued
   end
 end
