a9d63638a134baec903598c450465f9561db919c
Robert Lu <robberphex@gmail.com>
2017-07-23 11:03:46 +0800

    update youdao to 2.2.0
    remove appcast

--- Casks/youdao.rb
+++ Casks/youdao.rb
@@ -4,11 +4,9 @@ cask 'youdao' do
     sha256 '4754b764a49d53b224d5e92f3f9bffb57f3e90a0ce5801bfc70d1dc341dd6ae9'
     url "http://codown.youdao.com/cidian/download/MacDict#{version}.dmg"
   else
-    version '2.1.134'
-    sha256 '10cd8bc2093e0fe74028c18d2663489acde5810f2f87258dd3a9229e1a9b12cb'
+    version '2.2.0'
+    sha256 '027e6bb5b845c60c4854329594ce8704bb672f4703f3bd3f7d1014ffac3cfbc7'
     url 'http://codown.youdao.com/cidian/download/MacDict.dmg'
-    appcast 'http://cidian.youdao.com/apps/macupdate/update.xml',
-            checkpoint: 'ab00c1a82410a13cf7b7f69e14f5f5a57bc854bf05da6f6e41e3d549e4c8dfc1'
   end
 
   name '有道词典'
