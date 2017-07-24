ecbc3dff38da05eb3fb64e4e85db2f1de15ce60f
commitay <commitay@users.noreply.github.com>
2017-07-25 08:01:15 +1000

    Update smartsvn.rb

--- Casks/smartsvn.rb
+++ Casks/smartsvn.rb
@@ -2,9 +2,15 @@ cask 'smartsvn' do
   version '9.2.1'
   sha256 'e886077e9b793c01f899e8318a1608f8a5dba264ec1cc45300602e4e1fafc4f4'
 
-  url 'http://www.smartsvn.com/static/svn/download/smartsvn/smartsvn-macosx-9_2_1.dmg'
+  url "http://www.smartsvn.com/static/svn/download/smartsvn/smartsvn-macosx-#{version.dots_to_underscores}.dmg"
   name 'SmartSVN'
   homepage 'http://www.smartsvn.com/'
 
-  app 'SmartSVN 9.2.app'
+  app "SmartSVN #{version.major_minor}.app"
+
+  zap delete: '~/Library/Saved Application State/com.syntevo.smartsvn.savedState',
+      trash:  [
+                '~/Library/Preferences/com.syntevo.smartsvn.plist',
+                '~/Library/Preferences/SmartSVN',
+              ]
 end
