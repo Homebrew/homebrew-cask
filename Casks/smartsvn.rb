e7a86b91f9e238c9da7845635e183ec2961f0281
Jeremy Boy <github@jboy.eu>
2017-07-24 17:29:43 +0200

    Add SmartSVN 9.2.app

---
+++ Casks/smartsvn.rb
@@ -0,0 +1,10 @@
+cask 'smartsvn' do
+  version '9.2.1'
+  sha256 'e886077e9b793c01f899e8318a1608f8a5dba264ec1cc45300602e4e1fafc4f4'
+
+  url 'http://www.smartsvn.com/static/svn/download/smartsvn/smartsvn-macosx-9_2_1.dmg'
+  name 'SmartSVN'
+  homepage 'http://www.smartsvn.com/'
+
+  app 'SmartSVN 9.2.app'
+end
