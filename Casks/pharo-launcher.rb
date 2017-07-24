1370f3f3867863ced7e2b355c3498c46d890f9d0
commitay <commitay@users.noreply.github.com>
2017-07-25 08:53:39 +1000

    Update pharo-launcher to 0.2.13 - url / homepage

--- Casks/pharo-launcher.rb
+++ Casks/pharo-launcher.rb
@@ -1,11 +1,11 @@
 cask 'pharo-launcher' do
-  version :latest
-  sha256 :no_check
+  version '0.2.13'
+  sha256 '96959ac605a23ec9566a7ddf4c7f5f303cb46f2dd002b1013f90942cb29054f7'
 
-  # ci.inria.fr/pharo/view/Launcher/job/Launcher-Mac was verified as official when first introduced to the cask
-  url 'https://ci.inria.fr/pharo/view/Launcher/job/Launcher-Mac/lastSuccessfulBuild/artifact/latest.dmg'
+  # ci.inria.fr/pharo/view/Launcher/job/Publish-Launcher-stable-Mac was verified as official when first introduced to the cask
+  url "https://ci.inria.fr/pharo/view/Launcher/job/Publish-Launcher-stable-Mac/lastSuccessfulBuild/artifact/Pharo_#{version}.dmg"
   name 'Pharo Launcher'
-  homepage 'http://smalltalkhub.com/#!/~Pharo/PharoLauncher/'
+  homepage 'https://github.com/pharo-project/pharo-launcher'
 
   # Renamed to avoid conflict with pharo.
   app 'Pharo.app', target: 'Pharo Launcher.app'
