405b59796928b0e590f609494445603a78c9c4e2
commitay <commitay@users.noreply.github.com>
2017-07-25 09:53:59 +1000

    Update pagico to 8.8.2184

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,6 +1,6 @@
 cask 'pagico' do
-  version '8.7.2171'
-  sha256 '443bf9eb12e5f8a148edf65f0384719ad03ccc4cef113eef4862cdea804de160'
+  version '8.8.2184'
+  sha256 '2564bd9dfa0ffcf26879f6589dff477c029880abefb675c22792a75f887e4ce3'
 
   url "https://www.pagico.com/downloads/Pagico_Desktop_r#{version.patch}.dmg"
   name 'Pagico'
