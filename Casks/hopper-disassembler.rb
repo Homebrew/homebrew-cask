d467c6739dc42dadcd380f6556437fb0f42dbc77
commitay <commitay@users.noreply.github.com>
2017-07-25 08:00:21 +1000

    Update hopper-disassembler to 4.2.10

--- Casks/hopper-disassembler.rb
+++ Casks/hopper-disassembler.rb
@@ -1,11 +1,11 @@
 cask 'hopper-disassembler' do
-  version '4.2.9'
-  sha256 '239aa35f5c68f894c2d919439da84f07fc3b67db19d99e0628e7781bd715a595'
+  version '4.2.10'
+  sha256 '4efa4ab9a926c8ea55a1772f55d8bc943c380e0aeb958814d58474c4230e33cd'
 
   # d2ap6ypl1xbe4k.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-#{version}-demo.dmg"
   appcast "https://www.hopperapp.com/HopperWeb/appcast_v#{version.major}.php",
-          checkpoint: '8a6b2ffaab33cb54d948e44b98e57ebabe1b969a823647f9f21f68778c2d40ac'
+          checkpoint: 'b56595ce00dc68d3fbad5378e6b5887c4458eba6264e1c83a70ec990c276de95'
   name 'Hopper Disassembler'
   homepage 'https://www.hopperapp.com/'
 
