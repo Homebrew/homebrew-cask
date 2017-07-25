b46f26d87f8885074a6995b9fe1ec4fa90762b14
commitay <commitay@users.noreply.github.com>
2017-07-25 23:03:31 +1000

    verified comment

--- Casks/prusa-slic3r.rb
+++ Casks/prusa-slic3r.rb
@@ -2,7 +2,7 @@ cask 'prusa-slic3r' do
   version '1.36.0,201707170748'
   sha256 '52d3ccf5b398e9376b305728fdcc7c09f493c7e596103f2e25d20ea5bed3c761'
 
-  # github.com/prusa3d/Slic3r/releases/download/version_ was verified as official when first introduced to the cask.
+  # github.com/prusa3d/Slic3r was verified as official when first introduced to the cask.
   url "https://github.com/prusa3d/Slic3r/releases/download/version_#{version.before_comma}/Slic3r-#{version.before_comma}-prusa3d-full-#{version.after_comma}.dmg"
   appcast 'https://github.com/prusa3d/Slic3r/releases.atom',
           checkpoint: '4c7e3e797882391a61a0f3b229676a9c0bdeb5a8ee4e658fdc9ca0eb896bb8d1'
