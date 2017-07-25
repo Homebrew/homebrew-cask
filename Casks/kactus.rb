4e557a598a7587b31a9116d125778909472a27c1
commitay <commitay@users.noreply.github.com>
2017-07-26 09:23:20 +1000

    Update kactus.rb

--- Casks/kactus.rb
+++ Casks/kactus.rb
@@ -5,7 +5,7 @@ cask 'kactus' do
   # github.com/kactus-io/kactus was verified as official when first introduced to the cask
   url "https://github.com/kactus-io/kactus/releases/download/v#{version}/Kactus-macos.zip"
   appcast 'https://github.com/kactus-io/kactus/releases.atom',
-    checkpoint '23743bc8f8c0791b844e0d19868222628e96584c30fc1ac7a2afe2c4b220c419'
+          checkpoint: '23743bc8f8c0791b844e0d19868222628e96584c30fc1ac7a2afe2c4b220c419'
   name 'Kactus'
   homepage 'http://kactus.io/'
 
