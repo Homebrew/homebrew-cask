fbd6e15568f4dc38ba0c4e01018b1a87da2fe5c6
commitay <commitay@users.noreply.github.com>
2017-07-25 23:41:43 +1000

    Update dropdmg to 3.5 (#36954)

--- Casks/dropdmg.rb
+++ Casks/dropdmg.rb
@@ -1,6 +1,6 @@
 cask 'dropdmg' do
-  version '3.4.6'
-  sha256 '90d130084b39bd1b42f0cc140e8dbc972e0f6dc9583a42322b44631a2d6086fe'
+  version '3.5'
+  sha256 '6efa9fff298cdf3ec3e5d234220cafab6c0d5981d2fbbd0aa7b72005b4e4052e'
 
   url "https://c-command.com/downloads/DropDMG-#{version}.dmg"
   name 'DropDMG'
