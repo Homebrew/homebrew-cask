927c795f1ca959648e31e2ab3a637ff9246b7d5f
commitay <commitay@users.noreply.github.com>
2017-07-25 23:12:25 +1000

    appcast checkpoint

--- Casks/prusa-slic3r.rb
+++ Casks/prusa-slic3r.rb
@@ -5,7 +5,7 @@ cask 'prusa-slic3r' do
   # github.com/prusa3d/Slic3r was verified as official when first introduced to the cask.
   url "https://github.com/prusa3d/Slic3r/releases/download/version_#{version.before_comma}/Slic3r-#{version.before_comma}-prusa3d-full-#{version.after_comma}.dmg"
   appcast 'https://github.com/prusa3d/Slic3r/releases.atom',
-          checkpoint: '4c7e3e797882391a61a0f3b229676a9c0bdeb5a8ee4e658fdc9ca0eb896bb8d1'
+          checkpoint: '13fb6d5c7b1c2eb1c2b5b4758b84bc21c487de079188024f8b2b12faff46f801'
   name 'Slic3r - Prusa Edition'
   homepage 'http://www.prusa3d.com/slic3r-prusa-edition/'
 
