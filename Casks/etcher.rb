52acf95825fe5998a9b564569127a851957da5ae
commitay <commitay@users.noreply.github.com>
2017-07-26 09:16:10 +1000

    Update etcher to 1.1.1 (#36996)

--- Casks/etcher.rb
+++ Casks/etcher.rb
@@ -1,11 +1,11 @@
 cask 'etcher' do
-  version '1.0.0'
-  sha256 '11259ae4a3b36a60092bdf365fd3f54eba854e9f0a88e661d5a7969d26d51fae'
+  version '1.1.1'
+  sha256 '7e9819cfe9fd8be1c8267d084ff45286d1c4b6cf962603a2722f2c35829692eb'
 
   # github.com/resin-io/etcher/releases/download was verified as official when first introduced to the cask
   url "https://github.com/resin-io/etcher/releases/download/v#{version}/Etcher-#{version}-darwin-x64.dmg"
   appcast 'https://github.com/resin-io/etcher/releases.atom',
-          checkpoint: '224fc003a5ce5ef1fa4b7473d692a0eecf6c4b68790494875d42ecf13252c883'
+          checkpoint: '79f30bce292808848cd66af411c60ba2679931f61c1687e83cd7a22486172e04'
   name 'Etcher'
   homepage 'https://etcher.io/'
 
