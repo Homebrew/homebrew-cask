e503bc8a76f894b4cd148b1dc40de0f761c73388
Vítor Galvão <info@vitorgalvao.com>
2017-07-25 20:24:56 +0100

    Update kactus.rb

--- Casks/kactus.rb
+++ Casks/kactus.rb
@@ -2,7 +2,7 @@ cask 'kactus' do
   version '0.1.14'
   sha256 'cf5ba67e11e9a3ac7583dd5b90d1653d77149a4ec799aebaca3bdc1def12309f'
 
-  # github.com/kactus-io was verified as official when first introduced to the cask
+  # github.com/kactus-io/kactus was verified as official when first introduced to the cask
   url "https://github.com/kactus-io/kactus/releases/download/v#{version}/Kactus-macos.zip"
   name 'Kactus'
   homepage 'http://kactus.io/'
