7417bcb93ad411d102824bbbaf691dce97340a1c
Victor Popkov <victor@popkov.me>
2017-07-26 22:18:37 +0300

    Update kite to 0.20170725.1 (#37043)

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,11 +1,11 @@
 cask 'kite' do
-  version '0.20170713.0'
-  sha256 'e770e310ac049715cb01fb97a16e86f0795e635efccc615c864c0b12a766992e'
+  version '0.20170725.1'
+  sha256 '9e65b50a34a0606d15eb121db20b3aeb4da25809467cf2ea4ab687ed57f64316'
 
   # s3-us-west-1.amazonaws.com/kite-downloads was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/kite-downloads/Kite-#{version}.dmg"
   appcast 'https://release.kite.com/appcast.xml',
-          checkpoint: '22d3bc2dcbc481e0919d72756be78d05378eb8db7b0414d4a365aa5bf0a251c9'
+          checkpoint: 'd463940e7333433908d245169a2bc77a364117e83a1a9d08a3d652f6a813ec61'
   name 'Kite'
   homepage 'https://kite.com/'
 
