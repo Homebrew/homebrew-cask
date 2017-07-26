79dbf9b192bd17c47f59c3925828d1b746c55715
Victor Popkov <victor@popkov.me>
2017-07-26 20:44:45 +0300

    Update linkliar to 2.1.0

--- Casks/linkliar.rb
+++ Casks/linkliar.rb
@@ -3,13 +3,13 @@ cask 'linkliar' do
     version '1.1.3'
     sha256 '34c9baeaf1d6732c8ce9add689b281f9b71fddadd8f56cca614cba4f8c167962'
   else
-    version '2.0.0'
-    sha256 '0810a84a99ebd33d39d2b4b990d50df8caab796b36fe3c81a8b6fe159be276f8'
+    version '2.1.0'
+    sha256 'ccb5d99a51a549367dde621102dd313f8da8cc46fed6a65d8e7bfdf4fb39c07a'
   end
 
   url "https://github.com/halo/LinkLiar/releases/download/#{version}/LinkLiar.app.zip"
   appcast 'https://github.com/halo/LinkLiar/releases.atom',
-          checkpoint: '8a426993b3044f7d701dce456a90062ba2725db93299450559996ab3e8e074c9'
+          checkpoint: '02149724f71d7ba213a3bec2fd8cd744b86ae3524bb12fd24208d21e8577f941'
   name 'LinkLiar'
   homepage 'https://github.com/halo/LinkLiar'
 
