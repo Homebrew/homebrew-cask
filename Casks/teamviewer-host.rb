3cca2a171901c85e544cdb57aa392f62e3439028
Miccal Matthews <miccal.matthews@gmail.com>
2017-07-27 09:20:55 +0800

    Update teamviewer-host.rb

--- Casks/teamviewer-host.rb
+++ Casks/teamviewer-host.rb
@@ -2,7 +2,7 @@ cask 'teamviewer-host' do
   version '12'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://download.teamviewer.com/download/version_#{version.major}x/TeamViewerHost.dmg"
+  url "https://download.teamviewer.com/download/version_#{version}x/TeamViewerHost.dmg"
   name 'TeamViewer Host'
   homepage 'https://www.teamviewer.com/'
 
@@ -13,7 +13,7 @@ cask 'teamviewer-host' do
   uninstall pkgutil: 'com.teamviewer.teamviewerhost.*',
             delete:  [
                        '/Applications/TeamViewerHost.app',
-                       "/Library/Fonts/TeamViewer#{version.major}Host.otf",
+                       "/Library/Fonts/TeamViewer#{version}Host.otf",
                        '~/Library/Application Support/TeamViewer Host',
                        '~/Library/Caches/com.teamviewer.TeamViewerHost',
                        '~/Library/Preferences/com.teamviewer.TeamViewerHost.plist',
