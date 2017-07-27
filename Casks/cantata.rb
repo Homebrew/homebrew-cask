e206517840f75f88bb79b6e6e747f5edcf86525f
giannitm <giannitm@users.noreply.github.com>
2017-07-27 11:42:22 +0100

    Update cantata to 2.0.1

--- Casks/cantata.rb
+++ Casks/cantata.rb
@@ -4,7 +4,7 @@ cask 'cantata' do
 
   url "https://github.com/CDrummond/cantata/releases/download/v#{version}/Cantata-#{version}.dmg"
   appcast 'https://github.com/CDrummond/cantata/releases.atom',
-          checkpoint: '4421d33d3c8b121da3d7740b2ce6bc356d8f04c6a762581f6787ee2e7cdf1c0d'
+          checkpoint: '3b9ed09aca605ae43105f789fdd9c5f39a5f391d5132e5e9390056a8b7f2f5de'
   name 'Cantata'
   homepage 'https://github.com/cdrummond/cantata'
 
