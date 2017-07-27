2ae0815c45ff6617a21b7c88439e8dbac8ae094c
Pelle ten Cate <pelle@occsn.com>
2017-07-27 21:20:38 +0200

    Update arq to 5.9

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,10 +1,10 @@
 cask 'arq' do
-  version '5.8.6'
-  sha256 '0f488137af538e8761b670ee7e69f033572b3a5800e43dac8a40b8ebeafad562'
+  version '5.9'
+  sha256 '89a3f9472b07a7bbc0464279f4eb0c05150e458b18c08833919d5bf86602a8a5'
 
   url "https://www.arqbackup.com/download/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arq#{version.major}.xml",
-          checkpoint: 'c4d184bbab0eb181cec46d0343bc6e3bec0619546ef12a306ad8a9ddbde59bd6'
+          checkpoint: '5113fcba8699c7d137561917efcc1f045294e1aadd9df91d36ae0cbd001923c0'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
