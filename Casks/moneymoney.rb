28f27381cbab2fa5394166a2c96187f131fed7e2
Bastian Hofmann <bashofmann@googlemail.com>
2017-07-28 12:06:58 +0200

    Update moneymoney to 2.2.18 (#37141)

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,10 +1,10 @@
 cask 'moneymoney' do
-  version '2.2.17'
-  sha256 '91fb2cce75eade00dc202b3daca0cfbf18e93b9f21b37e7e022f7a57a47cbc40'
+  version '2.2.18'
+  sha256 'f745867f1e891adfe6734e26ae5441bd8989b30390bffd729aa697fa73fcb165'
 
   url 'https://service.moneymoney-app.com/1/MoneyMoney.zip'
   appcast 'https://service.moneymoney-app.com/1/Appcast.xml',
-          checkpoint: 'ff28ed32de5ae9fdac81f8a13348e5199ee7770a8d2f1482cbb3bca9493060ab'
+          checkpoint: '13ea64ecb8fa24a5734d5a23f3b9a052cb9f697a5d1a69f1dab57064cb04bfaf'
   name 'MoneyMoney'
   homepage 'https://moneymoney-app.com/'
 
