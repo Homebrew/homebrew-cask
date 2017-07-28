a8a5d909964b5d4d18773531daa47259ca6f8696
giannitm <giannitm@users.noreply.github.com>
2017-07-28 01:22:58 +0100

    Update clean-me to 1.1.1

--- Casks/clean-me.rb
+++ Casks/clean-me.rb
@@ -4,7 +4,7 @@ cask 'clean-me' do
 
   url "https://github.com/Kevin-De-Koninck/Clean-Me/releases/download/v#{version}/Clean.Me.app.zip"
   appcast 'https://github.com/Kevin-De-Koninck/Clean-Me/releases.atom',
-          checkpoint: 'bedef3ef059b767dabdc6112d4f76421d39a43781eac2eb1d07970fcf5c7e5c9'
+          checkpoint: 'a2c182a1428906cc0f8219d35da10bbd28042215db9dec862560d8d769ff2960'
   name 'Clean-me'
   homepage 'https://github.com/Kevin-De-Koninck/Clean-Me'
 
