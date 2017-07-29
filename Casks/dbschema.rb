c14aae9d5a3eb31b7d2e1b9d66acfc8eb331a53d
commitay <commitay@users.noreply.github.com>
2017-07-29 13:03:44 +1000

    Update dbschema to 7.6.0 (#37191)

--- Casks/dbschema.rb
+++ Casks/dbschema.rb
@@ -1,6 +1,6 @@
 cask 'dbschema' do
-  version '7.5.2'
-  sha256 'ce1ca78bad8aa817ab899bf57de1d4e215ed172d72773beea37f6126609c8993'
+  version '7.6.0'
+  sha256 'e33278c787a7560532d3623bf3c89b349ce9d13df561795f2ac87566d1bf81df'
 
   url "http://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
   name 'DbSchema'
