e12e561e041a371b570a5e036e6663c31f1d6c69
commitay <commitay@users.noreply.github.com>
2017-07-30 07:45:36 +1000

    version latest

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -1,10 +1,8 @@
 cask 'the-unarchiver' do
-  version '3.11.1'
-  sha256 '12d1af6475149647f996f1b0510e067f057444852f1aa08a8dad75d6debe6ddf'
+  version :latest
+  sha256 :no_check
 
   url 'https://theunarchiver.com/downloads/TheUnarchiver.zip'
-  appcast 'http://blog.macpaw.com/rss',
-          checkpoint: 'cd9dbb45bb6fd7c13de7493a2e9549221c5751a2ecb7f2e248bf69bf1faa5b7d'
   name 'The Unarchiver'
   homepage 'https://theunarchiver.com/'
 
@@ -16,6 +14,6 @@ cask 'the-unarchiver' do
   zap delete: [
                 '~/Library/Caches/cx.c3.theunarchiver',
                 '~/Library/Cookies/cx.c3.theunarchiver.binarycookies',
-                '~/Library/Preferences/cx.c3.theunarchiver.plist',
-              ]
+              ],
+      trash:  '~/Library/Preferences/cx.c3.theunarchiver.plist'
 end
