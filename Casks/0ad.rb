1b2de6f56638db13cc1fe60c29a0fe908c13f705
Jordi De Groof <jordi.degroof@gmail.com>
2017-07-29 11:53:33 +0200

    Update 0ad to 0.0.22-alpha

--- Casks/0ad.rb
+++ Casks/0ad.rb
@@ -1,11 +1,11 @@
 cask '0ad' do
-  version '0.0.21-alpha'
-  sha256 '4f616798252e4c814262e25b7d7e714619a6f0897c8a44a6a128766901d8b25d'
+  version '0.0.22-alpha'
+  sha256 '5876856a8641ffe736fc39f9f6472d7e1699c49a3154e35e713a382abc840caa'
 
   # wildfiregames.com was verified as official when first introduced to the cask
   url "http://releases.wildfiregames.com/0ad-#{version}-osx64.dmg"
   appcast 'https://play0ad.com/download/mac/',
-          checkpoint: '97895f114e670a52aee9a81f6fd55af5f0fe54aff60f9f6dd3b847f446f0a90b'
+          checkpoint: 'd0522e342051ad0fe14820172702c1db0c24cd8aa43e848458ad453274f7374e'
   name '0 A.D.'
   homepage 'https://play0ad.com/'
 
