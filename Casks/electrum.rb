240fee416a797deb430f2d9366e395dc6cb866aa
Pierrick Greze <pierrick.greze@gmail.com>
2017-07-30 18:19:47 +0900

    Update electrum to 2.9.0 (#37218)

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,10 +1,10 @@
 cask 'electrum' do
-  version '2.8.3'
-  sha256 '9090baab30f1d1b1ecde62b8eb976a1e025b85c8c2849ea0467a34de0c255a68'
+  version '2.9.0'
+  sha256 '2b3a740703ae3f2eefcb057a17744c31ea8de37ea71965d0345ff2c3a8242cfc'
 
   url "https://download.electrum.org/#{version}/electrum-#{version}.dmg"
   appcast 'https://github.com/spesmilo/electrum/releases.atom',
-          checkpoint: '6d8cc7c042551a1f0c037dad1eaf0666def3a81184078b2db109c1a0773b5958'
+          checkpoint: 'e88c7a60523d673f319302d6d4c1fe88fc677ee0868275bb1c4b84f9b1fb4793'
   name 'Electrum'
   homepage 'https://electrum.org/'
   gpg "#{url}.asc", key_id: '6694d8de7be8ee5631bed9502bd5824b7f9470e6'
