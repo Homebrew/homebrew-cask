7ec1ff051d9bcbe3c69ef55b46881d12ffb9e7bf
tiiiecherle <tiiiecherle@gmail.com>
2017-08-01 23:55:46 +0200

    Update istat-menus to 5.32

--- Casks/istat-menus.rb
+++ Casks/istat-menus.rb
@@ -1,6 +1,6 @@
 cask 'istat-menus' do
-  version '5.31'
-  sha256 '4a6056353a2252606a78147473d5b2e7ce35b7dbcd5a8ad6bd5b22c6d07e2b80'
+  version '5.32'
+  sha256 '0dca05fb06315820c537b4c76b3148c79a4c55ace345b2f82167a29c9c98b7c9'
 
   # amazonaws.com/bjango was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/bjango/files/istatmenus#{version.major}/istatmenus#{version}.zip"
