6c30abed3ccbdaca6db1aec0b97e06c305a4e34f
Danny Amey <dannyamey@users.noreply.github.com>
2017-08-01 12:15:59 +0100

    Add IVPN.app v2.6.2 (#37240)

---
+++ Casks/ivpn.rb
@@ -0,0 +1,23 @@
+cask 'ivpn' do
+  version '2.6.2'
+  sha256 '283ec6bec4c231ce408afe06c133305fd6dd569b543259b787b678e777c74062'
+
+  url "https://www.ivpn.net/releases/osx/IVPN-#{version}.dmg"
+  name 'IVPN'
+  homepage 'https://www.ivpn.net/apps-macos'
+
+  app 'IVPN.app'
+
+  uninstall_preflight do
+    set_ownership "#{appdir}/IVPN.app"
+  end
+
+  uninstall delete:    [
+                         '/Library/Application Support/IVPN',
+                         '/Library/PrivilegedHelperTools/net.ivpn.client.Helper',
+                       ],
+            launchctl: 'net.ivpn.client.Helper',
+            quit:      'net.ivpn.client.IVPN'
+
+  zap trash: '~/Library/Preferences/net.ivpn.client.IVPN.plist'
+end
