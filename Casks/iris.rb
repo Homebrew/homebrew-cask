0606d01fb9ead3d534702d3a3bf76e2071c54540
commitay <commitay@users.noreply.github.com>
2017-08-01 16:51:30 +1000

    Update iris to 0.9.1.7 (#37273)

--- Casks/iris.rb
+++ Casks/iris.rb
@@ -1,6 +1,6 @@
 cask 'iris' do
-  version '0.9.0.7'
-  sha256 '6074a029736dcc25f393d3bc28babdcc0973bef1fe15eda0ee95b0d12ebc7241'
+  version '0.9.1.7'
+  sha256 '2f44e0bcb32a87f421536efc4fda931d34f722ffe637f70c8e42c139383113a3'
 
   # raw.github.com/danielng01/Iris-Builds/master/OSX was verified as official when first introduced to the cask
   url "https://raw.github.com/danielng01/Iris-Builds/master/OSX/Iris-#{version}-OSX.zip"
