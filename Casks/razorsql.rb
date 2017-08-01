5f8523dfa5c2518abd461a6e9a871b36b482a2a6
commitay <commitay@users.noreply.github.com>
2017-08-02 07:28:56 +1000

    Update razorsql to 7.3.2

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,10 +1,10 @@
 cask 'razorsql' do
-  version '7.3.0'
-  sha256 'e1e3ae86d7496c4908cd7498eae8dc839f4e03b26e903518d9b0cff47974d106'
+  version '7.3.2'
+  sha256 '160b8e167879ade6d1f6d2797f6bd6a0c57b336021c990c3ed04f9c3e7561ee1'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   appcast 'https://razorsql.com/updates.html',
-          checkpoint: '317f8f8bf00910f05a870e27c1bd6a529c6839b318d4b90e7538885a987b0df6'
+          checkpoint: 'cc116654c6a1d1c5696ebf394eece02789fc76a885d66cde5787957c4441c277'
   name 'RazorSQL'
   homepage 'https://razorsql.com/download_mac.html'
 
