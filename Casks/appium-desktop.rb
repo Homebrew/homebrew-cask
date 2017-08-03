cfd6b0521f456cac91e9b51f6f0422f04a5e590b
Josh Reynolds <jshreynolds@users.noreply.github.com>
2017-08-03 16:10:12 -0400

    Upgrade Appium Desktop to v1.1.1 & github hosting
    Appium has migrated their code to github. The version
    in the current caskroom points to an old binary
    hosted on bitbucket.  The project is now
    hosted and distributed on github.
    
    I've also changed the cask token name to avoid
    confusion between 'appium', the server for mobile
    automation, and 'Appium Desktop' the desktop client
    that allows for access and interaction with an
    appium server.
    
    I've also deleted the outdated cask. This exact situation
    wasn't covered in the CONTRIBUTING.md, but seemed like
    the right way to go.
    
    Token: appium-desktop
    Version: 1.1.1

---
+++ Casks/appium-desktop.rb
@@ -0,0 +1,19 @@
+cask 'appium-desktop' do
+  version '1.1.1'
+  sha256 'bdb212c3e71b47e0c36ed246cb3bc10e704cc50c3f87de0447cc111be317e7b2'
+
+  # github.com/appium/appium-desktop was verified as official when first introduced to the cask.
+  url "https://github.com/appium/appium-desktop/releases/download/v#{version}/appium-desktop-#{version}-mac.zip"
+  appcast 'https://github.com/appium/appium-desktop/releases.atom',
+          checkpoint: '4a922036bab3fa4e403d263bcee9ae57775a5af44fc83adadd462beff75e3bc1'
+  name 'Appium'
+  name 'Appium Desktop'
+  homepage 'https://appium.io/'
+
+  app 'Appium.app'
+
+  zap delete: [
+                '~/Library/Preferences/io.appium.desktop.helper.plist',
+                '~/Library/Preferences/io.appium.desktop.plist',
+              ]
+end
