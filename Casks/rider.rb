c8dda5e2c3df03404e2547059447521a13ebb500
jcbot <jcb@leipert.io>
2017-08-04 01:05:12 +0200

    Update rider to 2017.1,171.4456.2169 (#37346)
    * Update rider to 2017.1,171.4456.2169
    
    * fix app name

--- Casks/rider.rb
+++ Casks/rider.rb
@@ -1,17 +1,16 @@
 cask 'rider' do
-  # Rider is RC for now
-  version '2017.1,171.4456.1432'
-  sha256 'f4bd0df5e45c2c4f356f0716bd2c8e9ec21b81a9161abcf5283234e099f7989e'
+  version '2017.1,171.4456.2169'
+  sha256 '9867f139c98b8ccaadfeb063b1919c8035d72d1021e0108369ef44df43cd765f'
 
-  url "https://download.jetbrains.com/resharper/Rider-RC-#{version.after_comma}.dmg"
-  appcast 'https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=eap',
-          checkpoint: '6391fe0c6b7d03925ad568e1d9ac88c11b5ff7d0120a85b967c1fd9e9c010471'
+  url "https://download.jetbrains.com/resharper/JetBrains.Rider-#{version.major_minor}.dmg"
+  appcast 'https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release',
+          checkpoint: 'be17267b11d726d6879f7c47e579543565dc613167e3564b4935eed2c08caae8'
   name 'Jetbrains Rider'
   homepage 'https://www.jetbrains.com/rider/'
 
   auto_updates true
 
-  app "Rider #{version.major_minor} RC.app"
+  app "Rider #{version.major_minor}.app"
 
   uninstall delete: ENV['PATH'].split(File::PATH_SEPARATOR).map { |p| "#{p}/rider" }.select { |f| File.exist?(f) }
 
