92addd573f9f2d001520fd0754c7e9d0d2639a53
Dayne Broderson <broderson@gmail.com>
2017-08-03 17:48:27 -0400

    panoply v4.8.2 (#37331)
    * panoply v4.8.2
    
    * Update panoply.rb

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,6 @@
 cask 'panoply' do
-  version '4.8.1'
-  sha256 'cd8c729cfa71f3bac7ab12eb4932de16486389990d850ef95377c0232129505d'
+  version '4.8.2'
+  sha256 'f08f3593961c49133bd9562bb240b6bfa89d4ebd0d16f86df612974be38fbdb4'
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
   name 'Panoply netCDF, HDF and GRIB Data Viewer'
