2aed7aeabb2973bdade92bed7c37b19bd380acb8
Vítor Galvão <info@vitorgalvao.com>
2017-08-04 22:38:31 +0100

    Update zulip.rb

--- Casks/zulip.rb
+++ Casks/zulip.rb
@@ -2,7 +2,7 @@ cask 'zulip' do
   version '1.2.0-beta'
   sha256 'a1416d3a2a08ff1de111c108b7702dddb840e4634f19760d0e76d31d2d72d1b1'
   
-  # zulipchat.com was verified as official when first introduced to the cask
+  # github.com/zulip/zulip-electron was verified as official when first introduced to the cask
   url "https://github.com/zulip/zulip-electron/releases/download/v#{version}/Zulip-#{version}-mac.zip"
   appcast 'https://github.com/zulip/zulip-electron/releases.atom',
           checkpoint: '354ae69858f6eaf8befa93b7b61424e6946a7601820897959f02411e950ff05d'
