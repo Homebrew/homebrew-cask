0646a163c16bfab69497f3d80700c42350c8f353
commitay <commitay@users.noreply.github.com>
2017-08-06 07:27:02 +1000

    verified comment

--- Casks/devrantron.rb
+++ Casks/devrantron.rb
@@ -2,7 +2,7 @@ cask 'devrantron' do
   version '1.2.0'
   sha256 'cf01835b96fea5fb127525d0477a47f0c6de5420c3dddb1b1bb62513585143ad'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/tahnik/devRantron was verified as official when first introduced to the cask
   url "https://github.com/tahnik/devRantron/releases/download/v#{version}/devrantron-#{version}.dmg"
   appcast 'https://github.com/tahnik/devRantron/releases.atom',
           checkpoint: '868ebf87e2efd29cb26a25c998002e625e8d190a96c80da256598a35bd3bd5e3'
