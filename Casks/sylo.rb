24cbf9e68d0a0a4c4a44b59c6a5496a0a2ec159c
commitay <commitay@users.noreply.github.com>
2017-08-05 20:48:09 +1000

    Update sylo to 4.0 (#37395)

--- Casks/sylo.rb
+++ Casks/sylo.rb
@@ -1,11 +1,11 @@
 cask 'sylo' do
-  version '3.9'
-  sha256 'd676d6e204dd15a10eed22efde791e46819344be6cbf31dfdefb8508bdd76854'
+  version '4.0'
+  sha256 'b55b79007336fd825feed77f8c2894aa0fa70479c49f7e38a971a7aec1081ebe'
 
   # s3-us-west-2.amazonaws.com/unision was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/unision/Sylo.dmg'
   appcast 'http://admin.unisionmusic.com/sylo/update.xml',
-          checkpoint: '80bb4b95f922dc23c9218daca0f1629b7dd3845e00f4cb5a623d2b847d3ecc28'
+          checkpoint: '97abeb26c0bc10ce27ab5944916747fdd51d64fc242d7d21af0842adac4918c7'
   name 'Sylo'
   homepage 'http://www.sylomusic.com/'
 
