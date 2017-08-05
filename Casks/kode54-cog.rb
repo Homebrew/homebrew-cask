53ad826560566c5d537ac3644a94d51b57bcb4d8
Jussi Tuominen <jussi@COLDwithin.local>
2017-08-05 18:44:03 +0300

    Update kode54-cog to 0.08,1a084b43

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,6 +1,6 @@
 cask 'kode54-cog' do
-  version '0.08,2575a2fb'
-  sha256 '4ee8fc4faac066aa162424e772fc4be0486ea4af4b65d12432593c7ab42a8599'
+  version '0.08,1a084b43'
+  sha256 '9c52cb8d79fe74991a58e814b46a8bcdc40067d304d617a837ae56ee1c6bb5cb'
 
   # losno.co/cog was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
