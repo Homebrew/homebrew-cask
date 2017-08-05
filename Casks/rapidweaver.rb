b20faf821df70bcfbdcf7d10549282c4a81d6bd8
Steven Conaway <steven.conaway@icloud.com>
2017-08-05 14:22:30 -0700

    Update RapidWeaver to use latest version. Closes Issue #37403 (#37404)
    * Update RapidWeaver to 7.4.1
    
    Update RapidWeaver to 7.4.1
    
    * Update rapidweaver.rb

--- Casks/rapidweaver.rb
+++ Casks/rapidweaver.rb
@@ -1,11 +1,11 @@
 cask 'rapidweaver' do
-  version '6.4,15176,1458573922'
-  sha256 '237a3e402194ae1b475a523a431a60cdb2ba8babfa50f22eaccd7ba5c619a89c'
+  version '7.4.1,18708.1496675639'
+  sha256 'be4336a72fd659f8a7706e482e74e5a4a8b026f02542d5f6278e30ff7909df5f'
 
-  # devmate.com/com.realmacsoftware.rapidweaver6 was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.realmacsoftware.rapidweaver6/#{version.after_comma.before_comma}/#{version.after_comma.after_comma}/RapidWeaver#{version.major}-#{version.after_comma.before_comma}.zip"
-  appcast "https://updates.devmate.com/com.realmacsoftware.rapidweaver#{version.major}.xml",
-          checkpoint: '42a364ce0f07cb09d7b55024becee4acc42646a530673f5ae1d178fb13b40c1e'
+  # devmate.com/com.realmacsoftware.rapidweaver was verified as official when first introduced to the cask
+  url "https://dl.devmate.com/com.realmacsoftware.rapidweaver/#{version.after_comma.major}/#{version.after_comma.minor}/RapidWeaver-#{version.after_comma.major}.zip"
+  appcast 'https://updates.devmate.com/com.realmacsoftware.rapidweaver.xml',
+          checkpoint: 'eab532b0ac3a667aa4b1f43581144a62d6e1229f93ae5451659d5c44ddb1398a'
   name 'RapidWeaver'
   homepage 'https://www.realmacsoftware.com/rapidweaver/'
 
