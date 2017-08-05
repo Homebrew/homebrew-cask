36b902dc5e103b21c3f5b18e1f4fbec0d1bff700
Vítor Galvão <info@vitorgalvao.com>
2017-08-05 20:38:38 +0100

    Update rapidweaver.rb

--- Casks/rapidweaver.rb
+++ Casks/rapidweaver.rb
@@ -1,9 +1,9 @@
 cask 'rapidweaver' do
-  version '7.4.1,18708,1496675639'
+  version '7.4.1,18708.1496675639'
   sha256 'be4336a72fd659f8a7706e482e74e5a4a8b026f02542d5f6278e30ff7909df5f'
 
   # devmate.com/com.realmacsoftware.rapidweaver was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.realmacsoftware.rapidweaver/#{version.after_comma.before_comma}/#{version.after_comma.after_comma}/RapidWeaver-#{version.after_comma.before_comma}.zip"
+  url "https://dl.devmate.com/com.realmacsoftware.rapidweaver/#{version.after_comma.major}/#{version.after_comma.minor}/RapidWeaver-#{version.after_comma.major}.zip"
   appcast 'https://updates.devmate.com/com.realmacsoftware.rapidweaver.xml',
           checkpoint: 'eab532b0ac3a667aa4b1f43581144a62d6e1229f93ae5451659d5c44ddb1398a'
   name 'RapidWeaver'
