aea52eba8a6cde9a0a1e81eddaf4582050af27ac
jarne <jarne@users.noreply.github.com>
2017-08-05 12:25:42 +0200

    Fix Sylo version

--- Casks/sylo.rb
+++ Casks/sylo.rb
@@ -1,11 +1,9 @@
 cask 'sylo' do
-  version '3.9'
-  sha256 'd676d6e204dd15a10eed22efde791e46819344be6cbf31dfdefb8508bdd76854'
+  version :latest
+  sha256 :no_check
 
   # s3-us-west-2.amazonaws.com/unision was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/unision/Sylo.dmg'
-  appcast 'http://admin.unisionmusic.com/sylo/update.xml',
-          checkpoint: '80bb4b95f922dc23c9218daca0f1629b7dd3845e00f4cb5a623d2b847d3ecc28'
   name 'Sylo'
   homepage 'http://www.sylomusic.com/'
 
