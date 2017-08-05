e9331a8b10c3758f88a0cd989c5f21f1544cc862
commitay <commitay@users.noreply.github.com>
2017-08-06 01:12:50 +1000

    Update peakhour to 4.0.2 (#37399)

--- Casks/peakhour.rb
+++ Casks/peakhour.rb
@@ -1,10 +1,10 @@
 cask 'peakhour' do
-  version '4.0.1'
-  sha256 '582d9d7205ffe29cea59ba6d4a36bb64b72ca7bb6761a951bab69cb384c84903'
+  version '4.0.2'
+  sha256 'bf62bc3b6e3cb4e8681be70451bf3ec219ed80557b3b1b921629bfb568105d5d'
 
   url "https://updates.peakhourapp.com/releases/PeakHour%20#{version}.zip"
   appcast "https://updates.peakhourapp.com/PeakHour#{version.major}Appcast.xml",
-          checkpoint: '5971c9c3e40a29c33cf9bf8eb53ecc67dbd03077f5e3bede697682a86fd70b5a'
+          checkpoint: '2374b834f4b403db314beca2448e6ec9ade501b53b91001658245b7decf13f84'
   name 'PeakHour'
   homepage 'https://www.peakhourapp.com/'
 
