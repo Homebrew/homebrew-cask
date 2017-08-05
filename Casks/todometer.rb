862631cf8452fc3c742883459b1d1154ee2c310c
commitay <commitay@users.noreply.github.com>
2017-08-06 08:27:34 +1000

    version / appcast

--- Casks/todometer.rb
+++ Casks/todometer.rb
@@ -3,7 +3,9 @@ cask 'todometer' do
   sha256 '49a744d996cfa925b50064f1853f8bf1372a3073f449a482f7896ccc5ca15d48'
 
   # github.com/cassidoo/todometer was verified as official when first introduced to the cask
-  url 'https://github.com/cassidoo/todometer/releases/download/v1.0.4/install-todometer.dmg'
+  url "https://github.com/cassidoo/todometer/releases/download/v#{version}/install-todometer.dmg"
+  appcast 'https://github.com/cassidoo/todometer/releases.atom',
+          checkpoint: 'd15eedbb844a21e27f9ac743cce59b6ae34f3e9ca1e184dcf3bfd1b48fed8726'
   name 'todometer'
   homepage 'https://cassidoo.github.io/todometer/'
 
