26a3e03c986e621be1df12fbb2f6cc429fe4b15c
Timothy Sutton <tim@synthist.net>
2017-08-06 13:45:20 -0400

    sauce-connect 4.4.8 (#37422)

--- Casks/sauce-connect.rb
+++ Casks/sauce-connect.rb
@@ -1,6 +1,6 @@
 cask 'sauce-connect' do
-  version '4.4.6'
-  sha256 'ebc6a92e4f269a46042c5a82f8da69ef17ffc7784e1d3bbb4d90ad1cf142d946'
+  version '4.4.8'
+  sha256 'efd6aee54d7ab7e677d813876166b26a7d51161dc81a4f4e5b7899864f671339'
 
   url "https://saucelabs.com/downloads/sc-#{version}-osx.zip"
   name 'Sauce Connect'
