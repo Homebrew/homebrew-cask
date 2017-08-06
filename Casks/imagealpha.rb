81883361f1fd04ca71727ecebc40d2c502ddb958
Victor Popkov <victor@popkov.me>
2017-08-06 23:56:41 +0300

    Update imagealpha to 1.5.1 (#37435)

--- Casks/imagealpha.rb
+++ Casks/imagealpha.rb
@@ -3,10 +3,10 @@ cask 'imagealpha' do
     version '1.2.3'
     sha256 '79388edcaf5cb29234f722a672b069c6d51fb812e89969ba8db8e957b9a32bf3'
   else
-    version '1.5.0'
-    sha256 'f0e06109d7348f41bc0fa9c62caac997b9601777f23f1fd522efdea2188ccf54'
+    version '1.5.1'
+    sha256 'be8a4544a2a855ba424e33819940b4b0ddb1221d1f46ca360181fd4ee2bef24b'
     appcast 'https://pngmini.com/appcast.xml',
-            checkpoint: '09bfb1631ef1024e04c78fccae8fb50bfabbb113304e442d6fc5c156b494b686'
+            checkpoint: '76ca766570428cdd1ce9e69bb3c302c087d41226489c76fd6b9ea0c4e0594363'
   end
 
   url "https://pngmini.com/ImageAlpha#{version}.tar.bz2"
