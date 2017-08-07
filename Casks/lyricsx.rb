a6c7dbdec4f08db1d8bd54e3fa1dccecbc174505
Vítor Galvão <info@vitorgalvao.com>
2017-08-07 19:00:13 +0100

    Update lyricsx.rb

--- Casks/lyricsx.rb
+++ Casks/lyricsx.rb
@@ -3,7 +3,8 @@ cask 'lyricsx' do
   sha256 '40e96ae29916c791d783c4ebabcfc715c60dc0e5bf0e6bae6e87a149974050a5'
 
   url "https://github.com/ddddxxx/LyricsX/releases/download/v#{version}/LyricsX.app.zip"
-  appcast 'https://github.com/ddddxxx/LyricsX/releases.atom', checkpoint: '380de848d028e25585aaed73dcda5360d8b137eee6d9493b1c8efeae00a793ec'
+  appcast 'https://github.com/ddddxxx/LyricsX/releases.atom',
+          checkpoint: '380de848d028e25585aaed73dcda5360d8b137eee6d9493b1c8efeae00a793ec'
   name 'LyricsX'
   homepage 'https://github.com/ddddxxx/LyricsX'
 
