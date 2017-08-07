14eff3ec2831e822e6185224b77dcc4b453a4b20
moritz <moritzf@gmail.com>
2017-08-07 19:00:39 +0200

    Add 360-total-security v1.1.2

---
+++ Casks/360-total-security.rb
@@ -0,0 +1,10 @@
+cask '360-total-security' do
+  version '1.1.2'
+  sha256 '6fdef1e93f9930f48ecf7144ed2412663138a1cee492aaa371550907999db8d9'
+
+  url 'https://free.360totalsecurity.com/totalsecurity/mac/360ts_mac_1.1.2.dmg'
+  name '360 Total Security'
+  homepage 'https://www.360totalsecurity.com/'
+
+  app '360Safe.app'
+end
