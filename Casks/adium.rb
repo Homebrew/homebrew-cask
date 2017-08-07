f39e89d96d7f10322dd6d3ca89712816a1e96ed0
ajkblue <ajkblue@users.noreply.github.com>
2017-08-07 13:27:00 -0600

    Update adium.rb (#4)

--- Casks/adium.rb
+++ Casks/adium.rb
@@ -7,6 +7,8 @@ cask 'adium' do
           checkpoint: 'bd77f84e4dba32a3337f79e6a7bb3e5678750f79248c048d3cf7661634c94028'
   name 'Adium'
   homepage 'https://www.adium.im/'
+  
+  auto_updates true
 
   app 'Adium.app'
 
