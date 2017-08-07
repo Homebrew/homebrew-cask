e7af627485d85f4638327a9d909dd8c16260d6f3
Sepehr Lajevardi <lajevardi@gmail.com>
2017-08-07 21:39:18 +0430

    Add latest Roger.

---
+++ Casks/roger.rb
@@ -0,0 +1,19 @@
+cask 'roger' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://api.useroger.com/download'
+  name 'Roger'
+  homepage 'http://www.useroger.com/'
+
+  app 'Roger.app'
+
+  uninstall login_item: 'Roger',
+            quit:       'com.placepixel.Roger'
+
+  zap delete: [
+                '~/Library/Caches/com.placepixel.Roger',
+                '~/Library/Preferences/com.placepixel.Roger.plist',
+                '~/Library/Application Support/com.placepixel.Roger',
+              ]
+end
