09a57559b70c7197672569b9fa6f20d5ab2883d7
Vítor Galvão <info@vitorgalvao.com>
2017-08-07 20:08:12 +0100

    Update mosaic.rb

--- Casks/mosaic.rb
+++ Casks/mosaic.rb
@@ -1,8 +1,10 @@
 cask 'mosaic' do
-  version :latest
-  sha256 :no_check
+  version '1.0.7'
+  sha256 '310a3554a31e04a4a337fe343b9bf4e01850161ec28e8c7fc3e5a8c187fbb89e'
 
   url 'http://www.lightpillar.com/appdata/mosaic/Mosaic.zip'
+  appcast 'http://lightpillar.com/appdata/mosaic/features/version-history.html',
+          checkpoint: 'bed0519ce94b3bcaeff59679ee8595dc028685097d164e044cf8077ded5ad6f4'
   name 'Mosaic'
   homepage 'https://lightpillar.com/mosaic.html'
 
