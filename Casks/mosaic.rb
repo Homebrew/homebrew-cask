83a65aaeedd1b0d68ccc9342650d3cbf31a57963
Josh Manders <josh@joshmanders.com>
2017-08-07 13:31:56 -0500

    add mosaic

---
+++ Casks/mosaic.rb
@@ -0,0 +1,10 @@
+cask 'mosaic' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.lightpillar.com/appdata/mosaic/Mosaic.zip'
+  name 'Mosaic'
+  homepage 'https://lightpillar.com/mosaic.html'
+
+  app 'Mosaic.app'
+end
