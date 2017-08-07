2c1d30942d09df1cd569ec091c09dc241acf848b
commitay <commitay@users.noreply.github.com>
2017-08-08 09:25:37 +1000

    Update roger.rb

--- Casks/roger.rb
+++ Casks/roger.rb
@@ -2,7 +2,8 @@ cask 'roger' do
   version :latest
   sha256 :no_check
 
-  url 'http://api.useroger.com/download'
+  # s3.amazonaws.com/placepixel-images/_walkietalkie_ was verified as official when first introduced to the cask
+  url 'https://s3.amazonaws.com/placepixel-images/_walkietalkie_/last/Roger.dmg'
   name 'Roger'
   homepage 'http://www.useroger.com/'
 
