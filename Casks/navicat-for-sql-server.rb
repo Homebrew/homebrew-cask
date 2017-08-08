406413e5a94007d1449e7cc7a3eed7f5d4fe677f
commitay <commitay@users.noreply.github.com>
2017-08-09 00:17:56 +1000

    Update navicat-for-sql-server to 12.0.11

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,10 +1,10 @@
 cask 'navicat-for-sql-server' do
-  version '12.0.10'
-  sha256 '59828dd38eeedbd1489a84526f17a5ca908ed3b3850bc03ffe2b7f90dd34de2a'
+  version '12.0.11'
+  sha256 'fe436b0ae16b04ca19fb434cc1fccddef45317201aac1ff4ce1893e907f27ef8'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-sqlserver-release-note#M',
-          checkpoint: '789127e485e0ae3dcd54eb327711cf9b1516e7c292b2982096e0dabb0b5fe08b'
+          checkpoint: '39bbf588df7ca4739b78e95580b50e4218c8eaa2e6e71db9d8e53b5fc5e00acd'
   name 'Navicat for SQL Server'
   homepage 'https://www.navicat.com/products/navicat-for-sqlserver'
 
