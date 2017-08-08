68fa3a743103d81987f2a3ebc0f5185b9d0d6f9f
commitay <commitay@users.noreply.github.com>
2017-08-08 17:06:04 +1000

    Update keystore-explorer.rb

--- Casks/keystore-explorer.rb
+++ Casks/keystore-explorer.rb
@@ -3,7 +3,7 @@ cask 'keystore-explorer' do
   sha256 '8ccf57e1282d2a4538d9a2b551bc0825cc3144c18068b0fd492251340497a5a7'
 
   # github.com/kaikramer/keystore-explorer was verified as official when first introduced to the cask
-  url "https://github.com/kaikramer/keystore-explorer/releases/download/v5.3/kse-#{version.no_dots}.dmg"
+  url "https://github.com/kaikramer/keystore-explorer/releases/download/v#{version.major_minor}/kse-#{version.no_dots}.dmg"
   appcast 'https://github.com/kaikramer/keystore-explorer/releases.atom',
           checkpoint: '5712e8f8baedb730cd02b27274daf3a052116caba13ce6e12d97f3a2ac93513d'
   name 'KeyStore Explorer'
