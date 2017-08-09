fbfa50e7c64e101d487b181eb25e5bbf721295fa
Pascal Iske <info@pascal-iske.de>
2017-08-09 23:43:18 +0200

    feat(casks): add cask file for pro version of blue-ray-player

---
+++ Casks/blu-ray-player-pro.rb
@@ -0,0 +1,10 @@
+cask 'blu-ray-player-pro' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.macblurayplayer.com/user/download/Macgo_Mac_Bluray_Player_Pro.dmg'
+  name 'Macgo Mac Blu-ray Player Pro'
+  homepage 'https://www.macblurayplayer.com/'
+
+  app 'Blu-ray Player Pro.app'
+end
