2528f948609b133ce59e8f4248b147a3a9f905f6
commitay <commitay@users.noreply.github.com>
2017-08-10 07:50:06 +1000

    Update airtable.rb

--- Casks/airtable.rb
+++ Casks/airtable.rb
@@ -2,7 +2,7 @@ cask 'airtable' do
   version '1.1.12'
   sha256 '3d54d45b7f65119dc2b94cc0a6d086b65cd1d188ddd2b4b1b7d22dc5e1def14c'
 
-  url 'https://static.airtable.com/download/macos/Airtable-1.1.12.dmg'
+  url "https://static.airtable.com/download/macos/Airtable-#{version}.dmg"
   name 'Airtable'
   homepage 'https://airtable.com/'
 
