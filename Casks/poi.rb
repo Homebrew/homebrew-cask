6d7e9e542d5cbc320defba26bc98de8ea1ab875d
darg <darg20127@gmail.com>
2017-08-10 21:44:04 +0800

    Update poi to 7.9.0

--- Casks/poi.rb
+++ Casks/poi.rb
@@ -1,11 +1,11 @@
 cask 'poi' do
-  version '7.8.0'
-  sha256 '840fc068b106b07de19098f06c98b6590dc6626e43587d3a65bf5c519a5dc3ba'
+  version '7.9.0'
+  sha256 'e5cef1c67f7a27424bb483dc8ad89f09f3f97b922cecbe18360448eff8ee65f9'
 
   # github.com/poooi/poi was verified as official when first introduced to the cask
   url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}.dmg"
   appcast 'https://github.com/poooi/poi/releases.atom',
-          checkpoint: '6e500d26ec04e58eb2cab4ef67b706ea027024f20543020ef19133e1a963d65f'
+          checkpoint: '991485a28130a1ead77efea2da6e1ca581b1c39f8808c7c91fb9ad8302ae929b'
   name 'poi'
   homepage 'https://poi.io/'
 
