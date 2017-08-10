203caff4e4ccb3231613e28d01f5feb888151a95
Maxime Gervais <gervais.maxime@gmail.com>
2017-08-10 15:07:59 +0200

    Update mediaconch-app to 17.07

--- Casks/mediaconch-app.rb
+++ Casks/mediaconch-app.rb
@@ -1,10 +1,10 @@
 cask 'mediaconch-app' do
-  version '17.06'
-  sha256 '320446267f0fb4641ed58c7661daf5451bf2fccf1354ed871e98ddc16a4b3c08'
+  version '17.07'
+  sha256 'e55fc35a283aa20a6f88fd0d79ac13cea833151c5cd24cc3a391d1291b91542b'
 
   url "https://mediaarea.net/download/binary/mediaconch-gui/#{version}/MediaConch_GUI_#{version}_Mac.dmg"
   appcast 'https://github.com/MediaArea/MediaConch_SourceCode/releases.atom',
-          checkpoint: 'b576679616159700ccc5236048759a15f0032cdfc7415d354796a2355ad080d9'
+          checkpoint: 'bafd4360128723c5db1492e4105d539dd8e9911db83386b10d8264c206afb8d6'
   name 'MediaConch'
   homepage 'https://mediaarea.net/MediaConch/'
 
