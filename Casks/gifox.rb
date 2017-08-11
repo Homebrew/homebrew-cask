23416d2ca0fe5c685d595f55d083d4bfd5518f60
Nils Neumann <mail@der-zyklop.de>
2017-08-11 22:30:28 +0200

    Add Gifox.app v1.3.0

---
+++ Casks/gifox.rb
@@ -0,0 +1,10 @@
+cask 'gifox' do
+  version '1.3.0'
+  sha256 'c5e1bd3bf6595bdf43463a2a150a20f1459dd9c239a79af8c3b86b54f7888c7a'
+
+  url 'https://gifox.io/download'
+  name 'gifox'
+  homepage 'https://gifox.io/'
+
+  app 'Gifox.app'
+end
