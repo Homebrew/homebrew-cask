766da1b13fb68c5f8826368414ae10b3a3fdd4a0
commitay <commitay@users.noreply.github.com>
2017-08-12 05:13:17 +1000

    Add focus-booster 2.2.0 (#37578)

---
+++ Casks/focus-booster.rb
@@ -0,0 +1,11 @@
+cask 'focus-booster' do
+  version '2.2.0'
+  sha256 'a58f376eaa0588893bf64555699408af5cf0ab6c3f3a15558430c0c3ec1e8a1a'
+
+  # dcwy7j973b6zk.cloudfront.net/downloads/stable was verified as official when first introduced to the cask
+  url "https://dcwy7j973b6zk.cloudfront.net/downloads/stable/focusbooster#{version}.dmg"
+  name 'Focus Booster'
+  homepage 'https://www.focusboosterapp.com/'
+
+  app 'focus booster.app'
+end
