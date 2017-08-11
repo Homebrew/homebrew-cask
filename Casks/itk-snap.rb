94edecab7d8e56a8d7d40b24a0b2bc08737fcda4
Fernando <fepegar@gmail.com>
2017-08-11 03:19:59 +0100

    Add ITK-SNAP 3.4.0

---
+++ Casks/itk-snap.rb
@@ -0,0 +1,10 @@
+cask 'itk-snap' do
+  version '3.4.0'
+  sha256 'f1b6d98db85e2f727402860baf6a7f9ff39e53aa4d27bc187ecf863cc5b86d59'
+
+  url 'http://www.nitrc.org/frs/downloadlink.php/7960'
+  name 'ITK-SNAP'
+  homepage 'http://www.itksnap.org/'
+
+  app 'ITK-SNAP.app'
+end
