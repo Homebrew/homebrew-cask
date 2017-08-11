4d95c4358ea6b742ec6811e1cc516b896e7ccdce
commitay <commitay@users.noreply.github.com>
2017-08-11 20:53:11 +1000

    Update forklift - uninstall / zap

--- Casks/forklift.rb
+++ Casks/forklift.rb
@@ -13,15 +13,26 @@ cask 'forklift' do
 
   app 'ForkLift.app'
 
+  uninstall delete:    '/Library/PrivilegedHelperTools/com.binarynights.ForkLiftHelper',
+            launchctl: [
+                         'com.binarynights.ForkLiftHelper',
+                         'com.binarynights.ForkLiftMini',
+                       ],
+            quit:      [
+                         "com.binarynights.ForkLift-#{version.major}",
+                         'com.binarynights.ForkLiftMini',
+                       ]
+
   zap delete: [
-                '/Library/LaunchDaemons/com.binarynights.ForkLiftHelper.plist',
-                '/Library/PrivilegedHelperTools/com.binarynights.ForkLiftHelper',
-                '~/Library/Application Support/ForkLift',
-                '~/Library/Caches/com.binarynights.ForkLift-3',
-                '~/Library/Cookies/com.binarynights.ForkLift-3.binarycookies',
+                "~/Library/Caches/com.binarynights.ForkLift-#{version.major}",
+                "~/Library/Cookies/com.binarynights.ForkLift-#{version.major}.binarycookies",
                 '~/Library/Logs/ForkLift',
-                '~/Library/Preferences/com.binarynights.ForkLift-3.plist',
+                '~/Library/Logs/ForkLiftMini',
+                "~/Library/Saved Application State/com.binarynights.ForkLift-#{version.major}.savedState",
+              ],
+      trash:  [
+                '~/Library/Application Support/ForkLift',
+                "~/Library/Preferences/com.binarynights.ForkLift-#{version.major}.plist",
                 '~/Library/Preferences/com.binarynights.ForkLiftMini.plist',
-                '~/Library/Saved Application State/com.binarynights.ForkLift-3.savedState',
               ]
 end
