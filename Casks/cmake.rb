60a0d884d1bdb341e48c34312abbdbe7b3c7cc3e
commitay <commitay@users.noreply.github.com>
2017-08-11 16:39:39 +1000

    Update cmake to 3.9.1 (#37589)

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -1,6 +1,6 @@
 cask 'cmake' do
-  version '3.9.0'
-  sha256 'a522b23ea1a86544f135c1261833975e7d63e6cdfadeb32f8367c485d998ee6a'
+  version '3.9.1'
+  sha256 'dfca4071944576b19953ea9fb3e6e034baf9fcee03ab4d7ea17163fd5c6c1e80'
 
   url "https://www.cmake.org/files/v#{version.major_minor}/cmake-#{version}-Darwin-x86_64.dmg"
   name 'CMake'
