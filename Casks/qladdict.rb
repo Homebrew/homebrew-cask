0ae09c0e390c982a0c9010cbb749e240e648a7c0
tattali <theoattali@gmail.com>
2017-08-12 02:12:51 +0200

    Update qladdict to 1.2.4 (#37613)

--- Casks/qladdict.rb
+++ Casks/qladdict.rb
@@ -1,10 +1,10 @@
 cask 'qladdict' do
-  version '1.2.3'
-  sha256 'bee39f46772ab709aae84bfbaa95f68294cc7de13969c8fa58afb3c94661d483'
+  version '1.2.4'
+  sha256 '2af44d8392f75616c7852f37e627b674d561306328351331cb592ac93ea244cd'
 
   url "https://github.com/tattali/QLAddict/releases/download/#{version}/QLAddict.qlgenerator.#{version}.zip"
   appcast 'https://github.com/tattali/QLAddict/releases.atom',
-          checkpoint: 'af4bd13681cb0a00e4fc3277257d01ffe8bbffc03292fcfa059687e0a7790c7d'
+          checkpoint: '6a7c4a7082cbcd9b69eccc2587ca853c4c840e79b33f3ad698962df108cf75bb'
   name 'QLAddict'
   homepage 'https://github.com/tattali/QLAddict/'
 
