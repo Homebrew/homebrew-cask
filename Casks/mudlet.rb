29b78664b71b2da5f61a173d0a33c4a04b8d1aef
commitay <commitay@users.noreply.github.com>
2017-08-13 09:00:48 +1000

    https

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -2,7 +2,7 @@ cask 'mudlet' do
   version '3.4.0'
   sha256 '5db3b751bdca324d8cc37db1f38d3f93600fbf0c722b4691f0f6d25274b02183'
 
-  url "http://www.mudlet.org/download/Mudlet-#{version}.dmg"
+  url "https://www.mudlet.org/download/Mudlet-#{version}.dmg"
   appcast 'https://github.com/Mudlet/Mudlet/releases.atom',
           checkpoint: '4e76377fea71f981fe492ab7cb5357de55fb06ba96a39fbb81d57f5a0cf0fd01'
   name 'Mudlet'
