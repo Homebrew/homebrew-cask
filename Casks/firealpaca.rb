900d96da43f2f4c78a0022b2a47809ff9dfb8325
commitay <commitay@users.noreply.github.com>
2017-08-15 07:42:56 +1000

    Update firealpaca.rb

--- Casks/firealpaca.rb
+++ Casks/firealpaca.rb
@@ -1,8 +1,9 @@
 cask 'firealpaca' do
-  version '1.8.2'
-  sha256 'ad0897f5b35cd54f411fcf19ef590b119f56801690d383e1fab5a199e9fa5fea'
+  version :latest
+  sha256 :no_check
 
-  url 'http://firealpaca.com/download/mac'
+  url 'http://firealpaca.com/download/mac',
+      referer: 'http://firealpaca.com/'
   name 'Fire Alpaca'
   homepage 'https://firealpaca.com/'
 
