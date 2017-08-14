055827fe55164188af67ebec9a58550bac95c1c7
commitay <commitay@users.noreply.github.com>
2017-08-15 08:24:39 +1000

    Update mono-mdk to 5.2.0.215 (#37669)

--- Casks/mono-mdk.rb
+++ Casks/mono-mdk.rb
@@ -1,11 +1,11 @@
 cask 'mono-mdk' do
-  version '5.0.1.1'
-  sha256 '60eef93597fd4fc0fa617e0bd1df14da506611087e4117ac366b332ecf70a5a0'
+  version '5.2.0.215'
+  sha256 'bf7e7c0439783015168662833754bd83f1a9ffe5970919213a762f313cc53909'
 
   # mono-project.azureedge.net/archive was verified as official when first introduced to the cask
   url "https://mono-project.azureedge.net/archive/#{version.major_minor_patch}/macos-10-universal/MonoFramework-MDK-#{version}.macos10.xamarin.universal.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '08fef545d50bd40a08d194b082bba6752cb27453d3381fd4c0504bdf18dd6ab2'
+          checkpoint: '3440e32fa546571a45474a6bc9bbe8047cb1f8035af1679c502e49f1ac9de4d7'
   name 'Mono'
   homepage 'http://www.mono-project.com/'
 
