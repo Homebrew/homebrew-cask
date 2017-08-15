e3e419fe48920388473d030a251f2bddf2551374
Mathew Peterson <mathewpeterson@me.com>
2017-08-15 03:22:23 -0500

    update dotnet-sdk version to  2.0.0 (#37661)
    * update dotnet-sdk version to  2.0.0
    
    * Update dotnet-sdk.rb

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -1,38 +1,14 @@
 cask 'dotnet-sdk' do
-  version '1.0.4'
-  sha256 'ec1e1eafad419c86933a5c2c07841a9f4bd65f1145883cad2f95d5028bf70460'
+  version '2.0.0'
+  sha256 '69300051d696f5ae8e42bd79e5aa13a08116723d79c324fd248df7ead2869291'
 
-  url "https://download.microsoft.com/download/B/9/F/B9F1AF57-C14A-4670-9973-CDF47209B5BF/dotnet-dev-osx-x64.#{version}.pkg"
+  url "https://download.microsoft.com/download/0/F/D/0FD852A4-7EA1-4E2A-983A-0484AC19B92C/dotnet-sdk-#{version}-osx-x64.pkg"
   name '.NET Core SDK'
   homepage 'https://www.microsoft.com/net/core#macos'
 
-  depends_on formula: 'openssl'
+  depends_on macos: '>= :sierra'
 
-  pkg "dotnet-dev-osx-x64.#{version}.pkg"
-
-  # Patch .NET Core to use the latest version of OpenSSL installed via Homebrew.
-  # https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md#openssl
-  postflight do
-    run_times = ['1.0.5', '1.1.2']
-    run_times.each do |version|
-      dotnet_core = "/usr/local/share/dotnet/shared/Microsoft.NETCore.App/#{version}"
-      open_ssl_lib = version == '1.0.5' ? 'System.Security.Cryptography.Native.dylib' : 'System.Security.Cryptography.Native.OpenSsl.dylib'
-
-      system_command '/usr/bin/install_name_tool',
-                     args: [
-                             "#{dotnet_core}/#{open_ssl_lib}",
-                             '-add_rpath', "#{HOMEBREW_PREFIX}/opt/openssl/lib"
-                           ],
-                     sudo: true
-      system_command '/usr/bin/install_name_tool',
-                     args: [
-                             "#{dotnet_core}/System.Net.Http.Native.dylib",
-                             '-change', '/usr/lib/libcurl.4.dylib',
-                             "#{HOMEBREW_PREFIX}/opt/curl/lib/libcurl.4.dylib"
-                           ],
-                     sudo: true
-    end
-  end
+  pkg "dotnet-sdk-#{version}-osx-x64.pkg"
 
   uninstall pkgutil: 'com.microsoft.dotnet.*',
             delete:  '/etc/paths.d/dotnet'
