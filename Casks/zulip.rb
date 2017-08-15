d90890ec8c36c07809f7631a18e02324c83f9b39
Akash Nimare <akashnimare@users.noreply.github.com>
2017-08-15 17:54:16 +0530

    Update zulip app to v1.3.0-beta (#37683)
    * update zulip app to v1.3.0-beta
    
    * appcast checkpoint

--- Casks/zulip.rb
+++ Casks/zulip.rb
@@ -1,11 +1,11 @@
 cask 'zulip' do
-  version '1.2.0-beta'
-  sha256 'a1416d3a2a08ff1de111c108b7702dddb840e4634f19760d0e76d31d2d72d1b1'
+  version '1.3.0-beta'
+  sha256 'dae94d80307256e7b756e69feafc9db0a3fe7ccbc9c5ff472236ef6d5121a776'
 
   # github.com/zulip/zulip-electron was verified as official when first introduced to the cask
   url "https://github.com/zulip/zulip-electron/releases/download/v#{version}/Zulip-#{version}-mac.zip"
   appcast 'https://github.com/zulip/zulip-electron/releases.atom',
-          checkpoint: '354ae69858f6eaf8befa93b7b61424e6946a7601820897959f02411e950ff05d'
+          checkpoint: 'd12d339f206fd44197f395f720ff297815daace0f97755b190ff8b37b650c91e'
   name 'Zulip'
   homepage 'https://zulipchat.com/'
 
