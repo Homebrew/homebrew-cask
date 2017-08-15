39c8976ad19323b894add6ee2f720669e7bf873c
commitay <commitay@users.noreply.github.com>
2017-08-15 22:17:13 +1000

    Update and rename gif-for-mac.rb to tenor.rb

--- Casks/tenor.rb
+++ Casks/tenor.rb
@@ -1,10 +1,10 @@
-cask 'gif-for-mac' do
+cask 'tenor' do
   version :latest
   sha256 :no_check
 
   # media.tenor.co/mac/bin was verified as official when first introduced to the cask
   url 'https://media.tenor.co/mac/bin/GIFforMac.dmg'
-  name 'GIF for Mac'
+  name 'Tenor'
   homepage 'https://tenor.com/mac'
 
   app 'Tenor.app'
