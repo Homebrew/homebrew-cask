9bc441b01304ccf74025474a01646b059ee981b6
commitay <commitay@users.noreply.github.com>
2017-08-15 10:42:45 +1000

    Update dotnet-sdk.rb

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -6,33 +6,9 @@ cask 'dotnet-sdk' do
   name '.NET Core SDK'
   homepage 'https://www.microsoft.com/net/core#macos'
 
-  depends_on formula: 'openssl'
+  depends_on macos: '>= :sierra'
 
   pkg "dotnet-sdk-#{version}-osx-x64.pkg"
-
-  # Patch .NET Core to use the latest version of OpenSSL installed via Homebrew.
-  # https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md#openssl
-  postflight do
-    run_times = ['2.0.0']
-    run_times.each do |version|
-      dotnet_core = "/usr/local/share/dotnet/shared/Microsoft.NETCore.App/#{version}"
-      open_ssl_lib = 'System.Security.Cryptography.Native.OpenSsl.dylib'
-
-      system_command '/usr/bin/install_name_tool',
-                     args: [
-                             "#{dotnet_core}/#{open_ssl_lib}",
-                             '-add_rpath', "#{HOMEBREW_PREFIX}/opt/openssl/lib"
-                           ],
-                     sudo: true
-      system_command '/usr/bin/install_name_tool',
-                     args: [
-                             "#{dotnet_core}/System.Net.Http.Native.dylib",
-                             '-change', '/usr/lib/libcurl.4.dylib',
-                             "#{HOMEBREW_PREFIX}/opt/curl/lib/libcurl.4.dylib"
-                           ],
-                     sudo: true
-    end
-  end
 
   uninstall pkgutil: 'com.microsoft.dotnet.*',
             delete:  '/etc/paths.d/dotnet'
