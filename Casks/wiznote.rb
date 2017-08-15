fbd66fbb5bbb2f72510ce3d7fc6e0dc883c17ecf
Robert LU <robberphex@gmail.com>
2017-08-15 20:53:51 +0800

    update wiznote to 2.5.6 (#37685)

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,6 +1,6 @@
 cask 'wiznote' do
-  version '2.5.5,2017-06-19'
-  sha256 'c2b9e1553312fd098a01449dafab16f3d3fecfe3ca0cb52a909b748f41841755'
+  version '2.5.6,2017-08-09'
+  sha256 '8efbec9678b1f87d04e50e5ffea0db1909eba98736b3a2d0a91e00351ac539a9'
 
   url "http://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
   name 'WizNote'
