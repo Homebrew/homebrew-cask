d9e825990b505e601efa115db6f700de724e9d14
Vítor Galvão <info@vitorgalvao.com>
2017-08-16 18:07:25 +0100

    Update clocker.rb

--- Casks/clocker.rb
+++ Casks/clocker.rb
@@ -2,8 +2,8 @@ cask 'clocker' do
   version '1.5.1'
   sha256 'd9003079b1c2451932281afab9742943897b0b4015181cc7e1ed1c6c3651db84'
 
-  # Homepage url shows promotional text and download links for the app
-  url 'https://github.com/abhishekbanthia/Clocker/releases/download/v1.5.1/Clocker.zip'
+  # github.com/abhishekbanthia/Clocker was verified as official when first introduced to the cask
+  url "https://github.com/abhishekbanthia/Clocker/releases/download/v#{version}/Clocker.zip"
   name 'Clocker'
   homepage 'https://abhishekbanthia.com/Clocker'
 
