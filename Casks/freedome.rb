4cc8793b6f3ec8024791554225de6fae7a1ccea9
commitay <commitay@users.noreply.github.com>
2017-08-16 16:08:11 +1000

    Update freedome - uninstall / zap (#37706)

--- Casks/freedome.rb
+++ Casks/freedome.rb
@@ -7,4 +7,24 @@ cask 'freedome' do
   homepage 'https://www.f-secure.com/en_US/web/home_us/freedome'
 
   app 'Freedome.app'
+
+  uninstall_preflight do
+    set_ownership "#{appdir}/Freedome.app"
+  end
+
+  uninstall delete:    '/Library/Application Support/F-Secure/FSFreedome',
+            launchctl: 'com.fsecure.freedome.uninstall',
+            quit:      'com.fsecure.freedome.osx',
+            script:    {
+                         executable: '/Library/Application Support/F-Secure/FSFreedome/admin/UninstallFreedome.sh',
+                         args:       ["#{appdir}/Freedome.app"],
+                         sudo:       true,
+                       }
+
+  zap delete: '~/Library/Caches/com.fsecure.freedome.osx',
+      rmdir:  [
+                '~/Library/Application Support/F-Secure/',
+                '/Library/Application Support/F-Secure',
+              ],
+      trash:  '~/Library/Application Support/F-Secure/FSFreedome'
 end
