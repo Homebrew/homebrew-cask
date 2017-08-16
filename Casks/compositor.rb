0bffb4090e43c27179f55f957f972886de7b7119
commitay <commitay@users.noreply.github.com>
2017-08-17 00:25:13 +1000

    Update compositor.rb

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,8 +1,10 @@
 cask 'compositor' do
-  version ':latest'
+  version '0.9.6'
   sha256 'ce3a597efaa23450f684af996e5566ebdf414cc8a0564e76e900ecf137dc6e2a'
 
   url 'http://compositorapp.com/downloads/Compositor.dmg'
+  appcast 'http://compositorapp.com/updates/appcast.xml',
+          checkpoint: 'fca97ef0ec6c707a7fae46a76081e9715ddf9bdc64541d36db073981b9aaea31'
   name 'Compositor'
   homepage 'http://compositorapp.com/'
 
