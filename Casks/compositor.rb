b95ffd1b5d1c79913a81ec1b3a3ae9786a255510
Samuel Badr <samuel.badr@gmail.com>
2017-08-16 16:11:45 +0200

    Add Compositor.app Latest

---
+++ Casks/compositor.rb
@@ -0,0 +1,10 @@
+cask 'compositor' do
+  version ':latest'
+  sha256 'ce3a597efaa23450f684af996e5566ebdf414cc8a0564e76e900ecf137dc6e2a'
+
+  url 'http://compositorapp.com/downloads/Compositor.dmg'
+  name 'Compositor'
+  homepage 'http://compositorapp.com/'
+
+  app 'Compositor.app'
+end
