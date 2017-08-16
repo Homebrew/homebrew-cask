1a9b9cea4745d0ca46d2cde3ab72873aa11985e2
Abhishek <abhishek_banthia@gallup.com>
2017-08-16 11:07:54 -0500

    Adding Clocker.

---
+++ Casks/clocker.rb
@@ -0,0 +1,10 @@
+cask 'Clocker' do
+  version :latest
+  sha256 '2dee0e5986200860e0d2743d1170a1589de00b33'
+
+  url "https://github.com/abhishekbanthia/Clocker/releases/download/v#{version}/Clocker.zip"
+  name 'Clocker'
+  homepage 'https://abhishekbanthia.com/Clocker/'
+  app 'Clocker.app'
+
+end
