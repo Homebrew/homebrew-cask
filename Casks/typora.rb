9b270d2ba2c69642792479ebfe8f1631811e83ca
commitay <commitay@users.noreply.github.com>
2017-08-16 17:45:45 +1000

    Update typora.rb

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -2,7 +2,7 @@ cask 'typora' do
   version '0.9.9.10.3'
   sha256 '9727c362270dfc2fb021305876983881a52d2cd6edde34f8124ef5cafc12d013'
 
-  url "https://www.typora.io/download/Typora.zip?"
+  url "https://www.typora.io/download/Typora.zip"
   appcast 'https://www.typora.io/download/dev_update.xml',
           checkpoint: '5c265f58de9778fc53cfa4d7e9f51fb51825f7f83042f48bce862e50a4a41d34'
   name 'Typora'
