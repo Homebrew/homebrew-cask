281ee46f13e0e012c8cd40fe2fee62c294d869b7
Abhishek <abhishek_banthia@gallup.com>
2017-08-16 14:23:43 -0500

    Added Appcast and zap delete.

--- Casks/clocker.rb
+++ Casks/clocker.rb
@@ -4,8 +4,16 @@ cask 'clocker' do
 
   # Homepage url shows promotional text and download links for the app
   url 'https://github.com/abhishekbanthia/Clocker/releases/download/v1.5.1/Clocker.zip'
+  appcast 'https://github.com/abhishekbanthia/Clocker/blob/v1.4.1/Releases/appcast.xml',
+          checkpoint: '7974dd324893c748f304e89a0e8e82531d14a21ddfa9938ce5ecff5b003605bc'
   name 'Clocker'
   homepage 'https://abhishekbanthia.com/Clocker'
 
   app 'Clocker.app'
+
+  zap delete: [
+                '~/Library/Preferences/com.abhishek.Clocker.plist',
+                '~/Library/Preferences/com.abhishek.ClockerHelper.plist',
+                '~/Library/Preferences/com.abhishek.Clocker.prefs',
+              ]
 end
