e7f8bade12e6ef613f8e72812cf694f7bbd17ed6
Vítor Galvão <info@vitorgalvao.com>
2017-08-17 11:51:02 +0100

    Update webpack-dashboard.rb

--- Casks/webpack-dashboard.rb
+++ Casks/webpack-dashboard.rb
@@ -1,8 +1,8 @@
 cask 'webpack-dashboard' do
-  version :latest
+  version '0.0.1-beta.1,1.0.0'
   sha256 :no_check
 
-  url 'https://github.com/FormidableLabs/electron-webpack-dashboard/releases/download/0.0.1-beta.1/Webpack.Dashboard-1.0.0.dmg'
+  url "https://github.com/FormidableLabs/electron-webpack-dashboard/releases/download/#{version.before_comma}/Webpack.Dashboard-#{version.after_comma}.dmg"
   name 'Webpack Dashboard'
   homepage 'https://github.com/FormidableLabs/electron-webpack-dashboard'
 
