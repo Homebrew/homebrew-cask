dc241cb43091cfb3672b67b2a5f716bf273e41a1
jcbot <jcb@leipert.io>
2017-08-17 23:29:01 +0200

    Update intellij-idea-ce to 2017.2.2,172.3757.52

--- Casks/intellij-idea-ce.rb
+++ Casks/intellij-idea-ce.rb
@@ -1,10 +1,10 @@
 cask 'intellij-idea-ce' do
-  version '2017.2.1,172.3544.35'
-  sha256 '0cea2649dddb6c778c975fb1d6e18c64936a23ec49457bb5d290aa624d7c0f7d'
+  version '2017.2.2,172.3757.52'
+  sha256 '4ec5b5440c7ba9fb5fb19c227457314dd8a7c8b238ac9bc452a9a8c83b993740'
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=release',
-          checkpoint: 'c3cb9e1d4ee4ca6e68e3ac7ae9301d355beef9a5903746b7c3655005c015bfd4'
+          checkpoint: '5c2708369a51548b1f5635846b43c8e393ea832a6f68db596879ee97f7609925'
   name 'IntelliJ IDEA Community Edition'
   name 'IntelliJ IDEA CE'
   homepage 'https://www.jetbrains.com/idea/'
