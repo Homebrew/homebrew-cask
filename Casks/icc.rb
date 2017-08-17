ab95f5efed8af6e99c83abac4e1474c66de82e4e
Vítor Galvão <info@vitorgalvao.com>
2017-08-17 18:16:27 +0100

    Update icc.rb

--- Casks/icc.rb
+++ Casks/icc.rb
@@ -2,11 +2,11 @@ cask 'icc' do
   version '1.0.r7564'
   sha256 '28b8fbd96efc571bc00f8088c41dd836a118451cbe39d69e83ba0c5d52c00491'
 
-  url "http://download.chessclub.com/desktop/mac/ICCforMac.#{%r{\d+\.\d+\.(.*)}.match(version)[1]}.pkg"
+  url "http://download.chessclub.com/desktop/mac/ICCforMac.#{version.patch}.pkg"
   name 'ICC for Mac'
   homepage 'https://www.chessclub.com/download-software/icc-for-mac'
 
-  pkg "ICCforMac.#{%r{\d+\.\d+\.(.*)}.match(version)[1]}.pkg"
+  pkg "ICCforMac.#{version.patch}.pkg"
 
   uninstall pkgutil: 'com.chessclub.*'
 end
