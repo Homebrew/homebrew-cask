c4b2b194fe83235f88d3e1bacf80a19a4ee82b18
commitay <commitay@users.noreply.github.com>
2017-08-18 20:38:58 +1000

    Update jaxx.rb

--- Casks/jaxx.rb
+++ Casks/jaxx.rb
@@ -2,7 +2,7 @@ cask 'jaxx' do
   version '1.2.25'
   sha256 'b5f6b8a76c445409e900ed9172cdada3f20cf359ae4955206aa4b5ab825e6c2c'
 
-  url "https://jaxx.io/files/#{version}/Jaxx-Installer-v#{version}.osx.dmg"
+  url "https://jaxx.io/files/#{version}/Jaxx-v#{version}.osx.dmg"
   name 'Jaxx Blockchain Wallet'
   homepage 'https://jaxx.io/'
 
