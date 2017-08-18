4f3c009d940cb37fd62ad5c9162690171ab53a1a
chiffon-rouge <chiffon-rouge@users.noreply.github.com>
2017-08-18 14:01:06 +0200

    Update jaxx.rb to 1.2.25 (#37773)
    * Update jaxx.rb
    
    Update Jaxx to the latest version.
    
    * Update jaxx.rb

--- Casks/jaxx.rb
+++ Casks/jaxx.rb
@@ -1,8 +1,8 @@
 cask 'jaxx' do
-  version '1.2.13'
-  sha256 'cf1d3ed46029fd6ae3b8e4d87bcdc703ba03ec2a9aaf004bee1e97233a8ec387'
+  version '1.2.25'
+  sha256 'b5f6b8a76c445409e900ed9172cdada3f20cf359ae4955206aa4b5ab825e6c2c'
 
-  url "https://jaxx.io/files/#{version}/Jaxx-Installer-v#{version}.osx.dmg"
+  url "https://jaxx.io/files/#{version}/Jaxx-v#{version}.osx.dmg"
   name 'Jaxx Blockchain Wallet'
   homepage 'https://jaxx.io/'
 
