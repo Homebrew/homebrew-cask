b3d34446972c69ed67f702f87a126fbd0b4c762f
commitay <commitay@users.noreply.github.com>
2017-08-20 09:16:36 +1000

    Update carbon-copy-cloner - uninstall / zap (#37826)

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -7,4 +7,19 @@ cask 'carbon-copy-cloner' do
   homepage 'https://bombich.com/'
 
   app 'Carbon Copy Cloner.app'
+
+  uninstall login_item: 'CCC User Agent',
+            quit:       [
+                          'com.bombich.ccc',
+                          'com.bombich.cccuseragent',
+                        ]
+
+  zap delete: [
+                '~/Library/Caches/com.bombich.ccc',
+                '~/Library/Saved Application State/com.bombich.ccc.savedState',
+              ],
+      trash:  [
+                '~/Library/Application Support/com.bombich.ccc',
+                '~/Library/Preferences/com.bombich.ccc.plist',
+              ]
 end
