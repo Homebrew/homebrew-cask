908b0ad5d59948a74545b242cd30f062b4ca161f
alswl <alswlx@gmail.com>
2017-08-20 00:02:34 +0800

    fix: cctalk name error

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -6,7 +6,7 @@ cask 'cctalk' do
   url "http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/archive/#{version.before_comma.hyphens_to_dots}/CCtalk-#{version.before_comma}-xianghu-#{version.after_comma}.dmg"
   appcast 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/update/info.xml',
           checkpoint: '684d0ca3504362b51993b9dd99a82c8a1ee77b9f91be7cf1a640ae0f5f27a7dd'
-  name 'CCTalk'
+  name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
 
   app 'CCtalk.app'
