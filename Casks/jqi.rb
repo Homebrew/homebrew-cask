3ff1cc24ba43be7367c16d617a189db880244e72
Steven Ringo <git@stevenringo.com>
2017-08-21 07:12:52 +1000

    Add new cask jqi 1.4.7

---
+++ Casks/jqi.rb
@@ -0,0 +1,11 @@
+cask 'jqi' do
+  version '1.4.7'
+  sha256 'ac2382f052b264e1084a688d1e72f476f4f462b59ba1d438840853aba211b26a'
+
+  url 'https://www.dropbox.com/s/cjcujamk1nzwmsa/jqi.dmg?dl=1'
+  # www.dropbox.com was verified as official when first introduced to the cask
+  name 'jqi'
+  homepage 'https://nire0510.github.io/jqi/'
+
+  app 'jqi.app'
+end
