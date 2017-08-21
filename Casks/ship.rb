4b2455a9fc74bb4f4a7c16dc5b459bc2f7c4c86b
Victor Popkov <victor@popkov.me>
2017-08-22 01:18:47 +0300

    Update ship to 2.6.1 (#37875)

--- Casks/ship.rb
+++ Casks/ship.rb
@@ -1,10 +1,10 @@
 cask 'ship' do
-  version '2.5.4'
-  sha256 '2217af411a6d146e1f67bce5e05d7c310089ef7b7d3c3bb4fb1217e1cf98197a'
+  version '2.6.1'
+  sha256 '69ed0b391662f585cb94e7364288a82896b8a147be6aeb4ea38bb2943b666cfb'
 
   url "https://www.realartists.com/builds/#{version.major}.0/Ship.app.zip"
   appcast "https://www.realartists.com/builds/#{version.major}.0/sparkle.xml",
-          checkpoint: '8385ead1633dbbd7e10234c3e1c9efea78827d2d4d516b8868b08bdf6ab84fbf'
+          checkpoint: 'ff13d107d4a23501b61b9395bef03006b54f40aa5319fd50b0f73fca54b61348'
   name 'Ship'
   homepage 'https://www.realartists.com/'
 
