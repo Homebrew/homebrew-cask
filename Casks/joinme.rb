6b0775974b40f05ae108cd8e75542068c8d1af13
commitay <commitay@users.noreply.github.com>
2017-08-21 20:22:52 +1000

    Update joinme.rb

--- Casks/joinme.rb
+++ Casks/joinme.rb
@@ -9,11 +9,13 @@ cask 'joinme' do
   app 'join.me.app'
 
   zap delete: [
+                '~/Library/Caches/com.logmein.join.me',
+                '~/Library/Logs/join.me*',
+                '~/Library/Saved Application State/com.logmein.join.me.savedState',
+              ],
+      trash:  [
                 '~/Library/Application Support/join.me',
                 '~/Library/Application Support/com.logmein.join.me',
                 '~/Library/Preferences/com.logmein.join.me.plist',
-                '~/Library/Saved Application State/com.logmein.join.me.savedState',
-                '~/Library/Caches/com.logmein.join.me',
-                '~/Library/Logs/join.me*',
               ]
 end
