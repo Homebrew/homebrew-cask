ac84e00fb00f702238c18997035b913c5dc8bc35
nrlquaker <ustynov.vy@gmail.com>
2017-08-21 13:07:40 +0300

    Add zap stanza to joinme

--- Casks/joinme.rb
+++ Casks/joinme.rb
@@ -7,4 +7,13 @@ cask 'joinme' do
   homepage 'https://www.join.me/'
 
   app 'join.me.app'
+
+  zap delete: [
+                '~/Library/Application Support/join.me',
+                '~/Library/Application Support/com.logmein.join.me',
+                '~/Library/Preferences/com.logmein.join.me.plist',
+                '~/Library/Saved Application State/com.logmein.join.me.savedState',
+                '~/Library/Caches/com.logmein.join.me',
+                '~/Library/Logs/join.me*',
+              ]
 end
