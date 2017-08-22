0b2b8cf168ac407e928a9f9a9ad1ce46ae95086b
commitay <commitay@users.noreply.github.com>
2017-08-22 11:38:38 +1000

    Update play to 2.0.8 (#37881)

--- Casks/play.rb
+++ Casks/play.rb
@@ -1,11 +1,11 @@
 cask 'play' do
-  version '2.0.7'
-  sha256 '38be5271a5bfe79168c3b57164f75a908dcec88f2a53607bef0b6516d90183b2'
+  version '2.0.8'
+  sha256 'b7c69c9149ddc46ed04020cbf87e4036d3d862584ad0da5f01044e0c1bdfa8c5'
 
   # github.com/pmsaue0/play was verified as official when first introduced to the cask
   url "https://github.com/pmsaue0/play/releases/download/v#{version}/play_#{version}.dmg.zip"
   appcast 'https://github.com/pmsaue0/play/releases.atom',
-          checkpoint: '5d4d468b6c0a2e4a17e8257400da857f92e1a2e338ae7d17a3a56a534361b3d4'
+          checkpoint: '918770f4ae2268efbf810b53e80ade1eb3795eebd2e5038ef7387f4991ca2f92'
   name 'Play'
   homepage 'https://pmsaue0.github.io/play/'
 
