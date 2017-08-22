1300587824916c1e4b17f64d6b2f417b9742d4cd
commitay <commitay@users.noreply.github.com>
2017-08-22 23:41:23 +1000

    Update integrity to 6.11.2

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -1,11 +1,11 @@
 cask 'integrity' do
-  version '6.11.1'
-  sha256 '9a497991b2dbd6387b6a4157ff3d797f1cdf8bc92a7f87edfe40abb561705fcc'
+  version '6.11.2'
+  sha256 'c0a36b7febd035a881c4e9f2bbf60f6d9aaca795e2399c4dd3cbf0ead0561526'
 
   # peacockmedia.co.uk/integrity was verified as official when first introduced to the cask
   url 'http://peacockmedia.co.uk/integrity/integrity.dmg'
   appcast 'http://peacockmedia.software/mac/integrity/version_history.html',
-          checkpoint: '262f0b16404fbb92b16278dacd7d07ecd9a9d5e6496bc248d6df219a2e7bf1ce'
+          checkpoint: '9af958b2724edad449f8d3b9023768f720f43a6b1e28ffba3b3b44cc500fe44a'
   name 'Integrity'
   homepage 'http://peacockmedia.software/mac/integrity/'
 
