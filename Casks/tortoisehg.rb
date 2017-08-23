ce366c2d0bf72a94f1c3387db12244047436dbc1
Andrey Dyagel <AndreyDyagel@coherentsolutions.com>
2017-08-23 15:17:39 +0300

    Update tortoisehg to 4.3.1 (#37912)

--- Casks/tortoisehg.rb
+++ Casks/tortoisehg.rb
@@ -1,6 +1,6 @@
 cask 'tortoisehg' do
-  version '4.2.2'
-  sha256 '4fe37018395233f1eaaeea9a1157c983204e3cd3f1b666ac97f45bb62e3fa39a'
+  version '4.3.1'
+  sha256 '7ef1964adfc50eb4c9614c09058eb15f99af99be42f6a782447339ba4896a4ac'
 
   # bitbucket.org/tortoisehg/files/downloads was verified as official when first introduced to the cask
   url "https://bitbucket.org/tortoisehg/files/downloads/TortoiseHg-#{version}-mac-x64.dmg"
