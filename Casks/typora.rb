b22680f163f66e063bea9bb156dc1a7b50ccca72
commitay <commitay@users.noreply.github.com>
2017-08-24 20:47:58 +1000

    Update typora.rb

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -11,6 +11,7 @@ cask 'typora' do
   app 'Typora.app'
 
   zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/abnerworks.typora.sfl',
                 '~/Library/Caches/abnerworks.Typora',
                 '~/Library/Cookies/abnerworks.Typora.binarycookies',
                 '~/Library/Saved Application State/abnerworks.Typora.savedState',
@@ -19,7 +20,6 @@ cask 'typora' do
       trash:  [
                 '~/Library/Application Support/Typora',
                 '~/Library/Application Support/abnerworks.Typora',
-                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/abnerworks.typora.sfl',
                 '~/Library/Preferences/abnerworks.Typora.plist',
               ]
 end
