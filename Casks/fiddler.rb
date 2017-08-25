0b09d34750625c4732c4ebe2070cfc0ac67064b8
Idan Asraf <idan.asraf@sap.com>
2017-08-25 14:26:39 +0300

    Update fiddler to latest

--- Casks/fiddler.rb
+++ Casks/fiddler.rb
@@ -2,11 +2,12 @@ cask 'fiddler' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.telerik.com/docs/default-source/fiddler/fiddler-mac.zip'
+  # telerik-fiddler.s3.amazonaws.com was verified as official when first introduced to the cask
+  url 'https://telerik-fiddler.s3.amazonaws.com/fiddler/fiddler-mac.zip'
   name 'Telerik Fiddler Proxy'
   homepage 'http://www.telerik.com/fiddler'
 
   depends_on cask: 'mono-mdk'
 
-  app 'Fiddler.app'
+  app 'mono Fiddler.exe', target: 'Fiddler.app'
 end
