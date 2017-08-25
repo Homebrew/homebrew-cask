d494957b4d8f1791f7a61dd375dc772c862e8e10
commitay <commitay@users.noreply.github.com>
2017-08-26 09:27:21 +1000

    Update scrutiny to 7.5.5 (#38009)

--- Casks/scrutiny.rb
+++ Casks/scrutiny.rb
@@ -1,10 +1,10 @@
 cask 'scrutiny' do
-  version '7.5.3'
+  version '7.5.5'
   sha256 '3692ca7be75487999766f2fd7a5aca5debea206a90b6468ebb8c5df18eece06a'
 
   url 'http://peacockmedia.software/mac/scrutiny/scrutiny.dmg'
-  appcast "http://peacockmedia.software/mac/scrutiny/updates/v#{version.major}currentversion.plist",
-          checkpoint: '9611e910e878931d72d0d42fe0858304cfa0429a2b48b5a851ce57bc3e868f07'
+  appcast 'http://peacockmedia.software/mac/scrutiny/version_history.html',
+          checkpoint: '50b6c966f3e646f152b388d775c7bb7ff5912731086f08c5619151317d7ef583'
   name 'Scrutiny'
   homepage 'http://peacockmedia.software/mac/scrutiny/'
 
