086dbb522e8e5f2856ec28547d7240af91d57beb
commitay <commitay@users.noreply.github.com>
2017-08-26 22:42:01 +1000

    teamviewer

--- Casks/teamviewer.rb
+++ Casks/teamviewer.rb
@@ -7,31 +7,37 @@ cask 'teamviewer' do
   homepage 'https://www.teamviewer.com/'
 
   auto_updates true
+  conflicts_with cask: 'teamviewer-host'
 
   pkg 'Install TeamViewer.pkg'
 
-  uninstall pkgutil: "com.teamviewer.teamviewer#{version}(?!AuthPlugin|PriviledgedHelper).*",
-            delete:  [
-                       '/Applications/TeamViewer.app',
-                       "/Library/Fonts/TeamViewer#{version}.otf",
-                     ]
+  uninstall pkgutil:   'com.teamviewer.*',
+            delete:    [
+                         '/Applications/TeamViewer.app',
+                         "/Library/Fonts/TeamViewer#{version}.otf",
+                         '/Library/PrivilegedHelperTools/com.teamviewer.Helper',
+                         '/Library/Security/SecurityAgentPlugins/TeamViewerAuthPlugin.bundle',
+                       ],
+            launchctl: [
+                         'com.teamviewer.Helper',
+                         'com.teamviewer.desktop',
+                         'com.teamviewer.service',
+                         'com.teamviewer.teamviewer',
+                         'com.teamviewer.teamviewer_service',
+                       ],
+            quit:      'com.teamviewer.TeamViewer'
 
   zap       delete: [
-                      '/Library/LaunchAgents/com.teamviewer.teamviewer_desktop.plist',
-                      '/Library/LaunchAgents/com.teamviewer.teamviewer.plist',
-                      '/Library/LaunchDaemons/com.teamviewer.Helper.plist',
-                      '/Library/LaunchDaemons/com.teamviewer.teamviewer_service.plist',
-                      '/Library/Preferences/com.teamviewer.teamviewer.preferences.plist',
-                      '/Library/PrivilegedHelperTools/com.teamviewer.Helper',
-                      '/Library/Security/SecurityAgentPlugins/TeamViewerAuthPlugin.bundle',
-                      '~/Library/Application Support/TeamViewer',
                       '~/Library/Caches/com.teamviewer.TeamViewer',
                       '~/Library/Cookies/com.teamviewer.TeamViewer.binarycookies',
                       '~/Library/Logs/TeamViewer',
-                      '~/Library/Preferences/com.teamviewer.TeamViewer.plist',
-                      '~/Library/Preferences/com.teamviewer.teamviewer.preferences.plist',
                       '~/Library/Saved Application State/com.teamviewer.TeamViewer.savedState',
+                    ],
+            trash:  [
+                      '/Library/Preferences/com.teamviewer.teamviewer.preferences.plist',
+                      '~/Library/Application Support/TeamViewer',
+                      '~/Library/Preferences/com.teamviewer.TeamViewer.plist',
+                      '~/Library/Preferences/com.teamviewer.teamviewer.preferences.Machine.plist',
+                      '~/Library/Preferences/com.teamviewer.teamviewer.preferences.plist',
                     ]
-
-  caveats   'Performing a zap on this cask removes files pertaining to both teamviewer and teamviewer-host, so it should not be done if you only want to uninstall one of them.'
 end
