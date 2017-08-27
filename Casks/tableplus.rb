29297f6c94c2cdd9408e44bd51a2b6ddbb373a1a
Dominik Münch <dominikmuench@gmail.com>
2017-08-27 12:12:19 +0200

    Update tableplus to 1.0 (#38035)
    * Update tableplus to 1.0
    
    * Update tableplus.rb

--- Casks/tableplus.rb
+++ Casks/tableplus.rb
@@ -1,10 +1,10 @@
 cask 'tableplus' do
-  version '1.0'
-  sha256 'ca5655445c2f4674eda66fd1833a9f8dedb80937073b0b4a204100e7f2b21fe5'
+  version '1.0,59'
+  sha256 'db93faa8a2d4634b64de9c8aaa3584d25fc94e2d36483109a747af2a7fafb3fe'
 
   url 'https://tableplus.io/release/osx/tableplus_latest.zip'
   appcast 'https://tableplus.io/osx/version.xml',
-          checkpoint: 'ce45c6d4a38e7b1a4af6e1f0960117ccf8191a41b41dc5db5ad1a898f3748b3c'
+          checkpoint: '51cb29aa41a145dff4a5a7d515fa0807b8624da215a5b9c5ffd1621dfd75795b'
   name 'TablePlus'
   homepage 'https://tableplus.io/'
 
