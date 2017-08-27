7724efe1084da8d2c8caeefff213cf0783644190
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-08-28 07:39:06 +0900

    Update now to 2.4.2

--- Casks/now.rb
+++ Casks/now.rb
@@ -1,11 +1,11 @@
 cask 'now' do
-  version '2.4.0'
-  sha256 '6aeb8765369d051e5831715b9b5471b0b370cd9e747db0852f933ec0fc421dda'
+  version '2.4.2'
+  sha256 'b5974ee8a8522961af2392c021466d0aa74b5b3615dbec3529b64610b41bfa78'
 
   # github.com/zeit/now-desktop was verified as official when first introduced to the cask
   url "https://github.com/zeit/now-desktop/releases/download/#{version}/now-desktop-#{version}-mac.zip"
   appcast 'https://github.com/zeit/now-desktop/releases.atom',
-          checkpoint: '3f4af0bd3fdc3adbfa693787b9016cd3227399e7c78323040a57e2be82e64652'
+          checkpoint: 'e4f5e47b5f2306bef3edbfe4409bf5e4e817368cdb3e73eacddc6ad41499ff42'
   name 'Now'
   homepage 'https://zeit.co/now'
 
