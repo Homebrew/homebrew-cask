d645664daea526b4c515d179246725abdf63c816
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-08-27 21:22:18 +0200

    Update festify.rb

--- Casks/festify.rb
+++ Casks/festify.rb
@@ -3,11 +3,10 @@ cask 'festify' do
   sha256 '3d6c789f92988d197b8d4bba0782adb83e6a81b9a38f73c9e5233f3ff3390804'
 
   url "https://github.com/festify/app/releases/download/v#{version}/Festify-#{version}.dmg"
+  appcast 'https://github.com/Festify/app/releases.atom',
+          checkpoint: '03af11f99136809bbc144b48fb06ff51cb404e58af6a3bbe3487c7c932530056'
   name 'Festify'
   homepage 'https://github.com/festify/app'
-
-  appcast 'https://github.com/Festify/app/releases.atom',
-            checkpoint: '03af11f99136809bbc144b48fb06ff51cb404e58af6a3bbe3487c7c932530056'
 
   app 'Festify.app'
 end
