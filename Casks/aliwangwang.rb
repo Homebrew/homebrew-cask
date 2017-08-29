1a8d628a110e1e198905dbaa08a25972e87504a2
commitay <commitay@users.noreply.github.com>
2017-08-29 16:55:02 +1000

    latest

--- Casks/aliwangwang.rb
+++ Casks/aliwangwang.rb
@@ -1,10 +1,8 @@
 cask 'aliwangwang' do
-  version '20170818-1459-8.00.28'
-  sha256 '343fbba48ce26b2f8f82ad8a8e65a553dead4bd128773cb7a5775ad4e33d0fe1'
+  version :latest
+  sha256 :no_check
 
-  # dbison.alicdn.com was verified as official when first introduced to the cask
-  url "https://alimarket.taobao.com/markets/qnww/aliww-download?wh_from=macos"
-  
+  url 'https://alimarket.taobao.com/markets/qnww/aliww-download?wh_from=macos'
   name 'Ali Wangwang'
   homepage 'https://alimarket.taobao.com/markets/qnww/portal-group/ww/index'
 
