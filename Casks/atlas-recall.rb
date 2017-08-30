f0f8a24d79243b4cfa7645a6bae4186a218e1db8
commitay <commitay@users.noreply.github.com>
2017-08-30 14:14:12 +1000

    uninstall / zap

--- Casks/atlas-recall.rb
+++ Casks/atlas-recall.rb
@@ -7,4 +7,14 @@ cask 'atlas-recall' do
   homepage 'https://www.atlas.co/'
 
   app 'Atlas Recall.app'
+
+  uninstall quit: 'co.atlasinformatics.Recall'
+
+  zap delete: [
+                '~/Library/Cookies/co.atlasinformatics.Recall.binarycookies',
+                '~/Library/Logs/Atlas',
+                '~/Library/Saved Application State/co.atlasinformatics.Recall.savedState',
+                '~/Library/WebKit/co.atlasinformatics.Recall',
+              ],
+      trash:  '~/Library/Preferences/co.atlasinformatics.Recall.plist'
 end
