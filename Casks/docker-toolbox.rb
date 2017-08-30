f4a29d09515cceedbda5950270cea3eba48f8015
commitay <commitay@users.noreply.github.com>
2017-08-31 08:24:31 +1000

    Update docker-toolbox to 17.07.0-ce

--- Casks/docker-toolbox.rb
+++ Casks/docker-toolbox.rb
@@ -1,11 +1,11 @@
 cask 'docker-toolbox' do
-  version '17.06.0a-ce'
-  sha256 'ad43de197e15ea040f4f03659b238edc0ac527d5dd2f48a143e4a37d05880e61'
+  version '17.07.0-ce'
+  sha256 'c1da313e06d3dfb716f6e94e92feb8efdd2085e52e84a2545955cade59236cbd'
 
   # github.com/docker/toolbox was verified as official when first introduced to the cask
   url "https://github.com/docker/toolbox/releases/download/v#{version}/DockerToolbox-#{version}.pkg"
   appcast 'https://github.com/docker/toolbox/releases.atom',
-          checkpoint: 'db84562a2bf0cbb02e857ca89e5550bb8b5e1814dedcf2a22a57e92358be8526'
+          checkpoint: '4121971cfd6bc5dc1705f5238d83498caab6f69e60b25837a21847f9ab185bfb'
   name 'Docker Toolbox'
   homepage 'https://www.docker.com/products/docker-toolbox'
 
