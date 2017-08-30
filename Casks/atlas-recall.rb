be526e404a381a0233fae4ed8d9571eec46adb5d
shedali <shedali@gmail.com>
2017-08-30 02:45:40 +0100

    add atlas-recall http://www.atlas.co

---
+++ Casks/atlas-recall.rb
@@ -0,0 +1,10 @@
+cask 'atlas-recall' do
+  version ':latest'
+  sha256 '3facbe607b7eaf4fac3a84d880cf122fa1a5ca513af34deb37db07c6a255833f'
+
+  url 'https://download.atlas.co/release/latest/AtlasRecall.dmg'
+  name 'Atlas Recall'
+  homepage 'https://www.atlas.co/'
+
+  app 'Atlas Recall.app'
+end
