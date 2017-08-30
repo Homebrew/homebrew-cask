8235b225bb115b846401094cf67a0a15849c6fd6
commitay <commitay@users.noreply.github.com>
2017-08-30 17:55:15 +1000

    https

--- Casks/memory-map.rb
+++ Casks/memory-map.rb
@@ -2,7 +2,7 @@ cask 'memory-map' do
   version '1.2'
   sha256 'c79bb56e4e147bef606533d72e719a846c4078393f82a57dbc2cd24c4b81817b'
 
-  url "http://memory-map.com/download/mac/Memory-Map.#{version}.pkg"
+  url "https://memory-map.com/download/mac/Memory-Map.#{version}.pkg"
   name 'Memory-Map'
   homepage 'https://memory-map.com/'
 
