25c9a4f86b6555c4c77faf173f4442f8ce7c51ce
tiiiecherle <tiiiecherle@gmail.com>
2017-08-30 18:50:03 +0200

    Update the-unarchiver to 3.11.2

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -1,10 +1,10 @@
 cask 'the-unarchiver' do
-  version '3.11.1'
+  version '3.11.2'
   sha256 '12d1af6475149647f996f1b0510e067f057444852f1aa08a8dad75d6debe6ddf'
 
   url 'https://theunarchiver.com/downloads/TheUnarchiver.zip'
   appcast 'https://theunarchiver.com/updates.rss',
-          checkpoint: 'ccc12c475bce837e293f6ac608eb6f5a8a3ff07728410899a39d9b3c48457aa3'
+          checkpoint: '96b0d76d58d91ed48f16edd2567ca314b2e833d7f1baa23a9f5e076201cb3bf6'
   name 'The Unarchiver'
   homepage 'https://theunarchiver.com/'
 
