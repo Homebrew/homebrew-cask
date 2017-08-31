f0bd5cf0e1ca15afc6441fe4a75d2098060f8d90
commitay <commitay@users.noreply.github.com>
2017-09-01 02:19:54 +1000

    Update ksdiff - version / url / pkg (#38169)

--- Casks/ksdiff.rb
+++ Casks/ksdiff.rb
@@ -1,14 +1,14 @@
 cask 'ksdiff' do
-  version '2.2.0 (122)'
+  version '2.2.0,122'
   sha256 'cf32401d631e61cbbc3dc9947626174b45e8317a6cac39380067e7017e8d4c87'
 
-  url "https://cdn.kaleidoscopeapp.com/releases/ksdiff-#{version.sub(%r{.*?\((\d+)\)}, '\1')}.zip"
+  url "https://cdn.kaleidoscopeapp.com/releases/ksdiff-#{version.after_comma}-#{version.before_comma}.zip"
   name 'ksdiff'
   homepage 'https://www.kaleidoscopeapp.com/ksdiff2'
 
   conflicts_with cask: 'kaleidoscope'
 
-  pkg 'Install ksdiff.pkg'
+  pkg "ksdiff-#{version.after_comma}/Install ksdiff.pkg"
 
   uninstall pkgutil: 'com.blackpixel.kaleidoscope.ksdiff.installer.pkg'
 
