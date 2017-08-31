e872ccd7c0e12378c8eea28303d244230b9193a1
Vincent Guo <vincent.weiguo@gmail.com>
2017-09-01 07:41:27 +0800

    Update wechatwebdevtools to 1.00.170830 (#38145)
    * Update wechatwebdevtools to 1.00.170830
    
    * Update wechatwebdevtools.rb

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,8 +1,8 @@
 cask 'wechatwebdevtools' do
-  version '0.20.191900'
-  sha256 '5b72cc4383e8f0776e57ac31127bd3ff05b3d1113a6571cb58005a0d5961e5e7'
+  version '1.0.0,170830'
+  sha256 '2e18b1a91f82b07e2eddf9efa97ed4ab74b1a7c046bab2f3556ef6d86153da76'
 
-  url "https://dldir1.qq.com/WechatWebDev/#{version.no_dots}/wechat_web_devtools_#{version}.dmg"
+  url "https://dldir1.qq.com/WechatWebDev/#{version.before_comma}/20#{version.after_comma}/wechat_devtools_#{version.major_minor}#{version.patch}.#{version.after_comma}.dmg"
   name 'wechat web devtools'
   homepage 'https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/download.html'
 
