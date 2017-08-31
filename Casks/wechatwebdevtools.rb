ff2c446866e2526efd4ceebfe7f5f142fcd61f93
commitay <commitay@users.noreply.github.com>
2017-08-31 17:13:56 +1000

    Update wechatwebdevtools.rb

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,8 +1,8 @@
 cask 'wechatwebdevtools' do
-  version '1.00.170830'
+  version '1.0.0,170830'
   sha256 '2e18b1a91f82b07e2eddf9efa97ed4ab74b1a7c046bab2f3556ef6d86153da76'
 
-  url 'https://dldir1.qq.com/WechatWebDev/1.0.0/20170830/wechat_devtools_1.00.170830.dmg'
+  url "https://dldir1.qq.com/WechatWebDev/#{version.before_comma}/20#{version.after_comma}/wechat_devtools_#{version.major_minor}#{version.patch}.#{version.after_comma}.dmg"
   name 'wechat web devtools'
   homepage 'https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/download.html'
 
