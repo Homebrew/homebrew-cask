6158ee4cdaf3aa8756d5be8d25846d78a175310b
commitay <commitay@users.noreply.github.com>
2017-09-01 13:05:08 +1000

    Update ableton-live casks (#38183)

--- Casks/ableton-live-suite.rb
+++ Casks/ableton-live-suite.rb
@@ -2,7 +2,7 @@ cask 'ableton-live-suite' do
   version '9.7.3'
   sha256 '97a8083b2b8776421a4f6a67f5074ce2f3b36def6b01f7d16e28ffdd86354dac'
 
-  url "http://cdn2-downloads.ableton.com/channels/#{version}/ableton_live_suite_#{version}_64.dmg"
+  url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_suite_#{version}_64.dmg"
   name 'Ableton Live Suite'
   homepage 'https://www.ableton.com/en/live/'
 
@@ -10,5 +10,4 @@ cask 'ableton-live-suite' do
 
   zap delete: '~/Library/*/*[Aa]bleton*',
       rmdir:  '~/Music/Ableton/Factory Packs'
-  #:trash => '~/Music/Ableton/User Library'
 end
--- Casks/ableton-live.rb
+++ Casks/ableton-live.rb
@@ -2,7 +2,7 @@ cask 'ableton-live' do
   version '9.7.3'
   sha256 'e37b9400515891cb8af23dbc5d3da5b7631b2152b667619704bd54d3403ff190'
 
-  url "http://cdn2-downloads.ableton.com/channels/#{version}/ableton_live_trial_#{version}_64.dmg"
+  url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_trial_#{version}_64.dmg"
   name 'Ableton Live'
   homepage 'https://www.ableton.com/en/live/'
 
@@ -10,5 +10,4 @@ cask 'ableton-live' do
 
   zap delete: '~/Library/*/*[Aa]bleton*',
       rmdir:  '~/Music/Ableton/Factory Packs'
-  # trash: '~/Music/Ableton/User Library'
 end
