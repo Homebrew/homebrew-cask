58845ef03b346f42e831b43fa30ffc27783d54ac
commitay <commitay@users.noreply.github.com>
2017-09-01 12:09:55 +1000

    Update ableton-live casks

--- Casks/ableton-live-lite.rb
+++ Casks/ableton-live-lite.rb
@@ -2,7 +2,7 @@ cask 'ableton-live-lite' do
   version '9.7.3'
   sha256 '53f5511bf85a925dc0c1fd7cc287d18d4879c6405b02169baa82df6fa5fd3caf'
 
-  url "http://cdn2-downloads.ableton.com/channels/#{version}/ableton_live_lite_#{version}_64.dmg"
+  url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_lite_#{version}_64.dmg"
   name 'Ableton Live Lite'
   homepage 'https://www.ableton.com/en/products/live-lite/'
 
--- Casks/ableton-live-standard.rb
+++ Casks/ableton-live-standard.rb
@@ -2,7 +2,7 @@ cask 'ableton-live-standard' do
   version '9.7.3'
   sha256 '8f33eb39a331cdd4456885c7a5a94372d9bef7d664ead431fe2845633b39f9a2'
 
-  url "http://cdn2-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_64.dmg"
+  url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_64.dmg"
   name 'Ableton Live Standard'
   homepage 'https://www.ableton.com/en/live/'
 
@@ -10,5 +10,4 @@ cask 'ableton-live-standard' do
 
   zap delete: '~/Library/*/*[Aa]bleton*',
       rmdir:  '~/Music/Ableton/Factory Packs'
-  #:trash => '~/Music/Ableton/User Library'
 end
