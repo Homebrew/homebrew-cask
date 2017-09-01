710d79c9f60d2548d4d8d49ebb79c575ee95a472
commitay <commitay@users.noreply.github.com>
2017-09-01 15:47:06 +1000

    Update ableton-live-standard to 9.7.4

--- Casks/ableton-live-standard.rb
+++ Casks/ableton-live-standard.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-standard' do
-  version '9.7.3'
-  sha256 '8f33eb39a331cdd4456885c7a5a94372d9bef7d664ead431fe2845633b39f9a2'
+  version '9.7.4'
+  sha256 'de4dba0f837448217b7f50cd1d4eaf7aa4575940b4ddd3782f84cbe0d4116dc0'
 
   url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_64.dmg"
   name 'Ableton Live Standard'
