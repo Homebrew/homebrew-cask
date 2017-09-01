79dc0a23c99c7847abbcbd935b3aab10d973edc1
commitay <commitay@users.noreply.github.com>
2017-09-01 16:12:15 +1000

    Update ableton-live-suite to 9.7.4 (#38185)

--- Casks/ableton-live-suite.rb
+++ Casks/ableton-live-suite.rb
@@ -1,12 +1,12 @@
 cask 'ableton-live-suite' do
-  version '9.7.3'
-  sha256 '97a8083b2b8776421a4f6a67f5074ce2f3b36def6b01f7d16e28ffdd86354dac'
+  version '9.7.4'
+  sha256 '45924ea6bab5d40fc8ec4f2a0e9c21795740776ae524dfb458dc002a2f90ff36'
 
   url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_suite_#{version}_64.dmg"
   name 'Ableton Live Suite'
   homepage 'https://www.ableton.com/en/live/'
 
-  app "Ableton Live #{version[0]} Suite.app"
+  app "Ableton Live #{version.major} Suite.app"
 
   zap delete: '~/Library/*/*[Aa]bleton*',
       rmdir:  '~/Music/Ableton/Factory Packs'
