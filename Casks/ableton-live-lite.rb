ccc69cb7826803c575e583c414c335ed351fbd98
commitay <commitay@users.noreply.github.com>
2017-09-01 16:12:57 +1000

    Update ableton-live-lite to 9.7.4 (#38188)

--- Casks/ableton-live-lite.rb
+++ Casks/ableton-live-lite.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-lite' do
-  version '9.7.3'
-  sha256 '53f5511bf85a925dc0c1fd7cc287d18d4879c6405b02169baa82df6fa5fd3caf'
+  version '9.7.4'
+  sha256 '4e7b227c4af39b3b4c15190c178d55e7a615d9b8e2f4812d3c3b16381d640afc'
 
   url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_lite_#{version}_64.dmg"
   name 'Ableton Live Lite'
