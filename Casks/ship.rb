ac23d7f0ca90fdef66888cfb40da27b9df06b1f5
commitay <commitay@users.noreply.github.com>
2017-09-05 08:46:20 +1000

    Update ship.rb

--- Casks/ship.rb
+++ Casks/ship.rb
@@ -1,9 +1,9 @@
 cask 'ship' do
-  version :latest
-  sha256 :no_check
+  version '2.6.4,702'
+  sha256 '358c2ea4f409f5688ac971086512b45fb55cb63cc11f166c5c72ca67ff407ce6'
 
-  url 'https://www.realartists.com/builds/2.0/Ship.app.zip'
-  appcast 'https://www.realartists.com/builds/2.0/sparkle.xml',
+  url "https://www.realartists.com/builds/#{version.major}.0/Ship.app.zip"
+  appcast "https://www.realartists.com/builds/#{version.major}.0/sparkle.xml",
           checkpoint: '22b8b7320532a014ac88154ecf0e04583d063af9aa4bb936816a3621178c0865'
   name 'Ship'
   homepage 'https://www.realartists.com/'
@@ -12,9 +12,9 @@ cask 'ship' do
 
   app 'Ship.app'
 
-  zap delete: [
-                '~/Library/Application Support/com.realartists.Ship2',
-                '~/Library/Preferences/com.realartists.Ship2.plist',
-                '~/Library/RealArtists/Ship2',
-              ]
+  zap trash: [
+               "~/Library/Application Support/com.realartists.Ship#{version.major}",
+               "~/Library/Preferences/com.realartists.Ship#{version.major}.plist",
+               "~/Library/RealArtists/Ship#{version.major}",
+             ]
 end
