69e4b214297bf58df226cb83124279c0f2727e5a
Stefano Canducci <stenowtf@users.noreply.github.com>
2017-09-04 23:21:51 +0200

    Update telegram to 3.2-104075 (#38246)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,10 +1,10 @@
 cask 'telegram' do
-  version '3.2-104070'
-  sha256 '8f1d1c0a6e6e4b937207d1386a3923b0bdda3fd30e491317e21bb17879d8dbd5'
+  version '3.2-104075'
+  sha256 '28c16afb4500073ddfb6d835c9c5a6c276af9ff50a503e5759e62c0cb805d514'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml',
-          checkpoint: '279f649bbe40ffc5260ba80bd3f7c595fa63d417539a5ec334a7e47dba91ef57'
+          checkpoint: '98c8f9d48f220460227fe66cc5743524f56e664fb9f9567587aca709deb8d20d'
   name 'Telegram for macOS'
   homepage 'https://macos.telegram.org/'
 
