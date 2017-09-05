a34f8e89c08100fa7a0b6d01e2f6e6522525bb12
Mallaiyan, Gobinath [GTSUS Non-J&J] <GMallai1@ITS.JNJ.com>
2017-09-05 07:45:40 -0400

    Update polymail to 1.44

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,10 +1,10 @@
 cask 'polymail' do
-  version '1.43'
-  sha256 '8b0c5beebcbdb164137a428c4b58c57a818efe66f92e581c2d95b146dad7dece'
+  version '1.44'
+  sha256 '220fccabca5e6f3eed9f6304f77f484839c2a41b197c26e0a8b95bc43fd47580'
 
   url "https://sparkle-updater.polymail.io/osx/builds/Polymail-v#{version.major_minor.no_dots}.zip"
   appcast 'https://sparkle-updater.polymail.io/cast.xml',
-          checkpoint: 'cf84dceae53d4240f02ced76d299bd5e8131332a4702b5e3417dc06778b01f47'
+          checkpoint: 'cc0e83cc80221e25ce65736c7ca619b0f0c0c59c3ad17b61a045dec0d564499f'
   name 'Polymail'
   homepage 'https://polymail.io/'
 
