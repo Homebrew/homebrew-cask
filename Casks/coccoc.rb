05d5d317bf77a0af5d41aae9d05d492438043152
Nguyen Tan Tai <taisang1996@gmail.com>
2017-09-07 17:21:05 +0700

    Add CocCoc Latest

---
+++ Casks/coccoc.rb
@@ -0,0 +1,17 @@
+cask 'coccoc' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://files.coccoc.com/browser/mac/coccoc.dmg'
+  name 'Cốc Cốc'
+  homepage 'http://coccoc.com/'
+
+  auto_updates true
+
+  app 'CocCoc.app'
+
+  zap delete: [
+    '~/Library/Application Support/Coccoc',
+    '~/Library/Caches/Coccoc'
+  ]
+end
