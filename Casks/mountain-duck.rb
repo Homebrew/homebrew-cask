a749487f0ae934e709dd06ce35e2f7dc420f37a5
commitay <commitay@users.noreply.github.com>
2017-09-08 00:25:48 +1000

    Update mountain-duck to 2.0.0.8014

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,10 +1,10 @@
 cask 'mountain-duck' do
-  version '1.9.9.7330'
-  sha256 '5e1cd826b75c6b2a16a9efc704e5201250e50c33ff13bac4f4da6076af8e32d1'
+  version '2.0.0.8014'
+  sha256 '65b0662f4bcef00a89c66e79d494018e8d5b149dc363d5610695d56e90646753'
 
   url "https://dist.mountainduck.io/Mountain%20Duck-#{version}.zip"
   appcast 'https://version.mountainduck.io/changelog.rss',
-          checkpoint: '480673a2acf4ceafbb395edffa2fb07c70532291d1bcc20a79df15655c35ddab'
+          checkpoint: 'ddd141012250ba0f4817bd3f1318c08eda359590716a18eb82a001b91941398d'
   name 'Mountain Duck'
   homepage 'https://mountainduck.io/'
 
