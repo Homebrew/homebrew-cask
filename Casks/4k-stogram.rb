a55b7e4eec1257fcdd1f734314ffd3adaae2bb1a
giannitm <giannitm@users.noreply.github.com>
2017-09-07 21:02:52 +0100

    Update 4k-stogram to 2.5.2.1356

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -1,10 +1,10 @@
 cask '4k-stogram' do
-  version '2.5.1.1346'
-  sha256 '68366a9a51879193f260e9e8634ddb09c05316ed6f665f8526e060c3a034c745'
+  version '2.5.2.1356'
+  sha256 '76d818c38d1ab294bc2b520b3a93e81fc4f5271d87d9283d8d5033904991e6a8'
 
   url "https://downloads2.4kdownload.com/app/4kstogram_#{version.major_minor_patch}.dmg"
   appcast 'https://www.4kdownload.com/download',
-          checkpoint: '8de0a5725c670e5fa688b8f7ede33e197766ff2acd12e1de4b8d6bb5a267fdd3'
+          checkpoint: 'c54b263fa24e27e9adf0941a0339b323768dce5b037022ba6f44e2c632ed1051'
   name '4K Stogram'
   homepage 'https://www.4kdownload.com/products/product-stogram'
 
