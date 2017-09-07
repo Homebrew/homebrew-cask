2d3fd702e20e7ddba2ede10111261f353ea48eaf
Jenkins <jenkins@jenkins.c.k8s-minikube.internal>
2017-09-07 18:39:30 +0000

    Update minikube to 0.22.0
    - [x] brew cask audit --download {{cask_file}} is error-free.
    - [x] brew cask style --fix {{cask_file}} reports no offenses.
    - [x] The commit message includes the cask’s name and version.

--- Casks/minikube.rb
+++ Casks/minikube.rb
@@ -1,11 +1,11 @@
 cask 'minikube' do
-  version '0.20.0'
-  sha256 '591737b728745dbf01f634bf714353c416c2e56c39e2e0431910fa51783b7a19'
+  version '0.22.0'
+  sha256 '389c4cbbef69e760e8c5e226035d5afcad05540e2dd1d77439f903be119873f4'
 
   # storage.googleapis.com/minikube was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/minikube/releases/v#{version}/minikube-darwin-amd64"
   appcast 'https://github.com/kubernetes/minikube/releases.atom',
-          checkpoint: 'e2e87909431bf9cb14f674040e819abe54f5184531dfeca9775b3a380a66755f'
+          checkpoint: 'd0e205e95fd8667cc02ecfe643ea07cbdefd67a1a6841c1c0db8d86a1ca51a82'
   name 'Minikube'
   homepage 'https://github.com/kubernetes/minikube'
 
