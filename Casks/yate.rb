824c9c0e478718516ae1cf1cfcae3a4b5f2d4644
commitay <commitay@users.noreply.github.com>
2017-09-08 21:41:21 +1000

    Update yate to 3.17.1.1

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,11 +1,11 @@
 cask 'yate' do
-  version '3.17.1'
-  sha256 'e9de30bb90d1cfbc1d031e070358a5355c9ba0cf8477ab4d7113e1056dd86e5c'
+  version '3.17.1.1'
+  sha256 '73474d2770fa45e9e429b2d815ab851cb6b306515385498440806e2f6f32febe'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip',
       using: :post
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '82be8965830e57d9a871bab2635370349cbf8832b65d64359c9589f5eef53884'
+          checkpoint: 'c06a35fb5f41bc6d409517167b2b9a7bdf3092bc6c3ac7289f64941e00b1dcac'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
 
