6f66d5438d4db8ad04bdd4f8655955e30c00011f
commitay <commitay@users.noreply.github.com>
2017-09-08 12:39:34 +1000

    verified comment / homepage

--- Casks/dynamodb-local.rb
+++ Casks/dynamodb-local.rb
@@ -2,10 +2,10 @@ cask 'dynamodb-local' do
   version :latest
   sha256 :no_check
 
-  # s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
+  # s3-us-west-2.amazonaws.com/dynamodb-local was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz'
   name 'Amazon DynamoDB Local'
-  homepage 'http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html'
+  homepage 'https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html'
 
   # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/dynamodb-local.wrapper.sh"
