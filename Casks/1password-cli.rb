3f4d92ec4fa55eb5f66929a186ad24c50484a1aa
commitay <commitay@users.noreply.github.com>
2017-09-08 18:12:49 +1000

    Update 1password-cli to 0.1.1 (#38387)

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -1,13 +1,15 @@
 cask '1password-cli' do
-  version '0.1'
-  sha256 'fccec086ef70d9fab464c8e5cb4b1748236cb7633c9aae52512fd6502686ad09'
+  version '0.1.1'
+  sha256 '95eb4cfd62a5bf1eab99d4acb24ab9229fc4dfb16a931dd7f9628f88b54c1fc7'
 
   # cache.agilebits.com/dist/1P/op/pkg was verified as official when first introduced to the cask
   url "https://cache.agilebits.com/dist/1P/op/pkg/v#{version}/op_darwin_amd64_v#{version}.zip"
   appcast 'https://app-updates.agilebits.com/product_history/CLI',
-          checkpoint: 'd33d33c7e3ee8eecdc68d529fd81c8a5291bce8ebf3125e674e58870ec039823'
+          checkpoint: 'a0c73938742c33399b0d408dac38e5aaa23a56e8a76ba0603876398f7cc38ccb'
   name '1Password CLI'
   homepage 'https://support.1password.com/command-line/'
 
   binary 'op'
+
+  zap trash: '~/.op'
 end
