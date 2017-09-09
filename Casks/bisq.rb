36c6698bda916a0297f5840617e9da9d48c2bdf2
giannitm <giannitm@users.noreply.github.com>
2017-09-09 19:25:22 +0100

    Update bisq to 0.5.3

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -2,10 +2,10 @@ cask 'bisq' do
   version '0.5.3'
   sha256 'fa3b44ea298f66be697556b967d53e78964abc8b9da52bc8bad5e0500fd4b65b'
 
-  # github.com/bitsquare/bitsquare was verified as official when first introduced to the cask
-  url "https://github.com/bitsquare/bitsquare/releases/download/v#{version}/Bisq-#{version}.dmg"
-  appcast 'https://github.com/bitsquare/bitsquare/releases.atom',
-          checkpoint: 'a4a386edb7bedaffe3f2f282a42042fed68a086538af81dc08ca4689b008221e'
+  # github.com/bisq-network/exchange was verified as official when first introduced to the cask
+  url "https://github.com/bisq-network/exchange/releases/download/v#{version}/Bisq-#{version}.dmg"
+  appcast 'https://github.com/bisq-network/exchange/releases.atom',
+          checkpoint: 'bead53dd88aa63b03eeffb93868ec02b972c9bc8fe1950f4a149d8d2b36fb075'
   name 'Bisq'
   homepage 'https://bisq.io/'
   gpg "#{url}.asc", key_id: '1dc3c8c4316a698ac494039cf5b84436f379a1c6'
