4bf75cdc109ad5da5aa9668ef0f128e194e8b55e
Ryan McKern <ryan@orangefort.com>
2017-09-09 17:20:46 -0700

    Upgrade Colormunki Display to v1.1.4 (#38439)
    * Upgrade Colormunki Display to v1.1.4
    
    * Update colormunki-display.rb

--- Casks/colormunki-display.rb
+++ Casks/colormunki-display.rb
@@ -1,9 +1,9 @@
 cask 'colormunki-display' do
-  version '1.1.3'
-  sha256 '763a6b9e8e704452de79ec07d96853c6a9adbb204fe59376754d79eac5487b92'
+  version '1.1.4'
+  sha256 'a1e1668730bcf8bf2762941375b295bf6d7d848514a9cb11e36c32c6688d6745'
 
-  # downloads.xrite.com was verified as official when first introduced to the cask
-  url "http://downloads.xrite.com/Downloads/Software/ColorMunki/Display/#{version}/Mac/ColorMunki_Display.zip"
+  # my.xrite.com/Downloads/Software/ColorMunki/Display was verified as official when first introduced to the cask
+  url "https://my.xrite.com/Downloads/Software/ColorMunki/Display/#{version}/Mac/ColorMunki_Display.zip"
   name 'ColorMunki Display'
   homepage 'https://xritephoto.com/colormunki-display'
 
