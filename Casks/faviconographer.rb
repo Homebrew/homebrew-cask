4219e1827730274dcd702d4fc571addfe3d724b9
commitay <commitay@users.noreply.github.com>
2017-09-12 09:10:50 +1000

    version / appcast

--- Casks/faviconographer.rb
+++ Casks/faviconographer.rb
@@ -1,8 +1,10 @@
 cask 'faviconographer' do
-  version :latest
-  sha256 :no_check
+  version '1.0'
+  sha256 '42251e3d8b766e2c561dcb511b98ff64f4ee635d8fcea06b34f713f3a59cced7'
 
-  url 'https://faviconographer.com/download/Faviconographer.dmg'
+  url "https://faviconographer.com/download/Faviconographer-#{version}.dmg"
+  appcast 'https://faviconographer.com/updates/faviconographer.xml',
+          checkpoint: 'e7ba2b7b39e59c1d11dd19eca92cfe02b70254e94eec3e001ce6757d57c8d78b'
   name 'Faviconographer'
   homepage 'https://faviconographer.com/'
 
