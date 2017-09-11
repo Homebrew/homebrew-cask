2bf98701ebca2bbd937683f1c625e3a6aac1faea
Jenn Kaplan <jkap@apple.com>
2017-09-11 10:35:49 -0700

    Add Faviconographer Latest

---
+++ Casks/faviconographer.rb
@@ -0,0 +1,10 @@
+cask 'faviconographer' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://faviconographer.com/download/Faviconographer.dmg'
+  name 'Faviconographer'
+  homepage 'https://faviconographer.com/'
+
+  app 'Faviconographer.app'
+end
