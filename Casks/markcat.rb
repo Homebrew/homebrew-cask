aae63f9dc7e19f321572dbd7f9d384e3a6d23a86
kkdd <kkondo_z0@nifty.com>
2017-09-11 23:24:06 +0900

    Update markcat.rb

--- Casks/markcat.rb
+++ Casks/markcat.rb
@@ -1,8 +1,10 @@
 cask 'markcat' do
-  homepage 'https://github.com/ma-tu/markcat'
   version '1.0.2'
   sha256 '85503e6c26a079371f49fffb4ef10256b0595a10c7f109af15f6973fa5266932'
+
   url 'https://github.com/ma-tu/markcat/releases/download/v1.0.2/MarkCat-darwin-x64.zip'
-  app 'MarkCat.app'
   name 'MarkCat'
+  homepage 'https://github.com/ma-tu/markcat'
+
+  app 'MarkCat.app'
 end
