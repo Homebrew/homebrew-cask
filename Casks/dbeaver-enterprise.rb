507d1c8f2f8651369e84607171f7eeba8796dfe1
commitay <commitay@users.noreply.github.com>
2017-09-12 11:05:04 +1000

    Update dbeaver-enterprise to 4.2.0

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,10 +1,10 @@
 cask 'dbeaver-enterprise' do
-  version '4.1.1'
-  sha256 '345fa36b74a08512d868e0232a3f5e043c132d1fa1e90ad57040d4d48c85fa6c'
+  version '4.2.0'
+  sha256 'be79572cdabee2ba108a8a50ff42dea7d9bb6b9061ddaeaf22e2c9ac4b4c886c'
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   appcast 'https://dbeaver.com/files/',
-          checkpoint: 'e96df7f78b09351188ce5e9eb0d0952fe14c0322e4b41b1d1ce6760d9cbab16a'
+          checkpoint: '3c4c8b473915ee3d7a2b78d96c1ba171be65af702377c21ca98aa27374d76cf4'
   name 'DBeaver Enterprise Edition'
   homepage 'https://dbeaver.com/'
 
