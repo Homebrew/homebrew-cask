2c0462b3688f8562ab598c331a1557ef5d38b46e
Elf Sundae <elf.sundae@gmail.com>
2017-09-12 12:17:17 +0800

    Update sourcetree to 2.6.3a (#38510)
    * Update sourcetree to 2.6.3a
    
    * sourcetree

--- Casks/sourcetree.rb
+++ Casks/sourcetree.rb
@@ -1,19 +1,9 @@
 cask 'sourcetree' do
-  if MacOS.version <= :snow_leopard
-    version '1.8.1'
-    sha256 '37a42f2d83940cc7e1fbd573a70c3c74a44134c956ac3305f6b153935dc01b80'
-  elsif MacOS.version <= :mountain_lion
-    version '2.0.5.5'
-    sha256 'f23129587703a706a37d5fdd9b2390875305b482a2b4e4b0e34bd49cba9b63c9'
-  else
-    version '2.6.2c'
-    sha256 '488e7077afd3f493f9a32678eb6c11087ab3917a15f2ee220cecff6a5cee5b0d'
-  end
+  version '2.6.3a'
+  sha256 '4b951e2d113a2ca5f8bb88be06b64c9390eae13b4339f983b4a0867c602dca53'
 
-  # atlassian.com was verified as official when first introduced to the cask
-  url "https://downloads.atlassian.com/software/sourcetree/SourceTree_#{version}.zip"
-  appcast 'https://www.sourcetreeapp.com/update/SparkleAppcast.xml',
-          checkpoint: '6031bd143374559a84872d0b9ecef5420173e11fb3a0453cfaa092878c6bc908'
+  # atlassian.com/software/sourcetree was verified as official when first introduced to the cask
+  url "https://downloads.atlassian.com/software/sourcetree/Sourcetree_#{version}.zip"
   name 'Atlassian SourceTree'
   homepage 'https://www.sourcetreeapp.com/'
 
@@ -26,10 +16,12 @@ cask 'sourcetree' do
             quit:      'com.torusknot.SourceTreeNotMAS'
 
   zap delete: [
-                '~/Library/Application Support/SourceTree',
                 '~/Library/Caches/com.torusknot.SourceTreeNotMAS',
+                '~/Library/Saved Application State/com.torusknot.SourceTreeNotMAS.savedState',
+              ],
+      trash:  [
+                '~/Library/Application Support/SourceTree',
                 '~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist',
                 '~/Library/Preferences/com.torusknot.SourceTreeNotMAS.LSSharedFileList.plist',
-                '~/Library/Saved Application State/com.torusknot.SourceTreeNotMAS.savedState',
               ]
 end
