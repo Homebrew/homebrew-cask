9937d4c8c7095051cf3391eefd1aeb60c4956a35
commitay <commitay@users.noreply.github.com>
2017-09-12 13:29:06 +1000

    myworkspace

--- Casks/myworkspace.rb
+++ Casks/myworkspace.rb
@@ -6,5 +6,15 @@ cask 'myworkspace' do
   name 'My Workspace'
   homepage 'https://myworkspace.ms/'
 
+  auto_updates true
+
   app 'MyWorkspace.app'
+
+  uninstall quit: 'com.microsoft.MyWorkspace'
+
+  zap delete: '~/Library/Caches/com.microsoft.MyWorkspace',
+      trash:  [
+                '~/Library/Application Support/com.microsoft.MyWorkspace',
+                '~/Library/Preferences/com.microsoft.MyWorkspace.plist',
+              ]
 end
