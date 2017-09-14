6e4cf6f4d91601884cdc79a91b29b60c1d27f7f7
Alexander Petin <axp@ngs.ru>
2017-09-14 17:11:41 +0700

    Update xamarin-ios to 10.12.3.3

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-ios' do
-  version '10.12.0.20'
-  sha256 '4ab04a976635be4499adc8f6bba42ff57ec76d524ee250ae7affe4c955df1efa'
+  version '10.12.3.3'
+  sha256 '54918118a857fb20de96b732a84deb9ab6c1f303406e2196419b19549fb08b51'
 
   url "https://dl.xamarin.com/MonoTouch/Mac/xamarin.ios-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
