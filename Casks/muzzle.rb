7d44d94eab6e02f7c4c841d65b7e3237a29c9bcd
commitay <commitay@users.noreply.github.com>
2017-09-14 11:56:30 +1000

    Update muzzle to 1.3 (#38570)

--- Casks/muzzle.rb
+++ Casks/muzzle.rb
@@ -1,10 +1,10 @@
 cask 'muzzle' do
-  version '1.2'
-  sha256 'e6a253075b3eaa8862d8ef9dfac3a4de6021d3a0de587657fe447df0b3193f2e'
+  version '1.3'
+  sha256 'e76e5f5358e8749e7fe2294fee3717ac7327e79eff227430bf3d8f601684a0e6'
 
   url 'https://muzzleapp.com/binaries/muzzle.zip'
   appcast 'https://muzzleapp.com/api/1/appcast.xml',
-          checkpoint: '5d0b59d99cd2bc53ee8ab8993af13d98ac70fd50860abb8a4ff85453bffaa057'
+          checkpoint: '738534763ca3af3ebc7db7b38bde7ec4c00c6474589e94b07164324ed05a5286'
   name 'Muzzle'
   homepage 'https://muzzleapp.com/'
 
