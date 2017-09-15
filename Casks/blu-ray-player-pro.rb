4888142f457bd96db2f03c2654d995542989d658
Victor Popkov <victor@popkov.me>
2017-09-15 22:09:26 +0300

    Update blu-ray-player-pro to 3.2.8

--- Casks/blu-ray-player-pro.rb
+++ Casks/blu-ray-player-pro.rb
@@ -1,10 +1,10 @@
 cask 'blu-ray-player-pro' do
-  version '3.2.7'
-  sha256 'eaef79b70877fd5ea221fa06c3cbcd8b4f8aa1530dceff6c37897dece8e5763f'
+  version '3.2.8'
+  sha256 'c50c83d44f869378ee124ce112a68c921915329473d6e2d8379e74c50b958513'
 
   url 'https://www.macblurayplayer.com/user/download/Macgo_Mac_Bluray_Player_Pro.dmg'
   appcast 'https://macblurayplayer.com/products/mac-bluray-player-pro/Appcast.xml',
-          checkpoint: '6631bf86fd7e361ff7e50539d531637a3145fa2a959a89efc625d523b2ccfff2'
+          checkpoint: 'ff5641c5f72850851eea3b7ee45730d5db2785b652c203aaaa5eb00b180eda1f'
   name 'Macgo Mac Blu-ray Player Pro'
   homepage 'https://www.macblurayplayer.com/'
 
