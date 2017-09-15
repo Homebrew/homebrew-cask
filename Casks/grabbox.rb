d26755cfb7fd0e1cbf70c74776e5578b4735708b
Victor Popkov <victor@popkov.me>
2017-09-15 22:20:45 +0300

    Update grabbox to 2.0.4

--- Casks/grabbox.rb
+++ Casks/grabbox.rb
@@ -1,10 +1,10 @@
 cask 'grabbox' do
-  version '2.0.2'
-  sha256 '4413c7e9035b4b0e18e979d3cb74dade8c9672bdca409cf3771af80e592ec3e3'
+  version '2.0.4'
+  sha256 '0609b8c1a4f03e3f458ac9e2053ff3c06d4b8de8867f7b5e786d2e4bb0bac3a2'
 
   url "https://grabbox.bitspatter.com/updates/GrabBox-#{version}.zip"
   appcast 'https://grabbox.bitspatter.com/updates/appcast.xml',
-          checkpoint: 'fd68ecdde99fa9c2ef46c5954487c9429d6035db816afac20902a53ad072bc88'
+          checkpoint: 'fab4b41695425327aab483578be941494d5d741fe7793e3c70f08676a145b8e6'
   name 'GrabBox'
   homepage 'https://grabbox.bitspatter.com/'
 
