1849499fa989fd0a3230f13ac2af65d369c48e6c
Fabian Maass <fabian.maass@adnovum.ch>
2017-09-15 17:36:11 +0200

    Update exodus to 1.34.1

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,11 +1,11 @@
 cask 'exodus' do
-  version '1.33.2'
-  sha256 '64f289fd74ca04f06063dbbad3310ac6bb57dda55e51749d018e6fabc8f82eca'
+  version '1.34.1'
+  sha256 '57723e1ad927b72170bf834b5221e4cf40c4ef213041a5dcbe793ff6b7d65a43'
 
   # exodusbin.azureedge.net was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-macos-#{version}.dmg"
   appcast 'https://www.exodus.io/releases/',
-          checkpoint: '567a0ecd874058dc4a8d2233340866131d07bbda35ece1885717228989ad9abd'
+          checkpoint: '121753bfa066ae1758ee2bf073df1d754ee4e09489e1ef501174c2603c61e70d'
   name 'Exodus'
   homepage 'https://www.exodus.io/'
 
