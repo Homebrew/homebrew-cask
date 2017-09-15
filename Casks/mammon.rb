017ca12e0e850c5ab8ece8804f7694ad3d79269f
Joao Bernardo Costa <jbernardoddc@gmail.com>
2017-09-15 14:53:20 +0100

    Update mammon to latest

--- Casks/mammon.rb
+++ Casks/mammon.rb
@@ -2,8 +2,8 @@ cask 'mammon' do
   version :latest
   sha256 :no_check
 
-  # dl.dropboxusercontent.com/u/5790728 was verified as official when first introduced to the cask
-  url 'https://dl.dropboxusercontent.com/u/5790728/Mammon-latest.zip'
+  # dropbox.com/s/kc8a9rcq2kpt94x was verified as official when first introduced to the cask
+  url 'https://www.dropbox.com/s/kc8a9rcq2kpt94x/Mammon-latest.zip'
   name 'Mammon'
   homepage 'https://teamfox.co/mammon/'
 
