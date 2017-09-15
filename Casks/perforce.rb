d45ecd2e2993c2c751d8765dc2a07de864fe14eb
Russell Finn <rsf@sprucehill.com>
2017-09-14 20:42:06 -0400

    Update perforce to 17.1 (#38595)

--- Casks/perforce.rb
+++ Casks/perforce.rb
@@ -1,5 +1,5 @@
 cask 'perforce' do
-  version '16.2'
+  version '17.1'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://cdist2.perforce.com/perforce/r#{version}/bin.darwin90x86/helix-versioning-engine.tgz"
