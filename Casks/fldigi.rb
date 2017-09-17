7aa8d60fedc8e907c208855d4cb1bb59a0df17a8
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-09-17 21:25:33 +0100

    Update fldigi to 4.0.9 (#38680)
    * Update fldigi to 4.0.9
    
    * checkpoint

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -1,10 +1,10 @@
 cask 'fldigi' do
-  version '4.0.8'
-  sha256 '9d45400b1b3942b5a79d01042bd284c99cf9575157542be152160717f17c6f5e'
+  version '4.0.9'
+  sha256 '1e092632d0f29de3911121c12685c60e9bd602a9fa06139266679c1439c4a832'
 
   url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}_i386.dmg"
   appcast 'https://sourceforge.net/projects/fldigi/rss?path=/fldigi',
-          checkpoint: 'f27999aae707e424c0d55062a925f2af154563bb4479dbfd87cead956b975d1c'
+          checkpoint: '2baa7fe03d9a6f22e8388d3533713a59ff4f6170881afdec34732ed0cad3c139'
   name 'fldigi'
   homepage 'https://sourceforge.net/projects/fldigi/files/fldigi/'
 
