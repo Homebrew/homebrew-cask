97b4a051414162b74e2e06eea48bb75cb4b31c77
commitay <commitay@users.noreply.github.com>
2017-09-18 06:32:40 +1000

    Update serial to 1.3.5

--- Casks/serial.rb
+++ Casks/serial.rb
@@ -1,10 +1,10 @@
 cask 'serial' do
-  version '1.3.4'
-  sha256 '0567d01acb62db7a1607cf27c47f632c0adbb7942bc0c453287bf1aabd4d4941'
+  version '1.3.5'
+  sha256 '5c6b7b31d8265a4d592a59af7b2cc96e1f89826a126b1057dc7ab4ae2a8424c1'
 
   url "https://download.decisivetactics.com/products/serial/dl/Serial_#{version}.zip"
   appcast 'https://www.decisivetactics.com/products/serial/release-notes',
-          checkpoint: '76b57f7849454980d4f22452981fe2b514891b7e4e5dbea60af547c020fefb6f'
+          checkpoint: '158ac5ee1d1d4311c623b744e5f9c65116826e49e3259bacd03cee4ee9538ddd'
   name 'Serial'
   homepage 'https://www.decisivetactics.com/products/serial/'
 
