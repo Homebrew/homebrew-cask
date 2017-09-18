a4194384343d62b3c62c1ab5f76f5885f3d606dc
commitay <commitay@users.noreply.github.com>
2017-09-19 06:52:34 +1000

    Update texshop to 3.88

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,10 +1,10 @@
 cask 'texshop' do
-  version '3.87'
-  sha256 'fe1164aa9c8a4898b390ea00568b74a42b29ab7e91a2c872198f40d66487619b'
+  version '3.88'
+  sha256 '96ee307a8062f4ca69b5b7fb517b0456a25957dbd02f19af6041284c5457e9b4'
 
   url "http://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.no_dots}.zip"
   appcast 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml',
-          checkpoint: '792050c8090854eb117d2abded51517f626e78512d9e9cafe2a3d27aa44f5944'
+          checkpoint: '37a73f8d3a4847a4ce21e3a14a9341811b317bea01fd4755bfe2152362a85d0f'
   name 'TeXShop'
   homepage 'http://pages.uoregon.edu/koch/texshop/'
 
