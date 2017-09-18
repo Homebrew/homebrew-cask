92838acfe046ed80285f207ffb2617763853418f
Jiehan Zheng <zheng@jiehan.org>
2017-09-18 15:45:13 -0400

    Update gitup to 1.0.8

--- Casks/gitup.rb
+++ Casks/gitup.rb
@@ -1,11 +1,11 @@
 cask 'gitup' do
-  version '1.0.7'
-  sha256 'b5bfe8c1802b1c7f72359214e58b2c768c5b7a573d6579a3101f8932b81d308f'
+  version '1.0.8'
+  sha256 'dfc7572c36807cbc70ef3c5d28429be1988ffc1c872e13ba092d643ddc964393'
 
   # s3-us-west-2.amazonaws.com/gitup-builds was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/gitup-builds/stable/GitUp.zip'
   appcast 'https://github.com/git-up/GitUp/releases.atom',
-          checkpoint: '6a0a080b65c4c9c4f8a2605759c1955eba2550a6b1314f6b53fa47f59e83cd58'
+          checkpoint: '37bf09620ee73ca5dbae0a6a54b1afa595d4e432e2054b51274e93999ccfccbe'
   name 'GitUp'
   homepage 'http://gitup.co/'
 
