4f525597346afba201b4ca79c5c42adc4720e5d5
commitay <commitay@users.noreply.github.com>
2017-09-18 21:09:24 +1000

    Update coconutbattery to 3.6.4 (#38704)

--- Casks/coconutbattery.rb
+++ Casks/coconutbattery.rb
@@ -4,8 +4,8 @@ cask 'coconutbattery' do
     sha256 '0edf6bdaf28fb3cc9c242fd916c348fbbae30a5356ddc1d6e5158d50f96d740d'
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version.dots_to_underscores}.zip"
   else
-    version '3.6.3'
-    sha256 '938eb166ba175a9e9b9a06ba12ce7bf689977ed4422c1f5a5378a2f820dc4152'
+    version '3.6.4'
+    sha256 '8e289fb4a75cb117fc1d7861020c9ab2384b09dfd18f066c7fadfc9d42c3ac56'
     url "https://www.coconut-flavour.com/downloads/coconutBattery_#{version}.zip"
     appcast 'https://coconut-flavour.com/updates/coconutBattery.xml',
             checkpoint: 'd85cf224a2714f724efc7e979f70aedf987237443e84c8cd616580fe1bf4543a'
