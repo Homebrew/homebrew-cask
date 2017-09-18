b202e792b5eb9ac5487dc8034223f78a07d38956
commitay <commitay@users.noreply.github.com>
2017-09-19 00:59:45 +1000

    Update jpki.rb

--- Casks/jpki.rb
+++ Casks/jpki.rb
@@ -1,17 +1,23 @@
 cask 'jpki' do
-  version '3.0'
+  version '03-00'
   sha256 '41a04566520b3a63b95b39fcce4faee212d84b1509178feda39578e753a17179'
 
-  url 'https://www.jpki.go.jp/client/download/101/JPKI03-00.dmg'
+  url "https://www.jpki.go.jp/client/download/101/JPKI#{version}.dmg"
   name 'JPKI'
   homepage 'https://www.jpki.go.jp/'
 
-  depends_on cask:  'java',
-             macos: '>= 10.10'
+  depends_on macos: '>= :yosemite'
 
   pkg 'JPKIInstall.pkg'
 
-  uninstall pkgutil: [
-                       'jp.go.jpki',
-                     ]
+  uninstall pkgutil: 'jp.go.jpki',
+            script:  {
+                       executable: 'JPKIUninstall.command',
+                       input:      ['Y'],
+                       sudo:       true,
+                     }
+
+  caveats do
+    depends_on_java('8+')
+  end
 end
