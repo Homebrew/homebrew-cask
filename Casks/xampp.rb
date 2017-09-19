4d5b1a46e61eeccfbf2a54fa0aa799717b6aae4a
commitay <commitay@users.noreply.github.com>
2017-09-19 22:39:52 +1000

    Update xampp to 7.1.9-0 (#38748)

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -1,6 +1,6 @@
 cask 'xampp' do
-  version '7.1.8-0'
-  sha256 '709f6d66ce93a20691d9382db52da574861af1bdece8674fd56f4f85c9463afb'
+  version '7.1.9-0'
+  sha256 'dcd716c7566ea4298517a411cec2e2405d8fa81c80238a566951deddd91103e7'
 
   # downloadsapachefriends.global.ssl.fastly.net/xampp-files was verified as official when first introduced to the cask
   url "https://downloadsapachefriends.global.ssl.fastly.net/xampp-files/#{version.major_minor_patch}/xampp-osx-#{version}-installer.dmg"
