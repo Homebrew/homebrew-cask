9274287bece1632bcceddb0a89c54a357ab77e2f
commitay <commitay@users.noreply.github.com>
2017-09-19 19:49:15 +1000

    Update tyke.rb

--- Casks/tyke.rb
+++ Casks/tyke.rb
@@ -1,10 +1,12 @@
 cask 'tyke' do
-  version '1.0'
-  sha256 '8279fb5bf6b34308dac2fc5cff046a4a3dbc42a085ed09b462eaa27bf3ab0b23'
+  version :latest
+  sha256 :no_check
 
   url 'http://tyke.io/tyke.dmg'
   name 'Tyke'
   homepage 'http://tyke.io/'
 
   app 'tyke.app'
+
+  uninstall quit: 'org.torrez.tyke'
 end
