8761fb54cf5bc295174727b9239dc7c6d3a012bc
optikfluffel <optik@fluffel.io>
2017-09-19 11:35:36 +0200

    Add tyke.app v1.0

---
+++ Casks/tyke.rb
@@ -0,0 +1,10 @@
+cask 'tyke' do
+  version '1.0'
+  sha256 '8279fb5bf6b34308dac2fc5cff046a4a3dbc42a085ed09b462eaa27bf3ab0b23'
+
+  url 'http://tyke.io/tyke.dmg'
+  name 'Tyke'
+  homepage 'http://tyke.io/'
+
+  app 'tyke.app'
+end
