38aed7d939718a6c13cf4e8b0735e4d0695d7c70
commitay <commitay@users.noreply.github.com>
2017-09-19 21:27:57 +1000

    Update ubar to 4.0.1

--- Casks/ubar.rb
+++ Casks/ubar.rb
@@ -1,10 +1,10 @@
 cask 'ubar' do
-  version '3.2.6'
-  sha256 '76f87ebb3a74b40d6299354cd4f34c63480cd5951c2276a3a4d95c65882dd1a0'
+  version '4.0.1'
+  sha256 '2ebc0cf7d74553f44308eb93f72f2a897e8d403a961cc16023cc843471b3e1f0'
 
   url "http://www.brawersoftware.com/downloads/ubar/ubar#{version.no_dots}.zip"
   appcast "https://brawersoftware.com/appcasts/feeds/ubar/ubar#{version.major}.xml",
-          checkpoint: 'bd56dabff2625be4c5843c8cd67eeac340dc7f38f0e05487336bb3907202b7c0'
+          checkpoint: '25f9839af108389ace53991d20df808e5f58de71bd3906bdab310244729b067a'
   name 'uBar'
   homepage 'https://brawersoftware.com/products/ubar'
 
