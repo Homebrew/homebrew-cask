17445852ba9e3a3442b6acdd9e0664fd80bec3a3
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2017-09-18 20:00:37 -0600

    Update backuploupe to 2.11 (#38727)

--- Casks/backuploupe.rb
+++ Casks/backuploupe.rb
@@ -1,10 +1,10 @@
 cask 'backuploupe' do
-  version '2.10'
-  sha256 '17c72fb27c93fe36a4ba5d46dcea5e4bf78c326541867fb926eed0f7feacc17e'
+  version '2.11'
+  sha256 '619a1c23cc3796395eee71c08e9b4a8a2bd715118bf71c10f5a766be3cfe7530'
 
   url "http://www.soma-zone.com/download/files/BackupLoupe_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/BackupLoupe/a/appcast.xml',
-          checkpoint: '0f0123717142216f86264df1275a2f67787158b0caf59da37f034b234f54a5be'
+          checkpoint: 'f3e77b07c93b1c88d3f13ae85c2ddb71056d1e0909ee05acbc085394fac36583'
   name 'BackupLoupe'
   homepage 'http://www.soma-zone.com/BackupLoupe/'
 
