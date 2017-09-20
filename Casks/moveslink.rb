738727e9dbd93c815712956439433deb5dbacf39
Vítor Galvão <info@vitorgalvao.com>
2017-09-20 18:12:23 +0100

    Update moveslink.rb

--- Casks/moveslink.rb
+++ Casks/moveslink.rb
@@ -4,8 +4,12 @@ cask 'moveslink' do
 
   # d1c229iib3zm7m.cloudfront.net was verified as official when first introduced to the cask
   url "https://d1c229iib3zm7m.cloudfront.net/mac/Moveslink#{version.dots_to_underscores}.dmg"
+  appcast 'https://d1c229iib3zm7m.cloudfront.net/mac/appcast.xml',
+          checkpoint: 'c1d5f1b9a7c848fde84a61e12ed6232eb517126ad92be680ba4e37c76f902e14'
   name 'Moveslink'
   homepage 'https://www.movescount.com/connect'
+
+  auto_updates true
 
   app "Moveslink#{version.major}.app"
 end
