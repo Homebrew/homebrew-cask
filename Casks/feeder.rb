921591f3a551058da995f1a35a0c96d587bf98fc
commitay <commitay@users.noreply.github.com>
2017-09-20 10:18:25 +1000

    Update feeder to 3.5 (#38753)

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -1,10 +1,10 @@
 cask 'feeder' do
-  version '3.4.6'
-  sha256 '7992036e6ab5a5df7ee077aeba926d745a02a89a83b37ff5d1d62fd669c48978'
+  version '3.5'
+  sha256 'c19e9b5d4819931c91b2581ef990419c85041edab849f04239e022bc1850f1d6'
 
   url "https://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
   appcast "https://reinventedsoftware.com/feeder/downloads/Feeder#{version.major}.xml",
-          checkpoint: 'eb54f81d111e9b6afa48b9d41c56edae9341e3b0a39464ed5510458e8ed9ab87'
+          checkpoint: '6d49c00af99ecf3b8cae9e88e31de55bddecdcc15b854aa2065c85d4991a496e'
   name 'Feeder'
   homepage 'https://reinventedsoftware.com/feeder/'
 
