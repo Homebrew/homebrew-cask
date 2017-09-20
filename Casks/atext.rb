b6a2ebecb8d47c61240ab03f2a0729e4db40eeb1
Serge Bibauw <sbibauw@gmail.com>
2017-09-20 02:30:53 +0200

    Update atext to 2.21 (#38754)

--- Casks/atext.rb
+++ Casks/atext.rb
@@ -1,10 +1,10 @@
 cask 'atext' do
   version '2.21'
-  sha256 '67539d037aee39e56329070cf1d151bdfa79a8d7c0b41897d8af0c468af7d08b'
+  sha256 '8ef2fc8f136f15ab4c1218a74097a9a83407ede3f79e6e616eade920bfff02ba'
 
   url 'https://www.trankynam.com/atext/downloads/aText.dmg'
   appcast 'https://www.trankynam.com/atext/aText-Appcast.xml',
-          checkpoint: '606782e61ba768bd9d3daa26d5d3451d37ea9dabdb6c9b658b95e7a0d186ca03'
+          checkpoint: '9d96d77e7584acfe70ee83f0d00342fbe0b9d0b39e23685f9c92001bbd12f983'
   name 'aText'
   homepage 'https://www.trankynam.com/atext/'
 
