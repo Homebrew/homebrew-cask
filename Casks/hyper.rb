ce30b7df0625afb50d14935e7d1ce9bec8adc584
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-09-21 07:44:27 +0900

    Update hyper to 2.0.1

--- Casks/hyper.rb
+++ Casks/hyper.rb
@@ -1,11 +1,11 @@
 cask 'hyper' do
-  version '1.4.6'
-  sha256 '183fe4f2823d4c3a817aac72f70cb4af621a68263d55c71da643256f006b0893'
+  version '2.0.1'
+  sha256 '25b59d0fa72eec26a1b33ee3af7f669c03443dca8dd49517930e96a27c8c2779'
 
   # github.com/zeit/hyper was verified as official when first introduced to the cask
   url "https://github.com/zeit/hyper/releases/download/#{version}/hyper-#{version}-mac.zip"
   appcast 'https://github.com/zeit/hyper/releases.atom',
-          checkpoint: '49467ab3ad2c9171c9afcac7b8d11755f78d46f13cc51d83cada12bfa3cc8602'
+          checkpoint: 'c9cf340c2f78fd5c2767e94ef2a3f2883040ad2393a96e1daafc24d26cfdb006'
   name 'Hyper'
   homepage 'https://hyper.is/'
 
