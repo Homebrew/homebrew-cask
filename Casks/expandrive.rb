33c2dd3afc2725e771499c0d654615a84014d242
commitay <commitay@users.noreply.github.com>
2017-09-20 16:48:07 +1000

    Remove outdated `appcast`

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -3,8 +3,6 @@ cask 'expandrive' do
   sha256 '44b2e869898574f2d7ee19b4f9e5ebddfd81ef5d32e7e742270536d35676d953'
 
   url "https://updates.expandrive.com/apps/expandrive/v/#{version.dots_to_hyphens}/download.dmg"
-  appcast 'https://updates.expandrive.com/appcast/expandrive.xml',
-          checkpoint: '5ef57ba7165a2afc67ab96fd08e68aed3a2a90a273fa717b887c0c34040b2ca7'
   name 'ExpanDrive'
   homepage 'https://www.expandrive.com/apps/expandrive/'
 
