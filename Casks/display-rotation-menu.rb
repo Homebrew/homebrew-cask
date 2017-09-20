84507952a83ba1026692aed94f9e6eb3b960658b
Chris @ Drawcard <signup@drawcard.com.au>
2017-09-20 12:52:52 +0930

    Create display-rotation-menu.rb

---
+++ Casks/display-rotation-menu.rb
@@ -0,0 +1,10 @@
+cask 'display-rotation-menu' do
+  version '1.2'
+  sha256 '818ddf7293873db7109d855396d4a7a79ac79813174cd3d546e536b5a5f0ab6a'
+
+  url 'http://www.magesw.com/displayrotation/DisplayRotationMenu_1.2.zip'
+  name 'Display Rotation Menu'
+  homepage 'http://www.magesw.com/displayrotation/'
+
+  app 'Display Rotation Menu.app'
+end
