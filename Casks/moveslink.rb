af4295ad58ab9f5ea568208061cd560eef34a4e4
Vítor Galvão <info@vitorgalvao.com>
2017-09-20 18:10:56 +0100

    Update and rename moveslink2.rb to moveslink.rb

--- Casks/moveslink.rb
+++ Casks/moveslink.rb
@@ -1,11 +1,11 @@
-cask 'moveslink2' do
+cask 'moveslink' do
   version '2.1.4.4'
   sha256 '474ec979b4fb9b6c2319a61162e95ca895eb9949b3f2a72a7365561bcf651a8c'
 
   # d1c229iib3zm7m.cloudfront.net was verified as official when first introduced to the cask
-  url "https://d1c229iib3zm7m.cloudfront.net/mac/Moveslink#{version.gsub('.', '_')}.dmg"
-  name 'Moveslink2'
+  url "https://d1c229iib3zm7m.cloudfront.net/mac/Moveslink#{version.dots_to_underscores}.dmg"
+  name 'Moveslink'
   homepage 'https://www.movescount.com/connect'
 
-  app 'Moveslink2.app'
+  app "Moveslink#{version.major}.app"
 end
