8ef8719237b8d5189129676774434c0e17113805
Pat White <byelipk@gmail.com>
2017-09-19 20:23:04 -0400

    Update framer to 9840 (#38769)

--- Casks/framer.rb
+++ Casks/framer.rb
@@ -1,11 +1,11 @@
 cask 'framer' do
-  version '9693'
-  sha256 '091de6bf11174b9c65e023f4b7327384f1f4f16372ebf363d332d0c4f1711901'
+  version '9840'
+  sha256 '3b781c56d0639fe2f398170780be900ec131da79196e81dd30bb495a02fc8b1d'
 
   # devmate.com/com.motif.framer was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.motif.framer/FramerStudio.zip'
   appcast 'https://updates.devmate.com/com.motif.framer.xml',
-          checkpoint: '08bb934f1d3243c9f2ffb54d3eb39e50d4a2e8451e1c18b5bacfb4401a47a964'
+          checkpoint: '4e882f5ba475c44bf5957e71b910c00fdf1462baf44edaed5cfc074fd805c9b3'
   name 'Framer'
   homepage 'https://framer.com/'
 
