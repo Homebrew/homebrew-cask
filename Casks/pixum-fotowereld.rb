9d7e53c2a0fc2472a77ab7ab6ed8f9ca6524141a
commitay <commitay@users.noreply.github.com>
2017-09-21 17:52:37 +1000

    url

--- Casks/pixum-fotowereld.rb
+++ Casks/pixum-fotowereld.rb
@@ -2,8 +2,8 @@ cask 'pixum-fotowereld' do
   version :latest
   sha256 :no_check
 
-  # https://nl.pixum.be/api/ was verified as official when first introduced to the cask
-  url 'https://nl.pixum.be/api/getClient?url=108'
+  # dls.photoprintit.com/api/getClient/12455-nl_BE/hps was verified as official when first introduced to the cask
+  url 'https://dls.photoprintit.com/api/getClient/12455-nl_BE/hps/a303030303030303030303030303030303030303030303030353535323830364/mac'
   name 'Pixum Fotowereld'
   homepage 'https://nl.pixum.be/fotoboek'
 
