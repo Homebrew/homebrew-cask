82d202ff00292671ce44f779d3524c8d6a93f39e
commitay <commitay@users.noreply.github.com>
2017-09-22 08:25:30 +1000

    milanote

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -1,6 +1,6 @@
 cask 'milanote' do
-  version '1.0.9'
-  sha256 'c09eec27601e8be53f8132b23a344b2fe5041bf6645a5ee6a83423a8efb1d6a1'
+  version '1.0.10'
+  sha256 '847eb803b255a9f27c21b475ac95abcec88182147b9da24136df73cc36342da4'
 
   # milanote-app-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg"
@@ -10,10 +10,12 @@ cask 'milanote' do
   app 'Milanote.app'
 
   zap delete: [
-                '~/Library/Application Support/Milanote',
                 '~/Library/Caches/com.milanote.app',
                 '~/Library/Caches/com.milanote.app.ShipIt',
                 '~/Library/Library/Logs/Milanote',
+              ],
+      trash:  [
+                '~/Library/Application Support/Milanote',
                 '~/Library/Preferences/com.milanote.app.helper.plist',
                 '~/Library/Preferences/com.milanote.app.plist',
               ]
