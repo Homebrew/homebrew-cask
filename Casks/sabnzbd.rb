66bcad57e97165991bcc7c1c807bb192c21618ec
Sujeet Akula <sujeet@freeboson.org>
2017-09-22 21:13:12 +1000

    update sabnzbd to 2.3.0 (#38840)

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -1,11 +1,11 @@
 cask 'sabnzbd' do
-  version '2.2.1'
-  sha256 'da52226a42f228f28f09b119894296eb9d7459ce8f39339f76e3d1b6f6cd7327'
+  version '2.3.0'
+  sha256 'cafe5a7d106b83f273c50cd5450e8d5e25e6d8655b65f48687cc783756a42e82'
 
   # github.com/sabnzbd/sabnzbd was verified as official when first introduced to the cask
   url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-osx.dmg"
   appcast 'https://github.com/sabnzbd/sabnzbd/releases.atom',
-          checkpoint: '5c20b8e404f133f56fb1c83116e7951828b3ce080e5198c12856677e60bb1533'
+          checkpoint: '2c4e96201a9aa5b45b3d906fec78fe946afad179b094a37dc9d3c52e05243b71'
   name 'SABnzbd'
   homepage 'https://sabnzbd.org/'
 
