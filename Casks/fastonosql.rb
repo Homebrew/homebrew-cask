e1061ed5c9b767e17c3edd0eb35aa47d1e96d516
Erick Guan <fantasticfears@gmail.com>
2017-09-22 12:36:37 +0200

    Add FastoNoSQL.app v1.8.1.0

---
+++ Casks/fastonosql.rb
@@ -0,0 +1,11 @@
+cask 'fastonosql' do
+  version '1.8.1.0'
+  sha256 'd24ddc9109c4d80d61570f7c176ad306895a04b1804052232ce0fef1d868efa8'
+
+  # First fetched and verified when the cask is created
+  url 'https://www.fastonosql.com/downloads/macosx/fastonosql-1.8.1-x86_64.dmg'
+  name 'fastonosql'
+  homepage 'https://www.fastonosql.com/'
+
+  app 'FastoNoSQL.app'
+end
