92cdfb88c4dd2654d8a48d408745ba8a6bdbc924
Dayne Broderson <broderson@gmail.com>
2017-09-22 10:30:59 -0800

    add casks for awips-python and awips2-cave

---
+++ Casks/awips2-cave.rb
@@ -0,0 +1,13 @@
+cask 'awips2-cave' do
+  version '17.1.1'
+  sha256 '20c8b6a0d3f71bcdb8735af75898ccfa58dc5eeacfe424b5a3b7a804120f5cf6'
+
+  url 'http://www.unidata.ucar.edu/downloads/awips2/awips2-cave-17.1.1.dmg'
+  name 'awips2-cave'
+  homepage 'http://www.unidata.ucar.edu/software/awips2/'
+  
+  depends_on cask: 'awips-python'
+
+  app 'Cave.app'
+
+end
