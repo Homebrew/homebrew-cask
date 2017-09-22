38df736798b296923b6a2532ee21d700587c5735
Ben <BenjaminHCCarr@users.noreply.github.com>
2017-09-22 11:56:36 -0400

    Changes: no version, new homepage, add Java caveat
    make changes requested by @commitay for merging

--- Casks/juicebox.rb
+++ Casks/juicebox.rb
@@ -1,11 +1,15 @@
 cask 'juicebox' do
-  version '1.5.2'
-  sha256 '94e577f362033829c1e9dc4016ad9681b949d75141a13689869f77c3dee4808c'
+  version :latest
+  sha256 :no_check
 
   # s3.amazonaws.com/hicfiles.tc4ga.com/public/juicebox was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/hicfiles.tc4ga.com/public/juicebox/Juicebox.dmg'
   name 'Juicebox'
-  homepage 'https://github.com/theaidenlab/juicebox/wiki'
+  homepage 'http://aidenlab.org/juicebox/'
 
   app 'Juicebox.app'
+  
+  caveats do
+    depends_on_java('8')
+  end
 end
