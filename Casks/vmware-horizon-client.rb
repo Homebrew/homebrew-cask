276e1e85a227d1bd38e25698b2b3a301e1a698e5
commitay <commitay@users.noreply.github.com>
2017-09-22 16:09:44 +1000

    Update vmware-horizon-client.rb

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -1,8 +1,8 @@
 cask 'vmware-horizon-client' do
-  version '4.6.0-6607320'
+  version '4.6.0-6607320,CART18FQ3'
   sha256 'f195d2a7357b1c791debda763c9cd8e11db134a2ab5da2d325c757a142141601'
 
-  url "https://download3.vmware.com/software/view/viewclients/CART18FQ3/VMware-Horizon-Client-#{version}.dmg"
+  url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
   name 'VMware Horizon Client'
   homepage 'https://www.vmware.com/'
 
