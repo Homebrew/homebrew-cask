fc996a1de9b2de916260301be69d229e1f6947a2
Akash Levy <akashlevy@gmail.com>
2017-09-23 14:03:44 -0400

    Updated version to 3.3.85 and checksum for gtkwave cask

--- Casks/gtkwave.rb
+++ Casks/gtkwave.rb
@@ -1,6 +1,6 @@
 cask 'gtkwave' do
-  version '3.3.84'
-  sha256 'c503fb140f94a433dcfdc6417110890c77938b97f4d32cd4640155585d738b7c'
+  version '3.3.85'
+  sha256 'a553e6d463b466a00a63842bf4a06bf0c0546e73539f3c023c1f1961729cd7b3'
 
   url "https://downloads.sourceforge.net/gtkwave/gtkwave-#{version}-osx-app/gtkwave.zip"
   appcast 'https://sourceforge.net/projects/gtkwave/rss',
