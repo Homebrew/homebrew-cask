59c34cfdf88f5105cde87721ca9832ae132c4eb2
Shaun Jackman <sjackman@gmail.com>
2017-09-24 22:55:43 -0700

    Update typora to 0.9.9.10.8

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,10 +1,10 @@
 cask 'typora' do
-  version '0.9.9.10.6'
-  sha256 '8991e21820be31016544a6875aeb7e8ebc17d15f63422c9c5e984751c6ad5ab7'
+  version :latest
+  sha256 :no_check
 
   url 'https://typora.io/download/Typora.dmg'
   appcast 'https://www.typora.io/download/dev_update.xml',
-          checkpoint: '20cf84715096508a4d806147cef2e02ffccef8be1665b28ebea9f54073552156'
+          checkpoint: '5824207ab434e6a2db6cf5e8493803645c68dd8c123b3cd72c0bf8fde589c817'
   name 'Typora'
   homepage 'https://typora.io/'
 
