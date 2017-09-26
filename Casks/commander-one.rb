423c220d37a88d4378aa580e8c748531c12aff05
commitay <commitay@users.noreply.github.com>
2017-09-26 21:11:13 +1000

    https

--- Casks/commander-one.rb
+++ Casks/commander-one.rb
@@ -2,8 +2,8 @@ cask 'commander-one' do
   version '1.7.4'
   sha256 '817b8268fb115b15eda107d304f05dc42185e7dc6b3a803cfdbf99a57b9626e1'
 
-  url 'http://mac.eltima.com/download/commander.dmg'
-  appcast 'http://www.eltima.com/download/commander-update/settings.xml',
+  url 'https://mac.eltima.com/download/commander.dmg'
+  appcast 'https://www.eltima.com/download/commander-update/settings.xml',
           checkpoint: 'a8ff91be0ebacdd3fc00fbdec002a1f9cc8590416f8a103ce04c5562534e21df'
   name 'Commander One'
   homepage 'https://mac.eltima.com/file-manager.html'
