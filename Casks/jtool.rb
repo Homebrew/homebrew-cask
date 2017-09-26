8a4445479c4f78378b422896880fbb1d374d7fa8
John Kooker <jkooker@gmail.com>
2017-09-25 17:48:16 -0700

    Update jtool URL (#38956)
    The author of jtool moved the tarball on his website.

--- Casks/jtool.rb
+++ Casks/jtool.rb
@@ -2,7 +2,7 @@ cask 'jtool' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.newosxbook.com/files/jtool.tar'
+  url 'http://www.newosxbook.com/tools/jtool.tar'
   name 'jtool'
   homepage 'http://newosxbook.com/tools/jtool.html'
 
