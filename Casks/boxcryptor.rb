2df0df0c559173630d6e73334419e5f8d928103f
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2017-09-26 02:59:51 +0200

    Update Boxcryptor to 2.15 (#38960)

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,6 +1,6 @@
 cask 'boxcryptor' do
-  version '2.14.867'
-  sha256 'e10760dc55aeb9ca987fe6bd4f716de71d987195b3907a6ef3f76d01945a17b5'
+  version '2.15.875'
+  sha256 '51fd06157dfb630257cbfdff167fccfe310f4d9f8864f1bb4d869c3c4421efdc'
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8',
