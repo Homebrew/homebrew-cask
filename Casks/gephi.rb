642b8ac3961b28629c75f57617895518243117df
duncdrum <d.paterson@me.com>
2017-09-26 16:18:22 +0200

    Update gephi to 0.9.2

--- Casks/gephi.rb
+++ Casks/gephi.rb
@@ -1,11 +1,11 @@
 cask 'gephi' do
-  version '0.9.1'
-  sha256 'c2a2f5c84f822303a3b314d43bc18f183dfa1d15545050bd8125d32a84e64f94'
+  version '0.9.2'
+  sha256 'e83641108bcab4326526293acf2da48ac107c95b811d7f23e1fc1c621489f097'
 
   # github.com/gephi/gephi was verified as official when first introduced to the cask
   url "https://github.com/gephi/gephi/releases/download/v#{version}/gephi-#{version}-macos.dmg"
   appcast 'https://github.com/gephi/gephi/releases.atom',
-          checkpoint: '69dacb6508d264fcd35789495674f909c6166cdae43625b208fed0c854082418'
+          checkpoint: 'a5e31f07ca40cb990942681ae1ee30889d98b77e7dcc6210872d2c3ba2fae974'
   name 'Gephi'
   homepage 'https://gephi.org/'
 
