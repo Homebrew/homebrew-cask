2d6b407d2edac01e2d5a03f8aa6180aced694aaa
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2017-09-27 18:10:48 +0100

    Update edm to 1.8.2

--- Casks/edm.rb
+++ Casks/edm.rb
@@ -1,6 +1,6 @@
 cask 'edm' do
-  version '1.7.1'
-  sha256 'cf205c429b5537cee33d4790a2760d87a78f6926ee66a2a1f2b99393f566d5ea'
+  version '1.8.2'
+  sha256 '26aa760ed41c1cca68a37d673b11d2f364ad118e267631ff41b5e25b1df94486'
 
   url "https://package-data.enthought.com/edm/osx_x86_64/#{version.major_minor}/edm_#{version}.pkg"
   name 'Enthought Deployment Manager'
