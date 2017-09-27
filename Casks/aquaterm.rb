b94d4ad779f8fda388cb2669d1fe8e3da652e538
Miccal Matthews <miccal.matthews@gmail.com>
2017-09-27 11:48:02 +0800

    Update aquaterm to 1.1.1 (#39026)

--- Casks/aquaterm.rb
+++ Casks/aquaterm.rb
@@ -1,10 +1,10 @@
 cask 'aquaterm' do
   version '1.1.1'
-  sha256 '94b33efea2ec037e6c06beef54b4b3cc48595453c874de863f25c26b3a7ffdb2'
+  sha256 '7a07d3f7cca5c0b38ca811984ef8da536da32932d68c1a6cce33ec2462b930bf'
 
   url "https://downloads.sourceforge.net/aquaterm/AquaTerm/v#{version}/AquaTerm-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/aquaterm/rss?path=/AquaTerm',
-          checkpoint: '919b1aaf6bdb5814d370b76ea198c20ee2595a738ba0697f3dd88c4b96b46050'
+          checkpoint: '7a07d3f7cca5c0b38ca811984ef8da536da32932d68c1a6cce33ec2462b930bf'
   name 'AquaTerm'
   homepage 'https://sourceforge.net/projects/aquaterm/'
 
