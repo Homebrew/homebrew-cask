3e6f79114ead14ff250b2e795cc4d06de1b6b98f
Brad Erhart <31287649+braderhart@users.noreply.github.com>
2017-09-27 13:46:57 -0700

    Update toggldesktop to 7.4.58 (#39055)

--- Casks/toggldesktop.rb
+++ Casks/toggldesktop.rb
@@ -1,11 +1,11 @@
 cask 'toggldesktop' do
-  version '7.4.56'
-  sha256 'c4917a2b06f884713fbd1c666877056a981b51c81d0c6435d7ed151645574b94'
+  version '7.4.58'
+  sha256 'd911ec53fdc4c0ffc678d024eb47cd3a1564c0882afd1884e4be4a434f5a52fe'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_stable_appcast.xml',
-          checkpoint: '9454d90a2bdf976c0ad4cf88495e6324e1c36c4a4c564335d2d4df2580b0c8a1'
+          checkpoint: '4b7a1222c778d10db0cf1aedd6d27249fc7df625663228fc28207a2510b50dd8'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com/'
 
