0424c8490f121c65c95535b751f775142b3b9465
Pelle ten Cate <pelle@occsn.com>
2017-09-27 16:24:08 +0200

    Update bitpay to 3.8.2

--- Casks/bitpay.rb
+++ Casks/bitpay.rb
@@ -1,11 +1,11 @@
 cask 'bitpay' do
-  version '3.7.1'
-  sha256 '9851b5ea2bea9a84de3d4b327ab8acb78a7bcd8738a8ad38497e91362cc58917'
+  version '3.8.2'
+  sha256 'b575724fb840c6af5c3824dfb80d374b9e9bb700c5ac5208085d040d16f46643'
 
   # github.com/bitpay/copay was verified as official when first introduced to the cask
   url "https://github.com/bitpay/copay/releases/download/v#{version}/BitPay.dmg"
   appcast 'https://github.com/bitpay/copay/releases.atom',
-          checkpoint: 'a489cf8938c1d1b05fc10ddc9101271030b52fa78d17d81315eac244e7bdfd23'
+          checkpoint: 'de635cd22d229ec1d0500232b41ea387061e5695f797ecc81e6cc262cff37999'
   name 'BitPay'
   homepage 'https://bitpay.com/'
   gpg "#{url}.sig", key_id: '9d17e656bb3b6163ae9d71725cd600a61112cfa1'
