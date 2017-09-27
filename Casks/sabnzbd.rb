e979434aa1a4efcdaf8dacb83c4cae1a4d14dfa9
commitay <commitay@users.noreply.github.com>
2017-09-27 15:34:04 +1000

    Update sabnzbd to 2.3.0

--- Casks/sabnzbd.rb
+++ Casks/sabnzbd.rb
@@ -1,6 +1,6 @@
 cask 'sabnzbd' do
   version '2.3.0'
-  sha256 'cafe5a7d106b83f273c50cd5450e8d5e25e6d8655b65f48687cc783756a42e82'
+  sha256 '2f501d77c9dac0f889443bc4187e2da66664163b1e1b5b8a13c96547ed9f866c'
 
   # github.com/sabnzbd/sabnzbd was verified as official when first introduced to the cask
   url "https://github.com/sabnzbd/sabnzbd/releases/download/#{version}/SABnzbd-#{version}-osx.dmg"
