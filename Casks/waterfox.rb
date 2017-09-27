d88c25a06d0479d49bceeeb032c3bcff51850720
commitay <commitay@users.noreply.github.com>
2017-09-27 20:30:15 +1000

    Update waterfox to 55.1.0.1 (#39035)

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,9 +1,9 @@
 cask 'waterfox' do
-  version '55.1.0'
-  sha256 '8d3d266c317f91552db67d1cb5894cd43207a6a99336c85d3fc6e69a10be97bf'
+  version '55.1.0.1'
+  sha256 '9d1cacde04a5fa27994e4b6d0cfa789d6890fbe1bd33aca0e62fc3d1b21eca48'
 
   # storage-waterfox.netdna-ssl.com was verified as official when first introduced to the cask
-  url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20#{version.before_comma}%20Setup.dmg"
+  url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20#{version}%20Setup.dmg"
   name 'Waterfox'
   homepage 'https://www.waterfoxproject.org/'
 
