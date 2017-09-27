c698a3feb86b114ffc35936fb0b5dc0c4d446c65
commitay <commitay@users.noreply.github.com>
2017-09-28 00:50:20 +1000

    onyx

--- Casks/onyx.rb
+++ Casks/onyx.rb
@@ -1,14 +1,13 @@
 cask 'onyx' do
   if MacOS.version == :el_capitan
-    version '3.1.9'
+    version '3.1.9,1011'
     sha256 '7f8df2c9e97eb465aba88b000fa2f58958421efeba1239303ff0071e9b7b0536'
-    url "https://www.titanium-software.fr/download/1011/OnyX.dmg"
   else
-    version '3.3.0'
+    version '3.3.0,1012'
     sha256 'd8f521bd348044fc2d1696f43910bfb193a9641968013cd5bbaada55e46e4799'
-    url "https://www.titanium-software.fr/download/1012/OnyX.dmg"
   end
 
+  url "https://www.titanium-software.fr/download/#{version.after_comma}/OnyX.dmg"
   appcast 'http://www.titanium-software.fr/en/release_onyx.html',
           checkpoint: '8264f1465f06db34e1c36e22f002d2200e7d50449be174ae614e7d5341a1dc90'
   name 'OnyX'
