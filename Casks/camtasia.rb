5ff02e28cc3825c3babb79cad392709c262e315f
Nick Wind-McJetters <nick@elearningexperts.net>
2017-09-27 11:02:56 -0400

    Update camtasia to 3.1.0 (#39045)

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,6 +1,6 @@
 cask 'camtasia' do
-  version '3.0.6'
-  sha256 '889445ea09adeb091c4d05d9651734bb545e521fe48bcef69dfd59ba12f98ff6'
+  version '3.1.0'
+  sha256 'c7d05d15284b962dd4c5067d46415bcc7c96fa2ef707f10927a8bdd8a5e71344'
 
   url "https://download.techsmith.com/camtasiamac/enu/#{version.no_dots}/camtasia.dmg"
   name 'Camtasia'
