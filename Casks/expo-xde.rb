592a3fe42a72a4e0309b11cc850b89ce3a0136a7
commitay <commitay@users.noreply.github.com>
2017-09-27 16:23:15 +1000

    Update expo-xde: appcast (#39029)

--- Casks/expo-xde.rb
+++ Casks/expo-xde.rb
@@ -2,8 +2,10 @@ cask 'expo-xde' do
   version '2.20.1'
   sha256 '53309014713ea1064499891767b1137d0d0202afc23c56f7fd513fe6284e7b1e'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/expo/xde was verified as official when first introduced to the cask
   url "https://github.com/expo/xde/releases/download/v#{version}/xde-#{version}.dmg"
+  appcast 'https://github.com/expo/xde/releases.atom',
+          checkpoint: '9025e19a272b131861a341d778fc044acb47fda005b6d80dd01b5f4654ff5dd2'
   name 'Expo Development Environment (XDE)'
   homepage 'https://expo.io/'
 
