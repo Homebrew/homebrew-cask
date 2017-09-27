05a8bff9e23ad2f2d08dfd8f60961f5f55fab1c2
commitay <commitay@users.noreply.github.com>
2017-09-27 20:49:40 +1000

    Update disk-drill to 3.5.860

--- Casks/disk-drill.rb
+++ Casks/disk-drill.rb
@@ -1,10 +1,10 @@
 cask 'disk-drill' do
-  version '3.3.846'
-  sha256 'cda0c421eee775e878dd2c1ddd1b331892f07c7ca76568ce2dd041e6dd7b8845'
+  version '3.5.860'
+  sha256 'd7c82a1a9102f92b7327782fa810293cdf305053d8e087f63ab0fb5b7619c8a0'
 
   url "https://www.cleverfiles.com/releases/DiskDrill_#{version}.zip"
   appcast 'https://www.cleverfiles.com/releases/auto-update/dd2-newestr.xml',
-          checkpoint: '75dd8f852613d43d2d7644da172700fd4133e10404d41bc0c6593a5530c02121'
+          checkpoint: '5948c511c26f337c20fc9a3d157962d7d1b44349438a546439acbf64a875c27a'
   name 'Disk Drill'
   homepage 'https://www.cleverfiles.com/'
 
