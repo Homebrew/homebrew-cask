11c49b257a60e8955f462b1d40a797b1cb472190
commitay <commitay@users.noreply.github.com>
2017-09-28 09:23:01 +1000

    versioned

--- Casks/sysdig-inspect.rb
+++ Casks/sysdig-inspect.rb
@@ -1,9 +1,11 @@
 cask 'sysdig-inspect' do
-  version :latest
-  sha256 :no_check
+  version '0.1.43'
+  sha256 '9ec74a9abb4748a038a4e98927ca711fa4795a6a199c4a72f071d4ee7ae7d652'
 
-  # download.sysdig.com was verified as official when first introduced to the cask
-  url 'https://download.sysdig.com/stable/sysdig-inspect/sysdig-inspect-latest-mac.dmg'
+  # download.sysdig.com/stable/sysdig-inspect was verified as official when first introduced to the cask
+  url "https://download.sysdig.com/stable/sysdig-inspect/sysdig-inspect-#{version}-mac.dmg"
+  appcast 'https://github.com/draios/sysdig-inspect/releases.atom',
+          checkpoint: 'd27e19ccf95ebab391b94762f40dd47778a1accf8f086fdc4ae53dc9d5ee3f65'
   name 'Sysdig Inspect'
   homepage 'https://github.com/draios/sysdig-inspect'
 
