3546235fb6974b72b662f0a40e877e2c9970d01e
commitay <commitay@users.noreply.github.com>
2017-09-29 08:03:13 +1000

    cave

--- Casks/cave.rb
+++ Casks/cave.rb
@@ -1,9 +1,9 @@
-cask 'awips2-cave' do
+cask 'cave' do
   version '17.1.1'
   sha256 '20c8b6a0d3f71bcdb8735af75898ccfa58dc5eeacfe424b5a3b7a804120f5cf6'
 
-  url 'http://www.unidata.ucar.edu/downloads/awips2/awips2-cave-17.1.1.dmg'
-  name 'awips2-cave'
+  url "http://www.unidata.ucar.edu/downloads/awips2/awips2-cave-#{version}.dmg"
+  name 'AWIPS Cave'
   homepage 'http://www.unidata.ucar.edu/software/awips2/'
 
   depends_on cask: 'awips-python'
