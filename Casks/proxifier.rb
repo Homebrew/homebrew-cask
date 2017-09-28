6ded93fcc64701b6ffed864baea7dd0405676549
老关 <cooclsee@KwanMBP.local>
2017-09-28 15:08:40 +0800

    Update proxifier to 2.21

--- Casks/proxifier.rb
+++ Casks/proxifier.rb
@@ -1,8 +1,8 @@
 cask 'proxifier' do
-  version '2.19'
-  sha256 'dcefba17e07a11d2ce8c12f949e9c59f80994588f3910516b58669e9e740854c'
+  version '2.21'
+  sha256 '139887d2f4468af222af2b8a1b806169918f359113547a918078792b47471540'
 
-  url 'https://www.proxifier.com/distr/ProxifierMac.zip'
+  url 'https://www.proxifier.com/distr/ProxifierMac.dmg'
   appcast 'https://www.proxifier.com/distr/last_versions/ProxifierMac.txt',
           checkpoint: '5c316b2043de3d51392e3b60fc0894167c9b2c8abb5d1fe91ccf9d9c0a2056c9'
   name 'Proxifier'
