2818811ec2133b29f773d938641635ab1d24aaf4
commitay <commitay@users.noreply.github.com>
2017-09-28 17:11:21 +1000

    appcast

--- Casks/proxifier.rb
+++ Casks/proxifier.rb
@@ -3,8 +3,8 @@ cask 'proxifier' do
   sha256 '139887d2f4468af222af2b8a1b806169918f359113547a918078792b47471540'
 
   url 'https://www.proxifier.com/distr/ProxifierMac.dmg'
-  appcast 'https://www.proxifier.com/distr/last_versions/ProxifierMac.txt',
-          checkpoint: '5c316b2043de3d51392e3b60fc0894167c9b2c8abb5d1fe91ccf9d9c0a2056c9'
+  appcast 'https://www.proxifier.com/mac/new.htm',
+          checkpoint: '602cadf245901c60c6fd178f80f5e0771ce290a1b03446f1d949ed310f7b97a2'
   name 'Proxifier'
   homepage 'https://www.proxifier.com/mac/'
 
