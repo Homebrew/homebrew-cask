4e52d4dfd3e400e2e93853df4b9d88f0ac15c0d4
Joe Wicentowski <joewiz@gmail.com>
2017-09-28 08:35:03 -0400

    Update exist-db to 3.5.0 (#39077)

--- Casks/exist-db.rb
+++ Casks/exist-db.rb
@@ -1,6 +1,6 @@
 cask 'exist-db' do
-  version '3.4.1'
-  sha256 'c4e00257a8de538f68615f211d8fca1c7e835779d5b70dcc37db563acd4e4647'
+  version '3.5.0'
+  sha256 '73853d06d9cd8100fb905e73d6dbdb70ef494818a66bc9d9f64be0d8b9c94243'
 
   # bintray.com/artifact/download/existdb was verified as official when first introduced to the cask
   url "https://bintray.com/artifact/download/existdb/releases/eXist-db-#{version}.dmg"
