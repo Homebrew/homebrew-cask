01bc700f0a3ac856f592b49ae6ecaed87d6e20b2
commitay <commitay@users.noreply.github.com>
2017-09-29 08:03:00 +1000

    awips-python

--- Casks/awips-python.rb
+++ Casks/awips-python.rb
@@ -1,10 +1,12 @@
 cask 'awips-python' do
-  version '17.1.1'
-  sha256 '48fd4fd41994c1b7321dcadd723b71fb7719512ac6f7bb47aadca42b417f7af0'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.unidata.ucar.edu/downloads/awips2/awips-python.pkg'
-  name 'awips-python.pkg'
+  name 'AWIPS Python'
   homepage 'http://www.unidata.ucar.edu/software/awips2/'
 
   pkg 'awips-python.pkg'
+
+  uninstall pkgutil: 'com.mygreatcompany.pkg.aiwps-python'
 end
