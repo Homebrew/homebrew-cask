6779d153a9d54a65c8b3e9b17ee7f005fea8d4c6
commitay <commitay@users.noreply.github.com>
2017-09-29 21:16:38 +1000

    wireshark

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -26,14 +26,6 @@ cask 'wireshark' do
 
   uninstall_preflight do
     set_ownership '/Library/Application Support/Wireshark'
-
-    system_command '/usr/sbin/dseditgroup',
-                   args: [
-                           '-o',
-                           'delete',
-                           'access_bpf',
-                         ],
-                   sudo: true
   end
 
   uninstall pkgutil:   'org.wireshark.*',
@@ -51,7 +43,13 @@ cask 'wireshark' do
                          '/usr/local/bin/text2pcap',
                          '/usr/local/bin/tshark',
                          '/usr/local/bin/wireshark',
-                       ]
+                       ],
+            script:    {
+                         executable:   '/usr/sbin/dseditgroup',
+                         args:         ['-o', 'delete', 'access_bpf'],
+                         must_succeed: false,
+                         sudo:         true,
+                       }
 
   zap delete: '~/Library/Saved Application State/org.wireshark.Wireshark.savedState'
 end
