d243172a3a435c239d2270d62f35c1a81f2d7003
Miccal Matthews <miccal.matthews@gmail.com>
2017-09-30 05:48:36 +0800

    Update local-by-flywheel.rb

--- Casks/local-by-flywheel.rb
+++ Casks/local-by-flywheel.rb
@@ -2,8 +2,8 @@ cask 'local-by-flywheel' do
   version '2.0.8'
   sha256 'ca0fc617dd1bf0f38d0274b54362c75f06d25411e3c7221aa198aab08cc7235d'
 
-  # local-by-flywheel-flywheel.netdna-ssl.com was verified as official when first introduced to the cask
-  url 'https://local-by-flywheel-flywheel.netdna-ssl.com/releases/2-0-8/local-by-flywheel-2-0-8-mac.zip'
+  # local-by-flywheel-flywheel.netdna-ssl.com/releases was verified as official when first introduced to the cask
+  url "https://local-by-flywheel-flywheel.netdna-ssl.com/releases/#{version.dots_to_underscores}/local-by-flywheel-#{version.dots_to_hyphens}-mac.zip"
   name 'Local by Flywheel'
   homepage 'https://local.getflywheel.com/'
 
