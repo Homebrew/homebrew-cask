e14b6a308c47e8e5db7cbd2026965d1da0556934
Brandon Weiss <brandon@anti-pattern.com>
2017-09-29 07:28:28 -0700

    Add Superhuman.app latest

---
+++ Casks/superhuman.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+cask 'superhuman' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://download.superhuman.com/Superhuman.dmg'
+  name 'Superhuman'
+  homepage 'https://superhuman.com/'
+
+  app 'Superhuman.app'
+
+  zap delete: [
+                '~/Library/Caches/Superhuman',
+              ],
+      trash:  [
+                '~/Library/Application Support/Milanote',
+                '~/Library/Preferences/io.nwjs.nwjs.plist',
+              ]
+end
