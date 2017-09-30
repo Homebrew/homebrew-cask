4d8a3cba869fb598543d25df21508f1164289fd7
monouser7dig <mandelkowb@gmail.com>
2017-09-30 19:47:24 +0200

    marta file manager
    dual pane file manager

---
+++ Casks/marta.rb
@@ -0,0 +1,10 @@
+cask 'marta' do
+  version '0.4.3'
+  sha256 '4412e88b45c42bdc3e328493d1f7d43ace30496a9be28da345a0d39bf4528d74'
+
+  url 'https://marta.yanex.org/updates/Marta-0.4.3.dmg'
+  name 'marta-file-manager'
+  homepage 'https://marta.yanex.org/'
+
+  app 'Marta.app'
+end
