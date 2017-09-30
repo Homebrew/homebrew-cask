d8215c73ff2226ae7bfae70808ccb607845aa50d
commitay <commitay@users.noreply.github.com>
2017-09-30 14:42:14 +1000

    google-drive-file-stream

--- Casks/google-drive-file-stream.rb
+++ Casks/google-drive-file-stream.rb
@@ -6,7 +6,21 @@ cask 'google-drive-file-stream' do
   name 'Google Drive File Stream'
   homepage 'https://www.google.com/drive/'
 
+  conflicts_with cask: [
+                         'google-backup-and-sync',
+                         'google-photos-backup-and-sync',
+                       ]
+  depends_on macos: '>= :el_capitan'
+
   pkg 'GoogleDriveFileStream.pkg'
 
-  uninstall pkgutil: 'com.google.drivefs'
+  uninstall login_item: 'Google Drive File Stream',
+            quit:       'com.google.drivefs',
+            pkgutil:    'com.google.drivefs'
+
+  zap delete: '~/Library/Caches/com.google.drivefs',
+      trash:  [
+                '~/Library/Preferences/Google Drive File Stream Helper.plist',
+                '~/Library/Preferences/com.google.drivefs.plist',
+              ]
 end
