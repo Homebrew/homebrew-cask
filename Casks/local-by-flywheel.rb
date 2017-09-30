be0432fff6d892e45d7ed2d123abf42e71ffeef0
Wassim Bourguiba <wassim@users.noreply.github.com>
2017-09-30 04:25:22 +0200

    Add Local by Flywheel v2.0.8 (#39112)
    * Add Local by Flywheel v2.0.8
    
    * Update local-by-flywheel.rb
    
    * Update local-by-flywheel.rb
    
    * depends_on cask / zap

---
+++ Casks/local-by-flywheel.rb
@@ -0,0 +1,23 @@
+cask 'local-by-flywheel' do
+  version '2.0.8'
+  sha256 'ca0fc617dd1bf0f38d0274b54362c75f06d25411e3c7221aa198aab08cc7235d'
+
+  # local-by-flywheel-flywheel.netdna-ssl.com/releases was verified as official when first introduced to the cask
+  url "https://local-by-flywheel-flywheel.netdna-ssl.com/releases/#{version.dots_to_hyphens}/local-by-flywheel-#{version.dots_to_hyphens}-mac.zip"
+  name 'Local by Flywheel'
+  homepage 'https://local.getflywheel.com/'
+
+  depends_on cask: 'virtualbox'
+
+  app 'Local by Flywheel.app'
+
+  zap delete: [
+                '~/Library/Logs/local-by-flywheel.log',
+                '~/Library/Saved Application State/com.getflywheel.local.savedState',
+              ],
+      trash:  [
+                '~/Library/Application Support/Local by Flywheel',
+                '~/Library/Preferences/com.getflywheel.local.plist',
+                '~/Library/Preferences/com.getflywheel.local.helper.plist',
+              ]
+end
