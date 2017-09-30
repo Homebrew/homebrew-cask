dd4ed83fbe39bc12d687e621c885b2eddcda3b05
commitay <commitay@users.noreply.github.com>
2017-09-30 11:21:20 +1000

    script.sh: `reinstall` / `uninstall`

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -22,7 +22,8 @@ elif [[ ${#modified_casks[@]} -gt 0 ]]; then
   run brew cask _audit_modified_casks "${TRAVIS_COMMIT_RANGE}"
   run brew cask style "${modified_casks[@]}"
   if [[ ${#modified_casks[@]} -le 10 ]]; then
-    run brew cask install "${modified_casks[@]}"
+    run brew cask reinstall "${modified_casks[@]}"
+    run brew cask uninstall "${modified_casks[@]}"
   else
     ohai 'More than 10 casks modified, skipping install'
   fi
