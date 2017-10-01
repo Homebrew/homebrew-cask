2bcd8dab8bb8438bf5f07a7f00bf285cf8d8cadd
Ryan Kelly <ryan@ryankellypercussion.com>
2017-10-01 16:27:22 -0400

    Update pinegrow-web-designer.rb
    Update to 3.1 and fix 'app source is not there' error.

--- Casks/pinegrow-web-designer.rb
+++ Casks/pinegrow-web-designer.rb
@@ -1,11 +1,11 @@
-cask 'pinegrow-web-designer' do
-  version '3.01'
+cask 'pinegrow' do
+  version '3.1'
   sha256 '4939f9df33471a4390fdcf3b49ab3d9bb20311b2d5eab860a9ce845929958092'
 
   # pinegrow.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://pinegrow.s3.amazonaws.com/PinegrowMac.#{version}.dmg"
-  name 'Pinegrow Web Designer'
+  url "http://download.pinegrow.com/PinegrowMac.#{version}.dmg"
+  name 'Pinegrow'
   homepage 'https://pinegrow.com/'
 
-  app 'Pinegrow Web Designer.app'
+  app 'Pinegrow.app'
 end
