c723b75f6f100d85542d626cb3af718c65c9a622
commitay <commitay@users.noreply.github.com>
2017-10-01 21:09:55 +1000

    Update komodo-ide to 11.0.0-90668
    .

--- Casks/komodo-ide.rb
+++ Casks/komodo-ide.rb
@@ -1,11 +1,10 @@
 cask 'komodo-ide' do
-  version '10.2.3-89902'
-  sha256 '7c46e4252d12f6c5083b8bd2869c92f7205c44f3f0d949a1b154738e2144c7a7'
+  version '11.0.0-90668'
+  sha256 '442c7f79ed67d76bba57898ab50e8a1ecd01f557d6fe687342c5697d833341ce'
 
-  # activestate.com/Komodo was verified as official when first introduced to the cask
   url "https://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*}, '')}/Komodo-IDE-#{version}-macosx-x86_64.dmg"
   name 'Komodo IDE'
-  homepage 'https://komodoide.com/'
+  homepage 'https://www.activestate.com/komodo-ide/'
 
   app "Komodo IDE #{version.major}.app"
 end
