3382df3fb2778bd96bae58f02731aab32038769f
Ben <BenjaminHCCarr@users.noreply.github.com>
2017-10-01 11:16:05 -0400

    Put sha sum in both appcash and sha256
    Typo on my part, overwrote `appcast checkpoint` with `sha256` when adding `sha256`

--- Casks/subutaitray.rb
+++ Casks/subutaitray.rb
@@ -5,7 +5,7 @@ cask 'subutaitray' do
   # cdn.subut.ai:8338/kurjun/rest/raw/ was verified as official when first introduced to the cask
   url 'https://cdn.subut.ai:8338/kurjun/rest/raw/get?name=subutai-tray.pkg'
   appcast 'https://github.com/subutai-io/tray/releases.atom',
-          checkpoint: '26de83957d95ae38d2ef124a3e00462e44bf47e31f70b12a2e350bf276a465b9'
+          checkpoint: '49db1d9212bc91293be4d38869e38c2c9f22539ae72d32b72291dec867167534'
   name 'Subutai Tray'
   homepage 'https://subutai.io/'
 
