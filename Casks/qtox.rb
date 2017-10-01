d80e34c82e5703fa24ea9e9755c7a9adae3424f1
commitay <commitay@users.noreply.github.com>
2017-10-02 07:22:58 +1000

    Update qtox to 1.12.0

--- Casks/qtox.rb
+++ Casks/qtox.rb
@@ -1,11 +1,11 @@
 cask 'qtox' do
-  version '1.11.0'
-  sha256 'c34dd0ba34f95a20ae22f51f0fa30a46bccda670a0ee8eb3ab63838c495366d3'
+  version '1.12.0'
+  sha256 '94e1e3de7a2aef703405b0abc4ce151740ab0b1806e28f9fc21ba7291ff88c86'
 
   # github.com/qTox/qTox was verified as official when first introduced to the cask
   url "https://github.com/qTox/qTox/releases/download/v#{version}/qTox.dmg"
   appcast 'https://github.com/qTox/qTox/releases.atom',
-          checkpoint: '77dd492202bc8738c56063241c9501447a57769c5f10a2cf4bd8f5124cc7b024'
+          checkpoint: 'fd55e7e69da1a0b43d2270170719538a61a61d175b67721ccab04effa93fe13d'
   name 'qTox'
   homepage 'https://qtox.github.io/'
 
