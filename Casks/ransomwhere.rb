68be9434b7d4e173c190dac598b8a8a241d55856
commitay <commitay@users.noreply.github.com>
2017-10-01 11:32:33 +1000

    Update ransomwhere to 1.2.3

--- Casks/ransomwhere.rb
+++ Casks/ransomwhere.rb
@@ -1,11 +1,11 @@
 cask 'ransomwhere' do
-  version '1.2.2'
-  sha256 '7f502e4e21991c5d294d383fbc65664c3fd0cccefabf7569b907bfe3fe2c61c3'
+  version '1.2.3'
+  sha256 'c9f21c0d5ba0512e97a89e12635f6380ada2b294ba49ce45d97bb8db3b53cefe'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/RansomWhere_#{version}.zip"
   appcast 'https://objective-see.com/products/changelogs/RansomWhere.txt',
-          checkpoint: 'ef351ca13e242fa46dea36eada86c407c3c4cb1b6531331d5d4f0c4bd446ca17'
+          checkpoint: '597301ae2d39100b0edb041933a5aa51cf07f481bb4363790bb4eca428e2964d'
   name 'RansomWhere'
   homepage 'https://objective-see.com/products/ransomwhere.html'
 
