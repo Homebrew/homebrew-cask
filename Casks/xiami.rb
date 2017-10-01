6057f531400299eb31eea38102748f733646aee2
nmchgx <i@nmchgx.com>
2017-10-01 10:42:06 +0800

    Update xiami to 3.0.9

--- Casks/xiami.rb
+++ Casks/xiami.rb
@@ -1,6 +1,6 @@
 cask 'xiami' do
-  version '3.0.8-190041D2'
-  sha256 'd1f258c8da64c47962bf00803fd280d2fcc49e1caf6e58b6c08a81dea6c3404c'
+  version '3.0.9-0A5FCD0'
+  sha256 '82e6d417da739211c0b70ea0f770754807d4f86837e1055bc7569794d3e3853b'
 
   # gxiami.alicdn.com/xiami-desktop was verified as official when first introduced to the cask
   url "http://gxiami.alicdn.com/xiami-desktop/update/%E8%99%BE%E7%B1%B3%E9%9F%B3%E4%B9%90-#{version}.dmg"
