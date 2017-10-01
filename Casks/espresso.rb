013d06e4ef015b0f89bfa9e0b97c4a9c2860e6cf
commitay <commitay@users.noreply.github.com>
2017-10-01 20:39:53 +1000

    Update espresso to 5.0.3

--- Casks/espresso.rb
+++ Casks/espresso.rb
@@ -1,11 +1,11 @@
 cask 'espresso' do
-  version '5.0.2'
+  version '5.0.3'
   sha256 '462a57b2364764f15b62da95cab24633cb59b3dca1c4f05ac6ad85701372dda4'
 
   # static.macrabbit.com was verified as official when first introduced to the cask
   url "https://static.macrabbit.com/downloads/Espresso%20v#{version.major}.zip"
   appcast "https://update.macrabbit.com/espresso/#{version}.xml",
-          checkpoint: 'bce000fa6fee673d2df9c3e73b096ee8e45e832d79e3d7b698506181d946db48'
+          checkpoint: '1a136a83d4663679501479c433d99ad45e8284a1f5296701686e974d357fa143'
   name 'Espresso'
   homepage 'https://espressoapp.com/'
 
