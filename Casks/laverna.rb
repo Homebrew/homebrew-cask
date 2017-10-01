12c6f6f06b158c8fb03b8c02bbe24f55f342e877
Futtetennista <futtetennista@gmail.com>
2017-10-01 10:13:29 +0200

    Update laverna to 0.7.5

--- Casks/laverna.rb
+++ Casks/laverna.rb
@@ -1,11 +1,11 @@
 cask 'laverna' do
-  version '0.7.3'
-  sha256 'd3fcc7ccc7130502093a53963dd0cb5dadfc37280420a3c5647de5b60424532e'
+  version '0.7.5'
+  sha256 'fa9ec7a96928bb1b85ebce671f8b7fc021dfd43e89066a642f774f53bf241c5f'
 
   # github.com/Laverna/laverna was verified as official when first introduced to the cask
   url "https://github.com/Laverna/laverna/releases/download/#{version}/laverna-#{version}-darwin-x64.zip"
   appcast 'https://github.com/Laverna/laverna/releases.atom',
-          checkpoint: '9237abe7697aa7294e5ffbbdd8ff70f6ded0e4657bfd26ae4d46140dc609262d'
+          checkpoint: '5639cfb3ef7406238312c5a45d537219276554adc69a24308af94230f78463a9'
   name 'Laverna'
   homepage 'https://laverna.cc/'
 
