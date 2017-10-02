2897204baaea97ea2b3937e4eb50c854f9c29432
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-02 20:24:35 +0800

    Update dd-utility.rb

--- Casks/dd-utility.rb
+++ Casks/dd-utility.rb
@@ -2,7 +2,7 @@ cask 'dd-utility' do
   version '1.9'
   sha256 'b28d2b5676b702aeab42ee0b973c58ed66fd293988d4aa43730bd90f4d79e663'
 
-  url 'https://github.com/thefanclub/dd-utility/raw/master/DMG/ddUtility-1.9.dmg'
+  url "https://github.com/thefanclub/dd-utility/raw/master/DMG/ddUtility-#{version}.dmg"
   name 'dd Utility'
   homepage 'https://github.com/thefanclub/dd-utility'
 
