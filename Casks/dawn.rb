02a33a80f480f75e59e11c95935aca67ff2bdd38
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-10-02 11:07:57 +0100

    Dawn 2.6.0 (new cask)

---
+++ Casks/dawn.rb
@@ -0,0 +1,11 @@
+cask 'dawn' do
+  version '2.6.0'
+  sha256 '4440940dbc1485faeb99386ab4e77927a4c98f1969f9ee8c577969d535b5c931'
+
+  # alfred.diamond.ac.uk was verified as official when first introduced to the cask
+  url 'https://alfred.diamond.ac.uk/DawnDiamond/2.6/downloads/builds-release/DawnDiamond-2.6.0.v20170904-0753-mac64.dmg'
+  name 'Dawn'
+  homepage 'http://dawnsci.org/'
+
+  app 'Dawn.app'
+end
