5fc4120c03bdacf4a613d3416359a7cb0aaf0d69
Oran Shuster <thristle666@gmail.com>
2017-10-02 11:27:25 +0300

    update catlight to version 2.12/2.14

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -1,7 +1,11 @@
 cask 'catlight' do
-  version '2.10.3'
-  sha256 '2b58a1c8e3d3f7b64d63330d668a294d1fb45afd76b8d9bccd1898563d0d2269'
-
+  if MacOS.version < '10.12'
+    version '2.12.5'
+    sha256 'e69a1dca258583d5fb16d45b71a04fe06f6f845d11bb4a4d9e909c7c3f3a835b'
+  elsif MacOS.version >= '10.12'
+    version '2.14.5'
+    sha256 '912a85e9da25eb65707869cee69c8eb84cbcc5c5169f98ef55df4910300b106e'
+  end
   url "https://www.catlight.io/dl/mac/beta/setup-#{version}.zip"
   name 'catlight'
   homepage 'https://catlight.io/'
