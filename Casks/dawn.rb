f92bb7231ce551d50f86bb34f4116836827d05dd
commitay <commitay@users.noreply.github.com>
2017-10-02 20:22:15 +1000

    dawn

--- Casks/dawn.rb
+++ Casks/dawn.rb
@@ -1,9 +1,9 @@
 cask 'dawn' do
-  version '2.6.0'
+  version '2.6.0,20170904-0753'
   sha256 '4440940dbc1485faeb99386ab4e77927a4c98f1969f9ee8c577969d535b5c931'
 
-  # alfred.diamond.ac.uk was verified as official when first introduced to the cask
-  url 'https://alfred.diamond.ac.uk/DawnDiamond/2.6/downloads/builds-release/DawnDiamond-2.6.0.v20170904-0753-mac64.dmg'
+  # alfred.diamond.ac.uk/DawnDiamond was verified as official when first introduced to the cask
+  url "https://alfred.diamond.ac.uk/DawnDiamond/#{version.major_minor}/downloads/builds-release/DawnDiamond-#{version.before_comma}.v#{version.after_comma}-mac64.dmg"
   name 'Dawn'
   homepage 'http://dawnsci.org/'
 
