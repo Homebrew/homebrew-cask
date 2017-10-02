d003d6d3ff52397fa496857e15bb4ff243521ee5
commitay <commitay@users.noreply.github.com>
2017-10-02 22:07:47 +1000

    Update vuescan to 9.5.87 (#39218)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,10 +1,10 @@
 cask 'vuescan' do
-  version '9.5.86'
-  sha256 'e83b7212b658fd148fd2b4b21559b704ffb2fd9bf42ab9a37e1bbdab37a265ab'
+  version '9.5.87'
+  sha256 'bdbfbc948daee135976ede9e8f740e9e8a4ea65c9c42fa942f4bd8fdda94fdc5'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/old-versions.html',
-          checkpoint: '7154c82944cc635229233bbdf0db27d8d4378cf900bdfd6a5fab84f3d23ae70b'
+          checkpoint: 'a9b303f2a9721b41a57e4b4513e2db7a31b068e5af35a83c40bc677cac69c81d'
   name 'VueScan'
   homepage 'https://www.hamrick.com/'
 
