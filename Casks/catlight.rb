0ed4f49b675a3713755843c230f0e30392856ea1
commitay <commitay@users.noreply.github.com>
2017-10-02 19:59:18 +1000

    `depends_on macos: '>= :sierra'`

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -1,14 +1,12 @@
 cask 'catlight' do
-  if MacOS.version < '10.12'
-    version '2.12.5'
-    sha256 'e69a1dca258583d5fb16d45b71a04fe06f6f845d11bb4a4d9e909c7c3f3a835b'
-  elsif MacOS.version >= '10.12'
-    version '2.14.5'
-    sha256 '912a85e9da25eb65707869cee69c8eb84cbcc5c5169f98ef55df4910300b106e'
-  end
+  version '2.14.5'
+  sha256 '912a85e9da25eb65707869cee69c8eb84cbcc5c5169f98ef55df4910300b106e'
+
   url "https://www.catlight.io/dl/mac/beta/setup-#{version}.zip"
   name 'catlight'
   homepage 'https://catlight.io/'
+
+  depends_on macos: '>= :sierra'
 
   app 'Catlight.app'
 end
