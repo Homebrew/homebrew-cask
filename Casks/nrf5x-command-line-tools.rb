2441a471e9ae285aa5dacea5a7d8852082a39914
Justin DeMartino <jdemar@users.noreply.github.com>
2017-10-02 15:29:54 -0700

    Update nrf5x-command-line-tools to 53406.17.93573317 (#39225)

--- Casks/nrf5x-command-line-tools.rb
+++ Casks/nrf5x-command-line-tools.rb
@@ -1,6 +1,6 @@
 cask 'nrf5x-command-line-tools' do
-  version '53412.12.75215164'
-  sha256 '40342170b40cc4a930b695a3695f1b47b29d4ba1362af280a74f6cbbbf1c925d'
+  version '53406.17.93573317'
+  sha256 '19c918e8fa44964d570721b204bf460451eabbc90030ccdc5f1eeb6513a0ace1'
 
   url "https://www.nordicsemi.com/eng/nordic/download_resource/#{version.major}/#{version.minor}/#{version.patch}"
   name 'nRF5x Command Line Tools'
