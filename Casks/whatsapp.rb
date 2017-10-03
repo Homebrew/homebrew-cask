6ebd4ef902d9228e2497369bcfd9301bff56ba0d
commitay <commitay@users.noreply.github.com>
2017-10-04 08:43:24 +1000

    url / zap

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,8 +1,8 @@
 cask 'whatsapp' do
   version '0.2.6424'
-  sha256 'a5b58bbe475377bc42ae2ef641661ca6ef37cfb7b1855e56a0a55881f5eedc96'
+  sha256 'ab2df2067986409901cdaa8dbe59b003fcf623a18c9423e7ccdb15876c85863e'
 
-  url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
+  url 'https://web.whatsapp.com/desktop/mac/files/WhatsApp.dmg'
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64',
           checkpoint: 'bc33c8de0caf113c719eec6b7d46a51cb322cbbba89e52fb0fd83527da03785e'
   name 'WhatsApp'
@@ -12,10 +12,10 @@ cask 'whatsapp' do
 
   app 'WhatsApp.app'
 
-  zap delete: [
+  zap delete: '~/Library/Caches/WhatsApp',
+      trash:  [
                 '~/Library/Application Support/WhatsApp',
                 '~/Library/Application Support/WhatsApp.ShipIt',
-                '~/Library/Caches/WhatsApp',
                 '~/Library/Preferences/WhatsApp.plist',
                 '~/Library/Preferences/WhatsApp-Helper.plist',
               ]
