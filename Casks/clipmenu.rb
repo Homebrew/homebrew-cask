811661575db992d509a81070c3d11bd287d55f56
jonasthiel <jonasthiel@users.noreply.github.com>
2017-10-03 23:39:41 +0200

    Update clipmenu.rb

--- Casks/clipmenu.rb
+++ Casks/clipmenu.rb
@@ -3,7 +3,7 @@ cask 'clipmenu' do
   sha256 '4f26de434a8d412bb063515b6f9354ae4eed12c265af36eefb83e2bfe3647eac'
 
   # dropbox.com/u/1140644 was verified as official when first introduced to the cask
-  url "https://www.dropbox.com/s/mz2v2psimbwdfql/ClipMenu.app.zip?dl=0"
+  url "https://www.dropbox.com/s/mz2v2psimbwdfql/ClipMenu.app.zip?dl=1"
   appcast 'https://feeds.feedburner.com/clipmenu-appcast',
           checkpoint: 'bcd91bd0f5da24998cbdb2ac0be516c156a60105a9504aedc05ae2a7a4165647'
   name 'ClipMenu'
