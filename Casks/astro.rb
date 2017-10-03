945447e1bea4e873692b9b69a947f94625658076
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-10-02 21:35:03 -0300

    Add Astro Latest

---
+++ Casks/astro.rb
@@ -0,0 +1,10 @@
+cask 'astro' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://download.helloastro.com/astro-mac.dmg'
+  name 'Astro'
+  homepage 'https://www.helloastro.com/'
+
+  app 'Astro.app'
+end
