4d306168cde251e3457cabd9e60cfda88eeb870f
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-04 18:51:25 +0800

    Update duet.rb

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -3,8 +3,8 @@ cask 'duet' do
   sha256 'bc259f289403c091b5fb8efeafc69af288d0c048c82283b36b6ac3bb3a2cacc0'
 
   # storage.googleapis.com/duetdisplay/ was verified as official when first introduced to the cask
-  url 'https://storage.googleapis.com/duetdisplay/duet-1-6-5-5.zip'
-  appcast 'https://s3-us-west-1.amazonaws.com/duetmac/1_6_5/DuetDisplayAppcast.xml',
+  url "https://storage.googleapis.com/duetdisplay/duet-#{version.dots_to_hyphens}.zip"
+  appcast "https://s3-us-west-1.amazonaws.com/duetmac/#{version.dots_to_underscores}/DuetDisplayAppcast.xml",
           checkpoint: '97af669d33ad9353ddbb7318681c48d8987fc8f3ce032af3d5f3815080f929c0'
   name 'Duet'
   homepage 'https://www.duetdisplay.com/'
