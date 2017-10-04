485857a6eb41aaaddf46c7902a81a496b84aaa79
Karim Naufal <rimkashox@gmail.com>
2017-10-04 10:47:47 +0200

    Update universal-media-server.rb
    update version from 6.6.0 to 6.7.4

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,11 +1,11 @@
 cask 'universal-media-server' do
-  version '6.6.0'
-  sha256 'bff9c419c5089030ac15881b37f83890e7482dc7636ac4b8c80d3f03844772b4'
+  version '6.7.4'
+  sha256 'd2c5e4579ad705bdbdfb89ac5326a96a43aa18e7f4c82b01656e3d40ca8890a0'
 
   # sourceforge.net/unimediaserver was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/unimediaserver/Official%20Releases/OS%20X/UMS-#{version}-Java8.dmg"
+  url "https://downloads.sourceforge.net/unimediaserver/Official%20Releases/OS%20X/UMS-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/unimediaserver/rss?path=/Official%20Releases',
-          checkpoint: '30aae405c758df9ac847221144dfb8e9615ef9111afa5976584a304a73f70560'
+          checkpoint: 'a6904fda55b82838bdc60eec7a5519776a1b7c418de20b0b9c747612f5e9ef59'
   name 'Universal Media Server'
   homepage 'http://www.universalmediaserver.com/'
 
