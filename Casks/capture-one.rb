f3e97388f7ba2e4f3448f62dfba5fa7d71181950
Maarc <marc@zottner.com>
2017-10-04 12:33:33 +0200

    Update capture-one to 10.2.0

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -1,6 +1,6 @@
 cask 'capture-one' do
-  version '10.1.2'
-  sha256 'fc4760f9e5e0470d9e851995cb36180ffc956607735fff36e0b73c737a536d90'
+  version '10.2.0'
+  sha256 '732a1f430c2c9aa61bf70ecb26f73ca70cedef72e5af57b17cf983a98dca3abb'
 
   url "http://downloads.phaseone.com/9c7cb7b7-1525-4cfc-86a8-7d8f5a51f38e/International/CaptureOne.Mac.#{version}.dmg"
   name 'Capture One'
