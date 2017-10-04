e06004a25338372d006e82d88dafe4c5a56baaae
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-10-05 01:02:31 +0200

    Added german language to discovercare (#39292)
    * Update discovercare to 1.0.4
    
    * Fixed german sha256

--- Casks/discovercare.rb
+++ Casks/discovercare.rb
@@ -1,8 +1,17 @@
 cask 'discovercare' do
   version '1.0.4'
-  sha256 '4f536f7e1c92787ee8a750fd4245c1d64c163807fb683ea8a59427aac57c18af'
 
-  url 'http://en.volkswagen.com/content/medialib/vwd4/global/discovercare/files/discovercare_macen/_jcr_content/renditions/rendition.download_attachment.file/discovercare_mac_en.zip'
+  language 'en', default: true do
+    sha256 '4f536f7e1c92787ee8a750fd4245c1d64c163807fb683ea8a59427aac57c18af'
+    'en'
+  end
+
+  language 'de' do
+    sha256 '1368c12cd3fa8fd0baef6cbcb9d130205778c4d249498422737b854b41b8f996'
+    'de'
+  end
+
+  url "http://en.volkswagen.com/content/medialib/vwd4/global/discovercare/files/discovercare_mac#{language}/_jcr_content/renditions/rendition.download_attachment.file/discovercare_mac_#{language}.zip"
   name 'Volkswagen DiscoverCare'
   homepage 'http://www.volkswagen.com/discovercare'
 
