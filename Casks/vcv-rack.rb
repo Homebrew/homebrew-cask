af3cbd29940cdcfb0ef431a02cbfe81269a3b27c
Vithon <vithon985@gmail.com>
2017-10-04 20:39:43 -0400

    Update vcv-rack.rb

--- Casks/vcv-rack.rb
+++ Casks/vcv-rack.rb
@@ -1,6 +1,6 @@
 cask 'vcv-rack' do
-  version '0.3.0'
-  sha256 '61222b8cf64ee22f28e94f143137b4f48fdd8365ccb07368123c097aae9ba75b'
+  version '0.3.2'
+  sha256 '7e9e820fe9b67b8ce84c317fb0169663a818989ae9f75042734da248960c6aec'
 
   url "https://vcvrack.com/downloads/Rack-#{version}-mac.dmg"
   name 'VCV Rack'
