6964e47be2e6f2d0ec3d93d92006565ebbfc8cc5
Victor Popkov <victor@popkov.me>
2017-10-06 02:37:59 +0300

    Update clarify to 2.0.8-209 (#39336)

--- Casks/clarify.rb
+++ Casks/clarify.rb
@@ -1,10 +1,11 @@
 cask 'clarify' do
-  version '2.0.5r4'
-  sha256 '454bff1e655bd8aee1c5bcb3c85339b2d09a386637b1cdb0246ab849c6283654'
+  version '2.0.8-209'
+  sha256 '76450580681426d12bea14fb4cc199d2a7b3c3febe5d2508d080fb7e7265abf3'
 
-  url "http://files.clarify-it.com/v#{version.major}/updaters/#{version}/Clarify.app.zip"
+  # amazonaws.com/files.clarify-it.com was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/files.clarify-it.com/v2/installers/Clarify%20#{version}.dmg"
   appcast "https://www.bluemangolearning.com/download/clarify/#{version.major_minor.dots_to_underscores}/auto_update/release/clarify_appcast.xml",
-          checkpoint: 'f2a5e10edb7f9d72167455e3629c0ffc516f0addafc71b40d8f1d1f822113ae9'
+          checkpoint: '168b1e6e383df8232bd9e3ac645975500c92705234f3188a9e1f89db18c23eef'
   name 'Clarify'
   homepage 'http://www.clarify-it.com/'
 
