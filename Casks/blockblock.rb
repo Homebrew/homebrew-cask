0a510dac48bec50ebd0aae3082ac2f65a708b0b3
commitay <commitay@users.noreply.github.com>
2017-10-05 16:10:39 +1000

    Update blockblock to 0.9.9 (#39309)

--- Casks/blockblock.rb
+++ Casks/blockblock.rb
@@ -1,11 +1,11 @@
 cask 'blockblock' do
-  version '0.9.8'
-  sha256 'c374461e3de75d0c258d48864ea76d4bbc5457340f41fd2b8bc51adde41542e2'
+  version '0.9.9'
+  sha256 '133ebdffd9847e17af048cdeb069ba5d1978595f6855d7fabf0cface0e562609'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/BlockBlock_#{version}.zip"
   appcast 'https://objective-see.com/products/changelogs/BlockBlock.txt',
-          checkpoint: '47a60649e6d63911b95a8d522011287117e9c7bddf201580a8d4d2053e36131e'
+          checkpoint: '143a59e168fcbdb4c391bf55d371ee09ea689246c25c005a2d2ffb0b7a62da11'
   name 'BlockBlock'
   homepage 'https://objective-see.com/products/blockblock.html'
 
