d9742225a332bfd8f98914dd9f1755aa6eb7de06
commitay <commitay@users.noreply.github.com>
2017-10-05 14:33:17 +1000

    ka-lite

--- Casks/ka-lite.rb
+++ Casks/ka-lite.rb
@@ -2,15 +2,17 @@ cask 'ka-lite' do
   version '0.17.3'
   sha256 '911746e4dcd86feac0ed3c7b88fb8bd9e96d5bac77544282e7d344d8b9e1fc4b'
 
-  url "http://pantry.learningequality.org/downloads/ka-lite/0.17/installers/mac/KA-Lite-#{version}.dmg"
+  url "http://pantry.learningequality.org/downloads/ka-lite/#{version.major_minor}/installers/mac/KA-Lite-#{version}.dmg"
   name 'Khan Academy Lite'
   homepage 'https://learningequality.org/ka-lite/'
 
   pkg 'KA-Lite.pkg'
 
-  uninstall script:  {
-                        executable: '/Applications/KA-Lite/support/scripts/ka-lite-remover.sh',
-                        sudo:       false,
-                        input:      ['Yes', 'Yes'],
+  uninstall pkgutil: 'org.learningequality.kalite',
+            signal:  ['TERM', 'org.learningequality.kalite'],
+            script:  {
+                       executable: '/Applications/KA-Lite/KA-Lite_Uninstall.tool',
+                       args:       ['Yes', 'Yes'],
+                       sudo:       true,
                      }
 end
