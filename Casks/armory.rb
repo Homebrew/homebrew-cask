2df4090f32cdcce5eecab1f3c9cde68be56de5ae
Victor Popkov <victor@popkov.me>
2017-10-05 23:23:58 +0300

    Update armory to 0.96.3.99

--- Casks/armory.rb
+++ Casks/armory.rb
@@ -1,11 +1,11 @@
 cask 'armory' do
-  version '0.96.3'
-  sha256 '851f218b78dbf0acb84c111b8989254155828d8f9fdfa5ab21c25fda10745fdc'
+  version '0.96.3.99'
+  sha256 '6129856a042b153185a06f0745cf5313477191cc89a8f8c3e5dc08fa0b49c8a0'
 
   # github.com was verified as official when first introduced to the cask
   url "https://github.com/goatpig/BitcoinArmory/releases/download/v#{version}/armory_#{version}_osx.tar.gz"
   appcast 'https://github.com/goatpig/BitcoinArmory/releases.atom',
-          checkpoint: '46af4ad65f91f3ae92d812b63a8efbce244d14c2b290b5e2c0f8d25e8f9ba7e0'
+          checkpoint: '4f71fb59453d47d90216ac261bffe9c3c1760b62bacc32fe19f637711e5cf4f8'
   name 'Armory'
   homepage 'https://btcarmory.com/'
 
