68ffee600a81965ba10d97ec59c18782dfadfbf7
commitay <commitay@users.noreply.github.com>
2017-10-07 08:21:22 +1000

    Update cloudapp to 4.3.1

--- Casks/cloudapp.rb
+++ Casks/cloudapp.rb
@@ -1,11 +1,11 @@
 cask 'cloudapp' do
   version '4.3.1'
-  sha256 'f3cfee8d40b7de45b43bf3e99c5f3c88fdb50f468513bc6feb6ed21bc83d311c'
+  sha256 '55222f91ac19e1809baa8488da6b843f0c3dd69b1ddd80ef1fdc1aed231d6381'
 
   # amazonaws.com/downloads.getcloudapp.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/downloads.getcloudapp.com/mac/CloudApp-#{version}.zip"
   appcast 'https://updates.getcloudapp.com/appcast.xml',
-          checkpoint: '1c250b628b810cb5c3cdbf38ec793c8c53e4ad47d51c69d7a5ac33082720cda0'
+          checkpoint: '4c7363e7d6e5cec86236da951fee7e57337c860590d6612b19c4afb487ab8df6'
   name 'CloudApp'
   homepage 'https://www.getcloudapp.com/'
 
