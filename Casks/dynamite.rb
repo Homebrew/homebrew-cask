9363683ce9bb96da2d04c605936246e134691dad
Victor Popkov <victor@popkov.me>
2017-10-06 03:36:27 +0300

    Update dynamite to 3.2.6 (#39340)

--- Casks/dynamite.rb
+++ Casks/dynamite.rb
@@ -1,10 +1,10 @@
 cask 'dynamite' do
-  version '3.2.5'
-  sha256 'd8f68bb331ae7e6201ad62bcee7483f73a91fefbb3ac0e3db28e3d87dcbae3d4'
+  version '3.2.6'
+  sha256 '8aceec8920ba0ca0fe143b646aa42539640eee941050f0b01ce0e89772e66bd6'
 
   url "https://mediaatelier.com/DynaMite3/DynaMite_#{version}.zip"
   appcast 'https://mediaatelier.com/DynaMite3/feed.xml',
-          checkpoint: 'e2ca62d7b2b3a650b30d572a0cb9a004588d7795eec6e550b206baef33e5bdfa'
+          checkpoint: 'b204b7b36cf64ce83d1d23034e458da365c256f10e87721994501ffd9901ea0b'
   name 'DynaMite'
   homepage 'https://www.mediaatelier.com/DynaMite3/'
 
