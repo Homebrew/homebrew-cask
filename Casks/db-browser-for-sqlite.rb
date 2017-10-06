0156854419a7d9f0cf1d52048fdb94354323896b
commitay <commitay@users.noreply.github.com>
2017-10-06 23:47:47 +1000

    Rename sqlitebrowser to db-browser-for-sqlite (#39367)

--- Casks/db-browser-for-sqlite.rb
+++ Casks/db-browser-for-sqlite.rb
@@ -1,11 +1,11 @@
-cask 'sqlitebrowser' do
+cask 'db-browser-for-sqlite' do
   version '3.10.1'
   sha256 '9456e8ff081004bd16711959dcf3b5ecf9d304ebb0284c51b520d6ad1e0283ed'
 
   # github.com/sqlitebrowser/sqlitebrowser was verified as official when first introduced to the cask
   url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version.major_minor_patch}/DB.Browser.for.SQLite-#{version}.dmg"
   appcast 'https://github.com/sqlitebrowser/sqlitebrowser/releases.atom',
-          checkpoint: 'a7b9929b01b4c4b67da144d2dedd26c8f0db460bc96de76fede907062458de67'
+          checkpoint: 'b481ca94a0597036e14c729767e4307ec871d9febce53415b87770adda7acf26'
   name 'SQLite Database Browser'
   homepage 'http://sqlitebrowser.org/'
 
