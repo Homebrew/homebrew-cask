11020a32aa2571edac83a6964881075361549750
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-10-06 09:21:43 -0300

    Update astro to latest

--- Casks/astro.rb
+++ Casks/astro.rb
@@ -1,11 +1,8 @@
 cask 'astro' do
-  version '2.0.18,3149'
-  sha256 '3f584b9f892d73542dac1ae69522c8840ba16b50d0deaba771c1eea97c169474'
+  version :latest
+  sha256 :no_check
 
-  # pexlabs-updates-xvuif5mcicazzducz2j2xy3lki.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
-  url "https://pexlabs-updates-xvuif5mcicazzducz2j2xy3lki.s3-us-west-2.amazonaws.com/Astro-#{version.after_comma}.dmg"
-  appcast 'https://pexlabs-updates-xvuif5mcicazzducz2j2xy3lki.s3-us-west-2.amazonaws.com/pexappcast.xml',
-          checkpoint: '7869d7606e0857110985f266dcf704bf8082135b34412952e13b98a81fe3a517'
+  url 'http://download.helloastro.com/astro-mac.dmg'
   name 'Astro'
   homepage 'https://www.helloastro.com/'
 
