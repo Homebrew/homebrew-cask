34022cc04196ce14cf477321700de4f4987bb5d4
commitay <commitay@users.noreply.github.com>
2017-10-08 07:13:09 +1000

    Update metabase-app to 0.26.1.1

--- Casks/metabase-app.rb
+++ Casks/metabase-app.rb
@@ -1,10 +1,10 @@
 cask 'metabase-app' do
-  version '0.26.1.0'
-  sha256 '9fcdf7f3153ca52b744e79e67a57bfef24a39c5d2292580607844b8445dc601d'
+  version '0.26.1.1'
+  sha256 '32d34717a38c564bf0d02869e75b91feaec43b2efe7fe1c67436c68c2e04cf61'
 
   url "http://downloads.metabase.com/v#{version.major_minor_patch}/Metabase.dmg"
   appcast 'http://downloads.metabase.com/appcast.xml',
-          checkpoint: '7e151a74263694368506f7c2b72eb83b92d56e2dd0bd77909cefb09fd6ccdc34'
+          checkpoint: 'f490d9cb1ff76e22d14ae58856df4cf33a27c48b6c0c6a7f41968076bbb8e9d6'
   name 'Metabase'
   homepage 'http://www.metabase.com/'
 
