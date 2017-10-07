60d500c85584a1890ddffe1cfcb2ec8f1c448ae3
commitay <commitay@users.noreply.github.com>
2017-10-08 07:04:14 +1000

    Update waves-central.rb

--- Casks/waves-central.rb
+++ Casks/waves-central.rb
@@ -1,6 +1,6 @@
 cask 'waves-central' do
-  version '1.3.3.4'
-  sha256 '8940c8a9d73832524501fcb2a48b578033bd868509521f3483d8a3a6bd731404'
+  version :latest
+  sha256 :no_check
 
   url 'http://cf-installers.waves.com/WavesCentral/Install_Waves_Central.dmg'
   name 'Waves Central'
