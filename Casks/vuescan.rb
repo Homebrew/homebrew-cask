d4f38c3e7f218960e48eb6373feef239466f33b3
Yaw Anokwa <yanokwa@users.noreply.github.com>
2017-10-07 06:21:30 +0300

    Update vuescan to 9.5.88 (#39392)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.5.88'
-  sha256 '93e7b8e51042242dcb7af0067787db31d3e47482eb7399b39b59c4c5e3cdd12f'
+  sha256 'e71ee71d83aff50933b2cb5121e5d878fd305b7ad54606f0d24c318ff9090f50'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/old-versions.html',
