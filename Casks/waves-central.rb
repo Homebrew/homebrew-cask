a68849476ce7bd6a8a88529c755fdc679a1f92b8
soycode <soycode@users.noreply.github.com>
2017-10-07 11:17:57 -0700

    Update waves-central to 1.3.3.4
    N.B. - download URL changed, can verify at https://www.waves.com/downloads/central

--- Casks/waves-central.rb
+++ Casks/waves-central.rb
@@ -1,8 +1,8 @@
 cask 'waves-central' do
-  version '1.3.2.4'
-  sha256 'a51c3cdc4c7c2d72920717b90349a139768cb2a09a8fd57a686cae9fb615482f'
+  version '1.3.3.4'
+  sha256 '8940c8a9d73832524501fcb2a48b578033bd868509521f3483d8a3a6bd731404'
 
-  url "http://installers.waves.com/WavesCentral/V#{version}/Install_Waves_Central.dmg"
+  url 'http://cf-installers.waves.com/WavesCentral/Install_Waves_Central.dmg'
   name 'Waves Central'
   homepage 'http://www.waves.com/'
 
