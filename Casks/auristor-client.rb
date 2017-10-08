d26931c4827401a3f2939824b5da768efb3c5b55
commitay <commitay@users.noreply.github.com>
2017-10-08 22:45:56 +1000

    Update auristor-client.rb

--- Casks/auristor-client.rb
+++ Casks/auristor-client.rb
@@ -10,9 +10,12 @@ cask 'auristor-client' do
   elsif MacOS.version == :el_capitan
     sha256 'ec5e76c06d8a1342dfb41173cac303999fbfa31779acdcb9e0d472d22688cf17'
     url "https://www.auristor.com/downloads/auristor/osx/macos-10.11/AuriStor-client-#{version}-ElCapitan.dmg"
-  else
+  elsif MacOS.version == :sierra
     sha256 '6577b6fa6c723dd703132650215aec0a1c300b832afe4b6525cb989407eb530a'
     url "https://www.auristor.com/downloads/auristor/osx/macos-10.12/AuriStor-client-#{version}-Sierra.dmg"
+  else
+    sha256 'b01815f0b3c4eb60bc0c19e17fa45da20fba7670cd156ca5f76a2b0260883690'
+    url "https://www.auristor.com/downloads/auristor/osx/macos-10.13/AuriStor-client-#{version}-HighSierra.dmg"
   end
 
   name 'AuriStor File System Client'
@@ -24,6 +27,7 @@ cask 'auristor-client' do
                       :yosemite,
                       :el_capitan,
                       :sierra,
+                      :high_sierra,
                     ]
 
   pkg 'Auristor-Lite.pkg'
