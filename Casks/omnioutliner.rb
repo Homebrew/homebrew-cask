860fad0b8766aef0d87aee3e0aa0ae83cd2675d8
masudakoji <masuda@kobe-kosen.ac.jp>
2017-10-08 11:45:22 +0900

    Update omnioutliner to 5.1.4

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -1,10 +1,10 @@
 cask 'omnioutliner' do
-  version '5.1.2'
-  sha256 'dad422b24ef079fda6dddc8f67ce661ac4649330ec62188869d7e529a5915c6e'
+  version '5.1.4'
+  sha256 '91817e87a29c6a86f64b22f36e292b354aab89f63a070eeab117f4fbb2704ff0'
 
   url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniOutliner-#{version}.dmg"
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniOutliner#{version.major}",
-          checkpoint: 'ddef8a1b4610bb0b23f5df5ff9468e74b080e4afe3d548602f494da7d398edac'
+          checkpoint: '9966a34bf39bb4d5d2aaebdf38953a8b63fbf851ede9a2cd3f70370b87585322'
   name 'OmniOutliner'
   homepage 'https://www.omnigroup.com/omnioutliner/'
 
