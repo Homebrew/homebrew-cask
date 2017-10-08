308b5cf7282a8effdcec03c448d71e1e45c96207
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-10-08 12:17:54 -0300

    Update toggldesktop to 7.4.58 (#39418)

--- Casks/toggldesktop.rb
+++ Casks/toggldesktop.rb
@@ -1,6 +1,6 @@
 cask 'toggldesktop' do
   version '7.4.58'
-  sha256 'd911ec53fdc4c0ffc678d024eb47cd3a1564c0882afd1884e4be4a434f5a52fe'
+  sha256 '44f238e7054fe02a002dd04093a34baee9420bc2c32320081f8509c5ca6af7d4'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
