ef0b641c2f611901942c1074f8eae0356b80251e
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-09 22:37:29 +0800

    Update djview.rb

--- Casks/djview.rb
+++ Casks/djview.rb
@@ -1,8 +1,8 @@
 cask 'djview' do
-  version '4.10.6'
+  version '4.10.6,57c'
   sha256 '6ad1fe387434da4d103cbe343a7d6bb0da0fa139787069b3ba76f124651402a2'
 
-  url "https://downloads.sourceforge.net/djvu/DjVuLibre-3.5.27%2BDjView-#{version}-qt57c-intel64.dmg"
+  url "https://downloads.sourceforge.net/djvu/DjVuLibre-3.5.27%2BDjView-#{version.before_comma}-qt#{version.after_comma}-intel64.dmg"
   appcast 'https://sourceforge.net/projects/djvu/rss',
           checkpoint: '608eefef3e99391add5aa043ba70bee99d2cea3a7276fab6d96398e11877dd39'
   name 'DjView'
