476044b9a71962e2fd85f6ee1eb1295e7e2519cb
Gobinath Mallaiyan <i@gobinath.com>
2017-10-09 07:11:45 -0400

    Update canary to 1.7.0,354

--- Casks/canary.rb
+++ Casks/canary.rb
@@ -1,11 +1,11 @@
 cask 'canary' do
-  version '1.6.8,351'
-  sha256 'b27751f158728422e4b1a8f6f1a10cf1f43cee47b57bb4ad26c2be953c5f0ba1'
+  version '1.7.0,354'
+  sha256 '1f50c23b54748cd0a90d3b6bc3760984eee51b6b36519b2634bd9f5499281623'
 
   # rink.hockeyapp.net/api was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050',
-          checkpoint: '24803d0b202511e4e2d9452ad745c07010d090dce0af9776d44b2373dbc1f7e8'
+          checkpoint: '92b04e7859877e6ac713d4ba1543cfe96d4a1e18887d908924e0f0ac42b190c4'
   name 'Canary'
   homepage 'https://canarymail.io/'
 
