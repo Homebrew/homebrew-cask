a8c0d8f0d804d450d0adb549afce0c14e76a7d73
Danny Amey <danny@dannyamey.com>
2017-10-09 22:22:58 +0100

    Update ivpn to 2.6.7

--- Casks/ivpn.rb
+++ Casks/ivpn.rb
@@ -1,6 +1,6 @@
 cask 'ivpn' do
-  version '2.6.6'
-  sha256 '1ba8daa7246503192c0283c3d4b1406aa82e5fc7699c72567eff760a8ecfa3be'
+  version '2.6.7'
+  sha256 'c95ab53abdf66de2bd673e45bdb61211d912bce123e3babac397333bc8957cf8'
 
   url "https://www.ivpn.net/releases/osx/IVPN-#{version}.dmg"
   name 'IVPN'
