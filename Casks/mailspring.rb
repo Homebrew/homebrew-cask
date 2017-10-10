8be4dd969ffbf010874895c52cc73e65be9b71fb
commitay <commitay@users.noreply.github.com>
2017-10-10 20:29:03 +1000

    latest / no_check

--- Casks/mailspring.rb
+++ Casks/mailspring.rb
@@ -1,10 +1,8 @@
 cask 'mailspring' do
-  version '1.0.1'
-  sha256 '4081550429898431dd3fd632af12be6dbcf9a5954d80f0d2c0f0ddbc71bb623e'
+  version :latest
+  sha256 :no_check
 
   url 'https://updates.getmailspring.com/download?platform=darwin'
-  appcast 'https://github.com/Foundry376/Mailspring/releases.atom',
-          checkpoint: '8e8f8ed372265019d0d9bc8280a2d241d5b41dadb6408468ae149cc2a09962a9'
   name 'Mailspring'
   homepage 'https://getmailspring.com/'
 
