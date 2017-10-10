7e588b7a2ba230c5f64a877cc5f6386917f0bd37
Schorsch <XiangChen888@users.noreply.github.com>
2017-10-10 22:21:57 +0200

    Update downie to 3.1672 (#39502)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.1661'
-  sha256 'dc0c1f0183756a5199f26f81c3274c18ab151cc0b1e8e6dab919df63484e99d7'
+  version '3.1672'
+  sha256 '2efcdb4ab6ae28f2bdc6671dea9b334170d61521a3e40522f5b61298684c7a86'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.dots_to_underscores}.zip"
   appcast 'https://trial.charliemonroe.net/downie/updates_2.0.xml',
