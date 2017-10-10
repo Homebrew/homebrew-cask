cb3ab409fe6cf0bdf5e6e234728477a256c7f6aa
commitay <commitay@users.noreply.github.com>
2017-10-10 15:01:38 +1000

    Update freefilesync.rb

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -2,7 +2,7 @@ cask 'freefilesync' do
   version '9.4'
   sha256 'ba62b487b0f13351a2fb61e47b0a4b68c720a8183286bb18e3bb1d539b6caabe'
 
-  url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
+  url "http://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake
   name 'FreeFileSync'
   homepage 'https://www.freefilesync.org/'
