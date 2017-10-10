b8147325986c9620d2ac7992e9b7582df7426364
Richard Quadling <RQuadling@GMail.com>
2017-10-10 11:30:21 +0100

    Update fastonosql to 1.9.1 (#39487)

--- Casks/fastonosql.rb
+++ Casks/fastonosql.rb
@@ -1,10 +1,10 @@
 cask 'fastonosql' do
-  version '1.8.1'
-  sha256 'd24ddc9109c4d80d61570f7c176ad306895a04b1804052232ce0fef1d868efa8'
+  version '1.9.1'
+  sha256 '2754220bf38a8891f82029176b81c4a33c7c61126a800c61123110eb86985f2a'
 
   url "https://www.fastonosql.com/downloads/macosx/fastonosql-#{version}-x86_64.dmg"
   appcast 'https://github.com/fastogt/fastonosql/releases.atom',
-          checkpoint: '04d359c4c787c831f7903f982bfac8261bb2ff308cc9ee22df4f9718054bea6c'
+          checkpoint: 'b2db1cebdb5ab9665fa41451eae277577d8af3dc828a0b00b7c99dae9903b623'
   name 'FastoNoSQL'
   homepage 'https://www.fastonosql.com/'
 
