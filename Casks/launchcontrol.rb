d3aed6553bc762df9f6ca16fb5ef86e9363c4dcb
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2017-10-09 23:46:21 -0600

    Update launchcontrol to 1.35 (#39479)

--- Casks/launchcontrol.rb
+++ Casks/launchcontrol.rb
@@ -1,10 +1,10 @@
 cask 'launchcontrol' do
-  version '1.34'
-  sha256 'd3738e150521ab47508debce7445865df046d8f8e8da2338ea1d4cce62010270'
+  version '1.35'
+  sha256 'b23eb7cebccad7e6fb3ff3e2cc60350eea6cde136b0872353b20b34ad4ef9966'
 
   url "http://www.soma-zone.com/download/files/LaunchControl_#{version}.tar.bz2"
   appcast 'http://www.soma-zone.com/LaunchControl/a/appcast.xml',
-          checkpoint: '27bc5748f894f69326f4ef6bf4c938524fa4a49416495f7a1bf1891b829d85dc'
+          checkpoint: '776efea8c485dc3c33aad071ee5ebfe77ba4a0329ad2971d3c7801b40e23ccda'
   name 'LaunchControl'
   homepage 'http://www.soma-zone.com/LaunchControl/'
 
