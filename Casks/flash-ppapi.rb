8ed14c76ed7b70899fac3109947b8f5b0506e31c
commitay <commitay@users.noreply.github.com>
2017-10-10 15:39:05 +1000

    Update flash-ppapi to 27.0.0.159 (#39474)

--- Casks/flash-ppapi.rb
+++ Casks/flash-ppapi.rb
@@ -1,10 +1,10 @@
 cask 'flash-ppapi' do
-  version '27.0.0.130'
-  sha256 'f0429125904e103da53a17440509ed483b8e9bcec26cba27445c3631075770e2'
+  version '27.0.0.159'
+  sha256 '5cf2dbec441cd9b1cf9522a2cd55710e6005118012dc89d0358c614b7905e779'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/pdc/#{version}/install_flash_player_osx_ppapi.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
-          checkpoint: '918f08ed443fe8c1da41ed988a4c80208063c10994a52b6b01322c410a86e6c9'
+          checkpoint: 'f9cdc3e74103ce8f2bf46664475ec4ab55be96fe3bd71cc5b9a31d052ecb9eca'
   name 'Adobe Flash Player PPAPI (plugin for Opera and Chromium)'
   homepage 'https://get.adobe.com/flashplayer/otherversions/'
 
