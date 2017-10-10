d61dcc98213541127a5f26842c75dfbd28f3aff9
Vítor Galvão <info@vitorgalvao.com>
2017-10-10 18:48:36 +0100

    Update casparcg-client.rb

--- Casks/casparcg-client.rb
+++ Casks/casparcg-client.rb
@@ -2,8 +2,8 @@ cask 'casparcg-client' do
   version '2.0.8'
   sha256 '0bb51b061ec6a0a01b3702869e4ca24b298660cd82e892c39395a0b21ea7fb5b'
 
-  # downloads.sourceforge.net/casparcg/CasparCG_Client/CasparCG_Client_2.0 was verified as official when first introduced to the cask
-  url 'https://downloads.sourceforge.net/casparcg/CasparCG_Client/CasparCG_Client_2.0/CasparCG_Client_2.0.8_Mac.dmg'
+  # downloads.sourceforge.net/casparcg/CasparCG_Client was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/casparcg/CasparCG_Client/CasparCG_Client_#{version.major_minor}/CasparCG_Client_#{version}_Mac.dmg"
   name 'CasparCG Client'
   homepage 'http://casparcg.com/'
 
