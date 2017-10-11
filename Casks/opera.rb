bb65533a6875e337bd4d7347e4e4196c33bfad81
commitay <commitay@users.noreply.github.com>
2017-10-11 10:07:24 +1000

    Update opera to 48.0.2685.39 (#39510)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '48.0.2685.35'
-  sha256 '6704895e75dff228cd861f09aae894c928a9fdb196f8449f5e616fc9ebfb4516'
+  version '48.0.2685.39'
+  sha256 '5d125cf7f32cb10547332bf903924a210a2f67b80a0ae4dc19899efbcc580076'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
