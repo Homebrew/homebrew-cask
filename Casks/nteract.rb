48a77f314536712955924fe22bdf2e0e00c9d8ed
Alex Vykaliuk <alekzvik@users.noreply.github.com>
2017-10-11 15:18:40 +0300

    Update nteract to 0.3.3 (#39529)

--- Casks/nteract.rb
+++ Casks/nteract.rb
@@ -1,10 +1,10 @@
 cask 'nteract' do
-  version '0.3.1'
-  sha256 '46c8b605bb6f34a599b6131854358c81742f1168d2b1768f7669345f8f88b504'
+  version '0.3.3'
+  sha256 '77b568f8eafce484a0694794d591b265633cfa62df765f3aaaa7664c8cea4716'
 
   url "https://github.com/nteract/nteract/releases/download/v#{version}/nteract-#{version}.dmg"
   appcast 'https://github.com/nteract/nteract/releases.atom',
-          checkpoint: '72a33edd534cb0077df628cfa1342a1904ca415b7755b65f4bfc0347a9c401c2'
+          checkpoint: '83e578730434294f3d77f253e5e56b7e2590890fccc4b9f42d811b5fdfe35940'
   name 'nteract'
   homepage 'https://github.com/nteract/nteract'
 
