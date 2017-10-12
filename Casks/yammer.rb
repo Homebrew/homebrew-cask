d267ee9009a3f6dc1b8934b60600485277354dcc
commitay <commitay@users.noreply.github.com>
2017-10-12 22:57:32 +1000

    yammer

---
+++ Casks/yammer.rb
@@ -0,0 +1,11 @@
+cask 'yammer' do
+  version '1.2.1'
+  sha256 'c9cdefc2098733fa1ce141d29328644aac10018e8a7389fb4b88413551a53015'
+
+  # yammerdesktopapp.blob.core.windows.net/binaries/dist was verified as official when first introduced to the cask
+  url "https://yammerdesktopapp.blob.core.windows.net/binaries/dist/darwin/x64/#{version}/Yammer-#{version}.dmg"
+  name 'Yammer'
+  homepage 'https://support.office.com/en-us/article/Yammer-for-Windows-and-Mac-50920c05-cbfc-4f11-8503-e20fb2e623a5'
+
+  app 'Yammer.app'
+end
