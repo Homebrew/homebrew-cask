d3bf83137ad612efbda1acb920411d53e0f50c13
VonUniGE <VonUniGE@github.com>
2017-10-12 22:20:20 +0200

    Update sts to 3.9.1

--- Casks/sts.rb
+++ Casks/sts.rb
@@ -1,10 +1,10 @@
 cask 'sts' do
-  version '3.9.0.RELEASE'
-  sha256 'fe36856fde5f76299814c02d2106c4d1cea1bd934bf61339934089cd7d64d24b'
+  version '3.9.1.RELEASE'
+  sha256 '17bdeecd0b485b02475a2b32f06d61b23450fca14d45acc18a557fc707fb657b'
 
   module Utils
     def self.eclipse_version
-      '4.7.0' # find eclipse version at https://spring.io/tools/sts/all
+      '4.7.1a' # find eclipse version at https://spring.io/tools/sts/all
     end
 
     def self.eclipse_version_major_minor
@@ -13,11 +13,11 @@ cask 'sts' do
   end
 
   # download.springsource.com/release/STS was verified as official when first introduced to the cask
-  url "http://download.springsource.com/release/STS/#{version}/dist/e#{Utils.eclipse_version_major_minor}/spring-tool-suite-#{version}-e#{Utils.eclipse_version}-macosx-cocoa-x86_64.tar.gz"
+  url "http://download.springsource.com/release/STS/#{version}/dist/e#{Utils.eclipse_version_major_minor}/spring-tool-suite-#{version}-e#{Utils.eclipse_version}-macosx-cocoa-x86_64.dmg"
   name 'Spring Tool Suite'
   homepage 'https://spring.io/tools/sts'
 
   auto_updates true
 
-  app 'sts-bundle/STS.app'
+  app 'STS.app'
 end
