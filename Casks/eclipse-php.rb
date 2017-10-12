7b306ca69e947dda7e3c66ae7b3973a4b4e01906
scribblemaniac <scribblemaniac@users.noreply.github.com>
2017-10-12 00:42:35 -0600

    Update eclipse-php to 4.7.1a

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-php' do
-  version '4.7.0,oxygen:R'
-  sha256 'cbaa369bfcea3bbc4692ec56791290cf752249c3e5d762facc631346129e9ebe'
+  version '4.7.1,oxygen:1a'
+  sha256 'ed9f433f5e8f5ad03a9bbbcf8d35b237d246c63826e583c9ff3a2fde2bfa7168'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-php-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for PHP Developers'
