879611ac154e26c9a20052919be1761ef28c54b1
Dieter Blomme <dieterblomme@gmail.com>
2017-10-13 09:52:05 +0200

    Fixed checksum for mamp 4.2.1 (#39485)

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -4,7 +4,7 @@ cask 'mamp' do
     sha256 'b935d118b6e14d43cf2570be2a05166a15b824c3edc0ede98cc30e6ab5af3697'
   else
     version '4.2.1'
-    sha256 '808e9685153c12a50d582e5eb27badbbc0cdc0df3fd3e0bf4bcaa74c4dc6a058'
+    sha256 '9428a3cf494d02d94ecaec302042822f54273ba9d412ecde25d1155ab07db56e'
   end
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
