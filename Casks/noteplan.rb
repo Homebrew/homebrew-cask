1b3128257535d1a09537bb917d1474b73208e06d
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-13 08:32:49 +0800

    Update noteplan.rb

--- Casks/noteplan.rb
+++ Casks/noteplan.rb
@@ -1,8 +1,11 @@
 cask 'noteplan' do
-  version :latest
-  sha256 :no_check
+  version '1.6.18'
+  sha256 '0427cc19a1e4c0705d1f0c8dc8fea720f474100c5f951ef4eaf79075a1582b9d'
 
-  url 'http://noteplan.co/download/'
+  # rink.hockeyapp.net/api/2/apps/304b4477155e428780c345bcab69b380 was verified as official when first introduced to the cask
+  url 'https://rink.hockeyapp.net/api/2/apps/304b4477155e428780c345bcab69b380/?format=zip'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/304b4477155e428780c345bcab69b380',
+          checkpoint: '5ccfecc72b7c1f398c209db3ab4c124b223e04bc4ca262c237a241507adb79c4'
   name 'Noteplan'
   homepage 'http://noteplan.co/'
 
