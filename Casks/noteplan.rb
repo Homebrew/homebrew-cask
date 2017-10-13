747869dcbdee79ce3baa3568ea430b6ee2be8697
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-13 08:26:51 +0800

    Update noteplan.rb

--- Casks/noteplan.rb
+++ Casks/noteplan.rb
@@ -2,7 +2,7 @@ cask 'noteplan' do
   version :latest
   sha256 :no_check
 
-  url 'http://noteplan.co/download'
+  url 'http://noteplan.co/download/'
   name 'Noteplan'
   homepage 'http://noteplan.co/'
 
