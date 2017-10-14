2e7f83163c06005f63f1eb54f50b52c37d802871
giannitm <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-10-14 20:12:17 +0100

    Update flightgear to 2017.3.1

--- Casks/flightgear.rb
+++ Casks/flightgear.rb
@@ -1,11 +1,11 @@
 cask 'flightgear' do
-  version '2017.2.1'
-  sha256 '117fb9d18681e52007895c876ab9864aaf99f5b7a7899928bc7e365de4f1b6d7'
+  version '2017.3.1'
+  sha256 '902100088b1f572be919660779160414c6eaa6b3cf196fcff9f416fa8f017f13'
 
   # sourceforge.net/flightgear was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/flightgear/FlightGear-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/flightgear/rss',
-          checkpoint: 'f3173cad3e18b44b9297ef200ea4d1ea9e171818af279e6fb834f5131d879574'
+          checkpoint: 'a89692e0882503af216e22ea381e20ce4ef7bfdd78fb3ee4f8b360ed634f4950'
   name 'FlightGear'
   homepage 'http://www.flightgear.org/'
 
