27adc7a53b624c5bfdcceeaf7d44edb36ea904ad
cooclsee <cooclsee@qq.com>
2017-10-14 16:03:08 +0800

    Update mqttfx to 1.5.1

--- Casks/mqttfx.rb
+++ Casks/mqttfx.rb
@@ -1,6 +1,6 @@
 cask 'mqttfx' do
-  version '1.5.0'
-  sha256 '894708779e1db41a7cd8f26d9e3d0776558921be1c10dba6a32f46df5b41e2f9'
+  version '1.5.1'
+  sha256 '7a6b75862f3d77afaf5195d5a34fa49be7c65817a0c08fbdeac5929b12e95fd5'
 
   # jensd.de/apps/mqttfx was verified as official when first introduced to the cask
   url "http://www.jensd.de/apps/mqttfx/#{version}/mqttfx-#{version}-macos.dmg"
