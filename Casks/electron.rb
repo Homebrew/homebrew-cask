f85fd830b9fe0a3cc78d08d6e99e94998c27350e
Stephane Paquet <spaquet@up4b.com>
2017-10-14 09:25:59 -0700

    Update electron to 1.7.9

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,11 +1,11 @@
 cask 'electron' do
-  version '1.7.8'
-  sha256 'f1a027f6356d226f85c9862ad175543f004df8ef7bd7950732e16e6466e8e2f3'
+  version '1.7.9'
+  sha256 '9e2a3ea75796f4e991512e055b858cdfdcc159b9c8e5b19c53be07102ed513e9'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/electron/electron/releases.atom',
-          checkpoint: '923d0b288ae07101d5e473b54e62cbea98ee8b172623969225fbb1930fcfb5c5'
+          checkpoint: '49d509921f87492a9a878dc876c44c06c0b10c2693969b0d28fcb3b6891d0fd6'
   name 'Electron'
   homepage 'https://electron.atom.io/'
 
