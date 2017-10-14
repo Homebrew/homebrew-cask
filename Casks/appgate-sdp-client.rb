83565c773eeb23538f4e591bdede4c8e26c1fc54
commitay <commitay@users.noreply.github.com>
2017-10-14 10:18:01 +1000

    version / zap

--- Casks/appgate-sdp-client.rb
+++ Casks/appgate-sdp-client.rb
@@ -1,8 +1,8 @@
 cask 'appgate-sdp-client' do
-  version '3.2.1.8632'
+  version '3.2.1'
   sha256 'db178b54893c5d291553f35ab5084cd289bf4f92180893b127d0129c4ba181f7'
 
-  url "https://download.cryptzone.com/files/download/AppGate-#{version.major_minor_patch}/clients/AppGate-#{version.major_minor_patch}-Installer.dmg"
+  url "https://download.cryptzone.com/files/download/AppGate-#{version}/clients/AppGate-#{version}-Installer.dmg"
   name 'AppGate SDP Client for macOS'
   homepage 'https://www.cryptzone.com/downloadcenter/appgate'
 
@@ -21,12 +21,14 @@ cask 'appgate-sdp-client' do
             pkgutil:   'com.appgate.pkg.appgatetun.component'
 
   zap delete: [
+                '~/Library/Caches/com.cryptzone.appgate.xdp.uninstall',
+                '~/Library/Saved Application State/com.electron.appgate.savedState',
+              ],
+      trash:  [
+                '~/Library/Application Support/appgate-ui',
+                '~/Library/Application Support/appgatexdp-ui',
                 '~/Library/Preferences/com.cryptzone.appgate.xdp*',
                 '~/Library/Preferences/com.electron.appgate.helper.plist',
                 '~/Library/Preferences/com.electron.appgate.plist',
-                '~/Library/Application\ Support/appgate-ui',
-                '~/Library/Application\ Support/appgatexdp-ui',
-                '~/Library/Saved\ Application\ State/com.electron.appgate.savedState',
-                '~/Library/Caches/com.cryptzone.appgate.xdp.uninstall',
               ]
 end
