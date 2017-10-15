2f8cd58551d62149762eb4ba7d45e610205cbfd9
Vítor Galvão <info@vitorgalvao.com>
2017-10-15 15:36:59 +0100

    Update playmemories-home.rb

--- Casks/playmemories-home.rb
+++ Casks/playmemories-home.rb
@@ -1,9 +1,9 @@
 cask 'playmemories-home' do
-  version '3.5.00'
+  version '3.5.00,6XZpdMN1Ie'
   sha256 '3b3ab315fd203c64b07d00b82d8a6ae46a677ad3e604e955a42c4ce209d0ca93'
 
   # pmb.update.sony.net/PMH was verified as official when first introduced to the cask
-  url "http://pmb.update.sony.net/PMH/6XZpdMN1Ie/PMHOME_#{version.no_dots}DL.dmg"
+  url "http://pmb.update.sony.net/PMH/#{version.after_comma}/PMHOME_#{version.before_comma.no_dots}DL.dmg"
   name 'PlayMemories Home'
   homepage 'https://support.d-imaging.sony.co.jp/www/disoft/int/download/playmemories-home/mac/en/'
 
