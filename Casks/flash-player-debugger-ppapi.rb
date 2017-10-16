647bd1815a407ff458dea17e189347a153909fbf
commitay <commitay@users.noreply.github.com>
2017-10-16 17:35:25 +1000

    Update flash-player-debugger-ppapi to 27.0.0.170 (#39682)

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -1,11 +1,11 @@
 cask 'flash-player-debugger-ppapi' do
-  version '27.0.0.159'
-  sha256 '61f288b6ca875824074668da7726ce1cda6325919663e8bf9a77e97d41d58f81'
+  version '27.0.0.170'
+  sha256 'f474e254503e25c29ecf32e8afc15eefb6b54a4c757af840e989d9e37b4ab8b4'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
-          checkpoint: 'f9cdc3e74103ce8f2bf46664475ec4ab55be96fe3bd71cc5b9a31d052ecb9eca'
+          checkpoint: 'dc42e381042fe881263b4462e00b306254e6defcc1faa54978364e2c8c14d182'
   name 'Adobe Flash Player PPAPI (plugin for Opera and chromium) content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
