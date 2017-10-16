0785cf9aeed251fb3ffa94da9fe27cbf798fa9b0
commitay <commitay@users.noreply.github.com>
2017-10-16 18:28:09 +1000

    Update unity-ios-support-for-editor to 2017.2.0f3,46dda1414e51

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-ios-support-for-editor' do
-  version '2017.1.1f1,5d30cf096e79'
-  sha256 'ff97af7d7b00b06c9e932ad372d96a0e7f0f36f16ebb053bae4ae1f819b6d4b4'
+  version '2017.2.0f3,46dda1414e51'
+  sha256 'c3e2c8601e3624e23d897cc969e6c865d605450209ce047bd6cc75b5250e7170'
 
   url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity iOS Build Support'
