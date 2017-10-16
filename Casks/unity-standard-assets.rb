8f00235e42296e84e820fcf5b7af623fab00cc47
commitay <commitay@users.noreply.github.com>
2017-10-16 18:02:41 +1000

    Update unity-standard-assets to 2017.2.0f3,46dda1414e51

--- Casks/unity-standard-assets.rb
+++ Casks/unity-standard-assets.rb
@@ -1,6 +1,6 @@
 cask 'unity-standard-assets' do
-  version '2017.1.1f1,5d30cf096e79'
-  sha256 '988c57c6208930b69e5832e632bda6d11b04e66b8f64cc3bcb44b0283c800da2'
+  version '2017.2.0f3,46dda1414e51'
+  sha256 'cc4db99b7ccac84040e53478045cba3d8272f27528a927e0e563f7ee0395e0d6'
 
   url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacStandardAssetsInstaller/StandardAssets-#{version.before_comma}.pkg"
   name 'Unity Standard Assets'
