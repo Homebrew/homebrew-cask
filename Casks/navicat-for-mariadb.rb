57537fda86ae313f4011173fa580e0bd35062a57
commitay <commitay@users.noreply.github.com>
2017-10-16 17:46:48 +1000

    Update navicat-for-mariadb to 12.0.15 (#39685)
    * Update navicat-for-mariadb to 12.0.15
    
    * Update navicat-for-mariadb.rb

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,10 +1,10 @@
 cask 'navicat-for-mariadb' do
-  version '12.0.14'
-  sha256 'eecaaf1656728f3ffd21b4b878e5fef25b2d995cacce22ebf2bb10d40d9d3a7b'
+  version '12.0.15'
+  sha256 'a00c942f61d5cd9f4d54c784108e4d80e7ce7eb1d394263847aa2c592c30fbb6'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
-  appcast 'https://www.navicat.com/products/navicat-for-mariadb-release-note#M',
-          checkpoint: '733ff7549bb72b001505d33a916b7fe4dce7a001cffc4a7a13fdee3c9ef59f12'
+  appcast 'https://www.navicat.com/products/navicat-for-mariadb-release-note',
+          checkpoint: '1ae400b2e257c6aeb2cd35489015bfc2380acc9b223588d552ab1c8d04dff8df'
   name 'Navicat for MariaDB'
   homepage 'https://www.navicat.com/products/navicat-for-mariadb'
 
