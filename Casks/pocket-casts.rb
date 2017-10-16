da3cb910a7206e8421c22f291e6488a204a611db
Bez Hermoso <bezalelhermoso@gmail.com>
2017-10-16 09:42:12 -0700

    Add the official Pocket Casts.app 0.9.3

---
+++ Casks/pocket-casts.rb
@@ -0,0 +1,10 @@
+cask 'pocket-casts' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://static.pocketcasts.com/mac/PocketCasts.zip'
+  name 'Pocket Casts'
+  homepage 'https://play.pocketcasts.com/'
+
+  app 'Pocket Casts.app'
+end
