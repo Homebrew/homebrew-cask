48efab32d832c77347b5e5c23727e35a049dc422
Beat Rupp <beatrupp@posteo.net>
2017-10-16 18:24:28 +0200

    Update riot to 0.12.7

--- Casks/riot.rb
+++ Casks/riot.rb
@@ -1,10 +1,10 @@
 cask 'riot' do
-  version '0.12.4'
-  sha256 '4353d9c343574960404734de56f331a3a73d1e237eaeb88296acde2553635bc7'
+  version '0.12.7'
+  sha256 '905e165d94efd2eae0803f0e7fa972d88c43aa6a95cfd8f9e41d7b0e2d202003'
 
   url "https://riot.im/download/desktop/install/macos/Riot-#{version}.dmg"
   appcast 'https://github.com/vector-im/riot-web/releases.atom',
-          checkpoint: 'e26202132045f455c4f059952c32e8acb567d6d76d26cdae606733bf0c408067'
+          checkpoint: '98b00a9a54ef0854ae165d3c597a407eccbaa2f36da3dc42b5c3f363c6842b4c'
   name 'Riot'
   homepage 'https://about.riot.im/'
 
