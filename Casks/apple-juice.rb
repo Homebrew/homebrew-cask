fa6cff87cbb747be9e16f8ed85e289e224fa15f1
jelflein <jelflein@mail.com>
2017-10-17 05:42:03 +0200

    Add Checkpoint

--- Casks/apple-juice.rb
+++ Casks/apple-juice.rb
@@ -3,6 +3,8 @@ cask 'apple-juice' do
   sha256 'a30c9d271bcda8d48c670dbf7e98c2a2c955152c413caea0868889d56b15e258'
 
   url "https://github.com/raphaelhanneken/apple-juice/releases/download/#{version}/Apple.Juice.dmg"
+  appcast 'https://github.com/raphaelhanneken/apple-juice/releases.atom',
+        checkpoint: '8eab87ff2f3bc47e3259d7b0f5f2da4bc8ae6c25fd71edd49212544ed01baba9'
   name 'Apple Juice'
   homepage 'https://github.com/raphaelhanneken/apple-juice'
 
