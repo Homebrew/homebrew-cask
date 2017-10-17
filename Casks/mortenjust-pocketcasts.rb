d8dac0767790966e9f48ef6df347f6d119b0890a
Bez Hermoso <bezhermoso@users.noreply.github.com>
2017-10-16 19:38:15 -0700

    Add the official Pocket Casts.app (#39706)
    * Add the official Pocket Casts.app 0.9.3
    
    * Rename unofficial pocketcasts => mortenjust-pocketcasts
    
    The appcast checkpoint was also re-calculated & updated
    
    * Update pocket-casts.rb

--- Casks/mortenjust-pocketcasts.rb
+++ Casks/mortenjust-pocketcasts.rb
@@ -1,10 +1,10 @@
-cask 'pocketcasts' do
+cask 'mortenjust-pocketcasts' do
   version '1.30'
   sha256 '9fee8fec0b5c5c3205a24c6b127064077dd6792ddf5d811b4df2dc84a34020da'
 
   url "https://github.com/mortenjust/PocketCastsOSX/releases/download/#{version}/PocketCasts.zip"
   appcast 'https://github.com/mortenjust/PocketCastsOSX/releases.atom',
-          checkpoint: '503438969aa8948af77fdcf08d2ea57535301c837e4690413679f9c652015212'
+          checkpoint: '671af329c2c010938ca567636af9fd5e9fdd83ab9dcab3b46be9792524e6dfbf'
   name 'Pocket Casts for Mac'
   homepage 'https://github.com/mortenjust/PocketCastsOSX'
 
