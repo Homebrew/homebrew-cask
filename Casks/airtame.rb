2aeceeb3a1315a532bf2f36795d299ae47c98512
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2017-10-17 12:01:27 +0200

    Airtame 3.0.0

--- Casks/airtame.rb
+++ Casks/airtame.rb
@@ -1,6 +1,6 @@
 cask 'airtame' do
-  version '2.4.1'
-  sha256 '7e6bd34618b56cfc0f02588560df04f4882be074912d761ebf7e0264db870655'
+  version '3.0.0'
+  sha256 '3058c2139c8e7b9518c6d68e8cf35be09f029b7ca920ce6bf785abae2056fa35'
 
   url "https://downloads-cdn.airtame.com/application/ga/osx_x64/releases/airtame-application-#{version}.dmg"
   name 'Airtame'
