528f2b9a91a8d8d3ca644bbb9e3a8995fc13dd0d
kixswift <10491362+kixswift@users.noreply.github.com>
2017-10-17 11:47:03 +0900

    Update paintcode to 3.3.3 (#39722)

--- Casks/paintcode.rb
+++ Casks/paintcode.rb
@@ -1,11 +1,11 @@
 cask 'paintcode' do
-  version '3.3.1'
-  sha256 '3b736b8e29eed8573d285123ea3fe3f5d9dd258549e1c78d667233736651cba9'
+  version '3.3.3'
+  sha256 'd0ab676ed46949c544f07aa7a076d4995c8731a4114283aefe54878cf2378611'
 
   # pixelcut.com/paintcode was verified as official when first introduced to the cask
   url "https://www.pixelcut.com/paintcode#{version.major}/paintcode.zip"
   appcast "https://www.pixelcut.com/paintcode#{version.major}/appcast.xml",
-          checkpoint: '29d13ab38d6162342c8b3f3da11df15655632a706ad849bf901a92ff7441d891'
+          checkpoint: 'c263ea15e0614837f297bf3c753c2b15dddd5a7c3ed708b74c122f8e44f50a8f'
   name 'PaintCode'
   homepage 'https://www.paintcodeapp.com/'
 
