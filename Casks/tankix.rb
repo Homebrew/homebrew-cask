de16e0bde7b61e8285a15329dc86ff9cdb2c8ab6
giannitm <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-10-17 19:12:49 +0100

    Update tankix to 5590

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version '5430'
-  sha256 '79e7f20e8b9fb37c68107d7bb55e9fc8ddfcfb34b67f35a55033c3620da99c8a'
+  version '5590'
+  sha256 'b40451c98e3953820f4ca6bfc9fc97865b82f930825fd508bdb4633fe6ce7a07'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/master-#{version}/TankiX.dmg"
   name 'Tanki X'
