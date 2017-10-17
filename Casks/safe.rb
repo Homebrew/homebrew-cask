8136820440462e9c0e3402601e8df8c80b95cf7f
Vítor Galvão <info@vitorgalvao.com>
2017-10-17 01:54:14 +0100

    Update and rename safe-launcher.rb to safe.rb (#39709)
    * Update and rename safe-launcher.rb to safe.rb
    
    * checkpoint

--- Casks/safe.rb
+++ Casks/safe.rb
@@ -1,11 +1,11 @@
-cask 'safe-launcher' do
+cask 'safe' do
   version '0.11.0'
   sha256 '4dd918574e835e32aaeb4a0fc23e2572df8641a432e82e201dd0c0b0c47fba3d'
 
   # github.com/maidsafe/safe_launcher was verified as official when first introduced to the cask
   url "https://github.com/maidsafe/safe_launcher/releases/download/#{version}/safe-launcher-v#{version}-osx-x64.zip"
   appcast 'https://github.com/maidsafe/safe_launcher/releases.atom',
-          checkpoint: '48376dbfa4b984bb2d6cc68d443be9147d1c440664899d8f914d3c6634c9e3dc'
+          checkpoint: '69c99e3c024e5d7c077bf93f684179e54b25ddaf9f9fb020085784f7c3c359e0'
   name 'SAFE Launcher'
   homepage 'https://www.maidsafe.net/'
 
