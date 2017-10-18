4035d0063a6807f41281377a9398f2523bae701d
commitay <commitay@users.noreply.github.com>
2017-10-18 13:10:53 +1000

    Update openra.rb

--- Casks/openra.rb
+++ Casks/openra.rb
@@ -1,9 +1,9 @@
 cask 'openra' do
   version '20171014'
-  sha256 '172a11826c5806c6ce698fc8c13f65c26d168148a6872887754918e8b8b97eff'
+  sha256 'f6896f9f18c215ca801a69649fc9f46037a9492b67b03bd04f40fb4c4f408cf2'
 
   # github.com/OpenRA/OpenRA was verified as official when first introduced to the cask
-  url "https://github.com/OpenRA/OpenRA/archive/release-#{version}.zip"
+  url "https://github.com/OpenRA/OpenRA/releases/download/release-#{version}/OpenRA-release-#{version}.dmg"
   appcast 'https://github.com/OpenRA/OpenRA/releases.atom',
           checkpoint: '375c84af42e7d70986d9e81a0556944fbbc9fbc3cc9f8ba5605f67522b2a24b4'
   name 'OpenRA'
