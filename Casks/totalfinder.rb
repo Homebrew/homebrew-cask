94d0e7874b97c87e28981612233b72d505a7234f
Oran Shuster <thristle666@gmail.com>
2017-10-18 22:51:04 +0300

    Update totalfinder to version 1.10.2 (#39772)

--- Casks/totalfinder.rb
+++ Casks/totalfinder.rb
@@ -1,6 +1,11 @@
 cask 'totalfinder' do
-  version '1.9.3'
-  sha256 'cde59340d0bfcbca36208a1b0ea0d811cf54355b42220214586477514039b2e2'
+  if MacOS.version <= :yosemite
+    version '1.9.3'
+    sha256 'cde59340d0bfcbca36208a1b0ea0d811cf54355b42220214586477514039b2e2'
+  else
+    version '1.10.2'
+    sha256 'c1809a245dd899cb51c54f0bc5b93492eb9611451ba5df9ce0c1bf22544ecf87'
+  end
 
   url "http://downloads.binaryage.com/TotalFinder-#{version}.dmg"
   name 'TotalFinder'
