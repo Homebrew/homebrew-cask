7f0c3c7c2ee65ed1113b570ac4f6f533e0a61b28
Oran Shuster <thristle666@gmail.com>
2017-10-18 14:13:11 +0300

    totalfinder.rb fix format using cask style

--- Casks/totalfinder.rb
+++ Casks/totalfinder.rb
@@ -1,7 +1,7 @@
 cask 'totalfinder' do
   if MacOS.version <= :yosemite
-  	version '1.9.3'
-  	sha256 'cde59340d0bfcbca36208a1b0ea0d811cf54355b42220214586477514039b2e2'
+    version '1.9.3'
+    sha256 'cde59340d0bfcbca36208a1b0ea0d811cf54355b42220214586477514039b2e2'
   else
     version '1.10.2'
     sha256 'c1809a245dd899cb51c54f0bc5b93492eb9611451ba5df9ce0c1bf22544ecf87'
