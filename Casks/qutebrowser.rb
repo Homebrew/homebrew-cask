f52174a04d87ce07f6e4aa6f8c3cd834d9cbf78a
Anton Platonov <platosha@gmail.com>
2017-10-19 14:17:29 +0300

    Update qutebrowser to 1.0.2 (#39801)

--- Casks/qutebrowser.rb
+++ Casks/qutebrowser.rb
@@ -1,11 +1,11 @@
 cask 'qutebrowser' do
-  version '1.0.1'
-  sha256 '6f012524d1d66d1201a024b4971dca5fe8f93693617670dba081b0e467a2f779'
+  version '1.0.2'
+  sha256 'f24491e725d089c66a870c321c89f6b228bc8fa0d30adc885d0b2d9a387dab87'
 
   # github.com/qutebrowser/qutebrowser was verified as official when first introduced to the cask
   url "https://github.com/qutebrowser/qutebrowser/releases/download/v#{version}/qutebrowser-#{version}.dmg"
   appcast 'https://github.com/qutebrowser/qutebrowser/releases.atom',
-          checkpoint: '750270f95b68b1b5f699d03250d7eea3be544ecf21686d0526734465aa4e8c87'
+          checkpoint: '84277963356ff7826c4a03cf358ae22231b1cbcd995beba14814a13876b34808'
   name 'qutebrowser'
   homepage 'https://www.qutebrowser.org/'
 
