a6ac4b0370587962822b46a43bb051cfcabd4c6b
Victor Popkov <victor@popkov.me>
2017-10-19 20:30:26 +0300

    Update byteball to 1.11.0

--- Casks/byteball.rb
+++ Casks/byteball.rb
@@ -1,11 +1,11 @@
 cask 'byteball' do
-  version '1.10.1'
-  sha256 'dfa748bfc055133b2da9d8c5d66294b2c7d0983398e0b38a719a9610763ef947'
+  version '1.11.0'
+  sha256 'bd8eabaab338b24c9bebcfbd6d51e3de82d63d2d7800282f5e4020747e37ea1b'
 
   # github.com/byteball/byteball was verified as official when first introduced to the cask
   url "https://github.com/byteball/byteball/releases/download/v#{version}/Byteball-osx64.dmg"
   appcast 'https://github.com/byteball/byteball/releases.atom',
-          checkpoint: '94fb6f10ab5d85357cad4af114143e92e22ea60b2f0a0c29b19d5c0d7464b6a3'
+          checkpoint: '2e7bcf4e94856a0347deaaab99ba1ee7a59cd66a064b6a6ad6de1021ce7f64d4'
   name 'Byteball'
   homepage 'https://byteball.org/'
 
