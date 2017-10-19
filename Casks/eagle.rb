f864bccf50cebffffdf1808116094ab9423b9163
commitay <commitay@users.noreply.github.com>
2017-10-20 08:58:58 +1000

    Update eagle.rb

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -3,7 +3,7 @@ cask 'eagle' do
   sha256 '53c7c6a76f5b7addce53fba5f17d236dc93eb8122ffeff871977b24e42b282e3'
 
   url "http://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
-  name 'CadSoft EAGLE'
+  name 'Autodesk EAGLE'
   homepage 'https://www.autodesk.com/products/eagle/overview'
 
   pkg "Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
