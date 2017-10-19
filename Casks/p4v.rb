7d579c3b804075563d2a3b2e45e71734ad59f9aa
commitay <commitay@users.noreply.github.com>
2017-10-19 10:31:52 +1000

    Update p4v: appcast / zap (#39780)

--- Casks/p4v.rb
+++ Casks/p4v.rb
@@ -3,6 +3,8 @@ cask 'p4v' do
   sha256 'b24a68eaac73003c252ccfc235d8191c58dc3a0aa1abe035081ed8b76f834dd3'
 
   url "http://cdist2.perforce.com/perforce/r#{version.sub(%r{\A20(\d\d\.\d+).*}, '\1')}/bin.macosx1011x86_64/P4V.dmg"
+  appcast "http://filehost.perforce.com/perforce/r#{version.sub(%r{\A20(\d\d\.\d+).*}, '\1')}/bin.macosx1011x86_64/SHA256SUMS",
+          checkpoint: 'bcb6d54ab29c9f90b64518a85ecb7fa4dcdcedda4f80bc1fbb280bacf203525c'
   name 'Perforce Visual Client'
   name 'P4V'
   homepage 'https://www.perforce.com/products/helix-core-apps/helix-visual-client-p4v'
@@ -12,9 +14,9 @@ cask 'p4v' do
   app 'p4merge.app'
   binary 'p4vc'
 
-  zap delete: [
+  zap delete: '~/Library/Saved Application State/com.perforce.p4v.savedState',
+      trash:  [
                 '~/Library/Preferences/com.perforce.p4v',
                 '~/Library/Preferences/com.perforce.p4v.plist',
-                '~/Library/Saved Application State/com.perforce.p4v.savedState',
               ]
 end
