8dd5bda354469a028f5c7fd429847811751e4f53
Frederik Kunze <frederikkunze@gmail.com>
2017-10-19 13:29:50 +0200

    Add X-Air-Edit.app v1.5

---
+++ Casks/x-air-edit.rb
@@ -0,0 +1,10 @@
+cask 'x-air-edit' do
+  version '1.5'
+  sha256 '4db3fb2204829384f59aae00b19f49e14f0151c9eb113776f0d455350868751b'
+
+  url 'https://downloads.music-group.com/software/behringer/XAIR/X-AIR-Edit_MAC_V1.5.zip'
+  name 'X-Air-Edit'
+  homepage 'https://www.music-group.com/brand/c/Behringer/downloads?active=Downloads'
+
+  app 'X-Air-Edit.app'
+end
