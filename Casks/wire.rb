595898d64ba58e15ad63766f1a073d4328687f4b
commitay <commitay@users.noreply.github.com>
2017-10-19 19:12:01 +1000

    Update wire to 3.1.2822

--- Casks/wire.rb
+++ Casks/wire.rb
@@ -1,11 +1,11 @@
 cask 'wire' do
-  version '3.0.2814'
-  sha256 'f4a000ce3a0bf73deef0f480a7219ca08832504ad08dd65bede3fcab0da1e386'
+  version '3.1.2822'
+  sha256 '969feb5757b956583d6da33445b840e51fa616924208eb941cb6e1610e0d90c4'
 
   # github.com/wireapp/wire-desktop was verified as official when first introduced to the cask
   url "https://github.com/wireapp/wire-desktop/releases/download/release%2F#{version}/wire-#{version}.pkg"
   appcast 'https://github.com/wireapp/wire-desktop/releases.atom',
-          checkpoint: 'a9da6b1ca1c36852f49529338ac583d2da3b5ee090bc0577fc5e0da42b3222d2'
+          checkpoint: 'ba06d3cc4687d5c1c2229eb2c5b14dd485e51a338ec267636553ce304581be6a'
   name 'Wire'
   homepage 'https://wire.com/'
 
