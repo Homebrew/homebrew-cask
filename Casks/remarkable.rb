6c3d8182ff087d4f01e125af5f1350865bbe24c5
Vítor Galvão <info@vitorgalvao.com>
2017-10-19 14:42:18 +0100

    Update remarkable.rb

--- Casks/remarkable.rb
+++ Casks/remarkable.rb
@@ -2,8 +2,7 @@ cask 'remarkable' do
   version '1.0.0'
   sha256 '5135c6878ebc1d1816aac4bc822b7cd0da3bb05c14167c201aefe20def1e05cf'
 
-  # storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://storage.googleapis.com/remarkable-client-software-540221a9321a8a52ec61a06bfab53423/desktop/production/mac/remarkable-1.0.0.dmg'
+  url "http://technology.remarkable.com/remarkable-#{version}.dmg"
   name 'reMarkable'
   homepage 'https://remarkable.com/'
 
