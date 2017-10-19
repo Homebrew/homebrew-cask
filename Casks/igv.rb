d8513d2cfe536d23b9335774255614f0a565c8d7
commitay <commitay@users.noreply.github.com>
2017-10-19 16:18:07 +1000

    Update igv.rb

--- Casks/igv.rb
+++ Casks/igv.rb
@@ -2,7 +2,7 @@ cask 'igv' do
   version '2.4.2'
   sha256 '6d1939b0228d46614cf0cdbad692a208f5e3475542c9fb238fdb1d8e4dde89b4'
 
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.4/IGV_#{version}.app.zip"
+  url "https://data.broadinstitute.org/igv/projects/downloads/#{version.major_minor}/IGV_#{version}.app.zip"
   name 'Integrative Genomics Viewer (IGV)'
   homepage 'https://software.broadinstitute.org/software/igv/'
 
