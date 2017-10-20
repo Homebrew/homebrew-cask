8c69abf4cd34aa759d6e90c196706c033c148a48
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-21 07:42:35 +0800

    Update rest-time.rb

--- Casks/rest-time.rb
+++ Casks/rest-time.rb
@@ -6,7 +6,7 @@ cask 'rest-time' do
   appcast 'http://www.publicspace.net/app/resttime.xml',
           checkpoint: 'ff19a758a930721474ef4120e27cf4b8755443c6ea803ff48f3aa404860d8323'
   name 'Rest Time'
-  homepage 'http://www.publicspace.net/RestTime/index.html'
+  homepage 'http://www.publicspace.net/RestTime/'
 
   auto_updates true
 
