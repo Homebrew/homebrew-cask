1c64e0f9065640b3b2f7267c9772eeeeca86a776
ericpardee <github@ericpardee.com>
2017-10-19 18:42:51 -0700

    Add appcast

--- Casks/tusk.rb
+++ Casks/tusk.rb
@@ -3,6 +3,8 @@ cask 'tusk' do
   sha256 '005329655d506f3563e6ecf49174fa15bbf649a5219cb89e7ce13fe54baa37bc'
 
   url "https://github.com/klauscfhq/tusk/releases/download/v#{version}/tusk-macos-#{version}.dmg"
+  appcast 'https://github.com/klauscfhq/tusk/releases.atom',
+          checkpoint: '5ac7ede64bda3686a38ae57f3d306874058f290288e224aa21be74f64bf2174b'
   name 'Tusk'
   homepage 'https://github.com/klauscfhq/tusk'
 
