b19e87b42b9e756c1af140e9929e3b848f9720e3
ShallmentMo <ShallmentMo@gmail.com>
2017-10-20 15:51:39 +0800

    Update wechatwebdevtools to 1.0.1,171019

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,6 +1,6 @@
 cask 'wechatwebdevtools' do
-  version '1.0.1,170913'
-  sha256 '9bb46d15ec1ae4014f434ebdb39b7e67fd31dc0af5e0ed72aaf5bc479daded06'
+  version '1.0.1,171019'
+  sha256 '80554a42badb14ccc7c66cb5ecca5e59f680eddf35105b3700b30d81dde2bcf5'
 
   url "https://dldir1.qq.com/WechatWebDev/1.0.0/20#{version.after_comma}/wechat_devtools_#{version.major_minor}#{version.patch}.#{version.after_comma}.dmg"
   name 'wechat web devtools'
