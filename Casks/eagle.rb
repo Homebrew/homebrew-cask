8a69c0d275de9a216f83b58e03c1a123e01baefa
Flo M <flow@frcy.org>
2017-10-20 05:10:01 +0200

    Update eagle to 8.4.0 (#39823)
    * Update eagle to 8.4.0
    
    * Update eagle.rb

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -1,9 +1,9 @@
 cask 'eagle' do
-  version '8.3.2'
-  sha256 '159ab335877abf6336d7142a4a97d0f15ad7e27b226ac783c39a35baf0828749'
+  version '8.4.0'
+  sha256 '53c7c6a76f5b7addce53fba5f17d236dc93eb8122ffeff871977b24e42b282e3'
 
   url "http://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
-  name 'CadSoft EAGLE'
+  name 'Autodesk EAGLE'
   homepage 'https://www.autodesk.com/products/eagle/overview'
 
   pkg "Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
