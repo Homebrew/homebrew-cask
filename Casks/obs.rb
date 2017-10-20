54a40a367ed2bfeb062011c16d34ba816730405a
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-10-20 14:07:14 +0900

    Update obs to 20.1.0 (#39830)
    * Update obs to 20.1.0
    
    * Update obs.rb

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -1,15 +1,15 @@
 cask 'obs' do
-  version '20.0.1'
-  sha256 '2cbdea6dd2eaf198d33707644c1e0e073307ded11434a4f423950ecd675dfa61'
+  version '20.1.0'
+  sha256 '8eb490f749b3717002fd55b0ee14e082191cc921c5913070890e95af8776445d'
 
   # github.com/jp9000/obs-studio was verified as official when first introduced to the cask
-  url "https://github.com/jp9000/obs-studio/releases/download/#{version}/obs-mac-#{version}-installer.pkg"
+  url "https://github.com/jp9000/obs-studio/releases/download/#{version}/obs-mac-#{version.major_minor}-installer.pkg"
   appcast 'https://github.com/jp9000/obs-studio/releases.atom',
-          checkpoint: '49f5329e9e99e97e804364395f599a3499f4368fcb03011c80f059f6a82c6db8'
+          checkpoint: 'bce365214c203a68f6978094931f9748ddac61be3b8de71adbb55a6cdf0f584e'
   name 'OBS'
   homepage 'https://obsproject.com/'
 
-  pkg "obs-mac-#{version}-installer.pkg"
+  pkg "obs-mac-#{version.major_minor}-installer.pkg"
 
   uninstall pkgutil: [
                        'org.obsproject.pkg.obs-studio',
