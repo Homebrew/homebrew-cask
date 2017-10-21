bb796df0d2751b3aef305905b59d886711b36eac
commitay <commitay@users.noreply.github.com>
2017-10-22 09:04:05 +1000

    Rename toggldesktop to toggl (#39900)

--- Casks/toggl.rb
+++ Casks/toggl.rb
@@ -1,4 +1,4 @@
-cask 'toggldesktop' do
+cask 'toggl' do
   version '7.4.68'
   sha256 '02ca17e1d4655a9076173eab8b04b64d768c8cc9ed5fa9e771ee1eea044b3f92'
 
@@ -8,6 +8,11 @@ cask 'toggldesktop' do
           checkpoint: '4b7a1222c778d10db0cf1aedd6d27249fc7df625663228fc28207a2510b50dd8'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com/'
+
+  conflicts_with cask: [
+                         'toggl-beta',
+                         'toggl-dev',
+                       ]
 
   app 'TogglDesktop.app'
 end
