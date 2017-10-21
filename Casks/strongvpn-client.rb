75e3d8eab2dae975e1d9e1742e2e6d80b1493ec0
Vítor Galvão <info@vitorgalvao.com>
2017-10-21 23:44:57 +0100

    Update strongvpn-client.rb

--- Casks/strongvpn-client.rb
+++ Casks/strongvpn-client.rb
@@ -1,9 +1,11 @@
 cask 'strongvpn-client' do
-  version :latest
-  sha256 :no_check
+  version '1.2.0'
+  sha256 '4b50f4f330394f141ffe9417eccdb86eaef9a595c2e5da238d7fee17b818fdb2'
 
   # mirror2.reliablehosting.com/mac was verified as official when first introduced to the cask
   url "https://mirror2.reliablehosting.com/mac/StrongVPN_Mac-latest.dmg"
+  appcast 'https://colomovers.com/mac.xml',
+          checkpoint: '734a1bf789034d192b42a82bb470a4eebb9d1800bb646fa9178583fb632b269b'
   name 'StrongVPN'
   homepage 'https://strongvpn.com/setup.html/'
 
