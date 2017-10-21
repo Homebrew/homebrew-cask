d2caec27ce45af5dd8b6832c84527f6c08bf86d8
commitay <commitay@users.noreply.github.com>
2017-10-22 08:57:13 +1000

    Rename authy-desktop to authy (#39898)

--- Casks/authy.rb
+++ Casks/authy.rb
@@ -1,4 +1,4 @@
-cask 'authy-desktop' do
+cask 'authy' do
   version '1.1.0'
   sha256 '672580bcc520514ad68e54fb9b43956a84cf429407e58d66033056b2e797cc63'
 
@@ -12,10 +12,12 @@ cask 'authy-desktop' do
   app 'Authy Desktop.app'
 
   zap delete: [
-                '~/Library/Application Support/Authy Desktop',
                 '~/Library/Caches/com.authy.authy-mac',
                 '~/Library/Caches/com.authy.authy-mac.ShipIt',
                 '~/Library/Cookies/com.authy.authy-mac.binarycookies',
+              ],
+      trash:  [
+                '~/Library/Application Support/Authy Desktop',
                 '~/Library/Preferences/com.authy.authy-mac.helper.plist',
                 '~/Library/Preferences/com.authy.authy-mac.plist',
               ]
