afebc463175875056f669037bb6ab41f1b136f71
commitay <commitay@users.noreply.github.com>
2017-10-23 05:49:38 +1000

    Update cakebrew to 1.2.5

--- Casks/cakebrew.rb
+++ Casks/cakebrew.rb
@@ -1,11 +1,11 @@
 cask 'cakebrew' do
-  version '1.2.4'
-  sha256 '00a400b2fed5f103ae705729c0ede1616e5819a758b50d5583557832ae282c0b'
+  version '1.2.5'
+  sha256 '2dc287acf04952a2b449a0d69cf1581915ef1e6c59d2ec1c83c3e37c263826ca'
 
   # cakebrew-377a.kxcdn.com was verified as official when first introduced to the cask
   url "https://cakebrew-377a.kxcdn.com/cakebrew-#{version}.dmg"
   appcast 'https://www.cakebrew.com/appcast/profileInfo.php',
-          checkpoint: '461890a01e3c15163fdc3186dedbfc0234b8f80d18ee5595e72c74eabd27f814'
+          checkpoint: '425c0d51a81dda9dc7747fb6deb77d8569d241f35cfe37221fabede63cba2aa4'
   name 'Cakebrew'
   homepage 'https://www.cakebrew.com/'
 
