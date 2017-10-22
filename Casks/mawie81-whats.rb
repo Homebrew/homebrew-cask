750e311162131a8f7e07290e2b8e61875a5e2998
commitay <commitay@users.noreply.github.com>
2017-10-23 07:15:43 +1000

    Rename whatsdesktop to mawie81-whats (#39914)

--- Casks/mawie81-whats.rb
+++ Casks/mawie81-whats.rb
@@ -1,10 +1,10 @@
-cask 'whatsdesktop' do
+cask 'mawie81-whats' do
   version '1.8.0'
   sha256 '9f68154a382dc4306b440a254662a3efd65b9e4bbd8cfbc76b625e70e9afcbc2'
 
   url "https://github.com/mawie81/whatsdesktop/releases/download/#{version}/WhatsDesktop-#{version}.dmg"
   appcast 'https://github.com/mawie81/whatsdesktop/releases.atom',
-          checkpoint: '1137e7be0e05431fd9cbff1fccd03a73fb898c6b9c4930cff7e82641b593b6ac'
+          checkpoint: '45370175f2ba07f9a035cee526dabc9960c1c9e00b23586302fb62494084a113'
   name 'WhatsDesktop'
   homepage 'https://github.com/mawie81/whatsdesktop'
 
