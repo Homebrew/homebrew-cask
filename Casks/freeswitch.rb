53efe1b76f4bb2bd9296bc236962f5205d050adc
commitay <commitay@users.noreply.github.com>
2017-10-22 10:39:47 +1000

    freeswitch

--- Casks/freeswitch.rb
+++ Casks/freeswitch.rb
@@ -1,10 +1,10 @@
-cask 'freeswitch-desktop' do
+cask 'freeswitch' do
   version '1.4.1'
   sha256 '3b6f010c94aea8abc2714ddc8f3244cfb4bab41496569730f8196b76b1bc226a'
 
   url "https://github.com/pablote/fs-mgmt/releases/download/v#{version}/freeswitch-desktop-osx64.zip"
   appcast 'https://github.com/pablote/fs-mgmt/releases.atom',
-          checkpoint: '99d42afd8ca85b6bcce51c8754ab061d7738447db36caf0a1c867fb520ea974e'
+          checkpoint: '9410487c6a486c32037493292b3fc8a806edf30faf184974540fc0a81c9366a0'
   name 'Freeswitch Desktop'
   homepage 'https://github.com/pablote/fs-mgmt'
 
