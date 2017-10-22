37a2074f342e060a172b02e43f4bb4e55a0606e5
Anver Housseini <contact@anver.ch>
2017-10-22 20:32:33 +0200

    Add AutoPkgr.app v1.4.2

---
+++ Casks/autopkgr.rb
@@ -0,0 +1,11 @@
+cask 'autopkgr' do
+  version '1.4.2'
+  sha256 '3f73bae06c0e61fe6af41ead9455ade181e6b62fec4a85c7235c0834c50546a5'
+
+  # github.com/lindegroup was verified as official when first introduced to the cask
+  url "https://github.com/lindegroup/autopkgr/releases/download/v#{version}/AutoPkgr-#{version}.dmg"
+  name 'AutoPkgr'
+  homepage 'https://www.lindegroup.com/autopkgr'
+
+  app 'AutoPkgr.app'
+end
