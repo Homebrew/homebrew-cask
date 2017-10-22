89afd0a42381fcc969eb1cecbaeffe7f1cda27b0
Brian Rutledge <bhrutledge@gmail.com>
2017-10-22 07:47:28 -0400

    Update sonic-pi to 3.0.1

--- Casks/sonic-pi.rb
+++ Casks/sonic-pi.rb
@@ -1,10 +1,10 @@
 cask 'sonic-pi' do
-  version '3.0.0'
-  sha256 '824e822c797547067952a7579b2c881fc2b90b1435f9ab1c824bfc3564869310'
+  version '3.0.1'
+  sha256 '6c8b789a8e226da6d1fe0a24110ef6f7cd2971e67cfc3339735bbccfc566ec51'
 
   url "http://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-v#{version.major}.dmg"
   appcast 'https://github.com/samaaron/sonic-pi/releases.atom',
-          checkpoint: '79311a5aa2256aec0bfa80f12338eace4daca987844fa9853c6a6e7de95c6a5d'
+          checkpoint: '8a9296c1835b8f4211157e08c06547c95513436615a5980a2832de4e23422234'
   name 'Sonic Pi'
   homepage 'http://sonic-pi.net/'
 
