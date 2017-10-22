1dbd0acedd381cc1537022e9c15de5a516d8363f
commitay <commitay@users.noreply.github.com>
2017-10-23 07:15:29 +1000

    Rename pb-for-desktop to sidneys-pb (#39912)

--- Casks/sidneys-pb.rb
+++ Casks/sidneys-pb.rb
@@ -1,10 +1,10 @@
-cask 'pb-for-desktop' do
+cask 'sidneys-pb' do
   version '5.9.6'
   sha256 'f0d5c0d899ffa770c8ee087fa778ef988cb8684fa1d30357a8fa00660cafbfe0'
 
   url "https://github.com/sidneys/pb-for-desktop/releases/download/v#{version}/pb-for-desktop-#{version}.dmg"
   appcast 'https://github.com/sidneys/pb-for-desktop/releases.atom',
-          checkpoint: 'bacf7728f06ef618f97199ade4a2528cf64715991a34898a339cec02bcb2d98a'
+          checkpoint: 'e9d94027091c0af029db5e19befdd5cdcac9f87e82b0d810efbad5b27113aafb'
   name 'PB for Desktop'
   homepage 'https://github.com/sidneys/pb-for-desktop'
 
