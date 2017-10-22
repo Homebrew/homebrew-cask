e88ee00b1d99fd4ef5b607c660d606629d043d51
commitay <commitay@users.noreply.github.com>
2017-10-22 22:00:54 +1000

    Update sonic-pi.rb

--- Casks/sonic-pi.rb
+++ Casks/sonic-pi.rb
@@ -1,6 +1,6 @@
 cask 'sonic-pi' do
   version '3.0.1'
-  sha256 ' d9eea57df86da3d39a2a068358fcc256e0dd0181a110fa2d83ad16ad153b10fd'
+  sha256 'd9eea57df86da3d39a2a068358fcc256e0dd0181a110fa2d83ad16ad153b10fd'
 
   url "http://sonic-pi.net/files/releases/v#{version}/Sonic-Pi-for-Mac-v#{version}.dmg"
   appcast 'https://github.com/samaaron/sonic-pi/releases.atom',
