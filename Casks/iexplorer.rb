40dd0a51085a7fee1ea5b5935bd0b7eab7034708
Maarc <marc@zottner.com>
2017-10-23 12:43:03 +0200

    Update iexplorer to 4.1.5

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,6 +1,6 @@
 cask 'iexplorer' do
-  version '4.1.4'
-  sha256 'e3cefd019406af7c787606840fb5146d688bb66b5df2688ed2759f6a0eba79e4'
+  version '4.1.5'
+  sha256 '8bea06b1c71b9ec7aeec7bebe3a1982cc6e9d8f2d18c6aafd910e1de98cdd47e'
 
   url "https://assets.macroplant.com/downloads/iExplorer-#{version}.dmg"
   name 'iExplorer'
