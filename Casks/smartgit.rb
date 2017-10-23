a6c5ab58fa4895ec9f56ee94943109cf55bd6b09
Jackson Law <178053+jlaw@users.noreply.github.com>
2017-10-23 10:27:43 -0700

    Update smartgit to 17.1.0

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,10 +1,10 @@
 cask 'smartgit' do
-  version '17.0.5'
-  sha256 '833b368e293d453bf2e8310538c04a2e8a0b74dbaa0c93ebf22333c4b41b83e9'
+  version '17.1.0'
+  sha256 '984096381e6d84ab69a6d3c254bff736a81e094a376757c8d41e2d2e04ce189b'
 
   url "https://www.syntevo.com/static/smart/download/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.syntevo.com/smartgit/changelog.txt',
-          checkpoint: '95de5bf73b4a15854ee29e7d6edce1010a3538d007c875e8dbf7937be13cc215'
+          checkpoint: 'bcf3bda18796f8569e84913593097f3b3ff3fb2539c57151368c8ad720028517'
   name 'SmartGit'
   homepage 'https://www.syntevo.com/smartgit/'
 
