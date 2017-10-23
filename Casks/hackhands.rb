c81a0c8a8657a49b21f80f0eff5868afdfe0b706
commitay <commitay@users.noreply.github.com>
2017-10-23 19:58:07 +1000

    url

--- Casks/hackhands.rb
+++ Casks/hackhands.rb
@@ -2,8 +2,8 @@ cask 'hackhands' do
   version '2.0.0'
   sha256 '4c428f65ee63dac8ed6f122d4830cef5c76d6abb033d7344eef90ecebb7c7b74'
 
-  # s.pluralsight.com was verified as official when first introduced to the cask
-  url 'https://s.pluralsight.com/mentors/desktop/HackHands-2.0.0.dmg'
+  # s.pluralsight.com/mentors/desktop/ was verified as official when first introduced to the cask
+  url "https://s.pluralsight.com/mentors/desktop/HackHands-#{version}.dmg"
   name 'HackHands'
   homepage 'https://hackhands.com/desktop/'
 
