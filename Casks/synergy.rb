169a3548a3e7905e6283bcf2f017a59e79b1e987
404NetworkError <fake@email.address>
2017-10-22 21:33:57 -0500

    Update synergy 1.8.8,25a8cb2 appcast

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -3,8 +3,8 @@ cask 'synergy' do
   sha256 'b09d8ffdb47ecde761e06e9beea672ad4b6999e09104fa9239ae30ae0f69b9c2'
 
   url "https://binaries.symless.com/v#{version.before_comma}/synergy-v#{version.before_comma}-stable-#{version.after_comma}-MacOSX-x86_64.dmg"
-  appcast 'https://github.com/symless/synergy/releases.atom',
-          checkpoint: '7805c24e0b75550d5803bef652d74804d796976096a7682821d166f938165a08'
+  appcast 'https://github.com/symless/synergy-core/releases.atom',
+          checkpoint: '627fed8c44fa46631f2041e8080615dbbb7fe1f4b28be9269464ecd41b30ef1f'
   name 'Synergy'
   homepage 'https://symless.com/synergy'
 
