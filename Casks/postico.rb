09858efdd7511e126f2d7534eb86b1ef17894fc9
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-10-23 16:58:59 +0200

    Update postico to 1.2.3 (#39968)

--- Casks/postico.rb
+++ Casks/postico.rb
@@ -1,6 +1,6 @@
 cask 'postico' do
-  version '1.2.2'
-  sha256 '6bae5e169ddf91af762be0ace4e8a9695c0b52ca5285c1cb330e287d29234404'
+  version '1.2.3'
+  sha256 'd8ce4f6a5bed2fb7df6a06d8b666b13df0c574854b52423d2293c07bcf1e8c11'
 
   # amazonaws.com/eggerapps-downloads was verified as official when first introduced to the cask
   url "https://s3-eu-west-1.amazonaws.com/eggerapps-downloads/postico-#{version}.zip"
