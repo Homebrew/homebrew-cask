e150e0a73ef7a40a12032a778236c27e611465c9
Mack Browne <mackbrowne@gmail.com>
2017-10-23 16:39:43 -0400

    Fix sha256 for version 3.6.0.0
    Not sure if this ever worked, or if the checksum changed for this Airdroid version.

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask 'airdroid' do
   version '3.6.0.0'
-  sha256 'fe773c633739bf8d512623828dc59f94a6c655fcee69e546ed1623ba34e5da87'
+  sha256 'a32117be6cd3a6fad5004fd32988d52c7ed69f88cb09196f957389afe6d1e901'
 
   # s3.amazonaws.com/dl.airdroid.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
