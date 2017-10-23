48750dd2e6ab71aa2656cb234995aeb096e94e5d
commitay <commitay@users.noreply.github.com>
2017-10-23 18:59:47 +1000

    Update hazel to 4.2.2 (#39958)

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -1,11 +1,11 @@
 cask 'hazel' do
-  version '4.2.1'
-  sha256 '8a35d92620bd311f3fb67edf540cb88a35dff96ef8919aa84dff8eef39f6f5fe'
+  version '4.2.2'
+  sha256 'aa5f4979b04d1edcf7c27a1ef20573c8dbae29f7eb016ff87b56dc2347edac15'
 
   # s3.amazonaws.com/Noodlesoft was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Noodlesoft/Hazel-#{version}.dmg"
   appcast 'https://www.noodlesoft.com/Products/Hazel/generate-appcast.php',
-          checkpoint: 'a13e761bd4969ce3a2f25168a61876ad7f5e75324b92e198378af9743da78887'
+          checkpoint: '07bd36dd4a36d8ceadc4662814e5161da5e3a7263d6d184a04a546bc115b32d8'
   name 'Hazel'
   homepage 'https://www.noodlesoft.com/'
 
@@ -13,9 +13,9 @@ cask 'hazel' do
 
   uninstall quit: 'com.noodlesoft.HazelHelper'
 
-  zap delete: [
-                '~/Library/Application Support/Hazel',
-                '~/Library/Preferences/com.noodlesoft.Hazel.plist',
-                '~/Library/Preferences/com.noodlesoft.HazelHelper.plist',
-              ]
+  zap trash: [
+               '~/Library/Application Support/Hazel',
+               '~/Library/Preferences/com.noodlesoft.Hazel.plist',
+               '~/Library/Preferences/com.noodlesoft.HazelHelper.plist',
+             ]
 end
