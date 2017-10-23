9c7399c81a011cfb2b4027a82de09e5289003bf7
Leif Denby <leif@denby.eu>
2017-10-23 23:29:49 +0100

    Add VAPOR 2.6.0

---
+++ Casks/vapor.rb
@@ -0,0 +1,10 @@
+cask 'vapor' do
+  version '2.6.0'
+  sha256 '0608952d436550bb22d692cef43087c2ed3d96fa09b5b13eaaedf24143ddd5c6'
+
+  url 'https://www.vapor.ucar.edu/sites/default/files/attachments/vapor-2.6.0-Darwin_x86_64.dmg'
+  name 'VAPOR is the Visualization and Analysis Platform for Ocean, Atmosphere, and Solar Researchers'
+  homepage 'https://www.vapor.ucar.edu/'
+
+  pkg 'vapor.pkg'
+end
