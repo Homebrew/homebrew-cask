5cbc498289e54c066418d98563749af5b4de2870
Jonathan 'J5' Cook <jonathan.cook@concur.com>
2017-10-24 15:36:50 -0400

    Koala 2.2.0
    Add Koala 2.2.0 app.

---
+++ Casks/koala.rb
@@ -0,0 +1,11 @@
+cask 'koala' do
+  version '2.2.0'
+  sha256 '3c8f0358a347b51c7efc7f8437888caaac0091bcfbd4f5bd6deed92e8c7cabf1'
+
+  # github.com/oklai/koala was verified as official when first introduced to the cask
+  url 'https://github.com/oklai/koala/releases/download/v2.2.0/Koala.dmg'
+  name 'Koala'
+  homepage 'http://koala-app.com/'
+
+  app 'Koala.app'
+end
