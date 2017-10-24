9b35ae2c36192f9b2db3ba3cf541d0b9a315c408
Jonathan 'J5' Cook <jonathan.cook@concur.com>
2017-10-24 15:19:58 -0400

    GoToFile 1.0.3
    Add GoToFile 1.0.3

---
+++ Casks/gotofile.rb
@@ -0,0 +1,10 @@
+cask 'gotofile' do
+  version '1.0.3'
+  sha256 '2baf82a8ac3a01f5751f8049327c4f4f63a923155d828d633ba39c3b34dbbb7c'
+
+  url 'http://www.soma-zone.com/download/files/GoToFile_1.0.3.tbz'
+  name 'GoToFile'
+  homepage 'http://www.soma-zone.com/GoToFile/'
+
+  app 'GoToFile.app'
+end
