4cbf8c56353bd7ae95f46c5af248b3bd6ff94f43
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-24 17:29:10 +0800

    Update expressions.rb

--- Casks/expressions.rb
+++ Casks/expressions.rb
@@ -1,5 +1,5 @@
 cask 'expressions' do
-  version '1.3'
+  version '1.3.1'
   sha256 '1d8daf05089da36d9d95e107e2b20c69e65e3160b73ba1da848220aa1114de08'
 
   url "http://www.apptorium.com/public/products/expressions/releases/Expressions-#{version}.zip"
