0c7332e97761ec19fc721632afc4be95486c5af4
Vítor Galvão <info@vitorgalvao.com>
2017-10-24 20:48:37 +0100

    Update muse.rb

--- Casks/muse.rb
+++ Casks/muse.rb
@@ -2,7 +2,7 @@ cask 'muse' do
   version '3.0'
   sha256 '82756fb49543f1148258649bbbff98aaf313eff1fe58f42ea3766a336beb208d'
 
-  url 'https://github.com/xzzz9097/Muse/files/1190355/Muse30.zip'
+  url "https://github.com/xzzz9097/Muse/files/1190355/Muse#{version.no_dots}.zip"
   name 'Muse'
   homepage 'https://github.com/xzzz9097/Muse'
 
