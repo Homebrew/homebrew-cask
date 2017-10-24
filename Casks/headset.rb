94d9148461cd164ecf195977d2933e5c0748261e
Daniel Ravina <daniel_ravina@acl.com>
2017-10-23 23:29:01 -0700

    Update headset to 1.6.1 (#39987)

--- Casks/headset.rb
+++ Casks/headset.rb
@@ -1,11 +1,11 @@
 cask 'headset' do
-  version '1.6.0'
-  sha256 '762af77950542b56622ddf889a00476f033113ab5c6babcd247f1c3460566486'
+  version '1.6.1'
+  sha256 '5b8aa6bb7aef989f2960136e8d86dee9340103614adc02d208bd7b558e611eb5'
 
   # github.com/headsetapp/headset-electron was verified as official when first introduced to the cask
   url "https://github.com/headsetapp/headset-electron/releases/download/v#{version}/Headset-#{version}.dmg"
   appcast 'https://github.com/headsetapp/headset-electron/releases.atom',
-          checkpoint: '0e8135ae560e24a849d84e51d2ee295433f2ca5fac2400aab7ead3f065a1251d'
+          checkpoint: '757797ecbe7bbda34e5efcf484dd843fd5a8f411e112448c91b90aeaf6d38650'
   name 'Headset'
   homepage 'http://headsetapp.co/'
 
