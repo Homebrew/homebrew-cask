8f93e06dcc3bada6f103af0dcfd737896ee97e7e
Vítor Galvão <info@vitorgalvao.com>
2017-10-24 20:58:43 +0100

    Update displayspacesmanager.rb

--- Casks/displayspacesmanager.rb
+++ Casks/displayspacesmanager.rb
@@ -2,7 +2,7 @@ cask 'displayspacesmanager' do
   version '1.2.3'
   sha256 '0bd268394a8766e03a3fd53496c6fddca41ffc9a84f2d5fae684ed0d9f18e934'
 
-  url 'https://downloads.binaryage.com/DisplaySpacesManager-1.2.3.zip'
+  url "https://downloads.binaryage.com/DisplaySpacesManager-#{version}.zip"
   name 'DisplaySpacesManager'
   homepage 'https://discuss.binaryage.com/t/is-it-possible-to-save-and-load-restore-a-layout-grid/5827'
 
