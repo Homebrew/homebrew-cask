5608cda123348188ba00ae9f3eb3bae7c3a77f73
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-25 06:59:17 +0800

    Update muse.rb

--- Casks/muse.rb
+++ Casks/muse.rb
@@ -1,8 +1,10 @@
 cask 'muse' do
-  version '3.0'
-  sha256 '82756fb49543f1148258649bbbff98aaf313eff1fe58f42ea3766a336beb208d'
+  version '4.0-alpha.1'
+  sha256 '356dbcba6c4f56bb00b1cabfbd8f6556aacf4fa70937cc717ec55039e528742d'
 
-  url "https://github.com/xzzz9097/Muse/files/1190355/Muse#{version.no_dots}.zip"
+  url "https://github.com/xzzz9097/Muse/releases/download/v#{version}/Muse.app.zip"
+  appcast 'https://github.com/xzzz9097/Muse/releases.atom',
+          checkpoint: '4560be159d9bba0347c2494ecdf5b53bdbcc8a50c8e22fa8b986baa32dcdb440'
   name 'Muse'
   homepage 'https://github.com/xzzz9097/Muse'
 
