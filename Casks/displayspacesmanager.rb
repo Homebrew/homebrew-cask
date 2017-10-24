9c32351fdcba3c90041dc72f2ebf25cb3f6ccd0e
Jonathan 'J5' Cook <jonathan.cook@concur.com>
2017-10-24 14:55:46 -0400

    DisplaySpacesManager 1.2.3

---
+++ Casks/displayspacesmanager.rb
@@ -0,0 +1,10 @@
+cask 'displayspacesmanager' do
+  version '1.2.3'
+  sha256 '0bd268394a8766e03a3fd53496c6fddca41ffc9a84f2d5fae684ed0d9f18e934'
+
+  url 'https://downloads.binaryage.com/DisplaySpacesManager-1.2.3.zip'
+  name 'DisplaySpacesManager'
+  homepage 'https://discuss.binaryage.com/t/is-it-possible-to-save-and-load-restore-a-layout-grid/5827'
+
+  app 'DisplaySpacesManager.app'
+end
