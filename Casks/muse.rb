453d6e1db8b07972bad9be4bd84a94c2f5b2701b
Pavel Sakun <pavel.sakun@gmail.com>
2017-10-24 19:06:31 +0300

    Add Muse 3.0

---
+++ Casks/muse.rb
@@ -0,0 +1,10 @@
+cask 'muse' do
+  version '3.0'
+  sha256 '82756fb49543f1148258649bbbff98aaf313eff1fe58f42ea3766a336beb208d'
+
+  url 'https://github.com/xzzz9097/Muse/files/1190355/Muse30.zip'
+  name 'Muse'
+  homepage 'https://github.com/xzzz9097/Muse'
+
+  app 'Muse.app'
+end
