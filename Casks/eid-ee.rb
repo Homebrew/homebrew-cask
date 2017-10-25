b815877e72faaf2713c37fa47eb84f7a68dde776
commitay <commitay@users.noreply.github.com>
2017-10-25 12:36:49 +1000

    Update eid-ee to 17.10.0.1750
    https://github.com/Homebrew/homebrew-cask-eid/pull/36

--- Casks/eid-ee.rb
+++ Casks/eid-ee.rb
@@ -1,6 +1,6 @@
 cask 'eid-ee' do
-  version '17.6.0.1724'
-  sha256 'e4d5df99d0ace588024ebe98e694d1f3d30ef13fa8ac9918dc998243841cf984'
+  version '17.10.0.1750'
+  sha256 '52d1c2cc7b2e5509267d28cd9d99b6c8a9d2ec952afb303703c0099f21e92cb5'
 
   url "https://installer.id.ee/media/osx/Open-EID_#{version}.dmg"
   name 'Electronic identity card software for Estonia'
@@ -12,6 +12,7 @@ cask 'eid-ee' do
   uninstall script: {
                       executable: 'uninstall.sh',
                       input:      ['y'],
+                      sudo:       true,
                     }
 
   caveats <<~EOS
