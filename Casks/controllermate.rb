312bf5a540481c865eacd9895bbc94fd20726a22
Victor Popkov <victor@popkov.me>
2017-10-25 18:14:06 +0300

    Update controllermate to 4.10.3

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -1,11 +1,11 @@
 cask 'controllermate' do
-  version '4.10.2'
-  sha256 'fbbfe4bf7140314a732f1d9ed0b42ddf54d0ee30a13c3d8f9560e5933da8addf'
+  version '4.10.3'
+  sha256 'a20f24420084fdaeccfd0e116f0d51d6195132892a6ff81bf24c2c0dbac621f2'
 
   # amazonaws.com/orderedbytes was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/orderedbytes/ControllerMate#{version.no_dots}.zip"
   appcast 'https://www.orderedbytes.com/sparkle/appcast_cm460.xml',
-          checkpoint: '745392e35517416f967acbb0fa5831acd50f7a57187b81c4f062c12488a8dabf'
+          checkpoint: '684bfc150d43530aabc7563ba0ad16f249eb90d3dca70c435f99314ae51f2cf7'
   name 'ControllerMate'
   homepage 'https://www.orderedbytes.com/controllermate/'
 
