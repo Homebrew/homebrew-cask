660b9dd4cc76b00fe7dc9fcf02fa14aa6b28a9dc
Andrew Wade <fincle@users.noreply.github.com>
2017-10-24 20:43:40 -0700

    Update goodsync to 10.6.4 (#40017)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,10 +1,10 @@
 cask 'goodsync' do
-  version '10.6.1'
-  sha256 '06f3ec8b89e8ae966b5325f8f8a651c0e4473a3957615ee328729dc1a9fecd40'
+  version '10.6.4'
+  sha256 'e77f41a8281964a2bb1552b8be0bd871f5030ee038aecb8e4e576e10df5a8016'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://www.goodsync.com/download',
-          checkpoint: '442f3147449d9b1864dca173e7cb03cb64726c2732d307fc519fefd168c26dd5'
+          checkpoint: '6f5106e4e44e7bf76e1e4eba9c9bea70140e8fcebef07f9235f41806026ad3ed'
   name 'GoodSync'
   homepage 'https://www.goodsync.com/'
 
