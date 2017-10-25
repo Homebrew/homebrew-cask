c74d22db2654cf11966f3d2b4cce170d393f9196
Victor Popkov <victor@popkov.me>
2017-10-26 00:22:14 +0300

    Update deepstream to 3.1.1 (#40037)

--- Casks/deepstream.rb
+++ Casks/deepstream.rb
@@ -1,11 +1,11 @@
 cask 'deepstream' do
-  version '3.1.0'
-  sha256 '210b14ae1460b7e5956313ad67d492846f997c77de2d760fcd436d0186827d85'
+  version '3.1.1'
+  sha256 '652fdb2c19ffba27cf455cb0b3f8919afe0ba7572bf8558e247e5e45b20fba04'
 
   # github.com/deepstreamIO/deepstream.io was verified as official when first introduced to the cask
   url "https://github.com/deepstreamIO/deepstream.io/releases/download/v#{version}/deepstream.io-mac-#{version}.pkg"
   appcast 'https://github.com/deepstreamIO/deepstream.io/releases.atom',
-          checkpoint: 'a232cf01164ec7966d1e3849af6703ca936f4c04caaa5d707fbf09ea50547a8e'
+          checkpoint: '6791a562a7593bb0f3cddc9b08dc52b923628031f663679c5e5b47f39d580531'
   name 'deepstream'
   homepage 'https://deepstream.io/'
 
