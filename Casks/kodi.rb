71c0f0691efaa630ca8db7b0afc7fbe416013389
commitay <commitay@users.noreply.github.com>
2017-10-26 12:34:23 +1000

    Update kodi to 17.5-Krypton

--- Casks/kodi.rb
+++ Casks/kodi.rb
@@ -1,20 +1,22 @@
 cask 'kodi' do
-  version '17.4-Krypton'
-  sha256 'b7ba23a154cb1551c3670296f1448fbd93d286b0e90dee8d724b4b26a6e8a7e2'
+  version '17.5-Krypton'
+  sha256 'ecb188a86c28a9c914ca7592a0dd07555a73bdcd42333eb773840c12494a817e'
 
   url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
   appcast 'https://github.com/xbmc/xbmc/releases.atom',
-          checkpoint: 'd3a045daf4c1e117cb6acec422a927e7bc893aec1c75e66552ede8a795c43163'
+          checkpoint: '7c779e1983f72da1d75e539e690cefb2911eb58170dc965d683a935d5bf82276'
   name 'Kodi'
   homepage 'https://kodi.tv/'
 
   app 'Kodi.app'
 
   zap delete: [
-                '~/.kodi',
-                '~/Library/Application Support/Kodi',
                 '~/Library/Logs/kodi.log',
                 '~/Library/Logs/kodi.old.log',
                 '~/Library/Saved Application State/org.xbmc.kodi.savedState',
+              ],
+      trash:  [
+                '~/.kodi',
+                '~/Library/Application Support/Kodi',
               ]
 end
