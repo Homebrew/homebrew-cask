a2547bdedc0b54b74288992f8fa99297ea7348b7
commitay <commitay@users.noreply.github.com>
2017-10-26 18:12:49 +1000

    Update lantern.rb

--- Casks/lantern.rb
+++ Casks/lantern.rb
@@ -2,7 +2,6 @@ cask 'lantern' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com/lantern was verified as official when first introduced to the cask
   url 'https://getlantern.org/lantern-installer.dmg'
   name 'Lantern'
   homepage 'https://getlantern.org/'
