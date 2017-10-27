3f9e8cac44ede13a9c25f851feaa82d0d45a8e06
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-10-27 07:29:40 +0100

    Update tankix to 6377 (#40093)

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version '5847'
-  sha256 '748232a830bf9cd2dc362f2bceb20f7b8255ccdad64c3c9cc2164f7b3c509d14'
+  version '6377'
+  sha256 '3adfd0a5c3450831c0da023b22daa13bc1adbd05956732951fc16ba513860b8e'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/master-#{version}/TankiX.dmg"
   name 'Tanki X'
