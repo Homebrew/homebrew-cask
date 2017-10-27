107a71ffcb527594d7d53768742dd786c0368bb6
commitay <commitay@users.noreply.github.com>
2017-10-27 15:44:28 +1000

    Update folx.rb

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -3,7 +3,7 @@ cask 'folx' do
   sha256 'f00214b419756505410fefcf312c7d2fc2f81ed8e4ba134918c22232d23af1bd'
 
   url "https://cdn.eltima.com/download/folx-updater/downloader_mac_#{version}.dmg"
-  appcast 'http://mac.eltima.com/download/folx-updater/folx.xml',
+  appcast 'https://cdn.eltima.com/download/folx-updater/folx.xml',
           checkpoint: 'f59ab26d33f3fa694a1d9d8898d19622cb2c6486b714e959e83156f8b7461dda'
   name 'Folx'
   homepage 'https://mac.eltima.com/download-manager.html'
