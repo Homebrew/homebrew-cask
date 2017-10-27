c994eaf367e96639c6e22fe64a957ef6901b5125
jcbot <jcb@leipert.io>
2017-10-27 10:06:24 +0200

    Update jetbrains-toolbox to 1.5.2871 (#40095)

--- Casks/jetbrains-toolbox.rb
+++ Casks/jetbrains-toolbox.rb
@@ -1,10 +1,10 @@
 cask 'jetbrains-toolbox' do
-  version '1.4.2492'
-  sha256 'd8426a5dc0c9c46773a8a41b72e71b8f80cdac0545e942d4fff3f2dabcbf3a68'
+  version '1.5.2871'
+  sha256 '1a9c86d2c020a7f454df3bdc3f60ebd9816e569ca37ed6ec62da3853c4821891'
 
   url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release',
-          checkpoint: '1284ec7ed798f102479a563ef7789d963c961fd9ccf2fc68fe35ea5fc14dd1ef'
+          checkpoint: '988da9360a907810e392d075e2bef14315646f313d4660b8257016a49765371d'
   name 'JetBrains Toolbox'
   homepage 'https://www.jetbrains.com/toolbox/app/'
 
