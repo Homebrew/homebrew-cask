36dd3e560674b4125dcda89df07cbe61faaaa1bd
Jeroen <jeroenooms@gmail.com>
2017-10-27 14:14:26 +0200

    add conflicts_with and pkgutil uninstaller

--- Casks/gfortran.rb
+++ Casks/gfortran.rb
@@ -7,14 +7,16 @@ cask 'gfortran' do
   name 'gfortran'
   homepage 'https://gcc.gnu.org/wiki/GFortranBinaries'
 
+  conflicts_with formula: 'gcc'
   depends_on macos: '>= :el_capitan'
 
   pkg "gfortran-#{version}-ElCapitan/gfortran.pkg"
 
-  uninstall delete: [
-                      '/usr/local/gfortran',
-                      '/usr/local/bin/gfortran',
-                    ]
+  uninstall delete:  [
+                       '/usr/local/gfortran',
+                       '/usr/local/bin/gfortran',
+                     ],
+            pkgutil: 'com.gnu.gfortran'
 
   caveats do
     files_in_usr_local
