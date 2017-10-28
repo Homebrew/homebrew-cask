03286a54893fd997a4b2ede33f1e509ba54e40e7
Otavio J S Alves <otaviojsalves@gmail.com>
2017-10-28 10:34:46 -0200

    Iceberg
     Iceberg is an Integrated Packaging Environment (IPE) that allows you to create packages or metapackages conforming to the Mac OS X specifications.

---
+++ Casks/iceberg.rb
@@ -0,0 +1,12 @@
+
+cask 'Iceberg' do
+  version :latest
+  sha256 :no_check
+
+  url "http://s.sudre.free.fr/Software/files/Iceberg.dmg"
+  name 'Iceberg'
+  homepage 'http://s.sudre.free.fr/Software/Iceberg.html'
+  license :gratis
+
+  app 'Iceberg.app'
+end
