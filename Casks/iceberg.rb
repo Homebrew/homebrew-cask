0345920cfd8a211b19798977159992988b045680
commitay <commitay@users.noreply.github.com>
2017-10-28 22:46:26 +1000

    iceberg

--- Casks/iceberg.rb
+++ Casks/iceberg.rb
@@ -1,12 +1,12 @@
+cask 'iceberg' do
+  version '1.3.1'
+  sha256 '08ba0cb792dd2121e4a492df03967c6c97d456bab4afda4b40400cd44ff01bd8'
 
-cask 'Iceberg' do
-  version :latest
-  sha256 :no_check
-
-  url "http://s.sudre.free.fr/Software/files/Iceberg.dmg"
+  url 'http://s.sudre.free.fr/Software/files/Iceberg.dmg'
+  appcast 'http://s.sudre.free.fr/Software/documentation/Iceberg/English.lproj/documentation/Release%20Notes.html',
+          checkpoint: '99215e7cfa27c45a0cf015c4decb27dfbbd1747cdc57c4d366367eb730e1825d'
   name 'Iceberg'
   homepage 'http://s.sudre.free.fr/Software/Iceberg.html'
-  license :gratis
 
   app 'Iceberg.app'
 end
