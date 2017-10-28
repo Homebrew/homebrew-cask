bdf1e50eb8e788fa804f980edd0ed65d517de492
Bernd Krannich <berndkrannich@users.noreply.github.com>
2017-10-28 20:41:22 +0200

    Update rubitrack-pro to 4.4.5 (#40128)

--- Casks/rubitrack-pro.rb
+++ Casks/rubitrack-pro.rb
@@ -1,10 +1,10 @@
 cask 'rubitrack-pro' do
-  version '4.4.4'
-  sha256 'fdaa6b12d77996c0806bea2230ba935ea3d9399ca356e696d9f5e56bfa406792'
+  version '4.4.5'
+  sha256 'eb29912f3511fad5175446395e84e8f15c5e9d13c3d1d04ba78cbb9617d1d6e0'
 
   url "https://www.rubitrack.com/files/rubiTrack-#{version}_u.dmg"
   appcast "https://www.rubitrack.com/autoupdate/sparkle#{version.major}.xml",
-          checkpoint: 'f285ddb3feb516b8ce18066ddf2f2d9cded8ba80b2ec0a8bf9fc3ac1bfba2935'
+          checkpoint: 'ae47d04bcfdeaa6281c683e5f6c633c0b08dbc51552c90a0e583137c38963e0b'
   name 'rubiTrack'
   homepage 'https://www.rubitrack.com/'
 
