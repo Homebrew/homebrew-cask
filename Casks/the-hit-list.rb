a94e2602ee6b5705aaeea867187fdedadc976deb
commitay <commitay@users.noreply.github.com>
2017-10-29 07:03:48 +1000

    Update the-hit-list.rb

--- Casks/the-hit-list.rb
+++ Casks/the-hit-list.rb
@@ -1,5 +1,5 @@
 cask 'the-hit-list' do
-  version '11.1.31,363'
+  version '1.1.31,363'
   sha256 'e1313860db752b2be1f17dad267417eac569e4ed3bc54d32c7545ea086772443'
 
   url "https://distrib.karelia.com/downloads/TheHitList-#{version.after_comma}.zip"
