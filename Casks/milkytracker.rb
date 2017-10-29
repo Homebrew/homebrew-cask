560f2b51ed87177b8c5204cbad347b6688b266da
James Kruth <james@kruth.org>
2017-10-29 00:49:42 -0400

    Update milkytracker to 1.01.00 (#40140)
    * Update milkytracker to 1.01.00
    
    * appcast

--- Casks/milkytracker.rb
+++ Casks/milkytracker.rb
@@ -1,9 +1,11 @@
 cask 'milkytracker' do
-  version '1.0.0,1.00.00'
-  sha256 '8b8c4ba5e5459b7c83959990d302dce780c01faea7786b47ca0e2b3100b5b364'
+  version '1.01.00'
+  sha256 'bfaa71fddd41aa0b9220eca3b0de6953061aa60b730dbea80a79a4c42ca33e9b'
 
   # github.com/milkytracker/MilkyTracker was verified as official when first introduced to the cask
-  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version.before_comma}/milkytracker-#{version.after_comma}-osx.dmg"
+  url "https://github.com/milkytracker/MilkyTracker/releases/download/v#{version}/milkytracker-#{version}.dmg"
+  appcast 'https://github.com/milkytracker/MilkyTracker/releases.atom',
+          checkpoint: '8505703fe680236448c273062676dc530af227958f3fb00c7803c4953b76ca84'
   name 'MilkyTracker'
   homepage 'http://milkytracker.titandemo.org/'
 
