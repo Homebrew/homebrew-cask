9c8aaba5c6536a3bc15a5bc72dd52d395733f222
Ryota Inakazu <mail.to.inakazu@gmail.com>
2017-10-30 11:41:18 +0900

    Add CompareMerge v2.03x

---
+++ Casks/comparemerge.rb
@@ -0,0 +1,10 @@
+cask 'comparemerge' do
+  version '2.03x'
+  sha256 'ed6f53dd59481868ff29dc1d8800f5a821d918ce7fee1677f450bf302f23025c'
+
+  url "https://sourceforge.net/projects/comparemergenosandbox/files/CompareMerge%20#{version}.zip/download"
+  name 'CompareMerge'
+  homepage 'https://sourceforge.net/projects/comparemergenosandbox/'
+
+  app 'CompareMerge 2.03x/CompareMerge 2.03x.app'
+end
