24847c40a59c1f0d2ee00f88490134f8a41fdb93
commitay <commitay@users.noreply.github.com>
2017-10-30 19:41:47 +1000

    comparemerge

--- Casks/comparemerge.rb
+++ Casks/comparemerge.rb
@@ -3,8 +3,10 @@ cask 'comparemerge' do
   sha256 'ed6f53dd59481868ff29dc1d8800f5a821d918ce7fee1677f450bf302f23025c'
 
   url "https://downloads.sourceforge.net/comparemergenosandbox/CompareMerge%20#{version}.zip"
+  appcast 'https://sourceforge.net/projects/comparemergenosandbox/rss',
+          checkpoint: '0f230c9700bd27af2530109a80f788da31766bbcc708f98f9401256f62fa3d16'
   name 'CompareMerge'
   homepage 'https://sourceforge.net/projects/comparemergenosandbox/'
 
-  app 'CompareMerge 2.03x/CompareMerge 2.03x.app'
+  app "CompareMerge #{version}/CompareMerge #{version}.app"
 end
