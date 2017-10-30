ff931b4a3fc4d68381a56ada1ef744cd7dba53aa
Grettir Asmundarson <grettir@users.noreply.github.com>
2017-10-30 02:41:23 -0600

    Update screenflow to 7.1.1

--- Casks/screenflow.rb
+++ Casks/screenflow.rb
@@ -1,10 +1,10 @@
 cask 'screenflow' do
-  version '7.1'
-  sha256 '584d16451aea1be65adcd60cdca27a8ceee7342bbb0b99df3d8d4adedf3a293e'
+  version '7.1.1'
+  sha256 '06b880cbff146da8d3cacbc5b78d2d1cfbda99e01b8314204041816461c65f18'
 
   url "https://www.telestream.net/download-files/screenflow/#{version.major_minor.dots_to_hyphens}/ScreenFlow-#{version}.dmg"
   appcast 'https://www.telestream.net/updater/screenflow/appcast.xml',
-          checkpoint: '8283f97fdbd4f5374acf9dcef4fe2359e68f5d5196620bb063ff884e0814fbc1'
+          checkpoint: '2ee00d2886035bfc23078ce6d4856a6bc6fe12bb1263289ce51fbca90984b46e'
   name 'ScreenFlow'
   homepage 'https://www.telestream.net/screenflow/'
 
