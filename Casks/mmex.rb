72ac7959addad42d45eafef13b8b31af9f2ec222
Ben <BenjaminHCCarr@users.noreply.github.com>
2017-10-30 22:44:47 -0400

    Same version new toolchain, new sha, new checkpoint
    From: https://github.com/moneymanagerex/moneymanagerex/releases/tag/v1.3.3
    ```
    NOTE: Mac build updated on Oct 7, 2017 to use wxWidgets 3.0.2 instead of 3.1. This fixes the issues with right click menus being greyed out.
    ```

--- Casks/mmex.rb
+++ Casks/mmex.rb
@@ -1,11 +1,11 @@
 cask 'mmex' do
   version '1.3.3'
-  sha256 '36380329f017c9edef4b7c00aa076605298830031f9ee2ccd0aba6f7911e0ea1'
+  sha256 '49eeb6b50a51bfa09da7f4bb063ad75c5695eeeac410ae13d4c8e8844ac43505'
 
   # github.com/moneymanagerex/moneymanagerex was verified as official when first introduced to the cask
-  url "https://github.com/moneymanagerex/moneymanagerex/releases/download/v#{version}/mmex_#{version}_macos10.9.dmg"
+  url "https://github.com/moneymanagerex/moneymanagerex/releases/download/v#{version}/mmex_#{version}_macos10.9-wx3.0.2build.dmg"
   appcast 'https://github.com/moneymanagerex/moneymanagerex/releases.atom',
-          checkpoint: 'b7710b29a059b3f20a943e6d06dbcb95883f135eec8adc63ce625c93c98c0618'
+          checkpoint: 'c027d7d307038432315067629e30c3b587e2fd1b10d9c7702af64995cb2831d0'
   name 'Money Manager Ex'
   homepage 'http://www.moneymanagerex.org/'
 
