d111e6a26977552c3b5c08bbcff7cef7c0f73618
Ivan Cheng <ivan_cheng@westpac.co.nz>
2017-10-31 20:38:42 +1300

    Update sha256 for Mockplus 3.2.6

--- Casks/mockplus.rb
+++ Casks/mockplus.rb
@@ -1,6 +1,6 @@
 cask 'mockplus' do
   version '3.2.6'
-  sha256 'a4edb8204801a534081427dcce04d0d87520e826abcdfe964d0b75e23c64d39f'
+  sha256 '1e9f552256e4b5d49d490981de1801c801963ed0c9de493a992c6dd83733a439'
 
   url "http://cdn.mockplus.cn/software/osx/Mockplus_v#{version}.dmg"
   name 'Mockplus'
