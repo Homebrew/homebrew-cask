dea684c62983407202a910d4eaf850570b05881c
Josh Brown <brownj@users.noreply.github.com>
2017-10-30 20:53:13 -0500

    Update borgbackup to 1.1.1 (#40181)
    * Update to borgbackup v1.1.1
    
    * Update checkpoint SHA-256 hash

--- Casks/borgbackup.rb
+++ Casks/borgbackup.rb
@@ -1,11 +1,11 @@
 cask 'borgbackup' do
-  version '1.1.0'
-  sha256 'e87a8e597d3d87dc4df79befbfd6a5c68576e99d66cb209a7ae34018b6daacd0'
+  version '1.1.1'
+  sha256 'cc0d092690ea4c86bf0d4e7ab7c8cad487e5df287559b2347531e1a62e9c8fb6'
 
   # github.com/borgbackup/borg was verified as official when first introduced to the cask
   url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
   appcast 'https://github.com/borgbackup/borg/releases.atom',
-          checkpoint: '99df3e9400b721345bd8c21e6e94b9957ce8d38fbff0b88d1be8d984b0d40147'
+          checkpoint: 'd22128da1dc5618f4a497403238d1930c39555764fa0b02926a1c23195b351b7'
   name 'BorgBackup'
   homepage 'https://borgbackup.readthedocs.io/en/stable/'
   gpg "#{url}.asc", key_id: '51F78E01'
