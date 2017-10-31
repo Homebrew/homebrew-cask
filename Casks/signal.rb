e441d98853e850309ef70563cba8484b781abeb1
Mike Kusold <git@mikekusold.com>
2017-10-31 13:30:22 -0600

    Update Signal.app v1.0.34 url to read from version field

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -2,7 +2,7 @@ cask 'signal' do
   version '1.0.34'
   sha256 'b45f89aeb9efd7d28eba8b2fdc5dd2fb0ca090daca3641afb1b2a549988337b3'
 
-  url 'https://updates.signal.org/desktop/Signal-mac-1.0.34.zip'
+  url "https://updates.signal.org/desktop/Signal-mac-#{version}.zip"
   name 'Signal'
   homepage 'https://signal.org/'
 
