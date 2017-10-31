e7ae891a6c61f17ebbfaa32eca97eebbb7a9319a
Alexander Petin <axp@ngs.ru>
2017-11-01 03:09:35 +0700

    Update xamarin-mac to 3.8.1.0

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-mac' do
-  version '3.8.0.49'
-  sha256 '4b3d236df92979f969fd04bd666719d4f591f84e562c7a0976cfbdc50ae16a02'
+  version '3.8.1.0'
+  sha256 '56f6a54b3df7fc0c27293fc19e738596a9a7ee25fb2fce0326a84ed7019e9e83'
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '796e78c229d3accc035db4eb28032362cd75666b907f0b8eb8169b00c883dd41'
+          checkpoint: 'baafed12ce3818833dbae144410143f428b4719b117fe33ec6634653d3762630'
   name 'Xamarin Mac'
   homepage 'https://www.xamarin.com/platform'
 
