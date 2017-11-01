1c5a3c11316cac585e4aab94e590722e6deac28d
Kalle Laine <pahakalle@protonmail.ch>
2017-11-01 06:47:47 +0200

    Update pyfa to 1.33.1,lifeblood-1.0 (#40214)

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,10 +1,10 @@
 cask 'pyfa' do
-  version '1.33.0,lifeblood-1.0'
-  sha256 '153f00f042c99031ebcd371f0880f73a8a88db75b7b67634464961038dbeb78a'
+  version '1.33.1,lifeblood-1.0'
+  sha256 '78a08e42220d9493042e964a71d24d05742864614bc57a2fc62b916280057571'
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version.before_comma}/pyfa-#{version.before_comma}-#{version.after_comma}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom',
-          checkpoint: '3a55e3e6c8677e14b5cb5b40edaee48e72229aaaaaa197e07097a1a9795f29fc'
+          checkpoint: '974a3b8ef7df6131b4f205180589172694d03f3f7f8d79b8d099bee2fc1f5875'
   name 'pyfa'
   homepage 'https://github.com/pyfa-org/Pyfa'
 
