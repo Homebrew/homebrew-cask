d3c9f271cd6eb23c6c0c8768c7cf54894d049d01
commitay <commitay@users.noreply.github.com>
2017-11-01 14:48:10 +1000

    Update screens to 4.2.2,10054 (#40212)

--- Casks/screens.rb
+++ Casks/screens.rb
@@ -1,11 +1,11 @@
 cask 'screens' do
-  version '4.2,10036'
-  sha256 '1d302b0e762f2e5a80f3c5c19b91aa65a9481828d776473eb6b87cfc5b0e1431'
+  version '4.2.2,10054'
+  sha256 '158422fa98a06ee46538153dfba341ee3e5c8ae9555db37d9bc3b45c7d40630f'
 
   # dl.devmate.com was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.edovia.screens4.mac/Screens#{version.major}.dmg"
   appcast "https://updates.devmate.com/com.edovia.screens#{version.major}.mac.xml",
-          checkpoint: 'c25df03eb261242564ba9afc570250ee9ebe385531e9e6005a349a6e874798af'
+          checkpoint: 'a45dbe4433e116e2392aabdde21d99cfb306f0566d059a81da23cd7e4db5426f'
   name 'Screens'
   homepage 'https://edovia.com/screens-mac/'
 
