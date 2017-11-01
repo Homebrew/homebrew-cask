1857500e024646e1adb535d6419604f39f3fdbd0
commitay <commitay@users.noreply.github.com>
2017-11-01 22:30:33 +1000

    Update komodo-edit to 11.0.1-18119

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -1,6 +1,6 @@
 cask 'komodo-edit' do
-  version '11.0.0-18063'
-  sha256 'b5dbc8a031679e29143136113082af98dcbcc1c4410ed0d4a457fc242d728cfc'
+  version '11.0.1-18119'
+  sha256 '74be0c20c4fb96bbbf697f6b52b4ea0abbda1d06381906e224ecf8eddd08f7c9'
 
   url "https://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*}, '')}/Komodo-Edit-#{version}-macosx-x86_64.dmg"
   name 'Komodo Edit'
