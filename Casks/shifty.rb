523c63d2820fc494cb501b7f2cae5ffb2dbf84ca
Arnaud De Bruecker <arnauddebruecker@MacBook-Pro-de-Arnaud.local>
2017-11-02 14:40:49 +0000

    Update shifty to 0.3

--- Casks/shifty.rb
+++ Casks/shifty.rb
@@ -1,11 +1,11 @@
 cask 'shifty' do
-  version '0.2'
-  sha256 '2810714ef55f09dbadd3ba2ae7b8126aaba8dd397651726b06b4f816c3af0c84'
+  version '0.3'
+  sha256 'a84e0cef821dcdd77c7cd00e6ac474c7a2081feea63998e44f3a132c1a5ac349'
 
   # github.com/thompsonate/Shifty was verified as official when first introduced to the cask
   url "https://github.com/thompsonate/Shifty/releases/download/#{version}/Shifty-#{version}.dmg"
   appcast 'https://github.com/thompsonate/Shifty/releases.atom',
-          checkpoint: '76467b5b95772d424c2fdb843fdc4c8f4021a5638d3a9496a2cff305e9ec5340'
+          checkpoint: '65de5b4a6c2a41b1f4a398500c44ed61956e23d7c541e73844273940a67799d2'
   name 'Shifty'
   homepage 'http://shifty.natethompson.io/'
 
