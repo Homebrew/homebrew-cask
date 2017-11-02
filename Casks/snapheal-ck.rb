f25b9f0c770f94daf0c5b1d853e361281179cd4c
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2017-11-02 02:59:27 +0100

    Rename snapheal-pro to snapheal-ck, update to 1.7.1200 (#40234)
    * Updating Snapheal CK app name
    
    * Renaming cask to snapheal-ck
    
    * snapheal-ck: 1.7.1200

--- Casks/snapheal-ck.rb
+++ Casks/snapheal-ck.rb
@@ -1,21 +1,21 @@
-cask 'snapheal-pro' do
-  version '1.6.1193'
-  sha256 '0989dcf995c1058c3f80f8898a8f590e079ae1aebedfd7f84dbef6a8bed72874'
+cask 'snapheal-ck' do
+  version '1.7.1200'
+  sha256 'b91c8b67bea96d5b95afcdcd55b76093fd2bce84fbc15b6272ac179c3da62a9f'
 
   # amazonaws.com/SnaphealCK was verified as official when first introduced to the cask
   url "https://creativekit.s3.amazonaws.com/SnaphealCK/SnaphealCKNA_Distribution_v#{version.dots_to_underscores}.zip"
   appcast 'http://cdn.macphun.com/updates/SnaphealMacPRO/appcast.xml',
-          checkpoint: '41be23f355c922362ddf47f2847cfd5050122ecfb5a1e57860187f16b04765b6'
-  name 'Macphun Snapheal Pro'
+          checkpoint: 'b3ccaf3df1bbd98f93d03b141775c94437821bb70489ee2d3f4b4b0a5f389eda'
+  name 'Macphun Snapheal CK'
   homepage 'https://macphun.com/snapheal'
 
-  app 'Snapheal Pro.app'
+  app 'Snapheal CK.app'
 
   uninstall quit: 'com.macphun.snaphealpro'
 
-  zap delete: [
-                '~/Library/Caches/com.macphun.snaphealpro',
-                '~/Library/Application Support/Snapheal Pro',
+  zap delete: '~/Library/Caches/com.macphun.snaphealpro',
+      trash:  [
+                '~/Library/Application Support/Snapheal CK',
                 '~/Library/Preferences/com.macphun.snaphealpro.plist',
               ]
 end
