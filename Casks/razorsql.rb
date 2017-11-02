43e7e89a1c9bf6f5d3b346a2a310a8e81b767575
Anver Housseini <contact@anver.ch>
2017-11-02 08:26:22 +0100

    Update razorsql to 7.3.13

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,10 +1,10 @@
 cask 'razorsql' do
-  version '7.3.12'
-  sha256 '9249fd6d3e85b936177a3058cd9ac4c80e2b6efaede50b68399f70c9b88cc7a9'
+  version '7.3.13'
+  sha256 'b3c0f6c06133b3f127861f67cd222c9c3a9ec86c4154db03fc6bb2dcf52d991c'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   appcast 'https://razorsql.com/updates.html',
-          checkpoint: '1838a06926cca35f999ae17255da683c507aa7a7721c4959a7fe13a01dffe798'
+          checkpoint: '3341ecb811376a26fe243dc37a481f701792350c0e2865dfbc7aa65f1cb6aa7e'
   name 'RazorSQL'
   homepage 'https://razorsql.com/download_mac.html'
 
