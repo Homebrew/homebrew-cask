c4724590cf14357f8b2d65ce7f594cdfd38bf7f7
jazzwang <jazzwang.tw@gmail.com>
2017-11-03 19:26:50 +0800

    Update microsoft-azure-storage-explorer to 0.9.2

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -1,10 +1,10 @@
 cask 'microsoft-azure-storage-explorer' do
-  version :latest
-  sha256 :no_check
+  version '0.9.2'
+  sha256 'd88fa022f6827571fddc36dc645d6c12c8bdacf04cf9b0b6fd3c7c27b870992c'
 
-  url 'https://go.microsoft.com/fwlink/?LinkId=708342'
+  url 'http://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer_signed.zip'
   name 'Microsoft Azure Storage Explorer'
   homepage 'https://azure.microsoft.com/en-us/features/storage-explorer/'
 
-  app 'MicrosoftAzureStorageExplorer_signed/Microsoft Azure Storage Explorer.app'
+  app 'MicrosoftAzureStorageExplorer/Microsoft Azure Storage Explorer.app'
 end
