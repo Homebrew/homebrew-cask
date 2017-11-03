0356136fa749fca4543a6e8fae70cd594dd5f6d0
Matthew Davis <matt@mattdavis.co.uk>
2017-11-03 10:25:34 +0000

    Update ThingsMacHelper to 3.9 (#40278)
    * Update ThingsMacHelper
    
    * homepage

--- Casks/thingsmacsandboxhelper.rb
+++ Casks/thingsmacsandboxhelper.rb
@@ -1,11 +1,11 @@
 cask 'thingsmacsandboxhelper' do
-  version '3.5'
-  sha256 '5df5902a0e28ebd1da5b6b02776ae78fb16ff69fb9a644841884cff8d6ce8cbd'
+  version '3.9'
+  sha256 '844273f52b848b8417d72de2ab3bc1a86beadf0c986b706ac2d09087ab0585aa'
 
   # culturedcode.cachefly.net was verified as official when first introduced to the cask
   url "https://culturedcode.cachefly.net/things/thingssandboxhelper/#{version}/ThingsHelper.zip"
   name 'ThingsMacSandboxHelper'
-  homepage 'https://culturedcode.com/things/mac/help/things-sandboxing-helper-download/'
+  homepage "https://culturedcode.com/things/mac/help/things-sandboxing-helper-things#{version.major}/"
 
   app 'ThingsMacSandboxHelper.app'
 end
