7d763579a1402e5bbcb39c999aebc930c8058b2e
Victor Popkov <victor@popkov.me>
2017-11-03 23:56:49 +0200

    Update mailbutler to 6901

--- Casks/mailbutler.rb
+++ Casks/mailbutler.rb
@@ -1,11 +1,11 @@
 cask 'mailbutler' do
-  version '6888'
-  sha256 'cdeba09c32e317681d0d15cbd0a51e096ab986f58a533bcfeea4ce29f3ebd981'
+  version '6901'
+  sha256 'aac6a940f478dff83bceafd6c28b1d869ad2e487152f623bda884b320c42016e'
 
   # mailbutler-io.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://mailbutler-io.s3.amazonaws.com/files/MailButler_#{version}.zip"
   appcast 'https://www.feingeist.io/fg-library/appcast.php?appName=MailButler',
-          checkpoint: '7aa96dc82458b21a8738e43c190a9d54ee9bc66f5a23a979ca5b497b930c0fcb'
+          checkpoint: 'ecfbf62ce1cb764869b4de948ea80c322070a8eb7903abb9e73e51b06b7402fa'
   name 'MailButler'
   homepage 'https://www.mailbutler.io/'
 
