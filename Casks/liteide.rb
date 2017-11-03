18f39f0fc7862243e01e4659b5528ce198bf78fe
commitay <commitay@users.noreply.github.com>
2017-11-03 10:48:02 +1000

    liteide

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -2,12 +2,12 @@ cask 'liteide' do
   version '33'
   sha256 '76f87d67599a787676c8cf65284009b20f957d39a13f203a486bba0f6a079b1a'
 
-  # downloads.sourceforge.net/liteide/liteidex was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/liteide was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/liteide/liteidex#{version}.macosx-qt5.zip"
-  appcast 'https://github.com/visualfc/liteide/releases.atom',
-          checkpoint: '9cfb38e688538cc323b96ba3dc99569f7a32b30f0759f532e64f02fecccc3245'
+  appcast 'https://sourceforge.net/projects/liteide/rss',
+          checkpoint: '6b4b8a37bfb57c6b71abaa22af6c70baa1d0630132ea6e67046a360e1422c207'
   name 'LiteIDE'
-  homepage 'https://github.com/visualfc/liteide'
+  homepage 'http://liteide.org/'
 
   app 'liteide/LiteIDE.app'
 end
