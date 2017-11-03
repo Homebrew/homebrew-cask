c12d17cb24252e7f53acadcee8f0c85a5e7b0e0c
Maxime Gervais <gervais.maxime@gmail.com>
2017-11-03 03:27:37 +0100

    Update mediainfo to 17.10 (#40263)
    * Update mediainfo to 17.10
    
    * checkpoint

--- Casks/mediainfo.rb
+++ Casks/mediainfo.rb
@@ -1,10 +1,10 @@
 cask 'mediainfo' do
-  version '0.7.99'
-  sha256 '34650dd22250e698d2c8c0468f68fe75380594c9cedc58ce4651ef108506df1f'
+  version '17.10'
+  sha256 'ac54926c6997d613d91daf8ded8779197e19331bcd7759dd2053418a0523b9f1'
 
   url "https://mediaarea.net/download/binary/mediainfo-gui/#{version}/MediaInfo_GUI_#{version}_Mac.dmg"
   appcast 'https://mediaarea.net/rss/mediainfo_updates.xml',
-          checkpoint: '93299a75547fe7a09abc95044b0eccd9615084f98b3359e604154fc4e24f0dc0'
+          checkpoint: 'e3ddd73ca6d2ff285dc82f91e307a266a315bd666e971df69ccc5af0d317d0c6'
   name 'MediaInfo'
   homepage 'https://mediaarea.net/en/MediaInfo'
 
