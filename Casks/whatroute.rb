5dcec3556de5ca9b1e00936ec45e31c2dedd0ac1
commitay <commitay@users.noreply.github.com>
2017-11-04 05:31:08 +1000

    Update whatroute to 2.0.24

--- Casks/whatroute.rb
+++ Casks/whatroute.rb
@@ -1,10 +1,10 @@
 cask 'whatroute' do
-  version '2.0.23'
-  sha256 'd974b0b238ac13c1e7f8bababc9dcff9e000b29e458446e540caeeab01c3a563'
+  version '2.0.24'
+  sha256 '6fd7ce7fe4d781664a25fea46aed0b30d6bf93c2944649f92d02060cf3db7d99'
 
   url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
   appcast "https://www.whatroute.net/whatroute#{version.major}appcast.xml",
-          checkpoint: '788527df9815494a83baaf328bb5c36f3eb0f06b54fb108b8af52994658cb504'
+          checkpoint: '4341713df452487d23c6e8c68aaec3424e4b675a642b68d79947e03a2130691f'
   name 'WhatRoute'
   homepage 'https://www.whatroute.net/'
 
