7539cb878602c3d31678e6fd57b2bb437ac8f508
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2017-11-03 20:23:51 +0300

    Update studio-3t to 5.6.3 (#40288)

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,10 +1,10 @@
 cask 'studio-3t' do
-  version '5.6.2'
-  sha256 '09c02e48627244941dff791f5e77a32723691f1b54889b35fc53890147f78c7f'
+  version '5.6.3'
+  sha256 '46e944f9800f5a8a47efc8adb3f14de6619abf07aabb495c8489a509b7736b62'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'http://files.studio3t.com/changelog/changelog.txt',
-          checkpoint: '7a41ead4099736f2eec72fcf8728c02d764dee91122f796d69fc8969b9bbb1f3'
+          checkpoint: '038f68111ad92ade55341348e691ef5515fac96e6730768455835e1b83da5af1'
   name 'Studio 3T'
   homepage 'https://studio3t.com/'
 
