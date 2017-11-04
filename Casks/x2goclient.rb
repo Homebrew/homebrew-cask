139de562a5dc475d02a58a153e27073d8b8b6840
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2017-11-04 15:26:38 +0100

    Update x2goclient to 4.1.1.0

--- Casks/x2goclient.rb
+++ Casks/x2goclient.rb
@@ -2,13 +2,13 @@ cask 'x2goclient' do
   version '4.1.1.0'
 
   if MacOS.version <= :mavericks
-    sha256 '0502fb19866824d4df6c7f75623f07a74202cb13143f6041df30457453f9100e'
+    sha256 '459ac9363a32601b078c43586e0c5b6a42f3f6eb98947877ef7eacae8fc3181c'
     url "https://code.x2go.org/releases/binary-macosx/x2goclient/releases/#{version}/x2goclient-#{version}.20171029_OSX_10_9.dmg"
   elsif MacOS.version == :yosemite || MacOS.version == :el_capitan
-    sha256 'daa29f7cb697ce04e70ee0401978e2318ff979d97cb0035f12dce4dc061a57c1'
+    sha256 'a852dbcd134a30f0d442001707982b7fc11c64d5fed5813feec20d06de274301'
     url "https://code.x2go.org/releases/binary-macosx/x2goclient/releases/#{version}/x2goclient-#{version}.20171029.OSX_10_10.dmg"
   elsif MacOS.version == :sierra
-    sha256 '4b35b9002f5a328c98dab213abade2d5a176c6c74efaead9e54987e408a290dd'
+    sha256 'e6e2c4384218711d6b4480c8c9f8f44a726930a594f4c15431ecbd2092f77cb4'
     url "https://code.x2go.org/releases/binary-macosx/x2goclient/releases/#{version}/x2goclient-#{version}.20171029.OSX_10_12.dmg"
   end
 
