7c9a66cc6f10258171e18f6db7acbc62d035dc61
Victor Popkov <victor@popkov.me>
2017-11-04 03:13:47 +0200

    Update drawio to 7.6.6 (#40306)

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -1,11 +1,11 @@
 cask 'drawio' do
-  version '7.4.3'
-  sha256 '6daa970645482c0dac22ec2f68a5d2f718fea616fbf88f6d3c0b63477f679ecc'
+  version '7.6.6'
+  sha256 'a58307cfd1f39b909bbf96347f3f81e6d875706af6e0576bcb694aea75ba5f13'
 
   # github.com/jgraph/drawio-desktop was verified as official when first introduced to the cask
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{version}.dmg"
   appcast 'https://github.com/jgraph/drawio-desktop/releases.atom',
-          checkpoint: 'dbc3397fe4e0ab0681f177fac97dbee5c32f57639b576682c842ff2b3cee7434'
+          checkpoint: '60dcab7df088afce24739de76e8cf9838bd61e4aa808b472f0ed65cac5004082'
   name 'draw.io Desktop'
   homepage 'https://www.draw.io/'
 
