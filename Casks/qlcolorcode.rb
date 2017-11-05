05afc32bef5cdedc8e9caab4b71f297d63039fb5
linbo0518 <linbo0518@gmail.com>
2017-11-05 16:49:50 +0800

    Update qlcolorcode to 2.0.9

--- Casks/qlcolorcode.rb
+++ Casks/qlcolorcode.rb
@@ -1,10 +1,10 @@
 cask 'qlcolorcode' do
-  version '2.0.8'
-  sha256 '598da4c881750943687b846a01a72cbaa2a91054db71d34bb4e7c8ffe4be9ac5'
+  version '2.0.9'
+  sha256 '2b373ea51148f43a0b7e7feafcf2a5422692d7a03e787afdd3692ec4341bf33f'
 
   url "https://github.com/anthonygelibert/QLColorCode/releases/download/release-#{version}/QLColorCode.qlgenerator.zip"
   appcast 'https://github.com/anthonygelibert/QLColorCode/releases.atom',
-          checkpoint: '0f3ff11b8f32c32869669d669f3386e8a8de60a9a68278130af75a1e68274d67'
+          checkpoint: '59313b59f78db19be800a35d199cc98729d9bf2650f14606a21ec517451ce745'
   name 'QLColorCode'
   homepage 'https://github.com/anthonygelibert/QLColorCode'
 
