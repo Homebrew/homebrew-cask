978ae459fadb3efaad18576fe0d973b46e1f5578
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-11-05 15:02:47 +0000

    Update tankix to 6951

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version '6729'
-  sha256 '7f147946658655acf44b91d6d2d8a78a1886cb1b2c8e708f29517985993df787'
+  version '6951'
+  sha256 'e360d3fc5d0799c1d281711bfd86c543a7ce2c7312a94f56261a6960210b3e9a'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/master-#{version}/TankiX.dmg"
   name 'Tanki X'
