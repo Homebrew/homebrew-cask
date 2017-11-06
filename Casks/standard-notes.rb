3dd04de26315964121713cb66225e58793ebf602
tgdnt <tiagodonato@gmail.com>
2017-11-06 16:29:51 -0600

    Added more files to zap

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -13,9 +13,14 @@ cask 'standard-notes' do
 
   app 'Standard Notes.app'
 
-  zap trash: [
-               '~/Library/Application Support/Standard Notes',
-               '~/Library/Preferences/org.standardnotes.standardnotes.plist',
-               '~/Library/Preferences/org.standardnotes.standardnotes.helper.plist',
-             ]
+  zap delete: [
+                '~/Library/Caches/org.standardnotes.standardnotes',
+                '~/Library/Caches/org.standardnotes.standardnotes.ShipIt',
+                '~/Library/Saved Application State/org.standardnotes.standardnotes.savedState',
+              ],
+      trash:  [
+                '~/Library/Application Support/Standard Notes',
+                '~/Library/Preferences/org.standardnotes.standardnotes.plist',
+                '~/Library/Preferences/org.standardnotes.standardnotes.helper.plist',
+              ]
 end
