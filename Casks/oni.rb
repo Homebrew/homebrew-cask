242c8b76427dac4414e0c4b910e64fffef1f7814
commitay <commitay@users.noreply.github.com>
2017-11-06 14:39:04 +1000

    homepage

--- Casks/oni.rb
+++ Casks/oni.rb
@@ -2,11 +2,12 @@ cask 'oni' do
   version '0.2.14'
   sha256 'd9097e43f0953b692acd17349b4302968d55b32722f7a5cf44631bf984046d3e'
 
+  # github.com/onivim/oni was verified as official when first introduced to the cask
   url "https://github.com/onivim/oni/releases/download/v#{version}/Oni-#{version}-osx.dmg"
   appcast 'https://github.com/onivim/oni/releases.atom',
           checkpoint: '4f6abd3776bcc171ce2511bd4a8146d6e6408611576a9f33e35ab5d312b7db2c'
   name 'Oni'
-  homepage 'https://github.com/onivim/oni'
+  homepage 'https://www.onivim.io/'
 
   app 'Oni.app'
 
