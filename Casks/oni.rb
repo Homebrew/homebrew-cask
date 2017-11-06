fe8061b26d8a55d6452e5cc48260d5c130a927b0
Alex Yeung <foray1010@users.noreply.github.com>
2017-11-06 20:32:49 +0800

    Update oni to 0.2.14 (#40370)
    * Update oni to 0.2.14
    
    * homepage

--- Casks/oni.rb
+++ Casks/oni.rb
@@ -1,12 +1,13 @@
 cask 'oni' do
-  version '0.2.8'
-  sha256 '6a0fed2f9b618b4d4c0be4e268e3f6255adc07b98e2fdd51387eb123d0b1382e'
+  version '0.2.14'
+  sha256 'd9097e43f0953b692acd17349b4302968d55b32722f7a5cf44631bf984046d3e'
 
-  url "https://github.com/extr0py/oni/releases/download/v#{version}/Oni-#{version}-osx.dmg"
-  appcast 'https://github.com/extr0py/oni/releases.atom',
-          checkpoint: 'a75cba06d24854eaf8e5da42e7c13c87849cbacd1816e766c40524eb26460eb9'
+  # github.com/onivim/oni was verified as official when first introduced to the cask
+  url "https://github.com/onivim/oni/releases/download/v#{version}/Oni-#{version}-osx.dmg"
+  appcast 'https://github.com/onivim/oni/releases.atom',
+          checkpoint: '4f6abd3776bcc171ce2511bd4a8146d6e6408611576a9f33e35ab5d312b7db2c'
   name 'Oni'
-  homepage 'https://github.com/extr0py/oni'
+  homepage 'https://www.onivim.io/'
 
   app 'Oni.app'
 
