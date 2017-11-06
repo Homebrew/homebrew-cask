fce94ecdf1d0c1ad17891dadefac476cbb2d3770
tgdnt <tiagodonato@gmail.com>
2017-11-05 19:27:30 -0600

    Fix standard notes url and make it autoupdating (#40367)
    * Fix Standard Notes package URL
    
    See https://github.com/standardnotes/desktop/releases for file naming standard.
    
    * Sets auto_updates to true for Standard Notes
    
    Standard Notes autoupdates, so should never be listed by `brew cask outdated`

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -3,11 +3,13 @@ cask 'standard-notes' do
   sha256 '99b5146b0b26a99bf261e478512070315fe6b16c4e8a2be778ec1479fdfe567c'
 
   # github.com/standardnotes/desktop was verified as official when first introduced to the cask
-  url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard.Notes-#{version}-mac.zip"
+  url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac.zip"
   appcast 'https://github.com/standardnotes/desktop/releases.atom',
           checkpoint: '361cefecd86033b616fbd32f06ccf6d9b1616d2a7c6a953330e29c86dafedd05'
   name 'Standard Notes'
   homepage 'https://standardnotes.org/'
+
+  auto_updates true
 
   app 'Standard Notes.app'
 end
