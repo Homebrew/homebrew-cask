db721801247bf9d0971154a64a90ccb2ac2bc7a9
Josh Brown <josh@brownj.org>
2017-11-05 18:42:32 -0600

    Update hash again

--- Casks/borgbackup.rb
+++ Casks/borgbackup.rb
@@ -1,6 +1,6 @@
 cask 'borgbackup' do
   version '1.1.2'
-  sha256 '0591e2e2ef86207649efa5dbf5307eaf50a47c6c14273d80ab311f789da4a089'
+  sha256 'ac858cb8819826bb428639359b20c7504924c26dbab760cd1c5bf54adf93ad6d'
 
   # github.com/borgbackup/borg was verified as official when first introduced to the cask
   url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
