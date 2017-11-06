c843c0e11c6bb4970c1e376f3914c7d6506dc0b1
Josh Brown <josh@brownj.org>
2017-11-05 18:46:15 -0600

    Update hash for appcast

--- Casks/borgbackup.rb
+++ Casks/borgbackup.rb
@@ -5,7 +5,7 @@ cask 'borgbackup' do
   # github.com/borgbackup/borg was verified as official when first introduced to the cask
   url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
   appcast 'https://github.com/borgbackup/borg/releases.atom',
-          checkpoint: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+          checkpoint: '0591e2e2ef86207649efa5dbf5307eaf50a47c6c14273d80ab311f789da4a089'
   name 'BorgBackup'
   homepage 'https://borgbackup.readthedocs.io/en/stable/'
   gpg "#{url}.asc", key_id: '51F78E01'
