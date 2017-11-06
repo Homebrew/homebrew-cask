0bab16ef54d87220b90b32f70f6503792f590472
Frances Coronel <hello@fvcproductions.com>
2017-11-06 13:52:32 -0800

    Style fix
    Signed-off-by: Frances Coronel <hello@fvcproductions.com>

--- Casks/keep.rb
+++ Casks/keep.rb
@@ -4,7 +4,7 @@ cask 'keep' do
 
   url "https://github.com/tmcinerney/keep/releases/download/v#{version}/keep.v#{version}.zip"
   appcast 'https://github.com/tmcinerney/keep/releases.atom',
-        checkpoint: 'dfbb63243f9d2c22b82e0b9fca0b5c0c0956be55feb19e77b341d0de3662f0a7'
+          checkpoint: 'dfbb63243f9d2c22b82e0b9fca0b5c0c0956be55feb19e77b341d0de3662f0a7'
   name 'Keep'
   homepage 'https://github.com/tmcinerney/keep/'
 
