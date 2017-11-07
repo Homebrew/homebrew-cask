dd0d9357ce280353267e0eef3157804bc2bd9526
Davee Nguyen <davee@daveenguyen.com>
2017-11-06 21:30:51 -0800

    Update ableton-live-intro to 9.7.5 (#40416)

--- Casks/ableton-live-intro.rb
+++ Casks/ableton-live-intro.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-intro' do
-  version '9.7.4'
-  sha256 '7062febe6ac590b48a936de876f08fb7e91b38a14016c5dd885a710991eb21f0'
+  version '9.7.5'
+  sha256 'ee0044f5651a391b04972f67ffd8fd34dc972f84d76777eb2299ced6e3573030'
 
   url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_64.dmg"
   name 'Ableton Live Intro'
