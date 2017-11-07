17170b7593c5cf2bf4d6166a2e86eaea14cc2ab4
miccal <miccal.matthews@gmail.com>
2017-11-07 09:10:12 +0800

    Update atom to 1.21.2

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -18,15 +18,17 @@ cask 'atom' do
 
   zap delete: [
                 '~/.atom',
+                '~/Library/Caches/com.github.atom',
+                '~/Library/Caches/com.github.atom.ShipIt',
+                '~/Library/Saved Application State/com.github.atom.savedState',
+              ],
+      trash:  [
                 '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.atom.sfl',
                 '~/Library/Application Support/ShipIt_stderr.log',
                 '~/Library/Application Support/Atom',
                 '~/Library/Application Support/ShipIt_stdout.log',
                 '~/Library/Application Support/com.github.atom.ShipIt',
-                '~/Library/Caches/com.github.atom',
-                '~/Library/Caches/com.github.atom.ShipIt',
                 '~/Library/Preferences/com.github.atom.helper.plist',
                 '~/Library/Preferences/com.github.atom.plist',
-                '~/Library/Saved Application State/com.github.atom.savedState',
               ]
 end
