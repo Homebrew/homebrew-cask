f0ce901bd37e802b5ab1524499b02474984098d4
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-11-07 23:54:40 +0100

    Update atom to 1.22.0 (#40433)

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,11 +1,11 @@
 cask 'atom' do
-  version '1.21.2'
-  sha256 '08f44468da53255001f7226b135620e507defa12aa608ab721e2324b3f1078b5'
+  version '1.22.0'
+  sha256 '47c4fb607510aa2da19995c671195180bb78704552b4dd34385a0dc029c94780'
 
   # github.com/atom/atom was verified as official when first introduced to the cask
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
   appcast 'https://github.com/atom/atom/releases.atom',
-          checkpoint: 'e61deb63c36f6a191a071f77993c18e3236502f872d0bdcaee4a3a80833b5947'
+          checkpoint: '5d91fb8b10735eaf9660d884117e7032dfb7adc210d40a47312b0087b7a91811'
   name 'Github Atom'
   homepage 'https://atom.io/'
 
