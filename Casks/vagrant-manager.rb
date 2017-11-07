2323841eda1d57cc8aa0b6d5d3cb92f2f3546f2b
Matthew Davis <matt@mattdavis.co.uk>
2017-11-07 01:43:17 +0000

    Upgrade vagrant-manager to 2.6.0 (#40394)
    * Upgrade vagrant-manager to 2.6.0
    
    * uninstall

--- Casks/vagrant-manager.rb
+++ Casks/vagrant-manager.rb
@@ -1,15 +1,18 @@
 cask 'vagrant-manager' do
-  version '2.5.4'
-  sha256 '9ad9d9f5d6eca2ef0f4493004f06acc6862701b1b731fe20ddbc7c5970079824'
+  version '2.6.0'
+  sha256 'ed9ad3f1b6a97e82c959812b0779036946903630627914b02684d53ad71b1052'
 
   # github.com/lanayotech/vagrant-manager was verified as official when first introduced to the cask
   url "https://github.com/lanayotech/vagrant-manager/releases/download/#{version}/vagrant-manager-#{version}.dmg"
   appcast 'https://github.com/lanayotech/vagrant-manager/releases.atom',
-          checkpoint: 'a6d3c3340cdfba7d45050e104530e7ad480d378975bf8217b8daa9863d92da9e'
+          checkpoint: 'bb3f4660861c88890122d0a388d847c54501de7109967d2f63484b3865a351d4'
   name 'Vagrant Manager'
   homepage 'http://vagrantmanager.com/'
 
   app 'Vagrant Manager.app'
+
+  uninstall login_item: 'Vagrant Manager',
+            quit:       'lanayo.Vagrant-Manager'
 
   zap delete: '~/Library/Caches/lanayo.Vagrant-Manager',
       trash:  '~/Library/Preferences/lanayo.Vagrant-Manager.plist'
