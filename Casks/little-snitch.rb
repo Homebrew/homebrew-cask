cc06b7de1b248139aa64fc62aee4daa3bb60d39d
miccal <miccal.matthews@gmail.com>
2017-11-07 09:41:18 +0800

    Update little-snitch to 4.0.3

--- Casks/little-snitch.rb
+++ Casks/little-snitch.rb
@@ -4,7 +4,7 @@ cask 'little-snitch' do
 
   url "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-#{version}.dmg"
   appcast 'https://www.obdev.at/products/littlesnitch/releasenotes.html',
-          checkpoint: 'e22f14013a36a533eda2a657a92867edc70451365c0343ec9024c91affeba9e4'
+          checkpoint: 'ca02c011efa7e8899f3a817d31024ecfdc8daf0d11c53c2feef66a95acdfc88b'
   name 'Little Snitch'
   homepage 'https://www.obdev.at/products/littlesnitch/index.html'
 
@@ -22,7 +22,6 @@ cask 'little-snitch' do
   zap delete: [
                 '/Library/Application Support/Objective Development/Little Snitch',
                 '/Library/Logs/LittleSnitchDaemon.log',
-                '~/Library/Application Support/Little Snitch',
                 '~/Library/Caches/at.obdev.LittleSnitchAgent',
                 '~/Library/Caches/at.obdev.LittleSnitchConfiguration',
                 '~/Library/Caches/at.obdev.LittleSnitchSoftwareUpdate',
@@ -34,6 +33,7 @@ cask 'little-snitch' do
                 '~/Library/Saved Application State/at.obdev.LittleSnitchInstaller.savedState',
               ],
       trash:  [
+                '~/Library/Application Support/Little Snitch',
                 '~/Library/Preferences/at.obdev.LittleSnitchAgent.plist',
                 '~/Library/Preferences/at.obdev.LittleSnitchConfiguration.plist',
                 '~/Library/Preferences/at.obdev.LittleSnitchInstaller.plist',
