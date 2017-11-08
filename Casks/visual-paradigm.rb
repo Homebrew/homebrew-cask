3fbc0be25677b8bbd582e4638d43a3004eac1fa9
Karl R. Wurst <karl@w-sts.com>
2017-11-08 10:06:45 -0500

    Update visual-paradigm to 14.2,20171103

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm' do
-  version '14.2,20171007'
-  sha256 'e7d98557576cb40fbf41f5a334d8d87709cc905017600330df6af879e899acd9'
+  version '14.2,20171103'
+  sha256 '9aa65a0b93b09027ff56715093f2f4a5a467697e6f26baf7775c86cb472ba84d'
 
   url "https://eu6.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.dots_to_underscores.gsub(',', '_')}_OSX_WithJRE.dmg"
   name 'Visual Paradigm'
