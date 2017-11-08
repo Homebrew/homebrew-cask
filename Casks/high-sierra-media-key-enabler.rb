2218105aaf587b878e0f231c360dd4e0d52dc048
Forrest Parker <forrestwparker@gmail.com>
2017-11-08 13:54:23 -0800

    Add high-sierra-media-key-enabler.app

---
+++ Casks/high-sierra-media-key-enabler.rb
@@ -0,0 +1,10 @@
+cask 'high-sierra-media-key-enabler' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://milgra.com/downloads/HighSierraMediaKeyEnabler.zip'
+  name 'High Sierra Media Key Enabler'
+  homepage 'http://milgra.com/high-sierra-media-key-enabler.html'
+
+  app 'HighSierraMediaKeyEnabler.app'
+end
