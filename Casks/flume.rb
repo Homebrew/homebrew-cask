1d97112eb76ee33db23d3771694e474151d64e19
commitay <commitay@users.noreply.github.com>
2017-11-08 14:24:04 +1000

    Update flume to 2.8.2

--- Casks/flume.rb
+++ Casks/flume.rb
@@ -1,10 +1,10 @@
 cask 'flume' do
-  version '2.8'
-  sha256 '348888606a8356a1d1644564ea55adc4289fea29818b26f584e4bc172b75cc5b'
+  version '2.8.2'
+  sha256 '8fee477756acca8abdb2fb615d22d05ddaf812b993aca5c29fa9ab2e39109147'
 
   url "https://flumeapp.com/files/Flume-#{version}.zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/c88c56b02dcd4dd3acceb6d7a24f7122',
-          checkpoint: '2c243d37571ce93562a815c28e993a840031b33f698253e1e04e7915c4ff8b9f'
+          checkpoint: 'a5e5d44a7bf0bd72a1474088dc89fef61e51b95f290aab0706d8c788a02924a1'
   name 'Flume'
   homepage 'https://flumeapp.com/'
 
