b33208b7ca10028cc289a231a8b956082dfbc0b0
Nine Points Solutions <NinePts@users.noreply.github.com>
2017-11-07 19:43:46 -0500

    update ctivo.rb to Release 3.0 (#40436)

--- Casks/ctivo.rb
+++ Casks/ctivo.rb
@@ -1,10 +1,10 @@
 cask 'ctivo' do
-  version '2.5.1'
-  sha256 '8ebef32944bcf98b7912d66b20cbc04f18386106203c1959c0e2089632d8d06d'
+  version '3.0Release'
+  sha256 '8fc7b51ba71530455c6aa54363706dad655d0d612e3fbad755ebb9bc08600b01'
 
   url "https://github.com/dscottbuch/cTiVo/releases/download/#{version}/cTiVo.zip"
   appcast 'https://github.com/dscottbuch/cTiVo/releases.atom',
-          checkpoint: '8ee3a8d13c36610f7256fc859799193fee6a16ae417f04be1a5ec850d657bc2c'
+          checkpoint: 'ee2f89f1a94d4c2740bd974385a1679a2d8aab5f9678120ae3e5b7cc7020c95c'
   name 'cTiVo'
   homepage 'https://github.com/dscottbuch/cTiVo'
 
