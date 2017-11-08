d50ac4dbea67f22ad6f1379a6b1873403f2ed22a
Roman Zhuzha <sanyer@users.noreply.github.com>
2017-11-08 11:44:34 +0100

    Update pomodone to 1.5.1163 (#40470)

--- Casks/pomodone.rb
+++ Casks/pomodone.rb
@@ -1,6 +1,6 @@
 cask 'pomodone' do
-  version '1.5.1126'
-  sha256 'd9f3131a2a7172b9842672acb36d694f7c552b3be75488f539169f29763f933d'
+  version '1.5.1163'
+  sha256 '476d22afec5932be416792e4ced8d8b7407607ccaf0efaef2329efd513785a36'
 
   url "https://app.pomodoneapp.com/installers/PomoDoneApp-#{version}.dmg"
   name 'PomoDone'
