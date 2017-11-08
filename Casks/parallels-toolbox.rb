fe5d3aedb9c01d7cf41df3371f2a7eab97d0c812
commitay <commitay@users.noreply.github.com>
2017-11-09 09:14:17 +1000

    parallels-toolbox

--- Casks/parallels-toolbox.rb
+++ Casks/parallels-toolbox.rb
@@ -2,16 +2,23 @@ cask 'parallels-toolbox' do
   version '2.0.2-1213'
   sha256 '6c11c2bae46dbe7138cf48a921ca595bedec91b977153ea1908484af36d0d0eb'
 
-  url 'https://www.parallels.com/try/toolbox/'
+  url "https://download.parallels.com/toolbox/v#{version.major}/#{version}/ParallelsToolbox-#{version}.dmg"
   name 'Paralells Toolbox'
   homepage 'https://www.parallels.com/products/toolbox/'
 
-  installer script: "#{staged_path}/Install Parallels Toolbox.app/Contents/MacOS/Parallels Toolbox"
+  installer manual: 'Install Parallels Toolbox.app'
 
-  uninstall quit:   [
-                      'com.parallels.toolbox',
-                    ],
-            delete: '/Applications/Parallels Access.app'
+  uninstall delete: '/Applications/Parallels Toolbox.app',
+            signal: ['TERM', 'com.parallels.toolbox']
 
-  zap delete: '~/Library/Group Containers/4C6364ACXT.com.parallels.toolbox'
+  zap delete: [
+                '~/Library/Group Containers/*.com.parallels.toolbox',
+                '~/Library/Logs/prl.toolbox.ceplog.log',
+                '~/Library/Logs/ParallelsToolbox',
+                '~/Library/Logs/prl.toolbox.ceplog.xml',
+              ],
+      trash:  [
+                '~/Library/Application Support/com.parallels.toolbox',
+                '~/Library/Preferences/com.parallels.toolbox.plist',
+              ]
 end
