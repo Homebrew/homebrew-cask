1b2550c0b5b4a0f8c32285e61fe9255f67f18124
commitay <commitay@users.noreply.github.com>
2017-11-09 09:22:43 +1000

    Update parallels-toolbox.rb

--- Casks/parallels-toolbox.rb
+++ Casks/parallels-toolbox.rb
@@ -3,7 +3,7 @@ cask 'parallels-toolbox' do
   sha256 '6c11c2bae46dbe7138cf48a921ca595bedec91b977153ea1908484af36d0d0eb'
 
   url "https://download.parallels.com/toolbox/v#{version.major}/#{version}/ParallelsToolbox-#{version}.dmg"
-  name 'Paralells Toolbox'
+  name 'Parallels Toolbox'
   homepage 'https://www.parallels.com/products/toolbox/'
 
   installer manual: 'Install Parallels Toolbox.app'
