4eabcdc69481fdb20def579750c76aef9b95dde9
Pelle ten Cate <pelle@occsn.com>
2017-11-08 09:30:12 +0100

    Update bartender to 3.0.27

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -3,8 +3,8 @@ cask 'bartender' do
     version '2.1.6'
     sha256 '013bb1f5dcc29ff1ecbc341da96b6e399dc3c85fc95bd8c7bee153ab0d8756f5'
   else
-    version '3.0.25'
-    sha256 '7011df179316135454896d37c7b1916e15bfb6cc834311b023c1e6d3c444f63c'
+    version '3.0.27'
+    sha256 'd7d2634a2f5723b35bc5fac1b831cc68197839593a592153ef0b0316e9f0b3b6'
   end
 
   url "https://macbartender.com/B2/updates/#{version.dots_to_hyphens}/Bartender%20#{version.major}.zip",
