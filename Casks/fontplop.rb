7b68c2e0ceead4139ad5d419a5e974e2e29cae41
Christian Kaisermann <christian@kaisermann.me>
2017-11-08 10:59:43 -0200

    Fix cask style

--- Casks/fontplop.rb
+++ Casks/fontplop.rb
@@ -3,9 +3,9 @@ cask 'fontplop' do
   sha256 'a98f595d8687eed977ad64aa1d924aa309dd97b2bfcc0ed7be889963ecf2d569'
 
   # github.com/matthewgonzalez/fontplop was verified as official when first introduced to the cask
-  url 'https://github.com/matthewgonzalez/fontplop/releases/download/v#{version}/fontplop-#{version}.dmg'
- appcast 'https://github.com/matthewgonzalez/fontplop/releases.atom', 
-         checkpoint: '4af6f6880c06a7c149b34b1a3752e55b085ef0efdf8dc8b9fa3b6a177f4c60bd' 
+  url "https://github.com/matthewgonzalez/fontplop/releases/download/v#{version}/fontplop-#{version}.dmg"
+  appcast 'https://github.com/matthewgonzalez/fontplop/releases.atom',
+          checkpoint: '4af6f6880c06a7c149b34b1a3752e55b085ef0efdf8dc8b9fa3b6a177f4c60bd'
   name 'Fontplop'
   homepage 'http://www.fontplop.com/'
 
