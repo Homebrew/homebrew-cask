79f79eb56029aa0403f1248dfc9e828ed5648a92
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2017-11-08 09:58:26 +0100

    Cardhop 1.0.2

--- Casks/cardhop.rb
+++ Casks/cardhop.rb
@@ -1,10 +1,10 @@
 cask 'cardhop' do
-  version '1.0.1'
-  sha256 '27a8a9cccd6063c1637ab3ce33a3046bbbe4d2a9bcbbc48a0e802f007f0ed4b3'
+  version '1.0.2'
+  sha256 '047082ca78af346c2aac62a4cdd96cc16a3c5e04fc4344d6dc350513787aff1c'
 
   url "http://cdn.flexibits.com/Cardhop_#{version}.zip"
   appcast 'https://flexibits.com/cardhop/appcast.php',
-          checkpoint: '9c44d66cba244033acf635818bdbdf734ec8016c26654520203d13f40b763747'
+          checkpoint: '9da1775e9aef37ac95866f98f5046a477aac840ee68cfb9f7cb246f66aa18c50'
   name 'Cardhop'
   homepage 'https://flexibits.com/cardhop'
 
