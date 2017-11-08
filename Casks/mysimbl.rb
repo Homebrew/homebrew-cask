1a1e16f35cd1ce7b9d0be4a110609b765220ac78
commitay <commitay@users.noreply.github.com>
2017-11-09 08:53:04 +1000

    url / appcast

--- Casks/mysimbl.rb
+++ Casks/mysimbl.rb
@@ -1,11 +1,10 @@
 cask 'mysimbl' do
-  version '0.5.3'
+  version '0.5.3,29'
   sha256 '809930f5b3260a905aa07b5dabfeef52c6cbfff08081c2ce2b271b12f4f658b1'
 
-  # githubusercontent.com/w0lfschild/app_updates/master/mySIMBL was verified as official when first introduced to the cask
-  url 'https://github.com/w0lfschild/mySIMBL/releases/download/0.5.3/mySIMBL-29.zip'
-  appcast 'https://raw.githubusercontent.com/w0lfschild/app_updates/master/mySIMBL/appcast.xml',
-          checkpoint: '7923369a4c5ca5e6df3a493697327505a974abd0bb7b25b893a31151c7150555'
+  url "https://github.com/w0lfschild/mySIMBL/releases/download/#{version.before_comma}/mySIMBL-#{version.after_comma}.zip"
+  appcast 'https://github.com/w0lfschild/mySIMBL/releases.atom',
+          checkpoint: 'cffdd993f9a158de17b8dcfd2daf56059d8817c83748c1b93a597cf3254c46c3'
   name 'mySIMBL'
   homepage 'https://github.com/w0lfschild/mySIMBL'
 
