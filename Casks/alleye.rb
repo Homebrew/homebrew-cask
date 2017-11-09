5fb8851565d079da3b7e6db42977b904f0514355
gvost <davidjohnson1297@gmail.com>
2017-11-09 01:03:41 -0800

    Add allEye.app v0.1.2

---
+++ Casks/alleye.rb
@@ -0,0 +1,11 @@
+cask 'alleye' do
+  version '0.1.2'
+  sha256 '82eb976baad116fcacdaec50f1f3f8f24e81bdea9f917bfde31bac99e4d6a39a'
+
+  # github.com was verified as official when first introduced to the cask
+  url 'https://github.com/gvost/alleye/releases/download/0.1.2/llEye-0.1.2.dmg'
+  name 'AllEye'
+  homepage 'http://all-eye.io/'
+
+  app 'allEye.app'
+end
