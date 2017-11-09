5359d6500d29a5cc08e6a93c443c641fd9dad6de
alswl <alswlx@gmail.com>
2017-11-09 11:22:33 +0800

    bump cctalk to 0.9.3-431

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,11 +1,11 @@
 cask 'cctalk' do
-  version '0.9.1-413,2017-10-30'
-  sha256 'e4efd2662d55e272853b6e318f1ea37e79bd727c36f861f84cb722af6ef90617'
+  version '0.9.3-431,2017-11-06'
+  sha256 '586bf9b861638653b5a1fe0eb4b26cd1b90859dfb3a50cbcd386f58b8fde8cf3'
 
   # f1.ct.hjfile.cn was verified as official when first introduced to the cask
   url "http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/archive/#{version.before_comma.hyphens_to_dots}/CCtalk-#{version.before_comma}-xianghu-#{version.after_comma}.dmg"
   appcast 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/update/info.xml',
-          checkpoint: '78ba392b8622e7ebd70bac606ea67e512736df04a2afe1b376732a61f559388d'
+          checkpoint: 'a55a1c201617c41c6b392a10e8b6af87557fd42d61dd87f9fdce76e908a72a4b'
   name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
 
