d2085f1950bf3924ad1548ee424081f878b9a266
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-11-10 08:30:48 +0100

    Update whatsapp to 0.2.6968 (#40563)
    * Update whatsapp to 0.2.6968
    
    * checkpoint

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,10 +1,10 @@
 cask 'whatsapp' do
-  version '0.2.6967'
-  sha256 '2b373db87b3cf1e0c6b47883365b7cbe46ebedec9226b96054c9bf47dccf8430'
+  version '0.2.6968'
+  sha256 '3aa9086d9812d0cfa16b40a0316600544aeae66cb127df00db08a5d6576bf627'
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64',
-          checkpoint: '5be1fd07457f52aab5f19d6260fcc8bbce5cc834e22ee2ce8d94a0da0a4d8459'
+          checkpoint: '2ea3377a1dc9d694f6601845854e7a044101f8da3466024b773cecb1e128ac9e'
   name 'WhatsApp'
   homepage 'https://www.whatsapp.com/'
 
