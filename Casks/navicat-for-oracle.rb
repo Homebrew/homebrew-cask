71257f806b27d6179215e092137827de586c867d
commitay <commitay@users.noreply.github.com>
2017-11-11 08:15:05 +1000

    Update navicat-for-oracle to 12.0.17

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,10 +1,10 @@
 cask 'navicat-for-oracle' do
-  version '12.0.15'
-  sha256 '51475f5711db126ff22c30abb89db1a4a8a59a92b5261714b79e869d693e1597'
+  version '12.0.17'
+  sha256 'faa3a630916e8232f83bfcc2a286f90ea9014f858370a00699cb88ced37b865a'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-oracle-release-note',
-          checkpoint: 'a99e1fbc6fbf4e283183859812b6c4e31ef81598629930d049ddc267cde39ad2'
+          checkpoint: '32b19739cb35bd01d3271dfe541c91f0bb051760333e763a542cee21fd6ec214'
   name 'Navicat for Oracle'
   homepage 'https://www.navicat.com/products/navicat-for-oracle'
 
