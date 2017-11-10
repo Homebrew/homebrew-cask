1093bd399eaf1293bd34f997409c45acc0219930
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2017-11-10 11:16:06 +0100

    Plex Media Server 1.9.6.4429-23901a099

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,10 +1,10 @@
 cask 'plex-media-server' do
-  version '1.9.4.4325-1bf240a65'
-  sha256 '910a0fe8ea3f7b0f9d921fe18f8391669a25dd77958fae90b4b4a35effbe1225'
+  version '1.9.6.4429-23901a099'
+  sha256 '3f32ea8e0a889d0f0e042728848f65bc6e861563ca5eaffd9e05e458ac0e5eb9'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   appcast 'https://plex.tv/api/downloads/1.json',
-          checkpoint: 'aa1ed92e9d1c697c4c6194f11b9168253718b284bb8c14dbcbd7d8d2c5d30af8'
+          checkpoint: 'afd4136adaa9288b2a1957cc3d646ff0cdd1b6bf2a6963d05bf6b97fff5dff4f'
   name 'Plex Media Server'
   homepage 'https://www.plex.tv/'
 
