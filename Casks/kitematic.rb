471efe25ab25d263bed004a2c9a56ad8b2a2d634
Mark Beacom <mark@drund.com>
2017-11-09 21:30:18 -0500

    Fix bad download URL for kitematic 0.17.2 (#40527)
    * Fix bad download URL for kitematic 0.17.2
    
    * Fix appcast and sha for kitematic 0.17.2 download fix

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,11 +1,11 @@
 cask 'kitematic' do
   version '0.17.2'
-  sha256 'f16ce273d15cb3e133f6f36eb0a152de7faaad49dc904259b40d649116c7c0ba'
+  sha256 '5a7c569d96461199dfb96bbb0df40452fe71698fd624b9e36a3edfd6228dacca'
 
   # github.com/docker/kitematic was verified as official when first introduced to the cask
-  url "https://github.com/docker/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
+  url "https://github.com/docker/kitematic/releases/download/#{version}/Kitematic-#{version}-Mac.zip"
   appcast 'https://github.com/docker/kitematic/releases.atom',
-          checkpoint: '44b691b3c62380ff7e2d39424eb5bcc5070f29decaa29c3d59c7eee588983de3'
+          checkpoint: '2965b3453eca2649575ec9b6fa255345b295485111f3e8386da50a47cf81bc50'
   name 'Kitematic'
   homepage 'https://kitematic.com/'
 
