462986f053492f88ca0d9ec9d7fbbe6aa3f7c5e4
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2017-11-11 20:33:36 +0000

    Update ghdl to 0.34 (#40608)
    * Update ghdl to 0.34
    
    * pkg
    
    * pkgutil

--- Casks/ghdl.rb
+++ Casks/ghdl.rb
@@ -1,15 +1,15 @@
 cask 'ghdl' do
-  version '0.33'
-  sha256 '665f487c936be041d27a0f9d638109e63f1706cf7b3d8ceddb81db5fb2f71d0e'
+  version '0.34'
+  sha256 '7d38686e10d313a2d891bf102fe2234886a4294243d0637861b3b8e5e8ac5bd4'
 
   # github.com/tgingold/ghdl was verified as official when first introduced to the cask
-  url "https://github.com/tgingold/ghdl/releases/download/v#{version}/ghdl-#{version}-llvm-darwin13.pkg"
+  url "https://github.com/tgingold/ghdl/releases/download/v#{version}/ghdl-v#{version}-llvm-darwin13.pkg"
   appcast 'https://github.com/tgingold/ghdl/releases.atom',
-          checkpoint: '23d87b7efbb471cdd2425f9a95a4e92dccf4d9cd8bcdce4316c983f6503f312a'
+          checkpoint: '87c8f2b544234fa20b7e9d61d7afa21cc01e084e285a6b4c4799a5ee1ae46706'
   name 'GHDL'
   homepage 'http://ghdl.free.fr/'
 
-  pkg "ghdl-#{version}-llvm-darwin13.pkg"
+  pkg "ghdl-v#{version}-llvm-darwin13.pkg"
 
-  uninstall pkgutil: "com.ktz.pkg.ghdl-#{version.no_dots}-llvm-darwin13"
+  uninstall pkgutil: "com.ktz.pkg.ghdl-#{version}-llvm-darwin13"
 end
