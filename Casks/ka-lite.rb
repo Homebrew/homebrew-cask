856e22f6db671f1665d4ed7e306058b81d0856c8
commitay <commitay@users.noreply.github.com>
2017-11-11 10:10:11 +1000

    add appcast

--- Casks/ka-lite.rb
+++ Casks/ka-lite.rb
@@ -3,6 +3,8 @@ cask 'ka-lite' do
   sha256 '911746e4dcd86feac0ed3c7b88fb8bd9e96d5bac77544282e7d344d8b9e1fc4b'
 
   url "http://pantry.learningequality.org/downloads/ka-lite/#{version.major_minor}/installers/mac/KA-Lite-#{version}.dmg"
+  appcast 'https://github.com/learningequality/ka-lite/releases.atom',
+          checkpoint: 'a701693e2b43a34bde3b2ad4940af6b94e262491a414b86b57c4c81ffeeafa05'
   name 'Khan Academy Lite'
   homepage 'https://learningequality.org/ka-lite/'
 
