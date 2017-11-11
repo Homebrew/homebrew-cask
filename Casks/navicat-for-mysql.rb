7d82e614072c8a7b580fb3de9fb8c044958f3b7a
commitay <commitay@users.noreply.github.com>
2017-11-11 13:09:16 +1000

    Update 8 navicat Casks: appcast (#40597)
    * add appcast
    
    * update appcast

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -3,7 +3,7 @@ cask 'navicat-for-mysql' do
   sha256 'b119e428faaab56272cab4f1846c32794f794ca2539c6ef8a251ddee3296cae5'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
-  appcast 'https://www.navicat.com/products/navicat-for-mysql-release-note',
+  appcast 'https://www.navicat.com/en/products/navicat-for-mysql-release-note',
           checkpoint: '9c61560177490e622ee7f523e42794e78df4a1168624f98c7ab37187107e37a7'
   name 'Navicat for MySQL'
   homepage 'https://www.navicat.com/products/navicat-for-mysql'
--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -3,7 +3,7 @@ cask 'navicat-for-postgresql' do
   sha256 '23e124f2ae80778ef780507441aaa72a2d05e259152f26a3bb18749c1bf5d4a4'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
-  appcast 'https://www.navicat.com/products/navicat-for-postgresql-release-note',
+  appcast 'https://www.navicat.com/en/products/navicat-for-postgresql-release-note',
           checkpoint: '55005bb76022ef4c7ac7b8b5575716695a39b4a2474bb9cf6f26dfa9055f974e'
   name 'Navicat for PostgreSQL'
   homepage 'https://www.navicat.com/products/navicat-for-postgresql'
