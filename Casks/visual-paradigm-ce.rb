3583a93db8fd1ce6c2ed6119c1efc97d9075dff0
commitay <commitay@users.noreply.github.com>
2017-11-11 11:01:49 +1000

    Update visual-paradigm Casks: add appcast

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -3,6 +3,8 @@ cask 'visual-paradigm-ce' do
   sha256 '5d48430ea1586081c81a898885f545e17178a99959d2fd9ff633ec44bebd5302'
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vpce#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
+  appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html',
+          checkpoint: '0faa8a7e1b7d77130add1edd6d95a6795fc4a113d2583ef7d65578233309b945'
   name 'Visual Paradigm'
   homepage 'https://www.visual-paradigm.com/'
 
