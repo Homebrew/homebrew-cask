ae0df26642531fea1884861b2120a259f01c3407
commitay <commitay@users.noreply.github.com>
2017-11-11 11:39:24 +1000

    checkpoint

--- Casks/frostwire.rb
+++ Casks/frostwire.rb
@@ -5,7 +5,7 @@ cask 'frostwire' do
   # downloads.sourceforge.net/frostwire was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/frostwire/frostwire-#{version.before_comma}.dmg"
   appcast "https://sourceforge.net/projects/frostwire/rss?path=/FrostWire%20#{version.major}.x",
-          checkpoint: '6b1194f4d2e2b83acb9fc8abf737755d64d35bf7220120be9da471a4d7a44844'
+          checkpoint: 'eab124727e648e21235cb2ec584816311d41c2bacbf43292025084a75a4ef103'
   name 'FrostWire'
   homepage 'http://www.frostwire.com/'
 
