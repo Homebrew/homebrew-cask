f32f119deeff2dd3214fc71ac2134d68dce1fef1
rtdaly <rtdaly94@gmail.com>
2017-11-12 02:57:07 -0500

    Update grids to 4.5.13

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,10 +1,10 @@
 cask 'grids' do
-  version '4.5.9'
-  sha256 '0198696f2110eaab4e97001de5a88d1242572f646b17e2168af567dc88fcd458'
+  version '4.5.13'
+  sha256 '4646d63e7c50fd7530ee3a3ffc33422787047a61ec7b7ff941aafdf4c39a4b61'
 
   url "http://thegridsapp.com/bin/Grids_#{version}.dmg"
   appcast 'http://thegridsapp.com/appcast.json',
-          checkpoint: 'f844ed9e6995c41adf9cff2837b7da464cf2b18162107c572820ffaab8c13f09'
+          checkpoint: 'aec99aa1a14d32ce960266a728daa2d89ac003559a30e9946db0c90a25c621f1'
   name 'Grids'
   homepage 'http://thegridsapp.com/'
 
