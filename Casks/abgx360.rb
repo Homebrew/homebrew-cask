17ed89842746cbf6fd22e1aa63badb2603720083
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-11-12 15:47:22 +0000

    Update abgx360 to 1.0.6

--- Casks/abgx360.rb
+++ Casks/abgx360.rb
@@ -2,9 +2,9 @@ cask 'abgx360' do
   version '1.0.6'
   sha256 'b2b066d1500e6c6a64d103b2c1a9e1760be0a4e16df314b4868024c19df8c741'
 
-  url "https://abgx360.xecuter.com/dl/abgx360-#{version}.pkg"
+  url "http://abgx360.xecuter.com/dl/abgx360-#{version}.pkg"
   name 'abgx360'
-  homepage 'https://abgx360.xecuter.com/'
+  homepage 'http://abgx360.xecuter.com/'
 
   pkg "abgx360-#{version}.pkg"
 
