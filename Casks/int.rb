f29b5126d10c03e3914441ceea26eaf6a76967aa
Jon Borgonia <jon@gomagames.com>
2017-11-12 15:03:21 -0800

    Fix Improved Network Tools.app 1.0.0 styles
    - add comment for differing url

---
+++ Casks/int.rb
@@ -0,0 +1,11 @@
+cask 'int' do
+  version '1.0.0'
+  sha256 'aaaa8fc8221d922d4b5a2e6af0e5f7007e49fdbb10bb7c1b4070335743e6345a'
+
+  # github.com was verified as official when first introduced to the cask
+  url 'https://github.com/Hackbit/nko2017-nodenodenode/releases/download/1.0.0/Improved.Network.Tools-darwin-x64.zip'
+  name 'Improved Network Tools'
+  homepage 'http://nodenodenode.2017.nodeknockout.com/'
+
+  app 'Improved Network Tools-darwin-x64/Improved Network Tools.app'
+end
