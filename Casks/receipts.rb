5a17bd310f66942b9ca14c493c6801a675169732
Martin Wilhelmi <mnin@mnin.de>
2017-11-12 18:26:18 +0100

    Add Receipts v1.6.1

---
+++ Casks/receipts.rb
@@ -0,0 +1,10 @@
+cask 'receipts' do
+  version '1.6.1'
+  sha256 'dd978f2fa630bd2594d59c810bb7bf6cf2c7b0855ae2435e323a8e83598e5706'
+
+  url 'https://www.receipts-app.com/download/Receipts-1.6.1.zip'
+  name 'Receipts'
+  homepage 'https://www.receipts-app.com/'
+
+  app 'Receipts.app'
+end
