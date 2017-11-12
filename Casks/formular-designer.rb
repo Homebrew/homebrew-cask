3fbbc902d7411dd4016938dcb92d5418fa7446b8
Anver Housseini <contact@anver.ch>
2017-11-12 12:16:58 +0100

    Add Formular-Designer.app

---
+++ Casks/formular-designer.rb
@@ -0,0 +1,10 @@
+cask 'formular-designer' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.lehreroffice.ch/lo/dateien/designer/lo_designer_mac.dmg'
+  name 'Formular-Designer'
+  homepage 'https://www.lehreroffice.ch/'
+
+  app 'Formular-Designer.app'
+end
