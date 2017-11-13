16cd167dec214e9e9329c9b58258b68b96fa8888
Victor Popkov <victor@popkov.me>
2017-11-13 22:54:34 +0200

    Update optimage to 1.3.1

--- Casks/optimage.rb
+++ Casks/optimage.rb
@@ -1,10 +1,10 @@
 cask 'optimage' do
-  version '1.3.0'
-  sha256 '6b6b3d89d98253aadd29f4b20205796cca746fd2aa5d92c2459799753d3f64d9'
+  version '1.3.1'
+  sha256 '7c8ee97f67aefe167423aa7416a6900eca77312f45fbd0e2ee4a9fdcc80e8f30'
 
   url 'https://getoptimage.com/download/optimage-mac.zip'
   appcast 'https://getoptimage.com/appcast.xml',
-          checkpoint: 'fe9efe28fa212edeec16ff175fe994974426b45eda32a12a366885cbc96f65bf'
+          checkpoint: '3fa2ff66d0b31fd292b231dff48c65a9525133de4068c4e48b0c83dd22b092e5'
   name 'Optimage'
   homepage 'https://getoptimage.com/'
 
