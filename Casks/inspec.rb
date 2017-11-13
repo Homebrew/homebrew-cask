c56e592e6cffaeb05e7883576cb7c2b7430a1336
commitay <commitay@users.noreply.github.com>
2017-11-14 08:33:12 +1000

    Update inspec.rb

--- Casks/inspec.rb
+++ Casks/inspec.rb
@@ -3,9 +3,9 @@ cask 'inspec' do
   sha256 '343da0f4c68191fd5938a8847a03b0cc85ef2f634af9e7d0050f88ce77a5a295'
 
   # packages.chef.io was verified as official when first introduced to the cask
-  url "https://packages.chef.io/files/stable/inspec/#{version.major_minor_patch}/mac_os_x/10.12/inspec-#{version}.dmg"
+  url "https://packages.chef.io/files/stable/inspec/#{version.major_minor_patch}/mac_os_x/10.13/inspec-#{version}.dmg"
   appcast 'https://github.com/chef/inspec/releases.atom',
-          checkpoint: 'e18a84b8ee52e5ac2d2e0558324a6d9cb1e9cb058c9e5c2f1a6ccdddd0918d6f'
+          checkpoint: '7b6935a1db75b5087e69df4e1529aa321ee50a7b6610346569b8c0030b603cbd'
   name 'InSpec by Chef'
   homepage 'https://www.inspec.io/'
 
