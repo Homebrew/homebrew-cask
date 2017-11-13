2429f4aa6931ed816c008c1df6596b437869bb0e
commitay <commitay@users.noreply.github.com>
2017-11-13 22:59:34 +1000

    Update navicat-premium-essentials to 12.0.18 (#40662)

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,10 +1,10 @@
 cask 'navicat-premium-essentials' do
-  version '12.0.17'
-  sha256 '13b82f347b8ad408dc7fd51ba7f0368d0a8684b78e5a6326bb30336645292e6c'
+  version '12.0.18'
+  sha256 'c462680a5932a64b85c1b7d1aa47fcf2fbc46780fe65c822dbcbfd496fd19003'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://www.navicat.com/en/products/navicat-premium-release-note',
-          checkpoint: '08541a2490969b1fdb312f588c330359355e23ce3bfc441e58081cd633c54e57'
+          checkpoint: '0db83877b751bf764e06dca014856ecb89d4b249dbe270e8a6dcd4cf81cb3e79'
   name 'Navicat Premium Essentials'
   homepage 'https://navicat.com/products/navicat-essentials'
 
