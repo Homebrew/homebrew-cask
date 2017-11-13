d4c11a39797be67b1ab26759646dade87a4b341b
Colin Dunn <cgd@vikingdev.net>
2017-11-13 15:36:46 -0800

    Update insync to 1.3.22.36179

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,6 +1,6 @@
 cask 'insync' do
-  version '1.3.19.36173'
-  sha256 '5d54777fa3fc0a3be58f3beab8c6e556c8ad762085b76dff7491b5e0f6efbd40'
+  version '1.3.22.36179'
+  sha256 '9d681d3a14449a64f1035245ddc848f0a03e86d4740b150876bfcef86dc1a7b9'
 
   url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
   name 'Insync'
