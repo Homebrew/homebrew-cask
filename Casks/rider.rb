cab510ce57b3b873a6f749cae01eb869ef427bdc
jcbot <jcb@leipert.io>
2017-11-13 21:45:56 +0100

    Update rider to 2017.2.1,172.4144.1873 (#40682)

--- Casks/rider.rb
+++ Casks/rider.rb
@@ -1,10 +1,10 @@
 cask 'rider' do
-  version '2017.2,172.4144.1459'
-  sha256 'eeb6f1cb1c83eee627603d3e2b1f81898c8b62a7a608080b1c5dad83256c0f0a'
+  version '2017.2.1,172.4144.1873'
+  sha256 '1aab149520a3b5db603f6b1adf258d2cc26068b3c55dc2fffa3ab03d20d0261d'
 
   url "https://download.jetbrains.com/resharper/JetBrains.Rider-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release',
-          checkpoint: '5626181012c54ef310ee2c177753f31a1c023e4a4e8cafe5a63f4c09c5cd1b2e'
+          checkpoint: '6e2a653eebf973cdd18953e66547b0259dd96fa03189cddca8c5a68d127e0db5'
   name 'Jetbrains Rider'
   homepage 'https://www.jetbrains.com/rider/'
 
