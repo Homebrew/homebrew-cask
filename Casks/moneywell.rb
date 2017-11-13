bbb527cb43555495df4d5440259bf51bd9a8a798
Colin Dunn <cgd@vikingdev.net>
2017-11-13 14:12:07 -0800

    Update moneywell to 3.0.5

--- Casks/moneywell.rb
+++ Casks/moneywell.rb
@@ -1,10 +1,10 @@
 cask 'moneywell' do
-  version '3.0.2'
-  sha256 'eda48abebc73591780046da0b7c3a4df3816a6315b107dafc9adcfe00d40fadc'
+  version '3.0.5'
+  sha256 'c2c888281b96922c8eff50034fca7565ef485ab693b7847f51de6cbd982345ce'
 
   url "https://moneywellapp.com/mw/builds/MoneyWell-#{version}.zip"
   appcast "https://moneywellapp.com/mw/feeds/#{version.major}x/appcast.xml",
-          checkpoint: '8f72dfce40eb99857414bc51defdc10e037eb84c9f316e5c875b1bdd70bf85a0'
+          checkpoint: 'eb5ad6e67142c0ca5b6c7e9d64425e4db0cab48f8d5f066696a778977efd3b12'
   name 'MoneyWell'
   homepage 'https://moneywellapp.com/'
 
