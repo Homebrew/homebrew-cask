3937bc46ea3ca17a67fbf453f2ec326944725b87
Victor Popkov <victor@popkov.me>
2017-11-13 22:51:24 +0200

    Update narodmon to 3.0.5

--- Casks/narodmon.rb
+++ Casks/narodmon.rb
@@ -1,11 +1,11 @@
 cask 'narodmon' do
-  version '3.0.4'
-  sha256 '03818b133df820a2005118f49e2a217f2a21389ac9cee748f0a3dcfd7c701242'
+  version '3.0.5'
+  sha256 '57081d4823f607f1417b243df83aeb189cd26027e899defcd3193f8fb6d2ebb1'
 
   # github.com/niki-timofe/NarodMonOSX was verified as official when first introduced to the cask
   url "https://github.com/niki-timofe/NarodMonOSX/releases/download/v#{version}/NarodMon.zip"
   appcast 'https://github.com/niki-timofe/NarodMonOSX/releases.atom',
-          checkpoint: '84a7bc56bc17a28d35492912da1bf94c950e526bb0c4f890d95e183e10f41ae6'
+          checkpoint: '238ef00d8eda38c5bee8a7f6c5867e3300dfa385dfee8010744fa24821ae96a6'
   name 'NarodMon'
   homepage 'https://narodmon.ru/'
 
