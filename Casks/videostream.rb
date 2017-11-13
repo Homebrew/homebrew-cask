ef3861ec05c13bb78697a86228a6d1df28e66b8a
commitay <commitay@users.noreply.github.com>
2017-11-13 13:40:17 +1000

    appcast / uninstall

--- Casks/videostream.rb
+++ Casks/videostream.rb
@@ -1,18 +1,19 @@
 cask 'videostream' do
-  version :latest
-  sha256 :no_check
+  version '0.3.4'
+  sha256 'e67f26983fcddabec09b6bf922d396078af008966b7841f3619c9cb5a9b26b40'
 
   url 'https://cdn.getvideostream.com/videostream-native-updates/macOS/Videostream.pkg'
+  appcast 'https://s3.amazonaws.com/videostream-cdn/videostream-native-updates/macOS/manifest.json',
+          checkpoint: '61859793bbedafe0069f9e3c86444dea2357bf471c5de83accc65d4ccc39e758'
   name 'Videostream'
   homepage 'https://getvideostream.com/'
 
   pkg 'Videostream.pkg'
 
-  uninstall quit:      'com.videostream',
-            launchctl: 'com.videostream.launcher',
-            delete:    [
-                         '/Library/LaunchAgents/com.videostream.launcher.plist',
-                         '/Library/LaunchDaemons/com.videostream.updater.0.3.4.plist',
+  uninstall launchctl: [
+                         'com.videostream.launcher',
+                         "com.videostream.updater.#{version}",
                        ],
-            pkgutil:   'com.videostream'
+            pkgutil:   'com.videostream',
+            signal:    ['TERM', 'com.videostream']
 end
