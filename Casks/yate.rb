c59e62f4b44b681140385f18fef892736ebce1ba
Colin Dunn <cgd@vikingdev.net>
2017-11-14 20:29:36 +1100

    Update yate to 3.17.3 (#40742)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,10 +1,10 @@
 cask 'yate' do
-  version '3.17.2'
-  sha256 'a7c143cbc48ebbbb72dde5fee994ff470d3b0d89cb16c216a6b3b6d62940613f'
+  version '3.17.3'
+  sha256 '390fe9467c6f88169d59116ef63a04082895c8510adfbf9322f42c8cbec26199'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '6b5abaff34209e42b7a3b241b419ba4b6b789a0c9adc0facba2388ba49921ee6'
+          checkpoint: '3b3aaed6114daa051b61aa126782d5af2cdfc25827d39f136dad286384bd5311'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
 
