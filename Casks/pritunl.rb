6b2435174a47d386d7b093134bede75cc32ca125
Victor Popkov <victor@popkov.me>
2017-11-14 05:24:03 +0200

    Update pritunl to 1.0.1461.29 (#40710)

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,11 +1,11 @@
 cask 'pritunl' do
-  version '1.0.1442.97'
-  sha256 'f3be2c4ec1ad2dcdae7b458682c7617d0e6659562d4173617a3478c2dfff96a5'
+  version '1.0.1461.29'
+  sha256 '1310a0ee6e17643f111b5fd27023796d4f5bc8e7dae1e8c69bda3b37d5ecd33a'
 
   # github.com/pritunl/pritunl-client-electron was verified as official when first introduced to the cask
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip"
   appcast 'https://github.com/pritunl/pritunl-client-electron/releases.atom',
-          checkpoint: '4fa4e3b03c625ea9fc62abad90b57972cf18ed2890d082e0416e9a903dde60d9'
+          checkpoint: '62a7e43a16f31b3540d854227e86a5b18f414e13428f0391a18b819ca02bbfc5'
   name 'Pritunl OpenVPN Client'
   homepage 'https://client.pritunl.com/'
 
