aff8caffe43b64ffe7aac135485ad57ab3ab1bcd
commitay <commitay@users.noreply.github.com>
2017-11-14 15:10:41 +1000

    Update yasu to 5.0.1,701 (#40733)
    * Update yasu to 5.0.1,701
    
    * Update yasu.rb

--- Casks/yasu.rb
+++ Casks/yasu.rb
@@ -6,13 +6,20 @@ cask 'yasu' do
         user_agent: :fake
     appcast 'https://yasuformac.com/appcasts/10.11/yasu.xml',
             checkpoint: 'd13bbaeca2a2c3610fa770ed3b2080fa040f525bbcc15c144d1802ecaf3ee487'
-  else
+  elsif MacOS.version <= :sierra
     version '4.0.5,605'
     sha256 'dc18c2ee91144bd37b94ce6bab5cf2861b698432a301d1dd03be2bba23dbd609'
     url "https://yasuformac.com/appcasts/10.12/yasuformac_#{version.after_comma}.zip",
         user_agent: :fake
     appcast 'https://yasuformac.com/appcasts/10.12/yasu.xml',
             checkpoint: '53e665930618eaff3dabec8946d5884d8d25f949001a83516faf700d60a27e26'
+  else
+    version '5.0.1,701'
+    sha256 '1fee2a849d9473b3d4776cf275ba48403a9b2c641077d4f4b61cd331d7f6459f'
+    url "https://yasuformac.com/appcasts/10.13/yasuformac_#{version.after_comma}.zip",
+        user_agent: :fake
+    appcast 'https://yasuformac.com/appcasts/10.13/yasu.xml',
+            checkpoint: '466c7e40951036fe7137b65c09d77fd42020face479abadfdc4b9d5e1c9a4cfc'
   end
 
   name 'Yasu'
