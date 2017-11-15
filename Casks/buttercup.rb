64f426acf8dac78e79c9712581a35b9693910dbf
Christian Kraemer <christian.kraemer@gmail.com>
2017-11-15 23:12:34 +0100

    Update buttercup to 0.23.0 (#40805)
    * Update buttercup to 0.23.0
    
    * checkpoint

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -1,11 +1,11 @@
 cask 'buttercup' do
-  version '0.22.3'
-  sha256 'ed3aa1bef79892dad63ee4c048862451e7fd59f51d47bde039283f3c566660a5'
+  version '0.23.0'
+  sha256 'd2213fadafb457ae6f16d3faa39a9059b93d2519477abc7037216d7981cc4018'
 
   # github.com/buttercup/buttercup-desktop was verified as official when first introduced to the cask
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/buttercup-desktop-#{version}-mac.zip"
   appcast 'https://github.com/buttercup/buttercup-desktop/releases.atom',
-          checkpoint: '1049a07a07eb7e38b6b8cd4ffa47b1809334460fa6098bb02a558a64769a21d4'
+          checkpoint: 'e0f718fabadedc4b6ad8593a83dd5e90ec4e5a291cc2b15b96851ff6a402db41'
   name 'Buttercup'
   homepage 'https://buttercup.pw/'
 
