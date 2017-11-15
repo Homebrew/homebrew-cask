b2f444b22c00e1ae8ece9837875381d09c6c06ab
Max Vorobev <vmax0770@gmail.com>
2017-11-15 16:00:11 +0300

    fixes from review

--- Casks/vk-messenger.rb
+++ Casks/vk-messenger.rb
@@ -2,7 +2,7 @@ cask 'vk-messenger' do
   version :latest
   sha256 :no_check
 
-  # desktop.userapi.com was verified as official when first introduced to the cask
+  # desktop.userapi.com/mac/master was verified as official when first introduced to the cask
   url 'https://desktop.userapi.com/mac/master/vk.dmg'
   name 'VK Messenger'
   homepage 'https://vk.com/messenger'
@@ -13,7 +13,7 @@ cask 'vk-messenger' do
   app 'VK Messenger.app'
 
   zap delete: [
-                '~/Library/Saved Application State/com.vk.messages.savedState/*',
+                '~/Library/Saved Application State/com.vk.messages.savedState',
               ],
       trash:  ['~/Library/Preferences/com.vk.messages.plist',
                '~/Library/Preferences/com.vk.messages.helper.plist']
