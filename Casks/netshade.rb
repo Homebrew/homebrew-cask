386799b85b3cfbc4411df2fc58bf79fd1edc39f6
Colin Dunn <cgd@vikingdev.net>
2017-11-15 22:33:26 +1100

    Update netshade to 7.1 (#40793)
    * Update netshade to 7.1
    
    * Update netshade.rb

--- Casks/netshade.rb
+++ Casks/netshade.rb
@@ -1,12 +1,12 @@
 cask 'netshade' do
-  version '7.0.3'
-  sha256 '5d6b2daec9d5cd19a3c14b6784f0138d20a3f9cc796b25a0b2940641c7e1478b'
+  version '7.1'
+  sha256 '41f16822c9edc21c09408813fd4fcedd553f6860a446671c34d3d1826821bbff'
 
   url "https://secure.raynersw.com/downloads/NetShade-#{version.dots_to_hyphens}.app.zip"
-  appcast 'https://secure.raynersw.com/appcast.php',
-          checkpoint: '495f1b5f37f6528781f7ce480e72fafe260c15e5dd19729a93b3fabc7968912a'
+  appcast 'https://secure.raynersw.com/changelog.php?prod=ns&format=std&warnpay=0',
+          checkpoint: 'e0c0979370c8b479f864f8887a2bde3ef898ec013ca5a8207bdb1077ad1960b7'
   name 'NetShade'
-  homepage 'http://raynersw.com/netshade.php'
+  homepage 'https://secure.raynersw.com/netshade.php'
 
   app 'NetShade.app'
 end
