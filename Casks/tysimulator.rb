730a369337ebb8b5b92346bc2313d2a859bf7a58
Colin Dunn <cgd@vikingdev.net>
2017-11-15 01:34:56 -0800

    Update tysimulator to 0.8.1

--- Casks/tysimulator.rb
+++ Casks/tysimulator.rb
@@ -1,11 +1,11 @@
 cask 'tysimulator' do
-  version '0.8.0'
-  sha256 '0bd46b70215ddc53ad349fb7d53d2bdd550b5481b37d62bedeef87d1298b831e'
+  version '0.8.1'
+  sha256 '064c8fa2a35607e262e131c150e837af28fb357e38308cb02f0bbf0eff51ea49'
 
   # github.com/luckytianyiyan/TySimulator was verified as official when first introduced to the cask
   url "https://github.com/luckytianyiyan/TySimulator/releases/download/#{version}/TySimulator.#{version}.dmg"
   appcast 'https://github.com/luckytianyiyan/TySimulator/releases.atom',
-          checkpoint: '46e996a6adbf9dfc6bb7c2455bb2790856f538c77bc0bce1b20a6d2016e85e30'
+          checkpoint: '99063f91750cc8930888dd145978da4dcad4a6344b9238c36d3fcee281f88e9c'
   name 'TySimulator'
   homepage 'https://tysimulator.com/'
 
