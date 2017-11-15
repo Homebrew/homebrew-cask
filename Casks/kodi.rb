83a72d009a609ceb99823e3440cf57679de713e1
commitay <commitay@users.noreply.github.com>
2017-11-16 08:19:25 +1000

    Update kodi to 17.6-Krypton

--- Casks/kodi.rb
+++ Casks/kodi.rb
@@ -1,10 +1,10 @@
 cask 'kodi' do
-  version '17.5-Krypton'
-  sha256 'ecb188a86c28a9c914ca7592a0dd07555a73bdcd42333eb773840c12494a817e'
+  version '17.6-Krypton'
+  sha256 '1c35b240f7a300455a2a76e7ea08b6290abdfe0f8c8d60e2a59d6fa5c3ce96ae'
 
   url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
   appcast 'https://github.com/xbmc/xbmc/releases.atom',
-          checkpoint: '7c779e1983f72da1d75e539e690cefb2911eb58170dc965d683a935d5bf82276'
+          checkpoint: '7549879e6648a1870c830256834e0b5ac2a5c7c00059de3902c1c6d84ecf9e33'
   name 'Kodi'
   homepage 'https://kodi.tv/'
 
