d7fd507a9f7f0cd475f12c24d8315d436d2300fc
Colin Dunn <cgd@vikingdev.net>
2017-11-16 14:16:20 +1100

    Update postbox to 5.0.20,1_5a07ebc81657bec0021d97411f131f0b0da7f99c (#40835)

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,6 +1,6 @@
 cask 'postbox' do
-  version '5.0.19,1_53323943bb57dc797d433c338c1d0f179438b0c2'
-  sha256 'dcf582c3d703f3a494172695c1de86195504bb92e8e62e6d5abfbfb21d699724'
+  version '5.0.20,1_5a07ebc81657bec0021d97411f131f0b0da7f99c'
+  sha256 'efdd3c61ed4e95baf121d18d95a648d0417cb207054ac853a4c39c15b2256dc2'
 
   # amazonaws.com/download.getpostbox.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/download.getpostbox.com/installers/#{version.before_comma}/#{version.after_comma}/postbox-#{version.before_comma}-mac64.dmg"
