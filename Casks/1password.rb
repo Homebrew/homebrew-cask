2d131e59750ed30ad2fea687a144cd71b7c3f649
tjvr <tim@tjvr.org>
2017-11-16 21:48:35 +0000

    Use cache.agilebits.com for 1Password
    Seems more official and also faster :-)

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -3,24 +3,21 @@ cask '1password' do
     version '3.8.22'
     sha256 '3afd75f1bddf791dc7dbc9a7d92ab6eb91ee891407d750cedb7b5aff5fe8bf17'
 
-    # d13itkw33a7sus.cloudfront.net was verified as official when first introduced to the cask
-    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac/1Password-#{version}.zip"
+    url "https://cache.agilebits.com/dist/1P/mac/1Password-#{version}.zip"
 
     app '1Password.app'
   elsif MacOS.version <= :mavericks
     version '4.4.3'
     sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
 
-    # d13itkw33a7sus.cloudfront.net was verified as official when first introduced to the cask
-    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac4/1Password-#{version}.zip"
+    url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
 
     app "1Password #{version.major}.app"
   else
     version '6.8.4'
     sha256 '6d86d1d7c75e1f2245af955c9c94ce49d5293c1cde7be98ca3268c0d3106b342'
 
-    # d13itkw33a7sus.cloudfront.net was verified as official when first introduced to the cask
-    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac4/1Password-#{version}.zip"
+    url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
 
     app "1Password #{version.major}.app"
   end
