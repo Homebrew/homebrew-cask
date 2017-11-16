de23c692a5e4332f9a07da256b65ff544cdc13d7
nuomi1 <nuomi1@users.noreply.github.com>
2017-11-16 22:19:16 +0800

    Update calibre.rb for zap (#40860)
    Update calibre.rb for zap

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -30,9 +30,13 @@ cask 'calibre' do
   binary "#{appdir}/calibre.app/Contents/MacOS/markdown-calibre"
   binary "#{appdir}/calibre.app/Contents/MacOS/web2disk"
 
-  zap delete: '~/Library/Caches/calibre',
+  zap delete: [
+                '~/Library/Caches/calibre',
+                '~/Library/Saved Application State/com.calibre-ebook.ebook-viewer.savedState',
+                '~/Library/Saved Application State/net.kovidgoyal.calibre.savedState',
+              ],
       trash:  [
-                '~/Library/Preferences/net.kovidgoyal.calibre.plist',
                 '~/Library/Preferences/calibre',
+                '~/Library/Preferences/net.kovidgoyal.calibre.plist',
               ]
 end
