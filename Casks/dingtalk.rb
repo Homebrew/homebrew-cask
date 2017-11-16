51d2b17a0076e9358c8c4a3306890adbc15f6c5c
Ray Chen <oldsharp@gmail.com>
2017-11-16 11:47:01 +0800

    Update dingtalk to 3.5.6.1

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -1,6 +1,6 @@
 cask 'dingtalk' do
-  version '3.5.3.3'
-  sha256 'e67470adaf3358caac246a364e8d612564d247eca6efc9d60ad4d0f55fc0dffb'
+  version '3.5.6.1'
+  sha256 'f3a6288e839a86b1863412029370fcc327fbe63a15be765de8b951c820bc53be'
 
   # download.alicdn.com/dingtalk-desktop was verified as official when first introduced to the cask
   url "https://download.alicdn.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
