5c5888d76a8770f6a0c9e9aeb92776b225f8a8bf
Anver Housseini <contact@anver.ch>
2017-11-16 09:04:49 +0100

    Update owncloud to 2.3.4.8537 (#40852)

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,10 +1,10 @@
 cask 'owncloud' do
-  version '2.3.3.8242'
-  sha256 '61536572d1dbc627677e373e69a5a01a785d099ddacf25f50a42b05699c52388'
+  version '2.3.4.8537'
+  sha256 '04a48607fb2ecbbc88b00ebf00b74875ad3dc53b104a6749d0f865f038737255'
 
   url "https://download.owncloud.com/desktop/stable/ownCloud-#{version}.pkg"
   appcast 'https://github.com/owncloud/client/releases.atom',
-          checkpoint: '24cdfdc52c2ae6ec670cfbfaed696257aafca1b2228ddbd0cb069ab56eecc16c'
+          checkpoint: 'b6b882ce94c26e285527e49f02c3e9ea91d37bb6276e95d8c513224f052564cf'
   name 'ownCloud'
   homepage 'https://owncloud.com/'
 
