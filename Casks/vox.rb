a01f61bc6d4f1f3ab2f99641ced48f8a14a31623
commitay <commitay@users.noreply.github.com>
2017-11-16 20:21:46 +1000

    Update vox to 3.0 (#40856)

--- Casks/vox.rb
+++ Casks/vox.rb
@@ -1,11 +1,11 @@
 cask 'vox' do
-  version '2.8.30'
-  sha256 'af382cb46d93bafc2f305abe8e5336dfc295aa1964a7de033a494aad865a1da3'
+  version '3.0'
+  sha256 'b5ab51ff85fe8b952c1e7701b033a5141bfa9ff124289c97357a7a1c53e239cf'
 
   # devmate.com/com.coppertino.Vox was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coppertino.Vox/Vox.dmg'
   appcast 'https://updates.devmate.com/com.coppertino.Vox.xml',
-          checkpoint: '7aaf176b3124282e9adece95999ffec90d65a93437a67edd86221af9d6098d78'
+          checkpoint: 'c6adf9777f25c56126b7c5504d18c2ff7a17e60e43f6d4cc356303b6d12cfe91'
   name 'VOX'
   homepage 'https://vox.rocks/mac-music-player'
 
