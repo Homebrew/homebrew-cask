6a6baee0aef18a8fabac810b3653f95274c8774e
Ruben <rubenopdebeeck01@gmail.com>
2017-11-16 22:25:03 +0100

    Update electrum to 3.0.2 (#40874)

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,10 +1,10 @@
 cask 'electrum' do
-  version '3.0.1'
-  sha256 'b2961dfda31aa7b2365bc438b56a6937b248e0bd94a5d9698f4bebfedafb5701'
+  version '3.0.2'
+  sha256 'e40faa222a91c2813d9ccb190025ad4bd3a565c6aa9fe149c34962294544ebfb'
 
   url "https://download.electrum.org/#{version}/electrum-#{version}.dmg"
   appcast 'https://github.com/spesmilo/electrum/releases.atom',
-          checkpoint: 'e4bb1abefa115062034ffffaea9faddc97cb6a70e8fd361afee4e61b16e496e7'
+          checkpoint: '494dae9cd3da494d5870df076a1605c85145452d900af0ff5f333a4aaf071576'
   name 'Electrum'
   homepage 'https://electrum.org/'
   gpg "#{url}.asc", key_id: '6694d8de7be8ee5631bed9502bd5824b7f9470e6'
