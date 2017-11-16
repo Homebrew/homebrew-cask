57e3971f8ef3b5451f471331c0173e679ff73aa6
commitay <commitay@users.noreply.github.com>
2017-11-17 08:25:12 +1000

    verified comments

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -3,6 +3,7 @@ cask '1password' do
     version '3.8.22'
     sha256 '3afd75f1bddf791dc7dbc9a7d92ab6eb91ee891407d750cedb7b5aff5fe8bf17'
 
+    # cache.agilebits.com/dist/1P/mac was verified as official when first introduced to the cask
     url "https://cache.agilebits.com/dist/1P/mac/1Password-#{version}.zip"
 
     app '1Password.app'
@@ -10,6 +11,7 @@ cask '1password' do
     version '4.4.3'
     sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
 
+    # cache.agilebits.com/dist/1P/mac4 was verified as official when first introduced to the cask
     url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
 
     app "1Password #{version.major}.app"
@@ -17,6 +19,7 @@ cask '1password' do
     version '6.8.4'
     sha256 '6d86d1d7c75e1f2245af955c9c94ce49d5293c1cde7be98ca3268c0d3106b342'
 
+    # cache.agilebits.com/dist/1P/mac4 was verified as official when first introduced to the cask
     url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
 
     app "1Password #{version.major}.app"
