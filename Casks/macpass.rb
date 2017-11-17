0ddff06a8c08dbf708c4c39dc5b2e865c54f2ea3
Colin Dunn <cgd@vikingdev.net>
2017-11-17 02:30:47 -0800

    Update macpass to 0.7.1

--- Casks/macpass.rb
+++ Casks/macpass.rb
@@ -1,11 +1,11 @@
 cask 'macpass' do
-  version '0.7'
-  sha256 '970e877e0a83b5f35dcd2f1925dcd1cc4ba3fcf488300dd9caab7479186a2eef'
+  version '0.7.1'
+  sha256 '7fd585aff756be13c211a2d8f587da827312f44c3f0c9fb91d14295170afbdfc'
 
   # github.com/mstarke/MacPass was verified as official when first introduced to the cask
   url "https://github.com/mstarke/MacPass/releases/download/#{version}/MacPass-#{version}.zip"
   appcast 'https://github.com/mstarke/MacPass/releases.atom',
-          checkpoint: '42772c052f4e95723428c49866086734d02061ea03ae905007cfa852c64bcdcf'
+          checkpoint: 'dbef902cb05c6628310fe575219bea3faab596420c02e4269d2c810572446e52'
   name 'MacPass'
   homepage 'https://mstarke.github.io/MacPass/'
 
