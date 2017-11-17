9c23b9e56697b23d66c0516167510edb996ff485
Colin Dunn <cgd@vikingdev.net>
2017-11-17 22:02:43 +1100

    Update goodsync to 10.6.7 (#40911)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,10 +1,10 @@
 cask 'goodsync' do
-  version '10.6.6'
-  sha256 '0ea3e696d6698a151cc24876d6ddeed05c5281315f136cd7946028982985227f'
+  version '10.6.7'
+  sha256 '1100012006296cda4f9b21faf27588416e47ee586aa97870b77df419d7712013'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://www.goodsync.com/download',
-          checkpoint: '59673985f7d98b8bae751a0f04fc221d489bf5fbe9b52540751c1596209ecc47'
+          checkpoint: 'f0704987e9bc056d8dd792f6fce24a84d9df285b335a8a2c9d38159741651997'
   name 'GoodSync'
   homepage 'https://www.goodsync.com/'
 
