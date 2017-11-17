5db43145453c753f5dd4e0342e945d4b7feb0a4c
commitay <commitay@users.noreply.github.com>
2017-11-18 07:58:22 +1000

    appcast

--- Casks/linphone.rb
+++ Casks/linphone.rb
@@ -3,8 +3,8 @@ cask 'linphone' do
   sha256 'b1a5e518c1c564d47f992adadb09ea8cdb51f35e1a0935d4e2060468f893328e'
 
   url "https://www.linphone.org/releases/macosx/Linphone-#{version}-mac.dmg"
-  appcast 'https://www.linphone.org/releases/macosx/',
-          checkpoint: '6d4eb104adb18508b1d1362a9bf29330b32abfac0832161ca6d22c71fa5ea757'
+  appcast 'https://www.linphone.org/releases/macosx/RELEASE',
+          checkpoint: '1c2e9c6b3dcf0ca0dad9a60ad32ee0de93f85c5239a0d56cd8119ca9af20d604'
   name 'Linphone'
   homepage 'https://www.linphone.org/'
 
