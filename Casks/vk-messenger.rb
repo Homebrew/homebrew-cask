0d52ae634f8f989c0a04712a6bde4631b08b197e
Max Vorobev <vmax0770@gmail.com>
2017-11-17 03:39:08 +0300

    Add vk-messenger :latest (#40801)
    * Add vk-messenger :latest
    
    * fixes from review
    
    * zap
    
    * Update vk-messenger.rb

---
+++ Casks/vk-messenger.rb
@@ -0,0 +1,20 @@
+cask 'vk-messenger' do
+  version :latest
+  sha256 :no_check
+
+  # desktop.userapi.com/mac/master was verified as official when first introduced to the cask
+  url 'https://desktop.userapi.com/mac/master/vk.dmg'
+  name 'VK Messenger'
+  homepage 'https://vk.com/messenger'
+
+  auto_updates true
+  depends_on macos: '>= :mavericks'
+
+  app 'VK Messenger.app'
+
+  zap delete: '~/Library/Saved Application State/com.vk.messages.savedState',
+      trash:  [
+                '~/Library/Preferences/com.vk.messages.plist',
+                '~/Library/Preferences/com.vk.messages.helper.plist',
+              ]
+end
