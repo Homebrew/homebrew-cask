c4ecffa838f9d53046dc870c87f77114c50ff185
Tim (blob8108) <tjvr@fastmail.com>
2017-11-17 01:03:32 +0000

    Use cache.agilebits.com for 1Password (#40879)
    * Use cache.agilebits.com for 1Password
    
    Seems more official and also faster :-)
    
    * verified comments
    
    * checkpoint

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -3,30 +3,30 @@ cask '1password' do
     version '3.8.22'
     sha256 '3afd75f1bddf791dc7dbc9a7d92ab6eb91ee891407d750cedb7b5aff5fe8bf17'
 
-    # d13itkw33a7sus.cloudfront.net was verified as official when first introduced to the cask
-    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac/1Password-#{version}.zip"
+    # cache.agilebits.com/dist/1P/mac was verified as official when first introduced to the cask
+    url "https://cache.agilebits.com/dist/1P/mac/1Password-#{version}.zip"
 
     app '1Password.app'
   elsif MacOS.version <= :mavericks
     version '4.4.3'
     sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
 
-    # d13itkw33a7sus.cloudfront.net was verified as official when first introduced to the cask
-    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac4/1Password-#{version}.zip"
+    # cache.agilebits.com/dist/1P/mac4 was verified as official when first introduced to the cask
+    url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
 
     app "1Password #{version.major}.app"
   else
     version '6.8.4'
     sha256 '6d86d1d7c75e1f2245af955c9c94ce49d5293c1cde7be98ca3268c0d3106b342'
 
-    # d13itkw33a7sus.cloudfront.net was verified as official when first introduced to the cask
-    url "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac4/1Password-#{version}.zip"
+    # cache.agilebits.com/dist/1P/mac4 was verified as official when first introduced to the cask
+    url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
 
     app "1Password #{version.major}.app"
   end
 
   appcast 'https://app-updates.agilebits.com/product_history/OPM4',
-          checkpoint: 'dbdda372503c53ee9f720fdeb8d32d28b77fcc7acee1ca46c46ae808cece0761'
+          checkpoint: '6dfb8fc9495094ee9af5fb9f603aed25391449119fefd35d7d374e8c97ad3be8'
   name '1Password'
   homepage 'https://1password.com/'
 
