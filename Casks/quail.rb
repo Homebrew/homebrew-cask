e9853fccaf6553f0ee1c3990b5c2efbad2bae1ba
Colin Dunn <cgd@vikingdev.net>
2017-11-17 14:39:21 -0800

    Update quail to 0.9.0

--- Casks/quail.rb
+++ Casks/quail.rb
@@ -1,10 +1,10 @@
 cask 'quail' do
-  version '0.7.2'
-  sha256 '3a4a472e5f369e4d99f66e1fa7b852ffc32c94585b4de04e99e48ea41a2be130'
+  version '0.9.0'
+  sha256 '702a3fe4b2c2c7024357b2ef144b0ef837d8047c5a4ba32963e6fe293e273f34'
 
   url "https://github.com/1000ch/quail/releases/download/v#{version}/Quail-macos-v#{version}.zip"
   appcast 'https://github.com/1000ch/quail/releases.atom',
-          checkpoint: '1aa228b633eddc26a9a83815277ade164efb1f2a4173e67276b7ffb63903b292'
+          checkpoint: '3558d2f1ecdd4cd9f9d33e948743f993c7d3e8b3579ced3f648d29e3bf83964a'
   name 'Quail'
   homepage 'https://github.com/1000ch/quail'
 
