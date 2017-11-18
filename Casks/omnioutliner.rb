6a3caf4e673d0c83fe19adfce5c7aa2b81c55e3e
Brian Bezanson <brian.bezanson@healthlx.com>
2017-11-17 18:20:16 -0600

    Core URL changed from 10.11 to 10.12
    Core URL changed from 10.11 to 10.12 for the Omnioutliner 11.2 update

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -1,8 +1,8 @@
 cask 'omnioutliner' do
-  version '5.1.4'
-  sha256 '91817e87a29c6a86f64b22f36e292b354aab89f63a070eeab117f4fbb2704ff0'
+  version '5.2'
+  sha256 '7d009220ce74570235e25d3c3855c804dec13a6d1e678ffd4e243bb9788938ac'
 
-  url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniOutliner-#{version}.dmg"
+  url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniOutliner-#{version}.dmg"
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniOutliner#{version.major}",
           checkpoint: '9966a34bf39bb4d5d2aaebdf38953a8b63fbf851ede9a2cd3f70370b87585322'
   name 'OmniOutliner'
