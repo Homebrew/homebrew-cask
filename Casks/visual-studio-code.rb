4b9a8077210535a957e98e6341b3294fdac4f94b
commitay <commitay@users.noreply.github.com>
2017-11-18 10:03:03 +1000

    Update visual-studio-code: add appcast

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -2,8 +2,10 @@ cask 'visual-studio-code' do
   version '1.18.1,929bacba01ef658b873545e26034d1a8067445e9'
   sha256 'b3aafb308a32154a0827313e7dcb9ea64f7e6f0ece0bcf43f81d445d85467324'
 
-  # az764295.vo.msecnd.net was verified as official when first introduced to the cask
+  # az764295.vo.msecnd.net/stable was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
+  appcast 'https://vscode-update.azurewebsites.net/api/update/darwin/stable/VERSION',
+          checkpoint: '4900baa6b3786449562a632406d4c223b4708d45177af6715a8cdba61cb5d8a6'
   name 'Microsoft Visual Studio Code'
   name 'VS Code'
   homepage 'https://code.visualstudio.com/'
