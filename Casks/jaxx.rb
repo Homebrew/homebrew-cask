c398f48c03a927193b2f4008ad72d75842530547
sparkDEMAND <jay.schwartz@sparkdemand.io>
2017-11-18 16:58:42 -0600

    Repaired Jaxx -- Updated to 1.3.7 -- Moved download hosting to GitHub.com and updated SHA256 (#41035)
    * Repaired Jaxx -- Updated to 1.3.7 -- Moved download hosting to github.com and updated SHA256
    
    * Added Appcast + Checkpoint for Jaxx 1.3.7

--- Casks/jaxx.rb
+++ Casks/jaxx.rb
@@ -1,8 +1,11 @@
 cask 'jaxx' do
-  version '1.3.3'
-  sha256 'b6060efa747afeca4f7cc2acc32891edf66057ebad5141b127f6b015a0a5799a'
+  version '1.3.7'
+  sha256 '5a71960a98cf4f09ce1e37d7d50a4d1341210253dc714553d7f2d8180e4ee68a'
 
-  url "https://jaxx.io/files/#{version}/Jaxx-#{version}.dmg"
+  # github.com/Jaxx-io/Jaxx was verified as official when first introduced to the cask
+  url "https://github.com/Jaxx-io/Jaxx/releases/download/v#{version}/Jaxx-#{version}.dmg"
+  appcast 'https://github.com/Jaxx-io/Jaxx/releases.atom',
+          checkpoint: '071a1ed39d4940ed96347ca4aa645b1ee199f4af2736b1cda367536949340358'
   name 'Jaxx Blockchain Wallet'
   homepage 'https://jaxx.io/'
 
