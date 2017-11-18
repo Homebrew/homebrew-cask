c3baae948be4ffc5e17872064ee7420ebf4f88e5
commitay <commitay@users.noreply.github.com>
2017-11-18 23:55:08 +1000

    Update dbeaver-enterprise: appcast (#41025)

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -3,8 +3,8 @@ cask 'dbeaver-enterprise' do
   sha256 'be79572cdabee2ba108a8a50ff42dea7d9bb6b9061ddaeaf22e2c9ac4b4c886c'
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
-  appcast 'https://dbeaver.com/files/',
-          checkpoint: '476f8e064acf08b2c082c65c88709abea20ea6b019e861f91df25320cd2f636e'
+  appcast 'https://dbeaver.com/product/version.xml',
+          checkpoint: 'c499fedde33d8066aac425bda71bb8abaa94696411fbc9dd6282dbc734581869'
   name 'DBeaver Enterprise Edition'
   homepage 'https://dbeaver.com/'
 
