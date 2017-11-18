6b47e97ebebf829806d69aec321bf427b24fa583
miccal <miccal.matthews@gmail.com>
2017-11-18 11:56:04 +0800

    Update dropbox.rb

--- Casks/dropbox.rb
+++ Casks/dropbox.rb
@@ -14,25 +14,23 @@ cask 'dropbox' do
   uninstall login_item: 'Dropbox',
             launchctl:  'com.dropbox.DropboxMacUpdate.agent'
 
-  zap delete: [
-                '/Library/DropboxHelperTools',
-                '~/Library/Application Scripts/com.getdropbox.dropbox.garcon',
-                '~/Library/Caches/CloudKit/com.apple.bird/iCloud.com.getdropbox.Dropbox',
-                '~/Library/Caches/com.dropbox.DropboxMacUpdate',
-                '~/Library/Caches/com.getdropbox.dropbox',
-                '~/Library/Caches/com.getdropbox.DropboxMetaInstaller',
-                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.dropbox.DropboxMacUpdate',
-                '~/Library/Containers/com.getdropbox.dropbox.garcon',
-                '~/Library/Group Containers/com.getdropbox.dropbox.garcon',
-                '~/Library/Logs/Dropbox_debug.log',
-              ],
-      trash:  [
-                '~/.dropbox',
-                '~/Library/Application Support/Dropbox',
-                '~/Library/Dropbox',
-                '~/Library/Preferences/com.dropbox.DropboxMacUpdate.plist',
-                '~/Library/Preferences/com.dropbox.DropboxMonitor.plist',
-                '~/Library/Preferences/com.dropbox.tungsten.helper.plist',
-                '~/Library/Preferences/com.getdropbox.dropbox.plist',
-              ]
+  zap trash: [
+               '/Library/DropboxHelperTools',
+               '~/.dropbox',
+               '~/Library/Application Scripts/com.getdropbox.dropbox.garcon',
+               '~/Library/Application Support/Dropbox',
+               '~/Library/Caches/CloudKit/com.apple.bird/iCloud.com.getdropbox.Dropbox',
+               '~/Library/Caches/com.dropbox.DropboxMacUpdate',
+               '~/Library/Caches/com.getdropbox.DropboxMetaInstaller',
+               '~/Library/Caches/com.getdropbox.dropbox',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.dropbox.DropboxMacUpdate',
+               '~/Library/Containers/com.getdropbox.dropbox.garcon',
+               '~/Library/Dropbox',
+               '~/Library/Group Containers/com.getdropbox.dropbox.garcon',
+               '~/Library/Logs/Dropbox_debug.log',
+               '~/Library/Preferences/com.dropbox.DropboxMacUpdate.plist',
+               '~/Library/Preferences/com.dropbox.DropboxMonitor.plist',
+               '~/Library/Preferences/com.dropbox.tungsten.helper.plist',
+               '~/Library/Preferences/com.getdropbox.dropbox.plist',
+             ]
 end
