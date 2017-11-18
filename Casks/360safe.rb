45ad7de48fb3c3f84335e721fb5e75e9330b4ef4
Colin Dunn <cgd@vikingdev.net>
2017-11-17 21:13:29 -0800

    Update 360safe to 1.2.4

--- Casks/360safe.rb
+++ Casks/360safe.rb
@@ -1,10 +1,10 @@
 cask '360safe' do
-  version '1.1.2'
-  sha256 '6fdef1e93f9930f48ecf7144ed2412663138a1cee492aaa371550907999db8d9'
+  version '1.2.4'
+  sha256 '1b33ced51458e301a9dd5f00caa99c9de189da92c59b7deb75b461709cf2de95'
 
   url "https://free.360totalsecurity.com/totalsecurity/mac/360ts_mac_#{version}.dmg"
   appcast 'https://www.360totalsecurity.com/en/version/360-total-security-mac/',
-          checkpoint: 'cbbe129c78691310d2ffa8274aa5709ce28cddfaa6f4f74ad0877716ec917b8e'
+          checkpoint: 'bf19ffd21e18c79522000a32425ef6ee19f6f556902814fa407c8a514b514171'
   name '360 Total Security'
   homepage 'https://www.360totalsecurity.com/features/360-total-security-mac/'
 
