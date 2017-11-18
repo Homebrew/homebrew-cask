842319cb22cce22f9764bc97fb8aa89a85d8c3f0
Travers McInerney <travers@mcinerney.me>
2017-11-17 22:32:56 -0800

    Update keep to 1.0.0 (#40979)

--- Casks/keep.rb
+++ Casks/keep.rb
@@ -1,10 +1,10 @@
 cask 'keep' do
-  version '0.4.0'
-  sha256 'e7a750deb250f21ef8b0e11bb01c62299d1574779bcfc83d74c5e238f1e40889'
+  version '1.0.0'
+  sha256 '391724f79b91eeddb07e47460be515cb6b343678517d099de86eaca3d23d831f'
 
   url "https://github.com/tmcinerney/keep/releases/download/v#{version}/keep.v#{version}.zip"
   appcast 'https://github.com/tmcinerney/keep/releases.atom',
-          checkpoint: 'dfbb63243f9d2c22b82e0b9fca0b5c0c0956be55feb19e77b341d0de3662f0a7'
+          checkpoint: 'b8ba6378ffa4f4c59e425cf39819f3c607c87dad5c1aa8652b02dbf25ef43290'
   name 'Keep'
   homepage 'https://github.com/tmcinerney/keep/'
 
