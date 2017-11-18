9ddb52b39446944fdb8f1607d33008a392df7d35
Colin Dunn <cgd@vikingdev.net>
2017-11-17 22:50:40 -0800

    Update iueditor to 2.0.6.13

--- Casks/iueditor.rb
+++ Casks/iueditor.rb
@@ -1,6 +1,6 @@
 cask 'iueditor' do
-  version '2.0.6.1'
-  sha256 'd03c18a5caa32af1e732ef5ac951f1bc9cc50952da9fa64fafc44341bd04e2ea'
+  version '2.0.6.13'
+  sha256 '4b21e63cda4808b1f29f78fb61f75d9e7c5b9694b9b63bc153fb3fa62a888794'
 
   url "https://cdn.iueditor.org/release/IUEditorV#{version}.pkg"
   name 'JDLab IUEditor'
