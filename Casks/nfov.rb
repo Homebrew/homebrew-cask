c6760615d7b6be03d5e02921f4e8556cf1648807
nrlquaker <ustynov.vy@gmail.com>
2017-11-19 02:08:04 +0200

    Update nfov to 1.0.4 (#41040)

--- Casks/nfov.rb
+++ Casks/nfov.rb
@@ -1,10 +1,10 @@
 cask 'nfov' do
-  version '1.0.3'
-  sha256 '4b832df6a65be9fb9317531ea734049b03e96cb93977882a9612268a3be6353e'
+  version '1.0.4'
+  sha256 '86881b34e3e96966c2958259fe6530ab3ca04d2166cbdd6d20d639fd412face6'
 
   url "https://github.com/nrlquaker/nfov/releases/download/v#{version}/nfov-darwin-x64-#{version}.zip"
   appcast 'https://github.com/nrlquaker/nfov/releases.atom',
-          checkpoint: '4f931faac9a6bda2818eead2c3e8fb49d4e06828b90d7e6f46a613da2c76ea54'
+          checkpoint: '9426e24cb40929a49a93245098363fa3ed277b6058cc2b9975f2294d677a1472'
   name 'nfov'
   homepage 'https://github.com/nrlquaker/nfov'
 
