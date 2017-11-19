809321025669550f5f2fdb091585b7a0f608444f
commitay <commitay@users.noreply.github.com>
2017-11-20 07:32:13 +1000

    remove comment

--- Casks/microsoft-bing-ads-editor.rb
+++ Casks/microsoft-bing-ads-editor.rb
@@ -2,7 +2,6 @@ cask 'microsoft-bing-ads-editor' do
   version :latest
   sha256 :no_check
 
-  # https://download.microsoft.com/download/6/6/2/6629C18B-8F68-43E8-A501-198B5AE0D69D/ was verified as official when first introduced to the cask
   url 'https://download.microsoft.com/download/6/6/2/6629C18B-8F68-43E8-A501-198B5AE0D69D/MicrosoftBingAdsEditor.dmg'
   name 'Microsoft Bing Ads Editor'
   homepage 'https://advertise.bingads.microsoft.com/solutions/tools/bing-ads-editor'
