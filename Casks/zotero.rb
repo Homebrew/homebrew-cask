8e3dcf0499285fe11d706b2f3a86df719a9af57a
Egor Kotov <ek@mbr12.beeline>
2017-11-20 23:31:06 +0300

    Update zotero to 5.0.27

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,10 +1,10 @@
 cask 'zotero' do
-  version '5.0.25'
-  sha256 '90989e396a61259af09717b4d78d3d7718c5ae4ae0b751f9ce4245f0731476c8'
+  version '5.0.27'
+  sha256 'b4f339c5b4392c80ad126e8083becdc01e14b41b7067daea15bc14b9c56a1560'
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast 'https://github.com/zotero/zotero/releases.atom',
-          checkpoint: 'd358a2e32cd2bba5ed3f4bea2a87f7fd768d70a99dc3d0950252ee697d5c2106'
+          checkpoint: '3bcdaff9b1cf6e6c7fe573047f7941e2491febcaa401fefd7691df3f157aba42'
   name 'Zotero'
   homepage 'https://www.zotero.org/'
 
