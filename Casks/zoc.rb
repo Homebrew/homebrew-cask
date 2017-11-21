e672f67e5fbfec77f94b9dcb54613b9c2c203d46
Vítor Galvão <info@vitorgalvao.com>
2017-11-21 19:18:53 +0000

    Update zoc.rb

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -3,7 +3,7 @@ cask 'zoc' do
   sha256 '2aa0cd9df0ea82d3d3e6ba1afe396151f0f062484eabd7cd98f54ad2573b69d4'
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
-  appcast 'https://www.emtec.com/downloads/zoc/zoc7_previous/',
+  appcast "https://www.emtec.com/downloads/zoc/zoc#{version.major}_previous/",
           checkpoint: 'e1932edb54cd11a8290aaae0ea26e0c5ec7800f452644b41fdda6edfeb7dc938'
   name 'ZOC'
   homepage 'https://www.emtec.com/zoc/'
