3e820f98344a2833af084cb8be215bd7eca6f81a
Fernando Tadashi Akimoto <f.tadashi@gmail.com>
2017-11-21 22:51:35 -0200

    Update adobe-connect.rb

--- Casks/adobe-connect.rb
+++ Casks/adobe-connect.rb
@@ -1,8 +1,8 @@
 cask 'adobe-connect' do
-  version :latest
+  version '9.7,982.437'
   sha256 'd310bda05cac63fca5c90e7b3dae9b09accd7460e88e76482efed2bc763e6cfc'
 
-  url 'http://download.adobe.com/pub/connect/updaters/meeting/9_7/AdobeConnect_982.437.dmg'
+  url 'http://download.adobe.com/pub/connect/updaters/meeting/#{version.before_comma.dots_to_underscores}/AdobeConnect_#{version.after_comma}.dmg'
   name 'Adobe Connect'
   homepage 'http://www.adobe.com/products/adobeconnect.html'
 
