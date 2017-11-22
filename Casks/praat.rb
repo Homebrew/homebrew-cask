aa43195146ddff3dc61d5a78aea6ab9b11e541fe
Colin Dunn <cgd@vikingdev.net>
2017-11-22 21:09:10 +1100

    Update praat to 6.0.36 (#41187)

--- Casks/praat.rb
+++ Casks/praat.rb
@@ -1,11 +1,11 @@
 cask 'praat' do
-  version '6.0.35'
-  sha256 '04185cdf2cc82c970250c5e9e1fc80bdbbf0e9b461919b60ab8f6e17cc66ee97'
+  version '6.0.36'
+  sha256 '5fe22254c0ce2a9ddea7de4d497fd3d5d620725ce690d123381dda44cccd7c30'
 
   # github.com/praat/praat/releases was verified as official when first introduced to the cask
   url "https://github.com/praat/praat/releases/download/v#{version}/praat#{version.no_dots}_mac64.dmg"
   appcast 'https://github.com/praat/praat/releases.atom',
-          checkpoint: '83328a6e18cda9f1038916cee83314c47d3b58ff46b64df7bca53f402542eee5'
+          checkpoint: '2a347fbe3d6ae15184feaf71a2856fdca856cb7e06daabc26d0fd3f88ed76709'
   name 'Praat'
   homepage 'http://www.fon.hum.uva.nl/praat/'
 
