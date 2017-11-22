5e33f8ee8760664427a901f556270a1d60c1b85b
Colin Dunn <cgd@vikingdev.net>
2017-11-22 03:26:44 -0800

    Update losslesscut to 1.9.0

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,10 +1,10 @@
 cask 'losslesscut' do
-  version '1.8.0'
-  sha256 '16446cfbe44ebcdc769dd3ce72211a7054bdaf2e2b48ac35c727bd3940c049b9'
+  version '1.9.0'
+  sha256 '4a67572e031b6f240718160df6c5e942f7b8187cd76b79aeaf90447528aca40a'
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-darwin-x64.zip"
   appcast 'https://github.com/mifi/lossless-cut/releases.atom',
-          checkpoint: '36635a3858f53da649718b2f21c033c7124310e5d50823e862e2588fd2b7019c'
+          checkpoint: '3a0d1ac2fe29edb13f3423762425921f124e03af7e7b51a9c79338eba3289d38'
   name 'Loslesscut'
   homepage 'https://github.com/mifi/lossless-cut'
 
