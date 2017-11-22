663b24c2b64aaea5235ef88638a73aac8229b8d6
Colin Dunn <cgd@vikingdev.net>
2017-11-21 20:06:25 -0800

    Update ghost-browser to 1.1.0.7

--- Casks/ghost-browser.rb
+++ Casks/ghost-browser.rb
@@ -1,6 +1,6 @@
 cask 'ghost-browser' do
-  version '1.1.0.4'
-  sha256 '61603e231b17f32f34363b9bf51bc2bc4522aa0f32ad9e3386ee86a37669fa57'
+  version '1.1.0.7'
+  sha256 '5113dcc38084f5e3baeb71258da133f3f8155478205358e62561c8fc7da4fef0'
 
   url "https://ghostbrowser.com/ghostdev/downloads/GhostBrowser-#{version}.dmg"
   name 'Ghost Browser'
