78b4820d4b114b1cd5fd45d9beed0697b62bf1a2
Colin Dunn <cgd@vikingdev.net>
2017-11-23 01:50:59 +1100

    Update avocode to 2.17.0 (#41200)

--- Casks/avocode.rb
+++ Casks/avocode.rb
@@ -1,6 +1,6 @@
 cask 'avocode' do
-  version '2.16.1'
-  sha256 '446849544f70150d9e18ef375c43160f075b48c338ecb2831f06c94a8f12e0b4'
+  version '2.17.0'
+  sha256 'a005d87c90db16216ec6fae806deff7f277d5eb7fe27759cb6b6b886f5a8071d'
 
   url "http://media.avocode.com/download/avocode-app/#{version}/Avocode-#{version}-mac.zip"
   name 'Avocode'
