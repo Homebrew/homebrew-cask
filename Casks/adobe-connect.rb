3de022c719398e2e2cf3f9600f3f6895290315c3
commitay <commitay@users.noreply.github.com>
2017-11-22 15:21:03 +1000

    url ""

--- Casks/adobe-connect.rb
+++ Casks/adobe-connect.rb
@@ -2,7 +2,7 @@ cask 'adobe-connect' do
   version '9.7,982.437'
   sha256 'd310bda05cac63fca5c90e7b3dae9b09accd7460e88e76482efed2bc763e6cfc'
 
-  url 'http://download.adobe.com/pub/connect/updaters/meeting/#{version.before_comma.dots_to_underscores}/AdobeConnect_#{version.after_comma}.dmg'
+  url "http://download.adobe.com/pub/connect/updaters/meeting/#{version.before_comma.dots_to_underscores}/AdobeConnect_#{version.after_comma}.dmg"
   name 'Adobe Connect'
   homepage 'http://www.adobe.com/products/adobeconnect.html'
 
