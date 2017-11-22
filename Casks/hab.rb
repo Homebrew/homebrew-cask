4ee367cb090f0045fb93c2bb17f007e76053d9d5
Jonathan Hartman <j@hartman.io>
2017-11-21 19:07:15 -0800

    Update hab to 0.39.1-20171117234750 (#41132)

--- Casks/hab.rb
+++ Casks/hab.rb
@@ -1,11 +1,11 @@
 cask 'hab' do
-  version '0.38.0-20171026215116'
-  sha256 '2ffcbf5520445ff0471b71657bb24005e11adb94644d6c4e5f953e68efb74df4'
+  version '0.39.1-20171117234750'
+  sha256 'ca735f66c701a74285ccae6dfef2427fbdc2322cb658610e40c85c5f650e5c2e'
 
   # habitat.bintray.com was verified as official when first introduced to the cask
   url "https://habitat.bintray.com/stable/darwin/x86_64/hab-#{version}-x86_64-darwin.zip"
   appcast 'https://github.com/habitat-sh/habitat/releases.atom',
-          checkpoint: 'a63d8468e5986f9eb9d1152563e63147deb3798cb63da9303ebc12eeb91851ab'
+          checkpoint: '66ba2eabc5e064f9231d0ca3761346b8502c0979de6b4284e643a6801741e618'
   name 'Habitat'
   homepage 'https://www.habitat.sh/'
 
