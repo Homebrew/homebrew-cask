5ee6f0725bd9ff58ae459ffe2051ac77a081db65
XuluWarrior <github@metaliving.plus.com>
2017-11-24 17:40:08 +0000

    Add Swagger Editor Offline.app v0.0.3

---
+++ Casks/swagger-editor-offline.rb
@@ -0,0 +1,10 @@
+cask 'swagger-editor-offline' do
+  version '0.0.3'
+  sha256 'e354fd3df95123af66da2e4dbc3f4092de723eadb4a7874a127aa723fbaf80f4'
+
+  url 'https://github.com/XuluWarrior/swagger-editor-offline/releases/download/v0.0.3/Swagger.Editor.Offline-0.0.3.dmg'
+  name 'Swagger Editor Offline'
+  homepage 'https://github.com/XuluWarrior/swagger-editor-offline'
+
+  app 'Swagger Editor Offline.app'
+end
