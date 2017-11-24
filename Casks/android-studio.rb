19c397e49de12828a2c8882a9b897a4e218cd180
Kamil Wargula <kwargula@gmail.com>
2017-11-24 21:18:19 +0100

    Update android-studio to 3.0.1.0

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,6 +1,6 @@
 cask 'android-studio' do
-  version '2.3.3.0,162.4069837'
-  sha256 'e9e11f1d20e6d3b40b4a69cf453037465d4a1f37946fd5949833d38720c84efd'
+  version '3.0.1.0,171.4443003'
+  sha256 '2c8a24ad2de21e8d1d77a748a35cc5ca47812d9f5e2a0865981e206073d1d981'
 
   # google.com/dl/android/studio was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
