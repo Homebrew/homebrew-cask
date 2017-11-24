389d2c414ec5a53975345ffbfe586690fabccd8d
Andy Croll <andy@goodscary.com>
2017-11-24 11:47:25 +0000

    Add WebPonize v1.2.2
    Download is from GitHub repo, homepage on GitHub pages

---
+++ Casks/webponize.rb
@@ -0,0 +1,11 @@
+cask 'webponize' do
+  version :latest
+  sha256 :no_check
+
+  # github.com was verified as official when first introduced to the cask
+  url 'https://github.com/1000ch/WebPonize/raw/master/webponize.tar.gz'
+  name 'WebPonize'
+  homepage 'https://webponize.github.io/'
+
+  app 'WebPonize.app'
+end
