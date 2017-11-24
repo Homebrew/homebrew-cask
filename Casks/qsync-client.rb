8ce89459003003b822e9912ea5a8411b1f5ba67d
commitay <commitay@users.noreply.github.com>
2017-11-24 12:07:35 +1000

    Update qsync-client to 3.2.1.1017 (#41282)

--- Casks/qsync-client.rb
+++ Casks/qsync-client.rb
@@ -1,8 +1,8 @@
 cask 'qsync-client' do
-  version :latest
-  sha256 :no_check
+  version '3.2.1.1017'
+  sha256 '59b51e7aa8b5a42f60d6ddfa780e8753d748365f2086d22b62bd6332c8ac188c'
 
-  url 'https://download.qnap.com/webstart/QNAPQsync_Mac.dmg'
+  url "https://download.qnap.com/Storage/Utility/QNAPQsyncClientMac-#{version}.dmg"
   name 'Qnap Qsync'
   homepage 'https://www.qnap.com/i/in/utility/#block_3'
 
