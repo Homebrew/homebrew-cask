102c5727ca288b6076e28e33c0f47b588db7b340
commitay <commitay@users.noreply.github.com>
2017-11-24 21:56:05 +1000

    comment

--- Casks/webponize.rb
+++ Casks/webponize.rb
@@ -2,7 +2,7 @@ cask 'webponize' do
   version :latest
   sha256 :no_check
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/1000ch/WebPonize was verified as official when first introduced to the cask
   url 'https://github.com/1000ch/WebPonize/raw/master/webponize.tar.gz'
   name 'WebPonize'
   homepage 'https://webponize.github.io/'
