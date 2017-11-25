9b609059e130932a85d849a499c23cf009d915ae
MS <mschreck@gmail.com>
2017-11-25 17:31:31 -0500

    Update electron-cash to latest version
    Updated from 2.9.3 to 2.9.4

--- Casks/electron-cash.rb
+++ Casks/electron-cash.rb
@@ -1,6 +1,6 @@
 cask 'electron-cash' do
-  version '2.9.3'
-  sha256 'bc0b983c1adbd13be3cf19cde15353872bb5ff91929b1a9271f9e3115ba8dacd'
+  version '2.9.4'
+  sha256 '834154ffecc6a1e77c5d0bd868905b573eef4e201bfd459aff2ab731dafb176d'
 
   url "https://electroncash.org/downloads/#{version}/mac/Electron-Cash-#{version}-macosx.dmg"
   name 'Electron Cash'
