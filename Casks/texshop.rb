1e6a643df49ecc044e6e6b185f8c8c7f3954f3cc
miccal <miccal.matthews@gmail.com>
2017-11-25 10:39:54 +0800

    Update texshop to 3.92

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,10 +1,10 @@
 cask 'texshop' do
-  version '3.91'
-  sha256 '74407940cfbf71d1504817b081622a7b7327de15b51e9e117b5f2f6a24aef795'
+  version '3.92'
+  sha256 '39874c04e306319206ec01bfde15c56689b3022a00b99a2e73c975c97aaade21'
 
   url "http://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.no_dots}.zip"
   appcast 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml',
-          checkpoint: '34eed1a36cb72de3011bffa7351f7bc89e5461e1a840c55d3f5f27674a09f8c5'
+          checkpoint: 'e557d3e81852a4c55b99196467e74b7faf0f96cc1138cd5bf70a0db71b79af6f'
   name 'TeXShop'
   homepage 'http://pages.uoregon.edu/koch/texshop/'
 
