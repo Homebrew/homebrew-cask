f433778859c42adde3c8e3c7d5bce8b2f5887e67
Julian Mayer <julian@corecode.io>
2017-11-27 17:12:59 +0100

    Update uninstallpkg to 1.1.1

--- Casks/uninstallpkg.rb
+++ Casks/uninstallpkg.rb
@@ -1,10 +1,10 @@
 cask 'uninstallpkg' do
-  version '1.0.29'
-  sha256 '8c9d6ecb29e786535490173d186c870bf6c95ea482e3687a76ff46b949449778'
+  version '1.1.1'
+  sha256 '24c29b2046751b85d78b196bb6f791cef689d18dc4a8996314b4c11855cf775c'
 
-  url 'https://www.corecode.io/downloads/uninstallpkg_latest.zip'
-  appcast 'https://www.corecode.io/uninstallpkg/history.html',
-          checkpoint: 'a2e0a1e27f2e3bfc4f2bd3872acdb117c73b4471baf137bb6b1587ec7932192b'
+  url 'https://www.corecode.io/downloads/uninstallpkg_1.1.1.zip'
+  appcast 'https://www.corecode.io/uninstallpkg/uninstallpkg.xml',
+          checkpoint: '274be382898eb3ec168bb1ade4ae7f184b82415e97ca7c9a495dc4702a25ee6d'
   name 'UninstallPKG'
   homepage 'https://www.corecode.io/uninstallpkg/'
 
