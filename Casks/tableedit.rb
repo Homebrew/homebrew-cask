2a6c7c8b0f79f1a23d923483f4d27bff6c24ca28
Julian Mayer <julian@corecode.io>
2017-11-27 17:28:39 +0100

    fix

--- Casks/tableedit.rb
+++ Casks/tableedit.rb
@@ -2,9 +2,15 @@ cask 'tableedit' do
   version '1.2.9'
   sha256 '980299dde4715a27e3abeac8c7c03efbca117b92029f6c1df69bcea1b6e517cb'
 
-  url '//https://www.corecode.io/downloads/tableedit_1.2.9.zip'
+  url 'https://www.corecode.io/downloads/tableedit_1.2.9.zip'
+  appcast 'https://www.corecode.io/tableedit/tableedit.xml',
+          checkpoint: '14389e6c97afa5886c16935ba61f67bd9d4efb3ad28de0daf4355b66066dd4b6'
   name 'TableEdit'
   homepage 'https://www.corecode.io/tableedit/'
 
   app 'TableEdit.app'
+
+  zap trash: [
+               '~/Library/Containers/com.corecode.TableEdit',
+             ]
 end
