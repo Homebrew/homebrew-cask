a4ee762f667e1234523cbe5c0389000c83cc3c21
Julian Mayer <julian@corecode.io>
2017-11-27 17:23:01 +0100

    add TableEdit

---
+++ Casks/tableedit.rb
@@ -0,0 +1,10 @@
+cask 'tableedit' do
+  version '1.2.9'
+  sha256 '980299dde4715a27e3abeac8c7c03efbca117b92029f6c1df69bcea1b6e517cb'
+
+  url '//https://www.corecode.io/downloads/tableedit_1.2.9.zip'
+  name 'TableEdit'
+  homepage 'https://www.corecode.io/tableedit/'
+
+  app 'TableEdit.app'
+end
