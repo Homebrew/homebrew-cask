d9772b689d5ae0e5fcb7a2bafcad7fa728ba635b
commitay <commitay@users.noreply.github.com>
2017-11-28 07:29:49 +1000

    Update uninstallpkg.rb

--- Casks/uninstallpkg.rb
+++ Casks/uninstallpkg.rb
@@ -2,7 +2,7 @@ cask 'uninstallpkg' do
   version '1.1.1'
   sha256 '24c29b2046751b85d78b196bb6f791cef689d18dc4a8996314b4c11855cf775c'
 
-  url 'https://www.corecode.io/downloads/uninstallpkg_1.1.1.zip'
+  url "https://www.corecode.io/downloads/uninstallpkg_#{version}.zip"
   appcast 'https://www.corecode.io/uninstallpkg/uninstallpkg.xml',
           checkpoint: '274be382898eb3ec168bb1ade4ae7f184b82415e97ca7c9a495dc4702a25ee6d'
   name 'UninstallPKG'
