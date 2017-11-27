db2193703a2051b0e2020789a9ba842b121aeba1
Julian Mayer <julian@corecode.io>
2017-11-27 17:33:53 +0100

    add VersionsManager 1.1.5

---
+++ Casks/versionsmanager.rb
@@ -0,0 +1,16 @@
+cask 'versionsmanager' do
+  version '1.1.5'
+  sha256 '79f99d88579d03598393cd624109d1d2e5a47b35b78bb9d4f1b75d55d9835e7e'
+
+  url 'https://www.corecode.io/downloads/versionsmanager_1.1.5.zip'
+  appcast 'https://www.corecode.io/versionsmanager/versionsmanager.xml',
+          checkpoint: 'ddf0a48915e5c98ac3799e54b70a2c15f14301e27af81d1d2e9da560a517fa69'
+  name 'VersionsManager'
+  homepage 'https://www.corecode.io/versionsmanager/'
+
+  app 'VersionsManager.app'
+
+  zap trash: [
+               '~/Library/Containers/com.corecode.VersionsManager',
+             ]
+end
