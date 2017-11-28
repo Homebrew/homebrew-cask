c39534d6ce701654f3c3aeae2964effd4a6089a6
Miguel Mendes Ruiz <migmruiz@gmail.com>
2017-11-28 18:55:29 -0200

    Add MollyGuard Latest Cask
    adding MollyGuard.app version 1.0.0 Cask

---
+++ Casks/mollyguard.rb
@@ -0,0 +1,11 @@
+cask 'mollyguard' do
+  version '1.0.0'
+  sha256 '1db2745811987d4dbb94aa586b91199eff3b5d715b684ab93c789231c0ca4f13'
+
+  # dl.dropboxusercontent.com was verified as official when first introduced to the cask
+  url 'https://dl.dropboxusercontent.com/s/e2l2dk2qz1u93cb/MollyGuard.dmg?dl=1'
+  name 'MollyGuard'
+  homepage 'http://mollyguard.infinitemonkeytheory.com/'
+
+  app 'MollyGuard.app'
+end
