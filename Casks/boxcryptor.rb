97fc2b5aa3044de8bc24fb55cdc8fa34e14bc941
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2017-11-28 23:23:21 +0100

    Boxcryptor 2.17.892 (#41262)
    * Boxcryptor 2.17.892
    
    * depends_on yosemite

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,14 +1,14 @@
 cask 'boxcryptor' do
-  version '2.16.880'
-  sha256 '47c0a9486d9b80ca0582d875a22307ff31320e36d036d5ee79bf4f2a55aafa02'
+  version '2.17.892'
+  sha256 '3d3069762effe09cf981e3b6668e53bef978725152706b641b0cc4dda5273d96'
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8',
-          checkpoint: '5294c8d43ba3e498dce1e23fe1cf11279292237eb22823573d88bf5f75b22efb'
+          checkpoint: 'cc45bdd46499c1d7ca27750083b8c727f4ed76ca0108147ce43686d14c8264db'
   name 'Boxcryptor'
   homepage 'https://www.boxcryptor.com/en/'
 
-  depends_on macos: '>= :mavericks'
+  depends_on macos: '>= :yosemite'
 
   app 'Boxcryptor.app'
 end
