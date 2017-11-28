d2a224b18d41242f3687980215bf454507e2ebd2
commitay <commitay@users.noreply.github.com>
2017-11-28 19:37:28 +1000

    rename to flawless

--- Casks/flawless.rb
+++ Casks/flawless.rb
@@ -1,9 +1,9 @@
-cask 'flawlessapp' do
-  version '15,1510910181'
+cask 'flawless' do
+  version '0.7.2,15:1510910181'
   sha256 'a97272d778224066a9dedb97c48128a39437b01fb32acf862b73e8a36e205ae4'
 
   # dl.devmate.com/com.flawless.app.FlawlessMac was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.flawless.app.FlawlessMac/#{version.before_comma}/#{version.after_comma}/FlawlessMac-#{version.before_comma}.zip"
+  url "https://dl.devmate.com/com.flawless.app.FlawlessMac/#{version.after_comma.before_colon}/#{version.after_colon}/FlawlessMac-#{version.after_comma.before_colon}.zip"
   appcast 'http://updates.devmate.com/com.flawless.app.FlawlessMac.xml',
           checkpoint: '549d6cc2b59b07467eb6a80524f879ce3a7197427b05daf632a4014f8df73cfc'
   name 'Flawless App'
