32aa70d05d8c85361e26c74898e0caac1b0aaa6e
commitay <commitay@users.noreply.github.com>
2017-11-28 20:42:00 +1000

    checkpoint

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -4,7 +4,7 @@ cask 'visual-paradigm-ce' do
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vpce#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html',
-          checkpoint: '0faa8a7e1b7d77130add1edd6d95a6795fc4a113d2583ef7d65578233309b945'
+          checkpoint: '4a241cbf4ee48c25dfcc2a5b86fbb28b25a552b34f31447a33d6c66712fd49ab'
   name 'Visual Paradigm Community Edition'
   homepage 'https://www.visual-paradigm.com/'
 
