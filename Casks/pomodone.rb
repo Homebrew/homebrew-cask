0c09386fcc03e0575e7cb5abcc3b80b4fd3346fd
Pelle ten Cate <pelle@occsn.com>
2017-11-29 14:22:06 +0100

    Update pomodone to 1.5.1190

--- Casks/pomodone.rb
+++ Casks/pomodone.rb
@@ -1,6 +1,6 @@
 cask 'pomodone' do
-  version '1.5.1163'
-  sha256 '476d22afec5932be416792e4ced8d8b7407607ccaf0efaef2329efd513785a36'
+  version '1.5.1190'
+  sha256 '529de40429a02589dd699d257393076b1e854801225b2600966e442594da801f'
 
   url "https://app.pomodoneapp.com/installers/PomoDoneApp-#{version}.dmg"
   name 'PomoDone'
