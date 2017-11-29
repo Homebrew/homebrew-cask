70b7486c4e1b06a3f1e9a6ae717a9386b09a2377
commitay <commitay@users.noreply.github.com>
2017-11-29 17:27:20 +1000

    Update harmony to 0.8.1 (#41409)

--- Casks/harmony.rb
+++ Casks/harmony.rb
@@ -1,11 +1,11 @@
 cask 'harmony' do
-  version '0.8.0'
-  sha256 'af5a1cd49cc9874d54d32ec8d10d49ed3f1cf0fd223b90f039998581a9e3275d'
+  version '0.8.1'
+  sha256 '25fbed3f5b829b143a057e46646c536076afe5ae018685e999edd7ce0219276c'
 
   # github.com/vincelwt/harmony was verified as official when first introduced to the cask
   url "https://github.com/vincelwt/harmony/releases/download/v#{version}/harmony-#{version}-mac.app.zip"
   appcast 'https://github.com/vincelwt/harmony/releases.atom',
-          checkpoint: '4e40043e6276282cb9032c81609f9984677881d0a41f8d876b6e51f839dcf3d8'
+          checkpoint: '26532afc689cd9e557babfde8dba6a8714f043e990ee95d2f1d31ae3b910adca'
   name 'Harmony'
   homepage 'https://getharmony.xyz/'
 
