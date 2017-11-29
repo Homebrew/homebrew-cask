105fae3feb2ac94b9a69196e4a78473474a036a7
miccal <miccal.matthews@gmail.com>
2017-11-29 20:58:18 +0800

    Update chalk.rb

--- Casks/chalk.rb
+++ Casks/chalk.rb
@@ -13,6 +13,7 @@ cask 'chalk' do
 
   zap trash: [
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/fr.chachatelier.pierre.chalk.sfl*',
+               '~/Library/Autosave Information/Calculator.chalk',
                '~/Library/Caches/fr.chachatelier.pierre.Chalk',
                '~/Library/Cookies/fr.chachatelier.pierre.Chalk.binarycookies',
                '~/Library/Preferences/fr.chachatelier.pierre.Chalk.plist',
