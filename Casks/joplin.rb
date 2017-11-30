dde275f9a321e46a8c8eea37c3059d7c8107697b
commitay <commitay@users.noreply.github.com>
2017-11-30 23:39:48 +1000

    homepage / zap

--- Casks/joplin.rb
+++ Casks/joplin.rb
@@ -2,11 +2,19 @@ cask 'joplin' do
   version '0.10.28'
   sha256 '71a9dc3fb9041b803982458246ee14bf62e595e83738ead48c7b86e38bdf339d'
 
+  # github.com/laurent22/joplin was verified as official when first introduced to the cask
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}.dmg"
   appcast 'https://github.com/laurent22/joplin/releases.atom',
           checkpoint: 'd6732e579a53fc37747a86924fa6b74fd770b4b093869053123b0bd500cf15bc'
   name 'Joplin'
-  homepage 'https://github.com/laurent22/joplin'
+  homepage 'http://joplin.cozic.net/'
 
   app 'Joplin.app'
+
+  zap trash: [
+               '~/Library/Application Support/Joplin',
+               '~/Library/Preferences/net.cozic.joplin-desktop.helper.plist',
+               '~/Library/Preferences/net.cozic.joplin-desktop.plist',
+               '~/Library/Saved Application State/net.cozic.joplin-desktop.savedState',
+             ]
 end
