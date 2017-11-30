9b1d6516937180008d77f2ff620ff1aa087f5c95
Jordan Potarf <jordan_potarf@baylor.edu>
2017-11-30 00:32:33 -0600

    Update lego-digital-designer: new download url
    The previous url no longer works. Updated Cask to reflect new location.

--- Casks/lego-digital-designer.rb
+++ Casks/lego-digital-designer.rb
@@ -2,7 +2,8 @@ cask 'lego-digital-designer' do
   version '4.3.10'
   sha256 'd48ccbf6b3eb6cf0115d07cf499c45c10c479f7f6e8ac32a3d44821325b56908'
 
-  url "http://cache.lego.com/downloads/ldd2.0/installer/setupLDD-MAC-#{version.dots_to_underscores}.zip"
+  # lc-www-live-s.legocdn.com was verified as official when first introduced to the cask
+  url "https://lc-www-live-s.legocdn.com/downloads/ldd2.0/installer/setupLDD-MAC-#{version.dots_to_underscores}.zip"
   name 'Lego Digital Designer'
   homepage 'http://ldd.lego.com/'
 
