2a795e654020252a26ddb4ade4cef6de8115c8be
atton <e115763@gmail.com>
2017-11-30 15:23:54 +0900

    Add Karbowanec.app 1.1.9

---
+++ Casks/karbowanec.rb
@@ -0,0 +1,10 @@
+cask 'karbowanec' do
+  version '1.1.9'
+  sha256 'aec84c217fd04714d20f429062c7e0d89117b43d6532b76c8d85f216e3c8385a'
+
+  url 'https://github.com/seredat/karbowanecwallet/releases/download/v1.1.9/Karbowanec_wallet_1.1.9.dmg'
+  name 'karbowanec'
+  homepage 'https://karbowanec.com'
+
+  app 'karbowanec.app'
+end
