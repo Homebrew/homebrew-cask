6c447b2b45cc5e15755b313bbda82973b4635a5a
commitay <commitay@users.noreply.github.com>
2017-11-30 13:26:21 +1000

    Update script.sh: add checks

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -10,6 +10,20 @@
 
 header 'Running script.sh...'
 
+apps () { /usr/bin/find /Applications -maxdepth 2 ; }
+kexts () { "$(brew --repository)/Library/Taps/caskroom/homebrew-cask/developer/bin/list_loaded_kext_ids" ; }
+launchjob_install () { "$(brew --repository)/Library/Taps/caskroom/homebrew-cask/developer/bin/list_installed_launchjob_ids" ; }
+launchjob_load () { "$(brew --repository)/Library/Taps/caskroom/homebrew-cask/developer/bin/list_loaded_launchjob_ids" ; }
+pkgs () { "$(brew --repository)/Library/Taps/caskroom/homebrew-cask/developer/bin/list_recent_pkg_ids" ; }
+
+checks=('apps' 'kexts' 'launchjob_install' 'launchjob_load' 'pkgs')
+
+mkdir -p ~/cask-checks/{"before","after"}
+
+for check in "${checks[@]}"; do
+  "${check}" > ~/cask-checks/before/"${check}"
+done
+
 modified_ruby_files=($(git diff --name-only --diff-filter=AMR "${TRAVIS_COMMIT_RANGE}" -- *.rb))
 
 for file in "${modified_ruby_files[@]}"; do
@@ -32,3 +46,11 @@ elif [[ ${#modified_casks[@]} -gt 0 ]]; then
 else
   ohai 'No Casks modified, skipping'
 fi
+
+for check in "${checks[@]}"; do
+  "${check}" > ~/cask-checks/after/"${check}"
+  if [[ $(/usr/bin/diff ~/cask-checks/before/"${check}" ~/cask-checks/after/"${check}") > /dev/null ]]; then 
+    ohai "Leftover: $check"
+    /usr/bin/diff ~/cask-checks/before/"${check}" ~/cask-checks/after/"${check}" | /usr/bin/tail -n +2
+  fi
+done
