55ced13a8ad9d1c0823680be0ac0f4d9f6e618c3
Mark Gibson <mgibson@adaptavist.com>
2017-11-30 14:58:19 +0000

    Update intellij-idea to 2017.3,173.3727.127

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,10 +1,10 @@
 cask 'intellij-idea' do
-  version '2017.2.6,172.4574.11'
-  sha256 'f1195941e812aa9df84557d1f091a4769111507612d96c750b4b7faadd46b786'
+  version '2017.3,173.3727.127'
+  sha256 '27dccf32fedde0b417af64ea3188bc34f9675403049fdb6c4edb891c14b5fc46'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release',
-          checkpoint: 'f8552394009e4f7e580f0b3c10380d2cb51dcdedeefbdda28771e722407b13e3'
+          checkpoint: '907bccd36a09523126154c1215e8131cd9ad07c6d16ee18390fd0f07021d9f6d'
   name 'IntelliJ IDEA Ultimate'
   homepage 'https://www.jetbrains.com/idea/'
 
