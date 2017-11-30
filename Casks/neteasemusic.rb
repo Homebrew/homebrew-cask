d5e4a127a125aba6dfe392329de795f6abdcee75
nuomi1 <nuomi1@users.noreply.github.com>
2017-11-30 19:54:44 +0800

    Update neteasemusic.rb

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -12,7 +12,7 @@ cask 'neteasemusic' do
 
   uninstall quit: 'com.netease.163music'
 
-  zap delee: [
+  zap delete: [
                '~/Library/Caches/com.netease.163music',
                '~/Library/Cookies/com.netease.163music.binarycookies',
                '~/Library/Saved Application State/com.netease.163music.savedState',
