c35b725b951d9b1b2e91d5ac1046e7bce81bc556
commitay <commitay@users.noreply.github.com>
2017-11-30 13:04:51 +1000

    Update teamviewer-host to 13

--- Casks/teamviewer-host.rb
+++ Casks/teamviewer-host.rb
@@ -1,5 +1,5 @@
 cask 'teamviewer-host' do
-  version '12'
+  version '13'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://download.teamviewer.com/download/version_#{version}x/TeamViewerHost.dmg"
