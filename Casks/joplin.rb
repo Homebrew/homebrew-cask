19e07a0a035328c63714fb85412b2e9e87b3b978
commitay <commitay@users.noreply.github.com>
2017-12-02 07:17:36 +1000

    0.10.31

--- Casks/joplin.rb
+++ Casks/joplin.rb
@@ -1,11 +1,11 @@
 cask 'joplin' do
-  version '0.10.30'
-  sha256 '1f0792976ef928e3a74cb01f07f3030fa9ac777130da0ef12c52bcc469cee175'
+  version '0.10.31'
+  sha256 '8061efc03fe09fc0674a0e35f46ca9c6fa803afaa5c0a200193e5a30e148d3e0'
 
   # github.com/laurent22/joplin was verified as official when first introduced to the cask
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}.dmg"
   appcast 'https://github.com/laurent22/joplin/releases.atom',
-          checkpoint: '03d0942edc221321694fd98775c2b9e06d10dad00ed69f244bccadf66582c657'
+          checkpoint: '99bdbe53b8209ca00548b61ba0612a9a6d9c95185fa5c0b1b7c4a5f20e5723da'
   name 'Joplin'
   homepage 'http://joplin.cozic.net/'
 
