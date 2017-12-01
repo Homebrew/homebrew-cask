ee031e88a4649c7c2058275b40701fdd7daa5606
Vítor Galvão <info@vitorgalvao.com>
2017-12-01 21:22:14 +0000

    Update nordvpn.rb

--- Casks/nordvpn.rb
+++ Casks/nordvpn.rb
@@ -1,22 +1,8 @@
 cask 'nordvpn' do
-  if MacOS.version <= :yosemite
-    version '3.3.10'
-    sha256 'b5d1f687db32714a6aeb6b5b3046f5381e2e30528a1a58eacd3606b35bb09dfe'
-    url "https://downloads.nordcdn.com/apps/macos/10.10/NordVPN-OpenVPN/#{version}/NordVPN.dmg"
-  elsif MacOS.version <= :el_capitan
-    version '3.3.10'
-    sha256 'b5d1f687db32714a6aeb6b5b3046f5381e2e30528a1a58eacd3606b35bb09dfe'
-    url "https://downloads.nordcdn.com/apps/macos/10.11/NordVPN-OpenVPN/#{version}/NordVPN.dmg"
-  elsif MacOS.version <= :sierra
-    version '3.3.10'
-    sha256 'b5d1f687db32714a6aeb6b5b3046f5381e2e30528a1a58eacd3606b35bb09dfe'
-    url "https://downloads.nordcdn.com/apps/macos/10.12/NordVPN-OpenVPN/#{version}/NordVPN.dmg"
-  elsif MacOS.version <= :high_sierra
-    version '3.3.10'
-    sha256 'b5d1f687db32714a6aeb6b5b3046f5381e2e30528a1a58eacd3606b35bb09dfe'
-    url "https://downloads.nordcdn.com/apps/macos/10.12/NordVPN-OpenVPN/#{version}/NordVPN.dmg"
-  end
+  version '3.3.10'
+  sha256 'b5d1f687db32714a6aeb6b5b3046f5381e2e30528a1a58eacd3606b35bb09dfe'
 
+  url "https://downloads.nordcdn.com/apps/macos/10.10/NordVPN-OpenVPN/#{version}/NordVPN.dmg"
   appcast 'https://downloads.nordvpn.com/apps/osx/update.xml',
           checkpoint: '589e0827c0a95f54ba267fdd61bc61d7cde34068951828e151b8578415385955'
   name 'NordVPN'
