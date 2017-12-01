d0e96b903bdbd9322315c361b96b5d1c22035fd3
commitay <commitay@users.noreply.github.com>
2017-12-02 08:22:43 +1000

    Update meshcommander.rb

--- Casks/meshcommander.rb
+++ Casks/meshcommander.rb
@@ -2,8 +2,8 @@ cask 'meshcommander' do
   version '0.4.7'
   sha256 '6f15b8c76f5f7cb5d8347cd1497463d8f7f20f6ddae564945ea71f38629282cd'
 
-  # bintray.com was verified as official when first introduced to the cask
-  url 'https://bintray.com/gomesjj/APPS/download_file?file_path=MeshCommander_0_4_7.dmg.zip'
+  # bintray.com/gomesjj/APPS was verified as official when first introduced to the cask
+  url "https://bintray.com/gomesjj/APPS/download_file?file_path=MeshCommander_#{version.dots_to_underscores}.dmg.zip"
   name 'MeshCommander'
   homepage 'http://www.devtty.uk/apple/Intel_Mesh_Commander_on_Mac_OS_X/'
 
