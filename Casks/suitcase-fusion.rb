6da508a7afa2d047cac8567f923d22ef69ba1be5
Stephen Ginn <callaginn@users.noreply.github.com>
2017-12-01 00:39:00 -0600

    Add Suitcase Fusion.app v19.0.2
    Created a new Cask for Extensis Suitcase Fusion, a font manager for macOS.

---
+++ Casks/suitcase-fusion.rb
@@ -0,0 +1,10 @@
+cask 'suitcase-fusion' do
+  version '19.0.2'
+  sha256 '50b716fe0dd2ceddd2d14741bcfb99ee67b1f845583a68c260a62c95f26f884f'
+
+  url 'https://bin.extensis.com/SuitcaseFusion8-M-19-0-2.dmg'
+  name 'Extensis Suitcase Fusion'
+  homepage 'https://www.extensis.com/'
+
+  app 'Suitcase Fusion.app'
+end
