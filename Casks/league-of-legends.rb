afc1c758b8f10dcc5d1d707930e9b1e3b788c918
Peter Souter <petems@users.noreply.github.com>
2017-12-02 14:44:26 +0000

    Update league-of-legends.rb: update url
    * Launcher has been moved to a new location

--- Casks/league-of-legends.rb
+++ Casks/league-of-legends.rb
@@ -3,7 +3,7 @@ cask 'league-of-legends' do
   sha256 :no_check
 
   # riotgamespatcher-a.akamaihd.net was verified as official when first introduced to the cask
-  url 'https://riotgamespatcher-a.akamaihd.net/Mac_ShellInstaller/NA/League%20of%20Legends%20NA.dmg'
+  url 'https://riotgamespatcher-a.akamaihd.net/releases/Maclive/installer/deploy/League%20of%20Legends%20installer%20NA.dmg'
   name 'League of Legends'
   homepage 'https://leagueoflegends.com/'
 
