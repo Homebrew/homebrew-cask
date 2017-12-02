d10ccd5de77dd320b484187c7798b69c01b4e916
commitay <commitay@users.noreply.github.com>
2017-12-03 08:13:12 +1000

    Update nanobox to 2.3.0

--- Casks/nanobox.rb
+++ Casks/nanobox.rb
@@ -1,11 +1,11 @@
 cask 'nanobox' do
   version '2.3.0'
-  sha256 'db475905b0907afdd46e03227ebd7c3df0e3270f28544f2fd16de54142656e2e'
+  sha256 '1ab3501db17a017debbdf8e1f3cba755ec62e2cf3bec90ac7bf9703e9583477a'
 
   # s3.amazonaws.com/tools.nanobox.io was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/tools.nanobox.io/installers/v#{version.major}/mac/Nanobox.pkg"
   appcast 'https://github.com/nanobox-io/nanobox/releases.atom',
-          checkpoint: '9c76c6fe3909ff7e2dd99a7468a7c59cf93d80a63e81947a340d48fa7994a71c'
+          checkpoint: 'fe081e2388dd048ce02ab30b1cdd1f2225d609201af633b1858c344c324aef2c'
   name 'nanobox'
   homepage 'https://www.nanobox.io/'
 
