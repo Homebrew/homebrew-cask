87eff38b763304563fe6d8b74a5f1b117f91bf17
commitay <commitay@users.noreply.github.com>
2017-12-03 08:21:25 +1000

    add comment

--- Casks/nordvpn.rb
+++ Casks/nordvpn.rb
@@ -2,7 +2,8 @@ cask 'nordvpn' do
   version '3.3.10'
   sha256 'b5d1f687db32714a6aeb6b5b3046f5381e2e30528a1a58eacd3606b35bb09dfe'
 
-  url "https://downloads.nordcdn.com/apps/macos/10.10/NordVPN-OpenVPN/#{version}/NordVPN.dmg"
+  # downloads.nordcdn.com/apps was verified as official when first introduced to the cask
+  url "https://downloads.nordcdn.com/apps/macos/10.12/NordVPN-OpenVPN/#{version}/NordVPN.dmg"
   appcast 'https://downloads.nordvpn.com/apps/osx/update.xml',
           checkpoint: '589e0827c0a95f54ba267fdd61bc61d7cde34068951828e151b8578415385955'
   name 'NordVPN'
