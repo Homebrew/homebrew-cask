a068d593bd8b7ce5ed91afe9e797bcf550b02b72
Gaurav Agarwal <gauravagarwalr@gmail.com>
2017-12-03 20:21:12 +0530

    Updating odrive to 6344.

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -1,6 +1,6 @@
 cask 'odrive' do
-  version '6306'
-  sha256 'e3b5287a8a0c1d61f3cbcf9af2ebb6c7ea7ab08d08b1f5c433a9474a4ff10a59'
+  version '6344'
+  sha256 'f6af36bfef76b9d6b5d192392f3d807d31f21320e7b80567131d4055c2ba7ab8'
 
   # downloads can be found at https://www.odrive.com/downloaddesktop
   # d3huse1s6vwzq6.cloudfront.net was verified as official when first introduced to the cask
