5558e78a7ee75b4f19a0ec94755a05283a3a1e7a
jcbot <jcb@leipert.io>
2017-12-03 23:35:01 +0100

    Update goland to 2017.3,173.3727.144 (#41621)

--- Casks/goland.rb
+++ Casks/goland.rb
@@ -1,18 +1,16 @@
 cask 'goland' do
-  # GoLand is EAP only for now
-  version '2017.3 EAP,173.3727.96'
-  sha256 'c64e67df12cc549e61e480bb68be1eec4b3eb5de4ee7fc463fc675a166f3ded1'
+  version '2017.3,173.3727.144'
+  sha256 '32fd2e5e67ed14412d1b5a120680f30c61cd84088756dc220870a573c595c5fe'
 
-  url "https://download.jetbrains.com/go/goland-#{version.after_comma}.dmg"
-  appcast 'https://data.services.jetbrains.com/products/releases?code=GO&latest=true&type=eap',
-          checkpoint: '068e574c0f457279e5b4eafd365d0b2c61fbf1f553b38ac83de3dba074a977fa'
+  url "https://download.jetbrains.com/go/goland-#{version.before_comma}.dmg"
+  appcast 'https://data.services.jetbrains.com/products/releases?code=GO&latest=true&type=release',
+          checkpoint: 'c43024fbe8fe751ee7601c2c904804da1f31ebf92eedb73e16f870224baabc8a'
   name 'Goland'
-  name 'Goland EAP'
   homepage 'https://www.jetbrains.com/go/'
 
   auto_updates true
 
-  app "GoLand #{version.before_comma}.app"
+  app 'GoLand.app'
 
   uninstall_postflight do
     ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'goland') }.each { |path| File.delete(path) if File.exist?(path) && File.readlines(path).grep(%r{# see com.intellij.idea.SocketLock for the server side of this interface}).any? }
