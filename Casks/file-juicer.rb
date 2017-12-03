533a64d2d1a7eb6cd548ebdd7bf3807ead2c8e20
Yaw Anokwa <yanokwa@users.noreply.github.com>
2017-12-02 22:13:53 -0800

    Update file-juicer to 4.64 (#41607)

--- Casks/file-juicer.rb
+++ Casks/file-juicer.rb
@@ -1,6 +1,6 @@
 cask 'file-juicer' do
   version '4.64'
-  sha256 '8ae0daae1764440a09fb68b2db1739e5fae2f209261efb85ce1594b77526bab5'
+  sha256 '184236048e1e687d9b3717231644f08bc5ed6b3469393da89ed6c8f17510cd35'
 
   url "https://echoone.com/filejuicer/FileJuicer-#{version}.zip"
   name 'File Juicer'
