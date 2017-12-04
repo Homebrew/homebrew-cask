443df52a12e746ce1ced9e68a5adaa4341e7deaf
commitay <commitay@users.noreply.github.com>
2017-12-04 18:53:11 +1000

    {version.major}

--- Casks/startupizer.rb
+++ Casks/startupizer.rb
@@ -3,16 +3,16 @@ cask 'startupizer' do
   sha256 'c9d469c263ac1ac866ea067214e2b20e0583bb0763e1180d0e4cdc311ae52318'
 
   # dl.devmate.com/com.gentlebytes.Startupizer2 was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.gentlebytes.Startupizer2/#{version.after_comma.before_colon}/#{version.after_colon}/Startupizer2-#{version.after_comma.before_colon}.zip"
-  appcast 'https://updates.devmate.com/com.gentlebytes.Startupizer2.xml',
+  url "https://dl.devmate.com/com.gentlebytes.Startupizer2/#{version.after_comma.before_colon}/#{version.after_colon}/Startupizer#{version.major}-#{version.after_comma.before_colon}.zip"
+  appcast "https://updates.devmate.com/com.gentlebytes.Startupizer#{version.major}.xml",
           checkpoint: '2329c9571a65c4c5283f5d5d7c8b6553ad423a8c8a1f287ca35ec81b7d82a6d9'
-  name 'Startupizer2'
+  name "Startupizer#{version.major}"
   homepage 'http://gentlebytes.com/startupizer/'
 
-  app 'Startupizer2.app'
+  app "Startupizer#{version.major}.app"
 
   zap trash: [
-               '~/Library/Caches/com.gentlebytes.Startupizer2',
-               '~/Library/Preferences/com.gentlebytes.Startupizer2.plist',
+               "~/Library/Caches/com.gentlebytes.Startupizer#{version.major}",
+               "~/Library/Preferences/com.gentlebytes.Startupizer#{version.major}.plist",
              ]
 end
