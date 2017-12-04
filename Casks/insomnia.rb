6e242ff962cbb792b94227df7f6dc42d97ec801a
commitay <commitay@users.noreply.github.com>
2017-12-04 22:56:57 +1000

    Update insomnia to 5.12.4 (#41630)

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,11 +1,11 @@
 cask 'insomnia' do
-  version '5.11.7'
-  sha256 'd724fd3b0ae97f771c55db00c56d54482312355e3f3a305e141a9d56cd0ed1d1'
+  version '5.12.4'
+  sha256 '220f0bd8d1741c6a1edc3f1f8856a5810d41bb7d5c62a98a670aec1621ac1066'
 
   # github.com/getinsomnia/insomnia was verified as official when first introduced to the cask
   url "https://github.com/getinsomnia/insomnia/releases/download/v#{version}/Insomnia-#{version}.dmg"
   appcast 'https://github.com/getinsomnia/insomnia/releases.atom',
-          checkpoint: '06117303f694f91f06c919299e14297072b3ac051fa77ddc0acc45ea62cd4498'
+          checkpoint: '1338d3fb0e98efc57b487eeb366932816f88d3ef83b1c36819a3bdd006137138'
   name 'Insomnia'
   homepage 'https://insomnia.rest/'
 
