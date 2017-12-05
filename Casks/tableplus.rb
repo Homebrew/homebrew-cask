e2a271b6e3a55f2b347c7daa0b93af8d9fdc991c
Diogo Azevedo <diogoazevedos@gmail.com>
2017-12-05 10:59:44 -0200

    Update tableplus to 1.0,74

--- Casks/tableplus.rb
+++ Casks/tableplus.rb
@@ -1,10 +1,10 @@
 cask 'tableplus' do
-  version '1.0,72'
-  sha256 '7da1aa85226ebe9e09e3c4b36abc8e4279655cfb5755f7662147710f055c8ff1'
+  version '1.0,74'
+  sha256 'fe1d5024fe86d3e0b16a04ff82e163472a8a631a113212aebdc4aeece1a7a8b1'
 
   url 'https://tableplus.io/release/osx/tableplus_latest.zip'
   appcast 'https://tableplus.io/osx/version.xml',
-          checkpoint: '9a87a41a66e8d2f855db02d7c12f27e364d6ee2aa9388bcc66635e719ed92c6b'
+          checkpoint: '6896d088bbef23869d78bc62b81b74062c38b1fe0c9bd10b50e792f0f5890087'
   name 'TablePlus'
   homepage 'https://tableplus.io/'
 
