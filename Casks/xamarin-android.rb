df2f6b53fb9e7b898ad772ce577a8262df87753d
Alex Petin <axp@ngs.ru>
2017-12-05 20:00:04 +0700

    Update xamarin-android to 8.1.0-24 (#41651)

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-android' do
-  version '8.0.2-1'
-  sha256 '5b1ad2c4c75fde8074e959acff0f9fb521ec713c59add8c76ac07ca9b1a65f7c'
+  version '8.1.0-24'
+  sha256 'c054680004907d38302b071edcc4e82b652ad9b1e3456e9995629cea63308614'
 
   url "https://dl.xamarin.com/MonoforAndroid/Mac/xamarin.android-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: 'baafed12ce3818833dbae144410143f428b4719b117fe33ec6634653d3762630'
+          checkpoint: '7cff51bb6b9eb86014ab5f0f12cd3d01b301b5548fbba9e575d016d91fc1f553'
   name 'Xamarin.Android'
   homepage 'https://www.xamarin.com/platform'
 
