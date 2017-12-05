d7f8f970a19d27cb6a3b3397dbf517cea687fb01
Alex Petin <axp@ngs.ru>
2017-12-05 19:59:46 +0700

    Update xamarin-ios to 11.4.0.214 (#41650)

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -1,10 +1,10 @@
 cask 'xamarin-ios' do
-  version '11.3.0.47'
-  sha256 '94da4adb80f1bf1ad32fb563cfe7ce2fefef442a9107a0a13e05685f2a4875ab'
+  version '11.4.0.214'
+  sha256 '44429b4a39636dadcfd3e7fed081d6cf1b7b0d1d7a362d09241b7258dbee9f93'
 
   url "https://dl.xamarin.com/MonoTouch/Mac/xamarin.ios-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '72454391ed3476126473c324f16b31b54518d0ab05c30aeb6c452b1ea1c61dce'
+          checkpoint: '7cff51bb6b9eb86014ab5f0f12cd3d01b301b5548fbba9e575d016d91fc1f553'
   name 'Xamarin.iOS'
   homepage 'https://www.xamarin.com/platform'
 
