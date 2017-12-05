bc7fbaf587dc0e9651287081964218ad9106252a
miccal <miccal.matthews@gmail.com>
2017-12-05 08:45:16 +0800

    Update libreoffice.rb

--- Casks/libreoffice.rb
+++ Casks/libreoffice.rb
@@ -8,7 +8,10 @@ cask 'libreoffice' do
   homepage 'https://www.libreoffice.org/'
   gpg "#{url}.asc", key_id: 'c2839ecad9408fbe9531c3e9f434a1efafeeaea3'
 
-  conflicts_with cask: 'libreoffice-still'
+  conflicts_with cask: [
+                         'libreoffice-rc',
+                         'libreoffice-still',
+                       ]
   depends_on macos: '>= :mountain_lion'
 
   app 'LibreOffice.app'
