3ca56225d62e4e73617bdcc7659efc9ad89bb487
cooclsee <cooclsee@qq.com>
2017-12-06 07:56:35 +0800

    Update quip to 5.1.58

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -1,6 +1,6 @@
 cask 'quip' do
-  version '5.1.54'
-  sha256 '59bfbecfcc50d47a61cf9890120a8792fd6d35229773acce1d0ff690a1f9bbfd'
+  version '5.1.58'
+  sha256 '4946b7e4ab70eca7e646b38bc3efa58b5d3d8e99f35fbd64249fb54b384677e0'
 
   # d2i1pl9gz4hwa7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2i1pl9gz4hwa7.cloudfront.net/macosx_#{version}.dmg"
