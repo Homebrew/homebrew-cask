aefb8e4d5a7ff85c2d682b38591ac96bf2e41b78
commitay <commitay@users.noreply.github.com>
2017-12-06 07:35:19 +1000

    java, zulu: remove <= mavericks conditionals (#41623)

--- Casks/zulu.rb
+++ Casks/zulu.rb
@@ -24,28 +24,12 @@ cask 'zulu' do
     system_command '/usr/libexec/PlistBuddy',
                    args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk/Contents/Info.plist"],
                    sudo: true
-
-    if MacOS.version <= :mavericks
-      system_command '/bin/rm',
-                     args: ['-rf', '--', '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK'],
-                     sudo: true
-      system_command '/bin/ln',
-                     args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk/Contents", '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK'],
-                     sudo: true
-    end
   end
 
-  uninstall pkgutil: [
-                       "com.azulsystems.zulu.#{version.major}",
-                     ],
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}",
             delete:  [
                        "/Library/Java/JavaVirtualMachines/zulu-#{version}.jdk",
                        "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk",
                        '/Library/Java/Home',
-                       if MacOS.version <= :mavericks
-                         [
-                           '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK',
-                         ]
-                       end,
-                     ].keep_if { |v| !v.nil? }
+                     ]
 end
