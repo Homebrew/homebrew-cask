e212baa96e5623b9ad26549dc93ceb9e889b4724
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-12-05 21:04:16 +0000

    Update appcast checkpoint polyphone 1.9

--- Casks/polyphone.rb
+++ Casks/polyphone.rb
@@ -3,8 +3,8 @@ cask 'polyphone' do
   sha256 '40d6a28e941d7ccc398782154f93290e1f9e3fbe7b9754c394f32e6a92636efa'
 
   url "https://polyphone-soundfonts.com/judownload/0_1512442550_rk9xVE/polyphone-#{version}.dmg"
-  appcast 'https://github.com/davy7125/polyphone/releases',
-          checkpoint: '9b28ebf9c77bc731936a19d72ac70a7fbe5e1d3c15a8ad14ffd0887bf555461d'
+  appcast 'https://github.com/davy7125/polyphone/releases.atom',
+          checkpoint: 'fcb07b0b711afece99dc255574155ca30decb45887d7a0e9ddee9ba211561843'
   name 'Polyphone'
   homepage 'https://polyphone-soundfonts.com/'
 
