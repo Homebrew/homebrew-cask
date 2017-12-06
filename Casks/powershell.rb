5f48076fc7b11bde44adbdf01b241e9c4d5dcc43
Mark Kraus <github@markekraus.com>
2017-12-05 19:03:44 -0600

    Add GSSAPI support to curl

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -30,7 +30,7 @@ cask 'powershell' do
   caveats <<~EOS
     A OpenSSL-backed libcurl is required for custom handling of certificates.
     This is rarely needed, but you can install it with
-      brew install curl --with-openssl
+      brew install curl --with-openssl --with-gssapi
     See https://github.com/PowerShell/PowerShell/issues/2211
   EOS
 end
