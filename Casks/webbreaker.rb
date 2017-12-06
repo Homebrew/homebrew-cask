490856152e163b4be63f9b85c07bab2cd9b18065
Jim Nelson <jim.nelson2@target.com>
2017-12-06 12:03:01 -0600

    webbreaker (new cask)
    New cask for webbreaker, version 2.0.04

---
+++ Casks/webbreaker.rb
@@ -0,0 +1,10 @@
+cask 'webbreaker' do
+  version '2.0.04'
+  sha256 'a3a962048a3a76dbf676de5c187e0fbcec03ce9d50a63fd3ab5cd0718a1531ff'
+
+  url 'https://github.com/target/webbreaker/releases/download/2.0.04/webbreaker.dmg'
+  name 'webbreaker'
+  homepage 'https://github.com/target/webbreaker'
+
+  app 'webbreaker.app'
+end
