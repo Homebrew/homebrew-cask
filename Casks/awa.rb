15deabd8d81675820acc242de240e9c3b623993c
Torgayev Tamirlan <tamirlan_torgayev@cyberagent.co.jp>
2017-12-06 16:32:54 +0900

    Add SHA256 checksum

--- Casks/awa.rb
+++ Casks/awa.rb
@@ -1,6 +1,6 @@
 cask 'awa' do
   version '1.4.2'
-  sha256 :no_check
+  sha256 '3fbd1cc851d4157c6a8abc340a4200eff9e2d0d51764b41b12929df09da4ce8c'
 
   # download-d.awa.io/mac/stable was verified as official when first introduced to the cask
   url "https://download-d.awa.io/mac/stable/AWASetup-v#{version}.dmg"
