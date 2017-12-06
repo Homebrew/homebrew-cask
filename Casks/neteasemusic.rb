0eb9d0825f259f676fd00c1c3f2f15f0324cbec5
dongxu <yandongxu@users.noreply.github.com>
2017-12-07 05:22:46 +0800

    upgrade neteasemusic to 1.5.8_600 (#41679)

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -1,6 +1,6 @@
 cask 'neteasemusic' do
-  version '1.5.7_580'
-  sha256 'a1ede5a8e3ec027ec670fb989dbefa23bd5a5c021af56f16c2c24fa0ff26489a'
+  version '1.5.8_600'
+  sha256 '74c03dd8f60b9462df7937d99e75a6336115c76e058dd0e48348b5c35c68706d'
 
   # d1.music.126.net was verified as official when first introduced to the cask
   url "https://d1.music.126.net/dmusic/NeteaseMusic_#{version}_web.dmg"
