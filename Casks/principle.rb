7c0c0128874f87e4e94c84d890e9b5382b3912c6
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-12-06 20:40:25 +0000

    Update principle to 3.7

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,10 +1,10 @@
 cask 'principle' do
-  version '3.6'
-  sha256 '0cc3fa7635b3a7bdbadf6a2349bf45ab78bde5b8554c5aa1ddd49cbecbb5eb8c'
+  version '3.7'
+  sha256 '2f2a7c56b3c461bf22e4595596e405c74cf2ece48b529b8e1693c6d6e1c05519'
 
   url "http://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
   appcast 'http://principleformac.com/update2.xml',
-          checkpoint: 'e9153520f388d312b4eb6af72c6be70b53d90d57582cb6e8f7de234a35207238'
+          checkpoint: 'f8111d5e07f56a4da6c3bb91ae93991777559aab03eba3bb0279faf56b6bf32d'
   name 'Principle'
   homepage 'http://principleformac.com/'
 
