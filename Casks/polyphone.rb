22e0a3f9168b64d1c9176b9c02c7c5b983858715
commitay <commitay@users.noreply.github.com>
2017-12-06 10:23:54 +1000

    remove empty appcast

--- Casks/polyphone.rb
+++ Casks/polyphone.rb
@@ -3,8 +3,6 @@ cask 'polyphone' do
   sha256 '40d6a28e941d7ccc398782154f93290e1f9e3fbe7b9754c394f32e6a92636efa'
 
   url "https://polyphone-soundfonts.com/judownload/0_1512442550_rk9xVE/polyphone-#{version}.dmg"
-  appcast 'https://github.com/davy7125/polyphone/releases.atom',
-          checkpoint: 'fcb07b0b711afece99dc255574155ca30decb45887d7a0e9ddee9ba211561843'
   name 'Polyphone'
   homepage 'https://polyphone-soundfonts.com/'
 
