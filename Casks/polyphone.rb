f50a8367102458a81098fb0760b889cd50ae6a93
commitay <commitay@users.noreply.github.com>
2017-12-06 23:44:40 +1000

    Update polyphone.rb

--- Casks/polyphone.rb
+++ Casks/polyphone.rb
@@ -2,9 +2,9 @@ cask 'polyphone' do
   version '1.9'
   sha256 '40d6a28e941d7ccc398782154f93290e1f9e3fbe7b9754c394f32e6a92636efa'
 
-  url "https://polyphone-soundfonts.com/judownload/0_1512442550_rk9xVE/polyphone-#{version}.dmg"
+  url "https://polyphone-soundfonts.com/en/download/file/559-polyphone-#{version.dots_to_hyphens}-dmg/latest/download"
   name 'Polyphone'
   homepage 'https://polyphone-soundfonts.com/'
 
-  app 'Polyphone.app'
+  app 'polyphone.app'
 end
