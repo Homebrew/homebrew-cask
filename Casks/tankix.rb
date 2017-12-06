49bc4395d17c7102419e8828369a211b7499ec1d
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-12-06 21:53:35 +0000

    Update for https tankix.rb

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -2,7 +2,7 @@ cask 'tankix' do
   version '9172'
   sha256 'f16c70e091df0838e9250c6bf9cafb1fba47c3e2f9eca4e70d0c6cc5d558144c'
 
-  url "http://static.tankix.com/app/StandaloneOSXIntel64/master-#{version}/TankiX.dmg"
+  url "https://static.tankix.com/app/StandaloneOSXIntel64/master-#{version}/TankiX.dmg"
   name 'Tanki X'
   homepage 'https://www.tankix.com/'
 
