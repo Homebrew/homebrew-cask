f5ca7a0c973f87a533fb0821b1a82bc1c9ee1bf9
yandongxu <nextocean@gmail.com>
2017-12-07 15:23:51 +0800

    Upgrade netease music to 1.5.8_604

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -1,6 +1,6 @@
 cask 'neteasemusic' do
-  version '1.5.8_600'
-  sha256 '74c03dd8f60b9462df7937d99e75a6336115c76e058dd0e48348b5c35c68706d'
+  version '1.5.8_604'
+  sha256 '14027d6d3145c9bd88db94b88a5f84d51d248cffc5ccbcf753b2fc593bd8818a'
 
   # d1.music.126.net was verified as official when first introduced to the cask
   url "https://d1.music.126.net/dmusic/NeteaseMusic_#{version}_web.dmg"
