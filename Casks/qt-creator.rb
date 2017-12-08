40b09dbf8e12b557c087d700b5106f960e811b01
commitay <commitay@users.noreply.github.com>
2017-12-08 20:07:24 +1000

    Update qt-creator.rb

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -2,7 +2,7 @@ cask 'qt-creator' do
   version '4.5.0'
   sha256 'cb3aac65ddadaccef1bf62a95120f8d74d3e4454f7afcdcb5425f20051d24b5b'
 
-  url "https://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
+  url "http://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
   homepage 'https://www1.qt.io/developers/'
 
