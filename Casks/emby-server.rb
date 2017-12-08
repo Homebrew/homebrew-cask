02a6dbd19f2db417f5a8a097296d6d7fa7857dc0
Victor Popkov <victor@popkov.me>
2017-12-08 21:35:46 +0200

    Update emby-server to 3.2.50.0

--- Casks/emby-server.rb
+++ Casks/emby-server.rb
@@ -1,11 +1,11 @@
 cask 'emby-server' do
-  version '3.2.40.0'
-  sha256 'aa0037e889329425a4a6a666b5430ce672fe9e8cfef440692d0178d644db046d'
+  version '3.2.50.0'
+  sha256 'bb5a6bf701d604a751fdec8183db48cf457aa431040e9d0813a3634bd592705b'
 
   # github.com/MediaBrowser/Emby was verified as official when first introduced to the cask
   url "https://github.com/MediaBrowser/Emby/releases/download/#{version}/embyserver-osx-x64-#{version}.zip"
   appcast 'https://github.com/MediaBrowser/Emby/releases.atom',
-          checkpoint: '383a5fddba34b06fbeac266600a2755095f8fca2257c68dfc5472eb1cc59392d'
+          checkpoint: '4753cd74957ef927b539ba5b3b5d1a18eaa75ce6a5e27a9ce63650badf3625d0'
   name 'Emby Server'
   homepage 'https://emby.media/'
 
