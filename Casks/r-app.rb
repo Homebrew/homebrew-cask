568b052139406155eb5b41084207736fa70691c8
Bradley Rentz <bradley.rentz@gmail.com>
2017-12-07 21:44:59 -1000

    Update r-app to 3.4.3 (#41729)

--- Casks/r-app.rb
+++ Casks/r-app.rb
@@ -1,10 +1,10 @@
 cask 'r-app' do
-  version '3.4.2'
-  sha256 '34d6be8299f3b5f31707453a62245b93def2abab92f690563d254c87fa04bf9f'
+  version '3.4.3'
+  sha256 '97797d7689eb183f96b0e89fa6b43fb9fe4a5b11474cd5a3d7a8942ff6933416'
 
   url "https://cloud.r-project.org/bin/macosx/R-#{version}.pkg"
   appcast 'https://www.r-project.org/',
-          checkpoint: '83fe7222a6a6f38223460fc32d82da887cfc25aee2647ee6ec5695ee7953c1a0'
+          checkpoint: '192972a418e60142e3283e22e2aeebc642869e6f46049b96b583a44a51dbfffd'
   name 'R'
   homepage 'https://www.r-project.org/'
 
