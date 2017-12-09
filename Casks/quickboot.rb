6f0a12910ef538ab0bc600f3a6cad39948edcc5b
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-12-09 21:47:58 +0000

    Update quickboot to 1.1-92

--- Casks/quickboot.rb
+++ Casks/quickboot.rb
@@ -3,8 +3,8 @@ cask 'quickboot' do
   sha256 '5119e1113949baae165efc726b757d58a887743f1dc6ae8d03a978be7b5da4a3'
 
   url "https://buttered-cat.com/downloads/get/4/QuickBoot-#{version}.zip"
-  appcast 'https://buttered-cat.com/products/QuickBoot',
-          checkpoint: '66c84a167917f3233bb58b4f37106ff044656ce4455986fcc14e239f8028b7c5'
+  appcast 'https://buttered-cat.com/products/QuickBoot/',
+          checkpoint: 'f2285f9ca61ad62057fc44c106a2ac2775e72d484fb458ca82344dcb973df0fc'
   name 'QuickBoot'
   homepage 'https://buttered-cat.com/products/QuickBoot/'
 
