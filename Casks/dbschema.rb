f55174902920d4cd70c438122c4161abdf74db1b
goodwillcoding <goodwillcoding@webhippo.net>
2017-12-10 15:17:46 -0800

    update url to HTTPS

--- Casks/dbschema.rb
+++ Casks/dbschema.rb
@@ -2,7 +2,7 @@ cask 'dbschema' do
   version '7.6.4'
   sha256 'a19568384b2ef5c6cb741d3b93c3448ef379b94016fb8e4ae21e82a3bfa72810'
 
-  url "http://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
+  url "https://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
   name 'DbSchema'
   homepage 'http://www.dbschema.com/'
 
