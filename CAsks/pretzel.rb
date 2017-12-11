fa412d544459f1c8fe0ef3f00cc09422342184f5
Vítor Galvão <info@vitorgalvao.com>
2017-12-11 18:02:01 +0000

    Update and rename pretzel-rocks.rb to pretzel.rb

--- CAsks/pretzel.rb
+++ CAsks/pretzel.rb
@@ -1,6 +1,6 @@
-cask 'pretzel-rocks' do
-  version '0.0.15'
-  sha256 '4b8363b2f113abe9bd123e55922abc9d01bc64835e5ae812832d683905e6b72b'
+cask 'pretzel' do
+  version :latest
+  sha256 :no_check
 
   url 'https://download.pretzel.rocks/PretzelDesktop.dmg'
   name 'PretzelDesktop'
