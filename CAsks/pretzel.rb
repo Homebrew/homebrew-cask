8b3d4f481879bb659eb099c477836ce025036d75
Vítor Galvão <info@vitorgalvao.com>
2017-12-11 20:43:46 +0000

    Update pretzel.rb

--- CAsks/pretzel.rb
+++ CAsks/pretzel.rb
@@ -1,6 +1,6 @@
 cask 'pretzel' do
-  version :latest
-  sha256 :no_check
+  version '0.0.15'
+  sha256 '4b8363b2f113abe9bd123e55922abc9d01bc64835e5ae812832d683905e6b72b'
 
   url 'https://download.pretzel.rocks/PretzelDesktop.dmg'
   name 'Pretzel'
