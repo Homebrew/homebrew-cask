50f5652f634a31e8ee1e719aa2a2aadf2c9f74b3
CryoRenegade <jledbetter460@gmail.com>
2017-12-11 09:07:17 -0700

    Upgrade Pretzel.app to :latest

---
+++ CAsks/pretzel-rocks.rb
@@ -0,0 +1,10 @@
+cask 'pretzel-rocks' do
+  version '0.0.15'
+  sha256 '4b8363b2f113abe9bd123e55922abc9d01bc64835e5ae812832d683905e6b72b'
+
+  url 'https://download.pretzel.rocks/PretzelDesktop.dmg'
+  name 'PretzelDesktop'
+  homepage 'https://www.pretzel.rocks/'
+
+  app 'Pretzel.app'
+end
