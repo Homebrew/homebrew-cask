708a5c6a23c98550b1c08bbad3e4a9fc4cbe4079
Devan Beitel <devan@devanb.us>
2017-12-12 10:48:02 -0500

    Update graphql-playground to 1.3.7

--- Casks/graphql-playground.rb
+++ Casks/graphql-playground.rb
@@ -1,10 +1,10 @@
 cask 'graphql-playground' do
-  version '1.3.5'
-  sha256 '9f5fae194811f250e18d2f8b51dfb6bd71d95f967e206b5ed99aba0c3749245b'
+  version '1.3.7'
+  sha256 '59319011c690eb724c57a2b4618068246534977331d2a0bae9eda3643b4edbb1'
 
   url "https://github.com/graphcool/graphql-playground/releases/download/v#{version}/playground-#{version}-mac.zip"
   appcast 'https://github.com/graphcool/graphql-playground/releases.atom',
-          checkpoint: 'ac2d4c0ed7344c8e880721d603cb5148996b7ea9f9e0569eac4d95dc6aa004c5'
+          checkpoint: 'd88ff50212c417c51a62be4169a28f29324d9ea5a134fe98ac7603599a2f228b'
   name 'GraphQL Playground'
   homepage 'https://github.com/graphcool/graphql-playground'
 
