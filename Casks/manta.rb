dc9d5ee00f2b9b507a086374601a3ca2c136f957
Bran Sorem <contact@bransorem.com>
2017-12-12 13:11:49 -0800

    Add Manta.app v1.0.0

---
+++ Casks/manta.rb
@@ -0,0 +1,11 @@
+cask 'manta' do
+  version '1.0.0'
+  sha256 '8677bff56254e96333f66f175e93a12da5cab98b9f366e68d5eb50a399c9e483'
+
+  # github.com/hql287 was verified as official when first introduced to the cask
+  url 'https://github.com/hql287/Manta/releases/download/v1.0.0/Manta-1.0.0-mac.zip'
+  name 'Manta'
+  homepage 'https://manta.life/'
+
+  app 'Manta.app'
+end
