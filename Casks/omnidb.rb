f7a293c10a6a26d2bad5940d0263f0eeff138698
commitay <commitay@users.noreply.github.com>
2017-12-12 19:43:25 +1000

    Update omnidb.rb

--- Casks/omnidb.rb
+++ Casks/omnidb.rb
@@ -2,7 +2,7 @@ cask 'omnidb' do
   version '2.4.0'
   sha256 '75cd788e229d8a74ab71b498ac530910574b2bd955975023ccab882675a00c8c'
 
-  url 'https://omnidb.org/dist/2.4.0/omnidb-app_2.4.0-mac.dmg'
+  url "https://omnidb.org/dist/#{version}/omnidb-app_#{version}-mac.dmg"
   name 'OmniDB'
   homepage 'https://omnidb.org/'
 
