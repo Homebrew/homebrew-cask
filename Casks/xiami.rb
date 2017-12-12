77c2d29ca08e39b399601d8b63ea54e6eaa752fa
commitay <commitay@users.noreply.github.com>
2017-12-12 21:00:56 +1000

    https

--- Casks/xiami.rb
+++ Casks/xiami.rb
@@ -3,7 +3,7 @@ cask 'xiami' do
   sha256 'e2723e2832d49fac3f27dbb8d13454733c551afbd7a47359634bfde950b226c9'
 
   # gxiami.alicdn.com/xiami-desktop was verified as official when first introduced to the cask
-  url "http://gxiami.alicdn.com/xiami-desktop/update/%E8%99%BE%E7%B1%B3%E9%9F%B3%E4%B9%90-#{version}.dmg"
+  url "https://gxiami.alicdn.com/xiami-desktop/update/%E8%99%BE%E7%B1%B3%E9%9F%B3%E4%B9%90-#{version}.dmg"
   name 'Xiami'
   name '虾米音乐'
   homepage 'http://www.xiami.com/'
