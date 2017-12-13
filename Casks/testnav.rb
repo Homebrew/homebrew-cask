305d724a713a445256921ce1b95a80261002a107
jledbetter460 <jledbetter460@gmail.com>
2017-12-13 14:48:36 -0700

    Update testnav.rb

--- Casks/testnav.rb
+++ Casks/testnav.rb
@@ -2,7 +2,7 @@ cask 'testnav' do
   version '1.6.5'
   sha256 'bdc201983df28d34f8b4fab8b5b0cf9ae625631022e56a673ae6dbb5c9dfca0a'
 
-  url 'http://download.testnav.com/installers/testnav-1.6.5.dmg'
+  url "http://download.testnav.com/installers/testnav-#{version}.dmg"
   name 'TestNav'
   homepage "http://download.testnav.com/"
 
