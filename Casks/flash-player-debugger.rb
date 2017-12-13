3e2ed85ff491035830f2b0af07a671af5ea6db88
commitay <commitay@users.noreply.github.com>
2017-12-13 13:19:20 +1000

    Update flash-player-debugger to 28.0.0.126 (#41878)

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,11 +1,11 @@
 cask 'flash-player-debugger' do
-  version '27.0.0.187'
-  sha256 '6a923a732fc2a130349af619a77ad2780e49f5504dc58f1e34a351ac9cf89597'
+  version '28.0.0.126'
+  sha256 '802d43edd01bb540b7a8190d784825f4cab6f1074488e919ffd73f890000ca9e'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
-          checkpoint: '26279ca81dea1717dec4c2d638ee46a14c9caa731f6bd2bc9ac35380bdef0ec9'
+          checkpoint: '6674b9b037cf5dbd0dd77b4e34b88c7b1c9113091d2911daa87b1fdd0163369b'
   name 'Adobe Flash Player projector content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
