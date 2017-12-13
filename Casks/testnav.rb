1e9a312ba755802ad58b4f4f46887b9ed3921c52
jledbetter460 <jledbetter460@gmail.com>
2017-12-13 14:46:56 -0700

    Update testnav.rb

--- Casks/testnav.rb
+++ Casks/testnav.rb
@@ -3,8 +3,8 @@ cask 'testnav' do
   sha256 'bdc201983df28d34f8b4fab8b5b0cf9ae625631022e56a673ae6dbb5c9dfca0a'
 
   url 'http://download.testnav.com/installers/testnav-1.6.5.dmg'
-  name 'testnav-1.6.5.dmg'
-  homepage 'http://download.testnav.com/'
+  name 'TestNav'
+  homepage "http://download.testnav.com/"
 
   app 'TestNav.app'
 end
