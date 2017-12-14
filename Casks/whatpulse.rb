04d16b3ad5b941382399b6ac369037f3fb8887f2
commitay <commitay@users.noreply.github.com>
2017-12-14 14:55:01 +1000

    Update whatpulse to 2.8.1 (#41927)

--- Casks/whatpulse.rb
+++ Casks/whatpulse.rb
@@ -2,7 +2,7 @@ cask 'whatpulse' do
   version '2.8.1'
   sha256 'e9b0435874830fb9de0ee32411e918e12a8651c0e676daf9eccf84946d8ffd6d'
 
-  url "http://amcdn.whatpulse.org/files/whatpulse-mac-#{version}.dmg"
+  url "https://static.whatpulse.org/files/whatpulse-mac-#{version}.dmg"
   name 'WhatPulse'
   homepage 'https://whatpulse.org/'
 
