ee253e2a86a17f634c6bed4733d9ca4bf40ebf6b
commitay <commitay@users.noreply.github.com>
2017-12-14 16:39:18 +1000

    Update 3cxphone.rb

--- Casks/3cxphone.rb
+++ Casks/3cxphone.rb
@@ -1,8 +1,8 @@
 cask '3cxphone' do
-  version '15.1.1'
-  sha256 '8aa0f85a61f618ec257065f5b70ed0b4e6d222e1c7bb385b65f15402a4fd22c5'
+  version '15'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://downloads.3cx.com/downloads/3CXPhoneForMac#{version.major}.dmg"
+  url "https://downloads.3cx.com/downloads/3CXPhoneForMac#{version}.dmg"
   name '3CXPhone'
   homepage 'https://www.3cx.com/'
 
