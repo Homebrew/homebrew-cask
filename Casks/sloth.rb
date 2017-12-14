bc81c34cd44f64c01bb1a329a5e0e88949262b32
commitay <commitay@users.noreply.github.com>
2017-12-14 14:14:49 +1000

    Update sloth to 2.0 (#41923)

--- Casks/sloth.rb
+++ Casks/sloth.rb
@@ -2,8 +2,8 @@ cask 'sloth' do
   version '2.0'
   sha256 '7b2aa0e5234e862ed8b9f2a7640e74d170030f56dd27e3419fcd7252e2d7a6f4'
 
-  url 'http://sveinbjorn.org/files/software/sloth.zip'
-  appcast 'http://sveinbjorn.org/files/appcasts/SlothAppcast.xml',
+  url 'https://sveinbjorn.org/files/software/sloth.zip'
+  appcast 'https://sveinbjorn.org/files/appcasts/SlothAppcast.xml',
           checkpoint: 'cd8eaa41fa7b240923196574b5a5afa74842fbffd5e26aaa40a14187a45df469'
   name 'Sloth'
   homepage 'https://sveinbjorn.org/sloth'
