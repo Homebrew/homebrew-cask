32d0118e3a55f2e477d6a526d9041ca6a805da38
Zach Adams <zach@zach-adams.com>
2017-12-14 09:50:48 -0700

    Update dyn-updater to 5.4.2

--- Casks/dyn-updater.rb
+++ Casks/dyn-updater.rb
@@ -1,6 +1,6 @@
 cask 'dyn-updater' do
-  version :latest
-  sha256 :no_check
+  version '5.4.2'
+  sha256 '3fa46a028288ed7fee2cce7c10796b651af40b9293085ddf8aa4baad15bb6fd3'
 
   url 'http://cdn.dyn.com/dynupdater/DynUpdater.dmg'
   name 'Dyn Updater'
