842da06a1ee1e20de13497d81c5218a167cc0c5a
commitay <commitay@users.noreply.github.com>
2017-12-14 11:39:45 +1000

    Update beyond-compare to 4.2.3.22587

--- Casks/beyond-compare.rb
+++ Casks/beyond-compare.rb
@@ -2,8 +2,8 @@ cask 'beyond-compare' do
   version '4.2.3.22587'
   sha256 'b33fc8925757801bc35a7ae3d11627f26b4ebde9d82a61a7c0f17e27385a359f'
 
-  url "http://www.scootersoftware.com/BCompareOSX-#{version}.zip"
-  appcast "http://www.scootersoftware.com/checkupdates.php?product=bc#{version.major}&platform=osx",
+  url "https://www.scootersoftware.com/BCompareOSX-#{version}.zip"
+  appcast "https://www.scootersoftware.com/checkupdates.php?product=bc#{version.major}&platform=osx",
           checkpoint: '49e6135f03914bc71bb68ae2c05326441e302eba963ed5a078365a41b9cb1942'
   name 'Beyond Compare'
   homepage 'https://www.scootersoftware.com/'
