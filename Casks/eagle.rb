e35160e36eda668ede5a024b99b7197c2a65eb3b
commitay <commitay@users.noreply.github.com>
2017-12-15 13:18:49 +1000

    Update eagle to 8.4.3 (#41965)

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -2,7 +2,7 @@ cask 'eagle' do
   version '8.4.3'
   sha256 '9b7375112f961419168a61b0ac34b49492765e3bdeee24118340e5ac9fa720a1'
 
-  url "http://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
+  url "https://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   name 'Autodesk EAGLE'
   homepage 'https://www.autodesk.com/products/eagle/overview'
 
