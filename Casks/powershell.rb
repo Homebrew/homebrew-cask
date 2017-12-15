338318646841570bc2b55db8994cff1a7f64a5f0
commitay <commitay@users.noreply.github.com>
2017-12-15 12:30:44 +1000

    Update powershell to 6.0.0-rc.2

--- Casks/powershell.rb
+++ Casks/powershell.rb
@@ -1,10 +1,10 @@
 cask 'powershell' do
-  version '6.0.0-rc'
-  sha256 '187dc2772e06319a8e71c71197a8a8287cb12515c4a9e6a9546dec717720609c'
+  version '6.0.0-rc.2'
+  sha256 'dd6c73c55cf34190cba7e9f955d70fa383c616b9a8e3db99a1e3167e5c646558'
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx.10.12-x64.pkg"
   appcast 'https://github.com/PowerShell/PowerShell/releases.atom',
-          checkpoint: 'd9937dd818ba87d17bd4410898582a5e4ab8d2937aa3c4715acc6dfa887e6960'
+          checkpoint: '1045202e4de4f3e5a6f158be6e563ffaf1f8f10ac1952b812d49ade823f67a10'
   name 'PowerShell'
   homepage 'https://github.com/PowerShell/PowerShell'
 
