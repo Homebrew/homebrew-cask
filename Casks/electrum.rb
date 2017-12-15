6af8c829bc6fa0225a0464c2540af8083eab85f4
Franklin Yu <franklinyu@hotmail.com>
2017-12-15 00:21:09 -0500

    Update electrum to 3.0.3 (#41971)

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,10 +1,10 @@
 cask 'electrum' do
-  version '3.0.2'
-  sha256 'e40faa222a91c2813d9ccb190025ad4bd3a565c6aa9fe149c34962294544ebfb'
+  version '3.0.3'
+  sha256 'ed1165c1efe0426a163e98cf02f50842ae9a9677dd9a289b5f7a2e46f619d942'
 
   url "https://download.electrum.org/#{version}/electrum-#{version}.dmg"
   appcast 'https://github.com/spesmilo/electrum/releases.atom',
-          checkpoint: '494dae9cd3da494d5870df076a1605c85145452d900af0ff5f333a4aaf071576'
+          checkpoint: 'd6c0770e5a8bed012dc881b0271cd82b825b40d54070406c0627d7d041bad77d'
   name 'Electrum'
   homepage 'https://electrum.org/'
   gpg "#{url}.asc", key_id: '6694d8de7be8ee5631bed9502bd5824b7f9470e6'
