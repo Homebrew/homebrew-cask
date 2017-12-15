e5f9eef8ae5b71ef2780ef6d5092ad6366102dd7
soycode <soycode@users.noreply.github.com>
2017-12-15 08:45:52 -0800

    Update sensiblesidebuttons to 1.0.5

--- Casks/sensiblesidebuttons.rb
+++ Casks/sensiblesidebuttons.rb
@@ -1,6 +1,6 @@
 cask 'sensiblesidebuttons' do
-  version '1.0'
-  sha256 '1e7456dde54fe443db5f75edd4a9e7de348fee27d9b0d78e2d233ca287c390c1'
+  version '1.0.5'
+  sha256 '5a0f48dff542f9f8b2b20e09ffff31414139bd124a2cdb1c4ad23c7c77db920a'
 
   url "http://sensible-side-buttons.archagon.net/SensibleSideButtons-#{version}.dmg"
   name 'Sensible Side Buttons'
