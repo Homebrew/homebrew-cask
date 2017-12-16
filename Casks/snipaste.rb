e1d8b73964d9c425abf10c279ea4ba6b65aaba67
ZEO <nieyujiang@gmail.com>
2017-12-16 12:13:01 +0800

    update

--- Casks/snipaste.rb
+++ Casks/snipaste.rb
@@ -1,6 +1,6 @@
 cask 'snipaste' do
-  version '1.16-beta'
-  sha256 'f0f0618d9be190d7570a4633553cbc063177a7eb279e0c2a142afd7035e776e1'
+  version '1.16-beta2'
+  sha256 'ff254bc86cbb593311d63cda913084ef49205e9f6f9baa8d5d05be8e0a322792'
 
   # bitbucket.org/liule/snipaste was verified as official when first introduced to the cask
   url "https://bitbucket.org/liule/snipaste/downloads/Snipaste-Mac-#{version}.tar.gz"
