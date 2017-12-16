72088195f95897cd2ff9c08694eb090f2e6bd21c
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-12-16 19:50:13 +0000

    Update apache-couchdb to 2.1.1

--- Casks/apache-couchdb.rb
+++ Casks/apache-couchdb.rb
@@ -1,11 +1,11 @@
 cask 'apache-couchdb' do
-  version '2.1.0'
-  sha256 '54682d6f2ca9771ec967f301d414855634941200b39fc143746f9dba59b01479'
+  version '2.1.1'
+  sha256 '31f780432af0d9cdbe942beb56e181de9f0e1fb69535d6e5af957fd4ccb9e147'
 
   # bintray.com/apache/couchdb was verified as official when first introduced to the cask
   url "https://dl.bintray.com/apache/couchdb/mac/#{version}/Apache-CouchDB-#{version}.zip"
   appcast 'https://github.com/apache/couchdb/releases.atom',
-          checkpoint: '37db2b080b18e2fcc03bb1df8596e05cf32f5c3551025aa9b71d752b3faa21a1'
+          checkpoint: '9b6339769352a4b5e9396691b84174c0b8f0e92b361fa8deca33aaa558f638d9'
   name 'Apache CouchDB'
   homepage 'https://couchdb.apache.org/'
 
