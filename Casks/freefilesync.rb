45717c2f55840592322da592e4a5a38ca9caf254
JiaYue <junji.jiayue+github@gmail.com>
2017-12-16 09:56:29 +0900

    Update freefilesync to 9.6 (#42006)
    * Update freefilesync to 9.6
    
    * downgrades to http

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,6 +1,6 @@
 cask 'freefilesync' do
-  version '9.5'
-  sha256 'e53c7889b424fc8bee0ea4a23788bde998b4886a63a705614fb12b1cf5d9b2d3'
+  version '9.6'
+  sha256 '899bc5db4760494f159a43dd1c6545f99a52232cd5ad802da69b96482a4c6653'
 
   url "http://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake
