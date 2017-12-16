4c64be6e1e4ccc3510f4a13b094f5e2c7e5c51da
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-12-16 18:45:29 +0000

    Update 4k-video-downloader to 4.4.0.2235

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,10 +1,10 @@
 cask '4k-video-downloader' do
-  version '4.4.0'
+  version '4.4.0.2235'
   sha256 '0141d884858c1b0c57b2589150d10c50cacef7753988c9eb8bc2d12888738250'
 
-  url "https://dl.4kdownload.com/app/4kvideodownloader_#{version}.dmg"
+  url "https://dl.4kdownload.com/app/4kvideodownloader_#{version.major_minor_patch}.dmg"
   appcast 'https://www.4kdownload.com/download',
-          checkpoint: '74c74ea1a5c9ed50ae5c6fa5556bb556dd70f5af5188400a5cfd3e53ef0ab12c'
+          checkpoint: 'bcc9ec5479e3d7db1f10175083eb4c73dd6590b8bfe3cab26f6d78efbbc4c583'
   name '4K Video Downloader'
   homepage 'https://www.4kdownload.com/products/product-videodownloader'
 
