980bcfd14c7176c2e933926e6c8ce3d39e40c965
Ben Phipathananunth <bensiraphob@hotmail.co.uk>
2017-12-16 14:02:10 +0700

    Add appcast and styling changes

--- Casks/next-browser.rb
+++ Casks/next-browser.rb
@@ -3,12 +3,12 @@ cask 'next-browser' do
   sha256 '64b8fa32387ce1d467238bb20405b9620f08dd00df121c49f948ac64aca6d6aa'
 
   url "https://github.com/nEXT-Browser/nEXT/releases/download/#{version}/nEXT.dmg"
+  appcast 'https://github.com/nEXT-Browser/nEXT/releases.atom',
+          checkpoint: '64b8fa32387ce1d467238bb20405b9620f08dd00df121c49f948ac64aca6d6aa'
   name 'nEXT-Browser'
   homepage 'https://github.com/nEXT-Browser/nEXT'
 
   app 'nEXT.app'
 
-  zap trash: [
-               '~/Library/Caches/next.browser',
-             ]
+  zap trash: '~/Library/Caches/next.browser'
 end
