0a78ae3f684ee307938eb4dd02d04c19f38fb8b0
commitay <commitay@users.noreply.github.com>
2017-12-16 10:43:03 +1000

    downgrades to http

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -2,7 +2,7 @@ cask 'freefilesync' do
   version '9.6'
   sha256 '899bc5db4760494f159a43dd1c6545f99a52232cd5ad802da69b96482a4c6653'
 
-  url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
+  url "http://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake
   name 'FreeFileSync'
   homepage 'https://www.freefilesync.org/'
