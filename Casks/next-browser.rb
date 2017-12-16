064f32c8509be4c62d14c6adebf26a84a45a1628
Ben Phipathananunth <bensiraphob@hotmail.co.uk>
2017-12-16 14:06:42 +0700

    Update checkpoint checksum

--- Casks/next-browser.rb
+++ Casks/next-browser.rb
@@ -4,7 +4,7 @@ cask 'next-browser' do
 
   url "https://github.com/nEXT-Browser/nEXT/releases/download/#{version}/nEXT.dmg"
   appcast 'https://github.com/nEXT-Browser/nEXT/releases.atom',
-          checkpoint: '64b8fa32387ce1d467238bb20405b9620f08dd00df121c49f948ac64aca6d6aa'
+          checkpoint: '5f62552a7609426aa1ed9c33b0cdf9dc4cdad7cfcff9ed0a6e6183dfba9a234a'
   name 'nEXT-Browser'
   homepage 'https://github.com/nEXT-Browser/nEXT'
 
