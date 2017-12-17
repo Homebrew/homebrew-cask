f5f2f8c4a581cea6549e2ac632866ff6a8b14672
numbermaniac <numbermaniac@users.noreply.github.com>
2017-12-17 20:54:15 +1100

    Add zaps to skype.rb (#42038)
    * Add zaps to skype.rb
    
    * Update skype.rb
    
    * Update skype.rb

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -12,11 +12,25 @@ cask 'skype' do
   app 'Skype.app'
 
   zap trash: [
+               '~/Library/Address Book Plug-Ins/SkypeABCaller.bundle',
+               '~/Library/Address Book Plug-Ins/SkypeABDialer.bundle',
+               '~/Library/Address Book Plug-Ins/SkypeABSMS.bundle',
+               '~/Library/Address Book Plug-Ins/SkypeABChatter.bundle',
+               '~/Library/Application Scripts/com.skype.skype.shareagent',
                '~/Library/Application Support/Skype',
+               '~/Library/Application Support/T/SkypeRT',
+               '~/Library/Application Support/CrashReporter/Skype_*.plist',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.skype.skype.sfl*',
                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.skype.skype',
                '~/Library/Caches/com.skype.skype',
+               '~/Library/Caches/com.skype.skype.ShipIt',
+               '~/Library/Containers/com.skype.skype.shareagent',
+               '~/Library/Cookies/com.skype.skype.binarycookies',
+               '~/Library/Group Containers/*.com.skype.skype',
                '~/Library/Preferences/com.skype.skype.plist',
                '~/Library/Preferences/com.skype.skypewifi.plist',
+               '~/Library/Preferences/ByHost/com.skype.skype.*.plist',
                '~/Library/Saved Application State/com.skype.skype.savedState',
+               '~/Library/WebKit/com.skype.skype',
              ]
 end
