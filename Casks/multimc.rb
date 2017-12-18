0a12ba7802b202fc3305d2d2b37daaca427ee42e
Chris Pinard <slarti@gweep.net>
2017-12-18 00:32:25 -0500

    Update multimc to 0.5.1-908

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -1,10 +1,10 @@
 cask 'multimc' do
-  version :latest
-  sha256 :no_check
+  version '0.5.1-908'
+  sha256 'cf963b590b1be1be57fde76cc1b5a2bd5af99c041b34f82549ed3efe4868eb4e'
 
   url 'https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz'
   name 'Multi MC'
   homepage 'https://multimc.org/'
 
-  app 'MultiMC/MultiMC.app'
+  app 'mmc-stable-osx64-0.5.1-908/MultiMC.app'
 end
