d291edf327d2b7a77b5c58ef733c023d17b28139
Hanlin He <hanling.he@gmail.com>
2017-12-19 07:15:40 +0800

    Add vu.app v1.1

---
+++ Casks/vu.rb
@@ -0,0 +1,10 @@
+cask 'vu' do
+  version '1.1'
+  sha256 '7c6424e0c4c3e0eb92edb1904617231afbb7f4de3aa120f803cc5403ae11406c'
+
+  url 'https://dl.devmate.com/com.boriskarulin.vu/vu.dmg'
+  name 'vu for Mac'
+  homepage 'https://datastills.com/vu/'
+
+  app ''
+end
