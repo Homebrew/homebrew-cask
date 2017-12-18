8f7ef27657e1367e9937daf52630f061715acb25
衣带渐宽人渐悔 <var.darling@gmail.com>
2017-12-18 12:23:22 +0800

    Update ieasemusic to 1.1.0 (#42055)

--- Casks/ieasemusic.rb
+++ Casks/ieasemusic.rb
@@ -1,10 +1,10 @@
 cask 'ieasemusic' do
-  version '1.0.7'
-  sha256 'e52c316c394d792af74016c5af77d2aa8de0106cec3d1736995750f98b7551c5'
+  version '1.1.0'
+  sha256 'bb73b4302de2641625eb5e290228903eb8012b52e79a5920d72851303974874b'
 
   url "https://github.com/trazyn/ieaseMusic/releases/download/v#{version}/ieaseMusic-#{version}-mac.dmg"
   appcast 'https://github.com/trazyn/ieaseMusic/releases.atom',
-          checkpoint: '37774ea91ba4603dc8943c7b79039da3cfc62b11d9e1c3bb2e911df7547e5974'
+          checkpoint: '4a641bc23767a3a56726f4c6d1477f9a0c13499da5008dfcacbcc9da2926ba5d'
   name 'ieaseMusic'
   homepage 'https://github.com/trazyn/ieaseMusic'
 
