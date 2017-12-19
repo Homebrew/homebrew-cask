8d957419942b36205f7c0d144f6e7d4013b47e6a
commitay <commitay@users.noreply.github.com>
2017-12-19 10:05:01 +1000

    vu

--- Casks/vu.rb
+++ Casks/vu.rb
@@ -2,9 +2,11 @@ cask 'vu' do
   version '1.1'
   sha256 '7c6424e0c4c3e0eb92edb1904617231afbb7f4de3aa120f803cc5403ae11406c'
 
-  # dl.devmate.com/com.boriskarulin.vu/vu.dmg was verified as official when first introduced to the cask
+  # dl.devmate.com/com.boriskarulin.vu was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.boriskarulin.vu/vu.dmg'
-  name 'vu for Mac'
+  appcast 'http://updates.devmate.com/com.boriskarulin.vu.xml',
+          checkpoint: '5caad13605150adf41ee58b113ba3669f4376b84e9a57feaed836b65f2d69f5d'
+  name 'vu'
   homepage 'https://datastills.com/vu/'
 
   app 'vu.app'
