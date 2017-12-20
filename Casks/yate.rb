c791ddc1e93cfc2e376b604456eadbf8aad31649
Grettir Asmundarson <grettir@users.noreply.github.com>
2017-12-20 15:12:38 -0700

    Update yate to 3.17.4

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,10 +1,10 @@
 cask 'yate' do
-  version '3.17.3'
-  sha256 '390fe9467c6f88169d59116ef63a04082895c8510adfbf9322f42c8cbec26199'
+  version '3.17.4'
+  sha256 '682c2d6fd1f1cb8483fc9e72dbbfaadfaca6ebe33ddf2bfecddaccf561ade1c1'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml',
-          checkpoint: '3b3aaed6114daa051b61aa126782d5af2cdfc25827d39f136dad286384bd5311'
+          checkpoint: '32375f7738f4ecde8c1501b645846083112a478db3aa5d6b8037819011b3473e'
   name 'Yate'
   homepage 'https://2manyrobots.com/yate/'
 
