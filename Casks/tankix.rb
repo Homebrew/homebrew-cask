fefe179df92ee85dce82e9efd390eebc69693bcb
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2017-12-20 12:56:32 +0000

    Update tankix to 10116

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version '9899'
-  sha256 '79358235490de1aa0a6d23dd7b9fd0d913af1fe7e882128bf10ed94456da1cb6'
+  version '10116'
+  sha256 'f45389c6ef9112cfcef4e100c4f38bdd64ff2afc0fd99ac642ce49dab722637b'
 
   url "https://static.tankix.com/app/StandaloneOSXIntel64/master-#{version}/TankiX.dmg"
   name 'Tanki X'
