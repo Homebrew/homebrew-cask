5e1d6977f27bda972e9a4cdef7d406b8301095a8
commitay <commitay@users.noreply.github.com>
2017-12-20 14:20:10 +1000

    appcast

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -1,8 +1,10 @@
 cask 'craftmanager' do
-  version :latest
-  sha256 :no_check
+  version '1.0.44'
+  sha256 'cd76f42fe63caaa0876736b0e9e3c27a5f4cb746808cc478e4068173638d5605'
 
   url 'https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip'
+  appcast 'https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml',
+          checkpoint: '2c9bb71d38c675d8990495aa41bf011af3fe9bf294d2451a41015003d16aee96'
   name 'CraftManager'
   homepage 'https://www.invisionapp.com/craft'
 
