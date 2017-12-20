620f123e90839cee4ce534dfeb5916202498ec3b
alswl <alswlx@gmail.com>
2017-12-20 16:12:28 +0800

    Update: wxwork to 2.4.0.1009

--- Casks/wxwork.rb
+++ Casks/wxwork.rb
@@ -1,10 +1,10 @@
 cask 'wxwork' do
-  version '2.1.2.3031'
-  sha256 '2dbf00389de7f8a3233bc351b68cecdb22cc16e1d3b7ed1e09cb531238c4e266'
+  version '2.4.0.1009'
+  sha256 'ae521d9cbfe7db89a1817d88349fbbc8468a1449424e51d199c763a7c9678c9b'
 
   url "https://dldir1.qq.com/foxmail/work_weixin/WXWork_#{version}.dmg"
   name 'Wechat Enterprise Version'
-  name '企业微信'
+  name 'WeChat Work'
   homepage 'https://work.weixin.qq.com/'
 
   app '企业微信.app'
