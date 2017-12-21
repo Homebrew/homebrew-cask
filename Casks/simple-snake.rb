0faeb0eff465bd8f148857421248847ea8a2b1f3
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-21 16:41:20 -0500

    simple-snake v2.0.1

--- Casks/simple-snake.rb
+++ Casks/simple-snake.rb
@@ -1,10 +1,11 @@
 cask 'simple-snake' do
-  version "2.0.0"
-  sha256 "36b43494f7def3694c52107998ea2f746d1a7258e0af811ff8da2d5a2a9b04bb"
+  version '2.0.0'
+  sha256 '36b43494f7def3694c52107998ea2f746d1a7258e0af811ff8da2d5a2a9b04bb'
 
-  # https://github.com/hasahmed/ was verified as official when first introduced to the cask
-  url "https://github.com/hasahmed/simple-snake/releases/download/v2.0.1/simple-snake.tar.gz"
+  # github.com/hasahmed/simple-snake/releases was verified as official when first introduced to the cask
+  url 'https://github.com/hasahmed/simple-snake/releases/download/v2.0.1/simple-snake.tar.gz'
   name 'Simple Snake'
-  homepage "http://pages.iu.edu/~hasahmed/snake/"
+  homepage 'http://pages.iu.edu/~hasahmed/snake/'
+
   app 'Simple Snake.app'
 end
