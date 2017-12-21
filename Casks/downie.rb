68903ae22bcacd5a8cd883f84ae883890019378f
Nick Wind-McJetters <nick@elearningexperts.net>
2017-12-21 16:26:08 -0500

    Update downie to 3.1.2,1735 (#42190)
    * Update downie to 3.1,1735
    
    * Update downie.rb

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,10 +1,10 @@
 cask 'downie' do
-  version '3.1,1730'
-  sha256 'b3fd6c2d75b0fd6ca28481e918c90205fdb16502b5a208d188704e901c06691f'
+  version '3.1.2,1735'
+  sha256 '0213c05c9878d72645fed228c955f739691ec0d1a52f1dec16ea94ed82edae34'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.major}_#{version.after_comma}.zip"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml",
-          checkpoint: 'ce0f33e7dba3c676d4c4517e897dbbdd31fe4f3b35a464cd396009142ec63a0a'
+          checkpoint: 'c6db67fec97f3251d9aa552e6974a370d4116659c49000973fd314bc5d8e0693'
   name 'Downie'
   homepage 'https://software.charliemonroe.net/downie.php'
 
