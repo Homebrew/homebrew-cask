3b3baabe679d02c468e74a878a5647191d6dfa88
commitay <commitay@users.noreply.github.com>
2017-12-22 05:56:54 +1000

    Update downie.rb

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,5 +1,5 @@
 cask 'downie' do
-  version '3.1,1735'
+  version '3.1.2,1735'
   sha256 '0213c05c9878d72645fed228c955f739691ec0d1a52f1dec16ea94ed82edae34'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.major}_#{version.after_comma}.zip"
