bd82b6a486091608feb036a14efb2cf55215fd35
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2017-12-21 14:44:01 +0000

    Update tankix to 10223 (#42179)

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version '10116'
-  sha256 'f45389c6ef9112cfcef4e100c4f38bdd64ff2afc0fd99ac642ce49dab722637b'
+  version '10223'
+  sha256 '7f96002d54bacb436ab8ef2efb2f19d1ec0f9b3212bfdfdc181b9aadcb4c9be0'
 
   url "https://static.tankix.com/app/StandaloneOSXIntel64/master-#{version}/TankiX.dmg"
   name 'Tanki X'
