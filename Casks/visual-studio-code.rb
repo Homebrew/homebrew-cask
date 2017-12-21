9275acda15dc0edb5eddf330bcb1f903e2724dfa
Leon Lin <linbo0518@gmail.com>
2017-12-21 10:57:16 +0800

    Update visual-studio-code to 1.19.1,0759f77bb8d86658bc935a10a64f6182c5a1eeba (#42157)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,11 +1,11 @@
 cask 'visual-studio-code' do
-  version '1.19.0,816be6780ca8bd0ab80314e11478c48c70d09383'
-  sha256 '8d544e30e637e24a67709fac2de72b40a86ba9faf62055949bc6a42aa65a67e0'
+  version '1.19.1,0759f77bb8d86658bc935a10a64f6182c5a1eeba'
+  sha256 '88edc7b8d39554f8de5e4d322e0e7313729dc1cf86bf7a5f98b66ede5e6b6cfd'
 
   # az764295.vo.msecnd.net/stable was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
   appcast 'https://vscode-update.azurewebsites.net/api/update/darwin/stable/VERSION',
-          checkpoint: 'dc174b2eebe5e34a7a3d45c4c61e7082e49a311eef9e26d7d0538a96061c2efa'
+          checkpoint: '4c5ef58e58572cbe80992710a6d660f220d626c508200c3248f4c25000e0c06f'
   name 'Microsoft Visual Studio Code'
   name 'VS Code'
   homepage 'https://code.visualstudio.com/'
