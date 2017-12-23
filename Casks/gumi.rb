5a4f3de4bcbd59426dd3bf44e9d07f799722765d
Flavio Maria De Stefano <destefano.flavio@gmail.com>
2017-12-23 01:08:25 +0100

    Add Gumi (#42212)
    * Add Gumi
    
    * Update gumi.rb
    
    * Update gumi.rb

---
+++ Casks/gumi.rb
@@ -0,0 +1,10 @@
+cask 'gumi' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://gumiapp.com/download/gumi-app.zip'
+  name 'gUMI'
+  homepage 'https://gumiapp.com/'
+
+  app 'gumi.app'
+end
