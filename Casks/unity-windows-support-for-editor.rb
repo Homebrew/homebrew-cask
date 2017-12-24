2806f65a687f1e9f3a51e54ce671d253fab36108
commitay <commitay@users.noreply.github.com>
2017-12-25 07:52:28 +1000

    Update unity-windows-support-for-editor to 2017.3.0f3,a9f86dcd79df (#42265)

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-windows-support-for-editor' do
-  version '2017.2.1f1,94bf3f9e6b5e'
-  sha256 'ecc7f3a9c817789d5280eb4f85f6ce00033dc5e827d0c5799ff33c36001e0223'
+  version '2017.3.0f3,a9f86dcd79df'
+  sha256 'b4b6c254f3bcb1c5f62dac44c187e6e4615c3758a2bd570727598e460d325c98'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity Windows Build Support'
