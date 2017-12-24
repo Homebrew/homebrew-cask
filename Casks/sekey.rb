4fc98dd0fde180de5a7ce6e2065808281ef4bb7a
commitay <commitay@users.noreply.github.com>
2017-12-24 12:32:31 +1000

    sekey: switch to pkg

--- Casks/sekey.rb
+++ Casks/sekey.rb
@@ -1,65 +1,38 @@
 cask 'sekey' do
   version '0.1'
-  sha256 '86cd69753de6a186f5991c7148014e8349ecc59d6bd15da19823a24e62faa4db'
+  sha256 '8d473f7214ba76b70ca30905e2ebd07d7c421f1cff320285bd8ec8d2a7a5b083'
 
-  url "https://github.com/ntrippar/sekey/releases/download/#{version}/SeKey-#{version}.zip"
+  url "https://github.com/ntrippar/sekey/releases/download/#{version}/SeKey-#{version}.pkg"
   appcast 'https://github.com/ntrippar/sekey/releases.atom',
           checkpoint: 'ed0d4a9db584f983da245bf83ec70ecdf1fe0e7109d10660b2dd43225e2f53f8'
   name 'SeKey'
   homepage 'https://github.com/ntrippar/sekey'
 
-  app 'SeKey.app'
+  pkg "SeKey-#{version}.pkg"
   binary "#{appdir}/SeKey.app/Contents/MacOS/sekey"
 
+  launchd_plist = "#{ENV['HOME']}/Library/LaunchAgents/com.ntrippar.sekey.plist"
+
   postflight do
-    launchd_plist = "#{ENV['HOME']}/Library/LaunchAgents/com.ntrippar.sekey.plist"
+    system_command '/bin/launchctl',
+                   args: ['unload', '-F', launchd_plist],
+                   sudo: true
+
+    set_ownership launchd_plist
 
     system_command '/bin/launchctl',
-                   args:         ['unload', '-F', launchd_plist],
-                   print_stdout: false,
-                   print_stderr: false
-
-    IO.write launchd_plist, <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-          <key>EnvironmentVariables</key>
-          <dict>
-            <key>PATH</key>
-            <string>/usr/bin:/bin:/usr/sbin:/sbin</string>
-          </dict>
-          <key>Label</key>
-          <string>com.ntrippar.sekey</string>
-          <key>ProgramArguments</key>
-          <array>
-              <string>#{appdir}/SeKey.app/Contents/MacOS/sekey</string>
-              <string>--daemon</string>
-          </array>
-          <key>StandardErrorPath</key>
-          <string>#{HOMEBREW_PREFIX}/var/log/#{token}/#{token}.err.log</string>
-          <key>StandardOutPath</key>
-          <string>#{HOMEBREW_PREFIX}/var/log/#{token}/#{token}.out.log</string>
-          <key>KeepAlive</key>
-          <true/>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>ThrottleInterval</key>
-          <integer>30</integer>
-      </dict>
-      </plist>
-    EOS
-
-    system_command '/bin/launchctl',
-                   args: ['load', launchd_plist]
+                   args: ['load', '-F', launchd_plist]
   end
 
-  uninstall launchctl: 'com.ntrippar.sekey'
+  # early_script / delete is a workaround for uninstall launchctl permissions error
+  uninstall early_script: {
+                            executable: '/bin/launchctl',
+                            args:       ['unload', '-F', launchd_plist],
+                          },
+            delete:       launchd_plist,
+            pkgutil:      'com.ntrippar.sekey'
 
-  zap trash: [
-               '~/.sekey',
-               "#{HOMEBREW_PREFIX}/var/log/#{token}",
-             ]
+  zap trash: '~/.sekey'
 
   caveats <<~EOS
     Append the following line to your ~/.bash_profile or ~/.zshrc:
