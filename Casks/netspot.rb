ad9c9f1d91ec6f0212aca54a634b6cf1e21e37ef
commitay <commitay@users.noreply.github.com>
2017-12-25 06:41:19 +1000

    Update netspot.rb

--- Casks/netspot.rb
+++ Casks/netspot.rb
@@ -1,5 +1,5 @@
 cask 'netspot' do
-  version '2.8.838'
+  version '2.8.830'
   sha256 '1888b8c359eec6ea9e5f851b742f7d4a3a98b81a78bbe0a5601b24647065fabf'
 
   url 'https://www.netspotapp.com/download/NetSpot.dmg'
