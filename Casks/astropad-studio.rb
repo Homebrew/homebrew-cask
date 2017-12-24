34e4b6d101b8f65da01dacc5ded21eb15ac0fbfe
commitay <commitay@users.noreply.github.com>
2017-12-25 07:59:30 +1000

    Update astropad-studio.rb

--- Casks/astropad-studio.rb
+++ Casks/astropad-studio.rb
@@ -2,11 +2,11 @@ cask 'astropad-studio' do
   version '1.8.0'
   sha256 '98e78f9fd35faf6bd5752208b64c2a6901bc3205b3053cf278f6842dba0a0c64'
 
-  url "http://astropad.com/downloads/AstropadStudio-#{version}.zip"
-  appcast 'http://astropad.com/downloads/studio-sparkle.xml',
+  url "https://astropad.com/downloads/AstropadStudio-#{version}.zip"
+  appcast 'https://astropad.com/downloads/studio-sparkle.xml',
           checkpoint: '090dac3584529d17a93fd97500e876efd50200092509eba327d2dceebd0424df'
   name 'Astropad Studio'
-  homepage 'http://astropad.com/'
+  homepage 'https://astropad.com/'
 
   depends_on macos: '>= :mavericks'
 
