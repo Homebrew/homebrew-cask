5d1b406c5791214306a2e0999e85178a5886fd8a
Grant Kinney <creativecoder@users.noreply.github.com>
2017-12-24 14:42:01 -0600

    Upgrade KeeWeb to v1.6.3 (#42256)

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,11 +1,11 @@
 cask 'keeweb' do
-  version '1.6.1'
-  sha256 '9c201acc87a696a4d1efdf970ee3177f5297ce7d954d5944947d6e762c8924a9'
+  version '1.6.3'
+  sha256 'd1b27e258eaba37f2956c5d145209dd9a1f4c568853bfe7ea29c26073ceff685'
 
   # github.com/keeweb/keeweb was verified as official when first introduced to the cask
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.dmg"
   appcast 'https://github.com/keeweb/keeweb/releases.atom',
-          checkpoint: 'b19eebea7070a68a43f81f4b549f11b28985908c9e43631b3b7a2c04561cf0a4'
+          checkpoint: '4913b4159a4eac3d588cdb18c5665adb9a2a2aed68b022e90e03a3df09199f7f'
   name 'KeeWeb'
   homepage 'https://keeweb.info/'
 
