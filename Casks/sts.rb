9b439529075cb4e42ae405c3e556dc82bbe8a928
VonUniGE <VonUniGE@github.com>
2017-12-25 05:29:21 +0100

    Update sts to 3.9.2

--- Casks/sts.rb
+++ Casks/sts.rb
@@ -1,6 +1,6 @@
 cask 'sts' do
-  version '3.9.1.RELEASE,4.7.1a'
-  sha256 '17bdeecd0b485b02475a2b32f06d61b23450fca14d45acc18a557fc707fb657b'
+  version '3.9.2.RELEASE,4.7.2'
+  sha256 '8281a549a9ea334d475525dd79a92f1ea2ffbb3aacaf4dcb6a8e8b740fe128c0'
 
   # download.springsource.com/release/STS was verified as official when first introduced to the cask
   url "http://download.springsource.com/release/STS/#{version.before_comma}/dist/e#{version.after_comma.major_minor}/spring-tool-suite-#{version.before_comma}-e#{version.after_comma}-macosx-cocoa-x86_64.dmg"
