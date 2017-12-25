db6ea03d16790ad476265671796fca26e35358cd
Victor Popkov <victor@popkov.me>
2017-12-25 21:16:10 +0200

    Update blockstack to 0.21.5

--- Casks/blockstack.rb
+++ Casks/blockstack.rb
@@ -1,11 +1,11 @@
 cask 'blockstack' do
-  version '0.21.3'
-  sha256 '8d380d877c36975873a041d6740ac5760162326be7624798ec41eda44c30b082'
+  version '0.21.5'
+  sha256 '43dd48ed2dc9e198182b4ecc861b901c8575392a37f1f125aa8418cba391bf20'
 
   # github.com/blockstack/blockstack-browser was verified as official when first introduced to the cask
   url "https://github.com/blockstack/blockstack-browser/releases/download/v#{version}/Blockstack-for-macOS-v#{version}.dmg"
   appcast 'https://github.com/blockstack/blockstack-browser/releases.atom',
-          checkpoint: '54c83ef1f8db9176b612f52d38c30e3b45ddb8296e79942e1019bd8ad8557d0b'
+          checkpoint: 'df341c1e52a89a14d899dd7d39cddc5e5f92b5a02cdeb60e892a1b0986b64ef4'
   name 'Blockstack'
   homepage 'https://blockstack.org/'
 
