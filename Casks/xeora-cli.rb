0dfad44b80ae94a76b2930e78772e01e4a9686ed
Tuna Celik <tuna@jakpark.com>
2017-12-26 00:30:03 +0100

    xeora scripting creation is changed with a better considiration

--- Casks/xeora-cli.rb
+++ Casks/xeora-cli.rb
@@ -8,11 +8,13 @@ cask 'xeora-cli' do
 
   depends_on cask: 'dotnet-sdk'
 
-  binary 'xeora'
+  xeorascript = "#{staged_path}/xeora-cli"
+  binary xeorascript, target: 'xeora'
 
   preflight do
-    FileUtils.mv "#{staged_path}/xeora", "#{staged_path}/xeora-cli"
-    File.open("#{staged_path}/xeora", 'w') { |f| f.write("#!/bin/sh\nXEORAPATH=#{staged_path} #{staged_path}/xeora-cli $@\n") }
-    set_permissions "#{staged_path}/xeora", '0755'
+    IO.write xeorascript, <<~EOS
+      #!/bin/sh
+      XEORAPATH="#{staged_path}" "#{staged_path}/xeora" "$@"
+     EOS
   end
 end
