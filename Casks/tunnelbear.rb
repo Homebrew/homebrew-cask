0e464371ad8377d0517742aced48de9d1dbcd8f9
Milo Gilad <milogaccnts@gmail.com>
2017-12-26 11:51:44 -0500

    Update tunnelbear to 3.5.0

--- Casks/tunnelbear.rb
+++ Casks/tunnelbear.rb
@@ -1,11 +1,11 @@
 cask 'tunnelbear' do
-  version '3.0.16'
-  sha256 'c9abb616faf4a7d8b752bd34a57037ea8b5075285957d16d6acfe4b22380d2bd'
+  version '3.5.0'
+  sha256 '95eddbd5da1c894d93f6700cdf9f6a48ccaa9bec06daea88bfaef6d9bff8fb36'
 
   # s3.amazonaws.com/tunnelbear was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/tunnelbear/downloads/mac/TunnelBear.v#{version}.zip"
+  url "https://s3.amazonaws.com/tunnelbear/downloads/mac/TunnelBear-#{version}.zip"
   appcast 'https://s3.amazonaws.com/tunnelbear/downloads/mac/appcast.xml',
-          checkpoint: '1b082eeb2a5a69687fcb0bb269a28ffdee80044baba181261d708b38a9a6b6c2'
+          checkpoint: 'd3858b49220a01bb25d69c6e7357d572901ed1a53798c9d07f6a5f2ce604a99c'
   name 'TunnelBear'
   homepage 'https://www.tunnelbear.com/'
 
