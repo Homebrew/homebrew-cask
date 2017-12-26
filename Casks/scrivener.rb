0153b7a67aec9754829c50f52c9787a70e2f7c22
commitay <commitay@users.noreply.github.com>
2017-12-27 07:38:44 +1000

    scrivener 3.0.1,966

--- Casks/scrivener.rb
+++ Casks/scrivener.rb
@@ -1,9 +1,9 @@
 cask 'scrivener' do
-  version '3.0.1'
-  sha256 '8cdeb3a27eb4f0ca9fb12ff6e987ad699e23a9d8497f352d68e1dffc18347a85'
+  version '3.0.1,966'
+  sha256 '704ae6632026a6b18f4f84df383696ca7bda37ea79ce5f3a269cd04ef3c622d4'
 
   # scrivener.s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://scrivener.s3.amazonaws.com/Scrivener.dmg'
+  url "https://scrivener.s3.amazonaws.com/mac_updates/Scrivener_1012_#{version.after_comma}.zip"
   appcast "https://www.literatureandlatte.com/downloads/scrivener-#{version.major}.xml",
           checkpoint: '86ba4658751c0ebb95046ca0c836e81c6d21259da8c051f41c4986cd59b6302b'
   name 'Scrivener'
