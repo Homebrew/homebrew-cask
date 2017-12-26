d79f5201c82813309c5d77be97f7b58e8eccd55e
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2017-12-26 05:37:52 +0100

    Add myCloudDesktop.app 17.43.26
    myCloudDesktop is the cloud offering of Swisscom, one of the local
    telecom operaters in Switzerland.
    
    The application is similar to well known alternatives like dropbox or
    google drive and will synchronize a local directory with contents
    stored on the cloud service.

---
+++ Casks/myclouddesktop.rb
@@ -0,0 +1,19 @@
+cask 'myclouddesktop' do
+  version '17.43.26'
+  sha256 '3d30ee7389686fce876949f7214881cceeb044f6ca0212021f5c06a83b969bd3'
+
+  # download-syncclient-mac.prod.mdl.swisscom.ch was verified as official when first introduced to the cask
+  url 'https://download-syncclient-mac.prod.mdl.swisscom.ch/mac/myCloudDesktop-installer-17.43.26.dmg'
+  name 'Swisscom myCloud Desktop'
+  homepage 'https://desktop.mycloud.ch/'
+
+  installer script: {
+                      executable: "#{staged_path}/myCloudDesktop-installer.app/Contents/MacOS/myCloudDesktop-installer",
+                      sudo:       false,
+                    }
+
+  uninstall quit:       'ch.swisscom.mycloud.desktop.finder',
+            signal:     ['KILL', 'ch.swisscom.mycloud.desktop'],
+            delete:     '/Applications/myCloudDesktop.app',
+            login_item: 'myCloudDesktop'
+end
