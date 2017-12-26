6cbec59076a2463a5ebf335badef9bc248e3da4f
commitay <commitay@users.noreply.github.com>
2017-12-27 07:41:54 +1000

    Update truefire.rb

--- Casks/truefire.rb
+++ Casks/truefire.rb
@@ -1,10 +1,10 @@
 cask 'truefire' do
-  version '3.1.7'
-  sha256 'd2e527e5f048c56e0106a868e95c60b65371c5d9d90affb84543b58655298a55'
+  version '3'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url 'https://truefire.com/player/TrueFireInstaller.dmg'
   name 'TrueFire'
   homepage 'https://truefire.com/apps'
 
-  app "TrueFire #{version.major}.app"
+  app "TrueFire #{version}.app"
 end
