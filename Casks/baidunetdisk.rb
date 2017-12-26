d3d32cec52e53efa14ca8952a19f7ee181ee213a
Elf Sundae <elf.sundae@gmail.com>
2017-12-26 13:39:45 +0800

    Update baidunetdisk to 2.2.1 (#42297)

--- Casks/baidunetdisk.rb
+++ Casks/baidunetdisk.rb
@@ -1,6 +1,6 @@
 cask 'baidunetdisk' do
-  version '2.2.0'
-  sha256 'efcf567403a3ffbf685b27f0427ba007f806b40df1a8d1d0274bafc27e8ef481'
+  version '2.2.1'
+  sha256 '55c248ea2380b56c025be91bc301daae8287cfe64dcba8a2d77eb484dd9b627d'
 
   # baidupcs.com/issue/netdisk/MACguanjia was verified as official when first introduced to the cask
   url "https://issuecdn.baidupcs.com/issue/netdisk/MACguanjia/BaiduNetdisk_mac_#{version}.dmg"
