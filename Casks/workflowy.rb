5d2a54c6dbccedc160f64eec9b17f1059904261d
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2017-12-27 20:25:04 +0000

    Update workflowy to 1.0.12 (#42328)

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,11 +1,11 @@
 cask 'workflowy' do
-  version '1.0.9'
-  sha256 'b34d5fa939b9667871f6149cb0d8095746ba8a7299f965e24cc9e32aa5ac0af5'
+  version '1.0.12'
+  sha256 '1174ef8ca59abaedef5fe2247b9339c38a10ad1dac8102b983d10deb3ee687e1'
 
   # github.com/workflowy/desktop was verified as official when first introduced to the cask
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.dmg"
   appcast 'https://github.com/workflowy/desktop/releases.atom',
-          checkpoint: '98e7c23a06871570fb338fb8d0c907c48fe64f3d721cbc04517c4e23d65413b2'
+          checkpoint: '045ffb0d3ce9bff488943ecff1eba5595ee6bd75af018b19482ea366c9824739'
   name 'WorkFlowy'
   homepage 'https://workflowy.com/downloads/mac/'
 
