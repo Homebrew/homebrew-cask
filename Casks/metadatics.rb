570290198e838ce70903533d15bd54211a2b23fe
commitay <commitay@users.noreply.github.com>
2017-12-28 20:04:49 +1000

    Update metadatics to 1.5.7

--- Casks/metadatics.rb
+++ Casks/metadatics.rb
@@ -2,7 +2,7 @@ cask 'metadatics' do
   version '1.5.7'
   sha256 'c8baea464a742d35ca0793128d7d2606e59358b7f3e156ac7e7a27694db67927'
 
-  url 'http://www.markvapps.com/applications/metadatics/Metadatics.zip'
+  url 'https://www.markvapps.com/applications/metadatics/Metadatics.zip'
   appcast 'https://www.markvapps.com/applications/metadatics/metadatics_appcast.xml',
           checkpoint: '5e5f3ccd1d7718ab02dc32fd1f36ef1b0fe29d6d096b74429a575cb1d1489bee'
   name 'Metadatics'
