03149c6b4c850ef437daadee0b1cdf3c1da4c16e
Anver Housseini <contact@anver.ch>
2017-12-28 16:16:02 +0100

    Add Passenger.app latest

---
+++ Casks/passenger.rb
@@ -0,0 +1,10 @@
+cask 'passenger' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://macinmind.com/Passenger.dmg'
+  name 'Passenger'
+  homepage 'http://macinmind.com/?area=app&app=passenger'
+
+  suite 'Passenger'
+end
