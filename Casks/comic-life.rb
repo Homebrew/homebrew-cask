e39c1cecbf39a560802038f6e7254486fffd737c
Vítor Galvão <info@vitorgalvao.com>
2017-12-28 15:43:00 +0000

    Update comic-life.rb

--- Casks/comic-life.rb
+++ Casks/comic-life.rb
@@ -3,6 +3,8 @@ cask 'comic-life' do
   sha256 '8bcc6fdaf11b306880cdcdead626e894cbfb89b745a5f3f1621deb3c61027913'
 
   url "http://downloads.plasq.com/comiclife#{version.major}.zip"
+  appcast "https://s3.amazonaws.com/updater.plasq.com/comiclife#{version.major}-appcast.xml",
+          checkpoint: '066b1bca833379a7be5228ba65a572103059b41145ac0019298fe85554b47ec0'
   name 'Comic Life'
   homepage 'https://plasq.com/apps/comiclife/macwin/'
 
