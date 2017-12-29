0a6a8a12cfe10800f365424315478588d92e4f54
epistrephein <epistrephein@gmail.com>
2017-12-29 09:00:02 +0100

    Add zap stanza to atext 2.22.2

--- Casks/atext.rb
+++ Casks/atext.rb
@@ -9,4 +9,14 @@ cask 'atext' do
   homepage 'https://www.trankynam.com/atext/'
 
   app 'aText.app'
+
+  zap trash: [
+               '~/Library/Application Scripts/com.trankynam.aText',
+               '~/Library/Application Support/com.trankynam.aText',
+               '~/Library/Caches/com.trankynam.aText',
+               '~/Library/Containers/com.trankynam.aText',
+               '~/Library/Cookies/com.trankynam.aText.binarycookies',
+               '~/Library/Preferences/com.trankynam.aText.plist',
+               '~/Library/Saved Application State/com.trankynam.aText.savedState',
+             ]
 end
