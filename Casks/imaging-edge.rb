29df52ab0dc6994fee50a3e4dab3c2de3cafc02e
RJ Hsiao <rj19860725@gmail.com>
2017-12-30 15:36:05 +0800

    Update imaging-edge to 1.0.1_1712a,boACM00MOv

--- Casks/imaging-edge.rb
+++ Casks/imaging-edge.rb
@@ -1,9 +1,9 @@
 cask 'imaging-edge' do
-  version '1.0.0,1711a'
-  sha256 '12caf3a51f91ed97607873cbbaeb30913cc207a3f76abe9be43f7330f16a3393'
+  version '1.0.1_1712a,boACM00MOv'
+  sha256 '24312f445c59b2a569913f12231911d9dd6c486fa394044d31518f07da328fe0'
 
-  # ids.update.sony.net/IDC/OTU7dPSwXY was verified as official when first introduced to the cask
-  url "http://ids.update.sony.net/IDC/OTU7dPSwXY/IE#{version.before_comma.no_dots}_#{version.after_comma}.dmg"
+  # ids.update.sony.net/IDC/ was verified as official when first introduced to the cask
+  url "http://ids.update.sony.net/IDC/#{version.after_comma}/IE#{version.before_comma.no_dots}.dmg"
   name 'Sony Imaging Edge'
   homepage 'https://support.d-imaging.sony.co.jp/app/imagingedge/'
 
