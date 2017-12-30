d3558d448018af4ae92007427de882575c735b88
Hong Minhee <hong.minhee@gmail.com>
2017-12-31 06:33:38 +0900

    Update realm-studio to 1.14.0 (#42405)

--- Casks/realm-studio.rb
+++ Casks/realm-studio.rb
@@ -1,10 +1,10 @@
 cask 'realm-studio' do
-  version '1.6.1'
-  sha256 '125f2885e061c535959fda873793c51f103cfcdd42d2e31690856bcd7cc1b92a'
+  version '1.14.0'
+  sha256 '46b47e70119da73923524dbbd7783b93f3ef83fa3f8cc10a801a5d6fce8eefb1'
 
   url "https://static.realm.io/downloads/realm-studio/Realm%20Studio-#{version}-mac.zip"
   appcast 'https://static.realm.io/downloads/realm-studio/latest-mac.yml',
-          checkpoint: 'd0fc66ed08901414158ea9358001bde0acd6acb721e29057781417097893a2bd'
+          checkpoint: 'a798fa52da75bba2039a1846b4367da2c7e2c9ff728dd9058edb0f04090f7f8b'
   name 'Realm Studio'
   homepage 'https://realm.io/products/realm-studio/'
 
