c7a44c8618f20d5a76bc940d21f0e096be5266f6
Dimitri Cappelle <14368678+dimitricappelle@users.noreply.github.com>
2017-12-30 18:35:48 +0100

    Update tinymediamanager to 2.9.7_8513bee (#42394)

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,10 +1,10 @@
 cask 'tinymediamanager' do
-  version '2.9.5_1bf3695'
-  sha256 '2056707e47746c5aa3c568fe264f1534a44eed9206e774aefd86921557d49552'
+  version '2.9.7_8513bee'
+  sha256 '5efc8c2ac5ea52f515c6a9bc3bb168190d337e79f85e1d7e2c2559dd4f11f766'
 
   url "https://release.tinymediamanager.org/dist/tmm_#{version}_mac.zip"
   appcast 'https://release.tinymediamanager.org/',
-          checkpoint: 'c4d3197a686e085fe1052b4fa26d024557cd64e256c190344ab75a57f7f2cab0'
+          checkpoint: '2f3d4f22d255adbad6220670675b4c8390cab776edbd80d9281f83bccabb6cbb'
   name 'tinyMediaManager'
   homepage 'https://www.tinymediamanager.org/'
 
