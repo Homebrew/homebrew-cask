f2ecf76b95e214b6d188cf9f65f7d0fa75d397a5
tiiiecherle <tiiiecherle@gmail.com>
2017-12-30 22:32:55 +0100

    Update xact to 2.43 (#42395)
    * Update xact to 2.43
    
    * Update xact.rb

--- Casks/xact.rb
+++ Casks/xact.rb
@@ -1,12 +1,12 @@
 cask 'xact' do
-  version '2.41'
-  sha256 'bb46554ea4092d3ad905a787466a2dc10db956cb0e30750d51fad2f25f7f27b0'
+  version '2.43'
+  sha256 '33d84f323875f165f2ea0e7ba295335cd74efb1b86f272b60d43e3a7ad460ea5'
 
   url "http://xact.scottcbrown.org/xACT#{version}.zip"
   appcast 'http://xactupdate.scottcbrown.org/xACT.xml',
-          checkpoint: '53320b5b35c5497b28f20a9cf85990474fb5b4efd33351c2a83d61d2671f4fbf'
+          checkpoint: 'b0dbdccf2c89725e1b4678de12b7eb81cecc1ba0ab5b6e3397846009e97cad9e'
   name 'xACT'
   homepage 'http://xact.scottcbrown.org/'
 
-  app "xACT #{version}/xACT.app"
+  app "xACT#{version}/xACT.app"
 end
