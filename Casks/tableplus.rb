9188e90e8a7a8f5e4c4544107a6b796bbe6f29f5
commitay <commitay@users.noreply.github.com>
2017-12-31 17:18:20 +1000

    Update tableplus.rb

--- Casks/tableplus.rb
+++ Casks/tableplus.rb
@@ -2,8 +2,8 @@ cask 'tableplus' do
   version '1.0,80'
   sha256 '9b94cf47d08df9025139c751c9771a4bae6008d0b0455aa4a549be10e164a875'
 
-  # s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/tableplus-osx-builds/80/TablePlus.zip'
+  # s3.amazonaws.com/tableplus-osx-builds was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/tableplus-osx-builds/#{version.after_comma}/TablePlus.zip"
   appcast 'https://tableplus.io/osx/version.xml',
           checkpoint: '38f9fdefd8b64b7d4986b2dcfaa03ed783abdd11087a85b80ff93c41051088a6'
   name 'TablePlus'
