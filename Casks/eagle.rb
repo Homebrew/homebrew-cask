e3f20718e76fc21aa1ce47a13cf888c1c90b94c0
FlechaMaker <flechacreador@gmail.com>
2018-01-01 12:57:40 +0900

    Update eagle to 8.5.1

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -1,8 +1,11 @@
 cask 'eagle' do
-  version '8.5.0'
-  sha256 '7058053c05fdf542ed53b15db151e362fa67147e3dc90231283be7b2efbac8e7'
+  version '8.5.1'
+  sha256 '04e9f900d75e9c13cf4b6d4a6f34dfec8d6f71fb6bb5e845c329a35a79af4870'
 
-  url "https://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
+  # eagle-updates.circuits.io was verified as official when first introduced to the cask
+  url "https://eagle-updates.circuits.io/downloads/#{version.split('.').join('_')}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
+  appcast 'http://eagle.autodesk.com/eagle/release-notes',
+          checkpoint: 'e44c3a72240f3a910f73f6d1900317d9d742844187610d94d5e47df00072b286'
   name 'Autodesk EAGLE'
   homepage 'https://www.autodesk.com/products/eagle/overview'
 
