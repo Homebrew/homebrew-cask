09ebe8d73777abeec736b0612a3529ec24507952
Victor Popkov <victor@popkov.me>
2018-01-02 17:02:59 +0200

    Update compositor to 0.9.20 (#42462)

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,10 +1,10 @@
 cask 'compositor' do
-  version '0.9.19'
-  sha256 'c55741768c5566dda8a28231a2d4d168acbdce68373f8cde3d52e6d3245da7d9'
+  version '0.9.20'
+  sha256 'bf4dae3d47f59c472089b4a65c634d8c213474b676af96143be57ec3332e16ab'
 
   url 'http://compositorapp.com/downloads/Compositor.dmg'
   appcast 'http://compositorapp.com/updates/appcast.xml',
-          checkpoint: '37c9430436d93250c5c5937613026a5016b6a3d51b91d8c5a990bce603506a29'
+          checkpoint: '83aa3e1735f0e06eb4f46ac9218fb2fa1c6ffaead4c898e6f7b68f5d57b06b8e'
   name 'Compositor'
   homepage 'http://compositorapp.com/'
 
