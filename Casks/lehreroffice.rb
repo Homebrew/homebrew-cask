9f1bba8f3817b7c0cf3fb44965a2ac3ebf08c6f6
Anver Housseini <contact@anver.ch>
2018-01-02 12:50:44 +0100

    Update lehreroffice.rb

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -3,6 +3,8 @@ cask 'lehreroffice' do
   sha256 :no_check
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_osx.dmg'
+  appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop',
+          checkpoint: '891f6ad23d0554d4e2c87f2667d64ab326d8858883ff80fcd7fa92118a4607f7'
   name 'LehrerOffice'
   homepage 'https://www.lehreroffice.ch/'
 
