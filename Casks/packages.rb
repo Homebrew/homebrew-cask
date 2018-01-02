7e15aa979bbe0347e4286dcebf80464354675ee4
Anver Housseini <contact@anver.ch>
2018-01-02 21:30:32 +0100

    Add appcast Packages.app (#42473)
    * Update packages.rb
    
    * Update packages.rb
    
    * Update packages.rb

--- Casks/packages.rb
+++ Casks/packages.rb
@@ -1,8 +1,10 @@
 cask 'packages' do
-  version :latest
-  sha256 :no_check
+  version '1.2.1'
+  sha256 'cf98848b287e6bda61d4692c5876370db951a83944e2cf8c1cf96adc0ad1ffde'
 
   url 'http://s.sudre.free.fr/Software/files/Packages.dmg'
+  appcast 'http://s.sudre.free.fr/Software/documentation/RemoteVersion.plist',
+          checkpoint: '685d6dc4ba3f9cf6ec6e65be5c4a5a500e1e13e219193362bcbc5f8e4927e830'
   name 'Packages'
   homepage 'http://s.sudre.free.fr/Software/Packages/about.html'
 
