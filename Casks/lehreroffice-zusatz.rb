53009b63d549072e131433299ebe96bff10b37ed
Anver Housseini <contact@anver.ch>
2018-01-02 15:20:49 +0100

    Add appcast LehrerOffice Zusatz.app (#42450)

--- Casks/lehreroffice-zusatz.rb
+++ Casks/lehreroffice-zusatz.rb
@@ -1,8 +1,10 @@
 cask 'lehreroffice-zusatz' do
-  version :latest
-  sha256 :no_check
+  version '2017.19.1'
+  sha256 '0448ea88d5286ca2a7bd0f924018cc4e4fe7266c57d88553fe8fd5407432c8d4'
 
   url 'https://www.lehreroffice.ch/lo/dateien/zusatz/lo_zusatz_osx.dmg'
+  appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Zusatz',
+          checkpoint: '891f6ad23d0554d4e2c87f2667d64ab326d8858883ff80fcd7fa92118a4607f7'
   name 'LehrerOffice Zusatz'
   homepage 'https://www.lehreroffice.ch/'
 
