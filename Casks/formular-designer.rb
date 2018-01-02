fdf3e2582c72eed16ee8f5c3ceaa9d9e33377bfe
Anver Housseini <contact@anver.ch>
2018-01-02 12:55:36 +0100

    Update formular-designer.rb

--- Casks/formular-designer.rb
+++ Casks/formular-designer.rb
@@ -3,6 +3,8 @@ cask 'formular-designer' do
   sha256 :no_check
 
   url 'https://www.lehreroffice.ch/lo/dateien/designer/lo_designer_mac.dmg'
+  appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Designer',
+          checkpoint: '3ed41774545c3c4d19ceb71cedfb8eb7f8231a16b61ca36e1d7f0b21a32a8316'
   name 'Formular-Designer'
   homepage 'https://www.lehreroffice.ch/'
 
