228e27f9e0cdec78ba8fdc65c170ff7bdad9713c
Vítor Galvão <info@vitorgalvao.com>
2018-01-02 16:32:26 +0000

    Update webponize.rb

--- Casks/webponize.rb
+++ Casks/webponize.rb
@@ -3,9 +3,9 @@ cask 'webponize' do
   sha256 'ce394e91d07f636571a2a21cf183993ad9170794877e6744148ff1faf0ded2b3'
 
   # github.com/webponize/webponize was verified as official when first introduced to the cask
-  url 'https://github.com/webponize/webponize/releases/download/v2.1.0/WebPonize.app.zip'
-  appcast 'https://webponize.org/appcast.xml',
-          checkpoint: '8447933d59c9a69610b891ff4ba95f02ba057b987d9058702f554658d3e9d395'
+  url "https://github.com/webponize/webponize/releases/download/v#{version}/WebPonize.app.zip"
+  appcast 'https://github.com/webponize/webponize/releases.atom',
+          checkpoint: '23acecb403ea619bcce7df816eefe9dea35d543958e1b339cee2d3d3081a6ca8'
   name 'WebPonize'
   homepage 'https://webponize.org/'
 
