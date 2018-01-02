124e759b6df44a534cf4cd4c88a5608ce040913b
Anver Housseini <contact@anver.ch>
2018-01-02 19:46:27 +0100

    Update packages.rb

--- Casks/packages.rb
+++ Casks/packages.rb
@@ -4,7 +4,7 @@ cask 'packages' do
 
   url 'http://s.sudre.free.fr/Software/files/Packages.dmg'
   appcast 'http://s.sudre.free.fr/Software/documentation/RemoteVersion.plist',
- +          checkpoint: '685d6dc4ba3f9cf6ec6e65be5c4a5a500e1e13e219193362bcbc5f8e4927e830'
+          checkpoint: '685d6dc4ba3f9cf6ec6e65be5c4a5a500e1e13e219193362bcbc5f8e4927e830'
   name 'Packages'
   homepage 'http://s.sudre.free.fr/Software/Packages/about.html'
 
