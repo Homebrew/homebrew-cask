a0b83b504593978bee7e733569b2377f4f32e77b
Budh Ram Gurung <budhram.gurung01@gmail.com>
2018-01-03 15:05:52 +0530

    Update minishift to 1.11.0

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -1,10 +1,10 @@
 cask 'minishift' do
-  version '1.10.0'
-  sha256 '7f4c5cc0f1c7ead530ecc2872346867803144e6cbd8d918217f3e0e35fe96e11'
+  version '1.11.0'
+  sha256 'e1012a1fe783f76f9a1548784c9c602d2605d0e84ca8259ad90835901edb18dd'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom',
-          checkpoint: 'e75c0d34e934e46230550fa345839aba546da786f025ba44fd30ed19d0bbdbc3'
+          checkpoint: '817b471f772b0ddc749de250edfead2af9aa257da800b5d1040c969bd85ea622'
   name 'Minishift'
   homepage 'https://github.com/minishift/minishift'
 
