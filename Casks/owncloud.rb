d06542a3a10ffc8febe437b6aecb6a44fc6a42b0
Riyad Preukschas <r.preukschas@getsafe.de>
2018-01-03 15:44:56 +0000

    Update ownCloud to 2.4.0

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,10 +1,10 @@
 cask 'owncloud' do
-  version '2.3.4.8537'
-  sha256 '04a48607fb2ecbbc88b00ebf00b74875ad3dc53b104a6749d0f865f038737255'
+  version '2.4.0.8836'
+  sha256 'fd302cacdd6a66de01c706ad351a92957c79769d5a4a16babf5feda4f97699d0'
 
   url "https://download.owncloud.com/desktop/stable/ownCloud-#{version}.pkg"
   appcast 'https://github.com/owncloud/client/releases.atom',
-          checkpoint: 'b6b882ce94c26e285527e49f02c3e9ea91d37bb6276e95d8c513224f052564cf'
+          checkpoint: '774c47e34cfe57ad923f30c7f914c8efe726a3fe982f70d3897eff1f24937cee'
   name 'ownCloud'
   homepage 'https://owncloud.com/'
 
