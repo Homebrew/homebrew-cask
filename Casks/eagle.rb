f4b37c137248f08b352b9a1b3bf726ad36bb6bfd
FlechaMaker <flechacreador@gmail.com>
2018-01-03 21:33:40 +0900

    Use version methods / Remove appcast

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -3,9 +3,7 @@ cask 'eagle' do
   sha256 '04e9f900d75e9c13cf4b6d4a6f34dfec8d6f71fb6bb5e845c329a35a79af4870'
 
   # eagle-updates.circuits.io was verified as official when first introduced to the cask
-  url "https://eagle-updates.circuits.io/downloads/#{version.split('.').join('_')}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
-  appcast 'http://eagle.autodesk.com/eagle/release-notes',
-          checkpoint: 'e44c3a72240f3a910f73f6d1900317d9d742844187610d94d5e47df00072b286'
+  url "https://eagle-updates.circuits.io/downloads/#{version.dots_to_underscores}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   name 'Autodesk EAGLE'
   homepage 'https://www.autodesk.com/products/eagle/overview'
 
