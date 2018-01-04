0e7c25ddac6882c5def4672bf8adee959878fde4
Victor Popkov <victor@popkov.me>
2018-01-04 17:16:43 +0200

    Update comparemerge to 2.04x

--- Casks/comparemerge.rb
+++ Casks/comparemerge.rb
@@ -1,10 +1,10 @@
 cask 'comparemerge' do
-  version '2.03x'
-  sha256 'ed6f53dd59481868ff29dc1d8800f5a821d918ce7fee1677f450bf302f23025c'
+  version '2.04x'
+  sha256 'ae931453d050686f36c3a7dffe2660e85a95df4a2627dfa6da32f915e491a268'
 
   url "https://downloads.sourceforge.net/comparemergenosandbox/CompareMerge%20#{version}.zip"
   appcast 'https://sourceforge.net/projects/comparemergenosandbox/rss',
-          checkpoint: '0f230c9700bd27af2530109a80f788da31766bbcc708f98f9401256f62fa3d16'
+          checkpoint: '327ed1761975d44d1fc05aa459ff69f4931759e5881e9e68030a38dea4379ba2'
   name 'CompareMerge'
   homepage 'https://sourceforge.net/projects/comparemergenosandbox/'
 
