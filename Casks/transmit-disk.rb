e78e62c831954ba4b9c294c9bf230d47da4f0497
Anaz Haidhar <mr.anazhd@gmail.com>
2018-01-04 18:34:59 +0800

    Update to version 4.4.13

--- Casks/transmit-disk.rb
+++ Casks/transmit-disk.rb
@@ -1,5 +1,5 @@
 cask 'transmit-disk' do
-  version '4.4.12'
+  version '4.4.13'
   sha256 '3a4e3515450e597fc7f0d75f0d53df215bea3b6d9615c0a960f471b1185602ef'
 
   url "https://download.panic.com/transmit/Transmit%20Disk%20#{version}.pkg.zip"
