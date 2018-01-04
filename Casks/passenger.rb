1477da112daa5e8064a593f5b76e8442e7f3a064
Anver Housseini <contact@anver.ch>
2018-01-04 21:55:42 +0100

    Update passenger to 4.6 (#42492)
    * Update passenger to 4.6
    
    * https

--- Casks/passenger.rb
+++ Casks/passenger.rb
@@ -1,12 +1,12 @@
 cask 'passenger' do
   version '4.6'
-  sha256 'c2905c8d3de7fe04fad0e18a243f093b8b50d910a61d2d0f1860c4a1f47d6e8c'
+  sha256 'd940757e97e89f173aeb8c874a82d62228c9f8b7e4c23fce14d3321135e9ff98'
 
-  url 'http://macinmind.com/Passenger.dmg'
+  url 'https://macinmind.com/Passenger.dmg'
   appcast 'https://macinmind.com/pads/PassengerAppcast.xml',
           checkpoint: '625e438d2cbe45ee5a80ebb23d7d84324682ff2cd10aeabcfa5dc7317f6d433f'
   name 'Passenger'
-  homepage 'http://macinmind.com/?area=app&app=passenger'
+  homepage 'https://macinmind.com/?area=app&app=passenger'
 
   suite 'Passenger'
 end
