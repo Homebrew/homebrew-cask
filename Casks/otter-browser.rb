b0b9b23dea3a412cb5707229ff5890f57829ecec
Vítor Galvão <info@vitorgalvao.com>
2018-01-05 21:55:47 +0000

    Update otter-browser.rb

--- Casks/otter-browser.rb
+++ Casks/otter-browser.rb
@@ -3,7 +3,7 @@ cask 'otter-browser' do
   sha256 '655e7078483dbedf7d3a99ea2ba9df1eb30bfb5e5ad656a1a34fb6b6c65ec5f9'
 
   # sourceforge.net/otter-browser was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/otter-browser/otter-browser-#{version}-rc1v2-setup.dmg"
+  url "https://downloads.sourceforge.net/otter-browser/otter-browser-#{version}-setup.dmg"
   appcast 'https://sourceforge.net/projects/otter-browser/rss',
           checkpoint: '0229f008fe06822195c01a46c8c29a515c1e6c01a3f9050b756390f65de6b70d'
   name 'Otter Browser'
