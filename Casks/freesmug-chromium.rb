410d3681ab0ebc1dcefdaebbae8a5d29ed5936a0
commitay <commitay@users.noreply.github.com>
2018-01-05 16:26:50 +1000

    Update freesmug-chromium to 63.0.3239.132

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -1,11 +1,11 @@
 cask 'freesmug-chromium' do
-  version '63.0.3239.108'
-  sha256 '789a2ec9b753b0448ec27665b30aae2c597c6f98ed2d906e2a54e1bd40c82733'
+  version '63.0.3239.132'
+  sha256 '9255680e9a07d493ab7af071c3b9781ecc7672bcdb83fcb42684fbad512961f0'
 
   # sourceforge.net/osxportableapps was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/osxportableapps/Chromium_OSX_#{version}.dmg"
   appcast 'https://sourceforge.net/projects/osxportableapps/rss?path=/Chromium',
-          checkpoint: '2aa7f29434fec83c60470a5686e5aa0adec5962d8a019f3e588dfc9d65672115'
+          checkpoint: '890507a835ef1b4d95f4640c565e936265f0f6a6f5060f286f9a579d12fcdc24'
   name 'Chromium'
   homepage 'http://www.freesmug.org/chromium'
 
