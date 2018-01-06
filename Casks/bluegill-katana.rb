a0fefe1de89e571ee08f240a83b0dd464ca6b764
gtmgianni <25732197+gtmgianni@users.noreply.github.com>
2018-01-06 19:35:08 +0000

    Update bluegill-katana to 1.1.1

--- Casks/bluegill-katana.rb
+++ Casks/bluegill-katana.rb
@@ -2,12 +2,11 @@ cask 'bluegill-katana' do
   version '1.1.1'
   sha256 'bb618628bd335c60dab87bc82c4e5d72df8e80e0baf5510c861962afda915fbc'
 
-  # github.com/bluegill/katana was verified as official when first introduced to the cask
-  url "https://github.com/bluegill/katana/releases/download/v#{version}/katana-#{version}-mac.zip"
-  appcast 'https://github.com/bluegill/katana/releases.atom',
-          checkpoint: '4ea7ef6c6c4aca75a63b6fff9bb15b78a53a40112e660a1d980e4d825e93e38f'
+  url "https://github.com/washedoutco/katana/releases/download/v#{version}/katana-#{version}-mac.zip"
+  appcast 'https://github.com/washedoutco/katana/releases.atom',
+          checkpoint: '39ca93772f2b3236537672f582296ce6eeafc652807ec475a11728d370ced13c'
   name 'Katana'
-  homepage 'https://getkatana.com/'
+  homepage 'https://github.com/washedoutco/katana/'
 
   app 'Katana.app'
 end
