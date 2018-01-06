c50fca4dbbf88575f8886d2eb9563c762edef707
Jeppe Toustrup <tenzer@tenzer.dk>
2018-01-06 18:35:34 +0000

    Update choosy to 1.3 (#42548)

--- Casks/choosy.rb
+++ Casks/choosy.rb
@@ -3,13 +3,13 @@ cask 'choosy' do
     version '1.1'
     sha256 'c6530d4e0dddbf47c6a8999bda8f3a5ef1857f4481b9325e56cfe00f05b2022c'
   else
-    version '1.2.2'
-    sha256 'a6d2ee7c3e9e0ab9b6170305877e17bd070a3d3eb589643bee7e361d9dd68f23'
+    version '1.3'
+    sha256 'cb1f40df11ac1b52354f4b81367462d2646a6d023c64bafe5022fcec52f796cd'
   end
 
   url "https://downloads.choosyosx.com/choosy_#{version}.zip"
   appcast 'https://www.choosyosx.com/sparkle/feed',
-          checkpoint: '26d78f4ed03425e2c1bb0bc7055a678c0df548a6f5aebf4f6d9b428811c92195'
+          checkpoint: '9db0cc8651f8f826450d5c6cb07a2303dc5727dabcab5fe779220ba2904cedb3'
   name 'Choosy'
   homepage 'https://www.choosyosx.com/'
 
