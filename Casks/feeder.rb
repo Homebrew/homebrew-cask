9e23aefee77f46acf128ba3b04d5639f060effd7
Victor Popkov <victor@popkov.me>
2018-01-07 20:59:49 +0200

    Update feeder to 3.5.6 (#42580)

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -1,10 +1,10 @@
 cask 'feeder' do
-  version '3.5.5'
-  sha256 'da842a18b6589bca7a3e9669b06590ce80444d6f3967c8414498e11c907426f1'
+  version '3.5.6'
+  sha256 '267e4e8b50345d53948d378a9f36f90edbe7f4c7023b8e22e2406ede51bcfe93'
 
   url "https://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
   appcast "https://reinventedsoftware.com/feeder/downloads/Feeder#{version.major}.xml",
-          checkpoint: '5ae4817d34e2483e02d8da49f3a21d7be34ba3af6de8b3d679d8de3d76f79c38'
+          checkpoint: '4f3d28521c56636c0b1264476c32ce1374ba4522920f641ee3d9f320f24c1a75'
   name 'Feeder'
   homepage 'https://reinventedsoftware.com/feeder/'
 
