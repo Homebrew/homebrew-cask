968c25b935630a5fb7098b3a876aff5aefae776f
Victor Popkov <victor@popkov.me>
2018-01-07 21:08:56 +0200

    Update houdahspot to 4.3.4 (#42582)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,10 +1,10 @@
 cask 'houdahspot' do
-  version '4.3.2'
-  sha256 'c634f327692cb1acb106e76b86621f267351ba2a2311e9a65ba01399765809e9'
+  version '4.3.4'
+  sha256 '82af7d666e902cf204fdddaed5f0de24c0f8a09e0adc92fce98f345c34f46aa0'
 
   url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml",
-          checkpoint: 'eee3109d1b1c4e22ad75d54e7a9158eb14db047f15b0e9ade6adb895202c787d'
+          checkpoint: '67d55dc37d56ffa279ba69e89ae007c3e1548080d065c3266f5e249d4832fb3e'
   name 'HoudahSpot'
   homepage 'https://www.houdah.com/houdahSpot/'
 
