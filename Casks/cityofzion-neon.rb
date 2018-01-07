f239a344f315c4873793c70320d2867a820c0d7c
Mateusz Czeladka <mateusz.szczap@gmail.com>
2018-01-07 14:24:48 +0100

    Bug fix for changed file name
    ```
    ==> Found outdated apps
         Cask             Current  Latest  A/U    Result    
    1/1  cityofzion-neon           0.0.9        [OUTDATED]  
    
    Do you want to upgrade 1 app [y/N]? y
    ==> Upgrading cityofzion-neon to 0.0.9
    ==> Satisfying dependencies
    ==> Downloading https://github.com/CityOfZion/neon-wallet/releases/download/0.0.9/Neon-0.0.9.dmg
    
    curl: (22) The requested URL returned error: 404 Not Found
    Error: Download failed on Cask 'cityofzion-neon' with message: Download failed: https://github.com/CityOfZion/neon-wallet/releases/download/0.0.9/Neon-0.0.9.dmg
    ```

--- Casks/cityofzion-neon.rb
+++ Casks/cityofzion-neon.rb
@@ -2,7 +2,7 @@ cask 'cityofzion-neon' do
   version '0.0.9'
   sha256 '5e76baeb7ae51a233270364a50886500b5ccfb5e5f6bd4c72eb0c49a344992c3'
 
-  url "https://github.com/CityOfZion/neon-wallet/releases/download/#{version}/Neon-#{version}.dmg"
+  url "https://github.com/CityOfZion/neon-wallet/releases/download/#{version}/Neon-#{version}.Mac.dmg"
   appcast 'https://github.com/CityOfZion/neon-wallet/releases.atom',
           checkpoint: '00888edfcf0a1f09c9002ea4ce2fea014e1089a82b6f3aa2dc24eb2e44a1a4b3'
   name 'Neon Wallet'
