2c67064694e16395dea1372664db460c1da4fab5
Ben Ramsey <ben@benramsey.com>
2018-01-08 01:06:06 -0600

    Specify the Evergreen version at 1.0d30 and add the sha256

--- Casks/evergreen.rb
+++ Casks/evergreen.rb
@@ -1,8 +1,8 @@
 cask 'evergreen' do
-  version :latest
-  sha256 :no_check
+  version '1.0d30'
+  sha256 'f9290d0fb028980e95d3d1cb1770f1f352d376317684cef6ea63ecd7aa844dc2'
 
-  url 'https://ranchero.com/downloads/Evergreen-latest.zip'
+  url "https://ranchero.com/downloads/Evergreen#{version}.zip"
   appcast 'https://ranchero.com/downloads/evergreen-beta.xml',
           checkpoint: '8e32778a4f5c31bf091df662426dd12834af89fde814bcb6dd2668c57644302b'
   name 'Evergreen'
