dadd5721d8eea92d8b6f51648918d5aad19b621f
SergioOS <ortega@tune-is.de>
2018-01-08 16:45:01 +0100

    Update shotcut to 18.01.02

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,11 +1,11 @@
 cask 'shotcut' do
-  version '17.12.03'
-  sha256 '0709d27608922b1f57d7c59abddcfd980500756e13b17297c830610ad291011a'
+  version '18.01.02'
+  sha256 '12ee301b211dba6d0aa062587d0ec2e673980570273faeb41b25083b5cdd49c9'
 
   # github.com/mltframework/shotcut was verified as official when first introduced to the cask
   url "https://github.com/mltframework/shotcut/releases/download/v#{version.major_minor}/shotcut-osx-x86_64-#{version.no_dots}.dmg"
   appcast 'https://github.com/mltframework/shotcut/releases.atom',
-          checkpoint: '68f903e0b3577c2d4e814023a23c49d83f1739c21286c3a7bc6ab075312fd23f'
+          checkpoint: '81e23e46cd9363d1b519dd1c755960501e66dcac70cf73b269930108356de701'
   name 'Shotcut'
   homepage 'https://www.shotcut.org/'
 
