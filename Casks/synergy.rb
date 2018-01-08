c723b7a28730f05a48c27bcf023da2b28894236b
Moritz Reiter <moritz@anothernode.com>
2018-01-08 10:27:48 +0100

    Update synergy to 2.0.4,b1322-fcb59be4 (#42604)

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,6 +1,6 @@
 cask 'synergy' do
-  version '2.0.3,b1243-8a08e664'
-  sha256 '98c94eab66516fe2d9d840562cc12fca09696c0882e1f14692ba20d9a78aeb8b'
+  version '2.0.4,b1322-fcb59be4'
+  sha256 'f2069f7097e7b2cf7f3160e17d5df04125b0d28ac6a9b32d9e97762e234b0789'
 
   url "https://binaries.symless.com/v#{version.before_comma}/Synergy_v#{version.before_comma}-stable_#{version.after_comma}.dmg"
   appcast 'https://github.com/symless/synergy-core/releases.atom',
