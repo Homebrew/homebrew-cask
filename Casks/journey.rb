89b280446a3ea8768630e8a3d1997a162225873c
Robert Martin <rmartin@rmart.in>
2018-01-07 23:22:48 -0500

    Update journey to 2.3.0

--- Casks/journey.rb
+++ Casks/journey.rb
@@ -1,11 +1,11 @@
 cask 'journey' do
-  version '2.2.11'
-  sha256 '7ab862b6db0337cbbc632a2b791dd73dd564570b6ff71ddbbd790d0ab850a0ba'
+  version '2.3.0'
+  sha256 'e6db687575a582c5fb8e7ef4eb7cc1b4bff8434ec7b81e64caa90d59c6ad1d20'
 
   # github.com/2-App-Studio/journey-releases was verified as official when first introduced to the cask
   url "https://github.com/2-App-Studio/journey-releases/releases/download/v#{version}/Journey-darwin-x64-#{version}.dmg"
   appcast 'https://github.com/2-App-Studio/journey-releases/releases.atom',
-          checkpoint: '91751f8f2b1540408acddb021454703f0fc0fa65aaf3aa9e9556c742dc86b351'
+          checkpoint: 'ecc94c671d71453cb05209e2e86b0d8984cdba5df8419a4039907f318af9853a'
   name 'Journey'
   homepage 'https://2appstudio.com/journey/'
 
