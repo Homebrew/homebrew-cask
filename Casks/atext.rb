01571f2e0e70fade97f6012863060270873c80e2
Vítor Galvão <info@vitorgalvao.com>
2018-01-09 17:14:37 +0000

    Update atext.rb

--- Casks/atext.rb
+++ Casks/atext.rb
@@ -2,7 +2,7 @@ cask 'atext' do
   version '2.22.3'
   sha256 '553957237e7dbbcc25208ba030f2efc1ce428efef93766b12141ba665f528edd'
 
-  url 'http://www.trankynam.com/atext/downloads/aText.dmg'
+  url 'https://www.trankynam.com/atext/downloads/aText.dmg'
   appcast 'https://www.trankynam.com/atext/aText-Appcast.xml',
           checkpoint: '01816bad0f3226f4d9941b5c889f7042717486b5aeb5e3847b8538d90ea939a9'
   name 'aText'
