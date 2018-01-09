c80a636db672421dadb69660b2b80be9ec053eb6
Anaz Haidhar <mr.anazhd@gmail.com>
2018-01-09 11:46:05 +0800

    Update filezilla to 3.30.0

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,6 +1,6 @@
 cask 'filezilla' do
-  version '3.29.0'
-  sha256 'e7264da974dc95ffb36af0bbe2a0233fbbb876a11f67e69417fa3e15f52fc1c2'
+  version '3.30.0'
+  sha256 '48bf02fe252be25901e6873ea4b752628a4d0b7760dd49b9cc44e10f2dbbec73'
 
   # sourceforge.net/filezilla was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
