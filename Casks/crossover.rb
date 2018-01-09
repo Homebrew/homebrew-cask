40b9e2b11cd7034bc6b4286b7563f9beb65d8b86
Jackson Law <178053+jlaw@users.noreply.github.com>
2018-01-09 13:00:33 -0800

    Update crossover to 17.0.0

--- Casks/crossover.rb
+++ Casks/crossover.rb
@@ -1,10 +1,10 @@
 cask 'crossover' do
-  version '16.2.5'
-  sha256 '4a9a4ab8020cdff302be32b79050453563e2d309307a02ee23a4bd0c01a3b1e0'
+  version '17.0.0'
+  sha256 'd8127a57ee04547fb9b87497d0abdde3bd6dd032d336c7ad0aeb3b234107bd79'
 
   url "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-#{version}.zip"
   appcast 'https://www.codeweavers.com/xml/versions/cxmac.xml',
-          checkpoint: 'e3fab70c871c49adbbe5423db6b1deb07160108d2d0970808569faa532c75b4d'
+          checkpoint: 'df59505326aa1d30307891eaaa673a5a5e944eb6dfb37eaafb72ed879fb04209'
   name 'CrossOver'
   homepage 'https://www.codeweavers.com/products/crossover-mac/'
 
