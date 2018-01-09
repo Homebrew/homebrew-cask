5266f605669d3069c10b3634b5a89ee78251fc8a
Vítor Galvão <info@vitorgalvao.com>
2018-01-09 20:34:54 +0000

    Update ganache to 1.0.2 (#42659)

--- Casks/ganache.rb
+++ Casks/ganache.rb
@@ -1,11 +1,11 @@
 cask 'ganache' do
-  version '1.0.1'
-  sha256 '40f1600a81737b653349152af9d94af8bc46233978584bf47d8182404e973d56'
+  version '1.0.2'
+  sha256 'fd79850eb2a7a6b28022cdf31724873c225817fdb06d00315aab50719bd69394'
 
   # github.com/trufflesuite/ganache was verified as official when first introduced to the cask
   url "https://github.com/trufflesuite/ganache/releases/download/v#{version}/Ganache-#{version}.dmg"
   appcast 'https://github.com/trufflesuite/ganache/releases.atom',
-          checkpoint: '4ec6dfca043d894dab810fa8284b25773e53a4cad38496342e7cc9105253f9b2'
+          checkpoint: '238b0de58cc42ce87403dcbe28bcb4f2a67acd28a23dfc0bd9092fc4f16d4a81'
   name 'Ganache'
   homepage 'http://truffleframework.com/ganache/'
 
