cc3a81d4d1b13a536b7530f3f5dd458a32902d3c
Vítor Galvão <info@vitorgalvao.com>
2018-01-09 20:39:32 +0000

    Update monal.rb

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,8 +1,10 @@
 cask 'monal' do
-  version :latest
-  sha256 :no_check
+  version '2b6'
+  sha256 'fdc28726d1387571e44202f943ff3b16ad4078104e6d2b6b461b34f0348d382d'
 
   url 'http://monal.im/Monal-OSX/Monal-OSX.zip'
+  appcast 'https://monal.im/Monal-OSX/appcast.xml',
+          checkpoint: '8303a6fd9f0389e1288716f5ac4a6d6cba677f0713867766c9b526e26b10518f'
   name 'Monal XMPP Beta'
   homepage 'https://monal.im/'
 
