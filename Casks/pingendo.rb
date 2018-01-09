5df5379d0038bc47b4932af16332f9e3db39f264
Alexander Myltsev <myltsev@yandex-team.ru>
2018-01-09 21:18:19 +0300

    Update Pingendo to 4.0. (#42587)
    * Update Pingendo to 4.0.
    
    * pingendo

--- Casks/pingendo.rb
+++ Casks/pingendo.rb
@@ -1,10 +1,11 @@
 cask 'pingendo' do
-  version '3'
-  sha256 :no_check # required as upstream package is updated in-place
+  version '4.0,0621d7bf-a02b-4901-91ba-7d8a25f95395'
+  sha256 '8bd102c73e98b4f5085b4908526b9af1b3dd24cbea9bb7d622674049258d1b69'
 
-  url "http://download.pingendo.com/v#{version}/Pingendo.dmg"
+  # firebasestorage.googleapis.com/v0/b/pingendoauth.appspot.com was verified as official when first introduced to the cask
+  url "https://firebasestorage.googleapis.com/v0/b/pingendoauth.appspot.com/o/Pingendo%20#{version.before_comma}.dmg?alt=media&amp;token=#{version.after_comma}"
   name 'Pingendo'
   homepage 'https://pingendo.com/'
 
-  app "Pingendo#{version}.app"
+  app "Pingendo #{version.before_comma}.app"
 end
