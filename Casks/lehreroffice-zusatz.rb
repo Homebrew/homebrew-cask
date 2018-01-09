608f5fd79064fcb002c44395d8ed7ea3ec72fbfc
Anver Housseini <contact@anver.ch>
2018-01-09 12:15:15 +0100

    Update lehreroffice-zusatz to 2017.19.2

--- Casks/lehreroffice-zusatz.rb
+++ Casks/lehreroffice-zusatz.rb
@@ -1,10 +1,10 @@
 cask 'lehreroffice-zusatz' do
-  version '2017.19.1'
-  sha256 '0448ea88d5286ca2a7bd0f924018cc4e4fe7266c57d88553fe8fd5407432c8d4'
+  version '2017.19.2'
+  sha256 '6bae74545e3f4d98b471a90f7970b31f4e0bbe3f09a29845f7bb598bcd93f253'
 
   url 'https://www.lehreroffice.ch/lo/dateien/zusatz/lo_zusatz_osx.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Zusatz',
-          checkpoint: '891f6ad23d0554d4e2c87f2667d64ab326d8858883ff80fcd7fa92118a4607f7'
+          checkpoint: '951662e2da7713050ae216bb179ab2e11489198f5ae1926c76e7ee45c7e6578a'
   name 'LehrerOffice Zusatz'
   homepage 'https://www.lehreroffice.ch/'
 
