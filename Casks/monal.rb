358a76f5a56c5e1d7fb37e65a6bdf32678c1ab0b
Anurodh Pokharel <anurodhp@alum.bu.edu>
2018-01-09 15:19:40 -0500

    Added Monal for OSX Beta

---
+++ Casks/monal.rb
@@ -0,0 +1,10 @@
+cask 'monal' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://monal.im/Monal-OSX/Monal-OSX.zip'
+  name 'Monal XMPP Beta'
+  homepage 'https://monal.im/'
+
+  app 'Monal.app'
+end
