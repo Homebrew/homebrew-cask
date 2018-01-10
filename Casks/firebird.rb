d0ef07600375b268fcf7faee81ea7bfdd7359b06
William Gulian <williamgulian@gmail.com>
2018-01-10 10:02:55 -0500

    Update sha256 and fix file path for Firebird

--- Casks/firebird.rb
+++ Casks/firebird.rb
@@ -1,8 +1,8 @@
 cask 'firebird' do
   version '1.4'
-  sha256 'eaf84c7853326107628eec8b90fc8e1e78ea1afc84a7dfb3177ca50c273b7250'
+  sha256 '65b4755ea5d72e6004972908ec8c416e96f9d5c466ab01379543c27e54439c6e'
 
-  url "https://github.com/nspire-emus/firebird/releases/download/v#{version}/firebird-mac.dmg"
+  url "https://github.com/nspire-emus/firebird/releases/download/v#{version}/firebird-emu-mac.dmg"
   appcast 'https://github.com/nspire-emus/firebird/releases.atom',
           checkpoint: '35d06123bf4aee8f5eef821600daffa5a4240ce91c53b07fe2577e827249ddf8'
   name 'firebird'
