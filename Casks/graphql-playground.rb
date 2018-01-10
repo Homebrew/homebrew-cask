cdb83f06d0400d6b67ee26377555f9170321cd5d
Alexandre Alonso <alex4551@gmail.com>
2018-01-10 13:36:58 +0800

    update graphql playground 1.3.23 (#42670)

--- Casks/graphql-playground.rb
+++ Casks/graphql-playground.rb
@@ -1,10 +1,10 @@
 cask 'graphql-playground' do
-  version '1.3.22'
-  sha256 '490dfaf6d2121660073ec1c762757e6777ae88ec2635e65c1f5f1d1f4c99ec22'
+  version '1.3.23'
+  sha256 'b7215849c8f5fb8e0fe4ccd5a6bb6fbe4ba31cd633927bfe3edb29a68d1b6b5b'
 
   url "https://github.com/graphcool/graphql-playground/releases/download/v#{version}/graphql-playground-electron-#{version}-mac.zip"
   appcast 'https://github.com/graphcool/graphql-playground/releases.atom',
-          checkpoint: '898abe2cf2f46e394ffe14fec40123aeaeb9e473128dd93b51ed85c84204578c'
+          checkpoint: '5f8475933c34ada20803b316798c686de15a3a41583baf996781934df6a71b9a'
   name 'GraphQL Playground'
   homepage 'https://github.com/graphcool/graphql-playground'
 
