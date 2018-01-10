5d60b139b007e4269c0a7f92008db829f42b4d8c
commitay <commitay@users.noreply.github.com>
2018-01-10 12:08:27 +1000

    2b7

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,10 +1,10 @@
 cask 'monal' do
-  version '2b6'
-  sha256 'fdc28726d1387571e44202f943ff3b16ad4078104e6d2b6b461b34f0348d382d'
+  version '2b7'
+  sha256 '3a2ac928be431a27e73f0ff4237ed52e8375e4e995d8f6512d4c3cd3fa2c7673'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
   appcast 'https://monal.im/Monal-OSX/appcast.xml',
-          checkpoint: '8303a6fd9f0389e1288716f5ac4a6d6cba677f0713867766c9b526e26b10518f'
+          checkpoint: 'c694282881493b66b7fda296ea1fdfc4ed142f5b8bf1bf94e686dba15d0e58c7'
   name 'Monal'
   homepage 'https://monal.im/'
 
