22593cfba3dbc967d158fb5c1c7989b9ece5f6c7
Chen <jiegec@qq.com>
2018-01-11 12:12:21 +0800

    Update next to 0.07 (#42698)

--- Casks/next.rb
+++ Casks/next.rb
@@ -1,10 +1,10 @@
 cask 'next' do
-  version '0.05'
-  sha256 '64b8fa32387ce1d467238bb20405b9620f08dd00df121c49f948ac64aca6d6aa'
+  version '0.07'
+  sha256 '88f92765fe9963b0800de73775cf9bf5fd8d793674f9da1755676e832d523d2a'
 
   url "https://github.com/nEXT-Browser/nEXT/releases/download/#{version}/nEXT.dmg"
   appcast 'https://github.com/nEXT-Browser/nEXT/releases.atom',
-          checkpoint: '5f62552a7609426aa1ed9c33b0cdf9dc4cdad7cfcff9ed0a6e6183dfba9a234a'
+          checkpoint: 'f5dec83b8e73c75011c335ec2e57be3a9b4c60f52246650657f68a89247db1e4'
   name 'nEXT-Browser'
   homepage 'https://github.com/nEXT-Browser/nEXT'
 
