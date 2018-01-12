78177ab0dcddf62cf556d1c18dc813915dfb4b10
Naruhito Kubota <n.kubota@opt.ne.jp>
2018-01-12 17:44:25 +0900

    Add ReactStudio v1.3.315

---
+++ Casks/react-studio.rb
@@ -0,0 +1,10 @@
+cask 'react-studio' do
+  version '1.3.315'
+  sha256 '356efa82b96a14133cf23cf3c6113ede482cf55143606d478b972598838b24e8'
+
+  url 'https://s3.amazonaws.com/sc.neonto.com/ReactStudio_v13_build315.zip'
+  name 'ReactStudio'
+  homepage 'https://reactstudio.com/'
+
+  app 'React Studio.app'
+end
