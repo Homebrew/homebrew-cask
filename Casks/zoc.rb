04423c986235f04050be456ad455628769ebfce9
commitay <commitay@users.noreply.github.com>
2018-01-13 06:55:32 +1000

    Update zoc to 7.17.2

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,10 +1,10 @@
 cask 'zoc' do
-  version '7.17.1'
-  sha256 '72a87390f25c78804edd756070606c793ab96ac47b19446cd6b21a45f56f2e12'
+  version '7.17.2'
+  sha256 '98427d92cbbf747b3e2e879c10c03a7126452244183b31a1b4bea8b6bc0145b9'
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast "https://www.emtec.com/downloads/zoc/zoc#{version.major}_previous/",
-          checkpoint: '554c31ae4d58710fe497a5968d5cffaf1d277d6f1c3e251a31fd987f273eb04d'
+          checkpoint: 'b07202bb41871a2d89cabc0f582a83c17ed18f449af8273dc0c0b3ac2d57f06e'
   name 'ZOC'
   homepage 'https://www.emtec.com/zoc/'
 
