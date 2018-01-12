9def2af33f1ce677f5438fb9d4f542b18375ecef
Naruhito Kubota <n.kubota@opt.ne.jp>
2018-01-12 19:01:36 +0900

    fix comment

--- Casks/react-studio.rb
+++ Casks/react-studio.rb
@@ -2,7 +2,7 @@ cask 'react-studio' do
   version '1.3.315'
   sha256 '356efa82b96a14133cf23cf3c6113ede482cf55143606d478b972598838b24e8'
 
-  # https://s3.amazonaws.com/sc.neonto.com/ was verified as official when first introduced to the cask
+  # s3.amazonaws.com/sc.neonto.com/ was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/sc.neonto.com/ReactStudio_v13_build315.zip'
   name 'ReactStudio'
   homepage 'https://reactstudio.com/'
