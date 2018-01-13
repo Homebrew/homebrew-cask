0ead91bab406ab2093e3bba4eaa2f76cd8aecb36
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2018-01-13 15:30:40 +0100

    Update plex-media-player to 2.2.1.758-5dad2d62

--- Casks/plex-media-player.rb
+++ Casks/plex-media-player.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-player' do
-  version '1.3.12.755-fed6185a'
-  sha256 'bdf620de2ea5cb5aa7fbf9e779955401f6bc0039ab637adb9c31f7b358dd39cd'
+  version '2.2.1.758-5dad2d62'
+  sha256 '97f0a6b9afa6b7be62d00de2ffc35d13290426e44383f85d6e40da5baa94c785'
 
   url "https://downloads.plex.tv/plexmediaplayer/#{version}/PlexMediaPlayer-#{version}-macosx-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/3.json',
