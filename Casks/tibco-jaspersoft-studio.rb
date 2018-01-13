f4aa104ac769e3360824884f0abc285f6ab87dc7
Michael <184316+muescha@users.noreply.github.com>
2018-01-13 09:31:03 +0100

    add final to app

--- Casks/tibco-jaspersoft-studio.rb
+++ Casks/tibco-jaspersoft-studio.rb
@@ -9,5 +9,5 @@ cask 'tibco-jaspersoft-studio' do
   name 'TIBCO Jaspersoft Studio'
   homepage 'https://community.jaspersoft.com/project/jaspersoft-studio'
 
-  app "TIBCO Jaspersoft Studio #{version}.app"
+  app "TIBCO Jaspersoft Studio #{version}.final.app"
 end
