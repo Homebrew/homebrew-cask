d8a63f61248bac1bc47a87084cb91ac65507a292
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2018-01-13 15:02:52 -0700

    Update cookie to 5.7.6 (#42761)

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.7.5'
-  sha256 '1116aa7b6ab8c39e6e99b77c510c579930cd472a64da28ca80a74c2abe10afea'
+  version '5.7.6'
+  sha256 '35c1f356e3e8a8268359e6d7ef0db40cde72b2375339f4d61dea8881179a7227'
 
   url "https://sweetpproductions.com/products/cookie#{version.major}/Cookie#{version.major}.dmg"
   appcast "https://sweetpproductions.com/products/cookie#{version.major}/appcast.xml",
-          checkpoint: '7f05c6231100cf3da70c5c7231f8cb9fcfe20a6cdc695320da4d0d4fccad0ef7'
+          checkpoint: '18e40ad747a7e7753a5cbb736c21803b8a83d9d778c2ea2ebb471a6a2e20a3d8'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
 
