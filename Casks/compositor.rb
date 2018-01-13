1b7b01f3637d9ae394cce8feede20b01270b6d51
Samuel Badr <samuel.badr@gmail.com>
2018-01-13 13:14:30 +0100

    Update compositor to 0.9.21

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,10 +1,10 @@
 cask 'compositor' do
-  version '0.9.20'
-  sha256 'bf4dae3d47f59c472089b4a65c634d8c213474b676af96143be57ec3332e16ab'
+  version '0.9.21'
+  sha256 '6818d22508a505ed1fd97a93f1b9d17a0476bb75accc1ac4457316a2267383f7'
 
   url 'http://compositorapp.com/downloads/Compositor.dmg'
   appcast 'http://compositorapp.com/updates/appcast.xml',
-          checkpoint: '83aa3e1735f0e06eb4f46ac9218fb2fa1c6ffaead4c898e6f7b68f5d57b06b8e'
+          checkpoint: 'de74490bed40366c789402d9125bb30d738d93526ac81b288f41c4ecaaed287e'
   name 'Compositor'
   homepage 'http://compositorapp.com/'
 
