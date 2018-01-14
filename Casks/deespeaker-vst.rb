73d490d7c7e9da36b17f0778ed642b5b6d948ef4
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-14 22:37:27 +0000

    Update deespeaker-vst to 1.0.5

--- Casks/deespeaker-vst.rb
+++ Casks/deespeaker-vst.rb
@@ -1,10 +1,12 @@
 cask 'deespeaker-vst' do
-  version :latest
-  sha256 :no_check
+  version '1.0.5'
+  sha256 'a90ee658d0f17032d8534734ff6150505d7d6391a3aacb982cd58051cbb4038e'
 
-  url 'https://dotec-audio.com/release/DeeSpeaker/latest/mac/DeeSpeakerMac.zip'
+  url "https://dotec-audio.com/release/DeeSpeaker/DeeSpeakerMacA_#{version}.zip"
   name 'DOTEC-AUDIO DeeSpeaker (VST)'
   homepage 'https://dotec-audio.com/deespeaker.html'
+
+  depends_on macos: '>= :lion'
 
   vst_plugin 'DeeSpeaker.vst'
 end
