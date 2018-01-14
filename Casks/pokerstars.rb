5e7fe457acc6bb5818cb6851e74722ae5af579fc
kiendang <trungkiendang@hotmail.com>
2018-01-14 22:48:38 +0800

    Update pokerstars to fix homepage

--- Casks/pokerstars.rb
+++ Casks/pokerstars.rb
@@ -3,14 +3,30 @@ cask 'pokerstars' do
   sha256 :no_check
 
   language 'US', default: true do
-    ['.net', '.net']
+    ['.net', '.net', '.net']
+  end
+
+  language 'DK' do
+    ['.dk', '.net', '.net']
+  end
+
+  language 'GR' do
+    ['.gr', '.net', '.net']
+  end
+
+  language 'IT' do
+    ['.it', '.net', '.net']
+  end
+
+  language 'UK' do
+    ['.uk', '.net', '.net']
   end
 
   language 'PT' do
-    ['.pt', 'PT']
+    ['.pt', 'PT', '.pt']
   end
 
-  url "http://www.pokerstars#{language[0]}/PokerStars#{language[1]}.app.zip"
+  url "http://www.pokerstars#{language[2]}/PokerStars#{language[1]}.app.zip"
   name 'PokerStars'
   homepage "http://www.pokerstars#{language[0]}"
 
