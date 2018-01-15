e193610f5a89342a9d70339e5da054d7184d16d0
commitay <commitay@users.noreply.github.com>
2018-01-15 17:47:08 +1000

    Update bearychat.rb

--- Casks/bearychat.rb
+++ Casks/bearychat.rb
@@ -1,6 +1,6 @@
 cask 'bearychat' do
-  version '20170330'
-  sha256 'c5f3d8be8c85285bf297a46ba0a479833373b39e2fa644e0689047d0816347a3'
+  version :latest
+  sha256 :no_check
 
   url 'https://static.bearychat.com/v2/BearyChat.dmg'
   name 'BearyChat'
