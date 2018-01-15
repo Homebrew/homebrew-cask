69990d6dedcf1eed1a7b87a99337fbc20bdba82a
commitay <commitay@users.noreply.github.com>
2018-01-15 18:27:54 +1000

    Update dashcam-viewer.rb

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -2,7 +2,7 @@ cask 'dashcam-viewer' do
   version '2.7.6'
   sha256 '974bc2a01db630126d301beb6e368957289e16e40a5b81bdd865d5b7306ab01a'
 
-  # s3.amazonaws.com/aws-website-dcv-downloads-c8kwd/dcv/Dashcam_Viewer_v was verified as official when first introduced to the cask
+  # s3.amazonaws.com/aws-website-dcv-downloads-c8kwd/dcv was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/aws-website-dcv-downloads-c8kwd/dcv/Dashcam_Viewer_v#{version}.dmg"
   name 'Dashcam Viewer'
   name 'Dashcam Viewer by Earthshine Software'
