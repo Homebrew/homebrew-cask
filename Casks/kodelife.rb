5a62b91172f71009fd1f15829a906056f4d37ce3
naotone <me@naotone.com>
2018-01-14 21:14:03 -0500

    Update kodelife to 0.5.2.0

--- Casks/kodelife.rb
+++ Casks/kodelife.rb
@@ -1,10 +1,10 @@
 cask 'kodelife' do
-  version '0.4.2.2'
-  sha256 '3c5240fb03d092767a1ce2364f46d763c0619416f85e5e9bc4a8b5fc995ad9b9'
+  version '0.5.2.0'
+  sha256 '2ea60e0da71bec3e6633a3235847221d3ff014ef31a93b846cc9dc143bf63fc6'
 
   url "http://hexler.net/pub/kodelife/kodelife-#{version}-osx.zip"
   appcast 'http://hexler.net/pub/kodelife/appcast.hex',
-          checkpoint: '69fc16feb2a20a22e68c075b15f35d2cb4e3e021266244e6ee31a888393ba6c2'
+          checkpoint: '9346742e456ba6e8de55b4aa9abea56fd8b4922a6a9b09b5945801c78c76bcb5'
   name 'KodeLife'
   homepage 'https://hexler.net/software/kodelife'
 
