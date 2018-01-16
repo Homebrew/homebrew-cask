93a6af388c6da9abf00b15978c2a870131a994e5
Vitaliy Levashov <Tpukep@live.com>
2018-01-16 13:12:42 +0300

    Update staruml to 2.8.1

--- Casks/staruml.rb
+++ Casks/staruml.rb
@@ -1,5 +1,5 @@
 cask 'staruml' do
-  version '2.8.0'
+  version '2.8.1'
   sha256 'cdeb05bdcfb83d8182c419638faa32cebef2d5c3c9c639b336bb2f12d7cd473d'
 
   url "http://staruml.io/download/release/v#{version}/StarUML-v#{version}.dmg"
