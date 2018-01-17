ccc7528b28a803133098e8601734c5ce9bb2f5a6
Tzin Mein Ho <tzinmein@kidotech.com>
2018-01-17 13:09:20 +0800

    Update skyfonts to 5.9.2.1

--- Casks/skyfonts.rb
+++ Casks/skyfonts.rb
@@ -1,10 +1,10 @@
 cask 'skyfonts' do
-  version '5.9.1.0'
-  sha256 '95282a85c259fac335bc5acbca4f3c7b9d94bfa0e3683eeee3d94eb665410bf6'
+  version '5.9.2.1'
+  sha256 '58eab505b64d025207d1b418ac3bbe23b68a413b81af74ad9d279964046a280d'
 
   url "http://cdn1.skyfonts.com/client/Monotype_SkyFonts_Mac64_#{version}.dmg"
   appcast 'https://www.fonts.com/other/skyfonts/getskyfontsclientdownloadinfo',
-          checkpoint: '17bbc003f08ec07517c947aa0d08d2bb48e0142982008beeeaf5344d9935d537'
+          checkpoint: '7b40834d20dbe6b093e403e15b0ebfc31f7596ca6183bdbb302400ebd0eb35cd'
   name 'SkyFonts'
   homepage 'https://skyfonts.com/'
 
