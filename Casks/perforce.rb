5ff3c086c77d46bd56fd2f69640289667fda6a06
Takahiro Fujita <takfjt@gmail.com>
2018-01-18 00:40:38 +0900

    Update perforce to 17.2

--- Casks/perforce.rb
+++ Casks/perforce.rb
@@ -1,8 +1,8 @@
 cask 'perforce' do
-  version '17.1'
-  sha256 :no_check # required as upstream package is updated in-place
+  version '17.2'
+  sha256 '1a1f87f665b787731d61df96e95156577d1ff60ee0cb145415216cde13b0cfdb'
 
-  url "https://cdist2.perforce.com/perforce/r#{version}/bin.darwin90x86/helix-versioning-engine.tgz"
+  url "https://cdist2.perforce.com/perforce/r#{version}/bin.darwin90x86_64/helix-versioning-engine.tgz"
   name 'Perforce Helix Versioning Engine'
   homepage 'https://www.perforce.com/'
 
