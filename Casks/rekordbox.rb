fd5a35a70c247304e9e4e4b3933a53261f280dff
Erik Minekus <tsunami@tsunami-productions.nl>
2018-01-18 11:59:28 +0100

    Update rekordbox to 5.1.0

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -1,6 +1,6 @@
 cask 'rekordbox' do
-  version '5.0.2'
-  sha256 'a2e5f1ce4483169332bb7d209fc5eba0c9b9b352fcc80c195a9fe8ce966bff4b'
+  version '5.1.0'
+  sha256 'dfb7f2e83c3505c262c3fd223904e3fea8b410ab24b022621a66183fb828e4af'
 
   url "https://rekordbox.com/_app/files/Install_rekordbox_#{version.dots_to_underscores}.pkg.zip"
   name 'rekordbox'
@@ -13,4 +13,9 @@ cask 'rekordbox' do
 
   uninstall pkgutil: "com.pioneer.rekordbox.#{version.major}.*",
             delete:  "/Applications/rekordbox #{version.major}"
+
+  zap trash: [
+               '~/Library/Application Support/Pioneer/rekordbox',
+               '~/Library/Pioneer/rekordbox',
+             ]
 end
