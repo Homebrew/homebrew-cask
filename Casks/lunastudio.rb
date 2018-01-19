435c50c3f94a16abec6914e25b4deae098c5d481
commitay <commitay@users.noreply.github.com>
2018-01-19 18:51:26 +1000

    Update and rename luna-studio.rb to lunastudio.rb

---
+++ Casks/lunastudio.rb
@@ -0,0 +1,12 @@
+cask 'lunastudio' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://packages.luna-lang.org/darwin/lunaInstaller.zip'
+  name 'Luna Studio'
+  homepage 'http://www.luna-lang.org/'
+
+  installer manual: 'lunaInstaller.app'
+
+  uninstall delete: '/Applications/LunaStudio.app'
+end
