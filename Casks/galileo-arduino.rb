dedc6bbe26ca5dc8fbea238cfa54ed6a10183798
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-19 18:40:27 +0000

    Update galileo-arduino to 1.8.5

--- Casks/galileo-arduino.rb
+++ Casks/galileo-arduino.rb
@@ -1,10 +1,12 @@
 cask 'galileo-arduino' do
-  version '1.6.0'
-  sha256 '529a8b71aa21fc1e8621d562f1e51fd9d5e36afb0610ccb6905ecef41d209d14'
+  version '1.8.5'
+  sha256 '49d0a255802167fc7644f4cc5b3d8b41c94859343c0c4d1f90816f18b11f3fd9'
 
-  url "https://downloadmirror.intel.com/24355/eng/IntelArduino-#{version}-OSX.zip"
+  url "https://downloads.arduino.cc/arduino-#{version}-macosx.zip"
   name 'Intel Galileo Arduino SW'
-  homepage 'https://downloadcenter.intel.com/download/26418?v=t'
+  homepage 'https://www.arduino.cc/'
+
+  depends_on macos: '>= :lion'
 
   app 'Arduino.app'
 end
