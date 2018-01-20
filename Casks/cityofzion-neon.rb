ed0ca3274df4c7efd1dcbfd8ad99c7638b031049
Fabian Maass <fabian.maass@gmail.com>
2018-01-20 13:06:00 +0100

    Update cityofzion-neon: add zap (#42920)
    * Add zap stanza for cityofzion-neon
    
    * Update cityofzion-neon to 0.1.1

--- Casks/cityofzion-neon.rb
+++ Casks/cityofzion-neon.rb
@@ -1,12 +1,19 @@
 cask 'cityofzion-neon' do
-  version '0.1.0'
-  sha256 '4017bcc1a9972501c97fbe8e99825faf255c1eee7a10a84cf891d04685e6d96a'
+  version '0.1.1'
+  sha256 '814200bfe0523a387f89a71b27716b4f023359ff61053d824d46ba234919e036'
 
   url "https://github.com/CityOfZion/neon-wallet/releases/download/#{version}/Neon-#{version}.Mac.dmg"
   appcast 'https://github.com/CityOfZion/neon-wallet/releases.atom',
-          checkpoint: '6d853ecc6479460a85515bc3e8803ab873e6dd530c1b9bc8866888f809b4c978'
+          checkpoint: '3fabfe5bd66c8b4a81930f6a37fd2f5cc436f1482786e93371ad8981e175ed9b'
   name 'Neon Wallet'
   homepage 'https://github.com/CityOfZion/neon-wallet'
 
   app 'Neon.app'
+
+  zap trash: [
+               '~/Library/Application Support/Neon',
+               '~/Library/Preferences/com.electron.neon.helper.plist',
+               '~/Library/Preferences/com.electron.neon.plist',
+               '~/Library/Saved Application State/com.electron.neon.savedState',
+             ]
 end
