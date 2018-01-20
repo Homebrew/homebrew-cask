7cd49eec6d8a0f68d802813ffd5830d26a4982b5
token <token@positrium.org>
2018-01-20 15:23:23 +0900

    Update nozbe to 3.6.3

--- Casks/nozbe.rb
+++ Casks/nozbe.rb
@@ -1,6 +1,6 @@
 cask 'nozbe' do
-  version '3.5.2'
-  sha256 '957a8b9d750dcfe07cf33d2d4d1a026b7c09c276688993e7d253179534f880e7'
+  version '3.6.3'
+  sha256 'ab15029f51d0bd92645b64e04ceb503f1797728a25a934d604e1189a697bc348'
 
   url "https://files.nozbe.com/#{version.no_dots}/Nozbe.app.zip"
   name 'Nozbe'
