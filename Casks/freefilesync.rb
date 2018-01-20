39eb9c5259786458f4f9e848f0053ac6a0b94bf8
Marius Schuller <marius.schuller@googlemail.com>
2018-01-20 01:43:32 +0100

    Update freefilesync to 9.7

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,6 +1,6 @@
 cask 'freefilesync' do
-  version '9.6'
-  sha256 '0b5459cb863bd6a3ee17b4f649826f0831ee80a2a9055be1b1750bfb340c2468'
+  version '9.7'
+  sha256 '8218d2cc58de4e214b8256be3d75652b58c894932c656c5b5e6dcf3297f1d75a'
 
   url "http://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake
