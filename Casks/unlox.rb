8aa21cc0439574e3fd0ca1487696830a71a4c5bc
Christopher Nethercott <ccnethercott@gmail.com>
2018-01-21 12:51:29 +0000

    Create unlox.rb

---
+++ Casks/unlox.rb
@@ -0,0 +1,10 @@
+cask 'unlox' do
+  version '1.0'
+  sha256 'b7e9252f8c46e498aedc09a34783d8242a08788ad935f3fcbd87f59e91800287'
+
+  url 'https://unlox.it/download/'
+  name 'Unlox'
+  homepage 'https://unlox.it/get'
+
+  app 'Unlox.app'
+end
