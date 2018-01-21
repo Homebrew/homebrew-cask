feb1bf9b8e0ba4b20f2542523adeccc2f5dd6733
leandro <leandro@leandro.org>
2018-01-21 20:05:43 +0100

    Therm: iTerm2 fork aims to have minimum features
    initial commit, version Therm 0.1.2

---
+++ Casks/therm.rb
@@ -0,0 +1,10 @@
+cask 'therm' do
+  version '0.1.2'
+  sha256 '4a1d43ef40f13d6cb6fadfa955489b7cd1f9976c4d332401565636ae543dbd1a'
+
+  url 'https://github.com/trufae/Therm/releases/download/0.1.2/Therm.app.zip'
+  name 'Therm'
+  homepage 'https://github.com/trufae/Therm'
+
+  app ''
+end
