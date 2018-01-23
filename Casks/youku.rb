2b6461dcb657ce0e6ee87c188dfa38c48e59d10e
Elf Sundae <elf.sundae@gmail.com>
2018-01-23 12:20:42 +0800

    Update youku to 1.3.5.01166

--- Casks/youku.rb
+++ Casks/youku.rb
@@ -1,6 +1,6 @@
 cask 'youku' do
-  version '1.3.1.12078'
-  sha256 '779d490b09ca392550c9bc357f630c9cff78d517f687a98ea8846dc661901893'
+  version '1.3.5.01166'
+  sha256 '5d9f2f546261e402468d0ba9b984c9060010cc0de1f6c94a549058eec6bcac1d'
 
   url "http://pcclient.download.youku.com/ikumac/youkumac_#{version}.dmg"
   name 'Youku'
