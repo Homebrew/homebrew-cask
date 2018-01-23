d3a0d6622d2f36045eb56565152dcfc0cc30727d
Christopher Nethercott <ccnethercott@gmail.com>
2018-01-23 09:41:17 +0000

    Add Sonos v8.2.2

---
+++ Casks/sonos.rb
@@ -0,0 +1,10 @@
+cask 'Sonos' do
+  version '8.2.2'
+  sha256 '5ef33957875795a2bf9b7933d35559e4fce62d9c5e2491a5623cdfe641c2d35a'
+
+  url 'https://www.sonos.com/redir/controller_software_mac'
+  name 'Sonos'
+  homepage 'https://www.sonos.com'
+
+  app 'Sonos.app'
+end
