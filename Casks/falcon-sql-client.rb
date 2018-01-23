0e77d22248d9b7ceeece161c4f7ddf6fdc14e772
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-23 08:29:38 +0000

    Update falcon-sql-client to 2.4.0 (#43015)

--- Casks/falcon-sql-client.rb
+++ Casks/falcon-sql-client.rb
@@ -1,11 +1,11 @@
 cask 'falcon-sql-client' do
-  version '2.3.2'
-  sha256 'e1f1d9fd7640e2441fbdb567e761854d82b08d6f03be38ddcfe1a18e412af1d2'
+  version '2.4.0'
+  sha256 'b1a4aca3015b482a2687103e02a48e444abb5536b0b5bd0ca3bcfc0c788c1c86'
 
   # github.com/plotly/falcon-sql-client was verified as official when first introduced to the cask
-  url "https://github.com/plotly/falcon-sql-client/releases/download/v#{version}/mac-falcon-sql-client.zip"
+  url "https://github.com/plotly/falcon-sql-client/releases/download/v#{version}/mac-falcon-v#{version}.zip"
   appcast 'https://github.com/plotly/falcon-sql-client/releases.atom',
-          checkpoint: '133de7d6300561f52d3bb599fd33b3bf0a6bf1988e462eafbdb188899a696945'
+          checkpoint: '50cab7fd7217005da62161e9b7339a5b7710cc0396db7d44d2d99d43b5f986d9'
   name 'Falcon SQL Client'
   homepage 'https://plot.ly/free-sql-client-download'
 
