4b0e8fd567a6e097e5dad8816f97388484411822
ericmarkmartin <ericmarkmartin@gmail.com>
2018-01-23 01:44:30 -0800

    Add Kindle Previewer 3.app v3.17.1

---
+++ Casks/kindle-previewer.rb
@@ -0,0 +1,13 @@
+cask 'kindle-previewer' do
+  version :latest
+  sha256 :no_check
+
+  # s3.amazonaws.com/kindlepreviewer3/KindlePreviewerInstaller.pkg was verified as official when first introduced to the cask
+  url 'https://s3.amazonaws.com/kindlepreviewer3/KindlePreviewerInstaller.pkg'
+  name 'Kindle Previewer 3'
+  homepage 'https://www.amazon.com/gp/feature.html'
+
+  pkg 'KindlePreviewerInstaller.pkg'
+
+  uninstall pkgutil: 'Amazon.Kindle.Previewer.pkg'
+end
