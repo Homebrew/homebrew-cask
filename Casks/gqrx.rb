65de83d9dced714c2936ee748daa15ffe02f04e8
Eric Martin <ericmark26@gmail.com>
2018-01-24 13:08:26 -0800

    Update gqrx: add binaries (#43042)
    * Link gqrx binaries in Gqrx.app Cask
    
    * Fix appcast checkpoint in Gqrx.app Cask
    
    * Update gqrx.rb
    
    * remove gqrx binary
    
    * Update gqrx.rb

--- Casks/gqrx.rb
+++ Casks/gqrx.rb
@@ -5,9 +5,36 @@ cask 'gqrx' do
   # github.com/csete/gqrx was verified as official when first introduced to the cask
   url "https://github.com/csete/gqrx/releases/download/v#{version.major_minor}/Gqrx-#{version}.dmg"
   appcast 'https://github.com/csete/gqrx/releases.atom',
-          checkpoint: 'ceec115a1c11b2d8c4600fb9448b8ff2de1939c53e4616180e55eb7d2c2000b5'
+          checkpoint: '3fccff79190926fb335ffd2f74bc2188a5cc56ba61781d4569c3a50c5d4f8bca'
   name 'Gqrx'
   homepage 'http://gqrx.dk/'
 
   app 'Gqrx.app'
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/airspy_info"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/airspy_rx"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/airspy_spiflash"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/hackrf_cpldjtag"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/hackrf_debug"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/hackrf_info"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/hackrf_spiflash"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/hackrf_transfer"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/rtl_adsb"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/rtl_eeprom"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/rtl_fm"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/rtl_power"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/rtl_sdr"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/rtl_tcp"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/rtl_test"
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/SoapySDRUtil", target: 'soapysdrutil'
+  binary "#{appdir}/Gqrx.app/Contents/MacOS/volk_profile"
+  # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/gqrx.wrapper.sh"
+  binary shimscript, target: 'gqrx'
+
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      '#{appdir}/Gqrx.app/Contents/MacOS/gqrx' "$@"
+    EOS
+  end
 end
