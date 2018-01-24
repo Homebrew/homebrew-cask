33f0ca2d309c9a8ff1f552611d2c1b5626021168
commitay <commitay@users.noreply.github.com>
2018-01-24 19:47:14 +1000

    Update pdfelement.rb

--- Casks/pdfelement.rb
+++ Casks/pdfelement.rb
@@ -1,5 +1,5 @@
 cask 'pdfelement' do
-  version '6.0.1.2654,2991'
+  version '6.3.6,2991'
   sha256 '36461546dcb7c853ecdac6257f356d4091134e79addd94bd383cb3e77283652b'
 
   url "http://download.wondershare.com/cbs_down/mac-pdfelement#{version.major}_full#{version.after_comma}.dmg"
