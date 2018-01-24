75391ba4ffc8c34cf0f35e5e3a7c1c9c0518480c
Sean Graham <github@sean-graham.com>
2018-01-24 17:27:30 -0500

    Added Farrago v1.0.0
    Rogue Amoeba's new Soundboard app

---
+++ Casks/farrago.rb
@@ -0,0 +1,10 @@
+cask 'farrago' do
+  version '1.0.0'
+  sha256 '74ced5e9e6961850e3e8da17f03dc237f4eb6f2b171d76ff5110c37a41c76558'
+
+  url 'https://rogueamoeba.com/farrago/download.php'
+  name 'Farrago'
+  homepage 'https://rogueamoeba.com/farrago/'
+
+  app 'Farrago.app'
+end
