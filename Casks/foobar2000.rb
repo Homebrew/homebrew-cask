9a2dbc1b79a4e5fcd4c3883ab79668dd14a6323f
commitay <commitay@users.noreply.github.com>
2018-01-24 19:22:18 +1000

    Update and rename foobar2000-beta.rb to foobar2000.rb

--- Casks/foobar2000.rb
+++ Casks/foobar2000.rb
@@ -1,8 +1,8 @@
-cask 'foobar2000-beta' do
+cask 'foobar2000' do
   version '2.1.7'
   sha256 '750ab9ebb817a7f4f4dd6b9d17990619037a9b6f829bca8964455ee2c09929c6'
 
-  url 'https://www.foobar2000.org/files/264a0d9bf25006ad99fca992b6740779/foobar2000-v2.1.7.dmg'
+  url "https://www.foobar2000.org/files/264a0d9bf25006ad99fca992b6740779/foobar2000-v#{version}.dmg"
   name 'foobar2000'
   homepage 'https://www.foobar2000.org/'
 
