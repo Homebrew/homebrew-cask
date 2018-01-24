015aca5a5ccb4fbb1bc380df0621ade91cb74ed2
Peter Dyhr <paadyhr@gmail.com>
2018-01-24 16:14:06 +0100

    Change to use GitHub instead of Bitbucket
    Change to use https://github.com/klembot/twinejs/ instead of https://bitbucket.org/klembot/twinejs as https://bitbucket.org/klembot/twinejs will be deleted permanently on February 1, 2018.

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -2,7 +2,7 @@ cask 'twine' do
   version '2.2.1'
   sha256 '0282c9e21167c51fb57d1b276447d6560fa22f5489e87aac215a3616edc5887f'
 
-  # github.com/klembot/twinejs/releases/download/ was verified as official when first introduced to the cask
+  # github.com/klembot/twinejs/ was verified as official when first introduced to the cask
   url "https://github.com/klembot/twinejs/releases/download/#{version}/twine_#{version}_osx.zip"
   name 'Twine'
   homepage 'https://twinery.org/'
