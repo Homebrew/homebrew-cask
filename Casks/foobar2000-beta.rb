ac8d8996f756c965accf502cad76084de98fce71
lcdtyph <lcdtyph@gmail.com>
2018-01-24 14:18:10 +0800

    fix style

--- Casks/foobar2000-beta.rb
+++ Casks/foobar2000-beta.rb
@@ -1,10 +1,10 @@
 cask 'foobar2000-beta' do
-    version '2.1.7'
-    sha256 '750ab9ebb817a7f4f4dd6b9d17990619037a9b6f829bca8964455ee2c09929c6'
+  version '2.1.7'
+  sha256 '750ab9ebb817a7f4f4dd6b9d17990619037a9b6f829bca8964455ee2c09929c6'
 
-    url 'https://www.foobar2000.org/files/264a0d9bf25006ad99fca992b6740779/foobar2000-v2.1.7.dmg'
-    name 'foobar2000'
-    homepage 'https://www.foobar2000.org'
+  url 'https://www.foobar2000.org/files/264a0d9bf25006ad99fca992b6740779/foobar2000-v2.1.7.dmg'
+  name 'foobar2000'
+  homepage 'https://www.foobar2000.org/'
 
-    app 'foobar2000.app'
+  app 'foobar2000.app'
 end
