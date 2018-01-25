9ba0af75c91a846b90fd921a333efaa3b5bfc638
commitay <commitay@users.noreply.github.com>
2018-01-25 15:04:09 +1000

    Update santa to 0.9.21

--- Casks/santa.rb
+++ Casks/santa.rb
@@ -1,10 +1,10 @@
 cask 'santa' do
-  version '0.9.20'
-  sha256 'dcfc535c97732380ce5a116f352110ca15ee7ebaef0eeb63b648148b21f5f3a4'
+  version '0.9.21'
+  sha256 '2cfe09a114c3e2ce12b6e54b4078a5aabe975c040bbf29734ec2f2f01ec10a56'
 
   url "https://github.com/google/santa/releases/download/#{version}/santa-#{version}.dmg"
   appcast 'https://github.com/google/santa/releases.atom',
-          checkpoint: 'fe503820c7dde7055f65eefcb2b09499031ecdb50e774d607ba451cb1003bf1e'
+          checkpoint: 'e1fa78a6cc0fb47fe008083d360ad24d8c7f765406f2b9247aab4b311f9e178e'
   name 'Santa'
   homepage 'https://github.com/google/santa'
 
