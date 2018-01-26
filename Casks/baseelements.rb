2859ac586d2e83ffc7ae8febea5ff3abe589aa04
Jan Zeleny <jan@zeleny.io>
2018-01-26 11:09:13 +0100

    Add BaseElements 16

---
+++ Casks/baseelements.rb
@@ -0,0 +1,11 @@
+cask 'baseelements' do
+  version '16'
+  sha256 '6f6141546ca51d871c2ead235ed1284b7426a40557a2506fe397d40a6330ff63'
+
+  # goya.com.au was verified as official when first introduced to the cask
+  url 'https://goya.com.au/files/becurrent/BaseElements_16_Mac_Runtime.dmg'
+  name 'BaseElements'
+  homepage 'http://www.baseelements.com/'
+
+  app 'BaseElements'
+end
