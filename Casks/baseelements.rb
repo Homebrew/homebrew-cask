eedbcfdd8ec7d52a50134b98b60401a98b6bdfea
commitay <commitay@users.noreply.github.com>
2018-01-26 20:43:07 +1000

    Update baseelements.rb

--- Casks/baseelements.rb
+++ Casks/baseelements.rb
@@ -1,11 +1,11 @@
 cask 'baseelements' do
   version '16'
-  sha256 '6f6141546ca51d871c2ead235ed1284b7426a40557a2506fe397d40a6330ff63'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  # goya.com.au was verified as official when first introduced to the cask
-  url 'https://goya.com.au/files/becurrent/BaseElements_16_Mac_Runtime.dmg'
+  # goya.com.au/files/becurrent was verified as official when first introduced to the cask
+  url "https://goya.com.au/files/becurrent/BaseElements_#{version}_Mac_Runtime.dmg"
   name 'BaseElements'
   homepage 'http://www.baseelements.com/'
 
-  app 'BaseElements'
+  suite 'BaseElements'
 end
