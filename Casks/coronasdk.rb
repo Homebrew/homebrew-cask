a6c6a4c0b0d249c4fb65c1dc1a3e6db7fe63cbd2
tsouhaieb <hv2320@email.vccs.edu>
2018-01-27 11:44:11 +0100

    Update coronasdk to 2017.3184 (#43228)

--- Casks/coronasdk.rb
+++ Casks/coronasdk.rb
@@ -1,6 +1,6 @@
 cask 'coronasdk' do
-  version '2017.3135'
-  sha256 '86654603f890edf2f27d54acdb97fac55e352b263c6950886860f27a3114ff22'
+  version '2017.3184'
+  sha256 '54fe7dfd2e9e67a948e08fc4f50c77136d22b4bdeb23f48b13ecb123da1098ae'
 
   url "https://developer.coronalabs.com/sites/default/files/Corona-#{version}.dmg"
   name 'Corona SDK'
