7bc9f7ff7c16b05bb4967d9f32cf18c8296826be
Kazuyuki SUZUKI <kechol@users.noreply.github.com>
2018-01-27 13:47:33 +0900

    Update jasper to 0.5.0

--- Casks/jasper.rb
+++ Casks/jasper.rb
@@ -1,11 +1,11 @@
 cask 'jasper' do
-  version '0.4.0'
-  sha256 '78177f7609977ea0aa8818e0cda26270b4e338c6a4b46411866d2fdc38c285d2'
+  version '0.5.0'
+  sha256 '962a38ad3a928894289643fd47febfdeb585c2e712c73dba23487edb940b25cd'
 
   # github.com/jasperapp/jasper was verified as official when first introduced to the cask
   url "https://github.com/jasperapp/jasper/releases/download/v#{version}/jasper_v#{version}_mac.zip"
   appcast 'https://jasperapp.io/-/versions-mac.json',
-          checkpoint: '02eaa2100598bb5e5ad11c23fb100a41cd65fb46ab35447db49fb8275d307fed'
+          checkpoint: '56d1635b6477649e9336bffc59dd307a3748ed9d4c2326792cc32a12c9d7d790'
   name 'Jasper'
   homepage 'https://jasperapp.io/'
 
