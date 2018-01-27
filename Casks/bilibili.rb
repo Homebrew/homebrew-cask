042e156f7d3468068e0dc5d4b2a8f9aa31d461ea
commitay <commitay@users.noreply.github.com>
2018-01-27 19:28:05 +1000

    Update bilibili.rb

--- Casks/bilibili.rb
+++ Casks/bilibili.rb
@@ -13,8 +13,8 @@ cask 'bilibili' do
   app 'Bilibili.app'
   
   zap trash: [
-               '~/Library/Application\ Support/com.typcn.bilibili',
-               '~/Library/Application\ Support/com.crashlytics/com.typcn.bilibili',
+               '~/Library/Application Support/com.typcn.bilibili',
+               '~/Library/Application Support/com.crashlytics/com.typcn.bilibili',
                '~/Library/WebKit/com.typcn.bilibili',
              ]
 end
