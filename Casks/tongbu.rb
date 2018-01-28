a8639162c99749411fbb4a3ba553dcbfaab81c10
Jasper Zhao <Jasper_Zhao@Homedepot.com>
2018-01-28 18:49:38 -0500

    Upgraded tongbu.app from v1.1.7 to v1.2.7.0

--- Casks/tongbu.rb
+++ Casks/tongbu.rb
@@ -1,11 +1,11 @@
 cask 'tongbu' do
-  version '1.1.7'
-  sha256 'e9ff364db18915318367bc8ba359f963ce9c274bb9e27be10f2ce87192ac8a68'
+  version '1.2.7.0,_1'
+  sha256 '5f655b0bf7097dd53a56129dceb5205a1ac4af717fd8e7b8d78328d366baca8a'
 
-  # qd.appdown.info/qd/zsmacqd was verified as official when first introduced to the cask
-  url "http://qd.appdown.info/qd/zsmacqd/Tongbu_mac_v#{version}_baidupinzhuan.dmg"
+  # url was verified as official on Jan.28, 2018
+  url "http://qd.tongbu.com/qd/tbzsqd/zsmac_#{version.before_comma}/Tongbu_mac_v#{version.before_comma}#{version.after_comma}.dmg"
   name 'Tongbu'
-  homepage 'http://www.tongbu.com/'
+  homepage 'http://www.tongbu.com/mac'
 
   app 'Tongbu.app'
 end
