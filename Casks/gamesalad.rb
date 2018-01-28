dc08319b40cfe073e7dacfce31fc500005d1cfd6
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-28 17:12:01 +0000

    Update gamesalad to 1.25.85

--- Casks/gamesalad.rb
+++ Casks/gamesalad.rb
@@ -1,10 +1,10 @@
 cask 'gamesalad' do
-  version '1.25.80'
-  sha256 'd0b40aaf79328b0d06dc42997e3b108de84c068e8e0e9efbe176e57b66105ebe'
+  version '1.25.85'
+  sha256 'd356da298c456160a8caee224e042b48c128d089d2b7aa528dd0fd7a94bd5e99'
 
   url "http://releases.gamesalad.com/creator/#{version}/GameSalad-Creator-#{version}.dmg"
   appcast 'https://gamesalad.com/download/studioUpdates',
-          checkpoint: '82f4d5166317d2e02c2b883d2e3c358a436d8bcf70f0d255c04338612cb0720c'
+          checkpoint: 'ffdb065bbedb2011e1616a48ac6f261e4279e02f9c7505ac2db8376b30b3f7f1'
   name 'GameSalad'
   homepage 'https://gamesalad.com/'
 
