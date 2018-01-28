ed28fd270f2896fc40915db1db78fd7ca8b3b4ea
nrlquaker <ustynov.vy@gmail.com>
2018-01-28 11:26:47 +0200

    Add referer

--- Casks/bdinfo.rb
+++ Casks/bdinfo.rb
@@ -2,7 +2,7 @@ cask 'bdinfo' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.videohelp.com/download/BDInfo%20OSX.dmg'
+  url 'https://www.videohelp.com/download/BDInfo%20OSX.dmg', referer: 'https://www.videohelp.com/software/BDInfo'
   name 'BDInfo'
   homepage 'https://www.videohelp.com/software/BDInfo'
 
