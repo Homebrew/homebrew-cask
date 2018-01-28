aa3fc09bc95c5ebf957ce449aa4b6ef34acc41bf
nrlquaker <ustynov.vy@gmail.com>
2018-01-28 11:03:18 +0200

    Update url

--- Casks/bdinfo.rb
+++ Casks/bdinfo.rb
@@ -2,7 +2,7 @@ cask 'bdinfo' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.videohelp.com/download/BDInfo%20OSX.dmg?r=gPfZKqBhTxb'
+  url 'https://www.videohelp.com/download/BDInfo%20OSX.dmg'
   name 'BDInfo'
   homepage 'https://www.videohelp.com/software/BDInfo'
 
