424f807dbb788252a4e50165e0375eda9b1514dc
nrlquaker <ustynov.vy@gmail.com>
2018-01-28 11:31:19 +0200

    Fix style

--- Casks/bdinfo.rb
+++ Casks/bdinfo.rb
@@ -2,7 +2,8 @@ cask 'bdinfo' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.videohelp.com/download/BDInfo%20OSX.dmg', referer: 'https://www.videohelp.com/software/BDInfo'
+  url 'https://www.videohelp.com/download/BDInfo%20OSX.dmg',
+      referer: 'https://www.videohelp.com/software/BDInfo'
   name 'BDInfo'
   homepage 'https://www.videohelp.com/software/BDInfo'
 
