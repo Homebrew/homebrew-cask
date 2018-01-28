38c5e9f71336b5466fa52764c4b1ebfac4c170c3
Sihan Zhu <zshbleaker@icloud.com>
2018-01-28 23:50:19 +0800

    Add Unlox for macOS.

---
+++ Casks/unlox.rb
@@ -0,0 +1,10 @@
+cask 'unlox' do
+  version '2.0.1'
+  sha256 'db14600045c310d5f9a08e70c46fe4d95967eece36c3b872dce31000ba9795ae'
+
+  url 'https://unlox.it/download/'
+  name 'Unlox'
+  homepage 'https://unlox.it'
+
+  app 'Unlox.app'
+end
