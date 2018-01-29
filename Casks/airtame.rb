ade334c6b078f2e936aa8a2b8bc878c966f52bd7
Brian Morton <brianmortondesign@gmail.com>
2018-01-29 13:31:36 -0800

    Update airtame to 3.1.1 (#43318)

--- Casks/airtame.rb
+++ Casks/airtame.rb
@@ -1,6 +1,6 @@
 cask 'airtame' do
-  version '3.0.1'
-  sha256 '43a0211ec92341d33c73efb0d5c5f3b6e4afb4ababfdb7d5ccbde5bcd1146bf7'
+  version '3.1.1'
+  sha256 '4947ae4186d62d1c0587e536ddecf078de187c518edf7f744e1b418939847de5'
 
   url "https://downloads-cdn.airtame.com/application/ga/osx_x64/releases/airtame-application-#{version}.dmg"
   name 'Airtame'
