16519b0469e0f44873f439cf60801a9bb8f2a453
Vítor Galvão <info@vitorgalvao.com>
2018-01-29 19:34:00 +0000

    Update beaker-browser.rb

--- Casks/beaker-browser.rb
+++ Casks/beaker-browser.rb
@@ -3,7 +3,7 @@ cask 'beaker-browser' do
   sha256 '78f988186e3668aa1e6e4459f3e5f3c045ef9fd291e0a790f9c595eb73874ee9'
 
   # github.com/beakerbrowser/beaker was verified as official when first introduced to the cask
-  url 'https://github.com/beakerbrowser/beaker/releases/download/0.7.11/beaker-browser-0.7.11.dmg'
+  url "https://github.com/beakerbrowser/beaker/releases/download/#{version}/beaker-browser-#{version}.dmg"
   appcast 'https://github.com/beakerbrowser/beaker/releases.atom',
           checkpoint: '81c94a620c59bd5ea85e1d562381d7f709d55edd33356947024015c7033d98a3'
   name 'Beaker Browser'
