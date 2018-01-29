328fa523209cfd1ac7540db4b1c52751062000c8
Dieter Blomme <dieterblomme@gmail.com>
2018-01-29 10:48:22 +0100

    Update MAMP to 4.3 (#43273)
    * Update mamp to 4.3
    
    * Change mamp 4.3 hash to the one I actually get.

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -3,8 +3,8 @@ cask 'mamp' do
     version '3.5.2'
     sha256 'b935d118b6e14d43cf2570be2a05166a15b824c3edc0ede98cc30e6ab5af3697'
   else
-    version '4.2.1'
-    sha256 '9428a3cf494d02d94ecaec302042822f54273ba9d412ecde25d1155ab07db56e'
+    version '4.3'
+    sha256 '2fb215c6ae5718796f53b4380c2807a7f78836cf6aac9d08aa81e6bac5921203'
   end
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
