6e62029fca21120865cc6d6d7afce8bafc73f9a9
ryounsumiko <ryounasumiko@gmail.com>
2018-01-29 18:14:09 -0500

    Update poi to 8.1.0 (#43330)

--- Casks/poi.rb
+++ Casks/poi.rb
@@ -1,11 +1,11 @@
 cask 'poi' do
-  version '8.0.0'
-  sha256 'e00353806047e341f99e1c6ca8f6fb1ff67ae6ae6be87edfb8e3a1e391c4e492'
+  version '8.1.0'
+  sha256 '4e0ad7d51f0c2c8fd0698f89a7f6263eeeed39579ec7729fd1ef01ba21ba90c5'
 
   # github.com/poooi/poi was verified as official when first introduced to the cask
   url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}.dmg"
   appcast 'https://github.com/poooi/poi/releases.atom',
-          checkpoint: 'e52744adc8162dad5fd33281851589989f76ee2a11236fcd2874883b3dbc2def'
+          checkpoint: 'e9279f9d31cf64b44e5cf2c1b57f4a8807b5bc9cbd9d33c75c51e9279d52bdf2'
   name 'poi'
   homepage 'https://poi.io/'
 
