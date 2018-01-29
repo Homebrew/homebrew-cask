9a9b030e18d69a8cb0df7b82ae8f0e4f86a1dea1
jalendport <jalen@dominion-designs.com>
2018-01-29 17:00:04 -0500

    Add F-Bar.app v2.0.2

---
+++ Casks/f-bar.rb
@@ -0,0 +1,11 @@
+cask 'f-bar' do
+  version '2.0.2'
+  sha256 '88f337c5558ea89c0486b6a86fd4be991bce20b99383c9119d5c187f55e9eeb7'
+
+  # eastwest.se was verified as official when first introduced to the cask
+  url 'https://apps.eastwest.se/fbar/updates/F-Bar_2.0.2.zip'
+  name 'F-Bar'
+  homepage 'https://laravel-forge-menubar.com/'
+
+  app 'F-Bar.app'
+end
