ab2e9ce47aea5926e38fe6a3e3eee2aca270b678
commitay <commitay@users.noreply.github.com>
2018-01-29 10:01:44 +1000

    Update tongbu.rb

--- Casks/tongbu.rb
+++ Casks/tongbu.rb
@@ -1,9 +1,8 @@
 cask 'tongbu' do
-  version '1.2.7.0,_1'
+  version '1.2.7.0_1'
   sha256 '5f655b0bf7097dd53a56129dceb5205a1ac4af717fd8e7b8d78328d366baca8a'
 
-  # url was verified as official on Jan.28, 2018
-  url "http://qd.tongbu.com/qd/tbzsqd/zsmac_#{version.before_comma}/Tongbu_mac_v#{version.before_comma}#{version.after_comma}.dmg"
+  url "http://qd.tongbu.com/qd/tbzsqd/zsmac_#{version.split('_')[0]}/Tongbu_mac_v#{version}.dmg"
   name 'Tongbu'
   homepage 'http://www.tongbu.com/mac'
 
