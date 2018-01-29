9300553ddcf4d1b211b364ae111fa9ed543e859a
Jasper Zhao <Jasper_Zhao@Homedepot.com>
2018-01-28 23:53:11 -0500

    Add Captin v1.0.11

---
+++ Casks/captin.rb
@@ -0,0 +1,11 @@
+cask 'captin' do
+  version '1.0.11'
+  sha256 'c09431aef7ceb8b4c2eb0c758cd47af27673a29e302d5f143be583046b5ea938'
+
+  # dl.devmate.com/com.100hps.captin/ was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.100hps.captin/Captin.dmg'
+  name 'Captin'
+  homepage 'http://captin.strikingly.com/'
+
+  app 'Captin.app'
+end
