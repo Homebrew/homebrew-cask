04d8dae190f294647bdadd22648670e4e8574023
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-29 18:10:57 +0000

    Update gpower to 3.1.9.3

--- Casks/gpower.rb
+++ Casks/gpower.rb
@@ -1,10 +1,10 @@
 cask 'gpower' do
   version '3.1.9.3'
-  sha256 'f851661a55993cacf4f255ebba79c185bd1186667345fe3b2d8aefd0436a5321'
+  sha256 '45161c9ee8736bd46b25650183d63c183f34a1f7acb04f801fdc0b0da5c2711e'
 
   url "http://www.gpower.hhu.de/fileadmin/redaktion/Fakultaeten/Mathematisch-Naturwissenschaftliche_Fakultaet/Psychologie/AAP/gpower/GPowerMac_#{version}.zip"
   appcast 'http://www.gpower.hhu.de/',
-          checkpoint: 'e3b34cca13aa049cf00a93d9341df77d8bcf8e2bd2076cdae9509411d4561502'
+          checkpoint: '68380e1857969b2f0d6f213501b07bdf754486344a3274eb5cb170f7b9bae2d4'
   name 'G*Power'
   homepage 'http://www.gpower.hhu.de/'
 
