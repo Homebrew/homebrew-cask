d0a35f124d3041fbe4dcd2f1d67c84597fb81670
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-29 19:31:56 +0000

    Update grandtotal to 5.0.3 (#43301)

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,10 +1,10 @@
 cask 'grandtotal' do
-  version '4.4.1'
-  sha256 'cf6170abd31413caa3de6eda6393abd576451c54a8eefbcce9df29f600ff4df0'
+  version '5.0.3'
+  sha256 '0afba805ad4137850886f9d37c865296811f2d36f856a16bacd70f2bc66b3401'
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   appcast "https://mediaatelier.com/GrandTotal#{version.major}/feed.php",
-          checkpoint: '974060b77e320be579beebe3b9de7b42d0ede2d01c653a5e69ea44eb30eb81a2'
+          checkpoint: '384b98383047955a26b608e0c579f3e092c13287c63fef015a5ed3abadf8a371'
   name 'GrandTotal'
   homepage 'https://www.mediaatelier.com/GrandTotal4/'
 
