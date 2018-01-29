ef6984f7e3ff0b347d8372144a903917d6f850bf
ohnosushi <ohnosushi@users.noreply.github.com>
2018-01-29 19:22:23 +0100

    Add Pdf Squeezer latest

---
+++ Casks/pdf-squeezer.rb
@@ -0,0 +1,10 @@
+cask 'pdf-squeezer' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://witt-software.com/downloads/pdfsqueezer/PDF%20Squeezer.dmg'
+  name 'Pdf Squeezer'
+  homepage 'https://witt-software.com/pdfsqueezer/'
+
+  app 'PDF Squeezer.app'
+end
