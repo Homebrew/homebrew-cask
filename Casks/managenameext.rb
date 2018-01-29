80cf6a72e861c2fbac2aa7b0c172ac5b4b08de75
commitay <commitay@users.noreply.github.com>
2018-01-30 06:57:21 +1000

    Update managenameext.rb

--- Casks/managenameext.rb
+++ Casks/managenameext.rb
@@ -3,7 +3,7 @@ cask 'managenameext' do
   sha256 '57f34571293b525ea7d9c0068f784c9684ac26f8da54f93b2fd3dd0f2c1d2aa1'
 
   url 'http://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_c.zip'
-  appcast 'throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_AffV.html',
+  appcast 'http://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_AffV.html',
           checkpoint: '48c241c749d43463b4f829520a97a22019cdbbef9389292636c262d3aab6ea2b'
   name 'ManageNameExt'
   homepage 'http://throb.pagesperso-orange.fr/site/ind_JS.html?Prg_S.html&Prg_ApplisRB.html#ManageNameExt'
