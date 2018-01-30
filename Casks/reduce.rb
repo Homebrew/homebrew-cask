b463605194d7c6d4929a195970cd1bfb28f38742
Vítor Galvão <info@vitorgalvao.com>
2018-01-30 14:19:31 +0000

    Update reduce.rb

--- Casks/reduce.rb
+++ Casks/reduce.rb
@@ -2,7 +2,7 @@ cask 'reduce' do
   version :latest
   sha256 :no_check
 
-  # dl.devmate.com was verified as official when first introduced to the cask
+  # dl.devmate.com/com.flawless.utils.Reduce was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.flawless.utils.Reduce/ReduceApp.zip'
   name 'Reduce'
   homepage 'https://flawlessapp.io/reduce'
