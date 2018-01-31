0791ec501711caa15b7c95de3ce588ea4269ba38
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2018-01-31 16:01:15 +0300

    Update laravel-kit.rb

--- Casks/laravel-kit.rb
+++ Casks/laravel-kit.rb
@@ -1,8 +1,9 @@
 cask 'laravel-kit' do
   version '1.1.0'
-  sha256 :no_check
+  sha256 '58e12c1934d1e9d2eb474dab13ba40c282fa3f1e8e20251c5c488e7df1adc56b'
 
   url "https://github.com/tarequemdhanif/laravel-kit/releases/download/v#{version}/laravel-kit-#{version}-mac.zip"
+  appcast 'https://github.com/tarequemdhanif/laravel-kit/releases.atom'
   name 'Laravel Kit'
   homepage 'https://github.com/tarequemdhanif/laravel-kit'
 
