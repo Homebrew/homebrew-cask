f97c3104201c54b8f222443519fac3715fcefd3a
Brian Morton <brianmortondesign@gmail.com>
2018-01-30 20:20:08 -0800

    Update bonitastudiocommunity to 7.6.1 (#43365)
    * Update bonitastudiocommunity to 7.6.1
    
    * Update bonitastudiocommunity.rb

--- Casks/bonitastudiocommunity.rb
+++ Casks/bonitastudiocommunity.rb
@@ -1,11 +1,11 @@
 cask 'bonitastudiocommunity' do
-  version '7.6.0'
-  sha256 'bb5a12a233ca7f0ede813070fedf34ecd3343c16dfae0f255f4707da130dba32'
+  version '7.6.1'
+  sha256 'a43bf73996bb2dc724dd386e69fbc015fde24c9428514f19fea25ae8ba7f07eb'
 
   # download.forge.ow2.org was verified as official when first introduced to the cask
   url "https://download.forge.ow2.org/bonita/BonitaStudioCommunity-#{version}-x86_64.dmg"
   appcast 'https://documentation.bonitasoft.com///?page=release-notes',
-          checkpoint: 'a66974da63b8e9383a454678b31a65a9008ce0ca0525949ce26cc9a1e884d075'
+          checkpoint: '0e9f27a8d871eabfb32def4978aecf0fa284f32fec85951bc19600b82d0e688d'
   name 'Bonita Studio Community Edition'
   homepage 'https://www.bonitasoft.com/downloads'
 
@@ -15,7 +15,7 @@ cask 'bonitastudiocommunity' do
                     }
 
   uninstall quit:   'org.bonitasoft.studio.product',
-            delete: '/Applications/BonitaStudioCommunity.app'
+            delete: "/Applications/BonitaStudioCommunity-#{version}.app"
 
   zap trash: [
                '~/Library/Preferences/org.bonitasoft.studio.product.plist',
