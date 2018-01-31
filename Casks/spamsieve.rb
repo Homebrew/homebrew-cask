e1a3ef87fc8e0ed0ac27265c7c47aa65e25f1b98
Emrah Er <emraher@users.noreply.github.com>
2018-01-31 10:18:59 +0300

    Update spamsieve to 2.9.30 (#43394)

--- Casks/spamsieve.rb
+++ Casks/spamsieve.rb
@@ -1,6 +1,6 @@
 cask 'spamsieve' do
-  version '2.9.29'
-  sha256 '750fd469d1c5049d461c03fbab0fc91486eb0bb08c46f96248d3efe95e53e4de'
+  version '2.9.30'
+  sha256 '6f33671a430ca556948b224f5ac84d75e4b7ba9722930433b857748b9b57209a'
 
   url "https://c-command.com/downloads/SpamSieve-#{version}.dmg"
   name 'SpamSieve'
