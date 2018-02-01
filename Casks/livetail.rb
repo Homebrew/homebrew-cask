bbe260a95e9724bdd432b1acdca7565dacb5b0f8
Rui Coelho <rjcoelho@users.noreply.github.com>
2018-02-01 10:19:51 +0000

    Update livetail to 2.0 (#43434)
    * Update livetail to 2.0
    
    * Update livetail.rb

--- Casks/livetail.rb
+++ Casks/livetail.rb
@@ -1,10 +1,10 @@
 cask 'livetail' do
-  version '1.0'
-  sha256 '40cc322b1f31ab9189798d4bb193d1eb43d00c95906bdb863dadefc72f4a839c'
+  version '2.0'
+  sha256 '960977b45cb758e629e3d9e352a57caef655ce1a312890dcceaa2754c24056a0'
 
-  url "https://github.com/SumoLogic/livetail-cli/releases/download/v#{version}/livetail_mac_os.zip"
+  url "https://github.com/SumoLogic/livetail-cli/releases/download/#{version}/livetail_mac_os.zip"
   appcast 'https://github.com/SumoLogic/livetail-cli/releases.atom',
-          checkpoint: 'f800c5aa6f95542b178d7f91aa5c9285fdf8dac1f48ed11b215f14f2136d2cda'
+          checkpoint: '784e51f1fdbbaf2d732299f9b8cddc5f2242daa76d58814e26dc9162fc779c4d'
   name 'Sumologic livetail CLI'
   homepage 'https://github.com/SumoLogic/livetail-cli'
 
