05e98f74780622b07ff775e868d65f37263edddb
Brian Morton <brianmortondesign@gmail.com>
2018-02-01 11:56:16 -0800

    Update joplin to 0.10.54

--- Casks/joplin.rb
+++ Casks/joplin.rb
@@ -1,11 +1,11 @@
 cask 'joplin' do
-  version '0.10.47'
-  sha256 '10674f455820e7e2108ce1f640d5991ed7ceccd1f1bd4e121bcfa07d04d521e0'
+  version '0.10.54'
+  sha256 '92e6a0274664c96f5306eb8bd37caffdaf79978a9259ad1bb2440ef0423a25fd'
 
   # github.com/laurent22/joplin was verified as official when first introduced to the cask
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}.dmg"
   appcast 'https://github.com/laurent22/joplin/releases.atom',
-          checkpoint: '16321f04f41fdf64d8c9ff6d050eb5b5d691ecb208a6af34c931cf10a58f0b92'
+          checkpoint: '50bd7c5fe57a0f0f6371d245a76c436b3e2df18bc3cd8b8420dca7ee50d7149b'
   name 'Joplin'
   homepage 'http://joplin.cozic.net/'
 
