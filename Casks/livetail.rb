0362f84c6aab60e02e80387b9e22e56c7b3d4a4b
commitay <commitay@users.noreply.github.com>
2018-02-01 20:16:29 +1000

    Update livetail.rb

--- Casks/livetail.rb
+++ Casks/livetail.rb
@@ -4,7 +4,7 @@ cask 'livetail' do
 
   url "https://github.com/SumoLogic/livetail-cli/releases/download/#{version}/livetail_mac_os.zip"
   appcast 'https://github.com/SumoLogic/livetail-cli/releases.atom',
-          checkpoint: 'f800c5aa6f95542b178d7f91aa5c9285fdf8dac1f48ed11b215f14f2136d2cda'
+          checkpoint: '784e51f1fdbbaf2d732299f9b8cddc5f2242daa76d58814e26dc9162fc779c4d'
   name 'Sumologic livetail CLI'
   homepage 'https://github.com/SumoLogic/livetail-cli'
 
