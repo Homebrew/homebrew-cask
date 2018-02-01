45541a85686d2cf6b22ff44be8eeb580fe4ace4d
Anver Housseini <contact@anver.ch>
2018-02-01 08:48:07 +0100

    Update eclipse-ptp to 4.7.2,oxygen:2

--- Casks/eclipse-ptp.rb
+++ Casks/eclipse-ptp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-ptp' do
-  version '4.7.1,oxygen:1a'
-  sha256 '6fd11c3758b82d32c0c28d19635cc024d37c5b29dcabc3e23a24b210ac07942c'
+  version '4.7.2,oxygen:2'
+  sha256 '400e176bfe0e7463c6c17af1d2eb852ab739a57c9b41f71755009b1e844bcbfe'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-parallel-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Parallel Application Developers'
