649a44ccaf629d317670aedfc025eec01943c079
Brian Morton <brianmortondesign@gmail.com>
2018-02-01 15:12:47 -0800

    Update qbserve to 1.73

--- Casks/qbserve.rb
+++ Casks/qbserve.rb
@@ -1,10 +1,10 @@
 cask 'qbserve' do
-  version '1.72'
-  sha256 'c1c179251ff13b462dd4699b9f6e9bfff0d30030462b714351733fcdd5275a8b'
+  version '1.73'
+  sha256 'bf804de6403e8803ae8e75a3cd3529a663920e609c5336980c3253d59be7eaf8'
 
   url "https://qotoqot.com/qbserve/app/Qbserve-#{version}.zip"
   appcast 'https://qotoqot.com/qbserve/app/appcast.xml',
-          checkpoint: 'ad7937a3ce7d5be2d2ac7b32f394fe15c0d42ae5883633b3991b45cf88017c6b'
+          checkpoint: 'afafa62d4e4bc9f1c2cf26b1f574b52002c964cd1399bf0db0c99b434bd6deca'
   name 'Qbserve'
   homepage 'https://qotoqot.com/qbserve/'
 
