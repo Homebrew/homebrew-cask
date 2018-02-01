7f967d7739324be1fd3e7adb10cf0e19a31c5e0c
Brian Morton <brianmortondesign@gmail.com>
2018-02-01 12:25:19 -0800

    Update noejectdelay to 7.1.4

--- Casks/noejectdelay.rb
+++ Casks/noejectdelay.rb
@@ -1,10 +1,10 @@
 cask 'noejectdelay' do
-  version '7.1.0'
-  sha256 'cfaeed50aa7ed9eac04716ab0e34c5ef10658802e691732f7589f9454e96aa94'
+  version '7.1.4'
+  sha256 '6a263c59efaaffb200db7d9ee6fe817f414e148053e610ccdcf37186fed27cc0'
 
   url "https://pqrs.org/osx/karabiner/files/NoEjectDelay-#{version}.dmg"
   appcast 'https://github.com/tekezo/NoEjectDelay/releases.atom',
-          checkpoint: '4475b83ad5927875fbc16e19c890dbaba1e466a758afcc0351ec86bb0e00468d'
+          checkpoint: '8cf6b036077bd9db8bd105ec548f60866403c2986e760dcc6d0c8b5b99916710'
   name 'NoEjectDelay'
   homepage 'https://pqrs.org/osx/karabiner/noejectdelay.html.en'
 
