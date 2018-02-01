9c95a8ed75e718c5e4e7713bbe628377cc28a755
Mikael Kjaer <mkjaer@users.noreply.github.com>
2018-02-01 20:10:36 +0400

    Update rocket-chat to 2.10.2 (#43437)

--- Casks/rocket-chat.rb
+++ Casks/rocket-chat.rb
@@ -1,11 +1,11 @@
 cask 'rocket-chat' do
-  version '2.10.1'
-  sha256 '66a090a87922438953a073dd87bae3780315945e2a29552b5ed92546c83a6581'
+  version '2.10.2'
+  sha256 '063c97e81a7b1e6946c51df72200d61285c4d476c90ccc413f0f260bfd91c6b8'
 
   # github.com/RocketChat/Rocket.Chat.Electron was verified as official when first introduced to the cask
   url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}.dmg"
   appcast 'https://github.com/RocketChat/Rocket.Chat.Electron/releases.atom',
-          checkpoint: '55489df75770a1c71d2a230462c195df63d5487b582f7615b4c8d64eaf4109e3'
+          checkpoint: '8d80b30869ac3da7a1266381624954bd877e830a87f926ff8e9d97a98ba3fc81'
   name 'Rocket.Chat'
   homepage 'https://rocket.chat/'
 
