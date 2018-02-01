1e754ab12bf0bbd6bcb6fd3851e94a46ae6ad9e6
Brian Morton <brianmortondesign@gmail.com>
2018-02-01 12:20:49 -0800

    Update murus to 1.4.15

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -1,6 +1,6 @@
 cask 'murus' do
-  version '1.4.11'
-  sha256 'a3f2e7c07fb6b117cd10cb0871ed922383c299e407700447a9a0215da457ad9c'
+  version '1.4.15'
+  sha256 'cbe24c01fa21546546d2067d50c88cab19135d1e56613bfabb0384acf6eb0bd6'
 
   url "https://www.murusfirewall.com/downloads/murus-#{version}.zip"
   name 'Murus Firewall'
