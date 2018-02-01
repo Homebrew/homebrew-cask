a28817cc135deaea449366b725d87def55e273a2
Brian Morton <brianmortondesign@gmail.com>
2018-02-01 13:43:33 -0800

    Update marta to 0.4.6 (#43469)

--- Casks/marta.rb
+++ Casks/marta.rb
@@ -1,10 +1,10 @@
 cask 'marta' do
-  version '0.4.4'
-  sha256 'de190a17f642a8c483af5565c848f3de9f9cf9aa59136b35db391fd763dcd956'
+  version '0.4.6'
+  sha256 '3ca5ac09f598cc63ef56b3b770f770602e74ba19f7f79dcab406e108f991e02d'
 
   url "https://marta.yanex.org/updates/Marta-#{version}.dmg"
   appcast 'https://marta.yanex.org/updates/appcast.xml',
-          checkpoint: '3b9badfc82c57663249260e4e7ddb1c63725c95e653e514beaad259e9556d50d'
+          checkpoint: '6da25ee66e0885d4c9e61a75d0d429dd3877be662436dadb6bc07820445aea63'
   name 'Marta File Manager'
   homepage 'https://marta.yanex.org/'
 
