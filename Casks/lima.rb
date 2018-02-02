e55b03c32c7e12fb9befb89ac75f6ca8bb1d491b
Brian Morton <brianmortondesign@gmail.com>
2018-02-02 12:32:39 -0800

    Update lima to 1.4.6 (#43571)
    * Update lima to 1.4.6
    
    * Update lima.rb

--- Casks/lima.rb
+++ Casks/lima.rb
@@ -1,6 +1,6 @@
 cask 'lima' do
-  version '1.4.5'
-  sha256 '57a83f9fa61658f27b0be07c5ab7c3795dba744f5f4212e2bb1319c6e9436182'
+  version '1.4.6'
+  sha256 '38a5929b1cf1d503f79cc5738f546f5cd97ffcb6f54e5c9993de39501cc33a61'
 
   url "https://update.api.meetlima.com/downloads/osx/dist/Lima_#{version}.dmg"
   name 'Lima'
