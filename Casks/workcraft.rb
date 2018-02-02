1144cbe01449424ca4f294d3bfe2dd0996a462ac
Brian Morton <brianmortondesign@gmail.com>
2018-02-01 16:59:06 -0800

    Update workcraft to 3.1.8 (#43547)

--- Casks/workcraft.rb
+++ Casks/workcraft.rb
@@ -1,6 +1,6 @@
 cask 'workcraft' do
-  version '3.1.7'
-  sha256 'b87911235218a6f329e4487fc20aa22c7c07d19f37f53a442ed499452dc20fcc'
+  version '3.1.8'
+  sha256 'd3d23dcd66f6f2118b67a2a570dcd154eef2756613e0bf47f29aa84f990f8dd8'
 
   url "https://www.workcraft.org/_media/download/workcraft-v#{version}-osx.tar.gz"
   name 'Workcraft'
