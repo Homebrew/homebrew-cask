e2cfc4d76cee485754cc7506aff0acf81d7beb5c
Brian Morton <brianmortondesign@gmail.com>
2018-02-02 10:22:28 -0800

    revert noejectdelay to 7.1.0, add depends and discontinued, fixed homepage url

--- Casks/noejectdelay.rb
+++ Casks/noejectdelay.rb
@@ -1,14 +1,19 @@
 cask 'noejectdelay' do
-  version '7.1.4'
-  sha256 '6a263c59efaaffb200db7d9ee6fe817f414e148053e610ccdcf37186fed27cc0'
+  version '7.1.0'
+  sha256 'cfaeed50aa7ed9eac04716ab0e34c5ef10658802e691732f7589f9454e96aa94'
 
   url "https://pqrs.org/osx/karabiner/files/NoEjectDelay-#{version}.dmg"
   appcast 'https://github.com/tekezo/NoEjectDelay/releases.atom',
           checkpoint: '8cf6b036077bd9db8bd105ec548f60866403c2986e760dcc6d0c8b5b99916710'
   name 'NoEjectDelay'
-  homepage 'https://pqrs.org/osx/karabiner/noejectdelay.html.en'
+  homepage 'https://pqrs.org/osx/karabiner/noejectdelay.html'
 
   pkg 'NoEjectDelay.sparkle_guided.pkg'
+
+  depends_on macos: '<= :el_capitan'
+  caveats do
+    discontinued
+  end
 
   uninstall kext:    'org.pqrs.driver.NoEjectDelay',
             pkgutil: 'org.pqrs.driver.NoEjectDelay'
