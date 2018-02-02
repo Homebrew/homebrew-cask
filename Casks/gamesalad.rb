b626be56b14f16b551a77110f9c4112a7250429d
Brian Morton <brianmortondesign@gmail.com>
2018-02-02 09:46:02 -0800

    Update gamesalad to 1.25.87

--- Casks/gamesalad.rb
+++ Casks/gamesalad.rb
@@ -1,10 +1,10 @@
 cask 'gamesalad' do
-  version '1.25.85'
-  sha256 'd356da298c456160a8caee224e042b48c128d089d2b7aa528dd0fd7a94bd5e99'
+  version '1.25.87'
+  sha256 '849c7f97db772e03fec1b1adb69cf7fd7b08e4c0542caddc27b613db029576b8'
 
   url "http://releases.gamesalad.com/creator/#{version}/GameSalad-Creator-#{version}.dmg"
   appcast 'https://gamesalad.com/download/studioUpdates',
-          checkpoint: 'ffdb065bbedb2011e1616a48ac6f261e4279e02f9c7505ac2db8376b30b3f7f1'
+          checkpoint: '8898dc1aa86016b6b8e8688ab24f3ad50d9f202c9c21e5efb6cbce8af6aed4ef'
   name 'GameSalad'
   homepage 'https://gamesalad.com/'
 
