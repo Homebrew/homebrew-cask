08bca4de57185ab1738f70d8267502602d5ee437
commitay <commitay@users.noreply.github.com>
2018-02-04 09:18:35 +1000

    Update emacsclient.rb

--- Casks/emacsclient.rb
+++ Casks/emacsclient.rb
@@ -1,6 +1,6 @@
 cask 'emacsclient' do
-  version '1.0'
-  sha256 '865c5779d37ee750abf75f8f57f4b2ad7532337b04e9be660a7644d2cd5aa6e4'
+  version :latest
+  sha256 :no_check
 
   url 'https://github.com/sprig/org-capture-extension/raw/master/EmacsClient.app.zip'
   name 'emacsclient'
