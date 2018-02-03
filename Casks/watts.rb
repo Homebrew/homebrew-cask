4d6788d5a8d35ba4431dac19697c06ca99648b5f
Victor Popkov <victor@popkov.me>
2018-02-03 22:29:33 +0200

    Remove comment

--- Casks/watts.rb
+++ Casks/watts.rb
@@ -2,7 +2,6 @@ cask 'watts' do
   version :latest
   sha256 :no_check
 
-  # matchingbrackets.com/abatt was verified as official when first introduced to the cask
   url 'https://binarytricks.com/watts2/public/Watts.app.tgz'
   name 'Watts'
   homepage 'https://binarytricks.com/'
