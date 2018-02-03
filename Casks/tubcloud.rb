7b5530ac34e6f098c108cabf7d48b85e793e8587
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-03 17:48:40 +0000

    Update tubcloud to latest

--- Casks/tubcloud.rb
+++ Casks/tubcloud.rb
@@ -2,9 +2,11 @@ cask 'tubcloud' do
   version :latest
   sha256 :no_check
 
-  url 'https://updates.owncloud.tu-berlin.de/tubcloud-latest-macos.pkg'
+  url 'https://updates.tubcloud.tu-berlin.de/clients/tubCloud-latest.pkg'
   name 'tubCloud Client'
   homepage 'https://www.tubit.tu-berlin.de/menue/dienste/daten_server/tubcloud/tubcloud_sync-client/'
+
+  depends_on macos: '>= :yosemite'
 
   pkg 'tubcloud-latest-macos.pkg'
 
