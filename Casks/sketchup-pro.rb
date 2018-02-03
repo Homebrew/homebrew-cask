129be04712dc5313fb12e438d4e9a92e0bc23bae
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-03 20:31:06 +0000

    Update sketchup-pro to 2018 (#43617)

--- Casks/sketchup-pro.rb
+++ Casks/sketchup-pro.rb
@@ -1,63 +1,63 @@
 cask 'sketchup-pro' do
-  version '2017'
+  version '2018'
 
   language 'de' do
-    sha256 '5c97409a3427ef10d36e9aa7e54f85c2a177bd364a7f8505fc547815f7685f5d'
+    sha256 '8c8d6d7d76530acdcf5e7adc142d6bef95e44599caf9d90e32ae33baa8a279f4'
     'de'
   end
 
   language 'en', default: true do
-    sha256 'ee5d2001aa76c9ada0becb01cff2254820b293b795498cc1c7995121d979d469'
+    sha256 '8550613522db048fd2542f96ce84ab2439dfd4b46b663dca0c490e53742dbb7a'
     'en'
   end
 
   language 'es' do
-    sha256 'c894967cd9a46b5d8a5cc610a1b1002ac3ed29247e2270a1f65928e1a5b71997'
+    sha256 '3ad389c2df4c86bb96bdb002770847fcb346b13820b96fd8c8af00813cb12916'
     'es'
   end
 
   language 'fr' do
-    sha256 '54faacfee36f320886fb613632aabf8745faacb8fbbada46d8fef062702d835c'
+    sha256 '4cab13f768f239d2b44eeeead8d81957c1d5fd6b6cac891fe46d17d3e69f870d'
     'fr'
   end
 
   language 'it' do
-    sha256 '5c737e037d788bc0ef0c9f0ab053a61c8c690ae33dd76e4b8db1a8c354ad19e9'
+    sha256 '9403f031d6e94060bd7268cd466b2db32f69289857157ef673903fa0fa49c29f'
     'it'
   end
 
   language 'ja' do
-    sha256 '509b0e73ea9d92d1ffde308faee208eebb0522249bcc8a85c079f6563399d861'
+    sha256 '083d35f1f08e8680dc46525a901b69dfc52f3462361d6aea05c7cd78e731c2b2'
     'ja'
   end
 
   language 'ko' do
-    sha256 'c00d3908754e8ea69522292e43054a4ce5955f5559f7baa66cfea8ad60ded683'
+    sha256 'ed4619b3f53d9a48792b37cc23201edbb23e9dad06bc80692e1c1a0ef31e8cf9'
     'ko'
   end
 
   language 'pt-BR' do
-    sha256 'adc5bb30d5655db6b070baf07b232a2fd0b43e9e15f2af31ea47635b249f2268'
+    sha256 'b0775fda5c4b89b2bc1704525660cd70e096ed3e13e06d67873d5931174513e5'
     'pt-BR'
   end
 
   language 'ru' do
-    sha256 'd3183f1d966a8b513101d41bbd0bacdd76bb5138a6fbb6a80bb80e1f7bcda679'
+    sha256 'd2d63dc53a5a63193516d911b4b9410f8e28aceea28d74815ff86aac9f65841d'
     'ru'
   end
 
   language 'sv' do
-    sha256 'f72b593bd22c0ad9482caf58d004ce7a024eb501d06e8d87288248381516d0d0'
+    sha256 'c444454b7e88c0dfe56aa669141b5b243bbc864cf0dec164685a5fb9892bed23'
     'sv'
   end
 
   language 'zh-CN' do
-    sha256 'ca8ad25646406a43365308a948e723854bd2e1b2ab4622bdfcdbdd9dae86e943'
+    sha256 '922ece75366b2f12406b82f8bf9c4777f2ed811b434b66da7a265e2d18f7ecaf'
     'zh-CN'
   end
 
   language 'zh-TW' do
-    sha256 'ea63cdba7208ad4b447c52305c5b60b206c049d383859e7c5d71071713f5e657'
+    sha256 '13643fd1836820095ae01cb425096cd5df156b488a65c9813c4484573f246176'
     'zh-TW'
   end
 
