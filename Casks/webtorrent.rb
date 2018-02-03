4e9d9d2b76e03bf7aff13472c4f2a6583b0cecba
Victor Popkov <victor@popkov.me>
2018-02-03 22:38:35 +0200

    Update comment

--- Casks/webtorrent.rb
+++ Casks/webtorrent.rb
@@ -2,7 +2,7 @@ cask 'webtorrent' do
   version '0.19.0'
   sha256 'b05c4f9f10d7aedbb2af7836b4cf0366db887e9695439b9475e5e357b917c720'
 
-  # github.com/feross/webtorrent-desktop was verified as official when first introduced to the cask
+  # github.com/webtorrent/webtorrent-desktop was verified as official when first introduced to the cask
   url "https://github.com/webtorrent/webtorrent-desktop/releases/download/v#{version}/WebTorrent-v#{version}.dmg"
   appcast 'https://github.com/webtorrent/webtorrent-desktop/releases.atom',
           checkpoint: '85b760f658ed353d6d978b2e899842637b59016d5e205f053b27ab4a433d5beb'
