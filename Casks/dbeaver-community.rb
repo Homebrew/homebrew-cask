ff2634756c085d2d0a9222c9571176af0c6fc7cf
commitay <commitay@users.noreply.github.com>
2018-02-05 07:06:52 +1000

    Update dbeaver-community.rb

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -3,7 +3,7 @@ cask 'dbeaver-community' do
   sha256 'c0d356d33015d43596286c6f722b3bf02ea6f792c3c1df5f5d4270f940784d1d'
 
   # github.com/dbeaver/dbeaver was verified as official when first introduced to the cask
-  url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version.major_minor_patch}-macos.dmg"
+  url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
   appcast 'https://github.com/dbeaver/dbeaver/releases.atom',
           checkpoint: 'e92f97d31ccfe51457bcfe76391493c5116e8ef2879f7ff98c2eccebd936ede1'
   name 'DBeaver Community Edition'
