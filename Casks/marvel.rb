8496fd9cec1ec8ef52420be5cc622048324d2d24
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-04 20:19:51 +0000

    Update marvel to 7.3.4

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,11 +1,11 @@
 cask 'marvel' do
-  version '7.3.1'
-  sha256 'e2a24ae3fc2a5242977d007946c45c07af4e41cf1033249a3773fe4ba4d16b44'
+  version '7.3.4'
+  sha256 'bcc2637a77285839249bd6417f6efab6e462fb10a0027ac275fda61b1bdd5a29'
 
   # storage.googleapis.com/sketch-plugin was verified as official when first introduced to the cask
   url 'https://storage.googleapis.com/sketch-plugin/Marvel.zip'
   appcast 'https://storage.googleapis.com/sketch-plugin/app.xml',
-          checkpoint: '525fcf6f57356a788014f9dc31e8dc5a59cf36ddbada08866c21a54a66a1779a'
+          checkpoint: '3c5f2ddfca2939ebef5ce23ee8f9ea10c2c735fa38bd37c06f38f560756eb9fb'
   name 'Marvel'
   homepage 'https://marvelapp.com/'
 
