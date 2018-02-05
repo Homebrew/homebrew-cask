649e1eef64bef87f1bb8dacbd724aee9c9703bad
Brian Morton <brianmortondesign@gmail.com>
2018-02-05 10:04:45 -0800

    Update aircall to 1.4.13 (#43653)

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -1,10 +1,10 @@
 cask 'aircall' do
-  version '1.4.11'
-  sha256 'eef8dd31b3b9c9a7fad88758474e7755b1715f5405382bfe705bf02572b5d295'
+  version '1.4.13'
+  sha256 'a9f268aa82648913b6332ccfef75cb7a04d2726a90717cbcd8125b3ed91d298c'
 
   url "https://electron.aircall.io/download/version/#{version}/osx_64?filetype=dmg&channel=stable"
   appcast 'https://electron.aircall.io/update/osx/1.1.0',
-          checkpoint: '4814444d1ab30b98414914c5c7b98c37b57ad7b66906b94efcd6b4d0e80cd234'
+          checkpoint: 'd1400fd60fc8d36e1fe09c48b03e9127a9e0453c873977c0d6a8d11d124bddfe'
   name 'Aircall'
   homepage 'https://aircall.io/'
 
