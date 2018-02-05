69cc26d1215050110aac452ce354a487981d12e8
Brian Morton <brianmortondesign@gmail.com>
2018-02-05 11:00:12 -0800

    Update openclonk to 8.0

--- Casks/openclonk.rb
+++ Casks/openclonk.rb
@@ -1,10 +1,10 @@
-cask 'openclonk-c54d917-darwin-amd64-clang' do
-  version '7.0'
-  sha256 'e81f7ef8dc5a345101a92f81c630f29443049d83b661b18d8a81f15134350ef2'
+cask 'openclonk' do
+  version '8.0'
+  sha256 'cba50deb936a70f0ba003313659b91c365f231a3f9d86d10e6918c3f32c7bced'
 
   url "http://openclonk.org/builds/release/#{version}/openclonk-#{version}-x64.app.zip"
   appcast 'http://www.openclonk.org/download/',
-          checkpoint: 'cbbae98cbbb4cd362b5fc6cd5de1e369dd2ba7156cdabb389fea6e4131b756c4'
+          checkpoint: '2aaf233efeca1ad718019436142ddcd18473c360fa001a616de96eac69801fd6'
   name 'OpenClonk'
   homepage 'http://openclonk.org/'
 
