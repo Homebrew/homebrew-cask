55b92a2e8ec4dc7eb386546e698caa70847e1cc5
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-05 22:32:41 +0000

    Update mdrp to 7.1.1 (#43685)

--- Casks/mdrp.rb
+++ Casks/mdrp.rb
@@ -1,12 +1,14 @@
 cask 'mdrp' do
-  version '7.0.5'
-  sha256 '5c21aa23698133aeb34d91676820588f533d5b02062f94abc9c19170ab990c41'
+  version '7.1.1'
+  sha256 '0b7fba44f24a6455528b21042055bbfe41aa2639e42efd67e0b97dcfb56ce7a6'
 
   url "http://www.macdvdripperpro.com/MDRP_v#{version.no_dots}.zip"
   appcast 'https://www.macdvdripperpro.com/mdrp_sparkle5.xml',
-          checkpoint: '08dbc4e4d1c36103b820a9b6f5b5a9c8037404502fc2d99d93eaba5bab2940be'
+          checkpoint: 'e4b1ef3cd2f8be5bf82825da0f684813af404c2bd8e9d43a9bf17aa3521acbb3'
   name 'Mac DVDRipper Pro'
   homepage 'https://www.macdvdripperpro.com/'
+
+  depends_on macos: '>= :mountain_lion'
 
   app 'MDRP.app'
 end
