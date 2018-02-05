ee9b7fe0cc4817d05c225b512fa1def3b1ae6d06
commitay <commitay@users.noreply.github.com>
2018-02-05 12:41:56 +1000

    Update vernier-graphical-analysis.rb

--- Casks/vernier-graphical-analysis.rb
+++ Casks/vernier-graphical-analysis.rb
@@ -3,7 +3,7 @@ cask 'vernier-graphical-analysis' do
   sha256 '500f8f0bf399e9bd01fc8c23a660a7675a1f4905d871d40f7c732307e5f1bff6'
 
   # graphicalanalysis.com was verified as official when first introduced to the cask
-  url 'https://software-releases.graphicalanalysis.com/ga/mac/release/Vernier-Graphical-Analysis-4.3.0-759.dmg'
+  url "https://software-releases.graphicalanalysis.com/ga/mac/release/Vernier-Graphical-Analysis-#{version}.dmg"
   name 'Vernier Graphical Analysis'
   homepage 'https://www.vernier.com/products/software/graphical-analysis/'
 
