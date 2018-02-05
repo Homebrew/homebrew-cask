87619e6672ddf66ba47790fefd0ec042c99d8bdd
Brian Morton <brianmortondesign@gmail.com>
2018-02-05 14:21:14 -0800

    added comment, changed url

--- Casks/mockplus.rb
+++ Casks/mockplus.rb
@@ -2,7 +2,8 @@ cask 'mockplus' do
   version '3.2.10.2'
   sha256 '84a9a1b6226b0496d7e2a875ceef747175c1b985d74ac5631850b42ec3f42105'
 
-  url "http://cdn.mockplus.cn/software/osx/Mockplus_v#{version}.dmg"
+  # mockplus.com is the english version of mockplus.cn (official), creators of the software
+  url "https://s3-us-west-1.amazonaws.com/mockplus-static/software/macos/Mockplus_v#{version}.dmg"
   name 'Mockplus'
   name '摩客'
   homepage 'https://www.mockplus.com/'
