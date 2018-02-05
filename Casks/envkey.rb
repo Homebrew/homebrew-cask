897dcd9a952deee3249925677ca91924cb761bf5
Brian Morton <brianmortondesign@gmail.com>
2018-02-05 09:45:53 -0800

    Update envkey to 1.2.9

--- Casks/envkey.rb
+++ Casks/envkey.rb
@@ -1,11 +1,11 @@
 cask 'envkey' do
-  version '1.2.8'
-  sha256 'c26d55b27781060b910a124821d6d3662c448b9b2d52817e4ca02698bd8798ff'
+  version '1.2.9'
+  sha256 '2e2dd1197afaaa0400b55cbc215524cbc7a4b47fe2dbbe35a1bcc0b6c2c83e03'
 
   # github.com/envkey/envkey-app was verified as official when first introduced to the cask
   url "https://github.com/envkey/envkey-app/releases/download/darwin-x64-prod-v#{version}/EnvKey-#{version}-mac.zip"
   appcast 'https://github.com/envkey/envkey-app/releases.atom',
-          checkpoint: 'b62623b612b51255b2456ae63579190029fb94cee1ca056dbe0c78d84c645ecf'
+          checkpoint: '005ce4776e0ecc452b9e0a005042a86313f73a22b29ebd7c14ac5187d9cacffb'
   name 'EnvKey'
   homepage 'https://www.envkey.com/'
 
