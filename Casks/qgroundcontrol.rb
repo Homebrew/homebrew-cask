04754f378df8d8095649ad5fd7a61ad5bd43d7bb
Brian Morton <brianmortondesign@gmail.com>
2018-02-05 12:37:43 -0800

    Update qgroundcontrol to 3.2.7 (#43679)

--- Casks/qgroundcontrol.rb
+++ Casks/qgroundcontrol.rb
@@ -1,11 +1,11 @@
 cask 'qgroundcontrol' do
-  version '3.2.5'
-  sha256 '246ab87302945861782230ddc5f171e5f226d728ff7974a2628230045f6c860f'
+  version '3.2.7'
+  sha256 '14d2cd2487554b89461e60a0bff4f335ae847927a40918944092a4ed09fe1e77'
 
   # github.com/mavlink/qgroundcontrol/releases/download was verified as official when first introduced to the cask
   url "https://github.com/mavlink/qgroundcontrol/releases/download/v#{version}/QGroundControl.dmg"
   appcast 'https://github.com/mavlink/qgroundcontrol/releases.atom',
-          checkpoint: '74f3d5dd1fc403aeef01d151d8960d0db72ea24c3a7e0cd1957adff2f033c43e'
+          checkpoint: 'd60cb67da1483476a59806fe7a8b45ba6d1d9fb54f9508f875c933f6d253d396'
   name 'QGroundControl'
   homepage 'http://qgroundcontrol.com/'
 
