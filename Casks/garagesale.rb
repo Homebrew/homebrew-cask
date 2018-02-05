c8a41c62588a6fc12195e8fc242bd05fcaa9fee3
Brian Morton <brianmortondesign@gmail.com>
2018-02-05 09:53:52 -0800

    Update garagesale to 7.0.11

--- Casks/garagesale.rb
+++ Casks/garagesale.rb
@@ -1,6 +1,6 @@
 cask 'garagesale' do
-  version '7.0.10'
-  sha256 'a3656e982a6a92519574ad8c5f9f80bd15b76de5c0f98b066e49958ea33d5a1c'
+  version '7.0.11'
+  sha256 '8f2ef6394908d8d361908bdf7a0d8302078a01ec81050cbb7eddec57beef5fa3'
 
   url "https://downloads.iwascoding.com/downloads/GarageSale_#{version}.dmg"
   name 'GarageSale'
