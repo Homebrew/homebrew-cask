cb87ef732827bb5029ded5c7bf55487534338af2
Brian Morton <brianmortondesign@gmail.com>
2018-02-05 10:49:49 -0800

    Update mockplus to 3.2.10.2

--- Casks/mockplus.rb
+++ Casks/mockplus.rb
@@ -1,11 +1,11 @@
 cask 'mockplus' do
-  version '3.2.6'
-  sha256 '983a5eadf7d17c4697304586c0e3d74abe785ae72581033a2b246006cb7d9426'
+  version '3.2.10.2'
+  sha256 '84a9a1b6226b0496d7e2a875ceef747175c1b985d74ac5631850b42ec3f42105'
 
   url "http://cdn.mockplus.cn/software/osx/Mockplus_v#{version}.dmg"
   name 'Mockplus'
   name '摩客'
-  homepage 'https://www.mockplus.cn/'
+  homepage 'https://www.mockplus.com/'
 
   app 'Mockplus.app'
 end
