d786107c1b7b9d651b62728c26f8b8ed83988cd6
Brian Morton <brianmortondesign@gmail.com>
2018-02-05 14:23:41 -0800

    fixed checksum

--- Casks/mockplus.rb
+++ Casks/mockplus.rb
@@ -1,6 +1,6 @@
 cask 'mockplus' do
   version '3.2.10.2'
-  sha256 '84a9a1b6226b0496d7e2a875ceef747175c1b985d74ac5631850b42ec3f42105'
+  sha256 '737974c1598848b9b30cdc10b6a60508d96f49b63e348d3c9139b280ccdf6c0b'
 
   # mockplus.com is the english version of mockplus.cn (official), creators of the software
   url "https://s3-us-west-1.amazonaws.com/mockplus-static/software/macos/Mockplus_v#{version}.dmg"
