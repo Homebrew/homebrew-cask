7e8eeec72b05b65c471bf1a434edba81f330c192
Brian Morton <brianmortondesign@gmail.com>
2018-02-06 13:40:05 -0800

    Update mightytext to 3.88.20 (#43720)

--- Casks/mightytext.rb
+++ Casks/mightytext.rb
@@ -1,6 +1,6 @@
 cask 'mightytext' do
-  version '3.88.18'
-  sha256 '9bece607493ef5997eda9ab11261217bece836ceafa9ae90291653028d3326dc'
+  version '3.88.20'
+  sha256 '03c4dff76d6894f185cdde8fa9b601e0d9037eacdaf9589dd8ac78d28125389c'
 
   url "https://dl-desktop.mightytext.net/MightyText-#{version}.dmg"
   name 'MightyText'
