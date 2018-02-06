a7ae0e0c9f6e90957005e7c8407bbad91dea3b19
Brian Morton <brianmortondesign@gmail.com>
2018-02-06 13:58:26 -0800

    Update ultimaker-cura to 3.2.0 (#43729)

--- Casks/ultimaker-cura.rb
+++ Casks/ultimaker-cura.rb
@@ -1,6 +1,6 @@
 cask 'ultimaker-cura' do
-  version '3.1.0'
-  sha256 'd34f124dfdd0d72d089e7dcc2dc7c8763f3047402748e6e1d7c40bfc5b880de0'
+  version '3.2.0'
+  sha256 '6416f3ec9f045d42c5547afa4e730d3faecfd28aac864ffd9296dfd91e0da5a9'
 
   url "https://software.ultimaker.com/current/Ultimaker%20Cura-#{version}-Darwin.dmg"
   name 'Ultimaker Cura'
