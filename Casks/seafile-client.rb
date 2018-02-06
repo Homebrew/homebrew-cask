59a97cd2d5033cbb23f05b15b2df07c937ec898d
Brian Morton <brianmortondesign@gmail.com>
2018-02-06 13:41:15 -0800

    Update seafile-client to 6.1.5

--- Casks/seafile-client.rb
+++ Casks/seafile-client.rb
@@ -1,6 +1,6 @@
 cask 'seafile-client' do
-  version '6.1.4'
-  sha256 'dc69d0ceb0191c31d3178b5cf96aef4172fbead904b85ab6dd729937deeaf792'
+  version '6.1.5'
+  sha256 '183db605f4cb364c24baeb9f7faab3e1b80a35c87ccdd8cb4fef02d084af3123'
 
   # seadrive.org was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seafile-client-#{version}.dmg"
