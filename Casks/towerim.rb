b1bae6a9767b34266a3ed47cbacdfa3a38b635d9
commitay <commitay@users.noreply.github.com>
2018-02-06 15:33:19 +1000

    Update towerim.rb

--- Casks/towerim.rb
+++ Casks/towerim.rb
@@ -1,9 +1,9 @@
 cask 'towerim' do
-  version :latest
-  sha256 :no_check
+  version '0.6.0'
+  sha256 'fcde1f98c5be96c19170e3c16442257a5396787bffea7c7b873d11723cef6b3e'
 
   # towerfiles.oss-cn-hangzhou.aliyuncs.com/electron/mac was verified as official when first introduced to the cask
-  url 'https://towerfiles.oss-cn-hangzhou.aliyuncs.com/electron/mac/Tower-latest.zip'
+  url "https://towerfiles.oss-cn-hangzhou.aliyuncs.com/electron/mac/Tower-#{version}.dmg"
   name 'Tower'
   homepage 'https://tower.im/'
 
