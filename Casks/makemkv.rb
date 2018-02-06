6023459e25ae93289b39f2c78fbbb4947a24664e
Fabian Maass <fabian.maass@adnovum.ch>
2018-02-06 09:43:12 +0100

    Update makemkv to 1.12.0

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,10 +1,10 @@
 cask 'makemkv' do
-  version '1.10.10'
-  sha256 'd8d8744be98f7c92abb45c25cf3784cd471cfaa62fd3e9ecd91430a501f7e18f'
+  version '1.12.0'
+  sha256 '9b43e950756553e5147c250bb39a6ef61a4b4aaca2a7828a221d49d127faf2f6'
 
   url "https://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   appcast 'http://www.makemkv.com/download/',
-          checkpoint: '8007a19099fc5ccd24766edaa0096068cff23701992c52140bd876e04842c18f'
+          checkpoint: 'e68f4b03f854fd14bbce6baaf4781e94261d181399d6b3f6dc34f2378f7b9696'
   name 'MakeMKV'
   homepage 'https://www.makemkv.com/'
 
