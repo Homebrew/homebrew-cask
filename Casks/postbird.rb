8ad6e8548127e53738be6b2b076790fcceaf624d
Brian Morton <brianmortondesign@gmail.com>
2018-02-06 13:45:20 -0800

    Update postbird to 0.7.1 (#43723)

--- Casks/postbird.rb
+++ Casks/postbird.rb
@@ -1,10 +1,10 @@
 cask 'postbird' do
-  version '0.7.0'
-  sha256 '105145077f78498a0746f91d045c2186a3d1dd9568dea21e592fdef881df1c49'
+  version '0.7.1'
+  sha256 'a079c87a85f5610d356260909243742c35f93883afb8eb6c740b3ae807fce666'
 
   url "https://github.com/Paxa/postbird/releases/download/#{version}/Postbird-#{version}.dmg"
   appcast 'https://github.com/Paxa/postbird/releases.atom',
-          checkpoint: '211d4201b11abe94195234492bc3b0fae6c64005fb76157b0d06b691645f5e8a'
+          checkpoint: '75051a908926bd97c430aa6a04859b212b337454610c0e7289f0754c455a85a7'
   name 'Postbird'
   homepage 'https://github.com/Paxa/postbird'
 
