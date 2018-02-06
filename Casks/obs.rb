99e5981bc11d9bbac5d2fde78a5c9e44bbddf889
Erik Minekus <tsunami@tsunami-productions.nl>
2018-02-06 17:45:54 +0100

    Update obs to 21.0.2

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -1,15 +1,15 @@
 cask 'obs' do
-  version '20.1.0'
-  sha256 '8eb490f749b3717002fd55b0ee14e082191cc921c5913070890e95af8776445d'
+  version '21.0.2'
+  sha256 'f2b7369a16b42fd8e73345c964a2b68130d55478bd5adb1dc207fb2d90b99b7d'
 
   # github.com/jp9000/obs-studio was verified as official when first introduced to the cask
-  url "https://github.com/jp9000/obs-studio/releases/download/#{version}/obs-mac-#{version.major_minor}-installer.pkg"
+  url "https://github.com/jp9000/obs-studio/releases/download/#{version}/obs-mac-#{version}-installer.pkg"
   appcast 'https://github.com/jp9000/obs-studio/releases.atom',
-          checkpoint: 'bce365214c203a68f6978094931f9748ddac61be3b8de71adbb55a6cdf0f584e'
+          checkpoint: '66148fae44e816f788ade0d4d40bc60ffbd6bf89abf756a3a1541c93fb864d63'
   name 'OBS'
   homepage 'https://obsproject.com/'
 
-  pkg "obs-mac-#{version.major_minor}-installer.pkg"
+  pkg "obs-mac-#{version}-installer.pkg"
 
   uninstall pkgutil: [
                        'org.obsproject.pkg.obs-studio',
