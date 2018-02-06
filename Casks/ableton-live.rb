0563cc9784f322ad2430203168703c29ae4719e5
Vítor Galvão <info@vitorgalvao.com>
2018-02-06 20:41:15 +0000

    Update ableton-live to 10.0.1 (#43708)

--- Casks/ableton-live.rb
+++ Casks/ableton-live.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live' do
-  version '9.7.5'
-  sha256 'b10d610377be15eb57c4a228c127381dae38e45c58273bc3936648a2e60ac2b9'
+  version '10.0.1'
+  sha256 '36abc5d8b9900fff2338ddc28bc854f98f40804958cf3fa9e3d812459f3f0263'
 
   url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_trial_#{version}_64.dmg"
   name 'Ableton Live'
