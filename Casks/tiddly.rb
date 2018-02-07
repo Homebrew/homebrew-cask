b33761b354c0465ff002626da05dbb9829402cb0
Brian Morton <brianmortondesign@gmail.com>
2018-02-07 04:55:41 -0800

    Update and rename tiddlywiki to tiddly 0.0.12 (#43532)
    * Update tiddlywiki to 0.0.12
    
    * Update and rename tiddlywiki.rb to tiddly.rb

--- Casks/tiddly.rb
+++ Casks/tiddly.rb
@@ -1,12 +1,12 @@
-cask 'tiddlywiki' do
-  version '0.0.9'
-  sha256 '85ca27b3819643f46c20fea16bcdc1c426bdf691fa124d49d96a71e76982b34c'
+cask 'tiddly' do
+  version '0.0.12'
+  sha256 'c75d8a68360ab10ebe15917eea3c570094f5adf23411435fd53190ab509785f5'
 
   url "https://github.com/Jermolene/TiddlyDesktop/releases/download/v#{version}/tiddlydesktop-mac64-v#{version}.zip"
   appcast 'https://github.com/Jermolene/TiddlyDesktop/releases.atom',
-          checkpoint: '2b31415d87ec8d870bf0fdf39f1b27f9f8b60e28eac39dd050175d7caa29cc4e'
+          checkpoint: '985ae77d9351beb2c4cb57eb8255fb08d166c170c09e03a2f01bab24e582da54'
   name 'TiddlyWiki'
   homepage 'https://github.com/Jermolene/TiddlyDesktop'
 
-  app "TiddlyDesktop-mac64-v#{version}/TiddlyWiki.app"
+  app "TiddlyDesktop-mac64-v#{version}/TiddlyDesktop.app"
 end
