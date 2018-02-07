fcdf972f905883aeca24326950ae495b3fd69683
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2018-02-07 16:57:00 -0700

    Update powerphotos to 1.3.6 (#43787)

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -8,13 +8,13 @@ cask 'powerphotos' do
     sha256 'b07eb9f8801fb397d55e3dd7e0569dbef5d3265debaf3ee68247062901d93fcb'
     url "https://www.fatcatsoftware.com/powerphotos/PowerPhotos_#{version.no_dots}.zip"
   else
-    version '1.3.5'
-    sha256 '0672fd3743ce9905ba742003b86ea188b60628ccacd301bfbabc75bb7869205f'
+    version '1.3.6'
+    sha256 'a205d06a4a26c736b1e6069c4d6a083d4c18e8431c07a54571a6d05c9f3fc64b'
     url 'https://www.fatcatsoftware.com/powerphotos/PowerPhotos.zip'
   end
 
   appcast 'https://www.fatcatsoftware.com/powerphotos/powerphotos_appcast.xml',
-          checkpoint: '6a3b876929b2491630f6b14f3156455d4054ce4484c6b17802034b90d0ad1222'
+          checkpoint: '51b170d10154183268573085a50c26cb168f92b7eae9d1ad9381a3536ed92a91'
   name 'PowerPhotos'
   homepage 'https://www.fatcatsoftware.com/powerphotos/'
 
