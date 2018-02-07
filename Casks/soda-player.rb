547b49cb3045a24df8c8d69d86418f1308e8163d
commitay <commitay@users.noreply.github.com>
2018-02-08 06:32:24 +1000

    Update soda-player.rb

--- Casks/soda-player.rb
+++ Casks/soda-player.rb
@@ -3,8 +3,6 @@ cask 'soda-player' do
   sha256 '67d8775e216685b05ded343b9093f91b0794e5396b43b96b3d588425ab7197bd'
 
   url "https://releases.sodaplayer.com/mac/Soda%20Player%20#{version}.dmg"
-  appcast 'https://headwayapp.co/soda-player-changelog',
-          checkpoint: 'b08ed4cb5eff7f523859a0b71710039fa380d264a005f007d58f892fe6f813e8'
   name 'Soda Player'
   homepage 'https://www.sodaplayer.com/'
 
