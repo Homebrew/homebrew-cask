3f4106e21306790cf663d4edbce35301caaeb298
Jason Charrier <jason@innovativelanguage.com>
2018-02-06 19:30:23 -0600

    Add SecuritySpy Latest

---
+++ Casks/securityspy.rb
@@ -0,0 +1,10 @@
+cask 'securityspy' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.bensoftware.com/securityspy/SecuritySpy.dmg'
+  name 'SecuritySpy'
+  homepage 'https://www.bensoftware.com/securityspy/'
+
+  app 'SecuritySpy.app'
+end
