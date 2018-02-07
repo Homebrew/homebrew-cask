a962c862f38f432c53115b643e6dd09ef45466cb
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2018-02-07 16:57:29 -0700

    Update blueharvest to 7.0.2 (#43789)

--- Casks/blueharvest.rb
+++ Casks/blueharvest.rb
@@ -1,10 +1,10 @@
 cask 'blueharvest' do
-  version '7.0.1'
-  sha256 '13966aa3ac041533ccbd1ee0ff756e4f15930f2843bc3304955ebbbc71b799fb'
+  version '7.0.2'
+  sha256 'dc86c780095ee4a32b070a14a5f9447865df482bcfe64d16510134f81734fecc'
 
   url "http://zeroonetwenty.com/downloads/BlueHarvest#{version.no_dots}.dmg"
   appcast 'https://cp37.ezyreg.com/~zeroonet/downloads/versioninfo/sparkle/blueharvest6.xml',
-          checkpoint: 'd62ab9b0052d3aadd47b4138134321df5eaa8ff3f34c2c638e86b4a1e5b83385'
+          checkpoint: '3b5ac8c61a90882c2d019a69d134ffdf1bd2fc96f1381d1c6f22e26fa9fb74d5'
   name 'BlueHarvest'
   homepage 'http://zeroonetwenty.com/blueharvest/'
 
