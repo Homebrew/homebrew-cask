0ee3baa98027194a02a13721523ec2c822983e8b
Vítor Galvão <info@vitorgalvao.com>
2018-02-07 18:56:16 +0000

    Update soda-player to 1.2.3

--- Casks/soda-player.rb
+++ Casks/soda-player.rb
@@ -1,8 +1,10 @@
 cask 'soda-player' do
-  version :latest
-  sha256 :no_check
+  version '1.2.3'
+  sha256 '67d8775e216685b05ded343b9093f91b0794e5396b43b96b3d588425ab7197bd'
 
-  url 'https://releases.sodaplayer.com/mac/Soda%20Player.dmg'
+  url "https://releases.sodaplayer.com/mac/Soda%20Player%20#{version}.dmg"
+  appcast 'https://headwayapp.co/soda-player-changelog',
+          checkpoint: 'b08ed4cb5eff7f523859a0b71710039fa380d264a005f007d58f892fe6f813e8'
   name 'Soda Player'
   homepage 'https://www.sodaplayer.com/'
 
