cb405b65f8e84392109ccbe7532c574146d50704
Brian Morton <brianmortondesign@gmail.com>
2018-02-07 12:29:28 -0800

    Update opencpn to 4.8.2 (#43768)
    * Update opencpn to 4.8.2
    
    * Update opencpn.rb

--- Casks/opencpn.rb
+++ Casks/opencpn.rb
@@ -1,11 +1,11 @@
 cask 'opencpn' do
-  version '4.8.0'
-  sha256 '86f8322fc8f9065e9696f6c0b38ed1cdc63543faeaf133c8cd39f0d66e245f91'
+  version '4.8.2'
+  sha256 '04a4bd7c7dd78fa2462abc33968d5fa62d268f10a4cb1f929c19eaad863084a5'
 
   # opencpn.navnux.org was verified as official when first introduced to the cask
   url "http://opencpn.navnux.org/#{version}/OpenCPN_#{version}.dmg"
   appcast 'https://github.com/OpenCPN/OpenCPN/releases.atom',
-          checkpoint: 'a6094ed534befa30a116f883cb06e4052d88a257c2043238ebc22377632933df'
+          checkpoint: 'a5abe6ccfc54897449fb98fa13f5b7f6f8047dd42505b1513bbac76677d2d6c3'
   name 'OpenCPN'
   homepage 'https://www.opencpn.org/'
 
