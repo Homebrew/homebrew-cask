2f37bb0ced9a4a71129e75c430be4247dcdbe206
Brian Morton <brianmortondesign@gmail.com>
2018-02-07 10:23:40 -0800

    Update abstract to 0.66.5 (#43759)

--- Casks/abstract.rb
+++ Casks/abstract.rb
@@ -1,6 +1,6 @@
 cask 'abstract' do
-  version '0.65.6'
-  sha256 '3fc866ef89636db1484aa9965696b3ddf7a84e977f976663adc93635112578ce'
+  version '0.66.5'
+  sha256 'e28b995824bc5326ba2de1ed0e4d1a190242e82660464b073288bf92b1585a81'
 
   # s3.amazonaws.com/propeller-internal-releases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/propeller-internal-releases/Abstract-#{version}.dmg"
