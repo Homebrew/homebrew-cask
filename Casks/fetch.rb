60a81e62a3887c4cc959b35520040ad081f0db2d
Brian Morton <brianmortondesign@gmail.com>
2018-02-08 12:47:11 -0800

    Update fetch to 5.7.7 (#43806)

--- Casks/fetch.rb
+++ Casks/fetch.rb
@@ -1,6 +1,6 @@
 cask 'fetch' do
-  version '5.7.6'
-  sha256 '0687113cd0b94784a5cd6c1f9f609cada175aac0163f3bf104305e68da847e16'
+  version '5.7.7'
+  sha256 '8b543edd7b31cb519fa8d898c6b5b6095cd3b725a477ce32238e08002ac77681'
 
   url "http://fetchsoftworks.com/fetch/download/Fetch_#{version}.dmg?direct=1"
   name 'Fetch'
