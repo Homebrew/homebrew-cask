b516891837e2f4fa4e2d07d651180fa6ffed6e09
Brian Morton <brianmortondesign@gmail.com>
2018-02-08 09:26:38 -0800

    fix checksum

--- Casks/genome-workbench.rb
+++ Casks/genome-workbench.rb
@@ -1,6 +1,6 @@
 cask 'genome-workbench' do
   version '2.12.5'
-  sha256 '52b2f90d2feff1d048b21a359ad54a9e8bd0b545b97927285e4feaf1e0929516'
+  sha256 'd6ef76f4133b9157c4c3d90f823d0a0d306b9ebc76dcc3fe21bbb46c7540dbae'
 
   url "ftp://ftp.ncbi.nlm.nih.gov/toolbox/gbench/ver-#{version}/gbench-macos64-10.10-#{version}.dmg"
   name 'Genome Workbench'
