b2ce9cb6430bc9c5e25a8a1302fc8b6a483f8b63
Zachary Cutlip <uid000@gmail.com>
2018-02-09 00:44:38 -0500

    Added missing Makefile and header file artifacts to 'kdv'

--- Casks/kdv.rb
+++ Casks/kdv.rb
@@ -9,4 +9,6 @@ cask 'kdv' do
   binary 'kdv.universal', target: 'kdv'
   artifact 'usr/share/misc/trace.codes', target: "#{HOMEBREW_PREFIX}/share/kdv/misc/trace.codes"
   artifact 'kdebugView.c', target: "#{HOMEBREW_PREFIX}/share/kdv/src/kdebugView.c"
+  artifact 'kdebug_private.h', target: "#{HOMEBREW_PREFIX}/share/kdv/src/kdebug_private.h"
+  artifact 'Makefile', target: "#{HOMEBREW_PREFIX}/share/kdv/src/Makefile"
 end
