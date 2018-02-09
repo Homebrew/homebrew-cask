5a1137fbcc2385b5001eaead473c4282d7a5dc8c
Zachary Cutlip <uid000@gmail.com>
2018-02-09 00:31:33 -0500

    Added Jonathan Levin's 'kdv' macOS & iOS kdebug tracing tool.
    This tool claims to have the functionality of fs_usage, sc_usage,
    latency, and trace, plus additional kdebug tracing that is not
    normally exposed by macOS and iOS.
    
    Jonathan bundles the source in the tarball, but it isn't a candidate for
    a source-based distribution in Homebrew for the following reasons:
    - The source requires the user to copy and modify headers from Xcode's
    macOS and iOS SDKs in order to build.
    - There is not version controlled public repository or a means of
    submitting bug reports or patches.

---
+++ Casks/kdv.rb
@@ -0,0 +1,12 @@
+cask 'kdv' do
+  version ':latest'
+  sha256 :no_check
+
+  url 'http://newosxbook.com/tools/kdv.tar'
+  name 'kdv'
+  homepage 'http://newosxbook.com/tools/kdv.html'
+
+  binary 'kdv.universal', target: 'kdv'
+  artifact 'usr/share/misc/trace.codes', target: "#{HOMEBREW_PREFIX}/share/kdv/misc/trace.codes"
+  artifact 'kdebugView.c', target: "#{HOMEBREW_PREFIX}/share/kdv/src/kdebugView.c"
+end
