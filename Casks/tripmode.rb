cf67e8cd389e9b7b458051a3c79370209e887d79
commitay <commitay@users.noreply.github.com>
2018-02-09 14:44:57 +1000

    update appcast

--- Casks/tripmode.rb
+++ Casks/tripmode.rb
@@ -3,8 +3,8 @@ cask 'tripmode' do
   sha256 '533d6696b17e1571c080f57f5ae80f00370be22ff61e2e35657eb1a19c0201b1'
 
   url "https://www.tripmode.ch/app/TripMode-#{version}-app-Release.dmg"
-  appcast 'http://updates.tripmode.ch/app/appcast.xml',
-          checkpoint: '1c589ebad8ed5f9c36a702b070882b73612030eb42bd23587d2106c1399d5e8e'
+  appcast 'https://www.tripmode.ch/app/appcast.xml',
+          checkpoint: 'da80b5877de1d7190298dbe7e253315b4d755c263a0af61012007905352308b1'
   name 'TripMode'
   homepage 'https://www.tripmode.ch/'
 
