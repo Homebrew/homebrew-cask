f9e79858154030e6c0c9ce86c4a49c45675bcfb1
Naoto Yoshimoto <me@naotone.com>
2018-02-08 23:38:06 -0500

    Update notion to 0.1.14 (#43826)

--- Casks/notion.rb
+++ Casks/notion.rb
@@ -1,6 +1,6 @@
 cask 'notion' do
-  version '0.1.13'
-  sha256 '26129cfac6cd6169ec1141cac2b172c4e1b7296bbf17db7358e322ff09a4d714'
+  version '0.1.14'
+  sha256 '45dcc33358ecf88f17eadc4588207aa6b39ff25eb6741707f816894db0a52a6f'
 
   # desktop-release.notion-static.com was verified as official when first introduced to the cask
   url "https://desktop-release.notion-static.com/Notion-#{version}.dmg"
