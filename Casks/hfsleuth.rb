d76b7dcda904f17a2089544a48a010d97fef172e
commitay <commitay@users.noreply.github.com>
2018-02-10 06:57:42 +1000

    Update hfsleuth.rb

--- Casks/hfsleuth.rb
+++ Casks/hfsleuth.rb
@@ -3,7 +3,7 @@ cask 'hfsleuth' do
   sha256 :no_check
 
   url 'http://newosxbook.com/files/hfsleuth.tar'
-  name 'HFSleuth − HFS+/HFSX file system inspection tool'
+  name 'HFSleuth'
   homepage 'http://newosxbook.com/tools/hfsleuth.html'
 
   binary 'hfsleuth.universal', target: 'hfsleuth'
