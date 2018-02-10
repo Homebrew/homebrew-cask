0a74e909fc9df2ddbcae13d101e2c8624aef745d
qinxin0720 <qinxin_0720@live.com>
2018-02-10 22:54:03 +0800

    Update folx to version 5.3.13709 (#43871)

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -1,10 +1,10 @@
 cask 'folx' do
-  version '5.2.1.13690'
-  sha256 'f00214b419756505410fefcf312c7d2fc2f81ed8e4ba134918c22232d23af1bd'
+  version '5.3.13709'
+  sha256 '3bfb5749ab2d774c8ec3224b19cdc8195dac566e629a9680382822833ec18142'
 
   url "https://cdn.eltima.com/download/folx-updater/downloader_mac_#{version}.dmg"
   appcast 'https://cdn.eltima.com/download/folx-updater/folx.xml',
-          checkpoint: 'f59ab26d33f3fa694a1d9d8898d19622cb2c6486b714e959e83156f8b7461dda'
+          checkpoint: '6aa8d8637d4e854dd8059a92f441986bd6e625365f7c218446429dcea9472bc8'
   name 'Folx'
   homepage 'https://mac.eltima.com/download-manager.html'
 
