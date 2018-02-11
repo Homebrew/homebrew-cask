e45d4520d3f8ff2855478980000f2cf6f9efbe43
Brandon Ooi <brandono@gmail.com>
2018-02-11 10:52:15 -0800

    Add comment that url and homepage differ.

--- Casks/etcher-cli.rb
+++ Casks/etcher-cli.rb
@@ -2,6 +2,7 @@ cask 'etcher-cli' do
   version '1.3.1'
   sha256 '6e862e2d2e64c264fb90ec409d82b43c1719453f5ed1cff18aa31b2d890ac6c7'
 
+  # github.com/resin-io/etcher/releases/download was verified as official when first introduced to the cask
   url "https://github.com/resin-io/etcher/releases/download/v#{version}/Etcher-cli-#{version}-darwin-x64.tar.gz"
   name 'Etcher CLI'
   homepage 'https://etcher.io/cli'
