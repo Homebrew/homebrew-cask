20ad23f29188c2510cab85b519cb3e37310ecb61
commitay <commitay@users.noreply.github.com>
2018-02-11 22:05:59 +1000

    Update roboform.rb

--- Casks/roboform.rb
+++ Casks/roboform.rb
@@ -1,20 +1,20 @@
 cask 'roboform' do
-  version :latest
-  sha256 :no_check
+  version '8.4.8'
+  sha256 '6792112bc8327b624259f574ab4e3a50cf00a376f74feb187c50f85b3c15595d'
 
   url 'https://www.roboform.com/dist/roboform-mac-v8.dmg'
+  appcast 'https://www.roboform.com/news-mac',
+          checkpoint: '14beeac423aae6ab16d18e54d6c824db79d2a9f1c6dc6114ebab168e3a43959f'
   name 'RoboForm'
   homepage 'https://www.roboform.com/'
 
   auto_updates true
-  depends_on macos: '>= :mavericks'
+  depends_on macos: '>= :yosemite'
 
   app 'RoboForm.app'
 
   zap trash: [
-               '~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion',
-               '~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion2',
-               '~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion3',
+               '~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion*',
                '~/Library/Application Support/RoboForm',
                '~/Library/Application Support/com.SiberSystems.RoboForm',
                '~/Library/Application Support/com.SiberSystems.RoboForm.RoboFormService',
@@ -24,10 +24,8 @@ cask 'roboform' do
                '~/Library/Caches/com.crashlytics.data/com.SiberSystems.RoboForm.RoboFormService',
                '~/Library/Caches/io.fabric.sdk.mac.data/com.SiberSystems.RoboForm',
                '~/Library/Caches/io.fabric.sdk.mac.data/com.SiberSystems.RoboForm.RoboFormService',
-               '~/Library/Containers/com.SiberSystems.RoboForm.safari-companion',
-               '~/Library/Containers/com.SiberSystems.RoboForm.safari-companion2',
-               '~/Library/Containers/com.SiberSystems.RoboForm.safari-companion3',
-               '~/Library/Group Containers/924KXEUXP3.com.SiberSystems.RoboForm',
+               '~/Library/Containers/com.SiberSystems.RoboForm.safari-companion*',
+               '~/Library/Group Containers/*.com.SiberSystems.RoboForm',
                '~/Library/Saved Application State/com.SiberSystems.RoboForm.savedState',
              ]
 end
