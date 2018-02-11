6b70422d87945914dea237d79e046e6c94071405
Lutz Roeder <lutzroeder@users.noreply.github.com>
2018-02-10 21:34:51 -0800

    Update netron to 1.4.4

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,10 +1,10 @@
 cask 'netron' do
-  version '1.4.3'
-  sha256 '7ddc4e28ba50d32fb5554006bac7ecdcca240fc6126b48956e222c87e3263a8a'
+  version '1.4.4'
+  sha256 'a9e897683e5ba0d8fd4b1e1a1a7dae14244e2df0976959920e0fb6e1594f0970'
 
   url "https://github.com/lutzroeder/Netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   appcast 'https://github.com/lutzroeder/Netron/releases.atom',
-          checkpoint: 'bfb3794e66d5103599f832df48c9ebd58b0b3d4b1d374666ed2589cf7b0a2d5e'
+          checkpoint: 'bdbfabe867d98e64b80683030df585ab47839cbb4c5fbb2966e06b9957b4b8f4'
   name 'Netron'
   homepage 'https://github.com/lutzroeder/Netron'
 
