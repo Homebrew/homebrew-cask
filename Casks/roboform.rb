c5a58f6dfaa9f6a28f1024700e6dfbe2812d3567
Alexander Groß <agross@therightstuff.de>
2018-02-11 12:34:52 +0100

    Add RoboForm Latest

---
+++ Casks/roboform.rb
@@ -0,0 +1,33 @@
+cask 'roboform' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.roboform.com/dist/roboform-mac-v8.dmg'
+  name 'RoboForm'
+  homepage 'https://www.roboform.com/'
+
+  auto_updates true
+  depends_on macos: '>= :mavericks'
+
+  app 'RoboForm.app'
+
+  zap trash: [
+               '~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion',
+               '~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion2',
+               '~/Library/Application Scripts/com.SiberSystems.RoboForm.safari-companion3',
+               '~/Library/Application Support/RoboForm',
+               '~/Library/Application Support/com.SiberSystems.RoboForm',
+               '~/Library/Application Support/com.SiberSystems.RoboForm.RoboFormService',
+               '~/Library/Caches/com.SiberSystems.RoboForm',
+               '~/Library/Caches/com.SiberSystems.RoboForm.RoboFormService',
+               '~/Library/Caches/com.crashlytics.data/com.SiberSystems.RoboForm',
+               '~/Library/Caches/com.crashlytics.data/com.SiberSystems.RoboForm.RoboFormService',
+               '~/Library/Caches/io.fabric.sdk.mac.data/com.SiberSystems.RoboForm',
+               '~/Library/Caches/io.fabric.sdk.mac.data/com.SiberSystems.RoboForm.RoboFormService',
+               '~/Library/Containers/com.SiberSystems.RoboForm.safari-companion',
+               '~/Library/Containers/com.SiberSystems.RoboForm.safari-companion2',
+               '~/Library/Containers/com.SiberSystems.RoboForm.safari-companion3',
+               '~/Library/Group Containers/924KXEUXP3.com.SiberSystems.RoboForm',
+               '~/Library/Saved Application State/com.SiberSystems.RoboForm.savedState',
+             ]
+end
