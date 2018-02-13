a96d13a0cb045295ac3e5290ca0e985e488e2a36
commitay <commitay@users.noreply.github.com>
2018-02-13 15:17:57 +1000

    Update backwords.rb

--- Casks/backwords.rb
+++ Casks/backwords.rb
@@ -2,7 +2,7 @@ cask 'backwords' do
   version '1.0.0'
   sha256 '102d00e333ef1c77e483d179e2276005e46c1de418c7004f08eba29579888044'
 
-  url 'https://backwords.fyi/static/builds/backwords-1.0.0.dmg'
+  url "https://backwords.fyi/static/builds/backwords-#{version}.dmg"
   name 'Backwords'
   homepage 'https://backwords.fyi/'
 
