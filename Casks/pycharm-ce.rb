e906f0371bdecc8733e80daf418596cb24ce663f
Shiquan Wang <shiquanwang@gmail.com>
2018-02-13 16:56:27 +0800

    Update pycharm-ce to 2017.3.3

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,5 +1,5 @@
 cask 'pycharm-ce' do
-  version '2017.3.3,173.4301.16'
+  version '2017.3.3'
   sha256 '22b76418c30da3a3b1a1a79417e69f1eddbe9482b31e6fb4b23efbd1eee83800'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.before_comma}.dmg"
