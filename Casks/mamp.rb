1b817d1f853f5b9ad7019d35a549eaf16243cf7e
Andrew Bergal <bergal@gmail.com>
2018-02-13 17:51:57 -0500

    Update mamp to 4.4.1

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -3,8 +3,8 @@ cask 'mamp' do
     version '3.5.2'
     sha256 'b935d118b6e14d43cf2570be2a05166a15b824c3edc0ede98cc30e6ab5af3697'
   else
-    version '4.4'
-    sha256 '9a86de2f1321851693bbf24f96c0aaca29fe7fb4d9b3c887aa464d41c36eed9c'
+    version '4.4.1'
+    sha256 'e7a83227a60d782d4ad8cea675834bb97cdd06718dc42f5f5e03e9c8944cac06'
   end
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
