d9ada3ef21526497f22abdd597f9c509d0248fb4
Hunter Braun <hunter.braun@gmail.com>
2018-02-12 23:10:15 -0600

    Add Backwords.app v1.0.0

---
+++ Casks/backwords.rb
@@ -0,0 +1,10 @@
+cask 'backwords' do
+  version '1.0.0'
+  sha256 '102d00e333ef1c77e483d179e2276005e46c1de418c7004f08eba29579888044'
+
+  url 'https://backwords.fyi/static/builds/backwords-1.0.0.dmg'
+  name 'Backwords'
+  homepage 'https://backwords.fyi/'
+
+  app 'Backwords.app'
+end
