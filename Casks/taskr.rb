85d4d1447a35a476b52a78a4aaf051e71095fe39
Brian Morton <brianmortondesign@gmail.com>
2018-02-13 10:33:23 -0800

    Update taskr to 1.0.0 (#43953)

--- Casks/taskr.rb
+++ Casks/taskr.rb
@@ -1,11 +1,11 @@
 cask 'taskr' do
-  version '0.4.0'
-  sha256 '34a5708790b1106196cc337a5c32ab00a998c404f8d4c1a67f73ba05bdb912cf'
+  version '1.0.0'
+  sha256 '6471c2337768dd2d58c231225a382536a1a0417d91caef68ebf1758a1ef1d4e9'
 
   # github.com/bukinoshita/taskr was verified as official when first introduced to the cask
   url "https://github.com/bukinoshita/taskr/releases/download/#{version}/taskr-#{version}-mac.zip"
   appcast 'https://github.com/bukinoshita/taskr/releases.atom',
-          checkpoint: '15a90c56346f1b469bda120e9cbd947b5ecab51bcb63fc28a16a553edb3e4eb3'
+          checkpoint: 'c92b3c149081305a131890732d15c78a0896e1c352fae683d6e22a2d3acc42dc'
   name 'Taskr'
   homepage 'https://taskr.now.sh/'
 
