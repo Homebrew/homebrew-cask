44cb088942fe4ec5a1401eb3bbfb7c64f101c485
commitay <commitay@users.noreply.github.com>
2018-02-15 09:34:04 +1000

    Update horos to 2.4.0 (#44018)

--- Casks/horos.rb
+++ Casks/horos.rb
@@ -6,13 +6,13 @@ cask 'horos' do
     version '2.0.2'
     sha256 '5cc1d6c71c8ae643b4df4fecee93dbe3cfacbcffef52001a76a7683a2725ac08'
   else
-    version '2.2.0'
-    sha256 'ea585fc74311db9e934ffb3b02829f25ea41f38cbc135e8e6507950240225c41'
+    version '2.4.0'
+    sha256 '79851d5acf7c7fe314d48437b47757db9339a7a101170ec796b12d0111532903'
   end
 
-  url "https://www.horosproject.org/wp-content/uploads/downloads/Horos#{version}.dmg"
+  url "https://horosproject.org/horos-content/Horos#{version}.dmg"
   name 'Horos – Free, open medical image viewer'
-  homepage 'https://www.horosproject.org/'
+  homepage 'https://horosproject.org/'
 
   app 'Horos.app'
 end
