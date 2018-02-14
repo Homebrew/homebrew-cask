a03a55b37e47e029150ec161d6589e98416c57b4
Ken <ken@mozartmbp.local>
2018-02-13 20:47:16 -0600

    Update mullvad to 67

--- Casks/mullvad.rb
+++ Casks/mullvad.rb
@@ -1,6 +1,6 @@
 cask 'mullvad' do
-  version '66'
-  sha256 'dc7b0da69c00ac5f632260d4990978cc928dd49ad323d5ba8070e9d21090cd52'
+  version '67'
+  sha256 '6bf7727467755eb0f1247721ce651fa35c8e485e57e244967495a343890cc44f'
 
   url "https://www.mullvad.net/media/client/Mullvad-#{version}.dmg"
   name 'Mullvad'
