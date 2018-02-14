d12bdf78485ce70c79b03ba9c8e7144124b52cbf
EricFromCanada <eric@lassosoft.com>
2018-02-14 17:33:18 -0500

    Fix hedgewars: use 0.9.22 on El Capitan

--- Casks/hedgewars.rb
+++ Casks/hedgewars.rb
@@ -1,6 +1,11 @@
 cask 'hedgewars' do
-  version '0.9.23'
-  sha256 '2a5fbfa005ec6aeea172270397025c17a2c117224dd21db5214b8cbbeade411b'
+  if MacOS.version <= :el_capitan
+    version '0.9.22'
+    sha256 'adc0b6dd3b47de115e85db1cb72841836444c0ebc77caee8139bfd6561e28fe8'
+  else
+    version '0.9.23'
+    sha256 '2a5fbfa005ec6aeea172270397025c17a2c117224dd21db5214b8cbbeade411b'
+  end
 
   url "http://www.hedgewars.org/download/releases/Hedgewars-#{version}.dmg"
   appcast 'https://www.hedgewars.org/download/appcast.xml',
