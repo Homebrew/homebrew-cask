440bb2a316ab8000b44b1f37ee8c6a8ea5c8bb36
commitay <commitay@users.noreply.github.com>
2018-02-14 22:06:48 +1000

    Update fldigi to 4.0.16 (#43990)

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -1,10 +1,10 @@
 cask 'fldigi' do
-  version '4.0.15'
-  sha256 '93158b435727876067e926f8706eb182fc672168dc530062d67fac65f0fb2c0f'
+  version '4.0.16'
+  sha256 '2105929d385a192f1e0168be1a6697234cf5998ce8902297ca8d1786b4b801e2'
 
   url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/fldigi/rss?path=/fldigi',
-          checkpoint: 'a117fb088fd46537e3d9d02d69776a01fe182b71811a72212390fda0c2227c8b'
+          checkpoint: '8b9dfcbeab09ec48fa03cb4d5840b6b8451dd72456197732d4646e9f124a744a'
   name 'fldigi'
   homepage 'https://sourceforge.net/projects/fldigi/files/fldigi/'
 
