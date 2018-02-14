6eb5f29e8012a954612fae389fe545982b12a19c
kwinke <kwinke@gmail.com>
2018-02-14 10:12:14 -0600

    Add Parallels Client.app v16.2.1 (#43977)

---
+++ Casks/parallels-client.rb
@@ -0,0 +1,13 @@
+cask 'parallels-client' do
+  version '16.2.1-19160'
+  sha256 'faef3493267c82b5125acaba95d654e54aeb02d8f32813139d10f1141151fcaf'
+
+  url "http://download.parallels.com/ras/v#{version.major}/#{version.hyphens_to_dots}/RasClient-Mac-#{version}.pkg"
+  name 'Parallels Client'
+  homepage 'https://www.parallels.com/products/ras/features/rdp-client/'
+
+  pkg "RasClient-Mac-#{version}.pkg"
+
+  uninstall pkgutil: 'com.2X.Client.Mac',
+            quit:    'com.2X.Client.Mac'
+end
