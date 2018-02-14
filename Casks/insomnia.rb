c4b677ed7a9bbf29f4c08dfec2fc10353a28c689
Brian Morton <brianmortondesign@gmail.com>
2018-02-14 09:10:08 -0800

    Update insomnia to 5.14.7

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,11 +1,11 @@
 cask 'insomnia' do
-  version '5.14.6'
-  sha256 '2a1242560ec878a56ea347afa91b28cb67c74920112f17f2692bded8fa52968a'
+  version '5.14.7'
+  sha256 'd02483e1320e9d490e681fe0732b8803e871b23989d35e850477bfeadfb0e1cd'
 
   # github.com/getinsomnia/insomnia was verified as official when first introduced to the cask
   url "https://github.com/getinsomnia/insomnia/releases/download/v#{version}/Insomnia-#{version}.dmg"
   appcast 'https://insomnia.rest/changelog/index.xml',
-          checkpoint: 'f76151b0f44d15ff135362f824b53270111907993dd5a3f8ab5a36e3b10cb4c2'
+          checkpoint: '061882d8217c71a0fe7e7ee633ba4cae0980ab05674a818d66279e2e04dd7cdb'
   name 'Insomnia'
   homepage 'https://insomnia.rest/'
 
