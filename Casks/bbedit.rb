6bb89ab496e3befaa80b3cfd800c95b03731e200
commitay <commitay@users.noreply.github.com>
2018-02-15 17:45:55 +1000

    https

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -3,7 +3,7 @@ cask 'bbedit' do
   sha256 '098bc306b17230e59ee54ae0a5ae327d534f075edb4905f3d9a990b521d5bf42'
 
   # s3.amazonaws.com/BBSW-download was verified as official when first introduced to the cask
-  url "http://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
+  url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
   appcast 'https://versioncheck.barebones.com/BBEdit.xml',
           checkpoint: '5b45e761294c946e7093d03b433af8795e75d314125141e3b840bab7935bddd5'
   name 'BBEdit'
