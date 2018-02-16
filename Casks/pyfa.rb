03a33aab1b734dd4a5115b94becbd781eb3f4301
that_lurker <pahakalle@protonmail.ch>
2018-02-17 00:23:48 +0200

    Update pyfa to 1.35.0,yc120.2-1.1

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,10 +1,10 @@
 cask 'pyfa' do
-  version '1.34.0,.arms.race-1.3'
-  sha256 '93c64b7fcee20c3dad94dbf7a6ca2ee5d48651f2119622a31e51a581960cd948'
+  version '1.35.0,yc120.2-1.1'
+  sha256 '0ebd34fa6d74c85470b9669d4b0caa6a08b9bdc134270b812e132747dce32379'
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version.before_comma}/pyfa-#{version.before_comma}-#{version.after_comma}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom',
-          checkpoint: 'd5b8613c5c0a368fe5456a2884e809c5ce3602cdfab9ffafca8a778e1915ebc2'
+          checkpoint: '698aa80a9b224094e5ecb71d332e547308789d9bd3fcb7f7d20e0a9e68e1720b'
   name 'pyfa'
   homepage 'https://github.com/pyfa-org/Pyfa'
 
