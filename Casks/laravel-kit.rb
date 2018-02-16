8bcfeec584cd81a5076d39fa20d84544cbb35c45
Brian Morton <brianmortondesign@gmail.com>
2018-02-16 13:52:13 -0800

    Update laravel-kit to 1.2.0 (#44071)

--- Casks/laravel-kit.rb
+++ Casks/laravel-kit.rb
@@ -1,10 +1,10 @@
 cask 'laravel-kit' do
-  version '1.1.0'
-  sha256 '58e12c1934d1e9d2eb474dab13ba40c282fa3f1e8e20251c5c488e7df1adc56b'
+  version '1.2.0'
+  sha256 '7836d5dffbee19efb6d5b86d817045fecf84106c553160461add4226adf28722'
 
   url "https://github.com/tarequemdhanif/laravel-kit/releases/download/v#{version}/laravel-kit-#{version}-mac.zip"
   appcast 'https://github.com/tarequemdhanif/laravel-kit/releases.atom',
-          checkpoint: '32456f9dffdbdc7b4334a55ac258f4fd3780223f33e16876276c94449a09514f'
+          checkpoint: '4d87a5800107e917d622e0bbd25bf3af6c26fc67a9107490eb21ab5e1b63df07'
   name 'Laravel Kit'
   homepage 'https://github.com/tarequemdhanif/laravel-kit'
 
