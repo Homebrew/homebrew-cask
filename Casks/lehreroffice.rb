dcb1f81ebaacf96cb7ab49ee0f92bc19497279ff
Anver Housseini <contact@anver.ch>
2018-02-16 04:35:01 +0100

    Update lehreroffice to 2018.1.2 (#44050)

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,10 +1,10 @@
 cask 'lehreroffice' do
-  version '2018.1.1'
-  sha256 '368a68a3e3a9bc34a645fc4abe444d15fe9ba010995938d7bfda6470dc8596e6'
+  version '2018.1.2'
+  sha256 'caccb2d5acd478b02b21513036db4589291ccef7015ba69030fe6cb58d703388'
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_osx.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop',
-          checkpoint: 'bd9aea9ce46c817c7fb0c4be6f91613491daba7b727db91fb53a14c4c612a7e0'
+          checkpoint: '3f6abfaa7a0af13d02b27553e85584cdb9130c2e07bcb549afae2bffdffb9c14'
   name 'LehrerOffice'
   homepage 'https://www.lehreroffice.ch/'
 
