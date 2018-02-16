6e59040adbaf7359422145fabb62c7b3875f5cbd
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-16 03:37:01 +0000

    Update aqua-data-studio to 18.5.0 (#44045)
    * Update aqua-data-studio to 18.5.0
    
    * Update aqua-data-studio.rb

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -2,9 +2,7 @@ cask 'aqua-data-studio' do
   version '18.5.0'
   sha256 '744955217e65c21d64ed0b7f5974e6cb75fb66ef2e2bde59c8ad384b7169d140'
 
-  url "http://www.aquafold.com/download/v#{version.major_minor}.0/osx/ads-osx-#{version}.tar.gz"
-  appcast 'http://www.aquafold.com/download/',
-          checkpoint: '5ff24a33622616e458cffce5236e0605e8d46a9ffcf5a0a6ab5ad768496587ff'
+  url "http://downloads.aquafold.com/v#{version.major_minor}.0/osx/ads-osx-#{version}.tar.gz"
   name 'Aquafold Aqua Data Studio'
   homepage 'http://www.aquafold.com/aquadatastudio.html'
 
