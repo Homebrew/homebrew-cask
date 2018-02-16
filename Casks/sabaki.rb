f90df0ab260da7a93911d149fb10e6b6160b8846
Adrian Petrescu <apetresc@gmail.com>
2018-02-16 12:14:32 -0500

    Update sabaki to 0.33.3 (#44063)

--- Casks/sabaki.rb
+++ Casks/sabaki.rb
@@ -1,11 +1,11 @@
 cask 'sabaki' do
-  version '0.33.2'
-  sha256 'f14b623b0e2b98b3a6ffd5dbc561381c271855b3c24b933df7e46c00b84ff629'
+  version '0.33.3'
+  sha256 '4d6edad6c772bb4a58b9f1bf652f228c0df5d19f69dc0f2a46b35203c40efb3d'
 
   # github.com/yishn/Sabaki was verified as official when first introduced to the cask
   url "https://github.com/yishn/Sabaki/releases/download/v#{version}/sabaki-v#{version}-mac-x64.7z"
   appcast 'https://github.com/yishn/Sabaki/releases.atom',
-          checkpoint: 'f35b751fa7be75fe2d61118d232d07759293505640720948729719ad92b156f0'
+          checkpoint: 'b601399f75065337978b026b57d39a2e91e1a91aa0108c85252ffeb3cf081308'
   name 'Sabaki'
   homepage 'http://sabaki.yichuanshen.de/'
 
