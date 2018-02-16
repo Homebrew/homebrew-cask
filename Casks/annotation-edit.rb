6e347093076631ab05de0bd0631058fdd622f201
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2018-02-16 12:04:47 -0500

    Add annotation edit 1.9.91

---
+++ Casks/annotation-edit.rb
@@ -0,0 +1,10 @@
+cask 'annotation-edit' do
+  version '1.9.91'
+  sha256 '2a1bbced6a82677a2f4f8f44dcf528c69885103d0a46cd98cd33d04d64e09d07'
+
+  url 'http://www.zeitanker.com/common/Annotation_Edit.zip'
+  name 'Annotation Edit'
+  homepage 'http://www.zeitanker.com/content/tools/zeitanker_tools/zeitanker_annotation_edit/'
+
+  app "Annotation Edit #{version}"
+end
