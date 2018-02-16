4b2487c83c1c3bd1b6a13ecbdb43e4cd82401999
commitay <commitay@users.noreply.github.com>
2018-02-16 15:19:15 +1000

    Update opensc.rb

--- Casks/opensc.rb
+++ Casks/opensc.rb
@@ -5,7 +5,7 @@ cask 'opensc' do
   url "https://github.com/OpenSC/OpenSC/releases/download/#{version}/OpenSC-#{version}.dmg"
   appcast 'https://github.com/OpenSC/OpenSC/releases.atom',
           checkpoint: '064e2a4870fa8f8989661fc602f396220906cea5c4d4e1383f92108a757bdeef'
-  name 'opensc'
+  name 'OpenSC'
   homepage 'https://github.com/OpenSC/OpenSC/wiki'
 
   pkg "OpenSC #{version}.pkg"
