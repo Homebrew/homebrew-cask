50dba94001b99a91a95bdb7280300f2ce10f18f2
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2018-02-16 11:30:38 -0500

    Add sirimote latest

---
+++ Casks/sirimote.rb
@@ -0,0 +1,10 @@
+cask 'sirimote' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://eternalstorms.at/sirimote/SiriMote.zip'
+  name 'SiriMote'
+  homepage 'http://eternalstorms.at/sirimote'
+
+  app 'SiriMote.app'
+end
