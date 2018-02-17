03fd04ccd9e38b2d778d630745684df7cefdfd38
commitay <commitay@users.noreply.github.com>
2018-02-18 09:45:52 +1000

    Update fish.rb

--- Casks/fish.rb
+++ Casks/fish.rb
@@ -2,7 +2,7 @@ cask 'fish' do
   version '2.7.1'
   sha256 'd37f093702ef7f05e11333ceb8a1a859452395720b75f5bbbe4851e20157f452'
 
-  # github.com/fish-shell/fish-shell/releases was verified as official when first introduced to the cask
+  # github.com/fish-shell/fish-shell was verified as official when first introduced to the cask
   url "https://github.com/fish-shell/fish-shell/releases/download/#{version}/fish-#{version}.app.zip"
   appcast 'https://fishshell.com/release_notes.html',
           checkpoint: 'd4345708490da3b072c98441c94e7b2c379d9dba141ed0e602e44f81624b62c3'
