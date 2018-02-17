c36b7f1dc6e9b5c96c4fccfb4c0e90e42e8b6678
Samuele Decarli <decarlis@student.ethz.ch>
2018-02-17 22:24:33 +0100

    Update qt-creator to 4.5.1 (#44090)

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator' do
-  version '4.5.0'
-  sha256 'cb3aac65ddadaccef1bf62a95120f8d74d3e4454f7afcdcb5425f20051d24b5b'
+  version '4.5.1'
+  sha256 '0e2fa581aeaf9b2fb502f613c2e9e2fb705cf1565945326b97bd8539822faa7f'
 
   url "http://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator'
