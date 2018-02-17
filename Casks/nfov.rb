cbb3af77edd16ddc04978350c3a5e24608d59daf
nrlquaker <ustynov.vy@gmail.com>
2018-02-17 09:58:24 +0200

    Update nfov to 1.0.7

--- Casks/nfov.rb
+++ Casks/nfov.rb
@@ -1,10 +1,10 @@
 cask 'nfov' do
-  version '1.0.6'
-  sha256 '8e535cc342de50b8b6f76cef91c41df2fbb04aaf440100f69ded1953a3c40aab'
+  version '1.0.7'
+  sha256 'b3b57db8d14ea24a5ada289fcda36d4e090bc6ec7cf04aef356c0ce96c2067eb'
 
   url "https://github.com/nrlquaker/nfov/releases/download/v#{version}/nfov-darwin-x64-#{version}.zip"
   appcast 'https://github.com/nrlquaker/nfov/releases.atom',
-          checkpoint: 'd56cd52fb1b00c8b3e4a210c4f049811752b2a0f146b30d1296544a163be52a6'
+          checkpoint: 'cb0c968bf40baf345c5715897a4de2db972245e37d06c0ce178ee396dcb5eee8'
   name 'nfov'
   homepage 'https://github.com/nrlquaker/nfov'
 
@@ -13,6 +13,7 @@ cask 'nfov' do
   zap trash: [
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.electron.nfov.sfl*',
                '~/Library/Application Support/nfov',
+               '~/Library/Logs/nfov',
                '~/Library/Preferences/com.electron.nfov.helper.plist',
                '~/Library/Preferences/com.electron.nfov.plist',
                '~/Library/Saved Application State/com.electron.nfov.savedState',
