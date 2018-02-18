932a3b5c7470a9541784f8251c656d4c1158054a
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-18 22:17:16 +0000

    Update canary to 1.8.1,396 (#44126)

--- Casks/canary.rb
+++ Casks/canary.rb
@@ -1,11 +1,11 @@
 cask 'canary' do
-  version '1.8.1,392'
-  sha256 '530006d36d58693c7f1f964d184d01facf159375aea73a3ff44ade291d8b3833'
+  version '1.8.1,396'
+  sha256 'c1950673c9975584dfa478867d1299c5ff78e3d296e97326d2674d88b593f460'
 
   # rink.hockeyapp.net/api was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/b36ac4a380ea4907940c2054f6163050',
-          checkpoint: '0f221a1d55db589f50b768d6c2a18a7ab661d7014ea01bd134c815ce03377205'
+          checkpoint: 'ab95777dfa75cacf26d1bce7d4f8290ee16e2cdee2a618cb44d8157c72cb60bb'
   name 'Canary'
   homepage 'https://canarymail.io/'
 
