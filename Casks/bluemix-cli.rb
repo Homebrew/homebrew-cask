424774d3976857ab8073e4538dc5daf2284e89c8
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-18 19:43:59 +0000

    Update bluemix-cli to 0.6.5

--- Casks/bluemix-cli.rb
+++ Casks/bluemix-cli.rb
@@ -1,15 +1,16 @@
 cask 'bluemix-cli' do
-  version '0.6.2'
-  sha256 '383a920469908b2371651fa4e181d0d071d7f0590ac9e5594df77c7fc5e3f25e'
+  version '0.6.5'
+  sha256 '718a6dd70b3bc1af43c9371bf0f432ffb2918c8defb26b4de7a554aef9c8fdf4'
 
   # public.dhe.ibm.com was verified as official when first introduced to the cask
-  url "https://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/#{version}/Bluemix_CLI_#{version}.pkg"
-  appcast 'https://github.com/IBM-Bluemix/bluemix-cli-release/releases.atom',
-          checkpoint: 'a8b4534e53a5e92e1092fa12f436d3951ba33ecfd52eab99359790fa30543fb5'
+  url "https://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/#{version}/IBM_Cloud_CLI_#{version}.pkg"
+  appcast 'https://github.com/IBM-Cloud/ibm-cloud-cli-release/releases.atom',
+          checkpoint: 'db99ee28d9d8bf308abe04b856c056488f84c3aac62e66bf2c87c632c1bc7c96'
   name 'Bluemix-CLI'
+  name 'IBM Cloud CLI'
   homepage 'https://clis.ng.bluemix.net/ui/home.html'
 
-  pkg "Bluemix_CLI_#{version}.pkg"
+  pkg "IBM_Cloud_CLI_#{version}.pkg"
 
   uninstall_postflight do
     if File.exist?('/etc/profile')
@@ -28,7 +29,7 @@ cask 'bluemix-cli' do
     system('/usr/bin/sed', '-E', '-i', '.bluemix_uninstall_bak', '-e', '/^### Added by the Bluemix CLI$/d', '-e', '/^source \/usr\/local\/Bluemix\/bx\/zsh_autocomplete$/d', "#{ENV['HOME']}/.zshrc") if File.exist?("#{ENV['HOME']}/.zshrc")
   end
 
-  uninstall pkgutil: 'com.ibm.bluemix.cli',
+  uninstall pkgutil: 'com.ibm.cloud.cli',
             delete:  [
                        '/usr/local/Bluemix',
                        '/usr/local/bin/bx',
