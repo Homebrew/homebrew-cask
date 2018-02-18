c6f5f43d9891525fb569fc8d720b69066c976d20
Vítor Galvão <info@vitorgalvao.com>
2018-02-18 17:38:24 +0000

    Update gitfinder.rb

--- Casks/gitfinder.rb
+++ Casks/gitfinder.rb
@@ -2,7 +2,7 @@ cask 'gitfinder' do
   version '1.0'
   sha256 'ddbc1410e24be56ef10c1deceeb8af07bb7156e10704a5bae3e8fa2806cef585'
 
-  # zigz.ag was verified as official when first introduced to the cask
+  # zigz.ag/GitFinder was verified as official when first introduced to the cask
   url 'https://zigz.ag/GitFinder/updates/GitFinder.dmg'
   appcast 'https://zigz.ag/GitFinder/updates/stablecast.xml',
           checkpoint: 'a53bc8279d1b10e0c76d00d3c0bd29c606f1054c88e9900bd54621c0c1508929'
