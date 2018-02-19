f3a20dc8fd4ea8864d43c8debbcc10f88dd8416e
fr0zn <fr0zn@protonmail.com>
2018-02-19 19:10:40 +0100

    Add Nimble Commander :latest
    Added Nimble Commander to the cask repo. Using the latest available
    version from the official web.

---
+++ Casks/nimble-commander.rb
@@ -0,0 +1,10 @@
+cask 'nimble-commander' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://magnumbytes.com/downloads/releases/nimble-commander.dmg'
+  name 'Nimble Commander'
+  homepage 'http://magnumbytes.com/'
+
+  app 'Nimble Commander.app'
+end
