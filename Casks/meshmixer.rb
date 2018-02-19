b1590f2de448c5ef686a97f8a50897a75b011221
commitay <commitay@users.noreply.github.com>
2018-02-20 07:47:44 +1000

    Update meshmixer.rb

--- Casks/meshmixer.rb
+++ Casks/meshmixer.rb
@@ -2,13 +2,15 @@ cask 'meshmixer' do
   version '3p4'
   sha256 '2a08316e18be5a8c688747d49e4ef4d09565ba298ecaff1d0cd8e2280f7ea373'
 
-  # https://s3.amazonaws.com/meshmixer.release/ was verified as official when first introduced to the cask
+  # s3.amazonaws.com/meshmixer.release was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/meshmixer.release/Autodesk_Meshmixer_v#{version}_macOS.pkg"
   name 'MeshMixer'
-  homepage 'meshmixer.com'
+  homepage 'https://meshmixer.com/'
 
-  pkg 'Autodesk_Meshmixer_v3p4_macOS.pkg'
+  pkg "Autodesk_Meshmixer_v#{version}_macOS.pkg"
 
-  uninstall pkgutil: ['com.meshmixer.libmodels.pkg',
-                      'com.meshmixer.meshmixer09.pkg']
+  uninstall pkgutil: [
+                       'com.meshmixer.libmodels.pkg',
+                       'com.meshmixer.meshmixer09.pkg',
+                     ]
 end
