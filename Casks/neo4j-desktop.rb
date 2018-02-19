4dc2302c060ab8a624ea40d2ce07e7431d4a625a
Karun Japhet <karun@japhet.in>
2018-02-19 15:50:04 -0500

    Add Neo4j Desktop (latest: v1.0.14)

---
+++ Casks/neo4j-desktop.rb
@@ -0,0 +1,10 @@
+cask 'neo4j-desktop' do
+  version '1.0.14'
+  sha256 'ff53133320b56e4e9ade8365a98a1cc78ee65b13e63c4b0c29eb04b5055b5727'
+
+  url "https://neo4j.com/artifact.php?name=neo4j-desktop-offline-#{version}.dmg"
+  name 'Neo4j Desktop'
+  homepage 'https://neo4j.com/download/'
+
+  app 'Neo4j Desktop.app'
+end
