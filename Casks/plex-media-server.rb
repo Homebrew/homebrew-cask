7a803fbbead82af4ae4b55fcb6aea46df0182572
Bob Olde Hampsink <boboldehampsink@Bobs-iMac.local>
2018-02-19 20:47:38 +0100

    Update plex-media-server to 1.11.3.4803-c40bba82e

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,10 +1,10 @@
 cask 'plex-media-server' do
-  version '1.10.1.4602-f54242b6b'
-  sha256 '9fb5aa288e75b1a7b49c54536c83db2ea4527ad11f3f15599f38ebe31e74a764'
+  version '1.11.3.4803-c40bba82e'
+  sha256 '26c00e66ec472f5e73d810c38b22749c9748cec48d5e145764445adb5cb50249'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   appcast 'https://plex.tv/api/downloads/1.json',
-          checkpoint: '71071b2d93a0d65b2d047e70fde47204374507e302cfab13ca9419331f028304'
+          checkpoint: '28fc6257dcb7faeb2e79917315a710609eb24e1311a56d37561b79e89ef779a6'
   name 'Plex Media Server'
   homepage 'https://www.plex.tv/'
 
