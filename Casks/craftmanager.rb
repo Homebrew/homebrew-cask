adc603f7d878e1374b466e18382483492cf81def
Scott Smith <mail@ScottHSmith.com>
2018-02-19 21:07:26 -0800

    Update craftmanager to 1.0.48

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -1,10 +1,10 @@
 cask 'craftmanager' do
-  version '1.0.47'
-  sha256 'cf63fdf37a3bb75fc77a66cce56b9a36693b3dbbc3ca66d6a769f5fd63ba657d'
+  version '1.0.48'
+  sha256 '718eb692962db70df589b3888906e075c0c5a5d1d69ed42710e7fcfb5706866b'
 
   url 'https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip'
   appcast 'https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml',
-          checkpoint: '0ec9c24b9d33602272046b77132c2d7c2bf83f2448e95dba0b9e9e9b72bfbc46'
+          checkpoint: '3550d48e7ae3f897ad3fd44b6c20d77c7baaee7f0dba0d8527a608b08c97a58e'
   name 'CraftManager'
   homepage 'https://www.invisionapp.com/craft'
 
