57368015d73aaa476a4ef63e810e7adeea539827
Brian Morton <brianmortondesign@gmail.com>
2018-02-20 13:44:30 -0800

    Update station to 1.6.0 (#44181)

--- Casks/station.rb
+++ Casks/station.rb
@@ -1,11 +1,11 @@
 cask 'station' do
-  version '1.5.1'
-  sha256 'ff86b548b63d93271f35ffe044843b63582373b51e9607235f8cab1dbad3ac49'
+  version '1.6.0'
+  sha256 '1ba2a0ceab7647c40bdada1670455522ffb8147b7fc3cecbe4853d00952e73d1'
 
   # github.com/getstation/desktop-app-releases was verified as official when first introduced to the cask
   url "https://github.com/getstation/desktop-app-releases/releases/download/#{version}/Station-#{version}-mac.zip"
   appcast 'https://github.com/getstation/desktop-app-releases/releases.atom',
-          checkpoint: 'affdfe3a38f667d3af8a9badee352bf935f89dd6deab5ca0afa28a0a6c0a550a'
+          checkpoint: '924b5134d34166d20c88109974b10c4c1e699d9e3b0d94c643eca716ea37330e'
   name 'Station'
   homepage 'https://getstation.com/'
 
