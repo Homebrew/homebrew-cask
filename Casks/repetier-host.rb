3d75651ac6a7a0e52c8faba0d84687df4fd4bfee
Iván Barcia <ivanbarciafernandez@gmail.com>
2018-02-20 17:32:50 +0100

    Update repetier-host to 1.1.0

--- Casks/repetier-host.rb
+++ Casks/repetier-host.rb
@@ -1,6 +1,6 @@
 cask 'repetier-host' do
-  version '1.0.1'
-  sha256 '14e7cb94e9078df23c2b8ac96e559187062b6b159b351cc8c7eb3254cb89a706'
+  version '1.1.0'
+  sha256 'cf65d6d89654b68bb3202d78ea2b0e61e33496e9e1b00fea5844253d795232c8'
 
   url "https://download1.repetier.com/files/host-mac/Repetier-Host-Mac_#{version.dots_to_underscores}.dmg"
   appcast 'https://www.repetier.com/updates/rhm/rhm-appcast.xml',
