4248bac9aebc8fd6a70bb34b5d8d36d65a850228
404NetworkError <404NetworkError@users.noreply.github.com>
2018-02-21 10:15:58 -0600

    Update ivpn appcast

--- Casks/ivpn.rb
+++ Casks/ivpn.rb
@@ -3,6 +3,8 @@ cask 'ivpn' do
   sha256 'c5837cbad898e3e5bbc7d9d9a32a64710076a8a9dfdf2959f4c9a076643e0cc5'
 
   url "https://www.ivpn.net/releases/osx/IVPN-#{version}.dmg"
+  appcast 'https://www.ivpn.net/setup/mac-changelog.html',
+          checkpoint: 'b5ee88531292fc857b70a65c7bb11d253d230a56f13d285b2fe15b0ae095d475'
   name 'IVPN'
   homepage 'https://www.ivpn.net/apps-macos'
 
