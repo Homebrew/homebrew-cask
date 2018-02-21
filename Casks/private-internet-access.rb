df1d172fc15420609630ef14304dcdcc06c1c7b5
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2018-02-21 09:44:11 +0100

    Update Private Internet Access 77 (#44197)

--- Casks/private-internet-access.rb
+++ Casks/private-internet-access.rb
@@ -1,6 +1,6 @@
 cask 'private-internet-access' do
-  version '75'
-  sha256 'c1a01cf56cfc593ef1a27a6cdd318f96a02359e68d16aa6dca9b19221f96055b'
+  version '77'
+  sha256 '7a31b46d03c1d59a1fd420ade299d329141e75212ab76c62453a8a1ac7e946c5'
 
   url "https://installers.privateinternetaccess.com/download/pia-v#{version}-installer-mac.dmg"
   name 'Private Internet Access'
