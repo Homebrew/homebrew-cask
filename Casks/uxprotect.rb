a8442884c6ecd887d4a7bbf55fff80725fb1d277
Rifai <abdelrahman.rifai@gmail.com>
2018-02-21 02:01:20 +0200

    Update uxprotect to 1.1

--- Casks/uxprotect.rb
+++ Casks/uxprotect.rb
@@ -1,10 +1,10 @@
 cask 'uxprotect' do
-  version '1.0'
-  sha256 '264529e914e4d5c859ebd6f2de5778f3149cf7dcdd3829f4b7338cb9287546ec'
+  version '1.1'
+  sha256 '89772d60ef2ade2ba4668f7517300962dc27c0feb530a21cdb6ee599bcfa468f'
 
   url "https://digitasecurity.com/download/uxprotect/UXProtect#{version}.zip"
   appcast 'https://digitasecurity.com/product/uxprotect/',
-          checkpoint: 'd7c9ac945981eedf97e05dd1f5fe4af2e341fb4856697ff931cc87e94d31cbcf'
+          checkpoint: '318c8fa6313a94dc6d1cef988efe2332265b1e1e8633c724b4b467e87d07bba8'
   name 'UXProtect'
   homepage 'https://digitasecurity.com/product/uxprotect/'
 
