f893b5838acf7293b1260299a50f81358acfe36b
commitay <commitay@users.noreply.github.com>
2018-02-22 17:38:07 +1000

    Update vaudtax.rb

--- Casks/vaudtax.rb
+++ Casks/vaudtax.rb
@@ -3,11 +3,11 @@ cask 'vaudtax' do
   sha256 'bf2aca64ee441fc4628b90cc829f5bb2f4748bc5458ae0fb4ca923e96645d28e'
 
   url "https://vaudtax-dl.vd.ch/vaudtax#{version}/telechargement/mac/VaudTax_#{version}.dmg"
-  name 'VaudTax 2017'
+  name 'VaudTax'
   homepage 'https://www.vd.ch/themes/etat-droit-finances/impots/vaudtax/'
 
   auto_updates true
-  depends_on macos: '>= 10.10'
+  depends_on macos: '>= :yosemite'
 
-  app 'VaudTax 2017.app'
+  app "VaudTax #{version}.app"
 end
