93507cc9cf6b80f8c070f3c79d9de92051c85a3a
Ben <BenjaminHCCarr@users.noreply.github.com>
2018-02-22 21:53:59 -0500

    Update zoomus to 4.1.20446.0209 (#44246)
    new version and shasum

--- Casks/zoomus.rb
+++ Casks/zoomus.rb
@@ -1,6 +1,6 @@
 cask 'zoomus' do
-  version '4.1.20199.0205'
-  sha256 '174fe622b484222df58e80cdfff3a18ed4ca9353ec8b9f7ff827bcb119fc7a9c'
+  version '4.1.20446.0209'
+  sha256 '87f26cbf531472db043802c97ebb8447459a3e32290199497a6da6a7d9b024f8'
 
   url "https://www.zoom.us/client/#{version}/zoomusInstaller.pkg"
   name 'Zoom.us'
