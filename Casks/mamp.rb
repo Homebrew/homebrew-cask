d5cf2727005012b70d7f8a43a465de004b2e55ca
Viktor Vasilev <liberalcho@gmail.com>
2018-02-23 20:55:59 +0200

    Fix MAMP 4.4.1 checksum (#44087)

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -4,7 +4,7 @@ cask 'mamp' do
     sha256 'b935d118b6e14d43cf2570be2a05166a15b824c3edc0ede98cc30e6ab5af3697'
   else
     version '4.4.1'
-    sha256 'e7a83227a60d782d4ad8cea675834bb97cdd06718dc42f5f5e03e9c8944cac06'
+    sha256 '5eab5e3bcca0e7c9641d4cf21c7ee4a5698fa0c2ed7980cdb6a728de7857084b'
   end
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
