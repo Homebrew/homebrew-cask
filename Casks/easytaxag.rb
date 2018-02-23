e4fd09f64a686fc25459f838774ccba378b007a6
Anver Housseini <contact@anver.ch>
2018-02-23 21:39:48 +0100

    Add easytaxagrb v2017,1.1

---
+++ Casks/easytaxag.rb
@@ -0,0 +1,24 @@
+cask 'easytaxag' do
+  version '2017,1.1'
+  sha256 'a488bf84734f45d0c95e017aa05e04ec4baf33f758eab368801d1b4406df133d'
+
+  url "https://msg-easytax.ch/ag/#{version.before_comma}/EasyTax2017AG_macos_#{version.after_comma.dots_to_underscores}_mitJRE.dmg"
+  name "EasyTax #{version.before_comma} AG"
+  homepage 'https://msg-easytax.ch/'
+
+  preflight do
+    system_command "#{staged_path}/EasyTax 2017 AG Installationsprogramm.app/Contents/MacOS/JavaApplicationStub",
+                   args:         [
+                                   '-q',
+                                 ],
+                   print_stderr: false
+  end
+
+  uninstall_preflight do
+    system_command "#{staged_path}/EasyTax 2017 AG Deinstallationsprogramm.app/Contents/MacOS/JavaApplicationStub",
+                   args:         [
+                                   '-q',
+                                 ],
+                   print_stderr: false
+  end
+end
