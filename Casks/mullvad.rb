95e45284c9654e7c499702c0486608499c506ffe
David Yantis <404NetworkError@users.noreply.github.com>
2018-02-23 04:35:48 -0600

    Update mullvad 67 appcast and PGP key (#44201)
    * Update mullvad 67 PGP key
    
    * Update mullvad 67 appcast

--- Casks/mullvad.rb
+++ Casks/mullvad.rb
@@ -3,8 +3,11 @@ cask 'mullvad' do
   sha256 '6bf7727467755eb0f1247721ce651fa35c8e485e57e244967495a343890cc44f'
 
   url "https://www.mullvad.net/media/client/Mullvad-#{version}.dmg"
+  appcast 'https://mullvad.net/media/CHANGES.rst',
+          checkpoint: '0f8edb7e8d4287a38402b6ab26f08947403782cf01f0a64d4711ca44df8faf68'
   name 'Mullvad'
   homepage 'https://mullvad.net/'
+  gpg "#{url}.asc", key_id: 'a1198702fc3e0a09a9ae5b75d5a1d4f266de8ddf'
 
   app 'Mullvad.app'
 end
