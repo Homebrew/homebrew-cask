2c48279626587c956c0481bc908f6578f743c64c
Anver Housseini <contact@anver.ch>
2018-02-23 21:45:41 +0100

    Update easytaxag.rb

--- Casks/easytaxag.rb
+++ Casks/easytaxag.rb
@@ -3,11 +3,11 @@ cask 'easytaxag' do
   sha256 'a488bf84734f45d0c95e017aa05e04ec4baf33f758eab368801d1b4406df133d'
 
   url "https://msg-easytax.ch/ag/#{version.before_comma}/EasyTax2017AG_macos_#{version.after_comma.dots_to_underscores}_mitJRE.dmg"
-  name "EasyTax #{version.before_comma} AG"
+  name "EasyTax AG"
   homepage 'https://msg-easytax.ch/'
 
   preflight do
-    system_command "#{staged_path}/EasyTax 2017 AG Installationsprogramm.app/Contents/MacOS/JavaApplicationStub",
+    system_command "#{staged_path}/EasyTax #{version.before_comma} AG Installationsprogramm.app/Contents/MacOS/JavaApplicationStub",
                    args:         [
                                    '-q',
                                  ],
@@ -15,7 +15,7 @@ cask 'easytaxag' do
   end
 
   uninstall_preflight do
-    system_command "#{staged_path}/EasyTax 2017 AG Deinstallationsprogramm.app/Contents/MacOS/JavaApplicationStub",
+    system_command "#{staged_path}/EasyTax #{version.before_comma} AG Deinstallationsprogramm.app/Contents/MacOS/JavaApplicationStub",
                    args:         [
                                    '-q',
                                  ],
