51a10f81e530fa9620006c5c5d38eebb55905e36
danielh96 <daniel.hanlon@ucdconnect.ie>
2018-02-23 21:49:31 +0000

    Update soda-player to 1.3.3

--- Casks/soda-player.rb
+++ Casks/soda-player.rb
@@ -1,6 +1,6 @@
 cask 'soda-player' do
-  version '1.3.2'
-  sha256 'aaba823b2403f4fb60a2aa3356e5aec659bc850f25a097ad5b46cf1eccd0549d'
+  version '1.3.3'
+  sha256 '95c68ac0681a214d7ede28f36336a8a95cee710217408d62f372eab662ae8420'
 
   url "https://releases.sodaplayer.com/mac/Soda%20Player%20#{version}.dmg"
   name 'Soda Player'
