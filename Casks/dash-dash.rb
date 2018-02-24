bdc05beffd2efe9c67ba10fc998f3aaa5c5a39e4
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-24 16:07:31 +0000

    Update dash-dash to 0.12.2.3

--- Casks/dash-dash.rb
+++ Casks/dash-dash.rb
@@ -1,11 +1,11 @@
 cask 'dash-dash' do
-  version '0.12.2.2'
-  sha256 '53fe3ec4d0572d906c7ef0f47deaa3c1c18ba57ba3f2121eb18d836f1201aa1c'
+  version '0.12.2.3'
+  sha256 '90ca27d6733df6fc69b0fc8220f2315623fe5b0cbd1fe31f247684d51808cb81'
 
   # github.com/dashpay/dash was verified as official when first introduced to the cask
   url "https://github.com/dashpay/dash/releases/download/v#{version}/dashcore-#{version}-osx.dmg"
   appcast 'https://github.com/dashpay/dash/releases.atom',
-          checkpoint: 'ed223b7aca79e10abb51ee21e3e8232962dc63072cfeec4a9b41ae56ff1d34d0'
+          checkpoint: '0b3aafa287ffd57f2e1f5cb672b449c655166e3249f976848ccccb0852822f78'
   name 'Dash'
   homepage 'https://www.dash.org/'
 
