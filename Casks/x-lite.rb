450f4ebad80602208d22c6953c46d70c9d22fbcc
SteamedFish <SteamedFish@users.noreply.github.com>
2018-02-24 14:08:42 +0800

    Update x-lite to 5.2.0_90533 (#44276)

--- Casks/x-lite.rb
+++ Casks/x-lite.rb
@@ -1,6 +1,6 @@
 cask 'x-lite' do
-  version '5.1.0_89320'
-  sha256 '33ec321d198c44b7d5b764d6eab8a55fdb3131462b4310d2b508f3063da0f9d3'
+  version '5.2.0_90533'
+  sha256 'd384e91a57881558950832b1918228596a3d3acb52ed5814acf91e732d851d6b'
 
   # counterpath.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://counterpath.s3.amazonaws.com/downloads/X-Lite_#{version}.dmg"
