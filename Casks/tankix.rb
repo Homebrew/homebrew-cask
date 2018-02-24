2c35d86f452cad287c555b96ecbb574dd060c6cc
Vítor Galvão <info@vitorgalvao.com>
2018-02-24 17:14:40 +0000

    Update tankix.rb

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,8 +1,8 @@
 cask 'tankix' do
-  version '13644'
+  version 'hydra3-13644'
   sha256 'ef9a268e9a38ee853c6d4838b8415603983899b3e019a52663890123668cf0e6'
 
-  url "http://static.tankix.com/app/StandaloneOSXIntel64/hydra3-#{version}/TankiX.dmg"
+  url "http://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
   name 'Tanki X'
   homepage 'https://www.tankix.com/'
 
