30ccbee32f73b3562d1749dc27157edb95a44c50
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-24 18:29:07 +0000

    Update tankix to 13644 (#44289)
    * Update tankix to 13644
    
    * Update tankix.rb

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,8 +1,8 @@
 cask 'tankix' do
-  version '13571'
-  sha256 '8e21f38af3f4ac7234b2c7c6184f1a5930090c73296d329fdd8a2366684c94ef'
+  version 'hydra3-13644'
+  sha256 'ef9a268e9a38ee853c6d4838b8415603983899b3e019a52663890123668cf0e6'
 
-  url "http://static.tankix.com/app/StandaloneOSXIntel64/andromeda2-#{version}/TankiX.dmg"
+  url "http://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
   name 'Tanki X'
   homepage 'https://www.tankix.com/'
 
