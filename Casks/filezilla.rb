17b2161950e4f9a57f647bb51a977ace03b33742
commitay <commitay@users.noreply.github.com>
2018-02-25 13:27:58 +1000

    Update filezilla.rb

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -3,7 +3,7 @@ cask 'filezilla' do
   sha256 '188e8d9af77dbcdd6176fb7eeca19083fff6f22e41331a1927c78c73c1faa769'
 
   # sourceforge.net/filezilla was verified as official when first introduced to the cask
-  url "https://jaist.dl.sourceforge.net/project/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
+  url "https://downloads.sourceforge.net/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
   appcast 'https://sourceforge.net/projects/filezilla/rss?path=/FileZilla_Client',
           checkpoint: '098d16fde9aa950ea2989b0b2c51ff340eb1c05b997497f7512f37dd7c86f177'
   name 'FileZilla'
