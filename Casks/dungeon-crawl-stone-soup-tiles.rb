62dfd6365f176dee160ea96ed167642c429bc70f
Klemen Košir <klemen.kosir@kream.io>
2018-02-25 12:58:55 +0100

    Update dungeon-crawl-stone-soup-tiles to 0.21.1 (#44320)

--- Casks/dungeon-crawl-stone-soup-tiles.rb
+++ Casks/dungeon-crawl-stone-soup-tiles.rb
@@ -1,10 +1,10 @@
 cask 'dungeon-crawl-stone-soup-tiles' do
-  version '0.21.0'
-  sha256 '2acb43ac451e579c67d360727c271086ad5f5391d92f52ef69df1bde9e515862'
+  version '0.21.1'
+  sha256 'f9f646e8293872d8f647a53da771f633f3f939833b601af6e6acddb942c63472'
 
   url "https://crawl.develz.org/release/#{version.major_minor}/stone_soup-#{version}-tiles-macosx.zip"
   appcast 'https://github.com/crawl/crawl/releases.atom',
-          checkpoint: 'dd96d66f02bc77ccf322d3031ca3b8a59f3870901cd6013b9e2c915ecce7e1b6'
+          checkpoint: '87d8536b2af9d3adeb1aeec0d72397767b80d96cc144ed912a1bbb3863cd5cd4'
   name 'Dungeon Crawl Stone Soup'
   homepage 'https://crawl.develz.org/'
 
