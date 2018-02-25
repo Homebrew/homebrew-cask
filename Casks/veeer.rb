cadd0f8321f5a8b1825cf9280e6bbb908f0fde47
Vítor Galvão <info@vitorgalvao.com>
2018-02-25 18:56:44 +0000

    Update veeer.rb

--- Casks/veeer.rb
+++ Casks/veeer.rb
@@ -1,8 +1,8 @@
 cask 'veeer' do
-  version '1.0.0'
+  version '2018.02'
   sha256 '9ce6c6d22d3f98e18226cad9cda11db970c91d2874773a9b739eabc9376b9f2e'
 
-  url 'http://veeer.io/wp-content/uploads/2018/02/VEEER.zip'
+  url "http://veeer.io/wp-content/uploads/#{version.dots_to_slashes}/VEEER.zip"
   name 'VEEER'
   homepage 'https://veeer.io/'
 
