8e9f0dbf9dfc7dfa29d2ddf34a7ce67b8d7c5576
MindTooth <33870508+MindTooth@users.noreply.github.com>
2018-02-25 12:59:57 +0100

    Update kitty to 0.8.0 (#44323)

--- Casks/kitty.rb
+++ Casks/kitty.rb
@@ -1,10 +1,10 @@
 cask 'kitty' do
-  version '0.7.1'
-  sha256 'db84d8bcc09c89bcf889caecef313c5034e0945fd2abe1fc50449b6b605694bb'
+  version '0.8.0'
+  sha256 '99fcc8e1a61de9bffa0a1b8de6d896dadb148e49c0a35a44534a7d1ad9067a3d'
 
   url "https://github.com/kovidgoyal/kitty/releases/download/v#{version}/kitty-#{version}.dmg"
   appcast 'https://github.com/kovidgoyal/kitty/releases.atom',
-          checkpoint: '211e54fdd1e18f33049d9e0ac4a2f4cc95765d393764025e5d1e26418badd784'
+          checkpoint: '056c22a637cba49b9e3938b0492cf7e6f7c85371636607d9b11d28c99983fec9'
   name 'kitty'
   homepage 'https://github.com/kovidgoyal/kitty'
 
