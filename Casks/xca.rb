8456251bb3dcafef1c6a3b0fe75f12daeae770bb
Torsten Fichtner <torsten@bashpipe.de>
2018-02-26 14:34:37 +0100

    updated xca to version 1.4.0

--- Casks/xca.rb
+++ Casks/xca.rb
@@ -1,10 +1,10 @@
 cask 'xca' do
-  version '1.3.2'
-  sha256 '8117821eef637c3fb78ce6e583fe0a3e667c714525e992b37d4d977a279a967e'
+  version '1.4.0'
+  sha256 '21d3f6e8199918173a188987de093dc9f16a867ccefb8c348950bb7d843cb1d8'
 
-  url "https://downloads.sourceforge.net/xca/xca/#{version}/xca-#{version}.dmg"
+  url "https://downloads.sourceforge.net/xca/xca/#{version}/xca-#{version}-Sierra.dmg"
   appcast 'https://sourceforge.net/projects/xca/rss?path=/xca',
-          checkpoint: '936bec75f8ece9b612ae7268ec93a6d2313a70342bda98b2a822f24f151750e3'
+          checkpoint: '648050906eff1ff308cc4011bdb9ba6871b30b6294fbe04226d22f678b10ac3a'
   name 'XCA'
   homepage 'http://xca.sourceforge.net/'
 
