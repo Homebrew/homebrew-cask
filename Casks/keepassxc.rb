8f5340c6e4c0c0c399e2fc7e82d0401bd9834698
Weslly <weslly@users.noreply.github.com>
2018-02-27 20:54:06 -0300

    Update KeePassXC to 2.3.0 (#44405)

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,11 +1,11 @@
 cask 'keepassxc' do
-  version '2.2.4'
-  sha256 '69ce702d25fb015521714bf6f2542261a65862ec36a176f30e9f612dc27bffb8'
+  version '2.3.0'
+  sha256 '2998d94d7bccdff9490c582a58fda6b63ae208b3ddd1f0f65257a694de6fb094'
 
   # github.com/keepassxreboot/keepassxc was verified as official when first introduced to the cask
   url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg"
   appcast 'https://github.com/keepassxreboot/keepassxc/releases.atom',
-          checkpoint: 'c41e0a4c48f2580113e4ec8d0b8a69bbd90dbe0a2ffc8f4d660c774a66e4d1d7'
+          checkpoint: 'fb8fad28545b2ba1c20079ce3c7624d12e0aa520288971393bad60b0bf973c44'
   name 'KeePassXC'
   homepage 'https://keepassxc.org/'
   gpg "#{url}.sig", key_url: 'https://keepassxc.org/keepassxc_master_signing_key.asc'
