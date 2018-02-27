3dbee2419aad77fb411f7fbe2f495a591c188052
Ansh Shukla <self@anshukla.com>
2018-02-26 21:15:35 -0800

    Add Screenotate Latest
    Screenotate (https://screenotate.com/) is a screenshot-taking tool which
    works just like macOS's screenshot tool – one keyboard shortcut and drag
    – and it uses OCR (Optical Character Recognition) to recognize text in
    your screenshots.
    
    The app has a fremium pricing model and can be unlocked without any
    extra downloads.

---
+++ Casks/screenotate.rb
@@ -0,0 +1,10 @@
+cask 'screenotate' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://screenotate.com/download/Screenotate-latest.dmg'
+  name 'Screenotate'
+  homepage 'https://screenotate.com/'
+
+  app 'Screenotate.app'
+end
