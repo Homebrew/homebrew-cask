526bbda0583b291a58f33fb1653d19c52560094b
commitay <commitay@users.noreply.github.com>
2018-02-28 06:58:45 +1000

    Update ckan.rb

--- Casks/ckan.rb
+++ Casks/ckan.rb
@@ -1,15 +1,15 @@
 cask 'ckan' do
-  version 'v1.24.0'
+  version '1.24.0'
   sha256 '4c116febeeaf4ed95cc6327ca3b220d22f16c8c601d878627367606573c205f8'
 
-  depends_on cask: "mono-mdk"
-
   # github.com/KSP-CKAN/CKAN was verified as official when first introduced to the cask
-  url "https://github.com/KSP-CKAN/CKAN/releases/download/#{version}/CKAN.dmg"
+  url "https://github.com/KSP-CKAN/CKAN/releases/download/v#{version}/CKAN.dmg"
   appcast 'https://github.com/KSP-CKAN/CKAN/releases.atom',
-          checkpoint: 'https://github.com/KSP-CKAN/CKAN/releases.atom'
-
+          checkpoint: 'ec64ae33f2ab43d9324749aa536b066f7cc1679b28035065d9a5d7a3df4ce5b9'
   name 'Comprehensive Kerbal Archive Network client'
   homepage 'https://forum.kerbalspaceprogram.com/index.php?/topic/154922-*'
+
+  depends_on cask: 'mono-mdk'
+
   app 'CKAN.app'
 end
