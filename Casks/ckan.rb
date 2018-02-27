6bc564ea53c99e2c3ff5cff51bb95386d03a653d
HebaruSan <HebaruSan@users.noreply.github.com>
2018-02-27 14:54:19 -0600

    Fix dependency syntax

--- Casks/ckan.rb
+++ Casks/ckan.rb
@@ -2,7 +2,7 @@ cask 'ckan' do
   version 'v1.24.0'
   sha256 '4c116febeeaf4ed95cc6327ca3b220d22f16c8c601d878627367606573c205f8'
 
-  depends_on cask: => "mono-mdk"
+  depends_on cask: "mono-mdk"
 
   # github.com/KSP-CKAN/CKAN was verified as official when first introduced to the cask
   url "https://github.com/KSP-CKAN/CKAN/releases/download/#{version}/CKAN.dmg"
