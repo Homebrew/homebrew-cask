96ed3569f596cf87767d27f083514f52be30fe19
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-27 23:38:13 +0000

    Update dropshare to 4.9,4699

--- Casks/dropshare.rb
+++ Casks/dropshare.rb
@@ -1,11 +1,11 @@
 cask 'dropshare' do
-  version '4.8,4693'
-  sha256 'df366b7c90e9d2fdc03c956fda11ca26c001ea660df96aaf3c687ba8fd608c1b'
+  version '4.9,4699'
+  sha256 '86179ad5bafb05ec2bb539760fd33a7b408ebdb6820edb7aa3ad883c255a9f4a'
 
   # d2wvuuix8c9e48.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2wvuuix8c9e48.cloudfront.net/Dropshare#{version.major}-#{version.after_comma}.app.zip"
   appcast "https://getdropsha.re/sparkle/Dropshare#{version.major}.xml",
-          checkpoint: '07f95e01339197f3d9ff6f22224d275dd8af2dcaf3fb1424f9016968d7f01a85'
+          checkpoint: 'd158c795fdb07b01c72a8d92fa44728d6c875e3e4a48a986a2f2a3c99cfbf6e1'
   name 'Dropshare'
   homepage 'https://getdropsha.re/'
 
