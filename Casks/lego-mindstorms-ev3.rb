2feb2598cdb3468ddc5fa99b01b008d37f8a24a5
Brian Morton <brianmortondesign@gmail.com>
2018-02-27 11:54:29 -0800

    Update lego-mindstorms-ev3 to 1.3.1

--- Casks/lego-mindstorms-ev3.rb
+++ Casks/lego-mindstorms-ev3.rb
@@ -1,7 +1,7 @@
 cask 'lego-mindstorms-ev3' do
   # note: "3" is not a version number, but an intrinsic part of the product name
-  version '1.3.0'
-  sha256 'ce5e0b047c096886d1a74540b306e704d6249c0c7eb09c76f58389841abe3528'
+  version '1.3.1'
+  sha256 '15ea0a61d87aa5172a0fd1587c904470287fa750ad9d47a3abf53233872808b2'
 
   # le-www-live-s.legocdn.com/downloads/LMS-EV3 was verified as official when first introduced to the cask
   url "https://le-www-live-s.legocdn.com/downloads/LMS-EV3/LMS-EV3_Full-setup_#{version}_en-us_osx.dmg"
