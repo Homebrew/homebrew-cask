0af1a52337d047ed06c0fa1ca7930a3be9ef0d32
Brian Morton <brianmortondesign@gmail.com>
2018-02-28 10:30:59 -0800

    Update fantastical to 2.4.7 (#44427)

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,10 +1,10 @@
 cask 'fantastical' do
-  version '2.4.6'
-  sha256 '8653a86192349a724c8c5608887a2b84bf0d8bd941ea046788ce30c29f6585e9'
+  version '2.4.7'
+  sha256 'c32945e883702ff20ef38fa2939ba86bb6b43351e087e043ea473359eb320852'
 
   url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast "https://flexibits.com/fantastical/appcast#{version.major}.php",
-          checkpoint: '878448d2c4c1337ed782740ba5abe1d0a72df2329de461d00e6538602143f910'
+          checkpoint: '92303b33b61886e2863eae07a036bea9593493c744e78005d2adf6de4d980f04'
   name 'Fantastical'
   homepage 'https://flexibits.com/fantastical'
 
