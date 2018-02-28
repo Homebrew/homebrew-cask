ebd57aae6dcd0fe167356ec0ede08c78d878460c
Tobias Breitwieser <tobias@breitwieser.biz>
2018-02-28 22:14:29 +0100

    betaflight-configurator 10.3.0 download name fix

--- Casks/betaflight-configurator.rb
+++ Casks/betaflight-configurator.rb
@@ -2,7 +2,7 @@ cask 'betaflight-configurator' do
   version '10.2.0'
   sha256 '44d7010baa81936df9356a615b09547958bf40a732784bbbfa29882dc5e82e13'
 
-  url "https://github.com/betaflight/betaflight-configurator/releases/download/#{version}/Betaflight-Configurator_macOS_#{version}.dmg"
+  url "https://github.com/betaflight/betaflight-configurator/releases/download/#{version}/betaflight-configurator_macOS_#{version}.dmg"
   appcast 'https://github.com/betaflight/betaflight-configurator/releases.atom',
           checkpoint: 'e88921861461d85427771b15833f8069b6e88728512035575c282609cc66a5eb'
   name 'Betaflight-Configurator'
