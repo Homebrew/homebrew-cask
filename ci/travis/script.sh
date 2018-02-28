6f5dcfd541acf8a98df127c4adf1ee179ea7ded8
Vítor Galvão <info@vitorgalvao.com>
2018-02-28 05:32:10 +0000

    travis: zap instead of uninstall (#44407)

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -43,7 +43,7 @@ elif [[ ${#modified_casks[@]} -gt 0 ]]; then
     fi
     for cask in "${modified_casks[@]}"; do
       run brew cask reinstall --verbose "${cask}"
-      run brew cask uninstall --verbose "${cask}"
+      run brew cask zap --verbose "${cask}"
     done
     if [[ -f "${HOME}/Brewfile" ]]; then
       run brew bundle cleanup --force --file="${HOME}/Brewfile"
