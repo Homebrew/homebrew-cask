7f7640df30598b40c785f582c51c2006433ad0f2
Naoto Yoshimoto <me@naotone.com>
2018-02-28 18:56:51 -0500

    Update craftmanager to 1.0.52 (#44449)

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -1,10 +1,10 @@
 cask 'craftmanager' do
-  version '1.0.51'
-  sha256 '9c754aa774cf59527448a848caa25e5c681d876486547de7fa30118facbfdcc1'
+  version '1.0.52'
+  sha256 '404d2500c59e22346d611e281c19380961bc5fcbc1dae30aa38c3938b52f5966'
 
   url 'https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip'
   appcast 'https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml',
-          checkpoint: 'ef3c8238b2d69132806f0f9ceaf955d80d142c24c3fb94829a91e1c3d918b4e6'
+          checkpoint: '37079ae0bead1704e4418f4966b0dc606295a95fffb4337a1bc1ad554e021016'
   name 'CraftManager'
   homepage 'https://www.invisionapp.com/craft'
 
