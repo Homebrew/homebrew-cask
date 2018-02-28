87cb89a89062d37bbd63cd1f68d05dc0906f9f0f
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-02-28 09:19:27 -0500

    Update Bitwarden.app to v1.0.5

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,11 +1,11 @@
 cask 'bitwarden' do
-  version '1.0.0'
-  sha256 '7b7d4cb38a0b4f1d2050b8f8d513b1ebc1d968e9c361d85b7e958b4703c2d834'
+  version '1.0.5'
+  sha256 'f7037edff6bf5e59efeafa87662129762e3284514a61e448352499e0a07cabde'
 
   # github.com/bitwarden/desktop was verified as official when first introduced to the cask
   url "https://github.com/bitwarden/desktop/releases/download/v#{version}/bitwarden-#{version}-mac.zip"
   appcast 'https://github.com/bitwarden/desktop/releases.atom',
-          checkpoint: 'c38806d5d92c19a3bad9d50569bd5e30ab99c2f53dd7e4591667517ced2f1a83'
+          checkpoint: '8299b04f1edc44fadcfd9f6b03148d6f784055bd144ee4e6197b10cd5cad6c0b'
   name 'Bitwarden'
   homepage 'https://bitwarden.com/'
 
