b191b85d470941f6c98578c5c2bcfcc6a33fcd07
Brian Morton <brianmortondesign@gmail.com>
2018-02-28 10:30:45 -0800

    Update hazel to 4.2.3 (#44429)

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -1,11 +1,11 @@
 cask 'hazel' do
-  version '4.2.2'
-  sha256 'aa5f4979b04d1edcf7c27a1ef20573c8dbae29f7eb016ff87b56dc2347edac15'
+  version '4.2.3'
+  sha256 'abd5999d2a1a773859233e69d34d0b17f32197e1c18ae370639b4d91341de14a'
 
   # s3.amazonaws.com/Noodlesoft was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Noodlesoft/Hazel-#{version}.dmg"
   appcast 'https://www.noodlesoft.com/Products/Hazel/generate-appcast.php',
-          checkpoint: '07bd36dd4a36d8ceadc4662814e5161da5e3a7263d6d184a04a546bc115b32d8'
+          checkpoint: '935f34544307a8ecc93834750214f7369690220bce97d60c425ce40bb87afa0c'
   name 'Hazel'
   homepage 'https://www.noodlesoft.com/'
 
