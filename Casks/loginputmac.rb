2c8b266e32d61377faed2c7270c6e3ba7057a280
R0uter <R0uter@users.noreply.github.com>
2018-02-28 11:21:08 +0800

    Update loginputmac to 1.9 (#44414)

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,11 +1,11 @@
 cask 'loginputmac' do
-  version '1.8'
-  sha256 '7780424e54aa2356bcb4aa9ad22c9933d6635e7dffe738440baf982c921bd0d6'
+  version '1.9'
+  sha256 'bbcab14fa4d067875427ad83275c13999e56ecfa0b02062615d1f0051de772b4'
 
   # nzhm461a0.qnssl.com was verified as official when first introduced to the cask
   url "https://nzhm461a0.qnssl.com/Installer.pkg#{version}.zip"
   appcast 'https://im.logcg.com/appcast.xml',
-          checkpoint: '572d6eb014c4b3a9512370cf7c89d3c3c6015be4c2f31f01c40ba1f012d9914a'
+          checkpoint: '555618d888149a863b41ee3f97d10c2964f7554913351bf17084695e74ffbeb1'
   name 'LoginputMac'
   homepage 'https://im.logcg.com/loginputmac'
 
