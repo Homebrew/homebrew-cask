a605b56771017d46cf2f8720c4abb60dcdf27d85
Brian Morton <brianmortondesign@gmail.com>
2018-02-28 10:30:31 -0800

    update towerim to 0.6.3 (#44431)

--- Casks/towerim-tower.rb
+++ Casks/towerim-tower.rb
@@ -1,6 +1,6 @@
 cask 'towerim-tower' do
-  version '0.6.2'
-  sha256 '89f030324e1372199b1017267cd9e4cc83b3c385fa24d7451dde65378b58b9d6'
+  version '0.6.3'
+  sha256 '666e58cc2ba8dfae1147f92b34ab59826b4b59d9b4fc3e3c3d4899d94966db02'
 
   # towerfiles.oss-cn-hangzhou.aliyuncs.com/electron/mac was verified as official when first introduced to the cask
   url "https://towerfiles.oss-cn-hangzhou.aliyuncs.com/electron/mac/Tower-#{version}.dmg"
