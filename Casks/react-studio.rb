f6b9502ba85d63b626c0d8d46a4fab14d440a3db
Naruhito Kubota <naru0504@users.noreply.github.com>
2018-02-28 10:11:51 +0900

    Update react-studio to 1.4,323 (#44411)

--- Casks/react-studio.rb
+++ Casks/react-studio.rb
@@ -1,6 +1,6 @@
 cask 'react-studio' do
-  version '1.3,315'
-  sha256 '356efa82b96a14133cf23cf3c6113ede482cf55143606d478b972598838b24e8'
+  version '1.4,323'
+  sha256 '32e1cad1849f5b74b97f2a891da81a699b2ad0b37b5eac2b4a7404c537a2ed67'
 
   # s3.amazonaws.com/sc.neonto.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/sc.neonto.com/ReactStudio_v#{version.before_comma.no_dots}_build#{version.after_comma}.zip"
