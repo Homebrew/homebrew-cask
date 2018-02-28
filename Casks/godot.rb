6279580ed597c725685782846acb22359168f59f
Anders Holmberg <anders.holmberg@endian.se>
2018-02-28 21:52:25 +0100

    Update godot to 3.0.1 (#44435)

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,11 +1,11 @@
 cask 'godot' do
-  version '3.0'
-  sha256 '38f7ba325c03dfe5efa7d311bf9637b2d03ae2a6572c7504a39a226102db8bfb'
+  version '3.0.1'
+  sha256 '6a04ed482cf51631257fdc2ad6de82dd0504700e3edf631f023d6fa6058e3a46'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.fat.zip"
   appcast 'https://github.com/godotengine/godot/releases.atom',
-          checkpoint: 'c61e0792af716aa733050c3214904b6e86029dc4e760914ae8d79f56286782ed'
+          checkpoint: '35fd1b2901289a82c3e5e9a19d7c617050c583e60ce3fde619da5dd8f3e440c5'
   name 'Godot Engine'
   homepage 'https://godotengine.org/'
 
