eb6d4f2239e10e418d1e06fb0422c112b5bfae50
Seonggwon Yoon <keyakoto@gmail.com>
2018-03-02 07:24:21 +0900

    Update genymotion to 2.12.0 (#44470)

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -1,6 +1,6 @@
 cask 'genymotion' do
-  version '2.11.1'
-  sha256 '338a4da6106c28a62d924b0818129adadd95b5924951b5d1f3ede880fa27e68a'
+  version '2.12.0'
+  sha256 '2a9b018b95e81775b2ecddc5a96b849cdac8ce79b6f38bb67dc6613706daebc9'
 
   url "https://dl.genymotion.com/releases/genymotion-#{version}/genymotion-#{version}.dmg"
   name 'Genymotion'
