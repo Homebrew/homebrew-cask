1d2f9ef5af56f7f280bf0703a676295858d848c4
commitay <commitay@users.noreply.github.com>
2018-03-02 08:10:05 +1000

    Update filezilla to 3.31.0

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -2,10 +2,9 @@ cask 'filezilla' do
   version '3.31.0'
   sha256 '188e8d9af77dbcdd6176fb7eeca19083fff6f22e41331a1927c78c73c1faa769'
 
-  # sourceforge.net/filezilla was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/filezilla/FileZilla_Client/#{version}/FileZilla_#{version}_macosx-x86.app.tar.bz2"
-  appcast 'https://sourceforge.net/projects/filezilla/rss?path=/FileZilla_Client',
-          checkpoint: '098d16fde9aa950ea2989b0b2c51ff340eb1c05b997497f7512f37dd7c86f177'
+  url "https://download.filezilla-project.org/client/FileZilla_#{version}_macosx-x86.app.tar.bz2"
+  appcast 'https://filezilla-project.org/versions.php?type=client',
+          checkpoint: '9b1a081a82ba46468494ab610ca341f26790ccc431193bda585571cf9ca722e0'
   name 'FileZilla'
   homepage 'https://filezilla-project.org/'
 
