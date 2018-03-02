fee9cc99d77b0cc77e932802b0de0711070d223b
Jonas Gefele <github.com@schrieb.de>
2018-03-02 20:39:41 +0100

    Update reaper to 5.76 (#44502)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,10 +1,10 @@
 cask 'reaper' do
-  version '5.75'
-  sha256 '6f22f160d968f84edc1255e16572e7149e882b3726feb70f1c54ce168ae94358'
+  version '5.76'
+  sha256 '14840dcee00734e1fd133b605b809670caad26af986bc2970f942fb9aba2f7e7'
 
   url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots}_x86_64.dmg"
   appcast 'https://www.reaper.fm/download.php',
-          checkpoint: 'f2e29f73a7d024c8819ea073ca48b50f52376e5fba6ab190c4ad1f64c01be280'
+          checkpoint: '5bcd78ab70500a854342826ee005ba27b3ab8ee19170f25ed89a9b9ef4b9b127'
   name 'REAPER'
   homepage 'https://www.reaper.fm/'
 
