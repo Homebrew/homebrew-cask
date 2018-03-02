1bbf5bd16c6371d2e80d68616a13c477d58d2537
tiiiecherle <tiiiecherle@gmail.com>
2018-03-02 15:14:09 +0100

    Update macupdate to 6.2.1

--- Casks/macupdate.rb
+++ Casks/macupdate.rb
@@ -1,8 +1,8 @@
 cask 'macupdate' do
-  version '6.1.9'
-  sha256 'dce250e6c000809ab25f52706f4bbce3ca1321f34de57c949f64d65638c38ceb'
+  version '6.2.1'
+  sha256 '6939d71a72f9199f14296ae92e13d54fc64f9c87055c2bfc9496fcc722225aa2'
 
-  url "http://dl2.macupdate.com/MacUpdateDesktop#{version}.zip"
+  url 'https://www.macupdate.com/download/8544/MacUpdate-Desktop'
   appcast 'https://www.macupdate.com/desktop/updates.xml',
           checkpoint: '4a5d6fea7b479baa4b9277e4bac1c29e358ac392b3f7a8e19de7bc44b4172f4b'
   name 'MacUpdate Desktop'
