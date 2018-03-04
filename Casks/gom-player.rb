344b2bb1b5363ef4acb1db0da1491280d78e8e65
commitay <commitay@users.noreply.github.com>
2018-03-04 22:43:39 +1000

    Update gom-player.rb

--- Casks/gom-player.rb
+++ Casks/gom-player.rb
@@ -1,6 +1,6 @@
 cask 'gom-player' do
-  version '1.059'
-  sha256 'cf97740bc09a172a93f9b2bb1b10cf5afdb2073465527439ecc617af82c7f186'
+  version :latest
+  sha256 :no_check
 
   url 'https://app.gomtv.com/GOMForMac/gom.pkg'
   name 'GOM Player'
@@ -9,18 +9,5 @@ cask 'gom-player' do
 
   pkg 'gom.pkg'
 
-  uninstall pkgutil: [
-                       'Gretech.com.gomPlayer.GOMPlayer.pkg',
-                       'Gretech.com.gomPlayer.GOMTVDownloadManager.pkg',
-                       'Gretech.com.gomPlayer.Uninstall.pkg',
-                       'Gretech.com.gomPlayer.FindeSubtitleProc.pkg',
-                       'Gretech.com.gomPlayer.GOMPlugin.pkg',
-                       'Gretech.com.gomPlayer.GOMTVDownloadPlugin.pkg',
-                       'Gretech.com.gomPlayer.UpdaterGOM.pkg',
-                       'Gretech.com.gomPlayer.libavcodec.56.pkg',
-                       'Gretech.com.gomPlayer.libavformat.56.pkg',
-                       'Gretech.com.gomPlayer.libavutil.54.pkg',
-                       'Gretech.com.gomPlayer.libswresample.1.pkg',
-                       'Gretech.com.gomPlayer.preinstall.pkg',
-                     ]
+  uninstall pkgutil: 'Gretech.com.gomPlayer.*'
 end
