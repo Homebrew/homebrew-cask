a1564958003bd97f0481563af1bfd9f1f12570ad
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-03-04 22:06:26 +0000

    Update forecast to 0.9_125

--- Casks/forecast.rb
+++ Casks/forecast.rb
@@ -1,11 +1,11 @@
 cask 'forecast' do
-  version '0.9_124'
-  sha256 '809af9ba53df7cbc77cb00cce4d99e82b02311f00574dc529762542fd2ca5432'
+  version '0.9_125'
+  sha256 '533baab40ab6bc7cef9ebfaa60804caffcdd3ff2915a7456733e5503ac7a64d4'
 
   # d2uzvmey2c90kn.cloudfront.net/appcast_download was verified as official when first introduced to the cask
   url "https://d2uzvmey2c90kn.cloudfront.net/appcast_download/Forecast_#{version}.zip"
   appcast 'https://overcast.fm/appcast/Forecast.xml',
-          checkpoint: '511888d9ac28bf8361f269ceaf310c1a28d672af4866f7d604786b905504f642'
+          checkpoint: 'd815a05f38a8e540b3c63aed82187ace77d845dbed2567627ce4db4fa8d5a23d'
   name 'Forecast'
   homepage 'https://overcast.fm/forecast'
 
