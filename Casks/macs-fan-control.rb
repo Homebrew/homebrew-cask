5fa865e6506688f0d2833a6d167bd84b14dc1889
commitay <commitay@users.noreply.github.com>
2018-03-05 19:53:51 +1000

    Update macs-fan-control to 1.4.10 (#44548)

--- Casks/macs-fan-control.rb
+++ Casks/macs-fan-control.rb
@@ -1,10 +1,10 @@
 cask 'macs-fan-control' do
-  version '1.4.9'
-  sha256 '2a423e7f3652a3c6e7ec67827eda41f4418390e72109566dda60143f63f7dbaf'
+  version '1.4.10'
+  sha256 'c07d71e1b3660c508f0f03f91be2fec0bf81efb2e680114478d16bb90c3039eb'
 
   url 'https://www.crystalidea.com/downloads/macsfancontrol.zip'
   appcast 'https://www.crystalidea.com/macs-fan-control/release-notes',
-          checkpoint: 'ab027ece8db076a9920fd6156d73e43a194f50dfe2cc7f86fc134b3c54b52c0e'
+          checkpoint: '808d65de7e43298d0f52f1af6e13f5cb173164fc4a942acc63763562d6161903'
   name 'Macs Fan Control'
   homepage 'https://www.crystalidea.com/macs-fan-control'
 
