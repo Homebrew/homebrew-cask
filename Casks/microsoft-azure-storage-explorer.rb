bf91de17f82d514accd97891ad4d73c0144c9e01
Prince <zhangzheng.prince@gmail.com>
2018-03-06 01:22:12 +0800

    Update microsoft-azure-storage-explorer to latest

--- Casks/microsoft-azure-storage-explorer.rb
+++ Casks/microsoft-azure-storage-explorer.rb
@@ -2,9 +2,11 @@ cask 'microsoft-azure-storage-explorer' do
   version :latest
   sha256 :no_check
 
-  url 'http://go.microsoft.com/fwlink/?LinkId=708342'
+  url 'http://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer_signed.zip'
   name 'Microsoft Azure Storage Explorer'
   homepage 'https://azure.microsoft.com/en-us/features/storage-explorer/'
 
-  app 'MicrosoftAzureStorageExplorer/Microsoft Azure Storage Explorer.app'
+  pkg 'MicrosoftAzureStorageExplorer-2/Microsoft Azure Storage Explorer.pkg'
+
+  uninstall pkgutil: 'com.microsoft.StorageExplorer.*'
 end
