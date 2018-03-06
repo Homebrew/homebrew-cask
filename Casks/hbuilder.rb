af4dee5560140913039e2015dc542c8c995f7342
Brian Morton <brianmortondesign@gmail.com>
2018-03-06 08:35:50 -0800

    update hbuilder to 9.0.2

--- Casks/hbuilder.rb
+++ Casks/hbuilder.rb
@@ -1,6 +1,6 @@
 cask 'hbuilder' do
-  version '9.0.1'
-  sha256 'dc8bcc073fe40450ff7cad9f098ba668e2e21066ff0984c6514f3d338a25084c'
+  version '9.0.2'
+  sha256 '44dd208cd27b228c7ce12afa28e749be70d0087c389431cf8638c372ecaff6d5'
 
   # download.dcloud.net.cn was verified as official when first introduced to the cask
   url "http://download.dcloud.net.cn/HBuilder.#{version}.macosx_64.dmg"
