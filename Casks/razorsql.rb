3eddd8c37f26eff1303d5dc7e81ce1be47114db1
Anver Housseini <contact@anver.ch>
2018-03-06 16:34:00 +0100

    Update razorsql to 7.4.8 (#44573)

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,10 +1,10 @@
 cask 'razorsql' do
-  version '7.4.7'
-  sha256 '1cf6f4381860cdb0afd2c48dbc80c7a5ac8bc46891297d2e72488ea80d048dc0'
+  version '7.4.8'
+  sha256 'abceebf728eab52c38cfa765f78113986d32694f37b48f69084b8a7193d2fa58'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   appcast 'https://razorsql.com/updates.html',
-          checkpoint: '4374790f346b673017238ba56ffae3ffe81bf720c3f70b3285430a947be0c807'
+          checkpoint: '7ef196e70903fb94192839823e8cfc1350089f7a716c1f5a094b9793286e77c3'
   name 'RazorSQL'
   homepage 'https://razorsql.com/download_mac.html'
 
