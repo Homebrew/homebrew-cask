76ffb818929032a131d2a406502e2fcee392d3a5
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-03-06 23:23:13 +0000

    Update gambit to 4.8.9 (#44604)
    * Update gambit to 4.8.9
    
    * Update gambit.rb

--- Casks/gambit.rb
+++ Casks/gambit.rb
@@ -1,17 +1,17 @@
 cask 'gambit' do
-  version '4.8.8'
-  sha256 'ab1769355391752c3700b61292b726116ec14f5b24a861139e50b43df17ff64e'
+  version '4.8.9'
+  sha256 'dd9945149dbf57930f01cc1b49848b9e37d38fd09ba9b9b2d74985f31f3e6c6c'
 
   # iro.umontreal.ca/~gambit/download/gambit was verified as official when first introduced to the cask
   url "https://www.iro.umontreal.ca/~gambit/download/gambit/v#{version.major_minor}/prebuilt/gambit-v#{version.dots_to_underscores}-macosx-intel64.dmg"
   appcast 'https://github.com/gambit/gambit/releases.atom',
-          checkpoint: '640c113048c260672eba4dd7d8675dcb386ee2cd6c701e313b218f4fb834b53b'
+          checkpoint: 'bea4e240e07be723439ba5a9ad7f246d4e3aadac6a574bc81670e760d47e0892'
   name 'Gambit'
   homepage 'http://gambitscheme.org/'
 
   pkg "gambit-v#{version.dots_to_underscores}-macosx-intel64.pkg"
 
-  uninstall pkgutil: 'marc.feeley.gambc',
+  uninstall pkgutil: 'marc.feeley.gambit',
             script:  {
                        executable: "/Library/Gambit/v#{version}/bin/uninstall-gambit",
                        args:       ["v#{version}"],
