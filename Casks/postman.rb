13ca3ffd46b838917bea288b3ce4783baa5198d7
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2018-03-06 10:56:54 +0100

    Update postman to 6.0.8 (#44569)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,6 +1,6 @@
 cask 'postman' do
-  version '6.0.7'
-  sha256 'f34cbc5c0646eba6c4e234b7d5b031b9845f1075ea1e7604fa4a15b878a84b07'
+  version '6.0.8'
+  sha256 'de0f4180be6a2ca9349be30836506f25138965d137bb156ec79a77e26023249d'
 
   # dl.pstmn.io/download/version/ was verified as official when first introduced to the cask
   url "https://dl.pstmn.io/download/version/#{version}/osx64"
