3f96c1e99b0a62f1098e84457022d8dd12f95c38
commitay <commitay@users.noreply.github.com>
2018-03-07 09:11:20 +1000

    Update the-unarchiver.rb

--- Casks/the-unarchiver.rb
+++ Casks/the-unarchiver.rb
@@ -3,7 +3,7 @@ cask 'the-unarchiver' do
   sha256 '0b65ed1af57766c9ce4db9207390f31ffc3a2c1c274616ac20749739661e4929'
 
   # devmate.com/cx.c3.theunarchiver was verified as official when first introduced to the cask
-  url 'https://dl.devmate.com/cx.c3.theunarchiver/TheUnarchiver.zip'
+  url "https://dl.devmate.com/cx.c3.theunarchiver/#{version.after_comma.before_colon}/#{version.after_colon}/TheUnarchiver-#{version.after_comma.before_colon}.zip"
   appcast 'https://updates.devmate.com/cx.c3.theunarchiver.xml',
           checkpoint: 'f0af10046b5de56a0de92d6be04aca3dea66afabab0b06dc58914d85828bc326'
   name 'The Unarchiver'
