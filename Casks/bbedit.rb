41f3b0fc099c9fc6d1f1094f6408b4a5e98a9f55
Brian Morton <brianmortondesign@gmail.com>
2018-03-06 17:06:09 -0800

    Update bbedit to 12.1.2 (#44611)

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -1,11 +1,11 @@
 cask 'bbedit' do
-  version '12.1.1'
-  sha256 '098bc306b17230e59ee54ae0a5ae327d534f075edb4905f3d9a990b521d5bf42'
+  version '12.1.2'
+  sha256 'd21677d69314489a61b92e93684afee2d2dc90a99d9960bdfc150d46e1bb7123'
 
   # s3.amazonaws.com/BBSW-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
   appcast 'https://versioncheck.barebones.com/BBEdit.xml',
-          checkpoint: '5b45e761294c946e7093d03b433af8795e75d314125141e3b840bab7935bddd5'
+          checkpoint: '078001a9ac34f3abb7bb12b59530c94d79fc3c5e9c1a5bc450be93e20c6d3dc8'
   name 'BBEdit'
   homepage 'https://www.barebones.com/products/bbedit/'
 
