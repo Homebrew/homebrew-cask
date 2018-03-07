e6cd01d952afecd88ebeea52b75580b18bae0ade
Vítor Galvão <info@vitorgalvao.com>
2018-03-07 18:53:38 +0000

    Update eagle.rb

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -2,7 +2,7 @@ cask 'eagle' do
   version '8.6.3'
   sha256 '9559c951775e206f561de5e13941d5a8dc50516374fa52c656c5b78435d7e450'
 
-  url "https://eagle-updates.circuits.io/downloads/#{version.gsub('.', '_')}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
+  url "https://eagle-updates.circuits.io/downloads/#{version.dots_to_underscores}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   name 'Autodesk EAGLE'
   homepage 'https://circuits.io/eagle/'
 
