82183c896263ab9dfa0bd5c9acd612cfd5c206c8
commitay <commitay@users.noreply.github.com>
2018-03-09 20:13:50 +1000

    Update textmate to 2.0-rc.7

--- Casks/textmate.rb
+++ Casks/textmate.rb
@@ -5,7 +5,7 @@ cask 'textmate' do
   # github.com/textmate/textmate was verified as official when first introduced to the cask
   url "https://github.com/textmate/textmate/releases/download/v#{version}/TextMate_#{version}.tbz"
   appcast 'https://github.com/textmate/textmate/releases.atom',
-          checkpoint: '3b9a2f7f90cf9f46232fd530bf98fb0295ad749eb6bb03312391c70e33a5a6ae'
+          checkpoint: '5983d646dd88065cd61ee95c2127765a6056621c23a1662a9c3dcf4cb5c6765c'
   name 'TextMate'
   homepage 'https://macromates.com/'
 
