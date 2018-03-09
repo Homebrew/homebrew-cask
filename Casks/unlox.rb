4e7c18684f99ca9235e3ca8fa665c8044bb39a98
Matt Coster <matthew@thecosters.co.uk>
2018-03-09 18:41:30 +0000

    Update unlox to 2.0.2.1

--- Casks/unlox.rb
+++ Casks/unlox.rb
@@ -1,10 +1,10 @@
 cask 'unlox' do
-  version '2.0.1'
-  sha256 'db14600045c310d5f9a08e70c46fe4d95967eece36c3b872dce31000ba9795ae'
+  version '2.0.2.1'
+  sha256 '20a5fa4b1120f23b99a8ee3171d06c6a3e75797a40216e7e480f860ff19d3da0'
 
   url 'https://unlox.it/download/'
   appcast 'https://unlox.it/download/update.php',
-          checkpoint: '8e932777b009577398658dacc56b581e704196af71df5047f4073d067a60782e'
+          checkpoint: '53e8b7889240d0ee4114abc3e4219f668046eb62621e532aa21039ab32639a54'
   name 'Unlox'
   homepage 'https://unlox.it/get'
 
