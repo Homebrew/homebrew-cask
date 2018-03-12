b20426782b21271801e706c1f90b34ecf4c825eb
Fabian Maass <fabian.maass@adnovum.ch>
2018-03-12 10:34:50 +0100

    Update cityofzion-neon to 0.2.1

--- Casks/cityofzion-neon.rb
+++ Casks/cityofzion-neon.rb
@@ -1,10 +1,10 @@
 cask 'cityofzion-neon' do
-  version '0.1.4'
-  sha256 '0e5ce25c0bd46f9a43b103fbdc75a6fdcaaf06088dd5e5781807fb1cf29cb5ce'
+  version '0.2.1'
+  sha256 'c1e75ed0c81d78173ea403fbb35843b2ecc61dde366409d908479dd4016fc093'
 
   url "https://github.com/CityOfZion/neon-wallet/releases/download/#{version}/Neon-#{version}.Mac.dmg"
   appcast 'https://github.com/CityOfZion/neon-wallet/releases.atom',
-          checkpoint: 'c66b7789cf71e827bf9277a94cde2da1e1ebbd54af0babe51312acc27507f375'
+          checkpoint: 'afb12029c1eb93d130105f5cd314325a17996e6becefdc81dff108fc34f26aaa'
   name 'Neon Wallet'
   homepage 'https://github.com/CityOfZion/neon-wallet'
 
