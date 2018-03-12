3cadbe0612764a514c0b0244f6bb7e87f6e3efa5
commitay <commitay@users.noreply.github.com>
2018-03-12 23:00:02 +1000

    Update mamp: remove <= mavericks version (#44753)

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,15 +1,12 @@
 cask 'mamp' do
-  if MacOS.version <= :mavericks
-    version '3.5.2'
-    sha256 'b935d118b6e14d43cf2570be2a05166a15b824c3edc0ede98cc30e6ab5af3697'
-  else
-    version '4.4.1'
-    sha256 '5eab5e3bcca0e7c9641d4cf21c7ee4a5698fa0c2ed7980cdb6a728de7857084b'
-  end
+  version '4.4.1'
+  sha256 '5eab5e3bcca0e7c9641d4cf21c7ee4a5698fa0c2ed7980cdb6a728de7857084b'
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
   name 'MAMP'
   homepage 'https://www.mamp.info/'
+
+  depends_on macos: '>= :yosemite'
 
   pkg "MAMP_MAMP_PRO_#{version}.pkg"
 
