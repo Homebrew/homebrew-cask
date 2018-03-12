279e641fc83549eefeae39c8ed3a7d6c97cf2b15
Vítor Galvão <info@vitorgalvao.com>
2018-03-12 18:08:37 +0000

    Update dbeaver-enterprise to 5.0.0 (#44762)

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,14 +1,14 @@
 cask 'dbeaver-enterprise' do
-  version '4.3.0'
-  sha256 '1d52c7fd7d84fdae32597a0ec105ec22e689524a9de67f0d8821ec626e2b94ad'
+  version '5.0.0'
+  sha256 '029b0d2843526c7b7284d6d4994d997ee82616200ee7df95e1e8c6776ba47f6d'
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   appcast 'https://dbeaver.com/product/version.xml',
-          checkpoint: '92df8a425b0e635172d1836be7a50e6311be6e35f4a29c455b09f42813bea3c0'
+          checkpoint: '239400a77a41d246b4a996f7b51c12afe9c2fd466df0813f999e4f5b0b73e008'
   name 'DBeaver Enterprise Edition'
   homepage 'https://dbeaver.com/'
 
-  app 'DBeaver.app'
+  app 'DBeaverEE.app'
 
   caveats do
     depends_on_java('8+')
