37b1071f15b2124bd7431997e1a793e0d119ab2d
Brian Morton <brianmortondesign@gmail.com>
2018-03-12 15:11:30 -0700

    Update electrum to 3.1.1

--- Casks/electrum.rb
+++ Casks/electrum.rb
@@ -1,10 +1,10 @@
 cask 'electrum' do
-  version '3.1.0'
-  sha256 '7752066b5e59dbefa9b4842c68e8a78acdcbeda9377b9476f9790b71aba87053'
+  version '3.1.1'
+  sha256 '75d18a8b75a4d2bc4c028744cfb7a6fbfb56889de056c6e0b1858628086ae663'
 
   url "https://download.electrum.org/#{version}/electrum-#{version}.dmg"
   appcast 'https://github.com/spesmilo/electrum/releases.atom',
-          checkpoint: '90b6d4bfd57e93af5bcb277af00c5303372f627f90f710bbf793421b940a14f4'
+          checkpoint: '51133ceff378c055e1db6ab03d81af178a98d4db68dddcd812ecee877ed2aca6'
   name 'Electrum'
   homepage 'https://electrum.org/'
   gpg "#{url}.asc", key_id: '6694d8de7be8ee5631bed9502bd5824b7f9470e6'
