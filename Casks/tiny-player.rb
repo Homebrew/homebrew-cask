34f06a00f8d311cdab4e8d23821c5d65ba7243c0
Vítor Galvão <info@vitorgalvao.com>
2018-03-13 20:15:06 +0000

    Update tiny-player.rb

--- Casks/tiny-player.rb
+++ Casks/tiny-player.rb
@@ -1,8 +1,10 @@
 cask 'tiny-player' do
-  version :latest
-  sha256 :no_check
+  version '1.2.1'
+  sha256 '08c85c0a35422d0a3fa5ff5b4993e5b9a68e697efe85057e1b792841b53330fc'
 
-  url 'http://download.catnapgames.com/TinyPlayer.zip'
+  url "http://download.catnapgames.com/TinyPlayer-#{version}.zip"
+  appcast 'http://download.catnapgames.com/TinyPlayerAppcast.xml',
+          checkpoint: '9108b3673b515cdc6b13179a5f62d8a48e2e482c5e02c3f1aba7f0a2fdb1d09c'
   name 'Tiny Player for Mac'
   homepage 'http://www.catnapgames.com/tiny-player-for-mac/'
 
