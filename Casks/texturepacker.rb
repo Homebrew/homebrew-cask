f3a5c4fb459229e5475eca64f139c068729135ff
Brian Morton <brianmortondesign@gmail.com>
2018-03-13 09:23:32 -0700

    Update texturepacker to 4.6.3

--- Casks/texturepacker.rb
+++ Casks/texturepacker.rb
@@ -1,10 +1,10 @@
 cask 'texturepacker' do
-  version '4.6.2'
-  sha256 'ecdc6c3b420580a971c524c68098d1f9b7e5063d2cefe64f00b9b8b6c581b2dd'
+  version '4.6.3'
+  sha256 '16eac43171c7a5e5cbdb25adb37990165f2b6ff8af225621353fed78051e95f0'
 
   url "https://www.codeandweb.com/download/texturepacker/#{version}/TexturePacker-#{version}-uni.dmg"
   appcast 'https://www.codeandweb.com/releases/TexturePacker/appcast-mac-release.xml',
-          checkpoint: 'cd95697c56439125ac45e645ea6ba45d1411caf42a7dd0a6eecb499c80e7f6cd'
+          checkpoint: 'aa627a4933cfa493f2e7b72d5e02b78f784cb44a09e4fd2036c6da4a8b559445'
   name 'TexturePacker'
   homepage 'https://www.codeandweb.com/texturepacker'
 
