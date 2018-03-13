76fec50b7076eafaf3f23747c5f3a20eb56b2b08
commitay <commitay@users.noreply.github.com>
2018-03-14 09:44:52 +1000

    Update flash-player to 29.0.0.113 (#44808)

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,11 +1,11 @@
 cask 'flash-player' do
-  version '28.0.0.161'
-  sha256 'be937350ca7324b6959eb5954ff03daa48d1ba7a2a16ba5fa8342383d5cc058b'
+  version '29.0.0.113'
+  sha256 '7c0691a7b60404519d5e33c285ca70a83b2b48fac679078304d9212bfebc44d3'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
-          checkpoint: '9328b81a03aa632970cd96be9f1d58a573a5e376debed48a59ab76e186e9786f'
+          checkpoint: 'a617234c8f9b680bc5d5f4ac249faf8d23cfc43f9d171a7665405b4a26f2c502'
   name 'Adobe Flash Player projector'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
