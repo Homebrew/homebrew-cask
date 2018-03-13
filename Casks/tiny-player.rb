d83462379050a160b3b79dad55c34297b620fee5
Vítor Galvão <info@vitorgalvao.com>
2018-03-13 20:12:06 +0000

    Update tiny-player.rb

--- Casks/tiny-player.rb
+++ Casks/tiny-player.rb
@@ -6,7 +6,7 @@ cask 'tiny-player' do
   name 'Tiny Player for Mac'
   homepage 'http://www.catnapgames.com/tiny-player-for-mac/'
 
-  auto_updates
+  auto_updates true
 
   app 'Tiny Player.app'
 end
