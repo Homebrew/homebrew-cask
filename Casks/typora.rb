fcfbd9c1a4108aebe519123366f163e970610c5a
commitay <commitay@users.noreply.github.com>
2018-03-13 15:49:32 +1000

    Update typora.rb

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -2,7 +2,7 @@ cask 'typora' do
   version '0.9.9.13'
   sha256 '3b0882a4dcbb704584523648fe5f3fedeb8db9d19a4f909398a4ef1465dd2cc6'
 
-  url 'https://typora.io/download/Typora.dmg'
+  url "https://www.typora.io/download/Typora-#{version}.dmg"
   appcast 'https://www.typora.io/download/dev_update.xml',
           checkpoint: 'b132e8186331d5675de1e664d313ccf779c74afcfcf898a710551e72faf61611'
   name 'Typora'
