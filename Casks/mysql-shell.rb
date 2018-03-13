567acef898994bd41d18c19e141acc54b228c131
Shinya Nakano <s-nakano@s-style.co.jp>
2018-03-13 19:01:20 +0900

    Update mysql-shell to 1.0.11

--- Casks/mysql-shell.rb
+++ Casks/mysql-shell.rb
@@ -1,14 +1,14 @@
 cask 'mysql-shell' do
-  version '1.0.10'
-  sha256 'e9089ee384b4f9de1c2566043b232db2e4fdb8e9a722e24ef1b6dbf2e3e76344'
+  version '1.0.11'
+  sha256 'f5805eeef78dc8a6d6ac8642d944fc82ccf2ceda2c1654687d4fa65e07a94005'
 
-  url "https://dev.mysql.com/get/Downloads/MySQL-Shell/mysql-shell-#{version}-macos10.12-x86-64bit.dmg"
+  url "https://dev.mysql.com/get/Downloads/MySQL-Shell/mysql-shell-#{version}-macos10.13-x86-64bit.dmg"
   name 'MySQL Shell'
   homepage 'https://dev.mysql.com/downloads/shell/'
 
-  pkg "mysql-shell-#{version}-macos10.12-x86-64bit.pkg"
+  pkg "mysql-shell-#{version}-macos10.13-x86-64bit.pkg"
 
   uninstall pkgutil: 'com.mysql.shell'
 
-  zap trash: '~/.mysqlsh'
+  zap delete: '~/.mysqlsh'
 end
