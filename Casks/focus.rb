2bbce03f819fafe866e369921e158eba49df7eb0
Vítor Galvão <info@vitorgalvao.com>
2018-03-14 17:40:33 +0000

    Update focus.rb

--- Casks/focus.rb
+++ Casks/focus.rb
@@ -3,8 +3,6 @@ cask 'focus' do
   sha256 'd9f4db081e42811eea38b6463bffb69b9b2c2afe09bf5ae03e398ac783e1c65b'
 
   url "https://heyfocus.com/releases/Focus-#{version}.zip"
-  appcast 'https://heyfocus.com/appcast.xml',
-          checkpoint: '250a501501d4e891c7ed828cf8f6f7f3aea2c8609052d0f6dd5919bb3d9892e6'
   name 'Focus'
   homepage 'https://heyfocus.com/'
 
