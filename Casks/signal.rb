06d1d0a78cc34e67845ff9834d6a533ce3230f42
Beat Rupp <beatrupp@posteo.net>
2018-03-14 13:30:03 +0100

    Update signal to 1.6.0 (#44837)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,10 +1,10 @@
 cask 'signal' do
-  version '1.5.2'
-  sha256 '8fb08b941636ac8789f616819912f774395d80ee1d994c76a1528f039f5587ab'
+  version '1.6.0'
+  sha256 'fb223013f8b6b0044bbd5d68a6e6eafad3feabed48af59c7f4fb5dc43507d31f'
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.zip"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom',
-          checkpoint: '5957ed7fbefddfaa317f648b3154f0942b50decd809e25cc1c58d93ae700257e'
+          checkpoint: '0892670f255fab87e6822e2f82f44128a0d55ce1ea314d93f32756f59b909415'
   name 'Signal'
   homepage 'https://signal.org/'
 
