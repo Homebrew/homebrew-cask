3796d0612189c105077e2edc928e035f091ac8e5
wickedsp1d3r <jussit@meiliboxi.fi>
2018-03-14 20:33:31 +0200

    Update smart-scroll to 4.3.8

--- Casks/smart-scroll.rb
+++ Casks/smart-scroll.rb
@@ -1,10 +1,10 @@
 cask 'smart-scroll' do
-  version '4.3.7'
-  sha256 'ac4ee8a5a32c3a1e4c89ed2a57788e4ef5e0e9977bd5261547df883460da4dd8'
+  version '4.3.8'
+  sha256 '9f4c83bfb9e141f30258e219f3ba15578aade75cda12ba631627e02dae7e8e29'
 
   url 'https://www.marcmoini.com/SmartScroll.dmg'
   appcast 'https://www.marcmoini.com/sxv_en.html',
-          checkpoint: '095b5b20a795778af5a7f6da7262e91f83ff0292cda1a94bf510eb5de9181abb'
+          checkpoint: '092d82976e142eee7196eae3ef6efa060c61580b202044d1de3c800b012465df'
   name 'Smart Scroll'
   homepage 'https://www.marcmoini.com/sx_en.html'
 
