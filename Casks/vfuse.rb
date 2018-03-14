664a631c45f9a491477db211cfc3d1dba0a9492d
commitay <commitay@users.noreply.github.com>
2018-03-15 07:27:07 +1000

    Update vfuse to 2.0.3

--- Casks/vfuse.rb
+++ Casks/vfuse.rb
@@ -1,10 +1,10 @@
 cask 'vfuse' do
-  version '2.0.0'
-  sha256 '5818e292500427a968b17007315e45de9bce3319248298e5f361fd3940704994'
+  version '2.0.3'
+  sha256 'a69e7357bea014f4c14ac9699274f559086844ffa46563c4619bf1addfd72ad9'
 
   url "https://github.com/chilcote/vfuse/releases/download/#{version}/vfuse-#{version}.pkg"
   appcast 'https://github.com/chilcote/vfuse/releases.atom',
-          checkpoint: '796026ea80a5adc3846abab852d5342f87c8203524ef2923fb7617f2fa47b61d'
+          checkpoint: '56d1707d3065bf0c75d75d7738571285273b7bf366d8f0f5a53eb8b457ad2453'
   name 'vfuse'
   homepage 'https://github.com/chilcote/vfuse'
 
