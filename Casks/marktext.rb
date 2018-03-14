7df4e8c037327eae4aaa4228ab474bff5a348cc9
Jocs <luoran1988@126.com>
2018-03-14 12:31:30 +0800

    Add Mark Text.app v0.6.14
    Mark Text is a Markdown editor for Mac, It is a concise text editor, dedicated to improving your editing efficiency

---
+++ Casks/marktext.rb
@@ -0,0 +1,10 @@
+cask 'marktext' do
+  version '0.6.14'
+  sha256 '04b8f78adbc4d7d3f11622516dde627ac7870c28220737ae546a4835b9265fe1'
+
+  url 'https://github.com/marktext/marktext/releases/download/v0.6.14/marktext-0.6.14.dmg'
+  name 'Mark Text'
+  homepage 'https://github.com/marktext/marktext'
+
+  app 'Mark Text.app'
+end
