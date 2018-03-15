2e3865d55b2a5cf331f074825ef5313947e3f4b8
commitay <commitay@users.noreply.github.com>
2018-03-16 09:46:41 +1000

    Update poll-everywhere.rb

--- Casks/poll-everywhere.rb
+++ Casks/poll-everywhere.rb
@@ -4,8 +4,8 @@ cask 'poll-everywhere' do
 
   # amazonaws.com/polleverywhere-app was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/polleverywhere-app/mac-stable/#{version}/pollev.dmg"
-  appcast 'https://polleverywhere-app.s3.amazonaws.com/mac-beta/appcast.xml',
-          checkpoint: '007c5488e8d9d53c03b5966338fd42186477b29d5a21789c156c9afad87f237e'
+  appcast 'https://s3.amazonaws.com/polleverywhere-app/mac-stable/appcast.xml',
+          checkpoint: '007e5d491389fca41a304d1b25b4dc1903fdfdc021a6195c24238fdb852beee9'
   name 'PollEv Presenter'
   homepage 'https://www.polleverywhere.com/'
 
