67e1cc4786a8a66680e08a7fe04ddb667e8e4544
commitay <commitay@users.noreply.github.com>
2018-03-15 22:01:59 +1000

    Update gisto.rb

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -2,12 +2,12 @@ cask 'gisto' do
   version '0.3.2'
   sha256 '5a3666977eca39099f99e52e77c82244cb2baf52baeb8f21efcf3b369de27789'
 
-  # github.com was verified as official when first introduced to the cask
-  url "https://github.com/Gisto/Gisto/releases/download/0.3.2-beta/Gisto-v#{version}-macos-x64.dmg"
-  appcast 'http://www.gistoapp.com/GistoAppCast.xml',
-          checkpoint: 'c959dc4ada9c64029566aa4b1174818e360df84444da255d9626c2bef41f9cf3'
+  # github.com/Gisto/Gisto was verified as official when first introduced to the cask
+  url "https://github.com/Gisto/Gisto/releases/download/#{version}-beta/Gisto-v#{version}-macos-x64.dmg"
+  appcast 'https://github.com/Gisto/Gisto/releases.atom',
+          checkpoint: '8bf2c2f03d41ef837bbd15be0d822d7c820ce77cb157b62db9e7f32d5a3fa259'
   name 'Gisto'
-  homepage 'http://www.gistoapp.com/'
+  homepage 'https://www.gistoapp.com/'
 
   app 'Gisto.app'
 end
