7516316ff99f26bb99744e34c6a75eeff40f2100
Simon Campano <scampano@simplicite.fr>
2018-03-15 12:49:49 +0100

    Update gisto to 0.3.2

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -1,8 +1,9 @@
 cask 'gisto' do
-  version '0.3.1'
-  sha256 '93df9da2888f1cca3e649ccc30b8c4784e95ad22000c77d684d89363d873cbba'
+  version '0.3.2'
+  sha256 '5a3666977eca39099f99e52e77c82244cb2baf52baeb8f21efcf3b369de27789'
 
-  url "http://download.gistoapp.com/Gisto-#{version}-OSX_x86_64.dmg"
+  # github.com was verified as official when first introduced to the cask
+  url "https://github.com/Gisto/Gisto/releases/download/0.3.2-beta/Gisto-v#{version}-macos-x64.dmg"
   appcast 'http://www.gistoapp.com/GistoAppCast.xml',
           checkpoint: 'c959dc4ada9c64029566aa4b1174818e360df84444da255d9626c2bef41f9cf3'
   name 'Gisto'
