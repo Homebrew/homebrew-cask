c54f553585fa2c48a598daf0d7d12d9382a34742
Roman Zhuzha <roman.zhuzha@trivago.com>
2018-03-15 01:38:01 +0100

    Update get-backup-pro to 3.4.3

--- Casks/get-backup-pro.rb
+++ Casks/get-backup-pro.rb
@@ -1,11 +1,11 @@
 cask 'get-backup-pro' do
-  version '3.4.2'
-  sha256 '01d7e4bd623ed7b079ec8bf0c38a418675e50de8396dfc3a453270c7a33dceeb'
+  version '3.4.3'
+  sha256 '76e7d49c5055607c5ac9efcc7a309a3c05a7d65263ef5f1db7cda8f84cf39329'
 
   # belightsoft.s3.amazonaws.com/updates was verified as official when first introduced to the cask
   url "https://belightsoft.s3.amazonaws.com/updates/Get+Backup+Pro+#{version.major}.zip"
   appcast "https://www.belightsoft.com/download/updates/appcast_getbackup_pro#{version.major}.xml",
-          checkpoint: 'd7762347d2ff3c6995fbf00ad36c7409126faad67706ccb17911bb6a55eeebaa'
+          checkpoint: 'c4177d351a4c4db0bb964a1345f5f24dbbca2bc48e3d82feea90a4dfdcf96a35'
   name "Get Backup Pro #{version.major}"
   homepage 'https://www.belightsoft.com/products/getbackup/'
 
