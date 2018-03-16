a7a4ad6fcf791d432aa73353fc3e447015b24787
Brian Morton <brianmortondesign@gmail.com>
2018-03-16 09:31:44 -0700

    Update quip to 5.2.39

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -1,6 +1,6 @@
 cask 'quip' do
-  version '5.2.31'
-  sha256 '2ce1de6dd533f9f070125270cf84c4128f1e4dca63723b9e90404be17a63c47f'
+  version '5.2.39'
+  sha256 '561a2bcec79436ee5223f4c6cfac7ec929a7ae9889d2b6d5703974aa9a166961'
 
   # d2i1pl9gz4hwa7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2i1pl9gz4hwa7.cloudfront.net/macosx_#{version}.dmg"
