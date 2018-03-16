38f9e3d9efb0baab3980edb2fb77b3daca6f95a7
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2018-03-16 10:41:13 +0100

    Update Fork 1.0.65.1

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,10 +1,10 @@
 cask 'fork' do
-  version '1.0.65'
-  sha256 'c7ddbc7f9759915a7285bf2db8d52a98833105365e5ceed76965e4b9f0e4e59c'
+  version '1.0.65.1'
+  sha256 '9582a2a7cf2a43e6a167199a0520d145c130dcf7555df7901b0f02e2611d75f6'
 
   url 'https://git-fork.com/update/files/Fork.dmg'
   appcast 'https://git-fork.com/update/feed.xml',
-          checkpoint: '67202807ee361834431c6b838eb9f012def8ba2e62ea32189c7b23963fd44ae5'
+          checkpoint: 'fa2ef7f0fc096180267bb2332af0f0074d77088e7bfe1f00e8f4b39beb5e60eb'
   name 'Fork'
   homepage 'https://git-fork.com/'
 
