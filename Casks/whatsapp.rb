2d97edbe645da87bb7143fc0325d18b5000ba214
tiiiecherle <tiiiecherle@gmail.com>
2018-03-16 22:23:09 +0100

    Update whatsapp to 0.2.8505 (#44919)

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,10 +1,10 @@
 cask 'whatsapp' do
-  version '0.2.8361'
-  sha256 'a1f47f0024f6a6fc1f01e574ecfa71ecf223728ac973378fbd0a3a3099c79fd8'
+  version '0.2.8505'
+  sha256 'b7c2e9e2bfd582f9f164049b60e8cc8343944c24f32608376e9678dffc4206a1'
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64',
-          checkpoint: '98622fd5eb90583b1723e610e887d5d2f2b9475d361f9c698979863214c28e88'
+          checkpoint: '841bf8964da7b77c5df21e2fb75981f2ccbfe0bd736f7734f8962a4e40d58058'
   name 'WhatsApp'
   homepage 'https://www.whatsapp.com/'
 
