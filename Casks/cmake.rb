b8bdffde5ba2d1bd788afa963ff0dec581c0e49d
Samuele Decarli <decarlis@student.ethz.ch>
2018-03-16 23:27:35 +0100

    Update cmake to 3.10.3 (#44923)

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -1,6 +1,6 @@
 cask 'cmake' do
-  version '3.10.2'
-  sha256 'f2bc1669a5be6c8325a3eaced4e0426b1b422188ff82febf10bcee0117c5518a'
+  version '3.10.3'
+  sha256 'c0fac18cee8c15d04812740fe70f454f0a6b42d374b6908cf0ceb4bf2fcdcaf8'
 
   url "https://www.cmake.org/files/v#{version.major_minor}/cmake-#{version}-Darwin-x86_64.dmg"
   name 'CMake'
