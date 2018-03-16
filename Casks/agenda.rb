7946d503afd3ba1e04bc66af5d1a9b4b2fea1348
Brian Morton <brianmortondesign@gmail.com>
2018-03-16 09:56:17 -0700

    Update agenda to 2.0 (#44916)

--- Casks/agenda.rb
+++ Casks/agenda.rb
@@ -1,10 +1,10 @@
 cask 'agenda' do
-  version '1.4'
-  sha256 'd3e33bebdc3795bc726810be13e73edac2afa896611541b3cff0527c8b3fb332'
+  version '2.0'
+  sha256 'e932ba001060cba4c460dc5a46b553a4ff2d3e8e37e994c6ce493f22cea113d5'
 
   url "https://downloads.agenda.com/Agenda_#{version}.zip"
   appcast 'https://downloads.agenda.com/AgendaSparkleAppcast.xml',
-          checkpoint: '89e84e70260e49d610810f491ff5008d002ccb50ef72288cf0e206e9aa72b8f4'
+          checkpoint: '5f232159da74ebca1a64068761601439f60c2348d13f3b926bfa3085a33e5b99'
   name 'Agenda'
   homepage 'https://agenda.com/'
 
