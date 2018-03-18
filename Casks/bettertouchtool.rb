ed249729dd6bc3a965443093e787a93b539a574c
Sergey Parhomenko <sparhomenko@users.noreply.github.com>
2018-03-18 23:25:02 +0100

    Update bettertouchtool to 2.405 (#44957)
    * Update bettertouchtool to 2.405
    
    * Update bettertouchtool.rb

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -18,13 +18,13 @@ cask 'bettertouchtool' do
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}_final_10_9.zip"
   else
-    version '2.403'
-    sha256 '61abb82673442d95aa8f7b95ae78fb1eb363d12b7689211de3ab5e6f3ee8ddb6'
+    version '2.405'
+    sha256 '268ef79817a02623bce4ff2290135567100e6b4e0700a11720909f80fbe47534'
 
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}.zip"
-    appcast 'https://updates.bettertouchtool.net/appcast.xml',
-            checkpoint: '6af441c57231d96750f1a6d4122fba5690b0d3920594a6c36cee1b7545e187f9'
+    appcast 'https://updates.bettertouchtool.net/bettertouchtool_release_notes.html',
+            checkpoint: '4520ee4fcee23c2367b2262ac827e548ca635ac33000cfb9becf4f4fc663c16a'
   end
 
   name 'BetterTouchTool'
