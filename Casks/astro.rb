a83e202df5bf62f4eed995d58e7e789946525a7a
Oran Shuster <thristle666@gmail.com>
2018-03-19 19:14:04 +0200

    Update astro to 3.0.8,3845 (#44974)

--- Casks/astro.rb
+++ Casks/astro.rb
@@ -1,11 +1,11 @@
 cask 'astro' do
-  version '3.0.7,3818'
-  sha256 '9fa7591bf5e39666fcaf18bc1668304e547ded27f82156798106ba876e069a76'
+  version '3.0.8,3845'
+  sha256 'fda7047d9a633253129b3e6cc5a49d1bd9986faebaa1270d677e3df9f21ecd3e'
 
   # pexlabs-updates-xvuif5mcicazzducz2j2xy3lki.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
   url "https://pexlabs-updates-xvuif5mcicazzducz2j2xy3lki.s3-us-west-2.amazonaws.com/Astro-#{version.after_comma}.dmg"
   appcast 'https://pexlabs-updates-xvuif5mcicazzducz2j2xy3lki.s3-us-west-2.amazonaws.com/pexappcast.xml',
-          checkpoint: '135d27ab349f3c939700ea0de5d6bbc9522bef52ae237c8e003ec5fb7ade0c36'
+          checkpoint: '7691e364086651297cc41260b5e8ff86e0c90daf99d7635898c2bf927f4a7f95'
   name 'Astro'
   homepage 'https://www.helloastro.com/'
 
