919a466e41178efeb0055891cf73f7011c9b30ef
tiiiecherle <tiiiecherle@gmail.com>
2018-03-19 17:58:36 +0100

    Update quicksilver to 1.5.9

--- Casks/quicksilver.rb
+++ Casks/quicksilver.rb
@@ -6,13 +6,13 @@ cask 'quicksilver' do
     version '1.2.0'
     sha256 '08c5aeaa7fe89427bcec30a9700eb0b7484bd68b647776b2c82c95355d1679d4'
   else
-    version '1.5.8'
-    sha256 'fc687a674cd1483bf1718fa89ee0e2d23a3ca9a7bad105ae714b4c48b18807c1'
+    version '1.5.9'
+    sha256 'c51412eb3d87743246d06f5b4687b6982ffc955565fb2d1331caa8e2c69bcc0a'
   end
 
   url "https://qsapp.com/archives/downloads/Quicksilver%20#{version}.dmg"
   appcast 'https://qsapp.com/archives/',
-          checkpoint: 'c514430f2367a2b1858806001775e3623bc055d0eeae40be8c792700c4f2436c'
+          checkpoint: 'eef69e586c9a1ddc88f6c5440205dcd5e8aa1bb34beaa22918f703360e3125a6'
   name 'Quicksilver'
   homepage 'https://qsapp.com/'
 
