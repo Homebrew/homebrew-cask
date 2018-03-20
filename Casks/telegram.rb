b768f61d4f1b92dd68f775308340eec44bbce8ef
Maksim Karelov <max.karelov@gmail.com>
2018-03-20 10:59:01 +0300

    Update telegram to 3.8.2-119501

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,10 +1,10 @@
 cask 'telegram' do
-  version '3.8.1-119441'
-  sha256 '775323d611ecd31358e94dfe67f9f2e58f4cc78b672802811d486b1136ab3a85'
+  version '3.8.2-119501'
+  sha256 '869635b70b2fcda95f6ee55ab3ead54f4c4aa8569921928532058b65449ad954'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml',
-          checkpoint: 'd9bd40e1396b9474cab2873772e9fd51d3d889406bd5e8b473b4a692cf16a616'
+          checkpoint: 'ca764ba57aeca12d3dccc073f1cf215b3164c2887b17904013b375f813cec3ed'
   name 'Telegram for macOS'
   homepage 'https://macos.telegram.org/'
 
