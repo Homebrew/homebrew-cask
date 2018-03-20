3185932ecc542bfea508aa6cbd196e80797da468
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2018-03-21 00:13:58 +0100

    Update plex-media-player to 2.7.0.806-608417d4

--- Casks/plex-media-player.rb
+++ Casks/plex-media-player.rb
@@ -1,10 +1,10 @@
 cask 'plex-media-player' do
-  version '2.6.0.803-edcf527b'
-  sha256 'd461793234919b6a76bb13fc2ddbf15e3f7149673e48e5294119d26c870ee71a'
+  version '2.7.0.806-608417d4'
+  sha256 '961bc4a9196ce70c42d5cc4f2b3a512ed3e4b9a7d09302361b7358e1579e19ba'
 
   url "https://downloads.plex.tv/plexmediaplayer/#{version}/PlexMediaPlayer-#{version}-macosx-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/3.json',
-          checkpoint: '6e06f39438721b5b504e66402382d2678033c222b4549e9c6824edb273a2ea80'
+          checkpoint: 'edbe55f036c3668053467a7dbfed97eae1561149892b60b9ad925d564372dadf'
   name 'Plex Media Player'
   homepage 'https://www.plex.tv/'
 
