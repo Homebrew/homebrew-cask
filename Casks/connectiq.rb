825b124c1ddefc709d7a32e839de98abf22d7374
Brian Morton <brianmortondesign@gmail.com>
2018-03-21 16:35:10 -0700

    Update connectiq to 2.4.4

--- Casks/connectiq.rb
+++ Casks/connectiq.rb
@@ -1,6 +1,6 @@
 cask 'connectiq' do
-  version '2.4.3'
-  sha256 '272bf8123da29064ea59dfe40bed8fb8f60782c14f0ac3049b2d96df783e58f0'
+  version '2.4.4'
+  sha256 'd7bce1e17334a5865ffaee0d76980fefaafaad9b91ba797efbbdf882f8321804'
 
   url "https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-mac-#{version}.dmg"
   name 'Garmin Connect IQ SDK'
