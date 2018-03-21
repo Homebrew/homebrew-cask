5079cd7b9f6c813e8d8130513538f6563dd1bae7
miccal <miccal.matthews@gmail.com>
2018-03-22 06:22:20 +0800

    Update iina.rb (#45035)

--- Casks/iina.rb
+++ Casks/iina.rb
@@ -5,11 +5,12 @@ cask 'iina' do
   # dl-portal.iina.io was verified as official when first introduced to the cask
   url "https://dl-portal.iina.io/IINA.v#{version}.dmg"
   appcast 'https://www.iina.io/appcast.xml',
-          checkpoint: 'f0333dfcf796aeee23f4fbf9115198b4143744dfc5198afa5ad7425688206b6b'
+          checkpoint: '369904f1c892883ae33a7ac6c33eb09b3517bc2651a66edfc1e409554ac64980'
   name 'IINA'
   homepage 'https://lhc70000.github.io/iina/'
 
   auto_updates true
+  conflicts_with cask: 'iina-beta'
   depends_on macos: '>= :yosemite'
 
   app 'IINA.app'
@@ -22,7 +23,7 @@ cask 'iina' do
                '~/Library/Cookies/com.colliderli.iina.binarycookies',
                '~/Library/Logs/DiagnosticReports/IINA*.crash',
                '~/Library/Preferences/com.colliderli.iina.plist',
-               '~/Library/Safari/Extensions/Open in IINA.safariextz',
+               '~/Library/Safari/Extensions/Open in IINA*.safariextz',
                '~/Library/Saved Application State/com.colliderli.iina.savedState',
              ]
 end
