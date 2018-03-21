efccd42ee4e86bfa4705f5816e0509e15b6a6596
Timothée Boucher <tim@superhuman.com>
2018-03-21 11:56:27 -0700

    Update Superhuman latest
    This was out-of-date (and wrong actually wrt. Milanote).
    Latest version doesn't require the same files to be deleted.

--- Casks/superhuman.rb
+++ Casks/superhuman.rb
@@ -9,8 +9,10 @@ cask 'superhuman' do
   app 'Superhuman.app'
 
   zap trash: [
-               '~/Library/Application Support/Milanote',
+               '~/Library/Application Support/Superhuman',
                '~/Library/Caches/Superhuman',
-               '~/Library/Preferences/io.nwjs.nwjs.plist',
+               '~/Library/Preferences/com.superhuman.electron.helper.plist',
+               '~/Library/Preferences/com.superhuman.electron.plist',
+               '~/Library/Preferences/com.superhuman.mail.plist',
              ]
 end
