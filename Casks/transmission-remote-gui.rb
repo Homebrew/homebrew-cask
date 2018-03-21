241732b7222cedeb2949f2129e44eafb23cb83ea
Peter Dave Hello <hsu@peterdavehello.org>
2018-03-21 12:12:53 +0800

    [Author] Update Transmission Remote GUI to v5.15.0

--- Casks/transmission-remote-gui.rb
+++ Casks/transmission-remote-gui.rb
@@ -1,10 +1,10 @@
 cask 'transmission-remote-gui' do
-  version '5.14.0'
-  sha256 '6213845c24243cdd24bef5010fb65a63244899be84ed73158874c32eb9f88854'
+  version '5.15.0'
+  sha256 '91da842ad8701bb253e999ca7614b9a2d5483d1d85120cd9d18cdceb6e42f26b'
 
   url "https://github.com/transmission-remote-gui/transgui/releases/download/v#{version}/transgui-#{version}.dmg"
   appcast 'https://github.com/transmission-remote-gui/transgui/releases.atom',
-          checkpoint: 'e83a537a586f875e6a0cb62b43477aef5a6e492732e77207c3b9fc12d9ed3ae1'
+          checkpoint: '46c3417c57b7cb424b770f2dd22a8e73cbbb3adaf31b6d5728aeafa1101eb402'
   name 'Transmission Remote GUI'
   homepage 'https://github.com/transmission-remote-gui/transgui'
 
