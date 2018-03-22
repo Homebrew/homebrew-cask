85dacde8ce74c7a35f70d45cee3bbe340e9639a1
Xiang <xiang.chen7531@gmail.com>
2018-03-22 16:02:08 +0100

    Update resilio-sync to 2.5.12(1191)

--- Casks/resilio-sync.rb
+++ Casks/resilio-sync.rb
@@ -1,6 +1,6 @@
 cask 'resilio-sync' do
-  version :latest
-  sha256 :no_check
+  version '2.5.12(1191)'
+  sha256 'f6c7a0b0e0b11399cbe5f3250b4067293d0c8a80b8a5ea3a938476e7923b0dba'
 
   url 'https://download-cdn.resilio.com/stable/osx/Resilio-Sync.dmg'
   name 'Resilio Sync'
