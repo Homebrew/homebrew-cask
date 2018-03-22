f4ab867add0b45e1d9c85ac607510c0f1bcf25a2
Yaw Anokwa <yanokwa@users.noreply.github.com>
2018-03-21 23:10:51 -0700

    Update tableau-public to 10.5.2 (#45066)

--- Casks/tableau-public.rb
+++ Casks/tableau-public.rb
@@ -1,6 +1,6 @@
 cask 'tableau-public' do
-  version '10.4.1'
-  sha256 'a3aa1c0c61659bb39ff9ca57775841e389e977954b7e60c3e28665b9e30cabcd'
+  version '10.5.2'
+  sha256 '55da0712ba595564517c87a17ae49d1ad2129dfa9f70630002fb2ccf0451dd57'
 
   url "https://downloads.tableau.com/public/TableauPublic-#{version.dots_to_hyphens}.dmg"
   name 'Tableau Public'
