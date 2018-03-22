0f387e9cc6d6748aa0c2cc3ac5a5a498d1f28d5d
Maarten Paauw <maartenpaauw@gmail.com>
2018-03-22 13:56:41 +0100

    Update telegram-desktop to 1.2.14

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,11 +1,11 @@
 cask 'telegram-desktop' do
-  version '1.2.6'
-  sha256 '6909060fbf5ff65b4d100ad29e82ae7b0415f988ea55c1f0a01afbcb9d3bfd4c'
+  version '1.2.14'
+  sha256 '6e5e461f164bce8b67632839d64510bbd196d33d631560a44ba984d09a30cfcf'
 
   # github.com/telegramdesktop/tdesktop/releases/download was verified as official when first introduced to the cask
   url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup.#{version}.dmg"
   appcast 'https://github.com/telegramdesktop/tdesktop/releases.atom',
-          checkpoint: '5814f6dd082b575004408ad924058020564d980acc9cf9c9e97b1f18e52e52e6'
+          checkpoint: 'c94d60475ad8ab334a789404c6bba1df2cb3c5d2208ac4119322a33d76a02437'
   name 'Telegram Desktop'
   homepage 'https://desktop.telegram.org/'
 
