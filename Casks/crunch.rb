d7258a8023bbac3778be4908a8425b0a9f17229a
Chris Simpkins <git.simpkins@gmail.com>
2018-03-23 01:50:38 -0400

    Update crunch to 0.10.0 (#45103)

--- Casks/crunch.rb
+++ Casks/crunch.rb
@@ -1,10 +1,10 @@
 cask 'crunch' do
-  version '0.9.0'
-  sha256 'b29d0c72d04f3e0e3347f44028930526adb04a39dedb4cc6e2bc0fa51245f9f7'
+  version '0.10.0'
+  sha256 '58b25a97f5bb797c270711f54989ce1bea1e3ed476aa4f6ce4865b0b3ffa650a'
 
   url "https://github.com/chrissimpkins/Crunch/releases/download/v#{version}/Crunch-Installer.dmg"
   appcast 'https://github.com/chrissimpkins/Crunch/releases.atom',
-          checkpoint: 'c75f897b89e6c810b134c92d933c2307481ca7f72c2600831c2706daf0ed534b'
+          checkpoint: '508addbe4f2021aa0614a66b93a261d5c1a6c3ebddf68884e21d9bd3e004d55f'
   name 'Crunch'
   homepage 'https://github.com/chrissimpkins/Crunch'
 
