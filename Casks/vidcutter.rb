64ee90ac6ca1ada7b9e5cdbdb3b52cbf0bf513ca
commitay <commitay@users.noreply.github.com>
2018-03-24 23:18:25 +1000

    Update vidcutter.rb

--- Casks/vidcutter.rb
+++ Casks/vidcutter.rb
@@ -3,7 +3,7 @@ cask 'vidcutter' do
   sha256 '66ec76e878ccf70a7ebadda94f4b599426201cc3b30b52c42d84c4a7142e6f1b'
 
   # github.com/ozmartian/vidcutter was verified as official when first introduced to the cask
-  url 'https://github.com/ozmartian/vidcutter/releases/download/5.5.0/VidCutter-5.5.0-macOS.dmg'
+  url "https://github.com/ozmartian/vidcutter/releases/download/#{version}/VidCutter-#{version}-macOS.dmg"
   appcast 'https://github.com/ozmartian/vidcutter/releases.atom',
           checkpoint: '76cd38a8f0820106c0cb92f17292a7b3bb78dadb5736bb84b9a3b98edab8ea8c'
   name 'VidCutter'
