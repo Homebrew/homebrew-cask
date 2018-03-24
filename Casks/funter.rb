ce83e49e309622782ac839fbc7cb956c1735d430
伊藤洋 <mail@hiro20.com>
2018-03-24 15:10:42 +0900

    Add Funter

---
+++ Casks/funter.rb
@@ -0,0 +1,10 @@
+cask 'funter' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://nektony.com/download/funter/Funter.dmg'
+  name 'Funter'
+  homepage 'https://nektony.com/products/funter'
+
+  app 'Funter.app'
+end
