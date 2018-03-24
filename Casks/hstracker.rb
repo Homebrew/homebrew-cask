8b9d0e556e5551d08122a0803882a81e0b18e5fb
Richard Lee <14349+dlackty@users.noreply.github.com>
2018-03-24 20:01:00 +0800

    Update hstracker to 1.3.5 (#45152)

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -1,11 +1,11 @@
 cask 'hstracker' do
-  version '1.3.4'
-  sha256 '1c7fb69f5de42592d647c4fcf5665af0bbfc65363361e2b73507c307bf141d79'
+  version '1.3.5'
+  sha256 '610ff12a929d71f6ef2c17eb1b74b6a4cf198e59b0e195896756595c1534b1e2'
 
   # github.com/HearthSim/HSTracker was verified as official when first introduced to the cask
   url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip"
   appcast 'https://github.com/HearthSim/HSTracker/releases.atom',
-          checkpoint: '32fde260af3d782fd2be575e43b0b8c4ac59fafba2bc487b0b7eead1e7ea6068'
+          checkpoint: 'a444c12b43cd494206b62aee8333b85a097bba7600ef2617d436ac2a5146a8bb'
   name 'Hearthstone Deck Tracker'
   homepage 'https://hsdecktracker.net/'
 
