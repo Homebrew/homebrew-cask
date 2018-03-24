2c3272cc2dfb5905d0515388b7927ef6890682b1
commitay <commitay@users.noreply.github.com>
2018-03-25 08:28:20 +1000

    Update inpad.rb

--- Casks/inpad.rb
+++ Casks/inpad.rb
@@ -2,12 +2,11 @@ cask 'inpad' do
   version '0.1.0'
   sha256 'a66a0be66bc98d31f36232ab9b5ea5a47fdb644e4234673d8ccc26bd637841f1'
 
-  # github.com/BoostIO/Boost was verified as official when first introduced to the cask
   url "https://github.com/BoostIO/Boost/releases/download/v#{version}/Inpad-#{version}.dmg"
   appcast 'https://github.com/BoostIO/Boost/releases.atom',
           checkpoint: 'cc8f63649c914eb792a14de4666f0671c869aa265de95496470157b19cd09299'
   name 'Inpad'
-  homepage 'https://sarah-seo.github.io/Inpad/'
+  homepage 'https://github.com/BoostIO/Boost'
 
   app 'Inpad.app'
 end
