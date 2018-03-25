d71417a85e09ce606ae593683a9585482cc1baeb
Nick Duijvelshoff <nick@duijvelshoff.com>
2018-03-26 00:38:45 +0200

    Fix comment for url BunqDesktop.app Cask

--- Casks/bunqdesktop.rb
+++ Casks/bunqdesktop.rb
@@ -2,6 +2,7 @@ cask 'bunqdesktop' do
   version '0.8.1'
   sha256 '15c6e2ccfcef741bf7fb839ad95ca8a8f514673dfdc8e09460cdf87830c28f74'
 
+  # github.com was verified as official when first introduced to the cask
   url 'https://github.com/BunqCommunity/BunqDesktop/releases/download/0.8.1/BunqDesktop-0.8.1.dmg'
   name 'BunqDesktop'
   homepage 'https://bunqdesktop.com/'
