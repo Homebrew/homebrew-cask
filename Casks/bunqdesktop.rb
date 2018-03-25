b64daae19dd7abf59d857c498c97db0fb52051f7
Nick Duijvelshoff <nick@duijvelshoff.com>
2018-03-26 00:16:45 +0200

    Add BunqDesktop.app v0.8.1

---
+++ Casks/bunqdesktop.rb
@@ -0,0 +1,10 @@
+cask 'bunqdesktop' do
+  version '0.8.1'
+  sha256 '15c6e2ccfcef741bf7fb839ad95ca8a8f514673dfdc8e09460cdf87830c28f74'
+
+  url 'https://github.com/BunqCommunity/BunqDesktop/releases/download/0.8.1/BunqDesktop-0.8.1.dmg'
+  name 'BunqDesktop'
+  homepage 'https://bunqdesktop.com/'
+
+  app 'BunqDesktop.app'
+end
