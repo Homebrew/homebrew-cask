de5e604f0b1631b8d6c6a2f7990fd90f994e506a
Louis Fischer <louis@tylto.com>
2018-03-25 15:36:54 +0200

    Add Meta :latest

---
+++ Casks/meta.rb
@@ -0,0 +1,10 @@
+cask 'meta' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.nightbirdsevolve.com/meta/updates/latest/'
+  name 'Meta'
+  homepage 'https://www.nightbirdsevolve.com/meta/'
+
+  app 'Meta.app'
+end
