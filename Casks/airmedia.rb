544d2d9d2c08816c5baf85d8516bf9d2a9527138
大東祐太 <h-1456@H-1456-ohigashi-yuta.local>
2018-03-26 12:13:35 +0900

    Update airmedia url

--- Casks/airmedia.rb
+++ Casks/airmedia.rb
@@ -2,7 +2,7 @@ cask 'airmedia' do
   version '3.1.2'
   sha256 '32869437df42f3a6321551a9251471e088e9faa16ebffc69ae1dd26b7bd0e750'
 
-  url "https://www.crestron.com/downloads/software/airmedia_#{version}_macOS_guest_dmg.zip"
+  url "https://p.widencdn.net/c0b4qy/software_airmedia_macOS_#{version}_guest.dmg"
   name 'Crestron AirMedia'
   homepage 'https://www.crestron.com/microsites/airmedia-mobile-wireless-hd-presentations'
 
