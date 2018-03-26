ad3779a6235e283f88d2d2e19c6b3537701189ca
Joseph Page <joseph.page@rentacar.fr>
2018-03-26 11:52:38 +0200

    Add Crisp.app

---
+++ Casks/crispchat.rb
@@ -0,0 +1,10 @@
+cask 'crispchat' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://storage.crisp.chat/apps/mac/Crisp.dmg'
+  name 'Crisp'
+  homepage 'https://crisp.chat/'
+
+  app 'Crisp.app'
+end
