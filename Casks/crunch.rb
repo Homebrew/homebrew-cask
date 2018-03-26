14419fb8ed636570b46bc07b83dca8e236e7686f
Chris Simpkins <git.simpkins@gmail.com>
2018-03-26 00:16:45 -0400

    updated appcast checkpoint for crunch.rb v1.0.0
    based upon output from `brew cask _appcast_checkpoint --calculate crunch`

--- Casks/crunch.rb
+++ Casks/crunch.rb
@@ -4,7 +4,7 @@ cask 'crunch' do
 
   url "https://github.com/chrissimpkins/Crunch/releases/download/v#{version}/Crunch-Installer.dmg"
   appcast 'https://github.com/chrissimpkins/Crunch/releases.atom',
-          checkpoint: '32a4d6304199eec025b2bbcecc1984e51558816e14cc0efbeac37c796bf5f0e4'
+          checkpoint: '163804ec141e7f909a6707cf74f3bd0b0a07944e5c4e18fd4fecde781dfad0b0'
   name 'Crunch'
   homepage 'https://github.com/chrissimpkins/Crunch'
   
