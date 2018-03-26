04965c4caaaa630afb7a8b5ebf69592aeacba04b
大東祐太 <h-1456@H-1456-ohigashi-yuta.local>
2018-03-26 12:22:32 +0900

    update correct sha256

--- Casks/airmedia.rb
+++ Casks/airmedia.rb
@@ -1,6 +1,6 @@
 cask 'airmedia' do
   version '3.1.2'
-  sha256 '32869437df42f3a6321551a9251471e088e9faa16ebffc69ae1dd26b7bd0e750'
+  sha256 '7e36017df29860cfc7bf613b1da160cdab80f633fa5f21f6f734eca933a1dda4'
 
   url "https://p.widencdn.net/c0b4qy/software_airmedia_macOS_#{version}_guest.dmg"
   name 'Crestron AirMedia'
