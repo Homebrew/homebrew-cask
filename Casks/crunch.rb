3c5685e323fc701dc29e67cf82af3a0245d276d5
commitay <commitay@users.noreply.github.com>
2018-03-26 14:38:38 +1000

    Update crunch.rb

--- Casks/crunch.rb
+++ Casks/crunch.rb
@@ -7,8 +7,11 @@ cask 'crunch' do
           checkpoint: '163804ec141e7f909a6707cf74f3bd0b0a07944e5c4e18fd4fecde781dfad0b0'
   name 'Crunch'
   homepage 'https://github.com/chrissimpkins/Crunch'
-  
-  depends_on formula: [ 'libpng', 'little-cms2' ]
+
+  depends_on formula: [
+                        'libpng',
+                        'little-cms2',
+                      ]
 
   app 'Crunch.app'
 end
