4bb8a3f03fc5a1d28efb0bf342ff843151064234
Joseph J.C. Tang <jinntrance@gmail.com>
2018-03-27 22:40:45 +0800

    Update airdroid to 3.6.2.0

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask 'airdroid' do
-  version '3.6.1.0'
-  sha256 '2f660e8d9159b8041dce81737a1b02c864a2918dd9f7eb49c60eb9b9f5550320'
+  version '3.6.2.0'
+  sha256 '0370fe1c2946c7edd442040e0c2699b01f5e814216d7fdf86164153607ffb6ff'
 
   # s3.amazonaws.com/dl.airdroid.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
