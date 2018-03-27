465539f7e2b002d661f0f118fb4820669a9063f9
Brian Morton <brianmortondesign@gmail.com>
2018-03-27 09:28:17 -0700

    Update zotero to 5.0.40

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,10 +1,10 @@
 cask 'zotero' do
-  version '5.0.38'
-  sha256 'dc6629182015822ace8170c21498e9aa0a420386f39f05cb01c16ea9420eeb09'
+  version '5.0.40'
+  sha256 '9a41227e529cb8186df95d30bcfb6998c070ce2834e9a7f44aa15f540219e2ec'
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast 'https://github.com/zotero/zotero/releases.atom',
-          checkpoint: '02fbad90c178d6f4a212b0c18d094db6911701e30f863beadb31d3e3be55c103'
+          checkpoint: '39afdac688d4bead9238a2a95a8121837b53850183594983e4d59026e5debb9d'
   name 'Zotero'
   homepage 'https://www.zotero.org/'
 
