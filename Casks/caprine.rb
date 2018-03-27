d4c2a84563ac2e1e7c6d32f92a7d0f2759222933
Gobinath Mallaiyan <GMallai1@ITS.JNJ.com>
2018-03-27 18:48:29 -0400

    Update caprine to 2.11.0

--- Casks/caprine.rb
+++ Casks/caprine.rb
@@ -1,10 +1,10 @@
 cask 'caprine' do
-  version '2.10.0'
-  sha256 'c13fbd3e4492dbe100f5594f4e6f2b38afb1e7dc9476d3a3096169225399463d'
+  version '2.11.0'
+  sha256 'f89dce60ecce7a8c5e0c8b049d460d3bf3b629779903c9f7a666b13c272665b1'
 
   url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/Caprine-#{version}.dmg"
   appcast 'https://github.com/sindresorhus/caprine/releases.atom',
-          checkpoint: '23607bbeb299f67402a719640f87c777f90947d8490736c99cd5f10a2c17e53d'
+          checkpoint: '83b67cdaaf421bfbdfb0824f5f141607b3ea562c8b51affb0cf0bad39d63bc3a'
   name 'Caprine'
   homepage 'https://github.com/sindresorhus/caprine'
 
