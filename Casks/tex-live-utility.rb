0efd7b638b855543baee4b149c06bf6f6e088d87
miccal <miccal.matthews@gmail.com>
2018-03-27 15:43:58 +0800

    Update tex-live-utility.rb

--- Casks/tex-live-utility.rb
+++ Casks/tex-live-utility.rb
@@ -3,8 +3,8 @@ cask 'tex-live-utility' do
   sha256 '6e587b646ba1f6b74ceb42eaeaf03f21ac92457f263c098155b942bf171f6073'
 
   url "https://github.com/amaxwell/tlutility/releases/download/#{version}/TeX.Live.Utility.app-#{version}.tar.gz"
-  appcast 'https://raw.githubusercontent.com/amaxwell/tlutility/master/appcast/tlu_appcast.xml',
-          checkpoint: '80246f0d1b0ffccd1b025d77f4331df40e31fd6698a03d5d9cade7d892ea346e'
+  appcast 'https://github.com/amaxwell/tlutility/releases.atom',
+          checkpoint: '982731a785be11020da9aee3d2814e08061a599d197cc394f2047f36deb23095'
   name 'TeX Live Utility'
   homepage 'https://github.com/amaxwell/tlutility'
 
