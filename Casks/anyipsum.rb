0d39dcf9588bcb9ddb3f57629cf5d6f9744f53f3
Joacim Löwgren <joacim.lowgren@gmail.com>
2018-03-28 15:09:01 +0200

    Update anyipsum to 0.2.0 (#45286)

--- Casks/anyipsum.rb
+++ Casks/anyipsum.rb
@@ -1,6 +1,6 @@
 cask 'anyipsum' do
   version '0.2.0'
-  sha256 '967d12bc531f239e1b89c1e6dd6f548e423064df95506b0f08fd801474846dfa'
+  sha256 '14beb22ab22fbb289e05ad658fcb7a52625f4f090bdc2d26ed5b1e2069d17eff'
 
   url "https://github.com/jlowgren/AnyIpsum/releases/download/v#{version}/AnyIpsum.dmg"
   appcast 'https://github.com/jlowgren/AnyIpsum/releases.atom',
