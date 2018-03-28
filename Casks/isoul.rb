fa410667085690ace130689b0e24a577beac46ec
Christopher Kolmatycki <chris@chriskol.com>
2018-03-28 16:21:12 -0300

    Add iSoul.app v0.5.0
    Added cask for iSoul Soulseek client app.  Personal favourite client I've been using for years now.

---
+++ Casks/isoul.rb
@@ -0,0 +1,10 @@
+cask 'isoul' do
+  version '0.5.0'
+  sha256 '3d86ed7f9a2b62cf6f824a63e67ae1757d50316e7490855fc7a0b2db28473425'
+
+  url 'https://github.com/downloads/arranger1044/iSoul/iSoul%200.5.0.zip'
+  name 'iSoul'
+  homepage 'https://github.com/arranger1044/iSoul'
+
+  app 'iSoul.app'
+end
