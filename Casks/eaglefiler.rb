581b33774b75b6ca1c8af1e38714541990a9a0eb
commitay <commitay@users.noreply.github.com>
2018-03-29 10:11:11 +1000

    Update eaglefiler.rb

--- Casks/eaglefiler.rb
+++ Casks/eaglefiler.rb
@@ -1,5 +1,5 @@
 cask 'eaglefiler' do
-  version '1.8.2'
+  version '1.8.3'
   sha256 '6f84a7da832faeb12b012a92d21b373ee3988a73b76fd60a572e7c80eb6e6ca4'
 
   url "https://c-command.com/downloads/EagleFiler-#{version}.dmg"
