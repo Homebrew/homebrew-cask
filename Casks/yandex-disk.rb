42af04ff139bc1b715db100439a20bcaee80c94b
Alexander Myltsev <a.myltsev@gmail.com>
2018-03-29 15:04:34 +0300

    Update yandex-disk to latest

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -2,7 +2,7 @@ cask 'yandex-disk' do
   version :latest
   sha256 :no_check
 
-  url 'https://disk.yandex.ru/download/Yandex.Disk.Mac.dmg?instant=1'
+  url 'https://disk.yandex.ru/download/YandexDisk30.dmg/?instant=1'
   name 'Yandex.Disk'
   homepage 'https://disk.yandex.ru/'
 
