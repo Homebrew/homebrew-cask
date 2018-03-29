74b293fc3869cc95273220605f532959a0b1ca42
Igotit <aochin@gmail.com>
2018-03-29 18:15:28 +0800

    Update qingg to 2.6.0

--- Casks/qingg.rb
+++ Casks/qingg.rb
@@ -1,10 +1,10 @@
 cask 'qingg' do
-  version '2.5.0'
-  sha256 '98f3eb1a56a38dd230aac44e571001859558e28feb9e756078998bf4b9a2cd7a'
+  version '2.6.0'
+  sha256 '4cb0f6399c4f59bcfd2ecb557702adce6a7d9ccf718019827ec54630a1d80ea9'
 
   url "https://qingg.im/download/Qingg-#{version}.dmg"
   appcast 'https://qingg.im/sparkle/appcast.php',
-          checkpoint: '751eb771d487c59dd6fafab1fd910807813f70d198c869081bcbb8d6f28066be'
+          checkpoint: '447bd53ca90a8a5d919c765f8a01f9d09c0c47d3ef10af5eab8b39e3ce9b724a'
   name 'QinggIM'
   name '清歌输入法'
   homepage 'https://qingg.im/mac/'
