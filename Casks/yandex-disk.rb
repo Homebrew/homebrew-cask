9d6cdbcc948c44db81b74dab3baedc18d7768806
Vítor Galvão <info@vitorgalvao.com>
2018-03-29 13:35:32 +0100

    Update yandex-disk.rb

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -2,7 +2,7 @@ cask 'yandex-disk' do
   version '3.0'
   sha256 '4cc3faad156c7c8fcddece0233c8b4042d239b4fa61f9772f6ae2dd59b9cc0f2'
 
-  url 'https://disk.yandex.ru/download/YandexDisk30.dmg/?instant=1'
+  url "https://disk.yandex.ru/download/YandexDisk#{version.no_dots}.dmg/?instant=1"
   name 'Yandex.Disk'
   homepage 'https://disk.yandex.ru/'
 
