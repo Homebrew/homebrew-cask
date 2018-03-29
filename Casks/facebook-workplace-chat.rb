d4e5c83ae15fe289dddd03b706fc54f4d8f50a3e
Flavio De Stefano <destefano.flavio@gmail.com>
2018-03-29 12:29:56 +0200

    Adding Facebook Workplace

---
+++ Casks/facebook-workplace-chat.rb
@@ -0,0 +1,10 @@
+cask 'facebook-workplace-chat' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.facebook.com/work/chat/download/mac'
+  name 'Facebook Workplace Chat'
+  homepage 'https://www.facebook.com/workplace/chat-app'
+
+  app 'Workplace Chat.app'
+end
