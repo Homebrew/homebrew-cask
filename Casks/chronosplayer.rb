f1cc24d2b3d9e66978644fc55891a65a29dc6c62
Nisar Ahmed <nisar.ahmed@advisernetwork.com.au>
2018-03-30 17:01:57 +1100

    Add ChronosPlayer.app v1.2

---
+++ Casks/chronosplayer.rb
@@ -0,0 +1,10 @@
+cask 'chronosplayer' do
+  version '1.4.110'
+  sha256 '9cab46861d52ad0f0fe0e67705fb382e0dc23752851a61f0724a9378558eded6'
+
+  url 'http://chronosplayer.s3-website-us-east-1.amazonaws.com/ChronosPlayer.zip'
+  name 'Chronos Player'
+  homepage 'http://chronosplayer.s3-website-us-east-1.amazonaws.com/'
+
+  app 'ChronosPlayer.app'
+end
