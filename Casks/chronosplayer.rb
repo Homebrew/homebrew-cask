8f8d15ec24fdca395e3ccb424e518ac268e38bf9
Nisar Ahmed <nisar.med@gmail.com>
2018-03-30 17:20:12 +1100

    Updating ChronosPlayer.app sha256 Checksum

--- Casks/chronosplayer.rb
+++ Casks/chronosplayer.rb
@@ -1,6 +1,6 @@
 cask 'chronosplayer' do
   version '1.4.110'
-  sha256 '9cab46861d52ad0f0fe0e67705fb382e0dc23752851a61f0724a9378558eded6'
+  sha256 '6456860df04a6c1e30a4b0decb980b80b683fc5e8ca31961b30079cdd611084e'
 
   url 'http://chronosplayer.s3-website-us-east-1.amazonaws.com/ChronosPlayer.zip'
   name 'Chronos Player'
