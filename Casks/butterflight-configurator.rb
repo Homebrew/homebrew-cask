f0851814804b91189ab10970970399372a80fe16
Saqoosha <a@saqoo.sh>
2018-03-30 12:17:19 +0900

    Update butterflight-configurator to 10.3.0

--- Casks/butterflight-configurator.rb
+++ Casks/butterflight-configurator.rb
@@ -1,10 +1,10 @@
 cask 'butterflight-configurator' do
-  version '10.2.0'
-  sha256 'd69c6bd6024b6b5595e82de2acb3fa8e53d92acac7cfe2233927c845152fd40d'
+  version '10.3.0'
+  sha256 '5d7c56abe689e26e03370733187faa42eb54e8de9468018532029cc8c8d3da41'
 
   url "https://github.com/ButterFlight/butterflight-configurator/releases/download/v#{version}/butterflight-configurator_#{version}_macOS.dmg"
   appcast 'https://github.com/ButterFlight/butterflight-configurator/releases.atom',
-          checkpoint: '0ede3dca5c9f5d9a8cff550a32b6e35cd2b6fb118c879c4269c920c01c322949'
+          checkpoint: '44019481db843133281f89797a56e6ce2acd37961353b571f918311fc489f3e3'
   name 'ButterFlight Configurator'
   homepage 'https://github.com/ButterFlight/butterflight-configurator'
 
