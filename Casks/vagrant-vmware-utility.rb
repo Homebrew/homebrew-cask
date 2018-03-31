f99112a36f88070d82156281483c87ed5e6e83a8
commitay <commitay@users.noreply.github.com>
2018-03-31 18:22:36 +1000

    Update vagrant-vmware-utility: appcast and url

--- Casks/vagrant-vmware-utility.rb
+++ Casks/vagrant-vmware-utility.rb
@@ -3,7 +3,9 @@ cask 'vagrant-vmware-utility' do
   sha256 'd52415922f2c607b717ee709dff21af2064709fb639954b7832d4c8c6b7f1a91'
 
   # releases.hashicorp.com/vagrant-vmware-utility was verified as official when first introduced to the cask
-  url 'https://releases.hashicorp.com/vagrant-vmware-utility/1.0.0/vagrant-vmware-utility_1.0.0_x86_64.dmg'
+  url "https://releases.hashicorp.com/vagrant-vmware-utility/#{version}/vagrant-vmware-utility_#{version}_x86_64.dmg"
+  appcast 'https://releases.hashicorp.com/vagrant-vmware-utility/',
+          checkpoint: 'e5a7bb35a2fa88275fc88e49ecc709699a7d9331e7d749ab59c488d0087325e5'
   name 'Vagrant VMware Utility'
   homepage 'https://www.vagrantup.com/vmware/downloads.html'
 
