b7d3c454768b8cf3180006fc5e696472ebb428ee
Brian Morton <brianmortondesign@gmail.com>
2018-04-03 09:26:36 -0700

    Update pinegrow to 4.6

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -1,6 +1,6 @@
 cask 'pinegrow' do
-  version '4.5'
-  sha256 '770dc7fc225f070de2d62c5a976e7179afd1b7ee74935b7b0da255012625f8ef'
+  version '4.6'
+  sha256 '81a2956dcf446f54a9c1e02fabede7a0259d0e8020d99a6c36780ee173e08fff'
 
   url "http://download.pinegrow.com/PinegrowMac.#{version}.dmg"
   name 'Pinegrow'
