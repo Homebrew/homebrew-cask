a5b1db5808c4a3c544a9424a37b2079d1549f326
Gobinath Mallaiyan <GMallai1@ITS.JNJ.com>
2018-04-03 07:20:52 -0400

    Update yakyak to 1.5.0

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,10 +1,10 @@
 cask 'yakyak' do
-  version '1.4.3'
-  sha256 'a8e220046279f2794087c8609bcc643e1171e83a84b93967b2c169362289a411'
+  version '1.5.0'
+  sha256 'c465634ced9dc21924ef299e7200ec7208b5ec685468c3f1c5c887ddb7ca80d5'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom',
-          checkpoint: '50e926210260f0821aa210704f2b92ef094feac07130d939c29fe52250ca595e'
+          checkpoint: '55ec10da868d87d6566058e5af1e1b7e3d66286ff2d7dea40f53879a75938b57'
   name 'Yakyak'
   homepage 'https://github.com/yakyak/yakyak'
 
