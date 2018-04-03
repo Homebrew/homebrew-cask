4a36cb107f21657f4052c20feb5e749d862cfdb5
Brian Morton <brianmortondesign@gmail.com>
2018-04-03 10:18:31 -0700

    Update minergate to 8.0 (#45481)

--- Casks/minergate.rb
+++ Casks/minergate.rb
@@ -1,6 +1,6 @@
 cask 'minergate' do
-  version '7.2'
-  sha256 '9e65d63c366fe7bd0093899d7a0e8082dfbcf05d4667f1a1ca7d0d72f0012be8'
+  version '8.0'
+  sha256 '2211c1be48c09225dcd27e6223a8243d40334932951fba6257e53220a55afbe7'
 
   url "https://download.minergate.com/mac/#{version}"
   name 'MinerGate'
