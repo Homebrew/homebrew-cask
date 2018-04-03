96acdf35b228fd548d50b0f2296ce9fe3442a312
Gobinath Mallaiyan <GMallai1@ITS.JNJ.com>
2018-04-03 12:56:47 -0400

    Update versions to 1.4.1-1144

--- Casks/versions.rb
+++ Casks/versions.rb
@@ -1,10 +1,10 @@
 cask 'versions' do
-  version '1.4.0-1126'
-  sha256 '2012bbf92bba601e781fbd74a956fe2702f283c3c1017fa1735f3c3f96d2f73f'
+  version '1.4.1-1144'
+  sha256 '84d4320883b97140b920f3bf4fa95285cb4ecfc9a13c58ef346288f5d37db208'
 
   url "https://cdn.versionsapp.com/releases/Versions-#{version}.zip"
   appcast 'https://updates.blackpixel.com/updates?app=vs',
-          checkpoint: 'b30875c95642cb07ffebc209298634efbc34650d152a53c511dde8002abea32d'
+          checkpoint: '12dc188d68288f0b003c64bca71de5fb8a44695813fe4c8493b0d2b809b8a2cb'
   name 'Versions'
   homepage 'https://versionsapp.com/'
 
