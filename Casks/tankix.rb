f16a7b4569d0b980aed1fe5438ddc146dc87d11a
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-04 17:23:53 +0100

    Update tankix to andromeda1-15620 (#45546)

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version 'hydra1-15154'
-  sha256 'a1d424639d64485b7192d04abadc41d8f45b44a9437dc0062e1b68afb8ecb2af'
+  version 'andromeda1-15620'
+  sha256 '3410dcbd1c6d446f2f976d03b8c011866577020b95393809efad5f1af433c379'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
   name 'Tanki X'
