a82f92f70d9bad556161f6f0bbdcfca67f71ee73
Felipe Turcheti <fturcheti@gmail.com>
2018-04-04 11:13:49 -0300

    Update oni to 0.3.2

--- Casks/oni.rb
+++ Casks/oni.rb
@@ -1,11 +1,11 @@
 cask 'oni' do
-  version '0.3.1'
-  sha256 '2364a5a0f9c0e4c31c2714f5741b665e431bd0a16ee65eea41e609b0cdbcbe8e'
+  version '0.3.2'
+  sha256 '86f45c6e933f128402a48ec4e62028acd0a4e2e90b6d17f6bb5db08bd9ac8fff'
 
   # github.com/onivim/oni was verified as official when first introduced to the cask
   url "https://github.com/onivim/oni/releases/download/v#{version}/Oni-#{version}-osx.dmg"
   appcast 'https://github.com/onivim/oni/releases.atom',
-          checkpoint: 'e2325811be2ee1bce9e853f4c8f483509dbbfd4781eb6ae2208e7bfe37e05bf0'
+          checkpoint: 'bed10e7113934eb3b96bdc5841d6784e870370800ba43f905a752642898a030a'
   name 'Oni'
   homepage 'https://www.onivim.io/'
 
