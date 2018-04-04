a9541ee65e39a32de93af649f64d1139c4a5b99f
commitay <commitay@users.noreply.github.com>
2018-04-04 12:24:33 +1000

    Update visual-paradigm-ce.rb

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -3,7 +3,7 @@ cask 'visual-paradigm-ce' do
   sha256 '57cccbccab79ed8d68526f67db2e676beddd318d5f76b64937de1efc14105722'
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vpce#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
-  appcast 'https://ca1.visual-paradigm.com/visual-paradigm/vpce15.0/20180401/checksum.html',
+  appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html',
           checkpoint: '9b577e6b55159f4020e176554943e972b951ee2d80563232e4ffa812f85f10ef'
   name 'Visual Paradigm Community Edition'
   homepage 'https://www.visual-paradigm.com/'
