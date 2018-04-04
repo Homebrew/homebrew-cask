8b216e83398c19ff3239ccd00b49cadb41f1b63e
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2018-04-04 00:05:57 -0400

    Update kap to 2.1.1 (#45524)

--- Casks/kap.rb
+++ Casks/kap.rb
@@ -1,11 +1,11 @@
 cask 'kap' do
-  version '2.0.0'
-  sha256 '02be9b2d4fdff6846686b33d24e95558da9a029370b0bb3bb75babab135d3a43'
+  version '2.1.1'
+  sha256 '1c04b17bb611a664c570021648e743fe8fa1ac4b7b82f57ac6796da4691374b2'
 
   # github.com/wulkano/kap was verified as official when first introduced to the cask
   url "https://github.com/wulkano/kap/releases/download/v#{version.major_minor_patch}/Kap-#{version}.dmg"
   appcast 'https://github.com/wulkano/kap/releases.atom',
-          checkpoint: 'c42f603ddd744a11dbcdef5fdbe41ac0e542c9e15d37fc3fec935a28a71215ee'
+          checkpoint: '49710199f0fd6a59df5a01c11a64fa6f0fc1514d7ff476efe135aec3f9e367b1'
   name 'Kap'
   homepage 'https://getkap.co/'
 
