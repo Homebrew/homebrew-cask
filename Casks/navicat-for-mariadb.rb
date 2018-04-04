765841f1a1040447f91c5e8262b302f2c8c4e21c
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-04 21:34:05 +0100

    Update navicat-for-mariadb to 12.0.24 (#45560)

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,10 +1,10 @@
 cask 'navicat-for-mariadb' do
-  version '12.0.23'
-  sha256 '073f84f5140093c0c3cdafa5c3794a539370c0512a33c9a64d417c7453dcc90a'
+  version '12.0.24'
+  sha256 '466d8ab7903f0045edcedabe911231b3b0025a101908212a946308de1459926d'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
   appcast 'https://www.navicat.com/en/products/navicat-for-mariadb-release-note',
-          checkpoint: '7f3443223f0b97327f291d415c28671ef9290f70ddb3801a336a2f13ebd0f1dc'
+          checkpoint: 'f7a31c3526925ac0a0be0481d6a0d90ffd833a5ececf983f7d708ecf0c7e5065'
   name 'Navicat for MariaDB'
   homepage 'https://www.navicat.com/products/navicat-for-mariadb'
 
