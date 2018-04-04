926ec636b111d30f7ef3ea0c7828710c23803f91
Gobinath Mallaiyan <GMallai1@ITS.JNJ.com>
2018-04-04 11:15:57 -0400

    Update inboxer to 1.0.4

--- Casks/inboxer.rb
+++ Casks/inboxer.rb
@@ -1,11 +1,11 @@
 cask 'inboxer' do
-  version '1.0.2'
-  sha256 'd60a7f25209602041cfb6e66c09c839a7068894daab7edc6205dbd10847c57bd'
+  version '1.0.4'
+  sha256 '708aa5e6394b869efca1d99b3d03d5936a8599c1d4d864d157e13982d93b424e'
 
   # github.com/denysdovhan/inboxer was verified as official when first introduced to the cask
   url "https://github.com/denysdovhan/inboxer/releases/download/v#{version}/inboxer-#{version}-mac.zip"
   appcast 'https://github.com/denysdovhan/inboxer/releases.atom',
-          checkpoint: '2c7ba053eab9a808222369960b35deac26569c4f38f2933f9b13a87fd35b6f7f'
+          checkpoint: '3c36ee31f7aa39052adbbf6376891cccf14e302b05f8c973f75e47b0295bced3'
   name 'inboxer'
   homepage 'https://denysdovhan.com/inboxer'
 
