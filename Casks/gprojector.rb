449f6d35452bc674f701fcc922f1adf3cd4d78a7
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-05 23:49:44 +0100

    Update gprojector to 2.1.7

--- Casks/gprojector.rb
+++ Casks/gprojector.rb
@@ -1,6 +1,6 @@
 cask 'gprojector' do
-  version '2.1.5'
-  sha256 '3c4a35f4b04b6c730c436c95057f74bf2e5a1622652e7d83c0b2d64b17fa2810'
+  version '2.1.7'
+  sha256 '55ed6b6b02094508bf9011964b2dba6cc305552c56ffb53ab2432a77fba52d25'
 
   url "https://www.giss.nasa.gov/tools/gprojector/download/G.ProjectorMacOS-#{version}.dmg"
   name 'G.Projector'
