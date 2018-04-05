6dc5e1179fac675a8ee7fac2d94594d2c206a1e1
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-05 17:54:17 +0100

    Update downie to 3.2.7,1800

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,10 +1,10 @@
 cask 'downie' do
-  version '3.2.2,1776'
-  sha256 'e6218195bb071c08ee030e8b5cd6ae3fd709d62496b57240a1f0315c59fd5b95'
+  version '3.2.7,1800'
+  sha256 'b3f1cb92b8b0ebdb3157a0de288fdd2bb2d0554df7b9a730d1c523c8d7ed62b8'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.major}_#{version.after_comma}.zip"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml",
-          checkpoint: 'b2a053aeeef465361989edc9a5be132ed6ade856b4e674db2e5d208d635b5e8c'
+          checkpoint: '210fcc35dac67cfdd2e42b7d7be38d945fa42eec7a357e4d1d7b384315895180'
   name 'Downie'
   homepage 'https://software.charliemonroe.net/downie.php'
 
