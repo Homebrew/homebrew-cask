c4ea9314025409bf22ae3991e4c1ebdc10a71f68
Stevie Howard <stvhwrd@users.noreply.github.com>
2018-04-05 23:55:07 -0700

    Update googleappengine to 1.9.68 (#45647)

--- Casks/googleappengine.rb
+++ Casks/googleappengine.rb
@@ -1,11 +1,11 @@
 cask 'googleappengine' do
-  version '1.9.67'
-  sha256 'c32ec08dc4783c49149486cf9d2990e7fe5e14d4bfeed1ae5dbdf073216fce54'
+  version '1.9.68'
+  sha256 '971627c085910c4af866f41f239cf5b2eaa7fbaced65a012ecc2655a65085e77'
 
   # storage.googleapis.com/appengine-sdks was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
   appcast 'https://storage.googleapis.com/appengine-sdks',
-          checkpoint: '196d23b35237839d9f67d1cc3da03c3326c3a94f962dda7f0430728ff5f8b32d'
+          checkpoint: 'bcc3a594a6ebc3da3804ffb88398774b4e81b722072e830bee67d8c245b2b0f5'
   name 'Google App Engine'
   homepage 'https://cloud.google.com/appengine/'
 
