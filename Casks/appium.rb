a94c2f540ceb3832016b05eb433c80d1d5df4e80
Roman Zhuzha <roman.zhuzha@trivago.com>
2018-04-06 12:10:10 +0200

    Update appium to 1.5.0

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -1,11 +1,11 @@
 cask 'appium' do
-  version '1.4.1'
-  sha256 '4d5363783638b3a6b1733bf9ed49746bd2990901327a91b2f659afc745faa594'
+  version '1.5.0'
+  sha256 '221187839a0218fbb3c6f254d3a0d070a5189d2fc7b9b3719fb74bdb2cc4d4bb'
 
   # github.com/appium/appium-desktop was verified as official when first introduced to the cask.
   url "https://github.com/appium/appium-desktop/releases/download/v#{version}/appium-desktop-#{version}-mac.zip"
   appcast 'https://github.com/appium/appium-desktop/releases.atom',
-          checkpoint: '2095efbf50aa994fefa62ef7e2b99556a7a84d437efcc1e5659c2651199ea19a'
+          checkpoint: 'a20a5a20aeb11ce6010d65d3a5b4bd51710f67a4a3538bcc5a06812fce8a5176'
   name 'Appium Desktop'
   homepage 'https://appium.io/'
 
