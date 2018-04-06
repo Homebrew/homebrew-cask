f341d9c56d51c73a4049ea29704c4ec28ef01e1c
Josh Freda <jfreda@users.noreply.github.com>
2018-04-06 00:46:24 -0500

    Update native-access to latest

--- Casks/native-access.rb
+++ Casks/native-access.rb
@@ -2,7 +2,7 @@ cask 'native-access' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.native-instruments.com/fileadmin/downloads/Native_Access_Installer.dmg'
+  url 'http://assets-cdn.native-instruments.com/public_downloads/Native_Access_Installer.dmg'
   name 'Native Access'
   homepage 'https://www.native-instruments.com/en/specials/native-access/'
 
