0df34925d362f0b09f342c01cb25168e1b8a04d4
Roberto Spreafico <roberto.spreafico@gmail.com>
2018-04-06 20:04:12 -0700

    Update juicebox to 1.8.8

--- Casks/juicebox.rb
+++ Casks/juicebox.rb
@@ -1,15 +1,15 @@
 cask 'juicebox' do
-  version :latest
-  sha256 :no_check
+  version '1.8.8'
+  sha256 'f3e825c062184dc11ef8f5a7ca3df3667b943663734d84e4cec86e93427dd1db'
 
   # s3.amazonaws.com/hicfiles.tc4ga.com/public/juicebox was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/hicfiles.tc4ga.com/public/juicebox/Juicebox.dmg'
+  url "https://s3.amazonaws.com/hicfiles.tc4ga.com/public/juicebox/Juicebox_#{version}.dmg"
   name 'Juicebox'
   homepage 'http://aidenlab.org/juicebox/'
 
   app 'Juicebox.app'
 
   caveats do
-    depends_on_java('8')
+    depends_on_java('8+')
   end
 end
