dbb8c50def33044650c06eebbc2b64962a078c96
jinxwu <jinxuanw@asu.edu>
2018-04-07 10:56:54 -0700

    add ao.app, version 5.4.0

--- Casks/ao.rb
+++ Casks/ao.rb
@@ -4,7 +4,7 @@ cask 'ao' do
 
   url "https://github.com/klauscfhq/ao/releases/download/v#{version}/ao-macos-#{version}.dmg"
   appcast 'https://github.com/klauscfhq/ao/releases.atom',
-    checkpoint: '12d547bfc877cbfb5d92315b2e1a4e5a87a48961c15863cbeaaae437d640edc5'
+          checkpoint: '12d547bfc877cbfb5d92315b2e1a4e5a87a48961c15863cbeaaae437d640edc5'
   name 'Ao'
   homepage 'https://github.com/klauscfhq/ao'
 
