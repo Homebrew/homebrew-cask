3800bacbbcccdd7b41463dce3760ad436928cacf
vicalloy <zbirder@gmail.com>
2018-04-07 16:52:58 +0800

    update core-tunnel to 1.0

--- Casks/core-tunnel.rb
+++ Casks/core-tunnel.rb
@@ -1,11 +1,11 @@
 cask 'core-tunnel' do
-  version '1.0-beta.3,884'
-  sha256 'cff14e17eec0fd8385b7e6656fd733c983ce465999364251764b8c5e8c405b3e'
+  version '1.0,905'
+  sha256 '798f9ad58174d4ec894389b7141e11007ec35b75844de66f11cf27e5b8694dee'
 
   # update.codinn.com/media/sparkle was verified as official when first introduced to the cask
   url "https://update.codinn.com/media/sparkle/Core_Tunnel-#{version.after_comma}.zip"
   appcast 'https://update.codinn.com/sparkle/2/appcast.xml',
-          checkpoint: 'e117d5e59b9ba9accd78146a416b84f92e22036cb32e4fae6431c54c4b33d4e4'
+          checkpoint: '478391272b0e0a7b3ffc740d12529ccaa43c60366f715e2e3bb1b1881328af3f'
   name 'Core Tunnel'
   homepage 'https://coressh.io/'
 
