2e97eeb7bcfe7a1a829a1f56fff070caa18d726f
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-09 00:34:01 +0100

    Update pd to 0.48-1 (#45710)

--- Casks/pd.rb
+++ Casks/pd.rb
@@ -1,10 +1,10 @@
 cask 'pd' do
-  version '0.48-0'
-  sha256 'a58b039b928e082df36b12c711c5373fe2e64a8015403166c15ef72696d6179a'
+  version '0.48-1'
+  sha256 'b7064bb20266da39e8257a94d09d75849f35d4f43520ce2c704a20562176e804'
 
   url "http://msp.ucsd.edu/Software/pd-#{version}.mac.tar.gz"
   appcast 'http://msp.ucsd.edu/software.html',
-          checkpoint: 'a527e66ce475915b9ecc2b37b777daecb5288b040dc5c2a13d7004ec138cb375'
+          checkpoint: 'e30ab3ff7597ee66a9c59014e05a0f254ec1c0575fd5afc7be8484dd754dac51'
   name 'Pd'
   homepage 'http://msp.ucsd.edu/software.html'
 
