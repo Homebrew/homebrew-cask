3bbc939b0b2980604a52478cb6511dd72dd1df87
ryan.c <jackblackevo@gmail.com>
2018-04-08 08:11:16 +0800

    Update teamsql to 3.2.195 (#45692)

--- Casks/teamsql.rb
+++ Casks/teamsql.rb
@@ -1,6 +1,6 @@
 cask 'teamsql' do
-  version '3.2.190'
-  sha256 'ab00986145f6b1c981856fb6c2da301a696cca4b37ca6fb315c519ff2e707a3c'
+  version '3.2.195'
+  sha256 'e4c0a92cba73c5c50f977a13538674ce8ca6af591d8e456fcbbc22c5611448b8'
 
   # dlpuop5av9e02.cloudfront.net/osx/stable was verified as official when first introduced to the cask
   url "https://dlpuop5av9e02.cloudfront.net/osx/stable/#{version}/TeamSQL-#{version}.dmg"
