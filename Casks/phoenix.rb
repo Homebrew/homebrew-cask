259b2cd0c14bfe4c6c4a980080c61e8a1b3f506c
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2018-04-08 22:08:49 +0300

    Upgrade Phoenix to 2.6.1 (#45703)

--- Casks/phoenix.rb
+++ Casks/phoenix.rb
@@ -1,10 +1,10 @@
 cask 'phoenix' do
-  version '2.6'
-  sha256 'a4635be6cfa92e0ee5db3e8b46edd6886e65f9c3b116b03f8892140c6286b72c'
+  version '2.6.1'
+  sha256 '5ac0b9592f54045a86a49a8a628f4c94e5c0afd032ca3c67444dfc01d4b9a294'
 
   url "https://github.com/kasper/phoenix/releases/download/#{version}/phoenix-#{version}.tar.gz"
   appcast 'https://github.com/kasper/phoenix/releases.atom',
-          checkpoint: 'c439c85fa22b77cd9bc3f32a717ba34bc21dca8c36acc5ee53e5273029a2827f'
+          checkpoint: 'b560e8ffdbe23df575dcf8b055d28eb7b468255bac209e54bb2b09634877750f'
   name 'Phoenix'
   homepage 'https://github.com/kasper/phoenix/'
 
