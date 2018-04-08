7d4926f41136970b8a7fce5f3a8d7e9d819fa7f0
commitay <commitay@users.noreply.github.com>
2018-04-09 07:40:48 +1000

    Update stubbymanager.rb

--- Casks/stubbymanager.rb
+++ Casks/stubbymanager.rb
@@ -1,8 +1,8 @@
 cask 'stubbymanager' do
-  version '1.0'
+  version '6782984'
   sha256 'b6b7bf8aee217ca66c2fb99a079e89d90f3c251c37bdcc07ef8706702f157c3d'
 
-  url 'https://dnsprivacy.org/wiki/download/attachments/6782984/StubbyManager.pkg'
+  url "https://dnsprivacy.org/wiki/download/attachments/#{version}/StubbyManager.pkg"
   name 'StubbyManager'
   homepage 'https://dnsprivacy.org/wiki/display/DP/Stubby+GUI+for+macOS'
 
