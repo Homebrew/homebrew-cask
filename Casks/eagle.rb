cfd249b8ca0e6929869b7e26256c5aa90c0b5836
Max Riveiro <kavu13@gmail.com>
2018-04-09 19:48:29 +0300

    EAGLE 8.7.1

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -1,6 +1,6 @@
 cask 'eagle' do
-  version '8.7.0'
-  sha256 'c7e8cbfd9aad93fcf4c2aca3ddde1bcada7ed64c039ae0599e207b5804d5f2ff'
+  version '8.7.1'
+  sha256 '0beb43e0f615f336a9efc30f74f751ebb331a2b937d82fffb9abfe3445f5f349'
 
   url "https://eagle-updates.circuits.io/downloads/#{version.dots_to_underscores}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   name 'Autodesk EAGLE'
