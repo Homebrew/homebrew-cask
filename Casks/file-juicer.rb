19fa9cdec9fa9111fffcfaf6b8e5b9ce4ace54f5
commitay <commitay@users.noreply.github.com>
2018-04-10 08:03:55 +1000

    Update file-juicer.rb

--- Casks/file-juicer.rb
+++ Casks/file-juicer.rb
@@ -1,6 +1,6 @@
 cask 'file-juicer' do
   version '4.66'
-  sha256 '34ba205e5ac834cda950821ef4508f5b6d20f9a54b38c94eec961d62838831ce'
+  sha256 '716f38251ed389a9cf6faedd10bf5f2028a8344b6c0dba0f47d9945aac7cb540'
 
   url "https://echoone.com/filejuicer/FileJuicer-#{version}.zip"
   name 'File Juicer'
