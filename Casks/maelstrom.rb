3834d38c31eee48fd2a5b4ce9c3be4d7fa3e1d3c
commitay <commitay@users.noreply.github.com>
2018-04-10 06:51:22 +1000

    Update maelstrom.rb

--- Casks/maelstrom.rb
+++ Casks/maelstrom.rb
@@ -2,7 +2,7 @@ cask 'maelstrom' do
   version '3.0.6a'
   sha256 '78e714a422800b5a5026e7e719f74adf30e164a4c72a5c5db51cabb72a284402'
 
-  url 'http://www.libsdl.org/projects/Maelstrom/bin/Maelstrom-3.0.6a-MacOSX.dmg'
+  url "http://www.libsdl.org/projects/Maelstrom/bin/Maelstrom-#{version}-MacOSX.dmg"
   name 'Maelstrom'
   homepage 'http://www.libsdl.org/projects/Maelstrom/index.html'
 
