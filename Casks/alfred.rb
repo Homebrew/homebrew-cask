47562b86e1b68d3e8575e9db9af6aadec0948146
Shoichi Aizawa <shoichiaizawa@gmail.com>
2018-04-09 23:35:32 +0900

    Update alfred to 3.6.1_910

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,6 +1,6 @@
 cask 'alfred' do
-  version '3.6_903'
-  sha256 'd128389caeba552cb297e72dfc889379db50f003e43872dc91d09eb2080511e7'
+  version '3.6.1_910'
+  sha256 'fdefdb35047e193e1d06f5a441f4aabd4b45b24fd43c63d223d8508ad11a131e'
 
   url "https://cachefly.alfredapp.com/Alfred_#{version}.dmg"
   name 'Alfred'
