268bd72d3182e3a93b58e879748436f4fd4ab59c
Johan Wassberg <johan@rymdvarel.se>
2018-04-09 22:39:44 +0200

    Add Maelstrom 3.0.6a.
    Add the classic game Maelstrom. Not sure if the "a" in the version is
    for alpha, but it's the only alternative do fetch from the developers
    site.

---
+++ Casks/maelstrom.rb
@@ -0,0 +1,10 @@
+cask 'maelstrom' do
+  version '3.0.6a'
+  sha256 '78e714a422800b5a5026e7e719f74adf30e164a4c72a5c5db51cabb72a284402'
+
+  url 'http://www.libsdl.org/projects/Maelstrom/bin/Maelstrom-3.0.6a-MacOSX.dmg'
+  name 'Maelstrom'
+  homepage 'http://www.libsdl.org/projects/Maelstrom/index.html'
+
+  app 'Maelstrom.app'
+end
