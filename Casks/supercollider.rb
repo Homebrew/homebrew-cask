a9a156a79058a321318e4bafa8e78e308cf29937
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2018-04-09 07:36:32 -0400

    Update supercollider to 3.9.3 (#45724)

--- Casks/supercollider.rb
+++ Casks/supercollider.rb
@@ -1,11 +1,11 @@
 cask 'supercollider' do
-  version '3.9.1'
-  sha256 '5b13cdc6d1fe9a6a947406fb22852d72f1ec623367f1cea0065b63ed86d5bdde'
+  version '3.9.3'
+  sha256 '84cdad611aca022ff218aff46e92021a334c5bb8e6ac3dbbe0051fe7f57bae07'
 
   # github.com/supercollider/supercollider was verified as official when first introduced to the cask
   url "https://github.com/supercollider/supercollider/releases/download/Version-#{version}/SuperCollider-#{version}-macOS.zip"
   appcast 'https://github.com/supercollider/supercollider/releases.atom',
-          checkpoint: 'e59593baaa6ede02f4abfc079e0e144b49fad2b103acc5f332385be6626b3d1e'
+          checkpoint: '72ee8bd7b4c3325c616a0eca42db28464b46904434215af74addfcad5f2a19f0'
   name 'SuperCollider'
   homepage 'https://supercollider.github.io/'
 
