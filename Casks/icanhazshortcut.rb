48809eca8e6c6c33e7f0048c59f01e7c72797dd2
Anaz Haidhar <mr.anazhd@gmail.com>
2018-04-09 18:40:57 +0800

    Fix Style

--- Casks/icanhazshortcut.rb
+++ Casks/icanhazshortcut.rb
@@ -1,15 +1,17 @@
 cask 'icanhazshortcut' do
-    version '0.8.2'
-    sha256 '8106a6c6d073065898fd1122eb25a8184d984ac1a4f1042779befb0160e692e2'
-  
-    url "https://github.com/deseven/icanhazshortcut/releases/download/#{version}/ichs.dmg"
-    appcast 'https://github.com/fikovnik/ShiftIt/releases.atom',
-            checkpoint: 'b3a9577641b0a8dc2d6cd46307a345bdc52a07007837a738cde53f50b6c40ace'
-    name 'iCanHazShortcut'
-    homepage 'https://github.com/deseven/icanhazshortcut/'
-  
-    app 'iCanHazShortcut.app'
-    uninstall quit: 'info.deseven.icanhazshortcut',
-              pkgutil: 'info.deseven.icanhazshortcut'
-    zap trash: '~/Library/Preference/info.deseven.icanhazshortcut'
+  version '0.8.2'
+  sha256 '8106a6c6d073065898fd1122eb25a8184d984ac1a4f1042779befb0160e692e2'
+
+  url "https://github.com/deseven/icanhazshortcut/releases/download/#{version}/ichs.dmg"
+  appcast 'https://github.com/fikovnik/ShiftIt/releases.atom',
+          checkpoint: 'b3a9577641b0a8dc2d6cd46307a345bdc52a07007837a738cde53f50b6c40ace'
+  name 'iCanHazShortcut'
+  homepage 'https://github.com/deseven/icanhazshortcut/'
+
+  app 'iCanHazShortcut.app'
+
+  uninstall quit:    'info.deseven.icanhazshortcut',
+            pkgutil: 'info.deseven.icanhazshortcut'
+
+  zap trash: '~/Library/Preference/info.deseven.icanhazshortcut'
 end
