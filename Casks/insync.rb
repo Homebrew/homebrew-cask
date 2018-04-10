300b9f266a9855e771e9e7d09d761734ce593a26
404NetworkError <404NetworkError@users.noreply.github.com>
2018-04-10 02:58:08 -0500

    Update insync to 1.4.5.37069

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,6 +1,6 @@
 cask 'insync' do
-  version '1.4.4.37065'
-  sha256 '1b74a89a0ea6df57a999dfd09f59cffaee7236f6e63011224b93ef69acfbc669'
+  version '1.4.5.37069'
+  sha256 '3d0f63d709b65ff44fc32aae34a2456809ac2e53ed9f4e1effbe35ab5b5dce5f'
 
   url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
   name 'Insync'
