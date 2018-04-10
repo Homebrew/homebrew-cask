a7f95393bc84683e155c9fc0d2477c69871f4d10
Anver Housseini <contact@anver.ch>
2018-04-10 09:40:23 +0200

    Update flash-ppapi to 29.0.0.140

--- Casks/flash-ppapi.rb
+++ Casks/flash-ppapi.rb
@@ -1,6 +1,6 @@
 cask 'flash-ppapi' do
-  version '29.0.0.113'
-  sha256 'f06ead47a3ff224e7d04fd47fb4815f9d4748eb956d89bca7e153923ca401b3a'
+  version '29.0.0.140'
+  sha256 'bc428ec42ac959e498a6a65d37ab148b9c75311662d3494196fdde85373a0e29'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/pdc/#{version}/install_flash_player_osx_ppapi.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
