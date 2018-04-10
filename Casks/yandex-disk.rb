fbec2252a6353aba30bb016c063bb94275f24b07
Vitaliy (glukki) Meshchaninov <glukki.spb.ru@gmail.com>
2018-04-10 16:27:51 +0300

    Update yandex-disk 3.0 checksum

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -1,6 +1,6 @@
 cask 'yandex-disk' do
   version '3.0'
-  sha256 '4cc3faad156c7c8fcddece0233c8b4042d239b4fa61f9772f6ae2dd59b9cc0f2'
+  sha256 'd5f9c1b43faeb99e30e203c612d678cbe6d2f08d187c74e7cf1c6e7efac5c53c'
 
   url "https://disk.yandex.ru/download/YandexDisk#{version.no_dots}.dmg/?instant=1"
   name 'Yandex.Disk'
