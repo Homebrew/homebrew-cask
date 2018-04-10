14f443f44631bf75eb9e89834b6c5f8bf76afa98
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-04-10 15:06:48 +0100

    Dawn: 2.8.0

--- Casks/dawn.rb
+++ Casks/dawn.rb
@@ -1,6 +1,6 @@
 cask 'dawn' do
-  version '2.7.0,20171114-1119'
-  sha256 '89add67d0f72378ac7ce5638f7090cf42e6e35afd0de1431e85b68c64db59cfc'
+  version '2.8.0,20180409-1550'
+  sha256 '739abe44ec6798a7a27180d6bfeca043ade5e2a81c9e61f28e24ed4e46dd98dc'
 
   # alfred.diamond.ac.uk/DawnDiamond was verified as official when first introduced to the cask
   url "https://alfred.diamond.ac.uk/DawnDiamond/#{version.major_minor}/downloads/builds-release/DawnDiamond-#{version.before_comma}.v#{version.after_comma}-mac64.dmg"
