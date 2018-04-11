ff9ff6a5dfd6ca3b192ddfe5e69f4075cba3a175
grokdesigns <chris@grokdesigns.com>
2018-04-10 20:10:42 -0700

    Adding TunesKit for Mac to repo.

---
+++ Casks/tuneskit.rb
@@ -0,0 +1,10 @@
+cask 'tuneskit' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.tuneskit.com/TunesKitforMac.dmg'
+  name 'TunesKit for Mac'
+  homepage 'https://www.tuneskit.com/'
+
+  app 'TunesKit for Mac.app'
+end
