db3dfa354ce349880dd4d00bde58365bcbb2030f
Maxime Gervais <gervais.maxime@gmail.com>
2018-04-11 16:27:48 +0200

    mediaconch-app 18.03.2

--- Casks/mediaconch-app.rb
+++ Casks/mediaconch-app.rb
@@ -1,10 +1,10 @@
 cask 'mediaconch-app' do
-  version '18.03'
-  sha256 '59ad61b44c01e30cdd39ffe912f7799aa7faa69ff39d250c248c49be7e85460b'
+  version '18.03.2'
+  sha256 '2dc51e39b74d516e094063860015c7ff37d6a038009e69dd121b7ff905911bbc'
 
   url "https://mediaarea.net/download/binary/mediaconch-gui/#{version}/MediaConch_GUI_#{version}_Mac.dmg"
   appcast 'https://github.com/MediaArea/MediaConch_SourceCode/releases.atom',
-          checkpoint: '26d9dbc7f67b6867e24ee9d40bef3caf17a37dc033e5a741943da0bbecaae9b4'
+          checkpoint: '8d4ae5de1b9f553d5309c0f9cdc50e7d0b26573e4b99b58c851f116dad4414ff'
   name 'MediaConch'
   homepage 'https://mediaarea.net/MediaConch/'
 
