41eab86cbdd3ea0d0a57267a21c672cc8b88937d
Maarten Bicknese <maarten.bicknese@gmail.com>
2018-04-12 09:25:19 +0200

    Added Unity Hub (unity-hub) beta, v0.15.0

---
+++ Casks/unity-hub.rb
@@ -0,0 +1,13 @@
+cask 'unity-hub' do
+  version '0.15.0'
+  sha256 '85777f1d4b3d11514ac45973fc4c25281b808c8de95d17c65867a73e954e6b1f'
+
+  url 'https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.dmg'
+  name 'Unity Hub'
+  homepage 'https://blogs.unity3d.com/2018/01/24/streamline-your-workflow-introducing-unity-hub-beta/'
+
+  uninstall quit: 'com.unity3d.unityhub',
+            delete: '/Applications/Unity/Hub'
+
+  app 'Unity Hub.app'
+end
