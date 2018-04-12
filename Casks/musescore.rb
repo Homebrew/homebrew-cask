8f31fc3de209dff0f50edd4b012ab87338f77198
Leonardo Fedalto <lfedalto@gmail.com>
2018-04-11 22:24:03 -0300

    Update musescore to 2.2.1

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -1,6 +1,6 @@
 cask 'musescore' do
-  version '2.2'
-  sha256 '92f365f1b2129cfe0ee0537f4f6074a529107821167650eafe472044d4f2bac1'
+  version '2.2.1'
+  sha256 'f1291f1f9ff5a85946215c926266f65cc51ab3558127e2cb554ac812d6fe320f'
 
   # ftp.osuosl.org/pub/musescore was verified as official when first introduced to the cask
   url "https://ftp.osuosl.org/pub/musescore/releases/MuseScore-#{version.major_minor_patch}/MuseScore-#{version}.dmg"
