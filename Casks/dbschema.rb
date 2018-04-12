87c8cb576e5f04d84c5f2284293b4da0dd28c3c5
Brian Morton <brianmortondesign@gmail.com>
2018-04-12 14:11:00 -0700

    Update dbschema to 7.6.6 (#45885)

--- Casks/dbschema.rb
+++ Casks/dbschema.rb
@@ -1,6 +1,6 @@
 cask 'dbschema' do
-  version '7.6.4'
-  sha256 'a19568384b2ef5c6cb741d3b93c3448ef379b94016fb8e4ae21e82a3bfa72810'
+  version '7.6.6'
+  sha256 '1371ed695c111a387a5fab90c8610b8a32382fe705f459968933205d1c8638ce'
 
   url "https://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
   name 'DbSchema'
