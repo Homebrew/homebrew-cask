219432e98ab70a6bc49d04aafe82d757a94178e0
Victor Popkov <victor@popkov.me>
2018-04-13 00:14:02 +0300

    Update contexts to 3.6.2 (#45886)

--- Casks/contexts.rb
+++ Casks/contexts.rb
@@ -1,10 +1,10 @@
 cask 'contexts' do
-  version '3.5.3'
-  sha256 '464e0b36e36261b5ad280283a6894337495b791b5bb9ea29f8462cb3a116678b'
+  version '3.6.2'
+  sha256 'f7598da2fe625a123a2db647f32cc67026763f5a262aec3875b5b49221efd1c2'
 
   url "https://contexts.co/releases/Contexts-#{version}.dmg"
   appcast 'https://contexts.co/appcasts/stable.xml',
-          checkpoint: '82f8ed707bed6e2df58251bc8c9ecf91c8645d4401dd7cae92d88b19994a2b89'
+          checkpoint: '6fce651add6d5e618f59d64319bab9da022008c26bcff69d269819c39b4acaee'
   name 'Contexts'
   homepage 'https://contexts.co/'
 
