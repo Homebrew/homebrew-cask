27a8c5eaa69b61bf01d4cf0cdb9de04362f94a18
miccal <miccal.matthews@gmail.com>
2018-04-12 08:37:41 +0800

    Update tex-live-utility to 1.34 (#45837)

--- Casks/tex-live-utility.rb
+++ Casks/tex-live-utility.rb
@@ -1,10 +1,10 @@
 cask 'tex-live-utility' do
-  version '1.33'
-  sha256 '8b8ac5cb9c4a8450e0fadfdf7d653f5a428eb8b4e4c1484cacd625b3f8acca8e'
+  version '1.34'
+  sha256 '57f34bec3802bae73509b817479aa7f0456abc67d509064080612227527233cb'
 
   url "https://github.com/amaxwell/tlutility/releases/download/#{version}/TeX.Live.Utility.app-#{version}.tar.gz"
   appcast 'https://github.com/amaxwell/tlutility/releases.atom',
-          checkpoint: 'c4e0ce62618c3c67cdf81425d379a4bda6bc3507ab82429da7f36a7fa3801249'
+          checkpoint: '295f8beecdc100b82dff5e1e00098316e7fcddad08d2fcb386033cb9b6134603'
   name 'TeX Live Utility'
   homepage 'https://github.com/amaxwell/tlutility'
 
