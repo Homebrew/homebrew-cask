4957c4097261981f436d64dc17e0e69f7d56a8ac
commitay <commitay@users.noreply.github.com>
2018-04-12 21:49:36 +1000

    Update smartgit to 18.1.0 (#45857)

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,10 +1,10 @@
 cask 'smartgit' do
-  version '17.1.6'
-  sha256 '4850c2538f083e8bafb14747ef2fa108e2e97e154fa03716faaad6e2f30c5f14'
+  version '18.1.0'
+  sha256 'e22320f046ca9dc85fcc07ff580748e2cfc9a58d4266652b82b999e7c81a41ac'
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.syntevo.com/smartgit/changelog.txt',
-          checkpoint: 'e970828c70a3818509baa9cf8879f689f6eefce74fb8e891bcfc1a5608ad521a'
+          checkpoint: 'f362b7a8f7f10ddcb210314c10d43698ed88ae98b422f05d073320d6c584e6af'
   name 'SmartGit'
   homepage 'https://www.syntevo.com/smartgit/'
 
