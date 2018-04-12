b080231d74929a579bd2cae8f15b63d08059f087
Maarten Bicknese <maarten.bicknese@gmail.com>
2018-04-12 10:49:59 +0200

    Update Unity Hub with correct beta content

--- Casks/unity-hub.rb
+++ Casks/unity-hub.rb
@@ -1,10 +1,10 @@
 cask 'unity-hub' do
-  version '0.15.0'
-  sha256 '85777f1d4b3d11514ac45973fc4c25281b808c8de95d17c65867a73e954e6b1f'
+  version :latest
+  sha256 :no_check
 
   url 'https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.dmg'
   name 'Unity Hub'
-  homepage 'https://blogs.unity3d.com/2018/01/24/streamline-your-workflow-introducing-unity-hub-beta/'
+  homepage 'https://unity3d.com/unity/beta-download'
 
   app 'Unity Hub.app'
 
