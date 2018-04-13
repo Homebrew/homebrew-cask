84ec2f7fc5979f3e371da050f2c0ccd02c72867c
Claudia <claui@users.noreply.github.com>
2018-04-13 23:12:12 +0200

    Update appcast checkpoint

--- Casks/aptanastudio.rb
+++ Casks/aptanastudio.rb
@@ -5,7 +5,7 @@ cask 'aptanastudio' do
   # github.com/aptana/studio3 was verified as official when first introduced to the cask
   url "https://github.com/aptana/studio3/releases/download/v#{version}/Aptana_Studio_#{version}_Setup.dmg"
   appcast "https://github.com/aptana/studio#{version.major}/releases.atom",
-          checkpoint: 'b5c873d8167e9460ca864629759e4343962029042edd29a46297ce46167a8eef'
+          checkpoint: 'bc7e44cbfb6450a4af49e41a1ceb499ea9ea90a7f86bd8f58be7dc29a9743daa'
   name 'Aptana Studio'
   homepage 'http://www.aptana.com/'
 
