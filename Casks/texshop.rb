83cf7570f1888f139a690c0fbe81baed745b5a56
miccal <miccal.matthews@gmail.com>
2018-04-14 07:55:22 +0800

    Update texshop to 4.00

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,10 +1,10 @@
 cask 'texshop' do
-  version '3.99'
-  sha256 '5a36a64cb8e0a608a9758824e3dd60fd7b1f801341db1a3cd83ec8e9e918189e'
+  version '4.00'
+  sha256 '6ed32af33ba920f9d570e026b1fdcd0aa53aeb45b1e69a562a519aeb19b9986a'
 
   url "http://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.no_dots}.zip"
   appcast 'http://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml',
-          checkpoint: '50a6a3d0b0a6b4c187bcddc38e1713de134d99a1ece815caa157d197915094b0'
+          checkpoint: '0dfbd132b4f63c91d9b4a8a6f89301327b1aacf002eb312bbc76fa1e7ddabeeb'
   name 'TeXShop'
   homepage 'http://pages.uoregon.edu/koch/texshop/'
 
