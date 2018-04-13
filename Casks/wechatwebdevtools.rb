fd505620914314b90893634477bd1533c278d39d
Vítor Galvão <info@vitorgalvao.com>
2018-04-13 18:27:03 +0100

    Update wechatwebdevtools.rb

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -2,7 +2,7 @@ cask 'wechatwebdevtools' do
   version '1.02.1804120'
   sha256 '73c47fad32956afce280149e5e1f9db08223dc3de9857cee2ed087cb769ef397'
 
-  url "https://dldir1.qq.com/WechatWebDev/1.0.0/20#{version.patch}/wechat_devtools_#{version}.dmg"
+  url "https://dldir1.qq.com/WechatWebDev/#{version.major}.0.0/20#{version.patch}/wechat_devtools_#{version}.dmg"
   name 'wechat web devtools'
   name '微信web开发者工具'
   homepage 'https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/download.html'
