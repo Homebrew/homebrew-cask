d817b24de8b9d67f918436982d2919c0a07b6229
warmwinter <github@miss77.net>
2018-04-13 18:03:51 +0800

    Update epic-games to 7.8.0

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,11 +1,13 @@
 cask 'epic-games' do
-  version '2.12.24-3321064'
-  sha256 'b1e37a5ff042fb5e13cd904d71acb14496bd65036612a726ff3b509947779c1d'
+  version '7.8.0'
+  sha256 '1d23e120553a664c0a88456f2b7c493a3389210ba4cdd0bbfad62d4e1dcfc2e6'
 
   # epicgames.com was verified as official when first introduced to the cask
-  url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicGamesLauncher-#{version}.dmg"
+  url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
   name 'Epic Games Launcher'
   homepage 'https://www.unrealengine.com/what-is-unreal-engine-4'
+
+  auto_updates true
 
   app 'Epic Games Launcher.app'
 
