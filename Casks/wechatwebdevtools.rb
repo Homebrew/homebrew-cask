8f15afb1a67b30a61cd43d3ec09059478ead1476
greenrock <dd331@163.com>
2018-04-14 06:03:13 +0800

    Update wechatwebdevtools to 1.02.1804120 (#45920)
    * Update wechatwebdevtools to 1.02.1804120
    
    * Update wechatwebdevtools.rb

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,8 +1,8 @@
 cask 'wechatwebdevtools' do
-  version '1.02.1803210'
-  sha256 '7390bc33d42d4d2392dea44448904f193fc392f550119c7cf09ec81e6933107a'
+  version '1.02.1804120'
+  sha256 '73c47fad32956afce280149e5e1f9db08223dc3de9857cee2ed087cb769ef397'
 
-  url "https://dldir1.qq.com/WechatWebDev/1.0.0/20#{version.patch}/wechat_devtools_#{version}.dmg"
+  url "https://dldir1.qq.com/WechatWebDev/#{version.major}.0.0/20#{version.patch}/wechat_devtools_#{version}.dmg"
   name 'wechat web devtools'
   name '微信web开发者工具'
   homepage 'https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/download.html'
