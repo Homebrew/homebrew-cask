8875ed7eb316844b33723a7773ad3375bfd1222a
commitay <commitay@users.noreply.github.com>
2018-04-13 18:19:15 +1000

    Update clamxav to 2.18_3608

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -1,10 +1,10 @@
 cask 'clamxav' do
-  version '2.17_3603'
-  sha256 '55849ec74a97283710699d62ed8fbbc421045ffd01bd1778b33dbc6002d6328f'
+  version '2.18_3608'
+  sha256 '76527f129fa1d695e4635366d008e0683a9ea195a5ad04a4693ffbb0a3d190bc'
 
   url "https://www.clamxav.com/downloads/ClamXAV_#{version}.zip"
   appcast 'https://www.clamxav.com/sparkle/appcast.xml',
-          checkpoint: 'fcb7e4e4e7189baf1f1a78f248a4183c0edfb7ce66f8429f86c552dafc63e884'
+          checkpoint: 'b375f71ee431356819bcce4bf01298b60e01ee486fc79991997ea014c172a8fb'
   name 'ClamXAV'
   homepage 'https://www.clamxav.com/'
 
