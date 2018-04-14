87f00da6cde020c85672def17faa6d6ca7854cde
commitay <commitay@users.noreply.github.com>
2018-04-14 11:05:48 +1000

    Update nozbe.rb

--- Casks/nozbe.rb
+++ Casks/nozbe.rb
@@ -1,8 +1,8 @@
 cask 'nozbe' do
-  version '3.7.1'
+  version '3.7.2'
   sha256 'fbbd86ee96037dc7e3ef289d12c547b1d25edf5baf51011ec1310d058bc8ca33'
 
-  url 'https://nozbe.com/mac'
+  url "https://files.nozbe.com/#{version.no_dots}/release/Nozbe.app.zip"
   name 'Nozbe'
   homepage 'https://nozbe.com/'
 
