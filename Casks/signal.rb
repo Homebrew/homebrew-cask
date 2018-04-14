3ebfbddd8a231efa84eb10dd881391487812df96
commitay <commitay@users.noreply.github.com>
2018-04-14 19:53:05 +1000

    Update signal to 1.7.1

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,10 +1,10 @@
 cask 'signal' do
-  version '1.7.0'
-  sha256 '70002d1566d9f32f4a10ebf0acc7b2e98d2542ce060c3e9039765c6cd82659c8'
+  version '1.7.1'
+  sha256 '04091ec8a220f2e4976e7165caf17ea12db0525ece6c9479ea708df987c9ccfe'
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.zip"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom',
-          checkpoint: '7edf95b5cf6fc06f4144dbc7533acf5c0624976e290234690a35d208638954e1'
+          checkpoint: '306db3a4895dc2125b099e8654759324f89aa76ca9c89bfdb98d9639a732d0f6'
   name 'Signal'
   homepage 'https://signal.org/'
 
