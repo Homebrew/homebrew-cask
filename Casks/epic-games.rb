9af2d561a93e7eb11545ccc4f24ae3198f45c5e7
warmwinter <github@miss77.net>
2018-04-14 21:34:58 +0800

    Update epic-games to 7.9.0

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,13 +1,21 @@
 cask 'epic-games' do
-  version '2.12.24-3321064'
-  sha256 'b1e37a5ff042fb5e13cd904d71acb14496bd65036612a726ff3b509947779c1d'
+  version '7.9.0'
+  sha256 'f24f650627e8808807039dbe7bbcb56216caec2aa407cf8e3d6df7e628f4530c'
 
   # epicgames.com was verified as official when first introduced to the cask
-  url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicGamesLauncher-#{version}.dmg"
+  url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
   name 'Epic Games Launcher'
   homepage 'https://www.unrealengine.com/what-is-unreal-engine-4'
 
+  auto_updates true
+
   app 'Epic Games Launcher.app'
 
-  zap trash: '~/Library/Caches/com.epicgames.EpicGamesLauncher'
+  zap trash: [
+               '~/Library/Caches/com.epicgames.EpicGamesLauncher',
+               '~/Library/Application Support/Epic',
+               '~/Library/Cookies/com.epicgames.EpicGamesLauncher.binarycookies',
+               '~/Library/Logs/Unreal Engine/EpicGamesLauncher',
+               '~/Library/Preferences/Unreal Engine/EpicGamesLauncher',
+             ]
 end
