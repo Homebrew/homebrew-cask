b6701895131b4ef5c2eeb76f40143a2a553ce3f3
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-15 20:19:50 +0100

    Update kstars to 2.9.4 (#45973)

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,9 +1,9 @@
 cask 'kstars' do
-  version :latest
-  sha256 :no_check
+  version '2.9.4'
+  sha256 '27c78fcccb3c96cd37f9c2a71f557a60ee6698c5baf170d1adfac3571d5ff9a6'
 
   # indilib.org/jdownloads/kstars was verified as official when first introduced to the cask
-  url 'http://www.indilib.org/jdownloads/kstars/kstars-latest.dmg'
+  url "http://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg"
   name 'KStars'
   homepage 'https://edu.kde.org/kstars/'
 
