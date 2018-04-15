504b45580421bbf2443e3e39cc27d7c3ac8ceb3c
Louis St-Amour <LouisStAmour@gmail.com>
2018-04-14 22:40:06 -0400

    Update superproductivity to 1.10.14

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,11 +1,11 @@
 cask 'superproductivity' do
-  version '1.10.8'
-  sha256 '4bd98eec162924dbb719763e55a2505780d5e58698b77433e20562b0967c66f7'
+  version '1.10.14'
+  sha256 'fa0631f72bd1e3d8884661b5c824298315270609368bb9341ed146be8f61a6e1'
 
   # github.com/johannesjo/super-productivity was verified as official when first introduced to the cask
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip"
   appcast 'https://github.com/johannesjo/super-productivity/releases.atom',
-          checkpoint: '2b692d28063a99035e339aa57f1e8086c70e50d81520648195af6b7348c43bd1'
+          checkpoint: 'a12a674e331deed8e2cd3b271a282b89b645be0e2e9da66069a479ef4c3fe99c'
   name 'Super Productivity'
   homepage 'https://super-productivity.com/'
 
