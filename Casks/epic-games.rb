70da748b30647a8db12e1c17ab09ff0772625f65
commitay <commitay@users.noreply.github.com>
2018-04-15 12:02:10 +1000

    Update epic-games.rb

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -2,10 +2,9 @@ cask 'epic-games' do
   version '7.9.0'
   sha256 'f24f650627e8808807039dbe7bbcb56216caec2aa407cf8e3d6df7e628f4530c'
 
-  # epicgames.com was verified as official when first introduced to the cask
   url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
   name 'Epic Games Launcher'
-  homepage 'https://www.unrealengine.com/what-is-unreal-engine-4'
+  homepage 'https://www.epicgames.com/'
 
   auto_updates true
 
