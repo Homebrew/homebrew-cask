c56b69546ed097a5d994d6c721fa7729a462c27b
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-16 22:39:53 +0100

    Update comic-life to 3.5.7 (#46023)

--- Casks/comic-life.rb
+++ Casks/comic-life.rb
@@ -1,10 +1,10 @@
 cask 'comic-life' do
-  version '3.5.6'
-  sha256 '8bcc6fdaf11b306880cdcdead626e894cbfb89b745a5f3f1621deb3c61027913'
+  version '3.5.7'
+  sha256 '6b1ba855f3738e103411d9595e2e67008de85ec25bd91b99659c674182b3fa1d'
 
   url "http://downloads.plasq.com/comiclife#{version.major}.zip"
   appcast "https://s3.amazonaws.com/updater.plasq.com/comiclife#{version.major}-appcast.xml",
-          checkpoint: '066b1bca833379a7be5228ba65a572103059b41145ac0019298fe85554b47ec0'
+          checkpoint: 'afe438be5128c6d6beb9204df5c874e74381459800f7a8c431bf1a032c43fcd6'
   name 'Comic Life'
   homepage 'https://plasq.com/apps/comiclife/macwin/'
 
