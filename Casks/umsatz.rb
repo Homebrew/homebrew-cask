dd420b43bb20e4c6b5a2621819a834d3fb848406
Julian Mayer <julian@corecode.at>
2018-04-17 14:05:14 +0200

    Update umsatz to 7.1

--- Casks/umsatz.rb
+++ Casks/umsatz.rb
@@ -1,11 +1,11 @@
 cask 'umsatz' do
-  version '7.0'
-  sha256 'b3b4c48106b9615a4f3f7db361432862d63ccec5dd91e52a07696c21290f26ec'
+  version '7.1'
+  sha256 'f6bc4485fa5bb1d3c3b1686cc2e1d9985daa4ade305266ea7f03047594b637c7'
 
   # moapp.software was verified as official when first introduced to the cask
   url 'https://download.moapp.software/Umsatz.zip'
   appcast 'https://sparkle.moapp.software/umsatz.xml',
-          checkpoint: 'c376278a20068a3f8d7e00aee7642d71a91236427a48371c292aa00df6ca653a'
+          checkpoint: '5bf4f0c7aa53a52a5ce95ec9d8096e2dc7ff8935756a6177f8ca88d14278b130'
   name 'Umsatz'
   homepage 'https://umsatz-programm.de/'
 
