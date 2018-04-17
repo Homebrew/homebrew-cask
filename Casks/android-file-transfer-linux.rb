bb8cd7364b53144d89040058e3f5109d0490e0d2
commitay <commitay@users.noreply.github.com>
2018-04-18 04:51:28 +1000

    Update android-file-transfer-linux.rb

--- Casks/android-file-transfer-linux.rb
+++ Casks/android-file-transfer-linux.rb
@@ -2,7 +2,7 @@ cask 'android-file-transfer-linux' do
   version '3.2-62d1c79'
   sha256 '84469fd3adba4046a9137093c3e0731e60ae2220381502b452d767cd4de4e676'
 
-  # github.com/whoozle/android-file-transfer-linux/ was verified as official when first introduced to the cask
+  # github.com/whoozle/android-file-transfer-linux was verified as official when first introduced to the cask
   url "https://github.com/whoozle/android-file-transfer-linux/releases/download/v#{version.split('-')[0]}/AFTOSX-#{version}.dmg"
   appcast 'https://github.com/whoozle/android-file-transfer-linux/releases.atom',
           checkpoint: 'bd62e37c3b26874adbf882b3e25808cfefef05ff788654cf15a3e5f97975dd03'
