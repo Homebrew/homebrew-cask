b7da90ee386319a95e04cf11335a00278c097482
Julian Mayer <julian@corecode.at>
2018-04-17 17:44:04 +0200

    Update lehreroffice to 2018.5.0

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,10 +1,10 @@
 cask 'lehreroffice' do
-  version '2018.4.0'
-  sha256 '2aa71874bc3300761360a9bb953f99e2429349718b78adfe1588b7256e5f628a'
+  version '2018.5.0'
+  sha256 'fd61efcb433d6952399e295104624b36572a4c1e6afb9b38615f888d8ec60928'
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_osx.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop',
-          checkpoint: '259a12f7fe89e686b1edcad440f4b88ce677539693f89ec20f31c60a87b76ed8'
+          checkpoint: '56468370af51073f2261b475ca6387f3ac9cb9725819e9c7fe80c5c2da5ac0d7'
   name 'LehrerOffice'
   homepage 'https://www.lehreroffice.ch/'
 
