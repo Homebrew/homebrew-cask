5e6db5b1f267be2c0210264ffd03df99c7962f1d
greenrock <dd331@163.com>
2018-04-17 22:37:35 +0800

    Update hsang to 1.8.6 (#46040)

--- Casks/hsang.rb
+++ Casks/hsang.rb
@@ -1,9 +1,9 @@
 cask 'hsang' do
-  version '1.8.2'
-  sha256 'a1e54fc34b5ca82ed1b3e24f4618b832ef2df8e9d1bdb918573ac6f3d339e449'
+  version '1.8.6'
+  sha256 '74a01e493ed8df166b95e1451a69dcf5ba988830a1f176855b67accd544bf263'
 
-  # nie.gdl.netease.com/lushi was verified as official when first introduced to the cask
-  url "http://nie.gdl.netease.com/lushi/HSAng_#{version}.dmg"
+  # opd.gdl.netease.com/ was verified as official when first introduced to the cask
+  url "http://opd.gdl.netease.com/HSAng_#{version}.dmg"
   name 'HSAng'
   homepage 'http://lushi.163.com/'
 
