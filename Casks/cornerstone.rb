106bc227d885c6e4ed4739923ede2855752b9ea9
commitay <commitay@users.noreply.github.com>
2018-04-17 19:15:34 +1000

    Update cornerstone.rb

--- Casks/cornerstone.rb
+++ Casks/cornerstone.rb
@@ -3,7 +3,7 @@ cask 'cornerstone' do
   sha256 '01b3ae6c003901d006b0a1e6153ed1db619beeac8a72cecd960a30d30b9cfc47'
 
   url "https://www.zennaware.com/cornerstone/downloads/Cornerstone-#{version}.zip"
-  appcast 'https://www.zennaware.com/cornerstone/appcast/feed3.php',
+  appcast "https://www.zennaware.com/cornerstone/appcast/feed#{version.major}.php",
           checkpoint: 'a3b51cff0b3ebd79e59f0d63287f0786effcc0b94625cf6ef77bedd329740163'
   name 'Cornerstone'
   homepage 'https://www.zennaware.com/cornerstone/'
