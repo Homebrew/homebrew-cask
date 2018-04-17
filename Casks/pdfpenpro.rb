9d400601bb3f71b94bfd8ab1406698b837074428
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2018-04-17 12:18:23 -0600

    Update pdfpenpro to 1001.3,1523931775 (#46051)

--- Casks/pdfpenpro.rb
+++ Casks/pdfpenpro.rb
@@ -1,10 +1,10 @@
 cask 'pdfpenpro' do
-  version '1000.14,1523363831'
-  sha256 '0d1e407615a3f8a785850235cbee20323e048881089a684211985aa36f58bd99'
+  version '1001.3,1523931775'
+  sha256 '607725db0eda0bcde0de17742f194e4536a79df453cd575c9d836dcfb5bab238'
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/#{version.before_comma}/#{version.after_comma}/PDFpenPro-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml',
-          checkpoint: '93be2feb3b7d61af093c6878e68e7d2329316d9bc1d79a554d81e7cbdae2b1ef'
+          checkpoint: '3d911edf1a23e36fc4942814e8adaba2dd3c94c64d342eb97ebe8429b3d95a9e'
   name 'PDFpenPro'
   homepage 'https://smilesoftware.com/PDFpenPro'
 
