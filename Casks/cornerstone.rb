a44232e350ef1bc623795240fcb0d8fab6a6399a
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-17 10:30:24 +0100

    Update cornerstone to 4 (#46025)

--- Casks/cornerstone.rb
+++ Casks/cornerstone.rb
@@ -1,10 +1,10 @@
 cask 'cornerstone' do
-  version '3.1'
-  sha256 'aa1f69944c672882fe0c62fe020872dc0f4421b4f415e5a0ef89f0735d9473a5'
+  version '4'
+  sha256 '01b3ae6c003901d006b0a1e6153ed1db619beeac8a72cecd960a30d30b9cfc47'
 
   url "https://www.zennaware.com/cornerstone/downloads/Cornerstone-#{version}.zip"
-  appcast 'https://www.zennaware.com/cornerstone/appcast/feed3.php',
-          checkpoint: 'dd75dc6a03a111efc07a2a9e2faab8488da12cb7ce4bb5501a1529eb7927d3d6'
+  appcast "https://www.zennaware.com/cornerstone/appcast/feed#{version.major}.php",
+          checkpoint: '25602f6fa004698b9aaa4ff2ec442329a6c9be2ba28933582e25f42aa52484de'
   name 'Cornerstone'
   homepage 'https://www.zennaware.com/cornerstone/'
 
