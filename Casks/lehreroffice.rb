c0ad0308c8c7bbc897baa21201cc09d6a5d8ac7c
CoreCode <core-code@users.noreply.github.com>
2018-04-17 16:37:02 +0200

    Update lehreroffice to 2018.4.0 (#46039)

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,10 +1,10 @@
 cask 'lehreroffice' do
-  version '2018.2.0'
-  sha256 '5b1db19466ee31f451ac5de32ca7fa1387187f4b76311ec6effd9e534a3cf45a'
+  version '2018.4.0'
+  sha256 '2aa71874bc3300761360a9bb953f99e2429349718b78adfe1588b7256e5f628a'
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_osx.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop',
-          checkpoint: '96aab4a8f7d62edade54ceb5a82d24e4710dab28af25be8b1ef4cbe68d1d6d58'
+          checkpoint: '259a12f7fe89e686b1edcad440f4b88ce677539693f89ec20f31c60a87b76ed8'
   name 'LehrerOffice'
   homepage 'https://www.lehreroffice.ch/'
 
