b4225da6981b9dcfddf866693e8ba5fc9320cbff
Andrey Izotov <faradaytrs@gmail.com>
2018-04-18 12:32:12 +0800

    Update kugoumusic checksum
    Fix error from issue #46071

--- Casks/kugoumusic.rb
+++ Casks/kugoumusic.rb
@@ -1,6 +1,6 @@
 cask 'kugoumusic' do
   version '2.5.6'
-  sha256 'a94bee1eb2177e67537af6fbba03a25f43acf798c017112bcc27412265032ecc'
+  sha256 'a02ea949d09cac5f1808606bc93c0902c433593602fc814b323723a5a3b6cfdb'
 
   url "http://downmini.kugou.com/mac/Kugou_V#{version}.dmg"
   name 'Kugou Music'
