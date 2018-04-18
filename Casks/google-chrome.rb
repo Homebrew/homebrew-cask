f896bbc4a13e1dc920482b88db32221cd50add76
miccal <miccal.matthews@gmail.com>
2018-04-18 08:15:50 +0800

    Update google-chrome to 66.0.3359.117 (#46065)

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,10 +1,10 @@
 cask 'google-chrome' do
-  version '65.0.3325.181'
-  sha256 '60f5d8e2d7d80f8170c85e247c319b7cfd58b740c389483a5eceae3969b1a3be'
+  version '66.0.3359.117'
+  sha256 '8d050591d8bd465dcae2d60a8e699bce037d0ce51f5da4349eed78b626e9ce47'
 
   url 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
   appcast 'https://omahaproxy.appspot.com/history?os=mac;channel=stable',
-          checkpoint: '2835090971229f8e92f73e954465aad2e6f67e3bfc1ed6b05d45680599c6360b'
+          checkpoint: 'a0c323c5386160c222f6c4bacf59ae1a68d83d4244d6b1e4e2effb511b84b25e'
   name 'Google Chrome'
   homepage 'https://www.google.com/chrome/'
 
