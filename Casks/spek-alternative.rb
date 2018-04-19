1bd9b52abff3846b5fdcb2e3780fd39d5ec78a62
StoneMoe <lakechan96@gmail.com>
2018-04-19 15:47:58 +0800

    Add Spek.app(spek-alternative) v0.8.2.3
    Original spek(https://github.com/alexkay/spek) seems not maintaining anymore
    So I add the fork version of spek which can work properly as a new cask

---
+++ Casks/spek-alternative.rb
@@ -0,0 +1,12 @@
+cask 'spek-alternative' do
+  version '0.8.2.3'
+  sha256 '6426c4c34ac8bcb2fa1020cb295e3c105ca912c853cbbe84d1d196a32bba361b'
+
+  url "https://github.com/withmorten/spek-alternative/releases/download/#{version}/spek-alternative-#{version}.dmg"
+  appcast 'https://github.com/withmorten/spek-alternative/releases.atom',
+          checkpoint: '02b1d685ab571f66cbe42ec638466034afe5c3b0016ba119a2922ca12197ba14'
+  name 'spek-alternative'
+  homepage 'https://github.com/withmorten/spek-alternative'
+
+  app 'Spek.app'
+end
