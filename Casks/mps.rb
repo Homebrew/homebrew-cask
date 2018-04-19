e85f0c81e20ad1042319a8c0ba54be6333ec5684
jcbot <jcb@leipert.io>
2018-04-19 16:06:32 +0200

    Update mps to 2018.1.1,181.1254

--- Casks/mps.rb
+++ Casks/mps.rb
@@ -1,10 +1,10 @@
 cask 'mps' do
-  version '2018.1,181.1168'
-  sha256 'be094a907f63502756b48beb70e5ccc7e30d43dfff61d0138f9a27d05504b14f'
+  version '2018.1.1,181.1254'
+  sha256 '8eb3da3a06468adb392c59ae64a854e3f34083fac71dd05268568612b20cdea6'
 
   url "https://download.jetbrains.com/mps/#{version.before_comma.major_minor}/MPS-#{version.before_comma}-macos-jdk-bundled.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=MPS&latest=true&type=release',
-          checkpoint: 'caaf4be8c21071fab067172bbc8c9f06730c138d710657bb6123113454380c98'
+          checkpoint: 'a0d3664114cfeafdc4ba9af0a202ef0ad147f35507fbcd3a05cd318b594f5011'
   name 'JetBrains MPS'
   homepage 'https://www.jetbrains.com/mps/'
 
