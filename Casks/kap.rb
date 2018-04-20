4e3be672d62fa14b41f21c8058947eadd1e079bf
nrlquaker <ustynov.vy@gmail.com>
2018-04-20 19:17:17 +0300

    Update kap to 2.1.2 (#46149)

--- Casks/kap.rb
+++ Casks/kap.rb
@@ -1,11 +1,11 @@
 cask 'kap' do
-  version '2.1.1'
-  sha256 '1c04b17bb611a664c570021648e743fe8fa1ac4b7b82f57ac6796da4691374b2'
+  version '2.1.2'
+  sha256 '422096dc0dca33fb1a92366f56b76bf1ff7c25529bae1e8f18b5416856832220'
 
   # github.com/wulkano/kap was verified as official when first introduced to the cask
   url "https://github.com/wulkano/kap/releases/download/v#{version.major_minor_patch}/Kap-#{version}.dmg"
   appcast 'https://github.com/wulkano/kap/releases.atom',
-          checkpoint: '49710199f0fd6a59df5a01c11a64fa6f0fc1514d7ff476efe135aec3f9e367b1'
+          checkpoint: '592622a90fdd3ce8429213ee18fabcc5fc88a9d394d7a354231051dd4cc267c3'
   name 'Kap'
   homepage 'https://getkap.co/'
 
@@ -15,11 +15,12 @@ cask 'kap' do
   app 'Kap.app'
 
   zap trash: [
-               '~/Library/Preferences/com.wulkano.kap.plist',
-               '~/Library/Preferences/com.wulkano.kap.helper.plist',
                '~/Library/Application Support/Kap',
-               '~/Library/Saved Application State/com.wulkano.kap.savedState',
                '~/Library/Caches/com.wulkano.kap',
                '~/Library/Caches/com.wulkano.kap.ShipIt',
+               '~/Library/Cookies/com.wulkano.kap.binarycookies',
+               '~/Library/Preferences/com.wulkano.kap.helper.plist',
+               '~/Library/Preferences/com.wulkano.kap.plist',
+               '~/Library/Saved Application State/com.wulkano.kap.savedState',
              ]
 end
