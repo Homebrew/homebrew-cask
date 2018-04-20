a2a2b670343bcfd18404d33e30d5bf8ad4c84e6d
Jonghyeop Na <autumn.bom@gmail.com>
2018-04-20 23:48:37 +0900

    Add ProtoPie Latest

---
+++ Casks/protopie.rb
@@ -0,0 +1,10 @@
+cask 'protopie' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://download.protopie.io/darwin/latest'
+  name 'ProtoPie'
+  homepage 'https://www.protopie.io/'
+
+  app 'ProtoPie.app'
+end
