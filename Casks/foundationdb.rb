b083eeaaf173d854f8a89cfbfb9f6a09417e1bcb
commitay <commitay@users.noreply.github.com>
2018-04-21 06:32:25 +1000

    Update foundationdb.rb

--- Casks/foundationdb.rb
+++ Casks/foundationdb.rb
@@ -3,13 +3,15 @@ cask 'foundationdb' do
   sha256 'a611e9a86d81e7039d2b026c7cc28a7c5b431111fbbce9ed95345905e6eb7b64'
 
   url "https://www.foundationdb.org/downloads/#{version}/macOS/installers/FoundationDB-#{version}.pkg"
+  appcast 'https://www.foundationdb.org/download/',
+          checkpoint: '16c41d63b4bc7290f1118e848f8d5a1079faed2311463ba0749a5a4b8b70be66'
   name 'FoundationDB'
-  homepage 'https://www.foundationdb.org/download/'
+  homepage 'https://www.foundationdb.org/'
 
   pkg "FoundationDB-#{version}.pkg"
 
   uninstall pkgutil: [
                        'FoundationDB-clients',
-                       'FoundationDB-server'
+                       'FoundationDB-server',
                      ]
 end
