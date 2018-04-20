e303d75601c28f54bcd11dfc600b6f019e98c227
Yurii Kolesnykov <yurikoles@gmail.com>
2018-04-20 17:46:09 +0300

    new cask: foundationdb v5.1.5

---
+++ Casks/foundationdb.rb
@@ -0,0 +1,13 @@
+cask 'foundationdb' do
+  version '5.1.5'
+  sha256 'a611e9a86d81e7039d2b026c7cc28a7c5b431111fbbce9ed95345905e6eb7b64'
+
+  url "https://www.foundationdb.org/downloads/#{version}/macOS/installers/FoundationDB-#{version}.pkg"
+  name 'FoundationDB'
+  homepage 'https://www.foundationdb.org/download/'
+
+  pkg "FoundationDB-#{version}.pkg"
+
+  uninstall pkgutil: ['FoundationDB-clients',
+                      'FoundationDB-server']
+end
