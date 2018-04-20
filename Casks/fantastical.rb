59272dd18b1908c680ecc6dca93266a00b6db788
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2018-04-20 12:14:31 +0200

    Update Fantastical 2.4.8

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,10 +1,10 @@
 cask 'fantastical' do
-  version '2.4.7'
-  sha256 'c32945e883702ff20ef38fa2939ba86bb6b43351e087e043ea473359eb320852'
+  version '2.4.8'
+  sha256 '29a6a86b5181c82c99a6f602c2e0d53c47f286b4a4ef12f36857d3f8fa97aedf'
 
   url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
   appcast "https://flexibits.com/fantastical/appcast#{version.major}.php",
-          checkpoint: '92303b33b61886e2863eae07a036bea9593493c744e78005d2adf6de4d980f04'
+          checkpoint: 'ad1b26917dfe1b685b7115ab662505d05180b40c7765e9499c979d84c4958392'
   name 'Fantastical'
   homepage 'https://flexibits.com/fantastical'
 
