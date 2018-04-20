21b3b73976260b059becba363d5e33aaae3b1af8
Shoichi Aizawa <shoichiaizawa@gmail.com>
2018-04-21 06:51:08 +0900

    Update vagrant to 2.0.4 (#46161)

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,11 +1,11 @@
 cask 'vagrant' do
-  version '2.0.3'
-  sha256 '4ccdd7530a4b0172339c84a61a22bf7868c9fd1d173e7b510cf21f6deb26776e'
+  version '2.0.4'
+  sha256 '105fb631d5ae29f9f01d9680f52ba31e63727fefd77f360df9e91a528c354d01'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
   appcast 'https://github.com/hashicorp/vagrant/releases.atom',
-          checkpoint: '15e49f1530c8501e14b361a4a2eba290bbcd2b32b402cc002035e30455938519'
+          checkpoint: '623a632723cdac4d06686439afa55f4c30385522340dfcd709ebaca504b5103b'
   name 'Vagrant'
   homepage 'https://www.vagrantup.com/'
 
