dfaf001e7ac0bafbb2c4345223713932668b3833
Yuval Marcus <ymarcus93@gmail.com>
2018-04-20 19:57:57 -0400

    Update patchwork to 3.9.0

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,10 +1,10 @@
 cask 'patchwork' do
-  version '3.8.10'
-  sha256 'e25fafdf1b219a0d77ae2a5b24001fb46106b29a05d63b5a5d80da8c50194895'
+  version '3.9.0'
+  sha256 'b2af77bdc568a6b005df1de1526d6901b6b87d80685abd4109c90daa7ff9e4a3'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}-mac.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom',
-          checkpoint: '952a6faed3af64e356ed3c8172cff3733511675e471b65a1c147dc5c8488c938'
+          checkpoint: 'c758b28773840d0f3b2ea36118d3399cfbd317fc4e8823d0756edc82ccc0c0f4'
   name 'Patchwork'
   homepage 'https://github.com/ssbc/patchwork'
 
