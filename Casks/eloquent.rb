f8d2f089f376388d1ea6884bb799bf508b237997
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-21 22:13:14 +0100

    Update eloquent to 2.6.1

--- Casks/eloquent.rb
+++ Casks/eloquent.rb
@@ -1,10 +1,10 @@
 cask 'eloquent' do
-  version '2.6.0'
-  sha256 '759fce0eb74900858e9eb8146f515f9fc05fa1e519708576f9322f13164261d2'
+  version '2.6.1'
+  sha256 'd5b8f47385f9ba0e82e7c8de28b74e514ec480ed886be65e6415b58c660ea215'
 
   url "https://github.com/mdbergmann/Eloquent/releases/download/#{version}/Eloquent-#{version}.app.zip"
   appcast 'https://github.com/mdbergmann/Eloquent/releases.atom',
-          checkpoint: '192dd4e82b93dc6cbbc7a5206f304694358e957d69aac610323174489df160b4'
+          checkpoint: 'af722d9c7f36969824627d4f7d4555616681f429f044d97bf9c2f041bb234df2'
   name 'Eloquent'
   homepage 'https://github.com/mdbergmann/Eloquent'
 
