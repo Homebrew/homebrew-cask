dd2294d16e13c3582cde6eda9ce83d81db760c04
Tiago Alves <alvesjtiago@gmail.com>
2018-04-21 22:13:12 +0800

    Add Airpass.app v1.0.1

---
+++ Casks/airpass.rb
@@ -0,0 +1,11 @@
+cask 'airpass' do
+  version '1.0.1'
+  sha256 'dd13d6de4d9b430a38e188a92f371525ceb9e539587003af5ed003597bbdc05c'
+
+  # github.com/alvesjtiago/airpass was verified as official when first introduced to the cask
+  url "https://github.com/alvesjtiago/airpass/releases/download/#{version}/Airpass.dmg"
+  name 'Airpass'
+  homepage 'http://airpass.tiagoalves.me/'
+
+  app 'Airpass.app'
+end
