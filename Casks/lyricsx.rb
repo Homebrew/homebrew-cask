5ac0f27a9b48b555d91f0a6f41821136020ba55e
yuanzhoucq <31144612+yuanzhoucq@users.noreply.github.com>
2018-04-21 08:24:39 +0200

    Update lyricsx to 1.3.0 (#46171)

--- Casks/lyricsx.rb
+++ Casks/lyricsx.rb
@@ -1,10 +1,10 @@
 cask 'lyricsx' do
-  version '1.2.3'
-  sha256 '70066b02b54a7c8ce84a1c7a63e7ab7f680f27e109d9361756085f6efce21fd2'
+  version '1.3.0'
+  sha256 '6d39e5eb7e903983c58fbf986cd2ad0c782b5ceb994eabac88f00aa7f9501904'
 
   url "https://github.com/ddddxxx/LyricsX/releases/download/v#{version}/LyricsX.app.zip"
   appcast 'https://github.com/ddddxxx/LyricsX/releases.atom',
-          checkpoint: '8ad7d3ff76ffc5d54f4f908c146929d2fa66b20402490db177446c9f40771571'
+          checkpoint: '932fc65788005b8f38d8f507df73d2045ac4041f3de425f66d2b8f5def2a2845'
   name 'LyricsX'
   homepage 'https://github.com/ddddxxx/LyricsX'
 
