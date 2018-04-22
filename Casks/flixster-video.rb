9b5c3b99474763b2f4b03769075ab56b4ab395e9
commitay <commitay@users.noreply.github.com>
2018-04-23 07:50:15 +1000

    Update flixster-video.rb

--- Casks/flixster-video.rb
+++ Casks/flixster-video.rb
@@ -3,8 +3,8 @@ cask 'flixster-video' do
   sha256 'eef953f1b4636d158a594728a9bed6ded61f5af1402bd7432cce817e836f1346'
 
   # dtmmt9rxsy2no.cloudfront.net was verified as official when first introduced to the cask
-  url 'http://dtmmt9rxsy2no.cloudfront.net/desktop/mac/FlixsterDesktop.zip'
-  appcast 'https://d1rtylazwb77ux.cloudfront.net/desktop/mac/FlixsterDesktopMacAppcast.xml',
+  url 'https://dtmmt9rxsy2no.cloudfront.net/desktop/mac/FlixsterDesktop.zip'
+  appcast 'https://dtmmt9rxsy2no.cloudfront.net/desktop/mac/FlixsterDesktopMacAppcast.xml',
           checkpoint: '58614097c1c907f7a693ffd55ffcfc493a51d5ad1e176f80ba7deebe361f606d'
   name 'Flixster Video'
   homepage 'https://www.flixstervideo.com/apps'
