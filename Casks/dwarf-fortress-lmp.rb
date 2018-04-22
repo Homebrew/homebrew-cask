503571cfccdbddd2b5416866ffee14b2cc9d3c62
commitay <commitay@users.noreply.github.com>
2018-04-23 06:53:09 +1000

    Update dwarf-fortress-lmp.rb

--- Casks/dwarf-fortress-lmp.rb
+++ Casks/dwarf-fortress-lmp.rb
@@ -3,12 +3,12 @@ cask 'dwarf-fortress-lmp' do
   sha256 '33bf89854dc84a98b7f149e56368eedb2c204b64eeede572280cf5fc9485fa34'
 
   # dffd.bay12games.com was verified as official when first introduced to the cask
-  url 'http://dffd.bay12games.com/download.php?id=13338&f=Lazy+Mac+Pack.v{version}.dmg'
+  url "http://dffd.bay12games.com/download.php?id=13338&f=Lazy+Mac+Pack.v#{version}.dmg"
   name 'Dwarf Fortress LMP (Lazy Mac Pack)'
   homepage 'http://www.bay12forums.com/smf/index.php?topic=128960.0'
 
   # Renamed for clarity: suite name is inconsistent with branding
-  suite 'Lazy Mac Pack v0.44.05 dfhack-r2', target: 'Dwarf Fortress LMP'
+  suite "Lazy Mac Pack v#{version.sub('-', ' ')}", target: 'Dwarf Fortress LMP'
 
   zap trash: [
                '~/Library/Preferences/Lazy Mac Pack.plist',
