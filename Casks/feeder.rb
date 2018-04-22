aadb93c2c1071036403c110215c9fad76a0a4507
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-22 20:18:36 +0100

    Update feeder to 3.5.9

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -1,10 +1,10 @@
 cask 'feeder' do
   version '3.5.9'
-  sha256 '03e3cdb567b6a023b3694e89e7f8061cd51f66f30b9603f6be4b69e94136b111'
+  sha256 '228020aa239e9fc9616df0446fc8471d1f3578ab45a3fabc29353546090177d0'
 
   url "https://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
   appcast "https://reinventedsoftware.com/feeder/downloads/Feeder#{version.major}.xml",
-          checkpoint: '1005915612df1be9b1ce4d99d19ab2ad3c317e0759e38697abf6283ab6b5a06d'
+          checkpoint: '5d62ec3c33af738c5cd404334b79bea3921d98e685182740693d9f2bb5e30b51'
   name 'Feeder'
   homepage 'https://reinventedsoftware.com/feeder/'
 
