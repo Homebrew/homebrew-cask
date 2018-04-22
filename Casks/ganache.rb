9016686dc5dba202c569e63d1f932399708cf4e3
Max Riveiro <kavu13@gmail.com>
2018-04-22 10:49:14 +0300

    Fix appcast checksum

--- Casks/ganache.rb
+++ Casks/ganache.rb
@@ -5,7 +5,7 @@ cask 'ganache' do
   # github.com/trufflesuite/ganache was verified as official when first introduced to the cask
   url "https://github.com/trufflesuite/ganache/releases/download/v#{version}/Ganache-#{version}.dmg"
   appcast 'https://github.com/trufflesuite/ganache/releases.atom',
-          checkpoint: 'ec43ed8e40d1fdcffbccccc3a6b659849222a34ba3e609c8dcdda67928a6f99f'
+          checkpoint: '7511886116d0e63af6d8e99b86ee117db4bc5126464a229428b2739390df9a95'
   name 'Ganache'
   homepage 'http://truffleframework.com/ganache/'
 
