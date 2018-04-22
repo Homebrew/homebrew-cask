49a7a5482976a19531e7220d67b94a95aa086ca7
Alex Oswald <aozwald3@gmail.com>
2018-04-22 17:25:41 -0400

    Fix checksum

--- Casks/persepolis-download-manager.rb
+++ Casks/persepolis-download-manager.rb
@@ -1,6 +1,6 @@
 cask 'persepolis-download-manager' do
   version '3.1.0'
-  sha256 'bed6e1b35468978fe734c8afab23fbb03cc19dc556df2360c222c7a3e3de7295'
+  sha256 '70be7692e4b7a5f596a570dbcd4b9c5c077ab5481986952fa59c3dc228bc9bb5'
 
   # github.com/persepolisdm/persepolis was verified as official when first introduced to the cask
   url "https://github.com/persepolisdm/persepolis/releases/download/#{version}/persepolis_#{version.dots_to_underscores}_mac.dmg"
