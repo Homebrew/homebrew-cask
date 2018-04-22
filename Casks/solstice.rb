4c279253bc244c79e3ba551558a617707bc5d0ec
commitay <commitay@users.noreply.github.com>
2018-04-23 09:10:28 +1000

    Update solstice.rb

--- Casks/solstice.rb
+++ Casks/solstice.rb
@@ -2,9 +2,11 @@ cask 'solstice' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.mersive.com/downloads/SolsticeClientMacWeb.zip'
+  url 'https://www.mersive.com/downloads/SolsticeClientMacWeb.zip'
   name 'Solstice Client'
   homepage 'https://www2.mersive.com/'
 
-  app 'SolsticeClientMacWeb.app'
+  installer manual: 'SolsticeClientMacWeb.app'
+
+  uninstall delete: '~/Desktop/Solstice Client.app'
 end
