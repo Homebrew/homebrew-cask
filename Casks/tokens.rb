e82caab53360f0611573f9e48545cc55c89e1aad
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-23 23:10:59 +0100

    Update tokens to latest (#46245)
    * Update tokens to latest
    
    * Update tokens.rb

--- Casks/tokens.rb
+++ Casks/tokens.rb
@@ -1,10 +1,11 @@
 cask 'tokens' do
-  version '1.5.4'
-  sha256 '9b33639cdd9037d7c385432c3e2aabc0cce066002b5f3b767b82257f3cda167b'
+  version '1.6.3-68'
+  sha256 'c9135ae7c5b6fff36857f5057eca67b052900cf062cc8226484c6b5b6e0cd5e2'
 
-  url "http://resources.usetokens.com/download/#{version}/Tokens.app.zip"
-  appcast 'http://api.usetokens.com/updates/stable',
-          checkpoint: 'd61bd5c28aeb1288b7e47d17456eb938182368935f54a77b973f3e5d1b645b1f'
+  # peerassembly.net/apps/tokens was verified as official when first introduced to the cask
+  url "https://peerassembly.net/apps/tokens/#{version}/Tokens.zip"
+  appcast 'https://peerassembly.net/apps/tokens/stable.xml',
+          checkpoint: '96cf1a8dec3e8a96c7eff2a6d79e89f014afee33d57a18ae30ae1466d13774cb'
   name 'Tokens'
   homepage 'http://usetokens.com/'
 
