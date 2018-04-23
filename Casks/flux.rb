b22867614a77586120688753381233d618b395ae
Victor Popkov <victor@popkov.me>
2018-04-24 00:04:30 +0300

    Update flux to latest

--- Casks/flux.rb
+++ Casks/flux.rb
@@ -1,10 +1,8 @@
 cask 'flux' do
-  version '39.983'
-  sha256 '7b04effd88f4b12029cc929d9b3a5646e35be89bb5ea4404157133e9c96348e6'
+  version :latest
+  sha256 :no_check
 
-  url "https://justgetflux.com/mac/Flux#{version}.zip"
-  appcast 'https://justgetflux.com/mac/macflux.xml',
-          checkpoint: '4bf6d537d257ade71e07a188d635e5c92afeb043eb04f0d6b0927ba202839159'
+  url 'https://justgetflux.com/mac/Flux.zip'
   name 'f.lux'
   homepage 'https://justgetflux.com/'
 
