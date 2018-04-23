d9d391635204beeffa02e31f45374e1d82104f62
Clone Liu <silver886@users.noreply.github.com>
2018-04-23 14:28:05 -0500

    Update wavebox to 3.14.3 (#46225)

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,11 +1,11 @@
 cask 'wavebox' do
-  version '3.14.2'
-  sha256 '28b1622eb19ee2285d4b18356a0583314ce2850d126b58a55ad53b503ac49899'
+  version '3.14.3'
+  sha256 'ca64f65d2640ab1081b6a8a6ad4881f8a73a7f158d8a635d3e391a48df9120a8'
 
   # github.com/wavebox/waveboxapp was verified as official when first introduced to the cask
   url "https://github.com/wavebox/waveboxapp/releases/download/v#{version}/Wavebox_#{version.dots_to_underscores}_osx.dmg"
   appcast 'https://github.com/wavebox/waveboxapp/releases.atom',
-          checkpoint: '05a6bef4c12a3ae2364d18d7932b9676e70899ce4448d834b087c78e341de677'
+          checkpoint: '16aaeed4b32c47e3401499a2486a936fde5fecadf1452db43b246731e563a953'
   name 'Wavebox'
   homepage 'https://wavebox.io/'
 
