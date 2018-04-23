b18a5b212b3b28d454f330eb161a8d228089e9c2
Victor Popkov <victor@popkov.me>
2018-04-23 22:58:58 +0300

    Update nicecast to 1.11.13

--- Casks/nicecast.rb
+++ Casks/nicecast.rb
@@ -1,10 +1,10 @@
 cask 'nicecast' do
-  version '1.11.12'
-  sha256 '2ef02a7f7ebca53fa04ca4bd2bdfea43051f4faf92ae00b3250c878855023141'
+  version '1.11.13'
+  sha256 '99ea875e2e4c8bfd7a976150140f25c85e006fdad2dc7124f7ae61fcba940537'
 
   url 'https://rogueamoeba.com/nicecast/download/Nicecast.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Nicecast&system=10114',
-          checkpoint: '27f3f694058494e971e7d283612e4fa33972479d5eccc2b87fe0bdc52ffbf984'
+          checkpoint: 'f46489742f12e8fdfaab7f518276cddf7c22916a389dd554196922d89f5ed89b'
   name 'Nicecast'
   homepage 'https://rogueamoeba.com/nicecast/'
 
