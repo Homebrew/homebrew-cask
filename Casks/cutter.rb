97b50c3d83715ef22d907ca963e902cbd2606ec6
nico <25092016+ndaprela@users.noreply.github.com>
2018-04-23 20:29:54 +0200

    Update Cutter 1.4

--- Casks/cutter.rb
+++ Casks/cutter.rb
@@ -1,11 +1,11 @@
 cask 'cutter' do
   version '1.4'
-  sha256 '2b91b1c423d4635e814554c5594104b90c76170719e83f0056d84c246a004184'
+  sha256 '77b8642cbf58345aece75ddb76683b080a43a7f92c6726d04bdb2d9a4aa52fc2'
 
   # github.com/radareorg/cutter was verified as official when first introduced to the cask
   url "https://github.com/radareorg/cutter/releases/download/v#{version}/Cutter.dmg"
   appcast 'https://github.com/radareorg/cutter/releases.atom',
-          checkpoint: '8709e44dc6961c326f8f71d641ccbe5c4f078f81a4667e1ab0e8788e50e0b967'
+          checkpoint: '2b91b1c423d4635e814554c5594104b90c76170719e83f0056d84c246a004184'
   name 'Cutter'
   homepage 'https://radare.org/cutter/'
 
