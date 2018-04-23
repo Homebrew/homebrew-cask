77aeb497327a5b58dc855c7470acea90ac6b51ab
nico <25092016+ndaprela@users.noreply.github.com>
2018-04-23 19:54:36 +0200

    Update Cutter to 1.4

--- Casks/cutter.rb
+++ Casks/cutter.rb
@@ -1,15 +1,15 @@
 cask 'cutter' do
-  version '1.3'
-  sha256 '880a229fac750976781d1bb82cd6a6622bd5274b90428583ee75e2e1362ea0e6'
+  version '1.4'
+  sha256 '77b8642cbf58345aece75ddb76683b080a43a7f92c6726d04bdb2d9a4aa52fc2'
 
   # github.com/radareorg/cutter was verified as official when first introduced to the cask
-  url "https://github.com/radareorg/cutter/releases/download/v#{version}/cutter-v#{version}.dmg"
+  url "https://github.com/radareorg/cutter/releases/download/v#{version}/Cutter.dmg"
   appcast 'https://github.com/radareorg/cutter/releases.atom',
-          checkpoint: 'fbc1b3bec7fd59304b7074bac59976ccd662aa583e142c83bb272db3892c6fbf'
+          checkpoint: 'fb725365fe7edafe21e8f00148a822d15683ea5d8314a63e13bf07e5eb9b7c48'
   name 'Cutter'
   homepage 'https://radare.org/cutter/'
 
-  app 'cutter.app'
+  app 'Cutter.app'
 
   zap trash: [
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/test.cutter.sfl*',
