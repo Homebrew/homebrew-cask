7cad72e74cfd8bc6fe9800d2f53c7bf59d970bb4
Vítor Galvão <info@vitorgalvao.com>
2018-04-23 21:54:47 +0100

    Update tokens.rb

--- Casks/tokens.rb
+++ Casks/tokens.rb
@@ -1,9 +1,11 @@
 cask 'tokens' do
-  version :latest
-  sha256 :no_check
+  version '1.6.3-68'
+  sha256 'c9135ae7c5b6fff36857f5057eca67b052900cf062cc8226484c6b5b6e0cd5e2'
 
   # peerassembly.net/apps/tokens was verified as official when first introduced to the cask
-  url 'http://peerassembly.net/apps/tokens/Tokens.zip'
+  url "https://peerassembly.net/apps/tokens/#{version}/Tokens.zip"
+  appcast 'https://peerassembly.net/apps/tokens/stable.xml',
+          checkpoint: '96cf1a8dec3e8a96c7eff2a6d79e89f014afee33d57a18ae30ae1466d13774cb'
   name 'Tokens'
   homepage 'http://usetokens.com/'
 
