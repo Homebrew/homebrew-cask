ea908bfe579b1f32564fd905fb6444c890d25149
Brian Morton <brianmortondesign@gmail.com>
2018-04-23 09:20:21 -0700

    Update portfolioperformance to 0.30.1

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,11 +1,11 @@
 cask 'portfolioperformance' do
-  version '0.29.0'
-  sha256 'e1f5536bcd8d8b79e2e1d8056e36be8cd3fecb30387687f60c2a5ae5888aae9f'
+  version '0.30.1'
+  sha256 '63db3e2e578e74e63c46dbac8bc2a69735dac7cf2686f07405b28ae93fccd60a'
 
   # s3.amazonaws.com/name.abuchen.portfolio was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/name.abuchen.portfolio/#{version}/PortfolioPerformance-#{version}-macosx.cocoa.x86_64.tar.gz"
   appcast 'https://github.com/buchen/portfolio/releases.atom',
-          checkpoint: 'ec20e1315a62d45ddf35877aa052e2603862dfec05d64d883864838a4ce56731'
+          checkpoint: 'b27eb43b6a17c2fb323de7da245e8f531fbddf8a351a2c2c24f6d5f29ac27844'
   name 'Portfolio Performance'
   homepage 'http://www.portfolio-performance.info/portfolio/'
 
