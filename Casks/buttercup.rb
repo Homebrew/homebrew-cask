ed333d7b85374082e6d699264ede902960b1ecb2
Victor Popkov <victor@popkov.me>
2018-04-23 22:43:21 +0300

    Update buttercup to 1.6.2

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -1,11 +1,11 @@
 cask 'buttercup' do
-  version '1.6.1'
-  sha256 '0ce076efe304f1d68f31906b428bb80a72b0a2c04572890c312fe8c605eb4d3a'
+  version '1.6.2'
+  sha256 'e73748a31f8eda1fe74aeb38025f5aef314498e10049ffb48152118036f29b9a'
 
   # github.com/buttercup/buttercup-desktop was verified as official when first introduced to the cask
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/buttercup-desktop-#{version}-mac.zip"
   appcast 'https://github.com/buttercup/buttercup-desktop/releases.atom',
-          checkpoint: 'b09503f840f848bdb171bb131ff30550d490b7ed535f47ef08361ec0e70a5f31'
+          checkpoint: 'ae5d4f2f2560672309a65359277815f140774e60bf15b6732a6b1aed70c0c84d'
   name 'Buttercup'
   homepage 'https://buttercup.pw/'
 
