8289e393d98387c0e4eb6a595dbed16f46867aee
Vítor Galvão <info@vitorgalvao.com>
2018-04-23 15:39:36 +0100

    Update nomachine.rb

--- Casks/nomachine.rb
+++ Casks/nomachine.rb
@@ -8,21 +8,16 @@ cask 'nomachine' do
 
   pkg 'NoMachine.pkg'
 
-  # a launchctl job ordinarily manages uninstall once the app bundle is removed
-
-  uninstall delete: '/Applications/NoMachine.app'
-
-  # however, we duplicate the uninstall process manually in the zap stanza just in case
-  zap early_script: {
-                      executable: '/bin/rm',
-                      args:       ['-f', '--', '/Library/Application Support/NoMachine/nxuninstall.sh'],
-                    },
-      quit:         'com.nomachine.nxdock',
-      kext:         [
-                      'com.nomachine.driver.nxau',
-                      'com.nomachine.driver.nxtun',
-                      'com.nomachine.kext.nxfs',
-                    ],
-      pkgutil:      'com.nomachine.nomachine.NoMachine.*',
-      launchctl:    'com.nomachine.uninstall'
+  # A launchctl job ordinarily manages uninstall once the app bundle is removed
+  uninstall delete: '/Applications/NoMachine.app',
+  # However, we duplicate the uninstall process manually just in case
+            script: '/Library/Application Support/NoMachine/nxuninstall.sh',
+            quit:         'com.nomachine.nxdock',
+            kext:         [
+                            'com.nomachine.driver.nxau',
+                            'com.nomachine.driver.nxtun',
+                            'com.nomachine.kext.nxfs',
+                          ],
+            pkgutil:      'com.nomachine.nomachine.NoMachine.*',
+            launchctl:    'com.nomachine.uninstall'
 end
