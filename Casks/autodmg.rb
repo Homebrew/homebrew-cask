331991532e37eb1636315f90dfdeeb492777c41a
commitay <commitay@users.noreply.github.com>
2018-04-24 20:15:30 +1000

    Update autodmg to 1.8.1 (#46272)

--- Casks/autodmg.rb
+++ Casks/autodmg.rb
@@ -1,10 +1,10 @@
 cask 'autodmg' do
-  version '1.8'
-  sha256 '595c32f269b613739ef7db49b04c3fa7aa830594545e5393811fb42bbc49a8da'
+  version '1.8.1'
+  sha256 '17716f7e87ce803250e10f8b58dc450e45c82c78a3ee63da0941067f8fa0b2d2'
 
   url "https://github.com/MagerValp/AutoDMG/releases/download/v#{version}/AutoDMG-#{version}.dmg"
   appcast 'https://github.com/MagerValp/AutoDMG/releases.atom',
-          checkpoint: 'c4e649d69f24ca008a9d5f7e22b695af1c1d1762ed6fceaaf8f87620f6673151'
+          checkpoint: 'ec2d86a5db77dee8f35e166f63c4a49c967ede31cf932b8797c343c2ff244360'
   name 'AutoDMG'
   homepage 'https://github.com/MagerValp/AutoDMG'
 
