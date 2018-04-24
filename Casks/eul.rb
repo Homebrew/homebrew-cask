674c8129c2994346edac2f82d3fbe9bd18a48db5
Alexander Medvednikov <alexander@medvednikov.com>
2018-04-24 10:19:27 +0300

    Fix eul url

--- Casks/eul.rb
+++ Casks/eul.rb
@@ -2,7 +2,7 @@ cask 'eul' do
   version '0.34'
   sha256 '4f603d2408b18c8dd6f854bc1b3229f319fd2c7f1652b685d10ac535422a3dbe'
 
-  url 'https://github.com/eul-im/eul/releases/download/v#{version}/eul_mac.zip'
+  url "https://github.com/eul-im/eul/releases/download/v#{version}/eul_mac.zip"
   name 'eul'
   homepage 'https://eul.im/'
 
