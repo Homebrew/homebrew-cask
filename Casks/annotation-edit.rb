39780592718c6f4af135ca265ed9c6b5adff2eda
CoreCode <core-code@users.noreply.github.com>
2018-04-25 00:54:16 +0200

    Update annotation-edit to 1.9.94 (#46295)

--- Casks/annotation-edit.rb
+++ Casks/annotation-edit.rb
@@ -1,10 +1,10 @@
 cask 'annotation-edit' do
-  version '1.9.93'
-  sha256 'e86f020d0c378df8f005c5b6688890ab109c789691e5ad2fa330020106c1e4df'
+  version '1.9.94'
+  sha256 '7ce5666636f950c6720d2c949337bfeb408769aadd2a9c54b510bd1dafce3cfe'
 
   url 'http://www.zeitanker.com/common/Annotation_Edit.zip'
   appcast 'http://zeitanker.com/updates.rss',
-          checkpoint: '2e87e16c81421562cc4d16a51031f70ca5e67c8465b9c1a91b88f7a1e8005a31'
+          checkpoint: 'cf288c041abf8edd02494b48f2ff6ff9135bc85bae84b630baf36dd12c5406f7'
   name 'Annotation Edit'
   homepage 'http://www.zeitanker.com/content/tools/zeitanker_tools/zeitanker_annotation_edit/'
 
