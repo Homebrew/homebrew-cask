c1989b3c5b26a7fbf30609be61b769483a9ab219
commitay <commitay@users.noreply.github.com>
2018-04-26 00:22:04 +1000

    Update polycom-realpresence.rb

--- Casks/polycom-realpresence.rb
+++ Casks/polycom-realpresence.rb
@@ -1,8 +1,8 @@
 cask 'polycom-realpresence' do
-  version '3_9_0_69879'
+  version '3.9.0.69879'
   sha256 '8b2e42522d79c438d0067cdfa8a99743fca98e1e5c759d74565ead4ff9d34265'
 
-  url "https://downloads.polycom.com/video/realpresence_desktop/RPDMac_#{version}.dmg"
+  url "https://downloads.polycom.com/video/realpresence_desktop/RPDMac_#{version.dots_to_underscores}.dmg"
   name 'Polycom RealPresence Desktop'
   homepage 'http://www.polycom.com/products-services/hd-telepresence-video-conferencing/realpresence-desktop/realpresence-desktop.html'
 
