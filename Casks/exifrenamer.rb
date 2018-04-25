2b27d6541f24285ad9143551cf090e36ba99043e
commitay <commitay@users.noreply.github.com>
2018-04-26 00:52:34 +1000

    Update exifrenamer.rb

--- Casks/exifrenamer.rb
+++ Casks/exifrenamer.rb
@@ -2,8 +2,8 @@ cask 'exifrenamer' do
   version '2.2.0'
   sha256 '9ab4382186943cbeaceaa2a5f22c0f6b69f095468a4aeb15537e68239820c60f'
 
-  url 'http://www.qdev.de/downloads/files/ExifRenamer.dmg'
-  appcast 'http://www.qdev.de/versions/ExifRenamer.txt',
+  url 'https://www.qdev.de/downloads/files/ExifRenamer.dmg'
+  appcast 'https://www.qdev.de/versions/ExifRenamer.txt',
           checkpoint: 'edc8e395a5182e3b231816e191b303407d511f70c1d9cb6d532927cc559c507c'
   name 'ExifRenamer'
   homepage 'https://www.qdev.de/?location=mac/exifrenamer&forcelang=en'
