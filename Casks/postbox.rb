f18a6bb3ab5a965963669598a23b4c9b173ba659
CoreCode <core-code@users.noreply.github.com>
2018-04-25 03:24:03 +0200

    Update postbox to 6.0.5,1_a8751702fe9e017c2d4ed3fd23c6b6aa9b230c49 (#46309)

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,13 +1,13 @@
 cask 'postbox' do
-  version '5.0.24,1_c5810ea796337b300b4f30dac967fb7623b5a16c'
-  sha256 '124704184e020d41cdbc7e587aea9a04cf8ccae6216ee330d895d8a827fc2665'
+  version '6.0.5,1_a8751702fe9e017c2d4ed3fd23c6b6aa9b230c49'
+  sha256 'cd3ef95c5778c87fd4da63d20a26fa524a1164d82644dbe873ba56b44a07cc70'
 
   # amazonaws.com/download.getpostbox.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/download.getpostbox.com/installers/#{version.before_comma}/#{version.after_comma}/postbox-#{version.before_comma}-mac64.dmg"
   name 'Postbox'
   homepage 'https://www.postbox-inc.com/'
 
-  depends_on macos: '>= :mavericks'
+  depends_on macos: '>= :el_capitan'
 
   app 'Postbox.app'
 
