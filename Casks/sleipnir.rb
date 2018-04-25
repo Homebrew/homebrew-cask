3734f57fd9df53e211839fd5accc33be0ef75c91
CoreCode <core-code@users.noreply.github.com>
2018-04-25 19:52:58 +0200

    Update sleipnir to 4.5.10 (#46352)

--- Casks/sleipnir.rb
+++ Casks/sleipnir.rb
@@ -1,10 +1,10 @@
 cask 'sleipnir' do
-  version '4.5.8'
-  sha256 'f94759d0061a6c666d75486aa574c4ab27d6fe3621c616546e4e7befe32c52e0'
+  version '4.5.10'
+  sha256 '384b646c53f62c1c52e1a480bd8182150bcd2c39ab7c4066cb6c2890fbb6d0bc'
 
   url 'http://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
   appcast 'https://update.fenrir.co.jp/smartupdate/mac/sleipnir/appcast.xml',
-          checkpoint: '0bee6e1f3e97bc49e3b9200ae5831861f15e426f20ae05bc975d08b316ce864f'
+          checkpoint: 'f3018f83204fb8ad6689ce0cfee0e3ff536172cd4675a81f127b9aa0a87df540'
   name 'Sleipnir'
   homepage 'https://www.fenrir-inc.com/jp/sleipnir/'
 
