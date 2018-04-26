0361eeecc11a2fc542875987abbe997dd8bf4e04
greenrock <dd331@163.com>
2018-04-26 09:35:40 +0800

    Update eclipse-ide to 4.7.3a,oxygen:3a (#46383)

--- Casks/eclipse-ide.rb
+++ Casks/eclipse-ide.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-ide' do
-  version '4.7.3,oxygen:3'
-  sha256 '8dc8c366f2f1eab94c6eb9adaff8506363debd84a953f932e1c28e5652e32853'
+  version '4.7.3a,oxygen:3a'
+  sha256 'de8dfa56b0e1047078e539831b3c958dab12d4ca870f8249293650840aa863ab'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-committers-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for Eclipse Committers'
