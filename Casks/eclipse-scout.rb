430e78fbcea44a3dd708597c9781ac4a51085619
warmwinter <github@miss77.net>
2018-04-26 14:04:49 +0800

    Update eclipse-scout to 4.7.3a,oxygen:3a

--- Casks/eclipse-scout.rb
+++ Casks/eclipse-scout.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-scout' do
-  version '4.7.3,oxygen:3'
-  sha256 '71cc7f05abe65055a2d353a0a0e217090ae9a980f77716074547e516474ef15d'
+  version '4.7.3a,oxygen:3a'
+  sha256 '98d244b15a7cec11e2a703f2fb6c5ab1a7595d87b20aa192bf641500b9380dc0'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-scout-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Scout Developers'
