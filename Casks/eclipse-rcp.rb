2e744e5c5a9e272fb13b3c7dd724711d93f1ab3d
warmwinter <github@miss77.net>
2018-04-26 14:35:51 +0800

    Update eclipse-rcp to 4.7.3a,oxygen:3a

--- Casks/eclipse-rcp.rb
+++ Casks/eclipse-rcp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-rcp' do
-  version '4.7.3,oxygen:3'
-  sha256 '0d5309817d0f49a43f776ec71bb5d38d690c29c701f143f7f276dc0c58644ed3'
+  version '4.7.3a,oxygen:3a'
+  sha256 '9aa9fa4d3f2e1e1b3776ea999276b82b1ff9179bf499da348ff9c16b9c503e60'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-rcp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for RCP and RAP Developers'
