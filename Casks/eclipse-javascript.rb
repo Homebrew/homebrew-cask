e4dd4a2e8a53b9c14145603b012cb7e2fb06be00
warmwinter <github@miss77.net>
2018-04-26 13:34:59 +0800

    Update eclipse-javascript to 4.7.3a,oxygen:3a

--- Casks/eclipse-javascript.rb
+++ Casks/eclipse-javascript.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-javascript' do
-  version '4.7.3,oxygen:3'
-  sha256 '566a771a65d5cf328e3a7803a845d6ec49cb40557e4d91ed95dc0499cab63565'
+  version '4.7.3a,oxygen:3a'
+  sha256 'd32d353ad5e2d07aef0fe30cff7e1abef79fd8810890519b25535b8fb2789201'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-javascript-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for JavaScript and Web Developers'
