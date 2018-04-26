190d8472c68ca3afcd9999f855a4294844cb9edf
Gobinath Mallaiyan <i@gobinath.com>
2018-04-25 20:16:29 -0400

    Update insomnia to 5.16.0

--- Casks/insomnia.rb
+++ Casks/insomnia.rb
@@ -1,11 +1,11 @@
 cask 'insomnia' do
-  version '5.15.0'
-  sha256 '2e9ff5be8bd18bd2cb8936966729ecf8f970090aee53407aebb6f42071e83ec8'
+  version '5.16.0'
+  sha256 'a3c3146b90b1d34f523767137d0b2f628e45817ca97586348a9ae5c923cc7c30'
 
   # github.com/getinsomnia/insomnia was verified as official when first introduced to the cask
   url "https://github.com/getinsomnia/insomnia/releases/download/v#{version}/Insomnia-#{version}.dmg"
   appcast 'https://insomnia.rest/changelog/index.xml',
-          checkpoint: '0094be413d34e5fb669c0c1816a5f32a5327c6c451bfa26c8480ab910844bfe4'
+          checkpoint: 'd224a2b037a10d2b77a5bc752fbf2311d1e479038197df6151a9eecd01bf5df8'
   name 'Insomnia'
   homepage 'https://insomnia.rest/'
 
