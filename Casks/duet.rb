43f4f7aa16120e11fcaf22c40b0748e2380937f8
commitay <commitay@users.noreply.github.com>
2018-04-28 07:31:59 +1000

    Update duet.rb

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -5,7 +5,7 @@ cask 'duet' do
   # duet.nyc3.digitaloceanspaces.com/Mac was verified as official when first introduced to the cask
   url "https://duet.nyc3.digitaloceanspaces.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip"
   appcast 'https://updates.duetdisplay.com/checkMacUpdates',
-          checkpoint: 'b536da0feb2140c61711c9a3ac37c0e33acfbbdff505a73a2fabf243704fa17e'
+          checkpoint: 'c0c43acdfb45bb85c673c36e191d16a74c46105f8bfdbb185de75bd9172b1624'
   name 'Duet'
   homepage 'https://www.duetdisplay.com/'
 
