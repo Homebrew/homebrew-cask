7d92173d205fc2b8dc3e9aa276f998c00c8175c5
commitay <commitay@users.noreply.github.com>
2018-04-28 22:21:40 +1000

    Update airunlock.rb

--- Casks/airunlock.rb
+++ Casks/airunlock.rb
@@ -2,7 +2,7 @@ cask 'airunlock' do
   version '0.4'
   sha256 '85f4aee6bc67f6b7c0d01ab36e50a117690574c0e31b210aa3f80931269bf170'
 
-  # airunlock.qtlin.tw was verified as official when first introduced to the cask
+  # github.com/pinetum/AirUnlock-for-Mac was verified as official when first introduced to the cask
   url "https://github.com/pinetum/AirUnlock-for-Mac/releases/download/#{version}/AirUnlock_mac_#{version}.zip"
   appcast 'https://github.com/pinetum/AirUnlock-for-Mac/releases.atom',
           checkpoint: '9726ba88fbaae1966769444d7dc4f845602b2c4e0fdcb723ae9ef792db696bdb'
