d52af8b4240225f4465f2f6005e0f8d871180034
commitay <commitay@users.noreply.github.com>
2018-04-28 16:48:08 +1000

    Update open-in-code.rb

--- Casks/open-in-code.rb
+++ Casks/open-in-code.rb
@@ -2,7 +2,7 @@ cask 'open-in-code' do
   version '1.0'
   sha256 'a473c247179c91621ef447f75f3103e1c9bc1459f6622b108e0c7bdd5b6f8367'
 
-  url 'https://github.com/sozercan/OpenInCode/releases/download/v1.0/OpenInCode.app.zip'
+  url "https://github.com/sozercan/OpenInCode/releases/download/v#{version}/OpenInCode.app.zip"
   appcast 'https://github.com/sozercan/OpenInCode/releases.atom',
           checkpoint: '8293a9f1f5369eadcae2c4b03f125adfdd59b53695362202a3f25dd4a0520954'
   name 'OpenInCode'
