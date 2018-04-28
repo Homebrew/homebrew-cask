e5f84af823f6d1096bfa864ac9d89738908a510f
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-29 00:21:41 +0100

    Update sejda-pdf to 3.4.0 (#46484)

--- Casks/sejda-pdf.rb
+++ Casks/sejda-pdf.rb
@@ -1,6 +1,6 @@
 cask 'sejda-pdf' do
-  version '3.2.5'
-  sha256 'adc2012860abba6717c81ba6b6a0e260929f3234a89c7c9dc3d38cf31400bc67'
+  version '3.4.0'
+  sha256 'da8d92037fb48d5794b250aa789ec616c0a865c60fda047e246f5983e9bff1c3'
 
   # bitbucket.org/sejdapdf/sejda-desktop-public was verified as official when first introduced to the cask
   url "https://bitbucket.org/sejdapdf/sejda-desktop-public/downloads/sejda-desktop_#{version}.dmg"
