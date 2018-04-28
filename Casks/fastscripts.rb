fe86ba2db6ff8d8d7ec59bcb2df6bc287d64cdc6
Victor Popkov <victor@popkov.me>
2018-04-28 03:51:28 +0300

    Update fastscripts to 2.6.13 (#46461)

--- Casks/fastscripts.rb
+++ Casks/fastscripts.rb
@@ -1,10 +1,10 @@
 cask 'fastscripts' do
-  version '2.6.12'
-  sha256 '591b2d6808b4bdc4d770ebb16ec2ded06b9b1fa1463bfe3296ad1503533e0959'
+  version '2.6.13'
+  sha256 '0324cf741f989e9f224259af2b79fcb163fcedcf45e790f8ccb747ffa1971b6c'
 
   url "https://www.red-sweater.com/fastscripts/FastScripts#{version}.zip"
   appcast 'https://red-sweater.com/fastscripts/appcast2.php',
-          checkpoint: 'a3c8821c03d51b36b0adda5a2e030dc27d052180e99c52e5b7bdae1d2bca1b62'
+          checkpoint: '1a6ece35fbf0c62b743bec5639d868e9a5ac715eaf7c9c88d71b7cb10e22ebe4'
   name 'FastScripts'
   homepage 'https://red-sweater.com/fastscripts/'
 
