6db3fcece8cc757866d5afd5e6b1d70962fe13d7
7IN0SAN9 <me@7in0.me>
2018-04-29 22:13:31 +0800

    Update CocosCreator to v1.9.1(20180417)

--- Casks/cocoscreator.rb
+++ Casks/cocoscreator.rb
@@ -1,8 +1,11 @@
 cask 'cocoscreator' do
-  version '1.5.1_2017061501'
-  sha256 'f882bc1b9a79dc07e02820baee2068ad449febd57585dec6e5af38271a23e365'
+  number = '1.9.1'
+  date = '20180417'
+  version "#{number}_#{date}"
+  sha256 '9d848ce4af7dfb84974bd24db36bdb766620c4e0eb81aaaa0eaccf122e3bdc66'
 
-  url "http://cdn.cocos2d-x.org/CocosCreator_v#{version}.dmg"
+  # download.cocos.com was verified as official when first introduced to the cask
+  url "http://download.cocos.com/CocosCreator/v#{number}/CocosCreator_v#{version}.dmg"
   name 'CocosCreator'
   homepage 'http://www.cocos2d-x.org/'
 
