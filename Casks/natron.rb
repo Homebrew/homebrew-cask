50a07f01686a06f558a42771c0f50beaff9d6a83
Jairo Caro-Accino Viciana <jairo@galax.be>
2018-04-29 21:10:25 +0200

    Update natron.rb

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -1,11 +1,11 @@
 cask 'natron' do
   version '2.3.10'
-  sha256 '8c7febdc66cc651777ad66a9a462bf170990bf3a452c315fd751206dca803972'
+  sha256 '3e70ea6078a43a07aa8c3bbd02e6dccbc80920e573e84efbd8279e248e576dac'
 
   url "https://downloads.natron.fr/Mac/releases/Natron-#{version}.dmg",
       referer: 'https://natron.fr/download/?os=Mac'
   appcast 'https://github.com/MrKepzie/Natron/releases.atom',
-          checkpoint: 'b891b1da77199c0f4e027c17053cf1dc506616c97bf88b851eaa154a4ff90db2'
+          checkpoint: '5c23b6da0499aad352b4f5e8f9435db3f2329d4c5b2b25725845e9759c060303'
   name 'Natron'
   homepage 'https://natron.fr/'
 
