37b1e473baedb608bb08e43751c57617344153c5
Vítor Galvão <info@vitorgalvao.com>
2018-04-29 17:16:42 +0100

    Update and rename tuneskit.rb to tuneskit-m4v-converter.rb

--- Casks/tuneskit-m4v-converter.rb
+++ Casks/tuneskit-m4v-converter.rb
@@ -1,9 +1,9 @@
-cask 'tuneskit' do
+cask 'tuneskit-m4v-converter' do
   version :latest
   sha256 :no_check
 
   url 'https://www.tuneskit.com/TunesKitforMac.dmg'
-  name 'TunesKit for Mac'
+  name 'TunesKit M4V Converter'
   homepage 'https://www.tuneskit.com/'
 
   app 'TunesKit M4V Converter.app'
