6da1236aee163a7d20ef11a4f710071e3def5c74
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-29 15:39:43 +0100

    Update gretl to 2018a

--- Casks/gretl.rb
+++ Casks/gretl.rb
@@ -1,10 +1,10 @@
 cask 'gretl' do
-  version '2017d'
-  sha256 'ee2c881924bd339a55ae912a7df6abc76abbbd8d0bf2dc50c0803c8113662329'
+  version '2018a'
+  sha256 '43336535c6d9f940b146a1197ffdc72f894755183ab2364d7950e562818c3e08'
 
   url "https://downloads.sourceforge.net/gretl/gretl-#{version}-quartz.pkg"
   appcast 'https://sourceforge.net/projects/gretl/rss?path=/gretl',
-          checkpoint: 'b26795fac004d7e7049cb8c650fa8a3ff85e3d5b7ff6a8190bb033493c995ec4'
+          checkpoint: '8b46ad14178f4fda65d449290905b83b5fa3b44fccf80650fe7e475ec6578852'
   name 'gretl'
   homepage 'http://gretl.sourceforge.net/'
 
