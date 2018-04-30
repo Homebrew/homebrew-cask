fbcdcfcd25330b0567658fc04fec6ce6bb0c3e15
Gavin Chappell <g-a-c@users.noreply.github.com>
2018-04-30 16:18:23 +0100

    Update quassel and quassel-client to 0.12.5 (#46538)

--- Casks/quassel.rb
+++ Casks/quassel.rb
@@ -1,12 +1,12 @@
 cask 'quassel' do
-  version '0.12.4'
-  sha256 'ac4e4e5f644f6fa9c1d7ed31f54060b3c676a6b327019d2e069d3172a362645d'
+  version '0.12.5'
+  sha256 'da76fae7ab612cdedf37e48b34fa68acf90385cdb20d5ec61f3c14bbac4a447e'
 
-  url "http://quassel-irc.org/pub/QuasselMono_MacOSX-x86_64_#{version}.dmg"
+  url "https://quassel-irc.org/pub/QuasselMono_MacOSX-x86_64_#{version}.dmg"
   appcast 'https://github.com/quassel/quassel/releases.atom',
-          checkpoint: '7a2bb5b73c8236569323678722040221cb28bd2e8a18d88a3c77bf89bb4b3b53'
+          checkpoint: '3490b6d26995028926931c9c45daf0bafdce06e2314af2640bd34a0ae317c745'
   name 'Quassel IRC'
-  homepage 'http://quassel-irc.org/'
+  homepage 'https://quassel-irc.org/'
 
   app 'Quassel.app'
 end
