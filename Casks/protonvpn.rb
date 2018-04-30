07365010c81cae70a2473b13789a7151e9caf047
commitay <commitay@users.noreply.github.com>
2018-05-01 07:51:34 +1000

    Update protonvpn.rb

--- Casks/protonvpn.rb
+++ Casks/protonvpn.rb
@@ -2,7 +2,7 @@ cask 'protonvpn' do
   version '1.0.0'
   sha256 '032d95003b2fb3c121a41dda736d4bc785ab6101eda65fb9e0b46c49487f6741'
 
-  url 'https://protonvpn.com/download/ProtonVPNv1.0.dmg'
+  url "https://protonvpn.com/download/ProtonVPNv#{version.major_minor}.dmg"
   appcast 'https://protonvpn.com/download/macos-update.xml',
           checkpoint: 'ea73947ab6726ad2116d95fe2c968a41d0b459b8fdaf8dc4446cfb30840c4857'
   name 'ProtonVPN'
