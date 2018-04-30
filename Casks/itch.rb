4ec161629312b8ce32fd26d5c3665c0fb591e29f
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-30 23:01:27 +0100

    Update itch to 23.6.2 (#46557)

--- Casks/itch.rb
+++ Casks/itch.rb
@@ -1,11 +1,11 @@
 cask 'itch' do
-  version '23.6.1'
-  sha256 '97b639ece01a74b6c1e0d3517623b79f5734aaf3c448dbb9cf2540a6bcb2ebdb'
+  version '23.6.2'
+  sha256 '6bab73e1eb791494ea560a4dd4b9b1045e33af6a3279bb481057ed045cc4a8f3'
 
   # github.com/itchio/itch was verified as official when first introduced to the cask
   url "https://github.com/itchio/itch/releases/download/v#{version}/itch-mac.dmg"
   appcast 'https://github.com/itchio/itch/releases.atom',
-          checkpoint: '9dcccac9a6abd8234de24bf75d3ab43a8ed7b49f910bf35ca09ad4e65a98bf27'
+          checkpoint: '765a71264737aa97a74969a0eae6260f6e2926814d33c969f1025754f2683b5a'
   name 'itch'
   homepage 'https://itch.io/app'
 
