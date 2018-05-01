463a50669fa45d5d5996052b8cd0e003a9b21c25
Anton Palgunov <Toxblh@users.noreply.github.com>
2018-05-01 10:39:52 +0100

    Update MTMR to v0.14 (#46574)

--- Casks/mtmr.rb
+++ Casks/mtmr.rb
@@ -1,10 +1,10 @@
 cask 'mtmr' do
-  version '0.13.1'
-  sha256 'e07f3c63e2b7fd1a254efe6fbccf8c7a3520e2d56622f7b30bbd2bbe4a17c9b1'
+  version '0.14'
+  sha256 '558a2a68f6c258120efd9ab34f3b9c70e4f869ed59d32c49b2bfd57b06537503'
 
   url "https://github.com/Toxblh/MTMR/releases/download/v#{version}/MTMR-#{version}.dmg"
   appcast 'https://github.com/Toxblh/MTMR/releases.atom',
-          checkpoint: '0c4e9767c884935ab3675c200ef121cdb5d0872e7794d41ba5256dd30fbeb3a2'
+          checkpoint: '56a4be29f5be421c9f5d9098b2a536faa78f3dd5c2785c0c983331f521408a10'
   name 'My TouchBar. My rules'
   homepage 'https://github.com/Toxblh/MTMR'
 
