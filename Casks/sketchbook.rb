9f2a8b735cd069eee8575e5a75e3d66f625fa2ce
Brian Morton <brianmortondesign@gmail.com>
2018-05-01 10:00:21 -0700

    Update sketchbook to 8.6.0

--- Casks/sketchbook.rb
+++ Casks/sketchbook.rb
@@ -1,6 +1,6 @@
 cask 'sketchbook' do
-  version '8.5.1'
-  sha256 '5362b635735071ba9d60e60fa88b07e45a6f27e9d416b9ed4081fa1ba375c0a0'
+  version '8.6.0'
+  sha256 '99a2a28aadbe2ee3e2dd222b6b8eef0b35d42f7354ca622743d8e7acca6d3d77'
 
   url "https://cdn.sketchbook.com/mac/SketchBook_v#{version}_mac.dmg"
   name 'Autodesk Sketchbook'
