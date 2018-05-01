c012dc5b27c81da5c4e605eddf0eea75e0da3928
Philipp Weißmann <mail@philipp-weissmann.de>
2018-05-01 12:17:19 +0200

    Update 'vnote' to 1.15, fix url

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -1,9 +1,9 @@
 cask 'vnote' do
-  version '1.14'
-  sha256 'f6a576fcb14990ded7c58fdba9d8f97a8e26c2b47602270af2efb4ba0b6f25d8'
+  version '1.15'
+  sha256 'eb64ee54426fb5525c077e4538f74aac20fc2108a39937aeee700835dfc95825'
 
   # github.com/tamlok/vnote was verified as official when first introduced to the cask
-  url "https://github.com/tamlok/vnote/releases/download/v#{version}/VNote_X64_#{version}.dmg"
+  url "https://github.com/tamlok/vnote/releases/download/v#{version}/VNote-#{version}-x64.dmg"
   appcast 'https://github.com/tamlok/vnote/releases.atom',
           checkpoint: '5b65722928f42af4f8c1fb06a3258a60e9e266d89993dcdc5e21bb566d26923d'
   name 'VNote'
