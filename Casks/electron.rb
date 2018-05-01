b6036d24a3a83ef6c9ac55854d13d086e625a85d
Gobinath Mallaiyan <i@gobinath.com>
2018-05-01 17:22:24 -0400

    Update electron to 2.0.0

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,11 +1,11 @@
 cask 'electron' do
-  version '1.8.6'
-  sha256 '5c5f59aa38d33198a6de2669cafc93f3ed9e1f9b28c38387cbade29dd80ae8d5'
+  version '2.0.0'
+  sha256 'a6bfdd5756af2b50ecf240d7fb5ef5b86c59bb7cd118d33bfe42ced48a90591c'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/electron/electron/releases.atom',
-          checkpoint: '4af38022353a43d3f7df690ba5b2236f7586fe07d535bc033370f0ad4f35fde5'
+          checkpoint: 'b363d4a451ff0693c2550bf0a8239ec7a5a691625f753657662173bcee5a63bc'
   name 'Electron'
   homepage 'https://electron.atom.io/'
 
