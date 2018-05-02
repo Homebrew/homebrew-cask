d67dfe04bd3b2e49761598fb22d6573293d6f9e6
Brian Morton <brianmortondesign@gmail.com>
2018-05-02 16:27:03 -0700

    Update todo-cloud to 10.4.0 (#46635)

--- Casks/todo-cloud.rb
+++ Casks/todo-cloud.rb
@@ -1,6 +1,6 @@
 cask 'todo-cloud' do
-  version '10.3.1'
-  sha256 '76eaed95d790bdaf62431b9284f3ce504801ba2de2d5a4978a1ce74d69b61704'
+  version '10.4.0'
+  sha256 '1ed815a4ab7e57833fcc4bb7c6e47113c1dcaaef7adb3a3256a7637a1a08b0d9'
 
   url "http://builds.appigo.com/todo-cloud/macos/Todo+Cloud-PROD-#{version}-mac.dmg"
   name 'Todo Cloud'
