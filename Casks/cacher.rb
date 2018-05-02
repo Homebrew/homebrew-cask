836ba398a10af436c5991dcd3b63e80a6919e6d6
Brian Morton <brianmortondesign@gmail.com>
2018-05-02 10:23:59 -0700

    Update cacher to 1.4.5

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,11 +1,11 @@
 cask 'cacher' do
-  version '1.4.4'
-  sha256 '17a64e9d4b8b5774c4c449c0dc73cfd08e7f6993385e86ba99b46a2762bb1cc9'
+  version '1.4.5'
+  sha256 '14d9708990da57505686ea6896ae380de06bf5bca6dfb886293d08bc4920a25c'
 
   # cacher-download.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
   appcast 'https://cacher-download.nyc3.digitaloceanspaces.com/latest-mac.json',
-          checkpoint: 'fb0df71ff68e4462a9ec4872b116f9cc2c3b58fe419bde3f61ba40b0cce0925f'
+          checkpoint: '7b775be005aca960a0120c84870f422022065bebd4f05e2900957e46f0bdb9ba'
   name 'Cacher'
   homepage 'https://www.cacher.io/'
 
