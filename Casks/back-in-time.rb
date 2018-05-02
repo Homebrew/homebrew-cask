8d6cda9c23a6695e5df4b53bdcab046d9317156a
Julian Mayer <julian@corecode.at>
2018-05-02 19:02:27 +0200

    Update back-in-time to latest

--- Casks/back-in-time.rb
+++ Casks/back-in-time.rb
@@ -2,9 +2,9 @@ cask 'back-in-time' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.tri-edre.com/pub/files/backintime4.dmg'
+  url 'http://www.tri-edre.com/pub/files/backintime5.dmg'
   name 'Back-In-Time'
   homepage 'http://www.tri-edre.com/english/backintime.html'
 
-  app 'Back-In-Time 4.app'
+  app 'Back-In-Time 5.app'
 end
