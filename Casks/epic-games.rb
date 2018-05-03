596e8c751c2342633dfba582f4d25df1e6a29217
Vítor Galvão <info@vitorgalvao.com>
2018-05-03 14:13:16 +0100

    Update epic-games.rb

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -2,7 +2,7 @@ cask 'epic-games' do
   version '7.9.2'
   sha256 'c1c410252f34d3e164f0c001bb679afbf66941ee70f31ab1cfc41cbd99461ae5'
 
-  url "https://download.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
+  url "https://download-dynamic.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
   name 'Epic Games Launcher'
   homepage 'https://www.epicgames.com/'
 
