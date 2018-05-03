3efc244db9d9e0de7707d7418f189c400b6a7954
scribblemaniac <scribblemaniac@users.noreply.github.com>
2018-05-03 13:15:21 -0600

    Update hype to 3.6.7

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -1,10 +1,10 @@
 cask 'hype' do
-  version '3.6.6'
-  sha256 'aaee53e8232769b986eb6545babff96b433a27bcbcaa301077dae24bc3a6e7b3'
+  version '3.6.7'
+  sha256 'bf7c50c3586db09eeb957900a156b4e0c14a53b5a92fdb6471a7a952649cc324'
 
   url 'https://tumult.com/hype/download/Hype.zip'
   appcast 'https://tumult.com/hype/appcast_hype2.xml',
-          checkpoint: '107554dd13a44b6e9cb34d58a868669c616e787e41e1cd218f22046cb35dac16'
+          checkpoint: 'bc9a8950da6a8434013469075aa5354bdd993bcbf4b78e58dc04640341c89be9'
   name 'Tumult Hype'
   homepage 'https://tumult.com/hype/'
 
