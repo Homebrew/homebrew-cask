31acfa25d83479a58bde4db9a9bdc01eefd00481
commitay <commitay@users.noreply.github.com>
2018-05-04 07:53:16 +1000

    Update vagrant to 2.1.0 (#46673)

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,11 +1,11 @@
 cask 'vagrant' do
-  version '2.0.4'
-  sha256 '105fb631d5ae29f9f01d9680f52ba31e63727fefd77f360df9e91a528c354d01'
+  version '2.1.0'
+  sha256 'fbbec9a335ab69b1a2b5535bca59b49034bb38c592c05f0f70fc120e25f71a4d'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
   appcast 'https://github.com/hashicorp/vagrant/releases.atom',
-          checkpoint: '623a632723cdac4d06686439afa55f4c30385522340dfcd709ebaca504b5103b'
+          checkpoint: '5070575f4f0570fc9f0027721b4919870386afd8ee9df368f2642f668e4dc103'
   name 'Vagrant'
   homepage 'https://www.vagrantup.com/'
 
