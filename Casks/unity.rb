9dd0e693b43537470e3bd3d90c13de04a5904a49
commitay <commitay@users.noreply.github.com>
2018-05-04 10:41:05 +1000

    Update unity to 2018.1.0f2,d4d99f31acba

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask 'unity' do
-  version '2017.4.1f1,9231f953d9d3'
-  sha256 '59b30dac3addf9f0a0234e8894f11f6f71c75ae18ad42abd0ff22c86160fb435'
+  version '2018.1.0f2,d4d99f31acba'
+  sha256 '78ff20252c560547278e6c411f06d5ca4bc9312fcd79327a216dee2290601d6b'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity.pkg"
   name 'Unity Editor'
