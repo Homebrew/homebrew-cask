24cd01165f4486ec7e22cdcd21e720a92e178a26
Jiaxin Shou <shoujiaxin@gmail.com>
2018-05-04 20:49:21 +0800

    Update duet to 1.7.1.4

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -1,6 +1,6 @@
 cask 'duet' do
-  version '1.7.1.2'
-  sha256 '1ed7d2e311ff1d4808b8364856da2689a3cebcea7abebce36dd9858634c7e8bf'
+  version '1.7.1.4'
+  sha256 '77279033e3776af86eb7d54d1177a75edefed72d4c829eea53dc87bf92dd0004'
 
   # duet.nyc3.digitaloceanspaces.com/Mac was verified as official when first introduced to the cask
   url "https://duet.nyc3.digitaloceanspaces.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip"
