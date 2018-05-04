79dc5a6201483ba9541dc34dee54152fb84a0555
Brian Morton <brianmortondesign@gmail.com>
2018-05-04 14:47:40 -0700

    Update retroarch-cg to 1.7.3

--- Casks/retroarch-cg.rb
+++ Casks/retroarch-cg.rb
@@ -1,6 +1,6 @@
 cask 'retroarch-cg' do
-  version '1.7.2'
-  sha256 'c4f61772a6c16187098d293ff0f9724392ae06f8f77f629d91f98432ecfdbbe3'
+  version '1.7.3'
+  sha256 'c47c19a3a3774de04bfeb9bba0b41f396fb2e0d14570062d1704a37d32cf527b'
 
   url "https://buildbot.libretro.com/stable/#{version}/apple/osx/x86_64/RetroArch_CG.dmg"
   name 'RetroArch CG'
