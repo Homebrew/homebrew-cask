4a687cc8c2a0b549374a18b347b19b000ba15f66
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-05 21:52:28 +0100

    Update espresso to 5.1.1

--- Casks/espresso.rb
+++ Casks/espresso.rb
@@ -2,9 +2,9 @@ cask 'espresso' do
   version '5.1.1'
   sha256 '728b5cb018479463bfb919533bfdcbd1788d1ec5b00ee312d3dc316d2e50527d'
 
-  url "https://presto.espressoapp.com/downloads/Espresso%20v#{version}.zip"
+  url "https://espressoapp.com/presto/downloads/Espresso%20v#{version}.zip"
   appcast "https://update.macrabbit.com/espresso/#{version}.xml",
-          checkpoint: 'c6be149ba0581d51a48418850d3378aa1523814e12f0eb066c552b0b0a6ec4bb'
+          checkpoint: 'a5b20ab9e901cdb75fa3bfbdf8036acaa555e2f774fc3b0e97bd4d384be5765b'
   name 'Espresso'
   homepage 'https://espressoapp.com/'
 
