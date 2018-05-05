1a366836572870c96a40ef9a6563d59783b02846
João Paulo Cercal <joao-paulo.cercal@flixbus.com>
2018-05-05 20:57:16 +0200

    Removed the https:// comment from the "Casks/ps4-remote-play.rb".

--- Casks/ps4-remote-play.rb
+++ Casks/ps4-remote-play.rb
@@ -2,7 +2,7 @@ cask 'ps4-remote-play' do
   version :latest
   sha256 :no_check
 
-  # https://remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg was verified as official when first introduced to the cask
+  # remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg was verified as official when first introduced to the cask
   url 'https://remoteplay.dl.playstation.net/remoteplay/module/mac/RemotePlayInstaller.pkg'
   name 'PS4 Remote Play'
   homepage 'https://www.playstation.com/'
