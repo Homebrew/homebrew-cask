3874a1e00da9cceaf3224315cbfdb31cfaa238bb
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-05 16:02:17 +0100

    Update fuse to 1.8.1.15610

--- Casks/fuse.rb
+++ Casks/fuse.rb
@@ -1,13 +1,14 @@
 cask 'fuse' do
-  version '1.1.0.13808'
-  sha256 '2ca9a5e60d2d504935a13ac6618b06df045aabc2d33ed50bec8ce5fd7768d598'
+  version '1.8.1.15610'
+  sha256 '448fd88fef4dde6b258db3304dcbf8416237fd35ee928d48c4a0bc9d40d0556a'
 
-  # fuse-dl.azureedge.net was verified as official when first introduced to the cask
-  url "https://fuse-dl.azureedge.net/releaseartifacts/fuse_osx_#{version.dots_to_underscores}.pkg"
+  url "https://www.fusetools.com/downloads/#{version}/osx"
   name 'Fuse Fusetools'
   homepage 'https://www.fusetools.com/'
 
-  pkg "fuse_osx_#{version.dots_to_underscores}.pkg"
+  depends_on macos: '>= :mavericks'
+
+  pkg "fuse_osx_#{version.dots_to_underscores}"
 
   uninstall pkgutil: 'com.fusetools.fuse'
 end
