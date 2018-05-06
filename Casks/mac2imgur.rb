be92bc5c2e699f967212df8de969341bcfc12e03
Vítor Galvão <info@vitorgalvao.com>
2018-05-06 01:05:41 +0100

    Update mac2imgur.rb

--- Casks/mac2imgur.rb
+++ Casks/mac2imgur.rb
@@ -3,8 +3,8 @@ cask 'mac2imgur' do
   sha256 '8faeb435fcb866267fa67d4b93d5fc9fb82bcfb2e959c7fcc6f59ab15fb05ccb'
 
   url "https://github.com/mileswd/mac2imgur/releases/download/#{version}/mac2imgur.zip"
-  appcast 'https://raw.githubusercontent.com/mileswd/mac2imgur/master/Resources/appcast.xml',
-          checkpoint: 'da366727457f59fc675cf7f486ec5f1dde7b3c5c80b0caa4f327af6b069daa3d'
+  appcast 'https://github.com/mileswd/mac2imgur/releases.atom',
+          checkpoint: '5078de9b5f76f2d5c9ad91d4b793fbaca67bd908253eb3576add883d674db837'
   name 'mac2imgur'
   homepage 'https://github.com/mileswd/mac2imgur'
 
