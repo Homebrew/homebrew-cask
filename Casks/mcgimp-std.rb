ebdb771bf2839b87509c8d86a21ee14e0e96dea0
Vítor Galvão <info@vitorgalvao.com>
2018-05-06 22:13:50 +0100

    Update mcgimp-std.rb

--- Casks/mcgimp-std.rb
+++ Casks/mcgimp-std.rb
@@ -2,9 +2,9 @@ cask 'mcgimp-std' do
   version '2.10.0'
   sha256 'bee94c104f07292781f26f34c4cd830b00bdf951eceb1f339d4c92c571594082'
 
-  url 'https://www.partha.com/downloads/McGimp-2.10-std.app.zip'
+  url "https://www.partha.com/downloads/McGimp-#{version}-std.app.zip"
   name 'McGimp'
   homepage 'https://www.partha.com/'
 
-  app 'McGimp-2.10-std.app'
+  app "McGimp-#{version}-std.app"
 end
