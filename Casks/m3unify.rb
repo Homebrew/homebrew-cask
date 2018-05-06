b3c263f3f6cb566eb33e247e9a560ee080f6b0f7
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-06 01:06:28 +0100

    Update m3unify to 1.9.3 (#46745)

--- Casks/m3unify.rb
+++ Casks/m3unify.rb
@@ -1,10 +1,10 @@
 cask 'm3unify' do
-  version '1.9.2'
-  sha256 '12881a7d4ec42f6cc43763513c5a68a9e1903f7e11cbd31aa272c42215499d2f'
+  version '1.9.3'
+  sha256 'dfab7567c18308d54cfe38caae2d97797650637cabef74fbca7bbc838008a350'
 
   url "https://dougscripts.com/itunes/scrx/m3unifyv#{version.no_dots}.zip"
   appcast 'https://dougscripts.com/itunes/itinfo/m3unify_appcast.xml',
-          checkpoint: 'c79e86fd0a547b87384537d8103b0a752e55b9b9294b2911531489b35afd1b5c'
+          checkpoint: '4196a93d710d3903e800f816f6a93378195403a35ba47d02900ed35110a792de'
   name 'M3Unify'
   homepage 'https://dougscripts.com/apps/m3unifyapp.php'
 
