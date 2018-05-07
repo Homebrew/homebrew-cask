77bbb72cfc0950e439d6ad8d160fee84804f616c
Brian Morton <brianmortondesign@gmail.com>
2018-05-07 11:49:05 -0700

    Update openframeworks to 0.10.0 (#46787)

--- Casks/openframeworks.rb
+++ Casks/openframeworks.rb
@@ -1,10 +1,10 @@
 cask 'openframeworks' do
-  version '0.9.8'
-  sha256 '483a298afdf52f0da4574d35fd67babce6a29f2ce7c0af917d790f6ffaf1302f'
+  version '0.10.0'
+  sha256 '23faa4c34decf7b1721df566e9b0e6717bffc6dcab3be4badde1ccd8b907ea12'
 
   url "http://openframeworks.cc/versions/v#{version}/of_v#{version}_osx_release.zip"
   appcast 'https://github.com/openframeworks/openFrameworks/releases.atom',
-          checkpoint: '991f9af155a29375905ee42cc8bb1716f3cce0a9d6880286af8ca93e6518af56'
+          checkpoint: 'b2722a481be3767330c6048528c783c6b958340028df8db3bfd6d3216c2e2800'
   name 'Openframeworks'
   homepage 'http://openframeworks.cc/'
 
