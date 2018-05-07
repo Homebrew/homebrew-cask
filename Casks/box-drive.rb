756016ab5a5a1956dc1064a93f5e8230d7a602f5
Wayne Graham <wgraham@clir.org>
2018-05-07 12:02:48 -0400

    Add cask for Box Drive

---
+++ Casks/box-drive.rb
@@ -0,0 +1,36 @@
+cask 'box-drive' do
+  version :latest
+  sha256 :no_check
+
+  # e3.boxcdn.net was verified as official when first introduced to the cask
+  url 'https://e3.boxcdn.net/box-installers/desktop/releases/mac/Box.pkg'
+  name 'Box Drive'
+  homepage 'https://www.box.com/drive'
+
+  app 'Box.app'
+  pkg 'Box.pkg'
+
+  uninstall quit:[
+    'com.box.Box-Local-Com-Server',
+    'com.box.desktop',
+    'com.box.desktop.findersyncext',
+    'com.box.desktop.helper',
+    'com.box.desktop.ui'
+  ],
+    script: {
+      executable: "/Library/Application Support/Box/uninstall_box_drive"
+    }
+
+  zap trash: [
+               '~/Library/Application Support/Box/Box',
+               '~/Library/Logs/Box/Box',
+             ],
+      rmdir: [
+        '~/Library/Application Support/Box/Box',
+        '~/Library/Containers/com.box.desktop.findersyncext'
+      ]
+
+  caveats <<~EOS
+    If you currently have Box Sync installed, uninstall Box Sync before installing Box Drive.
+  EOS
+end
