e9848f0588a64c537cfd9a7bb92657e78a07829b
Brian Morton <brianmortondesign@gmail.com>
2018-05-07 10:08:41 -0700

    Update el34-eddie to 3.3.3

--- Casks/el34-eddie.rb
+++ Casks/el34-eddie.rb
@@ -1,11 +1,11 @@
 cask 'el34-eddie' do
-  version '3.3.2'
-  sha256 '7326392b09e09e46178be65f750b892b7e78b150d1f2e4ed13060da0f9f2037b'
+  version '3.3.3'
+  sha256 '6e0b1aafabe3092c8803330c8e32cde35a4a9eb4f9aa75139dd323ad8bb3338d'
 
   # el34software.com was verified as official when first introduced to the cask
   url "http://el34software.com/latest/Eddie%20OSX%20#{version}.zip"
   appcast 'http://www.el34.com/EddieReleaseNotes.html',
-          checkpoint: 'fe11f93fe9396aba91bd6c8159a61e8223ab35080557ac3ceff8ef0b68dc79e5'
+          checkpoint: 'd5b9861054ed3949284c53b713c7865a3fb735be2cc318eccfca718b5800099a'
   name 'Eddie'
   homepage 'http://www.el34.com/'
 
