97a8586e25b43fd83f21924cdacfd49ff9fc96d2
CoreCode <core-code@users.noreply.github.com>
2018-05-08 00:00:03 +0200

    Update softraid to 5.6.7 (#46814)

--- Casks/softraid.rb
+++ Casks/softraid.rb
@@ -1,10 +1,10 @@
 cask 'softraid' do
-  version '5.6.5'
-  sha256 'd63197addd6d752dae34bdef49eb74935564f6e1e693d37a5b9af9344515db58'
+  version '5.6.7'
+  sha256 '92aae577e163487fc801304b46e0f99581ad63ba33af6897bc4181e631614022'
 
   url "https://softraid.com/updates/SoftRAID%20#{version}.dmg"
   appcast 'https://www.softraid.com/pages/support/download_latest_version.html',
-          checkpoint: 'fdcaef7786fadbb9afef2a2ecf63a7cf0977385eed6f603ccfa557d6974abd3c'
+          checkpoint: '41fb061b2e2628f686b8241ae60cfde1245bd6e9a68d53c84000abbc8428cacf'
   name 'SoftRAID'
   homepage 'https://www.softraid.com/'
 
