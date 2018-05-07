27f62877fb457e85cf4ecd30e72c622fa52d79b8
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-07 22:56:54 +0100

    Update modelio to 3.7.1 (#46800)

--- Casks/modelio.rb
+++ Casks/modelio.rb
@@ -1,11 +1,11 @@
 cask 'modelio' do
-  version '3.7.0'
-  sha256 '33b215ad7facf271d31b720cb1e69ee311d290e1997c60afc1edd35dc59ab4a3'
+  version '3.7.1'
+  sha256 'd55afecab1bc121c1c2e4ad90a187715db25d245df964cf61fc7cd3f27eb2f40'
 
   # sourceforge.net/modeliouml was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/modeliouml/modelio-open-source-#{version}-macosx.cocoa.x86_64.tar.gz"
   appcast 'https://sourceforge.net/projects/modeliouml/rss',
-          checkpoint: '49801a2d86031bb1d739039f4c4958c92d7661f0efc58c386897103b6b18c53a'
+          checkpoint: '909ad4be4bfb3fcf8a91da9ac5007531f06e2ac76e7374c072bf429df9847541'
   name 'Modelio'
   homepage 'https://www.modelio.org/'
 
