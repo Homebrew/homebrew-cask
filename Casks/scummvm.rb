00c5bc9507b82f8030feccbd39de4b594641efdf
Julian Mayer <julian@corecode.at>
2018-05-08 13:10:41 +0200

    Update scummvm to 2.0.0.1

--- Casks/scummvm.rb
+++ Casks/scummvm.rb
@@ -1,6 +1,6 @@
 cask 'scummvm' do
-  version '2.0.0'
-  sha256 'ac85e37d8bb923bbd1cdd998dc3e6d4a6a0f7dac042a863502c002e997c51414'
+  version '2.0.0.1'
+  sha256 'ed3e5a040702f78b0b395a801bea83ed2a70ea9fdb71b4bb985ec7084416bd26'
 
   url "https://scummvm.org/frs/scummvm/#{version.major_minor_patch}/scummvm-#{version}-macosx.dmg"
   name 'ScummVM'
