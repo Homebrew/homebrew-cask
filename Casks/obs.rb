62b76febaf6c2ec67c55242fb8251ebab62f4689
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-08 20:38:35 +0100

    Update obs to 21.1.1

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -2,10 +2,10 @@ cask 'obs' do
   version '21.1.1'
   sha256 '52a422a6cc42c62911c2a8d2d3b569c8ac725d5fcd13ab1f7bcaa23863ef9926'
 
-  # github.com/jp9000/obs-studio was verified as official when first introduced to the cask
-  url "https://github.com/jp9000/obs-studio/releases/download/#{version}/obs-mac-#{version}-installer.pkg"
-  appcast 'https://github.com/jp9000/obs-studio/releases.atom',
-          checkpoint: 'f31aeaf114bdad2ebf5e27ff816243dc919f4f794e9c408e4fd24b167433bb71'
+  # github.com/obsproject/obs-studio was verified as official when first introduced to the cask
+  url "https://github.com/obsproject/obs-studio/releases/download/#{version}/obs-mac-#{version}-installer.pkg"
+  appcast 'https://github.com/obsproject/obs-studio/releases.atom',
+          checkpoint: '4c487c3ce8a5cc21469607cb3382b0704d0cae40dd3af1a110a1ea4b064c7412'
   name 'OBS'
   homepage 'https://obsproject.com/'
 
