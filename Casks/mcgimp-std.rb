05dea50ead7cbf506246ca851584125cb8cb0d56
commitay <commitay@users.noreply.github.com>
2018-05-08 16:57:25 +1000

    Update mcgimp-std.rb

--- Casks/mcgimp-std.rb
+++ Casks/mcgimp-std.rb
@@ -1,5 +1,5 @@
 cask 'mcgimp-std' do
-  version '2.10.0'
+  version '2.10'
   sha256 'bee94c104f07292781f26f34c4cd830b00bdf951eceb1f339d4c92c571594082'
 
   url "https://www.partha.com/downloads/McGimp-#{version}-std.app.zip"
