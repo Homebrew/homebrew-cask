65a6f5d1033f83c49d4adcac19deafb0b1c093a9
womeimingzi11 <chenhan28@gmail.com>
2018-05-08 17:41:52 +0800

    Fix the download failed of ShooterSubX

--- Casks/shootersubx.rb
+++ Casks/shootersubx.rb
@@ -2,7 +2,7 @@ cask 'shootersubx' do
   version '0.6.2'
   sha256 '51fe5dcc69f2936f5923f688445fb02f574abcfe9812e6384e6029ff90fec06e'
 
-  url "https://github.com/songzhou21/ShooterSubX/releases/download/v#{version}/ShooterSubX.app.zip"
+  url "https://github.com/songzhou21/ShooterSubX/releases/download/#{version}/ShooterSubX.app.zip"
   appcast 'https://github.com/songzhou21/ShooterSubX/releases.atom',
           checkpoint: '243ad43ed90c55feda0707ba0caaaf76557646258efd45d51b6d12768c06e424'
   name 'ShooterSubX'
