0b70d2cc3590d009166c5f39805be344a7665314
Julian Mayer <julian@corecode.at>
2018-05-09 20:55:45 +0200

    Update prezi to 6.10.0

--- Casks/prezi.rb
+++ Casks/prezi.rb
@@ -1,9 +1,8 @@
 cask 'prezi' do
-  version '6.8.30'
-  sha256 '332137a21b5cbb23d996ba06169b5ddd8e9fba15454e5527b3c0db01adde74fc'
+  version '6.10.0'
+  sha256 '494c8d6cd63222b17de6ba0e90d6a5c29d13aa40479226b4b8ac0a5cda49a495'
 
-  # prezi-a.akamaihd.net was verified as official when first introduced to the cask
-  url "https://prezi-a.akamaihd.net/next-desktop/mac/Prezi_#{version}.dmg"
+  url "https://desktopassets.prezi.com/mac/pd6/releases/Prezi_Classic_#{version}.dmg"
   name 'Prezi'
   homepage 'https://prezi.com/'
 
