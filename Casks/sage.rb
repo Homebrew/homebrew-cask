f4739f77b80ed179a879f7e84d614044aefe07ca
CoreCode <core-code@users.noreply.github.com>
2018-05-09 23:43:16 +0200

    Update sage to 8.2,10.13.4 (#46910)
    * Update sage to 8.2,10.13.4
    
    * Update sage.rb

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -5,9 +5,12 @@ cask 'sage' do
   elsif MacOS.version <= :el_capitan
     version '7.6,10.11.6'
     sha256 'ba9ffba5dea394dc808c31a7b71af4d0db9759d9440b4dc2e35c921bd03e916f'
-  else
+  elsif MacOS.version <= :sierra
     version '8.1,10.12.6'
     sha256 'bd795369398873ccd26bae7e4ccc67370799d3038bebab911a626f496eba6d33'
+  else
+    version '8.2,10.13.4'
+    sha256 '3ff17cd1fe99295eb7a7dff2cd49be4f7703a3c605d87aa1601239f7ed14c4aa'
   end
 
   # mirrors.mit.edu/sage/osx/intel was verified as official when first introduced to the cask
