76c763018fbdce7e14aa5999687faa8762be9614
Brian Morton <brianmortondesign@gmail.com>
2018-05-09 09:02:41 -0700

    Update milanote to 1.0.32

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -1,6 +1,6 @@
 cask 'milanote' do
-  version '1.0.31'
-  sha256 'e2894937e4f0c8ea5e895b150b81783973eb8894f0b0b54b0cff5b75964e3bf1'
+  version '1.0.32'
+  sha256 '4ae8296c81ee729be07e0b0dcea351250b0391a9ad09f1ab69704628a6a4708f'
 
   # milanote-app-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg"
