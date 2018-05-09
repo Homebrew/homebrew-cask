40a4682bcaff7ecf15d2255727314179c82fb27f
Julian Mayer <julian@corecode.at>
2018-05-09 18:44:09 +0200

    Update aqua-data-studio to 19.0.0

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -1,6 +1,6 @@
 cask 'aqua-data-studio' do
-  version '18.5.0'
-  sha256 '744955217e65c21d64ed0b7f5974e6cb75fb66ef2e2bde59c8ad384b7169d140'
+  version '19.0.0'
+  sha256 '433eb4c0665d1638722628e33c6b80f52bc29e17ff4f11c745691df5b56158f9'
 
   url "http://downloads.aquafold.com/v#{version.major_minor}.0/osx/ads-osx-#{version}.tar.gz"
   name 'Aquafold Aqua Data Studio'
