1ff420746f8ab0b134dd02c8f905f24f4bf8e2a4
Moritz Reiter <moritz@anothernode.com>
2018-05-09 11:48:27 +0200

    Update synergy to 2.0.11

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,8 +1,8 @@
 cask 'synergy' do
-  version '2.0.10,b1746-ecfd5e95'
+  version '2.0.11,b1783-8c94f00f'
   sha256 'e46285a92e3c8a94eb60de7c222d1672c177e3d986111f37da17e61642329322'
 
-  url "https://binaries.symless.com/v#{version.before_comma}/synergy_v#{version.before_comma}-stable_#{version.after_comma}.dmg"
+  url "https://binaries.symless.com/v#{version.before_comma}/Synergy_v#{version.before_comma}-stable_#{version.after_comma}.dmg"
   name 'Synergy'
   homepage 'https://symless.com/synergy'
 
