b78d7bf06f70ba5caf9b72150aae3caf324b6a46
Moritz Reiter <moritz@anothernode.com>
2018-05-09 11:56:16 +0200

    Update SHA256 sum

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,6 +1,6 @@
 cask 'synergy' do
   version '2.0.11,b1783-8c94f00f'
-  sha256 'e46285a92e3c8a94eb60de7c222d1672c177e3d986111f37da17e61642329322'
+  sha256 'af13c4b26d00c4cd87173af7196b5d713a09e9c2818fa1538264f6f692a87355'
 
   url "https://binaries.symless.com/v#{version.before_comma}/Synergy_v#{version.before_comma}-stable_#{version.after_comma}.dmg"
   name 'Synergy'
