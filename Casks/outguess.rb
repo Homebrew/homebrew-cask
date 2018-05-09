dc3343e6ae378d42ef675a9cc72fb64e9ab9a403
Rbcafe <Webmaster@rbcafe.com>
2018-05-09 08:40:37 +0200

    update outguess.rb (#46877)
    Add user_agent: :fake to url

--- Casks/outguess.rb
+++ Casks/outguess.rb
@@ -2,7 +2,8 @@ cask 'outguess' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.rbcafe.com/download/outguess.zip'
+  url 'https://www.rbcafe.com/download/outguess.zip',
+      user_agent: :fake
   name 'Outguess'
   homepage 'https://www.rbcafe.com/software/outguess/'
 
