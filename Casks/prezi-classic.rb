8ece7932f618bd2b7599a2a8c1b283e415445255
Vítor Galvão <info@vitorgalvao.com>
2018-05-10 13:48:00 +0100

    Update and rename prezi.rb to prezi-classic.rb

--- Casks/prezi-classic.rb
+++ Casks/prezi-classic.rb
@@ -1,10 +1,10 @@
-cask 'prezi' do
+cask 'prezi-classic' do
   version '6.10.0'
   sha256 '494c8d6cd63222b17de6ba0e90d6a5c29d13aa40479226b4b8ac0a5cda49a495'
 
   url "https://desktopassets.prezi.com/mac/pd6/releases/Prezi_Classic_#{version}.dmg"
-  name 'Prezi'
+  name 'Prezi Classic'
   homepage 'https://prezi.com/'
 
-  app 'Prezi.app'
+  app 'Prezi Classic.app'
 end
