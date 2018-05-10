38e7e331a1e8569d0f17d929ca441d762e6bb355
Johannes Heck <yoyostile@gmail.com>
2018-05-10 14:17:58 +0200

    Add Zwift latest Cask

---
+++ Casks/zwift.rb
@@ -0,0 +1,17 @@
+cask 'zwift' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://cdn.zwift.com/app/ZwiftOSX.dmg'
+  name 'Zwift'
+  homepage 'https://www.zwift.com/'
+
+  pkg 'ZwiftInstaller.pkg'
+
+  uninstall \
+    quit:    'com.zwift.ZwiftLauncher',
+    pkgutil: 'com.zwift.ZwiftLauncher',
+    trash:   ['/Application/Zwift.app']
+
+  zap trash: ['~/Library/Application Support/Zwift/']
+end
