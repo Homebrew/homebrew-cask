f9e0ada42dbd5e8a0b70bc925d67fe9106c38414
Brian Morton <brianmortondesign@gmail.com>
2018-05-10 09:17:33 -0700

    Update flinto to 25.6

--- Casks/flinto.rb
+++ Casks/flinto.rb
@@ -1,6 +1,6 @@
 cask 'flinto' do
-  version '25.5'
-  sha256 '2181a70141b54614d13eb2277fd626db533dccdfa760a168586ca82e7d34f8b4'
+  version '25.6'
+  sha256 '2180df20938a06ac557686d9a103656067c8820a7e678a1a1e2d0bd1d9377929'
 
   url "https://www.flinto.com/assets/Flinto-#{version}.dmg"
   name 'Flinto'
