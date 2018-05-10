4edd2a4769c11ece83c3bd9434f012da80845bbe
Edwin Hermans <edwin@madtech.cx>
2018-05-10 11:07:04 -0400

    File dmg file name
    Not sure when this started but the release dmg file contains "Mac" in the name. The file without "Mac" in the name is not available.

--- Casks/cityofzion-neon.rb
+++ Casks/cityofzion-neon.rb
@@ -2,7 +2,7 @@ cask 'cityofzion-neon' do
   version '0.2.4'
   sha256 '7b39fab2f6182dfe8b399b228f7d84b0e231629bd893eb45a029056539d7904b'
 
-  url "https://github.com/CityOfZion/neon-wallet/releases/download/#{version}/Neon-#{version}.dmg"
+  url "https://github.com/CityOfZion/neon-wallet/releases/download/#{version}/Neon-#{version}.Mac.dmg"
   appcast 'https://github.com/CityOfZion/neon-wallet/releases.atom',
           checkpoint: 'a31dcb81982612298e16fe223e3f71711bff4c493b001241aec3bc9d1be73073'
   name 'Neon Wallet'
