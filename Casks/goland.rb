d4433eac42ec879b58cde5104c4ec7e1eb543ed1
秦新 <qinxin_0720@live.com>
2018-05-11 09:17:54 +0800

    Update GoLand to 2018.1.3,181.4892.55 (#46952)

--- Casks/goland.rb
+++ Casks/goland.rb
@@ -1,10 +1,10 @@
 cask 'goland' do
-  version '2018.1.2,181.4668.90'
-  sha256 '38025744da25c1db48235bb910524298885182343a35d85c4eb1752a84ef3084'
+  version '2018.1.3,181.4892.55'
+  sha256 'da00d0c2be3b49b46c119f513f9421bf879f6902747ca60305339e5bc543c94a'
 
   url "https://download.jetbrains.com/go/goland-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=GO&latest=true&type=release',
-          checkpoint: '747c6ae85e2c2785f7a15fa9d61e4994601488af8ae5d456a81dfca77ed2aebb'
+          checkpoint: '3ba58c81875b3ab5cb1c9f9e65b99539475225d7e0c70ce600e5598f6e89ce1b'
   name 'Goland'
   homepage 'https://www.jetbrains.com/go/'
 
