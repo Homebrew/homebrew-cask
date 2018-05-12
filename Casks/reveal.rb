befd6b7926d6e914e3b6919d4653fbb5b310a5ed
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 15:20:36 +0100

    Update reveal to 14

--- Casks/reveal.rb
+++ Casks/reveal.rb
@@ -1,10 +1,10 @@
 cask 'reveal' do
-  version '13'
-  sha256 '1cdae698fe0dd3d7d0f457fb572fc063dc006efa0c9a9b59e6beabebb65099ff'
+  version '14'
+  sha256 'a807bb42aee9125d83950ef8fc1b3f92019c33fb7cdfe07548f81bcedef4c891'
 
   url "https://download.revealapp.com/Reveal.app-#{version}.zip"
   appcast 'https://revealapp.com/download/',
-          checkpoint: 'ede640ade1401428ea099c57c18a40a08daafecedd9abe833a87139053d0e30d'
+          checkpoint: '0893944dbee59abcb6a8dfd57c4364642c517c62a7b66eaf3faeebb35cde8a17'
   name 'Reveal'
   homepage 'https://revealapp.com/'
 
