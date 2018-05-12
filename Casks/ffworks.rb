20342d00b6272e5af2408227c7fb856b1a280008
Julian Mayer <julian@corecode.at>
2018-05-12 14:03:50 +0200

    Add ffworks.app 1.0.10

---
+++ Casks/ffworks.rb
@@ -0,0 +1,10 @@
+cask 'ffworks' do
+  version '1.0.10'
+  sha256 'b4897d9ceba0dbba3f642dbfd1c84955fac646733d666c5bea9fd59443e3d428'
+
+  url 'http://www.ffworks.net/ffWorks.dmg'
+  name 'ff·Works'
+  homepage 'http://www.ffworks.net/'
+
+  app 'ff·Works.app'
+end
