0d397f1aa3420317b6dac956619f8739832d5966
nrlquaker <ustynov.vy@gmail.com>
2018-05-12 14:01:00 +0300

    Update cookie to 5.8.4 (#46992)

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,10 +1,10 @@
 cask 'cookie' do
-  version '5.8.3'
-  sha256 'ed806ec129f8b97dc68dd182b6293af5ce4d3fa8986a957036f6e0a73045e5f6'
+  version '5.8.4'
+  sha256 '0fc446e8d0866d02658e532100724c4a8e4e9fbbeef5bed4364993ab2825e81e'
 
   url "https://sweetpproductions.com/products/cookie#{version.major}/Cookie#{version.major}.dmg"
   appcast "https://sweetpproductions.com/products/cookie#{version.major}/appcast.xml",
-          checkpoint: '557148239d36f8d3c2ec79b6fa8755c9a242a1a590c8381c16c30b2957966fd9'
+          checkpoint: 'b1c3a07829bd50761cd161799db81225ad78c81ff9e195a100fe7eac6c77d822'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
 
