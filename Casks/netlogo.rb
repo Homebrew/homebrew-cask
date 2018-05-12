23356c1bbf1287ff5c2ef4429e2fe1a511c88390
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 21:17:43 +0100

    Update netlogo to 6.0.3

--- Casks/netlogo.rb
+++ Casks/netlogo.rb
@@ -1,10 +1,10 @@
 cask 'netlogo' do
   version '6.0.3'
-  sha256 '6d3c9da4d1bffc79689666d6319fa9ff3d2cf7e779027a1c457b3395cc12582f'
+  sha256 '5cccdfaff8af4f5556cf211f0bfe6e8c08ae7f17456932c8903125106acdf351'
 
   url "https://ccl.northwestern.edu/netlogo/#{version}/NetLogo-#{version}.dmg"
   appcast 'https://ccl.northwestern.edu/netlogo/oldversions.shtml',
-          checkpoint: '1351f25e5c59bbf9f6a08cda8371fa2b7aec4f0d75c3736020cb408656833473'
+          checkpoint: 'b0ff96ad08709796d91ff19f1525f9059b3fa36a9e38e8da8c1293d980a8b5ef'
   name 'NetLogo'
   homepage 'https://ccl.northwestern.edu/netlogo/'
 
