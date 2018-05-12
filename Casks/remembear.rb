4714b32411286a42c1f243bdc5a35a932980f396
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-13 00:04:55 +0100

    Update remembear to 1.0.1 (#47090)

--- Casks/remembear.rb
+++ Casks/remembear.rb
@@ -1,11 +1,11 @@
 cask 'remembear' do
-  version '1.0.0'
-  sha256 '57e1f69d588e13dff49b74cf0a51141bffd529ebe6197f0f3f79887546501a61'
+  version '1.0.1'
+  sha256 '50085d0f1c03dc760863991360ef70b591f858d50d744aa7463624073ea80694'
 
   # s3.amazonaws.com/remembear was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/remembear/app/release/downloads/macOS/RememBear-#{version}.zip"
   appcast 'https://s3.amazonaws.com/remembear/app/release/downloads/macOS/appcast.xml',
-          checkpoint: 'c5f3ed3091577767185578f9bd5b6a9b34f54bc2cc129c6148136563de347bad'
+          checkpoint: '051a54637054a24e1dc5475f88d224dd08b12bec90a45d84462f8235b6bd3842'
   name 'RememBear'
   homepage 'https://www.remembear.com/'
 
