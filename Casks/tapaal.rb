3f8893faa2f69268609208b1cbfa45ab4994f67d
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 15:22:29 +0100

    Update tapaal to 3.4.1 (#46998)

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -1,10 +1,10 @@
 cask 'tapaal' do
-  version '3.4.0'
-  sha256 'e34ade264118bbce01b1d4835eff71161f7347f2be2baa88f41557bd7b45b766'
+  version '3.4.1'
+  sha256 'e2678ca750376fb0f52adcea6caae10fd3e5df3e7c7e2d2ac5908ddfca6dd222'
 
   url "http://www.tapaal.net/fileadmin/download/tapaal-#{version.major_minor}/tapaal-#{version}-mac64.dmg"
   appcast 'http://www.tapaal.net/',
-          checkpoint: '0fcb1486bf83481d63dfeb827896adea9c64b5202db6c2e1d73d80ecb928ba12'
+          checkpoint: '4aad60322ed336d37e70755dec2cfb9811f3a0e7d16bb68c658da625c99fe2a2'
   name 'TAPAAL'
   homepage 'http://www.tapaal.net/'
 
