6445e5348dab1735c959d9b39ce89c8bab044dd5
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 21:28:55 +0100

    Update soulver to 2.6.7-5936 (#47083)

--- Casks/soulver.rb
+++ Casks/soulver.rb
@@ -1,12 +1,14 @@
 cask 'soulver' do
-  version '2.6.6-5881'
-  sha256 'a95219084fba53c99a0aec44e283d6acfad157399b004eae7cd1597b12f935ae'
+  version '2.6.7-5936'
+  sha256 '81df634e82418904b539bdc30d28e30486f3e5d1132d296c4cedc4a025f8c562'
 
   url "http://www.acqualia.com/files/sparkle/soulver_#{version}.zip"
   appcast "http://www.acqualia.com/soulver/appcast/soulver#{version.major}.xml",
-          checkpoint: 'a4e78d3e73ac5772ff213c2b2bfe700294afce50b1b745e79d7e448959d7669b'
+          checkpoint: 'a7872dca261ed69c5f485c3a0dc752f4619c9a4d41e1076a77810fbf12e01a23'
   name 'Soulver'
   homepage 'http://www.acqualia.com/soulver/'
+
+  depends_on macos: '>= :yosemite'
 
   app 'Soulver.app'
 
