3d1f6bbd1ae68f7bd957c349b44d885865cd37cf
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 21:28:33 +0100

    Update gifox to 010600.00 (#47078)

--- Casks/gifox.rb
+++ Casks/gifox.rb
@@ -1,11 +1,11 @@
 cask 'gifox' do
-  version '010501.01'
-  sha256 'ed25c3a2678ebe409535356f9ebf2e1583dfc75a928fc9787ef96f09f86720b6'
+  version '010600.00'
+  sha256 '3fda02e5dd7aac3ad128ffbfcd27640175cae83dd6d9c17bc6e93533026c97ff'
 
   # s3.eu-central-1.amazonaws.com/dstlalgzor/gifox was verified as official when first introduced to the cask
   url "https://s3.eu-central-1.amazonaws.com/dstlalgzor/gifox/#{version}.dmg"
   appcast 'https://s3.eu-central-1.amazonaws.com/dstlalgzor/gifox/appcast.xml',
-          checkpoint: 'b91b7581bb57a40a1cde0ca06b323fe9f4d951d4144c05eb6dcaad9bdbadee00'
+          checkpoint: 'c74d6a5d6b45d32ddf878eddd040bfe8bf6c84c7944cb6f116afc1ac177afa8b'
   name 'gifox'
   homepage 'https://gifox.io/'
 
