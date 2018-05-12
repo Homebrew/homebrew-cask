4b890fd9cdb410a1a118f620ea6e645b96c839f3
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 20:09:43 +0100

    Update qownnotes to 18.05.2,b3579-160121

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,11 +1,11 @@
 cask 'qownnotes' do
-  version '18.04.2,b3549-154607'
-  sha256 '8c4c57daa0282d871ea415a3b1b74a4913285df16adb5215e3526fac10ffeccc'
+  version '18.05.2,b3579-160121'
+  sha256 '0f7026ac81d9f835ed9799e8e44c69cdcd85d11d97cfdae7ffec847e7cbe2d9b'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
   appcast 'https://github.com/pbek/QOwnNotes/releases.atom',
-          checkpoint: '50a932d79bbe435e0f7a4460684b5980081452de15666db4a41622328ac52834'
+          checkpoint: '8e3f080d4761475bf9f1a577caf04c37fb03e6780a450be920cdc4f0875c9ac9'
   name 'QOwnNotes'
   homepage 'https://www.qownnotes.org/'
 
