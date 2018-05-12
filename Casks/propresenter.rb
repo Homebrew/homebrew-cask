5b2e87d8dad7093e1c05379c59fb2dbd07d31058
nicholas.ngai <ngai.nicholas@gmail.com>
2018-05-12 12:42:14 -0700

    Add zap stanza to ProPresenter 6.3.4 cask

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -9,4 +9,20 @@ cask 'propresenter' do
   homepage 'https://www.renewedvision.com/propresenter.php'
 
   app "ProPresenter #{version.major}.app"
+
+  zap trash: [
+               '~/Library/Application Support/RenewedVision/ProPresenter6',
+               '~/Library/Caches/KSCrashReports/ProPresenter 6',
+               '~/Library/Caches/Sessions/ProPresenter 6',
+               '~/Library/Caches/com.renewedvision.ProPresenter6',
+               '~/Library/Preferences/com.renewedvision.ProPresenter6.plist',
+               '/Library/Application Support/RenewedVision',
+               '/Library/Caches/com.renewedvision.ProPresenter6',
+               '/Users/Shared/Renewed Vision Media',
+             ],
+      rmdir: [
+               '~/Library/Application Support/RenewedVision',
+               '~/Library/Caches/KSCrashReports',
+               '~/Library/Caches/Sessions',
+             ]
 end
