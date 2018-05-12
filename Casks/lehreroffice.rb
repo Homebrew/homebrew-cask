5241b431865ec01aeabb69b9b4be5bda34696a4d
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 18:28:34 +0100

    Update lehreroffice to 2018.7.0

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,10 +1,10 @@
 cask 'lehreroffice' do
-  version '2018.6.0'
-  sha256 '18bfbb32f0a719857aa53d43e936edc96fde4835824b67fd416fe6a9846d10d3'
+  version '2018.7.0'
+  sha256 'd75b9fa3f40edc46d318863ea39ccff03dd0144cf6d51314faa4c92185a8f70e'
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_osx.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop',
-          checkpoint: '46f4af140891dbba91890b2a69bedd15adb7dd6e4b08bf7617924434796f9cac'
+          checkpoint: '257e5606f1d1c59e1122a05a25f26360210c5b690fd823da6ea039b16871962f'
   name 'LehrerOffice'
   homepage 'https://www.lehreroffice.ch/'
 
