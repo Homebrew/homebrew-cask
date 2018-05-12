8541b13ef053b9aba62dfd81e1c394659c4838ec
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 21:14:17 +0100

    Update tuxguitar to 1.5.1

--- Casks/tuxguitar.rb
+++ Casks/tuxguitar.rb
@@ -1,10 +1,10 @@
 cask 'tuxguitar' do
-  version '1.5'
-  sha256 'ba9101f87c89a36bc4983b61e7f372b04253867884605413576b13bd8cfb4768'
+  version '1.5.1'
+  sha256 '30ed6184f646bbd366d91abfae8fce568250617ddc560bdf819da32dd9f05547'
 
   url "https://downloads.sourceforge.net/tuxguitar/tuxguitar-#{version}-macosx-cocoa-64.app.tar.gz"
   appcast 'https://sourceforge.net/projects/tuxguitar/rss?path=/TuxGuitar',
-          checkpoint: '19da02f9bdb4d121b9110a32a4403f84ae19c5492dba34061da51267f8709417'
+          checkpoint: '35b4ada15a96d3f7b7b6d3db6b40ede0c621425df63119f7753aac64092fdc28'
   name 'TuxGuitar'
   homepage 'https://sourceforge.net/projects/tuxguitar/'
 
