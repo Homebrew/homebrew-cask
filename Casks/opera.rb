80c84ff7b70b47dcf0b3ff4941fb37a6bd1d450e
CoreCode <core-code@users.noreply.github.com>
2018-05-12 21:23:21 +0200

    Update opera to 53.0.2907.37 (#47050)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '52.0.2871.99'
-  sha256 'bf3541ff63ee001eee31d8edbaaed6bdf711182eda62b9ac31bd42047b7b2585'
+  version '53.0.2907.37'
+  sha256 'eca1b65401d11d77990f09ccec0cd4be41d7d0ecc140ef4b94d77858455f7858'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
