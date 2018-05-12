73ee4776801d263b42e41f6c1bd36cee0cd89140
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 19:29:39 +0100

    Update amm to 0.4.5

--- Casks/amm.rb
+++ Casks/amm.rb
@@ -1,10 +1,10 @@
 cask 'amm' do
-  version '0.4.4'
-  sha256 '7d01e568658d3f4186ed501e55734563768f3a02063c33281c716ff5988db98d'
+  version '0.4.5'
+  sha256 '1e9363fecf5fd21b2bb82a38f0a72e50468f0336f3f8985df8a36e3029ca6121'
 
   url "https://github.com/15cm/AMM/releases/download/v#{version}/AMM_v#{version}.dmg"
   appcast 'https://github.com/15cm/AMM/releases.atom',
-          checkpoint: 'b961b8f53805616fdcd465fabe2d47df7262751ddd7edb06491d3a21ccc4e020'
+          checkpoint: 'f9f7c7fcacee4c29ea6b49ff2b2765045d78b3ac9d0f4420552bacec5b0cdf61'
   name 'AMM'
   homepage 'https://github.com/15cm/AMM'
 
