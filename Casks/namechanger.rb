d4a5a7592b0d9c39cf94017703a73385c1526b31
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-12 21:28:12 +0100

    Update namechanger to 3.3.2-6 (#47075)

--- Casks/namechanger.rb
+++ Casks/namechanger.rb
@@ -1,12 +1,14 @@
 cask 'namechanger' do
-  version '3.3.1'
-  sha256 'd00f9d9eaaa9d228f686dc313e9c5f8978d2d35fc9e598d759e8a2c9ca993198'
+  version '3.3.2-6'
+  sha256 'cd44006b4f223a5adb862d9143349261df5bb41dd5bff2ad5998f219dfa57b06'
 
   url "https://www.mrrsoftware.com/Downloads/NameChanger/Updates/NameChanger-#{version.dots_to_underscores}.zip"
   appcast 'https://mrrsoftware.com/Downloads/NameChanger/Updates/NameChangerSoftwareUpdates.xml',
-          checkpoint: '3caded5e8f5c07e6ec260322176151019a091a429d46af9ea3230ab00b6d13cd'
+          checkpoint: '73031631d37b9173d54dc75900f9791429ef7c4ccf83a83ce278467d757779a3'
   name 'NameChanger'
   homepage 'https://mrrsoftware.com/namechanger/'
+
+  depends_on macos: '>= :lion'
 
   app 'NameChanger.app'
 end
