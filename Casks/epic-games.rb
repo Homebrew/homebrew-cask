6a8b661ecc71e9e84b20f1350d724347887d7db8
commitay <commitay@users.noreply.github.com>
2018-05-13 14:27:40 +1000

    Update epic-games.rb

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,4 +1,7 @@
 cask 'epic-games' do
+  # Auto-updates to a newer version than available on the homepage.
+  # Latest version is posted to the forum:
+  # https://forums.unrealengine.com/unreal-engine/announcements-and-releases/1928-latest-launcher-release-notes
   version '7.9.4'
   sha256 '1f5bc125a5173ab6c4c8e8a05f3e27d8fdb522360834e7a8c947788a1e998eed'
 
