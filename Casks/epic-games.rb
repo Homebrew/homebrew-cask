c41a52be0e8ae7cc0028143e630ed0a571872a7d
greenrock <dd331@163.com>
2018-05-13 18:25:22 +0800

    Update epic-games to 7.9.4 (#46893)
    * Update epic-games to 7.9.4
    
    * Update epic-games.rb

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,6 +1,9 @@
 cask 'epic-games' do
-  version '7.9.3'
-  sha256 '0798fd7cba8cc7d63df828917605471596870bf8bad0df66ea49bbce0d2b369e'
+  # Auto-updates to a newer version than available on the homepage.
+  # Latest version is posted to the forum:
+  # https://forums.unrealengine.com/unreal-engine/announcements-and-releases/1928-latest-launcher-release-notes
+  version '7.9.4'
+  sha256 '1f5bc125a5173ab6c4c8e8a05f3e27d8fdb522360834e7a8c947788a1e998eed'
 
   url "https://download-dynamic.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
   name 'Epic Games Launcher'
