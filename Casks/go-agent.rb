707147025f7233c427c3514eb24ce972c9a8ecab
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-13 20:38:28 +0100

    Update go-agent to 18.5.0-6679

--- Casks/go-agent.rb
+++ Casks/go-agent.rb
@@ -1,11 +1,11 @@
 cask 'go-agent' do
-  version '18.3.0-6540'
-  sha256 '52530a3f759c3f1a3cedb5eebc55bf8eeae509c9a97c070205f246910c9c6650'
+  version '18.5.0-6679'
+  sha256 'ebabafdbfc3396d36d1c8c8f26650a946ee5f175f00b89c856a5e127a2db2bcd'
 
   # download.gocd.io/binaries was verified as official when first introduced to the cask
   url "https://download.gocd.io/binaries/#{version}/osx/go-agent-#{version}-osx.zip"
   appcast 'https://github.com/gocd/gocd/releases.atom',
-          checkpoint: 'd00e4e6848947d154dde8fd401343a233eab949f9021ba9ba509a7903fd823bb'
+          checkpoint: 'c53e6d95ddaf9a7d3a51b82011f8348a7f9e81ff702b088a885f5f58291a8113'
   name 'Go Agent'
   homepage 'https://www.gocd.org/'
 
