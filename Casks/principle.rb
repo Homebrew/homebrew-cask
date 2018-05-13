248b0f29af93791f30c4297d1abaa5a74f2705fc
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-13 17:01:15 +0100

    Update principle to 3.9

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,12 +1,14 @@
 cask 'principle' do
-  version '3.7'
-  sha256 '2f2a7c56b3c461bf22e4595596e405c74cf2ece48b529b8e1693c6d6e1c05519'
+  version '3.9'
+  sha256 'b765259686fa41c46ce38544be91ff64a47b8c244cbd32740b37c67ec9702c02'
 
   url "http://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
   appcast 'http://principleformac.com/update2.xml',
-          checkpoint: 'f8111d5e07f56a4da6c3bb91ae93991777559aab03eba3bb0279faf56b6bf32d'
+          checkpoint: 'a93a838b33e9f3175f893dc6dcff84066d1e354860aa1c537e63cf0117a8db51'
   name 'Principle'
   homepage 'http://principleformac.com/'
+
+  depends_on macos: '>= :el_capitan'
 
   app 'Principle.app'
 end
