cf2a3d5ea349f27fed0dd6d4221e03b62160f7d6
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-13 21:04:05 +0100

    Update receipts to 1.7.4-183

--- Casks/receipts.rb
+++ Casks/receipts.rb
@@ -1,10 +1,10 @@
 cask 'receipts' do
-  version '1.7.0'
-  sha256 'cf172851eeeb2c6e4a9c6f22583e08e857c3a5c5bcdea5350a4fde9b8492b76e'
+  version '1.7.4-183'
+  sha256 '016430d49a5b1df2b416a913020178fba3ba2844f63d841954e9e2f4bd413bb2'
 
-  url "https://www.receipts-app.com/download/Receipts-#{version}.zip"
+  url "https://www.receipts-app.com/update/download/Receipts-#{version}.zip"
   appcast 'https://www.receipts-app.com/updater.php',
-          checkpoint: 'c1eab9f7572b0e419c0636530f9bbfa3826b34c99843c85b942291e3ec542d8c'
+          checkpoint: 'c6354604204e765de37ac78b65f79bd90b26038f255f075f92849e3a4250d992'
   name 'Receipts'
   homepage 'https://www.receipts-app.com/'
 
