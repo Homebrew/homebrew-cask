a5acd9ce8dddd8822cb4a5cbcc0756cf83eefc26
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-13 10:50:34 +0100

    Update photoscan to 1.4.2

--- Casks/photoscan.rb
+++ Casks/photoscan.rb
@@ -1,10 +1,10 @@
 cask 'photoscan' do
-  version '1.4.1'
-  sha256 'ccdeb02f6c40b77d0b1bd3287bdf8fce520e251fc8cc6cdb947cff0ea9751acf'
+  version '1.4.2'
+  sha256 '081e8b298690c44752901b7c32e98a045e3e40273dfbb195868d7a5172846053'
 
   url "http://download.agisoft.com/photoscan_#{version.dots_to_underscores}.dmg"
   appcast 'http://www.agisoft.com/downloads/installer/',
-          checkpoint: '10eaa297b07f68cce32b1e6ff2344f10b004c92875a4e6bc450a1f60f532c0a6'
+          checkpoint: 'ede2516641a9aecb49f24db6afe7b16fcd1172ccc543648cbc2b6353b4b436e0'
   name 'Agisoft Photoscan'
   homepage 'http://www.agisoft.com/'
 
