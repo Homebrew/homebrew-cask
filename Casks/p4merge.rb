4dbd721598e2ad420de610a9d69564a3c4ddb9fa
Christian Kohlstedde <christian@kohlsted.de>
2018-05-14 11:41:22 +0200

    Update p4merge to version 2018.2-1661700 (#47201)

--- Casks/p4merge.rb
+++ Casks/p4merge.rb
@@ -1,6 +1,6 @@
 cask 'p4merge' do
-  version '2018.2-1652877'
-  sha256 '78d5beaa245ecf023939e45bccc26a6f215908e50fbb4e7155ab2581f5b01dbb'
+  version '2018.2-1661700'
+  sha256 'f45c0bf6e7f1f773b00225a4dce11a4b25d01a5c6a9aa793ba194a37c9921cb8'
 
   url "http://filehost.perforce.com/perforce/r#{version.sub(%r{\A20(\d\d\.\d+).*}, '\1')}/bin.macosx1013x86_64/P4V.dmg"
   name 'Perforce P4Merge'
