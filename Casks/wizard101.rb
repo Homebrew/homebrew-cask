c4fc8d164dbea7f06b93326b46db84efdb264720
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-14 18:25:11 +0100

    Update wizard101 to 1.13

--- Casks/wizard101.rb
+++ Casks/wizard101.rb
@@ -1,10 +1,10 @@
 cask 'wizard101' do
   version '1.13'
-  sha256 'e994b59a6b53f2255168316b832962415b899260244ae36dfd4ba0aba201fcb9'
+  sha256 'd260d5930c9f32367c87cb68b634b0210e070b3acc62e290a90ffa8406975845'
 
   url 'http://versionec.us.wizard101.com/Wizard101.dmg'
   appcast 'http://versionec.us.wizard101.com/Wizard101.xml',
-          checkpoint: '20cdfeddb4bdcf8bca22c4c65b23525f8d2e58d7bbba049821f486a0cb79e85a'
+          checkpoint: 'e1c5fe7b3e946e3696cc63a2cb357ae20e36669b06c5c2e004ad38f30419f889'
   name 'Wizard101'
   homepage 'https://www.wizard101.com/'
 
