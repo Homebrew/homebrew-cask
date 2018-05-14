ed21be850c924b310c9575848dd51ae4204ed03e
Vítor Galvão <info@vitorgalvao.com>
2018-05-14 21:17:33 +0100

    Update autodesk-fusion-360.rb

--- Casks/autodesk-fusion-360.rb
+++ Casks/autodesk-fusion-360.rb
@@ -1,12 +1,11 @@
 cask 'autodesk-fusion-360' do
-  version ':latest'
-  sha256 'e950e156917029783f33e64d587beee9243adf6e3e77e00615dc59c1fa837804'
+  version :latest
+  sha256 :no_check
 
   url 'https://dl.appstreaming.autodesk.com/production/installers/Fusion%20360%20Client%20Downloader.dmg'
   name 'Autodesk Fusion 360'
   homepage 'https://www.autodesk.com/products/fusion-360/overview'
 
-  license :closed
   installer manual: 'Right-click > Open to Install.app'
 
   uninstall script: '~/Applications/Remove Autodesk Fusion 360.app/Contents/MacOS/Remove Autodesk Fusion 360',
