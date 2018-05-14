d7dfc1433ddef1db2f7b9823deb68b9819565756
commitay <commitay@users.noreply.github.com>
2018-05-14 10:35:21 +1000

    Update sublime-text to 3176

--- Casks/sublime-text.rb
+++ Casks/sublime-text.rb
@@ -1,10 +1,10 @@
 cask 'sublime-text' do
-  version '3170'
-  sha256 'c61af654721bd9de6b7e82db43378f583e577d8f6cfb7c1541771c64523b30f3'
+  version '3176'
+  sha256 '68cc4a12f30511b3e9fa45e6240f18b1963407a613d8933a57dec0fddf504aaa'
 
   url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version}.dmg"
   appcast 'https://www.sublimetext.com/updates/3/stable/appcast_osx.xml',
-          checkpoint: '6801661fd5d0fa4d25218b42d3bbbf945ec31f294d52d48642b8b0c48361e08e'
+          checkpoint: '853a79cf4d12c906924be0a0d4660b6606e18b02fa982966604c4fc0a981d529'
   name 'Sublime Text'
   homepage 'https://www.sublimetext.com/3'
 
