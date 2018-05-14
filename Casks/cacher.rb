c970d42cbc962a10128f6bae3c65357df59a0041
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-14 21:27:23 +0100

    Update cacher to 1.5.1

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,11 +1,11 @@
 cask 'cacher' do
-  version '1.5.0'
-  sha256 '534fea7c14b7d5cc78cb14c4f4d07809de4fedc180a40d792f3113093ef2e075'
+  version '1.5.1'
+  sha256 '309ab4d973634f4d670155c49802a0b0fab60ecb117d19d5e7859c6b4b1d5df0'
 
   # cacher-download.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
   appcast 'https://cacher-download.nyc3.digitaloceanspaces.com/latest-mac.json',
-          checkpoint: '944ada3a6043b772d26e9b44518b6c301f903494a7edd316e8632b8d3c3e09ed'
+          checkpoint: 'cbb66c36d64bfe45e3dfe287f7654fc1aab266c21dc8a588826e9ca1cf05e49a'
   name 'Cacher'
   homepage 'https://www.cacher.io/'
 
