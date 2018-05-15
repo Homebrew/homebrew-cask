154fd6a0213f0e9628650b6222dd7ff5b960187e
Johannes Heck <yoyostile@gmail.com>
2018-05-15 15:13:14 +0200

    fix identation

--- Casks/pdftk.rb
+++ Casks/pdftk.rb
@@ -9,5 +9,5 @@ cask 'pdftk' do
   pkg 'pdftk_server-2.02-mac_osx-10.11-setup.pkg'
 
   uninstall pkgutil: 'com.pdflabs.pdftkThePdfToolkit.pdftk.pkg',
-            delete: '/usr/local/bin/pdftk'
+            delete:  '/usr/local/bin/pdftk'
 end
