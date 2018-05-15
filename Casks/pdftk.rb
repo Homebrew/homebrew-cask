fa9cc85cea2ef2fae7961e00ba9b5ed1df545b06
Johannes Heck <yoyostile@gmail.com>
2018-05-15 14:51:42 +0200

    add pdftk 2.02 cask

---
+++ Casks/pdftk.rb
@@ -0,0 +1,12 @@
+cask 'pdftk' do
+  version '2.02'
+  sha256 'c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c'
+
+  url 'https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg'
+  name 'PDFtk Server'
+  homepage 'https://www.pdflabs.com/tools/pdftk-server'
+
+  pkg 'pdftk_server-2.02-mac_osx-10.11-setup.pkg'
+
+  uninstall pkgutil: 'com.pdflabs.pdftkThePdfToolkit.pdftk.pkg'
+end
