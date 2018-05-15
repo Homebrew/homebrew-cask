24fb7fc211288eaa6615e2552bc02b28b7e3127d
Stefano Canducci <stenowtf@users.noreply.github.com>
2018-05-15 11:53:26 +0200

    Rename real-vnc to vnc-server and update to 6.2.1 (#47228)
    * Rename real-vnc to vnc-connect and update to 6.2.1
    
    * Fix definition
    
    * Removed the client data

--- Casks/vnc-server.rb
+++ Casks/vnc-server.rb
@@ -1,16 +1,15 @@
-cask 'real-vnc' do
-  version '6.1.1'
-  sha256 '3b3437ba078774692b8073273163a93cefa9c5b936b718e2020a657d4496df79'
+cask 'vnc-server' do
+  version '6.2.1'
+  sha256 '7c105880fffee5cd350d713adee7ca8bef029325799f9bc483043ca27de503cd'
 
-  url "https://www.realvnc.com/download/file/vnc.files/VNC-#{version}-MacOSX-x86_64.pkg"
-  name 'Real VNC'
+  url "https://www.realvnc.com/download/file/vnc.files/VNC-Server-#{version}-MacOSX-x86_64.pkg"
+  name 'Real VNC Server'
   homepage 'https://www.realvnc.com/'
 
-  pkg "VNC-#{version}-MacOSX-x86_64.pkg"
+  pkg "VNC-Server-#{version}-MacOSX-x86_64.pkg"
 
   uninstall_preflight do
     system_command '/Applications/RealVNC/Uninstall VNC Server.app/Contents/Resources/uninstaller.sh', print_stderr: false, sudo: true
-    system_command '/Applications/RealVNC/Uninstall VNC Viewer.app/Contents/Resources/uninstaller.sh', print_stderr: false, sudo: true
   end
 
   uninstall launchctl: [
@@ -19,13 +18,11 @@ cask 'real-vnc' do
                        ],
             pkgutil:   [
                          'com.realvnc.vncserver.pkg',
-                         'com.realvnc.vncviewer.pkg',
                        ]
 
   zap trash: [
                '/Library/Logs/vncserver.log.bak',
                '~/Library/Saved Application State/com.realvnc.vnclicensewiz.savedState',
-               '~/Library/Saved Application State/com.realvnc.vncviewer.savedState',
                '~/Library/Saved Application State/com.realvnc.vncserver.savedState',
              ]
 end
