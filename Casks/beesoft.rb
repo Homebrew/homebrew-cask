778a9339894e464c66360d7d40565c7d716b4929
Julian Mayer <julian@corecode.at>
2018-05-15 15:35:15 +0200

    Update beesoft to 4.0.5

--- Casks/beesoft.rb
+++ Casks/beesoft.rb
@@ -1,8 +1,8 @@
 cask 'beesoft' do
-  version '3.15.1'
-  sha256 '8e55dd772af896516c1d5a3fc89875c4c71b25d64206eed89444d5e86bd60404'
+  version '4.0.5'
+  sha256 'f5557e4f13ac056e4fb05688ee111187b0cfe4d29774d95229b629d9ad47cd20'
 
-  url "https://www.beeverycreative.com/public/software/BEESOFT/BEESOFT-#{version}/Mac-BEESOFT-#{version}.pkg"
+  url "https://www.beeverycreative.com/public/software/BEESOFT/BEESOFT%20-%20#{version.major_minor}/Desktop%20Version/BEESOFT-#{version}.pkg"
   name 'Beesoft'
   homepage 'https://beeverycreative.com/beesoft/'
 
