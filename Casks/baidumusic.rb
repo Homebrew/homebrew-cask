e3037e02549110d1860afb1814678270ab48a4ff
Julian Mayer <julian@corecode.at>
2018-05-15 22:30:40 +0200

    Update baidumusic to latest

--- Casks/baidumusic.rb
+++ Casks/baidumusic.rb
@@ -2,7 +2,8 @@ cask 'baidumusic' do
   version :latest
   sha256 :no_check
 
-  url 'http://music.baidu.com/player/mac/baidumusic_mac.dmg'
+  # download.qianqian.com was verified as official when first introduced to the cask
+  url 'http://download.qianqian.com/channel/2/web_daohang'
   name 'Baidu Music'
   name '百度音乐'
   homepage 'https://music.baidu.com/'
