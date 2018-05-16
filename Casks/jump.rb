f7d4e870f881651fb66755ce7d8ad0c7bc0d6f9c
CoreCode <core-code@users.noreply.github.com>
2018-05-16 15:02:26 +0200

    Update jump to 7.1.3 (#47287)
    * Update idisplay to latest
    
    * Update jump to 7.1.3
    
    * remove erronous double commit introduced by cask-repair

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -1,10 +1,10 @@
 cask 'jump' do
-  version '7.1.2'
-  sha256 '9a96dd38636b9dff24e46e413556a0067ea93e5581d1f094a70f0cfeed7bef32'
+  version '7.1.3'
+  sha256 '5750edecdd4f2f2f15ae2be34494c1a171a6b69c73c62885f2cb66916c752126'
 
   url 'https://jumpdesktop.com/downloads/jdmac'
   appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml',
-          checkpoint: 'be327d110a622fd519c08f8dac9121d80674ea2c72e8d251c7d934ad9fd38765'
+          checkpoint: '9474ab62c55742147d5c88209f116ecb06fb14d0c0988a535cbadc1f236fabbc'
   name 'Jump Desktop'
   homepage 'https://jumpdesktop.com/#jdmac'
 
