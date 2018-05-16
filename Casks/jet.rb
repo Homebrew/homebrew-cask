3b409c36260dc9898a7994cec51d9744e2edee27
Brian Morton <brianmortondesign@gmail.com>
2018-05-16 09:02:35 -0700

    Update jet to 2.5.1

--- Casks/jet.rb
+++ Casks/jet.rb
@@ -4,6 +4,8 @@ cask 'jet' do
 
   # s3.amazonaws.com/codeship-jet-releases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/codeship-jet-releases/#{version}/jet-darwin_amd64_#{version}.tar.gz"
+  appcast 'https://documentation.codeship.com/pro/jet-cli/release-notes/',
+          checkpoint: '3e0de3787f279faf7c26373af09b3f75b270e059ddfe93a75fb4490d0ae4ac7b'
   name 'Codeship Jet'
   homepage 'https://documentation.codeship.com/pro/builds-and-configuration/cli/'
 
