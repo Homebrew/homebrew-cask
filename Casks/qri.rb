7de53e14ae88c9d32ecc3d569a2967867aed20f3
Thomas Osterbind <tom@qri.io>
2018-05-16 11:56:38 -0400

    Fix appcast in qri.app Cask

--- Casks/qri.rb
+++ Casks/qri.rb
@@ -4,6 +4,8 @@ cask 'qri' do
 
   # github.com/qri-io/frontend was verified as official when first introduced to the cask
   url 'https://github.com/qri-io/frontend/releases/download/v0.0.2/qri-0.0.2.dmg'
+  appcast 'https://github.com/qri-io/frontend/releases.atom',
+          checkpoint: 'df76297abfa55bf486284aa5d138e8d228826374748d1f61fa5593cbf921c0d0'
   name 'qri'
   homepage 'https://qri.io/'
 
