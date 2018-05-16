148ee2cb6e3e3350f7b295326f9d7c012f9bc7a9
commitay <commitay@users.noreply.github.com>
2018-05-16 17:55:39 +1000

    Update airtool.rb

--- Casks/airtool.rb
+++ Casks/airtool.rb
@@ -10,15 +10,17 @@ cask 'airtool' do
 
   pkg "airtool_#{version}.pkg"
 
+  uninstall_preflight do
+    set_ownership '/Library/Application Support/Airtool'
+  end
+
   uninstall pkgutil:    [
                           'com.adriangranados.airtool.airtool-bpf.*',
                           'com.adriangranados.airtool.Airtool.pkg',
                         ],
             launchctl:  'com.adriangranados.airtool.airtool-bpf.pkg',
-            login_item: 'Airtool'
+            login_item: 'Airtool',
+            delete:     '/Library/Application Support/Airtool'
 
-  zap trash: [
-               '/Library/Application Support/Airtool',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.adriangranados.airtool.sfl*',
-             ]
+  zap trash: '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.adriangranados.airtool.sfl*'
 end
