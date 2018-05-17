842f91fc08c47d5677cf9e6817fd128941c2bcc0
Brian Morton <brianmortondesign@gmail.com>
2018-05-16 21:33:01 -0700

    Update neo4j to 1.1.1 (#47326)

--- Casks/neo4j.rb
+++ Casks/neo4j.rb
@@ -1,7 +1,7 @@
 cask 'neo4j' do
   # note: "4" is not a version number, but an intrinsic part of the product name
-  version '1.1.0'
-  sha256 'b9251dbf3e80ad0cef50700fddf96400e8d0b392aa002bbbcf0a3c6a0bcbf785'
+  version '1.1.1'
+  sha256 '1709ff977343ca212b50b65fbacc5ccb8dd76e94a547eada035a60c837f9a663'
 
   url "https://neo4j.com/artifact.php?name=neo4j-desktop-offline-#{version}.dmg"
   name 'Neo4j Desktop'
