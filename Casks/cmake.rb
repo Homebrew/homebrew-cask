bc310dca503edebbffb6c8d50e49f3e4321bb95c
commitay <commitay@users.noreply.github.com>
2018-05-18 09:47:05 +1000

    Update cmake to 3.11.2 (#47361)

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -1,6 +1,6 @@
 cask 'cmake' do
-  version '3.11.1'
-  sha256 '26592a607263b0270c2792f569aeb63168e2e1e1dead0bd5e57d4c868356bae5'
+  version '3.11.2'
+  sha256 '8698f68bd62647993c3371571a1d326b4ee33947301cfec4592a7908c737ac36'
 
   url "https://www.cmake.org/files/v#{version.major_minor}/cmake-#{version}-Darwin-x86_64.dmg"
   name 'CMake'
