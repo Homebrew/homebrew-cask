cdb26946dd4ae67dea87bcb8d386319c2a1e03cb
jcbot <jcb@leipert.io>
2018-05-17 11:34:48 +0200

    Update pycharm-ce to 2018.1.3,181.4892.64 (#47336)

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,10 +1,10 @@
 cask 'pycharm-ce' do
-  version '2018.1.2,181.4668.75'
-  sha256 '8dedf70dd10f9d9581440e9c4e1e5cb08e52b31d45d39eea09bf77751f1dee6c'
+  version '2018.1.3,181.4892.64'
+  sha256 '60dfc72ecd9d607d41145b865943994af1e7636a4402222cbef868c5185efe14'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=PCC&latest=true&type=release',
-          checkpoint: '8481f058b90837188a0f6fcc296e1203a568ad461d851418ddb6276ef153cb68'
+          checkpoint: 'bb08e783980a805032d20f1105aaa4d4299e49636c80823cbda5f6fb6ce44823'
   name 'Jetbrains PyCharm Community Edition'
   name 'PyCharm CE'
   homepage 'https://www.jetbrains.com/pycharm/'
