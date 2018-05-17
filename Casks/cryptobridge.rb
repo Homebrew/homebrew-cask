d6732f99e8ff72ccbeb9707b77816cbb481b2503
Tyrael <wangkezun@gmail.com>
2018-05-17 17:30:33 +0800

    cryptobridge update version to 0.12.6 (#47334)

--- Casks/cryptobridge.rb
+++ Casks/cryptobridge.rb
@@ -1,10 +1,10 @@
 cask 'cryptobridge' do
-  version '0.10.5'
-  sha256 '8b08168721b9954f941f2d77ea1d2ef5c893fefe5925b161420bf4b027977a53'
+  version '0.12.6'
+  sha256 'e6e9fbd2a5ec28f1fb7d86e716616e02f08b1488ee184bd33a8b28b8634876e2'
 
-  url "https://github.com/CryptoBridge/cryptobridge-ui/releases/download/#{version}/CryptoBridge_#{version}.dmg"
+  url "https://github.com/CryptoBridge/cryptobridge-ui/releases/download/v#{version}/CryptoBridge-#{version}.dmg"
   appcast 'https://github.com/CryptoBridge/cryptobridge-ui/releases.atom',
-          checkpoint: '4fd4bda4eb59313f6f22a5e8eaed364cb55d587f1b59f599f8ea4e20a25320ec'
+          checkpoint: '40b68d90592165ccdaae0017f102d70eaa5eb3d54325c46c8cf76cfde965c74c'
   name 'CryptoBridge'
   homepage 'https://github.com/CryptoBridge/cryptobridge-ui'
 
