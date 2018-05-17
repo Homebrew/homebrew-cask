ca832e3c536e92c5c97f85f344070d736c62baeb
Julian Mayer <julian@corecode.io>
2018-05-17 20:57:21 +0200

    revert to original SHA as that one was fine anyway

--- Casks/art-of-illusion.rb
+++ Casks/art-of-illusion.rb
@@ -1,6 +1,6 @@
 cask 'art-of-illusion' do
   version '3.0.3'
-  sha256 'fae41002dd05973f8d2e24383722f6f864321a8ff9fe40a0d839304f80cd0526'
+  sha256 '855683968480ef24520e998d157bf94865232b3dddd7d8b2431f4dc8d6782984'
 
   # sourceforge.net/aoi was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/aoi/ArtOfIllusion#{version.no_dots}-Mac.dmg"
