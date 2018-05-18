d18a4d09bbdc002a1e6032825cfa2cb08d26083b
commitay <commitay@users.noreply.github.com>
2018-05-18 21:13:18 +1000

    Update maratis.rb

--- Casks/maratis.rb
+++ Casks/maratis.rb
@@ -8,6 +8,6 @@ cask 'maratis' do
   name 'Maratis'
   homepage 'http://www.maratis3d.org/'
 
-  app 'Bin/Maratis.app'
-  app 'Bin/MaratisPlayer.app'
+  app "Maratis-#{version}-osx/Bin/Maratis.app"
+  app "Maratis-#{version}-osx/Bin/MaratisPlayer.app"
 end
