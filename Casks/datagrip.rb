9191888b9b0d388f0432c33008b684a386b59be9
jcbot <jcb@leipert.io>
2018-05-18 12:31:27 +0200

    Update datagrip to 2018.1.3,181.5087.19

--- Casks/datagrip.rb
+++ Casks/datagrip.rb
@@ -1,10 +1,10 @@
 cask 'datagrip' do
-  version '2018.1.2,181.4892.10'
-  sha256 '1c2cffb083abf43290b1b655fa7a14f9229769ba872e231b850f5250fc50ba25'
+  version '2018.1.3,181.5087.19'
+  sha256 'dd26486fd438dab32c887f2ca1e77ef4c53cf9fa38a65af37bbc1b7c72f0dc01'
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release',
-          checkpoint: '4034d3c3530f897627a4542bbc5face2716e305c1ce6c65c3f7d1b150db0cae6'
+          checkpoint: 'eb4af824cefd72931f26a5da958e2a78881ef7c951c7ba1dcc5ec9f51f18f421'
   name 'DataGrip'
   homepage 'https://www.jetbrains.com/datagrip/'
 
