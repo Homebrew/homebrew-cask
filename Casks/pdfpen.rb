a9618f5ed90559d80a0b52b2c86bd5c1c3a0c7c3
suschizu <brigitte@corecode.io>
2018-05-18 18:35:23 +0200

    Update pdfpen to 1010.4,1526436120 (#47371)

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -1,10 +1,10 @@
 cask 'pdfpen' do
-  version '1001.3,1523932069'
-  sha256 'ac69c07435762db6b4f42f7bfef6af8d978be1153df007eec1f757c36cb5f695'
+  version '1010.4,1526436120'
+  sha256 '6aad129f1a4d0ab1c4c23d972f71922241373eb0081d88cc60c737c3a6299867'
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpen/#{version.before_comma}/#{version.after_comma}/PDFpen-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml',
-          checkpoint: '00a06e69d5591f449bd3b31c33eed377b8fddc95feaa94fe359dee15e93bafd2'
+          checkpoint: '7545ce01fafb971f5fef57844d39b3e57fde73d81ed6b382770a030864edd595'
   name 'PDFpen'
   homepage 'https://smilesoftware.com/PDFpen'
 
