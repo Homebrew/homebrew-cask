4c25e57a8b67c83ce2c56aa387e964209a38d1cf
suschizu <brigitte@corecode.io>
2018-05-18 18:33:49 +0200

    Update grandtotal to 5.1.5 (#47373)

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,10 +1,10 @@
 cask 'grandtotal' do
-  version '5.1.4'
-  sha256 'e76ee2f2d73c0d89276f525106ddf9be7f60b1a0824e90b01a1bdaf3942565e6'
+  version '5.1.5'
+  sha256 'aeda8d79516e5fd6bdfb29b50e606ff05bf638cae404f142c207f57bb4420eef'
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   appcast "https://mediaatelier.com/GrandTotal#{version.major}/feed.php",
-          checkpoint: '71a8a512a3dcfbe13db2652050c02f894560198ce426cffe2f1c3333e71b4183'
+          checkpoint: '6960c34c6da5c8d319ed744a3f16c11db989a2503fb24dc679573b33a88361af'
   name 'GrandTotal'
   homepage "https://www.mediaatelier.com/GrandTotal#{version.major}/"
 
