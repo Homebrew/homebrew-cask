9254b0b0e6d05f9a4c15d88b35d59d312a6318a0
CoreCode <core-code@users.noreply.github.com>
2018-05-18 05:37:30 +0200

    Update manager to 18.5.2 (#47355)

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -1,9 +1,9 @@
 cask 'manager' do
-  version :latest
-  sha256 :no_check
+  version '18.5.2'
+  sha256 '7d1e8ec09d80413f0429a9fc64c5bfa9873e071648b78d42fda5cb77b14f4326'
 
-  # mngr.s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://mngr.s3.amazonaws.com/Manager.dmg'
+  # d2ap5zrlkavzl7.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d2ap5zrlkavzl7.cloudfront.net/#{version}/Manager.dmg"
   name 'Manager'
   homepage 'https://www.manager.io/'
 
