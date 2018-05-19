ad71041b3694b50cea0512c80d2feeb5521184fb
commitay <commitay@users.noreply.github.com>
2018-05-19 22:47:20 +1000

    Update 9 navicat Casks: appcast

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -3,8 +3,8 @@ cask 'navicat-premium-essentials' do
   sha256 '6be93aefc82c02f6b185a47a142b158ac527847cec80e89a403b38cae48e76a7'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
-  appcast 'https://www.navicat.com/en/products/navicat-premium-release-note',
-          checkpoint: '3763059d158942dc11cf54311397f1dfda9ffcb7a4c1a526cfd885d00328b719'
+  appcast "https://www.navicat.com/updater/v#{version.major_minor.no_dots}/sysProfileInfo.php?appName=Navicat%20Premium%20Essentials",
+          checkpoint: '267ce420731d35d7c3e12901a85987b6941ba556cc30ae6a7cc30e32f16765fe'
   name 'Navicat Premium Essentials'
   homepage 'https://navicat.com/products/navicat-essentials'
 
--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -3,8 +3,8 @@ cask 'navicat-premium' do
   sha256 '5ea1823be5269b9e4838a3e5000143625db18828959afacb6356ab9461828cb2'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
-  appcast 'https://www.navicat.com/en/products/navicat-premium-release-note',
-          checkpoint: '3763059d158942dc11cf54311397f1dfda9ffcb7a4c1a526cfd885d00328b719'
+  appcast "https://www.navicat.com/updater/v#{version.major_minor.no_dots}/sysProfileInfo.php?appName=Navicat%20Premium",
+          checkpoint: 'ad04b44679960dccae2d6922831e6da8437f6baa6e91812c1c8c21acd835d5c1'
   name 'Navicat Premium'
   homepage 'https://www.navicat.com/products/navicat-premium'
 
