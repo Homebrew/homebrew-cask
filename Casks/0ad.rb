827218820d89086622c03fd143fcced4070facba
chrisben <chrisben@users.noreply.github.com>
2018-05-19 19:50:23 +0200

    Update 0ad.rb
    After making all changes to the cask:
    
     [x]  `brew cask audit --download {{cask_file}}` is error-free.
     [x]  `brew cask style --fix {{cask_file}}` left no offenses.
     [x]  The commit message includes the cask’s name and version.

--- Casks/0ad.rb
+++ Casks/0ad.rb
@@ -1,15 +1,15 @@
 cask '0ad' do
-  version '0.0.22-alpha'
-  sha256 '5876856a8641ffe736fc39f9f6472d7e1699c49a3154e35e713a382abc840caa'
+  version '0.0.23-alpha'
+  sha256 'c9de859ad0e2cc6b1085803d1fc107094423520e16c55b32175f43df11475441'
 
   # wildfiregames.com was verified as official when first introduced to the cask
   url "http://releases.wildfiregames.com/0ad-#{version}-osx64.dmg"
   appcast 'https://play0ad.com/download/mac/',
-          checkpoint: 'ea7ba45a506e088510719faf62e0b075ee7b049f61dc736aa826e95db1e74d7a'
+          checkpoint: '17f0f8823e6f642b18f7438471894c8d53a6d4d7c5eeaaf7d075f69530c0cfc0'
   name '0 A.D.'
   homepage 'https://play0ad.com/'
 
-  depends_on macos: '>= :lion'
+  depends_on macos: '>= :mountain_lion'
 
   app '0ad.app'
 end
