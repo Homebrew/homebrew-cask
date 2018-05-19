87a49dc720e4cca223560b35523c8df15f07b132
Julian Mayer <julian@corecode.at>
2018-05-19 12:36:48 +0200

    Update resolume-arena to 6.0.7

--- Casks/resolume-arena.rb
+++ Casks/resolume-arena.rb
@@ -1,6 +1,6 @@
 cask 'resolume-arena' do
-  version '6.0.4'
-  sha256 '246f6eb98184d7480450b708dfc48e9152ad9dc94fd0b659672e1c326b27fb99'
+  version '6.0.7'
+  sha256 'bfebbfde003956039e04193564a6b46210915a50df3b39c31c6e20f5044eb1b3'
 
   url "https://resolume.com/download/Resolume_Arena_#{version.dots_to_underscores}_Installer.dmg"
   name 'Resolume Arena'
