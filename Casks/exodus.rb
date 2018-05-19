a59bded14058da252fca2b32ed828085f228b48b
Simone Carletti <weppos@weppos.net>
2018-05-19 16:47:45 +0200

    Update exodus to 1.51.3

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,11 +1,11 @@
 cask 'exodus' do
-  version '1.51.2'
-  sha256 '4f2f01ec195be57ad290a808c936a8e3c306de1394cec2e3b000c22a68108d07'
+  version '1.51.3'
+  sha256 '539656cf71583358aff23a4e3a087c010dc01d5a716cba08944a81d10d671447'
 
   # exodusbin.azureedge.net was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-macos-#{version}.dmg"
   appcast 'https://www.exodus.io/releases/',
-          checkpoint: 'd8cc216be0c83eac4b7aca57201d1ca99cee2344bd9a6f70c5fce19d81f3e835'
+          checkpoint: '72d13d2cb59682b3cbaf59f4c917d3d3f955c082cf59c8c34d808cdc1f5750f6'
   name 'Exodus'
   homepage 'https://www.exodus.io/'
 
