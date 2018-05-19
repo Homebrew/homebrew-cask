4ba00d335c351680847a0de3596c4a48076c769f
Danny Mösch <danny.moesch@icloud.com>
2018-05-19 20:16:13 +0200

    Update jump to 7.1.4 (#47416)

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -1,10 +1,10 @@
 cask 'jump' do
-  version '7.1.3'
-  sha256 '5750edecdd4f2f2f15ae2be34494c1a171a6b69c73c62885f2cb66916c752126'
+  version '7.1.4'
+  sha256 'daf3191556ffa7af4b50e809a2bc96bcd465aa59c5613381a68b0f13ad9087a8'
 
   url 'https://jumpdesktop.com/downloads/jdmac'
   appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml',
-          checkpoint: '9474ab62c55742147d5c88209f116ecb06fb14d0c0988a535cbadc1f236fabbc'
+          checkpoint: '0e5daaec22be40cc11966ebc9b8d0180d1477129677fdf7bfba93f57c1bb178f'
   name 'Jump Desktop'
   homepage 'https://jumpdesktop.com/#jdmac'
 
