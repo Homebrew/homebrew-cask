a22f0cf2d1d0e18b78be1661c30e4c5edd824ea3
zeyujay <zeyujay@gmail.com>
2018-05-21 03:41:44 +0800

    change hunantv.rb

--- Casks/hunantv.rb
+++ Casks/hunantv.rb
@@ -1,13 +1,17 @@
 cask 'hunantv' do
   version '3.3.3_20180514'
   sha256 'f457b02aa7bca895677a21e55fd3c7fa05d02cfaefdc46c8558fc99b577e3c5a'
+
   url "http://downloadimgo.hunantv.com/app/mac/mgtv_#{version}_release.dmg"
   name 'hunantv'
   name '芒果视频'
   homepage 'https://www.mgtv.com/app'
+
   auto_updates true
   depends_on macos: '>= :yosemite'
+
   app 'Imgotv-Mac.app'
+
   zap trash: [
                '~/Library/Preferences/com.hunantv.osx.imgotv.plist',
                '~/Library/Caches/com.hunantv.LaunchAtLoginHelper',
@@ -16,5 +20,4 @@ cask 'hunantv' do
                '~/Library/Caches/Imgotv-Mac',
                '~/Library/Logs/Imgotv-Mac',
              ]
-
 end
