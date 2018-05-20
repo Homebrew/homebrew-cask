d4e38bf41b2078e4ca7bc381aea08c6c4b78ad6f
CoreCode <core-code@users.noreply.github.com>
2018-05-20 08:04:50 +0200

    Update resolume-arena to 6.0.7 (#47408)
    * Update resolume-arena to 6.0.7
    
    * Update resolume-arena.rb

--- Casks/resolume-arena.rb
+++ Casks/resolume-arena.rb
@@ -1,8 +1,10 @@
 cask 'resolume-arena' do
-  version '6.0.4'
-  sha256 '246f6eb98184d7480450b708dfc48e9152ad9dc94fd0b659672e1c326b27fb99'
+  version '6.0.7'
+  sha256 'bfebbfde003956039e04193564a6b46210915a50df3b39c31c6e20f5044eb1b3'
 
   url "https://resolume.com/download/Resolume_Arena_#{version.dots_to_underscores}_Installer.dmg"
+  appcast 'https://resolume.com/update/arena_mac.xml',
+          checkpoint: '405163af69fd9f5a822ed6ae985e07739e97ba13b54c72c7fd129602eb1a98cb'
   name 'Resolume Arena'
   homepage 'https://resolume.com/'
 
