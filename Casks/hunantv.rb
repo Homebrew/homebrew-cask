1258a73fb63fe44d623b08a0a710f3399ba726f2
zeyujay <zeyujay@gmail.com>
2018-05-21 04:06:06 +0800

    change hunantv.rb

--- Casks/hunantv.rb
+++ Casks/hunantv.rb
@@ -2,11 +2,10 @@ cask 'hunantv' do
   version '3.3.3_20180514'
   sha256 'f457b02aa7bca895677a21e55fd3c7fa05d02cfaefdc46c8558fc99b577e3c5a'
 
-  # URL_SECTION was verified as official when first introduced to the cask
   url "http://downloadimgo.hunantv.com/app/mac/mgtv_#{version}_release.dmg"
   name 'hunantv'
   name '芒果视频'
-  homepage 'https://www.mgtv.com/app'
+  homepage 'http://www.hunantv.com/app'
 
   auto_updates true
   depends_on macos: '>= :yosemite'
