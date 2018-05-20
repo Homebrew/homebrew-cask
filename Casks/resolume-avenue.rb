d4b612c1a644857090788a7fcf3ef0a5920ad092
Julian Mayer <julian@corecode.io>
2018-05-20 12:31:11 +0200

    add appcast

--- Casks/resolume-avenue.rb
+++ Casks/resolume-avenue.rb
@@ -3,6 +3,8 @@ cask 'resolume-avenue' do
   sha256 '6edbf4afdf3bf41f503862aac041384f59557f2ccf94c1bd9203a6ed25cfa2bf'
 
   url "https://resolume.com/download/Resolume_Avenue_#{version.dots_to_underscores}_Installer.dmg"
+  appcast 'https://resolume.com/update/avenue_mac.xml',
+          checkpoint: '6f87f513b75530818aaf1ddaa78454da3b16cfc0c5f00955621736eca9d5374b'
   name 'Resolume Avenue'
   homepage 'https://resolume.com/'
 
