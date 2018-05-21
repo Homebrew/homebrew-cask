292fe3578091b5696afc9bf7e2013d0af451bee6
Julian Mayer <julian@corecode.io>
2018-05-21 14:37:28 +0200

    Update subutaicontrolcenter to 7.0.1

--- Casks/subutaicontrolcenter.rb
+++ Casks/subutaicontrolcenter.rb
@@ -1,11 +1,11 @@
 cask 'subutaicontrolcenter' do
-  version '7.0.0'
-  sha256 'c3eb5e3c0e97b3db14f8f5a609ac22ebde92335ef4568cb83c6a978e8eb82ccc'
+  version '7.0.1'
+  sha256 '45e87ab51f97158a1f64afe8e0e5dead462438b384decfd2d039b303c19abd43'
 
   # cdn.subutai.io:8338/kurjun/rest/raw was verified as official when first introduced to the cask
   url 'https://cdn.subutai.io:8338/kurjun/rest/raw/get?name=subutai-control-center.pkg'
   appcast 'https://github.com/subutai-io/control-center/releases.atom',
-          checkpoint: '3826ac214986bc1353a310d84745a455c2cf54eb5e9a596150fb9fb73a777b9f'
+          checkpoint: 'a63fc73e1862a27ae4a278b963f8b0c40184400fb831a36acdef8ccbca2bcf3d'
   name 'Subutai Control Center'
   homepage 'https://subutai.io/'
 
