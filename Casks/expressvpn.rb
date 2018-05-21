ff7ad4c1952f9c741634852559192cdc428abfdb
Khaldoon Mutahar <khal@mutahar.me>
2018-05-22 07:13:50 +0800

    Update expressvpn to 6.7.3.3774

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -1,6 +1,6 @@
 cask 'expressvpn' do
-  version '6.7.1.3131'
-  sha256 '6a142c6e47e08c9c80229bce2e51fb3b04874dc5573cac72ebf1d9ffa12fcdb2'
+  version '6.7.3.3774'
+  sha256 '4cc39df2693e8d93858697f5966ff19d53870dfc956268f97fa5d0e2110d2544'
 
   url "https://download.expressvpn.xyz/clients/mac/expressvpn-install_v#{version}.pkg"
   name 'ExpressVPN'
