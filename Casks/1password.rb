d259d0747d8a01bb26040bc844ac063c632e28ab
CoreCode <core-code@users.noreply.github.com>
2018-05-23 11:53:04 +0200

    Update 1password to 7.0 (#47471)
    * Update 1password to 7.0
    
    * fix uninstallation
    
    * conditional pkg uninstall
    
    * 7.0 requires sierra, fix it
    
    * Update 1password.rb
    
    * canonical url

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -1,36 +1,18 @@
 cask '1password' do
-  if MacOS.version <= :lion
-    version '3.8.22'
-    sha256 '3afd75f1bddf791dc7dbc9a7d92ab6eb91ee891407d750cedb7b5aff5fe8bf17'
+  version '7.0'
+  sha256 '9c79de6b4ff9dadc7476cd140156087bd9ad12d66de0b7b05dbcb0a2f660d7ec'
 
-    # cache.agilebits.com/dist/1P/mac was verified as official when first introduced to the cask
-    url "https://cache.agilebits.com/dist/1P/mac/1Password-#{version}.zip"
-
-    app '1Password.app'
-  elsif MacOS.version <= :mavericks
-    version '4.4.3'
-    sha256 '6657fc9192b67dde63fa9f67b344dc3bc6b7ff3e501d3dbe0f5712a41d8ee428'
-
-    # cache.agilebits.com/dist/1P/mac4 was verified as official when first introduced to the cask
-    url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
-
-    app "1Password #{version.major}.app"
-  else
-    version '6.8.9'
-    sha256 'd7cc24dc354f27441929350b9e6e2e4a710d6ed0bdab06f0e9be07160fe04200'
-
-    # cache.agilebits.com/dist/1P/mac4 was verified as official when first introduced to the cask
-    url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
-
-    app "1Password #{version.major}.app"
-  end
-
-  appcast 'https://app-updates.agilebits.com/product_history/OPM4',
-          checkpoint: 'eb8b60476f30a53f496a84e809b948faf81ea9f9ab442f4c923e48720e5a1f2a'
+  # 1password.com was verified as official when first introduced to the cask
+  url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
+  appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}",
+          checkpoint: '8e429ed32182657fe4b490486947246d91251853f0f39778d0976b2f8b3ba573'
   name '1Password'
   homepage 'https://1password.com/'
 
   auto_updates true
+  depends_on macos: '>= :sierra'
+
+  app "1Password #{version.major}.app"
 
   zap trash: [
                '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
