cb6bcc7e254ff61b8558e8d07ef8970f46c80c92
commitay <commitay@users.noreply.github.com>
2018-05-23 18:40:56 +1000

    Update texpad.rb

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -1,23 +1,16 @@
 cask 'texpad' do
-  if MacOS.version <= :mountain_lion
-    version '1.6.14'
-    sha256 '18fcbe93e77e5b5bc848172546962fcde397a26fd543efcc1054004369192f7e'
+  version '1.8.4,392,0ea1407'
+  sha256 '88c0c140e136099656a21a3534078edf00ba7970c5a6f6c29b8718a4eff1438f'
 
-    url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.dots_to_underscores}.zip"
-  else
-    version '1.8.4,392,0ea1407'
-    sha256 '88c0c140e136099656a21a3534078edf00ba7970c5a6f6c29b8718a4eff1438f'
-
-    url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.before_comma.dots_to_underscores}__#{version.after_comma.before_comma}__#{version.after_comma.after_comma}.dmg"
-    appcast 'https://www.texpad.com/static-collected/upgrades/texpadappcast.xml',
-            checkpoint: '70324384fc8fdcd1d93ecb153a5308210768eafdebbf82b09ab24fe5e4e8223e'
-  end
-
+  # download.texpadapp.com was verified as official when first introduced to the cask
+  url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.before_comma.dots_to_underscores}__#{version.after_comma.before_comma}__#{version.after_comma.after_comma}.dmg"
+  appcast 'https://www.texpad.com/static-collected/upgrades/texpadappcast.xml',
+          checkpoint: '70324384fc8fdcd1d93ecb153a5308210768eafdebbf82b09ab24fe5e4e8223e'
   name 'Texpad'
   homepage 'https://www.texpad.com/osx'
 
   auto_updates true
-  depends_on macos: '>= :lion'
+  depends_on macos: '>= :el_capitan'
 
   app 'Texpad.app'
 
