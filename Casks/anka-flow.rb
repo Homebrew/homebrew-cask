ca66fdfdc4ef7b45fb6afd28976312ec7da01685
commitay <commitay@users.noreply.github.com>
2018-05-24 07:32:17 +1000

    Update anka-flow to 1.3.0.91

--- Casks/anka-flow.rb
+++ Casks/anka-flow.rb
@@ -1,12 +1,12 @@
 cask 'anka-flow' do
-  version '1.2.2.88'
-  sha256 '9b885d7301f8ffcd1a1af55e2e978a16561a2cc5493ff0bb56e16afb14dc6f53'
+  version '1.3.0.91'
+  sha256 'e270319e6546f188064f292a55d22c0d76d3669f259cdfcc93444ba4a178f019'
 
   # d1efqjhnhbvc57.cloudfront.net was verified as official when first introduced to the cask
   url "https://d1efqjhnhbvc57.cloudfront.net/AnkaFlow-#{version}.pkg",
       referer: 'https://veertu.com/download-anka-run/'
   appcast 'https://ankadoc.bitbucket.io/release-notes/index.html',
-          checkpoint: '2016e6da466725f424883294813c44ed959b04ce25f9e32737c3ecec58beb1ab'
+          checkpoint: 'e138f8dce8d0d13c635499a4ee797a6b543327b87a2715763c85097bc83848db'
   name 'Veertu Anka Flow'
   homepage 'https://veertu.com/'
 
