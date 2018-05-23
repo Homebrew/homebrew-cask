20556d517be9b4a251638b50b4074851d6bdaf40
Jeff Tougas <j@osmium.local>
2018-05-23 19:03:39 -0400

    Update microsoft-teams to 1.1.00.11551

--- Casks/microsoft-teams.rb
+++ Casks/microsoft-teams.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-teams' do
   version '1.1.00.11551'
-  sha256 '971b4cf18b4f8d8578d0e762b489ccf0a8f999174c0e2d4d2df11ee7516a0980'
+  sha256 '5239931c11303c89c10646600b01a847581f6e5d21974c847e29d04475cbcd93'
 
   url "https://statics.teams.microsoft.com/production-osx/#{version}/Teams_osx.dmg"
   appcast 'https://teams.microsoft.com/downloads/DesktopUrl?env=production&plat=osx',
