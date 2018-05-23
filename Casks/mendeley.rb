f6bfe81394d666c1b3f5e51e0d6e10f28858b488
Brian Morton <brianmortondesign@gmail.com>
2018-05-23 16:44:21 -0700

    Update mendeley to 1.19

--- Casks/mendeley.rb
+++ Casks/mendeley.rb
@@ -1,8 +1,10 @@
 cask 'mendeley' do
-  version '1.18'
-  sha256 '4c0cb453e131949fc56bb84275964fa1ed29490deb577b063f8cb89d2fd3dcc2'
+  version '1.19'
+  sha256 '6c9f79d5f9b1385fed8daa7a65c137e8a8c67a7b3ad9552956ea0ce723d59fe6'
 
   url "https://desktop-download.mendeley.com/download/Mendeley-Desktop-#{version}-OSX-Universal.dmg"
+  appcast 'https://www.mendeley.com/release-notes',
+          checkpoint: 'd57c3cd17af890a82b0f4342fcd6fabcb42f5b4ba1e701b1d8f66dcdcfcd36ec'
   name 'Mendeley'
   homepage 'https://www.mendeley.com/'
 
