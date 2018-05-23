278124a63ddecdaa359f962e5164a3b224fb7535
Jörg Eichhorn <jeichhorn@me.com>
2018-05-23 14:08:23 +0200

    Update yourkit-java-profiler to 2018.04-b70 (#47501)

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask 'yourkit-java-profiler' do
-  version '2018.04-b66'
-  sha256 'e659ca97a623477ffee668ec52c48279fab8c09399dadc339d51649b7e2f03fe'
+  version '2018.04-b70'
+  sha256 'dc6f91da7b47f530126d5e84217e6121e52231c18603a2d2fb5f297c9d528087'
 
   url "https://www.yourkit.com/download/YourKit-JavaProfiler-#{version}-mac.zip"
   name 'YourKit Java Profiler'
