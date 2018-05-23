3577989e329e1fed8ad425540db38a4af2c70898
liangfei <liangfei@360.cn>
2018-05-24 00:14:55 +0800

    Add gcc-arm-embedded@2015q1.rb && java@8.rb

---
+++ Casks/gcc-arm-embedded@2015q1.rb
@@ -0,0 +1,35 @@
+cask 'gcc-arm-embedded@2015q1' do
+  version '4_9-2015q1,20150306'
+  sha256 '049469f06c35d1a7c6d333236bc62ed340531c4377b4e16ce8c5aa8d134e8e8b'
+
+  url "https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-update/+download/gcc-arm-none-eabi-#{version.before_comma}-#{version.after_comma}-mac.tar.bz2"
+  name "GCC ARM Embedded"
+  homepage "https://launchpad.net/gcc-arm-embedded"
+
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-addr2line"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-ar"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-as"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-c++"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-c++filt"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-cpp"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-elfedit"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-g++"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-gcc"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-gcc-ar"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-gcc-nm"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-gcc-ranlib"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-gcov"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-gdb"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-gdb-py"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-gprof"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-ld"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-ld.bfd"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-nm"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-objcopy"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-objdump"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-ranlib"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-readelf"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-size"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-strings"
+  binary "gcc-arm-none-eabi-#{version.before_comma}/bin/arm-none-eabi-strip"
+end
