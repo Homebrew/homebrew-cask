57c4231deb5742b2a67b6195b9cbb340d526334c
commitay <commitay@users.noreply.github.com>
2018-05-23 17:00:27 +1000

    Update brook.rb

--- Casks/brook.rb
+++ Casks/brook.rb
@@ -3,8 +3,8 @@ cask 'brook' do
   sha256 '418ed03948df31c6b4d159690e3fe53040ce2b1b302ec482a3def654f86cb511'
 
   url "https://github.com/txthinking/brook/releases/download/v#{version}/Brook.dmg"
-  appcast 'https://github.com/fitztrev/shuttle/releases.atom',
-          checkpoint: '1b3e4ff1820c5d0c0d06e1950efd661fabc84c5aba1db9b661b7fd05ac0a0889'
+  appcast 'https://github.com/txthinking/brook/releases.atom',
+          checkpoint: '8ae3ef839fec9d5416139ac26a9796694cccbc334f9ecc8dde4919a3074bc547'
   name 'Brook'
   homepage 'https://github.com/txthinking/brook'
 
