a20854bbaf9251a90b814a9d1207984bb5fec0d9
Summit Suen <suensummit@users.noreply.github.com>
2018-05-23 16:18:25 +0800

    Update teamsql to 3.3.203 (#47495)

--- Casks/teamsql.rb
+++ Casks/teamsql.rb
@@ -1,6 +1,6 @@
 cask 'teamsql' do
-  version '3.2.195'
-  sha256 'e4c0a92cba73c5c50f977a13538674ce8ca6af591d8e456fcbbc22c5611448b8'
+  version '3.3.203'
+  sha256 'd13e00db85ab62b38ef3a8122d757b252a780ab12315922532e26ce1036a29bb'
 
   # dlpuop5av9e02.cloudfront.net/osx/stable was verified as official when first introduced to the cask
   url "https://dlpuop5av9e02.cloudfront.net/osx/stable/#{version}/TeamSQL-#{version}.dmg"
