bb98c92ea0f1041d422cd982b244d62ce33bed40
commitay <commitay@users.noreply.github.com>
2018-05-23 21:31:17 +1000

    Update photo-supreme-postgresql.rb

--- Casks/photo-supreme-postgresql.rb
+++ Casks/photo-supreme-postgresql.rb
@@ -1,14 +1,14 @@
 cask 'photo-supreme-postgresql' do
-  version '4.1.0'
-  sha256 '0439f51291f4ae9778f2b41b46dbfd229515bb36d4cce66cbdc7ae8b0224336f'
+  version '4'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "http://trial.idimager.com/PhotoSupremePostgreSQL_V#{version.major}.pkg"
+  url "http://trial.idimager.com/PhotoSupremePostgreSQL_V#{version}.pkg"
   name 'Photo Supreme with PostreSQL'
   homepage 'http://www.idimager.com/WP/?page_id=20'
 
   depends_on formula: 'postgresql'
 
-  pkg "PhotoSupremePostgreSQL_V#{version.major}.pkg"
+  pkg "PhotoSupremePostgreSQL_V#{version}.pkg"
 
   uninstall pkgutil: 'com.idimager.idimagersu'
 end
