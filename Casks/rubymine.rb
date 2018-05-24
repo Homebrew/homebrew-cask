bfafc19252c65b124f0a96903729510f93c5c291
jcbot <jcb@leipert.io>
2018-05-24 11:26:28 +0200

    Update rubymine to 2018.1.3,181.4892.67 (#47537)

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -1,10 +1,10 @@
 cask 'rubymine' do
-  version '2018.1.2,181.4668.84'
-  sha256 'a91ab0160863c7860356122840e69dabbc9448afc1ddd7dc31c211cfbde2fef7'
+  version '2018.1.3,181.4892.67'
+  sha256 'e7800ae4a5e6b21e9363c89a6cd513f926fe17e38e11a9dbb5fb69f9f9e2fe45'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=RM&latest=true&type=release',
-          checkpoint: 'e5b9dd256965b485ca45b5b42bb51553ecc14636b30ca1f29a2fd5a22a087149'
+          checkpoint: '9e6636654a2a7923abc2b73834d7b61b7f1b73ab97b3fcb52f18081d8d1dce45'
   name 'RubyMine'
   homepage 'https://www.jetbrains.com/ruby/'
 
