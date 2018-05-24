107cd442835a1e3c94cce49f2431ae32492c569b
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2018-05-24 17:28:48 -0600

    Update 1password to 7.0.1 (#47563)

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -1,11 +1,11 @@
 cask '1password' do
-  version '7.0'
-  sha256 '9c79de6b4ff9dadc7476cd140156087bd9ad12d66de0b7b05dbcb0a2f660d7ec'
+  version '7.0.1'
+  sha256 '5fa46bbff517cf5f8d6e795003b7832c2fe14f986bd58e6c9e371d24a8779628'
 
   # 1password.com was verified as official when first introduced to the cask
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}",
-          checkpoint: '8e429ed32182657fe4b490486947246d91251853f0f39778d0976b2f8b3ba573'
+          checkpoint: '6299d3c947f2e45f15b7a806aaf1bb99c8b938b4b2d1485e77fbb5d21d48e889'
   name '1Password'
   homepage 'https://1password.com/'
 
