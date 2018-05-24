0ce81c59cceaa4c2d5419556e030c9026b9622af
Anver Housseini <contact@anver.ch>
2018-05-24 20:05:11 +0200

    Update airdroid to 3.6.3.0

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -1,6 +1,6 @@
 cask 'airdroid' do
-  version '3.6.2.0'
-  sha256 '0370fe1c2946c7edd442040e0c2699b01f5e814216d7fdf86164153607ffb6ff'
+  version '3.6.3.0'
+  sha256 '3d23d7e711243322ed545badd60e6e1ca40d0ca97cb435fb281c60cbf7ec56c9'
 
   # s3.amazonaws.com/dl.airdroid.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
