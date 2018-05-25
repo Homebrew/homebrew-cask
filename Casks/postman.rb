ea4b5cccc0fae91386554d64b9c584d3e55c733f
may-better <34537400+may-better@users.noreply.github.com>
2018-05-25 18:33:42 +0800

    Update postman to 6.0.10 (#47582)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,6 +1,6 @@
 cask 'postman' do
-  version '6.1.2'
-  sha256 '6b468d1a902f53948157ee1dfd87927986c9375f309e0885579aa17b924b6a89'
+  version '6.0.10'
+  sha256 '023705ccd601471a3e3560bf9ef5d850cebccb787ad2f4c342aed71bb27b0152'
 
   # dl.pstmn.io/download/version/ was verified as official when first introduced to the cask
   url "https://dl.pstmn.io/download/version/#{version}/osx64"
