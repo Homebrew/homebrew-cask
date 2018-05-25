e6d8df201f71b780cf9804d6186104cacc1a3c41
linbo0518 <linbo0518@gmail.com>
2018-05-26 00:11:13 +0800

    Update pdf-expert to 2.4.2,511

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,11 +1,11 @@
 cask 'pdf-expert' do
-  version '2.4,511'
+  version '2.4.2,511'
   sha256 '22026d0de90b25b1bf04d06e51bfd95e6a1b5388cd839a69a8144aac17a5f2de'
 
   # readdle-test-binaries.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://readdle-test-binaries.s3.amazonaws.com/versions/#{version.after_comma}/PDFExpert.dmg"
   appcast 'https://readdle-test-binaries.s3.amazonaws.com/release/appcast.xml',
-          checkpoint: '04f046b8f417005d3fe57468fd5ab4a1e8a574d13753e355cba710cc26ddafc2'
+          checkpoint: '4376404cf45c6b5a80fb6c256cc45903ece6c0c8911d0bea56dc3f569f302b12'
   name 'PDF Expert'
   homepage 'https://pdfexpert.com/'
 
