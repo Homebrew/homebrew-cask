7e3a0229a9cb85990445429554ce72db70cf8803
nrlquaker <ustynov.vy@gmail.com>
2018-05-25 16:32:30 +0300

    Update zap stanza in 1password

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -15,9 +15,16 @@ cask '1password' do
   app "1Password #{version.major}.app"
 
   zap trash: [
-               '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
-               '~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
-               '~/Library/Containers/com.agilebits.onepassword-osx',
+               '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword7-helper',
+               '~/Library/Application Scripts/com.agilebits.onepassword7',
+               '~/Library/Application Scripts/com.agilebits.onepassword7-launcher',
+               '~/Library/Application Scripts/com.agilebits.onepasswordnativemessaginghost',
+               '~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword7-helper',
+               '~/Library/Containers/com.agilebits.onepassword7',
+               '~/Library/Containers/com.agilebits.onepassword7-launcher',
+               '~/Library/Containers/com.agilebits.onepasswordnativemessaginghost',
                '~/Library/Group Containers/2BUA8C4S2C.com.agilebits',
+               '~/Library/Logs/1Password',
+               '~/Library/Preferences/com.agilebits.onepassword7.plist',
              ]
 end
