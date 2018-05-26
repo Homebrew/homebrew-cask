0106e1fe9e0fcb442d1b702801e98cb08de9c2e5
Khaldoon Mutahar <khal@mutahar.me>
2018-05-27 07:40:13 +0800

    Update daisydisk to 4.6.2

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -8,11 +8,11 @@ cask 'daisydisk' do
     sha256 'fe2aa86f2ea8a1f0c4791857a5b7991ecad295b5b969849bb7b15a890ab54b86'
     url "https://www.daisydiskapp.com/downloads/DaisyDisk_#{version.dots_to_underscores}.zip"
   else
-    version '4.6.1'
-    sha256 'ce78b6a898b7d9857e7ad3fc66d678676c1a8f8a225079cc5ec7cdd3b4206f8a'
+    version '4.6.2'
+    sha256 'c3c3a7c6fea1c9ee72b5b7f8e6257b281aa1530f2219e33705af6d7d37d4e271'
     url 'https://www.daisydiskapp.com/downloads/DaisyDisk.zip'
     appcast 'https://daisydiskapp.com/downloads/appcastReleaseNotes.php?appEdition=Standard&osVersion=10.13',
-            checkpoint: 'ec8f5270b8debf67c084046b20fd9f910429f0eb0f0b591b977912c5419f42c4'
+            checkpoint: '1f53f48b4a5cc971e6bfa8ce91754aaedfca22a6c17a4810738e5dd5bb8f20ac'
   end
 
   name 'DaisyDisk'
