e0a7fbd19dcafea3e3e3bf5218f93f5a4d109b93
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-26 21:46:25 +0100

    Update mimolive to 4.1-25475 (#47677)

--- Casks/mimolive.rb
+++ Casks/mimolive.rb
@@ -1,10 +1,10 @@
 cask 'mimolive' do
-  version '4.0.3-24873'
-  sha256 'c5cbfb91bc3dc9554a952feed0a1596c0bd0479e2f8142dc77b8e4a46e067de5'
+  version '4.1-25475'
+  sha256 '80deae64507c7f6ee3e3ab3a907470de9bf499e4acae85671d1957068116cad4'
 
   url "https://cdn.boinx.com/software/mimolive/Boinx_mimoLive_#{version}.app.zip"
   appcast 'https://boinx.com/d/connect/histories/mimolive',
-          checkpoint: '609b33258f108282ae38fcac40662c0fbc09eca0f0b38df2a5181b002d505fbe'
+          checkpoint: '111086d7d4590aff9dcba3892e12312d01ab596769338bd3c3a49a8590596d57'
   name 'mimoLive'
   homepage 'https://boinx.com/mimolive/'
 
