7f5402a5f4bb1df3e48054a9407c803cd92ec10d
tiiiecherle <tiiiecherle@gmail.com>
2018-05-26 17:11:25 +0200

    Update jameica to 2.8.0 (#47619)

--- Casks/jameica.rb
+++ Casks/jameica.rb
@@ -1,8 +1,8 @@
 cask 'jameica' do
-  version :latest
-  sha256 :no_check
+  version '2.8.0'
+  sha256 '9aeb721ec258f363d4f63d9de156db95af18d3c4aaa1e9f08c4279d68576a375'
 
-  url 'https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-macos64.zip'
+  url "https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-macos64-#{version}.zip"
   name 'Jameica'
   homepage 'https://www.willuhn.de/products/jameica/'
   gpg "#{url}.asc", key_id: '5a8ed9cfc0db6c70'
