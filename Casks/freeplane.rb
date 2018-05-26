a54e0379ae1fa2cb208e32fc874826806daf3878
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-26 18:53:18 +0100

    Update freeplane to 1.6.15

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -1,10 +1,10 @@
 cask 'freeplane' do
-  version '1.6.14'
-  sha256 'e3449f844ed157bcc58ea8082b75c114085aa9802320b2c1fee398c10c5d276e'
+  version '1.6.15'
+  sha256 '79e506a9a6cf3f29471aeaba576c37998c251a3647eb3d9db84c014b1a00a784'
 
   url "https://downloads.sourceforge.net/freeplane/freeplane%20stable/freeplane_app_jre-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/freeplane/rss?path=/freeplane%20stable',
-          checkpoint: '7e7e3edcb806732cf8519efc7cf76f5b3a6cd416886f281785f79a4ebcb86b72'
+          checkpoint: '1711314eca57f6d0ef2155f0e1e5f6630ea627520ddcb8825bc5737d50fb531c'
   name 'Freeplane'
   homepage 'http://freeplane.sourceforge.net/'
 
