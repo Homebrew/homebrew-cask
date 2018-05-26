244a483a3f94dab9dc9bc309ab55bc9a1f6f03b8
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-26 17:25:32 +0100

    Update annotation-edit to 1.9.95

--- Casks/annotation-edit.rb
+++ Casks/annotation-edit.rb
@@ -1,10 +1,10 @@
 cask 'annotation-edit' do
-  version '1.9.94'
-  sha256 '7ce5666636f950c6720d2c949337bfeb408769aadd2a9c54b510bd1dafce3cfe'
+  version '1.9.95'
+  sha256 '415ffcd9d4b5e6ecec1efe708a222b7ac6523ed868199059703e325f889d8a23'
 
   url 'http://www.zeitanker.com/common/Annotation_Edit.zip'
   appcast 'http://zeitanker.com/updates.rss',
-          checkpoint: 'cf288c041abf8edd02494b48f2ff6ff9135bc85bae84b630baf36dd12c5406f7'
+          checkpoint: 'c18d91e803298f59dfa72487915d6169188b23e0404329a2d409c65d2c4f657e'
   name 'Annotation Edit'
   homepage 'http://www.zeitanker.com/content/tools/zeitanker_tools/zeitanker_annotation_edit/'
 
