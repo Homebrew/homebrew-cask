8fab91491aac4a3f64c2376f06cc6bb6e56be5f0
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-26 20:12:54 +0100

    Update loginputmac to 1.13,2729

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,11 +1,11 @@
 cask 'loginputmac' do
-  version '1.12,2716'
-  sha256 'e825de66afa84ac9097a96b8e0e462bff5d12685097928ea67df5b793be7935a'
+  version '1.13,2729'
+  sha256 '9b61f528abfca0e3dfb638a72f759a57fa8748203333ca07d9fa7da184828edc'
 
   # nzhm461a0.qnssl.com was verified as official when first introduced to the cask
   url "https://nzhm461a0.qnssl.com/LogInputMac#{version.after_comma}.app.zip"
   appcast 'https://im.logcg.com/appcast.xml',
-          checkpoint: '214ca756c6a79f1aae382bcb58e9828a3768158a4afab3c5a0f320dde0a6ad78'
+          checkpoint: '94ba58615844690fc7f537d36c1960c1bae27bb74b6d49c2ed1de7c5dbdc1c52'
   name 'LoginputMac'
   homepage 'https://im.logcg.com/loginputmac'
 
