2d92059b554253fd90156786a252563faaf870a0
Bill Xiong <bill@billxiong.com>
2018-05-27 13:03:02 -0700

    Update tyke.rb
    Tyke cask updated to reflect new domain change.
    Tyke.io -> Tyke.app

--- Casks/tyke.rb
+++ Casks/tyke.rb
@@ -2,9 +2,9 @@ cask 'tyke' do
   version :latest
   sha256 :no_check
 
-  url 'http://tyke.io/tyke.dmg'
+  url 'http://tyke.app/tyke.dmg'
   name 'Tyke'
-  homepage 'http://tyke.io/'
+  homepage 'http://tyke.app/'
 
   app 'tyke.app'
 
