12bb004de292a1eecfba94e0a263b79efe2eb2d2
suschizu <brigitte@corecode.io>
2018-05-27 14:42:16 +0200

    Update postbox to 6.0.10,1_9d44288fe2fa5ec561b1ffc601c179224ff7ae7a (#47742)

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -1,6 +1,6 @@
 cask 'postbox' do
-  version '6.0.8,1_2a08007f8e141ca2b84e20ac7f7ce60203b04054'
-  sha256 'ddd3ae10cf65bbcd2d6e9441e1e95294a05762569695a44633bd2a7489e67d52'
+  version '6.0.10,1_9d44288fe2fa5ec561b1ffc601c179224ff7ae7a'
+  sha256 'fed5e71c083d09fa2f43e2a0a9140929ca8c1fb776895ad385f1d73820a343e6'
 
   # amazonaws.com/download.getpostbox.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/download.getpostbox.com/installers/#{version.before_comma}/#{version.after_comma}/postbox-#{version.before_comma}-mac64.dmg"
