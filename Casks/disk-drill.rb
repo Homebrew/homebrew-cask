b5cfaaab3ede73586fb3b272ec42cb96459278bf
Work <>
2018-05-27 13:48:32 +0200

    Update disk-drill to 3.5.890

--- Casks/disk-drill.rb
+++ Casks/disk-drill.rb
@@ -1,10 +1,10 @@
 cask 'disk-drill' do
-  version '3.5.883'
-  sha256 '1eb24662f729f29c5b21210e24d7c6b55e182c5e2a53b6b2bed02e8573f52746'
+  version '3.5.890'
+  sha256 'e8bc3e2235aa0f566aa6834f6f10eeae2816dcdc600c910812ac25c611dc7862'
 
   url "https://www.cleverfiles.com/releases/DiskDrill_#{version}.zip"
   appcast 'https://www.cleverfiles.com/releases/auto-update/dd2-newestr.xml',
-          checkpoint: 'beaf6374680821cbeecc577846cf2e2e7816bacf8912cb9811567beab21434f0'
+          checkpoint: 'c1386c42735736c7bea41d599cc7af26a93df1ec462133951e2684186d5ffaa6'
   name 'Disk Drill'
   homepage 'https://www.cleverfiles.com/'
 
