6f47fa6387aef7972da56a89df033a6480587681
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-27 23:00:21 +0100

    Update cashculator to 1.4

--- Casks/cashculator.rb
+++ Casks/cashculator.rb
@@ -1,10 +1,10 @@
 cask 'cashculator' do
-  version '1.3.8'
-  sha256 'bc16aa9de05f7308b42999eaaba7511128780fa0f2ddc65a3578c4c9a9802d82'
+  version '1.4'
+  sha256 'ea5a1754909d84f725703c58f8444581ab321cd647af232458d10173c1856c36'
 
   url "https://dl.apparentsoft.com/Cashculator_#{version}.dmg"
   appcast 'https://dl.apparentsoft.com/cashculator.rss',
-          checkpoint: 'cd773b3b4fb0a7e4ff186f877d924ecdaac5ddfb3915f566d8af6aa9e71daab9'
+          checkpoint: '6bfdbcf528cae6963f77b96c5e50900865ff6641031440338b586137ff1749ca'
   name 'Cashculator'
   homepage 'https://www.apparentsoft.com/cashculator'
 
