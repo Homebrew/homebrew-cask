b3d777685561fb9ea5f1a10c02391aaa34a9778f
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-27 01:33:30 +0100

    Update timings to 3.2.3 (#47699)

--- Casks/timings.rb
+++ Casks/timings.rb
@@ -1,10 +1,10 @@
 cask 'timings' do
-  version '3.2.1'
-  sha256 '383a46f6610843bd2d0492214f79111662e25767ca800a79e65f02b72b9904a0'
+  version '3.2.3'
+  sha256 '284f9633808170fb630ce1a57482a7a580fecc4575933f9671f570e44d587672'
 
   url "https://mediaatelier.com/Timings#{version.major}/Timings_#{version}.zip"
   appcast "https://mediaatelier.com/Timings#{version.major}/feed.php",
-          checkpoint: '7630185bb7d021568437dc4c35ae01ab4afd1dc812432db315772f11384148b4'
+          checkpoint: '7073236fae20b3db2e6b18e3d696782c36d2b3d97f1575aca9e91e0b7d2d4fb7'
   name 'Timings'
   homepage 'https://www.mediaatelier.com/Timings3/'
 
