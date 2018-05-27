103f6f2772232dd2f4d7fbce4ea0dad3e9c378e4
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-27 01:40:16 +0100

    Update visual-paradigm to 15.0,20180501 (#47719)

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,10 +1,10 @@
 cask 'visual-paradigm' do
-  version '15.0,20180404'
-  sha256 '60ce4c5abc0de73d1b650a38ee966bbc3896d89fc4b7f7df4df0ce8a64fce9a1'
+  version '15.0,20180501'
+  sha256 'a8f57d3cafad34d7f70b1acb7537a6e7c680ffd415581b419698fe1a77020c05'
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vp/checksum.html',
-          checkpoint: 'bbd335e9f3e07c9698690d45cbaf30b3f51250e8db3b83e5f854888ff33b9d27'
+          checkpoint: 'db4074c50a7bf853376cc2ada3e710c6e9ae3cd1f22156790f1a36662fe839e7'
   name 'Visual Paradigm'
   homepage 'https://www.visual-paradigm.com/'
 
