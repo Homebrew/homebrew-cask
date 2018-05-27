6b15ac5d97b95fe888f31a8c8ab697d5cf5810b0
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-27 19:36:09 +0100

    Update razorsql to 8.0.0 (#47761)

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,10 +1,10 @@
 cask 'razorsql' do
-  version '7.4.10'
-  sha256 '201df9ccbbbe3e5347c474986ee456b58971f41e0b5ba8b77d7a5cee3db8b465'
+  version '8.0.0'
+  sha256 '4d79f2699acc08bb0055035ca097f2a96ff591ac4ef3ae255a88c4380c516215'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   appcast 'https://razorsql.com/updates.html',
-          checkpoint: '09a9047917ed8dcd6ec67979f126263097c1a46b723bf5deac21047f8ea6cac5'
+          checkpoint: 'ef8dbb2103fc5e7288e924dfa6fee693992dc158f6d347d75cd75710f3eae153'
   name 'RazorSQL'
   homepage 'https://razorsql.com/download_mac.html'
 
