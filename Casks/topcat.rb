c3983b7f5e9718b3d72a7043832705eb5752535f
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-27 02:13:01 +0100

    Update topcat to 4.6-1 (#47722)

--- Casks/topcat.rb
+++ Casks/topcat.rb
@@ -1,10 +1,10 @@
 cask 'topcat' do
-  version '4.6'
-  sha256 '343da1a5ad080859a4dda671c059fb8cd33d83c68cee06128a48aa76fcb503de'
+  version '4.6-1'
+  sha256 'd26dcdec120d1498a4d97140396b93dd1ea7738364fcfa959e66a9e3201cadf2'
 
   url 'http://www.star.bris.ac.uk/~mbt/topcat/topcat-full.dmg'
   appcast 'http://www.star.bris.ac.uk/~mbt/topcat/sun253/versions.html',
-          checkpoint: '48c09ca1e2fb4c0d5c6804fa220e936e1b2a66e14d61fd548bd9ecfb888a6942'
+          checkpoint: '4705495334d6bdb7df6633ae2e60becaadd8aa0e9de266c74691dc1f1a3add73'
   name 'TOPCAT'
   homepage 'http://www.star.bris.ac.uk/~mbt/topcat/'
 
