4ea27e4c8135797d49d46309fa7afc792779a8ca
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2018-05-27 11:56:41 +0300

    Update authy to 1.6.0
    Updated to 1.6.0

--- Casks/authy.rb
+++ Casks/authy.rb
@@ -1,9 +1,10 @@
 cask 'authy' do
-  version '1.4.0'
-  sha256 'dc9e0263110319ab9bdfc40c263292c490e7469d4a2f9226a1e365b6529850f0'
+  version '1.6.0'
+  sha256 'd8d4fe46e0e3cf39c3296e0c97d626cdfebc4be5e8eb141c1bca14e8eaf2cfb5'
 
   # s3.amazonaws.com/authy-electron-repository-production was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/authy-electron-repository-production/authy/stable/#{version}/darwin/x64/authy-installer.dmg"
+  url "https://s3.amazonaws.com/authy-electron-repository-production/authy/stable/#{version}/darwin/x64/Authy%20Desktop-#{version}.dmg"
+  
   name 'Authy Desktop'
   homepage 'https://authy.com/'
 
