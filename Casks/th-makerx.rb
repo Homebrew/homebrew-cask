98a7acd0106fef39dbaf905396bb99fa39be0922
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-28 16:31:40 +0100

    Update th-makerx.rb

--- Casks/th-makerx.rb
+++ Casks/th-makerx.rb
@@ -3,6 +3,8 @@ cask 'th-makerx' do
   sha256 '19a3141106beb377d5e00dff3a9c092f4e8aa68048fa30460824e944ebc2d797'
 
   url "http://www5.wind.ne.jp/miko/mac_soft/th-maker_x/hqx/Th-MakerX_#{version.no_dots}.zip"
+  appcast 'http://www5.wind.ne.jp/miko/mac_soft/th-maker_x/index-en.html',
+          checkpoint: '398404ea50e15570ddea6de130d98234c4790e5c44ef196d23f4438a761094c8'
   name 'Th-MakerX'
   homepage 'http://www5.wind.ne.jp/miko/mac_soft/th-maker_x/index-en.html'
 
