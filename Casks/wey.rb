747326577de483268bad74d84103e67fea743a0d
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-28 16:59:18 +0100

    Update wey to 0.3.0

--- Casks/wey.rb
+++ Casks/wey.rb
@@ -1,10 +1,10 @@
 cask 'wey' do
-  version '0.2.3'
-  sha256 'e3c14444fee3e04ef0f8a0808aa1cb3607f62dfba0f8aaa0edcf8b80fdd021cd'
+  version '0.3.0'
+  sha256 'b0d596aa92b0eb40181fba9201ed7604d1df3b0d6ce62d8e913cd1e99e1dd966'
 
   url "https://github.com/yue/wey/releases/download/v#{version}/wey-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/yue/wey/releases.atom',
-          checkpoint: 'bab5a35194352575519d4b99ffabab5c0fa1148ab25aefce3bb4e404fe148ab4'
+          checkpoint: '1030d04b556eb389d330ae05cb127cc6b49b6d1c96a3cd9d0764391c85dbc0ed'
   name 'Wey'
   homepage 'https://github.com/yue/wey'
 
