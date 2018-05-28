9b6bd76a511ecf8732d46f4e7f85e4ab5b325b95
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-28 14:59:04 +0100

    Update portfolioperformance to 0.31.2 (#47796)

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,11 +1,11 @@
 cask 'portfolioperformance' do
-  version '0.31.1'
-  sha256 '17c7f1c3ac81b29dad0a352ed3cfd6b8de929489e5f4d45d97f1d51b79da0cca'
+  version '0.31.2'
+  sha256 '91fa5edb63109b6b623d8fc10f8ff71423b2b68093e7b00b5b1cf0e9acee55ef'
 
   # bintray.com/buchen/downloads was verified as official when first introduced to the cask
   url "https://bintray.com/buchen/downloads/download_file?file_path=#{version}%2FPortfolioPerformance-#{version}-macosx.cocoa.x86_64.tar.gz"
   appcast 'https://github.com/buchen/portfolio/releases.atom',
-          checkpoint: '08089613cb9edd30652db4f0a2bc8eb91992ca58d9b413bad297a849f6a1a72c'
+          checkpoint: '58ef91436486941137add45e0e8a9172aab35db05c9b0192b6106047cb96aae7'
   name 'Portfolio Performance'
   homepage 'http://www.portfolio-performance.info/portfolio/'
 
