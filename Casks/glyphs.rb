7e9cded996c40a0b01f91319098a7066554e9f11
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-28 19:06:57 +0100

    Update glyphs to 2.5.2-1146 (#47816)

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,10 +1,10 @@
 cask 'glyphs' do
-  version '2.5.2-1143'
-  sha256 'e59513439318c0aa541f7d797de47330e9ccf220c1b33f1ea54b351b4cf0ce7f'
+  version '2.5.2-1146'
+  sha256 '624106132847fd3f1a81e9ae0b19d515a9c415cd5919a92eaf8926d6f203120e'
 
   url "https://updates.glyphsapp.com/Glyphs#{version}.zip"
   appcast "https://updates.glyphsapp.com/appcast#{version.major}.xml",
-          checkpoint: '67de019213d269f6fb99eaf9da04b9f6180780db1376382121bd428758348769'
+          checkpoint: '10743542cde70e57be302fdce5e7252731484d39f40e0ee4b6d73fd3699e845a'
   name 'Glyphs'
   homepage 'https://glyphsapp.com/'
 
