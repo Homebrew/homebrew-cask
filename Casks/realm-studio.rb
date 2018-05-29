39f9b3ee6aeb0609a05b5602ef5b07a7a7de3d14
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-29 16:23:10 +0100

    Update realm-studio to 2.0.0

--- Casks/realm-studio.rb
+++ Casks/realm-studio.rb
@@ -1,10 +1,10 @@
 cask 'realm-studio' do
-  version '1.21.1'
-  sha256 'f9ee8191d58095e1a56f1d815b1abbf0b57868aa3cd71b036790aa44ea4ed6fa'
+  version '2.0.0'
+  sha256 '7f63a44daf7a334eb32288f4868ea9d4a52976d7fa0dc71abe2ef8ad0581ec50'
 
   url "https://static.realm.io/downloads/realm-studio/Realm%20Studio-#{version}-mac.zip"
   appcast 'https://static.realm.io/downloads/realm-studio/latest-mac.yml',
-          checkpoint: 'ada7f7c1b7e73e6295d5f9172224908c9909eb2b30a8d7d10656b4d3cf9fc9e6'
+          checkpoint: '073d577c3b17104ce8a13700500105ff2706d027712e1e3b64dd6a26469210bb'
   name 'Realm Studio'
   homepage 'https://realm.io/products/realm-studio/'
 
