a5ec403b0604fe77aa29b0d03736ea992220a0db
Brian Morton <brianmortondesign@gmail.com>
2018-05-29 09:15:51 -0700

    Update focuswriter to 1.6.13 (#47849)

--- Casks/focuswriter.rb
+++ Casks/focuswriter.rb
@@ -1,6 +1,6 @@
 cask 'focuswriter' do
-  version '1.6.12'
-  sha256 'b7d920241a522d1781ee77c2560f71e70924d47a12c711a6342d6fd61d9a865f'
+  version '1.6.13'
+  sha256 '6312242d980029b86fa022197c00d5d044d244784454316337901013a700885c'
 
   url "https://gottcode.org/focuswriter/FocusWriter_#{version}.dmg"
   name 'FocusWriter'
