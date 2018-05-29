222cda3f291a8edce77f709753c358a368a43a06
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-29 16:29:38 +0100

    Update pycharm-edu to 2018.1.2,181.5087.43 (#47836)

--- Casks/pycharm-edu.rb
+++ Casks/pycharm-edu.rb
@@ -1,10 +1,10 @@
 cask 'pycharm-edu' do
-  version '2018.1.1,181.4445.100'
-  sha256 '69a7c08b97a8f26944805775a5f7f6e59c21ecdab29e4d34b30b9e0fe763c81a'
+  version '2018.1.2,181.5087.43'
+  sha256 '16e275118bad9e5efb0b1c2608ed108b980a4c08972019e66e27f79573f827d5'
 
   url "https://download.jetbrains.com/python/pycharm-edu-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=PCE&latest=true&type=release',
-          checkpoint: '560131f8e1cbeb5a5e3b767243169e0387deb2d8ade798cc4d9046ff3dad5afc'
+          checkpoint: 'b667f3e263ae31a0d9659ab5af02c666a0b6b78fcf790a798470086149c71046'
   name 'Jetbrains PyCharm Educational Edition'
   name 'PyCharm Edu'
   homepage 'https://www.jetbrains.com/pycharm-edu/'
