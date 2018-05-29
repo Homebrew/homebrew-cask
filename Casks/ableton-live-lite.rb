6340f679d81ddf051b09820cec6985237db836d5
Brian Morton <brianmortondesign@gmail.com>
2018-05-29 16:30:30 -0700

    Update ableton-live-lite to 10.0.2 (#47858)

--- Casks/ableton-live-lite.rb
+++ Casks/ableton-live-lite.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-lite' do
-  version '10.0.1'
-  sha256 'b4c0d7dfc4e05eddf1cab6a831962f8aad355fafc36c512112dc0e820a2f2c61'
+  version '10.0.2'
+  sha256 'e557de32d71c0b10c7eb1b5c8a699134f78e4298e496ba90a00684e6b815555d'
 
   url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_lite_#{version}_64.dmg"
   name 'Ableton Live Lite'
