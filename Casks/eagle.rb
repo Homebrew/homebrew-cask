124047e74b15a65576e13d496ce6cc530c7cc56f
commitay <commitay@users.noreply.github.com>
2018-05-30 08:40:11 +1000

    Update eagle to 9.0.1 (#47829)

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -1,10 +1,10 @@
 cask 'eagle' do
-  version '9.0.0'
-  sha256 'eeefc304222c0dd3f454c069d2d8da8a78b643bac6193fc9cf5bf3567bce95d0'
+  version '9.0.1'
+  sha256 '7b3eff937b72e34c375e07593e18bd22b09070d2eb8b3b711c20e3d171484a4e'
 
   url "https://eagle-updates.circuits.io/downloads/#{version.dots_to_underscores}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   name 'Autodesk EAGLE'
-  homepage 'https://circuits.io/eagle/'
+  homepage 'https://circuits.io/'
 
   pkg "Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
 
