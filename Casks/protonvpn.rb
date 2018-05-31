9a5aa0e0c1b96d712e5c3e772c422e9938ae679c
Robert Patchett <rob.patchett@yahoo.co.nz>
2018-05-31 11:30:43 +0200

    Update protonvpn to 1.0.0

--- Casks/protonvpn.rb
+++ Casks/protonvpn.rb
@@ -1,10 +1,10 @@
 cask 'protonvpn' do
   version '1.0.0'
-  sha256 '032d95003b2fb3c121a41dda736d4bc785ab6101eda65fb9e0b46c49487f6741'
+  sha256 '040da07ec2440a965cb2d1ce351c9041c3a407c16784c1ea1a63e0f47a57b050'
 
-  url "https://protonvpn.com/download/ProtonVPNv#{version.major_minor}.dmg"
+  url "https://protonvpn.com/download/ProtonVPN_mac_v#{version.major_minor_patch}.dmg"
   appcast 'https://protonvpn.com/download/macos-update.xml',
-          checkpoint: 'ea73947ab6726ad2116d95fe2c968a41d0b459b8fdaf8dc4446cfb30840c4857'
+          checkpoint: '37eedcd7ae33c52c18919e298fe6dc0c10b1ac6582129b0838b5e71284804e69'
   name 'ProtonVPN'
   homepage 'https://protonvpn.com/'
 
