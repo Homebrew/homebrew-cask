d49d660c25641e2c92ade1fe59b903a31ba3eb9f
commitay <commitay@users.noreply.github.com>
2018-05-31 19:42:17 +1000

    Update sqleditor.rb

--- Casks/sqleditor.rb
+++ Casks/sqleditor.rb
@@ -3,8 +3,8 @@ cask 'sqleditor' do
   sha256 '82a5b35d0e0f9ddd3d8df302c01cb24633c0404fdb924984a6c7f8ef965de2e9'
 
   url "https://www.malcolmhardie.com/sqleditor/releases/#{version}/SQLEditor-#{version.dots_to_hyphens}.zip"
-  appcast 'https://www.malcolmhardie.com/sqleditor/appcast/sq2release.xml',
-          checkpoint: '34db2e166e7c91e9f6fca5fa787bcdb7065d124253871d62bd5ce9480c90af73'
+  appcast "https://www.malcolmhardie.com/sqleditor/appcast/sq#{version.major}release.xml",
+          checkpoint: 'ce37975658c7539f6c64e517a10e44ea1a9f9a125d1782f555353b40e24493bb'
   name 'SQLEditor'
   homepage 'https://www.malcolmhardie.com/sqleditor/'
 
