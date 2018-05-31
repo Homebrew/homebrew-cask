4517081f9bb248e397bbbc1b8311de79ac587e26
leandro <leandro@leandro.org>
2018-05-31 11:43:02 +0200

    Upgrade Therm to 0.1.6 (#47911)

--- Casks/therm.rb
+++ Casks/therm.rb
@@ -1,10 +1,10 @@
 cask 'therm' do
-  version '0.1.5'
-  sha256 '3c31a55acdf4b2ed769157edd5d7b4dc0a98baa2a2e8e91084236285cd6b5523'
+  version '0.1.6'
+  sha256 'd2666b8c60018e0656c1671dccd3cdc75f19f2586a8b283e2a83691bdb963f4d'
 
   url "https://github.com/trufae/Therm/releases/download/#{version}/Therm.app.zip"
   appcast 'https://github.com/trufae/Therm/releases.atom',
-          checkpoint: '98f72f9c91e5a999da03cfdb9ca3e417928f26fa2eee32526828e9f00a9d5c7a'
+          checkpoint: '075d9b956da87b0b46eb15d855cb275dd6b1e35faae9645c9663c0cf3d275fd5'
   name 'Therm'
   homepage 'https://github.com/trufae/Therm'
 
