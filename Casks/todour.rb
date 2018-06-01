a1f252af839ef8a52e99f43a8eafbc379720ba20
Brian Morton <brianmortondesign@gmail.com>
2018-05-31 18:05:44 -0700

    Update todour to 2.12 (#47947)

--- Casks/todour.rb
+++ Casks/todour.rb
@@ -1,8 +1,10 @@
 cask 'todour' do
-  version '2.10'
-  sha256 'e5f1fb8b4d115793155662937aa36fae99d78463e3b47990e0147610e83ab613'
+  version '2.12'
+  sha256 'df5452d5fc0f5c4d71ed6a3dd853b517ac0433c962daf300b599fea6f79b73c7'
 
   url "http://nerdur.com/Todour-#{version}.dmg"
+  appcast 'https://github.com/SverrirValgeirsson/Todour/releases.atom',
+          checkpoint: 'c530156342121c8cda479c5065fc7d4aae43199e7b1cb95d7241e9f705c27491'
   name 'Todour'
   homepage 'http://nerdur.com/todour-pl/'
 
