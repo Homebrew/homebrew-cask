38bc38f2975c76e67c6dc5af60ec96e48af8ca02
Brian Morton <brianmortondesign@gmail.com>
2018-06-01 13:32:56 -0700

    Update fontbase to 2.5.3 (#47967)

--- Casks/fontbase.rb
+++ Casks/fontbase.rb
@@ -1,6 +1,6 @@
 cask 'fontbase' do
-  version '2.5.0'
-  sha256 'eeb694d64b495fb59895b77dd6a4e030de2060a7ab29156cbe104eadf3ed54bf'
+  version '2.5.3'
+  sha256 '1007a23bb7d13812dc40de99b1032fbc1f900fd382838566609fe7d76fa3a9d7'
 
   url "http://releases.fontba.se/mac/FontBase-#{version}.dmg"
   name 'FontBase'
