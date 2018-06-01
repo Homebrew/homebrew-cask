fab67ee72f68f6b7cc82cff7d8317f66d66104c6
Brian Morton <brianmortondesign@gmail.com>
2018-05-31 18:14:57 -0700

    Update camunda-modeler to 1.15.1 (#47932)
    * Update camunda-modeler to 1.15.0
    
    * Update camunda-modeler to 1.15.1

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask 'camunda-modeler' do
-  version '1.15.0'
-  sha256 'a8584337402b7ec6ece0c170d5810bc51fc49fcfcaf8ae79cc43f40b95c0bd8f'
+  version '1.15.1'
+  sha256 'f382873005fa0306d3ac105f4cf9f35d64de1892eaa8e68ea7b1b8eb57416345'
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-darwin-x64.tar.gz"
   name 'Camunda Modeler'
