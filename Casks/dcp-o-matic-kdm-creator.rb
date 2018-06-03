3363b28b87dc4de8cc20293ff829555095d57f9c
Philip Molares <git@molar.es>
2018-06-03 15:51:42 +0200

    no more style errors

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -3,8 +3,10 @@ cask 'dcp-o-matic-kdm-creator' do
   sha256 'd5f757ca2d7419dbb84552cf1b50493a5d2e50a858cdbb9cc1d90eefb4c2b87d'
 
   url "https://dcpomatic.com/dl.php?id=osx-kdm&version=#{version}"
+  appcast 'https://dcpomatic.com/download',
+           checkpoint: '510616fe834b672a089e02a95abbb43f7bee5ba80793d30c4adfb48b8de1411d'
   name 'DCP-o-matic 2 KDM Creator'
   homepage 'https://dcpomatic.com/'
 
-  app 'DCP-o-matic 2 KDM Creator.app'
+  app "DCP-o-matic #{verson.major} KDM Creator.app"
 end
