49c715cd0f572cb344862e7ac88c4b7222a2f386
Julian Mayer <julian@corecode.at>
2018-06-03 19:36:36 +0200

    Update jaksta to 2.2.3

--- Casks/jaksta.rb
+++ Casks/jaksta.rb
@@ -1,12 +1,14 @@
 cask 'jaksta' do
-  version '1.3.3'
-  sha256 '8f54cdba4ab42aec0430cdc15f9fd62370391bd2c42e0b734d6d5adea0355f69'
+  version '2.2.3'
+  sha256 '92cb1eeff5360e0d2ac890556f92c1f0ada74c32209eea5e4eaa27ca3204cc7c'
 
-  url "http://downloads.jaksta.com/release/mac/Jaksta-#{version}.dmg"
-  appcast 'http://downloads.jaksta.com/release/mac/sparkle/JakstaforMac.xml',
-          checkpoint: '164a2b0a2fa866c475947fe2ae1e4a1020f11ca6ab03d5525bc54fc41d274be1'
+  url "https://www.jaksta.com/services/download/mac/jaksta-media-recorder/#{version}"
+  appcast 'https://www.jaksta.com/Services/VersionDirector/jmr-mac?v=2.2.0',
+          checkpoint: '40e7d49f541432521eff2f08a733d88034114b56ec3980f32914b9f40c8099d5'
   name 'Jaksta'
   homepage 'https://www.jaksta.com/products'
 
-  app 'Jaksta.app'
+  pkg "JMR-#{version}.pkg"
+
+  uninstall pkgutil: 'com.shedwork.Jaksta'
 end
