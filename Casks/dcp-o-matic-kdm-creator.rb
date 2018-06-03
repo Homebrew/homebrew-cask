7ec5aaab31f6155121fbc7dbd95a0efb6546e2de
Philip Molares <git@molar.es>
2018-06-03 13:38:20 +0200

    Added DCP-o-matic 2 KDM Creator.app Version 2.12.6

---
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -0,0 +1,10 @@
+cask 'dcp-o-matic-kdm-creator' do
+  version '2.12.6'
+  sha256 'd5f757ca2d7419dbb84552cf1b50493a5d2e50a858cdbb9cc1d90eefb4c2b87d'
+
+  url "https://dcpomatic.com/dl.php?id=osx-kdm&version=#{version}"
+  name 'DCP-o-matic 2 KDM Creator'
+  homepage 'https://dcpomatic.com/'
+
+  app 'DCP-o-matic 2 KDM Creator.app'
+end
