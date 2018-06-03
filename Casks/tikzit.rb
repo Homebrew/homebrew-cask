9013b443db03a4e589ff612e935cdb80b65ea093
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-06-03 15:05:33 +0100

    Update tikzit.rb

--- Casks/tikzit.rb
+++ Casks/tikzit.rb
@@ -3,7 +3,7 @@ cask 'tikzit' do
   sha256 '7e00a64fe5c96963ae5b09f1f59698b8b487239c53eed01a3bf012cf024f0686'
 
   # downloads.sourceforge.net/tikzit was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/tikzit/TikZiT-osx-#{version}.zip"
+  url "https://bintray.com/tikzit/tikzit/download_file?file_path=releases%2F#{version}%2FTikZiT-osx.zip"
   appcast 'https://github.com/tikzit/tikzit/releases.atom',
           checkpoint: '1609bfd62f4456d9c1f5b3eca7fe6db2c2ff71b1a81dbfe3d41f32b283608dcc'
   name 'TikZiT'
