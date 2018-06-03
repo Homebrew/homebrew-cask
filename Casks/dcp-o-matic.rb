ddefe594f4022cd1f0e796486873499d4fec64d1
Philip Molares <git@molar.es>
2018-06-03 15:40:58 +0200

    added appcast and removed version from name #48027

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -3,7 +3,9 @@ cask 'dcp-o-matic' do
   sha256 '4b3ae99dec3bbf5f348f58a47f4841b8272fc3fad8372e5def4f98ea1a9f0665'
 
   url "https://dcpomatic.com/dl.php?id=osx-main&version=#{version}"
-  name 'DCP-o-matic 2'
+  appcast 'https://dcpomatic.com/download',
+          checkpoint: '510616fe834b672a089e02a95abbb43f7bee5ba80793d30c4adfb48b8de1411d'
+  name 'DCP-o-matic'
   homepage 'https://dcpomatic.com/'
 
   app 'DCP-o-matic #{version.major}.app'
