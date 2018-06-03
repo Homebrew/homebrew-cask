0dffdfefa435e7177032d4d36e609d0dbf0f0a5a
Vítor Galvão <info@vitorgalvao.com>
2018-06-03 14:55:33 +0100

    Update anytrans-for-android.rb

--- Casks/anytrans-for-android.rb
+++ Casks/anytrans-for-android.rb
@@ -3,7 +3,7 @@ cask 'anytrans-for-android' do
   sha256 :no_check
 
   url 'http://dl.imobie.com/anytrans-android-mac.dmg'
-  name 'AnyTrans'
+  name 'AnyTrans for Android'
   homepage 'https://www.imobie.com/anytrans/android-manager.htm'
 
   app 'AnyTrans for Android.app'
