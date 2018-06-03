af6df6af4c9189a59475dfd23b40155d042bdeed
Philip Molares <git@molar.es>
2018-06-03 13:34:29 +0200

    Added DCP-o-matic 2 Encode Server.app Version 2.12.6

---
+++ Casks/dcp-o-matic-encode-server.rb
@@ -0,0 +1,10 @@
+cask 'dcp-o-matic-encode-server' do
+  version '2.12.6'
+  sha256 '1395c27621219665756420438542369d7dd5257ee7a5114ed05bb50939a84fd2'
+
+  url "https://dcpomatic.com/dl.php?id=osx-server&version=#{version}"
+  name 'DCP-o-matic 2 Encode Server'
+  homepage 'https://dcpomatic.com/'
+
+  app 'DCP-o-matic 2 Encode Server.app'
+end
