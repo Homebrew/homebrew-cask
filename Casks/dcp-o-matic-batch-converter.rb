2b1b107ceb29e24a202c8d4a175e336c61a68128
Philip Molares <git@molar.es>
2018-06-03 15:44:45 +0200

    fixed Lint/InterpolationCheck

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -4,7 +4,7 @@ cask 'dcp-o-matic-batch-converter' do
 
   url "https://dcpomatic.com/dl.php?id=osx-batch&version=#{version}"
   appcast 'https://dcpomatic.com/download',
-          checkpoint: '510616fe834b672a089e02a95abbb43f7bee5ba80793d30c4adfb48b8de1411d'
+          checkpoint: "510616fe834b672a089e02a95abbb43f7bee5ba80793d30c4adfb48b8de1411d"
   name 'DCP-o-matic Batch converter'
   homepage 'https://dcpomatic.com/'
 
