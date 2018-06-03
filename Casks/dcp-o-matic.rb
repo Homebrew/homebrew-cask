2e0f30e10bdd06d54835b3b2229746510c7a1bc7
Philip Molares <git@molar.es>
2018-06-03 13:24:31 +0200

    Added DCP-o-matic2.app Version 2.12.6

---
+++ Casks/dcp-o-matic.rb
@@ -0,0 +1,10 @@
+cask 'dcp-o-matic' do
+  version '2.12.6'
+  sha256 '4b3ae99dec3bbf5f348f58a47f4841b8272fc3fad8372e5def4f98ea1a9f0665'
+
+  url "https://dcpomatic.com/dl.php?id=osx-main&version=#{version}"
+  name 'DCP-o-matic 2'
+  homepage 'https://dcpomatic.com/'
+
+  app 'DCP-o-matic 2.app'
+end
