78f97e0fbed823af8cc4d95e2564c88240521bd4
Philip Molares <git@molar.es>
2018-06-03 15:42:59 +0200

    added changes from #48027

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -3,8 +3,10 @@ cask 'dcp-o-matic-batch-converter' do
   sha256 '6791be7a476c04f0aa838b446f6ef2f6d6f5c8e770ac8a3a77701d0409a6132d'
 
   url "https://dcpomatic.com/dl.php?id=osx-batch&version=#{version}"
-  name 'DCP-o-matic 2 Batch converter'
+  appcast 'https://dcpomatic.com/download',
+          checkpoint: '510616fe834b672a089e02a95abbb43f7bee5ba80793d30c4adfb48b8de1411d'
+  name 'DCP-o-matic Batch converter'
   homepage 'https://dcpomatic.com/'
 
-  app 'DCP-o-matic 2 Batch converter.app'
+  app 'DCP-o-matic #{version.major} Batch converter.app'
 end
