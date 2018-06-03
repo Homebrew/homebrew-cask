406bb184c60a6c3d92ae8867f9ed2dfe84419f06
Vítor Galvão <info@vitorgalvao.com>
2018-06-03 14:27:34 +0100

    Update dcp-o-matic-player.rb

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -3,8 +3,10 @@ cask 'dcp-o-matic-player' do
   sha256 '3cb9b1064875d5a595d9d1a0a79bf60145f81421580ad3573f4a5e4906c3fb11'
 
   url "https://dcpomatic.com/dl.php?id=osx-player&version=#{version}"
-  name 'DCP-o-matic 2 Player'
+  appcast 'https://dcpomatic.com/download',
+          checkpoint: '510616fe834b672a089e02a95abbb43f7bee5ba80793d30c4adfb48b8de1411d'
+  name 'DCP-o-matic Player'
   homepage 'https://dcpomatic.com/'
 
-  app 'DCP-o-matic 2 Player.app'
+  app "DCP-o-matic #{version} Player.app"
 end
