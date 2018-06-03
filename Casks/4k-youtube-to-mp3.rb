b169e26d7574a9d7eb4e83e302188283bb4c5250
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-06-03 14:50:22 +0100

    Update 4k-youtube-to-mp3 to 3.3.6.1809 (#48033)
    * Update 4k-youtube-to-mp3 to 3.3.6.1809
    
    * Update 4k-youtube-to-mp3.rb

--- Casks/4k-youtube-to-mp3.rb
+++ Casks/4k-youtube-to-mp3.rb
@@ -1,11 +1,11 @@
 cask '4k-youtube-to-mp3' do
   # note: "3" is not a version number, but an intrinsic part of the product name
-  version '3.3.5.1797'
-  sha256 '9d9becc84c4df834ab13c62ccf063d333da677bd0973738305bac230c1500b3c'
+  version '3.3.6.1809'
+  sha256 '3166bba028cd7aa98021bbc7616881289d67760624b9c7747709e517851f7ba3'
 
   url "https://dl.4kdownload.com/app/4kyoutubetomp3_#{version.major_minor_patch}.dmg"
   appcast 'https://www.4kdownload.com/download',
-          checkpoint: '6976352a19a6acbaf5953cb3db8d18a88d5258e9d05fd2a0d5018be457741204'
+          checkpoint: 'bc602309e88e5f0311dbad6bb4feaa37187e97aa2db120383b4e765347959bf8'
   name '4K YouTube to MP3'
   homepage 'https://www.4kdownload.com/products/product-youtubetomp3'
 
