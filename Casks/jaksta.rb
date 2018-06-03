2fe95d6b5f25a579b397cbe0272982b519b7f4a7
Vítor Galvão <info@vitorgalvao.com>
2018-06-03 19:34:39 +0100

    Update jaksta.rb

--- Casks/jaksta.rb
+++ Casks/jaksta.rb
@@ -3,8 +3,8 @@ cask 'jaksta' do
   sha256 '92cb1eeff5360e0d2ac890556f92c1f0ada74c32209eea5e4eaa27ca3204cc7c'
 
   url "https://www.jaksta.com/services/download/mac/jaksta-media-recorder/#{version}"
-  appcast 'https://www.jaksta.com/Services/VersionDirector/jmr-mac?v=2.2.0',
-          checkpoint: '40e7d49f541432521eff2f08a733d88034114b56ec3980f32914b9f40c8099d5'
+  appcast 'https://www.jaksta.com/Services/VersionDirector/jmr-mac',
+          checkpoint: '44dd59d7ec02995687685d56ed99e0607f197b1a3eb03724c61b74daec209c62'
   name 'Jaksta'
   homepage 'https://www.jaksta.com/products'
 
