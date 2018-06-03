7e8e85ccec5015e5865f437f3b892b8626f4ab95
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-06-03 14:09:02 +0100

    Update mountain to 1.6.5

--- Casks/mountain.rb
+++ Casks/mountain.rb
@@ -1,10 +1,10 @@
 cask 'mountain' do
-  version '1.6.4'
-  sha256 '7290dca93600e5c59cf797cae3f3bd874f2d9d79811969b9dad8586cf2a5c53f'
+  version '1.6.5'
+  sha256 '86140c5469683ed64e9f275240e4e5e157b4b84b39aed65157e412e287c2c9dc'
 
   url 'https://appgineers.de/mountain/files/Mountain.zip'
   appcast 'https://appgineers.de/mountain/files/mountaincast.xml',
-          checkpoint: '324c8292e7c397d2a689b0237235aca0f0b0e19f9d46d6440b76695803c45c7f'
+          checkpoint: 'bdd14156f57d031ffce89ff013e7b8811e8931bde3333f1c29695549a7376435'
   name 'Mountain'
   homepage 'https://appgineers.de/mountain/'
 
