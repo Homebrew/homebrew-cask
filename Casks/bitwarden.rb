8111c6d98d7e8ba2cb8cc5deff402bfa4fd6eb85
Kyle Spearrin <kspearrin@users.noreply.github.com>
2018-06-04 16:45:34 -0400

    Update bitwarden to 1.3.0 (#48077)

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,11 +1,11 @@
 cask 'bitwarden' do
-  version '1.2.0'
-  sha256 'b21722f05332970c54b79d9b067056424f8ea86cbea5a86fa30e12179a59d7ad'
+  version '1.3.0'
+  sha256 'a8e24abd6c28b3deef00b30b9be632ab799e31c1557d26f3c732725b2439acb3'
 
   # github.com/bitwarden/desktop was verified as official when first introduced to the cask
   url "https://github.com/bitwarden/desktop/releases/download/v#{version}/bitwarden-#{version}-mac.zip"
   appcast 'https://github.com/bitwarden/desktop/releases.atom',
-          checkpoint: 'd35bd4d360bf22a1ccfbe3b2d5b1ec719e42d5ae6bf5559ce49165fc75ec20e9'
+          checkpoint: '146a6f7a095672276291e86889905854a60f1fedc3089ae044d479d08afba234'
   name 'Bitwarden'
   homepage 'https://bitwarden.com/'
 
