54f62fa2026359dfe1ed76adf523ed1029292432
Ezekiel Elin <ezekielelin@me.com>
2018-06-04 09:50:14 -0700

    Update wwdc to 6.0.1 (#48059)

--- Casks/wwdc.rb
+++ Casks/wwdc.rb
@@ -1,11 +1,11 @@
 cask 'wwdc' do
-  version '6.0'
-  sha256 '0d2d92915d6880a8360813a88c3f54e0b8e01a6f9d400defc73c852455aa9cc9'
+  version '6.0.1'
+  sha256 '72fe8e3cc8b7d06d0688c347e6a3405e648c132dcf12c3481557d9c3e8351368'
 
   # github.com/insidegui/WWDC was verified as official when first introduced to the cask
   url "https://github.com/insidegui/WWDC/releases/download/#{version}/WWDC_v#{version}.zip"
   appcast 'https://github.com/insidegui/WWDC/releases.atom',
-          checkpoint: '1d5b08e13209e7168d53b344ae20cdc6fe97a418e3a6405b996fc29f74fdddcd'
+          checkpoint: 'bb15dcaa4af98d85172a2c5c3aa5ca118ce160c07129e3e850a1c944db2071b1'
   name 'WWDC'
   homepage 'https://wwdc.io/'
 
