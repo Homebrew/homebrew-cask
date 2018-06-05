60d434e39b2b479ca294b0ba846951a9f6773864
commitay <commitay@users.noreply.github.com>
2018-06-06 09:12:43 +1000

    Update unity-download-assistant to 2018.1.3f1,a53ad04f7c7f

--- Casks/unity-download-assistant.rb
+++ Casks/unity-download-assistant.rb
@@ -1,6 +1,6 @@
 cask 'unity-download-assistant' do
-  version '2018.1.2f1,a46d718d282d'
-  sha256 'd6c5c2ed427798be81d5086f68d4463afa5fc6bf84c6a283a63bbc41e9409590'
+  version '2018.1.3f1,a53ad04f7c7f'
+  sha256 '93b03f493d8c388808f76e4b40c928520329c55c3177ed1e8fcbb77cd0ee0a67'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/UnityDownloadAssistant-#{version.before_comma}.dmg"
   name 'Unity'
