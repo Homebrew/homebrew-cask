b1c16a6e1a7e0c1273e640e80da9b8e6d6f0eb61
Vio <vio@dramaforum.ca>
2018-06-05 13:04:29 +1000

    Update unity-download-assistant to 2018.1.2f1,a46d718d282d

--- Casks/unity-download-assistant.rb
+++ Casks/unity-download-assistant.rb
@@ -1,6 +1,6 @@
 cask 'unity-download-assistant' do
-  version '2018.1.0f2,d4d99f31acba'
-  sha256 '5c9384222cd9f2f4d42fd354c3318e43231bdac72d1ddc8eb4893628363bd591'
+  version '2018.1.2f1,a46d718d282d'
+  sha256 'd6c5c2ed427798be81d5086f68d4463afa5fc6bf84c6a283a63bbc41e9409590'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/UnityDownloadAssistant-#{version.before_comma}.dmg"
   name 'Unity'
