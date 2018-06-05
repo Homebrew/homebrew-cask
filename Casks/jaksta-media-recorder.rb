329600b7a2fae905aaf733377d6e12fbeff12f8c
Vítor Galvão <info@vitorgalvao.com>
2018-06-05 14:45:31 +0100

    Update and rename jaksta.rb to jaksta-media-recorder.rb

--- Casks/jaksta-media-recorder.rb
+++ Casks/jaksta-media-recorder.rb
@@ -1,4 +1,4 @@
-cask 'jaksta' do
+cask 'jaksta-media-recorder' do
   version '2.2.3'
   sha256 '92cb1eeff5360e0d2ac890556f92c1f0ada74c32209eea5e4eaa27ca3204cc7c'
 
@@ -8,7 +8,13 @@ cask 'jaksta' do
   name 'Jaksta'
   homepage 'https://www.jaksta.com/products'
 
-  pkg "JMR-#{version}.pkg"
+  pkg 'JMR.pkg'
+
+  # This is a horrible hack to force the file extension.
+  # The backend code should be fixed so that this is not needed.
+  preflight do
+    system_command '/bin/mv', args: ['--', staged_path.join(version.to_s), staged_path.join('JMR.pkg')]
+  end
 
   uninstall pkgutil: 'com.shedwork.Jaksta'
 end
