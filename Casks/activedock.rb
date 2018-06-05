d4363d9b18137bfd5b31ee22494f3ac6495b02fb
Work <brigitte@corecode.io>
2018-06-05 18:06:39 +0200

    remove accidental filemaker commit

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -3,11 +3,9 @@ cask 'activedock' do
   sha256 :no_check
 
   # dl.devmate.com/com.sergey-gerasimenko.ActiveDock/ was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.sergey-gerasimenko.ActiveDock/ActiveDock.zip"
-
+  url 'https://dl.devmate.com/com.sergey-gerasimenko.ActiveDock/ActiveDock.zip'
   name 'ActiveDock'
   homepage 'http://www.noteifyapp.com/activedock/'
 
   app 'ActiveDock.app'
-
 end
