8ad8d7616e56b088ee5c79a2a1801ec5a6b8d8ac
404NetworkError <404NetworkError@users.noreply.github.com>
2018-06-05 13:47:26 -0400

    Update fork appcast

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -4,7 +4,7 @@ cask 'fork' do
 
   url 'https://git-fork.com/update/files/Fork.dmg'
   appcast 'https://git-fork.com/update/feed.xml',
-          checkpoint: '84ec63c7dbaa75d07f2d0fd8d98fb750b9005883d70432e4485a8f74f7e7c609'
+          checkpoint: '2a4def5f0763cd4a51f1496e65bc1e511663b843c91823f495c4ca6bb54ec971'
   name 'Fork'
   homepage 'https://git-fork.com/'
 
