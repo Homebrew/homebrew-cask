80ccb02259401271bf43e26964ea6978ddfa69ba
Vítor Galvão <info@vitorgalvao.com>
2018-06-05 12:53:00 +0100

    Update wtlibrary.rb

--- Casks/wtlibrary.rb
+++ Casks/wtlibrary.rb
@@ -2,9 +2,9 @@ cask 'wtlibrary' do
   version '1.12.1'
   sha256 '5d2f4a2e5b25bf18125bcec7c4fe2662d213c0a696e5b6bc09ec60f7d6ef7867'
 
-  # downloads.sourceforge.net was verified as official when first introduced to the cask
-  url 'https://downloads.sourceforge.net/wtlibrary/WtLibrary_1_12_1.dmg'
-  name 'Watchtower Library on Mac'
+  # downloads.sourceforge.net/wtlibrary was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/wtlibrary/WtLibrary_#{version.dots_to_underscores}.dmg"
+  name 'Watchtower Library'
   homepage 'http://www.wtlibrary.net/'
 
   app 'WtLibrary.app'
