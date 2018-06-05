d4acb35ec5aff936d0bbe1a948153d1bb8a8d9ef
Work <brigitte@corecode.io>
2018-06-05 21:13:05 +0200

    Add ActiveDock.app Latest

---
+++ Casks/activedock.rb
@@ -0,0 +1,11 @@
+cask 'activedock' do
+  version :latest
+  sha256 :no_check
+
+  # dl.devmate.com/com.sergey-gerasimenko.ActiveDock/ was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.sergey-gerasimenko.ActiveDock/ActiveDock.zip'
+  name 'ActiveDock'
+  homepage 'http://www.noteifyapp.com/activedock/'
+
+  app 'ActiveDock.app'
+end
