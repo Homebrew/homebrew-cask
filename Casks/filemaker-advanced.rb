8451b0b664de8a824af8be25cebd18e12492022d
Work <brigitte@corecode.io>
2018-06-05 14:53:09 +0200

    Add FileMaker Pro Advanced v17.0.1.143

--- Casks/filemaker-advanced.rb
+++ Casks/filemaker-advanced.rb
@@ -1,12 +1,14 @@
 cask 'filemaker-advanced' do
   version '17.0.1.143'
-  sha256 'e8553da6c6eaca569e87b0188b7c0bb6eab64dbc874c8c151768c6bf96c2470e'
+  sha256 'b4c56c516886949ef20ce2783acde1a4ba3a3d1b17dde4a2409f55fcfd31c44b'
 
   url "http://fmdl.filemaker.com/TBUB/#{version.major}/fmpa_#{version}.dmg"
   name 'FileMaker Pro Advanced'
-  homepage 'http://www.filemaker.com'
+  homepage 'http://www.filemaker.com/'
 
   auto_updates true
 
-  app 'FileMaker Pro Advanced.app'
+  pkg "FileMaker Pro #{version.major} Advanced.pkg"
+
+  uninstall pkgutil: "com.filemaker.FMPA#{version.major}*"
 end
