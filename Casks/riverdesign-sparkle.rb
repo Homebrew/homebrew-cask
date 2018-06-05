a227c85744c2f8b79946043825b93006ec26d5da
Julian Mayer <julian@corecode.io>
2018-06-05 12:01:40 +0200

    add appcast, change download url to same host

--- Casks/riverdesign-sparkle.rb
+++ Casks/riverdesign-sparkle.rb
@@ -2,8 +2,9 @@ cask 'riverdesign-sparkle' do
   version :latest
   sha256 :no_check
 
-  # sparklesoftware.s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://sparklesoftware.s3.amazonaws.com/store/Sparkle.zip'
+  url 'https://sparkleapp.com/update/Sparkle.zip'
+  appcast 'https://sparkleapp.com/update/appcast.php?lang=en',
+          checkpoint: 'b1d172a08ac115a5709f01e3eb99673936a8f245e74a95faa1c58749698b1c29'
   name 'Sparkle'
   homepage 'https://sparkleapp.com/'
 
