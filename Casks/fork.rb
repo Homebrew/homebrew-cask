0279cc317f6869c75c2f841599702582d4482a90
404NetworkError <404NetworkError@users.noreply.github.com>
2018-06-05 13:35:13 -0400

    Update fork to 1.0.67.5

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,6 +1,6 @@
 cask 'fork' do
-  version '1.0.67.4'
-  sha256 '4f10bb1543ec7a4fe71270358ce89373296b1463779a955e965d2f9395edd816'
+  version '1.0.67.5'
+  sha256 'f02fc78fe635dbd2a8f2e93e6053d58907d6b4a9a25be30f67f0eb3cc2cbb653'
 
   url 'https://git-fork.com/update/files/Fork.dmg'
   appcast 'https://git-fork.com/update/feed.xml',
