df6dddd4b3caad945ee3b6738ed32f1fd18bee8f
404NetworkError <404NetworkError@users.noreply.github.com>
2018-06-06 07:34:03 -0400

    Update mp3tag to 2.88a

--- Casks/mp3tag.rb
+++ Casks/mp3tag.rb
@@ -1,6 +1,6 @@
 cask 'mp3tag' do
-  version '2.87a'
-  sha256 'ae28fc762dc645d938f72eb77982e9517cf128f9d96493f17cd74a993de6ce65'
+  version '2.88a'
+  sha256 '535e337fe5e1021e6c3490bca074102adf08ebc1432b3f77b53815e6f6cfddff'
 
   url "http://download.mp3tag.de/mp3tagv#{version.no_dots}-macOS-Wine.zip"
   name 'MP3TAG'
