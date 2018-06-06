adcc66695602a568b2c0888d959adc72dbefa1fd
commitay <commitay@users.noreply.github.com>
2018-06-06 10:56:59 +1000

    Update unity-webgl-support-for-editor to 2018.1.3f1,a53ad04f7c7f (#48127)

--- Casks/unity-webgl-support-for-editor.rb
+++ Casks/unity-webgl-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-webgl-support-for-editor' do
-  version '2018.1.0f2,d4d99f31acba'
-  sha256 'c24ca52f86b3bbaeef6ee771528907aef6406e8eceeac8ccf8b1186b255d2eb0'
+  version '2018.1.3f1,a53ad04f7c7f'
+  sha256 '4f4d3f191d2de341322c1e2a99a0bb6cff4d5da9e388b8fe55b2bdf7d4d21931'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity WebGL Build Support'
