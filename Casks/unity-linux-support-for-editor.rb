ff3e0dd7e404f1a05f0e4cef06f2b32ca463cef9
commitay <commitay@users.noreply.github.com>
2018-06-06 10:56:48 +1000

    Update unity-linux-support-for-editor to 2018.1.3f1,a53ad04f7c7f (#48126)

--- Casks/unity-linux-support-for-editor.rb
+++ Casks/unity-linux-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-linux-support-for-editor' do
-  version '2018.1.0f2,d4d99f31acba'
-  sha256 '5e674d5cfac66c537340fb7edda80f546f817a81a49272417d429b134add89eb'
+  version '2018.1.3f1,a53ad04f7c7f'
+  sha256 'c3ebf0935f22c7836b8805d1281554cc4130ed2334678ed90329e1719e09d199'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity Linux Build Support'
