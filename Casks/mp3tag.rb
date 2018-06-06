9e120c2443ab49c172ce32fccd759a004ae3e590
404NetworkError <404NetworkError@users.noreply.github.com>
2018-06-06 07:44:23 -0400

    Add zap trash

--- Casks/mp3tag.rb
+++ Casks/mp3tag.rb
@@ -7,4 +7,10 @@ cask 'mp3tag' do
   homepage 'http://www.mp3tag.de/en/'
 
   app "mp3tagv#{version.no_dots}-macOS-Wine/Mp3tag.app"
+
+  zap trash: [
+               '~/Library/Application Support/de.mp3tag.Mp3tag_*',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/de.mp3tag.mp3tag_*.sfl*',
+               '~/Library/Preferences/org.kronenberg.Winetricks.plist',
+             ]
 end
