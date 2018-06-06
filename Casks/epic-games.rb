f38018ae833e412f6c2323ee4bcbaff3b56e3d89
7IN0SAN9 <me@7in0.me>
2018-06-07 06:47:42 +0800

    fix sha256 hash for Epic Games

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -3,7 +3,7 @@ cask 'epic-games' do
   # Latest version is posted to the forum:
   # https://forums.unrealengine.com/unreal-engine/announcements-and-releases/1928-latest-launcher-release-notes
   version '7.10.1'
-  sha256 '36078458b4540f6b3315972b1bbd846d7d063d5eb1456b1d4bed0aa91f95037e'
+  sha256 '2afaecc6f1d13b72d666136c4e2048891b99b122e5c51de65fcb4d002723b275'
 
   url "https://download-dynamic.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
   name 'Epic Games Launcher'
