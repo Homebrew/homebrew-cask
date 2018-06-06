7f27ee53d62b00bf61ab2fa64d0f5562fc0a20bd
Stefano Canducci <steno@fastmail.fm>
2018-06-06 23:46:35 +0200

    Update visual-studio-code to 1.24.0,6a6e02cef0f2122ee1469765b704faf5d0e0d859

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,11 +1,11 @@
 cask 'visual-studio-code' do
-  version '1.23.1,d0182c3417d225529c6d5ad24b7572815d0de9ac'
-  sha256 'ace64ab0b56284b270b4ff7e6f5b83837317f371a91ad8a50b98eb53e366220f'
+  version '1.24.0,6a6e02cef0f2122ee1469765b704faf5d0e0d859'
+  sha256 '8e8e6b10d199877e44d5e6c7a59438304dd031e97c676a8b9f798c85dc80a6f2'
 
   # az764295.vo.msecnd.net/stable was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
   appcast 'https://vscode-update.azurewebsites.net/api/update/darwin/stable/VERSION',
-          checkpoint: '9a8bcc4046153a4683b98740777b25981f113e6ba9d75257b142ee3e2e9dd2f6'
+          checkpoint: '6cc89b034fd0a5f5eb05f5c47c51c9be06a1f17dd911b3dc8019d72aab1e4126'
   name 'Microsoft Visual Studio Code'
   name 'VS Code'
   homepage 'https://code.visualstudio.com/'
