004931270e685f53cfc778b4ba6e23110af0abf0
Brian Morton <brianmortondesign@gmail.com>
2018-06-06 13:47:29 -0700

    Update prince to 11.4 (#48149)

--- Casks/prince.rb
+++ Casks/prince.rb
@@ -1,6 +1,6 @@
 cask 'prince' do
-  version '11.3'
-  sha256 'eb45c067620d7978eee6bc615fe19524039a06ef2ee1a25349e49c72ad2e84da'
+  version '11.4'
+  sha256 'd360101c46c7d6586584216a090f2c99f4a9f65d5133a6154eed5fbddf8b461a'
 
   url "https://www.princexml.com/download/prince-#{version}-macosx.tar.gz"
   name 'Prince'
