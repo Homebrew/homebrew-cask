e2a0d1dc811f9d30ffc7c669133ab5160893a7f1
Brian Morton <brianmortondesign@gmail.com>
2018-06-07 16:40:13 -0700

    Update traverse to 0.8.7

--- Casks/traverse.rb
+++ Casks/traverse.rb
@@ -1,11 +1,11 @@
 cask 'traverse' do
-  version '0.8.6'
-  sha256 '4c71901ab88d3ff8bbc0009ad608cfd74cc3cb5283282b68f51fce091ff9d70c'
+  version '0.8.7'
+  sha256 'ecf47a04e0069220fe57731c2eea9317f1100a74e44747cf0d306fd18f874360'
 
   # traverseapp.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://traverseapp.nyc3.digitaloceanspaces.com/builds/Traverse-#{version}-mac.zip"
   appcast 'https://traverse.site/release-notes',
-          checkpoint: 'cb9768e38ae29dbe3f2726e3cf0083c2f4c3f912d584c50b08bafa0fa1e3bc2a'
+          checkpoint: 'f03dd1f911b95c57d291d74cec55d3a46101eba805c629a29beb1b3d49fe501b'
   name 'Traverse'
   homepage 'https://traverse.site/'
 
