5c553a9af535f0fce1e93cc30ce5e410e31cf06f
404NetworkError <404NetworkError@users.noreply.github.com>
2018-06-07 08:30:30 -0400

    Update flash-ppapi to 30.0.0.113

--- Casks/flash-ppapi.rb
+++ Casks/flash-ppapi.rb
@@ -1,10 +1,10 @@
 cask 'flash-ppapi' do
-  version '29.0.0.171'
-  sha256 'b74c7e7493009650579c3d8a74eb6953633bbb28512307b277b090031738d601'
+  version '30.0.0.113'
+  sha256 '97fce3f1898590c75c36f1433afad0aa1c75b31592663cd07051a50da7252e0b'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/pdc/#{version}/install_flash_player_osx_ppapi.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
-          checkpoint: 'a8b1088272513abecdf52195eed55c7e5247ec8bc82de741c3aa1060099220be'
+          checkpoint: '8e5e13cdb1344fae81c37994076fcd3b176bdb06c57fdcb7fa1c62925dfec4ba'
   name 'Adobe Flash Player PPAPI (plugin for Opera and Chromium)'
   homepage 'https://get.adobe.com/flashplayer/otherversions/'
 
