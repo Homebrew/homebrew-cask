8bf0444d22a93dd9c34414c422ddfc3c00135453
Brian Morton <brianmortondesign@gmail.com>
2018-06-08 14:34:16 -0700

    Update monal to 2.1.2b2 (#48223)

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,10 +1,10 @@
 cask 'monal' do
-  version '2.1.1b1'
-  sha256 'da4d448959b98a7c2e0fd66962633f5d42f0a0a4459b4b018ee4fa6d891b1dc2'
+  version '2.1.2b2'
+  sha256 '59227c08142e298dc40c71d014214e25d57cccc1895fdeb273906e7aac52cecd'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
   appcast 'https://monal.im/Monal-OSX/appcast.xml',
-          checkpoint: 'e1f00250cd7c25b510e66be29ae3b6e1156eff8b89c0b06dcdf3a199153475ac'
+          checkpoint: '163a8946b68ef0e40b0acd2c657303c4f8a5beeafd227893590569f682124c85'
   name 'Monal'
   homepage 'https://monal.im/'
 
