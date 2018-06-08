3b98a972f2026bce994ca6f3a245a502128c6a93
Brian Morton <brianmortondesign@gmail.com>
2018-06-08 14:34:55 -0700

    Update zoc to 7.19.0 (#48228)

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,10 +1,10 @@
 cask 'zoc' do
-  version '7.18.4'
-  sha256 '5763b9821c12c31969afbcf15889bdd7fa340442e403c6c4097237b25bea3f1c'
+  version '7.19.0'
+  sha256 '8c5c69ee1239c2dd9e0ab4c5f4acef54690b673be0cc3b06606ac04a81e01a6c'
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast "https://www.emtec.com/downloads/zoc/zoc#{version.major}_previous/",
-          checkpoint: '16eaed3bcefbff0a6a38bcde04d3e7c3db9ebeeca2679370f11c2016934e8538'
+          checkpoint: 'c9bc3eac4a46c5e305b7167e8eeb5a4de3b2d9793cf6d3b3dd85849af05b13d6'
   name 'ZOC'
   homepage 'https://www.emtec.com/zoc/'
 
