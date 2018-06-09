ec72b26ac29cfd16d5c05311051e75c6002eedee
commitay <commitay@users.noreply.github.com>
2018-06-09 21:37:35 +1000

    Update endicia.rb

--- Casks/endicia.rb
+++ Casks/endicia.rb
@@ -1,8 +1,8 @@
 cask 'endicia' do
-  version '2.18.2,v750'
+  version '2.18.2,750'
   sha256 '6331785174da80eaeb68b7451d12fa883c2bd70a796ce754e155fcf26ffc1892'
 
-  url "https://download.endiciaformac.com/EndiciaForMac#{version.before_comma.no_dots}#{version.after_comma}.dmg"
+  url "https://download.endiciaformac.com/EndiciaForMac#{version.before_comma.no_dots}v#{version.after_comma}.dmg"
   appcast 'https://s3.amazonaws.com/endiciaformac/EndiciaForMacSparkle.xml',
           checkpoint: '6919823a9de15d0f7083ab56ac28c736eb0d3e7a3def53acbacc7484a3c2ad93'
   name 'Endicia for Mac'
