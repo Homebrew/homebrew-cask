478ce7ceaf9c18e32131c9511647b26a8c4548bf
commitay <commitay@users.noreply.github.com>
2018-06-11 22:28:20 +1000

    Update jietu.rb

--- Casks/jietu.rb
+++ Casks/jietu.rb
@@ -1,8 +1,8 @@
 cask 'jietu' do
-  version '2.2.2(11054)'
+  version '2.2.2,11054'
   sha256 '6b8c906a1f004e11aa956916fb3e652a3b72baee77d76f09e02a4199db8bddc4'
 
-  url "http://dldir1.qq.com/invc/tt/QB/Jietu/Jietu_#{version}.dmg"
+  url "https://dldir1.qq.com/invc/tt/QB/Jietu/Jietu_#{version.before_comma}(#{version.after_comma}).dmg"
   name 'Jietu'
   name '截图'
   homepage 'http://jietu.qq.com/'
