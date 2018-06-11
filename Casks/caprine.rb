881bae0b20c8164a0791f643a4c932858e9fdeab
János Orcsik <orcsikj@gmail.com>
2018-06-11 13:25:00 +0200

    Update caprine to 2.14.2 (#48283)

--- Casks/caprine.rb
+++ Casks/caprine.rb
@@ -1,10 +1,10 @@
 cask 'caprine' do
-  version '2.14.0'
-  sha256 'b43308cf6769209014685c98d01157a4e3385d6c86105ef0b603b4b0d48cc0a9'
+  version '2.14.2'
+  sha256 '7a682142d19b160db6091f0c0fec913d6fd74f0b53efb5bbe284a58b47a2b930'
 
   url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/Caprine-#{version}.dmg"
   appcast 'https://github.com/sindresorhus/caprine/releases.atom',
-          checkpoint: '1fde0f67e89061497c0203a20d0d36382d9b44e03862ebfaea320bd8c3d67adb'
+          checkpoint: '4f61889f5ebf635954c39ffe02a120abab5202a74794ad30ecc8163e9741f9f6'
   name 'Caprine'
   homepage 'https://github.com/sindresorhus/caprine'
 
