da0e46e8b9e5dc5fe2d726d84df56c6e5f1261db
Vítor Galvão <info@vitorgalvao.com>
2018-06-12 22:13:34 +0100

    Update findings.rb

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -3,8 +3,7 @@ cask 'findings' do
   sha256 '9a41fb186bb2f204653d2bc20a9f7e12753bdeec6ffd9408f8b17a6e56b4316b'
 
   url "http://downloads.findingsapp.com/Findings_#{version.after_comma}_#{version.before_comma}.zip"
-  appcast 'https://s3.amazonaws.com/downloads.findingsapp.com/appcastv2.xml',
-          checkpoint: '164b2175c8e6d39b3ab4ae8cdea41c1dc01857d5a08e5ce9210592c4e3ffd2de'
+  appcast 'https://s3.amazonaws.com/downloads.findingsapp.com/appcastv2.xml'
   name 'Findings'
   homepage 'http://findingsapp.com/'
 
