bddee49e39e5fc56aebecacb6747ad2ce5a03a7c
Brian Morton <brianmortondesign@gmail.com>
2018-06-12 09:39:00 -0700

    Update papers to 3.4.18,566

--- Casks/papers.rb
+++ Casks/papers.rb
@@ -1,6 +1,6 @@
 cask 'papers' do
-  version '3.4.16,555'
-  sha256 'de4a61730a1a343ad06f0aa41fd9b9881d0cd11436db7aa8b61b11108c22a03e'
+  version '3.4.18,566'
+  sha256 '15c226fd763479319fe115c710d1ee40180a7d8137954da71cf777b9ae0dfd7c'
 
   # appcaster.papersapp.com/apps/mac/production/download was verified as official when first introduced to the cask
   url "http://appcaster.papersapp.com/apps/mac/production/download/#{version.after_comma}/papers_#{version.before_comma.no_dots}_#{version.after_comma}.dmg"
