90699b0e8a8f269f723dd05522ef4f838aecfa1e
Chad Catlett <chad@catlett.info>
2018-06-12 09:14:31 -0600

    Update irccloud to 0.7.0

--- Casks/irccloud.rb
+++ Casks/irccloud.rb
@@ -1,6 +1,6 @@
 cask 'irccloud' do
-  version '0.6.0'
-  sha256 'f3d791032171fed6cca9d95878514b6c315a0ec13f7c7482d1b7e6b43d818440'
+  version '0.7.0'
+  sha256 'db2c90e891e5df195b641f84e2e92cac35e9267e448136e2f8083e2036484861'
 
   url "https://github.com/irccloud/irccloud-desktop/releases/download/v#{version}/IRCCloud-#{version}.dmg"
   appcast 'https://github.com/irccloud/irccloud-desktop/releases.atom'
