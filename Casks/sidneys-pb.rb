018631e09bf0cf2ffb7f049324c5acb327ded3dd
Brian Morton <brianmortondesign@gmail.com>
2018-06-12 09:58:06 -0700

    Update sidneys-pb to 7.4.0

--- Casks/sidneys-pb.rb
+++ Casks/sidneys-pb.rb
@@ -1,6 +1,6 @@
 cask 'sidneys-pb' do
-  version '7.0.0'
-  sha256 '35ea8c3f03ddea43999dc68a6a3fb026d43037f9c4aa534fd8db4fd6a9e68416'
+  version '7.4.0'
+  sha256 '6cfc78ddeb91784542904801bd82f101ba991a54d7c1fde24f8fd3862ddf00b2'
 
   url "https://github.com/sidneys/pb-for-desktop/releases/download/v#{version}/pb-for-desktop-#{version}.dmg"
   appcast 'https://github.com/sidneys/pb-for-desktop/releases.atom'
