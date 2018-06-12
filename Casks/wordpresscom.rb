70e27956a278902ac24e85903afe92907fdf0947
Brian Morton <brianmortondesign@gmail.com>
2018-06-12 14:05:53 -0700

    Update wordpresscom to 3.3.0 (#48362)

--- Casks/wordpresscom.rb
+++ Casks/wordpresscom.rb
@@ -1,6 +1,6 @@
 cask 'wordpresscom' do
-  version '3.2.0'
-  sha256 'dc7c8c7d901f46b5b4c7cc95b77dde2ef7edb47d0a084853e6c37e8270f66e5d'
+  version '3.3.0'
+  sha256 '8bcd0c163cdaf99ccaddbb98f8cd051e4778b886ebe8bff01d83487906551684'
 
   url "https://public-api.wordpress.com/rest/v1.1/desktop/osx/download?type=app&ref=update&version=#{version}"
   appcast 'https://public-api.wordpress.com/rest/v1.1/desktop/osx/version?compare=0.1.0&channel=stable'
