6f812dbf013253c1724acd406ecb7535737fedfc
Julian Mayer <julian@corecode.io>
2018-06-12 12:20:28 +0200

    Update amadeus-pro to 2.4.7

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -1,6 +1,6 @@
 cask 'amadeus-pro' do
-  version '2.4'
-  sha256 '78ebf488a0e601c360c711e7a774a9966d62daa582a79d64d66d0caa3883c075'
+  version '2.4.7'
+  sha256 'da059900ac42d53e4951cc9d76d53a2c006c5f138d1bf1f37fc5cf3dc9e0a746'
 
   # amazonaws.com/AmadeusPro2 was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.dmg'
