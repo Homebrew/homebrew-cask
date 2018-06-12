fd12d19dcc09c265564c1ddf5565bfd87bf1bf5f
Julian Mayer <julian@corecode.io>
2018-06-12 12:13:26 +0200

    Update findings to 2.0.4,5430

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -1,10 +1,10 @@
 cask 'findings' do
-  version '2.0.3,5423'
-  sha256 'eafb235e8fbcc071bc3d09e824d8ed3964b28fbe6ca6254bde7c91b9b1f7a971'
+  version '2.0.4,5430'
+  sha256 '9a41fb186bb2f204653d2bc20a9f7e12753bdeec6ffd9408f8b17a6e56b4316b'
 
   url "http://downloads.findingsapp.com/Findings_#{version.after_comma}_#{version.before_comma}.zip"
-  appcast 'http://downloads.findingsapp.com/appcast.xml',
-          checkpoint: '88b9b5a9fb9d8c50fdb668f279d7aff1d00b1caf30b9585894c6b6e497cb10f2'
+  appcast 'https://s3.amazonaws.com/downloads.findingsapp.com/appcastv2.xml',
+          checkpoint: '164b2175c8e6d39b3ab4ae8cdea41c1dc01857d5a08e5ce9210592c4e3ffd2de'
   name 'Findings'
   homepage 'http://findingsapp.com/'
 
