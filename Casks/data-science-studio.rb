877f54e2101efaa853b30fab4a33513c3b5958af
Brian Morton <brianmortondesign@gmail.com>
2018-06-12 17:16:47 -0700

    Update data-science-studio to 4.3.1 (#48374)

--- Casks/data-science-studio.rb
+++ Casks/data-science-studio.rb
@@ -1,6 +1,6 @@
 cask 'data-science-studio' do
-  version '4.3.0'
-  sha256 '0a8cd23465e2ff08b4f023439ebfe8da15d3776b41383ad8b07c5ec3c3c5f154'
+  version '4.3.1'
+  sha256 'c541c73a33a3d7a30243b098cc268c43607227fe589675075017809b13ca5723'
 
   url "https://downloads.dataiku.com/public/studio/#{version}/Data%20Science%20Studio%20#{version}.dmg"
   name 'Dataiku Data Science Studio'
