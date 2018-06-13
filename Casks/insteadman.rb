b9d57983f41be2b95dcfd78d7f89daf4ab4ec409
Brian Morton <brianmortondesign@gmail.com>
2018-06-13 11:46:46 -0700

    Update insteadman to 3.1.2 (#48399)

--- Casks/insteadman.rb
+++ Casks/insteadman.rb
@@ -1,6 +1,6 @@
 cask 'insteadman' do
-  version '3.1.1'
-  sha256 '6be571c6ddb16c6f034c66cce7e8699c2e79133ff3a012d3edbd14b6910f8938'
+  version '3.1.2'
+  sha256 '9307ac274c8550420116cc15eea1d7923ea0c7e1d8ac5cff098aaf528edc6894'
 
   # github.com/jhekasoft/insteadman3 was verified as official when first introduced to the cask
   url "https://github.com/jhekasoft/insteadman3/releases/download/v#{version}/InsteadMan-#{version}.dmg"
