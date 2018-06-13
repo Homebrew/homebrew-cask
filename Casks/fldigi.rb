52921b189fccbe4ac085607653fcb54b047c3e11
Vítor Galvão <info@vitorgalvao.com>
2018-06-13 19:48:13 +0100

    Update fldigi.rb

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -1,8 +1,8 @@
 cask 'fldigi' do
-  version '4.0.17_macos'
+  version '4.0.17'
   sha256 '1508c2c8357739417a4917321f871bf388850af009dddb7f9fc08481d9136e41'
 
-  url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}.dmg"
+  url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}_macos.dmg"
   appcast 'https://sourceforge.net/projects/fldigi/rss?path=/fldigi'
   name 'fldigi'
   homepage 'https://sourceforge.net/projects/fldigi/files/fldigi/'
