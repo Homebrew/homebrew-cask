089dd4a66edbfb2e827b3f4b42fb913341db0d79
Brian Morton <brianmortondesign@gmail.com>
2018-06-13 15:50:13 -0700

    Update gzdoom to 3.4.0

--- Casks/gzdoom.rb
+++ Casks/gzdoom.rb
@@ -1,6 +1,6 @@
 cask 'gzdoom' do
-  version '3.3.2'
-  sha256 'fb0ca7c6613a69fa7536164e80878df4c85acc0750b8db897ff70ef698e39a88'
+  version '3.4.0'
+  sha256 '8cba4fe3fee0c27d6bcc2ccdb506acb7a1b0c3b5d5789889ad2a92ba35a49759'
 
   url "https://zdoom.org/files/gzdoom/bin/gzdoom-bin-#{version.dots_to_hyphens}.dmg"
   appcast 'https://github.com/coelckers/gzdoom/releases.atom'
