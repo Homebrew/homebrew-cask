7ab76af378a0770c8f024be0eace2fcb34c5f075
Brian Morton <brianmortondesign@gmail.com>
2018-06-14 09:13:41 -0700

    Update godot-mono to 3.0.3

--- Casks/godot-mono.rb
+++ Casks/godot-mono.rb
@@ -1,9 +1,9 @@
 cask 'godot-mono' do
-  version '3.0.2'
-  sha256 '6451722d8154b878acd516f8e12fe53f5cf05fd12bc31c0017c3408fc9f5bba7'
+  version '3.0.3'
+  sha256 '84554bc4d0a6a427c7d289df3b73bbe18d03f974632a2d7e847746af74ff2524'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
-  url "https://downloads.tuxfamily.org/godotengine/#{version}/mono/Godot_v#{version}-stable_mono_osx64.zip"
+  url "https://downloads.tuxfamily.org/godotengine/#{version}/mono/Godot_v#{version}-stable_mono_osx.fat.zip"
   appcast 'https://github.com/godotengine/godot/releases.atom'
   name 'Godot Engine'
   homepage 'https://godotengine.org/'
