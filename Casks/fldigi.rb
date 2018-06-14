12f09c44171f2fbeae8ea82f0985911cde293f40
Brian Morton <brianmortondesign@gmail.com>
2018-06-13 21:41:06 -0700

    Update fldigi to 4.0.17 (#48394)
    * Update fldigi to 4.0.17_macos
    
    * Update fldigi.rb
    
    * Update fldigi.rb
    
    * Update fldigi.rb
    
    * Update fldigi.rb

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -1,12 +1,12 @@
 cask 'fldigi' do
-  version '4.0.16'
-  sha256 '2105929d385a192f1e0168be1a6697234cf5998ce8902297ca8d1786b4b801e2'
+  version '4.0.17,4.3.7'
+  sha256 '1508c2c8357739417a4917321f871bf388850af009dddb7f9fc08481d9136e41'
 
-  url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}.dmg"
+  url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version.before_comma}_macos.dmg"
   appcast 'https://sourceforge.net/projects/fldigi/rss?path=/fldigi'
   name 'fldigi'
   homepage 'https://sourceforge.net/projects/fldigi/files/fldigi/'
 
-  app "fldigi-#{version}.app"
-  app 'flarq-4.3.6.app'
+  app "fldigi-#{version.before_comma}.app"
+  app "flarq-#{version.after_comma}.app"
 end
