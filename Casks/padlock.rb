4b6ab33579ebd1db50c8f675f06210d99256d696
Brian Morton <brianmortondesign@gmail.com>
2018-06-13 21:42:13 -0700

    Update padlock to 2.7.2 (#48415)

--- Casks/padlock.rb
+++ Casks/padlock.rb
@@ -1,10 +1,10 @@
 cask 'padlock' do
-  version '2.7.1'
-  sha256 'b3c0ae95923d11997ccf1bd9aee341f751b57ca1e374528e4ffcf727fd541c1b'
+  version '2.7.2'
+  sha256 'cb25ba911a07476d4914a9f9c20b252cbd141ddeff2bf0c677ead33cdb2a84d8'
 
-  # github.com/MaKleSoft/padlock was verified as official when first introduced to the cask
-  url "https://github.com/MaKleSoft/padlock/releases/download/v#{version}/Padlock-#{version}.dmg"
-  appcast 'https://github.com/MaKleSoft/padlock/releases.atom'
+  # github.com/padlock/padlock was verified as official when first introduced to the cask
+  url "https://github.com/padlock/padlock/releases/download/v#{version}/Padlock-#{version}.dmg"
+  appcast 'https://github.com/padlock/padlock/releases.atom'
   name 'Padlock'
   homepage 'https://padlock.io/'
 
