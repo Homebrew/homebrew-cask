982caa94cc98356b090173609e74fdca2aac1dae
Brian Morton <brianmortondesign@gmail.com>
2018-06-14 13:21:50 -0700

    Update pocket-casts to 0.9.8 (#48456)

--- Casks/pocket-casts.rb
+++ Casks/pocket-casts.rb
@@ -1,6 +1,6 @@
 cask 'pocket-casts' do
-  version '0.9.7'
-  sha256 'cfd999dca93177cc5ab43b4ea22cee919161297776e4595932661cc5ded33e0c'
+  version '0.9.8'
+  sha256 'af64a8f2d7cf9cf669c338ffb48f7430c332db09cdfa16a891055ad671fdb8fe'
 
   url 'https://static.pocketcasts.com/mac/PocketCasts.zip'
   appcast 'https://static2.pocketcasts.com/mac/appcast.xml'
