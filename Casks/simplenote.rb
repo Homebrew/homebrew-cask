85e8e46c4ebd595d8842c999afe240be8d846b87
Brian Morton <brianmortondesign@gmail.com>
2018-06-15 09:37:59 -0700

    Update simplenote to 1.1.5

--- Casks/simplenote.rb
+++ Casks/simplenote.rb
@@ -1,6 +1,6 @@
 cask 'simplenote' do
-  version '1.1.3'
-  sha256 '6152ad389f0296c2715b9d916b816a2dc108a897ce97b457cfa48ef4c1ccdc1c'
+  version '1.1.5'
+  sha256 '7e3e75e50a8aaa0cf9a9ef1dc341c5961dbbcf77f65d34d1f31d90536a7863ee'
 
   url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.zip"
   appcast 'https://github.com/Automattic/simplenote-electron/releases.atom'
