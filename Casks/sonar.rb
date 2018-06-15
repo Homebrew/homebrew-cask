108594d6ef6189841c168bdfcb95971a093f00bd
commitay <commitay@users.noreply.github.com>
2018-06-15 17:36:35 +1000

    Update sonar.rb

--- Casks/sonar.rb
+++ Casks/sonar.rb
@@ -4,7 +4,7 @@ cask 'sonar' do
 
   # github.com/facebook/Sonar was verified as official when first introduced to the cask
   url "https://github.com/facebook/Sonar/releases/download/v#{version}/Sonar.zip"
-  appcast 'https://github.com/facebook/Sonar/releases.atom', checkpoint: '64530a80b66ea786290259618db07238d2b1b795d56b76b21ea314fc46c49d49'
+  appcast 'https://github.com/facebook/Sonar/releases.atom'
   name 'Sonar'
   homepage 'https://fbsonar.com/'
 
