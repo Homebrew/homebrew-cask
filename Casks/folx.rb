12f17b4743ced301772630910a45d1a07f6e0bd3
Robert LU <robberphex@gmail.com>
2018-06-15 18:29:46 +0800

    upgrade folx to 5.5.13723 (#48490)

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -1,6 +1,6 @@
 cask 'folx' do
-  version '5.4.13714'
-  sha256 'd5bdeee601d647c3171806404d7f7204c5d99cdc3ac4fa3cb3ec2f29a8d1623f'
+  version '5.5.13723'
+  sha256 'b31285b8eeccf7ca7124bc21a962361f95e326ea99db147f86ce1a7b6293fc22'
 
   url "https://cdn.eltima.com/download/folx-updater/downloader_mac_#{version}.dmg"
   appcast 'https://cdn.eltima.com/download/folx-updater/folx.xml'
