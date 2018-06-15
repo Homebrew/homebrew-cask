3fb0aa62a0caa170c14efec063daee3459c30bd8
commitay <commitay@users.noreply.github.com>
2018-06-15 21:29:47 +1000

    Update unity-windows-support-for-editor to 2018.1.4f1,1a308f4ebef1 (#48503)

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-windows-support-for-editor' do
-  version '2018.1.3f1,a53ad04f7c7f'
-  sha256 'ffec80667f1d760dd9c222f4b384af8048a77d2de55d24602286c9078b3b35b2'
+  version '2018.1.4f1,1a308f4ebef1'
+  sha256 'ba8f386ff5ed95210ad03b9c505514d5881c2def45f89ae0e1085c3eab9b521c'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.before_comma}.pkg"
   name 'Unity Windows (Mono) Build Support'
