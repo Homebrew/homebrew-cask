a276c5fbb81d7ed820b6623655f85522c4c57010
Brian Morton <brianmortondesign@gmail.com>
2018-06-15 16:52:12 -0700

    Update unity-linux-support-for-editor to 2018.1.5f1,732dbf75922d (#48539)

--- Casks/unity-linux-support-for-editor.rb
+++ Casks/unity-linux-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-linux-support-for-editor' do
-  version '2018.1.4f1,1a308f4ebef1'
-  sha256 'a436987ab7a33df7570ec341933204720a074f65672f73b524e745062fbe2488'
+  version '2018.1.5f1,732dbf75922d'
+  sha256 '231e4b31565a26ce668dca36ec4bc253e1b07fd63abe0d78a3d23432cfb84adb'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/update'
