f3c0cb238663ff5db8b5ee0d49e48a48fa5c6fb8
commitay <commitay@users.noreply.github.com>
2018-06-16 09:54:09 +1000

    Update octomouse.rb

--- Casks/octomouse.rb
+++ Casks/octomouse.rb
@@ -1,8 +1,8 @@
 cask 'octomouse' do
-  version 'v1.7'
+  version '1.7'
   sha256 'b47639b8b4504834caee65a61c0cbe3a42e798cf4901472241f9ce3ab72793e9'
 
-  url 'https://github.com/KonsomeJona/OctoMouse/releases/download/v1.7/OctoMouse.app.zip'
+  url "https://github.com/KonsomeJona/OctoMouse/releases/download/v#{version}/OctoMouse.app.zip"
   appcast 'https://github.com/KonsomeJona/OctoMouse/releases.atom'
   name 'OctoMouse'
   homepage 'https://github.com/KonsomeJona/OctoMouse'
