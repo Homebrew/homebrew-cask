e5ef45c79c36ec1c74059e5d174624da137be4e3
Vítor Galvão <info@vitorgalvao.com>
2018-06-16 13:14:45 +0100

    Update pluralsight.rb

--- Casks/pluralsight.rb
+++ Casks/pluralsight.rb
@@ -1,8 +1,9 @@
 cask 'pluralsight' do
-  version '1.8'
+  version '1.8.234'
   sha256 '55a2051c41457620d8de06cfde0aae9157b6a989d0aeb5e387fdb67e185e6298'
 
-  url 'https://macapp.pluralsight.com/current/installpluralsight.dmg'
+  url "https://macapp.pluralsight.com/installpluralsight#{version}.dmg"
+  appcast 'https://macapp.pluralsight.com/appcast'
   name 'Pluralsight'
   homepage 'https://www.pluralsight.com/'
 
