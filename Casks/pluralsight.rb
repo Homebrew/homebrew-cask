f921d2901de63fba03a708d6124bdf82425b18d1
Gus Fune <gusfune@epicawesome.co>
2018-06-15 23:55:44 -0300

    Added Pluralsight

---
+++ Casks/pluralsight.rb
@@ -0,0 +1,10 @@
+cask 'pluralsight' do
+  version '1.8'
+  sha256 '55a2051c41457620d8de06cfde0aae9157b6a989d0aeb5e387fdb67e185e6298'
+
+  url 'https://macapp.pluralsight.com/current/installpluralsight.dmg'
+  name 'Pluralsight'
+  homepage 'https://www.pluralsight.com/'
+
+  app 'Pluralsight.app'
+end
