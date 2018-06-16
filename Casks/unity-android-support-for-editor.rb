58e4111e8043edfa4c75f2dac0231a31096b4b01
Brian Morton <brianmortondesign@gmail.com>
2018-06-15 17:27:29 -0700

    Update unity-android-support-for-editor to 2018.1.5f1,732dbf75922d (#48535)

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-android-support-for-editor' do
-  version '2018.1.4f1,1a308f4ebef1'
-  sha256 '4e21ddccca6bff448a94bc1ccb9000735ffcf840ae98a8115cae738412c3eb74'
+  version '2018.1.5f1,732dbf75922d'
+  sha256 'efb54f05d6f6590806783de8d60cfc6e96d1af0a37454e4e8e45f4bffc673e55'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/update'
