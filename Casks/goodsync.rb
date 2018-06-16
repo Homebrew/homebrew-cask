fb887475675823a561d5bc10b42106c6c04848f8
Brian Morton <brianmortondesign@gmail.com>
2018-06-15 18:20:55 -0700

    Update goodsync to 10.9.2 (#48549)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.9.1'
-  sha256 'c1e65d51f5ee985ba3600474de9b599acd97580dfc193eb55065b81beec4308b'
+  version '10.9.2'
+  sha256 '3b8dc3e2ae9fe7f3c5434207fba73e3d922e48eaf51aea34663f5d6b8f3b14b0'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://www.goodsync.com/download'
