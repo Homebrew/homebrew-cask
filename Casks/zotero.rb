7d2c0eb3ea35b1fa53f22696e03c8bdcedd8820e
pooranis <4367939+pooranis@users.noreply.github.com>
2018-06-16 20:01:04 -0400

    Update zotero to 5.0.51 (#48556)
    * Update zotero to 5.0.51
    
    * Update zotero.rb

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask 'zotero' do
-  version '5.0.50'
-  sha256 'dcc87da3d7710cb0ebcd86e675a897e1f8f98c7ea2cc2cd0066e3c34b65dcebb'
+  version '5.0.51'
+  sha256 '65d21f6659ab65e2bcc958695ba181a0e664cd42602e6ce795c3b216b375d9b2'
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast 'https://github.com/zotero/zotero/releases.atom'
