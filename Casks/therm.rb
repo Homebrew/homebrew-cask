397d42cc7580255c3d4a98da55a5421f5d33cffa
leandro <leandro@leandro.org>
2018-06-17 17:33:55 +0200

    Upgrade Therm.app to v0.1.8

--- Casks/therm.rb
+++ Casks/therm.rb
@@ -1,6 +1,6 @@
 cask 'therm' do
-  version '0.1.7'
-  sha256 '465c225ef5c4289eb0176bc118ec16629390aa2fc114ffd53b2faa19eb6af009'
+  version '0.1.8'
+  sha256 'b83448377566beab5600f716c0503f54ae0151c45c3b5e0fed723560fedaa19b'
 
   url "https://github.com/trufae/Therm/releases/download/#{version}/Therm.app.zip"
   appcast 'https://github.com/trufae/Therm/releases.atom'
