ba74d9371b0e2fe1af04daae2459aaeb5fa48d1c
Alexander Untch <alexander@untch.com>
2018-06-18 15:38:01 +0200

    Update Postbox.app to v6.0.12 and fixed official download url
    Update Postbox.app to v6.0.12 and fixed official download url

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -2,7 +2,7 @@ cask 'postbox' do
   version '6.0.12'
   sha256 '54d29e6ccb69ee20a020166b6d430a74ef1a32f1c44327d56e08aab444255b7b'
 
-  # https://d3nx85trn0lqsg.cloudfront.net/mac/postbox-#{version}-mac64.dmg was verified as official when first introduced to the cask
+  # d3nx85trn0lqsg.cloudfront.net/mac/ was verified as official when first introduced to the cask
   url "https://d3nx85trn0lqsg.cloudfront.net/mac/postbox-#{version}-mac64.dmg"
   name 'Postbox'
   homepage 'https://www.postbox-inc.com/'
