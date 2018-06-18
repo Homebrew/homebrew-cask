dd653e4ac23e5b7f1bbf434857b616ab660f4a0d
commitay <commitay@users.noreply.github.com>
2018-06-18 11:53:54 +1000

    Update focused to 3.1,1817:1508267199

--- Casks/focused.rb
+++ Casks/focused.rb
@@ -1,9 +1,10 @@
 cask 'focused' do
-  version :latest
-  sha256 :no_check
+  version '3.1,1817:1508267199'
+  sha256 'c59d98631f8d4fcc15682b735ed0dc4e2991c52688168692c2fee5c8f55fb2c5'
 
   # devmate.com/com.71squared.focused was verified as official when first introduced to the cask
-  url 'https://dl.devmate.com/com.71squared.focused/Focused.zip'
+  url "https://dl.devmate.com/com.71squared.focused/#{version.after_comma.before_colon}/#{version.after_colon}/Focused-#{version.after_comma.before_colon}.zip"
+  appcast 'https://updates.devmate.com/com.71squared.focused.xml'
   name 'Focused'
   homepage 'https://codebots.co.uk/'
 
