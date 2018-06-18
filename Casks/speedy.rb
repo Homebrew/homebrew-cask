aa48e7a7fc7c9f6d26600a35352a7c3a8bdb4357
Brian Morton <brianmortondesign@gmail.com>
2018-06-18 09:47:23 -0700

    Update speedy to 4.0.5

--- Casks/speedy.rb
+++ Casks/speedy.rb
@@ -3,7 +3,6 @@ cask 'speedy' do
   sha256 '5dda979473073150d06cdeded654045063088e2f302cef45619281ac74242ca9'
 
   url "http://www.apimac.com/download/previous/Speedy-#{version.no_dots}.zip"
-  appcast 'http://www.apimac.com/version_checking/speedy_mac.xml'
   name 'Speedy'
   homepage 'https://www.apimac.com/'
 
