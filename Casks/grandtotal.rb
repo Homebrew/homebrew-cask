e4593373a2049f06fb9f83b1686b3ea8467ca90c
Brian Morton <brianmortondesign@gmail.com>
2018-06-18 09:15:20 -0700

    Update grandtotal to 5.1.7

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,6 +1,6 @@
 cask 'grandtotal' do
-  version '5.1.6.3'
-  sha256 '09fad1f55bfc4d4d099b1e5a40695edfe06e2ae73de1445130c692f043972e65'
+  version '5.1.7'
+  sha256 'dff6b0118d1d09979981957df17fccc3d57eca2f88b37fc5ab15630880ab970c'
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   appcast "https://mediaatelier.com/GrandTotal#{version.major}/feed.php"
