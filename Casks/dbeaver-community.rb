71e589a100cde523701ed58b6100a07be6e3419d
commitay <commitay@users.noreply.github.com>
2018-06-18 10:37:10 +1000

    Update dbeaver-community to 5.1.1 (#48577)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
-  version '5.1.0'
-  sha256 '3d6292cbb1ffdc5e1f6418cbe20d2b8b2a45a5023c018d78fb992705d1680576'
+  version '5.1.1'
+  sha256 '929e7b0e5ad048a4cae60741e65e99f442e8edcf27fb29b00d1f3c6821411bc9'
 
   # github.com/dbeaver/dbeaver was verified as official when first introduced to the cask
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
