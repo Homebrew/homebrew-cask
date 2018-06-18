a3ed79245b535f976b97fb9623f54a1c280719c3
Naoto Yoshimoto <me@naotone.com>
2018-06-18 18:43:41 -0400

    Update craftmanager to 1.0.67 (#48661)

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -1,6 +1,6 @@
 cask 'craftmanager' do
-  version '1.0.66'
-  sha256 '3640be67ecccff821cde6525d5fb9187d63a1a597ebdb66a0d1f411726a3ce0a'
+  version '1.0.67'
+  sha256 'dd6214107bee63cce7ab7bb3ce4b39441a7ee3e06e8802e80cc90ba609fc7ccd'
 
   url 'https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip'
   appcast 'https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml'
