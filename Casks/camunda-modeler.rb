a79dd7adb917232bd068f30bf92aa57c000be52e
Patrick Schalk <patrick.schalk@holisticon.de>
2018-06-18 08:46:01 +0200

    Fixed wrong Checksum for camunda-modeler (#48437)

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask 'camunda-modeler' do
   version '1.15.1'
-  sha256 'f382873005fa0306d3ac105f4cf9f35d64de1892eaa8e68ea7b1b8eb57416345'
+  sha256 '300d79e7a3d7a8a34839c82c604892637423d97a33aff9fbeffca13564c59cbb'
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-darwin-x64.tar.gz"
   name 'Camunda Modeler'
