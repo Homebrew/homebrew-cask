3aeb64d5483519e951c0733d14c98340dfe0a48c
Chris Nuccitelli <slothdog@ldschurch.org>
2018-06-18 13:48:12 -0600

    Update handbrake to 1.1.1

--- Casks/handbrake.rb
+++ Casks/handbrake.rb
@@ -1,6 +1,6 @@
 cask 'handbrake' do
-  version '1.1.0'
-  sha256 'd2457954cab59a7bbc58f0c12a2b03e262f6d2eb5e7b43960ab04e87531b1e33'
+  version '1.1.1'
+  sha256 '309f2c87d8d63819b1f6074dfd6e7827eb5c98cba56e08a8113413cfb984cfd9'
 
   url "https://download.handbrake.fr/handbrake/releases/#{version}/HandBrake-#{version}.dmg"
   appcast 'https://github.com/HandBrake/HandBrake/releases.atom'
