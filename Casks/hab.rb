58acdabb9251cacc6cf61ab8939c736dac03390c
Brian Morton <brianmortondesign@gmail.com>
2018-06-19 10:40:59 -0700

    Update hab to 0.57.0-20180614230204 (#48682)

--- Casks/hab.rb
+++ Casks/hab.rb
@@ -1,6 +1,6 @@
 cask 'hab' do
-  version '0.56.0-20180530234342'
-  sha256 'e75755f806d870592adba82e61eecdd8478c3c37c66c67d4901da94feb9dad0e'
+  version '0.57.0-20180614230204'
+  sha256 '4174b443765f927144b0db03e64d195afc72f02a7f4c228c94c93e5f52e10c72'
 
   # habitat.bintray.com was verified as official when first introduced to the cask
   url "https://habitat.bintray.com/stable/darwin/x86_64/hab-#{version}-x86_64-darwin.zip"
