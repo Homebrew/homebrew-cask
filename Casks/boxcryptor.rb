86ad8c40b1318fb8f6495058795fc04f6f4628ad
Julian Mayer <julian@corecode.io>
2018-06-19 18:50:06 +0200

    Update boxcryptor to 2.24.941

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,6 +1,6 @@
 cask 'boxcryptor' do
-  version '2.23.939'
-  sha256 '1ded3194ba86e899d803d29cdb9b44a87cf3fcf308b66ddb891cc94155e164c9'
+  version '2.24.941'
+  sha256 'ba112fd2383aa983b991f853be6abb509b7a4f8f68774742d7698f3b646b841b'
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8'
