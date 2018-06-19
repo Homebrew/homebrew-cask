c29fd71b460e7220ee2df62f2fc482d4d89c06a5
Vítor Galvão <info@vitorgalvao.com>
2018-06-19 18:38:44 +0100

    Update popo.rb

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -1,8 +1,8 @@
 cask 'popo' do
-  version '2_6_5,5505'
+  version '2.6.5,5505'
   sha256 '9452e674c585076a99755e44ca682e17ea179db48bd471af433c809dfb20dc45'
 
-  url "http://popo.netease.com/file/popomac/POPO_Mac_V#{version.before_comma}.dmg"
+  url "http://popo.netease.com/file/popomac/POPO_Mac_V#{version.before_comma.dots_to_underscores}.dmg"
   name 'NetEase POPO'
   homepage 'http://popo.netease.com/'
 
