334f4494cb0e5ef00a273236c61ebf92b99f6253
Brian Morton <brianmortondesign@gmail.com>
2018-06-19 09:21:10 -0700

    Update camunda-modeler to 1.16.0

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -3,6 +3,7 @@ cask 'camunda-modeler' do
   sha256 'e5a8a9ac8088426664ca515f21dec4f49f1cff5ccb9bd11497d77ca3fa69131c'
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-darwin-x64.tar.gz"
+  appcast 'https://camunda.com/download/modeler/'
   name 'Camunda Modeler'
   homepage 'https://camunda.org/'
 
