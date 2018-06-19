92f3a99d16769cb57fb73b92fdea9f3f23fab502
Vítor Galvão <info@vitorgalvao.com>
2018-06-19 18:42:56 +0100

    Update winds.rb

--- Casks/winds.rb
+++ Casks/winds.rb
@@ -3,7 +3,8 @@ cask 'winds' do
   sha256 '8d25e427d258183be058aaada8baacec6e97ae73862eb485394596db9464aa6b'
 
   # s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/winds-2.0-releases/releases/Winds-2.1.0.dmg'
+  url "https://s3.amazonaws.com/winds-#{version.major}.0-releases/releases/Winds-#{version}.dmg"
+  appcast 'https://github.com/GetStream/Winds/releases.atom'
   name 'Winds'
   homepage 'https://getstream.io/winds/'
 
