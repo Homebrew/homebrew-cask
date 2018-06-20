b4ae17b72d9c510c6aebe528df57f87fdf313a89
Brian Morton <brianmortondesign@gmail.com>
2018-06-20 09:42:59 -0700

    Update devonthink to 2.10.1

--- Casks/devonthink.rb
+++ Casks/devonthink.rb
@@ -1,6 +1,6 @@
 cask 'devonthink' do
-  version '2.10'
-  sha256 '43c9e2cbef1ae8e8e52d97b54be28b5ce019eaf2615db4921e9736b22c72d4c4'
+  version '2.10.1'
+  sha256 'e08348ad43b26bb0faab682c1546e8a3d0a1e40fa5f785b3daa456f48e4ae2cf'
 
   # amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_Personal.app.zip"
