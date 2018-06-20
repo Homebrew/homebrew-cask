41382c343a360ac2cdf06a1fb12407d483938744
commitay <commitay@users.noreply.github.com>
2018-06-20 10:19:13 +1000

    Update annotation-edit.rb

--- Casks/annotation-edit.rb
+++ Casks/annotation-edit.rb
@@ -1,6 +1,6 @@
 cask 'annotation-edit' do
   version '1.9.95.2'
-  sha256 '3f8892a8103a305d11891063ab19a8e34bef588e98b6e9553ee7400373d15e2a'
+  sha256 'ff43175b4f726b323b31aec669213aa72400003435518b4f1672a96bf159ef8b'
 
   url 'http://www.zeitanker.com/common/Annotation_Edit.zip'
   appcast 'http://zeitanker.com/updates.rss'
