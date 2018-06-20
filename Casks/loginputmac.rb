f65d008444d7893d7a81c3c0b7e3705bd60f4657
Brian Morton <brianmortondesign@gmail.com>
2018-06-20 10:19:53 -0700

    Update loginputmac to 1.14,2741 (#48736)

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,6 +1,6 @@
 cask 'loginputmac' do
-  version '1.13,2729'
-  sha256 '9b61f528abfca0e3dfb638a72f759a57fa8748203333ca07d9fa7da184828edc'
+  version '1.14,2741'
+  sha256 'f8f8b7fbda7bcfca80fdc80a3cb7e840c2fe151c3869c51ecfd197267d7c3c7f'
 
   # nzhm461a0.qnssl.com was verified as official when first introduced to the cask
   url "https://nzhm461a0.qnssl.com/LogInputMac#{version.after_comma}.app.zip"
