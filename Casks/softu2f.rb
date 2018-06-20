85bea1f70d56d52b1e0d28b3cfe81dc86b6e9f0d
Brian Morton <brianmortondesign@gmail.com>
2018-06-19 18:07:11 -0700

    Update softu2f to 0.0.17 (#48715)

--- Casks/softu2f.rb
+++ Casks/softu2f.rb
@@ -1,6 +1,6 @@
 cask 'softu2f' do
-  version '0.0.16'
-  sha256 '3a448829c95234475ff39464760dc5a192e3c076e8d1fc6e96ed52ecbf457937'
+  version '0.0.17'
+  sha256 '5587405776f3249732099059b7e8bc880bf67e7c1647f453ce77b727c0bcd052'
 
   url "https://github.com/github/SoftU2F/releases/download/#{version}/SoftU2F.pkg"
   appcast 'https://github.com/github/SoftU2F/releases.atom'
