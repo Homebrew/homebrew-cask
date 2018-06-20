7c21340bb0797a74af4caa809a8dc2000bbd87aa
Kevin Elliott <kevin@welikeinc.com>
2018-06-20 07:01:37 -0700

    Add Winds.app v2.1.0 (#48699)

---
+++ Casks/winds.rb
@@ -0,0 +1,12 @@
+cask 'winds' do
+  version '2.1.0'
+  sha256 '8d25e427d258183be058aaada8baacec6e97ae73862eb485394596db9464aa6b'
+
+  # s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/winds-#{version.major}.0-releases/releases/Winds-#{version}.dmg"
+  appcast "https://s3.amazonaws.com/winds-#{version.major}.0-releases/latest.html"
+  name 'Winds'
+  homepage 'https://getstream.io/winds/'
+
+  app 'Winds.app'
+end
