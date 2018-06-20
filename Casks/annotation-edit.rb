70c6a21c2a4c3e1179046bab0d1b2818f0c8a6fd
Brian Morton <brianmortondesign@gmail.com>
2018-06-19 17:29:12 -0700

    Update annotation-edit to 1.9.95.2 (#48677)

--- Casks/annotation-edit.rb
+++ Casks/annotation-edit.rb
@@ -1,11 +1,11 @@
 cask 'annotation-edit' do
-  version '1.9.95'
-  sha256 '415ffcd9d4b5e6ecec1efe708a222b7ac6523ed868199059703e325f889d8a23'
+  version '1.9.95.2'
+  sha256 'ff43175b4f726b323b31aec669213aa72400003435518b4f1672a96bf159ef8b'
 
   url 'http://www.zeitanker.com/common/Annotation_Edit.zip'
   appcast 'http://zeitanker.com/updates.rss'
   name 'Annotation Edit'
   homepage 'http://www.zeitanker.com/content/tools/zeitanker_tools/zeitanker_annotation_edit/'
 
-  suite "Annotation Edit #{version.major_minor_patch}"
+  suite "Annotation Edit #{version}"
 end
