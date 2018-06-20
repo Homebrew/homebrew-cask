80dd26db3b02226e24c1e83294237adcd10e341b
pierre-emmanuelJ <pierre-emmanuel.jacquier@epitech.eu>
2018-06-20 18:28:38 +0200

    Add exo v 0.10.0a4

--- Casks/exo.rb
+++ Casks/exo.rb
@@ -1,10 +1,10 @@
 cask 'exo' do
-    version '0.0.1'
+    version '0.10.0a4'
     sha256 '07cf006287e5164f80d8137484f4cf1bc39b6658ce697d1ede2d525cb994a3fe'
   
-    url "https://ppsos-ch-dk-2.exo.io/exo/exo"
-    name 'exo'
-    homepage 'https://exoscale.github.io/egoscale/'
+    url "https://github.com/exoscale/egoscale/releases/download/v#{version}/egoscale_#{version}_darwin_amd64.tar.gz"
+    name 'EXO'
+    homepage 'https://github.com/exoscale/egoscale/tree/master/cmd/exo'
   
     binary "exo"
   end
