0682d975892f3ac4ae37a804fe66b7094a2acb8b
Brian Morton <brianmortondesign@gmail.com>
2018-06-21 09:37:29 -0700

    Update mdrp to 7.2.0

--- Casks/mdrp.rb
+++ Casks/mdrp.rb
@@ -1,6 +1,6 @@
 cask 'mdrp' do
-  version '7.1.2'
-  sha256 '0024a05e8afcae40aff85a0704db7d7621457425fb18d46fe391c0f05dfcfe54'
+  version '7.2.0'
+  sha256 '6e30f8969a61bcba4e1ee973f3bb2be6758780582e7f43302c6d2eb818d04a50'
 
   url "http://www.macdvdripperpro.com/MDRP_v#{version.no_dots}.zip"
   appcast 'https://www.macdvdripperpro.com/mdrp_sparkle5.xml'
