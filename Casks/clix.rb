c073525168b73f27dea43cc3cceaed9456a7f8cb
Julian Mayer <julian@corecode.io>
2018-06-21 21:56:37 +0200

    Update clix to 2.4

--- Casks/clix.rb
+++ Casks/clix.rb
@@ -1,8 +1,8 @@
 cask 'clix' do
-  version '2.1'
-  sha256 'a4f9d270792e9da698326924e4c899c7a5f13f157c7793b82187688f8c189008'
+  version '2.4'
+  sha256 '32f40de6feb59237078e0fd592d0354f5fadd537eddfe5737a211d93aee60ed7'
 
-  url 'ftp://rixstep.com/CLIX.tar.bz2'
+  url 'ftp://rixstep.com/CLIX.zip'
   name 'CLIX'
   homepage 'http://rixstep.com/4/0/clix/'
 
