8d7958b84233a42451977d2d63f5f360f5518427
Brian Morton <brianmortondesign@gmail.com>
2018-06-21 09:33:47 -0700

    Update jetbrains-toolbox to 1.9.3935

--- Casks/jetbrains-toolbox.rb
+++ Casks/jetbrains-toolbox.rb
@@ -1,6 +1,6 @@
 cask 'jetbrains-toolbox' do
-  version '1.8.3868'
-  sha256 '9f4d958e6626b17f15db0b871be600e0faf3b251041a0386d8b3f290a400fef4'
+  version '1.9.3935'
+  sha256 'b08034316a2bf98fccbb4f53206c66282567a670595116d3df05b1dd1e31ef61'
 
   url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release'
