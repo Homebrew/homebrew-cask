f8bc4768d717de2787fe9030a319d37eef71c3f6
Brian Morton <brianmortondesign@gmail.com>
2018-06-21 09:16:58 -0700

    Update epic-games to 7.12.0

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -2,8 +2,8 @@ cask 'epic-games' do
   # Auto-updates to a newer version than available on the homepage.
   # Latest version is posted to the forum:
   # https://forums.unrealengine.com/unreal-engine/announcements-and-releases/1928-latest-launcher-release-notes
-  version '7.11.0'
-  sha256 '08c2d362d211baead8106deaeba6ea1f07af226066772554294298a96c9c8d56'
+  version '7.12.0'
+  sha256 'c30e81771c0d178e5077f6c5aa4b9495722f23337a4cc07e9e990de16ca21467'
 
   url "https://download-dynamic.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
   name 'Epic Games Launcher'
