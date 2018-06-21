24b3b709e2ffc060c0447dbd6083f7d8ad8b7f30
Brian Morton <brianmortondesign@gmail.com>
2018-06-21 09:58:58 -0700

    Update screenflow to 7.3.1

--- Casks/screenflow.rb
+++ Casks/screenflow.rb
@@ -1,6 +1,6 @@
 cask 'screenflow' do
-  version '7.3'
-  sha256 'e3fb0521f45c6eda9b7c708674c4193c1997949d85da00533aa2a2aeb646186b'
+  version '7.3.1'
+  sha256 '38055bf786a4096015c4013dc39b05a563003d843be2d55564a285d790cf6d04'
 
   url "https://www.telestream.net/download-files/screenflow/#{version.major_minor.dots_to_hyphens}/ScreenFlow-#{version}.dmg"
   appcast 'https://www.telestream.net/updater/screenflow/appcast.xml'
