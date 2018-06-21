3a0406f64e05e3e69dc4514f5d0a42989123e705
Gibson Fahnestock <gibfahn@gmail.com>
2018-06-21 13:54:13 +0100

    Update quip to 5.3.7

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -1,6 +1,6 @@
 cask 'quip' do
-  version '5.2.97'
-  sha256 'fb36d27c8cf37140d5901ef31cb0c6c3077ecfaa9ba881bcd54677300261533e'
+  version '5.3.7'
+  sha256 'afe59d815546124c9f9b3096320d38bfc187a9f16dcd731dde385a42e3a9190e'
 
   # d2i1pl9gz4hwa7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2i1pl9gz4hwa7.cloudfront.net/macosx_#{version}.dmg"
