283c1eba329d5a792ad5905b8eaf3028af3cbed4
Julian Mayer <julian@corecode.io>
2018-06-21 17:05:38 +0200

    Update fuse to 1.9.0

--- Casks/fuse.rb
+++ Casks/fuse.rb
@@ -1,15 +1,17 @@
 cask 'fuse' do
-  version '1.8.1.15610'
-  sha256 '448fd88fef4dde6b258db3304dcbf8416237fd35ee928d48c4a0bc9d40d0556a'
+  version '1.9.0'
+  sha256 '31e737086d546176f436a2792baca604487f529008a21c424610baec25146a20'
 
-  url "https://www.fusetools.com/downloads/#{version}/osx"
+  # github.com/fuse-open/fuse-studio was verified as official when first introduced to the cask
+  url "https://github.com/fuse-open/fuse-studio/releases/download/#{version}/fuse_osx_#{version.dots_to_underscores}.pkg"
+  appcast 'https://github.com/fuse-open/fuse-studio/releases.atom'
   name 'Fuse Fusetools'
   homepage 'https://www.fusetools.com/'
 
   depends_on macos: '>= :mavericks'
   container type: :pkg
 
-  pkg 'fuse.pkg'
+  pkg "fuse_osx_#{version.dots_to_underscores}.pkg"
 
   # This is a horrible hack to force the file extension.
   # The backend code should be fixed so that this is not needed.
