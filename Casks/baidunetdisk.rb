aef3e4b8a43babe13aa413476c6fc7be4c01955e
Zhang Dan Jie <dannyec@users.noreply.github.com>
2018-06-21 16:03:37 +0800

    Update Baidu NetDisk to 2.2.2

--- Casks/baidunetdisk.rb
+++ Casks/baidunetdisk.rb
@@ -3,7 +3,7 @@ cask 'baidunetdisk' do
   sha256 '89e3bdc7da7860e55f3fa6edf756a74bd7d7f55607db0358922f7e96c4cc8590'
 
   # baidupcs.com/issue/netdisk/MACguanjia was verified as official when first introduced to the cask
-  url "https://issuecdn.baidupcs.com/issue/netdisk/MACguanjia/BaiduNetdisk-mac-#{version}.dmg"
+  url "https://issuecdn.baidupcs.com/issue/netdisk/MACguanjia/BaiduNetdisk_mac_#{version}.dmg"
   name 'Baidu NetDisk'
   name '百度网盘'
   homepage 'https://pan.baidu.com/download'
