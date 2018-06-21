0c20f0985629c54654c13292d8faa6b682dbbdf8
Antony Peklo <antony.peklo@onereach.com>
2018-06-21 09:25:18 +0300

    fixed cask styling for sip2sip

--- Casks/sip2sip.rb
+++ Casks/sip2sip.rb
@@ -2,9 +2,10 @@ cask 'sip2sip' do
   version '4.3.3'
   sha256 '9c700a24d5c4c482e5d304eadcc2a3f6233914bc828c866e9df9ab915b23a6f0'
 
+  # blink.sipthor.net was verified as official when first introduced to the cask
   url 'https://blink.sipthor.net/download-sip2sip.phtml'
   name 'SIP2SIP'
-  homepage 'http://sip2sip.info'
+  homepage 'http://sip2sip.info/'
 
   app 'SIP2SIP.app'
 end
