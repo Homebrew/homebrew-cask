ee6cc1096d9719045cb551e326eb83fa1ea8c6e9
Julian Mayer <julian@corecode.io>
2018-06-21 20:27:26 +0200

    Update chronicle to 8.0.0

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -1,9 +1,9 @@
 cask 'chronicle' do
   version '8.0.0'
-  sha256 '3f28357cc22e64c779e47d03f219b736c09b5ae1c111af9a9984faae8dbb89d5'
+  sha256 'ef87cd5375df1b1503467d89a6b5cc33d01fa0d064580006464d485211706505'
 
-  url 'http://chronicleapp.com/static/downloads/chronicle.zip'
-  appcast 'http://www.littlefin.com/downloads/chronicle3.xml'
+  url "http://www.chronicleapp.com/static/downloads/chronicle#{version.major}.zip"
+  appcast "http://www.littlefin.com/downloads/chronicle#{version.major}.xml"
   name 'Chronicle'
   homepage 'http://chronicleapp.com/'
 
