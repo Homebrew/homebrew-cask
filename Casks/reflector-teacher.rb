c49b81b08f81ae6187d2c4d9045effbea0fb55b4
Brian Morton <brianmortondesign@gmail.com>
2018-06-22 09:59:09 -0700

    Update reflector-teacher to 3.1.1

--- Casks/reflector-teacher.rb
+++ Casks/reflector-teacher.rb
@@ -1,6 +1,6 @@
 cask 'reflector-teacher' do
-  version '3.0.2'
-  sha256 '1d9d2db04d6b5d4c2a4150718693792a813560d0af20d5afa8a067617f70558f'
+  version '3.1.1'
+  sha256 '5db3c9144d2d7f9b8487fda12d6c9d81ad6e87c516baadc33eb28d598d051b9d'
 
   url "https://download.airsquirrels.com/ReflectorTeacher/Mac/ReflectorTeacher-#{version}.dmg"
   appcast 'https://updates.airsquirrels.com/ReflectorTeacher/Mac/ReflectorTeacher.xml'
