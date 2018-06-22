eb39c9c6426e4157a8d934b834c37c7f91557e52
Brian Morton <brianmortondesign@gmail.com>
2018-06-22 09:46:32 -0700

    Update findings to 2.0.6,5438

--- Casks/findings.rb
+++ Casks/findings.rb
@@ -1,6 +1,6 @@
 cask 'findings' do
-  version '2.0.4,5430'
-  sha256 '9a41fb186bb2f204653d2bc20a9f7e12753bdeec6ffd9408f8b17a6e56b4316b'
+  version '2.0.6,5438'
+  sha256 '3a13090d64ef2887615fe54bfb6c4f6970d218ab9fb1edee04f869e5529c05e5'
 
   url "http://downloads.findingsapp.com/Findings_#{version.after_comma}_#{version.before_comma}.zip"
   appcast "https://s3.amazonaws.com/downloads.findingsapp.com/appcastv#{version.major}.xml"
