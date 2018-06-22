db5942f424f049bbc8d12746a626ef0b9e29ed69
commitay <commitay@users.noreply.github.com>
2018-06-22 11:00:00 +1000

    Update dotnet-sdk.rb

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -3,6 +3,7 @@ cask 'dotnet-sdk' do
   sha256 '9a7219bada5f4f98a778b60e05673dcdd1c661bc105d131dac39f2779020a3bf'
 
   url "https://download.microsoft.com/download/D/0/4/D04C5489-278D-4C11-9BD3-6128472A7626/dotnet-sdk-#{version}-osx-x64.pkg"
+  appcast 'https://www.microsoft.com/net/download/all'
   name '.NET Core SDK'
   homepage 'https://www.microsoft.com/net/core#macos'
 
