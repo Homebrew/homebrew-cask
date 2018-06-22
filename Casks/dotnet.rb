b22c9c789e94947baa49598589e9a1352e67da8d
commitay <commitay@users.noreply.github.com>
2018-06-22 11:00:11 +1000

    Update dotnet.rb

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -3,6 +3,7 @@ cask 'dotnet' do
   sha256 '5e2374630ea48d4e71d133a08c7e86d4e5ebb573a07dd99d6fc46af5ebe26e7f'
 
   url "https://download.microsoft.com/download/9/3/E/93ED35C8-57B9-4D50-AE32-0330111B38E8/dotnet-runtime-#{version}-osx-x64.pkg"
+  appcast 'https://www.microsoft.com/net/download/all'
   name '.Net Core Runtime'
   homepage 'https://www.microsoft.com/net/core#macos'
 
