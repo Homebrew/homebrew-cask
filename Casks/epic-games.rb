d8687bf4276bd28f4858b0667da0f724caf70a2b
commitay <commitay@users.noreply.github.com>
2018-06-22 11:01:39 +1000

    Update epic-games.rb

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,11 +1,9 @@
 cask 'epic-games' do
-  # Auto-updates to a newer version than available on the homepage.
-  # Latest version is posted to the forum:
-  # https://forums.unrealengine.com/unreal-engine/announcements-and-releases/1928-latest-launcher-release-notes
   version '7.12.0'
   sha256 'c30e81771c0d178e5077f6c5aa4b9495722f23337a4cc07e9e990de16ca21467'
 
   url "https://download-dynamic.epicgames.com/Builds/UnrealEngineLauncher/Installers/EpicInstaller-#{version}.dmg"
+  appcast 'https://forums.unrealengine.com/unreal-engine/announcements-and-releases/1928-latest-launcher-release-notes'
   name 'Epic Games Launcher'
   homepage 'https://www.epicgames.com/'
 
