aadfed7ad5946c8c421c4defd7f1341f35110302
nicholas <nicholas@battleship.attlocal.net>
2018-06-23 21:23:22 -0400

    Update freesmug-chromium to 67.0.3396.87

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -1,6 +1,6 @@
 cask 'freesmug-chromium' do
   version '67.0.3396.87'
-  sha256 '7e28db21d2562bca9dad9b341b0488df312f8bc812576c870f9212ee9fcd9bcb'
+  sha256 'ca36d90f76ee40b6c236c2e2f1aa2d988645d66deda753b406bd4eef6fa1ce20'
 
   # sourceforge.net/osxportableapps was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/osxportableapps/Chromium_OSX_#{version}.dmg"
