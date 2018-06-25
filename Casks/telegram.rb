e06c00e84eaf74492000bcdf58eaeac73d85e6f6
Marco Nenciarini <mnencia@kcore.it>
2018-06-25 21:54:59 +0200

    Update telegram to 4.1-131213 (#48896)

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '4.1-131203'
-  sha256 'fb12882e737045d468f8df26072e3c586b0b1d832c70d51858cb3cacb9bb7a05'
+  version '4.1-131213'
+  sha256 '713f3e9e5bdba15c9eb46e54afb02b06d2b5d54f1e9df4d767f22d1bfc139803'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml'
