4184d50d2d7fa4afb62e6c7eb6761ae5ac417c88
Brian Morton <brianmortondesign@gmail.com>
2018-06-25 12:22:20 -0700

    Update icab to 5.8.5 (#48903)
    * Update icab to 5.8.5
    
    * updated app stanza to include highsierra

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -1,6 +1,6 @@
 cask 'icab' do
-  version '5.8.3'
-  sha256 'b4e3a6a0c166cd5b12a5903939415ec81fa5521ba895e224e93a3d035b53cf8c'
+  version '5.8.5'
+  sha256 'dded84c2d0b1bc8d734002eb6fb1eb608cebc0417499f5587a95f8df78f7c940'
 
   # icab.clauss-net.de was verified as official when first introduced to the cask
   url "http://icab.clauss-net.de/icab/iCab_#{version}_Intel.zip"
@@ -10,5 +10,5 @@ cask 'icab' do
 
   depends_on macos: '>= :lion'
 
-  app "iCab #{version} (64+32 Bit Intel Version for MacOSX 10.7-10.12)/iCab.app"
+  app "iCab #{version} (64+32 Bit Intel Version for macOS 10.7-10.13)/iCab.app"
 end
