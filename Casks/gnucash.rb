f56602b7851f01b8d6e88010cdf1b03dd3581127
Brian Morton <brianmortondesign@gmail.com>
2018-06-25 12:54:38 -0700

    Update gnucash to 3.2-1 (#48915)

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,6 +1,6 @@
 cask 'gnucash' do
-  version '3.1-3'
-  sha256 '777e532a80c8061c352bf518e6948155af5e408b148df381a1e6cd85b13d66e9'
+  version '3.2-1'
+  sha256 '05e51cf24d0d85b9aef40f90d70ea118ef0cee2dee342af98ca64609524c4b9f'
 
   # github.com/Gnucash/gnucash was verified as official when first introduced to the cask
   url "https://github.com/Gnucash/gnucash/releases/download/#{version.major_minor_patch}/Gnucash-Intel-#{version}.dmg"
