f73417321c4d8353146ad414958047dd61b866c1
Brian Morton <brianmortondesign@gmail.com>
2018-06-25 09:38:39 -0700

    Update godot to 3.0.4 (#48904)

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,6 +1,6 @@
 cask 'godot' do
-  version '3.0.3'
-  sha256 'c4f20d16574a293d5e85cfd6f5c89aa8fc9196c57db68a84be9439319c9284e3'
+  version '3.0.4'
+  sha256 'ba04c27d358f75ea48da6d9f28706c45b7e362ef2dd0305a7931e08f47b36ed7'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.fat.zip"
