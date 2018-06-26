efd2834c7a5709d7fe3717514554661ac5303f00
Brian Morton <brianmortondesign@gmail.com>
2018-06-26 09:07:06 -0700

    Update arq to 5.12.1

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,6 +1,6 @@
 cask 'arq' do
-  version '5.11.3'
-  sha256 'ecc0628d06f1dbc9ceac6a98608714b1cedfc5b49c7b892827ff8f042d8a1ba2'
+  version '5.12.1'
+  sha256 '16cb7d8457efdd702976d13a005abd333b585a08992340bc0d3f114c135647b8'
 
   url "https://www.arqbackup.com/download/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arq#{version.major}.xml"
