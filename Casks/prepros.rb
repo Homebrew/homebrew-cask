efec1ac4dc24b9520ad780a11fad516e0966a86f
Brian Morton <brianmortondesign@gmail.com>
2018-06-26 09:01:27 -0700

    Update prepros to 6.2.3

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -1,6 +1,6 @@
 cask 'prepros' do
-  version '6.2.2'
-  sha256 '103322a9bc12d4b3b274bde9fa1fb91b67fcc00b7b36ba3225dd515e93a36f03'
+  version '6.2.3'
+  sha256 'deda79eccb33814513569438a222b3682328a3ac074de242b16188034360d6b7'
 
   # s3-us-west-2.amazonaws.com/prepros-io-releases was verified as official when first introduced to the cask
   url "https://s3-us-west-2.amazonaws.com/prepros-io-releases/stable/Prepros-Mac-#{version}.zip"
