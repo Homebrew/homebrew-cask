a8ade7b75a01660f0f78bd510363ba1438289a9a
Brian Morton <brianmortondesign@gmail.com>
2018-06-26 09:02:46 -0700

    Update scythebill to 14.0.0

--- Casks/scythebill.rb
+++ Casks/scythebill.rb
@@ -1,6 +1,6 @@
 cask 'scythebill' do
-  version '13.9.4'
-  sha256 '70ab53b1ac6c5d25fcab6e1f42882439155966d9c1a07c584b6143096a287528'
+  version '14.0.0'
+  sha256 '61cc8f2f7a6a47c6d689a82710efe9533a1afb309b5d2a7847544fc56f2a2825'
 
   # storage.googleapis.com/scythebill-releases was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/scythebill-releases/Scythebill-#{version}.dmg"
