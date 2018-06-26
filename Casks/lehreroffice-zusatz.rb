faa9e1f5b0da35763fe5697ef83ce8bb73caf058
Anver Housseini <contact@anver.ch>
2018-06-26 14:06:42 +0200

    Update lehreroffice-zusatz to 2018.10.0 (#48963)

--- Casks/lehreroffice-zusatz.rb
+++ Casks/lehreroffice-zusatz.rb
@@ -1,6 +1,6 @@
 cask 'lehreroffice-zusatz' do
-  version '2018.9.1'
-  sha256 '96639232a5be13667b6d0d6c84dbb3836e7f5d6b9b61b59db3ec197397eb1235'
+  version '2018.10.0'
+  sha256 'ea064554fd6f30a15705e738b0079e67fafcd9a2ae883170eaee2bfe3cb89e0b'
 
   url 'https://www.lehreroffice.ch/lo/dateien/zusatz/lo_zusatz_macos.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Zusatz'
