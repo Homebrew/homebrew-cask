c5cbcb0e6d65d55348fb0ac3f5b9ad406d369a92
Brian Morton <brianmortondesign@gmail.com>
2018-06-25 17:40:32 -0700

    Update blockstack to 0.29.2 (#48953)

--- Casks/blockstack.rb
+++ Casks/blockstack.rb
@@ -1,6 +1,6 @@
 cask 'blockstack' do
-  version '0.29.1'
-  sha256 'ac92cd6b94fb4a6f119155bce5972b7a70117236394226eabc25816441d1879c'
+  version '0.29.2'
+  sha256 '603bf7a7533d9643ede11b62c05133ffb7b12c91d9ae3508919f3212ab12c18d'
 
   # github.com/blockstack/blockstack-browser was verified as official when first introduced to the cask
   url "https://github.com/blockstack/blockstack-browser/releases/download/v#{version}/Blockstack-for-macOS-v#{version}.dmg"
