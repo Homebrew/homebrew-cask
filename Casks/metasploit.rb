8bd771e923c2a41fd3d14c92e9cf4ce6f24d7ef6
Brian Morton <brianmortondesign@gmail.com>
2018-06-26 09:40:37 -0700

    Update metasploit to 4.16.57+20180626104427.git.4.6219ce0 (#48988)

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask 'metasploit' do
-  version '4.16.57+20180625104434.git.4.6219ce0'
-  sha256 '6c644872fd3423f96a794364e28d7a5510c87d1617861291232fbcc9a44340b3'
+  version '4.16.57+20180626104427.git.4.6219ce0'
+  sha256 '9b4c6270aa34a0ca0cf629d2ec7fdc664c109894d420648d296a215156d87e74'
 
   url "https://osx.metasploit.com/metasploit-framework-#{version}-1rapid7-1.pkg"
   appcast 'https://osx.metasploit.com/LATEST'
