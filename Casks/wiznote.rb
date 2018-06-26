792ce565248260944f58088d99c7cdbd24725603
Vítor Galvão <info@vitorgalvao.com>
2018-06-26 12:08:48 +0100

    Update wiznote.rb

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,8 +1,8 @@
 cask 'wiznote' do
-  version '2.6.0'
+  version '2.6.0,2018-06-15'
   sha256 '4ed19bf7cf1c69dcf648aa82a0432b18a77693a13b9b6dd832300d76fa38000b'
 
-  url 'http://url.wiz.cn/u/mac'
+  url "https://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
   name 'WizNote'
   homepage 'https://www.wiz.cn/wiznote-mac.html'
 
