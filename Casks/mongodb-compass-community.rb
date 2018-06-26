aa7ccf60617c7db73af844695139e75805d1d3dc
Brian Morton <brianmortondesign@gmail.com>
2018-06-26 09:39:34 -0700

    Update mongodb-compass-community to 1.14.5 (#48975)

--- Casks/mongodb-compass-community.rb
+++ Casks/mongodb-compass-community.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass-community' do
-  version '1.13.1'
-  sha256 'a6108af919846c280b8bfd6eaa1067b5717c1c4700a9727b8dae6de5e3c455a7'
+  version '1.14.5'
+  sha256 '97a12f180f7d2bb8e5cd5cb9fc49f1784fb208e55966d3c171bcad3bad8b6833'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-community-#{version}-darwin-x64.dmg"
   name 'MongoDB Compass Community'
