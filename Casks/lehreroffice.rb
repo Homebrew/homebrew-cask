87e86f845d7acc5ddd6f4387e58c640ebf4b91b7
Anver Housseini <contact@anver.ch>
2018-06-26 13:55:17 +0200

    Update lehreroffice to 2018.10.0

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,6 +1,6 @@
 cask 'lehreroffice' do
-  version '2018.9.1'
-  sha256 'c436bdc73ab15c1c7b6c5ead19f61d0aa5cc89ef22a4bfde07db1518c77fa3ba'
+  version '2018.10.0'
+  sha256 '44a4002c3f07041090516f113d9d19e4cc10a1844dd67882fd0fbdd21a1bc480'
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_desktop_macos.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop'
