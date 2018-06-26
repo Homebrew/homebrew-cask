c1a4ae8db430716e7b7eafaf9d7fb9feb0a31275
Vitah <linw1225@gmail.com>
2018-06-26 12:14:23 +0800

    Update wiznote to 2.6.0

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,8 +1,8 @@
 cask 'wiznote' do
-  version '2.5.8,2018-01-12'
-  sha256 '5428cf52af8544c741009d9b56fc0264b53aa4bd7b6ee8b8a08160fa9a546d3b'
+  version '2.6.0'
+  sha256 '4ed19bf7cf1c69dcf648aa82a0432b18a77693a13b9b6dd832300d76fa38000b'
 
-  url "https://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
+  url 'http://url.wiz.cn/u/mac'
   name 'WizNote'
   homepage 'https://www.wiz.cn/wiznote-mac.html'
 
