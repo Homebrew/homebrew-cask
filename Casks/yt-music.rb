4803dc32619a4ac0525ebd5bdab48cf105f0ebd3
Marc Schwede <schwedenmut@googlemail.com>
2018-06-27 11:48:35 +0200

    Add YouTube-Music v 1.0.3 wrapper app.

---
+++ Casks/yt-music.rb
@@ -0,0 +1,11 @@
+cask 'yt-music' do
+  version '1.0.3'
+  sha256 'b72fccdd965d590dfb0d75af26ac1fd0c1a0abd712db63dba753eaa16f2362b4'
+
+  url "https://github.com/steve228uk/YouTube-Music/releases/download/#{version}/YT.Music.app.zip"
+  appcast 'https://github.com/steve228uk/YouTube-Music/releases.atom'
+  name 'YouTube-Music'
+  homepage 'https://github.com/steve228uk/YouTube-Music'
+
+  app 'YT Music.app'
+end
