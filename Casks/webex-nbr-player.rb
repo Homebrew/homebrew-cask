37052a42e78d5fde99106dfeb54916d1fd361816
Andrei Dziahel <andreydyagel@coherentsolutions.com>
2018-06-27 15:31:54 +0300

    Update webex-nbr-player to T31L

--- Casks/webex-nbr-player.rb
+++ Casks/webex-nbr-player.rb
@@ -1,6 +1,6 @@
 cask 'webex-nbr-player' do
   version 'T31L'
-  sha256 '76be3ec4e8d8c7d7efab8b6900e8d3d1e933faa9e9587293547d612c5b34b311'
+  sha256 '5d926ef819a91b3f00c53d2999974fc6d14f6d3b9ea9f779079cb480c5ae85d0'
 
   url "https://welcome.webex.com/client/#{version}/mac/intel/webexnbrplayer_intel.dmg"
   name 'Webex Network Recording player'
