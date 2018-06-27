35b84975025e8b4fe106ae99090755cf556ac1ee
Vítor Galvão <info@vitorgalvao.com>
2018-06-27 15:34:51 +0100

    Update minecraftpe.rb

--- Casks/minecraftpe.rb
+++ Casks/minecraftpe.rb
@@ -1,9 +1,9 @@
 cask 'minecraftpe' do
-  version :latest
-  sha256 :no_check
+  version '1-4-release-20180624'
+  sha256 'a2d733992b0ad9b2cc11cef45dc700724481d66892353617c204e2eadbc84f65'
 
-  # aka.ms/meeclientmacos was verified as official when first introduced to the cask
-  url 'https://aka.ms/meeclientmacos'
+  # meedownloads.azureedge.net was verified as official when first introduced to the cask
+  url "https://meedownloads.azureedge.net/#{version}/macOS/Minecraft_Education_Edition.dmg"
   name 'Minecraft Education Edition'
   homepage 'https://education.minecraft.net/'
 
