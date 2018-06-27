9c70ff0f0ad5a7a2ea666b959d26118acb62fb72
Vítor Galvão <info@vitorgalvao.com>
2018-06-27 03:28:35 +0100

    Update minecraft-ed.rb

--- Casks/minecraft-ed.rb
+++ Casks/minecraft-ed.rb
@@ -7,7 +7,7 @@ cask 'minecraft-ed' do
   name 'Minecraft Education Edition'
   homepage 'https://education.minecraft.net/'
 
-  depends_on macos: '>= 10.12'
+  depends_on macos: '>= :sierra'
 
   app 'minecraftpe.app'
 end
