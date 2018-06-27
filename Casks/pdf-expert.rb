8908cbea19a3518d770788f66ac1389cb015c87c
Brian Morton <brianmortondesign@gmail.com>
2018-06-27 09:36:15 -0700

    Update pdf-expert to 2.4.5,542

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,6 +1,6 @@
 cask 'pdf-expert' do
-  version '2.4.2,528'
-  sha256 '1c20a34a028a94b2c6d20a086d6062a6426a976a7626e4253efc3f506363d4a0'
+  version '2.4.5,542'
+  sha256 '51a16dedc8e64bb37d8dd619b8573804eadbf37f8aab5d8ed38fa7ed741d10f6'
 
   # readdle-test-binaries.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://readdle-test-binaries.s3.amazonaws.com/versions/#{version.after_comma}/PDFExpert.dmg"
