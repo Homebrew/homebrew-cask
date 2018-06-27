f098058d32a9a0e85d83e0849a989dbd39d71c58
Vítor Galvão <info@vitorgalvao.com>
2018-06-27 03:28:47 +0100

    Update minecraftpe.rb

--- Casks/minecraftpe.rb
+++ Casks/minecraftpe.rb
@@ -7,7 +7,7 @@ cask 'minecraftpe' do
   name 'Minecraft Education Edition'
   homepage 'https://education.minecraft.net/'
 
-  depends_on macos: '>= 10.12'
+  depends_on macos: '>= :sierra'
 
   app 'minecraftpe.app'
 end
