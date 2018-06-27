8209a0d62d8edc09081a8776bf916eaf80b86b54
CoreCode <core-code@users.noreply.github.com>
2018-06-27 16:02:11 +0200

    Update mweb to 2.3.0 (#49013)

--- Casks/mweb.rb
+++ Casks/mweb.rb
@@ -1,6 +1,6 @@
 cask 'mweb' do
-  version '2.2.9'
-  sha256 'ea8f2c6dc6f2c2873c7b60c71c351f0e08b93c4a0be2836d3d0f33d98ee7c3b6'
+  version '2.3.0'
+  sha256 'f1985e2c3e2e44e9243ac390663996c1ad2480166c52ca2bbcbe4a4623580fec'
 
   # dl.devmate.com/com.coderforart.MWeb was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coderforart.MWeb/MWeb-ProMarkdownwriting%2CnotetakingandstaticbloggeneratorApp.dmg'
