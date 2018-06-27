1bb2077215cb3b128a7b69ea8f4ba32f3bdb32be
Matt Cramer <b3cramer@gmail.com>
2018-06-26 19:14:12 -0600

    Add Minecraft Education Edition App

--- Casks/minecraft-ed.rb
+++ Casks/minecraft-ed.rb
@@ -2,12 +2,12 @@ cask 'minecraft-ed' do
   version :latest
   sha256 :no_check
 
-  url "https://aka.ms/meeclientmacos"
+  # aka.ms/meeclientmacos was verified as official when first introduced to the cask
+  url 'https://aka.ms/meeclientmacos'
   name 'Minecraft Education Edition'
-  homepage 'https://education.minecraft.net'
+  homepage 'https://education.minecraft.net/'
 
   depends_on macos: '>= 10.12'
 
   app 'minecraftpe.app'
-  
 end
