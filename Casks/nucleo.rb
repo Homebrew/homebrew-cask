fccb209bab4d4042b750a740524bc77f7811dfdf
Brian Morton <brianmortondesign@gmail.com>
2018-06-27 09:16:13 -0700

    Update nucleo to 2.4.1

--- Casks/nucleo.rb
+++ Casks/nucleo.rb
@@ -1,6 +1,6 @@
 cask 'nucleo' do
-  version '2.4.0'
-  sha256 '788fda7af0d2163abc7d48ebafc8821e6696c83d2145e40044054c22eb87bde7'
+  version '2.4.1'
+  sha256 'ac25a324fd3e3c3516df0c8a37df44a776a9d1b8c02ba0eeebe9d30e0f0b9852'
 
   # s3-us-west-2.amazonaws.com/nucleo-app-releases was verified as official when first introduced to the cask
   url "https://s3-us-west-2.amazonaws.com/nucleo-app-releases/mac/Nucleo_#{version}.zip"
