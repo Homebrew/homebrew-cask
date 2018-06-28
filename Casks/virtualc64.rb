558001fc3131a952ef1a21e16fdf53252d290487
Brian Morton <brianmortondesign@gmail.com>
2018-06-28 09:22:17 -0700

    Update virtualc64 to 2.1

--- Casks/virtualc64.rb
+++ Casks/virtualc64.rb
@@ -1,7 +1,7 @@
 cask 'virtualc64' do
   # note: "64" is not a version number, but an intrinsic part of the product name
-  version '2.0'
-  sha256 '69172a35efcdda0bdd05bc04b74db4d3a77ad3ca2c6aa18aed545d41a775a03c'
+  version '2.1'
+  sha256 '2de41a6b2cb240da80193ed5ff7fafd47729f061f2987d7a9e0daa43627fa832'
 
   url "http://www.dirkwhoffmann.de/virtualc64/VirtualC64_#{version}.zip"
   appcast 'http://dirkwhoffmann.de/virtualc64/VirtualC64Appcast.xml'
