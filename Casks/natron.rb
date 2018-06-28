775cbb45fcc8d095f313ab61e7f3b37d336399f3
Brian Morton <brianmortondesign@gmail.com>
2018-06-28 13:20:31 -0700

    update download url and appcast

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -2,9 +2,9 @@ cask 'natron' do
   version '2.3.13'
   sha256 '117aa5df7843a25e48941cd904236d510c679740b17c1eca98e94cccf7925e4c'
 
-  url "https://downloads.natron.fr/Mac/releases/Natron-#{version}.dmg",
-      referer: 'https://natron.fr/download/?os=Mac'
-  appcast 'https://github.com/NatronGithub/Natron/releases.atom'
+    # https://ayera.dl.sourceforge.net/project/natron/ was verified as official when first introduced to the cask
+  url "https://ayera.dl.sourceforge.net/project/natron/OSX/Universal/releases/Natron-#{version}.dmg"
+  appcast 'https://sourceforge.net/projects/natron/files/OSX/Universal/releases/'
   name 'Natron'
   homepage 'https://natron.fr/'
 
