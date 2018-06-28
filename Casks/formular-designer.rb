9798e1e1bd5d9aa395fb3aa39be0319ef6b1ce8e
Anver Housseini <contact@anver.ch>
2018-06-28 19:01:38 +0200

    Update formular-designer to 2018.2.2

--- Casks/formular-designer.rb
+++ Casks/formular-designer.rb
@@ -1,6 +1,6 @@
 cask 'formular-designer' do
-  version '2018.2.1'
-  sha256 'f7f42ba37690551b0c1132671d7730817fabfd8f2c5199b62c77efe46cf923df'
+  version '2018.2.2'
+  sha256 '14fe7118e247c17331dd6e10db67dd3495b9a17c1d994b372b591336a15de78b'
 
   url 'https://www.lehreroffice.ch/lo/dateien/designer/lo_designer_macos.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Designer'
