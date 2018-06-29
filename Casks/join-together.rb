4d00600282263bc70c9ebd73eaff53fd86daa541
Brian Morton <brianmortondesign@gmail.com>
2018-06-29 09:19:53 -0700

    Update join-together to 7.8.1

--- Casks/join-together.rb
+++ Casks/join-together.rb
@@ -1,6 +1,6 @@
 cask 'join-together' do
-  version '7.8.0'
-  sha256 '707624eba0ef871de19bcad9bc2f4db256f3b82b90fce5b2eb6e6b4bfd78032f'
+  version '7.8.1'
+  sha256 '1726dfdfa48f36011dfd6ef1dc4b7054f89c58ac959cc22984eabb8454d9a4f7'
 
   url "https://dougscripts.com/itunes/scrx/jointogether#{version.no_dots}.zip"
   appcast 'https://dougscripts.com/itunes/itinfo/jointogether_appcast.xml'
