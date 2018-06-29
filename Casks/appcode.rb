d91c3819ca3f5b0699652167b643ebad70f7f016
Brian Morton <brianmortondesign@gmail.com>
2018-06-28 18:05:44 -0700

    Update appcode to 2018.1.5,181.5451.8 (#49063)

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,6 +1,6 @@
 cask 'appcode' do
-  version '2018.1.4,181.5087.34'
-  sha256 '2842e1c2fa560ec3e5f8f924fcaaeb9316a7e7a675577571e68504f341626f0c'
+  version '2018.1.5,181.5451.8'
+  sha256 '00c053b50fa00c176c3c690435a7aee6a8108ad634762eef191408f0422df02b'
 
   url "https://download.jetbrains.com/objc/AppCode-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=AC&latest=true&type=release'
