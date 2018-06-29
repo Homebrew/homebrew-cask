b5a2c58fa03210773e9b59f77f5e6e15d760a75b
Brian Morton <brianmortondesign@gmail.com>
2018-06-28 18:06:33 -0700

    Update gingko to 2.1.2 (#49071)

--- Casks/gingko.rb
+++ Casks/gingko.rb
@@ -1,6 +1,6 @@
 cask 'gingko' do
-  version '2.1.1'
-  sha256 '17af34934998c27b5fb09366bd887daaca8458338fef950f79c00fcba92db506'
+  version '2.1.2'
+  sha256 'd2ab2098c3bd2e973911c80eeba204aec21759a11b33e3b0202f81f8ed4afd4e'
 
   # github.com/gingko/client was verified as official when first introduced to the cask
   url "https://github.com/gingko/client/releases/download/v#{version}/gingko-client-#{version}-mac.zip"
