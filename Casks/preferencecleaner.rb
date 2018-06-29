a54b600793b74ff481e8dd787948e08b42806985
commitay <commitay@users.noreply.github.com>
2018-06-29 21:22:43 +1000

    Update preferencecleaner.rb

--- Casks/preferencecleaner.rb
+++ Casks/preferencecleaner.rb
@@ -3,6 +3,7 @@ cask 'preferencecleaner' do
   sha256 '1407b17fd46e784fba7aa45cf5469047ae30e49dc99c749b3315c8bab0bd398d'
 
   url "https://www.echomist.co.uk/software/downloads/PreferenceCleaner_#{version}.dmg"
+  appcast 'https://www.echomist.co.uk/software/PreferenceCleaner.php'
   name 'PreferenceCleaner'
   homepage 'https://www.echomist.co.uk/software/PreferenceCleaner.php'
 
