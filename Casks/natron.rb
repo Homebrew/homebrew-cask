941dc61cbf3bad8f43cdda355c8f597d533f2353
Vítor Galvão <info@vitorgalvao.com>
2018-06-29 02:06:10 +0100

    Update natron.rb

--- Casks/natron.rb
+++ Casks/natron.rb
@@ -2,7 +2,7 @@ cask 'natron' do
   version '2.3.13'
   sha256 '117aa5df7843a25e48941cd904236d510c679740b17c1eca98e94cccf7925e4c'
 
-    # https://ayera.dl.sourceforge.net/project/natron/ was verified as official when first introduced to the cask
+  # https://ayera.dl.sourceforge.net/project/natron/ was verified as official when first introduced to the cask
   url "https://ayera.dl.sourceforge.net/project/natron/OSX/Universal/releases/Natron-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/natron/files/OSX/Universal/releases/'
   name 'Natron'
