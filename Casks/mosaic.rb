d6a55ea66b3b9aacd78b298d8e5444e714e1be15
Brian Morton <brianmortondesign@gmail.com>
2018-06-29 17:02:53 -0700

    Update mosaic to 1.1 (#49117)

--- Casks/mosaic.rb
+++ Casks/mosaic.rb
@@ -1,6 +1,6 @@
 cask 'mosaic' do
-  version '1.0.10'
-  sha256 'eae92c55c5c1b3ddfab4be6541ead5846919a057f1fbc0811c624ff27f2ac544'
+  version '1.1'
+  sha256 '06adf0c701841cdfa959accfe0c2ab685be5b209ede2c2854283ae05a7ee6d81'
 
   url "http://lightpillar.com/appdata/mosaic/archive/Mosaic_#{version.dots_to_underscores}.pkg"
   appcast 'http://lightpillar.com/appdata/mosaic/features/version-history.html'
