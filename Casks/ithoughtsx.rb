873cd53be84af782c07868e1ab0e605f087fbbc7
Sebastian Riehm <s.riehm@iwm.rwth-aachend.e>
2018-06-30 10:17:52 +0200

    added zap, some polishing

--- Casks/ithoughtsx.rb
+++ Casks/ithoughtsx.rb
@@ -1,9 +1,10 @@
 cask 'ithoughtsx' do
   version '5.9'
+  sha256 '0710eb5382f0467ca530e41497a2b3030317d9915621eb8592126c814d283afc'
 
-  # download-installer.cdn.mozilla.net/pub/firefox/releases was verified as official when first introduced to the cask
-  url "https://s3-eu-west-1.amazonaws.com/ithoughtsx/iThoughtsX_#{version.sub(%r{^(\d+)\.(\d+)$}, '\1_\2')}.zip"
-  appcast "https://s3-eu-west-1.amazonaws.com/ithoughtsx/ithoughtsx.xml"
+  # s3-eu-west-1.amazonaws.com/ithoughtsx was verified as official when first introduced to the cask
+  url "https://s3-eu-west-1.amazonaws.com/ithoughtsx/iThoughtsX_#{version.dots_to_underscores}.zip"
+  appcast 'https://s3-eu-west-1.amazonaws.com/ithoughtsx/ithoughtsx.xml'
   name 'iThoughtsX'
   homepage 'https://www.toketaware.com/ithoughts-osx'
 
@@ -11,4 +12,12 @@ cask 'ithoughtsx' do
 
   app 'iThoughtsX.app'
 
+  zap trash: [
+               '~/Library/Caches/CloudKit/com.apple.bird/iCloud.com.toketaware.ios.ithoughts/',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.toketaware.ithoughtsx/',
+               '~/Library/Caches/com.toketaware.ithoughtsx/',
+               '~/Library/Application Support/CloudDocs/session/containers/iCloud.com.toketaware.ios.ithoughts/',
+               '~/Library/Application Support/com.toketaware.ithoughtsx/',
+               '~/Library/Preferences/com.toketaware.ithoughtsx.plist',
+             ]
 end
