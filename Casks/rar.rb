7a4601df7352901facbe1eb266270deafb211f11
Gabriele Bonetti <gabriele.bonetti@gmail.com>
2018-07-01 15:14:11 +0200

    fix sha256 value

--- Casks/rar.rb
+++ Casks/rar.rb
@@ -1,6 +1,6 @@
 cask 'rar' do
   version '5.4.0'
-  sha256 '09a14f40718c68fc1c24b30acb55d0f2f90f3e13b372c48b6ef1e789d748b754'
+  sha256 '381ca05ca2701cf72148da49d36a4dda4e8e29cd0c3879f697b2861326277d5d'
 
   url "http://www.rarlab.com/rar/rarosx-#{version}.tar.gz"
   name 'RAR Archiver'
