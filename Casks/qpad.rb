ed5c1ef18e3e12544319f42423be2ef029354918
Qi <qz2280@columbia.edu>
2018-07-01 01:29:28 -0400

    Add qpad.app v1.0.2
    The official repo of this cask is at: https://github.com/HouQiming/qpad.

---
+++ Casks/qpad.rb
@@ -0,0 +1,10 @@
+cask 'qpad' do
+  version '1.0.2'
+  sha256 '2680e2656e0779503c3e42b07419bf03a3f3f39e39ab82774515beed8163c275'
+
+  url 'https://houqiming.github.io/qpad/update/qpad_mac.dmg'
+  name 'HouQiming QPad Text Editor'
+  homepage 'https://houqiming.github.io/qpad/'
+
+  app 'qpad.app'
+end
