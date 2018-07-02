5f2e995042637b3348b1aef4786b2a3fececaedf
Brian Morton <brianmortondesign@gmail.com>
2018-07-02 09:51:53 -0700

    Update reborn to 0.5.41

--- Casks/reborn.rb
+++ Casks/reborn.rb
@@ -1,6 +1,6 @@
 cask 'reborn' do
-  version '0.5.3'
-  sha256 'f86fa79fef9ba686e802690b6fa935779fb73e683d5e462553be608b177b6b1c'
+  version '0.5.41'
+  sha256 '6fd5045b9e8f8dceeb443f0df81ce8e0b01455191538e297e3c04644552c5681'
 
   url "https://github.com/langyanduan/Reborn/releases/download/#{version}/Reborn.zip"
   appcast 'https://github.com/langyanduan/Reborn/releases.atom'
