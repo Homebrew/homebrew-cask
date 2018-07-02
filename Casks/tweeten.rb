6c5a2418c4e64414b2942e7dbef95ebed8ca1027
Sebastian Schawohl <1510603+zebastiane@users.noreply.github.com>
2018-07-02 12:54:42 +0200

    Update tweeten to 4.5.2

--- Casks/tweeten.rb
+++ Casks/tweeten.rb
@@ -1,6 +1,6 @@
 cask 'tweeten' do
-  version 'untagged-98fbba98905058ce4e30'
-  sha256 'a64c246cff1e8d7cd3a029a1754e5404bd706ab65b849e79a5a580cecfb61748'
+  version '4.5.2'
+  sha256 'aee5fc5f245846bf15b8e63e64f06919f37618682e8efd536e270d24a2c2735c'
 
   # github.com/MehediH/Tweeten was verified as official when first introduced to the cask
   url "https://github.com/MehediH/Tweeten/releases/download/#{version}/tweeten-darwin-x64.zip"
