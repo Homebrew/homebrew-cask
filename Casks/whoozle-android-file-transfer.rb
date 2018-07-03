44ca824e38a1058f03ac0f1ae842f7327f4581cc
Brian Morton <brianmortondesign@gmail.com>
2018-07-03 09:59:54 -0700

    Update whoozle-android-file-transfer to 3.4

--- Casks/whoozle-android-file-transfer.rb
+++ Casks/whoozle-android-file-transfer.rb
@@ -1,9 +1,9 @@
 cask 'whoozle-android-file-transfer' do
-  version '3.2,62d1c79'
-  sha256 '84469fd3adba4046a9137093c3e0731e60ae2220381502b452d767cd4de4e676'
+  version '3.4'
+  sha256 'b32fd4958760f5f184721e61a589ea9d49d22778e8370bbf53f4a66dcb35cbef'
 
   # github.com/whoozle/android-file-transfer-linux was verified as official when first introduced to the cask
-  url "https://github.com/whoozle/android-file-transfer-linux/releases/download/v#{version.before_comma}/AFTOSX-#{version.before_comma}-#{version.after_comma}.dmg"
+  url "https://github.com/whoozle/android-file-transfer-linux/releases/download/v#{version}/AndroidFileTransferForLinux.dmg"
   appcast 'https://github.com/whoozle/android-file-transfer-linux/releases.atom'
   name 'Android File Transfer'
   homepage 'https://whoozle.github.io/android-file-transfer-linux/'
