5f7185524849ff1fab7bcd702ed8004956ffa5e2
commitay <commitay@users.noreply.github.com>
2018-07-03 16:11:18 +1000

    Update marta.rb

--- Casks/marta.rb
+++ Casks/marta.rb
@@ -3,7 +3,7 @@ cask 'marta' do
   sha256 'c5f75aba28cdf930700a7b6503bcf7a65d37aa3d00ccc183cb15790c1770ba0b'
 
   url "https://updates.marta.yanex.org/release/Marta-#{version}.dmg"
-  appcast 'https://marta.yanex.org/updates/appcast.xml'
+  appcast 'https://updates.marta.yanex.org/release/appcast.xml'
   name 'Marta File Manager'
   homepage 'https://marta.yanex.org/'
 
