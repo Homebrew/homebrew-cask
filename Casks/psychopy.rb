125f5b7bc876ca3e74a91275b4fbba3a5a34b9da
Brian Morton <brianmortondesign@gmail.com>
2018-07-03 11:22:00 -0700

    Update psychopy to 1.90.3 (#49237)

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,6 +1,6 @@
 cask 'psychopy' do
-  version '1.90.2'
-  sha256 'ec17d6b554f6239a3c1dbcf45f31c5ae10d0d57b5b42025a9f0ccaa4d55c93a6'
+  version '1.90.3'
+  sha256 '7e1dfac94c39bd2a8fa5489e5e856b73add5d23a1fd381ce0c60e2478e0f6b55'
 
   url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy2_PY3-#{version}-MacOS.dmg"
   appcast 'https://github.com/psychopy/psychopy/releases.atom'
