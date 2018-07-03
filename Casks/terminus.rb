f32ab6c559a3c5027fde2524908f99bf67482065
Brian Morton <brianmortondesign@gmail.com>
2018-07-02 17:15:45 -0700

    Update terminus to 1.0.0-alpha.48

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -1,6 +1,6 @@
 cask 'terminus' do
-  version '1.0.0-alpha.47'
-  sha256 '6b0aa8026206dc4979c1c658be615d1d199c23e2254ef098e547a27bf0ba9cbe'
+  version '1.0.0-alpha.48'
+  sha256 '2d8ab88ec1bf992c7899dcaf9488c368f285ee75b37a87a70c3de54fff7a1b62'
 
   # github.com/Eugeny/terminus was verified as official when first introduced to the cask
   url "https://github.com/Eugeny/terminus/releases/download/v#{version}/Terminus-#{version}.dmg"
