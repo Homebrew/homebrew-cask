bf6be61509c437282851cd04672c2cd8cec7808c
Brian Morton <brianmortondesign@gmail.com>
2018-07-02 17:14:49 -0700

    Update shotcut to 18.07.02

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,6 +1,6 @@
 cask 'shotcut' do
-  version '18.06.02'
-  sha256 '950c8b9ed8ad56718ca2be31bc8dd2fd6a35b9d3eb7b95ee88661fc6e7b19fbf'
+  version '18.07.02'
+  sha256 'ce92aadcec7f0f4d0aab9dc54ed28fd3805baebf68aa21b564800dddfca89707'
 
   # github.com/mltframework/shotcut was verified as official when first introduced to the cask
   url "https://github.com/mltframework/shotcut/releases/download/v#{version.major_minor}/shotcut-macos-x86_64-#{version.no_dots}.dmg"
