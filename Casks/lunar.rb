b614acc1ec6368f9c6aab47e0d4c2209b7b36077
Vítor Galvão <info@vitorgalvao.com>
2018-07-03 20:04:24 +0100

    Update lunar.rb

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -2,7 +2,7 @@ cask 'lunar' do
   version '1.0.0'
   sha256 'd6d5e6d0f1aea04872ccc50b8698bb18992a0ae46d7f450e5be6eb832635a733'
 
-  url 'https://lunarapp.site/Lunar.dmg'
+  url "https://lunarapp.site/Lunar-#{version}.dmg"
   appcast 'https://lunarapp.site/appcast.xml'
   name 'Lunar'
   homepage 'https://lunarapp.site/'
