e11b3bf314af885710e0e415380316dc794b69fd
commitay <commitay@users.noreply.github.com>
2018-07-05 09:23:35 +1000

    Update qtox to 1.16.1

--- Casks/qtox.rb
+++ Casks/qtox.rb
@@ -1,6 +1,6 @@
 cask 'qtox' do
-  version '1.16.0'
-  sha256 '88e21ecf403d024e5bbcc9fad4db0ad4fd6f1794abf706b744bff4a039f43793'
+  version '1.16.1'
+  sha256 '0b797d3cf42b72c5789efd2e9d9d1b9e4fe5e6743cb8480fb7731ac83471bf45'
 
   # github.com/qTox/qTox was verified as official when first introduced to the cask
   url "https://github.com/qTox/qTox/releases/download/v#{version}/qTox.dmg"
