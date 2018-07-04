84b848ad20bf3cd1c47ba1f1b2d135b6bfe1cfdd
Brian Morton <brianmortondesign@gmail.com>
2018-07-03 17:38:46 -0700

    Update downie to 3.3.8,1839 (#49233)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.3.7,1836'
-  sha256 '0fff6b409bfb1271435fa24dc98525d3cc6f8ede906d30f83782568411f65aeb'
+  version '3.3.8,1839'
+  sha256 '72150202fc3ee2392933e07687bb337eb631560c520f1a197724b9a9f6270e85'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.major}_#{version.after_comma}.zip"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml"
