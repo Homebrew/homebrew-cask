15a886cf83383fac8d1a66979ae78e09c9ebe2d1
Brian Morton <brianmortondesign@gmail.com>
2018-07-03 17:40:27 -0700

    Update yt-music to 1.0.4 (#49243)

--- Casks/yt-music.rb
+++ Casks/yt-music.rb
@@ -1,8 +1,8 @@
 cask 'yt-music' do
-  version '1.0.3'
-  sha256 'b72fccdd965d590dfb0d75af26ac1fd0c1a0abd712db63dba753eaa16f2362b4'
+  version '1.0.4'
+  sha256 'd69f7e854b5db387f04c8eee2e2479cfe71368d3c6c05b2b736645e6ba6db539'
 
-  url "https://github.com/steve228uk/YouTube-Music/releases/download/#{version}/YT.Music.app.zip"
+  url "https://github.com/steve228uk/YouTube-Music/releases/download/#{version}/YTMusic.zip"
   appcast 'https://github.com/steve228uk/YouTube-Music/releases.atom'
   name 'YouTube Music'
   homepage 'https://github.com/steve228uk/YouTube-Music'
