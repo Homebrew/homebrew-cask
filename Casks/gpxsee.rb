131917ad0dc365cec21fde1792836307f1024ced
Brian Morton <brianmortondesign@gmail.com>
2018-07-03 17:32:49 -0700

    Update gpxsee to 5.15 (#49256)

--- Casks/gpxsee.rb
+++ Casks/gpxsee.rb
@@ -1,6 +1,6 @@
 cask 'gpxsee' do
-  version '5.14'
-  sha256 '9d0acb61aa1a88524e0db719ad38bbbd27d4aac24560908d7b8c3e0b528cb57d'
+  version '5.15'
+  sha256 '9794d7e29aaf12aadd8c189430f2f021df1a86f1d1b5f71f125149ad992028e3'
 
   # sourceforge.net/gpxsee/Mac%20OS%20X was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg"
