735ed5e2405db7b0e08c58fe7dfe2aaa28379ba9
Anver Housseini <contact@anver.ch>
2018-07-04 11:54:20 +0200

    Update jamf-migrator to 2.8.2 (#49264)

--- Casks/jamf-migrator.rb
+++ Casks/jamf-migrator.rb
@@ -1,6 +1,6 @@
 cask 'jamf-migrator' do
-  version '2.7.2'
-  sha256 '781797e0116196b9b75bf243f98a1394a2e90da59d3ded0b81e61334bc7b34d6'
+  version '2.8.2'
+  sha256 'e5a1b13c7caf1794e218e4fd4c74d40ad4bdc4db14da65468fb42bfdbecc1035'
 
   url 'https://github.com/jamfprofessionalservices/JamfMigrator/releases/download/current/jamf-migrator.zip'
   appcast 'https://github.com/jamfprofessionalservices/JamfMigrator/releases.atom'
