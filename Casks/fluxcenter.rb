2aa3c1a059d0342f917deb9e139248545bc78e51
Yutaro Horikawa <yutaro.ver05+github@gmail.com>
2018-07-05 13:50:10 +0900

    Update fluxcenter 1.2.15.47859

--- Casks/fluxcenter.rb
+++ Casks/fluxcenter.rb
@@ -1,6 +1,6 @@
 cask 'fluxcenter' do
-  version '1.2.14.47805'
-  sha256 'f6d62fa65188b46514952f9c98ca51473b33ed387b6a9351a19b9fb8844aaca2'
+  version '1.2.15.47859'
+  sha256 'f9a6557d1ab8dfe21b8cb0d215907fbb174fe66a94c06d61c2650e4e90ad4043'
 
   url "https://fluxhome.com/wp-content/uploads/files/Center/MacOS/Flux_FluxCenter_MacOS_Installer_(#{version}).dmg"
   name 'FluxCenter'
