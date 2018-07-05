86a03918c5e997214ef2eba291436d7fb30c3159
Julian Mayer <julian@corecode.io>
2018-07-05 11:45:03 +0200

    Update mobirise to latest

--- Casks/mobirise.rb
+++ Casks/mobirise.rb
@@ -2,7 +2,7 @@ cask 'mobirise' do
   version :latest
   sha256 :no_check
 
-  url 'https://mobirise.com/mobirise-free-mac.zip'
+  url 'http://download.mobirise.com/mobirise-free-mac.zip'
   name 'Mobirise'
   homepage 'https://mobirise.com/'
 
