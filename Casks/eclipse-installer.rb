717eef37a7fb4bb50c0508833d11ed24d8c44720
Nono <nonohft@gmail.com>
2018-07-05 11:36:21 +0800

    Update eclipse-installer to 4.8.0,photon,R

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-installer' do
-  version '4.7.3,oxygen:R2'
-  sha256 '072bc4231a831566619298a8764bab73f3715c686d25609361d34e28e69b9440'
+  version '4.8.0,photon,R'
+  sha256 '7606a6b2e84c9ce1654313ba02b1ae3cf9b67015a73c30e7c0bd41f0f1c7adaa'
 
   url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-inst-mac64.tar.gz&r=1"
   name 'Eclipse Installer'
