e3d84d2ebfbb101e2fb23a86b0fcfc96ff9862d1
Brian Morton <brianmortondesign@gmail.com>
2018-07-06 10:03:35 -0700

    Update storyboarder to 1.7.0

--- Casks/storyboarder.rb
+++ Casks/storyboarder.rb
@@ -1,6 +1,6 @@
 cask 'storyboarder' do
-  version '1.6.3'
-  sha256 '242290533fea82b1508ecedaea1dfa50de714eb328311764bb39f1d1f7cedce4'
+  version '1.7.0'
+  sha256 'f9bf075aee8369a461731522a84fc3043fc6a5113c4422581d4e6ccd1de7690e'
 
   # github.com/wonderunit/storyboarder was verified as official when first introduced to the cask
   url "https://github.com/wonderunit/storyboarder/releases/download/v#{version}/storyboarder-#{version}-mac.zip"
