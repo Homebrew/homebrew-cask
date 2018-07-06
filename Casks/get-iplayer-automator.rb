e3f17f28db43d23265b9202f7d1c0f60b28e652b
Brian Morton <brianmortondesign@gmail.com>
2018-07-06 09:47:45 -0700

    Update get-iplayer-automator to 1.13.11.b20180705001

--- Casks/get-iplayer-automator.rb
+++ Casks/get-iplayer-automator.rb
@@ -1,6 +1,6 @@
 cask 'get-iplayer-automator' do
-  version '1.13.10.b20180621001'
-  sha256 'ef38c4d833779f93246128e04762f7aa8bcf10f70e96fd353b5de3f2acd788aa'
+  version '1.13.11.b20180705001'
+  sha256 '3aab180f3f0d4e35be18754855f66a5b3623f78d3cad3a23f1054a7ac301111a'
 
   url "https://github.com/Ascoware/get-iplayer-automator/releases/download/v#{version.major_minor_patch}/Get.iPlayer.Automator.v#{version}.zip"
   appcast 'https://github.com/Ascoware/get-iplayer-automator/releases.atom'
