7b8dc0ad44ae5b8c2c73b8f88c36664bc3f5c101
Nick Wind-McJetters <nick@elearningexperts.net>
2018-07-05 22:33:13 -0400

    Update fission to 2.4.2

--- Casks/fission.rb
+++ Casks/fission.rb
@@ -1,6 +1,6 @@
 cask 'fission' do
-  version '2.4.1'
-  sha256 'cbf9baea4899fe6f054e48609fd6ff6dae7c01d36e9ff8078caa52a1e16262bc'
+  version '2.4.2'
+  sha256 'daaddae2ecbb65154f432aa0ba418b1a1e958ee645e43d90c52b3a21b4752eea'
 
   url 'https://rogueamoeba.com/fission/download/Fission.zip'
   appcast 'https://rogueamoeba.com/fission/releasenotes.php'
