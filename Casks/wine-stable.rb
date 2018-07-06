ae863c5f4195e3384db27c53c83e74a44da6cc29
Brian Morton <brianmortondesign@gmail.com>
2018-07-06 10:17:55 -0700

    Update wine-stable to 3.0.2

--- Casks/wine-stable.rb
+++ Casks/wine-stable.rb
@@ -1,6 +1,6 @@
 cask 'wine-stable' do
-  version '3.0.1'
-  sha256 'd733e2bbad0d29635ad310bd0f0238b25d368d92373c8cbd05817a0361e8258c'
+  version '3.0.2'
+  sha256 'cf0b6bd9b722d387b17a62474217576c9ae13dadfeb7107f7cd6b271977f286a'
 
   url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-stable-#{version}.pkg"
   appcast 'https://dl.winehq.org/wine-builds/macosx/download.html'
