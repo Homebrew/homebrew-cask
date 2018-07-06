ca8b950d544233c9dfc0c23b786dc925edc5694e
Brian Morton <brianmortondesign@gmail.com>
2018-07-06 10:01:54 -0700

    Update qownnotes to 18.07.2,b3679-095757

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '18.07.1,b3670-183652'
-  sha256 'a27726ce67866e20a709353dd3cf7465d8ef3874a073916ca2b0d236a3522c5d'
+  version '18.07.2,b3679-095757'
+  sha256 '07e23aecf0ffee9cbf37454a9878ad219864b1fec761a4ed0566c66add36e6eb'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
