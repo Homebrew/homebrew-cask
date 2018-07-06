fb21d2c6e0dc0de5ec5168c6c16818193d9323a3
Brian Morton <brianmortondesign@gmail.com>
2018-07-06 09:44:33 -0700

    Update blocs to 2.6.2

--- Casks/blocs.rb
+++ Casks/blocs.rb
@@ -1,5 +1,5 @@
 cask 'blocs' do
-  version '2.6.1'
+  version '2.6.2'
   sha256 'ef48ca2977685b45f0077c91af58764fd6ec78cb73abedce57605729a6c189b3'
 
   # uistore.io was verified as official when first introduced to the cask
