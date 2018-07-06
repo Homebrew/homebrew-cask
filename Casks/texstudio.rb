0a253eb6eb467077b9ad71b8585b0e29606727d0
404NetworkError <404NetworkError@users.noreply.github.com>
2018-07-06 06:23:49 -0400

    Update texstudio to 2.12.10

--- Casks/texstudio.rb
+++ Casks/texstudio.rb
@@ -1,6 +1,6 @@
 cask 'texstudio' do
-  version '2.12.8'
-  sha256 'e5826d49bc2b37092b62455a1520ca211dcf4a1deb6ec33736b266e9b62e64c2'
+  version '2.12.10'
+  sha256 '03c9cfc4ae1b296e4a46f140b642b7b84121994d6cc0b1e63fbdb51c5f2e234b'
 
   # github.com/texstudio-org/texstudio was verified as official when first introduced to the cask
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg"
