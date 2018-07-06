1ba89dc88c6f1269c130d454f25e43c440b5d842
Brian Morton <brianmortondesign@gmail.com>
2018-07-06 14:13:11 -0700

    update navicat appcasts

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -3,7 +3,7 @@ cask 'navicat-for-mysql' do
   sha256 'd5cad433d6c765b04c895b5224b55270c6431b2992ee42457833f2e4bdd7766a'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
-  appcast "https://www.navicat.com/updater/v#{version.major_minor.no_dots}/sysProfileInfo.php?appName=Navicat%20for%20MySQL"
+  appcast 'https://www.navicat.com/en/products/navicat-for-mysql-release-note'
   name 'Navicat for MySQL'
   homepage 'https://www.navicat.com/products/navicat-for-mysql'
 
--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -3,7 +3,7 @@ cask 'navicat-for-sql-server' do
   sha256 '0f12cd141c16f4b86dd81cfc9ad574816acf69fdc4753ffeaae71b3522ff56d2'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
-  appcast "https://www.navicat.com/updater/v#{version.major_minor.no_dots}/sysProfileInfo.php?appName=Navicat%20for%20SQL%20Server"
+  appcast 'https://www.navicat.com/products/navicat-for-sqlserver-release-note'
   name 'Navicat for SQL Server'
   homepage 'https://www.navicat.com/products/navicat-for-sqlserver'
 
