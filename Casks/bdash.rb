a8a515e4464dbed314ab42a8616c2a230553d9d5
Brian Morton <brianmortondesign@gmail.com>
2018-07-06 09:33:45 -0700

    Update bdash to 1.4.0

--- Casks/bdash.rb
+++ Casks/bdash.rb
@@ -1,6 +1,6 @@
 cask 'bdash' do
-  version '1.3.0'
-  sha256 '1a1839f87759fcd08b81b332d3d6adcd40348edfd108d26f553390e5a281f188'
+  version '1.4.0'
+  sha256 '257bc19e68a16a8f3aea0a40aadbdffab7aaf6e285ceb6bc6895f9f96122c2e7'
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-mac.zip"
   appcast 'https://github.com/bdash-app/bdash/releases.atom'
