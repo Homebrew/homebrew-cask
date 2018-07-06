83c8b9eeaaab7768e954e9bb5f94752a6096c0e8
Brian Morton <brianmortondesign@gmail.com>
2018-07-06 10:32:17 -0700

    Update navicat-premium to 12.0.27

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -3,7 +3,7 @@ cask 'navicat-premium' do
   sha256 '97b1a87811077c5dd8a0cf174bd27bec3485c00cf9739fe7242413d42ecafe8e'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
-  appcast "https://www.navicat.com/updater/v#{version.major_minor.no_dots}/sysProfileInfo.php?appName=Navicat%20Premium"
+  appcast 'https://www.navicat.com/en/products/navicat-premium-release-note'
   name 'Navicat Premium'
   homepage 'https://www.navicat.com/products/navicat-premium'
 
