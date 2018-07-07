e08d77642806dd4e9ab3e4e15f367e51c56638e1
butekx <25536853+butekx@users.noreply.github.com>
2018-07-07 17:49:11 +0300

    Update a-better-finder-rename.rb

--- Casks/a-better-finder-rename.rb
+++ Casks/a-better-finder-rename.rb
@@ -2,7 +2,7 @@ cask 'a-better-finder-rename' do
   version '10.30'
   sha256 'b24ac9ff421e072ed2d3df4bf5ca1091236c8d5e32f55e2997a07a87aa357629'
 
-  url "http://www.publicspace.net/download/ABFRX.dmg"
+  url "http://www.publicspace.net/download/ABFRX#{version.major}.dmg"
   appcast "http://www.publicspace.net/app/signed_abfr#{version.major}.xml"
   name 'A Better Finder Rename'
   homepage 'http://www.publicspace.net/ABetterFinderRename/'
