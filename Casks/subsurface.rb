414f49b76254ec8783c0f1d923d1dad136fac06d
Brian Morton <brianmortondesign@gmail.com>
2018-07-07 13:45:24 -0700

    Update subsurface to 4.8.0

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,6 +1,6 @@
 cask 'subsurface' do
   version '4.8.0'
-  sha256 'b3b62120bb3c90e378d3b574ca8274b8ad474352c6b32d68fdf28699e2478d5a'
+  sha256 '51fe5c49eb726f5eda780cc3e845574648c3521b350a56d9d6574d035cabf3fc'
 
   url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
   name 'Subsurface'
