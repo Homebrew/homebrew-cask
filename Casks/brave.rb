0a1eff43df2b6f29505d55f8dcbab6c1c84ee259
commitay <commitay@users.noreply.github.com>
2018-07-07 21:08:10 +1000

    Update brave to 0.23.31 (#49373)

--- Casks/brave.rb
+++ Casks/brave.rb
@@ -1,6 +1,6 @@
 cask 'brave' do
-  version '0.23.19'
-  sha256 '4f41aaf671f70c10d4e630b2adf8f7974d66923c4e4a8316d9df8e4a89d6f1f1'
+  version '0.23.31'
+  sha256 'f84a8e241d80796a628e4e761b0b9ef7d805498ef1ad6340c13ba0f274c6692d'
 
   # github.com/brave/browser-laptop was verified as official when first introduced to the cask
   url "https://github.com/brave/browser-laptop/releases/download/v#{version}dev/Brave-#{version}.dmg"
