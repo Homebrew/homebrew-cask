503a679715838d9ed9f1c8dd2fb167dc8137cb99
Keith Smiley <keithbsmiley@gmail.com>
2018-07-07 16:55:17 -0700

    Update brisk to 1.2.0

--- Casks/brisk.rb
+++ Casks/brisk.rb
@@ -1,6 +1,6 @@
 cask 'brisk' do
-  version '1.1.2'
-  sha256 '5bc8e1e72cae618045875f1fbed9102413f056ea19b3c46dadf430ae618ae40e'
+  version '1.2.0'
+  sha256 '1ad1da10a3bb2b31af88bc19b6f5b3a018de63e922f632fbdcd7a5e626b96601'
 
   url "https://github.com/br1sk/brisk/releases/download/#{version}/Brisk.app.tar.gz"
   appcast 'https://github.com/br1sk/brisk/releases.atom'
