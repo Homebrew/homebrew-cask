e6f66b9608b8c5636dd2616ca8fb5256e1e2bfd4
Vadim Golub <vdmgolub@gmail.com>
2018-07-07 18:18:21 -0700

    Add Melodics.app Latest

---
+++ Casks/melodics.rb
@@ -0,0 +1,10 @@
+cask 'melodics' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://web-cdn.melodics.com/download/MelodicsV2.dmg'
+  name 'Melodics'
+  homepage 'https://melodics.com/'
+
+  app 'Melodics.app'
+end
