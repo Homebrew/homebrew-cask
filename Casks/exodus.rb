026c9362fcc7483a575aa64c3d8a8430a5d55024
Fabian Maass <fabian.maass@adnovum.ch>
2018-07-08 12:59:35 +0200

    Update exodus to 1.55.2

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,6 +1,6 @@
 cask 'exodus' do
-  version '1.55.1'
-  sha256 '44bcbe822bb63a45d450d0e064ebdaee6b460dc7f864847fab8d31ad19a3f922'
+  version '1.55.2'
+  sha256 '09fbf06debd2e9f426fb82d0979879085733e91c0c94d41dcc14b9f7fc872aaf'
 
   # exodusbin.azureedge.net was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-macos-#{version}.dmg"
