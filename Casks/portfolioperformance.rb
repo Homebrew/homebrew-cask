388419efa0f7ec241c5b4f9daaf5415fe5cd5a6e
Brian Morton <brianmortondesign@gmail.com>
2018-07-09 10:03:14 -0700

    Update portfolioperformance to 0.33.0

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,9 +1,9 @@
 cask 'portfolioperformance' do
-  version '0.32.0'
-  sha256 'f8eec0df57404110b479d908b73c94289aebfbe9e1ea27ae960ad1c47a6159b2'
+  version '0.33.0'
+  sha256 'be2c56aabdfc3d8f3c9a93057fb901abbda9e83ee15c6f1ff78977273d0bdc7f'
 
   # bintray.com/buchen/downloads was verified as official when first introduced to the cask
-  url "https://bintray.com/buchen/downloads/download_file?file_path=#{version}%2FPortfolioPerformance-#{version}-macosx.cocoa.x86_64.tar.gz"
+  url "https://bintray.com/buchen/downloads/download_file?file_path=#{version}%2FPortfolioPerformance-distro-#{version}-macosx.cocoa.x86_64.tar.gz"
   appcast 'https://github.com/buchen/portfolio/releases.atom'
   name 'Portfolio Performance'
   homepage 'http://www.portfolio-performance.info/portfolio/'
