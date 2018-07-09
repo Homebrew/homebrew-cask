4a8365ef929a62d493558fad04d9fdb9e736342d
Brian Morton <brianmortondesign@gmail.com>
2018-07-09 09:33:50 -0700

    Update coqide to 8.8.1

--- Casks/coqide.rb
+++ Casks/coqide.rb
@@ -1,6 +1,6 @@
 cask 'coqide' do
-  version '8.8.0'
-  sha256 '285353fff1c98231577e0a2aa77054df80821d7193d0378751b9c1b9facfc36e'
+  version '8.8.1'
+  sha256 '80b51cef6156872f95230c68dd7f3406619f52b6b49daad2b7def7d0d6c721a3'
 
   # github.com/coq/coq was verified as official when first introduced to the cask
   url "https://github.com/coq/coq/releases/download/V#{version.major_minor_patch}/coq-#{version}-installer-macos.dmg"
