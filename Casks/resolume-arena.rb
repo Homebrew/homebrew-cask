0afdb9954c40553dcc9a41c93bce46bef4585358
Brian Morton <brianmortondesign@gmail.com>
2018-07-09 09:55:16 -0700

    Update resolume-arena to 6.0.10

--- Casks/resolume-arena.rb
+++ Casks/resolume-arena.rb
@@ -1,6 +1,6 @@
 cask 'resolume-arena' do
-  version '6.0.8.60887'
-  sha256 '20308b7e5f012f9d98e22d5bdc666cb6e9e4329e8595ca05098267176995f284'
+  version '6.0.10'
+  sha256 'e7279853a3605fdc8d426f0ad2ded4ee4dcdfb968cb8fd30aae6ee621de46231'
 
   url "https://resolume.com/download/Resolume_Arena_#{version.major_minor_patch.dots_to_underscores}_Installer.dmg"
   appcast 'https://resolume.com/update/arena_mac.xml'
