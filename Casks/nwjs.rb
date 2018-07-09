0e199bb50841dcc818f6b31cec07d1c080fd121f
Brian Morton <brianmortondesign@gmail.com>
2018-07-09 12:20:00 -0700

    Update nwjs to 0.31.5 (#49408)

--- Casks/nwjs.rb
+++ Casks/nwjs.rb
@@ -1,6 +1,6 @@
 cask 'nwjs' do
-  version '0.31.4'
-  sha256 'cb18cea2a1e46052e6dcc52d49b825dd32fe020e4bc0ea529fbdf1920bf7aba9'
+  version '0.31.5'
+  sha256 'b6a632f28d65eab7745ae1ec6a4e94a25c880c5d19436b8f2741c30d05a567fb'
 
   url "https://dl.nwjs.io/v#{version}/nwjs-sdk-v#{version}-osx-x64.zip"
   appcast 'https://github.com/nwjs/nw.js/releases.atom'
