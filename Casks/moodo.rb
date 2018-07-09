259d05febb8dc654aada267d65fd67d98faa0505
Brian Morton <brianmortondesign@gmail.com>
2018-07-09 09:41:35 -0700

    Update moodo to 1.1.3

--- Casks/moodo.rb
+++ Casks/moodo.rb
@@ -1,6 +1,6 @@
 cask 'moodo' do
-  version '1.1.2'
-  sha256 '5a4b2dcefdfaccf14feb64deb9109b18352b2670ee3dda8f718e425bbc2d6201'
+  version '1.1.3'
+  sha256 '986d21068d25d7207f93f07901f48a387c36e59a6882b443df606ce3a846abb0'
 
   # github.com/MooDoApp/MooDoApp.github.io was verified as official when first introduced to the cask
   url "https://github.com/MooDoApp/MooDoApp.github.io/releases/download/v#{version}/Moo.do-#{version}-mac.zip"
