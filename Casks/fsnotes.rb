4a4616804d9aaeac632760e0a57019d2e9ef5f91
Brian Morton <brianmortondesign@gmail.com>
2018-07-09 09:35:52 -0700

    Update fsnotes to 1.9

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask 'fsnotes' do
-  version '1.8,b154'
-  sha256 'b0e96b44bf144194090bb90a4bfc2cafbcb6f92971c8c01e359257d67e2e82de'
+  version '1.9'
+  sha256 'c6695a1a8103dc1765f00fdd69a674c0b1338d55526bb89f97b50efc3a3c3e66'
 
   # github.com/glushchenko/fsnotes was verified as official when first introduced to the cask
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version.before_comma}/FSNotes_#{version.before_comma}#{version.after_comma}.zip"
