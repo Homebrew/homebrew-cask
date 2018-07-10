b00c7609e26e1e7a8e82fa09afc3657182127621
Alex Arslan <ararslan@comcast.net>
2018-07-10 16:31:27 -0700

    Update Julia to v0.6.4 (#49490)

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask 'julia' do
-  version '0.6.3'
-  sha256 'd912af363addbbeefc1edec53f4b7c0eff8d838805f623d80d883df31c12cab4'
+  version '0.6.4'
+  sha256 'b2f95e60ea70363d2146caa6c3168bf8f5b249e8be46d992267f3f4957dcd8bf'
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
   name 'Julia'
