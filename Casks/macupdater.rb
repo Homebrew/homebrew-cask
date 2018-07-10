88ac3035ff0b22554e26181ca45c811a65691eb8
Vítor Galvão <info@vitorgalvao.com>
2018-07-10 16:19:21 +0100

    Update macupdater.rb

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -8,4 +8,12 @@ cask 'macupdater' do
   homepage 'https://www.corecode.io/macupdater/index.html'
 
   app 'MacUpdater.app'
+
+  zap trash: [
+               '~/Library/Application Support/MacUpdater/',
+               '~/Library/Caches/com.corecode.MacUpdater',
+               '~/Library/Containers/com.corecode.MacUpdaterLaunchHelper',
+               '~/Library/Application Scripts/com.corecode.MacUpdaterLaunchHelper/',
+               '~/Library/Preferences/com.corecode.MacUpdater.plist',
+             ]
 end
