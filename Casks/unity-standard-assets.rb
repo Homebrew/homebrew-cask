6e4c063d0b259b7d4832bebc98a8a9170e99f9d2
Brian Morton <brianmortondesign@gmail.com>
2018-07-10 10:54:21 -0700

    update unity-standard assets to 2018.2.0f2,787658998520

--- Casks/unity-standard-assets.rb
+++ Casks/unity-standard-assets.rb
@@ -1,6 +1,6 @@
 cask 'unity-standard-assets' do
-  version '2018.1.7f1,4cb482063d12'
-  sha256 'a9762467ca1fb32bab7c1b12baee306662fc29e84d1d26bc551102e508e331ed'
+  version '2018.2.0f2,787658998520'
+  sha256 'abc123'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacStandardAssetsInstaller/StandardAssets-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/download/archive'
