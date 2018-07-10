db93c84a109ad5b8118073f9dfa51f195331b727
commitay <commitay@users.noreply.github.com>
2018-07-10 20:05:18 +1000

    script: various (#48662)

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -23,43 +23,44 @@ for check in "${checks[@]}"; do
   "${check}" > "${HOME}/cask-checks/before/${check}"
 done
 
-modified_ruby_files=($(git diff --name-only --diff-filter=AMR "${TRAVIS_COMMIT_RANGE}" -- *.rb))
-
-for file in "${modified_ruby_files[@]}"; do
-  [[ "${file}" == 'Casks/'* ]] && modified_casks+=("${file}") || casks_wrong_dir+=("${file}")
-done
+modified_casks=($(git diff --name-only --diff-filter=AMR "${TRAVIS_COMMIT_RANGE}" -- Casks/*.rb))
 
 run export HOMEBREW_NO_AUTO_UPDATE=1
 
-if [[ ${#casks_wrong_dir[@]} -gt 0 ]]; then
-  odie "Casks added outside Casks directory: ${casks_wrong_dir[*]}"
-elif [[ ${#modified_casks[@]} -gt 0 ]]; then
-  if [[ ${#modified_casks[@]} -le 3 ]]; then
-    for cask in "${modified_casks[@]}"; do
-      if brew cask _stanza gpg "${cask}" &> /dev/null; then
-        run brew outdated gnupg || run brew upgrade gnupg
-      fi
-    done
+if [[ ${#modified_casks[@]} -eq 0 ]]; then
+  onoe 'No Casks modified, skipping'
+fi
+
+run brew cask style "${modified_casks[@]}"
+
+if [[ "${TRAVIS_REPO_SLUG}" != 'Homebrew/homebrew-cask-fonts' ]]; then
+  if [[ ${#modified_casks[@]} -gt 1 ]]; then
+    run brew cask audit "${modified_casks[@]}"
+    odie "More than 1 Cask modified, didn't check Cask checksums or URLs"
   fi
-  run brew cask _audit_modified_casks "${TRAVIS_COMMIT_RANGE}"
-  run brew cask style "${modified_casks[@]}"
-  if [[ ${#modified_casks[@]} -le 3 ]]; then
-    if /usr/bin/grep "depends_on cask:" "${modified_casks[@]}" > /dev/null; then
-      run brew tap homebrew/bundle
-      run brew bundle dump --file="${HOME}/Brewfile"
+
+  # check if gnupg is up to date for _audit_modified_casks
+  for cask in "${modified_casks[@]}"; do
+    if brew cask _stanza gpg "${cask}" &> /dev/null; then
+      run brew outdated gnupg || run brew upgrade gnupg
     fi
-    for cask in "${modified_casks[@]}"; do
-      run brew cask reinstall --verbose "${cask}"
-      run brew cask uninstall --verbose "${cask}"
-    done
-    if [[ -f "${HOME}/Brewfile" ]]; then
-      run brew bundle cleanup --force --file="${HOME}/Brewfile"
-    fi
-  else
-    ohai 'More than 3 Casks modified, skipping install'
-  fi
-else
-  ohai 'No Casks modified, skipping'
+  done
+fi
+
+run brew cask _audit_modified_casks "${TRAVIS_COMMIT_RANGE}"
+
+if /usr/bin/grep --quiet "depends_on cask:" "${modified_casks[@]}"; then
+  run brew tap homebrew/bundle
+  run brew bundle dump --file="${HOME}/Brewfile"
+fi
+
+for cask in "${modified_casks[@]}"; do
+  run brew cask reinstall --verbose "${cask}"
+  run brew cask uninstall --verbose "${cask}"
+done
+
+if [[ -f "${HOME}/Brewfile" ]]; then
+  run brew bundle cleanup --force --file="${HOME}/Brewfile"
 fi
 
 sleep 5 # Rerunning the checks too soon can result in false positives
