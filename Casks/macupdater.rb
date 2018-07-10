87de6895d11d4118f6f49c13f3d0483ead98f894
suschizu <brigitte@corecode.io>
2018-07-10 18:32:56 +0200

    Add MacUpdater.app Latest (#49438)
    * Add MacUpdater.app Latest
    
    * Update macupdater.rb
    
    * Update macupdater.rb

---
+++ Casks/macupdater.rb
@@ -0,0 +1,19 @@
+cask 'macupdater' do
+  version '1.2.10'
+  sha256 'c7a0049e5fb6b4baa86c2fc8381ea6b9af746542382692f335885d52a5a3fd99'
+
+  url "https://www.corecode.io/downloads/macupdater_#{version}.zip"
+  appcast 'https://www.corecode.io/macupdater/macupdater.xml'
+  name 'MacUpdater'
+  homepage 'https://www.corecode.io/macupdater/index.html'
+
+  app 'MacUpdater.app'
+
+  zap trash: [
+               '~/Library/Application Support/MacUpdater/',
+               '~/Library/Caches/com.corecode.MacUpdater',
+               '~/Library/Containers/com.corecode.MacUpdaterLaunchHelper',
+               '~/Library/Application Scripts/com.corecode.MacUpdaterLaunchHelper/',
+               '~/Library/Preferences/com.corecode.MacUpdater.plist',
+             ]
+end
