61cfa76fe1bad20532aee66b93d60a779c7dfcbb
Austin M. Matherne <AustinMatherne@users.noreply.github.com>
2018-07-10 13:28:01 -0500

    Fix studio-3t sha
    install fails because download doesn't match expected sha

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask 'studio-3t' do
   version '2018.3.2'
-  sha256 '9854e2ec74687a131fe7392dd9a4c0c4256d5ea5ffb4dc2370965dd211f08957'
+  sha256 '9ea3a66fc952260eec335526ddb5f692064b919de4335446341cd64358c0d014'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'https://files.studio3t.com/changelog/changelog.txt'
