b982ce93bfd5ef67d112c4f49a7857e8558267c4
alswl <alswlx@gmail.com>
2018-07-10 12:47:31 +0800

    fix: antdevtools download url

--- Casks/antdevtools.rb
+++ Casks/antdevtools.rb
@@ -2,6 +2,7 @@ cask 'antdevtools' do
   version '0.20.1,63d9b84e-bbcf-4a00-9427-0bb3f713c769'
   sha256 '4c9e207838ad0f7c6159d4ebd8c828d54b1aec28622a9f67f4f4bd0acf93664a'
 
+  # gw.alipayobjects.com was verified as official when first introduced to the cask
   url "https://gw.alipayobjects.com/os/Ant-devtools-Release/#{version.after_comma}/#{version.major}_#{version.minor}_#{version.patch}/AntDevtools.dmg"
   name '蚂蚁开发者工具'
   homepage 'https://mini.open.alipay.com/channel/miniIndex.htm'
