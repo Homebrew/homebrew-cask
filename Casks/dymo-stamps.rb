5d2baae251b834ba71876f0337043e35474510c6
Claudia <claui@users.noreply.github.com>
2018-07-11 09:28:01 +0200

    dymo-stamps: fix homepage

--- Casks/dymo-stamps.rb
+++ Casks/dymo-stamps.rb
@@ -2,10 +2,9 @@ cask 'dymo-stamps' do
   version :latest
   sha256 :no_check
 
-  # endicia.com/dymostamps was verified as official when first introduced to the cask
   url 'http://download.endicia.com/dymostamps/dymostamps.dmg'
   name 'Dymo Stamps'
-  homepage 'https://www.dymo.com/en-US/online-support'
+  homepage 'http://mac.endicia.com/stamps/'
 
   depends_on cask: 'dymo-label'
 
