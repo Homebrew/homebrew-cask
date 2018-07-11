eb83feb01a913050e4ac63f5a6a1d2f7a45e1c16
Carlos Nunez <dev@carlosnunez.me>
2018-07-11 15:29:29 -0500

    Add SmoothScroll, an awesome application that makes every app on your Mac have stupid smooth scrolling.

---
+++ Casks/smoothscroll.rb
@@ -0,0 +1,12 @@
+cask 'smoothscroll' do
+  version '1.1.8'
+  sha256 '58030937437dac1dd914d9fef6950dc1a44f8762d7604b037650cf7873d5573d'
+
+  url 'https://www.smoothscroll.net/mac/download/SmoothScroll.app.zip'
+  name 'SmoothScroll'
+  homepage 'https://www.smoothscroll.net/'
+
+  app 'SmoothScroll.app'
+
+  caveats 'gSmoothScroll is not free and comes with a 21-day free trial.'
+end
