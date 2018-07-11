63214aba861a09844343632ace1bf79e7b514119
Brian Morton <brianmortondesign@gmail.com>
2018-07-11 04:49:12 -0700

    Update kitty to 0.11.3 (#49467)

--- Casks/kitty.rb
+++ Casks/kitty.rb
@@ -1,6 +1,6 @@
 cask 'kitty' do
-  version '0.11.2'
-  sha256 'bbcfc56ee2ecd85d74950225023e13355d73224a69f1626db202a425e5374a72'
+  version '0.11.3'
+  sha256 '1186f8e7a2ae3a5c7d70ac29e4a607bc52813b86988b602998a9183e57356d51'
 
   url "https://github.com/kovidgoyal/kitty/releases/download/v#{version}/kitty-#{version}.dmg"
   appcast 'https://github.com/kovidgoyal/kitty/releases.atom'
