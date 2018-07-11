dc9b5714dd3e527012e457ee2044f299251d66b2
commitay <commitay@users.noreply.github.com>
2018-07-12 09:03:59 +1000

    Update smoothscroll.rb

--- Casks/smoothscroll.rb
+++ Casks/smoothscroll.rb
@@ -3,6 +3,7 @@ cask 'smoothscroll' do
   sha256 '58030937437dac1dd914d9fef6950dc1a44f8762d7604b037650cf7873d5573d'
 
   url 'https://www.smoothscroll.net/mac/download/SmoothScroll.app.zip'
+  appcast 'https://updater.smoothscroll.net/mac/updater.xml'
   name 'SmoothScroll'
   homepage 'https://www.smoothscroll.net/'
 
