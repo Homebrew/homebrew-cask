53c52d234c74c95fe9cfc9a3188e0d7fe4aade90
weqopy <weqopy@gmail.com>
2018-07-12 22:06:37 +0800

    Update ticktick to 2.5.50,70

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -1,6 +1,6 @@
 cask 'ticktick' do
-  version '2.4.00,62'
-  sha256 'e9d822a1ecb5aeaf3abf6cd1674d20647d813a90633264d6ff3586e0b311b7a7'
+    version '2.5.50,70'
+  sha256 '9230e99db6318d75934c896a8b395b9707f72af93ac9dab99a07ecb6d82cc571'
 
   # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
