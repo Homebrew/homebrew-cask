30a15f7d71fc2002ff74580a379fc275377412de
weqopy <weqopy@gmail.com>
2018-07-12 22:22:06 +0800

    Update ticktick to 2.5.50,70. Delete extra spaces

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -1,5 +1,5 @@
 cask 'ticktick' do
-    version '2.5.50,70'
+  version '2.5.50,70'
   sha256 '9230e99db6318d75934c896a8b395b9707f72af93ac9dab99a07ecb6d82cc571'
 
   # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
