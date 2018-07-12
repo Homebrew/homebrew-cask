90e8cfef5708c8a891fbfacbfd06190e22a1d5b1
commitay <commitay@users.noreply.github.com>
2018-07-13 00:41:59 +1000

    Update mi to 3.0.0 (#49552)

--- Casks/mi.rb
+++ Casks/mi.rb
@@ -3,6 +3,7 @@ cask 'mi' do
   sha256 '6239a38e289a00555341ee55dbc97d99a11a1fca9b4f1724fac194df73167727'
 
   url "https://www.mimikaki.net/download/mi#{version}.dmg"
+  appcast 'https://www.mimikaki.net/download/appcast.xml'
   name 'mi'
   homepage 'https://www.mimikaki.net/'
 
