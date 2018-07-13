78f1611b40a5f9e1ea08d6e41909b89ab4a4add2
commitay <commitay@users.noreply.github.com>
2018-07-13 21:44:48 +1000

    Update leap to 3.8.5

--- Casks/leap.rb
+++ Casks/leap.rb
@@ -1,8 +1,9 @@
 cask 'leap' do
-  version :latest
-  sha256 :no_check
+  version '3.8.5'
+  sha256 '3a4c6844dfa79c113ea604de45e275c071e7696b61384471586db043e1565698'
 
   url 'https://www.ironicsoftware.com/downloads/Leap.zip'
+  appcast 'https://ironicsoftware.com/downloads/leap.xml'
   name 'Ironic Software Leap'
   homepage 'https://www.ironicsoftware.com/leap/'
 
