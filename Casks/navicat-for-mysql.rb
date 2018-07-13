980253a2c72772c546fb54ef35c08f74e92e53e2
Brian Morton <brianmortondesign@gmail.com>
2018-07-13 16:11:29 -0700

    Update navicat-for-mysql to 12.0.28 (#49638)

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-mysql' do
-  version '12.0.27'
-  sha256 'd5cad433d6c765b04c895b5224b55270c6431b2992ee42457833f2e4bdd7766a'
+  version '12.0.28'
+  sha256 '392f436f8061926a61b914a9fb66b2737e47c1d25e98c9f2302c594ce75b5247'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
   appcast 'https://www.navicat.com/en/products/navicat-for-mysql-release-note'
