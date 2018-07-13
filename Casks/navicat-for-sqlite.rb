10bc1fdcaac4bc1b545b0074a5a4ff1f4bb2f596
Brian Morton <brianmortondesign@gmail.com>
2018-07-13 16:12:03 -0700

    Update navicat-for-sqlite to 12.0.28 (#49642)

--- Casks/navicat-for-sqlite.rb
+++ Casks/navicat-for-sqlite.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-sqlite' do
-  version '12.0.27'
-  sha256 'd195019798cb4bfd80ab714a21a8422cbcffca79d58dc26a762c29f94005b74e'
+  version '12.0.28'
+  sha256 '077f289c71222d4b8fd282526a9cfc1b8b089b95e11e52d79ce1393263fce99c'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlite_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-sqlite-release-note'
