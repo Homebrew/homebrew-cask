96972f86781ca0f096f15249cdbe2343a5db4611
greenrock <dd331@163.com>
2018-07-13 22:52:43 +0800

    Update wechatwebdevtools to 1.02.1807120 (#49624)

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,6 +1,6 @@
 cask 'wechatwebdevtools' do
-  version '1.02.1806120'
-  sha256 '8833fb2e3c6c35b249b27453bcadeec9b6147dd8d269d2a23b05a9b8d47cb6ae'
+  version '1.02.1807120'
+  sha256 'ada2634be9262cf7cfbee57e5ab12a03d48dcdcda6f458cf0318455364824950'
 
   url "https://dldir1.qq.com/WechatWebDev/#{version.major}.0.0/20#{version.patch}/wechat_devtools_#{version}.dmg"
   name 'wechat web devtools'
