fc91628c7289c36bd67f9552030a064ecf563f7d
Brian Morton <brianmortondesign@gmail.com>
2018-07-12 17:21:25 -0700

    Update winds to 2.1.49 (#49589)

--- Casks/winds.rb
+++ Casks/winds.rb
@@ -1,6 +1,6 @@
 cask 'winds' do
-  version '2.1.47'
-  sha256 '65efab23915e3ef10d664648addc03fff3dfcd9cb9d2c32a5711e256e907f71d'
+  version '2.1.49'
+  sha256 '652d72efc757640858300551298d72239b6d532669064883e7a7216814dd557a'
 
   # s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/winds-#{version.major}.0-releases/releases/Winds-#{version}.dmg"
