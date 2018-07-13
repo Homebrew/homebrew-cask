e90dda581cc158229e1a647c3fadca6d9ca84a84
commitay <commitay@users.noreply.github.com>
2018-07-13 14:00:18 +1000

    Update mailspring to 1.2.2

--- Casks/mailspring.rb
+++ Casks/mailspring.rb
@@ -1,8 +1,10 @@
 cask 'mailspring' do
-  version :latest
-  sha256 :no_check
+  version '1.2.2'
+  sha256 '97ac5476e26fa698a6dfb3a87b7a1f14742f6f0feef6e5be39fa350eb4cf15ea'
 
-  url 'https://updates.getmailspring.com/download?platform=darwin'
+  # github.com/Foundry376/Mailspring was verified as official when first introduced to the cask
+  url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring.zip"
+  appcast 'https://github.com/Foundry376/Mailspring/releases.atom'
   name 'Mailspring'
   homepage 'https://getmailspring.com/'
 
