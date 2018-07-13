8dc6b7b694601349f61fcdd33c7482f34d3bb251
commitay <commitay@users.noreply.github.com>
2018-07-13 20:42:11 +1000

    Update itools to 2.9.2

--- Casks/itools.rb
+++ Casks/itools.rb
@@ -4,6 +4,7 @@ cask 'itools' do
 
   # dl2.itools.hk/dl was verified as official when first introduced to the cask
   url "http://dl2.itools.hk/dl/iTools64_#{version}.dmg"
+  appcast 'http://dl2.itools.hk/update/iTools64ForMacCast.xml'
   name 'iTools'
   homepage 'http://pro.itools.cn/mac/english'
 
