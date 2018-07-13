c690e10c5eed1a3055117b894261d3f3c6d33c63
Brian Morton <brianmortondesign@gmail.com>
2018-07-12 17:18:55 -0700

    Update texturepacker to 4.8.2 (#49573)

--- Casks/texturepacker.rb
+++ Casks/texturepacker.rb
@@ -1,6 +1,6 @@
 cask 'texturepacker' do
-  version '4.8.1'
-  sha256 'b27885af637ce4aa906fb41bf67f7e271c51153cdf3be2a662adae14881ebf00'
+  version '4.8.2'
+  sha256 'bdbe67662cae2ae2f10ab72ed5198c2996e9520b869f26f1c52b7bd42a46cbf0'
 
   url "https://www.codeandweb.com/download/texturepacker/#{version}/TexturePacker-#{version}-uni.dmg"
   appcast 'https://www.codeandweb.com/releases/TexturePacker/appcast-mac-release.xml'
