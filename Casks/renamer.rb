444a31bb7ca7efdf5c0445b4786f3b8a94e335da
commitay <commitay@users.noreply.github.com>
2018-07-13 20:49:38 +1000

    Update renamer to 5.2.0

--- Casks/renamer.rb
+++ Casks/renamer.rb
@@ -1,9 +1,10 @@
 cask 'renamer' do
-  version '5'
-  sha256 :no_check # required as upstream package is updated in-place
+  version '5.2.0'
+  sha256 'ccc339c82229ff228b6a56b8f087bd21e2f2e7b8c045fa98d4fdbc8715864bb9'
 
   # storage.googleapis.com/incrediblebee was verified as official when first introduced to the cask
-  url "https://storage.googleapis.com/incrediblebee/apps/Renamer-#{version.major}/Renamer.zip"
+  url "https://storage.googleapis.com/incrediblebee/apps/Renamer-#{version.major}/Renamer-#{version}.zip"
+  appcast "https://api.incrediblebee.com/appcasts/renamer-#{version.major}.xml"
   name 'Renamer'
   homepage 'http://renamer.com/'
 
