edbd2f5f1bbba9f2dfea059e4230230f63c5d630
Brian Morton <brianmortondesign@gmail.com>
2018-07-13 17:48:53 -0700

    Update electron to 2.0.5 (#49651)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '2.0.4'
-  sha256 'd18b4b975fa84d8e50fe2c868d6ab3de636c5bd9a3988898723023b3ecb8bea5'
+  version '2.0.5'
+  sha256 'cb58c05cb79e802a5ff4aa8bb8c3190ef072241e3a9e53f1afc56ae16f80e463'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
