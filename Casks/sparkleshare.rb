d166adf9be060a2ae52eea5c03077675dbca846a
Brian Morton <brianmortondesign@gmail.com>
2018-07-16 12:14:32 -0700

    Update sparkleshare to 3.28

--- Casks/sparkleshare.rb
+++ Casks/sparkleshare.rb
@@ -1,6 +1,6 @@
 cask 'sparkleshare' do
-  version '2.0.1'
-  sha256 'e8c393e380cd26bcf9dd28f02c74d5532b69a98723de8b85bfdf9c13972f826c'
+  version '3.28'
+  sha256 'd0e561706b65d379ae947f77a2fc443395b69462d1dd968ac334155c73a38381'
 
   # github.com/hbons/SparkleShare was verified as official when first introduced to the cask
   url "https://github.com/hbons/SparkleShare/releases/download/#{version}/sparkleshare-mac-#{version}.zip"
