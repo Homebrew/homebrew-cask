3b3472600223fdd1af89cf331b3f8f1687b15613
chris <mail@chohner.com>
2018-07-16 17:47:47 +0200

    Update sonic-visualiser to 3.1,2382 (#49677)

--- Casks/sonic-visualiser.rb
+++ Casks/sonic-visualiser.rb
@@ -1,6 +1,6 @@
 cask 'sonic-visualiser' do
-  version '3.0.3,2249'
-  sha256 '5900b988b32c929c7f422d5d575cb221a62099b7a4bd49051233e327e3630070'
+  version '3.1,2382'
+  sha256 '9994e17ef42218fad5fcf442ce3df939599a10972fd594080ccdf9408bede8ab'
 
   # code.soundsoftware.ac.uk was verified as official when first introduced to the cask
   url "https://code.soundsoftware.ac.uk/attachments/download/#{version.after_comma}/Sonic%20Visualiser-#{version.before_comma}.dmg"
