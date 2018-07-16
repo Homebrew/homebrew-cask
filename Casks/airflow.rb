704ac6e1d853f9a20587b53a7712ccda55a08eeb
Brian Morton <brianmortondesign@gmail.com>
2018-07-16 13:23:07 -0700

    Update airflow to 2.3.9u1 (#49684)

--- Casks/airflow.rb
+++ Casks/airflow.rb
@@ -1,6 +1,6 @@
 cask 'airflow' do
-  version '2.3.7'
-  sha256 '67d1bd9c08031f321512c2ad41980abf296803d29c3a097bf9674e9a3112033c'
+  version '2.3.9u1'
+  sha256 '4562442dc8ed62c93ecb2d61c012fabeb5f8acddbfe0475f4d01b1668645cf85'
 
   # amazonaws.com/Airflow was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Airflow/Download/Airflow%20#{version}.zip"
