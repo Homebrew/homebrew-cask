e0b21b1a5ab28876f4051b4ee7f915fa58c5517b
Brian Morton <brianmortondesign@gmail.com>
2018-07-16 12:13:49 -0700

    Update rhodes-kite to 1.9.3

--- Casks/rhodes-kite.rb
+++ Casks/rhodes-kite.rb
@@ -1,6 +1,6 @@
 cask 'rhodes-kite' do
-  version '1.9.2'
-  sha256 '498259975bff1035111b789c1ad45e6ce210f834dbecb0d959027ee4ee185f4a'
+  version '1.9.3'
+  sha256 '2e38a2053feb13804da43ecea299211ebd194ac7afdae1bc8567aef6a6402cde'
 
   url "https://kiteapp.co/downloads/Kite-#{version}.zip"
   appcast 'https://api.kiteapp.co/kite_appcast.xml'
