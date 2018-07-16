188ace071a9e9165e81a882b5d9488584480458d
Reinhard Pointner <rednoah@users.noreply.github.com>
2018-07-16 22:02:24 +0700

    FileBot 4.8.2
    * Upgrade to `FileBot 4.8.2`
    * Apply `brew cask style --fix`

--- Casks/filebot.rb
+++ Casks/filebot.rb
@@ -1,19 +1,19 @@
 cask 'filebot' do
-  version '4.7.9'
-  sha256 'bae8ec7ee51f3c11b8c190c14199c42a299d72755dd9e14763ca87189dd1d687'
+  version '4.8.2'
+  sha256 '20cd5761bc51a213d31ab035f28831fb0e4d9097727a80e32f2d477fcccb0525'
 
-  # sourceforge.net/filebot was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/filebot/filebot/FileBot_#{version}/FileBot_#{version}-darwin.tar.xz"
+  url "https://get.filebot.net/filebot/FileBot_#{version}/FileBot_#{version}.app.tar.xz"
   appcast 'https://app.filebot.net/update.xml'
   name 'FileBot'
   homepage 'https://www.filebot.net/'
+  gpg "#{url}.asc", key_url: 'https://raw.githubusercontent.com/filebot/plugins/master/gpg/maintainer.pub'
 
-  # The darwin package only includes the CLI tools. Launching the app bundle merely redirects to the Mac App Store.
+  app 'FileBot.app'
   binary 'FileBot.app/Contents/MacOS/filebot.sh', target: 'filebot'
 
   zap trash: '~/Library/Preferences/net.filebot.ui.plist'
 
   caveats do
-    depends_on_java '8'
+    depends_on_java '10+'
   end
 end
