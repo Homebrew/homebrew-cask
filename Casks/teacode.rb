85bce374a0e23289c69b2161169c975fa037349e
commitay <commitay@users.noreply.github.com>
2018-07-16 15:20:26 +1000

    Update teacode.rb

--- Casks/teacode.rb
+++ Casks/teacode.rb
@@ -1,12 +1,11 @@
 cask 'teacode' do
-        version '1.0'
+  version '1.0'
   sha256 '09b2b9ac3df1e8b9f5351e98c3699c763359d5142ea4b8bcf159bb7e820b9bb6'
 
-  url 'https://www.apptorium.com/public/products/teacode/releases/TeaCode-1.0.zip'
+  url "https://www.apptorium.com/public/products/teacode/releases/TeaCode-#{version}.zip"
   appcast 'https://updates.devmate.com/com.apptorium.TeaCode-dm.xml'
   name 'TeaCode'
   homepage 'https://www.apptorium.com/teacode'
 
   app 'TeaCode.app'
 end
-
