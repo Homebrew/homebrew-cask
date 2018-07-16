298db25efc6a6dc1f8a92d49582118ba62070255
Lennart Lövstrand <lennart.github@lovstrand.com>
2018-07-16 17:49:15 +0200

    Update vuescan to 9.6.12 (#49680)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
-  version '9.6.11'
-  sha256 '6a3024b480b24ef9ec65a0dc4f693801307c1a51e050edc7170dc1eda3b2c119'
+  version '9.6.12'
+  sha256 'a5a7ce420a3c1f8a0ac9f650314aab437e104d1aa57774589340de090531d561'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/old-versions.html'
