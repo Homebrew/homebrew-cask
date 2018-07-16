5d5b089639f5bfec68e7481b2aa5d05cb8a31307
Daniel Compton <desk@danielcompton.net>
2018-07-17 10:33:40 +1200

    Update Harvest 2.1.7 checksum

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -1,6 +1,6 @@
 cask 'harvest' do
   version '2.1.7'
-  sha256 'cf07dccf7db0273cb2661bf202813f3a93a1a3d321447eebee5cf599acc9797d'
+  sha256 'a1dfc944180736cfff5de4ab1219f528ccbcd6f261df940dc68817f4fddf642b'
 
   url "https://www.getharvest.com/harvest/mac/Harvest.#{version}.zip"
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml'
