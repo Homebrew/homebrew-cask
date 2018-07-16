790df209d417fc4439dce12d4b8ef2014712c293
Yurii Kolesnykov <yurikoles@gmail.com>
2018-07-16 23:29:26 +0300

    update zeplin download url closes 49711

--- Casks/zeplin.rb
+++ Casks/zeplin.rb
@@ -2,9 +2,9 @@ cask 'zeplin' do
   version :latest
   sha256 :no_check
 
-  # downloads can be found at https://zeplin.io/support.html#download
+  # downloads can be found at https://support.zeplin.io/quick-start/downloading-mac-and-windows-apps
   # zpl.io was verified as official when first introduced to the cask
-  url 'https://zpl.io/download'
+  url 'https://api.zeplin.io/urls/download-mac'
   name 'Zeplin'
   homepage 'https://zeplin.io/'
 
