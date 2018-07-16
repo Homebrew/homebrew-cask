030c238b69f5f726172735dd25790678e4720169
mashehu <mashehu@users.noreply.github.com>
2018-07-16 12:37:58 +0200

    Update agenda to 2.3 (#49673)

--- Casks/agenda.rb
+++ Casks/agenda.rb
@@ -1,6 +1,6 @@
 cask 'agenda' do
-  version '2.2.1'
-  sha256 '9dac10129525250258422524d2f087e461e003237625d7dcee324a9d9097e6c0'
+  version '2.3'
+  sha256 '818b928bb6ac0bc61e4127b687cddcffcd12547cc5896c03a3601635bb65248e'
 
   url "https://downloads.agenda.com/Agenda_#{version}.zip"
   appcast 'https://downloads.agenda.com/AgendaSparkleAppcast.xml'
