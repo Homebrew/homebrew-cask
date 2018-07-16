c4b730b321f967e0e8359228e90899ef1a645bc8
jayiwang <jayiwang@gmail.com>
2018-07-16 13:11:41 +0800

    Update opera.rb

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,8 +1,8 @@
 cask 'opera' do
-  version '54.0.2952.41'
+  version '54.0.2952.54'
   sha256 '274f8516837d7743bfb0abe4039175f58229d5f1e5cd76d1f7602139154c1be2'
 
-  url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
+  url "https://get.geo.opera.com/ftp/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
   homepage 'https://www.opera.com/'
 
