c27019f58aa285813581ad098e9958c1c60dced5
Robin Naundorf <r.naundorf@fh-muenster.de>
2018-07-17 10:42:53 +0200

    Add new Cask for sciebo
    * Add Cask for sciebo version 2.4.1.2038

---
+++ Casks/sciebo.rb
@@ -0,0 +1,16 @@
+cask 'sciebo' do
+  version '2.4.1.2038'
+  sha256 '7de4af04d0187ad2ed849e06d608e16576c85a10be31f9ce08c24865a9688ccc'
+
+  url "https://www.sciebo.de/install/sciebo-#{version}.pkg"
+  name 'sciebo'
+  homepage 'https://sciebo.de/'
+
+  depends_on macos: '>= 10.7'
+
+  pkg "sciebo-#{version}.pkg"
+
+  uninstall pkgutil: [
+                       'de.sciebo.sciebo',
+                     ]
+end
