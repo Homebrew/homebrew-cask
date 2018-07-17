e971eab6c0f81b39e7608ffea0ea538b555e6481
Grettir Asmundarson <grettir@users.noreply.github.com>
2018-07-17 16:16:46 -0600

    Update farrago to 1.2.0

--- Casks/farrago.rb
+++ Casks/farrago.rb
@@ -1,6 +1,6 @@
 cask 'farrago' do
-  version '1.1.0'
-  sha256 'bd836293bf59cd137f2a8f0155794c7bc8010b451688237ae11206938815522b'
+  version '1.2.0'
+  sha256 '67fd873285aa2b698d159d419a567504676984ab6d7de64dcc7e91461cbf8ac7'
 
   url 'https://rogueamoeba.com/farrago/download/Farrago.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Farrago'
