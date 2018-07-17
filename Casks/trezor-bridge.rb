53eb1726c96817fb10784f39344cbbb651db3ec2
Brian Morton <brianmortondesign@gmail.com>
2018-07-17 11:43:06 -0700

    Update trezor-bridge to 2.0.19 (#49748)

--- Casks/trezor-bridge.rb
+++ Casks/trezor-bridge.rb
@@ -1,6 +1,6 @@
 cask 'trezor-bridge' do
-  version '2.0.14'
-  sha256 '01cdc05500c1d465300c73235d6cc65ad3eacb733144f10cc64768ba4e7c0ccf'
+  version '2.0.19'
+  sha256 'b55c4ab275776b97947fb34d44423b429271d689f412ff6e1a28e6152dca88d4'
 
   url "https://wallet.trezor.io/data/bridge/#{version}/trezor-bridge-#{version}.pkg"
   appcast 'https://wallet.trezor.io/data/bridge/latest.txt'
