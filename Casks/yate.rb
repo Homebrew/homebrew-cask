df6d67d9cc7228b83a9d650f9a55ce439ec16f79
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2018-07-17 16:38:31 -0600

    Update yate to 4.2.0.1 (#49761)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,6 +1,6 @@
 cask 'yate' do
-  version '4.1.0.3'
-  sha256 '5f8a9baa1223f7b1d9431c2c42f7ce5e207c54bbd00c71a5ede6bfb3c7b1c6f4'
+  version '4.2.0.1'
+  sha256 '929cd754a29893cadf42f5b24535a9e52650175a7b0365892acd04aad72b6b20'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml'
