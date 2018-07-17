7d16c6ab8ce6722e2e522510f00ba057910befe0
Brian Morton <brianmortondesign@gmail.com>
2018-07-17 16:48:58 -0700

    re-add version num to dl url

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -2,7 +2,7 @@ cask 'zoc' do
   version '7.20.0'
   sha256 '417f24325fb1ffe6b9f8b064b0560f3e93ef9792c5f56a7e3ddb0cfdd6641f15'
 
-  url 'https://www.emtec.com/downloads/zoc/zoc.dmg'
+  url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast 'https://www.emtec.com/downloads/zoc/zoc_changes.txt'
   name 'ZOC'
   homepage 'https://www.emtec.com/zoc/'
