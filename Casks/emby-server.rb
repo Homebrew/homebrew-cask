fe63b49f6d5131ff73b7224f20dfb107c7d5b8d6
commitay <commitay@users.noreply.github.com>
2018-07-19 09:16:07 +1000

    Update emby-server.rb

--- Casks/emby-server.rb
+++ Casks/emby-server.rb
@@ -2,7 +2,7 @@ cask 'emby-server' do
   version '3.5.0.0'
   sha256 '2f9dfaa0e11a4fb4f7f88557bee3e58881f3084f672587c5405f9d8dcd972600'
 
-  # github.com/MediaBrowser/Emby was verified as official when first introduced to the cask
+  # github.com/MediaBrowser/Emby.Releases was verified as official when first introduced to the cask
   url "https://github.com/MediaBrowser/Emby.Releases/releases/download/#{version}/embyserver-osx-x64-#{version}.zip"
   appcast 'https://github.com/MediaBrowser/Emby.Releases/releases.atom'
   name 'Emby Server'
