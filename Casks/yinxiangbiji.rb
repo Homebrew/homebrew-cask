a6a1737a1b16bdf9b4838107644cdc8f8dca9cf5
秦新 <qinxin_0720@live.com>
2018-07-18 11:18:47 +0800

    Update yinxiangbiji.rb

--- Casks/yinxiangbiji.rb
+++ Casks/yinxiangbiji.rb
@@ -1,13 +1,13 @@
 cask 'yinxiangbiji' do
   version '8.1.1_456907'
   sha256 '307cf468c3e8af2a1977fceedc63fc32765b555a730413958b279875bad05d0c'
-  
+
   # cdn.yinxiang.com/mac-smd/public was verified as official when first introduced to the cask
   url 'https://cdn.yinxiang.com/mac-smd/public/YinxiangBiji_RELEASE_#{version}.dmg'
   name '印象笔记'
   homepage 'https://www.yinxiang.com'
-  
+
   auto_updates true
-  
+
   app '印象笔记.app'
 end
