2d4348301a3c23b7d55a69ea5752cfdf2815db8f
commitay <commitay@users.noreply.github.com>
2018-07-19 01:03:40 +1000

    Update sciebo.rb

--- Casks/sciebo.rb
+++ Casks/sciebo.rb
@@ -3,14 +3,12 @@ cask 'sciebo' do
   sha256 '7de4af04d0187ad2ed849e06d608e16576c85a10be31f9ce08c24865a9688ccc'
 
   url "https://www.sciebo.de/install/sciebo-#{version}.pkg"
-  name 'sciebo'
+  name 'Sciebo'
   homepage 'https://sciebo.de/'
 
-  depends_on macos: '>= 10.7'
+  depends_on macos: '>= :lion'
 
   pkg "sciebo-#{version}.pkg"
 
-  uninstall pkgutil: [
-                       'de.sciebo.sciebo',
-                     ]
+  uninstall pkgutil: 'de.sciebo.sciebo'
 end
