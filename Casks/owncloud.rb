81136b458e5b331cdc58d8bea57f0a0a61215d35
Brian Morton <brianmortondesign@gmail.com>
2018-07-18 09:23:33 -0700

    Update owncloud to 2.4.2.10200

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,8 +1,8 @@
 cask 'owncloud' do
-  version '2.4.1.9367'
-  sha256 '7f3a88d47cabdecf30681793e16a2f892224fa769d37af0bcd5472b3241b7386'
+  version '2.4.2.10200'
+  sha256 '7f3591c001702fed0cf9d16edc66af7944200238fff0400ec99e2cf3a42ef084'
 
-  url "https://download.owncloud.com/desktop/stable/ownCloud-#{version}.pkg"
+  url "https://download.owncloud.com/desktop/stable/ownCloud-qt5.6.2-#{version}.pkg"
   appcast 'https://github.com/owncloud/client/releases.atom'
   name 'ownCloud'
   homepage 'https://owncloud.com/'
