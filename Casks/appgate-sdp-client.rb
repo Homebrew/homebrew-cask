2e595868709a0144ae2f4fb2577c41bb994711a9
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2018-07-19 15:44:58 -0400

    Update the URL section for downloads

--- Casks/appgate-sdp-client.rb
+++ Casks/appgate-sdp-client.rb
@@ -2,6 +2,7 @@ cask 'appgate-sdp-client' do
   version '4.1.0'
   sha256 'a334916941a58299d7a5b89aaadf51ee9f7b612fd479af51792fbef6f7bb1081'
 
+  # sdpdownloads.cyxtera.com was verified as official when first introduced to the cask
   url "https://sdpdownloads.cyxtera.com/files/download/AppGate-SDP-#{version}/clients/AppGate-SDP-#{version}-Installer.dmg"
   name 'AppGate SDP Client for macOS'
   homepage 'https://www.cryptzone.com/downloadcenter/appgate-sdp'
