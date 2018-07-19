87a0b373f06f3fea2fce7154e273e48ab11ed804
Brian Morton <brianmortondesign@gmail.com>
2018-07-19 16:41:16 -0700

    add qt version to version string

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,13 +1,13 @@
 cask 'owncloud' do
-  version '2.4.2.10200'
+  version '2.4.2.10200,qt5.6.2'
   sha256 '7f3591c001702fed0cf9d16edc66af7944200238fff0400ec99e2cf3a42ef084'
 
-  url "https://download.owncloud.com/desktop/stable/ownCloud-qt5.6.2-#{version}.pkg"
+  url "https://download.owncloud.com/desktop/stable/ownCloud-#{version.after_comma}-#{version.before_comma}.pkg"
   appcast 'https://github.com/owncloud/client/releases.atom'
   name 'ownCloud'
   homepage 'https://owncloud.com/'
 
-  pkg "ownCloud-#{version}.pkg"
+  pkg "ownCloud-#{version.after_comma}-#{version.before_comma}.pkg"
 
   uninstall pkgutil: [
                        'com.ownCloud.client',
