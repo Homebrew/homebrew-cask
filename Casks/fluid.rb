8a175e37d9dd763e8a1d95729d2469135f2541c9
Brian Morton <brianmortondesign@gmail.com>
2018-07-19 15:55:58 -0700

    Update fluid to 2.0.2 (#49847)

--- Casks/fluid.rb
+++ Casks/fluid.rb
@@ -1,6 +1,6 @@
 cask 'fluid' do
-  version '2.0.1'
-  sha256 '5bbec56a8d1221ee96fb64a13bb1e5c735565907a2de2c544588ca4c22e4a863'
+  version '2.0.2'
+  sha256 'bcb77409f0453c3d6dadc5ee6d072a538f5c7f55ff2bc89eb709bef7c1356da8'
 
   url "http://fluidapp.com/dist/Fluid_#{version}.zip"
   appcast "http://fluidapp.com/appcast/fluid#{version.major}.rss"
