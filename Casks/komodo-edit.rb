994a8e80aa142c1f5796edfc6f43228527bcbce0
commitay <commitay@users.noreply.github.com>
2018-07-20 08:42:53 +1000

    Update komodo-edit.rb

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -3,10 +3,9 @@ cask 'komodo-edit' do
   sha256 '6615bd11dd7fe569d171c917721e6fcff0ee83f759f49dbda3d10b16497bf00d'
 
   url "https://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*}, '')}/Komodo-Edit-#{version}-macosx-x86_64.dmg"
+  appcast 'https://www.activestate.com/komodo-ide/downloads/edit'
   name 'Komodo Edit'
   homepage 'https://www.activestate.com/komodo-edit/'
-
-  appcase "http://docs.activestate.com/komodo/#{version.major}/get/relnotes/"
 
   app "Komodo Edit #{version.major}.app"
 end
