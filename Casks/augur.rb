cec37fe7b801c8ca74cef8ec4f9d88592b77c27c
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-07-19 22:41:52 -0700

    Add Augur.app v1.0.4

---
+++ Casks/augur.rb
@@ -0,0 +1,11 @@
+cask 'augur' do
+  version '1.0.4'
+  sha256 :no_check
+
+  url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-augur-#{version}.dmg"
+  appcast 'https://github.com/AugurProject/augur-app/releases.atom'
+  name 'Augur'
+  homepage 'https://github.com/AugurProject/augur-app/'
+
+  app 'augur.app'
+end
