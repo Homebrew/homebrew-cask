4759257922e2508a8687a385f92aeefc64469aca
Chenchen Zhu <isczhu@gmail.com>
2018-07-20 15:35:32 -0700

    Update igv to 2.4.13 (#49881)

--- Casks/igv.rb
+++ Casks/igv.rb
@@ -1,6 +1,6 @@
 cask 'igv' do
-  version '2.4.10'
-  sha256 '111a4ffed729cc8cd38cf0e56e773be3165ae68ecebc1ac4df231ec1c8683524'
+  version '2.4.13'
+  sha256 'fbb8cafc018182d0c68e3579d874a92629519d011d8b35ca8ebb7ad0566ab794'
 
   url "https://data.broadinstitute.org/igv/projects/downloads/#{version.major_minor}/IGV_#{version}.app.zip"
   name 'Integrative Genomics Viewer (IGV)'
