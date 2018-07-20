4b5bf96e79764c8fe93ed19c17917562511baf6f
Brian Morton <brianmortondesign@gmail.com>
2018-07-19 17:24:33 -0700

    Update owncloud to 2.4.2.10200 (#49799)
    * Update owncloud to 2.4.2.10200
    
    * add qt version to version string

--- Casks/owncloud.rb
+++ Casks/owncloud.rb
@@ -1,13 +1,13 @@
 cask 'owncloud' do
-  version '2.4.1.9367'
-  sha256 '7f3a88d47cabdecf30681793e16a2f892224fa769d37af0bcd5472b3241b7386'
+  version '2.4.2.10200,qt5.6.2'
+  sha256 '7f3591c001702fed0cf9d16edc66af7944200238fff0400ec99e2cf3a42ef084'
 
-  url "https://download.owncloud.com/desktop/stable/ownCloud-#{version}.pkg"
+  url "https://download.owncloud.com/desktop/stable/ownCloud-#{version.after_comma}-#{version.before_comma}.pkg"
   appcast 'https://github.com/owncloud/client/releases.atom'
   name 'ownCloud'
   homepage 'https://owncloud.com/'
 
-  pkg "ownCloud-#{version}.pkg"
+  pkg "ownCloud-#{version.after_comma}-#{version.before_comma}.pkg"
 
   uninstall pkgutil: [
                        'com.ownCloud.client',
