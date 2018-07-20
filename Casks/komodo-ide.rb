d53b8d20f071068840ebe11de7753b220daba47f
CoreCode <core-code@users.noreply.github.com>
2018-07-20 03:01:57 +0200

    Update komodo-ide to 11.1.0-91033 (#49864)
    * Update komodo-ide to 11.1.0-91033
    
    * Update komodo-ide.rb

--- Casks/komodo-ide.rb
+++ Casks/komodo-ide.rb
@@ -3,6 +3,7 @@ cask 'komodo-ide' do
   sha256 'b46304e7849a9c507d3e7602dd264be9961100aaf8ff3a6e7689cdeddeea1f49'
 
   url "https://downloads.activestate.com/Komodo/releases/#{version.sub(%r{-.*}, '')}/Komodo-IDE-#{version}-macosx-x86_64.dmg"
+  appcast 'https://www.activestate.com/komodo-ide/downloads/ide'
   name 'Komodo IDE'
   homepage 'https://www.activestate.com/komodo-ide/'
 
