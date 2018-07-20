0edc57e687fbd2d2ad10488deb889391ced879de
Brian Morton <brianmortondesign@gmail.com>
2018-07-20 10:46:15 -0700

    Update patchwork to 3.10.1

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,6 +1,6 @@
 cask 'patchwork' do
-  version '3.10.0'
-  sha256 '83c30a83329bfeebcdddb2a2723072b817305567e37cb754e10549eff75e9437'
+  version '3.10.1'
+  sha256 'dbf33a2954b22cd0bc833c292741ff05ff932f6b7bf16f86f2347021137d6a47'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}-mac.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom'
