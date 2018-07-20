325aecccf9a7bc1a2c56dac8d08fdb45e4867f52
Brian Morton <brianmortondesign@gmail.com>
2018-07-20 10:46:52 -0700

    Update pretzel to 0.0.19

--- Casks/pretzel.rb
+++ Casks/pretzel.rb
@@ -1,6 +1,6 @@
 cask 'pretzel' do
-  version '0.0.18'
-  sha256 'fe5d9bc77531b56f0b7a072fde26ba5788a4a5ab85c41c2f5242d306067f18d1'
+  version '0.0.19'
+  sha256 'c3d91a4db3b3fe8fd64db9ed037e568b324dc6276efdd7f3b586272222f027a9'
 
   url "https://download.pretzel.rocks/Pretzel-#{version}-mac.zip"
   appcast 'https://download.pretzel.rocks/latest-mac.json'
