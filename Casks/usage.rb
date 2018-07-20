7424b46e1b5ad821e5cbc759cd8aa5908821c949
Brian Morton <brianmortondesign@gmail.com>
2018-07-20 10:48:23 -0700

    Update usage to 1.4.3

--- Casks/usage.rb
+++ Casks/usage.rb
@@ -1,6 +1,6 @@
 cask 'usage' do
-  version '1.4.2'
-  sha256 'e42e63223b9e3a6882bc9b5eb4898c57c534573354cd2b6700c5704d7f7366c0'
+  version '1.4.3'
+  sha256 'a9a8c3a02027c9287ae895e6547540510e941b94d9d5035ae9cfd4b3ccca4b2b'
 
   url "https://mediaatelier.com/Usage/Usage_#{version}.zip"
   appcast 'https://mediaatelier.com/Usage/feed.php'
