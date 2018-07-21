6915812039e95d7b29a7d7b4ae42b982fd3f711f
Vítor Galvão <info@vitorgalvao.com>
2018-07-21 17:24:47 +0100

    Update augur.rb

--- Casks/augur.rb
+++ Casks/augur.rb
@@ -1,6 +1,6 @@
 cask 'augur' do
   version '1.0.4'
-  sha256 :no_check
+  sha256 '1964e2b15b537c870012751182d1b212704f321491eefb00dcc973fe8a7575fc'
 
   url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-augur-#{version}.dmg"
   appcast 'https://github.com/AugurProject/augur-app/releases.atom'
