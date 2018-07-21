2cd58a5ab4639fd35a5d79d2afb907536906faf0
Philip Molares <git@molar.es>
2018-07-21 17:40:40 +0200

    no style fails anymore

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -3,7 +3,6 @@ cask 'dcp-o-matic' do
   sha256 '4b3ae99dec3bbf5f348f58a47f4841b8272fc3fad8372e5def4f98ea1a9f0665'
 
   url "https://dcpomatic.com/dl.php?id=osx-main&version=#{version}"
-  
   appcast 'https://dcpomatic.com/download'
   name 'DCP-o-matic'
   homepage 'https://dcpomatic.com/'
