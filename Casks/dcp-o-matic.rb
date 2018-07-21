c2ebb8f9b686c533b47389bf88eba344656a35eb
Philip Molares <git@molar.es>
2018-07-21 17:04:43 +0200

    DCP-o-Matic.app 2.12.8

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,11 +1,18 @@
 cask 'dcp-o-matic' do
-  version '2.12.8'
-  sha256 '6daf2a2cf6f94486b365f994c42f93afde6c41dc4799a960766022ec5493c812'
+  version '2.12.6'
+  sha256 '4b3ae99dec3bbf5f348f58a47f4841b8272fc3fad8372e5def4f98ea1a9f0665'
 
   url "https://dcpomatic.com/dl.php?id=osx-main&version=#{version}"
+<<<<<<< HEAD
+  name 'DCP-o-matic 2'
+  homepage 'https://dcpomatic.com/'
+
+  app 'DCP-o-matic 2.app'
+=======
   appcast 'https://dcpomatic.com/download'
   name 'DCP-o-matic'
   homepage 'https://dcpomatic.com/'
 
   app "DCP-o-matic #{version.major}.app"
+>>>>>>> master
 end
