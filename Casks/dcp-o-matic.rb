8acf8766127116555e330a9e1a62374780911432
Philip Molares <git@molar.es>
2018-07-21 17:26:19 +0200

    fixed merge error

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -3,16 +3,10 @@ cask 'dcp-o-matic' do
   sha256 '4b3ae99dec3bbf5f348f58a47f4841b8272fc3fad8372e5def4f98ea1a9f0665'
 
   url "https://dcpomatic.com/dl.php?id=osx-main&version=#{version}"
-<<<<<<< HEAD
-  name 'DCP-o-matic 2'
-  homepage 'https://dcpomatic.com/'
-
-  app 'DCP-o-matic 2.app'
-=======
+  
   appcast 'https://dcpomatic.com/download'
   name 'DCP-o-matic'
   homepage 'https://dcpomatic.com/'
 
   app "DCP-o-matic #{version.major}.app"
->>>>>>> master
 end
