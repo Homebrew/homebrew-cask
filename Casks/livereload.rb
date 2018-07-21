f6bf74f276e737a95f2502d88fdcdb9e2fed849d
Julian Mayer <julian@corecode.io>
2018-07-21 12:23:35 +0200

    Update livereload to 2.3.84

--- Casks/livereload.rb
+++ Casks/livereload.rb
@@ -1,6 +1,6 @@
 cask 'livereload' do
-  version '2.3.74'
-  sha256 '7a192cda9c15efdeea00ca53153be0c649e3d413719f8291e4f1684c0132506b'
+  version '2.3.84'
+  sha256 '1c915d4956177cfa5981896cb060fd00743f3d74157ad4e189901d0123fd8c7f'
 
   url "http://download.livereload.com/LiveReload-#{version}.zip"
   appcast 'https://s3.amazonaws.com/download.livereload.com/LiveReload-Mac-appcast.xml'
