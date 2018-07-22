860a124afca28a3453ce78d3cb14dd3afd09e57b
Akash Nimare <akashnimare@users.noreply.github.com>
2018-07-22 19:27:48 +0530

    zulip: Remove mac os name from the zip file. (#49918)

--- Casks/zulip.rb
+++ Casks/zulip.rb
@@ -3,7 +3,7 @@ cask 'zulip' do
   sha256 '33f50ff39582a22b515c2d6c03073b6e1f98283e0f99355354c9bab25954486b'
 
   # github.com/zulip/zulip-electron was verified as official when first introduced to the cask
-  url "https://github.com/zulip/zulip-electron/releases/download/v#{version}/Zulip-#{version}-mac.zip"
+  url "https://github.com/zulip/zulip-electron/releases/download/v#{version}/Zulip-#{version}.zip"
   appcast 'https://github.com/zulip/zulip-electron/releases.atom'
   name 'Zulip'
   homepage 'https://zulipchat.com/'
