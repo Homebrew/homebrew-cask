4c91fdbd089ceff983da51474a3052e623202731
jomo <git@jomo.tv>
2018-07-22 05:20:59 +0200

    Update musicbrainz-picard to 2.0.1
    Also upgraded URL to https

--- Casks/musicbrainz-picard.rb
+++ Casks/musicbrainz-picard.rb
@@ -1,8 +1,9 @@
 cask 'musicbrainz-picard' do
-  version '2.0'
-  sha256 'a8ae8f9d863bbf7ae244762d986b45c3f62e57a3ff5f3776992db0a4b4defc35'
+  version '2.0.1'
+  sha256 '2a652ff5c562216033568cb33d0a2525cfd3043f43c67afdefaf4f8cd471cd5a'
 
-  url "http://ftp.musicbrainz.org/pub/musicbrainz/picard/MusicBrainz-Picard-#{version}.dmg"
+  # ftp.osuosl.org was verified as official when first introduced to the cask
+  url "https://ftp.osuosl.org/pub/musicbrainz/picard/MusicBrainz-Picard-#{version}.dmg"
   appcast 'https://picard.musicbrainz.org/changelog/'
   name 'MusicBrainz Picard'
   homepage 'https://picard.musicbrainz.org/'
