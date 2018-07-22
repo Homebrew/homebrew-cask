8231dbbd9ab4194a048f0117c6f0e5bb6249aac5
jomo <github@jomo.tv>
2018-07-22 06:26:51 +0200

    Update musicbrainz-picard to 2.0.1 (#49916)

--- Casks/musicbrainz-picard.rb
+++ Casks/musicbrainz-picard.rb
@@ -1,13 +1,13 @@
 cask 'musicbrainz-picard' do
-  version '2.0'
-  sha256 'a8ae8f9d863bbf7ae244762d986b45c3f62e57a3ff5f3776992db0a4b4defc35'
+  version '2.0.1'
+  sha256 '2a652ff5c562216033568cb33d0a2525cfd3043f43c67afdefaf4f8cd471cd5a'
 
   url "http://ftp.musicbrainz.org/pub/musicbrainz/picard/MusicBrainz-Picard-#{version}.dmg"
   appcast 'https://picard.musicbrainz.org/changelog/'
   name 'MusicBrainz Picard'
   homepage 'https://picard.musicbrainz.org/'
 
-  depends_on macos: '>= :lion'
+  depends_on macos: '>= :yosemite'
 
   app 'MusicBrainz Picard.app'
 
