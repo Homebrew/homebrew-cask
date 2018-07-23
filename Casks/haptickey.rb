68fe2d5148613b23fcca4ac9fd025576a6c988fc
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-23 23:11:20 +0100

    Actually fixed this following the document

--- Casks/haptickey.rb
+++ Casks/haptickey.rb
@@ -1,9 +1,10 @@
 cask 'haptickey' do
-  version 0.44
+  version '0.44'
   sha256 :no_check
   
-  url: "https://github.com/niw/HapticKey/releases/download/{version}/HapticKey.app.zip"
-  name: "haptickey"
-  homepage: "https://github.com/niw/HapticKey"
-  
+  url: 'https://github.com/niw/HapticKey/releases/download/{version}/HapticKey.app.zip'
+  name: 'haptickey'
+  homepage: 'https://github.com/niw/HapticKey'
+
+  app: 'HapticKey.app"
 end
