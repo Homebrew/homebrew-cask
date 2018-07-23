841799aa6265127d52d0dbd65a782236a42aaf4d
Julian Mayer <julian@corecode.io>
2018-07-23 19:06:59 +0200

    Update ocrkit to 18.4.19,cf4278314ef8e4b996e1b798d8eb92cf

--- Casks/ocrkit.rb
+++ Casks/ocrkit.rb
@@ -1,6 +1,6 @@
 cask 'ocrkit' do
-  version '15.11.24,62c3a0fc70ed4e5c64527ba781d634bc'
-  sha256 '39502f49af57eea167232cdb7cc71b9f2c102aea611dd11777ba95166368bb7a'
+  version '18.4.19,cf4278314ef8e4b996e1b798d8eb92cf'
+  sha256 'b8054ebca6e1417eac12255bb47f3320422f736dc6a8ceb70f3198386f9869ea'
 
   # exactcode.de was verified as official when first introduced to the cask
   url "https://dl.exactcode.de/tmp/#{version.after_comma}/OCRKit-#{version.before_comma}.dmg"
