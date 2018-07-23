82ada39547e3f8b7cb2daffa6355fa18c9f7d640
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-23 23:18:27 +0100

    Audit passed

--- Casks/haptickey.rb
+++ Casks/haptickey.rb
@@ -1,10 +1,11 @@
 cask 'haptickey' do
   version '0.44'
   sha256 :no_check
-  
-  url: 'https://github.com/niw/HapticKey/releases/download/{version}/HapticKey.app.zip'
-  name: 'haptickey'
-  homepage: 'https://github.com/niw/HapticKey'
 
-  app: 'HapticKey.app"
+  url "https://github.com/niw/HapticKey/releases/download/#{version}/HapticKey.app.zip"
+  appcast "https://github.com/niw/HapticKey/releases.atom"
+  name 'haptickey'
+  homepage 'https://github.com/niw/HapticKey'
+
+  app 'HapticKey.app'
 end
