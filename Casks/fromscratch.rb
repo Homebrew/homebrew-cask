24908cd04ec316c6768110da98202b87f8333dbb
Brian Morton <brianmortondesign@gmail.com>
2018-07-23 09:58:49 -0700

    Update fromscratch to 1.4.0

--- Casks/fromscratch.rb
+++ Casks/fromscratch.rb
@@ -1,9 +1,9 @@
 cask 'fromscratch' do
-  version '1.3.0'
-  sha256 'a3621940ae0eaafe7fdcabe038a027ffa6c609f8764e74edbfa91d24e419e960'
+  version '1.4.0'
+  sha256 '8cdfe9277c480fee3453ecfdd74cf5291be4574d3445ca46cfbad204395eecd5'
 
   # github.com/Kilian/fromscratch was verified as official when first introduced to the cask
-  url "https://github.com/Kilian/fromscratch/releases/download/v#{version}/FromScratch-darwin-x64-#{version}.zip"
+  url "https://github.com/Kilian/fromscratch/releases/download/v#{version}/FromScratch-#{version}.dmg"
   appcast 'https://github.com/Kilian/fromscratch/releases.atom'
   name 'FromScratch'
   homepage 'https://fromscratch.rocks/'
