ca7de25a04cb93656e7065865ba6df456365a3e6
Yurii Kolesnykov <yurikoles@gmail.com>
2018-07-23 22:47:44 +0300

    clover-configurator v5.1.4.0,CCG

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,8 +1,8 @@
 cask 'clover-configurator' do
-  version '4.60.3.4'
-  sha256 '42e23a824d2357083a1c81a55d4e1b4902fec9243aeaa21296b531358a05bc8c'
+  version '5.1.4.0,CCG'
+  sha256 '4c353c6a94aed7acd59fbf21517f7130a5655d7b2936ff6468aa13cc9fe5c476'
 
-  url 'http://mackie100projects.altervista.org/apps/cloverconf/10.10/CCV.zip'
+  url "http://mackie100projects.altervista.org/apps/cloverconf/#{version.after_comma}/builds-data/#{version.after_comma}.zip"
   appcast 'http://mackie100projects.altervista.org/apps/cloverconf/10.10/update.xml'
   name 'Clover Configurator'
   homepage 'http://mackie100projects.altervista.org/'
