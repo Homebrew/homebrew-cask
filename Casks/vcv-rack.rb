18fcf7dcfb2e0e7a7549a69ca44cee0dbc8cb6d8
CoreCode <core-code@users.noreply.github.com>
2018-07-23 10:12:49 +0200

    Update vcv-rack to 0.6.2b (#49927)

--- Casks/vcv-rack.rb
+++ Casks/vcv-rack.rb
@@ -3,6 +3,7 @@ cask 'vcv-rack' do
   sha256 '59bd6002ce861254c04e3e5dbc6892cc3f55d73d595f1f4f0b906214a977bb67'
 
   url "https://vcvrack.com/downloads/Rack-#{version}-mac.dmg"
+  appcast 'https://github.com/VCVRack/Rack/releases.atom'
   name 'VCV Rack'
   homepage 'https://vcvrack.com/'
 
