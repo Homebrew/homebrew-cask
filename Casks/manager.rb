d6fd0b0ce7bbe9b7aefb0d6d39a46b32a2df0562
Brian Morton <brianmortondesign@gmail.com>
2018-07-23 09:33:00 -0700

    Update manager to 18.7.44

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -1,6 +1,6 @@
 cask 'manager' do
-  version '18.7.36'
-  sha256 '93d3a582861c1a4bc822469cc78fd3768f5905e37dec7fe63e8dc452101b0e28'
+  version '18.7.44'
+  sha256 '628e703ace43b3e1f4706a59b8d47787aac227093d91fbe18b8e3c57186f91e4'
 
   # d2ap5zrlkavzl7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2ap5zrlkavzl7.cloudfront.net/#{version}/Manager.dmg"
