316eb04741df4fca846f44f26ace52a018512b62
Brian Morton <brianmortondesign@gmail.com>
2018-07-23 09:43:55 -0700

    Update vagrant-vmware-utility to 1.0.3

--- Casks/vagrant-vmware-utility.rb
+++ Casks/vagrant-vmware-utility.rb
@@ -1,6 +1,6 @@
 cask 'vagrant-vmware-utility' do
-  version '1.0.2'
-  sha256 'b6d60627ce0907eeaed05e3ee94ffc02ad10cb36a93340c6350ff34d699dfa16'
+  version '1.0.3'
+  sha256 '5d832520bedd30e4ea719453925bc2b94fed25b3f4bc936600cc84fe5cf97664'
 
   # releases.hashicorp.com/vagrant-vmware-utility was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant-vmware-utility/#{version}/vagrant-vmware-utility_#{version}_x86_64.dmg"
