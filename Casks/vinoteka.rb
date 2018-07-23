215981840896d2b406943af67a9e173f5d2106a6
Julian Mayer <julian@corecode.io>
2018-07-23 21:28:53 +0200

    Update vinoteka to 3.6.2

--- Casks/vinoteka.rb
+++ Casks/vinoteka.rb
@@ -1,6 +1,6 @@
 cask 'vinoteka' do
-  version '3.6.1'
-  sha256 '1be2a12d0714c0dc2ecccaecac8b11781354a2f00a460a2dcc09e07659b29b62'
+  version '3.6.2'
+  sha256 '94640e140edb6f2fd7375c6315d300b865047e6e60e3247d237cfbdac702e438'
 
   url 'http://download.vinotekasoft.com/Vinoteka.zip'
   appcast 'http://download.vinotekasoft.com/vinoteka_update.xml'
