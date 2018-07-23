d3b647fae02cde0935ec7482725be2b2c6dd38ea
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-23 18:21:40 +0100

    Create haptickey.rb
    Not sure if this is right so would love some input.

---
+++ Casks/haptickey.rb
@@ -0,0 +1,9 @@
+cask 'haptickey' do
+  version 0.44
+  sha256 :no_check
+  
+  url: "https://github.com/niw/HapticKey/releases/download/{version}/HapticKey.app.zip"
+  name: "haptickey"
+  homepage: "https://github.com/niw/HapticKey"
+  
+end
