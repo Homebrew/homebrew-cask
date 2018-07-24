ee5b38f94226cd238dcfd2136e98db27facffacc
ilovezfs <ilovezfs@icloud.com>
2018-07-24 11:20:55 +0200

    Update monero-wallet to 0.12.2.0 (#49993)

--- Casks/monero-wallet.rb
+++ Casks/monero-wallet.rb
@@ -1,6 +1,6 @@
 cask 'monero-wallet' do
-  version '0.12.0.0'
-  sha256 'f74c108d16bd70b6f0052ba4b3ce91fa3ca59622a0aee7d523a1f43967814c12'
+  version '0.12.2.0'
+  sha256 '298ff610d3c18afe1ae78691fa995d7f0e0ab39cd768de3ce6616e4678ee57b8'
 
   url "https://downloads.getmonero.org/gui/monero-gui-mac-x64-v#{version}.tar.bz2"
   appcast 'https://github.com/monero-project/monero-gui/releases.atom'
