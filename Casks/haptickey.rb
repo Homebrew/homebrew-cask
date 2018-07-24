42ce5f89302e7ea551f841d72324e0d19ab28ea8
Christopher Nethercott <ccnethercott@gmail.com>
2018-07-24 07:28:23 +0100

    Added appcast + sha256 hash. Audit and style both passed

--- Casks/haptickey.rb
+++ Casks/haptickey.rb
@@ -1,6 +1,6 @@
 cask 'haptickey' do
   version '0.44'
-  sha256 :no_check
+  sha256 'f1f46a1686d8f9a2884e0f045acb407d6ad583053ec46b733bce71328240ff29'
 
   url "https://github.com/niw/HapticKey/releases/download/#{version}/HapticKey.app.zip"
   appcast 'https://github.com/niw/HapticKey/releases.atom'
