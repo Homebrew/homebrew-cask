5dfe585d6455aed9e049cb21395e126ec125ef28
commitay <commitay@users.noreply.github.com>
2018-07-24 19:40:38 +1000

    Update haptickey.rb

--- Casks/haptickey.rb
+++ Casks/haptickey.rb
@@ -1,5 +1,5 @@
 cask 'haptickey' do
-  version '0.44'
+  version '0.4.4'
   sha256 'f1f46a1686d8f9a2884e0f045acb407d6ad583053ec46b733bce71328240ff29'
 
   url "https://github.com/niw/HapticKey/releases/download/#{version}/HapticKey.app.zip"
