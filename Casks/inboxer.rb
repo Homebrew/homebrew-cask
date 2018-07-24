a52c5a35475046851b7e8f2835b4e24446986561
Brian Morton <brianmortondesign@gmail.com>
2018-07-24 14:36:17 -0700

    Update inboxer to 1.1.3

--- Casks/inboxer.rb
+++ Casks/inboxer.rb
@@ -1,9 +1,9 @@
 cask 'inboxer' do
-  version '1.1.2'
-  sha256 '5e3ae2901946f1fae8b2a54fd8b488b57674d921742f16e8bd8ac16bcef973ea'
+  version '1.1.3'
+  sha256 '533c5f52fc56ea61434d92cbcf6b475159a5c15b7f96304a748e84df4a6cdc2b'
 
   # github.com/denysdovhan/inboxer was verified as official when first introduced to the cask
-  url "https://github.com/denysdovhan/inboxer/releases/download/v#{version}/inboxer-#{version}-mac.zip"
+  url "https://github.com/denysdovhan/inboxer/releases/download/#{version}/Inboxer-#{version}.dmg"
   appcast 'https://github.com/denysdovhan/inboxer/releases.atom'
   name 'inboxer'
   homepage 'https://denysdovhan.com/inboxer'
