29babad8fed620574c2e5659d806ebab1f5eb03f
Brian Morton <brianmortondesign@gmail.com>
2018-07-24 14:43:11 -0700

    Update omnifocus to 2.12.4

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -12,8 +12,8 @@ cask 'omnifocus' do
     sha256 'e808a72e60cdff9ff5aa1046d856bf62d6418e4915248816c4640e32e52fd8e8'
     url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniFocus-#{version}.dmg"
   else
-    version '2.12.3'
-    sha256 '1d9bbff9e8668ee88561f06599c115b0b936fc12b0b15568d42767a73fc47c6f'
+    version '2.12.4'
+    sha256 '8a2dc53331dba804f6781773fef546a03c181fc4ff0eb7ee4f871c10342621f0'
     url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniFocus-#{version}.dmg"
   end
 
