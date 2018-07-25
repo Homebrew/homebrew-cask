5d3028c56a2e093b3e439c8cfa1581b8063cb957
Ivan Pavlov <ivanpavlovv@gmail.com>
2018-07-26 00:30:56 +0300

    Update yandex-disk to 3.0

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -1,6 +1,6 @@
 cask 'yandex-disk' do
   version '3.0'
-  sha256 'dac1f0e1710bc06b59a872148c940fbd15f8e34e4e5e22009caba5d67163f888'
+  sha256 '2a4a1bf61b92a0cea2099330bfc35d2ccf2f579244985cbd2644740705a22cae'
 
   url "https://disk.yandex.ru/download/YandexDisk#{version.no_dots}.dmg/?instant=1"
   name 'Yandex.Disk'
