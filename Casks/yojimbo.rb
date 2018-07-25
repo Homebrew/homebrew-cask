de6501a69cd635d7df3a0cf54987d9d20f583ee3
Brian Morton <brianmortondesign@gmail.com>
2018-07-25 16:55:54 -0700

    Update yojimbo to 4.1.2

--- Casks/yojimbo.rb
+++ Casks/yojimbo.rb
@@ -1,6 +1,6 @@
 cask 'yojimbo' do
-  version '4.1.1'
-  sha256 '401b36cb12910a61d53b86fef571dcd8794f3970808098e1f0849e7d8a47f8e0'
+  version '4.1.2'
+  sha256 '33e087692c7fee4cb01c0014f6d5cca674060a1303f59f2294d9e9a43f220819'
 
   # amazonaws.com/BBSW-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/Yojimbo_#{version}.dmg"
