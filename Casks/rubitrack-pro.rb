2835632ff98065427c9f7817ad3a07f155f20f4c
Brian Morton <brianmortondesign@gmail.com>
2018-07-25 14:19:54 -0700

    Update rubitrack-pro to 5.0.7 (#50068)

--- Casks/rubitrack-pro.rb
+++ Casks/rubitrack-pro.rb
@@ -1,6 +1,6 @@
 cask 'rubitrack-pro' do
-  version '5.0.6'
-  sha256 '650f7353345cea38b3bbcc9b85717a8a9ff22c2c052f2f33b249839471af975e'
+  version '5.0.7'
+  sha256 '57d3f2989e9f0c4ccb97b8d8cf8c758edbdbd67ba233355b8b1bde94cc8701ff'
 
   url "https://www.rubitrack.com/files/rubiTrack-#{version}.dmg"
   appcast "https://www.rubitrack.com/autoupdate/sparkle#{version.major}.xml"
