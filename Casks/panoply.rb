69691d02c8c667e5cd9dbe981a55ed76a3f8e149
Carsten Heidmann <carsten@heidmann.info>
2018-07-25 08:18:32 +0200

    Adjusted Hash for panoply 4.9.2

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,6 @@
 cask 'panoply' do
   version '4.9.2'
-  sha256 '2d8b93185b071e094a857e07859ae443b34797680fd796708b5880083582501c'
+  sha256 'c1b972a8b6d686b1b5081c7cb2702fd5865628b1be4372b9070234cec3979468'
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
   name 'Panoply netCDF, HDF and GRIB Data Viewer'
