9e0608484531cb979a2f202f2bd056cc4a10ad88
Julian Mayer <julian@corecode.io>
2018-07-25 14:58:48 +0200

    update ocrkit

--- Casks/ocrkit.rb
+++ Casks/ocrkit.rb
@@ -1,9 +1,10 @@
 cask 'ocrkit' do
-  version '18.4.19,cf4278314ef8e4b996e1b798d8eb92cf'
+  version '18.7.15'
   sha256 'b8054ebca6e1417eac12255bb47f3320422f736dc6a8ceb70f3198386f9869ea'
 
-  # exactcode.de was verified as official when first introduced to the cask
-  url "https://dl.exactcode.de/tmp/#{version.after_comma}/OCRKit-#{version.before_comma}.dmg"
+  # dl.exactcode.de was verified as official when first introduced to the cask
+  url "http://dl.exactcode.de/sync/#{version.major}/OCRKit-#{version}.tar.gz"
+  appcast 'https://synchronicity.exactcode.de/OCRKit/seed-Darwin.xml'
   name 'OCRKit'
   homepage 'https://ocrkit.com/'
 
