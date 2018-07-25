69dbb9de5a1a5c4f67ad391a34f0e051321fc0df
Carsten Heidmann <carsten@heidmann.info>
2018-07-25 08:12:16 +0200

    Update panoply version to 4.9,2

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,5 +1,5 @@
 cask 'panoply' do
-  version '4.9.1'
+  version '4.9.2'
   sha256 '2d8b93185b071e094a857e07859ae443b34797680fd796708b5880083582501c'
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
