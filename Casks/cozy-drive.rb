2bb1fd3dc6aecb1558cf4181ec0228861e399c54
Antoine Lépée <alepee@gmail.com>
2018-07-25 10:39:08 +0200

    Update cozy-drive to 3.9.0

--- Casks/cozy-drive.rb
+++ Casks/cozy-drive.rb
@@ -1,6 +1,6 @@
 cask 'cozy-drive' do
-  version '3.8.1'
-  sha256 'a3243c970d48bbceed55f2c3b207129ee21bc4c47d89b93061b7afdf616c863a'
+  version '3.9.0'
+  sha256 '971c61e3363ee0d1173b9d9a71622ea92195bd56b280a28b278e8a65d51d2bac'
 
   # nuts.cozycloud.cc was verified as official when first introduced to the cask
   url "https://nuts.cozycloud.cc/download/channel/stable/CozyDrive-#{version}.dmg"
