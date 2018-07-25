7203d2cd13012a3f735aee92c873debcbcbda762
David Holm <dholmster@gmail.com>
2018-07-25 11:14:13 +0200

    sourcetrail 2018.2.77

---
+++ Casks/sourcetrail.rb
@@ -0,0 +1,10 @@
+cask 'sourcetrail' do
+  version '2018.2.77'
+  sha256 '49b38996b54727571a343bc0bea20fd03299c065dd55f489e2834d32cf4305ce'
+
+  url 'https://www.sourcetrail.com/downloads/2018.2.77/osx/64bit'
+  name 'Sourcetrail'
+  homepage 'https://www.sourcetrail.com/'
+
+  app 'Sourcetrail.app'
+end
