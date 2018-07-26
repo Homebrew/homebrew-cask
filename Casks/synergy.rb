fd380e94f445cb1e44274ef39b9ff3511726ae93
Julian Mayer <julian@corecode.io>
2018-07-26 20:49:21 +0200

    fix synergy.rb

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,6 +1,6 @@
 cask 'synergy' do
   version '1.10.0,b39:275cdc1c'
-  sha256 'b09d8ffdb47ecde761e06e9beea672ad4b6999e09104fa9239ae30ae0f69b9c2'
+  sha256 'dd4028c6166480ab544a2bcd781d158dd1c373cf28a0867cc5987ef9d091f3ca'
 
   url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/v#{version.before_comma}-stable-#{version.after_colon}/synergy_#{version.before_comma}-stable_#{version.after_comma.before_colon}-#{version.after_colon}_macos.dmg"
   name 'Synergy'
