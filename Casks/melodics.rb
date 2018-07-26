73bc709452cf4e57637bbaa942e908028f62c5a1
Vítor Galvão <info@vitorgalvao.com>
2018-07-26 12:50:41 +0100

    Update melodics.rb

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,8 +1,9 @@
 cask 'melodics' do
-  version :latest
-  sha256 :no_check
+  version '2.0.1872'
+  sha256 '41019fba93d7ef7fbb66b766b630fec9c971676ffe43078d494958df85745130'
 
-  url 'https://web-cdn.melodics.com/download/MelodicsV2.dmg'
+  url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
+  appcast "https://api.melodics.com/download/osxupdatescastv#{version.major}.xml"
   name 'Melodics'
   homepage 'https://melodics.com/'
 
