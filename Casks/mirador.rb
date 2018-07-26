0df413b5b8b679340254906f087b3a3ee140b668
Brian Morton <brianmortondesign@gmail.com>
2018-07-26 14:46:48 -0700

    revert url

--- Casks/mirador.rb
+++ Casks/mirador.rb
@@ -3,7 +3,7 @@ cask 'mirador' do
   sha256 '011ff609679258a0b5dc3f1ce1f0ad82f0c1c4730ff9d467c49581500cc6fc13'
 
   # github.com/mirador/mirador was verified as official when first introduced to the cask
-  url 'https://github.com/mirador/mirador/archive/latest-macos.zip'
+  url "https://github.com/mirador/mirador/releases/download/latest-macos/mirador-#{version}-macos.zip"
   appcast 'https://github.com/mirador/mirador/releases.atom'
   name 'Mirador'
   homepage 'https://fathom.info/mirador/'
