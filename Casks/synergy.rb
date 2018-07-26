1232740c3dff1c2855ff27a40df7c094aca46a9d
Julian Mayer <julian@corecode.io>
2018-07-26 20:45:38 +0200

    fix synergy.rb

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,8 +1,8 @@
 cask 'synergy' do
-  version '1.8.8,25a8cb2'
+  version '1.10.0,b39:275cdc1c'
   sha256 'b09d8ffdb47ecde761e06e9beea672ad4b6999e09104fa9239ae30ae0f69b9c2'
 
-  url "https://binaries.symless.com/v#{version.before_comma}/synergy-v#{version.before_comma}-stable-#{version.after_comma}-MacOSX-x86_64.dmg"
+  url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/v#{version.before_comma}-stable-#{version.after_colon}/synergy_#{version.before_comma}-stable_#{version.after_comma.before_colon}-#{version.after_colon}_macos.dmg"
   name 'Synergy'
   homepage 'https://symless.com/synergy'
 
