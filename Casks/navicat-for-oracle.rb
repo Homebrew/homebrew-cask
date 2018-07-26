9197a8e05992298d4f8c0167b722cf338c21a9f6
Brian Morton <brianmortondesign@gmail.com>
2018-07-26 09:33:36 -0700

    Update navicat-for-oracle to 12.1.3

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-oracle' do
-  version '12.0.28'
-  sha256 'df243be242a1d24c6923db341f612243c8e7efcb3daffd976f029b70e1d253bb'
+  version '12.1.3'
+  sha256 '3b2e3c8d56099ceaffbe1f6a536537b245c2d916216a4265925770c8cf8b49e5'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-oracle-release-note'
