155d9ab94e311d9e057fce3319df27c79c94094c
Brian Morton <brianmortondesign@gmail.com>
2018-07-26 09:31:41 -0700

    Update navicat-for-mariadb to 12.1.3

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-mariadb' do
-  version '12.0.28'
-  sha256 '601292a90722f830f7b27ed5978308f5bcb510e8d9cee27fd79f41de29be40cd'
+  version '12.1.3'
+  sha256 '0a38d2759072de55283e6af74cbd697990cca0da3fd4d4232ef1cad7b2cee8c2'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
   appcast 'https://www.navicat.com/en/products/navicat-for-mariadb-release-note'
