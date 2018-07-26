1b70a3cd47ad2becdb2ad52510ec7c8a7b753112
Brian Morton <brianmortondesign@gmail.com>
2018-07-26 09:24:05 -0700

    Update bitwarden to 1.4.0

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,6 +1,6 @@
 cask 'bitwarden' do
-  version '1.3.0'
-  sha256 'a8e24abd6c28b3deef00b30b9be632ab799e31c1557d26f3c732725b2439acb3'
+  version '1.4.0'
+  sha256 '046066f79672fb56f82e00b88e51c968eede3d4d73bc82038871ab9c1baf6c88'
 
   # github.com/bitwarden/desktop was verified as official when first introduced to the cask
   url "https://github.com/bitwarden/desktop/releases/download/v#{version}/bitwarden-#{version}-mac.zip"
