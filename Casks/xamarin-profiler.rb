82ab6e7bf5dc60976351d80f755f7273b26a008a
Vítor Galvão <info@vitorgalvao.com>
2018-07-26 13:27:28 +0100

    Update xamarin-profiler.rb

--- Casks/xamarin-profiler.rb
+++ Casks/xamarin-profiler.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-profiler' do
-  version '1.6.2-165'
-  sha256 '1050061b4aa013d21684cbd8e5474438d502161a7d24562de860f29339e05078'
+  version '1.6.3-444'
+  sha256 '7d981e87102bb392330e0b8ac13724366e381f13c7acfaeb2312ac3ccaa540c7'
 
   url "https://dl.xamarin.com/profiler/profiler-mac-#{version}.pkg"
   name 'Xamarin Profiler'
