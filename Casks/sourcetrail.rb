8e7d4b68be69ebbc7435108b88b787811d2cf917
Vítor Galvão <info@vitorgalvao.com>
2018-07-26 13:35:22 +0100

    Update sourcetrail.rb

--- Casks/sourcetrail.rb
+++ Casks/sourcetrail.rb
@@ -3,6 +3,7 @@ cask 'sourcetrail' do
   sha256 '49b38996b54727571a343bc0bea20fd03299c065dd55f489e2834d32cf4305ce'
 
   url "https://www.sourcetrail.com/downloads/#{version}/osx/64bit"
+  appcast 'https://raw.githubusercontent.com/CoatiSoftware/SourcetrailBugTracker/master/README.md'
   name 'Sourcetrail'
   homepage 'https://www.sourcetrail.com/'
 
