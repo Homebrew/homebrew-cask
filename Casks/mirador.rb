86499e9d98d0f7d68b12ecb328da6e5a7529edd5
Brian Morton <brianmortondesign@gmail.com>
2018-07-26 14:49:06 -0700

    fix checksum

--- Casks/mirador.rb
+++ Casks/mirador.rb
@@ -1,6 +1,6 @@
 cask 'mirador' do
   version '1.5.1'
-  sha256 '011ff609679258a0b5dc3f1ce1f0ad82f0c1c4730ff9d467c49581500cc6fc13'
+  sha256 '91387a237f338722306a7d288bd7d809f7595e10267e1ef0653c93c713028290'
 
   # github.com/mirador/mirador was verified as official when first introduced to the cask
   url "https://github.com/mirador/mirador/releases/download/latest-macos/mirador-#{version}-macos.zip"
