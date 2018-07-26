68f755759f6e335f12633a01bea9f1c85c7b6896
Kevin Ludovico Mussi <kevin_495@hotmail.it>
2018-07-26 14:36:52 +0200

    Updated cask eclipse-java to version 4.8.0,photon:R (#50099)

--- Casks/eclipse-java.rb
+++ Casks/eclipse-java.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-java' do
-  version '4.7.3a,oxygen:3a'
-  sha256 '88310307ed5481f4ac129ae3db88ddbb0a41e349d0dd044762c9eddffa2031a3'
+  version '4.8.0,photon:R'
+  sha256 'dce58bbf72a1ad666e658da24b9f161fcafb939388fce9be870e0d68c3c907e9'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-java-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for Java Developers'
