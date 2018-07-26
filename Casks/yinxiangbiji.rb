2dd76e0dd144f2102a66d3d2f0e64e899f40d3b5
秦新 <qinxin_0720@live.com>
2018-07-26 20:27:52 +0800

    add new cask for yinxiangbiji (#49770)
    * Create yinxiangbiji
    
    The yinxiangbiji is a Chinese version of Evernote
    
    * Update yinxiangbiji
    
    * Rename yinxiangbiji to yinxiangbiji.rb
    
    * Update yinxiangbiji.rb
    
    * Update yinxiangbiji.rb
    
    * Update yinxiangbiji.rb
    
    * Update and rename yinxiangbiji.rb to evernote-cn.rb
    
    * Update and rename evernote-cn.rb to yinxiangbiji.rb

---
+++ Casks/yinxiangbiji.rb
@@ -0,0 +1,14 @@
+cask 'yinxiangbiji' do
+  version '8.1.1_456907'
+  sha256 '307cf468c3e8af2a1977fceedc63fc32765b555a730413958b279875bad05d0c'
+
+  url "https://cdn.yinxiang.com/mac-smd/public/YinxiangBiji_RELEASE_#{version}.dmg"
+  name 'Evernote'
+  name '印象笔记'
+  homepage 'https://www.yinxiang.com/'
+
+  auto_updates true
+  depends_on macos: '>= :high_sierra'
+
+  app '印象笔记.app'
+end
