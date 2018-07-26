d26cd761b04df8abdbcf8fc0a4defc979e85320f
Brian Morton <brianmortondesign@gmail.com>
2018-07-26 09:44:38 -0700

    Update steermouse to 5.3.1

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,6 +1,6 @@
 cask 'steermouse' do
-  version '5.3'
-  sha256 '93225f1372f865b81666734c201e11a83ed78742b0e90bf52e2e28bba439b053'
+  version '5.3.1'
+  sha256 '12f5c3a31f032885676bdc42391c2f6388b64f59f3952ad5ae93a739df9e6ce9'
 
   url "http://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
   appcast 'http://plentycom.jp/en/steermouse/download.php'
