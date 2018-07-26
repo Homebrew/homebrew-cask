a7072bf655a6a627d0ed13eb0183bedbde06e5b2
Brian Morton <brianmortondesign@gmail.com>
2018-07-26 12:49:32 -0700

    Update jetbrains-toolbox to 1.10.4088 (#50120)

--- Casks/jetbrains-toolbox.rb
+++ Casks/jetbrains-toolbox.rb
@@ -1,6 +1,6 @@
 cask 'jetbrains-toolbox' do
-  version '1.9.3935'
-  sha256 'b08034316a2bf98fccbb4f53206c66282567a670595116d3df05b1dd1e31ef61'
+  version '1.10.4088'
+  sha256 'd13cc845e165abfa6216426494fa45ae2484701942f0e713e5f33c800b080a8f'
 
   url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release'
