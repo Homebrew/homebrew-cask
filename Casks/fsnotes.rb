fc6320b87e2e1a7b933450dfb1fb9d05adbc3437
Brian Morton <brianmortondesign@gmail.com>
2018-07-26 15:33:57 -0700

    Update fsnotes to 2.0.4

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask 'fsnotes' do
-  version '2.0.3'
-  sha256 '8c298fa7e5ec8acfe4cbc3935d1eac565ef48529321f46cf47becf97ea29df91'
+  version '2.0.4'
+  sha256 '9a30527129f2bf4de5b189730e6eb093d8cf4a7c322b1a711217a4d53283e91a'
 
   # github.com/glushchenko/fsnotes was verified as official when first introduced to the cask
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version.before_comma}/FSNotes_#{version.before_comma}#{version.after_comma}.zip"
