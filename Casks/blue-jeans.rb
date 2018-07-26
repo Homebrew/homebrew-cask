bf1f653080dfa39fdbb03f42a8f348584277ea30
Marc Zottner <marc@zottner.com>
2018-07-26 14:16:20 +0200

    Update blue-jeans to 2.5.1.33 (#47891)
    * Update blue-jeans to 2.5.1.33
    
    * Update blue-jeans.rb
    
    * Update blue-jeans.rb
    
    * Update blue-jeans.rb

--- Casks/blue-jeans.rb
+++ Casks/blue-jeans.rb
@@ -1,16 +1,16 @@
 cask 'blue-jeans' do
-  version '1.6.8'
-  sha256 '9f76c386659697c11752b8e0c04d40c4a07c08733a6c928bbff11210303789db'
+  version '2.5.1.33'
+  sha256 '20914226694e4a0734d85f8de9a42bc81e7166af933016de66e0a82dd5383e89'
 
-  url "https://swdl.bluejeans.com/desktop/mac/launchers/BlueJeansLauncher_live_#{version.no_dots}.dmg"
+  url "https://swdl.bluejeans.com/desktop-app/mac/#{version.major_minor_patch}/#{version}/BlueJeansInstaller.dmg"
   name 'Blue Jeans videoconferencing'
   homepage 'https://www.bluejeans.com/'
 
-  installer manual: 'Blue Jeans Launcher.app'
+  installer manual: 'BlueJeansInstaller.app'
 
   uninstall signal: [
-                      ['TERM', 'com.bluejeans.nw.app'],
-                      ['TERM', 'com.bluejeans.nw.helper'],
+                      ['TERM', 'com.bluejeansnet.Blue'],
+                      ['TERM', 'com.bluejeansnet.BlueMenulet'],
                     ],
-            delete: '~/Applications/Blue Jeans.app'
+            delete: '/Applications/Blue Jeans.app'
 end
