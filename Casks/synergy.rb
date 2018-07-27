e60cf768a9547bf9357eaec3743579bc6883f6ba
CoreCode <core-code@users.noreply.github.com>
2018-07-27 06:41:24 +0200

    Update synergy to 1.8.8,25a8cb2 (#50109)

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,8 +1,8 @@
 cask 'synergy' do
-  version '2.0.12,b1807-50472cde'
-  sha256 '41927a6a52a523797122a7d6a9c8817d09cce9966ad08118c693345f51aa41e9'
+  version '1.10.0,b39:275cdc1c'
+  sha256 'dd4028c6166480ab544a2bcd781d158dd1c373cf28a0867cc5987ef9d091f3ca'
 
-  url "https://binaries.symless.com/v#{version.before_comma}/Synergy_v#{version.before_comma}-beta_#{version.after_comma}.dmg"
+  url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/v#{version.before_comma}-stable-#{version.after_colon}/synergy_#{version.before_comma}-stable_#{version.after_comma.before_colon}-#{version.after_colon}_macos.dmg"
   name 'Synergy'
   homepage 'https://symless.com/synergy'
 
