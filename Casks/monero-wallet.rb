dbe21b1908fcce4520726016b92ad37893422f4d
Brian Morton <brianmortondesign@gmail.com>
2018-07-27 09:34:21 -0700

    Update monero-wallet to 0.12.3.0

--- Casks/monero-wallet.rb
+++ Casks/monero-wallet.rb
@@ -1,6 +1,6 @@
 cask 'monero-wallet' do
-  version '0.12.2.0'
-  sha256 '298ff610d3c18afe1ae78691fa995d7f0e0ab39cd768de3ce6616e4678ee57b8'
+  version '0.12.3.0'
+  sha256 '90a9fa02e7bcef653b034f8a0365f16ec96cd553791546df2200a3c2591d5104'
 
   url "https://downloads.getmonero.org/gui/monero-gui-mac-x64-v#{version}.tar.bz2"
   appcast 'https://github.com/monero-project/monero-gui/releases.atom'
