3f453d0082045caf574fc1efe16e4b5e927e6ec0
Vítor Galvão <info@vitorgalvao.com>
2018-07-28 11:08:23 +0100

    Update aptanastudio.rb

--- Casks/aptanastudio.rb
+++ Casks/aptanastudio.rb
@@ -3,7 +3,7 @@ cask 'aptanastudio' do
   sha256 '73b403c9801945e3bbe4f00da5fc6a252c419f0bf88dc88638d8861e0d53dc0d'
 
   # github.com/aptana/studio3 was verified as official when first introduced to the cask
-  url "https://github.com/aptana/studio3/releases/download/#{version}/Aptana_Studio_3.dmg"
+  url "https://github.com/aptana/studio3/releases/download/#{version}/Aptana_Studio_#{version.major}.dmg"
   appcast "https://github.com/aptana/studio#{version.major}/releases.atom"
   name 'Aptana Studio'
   homepage 'http://www.aptana.com/'
