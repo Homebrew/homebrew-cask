2601317aed2d1c0eebcae7eef46c674863313a0e
Bernd Krannich <berndkrannich@users.noreply.github.com>
2018-07-29 21:42:36 +0200

    Update beatunes to 5.1.8 (#50205)

--- Casks/beatunes.rb
+++ Casks/beatunes.rb
@@ -1,6 +1,6 @@
 cask 'beatunes' do
-  version '5.1.7'
-  sha256 'ae97f82042ca1fc8623d9d018feb33d335ffbc8daa247364d83323f673b78ce4'
+  version '5.1.8'
+  sha256 'a20407eef739406ca237b17ddb6f19c5ab2df1a805c11164629df3636c4704c8'
 
   url "http://coxy.beatunes.com/download/beaTunes-#{version.dots_to_hyphens}.dmg"
   name 'beaTunes'
