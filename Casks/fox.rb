038f85dee1c0702a77f274c635a3f7279dd831ee
alswl <alswlx@gmail.com>
2018-07-29 15:37:36 +0800

    feat: add fox 1.7.6

---
+++ Casks/fox.rb
@@ -0,0 +1,10 @@
+cask 'fox' do
+  version '1.7.6'
+  sha256 'ca18958ff70cef3561de736fe2eb7384eb11c03da0c7785bce07d991f6910f41'
+
+  url "https://www.fox.one/build/Fox-#{version}.dmg"
+  name 'Fox'
+  homepage 'https://www.fox.one/'
+
+  app 'Fox.app'
+end
