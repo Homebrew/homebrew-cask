b8ff1bb4415489b58bb62499dc9e9cf81b488689
Zsolt Horvath <zsolt@zsolthorvath.info>
2018-07-29 18:00:03 +0200

    Update meteorologist to 3.1.4

--- Casks/meteorologist.rb
+++ Casks/meteorologist.rb
@@ -1,6 +1,6 @@
 cask 'meteorologist' do
-  version '3.1.3'
-  sha256 'dbe05396be6289939774941512af21407dd3aeabdc800f4db330490b2e31028e'
+  version '3.1.4'
+  sha256 'ade644cbe2faad927c1bbede7479d873fe48471f94610da50dcdd8160ca0f812'
 
   url "https://downloads.sourceforge.net/heat-meteo/Meteorologist-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/heat-meteo/rss'
