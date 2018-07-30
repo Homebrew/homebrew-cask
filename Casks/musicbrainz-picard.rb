d2447441d48d1a74b0e8a8fcc16834c3afccb9f0
Julian Mayer <julian@corecode.io>
2018-07-30 19:40:41 +0200

    Update musicbrainz-picard to 2.0.2

--- Casks/musicbrainz-picard.rb
+++ Casks/musicbrainz-picard.rb
@@ -1,6 +1,6 @@
 cask 'musicbrainz-picard' do
-  version '2.0.1'
-  sha256 '2a652ff5c562216033568cb33d0a2525cfd3043f43c67afdefaf4f8cd471cd5a'
+  version '2.0.2'
+  sha256 '9d37fdba65359ac371b21178ae9c498f476f40c832d612d69a92d751e3418f5c'
 
   url "http://ftp.musicbrainz.org/pub/musicbrainz/picard/MusicBrainz-Picard-#{version}.dmg"
   appcast 'https://picard.musicbrainz.org/changelog/'
