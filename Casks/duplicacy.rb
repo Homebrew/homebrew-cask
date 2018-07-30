1ab91587eebaa38959278ea4c090739064222581
Ben Lee <ben_lee@post.harvard.edu>
2018-07-29 20:28:32 -0700

    Update duplicacy to 2.1.1

--- Casks/duplicacy.rb
+++ Casks/duplicacy.rb
@@ -1,6 +1,6 @@
 cask 'duplicacy' do
-  version '2.1.0'
-  sha256 'eb7ef9d82c923293efc6ad000ec821b4564b464766224a86a13cce8aaf550e24'
+  version '2.1.1'
+  sha256 'fe3b1dce02e54b662c24cbc67696fe25194b15d4a6785f92df5fc269a92ab206'
 
   # acrosync.com/duplicacy was verified as official when first introduced to the cask
   url "https://acrosync.com/duplicacy/duplicacy_gui_osx_#{version}.dmg"
