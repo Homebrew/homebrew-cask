f013f994fb2af72acf2b6d2eedb250b4b7556032
chenbin <chenbin@dmesoft.cn>
2018-07-30 10:35:44 +0800

    Update godot to 3.0.6

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,6 +1,6 @@
 cask 'godot' do
-  version '3.0.5'
-  sha256 '0d402559bc1d342e1c98b6905cdff423c91141ac890ee782d3b6f1e9b9d97036'
+  version '3.0.6'
+  sha256 'cd409615685d3b497b14cb774bbb315fb0a7a45a00c6865edbb2e43cc735a759'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.fat.zip"
