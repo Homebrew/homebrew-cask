01745b352231ad363e0c9b0e6c6653a622b6469f
Peter Jonas <shoogle@users.noreply.github.com>
2018-07-30 03:00:58 +0100

    musescore 2.3.1: add shim for command line usage
    MuseScore looks for the Qt library in a location relative to the
    executable. If we symlink to the executable then MuseScore will
    look in a location relative to the symlink and Qt won't be found.
    The shim solves this by running MuseScore inside the .app bundle.

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -10,4 +10,15 @@ cask 'musescore' do
   depends_on macos: '>= :lion'
 
   app "MuseScore #{version.major}.app"
+  # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/musescore.wrapper.sh"
+  binary shimscript, target: 'musescore'
+  binary shimscript, target: 'mscore'
+
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/MuseScore #{version.major}.app/Contents/MacOS/mscore' "$@"
+    EOS
+  end
 end
