a5496a6df1c10f6a635e71bc7d301cb86045414d
Mycatisanalien <37309780+Mycatisanalien@users.noreply.github.com>
2018-07-30 10:30:48 +0500

    Create hexels3.rb

---
+++ Casks/hexels3.rb
@@ -0,0 +1,10 @@
+cask 'hexels3' do
+  version '3.1.2'
+  sha256 '197d85e228d9c18807d4a4dde16b8d75bea5200f2506b8bfe83129080d66fb4c'
+
+  url "https://s3.amazonaws.com/mset/download/release/hexels_install_312.dmg"
+  name 'Hexels 3'
+  homepage 'http://marmoset.co/hexels/'
+
+  app 'Hexels 3.app'
+end
