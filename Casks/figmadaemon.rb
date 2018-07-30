82b5e611c5fb8d9ec09ffb4390fbbd14bf3c92ff
Yurii Kolesnykov <yurikoles@gmail.com>
2018-07-30 19:02:04 +0300

    new cask figmadaemon latest

---
+++ Casks/figmadaemon.rb
@@ -0,0 +1,17 @@
+cask 'figmadaemon' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://font-daemon.figma.com/FigmaInstaller.pkg'
+  name 'Figma Font Installers'
+  homepage 'https://www.figma.com/'
+
+  pkg 'FigmaInstaller.pkg'
+
+  uninstall delete: '~/Library/Application Support/Figma/FigmaDaemon.app'
+
+  zap delete: [
+                '~/Library/Preferences/com.figma.FigmaDaemon.plist',
+                '~/Library/Caches/com.figma.FigmaDaemon',
+              ]
+end
