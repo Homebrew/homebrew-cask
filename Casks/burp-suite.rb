4171a4d0e3a148dd73ffea97717b796c2ba4f113
Brian Morton <brianmortondesign@gmail.com>
2018-07-30 09:22:44 -0700

    Update burp-suite to 1.7.36

--- Casks/burp-suite.rb
+++ Casks/burp-suite.rb
@@ -1,6 +1,6 @@
 cask 'burp-suite' do
-  version '1.7.35'
-  sha256 '1fcc57822bc463acd8e72117cdf7b80abcae8075184c6a78af544bc92231a491'
+  version '1.7.36'
+  sha256 '51561f308d268410201154efecd60c30c3142ad532458197534abc0af006598a'
 
   url "https://portswigger.net/burp/releases/download?product=community&version=#{version}&type=macosx"
   appcast 'https://portswigger.net/burp/releasesarchive/community'
