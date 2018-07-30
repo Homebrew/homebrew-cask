2069a57fc838573fdc204ecbbfb39c6f26926cff
Mycatisanalien <37309780+Mycatisanalien@users.noreply.github.com>
2018-07-30 12:25:09 +0500

    Token reference edit

--- Casks/hexels.rb
+++ Casks/hexels.rb
@@ -1,9 +1,9 @@
-cask 'hexels3' do
+cask 'hexels' do
   version '3.1.2'
   sha256 '197d85e228d9c18807d4a4dde16b8d75bea5200f2506b8bfe83129080d66fb4c'
 
   url "https://s3.amazonaws.com/mset/download/release/hexels_install_312.dmg"
-  name 'Hexels 3'
+  name 'Hexels'
   homepage 'http://marmoset.co/hexels/'
 
   app 'Hexels 3.app'
