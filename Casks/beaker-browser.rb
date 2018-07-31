21ec1ba0dad786509e0c2422f5a3164276303d5b
Brian Morton <brianmortondesign@gmail.com>
2018-07-31 09:28:49 -0700

    Update beaker-browser to 0.8.0-prerelease.7

--- Casks/beaker-browser.rb
+++ Casks/beaker-browser.rb
@@ -1,6 +1,6 @@
 cask 'beaker-browser' do
-  version '0.8.0-prerelease.6'
-  sha256 '0382d8cf7d563677c444d01d5f9d9281e87ec97a714c14a1a183cffe4f85a5fb'
+  version '0.8.0-prerelease.7'
+  sha256 '7a9e81def0415f1dc085c40841a048d43dbe094c210d5b31fd2017e17ee28437'
 
   # github.com/beakerbrowser/beaker was verified as official when first introduced to the cask
   url "https://github.com/beakerbrowser/beaker/releases/download/#{version}/beaker-browser-#{version}.dmg"
