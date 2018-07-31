cf8c59a8dae1d875b84b0296e4d13d3712ccbcb1
CoreCode <core-code@users.noreply.github.com>
2018-07-31 12:37:21 +0200

    Update bbc-iplayer-downloads to 2.5.8 (#50267)

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -1,6 +1,6 @@
 cask 'bbc-iplayer-downloads' do
-  version '2.5.6'
-  sha256 '87656578b9ebb031ebaa658d56133f05b50a5cfcac4095d5fac8ccd50d625976'
+  version '2.5.8'
+  sha256 '8e8bd6d4aa884d29b379ef1b905a6a87276caad4f5810e51a904f4fdef97990e'
 
   # live-downloads-app-bucket-staticassetsbucket-ydn3z4ggyaof.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://live-downloads-app-bucket-staticassetsbucket-ydn3z4ggyaof.s3.amazonaws.com/releases/darwin-x64/BBCiPlayerDownloads-#{version}.dmg"
