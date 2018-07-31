bc9cc4be2440e5d3d3a85afc4f97d0d90e6a50ef
Sebastian Riehm <s.riehm@iwm.rwth-aachend.e>
2018-07-31 22:47:11 +0200

    Update busycontacts to 1.2.15

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask 'busycontacts' do
-  version :latest
-  sha256 :no_check
+  version '1.2.15'
+  sha256 '6411eed67fd9cbbf437497371d1855e026dd9a427f5d194606df366791da07ae'
 
   url 'https://www.busymac.com/download/BusyContacts.zip'
   name 'BusyContacts'
