1be0e85ad1b9aeb91535506e4e4ad3aed3957daf
CoreCode <core-code@users.noreply.github.com>
2018-08-01 03:11:27 +0200

    Update ridibooks to 2.5.2 (#50291)

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -1,6 +1,6 @@
 cask 'ridibooks' do
-  version '2.4.1'
-  sha256 '45861f50740eda1aba31a75de973b43a0eee29bdf644af70626d4fcfb518cbcf'
+  version '2.5.2'
+  sha256 'c02c59e6c655c101401c5981d8a02d028853b4d0ee2785d2c3986b27b595c199'
 
   url "https://viewer-ota.ridibooks.com/mac/ridibooks-#{version}.dmg"
   name 'Ridibooks'
