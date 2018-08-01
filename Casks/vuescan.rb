4fa814388a934f543f3f24bcc1877615f9a5ac83
commitay <commitay@users.noreply.github.com>
2018-08-02 02:01:55 +1000

    Update vuescan.rb

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.6.13'
-  sha256 '018c22c7d8de3b6e45e0708f393eeeb6822ef03e1f141015e03c57c5931f9d9d'
+  sha256 '1cc0bf81d5958bc186d22305c99fbe5fb725d709b92609bf8282cf99d1a4ce94'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/old-versions.html'
