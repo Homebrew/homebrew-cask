eecb0818d8b40e93331e7da2099d2002f00d647d
Brian Morton <brianmortondesign@gmail.com>
2018-07-31 18:15:47 -0700

    Update minishift to 1.22.0 (#50304)

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -1,6 +1,6 @@
 cask 'minishift' do
-  version '1.21.0'
-  sha256 'bb60c9799648aec7994bfff4085a5e9ab04bad4d06926e3cd3d01aa32ada1515'
+  version '1.22.0'
+  sha256 '8c2938db8cacfc3042aebfe429587f7f99247828d7cf83805c07fb3ba1d427f7'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom'
