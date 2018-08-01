afd758aeccb5ec523e38beaec06eb3be2e230755
me <me@mes-macbook-pro.attlocal.net>
2018-07-31 18:49:48 -0700

    Update totalspaces to 2.6.24

--- Casks/totalspaces.rb
+++ Casks/totalspaces.rb
@@ -1,10 +1,10 @@
 cask 'totalspaces' do
-  version '2.6.15'
-  sha256 'e0ef0851ba2922547657051e515e79151205f1d80ecfb730d1be7121839247fb'
+  version '2.6.24'
+  sha256 'd1c2a07df31b85648d94cee806448c7e78a6de35eef6635b1ec9fe7627601e91'
 
   url "http://downloads.binaryage.com/TotalSpaces#{version.major}-#{version}.dmg"
   appcast "http://updates-s3.binaryage.com/totalspaces#{version.major}.xml",
-          checkpoint: '188c44f1165a021d5f17e39ec6b7d6948e81d8bdd1c10feaefeb3686c55638a5'
+          checkpoint: '474b597cd38f038f5ad837512d5035db83249df30d3fb96732203ea8c9a3a175'
   name 'TotalSpaces'
   homepage 'https://totalspaces.binaryage.com/'
 
