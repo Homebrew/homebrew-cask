8b5ff95e6503380e7443624b613a23de9eea9851
Brian Morton <brianmortondesign@gmail.com>
2018-07-31 18:32:36 -0700

    Update diskmaker-x to 7.0.1 (#50278)

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -1,8 +1,8 @@
 cask 'diskmaker-x' do
-  version '7'
-  sha256 'd0c80961ca06dca436c0c784e8779defe56a0675d032f054064215a1b193a184'
+  version '7.0.1'
+  sha256 'c9b12c63d9742b2eb0136d352725b364a2af98f26e7efd310520a44a3d367471'
 
-  url "https://diskmakerx.com/downloads/DiskMaker_X_#{version}.dmg"
+  url "https://diskmakerx.com/downloads/DiskMaker_X_#{version.no_dots}.dmg"
   appcast 'https://diskmakerx.com/feed/'
   name 'DiskMaker X'
   homepage 'https://diskmakerx.com/'
