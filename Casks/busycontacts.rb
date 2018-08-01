f602d633f0e02d35f81ac2f96092d8539dae2053
Sebastian Riehm <herrriehm@users.noreply.github.com>
2018-08-01 03:37:10 +0200

    Update busycontacts to 1.2.15 (#50295)
    * Update busycontacts to 1.2.15
    
    * Update busycontacts.rb

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,8 +1,9 @@
 cask 'busycontacts' do
-  version :latest
-  sha256 :no_check
+  version '1.2.15'
+  sha256 '6411eed67fd9cbbf437497371d1855e026dd9a427f5d194606df366791da07ae'
 
   url 'https://www.busymac.com/download/BusyContacts.zip'
+  appcast 'https://www.busymac.com/busycontacts/releasenotes.html'
   name 'BusyContacts'
   homepage 'https://www.busymac.com/busycontacts/index.html'
 
