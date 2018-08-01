cf1815f4a59a1c9e0d79c119a8afdd111751c004
me <me@mes-macbook-pro.attlocal.net>
2018-07-31 19:00:13 -0700

    remove appcast checkpoint for totalspaces

--- Casks/totalspaces.rb
+++ Casks/totalspaces.rb
@@ -3,8 +3,7 @@ cask 'totalspaces' do
   sha256 'd1c2a07df31b85648d94cee806448c7e78a6de35eef6635b1ec9fe7627601e91'
 
   url "http://downloads.binaryage.com/TotalSpaces#{version.major}-#{version}.dmg"
-  appcast "http://updates-s3.binaryage.com/totalspaces#{version.major}.xml",
-          checkpoint: '474b597cd38f038f5ad837512d5035db83249df30d3fb96732203ea8c9a3a175'
+  appcast "http://updates-s3.binaryage.com/totalspaces#{version.major}.xml"
   name 'TotalSpaces'
   homepage 'https://totalspaces.binaryage.com/'
 
