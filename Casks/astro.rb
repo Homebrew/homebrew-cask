d80c97d47fb2fc36e1e68da94e2672cb8a36d58c
Brian Morton <brianmortondesign@gmail.com>
2018-08-01 16:43:19 -0700

    Update astro to 3.0.15,4135

--- Casks/astro.rb
+++ Casks/astro.rb
@@ -1,6 +1,6 @@
 cask 'astro' do
-  version '3.0.14,4121'
-  sha256 '3c97c577995a2272eb05e24631582d7c9bd68d05c0b1b09b93e59bb90d38d372'
+  version '3.0.15,4135'
+  sha256 '2a7ae8f9f88df720ee17f097c81f07a8cdb3f345e1783f36b5e5af7d7c146018'
 
   # pexlabs-updates-xvuif5mcicazzducz2j2xy3lki.s3-us-west-2.amazonaws.com was verified as official when first introduced to the cask
   url "https://pexlabs-updates-xvuif5mcicazzducz2j2xy3lki.s3-us-west-2.amazonaws.com/Astro-#{version.after_comma}.dmg"
