895774566008cc0fc396f4aecf72f6a4ae91762e
commitay <commitay@users.noreply.github.com>
2018-08-01 11:29:35 +1000

    Update busycontacts.rb

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -3,6 +3,7 @@ cask 'busycontacts' do
   sha256 '6411eed67fd9cbbf437497371d1855e026dd9a427f5d194606df366791da07ae'
 
   url 'https://www.busymac.com/download/BusyContacts.zip'
+  appcast 'https://www.busymac.com/busycontacts/releasenotes.html'
   name 'BusyContacts'
   homepage 'https://www.busymac.com/busycontacts/index.html'
 
