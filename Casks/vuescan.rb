ba1f9682c00468b1982eaf0af7447ace983c6d50
Brian Bezanson <brian.bezanson@healthlx.com>
2018-08-01 11:25:21 -0500

    Update vuescan to 9.6.13 (#50272)
    * Update vuescan to 9.6.13
    
    * Update vuescan.rb

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.6.13'
-  sha256 'db322268d2417626397705bf2227ef9fc1f4493cab353461140d6d56cf9b90b8'
+  sha256 '1cc0bf81d5958bc186d22305c99fbe5fb725d709b92609bf8282cf99d1a4ce94'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/old-versions.html'
