d639d2fb1f0e94da99abad8cbc509750d2c269bf
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@ProtonMail.com>
2018-08-02 19:27:33 +0800

    Add Aria2D v1.1

---
+++ Casks/aria2d.rb
@@ -0,0 +1,11 @@
+cask 'aria2d' do
+  version 'v1.1'
+  sha256 '14c2a4b42f75ceeb347371fe34c7917d23afb7d944d4f1584b8eda9d3b3baa58'
+
+  # dl.devmate.com/com.xjbeta.Aria2D/ was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.xjbeta.Aria2D/7/1498544488/Aria2D-7.zip'
+  name 'Aria2D'
+  homepage 'https://github.com/xjbeta/Aria2D'
+
+  app 'Aria2D.app'
+end
