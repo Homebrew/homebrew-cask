a017246d3bf9f1f5c462bd4fd2376b46778cd180
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2018-08-02 23:49:07 +0200

    Update Plex Media Server to 1.13.5.5291-6fa5e50a8 (#50382)

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-server' do
-  version '1.13.4.5271-200287a06'
-  sha256 '27785b24abcf0003a3e27ede80c81a369b7a0a2fde528f408bbd4ac62518d071'
+  version '1.13.5.5291-6fa5e50a8'
+  sha256 '163b84d43bd4593c942c851587e3d14232e9508daaad00e611341ae467517351'
 
   url "https://downloads.plex.tv/plex-media-server/#{version}/PlexMediaServer-#{version}-OSX.zip"
   appcast 'https://plex.tv/api/downloads/1.json'
