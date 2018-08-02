faaa6e5e3777c7d80d265ef1c8e39fa22caad5f4
KeLiu <specter119@users.noreply.github.com>
2018-08-02 17:28:10 +0800

    fix the app folder

--- Casks/presentation.rb
+++ Casks/presentation.rb
@@ -8,5 +8,5 @@ cask 'presentation' do
   homepage 'http://iihm.imag.fr/blanch/software/osx-presentation/'
 
   app 'Présentation.app'
-  binary "#{appdir}/Emacs.app/Contents/MacOS/presentation.py", target: 'presentation'
+  binary "#{appdir}/Présentation.app/Contents/MacOS/presentation.py", target: 'presentation'
 end
