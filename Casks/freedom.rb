23a82bd9a7716921217d5ea9f31dcce0935aaf32
Brian Morton <brianmortondesign@gmail.com>
2018-08-01 17:35:18 -0700

    Update freedom to 1.6.10 (#50347)

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,6 +1,6 @@
 cask 'freedom' do
-  version '1.6.8'
-  sha256 'b986714064bfa0f9ff29999918665694f9a6f9a0c8404892583ca2ad07eac071'
+  version '1.6.10'
+  sha256 '773480972f6bac66664615447765e5fe4e804802449a0887b85da0610ecf8926'
 
   url "https://cdn.freedom.to/installers/updates/mac/#{version}/Freedom.zip"
   appcast 'https://cdn.freedom.to/installers/updates/mac/Appcast.xml'
