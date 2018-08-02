59d8839fb8d15d34881b6e67d9090fc9f8ab5168
Brian Morton <brianmortondesign@gmail.com>
2018-08-02 16:59:13 -0700

    Update outline-manager to 1.1.4

--- Casks/outline-manager.rb
+++ Casks/outline-manager.rb
@@ -1,6 +1,6 @@
 cask 'outline-manager' do
-  version '1.1.3'
-  sha256 '46e919cf6eb841120b53e76d2c8d8948f758ddfb1a6e00e755b93bdbdbb84e32'
+  version '1.1.4'
+  sha256 '5699b69d828f2ec4f3d62000ee97515e37e14f89ad8e296d12451e09258a090f'
 
   # github.com/Jigsaw-Code/outline-server was verified as official when first introduced to the cask
   url "https://github.com/Jigsaw-Code/outline-server/releases/download/v#{version}/Outline-Manager.dmg"
