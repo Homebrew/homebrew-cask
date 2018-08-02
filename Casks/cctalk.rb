209699c693e97cdef2752f9a76e7ebf71166ea13
Brian Morton <brianmortondesign@gmail.com>
2018-08-02 10:22:55 -0700

    Update cctalk to 7.3.1,692 (#50321)

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,14 +1,16 @@
 cask 'cctalk' do
-  version '1.0.2-633,2018-05-02'
-  sha256 '911d55e413237d59fb6c56ef385ea1b4844af1125f2da4fabf9d442e07931eba'
+  version '7.3.1,692'
+  sha256 'e9e372308560f3438f56769cfc0918dfa725cc192a940fb7a307eb3c4f62ab58'
 
-  # f1.ct.hjfile.cn was verified as official when first introduced to the cask
-  url "http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/in/mac/cctalk/archive/#{version.before_comma.hyphens_to_dots}/CCtalk-#{version.before_comma}-xianghu-#{version.after_comma}.dmg"
+  # n1other.hjfile.cn was verified as official when first introduced to the cask
+  url "https://n1other.hjfile.cn/wx/CCtalk/#{version.after_comma}/CCtalk.#{version.before_comma}-#{version.after_comma}.pkg"
   appcast 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/update/info.xml'
   name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
 
   depends_on macos: '>= :yosemite'
 
-  app 'CCtalk.app'
+  pkg "CCtalk.#{version.before_comma}-#{version.after_comma}.pkg"
+
+  uninstall pkgutil: 'com.hujiang.mac.cctalk'
 end
