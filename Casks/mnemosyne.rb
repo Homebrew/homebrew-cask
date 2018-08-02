ccb1b325d7119bcc03e8bd042695b84b2a7ff724
Brian Morton <brianmortondesign@gmail.com>
2018-08-02 09:15:05 -0700

    Update mnemosyne to 2.6.1

--- Casks/mnemosyne.rb
+++ Casks/mnemosyne.rb
@@ -1,6 +1,6 @@
 cask 'mnemosyne' do
-  version '2.6'
-  sha256 'e4a965c4cdc8478baea0be940e333b83016deff3106b3d7f3fb993c479b45a20'
+  version '2.6.1'
+  sha256 '05a63b18d0f110db34898656855f71079f36bfa202f4c978ac7b52ce65463e89'
 
   # sourceforge.net/mnemosyne-proj was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/mnemosyne-proj/mnemosyne/mnemosyne-#{version}/Mnemosyne-#{version}.dmg"
