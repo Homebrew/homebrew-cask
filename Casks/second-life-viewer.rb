e4f5c54e3c30c04d8fc3714e077e33d37fc7be00
Julian Mayer <julian@corecode.io>
2018-08-03 12:05:37 +0200

    Update second-life-viewer to 5.1.7.516813

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,6 +1,6 @@
 cask 'second-life-viewer' do
-  version '5.1.4.515016'
-  sha256 '2bab017313d5114fae0e1df6b71b4b4a29b8319f4d028072b4b70571c23202ad'
+  version '5.1.7.516813'
+  sha256 'e5e2499e08f25c5739c112a20cb0ff635ff3ead7c8ca7128d3fb5c6258d52ff6'
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
   name 'Linden Lab Second Life Viewer'
