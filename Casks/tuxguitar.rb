d1b3f02c85bac3bf7934b617c5183330b7b88fe4
Brian Morton <brianmortondesign@gmail.com>
2018-08-02 17:02:16 -0700

    Update tuxguitar to 1.5.2

--- Casks/tuxguitar.rb
+++ Casks/tuxguitar.rb
@@ -1,6 +1,6 @@
 cask 'tuxguitar' do
-  version '1.5.1'
-  sha256 '30ed6184f646bbd366d91abfae8fce568250617ddc560bdf819da32dd9f05547'
+  version '1.5.2'
+  sha256 '0a08bacd93a1d3d685d79e7af490c63db0b95bb508e84deaca24f64853996e7c'
 
   url "https://downloads.sourceforge.net/tuxguitar/tuxguitar-#{version}-macosx-cocoa-64.app.tar.gz"
   appcast 'https://sourceforge.net/projects/tuxguitar/rss?path=/TuxGuitar'
