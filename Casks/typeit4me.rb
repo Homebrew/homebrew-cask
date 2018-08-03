0632dc71c88919eff874f52bb57813e505e6749a
commitay <commitay@users.noreply.github.com>
2018-08-04 09:49:02 +1000

    Update typeit4me.rb

--- Casks/typeit4me.rb
+++ Casks/typeit4me.rb
@@ -2,10 +2,10 @@ cask 'typeit4me' do
   version '6.0.5b156'
   sha256 'b1912aba43111bc9193e9dcbf3ff669a7543aba952bf3df5a40755d59dd151ac'
 
-  url "http://ettoresoftware.com/download/TypeIt4Me#{version}.zip"
-  appcast 'http://ettoresoftware.com/download/TypeIt4MeAppcast.xml'
+  url "https://ettoresoftware.com/download/TypeIt4Me#{version}.zip"
+  appcast 'https://ettoresoftware.com/download/TypeIt4MeAppcast.xml'
   name 'TypeIt4Me'
-  homepage 'http://www.ettoresoftware.com/mac-apps/typeit4me/'
+  homepage 'https://www.ettoresoftware.com/mac-apps/typeit4me/'
 
   auto_updates true
 
