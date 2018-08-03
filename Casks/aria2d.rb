5d5f1e0ff3c7c2136fb1cec3ec7fbb21076ad85c
commitay <commitay@users.noreply.github.com>
2018-08-03 17:16:26 +1000

    Update aria2d.rb

--- Casks/aria2d.rb
+++ Casks/aria2d.rb
@@ -1,11 +1,14 @@
 cask 'aria2d' do
-  version 'v1.1'
-  sha256 '14c2a4b42f75ceeb347371fe34c7917d23afb7d944d4f1584b8eda9d3b3baa58'
+  version '1.1,1531447123'
+  sha256 '5180dbd15d8711c1b79ea16a55c1190c0ad29841b1b7ab5fe0b12632e2b3cad5'
 
-  # dl.devmate.com/com.xjbeta.Aria2D/ was verified as official when first introduced to the cask
-  url 'https://dl.devmate.com/com.xjbeta.Aria2D/7/1498544488/Aria2D-7.zip'
+  # dl.devmate.com/com.xjbeta.Aria2D was verified as official when first introduced to the cask
+  url "https://dl.devmate.com/com.xjbeta.Aria2D/#{version.before_comma}/#{version.after_comma}/Aria2D-#{version.before_comma}.zip"
+  appcast 'https://updates.devmate.com/com.xjbeta.Aria2D.xml'
   name 'Aria2D'
   homepage 'https://github.com/xjbeta/Aria2D'
+
+  depends_on macos: '>= :sierra'
 
   app 'Aria2D.app'
 end
