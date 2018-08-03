f50333f6240db37af34c68bb19c6a0fc019f6930
commitay <commitay@users.noreply.github.com>
2018-08-04 09:52:30 +1000

    Update mweb.rb

--- Casks/mweb.rb
+++ Casks/mweb.rb
@@ -2,11 +2,11 @@ cask 'mweb' do
   version '3.1.6'
   sha256 '9dd14de39c14b94dc0e971a2da0245ac35527cb5695b0a4f5fd3c49a572113c4'
 
-  # dl.devmate.com/com.coderforart.MWeb was verified as official when first introduced to the cask
+  # dl.devmate.com/com.coderforart.MWeb3 was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coderforart.MWeb3/MWeb3-ProMarkdownwriting%2CnotetakingandstaticbloggeneratorApp.dmg'
   appcast 'https://updates.devmate.com/com.coderforart.MWeb3.xml'
   name 'MWeb'
-  homepage 'http://www.mweb.im/'
+  homepage 'https://www.mweb.im/'
 
   app 'MWeb.app'
 end
