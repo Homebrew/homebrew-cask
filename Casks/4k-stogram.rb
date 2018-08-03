7573f51708ea84f3e2c95c350db0bea145f05fb5
Brian Morton <brianmortondesign@gmail.com>
2018-08-03 16:45:52 -0700

    Update 4k-stogram to 2.6.16.1610 (#50420)

--- Casks/4k-stogram.rb
+++ Casks/4k-stogram.rb
@@ -1,6 +1,6 @@
 cask '4k-stogram' do
-  version '2.6.15.1600'
-  sha256 '7667f0a17a556d2177b5793adf6da4b775670bbfede334fa1f1d3c11029ebdd5'
+  version '2.6.16.1610'
+  sha256 'fee0e8c78aa2fb1fcf82dbcbd1df660ecc873014cb4100609eacb9ffd6efea3a'
 
   url "https://dl.4kdownload.com/app/4kstogram_#{version.major_minor_patch}.dmg"
   appcast 'https://www.4kdownload.com/download'
