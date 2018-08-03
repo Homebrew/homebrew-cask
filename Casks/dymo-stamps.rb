ab7a9a8f64051db843493a59747b4e823de761d4
Vítor Galvão <info@vitorgalvao.com>
2018-08-03 15:38:39 +0100

    Update dymo-stamps.rb

--- Casks/dymo-stamps.rb
+++ Casks/dymo-stamps.rb
@@ -1,8 +1,9 @@
 cask 'dymo-stamps' do
-  version :latest
-  sha256 :no_check
+  version '2.18.3v752'
+  sha256 'c124cc9afa96df68c0c880e7bf20cda844be439df17d8ecbea81a44e56403c28'
 
-  url 'http://download.endicia.com/dymostamps/dymostamps.dmg'
+  url "https://download.endiciaformac.com/EndiciaForMac#{version.no_dots}.dmg"
+  appcast 'https://s3.amazonaws.com/endiciaformac/EndiciaForMacSparkle.xml'
   name 'Dymo Stamps'
   homepage 'http://mac.endicia.com/stamps/'
 
