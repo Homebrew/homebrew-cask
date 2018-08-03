e9d82ffc5aad833581404c221a416dc46eef58a0
Brian Morton <brianmortondesign@gmail.com>
2018-08-03 16:49:43 -0700

    Update zulip to 2.3.5 (#50432)

--- Casks/zulip.rb
+++ Casks/zulip.rb
@@ -1,6 +1,6 @@
 cask 'zulip' do
-  version '2.3.3'
-  sha256 '33f50ff39582a22b515c2d6c03073b6e1f98283e0f99355354c9bab25954486b'
+  version '2.3.5'
+  sha256 'cc308d749481cb2c75d77c3e2f6142c3dd51ceb2641880bdb6442b7b8a142b46'
 
   # github.com/zulip/zulip-electron was verified as official when first introduced to the cask
   url "https://github.com/zulip/zulip-electron/releases/download/v#{version}/Zulip-#{version}.zip"
