ebbb5301c21ada8ccb5657087cd7a5c66acc2742
Brian Morton <brianmortondesign@gmail.com>
2018-08-03 16:46:08 -0700

    Update augur to 1.1.1 (#50421)

--- Casks/augur.rb
+++ Casks/augur.rb
@@ -1,6 +1,6 @@
 cask 'augur' do
-  version '1.1.0'
-  sha256 'a5c73bc34844d9666d91c58a45e09b0449b15aa59d13057cb96341e7a0d470a0'
+  version '1.1.1'
+  sha256 '93ed6a353755789db45c34573d2688b101ca8f9caf02967eeb8b1a4c2f319048'
 
   url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-augur-#{version}.dmg"
   appcast 'https://github.com/AugurProject/augur-app/releases.atom'
