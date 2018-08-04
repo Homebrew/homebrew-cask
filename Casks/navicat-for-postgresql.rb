85f2a9447a7da6d255e7b1f7abdfda8c2af99513
commitay <commitay@users.noreply.github.com>
2018-08-04 11:21:02 +1000

    Update navicat-for-postgresql to 12.1.4 (#50441)

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-postgresql' do
-  version '12.1.3'
-  sha256 'b3a01032dd78842fde3a2c116429dfbf7ce2a0e773a26ecc0e72a43e7b0f4179'
+  version '12.1.4'
+  sha256 '0056efa735966be13b7bbe6921d20ad1cb04a3e37c8c058b5ea3c112ba052cc4'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-postgresql-release-note'
