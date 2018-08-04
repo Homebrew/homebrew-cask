f0c3ef30c766128d1ef0318cf15b0b44a530194a
Julian Mayer <julian@corecode.io>
2018-08-04 20:24:02 +0200

    Update marsedit to 4.1.2

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,9 +1,9 @@
 cask 'marsedit' do
-  version '4.1'
-  sha256 '1c5ad80b6a7cf0702de461b74023b28f09ff1b25be03f1e4d1bc6eb99547b6b1'
+  version '4.1.2'
+  sha256 '6fb2abf9a07cc89b3998253e605c649105af252f038dbc13fd15535f180ac677'
 
   url "https://red-sweater.com/marsedit/MarsEdit#{version}.zip"
-  appcast 'https://red-sweater.com/marsedit/appcast3.php'
+  appcast 'https://red-sweater.com/marsedit/appcast4.php'
   name 'MarsEdit'
   homepage 'https://red-sweater.com/marsedit/'
 
