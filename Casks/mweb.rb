d3c438dba53961696ba6ab9bb4bd81784b22301c
suschizu <brigitte@corecode.io>
2018-08-04 02:11:53 +0200

    Update mweb to 3.1.6 (#50433)
    * Update mweb to 3.1.6
    
    * Update mweb.rb

--- Casks/mweb.rb
+++ Casks/mweb.rb
@@ -1,12 +1,12 @@
 cask 'mweb' do
-  version '2.3.0'
-  sha256 'f1985e2c3e2e44e9243ac390663996c1ad2480166c52ca2bbcbe4a4623580fec'
+  version '3.1.6'
+  sha256 '9dd14de39c14b94dc0e971a2da0245ac35527cb5695b0a4f5fd3c49a572113c4'
 
-  # dl.devmate.com/com.coderforart.MWeb was verified as official when first introduced to the cask
-  url 'https://dl.devmate.com/com.coderforart.MWeb/MWeb-ProMarkdownwriting%2CnotetakingandstaticbloggeneratorApp.dmg'
-  appcast 'https://updates.devmate.com/com.coderforart.MWeb.xml'
+  # dl.devmate.com/com.coderforart.MWeb3 was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.coderforart.MWeb3/MWeb3-ProMarkdownwriting%2CnotetakingandstaticbloggeneratorApp.dmg'
+  appcast 'https://updates.devmate.com/com.coderforart.MWeb3.xml'
   name 'MWeb'
-  homepage 'http://www.mweb.im/'
+  homepage 'https://www.mweb.im/'
 
   app 'MWeb.app'
 end
