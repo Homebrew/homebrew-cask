515a4fff70fd9751deca7aca1a9b42f832d25238
Valkum <rudi.floren@gmail.com>
2018-08-05 21:55:56 +0200

    Add comment about difference in url and homepage.
    The author provides the built app via bintray.

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -2,6 +2,7 @@ cask 'nheko' do
   version '0.5.2'
   sha256 '76eb8b542a224cfa989c995a08654d8e993324b770024f912d8983a2d3520c26'
 
+  # bintray.com/mujx/matrix was verified as official when first introduced to the cask
   url 'https://bintray.com/mujx/matrix/download_file?file_path=nheko%2Fv0.5.2%2Fnheko-v0.5.2.dmg'
   name 'Nheko'
   homepage 'https://github.com/mujx/nheko'
