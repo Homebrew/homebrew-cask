2dbea4e0a8b56f240a64ac23dd0688cb849f6bed
Valkum <rudi.floren@gmail.com>
2018-08-05 21:31:52 +0200

    Add Nheko.app v0.5.2
    This commit adds the Matrix chat client Nheko written in C++/Qt.
    Nheko is open-sourced but offers this build app for MacOS.

---
+++ Casks/nheko.rb
@@ -0,0 +1,10 @@
+cask 'nheko' do
+  version '0.5.2'
+  sha256 '76eb8b542a224cfa989c995a08654d8e993324b770024f912d8983a2d3520c26'
+
+  url 'https://bintray.com/mujx/matrix/download_file?file_path=nheko%2Fv0.5.2%2Fnheko-v0.5.2.dmg'
+  name 'Nheko'
+  homepage 'https://github.com/mujx/nheko'
+
+  app 'Nheko.app'
+end
