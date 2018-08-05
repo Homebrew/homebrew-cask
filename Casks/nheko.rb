9fc048937eb76fc3d9511232d488de10098d0e38
Vítor Galvão <info@vitorgalvao.com>
2018-08-06 00:59:52 +0100

    Update nheko.rb

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -3,7 +3,8 @@ cask 'nheko' do
   sha256 '76eb8b542a224cfa989c995a08654d8e993324b770024f912d8983a2d3520c26'
 
   # bintray.com/mujx/matrix was verified as official when first introduced to the cask
-  url 'https://bintray.com/mujx/matrix/download_file?file_path=nheko%2Fv0.5.2%2Fnheko-v0.5.2.dmg'
+  url "https://bintray.com/mujx/matrix/download_file?file_path=nheko%2Fv#{version}%2Fnheko-v#{version}.dmg"
+  appcast 'https://github.com/mujx/nheko/releases.atom'
   name 'Nheko'
   homepage 'https://github.com/mujx/nheko'
 
