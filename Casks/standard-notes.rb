ce9b3228db41f995ee7e63681e4cb2e0e185f08e
Brian Morton <brianmortondesign@gmail.com>
2018-08-06 09:42:59 -0700

    Update standard-notes to 2.3.3

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,6 +1,6 @@
 cask 'standard-notes' do
-  version '2.3.2'
-  sha256 'cd35b4351900e3cb31716f837aa64e3d67eedf8b995eec328c70d868b9af1271'
+  version '2.3.3'
+  sha256 'd76be13820c3061d980a80b3cd82df07d06b68cdf0ff0b4eff9b904734e5639a'
 
   # github.com/standardnotes/desktop was verified as official when first introduced to the cask
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac.zip"
