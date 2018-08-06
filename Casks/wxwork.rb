506948578cf637f6ba77426a911a8b438f48c42e
wu.nerd <wu-nerd@users.noreply.github.com>
2018-08-06 19:33:56 +0800

    Update wxwork to 2.4.991.1002 (#50476)

--- Casks/wxwork.rb
+++ Casks/wxwork.rb
@@ -1,6 +1,6 @@
 cask 'wxwork' do
-  version '2.4.22.1020'
-  sha256 '2f539832456e7988fb3af8cdae5cce7818e5137e61167610ed67d1aeea0e48ac'
+  version '2.4.991.1002'
+  sha256 '2f65b0ef04d4557f6dd9721c677d6c40c5797e15a195dd7185ef890d3a7a97ad'
 
   url "https://dldir1.qq.com/foxmail/work_weixin/WXWork_#{version}.dmg"
   name 'Wechat Enterprise Version'
