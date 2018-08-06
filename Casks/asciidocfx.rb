d4353b392e2f485029baa0ace24aadad0c2108c6
Rahman Usta <rahmanusta@users.noreply.github.com>
2018-08-06 13:14:56 +0300

    Update AsciidocFX version to 1.6.3 (#50465)
    * Update asciidocfx.rb
    
    * Update asciidocfx.rb

--- Casks/asciidocfx.rb
+++ Casks/asciidocfx.rb
@@ -1,6 +1,6 @@
 cask 'asciidocfx' do
-  version '1.6.2'
-  sha256 '149ccd0e030e7e8c3f30a1b1282174b4cbf932f406d12f80ec081598d4b69eb4'
+  version '1.6.3'
+  sha256 'a7385ccd9ee17e039f0a41e3509b6cbc39192a6c23cabef08eb4e9f55f4f8e1f'
 
   # github.com/asciidocfx/AsciidocFX was verified as official when first introduced to the cask
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac.dmg"
