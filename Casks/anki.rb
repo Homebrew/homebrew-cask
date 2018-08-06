18ccce98ebf96c108e581ddc1f290ebb98f112e2
Brian Morton <brianmortondesign@gmail.com>
2018-08-06 09:58:17 -0700

    Update anki to 2.1.0

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,13 +1,13 @@
 cask 'anki' do
-  version '2.0.52'
-  sha256 '468814d16ad3146e5551e5a7c5c2fff7f0f0301c1c7dc5fdaa8a049732e054c9'
+  version '2.1.0'
+  sha256 'ffca65bfc67bb77a46bc289201421e46dce8149d89e9ecd3e0d3702cf024b5d4'
 
-  url "https://apps.ankiweb.net/downloads/current/anki-#{version}.dmg"
+  url "https://apps.ankiweb.net/downloads/current/anki-#{version}-mac.dmg"
   appcast 'https://apps.ankiweb.net/docs/changes.html'
   name 'Anki'
   homepage 'https://apps.ankiweb.net/'
 
-  depends_on macos: '>= :lion'
+  depends_on macos: '>= :yosemite'
 
   app 'Anki.app'
 end
