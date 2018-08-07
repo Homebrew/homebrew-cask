7b3adf80918a34b3653b634566bca2ed35f2d0ec
Brian Morton <brianmortondesign@gmail.com>
2018-08-07 09:53:12 -0700

    Update reveal to 17

--- Casks/reveal.rb
+++ Casks/reveal.rb
@@ -1,6 +1,6 @@
 cask 'reveal' do
-  version '16'
-  sha256 '17e67e58b0ec8766c06c996a572b53e6b3b2beeb1f823d9d3412de9b6a38ede6'
+  version '17'
+  sha256 'c319812397b39062d05a445b5ee40f3d1eef304f26bb3871f1f2767bab8a8449'
 
   url "https://download.revealapp.com/Reveal.app-#{version}.zip"
   appcast 'https://revealapp.com/download/'
