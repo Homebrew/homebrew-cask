492c9127599861af162a4dd0e32ebbbbc20e5833
Lennart Lövstrand <lennart.github@lovstrand.com>
2018-08-07 12:40:38 +0200

    Update freefilesync to 10.3

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,6 +1,6 @@
 cask 'freefilesync' do
-  version '10.2'
-  sha256 'e64ec6a1c9c5355affadc2842adf0a7dda164476317f21baa3506aa8507b4315'
+  version '10.3'
+  sha256 '81d69b3e1040e40faa9c08b2721596620a05b46ecee06b4258bc09852b121a77'
 
   url "http://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake
