5f4264c970b1d977c50f54fc8803147c3b95ba9e
Brian Morton <brianmortondesign@gmail.com>
2018-08-07 09:45:48 -0700

    Update macupdater to 1.3.0

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -1,6 +1,6 @@
 cask 'macupdater' do
-  version '1.2.10'
-  sha256 'c7a0049e5fb6b4baa86c2fc8381ea6b9af746542382692f335885d52a5a3fd99'
+  version '1.3.0'
+  sha256 'dadd71f8163f91980435b601470c8ba7ed0b66455965f464e4aeee5874595794'
 
   url "https://www.corecode.io/downloads/macupdater_#{version}.zip"
   appcast 'https://www.corecode.io/macupdater/macupdater.xml'
