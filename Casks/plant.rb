e9bf0e9d1daa772d71c87b3a58cfe512c9de5988
Brian Morton <brianmortondesign@gmail.com>
2018-08-07 09:49:39 -0700

    Update plant to 1.4.2.3

--- Casks/plant.rb
+++ Casks/plant.rb
@@ -1,6 +1,6 @@
 cask 'plant' do
-  version '1.4.2.2'
-  sha256 '68592ce1b709222ffac3b07734e642031b21e78866d7920b2022cf09fe659b45'
+  version '1.4.2.3'
+  sha256 'f759fd217bc735ddc64bd13edd3bd5da3edd3bbd4226f6a015fe9e16bdbdb676'
 
   url "https://projects.plantapp.io/app/plant_v#{version}.dmg"
   appcast 'https://projects.plantapp.io/app/changelog.xml'
