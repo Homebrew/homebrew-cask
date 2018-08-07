dc0cdcaafbf3bae339b78fde1d699b7d324cc9b6
Brian Morton <brianmortondesign@gmail.com>
2018-08-07 09:51:27 -0700

    Update remembear to 1.1.0

--- Casks/remembear.rb
+++ Casks/remembear.rb
@@ -1,6 +1,6 @@
 cask 'remembear' do
-  version '1.0.5'
-  sha256 '5f69738acff0c843df47ad9a8472ca4c17c68c5e215598c61ba52990b142fc25'
+  version '1.1.0'
+  sha256 '158c7b6966ed65fed6f6fb6952f22c4a78427436dc1dc06e5795cc261fc24fcb'
 
   # s3.amazonaws.com/remembear was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/remembear/app/release/downloads/macOS/RememBear-#{version}.zip"
