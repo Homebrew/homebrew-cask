a4b434b93eb41074315bda776ec0375aa679d34e
Poorani <poorani.subramanian@nih.gov>
2018-08-07 17:51:39 -0400

    fix app stanza

--- Casks/jbrowse.rb
+++ Casks/jbrowse.rb
@@ -3,10 +3,10 @@ cask 'jbrowse' do
   sha256 'b56f0275d890f2c38c7410273f7c52c10df35972f34f1867492ad626854951f1'
 
   # github.com/GMOD/jbrowse was verified as official when first introduced to the cask
-  url 'https://github.com/GMOD/jbrowse/releases/download/1.15.1-release/JBrowse-1.15.1-desktop-darwin-x64.zip'
+  url "https://github.com/GMOD/jbrowse/releases/download/#{version}-release/JBrowse-#{version}-desktop-darwin-x64.zip"
   appcast 'https://github.com/GMOD/jbrowse/releases.atom'
   name 'jbrowse'
   homepage 'https://jbrowse.org/'
 
-  app 'JBrowseDesktop-darwin-x64/JBrowseDesktop.app'
+  app "JBrowse-#{version}-desktop-darwin-x64/JBrowse-#{version}-desktop.app"
 end
