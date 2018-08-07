4e2432fb4fd4f2cd60e94961caf19b795c0521d6
Brian Morton <brianmortondesign@gmail.com>
2018-08-07 09:54:11 -0700

    Update skim to 1.4.36

--- Casks/skim.rb
+++ Casks/skim.rb
@@ -1,6 +1,6 @@
 cask 'skim' do
-  version '1.4.35'
-  sha256 'e4922bd8231c20b478bd24dd1124188ae770d78626cc98858f84e3316ef54dc9'
+  version '1.4.36'
+  sha256 '23c32aa33453f6a584378590392740539184b3450e8887a77a20b1931b45af6a'
 
   url "https://downloads.sourceforge.net/skim-app/Skim/Skim-#{version}/Skim-#{version}.dmg"
   appcast 'https://skim-app.sourceforge.io/skim.xml'
