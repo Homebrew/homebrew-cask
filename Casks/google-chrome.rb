fc706d95a83022c285f870e777b960d50b7fba9a
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2018-08-08 13:26:32 +0200

    Update google-chrome.rb

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,18 +1,18 @@
 cask 'google-chrome' do
-	if MacOS.version >= :yosemite
-		version '68.0.3440.84'
-		sha256 'c61bf8dcf737cf32f64bac9547f0ffff5ce5b6d0fc7c828e7e75fead60b0a6d6'
-		url 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
-		appcast 'https://omahaproxy.appspot.com/history?os=mac;channel=stable'
-	elsif MacOS.version == :mavericks
-		version '65.0.3325.181s'
-		sha256 '60f5d8e2d7d80f8170c85e247c319b7cfd58b740c389483a5eceae3969b1a3be'
-		url 'https://dl.google.com/dl/chrome/mac/legacy10_9/GGRO/googlechrome.dmg'
-	else
-		version '49.0.2623.112'
-		sha256 '69c416f9ee5cb60ffd91baad3db567cbc98da9f58cb5348f0ef927820a6020c9'
-		url 'https://dl.google.com/dl/chrome/mac/legacy/GGRO/googlechrome.dmg'
-	end
+  if MacOS.version >= :yosemite
+    version '68.0.3440.84'
+    sha256 'c61bf8dcf737cf32f64bac9547f0ffff5ce5b6d0fc7c828e7e75fead60b0a6d6'
+    url 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
+    appcast 'https://omahaproxy.appspot.com/history?os=mac;channel=stable'
+  elsif MacOS.version == :mavericks
+    version '65.0.3325.181s'
+    sha256 '60f5d8e2d7d80f8170c85e247c319b7cfd58b740c389483a5eceae3969b1a3be'
+    url 'https://dl.google.com/dl/chrome/mac/legacy10_9/GGRO/googlechrome.dmg'
+  else
+    version '49.0.2623.112'
+    sha256 '69c416f9ee5cb60ffd91baad3db567cbc98da9f58cb5348f0ef927820a6020c9'
+    url 'https://dl.google.com/dl/chrome/mac/legacy/GGRO/googlechrome.dmg'
+  end
 
   name 'Google Chrome'
   homepage 'https://www.google.com/chrome/'
@@ -22,7 +22,7 @@ cask 'google-chrome' do
                          'google-chrome-beta',
                          'google-chrome-dev',
                        ]
-                       
+
   app 'Google Chrome.app'
 
   uninstall launchctl: [
