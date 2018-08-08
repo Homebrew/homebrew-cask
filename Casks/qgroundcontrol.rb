8cc45ae2c2b0e5aa7de573bc7962451cafe66a87
Brian Morton <brianmortondesign@gmail.com>
2018-08-08 10:46:40 -0700

    Update qgroundcontrol to 3.4.0 (#50610)

--- Casks/qgroundcontrol.rb
+++ Casks/qgroundcontrol.rb
@@ -1,6 +1,6 @@
 cask 'qgroundcontrol' do
-  version '3.3.2'
-  sha256 'fe9ad6822d795de62e7a7c8264d5f1425572ccdbf4712e442758208d6c6b1a77'
+  version '3.4.0'
+  sha256 'cc0a4ca5268edbb2d2ed2490b1b432a93ab61ad3690503b3188fa71af4833694'
 
   # github.com/mavlink/qgroundcontrol was verified as official when first introduced to the cask
   url "https://github.com/mavlink/qgroundcontrol/releases/download/v#{version}/QGroundControl.dmg"
