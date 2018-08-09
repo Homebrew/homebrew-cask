1bc79966582520c3e83e278df615a56375d5c8ae
Brian Morton <brianmortondesign@gmail.com>
2018-08-09 11:14:22 -0700

    Update razorsql to 8.0.4 (#50676)

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask 'razorsql' do
-  version '8.0.3'
-  sha256 '099f20819774c592f2489f2993ebb54aadbe35c83827d4aa64e99b93208aae31'
+  version '8.0.4'
+  sha256 '2664afd6e009dd56c23e86197e2acd4f540bc963fc2b811b3f65dcb089e859b2'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   appcast 'https://razorsql.com/updates.html'
