70ae2d23712f99cbe8e57c67eeb474d12cf4cf73
Brian Morton <brianmortondesign@gmail.com>
2018-08-10 09:34:45 -0700

    Update falcon-sql-client to 3.0.2 (#50696)

--- Casks/falcon-sql-client.rb
+++ Casks/falcon-sql-client.rb
@@ -1,6 +1,6 @@
 cask 'falcon-sql-client' do
-  version '3.0.1'
-  sha256 '2123ba88e2b89d246f808bd65a2e21d1f250f27a60a05c0f04f1476938554a2b'
+  version '3.0.2'
+  sha256 'f3155c4615fa6b3bf90abd274353fa74b7cdf992a9e769e659a047f691cd8c20'
 
   # github.com/plotly/falcon-sql-client was verified as official when first introduced to the cask
   url "https://github.com/plotly/falcon-sql-client/releases/download/v#{version}/mac-falcon-v#{version}.zip"
