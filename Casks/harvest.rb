d906fc717cd57ffc8d07d18a27afd8426e39495b
Brian Morton <brianmortondesign@gmail.com>
2018-08-10 09:34:38 -0700

    Update harvest to 2.1.8 (#50697)

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -1,6 +1,6 @@
 cask 'harvest' do
-  version '2.1.7'
-  sha256 '831513173a0bc0dbdb017116d4ae91a06c597c2e0d57e6900cf67b78aa441e8e'
+  version '2.1.8'
+  sha256 'e0205cb5831ef9e3db3bbd7ab1dcc44210e3e6ca35011b646fa52e7bccf5cb2b'
 
   url "https://www.getharvest.com/harvest/mac/Harvest.#{version}.zip"
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml'
