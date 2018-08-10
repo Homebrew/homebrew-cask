2b35fd937837b1fb8890b3eb891cbc67ae5cc6e2
layker <layker.zh@gmail.com>
2018-08-10 14:11:42 +0800

    update xiami to 7.0.9 08100149 (#50684)

--- Casks/xiami.rb
+++ Casks/xiami.rb
@@ -1,6 +1,6 @@
 cask 'xiami' do
-  version '7.0.8,08071904'
-  sha256 '60e6ece6fcd4ebb9bbf868e4e5ba90d932e0599707a36bb0dca76e5de84fb0cf'
+  version '7.0.9,08100149'
+  sha256 '5f4455a2dd7cec6d2c04ee9a00303a126661b84be0551a137d540cecc8645089'
 
   # gxiami.alicdn.com/xiami-desktop was verified as official when first introduced to the cask
   url "http://gxiami.alicdn.com/xiami-desktop/update/XiamiMac-#{version.after_comma}.dmg"
