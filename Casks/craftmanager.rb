39f1c3b04d5ee762dacfa1f1a8eb61ab13042a1c
Victor Popkov <victor@popkov.me>
2018-08-10 09:00:23 +0300

    Update craftmanager to 1.0.70

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -1,5 +1,5 @@
 cask 'craftmanager' do
-  version '1.7.0'
+  version '1.0.70'
   sha256 '6b0cd143163ce44ac320da5bb1f4ffd2c009d195464e45c62e46571465dcf8e4'
 
   url 'https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip'
