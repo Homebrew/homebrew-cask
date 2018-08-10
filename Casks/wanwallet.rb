4b5fbcfae1decd993c2a5a6100151fc48d13ef43
Vítor Galvão <info@vitorgalvao.com>
2018-08-10 18:46:11 +0100

    Update wanwallet.rb

--- Casks/wanwallet.rb
+++ Casks/wanwallet.rb
@@ -1,5 +1,5 @@
 cask 'wanwallet' do
-  version '2.1.0'
+  version '2.1.2'
   sha256 'd260cecc85ee72cf1c802f996bb5e5387fabe509c25c01186364146d9211d165'
 
   url "https://wanchain.org/download/macwallet#{version.major}"
