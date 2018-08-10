910c83cede0f3672a3064ef83e9dc5feff343dec
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-08-10 16:07:27 +0200

    Add Wanchain Wallet GUI 1.0.5

---
+++ Casks/wanchain-wallet.rb
@@ -0,0 +1,10 @@
+cask 'wanchain-wallet' do
+  version '1.0.5'
+  sha256 'ccc136cb77c27347906fe99911ff7b7cb2da03985960e80f96a27afc672db4a3'
+
+  url "https://wanchain.org/download/macwallet"
+  name 'Wanchain Wallet GUI'
+  homepage 'https://wanchain.org/product'
+
+  app 'WanWalletGui.app'
+end
