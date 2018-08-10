624b1d5c1487190189f1ca8db1ccf6329957c712
Huy Pham <duchuykun@gmail.com>
2018-08-10 12:48:04 +0700

    Update TablePlus build to 119.

--- Casks/tableplus.rb
+++ Casks/tableplus.rb
@@ -1,6 +1,6 @@
 cask 'tableplus' do
-  version '1.0,118'
-  sha256 'eab49b8a06b8fb55a577b29875bfb57b13fc0e1039fa7876bff4f638edb66807'
+  version '1.0,119'
+  sha256 'a7d472e529c137864d08d011ed37348e035490daf2e6413e071ad7479a0e2962'
 
   # s3.amazonaws.com/tableplus-osx-builds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/tableplus-osx-builds/#{version.after_comma}/TablePlus.zip"
