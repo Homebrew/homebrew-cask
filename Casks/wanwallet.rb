c3ee418f29886189fe05a302cf3bee7fe76d0937
Vítor Galvão <info@vitorgalvao.com>
2018-08-10 17:23:42 +0100

    Update and rename wanchain-wallet.rb to wanwallet.rb

--- Casks/wanwallet.rb
+++ Casks/wanwallet.rb
@@ -1,8 +1,8 @@
-cask 'wanchain-wallet' do
+cask 'wanwallet' do
   version '1.0.5'
   sha256 'ccc136cb77c27347906fe99911ff7b7cb2da03985960e80f96a27afc672db4a3'
 
-  url "https://wanchain.org/download/macwallet"
+  url 'https://wanchain.org/download/macwallet'
   name 'Wanchain Wallet GUI'
   homepage 'https://wanchain.org/product'
 
