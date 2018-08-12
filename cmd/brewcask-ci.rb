b7cc48ac15d65c908ea90a1f5ea3b8e7dece64f8
Markus Reiter <me@reitermark.us>
2018-08-12 23:07:56 +0200

    Output more helpful messages on Travis. (#50718)

--- cmd/brewcask-ci.rb
+++ cmd/brewcask-ci.rb
@@ -4,7 +4,7 @@ require "utils/github"
 require "utils/formatter"
 
 require_relative "lib/capture"
-require_relative "lib/diffable"
+require_relative "lib/check"
 require_relative "lib/travis"
 
 module Hbc
@@ -48,60 +48,12 @@ module Hbc
           was_installed = cask.installed?
           cask_dependencies = CaskDependencies.new(cask).reject(&:installed?)
 
-          checks = {
-            installed_apps: Diffable.new do
-              ["/Applications", File.expand_path("~/Applications")]
-                .flat_map { |dir| Dir["#{dir}/**/*.app"] }
-            end,
-            installed_kexts: Diffable.new do
-              system_command!("/usr/sbin/kextstat", args: ["-kl"], print_stderr: false)
-                .stdout
-                .lines
-                .map { |l| l.match(/^.{52}([^\s]+)/)[1] }
-                .grep_v(/^com\.apple\./)
-            end,
-            installed_launchjobs: Diffable.new do
-              format_launchjob = lambda { |file|
-                name = file.basename(".plist").to_s
-                label = Plist.parse_xml(File.read(file))["Label"]
-                (name == label) ? name : "#{name} (#{label})"
-              }
-
-              [
-                "~/Library/LaunchAgents",
-                "~/Library/LaunchDaemons",
-                "/Library/LaunchAgents",
-                "/Library/LaunchDaemons",
-              ].map { |p| Pathname(p).expand_path }
-                .select(&:directory?)
-                .flat_map(&:children)
-                .select { |child| child.extname == ".plist" }
-                .map(&format_launchjob)
-            end,
-            loaded_launchjobs: Diffable.new do
-              launchctl = lambda do |sudo|
-                system_command!("/bin/launchctl", args: ["list"], print_stderr: false, sudo: sudo)
-                  .stdout
-                  .lines.drop(1)
-              end
-
-              [false, true]
-                .flat_map(&launchctl)
-                .map { |l| l.split(/\s+/)[2] }
-                .grep_v(/^com\.apple\./)
-            end,
-            installed_pkgs: Diffable.new do
-              Pathname("/var/db/receipts")
-                .children
-                .grep(/\.plist$/)
-                .map(&:basename)
-            end,
-          }
+          check = Check.new
 
           overall_success &= step "brew cask install #{cask.token}", "install" do
             Installer.new(cask, force: true).uninstall if was_installed
 
-            checks.values.each(&:before)
+            check.before
 
             Installer.new(cask, verbose: true).install
           end
@@ -120,30 +72,11 @@ module Hbc
               end
             end
 
-            checks.each do |name, check|
-              check.after
-              next unless check.changed?
+            check.after
 
-              success = false
+            $stderr.puts check.message unless check.success?
 
-              message = case name
-              when :installed_pkgs
-                "Some packages were not uninstalled."
-              when :loaded_launchjobs
-                "Some launch jobs were not unloaded."
-              when :installed_launchjobs
-                "Some launch jobs were not uninstalled."
-              when :installed_kexts
-                "Some kernel extensions were not uninstalled."
-              when :installed_apps
-                "Some applications were not uninstalled."
-              end
-
-              $stderr.puts Formatter.error(message, label: "Error")
-              $stderr.puts check.diff_lines.join("\n")
-            end
-
-            success
+            success && check.success?
           end
         end
 
