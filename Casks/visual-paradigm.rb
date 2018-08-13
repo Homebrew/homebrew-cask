88d05befec2d94758a89b2f63fd182d94cc0c254
Brian Morton <brianmortondesign@gmail.com>
2018-08-13 11:37:36 -0700

    Update visual-paradigm to 15.1,20180807 (#50773)

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm' do
-  version '15.1,20180805'
-  sha256 '0da5c3ac6d7be39557c76d8e34229561794732b56499ea3a3dbdd046f6ba324e'
+  version '15.1,20180807'
+  sha256 'cb82a2ebae2f69bf46676110fcf322c570c3aa90d48c6e1f9090ff5c6bb1753f'
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vp/checksum.html'
