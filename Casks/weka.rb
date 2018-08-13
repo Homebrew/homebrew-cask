56a6c0d498adae59f9820d9f140732b2649e2dcf
Brian Morton <brianmortondesign@gmail.com>
2018-08-13 11:43:44 -0700

    Update weka to 3.9.2 (#50775)

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -1,6 +1,6 @@
 cask 'weka' do
-  version '3.8.2'
-  sha256 '59eeb176bbb43fd37d2dc278cb615d476110d6b8030d5cd912342f04eeecf8f9'
+  version '3.9.2'
+  sha256 'dc5051bd460f16abc0a63ad4039a75a15aa442f2de4d043e44e91e33e7d28115'
 
   # sourceforge.net/weka was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-oracle-jvm.dmg"
