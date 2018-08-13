3ad071ad89d66f9f1743121e4d759a700915f37d
Brian Morton <brianmortondesign@gmail.com>
2018-08-13 11:51:27 -0700

    Update altair to 1.7.9 (#50742)

--- Casks/altair.rb
+++ Casks/altair.rb
@@ -1,6 +1,6 @@
 cask 'altair' do
-  version '1.7.8'
-  sha256 'e8d067756edeed5a4c6e5cc40cf4fb258682e231602f280cecf7ecfe9a6ec05c'
+  version '1.7.9'
+  sha256 'dd5c98cb5cca4c3dbe1f5d13f0c2cbfaf634bb5fd44c6650d1d644bdb094bd49'
 
   # github.com/imolorhe/altair was verified as official when first introduced to the cask
   url "https://github.com/imolorhe/altair/releases/download/v#{version}/altair-#{version}-mac.zip"
