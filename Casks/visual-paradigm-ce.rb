41ddab79794bd6bfc993fb0232866e4bf279a021
Brian Morton <brianmortondesign@gmail.com>
2018-08-13 09:47:13 -0700

    Update visual-paradigm-ce to 15.1,20180807

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm-ce' do
-  version '15.1,20180805'
-  sha256 'bf06cdb51d30e107281586220ec20e93c6fdd926c2be8bdc944c27f8b360eefe'
+  version '15.1,20180807'
+  sha256 'fdd4a7c80077789c0ef1d7e222f2d5ee87d6d56ec5e5a324535fba40ceb3ed81'
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vpce#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html'
