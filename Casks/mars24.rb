dad13aff2726ca92e8e1d0b31a76d425053c07d9
Vítor Galvão <info@vitorgalvao.com>
2018-08-13 19:52:21 +0100

    Update mars24.rb

--- Casks/mars24.rb
+++ Casks/mars24.rb
@@ -1,4 +1,5 @@
 cask 'mars24' do
+  # note: "24" is not a version number, but an intrinsic part of the product name
   version '7.1.3'
   sha256 '78c40b1823f02ceb8de6ff6eb00461f272f0ce9305dda6c6f52361ed5cfb7489'
 
