507f7d37adbae0d03bfd98d7067a1ff2f7d3f04e
c0llision <c0llision@protonmail.com>
2018-08-13 14:46:17 +0100

    Update qbittorrent to 4.1.2

--- Casks/qbittorrent.rb
+++ Casks/qbittorrent.rb
@@ -1,6 +1,6 @@
 cask 'qbittorrent' do
-  version '4.1.1'
-  sha256 'a2983b792de0052c403fd0b0b571e7e40e867a81ac50753b154b733010cd04e5'
+  version '4.1.2'
+  sha256 '6371619305f90a184c38fed4353eec5a385dc70525b9834f5702a49e4e7b2c2b'
 
   # sourceforge.net/qbittorrent was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg"
