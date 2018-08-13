e6f4a8ea3aed8fbb4c7773400e717dd1eadaae7e
Brian Morton <brianmortondesign@gmail.com>
2018-08-13 09:21:27 -0700

    Update double-commander to 0.8.3-8160

--- Casks/double-commander.rb
+++ Casks/double-commander.rb
@@ -1,6 +1,6 @@
 cask 'double-commander' do
-  version '0.8.2-8010'
-  sha256 '7b18802679e4fe94d91df90f97c1e077c694ad9833141b534a2ef6cc21e64598'
+  version '0.8.3-8160'
+  sha256 '80f5b3fc3f736dbf99155f31d1003a83ee42e7207048a70f672cbf00afb811b1'
 
   url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version}.qt.x86_64.dmg"
   appcast 'https://sourceforge.net/projects/doublecmd/rss'
