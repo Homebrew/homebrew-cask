74cc948de74c8d51130b8196447bb3acab69d433
Brian Morton <brianmortondesign@gmail.com>
2018-08-13 11:25:23 -0700

    Update mullvadvpn to 2018.2

--- Casks/mullvadvpn.rb
+++ Casks/mullvadvpn.rb
@@ -1,9 +1,9 @@
 cask 'mullvadvpn' do
-  version '2018.1'
-  sha256 '9a0c912ed0cf2222649f9367c6cfea338e02c737865bd556f230353096c49f37'
+  version '2018.2'
+  sha256 '9a3436e566cd7577e78cfe87a77b874ce631da1502dc09914e14ca49ad26642f'
 
   # github.com/mullvad/mullvadvpn-app was verified as official when first introduced to the cask
-  url "https://github.com/mullvad/mullvadvpn-app/releases/download/#{version}/MullvadVPN-#{version}.dmg"
+  url "https://github.com/mullvad/mullvadvpn-app/releases/download/#{version}/MullvadVPN-#{version}.pkg"
   appcast 'https://github.com/mullvad/mullvadvpn-app/releases.atom'
   name 'Mullvad'
   homepage 'https://mullvad.net/'
