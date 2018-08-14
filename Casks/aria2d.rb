b992285fa3e28889a41330a17b4fe3685934c9bd
Brian Morton <brianmortondesign@gmail.com>
2018-08-14 09:52:27 -0700

    Update aria2d to 1.2,1534259211 (#50807)

--- Casks/aria2d.rb
+++ Casks/aria2d.rb
@@ -1,6 +1,6 @@
 cask 'aria2d' do
-  version '1.1,1531447123'
-  sha256 '5180dbd15d8711c1b79ea16a55c1190c0ad29841b1b7ab5fe0b12632e2b3cad5'
+  version '1.2,1534259211'
+  sha256 '1e2649855f2d4c422c3c916be5d6b202d3dca069f979d2a42967208da29978c7'
 
   # dl.devmate.com/com.xjbeta.Aria2D was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.xjbeta.Aria2D/#{version.before_comma}/#{version.after_comma}/Aria2D-#{version.before_comma}.zip"
