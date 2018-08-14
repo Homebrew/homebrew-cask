720e085698babb411490b426bd27b0428fed3449
Julian Mayer <julian@corecode.io>
2018-08-14 18:33:57 +0200

    Update understand to 5.0.954

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask 'understand' do
-  version '5.0.953'
-  sha256 '3a8e51d62e84f2d930b4768e1249239b16bdd39d850d3979a31d0ba6125fde3d'
+  version '5.0.954'
+  sha256 'b422190bcc6c7c4a09ba9f4a232d314bd017ca046aeb3e90eee093af208e1f26'
 
   url "http://latest.scitools.com/Understand/Understand-#{version}-MacOSX-x86.dmg"
   name 'SciTools Understand'
