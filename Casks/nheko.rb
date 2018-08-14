55ac4074c3d7696fc4546f16e20ffbf3b884a34b
Brian Morton <brianmortondesign@gmail.com>
2018-08-14 13:22:18 -0700

    Update nheko to 0.5.3 (#50761)

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -1,6 +1,6 @@
 cask 'nheko' do
-  version '0.5.2'
-  sha256 '76eb8b542a224cfa989c995a08654d8e993324b770024f912d8983a2d3520c26'
+  version '0.5.3'
+  sha256 '814423164cbe9d765c84e2c9ec80cb4929960f0e8880c05b4ab54ff502cf1a5d'
 
   # bintray.com/mujx/matrix was verified as official when first introduced to the cask
   url "https://bintray.com/mujx/matrix/download_file?file_path=nheko%2Fv#{version}%2Fnheko-v#{version}.dmg"
