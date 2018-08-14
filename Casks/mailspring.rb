df102b6fd55321c22cf96fa3dd00e7ba8238aba3
Albert Zhang <albertzhang66@gmail.com>
2018-08-13 23:32:25 -0500

    Update mailspring to 1.4.1

--- Casks/mailspring.rb
+++ Casks/mailspring.rb
@@ -1,6 +1,6 @@
 cask 'mailspring' do
-  version '1.3.0'
-  sha256 '24f2c157cb7cb4b44638044529111cd0b275629873e454d4b9caa42354eef7d8'
+  version '1.4.1'
+  sha256 '011c2372e64fcc3258865f911a06382fd23b72baec9447e49f7615671d790798'
 
   # github.com/Foundry376/Mailspring was verified as official when first introduced to the cask
   url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring.zip"
