ed7d9345c00846cac7c5d4fd64cc5ce50f66b215
Dustin <tools_2017@crypticvapor.com>
2018-08-14 22:26:18 -0600

    Update gtkwave to 3.3.93

--- Casks/gtkwave.rb
+++ Casks/gtkwave.rb
@@ -1,6 +1,6 @@
 cask 'gtkwave' do
-  version '3.3.92'
-  sha256 '78989e3be530d97a3404fccb0bb243ad602962e3b59753b9fa19cbb8a7325d54'
+  version '3.3.93'
+  sha256 'd95957803ee64659755d87cb0d49b8e9887e799814575c6f2af8b7b86e17a649'
 
   url "https://downloads.sourceforge.net/gtkwave/gtkwave-#{version}-osx-app/gtkwave.zip"
   appcast 'https://sourceforge.net/projects/gtkwave/rss'
