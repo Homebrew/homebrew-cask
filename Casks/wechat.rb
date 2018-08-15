99e1810cd5a1718eb32ef16859d573e861921a4c
Nevan Chow <iamzhouu@gmail.com>
2018-08-15 17:43:26 +0800

    Update wechat to 2.3.18.17 (#50825)

--- Casks/wechat.rb
+++ Casks/wechat.rb
@@ -1,6 +1,6 @@
 cask 'wechat' do
-  version '2.3.17.18'
-  sha256 '25c9d8aa5a2f62b08484940f43ca6a12a72ffdcea71c6459a217f25f3fef3c6c'
+  version '2.3.18.17'
+  sha256 '4206bb17ff07033488126f3eb3af82d2b55f5b43c8862459bfe9043c9e737d5f'
 
   url "https://dldir1.qq.com/weixin/mac/WeChat_#{version}.dmg"
   name 'WeChat for Mac'
