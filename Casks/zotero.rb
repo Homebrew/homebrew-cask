8b816dac6156031d00becaa1ecec0a837a2cedfb
Brian Morton <brianmortondesign@gmail.com>
2018-08-16 09:25:17 -0700

    Update zotero to 5.0.55

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask 'zotero' do
-  version '5.0.54'
-  sha256 'e3d2ec5d8467da8089cce470cdcf988d0414417c26773dd450181502037e4f66'
+  version '5.0.55'
+  sha256 '918944616f4cb1a39dc038eabd8c6677bded4e291568dbd03bedecc75c8982c4'
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast 'https://github.com/zotero/zotero/releases.atom'
