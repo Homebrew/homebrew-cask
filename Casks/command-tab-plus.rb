513a41374d587f7e72e08e0cdf2f8fb9e7ba8479
Brian Morton <brianmortondesign@gmail.com>
2018-08-16 09:14:37 -0700

    Update command-tab-plus to 1.65,291:1534352094

--- Casks/command-tab-plus.rb
+++ Casks/command-tab-plus.rb
@@ -1,6 +1,6 @@
 cask 'command-tab-plus' do
-  version '1.64,287:1533745486'
-  sha256 'd10d4a9074d3dafacce05b4138145daa30619268d672ed8a150dca20aa03a809'
+  version '1.65,291:1534352094'
+  sha256 'dc776d53420bde367a22e1df804718d2c6fbd48aadd72ed07ea974e9a711ca7c'
 
   # dl.devmate.com/com.sergey-gerasimenko.Command-Tab was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.sergey-gerasimenko.Command-Tab/#{version.after_comma.before_colon}/#{version.after_colon}/Command-Tab-#{version.after_comma.before_colon}.zip"
