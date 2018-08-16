7eeecbdaa3b89d8b34b9b24f8f2179da44b1bec0
Brian Morton <brianmortondesign@gmail.com>
2018-08-16 09:18:06 -0700

    Update melodics to 2.0.2072

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask 'melodics' do
-  version '2.0.2056'
-  sha256 '379f08b4b751a2b361d955cd3b76358cc98f7233b0cfcbb66ead42c22278a338'
+  version '2.0.2072'
+  sha256 'e81656ed36f8fe714606065ed2c698114059b507d8bfa8fbadb40f059c5559d1'
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://api.melodics.com/download/osxupdatescastv#{version.major}.xml"
