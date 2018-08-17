491ba34db7ba4e6beb26921e5d1d9e95c7044887
Pete Alexandrou <pete@ozmartians.com>
2018-08-18 00:43:32 +1000

    update vidcutter to 6.0.1 (#50934)
    * update 6.0.0 sha256sum
    
    * update to VidCutter 6.0.1
    
    * fix url
    
    * Update vidcutter.rb

--- Casks/vidcutter.rb
+++ Casks/vidcutter.rb
@@ -1,9 +1,9 @@
 cask 'vidcutter' do
-  version '6.0.0'
-  sha256 '33e18085470fc241373fb4f1b9cb8d2caebba2f3a1a4f5bb57da339d94d198e6'
+  version '6.0.1'
+  sha256 '33943d56de307b026554824bc9b06779428bd75e5197779897ee2b4952313961'
 
   # github.com/ozmartian/vidcutter was verified as official when first introduced to the cask
-  url "https://github.com/ozmartian/vidcutter/releases/download/#{version}/VidCutter-#{version}-macOS.dmg"
+  url "https://github.com/ozmartian/vidcutter/releases/download/#{version.major_minor}.0/VidCutter-#{version}-macOS.dmg"
   appcast 'https://github.com/ozmartian/vidcutter/releases.atom'
   name 'VidCutter'
   homepage 'https://vidcutter.ozmartians.com/'
