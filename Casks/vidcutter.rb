ea934ae283f4f33a3a3a3f14bf041d792032c1b4
Pete Alexandrou <pete@ozmartians.com>
2018-08-17 15:57:14 +1000

    update 6.0.0 sha256sum

--- Casks/vidcutter.rb
+++ Casks/vidcutter.rb
@@ -1,6 +1,6 @@
 cask 'vidcutter' do
   version '6.0.0'
-  sha256 '33e18085470fc241373fb4f1b9cb8d2caebba2f3a1a4f5bb57da339d94d198e6'
+  sha256 '33943d56de307b026554824bc9b06779428bd75e5197779897ee2b4952313961'
 
   # github.com/ozmartian/vidcutter was verified as official when first introduced to the cask
   url "https://github.com/ozmartian/vidcutter/releases/download/#{version}/VidCutter-#{version}-macOS.dmg"
