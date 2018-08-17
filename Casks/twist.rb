059304a0630bf05c1e60d4473627467b4a02541b
Brian Morton <brianmortondesign@gmail.com>
2018-08-17 09:45:53 -0700

    Update twist to 1.5.0,4773 (#50957)

--- Casks/twist.rb
+++ Casks/twist.rb
@@ -1,6 +1,6 @@
 cask 'twist' do
-  version '1.5.0,4767'
-  sha256 '3829a02234f9be43aff2f1cc92f52f2479b2900f02ce4088eae42e4551d614aa'
+  version '1.5.0,4773'
+  sha256 '05b69a82f7ed34e5e3dad61797017523c59c1412962b5865fa82fa892069b671'
 
   url "https://downloads.twistapp.com/mac/Twist-#{version.after_comma}.zip"
   appcast 'https://downloads.twistapp.com/mac/AppCast.xml'
