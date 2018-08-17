68ce424c24be8599fa1552cc3f18c28bd0f5432d
Brian Morton <brianmortondesign@gmail.com>
2018-08-17 09:21:54 -0700

    Update exodus to 1.58.1

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,6 +1,6 @@
 cask 'exodus' do
-  version '1.57.0'
-  sha256 'c819350938f9f7348662942e49c8367aae7bd0568ff7a889b0fd64bfc18ab1c3'
+  version '1.58.1'
+  sha256 '53cb8fc95cf80b8f202375a065295195ed67bcb02b2652f7b55b8ad7bc0b0d50'
 
   # exodusbin.azureedge.net was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-macos-#{version}.dmg"
