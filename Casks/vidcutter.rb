7f64ea8d5e0382f2c2d8266ad4f1941c875dae51
Pete Alexandrou <pete@ozmartians.com>
2018-08-17 16:44:45 +1000

    fix url

--- Casks/vidcutter.rb
+++ Casks/vidcutter.rb
@@ -3,7 +3,7 @@ cask 'vidcutter' do
   sha256 '33943d56de307b026554824bc9b06779428bd75e5197779897ee2b4952313961'
 
   # github.com/ozmartian/vidcutter was verified as official when first introduced to the cask
-  url "https://github.com/ozmartian/vidcutter/releases/download/#{version}/VidCutter-#{version}-macOS.dmg"
+  url "https://github.com/ozmartian/vidcutter/releases/download/6.0.0/VidCutter-#{version}-macOS.dmg"
   appcast 'https://github.com/ozmartian/vidcutter/releases.atom'
   name 'VidCutter'
   homepage 'https://vidcutter.ozmartians.com/'
