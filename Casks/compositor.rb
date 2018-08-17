cdfe87ab51297dcdbd9654cdb653043b4292eb14
Brian Morton <brianmortondesign@gmail.com>
2018-08-17 04:10:33 -0700

    Update compositor to 1.4.1 (#50918)

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,6 +1,6 @@
 cask 'compositor' do
-  version '1.4.0'
-  sha256 'db509407c9d52ac6df7c40ceb721780c58e1fd533e7a1003f5c53a897d9e9e4d'
+  version '1.4.1'
+  sha256 '7a8cceb474307773c7f51853d7c802aba69840395d2e09281922e0b4fe8b9b6f'
 
   url "http://compositorapp.com/updates/Compositor_#{version}.zip"
   appcast 'http://compositorapp.com/updates/appcast.xml'
