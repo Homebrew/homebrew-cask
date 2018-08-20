30cdc5e7dd193fd1d4ce97befe1ed0689c5b9765
Brian Morton <brianmortondesign@gmail.com>
2018-08-20 14:17:42 -0700

    Update netspot to 2.9.919 (#51039)

--- Casks/netspot.rb
+++ Casks/netspot.rb
@@ -1,6 +1,6 @@
 cask 'netspot' do
-  version '2.8.830'
-  sha256 '1888b8c359eec6ea9e5f851b742f7d4a3a98b81a78bbe0a5601b24647065fabf'
+  version '2.9.919'
+  sha256 '93e44f01e5f4cff1498ce289605319f26ab56cd9f34774ec9657c99619a6dc0e'
 
   url 'https://www.netspotapp.com/download/NetSpot.dmg'
   appcast "https://www.netspotapp.com/updates/netspot#{version.major}-appcast.xml"
