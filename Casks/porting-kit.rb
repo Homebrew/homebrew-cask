67cdf6671a7f588a32320a418a9d2cd59d3699ea
Brian Morton <brianmortondesign@gmail.com>
2018-08-20 14:17:49 -0700

    Update porting-kit to 2.9.501 (#51040)

--- Casks/porting-kit.rb
+++ Casks/porting-kit.rb
@@ -1,6 +1,6 @@
 cask 'porting-kit' do
-  version '2.9.495'
-  sha256 '04388919a3352e7b44e2d5cf09b97d8526e418cc1f7358118d9d0253d600c4d1'
+  version '2.9.501'
+  sha256 'b8e968d2e3487b1dafac28907f5a078be4f8ca01749bd96312d5ce02b2316fdb'
 
   url "http://portingkit.com/kit/Porting%20Kit%20#{version}.zip"
   appcast 'http://portingkit.com/kit/updatecast.xml'
