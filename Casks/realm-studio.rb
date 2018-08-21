c882a4f7c6ad7de3edf5a6f71cc0f29652ec26cb
Brian Morton <brianmortondesign@gmail.com>
2018-08-21 09:48:43 -0700

    Update realm-studio to 2.6.2

--- Casks/realm-studio.rb
+++ Casks/realm-studio.rb
@@ -1,6 +1,6 @@
 cask 'realm-studio' do
-  version '2.6.1'
-  sha256 'df7900fd19a17030048f5a04b62a096876ab0954f6b96b53592f624ea9d3bede'
+  version '2.6.2'
+  sha256 '74429f1f29b49693eebff9b0cd0f458eec5ce6130292af5e7a3cc456a1d52ffd'
 
   url "https://static.realm.io/downloads/realm-studio/Realm%20Studio-#{version}-mac.zip"
   appcast 'https://static.realm.io/downloads/realm-studio/latest-mac.yml'
