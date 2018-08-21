15ce118574904f601685d4dacee941162bbbc048
Stephen Wade <stephen@stephenwade.me>
2018-08-20 21:54:07 -0400

    Update mailplane to latest

--- Casks/mailplane.rb
+++ Casks/mailplane.rb
@@ -2,11 +2,11 @@ cask 'mailplane' do
   version :latest
   sha256 :no_check
 
-  url 'http://update.mailplaneapp.com/mailplane_3.php'
+  url 'https://update.mailplaneapp.com/mailplane_4.php'
   name 'Mailplane'
   homepage 'https://mailplaneapp.com/'
 
-  app 'Mailplane 3.app'
+  app 'Mailplane.app'
 
   zap trash: '~/Library/Preferences/com.mailplaneapp.Mailplane.plist'
 end
