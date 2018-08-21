9c074b1b06462ea5a62b89bc07fc4439cd1377ac
Brian Morton <brianmortondesign@gmail.com>
2018-08-21 09:36:51 -0700

    Update fastscripts to 2.7.2

--- Casks/fastscripts.rb
+++ Casks/fastscripts.rb
@@ -1,6 +1,6 @@
 cask 'fastscripts' do
-  version '2.7.1'
-  sha256 'f23ccfdce983bd652cb23d3eebb40ac5d03a55de537bafb87427471409bf94ac'
+  version '2.7.2'
+  sha256 '1e3565a2e878cfec205a8ad3318b60692466d1dba1d41965979fc2d1e27dcd22'
 
   url "https://www.red-sweater.com/fastscripts/FastScripts#{version}.zip"
   appcast 'https://red-sweater.com/fastscripts/appcast2.php'
