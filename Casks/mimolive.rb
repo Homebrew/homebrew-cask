61dec5d3b5dc53f970ab8c45b801282150b744d3
Brian Morton <brianmortondesign@gmail.com>
2018-08-21 09:46:27 -0700

    Update mimolive to 4.3-26305

--- Casks/mimolive.rb
+++ Casks/mimolive.rb
@@ -1,6 +1,6 @@
 cask 'mimolive' do
-  version '4.2.1-25918'
-  sha256 '60e28286ac660991323c988fcb256103bb31765c42fdcf279640437038505dc4'
+  version '4.3-26305'
+  sha256 'c1fb8ee47e4b8382c7a31fa490673c61fe3616b0bf5cfb8ac60fbfa41983f94b'
 
   url "https://cdn.boinx.com/software/mimolive/Boinx_mimoLive_#{version}.app.zip"
   appcast 'https://boinx.com/d/connect/histories/mimolive'
