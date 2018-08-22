05369c275acca3e05f042de67e51eb7d9fd636fe
Brian Morton <brianmortondesign@gmail.com>
2018-08-22 10:41:36 -0700

    Update sqlworkbenchj to 124 (#51092)

--- Casks/sqlworkbenchj.rb
+++ Casks/sqlworkbenchj.rb
@@ -1,6 +1,6 @@
 cask 'sqlworkbenchj' do
-  version '123'
-  sha256 'd85c8741f465aa9f8991a8aa5b54d9fce0c7b366c20b25b890776cb217ea838c'
+  version '124'
+  sha256 '2173c7f00172bef3fed23e7f57e168a9d156c72c311af032ba469139f80d9fe9'
 
   url "http://www.sql-workbench.net/Workbench-Build#{version}-Mac.tgz"
   appcast 'http://www.sql-workbench.net/wb_news.xml'
