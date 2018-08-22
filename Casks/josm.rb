c24983d84e6f9979fa23734b4e62ae83e22ff168
danbjoseph <danbjoseph@gmail.com>
2018-08-22 16:03:30 -0400

    Update josm to 14178

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask 'josm' do
-  version '14163'
-  sha256 '9d51b579b0e89991515ace5e6bc5e13b050a95662eae3ea13c3dfe1cf5775da7'
+  version '14178'
+  sha256 '4ed4b1f7757cf06a5147f5ebc0097f013c569fb41f354b192874e95580d537df'
 
   url "https://josm.openstreetmap.de/download/macosx/josm-macosx-#{version}.zip"
   name 'JOSM'
