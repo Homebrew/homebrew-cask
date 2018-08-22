1f1675f8028b25a23ef60479d89bbf7676c06d68
greenrock <dd331@163.com>
2018-08-23 01:45:02 +0800

    Update visual-studio to 7.6.0.2190 (#51062)
    * Update visual-studio to 7.6.0.2190
    
    * Update visual-studio.rb

--- Casks/visual-studio.rb
+++ Casks/visual-studio.rb
@@ -1,10 +1,10 @@
 cask 'visual-studio' do
-  version '7.5.4.3'
-  sha256 'c07ee3a414ded9b780f31ce70a4fb16a1e244965aef23786c7d687bf7e9a041d'
+  version '7.6.0.2190'
+  sha256 '8cf327900959af9f14624774e628e1dd8faa182fab35ff6a5d7114a5edef3d34'
 
   # dl.xamarin.com/VsMac was verified as official when first introduced to the cask
   url "https://dl.xamarin.com/VsMac/VisualStudioForMac-#{version}.dmg"
-  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/vsmac/Mac/Universal/InstallationManifest.xml'
+  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
   name 'Visual Studio for Mac'
   homepage 'https://www.visualstudio.com/vs/visual-studio-mac/'
 
