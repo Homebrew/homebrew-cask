ed60dd452ff6b3d80f642e687bfcfb5964383050
Vítor Galvão <info@vitorgalvao.com>
2018-08-23 00:11:08 +0100

    Update bforartists.rb

--- Casks/bforartists.rb
+++ Casks/bforartists.rb
@@ -2,7 +2,7 @@ cask 'bforartists' do
   version '1.0.0'
   sha256 '7720c815adaef5f947c5f61e62c2bcade98ed7661cd30cee69c648e77e88d543'
 
-  url 'http://www.bforartists.de/data/binaries/BforArtists_100.dmg'
+  url "http://www.bforartists.de/data/binaries/BforArtists_#{version.no_dots}.dmg"
   name 'Blender for Artists'
   name 'BforArtists'
   homepage 'http://www.bforartists.de/'
