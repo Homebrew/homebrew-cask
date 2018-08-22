a343ba031a28555fd84e95bd1150f5511783907c
Adam Petrovic <adam@petrovic.com.au>
2018-08-23 03:39:58 +1000

    Update blue-jeans to 2.7.0.450 (#51099)

--- Casks/blue-jeans.rb
+++ Casks/blue-jeans.rb
@@ -1,6 +1,6 @@
 cask 'blue-jeans' do
-  version '2.5.1.33'
-  sha256 '20914226694e4a0734d85f8de9a42bc81e7166af933016de66e0a82dd5383e89'
+  version '2.7.0.450'
+  sha256 '19fbd138f41973e05b19b52da7531afc8a3895c4984e175470dd5c47cf886071'
 
   url "https://swdl.bluejeans.com/desktop-app/mac/#{version.major_minor_patch}/#{version}/BlueJeansInstaller.dmg"
   name 'Blue Jeans videoconferencing'
