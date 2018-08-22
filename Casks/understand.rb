9114a0680a3517c5df36011fd58a1947f028d177
Qi Zhang <singularitti@outlook.com>
2018-08-22 00:54:38 -0400

    Update understand to 5.0.955

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask 'understand' do
-  version '5.0.954'
-  sha256 'b422190bcc6c7c4a09ba9f4a232d314bd017ca046aeb3e90eee093af208e1f26'
+  version '5.0.955'
+  sha256 'ecc2d19595ed0293f69a98c555aa5a0f24036702836477efbe888b0b63468f55'
 
   url "http://latest.scitools.com/Understand/Understand-#{version}-MacOSX-x86.dmg"
   name 'SciTools Understand'
