90f40cb1aea39afe06af6ff69492d3c4e88254f8
Hanstad <20923110+Hanstad@users.noreply.github.com>
2018-08-22 08:04:55 +0200

    Updated xamarin-mac to 4.4.1.193

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,9 +1,9 @@
 cask 'xamarin-mac' do
-  version '4.2.1.29'
-  sha256 'bc834bc7e07b8b689d57c34b374cfe5105549a5f49712a9681cbcc5ebd46b330'
+  version '4.4.1.193'
+  sha256 '9b004c50196a96b881aa36ef9edc0f8a755baab2ed64fc7275b087ba96e126c5'
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
-  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml'
+  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
   name 'Xamarin Mac'
   homepage 'https://www.xamarin.com/platform'
 
