dd9c9d1afdf71ab760f50ca706733658189ccb0e
alswl <alswlx@gmail.com>
2018-08-23 01:39:51 +0800

    feat: update fox to 1.7.8 (#51100)

--- Casks/fox.rb
+++ Casks/fox.rb
@@ -1,6 +1,6 @@
 cask 'fox' do
-  version '1.7.6'
-  sha256 'ca18958ff70cef3561de736fe2eb7384eb11c03da0c7785bce07d991f6910f41'
+  version '1.7.8'
+  sha256 '51da8d1b625cc6ae17c715599e52269751044330508ac084931d6a2bc2eccb43'
 
   url "https://www.fox.one/build/Fox-#{version}.dmg"
   name 'Fox'
