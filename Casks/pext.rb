d4c8274c631b132ed7a6f160c878a977c48cc631
Brian Morton <brianmortondesign@gmail.com>
2018-08-22 10:47:22 -0700

    Update pext to 0.18 (#51139)

--- Casks/pext.rb
+++ Casks/pext.rb
@@ -1,6 +1,6 @@
 cask 'pext' do
-  version '0.17'
-  sha256 'f8795983a12f1fe1ad159c6bfd5f8ded92a8a8312ffb149fd56dc8f63a578abd'
+  version '0.18'
+  sha256 '91d3721ca3d041f9e4a6dcb6d5ef826a237b49866741ac43edc413c9d7b8f946'
 
   # github.com/Pext/Pext was verified as official when first introduced to the cask
   url "https://github.com/Pext/Pext/releases/download/v#{version}/Pext.dmg"
