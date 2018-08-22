646dc78c0662ae7f6f98c3689202f034d0efc235
strafe <strafe@users.noreply.github.com>
2018-08-22 18:49:59 +0100

    Add Ripcord v0.3.2 (#51006)
    * Add Ripcord v0.3.2
    
    * Remove unrelated cookie file from zap stanza

---
+++ Casks/ripcord.rb
@@ -0,0 +1,20 @@
+cask 'ripcord' do
+  version '0.3.2'
+  sha256 '410638dbad101d64fd33848db211828a6151626f9da18c3ce138721243493e5a'
+
+  url "https://cancel.fm/dl/Ripcord_Mac_#{version}.zip"
+  appcast 'https://cancel.fm/ripcord/updates/v1'
+  name 'Ripcord'
+  homepage 'https://cancel.fm/ripcord/'
+
+  auto_updates true
+  depends_on macos: '>= :sierra'
+
+  app 'Ripcord.app'
+
+  zap trash: [
+               '~/Library/Application Support/Ripcord',
+               '~/Library/Preferences/com.cancelfm.Ripcord.plist',
+               '~/Library/Saved Application State/com.cancelfm.Ripcord.savedState',
+             ]
+end
