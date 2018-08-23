8e34aee8297bd1495b865e1396e1a1a32199581f
Brian Morton <brianmortondesign@gmail.com>
2018-08-23 09:41:14 -0700

    Update bitshares to 2.0.180815 (#51190)

--- Casks/bitshares.rb
+++ Casks/bitshares.rb
@@ -1,6 +1,6 @@
 cask 'bitshares' do
-  version '2.0.180814'
-  sha256 '70d9451f0f3b44576205710f37e20143b7e91be09c00af43d33bf7c96b895717'
+  version '2.0.180815'
+  sha256 'a05731fd1e89a6ec76f28c66b1b0cc3ad53a2c7947588592d6e4319f2a16abd9'
 
   # github.com/bitshares/bitshares-ui was verified as official when first introduced to the cask
   url "https://github.com/bitshares/bitshares-ui/releases/download/#{version}/BitShares-#{version}.dmg"
