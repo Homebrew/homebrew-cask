4c5d2011f4e67040afacd69212883dd1290e4155
Niklas Gustavsson <niklas@protocol7.com>
2018-08-23 21:49:21 +0200

    Fix download url for Pomello latest

--- Casks/pomello.rb
+++ Casks/pomello.rb
@@ -2,7 +2,7 @@ cask 'pomello' do
   version :latest
   sha256 :no_check
 
-  url 'https://pomelloapp.com/download/mac'
+  url 'https://pomelloapp.com/download/mac/latest'
   name 'Pomello'
   homepage 'https://pomelloapp.com/'
 
