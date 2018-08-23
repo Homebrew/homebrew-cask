ece2702f86f694b0b7ed22252455628173f12060
Brian Morton <brianmortondesign@gmail.com>
2018-08-23 09:45:02 -0700

    Update xamarin-mac to 4.6.0.13

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-mac' do
-  version '4.4.1.193'
-  sha256 '9b004c50196a96b881aa36ef9edc0f8a755baab2ed64fc7275b087ba96e126c5'
+  version '4.6.0.13'
+  sha256 'aa604f131c325c0a8146a1438e27874417e0546641dc071ad6c72ad6c7638d2f'
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
