1a56da806c9b6bfc01df585d4178d59af97c7f72
Brian Morton <brianmortondesign@gmail.com>
2018-08-23 09:53:54 -0700

    Update xamarin-android to 9.0.0-18 (#51207)

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-android' do
-  version '8.3.3-2'
-  sha256 '2894541caad0a4847b8a0ef5e634bcb7888588db044b7774a0bf15f4ce761025'
+  version '9.0.0-18'
+  sha256 '9c8a13c1edfd7cf3684136e6ae8bf172c1d2bbfa4b6233a265b1bbc164e95d3e'
 
   url "https://dl.xamarin.com/MonoforAndroid/Mac/xamarin.android-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
