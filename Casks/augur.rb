a9fb84d026473f9315fb918d8b509fe8443c0d56
Brian Morton <brianmortondesign@gmail.com>
2018-08-23 09:40:29 -0700

    Update augur to 1.4.0 (#51186)

--- Casks/augur.rb
+++ Casks/augur.rb
@@ -1,6 +1,6 @@
 cask 'augur' do
-  version '1.3.2'
-  sha256 'ce9f1b77f65fbd77977ca06f39cb30ca484e1a7fbacb98a637249ead17657b21'
+  version '1.4.0'
+  sha256 '0da999d014547f1d7d9715939f42a4371c5bf13cb6f834c5205cc58a8307947f'
 
   url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-augur-#{version}.dmg"
   appcast 'https://github.com/AugurProject/augur-app/releases.atom'
