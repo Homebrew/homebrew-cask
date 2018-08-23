0c6e5a67b1f7a21b51e2f5945f6ca8f3ab56835a
Brian Morton <brianmortondesign@gmail.com>
2018-08-23 09:36:39 -0700

    Update tapaal to 3.4.3

--- Casks/tapaal.rb
+++ Casks/tapaal.rb
@@ -3,7 +3,7 @@ cask 'tapaal' do
   sha256 '1eac6c8baa1f9def8c5723036e635d62c04f3ccdcff483b38955e1b8bc08d459'
 
   url "http://www.tapaal.net/fileadmin/download/tapaal-#{version.major_minor}/tapaal-#{version}-mac64.dmg"
-  appcast 'http://www.tapaal.net/'
+  appcast 'http://www.tapaal.net/download/'
   name 'TAPAAL'
   homepage 'http://www.tapaal.net/'
 
