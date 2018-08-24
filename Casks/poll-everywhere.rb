72d7fc46f26777ee3dce1664d73e30efe61eb02d
Brian Morton <brianmortondesign@gmail.com>
2018-08-24 10:51:15 -0700

    Update poll-everywhere to 2.17.0 (#51258)

--- Casks/poll-everywhere.rb
+++ Casks/poll-everywhere.rb
@@ -1,6 +1,6 @@
 cask 'poll-everywhere' do
-  version '2.16.0'
-  sha256 'b6142eb60f3671c359545f16670d17c3cc5ed309d6eba91e68ec4a18190ed3b8'
+  version '2.17.0'
+  sha256 'fddfe9efe8c9df77529234095ec815e4ea3db8b784460ecd6fc439724bf1b39d'
 
   # amazonaws.com/polleverywhere-app was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/polleverywhere-app/mac-stable/#{version}/pollev.dmg"
