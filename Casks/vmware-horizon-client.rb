490d29a5a43ada465c6b572f5ad45e267d653a31
David Yantis <404NetworkError@users.noreply.github.com>
2018-08-24 14:15:56 -0500

    Update vmware-horizon-client to 4.8.0-8537035,CART19FQ2 (#51268)

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -1,6 +1,6 @@
 cask 'vmware-horizon-client' do
-  version '4.7.0-7395166,CART18FQ4'
-  sha256 '5907cf801cc2fedfc20bcdb65b9fe9b9d69f9dd9e39e4d7a5a1127f3b8bdfcf9'
+  version '4.8.0-8537035,CART19FQ2'
+  sha256 '3a050ec744545f34cdcd9f3c9ffaf927d2af087e82755f7bb1c60a06e970db41'
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
   name 'VMware Horizon Client'
