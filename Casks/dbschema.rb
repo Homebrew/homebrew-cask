dd9e9f6a126fff503d1c614eb700190106a22be5
Matt McElheny <mmcelheny@covermymeds.com>
2018-08-24 15:46:03 -0400

    Update dbschema to 8.0.6

--- Casks/dbschema.rb
+++ Casks/dbschema.rb
@@ -1,6 +1,6 @@
 cask 'dbschema' do
-  version '7.6.6'
-  sha256 '1371ed695c111a387a5fab90c8610b8a32382fe705f459968933205d1c8638ce'
+  version '8.0.6'
+  sha256 '9cd6c6c484fe8e1c459bd3dba0a32362bc626bb37223f5b94153a043d24bc84b'
 
   url "https://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
   name 'DbSchema'
