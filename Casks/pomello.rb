2b8a150f8cdfb39494234476f684040391aeb37d
Vítor Galvão <info@vitorgalvao.com>
2018-08-25 21:01:38 +0100

    Update pomello to 0.9.4 (#51221)

--- Casks/pomello.rb
+++ Casks/pomello.rb
@@ -1,8 +1,9 @@
 cask 'pomello' do
-  version :latest
-  sha256 :no_check
+  version '0.9.4'
+  sha256 'a281a370b959dc80fe47c6243e2164129adbc25fe3ea006c3cfccbbdedb43a40'
 
   url 'https://pomelloapp.com/download/mac/latest'
+  appcast 'https://pomelloapp.com/download/mac'
   name 'Pomello'
   homepage 'https://pomelloapp.com/'
 
