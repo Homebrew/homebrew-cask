5fd175f9432b420403e63f12acdd65aa86ef8d2c
Vítor Galvão <info@vitorgalvao.com>
2018-08-25 20:18:31 +0100

    Update penc.rb

--- Casks/penc.rb
+++ Casks/penc.rb
@@ -2,7 +2,7 @@ cask 'penc' do
   version '0.3.3'
   sha256 '800a9a07e4c998b631b3da776add8b4bb93bcac64974190d328d0fdcb81ecd7f'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/dgurkaynak/Penc was verified as official when first introduced to the cask
   url "https://github.com/dgurkaynak/Penc/releases/download/#{version}/Penc-#{version}.dmg"
   appcast 'https://github.com/dgurkaynak/Penc/releases.atom'
   name 'Penc'
