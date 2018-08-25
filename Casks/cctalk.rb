98457e8a26b9088322011c83da0122cb4911d51b
Vítor Galvão <info@vitorgalvao.com>
2018-08-25 22:35:46 +0100

    Update cctalk.rb

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -2,7 +2,6 @@ cask 'cctalk' do
   version '7.3.2,707'
   sha256 '6eb2d1dd2e7c245e5b005554aaae0ca70ca8e5a8c9969005eb682f4cebcbe43e'
 
-  # n1other.hjfile.cn was verified as official when first introduced to the cask
   url 'https://www.cctalk.com/webapi/basic/v1.1/version/down?apptype=1&terminalType=8&versionType=103',
       user_agent: :fake
   appcast 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/update/info.xml'
