b7b0ba7d133853933c320c19539df2b9e56ebb7e
Vítor Galvão <info@vitorgalvao.com>
2018-08-26 10:30:28 +0100

    wechatwebdevtools.rb: no need for comma in version (#51182)

--- Casks/wechatwebdevtools.rb
+++ Casks/wechatwebdevtools.rb
@@ -1,8 +1,8 @@
 cask 'wechatwebdevtools' do
-  version '1.02.1808101,1808101'
+  version '1.02.1808101'
   sha256 'dc9a8b75b3bba08a24a41e1cf56ff03358fde837b14a1b1ad95e028c02b3c33b'
 
-  url "https://dldir1.qq.com/WechatWebDev/#{version.major}.0.0/20#{version.after_comma}/wechat_devtools_#{version.before_comma}.dmg"
+  url "https://dldir1.qq.com/WechatWebDev/#{version.major}.0.0/20#{version.patch}/wechat_devtools_#{version}.dmg"
   name 'wechat web devtools'
   name '微信web开发者工具'
   homepage 'https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/download.html'
