992a9ad21756d91ec3a81db118ab4e764a4f400f
Brian Morton <brianmortondesign@gmail.com>
2018-08-27 14:58:03 -0700

    Update graphql-playground to 1.6.3 (#51334)
    * Update graphql-playground to 1.6.3
    
    * Update graphql-playground.rb

--- Casks/graphql-playground.rb
+++ Casks/graphql-playground.rb
@@ -1,11 +1,11 @@
 cask 'graphql-playground' do
-  version '1.6.2'
-  sha256 'a26effde7d740f35e1d5aaa2ec937a1989f6ae5fd9c86361210d5e333b4cc234'
+  version '1.6.3'
+  sha256 '6852aadc02bd1ae208953ddc9947164b4c3037029b9b00a635198ff7df99e8aa'
 
-  url "https://github.com/prismagraphql/graphql-playground/releases/download/#{version}/graphql-playground-electron-#{version}.dmg"
-  appcast 'https://github.com/prismagraphql/graphql-playground/releases.atom'
+  url "https://github.com/prisma/graphql-playground/releases/download/v#{version}/graphql-playground-electron-#{version}.dmg"
+  appcast 'https://github.com/prisma/graphql-playground/releases.atom'
   name 'GraphQL Playground'
-  homepage 'https://github.com/prismagraphql/graphql-playground'
+  homepage 'https://github.com/prisma/graphql-playground'
 
   app 'GraphQL Playground.app'
 end
