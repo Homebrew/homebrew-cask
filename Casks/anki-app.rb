3cfe67ff6f85d15784f4cad4886e792dd5c9d7c9
David Nicolson <david.nicolson@gmail.com>
2018-08-27 20:50:02 +0200

    Add AnkiApp latest

---
+++ Casks/anki-app.rb
@@ -0,0 +1,14 @@
+cask 'anki-app' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.ankiapp.com/AnkiApp-MacOS.zip'
+  name 'AnkiApp'
+  homepage 'https://www.ankiapp.com/'
+
+  app 'AnkiApp.app'
+
+  zap trash: ['~/Library/Application Support/AnkiApp',
+              '~/Library/Preferences/com.electron.ankiapp.plist',
+              '~/Library/Preferences/com.electron.ankiapp.helper.plist']
+end
