6b5f2628a65b2a7b4c642cd3a4c2b875a6826a99
Michael Dahl <micdah@gmail.com>
2018-08-27 16:27:47 +0200

    Add Syncovery.app 8.01

---
+++ Casks/syncovery.rb
@@ -0,0 +1,12 @@
+cask 'syncovery' do
+  version '8.01'
+  sha256 '3d040cf065375687b45bac2c1f382698c99ea88826adeaf1f5b8f40a5e85c473'
+
+  url 'https://www.syncovery.com/release/SyncoveryMac.dmg'
+  name 'Syncovery'
+  homepage 'https://www.syncovery.com/'
+
+  pkg 'SyncoveryMac (double-click to install).pkg'
+
+  uninstall pkgutil: 'com.superflexible.syncovery.*'
+end
