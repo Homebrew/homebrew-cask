485a36d8ae2ed8d5c68cb6ad2bc0a7b0f45bb5bb
CryoRenegade <jledbetter460@gmail.com>
2018-08-27 09:19:41 -0600

    Update jgrasp.rb to 2.0.5_01 (#51297)

--- Casks/jgrasp.rb
+++ Casks/jgrasp.rb
@@ -1,6 +1,6 @@
 cask 'jgrasp' do
-  version '2.0.4_04'
-  sha256 '5ece62ccdb6ee38b24e1727ea3674c059dbe512226eb60670773609536defa7b'
+  version '2.0.5_01'
+  sha256 'c515b5c3b763799d5c7a9572ca5224595c8461fef561dece6eac08674964accf'
 
   url "http://www.jgrasp.org/dl4g/jgrasp/jgrasp#{version.no_dots}.pkg"
   name 'jgrasp'
