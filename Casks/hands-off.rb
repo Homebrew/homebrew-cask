4b3449a87d476124cf9e6eb29d03815283f1ced2
Brian Morton <brianmortondesign@gmail.com>
2018-08-27 09:37:06 -0700

    Update hands-off to 3.2.10

--- Casks/hands-off.rb
+++ Casks/hands-off.rb
@@ -1,6 +1,6 @@
 cask 'hands-off' do
-  version '3.2.9'
-  sha256 '218bf7f5f68bcdaa3cf4a3974b42f36b70a8a57efc3c2bc6638c517ec2f3c845'
+  version '3.2.10'
+  sha256 'b270f765950790bd7bc15959cb010ebb4442629e51340514448226cc74e2e3b4'
 
   url "https://www.oneperiodic.com/files/Hands%20Off!%20v#{version}.dmg"
   appcast "https://www.oneperiodic.com/handsoff#{version.major}.xml"
