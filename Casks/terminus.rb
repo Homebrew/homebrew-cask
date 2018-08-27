5d0e81e2fc9fc0a6fbd5bdecfb146161b1f43d80
Brian Morton <brianmortondesign@gmail.com>
2018-08-27 09:50:27 -0700

    Update terminus to 1.0.0-alpha.54

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -1,6 +1,6 @@
 cask 'terminus' do
-  version '1.0.0-alpha.52'
-  sha256 'cb165bd2afc9d08287088cd679239e750338145e4b6bea50e2f7980e170a1ac3'
+  version '1.0.0-alpha.54'
+  sha256 'f4bfc2bdf44b3124fbfa87a45513891d0091cd12667f415dff5c386c0d74f7ad'
 
   # github.com/Eugeny/terminus was verified as official when first introduced to the cask
   url "https://github.com/Eugeny/terminus/releases/download/v#{version}/Terminus-#{version}-mac.dmg"
