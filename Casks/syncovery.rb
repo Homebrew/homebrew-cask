6a39fe994d51dcbddea8696bb6c2856440d02ab5
Michael Dahl <micdah@gmail.com>
2018-08-28 15:40:01 +0200

    Add Syncovery.app 8.01 (#51296)
    * Add Syncovery.app 8.01
    
    * Updated uninstall pkgutil\n
    Was using wrong package ID's from older v7.x of the product
    
    * Added zap of Syncovery related files
    
    * Fixed style issues
    
    * Update syncovery.rb
    
    * Update syncovery.rb
    
    * Switched to version :latest with sha56 :no_check

---
+++ Casks/syncovery.rb
@@ -0,0 +1,20 @@
+cask 'syncovery' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.syncovery.com/release/SyncoveryMac.dmg'
+  name 'Syncovery'
+  homepage 'https://www.syncovery.com/'
+
+  pkg 'SyncoveryMac (double-click to install).pkg'
+
+  uninstall pkgutil: 'com.company.Syncovery*'
+
+  zap trash: '~/Library/Preferences/Syncovery.ini',
+      rmdir: [
+               '~/Library/Application Support/Syncovery',
+               '~/Saved Application State/com.company.Syncovery.savedState',
+               '~/Library/Preferences/Syncovery',
+               '~/Library/Logs/Syncovery',
+             ]
+end
