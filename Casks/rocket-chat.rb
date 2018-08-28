2c816e54d554a6d2a3ccc53cd763ed4955ad2692
Philip Molares <git@molar.es>
2018-08-28 11:14:06 +0200

    updated rockt-chat to 2.13.0 (#51354)
    * updated rockt-chat to 2.13.0
    
    * rocket-chat 2.13.0 changed the app name. Also added additional folders and files to zap

--- Casks/rocket-chat.rb
+++ Casks/rocket-chat.rb
@@ -1,6 +1,6 @@
 cask 'rocket-chat' do
-  version '2.12.1'
-  sha256 'c2a0fcc67b042a4096d7e7c70adac021a7531fbb4626cc9c3c93e19546332fd5'
+  version '2.13.0'
+  sha256 'd612b6d25c392fe8469da104c4411d3cd11f4069ddfd8f283ecb6e7aed45040c'
 
   # github.com/RocketChat/Rocket.Chat.Electron was verified as official when first introduced to the cask
   url "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/#{version}/rocketchat-#{version}.dmg"
@@ -8,13 +8,15 @@ cask 'rocket-chat' do
   name 'Rocket.Chat'
   homepage 'https://rocket.chat/'
 
-  app 'Rocket.Chat+.app'
+  app 'Rocket.Chat.app'
 
   zap trash: [
-               '~/Library/Application Support/Rocket.Chat+',
-               '~/Library/Caches/Rocket.Chat+',
+               '~/Library/Application Support/Rocket.Chat',
                '~/Library/Caches/chat.rocket',
+               '~/Library/Caches/chat.rocket.ShipIt',
+               '~/Library/Caches/chat.rocket.electron.helper',
                '~/Library/Preferences/chat.rocket.plist',
+               '~/Library/Preferences/chat.rocket.electron.helper.plist',
                '~/Library/Saved Application State/chat.rocket.savedState',
              ]
 end
