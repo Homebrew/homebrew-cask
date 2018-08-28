024897835c04af4839fb0da1364e4f60e303606b
nickdelja <nickdelja@gmail.com>
2018-08-27 17:03:01 -0700

    Updating polymail version

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,5 +1,5 @@
 cask 'polymail' do
-  version '2.0.6'
+  version '2.0.9'
   sha256 'ee5de0dcfca926bcc9c6ef44d2d23e41f7d0fbc1c989a43e5d1c81dc0dc56b17'
 
   url "https://sparkle-updater.polymail.io/macos/builds/Polymail-v#{version}.zip"
