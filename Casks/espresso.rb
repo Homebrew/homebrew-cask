4842278982f7f5a5a045e24893c70c9bbfcdaaa2
Brian Morton <brianmortondesign@gmail.com>
2018-08-28 11:20:10 -0700

    Update espresso to 5.1.1 (#51365)

--- Casks/espresso.rb
+++ Casks/espresso.rb
@@ -2,8 +2,8 @@ cask 'espresso' do
   version '5.1.1'
   sha256 '728b5cb018479463bfb919533bfdcbd1788d1ec5b00ee312d3dc316d2e50527d'
 
-  url "https://espressoapp.com/presto/downloads/Espresso%20v#{version}.zip"
-  appcast 'http://update.macrabbit.com.s3-website-eu-west-1.amazonaws.com/espresso/v3-plus.xml'
+  url "https://espressoapp.com/updates/archives/Espresso-#{version}.zip"
+  appcast 'https://espressoapp.com/updates/'
   name 'Espresso'
   homepage 'https://espressoapp.com/'
 
