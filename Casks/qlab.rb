eb4b77c17dfd29e9986a96b78e8542e9eb97b8f7
Brian Morton <brianmortondesign@gmail.com>
2018-08-28 09:16:33 -0700

    Update qlab to 4.3

--- Casks/qlab.rb
+++ Casks/qlab.rb
@@ -1,6 +1,6 @@
 cask 'qlab' do
-  version '4.2.5'
-  sha256 'a9ff60893329997c00f35c3e15c4f339e4b510304f164dc01960a0528df65f72'
+  version '4.3'
+  sha256 '4e801498e63a9ad1137c3f3e549f0c1d450bd5f20ceaae9d597d8461628a0327'
 
   url "https://figure53.com/qlab/downloads/QLab-#{version}.zip"
   appcast "https://figure53.com/qlab/downloads/appcast-v#{version.major}/"
