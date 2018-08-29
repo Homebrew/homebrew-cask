ff7d3c2bc006732bd8da95e487ede54bbc2b5b61
commitay <commitay@users.noreply.github.com>
2018-08-30 08:05:00 +1000

    Update vagrant to 2.1.3 (#51434)

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,6 +1,6 @@
 cask 'vagrant' do
-  version '2.1.2'
-  sha256 'dd9cacd2d1fcf03e22343de9698f89c1547ab74cfb4a64b7deb8ed1b3e12bb16'
+  version '2.1.3'
+  sha256 '0b7deb138092b016dd64061292ce6585bad670e4fc85162651dc93919a2e6b63'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
