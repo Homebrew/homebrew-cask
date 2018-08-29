e2564de2e45ade2a62e215facba1941be885cb4d
apropostech <info@apropostech.com>
2018-08-29 13:46:40 +0200

    Update fastrawviewer to 1.4.9.1358 (#51396)

--- Casks/fastrawviewer.rb
+++ Casks/fastrawviewer.rb
@@ -1,6 +1,6 @@
 cask 'fastrawviewer' do
-  version '1.4.8.1348'
-  sha256 'fc580fd87189a88d7cc0811d6dd854bef18df3b0db0ed84746df828ef269e15c'
+  version '1.4.9.1358'
+  sha256 '48521d8720674fee476365bc5717e1197794bc29709a772b0fa3d2bb3c691bb2'
 
   url "https://updates.fastrawviewer.com/data/FastRawViewer-#{version}.dmg"
   name 'FastRawViewer'
