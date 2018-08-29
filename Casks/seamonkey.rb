0ab794b5450abb3a52be9bb0dbdb3f34676ae7a4
Julian Mayer <julian@corecode.io>
2018-08-30 01:29:31 +0200

    update seamonkey to 2.49.4

--- Casks/seamonkey.rb
+++ Casks/seamonkey.rb
@@ -1,33 +1,33 @@
 cask 'seamonkey' do
-  version '2.49.3'
+  version '2.49.4'
 
   language 'de' do
-    sha256 '3bc0a913e420f77f5a44b1ff763ba6f1995b1f775e4a432793a08a57ddf058d1'
+    sha256 '8a634207b1c2f176ac06fd3c996cd57ffe00bee88946b3249901bfef8c574cd0'
     'de'
   end
 
   language 'en-GB' do
-    sha256 'd22813aff2321cc17fdef594142a2c292ecb5f45b61f590c42b660b2c6c48a0e'
+    sha256 '7ec831df90187daaf616ae6fca6ff5746ba12f36d08537484ab82e791f8d70ab'
     'en-GB'
   end
 
   language 'en', default: true do
-    sha256 '8e39beaedc4d0f340c2d92e4f584f644cf9c18e03f3af0913ac9cc95092c9c98'
+    sha256 '0afaa36a1be1f3b082e094fff7213c2d578fc5e3f4c6724c3d4e34a98bd537b6'
     'en-US'
   end
 
   language 'fr' do
-    sha256 '620501393f2475037e4552442b61dfc3245ba31980a8be2647d76bc5edfbb571'
+    sha256 'f83da993f99e682e85947683d7fd99c8e4ba04cd5c6e198dfc9d96ce05803b73'
     'fr'
   end
 
   language 'it' do
-    sha256 '3fb078dc08ca8756c76adde6313744d4f0ea3870b769c447070fd7f5f3796df8'
+    sha256 '57506a0b5cf47443f275b889b3d04653fb33d203239514cc53b992f845f5780e'
     'it'
   end
 
   language 'ru' do
-    sha256 '623145931366e21042fc226bd8103b06721c4bd7106197c33b0447a9521439c4'
+    sha256 'a7bb1b5b3b12015e8721b2f16ae4946dfbc0b824895b38caad131465d458c469'
     'ru'
   end
 
