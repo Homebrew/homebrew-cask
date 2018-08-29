92e7375d3ae53eec0a8114779af79970c053194c
Brian Morton <brianmortondesign@gmail.com>
2018-08-29 10:46:39 -0700

    Update principle to 4.1

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,6 +1,6 @@
 cask 'principle' do
-  version '3.11'
-  sha256 '7791e7e8357781dc5be5f8dfcc296e194d06b8d3d3fbe674918a70f95a3a383a'
+  version '4.1'
+  sha256 '783ae69dfcaeca83314b7ed40ba8ea501aee758021107e41780cb0cba529d4d0'
 
   url "http://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
   appcast 'http://principleformac.com/update2.xml'
