4a27ec19e6b8c1d933e670682fc4b0c3c4e7eca1
SanKo <sankoshine@users.noreply.github.com>
2018-08-30 06:09:06 +0800

    Update qqlive to 1.9.0.38546 (#51427)

--- Casks/qqlive.rb
+++ Casks/qqlive.rb
@@ -1,6 +1,6 @@
 cask 'qqlive' do
-  version '1.8.0.38108'
-  sha256 'f9d135293b88dc9ceda476d257cfc6d5221b6407ec1fb9d024bb248a6b67dfcb'
+  version '1.9.0.38546'
+  sha256 '85f093e88d02da409f7223521ada5d1cf23cd47ca3048d5e5d5cfa625e9b3703'
 
   url "https://dldir1.qq.com/qqtv/mac/TencentVideo_V#{version}.dmg"
   name 'QQLive'
