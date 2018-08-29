94b14daf22c9e6a4593abd1e5ea5799623a5ea6d
Brian Morton <brianmortondesign@gmail.com>
2018-08-29 10:33:11 -0700

    Update 1password to 7.1.1 (#51406)

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -1,6 +1,6 @@
 cask '1password' do
-  version '7.0.7'
-  sha256 'd6b593ab7c7156c840bff9e358bd6ef9aeef5e3abb41f02d268f34783265df1b'
+  version '7.1.1'
+  sha256 'ff60d1caf4a6ceb68e5ac90af76d558f8e2f4368606fef79027aa91fef479c98'
 
   # 1password.com was verified as official when first introduced to the cask
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
