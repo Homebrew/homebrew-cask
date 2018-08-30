4584763bd93f33a7fe827219248a66adea7827dd
Brian Morton <brianmortondesign@gmail.com>
2018-08-30 09:38:30 -0700

    Update smoothscroll to 1.1.11

--- Casks/smoothscroll.rb
+++ Casks/smoothscroll.rb
@@ -1,6 +1,6 @@
 cask 'smoothscroll' do
-  version '1.1.10'
-  sha256 '0c649cb3a9cfe64d89a9359c187b97a1c788bbc5f11fe4ae92163e04958b51d8'
+  version '1.1.11'
+  sha256 '497ac605dcae2833cace00c77d56acb7c699ca94e86fc8ac71d74ddf06e5113b'
 
   url 'https://www.smoothscroll.net/mac/download/SmoothScroll.app.zip'
   appcast 'https://updater.smoothscroll.net/mac/updater.xml'
