d981c7e8f25fbc81e96a15a8c9dc74bbefc30ed6
USAMI Kosuke <usami.kosuke@gmail.com>
2018-08-30 10:04:05 +0900

    Update sleipnir to 4.6.0

--- Casks/sleipnir.rb
+++ Casks/sleipnir.rb
@@ -1,6 +1,6 @@
 cask 'sleipnir' do
-  version '4.5.10'
-  sha256 '384b646c53f62c1c52e1a480bd8182150bcd2c39ab7c4066cb6c2890fbb6d0bc'
+  version '4.6.0'
+  sha256 '9d0c29ca3daea8fe6e337790c011cc03f92827ea5fc6099c1436a83d104ca6d0'
 
   url 'http://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
   appcast 'https://update.fenrir.co.jp/smartupdate/mac/sleipnir/appcast.xml'
