5eb403ae16ea66b217d9a46a999694ba09fd2fd1
SteamedFish <i@steamedfish.org>
2018-08-30 09:26:13 +0800

    Update popo to 2.6.9,5655

--- Casks/popo.rb
+++ Casks/popo.rb
@@ -1,6 +1,6 @@
 cask 'popo' do
-  version '2.6.8,5653'
-  sha256 'e3ce86e5575540a41528e6ae94b4197f34c248c94551ff71d108dcf088ee212a'
+  version '2.6.9,5655'
+  sha256 'f487a4148123a735850e6d616a64fc658615691216122e00a48fbc2dff7b071f'
 
   url "http://popo.netease.com/file/popomac/POPO_Mac_V#{version.dots_to_underscores.before_comma}.dmg"
   name 'NetEase POPO'
