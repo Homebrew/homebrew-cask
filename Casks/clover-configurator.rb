ec7ee601edc2a947956e3572c30bd91257283d7e
commitay <commitay@users.noreply.github.com>
2018-08-30 17:36:38 +1000

    Update clover-configurator.rb

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,5 +1,5 @@
 cask 'clover-configurator' do
-  version 'lastest'
+  version '5.2.0.0'
   sha256 'f26694143ad4c33e84992e0e282c5d745f3b0a24f50a3eacaad1fda1d2bbb5a0'
 
   url 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/builds-data/CCG.zip'
