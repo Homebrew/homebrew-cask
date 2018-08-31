044840f0ead958b64cff7a2d2142a8d146942ecd
Brian Morton <brianmortondesign@gmail.com>
2018-08-31 09:20:47 -0700

    Update kite to 0.20180830.0

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,6 +1,6 @@
 cask 'kite' do
-  version '0.20180829.0'
-  sha256 '94e11f5f5746636d6d711b24dc6bb0b7bd51837c45107a299a90d08142bf6fa9'
+  version '0.20180830.0'
+  sha256 '90b7bd963e5bc705e8d14e43ff7aa625acaddcb8b250e4d14804e263a1f66a6e'
 
   # s3-us-west-1.amazonaws.com/kite-downloads was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/kite-downloads/Kite-#{version}.dmg"
