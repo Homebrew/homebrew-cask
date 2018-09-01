c2d56c9075a68eb3954ceff426e8067c3c6dff75
Yurii Kolesnykov <root@yurikoles.com>
2018-09-01 22:08:11 +0300

    new cask: cryptoplugin v1.2.2

---
+++ Casks/cryptoplugin.rb
@@ -0,0 +1,16 @@
+cask 'cryptoplugin' do
+  version '1.2.2'
+  sha256 '97934a670ed0ab7e9eb0bcf992458cc6fcab428249300d3d3369b4943d9563a7'
+
+  url "https://eo.privatbank.ua/service/sign/CryptoPlugin/install/cryptoplugin-#{version}.zip"
+  name 'PrivatBank Crypto Plugin'
+  homepage 'https://client-bank.privatbank.ua/'
+
+  installer script: {
+                      executable: "#{staged_path}/cryptoplugin-#{version}.app/Contents/Resources/install.sh",
+                    }
+
+  uninstall script: {
+                      executable: "#{staged_path}/cryptoplugin-#{version}.app/Contents/Resources/remove.sh",
+                    }
+end
