f149c0be4ea49de05487afd6d15547c650a0c87d
anna mcd <ammcdevitt@users.noreply.github.com>
2018-09-01 10:32:24 -0600

    Update to version 18.011.20058

--- Casks/adobe-acrobat-reader.rb
+++ Casks/adobe-acrobat-reader.rb
@@ -1,5 +1,5 @@
 cask 'adobe-acrobat-reader' do
-  version '18.011.20040'
+  version '18.011.20058'
   sha256 'a81034bbfbafd9ad9b7c71c971eb7c5188aae5f7faa707141f1d83a1edacf014'
 
   url "http://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots}/AcroRdrDC_#{version.no_dots}_MUI.dmg"
