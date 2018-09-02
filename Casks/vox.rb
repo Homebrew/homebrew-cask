215be40a9bb9213734a97bce31887d200741ee30
tiiiecherle <tiiiecherle@gmail.com>
2018-09-02 18:00:38 +0200

    Update vox to 3.3.2 (#51558)

--- Casks/vox.rb
+++ Casks/vox.rb
@@ -1,6 +1,6 @@
 cask 'vox' do
-  version '3.3.1'
-  sha256 '9217769f35c36ee4873639ffe664b832040f6069c0ee8ea0b7e236138b23ee2c'
+  version '3.3.2'
+  sha256 'd07942089a2c616f80a845954a7daf4257d6d83eba59906f58c93871588afa22'
 
   # devmate.com/com.coppertino.Vox was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coppertino.Vox/Vox.dmg'
