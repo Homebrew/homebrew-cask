0b3276fe11098a6a3db3d36e333fd33751021da4
Vítor Galvão <info@vitorgalvao.com>
2018-09-02 11:52:11 +0100

    Update armortext.rb

--- Casks/armortext.rb
+++ Casks/armortext.rb
@@ -3,7 +3,7 @@ cask 'armortext' do
   sha256 'c7211c0ce0b999f35e2a8f6b0e4a7b87dfec08b44a6e58cc6f6fd2e5fc7c291a'
 
   # armortext.co was verified as official when first introduced to the cask
-  url 'https://downloads.armortext.co/desktop/release/0.19.32/ArmorText-0.19.32-mac64.dmg'
+  url "https://downloads.armortext.co/desktop/release/#{version}/ArmorText-#{version}-mac64.dmg"
   name 'Armor Text'
   homepage 'https://armortext.com/'
 
