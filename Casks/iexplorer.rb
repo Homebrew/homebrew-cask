67334b877268458bc65a5d16767d267557771429
Elf Sundae <elf.sundae@gmail.com>
2018-09-02 18:54:01 +0800

    Update iexplorer to 4.2.2

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,6 +1,6 @@
 cask 'iexplorer' do
-  version '4.2.1'
-  sha256 'b4fc350aacc2d1d6da04bf92ccb151300c945f037a73c9b4642262696013eb01'
+  version '4.2.2'
+  sha256 'c95d14c02107ae1d3e5ed633d649841759dd2905d9159246d116f2f38af21a16'
 
   url "https://assets.macroplant.com/downloads/iExplorer-#{version}.dmg"
   name 'iExplorer'
