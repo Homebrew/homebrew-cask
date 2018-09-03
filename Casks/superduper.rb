52c351b505bc5bcb2dad6e52c95e4457c1e5a8ba
Julian Mayer <julian@corecode.io>
2018-09-03 23:47:14 +0200

    Update superduper to 3.1.7,112

--- Casks/superduper.rb
+++ Casks/superduper.rb
@@ -1,9 +1,10 @@
 cask 'superduper' do
-  version :latest
-  sha256 :no_check
+  version '3.1.7,112'
+  sha256 '62861de305bac5fe9f123619d7cd5ec0251f018d296ec1e0f6a13c4f68bc3be9'
 
   # amazonaws.com/shirtpocket/SuperDuper was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/shirtpocket/SuperDuper/SuperDuper!.dmg'
+  appcast 'https://versioncheck.blacey.com/superduper/version.xml?VSN=100'
   name 'SuperDuper!'
   homepage 'http://www.shirt-pocket.com/SuperDuper/SuperDuperDescription.html'
 
