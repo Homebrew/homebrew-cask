b34b2264b935445b748f3fdca0fb5093c8dbd105
Jon Vincent <jeiv@users.noreply.github.com>
2018-09-04 15:12:13 -0700

    Update DOSBox to 0.74-2 (#51620)

--- Casks/dosbox.rb
+++ Casks/dosbox.rb
@@ -1,9 +1,9 @@
 cask 'dosbox' do
-  version '0.74'
-  sha256 '99057370b478608a6f0167ef522ef59e0e1b876f8565622d3dbd707925d61f6c'
+  version '0.74-2'
+  sha256 '582d17a29666cc36f9a2bb3804bcba2fadf4d26712ce801b7fbcf3c02f659348'
 
   # sourceforge.net/dosbox was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/dosbox/dosbox/#{version}/DOSBox-#{version}-1_Universal.dmg"
+  url "https://downloads.sourceforge.net/dosbox/dosbox/#{version}/DOSBox-#{version}_Universal.dmg"
   appcast 'https://sourceforge.net/projects/dosbox/rss?path=/dosbox'
   name 'DOSBox'
   homepage 'https://www.dosbox.com/'
