8f455662176e266c2b99f5115cb329d806aafad9
Julien Nicouleaud <juliennicouleaud@yahoo.fr>
2018-09-05 18:29:56 +0200

    Fixed version number and sha256
    Fixed version number and sha256 since the url does not point to a specific version, and there is no appcast.

--- Casks/send-anywhere.rb
+++ Casks/send-anywhere.rb
@@ -1,6 +1,6 @@
 cask 'send-anywhere' do
-  version '8.7.19'
-  sha256 'fdc06913486b2aed22da3ce56c1de5fd2be2dc07da6592487e3fcd05c464ab31'
+  version :latest
+  sha256 :no_check
 
   url 'https://update.send-anywhere.com/osx_downloads/Send%20Anywhere.dmg'
   name 'Send Anywhere'
