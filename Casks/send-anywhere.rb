ec978885b5474333c7c1ad37616ca42d4f15ea35
Julien Nicouleaud <juliennicouleaud@yahoo.fr>
2018-09-05 13:58:39 +0200

    Add Send Anywhere v8.7.19
    Adding Send Anywhere to casks since MAS version is outdated. (available on MAS is v8.4.24)

---
+++ Casks/send-anywhere.rb
@@ -0,0 +1,10 @@
+cask 'send-anywhere' do
+  version '8.7.19'
+  sha256 'fdc06913486b2aed22da3ce56c1de5fd2be2dc07da6592487e3fcd05c464ab31'
+
+  url 'https://update.send-anywhere.com/osx_downloads/Send%20Anywhere.dmg'
+  name 'Send Anywhere'
+  homepage 'https://send-anywhere.com/'
+
+  app 'Send Anywhere.app'
+end
