62a74a370bd03cc82781b5107275f5d6ebf3f8b0
CaptainArk <captainark@captainark.net>
2018-09-05 22:23:31 +0200

    Update seadrive to 0.9.4 (#51656)

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -1,6 +1,6 @@
 cask 'seadrive' do
-  version '0.9.3'
-  sha256 'e0704749a8fb6eb98cc74ebdb8a3fb590836a87708be4067688986b016d0c6ea'
+  version '0.9.4'
+  sha256 '077e8562b2cc2aeefdcc252582a440677a368212969a2472d984acab74d9f1d3'
 
   # download.seadrive.org was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seadrive-#{version}.dmg"
