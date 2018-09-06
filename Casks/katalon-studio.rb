9d71196614509cf5e30d86232204d8c78508a1b6
Chris Trevarthen <ctrevarthen@detroitlabs.com>
2018-09-06 10:41:59 -0400

    Update katalon-studio to 5.7.0

--- Casks/katalon-studio.rb
+++ Casks/katalon-studio.rb
@@ -1,6 +1,6 @@
 cask 'katalon-studio' do
-  version '5.6.3'
-  sha256 '471410c0864b993ac57e96137a2d728bc67dd50f80ca215a0373a7201b8e729c'
+  version '5.7.0'
+  sha256 'e0600de5cfcc7350c6d5b28d23cf4016f4ad66993a063eabdd022f295ef278a7'
 
   url "https://download.katalon.com/#{version}/Katalon_Studio.dmg"
   name 'Katalon Studio'
