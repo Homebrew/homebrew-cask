4739ce47591ca7b7689c76dfb1544048a15c66c3
Chris Ganga <3910877+gangachris@users.noreply.github.com>
2018-09-06 18:27:11 +0300

    Update visual-studio-code to 1.27.1,5944e81f3c46a3938a82c701f96d7a59b074cfdc (#51684)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.27.0,7b9afc4196bda60b0facdf62cfc02815509b23d6'
-  sha256 'ff417c70b2bd47dd2309f73508d3b0a370967857a3faf9429d107c0677756a7a'
+  version '1.27.1,5944e81f3c46a3938a82c701f96d7a59b074cfdc'
+  sha256 'a6be0defb39730054066e102f5b007dcbb792a7abf134cc7879ec8d1fe48a841'
 
   # az764295.vo.msecnd.net/stable was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
