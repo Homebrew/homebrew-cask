2040c275c213dc7fcd1787a78b870daabf6082e2
commitay <commitay@users.noreply.github.com>
2018-09-06 23:44:30 +1000

    Update apache-directory-studio: use apache mirror download strategy (#51681)

--- Casks/apache-directory-studio.rb
+++ Casks/apache-directory-studio.rb
@@ -2,7 +2,7 @@ cask 'apache-directory-studio' do
   version '2.0.0.v20170904-M13'
   sha256 '17df069735317034737a51439e463e61a1f8e283ea464e299ef023d68cc15dc3'
 
-  url "http://www.us.apache.org/dist/directory/studio/#{version}/ApacheDirectoryStudio-#{version}-macosx.cocoa.x86_64.dmg"
+  url "http://www.apache.org/dyn/closer.cgi?path=/directory/studio/#{version}/ApacheDirectoryStudio-#{version}-macosx.cocoa.x86_64.dmg"
   appcast 'http://apache.mirror.serversaustralia.com.au/directory/studio/'
   name 'Apache Directory Studio'
   homepage 'https://directory.apache.org/studio/'
