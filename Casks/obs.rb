fe22fe88514f2bb6133402ce621486f72ec8fb04
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2018-09-07 23:38:11 +0300

    Upgrade obs to 22.0.3 (#51710)
    Updated to 22.0.3

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -1,6 +1,6 @@
 cask 'obs' do
-  version '22.0.2'
-  sha256 'cf7d002dd6f98b35d1c5a59cbad4e3cd6a02e034f8d160859247f1f3e743bc30'
+  version '22.0.3'
+  sha256 '4e39f601aeea8f949d3570e3052cbaa919a43060fa1beeaee0da6ce6c7aa2b24'
 
   url "https://cdn-fastly.obsproject.com/downloads/obs-mac-#{version}-installer.pkg"
   appcast 'https://github.com/obsproject/obs-studio/releases.atom'
