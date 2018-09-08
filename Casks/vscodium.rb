543a97d82321296d30f03e6ee9b56c08142005b6
Victor Popkov <victor@popkov.me>
2018-09-08 22:54:33 +0300

    Update vscodium appcast

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -3,7 +3,7 @@ cask 'vscodium' do
   sha256 'f339bb6b3d90f71144b510aa27d741fe300e625305e765d5ed6d2ed5f97ba0a1'
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCode-darwin-#{version}.zip"
-  appcast 'https://github.com/VSCodium/vscodiumas/releases.atom'
+  appcast 'https://github.com/VSCodium/vscodium/releases.atom'
   name 'VSCodium'
   homepage 'https://github.com/VSCodium/vscodium'
 
