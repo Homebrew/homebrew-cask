8a0cac4e0914231e1c4d4dd2dc0dda4ef0d1e2cd
tem44 <35421983+tem44@users.noreply.github.com>
2018-09-08 15:15:32 +0100

    fixing tiny little issue

--- Casks/actoolbox.rb
+++ Casks/actoolbox.rb
@@ -4,7 +4,7 @@ cask 'actoolbox' do
     sha256 '#' # review
     url "http://www.actoolbox.net/data/documents/ACToolbox.4.5.6.dmg"
     # appcast '#' # review
-    name ‘JunXion’
+    name ‘actoolbox’
     homepage 'http://www.actoolbox.net/download/'
     app "/Applications/AC Toolbox/AC Toolbox.app "
     zap trash: '~/Library/Caches/Toolbox
