47c8768affee31a6de9768a07ece558608f985c8
Julian Mayer <julian@corecode.io>
2018-09-09 16:59:12 +0200

    Update copay to 4.6.2

--- Casks/copay.rb
+++ Casks/copay.rb
@@ -1,6 +1,6 @@
 cask 'copay' do
-  version '4.6.1'
-  sha256 '2bb455fd4a3a09eeb0bc5361f97a0fb568bcd253b7e6f8d47b3ee87f394da2b6'
+  version '4.6.2'
+  sha256 '0bc82e497b6576a8d0c861c607fb71565e84611f4172d788c8ec3698a98b45b2'
 
   # github.com/bitpay/copay was verified as official when first introduced to the cask
   url "https://github.com/bitpay/copay/releases/download/v#{version}/Copay.dmg"
