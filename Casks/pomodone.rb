a5c4d94640af3fcc9055dca45fb39770e2e719cb
CoreCode <core-code@users.noreply.github.com>
2018-09-09 17:27:31 +0200

    Update pomodone to 1.5.1389 (#51742)

--- Casks/pomodone.rb
+++ Casks/pomodone.rb
@@ -1,6 +1,6 @@
 cask 'pomodone' do
-  version '1.5.1227'
-  sha256 '079c3ddde0c117b10a5f434d66baa4bdc5bbb9a1f2fbb707ac2aa2dae9d24cd2'
+  version '1.5.1389'
+  sha256 '94e93aaedf3c63a15d5f4cac5c4d83c4c5c382772acb778092adf184fafc5e1c'
 
   url "https://app.pomodoneapp.com/installers/PomoDoneApp-#{version}.dmg"
   name 'PomoDone'
