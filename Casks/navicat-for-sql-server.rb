f52add5e82271cef8158056198153fa9da9e00ea
CoreCode <core-code@users.noreply.github.com>
2018-09-09 18:39:13 +0200

    Update navicat-for-sql-server to 12.1.7 (#51761)

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-sql-server' do
-  version '12.1.6'
-  sha256 '371ff41c13ffa39b8d1562e62d9714e217f333786d7a3f03fdb5234aa1a6473a'
+  version '12.1.7'
+  sha256 '5d178018a7e77a0c70473d09a659e2b4dbf997ff3a9afdf78beead2e1ff87c36'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-sqlserver-release-note'
