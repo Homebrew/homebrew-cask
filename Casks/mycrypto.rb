b5b309b59f77b66f6952a78f60d72a76a34d90a5
Vítor Galvão <info@vitorgalvao.com>
2018-09-10 10:29:11 +0100

    Update mycrypto.rb

--- Casks/mycrypto.rb
+++ Casks/mycrypto.rb
@@ -3,7 +3,7 @@ cask 'mycrypto' do
   sha256 '56904099e91260f9cf7acb76075bdd8ea015001c793b023abec6d02dac39640d'
 
   # github.com/MyCryptoHQ/MyCrypto was verified as official when first introduced to the cask
-  url "https://github.com/MyCryptoHQ/MyCrypto/releases/download/1.2.3/mac_#{version}_MyCrypto.dmg"
+  url "https://github.com/MyCryptoHQ/MyCrypto/releases/download/#{version}/mac_#{version}_MyCrypto.dmg"
   appcast 'https://github.com/MyCryptoHQ/MyCrypto/releases.atom'
   name 'MyCrypto'
   homepage 'https://mycrypto.com/'
