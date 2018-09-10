3bb6df85e8953d8ccecb689045833355b9f99ad7
brigitte zmoelnig <brigitte@corecode.io>
2018-09-10 22:25:21 +0200

    Update hands-off to 4.0.0

--- Casks/hands-off.rb
+++ Casks/hands-off.rb
@@ -1,6 +1,6 @@
 cask 'hands-off' do
-  version '3.2.10'
-  sha256 'b270f765950790bd7bc15959cb010ebb4442629e51340514448226cc74e2e3b4'
+  version '4.0.0'
+  sha256 'b3ce928dcd3b9d47399cb69384d2705aa05cbba01ed563b9629d954da9abda2b'
 
   url "https://www.oneperiodic.com/files/Hands%20Off!%20v#{version}.dmg"
   appcast "https://www.oneperiodic.com/handsoff#{version.major}.xml"
