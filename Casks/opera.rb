a27bec54170c150c87dfa0ec42f2aedc1e736f2c
Andrei izotov <faradaytrs@gmail.com>
2018-09-10 10:06:53 +0800

    Update opera to 55.0.2994.56

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '55.0.2994.44'
-  sha256 '8db0ae1d0d106e93e6192713e76850fecbd474e84b0ac8fe1883ff9bd719cb81'
+  version '55.0.2994.56'
+  sha256 '42b002f0a9121d8b8446db668ca5c87cf8cc455521ce2a8f81e25cc2773cda18'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
