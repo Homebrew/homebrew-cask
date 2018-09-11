74c8f0233707a275ee4faff1bbdd55b2a99e03c0
Timothy Sutton <tim@synthist.net>
2018-09-11 09:46:39 -0700

    flash-npapi 31.0.0.108 (#51817)

--- Casks/flash-npapi.rb
+++ Casks/flash-npapi.rb
@@ -1,6 +1,6 @@
 cask 'flash-npapi' do
-  version '30.0.0.154'
-  sha256 '7cf2a22334fc15d506f035d57e7cdeeefe283b7e880326d3a75d316b4481b18b'
+  version '31.0.0.108'
+  sha256 'bf901964a53493e076f9a8a15f5332317a32557ecf798507e5922559715184dc'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.adobe.com/get/flashplayer/pdc/#{version}/install_flash_player_osx.dmg"
