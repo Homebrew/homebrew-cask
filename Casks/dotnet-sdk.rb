2b030c930ec17e701159851e8c48acc64df62b44
dasMulli <martin.andreas.ullrich@gmail.com>
2018-09-11 21:34:17 +0200

    Update dotnet-sdk to 2.1.402

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-sdk' do
-  version '2.1.401'
-  sha256 '83af1dc6fdcf9847b7e27d0b6eb8272b0319d72380c6a2c0a87cc29b2978b390'
+  version '2.1.402'
+  sha256 'f5b6f230395735a8c31cae7f01d43f61bd3227082d7bc3555a91cebcd0deb9db'
 
-  url "https://download.microsoft.com/download/E/8/A/E8AF2EE0-5DDA-4420-A395-D1A50EEFD83E/dotnet-sdk-#{version}-osx-x64.pkg"
+  url "https://download.microsoft.com/download/8/A/7/8A765126-50CA-4C6F-890B-19AE47961E4B/dotnet-sdk-#{version}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
   name '.NET Core SDK'
   homepage 'https://www.microsoft.com/net/core#macos'
