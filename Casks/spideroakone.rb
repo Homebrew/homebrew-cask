69609dba414252bb5b3bbe4cc9f0e7bc2fb76ded
Gavin Chappell <g-a-c@noreply.users.github.com>
2018-09-11 20:25:18 +0100

    Update SpiderOakONE to 7.3.0

--- Casks/spideroakone.rb
+++ Casks/spideroakone.rb
@@ -2,15 +2,15 @@ cask 'spideroakone' do
   version :latest
   sha256 :no_check
 
-  if MacOS.version <= :sierra
-    url 'https://spideroak.com/release/spideroak/osx'
-  else
+  if MacOS.version == :high_sierra
     url 'https://spideroak.com/release/spideroak/osx_hs'
+  else
+    url 'https://spideroak.com/release/spideroak/osx'
   end
   name 'SpiderOakOne'
   homepage 'https://spideroak.com/'
 
-  depends_on macos: '>= :mountain_lion'
+  depends_on macos: '>= :mavericks'
 
   pkg 'SpiderOakONE.pkg'
 
