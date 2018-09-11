4cff6307d8d201a51a63cc1b3b21a32657a2f097
brigitte zmoelnig <brigitte@corecode.io>
2018-09-11 09:58:48 +0200

    Update birdfont to 3.12.4

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -1,6 +1,6 @@
 cask 'birdfont' do
-  version '3.12.0'
-  sha256 'f024872a3cb21a2ad77631bc030cebef9405256c09eac4611effde3a454bea7a'
+  version '3.12.4'
+  sha256 'f3ec1d508310aa57c7d449ca3d08ce151ff8d589cd47ce9785deaa8fd85ff72c'
 
   url "https://birdfont.org/download/birdfont-#{version}-free.dmg"
   name 'BirdFont'
