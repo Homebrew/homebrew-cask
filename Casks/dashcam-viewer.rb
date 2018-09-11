d506eacbee4e2033145e0bf26050b061386ae654
miccal <miccal.matthews@gmail.com>
2018-09-12 00:46:28 +0800

    Update dashcam-viewer to 3.1.2 (#51808)

--- Casks/dashcam-viewer.rb
+++ Casks/dashcam-viewer.rb
@@ -1,6 +1,6 @@
 cask 'dashcam-viewer' do
-  version '3.0.3'
-  sha256 'cabb0a283559e11f5667589d5a19d9fcc168d69825cf502a56d3b6e3230b99a3'
+  version '3.1.2'
+  sha256 'c56067b076f44f4c1968b8eae24f4ca8b71b6522f27bfbc65ae2eff5c585773a'
 
   # s3.amazonaws.com/aws-website-dcv-downloads-c8kwd/dcv was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/aws-website-dcv-downloads-c8kwd/dcv/Dashcam_Viewer_v#{version}.dmg"
