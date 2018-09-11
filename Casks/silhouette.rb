d4f7e1886e7aec51e92e16699ef5cd43142b357c
Dominik Nufer <dominik@nufer.org>
2018-09-11 19:30:00 +0200

    Add Silhouette.app v 4.1.468
    new App silhouette.app the app used for the silhouette plotter

---
+++ Casks/silhouette.rb
@@ -0,0 +1,10 @@
+cask 'silhouette' do
+  version '4.1.468'
+  sha256 '08212b5b7ac718ce64306abc6e6806c010e62603fd4f27cac2f16afae5251d65'
+
+  url 'https://dl.silhcdn.com/6145ed0d05415535'
+  name 'silhouette'
+  homepage 'https://www.silhouetteamerica.com/software'
+
+  app ''
+end
