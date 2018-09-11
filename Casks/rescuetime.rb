8cf97ea357a5c0b44b7a1d99467598ba83300cb8
Josh Tilles <MerelyAPseudonym@gmail.com>
2018-09-11 10:36:42 -0400

    Update rescuetime to latest

--- Casks/rescuetime.rb
+++ Casks/rescuetime.rb
@@ -2,11 +2,13 @@ cask 'rescuetime' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.rescuetime.com/installers/RescueTimeInstaller.dmg'
+  url 'https://www.rescuetime.com/installers/RescueTimeInstaller.pkg'
   name 'RescueTime'
   homepage 'https://www.rescuetime.com/'
 
-  app 'RescueTime.app'
+  pkg 'RescueTimeInstaller.pkg'
+
+  uninstall pkgutil: 'com.rescuetime.RescueTime'
 
   zap trash: '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.rescuetime.rescuetime.sfl*'
 end
