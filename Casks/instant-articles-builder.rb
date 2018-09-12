a497d0310135f53b74f5973ebc6f1daeff523570
Vítor Galvão <info@vitorgalvao.com>
2018-09-12 12:08:54 +0100

    Update instant-articles-builder.rb

--- Casks/instant-articles-builder.rb
+++ Casks/instant-articles-builder.rb
@@ -2,7 +2,7 @@ cask 'instant-articles-builder' do
   version '0.2.0'
   sha256 '0a74d3d60d891c5fa99cb9a76efa4d16298acae2f8e802e0c0d0a8b3fa775f71'
 
-  # github.com/facebook was verified as official when first introduced to the cask
+  # github.com/facebook/instant-articles-builder was verified as official when first introduced to the cask
   url "https://github.com/facebook/instant-articles-builder/releases/download/#{version}/Instant.Articles.Builder.for.Mac-#{version}.zip"
   appcast 'https://github.com/facebook/instant-articles-builder/releases.atom'
   name 'Instant Articles Builder'
