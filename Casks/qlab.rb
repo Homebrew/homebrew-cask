06f2ccbddc138756a7b3c42fbde5b764078067e2
Jason Fiore <jason@MBP15-macOS.local>
2018-09-12 13:10:51 -0700

    Update qlab to 4.3.1

--- Casks/qlab.rb
+++ Casks/qlab.rb
@@ -1,6 +1,6 @@
 cask 'qlab' do
-  version '4.3'
-  sha256 '4e801498e63a9ad1137c3f3e549f0c1d450bd5f20ceaae9d597d8461628a0327'
+  version '4.3.1'
+  sha256 'dc4b6a00e14e7fbaea1c2202646742bb832df75bacaeb49353f361fabb1e1464'
 
   url "https://figure53.com/qlab/downloads/QLab-#{version}.zip"
   appcast "https://figure53.com/qlab/downloads/appcast-v#{version.major}/"
