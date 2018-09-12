3aa1df86a42d43d4cdad5f0583a1f3723d62698c
miccal <miccal.matthews@gmail.com>
2018-09-12 08:47:43 +0800

    Update editready.rb

--- Casks/editready.rb
+++ Casks/editready.rb
@@ -3,7 +3,7 @@ cask 'editready' do
   sha256 'f3b16ff2519d9bb789835a7eaab532c7c11cec9bc5d4afcd412db6a9e20e9876'
 
   url "https://www.divergentmedia.com/fileRepository/EditReady%20#{version}.dmg"
-  name 'divergent media EditReady'
+  name 'Divergent Media EditReady'
   homepage 'https://www.divergentmedia.com/editready'
 
   app 'EditReady.app'
