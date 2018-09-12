241578195e24b78e9537f30cb47dba87fb480f44
Austin Shalit <austinshalit@gmail.com>
2018-09-12 10:45:01 -0400

    Update arduino to 1.8.7

--- Casks/arduino.rb
+++ Casks/arduino.rb
@@ -1,6 +1,6 @@
 cask 'arduino' do
-  version '1.8.6'
-  sha256 'ee8e6cca7a24521d08f2b2e79f9313f5c25a5e5c9a5f9c1fe693ead43628f0d5'
+  version '1.8.7'
+  sha256 'bc5fae3e0b54f000d335d93f2e6da66fc8549def015e3b136d34a10e171c1501'
 
   url "https://downloads.arduino.cc/arduino-#{version}-macosx.zip"
   appcast 'https://www.arduino.cc/en/Main/ReleaseNotes'
