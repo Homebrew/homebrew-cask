dae2f6a297df9f9a267258e61c6c9e99f9b9542b
Sasha Khamkov <sanusart@gmail.com>
2018-09-12 23:05:50 +0300

    Update gisto to 1.9.98

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -1,6 +1,6 @@
 cask 'gisto' do
-  version '1.9.87'
-  sha256 'b4ef8d0e2066c9dd089d1b87557651adbfc6bde0f39b236fe4544c5b9a6e9412'
+  version '1.9.98'
+  sha256 'e7990fc61589b446cefc1d580b8c80252754fcd90853e8deb525f8bb79bfad2c'
 
   # github.com/Gisto/Gisto was verified as official when first introduced to the cask
   url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto-#{version}.dmg"
