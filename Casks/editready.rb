cc834bab37db66b74aca29b98fada218cbf27c75
miccal <miccal.matthews@gmail.com>
2018-09-13 03:28:25 +0800

    Update editready to 2.1.6 (#51843)
    * Update editready to 2.1.6
    
    * Update editready.rb

--- Casks/editready.rb
+++ Casks/editready.rb
@@ -2,8 +2,8 @@ cask 'editready' do
   version '2.1.6'
   sha256 'f3b16ff2519d9bb789835a7eaab532c7c11cec9bc5d4afcd412db6a9e20e9876'
 
-  url "https://www.divergentmedia.com/filedownload/editready%20#{version}.dmg"
-  name 'divergent media EditReady'
+  url "https://www.divergentmedia.com/fileRepository/EditReady%20#{version}.dmg"
+  name 'Divergent Media EditReady'
   homepage 'https://www.divergentmedia.com/editready'
 
   app 'EditReady.app'
