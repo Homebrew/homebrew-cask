2c74827b454f96c3c8e5ed71c516119f940ffdef
Scott Humphries <git@sscotth.io>
2018-09-13 12:05:47 -0500

    Update integrity to latest

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -1,10 +1,9 @@
 cask 'integrity' do
-  version '8.1.10'
-  sha256 '398633df49423ce64e96a0ccc52d3bcdb564dae64fb4016eca35a970d094801b'
+  version :latest
+  sha256 :no_check
 
   # peacockmedia.co.uk/integrity was verified as official when first introduced to the cask
   url 'http://peacockmedia.co.uk/integrity/integrity.dmg'
-  appcast 'http://peacockmedia.software/mac/integrity/version_history.html'
   name 'Integrity'
   homepage 'http://peacockmedia.software/mac/integrity/'
 
