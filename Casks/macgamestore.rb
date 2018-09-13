a376a5886302a013babe240ce295a2333452cc01
Vítor Galvão <info@vitorgalvao.com>
2018-09-13 20:45:39 +0100

    Forgot to add the appcast

--- Casks/macgamestore.rb
+++ Casks/macgamestore.rb
@@ -3,6 +3,7 @@ cask 'macgamestore' do
   sha256 '8086a0643e646f407537e1497a9152dd3747044c12fbf8f2c6ac08dc48913a52'
 
   url 'https://www.macgamestore.com/api_clientapp/clientupdates/public/core5/MacGameStore.dmg'
+  appcast 'https://www.macgamestore.com/api_clientapp/clientupdates/public/update.xml'
   name 'MacGameStore'
   homepage 'https://www.macgamestore.com/app/'
 
