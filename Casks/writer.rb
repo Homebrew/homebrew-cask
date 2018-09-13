6ae8f6761a273d5ffde8f88595898c3e59f6a172
Litzsch <36682603+Litzsch@users.noreply.github.com>
2018-09-13 18:33:44 +0000

    Fixed an incomplete link and missing linebreak

--- Casks/writer.rb
+++ Casks/writer.rb
@@ -2,7 +2,8 @@ ask 'writer' do
   version '2.1.1'
   sha256 'ca71fad8d767f510ad3be08bacc4a128782fb93f8cf09b797f2b5d531750cbe1'
 
-  url "https://github.com/HendrikNoeller/Writer/releases/download/#{version}/Wr$  appcast 'https://github.com/HendrikNoeller/Writer/releases.atom'
+  url "https://github.com/HendrikNoeller/Writer/releases/download/#{version}/Writer.app.zip"
+  appcast 'https://github.com/HendrikNoeller/Writer/releases.atom'
   name 'Writer'
   homepage 'https://github.com/HendrikNoeller/Writer'
 
