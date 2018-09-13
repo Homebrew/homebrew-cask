7863fca7976731f9b4604a36166d12a5d1481490
Philip Molares <git@molar.es>
2018-09-13 15:40:16 +0200

    updated dcp-o-matic-encode-server to 2.12.10 (#51881)

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-encode-server' do
-  version '2.12.9'
-  sha256 '9d1e2792d5a78c1c2ecf5b48670192fa88955511b6117a8485918579b7efec19'
+  version '2.12.10'
+  sha256 'b5ff88898de8b7d4016d7e1b1f2215f7fbab09b7eec9f83c4884176176b0f699'
 
   url "https://dcpomatic.com/dl.php?id=osx-server&version=#{version}"
   appcast 'https://dcpomatic.com/download'
