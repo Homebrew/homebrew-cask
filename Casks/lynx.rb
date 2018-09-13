8fbd7ed905cbdfbf203d90056e194329cbf2f062
Phil Hannent <phil@elmstonesystems.co.uk>
2018-09-13 15:02:54 +0100

    Fix up feedback on pull request

--- Casks/lynx.rb
+++ Casks/lynx.rb
@@ -1,10 +1,13 @@
 cask 'lynx' do
-  version :latest
+  version '6.1.4'
   sha256 :no_check
 
-  url 'https://download.saharasupport.com/lynx/production/macx/Lynx.pkg'
+  url 'https://download.saharasupport.com/lynx/production/macx/Lynx-install.dmg'
+  appcast 'http://downloads.saharasupport.com/lynx/production/macx/version.txt'
   name 'Lynx by Clevertouch'
   homepage 'https://download.saharasupport.com/'
+
+  auto_updates true
 
   pkg 'Lynx.pkg'
 
