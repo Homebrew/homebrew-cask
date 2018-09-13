1c1d042279a42705cb6f6db4bcf25f8c812803b4
Litzsch <36682603+Litzsch@users.noreply.github.com>
2018-09-13 18:57:32 +0000

    Syntax fix; Added trash on zap

--- Casks/writer.rb
+++ Casks/writer.rb
@@ -1,4 +1,4 @@
-ask 'writer' do
+cask 'writer' do
   version '2.1.1'
   sha256 'ca71fad8d767f510ad3be08bacc4a128782fb93f8cf09b797f2b5d531750cbe1'
 
@@ -8,4 +8,10 @@ ask 'writer' do
   homepage 'https://github.com/HendrikNoeller/Writer'
 
   app 'Writer.app'
+
+  zap trash: [
+    '~/Application Support/Writer',
+    '~/Library/Preferences/de.HendrikNoeller.Writer.plist'
+  ]
 end
+
