05b4fc3d4ecdb9cab4b22752d8844ebfc790f573
Philip Molares <git@molar.es>
2018-09-13 10:09:34 +0200

    updated dcp-o-matic-kdm-creator to 2.12.10

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-kdm-creator' do
-  version '2.12.9'
-  sha256 '5aa525ce5964ac439cd3ea8e7c69cd3147e0bc46fe21bed9d82af1afbca82ab7'
+  version '2.12.10'
+  sha256 '4e7c73667178f999b0531d2bbcca3239cc90b13c83a7d0d1ca023d2921f3ee41'
 
   url "https://dcpomatic.com/dl.php?id=osx-kdm&version=#{version}"
   appcast 'https://dcpomatic.com/download'
