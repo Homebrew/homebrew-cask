842080eb72607ecb8bdaf94b830a8bab028b779b
Vítor Galvão <info@vitorgalvao.com>
2018-09-13 20:24:46 +0100

    Update daedalus.rb

--- Casks/daedalus.rb
+++ Casks/daedalus.rb
@@ -2,8 +2,8 @@ cask 'daedalus' do
   version '0.11.0,1.3.0:2260'
   sha256 '6ad90da2053f06f9f0e35c2ae05a8620094b83c6a6a5401c201a1e5abcc22ae4'
 
-  # update-cardano-mainnet.iohk.io was verified as official when first introduced to the cask
-  url "https://update-cardano-mainnet.iohk.io/daedalus-#{version.before_comma}-cardano-sl-#{version.after_comma.before_colon}-mainnet-macos-#{version.after_comma.after_colon}.pkg"
+  # github.com/input-output-hk/daedalus was verified as official when first introduced to the cask
+  url "https://github.com/input-output-hk/daedalus/releases/download/#{version.before_comma}/daedalus-#{version.before_comma}-cardano-sl-#{version.after_comma.before_colon}-mainnet-macos-#{version.after_comma.after_colon}.pkg"
   appcast 'https://github.com/input-output-hk/daedalus/releases.atom'
   name 'Daedalus'
   homepage 'https://daedaluswallet.io/'
