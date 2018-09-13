8509592665aeae92d0a90ac46c4c16281bfb2d38
Philip Molares <git@molar.es>
2018-09-13 10:12:08 +0200

    updated dcp-o-matic-player to 2.12.10

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-player' do
-  version '2.12.9'
-  sha256 '3edc777985cad02fabf251cafcfd2d3b95743f8728d3639055a7fbe82dc9464c'
+  version '2.12.10'
+  sha256 'bb18cd6b719be1536f4cbff506960afd2f9a9e04d6be8d07edf4fd4da281e7aa'
 
   url "https://dcpomatic.com/dl.php?id=osx-player&version=#{version}"
   appcast 'https://dcpomatic.com/download'
