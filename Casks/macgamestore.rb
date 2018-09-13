f55c2e46b031158f21f21dabf433d64f15d9195f
Santiago Maturana Arce <santiago.maturana@colaboradoresunidadeditorial.es>
2018-09-13 20:53:21 +0200

    Add MacGameStore lastest

---
+++ Casks/macgamestore.rb
@@ -0,0 +1,14 @@
+cask 'macgamestore' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.macgamestore.com/api_clientapp/clientupdates/public/core5/MacGameStore.dmg'
+  name 'MacGameStore'
+  homepage 'https://www.macgamestore.com/app/'
+
+  app 'MacGameStore.app'
+
+  zap "trash": [
+                 '~/Library/Application Support/MacGameStore.com',
+               ]
+end
