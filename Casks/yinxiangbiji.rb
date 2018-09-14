7ee26d29d101df789a46071132df810a393f193a
秦新 <qinxin_0720@live.com>
2018-09-14 14:51:02 +0800

    Update yinxiangbiji to version 8.2.0_457181

--- Casks/yinxiangbiji.rb
+++ Casks/yinxiangbiji.rb
@@ -1,6 +1,6 @@
 cask 'yinxiangbiji' do
-  version '8.1.2_456939'
-  sha256 '3857d9897bf3e7839e3ad95f657eb635bbfc4ad284203a9f251d1905d134abbf'
+  version '8.2.0_457181'
+  sha256 '2b64f2accb3b535923f2ae91b76074513d285001e2ffbb25287044675e7e6e55'
 
   url "https://cdn.yinxiang.com/mac-smd/public/YinxiangBiji_RELEASE_#{version}.dmg"
   name 'Evernote'
