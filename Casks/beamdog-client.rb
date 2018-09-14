7657a9ab6527ce93361178845f0f7d0634b50dec
Birger J. Nordølum <birgerjn@gmail.com>
2018-09-14 17:09:50 +0200

    Update beamdog-client to 2.1.9 (#51948)

--- Casks/beamdog-client.rb
+++ Casks/beamdog-client.rb
@@ -1,6 +1,6 @@
 cask 'beamdog-client' do
-  version '2.1.8'
-  sha256 'b5e23a1f505f839865ff7b2c05cfccd8e9b981cff49bb84899fbe2773734a067'
+  version '2.1.9'
+  sha256 'b3ee780be9c3e1d5eefb75c890531eca69d57a24b4b776a81e2cf3f2ecc4515a'
 
   url "http://client.beamdog.com/download/#{version}/osx_64/Beamdog%20Client-#{version}.dmg"
   name 'Beamdog Client'
