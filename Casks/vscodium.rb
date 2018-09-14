e8899e9ac23e56192751a75dd5d3f4a73c551a44
Peter <psquicciarini@redventures.com>
2018-09-14 13:36:50 -0400

    Update vscodium to 1.27.2

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,6 +1,6 @@
 cask 'vscodium' do
-  version '1.27.1'
-  sha256 'fd8a5e6bd5914b256f59449f857f6aaac79663064cdf20353905ff7d3a2e44ab'
+  version '1.27.2'
+  sha256 'fa7f09be214ad8581ac587c7c31827afeff63158df0f145f4c160a0ca6602860'
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCode-darwin-#{version}.zip"
   appcast 'https://github.com/VSCodium/vscodium/releases.atom'
