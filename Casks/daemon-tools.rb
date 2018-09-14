47c10abc7b11541ba74383dad61d7ba564ece45b
Julian Mayer <julian@corecode.io>
2018-09-15 01:31:50 +0200

    Update daemon-tools to 6.3.419

--- Casks/daemon-tools.rb
+++ Casks/daemon-tools.rb
@@ -1,6 +1,6 @@
 cask 'daemon-tools' do
-  version '6.2.397'
-  sha256 'e5f3c0f8e8757a01191dac5dce74e8af5219b8c992c4324c2545d85476351b12'
+  version '6.3.419'
+  sha256 '033fa39159d5ff6818d91ad3c8f15603511868b73dbb7acff849e3923fb14536'
 
   # web-search-home.com was verified as official when first introduced to the cask
   url 'http://web-search-home.com/download/dtLiteMac'
