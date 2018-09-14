2be83ff9021e10dd7aee59c54ec4ecc6541aa184
Victor Popkov <victor@popkov.me>
2018-09-14 23:17:39 +0300

    Update snowflake-snowsql url comment

--- Casks/snowflake-snowsql.rb
+++ Casks/snowflake-snowsql.rb
@@ -2,8 +2,7 @@ cask 'snowflake-snowsql' do
   version '1.1.62'
   sha256 'c6135f011524527361230f5fc9c911fcb673c56ee6c9bc5bf01e35a68380f4cf'
 
-  # s3 location from docs.
-  # https://docs.snowflake.net/manuals/user-guide/snowsql-install-config.html#amazon-s3
+  # sfc-snowsql-updates.s3.us-west-2.amazonaws.com was verified as official when first introduced to the cask
   url "https://sfc-snowsql-updates.s3.us-west-2.amazonaws.com/bootstrap/#{version.major_minor}/darwin_x86_64/snowsql-#{version}-darwin_x86_64.pkg"
   name 'SnowSQL'
   homepage 'https://www.snowflake.net/'
