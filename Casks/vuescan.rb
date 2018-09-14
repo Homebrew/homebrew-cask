bc4947b3f51bccd47c2cbdda7e7ff4cd2b766806
miccal <miccal.matthews@gmail.com>
2018-09-14 08:42:14 +0800

    Update vuescan to 9.6.16

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,9 +1,9 @@
 cask 'vuescan' do
-  version '9.6.15'
-  sha256 '13e09b319ce57f70d8dd14ca082cf1d5bc9444c73579f284e015b76ff12446ac'
+  version '9.6.16'
+  sha256 '6d9dbffc19a46b39063f285eafce54e54e9e07b2aef5f110848e0c938c23d6e6'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
-  appcast 'https://www.hamrick.com/old-versions.html'
+  appcast 'https://www.hamrick.com/alternate-versions.html'
   name 'VueScan'
   homepage 'https://www.hamrick.com/'
 
