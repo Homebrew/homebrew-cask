6c0283ed2fb7fe8985e28f1c6432df6dcfe896ee
Julian Mayer <julian@corecode.io>
2018-09-14 23:50:18 +0200

    Update bbedit to 12.1.6

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -1,6 +1,6 @@
 cask 'bbedit' do
-  version '12.1.5'
-  sha256 'ef8795bee09830944b4018377280888c28cf26a0591c2994c50cd2837fef9f67'
+  version '12.1.6'
+  sha256 '23b9fc6ef5c03cbcab041566503c556d5baf56b2ec18f551e6f0e9e6b48dc690'
 
   # s3.amazonaws.com/BBSW-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
