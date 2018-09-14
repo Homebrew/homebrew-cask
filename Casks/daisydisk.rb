aa50766a91e3fd0a201e1d23babf40b7df9cdfa3
Nick Wind-McJetters <nick@elearningexperts.net>
2018-09-14 10:15:15 -0500

    Update daisydisk to 4.6.5.1 (#51944)

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -8,8 +8,8 @@ cask 'daisydisk' do
     sha256 'fe2aa86f2ea8a1f0c4791857a5b7991ecad295b5b969849bb7b15a890ab54b86'
     url "https://www.daisydiskapp.com/downloads/DaisyDisk_#{version.dots_to_underscores}.zip"
   else
-    version '4.6.5'
-    sha256 'ada54ddb476d8badb2174dc73eb97198f1ed71afa0d39d201797f3e937a19dbd'
+    version '4.6.5.1'
+    sha256 'c99e4a5eb7209770e4ffc21a43e6c8c047567f04af0841afe064002c8d04e770'
     url 'https://www.daisydiskapp.com/downloads/DaisyDisk.zip'
     appcast 'https://daisydiskapp.com/downloads/appcastReleaseNotes.php?appEdition=Standard&osVersion=10.13'
   end
