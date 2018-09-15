e6d894ce41117e9ef544236f312528c088174751
CoreCode <core-code@users.noreply.github.com>
2018-09-15 13:44:01 +0200

    Update cctalk.rb

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -3,7 +3,9 @@ cask 'cctalk' do
   sha256 '96e08fb58f07aeb1b468ae1e7f29591e61396ebd5b428a8d56dafbc5c739f010'
 
   # n1other.hjfile.cn was verified as official when first introduced to the cask
-  url "https://n1other.hjfile.cn/wx/CCtalk/#{version.after_comma}/CCtalk-#{version.before_comma}-#{version.after_comma}.dmg"
+  url "https://n1other.hjfile.cn/wx/CCtalk/#{version.after_comma}/CCtalk-#{version.before_comma}-#{version.after_comma}.dmg",
+        user_agent: :fake
+
   appcast 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/update/info.xml'
   name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
