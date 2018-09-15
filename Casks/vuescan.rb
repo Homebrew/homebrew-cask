da49c93977b516cfab18dcb104c86090624fff79
Lantrix <lantrix@pobox.com>
2018-09-15 22:11:03 +1000

    Update vuescan to 9.6.16

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.6.16'
-  sha256 '6d9dbffc19a46b39063f285eafce54e54e9e07b2aef5f110848e0c938c23d6e6'
+  sha256 '93598183343f7e0c7c7b13331e7f1151bf997eaa197389276a13efaf74b3fc89'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
