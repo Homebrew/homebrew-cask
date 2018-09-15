11d1f24379a7af5b01decb1398f388c1bc2cef0e
Ted B <lantrix@pobox.com>
2018-09-16 01:39:21 +1000

    Fix bad SHA256 for vuescan to 9.6.16 (#52001)
    * Update vuescan to 9.6.16
    
    * Update vuescan.rb

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.6.16'
-  sha256 '6d9dbffc19a46b39063f285eafce54e54e9e07b2aef5f110848e0c938c23d6e6'
+  sha256 '8c350abc5301a52f030a3141edbf7e5d4521df4c4392b931ef6eb91919d9d5b2'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
