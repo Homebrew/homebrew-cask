f71bf49223e984ccdbb2e292da1cf101a0222976
Vítor Galvão <info@vitorgalvao.com>
2018-09-15 16:36:07 +0100

    Update vuescan.rb

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.6.16'
-  sha256 '93598183343f7e0c7c7b13331e7f1151bf997eaa197389276a13efaf74b3fc89'
+  sha256 '8c350abc5301a52f030a3141edbf7e5d4521df4c4392b931ef6eb91919d9d5b2'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
