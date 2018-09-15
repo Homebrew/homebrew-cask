6a8be44fb2e6bc0c1f1581198f00d261694bd84d
Vítor Galvão <info@vitorgalvao.com>
2018-09-15 13:17:57 +0100

    Update cctalk.rb

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -4,8 +4,7 @@ cask 'cctalk' do
 
   # n1other.hjfile.cn was verified as official when first introduced to the cask
   url "https://n1other.hjfile.cn/wx/CCtalk/#{version.after_comma}/CCtalk-#{version.before_comma}-#{version.after_comma}.dmg",
-        user_agent: :fake
-
+      user_agent: :fake
   appcast 'http://f1.ct.hjfile.cn/api/AutoUpdate/newupdate/out/mac/cctalk/update/info.xml'
   name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
