3ebc237dca75091cd51daa6f5e9d040fca174370
Max Langer <maxlanger@me.com>
2018-09-16 14:14:11 +0200

    Add Latest.app v0.4.3 (#52013)

---
+++ Casks/latest.rb
@@ -0,0 +1,11 @@
+cask 'latest' do
+  version '0.4.3'
+  sha256 '00a81cc1482067eeb21c57314c76f24938db87953eda9a6d1265b7f72b04721a'
+
+  url 'https://max.codes/latest/Latest.zip'
+  appcast 'https://max.codes/latest/feed.xml'
+  name 'Latest'
+  homepage 'https://max.codes/latest'
+
+  app 'Latest.app'
+end
