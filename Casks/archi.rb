6f0b50dc8cd1f428dd1edec061deac0bcd9ba96f
Vítor Galvão <info@vitorgalvao.com>
2018-09-16 13:13:42 +0100

    Update archi.rb

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -3,6 +3,7 @@ cask 'archi' do
   sha256 '37a85ef876943e9956518bb14d3e869acfe86b94070cba1ee5c203a76d0eb44f'
 
   url "https://www.archimatetool.com/downloads/root#{version.no_dots}/Archi-Mac-#{version}.zip",
+      user_agent: :fake,
       referer: 'https://www.archimatetool.com/download/'
   appcast 'https://github.com/archimatetool/archi/releases.atom'
   name 'Archi'
