8385dfa061456862b6f4853607d11b2db58f88f7
Yuriy Minin <me@yuriy.io>
2018-09-16 16:14:21 -0700

    Add Streaker.app v1.0.6
    Fixed cask audit by adding appcast

--- Casks/streaker.rb
+++ Casks/streaker.rb
@@ -3,6 +3,7 @@ cask 'streaker' do
   sha256 'ac9f9538b5f08c575600899704ff4d8333c44785076a348f51873554d458bcc3'
 
   url "https://github.com/jamieweavis/streaker/releases/download/v#{version}/Streaker-#{version}-mac.zip"
+  appcast 'https://github.com/jamieweavis/streaker/releases.atom'
   name 'Streaker'
   homepage 'https://github.com/jamieweavis/streaker'
 
