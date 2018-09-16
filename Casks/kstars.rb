9e18d0b9ba09f6bf638010f3c11cee2841b80d44
CoreCode <core-code@users.noreply.github.com>
2018-09-16 13:54:35 +0200

    Update kstars to 2.9.8 (#52037)

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask 'kstars' do
-  version '2.9.6'
-  sha256 '7c2d237a543d88acf426f71cc4e60cb3097e458a61a6f4965058143fa86a71f5'
+  version '2.9.8'
+  sha256 'a01ca011d8c796e8357e3a81520548bcec9b5e788c4d930fe727aaf236c65e90'
 
   # indilib.org/jdownloads/kstars was verified as official when first introduced to the cask
   url "http://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg"
