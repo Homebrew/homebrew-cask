80f17b19beee74f992d1b4cffee7ea13db1df1cd
Vítor Galvão <info@vitorgalvao.com>
2018-09-16 12:56:46 +0100

    Update lyricsx.rb

--- Casks/lyricsx.rb
+++ Casks/lyricsx.rb
@@ -1,8 +1,8 @@
 cask 'lyricsx' do
-  version '1.4.1+1846'
+  version '1.4.1,1846'
   sha256 'b7d93a65fd53de2d3f5a3673ffe49b44bb641f72907e74d5582b5676d25acb53'
 
-  url "https://github.com/ddddxxx/LyricsX/releases/download/v#{version.sub(%r{\+\d+$},'')}/LyricsX_#{version}.zip"
+  url "https://github.com/ddddxxx/LyricsX/releases/download/v#{version.before_comma}/LyricsX_#{version.before_comma}+#{version.after_comma}.zip"
   appcast 'https://github.com/ddddxxx/LyricsX/releases.atom'
   name 'LyricsX'
   homepage 'https://github.com/ddddxxx/LyricsX'
