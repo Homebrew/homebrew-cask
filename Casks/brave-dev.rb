1614020979e11ee7a50fa71bc3537747d7742352
Kevin Schaich <schaich.kevin@gmail.com>
2018-09-15 19:42:41 -0700

    Single-quoted strings for style

--- Casks/brave-dev.rb
+++ Casks/brave-dev.rb
@@ -2,7 +2,7 @@ cask 'brave-dev' do
   version '0.53.2'
   sha256 'f4082d20352c120dbef1dc2234dc9a4707a9e14c30e94b01859bc0ec9023dbe0'
 
-  url "https://brave-browser-downloads.s3.brave.com/latest/Brave-Browser-Dev.dmg"
+  url 'https://brave-browser-downloads.s3.brave.com/latest/Brave-Browser-Dev.dmg'
   appcast 'https://github.com/brave/brave-core/releases.atom'
   name 'Brave Dev'
   homepage 'https://brave.com/'
