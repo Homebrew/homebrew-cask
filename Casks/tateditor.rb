db1ce9e5f6f9f5ee344f7e63c8f8846756daaba2
CoreCode <core-code@users.noreply.github.com>
2018-09-16 23:42:25 +0200

    Update tateditor.rb

--- Casks/tateditor.rb
+++ Casks/tateditor.rb
@@ -2,8 +2,8 @@ cask 'tateditor' do
   version :latest
   sha256 :no_check
 
-  # drive.google.com/file/d/0B5RO0CVwwupjVTN0SzRjUGRxcVE was verified as official when first introduced to the cask
-  url 'https://drive.google.com/file/d/0B5RO0CVwwupjVTN0SzRjUGRxcVE/view?usp=drive_web'
+  # doc-0g-b4-docs.googleusercontent.com was verified as official when first introduced to the cask
+  url 'https://doc-0g-b4-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/anhs72m6jaa8buvihpi5rat56qu75qbc/1537128000000/16692779252864521799/*/0B5RO0CVwwupjVTN0SzRjUGRxcVE?e=download'
   name 'TATEditor'
   homepage 'http://www.cc4966.net/'
 
