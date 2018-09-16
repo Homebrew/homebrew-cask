ff7594fcdf26ca2bf865dc0c3ed9c89c797d0526
Julian Mayer <julian@corecode.io>
2018-09-16 12:32:42 +0200

    Update melodics to 2.0.2198

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask 'melodics' do
-  version '2.0.2152'
-  sha256 '2d33bbc900ff2c2dd95ad9da0679f9d142c00778e42c2ebf2697d4a4ab4a2030'
+  version '2.0.2198'
+  sha256 '3c611d73f68f4016e687f138c7ec4b1cd158657afa982ec63f95d7dfb2600f2f'
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://api.melodics.com/download/osxupdatescastv#{version.major}.xml"
