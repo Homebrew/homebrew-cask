03a727720472534faa5b78cd8557eedecea4c463
Herdinger <herdinger@fastmail.fm>
2018-09-17 22:19:43 +0200

    Add Emacs.app v26.1-2 without linking ctags
    Since emacs in homebrew-core refers to Cask as the canonical install
    we should mirror it's behaviour or at least provide an option to do so.
    
    Emacs in homebrew-core doesn't link ctags by default letting users
    install their own, this Casks does too.

---
+++ Casks/emacs-without-ctags.rb
@@ -0,0 +1,23 @@
+cask 'emacs-without-ctags' do
+  version '26.1-2'
+  sha256 '2ea8d0b0055d5d0ba604771dbb2f9731dd5c815776eec6a9bca3c44d7ab40d99'
+
+  url "https://emacsformacosx.com/emacs-builds/Emacs-#{version}-universal.dmg"
+  appcast 'https://emacsformacosx.com/atom/release'
+  name 'Emacs'
+  homepage 'https://emacsformacosx.com/'
+
+  conflicts_with formula: ['emacs']
+
+  app 'Emacs.app'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/ebrowse"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
+
+  zap trash: [
+               '~/Library/Caches/org.gnu.Emacs',
+               '~/Library/Preferences/org.gnu.Emacs.plist',
+               '~/Library/Saved Application State/org.gnu.Emacs.savedState',
+             ]
+end
