dd340ad6d9106ea3739d7a06b043164515877279
Vítor Galvão <info@vitorgalvao.com>
2018-09-17 02:11:56 +0100

    Update and rename sonar.rb to flipper.rb

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,9 +1,9 @@
-cask 'sonar' do
+cask 'flipper' do
   version '0.7.1'
   sha256 '0ebfa13be4537c4bd05d86ebd08f65213dfc41d5aa8f7855163f6882b44c0fd8'
 
-  # facebook.com was verified as official when first introduced to the cask
-  url 'https://www.facebook.com/fbflipper/public/mac'
+  # github.com/facebook/flipper was verified as official when first introduced to the cask
+  url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper.zip"
   appcast 'https://github.com/facebook/flipper/releases.atom'
   name 'Sonar'
   name 'Flipper'
