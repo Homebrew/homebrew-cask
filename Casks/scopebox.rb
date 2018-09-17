a3765c39629b4bed32a5d9704deaa9adfd815d25
CoreCode <core-code@users.noreply.github.com>
2018-09-17 03:08:49 +0200

    Update scopebox to 3.5.5 (#52072)

--- Casks/scopebox.rb
+++ Casks/scopebox.rb
@@ -2,7 +2,7 @@ cask 'scopebox' do
   version '3.5.5'
   sha256 '6f5b880dc7c4cb16b4407117dd1ddc619020b906aa122a6cf20caaf90da726cf'
 
-  url "https://www.divergentmedia.com/filedownload/ScopeBox%20#{version}.dmg"
+  url "https://www.divergentmedia.com/fileRepository/ScopeBox%20#{version}.dmg"
   name 'ScopeBox'
   homepage 'https://www.divergentmedia.com/scopebox'
 
