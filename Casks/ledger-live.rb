bea07a76f84bec3e4b35e35d157f50feda291983
Ramon Torres <ramon.torres@edgebound.com>
2018-09-18 09:13:43 -0500

    Update ledger-live to 1.1.11

--- Casks/ledger-live.rb
+++ Casks/ledger-live.rb
@@ -1,6 +1,6 @@
 cask 'ledger-live' do
-  version '1.1.7'
-  sha256 '2449a563d8c09cd1e37e1f55c0efd3c084c6cd3609459acb924dd0691bd0e4bf'
+  version '1.1.11'
+  sha256 '3a2f903f56d7697192467294fd7bf0b22a68bcfaa7584ae14963f9cad3e4e815'
 
   # github.com/LedgerHQ/ledger-live-desktop was verified as official when first introduced to the cask
   url "https://github.com/LedgerHQ/ledger-live-desktop/releases/download/v#{version}/ledger-live-desktop-#{version}-mac.dmg"
