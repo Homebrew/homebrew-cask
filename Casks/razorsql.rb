b3d4fc0516d3209c45594b3ece3c1d77ceca6f8c
Anver Housseini <contact@anver.ch>
2018-09-18 07:38:22 +0200

    Update razorsql to 8.0.8 (#52110)

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask 'razorsql' do
-  version '8.0.7'
-  sha256 '6e626920deee44edda16d7bfc52af931ea2a7338c545ba5f3863400906ab8db5'
+  version '8.0.8'
+  sha256 '7b624c4d7e43ba59c78cb4a212521531041aa21077b694c5705e3a91f796d288'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   appcast 'https://razorsql.com/updates.html'
