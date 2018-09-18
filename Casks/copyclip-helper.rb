161aebf3d9a0f9257d9ec3a432451f79398984e5
Vítor Galvão <info@vitorgalvao.com>
2018-09-18 19:11:19 +0100

    Update copyclip-helper.rb

--- Casks/copyclip-helper.rb
+++ Casks/copyclip-helper.rb
@@ -1,12 +1,12 @@
 cask 'copyclip-helper' do
-  version :latest
-  sha256 :no_check
+  version '2'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url 'https://fiplab.com/helpers/CopyClip-2-Helper.zip'
+  url "https://fiplab.com/helpers/CopyClip-#{version}-Helper.zip"
   name 'CopyClip Helper'
   homepage 'https://fiplab.com/apps/copyclip-helper'
 
   depends_on cask: 'copyclip'
 
-  app 'CopyClip 2 Helper.app'
+  app "CopyClip #{version} Helper.app"
 end
