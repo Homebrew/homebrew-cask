18d04fbcd7925394b185070287aa7febd70642ba
Julian Mayer <julian@corecode.io>
2018-09-18 22:05:50 +0200

    Update saoimage-ds9 to 7.6

--- Casks/saoimage-ds9.rb
+++ Casks/saoimage-ds9.rb
@@ -1,17 +1,21 @@
 cask 'saoimage-ds9' do
-  version '7.5'
+  version '7.6'
 
   if MacOS.version == :yosemite
-    sha256 '2b8df99204f6d739da4e4dc784056350ee7670ac896baa16be8f2fb0847a9755'
-    url "http://ds9.si.edu/download/macosxyosemite/SAOImage%20DS9%20#{version}.dmg"
+    sha256 'f1207cdd65a2f1820f4aa149fa0dd5c8c2032cafc36d805e9dce76ba402acac0'
+    url "http://ds9.si.edu/download/macosxyosemite/SAOImageDS9%20#{version}.dmg"
   elsif MacOS.version == :el_capitan
-    sha256 'bfb80ed0311034ac060fc7ef455fae4056870c93f06f90606da4fd49eb551b4b'
-    url "http://ds9.si.edu/download/macosxelcapitan/SAOImage%20DS9%20#{version}.dmg"
+    sha256 'eb1b01fcb75ec67a66f39294cf8ac18169f96d1ef1114a1ded66702b2fbe487e'
+    url "http://ds9.si.edu/download/macosxelcapitan/SAOImageDS9%20#{version}.dmg"
+  elsif MacOS.version == :sierra
+    sha256 '6ddd52ad39e573599c4cb4b21202d84dc6f46cb8800e3d698f00d3c41af6d30f'
+    url "http://ds9.si.edu/download/macossierra/SAOImageDS9%20#{version}.dmg"
   else
-    sha256 '4fde92df2c2b216de34eff8b962c24f0a49b93c641575f5ea4f68009b97b45e9'
-    url "http://ds9.si.edu/download/macosxsierra/SAOImage%20DS9%20#{version}.dmg"
+    sha256 'b45d1cb4f621388837836179c114f34e3ebd24e23b53feeed9774fc0db182f69'
+    url "http://ds9.si.edu/download/macoshighsierra/SAOImageDS9%20#{version}.dmg"
   end
 
+  appcast 'http://ds9.si.edu/site/Download.html'
   name 'SAOImage DS9'
   homepage 'http://ds9.si.edu/site/Home.html'
 
