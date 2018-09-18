1fc2c027a06e30c34e9fb018cf3de77cdd07900b
Litzsch <36682603+Litzsch@users.noreply.github.com>
2018-09-18 20:19:50 +0000

    Adding an appcast

--- Casks/mysql-community-server.rb
+++ Casks/mysql-community-server.rb
@@ -3,6 +3,7 @@ cask 'mysql-community-server' do
   sha256 '7b461b64f7eaee045941d37d5db8a0a8466828fb142d04abc9b9b2350d6176aa'
 
   url "https://dev.mysql.com/get/Downloads/MySQL-#{version.major_minor}/mysql-#{version}-macos10.13-x86_64.dmg"
+  appcast 'https://dev.mysql.com/doc/relnotes/mysql/8.0/en/'
   name 'MySQL Community Server'
   homepage 'https://www.mysql.com/'
 
