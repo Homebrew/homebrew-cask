18c7c8b1140a28608fc0e4dc4a9741d107686781
Julian Mayer <julian@corecode.io>
2018-09-19 20:53:28 +0200

    Update stattransfer to 14

--- Casks/stattransfer.rb
+++ Casks/stattransfer.rb
@@ -1,6 +1,6 @@
 cask 'stattransfer' do
-  version '13'
-  sha256 'a4a4372982fba0ebb4896b30d6bd51abf740ceda0bcf17f3603cc8843b2222dc'
+  version '14'
+  sha256 'b4d40db39f2ddae23622336771b0dfb27e874ba885320d2fe75482783342bfe6'
 
   url 'https://www.stattransfer.com/downloads/stdemo.dmg'
   name 'Stat/Transfer'
