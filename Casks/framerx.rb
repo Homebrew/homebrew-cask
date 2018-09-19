d48040f5b63d578cfba09b3d8b359e9d3e3fe785
Jakub Benes <jukben@gmail.com>
2018-09-19 18:45:44 +0200

    fix: added hostnames differ coment

--- Casks/framerx.rb
+++ Casks/framerx.rb
@@ -2,6 +2,7 @@ cask 'framerx' do
   version '24114,1537280058'
   sha256 :no_check
 
+  # devmate.com/com.framer.x was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.framer.x/FramerX.zip'
   appcast 'https://updates.devmate.com/com.framer.x.xml'
   name 'Framer X'
