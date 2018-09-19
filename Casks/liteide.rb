439505017f38b3253d6158f7ae3c032cff068765
karnauskas <marius.karnauskas@gmail.com>
2018-09-19 22:08:30 +0100

    Bump liteide to 34.2 (#52187)

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask 'liteide' do
-  version '34.1,5.9.5'
-  sha256 '5b89f4d59268a343e375a549f7c5a93f73730d73de77104dfbce8e11a2111be5'
+  version '34.2,5.9.5'
+  sha256 '4cee36757151c41ab3299be32edba2542fce12dca7801946123d8fd9f082bdcc'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
   url "https://github.com/visualfc/liteide/releases/download/x#{version.before_comma}/liteidex#{version.before_comma}.macosx-qt#{version.after_comma}.zip"
