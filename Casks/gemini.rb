89100d57b9248f66d72a1bd605982ae1f1461b6c
Santiago Maturana Arce <santiago.maturana@colaboradoresunidadeditorial.es>
2018-09-19 21:14:39 +0200

    Updated version minor and major.

--- Casks/gemini.rb
+++ Casks/gemini.rb
@@ -1,14 +1,14 @@
 cask 'gemini' do
-  version '303,1537359522'
+  version '2.303,1537359522'
   sha256 '1d4c7371ab88aad0295bcdf055eac2c11d82a2e1a2a7932e17c98a18dc249531'
 
   # dl.devmate.com/com.macpaw.site.Gemini was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.macpaw.site.Gemini2/#{version.before_comma}/#{version.after_comma}/Gemini2-#{version.before_comma}.zip"
-  appcast "https://updates.devmate.com/com.macpaw.site.Gemini2.xml"
+  url "https://dl.devmate.com/com.macpaw.site.Gemini#{version.major}/#{version.minor}/#{version.after_comma}/Gemini#{version.major}-#{version.minor}.zip"
+  appcast "https://updates.devmate.com/com.macpaw.site.Gemini#{version.major}.xml"
   name 'Gemini'
   homepage 'https://macpaw.com/gemini'
 
-  app "Gemini 2.app"
+  app "Gemini #{version.major}.app"
 
   zap trash: [
                '~/Library/Application Support/Gemini 2',
