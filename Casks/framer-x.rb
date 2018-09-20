e7adc6e965d76ea8ed50c5cdab40c00ff7adee86
Vítor Galvão <info@vitorgalvao.com>
2018-09-20 11:10:14 +0100

    Update framer-x.rb

--- Casks/framer-x.rb
+++ Casks/framer-x.rb
@@ -1,6 +1,6 @@
 cask 'framer-x' do
   version '24114,1537280058'
-  sha256 'be953850c92aad5eedd2b5434f95d45a2257f0eb62a73ae2ae0f947cfdc125a9' 
+  sha256 'be953850c92aad5eedd2b5434f95d45a2257f0eb62a73ae2ae0f947cfdc125a9'
 
   # dl.devmate.com/com.framer.x was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.framer.x/#{version.before_comma}/#{version.after_comma}/FramerX-#{version.before_comma}.zip"
