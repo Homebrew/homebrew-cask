9a0c2d251185b2144afdad2b0411534e8443141d
Julian Mayer <julian@corecode.io>
2018-09-21 00:53:17 +0200

    Update passenger to 4.6.1

--- Casks/passenger.rb
+++ Casks/passenger.rb
@@ -1,6 +1,6 @@
 cask 'passenger' do
   version '4.6.1'
-  sha256 '2e51501c65c339f7e9f1fa1c74b75fb7d60b4c3a2f0b02a99a28ec4d2b46d592'
+  sha256 '860af97614dd12a751b132f0cf7d88110462ad88151a96f7e2dfacd6d2c3c8b2'
 
   url 'https://macinmind.com/Passenger.dmg'
   appcast 'https://macinmind.com/pads/PassengerAppcast.xml'
