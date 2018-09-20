15bd9656028471c6e08913d674956d5b60b901c9
Bob Olde Hampsink <bob@robuust.digital>
2018-09-20 11:12:22 +0200

    Add Sublime Merge.app build 1055

---
+++ Casks/sublime-merge.rb
@@ -0,0 +1,11 @@
+cask 'sublime-merge' do
+  version '1055'
+  sha256 '9948229effa51b41794aad4521d32d3058191baf85642a3233b82c5b95621430'
+
+  # download.sublimetext.com was verified as official when first introduced to the cask
+  url "https://download.sublimetext.com/sublime_merge_build_#{version}_mac.zip"
+  name 'Sublime Merge'
+  homepage 'https://www.sublimemerge.com/'
+
+  app 'Sublime Merge.app'
+end
