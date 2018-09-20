2e2348fc64e49637093ff932490935a207b5fcea
Julian Mayer <julian@corecode.io>
2018-09-20 12:53:44 +0200

    Update google-earth-web-plugin to latest

--- Casks/google-earth-web-plugin.rb
+++ Casks/google-earth-web-plugin.rb
@@ -2,7 +2,8 @@ cask 'google-earth-web-plugin' do
   version :latest
   sha256 :no_check
 
-  url 'http://r2---sn-po4vapo3-j3ae.c.pack.google.com/edgedl/earth/plugin/current/googleearth-mac-plugin-intel.dmg'
+  url 'http://r1---sn-c0q7lns7.c.pack.google.com/edgedl/earth/plugin/current/googleearth-mac-plugin-intel.dmg?redirect_counter=1&cms_redirect=yes&shardbypass=yes',
+      user_agent: :fake
   name 'Google Earth plug-in'
   homepage 'https://www.google.com/intl/en/earth/explore/products/plugin.html'
 
