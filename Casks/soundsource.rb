560bbd6f9b88cc19a8f2f0ca78ee845b40f0b807
Julian Mayer <julian@corecode.io>
2018-09-21 00:48:03 +0200

    Update soundsource to 3.1.1

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,6 +1,6 @@
 cask 'soundsource' do
-  version '3.1.0'
-  sha256 '7c0cf8add4e882a3f0062a433a41d96f5e0f7fae08b6382cffe44c55bb774996'
+  version '3.1.1'
+  sha256 'c8e916361ab864c81c8cae212c93598d79a9ad40ac1c53c0a4a8d10c9ce85cab'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
   appcast 'https://rogueamoeba.com/soundsource/releasenotes.php'
