868725111ebc4c6299ef290adb7a2d4a0a25ba97
CoreCode <core-code@users.noreply.github.com>
2018-09-20 21:53:29 +0200

    Update navicat-for-sqlite to 12.1.8 (#52222)

--- Casks/navicat-for-sqlite.rb
+++ Casks/navicat-for-sqlite.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-sqlite' do
-  version '12.1.7'
-  sha256 '809d07c4a52faaf1f7c7d56b3e8bfa8f09daafd23ac48b7a7e88e53f957dc29b'
+  version '12.1.8'
+  sha256 '61c9f46cb8a015988d176345325327407419496243379fd605182471c7335dd4'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlite_en.dmg"
   appcast 'https://www.navicat.com/products/navicat-for-sqlite-release-note'
