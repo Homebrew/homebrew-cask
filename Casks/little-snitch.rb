81d37d7de8cb560a629d6a6bc0829ad9b143e221
Stefano Canducci <steno@fastmail.fm>
2018-09-22 00:10:03 +0200

    Update little-snitch to 4.2

--- Casks/little-snitch.rb
+++ Casks/little-snitch.rb
@@ -1,6 +1,6 @@
 cask 'little-snitch' do
-  version '4.1.3'
-  sha256 '350537d39974391008a35e609de3e4cb5e468c3aaed8fc70d3da2cac8c019e39'
+  version '4.2'
+  sha256 '4047f27b9d2979bed854c29e72949b15bb8f872c522383431bd8a3df95443cb9'
 
   url "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-#{version}.dmg"
   appcast 'https://www.obdev.at/products/littlesnitch/releasenotes.html'
