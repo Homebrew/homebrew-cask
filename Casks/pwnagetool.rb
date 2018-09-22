3d052930b19d6484a7d6fad361015cc6816ec623
Julian Mayer <julian@corecode.io>
2018-09-22 21:27:10 +0200

    Update pwnagetool to 5.1.1

--- Casks/pwnagetool.rb
+++ Casks/pwnagetool.rb
@@ -2,10 +2,9 @@ cask 'pwnagetool' do
   version '5.1.1'
   sha256 '84262734ad9f9186bce14a4f939d7ea290ed187782fdfa549a82c28bf837c808'
 
-  # sites.google.com/a/ipad-dev.com/files/pwnagetool was verified as official when first introduced to the cask
-  url "https://sites.google.com/a/ipad-dev.com/files/pwnagetool/PwnageTool_#{version}.dmg"
+  url "http://iphoneroot.com/download/PwnageTool_#{version}.dmg"
   name 'PwnageTool'
-  homepage 'http://blog.iphone-dev.org/tagged/PwnageTool'
+  homepage 'http://iphoneroot.com/utilities/#PwnageTool'
 
   app 'PwnageTool.app'
 end
