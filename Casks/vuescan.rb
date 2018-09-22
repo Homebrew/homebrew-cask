9f57090863eef9453c51bcf241b107462ca8733e
Vítor Galvão <info@vitorgalvao.com>
2018-09-22 17:38:46 +0100

    Update vuescan.rb (#52199)
    * Update vuescan.rb
    
    * Update vuescan.rb
    
    * Update vuescan.rb

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,8 +1,8 @@
 cask 'vuescan' do
-  version '9.6.16'
-  sha256 'b25a8f9af06ed10365370f04ba12072805f5b48fbb8cf3694707fc8c09df86b9'
+  version '9.6'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
+  url "https://www.hamrick.com/files/vuex64#{version.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
   name 'VueScan'
   homepage 'https://www.hamrick.com/'
