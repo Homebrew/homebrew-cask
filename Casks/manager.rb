8af959cce2076cf294f2c09a5044d84a125d48c6
CoreCode <core-code@users.noreply.github.com>
2018-09-23 02:34:59 +0200

    Update manager to 18.9.75 (#52337)

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -1,6 +1,6 @@
 cask 'manager' do
-  version '18.9.71'
-  sha256 '95cd0bc8e2b824b8e4487b853548dad7a10503c36d05b16c32dc190ff6704c7c'
+  version '18.9.75'
+  sha256 '7a171510203087eb720bddbf317c15df7d8b57e25a539b93810f8fb484a5e595'
 
   # d2ap5zrlkavzl7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2ap5zrlkavzl7.cloudfront.net/#{version}/Manager.dmg"
