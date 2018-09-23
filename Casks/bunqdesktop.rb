fee27399b89d1ba8024cc6d8a7227a0c82a530a0
Gregory Goijaerts <crecketgaming@gmail.com>
2018-09-23 23:54:22 +0200

    Fixed capitalization issues

--- Casks/bunqdesktop.rb
+++ Casks/bunqdesktop.rb
@@ -2,9 +2,9 @@ cask 'bunqdesktop' do
   version '0.9.0'
   sha256 '200479ac81f4944f71c8fb10491ed258672716749aaf9d9665bedb963092f0ba'
 
-  # github.com/BunqCommunity/BunqDesktop was verified as official when first introduced to the cask
-  url "https://github.com/BunqCommunity/bunqDesktop/releases/download/#{version}/bunqDesktop-#{version}.dmg"
-  appcast 'https://github.com/BunqCommunity/bunqDesktop/releases.atom'
+  # github.com/bunqCommunity/bunqDesktop was verified as official when first introduced to the cask
+  url "https://github.com/bunqCommunity/bunqDesktop/releases/download/#{version}/bunqDesktop-#{version}.dmg"
+  appcast 'https://github.com/bunqCommunity/bunqDesktop/releases.atom'
   name 'bunqDesktop'
   homepage 'https://bunqdesk.top/'
 
