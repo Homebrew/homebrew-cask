aa4b8da1937cf17b8f2925490eb2a0fb04bcabf5
Sebastian Andil <sebastian.andil@gmail.com>
2018-09-23 21:41:28 +0200

    Update graphql-playground to 1.7.0

--- Casks/graphql-playground.rb
+++ Casks/graphql-playground.rb
@@ -1,6 +1,6 @@
 cask 'graphql-playground' do
-  version '1.6.3'
-  sha256 '6852aadc02bd1ae208953ddc9947164b4c3037029b9b00a635198ff7df99e8aa'
+  version '1.7.0'
+  sha256 '61d3dc658a885b21103f522afa18194deca0752202ccd6ca02e9d630869ce79f'
 
   url "https://github.com/prisma/graphql-playground/releases/download/v#{version}/graphql-playground-electron-#{version}.dmg"
   appcast 'https://github.com/prisma/graphql-playground/releases.atom'
