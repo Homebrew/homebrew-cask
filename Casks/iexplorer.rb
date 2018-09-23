ea9c29bc82477807942c23056b38c4f3dd757352
brigitte zmoelnig <brigitte@corecode.io>
2018-09-23 20:37:51 +0200

    Update iexplorer to 4.2.3

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -1,6 +1,6 @@
 cask 'iexplorer' do
-  version '4.2.2'
-  sha256 'c95d14c02107ae1d3e5ed633d649841759dd2905d9159246d116f2f38af21a16'
+  version '4.2.3'
+  sha256 'bf4e41861f0dc0cdffb07d3369c37e422b3572602e35d4e7fb5ee8cea6fec803'
 
   url "https://assets.macroplant.com/downloads/iExplorer-#{version}.dmg"
   name 'iExplorer'
