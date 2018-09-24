a90cbbe71f9354096419a88e5ad64af1d92066e4
Gregory Goijaerts <crecketgaming@gmail.com>
2018-09-24 14:13:21 +0200

    Rename bunqdesktop to bunqcommunity-bunq

--- Casks/bunqcommunity-bunq.rb
+++ Casks/bunqcommunity-bunq.rb
@@ -1,12 +1,12 @@
-cask 'bunqdesktop' do
+cask 'bunqcommunity-bunq' do
   version '0.9.0'
   sha256 '200479ac81f4944f71c8fb10491ed258672716749aaf9d9665bedb963092f0ba'
 
   # github.com/bunqCommunity/bunqDesktop was verified as official when first introduced to the cask
   url "https://github.com/bunqCommunity/bunqDesktop/releases/download/#{version}/bunqDesktop-#{version}.dmg"
   appcast 'https://github.com/bunqCommunity/bunqDesktop/releases.atom'
-  name 'bunqDesktop'
+  name 'bunqcommunity-bunq'
   homepage 'https://bunqdesk.top/'
 
-  app 'bunqDesktop.app'
+  app 'bunqcommunity-bunq.app'
 end
