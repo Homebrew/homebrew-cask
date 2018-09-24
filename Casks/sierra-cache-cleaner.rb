72378588e225f28ed7db324fafe707a7fb0d8926
Julian Mayer <julian@corecode.io>
2018-09-24 19:32:12 +0200

    Update sierra-cache-cleaner to latest

--- Casks/sierra-cache-cleaner.rb
+++ Casks/sierra-cache-cleaner.rb
@@ -2,14 +2,14 @@ cask 'sierra-cache-cleaner' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.northernsoftworks.com/downloads/scc.dmg'
-  name 'Sierra Cache Cleaner'
-  name 'SCC'
-  homepage 'http://www.northernsoftworks.com/sierracachecleaner.html'
+  url 'http://www.northernsoftworks.com/downloads/mcc.dmg'
+  name 'Mojave Cache Cleaner'
+  name 'MCC'
+  homepage 'http://www.northernsoftworks.com/mojavecachecleaner.html'
 
   if MacOS.version <= :mountain_lion
-    app 'Sierra Cache Cleaner Legacy.app'
+    app 'Mojave Cache Cleaner Legacy.app'
   else
-    app 'Sierra Cache Cleaner.app'
+    app 'Mojave Cache Cleaner.app'
   end
 end
