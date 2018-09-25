df5b8577aebda82b72817e9937be014380ab49d3
Ryan Adam <2746755+rfadam@users.noreply.github.com>
2018-09-25 23:16:29 +0100

    Update photosync to 3.3

--- Casks/photosync.rb
+++ Casks/photosync.rb
@@ -1,6 +1,6 @@
 cask 'photosync' do
-  version '3.2.4'
-  sha256 'da491ac3b2de9ae526075d43074af20f5586359271cedfdfc047beb689184f18'
+  version '3.3'
+  sha256 '83c068387fabc3b6d59d693231b7975ff5365103eb01a9d8ab0f1d7c43be8b06'
 
   url "https://www.photosync-app.com/tl_files/photosync/publish/mac/photosync_#{version.dots_to_underscores}.dmg"
   appcast 'https://download.photosync-app.com/xml/photosyncmac-updates-standalone.xml'
