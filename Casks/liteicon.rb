d71425ce7bfb363c8b7afec81f22bdb02cce6174
Anver Housseini <contact@anver.ch>
2018-09-25 23:49:28 +0200

    Update liteicon to 3.9 (#52451)

--- Casks/liteicon.rb
+++ Casks/liteicon.rb
@@ -3,8 +3,8 @@ cask 'liteicon' do
     version '3.7.1'
     sha256 'b457521a698a0ef55cd3d9c044c82c28984eeebc20d8baf05a9c21b0fa1df432'
   else
-    version '3.8.2'
-    sha256 'ede528b762f4f979b488973db002442260c6eda36d4875655887956e62576e62'
+    version '3.9'
+    sha256 'd185503d1c6cbbc6f770517853bd9ef08dc620f4e7ce3de913251a57e4d450d9'
   end
 
   url "https://www.freemacsoft.net/downloads/LiteIcon_#{version}.zip"
