d4bc48035138b1615c2502ef654795f68919b0fb
Jesse Claven <jesse.claven@gmail.com>
2018-09-25 21:22:26 +0200

    Specify specific version URL for Plug

--- Casks/plug.rb
+++ Casks/plug.rb
@@ -6,8 +6,8 @@ cask 'plug' do
   else
     version '2.0.19'
     sha256 '418d1adcf0ca099e7d02f9773df133c8c3276f1e65c47ff60f587861fa07d063'
-    url "https://www.plugformac.com/updates/plug#{version.major}/Plug-latest.dmg"
-    appcast "https://www.plugformac.com/updates/plug#{version.major}/sparklecast.xml"
+    url 'https://www.plugformac.com/updates/plug2/Plug-2067.dmg'
+    appcast 'https://www.plugformac.com/updates/plug#{version.major}/sparklecast.xml'
   end
 
   name 'Plug'
