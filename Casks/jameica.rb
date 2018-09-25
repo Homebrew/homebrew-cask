b3b2d56d49087cfe7290002ca7e228abcb4b7f4b
tiiiecherle <tiiiecherle@gmail.com>
2018-09-25 23:34:52 +0200

    Update jameica to 2.8.2 (#52404)

--- Casks/jameica.rb
+++ Casks/jameica.rb
@@ -1,6 +1,6 @@
 cask 'jameica' do
-  version '2.8.1'
-  sha256 'aff01580063d62548ab33994bf9ab8e74b7615c19ec9841a515f02a546a86b02'
+  version '2.8.2'
+  sha256 '055833018c04c7d1ca5617db4472c5bbde256b6a789769d2c8ef6648e3b88312'
 
   url "https://www.willuhn.de/products/jameica/releases/current/jameica/jameica-macos64-#{version}.zip"
   name 'Jameica'
