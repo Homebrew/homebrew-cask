77cf44fd908c409ef592d3e72ac0e7d55619d809
Rajiv Shah <rajivshah1@icloud.com>
2018-09-26 16:06:39 -0400

    Add Trinity.app v0.3.5 (#52511)
    * Add Trinity.app v0.3.5
    
    * Update and rename trinity-wallet.rb to trinity.rb

---
+++ Casks/trinity.rb
@@ -0,0 +1,16 @@
+cask 'trinity' do
+  version '0.3.5'
+  sha256 '20afa7563294ddef8e5bdbcc8527417341bb3e356cb9c8edddcbdf0b824037da'
+
+  # github.com/iotaledger/trinity-wallet was verified as official when first introduced to the cask
+  url "https://github.com/iotaledger/trinity-wallet/releases/download/#{version}/trinity-desktop-#{version}.dmg"
+  appcast 'https://github.com/iotaledger/trinity-wallet/releases.atom'
+  name 'IOTA Trinity Wallet'
+  homepage 'https://trinity.iota.org/'
+
+  app 'Trinity.app'
+
+  uninstall quit: 'org.iota.trinity'
+
+  zap trash: '~/Library/Application Support/Trinity'
+end
