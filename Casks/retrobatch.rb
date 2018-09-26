7e867546540fe3c0516fb36aa7a98c673af95bd9
Miha Filej <miha@filej.net>
2018-09-26 18:36:46 +0200

    Add Retrobatch.app v1.1
    Retrobatch is a flexible, super charged, batch image processing app by
    Flying Meat Inc.

---
+++ Casks/retrobatch.rb
@@ -0,0 +1,10 @@
+cask 'retrobatch' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://flyingmeat.com/download/Retrobatch.zip'
+  name 'Retrobatch'
+  homepage 'https://flyingmeat.com/retrobatch/'
+
+  app 'Retrobatch.app'
+end
