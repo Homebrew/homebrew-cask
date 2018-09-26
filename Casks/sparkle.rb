c0d0b96886de375de4cb46dfa567f15f63062af5
brigitte zmoelnig <brigitte@corecode.io>
2018-09-26 12:12:59 +0200

    Update sparkle to 1.20.0

--- Casks/sparkle.rb
+++ Casks/sparkle.rb
@@ -1,6 +1,6 @@
 cask 'sparkle' do
-  version '1.19.0'
-  sha256 '56541c5ddd0859a96ba3d2c708ca734088ccd674a2fea45cf1a1aa405bb26ad3'
+  version '1.20.0'
+  sha256 '6608a56ff95d714b2a18bda8022f7d846d57601150b8c6e077b0936f74b1c6a8'
 
   # github.com/sparkle-project/Sparkle was verified as official when first introduced to the cask
   url "https://github.com/sparkle-project/Sparkle/releases/download/#{version}/Sparkle-#{version}.tar.bz2"
