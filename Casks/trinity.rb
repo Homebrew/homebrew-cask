dd2a10a666f10bc7383f331b81b44c90429f8aa1
Vítor Galvão <info@vitorgalvao.com>
2018-09-26 21:01:58 +0100

    Update and rename trinity-wallet.rb to trinity.rb

--- Casks/trinity.rb
+++ Casks/trinity.rb
@@ -1,20 +1,16 @@
-cask 'trinity-wallet' do
+cask 'trinity' do
   version '0.3.5'
   sha256 '20afa7563294ddef8e5bdbcc8527417341bb3e356cb9c8edddcbdf0b824037da'
 
   # github.com/iotaledger/trinity-wallet was verified as official when first introduced to the cask
-  url 'https://github.com/iotaledger/trinity-wallet/releases/download/0.3.5/trinity-desktop-0.3.5.dmg'
+  url "https://github.com/iotaledger/trinity-wallet/releases/download/#{version}/trinity-desktop-#{version}.dmg"
   appcast 'https://github.com/iotaledger/trinity-wallet/releases.atom'
-  name 'Trinity Wallet'
-  name 'Trinity Desktop'
   name 'IOTA Trinity Wallet'
   homepage 'https://trinity.iota.org/'
 
   app 'Trinity.app'
 
   uninstall quit: 'org.iota.trinity'
-  uninstall trash: [
-                     '/Applications/Trinity.app',
-                     '~/Library/Application Support/Trinity',
-                   ]
+
+  zap trash: '~/Library/Application Support/Trinity'
 end
