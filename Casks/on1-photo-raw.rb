decea48797315b430ee74bc34441ca95b831bf22
Igor Shishkin <me@teran.ru>
2018-09-28 02:18:58 +0300

    Add On1 Photo Raw 2018
    Signed-off-by: Igor Shishkin <me@teran.ru>

---
+++ Casks/on1-photo-raw.rb
@@ -0,0 +1,16 @@
+cask 'on1-photo-raw' do
+  version '2018'
+  sha256 'a1fb8465877bb27c600a8dfeebba62da6e40e70f5ddd56e2b1bdfb6833e46eb5'
+
+  # ononesoft.cachefly.net was verified as official when first introduced to the cask
+  url "http://ononesoft.cachefly.net/photoraw#{version}/mac/gm_5757/ON1_Photo_RAW_#{version}.dmg"
+  name 'On1 Photo RAW'
+  homepage 'https://www.on1.com/products/photo-raw/'
+
+  pkg "ON1 Photo RAW #{version}.pkg"
+
+  uninstall script: {
+                      executable: "/Library/Application Support/ON1/Remove ON1 Photo RAW #{version}.app/Contents/MacOS/applet",
+                      sudo:       true,
+                    }
+end
