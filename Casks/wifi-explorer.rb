a68f8b37171feff2cd21dfdb10f0ca2c178cac9f
Jorden Kreps <jakereps@users.noreply.github.com>
2018-09-27 01:13:11 -0700

    Update wifi-explorer to 2.5.1 (#52538)

--- Casks/wifi-explorer.rb
+++ Casks/wifi-explorer.rb
@@ -1,6 +1,6 @@
 cask 'wifi-explorer' do
-  version '2.5'
-  sha256 '28994dc73e78440544e0340f13eedea20eb6097763a3fdb44ef272d4a969fc43'
+  version '2.5.1'
+  sha256 '305c86f339dae6afa18bc788ba4cb2fba067c97aed88888c31cd7475ac61b87d'
 
   url 'https://www.adriangranados.com/downloads/wifiexplorer.zip'
   appcast 'https://www.adriangranados.com/appcasts/wifiexplorercast.xml'
