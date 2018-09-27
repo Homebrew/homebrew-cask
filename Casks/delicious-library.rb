2c48e40484baf27b5527a173634085fffa4eeb1e
CoreCode <core-code@users.noreply.github.com>
2018-09-27 13:41:13 +0200

    Update delicious-library to 3.8.2 (#52506)

--- Casks/delicious-library.rb
+++ Casks/delicious-library.rb
@@ -1,6 +1,6 @@
 cask 'delicious-library' do
-  version '3.7.2'
-  sha256 '63aeb949ff185823d88260902dc3e246df7e5d133067fa1383f5710b40872d2c'
+  version '3.8.2'
+  sha256 'd3118edde50f9d50c0e30d76870d6a412fcdd218562c031c64eec2f04b5a8ed0'
 
   url "https://delicious-monster.com/downloads/DeliciousLibrary#{version.major}/v#{version}/DeliciousLibrary#{version.major}.zip"
   appcast "https://www.delicious-monster.com/downloads/DeliciousLibrary#{version.major}.xml"
