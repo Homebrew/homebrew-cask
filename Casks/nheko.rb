5a7a7007063dfbc1639752c7c03b12d625864efb
commitay <commitay@users.noreply.github.com>
2018-09-27 12:36:02 +1000

    Update nheko.rb

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -2,7 +2,6 @@ cask 'nheko' do
   version '0.6.1'
   sha256 'caef9bd13e7177453ba8b49b22b64e98c6dba8ce8ab1d28c437514f2e13f381c'
 
-  # Official releases are done through GitHub releases from version 0.6.x
   url "https://github.com/mujx/nheko/releases/download/v#{version}/nheko-v#{version}.dmg"
   appcast 'https://github.com/mujx/nheko/releases.atom'
   name 'Nheko'
