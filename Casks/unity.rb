8640b9493a1646232f8fb4e42021802978a48018
Nick Badal <me@nbad.al>
2018-09-26 19:12:02 -0700

    Update unity to 2018.2.10f1,674aa5a67ed5 (#52528)

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask 'unity' do
-  version '2018.2.9f1,2207421190e9'
-  sha256 '5c5461ba90864b868ec69ef576f46cd1fe59f0500aff2adb908fdbdbe802c4b8'
+  version '2018.2.10f1,674aa5a67ed5'
+  sha256 'b9dcb10bcdbe63a3d286e68a2a2bb5cf82a3c72bf5354bb7303aa7010d5fdb52'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity.pkg"
   appcast 'https://unity3d.com/get-unity/download/archive'
