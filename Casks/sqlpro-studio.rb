b47ee1e36f2b193c37b5153e3fbd8aac4d61d77d
Eric Nielsen <eric@amalgamar.com.br>
2018-09-28 14:57:28 -0500

    Update sqlpro-studio to 1.0.302 (#52562)

--- Casks/sqlpro-studio.rb
+++ Casks/sqlpro-studio.rb
@@ -1,6 +1,6 @@
 cask 'sqlpro-studio' do
-  version '1.0.178'
-  sha256 '3385f49d2175a4385b08b49703e53c0645ac4d040e518e057f9531bef9d12649'
+  version '1.0.302'
+  sha256 '6e042b5b7db65752b2663711de8e2e2ee348871e66ac414a2b5eab77db90b74e'
 
   # d3fwkemdw8spx3.cloudfront.net/studio was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/studio/SQLProStudio.#{version}.app.zip"
