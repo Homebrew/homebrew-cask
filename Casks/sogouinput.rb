17f6f55fc6778d2a1e10ec44f6644ddbc8171a5a
Jack Rivers <ritksm@gmail.com>
2018-09-29 22:40:50 +0800

    Update sogouinput to 48a,1535352534
    this update fix performance issues in macos mojave
    and download file type changed from dmg to zip

--- Casks/sogouinput.rb
+++ Casks/sogouinput.rb
@@ -1,13 +1,13 @@
 cask 'sogouinput' do
-  version '46b,1519980135'
-  sha256 '63c8d2fef4d54ef5c72dffd573c0be89efa518d94373b88debb30cb7ac97191b'
+  version '48a,1535352534'
+  sha256 '8699b3e9080b7aaee2a19950b40e86a3f54b7ff8b09308aba116d26643bc63be'
 
-  url "http://cdn2.ime.sogou.com/dl/index/#{version.after_comma}/sogou_mac_#{version.before_comma}.dmg"
+  url "http://cdn2.ime.sogou.com/dl/index/#{version.after_comma}/sogou_mac_#{version.before_comma}.zip"
   name 'Sogou Input Method'
   name '搜狗输入法'
   homepage 'https://pinyin.sogou.com/mac/'
 
-  installer manual: '安装搜狗输入法.app'
+  installer manual: 'sogou_mac_48a.app'
 
   uninstall delete:    [
                          '/Library/Input Methods/SogouInput.app',
