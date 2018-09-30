c0c7650d3d24aaf4302d6b41359ad1e7af6f1778
Theodore Kokkoris <t.kokkoris@gmail.com>
2018-09-30 16:36:20 +0100

    Update joplin to 1.0.110

--- Casks/joplin.rb
+++ Casks/joplin.rb
@@ -1,6 +1,6 @@
 cask 'joplin' do
-  version '1.0.109'
-  sha256 'f2a64a9ea97ff17c7fc0facff9669b509a3227ddf2bd8f38680415c3ccec0916'
+  version '1.0.110'
+  sha256 '3527ef7824caa91542169239a5e0a2c785ca4137db38d343c3b2a501dcf4dd6c'
 
   # github.com/laurent22/joplin was verified as official when first introduced to the cask
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}.dmg"
