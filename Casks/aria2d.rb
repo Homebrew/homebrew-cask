e79b16a71e9809889f1952c797d344d939e13558
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@ProtonMail.com>
2018-10-01 13:52:05 +0800

    Upgrade Aria2D to Latest The author stopped offering download for previous versions, instead, now he provides a general url. So here I'm updating for this.

--- Casks/aria2d.rb
+++ Casks/aria2d.rb
@@ -1,9 +1,9 @@
 cask 'aria2d' do
-  version '1.2.1,1535339003'
-  sha256 '8338d48fa8c90c8f785568b7b16ca86613b6a8c64d107933a1bc72d6aa401e25'
+  version :latest
+  sha256 :no_check
 
   # dl.devmate.com/com.xjbeta.Aria2D was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.xjbeta.Aria2D/#{version.before_comma}/#{version.after_comma}/Aria2D-#{version.before_comma}.zip"
+  url 'https://dl.devmate.com/com.xjbeta.Aria2D/Aria2D.zip'
   appcast 'https://updates.devmate.com/com.xjbeta.Aria2D.xml'
   name 'Aria2D'
   homepage 'https://github.com/xjbeta/Aria2D'
