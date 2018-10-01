929a627a9c94402b79190b6bbac25007a14c93da
CoreCode <core-code@users.noreply.github.com>
2018-10-01 02:08:34 +0200

    Update xamarin-profiler to 1.6.4-20 (#52610)

--- Casks/xamarin-profiler.rb
+++ Casks/xamarin-profiler.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-profiler' do
-  version '1.6.4-16'
-  sha256 'fd9b384d63f63b9773cd15dcb8713177da8c5d59c8e07159424090147dc3a983'
+  version '1.6.4-20'
+  sha256 '16948ec6b3b93322b3439f5d12c0cc31f29c320dd3d244d5f0ec3c5189aa870b'
 
   url "https://dl.xamarin.com/profiler/profiler-mac-#{version}.pkg"
   appcast 'https://developer.xamarin.com/releases/profiler/'
