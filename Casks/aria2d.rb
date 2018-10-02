2b0f0837472481e960f3ddd1c87305c068023ab0
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@ProtonMail.com>
2018-10-02 10:23:45 +0800

    Upgrade Aria2D to 1.2.4

--- Casks/aria2d.rb
+++ Casks/aria2d.rb
@@ -1,9 +1,9 @@
 cask 'aria2d' do
-  version :latest
-  sha256 :no_check
+  version '1.2.4,1538136711'
+  sha256 'b13a5ca5897c3b6614e1ff974cf43f6293db91697cdb4bec4eb8b31ab4d1d527'
 
   # dl.devmate.com/com.xjbeta.Aria2D was verified as official when first introduced to the cask
-  url 'https://dl.devmate.com/com.xjbeta.Aria2D/Aria2D.zip'
+  url "https://dl.devmate.com/com.xjbeta.Aria2D/#{version.before_comma}/#{version.after_comma}/Aria2D-#{version.before_comma}.zip"
   appcast 'https://updates.devmate.com/com.xjbeta.Aria2D.xml'
   name 'Aria2D'
   homepage 'https://github.com/xjbeta/Aria2D'
