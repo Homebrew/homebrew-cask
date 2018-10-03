af04f74d15b9f53bb0f6d860ffbeb125fd2bf73a
Anders Damsgaard <anders@adamsgaard.dk>
2018-10-03 14:26:39 +0200

    Update GnuCash version and checksum (#52735)

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,6 +1,6 @@
 cask 'gnucash' do
-  version '3.2-2'
-  sha256 'c443629a73d0ec95ca8bcdb57fae755ec7915d91d0838e297404395587c6237d'
+  version '3.3-1'
+  sha256 'c1d97818e9f34e5113d030b4d9a850a297cb2d2ffb871e7cb9b80d5cf3ff2336'
 
   # github.com/Gnucash/gnucash was verified as official when first introduced to the cask
   url "https://github.com/Gnucash/gnucash/releases/download/#{version.major_minor_patch}/Gnucash-Intel-#{version}.dmg"
