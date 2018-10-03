e722a3480a2d87901d2186a7d442525eaf923601
Nick Wind-McJetters <nick@elearningexperts.net>
2018-10-03 09:42:14 -0400

    Update harvest to 2.1.10

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -1,6 +1,6 @@
 cask 'harvest' do
-  version '2.1.9'
-  sha256 '5c1ca460705b7a473d955dffe28a3329707bf196c203b1653667e2a917368023'
+  version '2.1.10'
+  sha256 '4cfd6f30dc20e0b4467d87d736bb6896f703644e446c549e0a34ef35463e5652'
 
   url "https://www.getharvest.com/harvest/mac/Harvest.#{version}.zip"
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml'
