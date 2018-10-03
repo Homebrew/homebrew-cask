4d483e3e4f3ad4178802ea2ad3ad4d68651fddb8
Bryce Glover <RandomDSdevel@gmail.com>
2018-10-03 19:00:38 -0400

    Java 11+28:  Remove now–non-applicable DSL code.   (#52605)

--- Casks/java.rb
+++ Casks/java.rb
@@ -15,18 +15,6 @@ cask 'java' do
   pkg "JDK #{version.before_comma}.pkg"
 
   postflight do
-    system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string BundledApp', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Info.plist"],
-                   sudo: true
-    system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Info.plist"],
-                   sudo: true
-    system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string WebStart', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Info.plist"],
-                   sudo: true
-    system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string Applets', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Info.plist"],
-                   sudo: true
     system_command '/bin/ln',
                    args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home", '/Library/Java/Home'],
                    sudo: true
@@ -41,55 +29,15 @@ cask 'java' do
                    sudo: true
   end
 
-  uninstall pkgutil:   [
-                         "com.oracle.jdk-#{version.before_comma}",
-                         'com.oracle.jre',
-                       ],
-            launchctl: [
-                         'com.oracle.java.Helper-Tool',
-                         'com.oracle.java.Java-Updater',
-                       ],
-            quit:      [
-                         'com.oracle.java.Java-Updater',
-                         'net.java.openjdk.cmd', # Java Control Panel
-                       ],
-            delete:    [
-                         '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin',
-                         "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents",
-                         '/Library/PreferencePanes/JavaControlPanel.prefPane',
-                         '/Library/Java/Home',
-                         '/Library/Java/MacOS',
-                       ],
-            rmdir:     "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk"
-
-  zap trash: [
-               '/Library/Application Support/Oracle/Java',
-               '/Library/Preferences/com.oracle.java.Deployment.plist',
-               '/Library/Preferences/com.oracle.java.Helper-Tool.plist',
-               '~/Library/Application Support/Java/',
-               '~/Library/Application Support/Oracle/Java',
-               '~/Library/Caches/com.oracle.java.Java-Updater',
-               '~/Library/Caches/Oracle.MacJREInstaller',
-               '~/Library/Caches/net.java.openjdk.cmd',
-               '~/Library/Preferences/com.oracle.java.Java-Updater.plist',
-               '~/Library/Preferences/com.oracle.java.JavaAppletPlugin.plist',
-               '~/Library/Preferences/com.oracle.javadeployment.plist',
-             ],
-      rmdir: [
-               '/Library/Application Support/Oracle/',
-               '~/Library/Application Support/Oracle/',
-             ]
+  uninstall pkgutil: "com.oracle.jdk-#{version.before_comma}",
+            delete:  [
+                       "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents",
+                       '/Library/Java/Home',
+                       '/Library/Java/MacOS',
+                     ],
+            rmdir:   "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk"
 
   caveats do
     license 'https://www.oracle.com/technetwork/java/javase/terms/license/javase-license.html'
-    <<~EOS
-      This Cask makes minor modifications to the JRE to prevent issues with
-      packaged applications, as discussed here:
-
-        https://bugs.eclipse.org/bugs/show_bug.cgi?id=411361
-
-      If your Java application still asks for JRE installation, you might need
-      to reboot or logout/login.
-    EOS
   end
 end
