a307d88d49db528b3f665d81fba315e216b41f22
Vítor Galvão <info@vitorgalvao.com>
2018-10-03 13:38:22 +0100

    Update clockify.rb

--- Casks/clockify.rb
+++ Casks/clockify.rb
@@ -1,14 +1,10 @@
 cask 'clockify' do
-  version '2.1.6'
+  version '2.1.6_35'
   sha256 'adf4e56de1b57664b6045cd157032b0a07c7b9836273e6689632851b0f6c92fc'
 
-  # drive.google.com was verified as official when first introduced to the cask
-  url 'https://drive.google.com/uc?export=download&id=1kPzQGOveprS_JFlQJG8wd0IYh-trmj1o'
+  url "https://clockify.me/downloads/ClockifyDesktop_#{version.no_dots}.zip"
   name 'Clockify'
   homepage 'https://clockify.me/mac-time-tracking'
-
-  depends_on macos: '>= 10.8'
-  container nested: 'Clockify Desktop.app'
 
   app 'Clockify Desktop.app'
 end
