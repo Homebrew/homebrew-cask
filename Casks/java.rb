c02425d0577eda56db18350cf58855bc396512e7
commitay <commitay@users.noreply.github.com>
2018-10-04 09:08:09 +1000

    java: rename

--- Casks/java.rb
+++ Casks/java.rb
@@ -1,43 +1,30 @@
 cask 'java' do
-  version '11,28:55eed80b163941c8885ad9298e6d786a'
-  sha256 'aa5fea6e2f009e63dd8a8d7f532104e6476195a49ee6dd0d2b11c64966d028cc'
+  version '11,28'
+  sha256 '6b969d2df6a9758d9458f5ba47939250e848dfba8b49e41c935cf210606b6d38'
 
-  url "http://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_osx-x64_bin.dmg",
-      cookies: {
-                 'oraclelicense' => 'accept-securebackup-cookie',
-               }
-  name 'Java Standard Edition Development Kit'
-  homepage 'https://www.oracle.com/technetwork/java/javase/overview/index.html'
-
-  # auto_updates true: JDK does not auto-update
-  depends_on macos: '>= :yosemite'
-
-  pkg "JDK #{version.before_comma}.pkg"
+  url "https://download.java.net/java/GA/jdk#{version.before_comma}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}+#{version.after_comma}_osx-x64_bin.tar.gz"
+  name 'OpenJDK'
+  homepage 'https://jdk.java.net/'
 
   postflight do
-    system_command '/bin/ln',
-                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home", '/Library/Java/Home'],
+    system_command '/bin/mv',
+                   args: ['-f', '--', "#{staged_path}/jdk-#{version.before_comma}.jdk",
+                          "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"],
                    sudo: true
-    system_command '/bin/ln',
-                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/MacOS", '/Library/Java/MacOS'],
-                   sudo: true
+
     system_command '/bin/mkdir',
-                   args: ['-p', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries"],
+                   args: ['-p', '--', "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries"],
                    sudo: true
+
     system_command '/bin/ln',
-                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib", "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"],
+                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib",
+                          "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"],
                    sudo: true
   end
 
-  uninstall pkgutil: "com.oracle.jdk-#{version.before_comma}",
-            delete:  [
-                       "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents",
-                       '/Library/Java/Home',
-                       '/Library/Java/MacOS',
-                     ],
-            rmdir:   "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk"
-
-  caveats do
-    license 'https://www.oracle.com/technetwork/java/javase/terms/license/javase-license.html'
+  uninstall_postflight do
+    system_command '/bin/rm',
+                   args: ['-rf', '--', "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"],
+                   sudo: true
   end
 end
