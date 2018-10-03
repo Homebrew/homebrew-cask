db212cc9c7bfe6ace568ed828d7725d4a27fe87b
Tim Hordern <tim.hordern@gmail.com>
2018-10-03 22:31:39 +1000

    Update ticktick to 2.7.51,81 (#52749)

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -1,6 +1,6 @@
 cask 'ticktick' do
-  version '2.7.50,77'
-  sha256 'a0ce491936bf4ea79f5111144555b13d1cb77bfa05d6e4413da7332b9326acf7'
+  version '2.7.51,81'
+  sha256 '69f9459bf2536f441ab94700fee54bd6b5c9c93b87ad5d15e8b518abdb02b39e'
 
   # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
