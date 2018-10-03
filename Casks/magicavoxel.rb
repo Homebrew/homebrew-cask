b4c445d1696e0130e5168c3de6bdce8589333d03
miccal <miccal.matthews@gmail.com>
2018-10-03 08:44:37 +0800

    Update magicavoxel.rb

--- Casks/magicavoxel.rb
+++ Casks/magicavoxel.rb
@@ -1,11 +1,11 @@
 cask 'magicavoxel' do
-  version '0.99.2'
+  version '0.99.2-alpha'
   sha256 'b83bbeebbcfb5f045b40d27266a805ec38536b1e54310db6592c2e33ce500089'
 
   # 192.241.207.218/uploads was verified as official when first introduced to the cask
-  url "http://192.241.207.218/uploads/MagicaVoxel-#{version}-alpha-mac.zip"
+  url "http://192.241.207.218/uploads/MagicaVoxel-#{version}-mac.zip"
   name 'MagicaVoxel'
   homepage 'https://ephtracy.github.io/'
 
-  suite "MagicaVoxel-#{version}-alpha-mac"
+  suite "MagicaVoxel-#{version}-mac"
 end
