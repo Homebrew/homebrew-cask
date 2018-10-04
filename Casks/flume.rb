c3c975efeb7a84f6104a6dc24c6f55621e942207
Ondřej Führer <ondrejfuhrer@users.noreply.github.com>
2018-10-04 02:48:14 +0200

    Update Flume to 2.8.5.3 (#52799)

--- Casks/flume.rb
+++ Casks/flume.rb
@@ -1,6 +1,6 @@
 cask 'flume' do
-  version '2.8.5.2'
-  sha256 'b33ed6d96467f2ccb4ddf212a6ddffb5846627e8ff6335ccb7a5055483977395'
+  version '2.8.5.3'
+  sha256 '8d42df3e1c948bb4ff0a87857b7cb780d190bbff452319f21f84bc7c6b241d87'
 
   url "https://flumeapp.com/files/Flume-#{version}.zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/c88c56b02dcd4dd3acceb6d7a24f7122'
