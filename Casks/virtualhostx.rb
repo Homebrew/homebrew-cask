57a4919aa86b31c23a56b302f1e8e2cf8822a266
CoreCode <core-code@users.noreply.github.com>
2018-10-05 00:19:55 +0200

    Update virtualhostx to 8.1.1,80_34 (#52850)

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -1,6 +1,6 @@
 cask 'virtualhostx' do
-  version '7.4.1,7_21'
-  sha256 '47b07d95f08011b0a74c8d18b3cf55d9a0e2effa9d17153c89eeca905a58df4e'
+  version '8.1.1,80_34'
+  sha256 '0a9f99f85118f1ce3f0192004b6524e7a3e62b88de3061d3969c1b94062ed84d'
 
   # downloads-clickonideas.netdna-ssl.com/virtualhostx was verified as official when first introduced to the cask
   url "https://downloads-clickonideas.netdna-ssl.com/virtualhostx/virtualhostx#{version.after_comma}.zip"
