6cb419e3dff50e9f82dd5943ceb2b1a154fb4338
Vítor Galvão <info@vitorgalvao.com>
2018-10-04 12:50:15 +0100

    Update understand.rb

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -2,7 +2,7 @@ cask 'understand' do
   version '5.0.962'
   sha256 '38dddc572b73bd15fda47c7deb7426f3022f1a47fb8b4018134c5ec518bf3478'
 
-  url "http://builds.scitools.com/all_builds/b#{version.minor}/Understand/Understand-#{version}-MacOSX-x86.dmg"
+  url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
   name 'SciTools Understand'
   homepage 'https://scitools.com/features/'
 
