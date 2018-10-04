e2db82d6c2780bd3d1faf6b0be2aac68df55432a
Vítor Galvão <info@vitorgalvao.com>
2018-10-04 12:54:35 +0100

    Update zeplin.rb

--- Casks/zeplin.rb
+++ Casks/zeplin.rb
@@ -1,5 +1,5 @@
 cask 'zeplin' do
-  version '2.1.1 (539)'
+  version '2.1.1,539'
   sha256 '1c6a91cb776010fec7872c8ec32904644a8f72d1889bd10135dbb20521609681'
 
   url 'https://api.zeplin.io/urls/download-mac'
