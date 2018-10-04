f5fdc20887579307f37cab0a5737ceff9384d42f
commitay <commitay@users.noreply.github.com>
2018-10-04 22:18:24 +1000

    Update clashx.rb

--- Casks/clashx.rb
+++ Casks/clashx.rb
@@ -3,10 +3,9 @@ cask 'clashx' do
   sha256 '00fabcc34e8cd70c720bace322fa2e10560280433eeb8cc650f36303372ed574'
 
   url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
+  appcast 'https://github.com/yichengchen/clashX/raw/master/appcast.xml'
   name 'ClashX'
   homepage 'https://github.com/yichengchen/clashX'
 
   app 'ClashX.app'
-
-  appcast 'https://github.com/yichengchen/clashX/raw/master/appcast.xml'
 end
