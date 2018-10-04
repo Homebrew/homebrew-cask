c4a96390aa0dd3e3fc0e8623d3dcc00197b7ac87
Andrew Mason <andrew.mason@n2pub.com>
2018-10-03 23:40:42 -0400

    re-add homepage

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -4,6 +4,7 @@ cask 'archipelago' do
 
   url 'https://archipelago-terminal.herokuapp.com/download/osx'
   name 'Archipelago'
+  homepage 'https://archipelago-terminal.herokuapp.com/download/osx'
 
   app 'Archipelago.app'
 end
