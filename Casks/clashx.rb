89baa41cb5a504bd5be4befc0b276ce3437bed19
Tong Zhao <tzhao11@illinois.edu>
2018-10-04 18:04:27 +0800

    Add ClashX.app v1.5.4

---
+++ Casks/clashx.rb
@@ -0,0 +1,12 @@
+cask 'clashx' do
+  version '1.5.4'
+  sha256 '00fabcc34e8cd70c720bace322fa2e10560280433eeb8cc650f36303372ed574'
+
+  url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
+  name 'ClashX'
+  homepage 'https://github.com/yichengchen/clashX'
+
+  app 'ClashX.app'
+
+  appcast 'https://github.com/yichengchen/clashX/raw/master/appcast.xml'
+end
