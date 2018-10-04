42c8171dbdd2ceac96393b3e1db6c5b810f44903
Julian DeMille <FireFaced@users.noreply.github.com>
2018-10-04 08:22:07 -0400

    Update dotnet-sdk to 2.1.403 (#52777)
    * Update dotnet-sdk to 2.1.403
    
    * Update download URL

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-sdk' do
-  version '2.1.402'
-  sha256 'f5b6f230395735a8c31cae7f01d43f61bd3227082d7bc3555a91cebcd0deb9db'
+  version '2.1.403'
+  sha256 'd290cefddb4fbdf1215724c049d86b4ce09f5dc2c5a658e3c1645c368f34c31a'
 
-  url "https://download.microsoft.com/download/8/A/7/8A765126-50CA-4C6F-890B-19AE47961E4B/dotnet-sdk-#{version}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/38102737-cb48-46c2-8f52-fb7102b50ae7/d81958d71c3c2679796e1ecfbd9cc903/dotnet-sdk-#{version}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
   name '.NET Core SDK'
   homepage 'https://www.microsoft.com/net/core#macos'
