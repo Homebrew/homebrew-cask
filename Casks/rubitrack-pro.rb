070bb0ecae5973c84c9838ea50f2851149d245b2
suschizu <brigitte@corecode.io>
2018-10-05 23:10:30 +0200

    Update rubitrack-pro to 5.1 (#52940)

--- Casks/rubitrack-pro.rb
+++ Casks/rubitrack-pro.rb
@@ -1,6 +1,6 @@
 cask 'rubitrack-pro' do
-  version '5.0.7'
-  sha256 '57d3f2989e9f0c4ccb97b8d8cf8c758edbdbd67ba233355b8b1bde94cc8701ff'
+  version '5.1'
+  sha256 '1b2096f3b6144430d34d7a93180a95cb849852fe51d3301d184f8934e78fd129'
 
   url "https://www.rubitrack.com/files/rubiTrack-#{version}.dmg"
   appcast "https://www.rubitrack.com/autoupdate/sparkle#{version.major}.xml"
