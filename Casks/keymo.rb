3b62d33582ca0da323cea5e68e88ac755d56922a
CoreCode <core-code@users.noreply.github.com>
2018-10-05 19:05:56 +0200

    Update keymo.rb

--- Casks/keymo.rb
+++ Casks/keymo.rb
@@ -2,14 +2,14 @@ cask 'keymo' do
   version '1.2.6'
   sha256 '5d9f74cfc915ea580b09b10db6e0170f310159e95d943910950bcc819819916c'
 
-  url 'https://manytricks.com/download/keymo'
-  appcast 'https://manytricks.com/keymo/appcast.xml'
-  name 'Keymo'
-  homepage 'https://manytricks.com/keymo/'
+  url 'https://manytricks.com/download/keymou'
+  appcast 'https://manytricks.com/keymou/appcast/'
+  name 'Keymou'
+  homepage 'https://manytricks.com/keymou/'
 
   auto_updates true
 
-  app 'Keymo.app'
+  app 'Keymou.app'
 
   zap trash: '~/Library/Preferences/com.manytricks.Keymo.plist'
 end
