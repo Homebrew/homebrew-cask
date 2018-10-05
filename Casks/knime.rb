864c25a2e7c1b3028165a468ccd5588c317bb158
Grega Vrbančič <grega.vrbancic@gmail.com>
2018-10-05 14:35:40 +0200

    Update knime to 3.6.0 (#52836)

--- Casks/knime.rb
+++ Casks/knime.rb
@@ -1,6 +1,6 @@
 cask 'knime' do
-  version '3.5.3'
-  sha256 '30cae5e96424e355a2c0a8715d62b5a989dcb59ffc70cff8ceecac52049cd856'
+  version '3.6.0'
+  sha256 '508b6a9b0530d04a3b3100ba1041378160d9b6065bd6a5775471614866c71e00'
 
   # download.knime.org/analytics-platform/macosx was verified as official when first introduced to the cask
   url "https://download.knime.org/analytics-platform/macosx/knime_#{version}.app.macosx.cocoa.x86_64.dmg"
