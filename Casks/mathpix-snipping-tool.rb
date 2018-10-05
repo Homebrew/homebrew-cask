3e293badf25cfd7ab1c287821ef6974f8cf2aa2e
Joseph Jin-Chuan Tang <jinntrance@gmail.com>
2018-10-06 05:34:12 +0800

    Add Mathpix 1.3.48 (#52542)
    * Add Mathpix 1.3.48
    
    * Update and rename mathpix.rb to mathpix-snipping-tool.rb

---
+++ Casks/mathpix-snipping-tool.rb
@@ -0,0 +1,10 @@
+cask 'mathpix-snipping-tool' do
+  version '1.3.48'
+  sha256 '5159384a19502bacbe2fb0bc3b6a5938e1900c1901b4d356655b7764a450998b'
+
+  url "https://mathpix.com/dmg/SnippingTool-v#{version}.dmg"
+  name 'Mathpix Snipping Tool'
+  homepage 'https://mathpix.com/'
+
+  app 'Mathpix Snipping Tool.app'
+end
