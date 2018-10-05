6f9bb0de21972d4b276abc9f4c51d5ee19996f60
404NetworkError <404NetworkError@users.noreply.github.com>
2018-10-05 11:12:45 -0500

    Update iterm2 to 3.2.3

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -1,7 +1,7 @@
 cask 'iterm2' do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  version '3.2.2'
-  sha256 'c97702eba2b943e800fd8417b780596c35cfccef2e579a6b6a6203efea40e745'
+  version '3.2.3'
+  sha256 '5ec9d0cbd085bc226fdce6d26b8fa7158c268e9555577264bf92994faf087a6f'
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
   appcast 'https://iterm2.com/appcasts/final.xml'
