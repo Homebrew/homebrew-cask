f19e410c9b7db698ec327487bbd2ae39a98fe865
brigitte zmoelnig <brigitte@corecode.io>
2018-10-05 23:30:47 +0200

    Update wifispoof to 3.4.1

--- Casks/wifispoof.rb
+++ Casks/wifispoof.rb
@@ -1,6 +1,6 @@
 cask 'wifispoof' do
-  version '3.3.2'
-  sha256 'c4c4f35686f42a45526fc4e6db5a7ddd693f2849b1c19c4239fa9ebb3db89117'
+  version '3.4.1'
+  sha256 '8c9d157877ecb397ef10d660b2fbf548efd3bd23bfe5ee3b43fc2ee86cdc176b'
 
   # sweetpproductions.com/products was verified as official when first introduced to the cask
   url "https://sweetpproductions.com/products/wifispoof#{version.major}/WiFiSpoof#{version.major}.dmg"
