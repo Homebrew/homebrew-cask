3a1b4fbdbb7b61b61674d4f83284cb4ae363cd37
Vítor Galvão <info@vitorgalvao.com>
2018-10-05 12:41:34 +0100

    Update archipelago.rb

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -3,6 +3,7 @@ cask 'archipelago' do
   sha256 :no_check
 
   url 'https://archipelago-terminal.herokuapp.com/download/osx'
+  appcast 'https://github.com/npezza93/archipelago/releases.atom'
   name 'Archipelago'
   homepage 'https://github.com/npezza93/archipelago'
 
