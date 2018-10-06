bc3cd29655ccb6955fa6e51fb9c5adabed1bf2d7
Roman Tsukanov <roman@tsukanov.me>
2018-10-05 23:07:55 -0700

    Add TrainerRoad

---
+++ Casks/trainerroad.rb
@@ -0,0 +1,19 @@
+cask 'trainerroad' do
+  version :latest
+  sha256 :no_check
+
+  # trainerroadcdn1.blob.core.windows.net was verified as official when first introduced to the cask
+  url 'https://trainerroadcdn1.blob.core.windows.net/mac/releases/production/TrainerRoad.pkg'
+  name 'TrainerRoad'
+  homepage 'https://www.trainerroad.com/download'
+
+  pkg 'TrainerRoad.pkg'
+
+  uninstall pkgutil: [
+                       'com.TrainerRoad.Mac',
+                     ]
+
+  zap trash: [
+               '~/Library/Application Support/TrainerRoad',
+             ]
+end
