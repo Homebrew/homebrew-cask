9dc3048f4613af18fb471681cad8ea6e3dacbff7
CoreCode <core-code@users.noreply.github.com>
2018-10-07 00:07:43 +0200

    Update dawnlabs-alchemy.rb

--- Casks/dawnlabs-alchemy.rb
+++ Casks/dawnlabs-alchemy.rb
@@ -1,6 +1,6 @@
 cask 'dawnlabs-alchemy' do
   version '0.5.1'
-  sha256 'c49f55f17f155a5e7c5241d364a927f0e9375cf51c11bc24238acf0108c9f958'
+  sha256 '12209d471d220a14e57ff0407278a4d9e8274ab0a116cfd2b4479297c26f666b'
 
   url "https://github.com/dawnlabs/alchemy/releases/download/#{version}/Alchemy-mac.zip"
   appcast 'https://github.com/dawnlabs/alchemy/releases.atom'
