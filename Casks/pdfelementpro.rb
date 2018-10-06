b9595a67492b32814841c11bba73d04ebc5bf68b
Julian Mayer <julian@corecode.io>
2018-10-06 20:04:41 +0200

    Update pdfelementpro to 6.7.5,2992

--- Casks/pdfelementpro.rb
+++ Casks/pdfelementpro.rb
@@ -1,6 +1,6 @@
 cask 'pdfelementpro' do
-  version '6.6.1,2992'
-  sha256 '1b4ea505e82cd01f5aacb0e7501d0999cbdf28159eb3874f1710041b5523a40b'
+  version '6.7.5,2992'
+  sha256 'e1c968b2f03af19ebc18dd0d2dbd002e70eedc0ed5b956cc17a0a2d32ddcebff'
 
   url "http://download.wondershare.com/cbs_down/mac-pdfelement#{version.major}-pro_full#{version.after_comma}.dmg"
   name 'Wondershare PDFelement Professional for Mac'
