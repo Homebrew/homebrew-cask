ae296d8e55188f50b5abe359e7c854bf3fe16b74
Victor Popkov <victor@popkov.me>
2018-10-06 04:07:58 +0300

    Update retrobatch: add version and appcast

--- Casks/retrobatch.rb
+++ Casks/retrobatch.rb
@@ -1,10 +1,13 @@
 cask 'retrobatch' do
-  version :latest
-  sha256 :no_check
+  version '1.1'
+  sha256 '22da92a29b4ba4e7cecc3e381b1bf3bc1f753b594ea62bcc5dc507892977d552'
 
   url 'https://flyingmeat.com/download/Retrobatch.zip'
+  appcast "https://www.flyingmeat.com/download/retrobatch#{version.major}update.xml"
   name 'Retrobatch'
   homepage 'https://flyingmeat.com/retrobatch/'
+
+  auto_updates true
 
   app 'Retrobatch.app'
 end
