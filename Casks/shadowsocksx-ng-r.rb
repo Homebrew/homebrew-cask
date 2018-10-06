02f6718eef33b363406d153d9e21bc7409eda75a
Victor Popkov <victor@popkov.me>
2018-10-06 19:33:52 +0300

    Update shadowsocksx-ng-r: add postflight

--- Casks/shadowsocksx-ng-r.rb
+++ Casks/shadowsocksx-ng-r.rb
@@ -12,6 +12,10 @@ cask 'shadowsocksx-ng-r' do
 
   app 'ShadowsocksX-NG-R8.app'
 
+  postflight do
+    system_command "#{appdir}/ShadowsocksX-NG-R8.app/Contents/Resources/install_helper.sh"
+  end
+
   uninstall delete:    '/Library/Application Support/ShadowsocksX-NG',
             launchctl: [
                          'com.qiuyuzhou.shadowsocksX-NG.http',
