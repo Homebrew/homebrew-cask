81e4baffed28d7e91f116ecdc19ab516d28d5447
Michael Piffret <michael.piffret@gmail.com>
2018-10-06 17:15:35 +0200

    Update qutebrowser to 1.5.0 (#52971)

--- Casks/qutebrowser.rb
+++ Casks/qutebrowser.rb
@@ -1,6 +1,6 @@
 cask 'qutebrowser' do
-  version '1.4.1'
-  sha256 'c03ce9304d439b3a8c8dcd5bf5c193625c505759fa1ca8481345f9a26e2a7cd6'
+  version '1.5.0'
+  sha256 '8eba307812ac9c95e7cc1b211251a94fda8d07bfb20141a248e548af943411f2'
 
   # github.com/qutebrowser/qutebrowser was verified as official when first introduced to the cask
   url "https://github.com/qutebrowser/qutebrowser/releases/download/v#{version}/qutebrowser-#{version}.dmg"
