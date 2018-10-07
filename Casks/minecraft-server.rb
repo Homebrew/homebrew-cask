ccace69ef50bb5bc6c8d93bccbd09b84d73f6e8f
commitay <commitay@users.noreply.github.com>
2018-10-07 18:57:02 +1000

    Update minecraft-server.rb

--- Casks/minecraft-server.rb
+++ Casks/minecraft-server.rb
@@ -1,8 +1,10 @@
 cask 'minecraft-server' do
-  version '1.13.1'
+  version '1.13.1,fe123682e9cb30031eae351764f653500b7396c9'
   sha256 '2ea6047e7651c429228340acd7d1e35f4f6c7af42f59f92b0b1cd476561253d1'
 
-  url 'https://launcher.mojang.com/v1/objects/fe123682e9cb30031eae351764f653500b7396c9/server.jar'
+  # launcher.mojang.com was verified as official when first introduced to the cask
+  url "https://launcher.mojang.com/v#{version.major}/objects/#{version.after_comma}/server.jar"
+  appcast 'https://minecraft.net/en-us/download/server/'
   name 'Minecraft Server'
   homepage 'https://minecraft.net/'
 
