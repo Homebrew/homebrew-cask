a45c2c40a3e3fe77f8cf2d89a5da74579373cb35
Viktor Szakats <commit@vszakats.net>
2018-10-07 23:06:11 +1000

    Secure URLs

--- Casks/bitpost.rb
+++ Casks/bitpost.rb
@@ -2,8 +2,8 @@ cask 'bitpost' do
   version '0.9.8.3'
   sha256 '8e58ddf022eae721bfca6ecb456edf40baa23ea31076ee9c1ad75e947b0cea2b'
 
-  # voluntary.net.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "http://voluntary.net.s3.amazonaws.com/Bitpost.#{version}.zip"
+  # s3.amazonaws.com/voluntary.net was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/voluntary.net/Bitpost.#{version}.zip"
   appcast 'https://voluntary.net/bitpost/'
   name 'Bitpost'
   homepage 'https://voluntary.net/bitpost/'
