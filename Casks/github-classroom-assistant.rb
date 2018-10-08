4ebb3ee4b0077177f58e0b1719dd6512f71c9aa4
Konstantin Malanchev <hombit@gmail.com>
2018-10-08 15:51:48 +0300

    Add GitHub Classroom Assistant v1.0.1

---
+++ Casks/github-classroom-assistant.rb
@@ -0,0 +1,12 @@
+cask 'github-classroom-assistant' do
+  version '1.0.1'
+  sha256 'c6be6a373dc043b1760628d750a123195788bec23935b1c7dfa8cf7a1bf98976'
+
+  # github.com/education/classroom-assistant was verified as official when first introduced to the cask
+  url "https://github.com/education/classroom-assistant/releases/download/v#{version}/Classroom.Assistant-darwin-x64-#{version}.zip"
+  appcast 'https://github.com/education/classroom-assistant/releases.atom'
+  name 'GitHub Classroom Assistant'
+  homepage 'https://classroom.github.com/assistant'
+
+  app 'Classroom Assistant.app'
+end
