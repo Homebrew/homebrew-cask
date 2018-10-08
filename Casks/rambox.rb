0f9c218feeb75321b498aeea581d8c43221e7a2e
Raul Bernardeli <raul@raulbcs.com.br>
2018-10-08 17:29:16 -0300

    Update Rambox to v0.6.1

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -1,10 +1,10 @@
 cask 'rambox' do
-  version '0.5.17'
-  sha256 '530ce10f337261e77be3c954dae7fd3cdda4299a55585876ca7e4ac54a315e97'
+  version '0.6.1'
+  sha256 'aa9d20ecfef8a8c1f2c2e9c55917e447bb2cdb0b58870eb6abd00cd66b9089c2'
 
-  # github.com/saenzramiro/rambox was verified as official when first introduced to the cask
-  url "https://github.com/saenzramiro/rambox/releases/download/#{version}/Rambox-#{version}-mac.zip"
-  appcast 'https://github.com/saenzramiro/rambox/releases.atom'
+  # github.com/ramboxapp/community-edition was verified as official when first introduced to the cask
+  url "https://github.com/ramboxapp/community-edition/releases/download/#{version}/Rambox-#{version}-mac.zip"
+  appcast 'https://github.com/ramboxapp/community-edition/releases.atom'
   name 'Rambox'
   homepage 'https://rambox.pro/'
 
