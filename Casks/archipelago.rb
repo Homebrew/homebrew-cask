32cba9f4891a014b3cc2243cd6c0469a047bcbed
Andrew Mason <andrew.mason@n2pub.com>
2018-10-08 18:39:57 -0400

    fix sha for ci

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -1,6 +1,6 @@
 cask 'archipelago' do
   version '2.3.2'
-  sha256 'b8136372aba64f209acc4b3142004940120ef2038ef8b7562c44a2a60df8297a'
+  sha256 '8bcc196c925c2f2de97cdaf816fd0b63f11b03fc0131d277449956b03bbdbd25'
 
   url 'https://github.com/npezza93/archipelago/releases/tag/v2.3.2'
   name 'Archipelago'
