d76b1b80bf06d59f4cdb2234446db00334e90882
Dirk Weise <code@dirk-weise.de>
2018-10-09 17:12:31 +0200

    Add Faxbot Latest

---
+++ Casks/faxbot.rb
@@ -0,0 +1,10 @@
+cask 'faxbot' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.hosy.de/faxer/Faxer.zip'
+  name 'Faxbot'
+  homepage 'https://www.hosy.de/faxer/'
+
+  app 'Faxbot.app'
+end
