fdb574686b88867acac48fac785abcfb0c6b2f4a
Qi Zhang <singularitti@outlook.com>
2018-10-09 11:30:47 -0400

    Add cask Recents.app

---
+++ Casks/recents.rb
@@ -0,0 +1,10 @@
+cask 'recents' do
+  version '2.0.0'
+  sha256 '3d06bd577120758e4649916533eff3a7bb1a9bee2e6fc768416d5eb9439e1273'
+
+  url 'http://recentsapp.com/releases/Recents_latest.zip'
+  name 'Recents'
+  homepage 'http://recentsapp.com/'
+
+  app 'Recents.app'
+end
