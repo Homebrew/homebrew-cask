aceb76f8239351b49d1b9d7e0d616e909bd32860
Vítor Galvão <info@vitorgalvao.com>
2018-10-09 16:54:30 +0100

    Update recents.rb

--- Casks/recents.rb
+++ Casks/recents.rb
@@ -1,8 +1,10 @@
 cask 'recents' do
-  version '2.0.0'
+  version '2.0.0,2868'
   sha256 '3d06bd577120758e4649916533eff3a7bb1a9bee2e6fc768416d5eb9439e1273'
 
-  url 'http://recentsapp.com/releases/Recents_latest.zip'
+  # rink.hockeyapp.net/api/2/apps/74f5ee9ebf2d4be3b92a3e8766433b8b was verified as official when first introduced to the cask
+  url "https://rink.hockeyapp.net/api/2/apps/74f5ee9ebf2d4be3b92a3e8766433b8b/app_versions/#{version.after_comma}?format=zip"
+  appcast 'https://rink.hockeyapp.net/api/2/apps/74f5ee9ebf2d4be3b92a3e8766433b8b'
   name 'Recents'
   homepage 'http://recentsapp.com/'
 
