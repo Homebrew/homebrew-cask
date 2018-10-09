feaae2529b83810cf5d17775c2db5679d5850700
brigitte zmoelnig <brigitte@corecode.io>
2018-10-09 10:04:21 +0200

    Update cisdem-document-reader to 3.2.0

--- Casks/cisdem-document-reader.rb
+++ Casks/cisdem-document-reader.rb
@@ -1,5 +1,5 @@
 cask 'cisdem-document-reader' do
-  version '3.1.0'
+  version '3.2.0'
   sha256 '6ec4fc6e7eb5a5d7e2c39096bc04322e82a20eef264603bcd9730ea6a44c0cf8'
 
   url 'https://www.cisdem.com/download/cisdem-documentreader.dmg'
