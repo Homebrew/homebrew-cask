208f49490e044644d18f3b650d3cf04ec7e58775
suschizu <brigitte@corecode.io>
2018-10-09 16:08:01 +0200

    Update cocktail to 11.6.3 (#53112)

--- Casks/cocktail.rb
+++ Casks/cocktail.rb
@@ -36,8 +36,8 @@ cask 'cocktail' do
     url "https://www.maintain.se/downloads/sparkle/highsierra/Cocktail_#{version}.zip"
     appcast 'https://www.maintain.se/downloads/sparkle/highsierra/highsierra.xml'
   else
-    version '12.0.0.10'
-    sha256 'cd51763b12b44898877866a9683c3d4acc2ad984acb1c294dfe357e24d48dcfc'
+    version '12.0.1'
+    sha256 '6a00bae70a5befa7fb9467c1320e8818939e5f074deadd70177021fbf4694b07'
 
     url "https://www.maintain.se/downloads/sparkle/mojave/Cocktail_#{version}.zip"
     appcast 'https://www.maintain.se/downloads/sparkle/mojave/mojave.xml'
