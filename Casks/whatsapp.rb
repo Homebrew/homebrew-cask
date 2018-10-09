35c0b33bb259e983a37a6a5c0f4596c7af05700e
miccal <miccal.matthews@gmail.com>
2018-10-10 06:27:27 +0800

    Update whatsapp to 0.3.1071

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,6 +1,6 @@
 cask 'whatsapp' do
-  version '0.3.953'
-  sha256 '0640c6deae22958063231da5658dd81b9504704401b0c643996a4c41e820e3dd'
+  version '0.3.1071'
+  sha256 'a2630e7daa300ebe234df053e2ddfeb2de1a771da9d42d52372761507f544957'
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64'
