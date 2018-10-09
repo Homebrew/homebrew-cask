909e69287785b9ab16300af54e24fa0ab03ac99d
Radek Simko <radek.simko@gmail.com>
2018-10-09 15:13:32 +0100

    Add cask for Read&Write

---
+++ Casks/read-and-write.rb
@@ -0,0 +1,10 @@
+cask 'read-and-write' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://fastdownloads2.texthelp.com/readwritemac7/installers/read&write.dmg'
+  name 'Read&Write'
+  homepage 'https://www.texthelp.com/en-gb/products/read-write/'
+
+  app 'Read&Write.app'
+end
