d65aeee1aa6923c5e414f760f23fe29d712dfb08
RexTg <rexx0520@gmail.com>
2018-10-11 00:14:59 +0800

    Update fork to 1.0.71.1

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,6 +1,6 @@
 cask 'fork' do
-  version '1.0.70'
-  sha256 '9ff2628cc88a55397a6058172c718b2815f132f164bb02222eed85dfdab81ffb'
+  version '1.0.71.1'
+  sha256 'b5c40da3f38bef6a3961a977bc5dda1854c735fdd75fa00d8f8b95faea12b31f'
 
   url 'https://git-fork.com/update/files/Fork.dmg'
   appcast 'https://git-fork.com/update/feed.xml'
