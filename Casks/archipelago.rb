a52da57b8d10a0ce5d3ed43bc7d9a1bdd385d667
Andrew Mason <andrew.mason@n2pub.com>
2018-10-10 17:16:45 -0400

    fix ordering

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -3,8 +3,9 @@ cask 'archipelago' do
   sha256 'c2eda8f6e0e63d0592ba94dc3969ddbee592f8035e162c05be345e32873be3a6'
 
   url 'https://github.com/npezza93/archipelago/releases/download/v2.3.2/Archipelago-2.3.2.dmg'
+  appcast 'https://github.com/npezza93/archipelago/releases.atom'
   name 'Archipelago'
   homepage 'https://github.com/npezza93/archipelago'
-  appcast 'https://github.com/npezza93/archipelago/releases.atom'
+  
   app 'Archipelago.app'
 end
