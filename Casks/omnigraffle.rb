b253f463dd29db5460eefa08959a7a1dce547b63
Anver Housseini <contact@anver.ch>
2018-10-10 11:50:04 +0200

    Update omnigraffle to 7.9.1

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -4,8 +4,8 @@ cask 'omnigraffle' do
     sha256 'ab463ea6c12d49c4104d3814ac3280d0359072702d4751f5074f644fc79de0c6'
     url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniGraffle-#{version}.dmg"
   else
-    version '7.9'
-    sha256 '5fed5d99cc826e1f99654abf0ab4c4f0a079505cde69b388a4073348530058f0'
+    version '7.9.1'
+    sha256 '536cd872e2e772836300c7c5d9259371a30db569729ec2a85c9cf8603d64054c'
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniGraffle-#{version}.dmg"
   end
 
