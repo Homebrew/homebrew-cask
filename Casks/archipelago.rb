55df7c66a17194bdd91e8115d8e8c4e8f74e7cfd
Vítor Galvão <info@vitorgalvao.com>
2018-10-10 23:45:13 +0100

    Update archipelago.rb

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -2,7 +2,7 @@ cask 'archipelago' do
   version '2.3.2'
   sha256 'c2eda8f6e0e63d0592ba94dc3969ddbee592f8035e162c05be345e32873be3a6'
 
-  url 'https://github.com/npezza93/archipelago/releases/download/v2.3.2/Archipelago-2.3.2.dmg'
+  url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}.dmg"
   appcast 'https://github.com/npezza93/archipelago/releases.atom'
   name 'Archipelago'
   homepage 'https://github.com/npezza93/archipelago'
