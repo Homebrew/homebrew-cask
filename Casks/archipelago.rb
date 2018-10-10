b125159d6298fb0e2581cfae98791d00c29d8a3a
Andrew Mason <andrew.mason@n2pub.com>
2018-10-10 17:27:59 -0400

    fix extra spaces

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -6,6 +6,6 @@ cask 'archipelago' do
   appcast 'https://github.com/npezza93/archipelago/releases.atom'
   name 'Archipelago'
   homepage 'https://github.com/npezza93/archipelago'
-  
+
   app 'Archipelago.app'
 end
