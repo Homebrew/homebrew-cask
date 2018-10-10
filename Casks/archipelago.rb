ee0960a79b299f6c110709541015614d783c66e5
Andrew Mason <andrew.mason@n2pub.com>
2018-10-10 17:10:01 -0400

    fix sha, re-add appcast

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -1,10 +1,10 @@
 cask 'archipelago' do
   version '2.3.2'
-  sha256 '8bcc196c925c2f2de97cdaf816fd0b63f11b03fc0131d277449956b03bbdbd25'
+  sha256 'c2eda8f6e0e63d0592ba94dc3969ddbee592f8035e162c05be345e32873be3a6'
 
-  url 'https://github.com/npezza93/archipelago/releases/tag/v2.3.2'
+  url 'https://github.com/npezza93/archipelago/releases/download/v2.3.2/Archipelago-2.3.2.dmg'
   name 'Archipelago'
   homepage 'https://github.com/npezza93/archipelago'
-
+  appcast 'https://github.com/npezza93/archipelago/releases.atom'
   app 'Archipelago.app'
 end
