4c3248ed0aba259fed31fe5366a14b55f7c586e8
UP4B LLC <spaquet@up4b.com>
2018-10-10 03:37:21 -0700

    Update electron to 3.0.3 (#53155)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '3.0.2'
-  sha256 '664195e09c4c174fcfdbaa33e644c0b09b72275a97d689201614fe602b35b479'
+  version '3.0.3'
+  sha256 '10995c03b1152d0fc03bf2025952f621d7dddf1a09918b32234f3ac39bbc3b9f'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
