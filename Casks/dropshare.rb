b75b15258fe04d6488ca84b61e58525502cdc860
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 11:27:34 -0700

    Update dropshare to 5,5036

--- Casks/dropshare.rb
+++ Casks/dropshare.rb
@@ -1,6 +1,6 @@
 cask 'dropshare' do
-  version '5,5026'
-  sha256 '0dd7d86991da4566b3ec25eaf8f35042caa2c5247a4436f29488c0a7613baddf'
+  version '5,5036'
+  sha256 'c28dd83d2da7107d85953dec8e03a5ddaff837ba34fdc547427144e26358e665'
 
   # d2wvuuix8c9e48.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2wvuuix8c9e48.cloudfront.net/Dropshare#{version.major}-#{version.after_comma}.app.zip"
