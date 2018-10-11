016815773861d8620ce3f7f5a00ac0683f22f899
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 15:45:52 -0700

    Update sysdig-inspect to 0.3.3

--- Casks/sysdig-inspect.rb
+++ Casks/sysdig-inspect.rb
@@ -1,6 +1,6 @@
 cask 'sysdig-inspect' do
-  version '0.3.1'
-  sha256 '2273baefbc20f9b96d703d7613b7782efb00b3a533cbec3b045182371f4ea812'
+  version '0.3.3'
+  sha256 '483a593c8301efcd83ccc99e5812d95a7ce80000d2c279d835579cb45799442f'
 
   # download.sysdig.com/stable/sysdig-inspect was verified as official when first introduced to the cask
   url "https://download.sysdig.com/stable/sysdig-inspect/sysdig-inspect-#{version}-mac.dmg"
