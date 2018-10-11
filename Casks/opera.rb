a5bd9e12b7f0d3b92bfb510b13c18eaae1bddd00
Andrey Izotov <faradaytrs@gmail.com>
2018-10-11 11:30:09 +0800

    Update opera to 56.0.3051.43 (#53203)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '56.0.3051.36'
-  sha256 'e3aa45dc9f8a048a2944f15880155f665d306140162c045fe35dad5e33c11cb2'
+  version '56.0.3051.43'
+  sha256 '72cbe7921a41e0b7f1fd6e1d7591d85099f37436a1bc0ff2330937c0d5f2eb22'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
