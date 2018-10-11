523e958206205f07ffbdafc1052b5fd00e57017a
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 13:21:50 -0700

    Update mailbutler to 2.2-10112

--- Casks/mailbutler.rb
+++ Casks/mailbutler.rb
@@ -1,6 +1,6 @@
 cask 'mailbutler' do
-  version '2.1.3-9666'
-  sha256 '53ea2df680b7e258d6e8caa37a1658b5480aecfa5d1ed1a3bef5319fdb519b17'
+  version '2.2-10112'
+  sha256 '8625fea4d460109af005e45ca3d8af918ea7a2839aafb9dc4111d95a0d183bf1'
 
   # mailbutler-data.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://mailbutler-data.s3.amazonaws.com/downloads/Mailbutler_#{version}.zip"
