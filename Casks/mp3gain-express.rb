fa13f25e413e5419b590ff7dea9a014d3d1293b1
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 13:37:57 -0700

    Update mp3gain-express to 2.2

--- Casks/mp3gain-express.rb
+++ Casks/mp3gain-express.rb
@@ -1,6 +1,6 @@
 cask 'mp3gain-express' do
-  version '2.1'
-  sha256 'bde9b129a87525a7e39e11176bdb9045346a822e2b18539a72446de53f9bb472'
+  version '2.2'
+  sha256 '261b3a0de62a3fd1b688a9ba7c01964a8ebff32f872105fe05a8eb02b1249a80'
 
   url "https://projects.sappharad.com/mp3gain/mp3gain_mac#{version.no_dots}.zip"
   appcast 'https://projects.sappharad.com/mp3gain/updates.xml'
