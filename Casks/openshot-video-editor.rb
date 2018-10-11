fba5f19c5303511ca05025bc3f550647859d2147
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 14:30:58 -0700

    Update openshot-video-editor to 2.4.3

--- Casks/openshot-video-editor.rb
+++ Casks/openshot-video-editor.rb
@@ -1,6 +1,6 @@
 cask 'openshot-video-editor' do
-  version '2.4.2'
-  sha256 'be8d67b2f5b07bdd23a4cd7877f961cef42c8b1d08b39a7f7c25fa5b1ceaf398'
+  version '2.4.3'
+  sha256 'e3b2b7175e6cd596c7c5676d4a254cd2a4a27f943bb5941120d3c0c18f19d53c'
 
   # github.com/OpenShot/openshot-qt was verified as official when first introduced to the cask
   url "https://github.com/OpenShot/openshot-qt/releases/download/v#{version}/OpenShot-v#{version}-x86_64.dmg"
