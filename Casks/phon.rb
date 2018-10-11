b3c4ebdd39b56fea220a896ca42bfc0b2daf8fc4
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 14:39:55 -0700

    Update phon to 3.0.1

--- Casks/phon.rb
+++ Casks/phon.rb
@@ -1,6 +1,6 @@
 cask 'phon' do
-  version '3.0.0'
-  sha256 'cb51103e1f87ae42208aa05973afb00a24f6f500f1aacaf9753eec33b557d4c4'
+  version '3.0.1'
+  sha256 '32ddc2bbb895add89c61501cd0efe3922414d020358145f48eba20f12b2532d3'
 
   # github.com/phon-ca/phon was verified as official when first introduced to the cask
   url "https://github.com/phon-ca/phon/releases/download/#{version}/Phon_macos_#{version.dots_to_underscores}.dmg"
