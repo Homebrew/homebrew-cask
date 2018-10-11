3110c87f1e5eb01695da13416b9eb80729c1a0dd
Johnathan Conley <jconley@users.noreply.github.com>
2018-10-11 16:27:53 -0500

    Update udig to 2.0.0 (#53152)
    * udig 2.0.0
    
    * Update udig.rb
    
    * Update udig.rb

--- Casks/udig.rb
+++ Casks/udig.rb
@@ -1,10 +1,20 @@
 cask 'udig' do
-  version '1.4.0'
-  sha256 '1364245d9e6f62a3ec4f21b5a0eb4046a0423ca778d3d550b348d5bb20706fa2'
+  version '2.0.0'
+  sha256 'b01e08bc9067a5d584c223293ba2d536a14487327fc73866836f67089545a99a'
 
   url "http://udig.refractions.net/files/downloads/udig-#{version}.macosx.cocoa.x86_64.dmg"
   name 'uDig'
   homepage 'http://udig.refractions.net/'
 
   suite 'udig'
+
+  caveats do
+    depends_on_java '8'
+
+    <<~EOS
+      #{token} will fail to launch if you don't have java 1.8 installed as the system default;
+      otherwise you can point #{token} to java 1.8 by editing:
+      /Applications/udig/udig.app/Contents/Info.plist
+    EOS
+  end
 end
