b13bcde18d93a64a02cf594d6ef5a8669bf0a377
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 13:08:06 -0700

    Update lbry to 0.25.1

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,6 +1,6 @@
 cask 'lbry' do
-  version '0.25.0'
-  sha256 '524c713406dd2d6f8bd12ef01d0d5922e3912455dfcd02e87681f95ce27e63e4'
+  version '0.25.1'
+  sha256 '5fb92b87d561cfaf0e4a9edf5dfa99f678e27718bfefc145f64c5943f4630146'
 
   # github.com/lbryio/lbry-app was verified as official when first introduced to the cask
   url "https://github.com/lbryio/lbry-app/releases/download/v#{version}/LBRY_#{version}.dmg"
