d816a9c7274590730d480accb32a1a940887aee9
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 11:09:10 -0700

    Update coinpricebar to 1.5.0.16

--- Casks/coinpricebar.rb
+++ Casks/coinpricebar.rb
@@ -1,6 +1,6 @@
 cask 'coinpricebar' do
-  version '1.5.0.13'
-  sha256 '9e59907f6cc9fe906715da3e622ff8ab27cc58465b8ad22c526a5f8d6ab7bb0a'
+  version '1.5.0.16'
+  sha256 'c2b7a8c6b8f42143ea5545892767ec0186f073a0c2bb79876c0acc226e07599c'
 
   url "https://github.com/T-Pham/CoinPriceBar/releases/download/#{version}/CoinPriceBar.app.zip"
   appcast 'https://github.com/T-Pham/CoinPriceBar/releases.atom'
