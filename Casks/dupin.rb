affaf19f0814f4ddaf4211f65d5f72d521ebdffa
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 11:29:08 -0700

    Update dupin to 2.14.1

--- Casks/dupin.rb
+++ Casks/dupin.rb
@@ -3,8 +3,8 @@ cask 'dupin' do
     version '2.7.4'
     sha256 '4aba53f356606614627d57f6a33c1ee9cf13ddf06c13e7ac8487b930cb647b85'
   else
-    version '2.14.0'
-    sha256 '5bba18216cdd01815df7c386d30c356f925b9bb264d3e0bc29c12d8eb7bdd791'
+    version '2.14.1'
+    sha256 'c4cd423768d2bd2982e10e965fcc8965dd78ce7b7e409a20f159a513573f6dd1'
 
     appcast 'https://dougscripts.com/itunes/itinfo/dupin_appcast.xml'
   end
