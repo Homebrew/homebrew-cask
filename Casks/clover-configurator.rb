676b578344362d230ff4865cd269d04e09b84bc9
Yurii Kolesnykov <root@yurikoles.com>
2018-10-11 18:03:50 +0300

    clover-configurator: set version to CCG, don't check for sha256

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,9 +1,9 @@
 cask 'clover-configurator' do
-  version '5.2.0.1'
-  sha256 'afed4c08d0bc3c8b017e75847c22d516adbd4a2be904cd4facf4889b8836bce8'
+  version 'CCG'
+  sha256 :no_check
 
-  url 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/builds-data/CCG.zip'
-  appcast 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/update-data-builds.xml'
+  url "https://mackie100projects.altervista.org/apps/cloverconf/#{version}/builds-data/CCG.zip"
+  appcast "https://mackie100projects.altervista.org/apps/cloverconf/#{version}/update-data-builds.xml"
   name 'Clover Configurator'
   homepage 'https://mackie100projects.altervista.org/'
 
