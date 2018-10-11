1fd8b886396fd9cae4afff42b30275ac3c87fa0c
Eric Nielsen <eric@amalgamar.com.br>
2018-10-10 22:27:27 -0500

    Update flash-player-debugger-ppapi to 31.0.0.122 (#53198)

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger-ppapi' do
-  version '31.0.0.108'
-  sha256 'c3c62f3ea9ff8291068b75eda58b6385be88771d91cf3402f4624d8b09e11a1b'
+  version '31.0.0.122'
+  sha256 'efa6dfb54d886a7b573f1c5ede7a5692de4d2cae80fcdbf4eccd79fd99ec8a43'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
