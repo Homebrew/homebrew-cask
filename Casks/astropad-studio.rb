1f2c2a76bcace00ce07ad1ef7ca7dba3693f8f7a
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 13:44:44 -0700

    Update astropad-studio to 2.2.0 (#53244)

--- Casks/astropad-studio.rb
+++ Casks/astropad-studio.rb
@@ -1,6 +1,6 @@
 cask 'astropad-studio' do
-  version '2.0'
-  sha256 '60fa3e3922d91767881eb3137b50060ba4f7e314073281ad939b8af75130eff2'
+  version '2.2.0'
+  sha256 '5867212a921f4bfbbc2657b616ff1282450fc4cc0769c4055e526fdd8e5cc46c'
 
   url "https://astropad.com/downloads/AstropadStudio-#{version}.zip"
   appcast 'https://astropad.com/downloads/studio-sparkle.xml'
