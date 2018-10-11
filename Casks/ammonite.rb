773876fc6420d177c918bc2780d110fa864184f6
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 10:23:39 -0700

    Update ammonite to 1.16.1

--- Casks/ammonite.rb
+++ Casks/ammonite.rb
@@ -1,6 +1,6 @@
 cask 'ammonite' do
-  version '1.15.1'
-  sha256 '7eebf9a07b1a2d996f8f959317872ff4f4919da7563d3b48f9d6a62cba1c56c8'
+  version '1.16.1'
+  sha256 '96f5b078ee1ded66da73d45879d469759a0d0f8ad6264048fa71b542a73bda85'
 
   url "http://www.soma-zone.com/download/files/Ammonite_#{version}.tbz"
   appcast 'http://www.soma-zone.com/Ammonite/a/appcast.xml'
