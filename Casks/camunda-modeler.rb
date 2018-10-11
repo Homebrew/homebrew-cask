a8311387a6d6844334a858f68a19783996a14f12
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 11:01:24 -0700

    Update camunda-modeler to 2.0.3

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask 'camunda-modeler' do
-  version '2.0.1'
-  sha256 'a0b8bf577b777e3472bb3a4e28f4f1f0901103527e37e4055d71c6639bcc8215'
+  version '2.0.3'
+  sha256 '5c31a2bd2c03676ec1f88479b4286035623bc80f7a93a632e45acd4bddaa66cc'
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac.zip"
   appcast 'https://camunda.com/download/modeler/'
