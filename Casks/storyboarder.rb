ad8ed7f9528dda1336dd937fc75de172459ba26c
Brian Morton <brianmortondesign@gmail.com>
2018-10-11 15:43:17 -0700

    Update storyboarder to 1.9.0

--- Casks/storyboarder.rb
+++ Casks/storyboarder.rb
@@ -1,6 +1,6 @@
 cask 'storyboarder' do
-  version '1.7.0'
-  sha256 'f9bf075aee8369a461731522a84fc3043fc6a5113c4422581d4e6ccd1de7690e'
+  version '1.9.0'
+  sha256 'c1392d8c4090fd112e03e4224fb5b0b7015d4593e14d374740b822d1a94a9e25'
 
   # github.com/wonderunit/storyboarder was verified as official when first introduced to the cask
   url "https://github.com/wonderunit/storyboarder/releases/download/v#{version}/storyboarder-#{version}-mac.zip"
