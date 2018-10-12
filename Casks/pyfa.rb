a9aeb46e63ca0528a937228ed66018b93b95ba33
Brian Morton <brianmortondesign@gmail.com>
2018-10-12 09:17:37 -0700

    Update pyfa to 2.5.0,yc120.10-1.0

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,6 +1,6 @@
 cask 'pyfa' do
-  version '2.4.0,yc120.8-1.0'
-  sha256 '608a70a072c0329ace93e426fcac86b7593780642da6b8bfff088daf5d89025a'
+  version '2.5.0,yc120.10-1.0'
+  sha256 '1924a6c9f184c14ecffc3fcecb58a8261fdf64ab86114e6a183b53c5f82bdb53'
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version.before_comma}/pyfa-#{version.before_comma}-#{version.after_comma}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom'
