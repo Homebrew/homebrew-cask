523ce00683cf41212db672d6b48ebc8ef92504f4
Brian Morton <brianmortondesign@gmail.com>
2018-10-12 09:01:39 -0700

    Update fastonosql to 1.20.1

--- Casks/fastonosql.rb
+++ Casks/fastonosql.rb
@@ -1,6 +1,6 @@
 cask 'fastonosql' do
-  version '1.20.0'
-  sha256 'f90252aa46ec3488136aecb22f0e29922e7fe75a5ade9fe75b56a7dd71204afb'
+  version '1.20.1'
+  sha256 '93490e5068336cbbb96cf9fda4c48e0295c7d051779ba9da95d46d5df95392f9'
 
   url "https://www.fastonosql.com/downloads/macosx/fastonosql-#{version}-x86_64.dmg"
   appcast 'https://github.com/fastogt/fastonosql/releases.atom'
