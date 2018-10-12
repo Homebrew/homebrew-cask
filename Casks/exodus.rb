6b8f526791c0256bd6b844cf6a4803066de3caca
Brian Morton <brianmortondesign@gmail.com>
2018-10-12 09:00:39 -0700

    Update exodus to 1.62.0

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,6 +1,6 @@
 cask 'exodus' do
-  version '1.61.1'
-  sha256 '26703eb0404d0c95aff034483eda3bedb2af21f4b1311c61aae6994af2fd6d01'
+  version '1.62.0'
+  sha256 'c191617381d54e183695f781a586a09d368aa1c0957cbfda046a5b3e4eb581b9'
 
   # exodusbin.azureedge.net was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-macos-#{version}.dmg"
