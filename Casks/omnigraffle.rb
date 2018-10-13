48b67b9425b494df722d53813e83c3579979d26e
Eric Zhang <eric.pucker@gmail.com>
2018-10-13 15:30:48 +0800

    bugfix: update omnigraffle download url

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -2,11 +2,11 @@ cask 'omnigraffle' do
   if MacOS.version <= :sierra
     version '7.8.2'
     sha256 'ab463ea6c12d49c4104d3814ac3280d0359072702d4751f5074f644fc79de0c6'
-    url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniGraffle-#{version}.dmg"
+    url "https://downloads.omnigroup.com/software/Archive/MacOSX/10.12/OmniGraffle-#{version}.dmg"
   else
     version '7.9.1'
     sha256 '536cd872e2e772836300c7c5d9259371a30db569729ec2a85c9cf8603d64054c'
-    url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniGraffle-#{version}.dmg"
+    url "https://downloads.omnigroup.com/software/Archive/MacOSX/10.13/OmniGraffle-#{version}.dmg"
   end
 
   name 'OmniGraffle'
