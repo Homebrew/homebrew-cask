85e011fa47bd5f704c59c059e5e72806c42a8b73
suschizu <brigitte@corecode.io>
2018-10-14 14:59:02 +0200

    Update qcad to 3.21.3 (#53551)

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -1,11 +1,11 @@
 cask 'qcad' do
-  version '3.21.2'
+  version '3.21.3'
 
   if MacOS.version <= :mavericks
     sha256 '9155a11b10991dbeb5bac5a15b810354d0462f13bee8317e4327765d83582466'
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-osx-10.9-10.13.dmg"
   else
-    sha256 '2c267dba49b68db33717a3773804182896d7633ded07ed2c8fe32947c3540061'
+    sha256 'dbc0dff4d7106c686a7f3b9d1ed80de2e82bb64e7901b8fd7d42018d2e6a39cc'
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-osx-10.10-10.13.dmg"
   end
 
