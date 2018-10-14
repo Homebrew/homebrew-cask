9e9d32ae01cadfdc105ed30b827034acc7822459
commitay <commitay@users.noreply.github.com>
2018-10-15 08:38:16 +1000

    autodmg:

--- Casks/autodmg.rb
+++ Casks/autodmg.rb
@@ -3,12 +3,11 @@ cask 'autodmg' do
   sha256 '92c10590ef5569797f1879f3b123e0a1f5a0434654a9cc6f6dbb517e779e6a79'
 
   url "https://github.com/MagerValp/AutoDMG/releases/download/v#{version}/AutoDMG-#{version}.dmg"
-  appcast 'https://github.com/MagerValp/AutoDMG/releases.atom'
+  appcast 'https://github.com/MagerValp/AutoDMG/releases'
   name 'AutoDMG'
   homepage 'https://github.com/MagerValp/AutoDMG'
 
   depends_on macos: '>= :sierra'
-
 
   app 'AutoDMG.app'
 
