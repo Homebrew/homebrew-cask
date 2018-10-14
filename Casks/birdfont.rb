b99f45acb1896068c3aad1f07a0156efa5a7528b
suschizu <brigitte@corecode.io>
2018-10-14 15:05:18 +0200

    Update birdfont to 3.12.12 (#53557)

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -1,6 +1,6 @@
 cask 'birdfont' do
-  version '3.12.11'
-  sha256 'deffe442f70efb6579fa4093c46271fbc6960aad4a1cbf53afc3f85036d56080'
+  version '3.12.12'
+  sha256 '30378755f0ed14d69460039a032e42c3b15f29caa5d1568b1d7d83bf4d1e544d'
 
   url "https://birdfont.org/download/birdfont-#{version}-free.dmg"
   name 'BirdFont'
