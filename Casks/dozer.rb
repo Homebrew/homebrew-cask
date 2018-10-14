e596da980a92dbde9dd9ae44fa307e17c4398638
Vítor Galvão <info@vitorgalvao.com>
2018-10-14 17:37:37 +0100

    Update dozer.rb

--- Casks/dozer.rb
+++ Casks/dozer.rb
@@ -3,11 +3,11 @@ cask 'dozer' do
   sha256 '4d44bf8a0227222023c4f0a751ab2eef82e8b377016640ffd3d6bf9b268bdc41'
 
   url "https://github.com/Mortennn/Dozer/releases/download/#{version}/Dozer.#{version}.dmg"
-  appcast 'https://raw.githubusercontent.com/Mortennn/Dozer/master/appcast.xml'
+  appcast 'https://github.com/Mortennn/Dozer/releases.atom'
   name 'Dozer'
   homepage 'https://github.com/Mortennn/Dozer'
 
-  auto_updates :yes
+  auto_updates true
   depends_on macos: '>= :sierra'
 
   app 'Dozer.app'
