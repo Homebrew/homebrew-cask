1cbf63149725a62271e8a6e66b20f62882b434a8
alissa-huskey <alissa.huskey@gmail.com>
2018-10-15 14:17:03 -0600

    Add Ondesoft AudioBook Converter.app v3.2.0
    Added Ondesoft AudioBook Converter, a paid app to convert Audible AA/AAX audiobooks to MP3/M4A

---
+++ Casks/ondesoft-audiobook-converter.rb
@@ -0,0 +1,10 @@
+cask 'ondesoft-audiobook-converter' do
+  version '3.2.0'
+  sha256 '6cfaaba5d77030fefc5675c89d8282705d7469c3e5972a2a60c316e5efe7c7ae'
+
+  url 'http://www.ondesoft.com/download/odaudiobookconverter_mac.dmg'
+  name 'OndeSoft Audible Audiobook Converter'
+  homepage 'https://www.ondesoft.com/audible-audiobook-converter.html'
+
+  app 'Ondesoft AudioBook Converter.app'
+end
