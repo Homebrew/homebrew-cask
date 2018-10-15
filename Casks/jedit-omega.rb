29b66c48ec718186f4d832f101d2d782b84078fd
brigitte zmoelnig <brigitte@corecode.io>
2018-10-15 16:49:41 +0200

    Update jedit-omega to 2.01

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -1,6 +1,6 @@
 cask 'jedit-omega' do
-  version '1.24'
-  sha256 '99ebefee3f7d805b8386f84a7ac77b8f021071fcce9dab0d3d5b7cd26128cd72'
+  version '2.01'
+  sha256 '2e9faaceb7117588eb5b992fed968db2b18bfa498d557d762556dcb882bc5363'
 
   # artman21.co.jp was verified as official when first introduced to the cask
   url "http://download.artman21.co.jp/product/JeditOmega/JeditOmega#{version.no_dots}.dmg"
