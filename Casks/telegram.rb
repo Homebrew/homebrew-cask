749a4d8f5601472a9761c3c66172e8c71f5f145d
SteamedFish <i@steamedfish.org>
2018-10-16 09:44:14 +0800

    Update telegram to 4.5-141573

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '4.4.1-140477'
-  sha256 '27e732459c499b4ee40766e2a91a42e426cb112b0deb4743b8a8a0b92702db18'
+  version '4.5-141573'
+  sha256 '6d2a4155c75dc4f0ccb253ac98e06c4e51fe5d26e28a15ccfbf8ea96d442c0f2'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml'
