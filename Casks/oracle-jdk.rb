3f7c46595d01fa45b457ace31ea1a05540afce57
commitay <commitay@users.noreply.github.com>
2018-10-17 09:14:13 +1000

    Update oracle-jdk to 11.0.1,13:90cf5d8f270a4347a95050320eef3fb7

--- Casks/oracle-jdk.rb
+++ Casks/oracle-jdk.rb
@@ -1,6 +1,6 @@
 cask 'oracle-jdk' do
-  version '11,28:55eed80b163941c8885ad9298e6d786a'
-  sha256 'aa5fea6e2f009e63dd8a8d7f532104e6476195a49ee6dd0d2b11c64966d028cc'
+  version '11.0.1,13:90cf5d8f270a4347a95050320eef3fb7'
+  sha256 '467fd06212438539078c56f768c42c41ba8b26cea1213991da5ba44a601af628'
 
   url "https://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_osx-x64_bin.dmg",
       cookies: {
