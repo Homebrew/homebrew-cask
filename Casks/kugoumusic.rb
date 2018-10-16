1872c605438772a3e8f3ff7ff94f7b070a707e15
Julian Mayer <julian@corecode.io>
2018-10-16 14:25:27 +0200

    Update kugoumusic to 2.7.0

--- Casks/kugoumusic.rb
+++ Casks/kugoumusic.rb
@@ -1,8 +1,9 @@
 cask 'kugoumusic' do
-  version '2.6.4'
-  sha256 '1dce7207e8a525d09feb478d85b0bf44e835b2cf2047be4a63d5073e9062f7c8'
+  version '2.7.0'
+  sha256 'bd0081ddacdf60a7774f599f7aad2abded65e1e5615872439efc28dd8e7a6f3b'
 
   url "http://downmini.kugou.com/mac/Kugou_V#{version}.dmg"
+  appcast 'http://download.kugou.com/mac.html'
   name 'Kugou Music'
   homepage 'https://www.kugou.com/'
 
