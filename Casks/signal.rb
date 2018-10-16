94c762b0e91971fdc868ea8f4edcc4d35097f839
RJ Hsiao <rj@rjhsiao.me>
2018-10-17 06:56:42 +0800

    Update signal to 1.16.3 (#53662)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,6 +1,6 @@
 cask 'signal' do
-  version '1.16.2'
-  sha256 '0b8cfa51b5b6cfc9025f863974aac602c92eeeb8cef6ab81e272b9d0a277aa33'
+  version '1.16.3'
+  sha256 '9889b0a8a2bc22aff6e496e3ee58e3c23288027d52e00e7f1f1f81cb1f9b92d1'
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.zip"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
