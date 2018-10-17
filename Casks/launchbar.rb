956075c8c51001527dbd7a9a4ba76c65ba84e604
CoreCode <core-code@users.noreply.github.com>
2018-10-17 16:30:25 +0200

    Update launchbar to 6.10 (#53499)

--- Casks/launchbar.rb
+++ Casks/launchbar.rb
@@ -3,9 +3,13 @@ cask 'launchbar' do
     version '5.6.4'
     sha256 '22a1ec0c10de940e5efbcccd18b8b048d95fb7c63213a01c7976a76d6be69a4d'
     url "https://www.obdev.at/downloads/launchbar/legacy/LaunchBar-#{version}.dmg"
+  elsif MacOS.version <= :mavericks
+    version '6.9.7'
+    sha256 'e1623c77a85857ec3191a82b8932909200f8d183bb654e9caa77e9c14afd7a63'
+    url "https://www.obdev.at/downloads/launchbar/legacy/LaunchBar-#{version}.dmg"
   else
-    version '6.9.6'
-    sha256 'c784dc1206a8707c1254577b71ab85834e92aeae0fe9adb5b9baf168f029b993'
+    version '6.10'
+    sha256 '481e0a490737317b75031c57d69d9a6f031d6ad09440e32bc341ddb4b46e1e70'
 
     url "https://www.obdev.at/downloads/launchbar/LaunchBar-#{version}.dmg"
     appcast "https://sw-update.obdev.at/update-feeds/launchbar-#{version.major}.plist"
