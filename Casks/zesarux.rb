106958ff22cd10f0545cf484baf200d85ce89fe5
CoreCode <core-code@users.noreply.github.com>
2018-10-17 16:31:12 +0200

    Update zesarux to 7.1 (#53504)

--- Casks/zesarux.rb
+++ Casks/zesarux.rb
@@ -1,8 +1,8 @@
 cask 'zesarux' do
-  version '7.0'
-  sha256 'c9c515218ec474bfb042294f57c99929996bc48796e8a3e1f458d385268a3d92'
+  version '7.1'
+  sha256 '4f1ff58d5d03a2e32c2f218ecbd5e6009e9123ca0e5141f4ba62863d0138a37e'
 
-  url "https://github.com/chernandezba/zesarux/releases/download/#{version}/ZEsarUX_macos_opengl-#{version}.dmg.gz"
+  url "https://github.com/chernandezba/zesarux/releases/download/#{version}-final/ZEsarUX_macos_opengl-#{version}.dmg.gz"
   appcast 'https://github.com/chernandezba/zesarux/releases.atom'
   name 'ZEsarUX'
   homepage 'https://github.com/chernandezba/zesarux'
