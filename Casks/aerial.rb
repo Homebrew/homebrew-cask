c975f632672a8c732f14252e5ddef22398d484a8
Matt Travi <github@travi.org>
2018-10-17 15:15:07 -0500

    Update aerial to 1.4.1 (#53694)

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -1,6 +1,6 @@
 cask 'aerial' do
-  version '1.4'
-  sha256 'bd22c7bd0e27a410a4263e844658a550a785ef822c9bff45c4332ef8c25e1ab2'
+  version '1.4.1'
+  sha256 'fc1e51bced2b21be74793f4ea6aa86961cc931dde22c8a25e84d69b237055413'
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip"
   appcast 'https://github.com/JohnCoates/Aerial/releases.atom'
