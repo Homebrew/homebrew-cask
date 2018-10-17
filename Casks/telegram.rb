d4ace4b1837ff2f80f8c723b2abb0d9ff21bdaa0
commitay <commitay@users.noreply.github.com>
2018-10-18 01:04:57 +1000

    Update telegram to 4.5-141717

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '4.4.1-140477'
-  sha256 '27e732459c499b4ee40766e2a91a42e426cb112b0deb4743b8a8a0b92702db18'
+  version '4.5-141717'
+  sha256 '109eebfcf604b3f07f7998da7230f861dcee7aaf1326bb0acc54c3e8ca9235b6'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml'
