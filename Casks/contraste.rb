cda63fb84f7a3a2afdbde5ca2bfd149bed9151aa
David Baumgold <david@davidbaumgold.com>
2018-10-17 11:01:42 +0200

    New cask: Contraste 1.0

---
+++ Casks/contraste.rb
@@ -0,0 +1,10 @@
+cask 'contraste' do
+  version '1.0'
+  sha256 'ab6ce81efcde8179f9fdf81baff46f90dae45bca580f8582d82c8d1e42aa2c4a'
+
+  url "https://contrasteapp.com/contraste-#{version}.zip"
+  name 'Contraste'
+  homepage 'https://contrasteapp.com/'
+
+  app 'Contraste.app'
+end
