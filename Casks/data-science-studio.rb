eef22db7403e0333364096949be009a769cecc80
suschizu <brigitte@corecode.io>
2018-10-17 21:36:44 +0200

    Update data-science-studio to 5.0.2 (#53633)

--- Casks/data-science-studio.rb
+++ Casks/data-science-studio.rb
@@ -1,6 +1,6 @@
 cask 'data-science-studio' do
-  version '4.3.1'
-  sha256 'c541c73a33a3d7a30243b098cc268c43607227fe589675075017809b13ca5723'
+  version '5.0.2'
+  sha256 'f2cb17bbdd33af6a38f13d20ccb281da2c17041d1793d31a3ea0fafd6a0ff66e'
 
   url "https://downloads.dataiku.com/public/studio/#{version}/Data%20Science%20Studio%20#{version}.dmg"
   name 'Dataiku Data Science Studio'
