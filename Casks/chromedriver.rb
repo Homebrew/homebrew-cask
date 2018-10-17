525eaa4b1ba4f36561bfb88bb821e6c15bb4b37e
Alexander Bayandin <a.bayandin@gmail.com>
2018-10-17 12:31:26 +0100

    Update chromedriver to 2.43

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,6 +1,6 @@
 cask 'chromedriver' do
-  version '2.42'
-  sha256 'fc8f27364966f4fdd824aff2317f9bafea6a7b374343f8c4f4e652923d1bbe0b'
+  version '2.43'
+  sha256 '84244e590d866294c1eaa5fa65ad51f1b0d2cc4fc1595aa3e414f5aac4da60ef'
 
   # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
