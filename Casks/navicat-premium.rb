8a72e7a20fd81a372eb4948bfc0696f2a03393ba
CoreCode <core-code@users.noreply.github.com>
2018-10-17 22:16:06 +0200

    Update navicat-premium to 12.1.10 (#53682)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium' do
-  version '12.1.9'
-  sha256 '868daee1d17f22fc6b2eef4d9813f462d7b07ce642a410f2e621cc8717b5974a'
+  version '12.1.10'
+  sha256 'b74e4c3c4a66653c344903dbf32bfd0cfd8a4e3a9dec0432de9ac13d669bc283'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://www.navicat.com/en/products/navicat-premium-release-note'
