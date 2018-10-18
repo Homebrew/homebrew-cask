80973146be86d728753934109415f425bda014d8
CoreCode <core-code@users.noreply.github.com>
2018-10-18 19:21:18 +0200

    Update omnifocus to 3.1.1 (#53753)

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -16,8 +16,8 @@ cask 'omnifocus' do
     sha256 '8a2dc53331dba804f6781773fef546a03c181fc4ff0eb7ee4f871c10342621f0'
     url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniFocus-#{version}.dmg"
   else
-    version '3.1'
-    sha256 'd49ac4beb418d9c48e61e9fcaacf2f254b94f7cf30dbc773a75c2edacba8146a'
+    version '3.1.1'
+    sha256 '666b2c68ee7a70cad6dba6d000e52173dc03e22561e98a671cf7084438672041'
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniFocus-#{version}.dmg"
   end
 
