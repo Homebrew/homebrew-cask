69dd2c641ab666db011a9d38650f2ed24123fa4c
William Cordis <whcordis@gmail.com>
2018-10-18 16:32:27 -0400

    Update tableau-reader to 2018.2.3

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -1,6 +1,6 @@
 cask 'tableau-reader' do
-  version '2018.2.2'
-  sha256 '98cd85886e2dec3c11fea43491511e33bd9beda78b312ec2d246f5a4c6ae8669'
+  version '2018.2.3'
+  sha256 'b0acf6084c696bf4247505ffaa0feb2a986ec560f1fdc182d7850971c7543911'
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
   name 'Tableau Reader'
