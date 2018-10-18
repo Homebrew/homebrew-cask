a0d95d400cd7dc4a636091842fb05468c0471547
cooclsee <cooclsee@qq.com>
2018-10-18 11:01:57 +0800

    Update postman to 6.4.4

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,6 +1,6 @@
 cask 'postman' do
-  version '6.4.2'
-  sha256 '7dd1124d9c2b7add3ea0ff55b74c0472a053994ee46c7abbd95cbba2c518ea02'
+  version '6.4.4'
+  sha256 'a39c62fe2bf9dc47f034db96f8ebba9a32d01388460658d012dbcd6831226df6'
 
   # dl.pstmn.io/download/version was verified as official when first introduced to the cask
   url "https://dl.pstmn.io/download/version/#{version}/osx64"
