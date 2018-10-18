c1421bc4d1bf938d682fac34d0a0ec8a04858eec
commitay <commitay@users.noreply.github.com>
2018-10-18 17:24:30 +1000

    Update fluid to 2.1

--- Casks/fluid.rb
+++ Casks/fluid.rb
@@ -1,6 +1,6 @@
 cask 'fluid' do
   version '2.1'
-  sha256 '7b27dfcec1b74209496eb1150f38e464c95ae695dc9d95f1eab3d11925189e58'
+  sha256 'dea3e4c93f679f227594fa1cb53f5e78880e879fa796aa6396641dc4a1962195'
 
   url "https://fluidapp.com/dist/Fluid_#{version}.zip"
   appcast "https://fluidapp.com/appcast/fluid#{version.major}.rss"
