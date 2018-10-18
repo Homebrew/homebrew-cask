c0c2c306a1764ac1717027af0b247cda13b07372
Shoichi Aizawa <shoichiaizawa@gmail.com>
2018-10-18 11:08:50 +0900

    Update visual-studio-code to 1.28.2,7f3ce96ff4729c91352ae6def877e59c561f4850 (#53738)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.28.1,3368db6750222d319c851f6d90eb619d886e08f5'
-  sha256 '561173e021fb5e3b7268ede4068d123bd6c92ad4ac3fc6c2e2ef51b93cdc5678'
+  version '1.28.2,7f3ce96ff4729c91352ae6def877e59c561f4850'
+  sha256 '6368df227a125ab9ef63a7b016a758bcbe7b76f8e413a37042383ceaef56f458'
 
   # az764295.vo.msecnd.net/stable was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/stable/#{version.after_comma}/VSCode-darwin-stable.zip"
