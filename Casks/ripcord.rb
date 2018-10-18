fe577b899c1311be9d464b14e1721a576e5f853e
strafe <strafe@users.noreply.github.com>
2018-10-18 03:05:16 +0100

    Update ripcord to 0.3.5 (#53736)

--- Casks/ripcord.rb
+++ Casks/ripcord.rb
@@ -1,6 +1,6 @@
 cask 'ripcord' do
-  version '0.3.4'
-  sha256 '5608630070fe4e71256befad7f6c259ba25f7323afa59dd282d33105555c4770'
+  version '0.3.5'
+  sha256 'e75af18e8160b6a70d3f97d1a4e1097f5d4b925ed55561ee3e0a9f2bbe681e23'
 
   url "https://cancel.fm/dl/Ripcord_Mac_#{version}.zip"
   appcast 'https://cancel.fm/ripcord/updates/v1'
