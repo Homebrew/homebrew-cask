e42cab9aa89f8d7c0923135f15a86617bf42d935
commitay <commitay@users.noreply.github.com>
2018-10-18 17:20:54 +1000

    Update doubletwist.rb

--- Casks/doubletwist.rb
+++ Casks/doubletwist.rb
@@ -1,6 +1,6 @@
 cask 'doubletwist' do
-  version '3.2.0'
-  sha256 '426a4468c79a99bdcf51d1be9601b7ebe5222fcc97d38b38d80b8116b75a8b13'
+  version :latest
+  sha256 :no_check
 
   url 'http://download.doubletwist.com/mac/doubleTwist.dmg'
   name 'doubleTwist'
