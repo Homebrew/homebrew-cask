9647b26b47336e1ed48acb387c48b2fe2aefad85
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 11:14:06 -0700

    Update go-server to 18.10.0-7703

--- Casks/go-server.rb
+++ Casks/go-server.rb
@@ -1,6 +1,6 @@
 cask 'go-server' do
-  version '18.9.0-7478'
-  sha256 'a43f46776bb10aa94bae35b095f1ef72450e737ff9a1004d268868038957d25c'
+  version '18.10.0-7703'
+  sha256 'ec75a894399d0979e56e2de4ddb942071a41c722f46f1ed666a0b108991c24d8'
 
   # download.gocd.io/binaries was verified as official when first introduced to the cask
   url "https://download.gocd.io/binaries/#{version}/osx/go-server-#{version}-osx.zip"
