0e859764e91ac8d9f8b7d6052df42f4bf8f2ba63
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 13:07:58 -0700

    Update funter to 4.3b79 (#53908)

--- Casks/funter.rb
+++ Casks/funter.rb
@@ -1,8 +1,8 @@
 cask 'funter' do
-  version '4.2'
-  sha256 'f6a06b1a5a8a6356e24781324669b6f41318c65481e191e1a6c49c34e7ec0596'
+  version '4.3b79'
+  sha256 'd5b7ba9d8dd48581875dacdcfc9e521b518e19256fe6e5d51366f14fb6f3db79'
 
-  url "https://nektony.com/promo/funter/update/funter_v#{version}.zip"
+  url "https://nektony.com/promo/funter/update/update_#{version}.zip"
   appcast 'https://nektony.com/promo/funter/update/update.xml'
   name 'Funter'
   homepage 'https://nektony.com/products/funter'
