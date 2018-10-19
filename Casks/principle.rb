e8c369820b44998fa95bd1f85e8ff68017fba359
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 11:44:54 -0700

    Update principle to 4.7

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,6 +1,6 @@
 cask 'principle' do
-  version '4.6'
-  sha256 '649814f82d07e82698961e050d3d900fe867f359b4edaa63f5c8bcee413ab17f'
+  version '4.7'
+  sha256 '648199e98c8d7ad689f2cac2a1ce3bb2f87d587b88624262c8d70d2efaf88188'
 
   url "http://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
   appcast 'http://principleformac.com/update2.xml'
