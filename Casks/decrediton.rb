0d499d3cf9b3535e3b52bb1bda3da79c9cca878f
Dmitry Chirkin <dmitry.chirkin@gmail.com>
2018-10-19 11:26:11 +0300

    Update decrediton to 1.3.1

--- Casks/decrediton.rb
+++ Casks/decrediton.rb
@@ -1,6 +1,6 @@
 cask 'decrediton' do
-  version '1.3.0'
-  sha256 '77753257c7ebaf1b3f77f0d6445d227dff20edd770702ee290140e42250b12aa'
+  version '1.3.1'
+  sha256 '3aad8659b9713a9e787450ed3bdfe8f5f73cb31bb57bc35eb97bb72877d3ef8a'
 
   url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-v#{version}.dmg"
   appcast 'https://github.com/decred/decred-binaries/releases.atom'
