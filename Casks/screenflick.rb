95e3776b35735f0c883d01a6e72fb2bc81bc2bd6
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 12:30:54 -0700

    Update screenflick to 2.7.38 (#53878)

--- Casks/screenflick.rb
+++ Casks/screenflick.rb
@@ -1,6 +1,6 @@
 cask 'screenflick' do
-  version '2.7.37'
-  sha256 '786bed4f46aa7361dbab447b2fa631e2f4d2fe0cd9beedbb1105d701190038ef'
+  version '2.7.38'
+  sha256 'a14fec78920a1992d836460235baed164592e112d4c1f9d742383779d3ab1962'
 
   url "https://store.araelium.com/screenflick/downloads/versions/Screenflick#{version}.zip"
   appcast "https://arweb-assets.s3.amazonaws.com/downloads/screenflick/screenflick#{version.major}.xml"
