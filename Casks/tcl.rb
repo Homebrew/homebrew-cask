f96e818930901b5d241b3a94448e1d080d522f95
Alejandro Martínez <6730082+H1rag4mi@users.noreply.github.com>
2018-10-19 16:22:09 +0200

    Removed duplicated code

--- Casks/tcl.rb
+++ Casks/tcl.rb
@@ -2,22 +2,16 @@ cask 'tcl' do
   version '8.6.8.0'
   sha256 '8788a7c50b6c9d467bdbf7ae473354173feebc75a8b33b175245ccf4171938d1'
 
+  extra = "-#{version.after_comma}" if version.after_comma != ''
+
   # downloads.activestate.com/ActiveTcl was verified as official when first introduced to the cask
-  if version.after_comma == ''
-    url "https://downloads.activestate.com/ActiveTcl/releases/#{version.before_comma}/ActiveTcl-#{version.before_comma}-macosx10.9-x86_64.pkg"
-  else
-    url "https://downloads.activestate.com/ActiveTcl/releases/#{version.before_comma}/ActiveTcl-#{version.before_comma}-macosx10.9-x86_64-#{version.after_comma}.pkg"
-  end
+  url "https://downloads.activestate.com/ActiveTcl/releases/#{version.before_comma}/ActiveTcl-#{version.before_comma}-macosx10.9-x86_64#{extra}.pkg"
   name 'ActiveTcl'
   homepage 'https://tcl.tk/'
 
   depends_on macos: '>= :mavericks'
 
-  if version.after_comma == ''
-    pkg "ActiveTcl-#{version.before_comma}-macosx10.9-x86_64.pkg"
-  else
-    pkg "ActiveTcl-#{version.before_comma}-macosx10.9-x86_64-#{version.after_comma}.pkg"
-  end
+  pkg "ActiveTcl-#{version.before_comma}-macosx10.9-x86_64#{extra}.pkg"
 
   uninstall pkgutil: "com.activestate.pkg.ActiveTcl#{version.major_minor}"
 end
