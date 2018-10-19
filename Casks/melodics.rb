16627e684a393823e75b41041919395d217758fc
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 13:00:04 -0700

    Update melodics to 2.0.2449 (#53855)

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask 'melodics' do
-  version '2.0.2359'
-  sha256 '11b80dd9502f8d1b50c2487344411bcb689792dc31f15668e36affb67aba41da'
+  version '2.0.2449'
+  sha256 '430873f084ccd71b21dd889e6062471973ca4e103060919f050599eccd126b49'
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://api.melodics.com/download/osxupdatescastv#{version.major}.xml"
