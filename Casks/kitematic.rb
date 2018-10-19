c84b05bdce6574b32134dff8a32abc00f2041231
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 11:24:52 -0700

    Update kitematic to 0.17.5

--- Casks/kitematic.rb
+++ Casks/kitematic.rb
@@ -1,6 +1,6 @@
 cask 'kitematic' do
-  version '0.17.3'
-  sha256 'f7822079a1c1747dabeebd95a57f46d50847f77d3f567862e7b624bff309bd2c'
+  version '0.17.5'
+  sha256 'dd697ee05d520e9dbbddb6177ca66a84a02eceb863d24845d1f787d226036e9d'
 
   # github.com/docker/kitematic was verified as official when first introduced to the cask
   url "https://github.com/docker/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
