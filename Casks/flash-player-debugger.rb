f9f304ef050be168745279eab75a6e1f8dbabfbd
Viktor Szakats <commit@vszakats.net>
2018-10-19 08:45:38 +0000

    flash-player-debugger: secure url(s)

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -2,9 +2,8 @@ cask 'flash-player-debugger' do
   version '31.0.0.122'
   sha256 '5845325e56195309e8711dd389b50cc882e0f50f4bd2d40d2f016a4482466b7e'
 
-  # macromedia.com was verified as official when first introduced to the cask
-  url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
-  appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml'
+  url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
+  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml'
   name 'Adobe Flash Player projector content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
