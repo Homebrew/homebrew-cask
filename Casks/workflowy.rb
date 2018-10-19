3fc5e8e0d73437b6d058e513d7aae6fb00855256
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 12:07:43 -0700

    Update workflowy to 1.1.5

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,6 +1,6 @@
 cask 'workflowy' do
-  version '1.1.4'
-  sha256 '72b30fcda33aef584b132d13fc41a2d8321ffc4e01b0f14fea122c764d90dee8'
+  version '1.1.5'
+  sha256 'e7526d369ba67bae8842885410a7dde914c1872236c2c16441927f490098a848'
 
   # github.com/workflowy/desktop was verified as official when first introduced to the cask
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.dmg"
