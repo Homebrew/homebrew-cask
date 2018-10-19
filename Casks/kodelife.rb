ccb27ca0685cabeebb31389b65b3e7e6bc8c2e1f
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 13:28:40 -0700

    fix version to 0.6.5.0

--- Casks/kodelife.rb
+++ Casks/kodelife.rb
@@ -1,6 +1,6 @@
 cask 'kodelife' do
-  version '0.6.4.0'
-  sha256 '164635644fe84f95f02d3ac9c250b6ec64b640c74c87394c6041381bc75534cd'
+  version '0.6.5.0'
+  sha256 'c242773836daaf65aa269517bc20e395e70f4d28853a7eca5880e1633fd32564'
 
   url "https://hexler.net/pub/kodelife/kodelife-#{version}-osx.zip"
   appcast 'https://hexler.net/pub/kodelife/appcast.hex'
