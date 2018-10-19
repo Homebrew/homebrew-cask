24c15ec900a66b721b48e6312eb2bd48fe537adc
Muhammad Abduh <muhammadabduh@macbook-pro.localdomain>
2018-10-19 21:57:17 +0200

    Update funter to 4.3

--- Casks/funter.rb
+++ Casks/funter.rb
@@ -1,8 +1,8 @@
 cask 'funter' do
-  version '4.2'
-  sha256 'f6a06b1a5a8a6356e24781324669b6f41318c65481e191e1a6c49c34e7ec0596'
+  version '4.3'
+  sha256 'd3eeb3d2c07ec0507b408ff73f0fae5f263b7c161a8b684f0bd0079b7e8bf1fd'
 
-  url "https://nektony.com/promo/funter/update/funter_v#{version}.zip"
+  url 'https://nektony.com/download/funter/Funter.dmg'
   appcast 'https://nektony.com/promo/funter/update/update.xml'
   name 'Funter'
   homepage 'https://nektony.com/products/funter'
