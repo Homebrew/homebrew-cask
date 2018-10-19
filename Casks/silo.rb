40c5e4ecbcdd30c80d430195ab0276f87b565b66
Viktor Szakats <commit@vszakats.net>
2018-10-19 08:45:39 +0000

    silo: secure url(s)

--- Casks/silo.rb
+++ Casks/silo.rb
@@ -2,7 +2,7 @@ cask 'silo' do
   version '2.5.5'
   sha256 '7d0a4af414dcfe623a76da5d23fa324651913b2258062e998274e336eb8c29f2'
 
-  url "http://nevercenter.com/silo/download_file/filearchive/Install_Silo_#{version.dots_to_underscores}_mac.dmg"
+  url "https://nevercenter.com/silo/download_file/filearchive/Install_Silo_#{version.dots_to_underscores}_mac.dmg"
   name 'Silo'
   homepage 'https://nevercenter.com/silo/'
 
