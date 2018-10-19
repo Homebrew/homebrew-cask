e61b7731f0a20c8640c2abba6ac883c0e68cabbe
Brian Morton <brianmortondesign@gmail.com>
2018-10-19 13:02:47 -0700

    Update mandrake to 3.2 (#53853)

--- Casks/mandrake.rb
+++ Casks/mandrake.rb
@@ -1,6 +1,6 @@
 cask 'mandrake' do
-  version '3.1'
-  sha256 '020ba2cdbe2ab219e7135311b35ba05e51c65c45e297c392e874a08c54b566f1'
+  version '3.2'
+  sha256 'f828625c31d2f031bc00836e0e9e6a69d59e7f2aa9d480d4c459bc85e20ab191'
 
   url "https://sveinbjorn.org/files/software/mandrake/ManDrake-#{version}.zip"
   appcast 'https://sveinbjorn.org/files/appcasts/ManDrakeAppcast.xml'
