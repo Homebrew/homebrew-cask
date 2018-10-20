d5536f5202c43a6b093d7127f01f60e4042c8318
CoreCode <core-code@users.noreply.github.com>
2018-10-20 19:37:32 +0200

    Update itch.rb

--- Casks/itch.rb
+++ Casks/itch.rb
@@ -1,6 +1,6 @@
 cask 'itch' do
   version '25.0.0'
-  sha256 '8dc76228c8e5e75da5e3c4b32a709762d339d5766fcb7c3a74eca3a75728fa10'
+  sha256 '06a579b135203dd5da5a6e7a0665e4495a2501cf0dbe11f52f8447f3ccd090ca'
 
   # nuts.itch.zone was verified as official when first introduced to the cask
   url 'http://nuts.itch.zone/download/osx'
