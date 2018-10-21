8f8fc3f6fe7ca036387a8440d391889ddf224c0e
CoreCode <core-code@users.noreply.github.com>
2018-10-21 18:41:20 +0200

    Update grids to 5.1 (#53946)

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,6 +1,6 @@
 cask 'grids' do
-  version '5.0.1'
-  sha256 'f43c6f46ac8acbec0273cd098086d916512fbef9ca3c14e43fc2930a1b5df93d'
+  version '5.1'
+  sha256 '41a5086a8e1898d6a1c249fe4a376ee3c828e88210609957b46bad382510c25c'
 
   url "https://gridsapp.net/bin/Grids_#{version}.zip"
   appcast 'https://gridsapp.net/appcast.json'
