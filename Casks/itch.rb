90cae04980f3e1d71626bb32bcdbd9ea1b80b7a2
CoreCode <core-code@users.noreply.github.com>
2018-10-21 19:05:58 +0200

    Update itch to 25.0.0 (#53685)
    * Update itch to 25.0.0
    
    * Update itch.rb
    
    * Update itch.rb
    
    * Update itch.rb
    
    * Update itch.rb
    
    * Update itch.rb
    
    * Update itch.rb
    
    * Update itch.rb
    
    * Update itch.rb
    
    * Update itch.rb
    
    * Update itch.rb

--- Casks/itch.rb
+++ Casks/itch.rb
@@ -1,12 +1,30 @@
 cask 'itch' do
-  version '23.6.3'
-  sha256 '3ed905c4e7e0ad37004dd235a0dcf27325adac10fb829d3bfcd076c25442fda4'
+  version '25.0.0'
+  sha256 '06a579b135203dd5da5a6e7a0665e4495a2501cf0dbe11f52f8447f3ccd090ca'
 
-  # github.com/itchio/itch was verified as official when first introduced to the cask
-  url "https://github.com/itchio/itch/releases/download/v#{version}/itch-mac.dmg"
+  # nuts.itch.zone was verified as official when first introduced to the cask
+  url 'http://nuts.itch.zone/download/osx'
   appcast 'https://github.com/itchio/itch/releases.atom'
   name 'itch'
   homepage 'https://itch.io/app'
 
-  app 'itch.app'
+  container nested: 'Install itch.dmg'
+
+  installer script: 'Install itch.app/Contents/MacOS/itch-setup'
+
+  preflight do
+    set_permissions "#{staged_path}/Install itch.app", '0777'
+  end
+
+  uninstall delete: [
+                      '~/Applications/itch.app',
+                      '~/Library/Application Support/itch-setup/',
+                    ],
+            quit:   'io.itch.mac'
+
+  zap trash: [
+               '~/Library/Application Support/itch/',
+               '~/Library/Preferences/io.itch.mac.helper.plist',
+               '~/Library/Preferences/io.itch.mac.plist',
+             ]
 end
