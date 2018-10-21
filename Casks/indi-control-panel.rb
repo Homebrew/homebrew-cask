422b59874117c8893e641a6714604de48975c227
Julian Mayer <julian@corecode.io>
2018-10-21 19:29:38 +0200

    Update indi-control-panel to 3.5

--- Casks/indi-control-panel.rb
+++ Casks/indi-control-panel.rb
@@ -1,10 +1,10 @@
 cask 'indi-control-panel' do
-  version '2.3'
-  sha256 'c2671bc8d8aef40638242aa3877474251b930a94ab47aa64327749af6f160279'
+  version '3.5'
+  sha256 '4d1402b819e5b1d63bf5308ceab6e73f21fc8589859104f7704d419d15a35868'
 
-  url "http://download.cloudmakers.eu/INDI_Control_Panel_#{version}.dmg"
-  name 'INDI Control Panel'
-  homepage 'http://www.cloudmakers.eu/'
+  url "http://download.cloudmakers.eu/INDIGO_Control_Panel_#{version}.dmg"
+  name 'INDIGO Control Panel'
+  homepage 'http://www.cloudmakers.eu/indicontrolpanel/'
 
-  app 'INDI Control Panel.app'
+  app 'INDIGO Control Panel.app'
 end
