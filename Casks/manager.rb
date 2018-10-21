0824ece1cd95d5601c53d0393092dd472f9e6a36
Julian Mayer <julian@corecode.io>
2018-10-21 11:48:24 +0200

    Update manager to 18.10.81

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -1,6 +1,6 @@
 cask 'manager' do
-  version '18.10.76'
-  sha256 'b2ef902a03e30b042aa7a784d55d2bec08902573f29d2a750226338d0dd915c1'
+  version '18.10.81'
+  sha256 '30e857c4f82fb13408ea39e97b5b19bde21704edb555dcbaadfa555aec58c7f4'
 
   # d2ap5zrlkavzl7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2ap5zrlkavzl7.cloudfront.net/#{version}/Manager.dmg"
