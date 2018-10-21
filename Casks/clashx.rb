5d2b28caa6de725a82cbab2438c37a2d700f2966
cooclsee <cooclsee@qq.com>
2018-10-21 19:24:11 +0800

    Update clashx to 1.6.1

--- Casks/clashx.rb
+++ Casks/clashx.rb
@@ -1,6 +1,6 @@
 cask 'clashx' do
-  version '1.5.5'
-  sha256 '94e078d9c0bc6835cf0039a4f5d59b1444eef5edb5c72972485d431879f62052'
+  version '1.6.1'
+  sha256 '8b02bb3207390c9b8b3f0b9cbc75e041b2a7f7c16e91a8951f22586081f851f0'
 
   url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
   appcast 'https://github.com/yichengchen/clashX/releases.atom'
