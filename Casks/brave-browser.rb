26a268e0ff7f104d134a2c150a32c49efb4229e3
Olivier Brewaeys <olbrew@users.noreply.github.com>
2018-10-21 12:09:06 +0000

    Update Brave.app to 0.55.18 (#53796)
    * Update Brave.app to 0.55.18
    
    Update Brave to new Chromium-based edition
    New Github repository: https://github.com/brave/brave-browser
    New version number: 0.55.18
    
    * Update brave.rb
    
    * Update and rename brave.rb to brave-browser.rb

---
+++ Casks/brave-browser.rb
@@ -0,0 +1,21 @@
+cask 'brave-browser' do
+  version '0.55.18'
+  sha256 '6991ff0aceb1826505e1c5cf98b2b45b12b51b149c73bbf7fb329bdbf9d83414'
+
+  # github.com/brave/brave-browser was verified as official when first introduced to the cask
+  url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser.dmg"
+  appcast 'https://github.com/brave/brave-browser/releases.atom'
+  name 'Brave'
+  homepage 'https://brave.com/'
+
+  auto_updates true
+  depends_on macos: '>= :mavericks'
+
+  app 'Brave Browser.app'
+
+  zap trash: [
+               '~/Library/Application Support/brave',
+               '~/Library/Preferences/com.electron.brave.plist',
+               '~/Library/Saved Application State/com.electron.brave.savedState',
+             ]
+end
