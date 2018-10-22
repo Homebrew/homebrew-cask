8984665c828121d4697b8c2f5a881a7bdf7e92f4
CoreCode <core-code@users.noreply.github.com>
2018-10-22 18:26:25 +0200

    Update hostbuddy.rb

--- Casks/hostbuddy.rb
+++ Casks/hostbuddy.rb
@@ -1,9 +1,9 @@
 cask 'hostbuddy' do
-  version '2.0.2,20_3'
+  version '2.0.3'
   sha256 'ae9a55245b8995a2cbdf3db4db434fd82795cc60dded4a952369c141aa00d012'
 
   # downloads-clickonideas.netdna-ssl.com/hostbuddy was verified as official when first introduced to the cask
-  url "https://downloads-clickonideas.netdna-ssl.com/hostbuddy/hostbuddy#{version.after_comma}.zip"
+  url "https://downloads-clickonideas.netdna-ssl.com/hostbuddy/hostbuddy_#{version}.zip"
   appcast 'https://shine.clickontyler.com/appcast.php?id=41'
   name 'Hostbuddy'
   homepage 'https://clickontyler.com/hostbuddy/'
