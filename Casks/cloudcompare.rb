9378799874915cf722db58120bf7df5616c6aacd
Sebastian Riehm <s.riehm@iwm.rwth-aachend.e>
2018-10-22 14:21:56 +0200

    Update cloudcompare to 2.9.1

--- Casks/cloudcompare.rb
+++ Casks/cloudcompare.rb
@@ -1,6 +1,6 @@
 cask 'cloudcompare' do
-  version :latest
-  sha256 :no_check
+  version '2.9.1'
+  sha256 'c5223b04265802c90a8f81455e80d26f3406881e9e273372390eb42e37d4c62e'
 
   # asmaloney.com/download/cloudcompare-mac-os-x was verified as official when first introduced to the cask
   url 'https://asmaloney.com/download/cloudcompare-mac-os-x/'
