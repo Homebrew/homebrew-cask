f3b4fdaefff7244ec17dfd68dade7e89baf6807e
mashehu <mashehu@users.noreply.github.com>
2018-10-23 16:03:39 +0200

    Update default-folder-x to 5.3.2 (#54002)

--- Casks/default-folder-x.rb
+++ Casks/default-folder-x.rb
@@ -1,6 +1,6 @@
 cask 'default-folder-x' do
-  version '5.2.5'
-  sha256 '164dddeb7b773d1d15e0db7cbb0449dca6e4b044b83f1b9ff523feccf024e3cc'
+  version '5.3.2'
+  sha256 '96049e6cfdde60189fbbc6ef1add3579aa48f5a32f7058fcc5cfab97e99280e8'
 
   url "https://www.stclairsoft.com/download/DefaultFolderX-#{version}.dmg"
   name 'Default Folder X'
