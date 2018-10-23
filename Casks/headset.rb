add892e3291e9588b56779d0f8ae621061a85ea3
Brian Morton <brianmortondesign@gmail.com>
2018-10-23 10:54:40 -0700

    Update headset to 2.0.5 (#54031)

--- Casks/headset.rb
+++ Casks/headset.rb
@@ -1,6 +1,6 @@
 cask 'headset' do
-  version '2.0.4'
-  sha256 '9b694dbf495a497859b1249f3bf30d1fe5b91f778fc696aaf4b6306c37cf8178'
+  version '2.0.5'
+  sha256 'f25ac27ec28c29a82b96552706fa75aa685bd782b18dbf98c74560af60404dad'
 
   # github.com/headsetapp/headset-electron was verified as official when first introduced to the cask
   url "https://github.com/headsetapp/headset-electron/releases/download/v#{version}/Headset-#{version}.dmg"
