26980fe052c0e9ccce2959b6bcdb478de7c6f7c8
commitay <commitay@users.noreply.github.com>
2018-10-23 21:58:30 +1000

    dbeaver-community: fix checksum (#54012)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
   version '5.2.3'
-  sha256 'c8997a31bb6f657ff4d957fa56e355914a41c138bd816f471b88f6ccf724b7a4'
+  sha256 '59696df710ad15e98232726be5009320e242f9bbcaa9858ae1f4336ec124d1d2'
 
   # github.com/dbeaver/dbeaver was verified as official when first introduced to the cask
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
