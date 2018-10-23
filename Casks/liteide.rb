53dc52d21488238c9a53f87ba3928b1c1ac3d6e2
benjamin-yin001 <44402456+benjamin-yin001@users.noreply.github.com>
2018-10-23 20:42:41 +0800

    update to 34.3
    update to 34.3

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask 'liteide' do
-  version '34.2,5.9.5'
-  sha256 '4cee36757151c41ab3299be32edba2542fce12dca7801946123d8fd9f082bdcc'
+  version '34.3,5.9.5'
+  sha256 'c93d6aa1cc5568d4845a511967e25da2bcedc3e03a8283860bef0280e3aa745c'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
   url "https://github.com/visualfc/liteide/releases/download/x#{version.before_comma}/liteidex#{version.before_comma}.macosx-qt#{version.after_comma}.zip"
