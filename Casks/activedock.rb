8834c403bcb8c4d11825432f3b0c86a7939467d5
Brian Morton <brianmortondesign@gmail.com>
2018-10-23 09:26:12 -0700

    Update activedock to 189,1540061961

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,6 +1,6 @@
 cask 'activedock' do
-  version '178,1536924717'
-  sha256 '1b4cfb4a1cff4322f3bdd4f604b6de68e794f4385d001b2ed8daf91e25519446'
+  version '189,1540061961'
+  sha256 'd0370888e8efa451ecec9ec4521755a583bcd0000f5c29bc73f80fd8b3630dd1'
 
   # dl.devmate.com/com.sergey-gerasimenko.ActiveDock was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.sergey-gerasimenko.ActiveDock/#{version.before_comma}/#{version.after_comma}/ActiveDock-#{version.before_comma}.zip"
