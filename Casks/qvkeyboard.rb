5d6a9c07ab1152b62381a7071b14e3f9fccafcfb
everettjf <everettjf@live.com>
2018-10-24 01:55:56 +0800

    QVKeyboardClient 1.2.9

--- Casks/qvkeyboard.rb
+++ Casks/qvkeyboard.rb
@@ -1,11 +1,11 @@
 cask 'qvkeyboard' do
-  version :latest
+  version '1.2.9'
   sha256 :no_check
 
-  url "https://github.com/qvkeyboard/qvkeyboard/releases/download/#{version}/MachOExplorer.#{version}.dmg"
+  url "https://github.com/qvkeyboard/qvkeyboard/releases/download/#{version}/QVKeyboardClient.#{version}.dmg"
   appcast 'https://github.com/qvkeyboard/qvkeyboard/releases.atom'
   name 'QVKeyboardClient'
-  homepage 'https://qvkeyboard.github.io'
+  homepage 'https://qvkeyboard.github.io/'
 
   app 'QVKeyboardClient.app'
 end
