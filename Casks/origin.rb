3491cf8ffc371e61b2ad9a965fae4cc9196058b7
yuusharo <36213375+yuusharo@users.noreply.github.com>
2018-10-23 10:59:38 -0700

    removed appcast
    Removing the appcast to comply with `brew cask audit`. The current version is of Origin at this time is 10.5.29.14153, but the appcast still shows it at 10.5.28.13288.

--- Casks/origin.rb
+++ Casks/origin.rb
@@ -4,7 +4,6 @@ cask 'origin' do
 
   # origin-a.akamaihd.net was verified as official when first introduced to the cask
   url 'https://origin-a.akamaihd.net/Origin-Client-Download/origin/mac/live/Origin.dmg'
-  appcast 'https://api1.origin.com/autopatch/2/upgradeFrom/10.5.24.5022/en_US/PROD?platform=MAC&osVersion=10.13.0'
   name 'Origin'
   homepage 'https://www.origin.com/'
 
