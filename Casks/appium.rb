9ee37019c12c9c61c8ecd277670b2a6352601c83
Brian Morton <brianmortondesign@gmail.com>
2018-10-23 10:56:33 -0700

    Update appium to 1.8.0 (#54054)

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -1,9 +1,9 @@
 cask 'appium' do
-  version '1.7.1'
-  sha256 '1b8a90e9fadf279e0353fad7910fa1ec198bb7423f34643450b177924592c0ba'
+  version '1.8.0'
+  sha256 '80f04956a9f1c766aeef1efe08871285cd613186dbc2e512e49b846d1c97fd45'
 
   # github.com/appium/appium-desktop was verified as official when first introduced to the cask.
-  url "https://github.com/appium/appium-desktop/releases/download/v#{version}/appium-desktop-#{version}-mac.zip"
+  url "https://github.com/appium/appium-desktop/releases/download/#{version}/appium-desktop-#{version}-mac.zip"
   appcast 'https://github.com/appium/appium-desktop/releases.atom'
   name 'Appium Desktop'
   homepage 'https://appium.io/'
