c8552cc52ff2c42cdd406efe343d119aa0074b55
Brian Morton <brianmortondesign@gmail.com>
2018-10-23 10:57:47 -0700

    Update protege to 5.5.0-beta-4 (#54044)

--- Casks/protege.rb
+++ Casks/protege.rb
@@ -1,6 +1,6 @@
 cask 'protege' do
-  version '5.2.0'
-  sha256 'a6f1c06f8740489c51245683e50493d62b7b019ffc409c137eb511a8d1d140be'
+  version '5.5.0-beta-4'
+  sha256 '7ca43f07b6a6bc086e428bfe1e23d0a725177636a32ee41ca6f1b5bb2f36d9a0'
 
   # github.com/protegeproject/protege-distribution was verified as official when first introduced to the cask
   url "https://github.com/protegeproject/protege-distribution/releases/download/v#{version}/Protege-#{version}-os-x.zip"
