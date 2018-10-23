616e1c8cb0aec7580fda26074300a06f6b8abffd
CoreCode <core-code@users.noreply.github.com>
2018-10-23 15:59:27 +0200

    Update downie to 3.4.9,1894 (#53969)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,5 +1,5 @@
 cask 'downie' do
-  version '3.4.8,1894'
+  version '3.4.9,1894'
   sha256 'c1a633c5e7fc9059bb47ab59488c54e574dde2830436575aadcd7049d7018d1e'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.major}_#{version.after_comma}.zip"
