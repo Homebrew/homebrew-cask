732636cdb96f4de54c82aa44c42918f8ec376034
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-10-23 15:00:52 +0100

    Update tankix to visual2-29187 (#54011)

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,8 +1,8 @@
 cask 'tankix' do
-  version 'hydra3-27226'
-  sha256 '06781806612c9c565846c4a65126603912e576e624eb2fdc5ceff5434ce8ffd4'
+  version 'visual2-29187'
+  sha256 'a2ad9c081af4747ddaa114a65b340514f26cd02fc3677091da02eca70f87e17a'
 
-  url "https://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
+  url "http://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
   name 'Tanki X'
   homepage 'https://www.tankix.com/'
 
