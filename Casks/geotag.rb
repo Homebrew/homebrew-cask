314d257db044ad5855a5d370687f1d21169d8d40
suschizu <brigitte@corecode.io>
2018-10-24 14:27:49 +0200

    Update geotag to 4.4.1 (#54063)

--- Casks/geotag.rb
+++ Casks/geotag.rb
@@ -1,13 +1,13 @@
 cask 'geotag' do
-  version '4.4'
-  sha256 '5ab97b2e09bab7386b5c66a8a389797a3f3b8e42dadde4863ad418695596efbe'
+  version '4.4.1'
+  sha256 'e87ac3a09ed5294117d22627067722bb6c774d32bba950cc279b4818c9226818'
 
   url "https://www.snafu.org/GeoTag/GeoTag-#{version}.dmg"
   appcast 'https://www.snafu.org/GeoTag/'
   name 'GeoTag'
   homepage 'https://www.snafu.org/GeoTag/'
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: '>= :sierra'
   depends_on formula: 'exiftool'
 
   app 'GeoTag.app'
