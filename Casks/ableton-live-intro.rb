dcd071526ea0adb84d17f1c1fba755c9eac338e2
Work <brigitte@corecode.io>
2018-10-24 23:45:54 +0200

    Update ableton-live-intro to 10.0.5

--- Casks/ableton-live-intro.rb
+++ Casks/ableton-live-intro.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-intro' do
-  version '10.0.4'
-  sha256 'baacf60916938a7013ad567fa220263fb566e60d63d01d66defc38fd8481835b'
+  version '10.0.5'
+  sha256 '524d8aa918ad6f155a7a3c2352e49605e6e4ba6f4aa5171031e0e71c17a0ad51'
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_64.dmg"
   appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
