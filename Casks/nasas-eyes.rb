12ac0a34736bfec54e6cf50a82ffdc61761cc293
Ryan Duryea <aguynamedryan@gmail.com>
2018-10-24 10:57:36 -0700

    Add NASA's Eyes 2018.08.28-15.35.05-ed9d39a

---
+++ Casks/nasas-eyes.rb
@@ -0,0 +1,10 @@
+cask 'nasas-eyes' do
+  version '2018.08.28-15.35.05-ed9d39a'
+  sha256 'cb5c7a506895f5ff585252e4d6e0d9722aacfda91db90b15865a172425e4e925'
+
+  url 'https://eyes.jpl.nasa.gov/eyesproduct/EYES/os/osx'
+  name "NASA's Eyes"
+  homepage 'https://eyes.jpl.nasa.gov/eyes-on-exoplanets.html'
+
+  app "NASA's Eyes.app"
+end
