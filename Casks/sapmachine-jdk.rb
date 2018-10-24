4781b42cc66ae212ace1713f77ede60da3bb8d28
commitay <commitay@users.noreply.github.com>
2018-10-24 15:31:34 +1000

    Update sapmachine-jdk.rb

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -2,7 +2,7 @@ cask 'sapmachine-jdk' do
   version '11.0.1,13-0'
   sha256 '600fc431d96fbc0b22a3cb160a1b1fbe878b887efd5be99668f3cf9677aa11cc'
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}+#{version.after_comma}/sapmachine-jdk-#{version.before_comma}+#{version.after_comma.split('-').first}_osx-x64_bin.tar.gz"
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}+#{version.after_comma}/sapmachine-jdk-#{version.before_comma}.#{version.after_comma.split('-').first}_osx-x64_bin.tar.gz"
   appcast "https://sap.github.io/SapMachine/latest/#{version.before_comma}"
   name 'SapMachine JDK'
   homepage 'https://github.com/SAP/SapMachine'
