c9c99a56d27524a2fb919797c34e8f3793474409
Victor Popkov <victor@popkov.me>
2018-10-24 19:26:08 +0300

    Update jedit-omega: add newline before comment

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -6,6 +6,7 @@ cask 'jedit-omega' do
     version '2.01'
     sha256 '2e9faaceb7117588eb5b992fed968db2b18bfa498d557d762556dcb882bc5363'
   end
+
   # artman21.co.jp was verified as official when first introduced to the cask
   url "http://download.artman21.co.jp/product/JeditOmega/JeditOmega#{version.no_dots}.dmg"
   name 'Jedit Ω'
