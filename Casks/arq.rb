b48c921776c66c46531246d4557ec618d5fc23cf
Sebastian Riehm <herrriehm@users.noreply.github.com>
2018-10-25 15:06:08 +0200

    Update arq to 5.14.1 (#54119)

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,9 +1,9 @@
 cask 'arq' do
-  version '5.14'
-  sha256 '50e50d5233dcbf3ecf08aa996654802cf74bdbf5a25fbdd4f4396c800f1ce167'
+  version '5.14.1'
+  sha256 '90bda44a0ac760a08decd2e887829557ed7669de9f82975a747729558865391b'
 
-  url "https://www.arqbackup.com/download/Arq_#{version}.zip"
-  appcast "https://www.arqbackup.com/download/arq#{version.major}.xml"
+  url "https://www.arqbackup.com/download/arqbackup/Arq_#{version}.zip"
+  appcast "https://www.arqbackup.com/download/arqbackup/arq#{version.major}.xml"
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
