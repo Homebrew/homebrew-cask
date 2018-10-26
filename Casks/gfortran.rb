5dcd9c0c60a7213dccf3cb9d58c9729b528abc6c
Yihui Xie <xie@yihui.name>
2018-10-26 09:07:20 -0500

    Add appcast

--- Casks/gfortran.rb
+++ Casks/gfortran.rb
@@ -17,6 +17,7 @@ cask 'gfortran' do
 
   name 'gfortran'
   homepage 'https://gcc.gnu.org/wiki/GFortranBinaries'
+  appcast 'https://github.com/fxcoudert/gfortran-for-macOS/releases.atom'
 
   conflicts_with formula: 'gcc'
   depends_on macos: '>= :el_capitan'
