60b39719ce1db4a322a34769e53622c55c019c55
Jason Gorst <jason.gorst@me.com>
2018-10-26 13:00:37 -0500

    Update yakyak to 1.5.2

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,6 +1,6 @@
 cask 'yakyak' do
   version '1.5.2'
-  sha256 'bdeae777191394e21ec089b856eaca805583d70213f6f03e36d33833bcbc41f2'
+  sha256 '84e8a6068eb2e2be0c56b14804e8ffa02cf10e7c53ee71758cd542028cccfc2f'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom'
