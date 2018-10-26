f2c99cec44ee0e1e61f05edfb3e165dfbed99b1d
Yihui Xie <xie@yihui.name>
2018-10-26 08:59:59 -0500

    The gfortran package has been moved to a new URL
    See https://www.coudert.name/software.html

--- Casks/gfortran.rb
+++ Casks/gfortran.rb
@@ -4,14 +4,14 @@ cask 'gfortran' do
     sha256 'eb817bce64bf9032595e09166bdaaf740c83bf7258f900b79cd6786437bacbf4'
 
     # coudert.name/software was verified as official when first introduced to the cask
-    url "http://coudert.name/software/gfortran-#{version}-ElCapitan.dmg"
+    url "https://github.com/fxcoudert/gfortran-for-macOS/releases/download/#{version}/gfortran-#{version}-ElCapitan.dmg"
     pkg "gfortran-#{version}-ElCapitan/gfortran.pkg"
   else
     version '6.3'
     sha256 '38b81bc878dba41cfdbb0c335aec5a97554a5d1766fb3e3ca6be7da0df9e8e09'
 
     # coudert.name/software was verified as official when first introduced to the cask
-    url "http://coudert.name/software/gfortran-#{version}-Sierra.dmg"
+    url "https://github.com/fxcoudert/gfortran-for-macOS/releases/download/#{version}/gfortran-#{version}-Sierra.dmg"
     pkg 'gfortran.pkg'
   end
 
