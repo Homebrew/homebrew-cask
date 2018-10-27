feefe2828d4fb377d0669c66f34e4a67a46337cf
Chris Caraccioli <chris@caraccio.li>
2018-10-26 17:36:47 -0700

    Update downie to 3.4.10,1899

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.4.9,1894'
-  sha256 'c1a633c5e7fc9059bb47ab59488c54e574dde2830436575aadcd7049d7018d1e'
+  version '3.4.10,1899'
+  sha256 'c0ead5624ddcc00131d592f757114225fe0f58128140638a0a26fd38b57eeb46'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.major}_#{version.after_comma}.zip"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml"
