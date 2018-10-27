3c8775468649d18c82f1a9006529a633ab383dab
CoreCode <core-code@users.noreply.github.com>
2018-10-27 13:23:43 +0200

    Update disk-drill to 3.6.918 (#54155)

--- Casks/disk-drill.rb
+++ Casks/disk-drill.rb
@@ -1,6 +1,6 @@
 cask 'disk-drill' do
-  version '3.6.916'
-  sha256 '942e4fff67a832848b6e232b0d13e4ee8b66084d3e45869da553c9e12c7fd888'
+  version '3.6.918'
+  sha256 'ae054877b0ed099a3f8fe407ece0d189f8d0099ae1e61bad9bd7823fc3bfed6c'
 
   url "https://www.cleverfiles.com/releases/DiskDrill_#{version}.zip"
   appcast 'https://www.cleverfiles.com/releases/auto-update/dd2-newestr.xml'
