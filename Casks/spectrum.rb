da12ad62841b5ff63495071088f273447cdf2ef3
Brian Hinton <mrbrianhinton@pm.me>
2018-10-28 12:42:45 -0400

    Added commentper style guidelines for URL

--- Casks/spectrum.rb
+++ Casks/spectrum.rb
@@ -2,9 +2,10 @@ cask 'spectrum' do
   version :latest
   sha256 :no_check
 
+  # github.com/withspectrum was verified as official when first introduced to the cask
   url 'https://github.com/withspectrum/spectrum/releases/download/desktop-v1.0.1/Spectrum-1.0.1-mac.zip'
   name 'Spectrum'
-  homepage 'http://spectrum.chat'
+  homepage 'http://spectrum.chat/'
 
   app 'Spectrum.app'
 end
