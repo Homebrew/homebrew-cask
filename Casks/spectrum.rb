683ac4995d90d9d958595569ea8b8994fafba941
Vítor Galvão <info@vitorgalvao.com>
2018-10-29 17:02:25 +0000

    Update spectrum.rb

--- Casks/spectrum.rb
+++ Casks/spectrum.rb
@@ -3,7 +3,7 @@ cask 'spectrum' do
   sha256 '230b2315c77252568454b168ccd585e6e177e180567367cdba4fe4e5124b5fc1'
 
   # github.com/withspectrum was verified as official when first introduced to the cask
-  url 'https://github.com/withspectrum/spectrum/releases/download/desktop-v1.0.1/Spectrum-1.0.1-mac.zip'
+  url "https://github.com/withspectrum/spectrum/releases/download/desktop-v#{version}/Spectrum-#{version}-mac.zip"
   appcast 'https://github.com/withspectrum/spectrum/releases.atom'
   name 'Spectrum'
   homepage 'http://spectrum.chat/'
