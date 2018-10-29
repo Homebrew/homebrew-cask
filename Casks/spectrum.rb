43d43b552f06749aea97f74f218e6757719fe033
Vítor Galvão <info@vitorgalvao.com>
2018-10-29 17:02:46 +0000

    Update spectrum.rb

--- Casks/spectrum.rb
+++ Casks/spectrum.rb
@@ -2,7 +2,7 @@ cask 'spectrum' do
   version '1.0.1'
   sha256 '230b2315c77252568454b168ccd585e6e177e180567367cdba4fe4e5124b5fc1'
 
-  # github.com/withspectrum was verified as official when first introduced to the cask
+  # github.com/withspectrum/spectrum was verified as official when first introduced to the cask
   url "https://github.com/withspectrum/spectrum/releases/download/desktop-v#{version}/Spectrum-#{version}-mac.zip"
   appcast 'https://github.com/withspectrum/spectrum/releases.atom'
   name 'Spectrum'
