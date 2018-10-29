9dea0bc65264fca964d9645cc6dc51c5ea079bd6
Brian Hinton <mrbrianhinton@pm.me>
2018-10-29 09:38:48 -0400

    Added sha256, appcast and version

--- Casks/spectrum.rb
+++ Casks/spectrum.rb
@@ -1,11 +1,12 @@
 cask 'spectrum' do
-  version :latest
-  sha256 :no_check
+  version '1.0.1'
+  sha256 '230b2315c77252568454b168ccd585e6e177e180567367cdba4fe4e5124b5fc'
 
   # github.com/withspectrum was verified as official when first introduced to the cask
-  url 'https://github.com/withspectrum/spectrum/releases/download/desktop-v1.0.1/Spectrum-1.0.1-mac.zip'
+  url 'https://github.com/withspectrum/spectrum/releases/download/desktop-v#{version}/Spectrum-1.0.1-mac.zip'
   name 'Spectrum'
   homepage 'http://spectrum.chat/'
+  appcast 'https://github.com/withspectrum/spectrum/releases.atom'
 
   app 'Spectrum.app'
 end
