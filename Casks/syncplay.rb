03eee70d6c6fec498ac8ef64633b1d45de510689
James <james@hicag.org>
2018-10-29 12:54:21 -0500

    Add Syncplay 1.6.0 (#54166)
    * Add Syncplay 1.6.0
    
    * Style/WordArray: Use [] for an array of words
    
    * Eliminate flight stanzas, use caveats
    
    * Update syncplay.rb

---
+++ Casks/syncplay.rb
@@ -0,0 +1,24 @@
+cask 'syncplay' do
+  version '1.6.0'
+  sha256 '50cb1c5a1d247cf097067c8ef56745366cf63fd916c68d26be3763959195498f'
+
+  # github.com/Syncplay/syncplay was verified as official when first introduced to the cask
+  url "https://github.com/Syncplay/syncplay/releases/download/v#{version}/Syncplay_#{version}.dmg"
+  appcast 'https://github.com/Syncplay/syncplay/releases.atom'
+  name 'Syncplay'
+  homepage 'https://syncplay.pl/'
+
+  auto_updates true
+
+  app 'Syncplay.app'
+
+  zap trash: [
+               '~/.syncplay',
+               '~/Library/Saved Application State/pl.syncplay.Syncplay.savedState',
+               '~/Library/Preferences/com.syncplay.MoreSettings.plist',
+               '~/Library/Preferences/com.syncplay.Interface.plist',
+               '~/Library/Preferences/com.syncplay.MainWindow.plist',
+               '~/Library/Preferences/pl.syncplay.Syncplay.plist',
+               '~/Library/Preferences/com.syncplay.PlayerList.plist',
+             ]
+end
