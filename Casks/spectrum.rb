6fceba4fcd4d04649cf76fcc44c946a1321e62ef
Brian Hinton <mrbrianhinton@pm.me>
2018-10-29 09:44:17 -0400

    Corrected hash and styling issues

--- Casks/spectrum.rb
+++ Casks/spectrum.rb
@@ -1,12 +1,12 @@
 cask 'spectrum' do
   version '1.0.1'
-  sha256 '230b2315c77252568454b168ccd585e6e177e180567367cdba4fe4e5124b5fc'
+  sha256 '230b2315c77252568454b168ccd585e6e177e180567367cdba4fe4e5124b5fc1'
 
   # github.com/withspectrum was verified as official when first introduced to the cask
-  url 'https://github.com/withspectrum/spectrum/releases/download/desktop-v#{version}/Spectrum-1.0.1-mac.zip'
+  url 'https://github.com/withspectrum/spectrum/releases/download/desktop-v1.0.1/Spectrum-1.0.1-mac.zip'
+  appcast 'https://github.com/withspectrum/spectrum/releases.atom'
   name 'Spectrum'
   homepage 'http://spectrum.chat/'
-  appcast 'https://github.com/withspectrum/spectrum/releases.atom'
 
   app 'Spectrum.app'
 end
