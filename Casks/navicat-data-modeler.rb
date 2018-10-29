a8205ed04a9c42b4fbc3f4c3489033cc728ce177
Santiago Maturana Arce <santiago.maturana@colaboradoresunidadeditorial.es>
2018-10-29 13:41:26 +0100

    Updated to v2.1.20.

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -1,5 +1,5 @@
 cask 'navicat-data-modeler' do
-  version '2.1.19'
+  version '2.1.20'
   sha256 'e91803229fdb610f47210d299b6162685f47972b5997d0c0e76b2984fa24b34b'
 
   url "http://download.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
