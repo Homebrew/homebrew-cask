d7fe214854f79546cb4114ae32c8696d522f32b6
Dave Meikle <david@meikle.io>
2018-10-30 01:15:22 +0000

    Updated Ocelot 3.0 Cask

--- Casks/ocelot.rb
+++ Casks/ocelot.rb
@@ -2,6 +2,8 @@ cask 'ocelot' do
   version '3.0'
   sha256 '1c53823f11bc2fb8952a7b23b6b31e5196571990e8b1ffdae9dc970c2d0e9eb1'
 
+  # bintray.com/okapi was verified as official when first introduced to the cask
+ was verified as official when first introduced to the cask
   url "https://bintray.com/okapi/Distribution/download_file?file_path=Ocelot-#{version}.dmg"
   name 'Ocelot'
   homepage 'https://open.vistatec.com/ocelot/'
@@ -13,5 +15,4 @@ cask 'ocelot' do
   caveats do
     depends_on_java '8+'
   end
-
 end
