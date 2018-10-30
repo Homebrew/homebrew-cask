969fcb880a601d0217e5338dc945121efe826b2f
CoreCode <core-code@users.noreply.github.com>
2018-10-30 23:26:42 +0100

    Update piezo to 1.5.11 (#54257)

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -1,9 +1,9 @@
 cask 'piezo' do
-  version '1.5.10'
-  sha256 '5b812b49da60705e13fa710c0ea901599f46c263db99f2292944b511ab71df69'
+  version '1.5.11'
+  sha256 '5854f52282071ace571d3d1a8ec026923489979eaa01aede220c91bf53cedb9e'
 
   url 'https://rogueamoeba.com/piezo/download/Piezo.zip'
-  appcast 'https://rogueamoeba.com/piezo/releasenotes.php'
+  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo&system=10114'
   name 'Piezo'
   homepage 'https://rogueamoeba.com/piezo/'
 
