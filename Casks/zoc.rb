e04ff91701e77f8c3cbde35650204edc49b7be3d
CoreCode <core-code@users.noreply.github.com>
2018-10-31 15:37:19 +0100

    Update zoc to 7.21.2 (#54293)

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask 'zoc' do
-  version '7.21.1'
-  sha256 '596dc212ada220e51a6bc0142c2b2abb0eff1655bbc8a72a34e0e9debae89012'
+  version '7.21.2'
+  sha256 'a3adb6f9491ce22478ed2e5a6c4e605a47e6662c440677ee6b84fcce81eecf24'
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast 'https://www.emtec.com/downloads/zoc/zoc_changes.txt'
