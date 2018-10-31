05e8f5aee612a66459098bf80b8477db95907c72
CoreCode <core-code@users.noreply.github.com>
2018-11-01 00:47:54 +0100

    Update simple-hub.rb

--- Casks/simple-hub.rb
+++ Casks/simple-hub.rb
@@ -1,11 +1,11 @@
 cask 'simple-hub' do
-  version '5.6.1'
+  version '5.6.1-1777'
   sha256 '3519df0c5ce82a57683a624ac10d2f7e209e71ad7e6e0d2f802fe53f1477780a'
 
-  url 'https://www.simplecontrol.com/b/SimpleHub.dmg'
+  url "https://www.simplecontrol.com/b/SimpleHub-macOS-#{version.no_dots}.zip"
   appcast 'https://www.simplecontrol.com/b/Simple-HubAppcast5.xml'
   name 'Simple Hub'
-  homepage 'https://www.simplecontrol.com/sc5release/'
+  homepage 'https://www.simplecontrol.com/'
 
   app 'Simple Hub.app'
 
