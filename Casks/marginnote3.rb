8d5c9dc6eeaeb04f2d59f18ade93803d78ceb00f
Jiaxin Shou <shoujiaxin@gmail.com>
2018-10-31 22:37:20 +0800

    Add MarginNote 3.app v3.1.4

---
+++ Casks/marginnote3.rb
@@ -0,0 +1,10 @@
+cask 'marginnote3' do
+  version '3.1.4'
+  sha256 '05cbea2a0d0b8fbf5940a1b3039d9f7610fa7156d85cbe5dc15fb46ae1f63588'
+
+  url 'https://s3.amazonaws.com/marginnote-product/macapp/MarginNote3.dmg'
+  name 'MarginNote 3'
+  homepage 'https://www.marginnote.com'
+
+  app 'MarginNote 3.app'
+end
