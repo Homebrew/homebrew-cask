72415a500d969de52bd9ada22c29b956de5947cb
CoreCode <core-code@users.noreply.github.com>
2018-10-31 20:47:40 +0100

    Update spamsieve to 2.9.34 (#54281)

--- Casks/spamsieve.rb
+++ Casks/spamsieve.rb
@@ -1,8 +1,9 @@
 cask 'spamsieve' do
-  version '2.9.33'
-  sha256 '709b65bbc3b825885bc8f9fe1e43b62f43f8a24d3604e586e95e903647470b40'
+  version '2.9.34'
+  sha256 '72a209bb0b09bacf6190b086116e838ad27a5fa4996887ee5e2dee5d8cbb59d6'
 
   url "https://c-command.com/downloads/SpamSieve-#{version}.dmg"
+  appcast 'https://c-command.com/spamsieve/'
   name 'SpamSieve'
   homepage 'https://c-command.com/spamsieve/'
 
