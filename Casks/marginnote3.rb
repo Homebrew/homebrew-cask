97fb6461886e775803b5713ed62cfb8a01892adc
Vítor Galvão <info@vitorgalvao.com>
2018-10-31 20:15:18 +0000

    Update marginnote3.rb

--- Casks/marginnote3.rb
+++ Casks/marginnote3.rb
@@ -3,11 +3,11 @@ cask 'marginnote3' do
   sha256 '05cbea2a0d0b8fbf5940a1b3039d9f7610fa7156d85cbe5dc15fb46ae1f63588'
 
   # s3.amazonaws.com/marginnote-product/macapp was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/marginnote-product/macapp/MarginNote3.dmg'
-  name 'MarginNote 3'
+  url "https://s3.amazonaws.com/marginnote-product/macapp/MarginNote#{version.major}.dmg"
+  name 'MarginNote'
   homepage 'https://www.marginnote.com/'
 
-  app 'MarginNote 3.app'
+  app "MarginNote #{version.major}.app"
 
   zap trash: [
                '~/Library/Application Support/QReader.MarginStudyMac',
