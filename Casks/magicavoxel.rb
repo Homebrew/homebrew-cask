c946451b7c7c11274931c3c9e480d565d4aa9ecd
Vítor Galvão <info@vitorgalvao.com>
2018-10-31 11:54:41 +0000

    Update magicavoxel to 0.99.2-alpha (#54244)
    * Update magicavoxel to 0.99.2-alpha
    
    * Update magicavoxel.rb

--- Casks/magicavoxel.rb
+++ Casks/magicavoxel.rb
@@ -1,11 +1,11 @@
 cask 'magicavoxel' do
-  version '0.99.1-alpha'
-  sha256 'a1dac17cc9a8062e29cc7e584d5be1213d4a73df8b94d8ec4b2d737232d3a78f'
+  version '0.99.2-alpha'
+  sha256 'b4e2587781512ca102dada015802d4f9269ff47eff5614c901ce795d83afb777'
 
   # 192.241.207.218/uploads was verified as official when first introduced to the cask
   url "http://192.241.207.218/uploads/MagicaVoxel-#{version}-mac.zip"
   name 'MagicaVoxel'
   homepage 'https://ephtracy.github.io/'
 
-  suite "MagicaVoxel-#{version}-mac"
+  suite staged_path, target: 'MagicaVoxel'
 end
