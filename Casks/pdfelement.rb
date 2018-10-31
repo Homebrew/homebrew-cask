1520702c7a1aeb5ce1e2de4d62c89760565e6650
Julian Mayer <julian@corecode.io>
2018-10-31 10:24:34 +0100

    Update pdfelement to 6.7.6,2991

--- Casks/pdfelement.rb
+++ Casks/pdfelement.rb
@@ -1,6 +1,6 @@
 cask 'pdfelement' do
-  version '6.7.5,2991'
-  sha256 '71ae9e2c7cc4f76401b0df90a616c9718079f8ca56e7e6170a67684d64c0aa34'
+  version '6.7.6,2991'
+  sha256 'ac293559a0d770ee1278cc87850ef795d45145dae404c17e95fb7dff96666d89'
 
   url "http://download.wondershare.com/cbs_down/mac-pdfelement#{version.major}_full#{version.after_comma}.dmg"
   name 'Wondershare PDFelement for Mac'
