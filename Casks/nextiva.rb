a849f0342db5a5cf92eaa76d14a1d99db36caddb
Julian Mayer <julian@corecode.io>
2018-10-31 14:01:18 +0100

    Update nextiva to 22.5.0.623

--- Casks/nextiva.rb
+++ Casks/nextiva.rb
@@ -3,6 +3,7 @@ cask 'nextiva' do
   sha256 '1b36c08b0a2b127cbb1ad501fb253d176bb1a479f28e01e974338d955f0a672e'
 
   url "https://dm.nextiva.com/dms/bc/pc/Nextiva_App.bc-uc.osx-#{version}.dmg"
+  appcast 'https://www.nextiva.com/support/articles/nextiva-app-mac-setup.html'
   name 'Nextiva'
   homepage 'https://www.nextiva.com/support/articles/nextiva-app-mac-setup.html'
 
