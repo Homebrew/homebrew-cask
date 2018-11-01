b1187840558658bde1ae8100f8ba520683d04f50
Tommy Chin Jr <tommychinjr@users.noreply.github.com>
2018-11-01 03:48:49 -0400

    Change adobe-acrobat-reader to use https

--- Casks/adobe-acrobat-reader.rb
+++ Casks/adobe-acrobat-reader.rb
@@ -2,7 +2,7 @@ cask 'adobe-acrobat-reader' do
   version '19.008.20071'
   sha256 '32bf967bfb96ec994623c9f914efb2a1fedd1e903aecc871e1e6a7998a74c18b'
 
-  url "http://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots}/AcroRdrDC_#{version.no_dots}_MUI.dmg"
+  url "https://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots}/AcroRdrDC_#{version.no_dots}_MUI.dmg"
   appcast 'https://get.adobe.com/reader/'
   name 'Adobe Acrobat Reader DC'
   homepage 'https://acrobat.adobe.com/us/en/acrobat/pdf-reader.html'
