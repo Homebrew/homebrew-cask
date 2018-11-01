fad45814861c4d28ae35edccd44f1d2471d7eb1b
CoreCode <core-code@users.noreply.github.com>
2018-11-01 01:09:31 +0100

    Update querious.rb

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -3,7 +3,7 @@ cask 'querious' do
   sha256 '1d4d59dbb4c62759d9c7c0abacec9e41088c496bffa46f2d15050ba74606399f'
 
   url "https://www.araelium.com/querious/downloads/versions/Querious#{version}.zip"
-  appcast 'https://arweb-assets.s3.amazonaws.com/downloads/querious/prerelease-updates.xml'
+  appcast 'https://arweb-assets.s3.amazonaws.com/downloads/querious/release-updates.xml'
   name "Querious #{version.major}"
   homepage 'https://www.araelium.com/querious/'
 
