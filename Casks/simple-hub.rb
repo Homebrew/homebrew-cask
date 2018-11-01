f142d0dfde49675cc7cf78564acfc209fbc5313c
Vítor Galvão <info@vitorgalvao.com>
2018-11-01 17:06:05 +0000

    Update simple-hub.rb

--- Casks/simple-hub.rb
+++ Casks/simple-hub.rb
@@ -3,7 +3,7 @@ cask 'simple-hub' do
   sha256 '35b2da18591ab9e474e68c05f806b8d87eb903bcaca8165b8e7301c9fce6553b'
 
   url "https://www.simplecontrol.com/b/SimpleHub-macOS-#{version.no_dots}.zip"
-  appcast 'https://www.simplecontrol.com/b/Simple-HubAppcast5.xml'
+  appcast "https://www.simplecontrol.com/b/Simple-HubAppcast#{version.major}.xml"
   name 'Simple Hub'
   homepage 'https://www.simplecontrol.com/'
 
