f64ec7692ac90f805386cf6194d8744cfa282496
Work <brigitte@corecode.io>
2018-11-01 21:38:34 +0100

    Update bitwig-studio to 2.4.2

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask 'bitwig-studio' do
-  version '2.4'
-  sha256 '17cc4d957bb787f8419c0482f42d7b3a2b381bc45966a8c3c9b3a2c2649b3022'
+  version '2.4.2'
+  sha256 'd1e44ad91da8548be138ef959616cc680090b8615c4b571ff0d9f1015f760468'
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   name 'Bitwig Studio'
