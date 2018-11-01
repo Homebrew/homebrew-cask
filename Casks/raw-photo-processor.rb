037fadf1b4db8d20adcdb4f71b6373d5ecbe5aed
apropostech <info@apropostech.com>
2018-11-01 04:21:37 +0100

    Update Raw Photo Processor to release 1864 (#54313)
    * Update Raw Photo Processor to release 1864
    
    Add version and SHA256 to Raw Photo Processor in order to download latest releases.
    
    * Update raw-photo-processor.rb

--- Casks/raw-photo-processor.rb
+++ Casks/raw-photo-processor.rb
@@ -1,8 +1,8 @@
 cask 'raw-photo-processor' do
-  version :latest
-  sha256 :no_check
+  version '1864Beta'
+  sha256 'f0d3419b2a773708093c4e5e33859800573da19637a3585ffd43625318480fe3'
 
-  url 'https://www.raw-photo-processor.com/RPP/RPP_64.zip'
+  url "https://www.raw-photo-processor.com/RPP/RPP64_#{version}.zip"
   name 'Raw Photo Processor'
   homepage 'https://www.raw-photo-processor.com/RPP/Overview.html'
 
