22e48db721c662cafad3f801f190abf06f7ee545
Julian Mayer <julian@corecode.io>
2018-11-01 11:22:41 +0100

    Update glueprint to 1.1

--- Casks/glueprint.rb
+++ Casks/glueprint.rb
@@ -3,7 +3,7 @@ cask 'glueprint' do
   sha256 '3a11d1150dd00b0e880ea0543aafc1bba5756a27f896a5e151a10d920595e747'
 
   url "http://glueprintapp.com/static/download/GluePrint-#{version}.app.zip"
-  appcast 'https://glueprint-update.s3.amazonaws.com/appcast.xml'
+  appcast 'http://glueprintapp.com/'
   name 'GluePrint'
   homepage 'http://glueprintapp.com/'
 
