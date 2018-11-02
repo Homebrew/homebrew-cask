ecdb2cd2b1b78fc6a4e0aa74407db82e267a67e3
CoreCode <core-code@users.noreply.github.com>
2018-11-02 21:31:07 +0100

    Update expressvpn to 6.7.10.4682 (#54446)

--- Casks/expressvpn.rb
+++ Casks/expressvpn.rb
@@ -3,6 +3,7 @@ cask 'expressvpn' do
   sha256 '3106173b8629008a7eaf1b5ab74ac0cc018440e2eabd7386502b1703c77b79c6'
 
   url "https://download.expressvpn.xyz/clients/mac/expressvpn-install_v#{version}.pkg"
+  appcast 'https://www.expressvpn.xyz/vpn-software/vpn-mac'
   name 'ExpressVPN'
   homepage 'https://www.expressvpn.xyz/'
 
