c5c6a0b65d2160379f0e11055011cbdbf80f924d
Sebastian Riehm <herrriehm@users.noreply.github.com>
2018-11-02 12:02:52 +0100

    Update eaglefiler to 1.8.4 (#54123)
    * Update eaglefiler to 1.8.4
    
    * removed old macOS-specific versions

--- Casks/eaglefiler.rb
+++ Casks/eaglefiler.rb
@@ -3,6 +3,7 @@ cask 'eaglefiler' do
   sha256 '7933b9f74f5917ac2e0f59d7f81dead4c2131c298ac1faefa831b21fd52a768b'
 
   url "https://c-command.com/downloads/EagleFiler-#{version}.dmg"
+  appcast 'https://c-command.com/eaglefiler/help/version-history'
   name 'EagleFiler'
   homepage 'https://c-command.com/eaglefiler/'
 
