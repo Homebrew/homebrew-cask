510d4e9de0fe565b0fa6fa1dea33c63a587c83e2
Shiva Huang <shiva.tw@gmail.com>
2018-11-02 21:44:28 +0800

    Add RcloneOSX.app v1.6.3 (#53603)
    * Add RcloneOSX.app v1.6.3
    
    * Fix offense detected by style check in RcloneOSX Cask

---
+++ Casks/rcloneosx.rb
@@ -0,0 +1,13 @@
+cask 'rcloneosx' do
+  version '1.6.3'
+  sha256 '7dd1cf203382a7d4cd71dd18495410aa58ddfcff478884e5cdadd9eb807b60ef'
+
+  url "https://github.com/rsyncOSX/rcloneosx/releases/download/v#{version}/RcloneOSX-#{version}.dmg"
+  appcast 'https://github.com/rsyncOSX/rcloneosx/releases.atom'
+  name 'RcloneOSX'
+  homepage 'https://github.com/rsyncOSX/rcloneosx'
+
+  depends_on formula: 'rclone'
+
+  app 'RcloneOSX.app'
+end
