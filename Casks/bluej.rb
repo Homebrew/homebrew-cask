1802b352236374ac5db71540ca19b50317cf1e80
Ms. Melody Lam <ladyadalovelace@users.noreply.github.com>
2018-11-01 21:37:41 -0400

    Update bluej to 4.1.4 (#54382)

--- Casks/bluej.rb
+++ Casks/bluej.rb
@@ -1,6 +1,6 @@
 cask 'bluej' do
-  version '4.1.3'
-  sha256 'f7fdd3c94912cec06da69f8db482f7f41ad115026e73f767c1ff62d18ce60081'
+  version '4.1.4'
+  sha256 '996f9d991aaa8fa56d3757fca45bf05d2c2fd80a5b51611588e7c802ab8e2d47'
 
   url "https://www.bluej.org/download/files/BlueJ-mac-#{version.no_dots}.zip"
   name 'BlueJ'
