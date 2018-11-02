39be2aa4374bba1dbd8d2b9740ec1ca1478f3be3
Sebastian Riehm <s.riehm@iwm.rwth-aachend.e>
2018-11-02 13:25:19 +0100

    Update spamsieve to 2.9.34

--- Casks/spamsieve.rb
+++ Casks/spamsieve.rb
@@ -3,7 +3,7 @@ cask 'spamsieve' do
   sha256 '72a209bb0b09bacf6190b086116e838ad27a5fa4996887ee5e2dee5d8cbb59d6'
 
   url "https://c-command.com/downloads/SpamSieve-#{version}.dmg"
-  appcast 'https://c-command.com/spamsieve/'
+  appcast 'https://c-command.com/spamsieve/help/version-history'
   name 'SpamSieve'
   homepage 'https://c-command.com/spamsieve/'
 
