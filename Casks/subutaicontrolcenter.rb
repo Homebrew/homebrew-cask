df9e8fb80d7ba68910cc68087cebd9f8940658c8
CoreCode <core-code@users.noreply.github.com>
2018-11-02 12:46:20 +0100

    Update subutaicontrolcenter.rb

--- Casks/subutaicontrolcenter.rb
+++ Casks/subutaicontrolcenter.rb
@@ -2,7 +2,6 @@ cask 'subutaicontrolcenter' do
   version '7.2.2'
   sha256 'd0c0801ed6978cfb5ed8a20d0cafdf73cc066589306781be5bfe05c56bcd2d2a'
 
-  # cdn.subutai.io:8338/kurjun/rest/raw was verified as official when first introduced to the cask
   url 'https://bazaar.subutai.io/rest/v1/cdn/raw?name=subutai-control-center.pkg&latest&download'
   appcast 'https://github.com/subutai-io/control-center/releases.atom'
   name 'Subutai Control Center'
