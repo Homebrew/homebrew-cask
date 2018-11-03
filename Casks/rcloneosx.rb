6a774c18ae838faa4788b9a95338fa064e0fa46f
Shiva Huang <shiva.tw@gmail.com>
2018-11-03 22:04:06 +0800

    Update rcloneosx to 1.6.5 (#54457)

--- Casks/rcloneosx.rb
+++ Casks/rcloneosx.rb
@@ -1,6 +1,6 @@
 cask 'rcloneosx' do
-  version '1.6.3'
-  sha256 '7dd1cf203382a7d4cd71dd18495410aa58ddfcff478884e5cdadd9eb807b60ef'
+  version '1.6.5'
+  sha256 '8ded0b44654f6d484e19703e756ef281a8663a09cad8659680a48864cdca2742'
 
   url "https://github.com/rsyncOSX/rcloneosx/releases/download/v#{version}/RcloneOSX-#{version}.dmg"
   appcast 'https://github.com/rsyncOSX/rcloneosx/releases.atom'
