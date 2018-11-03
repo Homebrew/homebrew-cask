349900442f234a6721de11ad138ebd2d4d104f06
Lanik <LanikSJ@users.noreply.github.com>
2018-11-03 07:04:42 -0700

    Update foxitreader to 2.4.4.0903 (#54460)

--- Casks/foxitreader.rb
+++ Casks/foxitreader.rb
@@ -1,6 +1,6 @@
 cask 'foxitreader' do
-  version '2.4.1.0609'
-  sha256 'a92e4c2af2f22381651f255603816704a265e961b485b7f4b95c50c7b8e66151'
+  version '2.4.4.0903'
+  sha256 '18d32ad8a01879aedc54c190df13cdb48a2932aa565fea12e242d087800c3e5f'
 
   url "https://cdn09.foxitsoftware.com/pub/foxit/reader/desktop/mac/#{version.major}.x/#{version.major_minor}/en_us/FoxitReader.#{version}.enu.setup.pkg"
   name 'Foxit Reader'
