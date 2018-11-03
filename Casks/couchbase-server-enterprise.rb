4396dbf06b33410a5fac283a3d5fb3eec363684d
Julian Mayer <julian@corecode.io>
2018-11-03 17:46:38 +0100

    Update couchbase-server-enterprise to 6.0.0

--- Casks/couchbase-server-enterprise.rb
+++ Casks/couchbase-server-enterprise.rb
@@ -3,8 +3,8 @@ cask 'couchbase-server-enterprise' do
     version '4.5.1'
     sha256 'de014c7c134eb97ff00be6b2e6f5d0da84295ce05bbb7bb3a4d3c747a365cd22'
   else
-    version '4.6.1'
-    sha256 'f1a8183bcc4c9b21db4c85a43fb18776f94be2843266a36cf401e47bf37c16b6'
+    version '6.0.0'
+    sha256 '5e0a8e9fb176905b3bab7b72a8474a3fb6be1b36790b1e2eb30de82249d5bbbc'
   end
 
   url "https://packages.couchbase.com/releases/#{version}/couchbase-server-enterprise_#{version}-macos_x86_64.zip"
