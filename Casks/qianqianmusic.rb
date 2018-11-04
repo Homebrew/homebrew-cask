1cfcce2914374527ee91628a246740575d357789
Markus Reiter <me@reitermark.us>
2018-11-04 17:11:41 +0100

    Update qianqianmusic.rb

--- Casks/qianqianmusic.rb
+++ Casks/qianqianmusic.rb
@@ -5,9 +5,8 @@ cask 'qianqianmusic' do
   # download.qianqian.com was verified as official when first introduced to the cask
   url 'https://download.qianqian.com/channel/2/web_daohang'
   name 'Qianqian Music'
-  name 'Baidu Music'
-  name '百度音乐'
-  homepage 'https://music.baidu.com/'
+  name '千千音乐'
+  homepage 'https://music.taihe.com/'
 
   app 'QianqianMusic.app'
 end
