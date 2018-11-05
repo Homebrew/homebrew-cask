1b78647a8835c7b19ff773a2f16792444c6c4459
suschizu <brigitte@corecode.io>
2018-11-05 15:55:12 +0100

    Update keep-it to 1.5.5 (#54511)

--- Casks/keep-it.rb
+++ Casks/keep-it.rb
@@ -1,11 +1,13 @@
 cask 'keep-it' do
-  version '1.5.3'
-  sha256 'ea4f0fef7279942cff345fbb138c4f108bc8d2c822985644d37b06323f1c0ac6'
+  version '1.5.5'
+  sha256 'a1e07da55c6225958bb372d925f0c43a7293f4230931e150cbcb9e5f0e318c63'
 
   url "https://reinventedsoftware.com/keepit/downloads/KeepIt_#{version}.dmg"
   appcast 'https://reinventedsoftware.com/keepit/downloads/keepit.xml'
   name 'Keep It'
   homepage 'https://reinventedsoftware.com/keepit/'
+
+  depends_on macos: '>= :high_sierra'
 
   app 'Keep It.app'
 
