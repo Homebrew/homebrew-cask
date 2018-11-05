58a78de7f74cecd314833d8788b23c201f73a652
Julian Mayer <julian@corecode.io>
2018-11-05 19:38:20 +0100

    Update mat to 1.8.1.20180910

--- Casks/mat.rb
+++ Casks/mat.rb
@@ -1,6 +1,6 @@
 cask 'mat' do
-  version '1.8.0.20180604'
-  sha256 '7cce1d33d8dd3f076c7ae574923b7ad1ce4ea947b8e9ff3f0f88013c8061e9b4'
+  version '1.8.1.20180910'
+  sha256 'f4510b2a3fe1d4ae708dddb280926ee7b86c58fe42dd0705f7fead351a2dceef'
 
   url "https://www.eclipse.org/downloads/download.php?r=1&file=/mat/#{version.major_minor}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.x86_64.zip"
   name 'Eclipse Memory Analyzer'
