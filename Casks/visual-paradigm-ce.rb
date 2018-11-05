63569feeb4dc196df6515519df0a071f3f41e411
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2018-11-05 15:55:54 +0100

    Upgrade Visual Paradigm CE to 15.1 (20181007) (#54506)

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,8 +1,8 @@
 cask 'visual-paradigm-ce' do
-  version '15.1,20181005'
-  sha256 '5f53b911fac9d30e4d44b08c539d6ad6b8d7f657adcb5a32b23b1618327ad4e6'
+  version '15.1,20181007'
+  sha256 'aa3b0ec294c2fdec3845fbcbaa0c3f1244bf98333d561f4e8e06d79ecc97ea7b'
 
-  url "https://usa6.visual-paradigm.com/visual-paradigm/vpce#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
+  url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html'
   name 'Visual Paradigm Community Edition'
   homepage 'https://www.visual-paradigm.com/'
