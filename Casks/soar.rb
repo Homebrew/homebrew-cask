12afec204c681460864ef7f32574d009e8f74cbb
martianzhang <martianzhang@hotmail.com>
2018-11-07 00:21:59 +0800

    fix audit issue

--- Casks/soar.rb
+++ Casks/soar.rb
@@ -3,8 +3,9 @@ cask 'soar' do
   sha256 '1938433fcba35e40757489bfc135b7668ad614e99cc6c1cdc05f76fe2d6f9e45'
 
   url 'https://github.com/XiaoMi/soar/releases/download/v0.8.1/soar.darwin-amd64'
+  appcast 'https://github.com/XiaoMi/soar/releases.atom'
   name 'SQL Optimizer And Rewriter'
   homepage 'https://github.com/XiaoMi/soar'
 
-  binary 'soar.darwin-amd64',     target: 'soar'
+  binary 'soar.darwin-amd64', target: 'soar'
 end
