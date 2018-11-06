852d45fde81be740eb2c38266b86a28eb75e6b71
CoreCode <core-code@users.noreply.github.com>
2018-11-06 17:17:26 +0100

    Update printnode.rb

--- Casks/printnode.rb
+++ Casks/printnode.rb
@@ -2,7 +2,7 @@ cask 'printnode' do
   version '4.19.0'
   sha256 'b9a5903168832858afbda81453c58c40920762bb1003c2520e2299a35a2d64e6'
 
-  url "https://app.printnode.com/bundles/printnodemain/downloads/printnode/#{version}/PrintNode-#{version}.dmg"
+  url "https://dl.printnode.com/client/printnode/#{version}/PrintNode-#{version}.dmg"
   appcast 'https://www.printnode.com/download'
   name 'PrintNode'
   homepage 'https://www.printnode.com/'
