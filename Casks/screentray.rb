7bbaf76918428a0c2ebeece93e3cd820c46ef46d
Matt Wean <matthew.wean@gmail.com>
2018-11-05 22:14:24 -0800

    Use dmg download instead of zip

--- Casks/screentray.rb
+++ Casks/screentray.rb
@@ -3,7 +3,7 @@ cask 'screentray' do
   sha256 'c014c85574196fd45550ebe964047d9e385bb405f7631f51814f944aa1a46210'
 
   # github.com/DSnopov/screentray-distribution was verified as official when first introduced to the cask
-  url "https://github.com/DSnopov/screentray-distribution/releases/download/v#{version}/screen-tray-#{version}-mac.zip"
+  url "https://github.com/DSnopov/screentray-distribution/releases/download/v#{version}/ScreenTray-#{version}.dmg"
   appcast 'https://github.com/DSnopov/screentray-distribution/releases.atom'
   name 'ScreenTray'
   homepage 'https://screentray.com/'
