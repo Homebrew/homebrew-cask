b95ccd338d1a835d7a5cda5088ace814eb955a4a
Marc Zottner <marc@zottner.com>
2018-11-06 20:55:29 +0100

    Update dbeaver-community to 5.2.4 (#54578)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
-  version '5.2.3'
-  sha256 '59696df710ad15e98232726be5009320e242f9bbcaa9858ae1f4336ec124d1d2'
+  version '5.2.4'
+  sha256 'd121e1fccfa6eb64c34386331ac46b9a7d1fc7b609441a90fe9072152b40e400'
 
   # github.com/dbeaver/dbeaver was verified as official when first introduced to the cask
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
