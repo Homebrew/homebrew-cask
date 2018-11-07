f8224cffebff25f16a96f41a5065416e44370e45
Marc Zottner <marc@zottner.com>
2018-11-07 01:37:43 +0100

    Update hands-off to 4.0.1 (#54574)

--- Casks/hands-off.rb
+++ Casks/hands-off.rb
@@ -1,6 +1,6 @@
 cask 'hands-off' do
-  version '4.0.0'
-  sha256 'b3ce928dcd3b9d47399cb69384d2705aa05cbba01ed563b9629d954da9abda2b'
+  version '4.0.1'
+  sha256 'ea9a93fd9a0f48927a353c9f286e0d8265f3032d837867c97b47939d05b41403'
 
   url "https://www.oneperiodic.com/files/Hands%20Off!%20v#{version}.dmg"
   appcast "https://www.oneperiodic.com/handsoff#{version.major}.xml"
