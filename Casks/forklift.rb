146e4e4725a1ff888790fb5ca06a81c6c346b541
Tom Brek <me@tombrek.com>
2018-11-07 15:44:24 +0000

    Update forklift to 3.2.9

--- Casks/forklift.rb
+++ Casks/forklift.rb
@@ -1,6 +1,6 @@
 cask 'forklift' do
-  version '3.2.8'
-  sha256 'dba57d6b89827e130e1b682e4416d5c8326f67eadc79e5d8cdbd523cc3dbf87f'
+  version '3.2.9'
+  sha256 '26f4d09d91e118cd4ed3a878f9eb4f2723fe3a99d848c942bdac7d71d5fd3a7e'
 
   url "https://download.binarynights.com/ForkLift#{version}.zip"
   appcast "https://updates.binarynights.com/ForkLift#{version.major}/update.xml"
