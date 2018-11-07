52a3ffd7811c5ffead6552237a20dff91f47b6e5
suschizu <brigitte@corecode.io>
2018-11-07 21:43:49 +0100

    Update mailspring to 1.5.2 (#54609)

--- Casks/mailspring.rb
+++ Casks/mailspring.rb
@@ -1,6 +1,6 @@
 cask 'mailspring' do
-  version '1.5.1'
-  sha256 '39bfcd3aa82e42675eaff2645a15f7634ceea493e79bfd97011cb66039bc1200'
+  version '1.5.2'
+  sha256 '8c0a098d297bc9116bfa9f9939996ddca61ff839be748395d871b3696b3b765e'
 
   # github.com/Foundry376/Mailspring was verified as official when first introduced to the cask
   url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring.zip"
