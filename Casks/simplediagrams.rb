aac98d8c80164ec6caf77fe71c9e1d6e6bb02de3
Tatsuya Kobayashi <SangatsuUsagi@outlook.com>
2018-11-08 11:31:06 +0900

    Add SimpleDiagrams4.app v4.0.28

--- Casks/simplediagrams.rb
+++ Casks/simplediagrams.rb
@@ -2,9 +2,9 @@ cask 'simplediagrams' do
   version '4.0.28'
   sha256 'f8878cae6a20f068c04218cadf576a5076c3e1bc4fc82e86645831d11f9045e7'
 
-  url 'https://www.simplediagrams.com/download/simplediagrams_installer_4_0_28.dmg'
+  url "https://www.simplediagrams.com/download/simplediagrams_installer_#{version.dots_to_underscores}.dmg"
   name 'SimpleDiagrams'
   homepage 'https://www.simplediagrams.com/'
 
-  app 'SimpleDiagrams4.app'
+  app "SimpleDiagrams#{version.major}.app"
 end
