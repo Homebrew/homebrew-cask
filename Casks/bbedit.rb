171012bf4d024cb9ba9a900d1267630555760231
chrisfinazzo <chris.finazzo@gmail.com>
2018-11-08 13:27:12 -0500

    Update BBEdit to 12.5

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -1,6 +1,6 @@
 cask 'bbedit' do
-  version '12.1.6'
-  sha256 '23b9fc6ef5c03cbcab041566503c556d5baf56b2ec18f551e6f0e9e6b48dc690'
+  version '12.5'
+  sha256 'fa346e6bc3ab98bcecddcfc86c024d4bf7aca1817290c6b50baf59382e55c59b'
 
   # s3.amazonaws.com/BBSW-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
@@ -8,7 +8,7 @@ cask 'bbedit' do
   name 'BBEdit'
   homepage 'https://www.barebones.com/products/bbedit/'
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: '>= :sierra'
 
   app 'BBEdit.app'
 
