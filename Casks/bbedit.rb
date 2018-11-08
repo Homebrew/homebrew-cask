d313618713ad08e78ff2530d5381533b62be03d8
chrisfinazzo <chris.finazzo@gmail.com>
2018-11-08 16:23:23 -0500

    Fix the checksum, add an if-else statement about 12.1.6 for users on El Capitan

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -1,21 +1,24 @@
-cask 'bbedit' do
-  version '12.5'
-  sha256 'fa346e6bc3ab98bcecddcfc86c024d4bf7aca1817290c6b50baf59382e55c59b'
-
+cask "bbedit" do
+  if MacOS.version <= :el_capitan
+    version "12.1.6"
+    sha256 "23b9fc6ef5c03cbcab041566503c556d5baf56b2ec18f551e6f0e9e6b48dc690"
+  elsif version "12.5"
+    sha256 "ab40f9f3177a23851b9607cfd6ac427dfec4982330a08a1b22cd7ed438548114"
+  end
   # s3.amazonaws.com/BBSW-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
-  appcast 'https://versioncheck.barebones.com/BBEdit.xml'
-  name 'BBEdit'
-  homepage 'https://www.barebones.com/products/bbedit/'
+  appcast "https://versioncheck.barebones.com/BBEdit.xml"
+  name "BBEdit"
+  homepage "https://www.barebones.com/products/bbedit/"
 
-  depends_on macos: '>= :sierra'
+  depends_on :macos => ">= :sierra"
 
-  app 'BBEdit.app'
+  app "BBEdit.app"
 
-  zap trash: [
-               '~/Library/Application Support/BBEdit',
-               '~/Library/Preferences/com.barebones.bbedit.plist',
-               '~/Library/BBEdit',
-               '~/Library/Caches/com.barebones.bbedit',
-             ]
+  zap :trash => [
+    "~/Library/Application Support/BBEdit",
+    "~/Library/Preferences/com.barebones.bbedit.plist",
+    "~/Library/BBEdit",
+    "~/Library/Caches/com.barebones.bbedit",
+  ]
 end
