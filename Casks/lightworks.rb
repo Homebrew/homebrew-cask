fb64182b12cc3a7e1b57af0981433969f377d37c
CoreCode <core-code@users.noreply.github.com>
2018-11-08 12:42:46 +0100

    Update lightworks to 14.5.0 (#54625)

--- Casks/lightworks.rb
+++ Casks/lightworks.rb
@@ -1,8 +1,8 @@
 cask 'lightworks' do
-  version '14.0.0'
-  sha256 '06d64bee517bfb13bbb1d5739dfd184fd2931bb8c1fab835bc066cbb6c41192d'
+  version '14.5.0'
+  sha256 '8b7d5890f7c0f2ede05d4de16a7a09e5bfc5b322690e70cbd2e481e3638e738c'
 
-  url "https://downloads.lwks.com/v#{version.major}/lightworks_v#{version}.dmg"
+  url "https://downloads.lwks.com/v#{version.major_minor.dots_to_hyphens}-new/lightworks_v#{version}.dmg"
   name 'Lightworks'
   homepage 'https://www.lwks.com/'
 
