f4d4db991abb9ca1179b7540425fccd2930bcabf
Gavin Chappell <g-a-c@noreply.users.github.com>
2018-11-08 19:55:22 +0000

    Add new Cask for SpiderOak Share 1.0.0

---
+++ Casks/spideroak-share.rb
@@ -0,0 +1,22 @@
+cask 'spideroak-share' do
+  version '1.0.0'
+  sha256 '3a2eea9b7e87e80ecf0ffeee9e54b83af0729ec42dcf0a875e81b8ef144dc628'
+
+  url 'https://spideroak.com/release/share'
+  name 'SpiderOak Share'
+  homepage 'https://spideroak.com/spideroak-share/'
+
+  depends_on macos: '>= :mountain_lion'
+
+  app 'Share.app'
+
+  uninstall trash: [
+                     '/Applications/Share.app',
+                   ]
+
+  zap trash: [
+               '~/Library/Logs/Share',
+               '~/Library/Preferences/com.spideroak.share.plist',
+               '~/Library/Saved Application State/com.spideroak.share.savedState',
+             ]
+end
