0cf6c529cd55d9fcfa44493b438219fc333c45c0
CoreCode <core-code@users.noreply.github.com>
2018-11-08 11:55:11 +0100

    Update openconnect-gui.rb

--- Casks/openconnect-gui.rb
+++ Casks/openconnect-gui.rb
@@ -8,5 +8,5 @@ cask 'openconnect-gui' do
   name 'OpenConnect-GUI'
   homepage 'https://openconnect.github.io/openconnect-gui/'
 
-  app 'openconnect-gui.app'
+  app 'openconnect-gui/#{version}/OpenConnect-GUI.app'
 end
