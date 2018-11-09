1cc724606ea8ce0533babc22899eb363f56fe965
suschizu <brigitte@corecode.io>
2018-11-09 21:37:26 +0100

    Update molotov to 3.0.2 (#54706)

--- Casks/molotov.rb
+++ Casks/molotov.rb
@@ -1,6 +1,6 @@
 cask 'molotov' do
-  version '3.0.0'
-  sha256 '77f921fd91d35ced1197e3854385052497645ca6b5fd33b1d3cabad880dca789'
+  version '3.0.2'
+  sha256 'e514b867375bf82915862334c9b409ef695056d73d02e2d2709f44a04d42d029'
 
   url "https://desktop-auto-upgrade.molotov.tv/mac/Molotov-v#{version}.dmg"
   name 'Molotov'
