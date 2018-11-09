6a70243b32871495e9d88270144645af757284f7
CoreCode <core-code@users.noreply.github.com>
2018-11-09 19:57:33 +0100

    Update mamp.rb

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -3,8 +3,9 @@ cask 'mamp' do
   sha256 'f05ede012b8a5d0e7c9cf17fee0fa1eb5cd8131f3c703ed14ea347f25be11a28'
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
+  appcast 'https://www.mamp.info/en/downloads/'
   name 'MAMP'
-  homepage 'https://www.mamp.info/en/downloads/'
+  homepage 'https://www.mamp.info/'
 
   depends_on macos: '>= :yosemite'
 
