b2cd61d7e9f0037ec58891077bc452dada4e315d
Work <brigitte@corecode.io>
2018-11-09 13:19:32 +0100

    Update makemkv to 1.14.1

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,6 +1,6 @@
 cask 'makemkv' do
-  version '1.12.3'
-  sha256 '682009477252bda4264f1216cf8d109e8abf1493a25431579c439fcc180ca3ae'
+  version '1.14.1'
+  sha256 '13c5172b627d8f89041f59824293a8c978bf9a3e2798ec37f70273cbc2b6aa94'
 
   url "https://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   appcast 'https://www.makemkv.com/download/'
