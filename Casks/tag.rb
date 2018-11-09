8246cf2a317267288bd1abeee60ae386cc573ecc
Ayaz Salikhov <mathbunnyru@users.noreply.github.com>
2018-11-09 06:53:58 +0300

    Update tag to latest (#54638)

--- Casks/tag.rb
+++ Casks/tag.rb
@@ -1,12 +1,11 @@
 cask 'tag' do
-  version '0.4.1'
-  sha256 '165631a76db33cbe5200ff64a64f5e1dad6606ae815c7609d59b7367f9344360'
+  version :latest
+  sha256 :no_check
 
-  # sourceforge.net/tagosx was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/tagosx/Tag-#{version}.zip"
-  appcast 'https://sourceforge.net/projects/tagosx/rss'
+  # s3.amazonaws.com/feisty-dog/ was verified as official when first introduced to the cask
+  url 'https://s3.amazonaws.com/feisty-dog/Tag.zip'
   name 'Tag'
-  homepage 'https://sbooth.org/Tag/'
+  homepage 'https://www.feisty-dog.com/tag/'
 
-  app "Tag-#{version}/Tag.app"
+  app 'Tag.app'
 end
