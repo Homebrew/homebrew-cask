94487e24ae2dc06a38f54b7a2d13004e2901d13d
suschizu <brigitte@corecode.io>
2018-11-09 21:32:44 +0100

    Update bitwarden to 1.10.0 (#54686)

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,6 +1,6 @@
 cask 'bitwarden' do
-  version '1.9.0'
-  sha256 'e7ebc57f8e5429befd2ad9b16326447b50829dda177a3d030b06490e29d135e3'
+  version '1.10.0'
+  sha256 'e153d74b823e3e35518b5eec37baa8df5d8fdc96f854fd40282854e6bbf21917'
 
   # github.com/bitwarden/desktop was verified as official when first introduced to the cask
   url "https://github.com/bitwarden/desktop/releases/download/v#{version}/bitwarden-#{version}-mac.zip"
