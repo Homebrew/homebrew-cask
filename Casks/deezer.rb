e4696a2f314de07bf592aa4839a833d20ecc0411
Vítor Galvão <info@vitorgalvao.com>
2018-11-09 20:31:15 +0000

    Update deezer.rb

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -1,7 +1,7 @@
 cask 'deezer' do
   version '0.16.6'
   sha256 'd2d727ad7d56416f4f6b703ff4bbed881e6cd7b2b6c9eba6ccf4c3e43a90c387'
-  
+
   url "https://www.deezer.com/desktop/download/artifact/darwin/x64/#{version}"
   name 'Deezer'
   homepage 'https://www.deezer.com/download'
