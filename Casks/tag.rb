d18438664cc1574b19373b8a5e1f813ef68e1616
Ayaz Salikhov <mathbunnyru@gmail.com>
2018-11-09 16:05:29 +0300

    Update tag to 1.0.5,017

--- Casks/tag.rb
+++ Casks/tag.rb
@@ -1,9 +1,10 @@
 cask 'tag' do
-  version :latest
-  sha256 :no_check
+  version '1.0.5,017'
+  sha256 '3b3b21909dc00d4ff87a501c6451fb14e99ef1c10bea64d69ace5cfaa5bfe1a4'
 
   # s3.amazonaws.com/feisty-dog/ was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/feisty-dog/Tag.zip'
+  url "https://s3.amazonaws.com/feisty-dog/versions/binaries/000/000/#{version.after_comma}/Tag-#{version.before_comma}.zip"
+  appcast 'https://www.feisty-dog.com/panel/updates/Tag'
   name 'Tag'
   homepage 'https://www.feisty-dog.com/tag/'
 
