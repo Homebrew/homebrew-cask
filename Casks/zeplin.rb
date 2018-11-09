7179e8264a6db47be20363cc155d1fa2653b9038
RJ Hsiao <rj@rjhsiao.me>
2018-11-09 12:03:16 +0800

    Update zeplin to 2.2.3,582 (#54675)

--- Casks/zeplin.rb
+++ Casks/zeplin.rb
@@ -1,6 +1,6 @@
 cask 'zeplin' do
-  version '2.2.1,573'
-  sha256 '2e76848e6c40153d6e33da6652bd237293bab00424f7f0f51775033300a35bf5'
+  version '2.2.3,582'
+  sha256 '9b21ceff44464aff534e5cf5f889398a64a00a8c9e74df275765ff9652d02b8e'
 
   url 'https://api.zeplin.io/urls/download-mac'
   appcast 'https://rink.hockeyapp.net/api/2/apps/8926efffe734b6d303d09f41d90c34fc'
