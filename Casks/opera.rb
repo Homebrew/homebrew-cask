6b4b172b53999003470c3aa049a9121e03669e1c
Work <brigitte@corecode.io>
2018-11-10 20:24:07 +0100

    Update opera to 56.0.3051.102

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '56.0.3051.99'
-  sha256 '4984898f0fe0d4ceb95d5f06db492d18047584393fddfb7578f15c133d5f5128'
+  version '56.0.3051.102'
+  sha256 '33c9266a41d1c4daf227950198c89548995e88001e120368d444233dc0d0045f'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
