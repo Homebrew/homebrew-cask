f77837198720767fb2b206444423c4ee09456492
CaptainArk <captainark@captainark.net>
2018-11-10 13:13:18 +0100

    Add ProfileCreator.app v0.2.3

---
+++ Casks/profilecreator.rb
@@ -0,0 +1,10 @@
+cask 'profilecreator' do
+  version '0.2.3'
+  sha256 '8a0b9a0b219d5b4047c0e1ce8c4af25e1286060e743ed3dcd51154aaf709b9ec'
+
+  url 'https://github.com/erikberglund/ProfileCreator/releases/download/v0.2.3-beta.8/ProfileCreator_v0.2.3-beta.8.dmg'
+  name 'ProfileCreator'
+  homepage 'https://github.com/erikberglund/ProfileCreator'
+
+  app 'ProfileCreator.app'
+end
