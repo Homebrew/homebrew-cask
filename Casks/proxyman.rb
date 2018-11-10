f0ab47349b8300a879a4d2bbb9258f26bff01b1c
Nghia Tran <vinhnghiatran@gmail.com>
2018-11-10 17:17:46 +0700

    Update Proxyman 1.1.2

--- Casks/proxyman.rb
+++ Casks/proxyman.rb
@@ -1,6 +1,6 @@
 cask 'proxyman' do
-  version '1.1.1'
-  sha256 'ca7e7b6a4beac481cf550f7e7dc3b0f1b943f8903d18f33c39a6c6643bf59018'
+  version '1.1.2'
+  sha256 '6ee99160bba09babf4b2ab966df89051f76d2334f431e7c31e37681d963dba7c'
 
   # github.com/ProxymanApp/Proxyman was verified as official when first introduced to the cask
   url "https://github.com/ProxymanApp/Proxyman/releases/download/#{version}/Proxyman_#{version}.dmg"
