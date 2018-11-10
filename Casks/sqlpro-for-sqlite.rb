625f7e7383c321bd9f4987f659c12a20817f3583
suschizu <brigitte@corecode.io>
2018-11-10 21:14:22 +0100

    Update sqlpro-for-sqlite to 1.0.310 (#54735)

--- Casks/sqlpro-for-sqlite.rb
+++ Casks/sqlpro-for-sqlite.rb
@@ -1,6 +1,6 @@
 cask 'sqlpro-for-sqlite' do
-  version '1.0.302'
-  sha256 '119f92e966ca48d8bfc42f367bd2247ea40629012a167d96e03b549ba7712be3'
+  version '1.0.310'
+  sha256 '8ccfd8eb6660ee443a4490c7437cd75608f811c129c36b45a1a8fa05c1892828'
 
   # d3fwkemdw8spx3.cloudfront.net/sqlite was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/sqlite/SQLProSQLite.#{version}.app.zip"
