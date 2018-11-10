3f166fe4f7f3f1326bdd598cf95116c914ad59fd
CoreCode <core-code@users.noreply.github.com>
2018-11-10 21:08:18 +0100

    Update pennywise to 0.6.2 (#54731)

--- Casks/pennywise.rb
+++ Casks/pennywise.rb
@@ -1,8 +1,8 @@
 cask 'pennywise' do
-  version '0.6.1'
-  sha256 '258a35cf13ece1e1b6f29ee636986a822cf374e7441cd8dc9c784c032121c75a'
+  version '0.6.2'
+  sha256 '42c1c07bbae28707f2fac0a56254ca957833c60049da6ff236e307471ef80f71'
 
-  url "https://github.com/kamranahmedse/pennywise/releases/download/#{version}/Pennywise-#{version}-mac.zip"
+  url "https://github.com/kamranahmedse/pennywise/releases/download/v#{version}/Pennywise-#{version}-mac.zip"
   appcast 'https://github.com/kamranahmedse/pennywise/releases.atom'
   name 'Pennywise'
   homepage 'https://github.com/kamranahmedse/pennywise'
