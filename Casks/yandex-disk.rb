26c6306c50aff3bb9c62803f87df27af9b414dbf
CoreCode <core-code@users.noreply.github.com>
2018-11-10 21:14:01 +0100

    Update yandex-disk to 3.0.5 (#54733)

--- Casks/yandex-disk.rb
+++ Casks/yandex-disk.rb
@@ -1,8 +1,8 @@
 cask 'yandex-disk' do
-  version '3.0'
-  sha256 '9fd19073892d080a835ab90403d6be155467daa7c71d9900154f4dadf71eff11'
+  version '3.0.5'
+  sha256 '173fd9e59143c0efe985ef822a215f7697e046b8469e94c2f87215f33aa90d06'
 
-  url "https://disk.yandex.ru/download/YandexDisk#{version.no_dots}.dmg/?instant=1"
+  url "https://disk.yandex.ru/download/YandexDisk#{version.major_minor.no_dots}.dmg/?instant=1"
   name 'Yandex.Disk'
   homepage 'https://disk.yandex.ru/'
 
