5a2495d08ff0935cea67131b6fdc4c60a65424bb
Vítor Galvão <info@vitorgalvao.com>
2018-11-10 17:21:36 +0000

    Update profilecreator.rb

--- Casks/profilecreator.rb
+++ Casks/profilecreator.rb
@@ -1,8 +1,8 @@
 cask 'profilecreator' do
-  version '0.2.3'
+  version '0.2.3-beta.8'
   sha256 '8a0b9a0b219d5b4047c0e1ce8c4af25e1286060e743ed3dcd51154aaf709b9ec'
 
-  url 'https://github.com/erikberglund/ProfileCreator/releases/download/v0.2.3-beta.8/ProfileCreator_v0.2.3-beta.8.dmg'
+  url "https://github.com/erikberglund/ProfileCreator/releases/download/v#{version}/ProfileCreator_v#{version}.dmg"
   appcast 'https://github.com/erikberglund/ProfileCreator/releases.atom'
   name 'ProfileCreator'
   homepage 'https://github.com/erikberglund/ProfileCreator'
