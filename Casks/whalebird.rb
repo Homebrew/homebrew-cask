2a9e9fa053199a32d258e936bf54fb3ba5398884
kPherox <admin@mail.kr-kp.com>
2018-11-12 02:30:01 +0900

    Update whalebird to 2.5.0

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -1,6 +1,6 @@
 cask 'whalebird' do
-  version '2.4.4'
-  sha256 '845972074dc7ac10fd2c702d38a78fb65d691ffb5bb0ce5495e935176947b36c'
+  version '2.5.0'
+  sha256 '562e51cfbd0061a43c38d2409e16b99a8e7f2d525d10a96d6e7a9726fd30921d'
 
   # github.com/h3poteto/whalebird-desktop was verified as official when first introduced to the cask
   url "https://github.com/h3poteto/whalebird-desktop/releases/download/#{version}/Whalebird-#{version}-darwin-x64.dmg"
