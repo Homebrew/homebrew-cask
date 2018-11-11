c05fd84bd0487820905177c235ec08894fbcd443
Carsten Hensiek <carsten@hensiek.com>
2018-11-11 15:18:35 +0100

    Add WKO Latest

---
+++ Casks/wko.rb
@@ -0,0 +1,10 @@
+cask 'wko' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://wko.trainingpeaks.com/wko4_osx_latest.dmg'
+  name 'wko'
+  homepage 'https://www.trainingpeaks.com/wko4/'
+
+  app 'WKO4.app'
+end
