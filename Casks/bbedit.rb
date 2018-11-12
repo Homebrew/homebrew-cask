c361b28bb07b0a9d015409a36c83bd5093f6e586
chrisfinazzo <chris.finazzo@gmail.com>
2018-11-11 20:20:25 -0500

    Use an else statement, revert Rubocop's changes

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -1,24 +1,25 @@
-cask "bbedit" do
+cask 'bbedit' do
   if MacOS.version <= :el_capitan
-    version "12.1.6"
-    sha256 "23b9fc6ef5c03cbcab041566503c556d5baf56b2ec18f551e6f0e9e6b48dc690"
-  elsif version "12.5"
-    sha256 "ab40f9f3177a23851b9607cfd6ac427dfec4982330a08a1b22cd7ed438548114"
+    version '12.1.6'
+    sha256 '23b9fc6ef5c03cbcab041566503c556d5baf56b2ec18f551e6f0e9e6b48dc690'
+  else
+    version '12.5'
+    sha256 'ab40f9f3177a23851b9607cfd6ac427dfec4982330a08a1b22cd7ed438548114'
   end
   # s3.amazonaws.com/BBSW-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
-  appcast "https://versioncheck.barebones.com/BBEdit.xml"
-  name "BBEdit"
-  homepage "https://www.barebones.com/products/bbedit/"
+  appcast 'https://versioncheck.barebones.com/BBEdit.xml'
+  name 'BBEdit'
+  homepage 'https://www.barebones.com/products/bbedit/'
 
-  depends_on :macos => ">= :sierra"
+  depends_on macos: '>= :sierra'
 
-  app "BBEdit.app"
+  app 'BBEdit.app'
 
-  zap :trash => [
-    "~/Library/Application Support/BBEdit",
-    "~/Library/Preferences/com.barebones.bbedit.plist",
-    "~/Library/BBEdit",
-    "~/Library/Caches/com.barebones.bbedit",
-  ]
-end
+  zap trash:  [
+                '~/Library/Application Support/BBEdit',
+                '~/Library/Preferences/com.barebones.bbedit.plist',
+                '~/Library/BBEdit',
+                '~/Library/Caches/com.barebones.bbedit',
+              ]
+ end
