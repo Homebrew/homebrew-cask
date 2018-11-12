239147f25c1abf516242974afe5bdd38fb70c9ac
Vítor Galvão <info@vitorgalvao.com>
2018-11-12 15:24:55 +0000

    Update feeder.rb

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -3,7 +3,7 @@ cask 'feeder' do
   sha256 '1e9952d59cdb18ebe9ca38df290e6f176499df82841d5b5be8f3ad0502edf96a'
 
   url "https://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
-  appcast 'https://reinventedsoftware.com/feeder/downloads/Feeder3.xml'
+  appcast "https://reinventedsoftware.com/feeder/downloads/Feeder#{version.major}.xml"
   name 'Feeder'
   homepage 'https://reinventedsoftware.com/feeder/'
 
