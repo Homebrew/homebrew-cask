bfa4b7233f7744c8a16cfa9c9c0c3b8f640a6dea
CoreCode <core-code@users.noreply.github.com>
2018-11-12 16:23:21 +0100

    Update macmediakeyforwarder to 2.0 (#54793)

--- Casks/macmediakeyforwarder.rb
+++ Casks/macmediakeyforwarder.rb
@@ -1,14 +1,14 @@
-cask "macmediakeyforwarder" do
-  version "2.0"
-  sha256 "4941afa2ed60bf098e25a62d2ef3a42dfadcd38c18c39d4ae59cfd591ed63d1a"
+cask 'macmediakeyforwarder' do
+  version '2.0'
+  sha256 '4941afa2ed60bf098e25a62d2ef3a42dfadcd38c18c39d4ae59cfd591ed63d1a'
 
-  url "http://milgra.com/downloads/hsme/MacMediaKeyForwarder2.0.zip"
-  name "Mac Media Key Forwarder"
-  homepage "http://milgra.com/mac-media-key-forwarder.html"
+  url "http://milgra.com/downloads/hsme/MacMediaKeyForwarder#{version}.zip"
+  name 'Mac Media Key Forwarder'
+  homepage 'http://milgra.com/mac-media-key-forwarder.html'
 
-  app "MacMediaKeyForwarder.app"
+  app 'MacMediaKeyForwarder.app'
 
-  uninstall :login_item => "MacMediaKeyForwarder"
+  uninstall login_item: 'MacMediaKeyForwarder'
 
-  zap :trash => "~/Library/Preferences/com.milgra.hsmke.plist"
+  zap trash: '~/Library/Preferences/com.milgra.hsmke.plist'
 end
