867eb83ac9fa8a479daced09a7a9e29aa5146d3d
Vítor Galvão <info@vitorgalvao.com>
2018-11-12 15:55:18 +0000

    Update notebooks.rb

--- Casks/notebooks.rb
+++ Casks/notebooks.rb
@@ -1,8 +1,8 @@
 cask 'notebooks' do
-  version :latest
-  sha256 :no_check
+  version '1.4.2'
+  sha256 'e71889f63548ab7d255ccfe75f4eef7750f4959b70f1e80c1c023a004d3d7fb6'
 
-  url 'https://www.notebooksapp.com/Download/Notebooks%20for%20Mac.dmg'
+  url 'https://notebooksapp.com/Download/Notebooks.zip'
   appcast 'https://www.notebooksapp.com/NBMacAppcast.xml'
   name 'Notebooks'
   homepage 'https://www.notebooksapp.com/mac/'
