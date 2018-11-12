b9927ba11df0bcd8a00e403f069a08220a507693
Julian Mayer <julian@corecode.io>
2018-11-12 11:35:59 +0100

    Update istopmotion to 3.8.2-16057

--- Casks/istopmotion.rb
+++ Casks/istopmotion.rb
@@ -3,7 +3,7 @@ cask 'istopmotion' do
   sha256 'eb31319488346e12d6f95b7be9d61d911eaaf0228f22b6053ee0be8f6902f520'
 
   url "https://cdn.boinx.com/software/istopmotion/Boinx_iStopMotion_#{version}.app.zip"
-  appcast 'https://boinx.com/d/connect/histories/istopmotion'
+  appcast 'https://sparkle.boinx.com/appcast.lasso?appName=iStopMotion'
   name 'iStopMotion'
   homepage 'https://boinx.com/istopmotion/mac/'
 
