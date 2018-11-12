2924c5ec0a4845ec3cc97d6535ebc8957f7c147c
Mathias Dannesbo <neic@neic.dk>
2018-11-12 18:16:29 +0100

    Update josm to 14382 (#54813)

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask 'josm' do
-  version '14289'
-  sha256 'cc798104095969d98128ab08c082d40a882c9e47e6e4cdb70245180a47f9f195'
+  version '14382'
+  sha256 '8693cb774b345c1e8e278907095f7f4c271eb05d782d89e3b3e38646f005d6f5'
 
   url "https://josm.openstreetmap.de/download/macosx/josm-macosx-#{version}.zip"
   name 'JOSM'
