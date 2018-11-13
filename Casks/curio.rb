be8b3e2df0cc67bf3986545a014c767dce2c37b6
Vítor Galvão <info@vitorgalvao.com>
2018-11-13 12:42:40 -0500

    Update Casks/curio.rb
    Co-Authored-By: chrisfinazzo <chris.finazzo@gmail.com>

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -1,5 +1,5 @@
 cask 'curio' do
-  version '12.2.1'
+  version '12.02.10'
   sha256 'bda3b8a787b89882d5e8850826bfe1ffad3c0f703b67ae8da10015cc3ec6ce40'
 
   url "https://www.zengobi.com/downloads/Curio#{version.no_dots}.zip"
