021965fce52c9c31dd01990471534e5d4e3d9387
Vítor Galvão <info@vitorgalvao.com>
2018-11-13 11:51:34 -0500

    Update version URL
    Co-Authored-By: chrisfinazzo <chris.finazzo@gmail.com>

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -2,7 +2,7 @@ cask 'curio' do
   version '12.2.1'
   sha256 'bda3b8a787b89882d5e8850826bfe1ffad3c0f703b67ae8da10015cc3ec6ce40'
 
-  url 'https://zengobi.com/downloads/Curio.zip'
+  url "https://www.zengobi.com/downloads/Curio#{version.no_dots}.zip"
   appcast 'https://www.zengobi.com/appcasts/Curio12MarkdownAppcast.xml'
   name 'Curio'
   homepage 'https://zengobi.com/curio/'
