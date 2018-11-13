8011bca1b8face40d6c57bd1c6bf166f1cd682a9
CoreCode <core-code@users.noreply.github.com>
2018-11-13 18:30:28 +0100

    Update navicat-premium-essentials to 12.1.12 (#54869)

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium-essentials' do
-  version '12.1.10'
-  sha256 'b031feff9b8c1a45606d989345f4f92df48273d90a9182300fdb9d59a7ec7376'
+  version '12.1.12'
+  sha256 'bed84435e5c97a4fe3e6ed7cea238a6caf502340e15a87933e31dcd0772127ea'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
   name 'Navicat Premium Essentials'
