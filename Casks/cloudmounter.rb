605f6783b6a1dc6c43780ebffff47af300b0e9c4
Draque <draque@N7-Hackintosh.local>
2018-11-13 11:48:47 -0800

    Update cloudmounter to 3.2

--- Casks/cloudmounter.rb
+++ Casks/cloudmounter.rb
@@ -1,6 +1,6 @@
 cask 'cloudmounter' do
-  version :latest
-  sha256 :no_check
+  version '3.2'
+  sha256 'f3dcfb05c47133d0ebc9ae553b7309e147e4e50568f83f703bdf13444d33e09a'
 
   url 'https://mac.eltima.com/download/cloudmounter.dmg'
   appcast 'https://cdn.eltima.com/download/cloudmounter-update/settings.xml'
