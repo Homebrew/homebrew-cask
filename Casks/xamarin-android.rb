bf10a83db4902e56f2c7d2dac792953046dcf253
nrlquaker <ustynov.vy@gmail.com>
2018-11-13 15:26:21 +0200

    Add zap stanza to Xamarin related formulas

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -1,13 +1,20 @@
-cask 'xamarin-android' do
-  version '9.0.0-20'
-  sha256 'dfc87f1c40e884ee9474eda8a8792880251d10a0e4c72eb81d9f3fcdde50a8d9'
+cask "xamarin-android" do
+  version "9.0.0-20"
+  sha256 "dfc87f1c40e884ee9474eda8a8792880251d10a0e4c72eb81d9f3fcdde50a8d9"
 
   url "https://dl.xamarin.com/MonoforAndroid/Mac/xamarin.android-#{version}.pkg"
-  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
-  name 'Xamarin.Android'
-  homepage 'https://www.xamarin.com/platform'
+  appcast "https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml"
+  name "Xamarin.Android"
+  homepage "https://www.xamarin.com/platform"
 
   pkg "xamarin.android-#{version}.pkg"
 
-  uninstall pkgutil: 'com.xamarin.android.pkg'
+  uninstall :pkgutil => "com.xamarin.android.pkg"
+
+  zap :trash => [
+    "/Developer/MonoAndroid",
+    "/Library/Frameworks/Xamarin.Android.framework",
+    "~/Library/Caches/Xamarin.Android",
+    "~/Library/Caches/XamarinBuildDownload",
+  ]
 end
