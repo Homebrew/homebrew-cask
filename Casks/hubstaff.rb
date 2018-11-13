44da011aabddd88e5dce6dd667ac1923c7c96c70
Draque <draque@N7-Hackintosh.local>
2018-11-13 12:35:59 -0800

    Update hubstaff to 1.4.2-8d78ff24

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,6 +1,6 @@
 cask 'hubstaff' do
-  version :latest
-  sha256 :no_check
+  version '1.4.2-8d78ff24'
+  sha256 'b388bede05079f2e0bb9f8804846c010c70b47263c7cd23a893a2f62f135ee99'
 
   url 'https://app.hubstaff.com/download/osx'
   appcast 'https://app.hubstaff.com/appcast.xml'
