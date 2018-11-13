9d9b1edc8f9c04c5e3b706dce49ef1fca3e203df
CoreCode <core-code@users.noreply.github.com>
2018-11-13 18:15:27 +0100

    Update minecraftpe to 1-7-release-20181106 (#54850)

--- Casks/minecraftpe.rb
+++ Casks/minecraftpe.rb
@@ -1,9 +1,10 @@
 cask 'minecraftpe' do
-  version '1-4-release-20180624'
-  sha256 'a2d733992b0ad9b2cc11cef45dc700724481d66892353617c204e2eadbc84f65'
+  version '1-7-release-20181106'
+  sha256 'fac08699bcff79a7deed436d9515ec5d0de687dcbd117a18f35e833e6dfcab16'
 
   # meedownloads.azureedge.net was verified as official when first introduced to the cask
-  url "https://meedownloads.azureedge.net/#{version}/macOS/Minecraft_Education_Edition.dmg"
+  url "https://meedownloads.azureedge.net/#{version}/Minecraft_Education_Edition.dmg",
+      referer: 'https://aka.ms/meeclientmacos'
   name 'Minecraft Education Edition'
   homepage 'https://education.minecraft.net/'
 
