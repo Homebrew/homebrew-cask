a0c71e57d0ac78b01e1090d87e4223c0e91b6d76
Vítor Galvão <info@vitorgalvao.com>
2018-11-13 12:12:34 -0500

    Update Casks/fman.rb
    Co-Authored-By: chrisfinazzo <chris.finazzo@gmail.com>

--- Casks/fman.rb
+++ Casks/fman.rb
@@ -2,7 +2,7 @@ cask 'fman' do
   version '1.4.4'
   sha256 '21c88f5662bcebd9e6f43250b0271c38e81096f979b55e7089972772f5b60439'
 
-  url 'http://download.fman.io/fman.dmg'
+  url "https://fman.io/updates/mac/#{version}.zip"
   appcast 'https://fman.io/updates/Appcast.xml'
   name 'fman'
   homepage 'https://fman.io/'
