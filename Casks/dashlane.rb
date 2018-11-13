a8394f1d2eaebdbbc169fb7d81b562ab5153fc6b
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-13 13:03:19 -0800

    Update dashlane to 6.1844.0 (#54909)

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,6 +1,6 @@
 cask 'dashlane' do
-  version :latest
-  sha256 :no_check
+  version '6.1844.0'
+  sha256 '24b8cfca4698cdbbf53309b3abe09a5308f888fc0e46096b53d596c2c30a4b3b'
 
   url 'https://www.dashlane.com/directdownload?platform=mac'
   appcast 'https://ws1.dashlane.com/5/binaries/query?target=archive&format=xml&currentSoftwareVersion=6.0.0&platform=server_osx&os=OS_X_10_13_6'
