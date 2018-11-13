a3ee3f164829df29a898d9e63fde5b351ba57a69
Amar1729 <amar.paul16@gmail.com>
2018-11-13 14:09:27 -0500

    Update Alacritty.app (0.2.2 -> 0.2.3)

--- Casks/alacritty.rb
+++ Casks/alacritty.rb
@@ -1,13 +1,13 @@
 cask 'alacritty' do
-  version '0.2.2'
-  sha256 '832b2f8e0e79f9ef159415435bf21b5db837b9febe849332039db2367519a534'
+  version '0.2.3'
+  sha256 '8e7ec7bfa245d68af4869adc8cc8ce93a2dfcf2ad5cc996e78c2bf37decd12fd'
 
-  url 'https://github.com/jwilm/alacritty/releases/download/binaries/Alacritty.dmg'
+  url "https://github.com/jwilm/alacritty/releases/download/v#{version}/Alacritty-v#{version}.dmg"
   appcast 'https://github.com/jwilm/alacritty/releases.atom'
   name 'Alacritty'
   homepage 'https://github.com/jwilm/alacritty/'
 
-  auto_updates true
+  auto_updates false
 
   app 'Alacritty.app'
   binary "#{appdir}/Alacritty.app/Contents/MacOS/alacritty"
