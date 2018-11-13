26cae1b7048feb9e063f722c8262ca4d008e7a1a
CoreCode <core-code@users.noreply.github.com>
2018-11-13 18:14:47 +0100

    Update notebooks to 1.4.3 (#54848)

--- Casks/notebooks.rb
+++ Casks/notebooks.rb
@@ -1,5 +1,5 @@
 cask 'notebooks' do
-  version '1.4.2'
+  version '1.4.3'
   sha256 'e71889f63548ab7d255ccfe75f4eef7750f4959b70f1e80c1c023a004d3d7fb6'
 
   url 'https://notebooksapp.com/Download/Notebooks.zip'
