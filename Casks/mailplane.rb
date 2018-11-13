2ca28c734f80513d8e7d3f4ec9b3170841d858c8
Julian Mayer <julian@corecode.io>
2018-11-13 15:08:29 +0100

    Update mailplane to 4.4516

--- Casks/mailplane.rb
+++ Casks/mailplane.rb
@@ -3,7 +3,7 @@ cask 'mailplane' do
   sha256 '8a4272daee8c38b4235cd824b8ca52a49e5562a22d72c27571ed869d5611df1f'
 
   url "https://update.mailplaneapp.com/builds/Mailplane_#{version.dots_to_underscores}.tbz"
-  appcast "https://mailplaneapp.com/releases/mailplane#{version.major}.html"
+  appcast 'https://update.mailplaneapp.com/appcast.php?rqsr=1&osVersion=10.14.1&appVersion=4565&shortVersionString=4.0.6'
   name 'Mailplane'
   homepage 'https://mailplaneapp.com/'
 
