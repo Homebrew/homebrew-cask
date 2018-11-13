b7d39b02374112001be03b2b5f1fea955a8c96fd
Draque <draque@N7-Hackintosh.local>
2018-11-12 22:16:49 -0800

    Update anylist to 1.0.8

--- Casks/anylist.rb
+++ Casks/anylist.rb
@@ -1,6 +1,6 @@
 cask 'anylist' do
-  version :latest
-  sha256 :no_check
+  version '1.0.8'
+  sha256 '82564129a68a06f3c0f8455b0f3c79fd0458edf2af1d3f63fa389696856b3cbf'
 
   url 'http://files.anylistapp.com/mac/AnyList.zip'
   appcast 'https://s3.amazonaws.com/files.anylistapp.com/mac/appcast.xml'
