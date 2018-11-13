23d65a1365a201bdfe1d3142e0cc5c00327fa49e
Vítor Galvão <info@vitorgalvao.com>
2018-11-13 14:07:50 +0000

    Update bill.rb

--- Casks/bill.rb
+++ Casks/bill.rb
@@ -3,10 +3,10 @@ cask 'bill' do
   sha256 '7400469acf2fc9675433a45d61e13f562de47c17aad0b299c0b92eb6f9107d64'
 
   # myownapp.com was verified as official when first introduced to the cask
-  url 'https://myownapp.com/downloads/Bill2.zip'
+  url "https://myownapp.com/downloads/Bill#{version.major}.zip"
   appcast 'https://sparkle.moapp.software/bill.xml'
   name 'Bill'
   homepage 'https://billtheapp.com/'
 
-  app 'Bill 2.app'
+  app "Bill #{version.major}.app"
 end
