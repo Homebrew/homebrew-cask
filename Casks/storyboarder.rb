85f0d9001e9427f3d6d81234d5459929f7f6189b
suschizu <brigitte@corecode.io>
2018-11-13 15:23:00 +0100

    Update storyboarder.rb

--- Casks/storyboarder.rb
+++ Casks/storyboarder.rb
@@ -1,9 +1,9 @@
 cask 'storyboarder' do
   version '1.9.2'
-  sha256 'd7f06c55c1c61de9f1620b34807f337a4c053ff0ec9efb1d980375d10fbcfe3b'
+  sha256 '121efaa9281970ce70e5896051c0a13627aadbedc3692a0ccac24541daf2db72'
 
   # github.com/wonderunit/storyboarder was verified as official when first introduced to the cask
-  url "https://github.com/wonderunit/storyboarder/releases/download/v#{version}/Storyboarder-#{version}-mac.zip"
+  url "https://github.com/wonderunit/storyboarder/releases/download/v#{version}/Storyboarder-#{version}.dmg"
   appcast 'https://github.com/wonderunit/storyboarder/releases.atom'
   name 'Wonder Unit Storyboarder'
   homepage 'https://wonderunit.com/storyboarder/'
