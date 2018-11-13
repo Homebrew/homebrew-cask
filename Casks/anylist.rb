577a439cf847b881cb4e451bb89c17ffb709f19e
Vítor Galvão <info@vitorgalvao.com>
2018-11-13 14:05:06 +0000

    Update anylist.rb

--- Casks/anylist.rb
+++ Casks/anylist.rb
@@ -2,7 +2,7 @@ cask 'anylist' do
   version '1.0.8'
   sha256 '82564129a68a06f3c0f8455b0f3c79fd0458edf2af1d3f63fa389696856b3cbf'
 
-  url 'http://files.anylistapp.com/mac/AnyList.zip'
+  url "https://s3.amazonaws.com/files.anylistapp.com/mac/AnyList_v#{version.dots_to_underscores}.zip"
   appcast 'https://s3.amazonaws.com/files.anylistapp.com/mac/appcast.xml'
   name 'AnyList'
   homepage 'https://www.anylistapp.com/'
