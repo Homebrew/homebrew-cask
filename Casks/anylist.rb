e4b4ed5f1f8647e0fdb0c5f35f96cb5f28f6fb9d
Vítor Galvão <info@vitorgalvao.com>
2018-11-13 14:12:37 +0000

    Update anylist.rb

--- Casks/anylist.rb
+++ Casks/anylist.rb
@@ -1,6 +1,6 @@
 cask 'anylist' do
   version '1.0.8'
-  sha256 '82564129a68a06f3c0f8455b0f3c79fd0458edf2af1d3f63fa389696856b3cbf'
+  sha256 'fa600270fd100898a1852f96d351bcfc04f20e5f843c0bcbce1f252a2d656bf9'
 
   # s3.amazonaws.com/files.anylistapp.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/files.anylistapp.com/mac/AnyList_v#{version.dots_to_underscores}.zip"
