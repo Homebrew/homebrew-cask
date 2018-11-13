12789458108e2c26c80c345d52b3f1da30f680ee
Vítor Galvão <info@vitorgalvao.com>
2018-11-13 11:55:54 -0500

    Update Casks/izip.rb
    Co-Authored-By: chrisfinazzo <chris.finazzo@gmail.com>

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -2,7 +2,7 @@ cask 'izip' do
   version '3.3'
   sha256 'd51cd6a698cb40a02f6930dd374a85bac6f78cb5d104a60476c9aefd45239329'
 
-  url 'https://www.izip.com/izip.dmg'
+  url "http://www.izip.com/izip_update_#{version.no_dots}.zip"
   appcast 'https://www.izip.com/updates'
   name 'iZip'
   homepage 'https://www.izip.com/'
