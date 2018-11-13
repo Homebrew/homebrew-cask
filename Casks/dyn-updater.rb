f673ac7ab2e62f9ad43255e7b9c4b84933b91083
Draque <draque@N7-Hackintosh.local>
2018-11-13 11:52:55 -0800

    Update dyn-updater to 5.4.7

--- Casks/dyn-updater.rb
+++ Casks/dyn-updater.rb
@@ -1,6 +1,6 @@
 cask 'dyn-updater' do
-  version :latest
-  sha256 :no_check
+  version '5.4.7'
+  sha256 '84a13897a60bb4dc55bd0508dafb9e234b1926d2874890cbd0b67c1cffab17d7'
 
   url 'http://cdn.dyn.com/dynupdater/DynUpdater.dmg'
   appcast 'http://cdn.dyn.com/dynupdater/appcast.xml'
