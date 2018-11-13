5ac6e7cd526dec9e4431d3a575b5e67fa87a438a
CoreCode <core-code@users.noreply.github.com>
2018-11-13 15:25:51 +0100

    Update mailplane.rb

--- Casks/mailplane.rb
+++ Casks/mailplane.rb
@@ -3,7 +3,7 @@ cask 'mailplane' do
   sha256 '8a4272daee8c38b4235cd824b8ca52a49e5562a22d72c27571ed869d5611df1f'
 
   url "https://update.mailplaneapp.com/builds/Mailplane_#{version.major}_#{version.after_comma}.tbz"
-  appcast 'https://update.mailplaneapp.com/appcast.php?rqsr=1&osVersion=10.14.1&appVersion=#{version.after_comma}&shortVersionString=#{version.before_comma}'
+  appcast "https://update.mailplaneapp.com/appcast.php?rqsr=1&osVersion=10.14.1&appVersion=#{version.after_comma}&shortVersionString=#{version.before_comma}"
   name 'Mailplane'
   homepage 'https://mailplaneapp.com/'
 
