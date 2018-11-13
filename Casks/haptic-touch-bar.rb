b2583924ff31872c6dee296f6a0454073520d98c
Draque <draque@N7-Hackintosh.local>
2018-11-13 12:34:25 -0800

    Update haptic-touch-bar to 2.4.0

--- Casks/haptic-touch-bar.rb
+++ Casks/haptic-touch-bar.rb
@@ -1,6 +1,6 @@
 cask 'haptic-touch-bar' do
-  version :latest
-  sha256 :no_check
+  version '2.4.0'
+  sha256 'c7b044f1516bb0912e863e2e3a3fd080fa06833eaddc0f056caf55a5e9df94b5'
 
   url 'https://www.haptictouchbar.com/download/HapticTouchBar.zip'
   appcast 'https://updates.devmate.com/com.bopsoft.HapticTouchBar.xml'
