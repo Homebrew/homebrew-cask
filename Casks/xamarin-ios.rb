dd8307f1ec17ca451f29084ca37bf9c19a79abc9
nrlquaker <ustynov.vy@gmail.com>
2018-11-13 19:01:01 +0200

    Fix style

--- Casks/xamarin-ios.rb
+++ Casks/xamarin-ios.rb
@@ -1,20 +1,20 @@
-cask "xamarin-ios" do
-  version "12.2.0.26"
-  sha256 "e0000012ded4272769f83cd28b63bfdbf61ee0e4bace2c6ae4a1e77f835516b0"
+cask 'xamarin-ios' do
+  version '12.2.0.26'
+  sha256 'e0000012ded4272769f83cd28b63bfdbf61ee0e4bace2c6ae4a1e77f835516b0'
 
   url "https://dl.xamarin.com/MonoTouch/Mac/xamarin.ios-#{version}.pkg"
-  appcast "https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml"
-  name "Xamarin.iOS"
-  homepage "https://www.xamarin.com/platform"
+  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
+  name 'Xamarin.iOS'
+  homepage 'https://www.xamarin.com/platform'
 
   pkg "xamarin.ios-#{version}.pkg"
 
-  uninstall :pkgutil => [
-    "com.xamarin.xamarin.ios.pkg",
-    "com.xamarin.xamarin-ios-build-host.pkg",
-  ]
-  trash :zap => [
-    "/Developer/MonoTouch",
-    "/Library/Frameworks/Xamarin.iOS.framework",
-  ]
+  uninstall pkgutil: [
+                       'com.xamarin.xamarin.ios.pkg',
+                       'com.xamarin.xamarin-ios-build-host.pkg',
+                     ]
+  trash zap: [
+               '/Developer/MonoTouch',
+               '/Library/Frameworks/Xamarin.iOS.framework',
+             ]
 end
