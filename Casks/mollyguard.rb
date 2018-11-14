f45e8bd32662ed721c43ea9a580247c0e4252e8b
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-14 06:26:10 -0800

    Update mollyguard to 1.0.0 (#54930)

--- Casks/mollyguard.rb
+++ Casks/mollyguard.rb
@@ -1,6 +1,6 @@
 cask 'mollyguard' do
-  version :latest
-  sha256 :no_check
+  version '1.0.0'
+  sha256 '1db2745811987d4dbb94aa586b91199eff3b5d715b684ab93c789231c0ca4f13'
 
   # dl.dropboxusercontent.com/s/e2l2dk2qz1u93cb was verified as official when first introduced to the cask
   url 'https://dl.dropboxusercontent.com/s/e2l2dk2qz1u93cb/MollyGuard.dmg'
