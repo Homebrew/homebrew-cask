b7b753ba84cef503b3eda03bec8fa6b31f4e9803
Julian Mayer <julian@corecode.io>
2018-11-14 17:03:36 +0100

    Update zoc to 7.21.4

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask 'zoc' do
-  version '7.21.3'
-  sha256 '03f4b71a8dd2789c47b30e1517de30d70da7a91cbd7dbb70548a40fa78505ae2'
+  version '7.21.4'
+  sha256 '44f3b4983167625dc984c5c1f02897cd8331c0b1b2b01b41d8a389082c7fa73a'
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast 'https://www.emtec.com/downloads/zoc/zoc_changes.txt'
