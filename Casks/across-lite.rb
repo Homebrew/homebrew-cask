8dd13a27a8c713d09f3307c33ea82aff5bb3d8a0
Work <brigitte@corecode.io>
2018-11-14 23:30:00 +0100

    Update across-lite to 2.4.4

--- Casks/across-lite.rb
+++ Casks/across-lite.rb
@@ -1,6 +1,6 @@
 cask 'across-lite' do
-  version '2.4.3'
-  sha256 '4e3a60742e6700602014e9c23196cecba4ba40c00fcb0221e4c48d9f02883868'
+  version '2.4.4'
+  sha256 '476fdbfb041b848562e52f34095fb7f3e519f73abab9bc1bda33afa00a3f1069'
 
   url "http://www.litsoft.com/across/alite/download/download.php/al#{version.no_dots}osx.dmg?os=macosx"
   name 'Across Lite'
