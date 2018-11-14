19920c899ade02d8e93013462a054c009464ca1e
jcbot <jcb@leipert.io>
2018-11-14 14:35:14 +0000

    Update jetbrains-toolbox to 1.12.4481

--- Casks/jetbrains-toolbox.rb
+++ Casks/jetbrains-toolbox.rb
@@ -1,6 +1,6 @@
 cask 'jetbrains-toolbox' do
-  version '1.11.4269'
-  sha256 '7681d2729e924d7f1f15b6a9472a0637ee6e4a09855e76caec883a945a2bf28c'
+  version '1.12.4481'
+  sha256 '8e85762a20c67f3d98f2a4e66074820be1c6b71b24c85919cc2187fec9339b01'
 
   url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release'
