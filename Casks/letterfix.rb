10da9e59fe537e9242559ce2b1683686f9e9a01c
CoreCode <core-code@users.noreply.github.com>
2018-11-14 18:24:36 +0100

    Update letterfix to 2.7.0.1,70082 (#54968)

--- Casks/letterfix.rb
+++ Casks/letterfix.rb
@@ -1,8 +1,8 @@
 cask 'letterfix' do
-  version '2.6.0,68346'
-  sha256 '89de44a2b0e6cb43915d9584615226811f939a42aed6d2a14ba099de18db2768'
+  version '2.7.0.1,70082'
+  sha256 '0eadd2aef702d9f940df8376d9a47101d4792fd59b083c6b53de0f93044e7048'
 
-  url "http://onet.dl.osdn.jp/letter-fix/#{version.after_comma}/LetterFix-#{version.before_comma}.dmg"
+  url "http://rwthaachen.dl.osdn.jp/letter-fix/#{version.after_comma}/LetterFix-#{version.before_comma}.dmg"
   appcast 'https://ja.osdn.net/projects/letter-fix/releases/rss'
   name 'LetterFix'
   homepage 'https://osdn.jp/projects/letter-fix/'
