88d1f41d0e2ad1b0a5eb7c323f40b31c7a8e657d
Work <brigitte@corecode.io>
2018-11-14 23:26:27 +0100

    Update geneious to 2019.0.3

--- Casks/geneious.rb
+++ Casks/geneious.rb
@@ -1,8 +1,8 @@
 cask 'geneious' do
-  version '11.1.5'
-  sha256 '8be154ed47e0476717a7194476817ee48601370cb8471cedd4df7fc1a6694712'
+  version '2019.0.3'
+  sha256 '73c65f63620fd3766a290becc7910d03e46184641b4b03529288fae968980b1b'
 
-  url "https://assets.geneious.com/installers/geneious/release/Geneious_mac64_#{version.dots_to_underscores}_with_jre.dmg"
+  url "https://assets.geneious.com/installers/geneious/release/Geneious_Prime_mac64_#{version.dots_to_underscores}_with_jre.dmg"
   name 'Geneious'
   homepage 'https://www.geneious.com/'
 
