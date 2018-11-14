a8a344a61cf2c88b7c2b3ab61730ced2962f3b3e
Clemens Giuliani <clemens@inailuig.it>
2018-11-14 22:00:28 +0100

    Update auryo to 2.2.1

--- Casks/auryo.rb
+++ Casks/auryo.rb
@@ -1,6 +1,6 @@
 cask 'auryo' do
-  version '2.1.2'
-  sha256 '39b04ffc45a4e2e67d066545d48b32c60d4b0d7741d72bc343eea5c5e20e022a'
+  version '2.2.1'
+  sha256 '3725030b00e8ba75d6514fd9c4c56cedb69b406042bd8c6b1e6c7bc33d55ffee'
 
   # github.com/Superjo149/auryo was verified as official when first introduced to the cask
   url "https://github.com/Superjo149/auryo/releases/download/v#{version}/Auryo-#{version}.dmg"
