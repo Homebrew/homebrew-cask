cd35df71eb9bbd313a0b301d4a561bd65c118dac
Marc Schwede <schwedenmut@googlemail.com>
2018-11-14 18:42:08 +0100

    Update textmate to 2.0-rc.19 (#54953)

--- Casks/textmate.rb
+++ Casks/textmate.rb
@@ -1,6 +1,6 @@
 cask 'textmate' do
-  version '2.0-rc.16'
-  sha256 '998c5b5aece79f2e27939149f8ed276e49aa1e5f451b359db977d55be2c1c873'
+  version '2.0-rc.19'
+  sha256 '31c16d92b4f267df0eb87bc3b04864f949f0007bb0661ab282ff89f397ffb41a'
 
   # github.com/textmate/textmate was verified as official when first introduced to the cask
   url "https://github.com/textmate/textmate/releases/download/v#{version}/TextMate_#{version}.tbz"
