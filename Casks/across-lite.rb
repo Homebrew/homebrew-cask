18e5f3f0ecce3e58cca856b7d56570599dad30b2
suschizu <brigitte@corecode.io>
2018-11-15 17:36:17 +0000

    Update across-lite.rb

--- Casks/across-lite.rb
+++ Casks/across-lite.rb
@@ -1,8 +1,8 @@
 cask 'across-lite' do
-  version '2.4.4'
-  sha256 '476fdbfb041b848562e52f34095fb7f3e519f73abab9bc1bda33afa00a3f1069'
+  version '2.4.4,2442'
+  sha256 '71e1b174415ea2582642f4861c0ae748b8afa1232c53adac5acddf5d5ea3a39b'
 
-  url "http://www.litsoft.com/across/alite/download/download.php/al#{version.no_dots}osx.dmg?os=macosx"
+  url "http://www.litsoft.com/across/alite/download/download.php/al#{version.after_comma}osx.dmg?os=macosx"
   name 'Across Lite'
   homepage 'http://www.litsoft.com/across/alite/download/'
 
