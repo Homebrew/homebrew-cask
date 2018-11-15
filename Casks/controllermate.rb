4bd1be5c96c6aa98fbec509add41f2a77f56e569
suschizu <brigitte@corecode.io>
2018-11-15 19:54:23 +0100

    Update controllermate.rb

--- Casks/controllermate.rb
+++ Casks/controllermate.rb
@@ -1,5 +1,8 @@
 cask 'controllermate' do
-  if MacOS.version <= :sierra
+  if MacOS.version <= :el_capitan
+    version '4.9.10'
+    sha256 '4f24f5763e96b0b0e959197dba5cc064928b59b74d49210bf5a484f4f9766d38'
+  elsif MacOS.version == :sierra
     version '4.10.4'
     sha256 'fdeb37ca8df145d927b9daef6dfa22ef6d1535f9ad1459c4f4ffcb52fbc19c3b'
   else
@@ -13,7 +16,7 @@ cask 'controllermate' do
   name 'ControllerMate'
   homepage 'https://www.orderedbytes.com/controllermate/'
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :mountain_lion'
 
   pkg '#temp#/ControllerMate.sparkle_interactive.pkg'
 
