9e31245e79456b528ddc7db65e83a188aa93b64c
Ralph Martin <ralph@cs.cf.ac.uk>
2018-11-15 12:45:07 +0000

    Update geekbench to 4.3.1
    Apparently it is now back to being delivered as a zip, not a dmg

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -6,11 +6,10 @@ cask 'geekbench' do
     # cdn.primatelabs.com was verified as official when first introduced to the cask
     url "https://cdn.primatelabs.com/Geekbench-#{version}-Mac.zip"
   else
-    version '4.3.0'
-    sha256 '707ad909ba864aae5acde3134579d5920710b0910ca3db9428c90fac0799bdac'
+    version '4.3.1'
+    sha256 '3f1a645dd9eeb49328a77d42651cd2b214ea67e670912d53ea49ef6a4ad9766b'
 
-    url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.dmg"
-    appcast "https://www.primatelabs.com/appcast/geekbench#{version.major}.xml"
+    url "https://cdn.primatelabs.com/Geekbench-#{version}-Mac.zip"
   end
 
   name 'Geekbench'
