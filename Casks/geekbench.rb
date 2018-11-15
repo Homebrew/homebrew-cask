4a22973a53b8b2961bd07d838f1d797c2df2ac83
Vítor Galvão <info@vitorgalvao.com>
2018-11-15 15:44:30 +0000

    Update geekbench.rb

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -9,7 +9,7 @@ cask 'geekbench' do
     version '4.3.1'
     sha256 '3f1a645dd9eeb49328a77d42651cd2b214ea67e670912d53ea49ef6a4ad9766b'
 
-    url "http://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
+    url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
     appcast "https://www.primatelabs.com/appcast/geekbench#{version.major}.xml"
   end
 
