e8a8c2e48bfaec6df1ef3c8f181e382c15f89f56
Randall <ran.dall@icloud.com>
2018-11-15 00:10:34 -0800

    Add iTuneConnect App 1.1
    Signed-off-by: Randall <ran.dall@icloud.com>

---
+++ Casks/itunesconnect-app.rb
@@ -0,0 +1,11 @@
+cask 'itunesconnect-app' do
+  version '1.1'
+  sha256 '8c22e0323284ed4bb70d6872bb31659353e5e898f30053b64d6d95a21f9ac081'
+
+  url "https://github.com/ronakdev/itunesconnect/releases/download/#{version}/iTunesConnect.app.zip"
+  appcast 'https://github.com/ronakdev/itunesconnect/releases.atom'
+  name 'iTunesConnect'
+  homepage 'https://github.com/ronakdev/itunesconnect'
+
+  app 'iTunesConnect,app'
+end
