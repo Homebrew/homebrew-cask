7f8afd7c75534a8905748bfc8072745f7ae75c78
秦新 <qinxin_0720@live.com>
2018-11-15 23:19:15 +0800

    Update sha256 checksum of folx.app (#54997)

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -1,6 +1,6 @@
 cask 'folx' do
   version '5.7'
-  sha256 '3b360f6f75ab40f07f2d8db4dbc3f06377d5c72e24fc48f2a10c2292a0172f37'
+  sha256 '6644cd53e359704a51d71db5ee2c6c4c43b9c8f682de1419526eebc33a746382'
 
   url 'https://cdn.eltima.com/download/downloader_mac.dmg'
   appcast 'https://cdn.eltima.com/download/folx-updater/folx.xml'
