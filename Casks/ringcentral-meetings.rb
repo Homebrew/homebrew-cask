cbb3bdd54ea388cf47ac974e0ee7d92635f32605
Colin Dean <git@cad.cx>
2018-11-15 13:24:14 -0500

    ringcentral-meetings
    I speculate that the number in the URL may be some kind of version
    number but I'm hesitant to use it because it's clearly not semantic and
    probably not a date, either. The hash for the file as of this commit is
    72bee0536e397c899cc2597a4a00ceca39119c710dc41caefcf9f1b50ef49384.

---
+++ Casks/ringcentral-meetings.rb
@@ -0,0 +1,27 @@
+cask 'ringcentral-meetings' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://dn.ringcentral.com/data/web/download/RCMeetings/1210/RCMeetingsClientSetup.pkg'
+  name 'RingCentral Meetings for Mac'
+  homepage 'https://www.ringcentral.com/online-meetings/overview.html'
+
+  pkg 'RCMeetingsClientSetup.pkg'
+
+  uninstall delete: '/Applications/RingCentral Meetings.app',
+            quit:   'us.zoom.RingCentralOpener',
+            signal: [
+                      ['KILL', 'us.zoom.ringcentral'],
+                    ]
+
+  zap trash: [
+               '~/Desktop/RingCentral Meetings',
+               '~/Library/Application Support/RingCentral Meetings',
+               '~/Library/Caches/us.zoom.ringcentral',
+               '~/Library/Internet Plug-Ins/RingCentralMeetings.plugin',
+               '~/Library/Logs/RingCentralMeetings',
+               '~/Library/Logs/rcmeetingsinstall.log',
+               '~/Library/Preferences/us.zoom.ringcentral.plist',
+               '~/Library/Saved Application State/us.zoom.ringcentral.savedState',
+             ]
+end
