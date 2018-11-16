88c0cf69c9033800695f6024ddebad51b4d5d336
Vítor Galvão <info@vitorgalvao.com>
2018-11-16 15:07:44 +0000

    Update gimp.rb

--- Casks/gimp.rb
+++ Casks/gimp.rb
@@ -1,8 +1,8 @@
 cask 'gimp' do
-  version '2.10.8-2'
+  version '2.10.8,2'
   sha256 '8ee527bfd5b4ac6a9b62dff3b0f0da6e4721b3dac208f29446cdeea6967d4f13'
 
-  url "https://download.gimp.org/pub/gimp/v#{version.major_minor}/osx/gimp-#{version.major_minor_patch}-x86_64-2.dmg"
+  url "https://download.gimp.org/pub/gimp/v#{version.major_minor}/osx/gimp-#{version.before_comma}-x86_64-#{version.after_comma}.dmg"
   name 'GIMP'
   homepage 'https://www.gimp.org/'
 
