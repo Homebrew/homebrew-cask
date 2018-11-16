48e19dfb45b7e997abddd863341007a4c3c286eb
Vítor Galvão <info@vitorgalvao.com>
2018-11-16 14:58:16 +0000

    Update krisp.rb

--- Casks/krisp.rb
+++ Casks/krisp.rb
@@ -2,7 +2,7 @@ cask 'krisp' do
   version '0.5.7'
   sha256 '5a43643864c30191dfafd2d17bf941cd049124146562f27be1bafc56421809b4'
 
-  url 'https://cdn.krisp.ai/installer/release/krisp_0.5.7.pkg'
+  url "https://cdn.krisp.ai/installer/release/krisp_#{version}.pkg"
   name 'Krisp'
   homepage 'https://krisp.ai/index.html'
 
