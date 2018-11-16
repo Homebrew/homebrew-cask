90612924da24c279bf32c9e338607b40713cb327
Alexander Untch <alexander@untch.com>
2018-11-16 15:57:54 +0100

    Update path-finder to 8.3.5 (#55065)

--- Casks/path-finder.rb
+++ Casks/path-finder.rb
@@ -1,6 +1,6 @@
 cask 'path-finder' do
-  version '8.3.4'
-  sha256 '65ecb40e0d00603f99fdb9a63f62136d95640417f3c2cf0e8c933063d0db9da6'
+  version '8.3.5'
+  sha256 '40ec6267e36fa4c88f89c6ae43b634bbcabb9d9c0194341f5acd8cb87ed18a0b'
 
   url 'https://get.cocoatech.com/PF8.dmg'
   appcast 'https://get.cocoatech.com/releasecast.xml'
