2190c2dabcb0722f54c316b2f2abf3c9cc896660
James Chevalier <jchevalier@gmail.com>
2018-11-17 16:18:30 -0500

    Update boostnote to 0.11.11 (#55079)

--- Casks/boostnote.rb
+++ Casks/boostnote.rb
@@ -1,9 +1,9 @@
 cask 'boostnote' do
-  version '0.11.10'
-  sha256 '03ce7dc5ce9df42a70b6de446317654efb877080258c30dcc42a23e6c4f9983b'
+  version '0.11.11'
+  sha256 '6bce521f3fccfe4676f58487297ab62e2784b7bf7ca1f94587f38247a5d2194e'
 
   # github.com/BoostIO/boost-releases was verified as official when first introduced to the cask
-  url "https://github.com/BoostIO/boost-releases/releases/download/v#{version}/Boostnote-mac.dmg"
+  url "https://github.com/BoostIO/boost-releases/releases/download/v#{version}/Boostnote-mac.zip"
   appcast 'https://github.com/BoostIO/boost-releases/releases.atom'
   name 'Boostnote'
   homepage 'https://boostnote.io/'
