f51bade58a72d69d9efc7034391e24a9d55e0055
R J Pontefract <rjpontefract@gmail.com>
2018-11-18 21:06:48 +1300

    Update goodsync to 10.9.16

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.9.14'
-  sha256 'e418e281b0be3fb64c775889660494ff62367363097ea7ed0a3e65a2b3f3c5c9'
+  version '10.9.16'
+  sha256 '02b149c962fb554e20551cfc4a1c3835998773e0d3513cf5ef2c64ebea07d928'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://www.goodsync.com/download'
