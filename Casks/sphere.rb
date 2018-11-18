1ab93286bf72c7d9dce83c8cbddfc2fb4396d411
Basil Shubin <basil.shubin@gmail.com>
2018-11-18 14:15:11 +0700

    Add Sphere Browser Cask

---
+++ Casks/sphere.rb
@@ -0,0 +1,10 @@
+cask 'sphere' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://sphere.tenebris.cc/download/sphere_mac.zip'
+  name 'Sphere'
+  homepage 'https://sphere.tenebris.cc/'
+
+  app 'Sphere.app'
+end
