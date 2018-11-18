e5d3aaedebcfa576bd9758dd4f442ca32e287eaa
LanikSJ <laniksj@gmail.com>
2018-11-18 12:48:57 -0800

    Update appcleaner to 3.5

--- Casks/appcleaner.rb
+++ Casks/appcleaner.rb
@@ -3,8 +3,8 @@ cask 'appcleaner' do
     version '2.3'
     sha256 '69da212e2972e23e361c93049e4b4505d7f226aff8652192125f078be7eecf7f'
   else
-    version '3.4'
-    sha256 '0c60d929478c1c91e0bad76d3c04795665c07a05e45e33321db845429c9aefa8'
+    version '3.5'
+    sha256 '9a67033977622f8523c6d11315fd646b4b3b7b2675ec7c10b37b4d5f5602acb3'
     appcast 'https://freemacsoft.net/appcleaner/Updates.xml'
   end
 
