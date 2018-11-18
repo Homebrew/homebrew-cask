508436d47932609c260a64d19504e8f5d5f05ea2
Tommy Yang <yt940928@gmail.com>
2018-11-17 16:37:28 -0800

    Update surge to 3.0.3-754

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -1,11 +1,14 @@
 cask 'surge' do
-  version '3.0.3-753'
-  sha256 'd31ae640c04bd5d5b84f8fd8a096e932a821df328e2d90b960537a999b48024e'
+  version '3.0.3-754'
+  sha256 'f99ad05f684e2ecc6bb2441f74a42776969ebfc4e169ec8e64ff3d64be1ccf95'
 
   url "https://www.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
   appcast "https://www.nssurge.com/mac/v#{version.major}/appcast.xml"
   name 'Surge'
   homepage 'https://nssurge.com/'
+
+  auto_updates true
+  depends_on macos: '>= :el_capitan'
 
   app "Surge #{version.major}.app"
 end
