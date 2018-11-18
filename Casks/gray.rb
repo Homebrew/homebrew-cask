9f00ec735123095e0103c5f5b426cb5f1cd3e006
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-18 10:14:19 -0800

    Added forgotten `end` to MacOS version switch

--- Casks/gray.rb
+++ Casks/gray.rb
@@ -5,6 +5,7 @@ cask 'gray' do
   else
     version '0.9.3'
     sha256 '0d9566ec7741603ddfcee4a241370aab97605b874ee168e79d0ea89b587b8154'
+  end
 
   url "https://github.com/zenangst/Gray/releases/download/#{version}/Gray.zip"
   appcast 'https://github.com/zenangst/Gray/releases.atom'
