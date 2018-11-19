f8e9de93ca37220f86055ee0d7a3978f62c8a0d0
DoonDoony <cream.doondoon@gmail.com>
2018-11-19 11:47:25 +0900

    Update ridibooks to v2.5.5

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -1,6 +1,6 @@
 cask 'ridibooks' do
-  version '2.5.3'
-  sha256 '51020bb7d36ed6af245591d6a653aa1d5d253ed9203598d87857059669b553d3'
+  version '2.5.5'
+  sha256 '3cca39ed89f768cbd2f8b8f28240c271ea4e4927f8a7133fe13999ddc629f5c6'
 
   url "https://viewer-ota.ridibooks.com/mac/ridibooks-#{version}.dmg"
   name 'Ridibooks'
