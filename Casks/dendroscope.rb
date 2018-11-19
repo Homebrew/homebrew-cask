9201304c0f9e53a543e2544f917ad8328a65c7dc
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-19 08:43:55 -0800

    Update dendroscope to 3.5.10 (#55104)
    * Update dendroscope to 3.5.10
    
    * Changed 'depends_on' statement back
    
    * Removed trailing white space

--- Casks/dendroscope.rb
+++ Casks/dendroscope.rb
@@ -1,6 +1,6 @@
 cask 'dendroscope' do
-  version '3.5.9'
-  sha256 '63d3f42a7143710d40c2b5d4cf9a80e98820f92bf43eb16f4617dcf2d0f5b6c6'
+  version '3.5.10'
+  sha256 '6fcf8a60e481f6880bf05966013cea02ebb512c0dbc0505d4c7759c129e7ce2c'
 
   # ab.inf.uni-tuebingen.de/data/software/dendroscope3 was verified as official when first introduced to the cask
   url "http://ab.inf.uni-tuebingen.de/data/software/dendroscope3/download/Dendroscope_macos_#{version.dots_to_underscores}.dmg"
