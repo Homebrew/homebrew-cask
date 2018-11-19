85ab05a695b75dd25ec753275678ce97c23d1dd3
Curtis Everingham <Everingham.Curtis@Gmail.com>
2018-11-18 17:41:46 -0700

    Update Espionage to 3.7.1 (#55082)
    * Update to version 3.7
    
    Am getting errors trying to install this cask due to the change in SHA256 sum for new version. Verified that the provided hash matches the .dmg downloaded from the link, and that the file inside matches the hash provided by the developer on their site: https://www.taoeffect.com/blog/2018/10/espionage-3-7-released/
    
    * Update espionage to v 3.7.1
    
    Verified that the provided hash matches the .dmg downloaded from the link, and that the file inside matches the hash provided by the developer on their site: https://www.taoeffect.com/blog/2018/10/espionage-3-7-released/

--- Casks/espionage.rb
+++ Casks/espionage.rb
@@ -1,6 +1,6 @@
 cask 'espionage' do
-  version '3.7.0'
-  sha256 '58e66ac38db30d30bc0f3c669167a13017724a93bb47409e8edb38fecafbe4ba'
+  version '3.7.1'
+  sha256 '4d35f4ff294981c7d614b33b163eb0b0587eab6fb8fca0913e0ecc979000d74d'
 
   url 'https://www.espionageapp.com/Espionage.dmg'
   appcast "https://updates.taoeffect.com/espionage#{version.major}/appcast.xml"
