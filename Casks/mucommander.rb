66a62b0354bfae2b1c31e6ac1d05c5c282583504
Yurii Kolesnykov <root@yurikoles.com>
2018-11-19 19:24:06 +0200

    Update mucommander to 0.9.3 (#55148)
    * Update mucommander to 0.9.3
    
    * Update mucommander.rb

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -1,6 +1,6 @@
 cask 'mucommander' do
-  version '0.9.2'
-  sha256 '54486fba723d3d54084414e31337b010c2f28fc4820cefc01cad65e4a5a84744'
+  version '0.9.3'
+  sha256 'cb74e0f857ff5bac804be0548e331de0dbfb9fa74acf948a095bdf2b256a4b34'
 
   # github.com/mucommander/mucommander was verified as official when first introduced to the cask
   url "https://github.com/mucommander/mucommander/releases/download/#{version}/mucommander-#{version}.dmg"
@@ -9,4 +9,8 @@ cask 'mucommander' do
   homepage 'https://www.mucommander.com/'
 
   app 'muCommander.app'
+
+  caveats do
+    depends_on_java '8'
+  end
 end
