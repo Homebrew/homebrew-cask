a763632cebb1c21958f1d479afb39bdd833141f3
Radek Simko <radek.simko@gmail.com>
2018-11-19 11:46:13 +0000

    readandwrite: Update to 7.1.1

--- Casks/readandwrite.rb
+++ Casks/readandwrite.rb
@@ -1,6 +1,6 @@
 cask 'readandwrite' do
-  version '7.1.0'
-  sha256 '1a747bb6aaf815cc971281b3ddba51566b347fd8eae0126859ed27c372c02356'
+  version '7.1.1'
+  sha256 'e67973d5f50f33deaf8c94033c93d566c7182eb69efcc2f51a585076bc6b49af'
 
   url "https://fastdownloads2.texthelp.com/readwritemac#{version.major}/updates/live/readwrite_#{version}.zip"
   appcast "https://readwrite.texthelp.com/rw#{version.major}mac/updatesp/appcast.xml"
