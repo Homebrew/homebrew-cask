d262389dac052744b90b4526cab687951a25ec88
Lutz Roeder <lutzroeder@users.noreply.github.com>
2018-11-19 09:16:14 -0800

    Update netron to 2.5.6 (#55159)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask 'netron' do
-  version '2.5.5'
-  sha256 '006e12ad5ab2da99d317db1a541ec8b90da504b86fa24c757d9df8765f160838'
+  version '2.5.6'
+  sha256 '67d438ac4680c6009f96580048206a165ae649d004e88e0c814d2147cc78a4f2'
 
   url "https://github.com/lutzroeder/Netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   appcast 'https://github.com/lutzroeder/Netron/releases.atom'
