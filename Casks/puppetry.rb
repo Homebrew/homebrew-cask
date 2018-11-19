454aa3063ff3c11cd76df4194e8c32037d91f3dc
Dmitry Sheiko <dmitry@crytek.com>
2018-11-19 23:54:13 +0100

    added puppetry 1.0.0

--- Casks/puppetry.rb
+++ Casks/puppetry.rb
@@ -2,10 +2,11 @@ cask 'puppetry' do
   version '1.0.0'
   sha256 '846c0808492721f5b22fa2ad5edf20bf056fb451c382c50533ddcb5ff3d3ff54'
 
-  url "https://github.com/dsheiko/puppetry/releases/download/v#{version}/puppetry-#{version}-mac.zip"
+  # github.com/dsheiko/puppetry was verified as official when first introduced to the cask
+  url "https://github.com/dsheiko/puppetry/releases/download/v#{version}/puppetry-mac-v#{version}.zip"
   appcast 'https://github.com/dsheiko/puppetry/releases.atom'
   name 'Web testing solution for non-developers on top of Puppeteer and Jest'
-  homepage 'https://puppetry.app'
+  homepage 'https://puppetry.app/'
 
   app 'puppetry.app'
 
