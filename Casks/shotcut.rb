f6e049abf7a0a687a0507408535824337ca7f2f6
Yurii Kolesnykov <root@yurikoles.com>
2018-11-19 11:47:58 +0200

    Update shotcut to 18.11.18

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,6 +1,6 @@
 cask 'shotcut' do
-  version '18.11.13'
-  sha256 'cf37baedea06e78eb8773b4930860443537c228337e11c81c61cd598a61c560a'
+  version '18.11.18'
+  sha256 'f4422d59879b61946871acc08d42011e77bb0683a8af2b6f3048df5a68c6e811'
 
   # github.com/mltframework/shotcut was verified as official when first introduced to the cask
   url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-macos-signed-#{version.no_dots}.dmg"
