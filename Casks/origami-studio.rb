eefc7a768da08481816ba077b041fa8c61ad24d7
Vítor Galvão <info@vitorgalvao.com>
2018-11-19 17:12:59 +0000

    Update origami-studio.rb

--- Casks/origami-studio.rb
+++ Casks/origami-studio.rb
@@ -8,7 +8,7 @@ cask 'origami-studio' do
   name 'Origami Studio'
   homepage 'https://origami.design/'
 
-  depends_on macos: '>= 10.12'
+  depends_on macos: '>= :sierra'
 
   app 'Origami Studio.app'
 end
