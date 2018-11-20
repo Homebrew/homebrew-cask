d85728576c58fd21dfba5986b5725cadfe4212a4
Jamie Willis <jlwillis@gmail.com>
2018-11-19 19:59:28 -0500

    Update pritunl to 1.0.1889.51 (#55189)

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -1,6 +1,6 @@
 cask 'pritunl' do
-  version '1.0.1804.90'
-  sha256 '8427a2e76fdaa257bdc0e5b9aa7ed7698720b85b8ab81d9299e62e5a1896ef49'
+  version '1.0.1889.51'
+  sha256 '7c35723d1d0407b7ff5fbb0cd175d52718cdd70d8b84911e46ba3ad901e9ca5c'
 
   # github.com/pritunl/pritunl-client-electron was verified as official when first introduced to the cask
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip"
