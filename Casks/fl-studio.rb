cf4619889b7313096389b1115f92db73a0d09581
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-20 15:31:24 -0800

    Update fl-studio to 20.0.99.123 (#55195)
    * Update fl-studio to 20.0.99.123
    
    * Update URL and sha256
    
    Signed-off-by: Randall <ran.dall@icloud.com>
    
    * Update app name
    
    Signed-off-by: Randall <ran.dall@icloud.com>
    
    * Update fl-studio.rb

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -1,8 +1,8 @@
 cask 'fl-studio' do
-  version '20.0.5.91'
+  version '20.0.99.123'
   sha256 '53166826d25940684e0dc66d8217cfa38820f760ea180b3778a73571c6517d30'
 
-  url "http://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
+  url "https://support.image-line.com/redirect/flstudio#{version.major}_mac_installer"
   name 'FL Studio'
   homepage 'https://www.image-line.com/flstudio/'
 
