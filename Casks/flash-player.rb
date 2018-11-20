7789bbe26784a057a2a7e350e34da3585b948608
404NetworkError <404NetworkError@users.noreply.github.com>
2018-11-20 12:02:14 -0600

    Update flash-player to 31.0.0.153

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,6 +1,6 @@
 cask 'flash-player' do
-  version '31.0.0.148'
-  sha256 'bb3d70cee69ff58ec42c18eb0f11de59d138616f23ad0dc6b3c532e43f03f4f7'
+  version '31.0.0.153'
+  sha256 '6a34978100dabdbb8653e9e9d707adb196f2300d4a7caacb8df1fc11770c2d1e'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pl.xml'
