662bf34e101702330b698a1b7be5c9462c0846db
404NetworkError <404NetworkError@users.noreply.github.com>
2018-11-20 12:04:05 -0600

    Update flash-player-debugger to 31.0.0.153

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger' do
-  version '31.0.0.148'
-  sha256 '58f926c0535ee26b68c4eee428cde5c16cd20d99b726ace5493124e6274ec9f4'
+  version '31.0.0.153'
+  sha256 '2e65e339bb4de6537048c31d6b12c1aecf919b419fd3dc8503d68d86761f46c2'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml'
