d5c4eceaa01a7484436a7073e628a7714388ec72
Claudia <claui@users.noreply.github.com>
2018-11-20 19:51:27 +0100

    liclipse: use stable URL

--- Casks/liclipse.rb
+++ Casks/liclipse.rb
@@ -1,9 +1,9 @@
 cask 'liclipse' do
-  version '5.1.3'
+  version '5.1.3,32gtr23s399ec2x'
   sha256 'b1a89184a6185d4a9f393912ad1446b2d2bc017ea362bebe35fdefc2db50c8d7'
 
-  # download2267.mediafire.com/23daeigrqddg/32gtr23s399ec2x was verified as official when first introduced to the cask
-  url "https://download2267.mediafire.com/23daeigrqddg/32gtr23s399ec2x/liclipse_#{version}_macosx.cocoa.x86_64.dmg"
+  # www.mediafire.com was verified as official when first introduced to the cask
+  url "https://www.mediafire.com/file/#{version.after_comma}/liclipse_#{version.before_comma}_macosx.cocoa.x86_64.dmg"
   appcast 'https://www.liclipse.com/download.html'
   name 'LiClipse'
   homepage 'https://www.liclipse.com/'
