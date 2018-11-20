68aa0225dbf15fbd830198c3a58db7b99d061834
kkim-sf <31526663+kkim-sf@users.noreply.github.com>
2018-11-20 16:47:59 -0500

    Update chromedriver.rb (#55220)

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,6 +1,6 @@
 cask 'chromedriver' do
-  version '2.43'
-  sha256 '84244e590d866294c1eaa5fa65ad51f1b0d2cc4fc1595aa3e414f5aac4da60ef'
+  version '2.44'
+  sha256 '3fd49c2782a5f93cb48ff2dee021004d9a7fb393798e4c4807b391cedcd30ed9'
 
   # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
