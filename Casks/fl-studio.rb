0e6b0ec5f512b69ee147641d73933e04da597c2f
Randall <ran.dall@icloud.com>
2018-11-20 08:07:19 -0800

    Update app name
    Signed-off-by: Randall <ran.dall@icloud.com>

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -3,7 +3,7 @@ cask 'fl-studio' do
   sha256 '53166826d25940684e0dc66d8217cfa38820f760ea180b3778a73571c6517d30'
 
   url "https://support.image-line.com/redirect/flstudio#{version.major}_mac_installer"
-  name 'FL Studio 20'
+  name "FL Studio #{version.major}"
   homepage 'https://www.image-line.com/flstudio/'
 
   pkg 'Install FL Studio.pkg'
