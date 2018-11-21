8ddb4bf88fbe5e110896dab2714a80366f6daac1
David Yantis <404NetworkError@users.noreply.github.com>
2018-11-21 04:28:25 -0600

    Update flash-player-debugger-npapi to 31.0.0.153 (#55218)

--- Casks/flash-player-debugger-npapi.rb
+++ Casks/flash-player-debugger-npapi.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger-npapi' do
-  version '31.0.0.148'
-  sha256 'dab280b84dbc31b0362141ed33cce9befb577a57c021051453af481105b11199'
+  version '31.0.0.153'
+  sha256 'b849fad1ebea447fd67b169e6276b4a933441b3c19c3e3383463e026fa9b3d81'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_plugin_debug.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pl.xml'
