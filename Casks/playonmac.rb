ee45d49c74e33c575097bf072cbd15d4b1254d79
chincheta0815 <Rouven_Weiler@gmx.net>
2018-11-21 11:51:12 +0100

    update version: playonmac to 4.3.3 (#55244)

--- Casks/playonmac.rb
+++ Casks/playonmac.rb
@@ -1,6 +1,6 @@
 cask 'playonmac' do
-  version '4.2.12'
-  sha256 '1e2b51cd1bbbde7a7837af3bb4b3cc113034adc4f7ba41e8bb85dd0b8f0f7f2a'
+  version '4.3.3'
+  sha256 '795099f03efde730daf9e30c8c55575851f89484d9d38dee7d010987fce1ed85'
 
   url "https://repository.playonmac.com/PlayOnMac/PlayOnMac_#{version}.dmg"
   appcast "https://github.com/PlayOnLinux/POL-POM-#{version.major}/releases.atom"
