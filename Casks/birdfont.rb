2b6cd543f2562583c8245b68f2770aa7a656cede
suschizu <brigitte@corecode.io>
2018-11-21 20:17:35 +0000

    Update birdfont.rb
    I got a connection error when updating this cask via cask-repair - I hope this way will work

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -1,12 +1,16 @@
 cask 'birdfont' do
-  version '3.15.0'
-  sha256 '0d4a4b0278db9b10b038364e1819d642ef39094c5ad719d44a9065461c3483ae'
-
+  if MacOS.version <= :el_capitan
+    version '2.19.4'
+    sha256 '013d9c42c2252b57079453bd27e4c18dbbc09eda55563ff1516fd079c0499f76'
+  else
+    version '3.16.0'
+    sha256 '43a9ef571cae34b9f8f3b4f5e2846472d5dce2dc8297dad76c358f4966f50502'
+end
   url "https://birdfont.org/download/birdfont-#{version}-free.dmg"
   name 'BirdFont'
   homepage 'https://birdfont.org/'
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :el_capitan'
 
   app 'BirdFontNonCommercial.app'
 end
