6c10717f8e71d506c5cbd855380f35b633e2aaf1
Alexandre Alonso <alex4551@gmail.com>
2018-11-21 18:26:18 +0800

    update studio-3t to 2018.5.1 (#55241)

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask 'studio-3t' do
-  version '2018.5.0'
-  sha256 '1128bbcae134f7f494cd11e14c32ea58d501f51aed0212b7bd9ed26547cd4c76'
+  version '2018.5.1'
+  sha256 'e1bc26a9c8cdea792c67b84b7712b4dbd794649b1f7baf3135cc2e101e9b19a4'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'https://files.studio3t.com/changelog/changelog.txt'
