44a0b80963b5acb25ea2a340c0f9af6c684f6ba3
Joseph J.C. Tang <jinntrance@gmail.com>
2018-11-23 07:22:06 +0800

    Update freedom to 2.0.3

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,6 +1,6 @@
 cask 'freedom' do
-  version '2.0.1'
-  sha256 '1ab1b4703395f0c0532e5e833cc7a2919b67f69eec467cc2008bd612ce29d43e'
+  version '2.0.3'
+  sha256 '4f824407b66ddf79bce9a37200139d4ddc1bfbfee5406948c3082f077bfc924a'
 
   url "https://cdn.freedom.to/installers/updates/mac/#{version}/Freedom.zip"
   appcast 'https://cdn.freedom.to/installers/updates/mac/Appcast.xml'
