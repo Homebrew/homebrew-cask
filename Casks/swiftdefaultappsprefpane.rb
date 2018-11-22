c1679e5584291a09ffeeca485693211ef2951efd
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2018-11-22 23:28:12 +0100

    Add SwiftDefaultApps.prefpane v1.1.3 (#55278)
    * Add SwiftDefaultApps.prefpane v1.1.3
    
    * Update and rename swift-default-apps.rb to swiftdefaultapps.rb
    
    * Update swiftdefaultapps.rb
    
    * Rename swiftdefaultapps.rb to swiftdefaultappsprefpane.rb

---
+++ Casks/swiftdefaultappsprefpane.rb
@@ -0,0 +1,11 @@
+cask 'swiftdefaultappsprefpane' do
+  version '1.1.3'
+  sha256 '240c97148580cca3ce256dd185346d1d31b36f4116c97273b3ab6a0d0f7d974b'
+
+  url "https://github.com/Lord-Kamina/SwiftDefaultApps/releases/download/v#{version}/SwiftDefaultApps-v#{version}.zip"
+  appcast 'https://github.com/Lord-Kamina/SwiftDefaultApps/releases.atom'
+  name 'SwiftDefaultApps'
+  homepage 'https://github.com/Lord-Kamina/SwiftDefaultApps'
+
+  prefpane 'SwiftDefaultApps.prefPane'
+end
