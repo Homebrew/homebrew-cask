f72b5a60f1ea52f5be8fb32f6731f28e83653028
Tom Bigelajzen <public@tombigel.com>
2018-11-22 19:27:11 +0200

    Update visual-paradigm-ce to 15.1,20181103 (#55290)

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm-ce' do
-  version '15.1,20181102'
-  sha256 'c02ff7d59e4c7244fcf3785c614bb2eb739b26d60bcedd92c7509b1f24a25b69'
+  version '15.1,20181103'
+  sha256 'd4bc4de3110b147821fb480d0073a1543bd5529d33a7f1d84d46ea4d9a93631c'
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html'
