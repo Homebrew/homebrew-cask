6f134930bf34e773dddf06c06b1184428a386fea
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-22 09:44:41 -0800

    Update realm-studio to 3.0.7 (#55272)

--- Casks/realm-studio.rb
+++ Casks/realm-studio.rb
@@ -1,6 +1,6 @@
 cask 'realm-studio' do
-  version '2.9.0'
-  sha256 '8cea28deec8d593d124d557a6363727b71f0ce0b2944271f33638d5f984a6feb'
+  version '3.0.7'
+  sha256 '7a113b9b59412044dbcfc0a0df6abbb177801846257b420ff7dcdab372115993'
 
   url "https://static.realm.io/downloads/realm-studio/Realm%20Studio-#{version}-mac.zip"
   appcast 'https://static.realm.io/downloads/realm-studio/latest-mac.yml'
