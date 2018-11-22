b881a5967d27df3adf41b829d9577ed5eaf8d91a
Julian Mayer <julian@corecode.io>
2018-11-22 07:21:12 -0400

    Update stremio to 4.4.10

--- Casks/stremio.rb
+++ Casks/stremio.rb
@@ -1,8 +1,9 @@
 cask 'stremio' do
-  version '3.6.5'
-  sha256 '079a39585cd50be2b914ecb1437295f87b894fa28816925d2382232bb0625fe8'
+  version '4.4.10'
+  sha256 'b0c185d2f161b13d9e1a22c3ec35305c34afa1d69be400cf6c684da0317a8f0e'
 
-  url "https://dl.strem.io/Stremio%20#{version}.dmg"
+  url "https://dl.strem.io/mac/v#{version}/Stremio+#{version}.dmg"
+  appcast 'https://www.stremio.com/downloads'
   name 'Stremio'
   homepage 'https://www.strem.io/'
 
