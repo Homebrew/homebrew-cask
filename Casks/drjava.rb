5e2853d994ee0839f97e47a93acaa53fc314b201
Hansel Wei <hwei3@uncc.edu>
2018-11-21 21:41:18 -0500

    Fixed Formula

--- Casks/drjava.rb
+++ Casks/drjava.rb
@@ -1,10 +1,12 @@
 cask 'drjava' do
-  version :latest
-  sha256 :no_check
+  version '20160913-225446,beta'
+  sha256 'e26b5ca518585ec3d9500d6709e4c1cd2ad41ea041606762cc66254af9c7f431'
 
-  url 'https://sourceforge.net/projects/drjava/files/latest/download'
+  # downloads.sourceforge.net/drjava/ was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/drjava/1.%20DrJava%20Stable%20Releases/drjava-#{version.after_comma}-#{version.before_comma}/drjava-#{version.after_comma}-#{version.before_comma}-osx.tar.gz"
+  appcast 'https://sourceforge.net/projects/drjava/rss?path=/1.%20DrJava%20Stable%20Releases'
   name 'DrJava'
-  homepage 'https://sourceforge.net/p/drjava'
+  homepage 'http://www.drjava.org/'
 
-  app 'drjava.jar'
+  app 'DrJava.app'
 end
