7380f552519fdb584b34fe3003ced0ef0ce36649
yuusharo <36213375+yuusharo@users.noreply.github.com>
2018-11-22 09:23:34 -0800

    Update duet to 2.0.3.8 (#55259)

--- Casks/duet.rb
+++ Casks/duet.rb
@@ -1,6 +1,6 @@
 cask 'duet' do
-  version '2.0.3.1'
-  sha256 '905f62b3af2c39d22dc1563dab8ee86b27b7c4457947875a7aec10800d016d87'
+  version '2.0.3.8'
+  sha256 'fc65cdc1baf0eaf80d301e1378ff83e29ba9282a350429205c2ebe87983af613'
 
   # duet.nyc3.cdn.digitaloceanspaces.com/Mac was verified as official when first introduced to the cask
   url "https://duet.nyc3.cdn.digitaloceanspaces.com/Mac/#{version.major_minor.dots_to_underscores}/duet-#{version.dots_to_hyphens}.zip"
