56df286f36211527b73058497b71808692be45b9
Chris Finazzo <chris.finazzo@gmail.com>
2018-11-22 12:38:55 -0500

    Update BBEdit to 12.5.1 (#55282)

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -3,8 +3,8 @@ cask 'bbedit' do
     version '12.1.6'
     sha256 '23b9fc6ef5c03cbcab041566503c556d5baf56b2ec18f551e6f0e9e6b48dc690'
   else
-    version '12.5'
-    sha256 'ab40f9f3177a23851b9607cfd6ac427dfec4982330a08a1b22cd7ed438548114'
+    version '12.5.1'
+    sha256 '92b59a759b04255cd4715e7d80f9053ca7d6c85f5c39a66d025fb71d0f30aee8'
   end
   # s3.amazonaws.com/BBSW-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
