c3601ded4d70ac2eaf6b6730ed528aad29cff63f
Vítor Galvão <info@vitorgalvao.com>
2018-11-22 22:00:05 +0000

    Update microsoft-office.rb

--- Casks/microsoft-office.rb
+++ Casks/microsoft-office.rb
@@ -4,7 +4,7 @@ cask 'microsoft-office' do
 
   # officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac was verified as official when first introduced to the cask
   url "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/Microsoft_Office_#{version}_Installer.pkg"
-  name 'Microsoft Office for Mac'
+  name 'Microsoft Office'
   homepage 'https://products.office.com/mac/microsoft-office-for-mac/'
 
   auto_updates true
