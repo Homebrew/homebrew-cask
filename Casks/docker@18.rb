e5e643d972d7cc7c41d48964e45c59ebe0d416b2
Amar1729 <amar.paul16@gmail.com>
2018-11-21 21:39:42 -0500

    Docker.app 18: keep for yosemite-el capitan compat

---
+++ Casks/docker@18.rb
@@ -0,0 +1,50 @@
+cask 'docker@18' do
+  version '18.06.1-ce-mac73,26764'
+  sha256 '3429eac38cf0d198039ad6e1adce0016f642cdb914a34c67ce40f069cdb047a5'
+
+  url "https://download.docker.com/mac/stable/#{version.after_comma}/Docker.dmg"
+  appcast 'https://download.docker.com/mac/stable/appcast.xml'
+  name 'Docker Community Edition'
+  name 'Docker CE'
+  homepage 'https://www.docker.com/community-edition'
+
+  auto_updates true
+  depends_on macos: '>= :yosemite'
+
+  app 'Docker.app'
+
+  uninstall delete:    [
+                         '/Library/PrivilegedHelperTools/com.docker.vmnetd',
+                         '/private/var/tmp/com.docker.vmnetd.socket',
+                         '/usr/local/bin/docker',
+                         '/usr/local/bin/docker-compose',
+                         '/usr/local/bin/docker-credential-osxkeychain',
+                         '/usr/local/bin/docker-machine',
+                         '/usr/local/bin/hyperkit',
+                         '/usr/local/bin/notary',
+                         '/usr/local/bin/vpnkit',
+                       ],
+            launchctl: [
+                         'com.docker.helper',
+                         'com.docker.vmnetd',
+                       ],
+            quit:      'com.docker.docker'
+
+  zap trash: [
+               '/usr/local/bin/docker-compose.backup',
+               '/usr/local/bin/docker-machine.backup',
+               '/usr/local/bin/docker.backup',
+               '~/Library/Application Scripts/com.docker.helper',
+               '~/Library/Caches/KSCrashReports/Docker',
+               '~/Library/Caches/com.docker.docker',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.docker.docker',
+               '~/Library/Containers/com.docker.docker',
+               '~/Library/Containers/com.docker.helper',
+               '~/Library/Group Containers/group.com.docker',
+               '~/Library/Preferences/com.docker.docker.plist',
+             ],
+      rmdir: [
+               '~/Library/Caches/KSCrashReports',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data',
+             ]
+end
