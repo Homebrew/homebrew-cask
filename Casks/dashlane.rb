bf0be431ec4f230f0c5e58d204a114a180c325c5
CoreCode <core-code@users.noreply.github.com>
2018-11-22 19:57:46 -0400

    Update dashlane.rb

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,6 +1,6 @@
 cask 'dashlane' do
   version '6.1844.0.14691'
-  sha256 '492432dc0bdae1482be7575ed1ad360b76f5c86b9546a9d2296d06b655a1f360'
+  sha256 '24b8cfca4698cdbbf53309b3abe09a5308f888fc0e46096b53d596c2c30a4b3b'
 
   url "https://cdn5.dashlane.com/proxy/d3mfqat9ni8wb5/releases/#{version.major_minor_patch}/#{version}/release/Dashlane.dmg"
   appcast 'https://ws1.dashlane.com/5/binaries/query?target=archive&format=xml&currentSoftwareVersion=6.0.0&platform=server_osx&os=OS_X_10_14_1'
