8546d5ac894207e747b137d80670a0aff2cbf3b0
Shoichi Aizawa <shoichiaizawa@gmail.com>
2018-11-23 02:46:06 +0900

    Update mark-text to 0.13.65 (#55267)

--- Casks/mark-text.rb
+++ Casks/mark-text.rb
@@ -1,6 +1,6 @@
 cask 'mark-text' do
-  version '0.13.53'
-  sha256 'e330e325a485165f43e462d097ccfea972e12d455b4b9cc37e408d628eb575c9'
+  version '0.13.65'
+  sha256 '674e1a2fe3290166786233f1df43b4c695a66dac0246f249f71ac04e0d123099'
 
   # github.com/marktext/marktext was verified as official when first introduced to the cask
   url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext-#{version}.dmg"
