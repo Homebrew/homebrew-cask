1a619c25553f69f9e53a9b20a735e952a0222e4d
Hsiao-nan Cheung <niheaven@gmail.com>
2018-11-23 15:19:06 +0800

    Update Terminus to 1.0.0-alpha.63

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -1,9 +1,9 @@
 cask 'terminus' do
-  version '1.0.0-alpha.61'
-  sha256 'dd8ed7947c264f6a0d6c3854b6137fe304c558b822761c4bae08987909ee2647'
+  version '1.0.0-alpha.63'
+  sha256 'd769c002c471c0c2dccc56cd9cd2da9dc97319faf7e5b47879eec2db1faa8410'
 
   # github.com/Eugeny/terminus was verified as official when first introduced to the cask
-  url "https://github.com/Eugeny/terminus/releases/download/v#{version}/Terminus-#{version}-macos.dmg"
+  url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos.dmg"
   appcast 'https://github.com/Eugeny/terminus/releases.atom'
   name 'Terminus'
   homepage 'https://eugeny.github.io/terminus/'
