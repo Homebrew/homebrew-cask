1de8e51584312e14cde70314f8a418ca0b218cbd
Vítor Galvão <info@vitorgalvao.com>
2018-11-23 09:56:41 -0400

    Update Casks/reaper.rb
    Co-Authored-By: core-code <core-code@users.noreply.github.com>

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,5 +1,5 @@
 cask 'reaper' do
-  version '5.96.200,5.962'
+  version '5.96.200'
   sha256 'fcf2b34cc479093c2b9c8c37c78a6633769b2e20d6864165398a13124f454dc9'
 
   url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
