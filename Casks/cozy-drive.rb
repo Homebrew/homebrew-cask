6959025e337f5b42252842476da67596ba2016fa
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-23 05:22:49 -0800

    Update cozy-drive to 3.10.3 (#55316)

--- Casks/cozy-drive.rb
+++ Casks/cozy-drive.rb
@@ -1,6 +1,6 @@
 cask 'cozy-drive' do
-  version '3.10.2'
-  sha256 '9d79a873347ca0784596c205d96bacbf525e049c584f2b4ce343a939fc6a1fee'
+  version '3.10.3'
+  sha256 '1f13e3d2e94c72b4332db4da760fdb8a168e4176e97d0dc496a8d78a818c34fd'
 
   # nuts.cozycloud.cc was verified as official when first introduced to the cask
   url "https://nuts.cozycloud.cc/download/channel/stable/CozyDrive-#{version}.dmg"
