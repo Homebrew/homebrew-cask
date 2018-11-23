9473a0b2422d3d0528f7c7d0ec7a6e989de7e858
Randall <ran.dall@icloud.com>
2018-11-22 21:09:54 -0800

    Update phoenix-slides to 1.4.3

--- Casks/phoenix-slides.rb
+++ Casks/phoenix-slides.rb
@@ -1,6 +1,6 @@
 cask 'phoenix-slides' do
-  version '1.4.2'
-  sha256 'bde43c827972b8539198a0838e59e5d8f0fb3376a767eb04e467b584c832d239'
+  version '1.4.3'
+  sha256 'ef150da77dc578cfdaaf94fd7680d4b09107d3cdf217e71f7065eb02bf5be934'
 
   # github.com/gobbledegook/creevey was verified as official when first introduced to the cask
   url "https://github.com/gobbledegook/creevey/releases/download/v#{version}/phoenix-slides-#{version.no_dots}.dmg"
