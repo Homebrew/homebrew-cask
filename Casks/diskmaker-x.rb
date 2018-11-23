2fe631c9350d4a667b4345fb06074ff861c569de
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-23 05:23:03 -0800

    Update diskmaker-x to 8.0.3 (#55318)

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -1,6 +1,6 @@
 cask 'diskmaker-x' do
-  version '8.0.2'
-  sha256 'b63537c31faa49e8a3f850d86d93a7185fcbf50ff50d242a8ff79fb748b9374e'
+  version '8.0.3'
+  sha256 '79b490dc829775450aafadeddd0afc58bdcef9c60fc82d9db1427c51b57e88a7'
 
   url "https://diskmakerx.com/downloads/DiskMaker_X_#{version.no_dots}.dmg"
   appcast 'https://diskmakerx.com/feed/'
