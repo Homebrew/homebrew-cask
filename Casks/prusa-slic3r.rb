255c1deb39fca7519971a293db9556689b74c2c5
Calvin Bui <cookiecaow@gmail.com>
2018-11-24 11:59:36 +1100

    Update prusa-slic3r to 1.41.2,201811221501

--- Casks/prusa-slic3r.rb
+++ Casks/prusa-slic3r.rb
@@ -1,6 +1,6 @@
 cask 'prusa-slic3r' do
-  version '1.41.1,201810261144'
-  sha256 'a3246a2f1e2cf37b7ae361bb5ae4c95a24f62fe1795e99946281b2d3b3afb9fc'
+  version '1.41.2,201811221501'
+  sha256 'f5bd25f8e675763e849b2b9a5b2a24e2a1f5750da0075402eafae52a67d0ef90'
 
   # github.com/prusa3d/Slic3r was verified as official when first introduced to the cask.
   url "https://github.com/prusa3d/Slic3r/releases/download/version_#{version.before_comma}/Slic3rPE-#{version.before_comma}+full-#{version.after_comma}.dmg"
