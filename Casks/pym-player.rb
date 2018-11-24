c10424a3e0b30d4e4fefd2cec4d2c6cc24709d8d
Antoni Rosciszewski <antoni.rosciszewski@gmail.com>
2018-11-24 15:54:31 +0100

    Add new Cask: PYM Player 6.0.9,528

---
+++ Casks/pym-player.rb
@@ -0,0 +1,11 @@
+cask 'pym-player' do
+  version '6.0.9,528'
+  sha256 '142529b44d496bd2e182a348bd4072d466fbc42021d26c68cacbb4e5ce1dbe21'
+
+  url "http://pym.uce.pl/download/pliki/PYMPlayer#{version.before_comma.no_dots}_#{version.after_comma}.dmg"
+  appcast 'http://pym.uce.pl/pym-player/'
+  name 'PYM Player'
+  homepage 'http://pym.uce.pl/pym-player/'
+
+  app 'PYM Player.app'
+end
