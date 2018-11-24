cae1b7d5d4989d108e4c0a31bd661edb21b3c107
suschizu <brigitte@corecode.io>
2018-11-24 11:22:37 -0400

    Update unity-linux-support-for-editor.rb
    update unity-linux-support-for-editor to version 2018.2.17f1,88933597c842

--- Casks/unity-linux-support-for-editor.rb
+++ Casks/unity-linux-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-linux-support-for-editor' do
-  version '2018.2.15f1,65e0713a5949'
-  sha256 'c850b5a9b8f210e297b3b5cf225bec3badba5a52d425f0ab92165d1a02d7fbff'
+  version '2018.2.17f1,88933597c842'
+  sha256 '0056398a015aa2aefcdb92096b1829d656dfd7bb839fdc3f3749e7e8e11c9237'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/download/archive'
