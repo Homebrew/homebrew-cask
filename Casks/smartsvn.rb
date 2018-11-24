6c66070015c51adb23c86f54a9c78742d9324e0a
Ralph Martin <ralph@cs.cf.ac.uk>
2018-11-24 19:45:55 +0000

    Update smartsvn to 11.0.0 (#55286)
    * Update smartsvn to 11.0.0
    
    * Update smartsvn.rb

--- Casks/smartsvn.rb
+++ Casks/smartsvn.rb
@@ -1,13 +1,13 @@
 cask 'smartsvn' do
-  version '9.3.2'
-  sha256 '69dee11c1230e1ec59b0022552c6dd26788d810295315be82e0f0186bacf8925'
+  version '11.0.0'
+  sha256 'f330c30be85dd5df101c2414f8c9f003f7305c786fbaabd3bafa5d506f78e329'
 
   url "https://www.smartsvn.com/downloads/smartsvn/smartsvn-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.smartsvn.com/documents/smartsvn/changelog.txt'
   name 'SmartSVN'
   homepage 'https://www.smartsvn.com/'
 
-  app "SmartSVN #{version.major_minor}.app"
+  app "SmartSVN #{version.major}.app"
 
   zap trash: [
                '~/Library/Preferences/com.syntevo.smartsvn.plist',
