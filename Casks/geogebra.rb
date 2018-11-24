9c7d0b75de42e806813189b868ba73611b4f009c
CoreCode <core-code@users.noreply.github.com>
2018-11-23 20:28:13 -0400

    Update geogebra.rb
    Update geogebra to 6.0.511.0

--- Casks/geogebra.rb
+++ Casks/geogebra.rb
@@ -1,6 +1,6 @@
 cask 'geogebra' do
-  version '6.0.509.0'
-  sha256 '26cc016667921fa956bebcd00e10b72924624247fe44b9dc7955601a85b2895d'
+  version '6.0.511.0'
+  sha256 '5d9a52e56bd5d0deeceff973132fe98455c7d1fb38e518ce8fb47a51942a87ec'
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-Classic-6-MacOS-Portable-#{version.dots_to_hyphens}.zip"
   name 'GeoGebra'
