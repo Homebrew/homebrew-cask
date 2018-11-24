852b5ccd86a59c48f0e414e49ae5d8faf1293b0c
Alex Alonso <alex4551@gmail.com>
2018-11-24 18:10:16 +0800

    update mongodb compass version 1.16.1

--- Casks/mongodb-compass.rb
+++ Casks/mongodb-compass.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass' do
-  version '1.15.4'
-  sha256 '7da08ba004a389a4d8cbd0bf55899e5e8d4d55e842b9625efd4e51405240bd0a'
+  version '1.16.1'
+  sha256 'a3f62b0f516bfb21ca5f695a9962ed499d2045691d4f974befca0e0298d95bc7'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-#{version}-darwin-x64.dmg"
   name 'MongoDB Compass'
