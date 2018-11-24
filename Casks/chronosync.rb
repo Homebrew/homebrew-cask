6456082f7a17c164b6e9d6dc67f70d8b761f320d
suschizu <brigitte@corecode.io>
2018-11-24 18:22:24 -0400

    Update chronosync.rb
    adding appcast-stanza and updating chronosync to version 4.8.7 (instead of version :latest)

--- Casks/chronosync.rb
+++ Casks/chronosync.rb
@@ -1,8 +1,9 @@
 cask 'chronosync' do
-  version :latest
-  sha256 :no_check
+  version '4.8.7'
+  sha256 '04ab013aea545632194c622a0dcd680c8c53e8f5ccee97e42d77bbc6c0214b60'
 
   url 'https://downloads.econtechnologies.com/CS4_Download.dmg'
+  appcast 'https://www.econtechnologies.com/UC/updatecheck.php?prod=ChronoSync&vers=4.8.0&lang=en&plat=mac&os=10.14.1&hw=i64&req=1'
   name 'ChronoSync'
   homepage 'https://www.econtechnologies.com/'
 
