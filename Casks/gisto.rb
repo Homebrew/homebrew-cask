18401ab1c87844b01f407c015202e8175f3af43d
Sasha Khamkov <sanusart@gmail.com>
2018-11-25 00:28:35 +0200

    Update gisto to 1.10.17 (#55370)

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -1,6 +1,6 @@
 cask 'gisto' do
-  version '1.10.16'
-  sha256 'e39df10e8e1cbe359f3a300739fdc7f3c3c3af0bb37d5b54840d55350fe3afb6'
+  version '1.10.17'
+  sha256 '8c95961fab98b6c7f70e70971597b6ad461884beeea17ea44d03896e797cac2a'
 
   # github.com/Gisto/Gisto was verified as official when first introduced to the cask
   url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto-#{version}.dmg"
