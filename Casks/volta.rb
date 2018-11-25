6f7afb4bec05515ee593c36d38f4f1cd31c5df15
Naoto Yoshimoto <me@naotone.com>
2018-11-25 15:43:02 -0500

    Add Volta Latest

---
+++ Casks/volta.rb
@@ -0,0 +1,10 @@
+cask 'volta' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://volta.garymathews.com/Volta.dmg'
+  name 'Volta'
+  homepage 'http://volta.garymathews.com'
+
+  app 'Volta.app'
+end
