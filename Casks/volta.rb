63e63070cc0425d10d217b83fa0a18c4d7ac3d54
Naoto Yoshimoto <me@naotone.com>
2018-11-25 16:30:25 -0500

    Add Volta Latest

--- Casks/volta.rb
+++ Casks/volta.rb
@@ -2,9 +2,17 @@ cask 'volta' do
   version :latest
   sha256 :no_check
 
-  url 'http://volta.garymathews.com/Volta.dmg/'
+  url 'http://volta.garymathews.com/Volta.dmg'
   name 'Volta'
   homepage 'http://volta.garymathews.com/'
 
+  depends_on macos: '>= :yosemite'
+
   app 'Volta.app'
+
+  uninstall login_item: 'Volta',
+            quit:       [
+                          'com.gmathews.volta',
+                          'com.gmathews.volta.helper',
+                        ]
 end
