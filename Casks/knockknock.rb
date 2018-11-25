949de8b6b59ed16dfc3350de25e90b1c4f38fccd
Randall <17261190+ran-dall@users.noreply.github.com>
2018-11-25 01:00:24 -0800

    Update knockknock to 2.0.1 (#55381)

--- Casks/knockknock.rb
+++ Casks/knockknock.rb
@@ -1,6 +1,6 @@
 cask 'knockknock' do
-  version '2.0.0'
-  sha256 '80873e95b66dee363828b386f08d759675ce85bf02e21484aacd95f2dc3ae0e5'
+  version '2.0.1'
+  sha256 '67916905ccd03b1339db578583ea72dbcec88caccd51b61300ad9561b124b393'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/KnockKnock_#{version}.zip"
