243066a50a48884e5e004bd24f2b20b9aba8dffb
Vítor Galvão <info@vitorgalvao.com>
2018-11-26 13:36:03 +0000

    Update shoebox.rb

--- Casks/shoebox.rb
+++ Casks/shoebox.rb
@@ -2,16 +2,9 @@ cask 'shoebox' do
   version :latest
   sha256 :no_check
 
-  if MacOS.version >= :mavericks
-    url 'http://downloads.shoeboxapp.com/desktopv3/Shoebox%20Setup.dmg'
-  else
-    url 'http://downloads.shoeboxapp.com/mac/Shoebox%20Installer.dmg'
-  end
-
+  url 'http://downloads.shoeboxapp.com/desktopv3/Shoebox%20Setup.dmg'
   name 'Shoebox'
   homepage 'https://shoeboxapp.com/'
-
-  depends_on macos: '>= :snow_leopard'
 
   app 'Shoebox.app'
 
