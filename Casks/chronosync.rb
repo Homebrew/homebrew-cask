6e0d981f9b36d76e07b74c2070274b6f25a21f11
Vítor Galvão <info@vitorgalvao.com>
2018-11-26 13:09:31 +0000

    Update chronosync.rb

--- Casks/chronosync.rb
+++ Casks/chronosync.rb
@@ -3,7 +3,7 @@ cask 'chronosync' do
   sha256 '04ab013aea545632194c622a0dcd680c8c53e8f5ccee97e42d77bbc6c0214b60'
 
   url 'https://downloads.econtechnologies.com/CS4_Download.dmg'
-  appcast 'https://www.econtechnologies.com/UC/updatecheck.php?prod=ChronoSync&vers=4.8.0&lang=en&plat=mac&os=10.14.1&hw=i64&req=1'
+  appcast "https://www.econtechnologies.com/UC/updatecheck.php?prod=ChronoSync&vers=#{version.major_minor}.0&lang=en&plat=mac&os=10.14.1&hw=i64&req=1"
   name 'ChronoSync'
   homepage 'https://www.econtechnologies.com/'
 
