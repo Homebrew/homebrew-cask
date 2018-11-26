df43eddd432f8d8a312addacea3efb26a4f2a288
CoreCode <core-code@users.noreply.github.com>
2018-11-26 17:40:44 -0400

    Update bonitastudiocommunity.rb

--- Casks/bonitastudiocommunity.rb
+++ Casks/bonitastudiocommunity.rb
@@ -1,9 +1,9 @@
 cask 'bonitastudiocommunity' do
-  version '7.7.1'
-  sha256 '9f6679f9bdb1a3d853cd519e9eade72ac531faabfae92c38e8d9fe52dd832f12'
+  version '7.7.5'
+  sha256 '5366d98b5c436ac20e7ed0603e17b16e15186a4a4beee23e30daac51a9f60e64'
 
   # download.forge.ow2.org was verified as official when first introduced to the cask
-  url "https://download.forge.ow2.org/bonita/BonitaStudioCommunity-#{version}-x86_64.dmg"
+  url "https://release.ow2.org/bonita/BonitaStudioCommunity-#{version}-x86_64.dmg"
   appcast 'https://www.bonitasoft.com/downloads'
   name 'Bonita Studio Community Edition'
   homepage 'https://www.bonitasoft.com/downloads'
