95eaf8c6d25b9c5fb09626fe94bc81b4649cff1a
Anver Housseini <contact@anver.ch>
2018-11-26 13:43:05 +0100

    Update iterm2 to 3.2.6

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -1,7 +1,7 @@
 cask 'iterm2' do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  version '3.2.5'
-  sha256 'ec1ee603de1f8665d67781df98857c7feeb72461729f7cab2fabcecc19cf7268'
+  version '3.2.6'
+  sha256 'dbc44180c2de9c741fdd8f96750f3502d79aa1c53ed884538648bdb558abd884'
 
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
   appcast 'https://iterm2.com/appcasts/final.xml'
