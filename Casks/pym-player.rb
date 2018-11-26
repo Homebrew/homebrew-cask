30d2b3f2f63d8bd88ecf9489c5b3e0a3300c7a1f
Vítor Galvão <info@vitorgalvao.com>
2018-11-26 13:53:53 +0000

    Update pym-player.rb

--- Casks/pym-player.rb
+++ Casks/pym-player.rb
@@ -1,5 +1,5 @@
 cask 'pym-player' do
-  version '6.0.9,528'
+  version '6.0.10,563'
   sha256 '142529b44d496bd2e182a348bd4072d466fbc42021d26c68cacbb4e5ce1dbe21'
 
   url "http://pym.uce.pl/download/pliki/PYMPlayer#{version.before_comma.no_dots}_#{version.after_comma}.dmg"
