b4a8ee4b5d1d84f25ebd211d3037d31f426fa82f
Jonathan Zisser <jzisser9@gmail.com>
2018-11-27 12:56:45 -0500

    Update pomello to 0.10.2 (#55456)

--- Casks/pomello.rb
+++ Casks/pomello.rb
@@ -1,6 +1,6 @@
 cask 'pomello' do
-  version '0.10.1'
-  sha256 'a206447b88e842d2d4aa886bee9721e3b7e995ecf1bbf73b379b1a273d737ad1'
+  version '0.10.2'
+  sha256 '382b1c3800fbe5105141b754c30923accdedc64bde4c4d2a7cda2f1704dfc617'
 
   url 'https://pomelloapp.com/download/mac/latest'
   appcast 'https://pomelloapp.com/download/mac'
