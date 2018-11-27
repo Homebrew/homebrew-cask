098d7e181532e78340ccc64a10c93682cd2275bf
Rick van Galen <DCKcode@users.noreply.github.com>
2018-11-27 21:01:00 +0100

    Update Seashore to different upstream source with version 0.6.5

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,18 +1,12 @@
 cask 'seashore' do
-  version '0.5.1'
-  sha256 '96463a3642f162a20b160d8df273e9b27a5fdbf9708bee6ebf6a6c8528047765'
+  version '0.6.5'
+  sha256 'c283e9225a6d0740812029772a55f0033db4e19f5ee0e81735114c9cf1017dee'
 
   # downloads.sourceforge.net/seashore was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/seashore/seashore%20binaries/Seashore%20#{version}/Seashore.zip"
-  appcast 'https://sourceforge.net/projects/seashore/rss?path=/seashore%20binaries'
+  url 'https://github.com/robaho/seashore/releases/download/v0.6.5/seashore-bin-0.6.5.dmg'
+  appcast 'https://github.com/robaho/seashore/releases'
   name 'Seashore'
-  homepage 'https://seashore.sourceforge.io/'
-
-  depends_on macos: '<= :el_capitan'
+  homepage 'https://github.com/robaho/seashore'
 
   app 'Seashore.app'
-
-  caveats do
-    discontinued
-  end
 end
