e230cf6cb4a1efc685e8eaaf2c8877f562aca7f7
Daniel Hoherd <daniel.hoherd@renovo.auto>
2018-11-27 17:41:10 -0800

    Use 'sha256 :no_check' as advised by 'brew cask audit nasas-eyes'

--- Casks/nasas-eyes.rb
+++ Casks/nasas-eyes.rb
@@ -1,6 +1,6 @@
 cask 'nasas-eyes' do
   version :latest
-  sha256 'cb5c7a506895f5ff585252e4d6e0d9722aacfda91db90b15865a172425e4e925'
+  sha256 :no_check
 
   url 'https://eyes.jpl.nasa.gov/eyesproduct/EYES/os/osx'
   name "NASA's Eyes"
