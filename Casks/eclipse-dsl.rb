845ac823da55f9d3bfdf568dbe2cb7cac233044b
OttNorml <ottnorml@gmail.com>
2018-11-28 13:05:27 +0100

    Update eclipse-dsl to 4.8.0,photon:R

--- Casks/eclipse-dsl.rb
+++ Casks/eclipse-dsl.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-dsl' do
-  version '4.7.3a,oxygen:3a'
-  sha256 '0611056f7e0cb26a05ad9ee5a2e4535e6ae47218db894da0ba1c5fc235f17a11'
+  version '4.8.0,photon:R'
+  sha256 '0c2ccd984fc9185c1e85b9c12c291d5ac98377fb808772824c5ec997563183b3'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-dsl-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for Java and DSL Developers'
