30e7640a0e94a16256109a5e8c11a302f75e583b
OttNorml <ottnorml@gmail.com>
2018-11-28 13:51:30 +0100

    Update eclipse-php to 4.8.0,photon:R (#55474)

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,9 +1,9 @@
 cask 'eclipse-php' do
-  version '4.7.3a,oxygen:3a'
-  sha256 '54f5c984ae0e3400ef109c45aed78f38a0dbe33e915f5e3093bea424890de9a2'
+  version '4.8.0,photon:R'
+  sha256 '80c39b287f76bf9728e2b7401b88217cadec80c8f40910786c5d8af8edf7b8a2'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-php-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
-  name 'Eclipse for PHP Developers'
+  name 'Eclipse IDE for PHP Developers'
   homepage 'https://eclipse.org/'
 
   depends_on macos: '>= :leopard'
