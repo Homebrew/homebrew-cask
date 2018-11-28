51a87e70b29adb6efc9f85c54b1f6c163955c0ae
Shoichi Aizawa <shoichiaizawa@gmail.com>
2018-11-28 10:30:24 +0900

    Update vagrant to 2.2.2 (#55469)

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,6 +1,6 @@
 cask 'vagrant' do
-  version '2.2.1'
-  sha256 '84794e12d0037fbd580ff3bc61418c0027dc01ec777a638cae2351c97a758d41'
+  version '2.2.2'
+  sha256 'c3b637e3d648c755b9b30197e820ca64a7062e40b3240bc08c4bfdd98925978b'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
