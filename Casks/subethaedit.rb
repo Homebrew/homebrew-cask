3e12443e9d4b97c21af0af44e8116b55592e49e6
Markus Fömpe <mystygage@users.noreply.github.com>
2018-11-28 20:07:38 +0100

    Add SubEthaEdit.app v5

---
+++ Casks/subethaedit.rb
@@ -0,0 +1,10 @@
+cask 'subethaedit' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://subethaedit.net/SubEthaEdit.zip'
+  name 'SubEthaEdit'
+  homepage 'https://subethaedit.net/'
+
+  app 'SubEthaEdit.app'
+end
