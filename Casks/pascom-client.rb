7cb839756409a2a969dc78f31ac7d9d72c76eed8
Anver Housseini <contact@anver.ch>
2018-11-29 23:18:54 +0100

    Update pascom-client to 44.R66_ff9b83e

--- Casks/pascom-client.rb
+++ Casks/pascom-client.rb
@@ -1,6 +1,6 @@
 cask 'pascom-client' do
-  version '42.R34_99e005c'
-  sha256 '5c5df293b5d6229f1404d1fae083faf141498c6f0159342dc7c4d9b723ee5afd'
+  version '44.R66_ff9b83e'
+  sha256 '6cae51e3a5cd9986fe96d2041d0efd8e6e09a842398564aa7e5178c152863ca8'
 
   url "https://download.pascom.net/release-archive/client/stable/pascom%20Client-#{version}.dmg"
   name 'pascom Client'
