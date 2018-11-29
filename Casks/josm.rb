6e617dc8fc322896cdab2bdb06b65cfbdc149cbc
Andrey Golovin <Andygol@users.noreply.github.com>
2018-11-29 18:59:57 +0200

    josm 14460 (#55504)

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask 'josm' do
-  version '14382'
-  sha256 '8693cb774b345c1e8e278907095f7f4c271eb05d782d89e3b3e38646f005d6f5'
+  version '14460'
+  sha256 '4f7f44b34a91a9c3226062028a08d9c7d53241e18f8a9414480517713ad4a3cf'
 
   url "https://josm.openstreetmap.de/download/macosx/josm-macosx-#{version}.zip"
   name 'JOSM'
