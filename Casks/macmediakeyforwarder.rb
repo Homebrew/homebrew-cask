90bbeb9d02f721314d3a77f2d6a7c30f98f3d3d3
Irvin Lim <irvinlimweiquan@gmail.com>
2018-11-29 12:32:42 +0800

    Update macmediakeyforwarder to 2.1 (#55500)

--- Casks/macmediakeyforwarder.rb
+++ Casks/macmediakeyforwarder.rb
@@ -1,6 +1,6 @@
 cask 'macmediakeyforwarder' do
-  version '2.0'
-  sha256 '3eff3459f07ccecfb0bd1c456bf4d4a281af12550166863599d7230567a8ed31'
+  version '2.1'
+  sha256 'cc9367f440aef6183a1378f5a8e814743a769c8c770b29ed05ba0f00c4084870'
 
   url "http://milgra.com/downloads/mmkf/MacMediaKeyForwarder#{version}.zip"
   name 'Mac Media Key Forwarder'
