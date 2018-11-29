23ed45b6d3e1cf0fa6fe19151a07e52e0a0ea5d0
ralph <ralph@cs.cf.ac.uk>
2018-11-29 09:10:37 +0000

    Update macpilot to 10.15

--- Casks/macpilot.rb
+++ Casks/macpilot.rb
@@ -1,6 +1,6 @@
 cask 'macpilot' do
-  version '10.13'
-  sha256 'aafe33934fb1a5a10c2e7e230b0bdd03865de604ba62dec599ef10920f57f578'
+  version '10.15'
+  sha256 'e2a9034fecb284032a57a1020c7d6402a25fa4c5f3338b08d44c0d53d04ba36a'
 
   url 'http://mirror.koingosw.com/products/macpilot/download/macpilot.dmg'
   appcast 'http://www.koingosw.com/postback/versioncheck.php?appname=macpilot&type=sparkle'
