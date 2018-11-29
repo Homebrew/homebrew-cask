7e3d1024904870cef590b983662d10493f6f9b01
Julian Mayer <julian@corecode.io>
2018-11-29 09:30:16 +0000

    Update chronosync to 4.9.0

--- Casks/chronosync.rb
+++ Casks/chronosync.rb
@@ -1,6 +1,6 @@
 cask 'chronosync' do
-  version '4.8.7'
-  sha256 '04ab013aea545632194c622a0dcd680c8c53e8f5ccee97e42d77bbc6c0214b60'
+  version '4.9.0'
+  sha256 'ce2d909ffaec10f23235b421e67b55c5847d4d10cbc176f136435239f7eb5d1e'
 
   url 'https://downloads.econtechnologies.com/CS4_Download.dmg'
   appcast "https://www.econtechnologies.com/UC/updatecheck.php?prod=ChronoSync&vers=#{version.major_minor}.0&lang=en&plat=mac&os=10.14.1&hw=i64&req=1"
