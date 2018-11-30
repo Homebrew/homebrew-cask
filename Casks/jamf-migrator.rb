a0cc50e32a017492df9aed16230398ced2c9d721
Anver Housseini <contact@anver.ch>
2018-11-30 19:23:26 +0100

    Update jamf-migrator to 3.1.0

--- Casks/jamf-migrator.rb
+++ Casks/jamf-migrator.rb
@@ -1,6 +1,6 @@
 cask 'jamf-migrator' do
-  version '3.0.0'
-  sha256 '27919f9976f5e48aee429210953133f238eae5247176f031228c939d974ddc61'
+  version '3.1.0'
+  sha256 '08ab008555ba5fb50e9b64a253a3064978d1bbc2bd9713e3f3b4719eeb0523c5'
 
   url 'https://github.com/jamfprofessionalservices/JamfMigrator/releases/download/current/jamf-migrator.zip'
   appcast 'https://github.com/jamfprofessionalservices/JamfMigrator/releases.atom'
