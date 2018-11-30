5453c45f0e5723b17199fc27fa21e1685478313d
OttNorml <ottnorml@gmail.com>
2018-11-30 21:23:17 +0100

    Update eclipse-rcp to 4.8.0,photon:R

--- Casks/eclipse-rcp.rb
+++ Casks/eclipse-rcp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-rcp' do
-  version '4.7.3a,oxygen:3a'
-  sha256 '9aa9fa4d3f2e1e1b3776ea999276b82b1ff9179bf499da348ff9c16b9c503e60'
+  version '4.8.0,photon:R'
+  sha256 'e3e6272b40bfed9e24768af432200d51b6dff0c84276a181f9d61bdb3609e54c'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-rcp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for RCP and RAP Developers'
