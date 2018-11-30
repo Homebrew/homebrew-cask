bd6f69a11eaef99628a9bf7e5799b24182d9ad0e
Marius Schuller <marius.schuller@googlemail.com>
2018-11-30 11:45:17 +0100

    Add DriveThruRPG Library App v2.0.3.2

---
+++ Casks/drivethrurpg-library.rb
@@ -0,0 +1,10 @@
+cask 'drivethrurpg-library' do
+  version '2.0.3.2'
+  sha256 'a3cb7400e6f474057ea3045e73eafb63d46c81f130bc37b9fa03f601f21ad9c2'
+
+  url 'https://watermark.drivethrurpg.com/DriveThruRPG-setup-osx.dmg'
+  name 'DriveThruRPG Library App'
+  homepage 'https://www.drivethrurpg.com/library_client.php'
+
+  app 'DriveThruRPG.app'
+end
