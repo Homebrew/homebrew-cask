6171c577e99ab4e0be5b6b606d04a4ca0d53bdd9
Christian Kraemer <christian.kraemer@gmail.com>
2018-11-30 18:51:15 +0100

    Update qownnotes to 18.12.1,b3962-171755

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '18.11.6,b3950-152109'
-  sha256 '92252430bfb18776af16f32ae8161d1ad9a50f36955680da320bfcee22ac8ad3'
+  version '18.12.1,b3962-171755'
+  sha256 'd9d495591ccc6813235b678cc50ba885143989b531fa4ccefdb1a1907f44b508'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
