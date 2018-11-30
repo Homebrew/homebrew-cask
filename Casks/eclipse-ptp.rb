23504c3ff6f7343aa14e7bce96f6d35a6a27de70
OttNorml <ottnorml@gmail.com>
2018-11-30 21:20:31 +0100

    Update eclipse-ptp to 4.8.0,photon:R

--- Casks/eclipse-ptp.rb
+++ Casks/eclipse-ptp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-ptp' do
-  version '4.7.3a,oxygen:3a'
-  sha256 '011254b1d66f6c1e680e0f88d84ba1adb0dcb00ed66277544dd375fe350ad3a9'
+  version '4.8.0,photon:R'
+  sha256 'b40bd1864dc8efaac8cbb92bfdf4fe5b03d0ccf7852e68949fcbbd482e0a418f'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-parallel-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Parallel Application Developers'
