1321784f560e07be315b3fc5fd3779eba348ab40
Randall <ran.dall@icloud.com>
2018-11-30 14:32:32 -0800

    Update ‘managenameext’ sha256 (credit @suschizu)
    As made appearent by @suschizu (https://github.com/Homebrew/homebrew-cask/pull/55199#issuecomment-443353008), this cask does not have a sha256.
    
    Signed-off-by: Randall <ran.dall@icloud.com>

--- Casks/managenameext.rb
+++ Casks/managenameext.rb
@@ -1,6 +1,6 @@
 cask 'managenameext' do
   version '1.6.6'
-  sha256 'edd09e9fcdd197727f3db310aaa768f6b086a9e55fb15a101cec894402fdb44a'
+  sha256 :no_check
 
   url 'http://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_c.zip'
   appcast 'http://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_AffV.html'
