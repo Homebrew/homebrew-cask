d0c86ca46405383b7c148bb2680a8c8a3a38dd2e
Mortennn <34214574+Mortennn@users.noreply.github.com>
2018-11-30 03:00:42 +0100

    Update Dozer v2.2.1 (#55528)

--- Casks/dozer.rb
+++ Casks/dozer.rb
@@ -1,6 +1,6 @@
 cask 'dozer' do
-  version '2.2.0'
-  sha256 '113b52efdd62cc802f5c065477f6aee1ec641c87bf25750abd126d1ad9b71be2'
+  version '2.2.1'
+  sha256 'c4582e2490060ee9eecdc349b88a02eceed56d1f35b1a90f969adf17e63b5d7b'
 
   url "https://github.com/Mortennn/Dozer/releases/download/#{version}/Dozer.#{version}.dmg"
   appcast 'https://github.com/Mortennn/Dozer/releases.atom'
