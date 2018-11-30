0d050c121133294e5c710e3a0322fea7e82c904d
Rick van Galen <DCKcode@users.noreply.github.com>
2018-11-30 14:31:13 +0100

    Clarified release history and moving away from original source

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -2,7 +2,8 @@ cask 'seashore' do
   version '0.6.5'
   sha256 'c283e9225a6d0740812029772a55f0033db4e19f5ee0e81735114c9cf1017dee'
 
-  # downloads.sourceforge.net/seashore was verified as official when first introduced to the cask
+  # The following is a fork of the original Seashore (hosted on SourceForge) based on the original source code
+  # A fork was included in place of the original project since the original project is unmaintained
   url 'https://github.com/robaho/seashore/releases/download/v0.6.5/seashore-bin-0.6.5.dmg'
   appcast 'https://github.com/robaho/seashore/releases'
   name 'Seashore'
