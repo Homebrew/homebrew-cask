fc30e5274d58e6a9632441f3f2e0542912c66466
OttNorml <ottnorml@gmail.com>
2018-12-01 01:57:05 +0100

    Update url of eclipse-platform (#55584)

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -2,9 +2,9 @@ cask 'eclipse-platform' do
   version '4.9,201809060745'
   sha256 '22028f455a9745165c48f4d92045560f8995a5fbd9037d533939dace9b6e1ead'
 
-  url "http://download.eclipse.org/eclipse/downloads/drops#{version.major}/R-#{version.before_comma}-#{version.after_comma}/eclipse-SDK-#{version.before_comma}-macosx-cocoa-x86_64.dmg"
+  url "https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.major}/R-#{version.before_comma}-#{version.after_comma}/eclipse-SDK-#{version.before_comma}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse SDK'
-  homepage 'https://eclipse.org/eclipse/'
+  homepage 'https://eclipse.org/'
 
   depends_on macos: '>= :leopard'
 
