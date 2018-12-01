7148dbc78aef174c365e47e98f278ad7244ddb3f
Julian Mayer <julian@corecode.io>
2018-12-01 15:11:50 +0100

    Update hexels to 3.1.4

--- Casks/hexels.rb
+++ Casks/hexels.rb
@@ -1,6 +1,6 @@
 cask 'hexels' do
-  version '3.1.3'
-  sha256 'c66a202cb5953dc653fa396966f641da757ce66f010c3f2f6a0c67871331ee38'
+  version '3.1.4'
+  sha256 'b2e180a6b6d2606055ca0868d3e4af9228b87e34af3f5e340a2d9b4178925c7a'
 
   # s3.amazonaws.com/mset/download/release was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/mset/download/release/hexels_install_#{version.no_dots}.dmg"
