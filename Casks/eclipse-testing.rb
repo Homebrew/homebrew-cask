035d265eda1811dff313c5d0288437272f6b252b
Loïc PÉRON <loic.peron@gmail.com>
2018-12-02 13:28:21 +0100

    Update eclipse-testing to 4.9.0,2018-09:R

--- Casks/eclipse-testing.rb
+++ Casks/eclipse-testing.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-testing' do
-  version '4.8.0,photon:R'
-  sha256 '7b59ed632bccd244eb69e220b10bb21fc00a9bbdee36a1f97dcf329fd87c86bd'
+  version '4.9.0,2018-09:R'
+  sha256 '7d8dedcbb70b269a8aac25b2903ee4d9f5f1c9a3ebafe0dc1080e88b2b6a8507'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-testing-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-testing-#{version.after_comma.before_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Testers'
   homepage 'https://eclipse.org/'
 
