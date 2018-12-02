a05410918478df11a0aaf1f8c60f67806f1c5cf1
Loïc PÉRON <loic.peron@gmail.com>
2018-12-02 13:19:41 +0100

    Update eclipse-ptp to 4.9.0,2018-09:R

--- Casks/eclipse-ptp.rb
+++ Casks/eclipse-ptp.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-ptp' do
-  version '4.8.0,photon:R'
-  sha256 'b40bd1864dc8efaac8cbb92bfdf4fe5b03d0ccf7852e68949fcbbd482e0a418f'
+  version '4.9.0,2018-09:R'
+  sha256 '6849576963fa51a3117012c330316985326c1c0ce224209881e26302c19c7882'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-parallel-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-parallel-#{version.after_comma.before_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Parallel Application Developers'
   homepage 'https://eclipse.org/'
 
