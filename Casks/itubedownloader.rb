2e2b805af80332429d18a38b535963a06bf53188
rjpontefract <rjpontefract@users.noreply.github.com>
2018-12-02 19:30:53 +1300

    Update itubedownloader to 6.4.4 (#55591)

--- Casks/itubedownloader.rb
+++ Casks/itubedownloader.rb
@@ -1,9 +1,9 @@
 cask 'itubedownloader' do
-  version '6.4.0.0,1537833301'
-  sha256 'df7e329a2ab0075bc91c0652f74d079d098dc058103fa71ed89c877824c756e7'
+  version '6.4.4'
+  sha256 '43d2db6224fae735b7070b64aedd6527281cc2dd8f3d6831cbcd3f9dd5ca51b6'
 
   # dl.devmate.com/com.AlphaSoft.iTubeDownloader was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.AlphaSoft.iTubeDownloader/#{version.before_comma.no_dots}/#{version.after_comma}/iTubeDownloader-#{version.before_comma.no_dots}.zip"
+  url 'https://dl.devmate.com/com.AlphaSoft.iTubeDownloader/iTubeDownloader.dmg'
   appcast 'https://updates.devmate.com/com.AlphaSoft.iTubeDownloader.xml'
   name 'iTubeDownloader'
   homepage 'https://alphasoftware.co/'
