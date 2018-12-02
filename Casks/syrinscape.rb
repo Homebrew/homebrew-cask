c571d86058ddf5126255626b317e7b60aff2c0fd
Work <brigitte@corecode.io>
2018-12-02 22:50:55 +0100

    Update syrinscape to 1.3.7-20180730

--- Casks/syrinscape.rb
+++ Casks/syrinscape.rb
@@ -1,9 +1,8 @@
 cask 'syrinscape' do
-  version '1.3.3-20160816'
-  sha256 'a4fbceeb78564e6648311cde5add027e26f814c7e2a5e36ebd3d8656781682c3'
+  version '1.3.7-20180730'
+  sha256 '9f7f441a05578bcfdfd67173c1c9928b60d2d6e2004699ac02e9e182a7df107f'
 
-  # syrinscape-us.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://syrinscape-us.s3.amazonaws.com/files/syrinscape-mac-#{version}.dmg"
+  url "https://syrinscape.com/get-download/syrinscape-mac-#{version}.dmg"
   name 'Syrinscape Fantasy Player'
   homepage 'https://syrinscape.com/'
 
