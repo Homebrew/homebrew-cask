44238a5c969d9b4feb2b280f3952a5165dea5f1d
Loïc PÉRON <loic.peron@gmail.com>
2018-12-02 12:38:28 +0100

    Update eclipse-cpp to 4.9.0,2018-09:R

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-cpp' do
-  version '4.8.0,photon:R'
-  sha256 '5a6c292ae35aca86df874cefcaf682e78209859ebb26aaf88541cb6d42b377cd'
+  version '4.9.0,2018-09:R'
+  sha256 '944edfbaca43f62a4d91bfd4d804633ae3ab70f0d98947a3b2d51ff50959fae9'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-cpp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-cpp-#{version.after_comma.before_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for C/C++ Developers'
   homepage 'https://eclipse.org/'
 
