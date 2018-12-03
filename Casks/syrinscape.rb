4cfb96f17a5bfe0efaf6a67b1f109b9cf16beaf9
suschizu <brigitte@corecode.io>
2018-12-03 10:25:19 +0100

    Update syrinscape.rb

--- Casks/syrinscape.rb
+++ Casks/syrinscape.rb
@@ -2,7 +2,7 @@ cask 'syrinscape' do
   version '1.3.7-20180730'
   sha256 'e5f49aae95968689400c72e03156790f8d49b49585979a230de573044fbbcb05'
 
-  url "https://syrinscape.com/get-download/syrinscape-mac-#{version}.dmg"
+  url "https://syrinscape.com/get-download/syrinscape-mac-#{version}?type=mac&version=#{version}"
   name 'Syrinscape Fantasy Player'
   homepage 'https://syrinscape.com/'
 
