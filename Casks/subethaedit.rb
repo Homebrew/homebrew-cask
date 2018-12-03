61b84cee70505a8ba59400266d05228ff170d79f
Victor Popkov <victor@popkov.me>
2018-12-03 20:11:14 +0200

    Update subethaedit: add appcast

--- Casks/subethaedit.rb
+++ Casks/subethaedit.rb
@@ -3,6 +3,7 @@ cask 'subethaedit' do
   sha256 :no_check
 
   url 'https://subethaedit.net/SubEthaEdit.zip'
+  appcast 'https://github.com/subethaedit/SubEthaEdit/releases.atom'
   name 'SubEthaEdit'
   homepage 'https://subethaedit.net/'
 
