193a78d8672b0d206aefa37b97e769a2e217c85a
Vítor Galvão <info@vitorgalvao.com>
2018-12-03 16:57:01 +0000

    Update macpilot.rb

--- Casks/macpilot.rb
+++ Casks/macpilot.rb
@@ -3,7 +3,7 @@ cask 'macpilot' do
   sha256 'e2a9034fecb284032a57a1020c7d6402a25fa4c5f3338b08d44c0d53d04ba36a'
 
   url 'http://mirror.koingosw.com/products/macpilot/download/macpilot.dmg'
-  appcast 'http://www.koingosw.com/postback/versioncheck.php?appname=macpilot&type=sparkle'
+  appcast 'https://www.koingosw.com/postback/versioncheck.php?appname=macpilot&type=sparkle'
   name 'MacPilot'
   homepage 'https://www.koingosw.com/products/macpilot/'
 
