4774e0b881a13deafd56e24fe7f26d4bb729ef01
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2018-12-03 19:03:54 +0100

    Update eclipse-installer to 4.9.0,2018-09:R (#55612)

--- Casks/eclipse-installer.rb
+++ Casks/eclipse-installer.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-installer' do
-  version '4.8.0,photon:R'
-  sha256 'eed8140f1fb1ba3145470c147f92dac7c50a12351902eefd0139f91474f77ebe'
+  version '4.9.0,2018-09:R'
+  sha256 'bc419431b18846547c6b0c7d19b50e4494ec799e050c4f7461a8234e5e171ac5'
 
   url "https://eclipse.org/downloads/download.php?file=/oomph/epp/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-inst-mac64.tar.gz&r=1"
   name 'Eclipse Installer'
