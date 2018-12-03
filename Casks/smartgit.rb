b4cbcdbf7eadd50a1e712019033585007e694cf0
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-02 16:05:30 -0800

    Update sha256

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,6 +1,6 @@
 cask 'smartgit' do
   version '18.2'
-  sha256 '00889b3af7a88742f7acea9f0aa89f9537364bf3fe952e462361ecef54096471'
+  sha256 'ffff5c5925fdf1d18b38be504be68ced1c832a93e235ffaabd9270f87c73ab8f'
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.syntevo.com/smartgit/changelog.txt'
