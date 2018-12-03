0a0ab37d49e712b3d638c7c9ae1ddd9859aaea1f
Vítor Galvão <info@vitorgalvao.com>
2018-12-03 18:58:02 +0000

    Update arq-cloud-backup.rb

--- Casks/arq-cloud-backup.rb
+++ Casks/arq-cloud-backup.rb
@@ -1,5 +1,5 @@
 cask 'arq-cloud-backup' do
-  version '1.2.1'
+  version '1.2.2'
   sha256 'f027225a30cdf8ae9faf60ddd170967d99614a0d8d8c617ce84ff0c6a319f5e3'
 
   url 'https://www.arqbackup.com/download/arqcloudbackup/ArqCloudBackup.dmg'
