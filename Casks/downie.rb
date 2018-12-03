803f1615d8e44f9a16a686ff3f88a872e2f7e40f
Nick Wind-McJetters <nick@elearningexperts.net>
2018-12-03 00:17:01 -0500

    Update downie to 3.5.3,1916

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.5.2,1914'
-  sha256 '9dd0479baed109f05f80ff61929a2e42d4d1c72ef3cfe40e173fdb2eafa82d42'
+  version '3.5.3,1916'
+  sha256 '034aa1200418d8e771a12c142d7ff5f8384de1deceb961877daf439e48df3796'
 
   url "https://trial.charliemonroe.net/downie/Downie_#{version.major}_#{version.after_comma}.zip"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml"
