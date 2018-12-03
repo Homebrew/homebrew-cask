de29d8b7544af2fcfc1c630990a12e31e2eb41d1
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-02 16:15:23 -0800

    Update URL (credit: @core-code)

--- Casks/hma-pro-vpn.rb
+++ Casks/hma-pro-vpn.rb
@@ -2,8 +2,8 @@ cask 'hma-pro-vpn' do
   version '3.3.0.3'
   sha256 '7ace9037ff1e531dd501633a330d659651b03e9424b73e679577ffb6476b677c'
 
-  # d1llq9kjmh5kle.cloudfront.net/HMA-Pro-VPN-macOS- was verified as official when first introduced to the cask
-  url "https://d1llq9kjmh5kle.cloudfront.net/HMA-Pro-VPN-macOS-#{version}.dmg"
+  # hidemyass.avcdn.net was verified as official when first introduced to the cask
+  url "https://hidemyass.avcdn.net/tools/privax/latest/HMA-Pro-VPN-macOS-#{version}.dmg"
   name 'HMA! Pro VPN'
   name 'HideMyAss! Pro VPN'
   homepage 'https://www.hidemyass.com/index'
