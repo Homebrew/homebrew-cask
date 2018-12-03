acf8289fbd9eb567c75ad73af2eac7d99d15436d
Vítor Galvão <info@vitorgalvao.com>
2018-12-03 17:58:04 +0000

    Update beersmith3.rb

--- Casks/beersmith3.rb
+++ Casks/beersmith3.rb
@@ -2,17 +2,18 @@ cask 'beersmith3' do
   version '3.0.8'
   sha256 'ac9033f214e1f3879acefe37b01698e904ad25d2b7947c7263dd5e97f13567bb'
 
-  # amazonaws.com/beersmith-3 was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/beersmith-3/BeerSmith#{version.dots_to_underscores}.dmg"
-  name 'BeerSmith3'
+  # amazonaws.com/beersmith-#{version.major} was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/beersmith-#{version.major}/BeerSmith#{version.dots_to_underscores}.dmg"
+  appcast 'http://beersmith.com/download-beersmith/'
+  name 'BeerSmith'
   homepage 'https://beersmith.com/'
 
-  app 'BeerSmith3.app'
+  app "BeerSmith#{version.major}.app"
 
   zap trash: [
-               '~/Library/Application Support/BeerSmith3',
-               '~/Library/Caches/BeerSmith-LLC.BeerSmith3',
-               '~/Library/Preferences/BeerSmith-LLC.BeerSmith3.plist',
-               '~/Library/Saved Application State/BeerSmith-LLC.BeerSmith3.savedStat',
+               '~/Library/Application Support/BeerSmith.*',
+               '~/Library/Caches/BeerSmith-LLC.BeerSmith.*',
+               '~/Library/Preferences/BeerSmith-LLC.BeerSmith.*.plist',
+               '~/Library/Saved Application State/BeerSmith-LLC.BeerSmith.*.savedStat',
              ]
 end
