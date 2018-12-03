df922d98790ceb3fe1a1b614e65a31f4a1a57574
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-03 10:28:56 -0800

    Update ondesoft-itunes-converter to 4.5.0 (#55685)

--- Casks/ondesoft-itunes-converter.rb
+++ Casks/ondesoft-itunes-converter.rb
@@ -1,6 +1,6 @@
 cask 'ondesoft-itunes-converter' do
-  version '4.2.0'
-  sha256 'b4cd5f3e72238b29395ad54d3c1373497d4903ca398f1c246af6aeb676ffc86b'
+  version '4.5.0'
+  sha256 '88381273b62108743eba0554a4d36bf483295af35414e1f36ab7887926b43aad'
 
   url 'https://www.ondesoft.com/download/oditunesconverter_mac.dmg'
   appcast 'https://www.ondesoft.com/itunes_converter/release-history.html'
