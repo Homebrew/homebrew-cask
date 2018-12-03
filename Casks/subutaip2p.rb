64d368055cca84f8cc894140a26a2f946bab97db
Randall <ran.dall@icloud.com>
2018-12-03 12:18:40 -0800

    Update subutaip2p to 8.1.0

--- Casks/subutaip2p.rb
+++ Casks/subutaip2p.rb
@@ -1,6 +1,6 @@
 cask 'subutaip2p' do
-  version '8.0.1'
-  sha256 '39a1810c94668e6063ae8048df08120703e0590271905dc653aeaa84226615ad'
+  version '8.1.0'
+  sha256 'b185aa3b623472fd4bd9710acb5280068933c4b070b8d5eede089738296561ff'
 
   # cdn.subutai.io:8338/kurjun/rest/raw was verified as official when first introduced to the cask
   url 'https://bazaar.subutai.io/rest/v1/cdn/raw?name=subutai-p2p.pkg&latest&download'
