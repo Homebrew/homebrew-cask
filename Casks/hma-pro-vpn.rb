2134eabca63da8122581e23629ab921a0fb635f9
Vítor Galvão <info@vitorgalvao.com>
2018-12-03 19:47:19 +0000

    Update hma-pro-vpn.rb

--- Casks/hma-pro-vpn.rb
+++ Casks/hma-pro-vpn.rb
@@ -2,7 +2,7 @@ cask 'hma-pro-vpn' do
   version '3.3.0.3'
   sha256 '7ace9037ff1e531dd501633a330d659651b03e9424b73e679577ffb6476b677c'
 
-  # hidemyass.avcdn.net was verified as official when first introduced to the cask
+  # s-mac-sl.avcdn.net was verified as official when first introduced to the cask
   url 'https://s-mac-sl.avcdn.net/macosx/privax/HMA-Pro-VPN.dmg'
   name 'HMA! Pro VPN'
   name 'HideMyAss! Pro VPN'
