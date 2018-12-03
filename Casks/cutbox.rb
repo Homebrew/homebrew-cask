e07af3824ddbe92f916dfa9cfaf92bfa7ef640fd
Vítor Galvão <info@vitorgalvao.com>
2018-12-03 19:25:32 +0000

    Update cutbox.rb

--- Casks/cutbox.rb
+++ Casks/cutbox.rb
@@ -2,7 +2,7 @@ cask 'cutbox' do
   version '1.3.13'
   sha256 '2703050492ca055e6ca060c658ef18ac70667a3e3dac385118146ecc32293910'
 
-  url 'https://github.com/cutbox/CutBox/releases/download/1.3.13/CutBox.dmg'
+  url "https://github.com/cutbox/CutBox/releases/download/#{version}/CutBox.dmg"
   appcast 'https://github.com/cutbox/CutBox/releases.atom'
   name 'CutBox'
   homepage 'https://github.com/cutbox/CutBox'
