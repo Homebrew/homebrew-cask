39a9e87cd710925ff69f2ee3a11099aebd3723f5
Vítor Galvão <info@vitorgalvao.com>
2018-12-03 19:37:29 +0000

    Update dust3d.rb

--- Casks/dust3d.rb
+++ Casks/dust3d.rb
@@ -1,6 +1,6 @@
 cask 'dust3d' do
   version '1.0.0-beta.10'
-  sha256 'd31b8d2253016d4ac848d2f49ac001a1a549adb37cbea7ad41f5b7a6fa906062'
+  sha256 'ff0ebfbe1e22f30e175b3c46fe6eb680c7d31d5867c0207b55898895ac464548'
 
   # github.com/huxingyi/dust3d was verified as official when first introduced to the cask
   url "https://github.com/huxingyi/dust3d/releases/download/#{version}/dust3d-#{version}.dmg"
