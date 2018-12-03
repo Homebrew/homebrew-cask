08eae107b00b694e1a7dececf9def0c1cd4cd95c
Vítor Galvão <info@vitorgalvao.com>
2018-12-03 17:31:58 +0000

    Update managenameext.rb

--- Casks/managenameext.rb
+++ Casks/managenameext.rb
@@ -1,6 +1,6 @@
 cask 'managenameext' do
   version '1.6.6'
-  sha256 :no_check
+  sha256 :no_check # required as upstream package is updated in-place
 
   url 'http://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_c.zip'
   appcast 'http://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_AffV.html'
