9b7de30f3e96691dab60ac78532e5dcfbfb39809
mystygage <mystygage@users.noreply.github.com>
2018-12-04 23:47:50 +0100

    update subethaedit: use `version` stanza in url

--- Casks/subethaedit.rb
+++ Casks/subethaedit.rb
@@ -2,7 +2,7 @@ cask 'subethaedit' do
   version '5.0'
   sha256 '22c4219873a73602794805ea26d671e914dcf667041ac7508beb2ad5b6cda982'
 
-  url 'https://subethaedit.net/Releases/SubEthaEdit-5.0rc1.zip'
+  url 'https://subethaedit.net/Releases/SubEthaEdit-#{version}.zip'
   appcast 'https://subethaedit.net/appcast.xml'
   name 'SubEthaEdit'
   homepage 'https://subethaedit.net/'
