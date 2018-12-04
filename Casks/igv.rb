ae5409d222786f197dd4b9f533cbf14221778607
Shaun Jackman <sjackman@gmail.com>
2018-12-04 10:45:20 -0800

    igv 2.4.16
    See https://github.com/igvteam/igv/releases/tag/v2.4.16

--- Casks/igv.rb
+++ Casks/igv.rb
@@ -1,6 +1,6 @@
 cask 'igv' do
-  version '2.4.15'
-  sha256 '8329e7f38971e3d3ca22c1979664b75ae12b4a1febc7c94bde8027cfe5843dff'
+  version '2.4.16'
+  sha256 '8ebda73fa77f755b8c223b5c45f7aeabdb044ac09ca2bc698b11578b1cf171ca'
 
   url "https://data.broadinstitute.org/igv/projects/downloads/#{version.major_minor}/IGV_#{version}.app.zip"
   name 'Integrative Genomics Viewer (IGV)'
