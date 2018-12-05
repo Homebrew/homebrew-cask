8335fbc884af76aacd2f85d99ceab471c23be12f
Work <brigitte@corecode.io>
2018-12-05 22:43:18 +0100

    Update protopie to 3.9.1

--- Casks/protopie.rb
+++ Casks/protopie.rb
@@ -2,7 +2,7 @@ cask 'protopie' do
   version '3.9.1'
   sha256 '4cda5fb8ad26b54f23e25a3dc5d8c18520c5391a61f479234b32999fad2d1b10'
 
-  url "https://release.protopie.io/ProtoPie-#{version}.dmg"
+  url "http://release.protopie.io/ProtoPie-#{version}.dmg"
   appcast 'https://www.protopie.io/support/updates/'
   name 'ProtoPie'
   homepage 'https://www.protopie.io/'
