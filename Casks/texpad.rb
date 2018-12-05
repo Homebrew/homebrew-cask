9ea90905a771ad51b0ae37becec0843cc0344d6a
Andrew Dunning <adunning@users.noreply.github.com>
2018-12-05 10:37:46 -0500

    Updated Texpad to v1.8.7

--- Casks/texpad.rb
+++ Casks/texpad.rb
@@ -15,8 +15,8 @@ cask 'texpad' do
     # download.texpadapp.com was verified as official when first introduced to the cask
     url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.before_comma.dots_to_underscores}__#{version.after_comma.before_comma}__#{version.after_comma.after_comma}.dmg"
   else
-    version '1.8.6,420,2e56cc8'
-    sha256 'b61d4dc7fbc59e9d35798a5733875c384ac5804a108dad3cbe734728874a7860'
+    version '1.8.7,447,09b5788'
+    sha256 '8f8601c2ed1d1acdd9e26448b3017ce59bdf288539b0fb9598c0cadda44afde6'
     # download.texpadapp.com was verified as official when first introduced to the cask
     url "https://download.texpadapp.com/apps/osx/updates/Texpad_#{version.before_comma.dots_to_underscores}__#{version.after_comma.before_comma}__#{version.after_comma.after_comma}.dmg"
   end
