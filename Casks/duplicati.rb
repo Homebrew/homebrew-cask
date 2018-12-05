5400ce997f19abec844c48a1c716927af8cf003a
Ted Carnahan <ted@tedcarnahan.com>
2018-12-05 17:11:37 -0600

    Clean up style

--- Casks/duplicati.rb
+++ Casks/duplicati.rb
@@ -1,7 +1,7 @@
 cask 'duplicati' do
   version '2.0.4.5,2018-11-28'
   sha256 '388d0fc6bfefd86d058e277c4152df4fb63c0f61fb7a116160f51f1ed479b0f0'
-  
+
   url "https://updates.duplicati.com/beta/duplicati-#{version.before_comma}_beta_#{version.after_comma}.dmg"
   appcast 'https://www.duplicati.com/download'
   name 'Duplicati'
