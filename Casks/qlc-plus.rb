99c1c8b04204f05eb5d8bad7d483b5651472d3a6
magcho <magcho@users.noreply.github.com>
2018-12-06 02:32:33 +0900

    update qlc-plus v4.12.0 (#55784)

--- Casks/qlc-plus.rb
+++ Casks/qlc-plus.rb
@@ -1,6 +1,6 @@
 cask 'qlc-plus' do
-  version '4.11.2'
-  sha256 '5d8b2c17d7be7f36d5ac18cb5ebfb09c0f57cfa6d0015a71e1ae488cb65b17b0'
+  version '4.12.0'
+  sha256 'caf2d7e13b5adac5fa8943b9fc2da7d73480ecb0003b3e309dd8b1a62e78e25e'
 
   url "https://qlcplus.org/downloads/#{version}/QLC+_#{version}.dmg"
   name 'Q Light Controller+'
