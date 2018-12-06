e606d0b7a7903e8d1d8425bef8a216d26ed5c1b6
Work <brigitte@corecode.io>
2018-12-06 20:06:19 +0100

    Update temperature-monitor to latest

--- Casks/temperature-monitor.rb
+++ Casks/temperature-monitor.rb
@@ -2,8 +2,8 @@ cask 'temperature-monitor' do
   version :latest
   sha256 :no_check
 
-  # ssl.webpack.de/bresink.eu was verified as official when first introduced to the cask
-  url 'https://ssl.webpack.de/bresink.eu/Downloads/TemperatureMonitor.dmg'
+  # bresink.eu was verified as official when first introduced to the cask
+  url 'https://www.bresink.eu/Downloads/TemperatureMonitor.dmg'
   name 'Temperature Monitor'
   homepage 'https://www.bresink.com/osx/LegacyProducts.html'
 
