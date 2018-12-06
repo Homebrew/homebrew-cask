a0803ac9c50277f701b3e217f690c22c4f2eb7c6
Julian Mayer <julian@corecode.io>
2018-12-06 21:41:16 +0100

    Update rawtherapee to 5.4

--- Casks/rawtherapee.rb
+++ Casks/rawtherapee.rb
@@ -3,6 +3,7 @@ cask 'rawtherapee' do
   sha256 'a3f88a3d65332a013c45b01a0f88c37191a6fda8045a6d03d07c54e3438e6315'
 
   url "https://rawtherapee.com/releases_head/mac/RawTherapee_OSX_10.9_64_#{version}.zip"
+  appcast 'https://github.com/Beep6581/RawTherapee/releases.atom'
   name 'RawTherapee'
   homepage 'https://rawtherapee.com/'
 
