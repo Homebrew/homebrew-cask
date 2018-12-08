d75ccf180dd9fe2facffba8d80b6ed0268c1e0ea
MartinForReal <fanshangxiang@gmail.com>
2018-12-09 02:07:33 +0800

    Update winds to 3.1.4 (#55896)
    * Update winds to 3.1.4
    
    * Update winds.rb

--- Casks/winds.rb
+++ Casks/winds.rb
@@ -1,10 +1,10 @@
 cask 'winds' do
-  version '2.1.174'
-  sha256 '53a316dc6a229b290eadc088376925d88f59777f73c8d153f74b282c8a41d906'
+  version '3.1.4'
+  sha256 'ebb116a2a3b7396eff1edfce7d69da31ea6f6d66efc04a98f6c260319eda2ba8'
 
   # s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/winds-#{version.major}.0-releases/releases/Winds-#{version}.dmg"
-  appcast "https://s3.amazonaws.com/winds-#{version.major}.0-releases/latest.html"
+  url "https://s3.amazonaws.com/winds-2.0-releases/releases/Winds-#{version}.dmg"
+  appcast 'https://github.com/GetStream/Winds/releases.atom'
   name 'Winds'
   homepage 'https://getstream.io/winds/'
 
