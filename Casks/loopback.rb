4ed4421e4d3bdfb658ea0992ff1e2282d2e5b850
Vítor Galvão <info@vitorgalvao.com>
2018-12-08 16:48:43 +0000

    Update loopback.rb

--- Casks/loopback.rb
+++ Casks/loopback.rb
@@ -2,7 +2,7 @@ cask 'loopback' do
   version '1.2.1'
   sha256 '8be69661a740a4966ad7048b5ab68a1322ae8a373b4a15f23d1fc02ed44e32b5'
 
-  url 'https://rogueamoeba.com/legacy/downloads/Loopback-121.zip'
+  url "https://rogueamoeba.com/legacy/downloads/Loopback-#{version.no_dots}.zip"
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Loopback&system=10140'
   name 'Loopback'
   homepage 'https://rogueamoeba.com/legacy/'
