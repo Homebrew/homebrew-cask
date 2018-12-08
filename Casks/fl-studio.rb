90a5110352c72c4561022b95b9a06527cfe5c4f5
CoreCode <core-code@users.noreply.github.com>
2018-12-08 18:20:05 +0100

    Update fl-studio to 20.0.5.91 (#55891)
    * Update fl-studio to 20.0.5.91
    
    * Update fl-studio.rb

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -1,8 +1,9 @@
 cask 'fl-studio' do
-  version '20.0.99.123'
+  version '20.0.5.91'
   sha256 '53166826d25940684e0dc66d8217cfa38820f760ea180b3778a73571c6517d30'
 
-  url "https://support.image-line.com/redirect/flstudio#{version.major}_mac_installer"
+  url "http://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
+  appcast 'https://support.image-line.com/api.php?call=get_version_info&callback=il_get_version_info_cb&prod_type=undefined'
   name 'FL Studio'
   homepage 'https://www.image-line.com/flstudio/'
 
