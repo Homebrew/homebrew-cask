2b1b0c89ed398f64a7bbcf9a697ac98f487de0e1
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2018-12-08 18:39:31 +0300

    Update Loopback URL and homepage
    Due the Loopback2 release, app undergo legacy position then URL changed

--- Casks/loopback.rb
+++ Casks/loopback.rb
@@ -2,10 +2,10 @@ cask 'loopback' do
   version '1.2.1'
   sha256 '8be69661a740a4966ad7048b5ab68a1322ae8a373b4a15f23d1fc02ed44e32b5'
 
-  url 'https://rogueamoeba.com/loopback/download/Loopback.zip'
+  url 'https://rogueamoeba.com/legacy/downloads/Loopback-121.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Loopback&system=10140'
   name 'Loopback'
-  homepage 'https://rogueamoeba.com/loopback/'
+  homepage 'https://rogueamoeba.com/legacy/'
 
   app 'Loopback.app'
 end
