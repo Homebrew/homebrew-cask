7ca54afde3ffe07c070b068e532523cc25a7b5a8
Vítor Galvão <info@vitorgalvao.com>
2018-12-08 17:12:13 +0000

    Update loopback.rb

--- Casks/loopback.rb
+++ Casks/loopback.rb
@@ -4,7 +4,7 @@ cask 'loopback' do
 
   # d2oxtzozd38ts8.cloudfront.net/loopback was verified as official when first introduced to the cask
   url 'https://d2oxtzozd38ts8.cloudfront.net/loopback/download/Loopback.zip'
-  appcast 'https://rogueamoeba.com/loopback/releasenotes.php'
+  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.loopback&system=10141&platform=osx&arch=x86_64&version=2008000'
   name 'Loopback'
   homepage 'https://rogueamoeba.com/loopback/'
 
