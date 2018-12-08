9c20bfeaf4e2493038542db4f6afc2839b203015
Vítor Galvão <info@vitorgalvao.com>
2018-12-08 17:55:01 +0000

    Update loopback to 2.0.0 (#55892)
    * Update loopback to 2.0.0
    
    * Update loopback.rb

--- Casks/loopback.rb
+++ Casks/loopback.rb
@@ -1,9 +1,10 @@
 cask 'loopback' do
-  version '1.2.1'
-  sha256 '8be69661a740a4966ad7048b5ab68a1322ae8a373b4a15f23d1fc02ed44e32b5'
+  version '2.0.0'
+  sha256 'fcfe14e2fa66535cd4483fc05846d6535f4819f7ac9f84194abd579f3c5f1251'
 
-  url 'https://rogueamoeba.com/loopback/download/Loopback.zip'
-  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Loopback&system=10140'
+  # d2oxtzozd38ts8.cloudfront.net/loopback was verified as official when first introduced to the cask
+  url 'https://d2oxtzozd38ts8.cloudfront.net/loopback/download/Loopback.zip'
+  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.loopback&system=10141&platform=osx&arch=x86_64&version=2008000'
   name 'Loopback'
   homepage 'https://rogueamoeba.com/loopback/'
 
