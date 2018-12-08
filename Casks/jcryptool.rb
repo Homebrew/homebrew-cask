139d5ad69228e8adf13579c23b3c6c5d972b3431
Vítor Galvão <info@vitorgalvao.com>
2018-12-08 13:39:52 +0000

    Update jcryptool.rb

--- Casks/jcryptool.rb
+++ Casks/jcryptool.rb
@@ -2,11 +2,9 @@ cask 'jcryptool' do
   version '0.9.9'
   sha256 '6614bbfb2d9b3c6df365cdcff2a6b18c47f006b6fdd6783eb5e4faadabcb18e4'
 
-  url 'https://www.cryptool.org/jctdownload/Builds/downloads/stable/jcryptool-0.9.9-macosx.cocoa.x86_64.tar.gz'
+  url "https://www.cryptool.org/jctdownload/Builds/downloads/stable/jcryptool-#{version}-macosx.cocoa.x86_64.tar.gz"
   name 'JCrypTool'
   homepage 'https://www.cryptool.org/en/jcryptool'
-
-  depends_on arch: :x86_64
 
   app 'jcryptool.app'
 
