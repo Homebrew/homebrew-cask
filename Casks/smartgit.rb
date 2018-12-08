32282e696f902e0b9818b14e6f1e6520d559545f
Ralph Martin <ralph@cs.cf.ac.uk>
2018-12-08 07:52:06 +0000

    Update smartgit to 18.2.1 (#55869)

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,6 +1,6 @@
 cask 'smartgit' do
-  version '18.2.0'
-  sha256 '1935e886cb4a18bc1d6925d18daecc0940855b15f24c0010e7d12bb79a8e77e3'
+  version '18.2.1'
+  sha256 '9a6060e1503e6149ad9f4eed0005b7ec187a8e379578fb7579e1578f42ce0473'
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.syntevo.com/smartgit/changelog.txt'
