3c3e416e0b7aaa406ed7b79ac63eb88969301dc2
Randall <ran.dall@icloud.com>
2018-12-08 09:50:52 -0800

    Update iridient-developer to 3.3.5

--- Casks/iridient-developer.rb
+++ Casks/iridient-developer.rb
@@ -1,6 +1,6 @@
 cask 'iridient-developer' do
-  version '3.3.4'
-  sha256 'ae7a279be57761dc14f0eb742e48de237241f0080f81ad5f8e61c4965aca9f6c'
+  version '3.3.5'
+  sha256 '6d7002298604c4e547b9ffc053d22c9911c16eeb031bca574535bd76b756ac6a'
 
   url "https://www.iridientdigital.com/downloads/IridientDeveloper_#{version.no_dots}.dmg"
   appcast 'https://www.iridientdigital.com/products/rawdeveloper_history.html'
