c74ea2385a24bfd73c9c6a34c026b3ba86e73913
Vítor Galvão <info@vitorgalvao.com>
2018-12-09 19:25:40 +0000

    Update squeak.rb

--- Casks/squeak.rb
+++ Casks/squeak.rb
@@ -2,7 +2,7 @@ cask 'squeak' do
   version '5.2-18225'
   sha256 '377d9c1354fbf20485f82acd3771c46c3f7235285ba8d231beaff9c81f03a539'
 
-  url "http://files.squeak.org/#{version.major_minor}/Squeak#{version}-64bit/Squeak#{version}-64bit-All-in-One.zip"
+  url "https://files.squeak.org/#{version.major_minor}/Squeak#{version}-64bit/Squeak#{version}-64bit-All-in-One.zip"
   name 'Squeak'
   homepage 'https://squeak.org/'
 
