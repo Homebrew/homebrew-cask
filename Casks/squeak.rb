d46f717759b5965f483c3cee501d676821d4e08d
Ralph Martin <ralph@cs.cf.ac.uk>
2018-12-09 10:44:13 +0800

    Update squeak.rb
    Move from 5.1 to 5.2, but also move from 32 bit version to 64 bit version for future compatibility

--- Casks/squeak.rb
+++ Casks/squeak.rb
@@ -1,12 +1,12 @@
 cask 'squeak' do
-  version '5.1-16549'
-  sha256 '42cfa72371a86e95c18dda29c2cbd41d48c71285e01a8e2e7e88e2d09e2962a2'
+  version '5.2-18225'
+  sha256 'ade03315db7c9ed229c9b8b0b0daf68f16dbaee95817db09bbdc1cdb4df2643f'
 
-  url "https://files.squeak.org/#{version.major_minor}/Squeak#{version}-32bit/Squeak#{version}-32bit-All-in-One.zip"
+  url "https://files.squeak.org/#{version.major_minor}/Squeak#{version}-64bit/Squeak#{version}-64bit-All-in-One.zip"
   name 'Squeak'
   homepage 'https://squeak.org/'
 
-  app "Squeak#{version}-32bit-All-in-One.app"
+  app "Squeak#{version}-64bit-All-in-One.app"
 
-  zap trash: "~/Library/Saved Application State/org.squeak.Squeak#{version.major_minor}.32.All-in-One.savedState"
+  zap trash: "~/Library/Saved Application State/org.squeak.Squeak#{version.major_minor}.64.All-in-One.savedState"
 end
