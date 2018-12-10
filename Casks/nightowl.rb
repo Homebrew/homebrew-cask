521457c351f18241157c702712c16326361f065d
Toni Förster <toni.foerster@gmail.com>
2018-12-10 20:17:52 +0100

    nightowl website does not provide versioned download links

--- Casks/nightowl.rb
+++ Casks/nightowl.rb
@@ -1,6 +1,6 @@
 cask 'nightowl' do
-  version '0.2.7'
-  sha256 '061610e404ae5ca95cf15058b70d49ef1649a9daf5c249a7cde8e281e524051d'
+  version :latest
+  sha256 :no_check
 
   url 'https://nightowl.kramser.xyz/files/NightOwl.dmg'
   appcast 'https://nightowl.kramser.xyz/'
