57bdb652418e305e9c1c5b9ef17bb662025c6211
Vítor Galvão <info@vitorgalvao.com>
2018-12-10 14:26:13 +0000

    Update ondesoft-audiobook-converter.rb

--- Casks/ondesoft-audiobook-converter.rb
+++ Casks/ondesoft-audiobook-converter.rb
@@ -1,6 +1,6 @@
 cask 'ondesoft-audiobook-converter' do
-  version '3.2.0'
-  sha256 '6cfaaba5d77030fefc5675c89d8282705d7469c3e5972a2a60c316e5efe7c7ae'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.ondesoft.com/download/odaudiobookconverter_mac.dmg'
   name 'OndeSoft Audible Audiobook Converter'
