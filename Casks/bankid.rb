6048bcd1b2e0a58ba3a25d31b2ab03e44a3e0604
Randall <ran.dall@icloud.com>
2018-12-09 16:38:58 -0800

    Update bankid to 7.7.0

--- Casks/bankid.rb
+++ Casks/bankid.rb
@@ -1,6 +1,6 @@
 cask 'bankid' do
-  version '7.6.0'
-  sha256 '5201f989ddf3f1f2386872c46de5c9809208fc6211330e6195aa02e3bc7736fe'
+  version '7.7.0'
+  sha256 '53e7f59049d32e541c6bdd584f88211f89ed3fb82430f4ce1bf7f5a6ab6bed65'
 
   url "https://install.bankid.com/Repository/BankID_installation_#{version.dots_to_underscores}.pkg"
   name 'BankID'
