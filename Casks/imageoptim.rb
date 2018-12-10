78a934044bb8f8b0fee91a2b5641bdb57594d8f9
Leon Klingele <git@leonklingele.de>
2018-12-10 17:45:57 +0100

    imageoptim 1.8.6

--- Casks/imageoptim.rb
+++ Casks/imageoptim.rb
@@ -1,6 +1,6 @@
 cask 'imageoptim' do
-  version '1.8.5'
-  sha256 '5ed912d4e5f726ea7a552c8f294c95c61cd9f6f8842239f65866fba5ca010f03'
+  version '1.8.6'
+  sha256 'c7239f4142c515c2e481cd0e8f74af7107f59ea5bd622025637f197236df981b'
 
   url "https://imageoptim.com/ImageOptim#{version}.tar.bz2"
   appcast 'https://imageoptim.com/appcast.xml'
