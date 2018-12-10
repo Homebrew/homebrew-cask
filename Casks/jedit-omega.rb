8e78fb522f7568c4a3cd48ccdab28017bde84ab2
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-10 10:53:49 -0800

    Update jedit-omega to 2.05 (#55950)

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -3,8 +3,8 @@ cask 'jedit-omega' do
     version '1.32'
     sha256 '251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9'
   else
-    version '2.04'
-    sha256 '73b0c1d9cfaae17f6027d6c78e602ba67a0eb7f1df0bebccdfef8b9af818f565'
+    version '2.05'
+    sha256 '252d705ce774c5d5d2beb34a814acea6c17ad9c3df6fe21636c9f210ed66bff8'
   end
 
   # artman21.co.jp was verified as official when first introduced to the cask
