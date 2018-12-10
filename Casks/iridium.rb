5a93fdb647f72a7d8ac792ea66d8d0a067dd5112
suschizu <brigitte@corecode.io>
2018-12-10 14:26:21 +0100

    Update iridium.rb
    Would it be possible to keep the bundle version in the version-stanza?

--- Casks/iridium.rb
+++ Casks/iridium.rb
@@ -1,8 +1,8 @@
 cask 'iridium' do
-  version '2018.11-1'
+  version '2018.11.71.0,2018.11-1'
   sha256 'd39b798d9819d0192611ffd9a89fdc67306a05a34e9838ae02fbc4b67953483e'
 
-  url "https://downloads.iridiumbrowser.de/macos/#{version}/iridium-browser_#{version}_macos_x64.dmg"
+  url "https://downloads.iridiumbrowser.de/macos/#{version.after_comma}/iridium-browser_#{version.after_comma}_macos_x64.dmg"
   appcast 'https://downloads.iridiumbrowser.de/macos/'
   name 'Iridium Browser'
   homepage 'https://iridiumbrowser.de/'
