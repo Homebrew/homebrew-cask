1bb1ad65371a8e32551e596c99c0600b9172573e
Randall <ran.dall@icloud.com>
2018-12-10 10:26:58 -0800

    Update milanote to 1.0.59

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -1,6 +1,6 @@
 cask 'milanote' do
-  version '1.0.44'
-  sha256 '9acd39a242713e7551e0cf5479754a98282ed20ac79e59549bd262b3585004dd'
+  version '1.0.59'
+  sha256 'c0e27cdd1f47bf3e43491c3d1cd61e53376771dc473475a4aa2b82fc47f1fe34'
 
   # milanote-app-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg"
