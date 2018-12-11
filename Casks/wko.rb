d3d7a248ae34581ba75bf7918c3e390a544b4314
Vítor Galvão <info@vitorgalvao.com>
2018-12-11 01:15:31 +0000

    Update wko.rb

--- Casks/wko.rb
+++ Casks/wko.rb
@@ -3,7 +3,7 @@ cask 'wko' do
   sha256 '74107f34bda30acd2cd0d0a148558e42ae5539b83b6bd6fd0b25bd292d578657'
 
   # wko4.com was verified as official when first introduced to the cask
-  url "http://updates.wko4.com/wko4release/WKO4_OSX_#{version}.dmg"
+  url "https://updates.wko4.com/wko4release/WKO4_OSX_#{version}.dmg"
   appcast 'https://updates.wko4.com/wko4release/wko4_osx_appcast.xml'
   name 'WKO'
   homepage 'https://www.trainingpeaks.com/wko4/'
