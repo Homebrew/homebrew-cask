b93914dd696b3ab00e6c7ff1614b119eb83cecd6
Randall <ran.dall@icloud.com>
2018-12-11 12:12:18 -0800

    Update terminus to 1.0.65

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -1,6 +1,6 @@
 cask 'terminus' do
-  version '1.0.1'
-  sha256 'f59d12c713ffc7979175298f1e36d0eb21c2d2a537e8ff95f849d031478034c5'
+  version '1.0.65'
+  sha256 '52eb9c724e2f1523f19920286e187007c5657a256fc7a1586ed326629174b83f'
 
   # github.com/Eugeny/terminus was verified as official when first introduced to the cask
   url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos.dmg"
