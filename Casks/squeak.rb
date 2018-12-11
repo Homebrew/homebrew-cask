950b4a1b23e1a7abcadd163a309eaa6d1bdb6f01
suschizu <brigitte@corecode.io>
2018-12-11 16:16:25 +0100

    Update squeak to 5.2-18228 (#55976)

--- Casks/squeak.rb
+++ Casks/squeak.rb
@@ -1,6 +1,6 @@
 cask 'squeak' do
-  version '5.2-18225'
-  sha256 '377d9c1354fbf20485f82acd3771c46c3f7235285ba8d231beaff9c81f03a539'
+  version '5.2-18228'
+  sha256 '7bd3de9beedcce792db32f1253f9243a299909368bad6f1ac21dd128f11b289d'
 
   url "https://files.squeak.org/#{version.major_minor}/Squeak#{version}-64bit/Squeak#{version}-64bit-All-in-One.zip"
   name 'Squeak'
