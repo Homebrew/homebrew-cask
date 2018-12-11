921b82eb28d5564fbb196b20cc844d4a6f8440a3
Ralph Martin <ralph@cs.cf.ac.uk>
2018-12-11 23:17:40 +0800

    Update macspice to 3.1.21 (#55966)

--- Casks/macspice.rb
+++ Casks/macspice.rb
@@ -1,6 +1,6 @@
 cask 'macspice' do
-  version '3.1.20'
-  sha256 'eb5006a4f3963fa934698adc9591db26f63c908c93f32bd1514a5bd2f8ab2430'
+  version '3.1.21'
+  sha256 '17ac2d9dca93c6da5caa4894abf42a8a4c04aae65aa36855ac842f42f204c2b2'
 
   url "https://www.macspice.com/mirror/binaries/v#{version}/MacSpice3f5.dmg"
   appcast 'https://www.macspice.com/AppCast-v2.xml'
