d97972eef8de4f4354d2fb90f9c21aab713b23a1
Stephane Paquet <spaquet@up4b.com>
2018-12-11 06:51:06 -0800

    Update electron to 3.0.11

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '3.0.10'
-  sha256 '302d8fc4fe9bc5d84dd96534139e4f1a3989d0ab6aac0c91351c57576e79e0a9'
+  version '3.0.11'
+  sha256 '95c774bbe3d082ed60f8156837254d016c67d75fdc7f0b8c813b2fb066db9994'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
