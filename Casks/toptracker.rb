797a162e780cb100da001279c81a66396c4651fb
Vítor Galvão <info@vitorgalvao.com>
2018-12-12 14:37:14 +0000

    Update toptracker.rb

--- Casks/toptracker.rb
+++ Casks/toptracker.rb
@@ -1,8 +1,10 @@
 cask 'toptracker' do
-  version :latest
-  sha256 :no_check
+  version '1.5.7.6079'
+  sha256 '1f7c116da6b3f6ad1c7584b84daf28329e9fe158dd0251063d9401db7d8bd5d4'
 
-  url 'https://tracker-api.toptal.com/desktop/latest/mac'
+  # d101nvfmxunqnl.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d101nvfmxunqnl.cloudfront.net/desktop/builds/mac/toptracker_#{version}.dmg"
+  appcast 'https://tracker.toptal.com/tracker/'
   name 'TopTracker'
   homepage 'https://tracker.toptal.com/tracker/'
 
