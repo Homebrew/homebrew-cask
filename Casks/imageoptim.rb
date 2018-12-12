1465379a43cb31d3692cbc2127a1f0635cd34b5a
Oleg <epushiron+github@gmail.com>
2018-12-12 18:16:51 +0500

    Update imageoptim to 1.8.7 (#56025)

--- Casks/imageoptim.rb
+++ Casks/imageoptim.rb
@@ -1,6 +1,6 @@
 cask 'imageoptim' do
-  version '1.8.6'
-  sha256 'c7239f4142c515c2e481cd0e8f74af7107f59ea5bd622025637f197236df981b'
+  version '1.8.7'
+  sha256 '94654a8f3166ccb0aebd5f863f44d8be609c996936812f3d30c4b5d495e3df06'
 
   url "https://imageoptim.com/ImageOptim#{version}.tar.bz2"
   appcast 'https://imageoptim.com/appcast.xml'
