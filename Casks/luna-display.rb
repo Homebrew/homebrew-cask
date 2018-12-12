f8c4da9e28c03c67b3308b9da06e8a3e48dc118b
Bruce Giovando <179564+bgiovand@users.noreply.github.com>
2018-12-12 12:37:45 -0800

    Add Luna Display
    Add Luna Display (https://lunadisplay.com)
    v1.1.2

---
+++ Casks/luna-display.rb
@@ -0,0 +1,22 @@
+cask 'luna-display' do
+  version '1.1.2'
+  sha256 '4bdd3494b3d8544eeff816445dba17a9a08da3c731de5827d243ab12beb3c22f'
+
+  url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaDisplay-#{version}.zip"
+  appcast 'https://s3.amazonaws.com/lunadisplay.com/downloads/sparkle.xml'
+  name 'LunaDisplay'
+  homepage 'https://lunadisplay.com'
+
+  depends_on macos: '>= :el_capitan'
+
+  app 'Luna Display.app'
+
+  uninstall quit: 'com.astro-hq.LunaDisplayMac'
+
+  zap trash: [
+    '~/Library/Caches/com.astro-hq.LunaDisplayMac',
+    '~/Library/Preferences/com.astro-hq.LunaDisplayMac.plist',
+    '~/Library/Saved Application State/com.astro-hq.LunaDisplayMac.savedState',
+  ]
+
+end
