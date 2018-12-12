1b9b9d40276c47e843628a5d53b209af34788c7c
Randall <ran.dall@icloud.com>
2018-12-12 11:25:39 -0800

    Update pocket-casts to 1.0

--- Casks/pocket-casts.rb
+++ Casks/pocket-casts.rb
@@ -1,6 +1,6 @@
 cask 'pocket-casts' do
-  version '0.9.9'
-  sha256 '5febb76c5ad67167c5fc73b461806e763c34ae1219368f54cf1e25519b07132b'
+  version '1.0'
+  sha256 '796211e2821081a0051ca5a1f1e783b98f40631a795fb52a80adf2dbf7ef71c8'
 
   url 'https://static.pocketcasts.com/mac/PocketCasts.zip'
   appcast 'https://static2.pocketcasts.com/mac/appcast.xml'
