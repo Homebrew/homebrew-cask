a62bfd65b10809ce7c32c17cdf2f899f6c6dc3ba
Bruce Giovando <179564+bgiovand@users.noreply.github.com>
2018-12-12 14:30:01 -0800

    fixed style errors

--- Casks/luna-display.rb
+++ Casks/luna-display.rb
@@ -2,7 +2,7 @@ cask 'luna-display' do
   version '1.1.2'
   sha256 '4bdd3494b3d8544eeff816445dba17a9a08da3c731de5827d243ab12beb3c22f'
 
-  # s3.amazonaws.com/s3.lunadisplay.com was verified as official when first introduced to the cask
+  # s3.amazonaws.com/s3.lunadisplay was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/s3.lunadisplay.com/downloads/LunaDisplay-#{version}.zip"
   appcast 'https://s3.amazonaws.com/lunadisplay.com/downloads/sparkle.xml'
   name 'LunaDisplay'
