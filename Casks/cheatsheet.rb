ce307fd7ff4a4ce66710bb1c60db5071839eccc6
Randall <ran.dall@icloud.com>
2018-12-12 11:33:48 -0800

    Update cheatsheet to 1.3.0.3

--- Casks/cheatsheet.rb
+++ Casks/cheatsheet.rb
@@ -1,6 +1,6 @@
 cask 'cheatsheet' do
-  version '1.3'
-  sha256 'fd93d3950b4aa4b982b60890ade4d80f52d1984b0fe38b08579c82339a8a0fe3'
+  version '1.3.0.3'
+  sha256 'ea768d73f9b5fd679c3a3612ae3301dbe96b5f1db07c170c7f213b7e24957904'
 
   # mediaatelier.com/CheatSheet was verified as official when first introduced to the cask
   url "https://mediaatelier.com/CheatSheet/CheatSheet_#{version}.zip"
