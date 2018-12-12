7511362782e5c285520d9b3723059810a55e7f84
Vítor Galvão <info@vitorgalvao.com>
2018-12-12 13:18:12 +0000

    Update rightfont.rb

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -3,11 +3,11 @@ cask 'rightfont' do
   sha256 '203a9bb92c6e07e028bdeeab6758a9e05bb0bcd428b2b9c61d9ca8d3ee09e948'
 
   url 'https://rightfontapp.com/update/rightfont.zip'
-  appcast 'https://rightfontapp.com/update/appcast5.xml'
+  appcast "https://rightfontapp.com/update/appcast#{version.major}.xml"
   name 'RightFont'
   homepage 'https://rightfontapp.com/'
 
   depends_on macos: '>= :yosemite'
 
-  app 'RightFont 5.app'
+  app "RightFont #{version.major}.app"
 end
