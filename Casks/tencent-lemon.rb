87c62b8b56a7213bac84810c5b2303bdcca96a54
laiqurufeng <3124692@163.com>
2018-12-12 22:30:39 +0800

    Tencent lemon cleaner (#56016)
    * Add Tencent Lemon Cleaner v1.0.5
    
    * Update and rename tencent-lemon-cleaner.rb to tencent-lemon.rb
    
    * Update tencent-lemon.rb
    
    * Upgrade Tencent Lemon v1.0.6
    
    * Upgrade Tencent Lemon v1.0.6
    
    * Update tencent-lemon.rb

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,9 +1,9 @@
 cask 'tencent-lemon' do
-  version '1.0.5,1543925356.0367'
-  sha256 '2e57befca7a6c8070f0d66b0766321c18ca2d6e0c928c85514be52e514370322'
+  version '1.0.6'
+  sha256 'edc9e91586b97df2fc2fd5f0928fdc41476732ccd7488fc2765f8536142d0c3f'
 
-  # pm.myapp.com was verified as official when first introduced to the cask
-  url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/#{version.after_comma}.Lemon.#{version.before_comma}.dmg"
+  # pm.myapp.com/invc/xfspeed/qqpcmgr was verified as official when first introduced to the cask
+  url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon_#{version}.dmg"
   name 'Tencent Lemon Cleaner'
   homepage 'https://mac.gj.qq.com/'
 
