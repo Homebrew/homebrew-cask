291d922f9a96bdbd0cbfa21fadfc2302f2840838
Sergio Ortega Sanchez <ortega@tune-is.de>
2018-12-12 14:17:45 +0100

    Update plex-media-player (#56017)

--- Casks/plex-media-player.rb
+++ Casks/plex-media-player.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-player' do
-  version '2.23.0.920-5bc1a2e5'
-  sha256 'de870ec013d95ca09834bb76d8102bf2b873b7e05d0c6102728f4e2560f0424e'
+  version '2.24.0.924-63fcaa8e'
+  sha256 '516fe7a9f97f8539a9f31893213f9570b1b89a587beac538310c2242e321549f'
 
   url "https://downloads.plex.tv/plexmediaplayer/#{version}/PlexMediaPlayer-#{version}-macosx-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/3.json'
