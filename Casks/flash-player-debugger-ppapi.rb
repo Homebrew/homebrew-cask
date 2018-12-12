140ad28beb85b1baad5bce02063024e9e343d971
Maxim Gavrilov <maxim.gavrilov@gmail.com>
2018-12-12 13:56:04 +0300

    Update flash-player-debugger-ppapi to 32.0.0.101

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger-ppapi' do
-  version '31.0.0.153'
-  sha256 '0d5180d7cc5fb6cffde04e0c6cbbef5281d3872be391bd47bf42a2744eb95c36'
+  version '32.0.0.101'
+  sha256 '9506a7a72a9297ea95e48a31ce5f19801e6003fa67c3bfdd3fca7c86fdce506e'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml'
