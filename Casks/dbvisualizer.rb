dfde5442fe10f5504b089bf1bbca7f0922d2aa82
Steve Mao <maochenyan@gmail.com>
2018-12-13 10:25:35 +1100

    update dbvisualizer download link

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -1,8 +1,8 @@
 cask 'dbvisualizer' do
-  version '10.0.15'
-  sha256 '1d9db45398f7e730ee9a53df793f5cef9d49dbb94827fc26d71490cd3cdc3fd3'
+  version '10.0.16'
+  sha256 '5f4ffa463d45200fd0683c2c18f936a9ba50c9d0df23c84f88bd14eefc037b17'
 
-  url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.dots_to_underscores}.dmg"
+  url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.dots_to_underscores}_jre.dmg"
   name 'DbVisualizer'
   homepage 'https://www.dbvis.com/'
 
