dfd4131150c12f944a93a446111657b99eea0cee
Randall <ran.dall@icloud.com>
2018-12-13 08:31:44 -0800

    Update latexit to 2.12.0

--- Casks/latexit.rb
+++ Casks/latexit.rb
@@ -1,6 +1,6 @@
 cask 'latexit' do
-  version '2.11.1'
-  sha256 '54829537c78778fdb07776a595659b7abd049551704bdf3b46ca7a83519c9335'
+  version '2.12.0'
+  sha256 '6d0fabea227d9c8447bd43c5c22589960c739aacac19168fd443c5d73a8a0fd6'
 
   url "https://www.chachatelier.fr/latexit/downloads/LaTeXiT-#{version.dots_to_underscores}.dmg",
       user_agent: :fake
