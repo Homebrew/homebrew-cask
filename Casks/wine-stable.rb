432b572712e202b88227711390632d1e20da1baf
Yurii Kolesnykov <root@yurikoles.com>
2018-12-13 16:29:58 +0200

    Update wine-stable to 3.0.4 (#56071)

--- Casks/wine-stable.rb
+++ Casks/wine-stable.rb
@@ -1,6 +1,6 @@
 cask 'wine-stable' do
-  version '3.0.3'
-  sha256 'f0dc85a61de75defcc5e7d0091b55ca3178abd71a5d45152228fe77575377490'
+  version '3.0.4'
+  sha256 'a13c4755d837439a0861472885eea0956b39a5eafaf0be674784764691da19ff'
 
   url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-stable-#{version}.pkg"
   appcast 'https://dl.winehq.org/wine-builds/macosx/download.html'
