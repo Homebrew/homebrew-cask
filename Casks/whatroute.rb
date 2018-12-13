3ccebe5d14e0ee79d106779c927eb2f25ca43286
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-13 08:55:11 -0800

    Update whatroute to 2.1.11 (#56100)

--- Casks/whatroute.rb
+++ Casks/whatroute.rb
@@ -1,6 +1,6 @@
 cask 'whatroute' do
-  version '2.1.8'
-  sha256 'b7cf2be3a4704f9cfea3342451989fdaa8a211841ca015bbfbae1cae279edc80'
+  version '2.1.11'
+  sha256 '2e12624708d1267da0f960b5cddb369dfb46681a12c4bf36f4929287f1c67d97'
 
   url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
   appcast "https://www.whatroute.net/whatroute#{version.major}appcast.xml"
