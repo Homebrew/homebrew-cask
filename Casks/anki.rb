0f4ca61be9a785f173e8d498d0a8f77cc97f623a
Klemen Košir <klemen.kosir@kream.io>
2018-12-13 23:29:14 +0900

    Update anki to 2.1.6 (#56067)

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,13 +1,13 @@
 cask 'anki' do
-  version '2.1.5'
-  sha256 'a239f97cb7ea052e8c58083e67c77d53c4610c1b838c6b5e8226caddddd24031'
+  version '2.1.6'
+  sha256 'f03866b906ac57a5e596696c8fc61547fef6d8639995eddbdc83bbef09d633e0'
 
   url "https://apps.ankiweb.net/downloads/current/anki-#{version}-mac.dmg"
   appcast 'https://apps.ankiweb.net/docs/changes.html'
   name 'Anki'
   homepage 'https://apps.ankiweb.net/'
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: '>= :sierra'
 
   app 'Anki.app'
 end
