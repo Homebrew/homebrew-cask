d0a41d3bbf9ff6480b369b482fb6dbe72bae4c46
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-13 12:55:46 -0800

    Update jasp to 0.9.2.0 (#56103)

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -1,6 +1,6 @@
 cask 'jasp' do
-  version '0.9.1.0'
-  sha256 'be683948a2797d17fdab4f6bd549999c7ba2aa2dd9cd5851baca5a4ed50c5005'
+  version '0.9.2.0'
+  sha256 'c9df6eb46d37b1996b424051e677a2329953adb92449e667f45b5dbfc506c309'
 
   url "https://static.jasp-stats.org/JASP-#{version}.dmg"
   name 'JASP'
