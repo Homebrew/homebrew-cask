dee0c51026d632eadd69c2f9ee8c45b7deea1d48
Randall <ran.dall@icloud.com>
2018-12-13 08:20:16 -0800

    Update augur to 1.8.4

--- Casks/augur.rb
+++ Casks/augur.rb
@@ -1,6 +1,6 @@
 cask 'augur' do
-  version '1.6.2'
-  sha256 '171769380a796f270314215d84d3d2349c5885bf5cee8b8dcaadf7a72b0a263e'
+  version '1.8.4'
+  sha256 '890534416a95a4b0ffd58a843e60ebb91bcf4c88d1c9a41cd33a126115f90ac6'
 
   url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-augur-#{version}.dmg"
   appcast 'https://github.com/AugurProject/augur-app/releases.atom'
