97ad8ace71a2f366d30f40e5e44c5046b6eb7799
Alexey Zagarin <zagarin@gmail.com>
2018-12-13 20:45:48 +0700

    Update bisq to 0.9.1

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask 'bisq' do
-  version '0.9.0'
-  sha256 '4a344c9759889a364b60ca35b8b577284548bb0f866d70bafd29bf452251bc3e'
+  version '0.9.1'
+  sha256 'ac026089ace53a9b07023a3d5f2757e76b0e0215a6f341955c30047e7f20b211'
 
   # github.com/bisq-network/bisq-desktop was verified as official when first introduced to the cask
   url "https://github.com/bisq-network/bisq-desktop/releases/download/v#{version}/Bisq-#{version}.dmg"
