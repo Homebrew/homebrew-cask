48ff6d02772ea7d1ab9213df3ffdf96523594576
CoreCode <core-code@users.noreply.github.com>
2018-12-14 10:12:55 +0100

    Update quicken to 5.9.1,59.25139.100 (#56116)

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask 'quicken' do
-  version '5.8.2,58.24609.100'
-  sha256 '5a8983206e311cccd35a6d87517137464ff4dda8dd4df806f1e104cb1c0e1901'
+  version '5.9.1,59.25139.100'
+  sha256 '966515c007c5b67831f1e87a3ec562ea1c0cd268261b750b1424ba91d57250ac'
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.after_comma}/Quicken-#{version.after_comma}.zip"
   appcast 'https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/appcast.xml'
