5ba93f25fed7cd6dd477f16cc81640298a86f88a
Alberto Gallego <albertogc2793@gmail.com>
2018-12-15 00:23:04 +0100

    sha256 added

--- Casks/notionso.rb
+++ Casks/notionso.rb
@@ -1,6 +1,6 @@
 cask 'notionso' do
   version '0.3.0'
-  sha256 :no_check
+  sha256 '6cfde9d52086fe1888477fa226c0c8df16360759352ca7d5c4ee6603f3b37eba'
 
   # desktop-release.notion-static.com was verified as official when first introduced to the cask
   url 'https://desktop-release.notion-static.com/Notion-0.3.0.dmg'
