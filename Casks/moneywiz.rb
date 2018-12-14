20a45ea9e634b8acae3c9e9b65cf3f30f4f2c9fa
Hsiang Lee <whalesingswee@gmail.com>
2018-12-15 01:27:24 +0800

    Add MoneyWiz Latest

---
+++ Casks/moneywiz.rb
@@ -0,0 +1,10 @@
+cask 'moneywiz' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://mac.wiz.money/MoneyWiz_Mac_Free.dmg'
+  name 'MoneyWiz'
+  homepage 'https://wiz.money/'
+
+  app 'MoneyWiz 3.app'
+end
