14b7081dbf189917599c50f739a1b0f467937529
Work <brigitte@corecode.io>
2018-12-14 21:13:02 +0100

    Update navicat-premium-essentials to 12.1.13

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium-essentials' do
-  version '12.1.12'
-  sha256 'bed84435e5c97a4fe3e6ed7cea238a6caf502340e15a87933e31dcd0772127ea'
+  version '12.1.13'
+  sha256 '03f77c20f6b938b8d3c500148f2cf9a7390f112ff7381e9483bc2314ed98123f'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20Premium%20Essentials&appLang=en'
