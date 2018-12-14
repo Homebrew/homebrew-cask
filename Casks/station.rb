69d61607ef3427fd899c00659c5998644fcf1f75
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-14 10:12:27 -0800

    Update station to 1.33.0 (#56158)

--- Casks/station.rb
+++ Casks/station.rb
@@ -1,6 +1,6 @@
 cask 'station' do
-  version '1.31.3'
-  sha256 'a49b53ecce744146e6251792e593aeed034e6fa5ae9c48ace30c756c25d11aeb'
+  version '1.33.0'
+  sha256 '8a362cafcb233e36605d7c0e6e2049ea3b8fe106063de715b4d1ef5563f41bb9'
 
   # github.com/getstation/desktop-app-releases was verified as official when first introduced to the cask
   url "https://github.com/getstation/desktop-app-releases/releases/download/#{version}/Station-#{version}.dmg"
