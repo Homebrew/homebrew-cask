815d2a92db8bce67e752fb279581209dacce88cc
Anver Housseini <contact@anver.ch>
2018-12-14 19:11:57 +0100

    Update passenger to 4.6.1 (#56131)

--- Casks/passenger.rb
+++ Casks/passenger.rb
@@ -1,6 +1,6 @@
 cask 'passenger' do
   version '4.6.1'
-  sha256 '860af97614dd12a751b132f0cf7d88110462ad88151a96f7e2dfacd6d2c3c8b2'
+  sha256 '2b385566b8d525169591b0f750e82767266d19aae5cfe86a7fbd122b73770547'
 
   url 'https://macinmind.com/Passenger.dmg'
   appcast 'https://macinmind.com/pads/PassengerAppcast.xml'
