ec0d7a0f8a68dbe34df8b840847749c5ba2b19ce
CoreCode <core-code@users.noreply.github.com>
2018-12-14 10:13:05 +0100

    Update 5kplayer to 5.5.0 (#56117)

--- Casks/5kplayer.rb
+++ Casks/5kplayer.rb
@@ -1,6 +1,6 @@
 cask '5kplayer' do
   version '5.5.0'
-  sha256 '54c6d807f1483bd97a5cde9c52c8181455a5755bd278fbe3fd9e841dfb9e11f3'
+  sha256 'b2ce216aa2f81236bee9f7020523a5b7f6b0e724002c0a8f9d122614f8d79d89'
 
   url 'https://www.5kplayer.com/download/5kplayer.dmg'
   appcast 'https://www.5kplayer.com/upgrade/mac/updatecast.xml'
