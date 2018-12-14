9b2bb6a0d8449be0128bda1f4b4f381cf7359026
Alberto Gallego <albertogc2793@gmail.com>
2018-12-14 18:42:18 +0100

    Add Notion.app v0.3.0

--- Casks/notionso.rb
+++ Casks/notionso.rb
@@ -2,9 +2,10 @@ cask 'notionso' do
   version '0.3.0'
   sha256 :no_check
 
-  url 'https://desktop-release.notion-static.com/Notion-#{version}.dmg'
+  # desktop-release.notion-static.com was verified as official when first introduced to the cask
+  url 'https://desktop-release.notion-static.com/Notion-0.3.0.dmg'
   name 'Notion'
-  homepage 'https://www.notion.so'
+  homepage 'https://www.notion.so/'
 
   app 'Notion.app'
 end
