219b6516c37a494cba12a76b5bba91183104ee8b
Rajiv Shah <rajivshah1@icloud.com>
2018-12-14 17:14:22 -0500

    Update trinity to 0.4.4

--- Casks/trinity.rb
+++ Casks/trinity.rb
@@ -1,6 +1,6 @@
 cask 'trinity' do
-  version '0.4.3'
-  sha256 '0bcccebdc60d95a6c6c0385e460556703a0d7d67d10ad1698ec9701b58888982'
+  version '0.4.4'
+  sha256 '407de95e33e4d221073f7d62866c938c6477d404e961a77b58011b518e52c9d8'
 
   # github.com/iotaledger/trinity-wallet was verified as official when first introduced to the cask
   url "https://github.com/iotaledger/trinity-wallet/releases/download/#{version}/trinity-desktop-#{version}.dmg"
