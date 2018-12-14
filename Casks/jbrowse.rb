a6398173c36969619e0d8bcf76bb669e0f0e3059
Work <brigitte@corecode.io>
2018-12-14 15:24:09 +0100

    Update jbrowse to 1.16.0

--- Casks/jbrowse.rb
+++ Casks/jbrowse.rb
@@ -1,6 +1,6 @@
 cask 'jbrowse' do
-  version '1.15.4'
-  sha256 '2377ddf29d2453fa6360cb181901c474fc0e84be8e750959d742617b8e540bc5'
+  version '1.16.0'
+  sha256 '86b5cf7d3fd3cecab1587c8edc24c07713cb2a5ece32e457af5b2904ddd692ea'
 
   # github.com/GMOD/jbrowse was verified as official when first introduced to the cask
   url "https://github.com/GMOD/jbrowse/releases/download/#{version}-release/JBrowse-#{version}-desktop-darwin-x64.zip"
