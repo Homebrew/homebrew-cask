ad980ae39265912a050ef2879a94e7508d423d26
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-15 09:11:29 -0800

    Fix HTTP --> HTTPS

--- Casks/polycom-realpresence.rb
+++ Casks/polycom-realpresence.rb
@@ -2,7 +2,7 @@ cask 'polycom-realpresence' do
   version '3.9.1.70422'
   sha256 '0f95b8cf2c35350324258ea699a878916390a6239f59fff890b975706adf6bd3'
 
-  url "http://downloads.polycom.com/video/realpresence_desktop/RPDesktop_#{version.dots_to_underscores}.dmg"
+  url "https://downloads.polycom.com/video/realpresence_desktop/RPDesktop_#{version.dots_to_underscores}.dmg"
   name 'Polycom RealPresence Desktop'
   homepage 'http://www.polycom.com/products-services/hd-telepresence-video-conferencing/realpresence-desktop/realpresence-desktop.html'
 
