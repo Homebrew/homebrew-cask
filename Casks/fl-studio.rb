856391433d6a7c73fa7479c4a796dfb2f203c3e0
Randall <ran.dall@icloud.com>
2018-12-15 08:57:33 -0800

    Update fl-studio to 20.1.0.161

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -1,6 +1,6 @@
 cask 'fl-studio' do
-  version '20.0.5.91'
-  sha256 '53166826d25940684e0dc66d8217cfa38820f760ea180b3778a73571c6517d30'
+  version '20.1.0.161'
+  sha256 '3f60763098ffd0fc245ddf78a508730e52f10659b66a8380e995839b88d67338'
 
   url "http://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
   appcast 'https://support.image-line.com/api.php?call=get_version_info&callback=il_get_version_info_cb&prod_type=undefined'
