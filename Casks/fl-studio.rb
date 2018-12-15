8e5d99c65771661d0ad3c8d498a500190faeadae
Vítor Galvão <info@vitorgalvao.com>
2018-12-15 23:48:25 +0000

    Update fl-studio.rb

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -1,6 +1,6 @@
 cask 'fl-studio' do
   version '20.1.0.161'
-  sha256 '65ac01099ea95ab26af57455ebf423468eec53314c1b8619268df574d9998512'
+  sha256 'ddf89d9b9d9e2e0a180229ed27857cbf38fd6e19eb3a02d3079a736aff72c696'
 
   url "https://support.image-line.com/action/redirect/flstudio#{version.major}_mac_installer"
   appcast 'https://support.image-line.com/api.php?call=get_version_info&callback=il_get_version_info_cb&prod_type=undefined'
