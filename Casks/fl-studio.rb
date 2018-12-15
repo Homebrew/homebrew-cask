b07f2abc35dab227f2fc6f17970b47f024e2ce08
Vítor Galvão <info@vitorgalvao.com>
2018-12-15 20:51:46 +0000

    Update fl-studio.rb

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -2,8 +2,7 @@ cask 'fl-studio' do
   version '20.1.0.161'
   sha256 '65ac01099ea95ab26af57455ebf423468eec53314c1b8619268df574d9998512'
 
-  url "http://demodownload.image-line.com/flstudio_mac_#{version}.dmg",
-      user_agent: :fake
+  url "https://support.image-line.com/action/redirect/flstudio#{version.major}_mac_installer"
   appcast 'https://support.image-line.com/api.php?call=get_version_info&callback=il_get_version_info_cb&prod_type=undefined'
   name 'FL Studio'
   homepage 'https://www.image-line.com/flstudio/'
