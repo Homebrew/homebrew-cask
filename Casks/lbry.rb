806e2918c8d4894fba0dd668edf7acbd902791b8
Randall <ran.dall@icloud.com>
2018-12-15 10:53:59 -0800

    Update lbry to 0.26.1

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,6 +1,6 @@
 cask 'lbry' do
-  version '0.25.1'
-  sha256 '5fb92b87d561cfaf0e4a9edf5dfa99f678e27718bfefc145f64c5943f4630146'
+  version '0.26.1'
+  sha256 '832a26cdb0b20eef5bc317cc3fa15c6bcef8c1d1a12aeb7acbccd82b96d085d1'
 
   # github.com/lbryio/lbry-app was verified as official when first introduced to the cask
   url "https://github.com/lbryio/lbry-app/releases/download/v#{version}/LBRY_#{version}.dmg"
