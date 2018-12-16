81b0e0666e009c2cd79db2a621d743103fbc59b5
Guillaume <7489814+255kb@users.noreply.github.com>
2018-12-16 16:14:23 +0100

    Update Mockoon to v1.3.0

--- Casks/mockoon.rb
+++ Casks/mockoon.rb
@@ -1,6 +1,6 @@
 cask 'mockoon' do
-  version '1.2.0'
-  sha256 '041830134077961090cb03f1dda7a78726a9f7098dbf961e923fee876b84a7a2'
+  version '1.3.0'
+  sha256 '1f8913f5719f2a47cce733f33188e7dc4b2edb4c63af13a2953040e0400d509e'
 
   # github.com/255kb/mockoon was verified as official when first introduced to the cask
   url "https://github.com/255kb/mockoon/releases/download/v#{version}/mockoon.setup.#{version}.dmg"
