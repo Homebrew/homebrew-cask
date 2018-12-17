dc439a8b36a409cd1cee0961a0e253e49cb83716
Randall <ran.dall@icloud.com>
2018-12-16 20:57:48 -0800

    Update fastonosql to 1.24.1

--- Casks/fastonosql.rb
+++ Casks/fastonosql.rb
@@ -1,6 +1,6 @@
 cask 'fastonosql' do
-  version '1.23.0'
-  sha256 'aed516263d8a830cdfb9b8558d84ab3f52d7a7e89b3613ae50e91cb2912d539e'
+  version '1.24.1'
+  sha256 'f8235b768525c352ce813799e75ba664faab653160a526bdef688e98fd77ef37'
 
   url "https://www.fastonosql.com/downloads/macosx/fastonosql-#{version}-x86_64.dmg"
   appcast 'https://github.com/fastogt/fastonosql/releases.atom'
