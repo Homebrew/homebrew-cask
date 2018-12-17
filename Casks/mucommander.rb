c4c0e2ee8886f9611c51463dffbb8ef441bd1b90
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-17 07:21:06 -0800

    Update mucommander to 0.9.3,2 (#56234)

--- Casks/mucommander.rb
+++ Casks/mucommander.rb
@@ -1,6 +1,6 @@
 cask 'mucommander' do
-  version '0.9.3,1'
-  sha256 '4bdc52fef2b9bc940320ea7c9fca9c8784bdfa0fdcda0ecb9c8b455f289e1f09'
+  version '0.9.3,2'
+  sha256 '2c5a16c5fff6d32763e901b32310a4e4e5e3cfc647b668e111a13197ed4be81c'
 
   # github.com/mucommander/mucommander was verified as official when first introduced to the cask
   url "https://github.com/mucommander/mucommander/releases/download/#{version.before_comma}-#{version.after_comma}/mucommander-#{version.before_comma}.dmg"
