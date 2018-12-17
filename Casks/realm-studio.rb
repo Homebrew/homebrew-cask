ecd5068d6bb0a0061090e3a4f0f03e146060eddc
Andres Riveros <aariveros@resit.cl>
2018-12-17 12:22:14 -0300

    Update realm-studio to 3.1.3 (#56253)

--- Casks/realm-studio.rb
+++ Casks/realm-studio.rb
@@ -1,6 +1,6 @@
 cask 'realm-studio' do
-  version '3.0.7'
-  sha256 '7a113b9b59412044dbcfc0a0df6abbb177801846257b420ff7dcdab372115993'
+  version '3.1.3'
+  sha256 '9ee88b4e27d0f23604a21dbb384809c5adb57d4cba5e0117e625142b802e2b0a'
 
   url "https://static.realm.io/downloads/realm-studio/Realm%20Studio-#{version}-mac.zip"
   appcast 'https://static.realm.io/downloads/realm-studio/latest-mac.yml'
