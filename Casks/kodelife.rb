a70f46debf74842fd6531ad606e025306881c071
Randall <ran.dall@icloud.com>
2018-12-17 10:27:07 -0800

    Update kodelife to 0.7.0.53

--- Casks/kodelife.rb
+++ Casks/kodelife.rb
@@ -1,6 +1,6 @@
 cask 'kodelife' do
-  version '0.6.5.0'
-  sha256 'c242773836daaf65aa269517bc20e395e70f4d28853a7eca5880e1633fd32564'
+  version '0.7.0.53'
+  sha256 '8a9a6c35b567b5deea1a614b660f307d957ec8b06d1af1c1177050c5d011c772'
 
   url "https://hexler.net/pub/kodelife/kodelife-#{version}-osx.zip"
   appcast 'https://hexler.net/pub/kodelife/appcast.hex'
