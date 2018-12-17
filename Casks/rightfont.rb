c56d0955c6423b6f5f484ccb9db03f367f8aa0a5
Randall <ran.dall@icloud.com>
2018-12-17 10:28:42 -0800

    Update rightfont to 5.2.4

--- Casks/rightfont.rb
+++ Casks/rightfont.rb
@@ -1,6 +1,6 @@
 cask 'rightfont' do
-  version '5.2.3'
-  sha256 '203a9bb92c6e07e028bdeeab6758a9e05bb0bcd428b2b9c61d9ca8d3ee09e948'
+  version '5.2.4'
+  sha256 '57b955fc1ef48284a8ac654f73cb3bb08c1ae4fec265792a508896f64c012e9f'
 
   url 'https://rightfontapp.com/update/rightfont.zip'
   appcast "https://rightfontapp.com/update/appcast#{version.major}.xml"
