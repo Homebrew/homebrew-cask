b30a6a714ddf957e50571dc16266669dbb36a52e
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-16 21:53:04 -0800

    Update URL

--- Casks/pennywise.rb
+++ Casks/pennywise.rb
@@ -2,7 +2,7 @@ cask 'pennywise' do
   version '0.6.3'
   sha256 '032a0fbcf1f37e74d7a83382c185d0f1cb29ed094005c42520c98a4030b65079'
 
-  url "https://github.com/kamranahmedse/pennywise/releases/download/v#{version}/Pennywise-#{version}-mac.zip"
+  url "https://github.com/kamranahmedse/pennywise/releases/download/v#{version}/Pennywise-#{version}.dmg"
   appcast 'https://github.com/kamranahmedse/pennywise/releases.atom'
   name 'Pennywise'
   homepage 'https://github.com/kamranahmedse/pennywise'
