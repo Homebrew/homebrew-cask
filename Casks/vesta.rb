1c69a842cc46cd4668f3ac80b589384661d6ddf6
KeLiu <specter119@users.noreply.github.com>
2018-12-17 20:13:08 +0800

    update vesta to 3.4.5 (#56225)

--- Casks/vesta.rb
+++ Casks/vesta.rb
@@ -1,6 +1,6 @@
 cask 'vesta' do
-  version '3.4.4'
-  sha256 'bd41a8863d4dc75d4e511bb4f5588c548740767781b4a1cfcea3dd5ca9465f1e'
+  version '3.4.5'
+  sha256 '6f75b55a46db1ebde062e346c15a56eabaff0e4fc0cad0e2035dc33de34c66bc'
 
   url "https://jp-minerals.org/vesta/archives/#{version}/VESTA.dmg"
   name 'VESTA'
