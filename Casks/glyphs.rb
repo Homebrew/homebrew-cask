235cf48723b05683f4480a5bc9414d085570845d
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-16 21:58:09 -0800

    Update URL

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -2,7 +2,7 @@ cask 'glyphs' do
   version '2.5.2,1189'
   sha256 '78e08330d8b642d6d8d92d058d5a8d3fee27b3c3dcb68b13d75eab507be36f62'
 
-  url "https://updates.glyphsapp.com/Glyphs#{version}.zip"
+  url "https://updates.glyphsapp.com/Glyphs-#{version}.zip"
   appcast "https://updates.glyphsapp.com/appcast#{version.major}.xml"
   name 'Glyphs'
   homepage 'https://glyphsapp.com/'
