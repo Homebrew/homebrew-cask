af0f6dab36493b3930a5e07db75a51c96eebd454
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-17 04:11:46 -0800

    Update macx-dvd-ripper-pro to 6.2.0 (#56228)
    * Update macx-dvd-ripper-pro to 6.2.0
    
    * Update to HTTPS in Appcast

--- Casks/macx-dvd-ripper-pro.rb
+++ Casks/macx-dvd-ripper-pro.rb
@@ -1,9 +1,9 @@
 cask 'macx-dvd-ripper-pro' do
-  version '6.1.1'
-  sha256 '1a7a4fdfc8d30569e2b45ec299f200808e083735aa5ce1808e0a2692b982b9b7'
+  version '6.2.0'
+  sha256 'be833ed61e2da6224216d49eddab58df020e7212ccf3a1ced8b620877a2f051e'
 
   url 'https://www.macxdvd.com/download/macx-dvd-ripper-pro.dmg'
-  appcast 'http://www.macxdvd.com/mac-dvd-ripper-pro/upgrade/macx-dvd-ripper-pro'
+  appcast 'https://www.macxdvd.com/mac-dvd-ripper-pro/upgrade/macx-dvd-ripper-pro'
   name 'MacX DVD Ripper Pro'
   homepage 'https://www.macxdvd.com/mac-dvd-ripper-pro/'
 
