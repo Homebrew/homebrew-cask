1214c2bbe712f93505e1df9c35e82bba237c5f75
Randall <17261190+ran-dall@users.noreply.github.com>
2018-12-17 11:34:33 -0800

    Update URL and SHA256

--- Casks/kodelife.rb
+++ Casks/kodelife.rb
@@ -1,8 +1,8 @@
 cask 'kodelife' do
   version '0.7.0.53'
-  sha256 '8a9a6c35b567b5deea1a614b660f307d957ec8b06d1af1c1177050c5d011c772'
+  sha256 'eccb944d3f60034f4216a29fdf4d7010ed4d5d3d80861315f102dd9d681aa9e1'
 
-  url "https://hexler.net/pub/kodelife/kodelife-#{version}-osx.zip"
+  url "https://hexler.net/pub/kodelife/kodelife-#{version}-macos.zip"
   appcast 'https://hexler.net/pub/kodelife/appcast.hex'
   name 'KodeLife'
   homepage 'https://hexler.net/software/kodelife'
