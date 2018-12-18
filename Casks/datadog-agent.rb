90c8b8bbca587897031bd9b53b0d10c8536c86a2
CoreCode <core-code@users.noreply.github.com>
2018-12-18 14:59:45 +0100

    Update datadog-agent.rb

--- Casks/datadog-agent.rb
+++ Casks/datadog-agent.rb
@@ -16,6 +16,7 @@ cask 'datadog-agent' do
   end
 
   uninstall launchctl: 'com.datadoghq.agent',
+#             pkgutil:   'com.datadoghq.agent' #this is commented out because PKG uninstallation seems to fail due to missing files caused by case insensitivity and files that differ only in case
             delete:    [
                          '/Applications/Datadog Agent.app',
                          '/opt/datadog-agent/',
