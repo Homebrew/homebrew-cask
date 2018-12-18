3a4a6e1751c967c3cd46ea1c7fdbbb778a493480
Ben <BenjaminHCCarr@users.noreply.github.com>
2018-12-17 22:04:55 -0500

    Please squash, missed a close apostrophe

--- Casks/rawtherapee.rb
+++ Casks/rawtherapee.rb
@@ -1,5 +1,5 @@
 cask 'rawtherapee' do
-  version '5.5
+  version '5.5'
   sha256 '2ca1635ab86decb46285a16f21e334e2949ebbf27ffc09567d83be7d36a4ec86'
 
   url "https://rawtherapee.com/releases_head/mac/RawTherapee_OSX_10.9_64_#{version}.zip"
