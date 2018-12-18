3181cc860cc547eddac02abe4c8367a43240317e
Paul Naranja <pnaranja@chegg.com>
2018-12-17 21:59:56 -0800

    Update oni to 0.3.9

--- Casks/oni.rb
+++ Casks/oni.rb
@@ -1,6 +1,6 @@
 cask 'oni' do
-  version '0.3.6'
-  sha256 '37bd52bc9ff788563664440a421abd200d85aeb9ff6aa1adf43bd455c271094b'
+  version '0.3.9'
+  sha256 'c1b6e0fcce1a69d6460197158d8e087496199a3d8368901b452ee214eab039c4'
 
   # github.com/onivim/oni was verified as official when first introduced to the cask
   url "https://github.com/onivim/oni/releases/download/v#{version}/Oni-#{version}-osx.dmg"
