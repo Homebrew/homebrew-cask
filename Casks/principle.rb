fb1cab17736ec44ae5651f773eb893e1472a32d7
Julian Mayer <julian@corecode.io>
2018-12-18 23:46:54 +0100

    Update principle to 5.2

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,6 +1,6 @@
 cask 'principle' do
-  version '4.7'
-  sha256 '648199e98c8d7ad689f2cac2a1ce3bb2f87d587b88624262c8d70d2efaf88188'
+  version '5.2'
+  sha256 '1ecd9c186864c96ed3b3f5e8facc1e86489e49d3d6a8b19900527d26af5eb8b4'
 
   url "http://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
   appcast 'http://principleformac.com/update2.xml'
