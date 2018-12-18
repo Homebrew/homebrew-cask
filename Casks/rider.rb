ec7fd63cbbb60d0c3ba3845adbfec5e9aa73c1a1
Michael Dahl <micdah@gmail.com>
2018-12-18 14:34:12 +0100

    Update rider to 2018.3,183.5047.86

--- Casks/rider.rb
+++ Casks/rider.rb
@@ -1,6 +1,6 @@
 cask 'rider' do
-  version '2018.2.3,182.4231.496'
-  sha256 '83817839fb3e5ef44bfe61f360adadf9e8a1597438589723282eed9fc7199dee'
+  version '2018.3,183.5047.86'
+  sha256 'e1701ff76e1805a0409dd15d47cc48a1d13d6f7b592b046f7c77579e343e1143'
 
   url "https://download.jetbrains.com/rider/JetBrains.Rider-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release'
