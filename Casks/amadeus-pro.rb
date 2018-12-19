1c1e73c769c5e14c508bbd8e2bde3120c2d3dc4b
Vítor Galvão <info@vitorgalvao.com>
2018-12-19 20:43:56 +0000

    Update amadeus-pro.rb

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -3,7 +3,7 @@ cask 'amadeus-pro' do
   sha256 'a994e2db0a2f42a28afc289cdf55696fbb31132b9d23aab4e2abc667d3f493b2'
 
   # amazonaws.com/AmadeusPro2 was verified as official when first introduced to the cask
-  url 'http://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip'
+  url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip'
   appcast 'https://s3.amazonaws.com/AmadeusPro2/versions.rtf'
   name 'Amadeus Pro'
   homepage 'https://www.hairersoft.com/pro.html'
