712b52b055b38cf638f3b388a0d39f743eb5ffbb
CoreCode <core-code@users.noreply.github.com>
2018-12-19 12:39:04 +0100

    Update gemini.rb

--- Casks/gemini.rb
+++ Casks/gemini.rb
@@ -1,9 +1,9 @@
 cask 'gemini' do
-  version '2.312,1539778530'
+  version '2.5.4,317:1545152224'
   sha256 '6897195c7da4f57f2972a6f42386979cd8ea908364daadc8122f700e370bcd7e'
 
   # dl.devmate.com/com.macpaw.site.Gemini was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.macpaw.site.Gemini#{version.major}/#{version.minor}/#{version.after_comma}/Gemini#{version.major}-#{version.minor}.zip"
+  url "https://dl.devmate.com/com.macpaw.site.Gemini#{version.major}/#{version.after_comma.before_colon}/#{version.after_colon}/Gemini#{version.major}-#{version.after_comma.before_colon}.zip"
   appcast "https://updates.devmate.com/com.macpaw.site.Gemini#{version.major}.xml"
   name 'Gemini'
   homepage 'https://macpaw.com/gemini'
