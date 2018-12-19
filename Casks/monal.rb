299f474476f3b70b5b209db0af68fab547ac8a93
CoreCode <core-code@users.noreply.github.com>
2018-12-19 02:23:20 +0100

    Update monal to 2.3b4 (#56333)

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,6 +1,6 @@
 cask 'monal' do
-  version '2.3b3'
-  sha256 'bc4428821bbe5c55c7ba7a01bfab8fd4d0357a212d92d4a7504d9c95385a4adc'
+  version '2.3b4'
+  sha256 'db619e074fbb9760279e3390160769da4e2481f68d62331102f72e86fe1f55a7'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
   appcast 'https://monal.im/Monal-OSX/appcast.xml'
