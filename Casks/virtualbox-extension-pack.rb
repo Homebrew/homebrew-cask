0b1a3f556de211f63f9387a4a93341037f6955d0
ralph <ralph@cs.cf.ac.uk>
2018-12-19 21:04:06 +0000

    Update virtualbox-extension-pack to 6.0.0,127566

--- Casks/virtualbox-extension-pack.rb
+++ Casks/virtualbox-extension-pack.rb
@@ -1,6 +1,6 @@
 cask 'virtualbox-extension-pack' do
-  version '5.2.22,126460'
-  sha256 '779250666551b2f5426e86c2d21ceb0209b46174536971611025f753535131ef'
+  version '6.0.0,127566'
+  sha256 'b99ef88b0ac1abebc7a6d7168dd5eec58c426f05acf69c4055defff175038f57'
 
   url "https://download.virtualbox.org/virtualbox/#{version.before_comma}/Oracle_VM_VirtualBox_Extension_Pack-#{version.before_comma}-#{version.after_comma}.vbox-extpack"
   appcast 'https://download.virtualbox.org/virtualbox/LATEST.TXT'
