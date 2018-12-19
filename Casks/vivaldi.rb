f105b17563bb0957a3dd90f38319f1fc146a478f
Alexander Lauster <alexander@macwinlin.de>
2018-12-19 15:36:57 +0100

    Update vivaldi to 2.2.1388.37 (#56381)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,6 +1,6 @@
 cask 'vivaldi' do
-  version '2.2.1388.34'
-  sha256 'f5c45c999913b5f08a303ed8e7207a75e02edfdac9c11d5a41ed291c0d1d40a9'
+  version '2.2.1388.37'
+  sha256 '27ef51dfb38c2a8822d337a821a3a493c5c846e4b5f336a0ffd71aeadd957e71'
 
   url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/public/mac/appcast.xml'
