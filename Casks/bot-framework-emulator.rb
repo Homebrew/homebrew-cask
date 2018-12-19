4c01e7cc2400901f89009686a78839c4a43abf0c
CoreCode <core-code@users.noreply.github.com>
2018-12-19 02:57:47 +0100

    Update botframework-emulator to 4.2.1 (#56335)
    * Update botframework-emulator to 4.2.1
    
    * Update botframework-emulator.rb
    
    * Update and rename botframework-emulator.rb to bot-framework-emulator.rb
    
    * Update bot-framework-emulator.rb

--- Casks/bot-framework-emulator.rb
+++ Casks/bot-framework-emulator.rb
@@ -1,6 +1,6 @@
-cask 'botframework-emulator' do
-  version '3.5.37'
-  sha256 '9186f75b8e1881fa536daeebdf5e08d632098f33f144dc003a7be5c265ba0bb1'
+cask 'bot-framework-emulator' do
+  version '4.2.1'
+  sha256 '6f9a525969ccd748d486d1b3a4d4f58dc7efbe0f4cc6802024bc9be542e8f8b3'
 
   url "https://github.com/Microsoft/BotFramework-Emulator/releases/download/v#{version}/botframework-emulator-#{version}-mac.zip"
   appcast 'https://github.com/Microsoft/BotFramework-Emulator/releases.atom'
@@ -9,7 +9,7 @@ cask 'botframework-emulator' do
 
   auto_updates true
 
-  app 'botframework-emulator.app'
+  app 'mac/Bot Framework Emulator.app'
 
   uninstall quit: 'com.electron.botframework-emulator'
 
