87d237b60d606f8ebe533f6f18d08c4a93c7e522
CoreCode <core-code@users.noreply.github.com>
2018-12-19 02:46:36 +0100

    Update protege to 5.5.0-beta-7 (#56344)

--- Casks/protege.rb
+++ Casks/protege.rb
@@ -1,6 +1,6 @@
 cask 'protege' do
-  version '5.5.0-beta-4'
-  sha256 '7ca43f07b6a6bc086e428bfe1e23d0a725177636a32ee41ca6f1b5bb2f36d9a0'
+  version '5.5.0-beta-7'
+  sha256 '7329053402e0d4a72c2e54deeeaef2006b841b8edd3cf092c9262268fb8094f9'
 
   # github.com/protegeproject/protege-distribution was verified as official when first introduced to the cask
   url "https://github.com/protegeproject/protege-distribution/releases/download/v#{version}/Protege-#{version}-os-x.zip"
