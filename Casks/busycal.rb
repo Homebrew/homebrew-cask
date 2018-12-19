ca578ea6ff38084e016fa4eff24a47da8f588dbe
brigitte zmoelnig <brigitte@corecode.io>
2018-12-19 11:29:33 +0100

    Update busycal to 3.5.7,350716

--- Casks/busycal.rb
+++ Casks/busycal.rb
@@ -1,6 +1,6 @@
 cask 'busycal' do
-  version '3.5.6,350600'
-  sha256 'dfd41fe7081ef53ca467fd832e64619b852c7035130d59ca1215d4b2385dce87'
+  version '3.5.7,350716'
+  sha256 '8e505442e0bcff773e7712afa2a812566c7994d7b49016671faa01c571e0048b'
 
   url 'https://www.busymac.com/download/BusyCal.zip'
   appcast 'https://busymac.com/busycal/releasenotes.html'
