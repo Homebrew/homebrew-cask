0269ece48ca602425667d5996e40f0038b2f330b
p1ne <8995611+p1ne@users.noreply.github.com>
2018-12-19 17:41:16 +0300

    Updated newmoon to 28.2.1

--- Casks/newmoon.rb
+++ Casks/newmoon.rb
@@ -1,8 +1,8 @@
 cask 'newmoon' do
-  version '27.9.0'
-  sha256 '1982b506d012db957e4bf017dcb2b7aaed65bb72d8e8a12fcc5f304f1231395e'
+  version '28.2.1,1mW5zlk_1jR81VnlHKPzNA4MYYbOz13MN'
+  sha256 'f48ade0855a24eae7e7e3b7df09d42932f84ed872a15765882a9681c70dd7f28'
 
-  url "https://mac.palemoon.org/dist/palemoon-#{version}.mac64.dmg"
+  url "https://drive.google.com/uc?export=download&id=#{version.after_comma}"
   name 'Pale Moon'
   homepage 'https://www.palemoon.org/'
 
