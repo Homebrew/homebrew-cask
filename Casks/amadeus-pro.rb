78ee863b6bda0a5e98c18a4d6bce8619f458a190
suschizu <brigitte@corecode.io>
2018-12-19 22:01:25 +0100

    Update amadeus-pro to 2.5.3 (#56398)
    * Update amadeus-pro to 2.5.3
    
    * Update amadeus-pro.rb

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -1,9 +1,9 @@
 cask 'amadeus-pro' do
-  version '2.5.2'
-  sha256 'fc4189dbadc4da8078d586b9453170e84e974816000bf66e22dcd0e8612789a6'
+  version '2.5.3'
+  sha256 'a994e2db0a2f42a28afc289cdf55696fbb31132b9d23aab4e2abc667d3f493b2'
 
   # amazonaws.com/AmadeusPro2 was verified as official when first introduced to the cask
-  url 'http://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip'
+  url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip'
   appcast 'https://s3.amazonaws.com/AmadeusPro2/versions.rtf'
   name 'Amadeus Pro'
   homepage 'https://www.hairersoft.com/pro.html'
