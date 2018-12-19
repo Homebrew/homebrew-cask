d0674ddfe700128f468ce203b2cd9756be9aed08
Vítor Galvão <info@vitorgalvao.com>
2018-12-19 20:44:35 +0000

    Update royal-tsx.rb

--- Casks/royal-tsx.rb
+++ Casks/royal-tsx.rb
@@ -2,7 +2,7 @@ cask 'royal-tsx' do
   version '4.0.0.1000'
   sha256 'f480f83023f75272a203b4d9451a19ef5cd1a8909ebf30f94923ff4bf9cfad37'
 
-  # https://royaltsx-v3.royalapplications.com was verified as official when first introduced to the cask
+  # https://royaltsx-v4.royalapplications.com was verified as official when first introduced to the cask
   url "https://royaltsx-v4.royalapplications.com/updates/royaltsx_#{version}.dmg"
   appcast "https://royaltsx-v#{version.major}.royalapplications.com/updates_stable.php"
   name 'Royal TSX'
