7b6d6f0d834bde26d474c4752c1e7e87b1a752f5
Gavin Chappell <g-a-c@noreply.users.github.com>
2018-12-19 13:38:38 +0000

    updated Quassel (client-only and combined client/server) to 0.13.0

--- Casks/quassel.rb
+++ Casks/quassel.rb
@@ -1,6 +1,6 @@
 cask 'quassel' do
-  version '0.12.5'
-  sha256 'da76fae7ab612cdedf37e48b34fa68acf90385cdb20d5ec61f3c14bbac4a447e'
+  version '0.13.0'
+  sha256 'f4f36f51208503ecc6782649f60ce2f4f0bd81e0b3152aa0818785eb60c5eac5'
 
   url "https://quassel-irc.org/pub/QuasselMono_MacOSX-x86_64_#{version}.dmg"
   appcast 'https://github.com/quassel/quassel/releases.atom'
