727801822151714167d3ad8461d22de74c1a8e33
Joerg Schoenfisch <joe776@users.noreply.github.com>
2018-12-19 20:09:04 +0100

    Update eclipse-jee to 4.10.0,2018-12:R

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-jee' do
-  version '4.9.0,2018-09:R'
-  sha256 '5ca049d86362534815a169224604f44c8c32a162920697f04f2e4f0230450bcf'
+  version '4.10.0,2018-12:R'
+  sha256 '1bf59382e7eae3f1de29db3e4931455e20c34493269b3567f973b85bf44cb2a0'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-jee-#{version.after_comma.before_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-jee-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg"
   name 'Eclipse IDE for Java EE Developers'
   homepage 'https://eclipse.org/'
 
