c9622e2db238db023c5008c777e0bdb0f8dded97
Vítor Galvão <info@vitorgalvao.com>
2018-12-20 14:13:28 +0000

    Update and rename cr3.rb to cr.rb

--- Casks/cr.rb
+++ Casks/cr.rb
@@ -1,17 +1,17 @@
-cask 'cr3' do
+cask 'cr' do
   version '3.0.56,10'
   sha256 '0b6ba965635e9f9af745421b2579c8c14d8e3931a77ebbb1afc427cd537c18f3'
 
   url "https://downloads.sourceforge.net/crengine/cr3.#{version.minor}.#{version.patch}-#{version.after_comma}.dmg"
-  appcast 'https://sourceforge.net/projects/crengine/rss?path=/CoolReader3'
+  appcast "https://sourceforge.net/projects/crengine/rss?path=/CoolReader#{version.major}"
   name 'Cool Reader'
   homepage 'https://sourceforge.net/projects/crengine/'
 
-  app 'cr3.app'
+  app "cr#{version}.app"
 
   zap trash: [
-               '~/.cr3',
-               '~/Library/Saved Application State/org.coolreader.cr3.savedState',
-               '/Applications/cr3.ini',
+               "~/.cr#{version}",
+               "~/Library/Saved Application State/org.coolreader.cr#{version}.savedState",
+               "/Applications/cr#{version}.ini",
              ]
 end
