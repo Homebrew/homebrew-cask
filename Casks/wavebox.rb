b1f300f4e732c4c09db9b2ea33edc39c771d3908
Leo Liu <karta1041337@gmail.com>
2018-12-21 01:42:50 +0800

    Update wavebox to 4.5.9

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,6 +1,6 @@
 cask 'wavebox' do
-  version '4.5.8'
-  sha256 'a9bbb9a87f09b7f27fde9c4ced8f6a37286a872626ac92a268af973aa6687c48'
+  version '4.5.9'
+  sha256 '53b9ab42059d0de893568dade028162ca98bc2846814e52419ccf99b7e1545b0'
 
   # github.com/wavebox/waveboxapp was verified as official when first introduced to the cask
   url "https://github.com/wavebox/waveboxapp/releases/download/v#{version}/Wavebox_#{version.dots_to_underscores}_osx.dmg"
