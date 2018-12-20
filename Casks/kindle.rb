8649b0870b5f5743c984ac0f92627374bf5bf06c
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2018-12-20 01:18:42 -0500

    Update kindle to 52065

--- Casks/kindle.rb
+++ Casks/kindle.rb
@@ -1,6 +1,6 @@
 cask 'kindle' do
-  version '50131'
-  sha256 'cc58e6fef6a30e98d78349016a9828009a1d7cd7d0d9f6bc88e74db43edbf7b6'
+  version '52065'
+  sha256 '043c9ec235141455ba0b92d3c7529e3112b90e1431358cf3d801e3d893a435f9'
 
   # s3.amazonaws.com/kindleformac was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/kindleformac/#{version}/KindleForMac-#{version}.dmg"
