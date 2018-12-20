09a3e2c58b86232e9367fe29cf91399e1def45ac
p1ne <8995611+p1ne@users.noreply.github.com>
2018-12-20 11:14:21 +0300

    Added cr3 3.0.56. Fixed download path and stanzas

--- Casks/cr3.rb
+++ Casks/cr3.rb
@@ -2,15 +2,16 @@ cask 'cr3' do
   version '3.0.56,10'
   sha256 '0b6ba965635e9f9af745421b2579c8c14d8e3931a77ebbb1afc427cd537c18f3'
 
-  url "https://sourceforge.net/projects/crengine/files/CoolReader3/cr3-#{version.major}.#{version.minor}.#{version.patch}/cr3.#{version.minor}.#{version.patch}-#{version.after_comma}.dmg/download"
+  url "https://downloads.sourceforge.net/crengine/cr3.#{version.minor}.#{version.patch}-#{version.after_comma}.dmg"
+  appcast 'https://sourceforge.net/projects/crengine/rss?path=/CoolReader3'
   name 'Cool Reader'
   homepage 'https://sourceforge.net/projects/crengine/'
 
-  app "cr3.app"
+  app 'cr3.app'
 
   zap trash: [
                '~/.cr3',
                '~/Library/Saved Application State/org.coolreader.cr3.savedState',
-               '/Applications/cr3.ini'
+               '/Applications/cr3.ini',
              ]
 end
