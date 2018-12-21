5ba0596eeadbd27b31a26cb25bf7aa60a1ed3f12
Lucio Rubens <luciorubeens@gmail.com>
2018-12-21 13:29:54 -0300

    Update ark-desktop-wallet to 2.1.1

--- Casks/ark-desktop-wallet.rb
+++ Casks/ark-desktop-wallet.rb
@@ -1,6 +1,6 @@
 cask 'ark-desktop-wallet' do
-  version '2.1.0'
-  sha256 '58fe5c8c7683a83ea0857f294e00502fee81204eaf5471d8b040209c06de0291'
+  version '2.1.1'
+  sha256 '5b72183397998966379a7dd853e941838315bbc7a3fdef961607e872453ea56d'
 
   # github.com/ArkEcosystem/ark-desktop was verified as official when first introduced to the cask
   url "https://github.com/ArkEcosystem/ark-desktop/releases/download/#{version}/Ark.Desktop.Wallet-#{version}.dmg"
