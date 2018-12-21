ae447b45868487e0230f679c6e86df2ee9cf094f
mars <marshalys@gmail.com>
2018-12-21 10:43:51 +0800

    update liteide to 35.2
    update to 35.2

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,9 +1,9 @@
 cask 'liteide' do
-  version '35.1'
-  sha256 'bb6b9623cb57ef1d92572cbc2cac858e5d2ff5032777752a97f094e041cab2cc'
+  version '35.2'
+  sha256 'e563158c95aa2c316f00ecaf4a599313c27311b73230a8df9afbf11f1e37a4dd'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
-  url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.9.5.zip"
+  url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.9.5-1.zip"
   appcast 'https://github.com/visualfc/liteide/releases.atom'
   name 'LiteIDE'
   homepage 'http://liteide.org/'
