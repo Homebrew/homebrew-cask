88a6437bfaf78b36f4c54a1b14f638ee1a4713d8
suschizu <brigitte@corecode.io>
2018-12-22 11:28:22 +0100

    Update savoir-faire-linux-ring.rb
    its a bit confusing - the app-name changed but not the bundle identifier (cx.ring) - additionally the url and the feed title is still Ring - nightly (https://dl.ring.cx/mac_osx/sparkle-ring.xml) - should the cask be renamed?

--- Casks/savoir-faire-linux-ring.rb
+++ Casks/savoir-faire-linux-ring.rb
@@ -1,14 +1,14 @@
 cask 'savoir-faire-linux-ring' do
-  version '201812181300'
-  sha256 '4730fe08a824a3263899af96013dda3ff813c4bfe5a4167f35a70a8072bfc77f'
+  version '20181219.1734'
+  sha256 '795d49b2c5c71826a53422f12c0e92efe83f9dab3dfa9d634fded7da270d8c75'
 
-  url "https://dl.ring.cx/mac_osx/jami-#{version}.dmg"
+  url "https://dl.ring.cx/mac_osx/ring-#{version.no_dots}.dmg"
   appcast 'https://dl.ring.cx/mac_osx/sparkle-ring.xml'
   name 'Savoir-faire LINUX Ring'
-  name 'Ring'
+  name 'Jami'
   homepage 'https://ring.cx/'
 
   auto_updates true
 
-  app 'Ring.app'
+  app 'Jami.app'
 end
