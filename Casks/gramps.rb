00d18b34862e1e34ab896ebdd0a61a776af4afc9
Vítor Galvão <info@vitorgalvao.com>
2018-12-22 19:16:05 +0000

    Update gramps.rb

--- Casks/gramps.rb
+++ Casks/gramps.rb
@@ -3,7 +3,7 @@ cask 'gramps' do
   sha256 '6e53348fdbd3e30b1e7979e5037824091963b66dae98db203d5c1b631f7a3742'
 
   # github.com/gramps-project/gramps was verified as official when first introduced to the cask
-  url "https://github.com/gramps-project/gramps/releases/download/v#{version.major_minor_patch}/Gramps-Intel-#{version}.dmg"
+  url "https://github.com/gramps-project/gramps/releases/download/v#{version.major_minor_patch}/Gramps-#{version}.dmg"
   appcast 'https://github.com/gramps-project/gramps/releases.atom'
   name 'Gramps'
   homepage 'https://gramps-project.org/introduction-WP/'
