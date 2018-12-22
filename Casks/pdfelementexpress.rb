4895f54a8905e03b29880905eba51841ae15911a
Vítor Galvão <info@vitorgalvao.com>
2018-12-22 01:05:56 +0000

    Update pdfelementexpress.rb

--- Casks/pdfelementexpress.rb
+++ Casks/pdfelementexpress.rb
@@ -1,9 +1,9 @@
-cask 'pdfelementexpress' do
+cask 'pdfelement.express' do
   version '1.2.0,4133'
   sha256 '13d506e32998e88c92a056ac978cb158085ef278df7867a20bfd68e20627d576'
 
   url "http://download.wondershare.com/cbs_down/mac-pdfelement-express_full#{version.after_comma}.dmg"
-  name 'Wondershare PDFelement Express for Mac'
+  name 'Wondershare PDFelement Express'
   homepage 'https://pdf.wondershare.com/pdfelement-express-mac.html'
 
   app 'PDFelement Express.app'
