3e3bffcd9e5a4f7442e57af0baae473260238271
suschizu <brigitte@corecode.io>
2018-12-22 02:08:33 +0100

    Update jamf-migrator to 3.1.2 (#56504)

--- Casks/jamf-migrator.rb
+++ Casks/jamf-migrator.rb
@@ -1,6 +1,6 @@
 cask 'jamf-migrator' do
-  version '3.1.0'
-  sha256 '08ab008555ba5fb50e9b64a253a3064978d1bbc2bd9713e3f3b4719eeb0523c5'
+  version '3.1.2'
+  sha256 'b4e79a941a631a1ee5a1bd99d30c438ad369b4a16e11d0d439e7200d95e816bb'
 
   url 'https://github.com/jamfprofessionalservices/JamfMigrator/releases/download/current/jamf-migrator.zip'
   appcast 'https://github.com/jamfprofessionalservices/JamfMigrator/releases.atom'
