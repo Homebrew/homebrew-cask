253418fd14b2924cc3835ff250e85bb8951d9bd8
suschizu <brigitte@corecode.io>
2018-12-22 02:09:51 +0100

    Update syncovery.rb (#56516)

--- Casks/syncovery.rb
+++ Casks/syncovery.rb
@@ -1,6 +1,6 @@
 cask 'syncovery' do
-  version '8.15b'
-  sha256 '8c61dc8045df1893d26476c4e5b1b6ebf1f06131d8c2482a6d81a8788e757c39'
+  version '8.16'
+  sha256 'ac1d75a6e2cc7d601df7d9157e29fdc761d844a05b0eaa06aceeecdc247f2be3'
 
   url "https://www.syncovery.com/release/SyncoveryMac#{version}.dmg"
   name 'Syncovery'
