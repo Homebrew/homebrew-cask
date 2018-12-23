c739c69bd590efe889297670c7a5590199cf4992
Anver Housseini <contact@anver.ch>
2018-12-23 14:41:58 +0100

    Update clamxav to 3.0.8_7687 (#56550)

--- Casks/clamxav.rb
+++ Casks/clamxav.rb
@@ -1,6 +1,6 @@
 cask 'clamxav' do
-  version '3.0.7_7641'
-  sha256 '4ab5ccc53d5a766e116f878fc04d84789807823fe74ab1ea0b5eb6f6aeca865c'
+  version '3.0.8_7687'
+  sha256 '2ad7c8dcd755087a0f02fb95b3b510fef6775430823f74939b276162456af131'
 
   url "https://cdn.clamxav.com/ClamXAVdownloads/ClamXAV_#{version}.zip"
   appcast "https://www.clamxav.com/sparkle/appcast#{version.major}.xml"
