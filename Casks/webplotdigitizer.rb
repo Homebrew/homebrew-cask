049993a6e81c05492156263d74bb10775c136c9c
Qi Zhang <singularitti@outlook.com>
2018-12-24 02:00:51 -0500

    Add new cask webplotdigitizer

---
+++ Casks/webplotdigitizer.rb
@@ -0,0 +1,10 @@
+cask 'webplotdigitizer' do
+  version '4.0.0'
+  sha256 '76718f04d95aadcc5e41e4d1e7e7a602aa82ed165a49c28c48f50695ee376ee3'
+
+  url 'https://automeris.io/downloads/WebPlotDigitizer-4.1-darwin-x64.zip'
+  name 'WebPlotDigitizer'
+  homepage 'https://automeris.io/WebPlotDigitizer/'
+
+  app 'WebPlotDigitizer-darwin-x64/WebPlotDigitizer.app'
+end
