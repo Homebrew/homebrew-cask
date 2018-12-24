9d7753e14f3d09d737724b1b6325e761a82ff8ff
Vítor Galvão <info@vitorgalvao.com>
2018-12-24 21:06:18 +0000

    Update kate.rb

--- Casks/kate.rb
+++ Casks/kate.rb
@@ -1,10 +1,10 @@
 cask 'kate' do
-  version '18.08.3,306'
+  version '18.08.3-307'
   sha256 'bea0ac909f8a8eb3d2d16c7d7b7d5efa1bfc30877c485dbbd4ae4785afea8f1c'
 
-  # binary-factory.kde.org was verified as official when first introduced to the cask
-  url "https://binary-factory.kde.org/job/Kate_Release_macos/#{version.after_comma}/artifact/kate-#{version.before_comma}-#{version.after_comma}-macos-64-clang.dmg"
-  appcast 'https://binary-factory.kde.org/job/Kate_Release_macos/rssAll'
+  # binary-factory.kde.org/view/MacOS/job/Kate_Release_macos was verified as official when first introduced to the cask
+  url "https://binary-factory.kde.org/view/MacOS/job/Kate_Release_macos/lastSuccessfulBuild/artifact/kate-#{version}-macos-64-clang.dmg"
+  appcast 'https://binary-factory.kde.org/view/MacOS/job/Kate_Release_macos/rssAll'
   name 'Kate'
   homepage 'https://kate-editor.org/'
 
