470754adcea1a71952fec7ddf6968944dbe5faed
Vítor Galvão <info@vitorgalvao.com>
2018-12-24 16:17:06 +0000

    Update webplotdigitizer.rb

--- Casks/webplotdigitizer.rb
+++ Casks/webplotdigitizer.rb
@@ -1,8 +1,9 @@
 cask 'webplotdigitizer' do
-  version '4.0.0'
+  version '4.1'
   sha256 '76718f04d95aadcc5e41e4d1e7e7a602aa82ed165a49c28c48f50695ee376ee3'
 
-  url 'https://automeris.io/downloads/WebPlotDigitizer-4.1-darwin-x64.zip'
+  url "https://automeris.io/downloads/WebPlotDigitizer-#{version}-darwin-x64.zip"
+  appcast 'https://github.com/ankitrohatgi/WebPlotDigitizer/releases.atom'
   name 'WebPlotDigitizer'
   homepage 'https://automeris.io/WebPlotDigitizer/'
 
