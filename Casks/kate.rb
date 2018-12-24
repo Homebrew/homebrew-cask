b9b95fe81e617d361a310c799323a8ee273922d4
Vítor Galvão <info@vitorgalvao.com>
2018-12-24 21:22:46 +0000

    Update kate.rb

--- Casks/kate.rb
+++ Casks/kate.rb
@@ -1,6 +1,6 @@
 cask 'kate' do
   version '18.08.3-307'
-  sha256 'bea0ac909f8a8eb3d2d16c7d7b7d5efa1bfc30877c485dbbd4ae4785afea8f1c'
+  sha256 '1b589f7edbd7c0c8b06107dc9d44ad1ecadf8b708c4a8a040faf5c0c58d7288b'
 
   # binary-factory.kde.org/view/MacOS/job/Kate_Release_macos was verified as official when first introduced to the cask
   url "https://binary-factory.kde.org/view/MacOS/job/Kate_Release_macos/lastSuccessfulBuild/artifact/kate-#{version}-macos-64-clang.dmg"
