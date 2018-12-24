4fc4860c1a9bbd29c48b030fcb81b3d90cbda21b
Vítor Galvão <info@vitorgalvao.com>
2018-12-24 16:20:22 +0000

    Update frhelper.rb

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -1,9 +1,9 @@
 cask 'frhelper' do
-  version '3.8.7'
+  version '2018-11-24'
   sha256 '95c5e5d2ffcb91faf6f7a7259367afacca0bb0e9672a35b408bb6fc52e0ebc8d'
 
   # static.frdic.com was verified as official when first introduced to the cask
-  url 'https://static.frdic.com/pkg/fhmac.dmg?v=2018-11-23'
+  url "https://static.frdic.com/pkg/fhmac.dmg?v=#{version}"
   name 'Frhelper'
   name '法语助手'
   homepage 'https://www.eudic.net/v4/fr/app/frhelper'
