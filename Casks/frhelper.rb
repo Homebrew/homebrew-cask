1027ca990d091aa328699a2fe9380af270532e2e
Qi Zhang <singularitti@outlook.com>
2018-12-24 01:41:50 -0500

    Add new cask frhelper

---
+++ Casks/frhelper.rb
@@ -0,0 +1,11 @@
+cask 'frhelper' do
+  version '3.8.7'
+  sha256 '95c5e5d2ffcb91faf6f7a7259367afacca0bb0e9672a35b408bb6fc52e0ebc8d'
+
+  url 'https://static.frdic.com/pkg/fhmac.dmg?v=2018-11-23'
+  name 'Frhelper'
+  name '法语助手'
+  homepage 'https://www.eudic.net/v4/fr/app/frhelper'
+
+  app 'Frhelper.app'
+end
