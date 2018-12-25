0be08080ada88b1d6477bcafce913d1bf3ca073f
sequentiality <sequentiality@gmail.com>
2018-12-25 15:37:55 +0200

    Update freac to 1.1-alpha-20181201a (#56621)

--- Casks/freac.rb
+++ Casks/freac.rb
@@ -1,6 +1,6 @@
 cask 'freac' do
-  version '1.1-alpha-20180913'
-  sha256 '592e05335c22a49ea5ee21d4a9c622f68f1efcd1a39835f019e0e2c4f0e200d1'
+  version '1.1-alpha-20181201a'
+  sha256 'dc9053232c0336f789ab57776a69ae4718e98d4fcf53afac1b0e1fefd35a0c76'
 
   # sourceforge.net/bonkenc was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/bonkenc/freac-#{version}-macosx.dmg"
