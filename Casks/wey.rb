8705d35ce6e554537a9eb74bb3e9893941ae6a82
Udo Kramer <optik@fluffel.io>
2018-12-25 16:29:39 +0100

    Update wey to 0.3.3 (#56627)

--- Casks/wey.rb
+++ Casks/wey.rb
@@ -1,6 +1,6 @@
 cask 'wey' do
-  version '0.3.2'
-  sha256 '5d19562b626fa7a3286e8870ac16659b357886526781408f0e18cce73d5e3ebb'
+  version '0.3.3'
+  sha256 '56f9a35eafea46e43bc51f1e4fd8fa1fac7c5aa4df19ad29b47c273d24baeb4d'
 
   url "https://github.com/yue/wey/releases/download/v#{version}/wey-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/yue/wey/releases.atom'
