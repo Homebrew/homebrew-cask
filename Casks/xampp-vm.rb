baf4d5aa0ad9fe98708964c8bb1be99d77329e4a
Vítor Galvão <info@vitorgalvao.com>
2018-12-25 13:44:34 +0000

    Update xampp-vm.rb

--- Casks/xampp-vm.rb
+++ Casks/xampp-vm.rb
@@ -1,10 +1,9 @@
 cask 'xampp-vm' do
-  version '7.3.0'
+  version '7.3.0-0'
   sha256 '499c5f5258de58f79e742a77f69ef662a04918b30b7a38471a5ee8e6d2f449f1'
 
-  url "https://www.apachefriends.org/xampp-files/#{version}/xampp-osx-#{version}-0-vm.dmg"
-  name 'XAMPP-VM for OS X'
-  name 'XAMPP'
+  url "https://www.apachefriends.org/xampp-files/#{version.major.minor.patch}/xampp-osx-#{version}-vm.dmg"
+  name 'XAMPP-VM'
   homepage 'https://www.apachefriends.org/index.html'
 
   app 'XAMPP.app'
