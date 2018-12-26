92364f1c949855b81964c49550ab92adddebbbc7
brigitte zmoelnig <brigitte@corecode.io>
2018-12-26 22:51:09 +0100

    Update fastonosql to 1.25.1

--- Casks/fastonosql.rb
+++ Casks/fastonosql.rb
@@ -1,6 +1,6 @@
 cask 'fastonosql' do
-  version '1.25.0'
-  sha256 'e61e25c9bdd0b25c4824db327677fa403d8cf1df3be7518014d3b91efa1d4253'
+  version '1.25.1'
+  sha256 'bbbfdfb580839c43d9cb646714726fce1e8ca2d91f15dfc02479c5b573541dc9'
 
   url "https://fastonosql.com/downloads_pro/macosx/fastonosql_pro-#{version}-x86_64.dmg"
   appcast 'https://github.com/fastogt/fastonosql/releases.atom'
