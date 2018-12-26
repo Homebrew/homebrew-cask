182785df7d1b086ca5e1d2f10d2a7bc4cd8c3ccf
Vítor Galvão <info@vitorgalvao.com>
2018-12-26 20:11:52 +0000

    Update psychopy.rb

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,11 +1,11 @@
 cask 'psychopy' do
-  version '3.0.0b13'
-  sha256 '94656b12e55bbbfa4529ddf36bfac4da3ef7a4ca611b3cd76fa9e23a6eb773e3'
+  version '1.90.3'
+  sha256 '7e1dfac94c39bd2a8fa5489e5e856b73add5d23a1fd381ce0c60e2478e0f6b55'
 
-  url "https://github.com/psychopy/psychopy/releases/download/3.0.0b13/StandalonePsychoPy3-#{version}-MacOS.dmg"
+  url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy2_PY3-#{version}-MacOS.dmg"
   appcast 'https://github.com/psychopy/psychopy/releases.atom'
   name 'PsychoPy'
   homepage 'https://github.com/psychopy/psychopy'
 
-  app 'PsychoPy3.app'
+  app 'PsychoPy2_PY3.app'
 end
