f3b81653efc2a9a85d3945ea4abb4884a2de038f
Brett Dikeman <brett.dikeman@gmail.com>
2018-12-26 15:17:14 -0500

    Update manuskript to 0.8.0 (#56666)

--- Casks/manuskript.rb
+++ Casks/manuskript.rb
@@ -1,6 +1,6 @@
 cask 'manuskript' do
-  version '0.7.0-3'
-  sha256 'f82046d9b41eb4604edd1cf9d3b816a3c54b54f9ac749ddafc9ddefe3714232a'
+  version '0.8.0'
+  sha256 'cd48f796945188c89e34b6e789bfcad7753a55ccd99a64fc5f0c92aaaebba0a9'
 
   # github.com/olivierkes/manuskript was verified as official when first introduced to the cask
   url "https://github.com/olivierkes/manuskript/releases/download/#{version.major_minor_patch}/manuskript-#{version}-osx.zip"
