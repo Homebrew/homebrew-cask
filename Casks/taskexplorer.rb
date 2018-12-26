0ed70e162ed71123f6f401cd306f25681825678b
brigitte zmoelnig <brigitte@corecode.io>
2018-12-26 19:01:26 +0100

    Update taskexplorer to 2.0.1

--- Casks/taskexplorer.rb
+++ Casks/taskexplorer.rb
@@ -1,6 +1,6 @@
 cask 'taskexplorer' do
-  version '2.0.0'
-  sha256 '7637f5011048dc3c5bd8dfd82220bc3ffdac00f024cd2f58bd8b4880229e52f5'
+  version '2.0.1'
+  sha256 '87867920e2d3bde91df9ebd7e416c20be997dfa707471b8b32732bd856de85fd'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/TaskExplorer_#{version}.zip"
