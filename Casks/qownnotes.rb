451b05676a197b0a99494b305d374214d46e7895
suschizu <brigitte@corecode.io>
2018-12-26 21:15:11 +0100

    Update qownnotes to 18.12.6,b4005-123725 (#56649)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '18.12.5,b3994-112006'
-  sha256 'f4a1932edf49229b91d40871deafa9e6f6c48160e0f5d66c10fe40fea9eb66b1'
+  version '18.12.6,b4005-123725'
+  sha256 '3914d4fdd6ed2e847837e00abc67e10264adf8c846b8c68072d596e8148dcb75'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
