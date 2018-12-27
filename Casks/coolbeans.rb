ad63ffc03a29eb6d9aeae14f15673425d3bdca4f
Tushar Joshi <tusharvjoshi@gmail.com>
2018-12-27 15:57:08 +0530

    Add CoolBeans 2018.12.app v2018.12

---
+++ Casks/coolbeans.rb
@@ -0,0 +1,10 @@
+cask 'coolbeans' do
+  version '2018.12'
+  sha256 '68df6bc7ff63598664e3526492aaf0489ba6a0b0ccf7ac99f726763dd6d1f564'
+
+  url 'https://paris.download.coolbeans.xyz/download/CoolBeans-2018.12.dmg'
+  name 'CoolBeans 2018.12'
+  homepage 'https://coolbeans.xyz/'
+
+  app 'CoolBeans 2018.12.app'
+end
