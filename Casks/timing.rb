8b7924c183da698babec8674711c1c64676e4ded
CoreCode <core-code@users.noreply.github.com>
2018-12-27 18:06:38 +0100

    Update timing to 2018.4 (#56704)

--- Casks/timing.rb
+++ Casks/timing.rb
@@ -1,5 +1,5 @@
 cask 'timing' do
-  version '2018.3.4'
+  version '2018.4'
   sha256 '2bbd41ec5985fc766a85c49edeabccd5206f77662d7bbc94765b75c4ca21f8cf'
 
   url 'https://timingapp.com/download/Timing.app.zip'
