1175d30df57d45485fbc6de9ffc85d020e53791f
suschizu <brigitte@corecode.io>
2018-12-28 21:49:48 +0100

    Update electorrent.rb

--- Casks/electorrent.rb
+++ Casks/electorrent.rb
@@ -1,6 +1,6 @@
 cask 'electorrent' do
-  version '2.4.0'
-  sha256 '95ebe44128aea36c7ecc15986bf50b4181087d0afb5b66ba3dba68b64b49bb3e'
+  version '2.5.0'
+  sha256 '1be6cdd596f7cda31526863c0fac8cc1cb46f1e66df0f1ef23c47ea119a7b0dc'
 
   url "https://github.com/Tympanix/Electorrent/releases/download/v#{version}/electorrent-#{version}.dmg"
   appcast 'https://github.com/Tympanix/Electorrent/releases.atom'
