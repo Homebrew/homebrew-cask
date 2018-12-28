2977f4363c8f8e4f54c7a412ccce22a10d3283eb
Vítor Galvão <info@vitorgalvao.com>
2018-12-28 21:19:55 +0000

    Update frhelper.rb

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -4,7 +4,7 @@ cask 'frhelper' do
 
   # static.frdic.com was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/fhmac.dmg?v=#{version.after_comma}"
-  appcast 'http://www.francochinois.com/update/frhelper_mac.xml'
+  appcast 'https://www.francochinois.com/update/frhelper_mac.xml'
   name 'Frhelper'
   name '法语助手'
   homepage 'https://www.eudic.net/v4/fr/app/frhelper'
