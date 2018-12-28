17f0f2f4a22380c665d58c9b5066c83a92642d39
Alex Alonso <alex4551@gmail.com>
2018-12-28 11:22:17 +0800

    update angular-console version 7.1.0

--- Casks/angular-console.rb
+++ Casks/angular-console.rb
@@ -1,6 +1,6 @@
 cask 'angular-console' do
-  version '7.0.0'
-  sha256 '7a3d35016e70e9ea6f820d4f0fc07f20a16af00ce08a127b45ce0294fedb9de2'
+  version '7.1.0'
+  sha256 '6512df0ca387546ce3c2b30451849a00caf7a7231406ab0bd3917fe88adaeffe'
 
   # github.com/nrwl/angular-console was verified as official when first introduced to the cask
   url "https://github.com/nrwl/angular-console/releases/download/v#{version}/AngularConsole-#{version}.dmg"
