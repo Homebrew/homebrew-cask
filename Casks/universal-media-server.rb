2ac076646ed82000109de8829f4cadd6b4c45e14
suschizu <brigitte@corecode.io>
2018-12-29 22:36:56 +0100

    Update universal-media-server to 7.7.1 (#56783)

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -1,6 +1,6 @@
 cask 'universal-media-server' do
-  version '7.7.0'
-  sha256 '52cb5264863304c890142ef9bfaff5ed5188694921fb33778fec03c09aa172dd'
+  version '7.7.1'
+  sha256 '28c7e112f3d86fac1677cef62438550d8caaa1f725b3a6657b742b2d206167a7'
 
   # sourceforge.net/unimediaserver was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/unimediaserver/Official%20Releases/OS%20X/UMS-#{version}.dmg"
