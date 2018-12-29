e93c69a31ed331680538cfb6182b3db1198bea9d
Vítor Galvão <info@vitorgalvao.com>
2018-12-29 21:55:58 +0000

    Update slite.rb

--- Casks/slite.rb
+++ Casks/slite.rb
@@ -2,7 +2,8 @@ cask 'slite' do
   version '1.0.17'
   sha256 'db0f53b3718dc8e832f14e02cc3fa480dd7d48698abec4508609bdb063e7c933'
 
-  url 'https://slite.com/api/desktop/download?platform=mac'
+  # storage.googleapis.com/slite-desktop was verified as official when first introduced to the cask
+  url "https://storage.googleapis.com/slite-desktop/mac/Slite-#{version}.dmg"
   name 'Slite'
   homepage 'https://slite.com/'
 
