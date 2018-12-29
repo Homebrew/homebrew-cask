c504cfeee11230454276c87fb8634b5514e40417
Thomas Berdy <thomas.berdy@outlook.com>
2018-12-29 09:31:20 +0100

    Add Slite.app v1.0.17

---
+++ Casks/slite.rb
@@ -0,0 +1,10 @@
+cask 'slite' do
+  version '1.0.17'
+  sha256 'db0f53b3718dc8e832f14e02cc3fa480dd7d48698abec4508609bdb063e7c933'
+
+  url 'https://slite.com/api/desktop/download?platform=mac'
+  name 'Slite'
+  homepage 'https://slite.com/'
+
+  app 'Slite.app'
+end
