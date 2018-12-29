ffe4ec35050ae2e2514668aaa4d05faedebf9797
CoreCode <core-code@users.noreply.github.com>
2018-12-29 01:04:53 +0100

    Update pdfsam-basic.rb

--- Casks/pdfsam-basic.rb
+++ Casks/pdfsam-basic.rb
@@ -9,6 +9,6 @@ cask 'pdfsam-basic' do
   homepage 'https://www.pdfsam.org/'
 
   pkg "PDFsam Basic-#{version}.pkg"
-  
-  uninstall pkgutil: 'org.pdfsam.basic'  
+
+  uninstall pkgutil: 'org.pdfsam.basic'
 end
