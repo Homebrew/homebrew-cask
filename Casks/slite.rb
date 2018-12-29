7b5f146be6080a7ed2fce87f6f370b6d6da090cd
Thomas Berdy <thomas.berdy@outlook.com>
2018-12-29 10:07:45 +0100

    Add Slite.app v1.0.17
    Update slite.rb to add zap behavior

--- Casks/slite.rb
+++ Casks/slite.rb
@@ -7,4 +7,13 @@ cask 'slite' do
   homepage 'https://slite.com/'
 
   app 'Slite.app'
+
+  zap trash: [
+               '~/Library/Application Support/Slite',
+               '~/Library/Caches/com.slite.desktop',
+               '~/Library/Caches/com.slite.desktop.ShipIt',
+               '~/Library/Preferences/com.slite.desktop.helper.plist',
+               '~/Library/Preferences/com.slite.desktop.plist',
+               '~/Library/Saved Application State/com.slite.desktop.savedState',
+             ]
 end
