a6b6092b25a33e5b058da73749a9b7a41f947191
zhangshaolong <zhangsl@rentongtech.com>
2018-12-29 14:07:52 +0800

    Update foxitreader to 3.0.1

--- Casks/foxitreader.rb
+++ Casks/foxitreader.rb
@@ -1,12 +1,12 @@
 cask 'foxitreader' do
-  version '2.4.4.0903'
-  sha256 '18d32ad8a01879aedc54c190df13cdb48a2932aa565fea12e242d087800c3e5f'
+  version '3.0.1'
+  sha256 '01697a12a46a30135eaba3dcaa8b487005d35dda7eeb67634efdb8427a82953c'
 
-  url "https://cdn09.foxitsoftware.com/pub/foxit/reader/desktop/mac/#{version.major}.x/#{version.major_minor}/en_us/FoxitReader.#{version}.enu.setup.pkg"
+  url "https://cdn09.foxitsoftware.com/pub/foxit/reader/desktop/mac/#{version.major}.x/#{version.major_minor}/en_us/FoxitReader#{version.no_dots}.enu.setup.pkg"
   name 'Foxit Reader'
   homepage 'https://www.foxitsoftware.com/pdf-reader/'
 
-  pkg "FoxitReader.#{version}.enu.setup.pkg"
+  pkg "FoxitReader#{version.no_dots}.enu.setup.pkg"
 
   uninstall pkgutil: 'com.foxitsoftware.reader.pkg'
 end
