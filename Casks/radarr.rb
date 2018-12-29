071ea359bdde87fb46f16f1ff734a4db55a198b2
Julian Mayer <julian@corecode.io>
2018-12-29 13:48:20 +0100

    Update radarr to 0.2.0.1217

--- Casks/radarr.rb
+++ Casks/radarr.rb
@@ -1,9 +1,9 @@
 cask 'radarr' do
-  version '0.2.0.1120'
-  sha256 'dad152e1e345654e3a4c6ffb03a451987d7353070635bd17dfc312c4cea3460a'
+  version '0.2.0.1217'
+  sha256 '6fceb4e578263cae8e25dd8122c2a15351de8aa62c02f7ec01e654ada09a6837'
 
   # github.com/Radarr/Radarr was verified as official when first introduced to the cask
-  url "https://github.com/Radarr/Radarr/releases/download/v#{version}/Radarr.develop.#{version}.osx-app.zip"
+  url "https://github.com/Radarr/Radarr/releases/download/v#{version}/Radarr.v#{version}.osx-app.zip"
   appcast 'https://github.com/Radarr/Radarr/releases.atom'
   name 'Radarr'
   homepage 'https://radarr.video/'
