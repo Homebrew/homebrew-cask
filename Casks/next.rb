76a3ea92ce035b6d1971d3dafc07989c081fcef8
Sean Escriva <sean.escriva@gmail.com>
2018-12-30 17:56:40 -0500

    Update cask for Next browser 1.2.0 (#56809)
    Next browser appears to have moved from https://github.com/nEXT-browser/ to
    https://github.com/atlas-engineer/next
    
    As a result the recipe is currently broken as is. This updates it to at least be
    functional.
    
    I determined the version number from
    https://github.com/atlas-engineer/next/blob/master/documents/CHANGELOG.org but
    it does not appear that they offer a version stamped release dmg file.

--- Casks/next.rb
+++ Casks/next.rb
@@ -1,11 +1,11 @@
 cask 'next' do
-  version '0.08'
-  sha256 '5d2feef32815f79e776bb7db543bc2e8e7bfd7e5ecdefa4e92c1c0d846b1db8a'
+  version '1.2.0 '
+  sha256 '286e9af278797db6c41bbc9efd5c97e6d249734b4bca5df687ebaa481b35dc7b'
 
-  url "https://github.com/next-browser/next/releases/download/#{version}/Next.dmg"
-  appcast 'https://github.com/next-browser/next/releases.atom'
+  url 'https://next.atlas.engineer/static/release/next-macos-webkit.dmg'
+  appcast 'https://github.com/atlas-engineer/next/releases.atom'
   name 'Next Browser'
-  homepage 'https://github.com/next-browser/next'
+  homepage 'https://next.atlas.engineer/'
 
   app 'Next.app'
 
