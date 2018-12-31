f3ff03ee3ba0b3f43fa9900dcaddd079b9d8b90e
soe-j <soeda@val.co.jp>
2018-12-31 20:55:03 +0900

    Add Kopypasta

---
+++ Casks/kopypasta.rb
@@ -0,0 +1,10 @@
+cask 'kopypasta' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://kopypasta.com/updates/Kopypasta.dmg'
+  name 'kopypasta'
+  homepage 'http://kopypasta.com/'
+
+  app 'Kopypasta.app'
+end
