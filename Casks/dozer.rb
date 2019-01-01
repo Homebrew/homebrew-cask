c2ba9327d29165ffa6ec7fb3a91500b7acc2b56b
Mortennn <34214574+Mortennn@users.noreply.github.com>
2019-01-01 17:46:03 +0100

    Update the download URL

--- Casks/dozer.rb
+++ Casks/dozer.rb
@@ -2,7 +2,7 @@ cask 'dozer' do
   version '3.0.3'
   sha256 '370909940cc2187848467405fb3d0a65a2280cffcc666a9883ea63516bdb274c'
 
-  url "https://github.com/Mortennn/Dozer/releases/download/#{version}/Dozer.#{version}.dmg"
+  url "https://github.com/Mortennn/Dozer/releases/download/v#{version}/Dozer.#{version}.dmg"
   appcast 'https://github.com/Mortennn/Dozer/releases.atom'
   name 'Dozer'
   homepage 'https://github.com/Mortennn/Dozer'
