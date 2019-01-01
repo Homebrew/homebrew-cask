5c22d63eab2427cdf116a98e3ee27698f6e295c9
Hsiao-nan Cheung <niheaven@gmail.com>
2019-01-01 13:30:42 +0800

    Shorten Caveats

--- Casks/miniconda.rb
+++ Casks/miniconda.rb
@@ -26,29 +26,9 @@ cask 'miniconda' do
     files_in_usr_local
 
     <<~EOS
-      If your shell is Bash or a Bourne variant, enable conda for the current user with
+      To enable the 'conda' command for the current user, run
 
         $ echo ". #{HOMEBREW_PREFIX}/miniconda3/etc/profile.d/conda.sh" >> ~/.bash_profile
-
-      or, for all users, enable conda with
-
-        $ sudo ln -s #{HOMEBREW_PREFIX}/miniconda3/etc/profile.d/conda.sh /etc/profile.d/conda.sh
-
-      The options above will permanently enable the 'conda' command, but they do NOT
-      put conda's base (root) environment on PATH.  To do so, run
-
-        $ conda activate
-
-      in your terminal, or to put the base environment on PATH permanently, run
-
-        $ echo "conda activate" >> ~/.bash_profile
-
-      Previous to conda 4.4, the recommended way to activate conda was to modify PATH in
-      your ~/.bash_profile file.  You should manually remove the line that looks like
-
-        export PATH="#{HOMEBREW_PREFIX}/miniconda3/bin:$PATH"
-
-      ^^^ The above line should NO LONGER be in your ~/.bash_profile file! ^^^
     EOS
   end
 end
