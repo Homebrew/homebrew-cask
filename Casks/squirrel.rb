dd58996303bf1df82dc82dffa37a015125b3980e
Vítor Galvão <info@vitorgalvao.com>
2019-01-02 17:17:10 +0000

    Update squirrel.rb

--- Casks/squirrel.rb
+++ Casks/squirrel.rb
@@ -13,15 +13,14 @@ cask 'squirrel' do
 
   uninstall pkgutil: [
                        'im.rime.inputmethod.Squirrel',
-                       # package name of older versions (<0.10.0)
-                       'com.googlecode.rimeime.Squirrel.pkg',
+                       'com.googlecode.rimeime.Squirrel.pkg', # Package name of older versions (< 0.10.0)
                      ],
             delete:  '/Library/Input Methods/Squirrel.app'
 
   zap trash: [
                '~/Library/Caches/im.rime.inputmethod.Squirrel',
                '~/Library/Preferences/im.rime.inputmethod.Squirrel.plist',
-               # also remove data for older versions
+               # Data for older versions (< 0.10.0)
                '~/Library/Caches/com.googlecode.rimeime.inputmethod.Squirrel',
                '~/Library/Preferences/com.googlecode.rimeime.inputmethod.Squirrel.plist',
              ]
