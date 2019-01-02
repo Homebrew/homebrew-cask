d6440207547c4397e62d533e4271f732886c12e8
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-01-02 07:16:09 +0100

    Add rambox 0.6.3 zap

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -1,6 +1,6 @@
 cask 'rambox' do
-  version '0.6.2'
-  sha256 'f83028d7875636aef457c9e5254846f84276551edaf74f6d283d846c5c2544c8'
+  version '0.6.3'
+  sha256 '9d8785554f688d7cacf9c32167ce15e2184dc2f2827b3adbbe4b66e7a0c6844f'
 
   # github.com/ramboxapp/community-edition was verified as official when first introduced to the cask
   url "https://github.com/ramboxapp/community-edition/releases/download/#{version}/Rambox-#{version}-mac.zip"
@@ -8,7 +8,22 @@ cask 'rambox' do
   name 'Rambox'
   homepage 'https://rambox.pro/'
 
+  depends_on macos: '>= :mavericks'
+
   app 'Rambox.app'
 
   uninstall login_item: 'Rambox'
+
+  zap trash: [
+               '~/Library/Caches/com.grupovrs.ramboxce',
+               '~/Library/Caches/com.grupovrs.ramboxce.ShipIt',
+               '~/Library/Caches/com.saenzramiro.rambox',
+               '~/Library/Logs/Rambox',
+               '~/Library/Preferences/com.grupovrs.ramboxce.helper.plist',
+               '~/Library/Preferences/com.grupovrs.ramboxce.plist',
+               '~/Library/Preferences/com.saenzramiro.rambox.helper.plist',
+               '~/Library/Preferences/com.saenzramiro.rambox.plist',
+               '~/Library/Saved Application State/com.grupovrs.ramboxce.savedState',
+               '~/Library/Saved Application State/com.saenzramiro.rambox.savedState',
+             ]
 end
