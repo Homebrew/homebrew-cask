540251a92634c200cf024af9533a86717fed2518
Alley <alley.ma@qq.com>
2019-01-03 01:16:15 +0800

    Update kugoumusic to 2.7.8 (#56858)

--- Casks/kugoumusic.rb
+++ Casks/kugoumusic.rb
@@ -1,6 +1,6 @@
 cask 'kugoumusic' do
-  version '2.7.4'
-  sha256 '30f4d6815fb6eade3d150b2f2fc47b60f16ef4f6fea6ab49378349e5f1f39416'
+  version '2.7.8'
+  sha256 '01d0c741b23b3cb4c7513f606b85c7f06236127877c8f2201dd368878ce99636'
 
   url "http://downmini.kugou.com/mac/Kugou_V#{version}.dmg"
   appcast 'http://download.kugou.com/mac.html'
