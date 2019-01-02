e944001546b4c479103b7e9762e8640f15cd994c
Derelle Redmond <Derelle.Redmond@gmail.com>
2019-01-02 12:15:25 -0500

    Update yourkit-java-profiler to 2018.04-b88 (#56854)

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask 'yourkit-java-profiler' do
-  version '2018.04-b87'
-  sha256 'd7cc446a83552f733a0c961efbb5050839fd0f6a5ac3d483c073502b7b829579'
+  version '2018.04-b88'
+  sha256 '29a985791e859407c22e9dadd6c069a42e4908d1f3c0e6759e5b778f91fff98d'
 
   url "https://www.yourkit.com/download/YourKit-JavaProfiler-#{version}.dmg"
   name 'YourKit Java Profiler'
