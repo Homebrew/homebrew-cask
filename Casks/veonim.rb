31d31ddd6ff711442fdb71e9a9e24cd542950dd5
Hong Minhee <hong.minhee@gmail.com>
2019-01-02 13:24:19 +0900

    Update veonim to 0.13.5

--- Casks/veonim.rb
+++ Casks/veonim.rb
@@ -1,6 +1,6 @@
 cask 'veonim' do
-  version '0.13.3'
-  sha256 'a4cf9d64b8bf39446160d223bd5265491e562e05cbc1c24afe591e4a1d8f5191'
+  version '0.13.5'
+  sha256 '43aaa40328a5aefe5e2f35f341da30e4dcd1fec1ae6554d650a2d463ef426d9b'
 
   url "https://github.com/veonim/veonim/releases/download/#{version}/veonim-#{version}-mac.zip"
   appcast 'https://github.com/veonim/veonim/releases.atom'
