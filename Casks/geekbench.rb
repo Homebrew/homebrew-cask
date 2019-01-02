7eef0ba78335675259ea2c5e3b53cdf76b9b7762
Yvo Mulder <ytbmulder@gmail.com>
2019-01-02 18:08:14 +0100

    Update geekbench to 4.3.2 (#56838)
    * Update geekbench to 4.3.2
    
    * Fix geekbench 4.3.2 appcast

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -1,20 +1,23 @@
 cask 'geekbench' do
   if MacOS.version <= :mavericks
-    version '3.4.1'
-    sha256 '9f2730472bba9fd39554290f465d37c32792debc5b20c9840efd1f79d40ca94c'
-
-    # cdn.primatelabs.com was verified as official when first introduced to the cask
-    url "https://cdn.primatelabs.com/Geekbench-#{version}-Mac.zip"
+    version '3.4.2'
+    sha256 '05e1b977a46648d38cf6c641be7ef34722200d0168a10d4372fca771ffa24e28'
   else
     version '4.3.2'
     sha256 '4f995a7e9f6059195ec4e585bc6e1c450e35ace0b72ca6978c446ced75dae534'
-
-    url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
-    appcast "https://www.primatelabs.com/appcast/geekbench#{version.major}.xml"
   end
 
+  url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
+  appcast "https://www.primatelabs.com/release/geekbench#{version.major}"
   name 'Geekbench'
   homepage 'https://www.geekbench.com/'
 
+  depends_on macos: '>= :snow_leopard'
+
   app "Geekbench #{version.major}.app"
+
+  zap trash: [
+               "~/Library/Caches/com.primatelabs.Geekbench#{version.major}",
+               "~/Library/Preferences/com.primatelabs.Geekbench#{version.major}.plist",
+             ]
 end
