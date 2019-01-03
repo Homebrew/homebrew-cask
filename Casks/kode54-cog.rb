f865b0b2465714cce45403374b7d4e3295c0e4d0
Ben <BenjaminHCCarr@users.noreply.github.com>
2019-01-03 11:08:42 -0600

    Remove errant whitespace in blank line
    Please squash

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,7 +1,7 @@
 cask 'kode54-cog' do
   version '0.08,fdab126cc'
   sha256 '648b1db1ba06125924ec4c8ef39dce4473d080d24f7da0f6a005834f03f5c0e8'
-  
+
   # losno.co/cog was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
   appcast 'https://balde.losno.co/cog/mercury.xml'
