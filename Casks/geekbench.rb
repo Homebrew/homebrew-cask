56828ba6c1f3118c19b407230bd91092f2efb383
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-01-03 07:05:42 +0100

    Update geekbench 4.3.2 appcast

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -2,13 +2,14 @@ cask 'geekbench' do
   if MacOS.version <= :mavericks
     version '3.4.2'
     sha256 '05e1b977a46648d38cf6c641be7ef34722200d0168a10d4372fca771ffa24e28'
+    appcast "https://www.primatelabs.com/release/geekbench#{version.major}"
   else
     version '4.3.2'
     sha256 '4f995a7e9f6059195ec4e585bc6e1c450e35ace0b72ca6978c446ced75dae534'
+    appcast "https://www.primatelabs.com/appcast/geekbench#{version.major}.xml"
   end
 
   url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
-  appcast "https://www.primatelabs.com/release/geekbench#{version.major}"
   name 'Geekbench'
   homepage 'https://www.geekbench.com/'
 
