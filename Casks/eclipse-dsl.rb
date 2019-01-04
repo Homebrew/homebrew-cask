191cf140deac6447af396dd4b95694f9ab641d7f
Loïc PÉRON <loic.peron@gmail.com>
2019-01-04 13:43:24 +0100

    Update eclipse-dsl to 4.10.0,2018-12:R

--- Casks/eclipse-dsl.rb
+++ Casks/eclipse-dsl.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-dsl' do
-  version '4.9.0,2018-09:R'
-  sha256 '5eb75b9e69e62d6af4d84c0eadf67ecf296efd0fd13adf0209889a96ce13c0c6'
+  version '4.10.0,2018-12:R'
+  sha256 '82958ebf1672c53e939885d1d4bd2135e342cb97e7b1e17f52fba1a03eb8afdc'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-dsl-#{version.after_comma.before_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-dsl-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for Java and DSL Developers'
   homepage 'https://eclipse.org/'
 
