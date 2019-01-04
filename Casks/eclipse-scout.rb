aff8e62e24877e3fe59920a921276246a3f51526
Loïc PÉRON <loic.peron@gmail.com>
2019-01-04 14:34:03 +0100

    Update eclipse-scout to 4.10.0,2018-12:R

--- Casks/eclipse-scout.rb
+++ Casks/eclipse-scout.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-scout' do
-  version '4.9.0,2018-09:R'
-  sha256 '0e2ba762853584bca66010ec118183e1ae45f625033fefb0b9b1f7d7f8a9bac3'
+  version '4.10.0,2018-12:R'
+  sha256 'b83525b7bc0ba2ff1b11d912f6ae8f10daf36db3785ce565025c634b220b1fc6'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-scout-#{version.after_comma.before_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-scout-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Scout Developers'
   homepage 'https://eclipse.org/'
 
