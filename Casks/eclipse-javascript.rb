5d6ffcdc3a442f2d1a297617311449d3a2f3fc9e
Loïc PÉRON <loic.peron@gmail.com>
2019-01-04 14:17:49 +0100

    Update eclipse-javascript to 4.10.0,2018-12:R

--- Casks/eclipse-javascript.rb
+++ Casks/eclipse-javascript.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-javascript' do
-  version '4.9.0,2018-09:R'
-  sha256 '5a6644cb046d9bfdeec07f60cd7f61656a9786743d78ade6087701db234dc80c'
+  version '4.10.0,2018-12:R'
+  sha256 '7494bb89966a6a508fbeae52624cb16152851a36b2243dd5e0f43adcdaf849c9'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-javascript-#{version.after_comma.before_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-javascript-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for JavaScript and Web Developers'
   homepage 'https://eclipse.org/'
 
