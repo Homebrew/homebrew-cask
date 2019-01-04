fdd141823153ae44fe8c762cddc1c87a841677f0
Markus Hubig <markus.hubig@dm.de>
2019-01-04 11:57:58 +0100

    Update notable to 1.1.0

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -1,6 +1,6 @@
 cask 'notable' do
-  version '1.0.1'
-  sha256 'f8fa62e01e02e5989525bf940814801e5b1745190f4dccf9f0c50f2a687fcf49'
+  version '1.1.0'
+  sha256 '175afd8da6d52a16571bf9e557d009554c8305d8f320d3472a8dbd3973f03068'
 
   url "https://github.com/fabiospampinato/notable/releases/download/v#{version}/Notable-#{version}-mac.zip"
   appcast 'https://github.com/fabiospampinato/notable/releases.atom'
