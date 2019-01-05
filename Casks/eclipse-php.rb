0be69c9a07d2ca91342c0212eb187e6b3af4e566
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2019-01-05 23:45:19 +0100

    Update eclipse-php to 4.10.0,2018-12:R (#56939)

--- Casks/eclipse-php.rb
+++ Casks/eclipse-php.rb
@@ -1,8 +1,8 @@
 cask 'eclipse-php' do
-  version '4.9.0,2018-09:R'
-  sha256 'e7508c5880a4e5feb88469ea1d451006c4be27ce08ea5700c6eef560c0d175e8'
+  version '4.10.0,2018-12:R'
+  sha256 'c83a2306c5b14f08b63601f0b807e33e6289fb868ab6736c8d98325d83f3dc6a'
 
-  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-php-#{version.after_comma.before_colon}-macosx-cocoa-x86_64.dmg&r=1"
+  url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-php-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for PHP Developers'
   homepage 'https://eclipse.org/'
 
