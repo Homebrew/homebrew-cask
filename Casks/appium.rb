157d38f966f542e4a8920065216c9d4dd211de46
brigitte zmoelnig <brigitte@corecode.io>
2019-01-05 09:42:44 +0100

    Update appium to 1.10.0

--- Casks/appium.rb
+++ Casks/appium.rb
@@ -1,9 +1,9 @@
 cask 'appium' do
-  version '1.8.2'
-  sha256 'd5351ca4259f34cd20c6d23685a3ce7f5b5492c44544c92feba793241875d56e'
+  version '1.10.0'
+  sha256 '4302162eed289c5533e1cf9c5a13e2513ca766143fd9262c027daccfb9b99ab1'
 
   # github.com/appium/appium-desktop was verified as official when first introduced to the cask.
-  url "https://github.com/appium/appium-desktop/releases/download/v#{version}/appium-desktop-#{version}-mac.zip"
+  url "https://github.com/appium/appium-desktop/releases/download/v#{version}/Appium-#{version}.dmg"
   appcast 'https://github.com/appium/appium-desktop/releases.atom'
   name 'Appium Desktop'
   homepage 'https://appium.io/'
