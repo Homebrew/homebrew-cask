9a928c9d53063410be11f3d7d05d32a6d66d51af
Julian Mayer <julian@corecode.io>
2019-01-05 23:13:39 +0100

    Update vpnsecure to 2.1.6

--- Casks/vpnsecure.rb
+++ Casks/vpnsecure.rb
@@ -1,8 +1,9 @@
 cask 'vpnsecure' do
-  version :latest
-  sha256 :no_check
+  version '2.1.6'
+  sha256 '85026f7408cb516f5605369bdf9cb84fe9ac7a367cc737d9b7753b83c16f9345'
 
-  url 'https://downloads.vpnsecure.me/vpnsecure.pkg'
+  url "https://downloads-us.vpnsecure.me/vpnsecure_#{version}.pkg"
+  appcast 'https://www.vpnsecure.me/support/download/vpn/macos/'
   name 'VPNSecure OpenVPN Encrypted VPN Client'
   homepage 'https://www.vpnsecure.me/'
 
