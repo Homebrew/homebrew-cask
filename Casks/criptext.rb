cd57c3bf104742d7b41e5bddf51567d7403c5c07
Eric Miller <eric.the.miller@icloud.com>
2019-01-04 20:22:36 -0600

    Adding Criptext 0.14.7

---
+++ Casks/criptext.rb
@@ -0,0 +1,17 @@
+cask 'criptext' do
+  version '0.14.7'
+  sha256 'b133b8344c7dd78381958a6d85bf3142eeed3cf64d2ebfc564393174613fb80c'
+
+  url "https://cdn.criptext.com/Criptext-Email-Desktop/mac/Criptext-#{version}.dmg"
+  appcast 'https://github.com/jkakavas/creepy/releases.atom'
+  name 'Criptext'
+  homepage 'https://criptext.com/'
+
+  app 'Criptext.app'
+
+  zap trash: [
+               '~/Library/Application Support/Criptext',
+               '~/Library/Logs/Criptext',
+               '~/Library/Saved Application State/com.criptext.criptextmail.savedState',
+             ]
+end
