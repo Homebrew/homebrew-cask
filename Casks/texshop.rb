7982ad4d8301edb0459cb3729a527b52b2a1fd60
Work <brigitte@corecode.io>
2019-01-05 23:09:48 +0100

    Update texshop to 4.22

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,6 +1,6 @@
 cask 'texshop' do
-  version '4.21'
-  sha256 'c94ea91c241e5544b8e298eacf433ad0164f103d5bba4da2fa5b76a16db4b61b'
+  version '4.22'
+  sha256 '36038deb4d2fb7c0b140f5f2e71e194ffc558fb02bb0a2538126cd13f3cec499'
 
   url "https://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.no_dots}.zip"
   appcast 'https://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml'
