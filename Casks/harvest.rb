6b194765aa4ba148317f9b633423efd3717fda94
suschizu <brigitte@corecode.io>
2019-01-05 23:41:08 +0100

    Update harvest to 2.1.13 (#56985)

--- Casks/harvest.rb
+++ Casks/harvest.rb
@@ -1,8 +1,8 @@
 cask 'harvest' do
-  version '2.1.12'
-  sha256 '6345cb40c55ca6cf51d5086b511fc976836e5d1dbb49e9f71f5b15284aa91bbd'
+  version '2.1.13'
+  sha256 'fb32ac6bf6c58e0edc514eeab38dcd60792af3fbf1064d62591cb1b032bcffe3'
 
-  url "https://www.getharvest.com/harvest/mac/Harvest.#{version}.zip"
+  url "https://www.getharvest.com//harvest/mac/Harvest-#{version}.zip"
   appcast 'https://www.getharvest.com/harvest/mac/appcast.xml'
   name 'Harvest'
   homepage 'https://www.getharvest.com/mac-time-tracking'
