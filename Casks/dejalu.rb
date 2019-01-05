efa8dc71ad55a6685b402a1c1c1176e0ebc0591d
brigitte zmoelnig <brigitte@corecode.io>
2019-01-05 09:47:18 +0100

    Update dejalu to 1.0,213

--- Casks/dejalu.rb
+++ Casks/dejalu.rb
@@ -1,6 +1,6 @@
 cask 'dejalu' do
-  version '1.0,192'
-  sha256 '35342ef3ceda90b4e58b74bd91af9cf7172119dc16f578e443b4bdc5a808ecd0'
+  version '1.0,213'
+  sha256 '527901fbaf9782caf5cded4132f856fa3922c5cba67323f4b29a9d32bcdc1c83'
 
   url "https://d.dejalu.me/dejalu-eKTEtTljm4/dejalu-#{version.after_comma}/DejaLu-#{version.after_comma}.zip"
   appcast 'https://d.dejalu.me/dejalu-eKTEtTljm4/sparkle.xml'
