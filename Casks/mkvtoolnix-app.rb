b2003ad64d700d9ffb429096970a5cd52f2c5295
Julian Mayer <julian@corecode.io>
2019-01-05 21:17:09 +0100

    Update mkvtoolnix-app to 30.1.0

--- Casks/mkvtoolnix-app.rb
+++ Casks/mkvtoolnix-app.rb
@@ -1,6 +1,6 @@
 cask 'mkvtoolnix-app' do
-  version '29.0.0'
-  sha256 '209578d5d25adb37a2cf857139afb35a421a64b104c2d59af0476d609037244d'
+  version '30.1.0'
+  sha256 '9c26cbf784cf06339d72c22b62ddcda1b868691526012befea04920834630280'
 
   url "https://mkvtoolnix.download/macos/MKVToolNix-#{version}.dmg"
   appcast 'https://www.bunkus.org/blog/feed/'
