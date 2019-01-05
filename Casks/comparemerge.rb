ceab6ec6b87247c71c26ae56ef4bd12986de6a9d
Julian Mayer <julian@corecode.io>
2019-01-05 23:14:24 +0100

    Update comparemerge to 2.10y

--- Casks/comparemerge.rb
+++ Casks/comparemerge.rb
@@ -1,6 +1,6 @@
 cask 'comparemerge' do
-  version '2.09y'
-  sha256 '5be37fca9fba28c38ab392ef33e80321617c6bfac541cfaaaa6a7406eb1d6a87'
+  version '2.10y'
+  sha256 'fa25244eba6609956fb2bbaff363f5829c453b99db08d89b3601e026f6a3c406'
 
   url "https://downloads.sourceforge.net/comparemergenosandbox/CompareMerge%20#{version}.zip"
   appcast 'https://sourceforge.net/projects/comparemergenosandbox/rss'
