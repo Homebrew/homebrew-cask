9148f4de18a7c384a673b0ee88f318a2f51775d1
CoreCode <core-code@users.noreply.github.com>
2019-01-06 01:32:15 +0100

    Update boom-recorder to 8.7.3 (#57054)

--- Casks/boom-recorder.rb
+++ Casks/boom-recorder.rb
@@ -1,6 +1,6 @@
 cask 'boom-recorder' do
   version '8.7.3'
-  sha256 '6356370b4462d3a3fa8d05a9e789284c50973f75f3cf2798ce582b54fc9bc1ed'
+  sha256 '2854f36ab9c341694335472da77fba83df19f64e4433240a43f051d5bddfb41a'
 
   url "http://www.vosgames.nl/media/downloads/Boom%20Recorder-#{version}.dmg"
   name 'Boom Recorder'
