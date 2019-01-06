d54bec8351c74ed1cde39a70a62e22f3ac36e792
Julian Mayer <julian@corecode.io>
2019-01-06 17:26:12 +0100

    Update jaxx to 2.1.1

--- Casks/jaxx.rb
+++ Casks/jaxx.rb
@@ -1,14 +1,13 @@
 cask 'jaxx' do
-  version '1.3.18'
-  sha256 'cd23115f58cd6779ca1c90507126de279caadb34f8358001e01e167ba35f62a2'
+  version '2.1.1'
+  sha256 'c2d9ad29629f547766bca8e0c963addda1c9c11786f2c2604fdbe24c0eebbf7d'
 
-  # github.com/Jaxx-io/Jaxx was verified as official when first introduced to the cask
-  url "https://github.com/Jaxx-io/Jaxx/releases/download/v#{version}/Jaxx-#{version}.dmg"
-  appcast 'https://github.com/Jaxx-io/Jaxx/releases.atom'
+  url "https://download-liberty.jaxx.io/Jaxx.Liberty-#{version}.dmg"
+  appcast 'https://jaxx.io/downloads.html'
   name 'Jaxx Blockchain Wallet'
   homepage 'https://jaxx.io/'
 
-  app 'Jaxx.app'
+  app 'Jaxx Liberty.app'
 
   zap trash: [
                '~/Library/Application Support/jaxx',
