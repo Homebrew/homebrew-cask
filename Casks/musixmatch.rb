62a5706103380b66bb295327ee37bc6cfa6c22de
CoreCode <core-code@users.noreply.github.com>
2019-01-06 20:15:37 +0100

    Update musixmatch to 0.21.13 (#57089)

--- Casks/musixmatch.rb
+++ Casks/musixmatch.rb
@@ -1,8 +1,8 @@
 cask 'musixmatch' do
-  version :latest
-  sha256 :no_check
+  version '0.21.13'
+  sha256 'b5c65f63a9c2a7cc52938ec202c489b116255995c5625382f4b5ed45ef20e77f'
 
-  url 'https://download-app.musixmatch.com/download/osx'
+  url "https://download-app.musixmatch.com/download/Musixmatch-#{version}.dmg"
   name 'Musixmatch'
   homepage 'https://www.musixmatch.com/'
 
