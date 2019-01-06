2b3d38cc91180e6663878741e99efb61ed6d3127
suschizu <brigitte@corecode.io>
2019-01-06 01:32:58 +0100

    Update anki to 2.1.8 (#57059)

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,6 +1,6 @@
 cask 'anki' do
-  version '2.1.7'
-  sha256 '15d151bcfa9cac3fcfdecfffef203346e9eff522f2e23e61a3f41048ab7f077e'
+  version '2.1.8'
+  sha256 'eb0876f4dce044b82f402e1a803360fe0940822b2f45d0e39c7700cad65bdf95'
 
   url "https://apps.ankiweb.net/downloads/current/anki-#{version}-mac.dmg"
   appcast 'https://apps.ankiweb.net/docs/changes.html'
