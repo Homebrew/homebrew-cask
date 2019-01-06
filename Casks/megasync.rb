87c5c5b441f589289dc1c1c664ee0a6da9a53bb0
Hsiao-nan Cheung <niheaven@gmail.com>
2019-01-06 14:54:12 +0800

    Update MEGAsync to 3.7.1, add appcast and zap trash

--- Casks/megasync.rb
+++ Casks/megasync.rb
@@ -1,15 +1,16 @@
 cask 'megasync' do
-  version :latest
-  sha256 :no_check
+  version '3.7.1.0'
+  sha256 'a808259d6f93be450b7e874b28329df6a3dad4526399331c7f5720da539c338e'
 
   url 'https://mega.nz/MEGAsyncSetup.dmg'
+  appcast 'https://github.com/meganz/MEGAsync/releases.atom'
   name 'MEGAsync'
   homepage 'https://mega.nz/'
 
   app 'MEGAsync.app'
 
-  caveats <<~EOS
-    #{token} only works if called from /Applications, so you may need to install it with
-      brew cask install --appdir=/Applications #{token}
-  EOS
+  zap trash: [
+               '~/Library/Caches/mega.mac',
+               '~/Library/Preferences/mega.mac.plist',
+             ]
 end
