9c81e7dbac3b27de8a70ab0f5c83da97c5a609b5
Vítor Galvão <info@vitorgalvao.com>
2019-01-06 00:30:59 +0000

    Update tankix.rb

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -2,9 +2,9 @@ cask 'tankix' do
   version 'andromeda2-33928'
   sha256 '71917f11db0719a805d6a3e4dcb4caeb0cf88f11af2ee6ab6dc4a59ec5a5c895'
 
-  url "http://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
+  url "https://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
   name 'Tanki X'
-  homepage 'https://www.tankix.com/'
+  homepage 'https://tankix.com/'
 
   auto_updates true
 
