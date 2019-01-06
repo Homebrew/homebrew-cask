a0e9e9e461258ce58f1b1a6e24459524bc969064
Julian Mayer <julian@corecode.io>
2019-01-06 01:30:34 +0100

    Update lytro to 4.3.3,151118.98

--- Casks/lytro.rb
+++ Casks/lytro.rb
@@ -2,8 +2,8 @@ cask 'lytro' do
   version '4.3.3,151118.98'
   sha256 '500b28ed7d67aaf18a59a8770e3fb83a4b96e692728ddbd522a52879af758966'
 
-  # amazonaws.com/lytro-distro was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/lytro-distro/lytro-#{version.after_comma}.dmg"
+  # optics.miloush.net was verified as official when first introduced to the cask
+  url "http://optics.miloush.net/lytro/TheResources/Archive/LytroDesktop/#{version.before_comma}/lytro-#{version.after_comma}.dmg"
   appcast 'http://optics.miloush.net/lytro/TheResources.aspx'
   name 'Lytro Desktop'
   homepage 'https://www.lytro.com/'
