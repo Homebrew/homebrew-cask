ac94353349f1d797d5fb83e7990eac72ad550fe4
Scott Colby <scolby33@gmail.com>
2019-01-05 21:09:54 -0500

    Create intelliscape-caffeine.rb as the forked version of the cask.
    Restore original caffeine.rb.
    
    Add conflicts_with annotation to intellisense-caffeine cask

--- Casks/caffeine.rb
+++ Casks/caffeine.rb
@@ -1,12 +1,11 @@
 cask 'caffeine' do
-  version '1.1.2'
-  sha256 '365367f8b3314ffabc1d821f996943755ebc4f90f5ca1433f3123ddb62e1835e'
+  version '1.1.1'
+  sha256 '9203c30951f9aab41ac294bbeb1dcef7bed401ff0b353dcb34d68af32ea51853'
 
-  # cl.ly/2d0e01d909e4 was verified as official when first introduced to the cask
-  url 'https://cl.ly/2d0e01d909e4/download/Caffeine.dmg'
-  appcast 'https://intelliscapesolutions.com/apps/caffeine/releasenotes'
+  url "http://lightheadsw.com/files/releases/com.lightheadsw.Caffeine/Caffeine#{version}.zip"
+  appcast 'http://lightheadsw.com/caffeine/'
   name 'Caffeine'
-  homepage 'https://intelliscapesolutions.com/apps/caffeine'
+  homepage 'http://lightheadsw.com/caffeine/'
 
   app 'Caffeine.app'
 
