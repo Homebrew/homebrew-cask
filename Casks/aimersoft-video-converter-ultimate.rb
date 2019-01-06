28230639e91f1ab59255b70199d025b904b6672d
CoreCode <core-code@users.noreply.github.com>
2019-01-06 18:47:04 +0100

    Update aimersoft-video-converter-ultimate to 747 (#57084)

--- Casks/aimersoft-video-converter-ultimate.rb
+++ Casks/aimersoft-video-converter-ultimate.rb
@@ -1,6 +1,6 @@
 cask 'aimersoft-video-converter-ultimate' do
   version '747'
-  sha256 'ac2911537a191d87e94ca4187986e1bdf29a4c1a7cf8815fcef7c3dbe41b74fe'
+  sha256 'fd7b68b6c6d1ba074e7ef01cbd203b60363070fda6622f50a33e1cbec1090f59'
 
   url "http://download.aimersoft.com/cbs_down/aimer-mac-video-converter-ultimate_full#{version}.dmg"
   name 'Aimersoft Video Converter Ultimate'
