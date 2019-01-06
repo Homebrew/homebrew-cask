ebd1ca84a658195d6aaa130dfb13bad986308b47
Vítor Galvão <info@vitorgalvao.com>
2019-01-06 00:41:30 +0000

    Update kopypasta.rb

--- Casks/kopypasta.rb
+++ Casks/kopypasta.rb
@@ -1,8 +1,9 @@
 cask 'kopypasta' do
-  version :latest
-  sha256 :no_check
+  version '1.4'
+  sha256 'c00eeb9e976cd9963919b7755d1233b69e01270398c074d4d8611994aab9daa0'
 
   url 'http://kopypasta.com/updates/Kopypasta.dmg'
+  appcast 'http://kopypasta.com/updates/update.rss'
   name 'kopypasta'
   homepage 'http://kopypasta.com/'
 
