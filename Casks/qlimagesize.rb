ed69ec5c768414480f6e276a14234b71f39dcdeb
Licardo <1014660822@qq.com>
2019-01-07 18:13:02 +0800

    add qlImageSize.qlgenerator v1.6.0

---
+++ Casks/qlimagesize.rb
@@ -0,0 +1,10 @@
+cask 'qlimagesize' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/L1cardo/qlImageSize/releases/download/1.6.0/qlImageSize.qlgenerator.zip'
+  name 'qlImageSize'
+  homepage 'https://github.com/L1cardo/qlImageSize'
+
+  qlplugin 'qlImageSize.qlgenerator'
+end
