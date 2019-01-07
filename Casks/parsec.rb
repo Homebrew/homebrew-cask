a773ebf890ef95f7dbdb153197872870a98e5b0c
Tobias "Knight" S <tobias@tobiassachs.de>
2019-01-07 02:28:24 +0100

    update parsec (latest) URL to use Dual-Stack

--- Casks/parsec.rb
+++ Casks/parsec.rb
@@ -3,7 +3,7 @@ cask 'parsec' do
   sha256 :no_check
 
   # s3.amazonaws.com/parsec-build was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/parsec-build/package/parsec-macos.pkg'
+  url 'https://s3.dualstack.us-east-1.amazonaws.com/parsec-build/package/parsec-macos.pkg'
   name 'Parsec'
   homepage 'https://parsecgaming.com/'
 
