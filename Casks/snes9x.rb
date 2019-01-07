30df37e5da5ed3113d6c3e83a2b99ed88ded1e15
suschizu <brigitte@corecode.io>
2019-01-07 17:57:15 +0100

    Update snes9x.rb

--- Casks/snes9x.rb
+++ Casks/snes9x.rb
@@ -1,11 +1,11 @@
 cask 'snes9x' do
-  version '1.58,i386'
+  version '1.58'
   sha256 '4723eac4aee3774436d422fbf74206386f56fcc308954414b2ba7c29ac7a1d4f'
 
   # s9x-w32.de was verified as official when first introduced to the cask
-  url "http://www.s9x-w32.de/dl/snes9x-#{version.before_comma}-macosx-#{version.after_comma}.zip"
+  url "http://www.s9x-w32.de/dl/snes9x-#{version}-macosx-i386.zip"
   name 'Snes9x'
   homepage 'http://www.snes9x.com/'
 
-  app 'Snes9x.app'
+  app 'snes9x-#{version}-macosx-i386/Snes9x.app'
 end
