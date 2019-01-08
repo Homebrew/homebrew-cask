353b5061bb701e48ff84ba00ead89869ee6ec793
Work <brigitte@corecode.io>
2019-01-08 21:21:56 +0100

    Update mimolive to 4.6-26922

--- Casks/mimolive.rb
+++ Casks/mimolive.rb
@@ -1,6 +1,6 @@
 cask 'mimolive' do
-  version '4.5.1-26768'
-  sha256 '38a25ff5bf5178ba37f251ff9efb32da61cd38ea4c4411b80fc7c56ab6b1d35e'
+  version '4.6-26922'
+  sha256 '9f7f60e6c660b5064d0bac37d84c767c8575345a7ce19c74d4a099638ef3206e'
 
   url "https://cdn.boinx.com/software/mimolive/Boinx_mimoLive_#{version}.app.zip"
   appcast 'https://sparkle.boinx.com/appcast.lasso?appName=mimoLive'
