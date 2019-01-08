37a49e2f1ce61d195bcde53f105f4929555dfd6f
Work <brigitte@corecode.io>
2019-01-08 19:10:29 +0100

    Update blockstack to 0.35.3

--- Casks/blockstack.rb
+++ Casks/blockstack.rb
@@ -1,6 +1,6 @@
 cask 'blockstack' do
-  version '0.35.2'
-  sha256 'bc66cc1be9ff68a739f21e6915ac34c06f06f3c6e796f514c38c5c3c68bce08e'
+  version '0.35.3'
+  sha256 'f95a23ac371d9071c57ee45cfede922d5191a54df011d0e9d5ca41babdbd0494'
 
   # github.com/blockstack/blockstack-browser was verified as official when first introduced to the cask
   url "https://github.com/blockstack/blockstack-browser/releases/download/v#{version}/Blockstack-for-macOS-v#{version}.dmg"
