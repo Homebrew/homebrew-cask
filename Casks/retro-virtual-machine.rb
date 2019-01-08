ba0cef10eac79d31212e67c659ecf3cb7c6b224a
suschizu <brigitte@corecode.io>
2019-01-08 21:44:08 +0100

    Update retro-virtual-machine.rb

--- Casks/retro-virtual-machine.rb
+++ Casks/retro-virtual-machine.rb
@@ -3,9 +3,9 @@ cask 'retro-virtual-machine' do
   sha256 '49ea57688d0afbe63cc886606067f400c0e827e60db9c781e2f8c93cc993d139'
 
   url "http://static1.retrovirtualmachine.org/release/Retro%20Virtual%20Machine%20v#{version}.dmg"
-  appcast 'http://www.retrovirtualmachine.org/en/changelog'
+  appcast 'https://www.retrovirtualmachine.org/en/downloads'
   name 'Retro Virtual Machine'
-  homepage 'http://www.retrovirtualmachine.org/'
+  homepage 'https://www.retrovirtualmachine.org/en/'
 
   app 'Retro Virtual Machine.app'
 end
