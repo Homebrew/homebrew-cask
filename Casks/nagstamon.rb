20b6130e49fb29444637d864ff8f2e5de3542f7c
CoreCode <core-code@users.noreply.github.com>
2019-01-08 12:06:58 +0100

    Update nagstamon to 3.2.1 (#57137)

--- Casks/nagstamon.rb
+++ Casks/nagstamon.rb
@@ -1,6 +1,6 @@
 cask 'nagstamon' do
-  version '3.2'
-  sha256 '29524356b454ee885f7315df5d056194776a4cf2247ccabbef2590232674c11e'
+  version '3.2.1'
+  sha256 'b588b40fdb59a1b434f9b0509148b42b9bcef7c35d8ba077bdc31fb9a565777a'
 
   url "https://nagstamon.ifw-dresden.de/files/stable/Nagstamon%20#{version}.dmg"
   appcast 'https://nagstamon.ifw-dresden.de/files/stable/sha256sums.txt'
