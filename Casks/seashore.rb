bdf72d895bae1ee5902ec7e982f8fbf9bd402f26
Anver Housseini <contact@anver.ch>
2019-01-09 10:47:05 +0100

    Update seashore to 2.1.6

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,6 +1,6 @@
 cask 'seashore' do
-  version '2.1.5'
-  sha256 '223fa54991af62eda5254ad96936f696adbf8d34912ce814d5de285376fec35c'
+  version '2.1.6'
+  sha256 '08b77eb56e242dda7c0cb41a4249d915527857c319a97137a9d6c19ad2092a36'
 
   url "https://github.com/robaho/seashore/releases/download/v#{version}/seashore-bin-#{version}.dmg"
   appcast 'https://github.com/robaho/seashore/releases.atom'
