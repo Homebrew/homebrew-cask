1d5ef07e0555a599a52bc28374561aaca1cb2762
CoreCode <core-code@users.noreply.github.com>
2019-01-10 00:56:05 +0100

    Update tikzit to 2.1 (#57254)

--- Casks/tikzit.rb
+++ Casks/tikzit.rb
@@ -1,6 +1,6 @@
 cask 'tikzit' do
-  version '2.0'
-  sha256 'cf06ea31b7b8e1ca85f209a77991df8c0d99f52584cb25ad795ca2c16b6ee204'
+  version '2.1'
+  sha256 '3b5ec458e6e50d5689124afc35a0cfc08d672ac7bc508037dfaccff082a8820e'
 
   # github.com/tikzit/tikzit was verified as official when first introduced to the cask
   url "https://github.com/tikzit/tikzit/releases/download/v#{version}/tikzit-osx.dmg"
