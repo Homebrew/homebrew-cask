d2971c57ae96d19cdbc52d682f77a14bddb961b4
CoreCode <core-code@users.noreply.github.com>
2019-01-09 21:03:45 +0100

    Update vpnsecure to 2.1.6 (#57027)
    * Update vpnsecure to 2.1.6
    
    * Update vpnsecure.rb
    
    * Update vpnsecure.rb

--- Casks/vpnsecure.rb
+++ Casks/vpnsecure.rb
@@ -1,12 +1,13 @@
 cask 'vpnsecure' do
-  version :latest
-  sha256 :no_check
+  version '2.1.6'
+  sha256 '85026f7408cb516f5605369bdf9cb84fe9ac7a367cc737d9b7753b83c16f9345'
 
-  url 'https://downloads.vpnsecure.me/vpnsecure.pkg'
+  url "https://downloads-us.vpnsecure.me/vpnsecure_#{version}.pkg"
+  appcast 'https://www.vpnsecure.me/support/download/vpn/macos/'
   name 'VPNSecure OpenVPN Encrypted VPN Client'
   homepage 'https://www.vpnsecure.me/'
 
-  pkg 'vpnsecure.pkg'
+  pkg "vpnsecure_#{version}.pkg"
 
   uninstall pkgutil: 'com.vpnsecure.vpnsecureOpenvpn.*'
 end
