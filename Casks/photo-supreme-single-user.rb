8a989da1ba0988b19f770b932e3c52d25f9a0cc2
Julian Mayer <julian@corecode.io>
2019-01-09 17:10:41 +0100

    Update photo-supreme-single-user to 4.3.2

--- Casks/photo-supreme-single-user.rb
+++ Casks/photo-supreme-single-user.rb
@@ -1,6 +1,6 @@
 cask 'photo-supreme-single-user' do
   version '4.3.2'
-  sha256 '68f9180951e0e60f84bf425439bb581a670179888841158ce4fd07c4c74f48c1'
+  sha256 '9343e0872192e856efc97eba7203982e112853fcea69b4bc5b9b69bd3140cce5'
 
   url "https://trial.idimager.com/PhotoSupreme_V#{version.major}.pkg"
   name 'Photo Supreme Single User'
