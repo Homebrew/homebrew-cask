7485dbcf9070c57cb6d614841d3ae1a4e294ed70
CoreCode <core-code@users.noreply.github.com>
2019-01-09 18:22:26 +0100

    Update musixmatch.rb

--- Casks/musixmatch.rb
+++ Casks/musixmatch.rb
@@ -3,7 +3,7 @@ cask 'musixmatch' do
   sha256 'b5c65f63a9c2a7cc52938ec202c489b116255995c5625382f4b5ed45ef20e77f'
 
   url "https://download-app.musixmatch.com/download/Musixmatch-#{version}.dmg"
-  appcast 'http://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=http://download-app.musixmatch.com/download/osx'
+  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=http://download-app.musixmatch.com/download/osx'
   name 'Musixmatch'
   homepage 'https://www.musixmatch.com/'
 
