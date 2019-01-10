717e8896b0ab7899308c732ae366c3e0860b2beb
Vítor Galvão <info@vitorgalvao.com>
2019-01-10 00:16:42 +0000

    Update miniconda.rb

--- Casks/miniconda.rb
+++ Casks/miniconda.rb
@@ -1,13 +1,13 @@
 cask 'miniconda' do
   version '4.5.12'
   sha256 '8ebb463ddf46dd003616b2f6b678403a708e2c54dcc58e212bd35e257761912c'
-  auto_updates true
 
   # repo.anaconda.com/miniconda was verified as official when first introduced to the cask
   url "https://repo.anaconda.com/miniconda/Miniconda3-#{version}-MacOSX-x86_64.sh"
   name 'Continuum Analytics Miniconda'
   homepage 'https://conda.io/miniconda.html'
 
+  auto_updates true
   container type: :naked
 
   installer script: {
