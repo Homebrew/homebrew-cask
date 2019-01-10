c9f4490976a8b36ca5a7026af5f6bee3c51e67fa
jcbot <jcb@leipert.io>
2019-01-10 18:30:52 +0000

    Update appcode to 2018.3.3,183.5153.34

--- Casks/appcode.rb
+++ Casks/appcode.rb
@@ -1,6 +1,6 @@
 cask 'appcode' do
-  version '2018.3.2,183.4886.47'
-  sha256 'e14b1ba15a863ed47e7119a0f2098777d77d6376c72390fd1a7394c63fbe1426'
+  version '2018.3.3,183.5153.34'
+  sha256 '64c35da223b6342d8be311298c9bd518c3de672e0057e94879fe81e6a6500bcd'
 
   url "https://download.jetbrains.com/objc/AppCode-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=AC&latest=true&type=release'
