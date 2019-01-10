a70d6b378292ea518473497da689eb7344a52563
Anver Housseini <contact@anver.ch>
2019-01-10 12:56:56 +0100

    Update vuescan to 9.6.26 (#57287)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,5 +1,5 @@
 cask 'vuescan' do
-  version '9.6.25'
+  version '9.6.26'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
