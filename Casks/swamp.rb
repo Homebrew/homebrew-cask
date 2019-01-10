e5aff4da13aae2a4cd921eeae13d24704446d84a
Shih Oon Liong <shih.liong@samsung.com>
2019-01-10 08:21:34 -0800

    Fix Swamp 0.9.1 installion with correct binary-type for Mac OS

--- Casks/swamp.rb
+++ Casks/swamp.rb
@@ -1,11 +1,11 @@
 cask 'swamp' do
   version '0.9.1'
-  sha256 '07e7e3cc9f28f4b20b37f21c1d08cb0e6fd5d7b82a4720206398516e3f164eee'
+  sha256 '07072fe149acd4d354179980c035758787b58646d9c012b0d12034a4ae0d428b'
 
-  url "https://github.com/felixb/swamp/releases/download/v#{version}/swamp_amd64"
+  url "https://github.com/felixb/swamp/releases/download/v#{version}/swamp_darwin"
   appcast 'https://github.com/felixb/swamp/releases.atom'
   name 'swamp'
   homepage 'https://github.com/felixb/swamp'
 
-  binary 'swamp_amd64', target: 'swamp'
+  binary 'swamp_darwin', target: 'swamp'
 end
