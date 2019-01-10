85635371ee8f5a0b085ce9f22a43eec199e40f84
suschizu <brigitte@corecode.io>
2019-01-10 15:49:57 +0100

    Update journey to 2.9.0 (#57313)

--- Casks/journey.rb
+++ Casks/journey.rb
@@ -1,6 +1,6 @@
 cask 'journey' do
-  version '2.8.10'
-  sha256 'abccb13314e92fabb90acbb549f43be25fc8c9204bcecf8113d9affbc8cbfbc9'
+  version '2.9.0'
+  sha256 '4d0bbe06d9d0ff5973a600805364ea0bf2c18cd31e5db44024999c2b752cd61c'
 
   # github.com/2-App-Studio/journey-releases was verified as official when first introduced to the cask
   url "https://github.com/2-App-Studio/journey-releases/releases/download/v#{version}/Journey-darwin-#{version}.dmg"
