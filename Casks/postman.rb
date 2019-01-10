acdff90703dd53372274704290b4ef1ba8111116
CoreCode <core-code@users.noreply.github.com>
2019-01-10 13:00:31 +0100

    Update postman to 6.7.1 (#57295)
    * Update ledger-live to 1.3.0
    
    * Update postman to 6.7.1
    
    * Update ledger-live.rb

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,6 +1,6 @@
 cask 'postman' do
-  version '6.6.1'
-  sha256 '178cc449227fb16cac7790a6e72b49823d68339f0d5ee32d5ba3a176946775b8'
+  version '6.7.1'
+  sha256 '820c9ecda03c8bc6f6c46837409a58e61db72342c16e12b95e188a33e9f7d375'
 
   # dl.pstmn.io/download/version was verified as official when first introduced to the cask
   url "https://dl.pstmn.io/download/version/#{version}/osx64"
