3fde166941843dea0bae1298053eb95f4e385070
Vítor Galvão <info@vitorgalvao.com>
2019-01-11 13:44:13 +0000

    Update vlc.rb

--- Casks/vlc.rb
+++ Casks/vlc.rb
@@ -3,7 +3,7 @@ cask 'vlc' do
   sha256 '0c807a2e352fd6fe7e591fe01a7a53794b7d817e1e1eda3f673df06f26db27d5'
 
   url "https://get.videolan.org/vlc/#{version}/macosx/vlc-#{version}.dmg"
-  appcast 'https://update.videolan.org/vlc/sparkle/vlc-intel64.xml'
+  appcast 'http://download.videolan.org/pub/videolan/vlc/'
   name 'VLC media player'
   homepage 'https://www.videolan.org/vlc/'
 
