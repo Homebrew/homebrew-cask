1be2c950ebcd43efd7c65850619b48b4101e3c99
anqixu <anqixu@tencent.com>
2019-01-11 11:22:43 +0800

    update Tencent Lemon v1.0.8

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,9 +1,9 @@
 cask 'tencent-lemon' do
-  version '1.0.7_80'
-  sha256 '32b261389fbdbc6b1cd8dc7e901b96ec521ffcc2fd2f8ac56ad2c6aa68621ade'
+  version '1.0.8'
+  sha256 '398a85009294a81535f2cdd6025e24d50de69c7550a6eabf528f40e45947b57f'
 
   # pm.myapp.com/invc/xfspeed/qqpcmgr was verified as official when first introduced to the cask
-  url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon#{version}.dmg"
+  url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon_#{version}.dmg"
   name 'Tencent Lemon Cleaner'
   homepage 'https://mac.gj.qq.com/'
 
