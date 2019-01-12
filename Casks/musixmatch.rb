2aad5bf58ccde7d13e7355cdb7d19e1b0a1e146d
Julian Mayer <julian@corecode.io>
2019-01-12 17:27:06 +0100

    Update musixmatch to 0.21.19

--- Casks/musixmatch.rb
+++ Casks/musixmatch.rb
@@ -1,8 +1,9 @@
 cask 'musixmatch' do
-  version '0.21.13'
-  sha256 'b5c65f63a9c2a7cc52938ec202c489b116255995c5625382f4b5ed45ef20e77f'
+  version '0.21.19'
+  sha256 'ebccdc3308908c8ffeb3e8e59fe26f9b48d3c305a54b6f08b73ca1d316378006'
 
   url "https://download-app.musixmatch.com/download/Musixmatch-#{version}.dmg"
+  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=http://download-app.musixmatch.com/download/osx'
   name 'Musixmatch'
   homepage 'https://www.musixmatch.com/'
 
