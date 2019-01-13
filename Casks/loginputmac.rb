47a8079273783f631c0a3b4b0d020282ed59456f
Erick Guan <fantasticfears@gmail.com>
2019-01-13 21:57:37 +0100

    Upgrade LoginputMac to v2.1.3 (#57251)
    * Upgrade LoginputMac to v2.1.3
    
    The developer has offered a new set of URLs and pkg for installation.
    
    * Update loginputmac.rb
    
    * Update loginputmac.rb
    
    * Update loginputmac.rb
    
    * Update loginputmac.rb
    
    * Update loginputmac.rb
    
    * Update loginputmac.rb
    
    * Update loginputmac.rb
    
    * Update loginputmac.rb

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,14 +1,16 @@
 cask 'loginputmac' do
-  version '1.20,3776'
-  sha256 'c171a34b51304b8d230073035b9901b3b274b2a656f73dd723892c6e38414ab1'
+  version '2.1.3'
+  sha256 '9e28b16cac9a071474c138ae597ba73e109b81bb2d072ce6cea46b760285aa57'
 
   # loginput-mac2.content-delivery.top was verified as official when first introduced to the cask
-  url "https://loginput-mac2.content-delivery.top/LogInputMac#{version.after_comma}.app.zip"
-  appcast 'https://im.logcg.com/appcast.xml'
+  url "https://loginput-mac2.content-delivery.top/loginputmac#{version.major}_latest.pkg"
+  appcast "https://im.logcg.com/appcast#{version.major}.xml"
   name 'LoginputMac'
-  homepage 'https://im.logcg.com/loginputmac'
+  homepage "https://im.logcg.com/loginputmac#{version.major}"
 
   auto_updates true
 
-  app 'LogInputMac.app'
+  pkg "loginputmac#{version.major}_latest.pkg"
+
+  uninstall pkgutil: "com.logcg.pkg.LoginputMac#{version.major}"
 end
