83b86aa77108f710357a9ba22c853ba5336e7f29
Vítor Galvão <info@vitorgalvao.com>
2019-01-13 01:22:18 +0000

    Update loginputmac.rb

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -1,16 +1,16 @@
 cask 'loginputmac' do
-  version '2.1.3'
+  version '2.1.3,8771'
   sha256 '9e28b16cac9a071474c138ae597ba73e109b81bb2d072ce6cea46b760285aa57'
 
   # loginput-mac2.content-delivery.top was verified as official when first introduced to the cask
-  url 'https://loginput-mac2.content-delivery.top/loginputmac2_latest.pkg'
-  appcast 'https://im.logcg.com/appcast2.xml'
+  url "https://loginput-mac2.content-delivery.top/LogInputMac#{version.major}.app#{version.after_comma}.zip#
+  appcast "https://im.logcg.com/appcast#{version}.xml"
   name 'LoginputMac'
-  homepage 'https://im.logcg.com/loginputmac2'
+  homepage "https://im.logcg.com/loginputmac#{version.major}"
 
   auto_updates true
 
-  pkg 'loginputmac2_latest.pkg'
+  pkg "loginputmac#{version.major}_latest.pkg"
 
-  uninstall pkgutil: 'com.logcg.pkg.LoginputMac2'
+  uninstall pkgutil: 'com.logcg.pkg.LoginputMac.*'
 end
