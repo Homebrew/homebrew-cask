ccf15b74c24bd14340cedf77ae0110f69f9b83fb
Vítor Galvão <info@vitorgalvao.com>
2019-01-13 19:23:48 +0000

    Update loginputmac.rb

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -3,7 +3,7 @@ cask 'loginputmac' do
   sha256 '9e28b16cac9a071474c138ae597ba73e109b81bb2d072ce6cea46b760285aa57'
 
   # loginput-mac2.content-delivery.top was verified as official when first introduced to the cask
-  url "https://loginput-mac2.content-delivery.top/LogInputMac#{version.after_comma}.app.zip"
+  url "https://loginput-mac2.content-delivery.top/loginputmac#{version.major}_latest.pkg"
   appcast "https://im.logcg.com/appcast#{version}.xml"
   name 'LoginputMac'
   homepage "https://im.logcg.com/loginputmac#{version.major}"
