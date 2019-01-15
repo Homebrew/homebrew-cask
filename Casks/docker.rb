d0b2c58652ae5eff20a7a4ac93292a08b250912b
Anver Housseini <contact@anver.ch>
2019-01-15 23:10:12 +0100

    Update docker to 2.0.0.2-ce-mac81,30215 (#57484)

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -3,7 +3,7 @@ cask 'docker' do
     version '18.06.1-ce-mac73,26764'
     sha256 '3429eac38cf0d198039ad6e1adce0016f642cdb914a34c67ce40f069cdb047a5'
   else
-    version '2.0.0.2,30215'
+    version '2.0.0.2-ce-mac81,30215'
     sha256 'a06307d8da9c3778b183786cb87037ed3b7226d36ebc978fd40aa90851c0a04e'
   end
 
