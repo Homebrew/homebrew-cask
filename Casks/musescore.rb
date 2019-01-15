6dff26d5525432e85cc59ffb40ec224da229a076
Riaan van Niekerk <riaanvn@gmail.com>
2019-01-15 20:27:48 +0200

    Update MuseScore to 3.0.1.5087

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -1,6 +1,6 @@
 cask 'musescore' do
-  version '3.0.0.20137'
-  sha256 '02d327851ec4e0cfaf7ba90b89269a07c5de1383f918d52a78df6220d6c58a05'
+  version '3.0.1.5087'
+  sha256 'f00e02852cdf917baf5f68ea4e08419da2d60857101ee2f408e89bfd4c9dd8da'
 
   # ftp.osuosl.org/pub/musescore was verified as official when first introduced to the cask
   url "https://ftp.osuosl.org/pub/musescore/releases/MuseScore-#{version.major_minor_patch}/MuseScore-#{version.major_minor_patch}.dmg"
