85a7e52fa44990b9a358667a2a0681529070e188
Serge Bibauw <sbibauw@gmail.com>
2019-01-15 15:40:50 +0100

    Added jamovi (solid release, 0.9.5.12)

---
+++ Casks/jamovi.rb
@@ -0,0 +1,19 @@
+cask 'jamovi' do
+  version '0.9.5.12'
+  sha256 '9170638d4aa53ddff35605593750209f22570ea73b7571b477c67503d8890448'
+
+  url 'https://www.jamovi.org/downloads/jamovi-0.9.5.12-macos.dmg'
+  name 'jamovi'
+  homepage 'https://www.jamovi.org/'
+
+  auto_updates true
+
+  app 'jamovi.app'
+
+  zap trash: [
+               '~/Library/Application Support/jamovi/',
+               '~/Library/Logs/jamovi',
+               '~/Library/Preferences/org.jamovi.jamovi.plist',
+               '~/Library/Saved Application State/org.jamovi.jamovi.savedState/',
+             ]
+end
