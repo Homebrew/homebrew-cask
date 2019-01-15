52bf2c31ad4a70d98be1e7d6a45eb23bde9ffb69
Anver Housseini <contact@anver.ch>
2019-01-15 23:02:59 +0100

    Update coteditor to 3.6.12 (#57458)

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -6,8 +6,8 @@ cask 'coteditor' do
     version '3.2.8'
     sha256 '73dd20d27b75c7b0c46242a465adb3df5b5f0b901f42c5a9a85777a57c4a17d6'
   else
-    version '3.6.11'
-    sha256 '4b5af4e9b41b3450fdfee8c2010d7a3d15df7817516e6492717ce4c048c42bba'
+    version '3.6.12'
+    sha256 '51ee770cf6c62b0e1a4e20948355c15c23529cabde6b6f6fc5489148a6bf263d'
   end
 
   # github.com/coteditor/CotEditor was verified as official when first introduced to the cask
