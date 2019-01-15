a4d066dfb36f240f4bcb4ad0d7a043f93826e46b
Thomas Bernard <miniupnp@free.fr>
2019-01-15 12:24:55 +0100

    Update grafx to 2.6

--- Casks/grafx.rb
+++ Casks/grafx.rb
@@ -1,8 +1,8 @@
 cask 'grafx' do
-  version :latest
-  sha256 :no_check
+  version '2.6'
+  sha256 '54857b80cc0f81c5a4170e7c78fdadd132c5f077d351b86c689dc45078ea6ec0'
 
-  url 'https://pulkomandy.tk/projects/GrafX2/downloads/24'
+  url 'https://pulkomandy.tk/projects/GrafX2/downloads/40'
   name 'GrafX2'
   homepage 'https://pulkomandy.tk/projects/GrafX2'
 
