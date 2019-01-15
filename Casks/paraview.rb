a0a8d3a4d04b1a166067538f73da19f18fff4cd5
Francisco Orlandini <francisco.orlandini@gmail.com>
2019-01-15 14:45:31 -0200

    Update paraview to 5.6.0

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,6 +1,6 @@
 cask 'paraview' do
   version '5.6.0'
-  sha256 '711ccfe071f32ee60b41b22c42e98193ccc336d89105c3786ac729e6951c4836'
+  sha256 '7e326051fec9b83a3144bae8a0b99cdf00d00d04022a1723a3c97be5a7f33576'
 
   url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.8-64bit.dmg",
       user_agent: :fake
