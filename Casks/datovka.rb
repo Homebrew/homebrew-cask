4be9770c1f20db460de4ffa7c819b173f7beecdc
Tomáš Holý <holy@interconnect.cz>
2019-01-16 12:56:18 +0100

    Update datovka to 4.12.0

--- Casks/datovka.rb
+++ Casks/datovka.rb
@@ -1,6 +1,6 @@
 cask 'datovka' do
-  version '4.11.1'
-  sha256 '568337b3555a079b4cd62f19b48a771da42777789fcaada5ba0878f0a0cc69ba'
+  version '4.12.0'
+  sha256 '9ed4473b4e7c0c999e537723bde27b8ae43fedd65b96f30d16e3b01bed82b6e4'
 
   # secure.nic.cz/files/datove_schranky was verified as official when first introduced to the cask
   url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-64bit-osx10.7.dmg"
