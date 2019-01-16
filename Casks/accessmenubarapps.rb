3ff40e68a2ec706a0a599da13bfd539bbf88e764
suschizu <brigitte@corecode.io>
2019-01-16 18:04:53 +0100

    Update accessmenubarapps.rb

--- Casks/accessmenubarapps.rb
+++ Casks/accessmenubarapps.rb
@@ -2,10 +2,10 @@ cask 'accessmenubarapps' do
   version '2.6.1'
   sha256 'fe8a08d721af4b9e5e71bb71f3b876169fecde8219b53c01938861c2781fb16c'
 
-  url "http://www.ortisoft.de/resources/AccessMenuBarApps#{version}.zip"
-  appcast 'http://www.ortisoft.de/accessmenubarapps/profileInfo.php'
+  url "https://www.ortisoft.de/resources/AccessMenuBarApps#{version}.zip"
+  appcast 'https://www.ortisoft.de/accessmenubarapps/profileInfo.php'
   name 'AccessMenuBarApps'
-  homepage 'http://www.ortisoft.de/accessmenubarapps/'
+  homepage 'https://www.ortisoft.de/accessmenubarapps/'
 
   app "AccessMenuBarApps#{version}/AccessMenuBarApps.app"
 
