54fb3ce7ce8bc8ad7b72db472f5bad4e8bb919a3
Chris Scheib <chris@scheib.io>
2019-01-15 22:57:33 -0500

    * New Cask: LightsOff v1.0.6

---
+++ Casks/lightsoff.rb
@@ -0,0 +1,10 @@
+cask 'lightsoff' do
+  version '1.0.6'
+  sha256 'f674d8c35f006bafadb2950c84268aca2d3791a1e5e71ec474eeada60ff43fd9'
+
+  url 'https://www.lightsoff-app.com/s/LightsOff_1_0_6.zip'
+  name 'LightsOff'
+  homepage 'https://www.lightsoff-app.com/'
+
+  app 'LightsOff.app'
+end
