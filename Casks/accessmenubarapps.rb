bd345544a63099880ac9db12311a76504181cb36
Work <brigitte@corecode.io>
2019-01-16 15:37:58 +0100

    Update accessmenubarapps to 2.6

--- Casks/accessmenubarapps.rb
+++ Casks/accessmenubarapps.rb
@@ -1,6 +1,6 @@
 cask 'accessmenubarapps' do
-  version '2.4'
-  sha256 '1065fbb4ca009758d4f303ed2106bd82a5ae21233b478f50fea3ddaa318473ce'
+  version '2.6'
+  sha256 'c9fc2f9537a589f138983d378cdb7dccd2aab749fe7a77db7781c8ac818e901e'
 
   url "http://www.ortisoft.de/resources/AccessMenuBarApps#{version}.dmg"
   appcast 'http://www.ortisoft.de/accessmenubarapps/profileInfo.php'
