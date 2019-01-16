ed31253c9a402aeb8e60d7cc8dcc69a5c20e77ac
Work <brigitte@corecode.io>
2019-01-16 17:01:47 +0100

    Update dcp-o-matic to 2.12.15

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic' do
-  version '2.12.13'
-  sha256 'c48cae7f1ef9e36880c2243c06f016b856a2999f7734bbe0dacc80dead0a4af8'
+  version '2.12.15'
+  sha256 '032f146270ef82403a0b36277706a43626bdc532277e601a5086fca2b68351ed'
 
   url "https://dcpomatic.com/dl.php?id=osx-main&version=#{version}"
   appcast 'https://dcpomatic.com/download'
