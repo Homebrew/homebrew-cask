e30160f006d50fbe419d62148e3c8f67ddc9685f
Vítor Galvão <info@vitorgalvao.com>
2019-01-16 14:32:55 +0000

    Update lightsoff.rb

--- Casks/lightsoff.rb
+++ Casks/lightsoff.rb
@@ -2,7 +2,8 @@ cask 'lightsoff' do
   version '1.0.6'
   sha256 'f674d8c35f006bafadb2950c84268aca2d3791a1e5e71ec474eeada60ff43fd9'
 
-  url 'https://www.lightsoff-app.com/s/LightsOff_1_0_6.zip'
+  url "https://www.lightsoff-app.com/s/LightsOff_#{version.dots_to_underscores}.zip"
+  appcast 'https://www.lightsoff-app.com/'
   name 'LightsOff'
   homepage 'https://www.lightsoff-app.com/'
 
