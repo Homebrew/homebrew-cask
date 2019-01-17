7fc4208b48acfb07b9f7987cc8d2b7c4bdc63ba2
Work <brigitte@corecode.io>
2019-01-17 10:49:24 +0100

    Update seashore to 2.2.0

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,6 +1,6 @@
 cask 'seashore' do
-  version '2.1.9'
-  sha256 '9d8e2ed821845763a6fdb08aecda025de3bc2a660047130b0d25876d4014d542'
+  version '2.2.0'
+  sha256 '682821d05dd1f5fcf6c158ee5ce8b11e3970c4300bf93cd297c2c90b0c156cf5'
 
   url "https://github.com/robaho/seashore/releases/download/v#{version}/seashore-bin-#{version}.dmg"
   appcast 'https://github.com/robaho/seashore/releases.atom'
