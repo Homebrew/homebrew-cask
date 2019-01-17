647b202f855abc0b341d0a76e5a873a9c4e1ef3f
Work <brigitte@corecode.io>
2019-01-17 10:52:44 +0100

    Update tropy to 1.4.0

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -1,6 +1,6 @@
 cask 'tropy' do
-  version '1.3.2'
-  sha256 'f9687c86fdf5954890d97e0384d954b10db06f675da33eba706ef64f61242a8c'
+  version '1.4.0'
+  sha256 'f440af13f7215bdaf4ba927aface680c8d99e8d82d58aa904d16e51a0d52ec8b'
 
   # github.com/tropy/tropy was verified as official when first introduced to the cask
   url "https://github.com/tropy/tropy/releases/download/#{version}/tropy-#{version}.dmg"
