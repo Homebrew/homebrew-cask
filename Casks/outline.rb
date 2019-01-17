8760384ecd67f05e5fcfedb486eb7204fd15ea46
Dominik Nufer <dominik@nufer.org>
2019-01-17 10:04:31 +0100

    Add outline latest

---
+++ Casks/outline.rb
@@ -0,0 +1,10 @@
+cask 'outline' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://static.outline.ws/Outline.zip'
+  name 'Outline'
+  homepage 'http://outline.ws/'
+
+  app 'Outline.app'
+end
