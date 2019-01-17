910b14bcac725ce18409a744e72bbea893e13ad1
Work <brigitte@corecode.io>
2019-01-17 23:01:40 +0100

    Update augur to 1.9.2

--- Casks/augur.rb
+++ Casks/augur.rb
@@ -1,8 +1,8 @@
 cask 'augur' do
-  version '1.8.6'
-  sha256 '9339d1c7fbcd6d68a5d60ee7793ca772ab4c3007142eb673c72b92fe0edcf38d'
+  version '1.9.2'
+  sha256 'e67e9cd37aac579ab0a6688398719593380a6889c7ef47e4d38e77859755d9fc'
 
-  url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-augur-#{version}.dmg"
+  url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-Augur-#{version}.dmg"
   appcast 'https://github.com/AugurProject/augur-app/releases.atom'
   name 'Augur'
   homepage 'https://github.com/AugurProject/augur-app/'
