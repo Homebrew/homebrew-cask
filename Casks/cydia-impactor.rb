3e0bf83201a297157b39d2bd6f2a7b34ef64f02c
Yassine ElBadaoui <byassine52@users.noreply.github.com>
2019-01-17 16:01:16 +0900

    Added Cydia Impactor

---
+++ Casks/cydia-impactor.rb
@@ -0,0 +1,11 @@
+cask 'cydia-impactor' do
+  version :latest
+  sha256 :no_check
+
+  # saurik.com was verified as official when first introduced to the cask
+  url 'https://cydia.saurik.com/api/latest/1'
+  name 'Cydia Impactor'
+  homepage 'http://www.cydiaimpactor.com/'
+
+  app 'Impactor.app'
+end
