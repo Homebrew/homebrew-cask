3305719f14df66677774863f7c3db0f913ccc51a
Anver Housseini <contact@anver.ch>
2019-01-17 16:51:01 +0100

    Update vnc-viewer to 6.19.107

--- Casks/vnc-viewer.rb
+++ Casks/vnc-viewer.rb
@@ -1,6 +1,6 @@
 cask 'vnc-viewer' do
-  version '6.18.907'
-  sha256 '7c37d9c2137bfe6a87019659a9f52cff0a008dd8881510381ecad2b258d30f73'
+  version '6.19.107'
+  sha256 'f17ce7e9aa21eeec88dfe9217800cc9cc2bba591b01df0ca9eec6244879cf9ee'
 
   url "https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-#{version}-MacOSX-x86_64.dmg"
   name 'Real VNC Viewer'
