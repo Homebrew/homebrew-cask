b46683c9ee48987f34263ef2ebcfc148e8bed396
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2019-01-17 10:50:27 +0100

    Update vaudtax to 2018

--- Casks/vaudtax.rb
+++ Casks/vaudtax.rb
@@ -1,10 +1,10 @@
 cask 'vaudtax' do
-  version '2017'
-  sha256 'bf2aca64ee441fc4628b90cc829f5bb2f4748bc5458ae0fb4ca923e96645d28e'
+  version '2018'
+  sha256 '6592a8cacd1c89feec0c2e72d9138623f6ebba572a8b9b2bfee76c077bda1d1d'
 
   url "https://vaudtax-dl.vd.ch/vaudtax#{version}/telechargement/mac/VaudTax_#{version}.dmg"
   name 'VaudTax'
-  homepage 'https://www.vd.ch/themes/etat-droit-finances/impots/vaudtax/'
+  homepage 'https://www.vd.ch/themes/etat-droit-finances/impots/impots-pour-les-individus/remplir-ma-declaration-dimpot/vaudtax/'
 
   auto_updates true
   depends_on macos: '>= :yosemite'
