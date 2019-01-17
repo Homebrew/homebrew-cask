5f627adb3df7e3bf33b845928a0265f90e33bd8b
Work <brigitte@corecode.io>
2019-01-17 10:46:53 +0100

    Update mu-editor to 1.0.2

--- Casks/mu-editor.rb
+++ Casks/mu-editor.rb
@@ -1,9 +1,9 @@
 cask 'mu-editor' do
-  version '1.0.0'
-  sha256 '1140ad93023bbaa858d2c48e4c32e9ccaf16b3d2aa77dfe31c1d852a1e9da191'
+  version '1.0.2'
+  sha256 'd0be1d6268cff60f65947280f1d0f2b05b28e67c07ee4c7dd3773a195a3d2f71'
 
   # github.com/mu-editor/mu was verified as official when first introduced to the cask
-  url "https://github.com/mu-editor/mu/releases/download/v#{version}/mu-editor_#{version.major_minor}_osx.dmg"
+  url "https://github.com/mu-editor/mu/releases/download/#{version}/mu-editor_#{version}_osx.dmg"
   appcast 'https://github.com/mu-editor/mu/releases.atom'
   name 'Mu'
   homepage 'https://codewith.mu/'
