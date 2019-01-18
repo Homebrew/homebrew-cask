1fb2692051c13690a3803de901f6214475cfd4ea
numbermaniac <numbermaniac@users.noreply.github.com>
2019-01-19 09:40:47 +1100

    istat-menus: New download URL (#57611)

--- Casks/istat-menus.rb
+++ Casks/istat-menus.rb
@@ -2,7 +2,7 @@ cask 'istat-menus' do
   version '6.31'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://file.bjango.com/istatmenus#{version.major}/istatmenus#{version}.zip"
+  url "https://files.bjango.com/istatmenus#{version.major}/istatmenus#{version}.zip"
   name 'iStats Menus'
   homepage 'https://bjango.com/mac/istatmenus/'
 
