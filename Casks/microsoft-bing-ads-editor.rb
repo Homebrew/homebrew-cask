fd8c6cb5f4f5ab86026de24e0c02d4ddd8336092
Julian Mayer <julian@corecode.io>
2019-01-18 12:43:51 +0100

    Update microsoft-bing-ads-editor to latest

--- Casks/microsoft-bing-ads-editor.rb
+++ Casks/microsoft-bing-ads-editor.rb
@@ -2,7 +2,8 @@ cask 'microsoft-bing-ads-editor' do
   version :latest
   sha256 :no_check
 
-  url 'https://download.microsoft.com/download/6/6/2/6629C18B-8F68-43E8-A501-198B5AE0D69D/MicrosoftBingAdsEditor.dmg'
+  # msbingadseditor.blob.core.windows.net was verified as official when first introduced to the cask
+  url 'https://msbingadseditor.blob.core.windows.net/production-mac/a/MicrosoftBingAdsEditor.dmg'
   name 'Microsoft Bing Ads Editor'
   homepage 'https://advertise.bingads.microsoft.com/solutions/tools/bing-ads-editor'
 
