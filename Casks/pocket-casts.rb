4e918538dfc6371a7b0ce4df683cbc29d79a05d7
Andres Riveros <aariveros@resit.cl>
2019-01-18 19:36:12 -0300

    Update pocket-casts to 1.0 (#57582)

--- Casks/pocket-casts.rb
+++ Casks/pocket-casts.rb
@@ -1,6 +1,6 @@
 cask 'pocket-casts' do
   version '1.0'
-  sha256 '796211e2821081a0051ca5a1f1e783b98f40631a795fb52a80adf2dbf7ef71c8'
+  sha256 '0d59476b30c2ddff1c361b1de9a16948fa86b900701624de14a8e6bce77574d4'
 
   url 'https://static.pocketcasts.com/mac/PocketCasts.zip'
   appcast 'https://static2.pocketcasts.com/mac/appcast.xml'
