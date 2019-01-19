8e24c8f46a1c53e51bf3fd5087bdc8ca04211cb9
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2019-01-19 17:40:44 +0300

    Update oracle-jdk to 11.0.2,9
    Updated to 11.0.2,9:f51449fcd52f4d52b93a989c5c56ed3c

--- Casks/oracle-jdk.rb
+++ Casks/oracle-jdk.rb
@@ -1,6 +1,6 @@
 cask 'oracle-jdk' do
-  version '11.0.2,7:f51449fcd52f4d52b93a989c5c56ed3c'
-  sha256 '4aa14260013df1ab31890d798cde774e220a73b5a9f99fd4f95757efc609feb5'
+  version '11.0.2,9:f51449fcd52f4d52b93a989c5c56ed3c'
+  sha256 '466935684e8772b8fdd9884fc24f7c342909abfc5aff07c908d248026edee737'
 
   url "https://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}+#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}_osx-x64_bin.dmg",
       cookies: {
