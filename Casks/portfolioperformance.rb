477d8ca853973bfb2d5c315f3fbeaf1b7b7279a8
brigitte zmoelnig <brigitte@corecode.io>
2019-01-20 23:52:53 +0100

    Update portfolioperformance to 0.37.1

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,6 +1,6 @@
 cask 'portfolioperformance' do
-  version '0.36.1'
-  sha256 '6824f9416abdbe0049a656b93fa720a85513659189e6087e325e1628c152e2bd'
+  version '0.37.1'
+  sha256 '4595b8ff91931f63a5a92c25847ef97d72e624e57b1c28cb026fc4c2dfa76dd8'
 
   # bintray.com/buchen/downloads was verified as official when first introduced to the cask
   url "https://bintray.com/buchen/downloads/download_file?file_path=#{version}%2FPortfolioPerformance-distro-#{version}-macosx.cocoa.x86_64.tar.gz"
