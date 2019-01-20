69151763035f03220810fc337b3d083187b8c3b3
suschizu <brigitte@corecode.io>
2019-01-21 00:39:03 +0100

    Update reikey to 1.3.0 (#57706)

--- Casks/reikey.rb
+++ Casks/reikey.rb
@@ -1,6 +1,6 @@
 cask 'reikey' do
-  version '1.1.0'
-  sha256 '4a612a18047c8d83fb68420626c7287ba4f6cdd191f38618bcfcbf04466c7611'
+  version '1.3.0'
+  sha256 'd65a64fda45dc5e02db7f8bbdd2fdf07ce814bd9bdcc7f48aee090030942d7c0'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/ReiKey_#{version}.zip"
