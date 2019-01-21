42dd4d15bff33ecf46c14559db30c335ccf4072d
Ron Mordechai <ronmrdechai@gmail.com>
2019-01-21 15:17:14 +0200

    Add Kaiosrt.app latest
    Added cask for KaiOS simulator.

---
+++ Casks/kaios-simulator.rb
@@ -0,0 +1,10 @@
+cask 'kaios-simulator' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://developer.kaiostech.com/simulator/mac'
+  name 'KaiOS Simulator'
+  homepage 'https://developer.kaiostech.com/simulator'
+
+  app 'Kaiosrt.app'
+end
