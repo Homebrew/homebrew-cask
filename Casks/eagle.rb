37a570cd4a151e69ec7e88fe93195f88bc005413
ra1nb0w <rainbow@irh.it>
2019-01-21 13:37:19 +0100

    bump up eagle to 9.3.0 (#57722)
    Signed-off-by: Davide `rainbow` Gerhard <rainbow@irh.it>

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -1,6 +1,6 @@
 cask 'eagle' do
-  version '9.2.2'
-  sha256 '23877a7ed1b71cba096f32e2e645edda44b1a1f7cc1ea20aebd0c4c6da200250'
+  version '9.3.0'
+  sha256 '10529ffe8b2b7681f873c1e3dc15d88319cf5d4a82bde6da4dab036402151476'
 
   url "https://eagle-updates.circuits.io/downloads/#{version.dots_to_underscores}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   name 'Autodesk EAGLE'
