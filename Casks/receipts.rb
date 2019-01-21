fce5d47bb25c567cfef59e5034a2a3fa7f806b11
Sebastian Riehm <s.riehm@iwm.rwth-aachend.e>
2019-01-21 18:09:14 +0100

    Update receipts to 1.9.2-253

--- Casks/receipts.rb
+++ Casks/receipts.rb
@@ -1,6 +1,6 @@
 cask 'receipts' do
-  version '1.9.1-235'
-  sha256 '420b12a33dbf31ebb2deccfbad5332bfa71b1104e69951b4c4d838a9211198d3'
+  version '1.9.2-253'
+  sha256 'f37c8cf9c6b6d1fd18e0083b0526a54b9787b1c90ab4511fac8bbb3e6e3df572'
 
   url "https://www.receipts-app.com/update/download/Receipts-#{version}.zip"
   appcast 'https://www.receipts-app.com/updater.php'
