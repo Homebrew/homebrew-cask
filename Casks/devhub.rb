1cb69691d4698741fe75bbc48701446939044e99
Tushar Joshi <tusharvjoshi@gmail.com>
2019-01-21 00:08:54 -0500

    Update devhub to v0.44.0 (#57719)

--- Casks/devhub.rb
+++ Casks/devhub.rb
@@ -1,6 +1,6 @@
 cask 'devhub' do
-  version '0.43.0'
-  sha256 '049274f31981c89ed54496ff656509bf583ef7fcdafe02b16bb783ed19d4d975'
+  version '0.44.0'
+  sha256 'eecdc44b964221522046a465cdaf9b96ee113720b453dea1f36b0d0bf1f36c01'
 
   url "https://github.com/devhubapp/devhub/releases/download/v#{version}/DevHub-#{version}.dmg"
   appcast 'https://github.com/devhubapp/devhub/releases.atom'
