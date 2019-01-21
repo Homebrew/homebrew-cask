aad3f9bc8db0a6dcfad7d38940774c3b703edcb3
Halbert Lei <princescar@gmail.com>
2019-01-21 14:24:45 +0800

    Update neteasemusic to 2.0.0_696

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -1,6 +1,6 @@
 cask 'neteasemusic' do
-  version '2.0.0_690'
-  sha256 '5fb73f29ee1cb377bc64edac5f4f00012e64b2f0bb88674f6bdc07f6c637caff'
+  version '2.0.0_696'
+  sha256 'c6dc7e77f2721cc3de14b336774fd97bfac2dea075f4954d03433cdbe0ca760c'
 
   # d1.music.126.net was verified as official when first introduced to the cask
   url "https://d1.music.126.net/dmusic/NeteaseMusic_#{version}_web.dmg"
