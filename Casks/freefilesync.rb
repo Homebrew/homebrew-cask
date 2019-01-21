797bbee7072d7181dc4f6c52240caa9073e59b0f
Vítor Galvão <info@vitorgalvao.com>
2019-01-21 00:11:02 +0000

    Update freefilesync.rb

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -3,7 +3,7 @@ cask 'freefilesync' do
   sha256 '38a3a50444deaf560358161deb52c5e6287771a0f9501b06fba7994c164e82cd'
 
   url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
-      user_agent: :fake
+      referer: 'https://freefilesync.org/download.php'
   name 'FreeFileSync'
   homepage 'https://www.freefilesync.org/'
 
