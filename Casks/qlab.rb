70de74e7706cb091b0cc6cc6ef05fb3ea9a88f5f
CoreCode <core-code@users.noreply.github.com>
2019-01-22 03:56:16 +0100

    Update qlab to 4.4.1 (#57746)

--- Casks/qlab.rb
+++ Casks/qlab.rb
@@ -1,6 +1,6 @@
 cask 'qlab' do
-  version '4.3.3'
-  sha256 'de5f892c85565174176c24b4c4dd98da8caeee471b83ad771562559f037305ae'
+  version '4.4.1'
+  sha256 '52fb818786e263b221e6079c1ff97121911647df97a249bd9f6860e92b156684'
 
   url "https://figure53.com/qlab/downloads/QLab-#{version}.zip"
   appcast "https://figure53.com/qlab/downloads/appcast-v#{version.major}/"
