48cfb83100654d066a25d63fef5b98a54e9a02fb
Work <brigitte@corecode.io>
2019-01-22 20:46:13 +0100

    Update exactscan to 18.12.24

--- Casks/exactscan.rb
+++ Casks/exactscan.rb
@@ -1,6 +1,6 @@
 cask 'exactscan' do
-  version '18.11.19'
-  sha256 'bed6d6adc41275b83085a054ca280dd5c8bdd3e636a9a586b6987cc0ced37fad'
+  version '18.12.24'
+  sha256 '490ebcf48b34b17eadfeb688e6a1fceaef2b87384a1fce9e91b3356591e2d69f'
 
   # dl.exactcode.com was verified as official when first introduced to the cask
   url "https://dl.exactcode.com/exactscan/ExactScan-#{version}.dmg"
