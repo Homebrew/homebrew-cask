52c7af89221fa0cecf7a60ac8d7efdef24f423c7
CoreCode <core-code@users.noreply.github.com>
2019-01-22 03:56:44 +0100

    Update umsatz to 8.1.1 (#57742)

--- Casks/umsatz.rb
+++ Casks/umsatz.rb
@@ -1,6 +1,6 @@
 cask 'umsatz' do
-  version '8.0.9'
-  sha256 'bee0ab7201351281f7af21b3763fc7c4bd6b444cb60f1899f399f2bff76b8f73'
+  version '8.1.1'
+  sha256 '83d2e8d7283c9e41f5b31807b73e363d3b56f6949b2e1188a9fdefbe19b56de4'
 
   # moapp.software was verified as official when first introduced to the cask
   url 'https://download.moapp.software/Umsatz.zip'
