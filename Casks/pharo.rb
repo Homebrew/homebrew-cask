5bbd821b36d35ff58405175faa2068ed06cb62ec
Julian Mayer <julian@corecode.io>
2019-01-23 00:30:07 +0100

    Update pharo to 7.0

--- Casks/pharo.rb
+++ Casks/pharo.rb
@@ -1,8 +1,8 @@
 cask 'pharo' do
-  version '6.1'
+  version '7.0'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://files.pharo.org/platform/Pharo#{version}-64-mac.zip"
+  url "https://files.pharo.org/get-files/#{version.no_dots}/pharo64-mac-stable.zip"
   name 'Pharo'
   homepage 'https://pharo.org/'
 
