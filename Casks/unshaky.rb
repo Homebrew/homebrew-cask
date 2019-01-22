fbdb62ab2a52792adf1bf2ff43eb18f2fc2c9817
Work <brigitte@corecode.io>
2019-01-22 21:10:20 +0100

    Update unshaky to 0.3.2

--- Casks/unshaky.rb
+++ Casks/unshaky.rb
@@ -1,6 +1,6 @@
 cask 'unshaky' do
-  version '0.3.1'
-  sha256 '471dc9fdb6613fe9fce5ba42327bbfb7ec4c29bf62a90740d91c16af52e7b521'
+  version '0.3.2'
+  sha256 '4e320d3acce9d68ba92e9cf73ee93a28fb3e5bcc1286aca693f88e5b8a23b342'
 
   url "https://github.com/aahung/Unshaky/releases/download/v#{version}/Unshaky_v#{version}.app.zip"
   appcast 'https://github.com/aahung/Unshaky/releases.atom'
