46b704f8a84d071b9b5f086f207afc51637f458e
Bogdan Popescu <support@kapeli.com>
2019-01-22 04:31:35 +0200

    Update dash to 4.6.1 (#57756)

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -1,6 +1,6 @@
 cask 'dash' do
-  version '4.6.0'
-  sha256 'afe990100a08f098042905c8279330a1399802ae6a7e50faadee3cb4084e8258'
+  version '4.6.1'
+  sha256 '3b7858f906bcd3b2275d81647c4b90dca84f9ea31615aed795e94e9a77ce02cf'
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
   appcast "https://kapeli.com/Dash#{version.major}.xml"
