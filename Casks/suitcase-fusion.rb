5f2cf90caeab8f5c30d537c21b0df6d69e23f3bc
Work <brigitte@corecode.io>
2019-01-22 18:10:50 +0100

    Update suitcase-fusion to 9,20.0.2

--- Casks/suitcase-fusion.rb
+++ Casks/suitcase-fusion.rb
@@ -1,11 +1,13 @@
 cask 'suitcase-fusion' do
   version '9,20.0.2'
-  sha256 'b93b513a022f1017bcf04a0343670c24ec566cb782d5646829b67081c85e23d5'
+  sha256 '1daab2f2065a7d3a07fac0dc32f217ff409d7cd891c4792911d85acabecde356'
 
-  url "https://bin.extensis.com/SuitcaseFusion#{version.before_comma}-M-#{version.after_comma.dots_to_hyphens}.dmg"
+  url "http://bin.extensis.com/sf#{version.before_comma}-#{version.after_comma}-m-all.zip"
   appcast "https://sparkle.extensis.com/u/ST/EN/suitcase#{version.after_comma.major}en.xml"
   name 'Extensis Suitcase Fusion'
   homepage 'https://www.extensis.com/'
+
+  depends_on macos: '>= :sierra'
 
   app 'Suitcase Fusion.app'
 end
