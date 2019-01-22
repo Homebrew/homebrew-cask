17708f841dba721dec6555b30bfa801e521f6797
Steve Coffman <StevenACoffman@users.noreply.github.com>
2019-01-22 09:24:15 -0500

    Update aws-vault to 4.5.0 (#57774)

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,6 +1,6 @@
 cask 'aws-vault' do
-  version '4.4.1'
-  sha256 '6c84a00b919629f153ad43a0889cc8f8d67708cfc85cce8bd6e98a57706368b0'
+  version '4.5.0'
+  sha256 'cd38a3fb54708d7b73877656a8db705235ff3a8fecd189066a58690b4bee1e6a'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64"
   appcast 'https://github.com/99designs/aws-vault/releases.atom'
