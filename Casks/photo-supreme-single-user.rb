3f2a51f8ba09d1911c94e5288fe32e5ad04c9107
Vítor Galvão <info@vitorgalvao.com>
2019-01-23 01:53:17 +0000

    Update photo-supreme-single-user.rb

--- Casks/photo-supreme-single-user.rb
+++ Casks/photo-supreme-single-user.rb
@@ -1,12 +1,12 @@
 cask 'photo-supreme-single-user' do
-  version '4.3.2,1901'
-  sha256 '888aaf3e163264d23b04c029acdef1f0ada3058ac96045c8c3fc4a2ceba1c68a'
+  version '4'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://trial.idimager.com/PhotoSupreme_V#{version.major}.pkg"
+  url "https://trial.idimager.com/PhotoSupreme_V#{version}.pkg"
   name 'Photo Supreme Single User'
   homepage 'https://www.idimager.com/home'
 
-  pkg "PhotoSupreme_V#{version.major}.pkg"
+  pkg "PhotoSupreme_V#{version}.pkg"
 
   uninstall pkgutil: 'com.idimager.idimagersu'
 end
