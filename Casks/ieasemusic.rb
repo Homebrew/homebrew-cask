171d23fc3f79c27ffedf258a59268fc6b743e38a
suschizu <brigitte@corecode.io>
2019-01-24 00:15:49 +0100

    Update ieasemusic to 1.3.3 (#57871)

--- Casks/ieasemusic.rb
+++ Casks/ieasemusic.rb
@@ -1,6 +1,6 @@
 cask 'ieasemusic' do
-  version '1.3.2'
-  sha256 'e9730756a5d7f2040875aed6e1cf5e8afa388576c5fabe8785bb380990c228af'
+  version '1.3.3'
+  sha256 'acd618f8f6e1b763a923b1d14f9d527fca5cd548fb432b5e2fb13aba8eca69d6'
 
   url "https://github.com/trazyn/ieaseMusic/releases/download/v#{version}/ieaseMusic-#{version}-mac.dmg"
   appcast 'https://github.com/trazyn/ieaseMusic/releases.atom'
