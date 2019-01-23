44be4c85128cc0041b1d5da5b24ac194d6e74e80
brigitte zmoelnig <brigitte@corecode.io>
2019-01-23 23:23:23 +0100

    Update fastrawviewer to 1.4.11.1402

--- Casks/fastrawviewer.rb
+++ Casks/fastrawviewer.rb
@@ -1,6 +1,6 @@
 cask 'fastrawviewer' do
-  version '1.4.10.1385'
-  sha256 '5ffda4ba6530ab6902a32ffe42109b341a542a3f8d8ce393b69323af9f473822'
+  version '1.4.11.1402'
+  sha256 '90db6b2c194aea42c50da33074dc7a78ce4bfa32c7f7a626768adb0d9df5bee3'
 
   url "https://updates.fastrawviewer.com/data/FastRawViewer-#{version}.dmg"
   name 'FastRawViewer'
