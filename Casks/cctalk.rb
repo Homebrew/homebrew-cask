3356febf55b8fb785492a825eb5ef6cb779abca8
Vítor Galvão <info@vitorgalvao.com>
2019-01-23 23:08:34 +0000

    Update cctalk.rb

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -3,7 +3,7 @@ cask 'cctalk' do
   sha256 'dc9843fb069aa56a2dfa55a4283bb0f7a160cf26348e3f3df6a5f28d607a2e5c'
 
   # cc.hjfile.cn was verified as official when first introduced to the cask
-  url "http://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg"
+  url "https://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg"
   appcast 'https://cc.hjfile.cn/mac/update/info.xml'
   name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
