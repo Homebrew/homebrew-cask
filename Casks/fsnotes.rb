a5fd3827384d11f8c10e2ca637d34cb9dd05bdd7
suschizu <brigitte@corecode.io>
2019-01-23 02:52:08 +0100

    Update fsnotes to 2.7.1 (#57800)

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask 'fsnotes' do
-  version '2.7.0'
-  sha256 '3f3cff821f4a6fb04e8f85fde6981d0ac4c24d2cbe7c5ea9248ceccbc18824de'
+  version '2.7.1'
+  sha256 'f359295bd7db4e66bf80e1a61ba77dd685eb1373cd062a9629866bf764c66c74'
 
   # github.com/glushchenko/fsnotes was verified as official when first introduced to the cask
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
