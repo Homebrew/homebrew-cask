605f282fc7e0c00827dd3e9d11e7c382ca0dacda
Vítor Galvão <info@vitorgalvao.com>
2019-01-23 18:52:19 +0000

    Update sameboy.rb

--- Casks/sameboy.rb
+++ Casks/sameboy.rb
@@ -3,7 +3,7 @@ cask 'sameboy' do
   sha256 '70fa66740a3c04282fed32e54631252840d9023a0183e05f34c64fdfca91cd0a'
 
   # github.com/LIJI32/SameBoy was verified as official when first introduced to the cask
-  url 'https://github.com/LIJI32/SameBoy/releases/download/v0.11.2/sameboy_cocoa_v0.11.2.zip'
+  url "https://github.com/LIJI32/SameBoy/releases/download/v#{version}/sameboy_cocoa_v#{version}.zip"
   appcast 'https://github.com/LIJI32/SameBoy/releases.atom'
   name 'SameBoy'
   homepage 'https://sameboy.github.io/'
