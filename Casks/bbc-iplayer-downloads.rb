c04d0048c85481c99ed16a6790d2c74ef9e9b486
CoreCode <core-code@users.noreply.github.com>
2019-01-24 01:49:34 +0100

    Update bbc-iplayer-downloads to 2.7.4 (#57816)
    * Update bbc-iplayer-downloads to 2.7.4
    
    * Update bbc-iplayer-downloads.rb

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -1,9 +1,10 @@
 cask 'bbc-iplayer-downloads' do
-  version '2.7.3'
-  sha256 '511d236956fa9ea5acad3f122704f23e90d075be4323b091938791d203ad71e5'
+  version '2.7.4'
+  sha256 '97a8995ca9b8a9423623790e89ed4dd4627bc1385c26ac38039573b8f024e45f'
 
   # live-downloads-app-bucket-staticassetsbucket-ydn3z4ggyaof.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://live-downloads-app-bucket-staticassetsbucket-ydn3z4ggyaof.s3.amazonaws.com/releases/darwin-x64/BBCiPlayerDownloads-#{version}.dmg"
+  appcast 'https://corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-app.iplayer.api.bbc.co.uk/stable/darwin-x64'
   name 'BBC iPlayer Downloads'
   homepage 'https://www.bbc.co.uk/iplayer/install'
 
