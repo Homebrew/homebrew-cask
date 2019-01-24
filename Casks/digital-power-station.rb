24a7c84f73e5fa37bc7ee0d03fbeabd92410ac1d
Julian Mayer <julian@corecode.io>
2019-01-24 01:11:14 +0100

    Update digital-power-station to 2.2.1.1,2018.12

--- Casks/digital-power-station.rb
+++ Casks/digital-power-station.rb
@@ -1,6 +1,6 @@
 cask 'digital-power-station' do
-  version '2.2.0.9,2018.08'
-  sha256 '251573a04075f85d458f7bf55b05dc2dd29a7afbccf03ab4a93ff9a845f23ff0'
+  version '2.2.1.1,2018.12'
+  sha256 '5b7f4dd2f63d6aa0a5ad843bda6ba9da5302ed7da0449c3f4d1253da2ca43f49'
 
   url "https://bongiovidps.com/wp-content/uploads/#{version.after_comma.dots_to_slashes}/Bongiovi_DPS_Mac_#{version.before_comma}.zip"
   appcast 'https://bongiovidps.com/download/'
