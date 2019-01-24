00f5c16a2d97891a99ead89fe9ddae0fac3d9c81
blogabe <6888675+blogabe@users.noreply.github.com>
2019-01-23 16:14:49 -0800

    Boxcryptor 2.27.977 update (#57880)
    * boxcryptor 2.27.977 update
    
    * Revert "boxcryptor 2.27.977 update"
    
    This reverts commit e60b94585b4f25a16fa9f0a398daa3aca7299e30.
    
    * boxcryptor 2.27.977 update

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,6 +1,6 @@
 cask 'boxcryptor' do
-  version '2.26.964'
-  sha256 '7522ffa0cc53f7a23f407e81905ee839133bff80bd053188d1d3fd97ec30e6ff'
+  version '2.27.977'
+  sha256 'd2f94dec6f14f3088de9bc146d4f220dae2aee604b325dfbdaa53e6be70e79ff'
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/7fd6db3e51a977132e3b120c613eaea8'
