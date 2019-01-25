e53b9d359172feb687d2a0699541a00909021782
János Orcsik <orcsikj@gmail.com>
2019-01-25 20:44:53 +0100

    Update remembear to 1.2.5

--- Casks/remembear.rb
+++ Casks/remembear.rb
@@ -1,6 +1,6 @@
 cask 'remembear' do
-  version '1.2.4'
-  sha256 'e2b04f0b402f908e42622475c2ba6590608d8244ccd4eaee53f21d515fec4554'
+  version '1.2.5'
+  sha256 'c80dd7f00a7c824e8f3e311146bf5557fdf82a6ebd5a45653d1e693f2eb4a55d'
 
   # s3.amazonaws.com/remembear was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/remembear/app/release/downloads/macOS/RememBear-#{version}.zip"
