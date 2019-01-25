2985e00ef50f523574cf4183dca50f0e40a63fb4
Julian Mayer <julian@corecode.io>
2019-01-25 15:33:58 +0100

    Update qt3dstudio to 2.2.0

--- Casks/qt3dstudio.rb
+++ Casks/qt3dstudio.rb
@@ -3,6 +3,7 @@ cask 'qt3dstudio' do
   sha256 '9433dc659ac4f844b85f9d5ed5b14eb051243d2296eea47ac5e1c38eec8e8fc9'
 
   url "https://download.qt.io/official_releases/qt3dstudio/#{version.major_minor}/qt-3dstudio-opensource-mac-x64-#{version}.dmg"
+  appcast 'https://download.qt.io/official_releases/qt3dstudio/'
   name 'Qt 3D Studio'
   homepage 'https://www.qt.io/developers/'
 
