fc3eff9d158d116a625bd32cc1c5f16fd864064e
suschizu <brigitte@corecode.io>
2019-01-25 04:49:32 +0100

    Update thebrain to 10.0.36.0 (#57895)

--- Casks/thebrain.rb
+++ Casks/thebrain.rb
@@ -1,6 +1,6 @@
 cask 'thebrain' do
-  version '10.0.32.0'
-  sha256 '67b3ff05a50fa92f9ad37363d0206a02f9dd3436297514788411dc49312e8297'
+  version '10.0.36.0'
+  sha256 '87c7103d5cb3faae3640ae5573d68c389bb5c35f48efcc5cc527300fecba9a62'
 
   url "http://updater.thebrain.com/files/TheBrain#{version}.dmg"
   name 'TheBrain'
