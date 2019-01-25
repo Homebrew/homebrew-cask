2afdb1659a26fb4a7572f402219dbcf913ae94c8
suschizu <brigitte@corecode.io>
2019-01-25 04:56:07 +0100

    add appcast to ipswdownloader to 2.7.0 (#57892)
    * Update ipswdownloader to 2.7.0
    
    * Update ipswdownloader.rb

--- Casks/ipswdownloader.rb
+++ Casks/ipswdownloader.rb
@@ -3,8 +3,9 @@ cask 'ipswdownloader' do
   sha256 '76f1fbff4496bb836bce3706bd0488880d82c91494fd58a5dfc99a1d51d75989'
 
   url "http://downloads.igrsoft.com/downloads/ipswDownloader/ipswDownloader_#{version.no_dots}.zip"
+  appcast 'http://igrsoft.com/wp-content/iPhone/update.xml'
   name 'ipswDownloader'
-  homepage 'https://igrsoft.com/ipswdownloader/'
+  homepage 'http://igrsoft.com/ipswdownloader/'
 
   app 'ipswDownloader.app'
 end
