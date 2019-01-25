830b7375b3974681dc3253f6c04e5e0270ddc5a2
Daniel Black <Daniel.Black@Honeywell.com>
2019-01-25 10:55:21 +0530

    Fixed style offenses

--- Casks/menumeters-emcrisostomo-fork.rb
+++ Casks/menumeters-emcrisostomo-fork.rb
@@ -1,14 +1,14 @@
 cask 'menumeters-emcrisostomo-fork' do
-    version '1.9.7+emc'
-    sha256 '9d4fa909eee01939be5a851502e3f77bbf990af3e228fbeb14a4af7d8fde5862'
-  
-    url 'https://github.com/emcrisostomo/MenuMeters/releases/download/1.9.7%2Bemc/MenuMeters-1.9.7+emc.dmg'
-    appcast 'https://github.com/emcrisostomo/MenuMeters/releases'
-    name 'Menumeters (emcrisostomo fork)'
-    homepage 'https://emcrisostomo.github.io/MenuMeters/'
-  
-    pkg 'MenuMeters.pkg'
-  
-    uninstall pkgutil: 'com.yujitach.MenuMeters'
+  version '1.9.7+emc'
+  sha256 '9d4fa909eee01939be5a851502e3f77bbf990af3e228fbeb14a4af7d8fde5862'
 
-  end
+  # github.com/emcrisostomo/MenuMeters/releases/download/1.9.7%2Bemc/MenuMeters-1.9.7+emc.dmg was verified as official when first introduced to the cask
+  url 'https://github.com/emcrisostomo/MenuMeters/releases/download/1.9.7%2Bemc/MenuMeters-1.9.7+emc.dmg'
+  appcast 'https://github.com/emcrisostomo/MenuMeters/releases'
+  name 'Menumeters (emcrisostomo fork)'
+  homepage 'https://emcrisostomo.github.io/MenuMeters/'
+
+  pkg 'MenuMeters.pkg'
+
+  uninstall pkgutil: 'com.yujitach.MenuMeters'
+end
