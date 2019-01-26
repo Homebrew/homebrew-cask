e20de87e0a241d7af6ef62396181865eb3ffdbfd
ralph <ralph@cs.cf.ac.uk>
2019-01-26 15:25:31 +1300

    Update houdahspot to 4.4

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,6 +1,6 @@
 cask 'houdahspot' do
-  version '4.3.9'
-  sha256 '035c0f46910461368ed7f0520cbb61fc4068335740f2e89f2f5257d10459eaff'
+  version '4.4'
+  sha256 '6219a3f278e4b3a5be824d7b3f9509f0c998e72599341e8687333e36f0febcd5'
 
   url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
