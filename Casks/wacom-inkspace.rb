a5959420d2b7641f113cef2554dc33219f9cea30
Vítor Galvão <info@vitorgalvao.com>
2019-01-26 15:48:27 +0000

    Update wacom-inkspace.rb

--- Casks/wacom-inkspace.rb
+++ Casks/wacom-inkspace.rb
@@ -2,7 +2,7 @@ cask 'wacom-inkspace' do
   version '2.6.0'
   sha256 'bc673839739055754c4319030a36821ab081a16669c5bed884cd925995435fa6'
 
-  url 'https://cdn.wacom.com/i/m/mac/WacomInkspaceApp_2.6.0.zip'
+  url "https://cdn.wacom.com/i/m/mac/WacomInkspaceApp_#{version}.zip"
   name 'Wacom Inkspace'
   homepage 'https://www.wacom.com/en-us/products/apps-services/inkspace'
 
