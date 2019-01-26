eab42423bfdb8a5b3b3f999dc99a184c81fc8d76
Brian Bezanson <bezanson@mac.com>
2019-01-25 21:49:58 -0600

    Update default-folder-x to 5.3.3 (#57929)

--- Casks/default-folder-x.rb
+++ Casks/default-folder-x.rb
@@ -1,6 +1,6 @@
 cask 'default-folder-x' do
   version '5.3.3'
-  sha256 '1d8dfffcf272bc155db2d2da235a5fb05d1c682302f8da3ca6fccd2022766339'
+  sha256 '098f2641e052d8bcf0e774c3b3bc1440ec2f0b736dfb04e5c8aaf72822995c9f'
 
   url "https://www.stclairsoft.com/download/DefaultFolderX-#{version}.dmg"
   appcast 'https://www.stclairsoft.com/cgi-bin/sparkle.cgi?DX5'
