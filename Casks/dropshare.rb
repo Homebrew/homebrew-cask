60d7632fc612d5030447b66094da084456b5b9e4
Timo Josten <tj@mkswap.net>
2019-01-26 18:05:38 +0100

    Update Dropshare to Version 5.1 Build 5047 (#57956)
    * Update Dropshare to Version 5.1 Build 5047
    
    Fix Dropshare 5 directory paths and files
    
    * Update dropshare.rb
    
    * Update dropshare.rb
    
    * Update dropshare.rb

--- Casks/dropshare.rb
+++ Casks/dropshare.rb
@@ -1,21 +1,21 @@
 cask 'dropshare' do
-  version '5,5036'
-  sha256 'c28dd83d2da7107d85953dec8e03a5ddaff837ba34fdc547427144e26358e665'
+  version '5.1,5047'
+  sha256 '7f727b7762f7c8ea6230f70ec748f0df760ea0edb95fefc00045d5235c42384f'
 
   # d2wvuuix8c9e48.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2wvuuix8c9e48.cloudfront.net/Dropshare#{version.major}-#{version.after_comma}.app.zip"
   appcast "https://getdropsha.re/sparkle/Dropshare#{version.major}.xml"
   name 'Dropshare'
-  homepage 'https://getdropsha.re/'
+  homepage 'https://dropshare.app/'
 
   app "Dropshare #{version.major}.app"
   binary "#{appdir}/Dropshare #{version.major}.app/Contents/Resources/ds.sh", target: 'ds'
 
   zap trash: [
-               '~/Library/Application Support/Dropshare 4',
-               '~/Library/Caches/net.mkswap.Dropshare4',
-               '~/Library/Cookies/net.mkswap.Dropshare4.binarycookies',
-               '~/Library/Logs/Dropshare 4',
-               '~/Library/Preferences/net.mkswap.Dropshare4.plist',
+               "~/Library/Application Support/Dropshare #{version.major}",
+               "~/Library/Caches/net.mkswap.Dropshare#{version.major}",
+               "~/Library/Cookies/net.mkswap.Dropshare#{version.major}.binarycookies",
+               "~/Library/Logs/Dropshare #{version.major}",
+               "~/Library/Preferences/net.mkswap.Dropshare#{version.major}.plist",
              ]
 end
