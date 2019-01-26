881fbb47f2ea51764ba0be7dfe360a91a25a9809
Randall <ran.dall@icloud.com>
2019-01-26 14:11:27 -0800

    Update meta to 1.9.1

--- Casks/meta.rb
+++ Casks/meta.rb
@@ -1,6 +1,6 @@
 cask 'meta' do
-  version '1.8.6'
-  sha256 'fb7f531240f3e546f5ff5277a7f9380949875025e53d109dac266d9d48294592'
+  version '1.9.1'
+  sha256 '9db6430a263a9c14781a4a8e6064629328b2b776a99ba540ed599702953562d6'
 
   url "https://www.nightbirdsevolve.com/meta/updates/bin/Meta%20#{version}.zip"
   appcast 'https://www.nightbirdsevolve.com/meta/updates/'
