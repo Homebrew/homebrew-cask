6d6d53f828e6bde61de5ef60eb3beb6d118c8d69
Randall <ran.dall@icloud.com>
2019-01-26 10:53:19 -0800

    Update manager to 19.1.26

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -1,6 +1,6 @@
 cask 'manager' do
-  version '19.1.28'
-  sha256 'f8595ac1734a989a47de4228882fb0458706eec9cd50399bf822efbe53fddcb1'
+  version '19.1.26'
+  sha256 'f2b2b82d260de22ec69ae987b17620203a89f2652f29c703c36e3a7811606c63'
 
   # d2ap5zrlkavzl7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2ap5zrlkavzl7.cloudfront.net/#{version}/Manager.dmg"
