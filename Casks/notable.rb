79334875186aca800d3f9deee0231fe32de8b256
Work <brigitte@corecode.io>
2019-01-26 11:29:43 +0100

    Update notable to 1.2.0

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -1,6 +1,6 @@
 cask 'notable' do
-  version '1.1.0'
-  sha256 '175afd8da6d52a16571bf9e557d009554c8305d8f320d3472a8dbd3973f03068'
+  version '1.2.0'
+  sha256 '6ea690d5e9ac5ccbb118d34840a5314bddd0410e90a4ea6f0b34487307c37ad6'
 
   url "https://github.com/fabiospampinato/notable/releases/download/v#{version}/Notable-#{version}-mac.zip"
   appcast 'https://github.com/fabiospampinato/notable/releases.atom'
