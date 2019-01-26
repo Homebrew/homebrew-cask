8f86fd440fad58a19795e3277b23c866201c7b8c
Work <brigitte@corecode.io>
2019-01-26 11:05:55 +0100

    Update pinegrow to 5.1

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -1,6 +1,6 @@
 cask 'pinegrow' do
-  version '5.0'
-  sha256 '4845f8a0e153ebfbf7a04937b4179909b955a31f9db172d5116a93a4afd5cf76'
+  version '5.1'
+  sha256 '80b6a81a06c182d71f967f6e1acbc70b1438249eaf4b06f8c1dffb83f77cbd1f'
 
   url "http://download.pinegrow.com/PinegrowMac.#{version}.dmg"
   name 'Pinegrow'
