7bc71ed9105d89f4ef5c3bf74eaa49b180922a9c
admin <admin@admin.local>
2019-01-27 11:20:54 +0100

    Add Kerio Connect.app v9.2.5.9920

---
+++ Casks/kerio-connect.rb
@@ -0,0 +1,11 @@
+cask 'kerio-connect' do
+  version '9.2.5.9920'
+  sha256 '5b8655db470ab4ab3016b09fa4db9b4d9213546050dc58b90e29432acf51f493'
+
+  # kerio-dc-releases.kerio.com was verified as official when first introduced to the cask
+  url "https://kerio-dc-releases.kerio.com/kerio-connect-client-darwin64-#{version}.dmg"
+  name 'Kerio Connect'
+  homepage 'https://www.kerio.de/products/kerio-connect'
+
+  app 'Kerio Connect.app'
+end
