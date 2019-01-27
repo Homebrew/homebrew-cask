bbe05c16a3efb53b09bf6c8ab3ba5455009499d0
CoreCode <core-code@users.noreply.github.com>
2019-01-27 17:02:28 +0100

    Update skype to 8.34.0.78 (#57915)
    * Update skype to 8.34.0.78
    
    * Update skype.rb

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,10 +1,10 @@
 cask 'skype' do
-  version '8.37.0.98'
-  sha256 '943f3a9e7dc9a6299ba87da47868f1e209cb48ff9d9817ed5db3b081feea3a10'
+  version '8.34.0.78'
+  sha256 '814b60d85381d4a6332944cdf5f67f0e067b01843821989e685e53b49e6edf80'
 
   # endpoint920510.azureedge.net/s4l/s4l/download/mac was verified as official when first introduced to the cask
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg"
-  appcast 'https://get.skype.com/s4l-update?version=8.30.0.0&os=mac&ring=production&app=s4l'
+  appcast 'https://get.skype.com/s4l-update?version&os=mac&ring=production&app=s4l'
   name 'Skype'
   homepage 'https://www.skype.com/'
 
