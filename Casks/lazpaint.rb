ce4fea7c1d3e66187adaa3d1dc65f6c3537e9ce3
Vítor Galvão <info@vitorgalvao.com>
2019-01-28 14:51:44 +0000

    Update lazpaint.rb

--- Casks/lazpaint.rb
+++ Casks/lazpaint.rb
@@ -2,6 +2,7 @@ cask 'lazpaint' do
   version '6.4.1'
   sha256 'b9518c828b3369030e25800ceb82dd6ccb0a2a105157b22332c4bc3711e42a07'
 
+  # github.com/bgrabitmap/lazpaint was verified as official when first introduced to the cask
   url "https://github.com/bgrabitmap/lazpaint/releases/download/v#{version}/LazPaint.dmg"
   appcast 'https://github.com/bgrabitmap/lazpaint/releases.atom'
   name 'LazPaint'
