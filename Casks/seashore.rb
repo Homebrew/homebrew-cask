93677ca69856d80f8db5af7a6782f1582d8f6e9c
Anver Housseini <contact@anver.ch>
2019-01-28 15:59:52 +0100

    Update seashore to 2.3.2 (#58036)
    * Add Kerio Connect.app v9.2.5.9920
    
    * Update seashore to 2.3.2
    
    * Delete kerio-connect.rb

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,6 +1,6 @@
 cask 'seashore' do
-  version '2.2.0'
-  sha256 '682821d05dd1f5fcf6c158ee5ce8b11e3970c4300bf93cd297c2c90b0c156cf5'
+  version '2.3.2'
+  sha256 '412cfbf7c74133d1d5bcf9d118caaf84350fedb5a8cf5ff4a89c98daa3f1ae37'
 
   url "https://github.com/robaho/seashore/releases/download/v#{version}/seashore-bin-#{version}.dmg"
   appcast 'https://github.com/robaho/seashore/releases.atom'
