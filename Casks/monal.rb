5692317f81dfd04a56a6fe1c1237b9a5e1122b79
Randall <ran.dall@icloud.com>
2019-01-28 15:40:02 -0800

    Update monal to 2.3b17

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,6 +1,6 @@
 cask 'monal' do
-  version '2.3b10'
-  sha256 'a098225234544e7565af73b93be4d8f29034041d0a56833c0acd673d77b6f8a4'
+  version '2.3b17'
+  sha256 '34eaedf3e3dbc76e4400c14a6b7d674d1e801d7998e24ea14220014aa7582ee1'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
   appcast 'https://monal.im/Monal-OSX/appcast.xml'
