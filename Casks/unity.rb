65ebbc34308b28b00b20cbabe0e2e2cf78f95d0f
Randall <17261190+ran-dall@users.noreply.github.com>
2019-01-28 08:52:26 -0800

    Update unity to 2018.3.3f1,393bae82dbb8 (#58009)
    * Update unity to 2018.3.3f1,393bae82dbb8
    
    * Update unity.rb
    
    * Update unity.rb
    
    * Update unity.rb

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,16 +1,15 @@
 cask 'unity' do
-  version '2018.3.2f1,b3c100a4b73a'
-  sha256 '78d9f1db6ca52ba873cb3ec1b842f15c2df9a2935236a303c5b99beef1d1e75b'
+  version '2018.3.3f1,393bae82dbb8'
+  sha256 'bf6d063a48c03d4d59e3129dd0b8d40b61fe5f69257f3feaa96fa34534fa131a'
 
-  url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity.pkg"
+  url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/download/archive'
   name 'Unity Editor'
   homepage 'https://unity3d.com/unity/'
 
-  pkg 'Unity.pkg'
+  pkg "Unity-#{version.before_comma}.pkg"
 
   uninstall quit:    'com.unity3d.UnityEditor5.x',
             pkgutil: 'com.unity3d.UnityEditor5.x',
-            delete:  '/Applications/Unity/Unity.app',
-            rmdir:   '/Applications/Unity'
+            delete:  '/Applications/Unity'
 end
