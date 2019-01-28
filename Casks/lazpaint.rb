a14c74ecca96961119e80237790daeb6e232335e
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2019-01-28 17:53:51 +0100

    Update lazpaint to 6.4.1 (#58035)
    * Update lazpaint to 6.4.1
    
    * Update lazpaint.rb

--- Casks/lazpaint.rb
+++ Casks/lazpaint.rb
@@ -1,11 +1,12 @@
 cask 'lazpaint' do
-  version '6.2'
-  sha256 '76fc3688444c5ba1b9b3ee1be52d1197b83eef644aedd08a4a046206231292fd'
+  version '6.4.1'
+  sha256 'b9518c828b3369030e25800ceb82dd6ccb0a2a105157b22332c4bc3711e42a07'
 
-  url "https://downloads.sourceforge.net/lazpaint/lazpaint#{version}_osx32.zip"
-  appcast 'https://sourceforge.net/projects/lazpaint/rss'
+  # github.com/bgrabitmap/lazpaint was verified as official when first introduced to the cask
+  url "https://github.com/bgrabitmap/lazpaint/releases/download/v#{version}/LazPaint.dmg"
+  appcast 'https://github.com/bgrabitmap/lazpaint/releases.atom'
   name 'LazPaint'
-  homepage 'https://sourceforge.net/projects/lazpaint/'
+  homepage 'https://bgrabitmap.github.io/lazpaint/'
 
   app 'LazPaint.app'
 end
