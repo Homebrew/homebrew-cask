3299f9640af3b29bc2ee704ef32231395b886a11
Scott Humphries <git@sscotth.io>
2019-01-28 16:52:36 -0600

    Update graphql-playground to 1.8.7

--- Casks/graphql-playground.rb
+++ Casks/graphql-playground.rb
@@ -1,6 +1,6 @@
 cask 'graphql-playground' do
-  version '1.8.6'
-  sha256 '40c4985dea9cfa2660728aa0d730f7f4ac12319890aae6f572fe2e1fa657b285'
+  version '1.8.7'
+  sha256 '91071e9fb0cce6ad6078ffd6a0bd4f23fe086e357fce426766286b713d1ca5ab'
 
   url "https://github.com/prisma/graphql-playground/releases/download/v#{version}/graphql-playground-electron-#{version}.dmg"
   appcast 'https://github.com/prisma/graphql-playground/releases.atom'
