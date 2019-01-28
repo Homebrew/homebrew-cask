121105e5580df46b2efc94ac5debee1b68cd6149
Randall <ran.dall@icloud.com>
2019-01-27 21:25:51 -0800

    Update fsnotes to 2.7.3
    Signed-off-by: Randall <ran.dall@icloud.com>

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,9 +1,9 @@
 cask 'fsnotes' do
-  version '2.7.2'
-  sha256 'fcd1b213203f937b886780ebfa36662a2a9fc23598a5d47e390b2e462fb9d5b2'
+  version '2.7.3'
+  sha256 '2da454881120be8fcaa5e01a35849fa9e4c1f27c44db1afd6516325c7e648317'
 
   # github.com/glushchenko/fsnotes was verified as official when first introduced to the cask
-  url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
+  url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes.zip"
   appcast 'https://github.com/glushchenko/fsnotes/releases.atom'
   name 'FSNotes'
   homepage 'https://fsnot.es/'
