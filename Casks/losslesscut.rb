272560658e718e19ad429ce4508ca47d9bf8a958
404NetworkError <404NetworkError@users.noreply.github.com>
2019-01-28 14:11:56 -0500

    Update losslesscut to 2.0.0

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,6 +1,6 @@
 cask 'losslesscut' do
-  version '1.14.0'
-  sha256 'ae2d672271b48900f8f75c46e4c8663c810bb3be5ddb4b69259ef0b69ea546c0'
+  version '2.0.0'
+  sha256 'b4f6a71fce24ed448ddc3b4797a2b387da24304b9ca0f52a716fe29dd295372e'
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-darwin-x64.zip"
   appcast 'https://github.com/mifi/lossless-cut/releases.atom'
