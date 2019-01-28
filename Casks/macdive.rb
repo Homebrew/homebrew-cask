20556d9b9a40b852e60dcfb397094329110a2655
Randall <17261190+ran-dall@users.noreply.github.com>
2019-01-28 01:00:57 -0800

    Update macdive to 2.10.8 (#58019)

--- Casks/macdive.rb
+++ Casks/macdive.rb
@@ -1,6 +1,6 @@
 cask 'macdive' do
-  version '2.10.7'
-  sha256 '7303d04b9da0f026a4e8c82f8ff2b14158df9294b1ae9649f41acbba0a3fa267'
+  version '2.10.8'
+  sha256 '22739b12beff621cf538f35f0813d770244257921789596c0f3999fef998c0bd'
 
   url "https://www.mac-dive.com/downloads/MacDive_#{version}.dmg"
   appcast 'https://www.mac-dive.com/shimmer/?appcast&appName=MacDive'
