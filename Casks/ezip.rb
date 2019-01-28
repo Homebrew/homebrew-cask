346df25425d07469a6653b01736a8aac7e3fc815
Vítor Galvão <info@vitorgalvao.com>
2019-01-28 14:58:29 +0000

    Update ezip.rb

--- Casks/ezip.rb
+++ Casks/ezip.rb
@@ -3,6 +3,7 @@ cask 'ezip' do
   sha256 '8b39049f12656fc7ec9dfe50579c1ce39917af34999a118165747c8aeec109b7'
 
   url "https://cdn.awehunt.com/ezip/release/eZip_V#{version}.dmg"
+  appcast 'https://ezip.awehunt.com/'
   name 'ezip'
   homepage 'http://ezip.awehunt.com/'
 
