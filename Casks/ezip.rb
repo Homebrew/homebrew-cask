06669a9c72b11466fbd9bae40d6c5608a3c8bad8
7IN0SAN9 <me@7in0.me>
2019-01-28 12:42:00 +0800

    Add eZip 1.5.2

---
+++ Casks/ezip.rb
@@ -0,0 +1,14 @@
+cask 'ezip' do
+  version '1.5.2'
+  sha256 '8b39049f12656fc7ec9dfe50579c1ce39917af34999a118165747c8aeec109b7'
+
+  url "https://cdn.awehunt.com/ezip/release/eZip_V#{version}.dmg"
+  name 'ezip'
+  homepage 'http://ezip.awehunt.com/'
+
+  app 'eZip.app'
+
+  zap trash: [
+               '~/Library/Containers/com.jinghaoshe.ezip',
+             ]
+end
