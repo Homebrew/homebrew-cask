5bd079a390f3a03d4d6b4cc6836994423a1ce86d
Work <brigitte@corecode.io>
2019-01-29 16:30:06 +0100

    Update omniplan to 3.11

--- Casks/omniplan.rb
+++ Casks/omniplan.rb
@@ -1,13 +1,13 @@
 cask 'omniplan' do
-  version '3.10.4'
-  sha256 'a30728e72ae970dbf37b2ef9942a6b54267aa3456288dcc1815f20b44667e9e5'
+  version '3.11'
+  sha256 '9956912c5759f3a58a21d04545791a4ec5d5202f0a395dab50a0c28b58146968'
 
-  url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniPlan-#{version}.dmg"
+  url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniPlan-#{version}.dmg"
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniPlan#{version.major}"
   name 'OmniPlan'
   homepage 'https://www.omnigroup.com/omniplan/'
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :high_sierra'
 
   app 'OmniPlan.app'
 
