62bf2fd6ae67b20cb4cffbddd4d3bce2c408772e
Julian Mayer <julian@corecode.io>
2019-01-29 14:02:38 +0100

    Update fontlab to 6.1.2.6927

--- Casks/fontlab.rb
+++ Casks/fontlab.rb
@@ -1,8 +1,9 @@
 cask 'fontlab' do
-  version :latest
-  sha256 :no_check
+  version '6.1.2.6927'
+  sha256 'ab5079a206cb1ddda0f39889ea10ccf6bf53b5ffd5bacceabb6ace99074db2b8'
 
-  url 'https://download.fontlab.com/fontlab-vi/get-mac.php'
+  url 'https://download.fontlab.com/fontlab-vi/upd-mac.php'
+  appcast 'https://download.fontlab.com/fontlab-vi/appcast-mac.xml'
   name 'Fontlab'
   homepage 'https://www.fontlab.com/font-editor/fontlab-vi'
 
