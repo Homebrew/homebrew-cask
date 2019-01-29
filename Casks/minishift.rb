c5976438c1f8d62ad7f965154c3a51dc32029fb2
yanaga <edson@yanaga.io>
2019-01-29 17:18:25 -0500

    Update minishift to 1.30.0

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -1,6 +1,6 @@
 cask 'minishift' do
-  version '1.28.0'
-  sha256 '31d82cacdee696e85810992788208f82a0339cfca6e4f7c84cccdbe7c315fd3f'
+  version '1.30.0'
+  sha256 '36b8c262c6d78820258be9e968d88fae9856bf73466d29edb6b261447f883760'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom'
