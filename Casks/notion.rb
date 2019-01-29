b6965972feb467f84248873718fba1c4828b6fd2
admin <admin@admin.local>
2019-01-29 21:13:52 +0100

    Update notion to 0.6.0

--- Casks/notion.rb
+++ Casks/notion.rb
@@ -1,6 +1,6 @@
 cask 'notion' do
-  version '0.3.0'
-  sha256 '6cfde9d52086fe1888477fa226c0c8df16360759352ca7d5c4ee6603f3b37eba'
+  version '0.6.0'
+  sha256 '611f28e92a315fd019584c45081b6b9a5932b6f254ec130ce4735fb735e22230'
 
   # desktop-release.notion-static.com was verified as official when first introduced to the cask
   url "https://desktop-release.notion-static.com/Notion-#{version}.dmg"
