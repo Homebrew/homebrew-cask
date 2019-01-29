1de5bde97f1e01721fe290397d1ba7bb13b70c04
Randall <ran.dall@icloud.com>
2019-01-28 17:29:23 -0800

    Update archipelago to 3.0.3

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -1,6 +1,6 @@
 cask 'archipelago' do
-  version '3.0.2'
-  sha256 'b80edf78e024cc780c07f00df8af3fd212acce4326bbf3f573f3939807fe8709'
+  version '3.0.3'
+  sha256 'a0337f45d124422bacae24e655ecef88769717db2edf88f62cd88f3ca7573e3c'
 
   url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}.dmg"
   appcast 'https://github.com/npezza93/archipelago/releases.atom'
