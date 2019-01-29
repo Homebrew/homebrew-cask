4b44145388313114ec0d0eb56ff305f8aac5c6a2
tiiiecherle <tiiiecherle@gmail.com>
2019-01-29 10:41:35 +0100

    Update xnconvert to 1.78

--- Casks/xnconvert.rb
+++ Casks/xnconvert.rb
@@ -1,6 +1,6 @@
 cask 'xnconvert' do
-  version :latest
-  sha256 :no_check
+  version '1.78'
+  sha256 'd386c5579921242af85424df374a1f50a5feb0a2ea7c85232e73cdb8d54baec0'
 
   url 'https://download.xnview.com/XnConvert-mac-x64.tgz'
   name 'XnSoft XnConvert'
