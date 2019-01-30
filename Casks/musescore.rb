0f10e8acf7d0f45f315c4065a61dfe03be651ba6
Riaan van Niekerk <riaanvn@gmail.com>
2019-01-30 13:24:05 +0200

    Update musescore.rb

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -2,7 +2,6 @@ cask 'musescore' do
   version '3.0.2.20666'
   sha256 '4f31564d3795fe265d229bcebb33999dd41a198be2fca0c6275033019714576a'
 
-  # ftp.osuosl.org/pub/musescore was verified as official when first introduced to the cask
   # 3.0.1 is the last on ftp.osuosl.org/pub/musescore
   # 3.0.2 is available at https://download.musescore.com/releases/MuseScore-3.0.2 (you cannot browse the parent dir). 
   url "https://download.musescore.com/releases/MuseScore-#{version.major_minor_patch}/MuseScore-#{version.major_minor_patch}.dmg"
