0ab7ce5ee5d5474413efa594091b3477be33f74b
Anver Housseini <contact@anver.ch>
2019-01-30 02:19:58 +0100

    Update onlyoffice to 5.2.2 (#58110)
    * Add Kerio Connect.app v9.2.5.9920
    
    * Update onlyoffice to 5.2.2
    
    * Delete kerio-connect.rb

--- Casks/onlyoffice.rb
+++ Casks/onlyoffice.rb
@@ -1,6 +1,6 @@
 cask 'onlyoffice' do
-  version '5.2.1'
-  sha256 'e6586f3385fad1574b35a0bc2f5fd4fc7efc2a7348eda1c6ebcd129b24fd139f'
+  version '5.2.2'
+  sha256 '61f2ba27cb69ef03561228c653575e35ee4d7856faed73853c596a24272cec2a'
 
   url "https://download.onlyoffice.com/install/desktop/editors/mac/updates/onlyoffice/ONLYOFFICE-#{version}.zip"
   appcast 'https://download.onlyoffice.com/install/desktop/editors/mac/onlyoffice.xml'
