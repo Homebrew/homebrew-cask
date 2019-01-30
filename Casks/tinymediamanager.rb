6d0db1550a1ccbd811738de2fc120022fa5daecd
Sean Leather <sean.leather@gmail.com>
2019-01-30 03:30:14 +0200

    Update tinymediamanager to 2.9.15_69b6104 (#58089)

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,7 +1,6 @@
 cask 'tinymediamanager' do
-  # NOTE! Update depends_on_java when updating the version.
-  version '2.9.14_096b083'
-  sha256 'd575154fa03d4b4b329cbea09d74c9ed38f60c01950b56342fb17908ad20cf60'
+  version '2.9.15_69b6104'
+  sha256 '7a1aa341d9701b0adc4c7a60d6183aaff1cd4efb10c013de82265020f6fcf84a'
 
   url "https://release.tinymediamanager.org/dist/tmm_#{version}_mac.zip"
   appcast 'https://release.tinymediamanager.org/'
@@ -11,9 +10,6 @@ cask 'tinymediamanager' do
   app 'tinyMediaManager.app'
 
   caveats do
-    # The Java dependency should be relaxed to 8+ on the next release.
-    # Keeping it at 8 for this version only.
-    # See https://github.com/tinyMediaManager/tinyMediaManager/pull/437
-    depends_on_java '8'
+    depends_on_java '8+'
   end
 end
