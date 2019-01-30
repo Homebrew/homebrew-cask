2f73ff86b72f704a29ee3a711fa21a77a87a26ae
nihalsharma <nihaldps@gmail.com>
2019-01-30 15:06:22 +0530

    Add Clock Bar 1.0

---
+++ Casks/clock-bar.rb
@@ -0,0 +1,10 @@
+cask 'clock-bar' do
+  version '1.0'
+  sha256 '88d1ee551f2edbe25e12098dbdbb2edc5f29e21cd52095e5ea275667a4cbffd1'
+
+  url 'https://github.com/nihalsharma/Clock-Bar/files/1801968/Clock.Bar.app.zip'
+  name 'Clock Bar'
+  homepage 'https://github.com/nihalsharma/Clock-Bar/blob/master/README.md'
+
+  app 'Clock Bar.app'
+end
