3ea38c06e95e3c580763d9f7e8924856737447e7
Vítor Galvão <info@vitorgalvao.com>
2019-01-30 15:44:26 +0000

    Update wireguardstatusbar.rb

--- Casks/wireguardstatusbar.rb
+++ Casks/wireguardstatusbar.rb
@@ -1,8 +1,8 @@
 cask 'wireguardstatusbar' do
-  version '1.23-128'
+  version '1.23,128'
   sha256 'c3abe28bf86238986cf887331825c3bfcbb48af5ae01f307133c716d8b793b35'
 
-  url "https://github.com/aequitas/macos-menubar-wireguard/releases/download/#{version.split('-')[0]}/WireGuardStatusbar-#{version}.dmg"
+  url "https://github.com/aequitas/macos-menubar-wireguard/releases/download/#{version.before_comma}/WireGuardStatusbar-#{version.before_comma}-#{version.after_comma}.dmg"
   appcast 'https://github.com/aequitas/macos-menubar-wireguard/releases.atom'
   name 'WireGuard Status Bar'
   homepage 'https://github.com/aequitas/macos-menubar-wireguard'
