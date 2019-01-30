b023f342891acf66298adaae5950e9e798def7b4
Xiangjun Zhang <easedays@gmail.com>
2019-01-31 00:10:45 +0800

    add WireGuard status bar  1.23-128 (#58145)
    * add WireGuard status bar
    
    * add appcast for wireguard status bar 1.23-128
    
    * Update wireguardstatusbar.rb

---
+++ Casks/wireguardstatusbar.rb
@@ -0,0 +1,11 @@
+cask 'wireguardstatusbar' do
+  version '1.23,128'
+  sha256 'c3abe28bf86238986cf887331825c3bfcbb48af5ae01f307133c716d8b793b35'
+
+  url "https://github.com/aequitas/macos-menubar-wireguard/releases/download/#{version.before_comma}/WireGuardStatusbar-#{version.before_comma}-#{version.after_comma}.dmg"
+  appcast 'https://github.com/aequitas/macos-menubar-wireguard/releases.atom'
+  name 'WireGuard Status Bar'
+  homepage 'https://github.com/aequitas/macos-menubar-wireguard'
+
+  app 'WireGuardStatusbar.app'
+end
