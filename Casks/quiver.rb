1fc03ab5febfd1706b8951afbb997569c4b5ed78
Yassine ElBadaoui <byassine52@users.noreply.github.com>
2019-01-30 13:34:04 +0900

    Add Quiver 3.2.4

---
+++ Casks/quiver.rb
@@ -0,0 +1,10 @@
+cask 'quiver' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://happenapps.com/downloads/QuiverFreeTrial.zip'
+  name 'Quiver'
+  homepage 'http://happenapps.com/'
+
+  app 'Quiver.app'
+end
