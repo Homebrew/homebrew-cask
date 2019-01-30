41571308bc736e4ca715d2cdd399abff9b33bc2d
Julian Mayer <julian@corecode.io>
2019-01-30 22:24:28 +0100

    Update freshback to 1.8

--- Casks/freshback.rb
+++ Casks/freshback.rb
@@ -1,8 +1,8 @@
 cask 'freshback' do
-  version :latest
-  sha256 :no_check
+  version '1.8'
+  sha256 '2755dbf5b96211677536b50675d0b47dfa81695783d05150c6856e86fb70e261'
 
-  url 'http://arkanath.com/FreshBackMac/FreshBackMac.zip'
+  url "http://arkanath.com/FreshBackMac/FreshBackMac_v#{version.dots_to_underscores}.zip"
   name 'FreshBackMac'
   homepage 'http://arkanath.com/FreshBackMac/'
 
