5a43b7f33f76a0daa8bf1d8d554cac92bc09668b
Vítor Galvão <info@vitorgalvao.com>
2019-01-30 17:26:05 +0000

    Update musescore.rb

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -2,6 +2,7 @@ cask 'musescore' do
   version '3.0.2.20666'
   sha256 '4f31564d3795fe265d229bcebb33999dd41a198be2fca0c6275033019714576a'
 
+  # musescore.com was verified as official when first introduced to the cask
   url "https://download.musescore.com/releases/MuseScore-#{version.major_minor_patch}/MuseScore-#{version.major_minor_patch}.dmg"
   appcast "https://sparkle.musescore.org/stable/#{version.major}/macos/appcast.xml"
   name 'MuseScore'
