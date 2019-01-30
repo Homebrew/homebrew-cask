e2d24f35954029567491319f064231c330b9122b
suschizu <brigitte@corecode.io>
2019-01-30 02:13:55 +0100

    Update traverse to 0.10.7 (#58095)

--- Casks/traverse.rb
+++ Casks/traverse.rb
@@ -1,6 +1,6 @@
 cask 'traverse' do
-  version '0.10.6'
-  sha256 'bd4e252c59b2787a9102022bd7710c712a093196f5ba983cb6050500bb867ddc'
+  version '0.10.7'
+  sha256 '2f34be44f24c593b996e2d0935ab230d1c4a546972f0add30e5d0daa4feb7be2'
 
   # github.com/jasonraimondi/traverse was verified as official when first introduced to the cask
   url "https://github.com/jasonraimondi/traverse/releases/download/v#{version}/Traverse-#{version}-mac.zip"
