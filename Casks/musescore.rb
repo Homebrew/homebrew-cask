e482fdbee50f4db6b6620a7a3c75e6fda88081ef
Vítor Galvão <info@vitorgalvao.com>
2019-01-30 15:51:20 +0000

    Update musescore.rb

--- Casks/musescore.rb
+++ Casks/musescore.rb
@@ -2,8 +2,6 @@ cask 'musescore' do
   version '3.0.2.20666'
   sha256 '4f31564d3795fe265d229bcebb33999dd41a198be2fca0c6275033019714576a'
 
-  # 3.0.1 is the last on ftp.osuosl.org/pub/musescore
-  # 3.0.2 is available at https://download.musescore.com/releases/MuseScore-3.0.2 (you cannot browse the parent dir). 
   url "https://download.musescore.com/releases/MuseScore-#{version.major_minor_patch}/MuseScore-#{version.major_minor_patch}.dmg"
   appcast "https://sparkle.musescore.org/stable/#{version.major}/macos/appcast.xml"
   name 'MuseScore'
