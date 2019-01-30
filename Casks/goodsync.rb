98da6f54510a6a5ddeaede28ab840dbaef3d1468
rjpontefract <rjpontefract@users.noreply.github.com>
2019-01-30 14:18:28 +1300

    Update goodsync to 10.9.22.2 (#58118)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.9.20.0'
-  sha256 '71971679fdb9caaf5f17dffa1b477b52ab165828c7fba549cb27834869ed7537'
+  version '10.9.22.2'
+  sha256 '0df1f59d0bc04c19fcac5ed38f5dcae98f85798069c332433f015e7df9395daf'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/8b491acdaa8942108b5d8b019be7fcef'
