62823cbc5b060e79d275ae946b0450f943520c91
Randall <17261190+ran-dall@users.noreply.github.com>
2019-01-29 17:22:06 -0800

    Update superproductivity to 1.999.1000 (#58081)

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,6 +1,6 @@
 cask 'superproductivity' do
-  version '1.10.56'
-  sha256 '8deec886bf25ebae6fae80551469ea0dbdc5736ef72cbdf26f5c1cd160cb5150'
+  version '1.999.1000'
+  sha256 '4545ca2b2d994e71e2c14ca16dfb989b2df31a078295a5291d480732d864761c'
 
   # github.com/johannesjo/super-productivity was verified as official when first introduced to the cask
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip"
