97318cffea1c0ee1ac2d437b0fc1b7290149b72f
SergioOS <ortega@tune-is.de>
2019-01-30 15:23:37 +0100

    Update manager to 19.1.33

--- Casks/manager.rb
+++ Casks/manager.rb
@@ -1,6 +1,6 @@
 cask 'manager' do
-  version '19.1.29'
-  sha256 'dd1dd10ad680edd92eae8efa0a656e271865418ef8960598496a9c5908568eff'
+  version '19.1.33'
+  sha256 'a3a1cfff3fa59567630f8b61b97c67d1f72337c2a498f86d0f2c8c02dd4aaade'
 
   # d2ap5zrlkavzl7.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2ap5zrlkavzl7.cloudfront.net/#{version}/Manager.dmg"
