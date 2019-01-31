edce896dd4433d800093ca6d0e51ce3bc8f8d8b3
CoreCode <core-code@users.noreply.github.com>
2019-01-31 02:33:41 +0100

    Update omnioutliner to 5.4.1 (#58175)

--- Casks/omnioutliner.rb
+++ Casks/omnioutliner.rb
@@ -2,7 +2,7 @@ cask 'omnioutliner' do
   version '5.4.1'
   sha256 '2f7147aa867ae2ba768ecdef58cc085df64216e123f836c7653d627426d6ffcd'
 
-  url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniOutliner-#{version}.dmg"
+  url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniOutliner-#{version}.dmg"
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniOutliner#{version.major}"
   name 'OmniOutliner'
   homepage 'https://www.omnigroup.com/omnioutliner/'
