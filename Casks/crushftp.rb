ea16c180ae821696c966c6745a745f721465983d
Vítor Galvão <info@vitorgalvao.com>
2019-01-31 21:14:34 +0000

    Update crushftp.rb

--- Casks/crushftp.rb
+++ Casks/crushftp.rb
@@ -1,12 +1,12 @@
 cask 'crushftp' do
-  version :latest
-  sha256 :no_check
+  version '9'
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url 'https://www.crushftp.com/CrushFTP9_OSX.zip'
-  name 'CrushFTP9'
+  url "https://www.crushftp.com/CrushFTP#{version}_OSX.zip"
+  name 'CrushFTP'
   homepage 'https://www.crushftp.com/'
 
-  app 'CrushFTP9_OSX/CrushFTP9.app'
+  app "CrushFTP#{version}_OSX/CrushFTP#{version}.app"
 
   caveats do
     depends_on_java
