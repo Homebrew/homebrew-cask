d590609b663f9b6b2222f27be009dfd3997413df
CoreCode <core-code@users.noreply.github.com>
2019-01-31 02:33:12 +0100

    Update freshback to 1.8 (#58170)
    * Update freshback to 1.8
    
    * Update freshback.rb

--- Casks/freshback.rb
+++ Casks/freshback.rb
@@ -1,10 +1,11 @@
 cask 'freshback' do
-  version :latest
-  sha256 :no_check
+  version '1.8'
+  sha256 '2755dbf5b96211677536b50675d0b47dfa81695783d05150c6856e86fb70e261'
 
-  url 'http://arkanath.com/FreshBackMac/FreshBackMac.zip'
+  url "https://arkanath.com/FreshBackMac/FreshBackMac_v#{version.dots_to_underscores}.zip"
+  appcast 'https://arkanath.com/FreshBackMac/changelog.html'
   name 'FreshBackMac'
-  homepage 'http://arkanath.com/FreshBackMac/'
+  homepage 'https://arkanath.com/FreshBackMac/'
 
   app 'FreshBackMac.app'
 end
