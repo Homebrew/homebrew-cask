74ea97dc9495333fa4f09794940d1fdd7e2d3e3c
brigitte zmoelnig <brigitte@corecode.io>
2019-02-01 00:38:48 +0100

    Update turtl to 0.7.2.5

--- Casks/turtl.rb
+++ Casks/turtl.rb
@@ -1,6 +1,6 @@
 cask 'turtl' do
-  version '0.7.2.3'
-  sha256 '092c200a89d88521a7679ae1c7cefdbbfedc3a7263763a51ec7def9a9d3a34ee'
+  version '0.7.2.5'
+  sha256 '266ad5026910431e1dd16c77b558c8503b178409ed486a98423491fc050b7b19'
 
   # github.com/turtl/desktop was verified as official when first introduced to the cask
   url "https://github.com/turtl/desktop/releases/download/v#{version}/turtl-#{version}-osx.zip"
