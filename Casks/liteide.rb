5206bf0c77bbcfcff967a66727acce82ed6af1b1
mars <marshalys@gmail.com>
2019-01-31 11:19:55 +0800

    Update liteide to 35.4 (#58188)
    Update liteide to 35.4

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask 'liteide' do
-  version '35.3'
-  sha256 'e95356434468157b67bf956bf94e05307d439c163c5de81bacd1308a567f61bb'
+  version '35.4'
+  sha256 '5a7684247f918302d43b64f173018d6b8fb7096061444d5654e8c17719d63299'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
   url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.9.5.zip"
