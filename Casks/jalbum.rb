96e7aa2667e932a10cd774eb893c4386942fd158
CoreCode <core-code@users.noreply.github.com>
2019-02-01 16:14:56 +0100

    Update jalbum to 17.1.5 (#58244)

--- Casks/jalbum.rb
+++ Casks/jalbum.rb
@@ -1,6 +1,6 @@
 cask 'jalbum' do
-  version '17.1.1'
-  sha256 'b86144622f299f703d650555976b47bd149a1d2a8acdf3fcf378dda2e819bbd8'
+  version '17.1.5'
+  sha256 'f14d519af2d1d0a1e9b1b473dc7c8a455866c0a5de058853677dd20ca568952b'
 
   url "https://download.jalbum.net/download/#{version}/MacOSX/jAlbum.dmg"
   appcast 'https://jalbum.net/en/software/download/previous'
