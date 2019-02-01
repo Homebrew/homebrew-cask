889e46117f6417c15de024bc2bbd68a257569933
suschizu <brigitte@corecode.io>
2019-02-01 02:51:08 +0100

    Update syncmate to 7.3.434 (#58227)

--- Casks/syncmate.rb
+++ Casks/syncmate.rb
@@ -1,6 +1,6 @@
 cask 'syncmate' do
-  version '7.3.427'
-  sha256 '2057a4a851b2f693674754f72ae81053cfb7cda4a0525c799417ba918fa9ad9e'
+  version '7.3.434'
+  sha256 '01caa5bb3a9512cab40eecd4116f74a8c4cae5704d8d1ff63a18d1805813e3c3'
 
   url "https://www.eltima.com/download/syncmate-update/SyncMate_#{version}.zip"
   appcast 'https://cdn.eltima.com/download/syncmate-update/syncmate6.xml'
