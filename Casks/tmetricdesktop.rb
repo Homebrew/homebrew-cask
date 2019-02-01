5cf4ee44882817565944d8da769b7a37aa1e623b
Randall <ran.dall@icloud.com>
2019-02-01 12:40:35 -0800

    Fixed `style`
    Signed-off-by: Randall <ran.dall@icloud.com>

--- Casks/tmetricdesktop.rb
+++ Casks/tmetricdesktop.rb
@@ -8,7 +8,7 @@ cask 'tmetricdesktop' do
 
   pkg "tmetric_desktop_#{version}.pkg"
 
-  uninstall signal: ['TERM', 'com.company.TMetricDesktop'],
-  			pkgutil: 'com.company.TMetricDesktop',
-  			delete: '/Applications/TMetricDesktop'
+  uninstall signal:  ['TERM', 'com.company.TMetricDesktop'],
+            pkgutil: 'com.company.TMetricDesktop',
+            delete:  '/Applications/TMetricDesktop'
 end
