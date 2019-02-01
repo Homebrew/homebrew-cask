937b1e74e979a8ecad36c4e9c4f54f8b6a6154d8
admin <admin@admin.local>
2019-02-01 21:49:56 +0100

    Update bettertouchtool to 2.701

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -6,8 +6,8 @@ cask 'bettertouchtool' do
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}_final_10_9.zip"
   else
-    version '2.698'
-    sha256 '2818d5d77d1e973b9bbfe896529869f01f18fbae5ce383e766e5192eb41b107c'
+    version '2.701'
+    sha256 '56635cb89f08d798846a80c2536d943b6ff4838a606cc874a3bd34ad3b80db3a'
 
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}.zip"
