6371360a805d1b4782542ebf66fb757bf154292e
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-01 07:13:25 -0800

    Update tribler to 7.2.0 (#58256)

--- Casks/tribler.rb
+++ Casks/tribler.rb
@@ -1,9 +1,9 @@
 cask 'tribler' do
-  version '7.1.5'
-  sha256 '81f4cd57cd23736e9705da022adcf4582f84ec6e6b594a58a7543be310502c01'
+  version '7.2.0'
+  sha256 '6f65237a55c48313107675e9a570a3e73522a6d3a6adcf4bce8fbaad743c78ab'
 
   # github.com/Tribler/tribler was verified as official when first introduced to the cask
-  url "https://github.com/Tribler/tribler/releases/download/V#{version}/Tribler-#{version}.dmg"
+  url "https://github.com/Tribler/tribler/releases/download/v#{version}/Tribler-#{version}.dmg"
   appcast 'https://github.com/Tribler/tribler/releases.atom'
   name 'Tribler'
   homepage 'https://www.tribler.org/'
