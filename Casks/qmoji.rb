deffb371eb8d04c50912103cdd7f798964d49e93
János Orcsik <orcsikj@gmail.com>
2019-02-01 20:44:59 +0100

    Update qmoji to 1.3.1 (#58276)

--- Casks/qmoji.rb
+++ Casks/qmoji.rb
@@ -1,6 +1,6 @@
 cask 'qmoji' do
-  version '1.3'
-  sha256 '094088d3e9bba789f4806eed99cc55b54cd5734f5ed511acdb6bfbe3c0e39479'
+  version '1.3.1'
+  sha256 'dbe2d6de7bbbba3160434d8638fdccb2cfa192ecd7cbb4e5d573df74e9f740c5'
 
   url "https://github.com/jaredly/qmoji/releases/download/#{version}/qmoji.zip"
   appcast 'https://github.com/jaredly/qmoji/releases.atom'
