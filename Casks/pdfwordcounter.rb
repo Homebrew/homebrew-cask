91ad9de5539cf5493a0593671d4dba1102440aa5
Elijah Z Granet <ezgranet@gmail.com>
2019-02-02 12:36:43 -0800

    Fixed style issues
    Fixing style issues caught out by Travis CI at https://travis-ci.org/Homebrew/homebrew-cask/builds/487945714?utm_source=github_status&utm_medium=notification

--- Casks/pdfwordcounter.rb
+++ Casks/pdfwordcounter.rb
@@ -2,12 +2,11 @@ cask 'pdfwordcounter' do
   version '2.0'
   sha256 '9792fca7a2cccf9a252a2f6c82b2fe5509a5f4d9b4ac000df8c198b92931a4b5'
   
-# github.com/ezgranet/pdfwordcounter was verified as official when first introduced to the cask
-  url "https://github.com/ezgranet/pdfwordcounter/releases/download/2.0/pdfwordcounter.zip"
+  url 'https://github.com/ezgranet/pdfwordcounter/releases/download/2.0/pdfwordcounter.zip'
   appcast 'https://github.com/ezgranet/pdfwordcounter/releases.atom'
   name 'pdfwordcounter'
   homepage 'https://github.com/ezgranet/pdfwordcounter'
-
+  
   app 'pdfwordcounter.app'
 
 end
