9fc39b7f8b2c3568df7d1f9cb9467b8b85d8ef42
CoreCode <core-code@users.noreply.github.com>
2019-02-02 21:24:18 +0100

    Update rstudio.rb

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -4,7 +4,7 @@ cask 'rstudio' do
 
   # rstudio.org was verified as official when first introduced to the cask
   url "https://download1.rstudio.org/RStudio-#{version}.dmg"
-  appcast 'https://www.rstudio.com/products/rstudio/download/'
+  appcast 'https://www.rstudio.com/products/rstudio/release-notes/'
   name 'RStudio'
   homepage 'https://www.rstudio.com/'
 
