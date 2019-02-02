2c18b0b2261183b1ce85fad686bc26f46835df91
Elijah Z Granet <ezgranet@gmail.com>
2019-02-02 12:56:45 -0800

    Actually fixed whitespace errors/empty line errors

--- Casks/pdfwordcounter.rb
+++ Casks/pdfwordcounter.rb
@@ -1,7 +1,7 @@
 cask 'pdfwordcounter' do
   version '2.0'
   sha256 '9792fca7a2cccf9a252a2f6c82b2fe5509a5f4d9b4ac000df8c198b92931a4b5'
-  
+
   url 'https://github.com/ezgranet/pdfwordcounter/releases/download/2.0/pdfwordcounter.zip'
   appcast 'https://github.com/ezgranet/pdfwordcounter/releases.atom'
   name 'pdfwordcounter'
