b52019bf140a90b6ac40b46b1ea37ae96b593e2a
@bugsbunny <neupaneshapath@gmail.com>
2019-02-02 11:59:53 -0700

    Update private-internet-access to new version

--- Casks/private-internet-access.rb
+++ Casks/private-internet-access.rb
@@ -1,20 +1,14 @@
 cask 'private-internet-access' do
-  version '82'
-  sha256 'e849df3ef9c7114b3b163ad5a3183ee9f55f37dc89089c2f4ca92c16cd758f5d'
-
-  url "https://installers.privateinternetaccess.com/download/pia-v#{version}-installer-mac.dmg"
+  version :latest
+  sha256 :no_check
+  url "https://www.privateinternetaccess.com/installer/download_installer_osx_beta"
   name 'Private Internet Access'
   homepage 'https://www.privateinternetaccess.com/'
+  license :commercial
 
-  depends_on macos: '>= :yosemite'
+  installer script: {
+                      executable: "#{staged_path}/Private Internet Access Installer.app/Contents/MacOS/pia-installer",
+                      sudo:       true,
+                    }
 
-  installer manual: 'Private Internet Access Installer.app'
-
-  postflight do
-    set_ownership '~/.pia_manager'
-  end
-
-  uninstall delete: '/Applications/Private Internet Access.app'
-
-  zap trash: '~/.pia_manager'
 end
