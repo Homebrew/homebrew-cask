662a664604f67585d63aaf8c95153e006f4376a7
Alexander Barton <alex@barton.de>
2019-02-02 20:40:22 +0100

    Add LittleSecrets 1.9.1

---
+++ Casks/littlesecrets.rb
@@ -0,0 +1,10 @@
+cask 'littlesecrets' do
+  version '1.9.1'
+  sha256 '231850150ff7f0a31ff6b1bc0b3f6502ca241f7c5e22e04b8521306b52a5aeee'
+
+  url 'https://www.mani.de/download/littlesecrets/LittleSecrets1.9.1.dmg'
+  name 'LittleSecrets'
+  homepage 'https://www.mani.de/de/littlesecrets/'
+
+  app 'LittleSecrets.app'
+end
