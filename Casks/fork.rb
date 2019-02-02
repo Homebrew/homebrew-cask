03e5d52c4e76a6fdc98475a057bf969b3ce954a7
Randall <ran.dall@icloud.com>
2019-02-01 19:23:37 -0800

    Update fork to 1.0.73.4

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,6 +1,6 @@
 cask 'fork' do
-  version '1.0.72.2'
-  sha256 'df1f7d18eb8ff242dcc838ba7e0a9a7d14a5bb4c24e2eed66e3227b9132e8438'
+  version '1.0.73.4'
+  sha256 'bfb2813ad201e432162567c2946f17754dda0293ff213f3e79ad825a6030dde6'
 
   url 'https://git-fork.com/update/files/Fork.dmg'
   appcast 'https://git-fork.com/update/feed.xml'
