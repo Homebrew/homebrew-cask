880a4c8b3f10b8d7543ff62123d8bbb4063657a9
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-02 02:01:34 -0800

    Update nordvpn to 4.11.2 (#58304)

--- Casks/nordvpn.rb
+++ Casks/nordvpn.rb
@@ -1,6 +1,6 @@
 cask 'nordvpn' do
-  version '4.10.3'
-  sha256 '51f76cfde8e604240fdb5695d430ba318b8350b9a299491d7b18ed9ca10cf14a'
+  version '4.11.2'
+  sha256 'ba8bab984c2b2338add41173e0f73b338ebd6fd2ad8363f7f820b5ca6c5fc9b6'
 
   # downloads.nordcdn.com/apps was verified as official when first introduced to the cask
   url "https://downloads.nordcdn.com/apps/macos/generic/NordVPN-OpenVPN/#{version}/NordVPN.pkg"
