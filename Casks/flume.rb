01fdb9600473944f79d8769245df20cd7eaa07cc
Max <msal.coding@gmail.com>
2019-02-02 11:25:25 +0100

    Update flume to 2.8.6

--- Casks/flume.rb
+++ Casks/flume.rb
@@ -1,6 +1,6 @@
 cask 'flume' do
-  version '2.8.5.3'
-  sha256 '8d42df3e1c948bb4ff0a87857b7cb780d190bbff452319f21f84bc7c6b241d87'
+  version '2.8.6'
+  sha256 '817a843a82c1ede234fb7ff147fee00bcd713b095951aea9395dc1aa61d7cd30'
 
   url "https://flumeapp.com/files/Flume-#{version}.zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/c88c56b02dcd4dd3acceb6d7a24f7122'
