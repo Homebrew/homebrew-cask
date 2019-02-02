95f207c807f553ee1012592b995941afe0d69598
HackingGate <i@hackinggate.com>
2019-02-02 20:31:47 +0900

    Update musixmatch to 0.21.24

--- Casks/musixmatch.rb
+++ Casks/musixmatch.rb
@@ -1,6 +1,6 @@
 cask 'musixmatch' do
-  version '0.21.21'
-  sha256 '6c72699837737c25a5c32bf424764a0b005ddcbf10777c8556f757a13862f256'
+  version '0.21.24'
+  sha256 'd91e3bbfd82beca8a1960efbb88ef6d5601b2a0381cd64a4d274f097da9ba8b9'
 
   url "https://download-app.musixmatch.com/download/Musixmatch-#{version}.dmg"
   appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=http://download-app.musixmatch.com/download/osx'
