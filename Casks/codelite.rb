43be9247b2d4c8a50d2ca14c0a5f881c7e095387
CoreCode <core-code@users.noreply.github.com>
2019-02-04 22:39:44 +0100

    Update codelite.rb

--- Casks/codelite.rb
+++ Casks/codelite.rb
@@ -1,9 +1,10 @@
 cask 'codelite' do
   version '12.0.0'
   sha256 '2e7de18f3f1ab8095e71f46e14945eb922a1600ceb071f42c8faf4d483abb10f'
-  appcast 'https://github.com/eranif/codelite/releases.atom'
 
   url 'https://downloads.codelite.org/downloads.php?osx'
+  appcast 'https://github.com/eranif/codelite/releases.atom'
+
   name 'CodeLite'
   homepage 'https://codelite.org/'
 
