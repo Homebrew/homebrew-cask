a6e9dcc725f15c274d3e7951377524169bd72e57
CoreCode <core-code@users.noreply.github.com>
2019-02-04 22:57:54 +0100

    Update qiyimedia.rb

--- Casks/qiyimedia.rb
+++ Casks/qiyimedia.rb
@@ -2,10 +2,10 @@ cask 'qiyimedia' do
   version '5.4.10'
   sha256 '6132c2644c35be55bd8c45416404986712c8ec8d03a98a4d9805fced8dd8c3d2'
 
-  url 'http://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg'
-  appcast 'http://app.iqiyi.com/mac/player/index.html'
+  url 'https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg'
+  appcast 'https://app.iqiyi.com/mac/player/index.html'
   name '爱奇艺视频'
-  homepage 'http://app.iqiyi.com/mac/player/index.html'
+  homepage 'https://app.iqiyi.com/mac/player/index.html'
 
   app '爱奇艺.app'
 
