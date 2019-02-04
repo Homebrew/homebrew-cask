df1d7ad4643e1dae4ffd47da24ef8be4bbcdbdd7
Andreya <andykostov@gmail.com>
2019-02-04 13:36:53 +0200

    Update sapmachine-jdk to 11.0.2

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -1,9 +1,9 @@
 cask 'sapmachine-jdk' do
-  version '11.0.1,13-0'
-  sha256 '600fc431d96fbc0b22a3cb160a1b1fbe878b887efd5be99668f3cf9677aa11cc'
+  version '11.0.2'
+  sha256 'c17526aca0891ab291916faeca7a6cdcef77d63de8acb8362c1af0eb95a9c680'
 
-  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}+#{version.after_comma}/sapmachine-jdk-#{version.before_comma}.#{version.after_comma.split('-').first}_osx-x64_bin.tar.gz"
-  appcast "https://sap.github.io/SapMachine/latest/#{version.before_comma}"
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.tar.gz"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
   name 'SapMachine JDK'
   homepage 'https://github.com/SAP/SapMachine'
 
@@ -11,23 +11,23 @@ cask 'sapmachine-jdk' do
     system_command '/bin/mv',
                    args: [
                            '-f', '--',
-                           "#{staged_path}/sapmachine-jdk-#{version.before_comma}.jdk",
-                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk"
+                           "#{staged_path}/sapmachine-jdk-#{version}.jdk",
+                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk"
                          ],
                    sudo: true
 
     system_command '/bin/mkdir',
-                   args: ['-p', '--', "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries"],
+                   args: ['-p', '--', "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk/Contents/Home/bundle/Libraries"],
                    sudo: true
 
     system_command '/bin/ln',
                    args: [
                            '-nsf', '--',
-                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib",
-                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
+                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk/Contents/Home/lib/server/libjvm.dylib",
+                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
                          ],
                    sudo: true
   end
 
-  uninstall delete: "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk"
+  uninstall delete: "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk"
 end
