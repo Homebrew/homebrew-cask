63438100d7702fc14ef7c666a9298dc36c0378a0
Julian Mayer <julian@corecode.io>
2019-02-05 16:56:40 +0100

    Update liteide to 35.5

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask 'liteide' do
-  version '35.4-1'
-  sha256 '7f95ef6c2fcb924fd1d98d16c6223d46b979194b499061db1cf8c5dc0c5cff7c'
+  version '35.5'
+  sha256 '43a6704d6d48702cdfa2b29fe711518e143da2f879f287c408996400bdfc6181'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
   url "https://github.com/visualfc/liteide/releases/download/x#{version.major_minor}/liteidex#{version}.macos-qt5.9.5.zip"
