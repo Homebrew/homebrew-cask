ab20a156c7ef2fa9580f52810d2c80eb93a3e96b
Randall <ran.dall@icloud.com>
2019-02-04 21:56:34 -0800

    Update umsatz to 8.1.4

--- Casks/umsatz.rb
+++ Casks/umsatz.rb
@@ -1,6 +1,6 @@
 cask 'umsatz' do
-  version '8.1.2'
-  sha256 '120657b35f0eb0afabe69bdd3588f73f7eb5377bbc8961b2b9252374f6908298'
+  version '8.1.4'
+  sha256 '0dc293391a59efeb14ec067956bf2b088110e0df63019e4e81ba37d90cfb4dfe'
 
   # moapp.software was verified as official when first introduced to the cask
   url 'https://download.moapp.software/Umsatz.zip'
