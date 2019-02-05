dcec9113bf88f7a8c62c67da7c6d5c3b1cb571ad
brigitte zmoelnig <brigitte@corecode.io>
2019-02-05 14:52:45 +0100

    Update syncmyl2p to 2.4.0

--- Casks/syncmyl2p.rb
+++ Casks/syncmyl2p.rb
@@ -1,9 +1,9 @@
 cask 'syncmyl2p' do
-  version '2.3.0'
-  sha256 '5501b2138279121ae6f4f8d44680801a7cbd45695bb314033ed5f310b20ea121'
+  version '2.4.0'
+  sha256 '175459183db212c63b168a64a399a275c7d5957abb463336f18364b3ab4744ee'
 
   # github.com/RobertKrajewski/Sync-my-L2P was verified as official when first introduced to the cask
-  url "https://github.com/RobertKrajewski/Sync-my-L2P/releases/download/v#{version}/SyncMyL2P-#{version}-macos.dmg"
+  url "https://github.com/RobertKrajewski/Sync-my-L2P/releases/download/v#{version}/SyncMyL2P-#{version}-osx.dmg"
   appcast 'https://github.com/RobertKrajewski/Sync-my-L2P/releases.atom'
   name 'Sync-my-L2P'
   homepage 'https://www.syncmyl2p.de/'
