d4d539912a57be39872d670655acc0f8870c09c9
Martin Delille <martin@phonations.com>
2019-02-05 13:40:22 +0100

    new formula: MusaicFM 1.1

---
+++ Casks/musaicfm.rb
@@ -0,0 +1,10 @@
+cask 'musaicfm' do
+  version '1.1'
+  sha256 '947c7eb45c680936eb86dc5f2c6f1f188e16d9c7c75bf68bb5216e343c0f6df9'
+
+  url "https://github.com/docterd/MusaicFM/releases/download/#{version}/MusaicFM.saver.zip"
+  name 'MusaicFM Screensaver'
+  homepage 'https://github.com/docterd/MusaicFM'
+
+  screen_saver 'MusaicFM.saver'
+end
