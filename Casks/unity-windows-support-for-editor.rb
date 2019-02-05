c2de1ff2e243bcdfebfd51cfaae19fc9e82c50c8
Taras Postument <tpostument@getsocial.im>
2019-02-05 16:53:09 +0200

    unity-windows-support-for-editor to 2018.3.4f1

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,9 +1,9 @@
 cask 'unity-windows-support-for-editor' do
-  version '2018.3.1f1,bb579dc42f1d'
-  sha256 'e826e4ef546aa2331c0d8688509ba9d9d272bd4f32236227467ecde400e283d1'
+  version '2018.3.4f1,1d952368ca3a'
+  sha256 '8b0be0563a3c17780dd406f5869b63d8cca169e27023079eaa0c23d2764002ab'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.before_comma}.pkg"
-  appcast 'https://unity3d.com/get-unity/download/archive'
+  appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
   name 'Unity Windows (Mono) Build Support'
   homepage 'https://unity3d.com/unity/'
 
