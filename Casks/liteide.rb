276cfa8d28115ea17f4264ea4ed41f5cf97b9f69
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-02-05 23:55:42 +0900

    update liteide35.4 (#58379)
    * update liteide35.4
    
    * fix versioning lite35.4

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,9 +1,9 @@
 cask 'liteide' do
-  version '35.4'
-  sha256 '5a7684247f918302d43b64f173018d6b8fb7096061444d5654e8c17719d63299'
+  version '35.4-1'
+  sha256 '7f95ef6c2fcb924fd1d98d16c6223d46b979194b499061db1cf8c5dc0c5cff7c'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
-  url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.9.5.zip"
+  url "https://github.com/visualfc/liteide/releases/download/x#{version.major_minor}/liteidex#{version}.macos-qt5.9.5.zip"
   appcast 'https://github.com/visualfc/liteide/releases.atom'
   name 'LiteIDE'
   homepage 'http://liteide.org/'
