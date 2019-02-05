8611e083e8281d099d7ee5ed86267862b4567548
Taras Postument <tpostument@getsocial.im>
2019-02-05 16:50:38 +0200

    unity-linux-support-for-editor to 2018.3.4f1

--- Casks/unity-linux-support-for-editor.rb
+++ Casks/unity-linux-support-for-editor.rb
@@ -1,9 +1,9 @@
 cask 'unity-linux-support-for-editor' do
-  version '2018.3.1f1,bb579dc42f1d'
-  sha256 '5283a40df4e797aa401569ea54a316f7e058ecb15ccda07a746b011e2290eed2'
+  version '2018.3.4f1,1d952368ca3a'
+  sha256 '553c0349d341b6c71eab617af0d0cbc18180c1e6b88c052e6fd8bc70832ee6bc'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-#{version.before_comma}.pkg"
-  appcast 'https://unity3d.com/get-unity/download/archive'
+  appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
   name 'Unity Linux Build Support'
   homepage 'https://unity3d.com/unity/'
 
