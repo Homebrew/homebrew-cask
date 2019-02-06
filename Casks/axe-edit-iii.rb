e6bd68729cd02950c1963188917bf378b2b613a1
Tames McTigue <tames@tamerz.com>
2019-02-06 21:28:46 +0300

    Add Axe-Edit III v1.00.13 (#58365)
    * Add Axe-Edit III v1.00.13
    
    Axe-Edit III is an editor for Fractal Audio Systems Axe-FX III Preamp FX
    Processor.
    
    * Added second package that needs to be uninstalled.

---
+++ Casks/axe-edit-iii.rb
@@ -0,0 +1,17 @@
+cask 'axe-edit-iii' do
+  version '1.00.13'
+  sha256 '0985215254eb20173d9afa9137ea11896f2c545f4ac00fb633b6e3ffb75abb81'
+
+  url "https://www.fractalaudio.com/downloads/Axe-Edit-III/Axe-Edit-III-OSX-v#{version.tr('.', 'p')}.dmg"
+  name 'Axe-Edit III'
+  homepage 'https://www.fractalaudio.com/axe-fx-iii-edit/'
+
+  pkg "Axe-Edit-III-OSX-v#{version.tr('.', 'p')}.pkg"
+
+  uninstall pkgutil: [
+                       'com.FractalAudio.AIIIEdit.pkg',
+                       'com.FractalAudio.AIIIEdit.ReleaseNotes.pkg',
+                     ]
+
+  zap trash: '~/Library/Application Support/Fractal Audio/Axe-Edit III'
+end
