debc02b8eb1da58b9e6190e40a1565eae682b327
Vítor Galvão <info@vitorgalvao.com>
2019-02-06 02:21:54 +0000

    Update eduvpn.rb

--- Casks/eduvpn.rb
+++ Casks/eduvpn.rb
@@ -3,7 +3,7 @@ cask 'eduvpn' do
   sha256 '7cf102702de2c8f3d83378399d6f48d12b20a8261d4f61702dca0d2277c192ba'
 
   url "https://app.eduvpn.nl/mac/eduVPN_#{version}.dmg"
-  name 'eduVPN for macOS'
+  name 'eduVPN'
   homepage 'https://app.eduvpn.nl/'
 
   app 'eduVPN.app'
