6ba56f903f0fb0f10983f4dc9d3301904b83a081
Markus Reiter <me@reitermark.us>
2019-02-06 22:10:52 +0100

    Remove workarounds in Java casks. (#58510)

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -4,30 +4,10 @@ cask 'sapmachine-jdk' do
 
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version.before_comma}+#{version.after_comma}/sapmachine-jdk-#{version.before_comma}.#{version.after_comma.split('-').first}_osx-x64_bin.tar.gz"
   appcast "https://sap.github.io/SapMachine/latest/#{version.before_comma}"
-  name 'SapMachine JDK'
+  name 'SapMachine Java Development Kit'
   homepage 'https://github.com/SAP/SapMachine'
 
-  postflight do
-    system_command '/bin/mv',
-                   args: [
-                           '-f', '--',
-                           "#{staged_path}/sapmachine-jdk-#{version.before_comma}.jdk",
-                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk"
-                         ],
-                   sudo: true
+  artifact "sapmachine-jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk"
 
-    system_command '/bin/mkdir',
-                   args: ['-p', '--', "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries"],
-                   sudo: true
-
-    system_command '/bin/ln',
-                   args: [
-                           '-nsf', '--',
-                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib",
-                           "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
-                         ],
-                   sudo: true
-  end
-
-  uninstall delete: "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version.before_comma}.jdk"
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 end
