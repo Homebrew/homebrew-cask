a9a47138386812f6ea8ba96e7ecd062a99d177a1
mchlrmrz <ramirez3494@gmail.com>
2019-02-06 13:07:41 -0500

    Update houdahspot to 4.4 (#58480)

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,6 +1,6 @@
 cask 'houdahspot' do
   version '4.4'
-  sha256 '6219a3f278e4b3a5be824d7b3f9509f0c998e72599341e8687333e36f0febcd5'
+  sha256 'e084d1ecf1bba86cf7c3b8245bee7515f898e9e3e08885861840eca1e8da9b1c'
 
   url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
