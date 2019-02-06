dbd69373809be40f3bd85af795fa4174a81b3f64
Markus Reiter <me@reitermark.us>
2019-02-06 19:22:54 +0100

    Remove workarounds in Java casks.

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -5,66 +5,12 @@ cask 'adoptopenjdk' do
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK11U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.tar.gz"
   appcast 'https://github.com/adoptopenjdk/openjdk11-binaries/releases.atom'
-  name 'AdoptOpenJDK'
+  name 'AdoptOpenJDK Java Development Kit'
   homepage 'https://adoptopenjdk.net/'
 
-  postflight do
-    system_command '/bin/mv',
-                   args: [
-                           '-f', '--', "#{staged_path}/jdk-#{version.before_comma}+#{version.after_comma}",
-                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"
-                         ],
-                   sudo: true
+  artifact "jdk-#{version.before_comma}+#{version.after_comma}", target: "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"
 
-    system_command '/bin/mkdir',
-                   args: ['-p', '--', "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries"],
-                   sudo: true
-
-    system_command '/bin/ln',
-                   args: [
-                           '-nsf', '--',
-                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib",
-                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
-                         ],
-                   sudo: true
-
-    system_command '/usr/libexec/PlistBuddy',
-                   args: [
-                           '-c', "Set :CFBundleGetInfoString AdoptOpenJDK #{version.before_comma}+#{version.after_comma}",
-                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"
-                         ],
-                   sudo: true
-
-    system_command '/usr/libexec/PlistBuddy',
-                   args: [
-                           '-c', "Set :CFBundleIdentifier net.adoptopenjdk.#{version.before_comma}.jdk",
-                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"
-                         ],
-                   sudo: true
-
-    system_command '/usr/libexec/PlistBuddy',
-                   args: [
-                           '-c', "Set :CFBundleName AdoptOpenJDK #{version.before_comma}",
-                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"
-                         ],
-                   sudo: true
-
-    system_command '/usr/libexec/PlistBuddy',
-                   args: [
-                           '-c', "Set :JavaVM:JVMPlatformVersion #{version.before_comma}.#{version.after_comma}",
-                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"
-                         ],
-                   sudo: true
-
-    system_command '/usr/libexec/PlistBuddy',
-                   args: [
-                           '-c', 'Set :JavaVM:JVMVendor AdoptOpenJDK',
-                           "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk/Contents/Info.plist"
-                         ],
-                   sudo: true
-  end
-
-  uninstall delete: "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 
   caveats <<~EOS
     More versions are available in the AdoptOpenJDK tap:
--- Casks/java.rb
+++ Casks/java.rb
@@ -3,30 +3,10 @@ cask 'java' do
   sha256 'f365750d4be6111be8a62feda24e265d97536712bc51783162982b8ad96a70ee'
 
   url "https://download.java.net/java/GA/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}_osx-x64_bin.tar.gz"
-  name 'OpenJDK'
+  name 'OpenJDK Java Development Kit'
   homepage 'https://jdk.java.net/'
 
-  postflight do
-    system_command '/bin/mv',
-                   args: [
-                           '-f', '--',
-                           "#{staged_path}/jdk-#{version.before_comma}.jdk",
-                           "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
-                         ],
-                   sudo: true
+  artifact "jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
 
-    system_command '/bin/mkdir',
-                   args: ['-p', '--', "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries"],
-                   sudo: true
-
-    system_command '/bin/ln',
-                   args: [
-                           '-nsf', '--',
-                           "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib",
-                           "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
-                         ],
-                   sudo: true
-  end
-
-  uninstall delete: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 end
