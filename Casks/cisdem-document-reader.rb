3dd0687720cbb4a283c95ae74bf55025946fec02
CoreCode <core-code@users.noreply.github.com>
2019-02-06 03:23:26 +0100

    Update cisdem-document-reader to 4.0.0 (#58452)

--- Casks/cisdem-document-reader.rb
+++ Casks/cisdem-document-reader.rb
@@ -1,6 +1,6 @@
 cask 'cisdem-document-reader' do
   version '4.0.0'
-  sha256 'cd709f08a4e6e0cc46cfdf047f23c9c900cc8dce24208639e54d46d624502c8b'
+  sha256 '94580363c2e8093097be85494b4c289e954fa1a5b3d31b83bc8e62d59ac7eaf0'
 
   url 'http://download.cisdem.com/cisdem-documentreader.dmg'
   appcast 'https://www.cisdem.com/document-reader-mac/release-notes.html'
