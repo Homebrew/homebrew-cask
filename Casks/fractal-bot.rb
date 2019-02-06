87098d1f6bf4de9827fbb9e51ddedd74efb42d39
Vítor Galvão <info@vitorgalvao.com>
2019-02-06 17:38:27 +0000

    Update fractal-bot.rb

--- Casks/fractal-bot.rb
+++ Casks/fractal-bot.rb
@@ -4,7 +4,7 @@ cask 'fractal-bot' do
 
   url "https://www.fractalaudio.com/downloads/fractal-bot/Fractal-Bot-OSX-v#{version.tr('.', 'p')}.dmg"
   appcast 'https://www.fractalaudio.com/fractal-bot/'
-  name 'Fractal-Bot'
+  name 'Fractal Bot'
   homepage 'https://www.fractalaudio.com/fractal-bot/'
 
   pkg "Fractal-Bot-OSX-v#{version.tr('.', 'p')}.pkg"
