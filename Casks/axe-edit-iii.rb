0c5eb38086af90fd61282aaf43d71aadbaa05039
Vítor Galvão <info@vitorgalvao.com>
2019-02-06 19:07:14 +0000

    axe-edit-iii.rb: add appcast

--- Casks/axe-edit-iii.rb
+++ Casks/axe-edit-iii.rb
@@ -3,6 +3,7 @@ cask 'axe-edit-iii' do
   sha256 '0985215254eb20173d9afa9137ea11896f2c545f4ac00fb633b6e3ffb75abb81'
 
   url "https://www.fractalaudio.com/downloads/Axe-Edit-III/Axe-Edit-III-OSX-v#{version.tr('.', 'p')}.dmg"
+  appcast 'https://www.fractalaudio.com/axe-fx-iii-edit/'
   name 'Axe-Edit III'
   homepage 'https://www.fractalaudio.com/axe-fx-iii-edit/'
 
