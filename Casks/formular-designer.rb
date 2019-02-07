0d81e22ead73c021de1ed156fd571da2a5340b71
brigitte zmoelnig <brigitte@corecode.io>
2019-02-07 15:04:24 +0100

    Update formular-designer to 2019.2.0

--- Casks/formular-designer.rb
+++ Casks/formular-designer.rb
@@ -1,6 +1,6 @@
 cask 'formular-designer' do
-  version '2019.1.0'
-  sha256 '64c3f95da72c023fff4121e2819477ed1ce3ad06d2359dfa271cc8d45c522186'
+  version '2019.2.0'
+  sha256 '744d07ca5381e4645ad6f033c5beaf5fa9804147be0498e00b99e08ba6b24c58'
 
   url 'https://www.lehreroffice.ch/lo/dateien/designer/lo_designer_macos.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Designer'
