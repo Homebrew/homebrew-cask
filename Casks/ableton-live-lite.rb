a7b3d5365fcbb585de088958abdf565718a17753
brigitte zmoelnig <brigitte@corecode.io>
2019-02-07 14:43:24 +0100

    Update ableton-live-lite to 10.0.6

--- Casks/ableton-live-lite.rb
+++ Casks/ableton-live-lite.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-lite' do
-  version '10.0.5'
-  sha256 '3afacecf373116337f4d48bc21b2ce720c11fa3925d47aa5ed83f1289aeb378a'
+  version '10.0.6'
+  sha256 'af276e8ece04eb707204e9fbf7e6a554c60d36d32649762730c09210bd705cff'
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_lite_#{version}_64.dmg"
   appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
