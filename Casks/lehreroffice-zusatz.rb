125dd099a40165997b690349a8734d9dee4e24cb
suschizu <brigitte@corecode.io>
2019-02-07 19:09:56 +0100

    Update lehreroffice-zusatz to 2019.2.0 (#58548)

--- Casks/lehreroffice-zusatz.rb
+++ Casks/lehreroffice-zusatz.rb
@@ -1,6 +1,6 @@
 cask 'lehreroffice-zusatz' do
-  version '2019.1.1'
-  sha256 'a95c1c8daf89043ff8d001aa9c244554ffdab598f763a6491bc03991daca6872'
+  version '2019.2.0'
+  sha256 '49a021d79f9236c403a677107aa20e21c3ff771619b99da1824773386cc830d5'
 
   url 'https://www.lehreroffice.ch/lo/dateien/zusatz/lo_zusatz_macos.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Zusatz'
