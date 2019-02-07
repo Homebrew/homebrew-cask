543c6918a9fb338fef4fa27f8a8cfefc8b17e5a8
optikfluffel <optik@fluffel.io>
2019-02-07 21:08:16 +0100

    Update pgadmin4 to 4.2

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -1,6 +1,6 @@
 cask 'pgadmin4' do
-  version '4.1'
-  sha256 'd3916f063a9e433b1929b9eba9e37cbcd89bcdef5517352604016e3129d73b23'
+  version '4.2'
+  sha256 'c58fdcf66c76c975d83806c609956de0915457cba9756f4ab0a8e2f62f62f7a0'
 
   # ftp.postgresql.org was verified as official when first introduced to the cask
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}.dmg"
