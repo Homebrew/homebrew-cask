ed667e602886892074f856854b3e8a0b55b2bfe6
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-07 06:03:24 -0800

    Update freedom to 2.1 (#58526)

--- Casks/freedom.rb
+++ Casks/freedom.rb
@@ -1,6 +1,6 @@
 cask 'freedom' do
-  version '2.0.5'
-  sha256 '0715908df326be50d159afb59a276eeaa515f53e8d712c79bbebbe81705b13df'
+  version '2.1'
+  sha256 'a670097fe80ddf990bc51692248ea3f9da164abe4a19cb2770954d80ec058cff'
 
   url "https://cdn.freedom.to/installers/updates/mac/#{version}/Freedom.zip"
   appcast 'https://cdn.freedom.to/installers/updates/mac/Appcast.xml'
