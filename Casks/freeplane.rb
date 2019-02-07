129b5fe0126d5d44788ad122bc0bcd7d6e12fda5
Joseph Reagle <joseph.2011@reagle.org>
2019-02-07 11:39:16 -0500

    restore interpolated URL and move '-hotfix1' to version

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -1,9 +1,9 @@
 cask 'freeplane' do
-  version '1.7.5'
+  version '1.7.5-hotfix1'
   sha256 '3095b390a2ffd0d8a9466d18e983f90a8e603c44839da5394ac96d99f4a91102'
 
   # downloads.sourceforge.net/freeplane was verified as official when first introduced to the cask
-  url 'https://downloads.sourceforge.net/freeplane/freeplane%20stable/freeplane_app_jre-1.7.5-hotfix1.dmg'
+  url 'https://downloads.sourceforge.net/freeplane/freeplane%20stable/freeplane_app_jre-#{version}.dmg'
   appcast 'https://sourceforge.net/projects/freeplane/rss?path=/freeplane%20stable'
   name 'Freeplane'
   homepage 'https://freeplane.sourceforge.io/'
