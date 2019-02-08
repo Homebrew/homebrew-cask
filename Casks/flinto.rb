6d38b05e5983c28490d30209ddb82ecc1c6122be
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-08 05:56:36 -0800

    Update flinto to 26.2.2 (#58592)

--- Casks/flinto.rb
+++ Casks/flinto.rb
@@ -1,6 +1,6 @@
 cask 'flinto' do
-  version '26.2.1'
-  sha256 'cee718c49a81ee9c6740c6213a8b87dab95f8366f21bea600d3d19eeef734c8e'
+  version '26.2.2'
+  sha256 '036716ae66f0324203cb784acf8e9b138a7488350767e87b25b76d4b97c4751a'
 
   url "https://www.flinto.com/assets/Flinto-#{version}.dmg"
   appcast 'https://www.flinto.com/appcast.rss'
