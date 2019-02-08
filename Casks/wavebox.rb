4fa3d1cee2986299ba18a03e18fb2d8643351daf
Parker Brown <17183625+parkerbrownxyz@users.noreply.github.com>
2019-02-08 11:24:26 -0500

    Update wavebox to 4.6.0

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,6 +1,6 @@
 cask 'wavebox' do
-  version '4.5.10'
-  sha256 '0aac1be54b390f2c64c5bada798c97cf3683e795354eb990796191834dcec60c'
+  version '4.6.0'
+  sha256 '6d2d90875f7bf7f36bbb898111a4a8c5f74ed4cba402ea5a2c3afce9a0e9fc80'
 
   # github.com/wavebox/waveboxapp was verified as official when first introduced to the cask
   url "https://github.com/wavebox/waveboxapp/releases/download/v#{version}/Wavebox_#{version.dots_to_underscores}_osx.dmg"
