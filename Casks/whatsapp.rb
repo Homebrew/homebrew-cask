17a4226a352e5c054d9b617bfa3d955c912a792b
Yoav Maman <yoavmmn@gmail.com>
2019-02-08 15:53:36 +0200

    Update whatsapp to 0.3.2043 (#58608)

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,6 +1,6 @@
 cask 'whatsapp' do
-  version '0.3.2041'
-  sha256 '08128e3f6993f7b9fe19d9027f36d31bddcfeda316d2fd95bd1d5c6a52536d28'
+  version '0.3.2043'
+  sha256 '8d6407cb57d0321914d108cd4a4dcd4ac05a0654acacdca82ca8d9420915f957'
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64'
