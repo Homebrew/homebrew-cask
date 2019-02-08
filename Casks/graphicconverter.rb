f061a45ccf7b5e8a03bc81cce7071c30ef33a8f0
Randall <ran.dall@icloud.com>
2019-02-07 22:22:45 -0800

    Update graphicconverter to 10.6.9,3340

--- Casks/graphicconverter.rb
+++ Casks/graphicconverter.rb
@@ -1,6 +1,6 @@
 cask 'graphicconverter' do
-  version '10.6.8,3327'
-  sha256 'e20275ea12aecaee0313a1aeffff78aab6ac70392fffeb35066633178a76ee07'
+  version '10.6.9,3340'
+  sha256 '558c79b8db75d6f24b939d785ed72837ed0fe08c4afb0337bcd8a1a7702cb5c3'
 
   # lemkesoft.info was verified as official when first introduced to the cask
   url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.after_comma}.zip"
