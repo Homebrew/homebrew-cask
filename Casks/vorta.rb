935c032e1976a0665d7020ea92f9db4a7c3c575a
Josh Brown <josh@brownj.org>
2019-02-08 11:28:24 -0600

    Update vorta to 0.6.8 (#58616)

--- Casks/vorta.rb
+++ Casks/vorta.rb
@@ -1,6 +1,6 @@
 cask 'vorta' do
-  version '0.6.7'
-  sha256 '03f8c9bce61ff7992c52aefd0ad6f5477965b32ed0b2f3ca049ba5b8dea0b04a'
+  version '0.6.8'
+  sha256 '2f02c624e6210783b5b71165c510fcfabc7a5457505dba4700edf7e6568b6df3'
 
   url "https://github.com/borgbase/vorta/releases/download/v#{version}/vorta-#{version}.dmg"
   appcast 'https://github.com/borgbase/vorta/releases.atom'
