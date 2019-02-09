3774ef1b7de3a1ec0f1cd825581b41f2761d15ec
CoreCode <core-code@users.noreply.github.com>
2019-02-09 20:06:16 +0100

    Update wifi-explorer to 2.5.4 (#58670)

--- Casks/wifi-explorer.rb
+++ Casks/wifi-explorer.rb
@@ -2,7 +2,7 @@ cask 'wifi-explorer' do
   version '2.5.4'
   sha256 '2dd3a6b42977c77aaa3df7800313306feed172cedbfd58e4b48db737feb42870'
 
-  url 'https://www.adriangranados.com/apps/wifiexplorer-download'
+  url "https://www.adriangranados.com/downloads/wifiexplorer_#{version}.zip"
   appcast 'https://www.adriangranados.com/appcasts/wifiexplorercast.xml'
   name 'WiFi Explorer'
   homepage 'https://www.adriangranados.com/apps/wifi-explorer'
