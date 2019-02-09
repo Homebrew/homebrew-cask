8d29aa1802fa1d41b9213cb27bb7f3c021f93ef1
suschizu <brigitte@corecode.io>
2019-02-09 16:01:25 +0100

    Update dosbox-x-app to 0.82.15,20190202110559 (#58666)

--- Casks/dosbox-x-app.rb
+++ Casks/dosbox-x-app.rb
@@ -1,6 +1,6 @@
 cask 'dosbox-x-app' do
-  version '0.82.14,20190101022247'
-  sha256 '13767ab3ae0599a4f166dc7c7a4c076f832f760e9a691eb0c3e5523fdcfdb461'
+  version '0.82.15,20190202110559'
+  sha256 '4e1c55c4e6662eae54f7a6cf76209acdab68de9565e7f8d6c29c0bd36b3e6630'
 
   # github.com/joncampbell123/dosbox-x was verified as official when first introduced to the cask
   url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x64-#{version.after_comma}.zip"
