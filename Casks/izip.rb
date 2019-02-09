72a504b2bd06da3a1e05ba12e093b1c18ac122c1
Vítor Galvão <info@vitorgalvao.com>
2019-02-09 20:44:14 +0000

    Update izip.rb

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -2,7 +2,7 @@ cask 'izip' do
   version '3.4'
   sha256 '6a17105e3d747135e91f69cdb7cfe0aa837e13683c30366022825b9e976151ef'
 
-  url "http://www.izip.com/izip_update_#{version.no_dots}.zip"
+  url "https://www.izip.com/izip_update_#{version.no_dots}.zip"
   appcast 'https://www.izip.com/updates'
   name 'iZip'
   homepage 'https://www.izip.com/'
