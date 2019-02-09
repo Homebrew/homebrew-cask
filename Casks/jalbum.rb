5f38403c679b39743ee43975192ade465491e17c
suschizu <brigitte@corecode.io>
2019-02-09 16:00:44 +0100

    Update jalbum to 18.0.1 (#58660)

--- Casks/jalbum.rb
+++ Casks/jalbum.rb
@@ -1,8 +1,8 @@
 cask 'jalbum' do
-  version '18'
-  sha256 'a723b974fd6d838a5dad1aae74490cf4af951c74472201f9dfe5961e403e26ed'
+  version '18.0.1'
+  sha256 '957d21a19c180f2d10e4b1845c59a4e90d9027894c9ffbc418dfb3867a531126'
 
-  url "https://download.jalbum.net/download/#{version}/MacOSX/jAlbum.dmg"
+  url "https://download.jalbum.net/download/#{version.major}/MacOSX/jAlbum.dmg"
   appcast 'https://jalbum.net/en/software/download/previous'
   name 'jAlbum'
   homepage 'https://jalbum.net/'
