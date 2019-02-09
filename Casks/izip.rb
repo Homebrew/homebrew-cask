ee3284350e070e1b84de3ba43431539114f8afa0
suschizu <brigitte@corecode.io>
2019-02-09 22:05:38 +0100

    Update izip to 3.4 (#58671)
    * Update izip to 3.4
    
    * Update izip.rb

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -1,8 +1,8 @@
 cask 'izip' do
-  version '3.3'
-  sha256 '3b0d8a8540fe0cfd98fe0d772fb30d9b8ececf0b16cea3fc21fbb266b93a7e6b'
+  version '3.4'
+  sha256 '6a17105e3d747135e91f69cdb7cfe0aa837e13683c30366022825b9e976151ef'
 
-  url "http://www.izip.com/izip_update_#{version.no_dots}.zip"
+  url "https://www.izip.com/izip_update_#{version.no_dots}.zip"
   appcast 'https://www.izip.com/updates'
   name 'iZip'
   homepage 'https://www.izip.com/'
