9b85c174642fb0839e24962e6bb996dec7108d13
Marcel Dold <marcel.dold@icloud.com>
2019-02-09 08:45:18 +0100

    Update blitz to 0.9.17

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,11 +1,13 @@
 cask 'blitz' do
-  version '0.9.16'
-  sha256 'fbc5a899ca22bbadd2751997d31bf4aa345d1b6e0e9d604dcfdd1ad16621c2ec'
+  version '0.9.17'
+  sha256 '6655bcf3626fe835e0ac30a0f76f34e9b77a39a18ac7a3f0c4eab7a05d3382cf'
 
   url 'https://dl.blitz.gg/download/mac'
   appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.blitz.gg/download/mac'
   name 'Blitz'
   homepage 'https://blitz.gg/'
+
+  auto_updates true
 
   app 'Blitz.app'
 
