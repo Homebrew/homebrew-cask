3735aae9c017e19c98999ba062ef8e774cfc1895
Jhen-Jie Hong <iainst0409@gmail.com>
2019-02-09 23:00:29 +0800

    Update react-native-debugger (0.9.4) (#58658)

--- Casks/react-native-debugger.rb
+++ Casks/react-native-debugger.rb
@@ -1,6 +1,6 @@
 cask 'react-native-debugger' do
-  version '0.9.3'
-  sha256 'b26b1d48810de0e43a146e5aca131881767ffc7a70ab35b1c9e39c0e02f50939'
+  version '0.9.4'
+  sha256 '20a1768de71b5dad0a08f434a65606bd7bf1a419a8293c31d024c646470fb5c4'
 
   url "https://github.com/jhen0409/react-native-debugger/releases/download/v#{version}/rn-debugger-macos-x64.zip"
   appcast 'https://github.com/jhen0409/react-native-debugger/releases.atom'
