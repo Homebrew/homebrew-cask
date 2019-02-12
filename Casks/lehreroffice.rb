0d9eba00f3c2321df3d887d9addd9f53713a60c3
suschizu <brigitte@corecode.io>
2019-02-12 21:26:59 +0100

    Update lehreroffice to 2019.2.3 (#58795)

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,6 +1,6 @@
 cask 'lehreroffice' do
-  version '2019.2.2'
-  sha256 'e4ed78578c8d0d8f9fdb297deeeadb4f22a5dab9db76af7b3489601e617b1f0f'
+  version '2019.2.3'
+  sha256 'f570317bf7b36648e59f67da1e17b059cd010751f48c2b728569375070267670'
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_desktop_macos.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop'
