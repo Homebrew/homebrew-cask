64bddc256664239daa15a35767c745f36363a685
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-12 12:15:00 -0800

    Update fsnotes to 2.9.0 (#58775)

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask 'fsnotes' do
-  version '2.8.2'
-  sha256 'c685e5ba01f04dbc47debae580e1f36918f197647128618425dbe7a994a4f89a'
+  version '2.9.0'
+  sha256 '2ed55098c0f265a8b18221225d8e30f3feab80bff1fa183208b3e40dbab902c7'
 
   # github.com/glushchenko/fsnotes was verified as official when first introduced to the cask
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
