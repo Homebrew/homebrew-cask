5f6eb57bd68d28ec7b4f4e845992a25a958743f9
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-11 23:57:45 -0800

    Update sha256

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,6 +1,6 @@
 cask 'yate' do
   version '4.6'
-  sha256 '42415321c358aa3eb8de2030f779b821b8cf830c1ec04f6bc46a03eac0808d2c'
+  sha256 'f76bd58c9f85e763b5f45885a206ae022843676f53c1b41fb3258cf9550e058b'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml'
