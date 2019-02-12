69714538eb515f6ed1569956ec2baf382f2830b2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-12 12:11:39 -0800

    Update poi to 10.2.2 (#58760)

--- Casks/poi.rb
+++ Casks/poi.rb
@@ -1,6 +1,6 @@
 cask 'poi' do
-  version '10.2.0'
-  sha256 '619d116d2714cdd973a8adb4c9e8185c77880ac23b4f3e75522f8f567efbf183'
+  version '10.2.2'
+  sha256 'f6ba6d5da7f0ea072ba9f456d917f05ccdc815b16b945bdaa9056a029eecb9a5'
 
   # github.com/poooi/poi was verified as official when first introduced to the cask
   url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}.dmg"
