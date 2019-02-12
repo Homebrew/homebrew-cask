82f0c90f209142e07b2179c9f72a6d9acfa9ba12
Randall <ran.dall@icloud.com>
2019-02-11 22:40:30 -0800

    Update izip to 3.5

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -1,6 +1,6 @@
 cask 'izip' do
-  version '3.4'
-  sha256 '6a17105e3d747135e91f69cdb7cfe0aa837e13683c30366022825b9e976151ef'
+  version '3.5'
+  sha256 '2501c6b9d237864aa5942afd5a67c5f89fbc13dee48b2ccb8cfb164715943327'
 
   url "https://www.izip.com/izip_update_#{version.no_dots}.zip"
   appcast 'https://www.izip.com/updates'
