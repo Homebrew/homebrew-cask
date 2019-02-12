607b82dccc2a55298688219b124f9c802674abdb
Marcel Dold <marcel.dold@icloud.com>
2019-02-12 01:21:20 +0100

    Update download url

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -2,7 +2,7 @@ cask 'blitz' do
   version '0.9.19'
   sha256 '5eb59aae3429b16f7921848ea51eda5461f553a98892d8b9401c44900e38a9ae'
 
-  url 'https://dl.blitz.gg/download/mac'
+  url 'http://dl.blitz.gg/download/version/{version}/osx_64'
   appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.blitz.gg/download/mac'
   name 'Blitz'
   homepage 'https://blitz.gg/'
