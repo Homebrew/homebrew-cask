1c855a14fb442d7aa2e124fd9c25b1136f076014
tiiiecherle <tiiiecherle@gmail.com>
2019-02-12 13:49:07 +0100

    Update adobe-acrobat-reader to 19.010.20091

--- Casks/adobe-acrobat-reader.rb
+++ Casks/adobe-acrobat-reader.rb
@@ -1,6 +1,6 @@
 cask 'adobe-acrobat-reader' do
-  version '19.010.20069'
-  sha256 '6ee27ec49e28f57b422288137d7b182d71925bd0096a7688b605bcf811123121'
+  version '19.010.20091'
+  sha256 'a1dcd94995e5aa4fee02c51beb038b4f5da6260183b143f691150997ebda587e'
 
   url "http://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots}/AcroRdrDC_#{version.no_dots}_MUI.dmg"
   appcast 'https://get.adobe.com/reader/'
