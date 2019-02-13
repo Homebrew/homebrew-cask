1483f2c2cf7f05c3710bb7fcb25d83181f5bfb81
Julian Mayer <julian@corecode.io>
2019-02-13 17:11:55 +0100

    Update powerphotos to 1.5.7

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -7,9 +7,13 @@ cask 'powerphotos' do
     version '1.2.3'
     sha256 'b07eb9f8801fb397d55e3dd7e0569dbef5d3265debaf3ee68247062901d93fcb'
     url "https://www.fatcatsoftware.com/powerphotos/PowerPhotos_#{version.no_dots}.zip"
+  elsif MacOS.version <= :sierra
+    version '1.4.2'
+    sha256 'ed9be64f4cb5a3d3848ad5177947bd8cd33e36846ea36266ef9d4d7b46813538'
+    url "https://www.fatcatsoftware.com/powerphotos/PowerPhotos_#{version.no_dots}.zip"
   else
-    version '1.5.6'
-    sha256 '5c2930829c765659ef61e9b99eec0bfd671693fadd6de7422f95cc9047870f15'
+    version '1.5.7'
+    sha256 '003dd46ec3b5d1d6926ca20604d29d6a165d1b526e558f2d6ab3f20f9cc4c12d'
     url 'https://www.fatcatsoftware.com/powerphotos/PowerPhotos.zip'
   end
 
