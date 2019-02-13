23782cfa6c41fb5d7ac8fca56dbe0f474e12ba09
Damian Maring <1820250+DamianLion@users.noreply.github.com>
2019-02-13 09:26:42 +0100

    Update balsamiq to latest version

--- Casks/balsamiq-mockups.rb
+++ Casks/balsamiq-mockups.rb
@@ -1,5 +1,5 @@
 cask 'balsamiq-mockups' do
-  version '3.5.16'
+  version '3.5.17'
   sha256 '5a80ea9169ffcc10b5dfa601b53fa7cc18795451ada4e1c11755fca06428ec05'
 
   url "https://builds.balsamiq.com/mockups-desktop/Balsamiq_Mockups_#{version}.dmg"
