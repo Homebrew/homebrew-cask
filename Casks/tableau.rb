3547c1903ba46edc42c8cd94a3f26e564e8c484c
CoreCode <core-code@users.noreply.github.com>
2019-02-13 18:22:17 +0100

    Update tableau.rb

--- Casks/tableau.rb
+++ Casks/tableau.rb
@@ -3,6 +3,7 @@ cask 'tableau' do
   sha256 '03c928c5495bb935be4e01c3bfa62609051ca77b3db5000edc8811b3b46c569f'
 
   url "https://downloads.tableau.com/tssoftware/TableauDesktop-#{version.dots_to_hyphens}.dmg"
+  appcast 'http://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/desktop/mac'
   name 'Tableau Desktop'
   homepage 'https://www.tableau.com/products/desktop/download'
 
