e545a139152f6156f578f9ca59bf6818e3998252
brigitte zmoelnig <brigitte@corecode.io>
2019-02-13 20:33:10 +0100

    Update wavebox to 4.7.0

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,6 +1,6 @@
 cask 'wavebox' do
-  version '4.6.0'
-  sha256 '6d2d90875f7bf7f36bbb898111a4a8c5f74ed4cba402ea5a2c3afce9a0e9fc80'
+  version '4.7.0'
+  sha256 '53414cf4704aa4208ed8cc674d58e57fc203f5f1567251567525e4d3e8630630'
 
   # github.com/wavebox/waveboxapp was verified as official when first introduced to the cask
   url "https://github.com/wavebox/waveboxapp/releases/download/v#{version}/Wavebox_#{version.dots_to_underscores}_osx.dmg"
