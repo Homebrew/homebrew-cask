79f7256569f9025f99faa8e6e0fb0ac6adb44177
Bruce Wang <number5@users.noreply.github.com>
2019-02-13 11:30:33 +1100

    Upgrade trilium-notes to 0.29.1

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask 'trilium-notes' do
-  version '0.28.3'
-  sha256 '1aba9af4bdd8a05c45cf67e83038ff0f8cc1a162bae1a0f66de04a4d2c5fc3a2'
+  version '0.29.1'
+  sha256 'cb6920bdf2357fc781a9c0c97dd91a9a4065e9a927b2b3c7830620d84bce1fbf'
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   appcast 'https://github.com/zadam/trilium/releases.atom'
