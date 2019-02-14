09acc544108f300cfa5bc5a005bfe7241ec0b134
FAN VINGA <fanalcest@gmail.com>
2019-02-14 15:54:15 +0800

    Add Kext Updater.app v2.7.4

---
+++ Casks/kext-updater.rb
@@ -0,0 +1,17 @@
+cask 'kext-updater' do
+  version '2.7.4'
+  sha256 '601254143831c86d3fbffce7a111af27ed7d0d058ce41e8f91d05771ec296cac'
+
+  url "https://bitbucket.org/profdrluigi/kextupdater/downloads/Kext%20Updater%20#{version}.zip"
+  name 'Kext Updater'
+  homepage 'https://bitbucket.org/profdrluigi/kextupdater'
+
+  app 'Kext Updater.app'
+
+  zap trash: [
+               '~/Library/Preferences/com.slsoft.kextupdater.plist',
+               '~/Library/Caches/com.slsoft.kextupdater',
+               '~/Library/Saved Application State/com.slsoft.kextupdater.savedState',
+               '/Library/Application Support/updater.bin',
+             ]
+end
