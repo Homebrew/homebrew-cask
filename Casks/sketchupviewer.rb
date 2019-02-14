0767c46f9263e0c285017606070ca0ee7439f746
suschizu <brigitte@corecode.io>
2019-02-15 00:31:04 +0100

    Update sketchupviewer to latest (#58948)

--- Casks/sketchupviewer.rb
+++ Casks/sketchupviewer.rb
@@ -2,9 +2,7 @@ cask 'sketchupviewer' do
   version :latest
   sha256 :no_check
 
-  # downloads can be found at https://www.sketchup.com/download/all
-  # dl.trimble.com/sketchup was verified as official when first introduced to the cask
-  url 'https://dl.trimble.com/sketchup/SketchUpViewer-en.dmg'
+  url 'https://www.sketchup.com/sketchup/SketchUpViewer-en-dmg'
   name 'SketchUpViewer'
   homepage 'https://www.sketchup.com/products/sketchup-viewer/'
 
