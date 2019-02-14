c5bd5b7fe2e80ed84e6df102e6607f3cdcced8ce
Vítor Galvão <info@vitorgalvao.com>
2019-02-14 23:46:55 +0000

    Update sharpshooter.rb

--- Casks/sharpshooter.rb
+++ Casks/sharpshooter.rb
@@ -3,7 +3,7 @@ cask 'sharpshooter' do
   sha256 '7593bf59c9e81ba5b1aa6f16c292237e2b5b8353a1d271ca47c25c28f99c4570'
 
   url "http://www.kerlmax.com/products/sharpshooter/Sharpshooter_#{version}.zip"
-  appcast 'http://www.kerlmax.com/products/sharpshooter/sharpshooter_v2_appcast.php'
+  appcast "http://www.kerlmax.com/products/sharpshooter/sharpshooter_v#{version.major}_appcast.php"
   name 'Sharpshooter'
   homepage 'http://www.kerlmax.com/products/sharpshooter/'
 
