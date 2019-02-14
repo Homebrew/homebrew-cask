bd15f7bdde3b5f81fd2f0719683be7b554d950c5
suschizu <brigitte@corecode.io>
2019-02-15 00:43:53 +0100

    Update cryptomator to 1.4.3 (#58844)

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask 'cryptomator' do
-  version '1.4.1'
-  sha256 'f28cbf35a1dc50a10a40cf190cdadb727db8d4981ce17b80866aeb7d664198b5'
+  version '1.4.3'
+  sha256 '72c2ae3a63ce74f400f0eb97c2f19709e8feb62bef419d497b2ed9a65b03b16a'
 
   # bintray.com/artifact/download/cryptomator was verified as official when first introduced to the cask
   url "https://bintray.com/artifact/download/cryptomator/cryptomator/Cryptomator-#{version}.dmg"
