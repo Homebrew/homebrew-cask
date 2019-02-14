3b027f47917d63673624731c9b00f2a116258869
Vítor Galvão <info@vitorgalvao.com>
2019-02-14 22:10:16 +0000

    Update twig.rb

--- Casks/twig.rb
+++ Casks/twig.rb
@@ -3,8 +3,7 @@ cask 'twig' do
   sha256 '1fa2335dae41afe430febaa01f3b796c89b84a0d2316d3d54736060a4435a99e'
 
   url "https://github.com/lukakerr/twig/releases/download/#{version}/Twig-#{version}.zip"
-  appcast 'https://github.com/lukakerr/twig/releases.atom',
-          checkpoint: '2abf673cf4461730e000dda9eda31162a7497d27f55048bf2a6e78de130c749f'
+  appcast 'https://github.com/lukakerr/twig/releases.atom'
   name 'Twig'
   homepage 'https://github.com/lukakerr/twig'
 
