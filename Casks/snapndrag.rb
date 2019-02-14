c5885774d78e0fb07bfca79d01ef6258324c5b7b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-14 09:20:44 -0800

    update https

--- Casks/snapndrag.rb
+++ Casks/snapndrag.rb
@@ -2,10 +2,10 @@ cask 'snapndrag' do
   version '4.3.1'
   sha256 'e643cd47fbc3ffbe2ef98f22987ad0e8588d090e017717164a7e14fec9a838da'
 
-  url "http://yellowmug.com/download/SnapNDrag_#{version}.dmg"
-  appcast 'http://yellowmug.com/snapndrag/appcast-1012.xml'
+  url "https://yellowmug.com/download/SnapNDrag_#{version}.dmg"
+  appcast 'https://yellowmug.com/snapndrag/appcast-1012.xml'
   name 'SnapNDrag'
-  homepage 'http://www.yellowmug.com/snapndrag/'
+  homepage 'https://www.yellowmug.com/snapndrag/'
 
   app 'SnapNDrag.app'
 end
