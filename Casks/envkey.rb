679eb9f1303ea350f808a0a4a7f5bd807110c03c
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-14 15:32:38 -0800

    Update envkey to 1.4.3 (#58931)

--- Casks/envkey.rb
+++ Casks/envkey.rb
@@ -1,6 +1,6 @@
 cask 'envkey' do
-  version '1.4.2'
-  sha256 '8b53dd1f3a554954deb038855a7281ba56ea49750b181e5242f0ef21bf061ca4'
+  version '1.4.3'
+  sha256 'a14a4759be7cf5009b402339585a3890ae3ebf6381d8c99d43b96cf551972766'
 
   # github.com/envkey/envkey-app was verified as official when first introduced to the cask
   url "https://github.com/envkey/envkey-app/releases/download/darwin-x64-prod-v#{version}/EnvKey-#{version}-mac.zip"
