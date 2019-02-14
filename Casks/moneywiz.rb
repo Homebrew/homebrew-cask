a4d8b7a550d6f53dc1b980caf03ecce76ec1c05e
Ronald Suwandi <ronald@ronaldsuwandi.com>
2019-02-14 14:51:49 +0800

    Update moneywiz to 3.3.4 (#58887)

--- Casks/moneywiz.rb
+++ Casks/moneywiz.rb
@@ -1,6 +1,6 @@
 cask 'moneywiz' do
-  version '3.3.3'
-  sha256 '528963181f40e852fb564e1a01c271a6fdd2b17d250743807d706b7608561391'
+  version '3.3.4'
+  sha256 '988e9da6e0a51975803c63c28669724326016b2879ccdcaeacaa5a36250e1009'
 
   url 'https://mac.wiz.money/MoneyWiz_Mac_Free.dmg'
   appcast 'https://macdistribution.wiz.money/version_info_free.xml'
