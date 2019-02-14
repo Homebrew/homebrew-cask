dc0cd131c91954cce4ad3f12ce4ebb3f7e5ff8a1
Anver Housseini <contact@anver.ch>
2019-02-15 00:45:15 +0100

    Update pascom-client to 47.R161_ca0d78b (#58855)

--- Casks/pascom-client.rb
+++ Casks/pascom-client.rb
@@ -1,6 +1,6 @@
 cask 'pascom-client' do
-  version '46.R152_f23040b'
-  sha256 'd54b3417289981008c0946a0f3125c3bb0ce3cac057a41116d13d78e1af1b76a'
+  version '47.R161_ca0d78b'
+  sha256 'd33672bb5fefa963604cda66c8d4cdecd5c9adbcb3511a069e7212fd6fe2cc5d'
 
   url "https://download.pascom.net/release-archive/client/stable/pascom%20Client-#{version}.dmg"
   name 'pascom Client'
