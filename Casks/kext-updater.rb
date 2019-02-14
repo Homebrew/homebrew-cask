6e3fc3c6ac381faab2feae056a59962cce4342b8
Vítor Galvão <info@vitorgalvao.com>
2019-02-14 23:54:39 +0000

    Update kext-updater.rb

--- Casks/kext-updater.rb
+++ Casks/kext-updater.rb
@@ -3,6 +3,7 @@ cask 'kext-updater' do
   sha256 '601254143831c86d3fbffce7a111af27ed7d0d058ce41e8f91d05771ec296cac'
 
   url "https://bitbucket.org/profdrluigi/kextupdater/downloads/Kext%20Updater%20#{version}.zip"
+  appcast 'https://update.kextupdater.de/kextupdater/appcast.xml'
   name 'Kext Updater'
   homepage 'https://bitbucket.org/profdrluigi/kextupdater'
 
