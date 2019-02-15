813956a581de33d0aafae04c83af02502ddbb4d3
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-15 15:23:59 -0800

    Update wickrme to 5.4.3 (#59015)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/wickrme.rb
+++ Casks/wickrme.rb
@@ -1,6 +1,6 @@
 cask 'wickrme' do
-  version '5.2.4'
-  sha256 '79ba4b6b1cd093647c152884725c3c513ebc314fd8e56e3756d18e6f3d44980b'
+  version '5.4.3'
+  sha256 '69cec0834955f6bf211d2c08e097aee26977b46871383a513ba987d9b90ac234'
 
   # s3.amazonaws.com/static.wickr.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/me/WickrMe-#{version}.dmg"
