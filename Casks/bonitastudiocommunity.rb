768e8f1227731583d60b3e9787b120ef1263ecd2
suschizu <brigitte@corecode.io>
2019-02-15 10:06:51 +0100

    Update bonitastudiocommunity to 7.8.2 (#58943)

--- Casks/bonitastudiocommunity.rb
+++ Casks/bonitastudiocommunity.rb
@@ -1,6 +1,6 @@
 cask 'bonitastudiocommunity' do
-  version '7.8.1'
-  sha256 '9d024f1107d17d6589b8746ac67296506c14dae9da0a608e86a8e1844febec93'
+  version '7.8.2'
+  sha256 'dbc47e17c8ff4e9ae315420c97b7e27aedca2a4786adfd830ee07049d949dcf6'
 
   # release.ow2.org was verified as official when first introduced to the cask
   url "https://release.ow2.org/bonita/BonitaStudioCommunity-#{version}-x86_64.dmg"
