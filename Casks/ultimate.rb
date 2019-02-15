98e63ad80336901e0af74389bcd1f88e706e5417
CoreCode <core-code@users.noreply.github.com>
2019-02-15 12:32:05 +0100

    Update ultimate to 3.0.11.212 (#58960)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/ultimate.rb
+++ Casks/ultimate.rb
@@ -1,6 +1,6 @@
 cask 'ultimate' do
-  version '3.0.11.104'
-  sha256 '724f6c39120b0c7a663d52b750ec973e43924b19ea5498fb1201ef84c43b145f'
+  version '3.0.11.212'
+  sha256 '3ebb03e09a9656856a7d0382eac487d98a3d5aa52e66aa77b07925a074c2d949'
 
   url 'https://download.epubor.com/epubor_ultimate.zip'
   appcast 'https://www.epubor.com/ultimate.html'
