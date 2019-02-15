109bb833fe2ee824627eb47e3c183cf2ea8a73c4
yikai <yikai@UNKNOWN-172-153-80-X.yahoo.com>
2019-02-15 18:28:46 +0800

    Init Yahoo! KeyKey Chinese input method
    Extract KeyKey.pkg from YahooKeyKey-OSX-Leopard-1.1.2535.dmg and add Cask yahoo-
    keykey.

---
+++ Casks/yahoo-keykey.rb
@@ -0,0 +1,13 @@
+cask 'yahoo-keykey' do
+  version '1.1.2535'
+  sha256 'affa3836cc1737e8a42bdb64d70e141979a1284055c30e27f253b38f54109404'
+
+  url "https://github.com/Yi-Kai/YahooKeyKey/releases/download/v#{version}/YahooKeyKey.zip"
+  appcast 'https://github.com/Yi-Kai/YahooKeyKey/releases.atom'
+  name 'Yahoo! KeyKey Chinese input method'
+  homepage 'https://github.com/Yi-Kai/YahooKeyKey'
+
+  pkg 'KeyKey.pkg'
+
+  uninstall pkgutil: 'com.yahoo.inputmethod.keykey'
+end
