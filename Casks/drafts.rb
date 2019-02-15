cf9e07729584989b4b086875166a95f81749b9cc
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-14 18:20:28 -0800

    Update drafts to 5.7.2 (#58902)

--- Casks/drafts.rb
+++ Casks/drafts.rb
@@ -1,6 +1,6 @@
 cask 'drafts' do
-  version '5.6.3.4'
-  sha256 'd06d95e30dcc2308a9f62b5043d1de0dd8506ef75c9e290ec4d31b37d036b2d4'
+  version '5.7.2'
+  sha256 'fd8ee1c6953445901d62583eca102000f805f8a51c584656975563383781acbe'
 
   # s3-us-west-2.amazonaws.com/downloads.agiletortoise.com was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/Drafts.app.zip'
