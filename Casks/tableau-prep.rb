3c397438997df487624a611da3cb5a7cdf0e4401
CoreCode <core-code@users.noreply.github.com>
2019-02-15 12:50:27 +0100

    Update tableau-prep.rb

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -1,6 +1,6 @@
 cask 'tableau-prep' do
   version '2019.1.2'
-  sha256 '964039fc8016b7a7ce33e12bf2ec5c7082dc44f944a5ff9053ea33bc91a0deb6'
+  sha256 'a28317be77a6ef781a5734e0cb9c9af9205bda7e8148c1dbf5c62c6f0d8f9aea'
 
   url "https://downloads.tableau.com/esdalt/tableau_prep/#{version}/TableauPrep-#{version.dots_to_hyphens}.dmg"
   appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/prep/mac'
