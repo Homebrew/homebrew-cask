4bc7ca777e10bd491ad5637e0cac5d670eb2473e
CoreCode <core-code@users.noreply.github.com>
2019-02-15 14:30:07 +0100

    Update tableau-prep to 2019.1.2 (#58839)
    * Update tableau-prep to 2019.1.2
    
    * Update tableau-prep.rb
    
    * Update tableau-prep.rb
    
    * Update tableau-prep.rb
    
    * Update tableau-prep.rb
    
    * Update tableau-prep.rb

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -1,14 +1,15 @@
 cask 'tableau-prep' do
-  version '2019.1.1'
-  sha256 '392db25721e22d93e8489ab07b6a6107b8c66db5eee0cf97e844601717c50d47'
+  version '2019.1.2'
+  sha256 'a28317be77a6ef781a5734e0cb9c9af9205bda7e8148c1dbf5c62c6f0d8f9aea'
 
   url "https://downloads.tableau.com/esdalt/tableau_prep/#{version}/TableauPrep-#{version.dots_to_hyphens}.dmg"
+  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/prep/mac'
   name 'Tableau Prep'
   homepage 'https://www.tableau.com/support/releases/prep'
 
   depends_on macos: '>= :el_capitan'
 
-  pkg 'Tableau Prep.pkg'
+  pkg 'Tableau Prep Builder.pkg'
 
   uninstall pkgutil: [
                        'com.amazon.redshiftodbc',
