688cbd1e9d24337fdcd7a20ec136257472536798
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-15 15:24:41 -0800

    Update electron to 4.0.5 (#59007)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '4.0.4'
-  sha256 '95c0840053037a2ed2f18c0cdc32564f75c3bc262cd246b9a63e547e5781cb06'
+  version '4.0.5'
+  sha256 'ced8c7742b0a9415974e7774bb4222a7757613b82ab5676df315d0fd59f28e8c'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
