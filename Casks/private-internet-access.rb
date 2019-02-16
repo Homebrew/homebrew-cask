8f96bdc19f6a74907c6a9276e11843c414d4c2b5
Julian Mayer <julian@corecode.io>
2019-02-16 22:37:20 +0100

    Update private-internet-access to 1.0.2-02363

--- Casks/private-internet-access.rb
+++ Casks/private-internet-access.rb
@@ -1,8 +1,9 @@
 cask 'private-internet-access' do
-  version '1.0.1-02349'
-  sha256 '59c3a8ad7661cb86c2b9b3f0b9d2994031493cc30fbe454d970d1c8edfb74926'
+  version '1.0.2-02363'
+  sha256 '3e9315166502925b9531de6c0a32ffd67491cd7745ab9eb79ef98b5ab917a296'
 
   url "https://installers.privateinternetaccess.com/download/pia-macos-#{version}.zip"
+  appcast 'https://www.privateinternetaccess.com/pages/download'
   name 'Private Internet Access'
   homepage 'https://www.privateinternetaccess.com/'
 
