db81a20b46799d6a8089a3fdb4845d039b960fc9
Tim Brust <github@timbrust.de>
2019-02-16 17:39:29 +0100

    Update cryptomator to 1.4.5

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask 'cryptomator' do
-  version '1.4.4'
-  sha256 '6f3956a6c6c24461dd58e1823e1e9d9555930e73230f395be5d7e414f7553fc5'
+  version '1.4.5'
+  sha256 'a961bbb06af06f903eaf76a37f28c94ad7234ed6bcbdaba9e0309d63f5ffa46f'
 
   # bintray.com/artifact/download/cryptomator was verified as official when first introduced to the cask
   url "https://bintray.com/artifact/download/cryptomator/cryptomator/Cryptomator-#{version}.dmg"
