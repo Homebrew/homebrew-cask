a2cf804a1b46ff9d5728e08a9eb5c678585aacb1
Thomas Strömberg <tstromberg@google.com>
2019-02-15 18:55:10 -0800

    Update minikube to v0.34.0, change name to lowercase as is now used (#59017)

--- Casks/minikube.rb
+++ Casks/minikube.rb
@@ -1,11 +1,11 @@
 cask 'minikube' do
-  version '0.33.1'
-  sha256 'ff5aee67920e9615aef804ed0db9d0dfaebfb9f54ca093ddc16c67b3834baaf0'
+  version '0.34.0'
+  sha256 '8932ee06a8db3fae696d97bfefa2d7ad2168ffa3046e0729f39164cf11a78911'
 
   # storage.googleapis.com/minikube was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/minikube/releases/v#{version}/minikube-darwin-amd64"
   appcast 'https://github.com/kubernetes/minikube/releases.atom'
-  name 'Minikube'
+  name 'minikube'
   homepage 'https://github.com/kubernetes/minikube'
 
   depends_on formula: 'kubernetes-cli'
