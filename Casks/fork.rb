b62f6adfdad409294505a8667153c720375bc637
404NetworkError <404NetworkError@users.noreply.github.com>
2019-02-16 12:10:14 -0500

    Fix missing comment when URL and homepage hostnames differ

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -2,6 +2,7 @@ cask 'fork' do
   version '1.0.74'
   sha256 '04b7a06735d34492830d12696cb5145798dee11eb8840a5e7fdf90807e42c5b8'
 
+  # forkapp.ams3.cdn.digitaloceanspaces.com/mac was verified as official when first introduced to the cask
   url "https://forkapp.ams3.cdn.digitaloceanspaces.com/mac/Fork-#{version}.dmg"
   appcast 'https://git-fork.com/update/feed.xml'
   name 'Fork'
