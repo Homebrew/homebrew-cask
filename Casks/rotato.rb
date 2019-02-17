d50bdce0b2a19230edbbe69c8311e1578f2f4fa6
Vítor Galvão <info@vitorgalvao.com>
2019-02-17 18:59:29 +0000

    Update rotato.rb

--- Casks/rotato.rb
+++ Casks/rotato.rb
@@ -1,8 +1,10 @@
 cask 'rotato' do 
-  version :latest
-  sha256 :no_check
+  version '50.1550354063'
+  sha256 '7e44dfb4fd7202b529391771f7d5d5b5c07ae45be5e23ea1f465e621118cff06'
   
-  url 'http://designcamera.app/latest'
+  # dl.devmate.com/com.mortenjust.Rendermock was verified as official when first introduced to the cask
+  url "https://dl.devmate.com/com.mortenjust.Rendermock/#{version.major}/#{version.minor}/DesignCamera-#{version.major}.zip"
+  appcast 'https://updates.devmate.com/com.mortenjust.Rendermock.xml'
   name 'Rotato'
   homepage 'https://rotato.xyz'
   
