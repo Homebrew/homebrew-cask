60bef2e88b55d19afdd62d4c050a39d220156ec8
suschizu <brigitte@corecode.io>
2019-02-17 20:51:41 +0100

    Update sip.rb

--- Casks/sip.rb
+++ Casks/sip.rb
@@ -1,13 +1,19 @@
 cask 'sip' do
-  version '2.0'
-  sha256 '14ec052b4edb5aa995d09672030ba8663b283ce4c0d4241a77111a5a67465981'
+  if MacOS.version <= :sierra
+    version '1.1.6'
+    sha256 'bb170a54090aab5703388a3e7a22e9cf4e4d98e84f5658893e1e6f9677b9a51e'
 
+  else
+    version '2.0'
+    sha256 '14ec052b4edb5aa995d09672030ba8663b283ce4c0d4241a77111a5a67465981'
+
+  end
   url "https://sipapp.io/updates/v#{version.major}/sip-#{version}.zip"
   appcast "https://sipapp.io/updates/v#{version.major}/sip.xml"
   name 'Sip'
   homepage 'https://sipapp.io/'
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: '>= :sierra'
 
   app 'Sip.app'
 
