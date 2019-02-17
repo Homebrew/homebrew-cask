7c3210d62572dc1a0e06f61bb8ea814fa2dfaa27
CoreCode <core-code@users.noreply.github.com>
2019-02-17 15:11:11 +0100

    Update ringcentral to 10.3.2 (#59000)
    * Update ringcentral to 10.3.2
    
    * Update ringcentral.rb

--- Casks/ringcentral.rb
+++ Casks/ringcentral.rb
@@ -1,8 +1,9 @@
 cask 'ringcentral' do
-  version :latest
-  sha256 :no_check
+  version '10.3.2'
+  sha256 '2cb11a5f8be4c68f8969918a5e24f96d4895870d030e3bf80ae291967fdd646f'
 
-  url 'https://downloads.ringcentral.com/sp/RingCentralForMac'
+  url "https://downloads.ringcentral.com/sp/RingCentralPhone-#{version}.dmg"
+  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=http://downloads.ringcentral.com/sp/RingCentralForMac'
   name 'RingCentral for Mac'
   homepage 'https://www.ringcentral.com/office/features/desktop-apps/overview.html'
 
