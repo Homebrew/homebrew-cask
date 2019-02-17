4de84b3265551722cb0a4583aea2eef641fe1748
Thomas Strömberg <tstromberg@google.com>
2019-02-17 05:41:57 -0800

    Update minikube to v0.34.1 (#59037)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/minikube.rb
+++ Casks/minikube.rb
@@ -1,6 +1,6 @@
 cask 'minikube' do
-  version '0.34.0'
-  sha256 '8932ee06a8db3fae696d97bfefa2d7ad2168ffa3046e0729f39164cf11a78911'
+  version '0.34.1'
+  sha256 '54b865528ff14a4cfa479bd7c5630db81e0695c3b4b85939b0735dc1203541d3'
 
   # storage.googleapis.com/minikube was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/minikube/releases/v#{version}/minikube-darwin-amd64"
