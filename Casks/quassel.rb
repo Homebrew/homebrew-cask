653db9be4bbd2e3855ec36619ba67b5c586560a7
suschizu <brigitte@corecode.io>
2019-02-17 20:56:46 +0100

    Update quassel.rb

--- Casks/quassel.rb
+++ Casks/quassel.rb
@@ -3,7 +3,8 @@ cask 'quassel' do
   sha256 'bda19cfd004a0b377f1c6a3974fd63f5665d5dc570eac2a24bf7c48d387f24fb'
 
   # github.com/quassel/quassel/ was verified as official when first introduced to the cask
-  url "https://github.com/quassel/quassel/releases/download/#{version}/QuasselCore_MacOSX-x86_64_#{version}.dmg"
+  url "https://github.com/quassel/quassel/releases/download/#{version}/QuasselMono_MacOSX-x86_64_#{version}.dmg"
+
   appcast 'https://github.com/quassel/quassel/releases.atom'
   name 'Quassel IRC'
   homepage 'https://quassel-irc.org/'
