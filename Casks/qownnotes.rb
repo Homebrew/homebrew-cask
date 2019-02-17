4019150568877833a7259de8805a75714364dbd4
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-17 05:41:39 -0800

    Update qownnotes to 19.2.4,b4165-055216 (#59045)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '19.2.3,b4148-062533'
-  sha256 '2ca85221a5e96617b85fd7f9e6833fbd19c74da440f352bbca099d9398e67b3f'
+  version '19.2.4,b4165-055216'
+  sha256 '2d2466a970676539780fc7bcce665296e62e8410bfbb41dea5fc353f680fe60e'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
