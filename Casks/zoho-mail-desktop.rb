f056ca1c186a9ca1c6b790eb0ea1134308ae9df2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-17 05:40:33 -0800

    Update zoho-mail-desktop to 1.0.11 (#59060)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/zoho-mail-desktop.rb
+++ Casks/zoho-mail-desktop.rb
@@ -1,6 +1,6 @@
 cask 'zoho-mail-desktop' do
-  version '1.0.10'
-  sha256 '9e5a08cd9bbf07e3ca07a7ce43e18764b22a022cdad7c749e6ef9baf3139f32b'
+  version '1.0.11'
+  sha256 '02738c978d43974e49c7bd794a6ffc51ed51862cd04472016ba558f21bffa4de'
 
   url "https://www.zoho.com/mail/desktop/mac/zoho-mail-desktop-installer-v#{version}.dmg"
   name 'zoho-mail-desktop'
