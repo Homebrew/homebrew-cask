8db62f548932c8ddbe20d97222a2d1b7886c17b6
Julian Mayer <julian@corecode.io>
2019-02-17 23:02:18 +0100

    Update cyberghost to 7.0.1.74

--- Casks/cyberghost.rb
+++ Casks/cyberghost.rb
@@ -1,11 +1,11 @@
 cask 'cyberghost' do
-  version '5.0.15.5'
-  sha256 '197e9501953861f0265c415b58f6a8983d97530860885f3135f6f75aeb9f5985'
+  version '7.0.1.74'
+  sha256 'a30ad341b29dde169e15f1af00cc68df1b8393fdd7d6cfc70b11c145e3241324'
 
-  url "https://download.cyberghostvpn.com/mac/cg5mac_#{version}.dmg"
-  appcast 'https://download.cyberghostvpn.com/mac/updates/cyberghost_mac_update.inf'
+  url "https://download.cyberghostvpn.com/mac/updates/v#{version.major}/cg#{version.major}mac_#{version}.dmg"
+  appcast "https://download.cyberghostvpn.com/mac/updates/v#{version.major}/cyberghost_mac_update.inf"
   name 'CyberGhost'
   homepage 'https://www.cyberghostvpn.com/'
 
-  app "CyberGhost #{version.major}.app"
+  app 'CyberGhost VPN.app'
 end
