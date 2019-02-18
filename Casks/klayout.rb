8aad3a93ed4bb4f38d05948614eae3dfb0e45290
Work <brigitte@corecode.io>
2019-02-18 12:41:10 +0100

    Update klayout to 0.25.7

--- Casks/klayout.rb
+++ Casks/klayout.rb
@@ -1,11 +1,27 @@
 cask 'klayout' do
-  version '0.24.10'
-  sha256 'a2097dd73f8bbdeb4297f1fdaba01635894941ae1b3c376e625948171061d6d6'
+  version '0.25.7'
 
-  # klayout.org was verified as official when first introduced to the cask
-  url "https://www.klayout.org/downloads/MacOS/klayout-#{version}-MacOSX-Yosemite-1-Qt487mp.dmg.bz2"
+  if MacOS.version <= :el_capitan
+    sha256 :no_check
+
+    # klayout.org was verified as official when first introduced to the cask
+    url "https://www.klayout.org/downloads/MacOS/klayout-#{version}-MacOSX-ElCapitan-1-Qt5113mp.dmg"
+  elsif MacOS.version <= :high_sierra
+    sha256 'd05d090956700e9f5349c1f7ac1773a6a1df893263ddd81097135610e49dd49e'
+
+    # klayout.org was verified as official when first introduced to the cask
+    url "https://www.klayout.org/downloads/MacOS/klayout-#{version}-macOS-HighSierra-1-Qt5120mp.dmg"
+  else
+    sha256 'd05d090956700e9f5349c1f7ac1773a6a1df893263ddd81097135610e49dd49e'
+
+    # klayout.org was verified as official when first introduced to the cask
+    url "https://www.klayout.org/downloads/MacOS/klayout-#{version}-macOS-Mojave-1-Qt5120mp.dmg"
+  end
+
   name 'KLayout'
   homepage 'https://www.klayout.de/'
+
+  depends_on macos: '>= :el_capitan'
 
   suite 'KLayout'
 
