d461a4af97a1151b6f4e09be65ea0772f666a779
suschizu <brigitte@corecode.io>
2019-02-18 17:09:48 +0100

    Update klayout to 0.25.7 (#59100)
    * Update klayout to 0.25.7
    
    * Update klayout.rb
    
    * Update klayout.rb

--- Casks/klayout.rb
+++ Casks/klayout.rb
@@ -1,11 +1,13 @@
 cask 'klayout' do
-  version '0.24.10'
-  sha256 'a2097dd73f8bbdeb4297f1fdaba01635894941ae1b3c376e625948171061d6d6'
+  version '0.25.7'
+  sha256 'd05d090956700e9f5349c1f7ac1773a6a1df893263ddd81097135610e49dd49e'
 
   # klayout.org was verified as official when first introduced to the cask
-  url "https://www.klayout.org/downloads/MacOS/klayout-#{version}-MacOSX-Yosemite-1-Qt487mp.dmg.bz2"
+  url "https://www.klayout.org/downloads/MacOS/klayout-#{version}-macOS-Mojave-1-Qt5120mp.dmg"
   name 'KLayout'
   homepage 'https://www.klayout.de/'
+
+  depends_on macos: '>= :mojave'
 
   suite 'KLayout'
 
