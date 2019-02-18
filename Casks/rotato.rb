d5686d4b0c6d60d73bd640639e820981ec2be64f
suschizu <brigitte@corecode.io>
2019-02-18 23:24:11 +0100

    Update rotato to 52.1550499356 (#59121)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/rotato.rb
+++ Casks/rotato.rb
@@ -1,6 +1,6 @@
 cask 'rotato' do
-  version '50.1550354063'
-  sha256 '7e44dfb4fd7202b529391771f7d5d5b5c07ae45be5e23ea1f465e621118cff06'
+  version '52.1550499356'
+  sha256 'cf6d850ab941ed396a6eb7d06b7a88c2b1263bc7626137ed7eab47ad6f74fca2'
 
   # dl.devmate.com/com.mortenjust.Rendermock was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.mortenjust.Rendermock/#{version.major}/#{version.minor}/DesignCamera-#{version.major}.zip"
