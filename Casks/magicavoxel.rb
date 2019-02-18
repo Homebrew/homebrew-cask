6e386029031c0fcb9dca6efc03d487709007ab38
Vítor Galvão <info@vitorgalvao.com>
2019-02-18 23:51:10 +0000

    Update magicavoxel.rb

--- Casks/magicavoxel.rb
+++ Casks/magicavoxel.rb
@@ -2,7 +2,7 @@ cask 'magicavoxel' do
   version '0.99.3'
   sha256 'd7c09a0178d4fd8b6484eae479b2b8f2daf60b8a0b39ba418981846906924e49'
 
-  # dropbox.com/s/abh4jkiopcbuzep/ was verified as official when first introduced to the cask
+  # dropbox.com/s/abh4jkiopcbuzep was verified as official when first introduced to the cask
   url "https://www.dropbox.com/s/abh4jkiopcbuzep/MagicaVoxel-#{version}-alpha-macos.zip?dl=1"
   name 'MagicaVoxel'
   homepage 'https://ephtracy.github.io/'
