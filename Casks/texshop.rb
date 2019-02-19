c6bb61c27c6e5b582f5ff13e4aa50681e6665401
ralph <ralph@cs.cf.ac.uk>
2019-02-19 21:43:29 +0000

    Update texshop to 4.25

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,6 +1,6 @@
 cask 'texshop' do
-  version '4.24'
-  sha256 '63af51b33c5e9e1160f914c89ca1dd2925b475880bb6f17158a2aa4bce8c6b5f'
+  version '4.25'
+  sha256 '506c25098c37d80c912c18eb088489490621c10e011cb73a167a9484e8c3341e'
 
   url "https://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.no_dots}.zip"
   appcast 'https://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml'
