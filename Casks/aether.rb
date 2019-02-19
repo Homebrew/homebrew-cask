2e1c54a383544b3e7cec4a1aa12422685755a6f2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-19 08:23:16 -0800

    Update aether to 2.0.0-dev.9,1902131456.97bf3870 (#59137)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/aether.rb
+++ Casks/aether.rb
@@ -1,6 +1,6 @@
 cask 'aether' do
-  version '2.0.0-dev.6,1811231746.7e1936d8'
-  sha256 'db8c1d499b04225ea10fd1869f996dd8cf83befe0bcc60b5ee8022af6a7fd77c'
+  version '2.0.0-dev.9,1902131456.97bf3870'
+  sha256 '3bb23fb85c25c7e61f540aa5bff709852f2278252369ec68f825294c19f8e8f2'
 
   url "https://static.getaether.net/Releases/Aether-#{version.before_comma}/#{version.after_comma}/mac/Aether-#{version.before_comma}%2B#{version.after_comma}.dmg"
   appcast 'https://getaether.net/dlstarted/'
