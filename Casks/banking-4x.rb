50e19613f5f04afc93ed44bf6fc95a039f005e3b
Julian Mayer <julian@corecode.io>
2019-02-19 19:23:27 +0100

    Update banking-4x to 7.0.1,6989

--- Casks/banking-4x.rb
+++ Casks/banking-4x.rb
@@ -1,14 +1,14 @@
 cask 'banking-4x' do
-  # note: "4x" is not a version number, but an intrinsic part of the product name
-  version '6.10.9,6900'
-  sha256 '7525f9d94ed6df1ea7d22d97d5feb7f346abfb56e8f3022ac1b80add46276f44'
+  # note: "4" is not a version number, but an intrinsic part of the product name
+  version '7.0.1,6989'
+  sha256 'c0f96d9c6d2846624e51a5b596972b6762ed2b7bd2c218a5adad1fdce4e35a71'
 
-  url 'https://subsembly.com/download/MacBanking.pkg'
-  appcast 'https://subsembly.com/banking4x-updates.php'
-  name 'Banking 4X'
+  url 'https://subsembly.com/download/MacBanking4.pkg'
+  appcast 'https://subsembly.com/banking4-macos-updates.php'
+  name 'Banking 4'
   homepage 'https://subsembly.com/banking4.html'
 
-  pkg 'MacBanking.pkg'
+  pkg 'MacBanking4.pkg'
 
-  uninstall pkgutil: 'com.Subsembly.Banking4X'
+  uninstall pkgutil: 'com.Subsembly.Banking4'
 end
