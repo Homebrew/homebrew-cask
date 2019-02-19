e8e83be8c8b8ee62dc2455909b6be3c465224097
Randall <ran.dall@icloud.com>
2019-02-19 01:22:48 -0800

    Update timely to 0.6.1

--- Casks/timely.rb
+++ Casks/timely.rb
@@ -1,6 +1,6 @@
 cask 'timely' do
-  version '0.6.0'
-  sha256 '07b960a77f008abaa2ac6fb73546be5de51e60913f0bbd38482ccf9d9b94ab06'
+  version '0.6.1'
+  sha256 'd6226090847503ca79b6e871854464bc105ff28243df08795876e4f3d85bf43b'
 
   # github.com/Timely was verified as official when first introduced to the cask
   url "https://github.com/Timely/desktop-releases/releases/download/darwin-x64-prod-v#{version}/Timely-#{version}.dmg"
