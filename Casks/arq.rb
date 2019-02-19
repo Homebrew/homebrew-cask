58695ed29722a7923c53466fc5b4305fdc8b0ae4
Jason Fiore <jason@MBP15-macOS.local>
2019-02-19 09:49:33 -0800

    Update arq to 5.16

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,6 +1,6 @@
 cask 'arq' do
-  version '5.15.3'
-  sha256 'b02cde3fca9b60c6ce5ab83aa641efe992e48cda997bd92e106aa19c894c45df'
+  version '5.16'
+  sha256 '9e54d4da3259e80ce8f82209622d47ea1fb38fab70e8db23bc53c63fb626263e'
 
   url "https://www.arqbackup.com/download/arqbackup/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arqbackup/arq#{version.major}.xml"
