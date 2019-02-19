03ef69422287b8b7d1216a95c7fce48d9e0aafea
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-02-20 01:30:34 +0900

    update goodsync10.9 (#59155)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
   version '10.9.24.4'
-  sha256 '26329a8e2cbfb41726c5654174f34b9362ab0c2ea0d69ccda4f1aae33ab4b78b'
+  sha256 '51d28b8761e754260404cdd039fbe4aad9db14278ef8dacb9137f8f47d177807'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/8b491acdaa8942108b5d8b019be7fcef'
