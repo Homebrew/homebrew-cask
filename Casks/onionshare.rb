7264e3c4fb9177b03c32c2185cc65a4f6909e903
Vítor Galvão <info@vitorgalvao.com>
2019-02-19 16:36:34 +0000

    Update onionshare.rb

--- Casks/onionshare.rb
+++ Casks/onionshare.rb
@@ -1,14 +1,14 @@
 cask 'onionshare' do
-  version '2.0,2'
+  version '2.0'
   sha256 '117f7373b3e23b6ad8a1f0105cbe81d467fedb9dcce89baf59ffd2c851903710'
 
   # github.com/micahflee/onionshare was verified as official when first introduced to the cask
-  url "https://github.com/micahflee/onionshare/releases/download/v#{version.before_comma}/OnionShare-#{version.after_comma}.pkg"
+  url "https://github.com/micahflee/onionshare/releases/download/v#{version}/OnionShare-#{version.major}.pkg"
   appcast 'https://github.com/micahflee/onionshare/releases.atom'
   name 'OnionShare'
   homepage 'https://onionshare.org/'
 
-  pkg "OnionShare-#{version.after_comma}.pkg"
+  pkg "OnionShare-#{version.major}.pkg"
 
   uninstall pkgutil: 'com.micahflee.onionshare'
 end
