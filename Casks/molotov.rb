35d007ad316fc587cea8f852ab458a41eddba3ba
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-19 08:22:38 -0800

    Update molotov to 3.1.0 (#59144)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/molotov.rb
+++ Casks/molotov.rb
@@ -1,6 +1,6 @@
 cask 'molotov' do
-  version '3.0.2'
-  sha256 'e514b867375bf82915862334c9b409ef695056d73d02e2d2709f44a04d42d029'
+  version '3.1.0'
+  sha256 '37f4372515ed4056f34bf50a010410c97cab704e7950330be8051f9c1679c307'
 
   url "https://desktop-auto-upgrade.molotov.tv/mac/Molotov-v#{version}.dmg"
   name 'Molotov'
