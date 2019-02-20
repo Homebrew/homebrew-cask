0c6b25ae0ee4b10201519e7f400f9dc4ef4ca406
acheronfail <acheronfail@gmail.com>
2019-02-20 13:42:40 +1100

    Update Pixel Picker.app to 1.3.3 (#59184)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/pixel-picker.rb
+++ Casks/pixel-picker.rb
@@ -1,6 +1,6 @@
 cask 'pixel-picker' do
-  version '1.3.2'
-  sha256 'e82420b680c53601c1a9ef4014416983dffbf4ac8ce74124e4375f4bbc48bb35'
+  version '1.3.3'
+  sha256 '67594e66473527d51aa5f7def7353196e714f1008107f4f9171fa8bf2c738df4'
 
   url "https://github.com/acheronfail/pixel-picker/releases/download/#{version}/Pixel.Picker.#{version}.dmg"
   appcast 'https://github.com/acheronfail/pixel-picker/releases.atom'
