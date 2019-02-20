b4af0614fe777b3c12dabcbabd3cb99609a6ade5
CoreCode <core-code@users.noreply.github.com>
2019-02-20 17:28:39 +0100

    Update capto to 1.2.11,1548837086 (#59213)

--- Casks/capto.rb
+++ Casks/capto.rb
@@ -1,6 +1,6 @@
 cask 'capto' do
-  version '1.2.10,1541678419'
-  sha256 '41a5c554791dd4432e9ede6923e2c40e7d0e2839fc2f970e69648a9c21a1f604'
+  version '1.2.11,1548837086'
+  sha256 '8e2e94a6c4d1f0e7abcf3dd26be2317b123a2039c7fdb72fe3c37cb744948ac9'
 
   # dl.devmate.com/com.globaldelight.Capto was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.globaldelight.Capto/#{version.before_comma}/#{version.after_comma}/Capto-#{version.before_comma}.dmg"
