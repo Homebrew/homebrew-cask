d34548ebb2f01c19ef77cb5db899ff351537aa7a
optikfluffel <optik@fluffel.io>
2019-02-20 18:31:39 +0100

    Update volt to 0.62

--- Casks/volt.rb
+++ Casks/volt.rb
@@ -1,6 +1,6 @@
 cask 'volt' do
-  version '0.61'
-  sha256 '5e480cc083a6351416ff63efe3ce3cdcca746df76af370dcfe1462cec7143705'
+  version '0.62'
+  sha256 '37c9f320f836058da4fbc1998dce1f16e0d09566fafac87ae345d92dce1b5a1f'
 
   # github.com/voltapp/volt was verified as official when first introduced to the cask
   url "https://github.com/voltapp/volt/releases/download/#{version}/Volt.dmg"
