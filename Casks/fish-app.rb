e6c3ee6bf699a5c21f9abdbeb8b992867dbac0c3
Work <brigitte@corecode.io>
2019-02-20 09:45:31 +0100

    Update fish-app to 3.0.2

--- Casks/fish-app.rb
+++ Casks/fish-app.rb
@@ -1,9 +1,9 @@
 cask 'fish-app' do
-  version '3.0.1'
-  sha256 '104e023ed25f2ca5d4bb565c868bebaf64ef4394b8a518804da83b63bad327ea'
+  version '3.0.2'
+  sha256 'cd842d08b06d888978f2e7da2b856e386a86e8180d2137da322045964f9badc5'
 
   # github.com/fish-shell/fish-shell was verified as official when first introduced to the cask
-  url "https://github.com/fish-shell/fish-shell/releases/download/#{version}/fish.app.zip"
+  url "https://github.com/fish-shell/fish-shell/releases/download/#{version}/fish-#{version}.app.zip"
   appcast 'https://fishshell.com/release_notes.html'
   name 'Fish App'
   homepage 'https://fishshell.com/'
