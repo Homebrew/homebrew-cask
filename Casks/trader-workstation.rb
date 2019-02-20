75e7807f89e3dd28064824e9568ea30cf448b068
Matthias Fasching <matthias.fasching@zuehlke.com>
2019-02-20 13:14:59 +0100

    Update Trader Workstation to use offline installer.

--- Casks/trader-workstation.rb
+++ Casks/trader-workstation.rb
@@ -2,18 +2,18 @@ cask 'trader-workstation' do
   version :latest
   sha256 :no_check
 
-  url 'https://download2.interactivebrokers.com/installers/tws/latest/tws-latest-macosx-x64.dmg'
+  url 'https://download2.interactivebrokers.com/installers/tws/latest-standalone/tws-latest-standalone-macosx-x64.dmg'
   name 'Trader Workstation'
   homepage 'https://www.interactivebrokers.com/'
 
   installer script: {
-                      executable: "#{staged_path}/Trader Workstation Installer.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{staged_path}/Trader Workstation 974 Installer.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 
   uninstall quit:   'com.install4j.5889-6375-8446-2021.22',
             script: {
-                      executable: "#{ENV['HOME']}/Applications/Trader Workstation/Trader Workstation Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{ENV['HOME']}/Applications/Trader Workstation 974/Trader Workstation 974 Uninstaller.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 end
