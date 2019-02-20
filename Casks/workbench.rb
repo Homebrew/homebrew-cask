c9d07a7d5fbd67be829701a8853408b38b2fa8d2
chrisfinazzo <chris.finazzo@gmail.com>
2019-02-19 23:06:35 -0500

    Add Workbench.app v1.0.0

---
+++ Casks/workbench.rb
@@ -0,0 +1,10 @@
+cask 'workbench' do
+  version '1.0.0'
+  sha256 'd57d4bb8c2029aeebe13cdd83a2d232fb2e511d542ea7863a23605b444dc62f0'
+
+  url 'https://github.com/mxcl/Workbench/archive/1.0.0.tar.gz'
+  name 'Workbench'
+  homepage 'https://github.com/mxcl/Workbench'
+
+  app 'Workbench.app'
+end
