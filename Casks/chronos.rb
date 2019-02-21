9c9eca8058fb68b9e219c51ada0d62bdf5e49b1d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-21 00:20:30 -0800

    Update chronos to 3.0.1 (#59239)

--- Casks/chronos.rb
+++ Casks/chronos.rb
@@ -1,6 +1,6 @@
 cask 'chronos' do
-  version '3.0.0'
-  sha256 'c906c34d5eadc174dfa18345d2bbc74e4d22b2901bc11cb046c3077bd7070e38'
+  version '3.0.1'
+  sha256 '198b30184ac23ca025cf8da8b14dd98f39285cc565073303d6fccb3ffc4fd5c9'
 
   url "https://github.com/web-pal/chronos-timetracker/releases/download/v#{version}/Chronos-#{version}-mac.zip"
   appcast 'https://github.com/web-pal/chronos-timetracker/releases.atom'
