492940f3285726bad5b1abdfc89d8a80ecc03081
Randall <ran.dall@icloud.com>
2019-02-21 00:28:04 -0800

    Update kite to 0.20190219.0

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,6 +1,6 @@
 cask 'kite' do
-  version '0.20190131.0'
-  sha256 '4b33884e77e4e000fb74750f4bec4cc3fbbb501e0d23e9efbc3acfcd53ca0246'
+  version '0.20190219.0'
+  sha256 'ae85aa98d0fe805ab6aa50f04f910bfd4c78bed88d660f3d3425d15eb7b0ad1c'
 
   # s3-us-west-1.amazonaws.com/kite-downloads was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/kite-downloads/Kite-#{version}.dmg"
