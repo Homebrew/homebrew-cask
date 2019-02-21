1ac31dec97bbfc4cd0e55f4cf628718e7a41dc9c
Oran Shuster <oran.shuster@spotinst.com>
2019-02-21 09:09:17 +0200

    Update volt to 0.63

--- Casks/volt.rb
+++ Casks/volt.rb
@@ -1,6 +1,6 @@
 cask 'volt' do
-  version '0.62'
-  sha256 '37c9f320f836058da4fbc1998dce1f16e0d09566fafac87ae345d92dce1b5a1f'
+  version '0.63'
+  sha256 '3bdb05e446b9f2748b099070b334b4497379b710636918312b4c399d5bc5d367'
 
   # github.com/voltapp/volt was verified as official when first introduced to the cask
   url "https://github.com/voltapp/volt/releases/download/#{version}/Volt.dmg"
