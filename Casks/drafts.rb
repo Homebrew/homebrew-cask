e0c0aa852735dd82de4489391f17d30cadcf3e9a
suschizu <brigitte@corecode.io>
2019-02-21 22:02:23 +0100

    Update drafts to 5.7.5 (#59276)

--- Casks/drafts.rb
+++ Casks/drafts.rb
@@ -1,6 +1,6 @@
 cask 'drafts' do
-  version '5.7.2'
-  sha256 'fd8ee1c6953445901d62583eca102000f805f8a51c584656975563383781acbe'
+  version '5.7.5'
+  sha256 'ec53c1563a80aaa8443350505d587636e812f11c0c4a2a2f0e6d7f11289675f8'
 
   # s3-us-west-2.amazonaws.com/downloads.agiletortoise.com was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/Drafts.app.zip'
