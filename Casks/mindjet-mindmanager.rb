42ec7adf66f48fa68ce00df3221c666453836800
suschizu <brigitte@corecode.io>
2019-02-21 22:36:53 +0100

    Update mindjet-mindmanager to 12.0.161 (#59261)

--- Casks/mindjet-mindmanager.rb
+++ Casks/mindjet-mindmanager.rb
@@ -1,6 +1,6 @@
 cask 'mindjet-mindmanager' do
-  version '11.2.117'
-  sha256 'e0f34d105e260522277852800af4ee94298f8ef8fde2784a9bf5899e9f5e2a4d'
+  version '12.0.161'
+  sha256 '8fe9b6d77cf4627f9397f11ca8520a7944f3db4de7983414f6f69ce8fac1ccc4'
 
   url "https://download.mindjet.com/Mac/Mindjet_MindManager_Mac_#{version}.dmg"
   name 'Mindjet Mindmanager'
