584066d02b801c082b9cc60cad9db59fa8ec4bfd
suschizu <brigitte@corecode.io>
2019-02-21 22:55:41 +0100

    Update fork to 1.0.74.2 (#59281)

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,6 +1,6 @@
 cask 'fork' do
-  version '1.0.74'
-  sha256 '04b7a06735d34492830d12696cb5145798dee11eb8840a5e7fdf90807e42c5b8'
+  version '1.0.74.2'
+  sha256 '74f971ea29adec68a41b2d0ea3fb6bc1dcbab3fad4d7aaaaa21394fe9e2bc0a6'
 
   # forkapp.ams3.cdn.digitaloceanspaces.com/mac was verified as official when first introduced to the cask
   url "https://forkapp.ams3.cdn.digitaloceanspaces.com/mac/Fork-#{version}.dmg"
