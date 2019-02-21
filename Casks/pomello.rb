e0cb10ace7a060c22b79d980cd7c820cb6bfbf06
suschizu <brigitte@corecode.io>
2019-02-21 22:08:08 +0100

    Update pomello to 0.10.5 (#59278)

--- Casks/pomello.rb
+++ Casks/pomello.rb
@@ -1,6 +1,6 @@
 cask 'pomello' do
-  version '0.10.4'
-  sha256 '34e64f837f43995bc946c3e2696cd6c5f535f6721526c2ced4638f2acc9267e7'
+  version '0.10.5'
+  sha256 '0a580ff06e64ae71b19ac05886523cc4c73bf8450a96622e455f2a8c9ad4a412'
 
   url 'https://pomelloapp.com/download/mac/latest'
   appcast 'https://pomelloapp.com/download/mac'
