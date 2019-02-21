85155ae89588e1e68485ecd7738877835e64048c
Tommy Chin Jr <tommychinjr@users.noreply.github.com>
2019-02-20 22:13:44 -0500

    Update Signal to 1.22.0

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,6 +1,6 @@
 cask 'signal' do
-  version '1.20.0'
-  sha256 '4d9c22af69fd62271ddc49d267c064f4fdc83f66036ff55360d3d04e35585265'
+  version '1.22.0'
+  sha256 '777496e4b23f1d088383dfc9694deb646313a2020bd5cd17ab93f68be31d5317'
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.zip"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
