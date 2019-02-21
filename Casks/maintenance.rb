98433edbf6686d2e1f52ceb3375e32b32c99a542
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-21 00:20:34 -0800

    Update maintenance to 2.4.2 (#59238)

--- Casks/maintenance.rb
+++ Casks/maintenance.rb
@@ -15,8 +15,8 @@ cask 'maintenance' do
     version '2.4.2'
     sha256 '94c7a322d4d796afc5e52534f3564a562240d9c0ec0a60de210e68372fef2137'
   else
-    version '2.4.9'
-    sha256 '1e81ed81379aea46a9530ede078009a56ac65db33deeeb808550bff14556f366'
+    version '2.5.0'
+    sha256 '94c7a322d4d796afc5e52534f3564a562240d9c0ec0a60de210e68372fef2137'
   end
 
   url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete('.')}/Maintenance.dmg"
