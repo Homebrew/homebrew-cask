7b5c373e07171b2ca1dce6ec707111cf23b016cf
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-21 05:53:30 -0800

    Update permute to 3.1.1,2090 (#59252)

--- Casks/permute.rb
+++ Casks/permute.rb
@@ -1,8 +1,8 @@
 cask 'permute' do
-  version '3.0.9,2075'
-  sha256 '37d069a67ff02c07a4aea8055498b467bd7f63b4a54af24982efde47ec4be269'
+  version '3.1.1,2090'
+  sha256 '7a9facd6316930999f4f196080703c736bcf609a79d6cb02f4de914ceaec7f96'
 
-  url "https://trial.charliemonroe.net/permute/Permute_#{version.major}_#{version.after_comma}.zip"
+  url "https://trial.charliemonroe.net/permute/v#{version.major}/Permute_#{version.major}_#{version.after_comma}.zip"
   appcast "https://trial.charliemonroe.net/permute/updates_#{version.major}.xml"
   name 'Permute'
   homepage 'https://software.charliemonroe.net/permute.php'
