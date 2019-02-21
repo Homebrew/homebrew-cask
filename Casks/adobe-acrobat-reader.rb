ad792a7937dd24dfe6629e758e030e2e31849396
Anver Housseini <contact@anver.ch>
2019-02-21 18:24:59 +0100

    Update adobe-acrobat-reader to 19.010.20098 (#59262)

--- Casks/adobe-acrobat-reader.rb
+++ Casks/adobe-acrobat-reader.rb
@@ -1,6 +1,6 @@
 cask 'adobe-acrobat-reader' do
-  version '19.010.20091'
-  sha256 'a1dcd94995e5aa4fee02c51beb038b4f5da6260183b143f691150997ebda587e'
+  version '19.010.20098'
+  sha256 '5403434a1879dcf5caa114bab6b5642fff57e296e38bbcd629ef4f3aea94bdfa'
 
   url "http://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/#{version.no_dots}/AcroRdrDC_#{version.no_dots}_MUI.dmg"
   appcast 'https://get.adobe.com/reader/'
