984aba86f8bbe7dce91b05e58952051a927a4df4
ahousseini <contact@anver.ch>
2019-02-21 13:37:00 +0100

    Update opera to 58.0.3135.68

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '58.0.3135.65'
-  sha256 '6ca52dc8d3a004ef0226e13cd6628f05b936f9696ac9ae5ffc32b68d861e074c'
+  version '58.0.3135.68'
+  sha256 '9809135d9d0a142c6a58f24220b8639401817765d48829594ed3b23cbb6bbc65'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   name 'Opera'
