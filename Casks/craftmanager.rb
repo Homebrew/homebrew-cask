0e1014dadca4a72d4e49b34b1135ecf05b921348
Naoto Yoshimoto <naotone@floatingdots.com>
2019-02-21 19:12:43 -0500

    Update craftmanager to 1.0.76

--- Casks/craftmanager.rb
+++ Casks/craftmanager.rb
@@ -1,6 +1,6 @@
 cask 'craftmanager' do
-  version '1.0.73'
-  sha256 '825294651f9a52ec708194f7c6d5c3372cdbbcb5e3ea57f6e4d75582b04d9b0a'
+  version '1.0.76'
+  sha256 '8a5f7d6f39f34f44ca93054b11213203920f7a0d6e01b8a30fe77502d54f2d6e'
 
   url 'https://craft-assets.invisionapp.com/CraftManager/production/CraftManager.zip'
   appcast 'https://craft-assets.invisionapp.com/CraftManager/production/appcast.xml'
