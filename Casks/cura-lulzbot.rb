792a8c271f4a3bb8c21e74af93ef824d10c14ecc
Vítor Galvão <info@vitorgalvao.com>
2019-02-22 20:13:51 +0000

    Update cura-lulzbot.rb

--- Casks/cura-lulzbot.rb
+++ Casks/cura-lulzbot.rb
@@ -14,20 +14,4 @@ cask 'cura-lulzbot' do
                '~/Library/Preferences/org.pythonmac.unspecified.cura-lulzbot.cura-lulzbot',
                '~/Library/Saved Application State/org.pythonmac.unspecified.cura-lulzbot.savedState',
              ]
-
-  # Caveat is included per the "Upgrading? Clean your Cache!" section, here:
-  # https://www.lulzbot.com/learn/tutorials/cura-lulzbot-edition-installation-macos
-  caveats <<-CAVEAT
-  If you are upgrading from a previous version of Cura Lulzbot Edition,
-  Lulzbot reccommends that you clear your cache.
-
-  The commands below will delete the older slicing profiles and 3D printer settings
-  from previous versions of Cura LulzBot Edition. Back up the files and folders
-  listed below first if any customizations have been made.
-
-  Open Terminal, and run the following command:
-
-     rm -rf ~/Library/Application\ Support/cura-lulzbot
-
-  CAVEAT
 end
