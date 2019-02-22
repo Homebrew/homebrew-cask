765b28b4fbf243677a7ea0fff6c4e6aec27afc78
Vítor Galvão <info@vitorgalvao.com>
2019-02-22 21:26:02 +0000

    Update workbench.rb

--- Casks/workbench.rb
+++ Casks/workbench.rb
@@ -1,6 +1,6 @@
 cask 'workbench' do
   version '1.0.0'
-  sha256 'f33d1ba7c516d2b6fbde9912b12048fff357c6cdff11abfd136715acb586ee09'
+  sha256 '4d5324c5c134ea680fab932967a612474452345b810c83617f2d07650a976203'
 
   url "https://github.com/mxcl/Workbench/releases/download/#{version}/Workbench-#{version}.zip"
   appcast 'https://github.com/mxcl/Workbench/releases.atom'
