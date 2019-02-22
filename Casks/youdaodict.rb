9eb4afc6d2e32de29594c25de675ab4a74c8c877
nuomi1 <nuomi1@qq.com>
2019-02-22 22:15:05 +0800

    Update youdaodict.rb

--- Casks/youdaodict.rb
+++ Casks/youdaodict.rb
@@ -3,6 +3,7 @@ cask 'youdaodict' do
   sha256 'e5ab7081c48420353f34234b30ebe5600a002e9d01a24ef1682afffbcdfaa167'
 
   url 'http://codown.youdao.com/cidian/download/MacDict.dmg'
+  appcast 'http://cidian.youdao.com/mac-update/macdict_update.xml'
   name 'YoudaoDict'
   name '网易有道词典'
   homepage 'https://cidian.youdao.com/index-mac.html'
