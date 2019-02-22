8a4a6fffcccb594c9eadd8f2d5fde772c1cf69af
Vítor Galvão <info@vitorgalvao.com>
2019-02-22 18:48:24 +0000

    Update ocenaudio.rb

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -2,7 +2,7 @@ cask 'ocenaudio' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg?'
+  url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg'
   name 'ocenaudio'
   homepage 'https://www.ocenaudio.com/en'
 
