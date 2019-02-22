73b2542863e4e5830998223d26d736591df566ec
Vítor Galvão <info@vitorgalvao.com>
2019-02-22 18:46:43 +0000

    Update flock.rb

--- Casks/flock.rb
+++ Casks/flock.rb
@@ -4,6 +4,7 @@ cask 'flock' do
 
   # flock.co was verified as official when first introduced to the cask
   url "https://updates.flock.co/fl_mac_electron/Flock-macOS-#{version}.dmg"
+  appcast 'https://bingo.flock.co/mac_electron.php'
   name 'Flock'
   homepage 'https://flock.com/'
 
