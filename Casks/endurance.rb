4b0f7d12ab83843c1022164bc97027bd04cafa9e
Work <brigitte@corecode.io>
2019-02-22 09:56:44 +0100

    Update endurance to 2.0,35

--- Casks/endurance.rb
+++ Casks/endurance.rb
@@ -1,8 +1,8 @@
 cask 'endurance' do
-  version '1.2'
-  sha256 '901114580f4e72241ad9233a8c935ba224ee32fa99fd78e3da54168cb21b0833'
+  version '2.0,35'
+  sha256 '4255023026ff263aa102c799b297b9f0c7bcda76bd7e1ce133dd5ed8061f4547'
 
-  url "https://enduranceapp.com/beta/Endurance#{version}.zip"
+  url "https://enduranceapp.com/beta/Endurance#{version.after_comma}.zip"
   appcast 'https://enduranceapp.com/appcast'
   name 'Endurance'
   homepage 'https://enduranceapp.com/'
