545e564f510a6e9bac9332aed704b996653a385e
Vítor Galvão <info@vitorgalvao.com>
2019-02-22 21:06:57 +0000

    ocenaudio.rb: remove interpolation in appcast

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -3,7 +3,7 @@ cask 'ocenaudio' do
   sha256 'b44f2f962b9ae204e6e12249d92784c639fc3949000bf834c7a6b80a21752eb8'
 
   url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg'
-  appcast "https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=#{url}"
+  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg'
   name 'ocenaudio'
   homepage 'https://www.ocenaudio.com/en'
 
