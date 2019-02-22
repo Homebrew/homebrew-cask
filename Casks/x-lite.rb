72e01dfdb7393e6342cb6b561d08f1d91df91bde
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-02-22 09:05:15 -0500

    Upgrade x-lite to 5.5.0_97576 (#59320)

--- Casks/x-lite.rb
+++ Casks/x-lite.rb
@@ -1,6 +1,6 @@
 cask 'x-lite' do
-  version '5.4.0_94385'
-  sha256 '9a84965af675ac5ff3aa84002b8bf27b298b990c2bd36e4a13d938ec2f4503f7'
+  version '5.5.0_97576'
+  sha256 '85a0b7e48a12bb6d4aecf48b0180f9fd2a5f491bd6170bbf5d8350cdbc56e582'
 
   # counterpath.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://counterpath.s3.amazonaws.com/downloads/X-Lite_#{version}.dmg"
