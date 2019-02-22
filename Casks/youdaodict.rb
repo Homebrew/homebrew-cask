5d39dc92fdd1855c3264acc681bdaf48ac2104a6
nuomi1 <nuomi1@qq.com>
2019-02-23 05:26:19 +0800

    Update youdaodict.rb for version, sha256, appcast and zap (#59322)
    * Update youdaodict.rb for version, sha256 and zap
    
    * Update youdaodict.rb
    
    * Update youdaodict.rb
    
    * Update youdaodict.rb
    
    * Update youdaodict.rb

--- Casks/youdaodict.rb
+++ Casks/youdaodict.rb
@@ -1,13 +1,19 @@
 cask 'youdaodict' do
-  version :latest
-  sha256 :no_check
+  version '151'
+  sha256 'e5ab7081c48420353f34234b30ebe5600a002e9d01a24ef1682afffbcdfaa167'
 
   url 'http://codown.youdao.com/cidian/download/MacDict.dmg'
+  appcast 'https://cidian.youdao.com/mac-update/macdict_update.xml'
   name 'YoudaoDict'
   name '网易有道词典'
   homepage 'https://cidian.youdao.com/index-mac.html'
 
   app '网易有道词典.app'
 
-  zap trash: '~/Library/com.youdao.YoudaoDict'
+  zap trash: [
+               '~/Library/Application Support/com.youdao.YoudaoDict',
+               '~/Library/Cookies/com.youdao.YoudaoDict.binarycookies',
+               '~/Library/Preferences/com.youdao.YoudaoDict.plist',
+               '~/Library/com.youdao.YoudaoDict',
+             ]
 end
