cad41adfc1dbbfde0d1d25dd16f9e58a83873a24
CoreCode <core-code@users.noreply.github.com>
2019-02-23 19:07:29 +0100

    Update autopano-giga.rb

--- Casks/autopano-giga.rb
+++ Casks/autopano-giga.rb
@@ -2,7 +2,7 @@ cask 'autopano-giga' do
   version '4.4.2'
   sha256 '0d5808f3904dae5937411ba7efdb275eaef2dea957c89b869c6c637aa29b824a'
 
-  url 'http://download.kolor.com/apg/stable/mac'
+  url 'https://download.kolor.com/apg/stable/mac'
   appcast "https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=#{url}"
   name 'Autopano Giga'
   homepage 'https://www.kolor.com/autopano/'
