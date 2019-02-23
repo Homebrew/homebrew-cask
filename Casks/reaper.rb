d9b446b8e0c09f93a6b8e4e4958021e9300c534e
suschizu <brigitte@corecode.io>
2019-02-23 14:46:00 +0100

    Update reaper to 5.97.0 (#59350)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,6 +1,6 @@
 cask 'reaper' do
-  version '5.96.500'
-  sha256 'e901cecd6615cdf827a271b15ca2c373ca6f32cf5237e04e8df655657112e512'
+  version '5.97.0'
+  sha256 '9d4d249ab7a1b8480c81225d4d6b391effa7fc7333a4f9cda31d72900ba25b66'
 
   url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots.sub(%r{0*$}, '')}_x86_64.dmg"
   appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64'
