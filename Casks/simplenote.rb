cef678a24a3716277c8c530e364b6085f0278c13
suschizu <brigitte@corecode.io>
2019-02-23 21:38:02 +0100

    Update simplenote to 1.5.0 (#59365)
    * Update simplenote to 1.5.0
    
    * Update simplenote.rb

--- Casks/simplenote.rb
+++ Casks/simplenote.rb
@@ -1,13 +1,13 @@
 cask 'simplenote' do
-  version '1.4.1'
-  sha256 '9c9d6972fdb687850b77de7cd82993c1bed1dba1984208e5b9ead0c0604e5deb'
+  version '1.5.0'
+  sha256 '003b3e304d592fa5ba35af5bcc434b8388ba8dfba9337c795c24ef74fe8ddba9'
 
-  url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.zip"
+  url "https://github.com/Automattic/simplenote-electron/releases/download/v#{version}/Simplenote-macOS-#{version}.dmg"
   appcast 'https://github.com/Automattic/simplenote-electron/releases.atom'
   name 'Simplenote'
   homepage 'https://github.com/Automattic/simplenote-electron'
 
-  app 'mac/Simplenote.app'
+  app 'Simplenote.app'
 
   zap trash: [
                '~/Library/Application Support/Simplenote',
