8c72e23eafca2a60ff94564378c005803d40a6c1
Robert Clark <returntoreptar@gmail.com>
2019-02-23 07:25:31 -0500

    Fix link to sqlitebrowser to reflect sqlitebrowser github releases page and hash (#59346)
    * Fix link to sqlitebrowser to reflect sqlitebrowser github releases page and hash
    
    * Update db-browser-for-sqlite.rb

--- Casks/db-browser-for-sqlite.rb
+++ Casks/db-browser-for-sqlite.rb
@@ -1,6 +1,6 @@
 cask 'db-browser-for-sqlite' do
-  version '3.11.1'
-  sha256 'bd4a74540a63a262fc49b816e8fc71fd816e81b215c31572d96b169d980a573e'
+  version '3.11.1v2'
+  sha256 'b0ee5b73b9c6305de79640f651ba59edd32c6a94c2245a2bda01ae8091a69b48'
 
   # github.com/sqlitebrowser/sqlitebrowser was verified as official when first introduced to the cask
   url "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v#{version.major_minor_patch}/DB.Browser.for.SQLite-#{version}.dmg"
