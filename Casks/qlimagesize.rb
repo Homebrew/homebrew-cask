6c4a0848720c5327ee4c776cc50c6b20ef727a73
Vítor Galvão <info@vitorgalvao.com>
2019-02-24 19:04:45 +0000

    qlimagesize.rb: update to 2.6

--- Casks/qlimagesize.rb
+++ Casks/qlimagesize.rb
@@ -1,9 +1,9 @@
 cask 'qlimagesize' do
-  version '1.6.1'
-  sha256 'd7110f94a6be53e01d01ed92fb894395f54c0e2db7e69507d4eaf2970942a820'
+  version '2.6'
+  sha256 '461c59f48d58f58f3e475aec592c77d511460668e763db04a609de800072f18e'
 
   url "https://github.com/Nyx0uf/qlImageSize/releases/download/#{version}/qlImageSize.qlgenerator.zip"
-  appcast 'https://github.com/martindelille/qlimagesize/releases.atom'
+  appcast 'https://github.com/Nyx0uf/qlimagesize/releases.atom'
   name 'qlImageSize'
   homepage 'https://github.com/Nyx0uf/qlImageSize'
 
