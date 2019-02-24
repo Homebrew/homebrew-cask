9694190bac50f92705575d643da24efff099a012
suschizu <brigitte@corecode.io>
2019-02-24 22:34:31 +0100

    Update rekordbox.rb

--- Casks/rekordbox.rb
+++ Casks/rekordbox.rb
@@ -3,6 +3,7 @@ cask 'rekordbox' do
   sha256 'f08e83968d410195e343b21194fc5c4142b8531bfeaef072f7d8c3350427d7c5'
 
   url "https://rekordbox.com/_app/files/Install_rekordbox_#{version.dots_to_underscores}.pkg.zip"
+  appcast 'https://rekordbox.com/en/support/releasenote.php'
   name 'rekordbox'
   homepage 'https://rekordbox.com/en/'
 
