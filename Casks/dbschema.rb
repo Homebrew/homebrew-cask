e860868422f1b46c1274af2bf711611d0b88ec8d
Julian Mayer <julian@corecode.io>
2019-02-24 14:27:38 +0100

    Update dbschema to 8.1.3

--- Casks/dbschema.rb
+++ Casks/dbschema.rb
@@ -3,7 +3,7 @@ cask 'dbschema' do
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.dbschema.com/download/DbSchema_macos_#{version.dots_to_underscores}.tgz"
-  appcast 'https://www.dbschema.com/dbschema-change-log.html'
+  appcast 'https://www.dbschema.com/changelog.txt'
   name 'DbSchema'
   homepage 'https://www.dbschema.com/'
 
