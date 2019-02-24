7510786e24e87562c7a0638fa325a8326667f98f
Derek Jones <derek.jones@ellislab.com>
2019-02-24 13:28:57 -0800

    Upgrade Nord Sound Manager to v7.42 (#59432)

--- Casks/nord-sound-manager.rb
+++ Casks/nord-sound-manager.rb
@@ -1,8 +1,8 @@
 cask 'nord-sound-manager' do
-  version '7.32'
-  sha256 '5711ec67216c421cd5e2c69e60846aafbfab7f682f3b80dff160de93bf933377'
+  version '7.42'
+  sha256 'e40968744c318d6a1f55ed2882f7ad993daa59c0ae0b2c3d37070c36154f378c'
 
-  url "http://www.nordkeyboards.com/sites/default/files/files/downloads/software/nord-sound-manager/Nord%20Sound%20Manager%20v#{version}.dmg"
+  url "https://www.nordkeyboards.com/sites/default/files/files/downloads/software/nord-sound-manager/Nord%20Sound%20Manager%20v#{version}.dmg"
   name 'Nord Sound Manager'
   homepage 'http://www.nordkeyboards.com/downloads/software-tools/nord-sound-manager'
 
