bca40def76158aba46f2225ad4ea5bcc4e8e8d0f
Julian Mayer <julian@corecode.io>
2019-02-24 14:13:50 +0100

    Update openttd to 1.8.0

--- Casks/openttd.rb
+++ Casks/openttd.rb
@@ -3,6 +3,7 @@ cask 'openttd' do
   sha256 'fb5ac81ddec5a62efcf3cc44fde983e20a0ca61820ae6ee72b67323f45f5494e'
 
   url "https://binaries.openttd.org/releases/#{version}/openttd-#{version}-macosx-universal.zip"
+  appcast 'https://www.openttd.org/downloads/openttd-releases/latest.html'
   name 'OpenTTD'
   homepage 'https://www.openttd.org/'
 
