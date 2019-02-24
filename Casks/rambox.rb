eb8f312ecd6d1a263449cf45249b53e175189eed
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-02-24 20:03:29 +0100

    Update rambox 0.6.3 zap

--- Casks/rambox.rb
+++ Casks/rambox.rb
@@ -13,15 +13,20 @@ cask 'rambox' do
   uninstall login_item: 'Rambox'
 
   zap trash: [
+               '~/Library/Application Support/CrashReporter/Rambox Helper_*.plist',
+               '~/Library/Application Support/CrashReporter/Rambox_*.plist',
+               '~/Library/Application Support/Rambox',
                '~/Library/Caches/com.grupovrs.ramboxce',
                '~/Library/Caches/com.grupovrs.ramboxce.ShipIt',
                '~/Library/Caches/com.saenzramiro.rambox',
                '~/Library/Logs/Rambox',
+               '~/Library/Preferences/ByHost/com.grupovrs.ramboxce.ShipIt.*.plist',
                '~/Library/Preferences/com.grupovrs.ramboxce.helper.plist',
                '~/Library/Preferences/com.grupovrs.ramboxce.plist',
                '~/Library/Preferences/com.saenzramiro.rambox.helper.plist',
                '~/Library/Preferences/com.saenzramiro.rambox.plist',
                '~/Library/Saved Application State/com.grupovrs.ramboxce.savedState',
                '~/Library/Saved Application State/com.saenzramiro.rambox.savedState',
+               '~/Library/WebKit/com.saenzramiro.rambox',
              ]
 end
