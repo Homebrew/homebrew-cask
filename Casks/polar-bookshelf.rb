83e08631acf5e619f1f93a235a670b9367899313
suschizu <brigitte@corecode.io>
2019-02-24 22:37:50 +0100

    Update polar-bookshelf to 1.13.4 (#59438)

--- Casks/polar-bookshelf.rb
+++ Casks/polar-bookshelf.rb
@@ -1,6 +1,6 @@
 cask 'polar-bookshelf' do
-  version '1.13.2'
-  sha256 '9386bbf689ac020b22cf9911bda444a06e231c909416a539d514e5f24cc5bbea'
+  version '1.13.4'
+  sha256 'dda35ecedf0aa23ea845b06b20dcfad9d8cc270b1a4bb4d5d292a36a894d2935'
 
   # github.com/burtonator/polar-bookshelf was verified as official when first introduced to the cask
   url "https://github.com/burtonator/polar-bookshelf/releases/download/v#{version}/polar-bookshelf-#{version}.dmg"
