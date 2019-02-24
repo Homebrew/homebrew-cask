265cccb0a4b8a5a680eebd3433d948ee61575554
Vítor Galvão <info@vitorgalvao.com>
2019-02-24 01:36:28 +0000

    Update marvin.rb

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -3,7 +3,7 @@ cask 'marvin' do
   sha256 'fce9293100fceacabc700e74cac4c6627074f796acf9b716962f16c0ac4b7ff6'
 
   # amazingmarvin.s3-website-us-east-1.amazonaws.com was verified as official when first introduced to the cask
-  url 'http://amazingmarvin.s3-website-us-east-1.amazonaws.com/Marvin.dmg'
+  url "https://s3.amazonaws.com/amazingmarvin/Marvin-#{version}.dmg"
   appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=http://amazingmarvin.s3-website-us-east-1.amazonaws.com/Marvin.dmg'
   name 'Amazing Marvin'
   homepage 'https://www.amazingmarvin.com/'
