cd710f8e735589e44c6aa8d4ca4e14a0a5de894e
CoreCode <core-code@users.noreply.github.com>
2019-02-24 02:12:49 +0100

    Update armortext to 0.20.84 (#59398)

--- Casks/armortext.rb
+++ Casks/armortext.rb
@@ -1,9 +1,10 @@
 cask 'armortext' do
-  version '0.19.32'
-  sha256 'c7211c0ce0b999f35e2a8f6b0e4a7b87dfec08b44a6e58cc6f6fd2e5fc7c291a'
+  version '0.20.84'
+  sha256 'b878f9bc0557b7be753f3975d90e6d3ddb63674ee2cec411a4f4f6ff8298e43b'
 
   # armortext.co was verified as official when first introduced to the cask
   url "https://downloads.armortext.co/desktop/release/#{version}/ArmorText-#{version}-mac64.dmg"
+  appcast 'https://armortext.com/download/'
   name 'Armor Text'
   homepage 'https://armortext.com/'
 
