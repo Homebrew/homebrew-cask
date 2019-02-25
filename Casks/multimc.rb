c318280eaee3afea10ee21ed67ed483f45debcaf
Julian Mayer <julian@corecode.io>
2019-02-25 11:02:35 +0100

    Update multimc to 0.6.5

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -1,6 +1,6 @@
 cask 'multimc' do
-  version '0.6.4'
-  sha256 '648e07e1346cd4f7819d941deebf2a0882f6be3f9a441e377291c10f4e50c55e'
+  version '0.6.5'
+  sha256 'a33588176c1dd4e680d36f0dbb3fff4c71ebdccc2bf7b1c235e087fe14c3bad2'
 
   url 'https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz'
   appcast 'https://github.com/MultiMC/MultiMC5/releases.atom'
