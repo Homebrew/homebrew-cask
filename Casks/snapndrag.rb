3c6f23470a50a530c2445b1f28c5cc29b1e36128
suschizu <brigitte@corecode.io>
2019-02-25 18:40:43 +0100

    Update snapndrag.rb

--- Casks/snapndrag.rb
+++ Casks/snapndrag.rb
@@ -3,9 +3,11 @@ cask 'snapndrag' do
   sha256 'e643cd47fbc3ffbe2ef98f22987ad0e8588d090e017717164a7e14fec9a838da'
 
   url "https://yellowmug.com/download/SnapNDrag_#{version}.dmg"
-  appcast 'https://yellowmug.com/snapndrag/appcast-1012.xml'
+  appcast 'https://yellowmug.com/snapndrag/appcast-1014.xml'
   name 'SnapNDrag'
   homepage 'https://www.yellowmug.com/snapndrag/'
+
+  depends_on macos: '>= :high_sierra'
 
   app 'SnapNDrag.app'
 end
