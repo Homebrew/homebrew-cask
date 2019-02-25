896c8feda967f97c8a58f14f014206f83f73303c
suschizu <brigitte@corecode.io>
2019-02-25 10:57:21 +0100

    Update pomodone.rb (#59445)

--- Casks/pomodone.rb
+++ Casks/pomodone.rb
@@ -3,6 +3,7 @@ cask 'pomodone' do
   sha256 '053d3bfee09fa7606725314d1a56500e5da5d83450ce64906aed1a5335b8bdd1'
 
   url "https://app.pomodoneapp.com/installers/PomoDoneApp-#{version}.dmg"
+  appcast 'https://pomodoneapp.com/download-pomodone-app.html'
   name 'PomoDone'
   homepage 'https://pomodoneapp.com/'
 
