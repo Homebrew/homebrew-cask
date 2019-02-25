bc3b33d8836c797c178b3ddebcd65566069c5e5e
suschizu <brigitte@corecode.io>
2019-02-25 14:27:52 +0100

    Update second-life-viewer.rb

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -3,6 +3,7 @@ cask 'second-life-viewer' do
   sha256 'c2dd672771dbe1761cbecd551c0c8ba8747c88a88f15fb935bdb0a000691be22'
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
+  appcast 'https://secondlife.com/support/downloads/'
   name 'Linden Lab Second Life Viewer'
   homepage 'https://secondlife.com/'
 
