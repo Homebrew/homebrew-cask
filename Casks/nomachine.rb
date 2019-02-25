8fb05377aee87eaed8512864b68fe5f1e1c1fd3e
CoreCode <core-code@users.noreply.github.com>
2019-02-25 12:51:34 +0100

    Update nomachine.rb

--- Casks/nomachine.rb
+++ Casks/nomachine.rb
@@ -1,8 +1,9 @@
 cask 'nomachine' do
-  version '6.4.6_1'
+  version '6.5.6_8'
   sha256 '1d8440ac3b93e7a04802d08e338fcd2ce29dd42834b37094a81ab19ef41882ba'
 
   url "https://download.nomachine.com/download/#{version.major_minor}/MacOSX/nomachine_#{version}.dmg"
+  appcast 'https://www.nomachine.com/download/download&id=7'
   name 'NoMachine'
   homepage 'https://www.nomachine.com/'
 
