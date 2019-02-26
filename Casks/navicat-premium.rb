17eeda04374726d95bf6426a0f67d621168561b9
hyuraku <yu@hill-wood.com>
2019-02-26 23:04:09 +0900

    update navicat-premium@12.1.15

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium' do
   version '12.1.15'
-  sha256 '04bb0dcad6e0157aa7f0e60ff35d4f758affcdc374fd0f3b16bdb782afdf0631'
+  sha256 '6950014d505249842999ca0923a6452065f38ec0b4252997fc7d5d5ec7b46efd'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20Premium&appLang=en'
