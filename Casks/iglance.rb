8abc2c12e13fb92e111085f90bd6aa24f71a2353
Vítor Galvão <info@vitorgalvao.com>
2019-02-26 15:00:40 +0000

    Update iglance.rb

--- Casks/iglance.rb
+++ Casks/iglance.rb
@@ -1,8 +1,8 @@
 cask 'iglance' do
-  version 'v1.3.1'
+  version '1.3.1'
   sha256 '0d81a4ee593cfac50162aa61156ee490218f75a1c8349bcd906825774debbc13'
 
-  url "https://github.com/iglance/iglance/releases/download/#{version}/iGlance.dmg"
+  url "https://github.com/iglance/iglance/releases/download/v#{version}/iGlance.dmg"
   appcast 'https://github.com/iglance/iglance/releases.atom'
   name 'iGlance'
   homepage 'https://github.com/iglance/iGlance'
