4f9dd01f6127ab0a9d2592bfeca88572dff994b2
Vítor Galvão <info@vitorgalvao.com>
2019-02-26 04:32:31 +0000

    Update overflow.rb

--- Casks/overflow.rb
+++ Casks/overflow.rb
@@ -2,16 +2,16 @@ cask 'overflow' do
   version '3.0.4'
   sha256 '77434b1beef3b77c950dcd8f1f8118c1f9bc4f479f8d12f085172fd0ac385eb7'
 
-  # https://stuntsoftware.com/overflow/ was verified as official when first introduced to the cask
-  url 'https://stuntsoftware.com/download/overflow.zip'
-  appcast 'https://stuntsoftware.com/PHP/sparkle/overflow3.php'
+  # stunt.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://stunt.s3.amazonaws.com/overflow#{version}.zip"
+  appcast "https://stuntsoftware.com/PHP/sparkle/overflow#{version.major}.php"
   name 'overflow'
   homepage 'https://stuntsoftware.com/overflow/'
 
-  app 'Overflow 3.app'
+  app "Overflow #{version.major}.app"
 
   zap trash: [
-               '~/Library/Containers/com.stuntsoftware.Overflow3/Data/Library/Preferences/com.stuntsoftware.Overflow3.plist',
+               "~/Library/Containers/com.stuntsoftware.Overflow3/Data/Library/Preferences/com.stuntsoftware.Overflow#{version.major}.plist",
                '~/Library/Containers/com.stuntsoftware.Overflow3/Data/Library/Application Support/default.realm',
              ]
 end
