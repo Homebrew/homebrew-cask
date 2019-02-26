1cc4aba5a4fba54bc38219f5426884639b85da99
Sergio Ortega Sanchez <ortega@tune-is.de>
2019-02-26 10:06:44 +0100

    Update orange to 3.20.1 (#59493)

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,6 +1,6 @@
 cask 'orange' do
-  version '3.18.0'
-  sha256 '1084fd3c60869c22a1c2d04599686bf4b6656c51788d78cc508b3aff739f421f'
+  version '3.20.1'
+  sha256 '0002bdc738eaf9d9b0d7f3a5aaafc37c7e6f7f531de1711d2028f43ca76a0d1e'
 
   url "https://orange.biolab.si/download/files/Orange#{version.major}-#{version}.dmg"
   name 'Orange'
