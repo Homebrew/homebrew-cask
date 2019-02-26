101ff02f805636515c713a1b8754ff3835798d21
Stufi <vcs+stefan@the-jutzis.net>
2019-02-26 14:21:10 +0100

    Update unity-linux-support-for-editor to 2018.3.6f1,a220877bc173 (#59500)

--- Casks/unity-linux-support-for-editor.rb
+++ Casks/unity-linux-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-linux-support-for-editor' do
-  version '2018.3.5f1,76b3e37670a4'
-  sha256 '60c2547409f455c0b576231dbf3a2ed67cb6704b39ede81b7906c471641b6743'
+  version '2018.3.6f1,a220877bc173'
+  sha256 '5c632ddfe649d8bbf2ec7ba079a277f1c92fc69c76d3e0dbf4e3f8fec53f910f'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
