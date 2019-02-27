c27da403529d8e50d871f107e79f2ea43822fc41
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2019-02-27 15:39:06 +0100

    Update minishift to 1.32.0

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -1,6 +1,6 @@
 cask 'minishift' do
-  version '1.31.0'
-  sha256 'f1a752ebb225d85d813ad7c44e34545d56c53e01aad817f8223a51ee95004f9c'
+  version '1.32.0'
+  sha256 'aac7cf70e13ffd39fa2821b105bb817a2edd802383627f9775de156a87aec4ec'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom'
