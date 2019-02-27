fcafe7452452067d83cf6bf20e035909b1141b8c
ralph <ralph@cs.cf.ac.uk>
2019-02-27 21:41:18 +0000

    Update smartsvn to 11.0.1

--- Casks/smartsvn.rb
+++ Casks/smartsvn.rb
@@ -1,6 +1,6 @@
 cask 'smartsvn' do
-  version '11.0.0'
-  sha256 'f330c30be85dd5df101c2414f8c9f003f7305c786fbaabd3bafa5d506f78e329'
+  version '11.0.1'
+  sha256 '8327315545c7be2f43599a9bb5ca4b55bd183936535410d7f0b90e937112674b'
 
   url "https://www.smartsvn.com/downloads/smartsvn/smartsvn-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.smartsvn.com/documents/smartsvn/changelog.txt'
