83053214c2ef2ff85fee7204da93dcc8a3102132
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-26 23:04:13 -0800

    Update navicat-for-mariadb to 12.1.16 (#59533)

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-mariadb' do
-  version '12.1.15'
-  sha256 'd04449ceac0c115e840645d7dc102c6b37f3c55e96ccd37dc81eed5299fbea6b'
+  version '12.1.16'
+  sha256 'bc8774c5c1c559f603b4a81c6c60c7d2c32b47eb008f41e07a76112526bfd4da'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20for%20MariaDB&appLang=en'
