412f51c51bf862302fd601a0eae27085bdbde43d
Vítor Galvão <info@vitorgalvao.com>
2019-02-27 14:38:02 +0000

    Update tankix.rb

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -2,7 +2,7 @@ cask 'tankix' do
   version 'visual1-37541'
   sha256 '0c9a3c1b5c18f7593eb9c733323599b61c218535b380d251737504472ead430f'
 
-  url "http://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
+  url "https://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
   name 'Tanki X'
   homepage 'https://tankix.com/'
 
