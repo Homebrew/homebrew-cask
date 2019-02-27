6eb626e6cad91f7c00051332ec52d0ec66fb9958
Vítor Galvão <info@vitorgalvao.com>
2019-02-27 14:00:58 +0000

    Update veraview.rb

--- Casks/veraview.rb
+++ Casks/veraview.rb
@@ -3,7 +3,7 @@ cask 'veraview' do
   sha256 '57c7a10cb232ff69294f2aad731d84d3b6ced53ce6650bd4c48f1bb30bbbec80'
 
   # newton.ornl.gov/casl was verified as official when first introduced to the cask
-  url "https://newton.ornl.gov/casl/VERAView-"{version}-MacOSX.dmg"
+  url "https://newton.ornl.gov/casl/VERAView-#{version}-MacOSX.dmg"
   appcast 'https://newton.ornl.gov/casl/'
   name 'veraview'
   homepage 'https://github.com/CASL/VERAview'
