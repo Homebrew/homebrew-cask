7c0b2bf9ae554c817fcf7f8086882b86c1058a50
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2019-02-27 12:49:43 +0300

    Update obs to 23.0.1 (#59556)

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -1,6 +1,6 @@
 cask 'obs' do
-  version '23.0'
-  sha256 '8f7364ab91c99626dd7c1e0b3cf255cca3e7a85c14d48e0ea13baa003e4396b0'
+  version '23.0.1'
+  sha256 'df34dbe0c4034943631164dcbc12610ae2b0e8ca7c5e257eaa3cd38781d9114d'
 
   url "https://cdn-fastly.obsproject.com/downloads/obs-mac-#{version}-installer.pkg"
   appcast 'https://github.com/obsproject/obs-studio/releases.atom'
