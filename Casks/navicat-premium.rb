02209b9c19d171f151e72e46238f714eacd5b6ba
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-26 23:07:37 -0800

    update sha256

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium' do
   version '12.1.16'
-  sha256 '6950014d505249842999ca0923a6452065f38ec0b4252997fc7d5d5ec7b46efd'
+  sha256 '1c115f34080f09550ad71cee070509bce1c4e03fe757f7dcf4d4a0f3a877514c'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20Premium&appLang=en'
