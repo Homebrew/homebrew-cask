14bd627247972a9bded169544de0aa78caefc329
Vítor Galvão <info@vitorgalvao.com>
2019-02-28 23:55:26 +0000

    chromium.rb: make versioned

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,9 +1,10 @@
 cask 'chromium' do
-  version :latest
-  sha256 :no_check
+  version '636579'
+  sha256 '5dca9cdd99650e6d4cd89b381864e36c3c274ab0081f32d74d9d64d0c3c822b3'
 
-  # download-chromium.appspot.com was verified as official when first introduced to the cask
-  url 'https://download-chromium.appspot.com/dl/Mac?type=snapshots'
+  # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac was verified as official when first introduced to the cask
+  url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
+  appcast 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE?alt=media'
   name 'Chromium'
   homepage 'https://www.chromium.org/Home'
 
