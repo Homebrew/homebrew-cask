f15e4f41335c4dfa043a3582242bd9858081761b
Vítor Galvão <info@vitorgalvao.com>
2019-02-28 15:23:37 -0500

    Update Casks/hangouts-chat.rb
    Changed name as suggested to "Google Hangouts Chat"
    
    Co-Authored-By: compuguy <alex@compuguy1088.com>

--- Casks/hangouts-chat.rb
+++ Casks/hangouts-chat.rb
@@ -3,7 +3,7 @@ cask 'hangouts-chat' do
   sha256 '30ac3007e565c8ac1f57c92291c9c58dec6a1d303eb19dd7c8c8cc080bb7949b'
 
   url "https://dl.google.com/chat/#{version}/InstallHangoutsChat.dmg"
-  name 'Hangouts Chat'
+  name 'Google Hangouts Chat'
   homepage 'https://gsuite.google.com/products/chat/'
 
   app 'Chat.app'
