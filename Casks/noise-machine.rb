4cdb44d5011be603a7b8cab94294c9d91bbc9f22
robin-cruk <46711368+robin-cruk@users.noreply.github.com>
2019-02-28 15:09:10 +0000

    Updated Noise Machine to 1.15 (#59622)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/noise-machine.rb
+++ Casks/noise-machine.rb
@@ -1,6 +1,6 @@
 cask 'noise-machine' do
-  version '1.14'
-  sha256 'c45197362b0c8e781505ca534952f5d75761fef6a59e356da9a84ac9905973d6'
+  version '1.15'
+  sha256 '6f2a0441eec72294b41805b03bf7c8675bfa721ba3faadd53527c61cf8ed1534'
 
   url 'http://www.publicspace.net/download/signedNM.zip'
   appcast 'http://www.publicspace.net/app/nm.xml'
