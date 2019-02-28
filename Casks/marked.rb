9184c4a112604a35879cf00ccedb906632fb08d0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-02-28 00:58:40 -0800

    Update marked to 2.5.31971 (#59591)
    * Update marked to 2.5.31971
    
    * Update `homepage`
    
    Update `homepage` to use`https`

--- Casks/marked.rb
+++ Casks/marked.rb
@@ -1,11 +1,11 @@
 cask 'marked' do
-  version '2.5.30969'
-  sha256 'c357d05474766bc41e16a98f415b1c176bc324bd683ebafd34385b898486d889'
+  version '2.5.31971'
+  sha256 'f8d067e122a9b3fb9d8244f63b5b137e17f6b43c48ceaf8597b7490f1ce4e034'
 
   url "https://updates.marked2app.com/Marked#{version}.zip"
   appcast 'https://updates.marked2app.com/marked.xml'
   name 'Marked'
-  homepage 'http://marked2app.com/'
+  homepage 'https://marked2app.com/'
 
   auto_updates true
   depends_on macos: '>= :yosemite'
