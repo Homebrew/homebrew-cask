a96f5a64fce73615634b7d5f6ef816004df80e08
Sky.Grain <SkyGrain@users.noreply.github.com>
2019-02-28 16:54:00 +0800

    Update ezip.rb (#59601)
    update version

--- Casks/ezip.rb
+++ Casks/ezip.rb
@@ -1,6 +1,6 @@
 cask 'ezip' do
-  version '1.5.5'
-  sha256 'c62c6fe55adfe6254a496686b039c0c48d3d3c7bd7dc7839264ddfe0f7933f71'
+  version '1.6'
+  sha256 'b534ea00f2b094e8a420b4d4a4ed763c3e373969e149b370d532a79027dc9cc8'
 
   url "https://cdn.awehunt.com/ezip/release/eZip_V#{version}.dmg"
   appcast 'https://ezip.awehunt.com/'
