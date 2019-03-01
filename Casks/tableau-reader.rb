647e8790883b6226a2b37dca408eb724056f4b98
Vítor Galvão <info@vitorgalvao.com>
2019-03-01 13:58:28 +0000

    Casks/tableau-reader.rb: changed appcast from corecode to macupdater

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -3,7 +3,7 @@ cask 'tableau-reader' do
   sha256 '9e178c097452f7f14da640cee66fef240711997fd24a744e7dca959e1d630bd3'
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
-  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/reader/mac'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/reader/mac'
   name 'Tableau Reader'
   homepage 'https://www.tableau.com/products/reader'
 
