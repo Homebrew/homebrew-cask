c1dec9bc3d65d4541ef673ca254bbc45bfb0228d
Randall <ran.dall@icloud.com>
2019-02-28 19:47:54 -0800

    Update cryptobridge to 0.16.4

--- Casks/cryptobridge.rb
+++ Casks/cryptobridge.rb
@@ -1,6 +1,6 @@
 cask 'cryptobridge' do
-  version '0.16.1'
-  sha256 '80bcf27ccb1c1135fff93db0a37b299e8d5a815de6d189294bccce912b8eda44'
+  version '0.16.4'
+  sha256 'e9e92e8f1e841eb09cb5e791d7cde7b7c4d4ea7b6631fdd443a04617a6cc1736'
 
   url "https://github.com/CryptoBridge/cryptobridge-ui/releases/download/v#{version}/CryptoBridge-#{version}.dmg"
   appcast 'https://github.com/CryptoBridge/cryptobridge-ui/releases.atom'
