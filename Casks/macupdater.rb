e31b1a9bd25772705d86aeeee179bc5cc4539d30
Vítor Galvão <info@vitorgalvao.com>
2019-03-01 13:58:28 +0000

    Casks/macupdater.rb: changed appcast from corecode to macupdater

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -3,7 +3,7 @@ cask 'macupdater' do
   sha256 '76ee48d25d86e53db370516f92c0b96ca29b9899b3bda60a3b8023d4158d7ad9'
 
   url "https://www.corecode.io/downloads/macupdater_#{version}.zip"
-  appcast 'https://www.corecode.io/macupdater/macupdater.xml'
+  appcast 'https://macupdater.net/macupdater/macupdater.xml'
   name 'MacUpdater'
   homepage 'https://www.corecode.io/macupdater/index.html'
 
