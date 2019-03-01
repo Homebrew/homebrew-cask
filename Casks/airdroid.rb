f0bed0bf605242336c30a62db6d5dd656f6f43ac
Vítor Galvão <info@vitorgalvao.com>
2019-03-01 13:58:27 +0000

    Casks/airdroid.rb: changed appcast from corecode to macupdater

--- Casks/airdroid.rb
+++ Casks/airdroid.rb
@@ -4,7 +4,7 @@ cask 'airdroid' do
 
   # s3.amazonaws.com/dl.airdroid.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/dl.airdroid.com/AirDroid_Desktop_Client_#{version}.dmg"
-  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=https://srv3.airdroid.com/p20/web/getbinaryredirect?type=dmg&channel=&version='
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://srv3.airdroid.com/p20/web/getbinaryredirect?type=dmg&channel=&version='
   name 'AirDroid'
   homepage 'https://www.airdroid.com/'
 
