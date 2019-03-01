3a0c5a37ce8bc66ce8c33597b3fd5d09242f3ad8
suschizu <brigitte@corecode.io>
2019-03-01 22:32:04 +0100

    Update goodsync to 10.9.26.81,648 (#59678)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.9.26.7,646'
-  sha256 '9eead4cb3b7cda3015740f674bb666d6a9116856157c99b460fb34c38665a643'
+  version '10.9.26.81,648'
+  sha256 '4a07a8a39b6dcb2c1d027603d2b4781c8b609da1c46996322dc088ffa942ae65'
 
   # rink.hockeyapp.net/api/2/apps/8b491acdaa8942108b5d8b019be7fcef was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/8b491acdaa8942108b5d8b019be7fcef/app_versions/#{version.after_comma}?format=zip&avtoken=6b1e930b304f74f53ebe6e776a505fa3bfbe6e6d&download_origin=hockeyapp"
