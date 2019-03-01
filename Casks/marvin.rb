414d0d33866031ec637451e80a371562d96a7062
Vítor Galvão <info@vitorgalvao.com>
2019-03-01 13:58:28 +0000

    Casks/marvin.rb: changed appcast from corecode to macupdater

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -4,7 +4,7 @@ cask 'marvin' do
 
   # s3.amazonaws.com/amazingmarvin was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/amazingmarvin/Marvin-#{version}.dmg"
-  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=http://amazingmarvin.s3-website-us-east-1.amazonaws.com/Marvin.dmg'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://amazingmarvin.s3-website-us-east-1.amazonaws.com/Marvin.dmg'
   name 'Amazing Marvin'
   homepage 'https://www.amazingmarvin.com/'
 
