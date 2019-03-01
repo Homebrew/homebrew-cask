801968497220f5f64a6b116e1d409924cf64c57a
Vítor Galvão <info@vitorgalvao.com>
2019-03-01 13:58:28 +0000

    Casks/deezer.rb: changed appcast from corecode to macupdater

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -3,7 +3,7 @@ cask 'deezer' do
   sha256 '018da811f6fb932cee8c7af4c89577a51bad93b6e3f5d69311965d02a151c820'
 
   url "https://www.deezer.com/desktop/download/artifact/darwin/x64/#{version}"
-  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.deezer.com/desktop/download%3Fplatform%3Ddarwin%26architecture=x64'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.deezer.com/desktop/download%3Fplatform%3Ddarwin%26architecture=x64'
   name 'Deezer'
   homepage 'https://www.deezer.com/download'
 
