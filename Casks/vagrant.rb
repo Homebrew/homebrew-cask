687a8a9145b055886413ed8edbb5632ee0830c8e
Metin BEKTAS <30674934+methbkts@users.noreply.github.com>
2019-03-01 01:01:33 +0100

    Update vagrant to 2.2.4 (#59640)

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,6 +1,6 @@
 cask 'vagrant' do
-  version '2.2.3'
-  sha256 'd6f68ed8d69d6465e13ab668542bacc9a1752cf60f47464fa46604c147c81be4'
+  version '2.2.4'
+  sha256 'a09a2f0fb3fcbd4cfb41c7812c01605173f64056449c3dbcc513b4211e9c1e8d'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
