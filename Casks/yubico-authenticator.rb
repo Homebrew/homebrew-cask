90deefc37c5b3b6c65ed7181272c0853b9cddc4d
Benjamin Julian <benjermanjoel@outlook.com>
2019-03-01 08:25:08 -0600

    Add Yubico Authenticator.app v.4.3.5 Rev.1

--- Casks/yubico-authenticator.rb
+++ Casks/yubico-authenticator.rb
@@ -2,11 +2,11 @@ cask 'yubico-authenticator' do
   version '4.3.5'
   sha256 'e9e668456e196ce8c04942111fc0b8cfad53ffc2f5165b3eff0e872d825b8d0f'
 
-  url 'https://developers.yubico.com/yubioath-desktop/Releases/yubioath-desktop-4.3.5-mac.pkg'
+  url 'https://developers.yubico.com/yubioath-desktop/Releases/yubioath-desktop-#{version}-mac.pkg'
   name 'Yubico Authenticator'
-  homepage 'https://www.yubico.com/products/services-software/download/yubico-authenticator/'
+  homepage 'https://developers.yubico.com/yubioath-desktop/'
 
-  pkg 'yubioath-desktop-4.3.5-mac.pkg'
+  pkg 'yubioath-desktop-#{version}-mac.pkg'
 
   uninstall pkgutil: 'com.yubico.yubioath'
 end
