525ad939052a6e85129eea727aa269e4667551bc
Vítor Galvão <info@vitorgalvao.com>
2019-03-01 13:58:28 +0000

    Casks/tableau-public.rb: changed appcast from corecode to macupdater

--- Casks/tableau-public.rb
+++ Casks/tableau-public.rb
@@ -3,7 +3,7 @@ cask 'tableau-public' do
   sha256 '702a2548a3da20197a6a0ab98a07e53ef7652846f81e475f8347b6cda20bf095'
 
   url "https://downloads.tableau.com/public/TableauPublic-#{version.dots_to_hyphens}.dmg"
-  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/public/mac'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/public/mac'
   name 'Tableau Public'
   homepage 'https://public.tableau.com/s/'
 
