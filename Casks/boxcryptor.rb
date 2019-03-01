5affc02b096df69a5a8b2ca7a442ba267531f68c
Vítor Galvão <info@vitorgalvao.com>
2019-03-01 13:58:27 +0000

    Casks/boxcryptor.rb: changed appcast from corecode to macupdater

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -3,7 +3,7 @@ cask 'boxcryptor' do
   sha256 'd2f94dec6f14f3088de9bc146d4f220dae2aee604b325dfbdaa53e6be70e79ff'
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
-  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_redirect.cgi?url=http://www.boxcryptor.com/l/download-macosx'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://www.boxcryptor.com/l/download-macosx'
   name 'Boxcryptor'
   homepage 'https://www.boxcryptor.com/en/'
 
