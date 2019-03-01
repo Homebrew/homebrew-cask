487056405fafd815ede2dda62583333fa67890e5
Vítor Galvão <info@vitorgalvao.com>
2019-03-01 13:58:28 +0000

    Casks/ocenaudio.rb: changed appcast from corecode to macupdater

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -3,7 +3,7 @@ cask 'ocenaudio' do
   sha256 'cd173c3fff41e65b162a594d0f99d6f4d287f0ca5cd61d0a11318cb3591ae85b'
 
   url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
-  appcast 'https://www.corecode.io/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
   name 'ocenaudio'
   homepage 'https://www.ocenaudio.com/en'
 
