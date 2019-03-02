65ca45ef325ecb040667530796cd5ffee8554015
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-02 15:05:39 -0800

    Update edex-ui to 2.1.0 (#59707)

--- Casks/edex-ui.rb
+++ Casks/edex-ui.rb
@@ -1,6 +1,6 @@
 cask 'edex-ui' do
-  version '2.0.1'
-  sha256 'ee4819d7543a2bc1ba60aa8b1252e3a6f0fae2a08580b4cb596c31ca008783c3'
+  version '2.1.0'
+  sha256 '41b737b90cf8fb45f083f506151cdc1ae2bfa224dc37f8ea17e1b11c8780e77a'
 
   url "https://github.com/GitSquared/edex-ui/releases/download/v#{version}/eDEX-UI.MacOS.Image.dmg"
   appcast 'https://github.com/GitSquared/edex-ui/releases.atom'
