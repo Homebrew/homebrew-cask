a9236acb37ad3bbd8ad09f36e04165e33282d467
nerolite <martisst@stud.ntnu.no>
2019-03-04 23:12:17 +0100

    Updated version
    Changed the version number so it is up to date. Updated from version '5.3.3' to version '5.3.5'.

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,7 +1,7 @@
 cask 'steermouse' do
-  version '5.3.3'
+  version '5.3.5'
   sha256 'a7018ee2549ab2f543456f8847811f495c911acb2ebaaa447900a536c2d0d382'
-
+  
   url "https://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
   appcast 'https://plentycom.jp/en/steermouse/download.php'
   name 'SteerMouse'
