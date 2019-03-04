8236babe06c46d7d28729f4a2d1c496d8db62cda
brigitte zmoelnig <brigitte@corecode.io>
2019-03-04 10:24:20 +0100

    Update ogdesign-eagle to 1.9.0,3

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,6 +1,6 @@
 cask 'ogdesign-eagle' do
-  version '1.8.2,build5'
-  sha256 '3f93ede904a1b88167f32738bacad65d67eaa852f45c47045d92ac9d362b5814'
+  version '1.9.0,3'
+  sha256 'b17c3195c46b2de7cb5cc79e23a109ee22529c15eae3ee96e3a50eb8f9f1c5b4'
 
   # eagleapp.s3-accelerate.amazonaws.com was verified as official when first introduced to the cask
   url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-#{version.after_comma}.dmg?download"
