89cede38b22ecf575574afc950c2fbd95492c5d4
brigitte zmoelnig <brigitte@corecode.io>
2019-03-04 10:54:16 +0100

    Update lepton to 1.7.3

--- Casks/lepton.rb
+++ Casks/lepton.rb
@@ -1,6 +1,6 @@
 cask 'lepton' do
-  version '1.7.0'
-  sha256 'be78582f86b5b77ff9a2ffc2cd23d736c2625d2333f79e80b70eaaca6804cc5a'
+  version '1.7.3'
+  sha256 '60754e75990ea312f1cec036171a610e37ef5ddc48cb38761924894780ab8929'
 
   # github.com/hackjutsu/Lepton was verified as official when first introduced to the cask
   url "https://github.com/hackjutsu/Lepton/releases/download/v#{version}/Lepton-#{version}-mac.zip"
