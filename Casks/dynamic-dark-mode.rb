e90f582f218ca8cd62d36c67bbb84f4cd279d192
suschizu <brigitte@corecode.io>
2019-03-04 15:30:37 +0100

    Update dynamic-dark-mode to 1.1.4 (#59724)
    * Update dynamic-dark-mode to 1.1.4
    
    * Update dynamic-dark-mode.rb
    
    * Update dynamic-dark-mode.rb
    
    * Update dynamic-dark-mode.rb
    
    * Update dynamic-dark-mode.rb

--- Casks/dynamic-dark-mode.rb
+++ Casks/dynamic-dark-mode.rb
@@ -1,13 +1,15 @@
 cask 'dynamic-dark-mode' do
-  version '1.1.3'
-  sha256 'a2363c8038a8babf30186c06674749a09de998dcb644030ecb68894847469b4e'
+  version '1.1.4'
+  sha256 '3101ef4222843c41a48122abef1f5e97501a67d8664c20a61a9397abeee1bdca'
 
-  url "https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases/download/#{version}/Dynamic_Dark_Mode-#{version}.zip"
+  url "https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases/download/#{version}/Dynamic_Dark_Mode-#{version}.pkg"
   appcast 'https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases.atom'
   name 'Dynamic Dark Mode'
   homepage 'https://github.com/ApolloZhu/Dynamic-Dark-Mode'
 
   depends_on macos: '>= :mojave'
 
-  app 'Dynamic Dark Mode.app'
+  pkg "Dynamic_Dark_Mode-#{version}.pkg"
+
+  uninstall pkgutil: 'io.github.apollozhu.Dynamic.pkg'
 end
