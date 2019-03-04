72e1d4798909b6db7de81f8fdca8248fde813773
Ryan R. Smith <rsmith@fullcompass.com>
2019-03-04 10:28:33 -0600

    Adding comment to verify stale url for welly.rb.

--- Casks/welly.rb
+++ Casks/welly.rb
@@ -2,6 +2,7 @@ cask 'welly' do
   version '2.7'
   sha256 'cb24a26432d8927b1159a1865602c3f30b5190f628167c954e4d6cc723cfcb0f'
 
+  # storage.googleapis.com was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/welly/Welly.v#{version}.fix.zip"
   appcast 'https://csie.io/welly/update.xml'
   name 'Welly'
