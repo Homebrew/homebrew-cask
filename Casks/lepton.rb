01dc281aec36fcda6926bd83c87d88211083ef59
suschizu <brigitte@corecode.io>
2019-03-05 14:10:23 +0100

    Update lepton.rb

--- Casks/lepton.rb
+++ Casks/lepton.rb
@@ -1,19 +1,12 @@
 cask 'lepton' do
   version '1.7.3'
-  sha256 '60754e75990ea312f1cec036171a610e37ef5ddc48cb38761924894780ab8929'
+  sha256 '64488f239d325b50b7bb601832f286cdc7223edf31d0e8b667b2cd72fee733e1'
 
   # github.com/hackjutsu/Lepton was verified as official when first introduced to the cask
-  url "https://github.com/hackjutsu/Lepton/releases/download/v#{version}/Lepton-#{version}-mac.zip"
+  url "https://github.com/hackjutsu/Lepton/releases/download/v#{version}/Lepton-#{version}.dmg"
   appcast 'https://github.com/hackjutsu/Lepton/releases.atom'
   name 'Lepton'
   homepage 'https://hackjutsu.com/Lepton/'
 
   app 'Lepton.app'
-
-  zap trash: [
-               '~/Library/Application Support/Lepton',
-               '~/Library/Preferences/com.cosmox.lepton.helper.plist',
-               '~/Library/Preferences/com.cosmox.lepton.plist',
-               '~/Library/Saved Application State/com.cosmox.lepton.savedState',
-             ]
 end
