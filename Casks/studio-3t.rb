5016bfcd38bd16a5c123297edbb65647520e5b0a
RJ Hsiao <rj@rjhsiao.me>
2019-03-06 00:17:23 +0800

    Update studio-3t to 2019.1.0 (#59816)

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask 'studio-3t' do
-  version '2018.6.1'
-  sha256 '6a6550d1240a80961ad4976e69bc54397ba4249617df64fd89ef05a9f6a23249'
+  version '2019.1.0'
+  sha256 'a5ad2439faf84f466bda1f86b366528d90621b9d5fdb16db6f0acd3b3697dfeb'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'https://files.studio3t.com/changelog/changelog.txt'
