19ed090b9d8d2e31858528379c5496cdaf533950
brigitte zmoelnig <brigitte@corecode.io>
2019-03-05 20:06:04 +0100

    Update fips to 3.3.1

--- Casks/fips.rb
+++ Casks/fips.rb
@@ -1,6 +1,6 @@
 cask 'fips' do
-  version '3.3.0'
-  sha256 '4bba26c41e48aef7d3cc760626b559c3a174672dd4e9231c41232012119827a8'
+  version '3.3.1'
+  sha256 '630ee2776117876d49d8073bb6715c9a7c7d927bc2745fca6ce5db2ab9c1456e'
 
   # github.com/matwey/fips3 was verified as official when first introduced to the cask
   url "https://github.com/matwey/fips3/releases/download/#{version}/Fips-#{version}-Darwin.dmg"
