c6b6e02e591a8051c89af6bae2976e2d907cc538
saka <876038559@qq.com>
2019-03-05 22:49:57 +0800

    pp

---
+++ Casks/ssokit.rb
@@ -0,0 +1,11 @@
+cask 'ssokit' do
+  version '1.0.0'
+  sha256 ''
+
+  url 'https://raw.githubusercontent.com/rangaofei/SSokit-qmake/master/packages/v1.0.0/SSokit.zip'
+  name 'ssokit'
+  homepage 'https://github.com/rangaofei/SSokit-qmake'
+
+  app 'ssokit'
+end
+
