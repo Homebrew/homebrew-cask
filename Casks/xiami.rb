0214245a602cc1984249087c3b352989628964b6
Junmo Liang <liangjunmo@163.com>
2019-03-05 15:33:33 +0800

    Update xiami to 7.5.2,533cc9cd574d876040c4580b0ce82eb7

--- Casks/xiami.rb
+++ Casks/xiami.rb
@@ -1,6 +1,6 @@
 cask 'xiami' do
-  version '7.5.0,e6f5fc55f5489bc4da7c18ffd0a86b40'
-  sha256 'f344fd5dbb54cbda4de3f832b03d33c7447c3efd8971ace06d14e5f092c07b7f'
+  version '7.5.2,533cc9cd574d876040c4580b0ce82eb7'
+  sha256 '856ac855a59d0e12e06accde2346ec21539b90d057f9fc113f9db3f19edac8bb'
 
   url "https://files.xiami.com/webh5/files/xiamiMac/#{version.after_comma}.xiamimac#{version.before_comma.dots_to_hyphens}.zip"
   appcast 'https://g.alicdn.com/music/desktop-app/XiamiMac.xml'
