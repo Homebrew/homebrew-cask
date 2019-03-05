da92463410b51ae6d0637de7900875767e17c261
saka <876038559@qq.com>
2019-03-05 23:01:34 +0800

    comm

--- Casks/ssokit.rb
+++ Casks/ssokit.rb
@@ -1,6 +1,6 @@
 cask 'ssokit' do
   version '1.0.0'
-  sha256 ''
+  sha256 '008037d3177ddacd6c35c967598581a2981003fe9a29b0db363722a3bdd1269e'
 
   url 'http://img.rangaofei.cn/SSokit-1.0.0.zip'
   name 'ssokit'
