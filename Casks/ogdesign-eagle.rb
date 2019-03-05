b529851b885baddb003a2a51e035a0f3872f67db
Snowail <snowail@outlook.com>
2019-03-05 22:06:16 +0800

    Update ogdesign-eagle to 1.9.0,4

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,6 +1,6 @@
 cask 'ogdesign-eagle' do
-  version '1.9.0,3'
-  sha256 'b17c3195c46b2de7cb5cc79e23a109ee22529c15eae3ee96e3a50eb8f9f1c5b4'
+  version '1.9.0,4'
+  sha256 '31e86b222a4d3343b4dc29df3d1ef3032d876eca654f4efce3a709c23b8007ec'
 
   # eagleapp.s3-accelerate.amazonaws.com was verified as official when first introduced to the cask
   url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-#{version.after_comma}.dmg?download"
