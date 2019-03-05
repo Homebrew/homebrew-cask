b1113570a6f965d9f5fce0d37d1c26e4abd2c306
suschizu <brigitte@corecode.io>
2019-03-05 14:10:45 +0100

    Update enpass to 6.0.6,367 (#59809)

--- Casks/enpass.rb
+++ Casks/enpass.rb
@@ -1,6 +1,6 @@
 cask 'enpass' do
-  version '6.0.5,333'
-  sha256 'b0b7aee6609730ec593f5dc70f60b81db72a7bc7016d56a8c210ca170274b046'
+  version '6.0.6,367'
+  sha256 '176ce6a6160a01f69744aae7ba9d63b88d349d6c69fd68f6918be5be77aeaa84'
 
   url "https://dl.enpass.io/stable/mac/package/#{version.after_comma}/Enpass.pkg"
   appcast 'https://dl.sinew.in/mac/package/appcast.xml'
