29af4d77c6ab6a2543e1a6347aa9eb284ae7cc8e
suschizu <brigitte@corecode.io>
2019-03-05 14:11:16 +0100

    Update second-life-viewer to 6.1.0.524670 (#59808)

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,6 +1,6 @@
 cask 'second-life-viewer' do
-  version '6.0.1.522263'
-  sha256 'c2dd672771dbe1761cbecd551c0c8ba8747c88a88f15fb935bdb0a000691be22'
+  version '6.1.0.524670'
+  sha256 '35892ed3f8f152f428125216ea5b185c6d8512d415d49262ea42f1ec25551d8d'
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
   appcast 'https://secondlife.com/support/downloads/'
