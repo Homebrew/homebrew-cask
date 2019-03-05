c3aa72a30b5f08a2ace900af26783eaa960b4b0a
suschizu <brigitte@corecode.io>
2019-03-05 14:14:53 +0100

    Update blue-jeans to 2.11.0.405 (#59810)

--- Casks/blue-jeans.rb
+++ Casks/blue-jeans.rb
@@ -1,6 +1,6 @@
 cask 'blue-jeans' do
-  version '2.11.0.399'
-  sha256 '854c38d1bee5ce44d6e88bfa600f4d275524cab129fd8d13f0477a20d3a72903'
+  version '2.11.0.405'
+  sha256 '200b14601ce11b28f4b4e84a542a1c87ae08af974566c2572c00b2891d6a047d'
 
   url "https://swdl.bluejeans.com/desktop-app/mac/#{version.major_minor_patch}/#{version}/BlueJeansInstaller.dmg"
   appcast 'https://www.bluejeans.com/downloads'
