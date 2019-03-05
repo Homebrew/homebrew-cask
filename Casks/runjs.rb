5d9d60455a5cae64eaebc6d28b5c39b21982496e
suschizu <brigitte@corecode.io>
2019-03-05 13:50:15 +0100

    Update runjs to 1.3.1 (#59801)

--- Casks/runjs.rb
+++ Casks/runjs.rb
@@ -1,6 +1,6 @@
 cask 'runjs' do
-  version '1.3.0'
-  sha256 '4a8d72795afbe3ba2781ffcf3c575922f0ec146080d747f6bc496a93974652b3'
+  version '1.3.1'
+  sha256 'e88d91e0e9e4a2dd9056b614cebffefb3f1becf5cea79affa79b58aa662a4990'
 
   # github.com/lukehaas/runjs was verified as official when first introduced to the cask
   url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}.dmg"
