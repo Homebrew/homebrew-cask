bd038aaec52523fd8279d08197bf75f703df5c77
jcbot <jcb@leipert.io>
2019-03-05 14:34:40 +0000

    Update mps to 2018.3.5,183.1735

--- Casks/mps.rb
+++ Casks/mps.rb
@@ -1,6 +1,6 @@
 cask 'mps' do
-  version '2018.3.4,183.1686'
-  sha256 '3859c6dc74f77cb237a360d9760c8ad8ff3b15920bba91e0ddc721ab4a9b3171'
+  version '2018.3.5,183.1735'
+  sha256 'a1377668dd3306fcda32bef362eb909a5b6c603b29b473dc5ff51a0d8df23bb5'
 
   url "https://download.jetbrains.com/mps/#{version.before_comma.major_minor}/MPS-#{version.before_comma}-macos-jdk-bundled.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=MPS&latest=true&type=release'
