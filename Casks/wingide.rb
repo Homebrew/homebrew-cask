42826eea6a74c57629c3f44470e0016b37942748
suschizu <brigitte@corecode.io>
2019-03-05 21:06:35 +0100

    Update wingide to 6.1.5-1 (#59833)

--- Casks/wingide.rb
+++ Casks/wingide.rb
@@ -1,6 +1,6 @@
 cask 'wingide' do
-  version '6.1.4-1'
-  sha256 '7fd388da07bf506579bb038b18487c415ce5e5d181e2770ba11a2f5fedb98cad'
+  version '6.1.5-1'
+  sha256 '755d9a50e4ac2f70c7cc8ba763adfa510805a9cd51876781a0e72587129df3fb'
 
   url "https://wingware.com/pub/wingide/#{version.sub(%r{-\d+}, '')}/wingide-#{version}.dmg"
   name 'WingIDE'
