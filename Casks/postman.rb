d99f53014c2f2aa13137b990b3f96246efe3d87b
Vítor Galvão <info@vitorgalvao.com>
2019-03-06 14:43:52 +0000

    postman.rb: back to 6.7.4

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,10 +1,10 @@
 cask 'postman' do
-  version '7.0.4'
-  sha256 '781841a49e11757ab4d1fbe0db627e323e3d5c32aa09ec53db001e3430bf8738'
+  version '6.7.4'
+  sha256 'f0ac7764e70eb8e9fd27efb82f69ba8e41b9cb7a3767e1db887b439dff09a029'
 
   # dl.pstmn.io/download/version was verified as official when first introduced to the cask
   url "https://dl.pstmn.io/download/version/#{version}/osx64"
-  appcast 'https://dl.pstmn.io/update/status?channel=stable&currentVersion=6.7.4&arch=64&platform=osx&syncEnabled=true&teamPlan='
+  appcast "https://dl.pstmn.io/update/status?channel=stable&currentVersion=#{version}&arch=64&platform=osx&syncEnabled=true&teamPlan="
   name 'Postman'
   homepage 'https://www.getpostman.com/'
 
