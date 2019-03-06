ea9fa3390eeb063ef62c8da051b59688f6d7c6aa
Pedro Carrasco <pedrommcarrasco@gmail.com>
2019-03-06 14:10:02 +0000

    Updated Brooklyn to 2.0.1

--- Casks/brooklyn.rb
+++ Casks/brooklyn.rb
@@ -1,6 +1,6 @@
 cask 'brooklyn' do
-  version '2.0.0'
-  sha256 'fd2dbc0bfdc5d12db202d7e6c7eb0a11e822e669ac1edbf12d205f2d91e9f088'
+  version '2.0.1'
+  sha256 '6ecde466f8f88c2649dbf3e38c98fd7abb95a7d1828ac4cc8d499f2c7a0cb9ec'
 
   url "https://github.com/pedrommcarrasco/Brooklyn/releases/download/#{version}/Brooklyn.saver.zip"
   appcast 'https://github.com/pedrommcarrasco/Brooklyn/releases.atom'
