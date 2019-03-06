895d2076314129528d8321f1be8e5ee65b09f051
Julian Mayer <julian@corecode.io>
2019-03-06 08:45:52 -0800

    Update dash-dash to 0.13.1.0

--- Casks/dash-dash.rb
+++ Casks/dash-dash.rb
@@ -1,9 +1,9 @@
 cask 'dash-dash' do
-  version '0.13.0.0'
-  sha256 '6f97f502732e5b63a431d0edb5a9d14e95ff8afb8e7eb94463566a75e7589a70'
+  version '0.13.1.0'
+  sha256 '8b89be3f47a45d86fda1015f07bac4dbfeab72b1636ee6837de467fe1dfe960d'
 
   # github.com/dashpay/dash was verified as official when first introduced to the cask
-  url "https://github.com/dashpay/dash/releases/download/v#{version}/dashcore-#{version}-osx-unsigned.dmg"
+  url "https://github.com/dashpay/dash/releases/download/v#{version}/dashcore-#{version}-osx.dmg"
   appcast 'https://github.com/dashpay/dash/releases.atom'
   name 'Dash'
   homepage 'https://www.dash.org/'
