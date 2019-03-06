86204a1eed4ae0168e677e51c4c331db0022e0f1
saka <876038559@qq.com>
2019-03-06 18:38:55 +0800

    Add SSokit.app

--- Casks/ssokit.rb
+++ Casks/ssokit.rb
@@ -1,11 +1,11 @@
 cask 'ssokit' do
   version '1.0.0'
-  sha256 '008037d3177ddacd6c35c967598581a2981003fe9a29b0db363722a3bdd1269e'
+  sha256 '47781359131db65e8d8dd11086ca5e55da19105a92b956275b69d430a48d78cb'
 
-  url 'http://img.rangaofei.cn/SSokit-1.0.0.zip'
-  name 'ssokit'
+  url 'http://img.rangaofei.cn/SSokit.dmg'
+  name 'SSokit'
   homepage 'https://github.com/rangaofei/SSokit-qmake'
 
-  app 'ssokit'
+  app 'SSokit.app'
 end
 
