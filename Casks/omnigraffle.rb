fa0f8455be17b1240d7578c4d0e6862b12943232
Pascal Jürgens <trifle@users.noreply.github.com>
2019-03-06 12:12:30 +0100

    push omnigraffle to version 7.10 (#59841)

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -4,8 +4,8 @@ cask 'omnigraffle' do
     sha256 'ab463ea6c12d49c4104d3814ac3280d0359072702d4751f5074f644fc79de0c6'
     url "https://downloads.omnigroup.com/software/Archive/MacOSX/10.12/OmniGraffle-#{version}.dmg"
   else
-    version '7.9.4'
-    sha256 '7b850a62507f25127fb9cd41a5bc658c8ca1c58398659e46a8bac499bbeaf106'
+    version '7.10'
+    sha256 '6b29a3904f7bb2addcbd28e10f60492a85ab2d75c5077bc96fa11151a3a4c63c'
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniGraffle-#{version}.dmg"
   end
 
