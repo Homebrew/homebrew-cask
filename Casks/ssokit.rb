d2f41323a2d1b724f05cd2b1f12496b4489aa3df
saka <876038559@qq.com>
2019-03-06 22:31:44 +0800

    Add SSokit.app and switch url to github

--- Casks/ssokit.rb
+++ Casks/ssokit.rb
@@ -1,11 +1,10 @@
 cask 'ssokit' do
   version '1.0.0'
   sha256 '47781359131db65e8d8dd11086ca5e55da19105a92b956275b69d430a48d78cb'
-
-  url 'http://img.rangaofei.cn/SSokit.dmg'
+  # github.com/rangaofei/SSokit-qmake was verified as official when first introduced to the cask
+  url "https://raw.githubusercontent.com/rangaofei/SSokit-qmake/master/packages/v#{version}/SSokit.dmg"
   name 'SSokit'
   homepage 'https://github.com/rangaofei/SSokit-qmake'
 
   app 'SSokit.app'
 end
-
