bb136d4a31b4ea7e4663dae68243e65a5b698280
Christian <cmbernard333@gmail.com>
2019-03-06 08:54:59 -0700

    Use github actions to generate cask json (#59521)
    Co-Authored-By: cmbernard333 <cmbernard333@gmail.com>

---
+++ .github/main.workflow.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# silence bundler complaining about being root
+mkdir ~/.bundle
+echo 'BUNDLE_SILENCE_ROOT_WARNING: "1"' > ~/.bundle/config
+
+# configure git
+git config --global user.name "BrewTestBot"
+git config --global user.email "homebrew-test-bot@lists.sfconservancy.org"
+
+CASK_DIR="$(brew --repo "$GITHUB_REPOSITORY")"
+mkdir -p "$CASK_DIR"
+rm -rf "$CASK_DIR"
+ln -s "$PWD" "$CASK_DIR"
+
+# setup Homebrew environment
+export PATH="$(brew --repo)/Library/Homebrew/vendor/portable-ruby/current/bin:$PATH"
+
+# setup SSH
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+echo "$FORMULAE_DEPLOY_KEY" > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+git config --global core.sshCommand "ssh -i ~/.ssh/id_ed25519 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+# clone formulae.brew.sh with SSH so we can push back
+git clone git@github.com:Homebrew/formulae.brew.sh
+cd formulae.brew.sh
+
+# setup analytics
+echo "$ANALYTICS_JSON_KEY" > ~/.homebrew_analytics.json
+
+# run rake (without a rake binary)
+ruby -e "load Gem.bin_path('rake', 'rake')" cask
+
+# commit and push generated files
+git commit -m '_data/cask: update from Homebrew/cask push' _data/cask
+git push
