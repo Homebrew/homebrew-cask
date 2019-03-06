72db0eccc8db5696530faa87719d1318b59595c9
brigitte zmoelnig <brigitte@corecode.io>
2019-03-06 15:38:35 +0100

    Update chromium to 638094

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask 'chromium' do
-  version '637735'
-  sha256 'df65972b1b61da82b757149923a95b78303c877e2bf4a144be4f0be0736cba0f'
+  version '638094'
+  sha256 '1a0f9f19cf4e89ea7a6111d50a58556113d2b713ce5c3626b13837c1a1332608'
 
   # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac was verified as official when first introduced to the cask
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
