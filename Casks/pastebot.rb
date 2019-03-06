b98ca705fd85c59460741df72c20aa89b0710723
Nathan Toone <nathan@toonetown.com>
2019-03-06 07:02:31 -0700

    Add Pastebot 2.2.1

---
+++ Casks/pastebot.rb
@@ -0,0 +1,16 @@
+cask 'pastebot' do
+  version '2.2.1'
+  sha256 '13842407492c88b9ff75e5c2a1172e9f4c5a9830fb241c5143469032db9249a8'
+
+  # tapbots.net/pastebot was verified as official when first introduced to the cask
+  url "https://tapbots.net/pastebot#{version.major}/Pastebot.dmg"
+  name 'Pastebot'
+  homepage 'https://tapbots.com/pastebot/'
+
+  app 'Pastebot.app'
+
+  zap delete: [
+                "~/Library/Containers/com.tapbots.Pastebot#{version.major}Mac",
+                "~/Library/Preferences/com.tapbots.Pastebot#{version.major}Mac.plist",
+              ]
+end
