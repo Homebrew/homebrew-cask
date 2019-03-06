01b1d27b7c9826dcbd60ef378eae44ed749e1461
Udo Kramer <optik@fluffel.io>
2019-03-06 01:06:16 +0100

    Update volt to 0.76 (#59793)

--- Casks/volt.rb
+++ Casks/volt.rb
@@ -1,12 +1,12 @@
 cask 'volt' do
-  version '0.70'
-  sha256 '43bfff76cc0898c8cf416ec26cccfdc64e4b9a2f71660d5e96b4073aaf874e97'
+  version '0.76'
+  sha256 'ab050199b52cb59f6dfcdcaac53bba4b13936481b4e3f9d69e64a5562c498cdd'
 
   # github.com/voltapp/volt was verified as official when first introduced to the cask
   url "https://github.com/voltapp/volt/releases/download/#{version}/Volt.dmg"
   appcast 'https://github.com/voltapp/volt/releases.atom'
   name 'Volt'
-  homepage 'https://volt.ws/'
+  homepage 'https://volt-app.com/'
 
   app 'Volt.app'
 end
