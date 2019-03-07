df57bbe80a798c5f0dd841c8022eae3b85634892
GyuYong Jung <obliviscence@gmail.com>
2019-03-07 21:44:54 +0900

    Update quassel-client to 0.13.1

--- Casks/quassel-client.rb
+++ Casks/quassel-client.rb
@@ -1,6 +1,6 @@
 cask 'quassel-client' do
-  version '0.13.0'
-  sha256 'c81bb026f103395d087706bd79887bf385ca511125f9d60af083bd218d9e2da0'
+  version '0.13.1'
+  sha256 '2ebfc69a0a5da2554080b6b3d6d27b577c8d5a591253549b5960788741d7aece'
 
   url "https://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_#{version}.dmg"
   name 'Quassel IRC'
