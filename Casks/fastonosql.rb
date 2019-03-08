53f545772ec6d38bf3b3ca93a2f8b3992736afb2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-08 08:59:36 -0800

    Update fastonosql to 2.0.1 (#59974)

--- Casks/fastonosql.rb
+++ Casks/fastonosql.rb
@@ -1,6 +1,6 @@
 cask 'fastonosql' do
-  version '2.0.0'
-  sha256 '21d90d54259a831b1c4321ca18ce5b82daa2031b4267a32e3081cac0dd1e815b'
+  version '2.0.1'
+  sha256 '360e7ccafb4d1b4bb05db8c19dc0f466679f1e5defb34e899d414c8da66ec31f'
 
   url "https://fastonosql.com/downloads_pro/macosx/fastonosql_pro-#{version}-x86_64.dmg"
   appcast 'https://github.com/fastogt/fastonosql/releases.atom'
