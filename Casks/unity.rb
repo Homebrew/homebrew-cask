6c800098f7dee5d6d7b8eecb48aed5ac7a0426ac
suschizu <brigitte@corecode.io>
2019-03-08 16:07:34 +0100

    Update unity to 2018.3.7f1,9e14d22a41bb (#59950)

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask 'unity' do
-  version '2018.3.6f1,a220877bc173'
-  sha256 '22ef6691098b5b9bc48df062c403538eb6dacb749e966424a97ed1fb6b4976ae'
+  version '2018.3.7f1,9e14d22a41bb'
+  sha256 'd5ec4df6a8699e812d0f7b7f32881917f3cfb388e128952e502239a0ed3ccab8'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/download/archive'
