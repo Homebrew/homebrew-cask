e5be22440ad15a3d87012797f373abf0f8d8f2b5
Andrey Sokolov <artas90@gmail.com>
2019-03-08 21:04:16 +0200

    Fix tinymediamanager download url (#59992)
    * Update tinymediamanager download url
    
    * Update tinymediamanager.rb
    
    * Update tinymediamanager.rb
    
    * Update tinymediamanager.rb

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -2,8 +2,8 @@ cask 'tinymediamanager' do
   version '2.9.16_42413ce'
   sha256 'd248355ff375bcdc5f830535b567630b78f2d214d1bc5c4d0945d2e1b356ac42'
 
-  url "http://release.tinymediamanager.org/dist/tmm_#{version}_mac.zip"
-  appcast 'http://release.tinymediamanager.org/'
+  url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
+  appcast 'https://release.tinymediamanager.org/'
   name 'tinyMediaManager'
   homepage 'https://www.tinymediamanager.org/'
 
