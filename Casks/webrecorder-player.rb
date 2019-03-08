5eeb7d70cbed88661ba995a5f72db8e9021f8a4d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-08 05:06:56 -0800

    Update webrecorder-player to 1.6.4 (#59949)

--- Casks/webrecorder-player.rb
+++ Casks/webrecorder-player.rb
@@ -1,6 +1,6 @@
 cask 'webrecorder-player' do
-  version '1.6.3'
-  sha256 '142a2c897246d84e9fa837691b1ab2ec15b3f2cd603aaa7a5f522ee440fa9bce'
+  version '1.6.4'
+  sha256 '11ccbe2040d53c673c38a08f6b13acec3a36c9ad0a18c3a51b86987219533dcc'
 
   url "https://github.com/webrecorder/webrecorder-player/releases/download/v#{version}/webrecorder-player-#{version}.dmg"
   appcast 'https://github.com/webrecorder/webrecorderplayer-electron/releases.atom'
