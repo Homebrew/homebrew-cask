5ce815342c76e3517efbfd6d21dbb3030e137440
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-08 09:37:37 -0800

    Update bitwig-studio to 2.5 (#59986)

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask 'bitwig-studio' do
-  version '2.4.3'
-  sha256 '7a3fd1ff984fc94964fe883b1b02442403d2385d18b2072ebf7d69be0ef4d48a'
+  version '2.5'
+  sha256 'b23834a869db96f2b82cc950c42e0cdfbed490f00cd19cf1d2d3d5e4864ae48b'
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   name 'Bitwig Studio'
