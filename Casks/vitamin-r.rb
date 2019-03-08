998cc4c6f92d68ede4839d6a8bac6ac06b25c40e
Randall <ran.dall@icloud.com>
2019-03-08 05:25:45 -0800

    Update vitamin-r to 3.05

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -3,8 +3,8 @@ cask 'vitamin-r' do
     version '2.58'
     sha256 'c6c631430b44359aa022d9ca5ca6e98dbdf7258f2ceae0353f344a035682661e'
   else
-    version '3.03'
-    sha256 'bd164d61e7605fdfca151fbfdb2446efc1247f4b1d3c75bdfd823c33a4e97535'
+    version '3.05'
+    sha256 '685de0390b0b80c517320a853c7e2f2c6a30e5c49c50ae0c342268cb9ebba632'
   end
 
   url "http://www.publicspace.net/download/signedVitamin#{version.major}.zip"
