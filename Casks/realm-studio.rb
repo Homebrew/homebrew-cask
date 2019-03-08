85149a898b06f0f2cef11fe6b14f45e61b437005
nuomi1 <nuomi1@qq.com>
2019-03-08 10:17:19 +0800

    Update realm-studio.rb for auto_updates and zap (#59918)
    * Update realm-studio.rb for auto_updates and zap
    
    * Update realm-studio.rb

--- Casks/realm-studio.rb
+++ Casks/realm-studio.rb
@@ -7,5 +7,17 @@ cask 'realm-studio' do
   name 'Realm Studio'
   homepage 'https://realm.io/products/realm-studio/'
 
+  auto_updates true
+
   app 'Realm Studio.app'
+
+  zap delete: [
+                '~/Library/Application Support/Realm Studio',
+                '~/Library/Caches/io.realm.realm-studio',
+                '~/Library/Caches/io.realm.realm-studio.ShipIt',
+                '~/Library/Logs/Realm Studio',
+                '~/Library/Preferences/io.realm.realm-studio.helper.plist',
+                '~/Library/Preferences/io.realm.realm-studio.plist',
+                '~/Library/Saved Application State/io.realm.realm-studio.savedState',
+              ]
 end
