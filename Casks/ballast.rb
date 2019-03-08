6603d0ce4c5403423f33d65dc635dc8bbdaf5274
Vítor Galvão <info@vitorgalvao.com>
2019-03-08 14:31:18 +0000

    Update ballast.rb

--- Casks/ballast.rb
+++ Casks/ballast.rb
@@ -1,9 +1,9 @@
 cask 'ballast' do
-  version 'v1.2.0'
+  version '1.2.0'
   sha256 '6976fb45da9648d4571e2cfefa6991acf53b85c4b3767887208130226c38d82c'
 
   # github.com/jamsinclair/ballast was verified as official when first introduced to the cask
-  url "https://github.com/jamsinclair/ballast/releases/download/#{version}/ballast-#{version}.zip"
+  url "https://github.com/jamsinclair/ballast/releases/download/v#{version}/ballast-v#{version}.zip"
   appcast 'https://github.com/jamsinclair/ballast/releases.atom'
   name 'ballast'
   homepage 'https://jamsinclair.nz/ballast'
