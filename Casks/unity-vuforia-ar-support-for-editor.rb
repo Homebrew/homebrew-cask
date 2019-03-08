89a0d3a0d09188e6ab86c4bfe8221da9bc3853d3
suschizu <brigitte@corecode.io>
2019-03-08 16:08:08 +0100

    Update unity-vuforia-ar-support-for-editor to 2018.3.7f1,9e14d22a41bb (#59952)

--- Casks/unity-vuforia-ar-support-for-editor.rb
+++ Casks/unity-vuforia-ar-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-vuforia-ar-support-for-editor' do
-  version '2018.3.6f1,a220877bc173'
-  sha256 '772adfa3631bd4011d33ee64b927905c0d1db9025d16a30152ad19381d460ee5'
+  version '2018.3.7f1,9e14d22a41bb'
+  sha256 'f0c8aea2cd8490c88d45e56de1433363ee57cab8e0082c08b3899b4acff23f00'
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Vuforia-AR-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
