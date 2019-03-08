fb403e4ffe5bb14774cf01a22665d5d9e6c4c02c
Randall <ran.dall@icloud.com>
2019-03-07 23:35:00 -0800

    Update mamp to 5.3

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,6 +1,6 @@
 cask 'mamp' do
-  version '5.2'
-  sha256 'f05ede012b8a5d0e7c9cf17fee0fa1eb5cd8131f3c703ed14ea347f25be11a28'
+  version '5.3'
+  sha256 '6ef0d21c06a850fd688867a33d949f0d15a32b40231205c2bd0871f5b0681722'
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
   appcast 'https://www.mamp.info/en/downloads/'
