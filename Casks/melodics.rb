36871c670590cc8921fd24a055c08de2104593c8
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-08 06:21:20 -0800

    Update melodics to 2.0.2714 (#59960)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask 'melodics' do
-  version '2.0.2694'
-  sha256 'aa787fe804204e0deec2ff2f43dba62fd0ef5f22aa0c28c1dd1bdf89764d901c'
+  version '2.0.2714'
+  sha256 '18ca95e79cc57c46c00924972e01f06fe641448cd7589c510ac7ca0dd6b3adfc'
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://api.melodics.com/download/osxupdatescastv#{version.major}.xml"
