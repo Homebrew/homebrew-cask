50fadf7d3eaa6ac9a761c5a66673c7aad66831b5
Vítor Galvão <info@vitorgalvao.com>
2019-03-08 03:12:59 +0000

    minecraftpe.rb: change referer to new line (#59881)

--- Casks/minecraftpe.rb
+++ Casks/minecraftpe.rb
@@ -3,7 +3,8 @@ cask 'minecraftpe' do
   sha256 '0aa5477a9b04d08dc18fbc873ed06e5947e739b26d5d338bc914bad324974f4c'
 
   # meedownloads.azureedge.net was verified as official when first introduced to the cask
-  url "https://meedownloads.azureedge.net/#{version}/Minecraft_Education_Edition.dmg", referer: 'https://aka.ms/meeclientmacos'
+  url "https://meedownloads.azureedge.net/#{version}/Minecraft_Education_Edition.dmg",
+      referer: 'https://aka.ms/meeclientmacos'
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://aka.ms/meeclientmacos'
   name 'Minecraft Education Edition'
   homepage 'https://education.minecraft.net/'
