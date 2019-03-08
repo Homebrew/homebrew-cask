fe9b6d82ee47e8190c055795a55ea97572849153
Randall <ran.dall@icloud.com>
2019-03-08 04:59:46 -0800

    Update acorn to 6.3.2

--- Casks/acorn.rb
+++ Casks/acorn.rb
@@ -1,6 +1,6 @@
 cask 'acorn' do
-  version '6.3.1'
-  sha256 '8bd173dd9e403205d08f7b5f92db51d9aacc0206f56c5bf3da108f97d761f457'
+  version '6.3.2'
+  sha256 '54a50ea516da3f64c01d2d5a5d462977c14202e43ae286186901fb65c5ed42f0'
 
   url 'https://flyingmeat.com/download/Acorn.zip'
   appcast "https://www.flyingmeat.com/download/acorn#{version.major}update.xml"
