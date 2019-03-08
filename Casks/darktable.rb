b59e824965a4f3140a2316ac7b2243071c91f359
Leonardo Medici <leonardo_medici@me.com>
2019-03-08 07:59:47 +0100

    Update darktable to 2.6.1

--- Casks/darktable.rb
+++ Casks/darktable.rb
@@ -1,6 +1,6 @@
 cask 'darktable' do
-  version '2.6.0'
-  sha256 '47d6f1d9ae64394663ffd804692d6b6d6722b312712e938f103cb43f58625c42'
+  version '2.6.1'
+  sha256 'dec8ebd36b9747a84ffad119ca1fa92a197297dc7d54e9a03035da99a76f66e8'
 
   # github.com/darktable-org/darktable was verified as official when first introduced to the cask
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version.major_minor_patch}/darktable-#{version}.dmg"
