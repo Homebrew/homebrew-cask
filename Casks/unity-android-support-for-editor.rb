70b8c8ee2e1810f88a6f4ced6b6be26e74d1c0dc
suschizu <brigitte@corecode.io>
2019-03-08 16:07:46 +0100

    Update unity-android-support-for-editor to 2018.3.7f1,9e14d22a41bb (#59957)

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-android-support-for-editor' do
-  version '2018.3.6f1,a220877bc173'
-  sha256 '581864ad8fdba2b5d3bb6b00da9abf85fbee674969702e8e0691ac1d22530db4'
+  version '2018.3.7f1,9e14d22a41bb'
+  sha256 '42199e9eeb6f065f7620259f80b4098ae5ea021e43d9c0cc12061fbc866615a0'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
