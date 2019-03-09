3856ac39810c187b88c8cc63e80ef919e7ed6ed9
Randall <ran.dall@icloud.com>
2019-03-09 10:30:16 -0800

    Update clip-studio-paint to 1.8.8

--- Casks/clip-studio-paint.rb
+++ Casks/clip-studio-paint.rb
@@ -1,6 +1,6 @@
 cask 'clip-studio-paint' do
-  version '1.8.5'
-  sha256 '3f35033d13eb89663d8b9a8bfc764b206dee3f34ab78ae3bfc6c930934ff1f32'
+  version '1.8.8'
+  sha256 '19ee1780cbace9932870aef0db7c3f77f6b50b1697d01e75532ba2a8893c2cf0'
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
   name 'CLIP STUDIO PAINT'
