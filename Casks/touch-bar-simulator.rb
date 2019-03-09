f503040ace302aced8b310d80be5e9f2a5fd6637
hyuraku <yu@hill-wood.com>
2019-03-09 19:24:50 +0900

    repair touch-bar-simulator@3.0.0

--- Casks/touch-bar-simulator.rb
+++ Casks/touch-bar-simulator.rb
@@ -5,7 +5,7 @@ cask 'touch-bar-simulator' do
     url "https://github.com/sindresorhus/touch-bar-simulator/releases/download/v#{version}/Touch-Bar-Simulator-#{version}.dmg"
   else
     version '3.0.0'
-    sha256 '4abe55de716ae56a41031cdb1d3b27bf6b1efae18b33b80bb0419669a9a76aa1'
+    sha256 'a32089c9c52be117751e08d82e6e79474d298877754c15c02f78b1efeec88312'
     url "https://github.com/sindresorhus/touch-bar-simulator/releases/download/v#{version}/Touch.Bar.Simulator.#{version}.dmg"
   end
 
