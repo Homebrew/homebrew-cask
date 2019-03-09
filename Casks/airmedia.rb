16cadaf7fe0381e0749cffaab9a0e7daa2d55f70
CoreCode <core-code@users.noreply.github.com>
2019-03-09 15:57:01 -0800

    Update airmedia.rb

--- Casks/airmedia.rb
+++ Casks/airmedia.rb
@@ -6,6 +6,8 @@ cask 'airmedia' do
   appcast 'https://www.crestron.com/en-US/Products/Featured-Solutions/Airmedia'
   name 'Crestron AirMedia'
   homepage 'https://www.crestron.com/microsites/airmedia-mobile-wireless-hd-presentations'
+  
+  container nested: "airmedia_osx_#{version}_guest/airmedia_osx_#{version}_guest.dmg"
 
   app 'Crestron AirMedia.app'
 end
