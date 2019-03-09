80766c05a8ccb04f3af93add1bea5548319700bf
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-09 12:39:44 -0800

    Update rotato to 62.1552052042 (#60039)

--- Casks/rotato.rb
+++ Casks/rotato.rb
@@ -1,6 +1,6 @@
 cask 'rotato' do
-  version '59.1551366700'
-  sha256 'f71b344efb8cb75e0a0300aea4c53daea4a97c6f1f9c870f3a48dda992ab0705'
+  version '62.1552052042'
+  sha256 '29447c02bb1a0f887b2d5b39fb6578cea295b7f846253de0a832d1f40c5269b4'
 
   # dl.devmate.com/com.mortenjust.Rendermock was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.mortenjust.Rendermock/#{version.major}/#{version.minor}/DesignCamera-#{version.major}.zip"
