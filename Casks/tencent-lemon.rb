734a6fb65d4f628725e3d27036f818e42729e033
metowolf <i@i-meto.com>
2019-03-09 14:33:34 +0800

    Update tencent-lemon to 1.1.1

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,6 +1,6 @@
 cask 'tencent-lemon' do
-  version '1.1.0'
-  sha256 '676756d2b02d369779f6a283615b98d2c2accc438c17818b07bda0c70688b83c'
+  version '1.1.1'
+  sha256 '5bf4bcc15fa5e3e6477f7a69e27cb33cad65ee55f3d0fba9c9acd4992459ab8b'
 
   # pm.myapp.com/invc/xfspeed/qqpcmgr was verified as official when first introduced to the cask
   url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon_#{version}.dmg"
