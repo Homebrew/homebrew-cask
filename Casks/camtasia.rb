e306cdba9dbf3f33c7accac7b6e51768f5aa43f3
Randall <ran.dall@icloud.com>
2019-03-09 12:18:20 -0800

    Update camtasia to 2018.0.9,161

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,6 +1,6 @@
 cask 'camtasia' do
-  version '2018.0.9,159'
-  sha256 '3e142e510478666607f7e1b7e523f7bf685b2c1e8f89a7bb8b5fe8e25c0b91be'
+  version '2018.0.9,161'
+  sha256 '95c81c267ed4b6b9abc4a7a9885ad5f53cc749ca2719fdfb303331bb94048b49'
 
   # rink.hockeyapp.net/api/2/apps/5d440dc130030d8a5db2ee6265d8df09 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/5d440dc130030d8a5db2ee6265d8df09/app_versions/#{version.after_comma}?format=zip"
