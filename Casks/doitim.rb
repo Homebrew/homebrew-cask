ad9905aa2fcb9b426c33d268874d844aa93490da
CoreCode <core-code@users.noreply.github.com>
2019-03-09 15:53:52 -0800

    Update doitim.rb

--- Casks/doitim.rb
+++ Casks/doitim.rb
@@ -3,7 +3,7 @@ cask 'doitim' do
   sha256 'bf875f3b200540a708a8d4f404e98bc73b355b81a27183d091d31af73fe4f122'
 
   url "https://version.doit.im/dl/doit.im.#{version}.zip"
-  appcast 'http://doit.im/#download'
+  appcast 'https://version.doit.im/json/versions.json'
   name 'Doit.im'
   homepage 'https://doit.im/'
 
