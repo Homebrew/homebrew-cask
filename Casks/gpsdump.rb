f6109b7f5db11ca8a39e1b51c25062ef607941ce
Jeff Wishnie <github@wishnie.org>
2019-03-08 19:40:06 -0800

    Correct SHA256 (#60005)
    SHA256 value incorrect. Replaced with value from `shasum -a 256` of archive downloaded directly from GpsDump site:
    ```$ shasum -a 256 GpsDumpMac049.zip 
    79fba744497f6739ed8db5f0b49f145b7ea0f0f8ab6fa21bdb0478b77924e831```

--- Casks/gpsdump.rb
+++ Casks/gpsdump.rb
@@ -1,6 +1,6 @@
 cask 'gpsdump' do
   version '0.49'
-  sha256 '05ad25e276cbbc9ce90e261805c8880796f1e5910725d8fa964c186f10e03d0f'
+  sha256 '79fba744497f6739ed8db5f0b49f145b7ea0f0f8ab6fa21bdb0478b77924e831'
 
   url "http://www.gpsdump.no/GpsDumpMac#{version.no_dots}.zip"
   appcast 'http://www.gpsdump.no/body_mac.htm'
