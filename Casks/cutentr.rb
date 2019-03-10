609ddc8b28d17059fc2261ebe178501637997053
Vítor Galvão <info@vitorgalvao.com>
2019-03-10 11:25:51 +0000

    Update cutentr.rb

--- Casks/cutentr.rb
+++ Casks/cutentr.rb
@@ -2,7 +2,8 @@ cask 'cutentr' do
   version '0.3.2'
   sha256 '4a418c7e7a8baad9c64b6ec9730421b9a5411e1c8e2183d610fc9a3e6e634e8c'
 
-  url 'https://github.com/EBLeifEricson/cuteNTR-OSX/releases/download/0.3.2/cuteNTR-0.3.2.dmg'
+  url "https://github.com/EBLeifEricson/cuteNTR-OSX/releases/download/#{version}/cuteNTR-#{version}.dmg"
+  appcast 'https://github.com/EBLeifEricson/cuteNTR-OSX/releases.atom'
   name 'cuteNTR'
   homepage 'https://github.com/EBLeifEricson/cuteNTR-OSX'
 
