5a2f24aac6fbbda6bbc19d1416f00b8a88451665
Nathaniel Suchy <me@lunorian.is>
2019-03-09 22:14:25 -0500

    Create cutentr.rb

---
+++ Casks/cutentr.rb
@@ -0,0 +1,10 @@
+cask 'cutentr' do
+  version '0.3.2'
+  sha256 '4a418c7e7a8baad9c64b6ec9730421b9a5411e1c8e2183d610fc9a3e6e634e8c'
+
+  url 'https://github.com/EBLeifEricson/cuteNTR-OSX/releases/download/0.3.2/cuteNTR-0.3.2.dmg'
+  name 'cuteNTR'
+  homepage 'https://github.com/EBLeifEricson/cuteNTR-OSX'
+
+  app 'cuteNTR.app'
+end
