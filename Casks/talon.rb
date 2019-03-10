38452ba9b793c236e7be4fc0353c02d63225af1a
CoreCode <core-code@users.noreply.github.com>
2019-03-10 04:30:45 -0700

    Update talon to 89-0.0.8.29 (#60044)

--- Casks/talon.rb
+++ Casks/talon.rb
@@ -1,9 +1,9 @@
 cask 'talon' do
-  version '78-0.0.8.18'
-  sha256 'cc5cf33854152b3f325de6291ac26ee8835caaa212b09f9d63e65c3bbddef700'
+  version '89-0.0.8.29'
+  sha256 '87d49ff38693872394f17dc9413e00de2c82693c6eaf0c44a45c8740c86748b2'
 
   url "https://talonvoice.com/update/nmi5s3faoq6NzROd2dbCRg/Talon-#{version}.dmg"
-  appcast "https://talonvoice.com/update/nmi5s3faoq6NzROd2dbCRg/Talon-#{version}.html"
+  appcast 'https://talonvoice.com/'
   name 'Talon'
   homepage 'https://talonvoice.com/'
 
