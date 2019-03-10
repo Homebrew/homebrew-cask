33929b01a43d0bc180a0ff183cd1561a2e7f4006
Vítor Galvão <info@vitorgalvao.com>
2019-03-10 11:31:14 +0000

    Update airmedia.rb

--- Casks/airmedia.rb
+++ Casks/airmedia.rb
@@ -6,7 +6,7 @@ cask 'airmedia' do
   appcast 'https://www.crestron.com/en-US/Products/Featured-Solutions/Airmedia'
   name 'Crestron AirMedia'
   homepage 'https://www.crestron.com/microsites/airmedia-mobile-wireless-hd-presentations'
-  
+
   container nested: "airmedia_osx_#{version}_guest/airmedia_osx_#{version}_guest.dmg"
 
   app 'Crestron AirMedia.app'
