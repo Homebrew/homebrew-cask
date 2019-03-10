39ad6e1b70ea39323ec6bfd4ce1c0cf6c1bc76a1
CoreCode <core-code@users.noreply.github.com>
2019-03-10 09:58:06 -0700

    Update paraview.rb

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -4,7 +4,7 @@ cask 'paraview' do
 
   url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.8-64bit.dmg",
       user_agent: :fake
-  appcast 'https://www.paraview.org/files/paraview-download.js'
+  appcast 'https://www.paraview.org/files/listing.txt'
   name 'ParaView'
   homepage 'https://www.paraview.org/'
 
