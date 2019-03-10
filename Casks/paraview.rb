7ba5f4437882146030922bdca4f703ceba99bfb2
CoreCode <core-code@users.noreply.github.com>
2019-03-10 14:04:07 -0700

    Update paraview appcast stanza (#60059)
    * Update paraview.rb
    
    * Update paraview.rb

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,10 +1,10 @@
 cask 'paraview' do
   version '5.6.0'
-  sha256 '7e326051fec9b83a3144bae8a0b99cdf00d00d04022a1723a3c97be5a7f33576'
+  sha256 '95f1d0eaa36452e2faae2f9d6077c933bfb78a31fca86eff1d80471aea471db7'
 
   url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.8-64bit.dmg",
       user_agent: :fake
-  appcast 'https://www.paraview.org/files/paraview-download.js'
+  appcast 'https://www.paraview.org/files/listing.txt'
   name 'ParaView'
   homepage 'https://www.paraview.org/'
 
