44c19ac2e66f103fdae703243b7c0ffab7f10c80
Vítor Galvão <info@vitorgalvao.com>
2019-03-10 23:42:20 +0000

    Update coyim.rb

--- Casks/coyim.rb
+++ Casks/coyim.rb
@@ -2,7 +2,7 @@ cask 'coyim' do
   version '0.3.10'
   sha256 '6623eb9f86ae44006a312de23537910558fe5c283e0b603daef24a38d577c325'
 
-  # bintray.com/coyim/coyim-bin was verified as official when first introduced to the cask
+  # dl.bintray.com/coyim/coyim-bin was verified as official when first introduced to the cask
   url "https://dl.bintray.com/coyim/coyim-bin/v#{version}/mac-bundle/coyim.dmg"
   name 'CoyIM'
   homepage 'https://coy.im/'
