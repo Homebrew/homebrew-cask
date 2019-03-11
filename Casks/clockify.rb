fa0ec46da1528760b70f3caf8c0a4a8a41cb4968
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-11 13:22:21 -0700

    Update clockify to 2.2.4_59 (#60136)

--- Casks/clockify.rb
+++ Casks/clockify.rb
@@ -1,6 +1,6 @@
 cask 'clockify' do
-  version '2.2.3_57'
-  sha256 '86d993cd13175b5431b26d420baede92020f9ca1086ad214a6de907e99cee61f'
+  version '2.2.4_59'
+  sha256 'e2e4804a259dc49f15be62017ce519725ee633dd3d6be5511515b1c342addad0'
 
   url "https://clockify.me/downloads/ClockifyDesktop_#{version.no_dots}.zip"
   name 'Clockify'
