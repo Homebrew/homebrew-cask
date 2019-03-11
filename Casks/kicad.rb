d5028a08e5b627b517d7aabdfb4f8ef5ec6ecd12
Max Riveiro <kavu13@gmail.com>
2019-03-11 21:42:52 +0300

    Update kicad to 5.1.0
    Signed-off-by: Max Riveiro <kavu13@gmail.com>

--- Casks/kicad.rb
+++ Casks/kicad.rb
@@ -1,13 +1,35 @@
 cask 'kicad' do
-  version '5.0.1'
-  sha256 '3066e7c8da09afea2e27b4dc5d0742c83c5a2e93408d9d76e342e14e594bc7b3'
+  version '5.1.0'
 
-  url "http://downloads.kicad-pcb.org/osx/stable/kicad-unified-#{version}.dmg"
-  appcast 'http://downloads.kicad-pcb.org/osx/stable/'
+  if MacOS.version <= :high_sierra
+    sha256 'be024d121a42c9d1e1ea08fdf9dd2ad24cf77879d7f32214b2de13322cd18bae'
+    url "https://kicad-downloads.s3.cern.ch/osx/stable/kicad-unified-#{version}.dmg"
+  else
+    sha256 '23955e59e0ac1fe7db86156e0470bd2bc8f1c1666ef1c5def5fa3a82932c9907'
+    url "https://kicad-downloads.s3.cern.ch/osx/stable/kicad-unified-#{version}-10_14.dmg"
+  end
+
+  appcast 'https://kicad-downloads.s3.cern.ch/?delimiter=/&prefix=osx/stable/'
   name 'KiCad'
   homepage 'http://kicad-pcb.org/'
 
-  suite 'KiCad'
+  app 'KiCad/kicad.app',            target: 'KiCad/KiCad.app'
+  app 'KiCad/bitmap2component.app', target: 'KiCad/bitmap2component.app'
+  app 'KiCad/eeschema.app',         target: 'KiCad/eeschema.app'
+  app 'KiCad/gerbview.app',         target: 'KiCad/gerbview.app'
+  app 'KiCad/pcb_calculator.app',   target: 'KiCad/pcb_calculator.app'
+  app 'KiCad/pcbnew.app',           target: 'KiCad/pcbnew.app'
+  app 'KiCad/pl_editor.app',        target: 'KiCad/pl_editor.app'
+  artifact 'kicad/help',            target: '/Library/Application Support/kicad/help'
+  artifact 'kicad/library',         target: '/Library/Application Support/kicad/library'
+  artifact 'kicad/modules',         target: '/Library/Application Support/kicad/modules'
+  artifact 'kicad/share',           target: '/Library/Application Support/kicad/share'
+  artifact 'kicad/template',        target: '/Library/Application Support/kicad/template'
+
+  uninstall rmdir: [
+                     '/Library/Application Support/kicad',
+                     '/Applications/KiCad',
+                   ]
 
   zap trash: '~/Library/Preferences/kicad'
 end
