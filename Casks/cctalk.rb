0e6aea212d4473f1646c1239cc01ffd32735fcbb
CoreCode <core-code@users.noreply.github.com>
2019-03-11 23:53:32 +0100

    Update cctalk to 7.3.16.4 (#60121)
    * Update cctalk.rb
    
    * Update cctalk.rb

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -1,10 +1,10 @@
 cask 'cctalk' do
-  version '7.3.14.8'
-  sha256 'd74846b88e0e09025f765bbd77c4b1d9327746ee32c4df32b8a05af6aebae574'
+  version '7.3.16.4'
+  sha256 '8f4d76551837dc22a80e85113ca8f806b1ef685f3e3b3671a2ea1afd7c95a0e0'
 
   # cc.hjfile.cn was verified as official when first introduced to the cask
   url "https://cc.hjfile.cn/cc/#{version}/8/1/103/#{version}.dmg"
-  appcast 'https://cc.hjfile.cn/mac/update/info.xml'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=http://www.cctalk.com/webapi/basic/v1.1/version/down%3Fapptype=1%26terminalType=8%26versionType=103'
   name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
 
