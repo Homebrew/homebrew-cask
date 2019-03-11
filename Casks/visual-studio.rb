0d44951f39711bc3e744756706da5445182d98d6
Julian Mayer <julian@corecode.io>
2019-03-11 16:04:48 +0100

    Update visual-studio to 7.8.2.1

--- Casks/visual-studio.rb
+++ Casks/visual-studio.rb
@@ -1,10 +1,10 @@
 cask 'visual-studio' do
-  version '7.7.4.1'
-  sha256 'e3274ba0cd65380149967f76a29151e50e55de7841f741f3a3033ff124cdf19b'
+  version '7.8.2.1'
+  sha256 '51f1b1d1ed1582d0f4922963e07f5e08a6f1c593165dbe136d18ed73e6247abf'
 
   # dl.xamarin.com/VsMac was verified as official when first introduced to the cask
   url "https://dl.xamarin.com/VsMac/VisualStudioForMac-#{version}.dmg"
-  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
+  appcast 'https://docs.microsoft.com/en-us/visualstudio/releasenotes/vs2017-mac-relnotes'
   name 'Visual Studio for Mac'
   homepage 'https://www.visualstudio.com/vs/visual-studio-mac/'
 
