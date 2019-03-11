6e08c1dae67429a837f97d6bb3de4e8b270586c7
Vítor Galvão <info@vitorgalvao.com>
2019-03-11 12:12:17 +0000

    Update muzie.rb

--- Casks/muzie.rb
+++ Casks/muzie.rb
@@ -2,8 +2,9 @@ cask 'muzie' do
   version '1.3.0'
   sha256 'd203ccc7251110ade0bf4314b1f1e5a750da57e80abcb71c4cf6bd6f61fc86df'
 
-  # amazonaws.com/muzie-release was verified as official when first introduced to the cask
+  # s3.amazonaws.com/muzie-release was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/muzie-release/Muzie-#{version}.dmg"
+  appcast 'https://muzie.netlify.com/'
   name 'Muzie'
   homepage 'https://muzie.netlify.com/'
 
