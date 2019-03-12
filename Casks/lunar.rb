c14586c9b32d1ab75f65ebf8ed7afa0a8d033912
Randall <ran.dall@icloud.com>
2019-03-12 08:27:59 -0700

    Update lunar to 2.2.1

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,6 +1,6 @@
 cask 'lunar' do
-  version '2.2.0'
-  sha256 'eec7d26743c27a304d99206380150404a0ab9f07b2c9e610c55dc20be2d6d350'
+  version '2.2.1'
+  sha256 '9905de514eead837380110fd9828e36ac6693ca4f9d6fbb94273d4021996e04e'
 
   # github.com/alin23/Lunar was verified as official when first introduced to the cask
   url "https://github.com/alin23/Lunar/releases/download/v#{version}/Lunar.dmg"
