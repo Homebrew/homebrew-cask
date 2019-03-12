a1b65caf49324443ae1097de15c38fe74940ecee
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-12 09:30:48 -0700

    Revert URL

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,8 +1,8 @@
 cask 'reaper' do
-  version '5.973'
+  version '5.97.300'
   sha256 '1523f9edb7ad3a5ad56ad310e785b49368484dc3e956c0b907919c547869ba1a'
 
-  url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots}_x86_64.dmg"
+  url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots.sub(%r{0*$}, '')}_x86_64.dmg"
   appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64'
   name 'REAPER'
   homepage 'https://www.reaper.fm/'
