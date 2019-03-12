7d57a39f8577caeb3ba4667891aaa8ecf9cdd5be
Julian Mayer <julian@corecode.io>
2019-03-12 23:51:59 +0100

    Update yammer to 3.3.0

--- Casks/yammer.rb
+++ Casks/yammer.rb
@@ -4,6 +4,7 @@ cask 'yammer' do
 
   # yammerdesktopapp.blob.core.windows.net/binaries/dist was verified as official when first introduced to the cask
   url "https://yammerdesktopapp.blob.core.windows.net/binaries/dist/darwin/x64/#{version}/Yammer-#{version}.dmg"
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://aka.ms/yammer_desktop_mac'
   name 'Yammer'
   homepage 'https://support.office.com/en-us/article/Yammer-for-Windows-and-Mac-50920c05-cbfc-4f11-8503-e20fb2e623a5'
 
