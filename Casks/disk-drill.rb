5acbd7cae4b95937ee2a9da08c1b1992f5caa4ba
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-12 10:10:03 -0700

    Update disk-drill to 3.7.932 (#60178)

--- Casks/disk-drill.rb
+++ Casks/disk-drill.rb
@@ -1,6 +1,6 @@
 cask 'disk-drill' do
-  version '3.7.929'
-  sha256 '876c3ce3bbe70c57f4bc7108ed03db98a18a6b1e56d77a0c309437cc03d54482'
+  version '3.7.932'
+  sha256 '870be28a0ac95d4277f1f30c59c12a68019110c081dc75ab13dffc79623934de'
 
   url "https://www.cleverfiles.com/releases/DiskDrill_#{version}.zip"
   appcast 'https://www.cleverfiles.com/releases/auto-update/dd2-newestr.xml'
