3651003a392d3b2a12f12effba011941ab83a41f
CoreCode <core-code@users.noreply.github.com>
2019-03-12 21:37:29 +0100

    Update zoho-mail to 1.0.12 (#60201)

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -1,8 +1,9 @@
 cask 'zoho-mail' do
-  version '1.0.11'
-  sha256 '02738c978d43974e49c7bd794a6ffc51ed51862cd04472016ba558f21bffa4de'
+  version '1.0.12'
+  sha256 '47e98f1ec76cf9ed8cd397464068824bf6b9ac0c88adf83bc7ef1c4ccd149d4f'
 
   url "https://www.zoho.com/mail/desktop/mac/zoho-mail-desktop-installer-v#{version}.dmg"
+  appcast 'https://www.zoho.com/mail/desktop/artifacts.json'
   name 'Zoho Mail'
   homepage 'https://www.zoho.com/mail/desktop/'
 
