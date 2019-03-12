ef200f070b040af7208f4d3bf04730a6d6c58d82
Randall <ran.dall@icloud.com>
2019-03-12 08:53:16 -0700

    Update droplr to 5.6.1,233

--- Casks/droplr.rb
+++ Casks/droplr.rb
@@ -1,6 +1,6 @@
 cask 'droplr' do
-  version '5.6.0,232'
-  sha256 '243f69ad61fc5a2656b142df666704f8f1b5455a0f4cad1ddb506731cf4a4189'
+  version '5.6.1,233'
+  sha256 '6a1fc57a572a5422f51ba79cc04b39c4315bd1428ee007e3ae7862f2a5484953'
 
   url "https://files.droplr.com/apps/mac/Droplr+#{version.after_comma}.zip"
   appcast 'https://droplr.com/appcast/appcast-sandbox.xml'
