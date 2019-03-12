678487b40630237c17db0e7906fe3423b9dd27d7
CoreCode <core-code@users.noreply.github.com>
2019-03-12 21:16:20 +0100

    Update majsoul-plus to 1.11.1 (#60152)

--- Casks/majsoul-plus.rb
+++ Casks/majsoul-plus.rb
@@ -1,8 +1,8 @@
 cask 'majsoul-plus' do
-  version '1.10.6'
-  sha256 'e95fad51b4e6ed2d4012dd4109e6a5ef4e6e8e20e7b9695d8dba1e51e6a91925'
+  version '1.11.1'
+  sha256 '0d44d28ce2bfd8d2419c2cc37916ee79d94ea153322823cb6edac41f37b7e3f1'
 
-  url "https://github.com/MajsoulPlus/majsoul-plus-client/releases/download/v#{version}/Majsoul_Plus-darwin.zip"
+  url "https://github.com/MajsoulPlus/majsoul-plus-client/releases/download/v#{version}/Majsoul_Plus-darwin.dmg"
   appcast 'https://github.com/MajsoulPlus/majsoul-plus-client/releases.atom'
   name 'Majsoul Plus'
   homepage 'https://github.com/MajsoulPlus/majsoul-plus-client'
