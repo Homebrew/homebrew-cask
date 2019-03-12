3a3c3e24bf73e01d9a2070beaeac06618d42389a
Work <brigitte@corecode.io>
2019-03-12 16:20:31 +0100

    Update pagico to 8.17,2382

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,6 +1,6 @@
 cask 'pagico' do
-  version '8.17.2379'
-  sha256 'b0639118bf30129e9d11277ac596bc2f50c5192ff05c9a2b78ba849ae8a87678'
+  version '8.17,2382'
+  sha256 'bea18148ddbb7f49e81b704d781bba2c2f7a623c1b979d07b7aff9d531b890f0'
 
   url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.patch}.dmg"
   appcast 'https://www.pagico.com/api/pagico8.mac.xml'
