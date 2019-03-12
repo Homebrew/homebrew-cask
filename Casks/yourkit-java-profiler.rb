71ecc85c3093d2e7192aaa358bb28de44deeb162
Julian Mayer <julian@corecode.io>
2019-03-12 23:24:57 +0100

    Update yourkit-java-profiler to 2019.1-b112

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,8 +1,9 @@
 cask 'yourkit-java-profiler' do
-  version '2018.04-b88'
-  sha256 '29a985791e859407c22e9dadd6c069a42e4908d1f3c0e6759e5b778f91fff98d'
+  version '2019.1-b112'
+  sha256 '30573a49e94df23043c29230778b8ee581f894d2421e2b0fe276295e30b66637'
 
   url "https://www.yourkit.com/download/YourKit-JavaProfiler-#{version}.dmg"
+  appcast 'https://www.yourkit.com/download/'
   name 'YourKit Java Profiler'
   homepage 'https://www.yourkit.com/features/'
 
