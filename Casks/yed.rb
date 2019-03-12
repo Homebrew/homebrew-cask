de189c521bf7c263a33cef1f2d66fa4fbac51842
Julian Mayer <julian@corecode.io>
2019-03-12 23:45:05 +0100

    Update yed to 3.18.2

--- Casks/yed.rb
+++ Casks/yed.rb
@@ -3,6 +3,7 @@ cask 'yed' do
   sha256 '9a6b3522cf68c8eca2e9a56f9e619e797ba97b675b043b8c36445454b86596ad'
 
   url "https://www.yworks.com/resources/yed/demo/yEd-#{version}_with-JRE10.dmg"
+  appcast 'https://www.yworks.com/products/yed/download#download'
   name 'yWorks yEd'
   homepage 'https://www.yworks.com/products/yed'
 
