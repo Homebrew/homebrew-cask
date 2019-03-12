497a9156878792071d9c7288259c4b4abc7c51df
CoreCode <core-code@users.noreply.github.com>
2019-03-12 19:34:45 +0100

    Update lightsoff.rb

--- Casks/lightsoff.rb
+++ Casks/lightsoff.rb
@@ -3,7 +3,7 @@ cask 'lightsoff' do
   sha256 'f674d8c35f006bafadb2950c84268aca2d3791a1e5e71ec474eeada60ff43fd9'
 
   url "https://www.lightsoff-app.com/s/LightsOff_#{version.dots_to_underscores}.zip"
-  appcast 'https://www.lightsoff-app.com/'
+  appcast 'https://gumroad.com/l/lightsoff/'
   name 'LightsOff'
   homepage 'https://www.lightsoff-app.com/'
 
