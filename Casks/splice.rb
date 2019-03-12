8036255a730d786da9306c43fffbbe940b23ac8f
CoreCode <core-code@users.noreply.github.com>
2019-03-12 21:49:52 +0100

    Update splice to 3.4.3 (#60213)

--- Casks/splice.rb
+++ Casks/splice.rb
@@ -1,6 +1,6 @@
 cask 'splice' do
-  version '3.4.2'
-  sha256 'd55d7a063238eb4e43a084699ded0e2720934e39fafcbc43f74abd6c5df80745'
+  version '3.4.3'
+  sha256 '9e1ee52719b269921eda48fe53e42219c3f94e119b96bf43648ece269069f9d0'
 
   # splicedesktop.s3-us-west-1.amazonaws.com was verified as official when first introduced to the cask
   url 'https://splicedesktop.s3-us-west-1.amazonaws.com/darwin/stable/Splice.app.zip'
