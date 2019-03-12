a3d02256eae69513d2aa77d41b67c713a12e9f7b
CoreCode <core-code@users.noreply.github.com>
2019-03-12 21:40:23 +0100

    Update postman.rb (#60208)

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -3,7 +3,7 @@ cask 'postman' do
   sha256 'bb44418f3f9c219c3d62f648cff8100b26994d025e9d95c404f8b2d73aaada3b'
 
   # dl.pstmn.io/download/version was verified as official when first introduced to the cask
-  url "https://dl.pstmn.io/download/version/#{version}/osx64"
+  url "https://dl.pstmn.io/download/version/#{version.major}.0.0/osx64"
   appcast "https://dl.pstmn.io/update/status?channel=stable&currentVersion=#{version}&arch=64&platform=osx&syncEnabled=true&teamPlan="
   name 'Postman'
   homepage 'https://www.getpostman.com/'
