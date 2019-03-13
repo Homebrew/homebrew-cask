d689cf35b1c5a7dea8550799beeb7a0448d774c8
Oliver Schneider <olsio@users.noreply.github.com>
2019-03-13 08:16:57 +0100

    Update gitkraken to 5.0.1 (#60245)

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,6 +1,6 @@
 cask 'gitkraken' do
-  version '5.0.0'
-  sha256 '2f46075fca220d7e60a98ad1b91190b18950972fe9c9cf84fe2b8b2cbbe17575'
+  version '5.0.1'
+  sha256 'e6378ec8071807133b1d6af84f59d1e89eb95d67a7ee7146c4a875c025ffe3ad'
 
   url "https://release.gitkraken.com/darwin/GitKraken-v#{version}.zip"
   appcast 'https://support.gitkraken.com/release-notes/current'
