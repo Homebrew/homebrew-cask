d4240ba1091e8e404e580838809287faaef86baa
Randall <ran.dall@icloud.com>
2019-03-13 10:05:45 -0700

    Update default-folder-x to 5.3.5

--- Casks/default-folder-x.rb
+++ Casks/default-folder-x.rb
@@ -1,6 +1,6 @@
 cask 'default-folder-x' do
-  version '5.3.4'
-  sha256 'ff61ed0758a67875e8510f161861110cc328fe9ad1cf24c385da2e9a073e3d82'
+  version '5.3.5'
+  sha256 '514afdb151681db8e9ee4332f8023ec6b05eed3e050b15229b1bf8741c5fc980'
 
   url "https://www.stclairsoft.com/download/DefaultFolderX-#{version}.dmg"
   appcast 'https://www.stclairsoft.com/cgi-bin/sparkle.cgi?DX5'
