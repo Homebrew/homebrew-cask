b42805cdea9578399723d0b04a2e8354a8d71add
suschizu <brigitte@corecode.io>
2019-03-13 23:39:09 +0100

    Update cacher to 2.6.4 (#60304)

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,6 +1,6 @@
 cask 'cacher' do
-  version '2.6.3'
-  sha256 '99db7193e8a8e846286507fadfef16df4234f714e3bd72b5d2d4d3658cbdd0dc'
+  version '2.6.4'
+  sha256 '8e04c2a980668f22cd58fc1692c2c167eb7c2d6f875b50c0eb22e5149539735f'
 
   # cacher-download.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
