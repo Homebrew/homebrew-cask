ad99b4112d0b009429fa9b9de3563128459be99c
Julian Mayer <julian@corecode.io>
2019-03-13 16:57:51 +0100

    Update screaming-frog-seo-spider to 11.1

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -1,6 +1,6 @@
 cask 'screaming-frog-seo-spider' do
-  version '11.0'
-  sha256 '9512a518b67f1b98b2066016afba63d49cd3699cc68e5832fd3bcae33ab6ceb6'
+  version '11.1'
+  sha256 'b9655712d2791e6085c3bbb67d7ac3096baa3854bfb697582b2c30e27bc933a7'
 
   url "https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}.dmg"
   appcast 'https://www.screamingfrog.co.uk/wp-content/themes/screamingfrog/inc/download-modal.php'
