6f6e69f5722f80371c3d363f40153e1f9275f007
Julian Mayer <julian@corecode.io>
2019-03-14 00:11:14 +0100

    Update wingide to 6.1.5-1

--- Casks/wingide.rb
+++ Casks/wingide.rb
@@ -3,6 +3,7 @@ cask 'wingide' do
   sha256 '755d9a50e4ac2f70c7cc8ba763adfa510805a9cd51876781a0e72587129df3fb'
 
   url "https://wingware.com/pub/wingide/#{version.sub(%r{-\d+}, '')}/wingide-#{version}.dmg"
+  appcast 'https://www.wingware.com/downloads/wing-pro'
   name 'WingIDE'
   homepage 'https://www.wingware.com/'
 
