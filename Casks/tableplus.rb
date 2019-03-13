f2988b6bfeda47497f086641f5d558b096bde93c
suschizu <brigitte@corecode.io>
2019-03-13 20:35:27 +0100

    Update tableplus to 2.0,200 (#60286)

--- Casks/tableplus.rb
+++ Casks/tableplus.rb
@@ -1,5 +1,5 @@
 cask 'tableplus' do
-  version '1.6,200'
+  version '2.0,200'
   sha256 '5a9d2452a71227a030937f82cedd14a685fdf853f33829215b2f7a9eedf7492e'
 
   # s3.amazonaws.com/tableplus-osx-builds was verified as official when first introduced to the cask
