3bf68ae0bdba826b507ca622b7494a4f7575d6a1
Randall <ran.dall@icloud.com>
2019-03-13 10:21:43 -0700

    Update vellum to 2.5.2

--- Casks/vellum.rb
+++ Casks/vellum.rb
@@ -1,6 +1,6 @@
 cask 'vellum' do
-  version '2.5.1'
-  sha256 'db25cf31443a71607ceb39769cbfe73b064e55e7d39fe9ee3069f4da9d01b59d'
+  version '2.5.2'
+  sha256 'c722f2181cf3137940f1a817711f44d517d93e92e84c41afbb5007fb05894121'
 
   # 180g.s3.amazonaws.com/downloads was verified as official when first introduced to the cask
   url "https://180g.s3.amazonaws.com/downloads/Vellum-#{version.no_dots}00.zip"
