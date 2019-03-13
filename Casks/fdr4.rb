08475c7f6f187938375b38ec69b527c7acb11539
Juan Cruz Viotti <jv@jviotti.com>
2019-03-13 13:41:57 +0000

    Add FDR4 v4.2.4
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ Casks/fdr4.rb
@@ -0,0 +1,10 @@
+cask 'fdr4' do
+  version '4.2.4'
+  sha256 '127b0b4b4e7e571f4e910452cf393fdcca642575710d4ad9b30577ba8cb1be81'
+
+  url 'https://www.cs.ox.ac.uk/projects/fdr/downloads/fdr-3800-mac-x86_64.zip'
+  name 'FDR4'
+  homepage 'https://www.cs.ox.ac.uk/projects/fdr/'
+
+  app 'FDR4.app'
+end
