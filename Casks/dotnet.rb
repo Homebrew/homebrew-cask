63dead31e16652e73a9c16c3393b35d21309ef14
Josh Jones <jjones@pltnm.com>
2019-03-13 12:55:14 -0500

    Update dotnet to 2.2.3

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -1,8 +1,8 @@
 cask 'dotnet' do
-  version '2.2.2'
-  sha256 '263cd8ea9c4579908ba6fb6b4c5f54170a533304ebce4074cc1df6bd0af3250a'
+  version '2.2.3'
+  sha256 '85fa291a86baaed7ae7b2806ae610d40e96155c15eb64ebbc8c9572d9f7534b9'
 
-  url "https://download.visualstudio.microsoft.com/download/pr/eb9047cf-9d6f-472a-940e-05f018cdb29e/62c457d7f2ead9eccc099978f038c1f8/dotnet-runtime-#{version}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/872243f8-de92-480f-accd-9a22304cd3f9/aad669c10799a3e6e1deac73e8559c49/dotnet-runtime-#{version}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
   name '.Net Core Runtime'
   homepage 'https://www.microsoft.com/net/core#macos'
