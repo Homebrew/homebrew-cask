39e5e41e522c4fa68366c6c7375969f8e82d861d
Julian Mayer <julian@corecode.io>
2019-03-14 00:03:22 +0100

    Update wiznote to 2.7.1,2019-01-18

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -3,6 +3,7 @@ cask 'wiznote' do
   sha256 'f0935a10008de631fb81e2a691e312cb455047c24160b6d020664fb080728381'
 
   url "https://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://url.wiz.cn/u/mac'
   name 'WizNote'
   homepage 'https://www.wiz.cn/wiznote-mac.html'
 
