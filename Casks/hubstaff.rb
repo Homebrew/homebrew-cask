5ec21f05fa0c87e42430f35d10e818a3e0c204c3
Randall <ran.dall@icloud.com>
2019-03-13 10:07:08 -0700

    Update hubstaff to 1.4.4,556fe980

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,5 +1,5 @@
 cask 'hubstaff' do
-  version '1.4.2,8d78ff24'
+  version '1.4.4,556fe980'
   sha256 'b388bede05079f2e0bb9f8804846c010c70b47263c7cd23a893a2f62f135ee99'
 
   url "https://app.hubstaff.com/download/344-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
