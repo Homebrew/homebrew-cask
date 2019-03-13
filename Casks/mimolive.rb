7b490d562c960b46e988eee056e88e41295332d9
Randall <ran.dall@icloud.com>
2019-03-13 10:10:28 -0700

    Update mimolive to 4.7.2-26993

--- Casks/mimolive.rb
+++ Casks/mimolive.rb
@@ -1,6 +1,6 @@
 cask 'mimolive' do
-  version '4.7.1-26979'
-  sha256 '8ada458d875dbaa608794032d5fabc04d1293003211a9f74181f567d6bfef674'
+  version '4.7.2-26993'
+  sha256 '9905d42f36a4fe4480d155448627a5cba266ce67b8dd3c671a643b40a904d122'
 
   url "https://cdn.boinx.com/software/mimolive/Boinx_mimoLive_#{version}.app.zip"
   appcast 'https://sparkle.boinx.com/appcast.lasso?appName=mimoLive'
