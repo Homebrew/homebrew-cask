1df3942cede48c9b36d4617c6408f46371fae35e
Colin Kinloch <colin@kinlo.ch>
2019-03-13 18:38:08 +0000

    Update godot-mono to 3.1

--- Casks/godot-mono.rb
+++ Casks/godot-mono.rb
@@ -1,9 +1,9 @@
 cask 'godot-mono' do
-  version '3.0.6'
-  sha256 '7ddd0c4f0b03be4f82e12072b05130c6f78fe79d0e62720bb7e095532a46d420'
+  version '3.1'
+  sha256 'fe4ec27344cd04c777f0c4cef0f2dd317b72e8bf293332721deea760db9fd21f'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
-  url "https://downloads.tuxfamily.org/godotengine/#{version}/mono/Godot_v#{version}-stable_mono_osx.fat.zip"
+  url "https://downloads.tuxfamily.org/godotengine/#{version}/mono/Godot_v#{version}-stable_mono_osx.64.zip"
   appcast 'https://github.com/godotengine/godot/releases.atom'
   name 'Godot Engine'
   homepage 'https://godotengine.org/'
