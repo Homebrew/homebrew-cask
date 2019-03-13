ec980ac9198501060b074d32d30da9ebeb62a1d5
suliveevil <1361136657@qq.com>
2019-03-13 19:14:21 +0800

    Add BetterAndBetter.app v1.6.28-beta
    This is the first time BetterAndBetter commit to Homebrew.

---
+++ Casks/betterandbetter.rb
@@ -0,0 +1,12 @@
+cask 'betterandbetter' do
+  version :latest
+  sha256 :no_check
+
+  # github.com/suliveevil/BetterAndBetter/releases/download/v1.6.28-beta/BetterAndBetter.app.dmg was verified as official when first introduced to the cask
+  url 'https://github.com/suliveevil/BetterAndBetter/releases/download/v1.6.28-beta/BetterAndBetter.app.dmg'
+  name 'BetterAndBetter'
+  name '越来越好'
+  homepage 'http://better365.cn/'
+
+  app 'BetterAndBetter.app'
+end
