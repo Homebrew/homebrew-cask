887c3f3cbaa61b840c7f7540554f783243f4538f
kleins05 <kleins@web.de>
2019-03-14 12:04:51 +0100

    update to sencha 6.7.0.63 (#60334)
    update to most recent build version as it is required to build most recent ExtJS 6.7.0.210

--- Casks/sencha.rb
+++ Casks/sencha.rb
@@ -1,6 +1,6 @@
 cask 'sencha' do
-  version '6.7.0.37'
-  sha256 '1965691742306653212696556774c8cfc702aa821898b433840a5b96ded6d5ea'
+  version '6.7.0.63'
+  sha256 '3e888afb613223a7584376fc63c68f06eb6010a996eca82ee7a2e0d78b6658f3'
 
   url "https://cdn.sencha.com/cmd/#{version}/jre/SenchaCmd-#{version}-osx.app.zip"
   name 'Sencha Cmd'
