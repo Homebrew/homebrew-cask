e1eaa408d004b954c273a1564d416ae8842f0898
CoreCode <core-code@users.noreply.github.com>
2019-03-14 12:32:23 +0100

    Update wacom-inkspace 2.7.0: add appcast (#60322)

--- Casks/wacom-inkspace.rb
+++ Casks/wacom-inkspace.rb
@@ -3,6 +3,7 @@ cask 'wacom-inkspace' do
   sha256 'efbf407f39c58394b8037280da4f05ccb354494c00ff3e65aee78a442dc19735'
 
   url "https://cdn.wacom.com/i/m/mac/WacomInkspaceApp_#{version}.zip"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://link.wacom.com/i/m?os=mac'
   name 'Wacom Inkspace'
   homepage 'https://www.wacom.com/en-us/products/apps-services/inkspace'
 
