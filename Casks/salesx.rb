36efe9c3fb483956be45e434caa32ecdd43e29c6
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-14 08:52:08 -0700

    Update salesx to 1.5.1 (#60361)

--- Casks/salesx.rb
+++ Casks/salesx.rb
@@ -1,6 +1,6 @@
 cask 'salesx' do
-  version '1.5'
-  sha256 'c8e458a038a62f81fb78b912f3a557d6af5d125bd9117a603e4f17eafad09656'
+  version '1.5.1'
+  sha256 '28021a36794163bcefd2eaab09f33c9613b0da5853be1f039a8ff917b4b87311'
 
   # dl.devmate.com/io.seedlab.salesx was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/io.seedlab.salesx/SalesX.zip'
