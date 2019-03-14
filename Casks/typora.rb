e2986759ff83685337b7a6c81a7ec78a6b2e1d7b
tiiiecherle <tiiiecherle@gmail.com>
2019-03-15 00:17:42 +0100

    Update typora to 0.9.9.23

--- Casks/typora.rb
+++ Casks/typora.rb
@@ -1,6 +1,6 @@
 cask 'typora' do
-  version '0.9.9.22'
-  sha256 'ef8f1ad1930f8b38ed9869291139b838d215c3865fdf339ad03ba6cea642f171'
+  version '0.9.9.23'
+  sha256 '59dd4a9d973ec768a22f5e492b9aca93ddbefece62c26ed7b181f1c35cd39a5e'
 
   url "https://www.typora.io/download/Typora-#{version}.dmg"
   appcast 'https://www.typora.io/download/dev_update.xml'
