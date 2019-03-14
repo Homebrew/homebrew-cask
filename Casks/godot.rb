009d2a39a6fe784544bc2fb91419e3538784fb95
Brian Gyss <brian.gyss@gmail.com>
2019-03-13 17:07:00 -0700

    updated godot to 3.1

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,9 +1,9 @@
 cask 'godot' do
-  version '3.0.6'
-  sha256 'cd409615685d3b497b14cb774bbb315fb0a7a45a00c6865edbb2e43cc735a759'
+  version '3.1'
+  sha256 'dde1738280c03ebdd6d540381a782e74cfa4099941751bb14a55513386849f0e'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
-  url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.fat.zip"
+  url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.64.zip"
   appcast 'https://github.com/godotengine/godot/releases.atom'
   name 'Godot Engine'
   homepage 'https://godotengine.org/'
