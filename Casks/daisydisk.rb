0de0ba085ac3c35fc9bdce9fdf716fac2bd39d31
ahousseini <admin@anver-2.local>
2019-03-14 18:02:46 +0100

    Update daisydisk to 4.7.2

--- Casks/daisydisk.rb
+++ Casks/daisydisk.rb
@@ -4,8 +4,8 @@ cask 'daisydisk' do
     sha256 'fe2aa86f2ea8a1f0c4791857a5b7991ecad295b5b969849bb7b15a890ab54b86'
     url "https://www.daisydiskapp.com/downloads/DaisyDisk_#{version.dots_to_underscores}.zip"
   else
-    version '4.7.1'
-    sha256 '800b85f1cb3f8cbef4d7ef73ff01260c798f4abf3a1844fdd1fcc4ee9ff5bab9'
+    version '4.7.2'
+    sha256 '67e7df4b3caa8c7f20114b9a1ddb22de67f159475468b3e8527f828f06583122'
     url 'https://www.daisydiskapp.com/downloads/DaisyDisk.zip'
     appcast 'https://daisydiskapp.com/downloads/appcastReleaseNotes.php?appEdition=Standard&osVersion=10.13'
   end
