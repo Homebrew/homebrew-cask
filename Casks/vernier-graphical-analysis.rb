7d75f5f4f467b4d4cb206d59ddf4db4ab5fa5cb6
Julian Mayer <julian@corecode.io>
2019-03-15 00:29:20 +0100

    Update vernier-graphical-analysis to 4.7.1-1118

--- Casks/vernier-graphical-analysis.rb
+++ Casks/vernier-graphical-analysis.rb
@@ -4,6 +4,7 @@ cask 'vernier-graphical-analysis' do
 
   # graphicalanalysis.com was verified as official when first introduced to the cask
   url "https://software-releases.graphicalanalysis.com/ga/mac/release/Vernier-Graphical-Analysis-#{version}.dmg"
+  appcast 'https://www.vernier.com/products/software/graphical-analysis/'
   name 'Vernier Graphical Analysis'
   homepage 'https://www.vernier.com/products/software/graphical-analysis/'
 
