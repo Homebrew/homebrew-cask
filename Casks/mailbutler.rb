f53e9ac6eb2244fa457de807a7e8af8a1f797a6a
Vítor Galvão <info@vitorgalvao.com>
2019-03-14 19:40:28 +0000

    Update mailbutler.rb

--- Casks/mailbutler.rb
+++ Casks/mailbutler.rb
@@ -2,7 +2,6 @@ cask 'mailbutler' do
   version '2,1405-10837'
   sha256 '0210927c43b279eb253ba31fe502773352db313c94ff2942700f0ae693145bb6'
 
-  # mailbutler-data.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://downloads.mailbutler.io/Mailbutler_#{version.after_comma}.zip"
   appcast "https://www.mailbutler.io/appcast#{version.major}.php"
   name 'MailButler'
