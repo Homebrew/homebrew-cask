015bbcf85bf7383f249fe3761bfc1cfb30d2d2b1
Mentor Palokaj <mentor@palokaj.co>
2019-03-14 17:43:42 +0100

    Fix version typo and stanza order wintertime 0.0.2

--- Casks/wintertime.rb
+++ Casks/wintertime.rb
@@ -1,11 +1,11 @@
 cask 'wintertime' do
-  version '0.0.'
+  version '0.0.1'
   sha256 '0c056dcfb6b9fe90e3708935b7b88bdb1568764ac9f50107a407eb18e91f713e'
 
   url 'https://github.com/actuallymentor/wintertime-mac-background-freezer/releases/download/0.0.2/Wintertime-0.0.2.dmg'
+  appcast 'https://github.com/actuallymentor/wintertime-mac-background-freezer/releases.atom'
   name 'Wintertime'
   homepage 'https://github.com/actuallymentor/wintertime-mac-background-freezer'
-  appcast 'https://github.com/actuallymentor/wintertime-mac-background-freezer/releases.atom'
 
   app 'Wintertime.app'
 end
