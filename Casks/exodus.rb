16e5c3e3a795c35b2196b040d6ed852b770c6910
Randall <ran.dall@icloud.com>
2019-03-15 04:29:23 -0700

    Update exodus to 19.3.15

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,6 +1,6 @@
 cask 'exodus' do
-  version '19.3.7'
-  sha256 'b61eb9453e48962f7b88a92e10f065f9fb2e6b3fab3a10dce63eae20f03ac325'
+  version '19.3.15'
+  sha256 '07345dae5d34f4006bbb71010d0f0361e7dafd6488ed563cf76af3b8820b4b91'
 
   # exodusbin.azureedge.net was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-macos-#{version}.dmg"
