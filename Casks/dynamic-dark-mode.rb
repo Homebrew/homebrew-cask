42737cad6fad6fe3b2105c09b3aca773d09b0970
Vítor Galvão <info@vitorgalvao.com>
2019-03-15 14:13:42 +0000

    Update dynamic-dark-mode.rb

--- Casks/dynamic-dark-mode.rb
+++ Casks/dynamic-dark-mode.rb
@@ -1,6 +1,6 @@
 cask 'dynamic-dark-mode' do
   version '1.1.4'
-  sha256 '3101ef4222843c41a48122abef1f5e97501a67d8664c20a61a9397abeee1bdca'
+  sha256 '2fb033042bfbdcb39590f74ca6479252e1fdcc1529b05e03e8733c87a7abab6f'
 
   url "https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases/download/#{version}/Dynamic_Dark_Mode-#{version}.zip"
   appcast 'https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases.atom'
