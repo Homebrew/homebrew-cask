10a68a2e6e989492a58ea0d093449950407ae301
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-14 19:14:45 -0700

    Update jedit-omega to 2.09 (#60366)

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -3,8 +3,8 @@ cask 'jedit-omega' do
     version '1.32'
     sha256 '251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9'
   else
-    version '2.08'
-    sha256 'f7198adcc44402f1c4e916012ad2133653d1e0a0b34d0bfc7337219c980da453'
+    version '2.09'
+    sha256 '53ea2d290e0f645fedd100231a821f92bfda1385f5dea4910717744f56678599'
   end
 
   # artman21.co.jp was verified as official when first introduced to the cask
