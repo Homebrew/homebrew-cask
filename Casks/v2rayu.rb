88ee16c37eb2b78754529cb560ac2edbeae0bf3f
Junior Hsu <junxy@users.noreply.github.com>
2019-03-15 13:39:39 +0800

    Update v2rayu.rb (#60409)
    Update v2rayu version to 1.2.2

--- Casks/v2rayu.rb
+++ Casks/v2rayu.rb
@@ -1,6 +1,6 @@
 cask 'v2rayu' do
-  version '1.2.0'
-  sha256 'be5ce181bb4f2102338b3622730635ce29659f3b77fac8b965e2d3e645594b32'
+  version '1.2.2'
+  sha256 '7909b5109f9330d1757688e853c52b615928affcd9a416ca97ff8faff1690e04'
 
   url "https://github.com/yanue/V2rayU/releases/download/#{version}/V2rayU.dmg"
   appcast 'https://github.com/yanue/V2rayU/releases.atom'
