815dce9a4b448f7c9a3f28b3986e2631698354a9
Vítor Galvão <info@vitorgalvao.com>
2019-03-15 02:18:41 +0000

    Update betterandbetter.rb

--- Casks/betterandbetter.rb
+++ Casks/betterandbetter.rb
@@ -3,7 +3,7 @@ cask 'betterandbetter' do
   sha256 '3eefc3a3f51a9be50fde40695c1d7f49c283a88765e555e4ae16e4d26d5ae4a0'
 
   # 13315641.s21d-13.faiusrd.com was verified as official when first introduced to the cask
-  url "http://13315641.s21d-13.faiusrd.com/0/1/ABUIABAAGAAg2aSO5AUowLyioAI?f=Better+And+Better+#{version}+Alpha%E7%89%88.dmg&amp;v=1552126553"
+  url "http://13315641.s21d-13.faiusrd.com/0/1/ABUIABAAGAAg2aSO5AUowLyioAI?f=Better+And+Better+#{version}+Alpha%E7%89%88.dmg
   name 'Better And Better'
   homepage 'http://www.better365.cn/'
 
