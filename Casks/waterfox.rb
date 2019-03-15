2504e22ec1ae493d171daf959c3d120a48d7fc80
CoreCode <core-code@users.noreply.github.com>
2019-03-15 03:15:09 +0100

    Update waterfox to 56.2.7.1 (#60320)

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -4,6 +4,7 @@ cask 'waterfox' do
 
   # storage-waterfox.netdna-ssl.com was verified as official when first introduced to the cask
   url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20#{version}%20Setup.dmg"
+  appcast 'https://www.waterfoxproject.org/en-US/waterfox/new/'
   name 'Waterfox'
   homepage 'https://www.waterfoxproject.org/en-US/'
 
