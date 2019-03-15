ad8941a64e1dbc38a06bdf950d43ccf08604ff73
Micah Lee <micah@micahflee.com>
2019-03-15 12:14:51 -0700

    Upgrade gpg-sync to 0.3.3

--- Casks/gpg-sync.rb
+++ Casks/gpg-sync.rb
@@ -1,6 +1,6 @@
 cask 'gpg-sync' do
-  version '0.3.2'
-  sha256 '8e34e505e3eadc0d2fb36e738aa39a4ee989eb3041cf317920756d70e5978e04'
+  version '0.3.3'
+  sha256 '9c6c865f0482a92f51873e440eede33bbe6ec72596cbf490ba99b6cf4511a989'
 
   url "https://github.com/firstlookmedia/gpgsync/releases/download/v#{version}/GPGSync-#{version}.pkg"
   appcast 'https://github.com/firstlookmedia/gpgsync/releases.atom'
