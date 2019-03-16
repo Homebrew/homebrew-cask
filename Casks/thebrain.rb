b378202198d7e41d82e9c150c7502b5875442f42
Randall <ran.dall@icloud.com>
2019-03-16 08:36:19 -0700

    Update thebrain to 10.0.39.0

--- Casks/thebrain.rb
+++ Casks/thebrain.rb
@@ -1,6 +1,6 @@
 cask 'thebrain' do
-  version '10.0.36.0'
-  sha256 '87c7103d5cb3faae3640ae5573d68c389bb5c35f48efcc5cc527300fecba9a62'
+  version '10.0.39.0'
+  sha256 'dae72bfe35073572e28cb821e767fc7c967d538f38e21f5d58feb79023485747'
 
   url "http://updater.thebrain.com/files/TheBrain#{version}.dmg"
   name 'TheBrain'
