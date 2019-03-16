ba10ff863a3232a4eebfbd3459963f1a17d73255
suschizu <brigitte@corecode.io>
2019-03-16 16:23:11 +0100

    Update tankix.rb (#60458)

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,8 +1,8 @@
 cask 'tankix' do
-  version 'visual1-37541'
-  sha256 '0c9a3c1b5c18f7593eb9c733323599b61c218535b380d251737504472ead430f'
+  version '5.3.5f1,visual3-38779'
+  sha256 '3e1481d73ae5837a2fef12e49ac9f60dccde7adeccc3e83deee7edae77ab79bd'
 
-  url "https://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
+  url "https://static.tankix.com/app/StandaloneOSXIntel64/#{version.after_comma}/TankiX.dmg"
   name 'Tanki X'
   homepage 'https://tankix.com/'
 
