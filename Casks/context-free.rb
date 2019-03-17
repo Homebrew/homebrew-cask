bdf6744c5e3a040e182093f6660ad06bb7c6edca
brigitte zmoelnig <brigitte@corecode.io>
2019-03-17 23:05:54 +0100

    Update context-free to 3.1

--- Casks/context-free.rb
+++ Casks/context-free.rb
@@ -1,6 +1,6 @@
 cask 'context-free' do
-  version '3.0.11.3'
-  sha256 '5baae1cf7487ea0902f781f0891d3f7dc2c01071ab9546a1e2523676ea45c0da'
+  version '3.1'
+  sha256 '57e93777b37f14ad697964fd435a638ef17216503011ebf87b861958cd16ccc3'
 
   url "https://www.contextfreeart.org/download/ContextFree#{version}.dmg"
   appcast 'https://github.com/MtnViewJohn/context-free/releases.atom'
