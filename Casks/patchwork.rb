8b6822b29460a6c91ebda830034c48c97d9f74e6
Miikka Koskinen <miikka.koskinen@iki.fi>
2019-03-17 12:36:30 +0200

    Update patchwork to 3.11.5

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,6 +1,6 @@
 cask 'patchwork' do
-  version '3.11.4'
-  sha256 '6a750a0279d55ced844d1f8c8761e44a1755b4f8e836041ad9ceb92b85f90a31'
+  version '3.11.5'
+  sha256 '1e66856dbfee4f5cb1c4b6d69b05a27bc195525e96fa386663572f6375557e70'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}-mac.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom'
