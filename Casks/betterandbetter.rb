a129eb34884b69b52f4da3af91b2fbf8e728970b
suliveevil <1361136657@qq.com>
2019-03-17 18:19:16 +0800

    Update betterandbetter.rb

--- Casks/betterandbetter.rb
+++ Casks/betterandbetter.rb
@@ -2,8 +2,8 @@ cask 'betterandbetter' do
   version :latest
   sha256 :no_check
 
-  # github.com/suliveevil/homebrew-BetterAndBetter/releases/download/v1.6.28/BetterAndBetter.app.dmg was verified as official when first introduced to the cask
-  url 'https://github.com/songhao/BetterAndBetter/releases/download/v1.6.28/BetterAndBetter.app.dmg'
+  # github.com/songhao/BetterAndBetter/releases/download/v1.6.31-alpha/Better.And.Better.1.6.31.Alpha.dmg was verified as official when first introduced to the cask
+  url 'https://github.com/songhao/BetterAndBetter/releases/download/v1.6.31-alpha/Better.And.Better.1.6.31.Alpha.dmg'
   name 'BetterAndBetter'
   name '越来越好'
   homepage 'http://better365.cn/'
