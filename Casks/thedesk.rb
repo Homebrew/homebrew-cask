0fb05960a29ad9950ec3b870fa92b6f3351612a1
Vítor Galvão <info@vitorgalvao.com>
2019-03-17 16:04:03 +0000

    Update thedesk.rb

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,8 +1,9 @@
 cask 'thedesk' do
-  version :latest
-  sha256 :no_check
+  version '18.0.2'
+  sha256 'bb176ecf2b18a5bdc80cca5395e8c884c9a875409e4016d9668e279f25ef81b1'
 
   url 'https://dl.thedesk.top/TheDesk-darwin-x64.zip'
+  appcast 'https://thedesk.top/'
   name 'TheDesk'
   homepage 'https://thedesk.top/'
 
