991d713276e20330dee5e8a7663c1dc23e0527ed
suschizu <brigitte@corecode.io>
2019-03-17 02:16:57 +0100

    Update vmware-horizon-client to 4.10.0-11013656,CART19FQ4 (#60475)

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -3,6 +3,7 @@ cask 'vmware-horizon-client' do
   sha256 'beec3bb8a8a1f31c39ab502d3b718d151c7c30fd0b8c7618223804640e417dbd'
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
+  appcast 'https://docs.vmware.com/en/VMware-Horizon-Client-for-Mac/'
   name 'VMware Horizon Client'
   homepage 'https://www.vmware.com/'
 
