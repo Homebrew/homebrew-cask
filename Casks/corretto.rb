07a408b2b376a512faa5736f9223c6a996178d03
Anvar Karimson <anvar@karimson.com>
2019-03-17 14:09:47 +0000

    Upgrade Corretto to 11.0.2.9.3 (#60485)
    * Upgrade Corretto to 11.0.2.9.3
    
    * Update corretto.rb

--- Casks/corretto.rb
+++ Casks/corretto.rb
@@ -1,14 +1,14 @@
 cask 'corretto' do
-  version '8,8.202.08.2'
-  sha256 '0b79bbf6e4fa5f5ad75e49a36b52def428e160582476530b20a68837deaf0e58'
+  version '11.0.2.9.3'
+  sha256 'd25da98b91cc954bea3d9a2cb4f3cb296158e79445b88fdb32190b4964d09f5b'
 
-  # d2znqt9b1bc64u.cloudfront.net was verified as official when first introduced to the cask
-  url "https://d2znqt9b1bc64u.cloudfront.net/amazon-corretto-#{version.after_comma}-macosx-x64.pkg"
-  appcast 'https://docs.aws.amazon.com/en_us/corretto/latest/corretto-8-ug/corretto-8-ug.rss'
+  # d3pxv6yz143wms.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d3pxv6yz143wms.cloudfront.net/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
+  appcast "https://docs.aws.amazon.com/en_us/corretto/latest/corretto-#{version.major}-ug/corretto-#{version.major}-ug.rss"
   name 'Amazon Corretto'
   homepage 'https://aws.amazon.com/corretto/'
 
-  pkg "amazon-corretto-#{version.after_comma}-macosx-x64.pkg"
+  pkg "amazon-corretto-#{version}-macosx-x64.pkg"
 
-  uninstall pkgutil: "com.amazon.corretto.#{version.before_comma}"
+  uninstall pkgutil: "com.amazon.corretto.#{version.major}"
 end
