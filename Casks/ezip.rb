013936a622f85c5d94ba217af7cb5d88bd20ee7a
Christian Kraemer <christian.kraemer@gmail.com>
2019-03-17 14:39:39 +0100

    Update ezip to 1.7 (#60490)

--- Casks/ezip.rb
+++ Casks/ezip.rb
@@ -1,6 +1,6 @@
 cask 'ezip' do
-  version '1.6'
-  sha256 'b534ea00f2b094e8a420b4d4a4ed763c3e373969e149b370d532a79027dc9cc8'
+  version '1.7'
+  sha256 '04dab63c503dcadd5efb44b6f8f3023f13153d94cb1b7b136ef48f5219116de1'
 
   url "https://cdn.awehunt.com/ezip/release/eZip_V#{version}.dmg"
   appcast 'https://ezip.awehunt.com/'
