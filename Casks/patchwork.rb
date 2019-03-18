9843c6425d94baccde0ee4b9eebb6c934d05da74
Seb Flippence <144435+sebflipper@users.noreply.github.com>
2019-03-18 21:27:00 +0000

    Fixed sha256 for patchwork to 3.11.5 (#60537)

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,6 +1,6 @@
 cask 'patchwork' do
   version '3.11.5'
-  sha256 '1e66856dbfee4f5cb1c4b6d69b05a27bc195525e96fa386663572f6375557e70'
+  sha256 '8e47477aa9e3b920b67bbada35225c2f88acc805efea4e11618ab56ede1a42bf'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}-mac.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom'
