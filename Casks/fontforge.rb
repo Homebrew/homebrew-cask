91c0e97bc38da9542e1bbdae40abb17b8b233a60
Vítor Galvão <info@vitorgalvao.com>
2019-03-18 13:22:46 +0000

    Update fontforge.rb

--- Casks/fontforge.rb
+++ Casks/fontforge.rb
@@ -3,7 +3,7 @@ cask 'fontforge' do
   sha256 '1b9a04a478e81501f1dda46e0c4ecccf7c586b5cc617e6c7dffdd06e9e63b102'
 
   # github.com/fontforge/fontforge was verified as official when first introduced to the cask
-  url "https://github.com/fontforge/fontforge/releases/download/#{version.major_minor_patch.no_dots}/FontForge-#{version.dots_to_dashes}.app.dmg"
+  url "https://github.com/fontforge/fontforge/releases/download/#{version.major_minor_patch.no_dots}/FontForge-#{version.dots_to_hyphens}.app.dmg"
   appcast 'https://github.com/fontforge/fontforge/releases.atom'
   name 'FontForge'
   homepage 'https://fontforge.github.io/en-US/'
