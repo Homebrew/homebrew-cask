546f9f00998df98432058b912b962afa1b18ee88
suschizu <brigitte@corecode.io>
2019-03-18 09:52:50 +0100

    Update visual-paradigm-ce to 15.2,20190307 (#60516)

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm-ce' do
-  version '15.2,20190302'
-  sha256 'e2794299f04ac5e5783f30eae4c19abde50758d59ec613a3a5dbff02f541e80a'
+  version '15.2,20190307'
+  sha256 '077f84838b536b398f2198cacf155e15180cc6c43f898e518ca295e2a935e8be'
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html'
