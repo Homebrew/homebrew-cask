87ce657261ab08ccb81637a589481c1f76921ed1
Henrique Dias <hacdias@gmail.com>
2019-03-18 12:18:29 +0000

    Update IPFS desktop to 0.7.1 (#60520)

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,6 +1,6 @@
 cask 'ipfs' do
-  version '0.7.0'
-  sha256 '4cce1d57191abbbdee7f499e1a056d335eaa1861f0f1c16b227975c4f31772b7'
+  version '0.7.1'
+  sha256 'c6b8c195475fca6dce68bed22d56942983cf42474e74c9afdb8b4971cde74775'
 
   url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/ipfs-desktop-#{version}.dmg"
   appcast 'https://github.com/ipfs-shipyard/ipfs-desktop/releases.atom'
