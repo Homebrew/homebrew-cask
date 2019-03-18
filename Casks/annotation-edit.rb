757155fd8b1b5a282f90b51fff40a3263e2d0129
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-17 19:21:15 -0700

    Update annotation-edit to 1.9.99.16 (#60511)

--- Casks/annotation-edit.rb
+++ Casks/annotation-edit.rb
@@ -1,6 +1,6 @@
 cask 'annotation-edit' do
-  version '1.9.99.15'
-  sha256 'bbcf4149620391fa023b450a08d9d28485c322bc58db2f040674975728ddd5b6'
+  version '1.9.99.16'
+  sha256 'eacd4f0fe5ef40339dfe1cea5e265b8e6ac4b20d34de2da65ca72a215aeeb02a'
 
   url 'http://www.zeitanker.com/common/Annotation_Edit.zip'
   appcast 'http://zeitanker.com/updates.rss'
