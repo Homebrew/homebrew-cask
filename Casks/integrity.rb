b51a000e6fea521cd2aece05b39a3b4d82a74b4e
Ralph Martin <ralph@cs.cf.ac.uk>
2019-03-19 08:57:41 +0000

    Update integrity to 8.3.2 (#60550)

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -1,6 +1,6 @@
 cask 'integrity' do
-  version '8.2.3'
-  sha256 '5ff7124a07df90ed0262a52c93fd5e5d87c4b7e5587f502d28c262f5b24018f6'
+  version '8.3.2'
+  sha256 '4cbb79d6841cec967228172fa9198798e310f0bf87b223cad27ee1baff54f1f2'
 
   # peacockmedia.co.uk/integrity was verified as official when first introduced to the cask
   url 'http://peacockmedia.co.uk/integrity/integrity.dmg'
