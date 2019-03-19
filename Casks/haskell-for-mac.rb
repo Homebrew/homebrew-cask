919eab8401b28dc6c5b153280d52e7867fe75fe2
suschizu <brigitte@corecode.io>
2019-03-19 14:42:14 +0100

    Update haskell-for-mac.rb

--- Casks/haskell-for-mac.rb
+++ Casks/haskell-for-mac.rb
@@ -9,7 +9,7 @@ cask 'haskell-for-mac' do
   homepage 'http://haskellformac.com/'
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :high_sierra'
 
   app 'Haskell.app'
 
