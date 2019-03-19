c5efbe25763f0e9777cf234870c60843af4a8efc
Justin P <jpartain89@gmail.com>
2019-03-19 04:25:36 -0500

    Fix the Versioning as asked

--- Casks/fing.rb
+++ Casks/fing.rb
@@ -1,12 +1,12 @@
 cask 'fing' do
-  version '5.3.3-osX'
+  version '5.3.3'
   sha256 'c3ba4964fcec0a027893e4f31065f47868b82f226e1da82871d66ed580249cd3'
 
   url 'https://www.fing.com/images/uploads/general/CLI_macOSX.zip'
   name 'Fing'
   homepage 'https://www.fing.com/'
 
-  pkg "fing-#{version}.pkg"
+  pkg "fing-#{version}-osX.pkg"
 
   uninstall pkgutil: 'com.fing.Fing.plist'
 end
