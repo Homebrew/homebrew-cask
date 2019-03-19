74a96bcc423431db752e77d97171ce26f7e81b15
CoreCode <core-code@users.noreply.github.com>
2019-03-20 00:08:50 +0100

    Update toneprint.rb

--- Casks/toneprint.rb
+++ Casks/toneprint.rb
@@ -3,7 +3,7 @@ cask 'toneprint' do
   sha256 '013b181a44d16f9d2e477b7f325b31f6f20598e96a91fd8597c50fa9b6a6f692'
 
   # downloads.music-group.com was verified as official when first introduced to the cask
-  url "http://downloads.music-group.com/software/tcelectronic/TonePrintEditor/TonePrint-#{version}_Mac.zip"
+  url "https://downloads.music-group.com/software/tcelectronic/TonePrintEditor/TonePrint-#{version}_Mac.zip"
   appcast 'https://www.tcelectronic.com/p/P0CLC/Downloads'
   name 'TonePrint Editor'
   homepage 'https://www.tcelectronic.com/toneprint-editor/support/'
