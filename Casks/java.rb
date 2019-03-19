723b74af15ee8bb06b602268e313e59ad3e62cee
Chandan Pasunoori <chandanpasunoori@gmail.com>
2019-03-20 02:14:12 +0530

    Update java.rb

--- Casks/java.rb
+++ Casks/java.rb
@@ -1,12 +1,12 @@
 cask 'java' do
-  version '12'
+  version '12,33'
   sha256 '52164a04db4d3fdfe128cfc7b868bc4dae52d969f03d53ae9d4239fe783e1a3a'
 
-  url "https://download.java.net/java/GA/jdk#{version}/GPL/openjdk-#{version}_osx-x64_bin.tar.gz"
+  url "https://download.java.net/java/GA/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}_osx-x64_bin.tar.gz"
   name 'OpenJDK Java Development Kit'
   homepage 'https://jdk.java.net/'
 
-  artifact "jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version}.jdk"
+  artifact "jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
 
   uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 end
