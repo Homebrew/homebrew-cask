40992756bff7d90c750a2ac47080b3404e9d7640
CoreCode <core-code@users.noreply.github.com>
2019-03-20 17:56:31 +0100

    Update eclipse-cpp to 4.11.0,2019-03:R (#60638)

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-cpp' do
-  version '4.10.0,2018-12:R'
-  sha256 '3d8e947d9c464c12d24cb9c1ccc2fe2e46e0227bb091129427f64224df2e9542'
+  version '4.11.0,2019-03:R'
+  sha256 'ca95ee6c442b0d43e9fa03374a869b335ba1f14c113f7b08e59bde66f7700c5b'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-cpp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for C/C++ Developers'
