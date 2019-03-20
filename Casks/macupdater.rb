b975ea16f6f024454b8a5c262e5e5976593f4489
CoreCode <core-code@users.noreply.github.com>
2019-03-21 00:08:13 +0100

    Update macupdater from 1.4.5 to 1.4.7 (#60666)
    * Update macupdater from 1.4.5 to 1.4.7
    
    * Update macupdater.rb

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -1,9 +1,9 @@
 cask 'macupdater' do
-  version '1.4.5'
-  sha256 '76ee48d25d86e53db370516f92c0b96ca29b9899b3bda60a3b8023d4158d7ad9'
+  version '1.4.7'
+  sha256 '82793a06ff6aca03ede5233f012cd98c525b0c4e5c8b32283f090dba9b5be1bc'
 
   url "https://www.corecode.io/downloads/macupdater_#{version}.zip"
-  appcast 'https://macupdater.net/macupdater/macupdater.xml'
+  appcast 'https://www.corecode.io/macupdater/macupdater.xml'
   name 'MacUpdater'
   homepage 'https://www.corecode.io/macupdater/index.html'
 
