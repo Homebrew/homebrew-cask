ec6d221a3453f281e1a9ba540f932b9866975820
Randall <ran.dall@icloud.com>
2019-03-20 10:26:37 -0700

    Update patchwork to 3.11.6

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,6 +1,6 @@
 cask 'patchwork' do
-  version '3.11.5'
-  sha256 '8e47477aa9e3b920b67bbada35225c2f88acc805efea4e11618ab56ede1a42bf'
+  version '3.11.6'
+  sha256 '6c085e4ba2146e7e379b3bedfb92c6ce81192ab9866ec0235d2efdfe9a724aeb'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}-mac.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom'
