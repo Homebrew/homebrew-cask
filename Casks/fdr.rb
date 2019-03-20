162c978bfa04a419bcaeb694fa3a763706100aea
Vítor Galvão <info@vitorgalvao.com>
2019-03-20 12:12:00 +0000

    Update fdr.rb

--- Casks/fdr.rb
+++ Casks/fdr.rb
@@ -1,8 +1,8 @@
 cask 'fdr' do
-  version '4.2.3'
+  version '4.2.3,3789'
   sha256 'c9a30591cd0f28cab68de59691c2ebe717134daa38eaac5803b19d0c086f123c'
 
-  url 'https://www.cs.ox.ac.uk/projects/fdr/downloads/fdr-3789-mac-x86_64.zip'
+  url "https://www.cs.ox.ac.uk/projects/fdr/downloads/fdr-#{version.after_comma}-mac-x86_64.zip"
   appcast 'https://www.cs.ox.ac.uk/projects/fdr/manual/changes.html'
   name 'FDR'
   homepage 'https://www.cs.ox.ac.uk/projects/fdr/'
