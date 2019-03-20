009db5e34836023b207351539c8ce6b5058921c7
Stewart Allen <allenstew@gmail.com>
2019-03-20 11:22:18 -0700

    Add corretto8 cask for Amazon Corretto 8
    The corretto cask tracks the latest version (version 11) of Amazon
    Corretto. This package allows users to install version 8.

---
+++ Casks/corretto8.rb
@@ -0,0 +1,14 @@
+cask 'corretto8' do
+  version '8,8.202.08.2'
+  sha256 '0b79bbf6e4fa5f5ad75e49a36b52def428e160582476530b20a68837deaf0e58'
+
+  # d2znqt9b1bc64u.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d2znqt9b1bc64u.cloudfront.net/amazon-corretto-#{version.after_comma}-macosx-x64.pkg"
+  appcast "https://docs.aws.amazon.com/en_us/corretto/latest/corretto-#{version.major}-ug/corretto-#{version.major}-ug.rss"
+  name 'Amazon Corretto'
+  homepage 'https://aws.amazon.com/corretto/'
+
+  pkg "amazon-corretto-#{version.after_comma}-macosx-x64.pkg"
+
+  uninstall pkgutil: "com.amazon.corretto.#{version.before_comma}"
+end
