e4deb86cd7bea402a3ebb0d452153f547811507d
CoreCode <core-code@users.noreply.github.com>
2019-03-20 21:50:05 +0100

    Update macupdater.rb

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -3,7 +3,7 @@ cask 'macupdater' do
   sha256 '82793a06ff6aca03ede5233f012cd98c525b0c4e5c8b32283f090dba9b5be1bc'
 
   url "https://www.corecode.io/downloads/macupdater_#{version}.zip"
-  appcast 'https://macupdater.net/macupdater/macupdater.xml'
+  appcast 'https://www.corecode.io/macupdater/macupdater.xml'
   name 'MacUpdater'
   homepage 'https://www.corecode.io/macupdater/index.html'
 
