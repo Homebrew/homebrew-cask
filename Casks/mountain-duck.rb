4dca88b5b9c5883f2b0dfe7a9729ce00a21e98fb
Kunanon Srisuntiroj <son9912@gmail.com>
2019-03-20 15:04:41 +0700

    Update Mountain Duck to 2.7.2.9873
    Update mountain to the latest version of Mountain Duck

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,6 +1,6 @@
 cask 'mountain-duck' do
-  version '2.7.1.9840'
-  sha256 'fac3937d56ca3c093b24fc94958b162dc18205322a8624d0f2258007e3735b48'
+  version '2.7.2.9873'
+  sha256 '24e68cd958de9932cd107fc14f192fa2c89ed926ba1b9cb3a93f433007644afb'
 
   url "https://dist.mountainduck.io/Mountain%20Duck-#{version}.zip"
   appcast 'https://version.mountainduck.io/changelog.rss'
