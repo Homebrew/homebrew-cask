f0f4aa0ebfcb5b34e5775d1c20c5217540b48d83
Julian Mayer <julian@corecode.io>
2019-03-20 17:49:34 +0100

    Update eclipse-ptp to 4.11.0,2019-03:R

--- Casks/eclipse-ptp.rb
+++ Casks/eclipse-ptp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-ptp' do
-  version '4.10.0,2018-12:R'
-  sha256 '923309efc439fd422d5a44f7bba9713c57eb9738f389d2e770cce18360719270'
+  version '4.11.0,2019-03:R'
+  sha256 'dae4fb9ed0ca62a9ffd520181b123f77bddf9a024eab45598563e4339e4bc7b0'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-parallel-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Parallel Application Developers'
