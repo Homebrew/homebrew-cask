6fc94002eed46815a8fbf347eb1140a778b9046b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-03-20 18:17:56 -0700

    Update udeler to 1.6.3 (#60688)

--- Casks/udeler.rb
+++ Casks/udeler.rb
@@ -1,6 +1,6 @@
 cask 'udeler' do
-  version '1.6.2'
-  sha256 'd7eb6ca284f753cbb38ab12b260f9a44fc375485c74a6de6af3f681047c19cad'
+  version '1.6.3'
+  sha256 '8cf08ad8c2045b8f5b6fce27b2bfed4169f7a5ac75b4e67c5c2eec7a0b23e24e'
 
   url "https://github.com/FaisalUmair/udemy-downloader-gui/releases/download/v#{version}/Udeler-#{version}-mac.zip"
   appcast 'https://github.com/FaisalUmair/udemy-downloader-gui/releases.atom'
