fb9977d6ed8826d66b158e884ffab55c61b045fd
ivanxfjiang(姜希凡) <ivanxfjiang@tencent.com>
2019-03-21 20:12:26 +0800

    Update url of TBS_Studio 1.3.3 to https

--- Casks/tbs-studio.rb
+++ Casks/tbs-studio.rb
@@ -3,7 +3,7 @@ cask 'tbs-studio' do
   sha256 '3b24b26556cb65fbf24da29e060a8f89ea3c36b1f0a973a54c523bccc034832c'
 
   # mcaredown.3g.qq.com/browser was verified as official when first introduced to the cask
-  url 'http://mcaredown.3g.qq.com/browser/tes/TBS_Studio_mac_v1.3.3_2017112800.dmg.zip'
+  url 'https://mcaredown.3g.qq.com/browser/tes/TBS_Studio_mac_v1.3.3_2017112800.dmg.zip'
   name 'TBS Studio'
   homepage 'https://x5.tencent.com/tbs/guide/debug.html'
 
