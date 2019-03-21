ff9bb0846495eff148eb8ab17e81b77229d93b2b
CoreCode <core-code@users.noreply.github.com>
2019-03-21 20:17:27 +0100

    Update eclipse-platform to 4.11.0,2019-03:R (#60648)
    * Update eclipse-platform to 4.11.0,2019-03:R
    
    * Update eclipse-platform.rb
    
    * Update eclipse-platform.rb
    
    * Update eclipse-platform.rb

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-platform' do
-  version '4.10,201812060815'
-  sha256 '8bee330659ae3464a8ae74dbc0d5221421af1380d207ceed9d6afe52337da80f'
+  version '4.11,201903070500'
+  sha256 'b841c5878def4c191f566925b2655838c6103541e74e09095a94ac6a550445d0'
 
   url "https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.major}/R-#{version.before_comma}-#{version.after_comma}/eclipse-SDK-#{version.before_comma}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse SDK'
