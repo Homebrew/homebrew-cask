12d4b331279f1d6f5a15ed02a26996d923abeb34
Bruce <yyycode@163.com>
2019-03-21 15:51:25 +0800

    Use https

--- Casks/sunlogin-control.rb
+++ Casks/sunlogin-control.rb
@@ -2,7 +2,7 @@ cask 'sunlogin-control' do
   version '3.0'
   sha256 '87ff12a9192ed809c2c8668e63cf44b1101544e52f43e8cc912ed2b20acf077f'
 
-  url "http://download.oray.com/sunlogin/mac/SunloginControl#{version}.dmg"
+  url "https://download.oray.com/sunlogin/mac/SunloginControl#{version}.dmg"
   name 'SunloginControl'
   name '向日葵控制端'
   homepage 'https://sunlogin.oray.com/'
