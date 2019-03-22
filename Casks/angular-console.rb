d516d9a462482f2f6458ea5799c72df863f40299
brigitte zmoelnig <brigitte@corecode.io>
2019-03-23 00:07:14 +0100

    Update angular-console to 7.4.3

--- Casks/angular-console.rb
+++ Casks/angular-console.rb
@@ -1,6 +1,6 @@
 cask 'angular-console' do
-  version '7.4.0'
-  sha256 '3b69210db89e1457c437d733ee7d7c003084fe829f68f54d4f7d05ea5d5b1ac8'
+  version '7.4.3'
+  sha256 '4aab003bf60e043fc4b81e32d1cef84635d9852fa7eee5397818296d27677b36'
 
   # github.com/nrwl/angular-console was verified as official when first introduced to the cask
   url "https://github.com/nrwl/angular-console/releases/download/v#{version}/AngularConsole-#{version}.dmg"
