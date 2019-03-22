d1a9cce1d95c89bd3770d325ce6ba99741e6206f
suschizu <brigitte@corecode.io>
2019-03-23 00:23:02 +0100

    Update lbry to 0.30.0 (#60771)

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,6 +1,6 @@
 cask 'lbry' do
-  version '0.29.4'
-  sha256 '87e802fd9cb1ea175afad5479c0b24c5ab2904424dde3e084f5f9e083921da77'
+  version '0.30.0'
+  sha256 '96cd3c5b269eb43290aa40d699d756a1c9f316fadf5fe53caed01de2e7e2e5a1'
 
   # github.com/lbryio/lbry-app was verified as official when first introduced to the cask
   url "https://github.com/lbryio/lbry-app/releases/download/v#{version}/LBRY_#{version}.dmg"
