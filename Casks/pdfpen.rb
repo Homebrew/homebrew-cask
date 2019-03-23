01a18c66b1dba5c2fc7b368b47bf614d6f680af9
ahousseini <admin@admin-3.local>
2019-03-23 08:22:07 +0100

    Update pdfpen to 1023.3,1553249640

--- Casks/pdfpen.rb
+++ Casks/pdfpen.rb
@@ -1,6 +1,6 @@
 cask 'pdfpen' do
-  version '1022.6,1551314434'
-  sha256 '164ff02581ba8b94cd244d15cf748cba56af73990a8e66807cbae1a51eba8538'
+  version '1023.3,1553249640'
+  sha256 'bcc4dfdf0b77aedaf0a291eae5192d0e8fca30cf10c5640f75bf4f398b123fef'
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpen/#{version.before_comma}/#{version.after_comma}/PDFpen-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpen.xml'
