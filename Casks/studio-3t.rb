e2d7f49097e9dee80344f9f0e4b247b300f83635
Alexandre Alonso <alex4551@gmail.com>
2019-03-23 19:36:51 +0800

    update studio-3t 2019.2.0 (#60744)

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,11 +1,13 @@
 cask 'studio-3t' do
-  version '2019.1.0'
-  sha256 'a5ad2439faf84f466bda1f86b366528d90621b9d5fdb16db6f0acd3b3697dfeb'
+  version '2019.2.0'
+  sha256 '30c0e1ef745f9e2c0e4a6530c6de72917a00df0bc26d6edda47d7f84ddd59187'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'https://files.studio3t.com/changelog/changelog.txt'
   name 'Studio 3T'
   homepage 'https://studio3t.com/'
+
+  auto_updates true
 
   app 'Studio 3T.app'
 end
