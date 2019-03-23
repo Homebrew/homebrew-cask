b525ecd2916e17827205d9ebf808b6dd0da667af
suschizu <brigitte@corecode.io>
2019-03-23 18:23:10 +0100

    Update ctivo.rb

--- Casks/ctivo.rb
+++ Casks/ctivo.rb
@@ -1,11 +1,11 @@
 cask 'ctivo' do
-  version '3.1.2'
-  sha256 'a428fadb9fcef043305023c771796ef210c3d12d22557894eb6b400647afa311'
+  version '3.3.0'
+  sha256 '67a75ea8449d593c6cb240a0acbc20e3bdef743651eda419eb38558a6a74f2ab'
 
-  url "https://github.com/dscottbuch/cTiVo/releases/download/#{version}/cTiVo.zip"
-  appcast 'https://github.com/dscottbuch/cTiVo/releases.atom'
+  url "https://github.com/mackworth/cTiVo/releases/download/#{version}/cTiVo.app.zip"
+  appcast 'https://github.com/mackworth/cTiVo/releases.atom'
   name 'cTiVo'
-  homepage 'https://github.com/dscottbuch/cTiVo'
+  homepage 'https://github.com/mackworth/cTiVo'
 
   app 'cTiVo.app'
 end
