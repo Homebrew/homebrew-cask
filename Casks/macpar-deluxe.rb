14c5ba2548cff865096273e6a7ddebb7643b8b20
suschizu <brigitte@corecode.io>
2019-03-25 16:29:20 +0100

    Update macpar-deluxe.rb

--- Casks/macpar-deluxe.rb
+++ Casks/macpar-deluxe.rb
@@ -3,6 +3,7 @@ cask 'macpar-deluxe' do
   sha256 '14799ac70f508d4a9fe65b8bdee6e4a9c3f227672d3804454c15e03bd0912249'
 
   url 'https://www.xs4all.nl/~gp/MacPAR_deLuxe/MacPARdeLuxe.dmg'
+      user_agent: :fake
   appcast 'https://gp.home.xs4all.nl/Site/MacPAR_deLuxe.html'
   name 'MacPAR deLuxe'
   homepage 'https://gp.home.xs4all.nl/Site/MacPAR_deLuxe.html'
