ad8f8348ed15586de078e38f36bbeead0ce82f27
Mario Kozjak <kozjakm1@gmail.com>
2019-03-25 13:55:26 +0100

    Update keepassxc to 2.4.0

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,6 +1,6 @@
 cask 'keepassxc' do
-  version '2.3.4'
-  sha256 '59d7530625866c3d7d5cfa753e12dee0f052f79e1a7572f8e5633ad915369228'
+  version '2.4.0'
+  sha256 'b611ada8bd6bc9fe93e8566b042ceaa3a9c2f1e8c5c2ddbefb866ce20a39254e'
 
   # github.com/keepassxreboot/keepassxc was verified as official when first introduced to the cask
   url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg"
