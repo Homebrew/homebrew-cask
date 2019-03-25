155daeee4b123df017ce5782f456609a7be999a7
Clayton Wong <clayton.wong@ookla.com>
2019-03-25 12:40:30 -0700

    add testing-1.0.0

---
+++ Casks/testing.rb
@@ -0,0 +1,11 @@
+cask 'testing' do
+  version '1.0.0'
+  sha256 '0cd1857999ed9f33a0041187dfde82cfa7b9995f1661128ef0e53255c3ddfdff'
+
+  # github.com/claytonjwong/cask was verified as official when first introduced to the cask
+  url 'https://github.com/claytonjwong/cask/raw/master/testing.tgz'
+  name 'testing'
+  homepage 'https://www.testing.com/'
+
+  binary 'testing'
+end
