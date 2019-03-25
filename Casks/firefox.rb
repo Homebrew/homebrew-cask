56b1efee03f610b3528b029ed3d4c37690f99690
Vítor Galvão <info@vitorgalvao.com>
2019-03-25 13:32:18 +0000

    Update firefox.rb

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -128,7 +128,7 @@ cask 'firefox' do
 
   # download-installer.cdn.mozilla.net/pub/firefox/releases was verified as official when first introduced to the cask
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/mac/#{language}/Firefox%20#{version}.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.mozilla.org/%3Fproduct=firefox-latest-ssl%26os=osx'
+  appcast 'https://www.mozilla.org/en-US/firefox/releases/'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/firefox/'
 
