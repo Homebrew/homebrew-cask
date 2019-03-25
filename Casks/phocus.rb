9d6fb3e3fd579012675c47ef4b3c59690d3be570
Work <brigitte@corecode.io>
2019-03-25 13:22:45 +0100

    Update phocus to 3.4.2

--- Casks/phocus.rb
+++ Casks/phocus.rb
@@ -1,6 +1,6 @@
 cask 'phocus' do
-  version '3.4.1'
-  sha256 'b10502e5a3e671b1c3ba377b26b27cf4b0eb1285b168ba7a754ba59d6120187d'
+  version '3.4.2'
+  sha256 '79ad542b8888b67c51a23222ba04cff75934a7bc109bc947775a2c3b567f94de'
 
   url "https://cdn.hasselblad.com/phocus/Phocus.#{version}.dmg"
   name 'Hasselblad Phocus'
