5c098fb5c6bc14545db9c3fd474915373e0a84f9
Work <brigitte@corecode.io>
2019-03-25 18:15:26 +0100

    Update sqlitemanager to 4.8.3

--- Casks/sqlitemanager.rb
+++ Casks/sqlitemanager.rb
@@ -3,7 +3,7 @@ cask 'sqlitemanager' do
   sha256 '0e0e99490f45e6145efde46ddc32dd1e131248b024824254579dc7985426793e'
 
   url 'https://www.sqlabs.com/download/SQLiteManager.zip'
-  appcast 'https://www.sqlabs.com/news/sqlitemanager/'
+  appcast 'https://www.sqlabs.com/sqlitemanager_version'
   name 'SQLiteManager'
   homepage 'https://www.sqlabs.com/sqlitemanager.php'
 
