7d5497c8e75d689606f625f1ef73595265d1b1e2
suschizu <brigitte@corecode.io>
2019-03-25 13:01:02 +0100

    Update vorta to 0.6.14 (#60832)

--- Casks/vorta.rb
+++ Casks/vorta.rb
@@ -1,6 +1,6 @@
 cask 'vorta' do
-  version '0.6.13'
-  sha256 'e1798294d863dde328d0fb312eab311e717bf48205866fafa1ab1a751bbb9f97'
+  version '0.6.14'
+  sha256 'd3e220bc2ad7f7df517357ff1ce7617d821dd6a90fd92ca2e1d66f98dcd3313f'
 
   url "https://github.com/borgbase/vorta/releases/download/v#{version}/vorta-#{version}.dmg"
   appcast 'https://github.com/borgbase/vorta/releases.atom'
