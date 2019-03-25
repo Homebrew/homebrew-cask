c6079f0baf86baa790c617b01350d5a9210257eb
SergioOS <ortega@tune-is.de>
2019-03-25 17:51:16 +0100

    Update grandperspective to 2.2.0

--- Casks/grandperspective.rb
+++ Casks/grandperspective.rb
@@ -1,6 +1,6 @@
 cask 'grandperspective' do
-  version '2.1.2'
-  sha256 'f3a247a3123a904f200e99708ca7ecbc32995a0715b51cb661bdec67bf88a094'
+  version '2.2.0'
+  sha256 'debf6e069338793f8f4739b8c27e2bc9b00ff4efea365e50ad87ecf845e06532'
 
   # downloads.sourceforge.net/grandperspectiv was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/grandperspectiv/grandperspective/#{version}/GrandPerspective-#{version.dots_to_underscores}.dmg"
