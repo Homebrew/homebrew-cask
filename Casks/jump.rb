362e17dc7c820b7dcd3b20846d7dee4129a13d74
Vítor Galvão <info@vitorgalvao.com>
2019-03-25 18:30:13 +0000

    Update jump.rb

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -2,7 +2,7 @@ cask 'jump' do
   version '8.2.17'
   sha256 '2a68bc68c2f2dd6c1b506b1ffc2346510992714b03b34950e4dc0f3f71eecd52'
 
-  url 'https://mirror.jumpdesktop.com/downloads/JumpDesktopMac-8.2.17.zip'
+  url "https://mirror.jumpdesktop.com/downloads/JumpDesktopMac-#{version}.zip"
   appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml'
   name 'Jump Desktop'
   homepage 'https://jumpdesktop.com/#jdmac'
