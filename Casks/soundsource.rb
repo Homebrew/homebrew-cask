50b98486d3304524a7cd60dc723b68282890ee6f
Chris Finazzo <chris.finazzo@gmail.com>
2019-03-26 14:07:36 -0400

    Update Soundsource to 4.0.0 (#60896)

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,13 +1,13 @@
 cask 'soundsource' do
-  version '3.1.2'
-  sha256 'b6f4aa22f70519f5e504a7d9d956e649f78522c735d4ea8224171b9b487f6139'
+  version '4.0.0'
+  sha256 '1a5bc1dc714e8cec80aebf92d14b200fc094e8066ca5bf49b8a41a6a37788ae5'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
   appcast 'https://rogueamoeba.com/soundsource/releasenotes.php'
   name 'SoundSource'
   homepage 'https://rogueamoeba.com/soundsource/'
 
-  depends_on macos: '>= :yosemite'
+  depends_on macos: '>= :el_capitan'
 
   app 'SoundSource.app'
 end
