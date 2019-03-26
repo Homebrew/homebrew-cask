4c16fb4761ce8e96fc30f3f7ef100a3e9c86d907
Anver Housseini <contact@anver.ch>
2019-03-26 19:02:46 +0100

    Update intellij-idea to 2018.3.6,183.6156.11 (#60903)

--- Casks/intellij-idea.rb
+++ Casks/intellij-idea.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea' do
-  version '2018.3.5,183.5912.21'
-  sha256 'c9053534d536bd3b59eb51d7acb7b23ae06b447736754d3c287736899c67bf8f'
+  version '2018.3.6,183.6156.11'
+  sha256 '2ae152de3933cae002b50d13735ec0cf7cbad23918ce3819640a3c7c14079e61'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release'
