4e13e122137e349d9d6b9ebba86ff416a18c7c2e
Alberto Sottile <alby128@gmail.com>
2019-03-27 09:13:25 +0100

    syncplay 1.6.3: add legacy version

--- Casks/syncplay.rb
+++ Casks/syncplay.rb
@@ -1,14 +1,18 @@
 cask 'syncplay' do
   version '1.6.3'
-  sha256 '27cc76a66a2a1c79d2e46ad6568ed8678b83f00d80a3ec7db5527774f2223caf'
+
+  if MacOS.version <= :el_capitan
+    url "https://github.com/Syncplay/syncplay/releases/download/v#{version}/Syncplay_#{version}_macOS_legacy.dmg"
+    sha256 'e81737848d3d03716daa813e6a47431f14f88db694a4ac5e341ea52103b4dea6'
+  else
+    url "https://github.com/Syncplay/syncplay/releases/download/v#{version}/Syncplay_#{version}.dmg"
+    sha256 '27cc76a66a2a1c79d2e46ad6568ed8678b83f00d80a3ec7db5527774f2223caf'
+  end
 
   # github.com/Syncplay/syncplay was verified as official when first introduced to the cask
-  url "https://github.com/Syncplay/syncplay/releases/download/v#{version}/Syncplay_#{version}.dmg"
   appcast 'https://github.com/Syncplay/syncplay/releases.atom'
   name 'Syncplay'
   homepage 'https://syncplay.pl/'
-
-  depends_on macos: '>= :sierra'
 
   app 'Syncplay.app'
 
@@ -20,5 +24,6 @@ cask 'syncplay' do
                '~/Library/Preferences/com.syncplay.MainWindow.plist',
                '~/Library/Preferences/pl.syncplay.Syncplay.plist',
                '~/Library/Preferences/com.syncplay.PlayerList.plist',
+               '~/Library/Application Support/org.videolan.vlc/lua/intf/syncplay.lua',
              ]
 end
