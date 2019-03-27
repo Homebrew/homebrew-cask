f3e7bfd832f92f5409d9b6b76062fbd083fd3d0f
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2019-03-27 17:45:12 +0100

    Fix url

--- Casks/cinebench.rb
+++ Casks/cinebench.rb
@@ -1,8 +1,8 @@
 cask 'cinebench' do
-  version 'R20.040'
+  version 'R20'
   sha256 '415eb2d477bbdd4a1228f16ed01cc0374b21624c5a54a0179f0ffc9c92cd2c8b'
 
-  url "https://http.maxon.net/pub/cinebench/Cinebench#{version.major}.dmg"
+  url "https://http.maxon.net/pub/cinebench/Cinebench#{version}.dmg"
   name 'Cinebench'
   homepage 'https://www.maxon.net/products/cinebench/'
 
