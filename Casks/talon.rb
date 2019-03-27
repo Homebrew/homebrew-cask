adda982663fc81df6fd1a211f2598a53a6db88a9
suschizu <brigitte@corecode.io>
2019-03-27 22:27:56 +0100

    Update talon from 89-0.0.8.29 to 98-0.0.8.38 (#60984)

--- Casks/talon.rb
+++ Casks/talon.rb
@@ -1,6 +1,6 @@
 cask 'talon' do
-  version '89-0.0.8.29'
-  sha256 '87d49ff38693872394f17dc9413e00de2c82693c6eaf0c44a45c8740c86748b2'
+  version '98-0.0.8.38'
+  sha256 '45de05c79f7119785cf01535521e2872ed0c409018a32453804db90c6e707368'
 
   url "https://talonvoice.com/update/nmi5s3faoq6NzROd2dbCRg/Talon-#{version}.dmg"
   appcast 'https://talonvoice.com/'
