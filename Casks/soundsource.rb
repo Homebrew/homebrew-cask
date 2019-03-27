fa424172a2f1e0365b5820e68fb4ef57f43397ea
Grettir Asmundarson <30250+grettir@users.noreply.github.com>
2019-03-26 23:05:35 -0600

    Update soundsource (#60926)

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,6 +1,6 @@
 cask 'soundsource' do
   version '4.0.0'
-  sha256 '1a5bc1dc714e8cec80aebf92d14b200fc094e8066ca5bf49b8a41a6a37788ae5'
+  sha256 '7a336135afb6b53f00ce898838281212b935631fd958bfc93facc239c3601889'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
   appcast 'https://rogueamoeba.com/soundsource/releasenotes.php'
