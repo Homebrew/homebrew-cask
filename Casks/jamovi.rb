97064de8066a671407abe251606b32ab85f82b2c
suschizu <brigitte@corecode.io>
2019-03-27 20:35:35 +0100

    Update jamovi from 0.9.6.3 to 0.9.6.4 (#60973)

--- Casks/jamovi.rb
+++ Casks/jamovi.rb
@@ -1,6 +1,6 @@
 cask 'jamovi' do
-  version '0.9.6.3'
-  sha256 '5f8f161e8a82db36ac42f838609caa0cbaf7b9b801a3088d553436b113c0d136'
+  version '0.9.6.4'
+  sha256 '741df258d78eef11d5ed11f4d8a92912426df3570994dc5a321ce6bed1b7526f'
 
   url "https://www.jamovi.org/downloads/jamovi-#{version}-macos.dmg"
   appcast 'https://www.jamovi.org/download.html'
