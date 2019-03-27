081687f95d9691faf1bcea1e82d48f24d6b1f9d5
Vítor Galvão <info@vitorgalvao.com>
2019-03-27 15:07:47 +0000

    Update visual-studio.rb

--- Casks/visual-studio.rb
+++ Casks/visual-studio.rb
@@ -1,28 +1,28 @@
-cask "visual-studio" do
-  version "7.8.2.1"
-  sha256 "51f1b1d1ed1582d0f4922963e07f5e08a6f1c593165dbe136d18ed73e6247abf"
+cask 'visual-studio' do
+  version '7.8.2.1'
+  sha256 '51f1b1d1ed1582d0f4922963e07f5e08a6f1c593165dbe136d18ed73e6247abf'
 
   # dl.xamarin.com/VsMac was verified as official when first introduced to the cask
   url "https://dl.xamarin.com/VsMac/VisualStudioForMac-#{version}.dmg"
-  appcast "https://docs.microsoft.com/en-us/visualstudio/releasenotes/vs2017-mac-relnotes"
-  name "Visual Studio for Mac"
-  homepage "https://www.visualstudio.com/vs/visual-studio-mac/"
+  appcast 'https://docs.microsoft.com/en-us/visualstudio/releasenotes/vs2017-mac-relnotes'
+  name 'Visual Studio for Mac'
+  homepage 'https://www.visualstudio.com/vs/visual-studio-mac/'
 
   auto_updates true
-  depends_on :cask => "cask-versions/mono-mdk-visual-studio"
+  depends_on cask: 'homebrew/cask-versions/mono-mdk-visual-studio'
 
-  app "Visual Studio.app"
+  app 'Visual Studio.app'
 
-  zap :trash => [
-    "/Applications/Xamarin Workbooks.app",
-    "/Applications/Xamarin Profiler.app",
-    "~/Library/Application Support/VisualStudio",
-    "~/Library/Application Support/CrashReporter/VisualStudio*",
-    "~/Library/Caches/VisualStudio",
-    "~/Library/Logs/VisualStudio",
-    "~/Library/Preferences/Visual*Studio",
-    "~/Library/Preferences/Xamarin",
-    "~/Library/Developer/Xamarin",
-    "~/Library/VisualStudio",
-  ]
+  zap trash: [
+               '/Applications/Xamarin Workbooks.app',
+               '/Applications/Xamarin Profiler.app',
+               '~/Library/Application Support/VisualStudio',
+               '~/Library/Application Support/CrashReporter/VisualStudio*',
+               '~/Library/Caches/VisualStudio',
+               '~/Library/Logs/VisualStudio',
+               '~/Library/Preferences/Visual*Studio',
+               '~/Library/Preferences/Xamarin',
+               '~/Library/Developer/Xamarin',
+               '~/Library/VisualStudio',
+             ]
 end
