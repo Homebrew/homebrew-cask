c02eebe35ea14bc46ef6cdfba291e71fcf6da149
Work <brigitte@corecode.io>
2019-03-27 19:39:40 +0100

    Update pomodone from 1.5.1527 to 1.5.1529

--- Casks/pomodone.rb
+++ Casks/pomodone.rb
@@ -1,6 +1,6 @@
 cask 'pomodone' do
-  version '1.5.1527'
-  sha256 '66a1f74b77b4d71227e6bfab953ccd5b90b70720965460de1b62c22de59124c7'
+  version '1.5.1529'
+  sha256 '92f8920226dc5fe1ef85b6875c120c535991e88346f0f3f48a65bfd0d84afc1a'
 
   url "https://app.pomodoneapp.com/installers/PomoDoneApp-#{version}.dmg"
   appcast 'https://pomodoneapp.com/download-pomodone-app.html'
