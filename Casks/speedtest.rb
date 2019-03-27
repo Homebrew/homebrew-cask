5654bc910a6046611cb474779b31b79cd3aedc7b
Clayton Wong <clayton.wong@ookla.com>
2019-03-27 16:59:44 -0700

    add speedtest-1.0.0

--- Casks/speedtest.rb
+++ Casks/speedtest.rb
@@ -3,7 +3,7 @@ cask 'speedtest' do
   sha256 'ff824d74042501c81e5920eb0acce0fd70910b0be50a3e375946e3a0e48ba2b1'
 
   # github.com/claytonjwong/cli was verified as official when first introduced to the cask
-  url 'https://github.com/claytonjwong/cli/raw/master/ookla-speedtest-1.0.0.35-devel.8c90f04-macosx.tgz'
+  url 'https://github.com/claytonjwong/cli/raw/master/ookla-speedtest-1.0.0.35-devel.de13d22-macosx.tgz'
   name 'Speedtest'
   homepage 'https://www.speedtest.net/'
 
