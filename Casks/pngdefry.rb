6109cab615d69ee04931612ef091a04026af2980
stephrost <smfrost1023@gmail.com>
2019-03-28 15:55:00 -0400

    * Add pngdefry Latest
    * Update pngdefry

---
+++ Casks/pngdefry.rb
@@ -0,0 +1,10 @@
+cask 'pngdefry' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.jongware.com/binaries/pngdefry.zip'
+  name 'pngdefry'
+  homepage 'http://www.jongware.com/pngdefry.html'
+
+  binary 'bin/pngdefry'
+end
