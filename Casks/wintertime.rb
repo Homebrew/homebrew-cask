76bc2b586b367619abbbac32904929f400a5bd76
Mentor Palokaj <mentor@palokaj.co>
2019-03-28 14:30:24 +0100

    Wintertime 0.0.7 (#61011)

--- Casks/wintertime.rb
+++ Casks/wintertime.rb
@@ -1,6 +1,6 @@
 cask 'wintertime' do
-  version '0.0.5'
-  sha256 '2c71c3f4b5939f69a8932ff4c9360a47eff96051140c15adaf26c1203ca0902e'
+  version '0.0.7'
+  sha256 'b7cb5b25172e3450982d673533931e721e009677e711fffa320b5e42abee3ff3'
 
   url "https://github.com/actuallymentor/wintertime-mac-background-freezer/releases/download/#{version}/Wintertime-#{version}.dmg"
   appcast 'https://github.com/actuallymentor/wintertime-mac-background-freezer/releases.atom'
