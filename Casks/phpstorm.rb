e98fd229dc1cfb0ccda47490b959f011dc308e4e
jcbot <jcb@leipert.io>
2019-03-28 14:07:18 +0100

    Update phpstorm to 2019.1,191.6183.95 (#61004)

--- Casks/phpstorm.rb
+++ Casks/phpstorm.rb
@@ -1,6 +1,6 @@
 cask 'phpstorm' do
-  version '2018.3.5,183.5912.26'
-  sha256 '00764bd3f9fcb2694c3691292f4d323c3575e9057b33cd17cd36c532dc43a27c'
+  version '2019.1,191.6183.95'
+  sha256 '858d7f87787f53822406acba75fc655f78f2e03b7b54405e65ab2538244ddf21'
 
   url "https://download.jetbrains.com/webide/PhpStorm-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=PS&latest=true&type=release'
