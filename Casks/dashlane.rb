6bf1b41be9495ead4f1198a246db799e4e36218b
Julian Mayer <julian@corecode.io>
2019-03-28 15:51:17 +0100

    Update dashlane from 6.1911.1.18938 to 6.1911.2.19115

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,8 +1,8 @@
 cask 'dashlane' do
-  version '6.1911.1.18938'
-  sha256 'c5c716a766f4301e23ec45c6f6dc155526e7eb52e9d261dc6f33c92d87cc7d5a'
+  version '6.1911.2.19115'
+  sha256 'a96fcd842e3a5b0a195f6ed236ac55384786e292f5cd51e74d6529b9614c29e5'
 
-  url "https://cdn5.dashlane.com/proxy/d3mfqat9ni8wb5/releases/#{version.major_minor_patch}/#{version}/release/Dashlane.dmg"
+  url "https://cdn5.dashlane.com/proxy/d3mfqat9ni8wb5/releases/#{version.major_minor_patch}/#{version}/release-#{version.minor}-#{version.patch}/Dashlane.dmg"
   appcast 'https://ws1.dashlane.com/5/binaries/query?target=archive&format=xml&currentSoftwareVersion=6.0.0&platform=server_osx&os=OS_X_10_14_1'
   name 'Dashlane'
   homepage 'https://www.dashlane.com/'
