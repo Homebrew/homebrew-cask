bc1ce96eb7cf8186022445289f033a7ce2291d5a
Work <brigitte@corecode.io>
2019-03-29 17:09:12 +0100

    Update ocenaudio from 3.6.0.1 to 3.6.1

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -1,6 +1,6 @@
 cask 'ocenaudio' do
-  version '3.6.0.1'
-  sha256 'cd173c3fff41e65b162a594d0f99d6f4d287f0ca5cd61d0a11318cb3591ae85b'
+  version '3.6.1'
+  sha256 '075c79480ddef77da947ca220b14311d1833b9699480040af4c5e6f234d6eb2b'
 
   url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
