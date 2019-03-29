86795131dd1c0bfc6b4af419e169c2add58228fb
Vítor Galvão <info@vitorgalvao.com>
2019-03-29 20:24:33 +0000

    Update metasploit.rb

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -2,7 +2,7 @@ cask 'metasploit' do
   version '5.0.14,20190326103810'
   sha256 '6620fa7d2d1ed88c99b9215f9e38ad5b17e9aa38bfe0be4aefcc66e44c113970'
 
-  url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma} #{version.after_comma}-1rapid7-1.pkg"
+  url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma}+#{version.after_comma}-1rapid7-1.pkg"
   appcast 'https://osx.metasploit.com/LATEST'
   name 'Metasploit Framework'
   homepage 'https://www.metasploit.com/'
