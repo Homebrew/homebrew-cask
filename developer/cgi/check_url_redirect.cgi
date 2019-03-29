b0c1c8ce03765bb216dab7fb256ecb8625773792
Vítor Galvão <info@vitorgalvao.com>
2019-03-29 15:18:22 +0000

    Auto-format python scripts with autopep8 (#61085)

--- developer/cgi/check_url_redirect.cgi
+++ developer/cgi/check_url_redirect.cgi
@@ -7,13 +7,13 @@ form = cgi.FieldStorage()
 url = form.getvalue("url", "")
 ua = form.getvalue("user_agent", "Mozilla")
 try:
-	headers = { 'User-Agent' : ua }
-	req = urllib2.Request(url, None, headers)
-	response = urllib2.urlopen(req)
-	redir = response.geturl()
+    headers = {'User-Agent': ua}
+    req = urllib2.Request(url, None, headers)
+    response = urllib2.urlopen(req)
+    redir = response.geturl()
 except Exception:
-	import traceback
-	redir = 'generic exception: ' + traceback.format_exc()
+    import traceback
+    redir = 'generic exception: ' + traceback.format_exc()
 
 print "Content-type: text/html"
 print
--- developer/cgi/check_url_redirect.py
+++ developer/cgi/check_url_redirect.py
@@ -5,17 +5,17 @@ import urllib2
 
 
 url = sys.argv[1]
-headers = {'User-Agent' : 'Mozilla'}
+headers = {'User-Agent': 'Mozilla'}
 if len(sys.argv) > 2:
-	ua = sys.argv[2]
-	headers = { 'User-Agent' : ua }
+    ua = sys.argv[2]
+    headers = {'User-Agent': ua}
 
 try:
-	req = urllib2.Request(url, None, headers)
-	response = urllib2.urlopen(req)
-	redir = response.geturl()
+    req = urllib2.Request(url, None, headers)
+    response = urllib2.urlopen(req)
+    redir = response.geturl()
 except Exception:
-	import traceback
-	redir = 'generic exception: ' + traceback.format_exc()
+    import traceback
+    redir = 'generic exception: ' + traceback.format_exc()
 
 print redir
