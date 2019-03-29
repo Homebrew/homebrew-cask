52d7527eb7d96ee232e5f8b8a03a5468580efa2d
suschizu <brigitte@corecode.io>
2019-03-29 13:58:42 +0100

    Update trickster from 2.8 to 3.0 (#61076)

--- Casks/trickster.rb
+++ Casks/trickster.rb
@@ -1,6 +1,6 @@
 cask 'trickster' do
-  version '2.8'
-  sha256 '4d2e4196e5baf7ace6e7258c867eae7e5310acb1de263c4bc631ae6a95e075b6'
+  version '3.0'
+  sha256 '187c9cfb4519363f342636744a0149ab8fbfd229bfbf3228e8bed34248d79ac4'
 
   url "https://dl.apparentsoft.com/Trickster_#{version}.zip"
   appcast 'https://dl.apparentsoft.com/trickster.rss'
