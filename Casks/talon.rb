8d0757103688dbb8b3c081ce43875f56770c0ae9
suschizu <brigitte@corecode.io>
2019-03-29 16:36:28 +0100

    Update talon.rb (#61058)

--- Casks/talon.rb
+++ Casks/talon.rb
@@ -3,7 +3,7 @@ cask 'talon' do
   sha256 '45de05c79f7119785cf01535521e2872ed0c409018a32453804db90c6e707368'
 
   url "https://talonvoice.com/update/nmi5s3faoq6NzROd2dbCRg/Talon-#{version}.dmg"
-  appcast 'https://talonvoice.com/'
+  appcast 'https://talonvoice.com/update/nmi5s3faoq6NzROd2dbCRg/appcast.xml'
   name 'Talon'
   homepage 'https://talonvoice.com/'
 
