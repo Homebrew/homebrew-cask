eb43e39ba4ff7bef495049a03e7b5de320dc8bf9
Julian Mayer <julian@corecode.io>
2019-03-30 00:29:08 +0100

    Update ticktick

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -4,6 +4,7 @@ cask 'ticktick' do
 
   # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.ticktick.com/static/getApp/download?type=mac'
   name 'TickTick'
   homepage 'https://www.ticktick.com/home'
 
