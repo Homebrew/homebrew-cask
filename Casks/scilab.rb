46157a8d24827f5fc1cb807ace4f560d00be8538
Vítor Galvão <info@vitorgalvao.com>
2019-03-29 20:26:01 +0000

    Update scilab.rb

--- Casks/scilab.rb
+++ Casks/scilab.rb
@@ -3,7 +3,7 @@ cask 'scilab' do
   sha256 '109d3ee1ed76deec88b8e16d6d0530e823aca2081b6074d9d0d732ef252dc115'
 
   # utc.fr/~mottelet/scilab was verified as official when first introduced to the cask
-  url "http://www.utc.fr/~mottelet/scilab/download/#{version}/scilab-#{version}-x86_64.dmg"
+  url "https://www.utc.fr/~mottelet/scilab/download/#{version}/scilab-#{version}-x86_64.dmg"
   appcast 'https://www.scilab.org/'
   name 'Scilab'
   homepage 'https://www.scilab.org/'
