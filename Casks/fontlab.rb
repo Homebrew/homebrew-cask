36482f5b1d70833aa01a089d1f8bbd829ea8312b
CoreCode <core-code@users.noreply.github.com>
2019-03-29 16:44:11 +0100

    Update fontlab (#61073)
    * Update fontlab
    
    * Update fontlab.rb

--- Casks/fontlab.rb
+++ Casks/fontlab.rb
@@ -1,8 +1,8 @@
 cask 'fontlab' do
-  version '6.1.3.7016'
+  version '6.1.3,7016'
   sha256 'c0a950ed702c60d35d9d9d4e5e8f803eadf258e66503c0edf473968f6e997605'
 
-  url 'https://download.fontlab.com/fontlab-vi/upd-mac.php'
+  url "https://download.fontlab.com/fontlab-vi/downloads/FontLab-VI-Mac-Install-#{version.after_comma}.dmg"
   appcast 'https://download.fontlab.com/fontlab-vi/appcast-mac.xml'
   name 'Fontlab'
   homepage 'https://www.fontlab.com/font-editor/fontlab-vi'
