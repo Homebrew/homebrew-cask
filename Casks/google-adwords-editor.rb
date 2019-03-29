c3bfe26a62e1e9e527ebcc3462c1faf8b3a52f6c
Tin Kyaw <tin@lsvp.com>
2019-03-29 16:11:02 -0700

    updated name and homepage as well

--- Casks/google-adwords-editor.rb
+++ Casks/google-adwords-editor.rb
@@ -3,8 +3,8 @@ cask 'google-adwords-editor' do
   sha256 :no_check
 
   url 'https://dl.google.com/adwords_editor/Google_AdWords_Editor.dmg'
-  name 'Google AdWords Editor'
-  homepage 'https://adwords.google.com/home/tools/adwords-editor/?zd=1'
+  name 'Google Ads Editor'
+  homepage 'https://ads.google.com/home/tools/ads-editor/'
 
   app 'Google Ads Editor.app'
 end
