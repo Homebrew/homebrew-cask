0022d0315d0f184c857ed8ac6bc216baab3879eb
CoreCode <core-code@users.noreply.github.com>
2019-03-30 02:40:08 +0100

    Update testnav (#61111)

--- Casks/testnav.rb
+++ Casks/testnav.rb
@@ -3,6 +3,7 @@ cask 'testnav' do
   sha256 '9fca7d7e9bb57af5841b9e34722f680383b397b37f298c0b69efdc6565272335'
 
   url "https://download.testnav.com/installers/testnav-#{version}.dmg"
+  appcast 'https://s3.amazonaws.com/tn8appinstallers/installerVersions.json'
   name 'TestNav'
   homepage 'https://download.testnav.com/'
 
