8ed3b3bbc6b8ba0ddacc3f78a5084ea2ffb43e93
suschizu <brigitte@corecode.io>
2019-03-30 19:58:56 +0100

    Update blisk from 10.1.262.114 to 11.0.157.186 (#61153)

--- Casks/blisk.rb
+++ Casks/blisk.rb
@@ -1,6 +1,6 @@
 cask 'blisk' do
-  version '10.1.262.114'
-  sha256 'f91e998eb2a3a02dda896f6dbb3b9f01b594750976e0182e65bc1a414570bf9b'
+  version '11.0.157.186'
+  sha256 '1dbf8c77a55089fa023bc26be4fa33a3926bbb405b0164f91168afb317ac2e48'
 
   # bliskcloudstorage.blob.core.windows.net was verified as official when first introduced to the cask
   url "https://bliskcloudstorage.blob.core.windows.net/mac-installers/BliskInstaller_#{version}.dmg"
