f0db43dc3757f4b53fe6f76e516a068fd8ccdef4
CoreCode <core-code@users.noreply.github.com>
2019-03-30 16:52:42 +0100

    Update signal.rb

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -2,8 +2,7 @@ cask 'signal' do
   version '1.23.1'
   sha256 'baf2a609e78c0314c3bbc509c8d7ea8878425ca34e3f43856cda3fe3d911dc1e'
 
-  url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.zip",
-      using: :nounzip
+  url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.zip"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
   name 'Signal'
   homepage 'https://signal.org/'
