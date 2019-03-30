963d3d1a01272e7ba76815175a9bda566f2cbd8d
suschizu <brigitte@corecode.io>
2019-03-30 09:02:56 +0100

    Update daedalus from 0.13.0,3.0.0:4954 to 0.13.1,3.0.1:5130 (#61118)

--- Casks/daedalus.rb
+++ Casks/daedalus.rb
@@ -1,6 +1,6 @@
 cask 'daedalus' do
-  version '0.13.0,3.0.0:4954'
-  sha256 '895f577b4b499574aead666f44a765c1f92617d79e50d122ec49e0f0487a8e62'
+  version '0.13.1,3.0.1:5130'
+  sha256 '92a4a72d4cdacdf256f061e779d380e6ba6479f0c9a085021c030da98f4bbdef'
 
   # github.com/input-output-hk/daedalus was verified as official when first introduced to the cask
   url "https://github.com/input-output-hk/daedalus/releases/download/#{version.before_comma}/daedalus-#{version.before_comma}-cardano-sl-#{version.after_comma.before_colon}-mainnet-macos-#{version.after_comma.after_colon}.pkg"
