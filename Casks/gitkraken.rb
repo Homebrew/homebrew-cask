c15817b9111aeb2a6a965fa13f068c42f508b2c3
suschizu <brigitte@corecode.io>
2019-03-30 09:14:05 +0100

    Update gitkraken from 5.0.3 to 5.0.4 (#61120)

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,6 +1,6 @@
 cask 'gitkraken' do
-  version '5.0.3'
-  sha256 '1f52af5f01f3a426f4b57bbc90d55e81fbe3fdc63a25dd17a4f3a1aa7dd9fa0b'
+  version '5.0.4'
+  sha256 '16bdd399bcd75ac4d48e639eecfd0b583efab0482882757e241987d85e0b9b41'
 
   url "https://release.gitkraken.com/darwin/GitKraken-v#{version}.zip"
   appcast 'https://support.gitkraken.com/release-notes/current'
