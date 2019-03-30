3523bc388cd3634ce9d0c5f1130ef50149c99cf5
Lance M <mightyfree@users.noreply.github.com>
2019-03-30 13:39:15 -0400

    Update filemaker-pro-advanced from 17.0.2.205 to 17.0.4.400

--- Casks/filemaker-pro-advanced.rb
+++ Casks/filemaker-pro-advanced.rb
@@ -1,5 +1,5 @@
 cask 'filemaker-pro-advanced' do
-  version '17.0.2.205'
+  version '17.0.4.400'
   sha256 '68956a994f95f3b184a41492bce223dc2c7407a71376554f546d1e8d71340f01'
 
   url "https://fmdl.filemaker.com/TBUB/#{version.major}/fmpa_#{version}.dmg"
