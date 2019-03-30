47ae089f2e3c08c3caadab817146e54e1757812b
CoreCode <core-code@users.noreply.github.com>
2019-03-30 02:43:34 +0100

    Update thingsmacsandboxhelper (#61114)

--- Casks/thingsmacsandboxhelper.rb
+++ Casks/thingsmacsandboxhelper.rb
@@ -4,6 +4,7 @@ cask 'thingsmacsandboxhelper' do
 
   # culturedcode.cachefly.net was verified as official when first introduced to the cask
   url "https://culturedcode.cachefly.net/things/thingssandboxhelper/#{version}/ThingsHelper.zip"
+  appcast 'https://culturedcode.com/things/mac/help/things-sandboxing-helper-things3/'
   name 'ThingsMacSandboxHelper'
   homepage "https://culturedcode.com/things/mac/help/things-sandboxing-helper-things#{version.major}/"
 
