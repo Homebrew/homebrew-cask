a37b6541b1b9611dc4581fe9405149ad353b77bb
Work <brigitte@corecode.io>
2019-03-30 17:22:40 +0100

    Update psychopy from 3.0.6 to 3.0.7

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,6 +1,6 @@
 cask 'psychopy' do
-  version '3.0.6'
-  sha256 'e3f04f4fbd866693fa596fc1713324df34a71ef4afe0d2f32482e8250939c994'
+  version '3.0.7'
+  sha256 'e1adf0cfb86f5183dc49f828780b8a1efa140408438eed7f27cdda9301bf0f43'
 
   url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy3-#{version}-MacOS.dmg"
   appcast 'https://github.com/psychopy/psychopy/releases.atom'
