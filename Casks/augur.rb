7fa0d20f53006cbf9c440482a3c33d8aaab181f8
Work <brigitte@corecode.io>
2019-03-31 12:38:52 +0200

    Update augur from 1.10.2 to 1.11.0

--- Casks/augur.rb
+++ Casks/augur.rb
@@ -1,6 +1,6 @@
 cask 'augur' do
-  version '1.10.2'
-  sha256 'e8ecb965e351d79dce6fd4d715b84fd8e48221277bbd39d58b6310017aa85102'
+  version '1.11.0'
+  sha256 '6516ee33d8f3e3a65d7becb214b69cd1814f4337f44eab949d5c11daf23802ee'
 
   url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-Augur-#{version}.dmg"
   appcast 'https://github.com/AugurProject/augur-app/releases.atom'
