7068fc2458ceb507ad1e8a91e080db5522a52431
suschizu <brigitte@corecode.io>
2019-03-31 19:04:34 +0200

    Update whatroute from 2.2.1 to 2.2.2 (#61182)

--- Casks/whatroute.rb
+++ Casks/whatroute.rb
@@ -1,6 +1,6 @@
 cask 'whatroute' do
-  version '2.2.1'
-  sha256 '11067888af1cdc72d6aed04edcf380745b172a1da408b49c07ea4571f65bb284'
+  version '2.2.2'
+  sha256 '128bc12b509965e78e40d9c31b7959f990a96417b72da0d71ef92a324277c13e'
 
   url "https://downloads.whatroute.net/software/whatroute-#{version}.zip"
   appcast "https://www.whatroute.net/whatroute#{version.major}appcast.xml"
