a14d438d6f29b2c6decb287f89d8acaeed851682
suschizu <brigitte@corecode.io>
2019-03-31 12:40:02 +0200

    Update umsatz from 8.1.4 to 8.28.2 (#61167)
    * Update umsatz from 8.1.4 to 8.28.2
    
    * Update umsatz.rb

--- Casks/umsatz.rb
+++ Casks/umsatz.rb
@@ -1,6 +1,6 @@
 cask 'umsatz' do
-  version '8.1.4'
-  sha256 '0dc293391a59efeb14ec067956bf2b088110e0df63019e4e81ba37d90cfb4dfe'
+  version '8.2'
+  sha256 '0078e9ce1ecb7e2f448a89ce44e9627327f309336aa83153882232abdca2eb1a'
 
   # moapp.software was verified as official when first introduced to the cask
   url 'https://download.moapp.software/Umsatz.zip'
