9f19b50a68a1ec46b9dc2967682fe6f86ef4bdfd
Stephane Paquet <spaquet@up4b.com>
2019-03-30 20:09:47 -0700

    Update electron from 4.1.1 to 4.1.3

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '4.1.1'
-  sha256 '5b172d5ca70cf9730516fab4c6898fac9dcf161e73b9c68959cceb9f586f40a9'
+  version '4.1.3'
+  sha256 '3fa77342666ba08a24b04208d1b6c980de786e0a33d0e470f995be8ab48f4e68'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
