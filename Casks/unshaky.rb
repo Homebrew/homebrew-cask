3f904db21d61505eaaa85c35246df059b9cfd9d3
suschizu <brigitte@corecode.io>
2019-04-01 12:09:51 +0200

    Update unshaky from 0.4.5 to 0.4.6 (#61221)

--- Casks/unshaky.rb
+++ Casks/unshaky.rb
@@ -1,6 +1,6 @@
 cask 'unshaky' do
-  version '0.4.5'
-  sha256 'f2b534fddde64193ee6621ec1b33fb73edfc10f86aa5f6fa16c621c824713dae'
+  version '0.4.6'
+  sha256 'c04ae51ee8cb38e9db75b61a5b0b8592df65855a5765b9f4a688a6bb28eab7f0'
 
   url "https://github.com/aahung/Unshaky/releases/download/v#{version}/Unshaky_v#{version}.zip"
   appcast 'https://github.com/aahung/Unshaky/releases.atom'
