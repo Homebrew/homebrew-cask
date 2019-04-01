bef2214fbd8e4c1f4291ad79b870a4c7330c6f29
Fabrizio <fabri.feno@gmail.com>
2019-04-01 15:40:51 +0100

    removed v prefix for new releases

--- Casks/bloomrpc.rb
+++ Casks/bloomrpc.rb
@@ -2,7 +2,7 @@ cask 'bloomrpc' do
   version '1.3.0'
   sha256 '2a129cb144f4a1b4f5e307c1aef4712baff318fe410ef1edc6d9ce3d802d862e'
 
-  url "https://github.com/uw-labs/bloomrpc/releases/download/v#{version}/BloomRPC-#{version}.dmg"
+  url "https://github.com/uw-labs/bloomrpc/releases/download/#{version}/BloomRPC-#{version}.dmg"
   appcast 'https://github.com/uw-labs/bloomrpc/releases.atom'
   name 'BloomRPC'
   homepage 'https://github.com/uw-labs/bloomrpc'
