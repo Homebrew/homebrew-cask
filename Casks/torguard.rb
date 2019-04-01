f93bc9cc8be8f39f4a0c0d5e38ed98b9eba0c9a4
suschizu <brigitte@corecode.io>
2019-04-01 12:32:56 +0200

    Update torguard from 3.93.1 to 3.93.2 (#61230)

--- Casks/torguard.rb
+++ Casks/torguard.rb
@@ -1,6 +1,6 @@
 cask 'torguard' do
-  version '3.93.1'
-  sha256 '4174bb919d757999a9423db688bcc77b95225d65219c5baeebefe609f3759450'
+  version '3.93.2'
+  sha256 '6cf08d3cd58eefde8e861597458433ecf45cea4090e912a2b91aa4749aea2b45'
 
   # torguard.biz was verified as official when first introduced to the cask
   url "https://updates.torguard.biz/Software/MacOSX/TorGuard-v#{version}.dmg"
