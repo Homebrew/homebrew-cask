b6bd394809385f1ffceea1741e630d65b7ec5dc0
CoreCode <core-code@users.noreply.github.com>
2019-04-02 01:39:28 +0200

    Update aqua-data-studio from 19.0.2 to 19.5.0 (#61191)

--- Casks/aqua-data-studio.rb
+++ Casks/aqua-data-studio.rb
@@ -1,8 +1,9 @@
 cask 'aqua-data-studio' do
-  version '19.0.2'
-  sha256 '56461ec9f63746ce84176cc0c7a566e217e9e16d84fe2ee8f4c9d600d25bcd91'
+  version '19.5.0'
+  sha256 '27af1b65ce476c8aa0a39456bbefc21dbf88b5c5a70ca374c67a4832209fcedb'
 
   url "http://downloads.aquafold.com/v#{version.major_minor}.0/osx/ads-osx-#{version}.tar.gz"
+  appcast 'https://www.aquafold.com/aquadatastudio_downloads'
   name 'Aquafold Aqua Data Studio'
   homepage 'https://www.aquafold.com/aquadatastudio'
 
