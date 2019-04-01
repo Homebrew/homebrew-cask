5667013df750383edcfe884b4e10fec3841d23b5
Brandon Patram <bpatram@users.noreply.github.com>
2019-04-01 10:07:28 -0400

    update steermouse to 5.3.6 (#61232)

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,6 +1,6 @@
 cask 'steermouse' do
-  version '5.3.5'
-  sha256 '96f580e93c21318a1af15e250f48f2157bad5570a0dd4593d3c15db94c7b4ec5'
+  version '5.3.6'
+  sha256 '3d121893638149f7044807cde83f56fcf6d00fb4e9ff2474d1aed01abab8becd'
 
   url "https://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
   appcast 'https://plentycom.jp/en/steermouse/download.php'
