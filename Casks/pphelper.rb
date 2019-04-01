cce4ef8957fd5d5da98aa763c6024ad5f42ca5cb
suschizu <brigitte@corecode.io>
2019-04-01 11:50:19 +0200

    Update pphelper from 2.3.7 to 2.4.0 (#61213)

--- Casks/pphelper.rb
+++ Casks/pphelper.rb
@@ -1,6 +1,6 @@
 cask 'pphelper' do
-  version '2.3.7'
-  sha256 '214366e0679c2df2bd5db78cf289eba9a099a4582b3f7c9882b29ac34eee24cd'
+  version '2.4.0'
+  sha256 '2bcade81898c8c1d41c2877d906b94bd889178204c53cd85655d7219d30e55c6'
 
   url 'https://ghost.25pp.com/soft/pp_mac.dmg'
   appcast 'https://liveupdate.25pp.com/macpc/Appcast.xml'
