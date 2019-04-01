03643d72e2928fe16041820194a5bea9e7df81b5
Work <brigitte@corecode.io>
2019-04-01 12:21:33 +0200

    Update wiznote from 2.7.1,2019-03-13 to 2.7.2,2019-03-13

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -1,5 +1,5 @@
 cask 'wiznote' do
-  version '2.7.1,2019-03-13'
+  version '2.7.2,2019-03-13'
   sha256 '9b7ba3700406297743c9bafa333b5885d342ade338eeae4d355e5985876bcb3b'
 
   url "https://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
