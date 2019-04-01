1c75dfabd6c9476e16408990ee38158e0015d335
CoreCode <core-code@users.noreply.github.com>
2019-04-02 01:38:30 +0200

    Update easyfind (#61200)

--- Casks/easyfind.rb
+++ Casks/easyfind.rb
@@ -4,8 +4,9 @@ cask 'easyfind' do
 
   # amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/DTWebsiteSupport/download/freeware/easyfind/#{version}/EasyFind.app.zip"
+  appcast 'https://www.devontechnologies.com/apps/freeware'
   name 'EasyFind'
-  homepage 'https://www.devontechnologies.com/download/products.html'
+  homepage 'https://www.devontechnologies.com/apps/freeware'
 
   app 'EasyFind.app'
 end
