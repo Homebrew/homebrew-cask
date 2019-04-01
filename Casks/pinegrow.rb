0e846e7df86cdd87d7f2e43c70e25d3007f0dbaa
CoreCode <core-code@users.noreply.github.com>
2019-04-02 01:38:14 +0200

    Update pinegrow (#61244)

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -3,6 +3,7 @@ cask 'pinegrow' do
   sha256 '06310d68c9748e033002ae1326a36fb377c4193ccc4ef857a9067bb292d8024f'
 
   url "http://download.pinegrow.com/PinegrowMac.#{version}.dmg"
+  appcast 'https://pinegrow.com/'
   name 'Pinegrow'
   homepage 'https://pinegrow.com/'
 
