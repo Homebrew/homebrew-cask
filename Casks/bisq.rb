ef5ba07f753a0c79025fe50a3f8de466ce546055
CoreCode <core-code@users.noreply.github.com>
2019-04-03 00:29:43 +0200

    Update bisq from 0.9.5 to 0.9.7 (#61281)

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask 'bisq' do
-  version '0.9.5'
-  sha256 '5ece20924db7a6d979861067d991b811da02d3f367e0c4f60c5576179a4e59d2'
+  version '0.9.7'
+  sha256 '2c1b81507bfd647b8355df20ecfdf130d6d45407990cdabb4750c9e4512d7e03'
 
   # github.com/bisq-network/bisq-desktop was verified as official when first introduced to the cask
   url "https://github.com/bisq-network/bisq-desktop/releases/download/v#{version}/Bisq-#{version}.dmg"
