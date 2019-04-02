97caa2168ea580e19b02498c1db04cfefded523c
suschizu <brigitte@corecode.io>
2019-04-02 16:36:29 +0200

    Update ocenaudio.rb

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -1,13 +1,20 @@
 cask 'ocenaudio' do
   version '3.6.1'
-  sha256 '075c79480ddef77da947ca220b14311d1833b9699480040af4c5e6f234d6eb2b'
 
-  url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
+  if MacOS.version <= :high_sierra
+    sha256 '3afd75f1bddf791dc7dbc9a7d92ab6eb91ee891407d750cedb7b5aff5fe8bf17'
+
+    url 'http://www.ocenaudio.com/start_download/ocenaudio_sierra.dmg'
+  else
+    sha256 '075c79480ddef77da947ca220b14311d1833b9699480040af4c5e6f234d6eb2b'
+
+    url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
+  end
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
   name 'ocenaudio'
   homepage 'https://www.ocenaudio.com/en'
 
-  depends_on macos: '>= :mojave'
+  depends_on macos: '>= :sierra'
 
   app 'ocenaudio.app'
 end
