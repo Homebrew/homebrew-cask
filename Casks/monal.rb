40e31232ee1deeef98c5bca8982dcfe7c9f43b2f
Julian Mayer <julian@corecode.io>
2019-04-02 23:55:23 +0200

    Update monal from 2.3b19 to 2.4b1

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,6 +1,6 @@
 cask 'monal' do
-  version '2.3b19'
-  sha256 '12b16696d6dc931e998a482563f9ee0d03d3a42f170b6a951d9c888fe6587f33'
+  version '2.4b1'
+  sha256 '87676da93e04ad7b12c943c44e932ebb643f852aac88f5a228c44b4d9956d91a'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
   appcast 'https://monal.im/Monal-OSX/appcast.xml'
