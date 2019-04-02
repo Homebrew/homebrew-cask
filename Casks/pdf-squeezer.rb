45c4a85340c07d5627bf30050c3b2e84aedc7d92
CoreCode <core-code@users.noreply.github.com>
2019-04-03 00:29:13 +0200

    Update pdf-squeezer from 3.9.3 to 3.10 (#61283)

--- Casks/pdf-squeezer.rb
+++ Casks/pdf-squeezer.rb
@@ -1,6 +1,6 @@
 cask 'pdf-squeezer' do
-  version '3.9.3'
-  sha256 '4053fdd9a250810f335c3a0e82a392a5934d3517a7d9e9d01f474ca226d882e1'
+  version '3.10'
+  sha256 '747498decdff62152ac24a27bf14b12172487f6bdf32618eeb467f1e2ae8e271'
 
   url 'https://witt-software.com/downloads/pdfsqueezer/PDF%20Squeezer.dmg'
   appcast 'https://witt-software.com/downloads/pdfsqueezer/appcast.xml'
