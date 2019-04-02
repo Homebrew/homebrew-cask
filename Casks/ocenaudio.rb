509bb7a9df350dda83c9ba73808cc410d31850a0
suschizu <brigitte@corecode.io>
2019-04-02 21:29:23 +0200

    Update ocenaudio.rb

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -4,7 +4,7 @@ cask 'ocenaudio' do
   if MacOS.version <= :high_sierra
     sha256 '1bc0c0742df9a2d683e72f50bef06ca14af2ac6e5c068eb96ebe973fa52a6d37'
 
-    url 'https://www.ocenaudio.com/start_download/ocenaudio_sierra.dmg'
+    url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg'
   else
     sha256 '075c79480ddef77da947ca220b14311d1833b9699480040af4c5e6f234d6eb2b'
 
