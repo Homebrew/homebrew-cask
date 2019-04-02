39f455873ec4b960b13420cac8098c28a005f448
CoreCode <core-code@users.noreply.github.com>
2019-04-03 00:29:09 +0200

    Update principle.rb

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -2,10 +2,10 @@ cask 'principle' do
   version '5.7'
   sha256 'a92291608dc37dcb2dc005b85b1c9850cdbac82ef87ba7d118cd12a3d84a03c1'
 
-  url "http://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
-  appcast 'http://principleformac.com/update2.xml'
+  url "https://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
+  appcast 'https://principleformac.com/update2.xml'
   name 'Principle'
-  homepage 'http://principleformac.com/'
+  homepage 'https://principleformac.com/'
 
   depends_on macos: '>= :high_sierra'
 
