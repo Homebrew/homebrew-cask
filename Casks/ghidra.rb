0b3adcc36cbe832805244883e3835d73a201f7d9
Vítor Galvão <info@vitorgalvao.com>
2019-04-03 21:42:55 +0100

    Update ghidra.rb

--- Casks/ghidra.rb
+++ Casks/ghidra.rb
@@ -1,5 +1,5 @@
 cask 'ghidra' do
-  version '9.0_PUBLIC_20190228'
+  version '9.0.1_PUBLIC_20190325'
   sha256 '3b65d29024b9decdbb1148b12fe87bcb7f3a6a56ff38475f5dc9dd1cfc7fd6b2'
 
   url "https://www.ghidra-sre.org/ghidra_#{version}.zip"
