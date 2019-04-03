9b7ff1236d0b999b9f71ed23fcb7bd7c481c6c03
CoreCode <core-code@users.noreply.github.com>
2019-04-03 03:06:21 +0200

    Update opera (#61301)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -3,6 +3,7 @@ cask 'opera' do
   sha256 'd909e3b9e4489223a86c3946290510abfa5a00caa7d301a32b501e98145d5583'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
+  appcast 'https://ftp.opera.com/pub/opera/desktop/'
   name 'Opera'
   homepage 'https://www.opera.com/'
 
