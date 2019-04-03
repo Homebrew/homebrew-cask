d47da45e79950bb0daebb4fb398cc44f582fd19f
Andrew Dunning <adunning@users.noreply.github.com>
2019-04-03 10:19:59 -0400

    Update lectionary from 1.17.1 to 1.17.2 (#61341)

--- Casks/lectionary.rb
+++ Casks/lectionary.rb
@@ -1,6 +1,6 @@
 cask 'lectionary' do
-  version '1.17.1'
-  sha256 '1420c2a7d206d6a6b39e703a2bb0b8f5fc2683f40abd2842fd18f63e2fa48ef3'
+  version '1.17.2'
+  sha256 '0c2176d61e30c97bced2cbd897fccac94a209fe477ef748f3c95e3f1d6de1450'
 
   url "https://myaccount.hymnsam.co.uk/PublicationRelatedFiles/21/Lectionary%20for%20Mac.#{version}.dmg"
   name 'Lectionary'
