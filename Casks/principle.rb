fdbda9360a8bb7babb51b951ed1d186154baa183
CoreCode <core-code@users.noreply.github.com>
2019-04-03 03:07:38 +0200

    Update principle from 5.3 to 5.7 (#61287)
    * Update principle from 5.3 to 5.7
    
    * Update principle.rb

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,11 +1,11 @@
 cask 'principle' do
-  version '5.3'
-  sha256 'e667d879888bc1c35875c75ce23945710eac75191a1daca76dbfb4c77f902f28'
+  version '5.7'
+  sha256 'a92291608dc37dcb2dc005b85b1c9850cdbac82ef87ba7d118cd12a3d84a03c1'
 
-  url "http://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
-  appcast 'http://principleformac.com/update2.xml'
+  url "https://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
+  appcast 'https://principleformac.com/update2.xml'
   name 'Principle'
-  homepage 'http://principleformac.com/'
+  homepage 'https://principleformac.com/'
 
   depends_on macos: '>= :high_sierra'
 
