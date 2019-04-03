81559f82091deb81fa6a78d0c8a0a91fa0d31c0c
Work <brigitte@corecode.io>
2019-04-03 13:53:32 +0200

    Update coyim from 0.3.10 to 0.3.11

--- Casks/coyim.rb
+++ Casks/coyim.rb
@@ -1,6 +1,6 @@
 cask 'coyim' do
-  version '0.3.10'
-  sha256 '6623eb9f86ae44006a312de23537910558fe5c283e0b603daef24a38d577c325'
+  version '0.3.11'
+  sha256 'ecb0eac42d2871fcb673bd89013490519e48b1afca76076353da3577162d6f5f'
 
   # dl.bintray.com/coyim/coyim-bin was verified as official when first introduced to the cask
   url "https://dl.bintray.com/coyim/coyim-bin/v#{version}/mac-bundle/coyim.dmg"
