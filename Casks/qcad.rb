b686642c3bd371ba6abf8e2f628c5c219a19ef54
CoreCode <core-code@users.noreply.github.com>
2019-04-04 00:31:58 +0200

    Update qcad.rb

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -2,7 +2,7 @@ cask 'qcad' do
   version '3.22.0'
 
   if MacOS.version <= :high_sierra
-    sha256 '9155a11b10991dbeb5bac5a15b810354d0462f13bee8317e4327765d83582466'
+    sha256 'a05bd044702f3e1f582e4b527a4d520ac433ad6231fd3161f3c85ccf3574db17'
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.10-10.13.dmg"
   else
     sha256 'c14c3b030b729e4e09abecfa85c077fcadbd2c3856fed26fe2d3c0413f11acd6'
