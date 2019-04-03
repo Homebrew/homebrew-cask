10b9613064ceb79ebf53588786e9233ea8a57eee
Lutz Roeder <lutzroeder@users.noreply.github.com>
2019-04-03 04:12:35 -0700

    Fix Netron url casing (#61270)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -2,10 +2,10 @@ cask 'netron' do
   version '2.9.8'
   sha256 '27d5454bb4e70cdd43b92d0a21c510ff0f16ccbe9886d2d0fde00503662da674'
 
-  url "https://github.com/lutzroeder/Netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
-  appcast 'https://github.com/lutzroeder/Netron/releases.atom'
+  url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
+  appcast 'https://github.com/lutzroeder/netron/releases.atom'
   name 'Netron'
-  homepage 'https://github.com/lutzroeder/Netron'
+  homepage 'https://github.com/lutzroeder/netron'
 
   auto_updates true
 
