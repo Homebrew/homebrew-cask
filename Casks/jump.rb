e512d93ab581121a26ec6db1df730a46e43e368f
LanikSJ <laniksj@gmail.com>
2019-04-03 16:37:49 -0700

    Update jump from 8.2.17 to 8.2.19

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -1,6 +1,6 @@
 cask 'jump' do
-  version '8.2.17'
-  sha256 '2a68bc68c2f2dd6c1b506b1ffc2346510992714b03b34950e4dc0f3f71eecd52'
+  version '8.2.19'
+  sha256 '4b225ce89d58d6ac1eb813768b48b8a5abf7211a3666acde6d766df076229518'
 
   url "https://mirror.jumpdesktop.com/downloads/JumpDesktopMac-#{version}.zip"
   appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml'
