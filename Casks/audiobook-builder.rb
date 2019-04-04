83bab3db12c663ea64afe4c6d0875cfae3b97549
suschizu <brigitte@corecode.io>
2019-04-04 18:27:17 +0200

    Update audiobook-builder from 2.0.1 to 2.0.2 (#61396)

--- Casks/audiobook-builder.rb
+++ Casks/audiobook-builder.rb
@@ -1,6 +1,6 @@
 cask 'audiobook-builder' do
-  version '2.0.1'
-  sha256 '5372b3dca0d7bd7fab08614312a5d9b5cceaa21ac387a854596cb2941b65e985'
+  version '2.0.2'
+  sha256 '8d235350b46fd5f06c6eddbb54fec9ca5875a0c664e7a2cf8f4e4d48ff99cd85'
 
   url "https://www.splasm.com/downloads/audiobookbuilder/Audiobook%20Builder%20#{version}.dmg"
   appcast 'https://www.splasm.com/audiobookbuilder/'
