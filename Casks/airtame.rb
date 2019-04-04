0d85f2108267a6ac7cb9bdf11223574e42c8e0d6
Work <brigitte@corecode.io>
2019-04-04 22:58:24 +0200

    Update airtame from 3.3.2 to 3.4.0

--- Casks/airtame.rb
+++ Casks/airtame.rb
@@ -1,6 +1,6 @@
 cask 'airtame' do
-  version '3.3.2'
-  sha256 '9ec8223a8998cf9756e1252f6831cc6357f8856813ddef08a788bddbd7594ceb'
+  version '3.4.0'
+  sha256 'e1834a01823708ff34c78eeb683fd86141b7c9f8e18f3d37179fe33b8b212d1a'
 
   url "https://downloads-cdn.airtame.com/application/ga/osx_x64/releases/airtame-application-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads-cdn.airtame.com/get.php?platform=osx_x64'
