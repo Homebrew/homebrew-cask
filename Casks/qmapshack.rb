c40d5c8fe3646c3c895584f25d0293f133c21122
CoreCode <core-code@users.noreply.github.com>
2019-04-04 13:12:01 +0200

    Update qmapshack (#61355)

--- Casks/qmapshack.rb
+++ Casks/qmapshack.rb
@@ -3,6 +3,7 @@ cask 'qmapshack' do
   sha256 '560a601b17a6642617b3b3239a8e01d1cd4ba5decf998c552d56dd622446a769'
 
   url "https://bitbucket.org/maproom/qmapshack/downloads/QMapShack-MacOSX_#{version}.tar.gz"
+  appcast 'https://bitbucket.org/maproom/qmapshack/downloads/'
   name 'QMapShack'
   homepage 'https://bitbucket.org/maproom/qmapshack/wiki/Home'
 
