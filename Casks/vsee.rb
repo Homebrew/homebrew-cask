f3426521efa1f34e2081782b8b77f4e26b8855f6
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-05 08:57:37 -0700

    Update vsee from 4.4.1,38194 to 4.4.2,38214 (#61448)

--- Casks/vsee.rb
+++ Casks/vsee.rb
@@ -1,6 +1,6 @@
 cask 'vsee' do
-  version '4.4.1,38194'
-  sha256 '274fe221c49fa965c338953d13b2aeef51f46b2533332d2ed5e8f37851a95d6c'
+  version '4.4.2,38214'
+  sha256 '8a1bed997d3a7500337e8f906fe98dcbac379ab3c12b793a6c24b5af8ec1707d'
 
   # d2q5hugz2rti4w.cloudfront.net was verified as official when first introduced to the cask
   url "https://d2q5hugz2rti4w.cloudfront.net/mac/#{version.after_comma}/vseemac.dmg"
