aa889babaad675522b3b0836acad4082c28954bc
Carsten Heidmann <carsten@heidmann.info>
2019-04-05 11:15:25 +0200

    Update pgadmin4 to version 4.4 (#61420)
    * Update panoply version to 4.9,2
    
    * Adjusted Hash for panoply 4.9.2
    
    * Update panoply.rb
    
    * new version 2.14.0
    
    * Updated rocket-chat to version 2.14.1
    
    * Update pgadmin4.rb
    
    new version 4.4

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -1,6 +1,6 @@
 cask 'pgadmin4' do
-  version '4.3'
-  sha256 'fffae3308e23e5c31438d7443523108fcc79401cddea80ab5e8dd57197d796ff'
+  version '4.4'
+  sha256 'f5df37fab1d96426da89b1743682887ae5448cb93971e02e7042ac580c5d9546'
 
   # ftp.postgresql.org was verified as official when first introduced to the cask
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}.dmg"
