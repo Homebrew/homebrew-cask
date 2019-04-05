491273488509cc560f5e3f9c0e819b0176f4f798
Randall <ran.dall@icloud.com>
2019-04-05 08:38:07 -0700

    Update icq from 3.0.20260 to 3.0.20390

--- Casks/icq.rb
+++ Casks/icq.rb
@@ -1,6 +1,6 @@
 cask 'icq' do
-  version '3.0.20260'
-  sha256 '26ff09b3fdc885e71fcf61602f94f0ec2526a7facd761867c9b5674ff1daac99'
+  version '3.0.20390'
+  sha256 '2576a208310ebfa64ebc4e292beb9b8e09c7a815cf89a00c87f13066b8559cba'
 
   # mra.mail.ru/icq_mac3_update was verified as official when first introduced to the cask
   url 'https://mra.mail.ru/icq_mac3_update/icq.dmg'
