8508bb0b8f747556cc1b3e2a24330c55e455ae6e
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-05 08:49:27 -0700

    Update hostbuddy from 2.0.4,20_5 to 2.1.0,20_8 (#61445)

--- Casks/hostbuddy.rb
+++ Casks/hostbuddy.rb
@@ -1,6 +1,6 @@
 cask 'hostbuddy' do
-  version '2.0.4,20_5'
-  sha256 '4b92dc74470538e06c86d327a2259abb087954038df31c0191b6f3abd61d62c2'
+  version '2.1.0,20_8'
+  sha256 '40b364de09259fba8812f53ce841a08a13aa0e6c3e9337db1697ecdf03c920aa'
 
   # downloads-clickonideas.netdna-ssl.com/hostbuddy was verified as official when first introduced to the cask
   url "https://downloads-clickonideas.netdna-ssl.com/hostbuddy/hostbuddy#{version.after_comma}.zip"
