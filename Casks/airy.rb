383dda94ee30ebc876babd83c482b32f26d4c30e
Julian Mayer <julian@corecode.io>
2019-04-06 21:48:43 +0200

    Update airy from 3.6,200 to 3.7,205

--- Casks/airy.rb
+++ Casks/airy.rb
@@ -1,6 +1,6 @@
 cask 'airy' do
-  version '3.6,200'
-  sha256 '0cc3c34b6e7b1fa567ca5d2d0968fbb56aada7b9a5d81d2e61bba8fcf169d1f6'
+  version '3.7,205'
+  sha256 '60d6306b52a94a40150856ebb26e134f264dea85528e12053c4215a16dcf4e09'
 
   url 'https://cdn.eltima.com/download/airy.dmg'
   appcast 'https://cdn.eltima.com/download/airy-update/airy.xml'
