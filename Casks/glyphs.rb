a14d17352e8df8fed04c3a15c0b6cb6952369faa
Randall <ran.dall@icloud.com>
2019-04-06 08:48:26 -0700

    Update glyphs from 2.6.1,1225 to 2.6.1,1226

--- Casks/glyphs.rb
+++ Casks/glyphs.rb
@@ -1,6 +1,6 @@
 cask 'glyphs' do
-  version '2.6.1,1225'
-  sha256 'f9238085456e706b17298128e35ffff4140fbbcc6a7dc5b90c710ee32758b08c'
+  version '2.6.1,1226'
+  sha256 '588d0f2af912d6c9a86f6c558f6b85be8666ebc922b434f8afefe5c3f5c94b03'
 
   url "https://updates.glyphsapp.com/Glyphs#{version.major_minor_patch}-#{version.after_comma}.zip"
   appcast "https://updates.glyphsapp.com/appcast#{version.major}.xml"
