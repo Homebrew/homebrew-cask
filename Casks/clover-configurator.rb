f70be56fef5bf80061379050029bb9fc34d77b62
Phuc Nguyen Minh <minhphuc429@gmail.com>
2019-04-06 23:16:41 +0700

    Update clover-configurator.rb
    fix url has https

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -2,7 +2,7 @@ cask 'clover-configurator' do
   version '5.4.2.1'
   sha256 'c7bcf7c953ee9a7c7ddcef0e66181585e6baabf8da31304ad4738b467e2b0f76'
 
-  url 'http://mackie100projects.altervista.org/apps/cloverconf/CCG/builds-data-ccg/CCG.zip'
+  url 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/builds-data-ccg/CCG.zip'
   appcast 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/update-data-builds.xml'
   name 'Clover Configurator'
   homepage 'https://mackie100projects.altervista.org/'
