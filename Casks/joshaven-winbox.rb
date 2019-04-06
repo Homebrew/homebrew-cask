16ad1c6acdb4a5d89a20469720297ab4717ac824
Alex Hall <alex@compuguy1088.com>
2019-04-05 23:07:42 -0400

    Fix issue with url to winbox4mac
    Now points to a zip file.

--- Casks/joshaven-winbox.rb
+++ Casks/joshaven-winbox.rb
@@ -2,7 +2,7 @@ cask 'joshaven-winbox' do
   version '3.18'
   sha256 'ba9d20ab9dc5703c5e3b118f0d080a85110dd9b3ae3daa6c0ec76bb366d74bbc'
 
-  url "https://joshaven.com/Winbox4Mac_#{version}.dmg"
+  url "http://joshaven.com/Winbox4Mac_3.18#{version}.zip"
   appcast 'https://joshaven.com/resources/tools/winbox-for-mac/'
   name 'Winbox4Mac'
   homepage 'https://joshaven.com/resources/tools/winbox-for-mac/'
