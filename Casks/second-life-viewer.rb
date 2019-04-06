b47a6acd79b485458968509c02aa8f836a33bec3
suschizu <brigitte@corecode.io>
2019-04-06 18:19:54 +0200

    Update second-life-viewer.rb

--- Casks/second-life-viewer.rb
+++ Casks/second-life-viewer.rb
@@ -1,5 +1,5 @@
 cask 'second-life-viewer' do
-  version '6.1_1.525446'
+  version '6.1.1.525446'
   sha256 '520e718ed194b7eea322e1e4926eda2aca5a93df878cfd53c5672f98da0dd35f'
 
   url "http://download.cloud.secondlife.com/Viewer_#{version.major}/Second_Life_#{version.dots_to_underscores}_x86_64.dmg"
