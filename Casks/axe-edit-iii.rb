94ea27dea7a77ec259dad151a69a6de504794369
Work <brigitte@corecode.io>
2019-04-06 17:54:49 +0200

    Update axe-edit-iii from 1.01.03 to 1.01.04

--- Casks/axe-edit-iii.rb
+++ Casks/axe-edit-iii.rb
@@ -1,6 +1,6 @@
 cask 'axe-edit-iii' do
-  version '1.01.03'
-  sha256 'a171a0bbb3304142cba1cd3c6c1cbb0433ec95ee4b38006f176a5c63ad21b6a5'
+  version '1.01.04'
+  sha256 '3c4246ee3b0eb47a671ec5c404a03766a6ec48e45251f83638e17ffa19a9d3fe'
 
   url "https://www.fractalaudio.com/downloads/Axe-Edit-III/Axe-Edit-III-OSX-v#{version.tr('.', 'p')}.dmg"
   appcast 'https://www.fractalaudio.com/axe-fx-iii-edit/'
