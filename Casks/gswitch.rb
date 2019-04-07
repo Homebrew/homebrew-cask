141181ed1e7828acfeab3d0f7a834d75d3e9cee8
Vítor Galvão <info@vitorgalvao.com>
2019-04-07 12:51:01 +0100

    Update gswitch.rb

--- Casks/gswitch.rb
+++ Casks/gswitch.rb
@@ -4,7 +4,7 @@ cask 'gswitch' do
 
   # github.com/CodySchrank/gSwitch was verified as official when first introduced to the cask
   url "https://github.com/CodySchrank/gSwitch/releases/download/#{version}/gSwitch.zip"
-  appcast 'https://raw.githubusercontent.com/CodySchrank/gSwitch/master/appcast.xml'
+  appcast 'https://github.com/CodySchrank/gSwitch/releases.atom'
   name 'gSwitch'
   homepage 'https://codyschrank.github.io/gSwitch/'
 
