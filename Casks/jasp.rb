dae8c681a286c7f7e4c55ba1e601136627f1a67d
CoreCode <core-code@users.noreply.github.com>
2019-04-08 00:23:17 +0200

    Update jasp (#61517)

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -3,6 +3,7 @@ cask 'jasp' do
   sha256 'c9df6eb46d37b1996b424051e677a2329953adb92449e667f45b5dbfc506c309'
 
   url "https://static.jasp-stats.org/JASP-#{version}.dmg"
+  appcast 'https://jasp-stats.org/download/'
   name 'JASP'
   homepage 'https://jasp-stats.org/'
 
