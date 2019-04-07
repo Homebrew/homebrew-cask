8722eb30078d977fd026ce5e0d6d88eb4d2f8b5d
Julian Mayer <julian@corecode.io>
2019-04-07 14:58:04 +0200

    Update crypho from latest to 4.1.3

--- Casks/crypho.rb
+++ Casks/crypho.rb
@@ -1,8 +1,9 @@
 cask 'crypho' do
-  version :latest
-  sha256 :no_check
+  version '4.1.3'
+  sha256 '9cec4de034eeba00f867f05e9d4a678193e6cee6a669685435d6918cb9e6272e'
 
-  url 'https://www.crypho.com/downloads/Crypho.dmg'
+  url "https://www.crypho.com/downloads/desktop/Crypho-#{version}.dmg"
+  appcast 'https://www.crypho.com/download/'
   name 'Crypho'
   homepage 'https://www.crypho.com/'
 
