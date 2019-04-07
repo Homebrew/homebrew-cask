952ad2ab87b3783384a7a794e1cbf327e1380fac
Randall <ran.dall@icloud.com>
2019-04-07 06:01:19 -0700

    Update snes9x from 1.58 to 1.59.2

--- Casks/snes9x.rb
+++ Casks/snes9x.rb
@@ -1,6 +1,6 @@
 cask 'snes9x' do
-  version '1.58'
-  sha256 '4723eac4aee3774436d422fbf74206386f56fcc308954414b2ba7c29ac7a1d4f'
+  version '1.59.2'
+  sha256 'a987c3d7e87b3aabe4d252db356f98ca9dbf7d5a252312bb97ef3b5322833476'
 
   # s9x-w32.de was verified as official when first introduced to the cask
   url "http://www.s9x-w32.de/dl/snes9x-#{version}-macosx-i386.zip"
