49d0011e1465f617dc7cc6ef5107790b3fae71be
Vítor Galvão <info@vitorgalvao.com>
2019-04-08 21:45:20 +0100

    Update moneywell.rb

--- Casks/moneywell.rb
+++ Casks/moneywell.rb
@@ -2,8 +2,7 @@ cask 'moneywell' do
   version '3.0.9'
   sha256 '4137e9ed9b1b7fb414c0e66c01ffde0bcd00d28152021ddefd5097ae01c48983'
 
-  # moneywell.app was verified as official when first introduced to the cask
-  url "https://moneywell.app/downloads/sparkle/MoneyWell-#{version}.zip"
+  url "https://moneywellapp.com/downloads/sparkle/MoneyWell-#{version}.zip"
   appcast "https://moneywellapp.com/mw/feeds/#{version.major}x/appcast.xml"
   name 'MoneyWell'
   homepage 'https://moneywellapp.com/'
