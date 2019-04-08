a7a83fe51615f780ecaf7bb5ec3f79519c053989
Ma3yTa <andriy@nowinfinity.com.au>
2019-04-08 09:50:06 +0700

    fman version upd

--- Casks/fman.rb
+++ Casks/fman.rb
@@ -1,5 +1,5 @@
 cask 'fman' do
-  version '1.5.7'
+  version '1.5.9'
   sha256 'b62d277d19a519048674e0dd822177040cd943d30429ed0efd9d7bf14080615f'
 
   url "https://fman.io/updates/mac/#{version}.zip"
