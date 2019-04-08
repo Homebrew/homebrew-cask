1bcaf62c6ffffdd020660e648d40ab34287abd7e
Tom Brek <me@tombrek.com>
2019-04-08 12:55:25 +0100

    Update mweb from 3.2.0 to 3.2.1 (#61551)

--- Casks/mweb.rb
+++ Casks/mweb.rb
@@ -1,6 +1,6 @@
 cask 'mweb' do
-  version '3.2.0'
-  sha256 'c0966a0796fc3a370c652182436673e037195ef63bdb4b9af9bec6cd76429866'
+  version '3.2.1'
+  sha256 'b862a3564b03fe4942f0e665f42448a87fbb49e779004648b0586c9674217576'
 
   # dl.devmate.com/com.coderforart.MWeb3 was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coderforart.MWeb3/MWeb3-ProMarkdownwriting,notetakingandstaticbloggeneratorApp.dmg'
