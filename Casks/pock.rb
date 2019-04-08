db0000c8a9b1fd9a350ada100fd71bd102793166
Vítor Galvão <info@vitorgalvao.com>
2019-04-08 14:24:34 +0100

    Update pock.rb

--- Casks/pock.rb
+++ Casks/pock.rb
@@ -2,8 +2,8 @@ cask 'pock' do
   version '0.4.5'
   sha256 '324e273727ae373d48912b820449e679db0ce9cbc68cb0eb3c9e705daed99b67'
 
-  url 'https://pock.pigigaldi.com/download.php?file=pock_0_4_5_eket.zip'
-  appcast 'https://github.com/pigigaldi/Pock/releases'
+  url "https://pock.pigigaldi.com/download.php?file=pock_#{version.dots_to_underscores}_eket.zip"
+  appcast 'https://github.com/pigigaldi/Pock/releases.atom'
   name 'Pock'
   homepage 'https://pock.pigigaldi.com/'
 
