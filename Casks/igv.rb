911bc31ec0369c2cea61f2a719f19344b8015c8d
Vítor Galvão <info@vitorgalvao.com>
2019-04-08 14:16:50 +0100

    Update igv.rb

--- Casks/igv.rb
+++ Casks/igv.rb
@@ -2,7 +2,7 @@ cask 'igv' do
   version '2.5.0'
   sha256 '6700e22a4768896c607134565f312fa9097ca77bbae84acaa24875e053e0dde9'
 
-  url 'https://data.broadinstitute.org/igv/projects/downloads/2.5/IGV_2.5.0.app.zip'
+  url "https://data.broadinstitute.org/igv/projects/downloads/#{version.major_minor}/IGV_#{version}.app.zip"
   name 'Integrative Genomics Viewer (IGV)'
   homepage 'https://software.broadinstitute.org/software/igv/'
 
