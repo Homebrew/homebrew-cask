1e8ab02c80dce83eb066ea1ad181f61f91699499
Julian Mayer <julian@corecode.io>
2019-04-09 23:41:30 +0200

    Update renamer from 5.2.2 to 5.3.1

--- Casks/renamer.rb
+++ Casks/renamer.rb
@@ -1,6 +1,6 @@
 cask 'renamer' do
-  version '5.2.2'
-  sha256 '4390b0af2889cda86f9124a30040e43e8275864ca57a5c5b2d8e4f0fab66e5a2'
+  version '5.3.1'
+  sha256 'b909bbc98bced9f74bcdc59492674f75e49434a6a75686ab67cc81e88c8d28c5'
 
   # storage.googleapis.com/incrediblebee was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/incrediblebee/apps/Renamer-#{version.major}/Renamer-#{version}.zip"
