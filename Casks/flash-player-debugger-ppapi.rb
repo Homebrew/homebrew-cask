321e0462fed82039f7c4378db2cbbc0cb4154447
Andy Li <andy@onthewings.net>
2019-04-09 13:59:25 +0800

    flash-player-debugger-ppapi 32.0.0.171

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger-ppapi' do
-  version '32.0.0.156'
-  sha256 '9257a943094a541c1627a9b1688c511818ff3c5f77aaecf33b0d4a55097abb00'
+  version '32.0.0.171'
+  sha256 '6bbf9a14a21538764a8f7699fffd9e800cd37771988f0d37aeea21db9fdfda17'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml'
