2c3f3188bce29bf87e968f09bb7af0a708cba54a
Julian Mayer <julian@corecode.io>
2019-04-09 23:30:00 +0200

    Update mjml from 2.9.0 to 2.11.0

--- Casks/mjml.rb
+++ Casks/mjml.rb
@@ -1,9 +1,9 @@
 cask 'mjml' do
-  version '2.9.0'
-  sha256 '0dfae8495120f5150bdca92f9ce0782f71412d56cdcfffb51fbc48b02c6cc318'
+  version '2.11.0'
+  sha256 'df16f34efa6f145f859e74333d6d37235e04a42e1b7d6cb096807d24551588d9'
 
   # github.com/mjmlio/mjml-app was verified as official when first introduced to the cask
-  url "https://github.com/mjmlio/mjml-app/releases/download/v#{version}/mjml-app-#{version}-mac.zip"
+  url "https://github.com/mjmlio/mjml-app/releases/download/#{version}/mjml-app-#{version}-mac.dmg"
   appcast 'https://github.com/mjmlio/mjml-app/releases.atom'
   name 'MJML'
   homepage 'https://mjmlio.github.io/mjml-app/'
