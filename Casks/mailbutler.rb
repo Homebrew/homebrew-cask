58a3af5145a1761c1d46e650f0f3328a2943f3ab
CoreCode <core-code@users.noreply.github.com>
2019-04-09 23:53:39 +0200

    Update mailbutler from 2,1502-11042 to 2,1509-11062 (#61688)

--- Casks/mailbutler.rb
+++ Casks/mailbutler.rb
@@ -1,6 +1,6 @@
 cask 'mailbutler' do
-  version '2,1502-11042'
-  sha256 '4b1ab6437876c50284cf6c28477cb9eabbb6c00d0346c71e65520203f7647f83'
+  version '2,1509-11062'
+  sha256 'e3a52131538c8556dfbc19294f39e5234d955df657048ad1bee1c6683e173ac0'
 
   url "https://downloads.mailbutler.io/Mailbutler_#{version.after_comma}.zip"
   appcast "https://www.mailbutler.io/appcast#{version.major}.php"
