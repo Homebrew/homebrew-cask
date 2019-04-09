0be152ce0deb50ffcdfa4be387cb938bd21b5f55
Julian Mayer <julian@corecode.io>
2019-04-09 23:55:54 +0200

    Update timing from 2018.4.4 to 2018.4.5

--- Casks/timing.rb
+++ Casks/timing.rb
@@ -1,5 +1,5 @@
 cask 'timing' do
-  version '2018.4.4'
+  version '2018.4.5'
   sha256 '24b30768c103d350914b16af193837539f4e1034c4cb036feefbff1a57502476'
 
   url 'https://timingapp.com/download/Timing.app.zip'
