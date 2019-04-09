0530c665c61bf173536d9c6e332d3c68d39a8ad4
Fabian Maass <fabian.maass@adnovum.ch>
2019-04-09 11:46:05 +0200

    Add Bettergram Latest

---
+++ Casks/bettergram.rb
@@ -0,0 +1,10 @@
+cask 'bettergram' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.bettergram.io/downloads/bettergram-mac.zip'
+  name 'bettergram'
+  homepage 'https://www.bettergram.io/'
+
+  app 'Bettergram.app'
+end
