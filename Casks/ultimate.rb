50bedd6393d09628539957dc4af896be1db64d87
Work <brigitte@corecode.io>
2019-04-10 16:57:06 +0200

    Update ultimate from 3.0.11.212 to 3.0.11.409

--- Casks/ultimate.rb
+++ Casks/ultimate.rb
@@ -1,6 +1,6 @@
 cask 'ultimate' do
-  version '3.0.11.212'
-  sha256 '3ebb03e09a9656856a7d0382eac487d98a3d5aa52e66aa77b07925a074c2d949'
+  version '3.0.11.409'
+  sha256 '39ceecc8efd0d05e439be107cf729797000ec2906b078164b3ccc5a99c6149e4'
 
   url 'https://download.epubor.com/epubor_ultimate.zip'
   appcast 'https://www.epubor.com/ultimate.html'
