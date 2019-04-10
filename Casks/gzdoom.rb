6796c27c07dfd18d14a3ea335c76c5de78aa4c40
Randall <ran.dall@icloud.com>
2019-04-10 06:27:10 -0700

    Update gzdoom from 3.7.2 to 4.0.0

--- Casks/gzdoom.rb
+++ Casks/gzdoom.rb
@@ -1,6 +1,6 @@
 cask 'gzdoom' do
-  version '3.7.2'
-  sha256 'a8f7d7453e3b5926a0606098fbed3433af39176512298e6f9c654e9a27626477'
+  version '4.0.0'
+  sha256 'cec834ab614ba5a514a808db68d2a63c7f4227b869694267e855fcc928dee384'
 
   url "https://zdoom.org/files/gzdoom/bin/gzdoom-bin-#{version.dots_to_hyphens}.dmg"
   appcast 'https://github.com/coelckers/gzdoom/releases.atom'
