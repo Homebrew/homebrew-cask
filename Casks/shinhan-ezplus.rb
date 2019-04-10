002fdab85f537679af614d168bb47a7ed6d26e1a
Vítor Galvão <info@vitorgalvao.com>
2019-04-10 18:03:35 +0100

    Update shinhan-ezplus.rb

--- Casks/shinhan-ezplus.rb
+++ Casks/shinhan-ezplus.rb
@@ -3,7 +3,7 @@ cask 'shinhan-ezplus' do
   sha256 :no_check
 
   url 'https://image.shinhan.com/nexhpe/document/ezplusmac.zip'
-  name 'Shinhan ezPlus for Mac'
+  name 'Shinhan ezPlus'
   homepage 'https://main.shinhan.com/hpe/index.jsp#040201040200'
 
   pkg 'SHBEzPlusMac.pkg'
