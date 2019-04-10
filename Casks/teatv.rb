6cad860adffc56dff4332d3263953a9259b9b1b4
XeL_Arjona <shell_ramix@hotmail.com>
2019-04-10 09:30:04 -0500

    Update teatv.rb
    Fix url

--- Casks/teatv.rb
+++ Casks/teatv.rb
@@ -3,7 +3,7 @@ cask 'teatv' do
   sha256 '92ed3632f5bd5baf8b25700f09db2d05d59e74f81277b88a89c7b0e4d3758b55'
   
   # github.com/TeaTV/TeaTV-macOS was verified as official when first introduced to the cask
-  url 'https://github.com/TeaTV/TeaTV-macOS/releases/download/v#{version}/TeaTV.v#{version}.dmg'
+  url "https://github.com/TeaTV/TeaTV-macOS/releases/download/v#{version}/TeaTV.v#{version}.dmg"
   appcast 'https://github.com/TeaTV/TeaTV-macOS/releases.atom'
   name 'TeaTV macOS Desktop App'
   homepage 'http://teatv.net/'
