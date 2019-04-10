b8dc2dcea97010e9f499bc81ea76b3f398bca73d
XeL_Arjona <shell_ramix@hotmail.com>
2019-04-10 01:50:09 -0500

    Update teatv.rb
    versioning number edit.

--- Casks/teatv.rb
+++ Casks/teatv.rb
@@ -2,8 +2,8 @@ cask 'teatv' do
   version '1.5.0'
   sha256 '92ed3632f5bd5baf8b25700f09db2d05d59e74f81277b88a89c7b0e4d3758b55'
   
-  # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/TeaTV/TeaTV-macOS/releases/download/v1.5.0/TeaTV.v1.5.0.dmg'
+  # github.com/TeaTV/TeaTV-macOS was verified as official when first introduced to the cask
+  url 'https://github.com/TeaTV/TeaTV-macOS/releases/download/v#{version}/TeaTV.v#{version}.dmg'
   appcast 'https://github.com/TeaTV/TeaTV-macOS/releases.atom'
   name 'TeaTV macOS Desktop App'
   homepage 'http://teatv.net/'
