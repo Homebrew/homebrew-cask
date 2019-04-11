7e202ba83d5ca5bb5e7ae60f836bae0c8cbe29c0
Work <brigitte@corecode.io>
2019-04-11 18:42:00 +0200

    Update jedit-omega from 2.10 to 2.11

--- Casks/jedit-omega.rb
+++ Casks/jedit-omega.rb
@@ -3,8 +3,8 @@ cask 'jedit-omega' do
     version '1.32'
     sha256 '251697fe6b76419b2cef41a89a2cb7d2e3f8caab0bc1ae82aed6c14a366fdad9'
   else
-    version '2.10'
-    sha256 'cbeeaca139635937b37629ad940fc38db07548cffb49e15fffc97686ba2c3a39'
+    version '2.11'
+    sha256 '6ce527fca6fa9803215e0a85de6e0909bf0afc9b82c71d0d6c3127aa36dfad43'
   end
 
   # artman21.co.jp was verified as official when first introduced to the cask
