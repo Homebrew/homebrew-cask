f090b1b1b1fa54e2a092321c4f873d7f6e21de1e
Julian Mayer <julian@corecode.io>
2019-04-11 18:58:12 +0200

    Update bink-player

--- Casks/bink-player.rb
+++ Casks/bink-player.rb
@@ -2,7 +2,7 @@ cask 'bink-player' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.radgametools.com/down/Bink/BinkMacPlayer.dmg'
+  url 'http://www.radgametools.com/down/Bink/BinkMacIntelPlayer.dmg'
   name 'Bink Video Player'
   homepage 'http://www.radgametools.com/bnkdown.htm'
 
