856a887c407a2b28092a81dfc7694503a303ad92
Markus Reiter <me@reitermark.us>
2019-04-12 00:46:53 +0200

    Update freefilesync.rb

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,9 +1,8 @@
 cask 'freefilesync' do
-  version '10.10'
+  version '10.11'
   sha256 '1a2e7883ad9e255ef18e1637a93c714b798f39db85a32ae95eef91789a875fc3'
 
-  # download2056.mediafire.com/97r5il51pvag/occuw8pd0p227zr was verified as official when first introduced to the cask
-  url "http://download2056.mediafire.com/97r5il51pvag/occuw8pd0p227zr/FreeFileSync_#{version}_macOS.zip",
+  url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake,
       referer:    'https://freefilesync.org/download.php'
   name 'FreeFileSync'
