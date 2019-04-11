adeab130f67ead0f2e449a8a6e916be48a86e61f
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-11 11:25:27 -0700

    Update sleipnir from 4.6.4 to 4.6.5 (#61818)

--- Casks/sleipnir.rb
+++ Casks/sleipnir.rb
@@ -1,6 +1,6 @@
 cask 'sleipnir' do
-  version '4.6.4'
-  sha256 '222424ce754db4eb162ce25890351b551a15d8d1eb3312aabd9fe1ae412ea6fc'
+  version '4.6.5'
+  sha256 'fd87b83113fa4205c1ba371f8b05a87aa30b46953549276ee1d28a87653ad6ec'
 
   url 'https://www.fenrir-inc.com/services/download.php?file=Sleipnir.dmg'
   appcast 'https://update.fenrir.co.jp/smartupdate/mac/sleipnir/appcast.xml'
