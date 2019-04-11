d858a681e6f58115725af85a5cd5ec9cbe905238
Markus Reiter <me@reitermark.us>
2019-04-12 01:00:54 +0200

    Update freefilesync.rb

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,6 +1,6 @@
 cask 'freefilesync' do
   version '10.11'
-  sha256 '1a2e7883ad9e255ef18e1637a93c714b798f39db85a32ae95eef91789a875fc3'
+  sha256 'c3e4e23bfe4189f57b6a46d50f4fe0dfd9cd5ff258da6c4b0e886e4e3588adbb'
 
   url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake,
