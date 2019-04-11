28e4f133cbcd5b5f12d2858ddc08cf944b4488db
suschizu <brigitte@corecode.io>
2019-04-11 16:35:14 +0200

    Update wondershare-video-converter-ultimate from 10.3.3.6,735 to 10.5.0.8,735 (#61785)
    * Update wondershare-video-converter-ultimate from 10.3.3.6,735 to 10.5.0.8,735
    
    * Update and rename wondershare-video-converter-ultimate.rb to uniconverter.rb

--- Casks/uniconverter.rb
+++ Casks/uniconverter.rb
@@ -1,12 +1,12 @@
-cask 'wondershare-video-converter-ultimate' do
-  version '10.3.3.6,735'
-  sha256 '185b01405409148ad2155eaff0cf545332d23b6c86a76d48ad9a20a46e09b9fc'
+cask 'uniconverter' do
+  version '10.5.0.8,735'
+  sha256 'a1aee6704cdfda186a18c62a2de7d22e0003079260bfb05b1d96de5bd864357e'
 
   url "http://download.wondershare.com/cbs_down/video-converter-ultimate-mac_full#{version.after_comma}.dmg"
-  name 'Wondershare Video Converter Ultimate'
+  name 'UniConverter'
   homepage 'https://videoconverter.wondershare.com/'
 
-  app 'Wondershare Video Converter Ultimate.app'
+  app 'UniConverter.app'
 
   zap trash: [
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.wondershare.video-converter-ultimate.sfl*',
