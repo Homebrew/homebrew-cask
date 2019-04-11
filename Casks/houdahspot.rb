44093caf642822cdb03ac928a1ed38bea772d522
Julian Mayer <julian@corecode.io>
2019-04-11 11:50:57 +0200

    Update houdahspot from 5.0 to 5.0.1

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,6 +1,6 @@
 cask 'houdahspot' do
-  version '5.0'
-  sha256 '6aa7e5970a7ba2396fe582346d0f1e9c50b29538e7cdbef93af93ae5355ee09a'
+  version '5.0.1'
+  sha256 'f3096898345807fe607182e014161cacd9d434b13795d7f7e254a975a52bd69f'
 
   url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
