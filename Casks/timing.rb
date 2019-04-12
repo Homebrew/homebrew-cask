39f3e721f342624cc9df7f2e78f92bafa4d2f89c
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-12 09:25:48 -0700

    Update timing from 2018.4.5 to 2018.4.6 (#61879)

--- Casks/timing.rb
+++ Casks/timing.rb
@@ -1,6 +1,6 @@
 cask 'timing' do
-  version '2018.4.5'
-  sha256 '24b30768c103d350914b16af193837539f4e1034c4cb036feefbff1a57502476'
+  version '2018.4.6'
+  sha256 '07b363028ef3a066bad45e2441bc2efde0b051af806b479bc1ef3c05975ad789'
 
   url 'https://timingapp.com/download/Timing.app.zip'
   appcast 'https://timingapp.com/updates/timing2.xml'
