ff0778bf23a206326f4f51929a953f5b08d0d99b
CoreCode <core-code@users.noreply.github.com>
2019-04-12 15:10:41 +0200

    Update futuniuniu from 9.2.484_201902141709 to 9.3.492_201903281501 (#61840)

--- Casks/futuniuniu.rb
+++ Casks/futuniuniu.rb
@@ -1,9 +1,10 @@
 cask 'futuniuniu' do
-  version '9.2.484_201902141709'
-  sha256 'c988ab0dc1ac95d066d9ce047b4660c16c1f8d5cea1dd852f83d45c6dae9169b'
+  version '9.3.492_201903281501'
+  sha256 '14daa15a44cad868946e42f49d026333da61a83a6bee5dd5deae13a52d16a6c7'
 
   # software-file-1251001049.cosgz.myqcloud.com was verified as official when first introduced to the cask
   url "https://software-file-1251001049.cosgz.myqcloud.com/FTNNForMac_#{version}_std_release_std_officially.dmg"
+  appcast 'https://www.futu5.com/tools'
   name 'FutuNiuniu'
   name '富途牛牛'
   homepage 'https://hk.futu5.com/'
