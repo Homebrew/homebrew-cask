473c07f2a1feb5886942fa56b8b7e431286f2a63
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-12 09:03:49 -0700

    Update dashlane from 6.1913.0.19270 to 6.1914.0.19481 (#61875)

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,6 +1,6 @@
 cask 'dashlane' do
-  version '6.1913.0.19270'
-  sha256 '6587e6c511f17a988c3e8796de48b821468f607755857c9bc42bb77a8bbc3d2e'
+  version '6.1914.0.19481'
+  sha256 '0d3852cb0b76e4fe20a541722f2ce56c1d79042b809dd76f824ffa1e15121658'
 
   url "https://cdn5.dashlane.com/proxy/d3mfqat9ni8wb5/releases/#{version.major_minor_patch}/#{version}/release/Dashlane.dmg"
   appcast 'https://ws1.dashlane.com/5/binaries/query?target=archive&format=xml&currentSoftwareVersion=6.0.0&platform=server_osx&os=OS_X_10_14_1'
