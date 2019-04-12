0dac2f361505f7b978e60767e59e652de4269881
WouterJanson <wouterjanson@icloud.com>
2019-04-12 14:38:11 +0200

    Add openbook-desktop v0.0.34-3

---
+++ Casks/openbook-desktop.rb
@@ -0,0 +1,10 @@
+cask 'openbook-desktop' do
+  version '0.0.34-3'
+  sha256 'e7b90eb7b48c807b102fe3d3e1b8dfe5ce450d17c306a7e7f2039269d8a23767'
+
+  url 'https://gitlab.com/999eagle/openbook-desktop/-/jobs/artifacts/0.0.34-3/download?job=release-macos'
+  name 'openbook-desktop'
+  homepage 'https://gitlab.com/999eagle/openbook-desktop'
+
+  app 'OpenbookDesktop.app'
+end
