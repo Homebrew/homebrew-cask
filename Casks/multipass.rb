99ae4790c1950eac12cf272979c0b16c45bfc737
Joe Borg <cyborg101010@gmail.com>
2019-04-12 15:19:25 +0100

    Update cask to reflect changes in multipass 0.6.0 (#61768)
    * Update cask to reflect changes in multipass 0.6.0
    
    * Update multipass.rb

--- Casks/multipass.rb
+++ Casks/multipass.rb
@@ -1,13 +1,13 @@
 cask 'multipass' do
-  version '0.5'
-  sha256 'b9cdf4bdb65fcebd4cdff510f3a559d2c86d591749bd3d4ea1ec0dd495a42590'
+  version '0.6.0'
+  sha256 '8883682aecea31efd4afa2838d692a89aceda50d72810405dfbd2747d0e62715'
 
-  url "https://github.com/CanonicalLtd/multipass/releases/download/v#{version}/multipass-v#{version}-full-Darwin.pkg"
+  url "https://github.com/CanonicalLtd/multipass/releases/download/v#{version}/multipass-#{version}+mac-Darwin.pkg"
   appcast 'https://github.com/CanonicalLtd/multipass/releases.atom'
   name 'Multipass'
   homepage 'https://github.com/CanonicalLtd/multipass/'
 
-  pkg "multipass-v#{version}-full-Darwin.pkg"
+  pkg "multipass-#{version} mac-Darwin.pkg"
 
   uninstall launchctl: 'com.canonical.multipassd',
             pkgutil:   'com.canonical.multipass.*',
