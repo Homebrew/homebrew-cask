0e3c0843e48dc442789eeb55f8f69ca082a8cf79
Vítor Galvão <info@vitorgalvao.com>
2019-04-12 14:07:40 +0100

    Update and rename openbook-desktop.rb to openbookdesktop.rb

--- Casks/openbookdesktop.rb
+++ Casks/openbookdesktop.rb
@@ -1,8 +1,8 @@
-cask 'openbook-desktop' do
+cask 'openbookdesktop' do
   version '0.0.34-3'
   sha256 'e7b90eb7b48c807b102fe3d3e1b8dfe5ce450d17c306a7e7f2039269d8a23767'
 
-  url 'https://gitlab.com/999eagle/openbook-desktop/-/jobs/artifacts/0.0.34-3/download?job=release-macos'
+  url "https://gitlab.com/999eagle/openbook-desktop/-/jobs/artifacts/#{version}/download?job=release-macos"
   name 'openbook-desktop'
   homepage 'https://gitlab.com/999eagle/openbook-desktop'
 
