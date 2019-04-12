024d489d9049d0f7454a77c1346874ded96b815a
Yurii Kolesnykov <root@yurikoles.com>
2019-04-12 14:54:41 +0300

    Update xamarin-mac from 5.6.0.25 to 5.8.0.0

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-mac' do
-  version '5.6.0.25'
-  sha256 'b319c04ed3c0ab7542efe40846b8c45d20df3f21bd37c6b17e950f2a7083fbe2'
+  version '5.8.0.0'
+  sha256 'eedd8bbdd0067ca4f56426856e98961b4d4a11d6a17776c49e926bda4ecc7902'
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
