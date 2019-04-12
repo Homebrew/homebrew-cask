d42dbf3587ec77037fae7dd96c26652985336214
kPherox <admin@mail.kr-kp.com>
2019-04-13 00:07:01 +0900

    Update thedesk to 18.2.1

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,12 +1,12 @@
 cask 'thedesk' do
-  version '18.2.0'
-  sha256 '0825725c99ef67b46fc586ac67b784b338051881c1a9964742a7d801eccfb474'
+  version '18.2.1'
+  sha256 '53b2175449700988e4b0a9e215672648f2a508b73a991b8401f62fda8c5fb5c8'
 
   # github.com/cutls/TheDesk was verified as official when first introduced to the cask
-  url "https://github.com/cutls/TheDesk/releases/download/m#{version}/TheDesk-darwin-x64.zip"
+  url "https://github.com/cutls/TheDesk/releases/download/#{version}/TheDesk-darwin-x64.zip"
   appcast 'https://github.com/cutls/TheDesk/releases.atom'
   name 'TheDesk'
   homepage 'https://thedesk.top/'
 
-  app 'TheDesk-darwin-x64/TheDesk.app'
+  app 'TheDesk.app'
 end
