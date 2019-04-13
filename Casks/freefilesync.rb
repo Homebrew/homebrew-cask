2018243b5c922dc180774546269aef95670e05a7
Markus Reiter <me@reitermark.us>
2019-04-13 22:21:42 +0200

    Use `:no_check` for FreeFileSync. (#61923)

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,6 +1,6 @@
 cask 'freefilesync' do
   version '10.11'
-  sha256 'c3e4e23bfe4189f57b6a46d50f4fe0dfd9cd5ff258da6c4b0e886e4e3588adbb'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip",
       user_agent: :fake,
