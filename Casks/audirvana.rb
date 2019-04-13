62c8523254eff6775d7f7746fd175daefc0befb7
mahori <mahori@users.noreply.github.com>
2019-04-13 14:54:57 +0900

    Update audirvana to 3.5.1

--- Casks/audirvana.rb
+++ Casks/audirvana.rb
@@ -1,6 +1,6 @@
 cask 'audirvana' do
-  version '3.5'
-  sha256 'df54107959fc6a7f8eb64ed1866add7cf78f2a2527fff8de9ec89767b3a7b9b9'
+  version '3.5.1'
+  sha256 '0ef2efe5c295ceb286ef3daec26fcc0b304a285604109b0e4831f4531d042c9e'
 
   url "https://audirvana.com/delivery/Audirvana_#{version}.dmg"
   appcast "https://audirvana.com/delivery/audirvanaplus#{version.major}_#{version.minor}_appcast.xml"
