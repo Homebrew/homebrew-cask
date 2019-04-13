365059a1574dbff7d5f01f6fcb1c9da2bc36b4aa
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-13 09:16:25 -0700

    Update lidarr from 0.5.0.583 to 0.6.0.815 (#61915)

--- Casks/lidarr.rb
+++ Casks/lidarr.rb
@@ -1,6 +1,6 @@
 cask 'lidarr' do
-  version '0.5.0.583'
-  sha256 'fad6fdf70bf95f002e09533d3c7b821bd55111d2ee60c597bcb63002a072c422'
+  version '0.6.0.815'
+  sha256 '5dcb950ce62ccf3e831e90995714dbc48e8a49c0d9f93953c51d146bc56d91f4'
 
   # github.com/lidarr/Lidarr was verified as official when first introduced to the cask
   url "https://github.com/lidarr/Lidarr/releases/download/v#{version}/Lidarr.develop.#{version}.osx-app.zip"
