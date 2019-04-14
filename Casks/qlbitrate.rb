25bc2dc96f68d69ad7f2949c88c97532f990bda4
Vítor Galvão <info@vitorgalvao.com>
2019-04-14 03:03:15 +0100

    Update qlbitrate.rb

--- Casks/qlbitrate.rb
+++ Casks/qlbitrate.rb
@@ -2,8 +2,8 @@ cask 'qlbitrate' do
   version '1.1'
   sha256 '4b164f258c146248194379d5d559be381b7d5cfc13e395c1e050ceba245ab7bb'
 
-  url 'https://github.com/jordansaints/qlBitRate/releases/download/v1.1/qlBitRate.qlgenerator.1.1.zip'
-  appcast 'https://github.com/jordansaints/qlBitRate/releases'
+  url "https://github.com/jordansaints/qlBitRate/releases/download/v#{version}/qlBitRate.qlgenerator.#{version}.zip"
+  appcast 'https://github.com/jordansaints/qlBitRate/releases.atom'
   name 'qlBitRate'
   homepage 'https://github.com/jordansaints/qlBitRate'
 
