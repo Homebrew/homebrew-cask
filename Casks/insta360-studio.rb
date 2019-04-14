d6a6a66a6fddb82311fae8261aa98ffc3ffeda50
Vítor Galvão <info@vitorgalvao.com>
2019-04-14 03:03:58 +0100

    Update insta360-studio.rb

--- Casks/insta360-studio.rb
+++ Casks/insta360-studio.rb
@@ -3,7 +3,7 @@ cask 'insta360-studio' do
   sha256 '251b9b7c60f23883eeaa2266e7c64e91b97e356634f22d0cdc91e119afe70a38'
 
   url "https://static.insta360.com/software/Studio/Mac/Insta360%20Studio%202019_Mac_#{version}.pkg"
-  name 'Insta360 Studio 2019'
+  name 'Insta360 Studio'
   homepage 'https://www.insta360.com/'
 
   pkg "Insta360 Studio 2019_Mac_#{version}.pkg"
