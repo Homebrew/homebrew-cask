d7b65829b77b761cb796ac62c3ae289d00502bcd
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-14 07:17:51 -0700

    Update zoomus from 4.3.59242.0310 to 4.4.52534.0413 (#61949)

--- Casks/zoomus.rb
+++ Casks/zoomus.rb
@@ -1,6 +1,6 @@
 cask 'zoomus' do
-  version '4.3.59242.0310'
-  sha256 '7d9607abcc73d984c1ccdf7a218a446974bb8f72a03e59d74349fc50baa4379b'
+  version '4.4.52534.0413'
+  sha256 'b6a3841b6b11d7ed74ba8bf2cd68e39bb0a396999b29657327a77e88f8713620'
 
   url "https://www.zoom.us/client/#{version}/zoomusInstaller.pkg"
   appcast 'https://support.zoom.us/hc/en-us/articles/201361963-New-Updates-for-Mac-OS'
