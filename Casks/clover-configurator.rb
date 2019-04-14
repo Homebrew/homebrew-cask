169271bd3e31b501a1b30705b10f10b05e995829
Phuc Nguyen Minh <minhphuc429@gmail.com>
2019-04-14 19:44:11 +0900

    Update clover-configurator.rb (#61927)
    Clover Configurator update to version 5.4.3.1

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,5 +1,5 @@
 cask 'clover-configurator' do
-  version '5.4.3.0'
+  version '5.4.3.1'
   sha256 'cef05a253dbb51605332d0424e16e4164b758a4ca69d5e162d7e9862370bb196'
 
   url 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/builds-data-ccg/CCG.zip'
