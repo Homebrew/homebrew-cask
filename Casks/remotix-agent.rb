e16b9f17306fae1101fc1fefd5b08648255eec60
Stu Bevan <stu@mees.st>
2019-04-14 09:04:39 +0100

    Remotix Agent.app v1.0.6

---
+++ Casks/remotix-agent.rb
@@ -0,0 +1,18 @@
+cask 'remotix-agent' do
+  version '1.0.6'
+  sha256 '66ecec55fa40ead964ad8da472c99d4eda3a1cbf65e60e531f0fd790f5743b11'
+
+  url 'https://downloads.remotixcloud.com/agent-mac/RemotixAgent.pkg'
+  name 'Remotix Agent'
+  homepage 'https://remotixcloud.com/'
+
+  auto_updates true
+
+  pkg 'RemotixAgent.pkg'
+
+  uninstall login_item: 'Remotix Agent',
+            pkgutil:    [
+                          'com.nuana.rxagentmac.daemon',
+                          'com.nulana.rxagentmac',
+                        ]
+end
