e9fba58bbe1795aa4de31417a2c07b3887fc152d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-15 07:43:18 -0700

    Update qtum-qt from 0.17.2 to 0.17.3 (#61939)

--- Casks/qtum-qt.rb
+++ Casks/qtum-qt.rb
@@ -1,6 +1,6 @@
 cask 'qtum-qt' do
-  version '0.17.2'
-  sha256 '90c1f2e5c2f66c2b101826c4ce4dd02ad52bf5510b61aa97e4df7bc366b6133c'
+  version '0.17.3'
+  sha256 'cbb8fc885ad327b13e71e4a4357884d1723e0a212557cdd85798d5f74334e01c'
 
   # github.com/qtumproject/qtum was verified as official when first introduced to the cask
   url "https://github.com/qtumproject/qtum/releases/download/mainnet-ignition-v#{version}/qtum-#{version}-osx-unsigned.dmg"
