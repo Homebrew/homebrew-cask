daf707f41eea66f822bfdf4bbbab7208bc9720a7
kPherox <admin@mail.kr-kp.com>
2019-04-16 00:16:36 +0900

    Fix checksum and download link for thedesk

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,9 +1,9 @@
 cask 'thedesk' do
   version '18.2.3'
-  sha256 'ed97a9e0696939ea8bcfb7493eff8d849cbdaca9caf61334a17e01fa1db99007'
+  sha256 'b9fe3bfb84776378600a246e806cb189e1abb6cc46696fb0dc429ce675130deb'
 
   # github.com/cutls/TheDesk was verified as official when first introduced to the cask
-  url "https://github.com/cutls/TheDesk/releases/download/#{version}/TheDesk-darwin-x64.zip"
+  url "https://github.com/cutls/TheDesk/releases/download/#{version}/TheDesk-#{version}.dmg"
   appcast 'https://github.com/cutls/TheDesk/releases.atom'
   name 'TheDesk'
   homepage 'https://thedesk.top/'
