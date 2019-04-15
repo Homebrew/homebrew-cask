0517bfa74eb8b4c060c89e283d91da95aaad0034
Morten <Mortennn1@protonmail.com>
2019-04-15 09:36:36 +0200

    Update Dozer to v3.0.5

--- Casks/dozer.rb
+++ Casks/dozer.rb
@@ -1,6 +1,6 @@
 cask 'dozer' do
-  version '3.0.4'
-  sha256 '1dede8c4e4e509b5fc8275fa9bca2a968ed05db596468f0f8038d970056e1f36'
+  version '3.0.5'
+  sha256 '8d79dfd8b1678d833618a2b65003cc596461f573c5d5e404f6d7a6c5a4e91278'
 
   url "https://github.com/Mortennn/Dozer/releases/download/v#{version}/Dozer.#{version}.dmg"
   appcast 'https://github.com/Mortennn/Dozer/releases.atom'
