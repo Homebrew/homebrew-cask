d9ca1d1a351695e5e391df5466610cfd089d7862
Lasse Bang Mikkelsen <lbm@lbm.dk>
2019-04-15 21:20:11 +0200

    Update betaflight-configurator (#61976)

--- Casks/betaflight-configurator.rb
+++ Casks/betaflight-configurator.rb
@@ -1,6 +1,6 @@
 cask 'betaflight-configurator' do
   version '10.5.1'
-  sha256 '22701436e2049ee80d439f79c9cd4a4ce40027c103b6fec4432e5aa3883dffdf'
+  sha256 '6ee33a722fa5d86e369676ecea4968a347fe5df923052ecc469ae70ab7e582c7'
 
   url "https://github.com/betaflight/betaflight-configurator/releases/download/#{version}/betaflight-configurator_#{version}_macOS.dmg"
   appcast 'https://github.com/betaflight/betaflight-configurator/releases.atom'
