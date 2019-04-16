2a9b7fead59c263fff8286fb3b7d52ab067fe7d4
Jason Di Benedetto <jason@mangochutney.com.au>
2019-04-17 08:23:29 +0930

    Update unity from 2018.3.12f1,8afd630d1f5b to 2019.1.0f2,292b93d75a2c (#62024)

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask 'unity' do
-  version '2018.3.12f1,8afd630d1f5b'
-  sha256 'ba3dd3bddfab49fc5bf7d06ee7e2586f7914a2b5a6e197e4be1d1268183a1c13'
+  version '2019.1.0f2,292b93d75a2c'
+  sha256 '995d9c0e73f3efff9b3b86cbec52deaa0c8dda83977622740f9227e2655841b5'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/download/archive'
