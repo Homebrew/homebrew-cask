dcc966ea806f5025116352634b1caa26aa5bea9e
Randall <ran.dall@icloud.com>
2019-04-16 08:32:48 -0700

    Update swimat from 1.6 to 1.6.1

--- Casks/swimat.rb
+++ Casks/swimat.rb
@@ -1,6 +1,6 @@
 cask 'swimat' do
-  version '1.6'
-  sha256 '1d76589e074c2d05351de098d1d78156b96dfb9e5bfd01e421b6e0373e0a0101'
+  version '1.6.1'
+  sha256 '712719277aea8209bec2bea88438af3a391cacd7dd5a0b490b840868d4f3362d'
 
   url "https://github.com/Jintin/Swimat/releases/download/v#{version}/Swimat.zip"
   appcast 'https://github.com/Jintin/Swimat/releases.atom'
