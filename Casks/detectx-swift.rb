f8b0a6da67d144d6275e5a62289aff2c4014ea1b
Vítor Galvão <info@vitorgalvao.com>
2019-04-16 03:05:17 +0100

    Update detectx-swift.rb

--- Casks/detectx-swift.rb
+++ Casks/detectx-swift.rb
@@ -14,20 +14,6 @@ cask 'detectx-swift' do
 
   app 'DetectX Swift.app'
 
-  if MacOS.version <= :mojave
-    # https://sqwarq.com/2018/12/03/how-to-add-detectx-to-full-disk-access/
-    caveats <<~EOS
-      On macOS 10.14 Mojave, DetectX Swift requires Full Disk Access in order to fully protect your Mac.
-       Without this access, some areas where adware and malware can hide may be hidden from DetectX’s search and profile functions.
-       Therefore, we recommend that you add DetectX Swift to the Full Disk Access pane in System Preferences’ Security & Privacy tab.
-       1. Open System Preferences.app (you can search for it in Spotlight or the Finder).
-      2. Select the Privacy tab, and then select Full Disk Access in the sidebar.
-      3. Click the padlock in the bottom left corner and enter an Admin password to unlock it.
-      4. Drag the DetectX Swift icon from your Applications folder and drop it onto the list view in System Preferences.
-      5. Click the padlock once more to prevent further changes.
-    EOS
-  end
-
   zap trash: [
                '~/Library/Application Support/DetectX Swift',
                '~/Library/Application Support/com.sqwarq.DetectX-Swift',
