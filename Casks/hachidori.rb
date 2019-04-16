6e6c0dd138f4f2f4b7806f9bdd3c6c0c54eb54ca
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-16 08:31:35 -0700

    Update hachidori from 3.1.5 to 3.1.6 (#61997)

--- Casks/hachidori.rb
+++ Casks/hachidori.rb
@@ -1,6 +1,6 @@
 cask 'hachidori' do
-  version '3.1.5'
-  sha256 '73d4dd0db905b56d64ab3005eabfac6795692be13a4b4a102323af03c62a3c72'
+  version '3.1.6'
+  sha256 '7c9e9147417d07b5c237b16c79e99989332976202f4d069f5ff9cf8ad0618f40'
 
   # github.com/Atelier-Shiori/hachidori was verified as official when first introduced to the cask
   url "https://github.com/Atelier-Shiori/hachidori/releases/download/#{version}/hachidori-#{version}.dmg"
