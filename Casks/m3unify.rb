87f29ec589147f27a04b70cfee3c7bf4645c6c7f
Randall <ran.dall@icloud.com>
2019-04-17 06:22:15 -0700

    Update m3unify from 1.12.0 to 1.12.1

--- Casks/m3unify.rb
+++ Casks/m3unify.rb
@@ -1,6 +1,6 @@
 cask 'm3unify' do
-  version '1.12.0'
-  sha256 '3147a00abee9528cca55767a54f345de5d5d27177371b6f9a6a368701cbef7a3'
+  version '1.12.1'
+  sha256 'cc961fb2cd380f646db79d4cc82147d82473a642c4544ae11d6aeef4cd167bdf'
 
   url "https://dougscripts.com/itunes/scrx/m3unifyv#{version.no_dots}.zip"
   appcast 'https://dougscripts.com/itunes/itinfo/m3unify_appcast.xml'
