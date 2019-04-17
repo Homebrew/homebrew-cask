b90b84388c288bd6ec488a3d8646c0b6cd981f67
mystygage <mystygage@users.noreply.github.com>
2019-04-17 10:30:09 +0200

    Update corretto from 11.0.2.9.3 to 11.0.3.7.1 (#62037)

--- Casks/corretto.rb
+++ Casks/corretto.rb
@@ -1,6 +1,6 @@
 cask 'corretto' do
-  version '11.0.2.9.3'
-  sha256 '87d26f592e29981807229008e5bbdbaa58d0a80e80d71cdcd86732b4203596b3'
+  version '11.0.3.7.1'
+  sha256 '1e9b1eb77b51d401619ee5ea433e2b4d61a75c29f0a3255fc0c06e81fa44a650'
 
   # d3pxv6yz143wms.cloudfront.net was verified as official when first introduced to the cask
   url "https://d3pxv6yz143wms.cloudfront.net/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
