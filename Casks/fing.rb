55a83b1688b56eb0e8fdea326834abb700030f79
Vítor Galvão <info@vitorgalvao.com>
2019-04-17 16:51:33 +0100

    fing.rb: removed _to_slahses

--- Casks/fing.rb
+++ Casks/fing.rb
@@ -1,9 +1,9 @@
 cask 'fing' do
-  version '3.0,2016-09'
+  version '3.0,2016.09'
   sha256 'a8497ce00d58609d8677c6c7850e479420516d94e9f37d681dbdc970359294c6'
 
   # 39qiv73eht2y1az3q51pykkf-wpengine.netdna-ssl.com was verified as official when first introduced to the cask
-  url "https://39qiv73eht2y1az3q51pykkf-wpengine.netdna-ssl.com/wp-content/uploads/#{version.after_comma.hyphens_to_slashes}/overlook-fing-#{version.before_comma}.dmg_.zip"
+  url "https://39qiv73eht2y1az3q51pykkf-wpengine.netdna-ssl.com/wp-content/uploads/#{version.after_comma.major}/#{version.after_comma.minor}/overlook-fing-#{version.before_comma}.dmg_.zip"
   name 'Fing'
   homepage 'https://www.fing.io/'
 
