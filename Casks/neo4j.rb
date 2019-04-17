27d78bfca17f2089110c30eba576a118db1e161a
Randall <ran.dall@icloud.com>
2019-04-17 06:46:11 -0700

    Update neo4j from 1.1.13 to 1.1.20

--- Casks/neo4j.rb
+++ Casks/neo4j.rb
@@ -1,7 +1,7 @@
 cask 'neo4j' do
   # note: "4" is not a version number, but an intrinsic part of the product name
-  version '1.1.13'
-  sha256 'e14f9332218f4d6e2ff292e3b036bde2f48f5fc63250319abedaa47d68c1c7c2'
+  version '1.1.20'
+  sha256 '800671362cc7684ecc459398e8684de27a5bcf4db0580d2a17165a8fd662aae5'
 
   url "https://neo4j.com/artifact.php?name=neo4j-desktop-offline-#{version}.dmg"
   name 'Neo4j Desktop'
