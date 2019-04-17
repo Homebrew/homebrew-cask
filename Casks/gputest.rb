1322847c6c9db28fb95dd4ca98acb8bcc353a3d4
Yurii Kolesnykov <root@yurikoles.com>
2019-04-17 20:01:07 +0300

    new cask: gputest, 0.7.0

---
+++ Casks/gputest.rb
@@ -0,0 +1,11 @@
+cask 'gputest' do
+  version '0.7.0'
+  sha256 'b02cd3c381c751bdc080e7bd004267d219d6112f5ecf9ab1db94196ccc85d7c0'
+
+  url 'https://www.geeks3d.com/dl/get/391'
+  name 'GpuTest'
+  homepage 'http://www.geeks3d.com/GpuTest/'
+
+  app 'GpuTest.app'
+  app 'GpuTest_GUI.app'
+end
