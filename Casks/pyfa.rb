2075a170420ac72c65217f236e7fc8c21de462f1
Austin Kootz <austin.kootz@live.com>
2019-04-18 13:29:58 -0600

    Update to PyFA Version 2.9.1 (#62137)
    changed version number and sha256 hash.

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,6 +1,6 @@
 cask 'pyfa' do
-  version '2.9.0'
-  sha256 'aacbc789a528585409fda7f1bad75a5c237ef4ee85bbfe7a5e2aef75e363ba46'
+  version '2.9.1'
+  sha256 '494fc2e23c8a1a361659216a33703f626342e4875ffca25dc8d258686e72d14b'
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom'
