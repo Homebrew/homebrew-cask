ef1e503c0fead87a13f7b0534d59c883d0ee5261
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-18 03:38:07 -0700

    Update rubitrack-pro from 5.2.2 to 5.2.3 (#62114)

--- Casks/rubitrack-pro.rb
+++ Casks/rubitrack-pro.rb
@@ -1,6 +1,6 @@
 cask 'rubitrack-pro' do
-  version '5.2.2'
-  sha256 'da98016bc0e89a592cbe37fdd9396b3040d17cbc3c9c5f0a297548a35f733d0f'
+  version '5.2.3'
+  sha256 '1fba18c814d35b9475239b3092a2cecfeffb9022fd1df1addce0b1277306d33a'
 
   url "https://www.rubitrack.com/files/rubiTrack-#{version}.dmg"
   appcast "https://www.rubitrack.com/autoupdate/sparkle#{version.major}.xml"
