ae2c6e44a8bf733eec64849397c14f21b8183c11
Randall <ran.dall@icloud.com>
2019-04-18 03:21:51 -0700

    Update feeder from 3.6.4 to 3.6.5

--- Casks/feeder.rb
+++ Casks/feeder.rb
@@ -1,6 +1,6 @@
 cask 'feeder' do
-  version '3.6.4'
-  sha256 '6b82440fcfd70c7d77e7dfbefe8bb68d876c0bda858e791fcaf64c216ba9f0ee'
+  version '3.6.5'
+  sha256 '89b00013e4e3e908de13d3766e65334e696e08d35d0ca25808b99746a779fad2'
 
   url "https://reinventedsoftware.com/feeder/downloads/Feeder_#{version}.dmg"
   appcast "https://reinventedsoftware.com/feeder/downloads/Feeder#{version.major}.xml"
