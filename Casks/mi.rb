fb09c1d58f9219ab3d3f7e320c04ad9e8d72ca16
suschizu <brigitte@corecode.io>
2019-04-18 20:24:26 +0200

    Update mi from 3.0.3 to 3.0.3r1 (#62135)

--- Casks/mi.rb
+++ Casks/mi.rb
@@ -1,6 +1,6 @@
 cask 'mi' do
-  version '3.0.3'
-  sha256 'c8b6d0c8ef7954b45f92544b33fa5605437dec239168ee4ac6f6751c85a87762'
+  version '3.0.3r1'
+  sha256 'dd711bbba83663757076ecd62104ef439682d9006f29daa859ff5e6cfc3ed209'
 
   url "https://www.mimikaki.net/download/mi#{version}.dmg"
   appcast 'https://www.mimikaki.net/download/appcast.xml'
