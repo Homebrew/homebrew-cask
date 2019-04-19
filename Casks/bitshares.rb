b0d808e18f4cfa0a83e38a3dded184758481cedb
Randall <ran.dall@icloud.com>
2019-04-19 01:57:26 -0700

    Update bitshares from 2.0.190227 to 2.0.190410

--- Casks/bitshares.rb
+++ Casks/bitshares.rb
@@ -1,6 +1,6 @@
 cask 'bitshares' do
-  version '2.0.190227'
-  sha256 '05d90297a7b86075c1cc6466a63abc297c5a450ee6d882aeadc7bbd1eab8fe62'
+  version '2.0.190410'
+  sha256 '6069f3b61233f0b8ff5563c45bdb24e3d5f052c6d0aae851a780717fbcb26c1a'
 
   # github.com/bitshares/bitshares-ui was verified as official when first introduced to the cask
   url "https://github.com/bitshares/bitshares-ui/releases/download/#{version}/BitShares-#{version}.dmg"
