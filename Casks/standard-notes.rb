048e447d6922214fabe2879f6ee22c321fe34117
Randall <ran.dall@icloud.com>
2019-04-19 02:05:36 -0700

    Update standard-notes from 3.0.6 to 3.0.8

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,6 +1,6 @@
 cask 'standard-notes' do
-  version '3.0.6'
-  sha256 '767f076849acf94a4d5e05867c2a5b3bfee3abb704e9881a2ea6e6c451a91511'
+  version '3.0.8'
+  sha256 '0da997ef89d1cf9d8c7cb186acbe88d2c5d7fa7625076fd2d9658660d24cfc17'
 
   # github.com/standardnotes/desktop was verified as official when first introduced to the cask
   url "https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard-Notes-#{version}-mac.zip"
