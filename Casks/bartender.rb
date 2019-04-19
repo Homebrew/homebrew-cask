329ada7c08ec13782623b792569b605e67f60a7e
Birger J. Nordølum <contact@mindtooth.no>
2019-04-19 11:39:29 +0200

    Update bartender from 3.0.47 to 3.0.64 (#62172)

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -3,8 +3,8 @@ cask 'bartender' do
     version '2.1.6'
     sha256 '013bb1f5dcc29ff1ecbc341da96b6e399dc3c85fc95bd8c7bee153ab0d8756f5'
   else
-    version '3.0.47'
-    sha256 '8c92401a39471edc1e151850901991e8d1d25c14c92e2fe3ca3d74791608cb6c'
+    version '3.0.64'
+    sha256 '8d9c85934682f395a642a00c62a0a633ecf7834e70ab2ac0442b4dbccbb4c0f3'
   end
 
   url "https://macbartender.com/B2/updates/#{version.dots_to_hyphens}/Bartender%20#{version.major}.zip",
