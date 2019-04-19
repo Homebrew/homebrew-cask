03ded21d8c03b5cdc57090f65fe5805e3686cc4d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-19 02:49:45 -0700

    Update picgo from 2.1.1 to 2.1.2 (#62175)

--- Casks/picgo.rb
+++ Casks/picgo.rb
@@ -1,6 +1,6 @@
 cask 'picgo' do
-  version '2.1.1'
-  sha256 '0aaec3b31a89e3832d9b939a10e7c0e1817dbcafca3a44dfea35f16f5ac96deb'
+  version '2.1.2'
+  sha256 '518483a0bf303f09442aa749b4236af89223a47a82a8c0fcd9ff09603f2ac72f'
 
   url "https://github.com/Molunerfinn/PicGo/releases/download/v#{version}/PicGo-#{version}-mac.zip"
   appcast 'https://github.com/Molunerfinn/PicGo/releases.atom'
