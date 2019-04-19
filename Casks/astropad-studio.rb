bf060d3ace8733d082ad3cad1c4a8b074c641005
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-19 01:57:16 -0700

    Update astropad-studio from 3.0.1 to 3.1 (#62156)

--- Casks/astropad-studio.rb
+++ Casks/astropad-studio.rb
@@ -1,6 +1,6 @@
 cask 'astropad-studio' do
-  version '3.0.1'
-  sha256 'ff49790ab2cd57e32e9e3ae3f0db64ff636c11a1ce41cb7407c41390634fea97'
+  version '3.1'
+  sha256 'c8bb192e60f54f8fee090aa94a206f51813bed969c123538f429a2215f65e143'
 
   url "https://astropad.com/downloads/AstropadStudio-#{version}.zip"
   appcast 'https://astropad.com/downloads/studio-sparkle.xml'
