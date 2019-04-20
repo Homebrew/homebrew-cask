0ccd72e0d97e4b1f5a8bbf452e509540c18780d8
Randall <ran.dall@icloud.com>
2019-04-20 08:44:27 -0700

    Update Byteball 2.6.0t to Obyte 2.7.0
    Signed-off-by: Randall <ran.dall@icloud.com>

--- Casks/byteball.rb
+++ Casks/byteball.rb
@@ -1,19 +1,19 @@
-cask 'byteball' do
-  version '2.6.0'
+cask 'obyte' do
+  version '2.7.0'
   sha256 'cf73029f39882893829d5e471191059083f8f2d525c29ba2ff3f1eb40671ba95'
 
-  # github.com/byteball/byteball was verified as official when first introduced to the cask
-  url "https://github.com/byteball/byteball/releases/download/v#{version}/Byteball-osx64.dmg"
-  appcast 'https://github.com/byteball/byteball/releases.atom'
-  name 'Byteball'
-  homepage 'https://byteball.org/'
+  # github.com/byteball/obyte-gui-wallet was verified as official when first introduced to the cask
+  url "https://github.com/byteball/obyte-gui-wallet/releases/download/v#{version}/Obyte-osx64.dmg"
+  appcast 'https://github.com/byteball/obyte-gui-wallet/releases'
+  name 'Obyte'
+  homepage 'https://obyte.org/'
 
-  app 'byteball.app'
+  app 'Obyte.app'
 
   zap trash: [
-               '~/Library/Application Support/byteball',
-               '~/Library/Caches/byteball',
-               '~/Library/Preferences/com.nw-builder.byteball.plist',
-               '~/Library/Saved Application State/com.nw-builder.byteball.savedState',
+               '~/Library/Application Support/obyte',
+               '~/Library/Caches/obyte',
+               '~/Library/Preferences/com.nw-builder.obyte.plist',
+               '~/Library/Saved Application State/com.nw-builder.obyte.savedState',
              ]
 end
