fa1f6a9ef92dbef1f740c25d2a36119c70757b2b
Vítor Galvão <info@vitorgalvao.com>
2019-04-20 23:48:38 +0100

    Update houdahspot.rb

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,6 +1,6 @@
 cask 'houdahspot' do
   version '5.0.2'
-  sha256 '41d03a53b37db368510abfa993e6ad68ffd30d1a958eb40d861be0143f1fefb2'
+  sha256 'bfe341bea6faf06a12d0b6fba8352cfcf307d4fee7262c2e25079a0502227de1'
 
   url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
