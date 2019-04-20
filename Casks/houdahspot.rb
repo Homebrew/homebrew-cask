c88ca23f31327519cd84eb334a82e3527df6cccc
Scott Drennan <dttocs@pobox.com>
2019-04-20 10:15:08 -0700

    Update houdahspot to 5.0.2

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,6 +1,6 @@
 cask 'houdahspot' do
   version '5.0.2'
-  sha256 '3769423338196f34573148d68db745285b49ae1fcbeaf70c7c003c57049e2bed'
+  sha256 '41d03a53b37db368510abfa993e6ad68ffd30d1a958eb40d861be0143f1fefb2'
 
   url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
