155d8d05528bb402dd3f8c6676e15cf347ca2250
抱紧我的小鲤鱼 <wangjia_1919@163.com>
2019-04-21 06:27:57 +0800

    Add wps-office v1.0.1(1354) (#62153)
    * Add wps-office v1.0.1(1354)
    
    * Update and rename wps-office.rb to wpsoffice.rb
    
    * Update wpsoffice.rb
    
    * Update wpsoffice.rb

---
+++ Casks/wpsoffice.rb
@@ -0,0 +1,16 @@
+cask 'wpsoffice' do
+  version '1.0.1(1354)'
+  sha256 '6437bc2dd67e72e6a36c22435f0a7657dcefa4a1cb63029c65c98ee9979091cd'
+
+  # package.mac.wpscdn.cn was verified as official when first introduced to the cask
+  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.major_minor_patch}/WPS_Office_#{version}.dmg"
+  appcast 'https://www.wps.cn/product/wpsmac/'
+  name 'WPS Office'
+  homepage 'https://www.wps.cn/product/wpsmac/'
+
+  app 'wpsoffice.app'
+
+  uninstall quit: 'com.kingsoft.wpsoffice.mac'
+
+  zap trash: '~/Library/Containers/com.kingsoft.wpsoffice.mac'
+end
