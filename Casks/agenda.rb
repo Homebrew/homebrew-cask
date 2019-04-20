395a4e5a70ebbe92a1d71d1483e1065627f4c77f
Randall <ran.dall@icloud.com>
2019-04-20 07:59:54 -0700

    Update agenda from 5.2 to 5.3

--- Casks/agenda.rb
+++ Casks/agenda.rb
@@ -1,6 +1,6 @@
 cask 'agenda' do
-  version '5.2'
-  sha256 'e546aa4f9c16f9a98f834385be00f922808ca1ef5566ffb5198c58b818cc26cc'
+  version '5.3'
+  sha256 '4528742404bcafd5b89d2295e96da57204b30ab052ae6bfb711ad3634a4dfdfc'
 
   url "https://downloads.agenda.com/Agenda_#{version}.zip"
   appcast 'https://downloads.agenda.com/AgendaSparkleAppcast.xml'
