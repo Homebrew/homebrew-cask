d84df1e1d6d78528512039108e93db83fe2cca57
Stephen Walker-Weinshenker <sww1235@users.noreply.github.com>
2019-04-20 16:27:48 -0600

    Update freecad from 0.17-13541.9948ee4 to 0.18-16110 (#62147)
    * Update freecad from 0.17-13541.9948ee4 to 0.18-16110
    
    * Update freecad.rb

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,9 +1,9 @@
 cask 'freecad' do
-  version '0.17-13541.9948ee4'
-  sha256 '48623a0dc48c93863ed5bdd76e19008e96c3c2492d3e49041e123b1d582dc383'
+  version '0.18-16110'
+  sha256 '4b5de3237e498041e5fa7164dad4f70723c610e703696469486b9cd2a015a4be'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
-  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.split('-')[0]}/FreeCAD_#{version}-OSX-x86_64-Qt5.dmg"
+  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.major_minor}.1/FreeCAD_#{version}-OSX-x86_64-conda-Qt5-Py3.dmg"
   appcast 'https://github.com/FreeCAD/FreeCAD/releases.atom'
   name 'FreeCAD'
   homepage 'https://www.freecadweb.org/'
