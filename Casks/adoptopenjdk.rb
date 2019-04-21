5b210870b5bf839e4f4d3a09fe0dacc0e8c3fba4
tiiiecherle <tiiiecherle@gmail.com>
2019-04-21 18:32:39 +0200

    Update adoptopenjdk from 11.0.2,9 to 12,33 (#62201)
    * Update adoptopenjdk from 11.0.2,9 to 12,33
    
    * Update adoptopenjdk.rb
    
    * Update adoptopenjdk.rb
    
    * Update adoptopenjdk.rb
    
    * Update adoptopenjdk.rb
    
    * Update adoptopenjdk.rb
    
    * Update adoptopenjdk.rb
    
    * Update adoptopenjdk.rb

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -1,14 +1,14 @@
 cask 'adoptopenjdk' do
-  version '11.0.2,9'
-  sha256 'fffd4ed283e5cd443760a8ec8af215c8ca4d33ec5050c24c1277ba64b5b5e81a'
+  version '12.33'
+  sha256 '985036459d4ef0867a3fe83b0bf87877d8e66a121c7b9c145bb97bd921aaf3f1'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK11U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.tar.gz"
-  appcast 'https://github.com/adoptopenjdk/openjdk11-binaries/releases.atom'
+  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.major}+#{version.minor}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.major}_#{version.minor}.tar.gz"
+  appcast "https://github.com/adoptopenjdk/openjdk#{version.major}-binaries/releases.atom"
   name 'AdoptOpenJDK Java Development Kit'
   homepage 'https://adoptopenjdk.net/'
 
-  artifact "jdk-#{version.before_comma}+#{version.after_comma}", target: "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"
+  artifact "jdk-#{version.major}+#{version.minor}", target: "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.major}.jdk"
 
   uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 
