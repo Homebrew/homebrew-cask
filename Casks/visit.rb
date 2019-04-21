1cb501e38a3d0af0eb8cf32f32604662677dee0b
Kyle Gerard Felker <kfelker@princeton.edu>
2019-04-21 17:50:15 -0500

    Upgrade VisIt to v3.0.0b
    Drop the *-10.11.dmg file suffix for *.dmg in url field

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -1,9 +1,9 @@
 cask 'visit' do
-  version '2.13.2'
-  sha256 'e7e05ec4393f1f1c944b1963ff8f2619383f506a3b05546c91de61082ba4bb21'
+  version '3.0.0b'
+  sha256 'cb0b8f6e9f59e268eb25aaf5f5135630e01d12b95f329f68a6023a3cb1a9dc6d'
 
   # portal.nersc.gov/project/visit was verified as official when first introduced to the cask
-  url "https://portal.nersc.gov/project/visit/releases/#{version}/VisIt-#{version}-10.11.dmg"
+  url "https://portal.nersc.gov/project/visit/releases/#{version}/VisIt-#{version}.dmg"
   appcast 'https://wci.llnl.gov/simulation/computer-codes/visit/executables'
   name 'VisIt'
   homepage 'https://wci.llnl.gov/simulation/computer-codes/visit'
