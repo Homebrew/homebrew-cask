2e1e37e8c8fc0cbdfae585cc298a19d5d6c01a8a
tiiiecherle <tiiiecherle@gmail.com>
2019-04-21 18:22:13 +0200

    Update adoptopenjdk.rb

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -10,7 +10,7 @@ cask 'adoptopenjdk' do
 
   artifact "jdk-#{version.before_comma}+#{version.after_comma}", target: "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"
   
-  uninstall pkgutil: "net.adoptopenjdk.#{version.before_comma}.jdk"
+  uninstall rmdir: '/Library/Java/JavaVirtualMachines'
 
   caveats <<~EOS
     More versions are available in the AdoptOpenJDK tap:
