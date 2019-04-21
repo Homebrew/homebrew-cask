d3d94e3aa35adebd7f8c0f6cf81ac6c5a284f3a8
Yurii Kolesnykov <root@yurikoles.com>
2019-04-21 19:11:32 +0300

    supertuxkart 1.0 (#62247)

--- Casks/supertuxkart.rb
+++ Casks/supertuxkart.rb
@@ -1,9 +1,9 @@
 cask 'supertuxkart' do
-  version '0.9.3'
-  sha256 'b95923943e37ca3203fa1b339fce5af57f5a4d121d1c84b3d4ba84389373d05b'
+  version '1.0'
+  sha256 '20684fd46756ac941f99e2e19578818a92ebf9cf8abce58bb1ea878363cccf96'
 
   # sourceforge.net/supertuxkart was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/supertuxkart/SuperTuxKart-#{version}-osx.dmg"
+  url "https://downloads.sourceforge.net/supertuxkart/SuperTuxKart-#{version}-mac.zip"
   appcast 'https://sourceforge.net/projects/supertuxkart/rss?path=/SuperTuxKart'
   name 'SuperTuxKart'
   homepage 'https://supertuxkart.net/Main_Page'
