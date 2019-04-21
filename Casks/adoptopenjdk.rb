da91562346211ff269372b481dedbd01372fa377
tiiiecherle <tiiiecherle@gmail.com>
2019-04-21 16:56:45 +0200

    Update adoptopenjdk.rb

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -3,7 +3,7 @@ cask 'adoptopenjdk' do
   sha256 '3b974189c4eb74aa43a365676905f6534faf599075e3910fa6ec3b8ffb101cbf'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk-#{version.before_comma}+#{version.after_comma.before_colon}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma.before_colon}#{version.after_colon}.pkg"
+  url "https://github.com/AdoptOpenJDK/openjdk#{version.before_comma}-binaries/releases/download/jdk-#{version.before_comma}+#{version.after_comma}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.tar.gz"
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
   name 'AdoptOpenJDK Java Development Kit'
   homepage 'https://adoptopenjdk.net/'
