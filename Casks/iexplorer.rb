96158596697db92c0943b3b9c535bd4d06da3d37
Elf Sundae <elf.sundae@gmail.com>
2019-04-22 11:27:47 +0800

    Update iexplorer

--- Casks/iexplorer.rb
+++ Casks/iexplorer.rb
@@ -2,7 +2,7 @@ cask 'iexplorer' do
   version '4.2.10'
   sha256 '5d83237a0eeb81b0733e642c01863d051f19fa07741e7af26cd601afccc907db'
 
-  url "https://assets.macroplant.com/downloads/iExplorer-#{version}.dmg"
+  url "https://assets.macroplant.com/download/153/iExplorer-#{version}.dmg"
   appcast 'https://macroplant.com/iexplorer/mac/v4/appcast'
   name 'iExplorer'
   homepage 'https://macroplant.com/iexplorer'
