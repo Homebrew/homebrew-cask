d2f23bf608015f6074a8795df02d83df9218916a
jipsterk <jipsterk1998@gmail.com>
2019-04-22 11:34:25 +0200

    Update unity-web-player from latest to 2019.1.0f2,292b93d75a2c

--- Casks/unity-web-player.rb
+++ Casks/unity-web-player.rb
@@ -1,6 +1,6 @@
 cask 'unity-web-player' do
-  version :latest
-  sha256 :no_check
+  version '2019.1.0f2,292b93d75a2c'
+  sha256 '1058714920302d0c1d0b5fb0cc8404d6b5e472b16f6391f318fe09fb74d63e6a'
 
   url 'http://webplayer.unity3d.com/download_webplayer-3.x/webplayer-mini.dmg'
   name 'Unity Web Player'
