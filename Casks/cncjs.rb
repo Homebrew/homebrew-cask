2a263c2eced6f3a49fdfb176adb756b569d34252
Randall <ran.dall@icloud.com>
2019-04-22 09:00:16 -0700

    Update cncjs from 1.9.18 to 1.9.19

--- Casks/cncjs.rb
+++ Casks/cncjs.rb
@@ -1,6 +1,6 @@
 cask 'cncjs' do
-  version '1.9.18'
-  sha256 '6b4fe786e04e2eb46c498efba8d9a320f4401b24b6687ef0fa1b9c2b45d19efb'
+  version '1.9.19'
+  sha256 '4a6f4a5c688a2440b4bb86b1c75e112a6e2d3c9c01353e47bd6b3fa015c3e8ff'
 
   # github.com/cncjs/cncjs was verified as official when first introduced to the cask
   url "https://github.com/cncjs/cncjs/releases/download/v#{version}/cncjs-app-#{version}-mac-x64.dmg"
