d4f2b7b3b89de42f9fff10ea4a028de145ed8b6c
Randall <ran.dall@icloud.com>
2019-04-22 09:26:52 -0700

    Update abstract from 78.0.2 to 78.1.0

--- Casks/abstract.rb
+++ Casks/abstract.rb
@@ -1,6 +1,6 @@
 cask 'abstract' do
-  version '78.0.2'
-  sha256 '5627a3d169fe50d2c1c1b9db86174f4a580e24855e26f870590eae9066cf4b4d'
+  version '78.1.0'
+  sha256 '4f410d497ce574ce6f2789f0b989231c7236d0e5c1815067081415de5022b8c5'
 
   url "https://downloads.goabstract.com/Abstract-#{version}.dmg"
   appcast 'https://www.goabstract.com/release-notes/'
