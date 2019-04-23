9285ee4a3a04c9fb61c31feff574b89ab9ee6cbf
Lucas Kellar <lucas@lkellar.org>
2019-04-23 13:48:06 -0500

    Update anki from 2.1.11 to 2.1.12 (#62338)

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,6 +1,6 @@
 cask 'anki' do
-  version '2.1.11'
-  sha256 '8ec56b3537ad946c9b3b76b163b2416fa4362da1f9c914eaa7e13849feb8672d'
+  version '2.1.12'
+  sha256 'f0f2a36fd2c8fc7f52ed047ccf3ceae6fc65fc6207bda9e64ac2141bc1cc3726'
 
   url "https://apps.ankiweb.net/downloads/current/anki-#{version}-mac.dmg"
   appcast 'https://apps.ankiweb.net/docs/changes.html'
