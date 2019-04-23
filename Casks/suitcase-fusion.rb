9f5d0a5782a1f9ef8b4334dd9cd2444ed9bd36dd
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-23 08:04:14 -0700

    Update suitcase-fusion from 9,20.0.3 to 9,20.0.4 (#62328)

--- Casks/suitcase-fusion.rb
+++ Casks/suitcase-fusion.rb
@@ -1,6 +1,6 @@
 cask 'suitcase-fusion' do
-  version '9,20.0.3'
-  sha256 '795993e13124419cd5d06b36ed99f7c8d5fc378be07801f4838621c9b9d5e56b'
+  version '9,20.0.4'
+  sha256 '32810dd6f8abac60e431f6dae266c39206f2be7ed8ea9774f60ba6a23b78db1e'
 
   url "https://bin.extensis.com/SuitcaseFusion#{version.before_comma}-M-#{version.after_comma.dots_to_hyphens}.dmg"
   appcast "https://sparkle.extensis.com/u/ST/EN/suitcase#{version.after_comma.major}en.xml"
