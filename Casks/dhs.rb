a0b1285b93460710ca00f483f7d633abffb9aa9c
suschizu <brigitte@corecode.io>
2019-04-23 15:40:09 +0200

    Update dhs from 1.4.0 to 1.4.1 (#62313)

--- Casks/dhs.rb
+++ Casks/dhs.rb
@@ -1,6 +1,6 @@
 cask 'dhs' do
-  version '1.4.0'
-  sha256 '37211f5051413fa866781e6943ad2adb3c68546fcfd3033c0d69a0bbeadc5462'
+  version '1.4.1'
+  sha256 '6d0ce00e0e111059e157bae26d4a913d7cb7c42cee6493b638c2c5019fe987fc'
 
   # bitbucket.org/objective-see was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/DHS_#{version}.zip"
