90bb772b6ab1f6dfca6d1e83c6d919200cd1ef92
suschizu <brigitte@corecode.io>
2019-04-23 13:31:04 +0200

    Update diskcatalogmaker from 7.4.12 to 7.4.13 (#62307)

--- Casks/diskcatalogmaker.rb
+++ Casks/diskcatalogmaker.rb
@@ -1,6 +1,6 @@
 cask 'diskcatalogmaker' do
-  version '7.4.12'
-  sha256 '16e96429e255f216250a83649ba3b10f107a1845d129d61fe8c76b376481d1f9'
+  version '7.4.13'
+  sha256 'bf79d87d943ef9b6bfa97df17238d2fa57d0fca9774b12c4492c89b5a0303b79'
 
   url 'https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip'
   appcast 'https://fujiwara.sakura.ne.jp/info/appcast/DiskCatalogMaker.xml'
