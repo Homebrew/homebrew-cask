a2399f519bb964ec3626217eee4f81e47240e983
Vítor Galvão <info@vitorgalvao.com>
2019-04-23 10:02:00 +0100

    Update gitblade.rb

--- Casks/gitblade.rb
+++ Casks/gitblade.rb
@@ -3,6 +3,7 @@ cask 'gitblade' do
   sha256 'cc759c3242e3ea26aea46b778bbcd7f2ca7db1ead5895f16db7e66a39d05fc03'
 
   url "https://gitblade.com/dl/gitblade-#{version}-darwin-x64.dmg"
+  appcast 'https://gitblade.com/releases'
   name 'GitBlade'
   homepage 'https://gitblade.com/'
 
