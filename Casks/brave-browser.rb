9683e2be2b79508d4880a3b3fe5af1ee5f91e058
Hubert Léo <leohub@live.fr>
2019-04-24 17:17:34 -0400

    Update brave-browser from 0.62.51 to 0.63.48

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -1,6 +1,6 @@
 cask 'brave-browser' do
-  version '0.62.51'
-  sha256 '2a09d8c00d5423719b629b0a97aa37b9d68196504ad13bf9377bc58bc4813c64'
+  version '0.63.48'
+  sha256 '7f50c86f45446d6c930f3198c11cacedf64e73915ec65e5d090e274a95760f51'
 
   # github.com/brave/brave-browser was verified as official when first introduced to the cask
   url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser.dmg"
