3440da71e61bff1eb1953609450a575c9f145f39
suschizu <brigitte@corecode.io>
2019-04-24 13:48:30 +0200

    Update fldigi.rb

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -1,5 +1,5 @@
 cask 'fldigi' do
-  version '4.1.03,4.3.4'
+  version '4.1.03,4.3.7'
   sha256 '059b72c165b7bd7c385938a14315664cb3309160a4c43589ba0d8ae53cdde14e'
 
   url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version.before_comma}_x86_64.dmg"
