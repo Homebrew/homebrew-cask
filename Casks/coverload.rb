58a9d2cb08f9112e58e2a7b99c2a1614dbf0ec48
Johan Bassa <jdbassa@hotmail.com>
2019-04-24 13:27:15 +0700

    Update coverload from 1.2.2-503 to 1.2.3-505

--- Casks/coverload.rb
+++ Casks/coverload.rb
@@ -1,6 +1,6 @@
 cask 'coverload' do
-  version '1.2.2-503'
-  sha256 '0b6167e30478a024bc479fc53237c0e948c81a4c7347dfe6cac77328e546d164'
+  version '1.2.3-505'
+  sha256 'a6028ca94789df6d18f7f296c04413c542e57237d27fac6061a5c43018e48847'
 
   # amazonaws.com/coverloadapp.com was verified as official when first introduced to the cask
   url "https://s3-us-west-2.amazonaws.com/coverloadapp.com/Uploads/CoverLoad-#{version}.zip"
