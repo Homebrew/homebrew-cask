37703fa07243b96d2090b88f86941e858705527d
Randall <ran.dall@icloud.com>
2019-04-24 07:59:12 -0700

    Update grandtotal from 5.2.7 to 5.2.7.1

--- Casks/grandtotal.rb
+++ Casks/grandtotal.rb
@@ -1,6 +1,6 @@
 cask 'grandtotal' do
-  version '5.2.7'
-  sha256 'be327403637a2d993aacacd3e4af48728831fba0bcc991c135090e66fc15b2cc'
+  version '5.2.7.1'
+  sha256 '55bd18c2b5cd2901d3fa156b60441092ddb0b46adf9afd84544c40c8d4e56450'
 
   url "https://mediaatelier.com/GrandTotal#{version.major}/GrandTotal_#{version}.zip"
   appcast "https://mediaatelier.com/GrandTotal#{version.major}/feed.php"
