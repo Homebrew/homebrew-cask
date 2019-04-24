bd3e4681de1125234a491f5bdc8111c0a9a0ffe8
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-24 08:07:15 -0700

    Update portfolioperformance from 0.39.0 to 0.39.1 (#62361)

--- Casks/portfolioperformance.rb
+++ Casks/portfolioperformance.rb
@@ -1,6 +1,6 @@
 cask 'portfolioperformance' do
-  version '0.39.0'
-  sha256 '54a577672ca9cfa234afda5d602730a7a441ab465f39772fe614e27e66991059'
+  version '0.39.1'
+  sha256 'dfbdfe01b4fcba0ad65de2801cd86927fb802580940bda5d71180f7a2828247e'
 
   # github.com/buchen/portfolio was verified as official when first introduced to the cask
   url "https://github.com/buchen/portfolio/releases/download/#{version}/PortfolioPerformance-distro-#{version}-macosx.cocoa.x86_64.tar.gz"
