f83174707a70db064917660fae2310e11832efef
Elf Sundae <elf.sundae@gmail.com>
2019-04-25 20:07:05 +0800

    Update yinxiangbiji from 9.0.2_457849 to 9.0.4_457911

--- Casks/yinxiangbiji.rb
+++ Casks/yinxiangbiji.rb
@@ -1,6 +1,6 @@
 cask 'yinxiangbiji' do
-  version '9.0.2_457849'
-  sha256 '6c72293b2ad3dff55c1f7a7a03c2130e25a4ad1202075246aff27ec475a2708b'
+  version '9.0.4_457911'
+  sha256 'af71a01294dbc408f5eb272c0a32f592212c2c85789611ecf7295411ef492491'
 
   url "https://cdn.yinxiang.com/mac-smd/public/YinxiangBiji_RELEASE_#{version}.dmg"
   appcast 'https://update.yinxiang.com/public/ENMacSMD/EvernoteMacUpdate.xml'
