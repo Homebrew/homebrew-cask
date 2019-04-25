2ffb5da3b664129ed77e461056cbc59104d54058
Kyle Gerard Felker <kfelker@math.princeton.edu>
2019-04-25 16:01:51 -0500

    Upgrade VisIt to v2.13.3 (#62441)
    Drop the *-10.11.dmg file suffix for *.dmg in url field so that the
    cask-repair script will again work for this Cask.

--- Casks/visit.rb
+++ Casks/visit.rb
@@ -1,9 +1,9 @@
 cask 'visit' do
-  version '2.13.2'
-  sha256 'e7e05ec4393f1f1c944b1963ff8f2619383f506a3b05546c91de61082ba4bb21'
+  version '2.13.3'
+  sha256 '984d85a597ecd391ff4fcf05fede6d320ef7eb138ce296aa88b7136b0ea1ea50'
 
   # portal.nersc.gov/project/visit was verified as official when first introduced to the cask
-  url "https://portal.nersc.gov/project/visit/releases/#{version}/VisIt-#{version}-10.11.dmg"
+  url "https://portal.nersc.gov/project/visit/releases/#{version}/VisIt-#{version}.dmg"
   appcast 'https://wci.llnl.gov/simulation/computer-codes/visit/executables'
   name 'VisIt'
   homepage 'https://wci.llnl.gov/simulation/computer-codes/visit'
