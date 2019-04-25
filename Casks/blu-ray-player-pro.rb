002b26c31ba1186cd7c36610de02515bb80a8f38
Randall <ran.dall@icloud.com>
2019-04-25 10:28:12 -0700

    Update blu-ray-player-pro from 3.3.13_190402_2324 to 3.3.14_190425_1634

--- Casks/blu-ray-player-pro.rb
+++ Casks/blu-ray-player-pro.rb
@@ -1,6 +1,6 @@
 cask 'blu-ray-player-pro' do
-  version '3.3.13_190402_2324'
-  sha256 'f993dd869641d05bfcec8f7cb97765e7ddc273bd48b2d4b484f458cbb54c35f2'
+  version '3.3.14_190425_1634'
+  sha256 '718334d18fc2030ebd72609ff45f8af29a53936fb0d0efdc819ec0c0eaf91fa5'
 
   url "https://cdn.macblurayplayer.com/mac-bluray-player-pro3/Blu-ray_Player_Pro_#{version}.dmg"
   appcast 'https://cdn.macblurayplayer.com/mac-bluray-player-pro3/appcast/Appcast.xml'
