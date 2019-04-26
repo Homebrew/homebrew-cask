03046d4dd0b8e1dc13dd96b71491350c173cf35b
Jiří Málek <jiri.malek@gmail.com>
2019-04-26 10:02:08 +0200

    Add Runway Latest

---
+++ Casks/runway.rb
@@ -0,0 +1,10 @@
+cask 'runway' do
+  version '1.9'
+  sha256 'bd063e9f8db568aa2c57d5f7464dbdfead7b21dec8a80f112b0b78a349578114'
+
+  url 'http://celestialteapot.com/runway/dist/Runway_1.9.zip'
+  name 'Runway'
+  homepage 'http://celestialteapot.com/runway/'
+
+  app 'Runway.app'
+end
