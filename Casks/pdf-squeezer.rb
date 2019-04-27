ae955011af33f730f8a6c440231553b052700e55
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-27 10:29:09 -0700

    Update pdf-squeezer from 3.10.2 to 3.10.3 (#62517)

--- Casks/pdf-squeezer.rb
+++ Casks/pdf-squeezer.rb
@@ -1,6 +1,6 @@
 cask 'pdf-squeezer' do
-  version '3.10.2'
-  sha256 '8366ba407b64475015da142f1380706ba6ad50dba64d4ef72e72e885e91342b6'
+  version '3.10.3'
+  sha256 '1bc08bab64ea3d9de61d3048f7007d3aba082fc80384463e409b0047d2371f2a'
 
   url 'https://witt-software.com/downloads/pdfsqueezer/PDF%20Squeezer.dmg'
   appcast 'https://witt-software.com/downloads/pdfsqueezer/appcast.xml'
