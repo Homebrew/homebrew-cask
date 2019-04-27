e5c032c95a1241defd2d851f4355aa88deab0b36
Randall <ran.dall@icloud.com>
2019-04-27 09:41:42 -0700

    Update qgroundcontrol from 3.5.0 to 3.5.1

--- Casks/qgroundcontrol.rb
+++ Casks/qgroundcontrol.rb
@@ -1,6 +1,6 @@
 cask 'qgroundcontrol' do
-  version '3.5.0'
-  sha256 '0c6eb7316e895bef1b56981156ce154f6809fcd1094b8ba163ab9024612ea56d'
+  version '3.5.1'
+  sha256 'c749c16e2813f150fc95cc612cba3b06c73eb7d551bad5600e476f6408f6937f'
 
   # github.com/mavlink/qgroundcontrol was verified as official when first introduced to the cask
   url "https://github.com/mavlink/qgroundcontrol/releases/download/v#{version}/QGroundControl.dmg"
