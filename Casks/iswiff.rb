409ccf42df78c5ceeb50e65fe8dd71a604c7290e
CoreCode <core-code@users.noreply.github.com>
2019-04-27 18:34:36 +0200

    Update iswiff.rb

--- Casks/iswiff.rb
+++ Casks/iswiff.rb
@@ -2,7 +2,8 @@ cask 'iswiff' do
   version :latest
   sha256 :no_check
 
-  url 'https://echoone.com/iswiff/iSwiff.dmg'
+  url 'https://echoone.com/iswiff/iSwiff.zip'
+  appcast 'https://echoone.com/iswiff/Readme.html'
   name 'iSwiff'
   homepage 'https://echoone.com/iswiff/'
 
