9aa8ffd3884e603a69ad5aafb75a9285db22aac5
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-27 10:19:25 -0700

    Upgrade to `https`

--- Casks/ammonite.rb
+++ Casks/ammonite.rb
@@ -2,10 +2,10 @@ cask 'ammonite' do
   version '1.18.1'
   sha256 '533e6bf3ce9379ab1cd214c2e01c950946d1d0ab9c9f674dfbc3f4e91df2587e'
 
-  url "http://www.soma-zone.com/download/files/Ammonite-#{version}.tar.bz2"
-  appcast 'http://www.soma-zone.com/Ammonite/a/appcast.xml'
+  url "https://www.soma-zone.com/download/files/Ammonite-#{version}.tar.bz2"
+  appcast 'https://www.soma-zone.com/Ammonite/a/appcast.xml'
   name 'Ammonite'
-  homepage 'http://www.soma-zone.com/Ammonite/'
+  homepage 'https://www.soma-zone.com/Ammonite/'
 
   app 'Ammonite.app'
 end
