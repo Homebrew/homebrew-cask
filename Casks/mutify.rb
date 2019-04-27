29f8bee35caafcaccee30fd19b802761123ca877
Vítor Galvão <info@vitorgalvao.com>
2019-04-27 05:48:01 +0100

    Update mutify.rb

--- Casks/mutify.rb
+++ Casks/mutify.rb
@@ -1,8 +1,9 @@
 cask 'mutify' do
-  version :latest
-  sha256 :no_check
+  version '1.1.1'
+  sha256 '1ac19cef9d59cd1162ddfbd97c7bbe882159c763458b0fc5ce19690f433b673c'
 
   url 'https://storage.mutify.app/data/Mutify.dmg'
+  appcast 'https://storage.mutify.app/data/appcast.xml'
   name 'Mutify'
   homepage 'https://mutify.app/'
 
