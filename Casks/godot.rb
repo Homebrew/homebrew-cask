4a9cc27547d377404e02f8905c11693dc654afc5
Matt Robenolt <matt@ydekproductions.com>
2019-04-28 10:37:05 -0700

    Update godot from 3.1 to 3.1.1

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,6 +1,6 @@
 cask 'godot' do
-  version '3.1'
-  sha256 'dde1738280c03ebdd6d540381a782e74cfa4099941751bb14a55513386849f0e'
+  version '3.1.1'
+  sha256 'bd6beb619c759988d6142a179b0cebe53ce26975e3e798cb124d9ed6d9b1c6e1'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.64.zip"
