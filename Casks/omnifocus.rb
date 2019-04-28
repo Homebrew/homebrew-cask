ba33d241e8a7d5f8894233e44754be208ec54c48
ralph <ralph@cs.cf.ac.uk>
2019-04-28 08:33:02 +0100

    Update omnifocus from 3.3.1 to 3.3.2

--- Casks/omnifocus.rb
+++ Casks/omnifocus.rb
@@ -16,8 +16,8 @@ cask 'omnifocus' do
     sha256 '8a2dc53331dba804f6781773fef546a03c181fc4ff0eb7ee4f871c10342621f0'
     url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniFocus-#{version}.dmg"
   else
-    version '3.3.1'
-    sha256 '64170d1f287fa0d594b06ba61904012fc8e0687f9f55e61d2e63587ef23e27d3'
+    version '3.3.2'
+    sha256 'a6ac601e6291dcdc3099aa1613fd7aec56fa41438833293a7d4fa6b943c50d7a'
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniFocus-#{version}.dmg"
   end
 
