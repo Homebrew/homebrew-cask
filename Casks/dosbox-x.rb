ad633c1ca4b346425577355417b7c68881070e05
Randall <17261190+ran-dall@users.noreply.github.com>
2019-04-28 07:08:35 -0700

    Update dosbox-x from 0.82.17,20190405211829 to 0.82.18,20190427181609 (#62534)

--- Casks/dosbox-x.rb
+++ Casks/dosbox-x.rb
@@ -1,6 +1,6 @@
 cask 'dosbox-x' do
-  version '0.82.17,20190405211829'
-  sha256 '23996d4eef7849db3958f74a72ad2e9a17e6777220d4439431787dd6bdbb38db'
+  version '0.82.18,20190427181609'
+  sha256 'fae850fe9088fc47fe1a64398898a7f58a9c54f1e748fac4be0be4a718c9848a'
 
   # github.com/joncampbell123/dosbox-x was verified as official when first introduced to the cask
   url "https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v#{version.before_comma}/dosbox-x-macosx-x64-#{version.after_comma}.zip"
