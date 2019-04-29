f0abab5b18eb113d880fb6a72edbdb603d9b6ec4
jcbot <jcb@leipert.io>
2019-04-29 16:35:08 +0000

    Update pycharm-edu to 2019.1,191.6707.81

--- Casks/pycharm-edu.rb
+++ Casks/pycharm-edu.rb
@@ -1,6 +1,6 @@
 cask 'pycharm-edu' do
-  version '2018.3,183.4588.180'
-  sha256 'b9491701a167117de33155f149b180db86db5f325965e4f25d77fc580d924252'
+  version '2019.1,191.6707.81'
+  sha256 '62b128449f76ed367adf59c4ddd2aecdbd9c42683308cd4f5b5cd77b1f6bf40c'
 
   url "https://download.jetbrains.com/python/pycharm-edu-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=PCE&latest=true&type=release'
