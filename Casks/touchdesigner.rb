eb714d62929792e7fc7bfe72ac56cc1282dc5158
suschizu <brigitte@corecode.io>
2019-04-29 16:06:51 +0200

    Revert "Update touchdesigner from 099.2018.28070 to 099.2019.13330 (#62434)"
    This reverts commit 1c0b8b629253e518c504f904d863c5182a12f2a2.

--- Casks/touchdesigner.rb
+++ Casks/touchdesigner.rb
@@ -1,6 +1,6 @@
 cask 'touchdesigner' do
-  version '099.2019.13330'
-  sha256 '12cf7858b4e6094e5c9ce3c272facec5e1b65bac1e2caf44278095be5b374aa0'
+  version '099.2018.28070'
+  sha256 '88366226083c9041c123ddf4b53d4d42d3bc0bfa19b15169c55c95f13e9f7607'
 
   url "https://www.derivative.ca/Builds/TouchDesigner#{version}.dmg"
   appcast "https://www.derivative.ca/#{version.major}/Downloads/Default.asp"
