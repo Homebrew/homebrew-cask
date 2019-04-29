15f4e82dab6db9d771844b6c86df9cbd9ff90f86
Sharif Elgamal <selgamal@google.com>
2019-04-29 12:09:49 -0700

    Update minikube to 1.0.1 (#62582)

--- Casks/minikube.rb
+++ Casks/minikube.rb
@@ -1,6 +1,6 @@
 cask 'minikube' do
-  version '1.0.0'
-  sha256 '865bd3a13c1ad3b7732b2bea35b26fef150f2b3cbfc257c5d1835527d1b331e9'
+  version '1.0.1'
+  sha256 '0af8a3f582c9284ffe10e99444b60a75241325f2bc9ab43ec758802f2b89e1db'
 
   # storage.googleapis.com/minikube was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/minikube/releases/v#{version}/minikube-darwin-amd64"
