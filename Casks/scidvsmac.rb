ab47fd267a82db46648fea09888d83efeb1fba6d
Adrian Petrescu <apetresc@gmail.com>
2019-04-29 08:55:32 -0400

    Update scidvsmac (#62558)

--- Casks/scidvsmac.rb
+++ Casks/scidvsmac.rb
@@ -1,6 +1,6 @@
 cask 'scidvsmac' do
   version '4.20'
-  sha256 'c948fc858ef685ee71f7941bc69ff53a72b48e28bd589a94e21b978eac3aa6e8'
+  sha256 'c4a17efc2a12a1745d2a34bf0f6fc4b1b46eaf98f1f98f365d2dbec583ad9ae9'
 
   # downloads.sourceforge.net/scidvspc was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/scidvspc/ScidvsMac-#{version}.dmg"
