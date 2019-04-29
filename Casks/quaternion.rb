2fce3e65a1abad7c8f0760ab902ca2b04e4609cc
Aaron Raimist <aaron@raim.ist>
2019-04-28 21:03:34 -0500

    Upgrade Quaternion.app to 0.0.9.4 (#62542)

--- Casks/quaternion.rb
+++ Casks/quaternion.rb
@@ -1,8 +1,8 @@
 cask 'quaternion' do
-  version '0.0.9.3'
-  sha256 '5e88aba4e1ea13a202412452e5b35dd64c917952f4bed23bd1f178702957d281'
+  version '0.0.9.4'
+  sha256 'c65511dfaeaa0f9b2fc733c554d2bc9cc18088b0093db5c859c560a43e41c38c'
 
-  url "https://github.com/QMatrixClient/Quaternion/releases/download/v#{version}/quaternion_macos.zip"
+  url "https://github.com/QMatrixClient/Quaternion/releases/download/#{version}/quaternion-#{version}.dmg"
   appcast 'https://github.com/QMatrixClient/Quaternion/releases.atom'
   name 'Quaternion'
   homepage 'https://github.com/QMatrixClient/Quaternion'
