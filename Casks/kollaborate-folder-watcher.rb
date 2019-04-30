7f94e360512aa6c2796a4c6f029aac8e0fe67368
Vítor Galvão <info@vitorgalvao.com>
2019-04-30 01:19:56 +0100

    Update kollaborate-folder-watcher.rb

--- Casks/kollaborate-folder-watcher.rb
+++ Casks/kollaborate-folder-watcher.rb
@@ -2,8 +2,8 @@ cask 'kollaborate-folder-watcher' do
   version '1.3.3.0'
   sha256 '8fb69e5b6b5977ae99a2f2e6fac222f6e4ace7595fde304cfa9cd176f698435f'
 
-  # digitalrebellion.com was verified as official when first introduced to the cask
-  url "https://www.digitalrebellion.com/download/kollabfolderwatcher?version=#{version.no_dots}"
+  # download.digitalrebellion.com/kollabfolderwatcher was verified as official when first introduced to the cask
+  url "http://download.digitalrebellion.com/kollabfolderwatcher/Kollaborate_Folder_Watcher_#{version.no_dots}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://www.digitalrebellion.com/download/kollabfolderwatcher'
   name 'Kollaborate Folder Watcher'
   homepage 'https://www.kollaborate.tv/resources'
