cc00e2f805b8819b5ab7fc63af30066efe225cf7
王子 <hiwangzi@outlook.com>
2019-04-30 10:40:55 +0800

    Update axure-rp (#62601)

--- Casks/axure-rp.rb
+++ Casks/axure-rp.rb
@@ -1,6 +1,6 @@
 cask 'axure-rp' do
   version '9.0.0.3646'
-  sha256 '66c98ef8db416b490cf3b43c21bc8074fbc3671fcbc82cfd29a7ff00c7802947'
+  sha256 '39df9c414cc42292de88fcde1b11b7b7705c1290b57b0cecce8abf855109f48c'
 
   # axure.cachefly.net was verified as official when first introduced to the cask
   url 'https://axure.cachefly.net/AxureRP-Setup.dmg'
