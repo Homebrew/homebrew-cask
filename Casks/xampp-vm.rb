3a68c43b5e1baf6c9235562b52c04e9b662b14a8
Greg Rynkowski <rynkowsg@users.noreply.github.com>
2019-05-01 16:10:23 +0100

    Update xampp-vm to 7.3.4-0 (#62629)

--- Casks/xampp-vm.rb
+++ Casks/xampp-vm.rb
@@ -1,6 +1,6 @@
 cask 'xampp-vm' do
-  version '7.3.0-0'
-  sha256 '499c5f5258de58f79e742a77f69ef662a04918b30b7a38471a5ee8e6d2f449f1'
+  version '7.3.4-0'
+  sha256 'fc2db47cd2705393468ed11dd1c31a70626baf4a185cf02e036ee44ab33ab45f'
 
   url "https://www.apachefriends.org/xampp-files/#{version.major_minor_patch}/xampp-osx-#{version}-vm.dmg"
   name 'XAMPP-VM'
