eed1d927ab961d8e80e24f5a5f009974209bd78e
Work <brigitte@corecode.io>
2019-05-01 19:57:17 +0200

    Update aircall from 2.1.4 to 2.1.5

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -1,6 +1,6 @@
 cask 'aircall' do
-  version '2.1.4'
-  sha256 '942366eeda877965adbd07f443c74bb51e4efcd5487e549ead2c3ff156f9d801'
+  version '2.1.5'
+  sha256 'b6389e0b896ae3da2135f030eb19c03d28886b553e4219c5d7682c143f5dbec0'
 
   # s3-us-west-1.amazonaws.com/aircall-electron-releases was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/aircall-electron-releases/production/Aircall-#{version}-mac.zip"
