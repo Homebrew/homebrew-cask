f10a2a1584d496aa94f0a54ee74c3c59b06642f8
ralph <ralph@cs.cf.ac.uk>
2019-05-01 22:19:25 +0100

    Update ltspice

--- Casks/ltspice.rb
+++ Casks/ltspice.rb
@@ -2,7 +2,7 @@ cask 'ltspice' do
   version :latest
   sha256 :no_check
 
-  url 'http://ltspice.analog.com/software/LTspiceIV.dmg'
+  url 'http://ltspice.analog.com/software/LTspice.dmg'
   name 'LTspice'
   homepage 'http://www.analog.com/en/design-center/design-tools-and-calculators/ltspice-simulator.html'
 
