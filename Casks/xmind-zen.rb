30e4621400213ce5c2bd32edae6459bac17ce80d
suschizu <brigitte@corecode.io>
2019-05-02 10:37:00 +0200

    Update xmind-zen.rb

--- Casks/xmind-zen.rb
+++ Casks/xmind-zen.rb
@@ -1,8 +1,8 @@
 cask 'xmind-zen' do
-  version '9.2.0.201904242030,2019'
+  version '9.2.0-201904242030,2019'
   sha256 '1cee4c18b0a39ea04238f6e4ed272df6663bbbc471a9cbeac7cae705e24d3af2'
 
-  url "http://dl2.xmind.net/xmind-downloads/XMind-ZEN-Update-#{version.after_comma}-for-macOS-#{version.before_comma}.dmg"
+  url "https://dl3.xmind.net/XMind-ZEN-Update-#{version.after_comma}-for-macOS-#{version.before_comma}.dmg"
   appcast 'https://www.xmind.net/download/'
   name 'XMind ZEN'
   homepage 'https://www.xmind.net/zen/'
