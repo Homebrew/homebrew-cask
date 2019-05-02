d7a55bba73b78c377ecd561ac6d1445e7aacf958
Julian Mayer <julian@corecode.io>
2019-05-03 00:36:08 +0200

    Update ebmac

--- Casks/ebmac.rb
+++ Casks/ebmac.rb
@@ -3,6 +3,7 @@ cask 'ebmac' do
   sha256 '5ab3a9ad75a66ffcc8c96a97707dfd978bc779a7b9b65435762593cab2cd4fe3'
 
   url "http://ebstudio.info/download/ebpocket/EBMac#{version}.dmg"
+  appcast 'http://ebstudio.info/manual/EBMac/'
   name 'EBMac'
   homepage 'http://ebstudio.info/manual/EBMac/'
 
