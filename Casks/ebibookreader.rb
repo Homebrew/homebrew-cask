940c1ff3aed0c334abfbee3191eeb4ad99af2542
Julian Mayer <julian@corecode.io>
2019-05-03 00:34:38 +0200

    Update ebibookreader

--- Casks/ebibookreader.rb
+++ Casks/ebibookreader.rb
@@ -3,6 +3,7 @@ cask 'ebibookreader' do
   sha256 '45744649ebc146a500571278839874fb1283fd87e06dd2120fbbfe805469b4da'
 
   url 'http://haishin.ebookjapan.jp/contents/appli/reader/ebiBookReader.dmg'
+  appcast 'https://www.ebookjapan.jp/ebj/reader/mac/reader.asp'
   name 'ebi.BookReader'
   homepage 'http://www.ebookjapan.jp/ebj/reader/mac/'
 
