0610a6ecbc7d67a660967c70c6358dae23b0dbef
Vítor Galvão <info@vitorgalvao.com>
2019-05-02 15:14:49 +0100

    Update fsnotes.rb

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,9 +1,9 @@
 cask 'fsnotes' do
-  version 'v3'
+  version '3'
   sha256 '74b3439e52eb1d18e05d2f0e5607e820a6d90113a304395d2130d3e84eff5cfe'
 
   # github.com/glushchenko/fsnotes was verified as official when first introduced to the cask
-  url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
+  url "https://github.com/glushchenko/fsnotes/releases/download/v#{version}/FSNotes_v#{version}.zip"
   appcast 'https://github.com/glushchenko/fsnotes/releases.atom'
   name 'FSNotes'
   homepage 'https://fsnot.es/'
