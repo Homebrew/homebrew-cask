6ca887d3a11c123e2bb4d730b4bdb75f1178b22a
CoreCode <core-code@users.noreply.github.com>
2019-05-03 16:06:17 +0200

    Update navicat-data-modeler from 2.1.20 to 2.1.21 (#62749)

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -1,9 +1,9 @@
 cask 'navicat-data-modeler' do
-  version '2.1.20'
-  sha256 'e91803229fdb610f47210d299b6162685f47972b5997d0c0e76b2984fa24b34b'
+  version '2.1.21'
+  sha256 '5b8ace74609c37607bf1efae4455f792feb1950c69671076d3fc7d2dde1756ac'
 
   url "http://download.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
-  appcast 'https://www.navicat.com/products/navicat-data-modeler-release-note'
+  appcast 'https://www.navicat.com/updater/modeler_v2/sysProfileInfo.php?appName=Navicat%20Data%20Modeler'
   name 'Navicat Data Modeler'
   homepage 'https://www.navicat.com/products/navicat-data-modeler'
 
