2bec4a2dce276d54272ce078024a4c773d9614d4
suschizu <brigitte@corecode.io>
2019-05-03 22:05:18 +0200

    Update nvidia-geforce-now.rb

--- Casks/nvidia-geforce-now.rb
+++ Casks/nvidia-geforce-now.rb
@@ -1,8 +1,8 @@
 cask 'nvidia-geforce-now' do
   version '2.0.6.87'
-  sha256 '2aa9c198646f2c32c8d12eb4bc12970613349120895e3673d3d62a7982140474'
+  sha256 '2ca393195123cc669a1b21bd509780d80d9600545aa4e3363c8a2a5dab034e65'
 
-  url 'https://ota-downloads.nvidia.com/ota/GeForceNOW-release_E943AA.dmg'
+  url 'https://ota-downloads.nvidia.com/ota/GeForceNOW-release_8AC740.dmg'
   appcast 'https://ota.nvidia.com/release/available?product=GFN-mac&version=1.17.2.0&channel=OFFICIAL'
   name 'NVIDIA GeForce NOW'
   homepage 'https://www.nvidia.com/en-us/geforce/products/geforce-now/'
