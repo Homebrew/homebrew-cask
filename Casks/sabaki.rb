d911eb69c22dab0fdf91e9316195c0d0ad2156b4
Adrian Petrescu <apetresc@gmail.com>
2019-05-03 08:41:16 -0400

    Update sabaki from 0.43.1 to 0.43.2

--- Casks/sabaki.rb
+++ Casks/sabaki.rb
@@ -1,6 +1,6 @@
 cask 'sabaki' do
-  version '0.43.1'
-  sha256 '84b1c02ae8facc7cfb884233df39e82e5b37e33daf9dee119281428f24e271fa'
+  version '0.43.2'
+  sha256 '10a2915438e0d0d8ed4daa415a91e4a191814d2b8d21d14ecaeef926aac8565f'
 
   # github.com/SabakiHQ/Sabaki was verified as official when first introduced to the cask
   url "https://github.com/SabakiHQ/Sabaki/releases/download/v#{version}/sabaki-v#{version}-mac-x64.7z"
