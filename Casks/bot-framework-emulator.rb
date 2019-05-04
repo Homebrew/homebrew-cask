4bb71e362c6445c75fc4879bd1a20ccca4846527
Work <brigitte@corecode.io>
2019-05-04 16:43:47 +0200

    Update bot-framework-emulator from 4.3.3 to 4.4.0

--- Casks/bot-framework-emulator.rb
+++ Casks/bot-framework-emulator.rb
@@ -1,6 +1,6 @@
 cask 'bot-framework-emulator' do
-  version '4.3.3'
-  sha256 'b286b38f06b85d07c3315fbb19afd16b69d2da3a2d2ca787c60334ea38d480de'
+  version '4.4.0'
+  sha256 '066247146eec2197ae00c4bc93d5790985cee66d6181cdb20178d92e259f3085'
 
   url "https://github.com/Microsoft/BotFramework-Emulator/releases/download/v#{version}/botframework-emulator-#{version}-mac.zip"
   appcast 'https://github.com/Microsoft/BotFramework-Emulator/releases.atom'
