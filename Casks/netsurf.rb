078e09c09debaabfafae500378968aef77ff11ab
Vítor Galvão <info@vitorgalvao.com>
2019-05-04 21:58:59 +0100

    Update netsurf.rb

--- Casks/netsurf.rb
+++ Casks/netsurf.rb
@@ -3,6 +3,7 @@ cask 'netsurf' do
   sha256 '7c005b7a2519a2c096d6a4a044ebc53788d205e1499f4bd2ea4cefe129c1674b'
 
   url "https://download.netsurf-browser.org/netsurf/releases/pre-built/macosx/NetSurf-#{version}.dmg"
+  appcast 'https://www.netsurf-browser.org/downloads/macosx/'
   name 'NetSurf'
   homepage 'https://www.netsurf-browser.org/'
 
