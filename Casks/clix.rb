62db8ff89f258d7a18fbcea74e04cc64d814c201
CoreCode <core-code@users.noreply.github.com>
2019-05-04 20:06:27 +0200

    Update clix (#62795)
    * Update clix
    
    * Update clix.rb

--- Casks/clix.rb
+++ Casks/clix.rb
@@ -1,10 +1,10 @@
 cask 'clix' do
   version '2.4.0.0'
-  sha256 '32f40de6feb59237078e0fd592d0354f5fadd537eddfe5737a211d93aee60ed7'
+  sha256 '286361c88e11cb8c308ce97236ac04b6b328b6f3eead51057d22c3fa82db4006'
 
   url 'ftp://rixstep.com/CLIX.zip'
   name 'CLIX'
-  homepage 'http://rixstep.com/4/0/clix/'
+  homepage 'https://rixstep.com/4/0/clix/'
 
   app "CLIX/#{version.no_dots}/CLIX.app"
 
