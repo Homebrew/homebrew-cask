56234055e30c1a559dc420dcb3922953361051c9
Work <brigitte@corecode.io>
2019-05-04 17:04:41 +0200

    Update dynamic-dark-mode from 1.1.4 to 1.1.5

--- Casks/dynamic-dark-mode.rb
+++ Casks/dynamic-dark-mode.rb
@@ -1,6 +1,6 @@
 cask 'dynamic-dark-mode' do
-  version '1.1.4'
-  sha256 '2fb033042bfbdcb39590f74ca6479252e1fdcc1529b05e03e8733c87a7abab6f'
+  version '1.1.5'
+  sha256 'f0bb7542fb46eed2fd69f5e64f72bd15628d9e1b7be063ea41124fbc88aa1222'
 
   url "https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases/download/#{version}/Dynamic_Dark_Mode-#{version}.zip"
   appcast 'https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases.atom'
