e3a377a13d88623ec7ce1756f261b14000953f7f
Stefan Stüben <MSNexploder@gmail.com>
2019-05-04 15:22:48 +0200

    Update hashbackup from 2282 to 2295

--- Casks/hashbackup.rb
+++ Casks/hashbackup.rb
@@ -1,6 +1,6 @@
 cask 'hashbackup' do
-  version '2282'
-  sha256 '7a699886fdc4338ac05522feb194f5010caefbfbff559fd51478bca819fc2647'
+  version '2295'
+  sha256 'e3930f5f49b742faeb5295b88fed4b42cdbe0cb2c7ddb0ce274101bfdc3203e1'
 
   url "http://www.hashbackup.com/download/hb-#{version}-mac-64bit.tar.gz"
   name 'hashbackup'
