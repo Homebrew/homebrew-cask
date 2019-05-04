0bd63164778a3fca4bc1813a3aed2d4784262a07
suschizu <brigitte@corecode.io>
2019-05-04 12:42:36 +0200

    Update electron from 5.0.0 to 5.0.1 (#62786)

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '5.0.0'
-  sha256 'f5aaf5115e2be4f4a65b6bce117157baa03ce66630c0dce535a85fe70866421f'
+  version '5.0.1'
+  sha256 '00694c5541385d6709d9652930011cb5f462c222642c7aa64dd266905abfda12'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
