64c86ab0403a900d88becec7a9190196022e1bf0
CoreCode <core-code@users.noreply.github.com>
2019-05-06 17:05:31 +0200

    Update flock from 2.2.230 to 2.2.248 (#62894)

--- Casks/flock.rb
+++ Casks/flock.rb
@@ -1,6 +1,6 @@
 cask 'flock' do
-  version '2.2.230'
-  sha256 '339f500c1f376e8283c2b363184b6d63b0cc3166aa22c3327b375a35e4fb93ee'
+  version '2.2.248'
+  sha256 '1b0ff6b9b127af44a705bbaa9700debf1366ebe4dc45952b94b4395a1a29b636'
 
   # flock.co was verified as official when first introduced to the cask
   url "https://updates.flock.co/fl_mac_electron/Flock-macOS-#{version}.dmg"
