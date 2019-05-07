e05eca93f0936d644a5727fece2185ea285b7fe5
suschizu <brigitte@corecode.io>
2019-05-07 16:23:57 +0200

    Update gog-galaxy from 1.2.55.53 to 1.2.56.20 (#62956)

--- Casks/gog-galaxy.rb
+++ Casks/gog-galaxy.rb
@@ -1,6 +1,6 @@
 cask 'gog-galaxy' do
-  version '1.2.55.53'
-  sha256 'e3009a71c14e4ee35202e96ef6b7a1312c2adfc56f35089c5e2297baa5acc792'
+  version '1.2.56.20'
+  sha256 'b8e8c3949acdaf01b62302beda8e8f6528a832d4b696cd3069803baf54bc02ac'
 
   url "https://cdn.gog.com/open/galaxy/client/galaxy_client_#{version}.pkg"
   name 'GOG Galaxy Client'
