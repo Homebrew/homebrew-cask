ce1363c5851b49d3ffbcca1bae9af225886032e5
Work <brigitte@corecode.io>
2019-05-07 14:17:50 +0200

    Update hachidori from 3.1.7 to 3.1.8

--- Casks/hachidori.rb
+++ Casks/hachidori.rb
@@ -1,6 +1,6 @@
 cask 'hachidori' do
-  version '3.1.7'
-  sha256 'a775f49de9b6da9f1d5a2f04f900c76b4935cc11a2069517084511e694b416e0'
+  version '3.1.8'
+  sha256 '41cc21439ec4485eb84d96ffe489ff3529a5af218c44d86f465835947b9c22c3'
 
   # github.com/Atelier-Shiori/hachidori was verified as official when first introduced to the cask
   url "https://github.com/Atelier-Shiori/hachidori/releases/download/#{version}/hachidori-#{version}.dmg"
