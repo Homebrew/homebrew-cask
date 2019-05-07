2bfaff7964f6ba51bc779bfb9a0bc2763f360305
suschizu <brigitte@corecode.io>
2019-05-07 19:49:38 +0200

    Update cisdem-document-reader.rb

--- Casks/cisdem-document-reader.rb
+++ Casks/cisdem-document-reader.rb
@@ -2,7 +2,7 @@ cask 'cisdem-document-reader' do
   version '4.2.0'
   sha256 '3a52424200895d0ae292fce55243de69f82f89f108472a1e42b0e5f56fbcbd00'
 
-  url 'https://www.cisdem.com/downloads/cisdem-documentreader-18.dmg'
+  url 'http://download.cisdem.com/cisdem-documentreader.dmg'
   appcast 'https://www.cisdem.com/document-reader-mac/release-notes.html'
   name 'Cisdem Document Reader'
   homepage 'https://www.cisdem.com/document-reader-mac.html'
