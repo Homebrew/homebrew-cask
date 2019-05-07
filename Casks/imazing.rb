fa82ec77e281b7a5329bf79cd0271aedf62918da
tiiiecherle <tiiiecherle@gmail.com>
2019-05-07 13:27:05 +0200

    Update imazing (#62927)

--- Casks/imazing.rb
+++ Casks/imazing.rb
@@ -1,6 +1,6 @@
 cask 'imazing' do
-  version '2.9.5,10630:1553604114'
-  sha256 '10840254b71b422ae2338e96fd1d43aea4ae89a5e32f22f7151dc628f100cc56'
+  version '2.9.6,10702:1557156734'
+  sha256 '9d9a1d9005d208c3b74aa43448bf753c576ea6319db963e7e6b56d5c6c2a4860'
 
   # dl.devmate.com/com.DigiDNA.iMazing2Mac was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.DigiDNA.iMazing2Mac/iMazing#{version.major}forMac.dmg"
