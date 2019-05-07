45b7a56d86c1d6da2ed4971adda5c2d8fa5653a5
Micah Lee <micah@micahflee.com>
2019-05-07 13:49:38 -0700

    OnionShare filename should actually OnionShare-#{version}.pkg not OnionShare-#{version.major}.pkg

--- Casks/onionshare.rb
+++ Casks/onionshare.rb
@@ -2,12 +2,12 @@ cask 'onionshare' do
   version '2.1'
   sha256 '5f1d861eeb1422f592b388b200286704c9d94288a20c146164ebed57fef6b077'
 
-  url "https://onionshare.org/dist/#{version}/OnionShare-#{version.major}.pkg"
+  url "https://onionshare.org/dist/#{version}/OnionShare-#{version}.pkg"
   appcast 'https://github.com/micahflee/onionshare/releases.atom'
   name 'OnionShare'
   homepage 'https://onionshare.org/'
 
-  pkg "OnionShare-#{version.major}.pkg"
+  pkg "OnionShare-#{version}.pkg"
 
   uninstall pkgutil: 'com.micahflee.onionshare'
 end
