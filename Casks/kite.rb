0c726688708d76c8f5e47e4a75936b2f878c0db5
Julian Mayer <julian@corecode.io>
2019-05-07 13:08:59 +0200

    Update kite from 0.20190501.0 to 0.20190502.0

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,6 +1,6 @@
 cask 'kite' do
-  version '0.20190501.0'
-  sha256 'bdc79bdcf4277ab4e726292828a9c081490859d31f8707746a4aef9433ddf6cc'
+  version '0.20190502.0'
+  sha256 '52b87b914ae9f7798a2cedd312a99065950dc0d14a351b5caaf65f81e51c2cbd'
 
   # s3-us-west-1.amazonaws.com/kite-downloads was verified as official when first introduced to the cask
   url "https://s3-us-west-1.amazonaws.com/kite-downloads/Kite-#{version}.dmg"
