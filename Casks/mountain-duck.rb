2564740a1bac0c1ad8087641c852fd9c2e7a52a9
Vítor Galvão <info@vitorgalvao.com>
2019-05-08 17:18:33 +0100

    Update mountain-duck.rb

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,6 +1,7 @@
 cask 'mountain-duck' do
   version '3.0.1.14013'
   sha256 '64450b878dbf4d4b48b1f14f1da74b872c37899b869c8a83a1189be483f021b7'
+
   url "https://dist.mountainduck.io/Mountain%20Duck-#{version}.zip"
   appcast 'https://version.mountainduck.io/changelog.rss'
   name 'Mountain Duck'
