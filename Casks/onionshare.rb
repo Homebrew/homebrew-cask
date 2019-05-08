482870908f3651c0aae7910da2da93c1c1d6a7be
Micah Lee <micah@micahflee.com>
2019-05-08 11:39:44 -0700

    OnionShare 2.1 (#62968)
    * Update onionshare to 2.1, and change download URL
    
    * OnionShare filename should actually OnionShare-#{version}.pkg not OnionShare-#{version.major}.pkg

--- Casks/onionshare.rb
+++ Casks/onionshare.rb
@@ -2,8 +2,7 @@ cask 'onionshare' do
   version '2.1'
   sha256 '5f1d861eeb1422f592b388b200286704c9d94288a20c146164ebed57fef6b077'
 
-  # github.com/micahflee/onionshare was verified as official when first introduced to the cask
-  url "https://github.com/micahflee/onionshare/releases/download/v#{version}/OnionShare-#{version}.pkg"
+  url "https://onionshare.org/dist/#{version}/OnionShare-#{version}.pkg"
   appcast 'https://github.com/micahflee/onionshare/releases.atom'
   name 'OnionShare'
   homepage 'https://onionshare.org/'
