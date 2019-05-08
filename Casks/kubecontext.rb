481f2c0c4ce05c98443c69cdbc6f4ebaaf9950c3
Hasan Turken <turkenh@gmail.com>
2019-05-09 01:08:16 +0300

    Use single quote to pass style check

--- Casks/kubecontext.rb
+++ Casks/kubecontext.rb
@@ -3,7 +3,7 @@ cask 'kubecontext' do
   sha256 '63396f87f4fda10cd357c76d49cde1001e0644ad91f93ad3fdec1899b519990c'
 
   url "https://github.com/turkenh/KubeContext/releases/download/v#{version}/KubeContext.zip"
-  appcast "https://github.com/turkenh/KubeContext/releases.atom"
+  appcast 'https://github.com/turkenh/KubeContext/releases.atom'
   name 'KubeContext'
   homepage 'https://github.com/turkenh/KubeContext'
 
