ec791becffb6c86436856e41d9283b400ef1ce6b
Nilesh Suthar <nileshsuthar@live.in>
2019-05-09 11:34:13 +0530

    Update android-platform-tools 28.0.2 to 28.0.3 (#63027)

--- Casks/android-platform-tools.rb
+++ Casks/android-platform-tools.rb
@@ -1,6 +1,6 @@
 cask 'android-platform-tools' do
-  version '28.0.2'
-  sha256 'ee3a6762bead5d77c19d8772f205b846c0170b8cd387f8e6d6932aed60d7814c'
+  version '28.0.3'
+  sha256 '13d687edf28402794c6e20ad1121c110abf8443b0c718f11a25b106c88f174c8'
 
   # google.com/android/repository/platform-tools was verified as official when first introduced to the cask
   url "https://dl.google.com/android/repository/platform-tools_r#{version}-darwin.zip"
