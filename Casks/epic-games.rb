66c84c7ea79f015c86bb8cd698b995a7b7ca57e4
CoreCode <core-code@users.noreply.github.com>
2019-05-10 01:12:55 +0200

    Update epic-games from 9.7.0 to 9.11.2 (#63035)
    * Update epic-games from 9.7.0 to 9.11.2
    
    * Update epic-games.rb
    
    * Update epic-games.rb

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,10 +1,10 @@
 cask 'epic-games' do
-  version '9.7.0'
-  sha256 '24b4c54a3e93b4879087cf99bd676b53543ffaa4c8c122b85efed6c747dc13ea'
+  version '9.11.2'
+  sha256 'd1d2550e0497f968a4be50ece4a66afa62f0792bb65204dd73dd80265e0e9e36'
 
   # epicgames-download1.akamaized.net was verified as official when first introduced to the cask
   url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg"
-  appcast 'https://forums.unrealengine.com/unreal-engine/announcements-and-releases/1928-latest-launcher-release-notes'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg'
   name 'Epic Games Launcher'
   homepage 'https://www.epicgames.com/'
 
