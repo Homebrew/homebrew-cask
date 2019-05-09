2dc4a724702b4a37f91a27875c97372d58f00208
CoreCode <core-code@users.noreply.github.com>
2019-05-09 16:52:42 +0200

    Update wingpersonal from 7.0.1.2 to 7.0.2.0 (#63048)

--- Casks/wingpersonal.rb
+++ Casks/wingpersonal.rb
@@ -1,6 +1,6 @@
 cask 'wingpersonal' do
-  version '7.0.1.2'
-  sha256 'bfe754577dd15b8fec79618cddafba62c7da3ec0a2b3401cab2eae214d46e914'
+  version '7.0.2.0'
+  sha256 'e90bfbbcdbc79992717937d61f30a8becf216d8b032fdd63193940e0bf764c20'
 
   url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}.dmg"
   appcast 'https://www.wingware.com/downloads/wing-personal'
