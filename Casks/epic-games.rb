afe6cc1df9fa7c8ce3d02f8efa0ee04c1843b2a3
Julian Mayer <julian@corecode.io>
2019-05-09 15:20:27 +0200

    Update epic-games from 9.7.0 to 9.11.2

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,6 +1,6 @@
 cask 'epic-games' do
-  version '9.7.0'
-  sha256 '24b4c54a3e93b4879087cf99bd676b53543ffaa4c8c122b85efed6c747dc13ea'
+  version '9.11.2'
+  sha256 'd1d2550e0497f968a4be50ece4a66afa62f0792bb65204dd73dd80265e0e9e36'
 
   # epicgames-download1.akamaized.net was verified as official when first introduced to the cask
   url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg"
