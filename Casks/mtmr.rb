f676c8861b5733eb38d69de02de8106ee21bf917
Anton Palgunov <Toxblh@users.noreply.github.com>
2019-05-09 22:25:04 +0100

    Update mtmr.rb

--- Casks/mtmr.rb
+++ Casks/mtmr.rb
@@ -1,6 +1,6 @@
 cask 'mtmr' do
   version '0.20.3'
-  sha256 '589cabbf3c39ca3ae7a870b2608c6309da793491e6ef99e0181ee3e33e6e8c54'
+  sha256 'c7a162669669f018e036dfb4fcd15441b75c73ad01dd5f396b479d04ab55bc8a'
 
   url "https://mtmr.app/MTMR%20#{version}.dmg"
   appcast 'https://mtmr.app/appcast.xml'
