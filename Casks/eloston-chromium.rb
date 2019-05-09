971d251965ad87748023faa2d224cf58bdfb8e8f
Vítor Galvão <info@vitorgalvao.com>
2019-05-10 00:14:56 +0100

    Update eloston-chromium.rb

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,7 +2,7 @@ cask 'eloston-chromium' do
   version '74.0.3729.131-1.1'
   sha256 'ff91741de6bbe95cf4bb7f9ac85e3a0be3612707f3421b0c25f2b14e897e7a5c'
 
-  # github.com/chase9/ungoogled-chromium-binaries was verified as official when first introduced to the cask
+  # github.com/unpredictable/ungoogled-chromium-binaries was verified as official when first introduced to the cask
   url "https://github.com/unpredictable/ungoogled-chromium-binaries/releases/download/#{version}/ungoogled-chromium_#{version}_macos.dmg"
   appcast 'https://ungoogled-software.github.io/ungoogled-chromium-binaries/releases/macos/'
   name 'Ungoogled Chromium'
