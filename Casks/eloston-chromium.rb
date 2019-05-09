ce546ec3a806387d1599ab97d179e0435b99b093
iliyan <18247476+okay@users.noreply.github.com>
2019-05-09 19:07:00 -0400

    Update eloston-chromium to version 74.0.3729.131-1.1

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -1,9 +1,9 @@
 cask 'eloston-chromium' do
-  version '73.0.3683.103-1.1'
-  sha256 '306441ac633f75f64004d95ac86df2270ced578825d23c955ef49d2fbcd876af'
+  version '74.0.3729.131-1.1'
+  sha256 'ff91741de6bbe95cf4bb7f9ac85e3a0be3612707f3421b0c25f2b14e897e7a5c'
 
   # github.com/chase9/ungoogled-chromium-binaries was verified as official when first introduced to the cask
-  url "https://github.com/chase9/ungoogled-chromium-binaries/releases/download/#{version}/ungoogled-chromium_#{version}_macos.dmg"
+  url "https://github.com/unpredictable/ungoogled-chromium-binaries/releases/download/#{version}/ungoogled-chromium_#{version}_macos.dmg"
   appcast 'https://ungoogled-software.github.io/ungoogled-chromium-binaries/releases/macos/'
   name 'Ungoogled Chromium'
   homepage 'https://ungoogled-software.github.io/ungoogled-chromium-binaries/'
