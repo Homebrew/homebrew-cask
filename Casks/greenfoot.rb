66057081b37aad24cd239ef946859437b7c8b8aa
Randall <ran.dall@icloud.com>
2019-05-09 09:35:41 -0700

    Update greenfoot from 3.5.3 to 3.5.4

--- Casks/greenfoot.rb
+++ Casks/greenfoot.rb
@@ -1,6 +1,6 @@
 cask 'greenfoot' do
-  version '3.5.3'
-  sha256 'c0ae05267c4493f067a103ac7eb4e6ad045279e0180d9b32907dfa9b61eab08e'
+  version '3.5.4'
+  sha256 'bb6f2942f6fc6b056917714d63fc1b4d9853a38f85e2296ab529bea45dd4f878'
 
   url "https://www.greenfoot.org/download/files/Greenfoot-mac-#{version.no_dots}.zip"
   name 'Greenfoot'
