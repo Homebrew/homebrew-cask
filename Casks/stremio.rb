85b13ffb95c4dd6a18bdaefc97802a530c0dfb91
CoreCode <core-code@users.noreply.github.com>
2019-05-10 00:56:44 +0200

    Update stremio (#63033)

--- Casks/stremio.rb
+++ Casks/stremio.rb
@@ -3,7 +3,7 @@ cask 'stremio' do
   sha256 '385df67729d8ac7af2f1431b4adfdee91489152e8668687fa2c8e7b8814e6351'
 
   url "https://dl.strem.io/mac/v#{version}/Stremio+#{version}.dmg"
-  appcast 'https://www.stremio.com/downloads'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.strem.io/download?platform=mac%26four=true'
   name 'Stremio'
   homepage 'https://www.strem.io/'
 
