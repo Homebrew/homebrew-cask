67cbe576ee365e7760cfc3e0db741f80b169b41f
Erica Ehrhardt <955421+SteelPangolin@users.noreply.github.com>
2019-05-08 20:50:39 -0700

    Update focuswriter from 1.7.1 to 1.7.2

--- Casks/focuswriter.rb
+++ Casks/focuswriter.rb
@@ -1,6 +1,6 @@
 cask 'focuswriter' do
-  version '1.7.1'
-  sha256 '2f1763e2bc7ef3363c8006eb3081cb34358be98ae0567bbe007ea8e94579cd61'
+  version '1.7.2'
+  sha256 '78ebc45618f5826cdc216752628fefe6163479803b394d200ef6c0d547e694eb'
 
   url "https://gottcode.org/focuswriter/FocusWriter_#{version}.dmg"
   name 'FocusWriter'
