21f115079a67612f3b501287bc184662ba08f318
CoreCode <core-code@users.noreply.github.com>
2019-05-09 15:26:58 +0200

    Update epic-games.rb

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -4,7 +4,7 @@ cask 'epic-games' do
 
   # epicgames-download1.akamaized.net was verified as official when first introduced to the cask
   url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg"
-  appcast 'https://forums.unrealengine.com/unreal-engine/announcements-and-releases/1928-latest-launcher-release-notes'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg?productName=unrealengine'
   name 'Epic Games Launcher'
   homepage 'https://www.epicgames.com/'
 
