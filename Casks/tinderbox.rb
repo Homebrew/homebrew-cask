65fb565c8975b10c5b0565efe811823092e2563f
Julian Mayer <julian@corecode.io>
2019-05-09 16:10:15 +0200

    Update tinderbox from 8.0.0 to 8.0.1

--- Casks/tinderbox.rb
+++ Casks/tinderbox.rb
@@ -4,8 +4,8 @@ cask 'tinderbox' do
     sha256 '765a6245d25f9c2185802f36caa1f620f276637b884260fffa74bf639670e211'
     app 'TinderboxSix.app'
   else
-    version '8.0.0'
-    sha256 'e2c4eeddbe9489edb2af80d4fbb7dbe4f1da49ef8d6fff665fb292c5337b4a92'
+    version '8.0.1'
+    sha256 'd07ffbf2f5ff820a6e9807dfa64040fcc8763a051763b3f4630abe10b5019d15'
     app "Tinderbox #{version.major}.app"
   end
 
