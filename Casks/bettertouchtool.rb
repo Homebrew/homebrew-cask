dd31954d7f4a03adccc795be21e2011cfcb590f6
CoreCode <core-code@users.noreply.github.com>
2019-05-10 19:39:16 +0200

    Update bettertouchtool from 2.809 to 2.813 (#63108)

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -6,8 +6,8 @@ cask 'bettertouchtool' do
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}_final_10_9.zip"
   else
-    version '2.809'
-    sha256 '8f3ee5ad14eae3e0a6ee2139b1d67df0cce18c92fbc9d94ef915ba5b658e38eb'
+    version '2.813'
+    sha256 'c82f20d6996d9ee543b1aec660610df12e5158bc607cb02ff3b5a3781ea75ab3'
 
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}.zip"
