7bdbd1db92190b9eee1bb2f1fc5983942aea774e
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2019-05-10 09:31:12 +0200

    Update telegram from 5.1.1-166797 to 5.2-170089

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '5.1.1-166797'
-  sha256 '0cb4ca1ec8e768d5a4a750a2bbd3280e850cd25fd2b76d708bb5fb1b1509e3f8'
+  version '5.2-170089'
+  sha256 '5cffe875319c2681d8ad0e3b29fa04f5fcab4022eec79cc3497605c5fb2dc13c'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml'
