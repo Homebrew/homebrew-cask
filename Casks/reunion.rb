84e56a849a41e4bf3ec6e19d8589e003a1fad0cd
Erlend Mongstad <eeerlend@gmail.com>
2019-05-11 01:49:40 +0200

    Added cask for Reunion 12

---
+++ Casks/reunion.rb
@@ -0,0 +1,10 @@
+cask 'reunion' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.leisterpro.com/download/Reunion-12.dmg'
+  name 'Reunion 12'
+  homepage 'http://www.leisterpro.com/'
+
+  app 'Reunion 12.app'
+end
