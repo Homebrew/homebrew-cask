6f8e5224663017d21453f54abe82658e9723741a
J. Russell Smyth <jrussell.smyth@gmail.com>
2019-05-09 22:52:34 -0700

    add cask for synth1 Audio Unit

--- Casks/synth1-au.rb
+++ Casks/synth1-au.rb
@@ -1,10 +1,10 @@
-cask 'synth1-vst' do
+cask 'synth1-au' do
   version '1.13beta8'
-  sha256 '6b172b9433358bce21f0af75a28505c3365d934fadee04a738336e7f5f601675'
+  sha256 '78660848d78c19cdda2597bd050a2ec95fa915cc1a1ab598bde0192a1cbc9a6f'
 
   url "https://web.archive.org/web/20190331141735/http://www.geocities.jp/daichi1969/softsynth/Synth1macau#{version.no_dots}.zip"
   name 'Synth1 (VST)'
   homepage 'https://web.archive.org/web/20190331141735/http://www.geocities.jp/daichi1969/softsynth/'
 
-  au_plugin 'Synth1.vst'
+  audio_unit_plugin 'Synth1.component'
 end
