310f951705f85db3720ae13c3eaaf9ba06534b78
Vítor Galvão <info@vitorgalvao.com>
2019-05-11 13:28:44 +0100

    Update lingon-x.rb

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -2,14 +2,13 @@ cask 'lingon-x' do
   if MacOS.version <= :high_sierra
     version '6.6.4'
     sha256 'fc788402fa16df39a3d48cdc501dae31368ec6fd69ffe0026ba99932b28cae19'
-    appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}-appcast.xml"
   else
     version '7.0.0'
     sha256 '4c398afdbb595ae20af141063bffceefa6c3fabf77f86078a960df9631e83a96'
-    appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}.plist"
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
+  appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}.plist"
   name 'Lingon X'
   homepage 'https://www.peterborgapps.com/lingon/'
 
