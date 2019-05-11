b72e8b02eff5cb1ef3ef1919575dfbd2566bd538
Vítor Galvão <info@vitorgalvao.com>
2019-05-11 13:30:54 +0100

    Update redcine-x-pro.rb

--- Casks/redcine-x-pro.rb
+++ Casks/redcine-x-pro.rb
@@ -3,7 +3,7 @@ cask 'redcine-x-pro' do
   sha256 '4f8021224cd4e705ca18f2709540731046e991efb23e7232e157ad59b89604c1'
 
   # s3.amazonaws.com/red_3/ was verified as official when first introduced to the cask
-  url "http://s3.amazonaws.com/red_3/downloads/software/rcx/RELEASE%20CANDIDATE/REDCINE-X_PRO_Build_#{version.major_minor}.pkg"
+  url "https://s3.amazonaws.com/red_3/downloads/software/rcx/RELEASE%20CANDIDATE/REDCINE-X_PRO_Build_#{version.major_minor}.pkg"
   appcast 'https://www.red.com/downloads/options?itemInternalId=16144'
   name 'REDCINE-X PRO'
   homepage 'https://www.red.com/'
