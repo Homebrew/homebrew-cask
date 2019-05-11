5e9a82e8d2a9096a7081c82451ef24360cd3832b
ralph <ralph@cs.cf.ac.uk>
2019-05-11 07:50:19 +0100

    Update lingon-x from 6.6.4 to 7.0.0

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,6 +1,6 @@
 cask 'lingon-x' do
-  version '6.6.4'
-  sha256 'fc788402fa16df39a3d48cdc501dae31368ec6fd69ffe0026ba99932b28cae19'
+  version '7.0.0'
+  sha256 '4c398afdbb595ae20af141063bffceefa6c3fabf77f86078a960df9631e83a96'
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
   appcast "https://www.peterborgapps.com/updates/lingonx#{version.major}-appcast.xml"
