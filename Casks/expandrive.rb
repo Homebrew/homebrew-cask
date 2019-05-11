6c505b1a7763c9e140f1ccacf64a79e3d47fbdba
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-11 06:32:26 -0700

    Update expandrive from 7.0.15 to 7.0.16 (#63133)

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,6 +1,6 @@
 cask 'expandrive' do
-  version '7.0.15'
-  sha256 '23d94753ffa7090632a5457338ed836abbf9b3ec81d134edce5a08d6aecd2ab6'
+  version '7.0.16'
+  sha256 'c51a4e843ad6418c181686edc56a878b05e1fa6fdd2627247d91b237b8423847'
 
   url "https://updates.expandrive.com/apps/expandrive#{version.major}/v/#{version.dots_to_hyphens}/update_download"
   appcast "https://updates.expandrive.com/appcast/expandrive#{version.major}.json?version=#{version.major}.0.0"
