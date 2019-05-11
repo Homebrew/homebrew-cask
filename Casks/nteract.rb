e2a5c04206e8b05cd974d75666fc35f370e6c459
Leonardo <DocLM@users.noreply.github.com>
2019-05-11 02:40:53 +0200

    Nteract v0.14.2 (#63086)
    * nteract: auto update
    
    * Update nteract to 0.14.2

--- Casks/nteract.rb
+++ Casks/nteract.rb
@@ -1,11 +1,13 @@
 cask 'nteract' do
-  version '0.14.0'
-  sha256 '366fa9f3cc56b1cef936eaf5f8038857dd7cd57e457436680b7b3d0868714778'
+  version '0.14.2'
+  sha256 'c7089e7b85e5afb53624345bfd4622fb8fe466f5cd30498a57023d78736014ba'
 
   url "https://github.com/nteract/nteract/releases/download/v#{version}/nteract-#{version}.dmg"
   appcast 'https://github.com/nteract/nteract/releases.atom'
   name 'nteract'
   homepage 'https://github.com/nteract/nteract'
+
+  auto_updates true
 
   app 'nteract.app'
 end
