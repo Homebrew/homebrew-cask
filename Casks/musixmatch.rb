ad96201f1e73eba5709d803e1b491c44b2b7b527
Kunanon Srisuntiroj <son9912@gmail.com>
2019-05-11 23:53:15 +0700

    Add Musixmatch v3.2.0

---
+++ Casks/musixmatch.rb
@@ -0,0 +1,10 @@
+cask 'musixmatch' do
+  version '3.2.0'
+  sha256 'ca35c5c87e4054a38956393ecc8c36bf9ad67169bce9f9a380de2df58a7747f7'
+
+  url "https://download-app.musixmatch.com/download/Musixmatch-#{version}.dmg"
+  name 'Musixmatch'
+  homepage 'https://musixmatch.com/'
+
+  app ''
+end
