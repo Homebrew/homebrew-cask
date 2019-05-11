e6ce77fe25b8e781aa5ed561af2dc90dce56dd4b
Stefan Stüben <MSNexploder@gmail.com>
2019-05-11 19:00:58 +0200

    Update hashbackup from 2298 to 2302

--- Casks/hashbackup.rb
+++ Casks/hashbackup.rb
@@ -1,6 +1,6 @@
 cask 'hashbackup' do
-  version '2298'
-  sha256 '9747604a5350e8a95ea091de3d7303b2d759ffd9994063123e0b38ba47a25587'
+  version '2302'
+  sha256 '3f03bd6b565e63a45b7955f83751ef80ecd6a856ef6d699cc5b84e4fcd268a01'
 
   url "http://upgrade.hashbackup.com/#{version}/hb.r#{version}.Darwin.i386.bz2"
   name 'hashbackup'
