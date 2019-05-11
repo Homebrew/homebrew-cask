9bacace1d0e8f1fabe577a1d8af1822bda550bd5
Erlend Mongstad <eeerlend@gmail.com>
2019-05-11 02:05:27 +0200

    Added version 12 to the cask file for Reunion, according to naming conventions

--- Casks/reunion.rb
+++ Casks/reunion.rb
@@ -1,9 +1,9 @@
 cask 'reunion' do
-  version :latest
+  version '12'
   sha256 :no_check
 
   url 'http://www.leisterpro.com/download/Reunion-12.dmg'
-  name 'Reunion 12'
+  name 'Reunion'
   homepage 'http://www.leisterpro.com/'
 
   app 'Reunion 12.app'
