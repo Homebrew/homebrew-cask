f0218d9c8bd12caaf25575e6106b47c961d1d20b
suschizu <brigitte@corecode.io>
2019-05-12 11:55:59 +0200

    Update electrum-ltc from 3.3.4.1 to 3.3.5.1 (#63150)

--- Casks/electrum-ltc.rb
+++ Casks/electrum-ltc.rb
@@ -1,6 +1,6 @@
 cask 'electrum-ltc' do
-  version '3.3.4.1'
-  sha256 'bfc082b692905fe279b5666adf2fe62ba83652d4b6162e539f7a81f651d70bfe'
+  version '3.3.5.1'
+  sha256 'c266dae2e694b7ccf943f42ad068d0f188d3d683881c2f2686969f018c393b40'
 
   url "https://electrum-ltc.org/download/electrum-ltc-#{version}.dmg"
   appcast 'https://electrum-ltc.org/'
