4a7dbbff08b1c0c970d0601fc90ce94490ddd298
Randall <ran.dall@icloud.com>
2019-05-12 03:06:08 -0700

    Update superproductivity from 2.6.1 to 2.6.3

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,6 +1,6 @@
 cask 'superproductivity' do
-  version '2.6.1'
-  sha256 '087e07b9a4ad0b91f183b67aa892aa75abd564583b90392bfb19ac5eb8247cab'
+  version '2.6.3'
+  sha256 '0d0cbe40a8b2d93b64584aac6f07a7c3c34a1b82d60519ed24e17616f945f365'
 
   # github.com/johannesjo/super-productivity was verified as official when first introduced to the cask
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip"
