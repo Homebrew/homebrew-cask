5324c804650b0c217e617c49eb3ee5bdbd5f36a7
brigitte zmoelnig <brigitte@corecode.io>
2019-05-12 21:22:30 +0200

    Update axure-rp from 9.0.0.3647 to 9.0.0.3648

--- Casks/axure-rp.rb
+++ Casks/axure-rp.rb
@@ -1,5 +1,5 @@
 cask 'axure-rp' do
-  version '9.0.0.3647'
+  version '9.0.0.3648'
   sha256 'a08ea6dae3c612545dfcf023549deee7259df14edfe263eb3295c43e5610fe4b'
 
   # axure.cachefly.net was verified as official when first introduced to the cask
