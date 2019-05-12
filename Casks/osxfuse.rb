5d0eaac4fbfedf7d372e824b229513a20a41344f
Kelvin Sherlock <ksherlock@gmail.com>
2019-05-12 13:33:42 -0400

    Update osxfuse to 3.9.0

--- Casks/osxfuse.rb
+++ Casks/osxfuse.rb
@@ -1,6 +1,6 @@
 cask 'osxfuse' do
-  version '3.8.3'
-  sha256 '87e507c44c19689beefa3d47dd00ba953254d9e616cb633c1b4343407fe99700'
+  version '3.9.0'
+  sha256 '9bd40e487c18abcf94a72b96c9f3b1378b69a1a0906f05c451aa918180bcc394'
 
   # github.com/osxfuse was verified as official when first introduced to the cask
   url "https://github.com/osxfuse/osxfuse/releases/download/osxfuse-#{version}/osxfuse-#{version}.dmg"
