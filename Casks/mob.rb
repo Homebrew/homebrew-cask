f1f429f016c2b60b0d3e90a27f5be84a32ba133e
Jason Zeng <zenghongtu@qq.com>
2019-05-13 19:44:39 +0800

    add Mob v0.1.2 (#63177)
    * Add Mob.app v0.1.2
    
    * Update mob.rb
    
    * Update mob.rb

---
+++ Casks/mob.rb
@@ -0,0 +1,13 @@
+cask 'mob' do
+  version '0.1.2'
+  sha256 '5506cfce02ed8d9f6554a27af5f0d75617e0496dd3174a695de8ed943428c351'
+
+  url "https://github.com/zenghongtu/Mob/releases/download/v#{version}/Mob-#{version}-mac.dmg"
+  appcast 'https://github.com/zenghongtu/Mob/releases.atom'
+  name 'Mob'
+  homepage 'https://github.com/zenghongtu/Mob'
+
+  app 'Mob.app'
+
+  zap trash: '~/Library/Application Support/mob'
+end
