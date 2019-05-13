aa5390c59b301c30d9d60090f77791a85e6c13f0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-13 10:46:46 -0700

    Update unicodechecker from 1.21 to 1.21.1 (#63209)
    * Update unicodechecker from 1.21 to 1.21.1
    
    * Update App folder

--- Casks/unicodechecker.rb
+++ Casks/unicodechecker.rb
@@ -1,13 +1,13 @@
 cask 'unicodechecker' do
-  version '1.21'
-  sha256 'b478c39d96f58aa355a073ce39b80320c7f717e738a1629ee2653bdf288889f9'
+  version '1.21.1,755'
+  sha256 '0b6019277bade20ccd2904adf0fd29c7395ab495e02ace8c5b58d4e33ebd6587'
 
   url 'https://earthlingsoft.net/UnicodeChecker/UnicodeChecker.zip'
   appcast 'https://earthlingsoft.net/UnicodeChecker/appcast.xml'
   name 'UnicodeChecker'
   homepage 'https://earthlingsoft.net/UnicodeChecker/'
 
-  app "UnicodeChecker #{version}/UnicodeChecker.app"
+  app "UnicodeChecker #{version.before_comma} (#{version.after_comma})/UnicodeChecker.app"
 
   zap trash: [
                '~/Library/Application Support/UnicodeChecker',
