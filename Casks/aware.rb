47a07226f05622712532c69f46d24220bb6a4fd2
Lorenzo Fanchi <lorenzo.fanchi@gmail.com>
2019-05-13 12:11:16 +0200

    Update Aware to 1.0.4

--- Casks/aware.rb
+++ Casks/aware.rb
@@ -1,9 +1,9 @@
 cask 'aware' do
-  version '1.0.3'
-  sha256 '65dcaef6735c649e2b6a406f48583609ca9d66e7680ca69eb61871502613dec3'
+  version '1.0.4'
+  sha256 '040e4ef92a12e5db5f5d1b3e81f9cdefc3011dd354d201bd5172fa08c9df8a09'
 
   # github.com/josh/Aware was verified as official when first introduced to the cask
-  url "https://github.com/josh/Aware/releases/download/v#{version}/Aware.zip"
+  url "https://github.com/josh/Aware/archive/v#{version}.zip"
   appcast 'https://github.com/josh/Aware/releases.atom'
   name 'Aware'
   homepage 'https://awaremac.com/'
