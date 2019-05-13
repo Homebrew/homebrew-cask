d284cd4cdc089d7e2c615a181b4c0ad33511de1a
Julian Mayer <julian@corecode.io>
2019-05-13 14:54:40 +0200

    Update code42-crashplan from 6.9.0 to 6.9.4

--- Casks/code42-crashplan.rb
+++ Casks/code42-crashplan.rb
@@ -1,9 +1,10 @@
 cask 'code42-crashplan' do
-  version '6.9.0'
-  sha256 '2aa00419b2b02238ec7f96835d2e82fa80de288276a8a46995f4146032115097'
+  version '6.9.4'
+  sha256 'a58050cecec00b2fadf3a81d5f8e536c341d351d238a1d37bc70e490940baad3'
 
   # download.code42.com was verified as official when first introduced to the cask
   url "https://download.code42.com/installs/mac/install/Code42CrashPlan/Code42CrashPlan_#{version}_Mac.dmg"
+  appcast 'https://support.code42.com/Release_Notes/CrashPlan_for_Small_Business_release_notes'
   name 'CrashPlan'
   homepage 'https://www.crashplan.com/'
 
