b25a4a46641b3f2f71e38cfabdcbce3f9defb811
Work <brigitte@corecode.io>
2019-05-13 14:15:20 +0200

    Update tankix from visual3-40493 to visual3-41947

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -1,6 +1,6 @@
 cask 'tankix' do
-  version 'visual3-40493'
-  sha256 'bccdfa37464c64087e132380427db4b7aced4c79544bf622d09b686185603110'
+  version 'visual3-41947'
+  sha256 '242c4a588191689434a08e6bfb2fb8a94e74a1c8ad866e29e2f12ab6c41d40e8'
 
   url "http://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
   appcast 'https://tankix.com/en/download/'
