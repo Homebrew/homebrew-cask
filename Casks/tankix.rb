3183b2f0b70065e7ddc08af0f262692838b4f2f9
suschizu <brigitte@corecode.io>
2019-05-13 16:52:27 +0200

    Update tankix.rb

--- Casks/tankix.rb
+++ Casks/tankix.rb
@@ -2,7 +2,7 @@ cask 'tankix' do
   version 'visual3-41947'
   sha256 '242c4a588191689434a08e6bfb2fb8a94e74a1c8ad866e29e2f12ab6c41d40e8'
 
-  url "http://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
+  url "https://static.tankix.com/app/StandaloneOSXIntel64/#{version}/TankiX.dmg"
   appcast 'https://tankix.com/en/download/'
   name 'Tanki X'
   homepage 'https://tankix.com/'
