3cf62a5eb6a7083da1f7ddcdc90d18b2f8fbb049
Work <brigitte@corecode.io>
2019-05-14 16:12:19 +0200

    Update wordpresscom from 4.0.0 to 4.1.0

--- Casks/wordpresscom.rb
+++ Casks/wordpresscom.rb
@@ -1,6 +1,6 @@
 cask 'wordpresscom' do
-  version '4.0.0'
-  sha256 'c55992a869274a1f6d7509f27c15000d3b8842d10584a214570a90c83024f93d'
+  version '4.1.0'
+  sha256 '75ad26ede927a5f3d796b7d9525ec59ffa76f2a64cfc0088a6ad0a1a9b31d2e7'
 
   url "https://public-api.wordpress.com/rest/v1.1/desktop/osx/download?type=app&ref=update&version=#{version}"
   appcast 'https://public-api.wordpress.com/rest/v1.1/desktop/osx/version?compare=0.1.0&channel=stable'
