605d532e3df7a953e2818d11a511c40cb95d9bbb
Randall <ran.dall@icloud.com>
2019-05-14 12:51:30 -0700

    Update itubedownloader from 6.4.8 to 6.4.9

--- Casks/itubedownloader.rb
+++ Casks/itubedownloader.rb
@@ -1,6 +1,6 @@
 cask 'itubedownloader' do
-  version '6.4.8'
-  sha256 '691003498c6214d779e2d78581b13a0af839111cae867f3e1f2ffa0e29f92593'
+  version '6.4.9'
+  sha256 'ccc208d6f6a4587a8ceb6e846c98e00e701c23584deb16962d96488ce3fd4a1e'
 
   # dl.devmate.com/com.AlphaSoft.iTubeDownloader was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.AlphaSoft.iTubeDownloader/iTubeDownloader.dmg'
