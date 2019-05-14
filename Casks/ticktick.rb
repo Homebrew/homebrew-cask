dcd6b70ce819b4af605eacc19090d762d64587d4
Sky.Grain <SkyGrain@users.noreply.github.com>
2019-05-14 10:18:17 +0800

    Update ticktick.rb
    version update

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -1,6 +1,6 @@
 cask 'ticktick' do
-  version '3.0.01,93'
-  sha256 '86e6da635af467c4e2bd566ce8227c98e8bcae3509f3a29c7b85b12772423dba'
+  version '3.0.05,94'
+  sha256 '658c769299bba844ec2393a14c13b6890bbfc4d9a7bfc0782867d3054e942937'
 
   # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
