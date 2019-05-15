6b62386c2fd23bb0f9bbad7058b81f864631ed62
suschizu <brigitte@corecode.io>
2019-05-15 13:05:03 +0200

    Update megasync (#63304)

--- Casks/megasync.rb
+++ Casks/megasync.rb
@@ -1,6 +1,6 @@
 cask 'megasync' do
   version '4.0.2'
-  sha256 '2d261437650dad496b073bf13bfd5026e3770674bb5de7380aa6aa9e0da13016'
+  sha256 '9a294ac8de233f07014832d5ad217eb540a75e1debb8ec4eb3df4b5e48776f22'
 
   url 'https://mega.nz/MEGAsyncSetup.dmg'
   appcast 'https://github.com/meganz/MEGAsync/releases.atom'
