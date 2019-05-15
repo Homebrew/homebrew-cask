7b142f8922c808098e41a677c4e03809e8c5879c
suschizu <brigitte@corecode.io>
2019-05-15 14:54:32 +0200

    Update tinymediamanager from 2.9.16_42413ce to 2.9.17_a55d726 (#63310)

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,6 +1,6 @@
 cask 'tinymediamanager' do
-  version '2.9.16_42413ce'
-  sha256 'd248355ff375bcdc5f830535b567630b78f2d214d1bc5c4d0945d2e1b356ac42'
+  version '2.9.17_a55d726'
+  sha256 'db80b8677f04c53d38441a60b930b81c0cbb1ad41a911e69889656bb64802409'
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
   appcast 'https://release.tinymediamanager.org/'
