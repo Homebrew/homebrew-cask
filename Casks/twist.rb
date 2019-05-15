1b3c34cec2c996a2cbc8a248213e42bff084f4e7
Randall <ran.dall@icloud.com>
2019-05-15 06:54:06 -0700

    Update twist from 1.6.10,6257 to 1.6.11,6301

--- Casks/twist.rb
+++ Casks/twist.rb
@@ -1,6 +1,6 @@
 cask 'twist' do
-  version '1.6.10,6257'
-  sha256 'cb4dd5181b66e39833c025f9c6d93d74e2abbce934d01a3121d18559a0e0eb1c'
+  version '1.6.11,6301'
+  sha256 '51fec3d4b0af141c896a847153417a032c8c8bb1a93f7168cdca99d916b16e4d'
 
   url "https://downloads.twistapp.com/mac/Twist-#{version.after_comma}.zip"
   appcast 'https://downloads.twistapp.com/mac/AppCast.xml'
