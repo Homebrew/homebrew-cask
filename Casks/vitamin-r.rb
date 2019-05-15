520ac131adf8868c2838e39b3c6013c1f37ae271
Gregory R. Everitt <greg@elysiumhealth.com>
2019-05-15 14:44:36 -0400

    Marks Vitamin-R as auto-updating above v3
    Applies to vitamin-r versions 3.08 and 2.58, enabling auto-updates for
    the former and explicitly marking auto-update as false for 2.58 & below.

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -2,9 +2,11 @@ cask 'vitamin-r' do
   if MacOS.version <= :el_capitan
     version '2.58'
     sha256 'c6c631430b44359aa022d9ca5ca6e98dbdf7258f2ceae0353f344a035682661e'
+    auto_updates false
   else
     version '3.08'
     sha256 '0be990ba917fbbb09becc2eda05fe47ec7869ffe48b713a6bf57d11d18344d13'
+    auto_updates true
   end
 
   url "http://www.publicspace.net/download/signedVitamin#{version.major}.zip"
