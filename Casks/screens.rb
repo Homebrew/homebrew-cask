c20d22d6d58bff8110cdf30c62ec1e5747b8dc15
Randall <ran.dall@icloud.com>
2019-05-15 06:53:25 -0700

    Update screens from 4.6.9 to 4.6.10

--- Casks/screens.rb
+++ Casks/screens.rb
@@ -1,6 +1,6 @@
 cask 'screens' do
-  version '4.6.9'
-  sha256 'f27be998592bb290bd6715603d44aeaaac0a451e422098c08cc59e61a613f138'
+  version '4.6.10'
+  sha256 'e352d4937b13217423e7c1f8b16d93743be40bb6f246d4fc64a093b2e0d4111f'
 
   # dl.devmate.com/com.edovia.screens4.mac was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.edovia.screens4.mac/Screens4.dmg'
