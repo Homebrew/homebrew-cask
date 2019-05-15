f6f933c3b760c4040781d2f8532dfd67ed91ee66
garyfleming <gary.fleming@gmail.com>
2019-05-15 09:12:24 +0100

    Update deckset from 2.0.8,2503 to 2.0.9,2513 (#63300)

--- Casks/deckset.rb
+++ Casks/deckset.rb
@@ -1,6 +1,6 @@
 cask 'deckset' do
-  version '2.0.8,2503'
-  sha256 'eb74188d7e8810085f44eb3532ec9aad2fa35f58bc73a735cc4314e785084db7'
+  version '2.0.9,2513'
+  sha256 '62bc5cebeddd0601c5e7e4b5a1646355549edf4621703e9d7da69cc11a67e7b1'
 
   url "https://dl.decksetapp.com/Deckset+#{version.before_comma}+(#{version.after_comma}).dmg"
   appcast 'https://dl.decksetapp.com/appcast.xml'
