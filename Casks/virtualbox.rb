c774b25eee695c3032b6928e646f2118eab8eabf
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2019-05-15 18:37:37 +0200

    Update virtualbox from 6.0.6,130049 to 6.0.8,130520 (#63302)

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,6 +1,6 @@
 cask 'virtualbox' do
-  version '6.0.6,130049'
-  sha256 'e7fea3a51d7be7e00b7d0115e33eb62dc5e4cbd10542fd7b0b83de904666b6bf'
+  version '6.0.8,130520'
+  sha256 '8e3ed48842821adba3cb48c37a2ffea8e1446ea7a99446df6a42c681720a90dd'
 
   url "https://download.virtualbox.org/virtualbox/#{version.before_comma}/VirtualBox-#{version.before_comma}-#{version.after_comma}-OSX.dmg"
   appcast 'https://download.virtualbox.org/virtualbox/LATEST.TXT'
