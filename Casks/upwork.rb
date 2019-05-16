00b0d4c5cd8bcdc6cb895ad130bc797cd5f8650a
Yurii Kolesnykov <root@yurikoles.com>
2019-05-16 19:20:52 +0300

    Update upwork from 5_1_0_647_g0nxmj7uab8q069t to 5_2_3_717_7tq7l0sg49p0kixc (#63380)
    * Update upwork from 5_1_0_647_g0nxmj7uab8q069t to 5_2_3_717_7tq7l0sg49p0kixc
    
    * Update upwork.rb
    
    * Update upwork.rb

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -1,6 +1,6 @@
 cask 'upwork' do
-  version '5_1_0_647_g0nxmj7uab8q069t'
-  sha256 '9119a78f4262c8fe4c6b9f50f768759ade8959495d32f81511c46a3289be998a'
+  version '5_2_3_717_7tq7l0sg49p0kixc'
+  sha256 'a4588d607abd3e8616af117b962047238dfdfb4e0d85d21764ab4c011c479af9'
 
   url "https://updates-desktopapp.upwork.com/binaries/v#{version}/Upwork.dmg"
   name 'Upwork'
