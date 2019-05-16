2bd6e93b967697adbaaa7b48caaea68fe7f56668
Randall <ran.dall@icloud.com>
2019-05-16 07:53:22 -0700

    Update principle from 5.7 to 5.8

--- Casks/principle.rb
+++ Casks/principle.rb
@@ -1,6 +1,6 @@
 cask 'principle' do
-  version '5.7'
-  sha256 'a92291608dc37dcb2dc005b85b1c9850cdbac82ef87ba7d118cd12a3d84a03c1'
+  version '5.8'
+  sha256 'db05b5282a1bab73cd58476f1a9738cadee533cd1a1c543ec3825280612eb9fa'
 
   url "https://principleformac.com/download/Principle_#{version.dots_to_underscores}.zip"
   appcast 'https://principleformac.com/update2.xml'
