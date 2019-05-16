b8602c0faf6760923a38ced407acdd2817346ba7
Julian Mayer <julian@corecode.io>
2019-05-16 23:01:27 +0200

    Update thedesk

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask 'thedesk' do
   version '18.4.0'
-  sha256 '3edf78fd5af68b06d5c1d1be2981b71e135007bd373d35c9198f530750c15ea1'
+  sha256 '2248c451c0666e39e9c150e76f49f388243c36201e2d66ab2ec578a63be9e3b5'
 
   # github.com/cutls/TheDesk was verified as official when first introduced to the cask
   url "https://github.com/cutls/TheDesk/releases/download/#{version}/TheDesk-#{version}.dmg"
