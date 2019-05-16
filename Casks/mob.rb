ebbfa4d67de52784ca9c1ee8eefa2324badedb18
Jason Zeng <zenghongtu@qq.com>
2019-05-17 00:38:17 +0800

    Update Mob to v0.2.0

--- Casks/mob.rb
+++ Casks/mob.rb
@@ -1,6 +1,6 @@
 cask 'mob' do
-  version '0.1.4'
-  sha256 '1eca1a36e2fbd71a002a9adb63c8204c87ce101cb531e4c048942c4cca1a81af'
+  version '0.2.0'
+  sha256 '59a685bb17fbcacfae4ea75afdd3a47ff123192e383d7cd0a7d97c186c57e0d0'
 
   url "https://github.com/zenghongtu/Mob/releases/download/v#{version}/Mob-#{version}-mac.dmg"
   appcast 'https://github.com/zenghongtu/Mob/releases.atom'
