1751186d358055758d936229af884fc14db22ad7
Jianing Wang <wjnmailg@gmail.com>
2019-05-16 12:06:43 +0800

    Update OpenInTerminal-lite from 0.4.2 to 0.4.3

--- Casks/openinterminal-lite.rb
+++ Casks/openinterminal-lite.rb
@@ -1,6 +1,6 @@
 cask 'openinterminal-lite' do
-  version '0.4.2'
-  sha256 '25d9b7116c5a9d4b46c7a6e49145b03e01026f03bc6babe8b6e57187e50fd231'
+  version '0.4.3'
+  sha256 'fe2d2e5892632b537118cd8bf46a0d84df137a52c2f795c9492e5d6c2a181661'
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInTerminal-Lite.app.zip"
   appcast 'https://github.com/Ji4n1ng/OpenInTerminal/releases.atom'
