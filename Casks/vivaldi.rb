a27b0601c7adf9c126a9949913f8938a91b44ea5
Anver Housseini <contact@anver.ch>
2019-05-16 16:09:56 +0200

    Update vivaldi from 2.5.1525.41 to 2.5.1525.43 (#63362)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,6 +1,6 @@
 cask 'vivaldi' do
-  version '2.5.1525.41'
-  sha256 '1682d324b6f902cc4a1114187c9c573bb0dd79b1dac74c0816fccae57a71a6fc'
+  version '2.5.1525.43'
+  sha256 '95f8e3fdacc5db9f8740cd1dfa7263ffc65509716e8c3418e9d1a9818345ef62'
 
   url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/public/mac/appcast.xml'
