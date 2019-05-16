7fa9d7267f570a0108893b493b6142fce9d78a52
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-16 08:57:06 -0700

    Update `version`

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,5 +1,5 @@
 cask 'yate' do
-  version '4.7.0'
+  version '4.7.0.1'
   sha256 'c4d6f2aae89801aaccbf0a684fe4edc5e95b302b6c4bfec2ee2d633314dde91a'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
