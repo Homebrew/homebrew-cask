7515c9e8cefdd87e23ec000c1917dcb1679be55c
Julian Mayer <julian@corecode.io>
2019-05-16 22:55:13 +0200

    Update softraid

--- Casks/softraid.rb
+++ Casks/softraid.rb
@@ -2,7 +2,8 @@ cask 'softraid' do
   version '5.7.5'
   sha256 '2d3914eadf524fc7e272ad50ffd3b742ded45b6c3757a452e8278a87d9ffdf1a'
 
-  url "https://softraid.com/updates/SoftRAID%20#{version}.dmg"
+  # download.owcdigital.com/softraid was verified as official when first introduced to the cask
+  url "https://download.owcdigital.com/softraid/mac/#{version.major}/softraid/SoftRAID_#{version}.dmg"
   appcast 'https://www.softraid.com/pages/support/download_latest_version.html'
   name 'SoftRAID'
   homepage 'https://www.softraid.com/'
