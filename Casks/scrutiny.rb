403409f685a7e7362665e9db5b3dbb4faced1cec
CoreCode <core-code@users.noreply.github.com>
2019-05-17 00:05:18 +0200

    Update scrutiny from 8.3.13 to 8.3.14 (#63418)

--- Casks/scrutiny.rb
+++ Casks/scrutiny.rb
@@ -1,6 +1,6 @@
 cask 'scrutiny' do
-  version '8.3.13'
-  sha256 'b5a08d4c766f06f2ff910148c74007ae4e83e03a23ccc3a016c276136e68f6a4'
+  version '8.3.14'
+  sha256 '1053ef1587ecdad98a12331936807a2fcb3bd34ae18fbf51d9aa1a43f147316d'
 
   url 'https://peacockmedia.software/mac/scrutiny/scrutiny.dmg'
   appcast 'https://peacockmedia.software/mac/scrutiny/version_history.html'
