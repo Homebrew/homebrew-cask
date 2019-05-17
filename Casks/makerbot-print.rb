d7a427f7bc01a0fa270e649b033e892ee86a9599
CoreCode <core-code@users.noreply.github.com>
2019-05-17 12:36:23 +0200

    Update makerbot-print (#63403)

--- Casks/makerbot-print.rb
+++ Casks/makerbot-print.rb
@@ -3,15 +3,26 @@ cask 'makerbot-print' do
   sha256 :no_check
 
   # s3.amazonaws.com/downloads-makerbot-com/installer/release was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/downloads-makerbot-com/installer/release/MakerBotDesktopInstaller.pkg'
+  url 'https://s3.amazonaws.com/downloads-makerbot-com/installer/release/MakerBotPrintInstaller.pkg'
   name 'MakerBot Print'
   homepage 'https://www.makerbot.com/print/'
 
-  pkg 'MakerBotDesktopInstaller.pkg'
+  pkg 'MakerBotPrintInstaller.pkg'
 
-  uninstall pkgutil: [
-                       'com.makerbot.MakerBot Print',
-                       'com.makerbot.MakerBotPrintInstallScripts',
-                       'com.makerbot.usb_daemon',
-                     ]
+  uninstall pkgutil:   [
+                         'com.makerbot.electron',
+                         'com.makerbot.MakerBot Print',
+                         'com.makerbot.MakerBotPrintInstallScripts',
+                         'com.makerbot.usb_daemon',
+                       ],
+            delete:    '/Applications/MakerBot Print.app',
+            quit:      'com.makerbot.electron',
+            launchctl: [
+                         'com.makerbot.fire_e',
+                         'com.makerbot.mini_4',
+                         'com.makerbot.mini_8',
+                         'com.makerbot.replicator_5',
+                         'com.makerbot.replicator_b',
+                         'com.makerbot.z18_6',
+                       ]
 end
