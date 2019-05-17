d63408b45c8e567d1eeca4a44ac344b8248c5f46
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-17 07:57:02 -0700

    Update gzdoom from 4.1.1 to 4.1.2 (#63447)

--- Casks/gzdoom.rb
+++ Casks/gzdoom.rb
@@ -1,6 +1,6 @@
 cask 'gzdoom' do
-  version '4.1.1'
-  sha256 'e53cabe3bf0051263d15d8a8d09af735e74d7c1d32d4cf8e00acaaef1de0aa67'
+  version '4.1.2'
+  sha256 'd0cd2fd4e83be135301229b2e748c7d168670d519aafb66f8fa6b6547a313392'
 
   url "https://zdoom.org/files/gzdoom/bin/gzdoom-bin-#{version.dots_to_hyphens}.dmg"
   appcast 'https://github.com/coelckers/gzdoom/releases.atom'
