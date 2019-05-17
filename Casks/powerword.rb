340b172f1f18ae09204f6ab2b66489ea1b3232fc
CoreCode <core-code@users.noreply.github.com>
2019-05-17 11:11:16 +0200

    Update powerword (#63405)

--- Casks/powerword.rb
+++ Casks/powerword.rb
@@ -2,7 +2,7 @@ cask 'powerword' do
   version '1.1.1,1.0.1'
   sha256 '87c5347065f37342939b9d9c1874a582f9a144d0a5d494152035cffcc4ca8a5b'
 
-  url "https://download.iciba.com/mac/mac#{version.after_comma}/PowerWord.dmg"
+  url "http://download.iciba.com/mac/mac#{version.after_comma}/PowerWord.dmg"
   name 'PowerWord'
   name '金山词霸'
   homepage 'http://cp.iciba.com/mac/'
