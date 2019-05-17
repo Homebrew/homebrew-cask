e65f7db98077c610800f021c65466f8e25f504e2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-17 07:48:00 -0700

    Update clashx from 1.9.10 to 1.9.11 (#63444)

--- Casks/clashx.rb
+++ Casks/clashx.rb
@@ -1,6 +1,6 @@
 cask 'clashx' do
-  version '1.9.10'
-  sha256 '6931e87a00754eb144d6ebbaf55c408b254981f3dcf3102dd63fde59a7a8a386'
+  version '1.9.11'
+  sha256 '56f798f97ca08dcbf1c62fb56cf1a076b3a21f225e50c3d3c3da8fcd527d712b'
 
   url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
   appcast 'https://github.com/yichengchen/clashX/releases.atom'
