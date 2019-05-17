b8562ded02951eab77c877d5a68caf2ff23b77e1
Vítor Galvão <info@vitorgalvao.com>
2019-05-17 13:32:51 +0100

    Update geburtstagschecker.rb

--- Casks/geburtstagschecker.rb
+++ Casks/geburtstagschecker.rb
@@ -1,9 +1,8 @@
 cask 'geburtstagschecker' do
-  version '1.8,177'
+  version '1.8.177'
   sha256 '9f86d7a04fc8de127a9f2ec5c3ca7eb4742429f007f8041464540b9c92c726af'
 
-  # https://earthlingsoft.net/GeburtstagsChecker/ was verified as official when first introduced to the cask
-  url "https://earthlingsoft.net/GeburtstagsChecker/GeburtstagsChecker%20#{version.before_comma}%20(#{version.after_comma}).zip"
+  url "https://earthlingsoft.net/GeburtstagsChecker/GeburtstagsChecker%20#{version.major_minor}%20(#{version.patch}).zip"
   appcast 'https://earthlingsoft.net/GeburtstagsChecker/appcast.xml'
   name 'GeburtstagsChecker'
   homepage 'https://earthlingsoft.net/GeburtstagsChecker/'
