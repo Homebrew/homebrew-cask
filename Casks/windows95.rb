e84f6cf58fdc71b99f183e346b207a2cc7c7b12e
Randall <ran.dall@icloud.com>
2019-05-17 07:47:41 -0700

    Update windows95 from 2.0.0 to 2.1.0

--- Casks/windows95.rb
+++ Casks/windows95.rb
@@ -1,7 +1,7 @@
 cask 'windows95' do
   # note: "95" is not a version number, but an intrinsic part of the product name
-  version '2.0.0'
-  sha256 '9f2ce72b7ec7a79cb3521bd75722b39f99a4234784dc232667ae682e6104637b'
+  version '2.1.0'
+  sha256 'e177a52041ff7b1952ad230c5fc71a94935d65acd8bf8558bf5b7f0cff82f583'
 
   url "https://github.com/felixrieseberg/windows95/releases/download/v#{version}/windows95-macos-#{version}.zip"
   appcast 'https://github.com/felixrieseberg/windows95/releases.atom'
