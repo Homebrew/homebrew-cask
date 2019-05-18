8b5fdb93945c74b655b7bbe3c5e10a732e0004e9
CoreCode <core-code@users.noreply.github.com>
2019-05-18 17:17:09 +0200

    Update mkvtoolnix from 33.1.0 to 34.0.0 (#63471)

--- Casks/mkvtoolnix.rb
+++ Casks/mkvtoolnix.rb
@@ -1,6 +1,6 @@
 cask 'mkvtoolnix' do
-  version '33.1.0'
-  sha256 '181b8a126dff77a9e01ba248dc746bc67552d38c1701c888f500b27d4c7f0887'
+  version '34.0.0'
+  sha256 '8528a6d56a51919a55ef30e505c717c43579c2fa766efd75940b8dd8169ae0d8'
 
   url "https://mkvtoolnix.download/macos/MKVToolNix-#{version}.dmg"
   appcast 'https://www.bunkus.org/blog/feed/'
