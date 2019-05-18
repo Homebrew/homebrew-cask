6c763664ecb394eb87424b16c0411655989c9ea8
Randall <ran.dall@icloud.com>
2019-05-18 08:41:38 -0700

    Update vectoraster from 7.4.0 to 7.4.1

--- Casks/vectoraster.rb
+++ Casks/vectoraster.rb
@@ -1,6 +1,6 @@
 cask 'vectoraster' do
-  version '7.4.0'
-  sha256 '8365d3bb056bc493783acf03090bd35e9b1a5879f13c8992820b725bb4bc5488'
+  version '7.4.1'
+  sha256 '70251678707654f252c9b0e2d070300cd206b20153c7c059209fb6065bf50b15'
 
   url "https://www.lostminds.com/downloads/vectoraster#{version.major}.dmg"
   appcast "https://www.lostminds.com/vectoraster#{version.major}/version_history.php"
