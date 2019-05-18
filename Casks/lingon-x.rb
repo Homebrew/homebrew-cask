160fe936f0bb187dbf89fcffe3a6e1724cc4e0a1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-18 09:02:38 -0700

    Update lingon-x from 7.0.1 to 7.0.2 (#63488)
    * Update lingon-x
    
    * Update sha256

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -3,8 +3,8 @@ cask 'lingon-x' do
     version '6.6.4'
     sha256 'fc788402fa16df39a3d48cdc501dae31368ec6fd69ffe0026ba99932b28cae19'
   else
-    version '7.0.1'
-    sha256 '57781523695e7a05d29c9665965d1f1091c151fecc88dff9e6c05c95d3763fb3'
+    version '7.0.2'
+    sha256 'eaa2e1a45626d1a3098926152cf675e91348c174585406cf7d230487e09d9100'
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
