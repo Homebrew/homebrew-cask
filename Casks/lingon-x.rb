e80e2a2d196a1da819de0c130df6d2c47a327c6d
Randall <ran.dall@icloud.com>
2019-05-18 08:52:05 -0700

    Update lingon-x

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,10 +1,10 @@
 cask 'lingon-x' do
   if MacOS.version <= :high_sierra
     version '6.6.4'
-    sha256 'fc788402fa16df39a3d48cdc501dae31368ec6fd69ffe0026ba99932b28cae19'
+    sha256 :no_check
   else
-    version '7.0.1'
-    sha256 '57781523695e7a05d29c9665965d1f1091c151fecc88dff9e6c05c95d3763fb3'
+    version '7.0.2'
+    sha256 'fc788402fa16df39a3d48cdc501dae31368ec6fd69ffe0026ba99932b28cae19'
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
