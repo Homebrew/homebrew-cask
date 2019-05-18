58879fa76ee7ecde115374f71f5c737ed4561991
Metri Dmitriev <metri@users.noreply.github.com>
2019-05-18 13:03:14 +0300

    Update seahore version
    Minore update.
    Version 2.4.3 unavailable

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,6 +1,6 @@
 cask 'seashore' do
-  version '2.4.3'
-  sha256 '38b317c33bb4de88c54c5bee64c4b446aca9026e3c9314e19339cfd26b1e40fa'
+  version '2.4.4'
+  sha256 '7513df22fb037ab3374fc71640f5a300590ecbedc9ee35f1c47025ad86aa411d'
 
   url "https://github.com/robaho/seashore/releases/download/v#{version}/seashore-bin-#{version}.dmg"
   appcast 'https://github.com/robaho/seashore/releases.atom'
