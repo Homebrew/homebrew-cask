7c9ee781eca54bc643cc705891a01728244dda48
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-19 05:17:04 -0700

    Update advancedrestclient from 13.0.6 to 13.0.7 (#63472)

--- Casks/advancedrestclient.rb
+++ Casks/advancedrestclient.rb
@@ -1,6 +1,6 @@
 cask 'advancedrestclient' do
-  version '13.0.6'
-  sha256 'f5c851323730f0b9301e4e9087ea85074ae308dfebbfcbe70b7c1aba8ebd8364'
+  version '13.0.7'
+  sha256 '1abde754de131b3d2929036950bc363ceaa737abf140259a0938a026d5406c23'
 
   # github.com/advanced-rest-client/arc-electron was verified as official when first introduced to the cask
   url "https://github.com/advanced-rest-client/arc-electron/releases/download/v#{version}/arc-#{version}.dmg"
