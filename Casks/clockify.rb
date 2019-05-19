398dbda41b619aad0dbee8f089cd693cca975d7e
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2019-05-19 09:25:37 -0500

    Update clockify from 2.2.5.69 to 2.2.6.73 (#63499)

--- Casks/clockify.rb
+++ Casks/clockify.rb
@@ -1,6 +1,6 @@
 cask 'clockify' do
-  version '2.2.5.69'
-  sha256 'dc35b3a92338ae794825c1018f836c653e88a1613738592fcd09a9260ddd1a0a'
+  version '2.2.6.73'
+  sha256 'd83bcfb29c79418eb76150d7c01a54f0321a4bc25269fbc92d88ac32a3c82531'
 
   url "https://clockify.me/downloads/ClockifyDesktop_#{version.no_dots}.zip"
   name 'Clockify'
