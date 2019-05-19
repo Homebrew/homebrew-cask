70ae85ffa89ab25dfc53e014e5d874abc3f1f48a
Tames McTigue <tames@tamerz.com>
2019-05-19 14:11:08 +0300

    Update axe-edit-iii from 1.01.06 to 1.01.07 (#63433)
    * Update axe-edit-iii from 1.01.06 to 1.01.07
    
    * They switched from a .pkg installer to a .app in this version

--- Casks/axe-edit-iii.rb
+++ Casks/axe-edit-iii.rb
@@ -1,18 +1,13 @@
 cask 'axe-edit-iii' do
-  version '1.01.06'
-  sha256 '5b025c9407bebe6de4c7d5dd1e432daea605e86ccb7d4985862fb648f785bf2f'
+  version '1.01.07'
+  sha256 '28bceb1b64e4c0ec68dc5ef26cf1c33b496c4155503114cf012dc9b261dc875e'
 
   url "https://www.fractalaudio.com/downloads/Axe-Edit-III/Axe-Edit-III-OSX-v#{version.tr('.', 'p')}.dmg"
   appcast 'https://www.fractalaudio.com/axe-fx-iii-edit/'
   name 'Axe-Edit III'
   homepage 'https://www.fractalaudio.com/axe-fx-iii-edit/'
 
-  pkg "Axe-Edit-III-OSX-v#{version.tr('.', 'p')}.pkg"
-
-  uninstall pkgutil: [
-                       'com.FractalAudio.AIIIEdit.pkg',
-                       'com.FractalAudio.AIIIEdit.ReleaseNotes.pkg',
-                     ]
+  app 'Axe-Edit III.app'
 
   zap trash: '~/Library/Application Support/Fractal Audio/Axe-Edit III'
 end
