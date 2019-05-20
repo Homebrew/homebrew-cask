5697c70bcbdec03ad1a04a04056578c7f6f5ba4d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-20 07:40:59 -0700

    Update ledger-live from 1.8.0 to 1.8.1 (#63546)

--- Casks/ledger-live.rb
+++ Casks/ledger-live.rb
@@ -1,6 +1,6 @@
 cask 'ledger-live' do
-  version '1.8.0'
-  sha256 '2db6a1cef794fe5e8d984b4ee8587e014437721f5b40fedcb54bed2240090d8c'
+  version '1.8.1'
+  sha256 '3da6a6d9dee0ccbb199aba5a8bb168721d0b7c131b240ce87050958ec3812ae3'
 
   # github.com/LedgerHQ/ledger-live-desktop was verified as official when first introduced to the cask
   url "https://github.com/LedgerHQ/ledger-live-desktop/releases/download/v#{version}/ledger-live-desktop-#{version}-mac.dmg"
