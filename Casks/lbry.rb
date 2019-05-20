f8ac16996876de22bf320e13ff658aa5f1b850e7
Julian Mayer <julian@corecode.io>
2019-05-21 00:10:47 +0200

    Update lbry from 0.32.1 to 0.32.2

--- Casks/lbry.rb
+++ Casks/lbry.rb
@@ -1,10 +1,10 @@
 cask 'lbry' do
-  version '0.32.1'
-  sha256 '02c304e62a2687212e69e0fbfcb44ca541e30428a016864bdd857f7ed017e935'
+  version '0.32.2'
+  sha256 'daa80d2e57c06260c069197360b5ff18f4338d78af9d00f06e9018d52ad15c80'
 
-  # github.com/lbryio/lbry-app was verified as official when first introduced to the cask
-  url "https://github.com/lbryio/lbry-app/releases/download/v#{version}/LBRY_#{version}.dmg"
-  appcast 'https://github.com/lbryio/lbry-app/releases.atom'
+  # github.com/lbryio/lbry-desktop was verified as official when first introduced to the cask
+  url "https://github.com/lbryio/lbry-desktop/releases/download/v#{version}/LBRY_#{version}.dmg"
+  appcast 'https://github.com/lbryio/lbry-desktop/releases.atom'
   name 'LBRY'
   homepage 'https://lbry.io/'
 
