63611d04e7c7783f468d2fdab831ffe06049817d
Vítor Galvão <info@vitorgalvao.com>
2019-05-20 13:35:14 +0100

    Update amadine.rb

--- Casks/amadine.rb
+++ Casks/amadine.rb
@@ -1,9 +1,10 @@
 cask 'amadine' do
-  version :latest
-  sha256 :no_check
+  version '1.0.1'
+  sha256 '869510a25a3a710de4904f0a5035767d56b8725aa430152a7f785a3756d7aff2'
 
   # s3.amazonaws.com/belightsoft was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/belightsoft/Amadine.dmg'
+  appcast 'https://www.belightsoft.com/download/updates/appcast_Amadine.xml'
   name 'Amadine'
   homepage 'https://amadine.com/'
 
