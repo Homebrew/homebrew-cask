391234b120ef1b43f051b01f67c441dcdb30a82b
Julian Mayer <julian@corecode.io>
2019-05-21 01:04:49 +0200

    Update bisq

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -2,9 +2,9 @@ cask 'bisq' do
   version '1.1.1'
   sha256 '1d664747db39926ef630a051ef732455b2056b3e2dee232debc503b5740975ed'
 
-  # github.com/bisq-network/bisq-desktop was verified as official when first introduced to the cask
-  url "https://github.com/bisq-network/bisq-desktop/releases/download/v#{version}/Bisq-#{version}.dmg"
-  appcast 'https://github.com/bisq-network/bisq-desktop/releases.atom'
+  # github.com/bisq-network/bisq was verified as official when first introduced to the cask
+  url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg"
+  appcast 'https://github.com/bisq-network/bisq/releases.atom'
   name 'Bisq'
   homepage 'https://bisq.network/'
 
