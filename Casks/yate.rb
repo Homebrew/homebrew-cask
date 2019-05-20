c249175da838e3971c25e463666d2f8e42410e43
Randall <ran.dall@icloud.com>
2019-05-20 07:29:14 -0700

    Update yate from 4.7.0.1 to 4.7.0.2

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,6 +1,6 @@
 cask 'yate' do
-  version '4.7.0.1'
-  sha256 'c4d6f2aae89801aaccbf0a684fe4edc5e95b302b6c4bfec2ee2d633314dde91a'
+  version '4.7.0.2'
+  sha256 'ab7e072a2aaca42ea731a64aefe35382e754c3e4d7d6465ccd53f829266673a5'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml'
