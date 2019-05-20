482132109ab49706365ab09611e48f619c851a81
Ahmed Rezk <rezk84@gmail.com>
2019-05-20 13:19:52 +0300

    Update to the latest stable version 1.18.0

--- Casks/mongodb-compass.rb
+++ Casks/mongodb-compass.rb
@@ -1,5 +1,5 @@
 cask 'mongodb-compass' do
-  version '1.17.0'
+  version '1.18.0'
   sha256 '5bb0f440a17fc45616926a5f00d0bcc8d63db8e031f1f125e4c41b6900e4fd55'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-#{version}-darwin-x64.dmg"
