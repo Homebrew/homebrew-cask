a4a15b9fb683836dd43aa7ae825f909df9b41b2e
b-bech <45978702+b-bech@users.noreply.github.com>
2019-05-21 15:04:08 -0400

    Add Bunch.app v1.0.7 (#63623)
    * Add Bunch.app v1.0.7
    
    This is to add the app Bunch to the cask available.
    
    * Update bunch.rb
    
    * Update bunch.rb
    
    * Update bunch.rb

---
+++ Casks/bunch.rb
@@ -0,0 +1,13 @@
+cask 'bunch' do
+  version '1.0.722'
+  sha256 'e3a06d0373205e6fefe2d4eae63feff1f02676ddb928f4b53804e5c1bba26a7d'
+
+  url "https://cdn3.brettterpstra.com/updates/bunch/Bunch#{version}.dmg"
+  appcast 'https://brettterpstra.com/updates/bunch/appcast.xml'
+  name 'Bunch'
+  homepage 'https://brettterpstra.com/projects/bunch/'
+
+  app 'Bunch.app'
+
+  zap trash: '~/Library/Preferences/com.brettterpstra.Bunch.plist'
+end
