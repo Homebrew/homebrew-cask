77108252bbb799fab89c50be05b2b0bc43205f45
suschizu <brigitte@corecode.io>
2019-05-21 19:46:52 +0200

    Update prudent.rb

--- Casks/prudent.rb
+++ Casks/prudent.rb
@@ -1,9 +1,9 @@
 cask 'prudent' do
-  version 'initial'
+  version '55.0.2883.87,initial'
   sha256 '128c50e2c4e00c71e35212f23b74b93d6256d219d05ffed55079369310950d8f'
 
   # github.com/PrudentMe/main was verified as official when first introduced to the cask
-  url "https://github.com/PrudentMe/main/releases/download/#{version}/Prudent.zip"
+  url "https://github.com/PrudentMe/main/releases/download/#{version.after_comma}/Prudent.zip"
   appcast 'https://github.com/PrudentMe/main/releases.atom'
   name 'Prudent'
   homepage 'https://prudent.me/'
