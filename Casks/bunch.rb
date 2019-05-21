9c8cb6fb3806857d9fcf033ed8eab9ed176abf5b
Vítor Galvão <info@vitorgalvao.com>
2019-05-21 18:04:53 +0100

    Update bunch.rb

--- Casks/bunch.rb
+++ Casks/bunch.rb
@@ -2,7 +2,6 @@ cask 'bunch' do
   version '1.0.722'
   sha256 'e3a06d0373205e6fefe2d4eae63feff1f02676ddb928f4b53804e5c1bba26a7d'
 
-  # https://brettterpstra.com/projects/bunch/ was verified as official when first introduced to the cask
   url "https://cdn3.brettterpstra.com/updates/bunch/Bunch#{version}.dmg"
   name 'Bunch'
   homepage 'https://brettterpstra.com/projects/bunch/'
