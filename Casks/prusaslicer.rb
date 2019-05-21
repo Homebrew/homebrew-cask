61cf288b2ef8c64ecd66571708e9f8825d25b90f
suschizu <brigitte@corecode.io>
2019-05-21 15:00:11 +0200

    Update prusaslicer from 2.0.0-rc2,201905190649 to 2.0.0,201905201652 (#63593)

--- Casks/prusaslicer.rb
+++ Casks/prusaslicer.rb
@@ -1,6 +1,6 @@
 cask 'prusaslicer' do
-  version '2.0.0-rc2,201905190649'
-  sha256 '9b6d8d42c6ea63a19682b0aa4cfd214d1fce9be2a5b093d0c94707e3a5f52987'
+  version '2.0.0,201905201652'
+  sha256 '2882e0e58be97c50570f9aca931f2ea6eff206c748ce94bb42ff341dd4938c4b'
 
   # github.com/prusa3d/PrusaSlicer was verified as official when first introduced to the cask.
   url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.before_comma}/PrusaSlicer-#{version.before_comma}+-#{version.after_comma}.dmg"
