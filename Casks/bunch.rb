2cad9da88003cea35dba99ebdad3af3e495484f1
Vítor Galvão <info@vitorgalvao.com>
2019-05-21 18:15:02 +0100

    Update bunch.rb

--- Casks/bunch.rb
+++ Casks/bunch.rb
@@ -3,6 +3,7 @@ cask 'bunch' do
   sha256 'e3a06d0373205e6fefe2d4eae63feff1f02676ddb928f4b53804e5c1bba26a7d'
 
   url "https://cdn3.brettterpstra.com/updates/bunch/Bunch#{version}.dmg"
+  appcast 'https://brettterpstra.com/updates/bunch/appcast.xml'
   name 'Bunch'
   homepage 'https://brettterpstra.com/projects/bunch/'
 
