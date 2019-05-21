c6f25978eaeef0362a2373ee394f4a318e8a4a0c
George Lee <1092908+grlee@users.noreply.github.com>
2019-05-21 12:52:05 -0400

    Add Kotlin Native v1.3.31 - code review fixes

--- Casks/kotlin-native.rb
+++ Casks/kotlin-native.rb
@@ -1,13 +1,8 @@
-require 'fileutils'
-require 'open3'
-require 'tmpdir'
-tmpdir = Dir.mktmpdir
-
 cask 'kotlin-native' do
   version '1.3.31'
   sha256 '964e54649f93d3f269f3753cb7b97739d03c26fe2237a144dcecb92779c36d77'
 
-  # github.com/JetBrains/kotlin/releases/download/v was verified as official when first introduced to the cask
+  # github.com/JetBrains/kotlin was verified as official when first introduced to the cask
   url "https://github.com/JetBrains/kotlin/releases/download/v#{version}/kotlin-native-macos-#{version}.tar.gz"
   appcast 'https://github.com/JetBrains/kotlin-native/releases.atom'
   name 'Kotlin Native'
@@ -24,21 +19,7 @@ cask 'kotlin-native' do
   binary "kotlin-native-macos-#{version}/bin/kotlinc-native"
   binary "kotlin-native-macos-#{version}/bin/run_konan"
 
-  postflight do
-    puts 'Testing kotlinc-native...'
-    File.write "#{tmpdir}/test.kt", <<~EOS
-      fun main(args: Array<String>) {
-        println("Hello World!")
-      }
-    EOS
-    system 'kotlinc-native', "#{tmpdir}/test.kt", '-o', "#{tmpdir}/test.kexe"
+  caveats do
+    depends_on_java '6+'
   end
-
-  uninstall delete: "#{staged_path}/#{token}"
-
-  caveats depends_on_java '6+'
-  caveats <<~EOS
-    See https://github.com/JetBrains/kotlin-native/blob/master/README.md
-    for Prerequisites using Kotlin Native.
-  EOS
 end
