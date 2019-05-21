0db588723b53552ea1b4271750cf7a02fcc3bd40
gotow <gotow@stclairsoft.com>
2019-05-21 17:14:54 -0600

    Update app-tamer.rb (#63642)
    Updated to version 2.4.5

--- Casks/app-tamer.rb
+++ Casks/app-tamer.rb
@@ -1,6 +1,6 @@
 cask 'app-tamer' do
-  version '2.4.4'
-  sha256 'd92737991c06e746dfe0ddc0cfc5f81b14ffeb91b5fc3fdca03c49de96aec639'
+  version '2.4.5'
+  sha256 '598c351e22c26e32166b0a4f7c1274a094aff23a1df54bf630d9d884f8fcfb26'
 
   url "https://www.stclairsoft.com/download/AppTamer-#{version}.dmg"
   appcast 'https://www.stclairsoft.com/cgi-bin/sparkle.cgi?AT'
