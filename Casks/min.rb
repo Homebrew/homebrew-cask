1fb0ab7b6b106f02ae0305cca7b558e5a0d7f1c3
Randall <ran.dall@icloud.com>
2019-05-21 08:27:11 -0700

    Update min from 1.9.2 to 1.10.1

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,6 +1,6 @@
 cask 'min' do
-  version '1.9.2'
-  sha256 '609e55bdf0f175462f03d8524ec7e95fad2511efd22b3d6a2ccee0f82f378137'
+  version '1.10.1'
+  sha256 'cf6d074437f71417ff47ce838aa04c0c7ee46417cb76647af4ee5faeca9de4c1'
 
   # github.com/minbrowser/min was verified as official when first introduced to the cask
   url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip"
