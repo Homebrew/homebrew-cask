1fa703dc7fa13a57379a7a333c11f225e4eff398
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-22 09:39:14 -0700

    Update gray from 0.11.0 to 0.12.0 (#63673)

--- Casks/gray.rb
+++ Casks/gray.rb
@@ -1,6 +1,6 @@
 cask 'gray' do
-  version '0.11.0'
-  sha256 'f48c2962d19f6b4a2285e9757ae3ec1c506ff6dc796db79ca07a6a00bd88754a'
+  version '0.12.0'
+  sha256 'bfe5900bf00ec8173dbf921d2077b2abe8a5dffb3ff7ff21ab8321688260db67'
 
   url "https://github.com/zenangst/Gray/releases/download/#{version}/Gray.zip"
   appcast 'https://github.com/zenangst/Gray/releases.atom'
