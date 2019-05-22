91a713abe2231f01cdd0c39720f1002d35546999
brigitte zmoelnig <brigitte@corecode.io>
2019-05-22 22:41:55 +0200

    Update tableau-prep from 2019.1.4 to 2019.2.1

--- Casks/tableau-prep.rb
+++ Casks/tableau-prep.rb
@@ -1,6 +1,6 @@
 cask 'tableau-prep' do
-  version '2019.1.4'
-  sha256 'a272ced24e1a39b1dc49daa08959cd3087512b92d7573d91db6d2cc335ddd2b1'
+  version '2019.2.1'
+  sha256 '54ea5395bca454fff9d48500633aa866e6fa80828896a477baa4148051c5ba74'
 
   url "https://downloads.tableau.com/esdalt/tableau_prep/#{version}/TableauPrep-#{version.dots_to_hyphens}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/prep/mac'
