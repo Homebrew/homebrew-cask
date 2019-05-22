9b8d9bfc405b1f4e3cd9c409e34a62b1007efd22
Vítor Galvão <info@vitorgalvao.com>
2019-05-22 17:41:59 +0100

    Update electron-cash.rb

--- Casks/electron-cash.rb
+++ Casks/electron-cash.rb
@@ -3,7 +3,7 @@ cask 'electron-cash' do
   sha256 '3c3e43a397c06c73fb00fcc924f3c64ed030e3a8342ef2fda013dbc4006212d5'
 
   url "https://electroncash.org/downloads/#{version}/mac/Electron-Cash-#{version}-macosx.dmg"
-  appcast 'https://github.com/fyookball/electrum/releases.atom'
+  appcast 'https://github.com/Electron-Cash/Electron-Cash/releases.atom'
   name 'Electron Cash'
   homepage 'https://www.electroncash.org/'
 
