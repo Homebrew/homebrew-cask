ebf23aac3bb0207839b4a5adf5401b235636eb08
Randall <ran.dall@icloud.com>
2019-05-22 08:42:05 -0700

    Update compositor from 1.9.2 to 1.10.0

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,6 +1,6 @@
 cask 'compositor' do
-  version '1.9.2'
-  sha256 'd83950a1ecaede380db3bf9db1ed65db306457eae5f3f624a16428f8b4bbd1a1'
+  version '1.10.0'
+  sha256 '26ab3cb3aae18fa9fa20c98cee681d7c688db14b9e44058f4887d3a8c9c268bf'
 
   url "https://compositorapp.com/updates/Compositor_#{version}.zip"
   appcast 'https://compositorapp.com/updates/appcast.xml'
