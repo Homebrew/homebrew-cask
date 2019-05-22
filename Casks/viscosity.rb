4f2c1806f7a2fdcda1d78ff72a19a557bc245e49
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-22 09:30:17 -0700

    Update viscosity from 1.7.14 to 1.7.15 (#63689)

--- Casks/viscosity.rb
+++ Casks/viscosity.rb
@@ -1,6 +1,6 @@
 cask 'viscosity' do
-  version '1.7.14'
-  sha256 '71e0d6e63b86122fc771095cb0c4a6c99758c8a71f48f31651a03e64bc7705c5'
+  version '1.7.15'
+  sha256 '24d29a1375fe24e4cfcd44575d1475612445971601f7f6f309b899c6ff4bc3a4'
 
   url "https://swupdate.sparklabs.com/download/mac/release/viscosity/Viscosity%20#{version}.dmg"
   appcast 'https://swupdate.sparklabs.com/appcast/mac/release/viscosity/'
