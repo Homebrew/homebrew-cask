130251bfee5ca0c18e1383a858ee7c73b026c6a1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-22 09:45:19 -0700

    Update patchwork from 3.11.6 to 3.12.0 (#63674)

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,8 +1,8 @@
 cask 'patchwork' do
-  version '3.11.6'
-  sha256 '6c085e4ba2146e7e379b3bedfb92c6ce81192ab9866ec0235d2efdfe9a724aeb'
+  version '3.12.0'
+  sha256 '3473a97212cf3fd35cabead07cb6f66850cad176386feb07db435160922f6114'
 
-  url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}-mac.dmg"
+  url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom'
   name 'Patchwork'
   homepage 'https://github.com/ssbc/patchwork'
