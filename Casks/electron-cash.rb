4b99f12a9a34bd94c9033ace9586ab434a0913be
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-22 10:34:07 -0700

    Update electron-cash from 4.0.2 to 4.0.3 (#63670)
    * Update electron-cash from 4.0.2 to 4.0.3
    
    * Update electron-cash.rb

--- Casks/electron-cash.rb
+++ Casks/electron-cash.rb
@@ -1,9 +1,9 @@
 cask 'electron-cash' do
-  version '4.0.2'
-  sha256 'edce2676a1c57814f99c95dd6a31d2b8778b6129d64559d61103f09daf0f3579'
+  version '4.0.3'
+  sha256 '3c3e43a397c06c73fb00fcc924f3c64ed030e3a8342ef2fda013dbc4006212d5'
 
   url "https://electroncash.org/downloads/#{version}/mac/Electron-Cash-#{version}-macosx.dmg"
-  appcast 'https://github.com/fyookball/electrum/releases.atom'
+  appcast 'https://github.com/Electron-Cash/Electron-Cash/releases.atom'
   name 'Electron Cash'
   homepage 'https://www.electroncash.org/'
 
