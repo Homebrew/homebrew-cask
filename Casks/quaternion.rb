5ade6e62ea12fdc452de36b936309cbec16de296
CoreCode <core-code@users.noreply.github.com>
2019-05-22 17:58:27 +0200

    Update quaternion.rb

--- Casks/quaternion.rb
+++ Casks/quaternion.rb
@@ -2,10 +2,10 @@ cask 'quaternion' do
   version '0.0.9.4b'
   sha256 'f070c28a0b248b5dff5540de93d47bed75cab8a41d9936eb043d9a28b9634ea2'
 
-  url "https://github.com/QMatrixClient/Quaternion/releases/download/#{version}/quaternion-#{version}.dmg"
-  appcast 'https://github.com/QMatrixClient/Quaternion/releases.atom'
+  url "https://github.com/quotient-im/Quaternion/releases/download/#{version}/quaternion-#{version}.dmg"
+  appcast 'https://github.com/quotient-im/Quaternion/releases.atom'
   name 'Quaternion'
-  homepage 'https://github.com/QMatrixClient/Quaternion'
+  homepage 'https://github.com/quotient-im/Quaternion'
 
   depends_on macos: '>= :high_sierra'
 
