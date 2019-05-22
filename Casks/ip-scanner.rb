d2de1b9a214879165426b6b0a3ac8487e3802306
suschizu <brigitte@corecode.io>
2019-05-22 16:12:56 +0200

    Update ip-scanner from 3.70 to 3.80 (#63662)

--- Casks/ip-scanner.rb
+++ Casks/ip-scanner.rb
@@ -1,6 +1,6 @@
 cask 'ip-scanner' do
-  version '3.70'
-  sha256 'face2f2520041fefafa31ac551fbc449276706983de710071fd50087e957d50b'
+  version '3.80'
+  sha256 '0e9f5e2f4436952708a988d2df4618375374b01146e2c3c55883d88f68e5169c'
 
   url "https://10base-t.com/downloads/IPScanner_#{version}.zip"
   appcast 'https://updates.10base-t.com/appcasts/ipscanner_appcast_sandbox.xml'
