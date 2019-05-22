6ae5742ceccd9f29710650c4f31d710b1988aec1
suschizu <brigitte@corecode.io>
2019-05-23 00:02:48 +0200

    Update tencent-lemon from 1.1.3 to 1.2.0 (#63700)

--- Casks/tencent-lemon.rb
+++ Casks/tencent-lemon.rb
@@ -1,6 +1,6 @@
 cask 'tencent-lemon' do
-  version '1.1.3'
-  sha256 'b2aa08a46f4541bcea6dbc444ed5a3e1d6a80a5dcccb23ea45314ef5e95dcfc1'
+  version '1.2.0'
+  sha256 '3a67fed15bb801527de4a77d8d4a21c9d8e29624e699c0a612296600a86410f0'
 
   # pm.myapp.com/invc/xfspeed/qqpcmgr was verified as official when first introduced to the cask
   url "https://pm.myapp.com/invc/xfspeed/qqpcmgr/module_update/Lemon_#{version}.dmg"
