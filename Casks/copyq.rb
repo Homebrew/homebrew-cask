2c4866a2f984bdd5cfcb419703d92ba5c212d29f
Jeff Robbins <jwrobb@users.noreply.github.com>
2019-05-22 12:59:00 -0700

    Update copyq from 3.7.2 to 3.8.0 (#63695)

--- Casks/copyq.rb
+++ Casks/copyq.rb
@@ -1,6 +1,6 @@
 cask 'copyq' do
-  version '3.7.2'
-  sha256 '7285b597062c9f93e854c1a6c32299cdcb64146c36fda3068747efca7eda2a5f'
+  version '3.8.0'
+  sha256 'fabec84ecc6f5bde2e2c9939cee64e3f54e1e45cd79e975a2fda0b35531b59c1'
 
   # github.com/hluk/CopyQ was verified as official when first introduced to the cask
   url "https://github.com/hluk/CopyQ/releases/download/v#{version}/CopyQ.dmg"
