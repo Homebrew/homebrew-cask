698211f7c0a61efdd0ec605edd4106472c592478
Vítor Galvão <info@vitorgalvao.com>
2019-05-23 21:23:30 +0100

    Update hwsensors.rb

--- Casks/hwsensors.rb
+++ Casks/hwsensors.rb
@@ -3,7 +3,8 @@ cask 'hwsensors' do
   sha256 '1ea229bebb1cdfa3b6091cacfd7ab04da781cd6191db9ed08a73eb901aefa418'
 
   url "https://github.com/kozlekek/HWSensors/releases/download/#{version.major_minor}/HWSensors.#{version}.pkg.zip"
-  appcast 'https://github.com/kozlekek/HWSensors/releases.atom'
+  appcast 'https://github.com/kozlekek/HWSensors/releases.atom',
+          configuration: version.major_minor
   name 'HWSensors'
   homepage 'https://github.com/kozlekek/HWSensors/'
 
