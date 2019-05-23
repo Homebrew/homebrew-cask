6fa15ddb526712e42c9c9a735a257b4365ec4063
Vítor Galvão <info@vitorgalvao.com>
2019-05-23 19:28:39 +0100

    Update sql-tabs.rb

--- Casks/sql-tabs.rb
+++ Casks/sql-tabs.rb
@@ -2,7 +2,9 @@ cask 'sql-tabs' do
   version '1.0.0'
   sha256 'd3fa156de487b0f8956136d2999617ceec365cf989578340f523544fe0ccf81c'
 
-  url 'https://github.com/sasha-alias/sqltabs/releases/download/v1.0.0/sqltabs.mac.dmg'
+  # github.com/sasha-alias/sqltabs was verified as official when first introduced to the cask
+  url "https://github.com/sasha-alias/sqltabs/releases/download/v#{version}/sqltabs.mac.dmg"
+  appcast 'https://github.com/sasha-alias/sqltabs/releases.atom'
   name 'SQL Tabs'
   homepage 'https://www.sqltabs.com/'
 
