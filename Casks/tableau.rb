ce1d12fcfb42f62f5f30d2c0d83340a174b7434c
Vítor Galvão <info@vitorgalvao.com>
2019-05-23 12:46:43 +0100

    Update tableau.rb

--- Casks/tableau.rb
+++ Casks/tableau.rb
@@ -3,7 +3,8 @@ cask 'tableau' do
   sha256 '5f292ec62d21b47a2c711879ca844fd59708a0a857e5f7f20719eaeeecc2ba1d'
 
   url "https://downloads.tableau.com/tssoftware/TableauDesktop-#{version.dots_to_hyphens}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/desktop/mac'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/desktop/mac',
+          configuration: version.dots_to_hyphens.to_s
   name 'Tableau Desktop'
   homepage 'https://www.tableau.com/products/desktop/download'
 
