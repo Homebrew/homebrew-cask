5fa4f6baa6ce66e050dcc300b99e5b0915989870
suschizu <brigitte@corecode.io>
2019-05-23 12:20:21 +0200

    Update tableau-reader.rb

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -3,7 +3,8 @@ cask 'tableau-reader' do
   sha256 '1266664c89ec3f1d42027d1cd8f928d47e3a91166c6d1b932218cb5bbb0a9285'
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/reader/mac'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/reader/mac',
+          configuration: version.dots_to_hyphens.to_s
   name 'Tableau Reader'
   homepage 'https://www.tableau.com/products/reader'
 
