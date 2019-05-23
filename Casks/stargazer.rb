679d8f3bdde914e8ae9df01b025a101d06f0b03e
suschizu <brigitte@corecode.io>
2019-05-23 22:29:57 +0200

    Update stargazer.rb

--- Casks/stargazer.rb
+++ Casks/stargazer.rb
@@ -2,9 +2,9 @@ cask 'stargazer' do
   version '1.7.0'
   sha256 'b6f735a4409f012dc2fe8a4a35ae97e3e0d361e7fdf8481a7bda3731bd8a814a'
 
-  # github.com/johansten/stargazer was verified as official when first introduced to the cask
-  url "https://github.com/johansten/stargazer/releases/download/v#{version}/Stargazer-#{version}.dmg"
-  appcast 'https://github.com/johansten/stargazer/releases.atom'
+  # github.com/future-tense/stargazer was verified as official when first introduced to the cask
+  url "https://github.com/future-tense/stargazer/releases/download/v#{version}/Stargazer-#{version}.dmg"
+  appcast 'https://github.com/future-tense/stargazer/releases.atom'
   name 'Stargazer'
   homepage 'https://getstargazer.com/'
 
