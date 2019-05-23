4bd797139c756825cd41bfe6bc77cb0459fe5f0e
Randall <ran.dall@icloud.com>
2019-05-23 07:00:24 -0700

    Update mob from 0.2.0 to 0.2.1

--- Casks/mob.rb
+++ Casks/mob.rb
@@ -1,6 +1,6 @@
 cask 'mob' do
-  version '0.2.0'
-  sha256 '59a685bb17fbcacfae4ea75afdd3a47ff123192e383d7cd0a7d97c186c57e0d0'
+  version '0.2.1'
+  sha256 '18ab080133100b87982b5d54af5dcb3d5bff18b99d0635603395600621a0c68d'
 
   url "https://github.com/zenghongtu/Mob/releases/download/v#{version}/Mob-#{version}-mac.dmg"
   appcast 'https://github.com/zenghongtu/Mob/releases.atom'
