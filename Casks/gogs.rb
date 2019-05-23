cbeaf2c0b4a4a011a83e8a7b2a25f5617f50ab6f
suschizu <brigitte@corecode.io>
2019-05-23 22:01:48 +0200

    Update gogs.rb

--- Casks/gogs.rb
+++ Casks/gogs.rb
@@ -2,9 +2,9 @@ cask 'gogs' do
   version '0.11.66'
   sha256 'f92e7bb780efd66ba0c714c333ca4c67152f93396644b75750da6bf413b762d5'
 
-  # github.com/gogits/gogs was verified as official when first introduced to the cask
-  url "https://github.com/gogits/gogs/releases/download/v#{version}/darwin_amd64.zip"
-  appcast 'https://github.com/gogits/gogs/releases.atom'
+  # github.com/gogs/gogs was verified as official when first introduced to the cask
+  url "https://github.com/gogs/gogs/releases/download/v#{version}/darwin_amd64.zip"
+  appcast 'https://github.com/gogs/gogs/releases.atom'
   name 'Go Git Service'
   homepage 'https://gogs.io/'
 
