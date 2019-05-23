2ea6bfdeccf90fa46783c5667adbad25894dbd82
suschizu <brigitte@corecode.io>
2019-05-23 22:10:13 +0200

    Update majsoul-plus.rb

--- Casks/majsoul-plus.rb
+++ Casks/majsoul-plus.rb
@@ -2,10 +2,10 @@ cask 'majsoul-plus' do
   version '1.11.2'
   sha256 '380225037efd0c3347266ff00e8bcf2c94e0926499b459101e8f9d12a27aa6a9'
 
-  url "https://github.com/MajsoulPlus/majsoul-plus-client/releases/download/v#{version}/Majsoul_Plus-darwin.dmg"
-  appcast 'https://github.com/MajsoulPlus/majsoul-plus-client/releases.atom'
+  url "https://github.com/MajsoulPlus/majsoul-plus/releases/download/v#{version}/Majsoul_Plus-darwin.dmg"
+  appcast 'https://github.com/MajsoulPlus/majsoul-plus/releases.atom'
   name 'Majsoul Plus'
-  homepage 'https://github.com/MajsoulPlus/majsoul-plus-client'
+  homepage 'https://github.com/MajsoulPlus/majsoul-plus/'
 
   app 'Majsoul Plus.app'
 end
