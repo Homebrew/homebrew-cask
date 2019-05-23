a1c0c97fad8af8936f4d8e48b8c88a658285c47f
suschizu <brigitte@corecode.io>
2019-05-23 12:19:44 +0200

    Update tableau-public.rb

--- Casks/tableau-public.rb
+++ Casks/tableau-public.rb
@@ -3,7 +3,8 @@ cask 'tableau-public' do
   sha256 '46d27c225a8d7f7a9ba2be82f6d08f529ee09f435835716d6882997155da780f'
 
   url "https://downloads.tableau.com/public/TableauPublic-#{version.dots_to_hyphens}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/public/mac'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/public/mac',
+          configuration: version.dots_to_hyphens.to_s
   name 'Tableau Public'
   homepage 'https://public.tableau.com/s/'
 
