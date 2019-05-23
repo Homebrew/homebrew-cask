9456da5b2c56a25922d60e9c376c5fe7aa953d7b
suschizu <brigitte@corecode.io>
2019-05-23 15:58:57 +0200

    Update navicat-for-sql-server from 12.1.20 to 12.1.22 (#63733)

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-sql-server' do
-  version '12.1.20'
-  sha256 '286f1c9e5121de3e2de389765c254fec4518f81bc91f6a7f7c5c324a124d51e8'
+  version '12.1.22'
+  sha256 'f1bd1ba4961b0bd1b5661931a64c3b689bfac0c572705364303807afe81b6ed2'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20for%20SQL%20Server&appLang=en'
