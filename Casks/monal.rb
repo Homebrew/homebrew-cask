a280436f0dd63aaed93bf307429147fd3d896a32
Randall <ran.dall@icloud.com>
2019-05-24 08:30:14 -0700

    Update monal from 2.4b2 to 2.4b4

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,6 +1,6 @@
 cask 'monal' do
-  version '2.4b2'
-  sha256 '81e9e8d460fcc937e98c4988766330b134431e7705f75bbae8f20169bef9d410'
+  version '2.4b4'
+  sha256 'a5816318dabfe7b16d803d2be69172e6f3e0f6d892c0218a9bef1ed9852087b7'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
   appcast 'https://monal.im/Monal-OSX/appcast.xml'
