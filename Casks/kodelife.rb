17ce028e25aee38b336cd9d7a6ffa5bb1499b26f
Randall <ran.dall@icloud.com>
2019-05-24 08:29:20 -0700

    Update kodelife from 0.8.0.88 to 0.8.2.92

--- Casks/kodelife.rb
+++ Casks/kodelife.rb
@@ -1,6 +1,6 @@
 cask 'kodelife' do
-  version '0.8.0.88'
-  sha256 '05b36deb59b7dc28445060ff00e41ea14a2370e4ca8239abee261ede0c6ad8aa'
+  version '0.8.2.92'
+  sha256 'f4a79d82e1bb239195f590068b33ecbff64ebdb0ec1e573387bd5e18cf01e822'
 
   url "https://hexler.net/pub/kodelife/kodelife-#{version}-macos.zip"
   appcast 'https://hexler.net/pub/kodelife/appcast.hex'
