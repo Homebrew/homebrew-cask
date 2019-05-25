1310d7ecbd9edadc247be28fbb5824a289e1b840
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-25 09:37:00 -0700

    Update ios-app-signer from 1.11 to 1.12 (#63841)

--- Casks/ios-app-signer.rb
+++ Casks/ios-app-signer.rb
@@ -1,6 +1,6 @@
 cask 'ios-app-signer' do
-  version '1.11'
-  sha256 'd81a1ac9e59c4a06754ce0207d13b4b1f716baf172af6b31258303752c2f58e7'
+  version '1.12'
+  sha256 'd8b0eea20984854ebbdb603923087ca0d182aeaeb7a81c08c5dd2d8b734b5235'
 
   # github.com/DanTheMan827/ios-app-signer was verified as official when first introduced to the cask
   url "https://github.com/DanTheMan827/ios-app-signer/releases/download/#{version}/iOS.App.Signer.app.zip"
