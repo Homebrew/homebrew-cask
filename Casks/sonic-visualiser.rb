9caa46049eb58153353fc5d82f380afec451beeb
suschizu <brigitte@corecode.io>
2019-05-25 20:13:22 +0200

    Update sonic-visualiser.rb

--- Casks/sonic-visualiser.rb
+++ Casks/sonic-visualiser.rb
@@ -1,9 +1,9 @@
 cask 'sonic-visualiser' do
-  version '3.3,2486'
+  version '3.3.0,2486'
   sha256 '0697f375ab8796e5ad0e14c682f9a94a61e1a6708baced55158e16b34cf9fd73'
 
   # code.soundsoftware.ac.uk was verified as official when first introduced to the cask
-  url "https://code.soundsoftware.ac.uk/attachments/download/#{version.after_comma}/Sonic%20Visualiser-#{version.before_comma}.dmg"
+  url "https://code.soundsoftware.ac.uk/attachments/download/#{version.after_comma}/Sonic%20Visualiser-#{version.before_comma.major_minor}.dmg"
   appcast 'https://code.soundsoftware.ac.uk/projects/sonic-visualiser/repository/raw/CHANGELOG'
   name 'Sonic Visualiser'
   homepage 'https://www.sonicvisualiser.org/'
