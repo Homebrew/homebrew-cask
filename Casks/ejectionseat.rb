f828ea499d675f6bf60964bb6c455071982f2773
Austin Kootz <austin.kootz@live.com>
2019-05-25 13:03:06 -0600

    Updated to pass audit and style checks
    Signed-off-by: Austin Kootz <austin.kootz@live.com>

--- Casks/ejectionseat.rb
+++ Casks/ejectionseat.rb
@@ -1,7 +1,7 @@
 cask 'ejectionseat' do
   version '1.4.4'
   sha256 'c2b6920ffa1f477a5542994045b8c583cffa2373d56d50c67e73745830cd6d17'
-  
+
   url "https://github.com/pilotchute/EjectionSeat/releases/download/#{version}/EjectionSeat.app.zip"
   appcast 'https://github.com/pilotchute/EjectionSeat/releases.atom'
   name 'ejectionseat'
