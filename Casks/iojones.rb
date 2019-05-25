b793f9f9a424dd17c2deec27e102c48846de2b4e
chiyo <chiyo@mydesktop1.local>
2019-05-26 00:48:05 +0930

    new IOJones cask

---
+++ Casks/iojones.rb
@@ -0,0 +1,10 @@
+cask 'iojones' do
+  version '1.1'
+  sha256 '381cd0f643c89f016e30cf978d361dcf06c396d6d1ed76e9ae9c192a3ed060b7'
+
+  url "https://jaist.dl.sourceforge.net/project/iojones/IOJones/1.1/IOJones.zip"
+  name 'IOJones'
+  homepage 'https://sourceforge.net/projects/iojones/'
+
+  app 'IOJones.app'
+end
