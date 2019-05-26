4aca55a778c5dd3ad9e55d0ae2c0c1d573b6d430
Julian Mayer <julian@corecode.io>
2019-05-26 12:29:50 +0200

    Update nosqlbooster-for-mongodb

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -3,6 +3,7 @@ cask 'nosqlbooster-for-mongodb' do
   sha256 'd3a6218771a5e0d46cf7332bbf1f9a6539d40f7dd27198509637c4b9d55228a0'
 
   url "https://nosqlbooster.com/s3/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
+  appcast 'https://nosqlbooster.com/downloads'
   name 'NoSQLBooster for MongoDB'
   homepage 'https://nosqlbooster.com/'
 
