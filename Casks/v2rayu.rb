4af770ef00024bff8301cdbd2afbb92aae08f12c
Randall <ran.dall@icloud.com>
2019-05-26 09:58:47 -0700

    Update v2rayu from 1.3.0 to 1.3.1

--- Casks/v2rayu.rb
+++ Casks/v2rayu.rb
@@ -1,6 +1,6 @@
 cask 'v2rayu' do
-  version '1.3.0'
-  sha256 '31bb20a2dae246c0dd29ae00608af3a8c9081491e943338e4c7979edd4b25992'
+  version '1.3.1'
+  sha256 '25c1c0818b8d5d75dddb9a13b46430bceb3aad2775c4ef5f73a2424bcf58f17c'
 
   url "https://github.com/yanue/V2rayU/releases/download/#{version}/V2rayU.dmg"
   appcast 'https://github.com/yanue/V2rayU/releases.atom'
