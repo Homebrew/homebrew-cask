6a0da3429a4225e98b0330397800b036746ee9f3
chiyo <chiyo@mydesktop1.local>
2019-05-26 19:18:05 +0930

    not hardcoding version number on download link

--- Casks/iojones.rb
+++ Casks/iojones.rb
@@ -2,7 +2,7 @@ cask 'iojones' do
   version '1.1'
   sha256 '381cd0f643c89f016e30cf978d361dcf06c396d6d1ed76e9ae9c192a3ed060b7'
 
-  url "https://jaist.dl.sourceforge.net/project/iojones/IOJones/1.1/IOJones.zip"
+  url "https://jaist.dl.sourceforge.net/project/iojones/IOJones/#{version}/IOJones.zip"
   name 'IOJones'
   homepage 'https://sourceforge.net/projects/iojones/'
 
