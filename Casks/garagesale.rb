4155cf76155d2f97d22df1f331f4d2cb64c474ae
CoreCode <core-code@users.noreply.github.com>
2019-05-26 13:17:11 +0200

    Update garagesale.rb

--- Casks/garagesale.rb
+++ Casks/garagesale.rb
@@ -3,7 +3,7 @@ cask 'garagesale' do
   sha256 '7e2bcf398121331a819e50970cf306486eb492c0078e8c1376233c232e021f87'
 
   url "https://downloads.iwascoding.com/downloads/GarageSale_#{version}.dmg"
-  appcast 'https://rink.hockeyapp.net/api/2/apps/b83b659a4c853a6138def4da94c6fcdd'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/b83b659a4c853a6138def4da94c6fcdd.rss'
   name 'GarageSale'
   homepage 'https://www.iwascoding.com/GarageSale/'
 
