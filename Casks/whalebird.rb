a9297b2067f242aead0836c757e8b6c3adf4bf7b
AkiraFukushima <h3.poteto@gmail.com>
2019-05-26 12:56:32 +0900

    Update whalebird to 2.7.3

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -1,6 +1,6 @@
 cask 'whalebird' do
-  version '2.7.2'
-  sha256 '9f772815c900a63a030ef7c54153fac5e7be0267edfa467ca8bacbcbe8595486'
+  version '2.7.3'
+  sha256 'ac694f8ed71479cd23978f5cdded388b751947b8c6d6fa9e1565f114b9c6753e'
 
   # github.com/h3poteto/whalebird-desktop was verified as official when first introduced to the cask
   url "https://github.com/h3poteto/whalebird-desktop/releases/download/#{version}/Whalebird-#{version}-darwin-x64.dmg"
