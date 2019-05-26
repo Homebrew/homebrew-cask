051ba169310cdfd17a2472e2f1240a2b11234597
CoreCode <core-code@users.noreply.github.com>
2019-05-26 14:25:44 +0200

    Update glip (#63871)

--- Casks/glip.rb
+++ Casks/glip.rb
@@ -4,6 +4,7 @@ cask 'glip' do
 
   # downloads.ringcentral.com/glip/rc was verified as official when first introduced to the cask
   url "https://downloads.ringcentral.com/glip/rc/#{version}/mac/RingCentral-#{version}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.ringcentral.com/glip/rc/GlipForMac'
   name 'Glip'
   homepage 'https://glip.com/'
 
