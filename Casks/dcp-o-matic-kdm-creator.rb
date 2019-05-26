b9988d9cf95168b5bedc3a73980812a17e0e7b5d
Work <brigitte@corecode.io>
2019-05-26 16:41:01 +0200

    Update dcp-o-matic-kdm-creator from 2.14.2 to 2.14.4

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-kdm-creator' do
-  version '2.14.2'
-  sha256 '70b0d0e5df442f8d045c3ae1ba5ea368ce98a803b378711619429d7daae40cda'
+  version '2.14.4'
+  sha256 '9815f5a685e54af346f2aafdde4e66538b38863928ea32c5ee650c91f644e3f6'
 
   url "https://dcpomatic.com/dl.php?id=osx-kdm&version=#{version}"
   appcast 'https://dcpomatic.com/download'
