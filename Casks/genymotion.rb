70dee0b64b33a36641f0819ddc1a5d08cecfa83a
CoreCode <core-code@users.noreply.github.com>
2019-05-26 14:45:12 +0200

    Update genymotion (#63873)

--- Casks/genymotion.rb
+++ Casks/genymotion.rb
@@ -3,6 +3,7 @@ cask 'genymotion' do
   sha256 '97e7cc585eea32ddc8a55494d31046760e70ea42f8de11bddf9f7a2970d46b16'
 
   url "https://dl.genymotion.com/releases/genymotion-#{version}/genymotion-#{version}.dmg"
+  appcast 'https://www.genymotion.com/product-release-note/desktop/'
   name 'Genymotion'
   homepage 'https://www.genymotion.com/'
 
