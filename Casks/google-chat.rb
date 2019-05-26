36067188ff7f044239039bc5b8750fd0e8efa805
CoreCode <core-code@users.noreply.github.com>
2019-05-26 14:26:11 +0200

    Update google-chat (#63878)

--- Casks/google-chat.rb
+++ Casks/google-chat.rb
@@ -3,6 +3,7 @@ cask 'google-chat' do
   sha256 'ea09e8e1e42b526f2deee7fa78ca4886ebd36ec90a4b79a22b31c1a550491a04'
 
   url "https://dl.google.com/chat/#{version}/InstallHangoutsChat.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://dl.google.com/chat/latest/InstallHangoutsChat.dmg'
   name 'Chat'
   homepage 'https://gsuite.google.com/products/chat/'
 
