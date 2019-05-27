55bf1ff0db90a856433ab6da67d9780d1454b465
Minijus L <3633549+minijus@users.noreply.github.com>
2019-05-27 21:35:23 +0300

    Update sha256 for AngularConsole v7.4.24 (#63952)
    Provided sha256 was incorrect, new sha256 is taken from https://github.com/nrwl/angular-console/releases/download/v7.4.24/AngularConsole-7.4.24.dmg

--- Casks/angular-console.rb
+++ Casks/angular-console.rb
@@ -1,6 +1,6 @@
 cask 'angular-console' do
   version '7.4.24'
-  sha256 '787ead1cf41ddf46813716e81e39b181378fa3b07bf21f1ebc488d17d0ee2dbc'
+  sha256 'ae2fc4f2ab1c8f473064d7c8648c343d6ca7ed037983f12e617d679ad608c640'
 
   # github.com/nrwl/angular-console was verified as official when first introduced to the cask
   url "https://github.com/nrwl/angular-console/releases/download/v#{version}/AngularConsole-#{version}.dmg"
