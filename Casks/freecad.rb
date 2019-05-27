d387bf051160a0151331833216ff6ceaa3d66657
Henning Steinke <lamemate@users.noreply.github.com>
2019-05-27 12:18:00 +0200

    Update freecad (#63916)

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,6 +1,6 @@
 cask 'freecad' do
   version '0.18.1,16117'
-  sha256 'd9ed9730697d0d22bacdd55159286e9a19cbc47bd6b4be55f32a34596a7b1286'
+  sha256 '72767892e5a5703c4a1e2d98ecf129ad8c672c495c424fbe8f1b152c771eaed4'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg"
