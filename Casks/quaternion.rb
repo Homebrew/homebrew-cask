1d5048997432325adbab1c45cacf1834894d97ca
Work <brigitte@corecode.io>
2019-05-27 13:50:21 +0200

    Update quaternion from 0.0.9.4b to 0.0.9.4

--- Casks/quaternion.rb
+++ Casks/quaternion.rb
@@ -1,6 +1,6 @@
 cask 'quaternion' do
-  version '0.0.9.4b'
-  sha256 'f070c28a0b248b5dff5540de93d47bed75cab8a41d9936eb043d9a28b9634ea2'
+  version '0.0.9.4'
+  sha256 'e33ecf94cbafedd3f665db8ff52764ee403af3acb45aff676788ec96225e8d03'
 
   url "https://github.com/quotient-im/Quaternion/releases/download/#{version}/quaternion-#{version}.dmg"
   appcast 'https://github.com/quotient-im/Quaternion/releases.atom'
