0bd214d56f70d5af72d8c2915c8846525c99683e
Work <brigitte@corecode.io>
2019-05-27 15:50:13 +0200

    Update dcp-o-matic-encode-server from 2.14.4 to 2.14.5

--- Casks/dcp-o-matic-encode-server.rb
+++ Casks/dcp-o-matic-encode-server.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-encode-server' do
-  version '2.14.4'
-  sha256 'ae0a17f9e4367d1eee3c5970476e25f7a459705d637b5c98f624e0bd21ccaf50'
+  version '2.14.5'
+  sha256 'aa47c4c36d578284b1f9f484a4cfa062b14759221a8a97f2aff562c946173791'
 
   url "https://dcpomatic.com/dl.php?id=osx-server&version=#{version}"
   appcast 'https://dcpomatic.com/download'
