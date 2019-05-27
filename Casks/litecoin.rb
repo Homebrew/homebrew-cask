1edaac87fb4bc71ea11842d8456cdc60cb65054b
CoreCode <core-code@users.noreply.github.com>
2019-05-28 00:03:40 +0200

    Update litecoin.rb

--- Casks/litecoin.rb
+++ Casks/litecoin.rb
@@ -3,7 +3,7 @@ cask 'litecoin' do
   sha256 '7f8ec34706701482970baae4d4bc0fdb19016a6ab5260f983bf478428f0da518'
 
   url "https://download.litecoin.org/litecoin-#{version}/osx/litecoin-#{version}-osx.dmg"
-  appcast 'https://github.com/litecoin-project/litecoin/releases'
+  appcast 'https://github.com/litecoin-project/litecoin/releases.atom'
   name 'Litecoin'
   homepage 'https://litecoin.org/'
 
