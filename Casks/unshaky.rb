0371e00ef23ce0175ca7ce59dd6ecfeb2d4c230a
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-27 07:15:13 -0700

    Update unshaky from 0.5.4 to 0.5.5 (#63941)

--- Casks/unshaky.rb
+++ Casks/unshaky.rb
@@ -1,6 +1,6 @@
 cask 'unshaky' do
-  version '0.5.4'
-  sha256 '116443f801fbed3487ac19bd9ed9fc8c9843e7501df2fa43f50b85076bd7d46a'
+  version '0.5.5'
+  sha256 '36ce63073977d5fe0a6719842ef67ef56380d4790134d4d0aab96d7b5064619f'
 
   url "https://github.com/aahung/Unshaky/releases/download/v#{version}/Unshaky_v#{version}.zip"
   appcast 'https://github.com/aahung/Unshaky/releases.atom'
