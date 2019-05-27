a22c42dffe6d9ea4ad3eaf079198696f57b1a01e
suschizu <brigitte@corecode.io>
2019-05-27 16:05:55 +0200

    Update dcp-o-matic-player from 2.14.4 to 2.14.5 (#63938)

--- Casks/dcp-o-matic-player.rb
+++ Casks/dcp-o-matic-player.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-player' do
-  version '2.14.4'
-  sha256 '50f8253079e921edf0f272b71ebb9986f013ac851f007390bf0e4dfd8c8af9f3'
+  version '2.14.5'
+  sha256 'cf0fc1cce589b8610287c192fd5579ba648ab3c9f8a004d2b52364ca20136e81'
 
   url "https://dcpomatic.com/dl.php?id=osx-player&version=#{version}"
   appcast 'https://dcpomatic.com/download'
