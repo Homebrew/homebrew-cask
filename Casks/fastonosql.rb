8be36fd75120d68e10be4c17cf76be9991068be8
CoreCode <core-code@users.noreply.github.com>
2019-05-28 11:43:09 +0200

    Update fastonosql from 2.2.2 to 2.4.0 (#63977)

--- Casks/fastonosql.rb
+++ Casks/fastonosql.rb
@@ -1,6 +1,6 @@
 cask 'fastonosql' do
-  version '2.2.2'
-  sha256 'afc63a1926c74732bbab20b0a9cbefad9628935abb29539b295a4464b703026f'
+  version '2.4.0'
+  sha256 'e10ff9703e960adbf9054957e11a5384d12e96d58146529532bc7caa8d6539b1'
 
   url "https://fastonosql.com/downloads_pro/macosx/fastonosql_pro-#{version}-x86_64.dmg"
   appcast 'https://github.com/fastogt/fastonosql/releases.atom'
