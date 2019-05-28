0225bf3b3b3ec2d136d0495b3ff1960a7a893a10
Vítor Galvão <info@vitorgalvao.com>
2019-05-28 14:26:19 +0100

    Update dissenter-browser.rb

--- Casks/dissenter-browser.rb
+++ Casks/dissenter-browser.rb
@@ -2,8 +2,8 @@ cask 'dissenter-browser' do
   version '0.67.13'
   sha256 '623ad116b173bf07af4956323147dd376b0261dd2a69c7db40e3fef4a0c3e6a4'
 
-  # github.com/brave/brave-browser was verified as official when first introduced to the cask
   url "https://dissenter.com/dist/browser/dissenter-browser-#{version}.dmg"
+  appcast 'https://dissenter.com/'
   name 'Dissenter'
   homepage 'https://dissenter.com/'
 
