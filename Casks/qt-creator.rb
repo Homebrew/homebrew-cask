a366c41da4bbb17f00e308ff77191eedf2779654
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2019-05-28 16:20:18 +0300

    Update qt-creator from 4.9.0 to 4.9.1

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator' do
-  version '4.9.0'
-  sha256 '11e897c807826d153a159dd0f84e7fda2bf559a44e66a17cb2550ed27dbb0279'
+  version '4.9.1'
+  sha256 'a508505c919d9d5b3c5660afb223ecbba3ea78606b34dedc40feb8c5170d3c4a'
 
   url "https://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   appcast 'https://download.qt.io/official_releases/qtcreator/'
