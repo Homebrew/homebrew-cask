c6b4604dfa29e29f3e19e927715de914350d2631
CoreCode <core-code@users.noreply.github.com>
2019-05-28 15:10:08 +0200

    Update pascom-client from 50.R318_5cd8b40 to 51.R365_e50571e (#63986)

--- Casks/pascom-client.rb
+++ Casks/pascom-client.rb
@@ -1,8 +1,9 @@
 cask 'pascom-client' do
-  version '50.R318_5cd8b40'
-  sha256 '621ec0cc4f5c5f09e94d617c6a8489598f7860f64fc3f59f425be9a4785cbece'
+  version '51.R365_e50571e'
+  sha256 'ac823d6c7c1191c088c51477bfb59c51c5418f4ae92de1393ac934431bcbc8f9'
 
   url "https://download.pascom.net/release-archive/client/stable/pascom%20Client-#{version}.dmg"
+  appcast 'https://my.pascom.net/update/client/stable?_=1559037394513'
   name 'pascom Client'
   homepage 'https://www.pascom.net/'
 
