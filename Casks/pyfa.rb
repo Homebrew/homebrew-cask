4c98d3bc06c5af1356aea95dbd34f9e7b2384597
Austin Kootz <austin.kootz@live.com>
2019-05-28 14:24:43 -0600

    Updated for Pyfa v2.9.4

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,6 +1,6 @@
 cask 'pyfa' do
-  version '2.9.3'
-  sha256 '08e2429d2f6585d6204b9eb190c416d809ccd961fb033ec65014a1a8a4ae972f'
+  version '2.9.4'
+  sha256 '17a66ad4af414a9c69458532db3f5c2da0f0504bd58da7d8165de04dfc8f6913'
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom'
