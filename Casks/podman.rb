fe42c6425688f830c373664ac43c934fc22bbb03
Vítor Galvão <info@vitorgalvao.com>
2019-05-29 12:38:40 +0100

    Update podman.rb

--- Casks/podman.rb
+++ Casks/podman.rb
@@ -2,7 +2,7 @@ cask 'podman' do
   version '1.3.1'
   sha256 '438f7788fe28e88ae800e99896eb42a07775682e4bc69a2f618fc2c36c8a4a21'
 
-  url 'https://github.com/containers/libpod/releases/download/v1.3.1/podman-v1.3.1.dmg'
+  url "https://github.com/containers/libpod/releases/download/v#{version}/podman-v#{version}.dmg"
   appcast 'https://github.com/containers/libpod/releases.atom'
   name 'podman'
   homepage 'https://github.com/containers/libpod/'
