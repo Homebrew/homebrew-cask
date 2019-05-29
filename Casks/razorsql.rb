278e3b37ac8c0da196a100dbffcb333e9a814970
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-29 14:59:48 -0700

    Update razorsql from 8.3.4 to 8.3.5 (#64093)

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask 'razorsql' do
-  version '8.3.4'
-  sha256 '75f78dcdfe0bd7184d64db2e159fcf0e426a81481db793518292168fe6a16294'
+  version '8.3.5'
+  sha256 'f22583095e5b75be26daaa38a227cf04d58b43bf377a1f6028c12af92ffb7da3'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   appcast 'https://razorsql.com/updates.html'
