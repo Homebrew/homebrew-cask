0e2d1c534bb46996dd3e97cbfa8e66b29e6424cb
insyria <insyria@gmail.com>
2019-05-29 14:39:25 -0400

    2.5.2 is outdated / change to latest
    * The version that gets installed is outdated (currently the version is 4.0).
    * Changed the version to latest
    * appcast that supports latest updates is not available, its currently within the app.

--- Casks/quickbooks-online.rb
+++ Casks/quickbooks-online.rb
@@ -1,9 +1,8 @@
 cask 'quickbooks-online' do
-  version '2.5.2'
-  sha256 'a345e484108200214cbacb8f0e88c395c601baa1f7b6d9e59bef81edf634c376'
+  version 'latest'
+  sha256 :no_check
 
-  url "https://http-download.intuit.com/http.intuit/CMO/apple_web/sample/static/qbomac/releases/QuickBooks_#{version}/QuickBooks.QBOWeb.dmg"
-  appcast 'https://http-download.intuit.com/http.intuit/CMO/apple_web/sample/static/qbomac/rssfeeds/Staged/QuickBooks.QBOWeb.Release.xml'
+  url "https://http-download.intuit.com/http.intuit/CMO/qbo_client_web/static/release/mac/latest/QuickBooks.dmg"
   name 'QuickBooks'
   homepage 'https://qbo.intuit.com/'
 
