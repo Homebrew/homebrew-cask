c3c51eb47d7dcf8cc87cd1123b321bb541d8e64c
Daniel Stone <me@danstone.uk>
2019-05-29 12:15:14 +0100

    Update musaicfm from 1.1.2 to 1.1.3

--- Casks/musaicfm.rb
+++ Casks/musaicfm.rb
@@ -1,6 +1,6 @@
 cask 'musaicfm' do
-  version '1.1.2'
-  sha256 '966b84ee5941ffec8f439daf60e499dd6a07401828dc769411a98b0b1458c0b5'
+  version '1.1.3'
+  sha256 '4f942a9b9cab9af7178fa4de7ca5809ba36c1ecbf551c7db9567a48de982336f'
 
   url "https://github.com/docterd/MusaicFM/releases/download/#{version}/MusaicFM.saver.zip"
   appcast 'https://github.com/docterd/MusaicFM/releases.atom'
