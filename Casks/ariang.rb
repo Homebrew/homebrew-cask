cb153de78f6b84628a8cef21a781bc0d3d2a8687
Ye Liu <879316283@qq.com>
2019-05-29 11:42:44 +0800

    Update ariang from 1.1.0 to 1.1.1

--- Casks/ariang.rb
+++ Casks/ariang.rb
@@ -1,6 +1,6 @@
 cask 'ariang' do
-  version '1.1.0'
-  sha256 'e6d0e8ec1b40fcc18aa327c706f77e82a4628496b922df420bd33a59f6ed92f7'
+  version '1.1.1'
+  sha256 '4e354fa76ce275297b506f2092b8ae156d1bbb3bc4a418ac6e0b60e5514f5685'
 
   url "https://github.com/mayswind/AriaNg-Native/releases/download/#{version}/AriaNg_Native-#{version}-macOS.dmg"
   appcast 'https://github.com/mayswind/AriaNg-Native/releases.atom'
