312bac6132935fe5af151994432f580e9084ce8a
Vítor Galvão <info@vitorgalvao.com>
2019-05-29 23:08:13 +0100

    Update qt-creator.rb

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -3,7 +3,8 @@ cask 'qt-creator' do
   sha256 'a508505c919d9d5b3c5660afb223ecbba3ea78606b34dedc40feb8c5170d3c4a'
 
   url "https://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
-  appcast 'https://download.qt.io/official_releases/qtcreator/'
+  appcast 'https://download.qt.io/official_releases/qtcreator/',
+          configuration: version.major_minor
   name 'Qt Creator'
   homepage 'https://www.qt.io/developers/'
 
