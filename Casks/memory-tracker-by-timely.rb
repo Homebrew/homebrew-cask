f91976213b1b5d4d180acf4aae9077e66f115ce3
Randall <ran.dall@icloud.com>
2019-05-29 12:36:32 -0700

    Update memory-tracker-by-timely from 1.2.6 to 1.2.7

--- Casks/memory-tracker-by-timely.rb
+++ Casks/memory-tracker-by-timely.rb
@@ -1,6 +1,6 @@
 cask 'memory-tracker-by-timely' do
-  version '1.2.6'
-  sha256 '1948e319b139828be9913ca2878a20a1e49eba8a9ba57e900f750bc4973d6132'
+  version '1.2.7'
+  sha256 '9f9f3e2eb5130d174fb2f961e694232721c56e51eeb2e1f89376d1cb1ba0a690'
 
   # timelytimetracking.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://timelytimetracking.s3.amazonaws.com/mac_tracker/Memory%20Tracker%20by%20Timely.zip'
