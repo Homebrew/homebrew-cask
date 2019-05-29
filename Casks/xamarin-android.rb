b3a77c67a7ac0ad4e9ff98f8e717549b111178ec
Hanstad <20923110+Hanstad@users.noreply.github.com>
2019-05-29 12:36:49 +0200

    Update xamarin-android to 9.3.0-22

--- Casks/xamarin-android.rb
+++ Casks/xamarin-android.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-android' do
-  version '9.2.3-0'
-  sha256 'b79af0a777be9e3a1e440bf16383835cb5aa198171fa299ccc9e5fbd8e37a4cc'
+  version '9.3.0-22'
+  sha256 '790dd55032581532a70d0d419e44b7dea02c0f3c543b8cadacd1ae64b8d2ef25'
 
   url "https://dl.xamarin.com/MonoforAndroid/Mac/xamarin.android-#{version}.pkg"
   appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v4/Mac/Universal/InstallationManifest.xml'
