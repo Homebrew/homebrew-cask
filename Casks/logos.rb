e1338d03cae41baf4ef2528cb46a7499b9da54bd
Soneé John <sonee@alphasoftware.co>
2019-05-30 09:37:18 -0400

    Add appcast to Logos cask

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -3,6 +3,7 @@ cask 'logos' do
   sha256 :no_check
 
   url 'https://downloads.logoscdn.com/LBS8/Installer/#{version}/LogosMac.dmg'
+  appcast "https://clientservices.logos.com/update/v1/feed/logos8-mac/stable.xml"
   name 'Logos Bible Software'
   homepage 'https://www.logos.com/'
 
