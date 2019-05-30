f0ef1b52257c0c2704cb70232ad14d0ce199bb23
suschizu <brigitte@corecode.io>
2019-05-30 18:05:16 +0200

    Update marvin from 1.41.1 to 1.41.2 (#64138)

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -1,6 +1,6 @@
 cask 'marvin' do
-  version '1.41.1'
-  sha256 '17cb7f882d86a3743cad203ea9fdfba18d2b8fa66b998ef94f94cc620142dca8'
+  version '1.41.2'
+  sha256 '92a9b94d93eae49e5cecb944a01ca4f3649e867e867b440ae568701c5c6ff180'
 
   # s3.amazonaws.com/amazingmarvin was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/amazingmarvin/Marvin-#{version}.dmg"
