2acd8ca5e4aeae257b316cd0812a576051c48a4e
Work <brigitte@corecode.io>
2019-05-30 13:51:43 +0200

    Update dvdstyler

--- Casks/dvdstyler.rb
+++ Casks/dvdstyler.rb
@@ -1,6 +1,6 @@
 cask 'dvdstyler' do
   version '3.1'
-  sha256 '08ad4af74c8776908b7d2d8930ea1694a38ef0982164d8af26954156358d7ef9'
+  sha256 'c66d291db8b2dbe9af21b8e210983198b930b02fef1b51866c365bb2493376c5'
 
   # sourceforge.net/dvdstyler was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/dvdstyler/DVDStyler-#{version}-MacOSX.dmg"
