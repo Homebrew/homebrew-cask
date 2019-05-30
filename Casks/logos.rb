1d38d540b196f04084459df357a588612974e586
Soneé John <sonee@alphasoftware.co>
2019-05-30 09:50:45 -0400

    Add checksum

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -1,6 +1,6 @@
 cask 'logos' do
   version '8.5.0.0026'
-  sha256 :no_check
+  sha256 '4a1c9d9eef3f51f40b1fb85e3e33783e5e5fb5b5283cdd0f6ddc59acce71902b'
 
   url "https://downloads.logoscdn.com/LBS8/Installer/#{version}/LogosMac.dmg"
   appcast 'https://clientservices.logos.com/update/v1/feed/logos8-mac/stable.xml'
