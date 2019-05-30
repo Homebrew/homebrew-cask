39b1db2f304512eb35154314eb65362a80623869
Soneé John <sonee@alphasoftware.co>
2019-05-30 09:50:13 -0400

    Properly format url and appcast

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -2,8 +2,8 @@ cask 'logos' do
   version '8.5.0.0026'
   sha256 :no_check
 
-  url 'https://downloads.logoscdn.com/LBS8/Installer/#{version}/LogosMac.dmg'
-  appcast "https://clientservices.logos.com/update/v1/feed/logos8-mac/stable.xml"
+  url "https://downloads.logoscdn.com/LBS8/Installer/#{version}/LogosMac.dmg"
+  appcast 'https://clientservices.logos.com/update/v1/feed/logos8-mac/stable.xml'
   name 'Logos Bible Software'
   homepage 'https://www.logos.com/'
   
