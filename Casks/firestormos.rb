852f808d4946f6b40f1ca8f380e25bacc6b0ab36
CoreCode <core-code@users.noreply.github.com>
2019-05-31 00:14:29 +0200

    Update firestormos (#64164)

--- Casks/firestormos.rb
+++ Casks/firestormos.rb
@@ -3,6 +3,7 @@ cask 'firestormos' do
   sha256 'f8e5fa3b1a72aff67ce91d44f82f536d898a9b4c3a818414799ccb8d288e7e97'
 
   url "https://downloads.firestormviewer.org/mac/Phoenix-FirestormOS-Releasex64_#{version.dots_to_underscores}_x86_64.dmg"
+  appcast 'https://www.firestormviewer.org/mac/'
   name 'Phoenix Firestorm viewer for Second Life'
   homepage 'https://www.firestormviewer.org/'
 
