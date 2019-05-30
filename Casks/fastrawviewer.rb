a38e7053118faaa225d79b120b46e9ee0e4d58f6
Julian Mayer <julian@corecode.io>
2019-05-30 23:21:50 +0200

    Update fastrawviewer

--- Casks/fastrawviewer.rb
+++ Casks/fastrawviewer.rb
@@ -3,6 +3,7 @@ cask 'fastrawviewer' do
   sha256 '560cfb44eeba1574684987aa4f7b80cdfa11b2082a2ddf5e9d742bfddb6dca28'
 
   url "https://updates.fastrawviewer.com/data/FastRawViewer-#{version}.dmg"
+  appcast 'https://www.fastrawviewer.com/download'
   name 'FastRawViewer'
   homepage 'https://www.fastrawviewer.com/'
 
