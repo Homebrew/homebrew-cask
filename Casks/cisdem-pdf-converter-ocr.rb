9749e069b9d311cb64d1fc7e91270368c29c04f6
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-30 08:55:38 -0700

    Update cisdem-pdf-converter-ocr from 6.5.0 to 6.6.0 (#64134)

--- Casks/cisdem-pdf-converter-ocr.rb
+++ Casks/cisdem-pdf-converter-ocr.rb
@@ -1,6 +1,6 @@
 cask 'cisdem-pdf-converter-ocr' do
-  version '6.5.0'
-  sha256 'cb49366f28c3e8815d30ea68bbbf4affbc397a3a2ceb8d03d2d48a812fea9ba9'
+  version '6.6.0'
+  sha256 '4ad587a08725cf847b4f4abbcfe06f91beea8f6beca933d795457532c74051ad'
 
   url 'http://download.cisdem.com/cisdem-pdfconverterocr.dmg'
   appcast 'https://www.cisdem.com/pdf-converter-ocr-mac/release-notes.html'
