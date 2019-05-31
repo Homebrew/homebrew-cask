4361be9843ec3c777abae0599390a520c6f6473e
Vítor Galvão <info@vitorgalvao.com>
2019-05-31 14:04:46 +0100

    Update yyets.rb

--- Casks/yyets.rb
+++ Casks/yyets.rb
@@ -2,8 +2,8 @@ cask 'yyets' do
   version '3.1.1'
   sha256 'acc48e3a6c450149262fbdadea5350a176b8e8f79d1da4db282dd690fd8c9af3'
 
-  # appdown.rrys.tv was verified as official when first introduced to the cask
   url "http://appdown.rrys.tv/YYetsShare.#{version}.dmg"
+  appcast 'http://app.rrys.tv/'
   name 'YYeTs'
   name '人人影视'
   homepage 'http://app.rrys.tv/'
