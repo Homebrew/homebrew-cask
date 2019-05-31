a62a6c9560add28777b58dca8feacdcf6f54846c
Sebastiaan Lokhorst <sebastiaanlokhorst@gmail.com>
2019-05-31 19:25:22 +0200

    Update borgbackup from 1.1.9 to 1.1.10 (#64208)

--- Casks/borgbackup.rb
+++ Casks/borgbackup.rb
@@ -1,6 +1,6 @@
 cask 'borgbackup' do
-  version '1.1.9'
-  sha256 '29908ee70db3bc64395633bf804872bb242c342f31432e4ffdf3344b9ba838ca'
+  version '1.1.10'
+  sha256 'a9432478993d29f4e5c2523473ebe814ed583f03a19ee2ec7f7d371cd45c2d43'
 
   # github.com/borgbackup/borg was verified as official when first introduced to the cask
   url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
