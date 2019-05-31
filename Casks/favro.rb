3d288ac61524a9fd6570111783e8e911fdbd155e
CoreCode <core-code@users.noreply.github.com>
2019-05-31 02:45:07 +0200

    Update favro from 1.0.22 to 1.0.25 (#64161)

--- Casks/favro.rb
+++ Casks/favro.rb
@@ -1,8 +1,9 @@
 cask 'favro' do
-  version '1.0.22'
-  sha256 '71c2f7a06fd609e5a4f44d94cbcb2b7b712d179f6359de3112786041f5b5d8df'
+  version '1.0.25'
+  sha256 'ab554e74c697b3d3f4477e3d4a3f3cda8aa564e6789e6128d364e744f7882dd5'
 
   url "https://download.favro.com/FavroDesktop/macOS/x64/Favro-#{version}.dmg"
+  appcast 'https://download.favro.com/FavroDesktop/macOS/x64/Latest.json'
   name 'Favro'
   homepage 'https://www.favro.com/'
 
