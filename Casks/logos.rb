556ca05a786e7e90103e42025071f94910e8fcb0
Soneé John <sonee@alphasoftware.co>
2019-05-30 20:44:13 -0400

    Upgrade Logos.app to v8.5.0.0026 (#64128)
    * Add version number to Logos read value for download URL
    
    * Add appcast to Logos cask
    
    * Add system requirement to Logos cask
    
    * Properly format url and appcast
    
    * Add checksum
    
    * Add uninstall stanaza
    
    * Fix cask style
    
    * Add comment about url and homepage discrepancy
    
    * Update logos.rb

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -1,10 +1,23 @@
 cask 'logos' do
-  version :latest
-  sha256 :no_check
+  version '8.5.0.0026'
+  sha256 '4a1c9d9eef3f51f40b1fb85e3e33783e5e5fb5b5283cdd0f6ddc59acce71902b'
 
-  url 'http://downloads.logos.com/LBS7/Installer/LogosMac.dmg'
+  # downloads.logoscdn.com was verified as official when first introduced to the cask
+  url "https://downloads.logoscdn.com/LBS8/Installer/#{version}/LogosMac.dmg"
+  appcast "https://clientservices.logos.com/update/v1/feed/logos#{version.major}-mac/stable.xml"
   name 'Logos Bible Software'
   homepage 'https://www.logos.com/'
 
+  depends_on macos: '>= :el_capitan'
+
   app 'Logos.app'
+
+  uninstall launchctl: 'com.logos.LogosIndexer',
+            quit:      'com.logos.Logos'
+
+  zap trash: [
+               '~/Library/Preferences/com.logos.LogosIndexer.plist',
+               '~/Library/Preferences/com.logos.LogosCEF.plist',
+               '~/Library/Preferences/com.logos.Logos.plist',
+             ]
 end
