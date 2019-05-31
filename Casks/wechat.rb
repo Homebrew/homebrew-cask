de42bbc606246aa1f66b649cf93eefed31dc5e22
Markus Reiter <me@reitermark.us>
2019-05-31 19:27:56 +0200

    Update wechat.rb

--- Casks/wechat.rb
+++ Casks/wechat.rb
@@ -1,7 +1,7 @@
 cask 'wechat' do
   version '2.3.25.20'
-	sha256 'a71fb87464e456c92545d1c0bd46008a74fe15712099c17d56796ce5bfb30cd5'
-	
+  sha256 'a71fb87464e456c92545d1c0bd46008a74fe15712099c17d56796ce5bfb30cd5'
+
   url 'https://dldir1.qq.com/weixin/mac/WeChatMac.dmg'
   appcast 'https://dldir1.qq.com/weixin/mac/mac-release.xml'
   name 'WeChat for Mac'
