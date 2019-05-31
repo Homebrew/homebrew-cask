32a3423fbec7a0548670f1215091e252cba3453d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-05-31 13:47:50 -0700

    Update monal from 2.4b4 to 2.4b5 (#64205)
    * Update monal from 2.4b4 to 2.5b5
    
    * Fixed typo in `Version` number

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,6 +1,6 @@
 cask 'monal' do
-  version '2.4b4'
-  sha256 'a5816318dabfe7b16d803d2be69172e6f3e0f6d892c0218a9bef1ed9852087b7'
+  version '2.4b5'
+  sha256 'bcaae218600ceacaa307d0ccb1dfeb33ad7bbae63531668cc7169b27f6553140'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
   appcast 'https://monal.im/Monal-OSX/appcast.xml',
