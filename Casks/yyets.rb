aef0f314f12242e0c910ce67b7f7b73473282f4a
我是洗衣机 <womeimingzi11@users.noreply.github.com>
2019-05-31 21:28:26 +0800

    Upgrade YYeTs.app to v3.1.1 (#64106)
    * Upgrade YYeTs.app to v3.1.1
    
    * Update homepage and Upgrade YYeTs.app to v3.1.1
    
    * Remove unnecessary appcast of YYeTs
    
    Because there is no version info about the YYeTs
    
    * Update yyets.rb

--- Casks/yyets.rb
+++ Casks/yyets.rb
@@ -1,13 +1,12 @@
 cask 'yyets' do
-  version '2.55'
-  sha256 '3ac6d420585003160ce3e4053c749502c8e034c2c63f34dcecb30b180e768e2d'
+  version '3.1.1'
+  sha256 'acc48e3a6c450149262fbdadea5350a176b8e8f79d1da4db282dd690fd8c9af3'
 
-  # appdown.rrys.tv was verified as official when first introduced to the cask
   url "http://appdown.rrys.tv/YYetsShare.#{version}.dmg"
-  appcast 'http://app.zimuzu.io/'
+  appcast 'http://app.rrys.tv/'
   name 'YYeTs'
   name '人人影视'
-  homepage 'http://app.zimuzu.io/'
+  homepage 'http://app.rrys.tv/'
 
   app '人人影视.app'
 
