2bde17a285c21dbd44bc586fe84661e086d16449
suschizu <brigitte@corecode.io>
2019-05-31 13:57:42 +0200

    Update archipelago from 3.3.0 to 3.4.0 (#64193)

--- Casks/archipelago.rb
+++ Casks/archipelago.rb
@@ -1,6 +1,6 @@
 cask 'archipelago' do
-  version '3.3.0'
-  sha256 'ecb5ef8895e68a30c4ba3e5dc75c75893c3d56c6baaa66687045f2bf61aa71e3'
+  version '3.4.0'
+  sha256 'b44ff8911eadd692a9eabeafb3eed55700603bb44814d09c12dd6ae55e37a8e6'
 
   url "https://github.com/npezza93/archipelago/releases/download/v#{version}/Archipelago-#{version}.dmg"
   appcast 'https://github.com/npezza93/archipelago/releases.atom'
