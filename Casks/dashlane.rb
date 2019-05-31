b742857e7a272b1b8bb578ea71f3edc036c8dc4f
CoreCode <core-code@users.noreply.github.com>
2019-05-31 16:25:43 +0200

    Update dashlane from 6.1914.0.19481 to 6.1918.1.20296 (#64156)
    * Update dashlane from 6.1914.0.19481 to 6.1918.1.20296
    
    * Update dashlane.rb

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,9 +1,9 @@
 cask 'dashlane' do
-  version '6.1914.0.19481'
-  sha256 '0d3852cb0b76e4fe20a541722f2ce56c1d79042b809dd76f824ffa1e15121658'
+  version '6.1922.0.20633'
+  sha256 'af0a59b059d2ec8f56bf4ffd036002189fae938fc80913d6eabad33a90f884cb'
 
-  url "https://cdn5.dashlane.com/proxy/d3mfqat9ni8wb5/releases/#{version.major_minor_patch}/#{version}/release/Dashlane.dmg"
-  appcast 'https://ws1.dashlane.com/5/binaries/query?target=archive&format=xml&currentSoftwareVersion=6.0.0&platform=server_osx&os=OS_X_10_14_1'
+  url "https://cdn5.dashlane.com/proxy/d3mfqat9ni8wb5/releases/#{version.major_minor_patch}/#{version}/qa-safariAppExtension/Dashlane.dmg"
+  appcast 'https://ws1.dashlane.com/5/binaries/query?format=json&os=OS_X_10_14_5&target=archive&platform=launcher_macosx'
   name 'Dashlane'
   homepage 'https://www.dashlane.com/'
 
