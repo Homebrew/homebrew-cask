418cc8c2b382a20cdfe2db243819017e65891e6c
Vítor Galvão <info@vitorgalvao.com>
2019-06-01 00:48:59 +0100

    Update localdev.rb

--- Casks/localdev.rb
+++ Casks/localdev.rb
@@ -2,7 +2,7 @@ cask 'localdev' do
   version '0.4.0'
   sha256 '5ce8b74ab6af8214abb2cd8a4d56d3ed431f76cfa9083ba1606aa25e947af96a'
 
-  # pantheon-localdev.s3.amazonaws.com/ was verified as official when first introduced to the cask
+  # pantheon-localdev.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://pantheon-localdev.s3.amazonaws.com/Localdev-v#{version}.dmg"
   name 'Localdev'
   homepage 'https://pantheon.io/pantheon-localdev-power-users-pre-release'
