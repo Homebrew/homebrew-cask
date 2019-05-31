f2b2dc3d3db8c623044e3fc031aa4c381238265c
Vítor Galvão <info@vitorgalvao.com>
2019-05-31 01:59:50 +0100

    Update yinxiangbiji.rb

--- Casks/yinxiangbiji.rb
+++ Casks/yinxiangbiji.rb
@@ -3,7 +3,8 @@ cask 'yinxiangbiji' do
   sha256 '63ac655383068912641d74f4ae701f0a0a49835622bb326b18d2b84bf70a3091'
 
   url "https://cdn.yinxiang.com/mac-smd/public/YinxiangBiji_RELEASE_#{version}.dmg"
-  appcast 'https://update.yinxiang.com/public/ENMacSMD/EvernoteMacUpdate.xml'
+  appcast 'https://update.yinxiang.com/public/ENMacSMD/EvernoteMacUpdate.xml',
+          configuration: version.sub('_', ' ')
   name 'Evernote'
   name '印象笔记'
   homepage 'https://www.yinxiang.com/'
