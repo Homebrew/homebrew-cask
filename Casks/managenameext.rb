977511146f2dc7198eba82d0bcfa8bcd53d37c31
suschizu <brigitte@corecode.io>
2019-05-31 15:01:09 +0200

    Update managenameext.rb

--- Casks/managenameext.rb
+++ Casks/managenameext.rb
@@ -2,10 +2,10 @@ cask 'managenameext' do
   version '1.6.8'
   sha256 '8e9234e1a940c2ed2a933f1d0ba07bfc8d14fc3b61f5c963f07ecf6fe351c8b3'
 
-  url 'http://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_c.zip'
-  appcast 'http://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_V.txt'
+  url 'https://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_c.zip'
+  appcast 'https://throb.pagesperso-orange.fr/prg/Xojo/ManageNameExt_V.txt'
   name 'ManageNameExt'
-  homepage 'http://throb.pagesperso-orange.fr/site/ind_JS.html?Prg_S.html&Prg_ApplisRB.html#ManageNameExt'
+  homepage 'https://throb.pagesperso-orange.fr/site/ind_JS.html?Prg_S.html&Prg_ApplisRB.html#ManageNameExt'
 
   app 'ManageNameExt.app'
 end
