3ace3f99b1df770c8999a7ed2a4ce5d634ac7645
Vítor Galvão <info@vitorgalvao.com>
2019-05-31 14:01:25 +0100

    Update dashlane.rb

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,9 +1,9 @@
 cask 'dashlane' do
-  version '6.1918.1.20296'
-  sha256 'c80a1f6a1934aad9792ef63f8c8d7249fda9781e736cba087041f19693d6662b'
+  version '6.1922.0.20633'
+  sha256 'af0a59b059d2ec8f56bf4ffd036002189fae938fc80913d6eabad33a90f884cb'
 
   url "https://cdn5.dashlane.com/proxy/d3mfqat9ni8wb5/releases/#{version.major_minor_patch}/#{version}/qa-safariAppExtension/Dashlane.dmg"
-  appcast 'https://ws1.dashlane.com/5/binaries/query?target=archive&format=xml&currentSoftwareVersion=6.0.0&platform=server_osx&os=OS_X_10_14_1'
+  appcast 'https://ws1.dashlane.com/5/binaries/query?format=json&os=OS_X_10_14_5&target=archive&platform=launcher_macosx'
   name 'Dashlane'
   homepage 'https://www.dashlane.com/'
 
