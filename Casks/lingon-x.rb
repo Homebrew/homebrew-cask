e404fd6a15ea20c21dddb00abe2b1cd33288916f
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-01 08:42:30 -0700

    Fix sha256 sums

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -1,10 +1,10 @@
 cask 'lingon-x' do
   if MacOS.version <= :high_sierra
     version '6.6.4'
-    sha256 :no_check
+    sha256 'fc788402fa16df39a3d48cdc501dae31368ec6fd69ffe0026ba99932b28cae19'
   else
     version '7.2.2'
-    sha256 'fc788402fa16df39a3d48cdc501dae31368ec6fd69ffe0026ba99932b28cae19'
+    sha256 'bbc6939fd4cfa3dcf1da534b4ea4daef0c5241d644374d6cdcd59c91e3040ec9'
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
