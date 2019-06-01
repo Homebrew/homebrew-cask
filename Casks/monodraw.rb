56581568bbce3190279df585e71b6d7a3ef50d02
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-01 08:44:30 -0700

    Update `version`

--- Casks/monodraw.rb
+++ Casks/monodraw.rb
@@ -1,5 +1,5 @@
 cask 'monodraw' do
-  version '1.4.0,b103'
+  version '1.4,b103'
   sha256 'a3a61a256908741c7da879726940e27f0b246400c7abe8e862e477db790d3a12'
 
   url "https://updates.helftone.com/monodraw/downloads/Monodraw-#{version.after_comma}.zip"
