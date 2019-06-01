99ab54bcea1ad208d12e8e2b944e92ec6e0e4b45
suschizu <brigitte@corecode.io>
2019-06-01 21:53:25 +0200

    Update eiskaltdcpp from 2.2.10-583-macOS10.12 to 2.2.10-631-macOS10.12 (#64254)

--- Casks/eiskaltdcpp.rb
+++ Casks/eiskaltdcpp.rb
@@ -1,6 +1,6 @@
 cask 'eiskaltdcpp' do
-  version '2.2.10-583-macOS10.12'
-  sha256 'fb272380d0892116cfa3a8bfbd4ba746423e7102d1c8ee83f0c7d1c13bc62aad'
+  version '2.2.10-631-macOS10.12'
+  sha256 '36708cc72ad9ffb36ac2cfcf42348f4a7bff72ebfb65df80825af934924e9f41'
 
   url "https://downloads.sourceforge.net/eiskaltdcpp/EiskaltDC++-#{version}-x86_64.dmg"
   appcast 'https://sourceforge.net/projects/eiskaltdcpp/rss'
