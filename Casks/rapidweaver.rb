c5fcb3fe8918f06e8f226632896f30dd60573fa7
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-01 09:13:53 -0700

    Update rapidweaver from 8.2,20740.1558691647 to 8.2.1,20758.1559229313 (#64247)

--- Casks/rapidweaver.rb
+++ Casks/rapidweaver.rb
@@ -1,6 +1,6 @@
 cask 'rapidweaver' do
-  version '8.2,20740.1558691647'
-  sha256 '49f31d6c85ba5452c8b332dda03e6879945d2b02e4ebffaf4cbf4f1c58c8351a'
+  version '8.2.1,20758.1559229313'
+  sha256 'e739f786cbe18f42bdc766e8bc4d6d2777d7112ce54797674e091ae0ae38f182'
 
   # devmate.com/com.realmacsoftware.rapidweaver was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.realmacsoftware.rapidweaver#{version.major}/#{version.after_comma.major}/#{version.after_comma.minor}/RapidWeaver#{version.major}-#{version.after_comma.major}.zip"
