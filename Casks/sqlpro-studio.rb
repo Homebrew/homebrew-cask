20f62026a66cfcabde20b24716c57071d74fddff
Work <brigitte@corecode.io>
2019-06-02 14:35:13 +0200

    Update sqlpro-studio from 2019.05.22 to 2019.05.29

--- Casks/sqlpro-studio.rb
+++ Casks/sqlpro-studio.rb
@@ -1,6 +1,6 @@
 cask 'sqlpro-studio' do
-  version '2019.05.22'
-  sha256 'ab5d36e740e83cb3039cbfa24619d2e9b1f9e682f5d5139f8c233a580fa74972'
+  version '2019.05.29'
+  sha256 '9954dc5e1071089074b90a230ef68b6d4f7ee8750fb1a947d09df0c2f248585e'
 
   # d3fwkemdw8spx3.cloudfront.net/studio was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/studio/SQLProStudio.#{version}.app.zip"
