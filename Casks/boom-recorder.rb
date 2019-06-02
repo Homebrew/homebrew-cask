5484610700734ce0cc56ce586764170b21a5b936
CoreCode <core-code@users.noreply.github.com>
2019-06-02 05:22:44 +0200

    Update boom-recorder (#64265)

--- Casks/boom-recorder.rb
+++ Casks/boom-recorder.rb
@@ -3,6 +3,7 @@ cask 'boom-recorder' do
   sha256 '2854f36ab9c341694335472da77fba83df19f64e4433240a43f051d5bddfb41a'
 
   url "https://www.pokitec.com/downloads/Boom%20Recorder-#{version}.dmg"
+  appcast 'https://www.pokitec.com/products/BoomRecorder/'
   name 'Boom Recorder'
   homepage 'https://www.pokitec.com/products/BoomRecorder/'
 
