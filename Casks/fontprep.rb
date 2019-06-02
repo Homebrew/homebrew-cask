b2592ddd52e2e99cbac00f247e585d565d2ab4ab
suschizu <brigitte@corecode.io>
2019-06-02 13:32:42 +0200

    Update fontprep.rb

--- Casks/fontprep.rb
+++ Casks/fontprep.rb
@@ -2,7 +2,7 @@ cask 'fontprep' do
   version '3.1.1'
   sha256 '769d64b78d1a8db42dcb02beff6f929670448f77259388c9d01692374be2ec46'
 
-  url "https://github.com/briangonzalez/fontprep/releases/download/v3.1.1/FontPrep_#{version}.zip"
+  url "https://github.com/briangonzalez/fontprep/releases/download/v#{version}/FontPrep_#{version}.zip"
   appcast 'https://github.com/briangonzalez/fontprep/releases.atom'
   name 'FontPrep'
   homepage 'https://github.com/briangonzalez/fontprep'
