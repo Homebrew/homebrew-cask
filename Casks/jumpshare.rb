b8bcce9435e71cdf25d3bb44bbd9682aa79a1856
Jarrod Davis <developer@jarrodldavis.com>
2019-06-02 20:51:43 -0600

    Update jumpshare.rb

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,10 +1,9 @@
 cask 'jumpshare' do
   version '2.5.0'
-  sha256 '90145cf8e7499c31ba3ad350a697745127fc2b338297c2d50a03a8176a4ea6b1'
+  sha256 'c321f977cd1fd1802b7b4247dbf5d4108432320098b8f40491db837893feda36'
 
-  # d21hi1or3tbtjm.cloudfront.net was verified as official when first introduced to the cask
-  url 'https://d21hi1or3tbtjm.cloudfront.net/desktop/mac/Jumpshare.zip'
-  appcast 'https://rink.hockeyapp.net/api/2/apps/2e15b9ed1bad4078ac88b8fae1771bfb'
+  url 'https://apps.jumpshare.com/desktop/mac/updates/Jumpshare-2.5.0.tar.bz2'
+  appcast 'https://apps.jumpshare.com/desktop/mac/updates/appcast.xml'
   name 'Jumpshare'
   homepage 'https://jumpshare.com/'
 
