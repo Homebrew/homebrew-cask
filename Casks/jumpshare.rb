bbd4412ffda18063bc8ccf0448c0751ac0b1a769
Vítor Galvão <info@vitorgalvao.com>
2019-06-03 13:27:57 +0100

    Update jumpshare.rb

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -2,7 +2,7 @@ cask 'jumpshare' do
   version '2.5.0'
   sha256 'c321f977cd1fd1802b7b4247dbf5d4108432320098b8f40491db837893feda36'
 
-  url 'https://apps.jumpshare.com/desktop/mac/updates/Jumpshare-2.5.0.tar.bz2'
+  url "https://apps.jumpshare.com/desktop/mac/updates/Jumpshare-#{version}.tar.bz2"
   appcast 'https://apps.jumpshare.com/desktop/mac/updates/appcast.xml'
   name 'Jumpshare'
   homepage 'https://jumpshare.com/'
