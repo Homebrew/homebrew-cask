c8ce333a688474d445cac5bd8ba8ad0503cc2cff
Vítor Galvão <info@vitorgalvao.com>
2019-06-03 14:06:39 +0100

    Update quickbooks-online.rb

--- Casks/quickbooks-online.rb
+++ Casks/quickbooks-online.rb
@@ -1,8 +1,9 @@
 cask 'quickbooks-online' do
-  version 'latest'
-  sha256 :no_check
+  version '4.0.7-217'
+  sha256 '32df37d609ca7c30ea643ec29bb640c884d97df059040a2a70c122dfa30a0252'
 
-  url "https://http-download.intuit.com/http.intuit/CMO/qbo_client_web/static/release/mac/latest/QuickBooks.dmg"
+  url "https://http-download.intuit.com/http.intuit/CMO/qbo_client_web/static/release/mac/QuickBooks-#{version}-mac.zip"
+  appcast 'https://http-download.intuit.com/http.intuit/CMO/qbo_client_web/static/release/mac/latest-mac.yml'
   name 'QuickBooks'
   homepage 'https://qbo.intuit.com/'
 
