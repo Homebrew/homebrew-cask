a71849fba3e5a91b3ebb9efbe253d2932cc92373
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-03 10:46:36 -0700

    Update classroom-assistant from 1.0.3 to 1.0.4 (#64324)

--- Casks/classroom-assistant.rb
+++ Casks/classroom-assistant.rb
@@ -1,6 +1,6 @@
 cask 'classroom-assistant' do
-  version '1.0.3'
-  sha256 'b5ba0785d3f212b24098cfcd96b4a2d6e16b3aacc75ae732786c8670b7d62aca'
+  version '1.0.4'
+  sha256 '48fecda2ce6d882c1ecf7714844bc613b8fced4b24c75d0e5c8525a9265556e8'
 
   url "https://github.com/education/classroom-assistant/releases/download/v#{version}/Classroom.Assistant-darwin-x64-#{version}.zip"
   appcast 'https://github.com/education/classroom-assistant/releases.atom'
