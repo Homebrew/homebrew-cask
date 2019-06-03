c2f5254246173770a46e1948c10f7851e2add1be
Henrique Dias <hacdias@gmail.com>
2019-06-03 21:25:12 +0100

    Update ipfs to 0.8.0 (#64335)

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,6 +1,6 @@
 cask 'ipfs' do
-  version '0.7.3'
-  sha256 'b11c8c84508ba75ba09bea6eebff5eb1877344396aae81c84f522d194d9469da'
+  version '0.8.0'
+  sha256 'a1bd934b4f3b4f796b5fbd384a9d552b550d9502981ca1ad167fadcf03d6cffe'
 
   url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/ipfs-desktop-#{version}.dmg"
   appcast 'https://github.com/ipfs-shipyard/ipfs-desktop/releases.atom'
