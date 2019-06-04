45b09ecf8eeec709ceaf68cfe20f18bb2f81639a
Ralph Martin <ralph@cs.cf.ac.uk>
2019-06-04 08:27:09 +0100

    Update cmake from 3.14.4 to 3.14.5 (#64352)

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -1,6 +1,6 @@
 cask 'cmake' do
-  version '3.14.4'
-  sha256 'ca56fb4d590c7518f3cb20297be92dae15ab55be4cc907b020c2d8cfe07da678'
+  version '3.14.5'
+  sha256 'a1dd64edb6036fdaa365ed0cd850e437ce6ab2e67da817f080376a2aa05eefc8'
 
   url "https://www.cmake.org/files/v#{version.major_minor}/cmake-#{version}-Darwin-x86_64.dmg"
   name 'CMake'
