8c93b66fef4e2905915303c7ba0e034096b13472
Jonathan Gao <gaolei33@gmail.com>
2019-06-05 02:03:59 +0800

    Update ApplePi-Baker homepage

--- Casks/applepi-baker.rb
+++ Casks/applepi-baker.rb
@@ -4,7 +4,7 @@ cask 'applepi-baker' do
 
   url "https://www.tweaking4all.com/downloads/raspberrypi/ApplePi-Baker-v#{version}.dmg"
   name 'ApplePi-Baker'
-  homepage 'https://www.tweaking4all.com/hardware/raspberry-pi/macosx-apple-pi-baker/'
+  homepage 'https://www.tweaking4all.com/hardware/raspberry-pi/applepi-baker-v2/'
 
   app 'ApplePi-Baker.app'
 
