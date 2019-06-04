bf1454fa262feef129ad437c410f1d9d43b9007a
kPherox <admin@mail.kr-kp.com>
2019-06-05 00:40:20 +0900

    Fix checksum

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask 'thedesk' do
   version '18.5.1'
-  sha256 '2248c451c0666e39e9c150e76f49f388243c36201e2d66ab2ec578a63be9e3b5'
+  sha256 'd9f5450ba7360bc479c508a54a283fab02342bf3ee3048acd5184b1ce1451244'
 
   # github.com/cutls/TheDesk was verified as official when first introduced to the cask
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}.dmg"
