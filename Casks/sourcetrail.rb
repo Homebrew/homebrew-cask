f37b2a5c1fe8456a1013c59761bde84260e45448
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-04 09:30:00 -0700

    Update sourcetrail from 2019.2.25 to 2019.2.39 (#64376)

--- Casks/sourcetrail.rb
+++ Casks/sourcetrail.rb
@@ -1,6 +1,6 @@
 cask 'sourcetrail' do
-  version '2019.2.25'
-  sha256 '13caae8924683c9edb07325697e2c55279f3c455f089308021757971f8429730'
+  version '2019.2.39'
+  sha256 '4ba6b422ca7027acd94325a1425eaf324a567107a8a0ad6ee37521336370ed37'
 
   url "https://www.sourcetrail.com/downloads/#{version}/osx/64bit"
   appcast 'https://raw.githubusercontent.com/CoatiSoftware/SourcetrailBugTracker/master/README.md'
