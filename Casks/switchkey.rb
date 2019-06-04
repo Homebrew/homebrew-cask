13543eb0bddf527f7144a8fee1147c425798387d
FAN VINGA <fanalcest@gmail.com>
2019-06-05 00:23:30 +0800

    Remove unnecessary comment

--- Casks/switchkey.rb
+++ Casks/switchkey.rb
@@ -2,7 +2,6 @@ cask 'switchkey' do
   version '1.1.1'
   sha256 '1286e2e066f4f211f8508dd5ff98679129cfea04fe267000a057e99a65def347'
 
-# github.com/itsuhane/SwitchKey was verified as official when first introduced to the cask
   url "https://github.com/itsuhane/SwitchKey/releases/download/v#{version}/SwitchKey.zip"
   appcast 'https://github.com/itsuhane/SwitchKey/releases.atom'
   name 'SwitchKey'
