0c6d2b64d6496744415ad4b35c2f2e2f5065df5d
Vítor Galvão <info@vitorgalvao.com>
2019-06-04 20:05:49 +0100

    Update celldesigner.rb

--- Casks/celldesigner.rb
+++ Casks/celldesigner.rb
@@ -2,11 +2,10 @@ cask 'celldesigner' do
   version '4.4.2'
   sha256 'b23708f82842f4c138c2ba8d2ecd7d94942c9cedf05bcf6cc6b683e11305209c'
 
-  # systems-biology.org was verified as official when first introduced to the cask
   url "http://www.systems-biology.org/download/CellDesigner-#{version}-osx-installer.dmg"
   appcast 'http://systems-biology.org/software/celldesigner/'
   name 'CellDesigner'
-  homepage 'http://www.celldesigner.org/'
+  homepage 'http://systems-biology.org/software/celldesigner/'
 
   installer script: {
                       executable: "#{staged_path}/CellDesigner-#{version}-osx-installer.app/Contents/MacOS/installbuilder.sh",
@@ -16,10 +15,10 @@ cask 'celldesigner' do
   uninstall script: {
                       executable: "/Applications/CellDesigner#{version}/uninstall.app/Contents/MacOS/installbuilder.sh",
                       args:       ['--mode', 'unattended'],
-                    }
+                    },
+            delete: "/Applications/CellDesigner#{version}"
 
   zap trash: [
-               "/Applications/CellDesigner#{version}",
                '~/.celldesigner',
                '~/.copasi',
                '~/.sbw',
