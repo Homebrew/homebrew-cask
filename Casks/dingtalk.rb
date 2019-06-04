8350d21511d31cdd27c04c6d4449df4ff9a307e6
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-04 09:49:53 -0700

    Update dingtalk from 4.6.28.12 to 4.6.33.5 (#64373)

--- Casks/dingtalk.rb
+++ Casks/dingtalk.rb
@@ -1,9 +1,8 @@
 cask 'dingtalk' do
-  version '4.6.28.12'
-  sha256 'cc86122eb93b2c06a6a8ef359135a4b4187d2f2ef23c59db3062e25823f9e017'
+  version '4.6.33.5'
+  sha256 '2c7c94777e36de605d1e65295ae43a41f1a4534607ef17b2a37eab2c6b78e978'
 
-  # download.alicdn.com/dingtalk-desktop was verified as official when first introduced to the cask
-  url "https://download.alicdn.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
+  url "https://dtapp-pub.dingtalk.com/dingtalk-desktop/mac_dmg/Release/DingTalk_v#{version}.dmg"
   appcast 'https://im.dingtalk.com/manifest/appcast_en.xml'
   name 'DingTalk'
   name '钉钉'
