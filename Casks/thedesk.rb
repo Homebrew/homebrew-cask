1f41d07d02d421f741e7eeef970bef9bda8727e4
kPherox <admin@mail.kr-kp.com>
2019-06-05 01:51:42 +0900

    Update thedesk to 18.5.1 (#64365)
    * Update thedesk to 18.5.1
    
    * Fix checksum

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,9 +1,9 @@
 cask 'thedesk' do
-  version '18.5.0'
-  sha256 'b473de6a6561a5018c23b30779744f95e00bb002ec327a07899f2b1f919d5967'
+  version '18.5.1'
+  sha256 'd9f5450ba7360bc479c508a54a283fab02342bf3ee3048acd5184b1ce1451244'
 
   # github.com/cutls/TheDesk was verified as official when first introduced to the cask
-  url "https://github.com/cutls/TheDesk/releases/download/#{version}/TheDesk-#{version}.dmg"
+  url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}.dmg"
   appcast 'https://github.com/cutls/TheDesk/releases.atom'
   name 'TheDesk'
   homepage 'https://thedesk.top/'
