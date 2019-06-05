2d55da9f1f4205725ff2393a020fdceef98e057d
Work <brigitte@corecode.io>
2019-06-05 14:20:13 +0200

    Update josm from 15031 to 15155

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask 'josm' do
-  version '15031'
-  sha256 '8e7e30144bbceb0d0c58b5301fe78cc1e0b100a70db43d74880f968235f94bfa'
+  version '15155'
+  sha256 '9757b9c06e52b9630351ad023c51c3429659e056fd7a0d813120483717cfac0b'
 
   url "https://josm.openstreetmap.de/download/macosx/josm-macosx-#{version}.zip"
   appcast 'https://josm.openstreetmap.de/'
