2282a8bd3f3ffb8b3d10ef2f000f2e8c43775679
Work <brigitte@corecode.io>
2019-06-05 13:10:50 +0200

    Update scrutiny from 8.4.0 to 8.4.1

--- Casks/scrutiny.rb
+++ Casks/scrutiny.rb
@@ -1,6 +1,6 @@
 cask 'scrutiny' do
-  version '8.4.0'
-  sha256 '524acbbdf02526eeaa777a8cd3ac274da40aa0143f36e16d2b48ce49c26f64e9'
+  version '8.4.1'
+  sha256 '3ed52a04b9709b19a9e4156ca15b6ed8e75d6de15d9dda40bbc20a328596072f'
 
   url 'https://peacockmedia.software/mac/scrutiny/scrutiny.dmg'
   appcast 'https://peacockmedia.software/mac/scrutiny/version_history.html'
