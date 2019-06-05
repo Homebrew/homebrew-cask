0399b6d4bc709f48c5425129bb313da2bcb948d4
Trevor Sullivan <trevor@trevorsullivan.net>
2019-06-05 08:51:46 -0700

    Update multipass to 0.7.0

--- Casks/multipass.rb
+++ Casks/multipass.rb
@@ -1,6 +1,6 @@
 cask 'multipass' do
-  version '0.6.0'
-  sha256 '8883682aecea31efd4afa2838d692a89aceda50d72810405dfbd2747d0e62715'
+  version '0.7.0'
+  sha256 'f170958e295860d4c4cc0844afe3cc2a8d6def3c1f7ccca9eb21e0c72edb98df'
 
   url "https://github.com/CanonicalLtd/multipass/releases/download/v#{version}/multipass-#{version}+mac-Darwin.pkg"
   appcast 'https://github.com/CanonicalLtd/multipass/releases.atom'
