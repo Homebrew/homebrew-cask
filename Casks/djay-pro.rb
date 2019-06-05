6681b6503cc30ac5a5cbdf0d75d650a92a9cddd1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-05 11:50:46 -0700

    Update djay-pro from 2.0.12,201905091333 to 2.0.13,201906011335 (#64433)

--- Casks/djay-pro.rb
+++ Casks/djay-pro.rb
@@ -1,6 +1,6 @@
 cask 'djay-pro' do
-  version '2.0.12,201905091333'
-  sha256 'ba2c02a5e8040b7b639aee24c2b1ee8e053fd6e9968ee2e84a337ad856148614'
+  version '2.0.13,201906011335'
+  sha256 '6b1bd826c374cc5545a3d2650939e06bfedecd78412ccf85dd8447ead0f45c4e'
 
   url "http://download.algoriddim.com/djay/#{version.after_comma}/djay_Pro_#{version.before_comma}.zip"
   appcast 'https://www.algoriddim.com/djay-pro-mac/releasenotes/appcast'
