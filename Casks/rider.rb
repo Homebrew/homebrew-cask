4385c9789039a65ad89e6f32e9d8c2a967f5d2f2
jcbot <jcb@leipert.io>
2019-06-06 11:34:47 +0000

    Update rider to 2019.1.2,191.7141.355

--- Casks/rider.rb
+++ Casks/rider.rb
@@ -1,6 +1,6 @@
 cask 'rider' do
-  version '2019.1.1,191.6733.985'
-  sha256 'd6a07b1af1bd25f50cc3cc73a835107fc7c58756e06365b11ee9348b367a975a'
+  version '2019.1.2,191.7141.355'
+  sha256 '749e7f9b12033018b1499297a9ff863b14f9d4b2cd13a8dd1d22882afa101cce'
 
   url "https://download.jetbrains.com/rider/JetBrains.Rider-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release'
