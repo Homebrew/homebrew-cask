5880e2ef14d5b153ab76c6c59d25ce37c6e2b0dc
Julian Mayer <julian@corecode.io>
2019-06-06 14:53:12 +0200

    Update cozy-drive

--- Casks/cozy-drive.rb
+++ Casks/cozy-drive.rb
@@ -4,6 +4,7 @@ cask 'cozy-drive' do
 
   # nuts.cozycloud.cc was verified as official when first introduced to the cask
   url "https://nuts.cozycloud.cc/download/channel/stable/CozyDrive-#{version}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://nuts.cozycloud.cc/download/channel/stable/osx'
   name 'Cozy Drive'
   homepage 'https://cozy.io/'
 
