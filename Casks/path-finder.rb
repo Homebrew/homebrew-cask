cc01ce057c8a6ff959e5a2466176453d3b7ffc04
ahousseini <admin@anver-mac.local>
2019-06-06 13:32:49 +0200

    Update path-finder from 8.5.1 to 8.5.2

--- Casks/path-finder.rb
+++ Casks/path-finder.rb
@@ -1,6 +1,6 @@
 cask 'path-finder' do
-  version '8.5.1'
-  sha256 '8f01547f8decec50f2109f848788e35a933acf4a277bb4088507ba42bbf17a74'
+  version '8.5.2'
+  sha256 'bd76318e356a6cb01aeb492dc16234b510cf7d99dfdaba60744dba222b835a23'
 
   url 'https://get.cocoatech.com/PF8.dmg'
   appcast 'https://get.cocoatech.com/releasecast.xml'
