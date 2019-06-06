f5ec0eb14da6afcdfbddb7f30a54c1ce0b637cc9
CoreCode <core-code@users.noreply.github.com>
2019-06-06 20:13:15 +0200

    Update scilab (#64485)

--- Casks/scilab.rb
+++ Casks/scilab.rb
@@ -1,6 +1,6 @@
 cask 'scilab' do
   version '6.0.2'
-  sha256 '4f0e5e9cbdac3fe2baa57b2b2535bc23fe5862e0e99502aa84ac41259b9e1e74'
+  sha256 'e3f0f2117ae62914aca9053f4d3cdb3c8e5d15925a8ea9ff109fb953654ac0f9'
 
   # utc.fr/~mottelet/scilab was verified as official when first introduced to the cask
   url "https://www.utc.fr/~mottelet/scilab/download/#{version}/scilab-#{version}-x86_64.dmg"
