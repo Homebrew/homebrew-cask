5f1ff4ae980fa0ba73bfa5ae1e5b1b2053aa9c38
Viktor Szakats <vszakats@users.noreply.github.com>
2019-06-06 19:00:08 +0000

    Secure URLs (#64483)

--- Casks/fastscripts.rb
+++ Casks/fastscripts.rb
@@ -2,7 +2,7 @@ cask 'fastscripts' do
   version '2.7.7'
   sha256 'f909d9ab3f55f77f98a2407008f707dd7c1ae10f3fbb366052318fe6d33603e4'
 
-  url "https://www.red-sweater.com/fastscripts/FastScripts#{version}.zip"
+  url "https://red-sweater.com/fastscripts/FastScripts#{version}.zip"
   appcast 'https://red-sweater.com/fastscripts/appcast2.php'
   name 'FastScripts'
   homepage 'https://red-sweater.com/fastscripts/'
