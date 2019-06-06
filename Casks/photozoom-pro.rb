23412e6cdec4c5a9b5149df46a2b4861bcdb1308
Julian Mayer <julian@corecode.io>
2019-06-06 20:14:47 +0200

    Update photozoom-pro

--- Casks/photozoom-pro.rb
+++ Casks/photozoom-pro.rb
@@ -2,7 +2,7 @@ cask 'photozoom-pro' do
   version '7'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "http://files.benvista.com/PhotoZoom_Pro_#{version}.dmg"
+  url "https://www.benvista.com/photozoompro#{version}/download/mac"
   name 'PhotoZoom Pro'
   homepage 'https://www.benvista.com/photozoompro'
 
