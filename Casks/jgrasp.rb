32f6467c38e5de5a39e72ef589ac2a77128b27bd
Julian Mayer <julian@corecode.io>
2019-06-06 20:29:15 +0200

    Update jgrasp from 2.0.5_05 to 2.0.5_06

--- Casks/jgrasp.rb
+++ Casks/jgrasp.rb
@@ -1,6 +1,6 @@
 cask 'jgrasp' do
-  version '2.0.5_05'
-  sha256 '753bf46747d4182eb88339ec27c9f51a0379b8756ba80319b884032be8418a33'
+  version '2.0.5_06'
+  sha256 '4afbac7f5fac3851dcd7524ef242143f3b7301ff6bc08af5b86e388432020934'
 
   url "https://jgrasp.org/dl4g/jgrasp/jgrasp#{version.no_dots}.pkg"
   appcast 'https://jgrasp.org/'
