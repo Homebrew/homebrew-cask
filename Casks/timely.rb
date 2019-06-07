e143780733ffa71f8cc4ca48b492e982f698aeed
CoreCode <core-code@users.noreply.github.com>
2019-06-07 16:20:57 +0200

    Update timely from 0.6.1 to 1.0.1 (#64491)
    * Update timely from 0.6.1 to 1.0.1
    
    * Update timely.rb

--- Casks/timely.rb
+++ Casks/timely.rb
@@ -1,10 +1,10 @@
 cask 'timely' do
-  version '0.6.1'
-  sha256 'd6226090847503ca79b6e871854464bc105ff28243df08795876e4f3d85bf43b'
+  version '1.0.1'
+  sha256 'e6bd970c8ba5c4a649aff87b2c6338996882babd8bd172b97f835060f1cfb502'
 
   # github.com/Timely was verified as official when first introduced to the cask
   url "https://github.com/Timely/desktop-releases/releases/download/darwin-x64-prod-v#{version}/Timely-#{version}.dmg"
-  appcast 'https://github.com/Timely/desktop-releases/releases.atom'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://app.timelyapp.com/download/mac'
   name 'Timely'
   homepage 'https://timelyapp.com/'
 
