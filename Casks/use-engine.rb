6694efaf9b93be04fca222dee38db235c863ef7b
Vítor Galvão <info@vitorgalvao.com>
2019-06-07 15:29:15 +0100

    Update use-engine.rb

--- Casks/use-engine.rb
+++ Casks/use-engine.rb
@@ -3,7 +3,7 @@ cask 'use-engine' do
   sha256 'ad87d78fbd99a4c6d2c8b70787a16efea20352578d060365b84f75651fa4711b'
 
   url "https://repository.use-together.com/stable/use-engine/macos/#{version.major}.x/#{version}/use-engine.dmg"
-  name 'USE-Engine'
+  name 'USE Engine'
   homepage 'https://www.use-together.com/'
 
   app 'USE Engine.app'
