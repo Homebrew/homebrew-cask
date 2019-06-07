ee78a928ed5dad8f0af20130e0ffb098ed0e8fb8
Vítor Galvão <info@vitorgalvao.com>
2019-06-07 19:18:22 +0100

    Update razorsql.rb

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -3,7 +3,8 @@ cask 'razorsql' do
   sha256 '70049a78e20cc2f234ad0cb6b71e6e327ce0573b84b1533280cd126e048129dd'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
-  appcast 'https://razorsql.com/updates.html'
+  appcast 'https://razorsql.com/updates.html',
+          configuration: version.dots_to_underscores
   name 'RazorSQL'
   homepage 'https://razorsql.com/download_mac.html'
 
