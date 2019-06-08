7e50f2c09ed192bf43a35f2aa432c460d5c5a89e
Randall <ran.dall@icloud.com>
2019-06-08 09:04:16 -0700

    Update inkdrop from 3.25.4 to 4.0.0

--- Casks/inkdrop.rb
+++ Casks/inkdrop.rb
@@ -1,6 +1,6 @@
 cask 'inkdrop' do
-  version '3.25.4'
-  sha256 '82fd6ba4aea97630b44bd2cb8dbba152aaacaa656c169328332a9741391283b2'
+  version '4.0.0'
+  sha256 '5d95fce48753218741efd523e2e30064411c8aacc1373a6aad1fbc59bfa8e88c'
 
   # d3ip0rje8grhnl.cloudfront.net was verified as official when first introduced to the cask
   url "https://d3ip0rje8grhnl.cloudfront.net/v#{version}/Inkdrop-#{version}-Mac.zip"
