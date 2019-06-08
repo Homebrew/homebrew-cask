30ca07dbb04619cf62b9d2bd399bdd475d25b102
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-08 09:11:05 -0700

    Update tiny-player from 1.2.9 to 1.2.10 (#64561)

--- Casks/tiny-player.rb
+++ Casks/tiny-player.rb
@@ -1,6 +1,6 @@
 cask 'tiny-player' do
-  version '1.2.9'
-  sha256 '9a08626732d73fcc3a5c09901cf8e06059ecc41181580588d410468f368f3190'
+  version '1.2.10'
+  sha256 '2ae80b4b6d704d3a97ab212f8374a85cd4e9a8412ae1ea58c2d5539bd99db1d9'
 
   url "http://download.catnapgames.com/TinyPlayer-#{version}.zip"
   appcast 'http://download.catnapgames.com/TinyPlayerAppcast.xml'
