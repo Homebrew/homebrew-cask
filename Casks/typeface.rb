69cca0654ba1680c2dc88a1ebbfea6e4bd67450c
Matthew Morek <matthew@madbit.co>
2019-06-08 14:56:28 +0100

    Add Typeface.app [latest]

---
+++ Casks/typeface.rb
@@ -0,0 +1,10 @@
+cask 'typeface' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://dcdn.typefaceapp.com/latest'
+  name 'Typeface'
+  homepage 'https://typefaceapp.com/'
+
+  app 'Typeface.app'
+end
