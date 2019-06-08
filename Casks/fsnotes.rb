8c98a8f0de8773751bf30ec50ff575a5771fb9f3
Julian Mayer <julian@corecode.io>
2019-06-08 15:41:31 +0200

    Update fsnotes from 3.3.0 to 3.3.1

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask 'fsnotes' do
-  version '3.3.0'
-  sha256 '52bcdb3a8d236525e567406c468bddaa9cbeba0dfbc1457bba748f52acbde590'
+  version '3.3.1'
+  sha256 '3d35d96859c7176b1ebda825f3f857cd6ea7e53d66a25fe81708418ade0448a7'
 
   # github.com/glushchenko/fsnotes was verified as official when first introduced to the cask
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
