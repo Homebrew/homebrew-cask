c1df7109a2e1d65fb8d0faf972c6cc8926787861
Matthew Morek <matthew@madbit.co>
2019-06-08 15:25:44 +0100

    [Typeface.app] Update version and sha356 sum

--- Casks/typeface.rb
+++ Casks/typeface.rb
@@ -1,6 +1,6 @@
 cask 'typeface' do
-  version :latest
-  sha256 :no_check
+  version '2.4.1'
+  sha256 'ce19dd3a317dd953d43c9a39bd2cd55bf87ac4e22548b5c840fb1cecc45f21b2'
 
   url 'https://dcdn.typefaceapp.com/latest'
   appcast 'https://dcdn.typefaceapp.com/appcast.xml'
