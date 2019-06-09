1d25298a41ad1019cb407dde2b4fb1305de15143
Work <brigitte@corecode.io>
2019-06-09 12:58:42 +0200

    Update remotix-agent from 1.0.6 to 1.1

--- Casks/remotix-agent.rb
+++ Casks/remotix-agent.rb
@@ -1,5 +1,5 @@
 cask 'remotix-agent' do
-  version '1.0.6'
+  version '1.1'
   sha256 '66ecec55fa40ead964ad8da472c99d4eda3a1cbf65e60e531f0fd790f5743b11'
 
   url 'https://downloads.remotixcloud.com/agent-mac/RemotixAgent.pkg'
