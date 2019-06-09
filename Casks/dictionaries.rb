aa235031c8ba2551130dd1432c681d88a26556f9
János Orcsik <orcsikj@gmail.com>
2019-06-09 15:33:48 +0200

    Update dictionaries to 1.3.3,305:1559998396 (#64586)

--- Casks/dictionaries.rb
+++ Casks/dictionaries.rb
@@ -1,6 +1,6 @@
 cask 'dictionaries' do
-  version '1.3.2,301:1555346620'
-  sha256 'ef661eae20858556282831391e49a57e31baec0a13081229ac7f69e8b4795909'
+  version '1.3.3,305:1559998396'
+  sha256 '730a524969d58c18434e797d15042f9e56d1ea365ebba85a7c3f7d5be3a9e409'
 
   # dl.devmate.com/io.dictionaries.Dictionaries was verified as official when first introduced to the cask
   url "https://dl.devmate.com/io.dictionaries.Dictionaries/#{version.after_comma.before_colon}/#{version.after_colon}/Dictionaries-#{version.after_comma.before_colon}.zip"
