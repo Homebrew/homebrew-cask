494b3d3a6dd611c26729e1e8d112a1ffec0f61c9
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-10 09:49:17 -0700

    Update dbeaver-enterprise from 6.0.0 to 6.1.0 (#64629)

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-enterprise' do
-  version '6.0.0'
-  sha256 'eac84353154ae0b21d633b8b7b6382200659a0cace78bb5ec5c1a56b89cfa4a7'
+  version '6.1.0'
+  sha256 '175e4a9a16d5c7ccd17b604c8dc10a1047be0965b47fc03d73b0b1249509448b'
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   appcast 'https://dbeaver.com/product/version.xml'
