5265233e599c5f6e3f2e0f27c6352a072ae55935
foriequal0 <foriequal@gmail.com>
2019-06-10 22:15:38 +0900

    Update gureumkim from 1.9.0c to 1.10.0 (#64602)
    * Update gureumkim from 1.9.0c to 1.10.0
    
    * Update gureumkim.rb

--- Casks/gureumkim.rb
+++ Casks/gureumkim.rb
@@ -1,14 +1,14 @@
 cask 'gureumkim' do
-  version '1.9.0c'
-  sha256 'eac1817137be39e0f36a9f2e2f0170c3eb3eeb6bf28ec25c5143c7de82a50050'
+  version '1.10.0'
+  sha256 '144f6625d95101a91fd176afd1ff4cf159d22b52cf522bdedba0b51097de2bbe'
 
   # github.com/gureum/gureum was verified as official when first introduced to the cask
-  url "https://github.com/gureum/gureum/releases/download/GureumKIM-#{version.delete('a-z')}/GureumKIM-#{version}.pkg"
+  url "https://github.com/gureum/gureum/releases/download/#{version}/Gureum-#{version}.pkg"
   appcast 'https://github.com/gureum/gureum/releases.atom'
   name '구름 입력기'
   homepage 'http://gureum.io/'
 
-  pkg "GureumKIM-#{version}.pkg"
+  pkg "Gureum-#{version}.pkg"
 
   uninstall pkgutil: 'org.youknowone.inputmethod.Gureum'
 end
