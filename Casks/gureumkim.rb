db0117f0f78336a787165254aebb5a6dfad7be1b
Vítor Galvão <info@vitorgalvao.com>
2019-06-10 14:08:36 +0100

    Update gureumkim.rb

--- Casks/gureumkim.rb
+++ Casks/gureumkim.rb
@@ -3,7 +3,7 @@ cask 'gureumkim' do
   sha256 '144f6625d95101a91fd176afd1ff4cf159d22b52cf522bdedba0b51097de2bbe'
 
   # github.com/gureum/gureum was verified as official when first introduced to the cask
-  url "https://github.com/gureum/gureum/releases/download/#{version.delete('a-z')}/Gureum-#{version}.pkg"
+  url "https://github.com/gureum/gureum/releases/download/#{version}/Gureum-#{version}.pkg"
   appcast 'https://github.com/gureum/gureum/releases.atom'
   name '구름 입력기'
   homepage 'http://gureum.io/'
