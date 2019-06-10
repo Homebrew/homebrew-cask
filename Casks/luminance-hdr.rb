b2c208076596e74224afb92b3f74e3aa8a0dbba3
suschizu <brigitte@corecode.io>
2019-06-10 22:29:32 +0200

    Update luminance-hdr.rb

--- Casks/luminance-hdr.rb
+++ Casks/luminance-hdr.rb
@@ -1,9 +1,12 @@
 cask 'luminance-hdr' do
-  version '2.5.1_1'
-  sha256 'be4ae6c1110bbac4fd57358e1a0489871ba143f4f295a6d07f95be9aaecb4d18'
+  version '2.6.0'
+  sha256 '14b201123036f826a3d4f2f2d5c00a7ef60938adf8f8cf35ca58f38ee8ef6642'
 
-  # downloads.sourceforge.net/qtpfsgui was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/qtpfsgui/Luminance_HDR_#{version}.dmg"
+  # master.dl.sourceforge.net/project/qtpfsgui was verified as official when first introduced to the cask
+  url "https://master.dl.sourceforge.net/project/qtpfsgui/luminance/#{version}/LuminanceHDR_#{version}-Qt5.13.dmg"
+  
+
+  
   appcast 'https://sourceforge.net/projects/qtpfsgui/rss'
   name 'Luminance HDR'
   homepage 'https://qtpfsgui.sourceforge.io/'
