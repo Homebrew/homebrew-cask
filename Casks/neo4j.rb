8c60cda64c66d7b1ef0f7f1ad03fa8907bc95213
CoreCode <core-code@users.noreply.github.com>
2019-06-10 19:16:04 +0200

    Update neo4j from 1.1.22 to 1.2.0 (#64633)

--- Casks/neo4j.rb
+++ Casks/neo4j.rb
@@ -1,7 +1,7 @@
 cask 'neo4j' do
   # note: "4" is not a version number, but an intrinsic part of the product name
-  version '1.1.22'
-  sha256 '62a9d79b9d146f2a4445a9625d4ece817ea7403341384cf295cd7424a24443bf'
+  version '1.2.0'
+  sha256 '842dcd8156619549dfc3e0025812643f8dfd4dbe533ae5eb7bdc212cff4d65ee'
 
   url "https://neo4j.com/artifact.php?name=neo4j-desktop-offline-#{version}.dmg"
   appcast 'https://neo4j.com/download/'
