408de3038933cf6e28b742228ac0ceacef71b198
Andy Li <andy@onthewings.net>
2019-06-11 20:14:03 +0800

    Update flash-player-debugger from 32.0.0.192 to 32.0.0.207 (#64650)
    * Update flash-player-debugger from 32.0.0.192 to 32.0.0.207
    
    * Update flash-player-debugger.rb

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,9 +1,10 @@
 cask 'flash-player-debugger' do
-  version '32.0.0.192'
-  sha256 '100f0687e10dc50f6d7b460e7674534b8f6314dce52f30cf1f276c20cdca7f72'
+  version '32.0.0.207'
+  sha256 'a76301f161cd57657673ca3e1173f60e51a0ec3635b30dc5e03bc601bb0b0110'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
-  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml'
+  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml',
+          configuration: version.tr('.', ',')
   name 'Adobe Flash Player projector content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
