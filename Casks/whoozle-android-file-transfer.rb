21b7f8252743f0ecf04c64940f2ffc115f6ceae5
Yurii Kolesnykov <root@yurikoles.com>
2019-06-11 19:55:41 +0300

    whoozle-android-file-transfer: conflicts with android-file-transfer only in runtime (#64666)
    * whoozle-android-file-transfer: conflicts with android-file-transfer only in runtime
    
    * Update whoozle-android-file-transfer.rb

--- Casks/whoozle-android-file-transfer.rb
+++ Casks/whoozle-android-file-transfer.rb
@@ -8,10 +8,7 @@ cask 'whoozle-android-file-transfer' do
   name 'Android File Transfer'
   homepage 'https://whoozle.github.io/android-file-transfer-linux/'
 
-  conflicts_with cask: [
-                         'android-file-transfer',
-                         'whoozle-android-file-transfer-nightly',
-                       ]
+  conflicts_with cask: 'whoozle-android-file-transfer-nightly'
 
   app 'Android File Transfer for Linux.app'
   binary "#{appdir}/Android File Transfer for Linux.app/Contents/SharedSupport/bin/aft-mtp-cli"
