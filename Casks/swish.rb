dba89fb86283778a49c8c31216a4ff2e5cac2fba
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-11 10:37:25 -0700

    Update swish from 1.0.0 to 1.0.1 (#64681)

--- Casks/swish.rb
+++ Casks/swish.rb
@@ -1,6 +1,6 @@
 cask 'swish' do
-  version '1.0.0'
-  sha256 '3869a417458d4b9085894596be6e9b939a7d6d58ed74fb6e2d810e3c1e999f2b'
+  version '1.0.1'
+  sha256 '6fa3ab7f37704855c97cd52b854ed9b0ee69787497930011a43da261c0dcaeaf'
 
   url "https://highlyopinionated.co/swish/Swish-#{version}.zip"
   appcast 'https://highlyopinionated.co/swish/appcast.xml'
