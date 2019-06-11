c75848340826413a5d685fdee4cf877a3e0e9f8f
Vítor Galvão <info@vitorgalvao.com>
2019-06-11 12:35:14 +0100

    Update flash-player.rb

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -3,7 +3,8 @@ cask 'flash-player' do
   sha256 '9d6bd9bd3cb2a320ff5862f1532df4767d691fe53b1d6b03890970856ca78ca2'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
-  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pl.xml'
+  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pl.xml',
+          configuration: version.tr('.', ',')
   name 'Adobe Flash Player projector'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
