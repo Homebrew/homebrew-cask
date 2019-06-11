d43b57d305a0eefd03cccb431e7a0ba2a4379dd8
Vítor Galvão <info@vitorgalvao.com>
2019-06-11 12:35:23 +0100

    Update flash-player-debugger-npapi.rb

--- Casks/flash-player-debugger-npapi.rb
+++ Casks/flash-player-debugger-npapi.rb
@@ -3,7 +3,8 @@ cask 'flash-player-debugger-npapi' do
   sha256 'e2ce64798055b721f95d3c1db7664d17fb02fc3585ffe3a12ef6e0939e111add'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_plugin_debug.dmg"
-  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pl.xml'
+  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pl.xml',
+          configuration: version.tr('.', ',')
   name 'Adobe Flash Player NPAPI (plugin for Safari and Firefox) content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
