e25ede55b9b8b6725e70f6c086e17aaf01d12511
Vítor Galvão <info@vitorgalvao.com>
2019-06-11 12:35:48 +0100

    Update flash-player-debugger.rb

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -3,7 +3,8 @@ cask 'flash-player-debugger' do
   sha256 'a76301f161cd57657673ca3e1173f60e51a0ec3635b30dc5e03bc601bb0b0110'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
-  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml'
+  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml',
+          configuration: version.tr('.', ',')
   name 'Adobe Flash Player projector content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
