9fdceca949bc32506c1cbad7dd94856454548fa4
Vítor Galvão <info@vitorgalvao.com>
2019-06-11 12:35:31 +0100

    Update flash-player-debugger-ppapi.rb

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -3,7 +3,8 @@ cask 'flash-player-debugger-ppapi' do
   sha256 'b1fbe7cfcb4997b6e6c0b2e3301a011223a9f8b8aefd7a65a6b1f9049c048cf6'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
-  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml'
+  appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml',
+          configuration: version.tr('.', ',')
   name 'Adobe Flash Player PPAPI (plugin for Opera and chromium) content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
