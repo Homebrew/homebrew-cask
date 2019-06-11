8fb1665194c587b1eb3a12e727204801ee548727
Randall <ran.dall@icloud.com>
2019-06-11 09:50:22 -0700

    Update sigil from 0.9.13 to 0.9.14

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -1,6 +1,6 @@
 cask 'sigil' do
-  version '0.9.13'
-  sha256 'ebbace26cb597da3f605b1042c492fc230ec549c63c84cf111d88587525a413c'
+  version '0.9.14'
+  sha256 'd8d96cfc95301cbaf5f3d0d8da11b060d2a82334cf1aa86fe65472eb4a2dd2be'
 
   # github.com/Sigil-Ebook/Sigil was verified as official when first introduced to the cask
   url "https://github.com/Sigil-Ebook/Sigil/releases/download/#{version}/Sigil-#{version}-Mac-Package.dmg"
