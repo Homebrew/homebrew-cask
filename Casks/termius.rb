c1308cf3353470b4ecd9ef82934858ef879e9eb9
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2019-06-12 19:56:45 +0200

    Update termius from 4.5.0 to 4.7.2 (#64706)
    * Update termius from 4.5.0 to 4.7.2
    
    * Update termius.rb

--- Casks/termius.rb
+++ Casks/termius.rb
@@ -1,10 +1,10 @@
 cask 'termius' do
-  version '4.5.0'
-  sha256 'ab7a4625cfc10e6872bbdf232cbc8b55e67f40634b8f9475ad593de66cf311e1'
+  version '4.7.2'
+  sha256 '97f727edbd6a61f04c96a1e502570d89d391057c8f6cc5d0310c2d0785d2ae02'
 
   # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/termius.desktop.autoupdate/mac/Termius.dmg'
-  appcast 'https://docs.termius.com/changelog/desktop'
+  appcast 'https://www.termius.com/mac-os'
   name 'Termius'
   homepage 'https://www.termius.com/'
 
