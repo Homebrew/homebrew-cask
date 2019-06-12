4587a895ae53cf03bae73106b30eb5937d446ff5
Oliver Waldhorst <oliver.waldhorst@hs-karlsruhe.de>
2019-06-12 17:34:49 +0200

    Update banking-4 from 7.0.4,7076 to 7.0.5,7101

--- Casks/banking-4.rb
+++ Casks/banking-4.rb
@@ -1,7 +1,7 @@
 cask 'banking-4' do
   # note: "4" is not a version number, but an intrinsic part of the product name
-  version '7.0.4,7076'
-  sha256 '524ea9b544f879dd85917fdb44dc31bef626293b0778c05f0308f797527f321a'
+  version '7.0.5,7101'
+  sha256 '23acd8e93544bb1f45ca6610e1905c41c181c60122e283756e0e5deb2df516d6'
 
   url 'https://subsembly.com/download/MacBanking4.pkg'
   appcast 'https://subsembly.com/banking4-macos-updates.php'
