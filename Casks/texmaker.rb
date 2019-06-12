17fb58f330df8fa117eb1b7f94054f0a257ab9d5
CoreCode <core-code@users.noreply.github.com>
2019-06-12 18:13:47 +0200

    Update texmaker (#64713)

--- Casks/texmaker.rb
+++ Casks/texmaker.rb
@@ -3,7 +3,7 @@ cask 'texmaker' do
   sha256 '024e9c3ad09743b6914e2a1bfed0fceb5eb8000d8f7f78360d02884299d526ca'
 
   url "http://www.xm1math.net/texmaker/assets/files/texmaker-#{version}.dmg"
-  appcast 'http://www.xm1math.net/texmaker/log.html'
+  appcast 'http://www.xm1math.net/texmaker/assets/files/'
   name 'Texmaker'
   homepage 'http://www.xm1math.net/texmaker/'
 
