33e56b32897da84776410e36971a4e81ddbc483f
Tomáš Holý <vantomas@vantomas.net>
2019-06-12 17:54:51 +0200

    Update navicat-premium from 12.1.22 to 12.1.23 (#64734)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium' do
-  version '12.1.22'
-  sha256 '5b82b28d73edf66178fc27435a243aa49a859cf0139dde9d9203d8c89ed4be64'
+  version '12.1.23'
+  sha256 '4a8611f2654b7d1e3c563d40c9ad16a584acd7693030f959101e862900725003'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20Premium&appLang=en'
