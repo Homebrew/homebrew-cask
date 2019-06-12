adec9bd7623f3034a3bc5a6ad065a69db8660b1b
Tomáš Holý <vantomas@vantomas.net>
2019-06-12 17:39:15 +0200

    Update navicat-for-oracle from 12.1.22 to 12.1.23 (#64730)

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-oracle' do
-  version '12.1.22'
-  sha256 '1bcc9f2e21b0563fbc0bae115f38083588c2797668f618dd0e954e6c27c5caaf'
+  version '12.1.23'
+  sha256 '5bb7a46a5f3df3e7ee9449fd531bf26eaeb4493319d783d289efe190857317d6'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20for%20Oracle&appLang=en'
