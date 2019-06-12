a9525718c28eb55f4aee26fe0500827a0fe6e4ec
CoreCode <core-code@users.noreply.github.com>
2019-06-12 18:13:54 +0200

    Update understand from 5.1.981 to 5.1.989 (#64712)
    * Update understand from 5.1.981 to 5.1.989
    
    * Update understand.rb
    
    * Update understand.rb
    
    * Update understand.rb

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,9 +1,10 @@
 cask 'understand' do
-  version '5.1.981'
-  sha256 '5b72bba4814aa56e0f18f9730b47f70bd56c3da2e4e619bc20560499c231dbea'
+  version '5.1.989'
+  sha256 'f562bfe30005ed484ba99c9b75d4bfd8a66937e217dfc8b235ea2f19cc2bde30'
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
-  appcast 'https://scitools.com/build-notes/'
+  appcast 'https://scitools.com/build-notes/',
+          configuration: version.patch
   name 'SciTools Understand'
   homepage 'https://scitools.com/features/'
 
