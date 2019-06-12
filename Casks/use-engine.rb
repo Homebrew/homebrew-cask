a5022d6d1664ddd4e996a4e74f592496c445c003
Paul Mucur <mudge@mudge.name>
2019-06-12 13:19:35 +0100

    Update use-engine from 2.1.2 to 2.1.3

--- Casks/use-engine.rb
+++ Casks/use-engine.rb
@@ -1,6 +1,6 @@
 cask 'use-engine' do
-  version '2.1.2'
-  sha256 'ad87d78fbd99a4c6d2c8b70787a16efea20352578d060365b84f75651fa4711b'
+  version '2.1.3'
+  sha256 '8f6176a398176ccf9457b8cd855cdc2ae7dbcfc0002ae683e8a44d3ee93f6a56'
 
   url "https://repository.use-together.com/stable/use-engine/macos/#{version.major}.x/#{version}/use-engine.dmg"
   name 'USE Engine'
