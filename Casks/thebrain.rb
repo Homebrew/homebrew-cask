fc49e3761e0c8e01760c74de366823f054a297aa
Randall <ran.dall@icloud.com>
2019-06-12 09:53:20 -0700

    Update thebrain from 10.0.48.0 to 10.0.50.0

--- Casks/thebrain.rb
+++ Casks/thebrain.rb
@@ -1,6 +1,6 @@
 cask 'thebrain' do
-  version '10.0.48.0'
-  sha256 '8fc3f33fa0ee35655a5bffe3e68272b68cae81bcb2b4cc67fa813b413a058903'
+  version '10.0.50.0'
+  sha256 '32eab7a6baa5887abd875a860ab4efdf9b0f4a63e84d35eedecc1244a28d0440'
 
   url "http://updater.thebrain.com/files/TheBrain#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://salesapi.thebrain.com/?a=doDirectDownload%26id=10000'
