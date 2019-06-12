2a0b9bfc13524ad1fdd5039e3dae3df69a2fa13d
CoreCode <core-code@users.noreply.github.com>
2019-06-12 19:57:33 +0200

    Update googleappengine from 1.9.84 to 1.9.86 (#64767)

--- Casks/googleappengine.rb
+++ Casks/googleappengine.rb
@@ -1,10 +1,10 @@
 cask 'googleappengine' do
-  version '1.9.84'
-  sha256 '222226649685d31703a4771faf48f5bbb546bc267bd36750c5ec5917fc2dd7ff'
+  version '1.9.86'
+  sha256 '2e673098fb2a7b3f810ff904dd13905e8a4720e956c7558c10af2c96e334aeea'
 
   # storage.googleapis.com/appengine-sdks was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/appengine-sdks/featured/GoogleAppEngineLauncher-#{version}.dmg"
-  appcast 'https://storage.googleapis.com/appengine-sdks'
+  appcast 'https://storage.googleapis.com/appengine-sdks/featured/VERSION'
   name 'Google App Engine'
   homepage 'https://cloud.google.com/appengine/docs/standard/python/download#appengine_sdk/'
 
