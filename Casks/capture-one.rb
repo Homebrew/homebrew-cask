76d0ef76128f4b45a9b3c0d13849310dcf1de8c0
Luis R <lri@me.com>
2019-06-13 20:19:29 +0200

    Update capture-one from 12.0.4.21 to 12.1.0 (#64834)

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -1,6 +1,6 @@
 cask 'capture-one' do
-  version '12.0.4.21'
-  sha256 '4b63fe1c0d72371f66594c80343fe52bed5898d4df331eb81a5b7849031773c6'
+  version '12.1.0'
+  sha256 '57bd9a4bd598869f282ccb79a93e401912adbfd69a33560a5a6cc3ed4910e812'
 
   url "https://downloads.phaseone.com/d972230a-e941-47ca-a751-35f57a3f2d94/International/CaptureOne.Mac.#{version}.dmg"
   name 'Capture One'
