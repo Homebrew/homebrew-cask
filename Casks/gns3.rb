b2116942613a642a5510768b091a22b4550770a0
Randall <ran.dall@icloud.com>
2019-06-14 09:24:29 -0700

    Update gns3 from 2.1.20 to 2.1.21

--- Casks/gns3.rb
+++ Casks/gns3.rb
@@ -1,7 +1,7 @@
 cask 'gns3' do
   # note: "3" is not a version number, but an intrinsic part of the product name
-  version '2.1.20'
-  sha256 '45ae610b779269968b03d863e54124f65034c0ce70c448a7f98b5e7f262cd1da'
+  version '2.1.21'
+  sha256 '455adf7d65fb4411e1e6481cdbc1a41cd251112dc55cbcdbd983e75045f0c89d'
 
   # github.com/GNS3/gns3-gui was verified as official when first introduced to the cask
   url "https://github.com/GNS3/gns3-gui/releases/download/v#{version}/GNS3-#{version}.dmg"
