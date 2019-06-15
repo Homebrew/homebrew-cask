f63dedee3dc99132f2f01644353f75537b07e639
cclauss <cclauss@me.com>
2019-06-15 09:32:51 +0200

    Use print() function in both Python 2 and Python 3 (#64852)

--- developer/cgi/check_url_redirect.py
+++ developer/cgi/check_url_redirect.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
+from __future__ import print_function
 import sys
 import cgi
-import urllib2
+try:  # Python 3
+  from urllib.request import Request, urlopen
+except NameError:  # Python 2
+  from urllib2 import Request, urlopen
 
 
 url = sys.argv[1]
@@ -11,11 +15,11 @@ if len(sys.argv) > 2:
     headers = {'User-Agent': ua}
 
 try:
-    req = urllib2.Request(url, None, headers)
-    response = urllib2.urlopen(req)
+    req = Request(url, None, headers)
+    response = urlopen(req)
     redir = response.geturl()
 except Exception:
     import traceback
     redir = 'generic exception: ' + traceback.format_exc()
 
-print redir
+print(redir)
