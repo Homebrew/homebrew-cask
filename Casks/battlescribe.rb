8d357fdbe41ad26ce6b5e8f7eaf3afd5620bbaa7
Janek Łukaszewicz <janluk@google.com>
2019-06-16 12:42:39 +0200

    Add BattleScribe.app 2.02.04

---
+++ Casks/battlescribe.rb
@@ -0,0 +1,10 @@
+cask 'battlescribe' do
+  version '2.02.04'
+  sha256 '5cdddc9881f4911279fd74fee71b4a1ff408682d3aeaf5641d53cb8a3095e4e9'
+
+  url "https://battlescribe.net/files/BattleScribe_#{version}_Installer.dmg"
+  name 'BattleScribe'
+  homepage 'https://battlescribe.net/'
+
+  app 'BattleScribe.app'
+end
