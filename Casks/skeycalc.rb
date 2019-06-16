47dac6b7ececcac7bff3deaddf825c13a984064c
Chris Lambertus <clambertus@gmail.com>
2019-06-15 17:11:11 -0700

    Add SkeyCalc v3.0
    An RFC-2289 compliant OTP response generator (S/Key Calculator).
    Designed for Mac OS X. Keychain-compliant.

---
+++ Casks/skeycalc.rb
@@ -0,0 +1,11 @@
+cask 'skeycalc' do
+  version '3.0'
+  sha256 'bf04dc48de81023f1ba9bbe5986eb8b6699ceb1c3575e32c45d42cdec8bf316f'
+
+  # http://www.orange-carb.org/SkeyCalc/
+  url 'http://quux.orange-carb.org/dist/SkeyCalc-3.0.dmg'
+  name 'SkeyCalc'
+  homepage 'http://www.orange-carb.org/SkeyCalc/'
+
+  app 'SkeyCalc.app'
+end
