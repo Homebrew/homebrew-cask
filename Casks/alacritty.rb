f2bb0dda0b53bddcfeb7af39cdaa0d0afa1cb1f6
Svend Andersen <SV3A@users.noreply.github.com>
2019-06-16 19:28:16 +0200

    Update alacritty from 0.3.3-rc2 to 0.3.3

--- Casks/alacritty.rb
+++ Casks/alacritty.rb
@@ -1,6 +1,6 @@
 cask 'alacritty' do
-  version '0.3.3-rc2'
-  sha256 '4fbcba810c9247dbfe4378320a5126cddaf27ea32e391b34caf1ba8636e1a84f'
+  version '0.3.3'
+  sha256 '0e53bee6e52356273db6aa9a51247c028e07cf61c2c9be98908594d49a67465c'
 
   url "https://github.com/jwilm/alacritty/releases/download/v#{version}/Alacritty-v#{version}.dmg"
   appcast 'https://github.com/jwilm/alacritty/releases.atom'
