83b23dedf956a6b99206b69b6eb1bd8aab3327aa
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-16 14:23:44 -0700

    Update netnewswire from 5.0a3 to 5.0a4 (#64922)

--- Casks/netnewswire.rb
+++ Casks/netnewswire.rb
@@ -1,6 +1,6 @@
 cask 'netnewswire' do
-  version '5.0a3'
-  sha256 '576a7bdb059827875d23b6e0cb721a40d70ceb19d8e84177f679a611c2fe1de0'
+  version '5.0a4'
+  sha256 'a85ec1a3f7dddf35934443926dc7138b6429b0673908e0cbcbeec2c53134859b'
 
   url "https://ranchero.com/downloads/NetNewsWire#{version}.zip"
   appcast 'https://ranchero.com/downloads/netnewswire-beta.xml'
