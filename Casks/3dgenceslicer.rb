75de6e4fcc480a05c33e94d2be8ba81044fbe524
Vítor Galvão <info@vitorgalvao.com>
2019-06-16 13:29:54 +0100

    Update and rename 3dgence-slicer.rb to 3dgenceslicer.rb

--- Casks/3dgenceslicer.rb
+++ Casks/3dgenceslicer.rb
@@ -1,8 +1,8 @@
-cask '3dgence-slicer' do
-  version '1.3.2'
+cask '3dgenceslicer' do
+  version '1.3.2_r1405'
   sha256 'd03233bc7d999f3df93ccc3fdcfb8ae3ef5b099b7fa970d6ed4cd13a7535c7f7'
 
-  url "https://3dgence.com/downloads/3DGence_Slicer_#{version}_r1405.dmg"
+  url "https://3dgence.com/downloads/3DGence_Slicer_#{version}.dmg"
   name '3DGence Slicer'
   homepage 'https://3dgence.com/'
 
