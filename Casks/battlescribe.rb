771a68ff464b9b53370d6976101a529d820b8a1c
Vítor Galvão <info@vitorgalvao.com>
2019-06-16 13:29:16 +0100

    Update battlescribe.rb

--- Casks/battlescribe.rb
+++ Casks/battlescribe.rb
@@ -3,8 +3,10 @@ cask 'battlescribe' do
   sha256 '5cdddc9881f4911279fd74fee71b4a1ff408682d3aeaf5641d53cb8a3095e4e9'
 
   url "https://battlescribe.net/files/BattleScribe_#{version}_Installer.dmg"
+  appcast 'https://battlescribe.net/?tab=downloads'
   name 'BattleScribe'
   homepage 'https://battlescribe.net/'
 
   app 'BattleScribe.app'
+  suite 'BattleScribe Tools'
 end
