095c13220672c17fcd2c146a3d000c6a7ed84798
Janek Łukaszewicz <janluk@google.com>
2019-06-16 12:53:23 +0200

    Add 3DGenceSlicer.app 1.3.2

---
+++ Casks/3dgence-slicer.rb
@@ -0,0 +1,10 @@
+cask '3dgence-slicer' do
+  version '1.3.2'
+  sha256 'd03233bc7d999f3df93ccc3fdcfb8ae3ef5b099b7fa970d6ed4cd13a7535c7f7'
+
+  url "https://3dgence.com/downloads/3DGence_Slicer_#{version}_r1405.dmg"
+  name '3DGence Slicer'
+  homepage 'https://3dgence.com/'
+
+  app '3DGenceSlicer.app'
+end
