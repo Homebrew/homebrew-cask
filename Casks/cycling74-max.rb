0089cb80da506a558a775fd873bba091b0794321
chohner <mail@chohner.com>
2019-06-17 13:27:34 +0200

    fix cycling74-max 8.0.6_190611 sha hash

--- Casks/cycling74-max.rb
+++ Casks/cycling74-max.rb
@@ -1,6 +1,6 @@
 cask 'cycling74-max' do
   version '8.0.6_190611'
-  sha256 '0cb01d888810863020006dda7e0dc5c3e985730d67721ba954179526b13f89bc'
+  sha256 '2d7bf4dfb26a1ec2fdaa6896669a270fa8966d6982f550a9bf4bbd199e46ef28'
 
   # akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max#{version.no_dots}.dmg"
