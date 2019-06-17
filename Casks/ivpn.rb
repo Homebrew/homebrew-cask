8c6f168cc3e935f2c34389078096ce70ab0cb2af
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-17 16:32:39 -0700

    Update ivpn from 2.9.8 to 2.9.9 (#64950)

--- Casks/ivpn.rb
+++ Casks/ivpn.rb
@@ -1,6 +1,6 @@
 cask 'ivpn' do
-  version '2.9.8'
-  sha256 '9d54b1d2123ac76306b3ee5900b3b6e4a0b755091d0f03882f1bf0953ae8a1ad'
+  version '2.9.9'
+  sha256 'c7237eebd7307069f5b181524f8ae709faf3dfe150191d19fbc6d9f1ba5060b5'
 
   url "https://cdn.ivpn.net/releases/osx/IVPN-#{version}.dmg"
   appcast 'https://www.ivpn.net/updates/mac/sparkle/ivpn_mac_appcast.xml'
