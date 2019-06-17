aec323630805243a22735bb5877eff97f3ac28cb
Randall <ran.dall@icloud.com>
2019-06-17 14:30:20 -0700

    Update itunes-volume-control from 1.6.5 to 1.6.6

--- Casks/itunes-volume-control.rb
+++ Casks/itunes-volume-control.rb
@@ -1,6 +1,6 @@
 cask 'itunes-volume-control' do
-  version '1.6.5'
-  sha256 '5ed3b145bcdca69fd6cda58098304e8084beb6629579ed91bdc98ba85c6179cf'
+  version '1.6.6'
+  sha256 '6256c424b8c815ba1d9f50c91869c8a161db202ff0d4fc6573d9d896da5b3a65'
 
   # uni-bonn.de/alberti/iTunesVolumeControl was verified as official when first introduced to the cask
   url "http://quantum-technologies.iap.uni-bonn.de/alberti/iTunesVolumeControl/iTunesVolumeControl-v#{version}.zip"
