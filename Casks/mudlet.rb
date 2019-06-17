e6806b208fea28cba3a1204ffcaac4ae29a5660d
suschizu <brigitte@corecode.io>
2019-06-17 15:13:33 +0200

    Update mudlet from 3.20.1 to 3.21.0 (#64942)

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -1,6 +1,6 @@
 cask 'mudlet' do
-  version '3.20.1'
-  sha256 '27c20c4fc104dab72468b099c21ebc87066a4b3aa473a224c79540ff584b13c2'
+  version '3.21.0'
+  sha256 '56969a16a191d7a6ae34893d65265b785f9c887790f7c87247fe766e5b9aa0c3'
 
   url "https://www.mudlet.org/download/Mudlet-#{version}.dmg"
   appcast 'https://github.com/Mudlet/Mudlet/releases.atom'
