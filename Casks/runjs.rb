ba53a292b0ff4209c1c62f85183dafcfb57f01ac
Randall <ran.dall@icloud.com>
2019-06-18 13:34:35 -0700

    Update runjs from 1.5.2 to 1.5.3

--- Casks/runjs.rb
+++ Casks/runjs.rb
@@ -1,6 +1,6 @@
 cask 'runjs' do
-  version '1.5.2'
-  sha256 '72e39b0baecab0c9b7e8e73814b54a141ed10c650091043ee6597c275587504e'
+  version '1.5.3'
+  sha256 '40350b0b404778a8ab7da4dabcd2b226585fee3b08299a87ba325505fecdf0ba'
 
   # github.com/lukehaas/runjs was verified as official when first introduced to the cask
   url "https://github.com/lukehaas/runjs/releases/download/v#{version}/RunJS-#{version}.dmg"
