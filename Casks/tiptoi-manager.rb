622887c42d437f9c5e444f353bb22b96f61c8500
JasperV <1297464+JasperV@users.noreply.github.com>
2019-06-18 11:18:54 +0200

    Fix style in tiptoi® Manager.app Cask

--- Casks/tiptoi-manager.rb
+++ Casks/tiptoi-manager.rb
@@ -3,16 +3,16 @@ cask 'tiptoi-manager' do
   sha256 :no_check
 
   # tiptoidata.s3-de.object.vdc.interoute.com was verified as official when first introduced to the cask
-  url "http://tiptoidata.s3-de.object.vdc.interoute.com/installer_flat/tiptoi_Manager_Installer.pkg"
+  url 'http://tiptoidata.s3-de.object.vdc.interoute.com/installer_flat/tiptoi_Manager_Installer.pkg'
   name 'tiptoi® Manager'
   homepage 'https://www.ravensburger.org/nl/ontdek/tiptoi/index.html'
 
   pkg 'tiptoi_Manager_Installer.pkg'
 
-  uninstall quit: 'com.ravensburger.tiptoimanager',
-            delete: '/Applications/tiptoi® Manager.app',
-            pkgutil: [
-              'com.ravensburger.tiptoimanager',
-              'com.ravensburger.tiptoi.TipToiBindings'
-            ]
+  uninstall quit:     'com.ravensburger.tiptoimanager',
+            delete:   '/Applications/tiptoi® Manager.app',
+            pkgutil:  [
+                        'com.ravensburger.tiptoimanager',
+                        'com.ravensburger.tiptoi.TipToiBindings',
+                      ]
 end
