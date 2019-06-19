15dadca633c65180b4aae8adf1461b4af8593186
Gabriel Leventhal <gabriel@leventhal.ch>
2019-06-19 08:39:37 -0400

    App Tracer.app v1.7.1

---
+++ Casks/tracer.rb
@@ -0,0 +1,16 @@
+cask 'tracer' do
+  version '1.7.1'
+  sha256 '21b7a4bbd1a4ed1f5c501dbaf70bb85a53ff660af598ec5c9308f1e3d4f71af0'
+
+  # github.com was verified as official when first introduced to the cask
+  url "https://github.com/beast-dev/tracer/releases/download/v#{version}/Tracer.v#{version}.dmg"
+  appcast 'https://github.com/beast-dev/tracer/releases.atom'
+  name 'Tracer'
+  homepage 'https://beast.community/tracer'
+
+  app "Tracer v#{version}.app"
+
+  caveats do
+    depends_on_java
+  end
+end
