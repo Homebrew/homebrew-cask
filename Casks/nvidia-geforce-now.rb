b5299ccf9d1d4114946f2eb4656d153bc534e265
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-18 17:26:25 -0700

    Update nvidia-geforce-now from 2.0.8.74,767C12 to 2.0.8.78,1592B5 (#64991)

--- Casks/nvidia-geforce-now.rb
+++ Casks/nvidia-geforce-now.rb
@@ -1,6 +1,6 @@
 cask 'nvidia-geforce-now' do
-  version '2.0.8.74,767C12'
-  sha256 'f0fb61856d142294fe29aca6251adb9a08ca0d1468bf4d9132fda8697c501912'
+  version '2.0.8.78,1592B5'
+  sha256 'dfdb9025505e5d6912a4f801640a8df9a394d6c22af3f8e2dddfdf7c6359891e'
 
   url "https://ota-downloads.nvidia.com/ota/GeForceNOW-release_#{version.after_comma}.dmg"
   appcast 'https://ota.nvidia.com/release/available?product=GFN-mac&version=1.17.2.0&channel=OFFICIAL'
