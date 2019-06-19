6bc7b80926feec8d5b6f4a76f2e1c51e71431dc3
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-18 21:21:17 -0700

    Update ridibooks from 2.6.3 to 2.6.4 (#64996)

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -1,6 +1,6 @@
 cask 'ridibooks' do
-  version '2.6.3'
-  sha256 '8b46f1746c48519c4d05495793347e8ffeabdd7da3f7adbebbce2f469d946a70'
+  version '2.6.4'
+  sha256 'e6e40614ceea3057f40ac2f45c069164a8ccdea66945d29b4ab1ea1a92cc8840'
 
   url "https://viewer-ota.ridibooks.com/mac/ridibooks-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://getapp.ridibooks.com/mac'
