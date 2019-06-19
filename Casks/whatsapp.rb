6abb80332f077adca5ac0290a6351db03d11b3a7
tienbuiDE <mail@tienbui.de>
2019-06-19 19:30:14 +0200

    Update whatsapp from 0.3.3328 to 0.3.3330

--- Casks/whatsapp.rb
+++ Casks/whatsapp.rb
@@ -1,6 +1,6 @@
 cask 'whatsapp' do
-  version '0.3.3328'
-  sha256 'e3ea29bf862653a010d604fde664618c6e5e0051005c736f6d4014dfe20c9fd8'
+  version '0.3.3330'
+  sha256 '486b8d7a2d3ca863679ee5d78eeea854c32c3f9fed56c3d7c11d3fa599db5e57'
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   appcast 'https://web.whatsapp.com/desktop/mac/releases?platform=darwin&arch=x64'
