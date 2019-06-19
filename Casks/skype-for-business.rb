28fd2ae49256cc249ca499e9d369bfe6abd2dfd2
CoreCode <core-code@users.noreply.github.com>
2019-06-19 15:46:31 +0200

    Update skype-for-business from 16.26.0.19 to 16.27.0.36 (#64975)

--- Casks/skype-for-business.rb
+++ Casks/skype-for-business.rb
@@ -1,6 +1,6 @@
 cask 'skype-for-business' do
-  version '16.26.0.19'
-  sha256 '8b7b4e2b233f7be1dab1b5fecb1a373cb82a12b607331d3a62bee418a49071f3'
+  version '16.27.0.36'
+  sha256 'c934111b95142b47c1e4f93876b79290a655e9824e29aa13ba2cdedfc7d28515'
 
   url "https://download.microsoft.com/download/D/0/5/D055DA17-C7B8-4257-89A1-78E7BBE3833F/SkypeForBusinessInstaller-#{version}.pkg"
   appcast 'https://www.microsoft.com/en-us/download/details.aspx?id=54108'
