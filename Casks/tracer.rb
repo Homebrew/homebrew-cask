7e70527d1f1d1079dd067127ab17fbd63ec82f18
Vítor Galvão <info@vitorgalvao.com>
2019-06-19 14:39:36 +0100

    Update tracer.rb

--- Casks/tracer.rb
+++ Casks/tracer.rb
@@ -2,7 +2,7 @@ cask 'tracer' do
   version '1.7.1'
   sha256 '21b7a4bbd1a4ed1f5c501dbaf70bb85a53ff660af598ec5c9308f1e3d4f71af0'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/beast-dev/tracer was verified as official when first introduced to the cask
   url "https://github.com/beast-dev/tracer/releases/download/v#{version}/Tracer.v#{version}.dmg"
   appcast 'https://github.com/beast-dev/tracer/releases.atom'
   name 'Tracer'
