f4b3db80c6b95fd4ebbce1d9a4d219d8e3d4a409
Anver Housseini <contact@anver.ch>
2019-06-20 08:27:38 +0200

    Update zeplin from 2.4.3,702 to 2.5,717 (#65059)

--- Casks/zeplin.rb
+++ Casks/zeplin.rb
@@ -1,6 +1,6 @@
 cask 'zeplin' do
-  version '2.4.3,702'
-  sha256 '18a92cf56ad0754d428028e13e2974ab774b162601da099afb72cf45b5f2e4bb'
+  version '2.5,717'
+  sha256 '39c4d60e605a4a5ae72d72ccc308bfefec4333e780b4e8d16c25d3d9f03422ed'
 
   url 'https://api.zeplin.io/urls/download-mac'
   appcast 'https://rink.hockeyapp.net/api/2/apps/8926efffe734b6d303d09f41d90c34fc'
