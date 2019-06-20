b76da56e768a1d9b20d5e75514c73a743f8560bd
suschizu <brigitte@corecode.io>
2019-06-20 13:13:37 +0200

    Update cinebench.rb

--- Casks/cinebench.rb
+++ Casks/cinebench.rb
@@ -1,8 +1,8 @@
 cask 'cinebench' do
-  version 'R20'
+  version 'R20,281795'
   sha256 'c2ee9b2dab04cdd8fc1b1a03bb2303cdaf1495cbc2614cd2f3fa56cd8151e47e'
 
-  url "https://http.maxon.net/pub/cinebench/Cinebench#{version}.dmg"
+  url "https://http.maxon.net/pub/cinebench/Cinebench#{version.before_comma}.dmg"
   name 'Cinebench'
   homepage 'https://www.maxon.net/products/cinebench/'
 
