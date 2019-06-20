904c7947e724563984aff2396df564814b6b6253
Work <brigitte@corecode.io>
2019-06-20 13:37:38 +0200

    Update macx-dvd-ripper-pro from 6.2.2,20190521 to 6.2.3,20190619

--- Casks/macx-dvd-ripper-pro.rb
+++ Casks/macx-dvd-ripper-pro.rb
@@ -1,6 +1,6 @@
 cask 'macx-dvd-ripper-pro' do
-  version '6.2.2,20190521'
-  sha256 '05d2e94458609ef12097c3f5962da52c0cb52e4e267b4f3bccae3f80f9ef63b5'
+  version '6.2.3,20190619'
+  sha256 'c8a4b3b4657bde31a5b2510c1a99caaf6a175491090c46d35250a02d17aa0a67'
 
   url 'https://www.macxdvd.com/download/macx-dvd-ripper-pro.dmg'
   appcast 'https://www.macxdvd.com/mac-dvd-ripper-pro/upgrade/macx-dvd-ripper-pro'
