384838d4f8ee129731a7c1e4b610bb1fbcdd995d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-19 20:00:12 -0700

    Add `flarq` version number to `version`

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -1,5 +1,5 @@
 cask 'fldigi' do
-  version '4.1.04'
+  version '4.1.04,4.3.7'
   sha256 '31d5f23f5898f7ca89d1e99f84c175e33ec90b572d7d2397fb3628d20c391111'
 
   url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version.before_comma}_x86_64.dmg"
