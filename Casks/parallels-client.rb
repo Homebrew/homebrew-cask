4ed26a5e123a66afc142cc8824d534f26b8d1b28
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-20 07:16:24 -0700

    Update `appcast` (credit: @mightyfree)

--- Casks/parallels-client.rb
+++ Casks/parallels-client.rb
@@ -3,7 +3,7 @@ cask 'parallels-client' do
   sha256 '8cb12254b5b40ed5207f9d63807c47d0654c49c60c23382963249059b8e9949f'
 
   url "https://download.parallels.com/ras/v#{version.major}/#{version.hyphens_to_dots}/RasClient-Mac-#{version}.pkg"
-  appcast "https://download.parallels.com/ras/v#{version}/RAS%20Client%20for%20Mac%20Changelog.txt"
+  appcast "https://download.parallels.com/ras/v#{version.major}/RAS%20Client%20for%20Mac%20Changelog.txt"
   name 'Parallels Client'
   homepage 'https://www.parallels.com/products/ras/features/rdp-client/'
 
