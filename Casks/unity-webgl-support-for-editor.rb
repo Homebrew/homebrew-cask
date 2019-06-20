465db8945456af0149e00f2e435e79211eb3ddfc
suschizu <brigitte@corecode.io>
2019-06-20 20:46:49 +0200

    Update unity-webgl-support-for-editor from 2019.1.5f1,0ca0f5646614 to 2019.1.7f1,f3c4928e5742 (#65106)

--- Casks/unity-webgl-support-for-editor.rb
+++ Casks/unity-webgl-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-webgl-support-for-editor' do
-  version '2019.1.5f1,0ca0f5646614'
-  sha256 'e7d8f91f3097c2c2dbcadc9be39da35c8fc714f34d373da990c179d3becfb760'
+  version '2019.1.7f1,f3c4928e5742'
+  sha256 'd2d78268d159f882625a2a515a4d7e95e6ca4c338b44b0667f8fda3abafdb40b'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
