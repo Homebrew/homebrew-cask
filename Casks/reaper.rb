004b18f3a191d966d179bb58af9e0e0b6a8485c5
Vítor Galvão <info@vitorgalvao.com>
2019-06-20 18:28:47 +0100

    Update reaper.rb

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,9 +1,10 @@
 cask 'reaper' do
-  version '5.979'
+  version '5.97.900'
   sha256 'dd87ab39b73db930b3876d3d18208732fa3550645a5a72b6a1962d95958eb120'
 
-  url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots}_x86_64.dmg"
-  appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64'
+  url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots.sub(%r{0*$}, '')}_x86_64.dmg"
+  appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64',
+          configuration: "#{version.major}.#{version.minor}#{version.patch.sub(%r{0*$}, '')}"
   name 'REAPER'
   homepage 'https://www.reaper.fm/'
 
