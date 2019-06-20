e5e9d99971d36dd2e11c9500d440839875d60f85
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-19 19:01:13 -0700

    Update mailspring from 1.6.1 to 1.6.2 (#65029)

--- Casks/mailspring.rb
+++ Casks/mailspring.rb
@@ -1,6 +1,6 @@
 cask 'mailspring' do
-  version '1.6.1'
-  sha256 'a6ab898bbb966705a760e8ebb1960eab55fa5ae3ef7282a5332ab76576f33bc9'
+  version '1.6.2'
+  sha256 'a9b138865653cc39d02f2402d80378bb6f49175b4abaa0d49b21f5c79d519b77'
 
   # github.com/Foundry376/Mailspring was verified as official when first introduced to the cask
   url "https://github.com/Foundry376/Mailspring/releases/download/#{version}/Mailspring.zip"
