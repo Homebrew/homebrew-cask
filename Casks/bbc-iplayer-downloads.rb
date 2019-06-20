e6dd814d109d62b5dd522e1bbe2c64d17a2bfc34
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-19 19:58:56 -0700

    Update bbc-iplayer-downloads from 2.8.6 to 2.8.10 (#65043)

--- Casks/bbc-iplayer-downloads.rb
+++ Casks/bbc-iplayer-downloads.rb
@@ -1,6 +1,6 @@
 cask 'bbc-iplayer-downloads' do
-  version '2.8.6'
-  sha256 '2372eeef43aa12f2aabcc0ad28a8de8024be8bb850ea39894e5f2d17a855b92e'
+  version '2.8.10'
+  sha256 '1de1a3ab9d7c1571b08d416a282894b171a4a15118789edae8bc2aead8cf9bc3'
 
   # live-downloads-app-bucket-staticassetsbucket-ydn3z4ggyaof.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://live-downloads-app-bucket-staticassetsbucket-ydn3z4ggyaof.s3.amazonaws.com/releases/darwin-x64/BBCiPlayerDownloads-#{version}.dmg"
