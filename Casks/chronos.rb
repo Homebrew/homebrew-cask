22da73787eefa639724ca456f28491252101e247
Randall <ran.dall@icloud.com>
2019-06-19 18:47:51 -0700

    Update chronos from 3.2.4 to 3.2.5

--- Casks/chronos.rb
+++ Casks/chronos.rb
@@ -1,6 +1,6 @@
 cask 'chronos' do
-  version '3.2.4'
-  sha256 '8abeee610d4b1e369746119597c11c3c3595cfebb216eec4d78773a627fe93ba'
+  version '3.2.5'
+  sha256 '5dd38500185043d82c0b5d579de0595aeab9de976648e3ac441f7dc8aa498365'
 
   url "https://github.com/web-pal/chronos-timetracker/releases/download/v#{version}/Chronos-#{version}-mac.zip"
   appcast 'https://github.com/web-pal/chronos-timetracker/releases.atom'
