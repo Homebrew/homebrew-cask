0a71171f095aac5f6d9b9355f683e76d5cb10e93
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-20 07:41:56 -0700

    Update downie from 3.7.6,2029 to 3.7.7,2033 (#65084)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.7.6,2029'
-  sha256 '461088c42c9f90569a25a27ba5e7626bbb54cec03fdc17362e82df1dddd4f7c1'
+  version '3.7.7,2033'
+  sha256 'c362426902200cb55f14e11b125c988c3fb989b214a3fb38b018ad4ab05989be'
 
   url "https://trial.charliemonroe.net/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.zip"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml"
