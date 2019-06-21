dcee5a36da4f47c0cd3805f44860ffd4fef136fc
Randall <ran.dall@icloud.com>
2019-06-21 08:37:22 -0700

    Update jd-gui from 1.6.0 to 1.6.1

--- Casks/jd-gui.rb
+++ Casks/jd-gui.rb
@@ -1,6 +1,6 @@
 cask 'jd-gui' do
-  version '1.6.0'
-  sha256 'd8288b16136cb57ca02e60b5d867d27b0beaa09451d339b0bf666a18cf0b3f89'
+  version '1.6.1'
+  sha256 '4f0c0ed6afbf6097559722756e689fe3018ffb386661350466f13bcec97e0601'
 
   # github.com/java-decompiler/jd-gui was verified as official when first introduced to the cask
   url "https://github.com/java-decompiler/jd-gui/releases/download/v#{version}/jd-gui-osx-#{version}.tar"
