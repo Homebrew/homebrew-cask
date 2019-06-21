4dda4d7fc43a4a8422bdb424561180830afa1071
Stephane Paquet <spaquet@up4b.com>
2019-06-21 13:10:45 -0700

    Update electron from 5.0.4 to 5.0.5

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '5.0.4'
-  sha256 '2d6dc206cc2c241377071272402bfe6b28323ec4dfe482b260a6259d57d2a9c7'
+  version '5.0.5'
+  sha256 'd14b45e16618251515a2fcb2301abab1cc105aa42e6a24cd7f6c4f0370b166fa'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
