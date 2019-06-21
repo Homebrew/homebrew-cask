08e085f2effa1299e0ca5e517fc392ee7b5aa820
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-21 09:41:58 -0700

    Update jamovi from 1.0.1.0 to 1.0.2.0 (#65149)

--- Casks/jamovi.rb
+++ Casks/jamovi.rb
@@ -1,6 +1,6 @@
 cask 'jamovi' do
-  version '1.0.1.0'
-  sha256 '0e32b79504e43843ac189167a79b320c616bc25456bbb1e745d3f418856f6459'
+  version '1.0.2.0'
+  sha256 '00ea85c183ac5198e22cd372d9d5dabab9bb924498def9dd81e5d79595f616a6'
 
   url "https://www.jamovi.org/downloads/jamovi-#{version}-macos.dmg"
   appcast 'https://www.jamovi.org/download.html'
