487bd0d233a46f87dee907ea87693622653c7bb4
suschizu <brigitte@corecode.io>
2019-06-21 17:26:06 +0200

    Update upwork.rb

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -1,5 +1,5 @@
 cask 'upwork' do
-  version '5.2.3.757,5_2_3_757_3idn3cdxmlq3m6d5'
+  version '3.1.9,5_2_3_757_3idn3cdxmlq3m6d5'
   sha256 'd7da1baf90469e23cb3f23da0151a8b897126205998d67502f7f9440386d6800'
 
   url "https://updates-desktopapp.upwork.com/binaries/v#{version.after_comma}/Upwork.dmg"
