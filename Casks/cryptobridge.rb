a01ca5c0a7500dc333dfbe67f15b208cdd8b20c3
Work <brigitte@corecode.io>
2019-06-21 15:58:32 +0200

    Update cryptobridge from 0.2.4 to 0.2.6

--- Casks/cryptobridge.rb
+++ Casks/cryptobridge.rb
@@ -1,6 +1,6 @@
 cask 'cryptobridge' do
-  version '0.2.4'
-  sha256 '8c97ff700971f1045312e01d772e91e03a991923bb054088f6d077cb6aa7b298'
+  version '0.2.6'
+  sha256 '8494f7c327684681307c0069315ed2375238e98e615a9b26db34480b72a5e384'
 
   url "https://github.com/CryptoBridge/cryptobridge-ui/releases/download/v#{version}/CryptoBridge-#{version}.dmg"
   appcast 'https://github.com/CryptoBridge/cryptobridge-ui/releases.atom'
