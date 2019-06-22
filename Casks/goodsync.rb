50000b1e2a53b983d4d9131dad251c0a7083e4c5
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-22 16:48:45 -0700

    Update goodsync from 10.9.34.4 to 10.9.35.5 (#65185)
    * Update goodsync from 10.9.34.4 to 10.9.35.5
    
    * Update goodsync.rb

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,9 +1,10 @@
 cask 'goodsync' do
-  version '10.9.34.4'
-  sha256 'ca1487a6767c63a810da3f317298bd32dcf63a2a4f45f14ae7c7fb260a80d173'
+  version '10.9.35.5'
+  sha256 'c073d3cbd5b46c9b4222c8495c06f88815521bde25241a0f1148d80ef9843dd8'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
-  appcast 'https://www.goodsync.com/news-mac'
+  appcast 'https://www.goodsync.com/news-mac',
+          configuration: version.major_minor_patch
   name 'GoodSync'
   homepage 'https://www.goodsync.com/'
 
