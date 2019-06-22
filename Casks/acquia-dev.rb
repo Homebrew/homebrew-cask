b6e7449ac7a69d9a77feea34c3a6e4bd237392d3
Vítor Galvão <info@vitorgalvao.com>
2019-06-22 12:04:32 +0100

    Update acquia-dev.rb

--- Casks/acquia-dev.rb
+++ Casks/acquia-dev.rb
@@ -2,7 +2,7 @@ cask 'acquia-dev' do
   version '2.2019-06.06'
   sha256 '27caf4ac1cc89314a2f89f1d463a784d224d7950792a46735bb3dc6ffa18f47f'
 
-  url "https://dev.acquia.com/sites/default/files/file/#{version.minor}/AcquiaDevDesktop-#{version}.dmg"
+  url "https://dev.acquia.com/sites/default/files/file/#{version.minor}/AcquiaDevDesktop-#{version.dots_to_hyphens}.dmg"
   appcast 'https://dev.acquia.com/downloads'
   name 'Acquia Dev Desktop'
   homepage 'https://www.acquia.com/drupal/acquia-dev-desktop'
