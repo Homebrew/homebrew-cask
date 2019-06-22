e6b6f0acbdacc5db4763718920a5c3356060b735
Randall <ran.dall@icloud.com>
2019-06-22 10:33:37 -0700

    Update farrago from 1.2.4 to 1.2.5

--- Casks/farrago.rb
+++ Casks/farrago.rb
@@ -1,6 +1,6 @@
 cask 'farrago' do
-  version '1.2.4'
-  sha256 '2a10dd26d4d727e371d5edf682568b2b35c309639380fc84797e34f760ba2ed7'
+  version '1.2.5'
+  sha256 '9ef80c9861495a45516abf8d630a34a6e67b989a24028a0e23a35f485d5364c2'
 
   url 'https://rogueamoeba.com/farrago/download/Farrago.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.farrago&system=10140'
