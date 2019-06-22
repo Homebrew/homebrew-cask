ca9aa4d6a01072bbcd704d2b8798eb13b020e2c7
Vítor Galvão <info@vitorgalvao.com>
2019-06-23 00:33:00 +0100

    Update fontbase.rb

--- Casks/fontbase.rb
+++ Casks/fontbase.rb
@@ -3,7 +3,8 @@ cask 'fontbase' do
   sha256 'aa9fcd3907800ad1f953a6015fefe4c23542331a5a5f71418b82a829e6d64d9b'
 
   url "https://releases.fontba.se/mac/FontBase-#{version}.dmg"
-  appcast 'https://fontba.se/updates'
+  appcast 'https://fontba.se/updates',
+          configuration: version.major_minor
   name 'FontBase'
   homepage 'https://fontba.se/'
 
