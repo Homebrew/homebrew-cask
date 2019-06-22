ae35f9016a41859befb6058d04db0bd71b22d5bd
Randall <ran.dall@icloud.com>
2019-06-22 12:19:30 -0700

    Update nosqlbooster-for-mongodb from 5.1.8 to 5.1.9

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -1,6 +1,6 @@
 cask 'nosqlbooster-for-mongodb' do
-  version '5.1.8'
-  sha256 'd3a6218771a5e0d46cf7332bbf1f9a6539d40f7dd27198509637c4b9d55228a0'
+  version '5.1.9'
+  sha256 '02dc75060a5cf1469955f915d519161160bb7b8df1c7d1aa6347fc2bc725c2d3'
 
   url "https://nosqlbooster.com/s3/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
   appcast 'https://nosqlbooster.com/downloads'
