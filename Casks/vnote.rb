1ac52bb69cf938138bda73b88edc288c8456014f
Randall <ran.dall@icloud.com>
2019-06-22 11:08:38 -0700

    Update vnote from 2.6 to 2.7

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -1,6 +1,6 @@
 cask 'vnote' do
-  version '2.6'
-  sha256 '8c101c0af9d002628fc2b2d3684c6f89c11f512c99f98d8662fc947d3b5f2204'
+  version '2.7'
+  sha256 'ad6ce63e5b87e9915e4c28c1d6628567b7391b24ff38a6ef67618d5224c603c4'
 
   # github.com/tamlok/vnote was verified as official when first introduced to the cask
   url "https://github.com/tamlok/vnote/releases/download/v#{version}/VNote-#{version}-x64.dmg"
