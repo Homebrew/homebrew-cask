8364d2c4789c56ee1329c398cc18331fcb1efbe0
suschizu <brigitte@corecode.io>
2019-06-23 01:48:37 +0200

    Update fontbase from 2.8.2 to 2.8.3 (#65099)
    * Update fontbase from 2.8.2 to 2.8.3
    
    * Update fontbase.rb

--- Casks/fontbase.rb
+++ Casks/fontbase.rb
@@ -1,9 +1,10 @@
 cask 'fontbase' do
-  version '2.8.2'
-  sha256 'e120cff005b12b8452e928bfd16cba7e535a7a2603d231ca30063952dfe9930c'
+  version '2.8.3'
+  sha256 'aa9fcd3907800ad1f953a6015fefe4c23542331a5a5f71418b82a829e6d64d9b'
 
   url "https://releases.fontba.se/mac/FontBase-#{version}.dmg"
-  appcast 'https://fontba.se/updates'
+  appcast 'https://fontba.se/updates',
+          configuration: version.major_minor
   name 'FontBase'
   homepage 'https://fontba.se/'
 
