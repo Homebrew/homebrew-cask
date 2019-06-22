c537d2e14c402be3e114bd70052ca8291ea151ea
Randall <ran.dall@icloud.com>
2019-06-22 11:27:41 -0700

    Update touchdesigner from 099.2019.15840 to 099.2019.16600

--- Casks/touchdesigner.rb
+++ Casks/touchdesigner.rb
@@ -1,6 +1,6 @@
 cask 'touchdesigner' do
-  version '099.2019.15840'
-  sha256 '8e29d065c21915f72261e2ad678c31df673ba23f7493f3ac638a7330a3821658'
+  version '099.2019.16600'
+  sha256 'd10e67535300864d98ed02db42abff132001694b03d39ef05e0c87cd4aba6ab9'
 
   url "https://www.derivative.ca/Builds/TouchDesigner#{version}.dmg"
   appcast "https://www.derivative.ca/#{version.major}/Downloads/Default.asp"
