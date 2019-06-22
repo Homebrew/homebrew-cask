2b569e4590d6f8c691dee2d990fafc81a33c1620
Randall <ran.dall@icloud.com>
2019-06-22 10:36:09 -0700

    Update netshade from 8.0.1 to 8.1

--- Casks/netshade.rb
+++ Casks/netshade.rb
@@ -1,6 +1,6 @@
 cask 'netshade' do
-  version '8.0.1'
-  sha256 '0c0c680e1084219110f16e2782f0641ffe7c84a7d42bc797aee8ecb03873bad2'
+  version '8.1'
+  sha256 '476e5361f06e848dfdb3563c358b98abe9d11d9cbf7fa360de6e53193d658941'
 
   url "https://secure.raynersw.com/downloads/NetShade-#{version.dots_to_hyphens}.app.zip"
   appcast 'https://www.raynersw.com/appcast.php'
