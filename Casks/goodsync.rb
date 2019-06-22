c970a19ad7ce28fcdc03321b49014883ac9b09ed
Vítor Galvão <info@vitorgalvao.com>
2019-06-23 00:39:13 +0100

    Update goodsync.rb

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -3,7 +3,8 @@ cask 'goodsync' do
   sha256 'c073d3cbd5b46c9b4222c8495c06f88815521bde25241a0f1148d80ef9843dd8'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
-  appcast 'https://www.goodsync.com/news-mac'
+  appcast 'https://www.goodsync.com/news-mac',
+          configuration: version.major_minor_patch
   name 'GoodSync'
   homepage 'https://www.goodsync.com/'
 
