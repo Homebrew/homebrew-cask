4663383ac179dcc343cf44ae13b7bd95a677d7d7
iskeld <iskeld@gmail.com>
2019-06-23 11:15:16 +0200

    Update clockify from 2.2.6.73 to 2.2.7.77

--- Casks/clockify.rb
+++ Casks/clockify.rb
@@ -1,8 +1,9 @@
 cask 'clockify' do
-  version '2.2.6.73'
-  sha256 'd83bcfb29c79418eb76150d7c01a54f0321a4bc25269fbc92d88ac32a3c82531'
+  version '2.2.7.77'
+  sha256 '9ee5279aedf19b9026d51ab0e3e27741ecb518caff932ef21ed5eaafc246acb0'
 
-  url "https://clockify.me/downloads/ClockifyDesktop_#{version.no_dots}.zip"
+  # clockify-resources.s3.eu-central-1.amazonaws.com was verified as official when first introduced to the cask
+  url "https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/ClockifyDesktop_#{version.no_dots}.zip"
   name 'Clockify'
   homepage 'https://clockify.me/mac-time-tracking'
 
