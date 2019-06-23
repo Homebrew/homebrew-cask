e9db5c24e1a40bafa29b9541f62a2eab34f3457e
Chen Gong <chen.sst@gmail.com>
2019-06-23 22:46:05 +0800

    Update squirrel from 0.13.0 to 0.14.0

--- Casks/squirrel.rb
+++ Casks/squirrel.rb
@@ -1,6 +1,6 @@
 cask 'squirrel' do
-  version '0.13.0'
-  sha256 'c713150157344f02b2efba2852059d22373e6b3cd33ca6e7e028d7c480be3d84'
+  version '0.14.0'
+  sha256 'e7bc74ed1f0447a48b8c86f3685d8c6d0640d39895e6e4550488beeb4f870926'
 
   # dl.bintray.com/rime/squirrel was verified as official when first introduced to the cask
   url "https://dl.bintray.com/rime/squirrel/Squirrel-#{version}.zip"
