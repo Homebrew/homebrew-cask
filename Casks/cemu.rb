d3db8e6dd969c2a993656a6f4d2d4e6b790b88dc
Steven Conaway <steven.conaway@icloud.com>
2019-06-22 19:39:34 -0700

    Update CEmu to v1.3 (#65194)

--- Casks/cemu.rb
+++ Casks/cemu.rb
@@ -1,6 +1,6 @@
 cask 'cemu' do
-  version '1.2'
-  sha256 'c1c5cbf0e5099287a312d8a4a4f3005ff4a5caadce15ad4e28252f873d61c2ff'
+  version '1.3'
+  sha256 'db28a0497ea944a6118f869b50268d5f8c3730c37367033eeebc7cdec08fd60c'
 
   # github.com/CE-Programming/CEmu was verified as official when first introduced to the cask
   url "https://github.com/CE-Programming/CEmu/releases/download/v#{version}/macOS_CEmu.dmg"
