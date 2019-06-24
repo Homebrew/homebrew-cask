5d589e3286ce2f63a839dcb4a7c11b8d4bf1d405
Julian Mayer <julian@corecode.io>
2019-06-25 01:11:38 +0200

    Update remember-the-milk

--- Casks/remember-the-milk.rb
+++ Casks/remember-the-milk.rb
@@ -3,6 +3,7 @@ cask 'remember-the-milk' do
   sha256 '1a7e84d1bf77373b2dc5b32abde04bf6f0a079b990a9c12ab3f59fda58fccb12'
 
   url "https://www.rememberthemilk.com/download/mac/RememberTheMilk-#{version}.zip"
+  appcast 'https://www.rememberthemilk.com/services/mac/'
   name 'Remember The Milk'
   homepage 'https://www.rememberthemilk.com/'
 
