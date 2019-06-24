707fd23eec2f040cfb335ca052f78f87fab68923
Vítor Galvão <info@vitorgalvao.com>
2019-06-24 23:08:02 +0100

    Update wiznote.rb

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -3,7 +3,8 @@ cask 'wiznote' do
   sha256 'c12a79f12287013d9f817674d11c18e054ab82d3c55605db5d39d06267c2f3f9'
 
   url "https://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://url.wiz.cn/u/mac'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://url.wiz.cn/u/mac',
+          configuration: version.after_comma
   name 'WizNote'
   homepage 'https://www.wiz.cn/wiznote-mac.html'
 
