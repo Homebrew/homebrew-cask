111ee398a4803577632160326bcf564a2494cf47
Randall <ran.dall@icloud.com>
2019-06-25 08:50:29 -0700

    Update openinterminal from 0.10.1 to 0.10.2

--- Casks/openinterminal.rb
+++ Casks/openinterminal.rb
@@ -1,6 +1,6 @@
 cask 'openinterminal' do
-  version '0.10.1'
-  sha256 '08cbdab28ade7d7541487fc7779a1e6f72851ac485ebac4b8ba8f4002e1766ef'
+  version '0.10.2'
+  sha256 'f21b167961031dd9ae6f431a23427ba2dceb37b8b3313ab26f24ce90bd8db1f8'
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInTerminal.app.zip"
   appcast 'https://github.com/Ji4n1ng/OpenInTerminal/releases.atom'
