79f55543b8c1cc7193ce86e63d0c52d67c6112c0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-25 08:57:42 -0700

    Update hstracker from 1.5.9 to 1.6.0 (#65265)

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -1,6 +1,6 @@
 cask 'hstracker' do
-  version '1.5.9'
-  sha256 'dad2e1e115bdeb598733f5e66eadf24e41d59115e6f4db348ea2a68fa1bbc0ef'
+  version '1.6.0'
+  sha256 'afd0ccfa870f6c0c3fce79731b7aa3fc15b3451863162f9910f056f9b8a2381a'
 
   # github.com/HearthSim/HSTracker was verified as official when first introduced to the cask
   url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip"
