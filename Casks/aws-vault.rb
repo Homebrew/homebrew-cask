9c30f3261de8c5da4c79e085139c9720c554c5f2
Matthew Hodgkins <matthodge@gmail.com>
2019-06-25 08:07:16 +0200

    Upgrade aws-vault to version 4.6.1

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,6 +1,6 @@
 cask 'aws-vault' do
-  version '4.6.0'
-  sha256 'bba76d9a314cd69cc0bcf12d94bff4d1ff624d74b404b1e423f25ce97c05f854'
+  version '4.6.1'
+  sha256 '81e97fb532489429737ac06490e61aaa8e9911ce9885770aba9cad9b5db7a5f6'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64"
   appcast 'https://github.com/99designs/aws-vault/releases.atom'
