1dfbd5b47c7c792d838a8a8431a6b55640052e01
CoreCode <core-code@users.noreply.github.com>
2019-06-25 16:05:11 +0200

    Update robo-3t (#65243)

--- Casks/robo-3t.rb
+++ Casks/robo-3t.rb
@@ -3,6 +3,7 @@ cask 'robo-3t' do
   sha256 'a853ab5f61ee839540a0f43cfcd545c8115355d639b6d038a62ddf08c94a41b9'
 
   url "https://download-test.robomongo.org/mac/robo3t-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg"
+  appcast 'https://robomongo.org/download'
   name 'Robo 3T (formerly Robomongo)'
   homepage 'https://robomongo.org/'
 
