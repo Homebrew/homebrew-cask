29b5c0f14424261343118dda9234b8b04bca0fa2
suschizu <brigitte@corecode.io>
2019-06-26 16:12:34 +0200

    Update pinegrow from 5.5 to 5.6 (#65330)

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -1,6 +1,6 @@
 cask 'pinegrow' do
-  version '5.5'
-  sha256 '437b42e4c1f6f8eddf219b36b73166abd52ec5d29f1a199682e41565e4b7076c'
+  version '5.6'
+  sha256 '21b68d9cff80ea439c81b880ba956942a4a8df83048510c087b1b51e79e0500b'
 
   url "http://download.pinegrow.com/PinegrowMac.#{version}.dmg"
   appcast 'https://pinegrow.com/'
