41f2366d38d36d011f9070c7ddc5a53fce4436ec
CoreCode <core-code@users.noreply.github.com>
2019-06-26 18:45:11 +0200

    Update wowmatrix (#65319)

--- Casks/wowmatrix.rb
+++ Casks/wowmatrix.rb
@@ -2,7 +2,7 @@ cask 'wowmatrix' do
   version :latest
   sha256 :no_check
 
-  url 'https://swupdate.wowmatrix.com/mac/WowMatrix.zip'
+  url 'http://swupdate.wowmatrix.com/mac/WowMatrix.zip'
   name 'WowMatrix'
   homepage 'https://www.wowmatrix.com/'
 
