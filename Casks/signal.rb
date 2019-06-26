6d19ed7bb6ee573d94152e98fa3354dc6213de76
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-26 09:24:55 -0700

    Update signal from 1.25.1 to 1.25.2 (#65344)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,6 +1,6 @@
 cask 'signal' do
-  version '1.25.1'
-  sha256 'b8e2b2be6ae492d122ff69418308ac2449a209a3cafcfdda7412b00415362e22'
+  version '1.25.2'
+  sha256 '7fbc2de138f43501cebbc0d4c002bfdc8b013debe80ba18bcf32de6f6c5e8759'
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.zip"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
