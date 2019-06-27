926a1d0fb33446a73cecdcbb7818b7dd9fc495bc
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-27 09:38:16 -0700

    Update scapple from 1.3.2 to 1.3.3 (#65393)

--- Casks/scapple.rb
+++ Casks/scapple.rb
@@ -1,6 +1,6 @@
 cask 'scapple' do
-  version '1.3.2'
-  sha256 'c4799d55939e635e42cb67b588f41a8a5fb58efeb768ca36d455072828dc7172'
+  version '1.3.3'
+  sha256 '2693a64a4eed698fa64d2d772a6224a1b2a4c3c88c06af0fa7c5584d53fab8c7'
 
   # scrivener.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://scrivener.s3.amazonaws.com/Scapple.dmg'
