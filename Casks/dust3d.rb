7966d318f92ad7068bf728fdd5f8d2c8ee54952c
Work <brigitte@corecode.io>
2019-06-27 17:44:13 +0200

    Update dust3d from 1.0.0-beta.16 to 1.0.0-beta.19

--- Casks/dust3d.rb
+++ Casks/dust3d.rb
@@ -1,6 +1,6 @@
 cask 'dust3d' do
-  version '1.0.0-beta.16'
-  sha256 '6ad9e135cf9f35fefd2b15ac1b7277f5002a2d6102404be9a81b95906633771b'
+  version '1.0.0-beta.19'
+  sha256 '33e0fcc4dc4ce6786e4750def044fb13de32e2cc9b8daa3f080b85eab78ef42c'
 
   # github.com/huxingyi/dust3d was verified as official when first introduced to the cask
   url "https://github.com/huxingyi/dust3d/releases/download/#{version}/dust3d-#{version}.dmg"
