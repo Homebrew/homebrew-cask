2f86eaed2941d5bd00a1607f2f2277d54c5a25c8
Julian Mayer <julian@corecode.io>
2019-06-28 01:15:53 +0200

    Update clip-studio-paint

--- Casks/clip-studio-paint.rb
+++ Casks/clip-studio-paint.rb
@@ -3,6 +3,7 @@ cask 'clip-studio-paint' do
   sha256 'c005010ca84d0aa88503aa9a81a89cc3ebe6f762533a713dceacd769391f5669'
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
+  appcast 'https://www.clipstudio.net/en/dl'
   name 'CLIP STUDIO PAINT'
   homepage 'https://www.clipstudio.net/en'
 
