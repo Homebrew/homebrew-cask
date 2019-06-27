2cf7aff1441cb8927453b85b311354ab1f1bdf38
CoreCode <core-code@users.noreply.github.com>
2019-06-28 01:02:06 +0200

    Update cineplay.rb

--- Casks/cineplay.rb
+++ Casks/cineplay.rb
@@ -1,5 +1,5 @@
 cask 'cineplay' do
-  version '1.5.5.7'
+  version '1.5.7.0'
   sha256 '831cc5d13ead51c4189fb991c68b3dbb4c86e36d46118014cc5694481b4e8948'
 
   url "https://www.digitalrebellion.com/download/cineplay?version=#{version.no_dots}"
