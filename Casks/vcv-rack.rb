d0365af9ec2d6301b7581b9a3c15f85af0a1135b
CoreCode <core-code@users.noreply.github.com>
2019-06-27 17:53:36 +0200

    Update vcv-rack from 1.0.0 to 1.1.0 (#65380)

--- Casks/vcv-rack.rb
+++ Casks/vcv-rack.rb
@@ -1,6 +1,6 @@
 cask 'vcv-rack' do
-  version '1.0.0'
-  sha256 'd6ddb5abaae7de6ff5d47df95fd5e49ade6d168fe8685799b79abe457c151273'
+  version '1.1.0'
+  sha256 'd98a494c0259b0a1e1f6e9bada29318ef251bdd1922a884320958c3a1c9f42c3'
 
   url "https://vcvrack.com/downloads/Rack-#{version}-mac.zip"
   appcast 'https://github.com/VCVRack/Rack/releases.atom'
