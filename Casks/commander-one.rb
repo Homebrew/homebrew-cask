62f70485e1d2d136599ee4ff9775fb436ee4b45b
Work <brigitte@corecode.io>
2019-06-27 17:43:13 +0200

    Update commander-one from 1.7.4 to 2.0

--- Casks/commander-one.rb
+++ Casks/commander-one.rb
@@ -1,6 +1,6 @@
 cask 'commander-one' do
-  version '1.7.4'
-  sha256 '817b8268fb115b15eda107d304f05dc42185e7dc6b3a803cfdbf99a57b9626e1'
+  version '2.0'
+  sha256 'e6e09e6aa7fc22d98cb3fe219fe623f6fc277c159c3930f8c14543da822dd386'
 
   url 'https://cdn.eltima.com/download/commander.dmg'
   appcast 'https://cdn.eltima.com/download/commander-update/settings.xml'
