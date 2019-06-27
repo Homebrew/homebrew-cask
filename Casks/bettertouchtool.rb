830c5cade03c48b30c87647a645d610bd7125456
ahousseini <admin@anver-mac.local>
2019-06-27 10:01:04 +0200

    Update bettertouchtool from 3.086 to 3.088

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -6,8 +6,8 @@ cask 'bettertouchtool' do
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}_final_10_9.zip"
   else
-    version '3.086'
-    sha256 'c0813657084dfabc960615fe4653ad07bd90dcf74e20b26558321a7467f22728'
+    version '3.088'
+    sha256 '9a889718f9b43fe98a5b3c0a1fd0f4fe2a41a8287b2f519d97c364ada66f8071'
 
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}.zip"
