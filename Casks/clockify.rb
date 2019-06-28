69a859ef7d433c55241c7dfda648a78ba73df7f5
CoreCode <core-code@users.noreply.github.com>
2019-06-28 14:19:55 +0200

    Update clockify from 2.2.7.77 to 2.2.7.79 (#65440)

--- Casks/clockify.rb
+++ Casks/clockify.rb
@@ -4,6 +4,8 @@ cask 'clockify' do
 
   # clockify-resources.s3.eu-central-1.amazonaws.com was verified as official when first introduced to the cask
   url "https://clockify-resources.s3.eu-central-1.amazonaws.com/downloads/ClockifyDesktop_#{version.no_dots}.zip"
+  appcast 'https://clockify.me/mac-time-tracking',
+          configuration: version.no_dots
   name 'Clockify'
   homepage 'https://clockify.me/mac-time-tracking'
 
