8db347f662b218d157248a19694947af540ec009
CoreCode <core-code@users.noreply.github.com>
2019-06-28 12:27:22 +0200

    Update pdfelement.rb

--- Casks/pdfelement.rb
+++ Casks/pdfelement.rb
@@ -3,7 +3,7 @@ cask 'pdfelement' do
   sha256 '97896572661c3c9baa7c4fbe60c6d78baeada672a0087e1c4e3287a8275ba599'
 
   url "http://download.wondershare.com/cbs_down/mac-pdfelement_full#{version.after_comma}.dmg"
-  appcast 'http://cbs.wondershare.com/go.php?m=upgrade_info&pid=5237&version=7.0.0'
+  appcast 'https://cbs.wondershare.com/go.php?m=upgrade_info&pid=5237&version=7.0.0'
   name 'Wondershare PDFelement for Mac'
   homepage 'https://pdf.wondershare.com/'
 
