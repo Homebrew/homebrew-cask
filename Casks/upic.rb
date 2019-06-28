16035c2d92ae71e616a45e8c1b25a5b35d47cd13
alley <alley.ma@qq.com>
2019-06-28 20:16:46 +0800

    Update upic from 0.6.0 to 0.6.1 (#65448)
    * Update upic from 0.6.0 to 0.6.1
    
    * Update upic.rb

--- Casks/upic.rb
+++ Casks/upic.rb
@@ -1,13 +1,16 @@
 cask 'upic' do
-  version '0.6.0'
-  sha256 'c9dac6e4a5703b5dcb48d37828ef608ca788d2d927b75fb230a4b80b290501df'
+  version '0.6.1'
+  sha256 'e018a0c61d17b500b8bf79ff5672fa47994e7aaa015526983247a112fcbfa2f5'
 
-  url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic_v#{version}.dmg"
+  url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic.dmg"
   appcast 'https://github.com/gee1k/uPic/releases.atom'
   name 'uPic'
   homepage 'https://github.com/gee1k/uPic'
 
   app 'UPic.app'
 
-  zap trash: '~/Library/Preferences/com.svend.uPic.plist'
+  zap trash: [
+               '~/Library/Preferences/com.svend.uPic.plist',
+               '~/Library/Caches/com.svend.uPic',
+             ]
 end
