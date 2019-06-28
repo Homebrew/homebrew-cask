44fe7e7458cb2a9a209b07b80b8f3a763d64f319
Randall <ran.dall@icloud.com>
2019-06-28 09:02:05 -0700

    Update bunch from 1.1.3,29 to 1.1.4,30

--- Casks/bunch.rb
+++ Casks/bunch.rb
@@ -1,6 +1,6 @@
 cask 'bunch' do
-  version '1.1.3,29'
-  sha256 '1730dd20cb0da4f0ac55f7ed9c9fd73b861870134a74b60bacc29affb5d74106'
+  version '1.1.4,30'
+  sha256 '780bdd519496b9b7c36bd4cdc3be65e1df889f5926fad88d6a1d3644a45bd179'
 
   url "https://cdn3.brettterpstra.com/updates/bunch/Bunch#{version.before_comma}#{version.after_comma}.dmg"
   appcast 'https://brettterpstra.com/updates/bunch/appcast.xml'
