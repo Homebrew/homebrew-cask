5edcf05c9911a1fac0058033f17f0bc932be191c
CoreCode <core-code@users.noreply.github.com>
2019-06-28 18:02:09 +0200

    Update qt3dstudio.rb

--- Casks/qt3dstudio.rb
+++ Casks/qt3dstudio.rb
@@ -3,7 +3,8 @@ cask 'qt3dstudio' do
   sha256 '5f5fdf827564ea45209c755a232642b63a24ce459d7c06714aa6edf49604d977'
 
   url "https://download.qt.io/official_releases/qt3dstudio/#{version.major_minor}/qt-3dstudio-opensource-mac-x64-#{version}.dmg"
-  appcast 'https://download.qt.io/official_releases/qt3dstudio/'
+  appcast 'https://download.qt.io/official_releases/qt3dstudio/',
+          configuration: version.major_minor
   name 'Qt 3D Studio'
   homepage 'https://www.qt.io/developers/'
 
