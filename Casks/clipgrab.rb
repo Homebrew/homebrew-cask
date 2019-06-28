58d94f36b70826db36eb05c69650c4c0cd6512e3
CoreCode <core-code@users.noreply.github.com>
2019-06-28 14:20:03 +0200

    Update clipgrab (#65439)

--- Casks/clipgrab.rb
+++ Casks/clipgrab.rb
@@ -3,6 +3,7 @@ cask 'clipgrab' do
   sha256 '2e5cc18adfa0b7cd7e0491bd299b8a5af710c32edd31aa98bc56f553ac2886a8'
 
   url "https://download.clipgrab.org/ClipGrab-#{version}.dmg"
+  appcast 'https://clipgrab.org/'
   name 'ClipGrab'
   homepage 'https://clipgrab.org/'
 
