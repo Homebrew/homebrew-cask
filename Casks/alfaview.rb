2359a34744dc0201fcd1236099839013f161aaf4
Randall <ran.dall@icloud.com>
2019-06-29 09:09:31 -0700

    Update alfaview from 7.41015 to 7.43036

--- Casks/alfaview.rb
+++ Casks/alfaview.rb
@@ -1,6 +1,6 @@
 cask 'alfaview' do
-  version '7.41015'
-  sha256 '1a35c58ac901716d372a905d26c6739424509e23ed10d7718dab76bc1a7fc9a3'
+  version '7.43036'
+  sha256 'b7f8ba2602875d06f57f69846d0bde58216c5380e2d3620970a5607a02bb48e8'
 
   url "https://assets.alfaview.com/stable/mac/alfaview-mac-production-#{version}.dmg"
   appcast 'https://alfaview.com/downloads'
