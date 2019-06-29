8bc659cab63a317db6c31a8d7a53073ba2d2a32a
CoreCode <core-code@users.noreply.github.com>
2019-06-29 18:52:00 +0200

    Update pdfelement from 6.7.12,2991 to 7.0.4,5237 (#65451)
    * Update pdfelement from 6.7.12,2991 to 7.0.4,5237
    
    * Update pdfelement.rb
    
    * Update pdfelement.rb
    
    * Update pdfelement.rb

--- Casks/pdfelement.rb
+++ Casks/pdfelement.rb
@@ -1,11 +1,11 @@
 cask 'pdfelement' do
-  version '6.7.12,2991'
-  sha256 'a4fd5421b98d00fb2433c9d64e2f29962d39dadf64e25d98e31325cdab37a711'
+  version '7.0.4,5237'
+  sha256 '97896572661c3c9baa7c4fbe60c6d78baeada672a0087e1c4e3287a8275ba599'
 
-  url "http://download.wondershare.com/cbs_down/mac-pdfelement#{version.major}_full#{version.after_comma}.dmg"
+  url "http://download.wondershare.com/cbs_down/mac-pdfelement_full#{version.after_comma}.dmg"
+  appcast 'https://cbs.wondershare.com/go.php?m=upgrade_info&pid=5237'
   name 'Wondershare PDFelement for Mac'
   homepage 'https://pdf.wondershare.com/'
 
-  # Renamed for consistency: app name is different in the Finder and in a shell.
-  app "PDFelementStd#{version.major}.app", target: "PDFelement #{version.major}.app"
+  app 'PDFelement.app'
 end
