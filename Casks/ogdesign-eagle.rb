9bb8cb6f9220b8a84826e6ef3aad930bc5b85cbb
Randall <17261190+ran-dall@users.noreply.github.com>
2019-06-29 13:49:48 -0700

    Update ogdesign-eagle from 1.9.1,build14 to 1.9.2 (#65506)
    * Update ogdesign-eagle from 1.9.1,build14 to 1.9.2
    
    * Update ogdesign-eagle.rb

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,9 +1,9 @@
 cask 'ogdesign-eagle' do
-  version '1.9.1,build14'
-  sha256 'eb9fcc1834fd9cd1042cb50fb7e7a8eb3ebb451ab6794264e56012180d06be21'
+  version '1.9.2'
+  sha256 '83bb4c55e24a10ad7441d9eef0d9c12fe58482ff57242f06e7a9b9d490a557fc'
 
   # eagleapp.s3-accelerate.amazonaws.com was verified as official when first introduced to the cask
-  url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-#{version.after_comma}.dmg?download"
+  url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version}.dmg"
   appcast 'https://eagle.cool/changelog'
   name 'Eagle'
   homepage 'https://eagle.cool/macOS'
