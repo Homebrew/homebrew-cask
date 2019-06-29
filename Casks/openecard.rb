610548c7c641802935b8407046c5840801059133
Joachim Astel <achim@astel.de>
2019-06-29 22:29:17 +0200

    Add Open eCard ISO/IEC 24727 authentication and signature for SmartCards

--- Casks/openecard.rb
+++ Casks/openecard.rb
@@ -2,12 +2,17 @@ cask 'openecard' do
   version '1.3.0'
   sha256 'b23d918029403591eb45ac8d52e58df50c1bcb3429b8394173b9063334fc52b1'
 
+  # https://www.openecard.org/download/pc/ on project homepage directs to GitHub
   url "https://github.com/ecsec/open-ecard/releases/download/#{version}/Open-eCard-App-#{version}.dmg"
-  appcast "https://github.com/ecsec/open-ecard/releases.atom"
+  appcast 'https://github.com/ecsec/open-ecard/releases.atom'
   name "Open eCard App #{version}"
   homepage 'https://www.openecard.org/'
 
-  pkg "Open eCard App.pkg"
+  pkg 'Open eCard App.pkg'
 
   uninstall delete: "#{appdir}/Open-eCard-App.app/"
+
+  zap trash: [
+               '~/.openecard/logs/richclient_info.log',
+             ]
 end
