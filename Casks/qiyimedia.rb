7ccc95e6ad012382d8e2a5c5554a69411cb56bcc
suschizu <brigitte@corecode.io>
2019-06-29 14:30:55 +0200

    Update qiyimedia.rb

--- Casks/qiyimedia.rb
+++ Casks/qiyimedia.rb
@@ -1,9 +1,9 @@
 cask 'qiyimedia' do
-  version '5.10.10'
+  version '20190628,5.10.10'
   sha256 '38be0152e83714239ca9fd4295fc061b811a66b200907e4f5d6a5f1f0f805762'
 
   url 'https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg'
-  appcast 'https://app.iqiyi.com/mac/player/index.html'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg'
   name '爱奇艺视频'
   homepage 'https://app.iqiyi.com/mac/player/index.html'
 
