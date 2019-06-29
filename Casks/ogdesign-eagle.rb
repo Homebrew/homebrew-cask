7e4c252b66c98026af8b3da3cd1f15725213a8d0
Vítor Galvão <info@vitorgalvao.com>
2019-06-29 21:40:14 +0100

    Update ogdesign-eagle.rb

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -3,7 +3,7 @@ cask 'ogdesign-eagle' do
   sha256 '83bb4c55e24a10ad7441d9eef0d9c12fe58482ff57242f06e7a9b9d490a557fc'
 
   # eagleapp.s3-accelerate.amazonaws.com was verified as official when first introduced to the cask
-  url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version}.dmg?download"
+  url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version}.dmg"
   appcast 'https://eagle.cool/changelog'
   name 'Eagle'
   homepage 'https://eagle.cool/macOS'
