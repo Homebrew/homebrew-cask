a6c1df833c4c061e092f13a76338224b1b5e39a8
Randall <ran.dall@icloud.com>
2019-06-30 10:18:10 -0700

    Update melodics from 2.0.2952 to 2.0.2957

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask 'melodics' do
-  version '2.0.2952'
-  sha256 'ef5aa21b7e74966728615f7b3567508ab207e7c4de036e69e5f007524bcd0d37'
+  version '2.0.2957'
+  sha256 'a7bcd3dadbaf77132ebe19fd3b7025b567850313de58d2ec7da1daeddea2c140'
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://web-cdn.melodics.com/download/osxupdatescastv#{version.major}.xml"
