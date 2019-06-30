ee9ed670df03109ca99d0be83f27b6cb2605bd6b
Khaldoon Mutahar <git@mutahar.me>
2019-06-30 20:41:20 +0800

    added new cask Rocket Typist 2.1.3 (#65523)
    * added new cask Rocket Typist
    
    * Update rocket-typist.rb

---
+++ Casks/rocket-typist.rb
@@ -0,0 +1,11 @@
+cask 'rocket-typist' do
+  version '2.1.3'
+  sha256 'a36c33209bdf22744ccd72cc04a74df68634d59c01f35d373ffb7c686616165f'
+
+  url 'https://witt-software.com/downloads/rockettypist/Rocket%20Typist.dmg'
+  appcast 'https://witt-software.com/downloads/rockettypist/appcast.xml'
+  name 'Rocket Typist'
+  homepage 'https://witt-software.com/rockettypist/'
+
+  app 'Rocket Typist.app'
+end
