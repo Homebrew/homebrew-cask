7b9a378d12509d9cb1cb5ab18101f4d08ff46e0d
Vítor Galvão <info@vitorgalvao.com>
2019-06-30 12:45:00 +0100

    Update rocket-typist.rb

--- Casks/rocket-typist.rb
+++ Casks/rocket-typist.rb
@@ -3,9 +3,9 @@ cask 'rocket-typist' do
   sha256 'a36c33209bdf22744ccd72cc04a74df68634d59c01f35d373ffb7c686616165f'
 
   url 'https://witt-software.com/downloads/rockettypist/Rocket%20Typist.dmg'
-  appcast 'https://witt-software.com/rockettypist/releasenotes/'
+  appcast 'https://witt-software.com/downloads/rockettypist/appcast.xml'
   name 'Rocket Typist'
   homepage 'https://witt-software.com/rockettypist/'
 
-  app 'Rocket Typist'
+  app 'Rocket Typist.app'
 end
