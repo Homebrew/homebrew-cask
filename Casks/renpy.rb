c41a7baf62b43aa2ae74e77b1f133a7d55b697ca
Yishen Miao <mys721tx@gmail.com>
2019-06-29 21:00:18 -0400

    Bumping renpy version to 7.3.0 (#65520)
    Bumps renpy version to 7.3.0.

--- Casks/renpy.rb
+++ Casks/renpy.rb
@@ -1,6 +1,6 @@
 cask 'renpy' do
-  version '7.2.2'
-  sha256 'ecea56c40b8a2ea96c533a689e3fffb16c184677835bc1e5e42f0ef5657ce392'
+  version '7.3.0'
+  sha256 'f40b0269ea614ef0292c974b9e03a05c5c9d71a10f31693c2e037921f815cb50'
 
   url "https://www.renpy.org/dl/#{version}/renpy-#{version}-sdk.zip"
   name 'Ren\'Py'
