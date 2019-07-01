8d002ba9f4cb835a477d2408271a3ef2f4f4d811
Khaldoon Mutahar <git@mutahar.me>
2019-07-01 19:20:38 +0800

    Update plex-media-server from 1.16.0.1226-7eb2c8f6f to 1.16.1.1291-158e5b199

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-server' do
-  version '1.16.0.1226-7eb2c8f6f'
-  sha256 'ba4d3a34aeb85e287258a07e87608cb2117aec74b257993a30c87f723b1e553a'
+  version '1.16.1.1291-158e5b199'
+  sha256 'cdcc13d67ca242b167aa853fa49ae68248e6fe0cc91e3e9e9c51c85ec9e6aa6c'
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.dmg"
   appcast 'https://plex.tv/api/downloads/5.json'
