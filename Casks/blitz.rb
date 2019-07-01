705a7d4485570e52261efe09e7f81f2bc2f84b2b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-01 10:20:42 -0700

    Update blitz from 1.0.5 to 1.0.6 (#65580)

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask 'blitz' do
-  version '1.0.5'
-  sha256 '22e499b1ebaf9e3037838f1b65975b1bea04b78f8a8c7a7f3acc2a1047ff12d5'
+  version '1.0.6'
+  sha256 'a9b964ed078411c5b1430ac9f06eb613b78916faa5507d49bba0ef4794b2330b'
 
   url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.blitz.gg/download/mac'
