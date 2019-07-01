9718aa821a1483dde51714b5be7ed5e6db55c1a4
CoreCode <core-code@users.noreply.github.com>
2019-07-01 23:04:18 +0200

    Update localdev from 0.4.0 to 0.4.2 (#65592)

--- Casks/localdev.rb
+++ Casks/localdev.rb
@@ -1,6 +1,6 @@
 cask 'localdev' do
-  version '0.4.0'
-  sha256 '5ce8b74ab6af8214abb2cd8a4d56d3ed431f76cfa9083ba1606aa25e947af96a'
+  version '0.4.2'
+  sha256 '3581f19ccaea66436ca61bab1075970d650fc1215607b6e6a96a2bf201325eda'
 
   # pantheon-localdev.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://pantheon-localdev.s3.amazonaws.com/Localdev-v#{version}.dmg"
