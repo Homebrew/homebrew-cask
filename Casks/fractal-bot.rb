6e6af42fbe4a0ebfec1be4a62004bd16078e7e2b
CoreCode <core-code@users.noreply.github.com>
2019-07-01 22:53:51 +0200

    Update fractal-bot.rb

--- Casks/fractal-bot.rb
+++ Casks/fractal-bot.rb
@@ -3,7 +3,8 @@ cask 'fractal-bot' do
   sha256 'bcb6d63b0b4069bda5db02cb8d33509abe5c5aeba5a6fb67cdd62a97e9f57980'
 
   url "https://www.fractalaudio.com/downloads/fractal-bot/Fractal-Bot-OSX-v#{version.tr('.', 'p')}.dmg"
-  appcast 'https://www.fractalaudio.com/fractal-bot/'
+  appcast 'https://www.fractalaudio.com/fractal-bot/',
+          configuration: "#{version.tr('.', 'p')}.dmg"
   name 'Fractal Bot'
   homepage 'https://www.fractalaudio.com/fractal-bot/'
 
