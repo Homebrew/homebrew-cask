9b97470fa2afb7e843cd514a02ef776c1853acb0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-01 10:37:09 -0700

    Update textadept from 10.4 to 10.5 (#65585)

--- Casks/textadept.rb
+++ Casks/textadept.rb
@@ -1,6 +1,6 @@
 cask 'textadept' do
-  version '10.4'
-  sha256 '2f48b8ff639842a50336462d37d7c309a9406a1225062fe70eae5e6a019dd470'
+  version '10.5'
+  sha256 'ab691e3a4a8958d390dcaacff1083f8acdc28539b7d54de77077d5145401308b'
 
   url "https://foicica.com/textadept/download/textadept_#{version}.osx.zip"
   appcast 'https://foicica.com/textadept/feed'
