84fe3d7049b8bfec786eacdaff95ae577a33d4ca
Tim Kelty <tim@fusionary.com>
2019-07-02 08:23:08 -0400

    Add comment for differing url.

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -1,7 +1,8 @@
 cask 'timeular' do
     version :latest
     sha256 :no_check
-  
+
+    # timeular-desktop-packages.s3.amazonaws.com was verified as official when first introduced to the cask
     url 'https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular.dmg'
     name 'Timeular'
     homepage 'https://timeular.com/'
