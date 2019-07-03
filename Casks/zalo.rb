cfae59ca4d3dabd5ec2cf1b7f7531d97f31463cf
Vítor Galvão <info@vitorgalvao.com>
2019-07-03 14:19:13 +0100

    Update zalo.rb

--- Casks/zalo.rb
+++ Casks/zalo.rb
@@ -2,8 +2,8 @@ cask 'zalo' do
   version '19.6.1'
   sha256 '25d1cffcbb882ecc6a801cf26039a5af73442371135c320b29b400c1c29fc251'
 
-  # res-download-pc-te-vnno-zn-2.zadn.vn/mac was verified as official when first introduced to the cask
-  url "https://res-download-pc-te-vnno-zn-2.zadn.vn/mac/ZaloSetup-#{version}.dmg"
+  # res-download-pc-te-vnno-zn-3.zadn.vn/mac was verified as official when first introduced to the cask
+  url "https://res-download-pc-te-vnno-zn-3.zadn.vn/mac/ZaloSetup-#{version}.dmg"
   name 'Zalo'
   homepage 'https://zalo.me/'
 
