ccd663ca1c76fb2a07f616f20c7da0b24d4c8d77
Vítor Galvão <info@vitorgalvao.com>
2019-07-03 14:15:40 +0100

    Update visual-paradigm.rb

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -3,7 +3,8 @@ cask 'visual-paradigm' do
   sha256 'fe9ae84dc41477ef89ad55218217e67e9123ecce79ba46b3990aa8ca84019722'
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
-  appcast 'https://www.visual-paradigm.com/downloads/vp/checksum.html'
+  appcast 'https://www.visual-paradigm.com/downloads/vp/checksum.html',
+          configuration: "#{version.before_comma.dots_to_underscores}_#{version.after_comma}"
   name 'Visual Paradigm'
   homepage 'https://www.visual-paradigm.com/'
 
