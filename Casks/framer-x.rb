4acf72507a3c0364e89736c220f8d42f0661f9c1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-03 09:34:57 -0700

    Update framer-x from 33072,1561453763 to 33255,1562060195 (#65677)

--- Casks/framer-x.rb
+++ Casks/framer-x.rb
@@ -1,6 +1,6 @@
 cask 'framer-x' do
-  version '33072,1561453763'
-  sha256 '3c25004916c30588f7f4e0fe0b650dfd443e97d9768cff6f9601cf794723059c'
+  version '33255,1562060195'
+  sha256 '493bdc78ae34093e71b0dacfa855e1cefc8baa83efaa52d1d78133e01f026b5d'
 
   # dl.devmate.com/com.framer.x was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.framer.x/#{version.before_comma}/#{version.after_comma}/FramerX-#{version.before_comma}.zip"
