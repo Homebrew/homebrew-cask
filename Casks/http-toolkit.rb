41ed558455a18c639d8ead9e95c74888521ef47f
Dave Nicolson <david.nicolson@gmail.com>
2019-07-03 15:29:46 +0200

    Add HTTP Toolkit latest (#65586)

---
+++ Casks/http-toolkit.rb
@@ -0,0 +1,21 @@
+cask 'http-toolkit' do
+  version '0.1.7'
+  sha256 '404a3cc287207a53f9ece748f6aea54d0babca0797c83889d7267a4e8b892efe'
+
+  # github.com/httptoolkit/httptoolkit-desktop was verified as official when first introduced to the cask
+  url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HTTP.Toolkit.dmg"
+  appcast 'https://github.com/httptoolkit/httptoolkit-desktop/releases.atom'
+  name 'HTTP Toolkit'
+  homepage 'https://httptoolkit.tech/'
+
+  app 'HTTP Toolkit.app'
+
+  zap trash: [
+               '~/Library/Application Support/HTTP Toolkit',
+               '~/Library/Caches/httptoolkit-server',
+               '~/Library/Logs/HTTP Toolkit',
+               '~/Library/Preferences/httptoolkit',
+               '~/Library/Preferences/tech.httptoolkit.desktop.plist',
+               '~/Library/Saved Application State/tech.httptoolkit.desktop.savedStat',
+             ]
+end
