0af7bf86af952105cd81f0cf194ffd41c14e2e27
CoreCode <core-code@users.noreply.github.com>
2019-07-03 19:45:58 +0200

    Update 4k-video-downloader from 4.7.3.2742 to 4.8.0.2852 (#65696)

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,6 +1,6 @@
 cask '4k-video-downloader' do
-  version '4.7.3.2742'
-  sha256 '210849d992e1a89fe56c6ad8fd01b2f7e3efba815f6a5792a8309cbd72aa69d8'
+  version '4.8.0.2852'
+  sha256 'ffa643672831ba116552b70c13a51f851fbe5be9afd4d1435e2fbe6657befa41'
 
   url "https://dl.4kdownload.com/app/4kvideodownloader_#{version.major_minor_patch}.dmg"
   appcast 'https://www.4kdownload.com/download'
