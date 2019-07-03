6d6ea1eeca45ab24d1e48930f01d4ceeb5367659
Randall <ran.dall@icloud.com>
2019-07-03 09:16:10 -0700

    Update amadine from 1.0.3 to 1.0.4

--- Casks/amadine.rb
+++ Casks/amadine.rb
@@ -1,6 +1,6 @@
 cask 'amadine' do
-  version '1.0.3'
-  sha256 'b294621713bb3c0bdee82e50e97c2a82a5502bff3e7e3a4cfddb28f76c754288'
+  version '1.0.4'
+  sha256 '2c5c46f92a3e40b2ecabf1d369d3449e4ee8e44f5917bd3caef169c682832c0a'
 
   # belightsoft.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://belightsoft.s3.amazonaws.com/Amadine.dmg'
