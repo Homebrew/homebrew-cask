9e3581497c76458ac36db663df59628f43899c9a
Vítor Galvão <info@vitorgalvao.com>
2019-07-03 14:15:11 +0100

    Update visual-paradigm-ce.rb

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -3,7 +3,8 @@ cask 'visual-paradigm-ce' do
   sha256 '4e22eff32abb8a09d888aef7553e0309b7e71bd4c2aea07f1b84dad3a8042f78'
 
   url "https://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
-  appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html'
+  appcast 'https://www.visual-paradigm.com/downloads/vpce/checksum.html',
+          configuration: "#{version.before_comma.dots_to_underscores}_#{version.after_comma}"
   name 'Visual Paradigm Community Edition'
   homepage 'https://www.visual-paradigm.com/'
 
