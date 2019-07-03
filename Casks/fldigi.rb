c178e5a8a88a6f0ed898a07b237df2ae78f59ca0
CoreCode <core-code@users.noreply.github.com>
2019-07-03 19:22:43 +0200

    Update fldigi.rb

--- Casks/fldigi.rb
+++ Casks/fldigi.rb
@@ -2,7 +2,7 @@ cask 'fldigi' do
   version '4.1.05'
   sha256 '4afd31b3061b84f58544e8a160c31f6ab9b7927411098b09c84a73d8493597f7'
 
-  url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version.before_comma}_x86_64.dmg"
+  url "https://downloads.sourceforge.net/fldigi/fldigi/fldigi-#{version}_x86_64.dmg"
   appcast 'https://sourceforge.net/projects/fldigi/rss?path=/fldigi'
   name 'fldigi'
   homepage 'https://sourceforge.net/projects/fldigi/files/fldigi/'
