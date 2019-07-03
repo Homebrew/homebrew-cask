8f15003f526199ed38e964746ac6d694a24b5846
whatGhost <chandler.lu1997@gmail.com>
2019-07-03 22:54:48 +0800

    Update downie from 3.7.7,2033 to 3.7.8,2037 (#65570)
    * Update downie from 3.7.7,2033 to 3.7.7,2037
    
    * Update downie from 3.7.7,2033 to 3.7.8,2037

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.7.7,2033'
-  sha256 'c362426902200cb55f14e11b125c988c3fb989b214a3fb38b018ad4ab05989be'
+  version '3.7.8,2037'
+  sha256 'dae942ec740eb41ac2f85b61df78a047e9d990bffb80519a806238136679ca21'
 
   url "https://trial.charliemonroe.net/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.zip"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml"
