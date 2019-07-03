e32134ebd6297d2dd9eab5288f4ff2ab588ccaad
CoreCode <core-code@users.noreply.github.com>
2019-07-03 15:28:28 +0200

    Update fractal-bot from 2.11.1 to 3.00.00 (#65590)
    * Update fractal-bot from 2.11.1 to 3.00.00
    
    * Update fractal-bot.rb
    
    * Update fractal-bot.rb

--- Casks/fractal-bot.rb
+++ Casks/fractal-bot.rb
@@ -1,15 +1,14 @@
 cask 'fractal-bot' do
-  version '2.11.1'
-  sha256 'f03e4760201eca07e64246945eb2df61fac2f45e9394d29ff21ea330b89111f9'
+  version '3.00.00'
+  sha256 'bcb6d63b0b4069bda5db02cb8d33509abe5c5aeba5a6fb67cdd62a97e9f57980'
 
   url "https://www.fractalaudio.com/downloads/fractal-bot/Fractal-Bot-OSX-v#{version.tr('.', 'p')}.dmg"
-  appcast 'https://www.fractalaudio.com/fractal-bot/'
+  appcast 'https://www.fractalaudio.com/fractal-bot/',
+          configuration: "#{version.tr('.', 'p')}.dmg"
   name 'Fractal Bot'
   homepage 'https://www.fractalaudio.com/fractal-bot/'
 
-  pkg "Fractal-Bot-OSX-v#{version.tr('.', 'p')}.pkg"
-
-  uninstall pkgutil: 'com.fractalaudio.fractalAudioSystemsFractalbot.Fractal-Bot.pkg'
+  app 'Fractal-Bot.app'
 
   zap trash: '~/Library/Application Support/Fractal Audio/Fractal-Bot'
 end
