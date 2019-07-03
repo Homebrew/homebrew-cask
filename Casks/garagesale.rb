90d50dac04a697e59ff2ac045a2403dd4f52d871
Randall <ran.dall@icloud.com>
2019-07-03 09:42:44 -0700

    Update garagesale from 7.0.16 to 7.0.17

--- Casks/garagesale.rb
+++ Casks/garagesale.rb
@@ -1,6 +1,6 @@
 cask 'garagesale' do
-  version '7.0.16'
-  sha256 '7e2bcf398121331a819e50970cf306486eb492c0078e8c1376233c232e021f87'
+  version '7.0.17'
+  sha256 '89590b6e45b9a6d44cedc652ef39af64359fc8090109403162c3a5457a6bc562'
 
   url "https://downloads.iwascoding.com/downloads/GarageSale_#{version}.dmg"
   appcast 'https://rink.hockeyapp.net/api/2/apps/b83b659a4c853a6138def4da94c6fcdd.rss'
