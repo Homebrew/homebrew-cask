a240766f1716685c0a862c5acfdcc6cf4133f202
Randall <ran.dall@icloud.com>
2019-07-03 09:49:55 -0700

    Update fractal-bot from 3.00.00 to 3.0.0

--- Casks/fractal-bot.rb
+++ Casks/fractal-bot.rb
@@ -1,6 +1,6 @@
 cask 'fractal-bot' do
-  version '3.00.00'
-  sha256 'bcb6d63b0b4069bda5db02cb8d33509abe5c5aeba5a6fb67cdd62a97e9f57980'
+  version '3.0.0'
+  sha256 '2774f8da135ac58d0897ec3ce427018354a746cd7bfafc55c7e602262bbb0170'
 
   url "https://www.fractalaudio.com/downloads/fractal-bot/Fractal-Bot-OSX-v#{version.tr('.', 'p')}.dmg"
   appcast 'https://www.fractalaudio.com/fractal-bot/',
