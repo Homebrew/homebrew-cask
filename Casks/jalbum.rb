0d3f495cd38bcccb5fff67f8936085c9e82cfe5d
Vítor Galvão <info@vitorgalvao.com>
2019-07-03 14:42:33 +0100

    Update jalbum.rb

--- Casks/jalbum.rb
+++ Casks/jalbum.rb
@@ -3,7 +3,8 @@ cask 'jalbum' do
   sha256 '0bdb2e6c64d7996cd15ef58687c109b5627f6c2b2a3b18910c4d9215df51f5b1'
 
   url "https://download.jalbum.net/download/#{version.major_minor}/MacOSX/jAlbum.dmg"
-  appcast 'https://jalbum.net/en/software/download/previous'
+  appcast 'https://jalbum.net/en/software/download/previous',
+          configuration: version.major_minor
   name 'jAlbum'
   homepage 'https://jalbum.net/'
 
