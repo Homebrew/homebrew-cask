4185d0daf8ab636e85860cc2bf7cf96ee4ccb995
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-04 10:05:48 -0700

    Update bunch from 1.1.5,31 to 1.1.6,32 (#65736)

--- Casks/bunch.rb
+++ Casks/bunch.rb
@@ -1,6 +1,6 @@
 cask 'bunch' do
-  version '1.1.5,31'
-  sha256 'eef1d9a7bc093e620f54892c600690e77561a1cb0ef03797535d3ce30874ffb2'
+  version '1.1.6,32'
+  sha256 'ec62c356f75e67de3790ceb289cc34235837415377a104c2960119ae44dd8dad'
 
   url "https://cdn3.brettterpstra.com/updates/bunch/Bunch#{version.before_comma}#{version.after_comma}.dmg"
   appcast 'https://brettterpstra.com/updates/bunch/appcast.xml'
