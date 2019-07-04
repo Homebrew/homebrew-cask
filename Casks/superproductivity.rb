d95969c09f5d9b18fd324d524a15c6756053d0fc
Randall <ran.dall@icloud.com>
2019-07-04 09:47:09 -0700

    Update superproductivity from 2.8.4 to 2.8.5

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,6 +1,6 @@
 cask 'superproductivity' do
-  version '2.8.4'
-  sha256 '9da2191a625945709f4765a3bb1ef6780c0c807bbd131fea5b6f7ec3118bb344'
+  version '2.8.5'
+  sha256 '7e1e0ba180c7a3383fb5a12535d15a02763a2465bc367a36cd987fcf61ae1f08'
 
   # github.com/johannesjo/super-productivity was verified as official when first introduced to the cask
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip"
