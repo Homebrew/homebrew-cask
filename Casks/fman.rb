10b949b9dc97c56595a62f32c040aff1a0fa08ea
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-04 10:09:33 -0700

    Update fman from 1.6.3 to 1.6.4 (#65738)

--- Casks/fman.rb
+++ Casks/fman.rb
@@ -1,6 +1,6 @@
 cask 'fman' do
-  version '1.6.3'
-  sha256 'bffb9ee85b694c9451fe0e48f675fc795579f808ffede8f19881821327d981f6'
+  version '1.6.4'
+  sha256 'e2cffda7e96fdb43b6eeef9235aa075487243bb689ebc8ce886d417cf5f73cf0'
 
   url "https://fman.io/updates/mac/#{version}.zip"
   appcast 'https://fman.io/updates/Appcast.xml'
