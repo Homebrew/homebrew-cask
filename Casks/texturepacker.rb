8f67a15ec5a34a3f7a2a89ce5507e7e49fe38c68
Randall <ran.dall@icloud.com>
2019-07-05 08:53:44 -0700

    Update texturepacker from 5.0.0 to 5.1.0

--- Casks/texturepacker.rb
+++ Casks/texturepacker.rb
@@ -1,6 +1,6 @@
 cask 'texturepacker' do
-  version '5.0.0'
-  sha256 '27399322354ac62fbdc59418c269b5c8779edceeb588c32d19a58e4b498ce8c5'
+  version '5.1.0'
+  sha256 '4b849ced31b4c209ef409746ed7ef628ce1ca6a6ecf09821a2c67b448dfbe022'
 
   url "https://www.codeandweb.com/download/texturepacker/#{version}/TexturePacker-#{version}-uni.dmg"
   appcast 'https://www.codeandweb.com/releases/TexturePacker/appcast-mac-release.xml'
