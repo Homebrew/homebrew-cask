98cbed0395937c7307706c20ef3da7c19ff7bebe
Jesper Ryom <jesper@ryom.me>
2019-07-05 17:44:56 +0200

    Add SpaceId.app v1.3

---
+++ Casks/spaceid.rb
@@ -0,0 +1,11 @@
+cask 'spaceid' do
+  version '1.3'
+  sha256 '3df4440e9ef96c8408342297a7a287be931976c655a3ec772a96c8082f3a7a81'
+
+  url "https://github.com/dshnkao/SpaceId/releases/download/v#{version}/SpaceId.app.zip"
+  appcast 'https://github.com/dshnkao/SpaceId/releases.atom'
+  name 'SpaceId'
+  homepage 'https://github.com/dshnkao/SpaceId/'
+
+  app 'SpaceId.app'
+end
