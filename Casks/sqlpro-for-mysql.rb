74c991a5456e7c5c0a42e1e4b2b1e4c0e81f2af2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-05 08:57:54 -0700

    Update sqlpro-for-mysql from 2019.05.16 to 2019.06.28 (#65749)

--- Casks/sqlpro-for-mysql.rb
+++ Casks/sqlpro-for-mysql.rb
@@ -1,6 +1,6 @@
 cask 'sqlpro-for-mysql' do
-  version '2019.05.16'
-  sha256 'b4dd47ceb254f4f55d5331eece555a0f15fe2d8a5ec9ed99fb1850298833059f'
+  version '2019.06.28'
+  sha256 'f094ae34f89738afc6487d3fa83575caeb0b3763594d61dc78b80f8aaae791d7'
 
   # d3fwkemdw8spx3.cloudfront.net/mysql was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/mysql/SQLProMySQL.#{version}.app.zip"
