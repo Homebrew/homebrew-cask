0e0755e5f93ddfb5c2d68e5f6bf84fc1c9fae1a4
brigitte zmoelnig <brigitte@corecode.io>
2019-07-06 17:07:34 +0200

    Update eclipse-platform from 4.12,201906051800 to 4.12.0,2019-06:R

--- Casks/eclipse-platform.rb
+++ Casks/eclipse-platform.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-platform' do
-  version '4.12,201906051800'
-  sha256 'e62c6c051f96bb6d6bb414531c9c09bdcf14e71997cedc23339e99a1cacf0dbb'
+  version '4.12.0,2019-06:R'
+  sha256 'fa81f33c58afa99a6a81c275d6bca0fc6d838af15206fc2f181c942438078d27'
 
   url "https://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops#{version.major}/R-#{version.before_comma}-#{version.after_comma}/eclipse-SDK-#{version.before_comma}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse SDK'
