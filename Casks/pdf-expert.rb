ffab5e991bd7e807c45b1dfaa3d56e68f1971115
Qi Zhang <singularitti@outlook.com>
2019-07-06 08:53:18 -0400

    Update pdf-expert from 2.4.26,634 to 2.4.28,636 (#65693)

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,6 +1,6 @@
 cask 'pdf-expert' do
-  version '2.4.26,634'
-  sha256 '5434c35470fd4f187e3d27865e0f59efb81894faa37dd549f1a5114532ee1ec1'
+  version '2.4.28,636'
+  sha256 '3279ebad5021a55469c999ace4be44fd488dc6d3181478b5892b344e2b803ba0'
 
   # d1ke680phyeohy.cloudfront.net was verified as official when first introduced to the cask
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
