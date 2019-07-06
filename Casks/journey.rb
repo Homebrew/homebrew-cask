0c6642d176059467e27e0b446c8804f53f483efe
Randall <ran.dall@icloud.com>
2019-07-06 08:32:51 -0700

    Update journey from 2.12.9 to 2.12.11

--- Casks/journey.rb
+++ Casks/journey.rb
@@ -1,6 +1,6 @@
 cask 'journey' do
-  version '2.12.9'
-  sha256 'b99281dd8b9bdfc9e9d57a86af9b5c38a3f8aff1df936c48c1677cf6f0991364'
+  version '2.12.11'
+  sha256 '569ac0befbee48f75f73f53728de0749d2994c96cbce61cf10401905e8385bfb'
 
   # github.com/2-App-Studio/journey-releases was verified as official when first introduced to the cask
   url "https://github.com/2-App-Studio/journey-releases/releases/download/v#{version}/Journey-darwin-#{version}.dmg"
