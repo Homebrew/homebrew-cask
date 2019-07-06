314e8ebb74d74391f627261f3b58b121cb29002f
CoreCode <core-code@users.noreply.github.com>
2019-07-06 02:32:54 +0200

    Update dbvisualizer (#65762)

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -3,6 +3,7 @@ cask 'dbvisualizer' do
   sha256 '0c61d818c0acd57e381dc32deb3747a606da67581160df2c4e90f5b548fef457'
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.dots_to_underscores}_jre.dmg"
+  appcast "https://www.dbvis.com/download/#{version.major}.0"
   name 'DbVisualizer'
   homepage 'https://www.dbvis.com/'
 
