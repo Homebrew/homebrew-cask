ee3e0ceb6c05c3e0023b84b5ac94fe75a0864f5a
Vítor Galvão <info@vitorgalvao.com>
2019-07-06 20:26:26 +0100

    Update garagesale.rb

--- Casks/garagesale.rb
+++ Casks/garagesale.rb
@@ -3,7 +3,7 @@ cask 'garagesale' do
   sha256 '52c1bcbf249c3f2b42dd1eaf69e7028169bc9968d4111f43dbd706356ba5b77f'
 
   url "https://downloads.iwascoding.com/downloads/GarageSale_#{version}.dmg"
-  appcast 'https://www.iwascoding.com/cgi-bin/version_check.cgi?GZIP=1&APPLICATION=GarageSale&APP_BUNDLE_VERSION=800'
+  appcast "https://www.iwascoding.com/cgi-bin/version_check.cgi?APPLICATION=GarageSale&APP_BUNDLE_VERSION=#{version.major}00"
   name 'GarageSale'
   homepage 'https://www.iwascoding.com/GarageSale/'
 
