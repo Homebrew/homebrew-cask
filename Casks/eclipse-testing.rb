fb42c8de4d86d40939bc39af37138960592c93a1
brigitte zmoelnig <brigitte@corecode.io>
2019-07-06 17:18:04 +0200

    Update eclipse-testing from 4.11.0,2019-03:R to 4.12.0,2019-06:R

--- Casks/eclipse-testing.rb
+++ Casks/eclipse-testing.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-testing' do
-  version '4.11.0,2019-03:R'
-  sha256 '6dfdcfeddbe1feada08bc6521382c3e9e97de3d55581769828baa880f6fd9f42'
+  version '4.12.0,2019-06:R'
+  sha256 'b0cdaba4c66762ea1878ec0ed081c78a7d16aff34a8ffd74085a398bcff2ab41'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-testing-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse for Testers'
