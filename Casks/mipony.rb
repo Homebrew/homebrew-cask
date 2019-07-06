ae13e1d1c7539e1275da39c1f50151b94b73cac3
CoreCode <core-code@users.noreply.github.com>
2019-07-06 16:13:56 +0200

    Update mipony (#65815)

--- Casks/mipony.rb
+++ Casks/mipony.rb
@@ -2,11 +2,12 @@ cask 'mipony' do
   version :latest
   sha256 :no_check
 
-  url 'https://download.mipony.net/downloads/mac/Mipony-Installer.dmg'
+  url 'https://download.mipony.net/downloads/mac/Mipony-Installer.pkg'
   name 'Mipony'
   homepage 'https://www.mipony.net/en/'
 
   pkg 'Mipony-Installer.pkg'
 
-  uninstall pkgutil: 'net.installer.mipony.*'
+  uninstall pkgutil: 'net.installer.mipony.*',
+            quit:    'com.downloader.Mipony'
 end
