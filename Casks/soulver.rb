15a3d996fdff44d6ed8e74ca7128f56e4f28fed4
Oliver F <30932609+olfuerniss@users.noreply.github.com>
2019-07-06 15:25:12 +0200

    Update soulver from 3.0.3-25 to 3.0.4-32 (#65826)

--- Casks/soulver.rb
+++ Casks/soulver.rb
@@ -1,6 +1,6 @@
 cask 'soulver' do
-  version '3.0.3-25'
-  sha256 '933d039fd2fa24f9a900dcaa9fda1f7312bd134dc0c8dec477064433c26aaab3'
+  version '3.0.4-32'
+  sha256 '58b3b371987311a565f589f1af0c647866359fdf7776cc1b1f01d43d05a092e2'
 
   url "https://soulver.app/mac/sparkle/soulver-#{version}.zip"
   appcast 'https://soulver.app/mac/sparkle/appcast.xml'
