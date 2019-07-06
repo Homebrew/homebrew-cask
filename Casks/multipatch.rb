93713c5792c964f837b9fafb2a59627525ddbbdf
CoreCode <core-code@users.noreply.github.com>
2019-07-06 03:09:18 +0200

    Update multipatch (#65810)

--- Casks/multipatch.rb
+++ Casks/multipatch.rb
@@ -2,9 +2,9 @@ cask 'multipatch' do
   version '1.7'
   sha256 '4d92147ac60aa4224959e53d91ef3c457ad2fb47dad739ce7f87bc456ec94e06'
 
-  url "https://projects.sappharad.com/tools/multipatch#{version.no_dots}.zip"
+  url "http://projects.sappharad.com/tools/multipatch#{version.no_dots}.zip"
   name 'MultiPatch'
-  homepage 'https://projects.sappharad.com/tools/multipatch.html'
+  homepage 'http://projects.sappharad.com/tools/multipatch.html'
 
   app 'MultiPatch/MultiPatch.app'
 
