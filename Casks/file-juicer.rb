e6e87e414ed5dff95f72d51f16e4e1e28f4714f6
Eric <github@ideolo.gy>
2019-07-07 11:25:24 -0700

    Update file-juicer

--- Casks/file-juicer.rb
+++ Casks/file-juicer.rb
@@ -1,6 +1,6 @@
 cask 'file-juicer' do
   version '4.77'
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 '1d9e2827eb16e036b29ab71297272f3640a3491213de6d7869dd279d6a9c3617'
 
   url "https://echoone.com/filejuicer/FileJuicer-#{version}.zip"
   appcast 'https://echoone.com/filejuicer/download'
