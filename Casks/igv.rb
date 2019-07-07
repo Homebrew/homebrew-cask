a8f13d4bc905634db5a41a17712f4e048738e404
CoreCode <core-code@users.noreply.github.com>
2019-07-07 23:09:22 +0200

    Update igv (#65897)

--- Casks/igv.rb
+++ Casks/igv.rb
@@ -3,6 +3,8 @@ cask 'igv' do
   sha256 'bf4bce3a2f15af606479b6f738749b82acaf68ff0ec4b8525580e3e197aa7e13'
 
   url "https://data.broadinstitute.org/igv/projects/downloads/#{version.major_minor}/IGV_#{version}.app.zip"
+  appcast 'https://data.broadinstitute.org/igv/projects/downloads/',
+          configuration: version.major_minor
   name 'Integrative Genomics Viewer (IGV)'
   homepage 'https://software.broadinstitute.org/software/igv/'
 
