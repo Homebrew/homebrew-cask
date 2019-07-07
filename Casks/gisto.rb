ef3b3728dc9b6c6e416fabb646b490f2323f07a1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-07 09:13:54 -0700

    Update gisto from 1.12.4 to 1.12.5 (#65879)

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -1,6 +1,6 @@
 cask 'gisto' do
-  version '1.12.4'
-  sha256 '98f3061866d89eb5194a2405328f70aaa56efdb7e6ada390244e9f93ef82a858'
+  version '1.12.5'
+  sha256 'c9e70ba017ed022a17976bc6cacab495d5641b95e84ff977078493fd4eb52401'
 
   # github.com/Gisto/Gisto was verified as official when first introduced to the cask
   url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto-#{version}.dmg"
