1c57f5eae5d4d24262a402401d64369c7e7e4258
Julian Mayer <julian@corecode.io>
2019-07-08 00:19:44 +0200

    Update mongodb-compass-readonly

--- Casks/mongodb-compass-readonly.rb
+++ Casks/mongodb-compass-readonly.rb
@@ -3,6 +3,7 @@ cask 'mongodb-compass-readonly' do
   sha256 '76652eea53584c70ac838005c97c03e150ccb321aa09f1cc6b8ad230401014df'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-readonly-#{version}-darwin-x64.dmg"
+  appcast 'https://www.mongodb.com/download-center/compass'
   name 'MongoDB Compass Readonly'
   homepage 'https://www.mongodb.com/products/compass'
 
