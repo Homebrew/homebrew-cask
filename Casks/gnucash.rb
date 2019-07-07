175586322d2a158fcc4222a63a4fd43943733384
Przemyslaw Kryger <pkryger@gmail.com>
2019-07-07 08:42:21 +0100

    Update gnucash from 3.5-1 to 3.6-1

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,6 +1,6 @@
 cask 'gnucash' do
-  version '3.5-1'
-  sha256 '9ceda37f952bb53364b5ecbe5005642a8ff4158610e1c268ccbd041cdb2b1535'
+  version '3.6-1'
+  sha256 'a9275a8d2aef576016b7f58b08bcc71739902e2e81787171af83c2576e21d085'
 
   # github.com/Gnucash/gnucash was verified as official when first introduced to the cask
   url "https://github.com/Gnucash/gnucash/releases/download/#{version.major_minor_patch}/Gnucash-Intel-#{version}.dmg"
