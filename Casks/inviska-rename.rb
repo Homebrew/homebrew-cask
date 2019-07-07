ca62c38a9c12198cc92bc889e3028f70a9094b13
Randall <ran.dall@icloud.com>
2019-07-07 09:15:40 -0700

    Update inviska-rename from 8.0 to 9.0

--- Casks/inviska-rename.rb
+++ Casks/inviska-rename.rb
@@ -1,6 +1,6 @@
 cask 'inviska-rename' do
-  version '8.0'
-  sha256 'a80bdef468ed67d0a4a2d023a6c025cbcf6d372addf3f69df6e8882c0f38f152'
+  version '9.0'
+  sha256 'd7a6b85ebdd4abb014a2a2f7ddce2e9a3c024220b1dc31b96a6a244e4d3df305'
 
   url "https://www.inviska.com/files/rename/mac/Inviska_Rename-#{version}.tar.gz"
   appcast 'https://www.inviska.com/rename/'
