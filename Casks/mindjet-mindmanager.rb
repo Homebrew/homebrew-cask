acabf6fd30ee3d5fd461832fd68339d7186ce2fe
Julian Mayer <julian@corecode.io>
2019-07-08 00:47:30 +0200

    Update mindjet-mindmanager

--- Casks/mindjet-mindmanager.rb
+++ Casks/mindjet-mindmanager.rb
@@ -3,6 +3,8 @@ cask 'mindjet-mindmanager' do
   sha256 '716287e692bbba2c14ba7d5efae3be6148d4b0c62bb57af7195caaec5fc25fa9'
 
   url "https://download.mindjet.com/Mindjet_MindManager_Mac_#{version}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.mindjet.com/latest-release-notes-mac-english',
+          configuration: version.dots_to_underscores
   name 'Mindjet Mindmanager'
   homepage 'https://www.mindjet.com/mindmanager/'
 
