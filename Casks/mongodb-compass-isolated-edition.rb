2af5541bc13a3cecfb9e35b834b712012d647e1c
Julian Mayer <julian@corecode.io>
2019-07-08 00:17:51 +0200

    Update mongodb-compass-isolated-edition

--- Casks/mongodb-compass-isolated-edition.rb
+++ Casks/mongodb-compass-isolated-edition.rb
@@ -3,6 +3,7 @@ cask 'mongodb-compass-isolated-edition' do
   sha256 '33841ca51d9fcd04dcaebf84e2950de25f02a0b1e543e260ee01f7292e5cf923'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-isolated-#{version}-darwin-x64.dmg"
+  appcast 'https://www.mongodb.com/download-center/compass'
   name 'MongoDB Compass Isolated'
   homepage 'https://www.mongodb.com/products/compass'
 
