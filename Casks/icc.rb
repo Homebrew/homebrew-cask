30f2d611aa6d9dc8103b2993298dc3abaf9399b5
CoreCode <core-code@users.noreply.github.com>
2019-07-07 22:48:08 +0200

    Update icc.rb

--- Casks/icc.rb
+++ Casks/icc.rb
@@ -1,9 +1,9 @@
 cask 'icc' do
-  version '1.0,r7587'
+  version '1.0,r7611'
   sha256 '01f4ad8775900654d3209b90132491a3d6325945f20228f6b09a5651a64a19fa'
 
   url "http://download.chessclub.com/desktop/mac/ICCforMac.#{version.after_comma}.pkg"
-  appcast 'http://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://download.chessclub.com/desktop/mac/ICCforMac.latest.pkg',
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://download.chessclub.com/desktop/mac/ICCforMac.latest.pkg',
           configuration: version.after_comma
   name 'ICC for Mac'
   homepage 'https://www.chessclub.com/download-software/icc-for-mac'
