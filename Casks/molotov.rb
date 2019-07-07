9c015f07693a734e481238bedbb3f27af2a4d3cc
Julian Mayer <julian@corecode.io>
2019-07-08 00:13:35 +0200

    Update molotov

--- Casks/molotov.rb
+++ Casks/molotov.rb
@@ -3,6 +3,7 @@ cask 'molotov' do
   sha256 '7de54b9810573e9145531fe0f4547c7a713305f0f9ac02493ecd117365412275'
 
   url "https://desktop-auto-upgrade.molotov.tv/mac/Molotov-v#{version}.dmg"
+  appcast 'https://desktop-auto-upgrade.molotov.tv/mac/manifest.json'
   name 'Molotov'
   homepage 'https://www.molotov.tv/'
 
