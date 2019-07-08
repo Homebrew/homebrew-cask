771400db1389b70f009f523f98b2eb41622948ce
Julian Mayer <julian@corecode.io>
2019-07-08 15:43:51 +0200

    Update double-commander

--- Casks/double-commander.rb
+++ Casks/double-commander.rb
@@ -1,9 +1,9 @@
 cask 'double-commander' do
   version '0.9.5-8947'
-  sha256 '924e9a25b872ccbb91eacb44bc4a7a4f99a6136c718d2d94a87768deb68cfa85'
+  sha256 '4147954fa8dc47d0e4f1393452091a4900ba091edcf8c90252ccb40d0e7a78a3'
 
   # downloads.sourceforge.net/doublecmd was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version}.qt.x86_64.dmg"
+  url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version}.cocoa.x86_64.zip"
   appcast 'https://sourceforge.net/projects/doublecmd/rss'
   name 'Double Commander'
   homepage 'https://doublecmd.sourceforge.io/'
