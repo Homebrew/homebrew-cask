120eefe0eda1176cc62e101373c2af512f5615f9
Vítor Galvão <info@vitorgalvao.com>
2019-07-08 18:53:48 +0100

    Update hook.rb

--- Casks/hook.rb
+++ Casks/hook.rb
@@ -1,13 +1,11 @@
 cask 'hook' do
-  version '2545'
+  version '2545,1562389569'
   sha256 'd867ab7bff6dc2a7fc6046cd9975d3ba0f523692ea38a77f7b6a17e93be51b01'
 
   # dl.devmate.com/com.cogsciapps.hook/ was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.cogsciapps.hook/#{version}/1562389569/Hook-#{version}.dmg"
+  url "https://dl.devmate.com/com.cogsciapps.hook/#{version.before_comma}/#{version.after_comma}/Hook-#{version.before_comma}.dmg"
   appcast 'https://updates.devmate.com/com.cogsciapps.hook.xml'
-  # the url is badwrongandbroken, because the pubDate in the appcast is (varyingly) 2-4 minutes earlier than the
-  # unix epoch string in the URL.
-  name 'Hook for macOS'
+  name 'Hook'
   homepage 'https://hookproductivity.com/'
 
   auto_updates true
