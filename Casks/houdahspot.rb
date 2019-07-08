ea9d3f13e8e8ae1f81e38800b60ab83fe1660e95
Randall <ran.dall@icloud.com>
2019-07-08 10:47:16 -0700

    Update houdahspot from 5.0.6 to 5.0.7

--- Casks/houdahspot.rb
+++ Casks/houdahspot.rb
@@ -1,8 +1,8 @@
 cask 'houdahspot' do
-  version '5.0.6'
-  sha256 '78fdaefec8e49d04a95f1fa06b3692ecd800ca7e3bf0bc810f3b112e7745beee'
+  version '5.0.7'
+  sha256 'df57bc36042450d9213afe9c28eba4796f186344ea7379028066f9b721416d13'
 
-  url "https://www.houdah.com/houdahSpot/updates/cast#{version.major}_assets/HoudahSpot#{version}.zip"
+  url "https://www.houdah.com/HoudahSpot#{version}.zip"
   appcast "https://www.houdah.com/houdahSpot/updates/cast#{version.major}.xml"
   name 'HoudahSpot'
   homepage 'https://www.houdah.com/houdahSpot/'
