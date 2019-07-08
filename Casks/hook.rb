868b5da26be84c918b16421b8b2db8ee15294a2c
Vítor Galvão <info@vitorgalvao.com>
2019-07-08 18:54:17 +0100

    Update hook.rb

--- Casks/hook.rb
+++ Casks/hook.rb
@@ -2,7 +2,7 @@ cask 'hook' do
   version '2545,1562389569'
   sha256 'd867ab7bff6dc2a7fc6046cd9975d3ba0f523692ea38a77f7b6a17e93be51b01'
 
-  # dl.devmate.com/com.cogsciapps.hook/ was verified as official when first introduced to the cask
+  # dl.devmate.com/com.cogsciapps.hook was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.cogsciapps.hook/#{version.before_comma}/#{version.after_comma}/Hook-#{version.before_comma}.dmg"
   appcast 'https://updates.devmate.com/com.cogsciapps.hook.xml'
   name 'Hook'
