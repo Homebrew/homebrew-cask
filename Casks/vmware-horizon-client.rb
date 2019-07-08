c66e02fa4ebba653dcbc712c6d0719b591357aa9
suschizu <brigitte@corecode.io>
2019-07-08 18:43:05 +0200

    Update vmware-horizon-client.rb

--- Casks/vmware-horizon-client.rb
+++ Casks/vmware-horizon-client.rb
@@ -3,7 +3,8 @@ cask 'vmware-horizon-client' do
   sha256 '2341c1e1265b38117b74660d748b4625edf1248bfc86932523d1abf947b005db'
 
   url "https://download3.vmware.com/software/view/viewclients/#{version.after_comma}/VMware-Horizon-Client-#{version.before_comma}.dmg"
-  appcast 'https://docs.vmware.com/en/VMware-Horizon-Client-for-Mac/'
+  appcast 'https://docs.vmware.com/en/VMware-Horizon-Client-for-Mac/',
+          configuration: version.major_minor
   name 'VMware Horizon Client'
   homepage 'https://www.vmware.com/'
 
