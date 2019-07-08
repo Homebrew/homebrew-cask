3d3db3eb25bb2103ab955ac5780af88c24c7a28b
CoreCode <core-code@users.noreply.github.com>
2019-07-08 19:57:45 +0200

    Update mightytext (#65918)

--- Casks/mightytext.rb
+++ Casks/mightytext.rb
@@ -3,6 +3,7 @@ cask 'mightytext' do
   sha256 'fe47c32999ed1d98fa0f8824b345a2a0776f5342f8170ddb71a26e26049698d9'
 
   url "https://dl-desktop.mightytext.net/MightyText-#{version}.dmg"
+  appcast 'https://mightytext.net/mac-desktop-download'
   name 'MightyText'
   homepage 'https://mightytext.net/'
 
