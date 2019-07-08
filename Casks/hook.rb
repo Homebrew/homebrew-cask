901a0a2c13833f2c653de861d5ea71258ec4f925
Eric <github@ideolo.gy>
2019-07-08 11:15:21 -0700

    Add Hook 1.0-beta build 2545 (#65890)
    * Add Hook 1.0-beta build 2545
    
    This adds CogSci Productivity's Hook.app; the url is not fully correct
    (see comments), and it is still in beta until July 10th, but this
    should be a good start.
    
    * Update hook.rb
    
    * Update hook.rb

---
+++ Casks/hook.rb
@@ -0,0 +1,25 @@
+cask 'hook' do
+  version '2545,1562389569'
+  sha256 'd867ab7bff6dc2a7fc6046cd9975d3ba0f523692ea38a77f7b6a17e93be51b01'
+
+  # dl.devmate.com/com.cogsciapps.hook was verified as official when first introduced to the cask
+  url "https://dl.devmate.com/com.cogsciapps.hook/#{version.before_comma}/#{version.after_comma}/Hook-#{version.before_comma}.dmg"
+  appcast 'https://updates.devmate.com/com.cogsciapps.hook.xml'
+  name 'Hook'
+  homepage 'https://hookproductivity.com/'
+
+  auto_updates true
+  depends_on macos: '>= :sierra'
+
+  app 'Hook.app'
+
+  uninstall launchctl: 'com.cogsciapps.hookautolaunchhelper',
+            quit:      'com.cogsciapps.hook'
+
+  zap trash:
+             [
+               '~/Library/Caches/com.cogsciapps.hook',
+               '~/Library/Logs/com.cogsciapps.hook',
+               '~/Library/Preferences/com.cogsciapps.hook.plist',
+             ]
+end
