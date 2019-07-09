bbf1054a4665385859e02ea3069c059683f01c14
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2019-07-09 14:49:19 +0100

    Update flash-player-debugger-ppapi from 32.0.0.207 to 32.0.0.223 (#65984)

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger-ppapi' do
-  version '32.0.0.207'
-  sha256 'b1fbe7cfcb4997b6e6c0b2e3301a011223a9f8b8aefd7a65a6b1f9049c048cf6'
+  version '32.0.0.223'
+  sha256 'b94452464056faeee58f34fc6f772e183e1e181aade3f686a8a1c24f4e91ba97'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml',
