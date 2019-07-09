61e33ea4e9e3b2cdf714d9fa52762904cc134190
Sayo Melu <sayo-melu@outlook.com>
2019-07-09 23:15:31 +0800

    Add app-cleaner-uninstaller

---
+++ Casks/app-cleaner-uninstaller.rb
@@ -0,0 +1,20 @@
+cask 'app-cleaner-uninstaller' do
+    version :latest
+    sha256 :no_check
+  
+    url 'https://nektony.com/download/app-cleaner-uninstaller/app-cleaner-uninstaller.dmg'
+    name 'App Cleaner & Uninstaller'
+    homepage 'https://nektony.com/mac-app-cleaner'
+  
+    app 'App Cleaner & Uninstaller.app'
+  
+    zap trash: [
+                 '~/Library/Application\ Scripts/com.nektony.App-Cleaner-Pro'
+                 '~/Library/Application\ Support/com.nektony.App-Cleaner-Pro',
+                 '~/Library/Caches/com.nektony.App-Cleaner-Pro ',
+                 '~/Library/Containers/com.nektony.StartupDiskFull'
+                 '~/Library/Preferences/com.nektony.App-Cleaner-Pro.plist'
+                 '~/Library/Saved\ Application\ State/com.nektony.App-Cleaner-Pro.savedState'
+               ]
+  end
+  
