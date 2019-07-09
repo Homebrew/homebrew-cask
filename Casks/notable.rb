d89d2c3763e7ab91b66fb501d46393508bcf8dbb
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-09 11:25:54 -0700

    Update notable from 1.6.0 to 1.6.1 (#66001)

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -1,6 +1,6 @@
 cask 'notable' do
-  version '1.6.0'
-  sha256 '825a306ed960423cf9d2c4970edbdcea5432dd04cf031ee83f00ad340be49aa7'
+  version '1.6.1'
+  sha256 'b898f849ac10c7ce0b836f2afab7e51f176dfb68a55c30aae6d0a8026513e588'
 
   url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg"
   appcast 'https://github.com/notable/notable/releases.atom'
