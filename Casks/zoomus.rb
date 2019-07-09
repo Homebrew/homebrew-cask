80eb8b3b0c49a06071591ca39c162132b02dae9c
Vítor Galvão <info@vitorgalvao.com>
2019-07-09 12:15:11 +0100

    Update zoomus.rb

--- Casks/zoomus.rb
+++ Casks/zoomus.rb
@@ -24,6 +24,7 @@ cask 'zoomus' do
             delete:     [
                           '/Applications/zoom.us.app'
                           '~/.zoomus/ZoomOpener.app',
+                          '~/Library/Internet Plug-Ins/ZoomUsPlugIn.plugin'
                         ]
 
   zap trash: [
