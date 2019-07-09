4dd14e06b7cfa1dc244af91db63a40ea0f41ef56
Vítor Galvão <info@vitorgalvao.com>
2019-07-09 12:23:48 +0100

    Update chromium.rb

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask 'chromium' do
   version '675525'
-  sha256 'f4a594a2623d2479d0afc733893f3b57ec24b9c88061d94637e3adb44ca89ae5' 
+  sha256 'f4a594a2623d2479d0afc733893f3b57ec24b9c88061d94637e3adb44ca89ae5'
 
   # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac was verified as official when first introduced to the cask
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
