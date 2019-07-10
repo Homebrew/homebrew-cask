67aad9f08f5522074ef1b6f7fcd7f6f16f14efdc
Slawek Niedziela <niedziela@users.noreply.github.com>
2019-07-10 11:51:18 +0200

    Update double-commander (#66045)

--- Casks/double-commander.rb
+++ Casks/double-commander.rb
@@ -1,6 +1,6 @@
 cask 'double-commander' do
   version '0.9.5-8947'
-  sha256 '4147954fa8dc47d0e4f1393452091a4900ba091edcf8c90252ccb40d0e7a78a3'
+  sha256 '6caa7dfdb28be70f26a4588d16b70df460220257a9a7f3260532e148eda6d65f'
 
   # downloads.sourceforge.net/doublecmd was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/doublecmd/doublecmd-#{version}.cocoa.x86_64.zip"
