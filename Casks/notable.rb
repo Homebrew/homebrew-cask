8af2d69c7ca3a97990b0af3139d097c8d6f222ed
Jérémy CHABERNAUD <djerfy@gmail.com>
2019-07-10 12:29:37 +0200

    Update notable from 1.6.1 to 1.6.2

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -1,6 +1,6 @@
 cask 'notable' do
-  version '1.6.1'
-  sha256 'b898f849ac10c7ce0b836f2afab7e51f176dfb68a55c30aae6d0a8026513e588'
+  version '1.6.2'
+  sha256 '3549c247834300deefe81b280d602bec5de82e64cae93ea77d8c73a1f8af00ec'
 
   url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg"
   appcast 'https://github.com/notable/notable/releases.atom'
