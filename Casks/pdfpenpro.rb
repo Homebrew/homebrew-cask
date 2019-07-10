c55ab2843e9624b89f93d1adac98ce770c83f659
Work <brigitte@corecode.io>
2019-07-10 23:03:29 +0200

    Update pdfpenpro from 1103.3,1560869374 to 1110.2,1562632601

--- Casks/pdfpenpro.rb
+++ Casks/pdfpenpro.rb
@@ -1,6 +1,6 @@
 cask 'pdfpenpro' do
-  version '1103.3,1560869374'
-  sha256 '3b2530928201bd2197fa884c00c0f05c4b06d62d5670ef407cc2b284c5e6e924'
+  version '1110.2,1562632601'
+  sha256 '7cfa2436fa7d17f1bf0d5b24e6a6bc3717dfbfd78260540a55576d4f10a601c0'
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/#{version.before_comma}/#{version.after_comma}/PDFpenPro-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml'
