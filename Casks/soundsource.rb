c5701a9b780c21ba87a148762232ab2393759143
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-11 14:33:12 -0700

    Update soundsource from 4.1 to 4.1.1 (#66130)

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,6 +1,6 @@
 cask 'soundsource' do
-  version '4.1'
-  sha256 '4482d24a4700d5978457a5736095ba6644184eb6c43ea8c0c4628520a483655b'
+  version '4.1.1'
+  sha256 '36f7b5547d378edbbd5de575dc1fdef32d7f2dfd9fbebe2ef8f6419f3545450a'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&version=4008000'
