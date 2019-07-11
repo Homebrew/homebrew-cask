b345b0cd645f9ce33ba7fb5dc07d72f0c14f861d
Randall <ran.dall@icloud.com>
2019-07-11 15:20:38 -0700

    Update wechatwork from 2.8.7.2050 to 2.8.8.2002

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,6 +1,6 @@
 cask 'wechatwork' do
-  version '2.8.7.2050'
-  sha256 '9cfd4ee3d6a48ad72662b47dc09b1f9c24e7cb161b9c515a8c7bc29e607fdaef'
+  version '2.8.8.2002'
+  sha256 '06bf1c8205ef491ee9f8991b6ab4662723316f8eedd76cba7d35f9e42bd0c6b7'
 
   url "https://dldir1.qq.com/foxmail/work_weixin/WXWork_#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://work.weixin.qq.com/wework_admin/commdownload?platform=mac'
