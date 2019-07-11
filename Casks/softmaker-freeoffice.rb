cf2a42ab29119865560b14966baa47b23bb312d5
Vítor Galvão <info@vitorgalvao.com>
2019-07-11 16:13:02 +0100

    Update softmaker-freeoffice.rb

--- Casks/softmaker-freeoffice.rb
+++ Casks/softmaker-freeoffice.rb
@@ -1,13 +1,13 @@
 cask 'softmaker-freeoffice' do
-  version :latest
-  sha256 :no_check
+  version '2018'
+  sha256 :no_check # required as upstream package is updated in-place
 
   # softmaker.net was verified as official when first introduced to the cask
-  url 'https://www.softmaker.net/down/softmaker-freeoffice-2018.pkg'
-  name 'SoftMaker FreeOffice 2018'
+  url "https://www.softmaker.net/down/softmaker-freeoffice-#{version}.pkg"
+  name 'SoftMaker FreeOffice'
   homepage 'https://www.freeoffice.com/'
 
-  pkg 'softmaker-freeoffice-2018.pkg'
+  pkg "softmaker-freeoffice-#{version}.pkg"
 
-  uninstall pkgutil: 'com.SoftMaker.FreeOffice2018'
+  uninstall pkgutil: "com.SoftMaker.FreeOffice#{version}"
 end
