f6877b784c429a83107307b0b8ce4d7a08cbdc96
Viktor Szakats <commit@vszakats.net>
2019-07-11 21:50:34 +0000

    Secure URLs

--- Casks/vitamin-r.rb
+++ Casks/vitamin-r.rb
@@ -7,10 +7,10 @@ cask 'vitamin-r' do
     sha256 '3abea1f8b4c63cdd51fc451238b000a428e26c0a4c110706e858ed217b21589d'
   end
 
-  url "http://www.publicspace.net/download/signedVitamin#{version.major}.zip"
-  appcast "http://www.publicspace.net/app/vitamin#{version.major}.xml"
+  url "https://www.publicspace.net/download/signedVitamin#{version.major}.zip"
+  appcast "https://www.publicspace.net/app/vitamin#{version.major}.xml"
   name 'Vitamin-R'
-  homepage 'http://www.publicspace.net/Vitamin-R/'
+  homepage 'https://www.publicspace.net/Vitamin-R/'
 
   auto_updates true
 
