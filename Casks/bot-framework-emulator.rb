93a90e0b7d2c522a5382eade7fd379b664fda3a1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-12 08:25:22 -0700

    Update bot-framework-emulator from 4.4.2 to 4.5.0 (#66156)

--- Casks/bot-framework-emulator.rb
+++ Casks/bot-framework-emulator.rb
@@ -1,6 +1,6 @@
 cask 'bot-framework-emulator' do
-  version '4.4.2'
-  sha256 '55bcaa619a6f150b885f66144322d5b1c907839799a553e5c27137e342084f57'
+  version '4.5.0'
+  sha256 '36a5e6cd0f0c555dbb54835889874efe19010a569e32da34ccc89853fcf44e3a'
 
   url "https://github.com/Microsoft/BotFramework-Emulator/releases/download/v#{version}/botframework-emulator-#{version}-mac.zip"
   appcast 'https://github.com/Microsoft/BotFramework-Emulator/releases.atom'
