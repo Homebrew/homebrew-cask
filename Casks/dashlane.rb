c7a79cba92b71f321a08f593657e12eb07bef50a
brigitte zmoelnig <brigitte@corecode.io>
2019-07-12 15:53:48 +0200

    Update dashlane from 6.1922.0.20633 to 6.1927.0.21578

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,8 +1,9 @@
 cask 'dashlane' do
-  version '6.1922.0.20633'
-  sha256 'af0a59b059d2ec8f56bf4ffd036002189fae938fc80913d6eabad33a90f884cb'
+  version '6.1927.0.21578'
+  sha256 '2fcb3f8d3ab35423430fc39ecd7bf01d876123864adff69723d4d086e06d7cd5'
 
-  url "https://cdn5.dashlane.com/proxy/d3mfqat9ni8wb5/releases/#{version.major_minor_patch}/#{version}/qa-safariAppExtension/Dashlane.dmg"
+  # d3mfqat9ni8wb5.cloudfront.net/releases was verified as official when first introduced to the cask
+  url "https://d3mfqat9ni8wb5.cloudfront.net/releases/#{version.major_minor_patch}/#{version}/release/Dashlane.dmg"
   appcast 'https://ws1.dashlane.com/5/binaries/query?format=json&os=OS_X_10_14_5&target=archive&platform=launcher_macosx'
   name 'Dashlane'
   homepage 'https://www.dashlane.com/'
