2596ce8b27e77d3f0ec5ce34cbb6a1024d89b90a
Randall <ran.dall@icloud.com>
2019-07-12 08:44:55 -0700

    Update deezer from 4.10.2 to 4.11.2

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -1,6 +1,6 @@
 cask 'deezer' do
-  version '4.10.2'
-  sha256 '66acb3f7e00d7b818e4180b6ad0ff31eccdae6e229090949a3da358d6da4a745'
+  version '4.11.2'
+  sha256 'a4e2df87827a4983e20fdd9cfc1b36ea6a8298fa98c3ca675872f63f80f10bee'
 
   url "https://www.deezer.com/desktop/download/artifact/darwin/x64/#{version}"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.deezer.com/desktop/download%3Fplatform%3Ddarwin%26architecture=x64'
