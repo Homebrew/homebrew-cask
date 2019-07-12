8019d4ea2353f56750dd733dd604945a1fb1b500
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-12 08:55:27 -0700

    Update trilium-notes from 0.33.6 to 0.33.7 (#66165)

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask 'trilium-notes' do
-  version '0.33.6'
-  sha256 'cb41390dc359c9878b6b16b4acd40c9ec0ae58fad20747279d57ba7902d61ca4'
+  version '0.33.7'
+  sha256 'abf03a88988b51d22856180636bbf4f7f2f7365e1b1a98e6ec12f882923374e7'
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   appcast 'https://github.com/zadam/trilium/releases.atom'
