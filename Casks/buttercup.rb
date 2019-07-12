41b2ff55b71dbed1ec40d8ec45c76ad329574b2b
Randall <ran.dall@icloud.com>
2019-07-12 08:17:46 -0700

    Update buttercup from 1.15.0 to 1.16.0

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -1,6 +1,6 @@
 cask 'buttercup' do
-  version '1.15.0'
-  sha256 '6405030505909def7703aa517b1bf04f266bb3f005ed3d14bb80b5ea5b3fd39d'
+  version '1.16.0'
+  sha256 '5add60cd1fa14d29d83447600dc12e2db4256e4fff6a7f4df68934dd0a3dc78d'
 
   # github.com/buttercup/buttercup-desktop was verified as official when first introduced to the cask
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-#{version}-mac.zip"
