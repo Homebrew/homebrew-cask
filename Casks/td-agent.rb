a79cecab41bd4b919bf54c957bc34a17fdb864a5
Vítor Galvão <info@vitorgalvao.com>
2019-07-12 13:46:13 +0100

    Update td-agent.rb

--- Casks/td-agent.rb
+++ Casks/td-agent.rb
@@ -3,7 +3,7 @@ cask 'td-agent' do
   sha256 'aea92474070fc973315228dde287a32111d29cb52f4a94bc7f73cfa73d88afc8'
 
   # s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/packages.treasuredata.com/#{version.major}/macosx/td-agent-#{version}.dmg"
+  url "http://packages.treasuredata.com.s3.amazonaws.com/#{version.major}/macosx/td-agent-#{version}.dmg"
   appcast "https://td-agent-package-browser.herokuapp.com/#{version.major}/macosx"
   name 'td-agent'
   homepage 'https://www.fluentd.org/'
