05a92993dfc0ace23279e00914ec448f87a6cb6e
Randall <ran.dall@icloud.com>
2019-07-13 15:26:39 -0700

    Update sqlworkbenchj from 124 to 125.1

--- Casks/sqlworkbenchj.rb
+++ Casks/sqlworkbenchj.rb
@@ -1,6 +1,6 @@
 cask 'sqlworkbenchj' do
-  version '124'
-  sha256 '2173c7f00172bef3fed23e7f57e168a9d156c72c311af032ba469139f80d9fe9'
+  version '125.1'
+  sha256 '06b34fbeef8a3bbf6cca17a922b2be050dc16a6e8900ea8e50d9968dfaa05407'
 
   url "http://www.sql-workbench.net/Workbench-Build#{version}-Mac.tgz"
   appcast 'http://www.sql-workbench.net/wb_news.xml'
