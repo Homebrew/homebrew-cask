f5eef23d470b3a092b3cec88edefb5d8cf147d7b
Dewayne McNair <dmcnair@imac.local>
2019-07-13 09:55:21 -0500

    Add Phiewer.app v2.0.3

---
+++ Casks/phiewer.rb
@@ -0,0 +1,10 @@
+cask 'phiewer' do
+  version '2.0.3'
+  sha256 'b9ef9894d6ee079ef44d8df07a4e261996ab1e9a723ca807b637711fa17cee61'
+
+  url 'https://phiewer.com/wp-content/uploads/Phiewer-2.0.3.dmg'
+  name 'Phiewer'
+  homepage 'https://phiewer.com/'
+
+  app 'Phiewer.app'
+end
