d9800521552f1cdef4c2009f30c7b667847fbcec
Randall <ran.dall@icloud.com>
2019-07-13 15:10:43 -0700

    Update thedesk from 18.6.7 to 18.7.0

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask 'thedesk' do
-  version '18.6.7'
-  sha256 '1feed8cf06330f2e41a7ae25466db93d72816058c43d17bdeafffd9fe96abdcb'
+  version '18.7.0'
+  sha256 '12d2a42c0f35b150044df90dc22753f95d42eab6fef916acedbd11b92a37a3c8'
 
   # github.com/cutls/TheDesk was verified as official when first introduced to the cask
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}.dmg"
