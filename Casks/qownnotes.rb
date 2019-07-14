7e60ebf81e56cfa1cbe3c69aa8ddde71f439bca7
brigitte zmoelnig <brigitte@corecode.io>
2019-07-14 18:40:09 +0200

    Update qownnotes from 19.7.2,b4361-141852 to 19.7.3,b4369-074114

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '19.7.2,b4361-141852'
-  sha256 '20ba7d2f7ca6d81b8988309f503052e4a9903121eade26ae6d809a9efb8cdb28'
+  version '19.7.3,b4369-074114'
+  sha256 '0688903277ebedd07e5acfcebfe9ded2ab36aa7e0db9644cc0da4e7d35183e69'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
