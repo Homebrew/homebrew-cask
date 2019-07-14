739b5ec20cbf6475ea9e9381c25f12ca7464d46b
Dewayne McNair <dmcnair@imac.local>
2019-07-14 09:06:54 -0500

    Interpolate the version into the url definition

--- Casks/phiewer.rb
+++ Casks/phiewer.rb
@@ -2,7 +2,7 @@ cask 'phiewer' do
   version '2.0.3'
   sha256 'b9ef9894d6ee079ef44d8df07a4e261996ab1e9a723ca807b637711fa17cee61'
 
-  url 'https://phiewer.com/wp-content/uploads/Phiewer-2.0.3.dmg'
+  url "https://phiewer.com/wp-content/uploads/Phiewer-#{version}.dmg"
   name 'Phiewer'
   homepage 'https://phiewer.com/'
 
