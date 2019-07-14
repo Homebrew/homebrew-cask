af47acc4ca7f229cc86a2beaf57173ce60cf0a0a
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-14 11:07:32 -0700

    Update stretchly from 0.20.0 to 0.20.1 (#66224)

--- Casks/stretchly.rb
+++ Casks/stretchly.rb
@@ -1,6 +1,6 @@
 cask 'stretchly' do
-  version '0.20.0'
-  sha256 'efc14533c61f1313dd45ca0edbd7b00b7357ff4e4626e581cf188e252512b58c'
+  version '0.20.1'
+  sha256 '1c51f0aeb01bde477c23c7f9908e14729a6661fff78060f3bdda22b9e4609ea5'
 
   # github.com/hovancik/stretchly was verified as official when first introduced to the cask
   url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}-mac.zip"
