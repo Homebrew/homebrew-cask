c3bfcf691e8b8e5ed9ba2bfb32e72636b26ac5b1
Kyungwook Park <parksama@gmail.com>
2019-07-15 11:17:34 +0900

    Add Movist Pro.app

---
+++ Casks/movist-pro.rb
@@ -0,0 +1,10 @@
+cask 'movist-pro' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://d2uukrxj8lf22z.cloudfront.net/MovistPro.app.zip'
+  name 'Movist Pro'
+  homepage 'https://movistprime.com/kr/'
+
+  app 'Movist Pro.app'
+end
