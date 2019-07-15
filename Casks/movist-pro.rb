929ca8c35088dcb705412b81f638450817b4c23e
Kyungwook Park <parksama@gmail.com>
2019-07-15 11:26:32 +0900

    movist-pro.rb: updated vendor comment and license

--- Casks/movist-pro.rb
+++ Casks/movist-pro.rb
@@ -2,9 +2,11 @@ cask 'movist-pro' do
   version :latest
   sha256 :no_check
 
+  # d2uukrxj8lf22z.cloudfront.net was verified as official when first introduced to the cask
   url 'https://d2uukrxj8lf22z.cloudfront.net/MovistPro.app.zip'
   name 'Movist Pro'
   homepage 'https://movistprime.com/kr/'
+  license :commercial
 
   app 'Movist Pro.app'
 end
