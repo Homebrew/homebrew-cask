4656f598e02ed7277e25d2c8c08664cbda35476f
David Yantis <404NetworkError@users.noreply.github.com>
2019-07-15 11:09:46 -0400

    Update mullvadvpn from 2019.5 to 2019.6 (#66257)

--- Casks/mullvadvpn.rb
+++ Casks/mullvadvpn.rb
@@ -1,6 +1,6 @@
 cask 'mullvadvpn' do
-  version '2019.5'
-  sha256 '1cdd17d32a54480a768d9d9b23114f402b8c4e2f81c67027145409ef20e5cd58'
+  version '2019.6'
+  sha256 'ee2997e3be380644a3588262bb75868eb68d98470ba7999f261c94fae1ed4328'
 
   # github.com/mullvad/mullvadvpn-app was verified as official when first introduced to the cask
   url "https://github.com/mullvad/mullvadvpn-app/releases/download/#{version}/MullvadVPN-#{version}.pkg"
