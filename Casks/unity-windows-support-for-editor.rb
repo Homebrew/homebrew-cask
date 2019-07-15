c17cbe4dc392c08eb5d97980457afa78348f6f4d
suschizu <brigitte@corecode.io>
2019-07-15 16:18:20 +0200

    Update unity-windows-support-for-editor from 2019.1.9f1,d5f1b37da199 to 2019.1.10f1,f007ed779b7a (#66253)

--- Casks/unity-windows-support-for-editor.rb
+++ Casks/unity-windows-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-windows-support-for-editor' do
-  version '2019.1.9f1,d5f1b37da199'
-  sha256 'fa1923907221ab6ce7bba2fd0b62885609c9fd896d2b43b6ad4c6a0aa78fe6c6'
+  version '2019.1.10f1,f007ed779b7a'
+  sha256 '40d000055368fa9ac2b3bedc360a1c19c054be11baac7a519c6c74021388e9e1'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
