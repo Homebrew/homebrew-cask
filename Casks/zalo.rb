b747d054f729982b3b54914811d31e84e08cdc72
CoreCode <core-code@users.noreply.github.com>
2019-07-15 16:18:23 +0200

    Update zalo.rb (#66246)

--- Casks/zalo.rb
+++ Casks/zalo.rb
@@ -4,6 +4,7 @@ cask 'zalo' do
 
   # res-download-pc-te-vnno-zn-3.zadn.vn/mac was verified as official when first introduced to the cask
   url "https://res-download-pc-te-vnno-zn-3.zadn.vn/mac/ZaloSetup-#{version}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://zalo.me/download/zalo-pc'
   name 'Zalo'
   homepage 'https://zalo.me/'
 
