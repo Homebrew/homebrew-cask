cd0d4a240fbe1c1dba9bc3ca0aa3226bcb45d825
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-15 09:00:43 -0700

    Update slite from 1.1.0 to 1.1.1 (#66265)

--- Casks/slite.rb
+++ Casks/slite.rb
@@ -1,6 +1,6 @@
 cask 'slite' do
-  version '1.1.0'
-  sha256 '45dbfdd803fdee3e6afe5028f793c887d3d1a743f7ffeba4070643fb82dab1ca'
+  version '1.1.1'
+  sha256 '903dce9c987e127b2f0d55f4f489eedf6972e14e6e525a68743df0f6ff31aadd'
 
   # storage.googleapis.com/slite-desktop was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/slite-desktop/mac/Slite-#{version}.dmg"
