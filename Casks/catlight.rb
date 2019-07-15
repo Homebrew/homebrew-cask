212e3e79941f0a73229a9d70e8f0d7d39284ac22
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-15 09:03:59 -0700

    Update catlight from 2.27.4 to 2.28.0 (#66266)

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -1,6 +1,6 @@
 cask 'catlight' do
-  version '2.27.4'
-  sha256 '0467c860101ca33d033a31150d74d399eadd5a25432ab30bb66b20fb786a5fcd'
+  version '2.28.0'
+  sha256 '8fcbd19358bd795f144bfa868ceb5fc1e19be3bce3f3d672cd51adf644a8defc'
 
   # de2nac35bcll0.cloudfront.net was verified as official when first introduced to the cask
   url "https://de2nac35bcll0.cloudfront.net/dl/mac/beta/CatLightSetup-#{version}.zip"
