2cf1e6d15b5b2e5aa251b88e21df580839bf1978
Richard Reader <RJReader@gmail.com>
2019-07-16 21:49:22 +0100

    Update asciidocfx

--- Casks/asciidocfx.rb
+++ Casks/asciidocfx.rb
@@ -1,6 +1,6 @@
 cask 'asciidocfx' do
   version '1.7.0'
-  sha256 'c110cf444860245b672561aa83c2a4edbe802dd17159e1b7d2b5c8f23ac31292'
+  sha256 '3a458f99feced260ada12e1e510f7fec09c57d193e60b1b33baa310cffe4dfd5'
 
   # github.com/asciidocfx/AsciidocFX was verified as official when first introduced to the cask
   url "https://github.com/asciidocfx/AsciidocFX/releases/download/v#{version}/AsciidocFX_Mac.dmg"
