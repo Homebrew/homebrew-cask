aeb6df099b27322b15bcc7bd5c89f636d03a7e9c
jcbot <jcb@leipert.io>
2019-07-16 19:33:36 +0000

    Update rubymine to 2019.1.3,191.7479.66

--- Casks/rubymine.rb
+++ Casks/rubymine.rb
@@ -1,6 +1,6 @@
 cask 'rubymine' do
-  version '2019.1.2,191.7141.58'
-  sha256 'de490531e333002f681a13f074c5815e35455e068dc0b3796b39b4fcb101d22b'
+  version '2019.1.3,191.7479.66'
+  sha256 '687c3f1d84c218e7e44c7d775454c6f40ccff96b537f20ae5ea939233cbafb34'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=RM&latest=true&type=release'
