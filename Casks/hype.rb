c379310c5cad1637ebca19f6c6d7bdf6d363037f
Vítor Galvão <info@vitorgalvao.com>
2019-07-16 18:00:50 +0100

    Update hype.rb

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -3,7 +3,7 @@ cask 'hype' do
   sha256 'f8fcf2cbd5b0e0412c6b7ee00c7f5fe7141514fd075e7acebf2081f2ec2c13d0'
 
   url 'https://tumult.com/hype/download/Hype.zip'
-  appcast 'https://tumult.com/hype/appcast_hype2.xml'
+  appcast "https://tumult.com/hype/appcast_hype#{version.major}.xml"
   name 'Tumult Hype'
   homepage 'https://tumult.com/hype/'
 
