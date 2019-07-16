61ef272f6830d9f35afb42f9d2d0439651f87eb6
Randall <ran.dall@icloud.com>
2019-07-16 09:06:34 -0700

    Update openbazaar from 2.3.3 to 2.3.4

--- Casks/openbazaar.rb
+++ Casks/openbazaar.rb
@@ -1,6 +1,6 @@
 cask 'openbazaar' do
-  version '2.3.3'
-  sha256 'd4e79cc39e2ba13af23816305d2f587b61f83cfedad3988b9eb8047727209ab7'
+  version '2.3.4'
+  sha256 'e3248bd24e0cd57e4eac78b0af59b4fd6aa84375a3965f31205cd3decd46cb9a'
 
   # github.com/OpenBazaar/openbazaar-desktop was verified as official when first introduced to the cask
   url "https://github.com/OpenBazaar/openbazaar-desktop/releases/download/v#{version.before_comma}/OpenBazaar#{version.major}-#{version}.dmg"
