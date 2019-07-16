4cdb20bfd2418b7062f3ef4cc79b09927177f5a3
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-16 10:56:14 -0700

    Update hype from 4.0.0 to 4.0.1 (#66298)
    * Update hype from 4.0.0 to 4.0.1
    
    * Update hype.rb
    
    * Update hype.rb

--- Casks/hype.rb
+++ Casks/hype.rb
@@ -1,12 +1,12 @@
 cask 'hype' do
-  version '4.0.0'
-  sha256 '11c6669185ce0a2fca39abbbb9d16ac5746a4e6ba3e27ad1f504069df6ea3cdf'
+  version '4.0.1'
+  sha256 'f8fcf2cbd5b0e0412c6b7ee00c7f5fe7141514fd075e7acebf2081f2ec2c13d0'
 
   url 'https://tumult.com/hype/download/Hype.zip'
-  appcast 'https://tumult.com/hype/appcast_hype2.xml'
+  appcast "https://tumult.com/hype/appcast_hype#{version.major}.xml"
   name 'Tumult Hype'
   homepage 'https://tumult.com/hype/'
 
   # Renamed for consistency: app name is different in the Finder and in a shell
-  app 'Hype4.app', target: "Hype #{version.major}.app"
+  app "Hype#{version.major}.app", target: "Hype #{version.major}.app"
 end
