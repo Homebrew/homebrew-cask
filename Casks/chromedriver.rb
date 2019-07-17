b475f2915813f7469694d6f9ed3a96a612e5a9d5
Mikko Drocan <mikko.drocan@iki.fi>
2019-07-17 15:17:08 +0300

    Update chromedriver

--- Casks/chromedriver.rb
+++ Casks/chromedriver.rb
@@ -1,6 +1,6 @@
 cask 'chromedriver' do
-  version '75.0.3770.90'
-  sha256 '594f2a9f0475cad6b53fc0f482727e769da734e0549c3d4a1f168dc80cbd0ef7'
+  version '75.0.3770.140'
+  sha256 '34eda7761e4059c0289a81c2a83ad3c63f5187afa2726b8f7b5f5e635275c406'
 
   # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
