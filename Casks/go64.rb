62fd98be7861ac307c8c7813464f0e86ac9e24f9
Vítor Galvão <info@vitorgalvao.com>
2019-07-17 14:21:09 +0100

    Update go64.rb

--- Casks/go64.rb
+++ Casks/go64.rb
@@ -1,4 +1,5 @@
 cask 'go64' do
+  # note: "64" is not a version number, but an intrinsic part of the product name
   version '1.0.1'
   sha256 '538110901c2e8ebe59a4e5df4c431139a9c02e21a4c574f61647f8bc582ef81e'
 
