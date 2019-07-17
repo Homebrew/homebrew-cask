6dd43a6a98eec1f7091bda0bdf93e2ce5f5b7e8b
Julian Mayer <julian@corecode.io>
2019-07-17 18:19:19 +0200

    Update cmake from 3.14.5 to 3.14.6

--- Casks/cmake.rb
+++ Casks/cmake.rb
@@ -1,6 +1,6 @@
 cask 'cmake' do
-  version '3.14.5'
-  sha256 'a1dd64edb6036fdaa365ed0cd850e437ce6ab2e67da817f080376a2aa05eefc8'
+  version '3.14.6'
+  sha256 '0e43450c2808121316204e5ff560054e10c84148fe1cb07368833d7c4b1f3b72'
 
   url "https://www.cmake.org/files/v#{version.major_minor}/cmake-#{version}-Darwin-x86_64.dmg"
   appcast 'https://cmake.org/files/LatestRelease/'
