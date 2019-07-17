510146b59759a2168e4aab15365a7f6ef67b32f7
Vítor Galvão <info@vitorgalvao.com>
2019-07-17 14:27:18 +0100

    Update uc-one.rb

--- Casks/uc-one.rb
+++ Casks/uc-one.rb
@@ -5,8 +5,6 @@ cask 'uc-one' do
   # storage.googleapis.com/ucone_saas_and_meet_clients was verified as official when first introduced to the cask
   url 'https://storage.googleapis.com/ucone_saas_and_meet_clients/ucone/mac/latest/UC-One.dmg'
   name 'Cisco UC-One'
-  name 'UC-One'
-  name 'UC One'
   homepage 'https://uc-one.com/'
 
   app 'UC-One.app'
