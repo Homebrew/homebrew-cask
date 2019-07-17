be9f24ea5dd6697fca3ce8c5d507f77845712bbc
Randall <ran.dall@icloud.com>
2019-07-17 08:49:05 -0700

    Update clip-studio-paint from 1.9.0 to 1.9.2

--- Casks/clip-studio-paint.rb
+++ Casks/clip-studio-paint.rb
@@ -1,6 +1,6 @@
 cask 'clip-studio-paint' do
-  version '1.9.0'
-  sha256 'c005010ca84d0aa88503aa9a81a89cc3ebe6f762533a713dceacd769391f5669'
+  version '1.9.2'
+  sha256 '77ed576d4305f918a6dbb66279bbc0ff01595490fbae41130c2d6760a5f80a1e'
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
   appcast 'https://www.clipstudio.net/en/dl'
