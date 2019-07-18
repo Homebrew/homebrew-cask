450e36bd138ccea2cdd51f91a3b9860a656fcf86
Randall <ran.dall@icloud.com>
2019-07-18 09:01:47 -0700

    Update zotero from 5.0.70 to 5.0.71

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask 'zotero' do
-  version '5.0.70'
-  sha256 '1ee127d1c732c4d45e280fa6d3fca0ebe330ca46c9bcd9afac0edf9e2d746d41'
+  version '5.0.71'
+  sha256 '1b2e235df7b9dfde9dcdb09f55d64a8fef4ba3b6875721b36edf016d62e4283a'
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast 'https://github.com/zotero/zotero/releases.atom'
