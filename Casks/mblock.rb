2208271e7e1de24a9f4956d50ddf2b2dccd16690
CoreCode <core-code@users.noreply.github.com>
2019-07-18 10:25:59 +0200

    Update mblock.rb

--- Casks/mblock.rb
+++ Casks/mblock.rb
@@ -1,6 +1,6 @@
 cask 'mblock' do
   version '5.1.0'
-  sha256 '3543c2691422edb9213773fd92da5898a8f8cdc7c9b490d0c997c60209f97032'
+  sha256 '03de14bb9a2f2b8784b627ab1611aa04b7d8290fafa39115708891bf3c3178bf'
 
   # dl.makeblock.com was verified as official when first introduced to the cask
   url "https://dl.makeblock.com/mblock#{version.major}/darwin/V#{version}.pkg"
