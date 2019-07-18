9bc21ef90150e3b921ebabd8aa11e62eec9b4db3
Randall <ran.dall@icloud.com>
2019-07-18 08:55:51 -0700

    Update lockrattler from 4.21,2019.06 to 4.22,2019.07

--- Casks/lockrattler.rb
+++ Casks/lockrattler.rb
@@ -1,6 +1,6 @@
 cask 'lockrattler' do
-  version '4.21,2019.06'
-  sha256 '74e8d2f7b7e8dd0b07725e71006f2cac77fcf04231f04c530faf431206c66497'
+  version '4.22,2019.07'
+  sha256 'd9f2da8b1cbf996acda2572ad8a23920758448dc4efbc7bbb1c13c9d23f10905'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/lockrattler#{version.before_comma.no_dots}.zip"
