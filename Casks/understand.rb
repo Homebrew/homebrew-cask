83f5889a5b8132d2978f12d7c5a838973f196274
Qi Zhang <singularitti@outlook.com>
2019-07-18 03:02:11 -0400

    Update understand from 5.1.991 to 5.1.992 (#66397)

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask 'understand' do
-  version '5.1.991'
-  sha256 '5229efd8fe46fd593e3d77a15f789d38c60deaf84717f90d2a06faddab8adc37'
+  version '5.1.992'
+  sha256 'ca4f0bc398fc1484061037cd0bc11a966eda3a0f0136337ebcb27f5426fd0b0d'
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
   appcast 'https://scitools.com/build-notes/',
