6723fe3e02325fb4263a3c315f39085c41ea50ce
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-19 09:04:02 -0700

    Update keymanager from 2.2.0 to 2.5.0 (#66481)

--- Casks/keymanager.rb
+++ Casks/keymanager.rb
@@ -1,6 +1,6 @@
 cask 'keymanager' do
-  version '2.2.0'
-  sha256 '2a95a3c4b6ab0919fee7bab8015a6f40beb97efd6e574877ab86b295b7df03f3'
+  version '2.5.0'
+  sha256 '82874c3b541cdc4322a67c58482628ceebf4cdf5b15622eb26b6dc43693fdc72'
 
   # keymanager.trustasia.com was verified as official when first introduced to the cask
   url "https://keymanager.trustasia.com/release/KeyManager-#{version}.dmg"
