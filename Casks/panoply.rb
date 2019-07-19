ad5a7a82c3b631c5d886c268b3095837e0f78a46
CoreCode <core-code@users.noreply.github.com>
2019-07-19 11:12:37 +0200

    Update panoply.rb (#66429)

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -3,6 +3,7 @@ cask 'panoply' do
   sha256 '59ea977cab9ea6128ee63aee20d38d79947039c6cd00b757b22bc4a39673558c'
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
+  appcast 'https://www.giss.nasa.gov/tools/panoply/download/'
   name 'Panoply netCDF, HDF and GRIB Data Viewer'
   homepage 'https://www.giss.nasa.gov/tools/panoply/'
 
