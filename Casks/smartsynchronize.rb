902d0034c525ef9809f84616fda91cd5cb5d01ea
suschizu <brigitte@corecode.io>
2019-07-19 14:41:42 +0200

    Update smartsynchronize.rb

--- Casks/smartsynchronize.rb
+++ Casks/smartsynchronize.rb
@@ -3,7 +3,8 @@ cask 'smartsynchronize' do
   sha256 '0a88efcebd9e969fb1a72b04b483d89253a0eb6b8548c2f4e9aec1be0168e793'
 
   url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-macosx-#{version.before_comma}.dmg"
-  appcast 'https://www.syntevo.com/smartsynchronize/changelog.txt'
+  appcast 'https://www.syntevo.com/smartsynchronize/changelog.txt',
+          configuration: version.major_minor
   name 'SmartSynchronize'
   homepage 'https://www.syntevo.com/smartsynchronize/'
 
