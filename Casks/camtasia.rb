1d2b2950887e1e82f7d14a8f6366d50e70434de6
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-19 09:01:01 -0700

    Update camtasia from 2019.0.1 to 2019.0.2 (#66478)

--- Casks/camtasia.rb
+++ Casks/camtasia.rb
@@ -1,6 +1,6 @@
 cask 'camtasia' do
-  version '2019.0.1'
-  sha256 '4ac81733639e962ef72ea160d6bdbc0f5cedd3a2fb1d74ce909f0504032c15c2'
+  version '2019.0.2'
+  sha256 'e41a329d9d6ee9808ad0dfdfe2e0f655ce6fef538607b2e0935109f4513bff19'
 
   url 'https://download.techsmith.com/camtasiamac/releases/Camtasia.dmg'
   appcast 'https://support.techsmith.com/hc/en-us/articles/115006624748-Camtasia-Mac-Version-History'
