a31369f6bc0316622b283ce547395b7185133392
Vítor Galvão <info@vitorgalvao.com>
2019-07-19 15:27:05 +0100

    Update pivy.rb

--- Casks/pivy.rb
+++ Casks/pivy.rb
@@ -12,6 +12,4 @@ cask 'pivy' do
   uninstall pkgutil:   'net.cooperi.pivy',
             launchctl: 'net.cooperi.pivy-agent',
             quit:      'net.cooperi.pivy-agent'
-
-  caveats 'After installing the program itself, the installer will prompt you to insert a YubiKey or other PIV token using a dialog box. Then it will generate a user launchd service to run the agent for you, and add lines to /etc/profile to default to using it in place of the Keychain agent. The pivy- programs will also be added to your PATH, so they should be accessible from any terminal. You’ll find them in /opt/pivy if you need them for any other reason.'
 end
