629377b7948460644aef9a5c469d692668d57d65
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-20 12:43:19 -0700

    Update hubstaff from 1.4.7,938 to 1.4.9,1136 (#66511)
    * Update hubstaff from 1.4.7,938 to 1.4.9,86828309
    
    * Update `sha256`
    
    * Update `sha256` again
    
    * Update `version`

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,6 +1,6 @@
 cask 'hubstaff' do
-  version '1.4.7,938'
-  sha256 'd6116e698a2e6341150f077232cdb9bcc08f021cbd979d78acd81eb2e4e3d05b'
+  version '1.4.9,1136'
+  sha256 '0d6b2a79f032f87068b6bd6b847242b0d7dd958ee33c34c767a022771b2872ae'
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
   appcast 'https://app.hubstaff.com/appcast.xml'
