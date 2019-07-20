9a186632f1b2d977664e4c0edfd06c94a1b0794d
Julian Mayer <julian@corecode.io>
2019-07-20 22:32:48 +0200

    Update jami from 201907191319 to 20190719.1319

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -1,8 +1,8 @@
 cask 'jami' do
-  version '201907191319'
+  version '20190719.1319'
   sha256 'bd40a94d31371756a4d7ecdeca49e0592f0d20a786ce60589e97be7091293fb6'
 
-  url "https://dl.ring.cx/mac_osx/jami-#{version}.dmg"
+  url "https://dl.ring.cx/mac_osx/jami-#{version.no_dots}.dmg"
   appcast 'https://dl.ring.cx/mac_osx/sparkle-ring.xml'
   name 'Jami'
   name 'Savoir-faire Linux Ring'
