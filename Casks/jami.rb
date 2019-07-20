8a5d204c6a3ac1b2063e71d98db0d45db3395949
CoreCode <core-code@users.noreply.github.com>
2019-07-20 23:13:50 +0200

    Update jami.rb

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -3,7 +3,7 @@ cask 'jami' do
   sha256 'bd40a94d31371756a4d7ecdeca49e0592f0d20a786ce60589e97be7091293fb6'
 
   url "https://dl.ring.cx/mac_osx/jami-#{version.no_dots}.dmg"
-  appcast 'https://dl.ring.cx/mac_osx/sparkle-ring.xml'
+  appcast 'https://dl.ring.cx/mac_osx/sparkle-ring.xml',
           configuration: version.no_dots
   name 'Jami'
   name 'Savoir-faire Linux Ring'
