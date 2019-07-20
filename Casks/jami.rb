458a4d18eba61e87e2ef258cc7f37d895606fe26
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-20 12:39:36 -0700

    Update jami from 20190602.0216 to 201907191319 (#66502)
    * Update jami from 20190602.0216 to 201907191319
    
    * Update jami.rb

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -1,10 +1,9 @@
 cask 'jami' do
-  version '20190602.0216'
-  sha256 'fc813559c65295796fd5b9b69da31cec4a5e98fea5213e792f7fcfa9c2dbcf9c'
+  version '201907191319'
+  sha256 'bd40a94d31371756a4d7ecdeca49e0592f0d20a786ce60589e97be7091293fb6'
 
-  url "https://dl.ring.cx/mac_osx/ring-#{version.no_dots}.dmg"
-  appcast 'https://dl.ring.cx/mac_osx/sparkle-ring.xml',
-          configuration: version.major
+  url "https://dl.ring.cx/mac_osx/jami-#{version}.dmg"
+  appcast 'https://dl.ring.cx/mac_osx/sparkle-ring.xml'
   name 'Jami'
   name 'Savoir-faire Linux Ring'
   homepage 'https://ring.cx/'
