92e571fbb02a6e6143a15e127b4fb00a28380ee1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-20 12:37:00 -0700

    Update `version`

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,5 +1,5 @@
 cask 'hubstaff' do
-  version '1.4.9,86828309'
+  version '1.4.9,1136'
   sha256 '0d6b2a79f032f87068b6bd6b847242b0d7dd958ee33c34c767a022771b2872ae'
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
