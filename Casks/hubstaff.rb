f5a9d0bbf8723eefaaa0041939d65fb0183c3c01
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-20 12:27:02 -0700

    Update `sha256` again

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,6 +1,6 @@
 cask 'hubstaff' do
   version '1.4.9,86828309'
-  sha256 '53af33ebf4ac2b69d0b729c58088713f4071e15ef23b57d8646b74bba822706b'
+  sha256 '0d6b2a79f032f87068b6bd6b847242b0d7dd958ee33c34c767a022771b2872ae'
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
   appcast 'https://app.hubstaff.com/appcast.xml'
