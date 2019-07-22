c6cf6df8c00cc66ab7449df6299f382550fdbc9b
Masahiro Nagae <two.big.suns@gmail.com>
2019-07-23 02:50:04 +0900

    Update clip-studio-paint.rb checksum

--- Casks/clip-studio-paint.rb
+++ Casks/clip-studio-paint.rb
@@ -1,6 +1,6 @@
 cask 'clip-studio-paint' do
   version '1.9.2'
-  sha256 '77ed576d4305f918a6dbb66279bbc0ff01595490fbae41130c2d6760a5f80a1e'
+  sha256 'baf0481456ed8be3b0240192a04c049bc5226e005e66a649316bc7e638abf174'
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
   appcast 'https://www.clipstudio.net/en/dl'
