dbd2afc6c675547fc615771aee54e4eb7d63ff9d
Vítor Galvão <info@vitorgalvao.com>
2019-07-22 18:03:27 +0100

    Update qcad.rb

--- Casks/qcad.rb
+++ Casks/qcad.rb
@@ -2,7 +2,7 @@ cask 'qcad' do
   version '3.23.0'
 
   if MacOS.version <= :high_sierra
-    sha256 '0003262298641edd4c996cb6312b2340b94c692e998c2c350a5b75f490f74ae2'
+    sha256 '8668cf4407331bac9cdfeed2a735eb34a9bf8b29d9982c693416d0c36ab412c7'
     url "https://www.qcad.org/archives/qcad/qcad-#{version}-trial-macos-10.10-10.13.dmg"
   else
     sha256 '50e721239e6384f836e360c527b0d2b855bbe19da00512620f5ed059274f1daa'
