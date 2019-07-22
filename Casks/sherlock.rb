ce8f01d30b99bace7f58ceb631c10907560d79eb
CoreCode <core-code@users.noreply.github.com>
2019-07-22 22:54:54 +0200

    Update sherlock.rb

--- Casks/sherlock.rb
+++ Casks/sherlock.rb
@@ -3,6 +3,7 @@ cask 'sherlock' do
   sha256 '429ae9abd216d657987acf0b91f502edaeb3d560dd93cef9d88f5310c5e30c7e'
 
   url 'https://sherlock.inspiredcode.io/download-dmg'
+  appcast 'https://updates.devmate.com/io.inspiredcode.Sherlock.xml'
   name 'Sherlock'
   homepage 'https://sherlock.inspiredcode.io/'
 
