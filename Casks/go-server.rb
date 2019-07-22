1a91912096aeaa725dfc2b74acd62f915e344bfd
Randall <ran.dall@icloud.com>
2019-07-22 09:20:40 -0700

    Update go-server from 19.1.0-8469 to 19.6.0-9515

--- Casks/go-server.rb
+++ Casks/go-server.rb
@@ -1,6 +1,6 @@
 cask 'go-server' do
-  version '19.1.0-8469'
-  sha256 '6f7d6ce6875d51ab7194834ec836bc502a405b82c90b9359df520850850a27ee'
+  version '19.6.0-9515'
+  sha256 '63fef043098a072517022ee08a84a9b631d33487a8eb252e2826807f220af1ce'
 
   # download.gocd.io/binaries was verified as official when first introduced to the cask
   url "https://download.gocd.io/binaries/#{version}/osx/go-server-#{version}-osx.zip"
