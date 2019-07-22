ac7bcc60c0e964b7775c2948bae60d0647b03020
Randall <ran.dall@icloud.com>
2019-07-22 08:14:14 -0700

    Update kube-forwarder from 1.2.0 to 1.3.0

--- Casks/kube-forwarder.rb
+++ Casks/kube-forwarder.rb
@@ -1,6 +1,6 @@
 cask 'kube-forwarder' do
-  version '1.2.0'
-  sha256 '4951a5f7afcd14172a7ad66c50bb636e7a83ac67d76f2a39e59dae8df9f20459'
+  version '1.3.0'
+  sha256 'aabb0997d1bfcf575b39538623362399eb19e82774bdbb42d16f75c8c0e800c5'
 
   # github.com/pixel-point/kube-forwarder was verified as official when first introduced to the cask
   url "https://github.com/pixel-point/kube-forwarder/releases/download/v#{version}/kube-forwarder.dmg"
