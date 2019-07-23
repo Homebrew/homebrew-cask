0e5d52c9be6277344d418f7b121666f5e1eecfc7
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2019-07-23 17:20:16 +0200

    Update burn from 2.6.7 to 2.6.8 (#66614)

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -1,9 +1,9 @@
 cask 'burn' do
-  version '2.6.7'
-  sha256 'ba40371d2b0e557496ecdc61bfaa6778e6b174c0cab4a5c5baa66fa219d7960c'
+  version '2.6.8'
+  sha256 '58858c2cec125ea0aeb410b04e5465e613459c03059353d1d0df6bb32d193703'
 
   # downloads.sourceforge.net/burn-osx was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/burn-osx/Burn/#{version}/burn#{version.no_dots}.zip"
+  url "https://downloads.sourceforge.net/burn-osx/Burn/#{version}/burn-#{version}.zip"
   appcast 'https://sourceforge.net/projects/burn-osx/rss?path=/Burn'
   name 'Burn'
   homepage 'https://burn-osx.sourceforge.io/'
