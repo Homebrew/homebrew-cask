9ba67efe2c5001133b59213a20fe8d77f252388f
CoreCode <core-code@users.noreply.github.com>
2019-07-23 11:37:53 +0200

    Update pingplotter from latest to 5.10.4 (#66571)

--- Casks/pingplotter.rb
+++ Casks/pingplotter.rb
@@ -1,8 +1,9 @@
 cask 'pingplotter' do
-  version :latest
-  sha256 :no_check
+  version '5.10.4'
+  sha256 '3793de83f80f75e309a2c54f51353a32b27cc9c2e21dd5777094d1c24db464ea'
 
   url 'https://www.pingplotter.com/downloads/pingplotter_osx.zip'
+  appcast 'https://www.pingplotter.com/download'
   name 'PingPlotter'
   homepage 'https://www.pingplotter.com/'
 
