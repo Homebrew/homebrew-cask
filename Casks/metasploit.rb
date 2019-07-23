682673987fbe1c8778b6f3df46a795fb676afd7c
bleepnetworks <bleepnetworks@users.noreply.github.com>
2019-07-23 23:53:55 +0700

    update metasploit to 5.0.38,20190723103437

--- Casks/metasploit.rb
+++ Casks/metasploit.rb
@@ -1,6 +1,6 @@
 cask 'metasploit' do
-  version '5.0.38,20190719103443'
-  sha256 'b32bc261bf8c5140831274bb74ab5d947af062ca59eba2d7b6540e7c7a8cbacf'
+  version '5.0.38,20190723103437'
+  sha256 '63d39b031c1564ab1faf37c7ca9c7b68752921ccb9f308400ed05620f0b931bf'
 
   url "https://osx.metasploit.com/metasploit-framework-#{version.before_comma}+#{version.after_comma}-1rapid7-1.pkg"
   appcast 'https://osx.metasploit.com/LATEST'
