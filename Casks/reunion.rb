497ab9ccd64b69a1a9b8f24c85531590bd6c1755
Randall <ran.dall@icloud.com>
2019-07-23 10:33:26 -0700

    Update reunion from 12.0.0.181218 to 12.0.0.190722

--- Casks/reunion.rb
+++ Casks/reunion.rb
@@ -1,6 +1,6 @@
 cask 'reunion' do
-  version '12.0.0.181218'
-  sha256 '6c4946169eb2f57d3726615265b29a6483ddfbccf19e610f832c5fadba8fd1e6'
+  version '12.0.0.190722'
+  sha256 '436b9ef132b711973a6f7a980609bf6d69e000d732a090ac10e4e6d2a471ce73'
 
   url "https://store.leisterpro.com/updates/reunion#{version.major}/Reunion-#{version.dots_to_hyphens}.zip"
   appcast "https://store.leisterpro.com/updates/reunion#{version.major}/appcast.xml"
