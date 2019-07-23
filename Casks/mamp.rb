a429b58a411cf2926a1f996cc9de867d56aa2520
Ben Seven <ben@kin.works>
2019-07-24 00:29:28 +0100

    Update mamp

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,6 +1,6 @@
 cask 'mamp' do
   version '5.4'
-  sha256 'dcb1f34de2c0c6d201f95198c38df1536c32d14713cc6dc0f4212eab93302e90'
+  sha256 '3c5483d8f96f90f5c4f44bd7f313a1681bfc81e79e328242dd18c45358f8b483'
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
   appcast 'https://www.mamp.info/en/downloads/'
