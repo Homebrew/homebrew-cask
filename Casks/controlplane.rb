d8f41d037e420cec086c5fa2476cae7c9a8bb6be
CoreCode <core-code@users.noreply.github.com>
2019-07-23 22:37:42 +0200

    Update controlplane.rb

--- Casks/controlplane.rb
+++ Casks/controlplane.rb
@@ -1,7 +1,8 @@
 cask 'controlplane' do
-  version '1.6.6'
-  sha256 'a9c9cdfe350bc9f038764a0b4c8f4337750113f44aacd68aa81b6f0aa391af1b'
+  version '1.6.7'
+  sha256 'beb444b8117ed91898921a6babc384501dfd92679f5b718fceb6d3aa7a0bf651'
 
+  # uc7623c46083c7e293e1e735ccb3.dl.dropboxusercontent.com was verified as official when first introduced to the cask
   url "https://uc7623c46083c7e293e1e735ccb3.dl.dropboxusercontent.com/cd/0/get/AlPevCimNr-vZXgD2_x2Mh7r-YuYblzCbC9lL3zOCypF0DLpyhWuwPeMwMuWXbaMKhSh0xEY99Ba7iV4adZdJc6QEH4OiuoYWowGiBqY23hYxg/file?dl=1"
   name 'ControlPlane'
   homepage 'https://www.controlplaneapp.com/'
