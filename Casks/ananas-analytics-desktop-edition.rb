d0fdabb7e8955050714423435cad706349a5e16b
Vítor Galvão <info@vitorgalvao.com>
2019-07-23 16:07:44 +0100

    Update and rename ananas-desktop.rb to ananas-analytics-desktop-edition.rb

--- Casks/ananas-analytics-desktop-edition.rb
+++ Casks/ananas-analytics-desktop-edition.rb
@@ -1,9 +1,9 @@
-cask 'ananas-desktop' do
+cask 'ananas-analytics-desktop-edition' do
   version '0.8.1'
   sha256 '2a5d6373369dfe2308fedcfb8ae3264ef02ace3fb905b20b20014021be53b153'
 
   # github.com/ananas-analytics/ananas-desktop was verified as official when first introduced to the cask
-  url 'https://github.com/ananas-analytics/ananas-desktop/releases/download/v0.8.1/Ananas.Analytics.Desktop.Edition-0.8.1.macos.dmg'
+  url "https://github.com/ananas-analytics/ananas-desktop/releases/download/v#{version}/Ananas.Analytics.Desktop.Edition-#{version}.macos.dmg"
   appcast 'https://github.com/ananas-analytics/ananas-desktop/releases.atom'
   name 'Ananas Analytics Desktop Edition'
   homepage 'https://ananasanalytics.com/'
