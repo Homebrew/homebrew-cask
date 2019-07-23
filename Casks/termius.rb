3e88518534656ffb612a48ea6a11417f849cea16
brigitte zmoelnig <brigitte@corecode.io>
2019-07-23 18:20:40 +0200

    Update termius from 4.9.2 to 4.9.5

--- Casks/termius.rb
+++ Casks/termius.rb
@@ -1,6 +1,6 @@
 cask 'termius' do
-  version '4.9.2'
-  sha256 '56b437f9d523dd5b2033a695e4b217e912f396d2c3fb268e39569dc8b1ab0cc9'
+  version '4.9.5'
+  sha256 '47604c550fd3be817a22f81d815b7ade460cd065584eaec6f257b5046ef6b0bb'
 
   # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/termius.desktop.autoupdate/mac/Termius.dmg'
