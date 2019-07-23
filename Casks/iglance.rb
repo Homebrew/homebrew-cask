ef97e706d9ac261f426d41cb098223b48f4f5c39
Dominik <36657497+D0miH@users.noreply.github.com>
2019-07-23 19:23:29 +0200

    Update iglance from 1.3.4 to 1.3.5

--- Casks/iglance.rb
+++ Casks/iglance.rb
@@ -1,6 +1,6 @@
 cask 'iglance' do
-  version '1.3.4'
-  sha256 '0f4dc62f40d207075143c4e56aff504f92677e4165426c2067d6f8bb8f6b3b05'
+  version '1.3.5'
+  sha256 'eed57f2c81522836bdae440d09c970551df9d64faa326df3467f9922c517d48b'
 
   url "https://github.com/iglance/iglance/releases/download/v#{version}/iGlance.dmg"
   appcast 'https://github.com/iglance/iglance/releases.atom'
