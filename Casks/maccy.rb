e23d7a9ca07813fe2024fff1e4ee67c485fc0e1b
Saeed Seyfi <me@saeedseyfi.com>
2019-07-23 12:44:57 +0200

    Use single qoutes

--- Casks/maccy.rb
+++ Casks/maccy.rb
@@ -2,7 +2,7 @@ cask 'maccy' do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/p0deje/Maccy/releases/latest/download/Maccy.app.zip"
+  url 'https://github.com/p0deje/Maccy/releases/latest/download/Maccy.app.zip'
   appcast 'https://github.com/p0deje/Maccy/releases.atom'
   name 'Maccy'
   homepage 'https://github.com/p0deje/Maccy'
