04f9003af560766c720d2c35db7ed539f6bb5a03
suschizu <brigitte@corecode.io>
2019-07-23 16:38:30 +0200

    Update birdfont.rb

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -3,8 +3,8 @@ cask 'birdfont' do
     version '2.19.4'
     sha256 '013d9c42c2252b57079453bd27e4c18dbbc09eda55563ff1516fd079c0499f76'
   else
-    version '3.27.1'
-    sha256 '77c0bb2407ffb19e6fe3f33924e30044697ee1bc7f3d803b97ec034cb98daebf'
+    version '3.28.1'
+    sha256 '548c6a0a037f758569b25bd0c40d26e6114bb9c48df4da72e171bf35c7bfff85'
   end
 
   url "https://birdfont.org/download/birdfont-#{version}-free.dmg"
