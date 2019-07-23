33baae5e43bf89fe110e0ac5aef3cca3a4148639
Saeed Seyfi <me@saeedseyfi.com>
2019-07-23 12:23:58 +0200

    Update maccy (version :latest, sha256 :no_check)

--- Casks/maccy.rb
+++ Casks/maccy.rb
@@ -1,8 +1,8 @@
 cask 'maccy' do
-  version '0.3.6'
-  sha256 '314d56cddd34a89443af3995b1a3ddbc63a8d18db12b043309b76ded260c5d83'
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/p0deje/Maccy/releases/download/#{version}/Maccy.app.zip"
+  url "https://github.com/p0deje/Maccy/releases/latest/download/Maccy.app.zip"
   appcast 'https://github.com/p0deje/Maccy/releases.atom'
   name 'Maccy'
   homepage 'https://github.com/p0deje/Maccy'
