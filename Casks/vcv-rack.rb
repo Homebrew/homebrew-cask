ad4868dc5ff6981d788719373e74144fef0854ff
Hannes Braun <hannesbraun@mail.de>
2019-07-24 13:33:41 +0200

    Update vcv-rack from 1.1.2 to 1.1.3

--- Casks/vcv-rack.rb
+++ Casks/vcv-rack.rb
@@ -1,6 +1,6 @@
 cask 'vcv-rack' do
-  version '1.1.2'
-  sha256 'd6b43853aaaf31e6306a915d836f7c620ee348f8bb341f9ea0be6e33b2e8afc6'
+  version '1.1.3'
+  sha256 '80de61ee62994aeab016170832792e10ecb20dc0275258f7cfc1d48c7246c3e7'
 
   url "https://vcvrack.com/downloads/Rack-#{version}-mac.zip"
   appcast 'https://github.com/VCVRack/Rack/releases.atom'
