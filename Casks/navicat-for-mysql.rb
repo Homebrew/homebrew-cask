731cb788dbbefcb3210a81bd6b5159d192234f3d
Randall <ran.dall@icloud.com>
2019-07-24 10:54:54 -0700

    Update navicat-for-mysql from 12.1.23 to 12.1.24

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-mysql' do
-  version '12.1.23'
-  sha256 '276dddf8873a0dfcaa33453f55421c91366355be9c776396960c894b2f4d76f7'
+  version '12.1.24'
+  sha256 '8c3500d98ed49b655147164e7536c55abfc49da9d3f00f4ca2fadfa2efe197f9'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20for%20MySQL&appLang=en'
