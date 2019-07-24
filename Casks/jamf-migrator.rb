fe728add0170f2d41ea788bde5cb1f6f81b8827d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-24 10:49:18 -0700

    Update jamf-migrator from 3.3.4 to 3.3.5 (#66681)

--- Casks/jamf-migrator.rb
+++ Casks/jamf-migrator.rb
@@ -1,6 +1,6 @@
 cask 'jamf-migrator' do
-  version '3.3.4'
-  sha256 '13ccb9dc0b6a3bd24fd29bcc3bf717fe8acbc9a79b00060319493013d18c064c'
+  version '3.3.5'
+  sha256 'd33922f2542339393cb75d084c7aae8ce52019f997016f7730ad63b25603763d'
 
   url 'https://github.com/jamf/JamfMigrator/releases/download/current/jamf-migrator.zip'
   appcast 'https://github.com/jamf/JamfMigrator/releases.atom'
