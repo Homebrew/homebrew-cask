8d3645373cde16a9dfa159819627f36d1b02f268
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-24 10:57:25 -0700

    Update cryptomator from 1.4.11 to 1.4.12 (#66683)

--- Casks/cryptomator.rb
+++ Casks/cryptomator.rb
@@ -1,6 +1,6 @@
 cask 'cryptomator' do
-  version '1.4.11'
-  sha256 '7beb112bbd2597781e48be3cd9dd729e093431245236e011326fa969328280b4'
+  version '1.4.12'
+  sha256 'f6a25e46358d764105bc11178d13c22afc2838d273f18b97a093c97567c5fe8d'
 
   # dl.bintray.com/cryptomator/cryptomator was verified as official when first introduced to the cask
   url "https://dl.bintray.com/cryptomator/cryptomator/#{version}/Cryptomator-#{version}.dmg"
