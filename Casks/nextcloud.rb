921cf62ea05c3d0d6ee8497c85b05f3f1c561b49
CoreCode <core-code@users.noreply.github.com>
2019-07-25 22:32:52 +0200

    Update nextcloud.rb (#66656)

--- Casks/nextcloud.rb
+++ Casks/nextcloud.rb
@@ -3,7 +3,7 @@ cask 'nextcloud' do
   sha256 '42a912766160edf4d0900ebc648f3304656e7eb57a2036de4756113a99ccb2cc'
 
   url "https://download.nextcloud.com/desktop/releases/Mac/Installer/Nextcloud-#{version}.pkg"
-  appcast 'https://github.com/nextcloud/desktop/releases.atom'
+  appcast 'https://download.nextcloud.com/desktop/releases/Mac/Installer/'
   name 'Nextcloud'
   homepage 'https://nextcloud.com/'
 
