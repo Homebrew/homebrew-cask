7a1f56bab51f3c6dbf1354ff9c14379720134a90
Thom Williams <thomscode@gmail.com>
2019-07-26 09:00:14 -0700

    Update version for LastPass to 4.31.1

--- Casks/lastpass.rb
+++ Casks/lastpass.rb
@@ -1,5 +1,5 @@
 cask 'lastpass' do
-  version '4.31.0'
+  version '4.31.1'
   sha256 'b35fec64bd6528918827759bb62454bc4410e2d3c32eb03df999e6a106de5225'
 
   url 'https://download.cloud.lastpass.com/mac/LastPass.dmg'
