6ea5442a61120ede99601961b3e46ff861bf19f9
CoreCode <core-code@users.noreply.github.com>
2019-07-26 23:00:53 +0200

    Update integrity from 9.0.5 to 9.0.6 (#66831)

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -1,5 +1,5 @@
 cask 'integrity' do
-  version '9.0.5'
+  version '9.0.6'
   sha256 'a674ea5e2fd7715d99bc2907cda23a7f9444dfda35e8908146c6d0fb5e2a43de'
 
   # peacockmedia.co.uk/integrity was verified as official when first introduced to the cask
