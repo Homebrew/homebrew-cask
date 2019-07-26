d17b76d5a33e4dc9098ddc38a9d7ee926b3a8014
Vítor Galvão <info@vitorgalvao.com>
2019-07-26 12:04:27 +0100

    Update podolski.rb

--- Casks/podolski.rb
+++ Casks/podolski.rb
@@ -1,13 +1,13 @@
 cask 'podolski' do
-  version '1.2.1.3898'
+  version '1.2.1,3898'
   sha256 '2fb8736267a01aca6e65cdc855d8159a21ab25dcc2f81290a5e2f413b5062fe8'
 
   # uhedownloads-heckmannaudiogmb.netdna-ssl.com was verified as official when first introduced to the cask
-  url "https://uhedownloads-heckmannaudiogmb.netdna-ssl.com/releases/Podolski_#{version.major_minor_patch.dots_to_underscores}_Mac.zip"
+  url "https://uhedownloads-heckmannaudiogmb.netdna-ssl.com/releases/Podolski_#{version.before_comma.dots_to_underscores}_Mac.zip"
   name 'Podolski'
   homepage 'https://u-he.com/products/podolski/'
 
-  pkg "Podolski#{version.split('.')[3]}Mac/Podolski #{version} Installer.pkg"
+  pkg "Podolski#{version.after_comma}Mac/Podolski #{version.before_comma}.#{version.after_comma} Installer.pkg"
 
   uninstall pkgutil: [
                        'com.u-he.Podolski.aax.pkg',
