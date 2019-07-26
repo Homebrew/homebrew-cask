13c2749677988bccf0e765d51c55041605d555d1
Vítor Galvão <info@vitorgalvao.com>
2019-07-26 12:11:05 +0100

    Update powerpanel.rb

--- Casks/powerpanel.rb
+++ Casks/powerpanel.rb
@@ -1,11 +1,6 @@
 cask 'powerpanel' do
-  if MacOS.version <= :sierra
-    version '1.0.9'
-    sha256 '7b021f9cf182afd04fa25522cee115b271e462f73fa9c76571681e6b4ebda36d'
-  else
-    version '1.1.0'
-    sha256 '9ef3160ad516cf246647a11bdbeaa756a98b2c0d9915ccabbf0d6cc72bebdf51'
-  end
+  version '1.1.0'
+  sha256 '9ef3160ad516cf246647a11bdbeaa756a98b2c0d9915ccabbf0d6cc72bebdf51'
 
   # dl4jz3rbrsfum.cloudfront.net was verified as official when first introduced to the cask
   url "https://dl4jz3rbrsfum.cloudfront.net/software/ppm#{version.no_dots}.dmg"
