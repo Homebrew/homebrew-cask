4046e6454af3cb8a91de406d9fdad9fa306c8407
Johannes Klein <jo@temka.de>
2019-07-26 14:32:55 +0200

    Update microsoft-word
    add zap stanza (adapted from microsoft-office cask)

--- Casks/microsoft-word.rb
+++ Casks/microsoft-word.rb
@@ -15,13 +15,37 @@ cask 'microsoft-word' do
 
   uninstall pkgutil:   [
                          'com.microsoft.package.Microsoft_Word.app',
-                         'com.microsoft.OneDrive',
-                         'com.microsoft.package.DFonts',
                          'com.microsoft.package.Microsoft_AutoUpdate.app',
                          'com.microsoft.pkg.licensing',
                        ],
             launchctl: [
-                         'com.microsoft.office.licensing.helper',
                          'com.microsoft.office.licensingV2.helper',
                        ]
+  zap trash:     [
+                   '~/Library/Application Scripts/com.microsoft.Word',
+                   '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.word.sfl*',
+                   '~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate.fba',
+                   '~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate2',
+                   '~/Library/Caches/com.microsoft.autoupdate.fba',
+                   '~/Library/Caches/com.microsoft.autoupdate2',
+                   '~/Library/Containers/com.microsoft.Word',
+                   '~/Library/Cookies/com.microsoft.autoupdate.fba.binarycookies',
+                   '~/Library/Cookies/com.microsoft.autoupdate2.binarycookies',
+                   '~/Library/Preferences/com.microsoft.Word.plist',
+                   '~/Library/Preferences/com.microsoft.autoupdate.fba.plist',
+                   '~/Library/Preferences/com.microsoft.autoupdate2.plist',
+                   '~/Library/Saved Application State/com.microsoft.autoupdate2.savedState',
+                 ],
+      rmdir:     [
+                   '~/Library/Caches/Microsoft/uls',
+                   '~/Library/Caches/Microsoft',
+                 ],
+      launchctl: [
+                   'com.microsoft.autoupdate.helpertool',
+                   'com.microsoft.autoupdate.helper',
+                   'com.microsoft.update.agent',
+                 ],
+      pkgutil:   [
+                   'com.microsoft.package.Microsoft_AutoUpdate.app',
+                 ]
 end
