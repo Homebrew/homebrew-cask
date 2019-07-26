5f4a33190bb0c219152f76df4bcd7c8546f78740
brigitte zmoelnig <brigitte@corecode.io>
2019-07-26 21:52:45 +0200

    Update touchdesigner from 099.2019.16600 to 099.2019.17550

--- Casks/touchdesigner.rb
+++ Casks/touchdesigner.rb
@@ -1,6 +1,6 @@
 cask 'touchdesigner' do
-  version '099.2019.16600'
-  sha256 'd10e67535300864d98ed02db42abff132001694b03d39ef05e0c87cd4aba6ab9'
+  version '099.2019.17550'
+  sha256 '135261081607355f7829ceb7662396934a52ecb61e6593acd2c58d06776f7c9f'
 
   url "https://www.derivative.ca/Builds/TouchDesigner#{version}.dmg"
   appcast "https://www.derivative.ca/#{version.major}/Downloads/Default.asp"
