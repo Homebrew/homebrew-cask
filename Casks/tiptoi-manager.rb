a748436ec0c3348ed1fec3d8d97d9b6922a1fe9c
Philipp Schaer <philipp.schaer@googlemail.com>
2019-07-28 22:53:34 +0200

    Add TipToi-Manager v1.4.2

---
+++ Casks/tiptoi-manager.rb
@@ -0,0 +1,22 @@
+cask 'tiptoi-manager' do
+  version '4.1.2'
+  sha256 '6f3d1a109cffb49c2942243a2ec1101561970fbeb57166f45b00972ce7e72c65'
+
+  url 'https://tiptoidata.s3-de.object.vdc.interoute.com/installer_flat/tiptoi_Manager_Installer.pkg'
+  name 'tiptoi® Manager'
+
+  homepage 'https://www.ravensburger.de/entdecken/ravensburger-marken/tiptoi/tiptoi-manager/index.html'
+
+  pkg 'tiptoi_Manager_Installer.pkg'
+
+  uninstall pkgutil: 'com.ravensburger.tiptoi.TipToiBindings',
+            delete: '/Applications/tiptoi®\ Manager.app'
+
+  zap trash: [
+              '/Applications/tiptoi®\ Manager.app',
+              '~/Library/Application\ Support/com.ravensburger.tiptoimanager',
+              '~/Library/Preferences/com.ravensburger.tiptoimanager.plist',
+              '~/Library/Saved\ Application\ State/com.ravensburger.tiptoimanager.savedState'
+             ]
+
+end
