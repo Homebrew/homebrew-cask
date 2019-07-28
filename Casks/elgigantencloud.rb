6e9dd78ea66e1bc750e58862381e46b335b2cdb5
Cubixzno1 <Christian.maribo@gmail.com>
2019-07-28 13:32:03 +0200

    Add Elgiganten Cloud Latest

---
+++ Casks/elgigantencloud.rb
@@ -0,0 +1,10 @@
+cask 'elgigantencloud' do
+  version '3.2.90.974'
+  sha256 '91de8580658b66f99f9d0a84d18eb8024aef6a9c4a2c3958a0268afbd278390c'
+
+  url 'https://cloud.elgiganten.dk/update/elgiganten/current/elgiganten.dmg'
+  name 'Elgiganten Cloud'
+  homepage 'https://cloud.elgiganten.dk/'
+
+  app 'Jotta.app'
+end
