1c9c07b1488400163c0d9a3df36240eda4a01f81
Vítor Galvão <info@vitorgalvao.com>
2019-07-29 13:12:47 +0100

    Update mtmr.rb

--- Casks/mtmr.rb
+++ Casks/mtmr.rb
@@ -1,7 +1,7 @@
 cask 'mtmr' do
   version '0.22.1'
   sha256 'a01ea28460bf9e2af847d4b8f8d9837f4119e91ae70ee4647af5f47ed1bc01c7'
-  
+
   url "https://mtmr.app/MTMR%20#{version}.dmg"
   appcast 'https://mtmr.app/appcast.xml'
   name 'My TouchBar. My rules'
