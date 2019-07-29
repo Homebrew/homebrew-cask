484a010429af39d1a2063b562f447520be823438
Hannes Braun <hannesbraun@mail.de>
2019-07-29 19:02:28 +0200

    Update workflowy from 1.3.2 to 1.3.3

--- Casks/workflowy.rb
+++ Casks/workflowy.rb
@@ -1,6 +1,6 @@
 cask 'workflowy' do
-  version '1.3.2'
-  sha256 '6a88353160b4d365dc43275e7e5a31a18750495cc254fee7f14fcd155c86d45c'
+  version '1.3.3'
+  sha256 '6d27f2460906594866c2bf7308844857b329de32801f0a77ee5416bc2fa3ace0'
 
   # github.com/workflowy/desktop was verified as official when first introduced to the cask
   url "https://github.com/workflowy/desktop/releases/download/v#{version}/WorkFlowy.dmg"
