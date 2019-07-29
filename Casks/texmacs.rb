8076e4c219d73526b66f21ed768c7d1640f624ee
Hannes Braun <hannesbraun@mail.de>
2019-07-29 17:58:54 +0200

    Update texmacs from 1.99.10 to 1.99.11

--- Casks/texmacs.rb
+++ Casks/texmacs.rb
@@ -1,6 +1,6 @@
 cask 'texmacs' do
-  version '1.99.10'
-  sha256 '55f5731d72916d7bdd1c1d7b0da5f4bda9db5078eb04e8cf6785df4cb86204b8'
+  version '1.99.11'
+  sha256 '7ebadb569ef05cddb853aa3b70cd354b3a969b30f72914de170133924696cec4'
 
   url "https://www.texmacs.org/Download/ftp/tmftp/macos/TeXmacs-#{version}.dmg"
   appcast 'https://www.texmacs.org/tmweb/download/macosx.en.html'
