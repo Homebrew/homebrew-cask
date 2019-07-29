3ed97d73607cc427c78631449b1b5d6d80846cb2
Vítor Galvão <info@vitorgalvao.com>
2019-07-29 13:28:25 +0100

    Update and rename elgigantencloud.rb to elgiganten-jotta.rb

--- Casks/elgiganten-jotta.rb
+++ Casks/elgiganten-jotta.rb
@@ -1,6 +1,6 @@
-cask 'elgigantencloud' do
-  version '3.2.90.974'
-  sha256 '91de8580658b66f99f9d0a84d18eb8024aef6a9c4a2c3958a0268afbd278390c'
+cask 'elgiganten-jotta' do
+  version :latest
+  sha256 :nocheck
 
   url 'https://cloud.elgiganten.dk/update/elgiganten/current/elgiganten.dmg'
   name 'Elgiganten Cloud'
