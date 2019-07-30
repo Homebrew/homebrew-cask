2cf15c055149c4e180f260d6a6e041de2b13df1b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-07-30 13:36:58 -0700

    Update moneymoney from 2.3.15 to 2.3.16 (#66924)

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,6 +1,6 @@
 cask 'moneymoney' do
-  version '2.3.15'
-  sha256 '25793d98f1b7e31ceb222b900e0874a6d4eebb3b6532075301921445d0e4893e'
+  version '2.3.16'
+  sha256 'de69d4a3ca0db2f1e4d065e05467d47c36db66593feeaaa2c6905b0a9a659708'
 
   url 'https://service.moneymoney-app.com/1/MoneyMoney.zip'
   appcast 'https://service.moneymoney-app.com/1/Appcast.xml'
