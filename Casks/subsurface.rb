5f5ffa03300933219274015f4282d5a02a7a1cbe
Yishen Miao <mys721tx@gmail.com>
2019-07-30 06:09:07 -0400

    Bumping subsurface version to 4.9.0 (#66912)
    Bumps subsurface version to 4.9.0.

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,6 +1,6 @@
 cask 'subsurface' do
-  version '4.8.6'
-  sha256 'f9d2c8cd2dd8205a24cc29e87a674e1160b02a4cfc4669e33ae5e7475917c3eb'
+  version '4.9.0'
+  sha256 'be29cabb510307e427e7c44335915730dcbea7b7f11b09d987bded50262e37c0'
 
   url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
   name 'Subsurface'
