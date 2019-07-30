280e1b94d313f9dbf817bc82e31cdab3335aaa02
Vítor Galvão <info@vitorgalvao.com>
2019-07-30 12:29:32 +0100

    Update elgiganten-jotta.rb

--- Casks/elgiganten-jotta.rb
+++ Casks/elgiganten-jotta.rb
@@ -1,6 +1,6 @@
 cask 'elgiganten-jotta' do
   version :latest
-  sha256 :nocheck
+  sha256 :no_check
 
   url 'https://cloud.elgiganten.dk/update/elgiganten/current/elgiganten.dmg'
   name 'Elgiganten Cloud'
