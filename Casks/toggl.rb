46239cafe7e9f0b20c41f3586cfba34486e2e1c8
Alec Shaw <alecsshaw@gmail.com>
2019-07-30 22:29:17 -0400

    Fix checksum for Toggl v7.4.484

--- Casks/toggl.rb
+++ Casks/toggl.rb
@@ -1,6 +1,6 @@
 cask 'toggl' do
   version '7.4.484'
-  sha256 '8aa8d739138053dec2081bfbf47c4b3f27f73ab810af13a29497bd54846fd2e3'
+  sha256 'a65926853104485ee545595ada8ea5bbd518489e97d97f7bdae04bde87090bfc'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
