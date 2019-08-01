ca2a65bd6039c93dd6e777d2f9c8b8c4d0598843
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-01 15:05:39 -0700

    Update digikam from 6.1.0 to 6.2.0 (#67050)

--- Casks/digikam.rb
+++ Casks/digikam.rb
@@ -1,6 +1,6 @@
 cask 'digikam' do
-  version '6.1.0'
-  sha256 '097cb08e6843c4c44d14dad4740cb157dbf2cbe15f4a828a34f7c278ba59d7bd'
+  version '6.2.0'
+  sha256 '4f0d9c534287f755c59c0ceb00fb56c8b4c5a61bb52301d19f89bcac43d2fe0f'
 
   # kde.org/stable/digikam was verified as official when first introduced to the cask
   url "https://download.kde.org/stable/digikam/#{version}/digiKam-#{version}-MacOS-x86-64.pkg"
