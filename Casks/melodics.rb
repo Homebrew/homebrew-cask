f2995622720172e4a894e6dd8cceef1c72e863b3
ralph <ralph@cs.cf.ac.uk>
2019-08-01 07:09:04 +0100

    Update melodics

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask 'melodics' do
   version '2.1.3183'
-  sha256 '317a7b62d1cf1c35dd83fdbc1390b532d20293b0d16f4024bf556f65091e6ac3'
+  sha256 '93c78881539ffac5456539ba1eb0f97288bac3bd06e9504730a68ad0d1e7077a'
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://web-cdn.melodics.com/download/osxupdatescastv#{version.major}.xml"
