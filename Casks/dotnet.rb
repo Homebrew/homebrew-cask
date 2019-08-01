ab21301283e6f9936fda48fbd8a187c4cd99d7e5
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2019-08-01 09:54:28 -0500

    Update dotnet.rb

--- Casks/dotnet.rb
+++ Casks/dotnet.rb
@@ -1,8 +1,8 @@
 cask 'dotnet' do
-  version '2.2.6'
+  version '2.2.6,7fd8704c-560f-47dc-8fe0-b777e5e743e7:d7a4476f50828bf4095455b49c02cc01
   sha256 'ae58557457f2876e639cf166362456e76b17939f065784c146a0a459b1cfb389'
 
-  url "https://download.visualstudio.microsoft.com/download/pr/7fd8704c-560f-47dc-8fe0-b777e5e743e7/d7a4476f50828bf4095455b49c02cc01/dotnet-runtime-#{version}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
   name '.Net Core Runtime'
   homepage 'https://www.microsoft.com/net/core#macos'
