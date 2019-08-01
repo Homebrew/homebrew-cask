73bef4f02a66b69ee066efc7dc6d9c65ebcdef2f
CoreCode <core-code@users.noreply.github.com>
2019-08-01 13:23:17 +0200

    Update netbeans.rb

--- Casks/netbeans.rb
+++ Casks/netbeans.rb
@@ -2,7 +2,7 @@ cask 'netbeans' do
   version '11.1'
   sha256 '081d564e6b0c3b7aabd906a699de42fe5fff73b1e5e8ed3977afa0010d0b2f33'
 
-  url "https://www.apache.org/dyn/closer.cgi/netbeans/netbeans/#{version}/Apache-NetBeans-#{version}-bin-macosx.dmg"
+  url "https://www-eu.apache.org/dist/netbeans/netbeans/#{version}/Apache-NetBeans-#{version}-bin-macosx.dmg"
   name 'NetBeans IDE'
   homepage 'https://netbeans.apache.org/download/index.html'
 
