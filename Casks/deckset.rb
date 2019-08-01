7dd97ca98bea8dcf23f7a280b44bee6b25eff158
Randall <ran.dall@icloud.com>
2019-08-01 11:41:43 -0700

    Update deckset from 2.0.10,2521 to 2.0.11,2522

--- Casks/deckset.rb
+++ Casks/deckset.rb
@@ -1,6 +1,6 @@
 cask 'deckset' do
-  version '2.0.10,2521'
-  sha256 '1bf4aa857a8a2bee28962a6c7b19c2ac50a47b71937b0f97834fa253451c0453'
+  version '2.0.11,2522'
+  sha256 'dc84134de72fa0178fb510ab1857a96b03c484af1d38e7ed1dd886731427ca50'
 
   url "https://dl.decksetapp.com/Deckset+#{version.before_comma}+(#{version.after_comma}).dmg"
   appcast 'https://dl.decksetapp.com/appcast.xml'
