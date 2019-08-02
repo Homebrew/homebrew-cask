bafaeeee8a7f435dc0b96d249215b6d4c929e4e1
Randall <ran.dall@icloud.com>
2019-08-02 09:59:41 -0700

    Update krita from 4.2.4 to 4.2.5

--- Casks/krita.rb
+++ Casks/krita.rb
@@ -1,6 +1,6 @@
 cask 'krita' do
-  version '4.2.4'
-  sha256 'abe314837bd446ddae9b13ac1d2da10114f3caaa2c38de21ce48041653c1eb2d'
+  version '4.2.5'
+  sha256 '4967623a6e7430bb6751b4be367efd300f9a1685d985c0934a7b1b453cca308d'
 
   # kde.org/stable/krita was verified as official when first introduced to the cask
   url "https://download.kde.org/stable/krita/#{version.major_minor_patch}/krita-#{version}.dmg"
