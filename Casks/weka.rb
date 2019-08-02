2309015a2c6ae2c2ce471cb5be0b5b080ee95928
CoreCode <core-code@users.noreply.github.com>
2019-08-02 22:21:15 +0200

    Revert weka to stable

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -1,10 +1,10 @@
 cask 'weka' do
-  version '3.9.3'
-  sha256 '5a2313c2455bdc09439724e4c450bf974bb238355ebda582a043ba310537cb66'
+  version '3.8.3'
+  sha256 '3689a2ef1fbc5de143ca5b751c8b8183351ed4452955bb222a3f6fc5ea9d42c2'
 
   # sourceforge.net/weka was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-corretto-jvm.dmg"
-  appcast 'https://sourceforge.net/projects/weka/rss'
+  appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://www.cs.waikato.ac.nz/ml/weka/downloading.html&splitter_1=Developer&index_1=0'
   name 'Weka'
   homepage 'https://www.cs.waikato.ac.nz/ml/weka/'
 
