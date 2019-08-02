cca2f42ed26ba8153d89a5eddd72f1798398d20a
Julian Mayer <julian@corecode.io>
2019-08-02 14:06:11 +0200

    Update mirrorop

--- Casks/mirrorop.rb
+++ Casks/mirrorop.rb
@@ -2,9 +2,9 @@ cask 'mirrorop' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.mirrorop.com/downloads/MirrorOpSender_mac.zip'
+  url 'https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R33050100&TdeType=3&MajorVersion=2&MinorVersion=5&PatchVersion=0&BuildVersion=52&ShowDownloadPage=False'
   name 'MirrorOp Sender'
-  homepage 'https://www.mirrorop.com/'
+  homepage 'https://www.barco.com/product/mirrorop'
 
   app 'MirrorOp.app'
 end
