b868e4a72b7e38a2062d82b985bcc1a2d8f8755f
Stephen Rudolph <srudolph@credera.com>
2019-08-02 12:17:30 -0500

    Update mirrorop from latest to 2.5.0.52

--- Casks/mirrorop.rb
+++ Casks/mirrorop.rb
@@ -1,10 +1,10 @@
 cask 'mirrorop' do
-  version :latest
-  sha256 :no_check
+  version '2.5.0.52'
+  sha256 '00583d9f5e282fcd32cc0d940c9eec24aea5aeb34793d274a4cf0df28de924b8'
 
-  url 'https://www.mirrorop.com/downloads/MirrorOpSender_mac.zip'
+  url 'https://www.barco.com/services/website/en/TdeFiles/Download?FileNumber=R33050100&TdeType=3&MajorVersion=2&MinorVersion=5&PatchVersion=0&BuildVersion=52&ShowDownloadPage=False'
   name 'MirrorOp Sender'
-  homepage 'https://www.mirrorop.com/'
+  homepage 'https://www.barco.com/en/product/mirrorop'
 
   app 'MirrorOp.app'
 end
