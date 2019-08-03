ac2672f2ec8a1143ef1c201ddde4a2b417c585a1
Johannes Klein <jo@temka.de>
2019-08-03 03:21:58 +0200

    Add microsoft-excel.rb 16.27.19071500 (#66916)
    * Create microsoft-excel.rb
    
    * Update microsoft-excel.rb

---
+++ Casks/microsoft-excel.rb
@@ -0,0 +1,47 @@
+cask 'microsoft-excel' do
+  version '16.27.19071500'
+  sha256 '3d024b0d0362692854fa872f29fd98a3c43fabe2a30f590e42eb86bb68c909e5'
+
+  # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate was verified as official when first introduced to the cask
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Installer.pkg"
+  appcast 'https://docs.microsoft.com/en-us/officeupdates/update-history-office-for-mac'
+  name 'Microsoft Excel'
+  homepage 'https://products.office.com/en-US/excel'
+
+  auto_updates true
+  depends_on macos: '>= :sierra'
+
+  pkg "Microsoft_Excel_#{version}_Installer.pkg"
+
+  uninstall pkgutil:   [
+                         'com.microsoft.package.Microsoft_Excel.app',
+                         'com.microsoft.pkg.licensing',
+                       ],
+            launchctl: 'com.microsoft.office.licensingV2.helper'
+
+  zap trash:     [
+                   '~/Library/Application Scripts/com.microsoft.Excel',
+                   '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.excel.sfl*',
+                   '~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate.fba',
+                   '~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate2',
+                   '~/Library/Caches/com.microsoft.autoupdate.fba',
+                   '~/Library/Caches/com.microsoft.autoupdate2',
+                   '~/Library/Containers/com.microsoft.Excel',
+                   '~/Library/Cookies/com.microsoft.autoupdate.fba.binarycookies',
+                   '~/Library/Cookies/com.microsoft.autoupdate2.binarycookies',
+                   '~/Library/Preferences/com.microsoft.Excel.plist',
+                   '~/Library/Preferences/com.microsoft.autoupdate.fba.plist',
+                   '~/Library/Preferences/com.microsoft.autoupdate2.plist',
+                   '~/Library/Saved Application State/com.microsoft.autoupdate2.savedState',
+                 ],
+      rmdir:     [
+                   '~/Library/Caches/Microsoft/uls',
+                   '~/Library/Caches/Microsoft',
+                 ],
+      launchctl: [
+                   'com.microsoft.autoupdate.helpertool',
+                   'com.microsoft.autoupdate.helper',
+                   'com.microsoft.update.agent',
+                 ],
+      pkgutil:   'com.microsoft.package.Microsoft_AutoUpdate.app'
+end
