a813aa435ed0bde3544aa1df93cf1b118785617f
CoreCode <core-code@users.noreply.github.com>
2019-08-03 11:24:12 +0200

    Update weka.rb

--- Casks/weka.rb
+++ Casks/weka.rb
@@ -1,6 +1,6 @@
 cask 'weka' do
   version '3.8.3'
-  sha256 '3689a2ef1fbc5de143ca5b751c8b8183351ed4452955bb222a3f6fc5ea9d42c2'
+  sha256 '3ff3f75619f0e175ab4605ef7289e55d53d005bf84fd8385722256bd60e257ab'
 
   # sourceforge.net/weka was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-corretto-jvm.dmg"
