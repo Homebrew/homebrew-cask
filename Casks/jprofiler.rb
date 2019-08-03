c27e06795bae8d762f4b4a271032fef9da831c14
CoreCode <core-code@users.noreply.github.com>
2019-08-03 11:53:23 +0200

    Update jprofiler (#67096)

--- Casks/jprofiler.rb
+++ Casks/jprofiler.rb
@@ -1,6 +1,6 @@
 cask 'jprofiler' do
   version '11.0.1'
-  sha256 '90603680c12f94c19db9dc7a30b3dc9cd6e2451a6b7ece7c4679174bb8d82b6f'
+  sha256 '7eae79230fed77efc178231b1f6d6cf6e5551907758bf757e27d7a9a997ed740'
 
   url "https://download-keycdn.ej-technologies.com/jprofiler/jprofiler_macos_#{version.dots_to_underscores}.dmg"
   appcast 'https://www.ej-technologies.com/feeds/jprofiler/'
