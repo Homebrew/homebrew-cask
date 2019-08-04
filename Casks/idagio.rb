4c4291b25484b353e3ad3c8dc277c525449a4c72
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-03 19:18:54 -0700

    Update idagio from 0.0.92 to 0.0.93 (#67121)

--- Casks/idagio.rb
+++ Casks/idagio.rb
@@ -1,6 +1,6 @@
 cask 'idagio' do
-  version '0.0.92'
-  sha256 '6ad889934457213d9cac30043cc331e0e00fe1fc41d8fbf87d7113a286ab4671'
+  version '0.0.93'
+  sha256 'd7bb5ef47f232bc4ea2d78223ef6a44d6d46c19a80daa7d7c74246de0df897af'
 
   url "https://dl.idagio.com/IDAGIO-#{version}.dmg"
   name 'IDAGIO'
