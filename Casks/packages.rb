4e1e00bb9c0921ecf77b085902996a06377df171
mikepflu <36419383+mikepflu@users.noreply.github.com>
2019-08-04 05:44:07 -0400

    Update packages from 1.2.5 to 1.2.6 (#67061)
    * Update packages from 1.2.5 to 1.2.6
    
    * Update packages.rb

--- Casks/packages.rb
+++ Casks/packages.rb
@@ -1,6 +1,6 @@
 cask 'packages' do
-  version '1.2.5'
-  sha256 '44682aff97b6c9b4a87ede9ade95fa3b3ec9b44affb6bbb6f5ea016df72a3dde'
+  version '1.2.6'
+  sha256 'a24225be6981228f16f17140af2af91c0d27cac82c879de98e6ca788a4591f18'
 
   url 'http://s.sudre.free.fr/Software/files/Packages.dmg'
   appcast 'http://s.sudre.free.fr/Software/documentation/RemoteVersion.plist'
