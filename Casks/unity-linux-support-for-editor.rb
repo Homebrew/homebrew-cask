dfe177c74e3eaa5b2f5564bd5fbadfd6f858e0ea
suschizu <brigitte@corecode.io>
2019-08-04 11:00:32 +0200

    Update unity-linux-support-for-editor.rb

--- Casks/unity-linux-support-for-editor.rb
+++ Casks/unity-linux-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-linux-support-for-editor' do
-  version '2019.1.12f1,f04f5427219e'
-  sha256 '8ca5c3d28db27e61decfd4fee24154c5ba86935d22ebdc99d5f929fc3436b834'
+  version '2019.2.0f1,20c1667945cf'
+  sha256 'abdd0093532ec3a94227eb16335f702007ac62f16582ddcb27370e3d30c91f51'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
