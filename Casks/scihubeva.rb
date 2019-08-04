42af489b670685719d5d9a8fcab2ecf885777cb6
Leo <leo-van@hotmail.com>
2019-08-04 18:41:53 +0800

    Update SciHubEVA from 2.1.2 to 3.0.0

--- Casks/scihubeva.rb
+++ Casks/scihubeva.rb
@@ -1,6 +1,6 @@
 cask 'scihubeva' do
-  version 'v2.1.2'
-  sha256 '84e4dc32437d6474aa90808cf8d97c3e1797770eb2911e4a0f55176ddb042197'
+  version 'v3.0.0'
+  sha256 'ca6dafe8ece10d542c3aa2dfdca07d2ab87f782f5da7a4de084633104c751b46'
 
   url "https://github.com/leovan/SciHubEVA/releases/download/#{version}/SciHubEVA-#{version}.dmg"
   appcast 'https://github.com/leovan/SciHubEVA/releases.atom'
