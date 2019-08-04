5df466518713d974300bef46485dbadae18bd9ca
Randall <ran.dall@icloud.com>
2019-08-03 18:25:11 -0700

    Update microsoft-teams from 1.2.00.13765 to 1.2.00.17856

--- Casks/microsoft-teams.rb
+++ Casks/microsoft-teams.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-teams' do
-  version '1.2.00.13765'
-  sha256 'b991b177a9442eb237869e23df63abe83a29cbc2054d97a7459d08cc79121f4c'
+  version '1.2.00.17856'
+  sha256 '09cab03f971e374e1beab5ee81cda7d9e7365cc7c616d17899e0d13c9079e485'
 
   url "https://statics.teams.microsoft.com/production-osx/#{version}/Teams_osx.pkg"
   appcast 'https://teams.microsoft.com/downloads/DesktopUrl?env=production&plat=osx'
