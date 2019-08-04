0892b445ba731cdcf9f2a5f7c92e619b440150ab
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-03 18:13:13 -0700

    Update tropy from 1.5.2 to 1.5.3 (#67116)

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -1,6 +1,6 @@
 cask 'tropy' do
-  version '1.5.2'
-  sha256 '8e2ca3fc3f4e782640bfd76936bd79895683184879a59bb652059a835fdbaf03'
+  version '1.5.3'
+  sha256 'dc1018fe1ecfec17459695635920db01f7f3dbb86377cbb405bb40f14a446728'
 
   # github.com/tropy/tropy was verified as official when first introduced to the cask
   url "https://github.com/tropy/tropy/releases/download/#{version}/tropy-#{version}.dmg"
