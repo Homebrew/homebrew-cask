6a7391b4099004f2963026e17e0971f26b684120
Randall <ran.dall@icloud.com>
2019-08-03 18:27:49 -0700

    Update studio-3t from 2019.4.0 to 2019.4.1

--- Casks/studio-3t.rb
+++ Casks/studio-3t.rb
@@ -1,6 +1,6 @@
 cask 'studio-3t' do
-  version '2019.4.0'
-  sha256 'f3a76303958ce59c9f3101804d5112cbd9c23f7d3ef3328984071ece93a530cf'
+  version '2019.4.1'
+  sha256 'ed7b326120f8c072de2068680e565468f4027ce343713602c154e8a5701fdeeb'
 
   url "https://download.studio3t.com/studio-3t/mac/#{version}/Studio-3T.dmg"
   appcast 'https://files.studio3t.com/changelog/changelog.txt'
