8fbcbde12f56d245a178524d8167ff2a9aea15b4
Val Lopez <val@Vals-MacBook-Pro.local>
2019-08-05 17:30:12 +0800

    Update musique from 1.6 to 1.7

--- Casks/musique.rb
+++ Casks/musique.rb
@@ -1,6 +1,6 @@
 cask 'musique' do
-  version '1.6'
-  sha256 '4feb88a633abda99b498bcb00b510f395fcc7fc747990825ac9cc215e666aded'
+  version '1.7'
+  sha256 'be957cf7228fd7aafae304b7a7aa7cddac1e7f05afe73b62331e8b3021c9f4df'
 
   url 'https://flavio.tordini.org/files/musique/musique.dmg'
   appcast 'https://flavio.tordini.org/musique-ws/appcast.xml'
