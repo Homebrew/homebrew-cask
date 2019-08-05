a2b4d189119e0dca922fae77273fed386c467d42
Randall <ran.dall@icloud.com>
2019-08-05 12:15:19 -0700

    Update lynx from 6.5.8.0 to 6.5.9.0

--- Casks/lynx.rb
+++ Casks/lynx.rb
@@ -1,6 +1,6 @@
 cask 'lynx' do
-  version '6.5.8.0'
-  sha256 'be0f056c05892400f80c4e8802242eb83f90415b2b1a5d237ddf19273aac5378'
+  version '6.5.9.0'
+  sha256 '1d724bbcf0d5940d88851057c3d6156bb9b4968c731d0cc96192190d72f5952f'
 
   url 'https://download.saharasupport.com/lynx/production/macx/Lynx-install.dmg'
   appcast 'https://downloads.saharasupport.com/lynx/production/macx/version.txt'
