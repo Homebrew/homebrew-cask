675da1c7304aeee076bb2bad0139a07018f3f317
Julian Mayer <julian@corecode.io>
2019-08-05 18:15:19 +0200

    Update massreplaceit

--- Casks/massreplaceit.rb
+++ Casks/massreplaceit.rb
@@ -2,7 +2,7 @@ cask 'massreplaceit' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.hexmonkeysoftware.com/files/mri.dmg.zip'
+  url 'http://www.hexmonkeysoftware.com/files/MassReplaceIt.dmg'
   name 'MassReplaceIt'
   homepage 'http://www.hexmonkeysoftware.com/'
 
