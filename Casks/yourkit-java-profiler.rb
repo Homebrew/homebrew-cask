336ab01b68c0cc8499cb6909390c217940299795
Randall <ran.dall@icloud.com>
2019-08-05 12:04:24 -0700

    Update yourkit-java-profiler from 2019.8-b108 to 2019.8-b109

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask 'yourkit-java-profiler' do
-  version '2019.8-b108'
-  sha256 'bc92c98066f3c369eaed50b8b339bb4dd13818d25340ee007a828e938228aedf'
+  version '2019.8-b109'
+  sha256 '15543270e915fbf37f897a171665ec7465b165318517eab269327a08ec78259e'
 
   url "https://www.yourkit.com/download/YourKit-JavaProfiler-#{version}.dmg"
   appcast 'https://www.yourkit.com/download/'
