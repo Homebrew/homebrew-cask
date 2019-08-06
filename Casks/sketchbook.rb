870a0ac38b52b763163a37054e0c5c50d3d6ee6b
CoreCode <core-code@users.noreply.github.com>
2019-08-06 15:10:40 +0200

    Update sketchbook (#67155)

--- Casks/sketchbook.rb
+++ Casks/sketchbook.rb
@@ -2,7 +2,8 @@ cask 'sketchbook' do
   version '8.6.1'
   sha256 '6f7c1b4feb32fea7172068e3f3b02748a9fa9e21e099579c7ca7ce00648c2518'
 
-  url "https://cdn.sketchbook.com/mac/SketchBook_v#{version}_mac.dmg"
+  url "https://update.sketchbook.com/mac/SketchBook_v#{version}_mac.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://update.sketchbook.com/mac/latest'
   name 'Autodesk Sketchbook'
   homepage 'https://www.sketchbook.com/'
 
