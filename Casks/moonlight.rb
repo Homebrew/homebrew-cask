00fec572186d4cad7006a6adf376c938bb4ceb55
Randall <ran.dall@icloud.com>
2019-08-06 16:44:43 -0700

    Update moonlight from 1.0.1 to 1.1.0

--- Casks/moonlight.rb
+++ Casks/moonlight.rb
@@ -1,6 +1,6 @@
 cask 'moonlight' do
-  version '1.0.1'
-  sha256 '7ed0d2438cb615ec8732c4c46416e26cf4f3929064dc9e73c6c8e3d554cbaaa9'
+  version '1.1.0'
+  sha256 'fdbda47439f97b554f55ba3919b23cbf10c1765ecbb2eca249330c29eda1fc2c'
 
   # github.com/moonlight-stream/moonlight-qt was verified as official when first introduced to the cask
   url "https://github.com/moonlight-stream/moonlight-qt/releases/download/v#{version}/Moonlight-#{version}.dmg"
