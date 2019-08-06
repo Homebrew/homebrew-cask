7308016b2faa21d22f45ba054d75931bb05f5ba2
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2019-08-06 07:44:19 +0200

    Update archi from 4.4.0 to 4.5.0 (#67189)

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -1,6 +1,6 @@
 cask 'archi' do
-  version '4.4.0'
-  sha256 '7205a6c8ba9eb797c2e6e659301b44ac67520af74d970a1ec39378fbe71a7a82'
+  version '4.5.0'
+  sha256 '9f22ce2270c1a947dfab2076837691b133933a5060bb1ee7b2fc11ef5b9d1d6c'
 
   url "https://www.archimatetool.com/downloads/#{version}/Archi-Mac-#{version}.zip",
       referer: 'https://www.archimatetool.com/download/'
