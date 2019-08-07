30d01070356b79a4edc89abba019a52f8a3b2bf5
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-07 09:59:45 -0700

    Update remembear from 1.4.7 to 1.4.8 (#67269)

--- Casks/remembear.rb
+++ Casks/remembear.rb
@@ -1,6 +1,6 @@
 cask 'remembear' do
-  version '1.4.7'
-  sha256 '9943c74841d9deeea274b66ffbb43b2d5a2b6f9a42f38034a0d48827062c3294'
+  version '1.4.8'
+  sha256 '236b123a6667ba6823cbe9ef251e852411d1f227a767b8f419c1fbf62c2c5040'
 
   # remembear.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://remembear.s3.amazonaws.com/app/release/downloads/macOS/RememBear-#{version}.dmg"
