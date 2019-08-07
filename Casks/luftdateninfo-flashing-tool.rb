40455cc5277f8b70c8d87f8f64f191b37814cca3
Raoul Bhatia <raoul@bhatia.at>
2019-08-07 16:40:02 +0200

    Add Luftdaten.info Flashing Tool Latest

---
+++ Casks/luftdateninfo-flashing-tool.rb
@@ -0,0 +1,10 @@
+cask 'luftdateninfo-flashing-tool' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://luftdaten.info/flashtool/luftdaten-tool.dmg'
+  name 'Luftdaten.info Flashing Tool'
+  homepage 'https://luftdaten.info/'
+
+  app 'Luftdaten.info Flashing Tool.app'
+end
