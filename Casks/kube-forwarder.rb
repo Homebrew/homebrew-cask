539ca0104a83295477a8b80e04e0d680cf7318b7
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-06 17:13:56 -0700

    Update kube-forwarder from 1.4.0 to 1.4.1 (#67207)

--- Casks/kube-forwarder.rb
+++ Casks/kube-forwarder.rb
@@ -1,6 +1,6 @@
 cask 'kube-forwarder' do
-  version '1.4.0'
-  sha256 '10df5ea1228341f692c6e0e48499e5f04596e0c7a3808f1fc7f0f5213257df56'
+  version '1.4.1'
+  sha256 '648788b32991a48916d9d411ee32112e9e2a672f07ea9fcbec4974424344e50f'
 
   # github.com/pixel-point/kube-forwarder was verified as official when first introduced to the cask
   url "https://github.com/pixel-point/kube-forwarder/releases/download/v#{version}/kube-forwarder.dmg"
