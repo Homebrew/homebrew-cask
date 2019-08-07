20ed46cc4dfd4f1997f24e60a8a1e1d0bcde539c
Julien Brochet <556303+aerialls@users.noreply.github.com>
2019-08-07 20:26:27 +0200

    fix(mattermost): fix version 4.2.2 version SHA-256 (#67289)

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,6 +1,6 @@
 cask 'mattermost' do
   version '4.2.2'
-  sha256 'a908c990933400de2fb40ca947a3e3dce469ea7cb16fd2e9019324b4a6f491d6'
+  sha256 'a04d09fa0e5f9e23d1c4c07e9007a911319bbaa54c1e234c2e8c9eed60dfd18c'
 
   url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac.zip"
   appcast 'https://github.com/mattermost/desktop/releases.atom'
