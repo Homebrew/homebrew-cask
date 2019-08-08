1d2cb935df8f5421d9ed8e2f96c1b83f9ace0246
Jacob Zaval <jazava@microsoft.com>
2019-08-08 14:28:04 -0700

    use inittool instead of Install to skip Software License Agreement popup

--- Casks/parallels.rb
+++ Casks/parallels.rb
@@ -15,7 +15,8 @@ cask 'parallels' do
   preflight do
     system_command '/usr/bin/hdiutil',
                    args: ['attach', '-nobrowse', "#{staged_path}/ParallelsDesktop-#{version}.dmg"]
-    system_command "/Volumes/Parallels Desktop #{version.major}/Install.app/Contents/MacOS/Install",
+    system_command "/Volumes/Parallels Desktop #{version.major}/Parallels Desktop.app/Contents/MacOS/inittool",
+                   args: ['install', '-t', "#{appdir}/Parallels Desktop.app", '-s'],
                    sudo: true
     system_command '/usr/bin/hdiutil',
                    args: ['detach', "/Volumes/Parallels Desktop #{version.major}"]
