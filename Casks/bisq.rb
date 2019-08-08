f0e21bf107670b1b6ae799a0a8033d92fa4d1aa4
Alexey Zagarin <leshik@users.noreply.github.com>
2019-08-08 18:10:23 +0700

    Update bisq to 1.1.5 (#67303)

--- Casks/bisq.rb
+++ Casks/bisq.rb
@@ -1,6 +1,6 @@
 cask 'bisq' do
-  version '1.1.4'
-  sha256 'a53cbce3a08c88b92aac8c4f9ed7df6d46792d6e1f4e924293e090393ee2acad'
+  version '1.1.5'
+  sha256 '564e644f91e886be92fc739906c8909e271453c67f43ab9b2fda15470e9aee69'
 
   # github.com/bisq-network/bisq was verified as official when first introduced to the cask
   url "https://github.com/bisq-network/bisq/releases/download/v#{version}/Bisq-#{version}.dmg"
