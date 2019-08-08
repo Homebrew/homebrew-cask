c07acf20199c21bf68d1030712b447e6e0360f7e
Zaoqi <zaomir@outlook.com>
2019-08-08 19:45:51 +0800

    Add racket-cs v7.4.0.4

---
+++ Casks/racket-cs.rb
@@ -0,0 +1,33 @@
+cask 'racket-cs' do
+  version '7.4.0.4'
+  sha256 'aa07477625d7cd8167a2418b28cc2bda17249ffaac518505c07130a87be8be6a'
+
+  url "https://www.cs.utah.edu/plt/snapshots/20190805-044c15ec8f/installers/racket-#{version}-x86_64-macosx-cs.dmg"
+  appcast 'https://www.cs.utah.edu/plt/snapshots/'
+  name 'Racket on Chez Scheme'
+  homepage 'https://www.cs.utah.edu/plt/snapshots/'
+
+  suite "Racket v#{version}"
+  binary "#{appdir}/Racket v#{version}/bin/drracket"
+  binary "#{appdir}/Racket v#{version}/bin/gracket"
+  binary "#{appdir}/Racket v#{version}/bin/gracket-text"
+  binary "#{appdir}/Racket v#{version}/bin/mred"
+  binary "#{appdir}/Racket v#{version}/bin/mred-text"
+  binary "#{appdir}/Racket v#{version}/bin/mzc"
+  binary "#{appdir}/Racket v#{version}/bin/mzpp"
+  binary "#{appdir}/Racket v#{version}/bin/mzscheme"
+  binary "#{appdir}/Racket v#{version}/bin/mztext"
+  binary "#{appdir}/Racket v#{version}/bin/pdf-slatex"
+  binary "#{appdir}/Racket v#{version}/bin/plt-games"
+  binary "#{appdir}/Racket v#{version}/bin/plt-help"
+  binary "#{appdir}/Racket v#{version}/bin/plt-r5rs"
+  binary "#{appdir}/Racket v#{version}/bin/plt-r6rs"
+  binary "#{appdir}/Racket v#{version}/bin/plt-web-server"
+  binary "#{appdir}/Racket v#{version}/bin/racket"
+  binary "#{appdir}/Racket v#{version}/bin/raco"
+  binary "#{appdir}/Racket v#{version}/bin/scribble"
+  binary "#{appdir}/Racket v#{version}/bin/setup-plt"
+  binary "#{appdir}/Racket v#{version}/bin/slatex"
+  binary "#{appdir}/Racket v#{version}/bin/slideshow"
+  binary "#{appdir}/Racket v#{version}/bin/swindle"
+end
