97e19e585640bc32267e12616310c3d64ed6d49c
CoreCode <core-code@users.noreply.github.com>
2019-08-08 11:50:01 +0200

    Update textmate.rb

--- Casks/textmate.rb
+++ Casks/textmate.rb
@@ -4,7 +4,8 @@ cask 'textmate' do
 
   # github.com/textmate/textmate was verified as official when first introduced to the cask
   url "https://github.com/textmate/textmate/releases/download/v#{version}/TextMate_#{version}.tbz"
-  appcast 'https://api.textmate.org/releases/release?os=10.14.6'
+  appcast 'https://api.textmate.org/releases/release?os=10.14.6',
+          configuration: version
   name 'TextMate'
   homepage 'https://macromates.com/'
 
