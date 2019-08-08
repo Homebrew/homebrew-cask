09852cc9f34b7a946e5844ca29772c7ff10ef07e
Randall <ran.dall@icloud.com>
2019-08-08 09:40:00 -0700

    Update time-out from 2.5 to 2.6

--- Casks/time-out.rb
+++ Casks/time-out.rb
@@ -3,8 +3,8 @@ cask 'time-out' do
     version '1.7.1'
     sha256 '3c9892344c8313b8ccf0a76cceb00834ddbe26e5114bcd674c4fd53aeb44e310'
   else
-    version '2.5'
-    sha256 '2947b5af6489d4cc5328bd943ec5e0440484970539ee18810bb4f6e6bf1fbbf6'
+    version '2.6'
+    sha256 '7a3787fa9e022a3e3509a11e8adcef75e078dfae17036986ddb0392522aecfe3'
   end
 
   url "https://www.dejal.com/download/timeout-#{version}.zip"
