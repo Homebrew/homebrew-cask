ba9ef78d40db8aad6aea2901ca477b345f27a810
Benjamin Garcia <bgarcia@zendesk.com>
2019-08-09 11:31:49 +0200

    Update gitkraken from 6.0.1 to 6.1.0

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,8 +1,9 @@
 cask 'gitkraken' do
-  version '6.0.1'
-  sha256 '1a4a574b11e6f3f5d2d1f87aa2bcbe088545f3df9f35ad857fca0f0bed09e850'
+  version '6.1.0'
+  sha256 '5f2e5e3df06d6da8e427ad36dd9b8f58a5a86229287dfd4250776e510a637d03'
 
-  url 'https://release.gitkraken.com/darwin/installGitKraken.dmg'
+  # release.axocdn.com/darwin/installGitKraken.dmg was verified as official when first introduced to the cask
+  url 'https://release.axocdn.com/darwin/installGitKraken.dmg'
   appcast 'https://www.gitkraken.com/download'
   name 'GitKraken'
   homepage 'https://www.gitkraken.com/'
