b8aeb1a5e0f9b98910eeff8a81c76aad461a3ea6
Julian Mayer <julian@corecode.io>
2019-08-09 19:35:32 +0200

    Update graphicconverter from 11.0,4003 to 11.0.1,4059

--- Casks/graphicconverter.rb
+++ Casks/graphicconverter.rb
@@ -1,9 +1,9 @@
 cask 'graphicconverter' do
-  version '11.0,4003'
-  sha256 '1c73db344b870252313962131c23e75a52e373ddf7f072fe959b596eea001675'
+  version '11.0.1,4059'
+  sha256 'b869c73fa0727718e171b54a6247bc8c442e56ddda63d1850c92a28da4e349d0'
 
   # lemkesoft.info was verified as official when first introduced to the cask
-  url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}.dmg"
+  url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.after_comma}.zip"
   appcast "https://www.lemkesoft.info/sparkle/graphicconverter/graphicconverter#{version.major}.xml"
   name 'GraphicConverter'
   homepage 'https://www.lemkesoft.de/en/products/graphicconverter/'
