08f532913e6c9bb8563d0e0bd880e1f7b378426d
Stephane Paquet <spaquet@up4b.com>
2019-08-09 13:35:50 -0700

    Update electron from 6.0.0 to 6.0.1

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '6.0.0'
-  sha256 '55a3741df3a6a2eaf26d310520f0fde1fdf259b7d7141d59c6c87ecb1446fa54'
+  version '6.0.1'
+  sha256 '2206346c073ebbc8221fff2082872ceb25a9545d559be943cb56cd64b42b1b55'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
