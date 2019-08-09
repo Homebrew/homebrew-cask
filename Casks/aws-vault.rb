b2b41618f44681ffc4ecfed65645ddd5e88b9383
Michael Tibben <michael.tibben@99designs.com>
2019-08-09 10:21:50 +1000

    Update aws-vault from 4.6.2 to 4.6.3 (#67356)

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,6 +1,6 @@
 cask 'aws-vault' do
-  version '4.6.2'
-  sha256 '8328eead56d599fdb66ecb941e835249e028231a451e8d72ffa3975a608bf872'
+  version '4.6.3'
+  sha256 'b199a772d3b4b6f31c5e9e67a47688b8a3bc73d89908e74deb324096fa0301a5'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64"
   appcast 'https://github.com/99designs/aws-vault/releases.atom'
