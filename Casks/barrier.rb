802b9ebf56f714066e66bd742a685d3ba7bf56ed
David Yantis <404NetworkError@users.noreply.github.com>
2019-08-10 04:09:21 -0400

    Update barrier from 2.3.0 to 2.3.1 (#67408)

--- Casks/barrier.rb
+++ Casks/barrier.rb
@@ -1,11 +1,18 @@
 cask 'barrier' do
-  version '2.3.0'
-  sha256 'c00706fd2c38c1f0dda0b298f55d2fc0d423dcb15187041c379273da1e347714'
+  version '2.3.1'
+  sha256 'e8238c109293c8a33fed8604725bd5c0275c24adbc223d1f4c01663cd2d9b45f'
 
   url "https://github.com/debauchee/barrier/releases/download/v#{version}/Barrier-#{version}-Release.dmg"
   appcast 'https://github.com/debauchee/barrier/releases.atom'
   name 'Barrier'
   homepage 'https://github.com/debauchee/barrier/'
 
+  depends_on macos: '>= :sierra'
+
   app 'Barrier.app'
+
+  zap trash: [
+               '~/Library/Application Support/barrier',
+               '~/Library/Saved Application State/barrier.savedState',
+             ]
 end
