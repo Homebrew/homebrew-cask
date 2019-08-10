ed65806e41e30dea6dbd1f7d6b64f8529c66b0c4
CoreCode <core-code@users.noreply.github.com>
2019-08-10 04:49:29 +0200

    Update nvidia-geforce-now from 2.0.8.85,3AB74C to 2.0.10.71,350E76 (#67286)
    * Update nvidia-geforce-now from 2.0.8.85,3AB74C to 2.0.10.71,350E76
    
    * Update nvidia-geforce-now.rb
    
    * Update nvidia-geforce-now.rb
    
    * Update nvidia-geforce-now.rb

--- Casks/nvidia-geforce-now.rb
+++ Casks/nvidia-geforce-now.rb
@@ -1,9 +1,10 @@
 cask 'nvidia-geforce-now' do
-  version '2.0.8.85,3AB74C'
-  sha256 'bfc0966e8580673caae1d4c88ce9c20b1679b40edad0cbfdab98ab71e1263824'
+  version '2.0.10.71,350E76'
+  sha256 '05bed0f13eee22f63e45832712e3d4ac2629107221cc282c564f65ceb7b41f83'
 
   url "https://ota-downloads.nvidia.com/ota/GeForceNOW-release_#{version.after_comma}.dmg"
-  appcast 'https://ota.nvidia.com/release/available?product=GFN-mac&version=1.17.2.0&channel=OFFICIAL'
+  appcast "https://ota.nvidia.com/release/available?product=GFN-mac&version=#{version.before_comma}&channel=OFFICIAL",
+          configuration: '[]' # Only happens when there are no newer versions
   name 'NVIDIA GeForce NOW'
   homepage 'https://www.nvidia.com/en-us/geforce/products/geforce-now/'
 
