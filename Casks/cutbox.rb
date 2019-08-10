055774ad192c21834ab0176c4ff2d7738900a05c
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-10 11:25:14 -0700

    Update cutbox from 1.4.3 to 1.4.4 (#67427)

--- Casks/cutbox.rb
+++ Casks/cutbox.rb
@@ -1,6 +1,6 @@
 cask 'cutbox' do
-  version '1.4.3'
-  sha256 '244e2eb708369e006e84b1c3db0aa969a931cf7473096b6b9ce4d25e8e7f4a96'
+  version '1.4.4'
+  sha256 'd02690579212d8f974c70be1d89c25f6b20cb1a8bd8ef9afaca88ff98fffe815'
 
   url "https://github.com/cutbox/CutBox/releases/download/#{version}/CutBox.dmg"
   appcast 'https://github.com/cutbox/CutBox/releases.atom'
