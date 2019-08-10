52b6ebaee0359dfc7436978429b59e31f24eecc1
suschizu <brigitte@corecode.io>
2019-08-10 23:41:30 +0200

    Update preform from 3.0.3,1340 to 3.0.4,1365 (#67437)

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,9 +1,9 @@
 cask 'preform' do
-  version '3.0.3,1340'
-  sha256 'b793d2ab21b9c47b6f983e2f346fa0ddf63f4c880a3f8fd11c3b7dc7e9489eec'
+  version '3.0.4,1365'
+  sha256 '7447246e0aada5f0e99384fd874628cb879b5f261fff15f0d409f638ff47f544'
 
   # s3.amazonaws.com/FormlabsReleases was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release_origin_release_#{version.before_comma}_build_#{version.after_comma}.dmg"
+  url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release_origin_testing_#{version.before_comma}_build_#{version.after_comma}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://formlabs.com/download-preform-mac/'
   name 'PreForm'
   homepage 'https://formlabs.com/tools/preform/'
