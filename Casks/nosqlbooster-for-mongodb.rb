97ade969821a852cbd991fe7595c8f2f1e755d06
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-10 11:45:14 -0700

    Update nosqlbooster-for-mongodb from 5.1.12 to 5.1.13 (#67435)

--- Casks/nosqlbooster-for-mongodb.rb
+++ Casks/nosqlbooster-for-mongodb.rb
@@ -1,6 +1,6 @@
 cask 'nosqlbooster-for-mongodb' do
-  version '5.1.12'
-  sha256 '54864fe02d2e479e1a31fb360bdf23da9fed2fdd780f13d482f7cfc72a38ae2a'
+  version '5.1.13'
+  sha256 '3c652373520a628759982a22f25f04bcf4633a2485648ba7786d776c4df5fb3b'
 
   url "https://nosqlbooster.com/s3/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
   appcast 'https://nosqlbooster.com/downloads'
