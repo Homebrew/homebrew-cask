6866804c229ce540d62b01822a77c55d98cc032e
suschizu <brigitte@corecode.io>
2019-08-10 20:11:03 +0200

    Update goodsync from 10.10.3.3 to 10.10.4.4 (#67422)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,5 +1,5 @@
 cask 'goodsync' do
-  version '10.10.3.3'
+  version '10.10.4.4'
   sha256 '392de7ff0a1e9d10191d66c88bf6c7b903bb5645b4fe52e733118fe9e7d83e7b'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
