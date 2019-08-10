9d115964525257357ed8ec67c0854ff4ff746e79
Randall <ran.dall@icloud.com>
2019-08-09 17:19:08 -0700

    Update visicut from 1.8-80-g681e741c to 1.8-90-ga030e4e8

--- Casks/visicut.rb
+++ Casks/visicut.rb
@@ -1,6 +1,6 @@
 cask 'visicut' do
-  version '1.8-80-g681e741c'
-  sha256 '29d319637d16e9c32d9181726274e8a1bd7e3bcf70e5f5d04cbff58ca257d270'
+  version '1.8-90-ga030e4e8'
+  sha256 '6e95152d946101a3afe9c442c6ec24dc277c39eaffc1ff07fe94d1870f0138c9'
 
   url "https://download.visicut.org/files/master/MacOSX/VisiCutMac-#{version}.zip"
   appcast 'https://download.visicut.org'
