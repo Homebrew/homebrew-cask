536a204b76ec4f9ab897cbf0d5f57c41d856f7bd
linbo0518 <linbo0518@gmail.com>
2019-08-11 14:14:27 +0800

    Update notable from 1.7.0 to 1.7.1

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -1,6 +1,6 @@
 cask 'notable' do
-  version '1.7.0'
-  sha256 '3ccb41351c44706c9775fe3f5ed74fdc663cce28f76616e2450abd24c5e190cd'
+  version '1.7.1'
+  sha256 'e43e38afff6cb2f4997c4f2f6e62b123f57efaef2f7ae63b34e5812cd9c8a576'
 
   url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg"
   appcast 'https://github.com/notable/notable/releases.atom'
