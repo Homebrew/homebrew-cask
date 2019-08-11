3edd9c8a14601519a83db456389fea8e852018b0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-11 12:38:27 -0700

    Update soqlxplorer from 3.1 to 3.2 (#67468)

--- Casks/soqlxplorer.rb
+++ Casks/soqlxplorer.rb
@@ -1,6 +1,6 @@
 cask 'soqlxplorer' do
-  version '3.1'
-  sha256 '483b940c0ac4dd3e79aa6418379788a6598d8a0d6ab1070f582de4e9aff641a5'
+  version '3.2'
+  sha256 '66103063a180c1c44e42a9c185cc04b38d8e48db752d58f9cd69cf287e86420a'
 
   url "https://www.pocketsoap.com/osx/soqlx/SoqlXplorer_v#{version}.zip"
   appcast 'https://www.pocketsoap.com/osx/soqlx/appcast.xml'
