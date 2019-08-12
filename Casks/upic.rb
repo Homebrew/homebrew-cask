a23dc397ad8263c29e9e911cac5f1fd5f4313135
Svend <svend.jin@gmail.com>
2019-08-12 21:54:50 +0800

    update upic from v0.10.2 to v0.10.3 (#67487)

--- Casks/upic.rb
+++ Casks/upic.rb
@@ -1,6 +1,6 @@
 cask 'upic' do
-  version '0.10.2'
-  sha256 'a212f4f12abf025c192551e0994826be812ed59cd8077557f305b85191978aca'
+  version '0.10.3'
+  sha256 '53cdc6ec4e532a7ebd980b1a1649490c6b20e5f5365c5b97a54b8ec6b91822e6'
 
   url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic.dmg"
   appcast 'https://github.com/gee1k/uPic/releases.atom'
