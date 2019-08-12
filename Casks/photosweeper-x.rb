83ae156fd041e98b7d5c1a21b2a44e1375e32480
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-12 10:55:13 -0700

    Update photosweeper-x from 3.3.1 to 3.4.0 (#67508)

--- Casks/photosweeper-x.rb
+++ Casks/photosweeper-x.rb
@@ -1,6 +1,6 @@
 cask 'photosweeper-x' do
-  version '3.3.1'
-  sha256 'd89af6358d3547103ea7cbc65f3beec50dafeb32e35c1fdaf436e14c2234f5d1'
+  version '3.4.0'
+  sha256 'bc509436c91f33374661518fca9c3fc675b072f2507fb72172f5d4b4c7745bbb'
 
   url 'https://overmacs.com/photosweeper/downloads/PhotoSweeperTrial.dmg'
   appcast 'https://overmacs.com/photosweeper/updates/photosweeper_update.xml'
