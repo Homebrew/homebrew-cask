24b5640f58cbff05116509171e088e74a4a823c6
Zaoqi <zaomir@outlook.com>
2019-08-12 09:24:49 +0800

    Update racket-cs from 20190805-044c15ec8f to 20190807-19fad3f8d9; con… (#67359)
    * Update racket-cs from 20190805-044c15ec8f to 20190807-19fad3f8d9; conflicts_with racket; fix homepage
    
    * racket-cs 20190807-19fad3f8d9: add `cs.utah.edu was verified as official when first introduced to the cask`
    
    * Update racket-cs 20190807-19fad3f8d9: stanza groups should be separated by a single empty line
    
    * Update racket-cs.rb

--- Casks/racket-cs.rb
+++ Casks/racket-cs.rb
@@ -1,33 +1,36 @@
 cask 'racket-cs' do
-  version '7.4.0.4'
-  sha256 'aa07477625d7cd8167a2418b28cc2bda17249ffaac518505c07130a87be8be6a'
+  version '7.4.0.4,20190807-19fad3f8d9'
+  sha256 'd27c9befee4b16137a0cd8caa8547cdc76df5f628739f532aa57fbcfb1b34b96'
 
-  url "https://www.cs.utah.edu/plt/snapshots/20190805-044c15ec8f/installers/racket-#{version}-x86_64-macosx-cs.dmg"
+  # cs.utah.edu/plt/snapshots was verified as official when first introduced to the cask
+  url "https://www.cs.utah.edu/plt/snapshots/#{version.after_comma}/installers/racket-#{version.before_comma}-x86_64-macosx-cs.dmg"
   appcast 'https://www.cs.utah.edu/plt/snapshots/'
   name 'Racket on Chez Scheme'
-  homepage 'https://www.cs.utah.edu/plt/snapshots/'
+  homepage 'https://racket-lang.org/'
 
-  suite "Racket v#{version}"
-  binary "#{appdir}/Racket v#{version}/bin/drracket"
-  binary "#{appdir}/Racket v#{version}/bin/gracket"
-  binary "#{appdir}/Racket v#{version}/bin/gracket-text"
-  binary "#{appdir}/Racket v#{version}/bin/mred"
-  binary "#{appdir}/Racket v#{version}/bin/mred-text"
-  binary "#{appdir}/Racket v#{version}/bin/mzc"
-  binary "#{appdir}/Racket v#{version}/bin/mzpp"
-  binary "#{appdir}/Racket v#{version}/bin/mzscheme"
-  binary "#{appdir}/Racket v#{version}/bin/mztext"
-  binary "#{appdir}/Racket v#{version}/bin/pdf-slatex"
-  binary "#{appdir}/Racket v#{version}/bin/plt-games"
-  binary "#{appdir}/Racket v#{version}/bin/plt-help"
-  binary "#{appdir}/Racket v#{version}/bin/plt-r5rs"
-  binary "#{appdir}/Racket v#{version}/bin/plt-r6rs"
-  binary "#{appdir}/Racket v#{version}/bin/plt-web-server"
-  binary "#{appdir}/Racket v#{version}/bin/racket"
-  binary "#{appdir}/Racket v#{version}/bin/raco"
-  binary "#{appdir}/Racket v#{version}/bin/scribble"
-  binary "#{appdir}/Racket v#{version}/bin/setup-plt"
-  binary "#{appdir}/Racket v#{version}/bin/slatex"
-  binary "#{appdir}/Racket v#{version}/bin/slideshow"
-  binary "#{appdir}/Racket v#{version}/bin/swindle"
+  conflicts_with cask: 'racket'
+
+  suite "Racket v#{version.before_comma}"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/drracket"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/gracket"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/gracket-text"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/mred"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/mred-text"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/mzc"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/mzpp"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/mzscheme"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/mztext"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/pdf-slatex"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/plt-games"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/plt-help"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/plt-r5rs"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/plt-r6rs"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/plt-web-server"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/racket"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/raco"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/scribble"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/setup-plt"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/slatex"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/slideshow"
+  binary "#{appdir}/Racket v#{version.before_comma}/bin/swindle"
 end
