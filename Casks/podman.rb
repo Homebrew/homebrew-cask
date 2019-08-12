1abcde66a5bc0d79bbf3defc6391d8d0d814d168
Ashley Cui <ashleycui16@gmail.com>
2019-08-12 16:21:07 -0400

    Update podman from 1.4.2 to 1.5.0

--- Casks/podman.rb
+++ Casks/podman.rb
@@ -1,11 +1,11 @@
 cask 'podman' do
-  version '1.4.2'
-  sha256 '73850911bb49f2e0a970ff44f785634e2aa9a7a1f20845ff508bdccbead17444'
+  version '1.5.0'
+  sha256 '4220d1e513bb2ae7833a7941d140e6c1bbd6e98c8258e766d890cdf3ba6df73b'
 
-  url "https://github.com/containers/libpod/releases/download/v#{version}/podman-v#{version}.dmg"
+  url "https://github.com/containers/libpod/releases/download/v#{version}/podman-v#{version}.tar.gz"
   appcast 'https://github.com/containers/libpod/releases.atom'
   name 'podman'
   homepage 'https://github.com/containers/libpod/'
 
-  binary 'podman'
+  binary 'brew/podman'
 end
