e9fca6bbb4a670b10321a679c39d73a4afb41048
Vítor Galvão <info@vitorgalvao.com>
2019-08-12 02:29:19 +0100

    Update power-manager.rb

--- Casks/power-manager.rb
+++ Casks/power-manager.rb
@@ -3,9 +3,9 @@ cask 'power-manager' do
   sha256 '94d554b8bd3e1f59232abe60644fa689a2ee42a4addf97b901de57651aba9a09'
 
   url "https://www.dssw.co.uk/powermanager/dsswpowermanager-#{version.no_dots}.dmg"
-  appcast 'https://version.dssw.co.uk/powermanager/application'
+  appcast 'https://version.dssw.co.uk/powermanager/'
   name 'Power Manager'
-  homepage 'https://www.dssw.co.uk/powermanager/'
+  homepage 'https://dssw.co.uk/powermanager/'
 
   auto_updates true
 
