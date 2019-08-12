8f4287a080bcce01a67a1737379a24d31b438f40
Vítor Galvão <info@vitorgalvao.com>
2019-08-12 02:21:40 +0100

    timing.rb: remove extraneous lines

--- Casks/timing.rb
+++ Casks/timing.rb
@@ -15,14 +15,11 @@ cask 'timing' do
                '~/Library/Application Support/info.eurocomp.TimingHelper.InfoExtractorService',
                '~/Library/Application Support/info.eurocomp.Timing2/',
                '~/Library/Application Support/info.eurocomp.TimingHelper/',
-
                '~/Library/Caches/info.eurocomp.TimingHelper.InfoExtractorService',
                '~/Library/Caches/info.eurocomp.Timing2/',
                '~/Library/Caches/info.eurocomp.TimingHelper/',
-
                '~/Library/Preferences/info.eurocomp.TimingHelper.InfoExtractorService.plist',
                '~/Library/Preferences/info.eurocomp.Timing2.plist',
                '~/Library/Preferences/info.eurocomp.TimingHelper.plist',
-
              ]
 end
