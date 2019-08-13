971275c5d8c771c18331421bd38582aefc309f5b
Lance M <mightyfree@users.noreply.github.com>
2019-08-12 21:59:21 -0400

    Update filemaker-pro-advanced to v18.0.2.209
    Updated filemaker-pro-advanced to version 18.0.2.209, updated sha256 sum.

--- Casks/filemaker-pro-advanced.rb
+++ Casks/filemaker-pro-advanced.rb
@@ -1,6 +1,6 @@
 cask 'filemaker-pro-advanced' do
-  version '18.0.1.122'
-  sha256 'a0393932aafa2b02afc71ef53fa4a34496bdef6d50d6fc8ea0d8e68ca25e3e60'
+  version '18.0.2.209'
+  sha256 '8915757ec0c99361fb9690d68e027be485cd4081aaa72178699a4669eafb7348'
 
   url "https://fmdl.filemaker.com/esd/fmpa_#{version}.dmg"
   name 'FileMaker Pro Advanced'
