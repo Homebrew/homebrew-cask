2f3648eb083dc430c8228b2b9823a26d0cff4620
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2019-08-13 14:52:12 +0200

    Update memory-tracker-by-timely from 2019.12 to 2019.13 (#67537)

--- Casks/memory-tracker-by-timely.rb
+++ Casks/memory-tracker-by-timely.rb
@@ -1,6 +1,6 @@
 cask 'memory-tracker-by-timely' do
-  version '2019.12'
-  sha256 'bf17d553b639a2b5511b5f6802733897eac3ec7279eaf5124cd9bff7ece20e69'
+  version '2019.13'
+  sha256 'b0498923a89771e388c327d8fb214ecd849ddc824fe61322667027f9e9908175'
 
   # timelytimetracking.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://timelytimetracking.s3.amazonaws.com/mac_tracker/Memory%20Tracker%20by%20Timely.zip'
