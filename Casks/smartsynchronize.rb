1d899a39cebd7b2cf7a20df951e689ce16ffbc24
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-13 12:32:01 -0700

    Update `URL`

--- Casks/smartsynchronize.rb
+++ Casks/smartsynchronize.rb
@@ -2,7 +2,7 @@ cask 'smartsynchronize' do
   version '4.0.1'
   sha256 'd952c4b96c7c87a700bf837f7ef0a9f959bb8a4d6e1454a861647bf0aff98da2'
 
-  url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-macosx-#{version.before_comma}.dmg"
+  url "https://www.syntevo.com/downloads/smartsynchronize/smartsynchronize-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.syntevo.com/smartsynchronize/changelog.txt',
           configuration: version.major_minor
   name 'SmartSynchronize'
