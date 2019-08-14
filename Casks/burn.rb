19bcae8c4351d66dbeadff7dc2a775c3682ff55e
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-14 15:50:57 -0700

    Update burn from 2.7.1 to 2.7.2 (#67622)

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -1,6 +1,6 @@
 cask 'burn' do
-  version '2.7.1'
-  sha256 'c9496dc32624665c547efdb66ba0ff7b8ab8a12aa76a4265f390487bbd3c4f59'
+  version '2.7.2'
+  sha256 '98e6a17bdd8ae22949c1d2b2a7417ad168ab0fe5621d9bb8159edb95496c7b97'
 
   # downloads.sourceforge.net/burn-osx was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/burn-osx/Burn/#{version}/burn-#{version}.zip"
