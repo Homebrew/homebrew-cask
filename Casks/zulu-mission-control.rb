85ea0d23000923c402d78ff88372fb7b98b3318f
Pine Mizune <pinemz@gmail.com>
2019-08-14 17:18:42 +0900

    Add zulu-mission-control 7.0.0.49-ca

---
+++ Casks/zulu-mission-control.rb
@@ -0,0 +1,11 @@
+cask 'zulu-mission-control' do
+  version '7.0.0.49-ca'
+  sha256 'b866690e9e95a5c0bd1b8bd40fc8658e3c602de06c5f50ba427ffb978341c923'
+
+  url "https://static.azul.com/zmc/bin/zmc#{version}-macos_x64.tar.gz",
+      referer: 'https://www.azul.com/products/zulu-mission-control/'
+  name 'Zulu Mission Control'
+  homepage 'https://www.azul.com/products/zulu-mission-control/'
+
+  app "zmc#{version}-macos_x64/Zulu Mission Control.app", target: 'Zulu Mission Control.app'
+end
