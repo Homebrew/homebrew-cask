3e07beb8846d7175863f7b02fe3b962b29248fce
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-14 16:22:10 -0700

    Update mongodb-compass-community from 1.19.1 to 1.19.3 (#67632)

--- Casks/mongodb-compass-community.rb
+++ Casks/mongodb-compass-community.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass-community' do
-  version '1.19.1'
-  sha256 'fe4793746cfce313cc80cd69201b13046dcdc1cf120c0ee27937e5da8a3db035'
+  version '1.19.3'
+  sha256 '917ccda640f0be826cc3fff017b1b92fc42880811ceef7406de7e6768d5f25e4'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-community-#{version}-darwin-x64.dmg"
   appcast 'https://www.mongodb.com/download-center/compass'
