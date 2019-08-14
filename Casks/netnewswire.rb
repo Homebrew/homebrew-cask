bd108801632df9ff1fdfff4733ace45ed30be3ab
ralph <ralph@cs.cf.ac.uk>
2019-08-14 09:04:51 +0100

    Update netnewswire from 5.0b2 to 5.0b4

--- Casks/netnewswire.rb
+++ Casks/netnewswire.rb
@@ -1,6 +1,6 @@
 cask 'netnewswire' do
-  version '5.0b2'
-  sha256 '1376b49e0ce1d72c8c6c9ba24cc78e84ae656d96ec6ea81362a5e5dd911c188d'
+  version '5.0b4'
+  sha256 '301caddba27bad5c83c3ae6aef73482d726dd0cc4a4e786354d833c13dbb2579'
 
   url "https://ranchero.com/downloads/NetNewsWire#{version}.zip"
   appcast 'https://ranchero.com/downloads/netnewswire-beta.xml'
