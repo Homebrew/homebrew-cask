4b579416427215b246fea82d1fd95202e4072df9
Randall <ran.dall@icloud.com>
2019-08-15 15:41:40 -0700

    Update sidequest from 0.6.4 to 0.6.6

--- Casks/sidequest.rb
+++ Casks/sidequest.rb
@@ -1,6 +1,6 @@
 cask 'sidequest' do
-  version '0.6.4'
-  sha256 '35e4aac6322e0c7cf13657f903ce9197482fe42ec8d75ab3a23528118ee3972f'
+  version '0.6.6'
+  sha256 '96accbc328c3d071989de9a3f86a988f4796bad634e37f799995c1c8bcf7eb57'
 
   # github.com/the-expanse/SideQuest was verified as official when first introduced to the cask
   url "https://github.com/the-expanse/SideQuest/releases/download/v#{version}/SideQuest-#{version}.dmg"
