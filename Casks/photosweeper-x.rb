147ecd80f2369255bf83dabece988f769dbe9831
Randall <ran.dall@icloud.com>
2019-08-15 15:55:31 -0700

    Update photosweeper-x from 3.4.1 to 3.4.2

--- Casks/photosweeper-x.rb
+++ Casks/photosweeper-x.rb
@@ -1,6 +1,6 @@
 cask 'photosweeper-x' do
-  version '3.4.1'
-  sha256 '92ec97ad68f26b7dadd60f45d8a7f5c59c5b9bae7cef04f5dd486025021f9ab4'
+  version '3.4.2'
+  sha256 '7320db1cc9b28c2696a45410d42e1529b9297481885ab9e841e386d73a6e25af'
 
   url 'https://overmacs.com/photosweeper/downloads/PhotoSweeperTrial.dmg'
   appcast 'https://overmacs.com/photosweeper/updates/photosweeper_update.xml'
