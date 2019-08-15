2a0436d3fa707af5877a042ccba22f4ff4d4fd78
Dung Nguyen <contact@dungnt.net>
2019-08-15 19:38:06 +0700

    OpenKey v1.2.2 - add sha256

--- Casks/openkey.rb
+++ Casks/openkey.rb
@@ -1,6 +1,6 @@
 cask 'openkey' do
   version '1.2.2'
-  sha256 :no_check
+  sha256 '3c78e592837c2a66d579ac221937df2655a1772029fbb6563b056bcd13bcfaf0'
 
   url "https://github.com/tuyenvm/OpenKey/releases/download/v#{version}/OpenKey1.2RC2.dmg"
   name 'OpenKey'
