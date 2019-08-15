aa2bf396cf7251c267ac7dab75ec15aabaed9f9d
Kirk Klobe <kklobe@gmail.com>
2019-08-15 13:51:13 -0500

    Update segger-embedded-studio-for-arm from 4.18 to 4.20 (#67669)

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -1,6 +1,6 @@
 cask 'segger-embedded-studio-for-arm' do
-  version '4.18'
-  sha256 '3a29e54cd5e10ba2fb5b2f20db88d06527b6086a81ffeca662409db0ea7fd2ef'
+  version '4.20'
+  sha256 'd394094ffd0ce72dbc548c39c1ff6f3053197c88915819bac2199d011223b456'
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
   name 'SEGGER Embedded Studio for ARM'
