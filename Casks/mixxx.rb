56b6a419a8599ac7e24d3eaeb1ed101004497046
luzpaz <luzpaz@users.noreply.github.com>
2019-08-15 12:05:27 -0400

    Update mixxx from 2.2.1 to 2.2.2 (#67665)

--- Casks/mixxx.rb
+++ Casks/mixxx.rb
@@ -1,6 +1,6 @@
 cask 'mixxx' do
-  version '2.2.1'
-  sha256 '2c807e4df10e0a3d1ac68578eefe12831f18598135d54c1ec056cfcdd67731b4'
+  version '2.2.2'
+  sha256 '877290bee0825bdf7424990545f71b9c295896e7ab16c1105caa1b6c8297b45f'
 
   url "https://downloads.mixxx.org/mixxx-#{version}/mixxx-#{version}-osxintel.dmg"
   appcast 'https://www.mixxx.org/download/'
