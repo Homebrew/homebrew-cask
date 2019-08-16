5c7c31b39e01fc67fc3fc101e42b14e2263c99ae
Hannes Braun <hannesbraun@mail.de>
2019-08-16 12:38:05 +0200

    Update birdfont from 3.28.1 to 3.29.4

--- Casks/birdfont.rb
+++ Casks/birdfont.rb
@@ -3,8 +3,8 @@ cask 'birdfont' do
     version '2.19.4'
     sha256 '013d9c42c2252b57079453bd27e4c18dbbc09eda55563ff1516fd079c0499f76'
   else
-    version '3.28.1'
-    sha256 '548c6a0a037f758569b25bd0c40d26e6114bb9c48df4da72e171bf35c7bfff85'
+    version '3.29.4'
+    sha256 '3c0619f16f1590ae416aa76462f069f71894da64818cb4d80ebfcb0f49e08757'
   end
 
   url "https://birdfont.org/download/birdfont-#{version}-free.dmg"
