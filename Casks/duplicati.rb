a9515f273269735b822ab97424eddacc8a64825a
Rasmus <rasmusbe@users.noreply.github.com>
2019-08-16 17:38:27 +0200

    Change duplicati 2.0.4 download URLs to github (#67545)
    The current one gives a 502 Bad Gateway error and the Duplicati forum recommends downloading via github

--- Casks/duplicati.rb
+++ Casks/duplicati.rb
@@ -2,8 +2,9 @@ cask 'duplicati' do
   version '2.0.4.23,2019-07-14'
   sha256 '160ed45028da188853e714df922a6498cebae12170a77d4c2c087e49c8d7b5de'
 
-  url "https://updates.duplicati.com/beta/duplicati-#{version.before_comma}_beta_#{version.after_comma}.dmg"
-  appcast 'https://updates.duplicati.com/beta/latest-installers.js'
+  # github.com/duplicati/duplicati was verified as official when first introduced to the cask
+  url "https://github.com/duplicati/duplicati/releases/download/v#{version.before_comma}-#{version.before_comma}_beta_#{version.after_comma}/duplicati-#{version.before_comma}_beta_#{version.after_comma}.dmg"
+  appcast 'https://github.com/duplicati/duplicati/releases.atom'
   name 'Duplicati'
   homepage 'https://www.duplicati.com/'
 
