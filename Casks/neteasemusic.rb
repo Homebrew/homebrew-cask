c6f7e00c27d75f1b6b1463ed2f23e4c58796a8ec
FAN VINGA <fanalcest@gmail.com>
2019-08-16 23:39:06 +0800

    Update NeteaseMusic from 2.1.1 to 2.1.2 (#67589)

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -1,6 +1,6 @@
 cask 'neteasemusic' do
-  version '2.1.1_790,d5a7:2019618171016'
-  sha256 'a866c75d2c5a8ddf5e3584433d0de1cb4472fbc18fb31750d3e927d890484e33'
+  version '2.1.2_796,c01a:2019713112713'
+  sha256 '2d19f542f18679ddeff99c1cf34efa1947e71b1c2a77734903a7e35f9bbf2a7e'
 
   # d1.music.126.net was verified as official when first introduced to the cask
   url "https://d1.music.126.net/dmusic/#{version.after_comma.before_colon}/#{version.after_colon}/NeteaseMusic_#{version.before_comma}_web.dmg"
