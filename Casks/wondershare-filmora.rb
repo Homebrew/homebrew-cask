93c53ba301da28c47d88862256ee242a2bcef28c
Vítor Galvão <info@vitorgalvao.com>
2019-08-16 17:02:13 +0100

    Update wondershare-filmora.rb

--- Casks/wondershare-filmora.rb
+++ Casks/wondershare-filmora.rb
@@ -1,10 +1,10 @@
 cask 'wondershare-filmora' do
-  version '9,718'
+  version '9.718'
   sha256 'd7562f6f98f6cd8a64d6571192b506bfdcef09a6050504e8ddf545d55fde145f'
 
-  url "http://download.wondershare.com/filmora#{version.before_comma}-mac_full#{version.after_comma}.dmg"
+  url "http://download.wondershare.com/filmora#{version.major}-mac_full#{version.minor}.dmg"
   name 'Wondershare Filmora9'
   homepage 'https://filmora.wondershare.com/video-editor/'
 
-  app 'Wondershare Filmora9.app'
+  app "Wondershare Filmora#{version.major}.app"
 end
