0f8abb36a81daae55a6716176bb54905de30f20e
Lutz Roeder <lutzroeder@users.noreply.github.com>
2019-08-16 20:51:22 -0700

    Update netron from 3.3.1 to 3.3.2 (#67744)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask 'netron' do
-  version '3.3.1'
-  sha256 '0a79f7bd5e4dc2dc61845fbd96f0fedbe3e60af18d782fdd6dc07218f60d3903'
+  version '3.3.2'
+  sha256 '9fa942a1fe92cb28ba1a2a408d9dae4d06dc15fde6477ad278d9c4a9fd6c3225'
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   appcast 'https://github.com/lutzroeder/netron/releases.atom'
