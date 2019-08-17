37855ab0c797a34af94b64b04c8ee146af641612
Dung Nguyen <contact@dungnt.net>
2019-08-17 18:59:04 +0700

    update OpenKey v1.2.3

--- Casks/openkey.rb
+++ Casks/openkey.rb
@@ -1,8 +1,8 @@
 cask 'openkey' do
-  version '1.2.2'
-  sha256 'b1c8e688eba87713a97a52f17cd1a980caf4930f64c077b0fa787e910369d6a8'
+  version '1.2.3'
+  sha256 '803e1f96232b40740a43c235748dea6ac292473b777b4b4bcb7ba8198af416fb'
 
-  url "https://github.com/tuyenvm/OpenKey/releases/download/v#{version}/OpenKey1.2RC2.dmg"
+  url "https://github.com/tuyenvm/OpenKey/releases/download/v#{version}/OpenKey#{version}.dmg"
   appcast 'https://github.com/tuyenvm/OpenKey/releases.atom'
   name 'OpenKey'
   homepage 'https://github.com/tuyenvm/OpenKey/'
