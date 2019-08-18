7c0dd9f01a27d52690c5b23bc542b31fe3960f24
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-17 19:20:18 -0700

    Update media-converter from 1.2 to 2.0 (#67786)

--- Casks/media-converter.rb
+++ Casks/media-converter.rb
@@ -1,6 +1,6 @@
 cask 'media-converter' do
-  version '1.2'
-  sha256 '4893e6e5bc18b9cb0f0d8e1bcb861f92595314e4f9768ef638affdfc866f37e1'
+  version '2.0'
+  sha256 '2e1da59af62a994cf45de697cfab39952030210d8c355648edf233c43fa06324'
 
   # downloads.sourceforge.net/media-converter was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/media-converter/media-converter/#{version}/media-converter-#{version}.zip"
