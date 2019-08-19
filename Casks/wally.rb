f958f38e5123ef89155974539efa3f6f2cac5def
Florian Didron <fd@librem.one>
2019-08-19 21:44:51 +0900

    Add Wally.app Latest
    Wally is an open source application allowing users to flash new
    firmwares to their ZSA keyboards.

---
+++ Casks/wally.rb
@@ -0,0 +1,10 @@
+cask 'wally' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://configure.ergodox-ez.com/wally/osx'
+  name 'Wally'
+  homepage 'https://ergodox-ez.com/pages/wally'
+
+  app 'Wally.app'
+end
