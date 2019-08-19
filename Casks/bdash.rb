9a3217d0aa8be5f7099a566776cc9e98d94bb9b6
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-19 16:22:25 -0700

    Update bdash from 1.5.7 to 1.5.8 (#67855)

--- Casks/bdash.rb
+++ Casks/bdash.rb
@@ -1,6 +1,6 @@
 cask 'bdash' do
-  version '1.5.7'
-  sha256 '1cfa66d219e2348994e5a2dc4ca18b976466e27078a6c239ce71336131e64bcf'
+  version '1.5.8'
+  sha256 '10b9650c36e28b04bc56d5c64cb9807824fe982c739cc7bdfccab90e21449581'
 
   url "https://github.com/bdash-app/bdash/releases/download/v#{version}/Bdash-#{version}-mac.zip"
   appcast 'https://github.com/bdash-app/bdash/releases.atom'
