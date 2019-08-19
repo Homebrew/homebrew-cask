daa58d92dc7e4cfda02ed38294895f30caeda20f
Julian Mayer <julian@corecode.io>
2019-08-19 16:55:37 +0200

    Update understand from 5.1.995 to 5.1.996

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask 'understand' do
-  version '5.1.995'
-  sha256 '5cf1032d4e5828f619c4b17765ab0887f13643a7d6951c6f60ac0de35d0962f7'
+  version '5.1.996'
+  sha256 '13698578b9585fa5fa7ab97df780f132768679229d4f87055296c7cab053b329'
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
   appcast 'https://scitools.com/build-notes/',
