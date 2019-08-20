540b57a3b0f9c06414b25ade60fce4dad7c0070e
Vítor Galvão <info@vitorgalvao.com>
2019-08-20 11:55:29 +0100

    Update wally.rb

--- Casks/wally.rb
+++ Casks/wally.rb
@@ -2,8 +2,8 @@ cask 'wally' do
   version '1.1.1'
   sha256 'd06c1270e9f369b3176d66b94a30b4d7f31c2a4c1c261926c7448ce3f270cf77'
 
-  # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/zsa/wally/releases/download/1.1.1-osx/wally-osx-1.1.1.dmg'
+  # github.com/zsa/wally was verified as official when first introduced to the cask
+  url "https://github.com/zsa/wally/releases/download/#{version}-osx/wally-osx-#{version}.dmg"
   appcast 'https://github.com/zsa/wally/releases.atom'
   name 'Wally'
   homepage 'https://ergodox-ez.com/pages/wally'
