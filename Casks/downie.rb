854b6c91474a2db689b7fc010c37a478d5a392c0
whatGhost <chandler.lu1997@gmail.com>
2019-08-20 18:20:46 +0800

    Update downie from 3.8.1,2047 to 3.8.2,2050 (#67877)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.8.1,2047'
-  sha256 'd7caf2f7fcec822701e6570a6f24bf17db6319b104a30215ae770449e8a7ce5f'
+  version '3.8.2,2050'
+  sha256 '3c8e1dca2808f6379eb178fc431a045f29de8ed4e3624d279496cb28a2eff722'
 
   url "https://trial.charliemonroe.net/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml"
