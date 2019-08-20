526a61c7a9030162d46430045284f2123a95d5f1
Vítor Galvão <info@vitorgalvao.com>
2019-08-20 14:06:25 +0100

    Update middleclick.rb

--- Casks/middleclick.rb
+++ Casks/middleclick.rb
@@ -3,7 +3,7 @@ cask 'middleclick' do
   sha256 'a83a329493b9140cb73b418ddfa2e921555fc7706a11d8ec2ba13c4016392aca'
 
   # github.com was verified as official when first introduced to the cask
-  url "https://github.com/cl3m/MiddleClick/releases/download/mojave/MiddleClick_#{version}.zip"
+  url "https://github.com/cl3m/MiddleClick/releases/download/mojave/MiddleClick_Mojave.zip"
   appcast 'https://github.com/cl3m/MiddleClick/releases.atom'
   name 'MiddleClick'
   homepage 'https://rouge41.com/labs/'
