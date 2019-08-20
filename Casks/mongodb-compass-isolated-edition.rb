8b360cb0053813c9f7a06a05760d25524b570efd
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-20 11:20:07 -0700

    Update mongodb-compass-isolated-edition from 1.19.3 to 1.19.6 (#67906)

--- Casks/mongodb-compass-isolated-edition.rb
+++ Casks/mongodb-compass-isolated-edition.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass-isolated-edition' do
-  version '1.19.3'
-  sha256 'd8d0be19ec53235748672059f97a65a87a7a50263ddf1acc8eff939431e9e542'
+  version '1.19.6'
+  sha256 '24fe0d67e5eb2014fe695f08259c5a19b582bcc9935bc03e0c60a7b4cbb03f07'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-isolated-#{version}-darwin-x64.dmg"
   appcast 'https://www.mongodb.com/download-center/compass'
