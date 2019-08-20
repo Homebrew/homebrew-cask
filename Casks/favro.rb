cc741a9acf7341edd07505a15a4a886b646b4230
Randall <ran.dall@icloud.com>
2019-08-20 10:38:33 -0700

    Update favro from 1.0.33 to 1.0.34

--- Casks/favro.rb
+++ Casks/favro.rb
@@ -1,6 +1,6 @@
 cask 'favro' do
-  version '1.0.33'
-  sha256 'dd1309fc5d439763b20b2d9ced34fbb124015c6b2f74e2ba80670c74e00ce3a6'
+  version '1.0.34'
+  sha256 'ed4cba5eea735cf113fb82b97a7068faaee7ba05c3f77f33b93e9e994c47bff0'
 
   url "https://download.favro.com/FavroDesktop/macOS/x64/Favro-#{version}.dmg"
   appcast 'https://download.favro.com/FavroDesktop/macOS/x64/Latest.json'
