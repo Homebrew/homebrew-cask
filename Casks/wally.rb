ef2158ddcbd3398e4df10775f7cb2af441639e85
Florian Didron <fd@librem.one>
2019-08-20 09:12:42 +0900

    Add Wally v1.1.1

--- Casks/wally.rb
+++ Casks/wally.rb
@@ -1,8 +1,10 @@
 cask 'wally' do
-  version :latest
-  sha256 :no_check
+  version '1.1.1'
+  sha256 'd06c1270e9f369b3176d66b94a30b4d7f31c2a4c1c261926c7448ce3f270cf77'
 
-  url 'https://configure.ergodox-ez.com/wally/osx'
+  # github.com was verified as official when first introduced to the cask
+  url 'https://github.com/zsa/wally/releases/download/1.1.1-osx/wally-osx-1.1.1.dmg'
+  appcast 'https://github.com/zsa/wally/releases.atom'
   name 'Wally'
   homepage 'https://ergodox-ez.com/pages/wally'
 
