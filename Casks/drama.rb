b4977e68869e5666be1999632976b90bd552b396
Sungbin Jo <pcr910303@icloud.com>
2019-08-21 19:03:20 +0900

    Fix style issues in Drama.app

--- Casks/drama.rb
+++ Casks/drama.rb
@@ -3,18 +3,18 @@ cask 'drama' do
   sha256 :no_check
 
   # pixelcut.com/drama was verified as official when first introduced to the cask
-  url "https://www.pixelcut.com/drama/drama.zip"
+  url 'https://www.pixelcut.com/drama/drama.zip'
   name 'Drama'
-  homepage 'https://www.drama.app'
+  homepage 'https://www.drama.app/'
 
   app 'Drama.app'
 
   zap trash: [
-        '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.pixelcut.drama.sfl*',
-        '~/Library/Application Support/com.pixelcut.drama',
-        '~/Library/Preferences/com.pixelcut.drama.plist',
-        '~/Library/Caches/com.apple.nsurlsessiond/Downloads/com.pixelcut.drama',
-        '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.pixelcut.drama',
-        '~/Library/Caches/com.pixelcut.drama'
-      ]
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.pixelcut.drama.sfl*',
+               '~/Library/Application Support/com.pixelcut.drama',
+               '~/Library/Preferences/com.pixelcut.drama.plist',
+               '~/Library/Caches/com.apple.nsurlsessiond/Downloads/com.pixelcut.drama',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.pixelcut.drama',
+               '~/Library/Caches/com.pixelcut.drama',
+             ]
 end
