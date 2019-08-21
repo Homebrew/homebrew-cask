85092bbf2e69cf20a8fae498ba3757e205f05234
Sebastian Riehm <hallo@herrriehm.de>
2019-08-21 07:22:24 +0200

    Update moneymoney from 2.3.16 to 2.3.17

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,6 +1,6 @@
 cask 'moneymoney' do
-  version '2.3.16'
-  sha256 'de69d4a3ca0db2f1e4d065e05467d47c36db66593feeaaa2c6905b0a9a659708'
+  version '2.3.17'
+  sha256 'e9e3c75e4f2781fc205735523dd6d170780c3c4f4188e5959b3236f276ea4f59'
 
   url 'https://service.moneymoney-app.com/1/MoneyMoney.zip'
   appcast 'https://service.moneymoney-app.com/1/Appcast.xml'
