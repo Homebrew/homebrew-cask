1a96ed153512d1f94b2e13a22f39ffd4e9f7a1c9
suschizu <brigitte@corecode.io>
2019-08-21 16:48:28 +0200

    Update bettertouchtool from 3.172 to 3.182 (#67945)

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -6,8 +6,8 @@ cask 'bettertouchtool' do
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}_final_10_9.zip"
   else
-    version '3.172'
-    sha256 '89dfdcd9e58f2d6367b079dc2dd58f0f5915f5081fe9012ad2a4674c93e55390'
+    version '3.182'
+    sha256 'e87b3e6697550b0c9f4b4e9629b2f21cc1ac41c874002d1eba6262859b98e399'
 
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}.zip"
