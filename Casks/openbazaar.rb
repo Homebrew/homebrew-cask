aa9f50c5a9ed4d0b018095dd0c12f6bd8d122125
Randall <ran.dall@icloud.com>
2019-08-21 15:49:32 -0700

    Update openbazaar from 2.3.5,rc2 to 2.3.5

--- Casks/openbazaar.rb
+++ Casks/openbazaar.rb
@@ -1,9 +1,9 @@
 cask 'openbazaar' do
-  version '2.3.5,rc2'
-  sha256 'bb7a712ed6dff5e621a4fd176af9aa05ea3158690970b5777ceee645616d2cac'
+  version '2.3.5'
+  sha256 'ab137cf1ae0dc4973b2efd221b5b89b3fb9ebd4edfb2894210b5c2abe043ab11'
 
   # github.com/OpenBazaar/openbazaar-desktop was verified as official when first introduced to the cask
-  url "https://github.com/OpenBazaar/openbazaar-desktop/releases/download/v#{version.before_comma}-#{version.after_comma}/OpenBazaar#{version.major}-#{version.before_comma}.dmg"
+  url "https://github.com/OpenBazaar/openbazaar-desktop/releases/download/v#{version}/OpenBazaar2-#{version}.dmg"
   appcast 'https://github.com/OpenBazaar/openbazaar-desktop/releases.atom'
   name "OpenBazaar#{version.major}"
   homepage 'https://www.openbazaar.org/'
