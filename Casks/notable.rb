6cbc33f65e58fa367c20142a91dd753c8134c2dc
Jérémy CHABERNAUD <djerfy@gmail.com>
2019-08-22 00:24:35 +0200

    Update notable from 1.7.2 to 1.7.3

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -1,6 +1,6 @@
 cask 'notable' do
-  version '1.7.2'
-  sha256 'ff5c29c8932fc76193f2fb43b7410edb60b37a65fe6ea35c07980d314eac7cc6'
+  version '1.7.3'
+  sha256 '40740ab18fe83ecaa952b248c18cd6ad790726a54faa8f4860f6236b2757d4f1'
 
   url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg"
   appcast 'https://github.com/notable/notable/releases.atom'
