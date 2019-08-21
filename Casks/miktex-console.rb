e25e9cb1e9f8651fa862753045d250f81ef54a5b
Aathish <aathish04@gmail.com>
2019-08-21 20:17:07 +0530

    Update miktex-console.rb

--- Casks/miktex-console.rb
+++ Casks/miktex-console.rb
@@ -3,7 +3,7 @@ cask 'miktex-console' do
   sha256 '46689b9de8bf158d6c74a17ecdcacc418e8303bc47988228fa395c660d41a9fa'
 
   url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-1-darwin-x86_64.dmg"
-  name 'MiKteX'
+  name 'MiKTeX'
   homepage 'https://miktex.org'
 
   app 'MiKTeX Console.app'
