ded22e54257237ef1f52751cf4c640e045c080f3
hussainweb <hussainweb@gmail.com>
2019-08-22 02:53:13 +0530

    Update pock.rb

--- Casks/pock.rb
+++ Casks/pock.rb
@@ -2,7 +2,7 @@ cask 'pock' do
   version '0.6'
   sha256 'bd658a234cb87ac7cb273884b694e6459ec1f8e4a1cd527407f34ed23fa59e2d'
 
-  url "https://pock.dev/download.php?file=pock_#{version.dots_to_underscores}_lack.zip"
+  url "https://pock.dev/download.php?file=pock_#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/pigigaldi/Pock/releases.atom'
   name 'Pock'
   homepage 'https://pock.dev/'
