31c28ef918c8464ade2df543f44a109598072808
Aathish04 <aathish04@gmail.com>
2019-08-21 20:03:16 +0530

    Added MiKTeX Console.app V2.9.7050

---
+++ Casks/miktex-console.rb
@@ -0,0 +1,10 @@
+cask 'miktex-console' do
+  version '2.9.7050'
+  sha256 '46689b9de8bf158d6c74a17ecdcacc418e8303bc47988228fa395c660d41a9fa'
+
+  url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-1-darwin-x86_64.dmg"
+  name 'MiKteX'
+  homepage 'https://miktex.org'
+
+  app 'MiKTeX Console.app'
+end
