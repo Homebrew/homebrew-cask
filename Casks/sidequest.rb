6aca2172675aa41886707675d8a61d7e86cf3f4e
Michael Johnson <youngmug@animeneko.net>
2019-08-21 14:50:54 -0700

    Update SideQuest to 0.6.7 (#67955)
    Update the cask file for SideQuest 0.6.7 release.

--- Casks/sidequest.rb
+++ Casks/sidequest.rb
@@ -1,6 +1,6 @@
 cask 'sidequest' do
-  version '0.6.6'
-  sha256 '96accbc328c3d071989de9a3f86a988f4796bad634e37f799995c1c8bcf7eb57'
+  version '0.6.7'
+  sha256 'fbd1aa72131a4001e303171194c720acfd3e549d07956a267a7afe1b101f3765'
 
   # github.com/the-expanse/SideQuest was verified as official when first introduced to the cask
   url "https://github.com/the-expanse/SideQuest/releases/download/v#{version}/SideQuest-#{version}.dmg"
