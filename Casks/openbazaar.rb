86040d20c58b9c8c9b425c65a52c2a4c1f1253ee
Vítor Galvão <info@vitorgalvao.com>
2019-08-22 16:33:45 +0100

    Update openbazaar.rb

--- Casks/openbazaar.rb
+++ Casks/openbazaar.rb
@@ -3,7 +3,7 @@ cask 'openbazaar' do
   sha256 'ab137cf1ae0dc4973b2efd221b5b89b3fb9ebd4edfb2894210b5c2abe043ab11'
 
   # github.com/OpenBazaar/openbazaar-desktop was verified as official when first introduced to the cask
-  url "https://github.com/OpenBazaar/openbazaar-desktop/releases/download/v#{version}/OpenBazaar2-#{version}.dmg"
+  url "https://github.com/OpenBazaar/openbazaar-desktop/releases/download/v#{version}/OpenBazaar#{version.major}-#{version}.dmg"
   appcast 'https://github.com/OpenBazaar/openbazaar-desktop/releases.atom'
   name "OpenBazaar#{version.major}"
   homepage 'https://www.openbazaar.org/'
