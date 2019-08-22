287fd1f2f9136a7e42b105ccb3acd1e9d641f93e
John Pitchko <john.pitchko@icloud.com>
2019-08-21 21:57:41 -0600

    Add cask for Novabench benchmarking tool

---
+++ Casks/novabench.rb
@@ -0,0 +1,10 @@
+cask 'novabench' do
+  version '4.0.1'
+  sha256 '024eae6de4317de11bcae5e7211dfac02d4bc19eb9ff79ba9ca9bfe33196804b'
+
+  url 'https://novabench.com/files/novabench.dmg'
+  name 'Novabench'
+  homepage 'https://novabench.com/'
+
+  app 'Novabench.app'
+end
