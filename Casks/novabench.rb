3178de4938f5eb60b5e50215b5c8aa33f245cb3e
Vítor Galvão <info@vitorgalvao.com>
2019-08-22 16:29:39 +0100

    Update novabench.rb

--- Casks/novabench.rb
+++ Casks/novabench.rb
@@ -3,6 +3,7 @@ cask 'novabench' do
   sha256 '024eae6de4317de11bcae5e7211dfac02d4bc19eb9ff79ba9ca9bfe33196804b'
 
   url 'https://novabench.com/files/novabench.dmg'
+  appcast 'https://novabench.com/download#personal'
   name 'Novabench'
   homepage 'https://novabench.com/'
 
