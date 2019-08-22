cfd40d83a336d9f94bc0d810cd006ac3c00e27cd
Julian Mayer <julian@corecode.io>
2019-08-22 18:48:04 +0200

    Update sf-symbols from 1.0 to 1.0,12

--- Casks/sf-symbols.rb
+++ Casks/sf-symbols.rb
@@ -1,6 +1,6 @@
 cask 'sf-symbols' do
-  version '1.0'
-  sha256 'f822046b1880377e4c8d1df8fd807d9335404a21245bb8abcb3481c6dd47c5a0'
+  version '1.0,12'
+  sha256 'bf5854278d00bd2eaba0c5ae25aa6d4bfbfd7b5113c782a600d6aee5e67caf05'
 
   url 'https://developer.apple.com/design/downloads/SF-Symbols.dmg'
   name 'SF Symbols'
