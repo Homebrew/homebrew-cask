98eaadd4c8f0d0b5afc9ea09a1f1d9c5108e38c6
Vítor Galvão <info@vitorgalvao.com>
2019-08-22 16:27:43 +0100

    Update miktex-console.rb

--- Casks/miktex-console.rb
+++ Casks/miktex-console.rb
@@ -1,8 +1,9 @@
 cask 'miktex-console' do
-  version '2.9.7050'
+  version '2.9.7050-1'
   sha256 '46689b9de8bf158d6c74a17ecdcacc418e8303bc47988228fa395c660d41a9fa'
 
-  url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-1-darwin-x86_64.dmg"
+  url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-darwin-x86_64.dmg"
+  appcast 'https://miktex.org/download'
   name 'MiKTeX'
   homepage 'https://miktex.org/'
 
