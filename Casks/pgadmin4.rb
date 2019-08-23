801a4f2fd4eee002cd4a26ae4af64ce1a90f07aa
suschizu <brigitte@corecode.io>
2019-08-23 19:34:22 +0200

    Update pgadmin4 from 4.11 to 4.12 (#68060)

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -1,6 +1,6 @@
 cask 'pgadmin4' do
-  version '4.11'
-  sha256 'de4dd1ed04b606b39c0f4aefcf606852c5443ceac815c9f23ed64d1f0408c217'
+  version '4.12'
+  sha256 '20fd85dd65ffe8149c610aa1b10cab94984f5c7a471f115ec3d8390e8ac2b245'
 
   # ftp.postgresql.org was verified as official when first introduced to the cask
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}.dmg"
