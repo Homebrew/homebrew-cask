da61b406f4240183f94fc8997de239f5d1c8101b
Mochaa <im@zhai2333.xyz>
2019-08-23 11:06:22 +0800

    Update Keeweb to 1.9.2 (#68038)

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,6 +1,6 @@
 cask 'keeweb' do
-  version '1.9.1'
-  sha256 '6f982d37900f28d8c398ef75f0a2866db7cb800844a484a05fffe0570626718b'
+  version '1.9.2'
+  sha256 '907baf0fe4f3331b12dbc3d15531d3173f12de9ea358fd2a5262c4d323ee45f1'
 
   # github.com/keeweb/keeweb was verified as official when first introduced to the cask
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.dmg"
