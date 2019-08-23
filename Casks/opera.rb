f2d95f7cdf13b2d18c1c505a3b9fc80a3c6dc53c
Randall <ran.dall@icloud.com>
2019-08-22 19:17:54 -0700

    Update opera from 63.0.3368.35 to 63.0.3368.43

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '63.0.3368.35'
-  sha256 '8417804ad1e53f7e607fca5dbaf2734d02b6038b230bda91a85a94d4735e5a38'
+  version '63.0.3368.43'
+  sha256 'f8649efa7d37f3a9dc8344ba1fa3b18bbbc0d4b9d680b97d38e20e87dca2f730'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   appcast 'https://ftp.opera.com/pub/opera/desktop/'
