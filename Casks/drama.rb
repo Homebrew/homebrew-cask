45e22016586e054b62a2e6c08578ad16777e4498
조성빈 <37990858+pcr910303@users.noreply.github.com>
2019-08-23 22:25:25 +0900

    Add Drama.app Latest (#67935)

---
+++ Casks/drama.rb
@@ -0,0 +1,21 @@
+cask 'drama' do
+  version '1.0.14'
+  sha256 'f28a5d05c8c4bba736e8343f115afbde57ed0f295808aa186f37a11b75b2b29d'
+
+  # pixelcut.com/drama was verified as official when first introduced to the cask
+  url 'https://www.pixelcut.com/drama/drama.zip'
+  appcast 'https://www.pixelcut.com/drama/appcast.xml'
+  name 'Drama'
+  homepage 'https://www.drama.app/'
+
+  app 'Drama.app'
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.pixelcut.drama.sfl*',
+               '~/Library/Application Support/com.pixelcut.drama',
+               '~/Library/Preferences/com.pixelcut.drama.plist',
+               '~/Library/Caches/com.apple.nsurlsessiond/Downloads/com.pixelcut.drama',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.pixelcut.drama',
+               '~/Library/Caches/com.pixelcut.drama',
+             ]
+end
