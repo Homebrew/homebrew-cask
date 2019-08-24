d6eaf80f2ff21d50ec26acff21243b656c6fda40
CoreCode <core-code@users.noreply.github.com>
2019-08-24 20:49:03 +0200

    Update wanwallet.rb

--- Casks/wanwallet.rb
+++ Casks/wanwallet.rb
@@ -3,7 +3,7 @@ cask 'wanwallet' do
   sha256 'e43eb7880f7221b879915de8639c142fdc9fa08f38ca7d81a70247f6d73a47b7'
 
   url "https://www.wanchain.org/wanwallet/desktop/WanWalletGui-mac-#{version}.zip"
-  appcast 'https://github.com/wanchain/wanwallet/releases.atom'
+  appcast 'https://www.wanchain.org/getstarted/'
   name 'Wanchain Wallet GUI'
   homepage 'https://wanchain.org/'
 
