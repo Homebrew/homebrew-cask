90b6a1b422bdb78f8635908b99c5e1228dffd321
Shashank Rammoorthy <sr22@stanford.edu>
2019-08-24 00:03:06 -0700

    Add bmux.app v1.0

--- Casks/bmux.rb
+++ Casks/bmux.rb
@@ -2,7 +2,8 @@ cask 'bmux' do
   version ':latest'
   sha256 '971ad14ddb59e2b3a40c431f83aa7ae45c4125313351209e49facd3664e36bcf'
 
-  url "https://github.com/shashank2000/bmux/releases/download/v1.0-alpha/bmux.zip"
+  url 'https://github.com/shashank2000/bmux/releases/download/v1.0-alpha/bmux.zip'
+  appcast 'https://github.com/shashank2000/bmux/releases.atom'
   name 'BMUX'
   homepage 'https://github.com/shashank2000/bmux'
 
