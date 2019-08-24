56aa7a01a494a7c7073c4ce2cefbe4779815df3f
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-23 18:36:02 -0700

    Update glip from 19.7.1 to 19.8.1 (#68087)

--- Casks/glip.rb
+++ Casks/glip.rb
@@ -1,6 +1,6 @@
 cask 'glip' do
-  version '19.7.1'
-  sha256 '03eeaa23a4b849d3f0f2672197a8f6dc9779783a23aa087b37e40cf3fd9a4c85'
+  version '19.8.1'
+  sha256 '3e4650adde9075b420ce9c28b262cf828f26041c76b1f27e89da064b793208bf'
 
   # downloads.ringcentral.com/glip/rc was verified as official when first introduced to the cask
   url "https://downloads.ringcentral.com/glip/rc/#{version}/mac/RingCentral-#{version}.dmg"
