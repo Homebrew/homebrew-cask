16609970dad32ec7ddc44cae3d41e6934ccb09d0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-24 15:52:42 -0700

    Update pd from 0.49-1 to 0.50-0 (#68083)

--- Casks/pd.rb
+++ Casks/pd.rb
@@ -1,6 +1,6 @@
 cask 'pd' do
-  version '0.49-1'
-  sha256 '6628074df8951480d8b728cd18e1bda1470d1b7a3572bd7816a28a85e12d85c2'
+  version '0.50-0'
+  sha256 '14eb88990b97046946e803bb8c5aac7d7350c443572ba507a2ede8de9628d7c2'
 
   url "http://msp.ucsd.edu/Software/pd-#{version}.mac.tar.gz"
   appcast 'http://msp.ucsd.edu/software.html'
