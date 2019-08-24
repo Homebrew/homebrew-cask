625a508e18705ca81d341b7e780f8ef7dec876ea
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-24 15:52:45 -0700

    Update quickbooks-online from 4.0.7-217 to 4.0.8-34 (#68088)

--- Casks/quickbooks-online.rb
+++ Casks/quickbooks-online.rb
@@ -1,6 +1,6 @@
 cask 'quickbooks-online' do
-  version '4.0.7-217'
-  sha256 '32df37d609ca7c30ea643ec29bb640c884d97df059040a2a70c122dfa30a0252'
+  version '4.0.8-34'
+  sha256 '19b28bdf0a09b7d1dd19cbc0f27f3ceccb79a5f1c6fa775897e71e234df9d10a'
 
   url "https://http-download.intuit.com/http.intuit/CMO/qbo_client_web/static/release/mac/QuickBooks-#{version}-mac.zip"
   appcast 'https://http-download.intuit.com/http.intuit/CMO/qbo_client_web/static/release/mac/latest-mac.yml'
