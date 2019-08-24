4a41673ff05b7dfa958d1450877a083d30c38722
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-23 17:52:26 -0700

    Update cyberghost-vpn from 7.0.2.82 to 7.0.3.83 (#68079)

--- Casks/cyberghost-vpn.rb
+++ Casks/cyberghost-vpn.rb
@@ -1,6 +1,6 @@
 cask 'cyberghost-vpn' do
-  version '7.0.2.82'
-  sha256 '310d18486eeeced68c93a23d4e9b054cfb6bd8338318ea73ce39e8ffc8f38605'
+  version '7.0.3.83'
+  sha256 '00d64ce5e4d05c96aa1faa686ce6f388aaf5db8ec46590eff6601755d580ff56'
 
   url "https://download.cyberghostvpn.com/mac/updates/v#{version.major}/cg#{version.major}mac_#{version}.dmg"
   appcast "https://download.cyberghostvpn.com/mac/updates/v#{version.major}/cyberghost_mac_update.inf"
