64bbc4a842ca00c4425fa46fc0052a61bb4afdf0
qii404 <shiboqi123@gmail.com>
2019-08-24 20:30:36 +0800

    add Another.Redis.Desktop.Manager app to cask repo
    🚀🚀🚀A faster, better and more stable redis desktop manager, compatible with Linux, windows, mac. What's more, it won't crash when loading a large number of keys.
    https://github.com/qishibo/AnotherRedisDesktopManager

---
+++ Casks/ardm.rb
@@ -0,0 +1,10 @@
+cask 'ardm' do
+  version '1.2.5'
+  name 'Another.Redis.Desktop.Manager'
+  url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v1.2.5/Another.Redis.Desktop.Manager.1.2.5.dmg"
+  sha256 "fe1c2c08c1c0972dee58fd4f0876b1bce80ce6ac9d56842e4c34979249f85050"
+  homepage 'https://github.com/qishibo/AnotherRedisDesktopManager/'
+
+  app 'Another.Redis.Desktop.Manager.app'
+
+end
