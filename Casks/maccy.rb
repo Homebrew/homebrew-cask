53d81b3cd5916462bf40c90e92396f68647bc277
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-24 13:51:10 -0700

    Update maccy from 0.4.0 to 0.4.1 (#68014)

--- Casks/maccy.rb
+++ Casks/maccy.rb
@@ -1,6 +1,6 @@
 cask 'maccy' do
-  version '0.4.0'
-  sha256 '86f9821c261550556c08940324d3862f890e5ab57bd97e70640f77c89e05f8b1'
+  version '0.4.1'
+  sha256 '467b57e4aeeee528a61942cd8ccf68ddb292a4f4661edbc3db0de399bf20add4'
 
   url "https://github.com/p0deje/Maccy/releases/download/#{version}/Maccy.app.zip"
   appcast 'https://github.com/p0deje/Maccy/releases.atom'
