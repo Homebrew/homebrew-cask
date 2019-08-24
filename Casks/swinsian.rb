3b84d546e506e973b76da98e27e5ffb2cb9d8a60
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-23 17:36:26 -0700

    Update swinsian from 2.1.14 to 2.1.15 (#68075)

--- Casks/swinsian.rb
+++ Casks/swinsian.rb
@@ -1,6 +1,6 @@
 cask 'swinsian' do
-  version '2.1.14'
-  sha256 '44a05d538fdb071dd02f628cb4fceb7eef27d158ccbb7f19bf8ac5a0a9b284b5'
+  version '2.1.15'
+  sha256 '76987c0a2437f10c67b30e7fffc9b3e768a8c9e373fa16c98d4b8f8fa01979bb'
 
   url "https://www.swinsian.com/sparkle/Swinsian_#{version}.zip"
   appcast 'https://www.swinsian.com/sparkle/sparklecast.xml'
