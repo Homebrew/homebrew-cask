c9b6eee634a526b0d7201a944ef0cecfe56fcea5
Vítor Galvão <info@vitorgalvao.com>
2019-08-25 11:38:04 +0100

    Update wanna.rb

--- Casks/wanna.rb
+++ Casks/wanna.rb
@@ -2,10 +2,10 @@ cask 'wanna' do
   version '1.0.0-alpha.2+flex'
   sha256 '73a126f1f130908e91dac1c16117516dfc1d92c9eaf6a105fd02c2c0c4872a72'
 
-  # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/mkermani144/wanna/releases/download/1.0.0-alpha.2%2Bflex/wanna-1.0.0-alpha.2.dmg'
+  # github.com/mkermani144/wanna was verified as official when first introduced to the cask
+  url "https://github.com/mkermani144/wanna/releases/download/#{version}/wanna-#{version.split('+').first}.dmg"
   appcast 'https://github.com/mkermani144/wanna/releases.atom'
-  name 'Wanna To-Do List'
+  name 'Wanna'
   homepage 'https://wanna.js.org/'
 
   app 'wanna.app'
