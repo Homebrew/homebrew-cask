925f8134d2edcbc6ec34904be1298f1306a190b8
Jianing Wang <wjnmailg@gmail.com>
2019-08-25 11:48:34 +0800

    Update OpenInTerminal from 2.0.3 to 2.0.4 (#68113)

--- Casks/openinterminal.rb
+++ Casks/openinterminal.rb
@@ -1,6 +1,6 @@
 cask 'openinterminal' do
-  version '2.0.3'
-  sha256 '6397d356259546b60236fc882c78b6bb09917eb975832e67176de4b669f45717'
+  version '2.0.4'
+  sha256 '0c2fa8342271971f16bef1023f344b930fad938217a2e1b17335c216685a7ec9'
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInTerminal.app.zip"
   appcast 'https://github.com/Ji4n1ng/OpenInTerminal/releases.atom'
