944d3c0a2bb5c97f8db1e82730fd38233e94d2a9
Chris Finazzo <chris.finazzo@gmail.com>
2019-08-25 12:04:02 -0400

    Update MarsEdit from 4.3.3 to 4.3.4 (#68132)

--- Casks/marsedit.rb
+++ Casks/marsedit.rb
@@ -1,6 +1,6 @@
 cask 'marsedit' do
-  version '4.3.3'
-  sha256 '9f25d596a05713100a067881fe0f9a299acbf0e0fac4983576274e7f0356e5e4'
+  version '4.3.4'
+  sha256 '027ec71207f82f67f9f31a0b32699a45f5ede485a43ef3d319ae5ff422be6eae'
 
   url "https://red-sweater.com/marsedit/MarsEdit#{version}.zip"
   appcast 'https://red-sweater.com/marsedit/appcast4.php'
