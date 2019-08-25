3c2bb7a7aeac66f63d03ce9b68cb4fc9bf4d22d2
mattmc3 <mattmc3@gmail.com>
2019-08-24 20:31:35 -0400

    Update Chrysalis to remove trailing whitespace

--- Casks/chrysalis.rb
+++ Casks/chrysalis.rb
@@ -2,7 +2,7 @@ cask 'chrysalis' do
   version '0.6.2'
   sha256 'feae2a3cca2330db8cff076603c7d82d398842659059ffec4f3b9fc0a9537c4f'
 
-  url "https://github.com/keyboardio/Chrysalis/releases/download/chrysalis-#{version}/Chrysalis-#{version}.dmg" 
+  url "https://github.com/keyboardio/Chrysalis/releases/download/chrysalis-#{version}/Chrysalis-#{version}.dmg"
   appcast 'https://github.com/keyboardio/Chrysalis/releases.atom'
   name 'Chrysalis'
   homepage 'https://github.com/keyboardio/Chrysalis'
