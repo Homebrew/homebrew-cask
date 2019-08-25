e6092d826d8416a0c17f3aa9e0e8bb15e7c1b21b
mattmc3 <mattmc3@gmail.com>
2019-08-24 20:16:32 -0400

    Remove url comment for Chrysalis
    CI build halted on url comment.

--- Casks/chrysalis.rb
+++ Casks/chrysalis.rb
@@ -2,7 +2,6 @@ cask 'chrysalis' do
   version '0.6.2'
   sha256 'feae2a3cca2330db8cff076603c7d82d398842659059ffec4f3b9fc0a9537c4f'
 
-  # github.com/keyboardio/Chrysalis was verified as official when first introduced to the cask
   url "https://github.com/keyboardio/Chrysalis/releases/download/chrysalis-#{version}/Chrysalis-#{version}.dmg" 
   name 'Chrysalis'
   homepage 'https://github.com/keyboardio/Chrysalis'
