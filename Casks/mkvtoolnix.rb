f003331273403a5f79ec1f9218cbeae338fb824c
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-24 22:33:37 -0700

    Update mkvtoolnix from 36.0.0 to 37.0.0 (#68126)

--- Casks/mkvtoolnix.rb
+++ Casks/mkvtoolnix.rb
@@ -1,6 +1,6 @@
 cask 'mkvtoolnix' do
-  version '36.0.0'
-  sha256 'f13112bca86713ac68d6a937b2421e50005a63d7e6aa43b151ab3bc0e7c6192e'
+  version '37.0.0'
+  sha256 '722c5df4f1922cd36fb618e0871e1a34018190934b58c35ff768e9711f73285e'
 
   url "https://mkvtoolnix.download/macos/MKVToolNix-#{version}.dmg"
   appcast 'https://www.bunkus.org/blog/feed/'
