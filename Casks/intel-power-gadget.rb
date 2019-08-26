944417ba4d37f1d680a00c727063d39b8bee6488
CoreCode <core-code@users.noreply.github.com>
2019-08-26 13:14:50 +0200

    Update intel-power-gadget.rb

--- Casks/intel-power-gadget.rb
+++ Casks/intel-power-gadget.rb
@@ -3,6 +3,7 @@ cask 'intel-power-gadget' do
   sha256 'efd306800c28abda0d5543fbc5bf78eb142a43a96feb56fcf211e3bbc83a78d3'
 
   url "https://software.intel.com/file/#{version.after_comma}/download"
+  appcast 'https://software.intel.com/en-us/articles/intel-power-gadget'
   name 'Intel Power Gadget'
   homepage 'https://software.intel.com/en-us/articles/intel-power-gadget'
 
