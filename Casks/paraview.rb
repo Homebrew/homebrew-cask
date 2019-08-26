e10dfc74b6be163f78af50a3ecaafefa7581a57a
suschizu <brigitte@corecode.io>
2019-08-26 12:05:24 +0200

    Update paraview.rb

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,6 +1,6 @@
 cask 'paraview' do
   version '5.6.2'
-  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  sha256 '9cbc9ff9b10625ccd38fd018d3e5b0424c5d394b1b57ff9d1faec182efd6101a'
 
   url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.12-64bit.dmg",
       user_agent: :fake
