b5162331a674fef7241f56c8142e32ed3085d10b
Vítor Galvão <info@vitorgalvao.com>
2019-08-26 09:21:20 +0100

    Update tiles.rb

--- Casks/tiles.rb
+++ Casks/tiles.rb
@@ -1,20 +1,18 @@
 cask 'tiles' do
-  version :latest
-  sha256 :no_check
+  version '1.0.1'
+  sha256 '65e630a26c74f49a879a13317aa7b874788dd14216feb677d9e578e4e45ef77a'
 
-  # updates.sempliva.com was verified as official when first introduced to the cask
-  url 'https://updates.sempliva.com/tiles/Tiles-latest.dmg'
+  # updates.sempliva.com/tiles was verified as official when first introduced to the cask
+  url "https://updates.sempliva.com/tiles/Tiles-#{version.after_comma}.dmg"
+  appcat 'https://updates.sempliva.com/tiles/updates.xml',
+         configuration: version.after_comma
   name 'FreeMacSoft Tiles'
   homepage 'https://freemacsoft.net/tiles/'
 
   app 'Tiles.app'
 
-  uninstall launchctl: [
-                         'com.sempliva.TilesHelper',
-                       ],
-            quit:      [
-                         'com.sempliva.Tiles',
-                       ]
+  uninstall launchctl: 'com.sempliva.TilesHelper',
+            quit:      'com.sempliva.Tiles'
 
   zap trash: [
                '~/Library/Application Support/com.sempliva.Tiles',
