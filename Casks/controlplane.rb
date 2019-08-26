af64fcb29346bbfe036110cb40439c2715eb15db
CoreCode <core-code@users.noreply.github.com>
2019-08-26 16:18:49 +0200

    Revert "Delete controlplane.rb" (#68155)
    * Revert "Delete controlplane.rb (#67191)"
    
    This reverts commit b55d6c881d609086a043e07ed983187cfb637e07.
    
    * Update controlplane.rb
    
    * Update controlplane.rb

---
+++ Casks/controlplane.rb
@@ -0,0 +1,14 @@
+cask 'controlplane' do
+  version '1.6.7'
+  sha256 'beb444b8117ed91898921a6babc384501dfd92679f5b718fceb6d3aa7a0bf651'
+
+  # github.com/dustinrue/ControlPlane was verified as official when first introduced to the cask
+  url "https://github.com/dustinrue/ControlPlane/releases/download/#{version}/ControlPlane-#{version}.dmg"
+  appcast 'https://github.com/dustinrue/ControlPlane/releases.atom'
+  name 'ControlPlane'
+  homepage 'https://www.controlplaneapp.com/'
+
+  app 'ControlPlane.app'
+
+  zap trash: '~/Library/Preferences/com.dustinrue.ControlPlane.plist'
+end
