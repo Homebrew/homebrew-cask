9c4cabb794bbcdc3891782d5b88a478715dee117
li-kbotelho <37940059+li-kbotelho@users.noreply.github.com>
2019-08-26 12:13:23 -0700

    Update Google Chrome to version 76.0.3809.132

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,6 +1,6 @@
 cask 'google-chrome' do
-  version '76.0.3809.100'
-  sha256 '366e6ca8d79c4c924cba16a476a250e9e2983b19419da3b1b347e12b151820bc'
+  version '76.0.3809.132'
+  sha256 '64d20453923386f48bffe1d7597036ed92e0121f88adc74a59a7ffb52f226bbc'
 
   url 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
   appcast 'https://omahaproxy.appspot.com/history?os=mac;channel=stable'
