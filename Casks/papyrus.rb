5bf33b0a97317cfe56c6b34cbb9255984f2533bb
CoreCode <core-code@users.noreply.github.com>
2019-08-26 20:02:52 +0200

    Update papyrus.rb

--- Casks/papyrus.rb
+++ Casks/papyrus.rb
@@ -3,7 +3,7 @@ cask 'papyrus' do
   sha256 '1aee92356a57ecf738a3e801ea306a945a111d4e76bca3234d0bc0f7ae63b93f'
 
   url "https://www.eclipse.org/downloads/download.php?file=/modeling/mdt/papyrus/rcp/#{version.after_comma}/#{version.before_comma}/papyrus-#{version.after_comma}-#{version.before_comma}-macosx64.tar.gz&r=1"
-  appcast 'https://mirrors.dotsrc.org/eclipse//modeling/mdt/papyrus/rcp/'
+  appcast 'https://mirrors.dotsrc.org/eclipse//modeling/mdt/papyrus/rcp/',
           configuration: version.after_comma
   name 'Papyrus'
   homepage 'https://eclipse.org/papyrus/'
