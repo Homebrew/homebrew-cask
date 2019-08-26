2f37c24eccb85d20c4be26184febd33f856d5a4d
Lorenzo Pane <lorenzo@pane.net>
2019-08-26 18:28:24 -0400

    Update ripcord from 0.4.13 to 0.4.14

--- Casks/ripcord.rb
+++ Casks/ripcord.rb
@@ -1,6 +1,6 @@
 cask 'ripcord' do
-  version '0.4.13'
-  sha256 '1730953872fb4233ee8d9b9d88e8eb87f9fac7cc5b615bc77bc38fa569a486fa'
+  version '0.4.14'
+  sha256 '55522da8f7aec8d8d1d487143d6e359c3596efed6e649d4396ed5dce8245cac7'
 
   url "https://cancel.fm/dl/Ripcord_Mac_#{version}.zip"
   appcast 'https://cancel.fm/ripcord/updates/v1'
