d6cc3e1f404504c183180243a29107699a70a4e7
CoreCode <core-code@users.noreply.github.com>
2019-08-27 23:50:16 +0200

    Update segger-embedded-studio-for-risc-v.rb

--- Casks/segger-embedded-studio-for-risc-v.rb
+++ Casks/segger-embedded-studio-for-risc-v.rb
@@ -3,7 +3,8 @@ cask 'segger-embedded-studio-for-risc-v' do
   sha256 '708fed7bcb5581a09a9351997061ef286e4f42efc440ec6928ecbdee434ef004'
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_RISCV_v#{version.no_dots}_macos_x64.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.segger.com/downloads/embedded-studio/EmbeddedStudio_RISCV_Mac'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.segger.com/downloads/embedded-studio/EmbeddedStudio_RISCV_Mac',
+          configuration: version.no_dots
   name 'Embedded Studio for RISC-V'
   homepage 'https://www.segger.com/products/development-tools/embedded-studio'
 
