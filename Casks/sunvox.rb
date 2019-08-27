4ac7e1ac19db43291ddda62b79d630c89926b5c1
CoreCode <core-code@users.noreply.github.com>
2019-08-27 23:09:53 +0200

    Update sunvox.rb

--- Casks/sunvox.rb
+++ Casks/sunvox.rb
@@ -3,6 +3,7 @@ cask 'sunvox' do
   sha256 '19c1a4e28459e31e1a19986f219d4caa4eb2cb5bc9f6aa994abdbb2ebf6ac4ac'
 
   url "http://www.warmplace.ru/soft/sunvox/sunvox-#{version}.zip"
+  appcast 'https://www.warmplace.ru/soft/sunvox/changelog.txt'
   name 'SunVox'
   homepage 'http://www.warmplace.ru/soft/sunvox/'
 
