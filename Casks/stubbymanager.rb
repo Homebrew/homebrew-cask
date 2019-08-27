b2964b453278eb044a18afc148c146e26cae7350
CoreCode <core-code@users.noreply.github.com>
2019-08-27 23:12:42 +0200

    Update stubbymanager.rb

--- Casks/stubbymanager.rb
+++ Casks/stubbymanager.rb
@@ -3,6 +3,7 @@ cask 'stubbymanager' do
   sha256 '0b9de130ac55e02928a21f334ac5c481b092e6535445a402caa7bb165b0b3dba'
 
   url "https://dnsprivacy.org/wiki/download/attachments/#{version.after_comma}/StubbyManager.pkg"
+  appcast 'https://github.com/Sinodun/stubby_manager_gui/releases.atom'
   name 'StubbyManager'
   homepage 'https://dnsprivacy.org/wiki/display/DP/Stubby+GUI+for+macOS'
 
