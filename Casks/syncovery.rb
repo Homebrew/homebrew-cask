7d38a20a462182c7be4d9999ba40cb0ff413a0dd
CoreCode <core-code@users.noreply.github.com>
2019-08-27 23:04:43 +0200

    Update syncovery.rb

--- Casks/syncovery.rb
+++ Casks/syncovery.rb
@@ -3,6 +3,7 @@ cask 'syncovery' do
   sha256 '66364995e0166a80fc5523dc7bcc9a322072a3c0a6f0baa3a86b3b3ed34a8d43'
 
   url "https://www.syncovery.com/release/SyncoveryMac#{version}.dmg"
+  appcast 'https://www.syncovery.com/download/mac/'
   name 'Syncovery'
   homepage 'https://www.syncovery.com/'
 
