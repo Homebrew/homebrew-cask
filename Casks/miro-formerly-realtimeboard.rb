6200a17696ffc9e8185fe079a77e19e8b6571c8f
Vítor Galvão <info@vitorgalvao.com>
2019-08-27 23:26:00 +0100

    Update miro-formerly-realtimeboard.rb

--- Casks/miro-formerly-realtimeboard.rb
+++ Casks/miro-formerly-realtimeboard.rb
@@ -1,10 +1,11 @@
 cask 'miro-formerly-realtimeboard' do
-  version '0.3.32'
-  sha256 '5122a0ae9fe99667143eaca37989c06c7c078c7d2a83894d7b088132cb44dc94'
+  version :latest
+  sha256 :no_check
 
   # desktop.realtimeboard.com was verified as official when first introduced to the cask
   url 'https://desktop.realtimeboard.com/platforms/darwin/Miro%20-%20formerly%20RealtimeBoard.dmg'
-  name 'Miro - formerly RealtimeBoard.dmg'
+  name 'Miro'
+  name 'RealtimeBoard'
   homepage 'https://miro.com/'
 
   app 'Miro - formerly RealtimeBoard.app'
