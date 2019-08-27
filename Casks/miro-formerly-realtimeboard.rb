7c317b25292e72aecde696efe13ca0b7d1103d98
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2019-08-28 04:13:28 +0900

    Fix style in Miro - formerly RealtimeBoard.app Cask

--- Casks/miro-formerly-realtimeboard.rb
+++ Casks/miro-formerly-realtimeboard.rb
@@ -2,7 +2,8 @@ cask 'miro-formerly-realtimeboard' do
   version '0.3.32'
   sha256 '5122a0ae9fe99667143eaca37989c06c7c078c7d2a83894d7b088132cb44dc94'
 
-  url "https://desktop.realtimeboard.com/platforms/darwin/Miro%20-%20formerly%20RealtimeBoard.dmg"
+  # desktop.realtimeboard.com was verified as official when first introduced to the cask
+  url 'https://desktop.realtimeboard.com/platforms/darwin/Miro%20-%20formerly%20RealtimeBoard.dmg'
   name 'Miro - formerly RealtimeBoard.dmg'
   homepage 'https://miro.com/'
 
@@ -10,6 +11,6 @@ cask 'miro-formerly-realtimeboard' do
 
   zap trash: [
                '~/Library/Preferences/com.electron.realtimeboard.plist',
-               '~/Library/Saved Application State/com.electron.realtimeboard.savedState'
+               '~/Library/Saved Application State/com.electron.realtimeboard.savedState',
              ]
 end
