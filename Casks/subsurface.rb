2eb3e94496eb86a174305d3f3b7e03ce0564ffa6
CoreCode <core-code@users.noreply.github.com>
2019-08-28 00:21:21 +0200

    Update subsurface.rb (#68232)

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -3,6 +3,7 @@ cask 'subsurface' do
   sha256 'c71bab895466d4c7baf7d58a158b60b53cc0488e9eab62ab4d4d74c92ab73db0'
 
   url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
+  appcast 'https://subsurface-divelog.org/download/'
   name 'Subsurface'
   homepage 'https://subsurface-divelog.org/'
 
