27db5de0659a57b7122fe7d66ac840761f1deee6
chrisfinazzo <chris.finazzo@gmail.com>
2019-08-28 12:42:36 -0400

    Update appcast stanza

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -3,7 +3,7 @@ cask 'piezo' do
   sha256 '36a264ae1b68cf2c73bcaff1fd1f7af52cafc31210ac8a03029faa17e5394db5'
 
   url 'https://rogueamoeba.com/piezo/download/Piezo.zip'
-  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo&system=10114'
+  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo&system=10146'
   name 'Piezo'
   homepage 'https://rogueamoeba.com/piezo/'
 
