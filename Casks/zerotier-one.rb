12998fd647e3550ee5c70a1de6cd4f1defeb0442
Randall <ran.dall@icloud.com>
2019-08-27 23:39:27 -0700

    Update zerotier-one from 1.4.2 to 1.4.4

--- Casks/zerotier-one.rb
+++ Casks/zerotier-one.rb
@@ -1,5 +1,5 @@
 cask 'zerotier-one' do
-  version '1.4.2'
+  version '1.4.4'
   sha256 '1961d1f7faafa6699bf472825408d87e5cd15ae8c41e40a8c1e2bdbd604cbcd9'
 
   url 'https://download.zerotier.com/dist/ZeroTier%20One.pkg'
