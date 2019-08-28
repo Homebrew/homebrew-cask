16ba3cad63535a0fa294fc3cabec4a6dde240602
Randall <ran.dall@icloud.com>
2019-08-27 23:49:27 -0700

    Update hubstaff from 1.4.9,1136 to 1.4.10,1235

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,6 +1,6 @@
 cask 'hubstaff' do
-  version '1.4.9,1136'
-  sha256 '0d6b2a79f032f87068b6bd6b847242b0d7dd958ee33c34c767a022771b2872ae'
+  version '1.4.10,1235'
+  sha256 'de6b22f850df6c8e306e78f12d289df6d0de5a1232a1d484f331e5290bd54b15'
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
   appcast 'https://app.hubstaff.com/appcast.xml'
