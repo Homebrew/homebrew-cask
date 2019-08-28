3275488ad371a52241a94bc299881608f743ff40
suschizu <brigitte@corecode.io>
2019-08-28 15:05:36 +0200

    Update disk-drill from 3.7.934 to 3.7.942 (#68267)

--- Casks/disk-drill.rb
+++ Casks/disk-drill.rb
@@ -1,6 +1,6 @@
 cask 'disk-drill' do
-  version '3.7.934'
-  sha256 'ff9258f14907078ebb692c746cce909c5620f4cd04da353e44155279aaa322d7'
+  version '3.7.942'
+  sha256 '42ff300cf10263ee208ec314a19e31cc67a799c277129ce8b33141eabb146fec'
 
   url "https://www.cleverfiles.com/releases/DiskDrill_#{version}.zip"
   appcast 'https://www.cleverfiles.com/releases/auto-update/dd2-newestr.xml'
