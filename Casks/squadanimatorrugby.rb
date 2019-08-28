41b92a5a01706c727df49a44301f29b0b778e271
Vítor Galvão <info@vitorgalvao.com>
2019-08-28 11:56:03 +0100

    squadanimatorrugby.rb: remove hyphen from name

--- Casks/squadanimatorrugby.rb
+++ Casks/squadanimatorrugby.rb
@@ -3,7 +3,7 @@ cask 'squadanimatorrugby' do
   sha256 'd001ddfc424ae3e47fd9fab318e4b85aa8af40a4a5755a99b717e79fa877c33b'
 
   url "https://www.squadanimator.com/downloads/SquadAnimatorRugbyOSX-#{version}.zip"
-  name 'SquadAnimator-Rugby'
+  name 'SquadAnimator Rugby'
   homepage 'https://www.squadanimator.com/'
 
   app "SquadAnimatorRugby-#{version}.app"
