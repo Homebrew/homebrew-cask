0be1adba88843ebc20677e5cacc5733fad6d7820
CoreCode <core-code@users.noreply.github.com>
2019-08-29 16:41:20 +0200

    Update enpass.rb

--- Casks/enpass.rb
+++ Casks/enpass.rb
@@ -3,7 +3,7 @@ cask 'enpass' do
   sha256 'c02d10b4ba5a2fa38402c1a2af306837e5f55d181b76a894f8763856c0c473d8'
 
   url "https://dl.enpass.io/stable/mac/package/#{version.after_comma}/Enpass.pkg"
-  appcast 'https://www.enpass.io/release-notes/macos-website-ver/'
+  appcast 'https://rest.enpass.io/enpass/alert/?package=in.sinew.Enpass-Desktop.App&os=osx%7C10.14&version=6.1.0&version_code=437&language=en%7Cen'
   name 'Enpass'
   homepage 'https://www.enpass.io/'
 
