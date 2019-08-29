a80e265572c130902dfe32b94ba01d3716e4c962
Randall <17261190+ran-dall@users.noreply.github.com>
2019-08-28 23:51:03 -0700

    Update melodics from 2.1.3331 to 2.1.3392 (#68312)

--- Casks/melodics.rb
+++ Casks/melodics.rb
@@ -1,6 +1,6 @@
 cask 'melodics' do
-  version '2.1.3331'
-  sha256 '1ebc9e9e530b402a756988ba1d16aa9afa651c2b7a9567a6efaa428b7178e2ec'
+  version '2.1.3392'
+  sha256 '170c0ac92cee21a6e352875bccc530632486d5f84b812ac9e36193e9bc2cef6b'
 
   url "https://web-cdn.melodics.com/download/MelodicsV#{version.major}.dmg"
   appcast "https://web-cdn.melodics.com/download/osxupdatescastv#{version.major}.xml"
