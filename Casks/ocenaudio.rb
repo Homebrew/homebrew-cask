ab284ce4ae1519c9e04990894dd2cc8fbde3f040
suschizu <brigitte@corecode.io>
2019-08-29 08:02:15 +0200

    Update ocenaudio.rb (#68265)

--- Casks/ocenaudio.rb
+++ Casks/ocenaudio.rb
@@ -1,12 +1,12 @@
 cask 'ocenaudio' do
-  version '3.7.3'
+  version '3.7.4'
 
   if MacOS.version <= :high_sierra
-    sha256 'd359dd72f7bd8ede0e9557b0818336f1418d66b5c50fe740916f4820857079bb'
+    sha256 'a20b4a2b87ede0e11cb05616eb46028fecea8f46df63665fd0b3d5d76d789ce4'
 
     url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_sierra.dmg'
   else
-    sha256 '6d0b42cb13083b7d7559cc060604a73e3e289886b64159e89d7d47b9e0fabd75'
+    sha256 '303d78a55aab37428ee369d656253c5e904adfa6880758aaafbe3e239ec7dcf3'
 
     url 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_mojave.dmg'
   end
