256c674ca18124acdde04f9df52d5eb2527ae09c
suschizu <brigitte@corecode.io>
2019-08-29 12:19:23 +0200

    Update liteide.rb

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -3,10 +3,10 @@ cask 'liteide' do
   sha256 'f9094b6f0732cd2b99f3f3501555e46249b3420703ef82b02150a34f5df6d023'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
-  url "https://github.com/visualfc/liteide/releases/download/x#{version.major_minor}/liteidex#{version}.macos-qt5.9.5.zip"
+  url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.9.5.zip"
   appcast 'https://github.com/visualfc/liteide/releases.atom'
   name 'LiteIDE'
   homepage 'http://liteide.org/'
 
-  app 'liteide/LiteIDE.app'
+  app "liteidex#{version}.macos-qt5.9.5/LiteIDE.app"
 end
