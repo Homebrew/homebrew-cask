867987feeb22e19dd6a05da50d8cd2bf520b0b2a
James Kelly <james@hicag.org>
2019-08-28 21:36:36 -0500

    Update Syncplay to 1.6.4a

--- Casks/syncplay.rb
+++ Casks/syncplay.rb
@@ -1,6 +1,6 @@
 cask 'syncplay' do
-  version '1.6.4'
-  sha256 'efa523f9e8fff814ed2576f35a0ffe2e75e03ce81a06c95224a46d25e0004c42'
+  version '1.6.4a'
+  sha256 'bab6c7afcfd0d30167bba3bb1cd76e12becaae9f249f04e79c6610badef10023'
 
   # github.com/Syncplay/syncplay was verified as official when first introduced to the cask
   url "https://github.com/Syncplay/syncplay/releases/download/v#{version}/Syncplay_#{version}.dmg"
