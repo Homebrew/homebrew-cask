11365e981dfa1bcb67f28a668e081db7352d5726
Randall <ran.dall@icloud.com>
2019-08-30 14:30:58 -0700

    Update bloodhound from 2.2.0 to 2.2.1

--- Casks/bloodhound.rb
+++ Casks/bloodhound.rb
@@ -1,6 +1,6 @@
 cask 'bloodhound' do
-  version '2.2.0'
-  sha256 'fffaec6a59c6d0b368fd980414500ffeb17f3155ec3353f679dfc6caf392ce84'
+  version '2.2.1'
+  sha256 '795845daafefd3f3b79a5cd599e45605aed74244de4a6d17eb1ccfb3568f938e'
 
   url "https://github.com/BloodHoundAD/BloodHound/releases/download/#{version}/BloodHound-darwin-x64.zip"
   appcast 'https://github.com/BloodHoundAD/BloodHound/releases.atom'
