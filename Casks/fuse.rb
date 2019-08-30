cbf0651e3004aa82b0acd821fd21b5e18e08118e
Aaron Liu <aaronliu0130@gmail.com>
2019-08-30 12:45:58 +0800

    Add FUSE
    Add FUSE for macOS

---
+++ Casks/fuse.rb
@@ -0,0 +1,17 @@
+cask 'fuse' do
+  version '3.10.2'
+  sha256 'fb07b8027d0cf7509b92c6a99110b98d5b7584512128ab6331ed30308363614f'
+
+  # github.com/osxfuse/osxfuse was verified as official when first introduced to the cask
+  url "https://github.com/osxfuse/osxfuse/releases/download/osxfuse-#{version}/osxfuse-3.10.2.dmg"
+  appcast 'https://github.com/osxfuse/osxfuse/releases.atom'
+  name 'FUSE for macOS'
+  homepage 'https://osxfuse.github.io/'
+
+  pkg 'FUSE for macOS.pkg'
+
+  uninstall script: {
+                      executable: "#{staged_path}/Extras/Uninstaller.app/Contents/Resources/Scripts/uninstall_osxfuse.sh",
+                      sudo:       true,
+                    }
+end
