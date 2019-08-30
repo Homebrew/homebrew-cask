70f4cf45302b9ee443685966a12a035c89f0f750
Randall <ran.dall@icloud.com>
2019-08-29 20:18:46 -0700

    Update funter from 4.4,83 to 4.5.1,87

--- Casks/funter.rb
+++ Casks/funter.rb
@@ -4,8 +4,8 @@ cask 'funter' do
     sha256 'b059000717f7f8af67ee027d81aea6c2d9f343e0eb72e439a9f286ab265b7b93'
     url 'https://nektony.com/download/funter/funter_10.12.dmg'
   else
-    version '4.4,83'
-    sha256 'a8d0182281e6e9fb4fb8d62e957ab600d3937da4e48f229a8ff4678f3be4f088'
+    version '4.5.1,87'
+    sha256 '4ec92d8d507fa78d0b5194700d62040c21f99d5c3e6a53c5badd8fb3306678f3'
     url "https://nektony.com/promo/funter/update/update_#{version.before_comma}(#{version.after_comma}).zip"
     appcast 'https://nektony.com/promo/funter/update/update.xml'
   end
