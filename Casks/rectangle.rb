230dae2ca9ef93780b432a3f5526818ac4cc28de
Randall <ran.dall@icloud.com>
2019-08-30 15:48:01 -0700

    Update rectangle from 0.3 to 0.4

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -1,6 +1,6 @@
 cask 'rectangle' do
-  version '0.3'
-  sha256 '46f95d31ab997edea02fa540bbab5ad5fac1dc3f6b317c0071ff4a51301923a1'
+  version '0.4'
+  sha256 '07f8337bfb0db2b5735d065be9bceb136c57b8affe61025b8b3648c88b42dd45'
 
   url "https://rectangleapp.com/downloads/Rectangle#{version}.dmg"
   appcast 'https://www.rectangleapp.com/downloads/updates.xml'
