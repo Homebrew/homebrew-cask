df24fa40c01342477cc4fcd41eb47206f6001393
brigitte zmoelnig <brigitte@corecode.io>
2019-08-30 20:27:51 +0200

    Update framer-x from 34743,1567006773 to 34754,1567173519

--- Casks/framer-x.rb
+++ Casks/framer-x.rb
@@ -1,6 +1,6 @@
 cask 'framer-x' do
-  version '34743,1567006773'
-  sha256 'cf9561453d39119dba9c5c90f35c7dc564ecf180c4986b14339591d58c1e0885'
+  version '34754,1567173519'
+  sha256 '7f690b11e1f5b1806949875d38d1f0e7a94eb34278cf95040955a872c37e6ea4'
 
   # dl.devmate.com/com.framer.x was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.framer.x/#{version.before_comma}/#{version.after_comma}/FramerX-#{version.before_comma}.zip"
