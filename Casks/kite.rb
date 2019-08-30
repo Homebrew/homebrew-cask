60acb545a4e2a0710ff43b0ceeb4aa3e9ab45b14
Hannes Braun <hannesbraun@mail.de>
2019-08-30 14:03:56 +0200

    Update kite from 0.20190828.0 to 0.20190829.0

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,6 +1,6 @@
 cask 'kite' do
-  version '0.20190828.0'
-  sha256 'f55453fe662d68f5a8fda2e3392f897a4367dc71afaf222aa27727b85c59a28b'
+  version '0.20190829.0'
+  sha256 '4ec65a12f483155c2f44c6e08744669341364b9798991ef7f211f591fff9da78'
 
   # kite-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://kite-downloads.s3.amazonaws.com/Kite-#{version}.dmg"
