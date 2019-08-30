8777930bc149b87e781b6a9359335404003ef2cf
suschizu <brigitte@corecode.io>
2019-08-30 08:32:44 +0200

    Update ti-connect-ce.rb from 5.3.5.924 to 5.4.0.1181 (#68324)

--- Casks/ti-connect-ce.rb
+++ Casks/ti-connect-ce.rb
@@ -1,8 +1,8 @@
 cask 'ti-connect-ce' do
-  version '5.3.5.924'
-  sha256 'fa4f5c6ffd22e90a6da8ec93a25a8fd9415e3c96ce6683e4de3118827a3ba24c'
+  version '5.4.0.1181,6E275F86CC13415BB657A9999AF403F2'
+  sha256 'c29d64c78f1f2dc0d7586841fc9767d7a1400098fb982366d3f86672d2877603'
 
-  url "https://education.ti.com/download/en/ed-tech/68CEDD34FDC94622B4DBD173E6A0D8C3/1C8292E230F145668933805CD9149505/TIConnectCE-#{version}.dmg"
+  url "https://education.ti.com/download/en/ed-tech/68CEDD34FDC94622B4DBD173E6A0D8C3/#{version.after_comma}/TIConnectCE-#{version.before_comma}.dmg"
   appcast 'https://education.ti.com/en/software/details/en/68CEDD34FDC94622B4DBD173E6A0D8C3/swticonnectcesoftwareformacintosh'
   name 'TI Connect™ CE'
   homepage 'https://education.ti.com/en/products/computer-software/ti-connect-ce-sw'
