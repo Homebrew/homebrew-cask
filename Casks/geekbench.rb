11e2d5f59510e1ae23d4d735cc6e4c4ff52ccbb1
ralph <ralph@cs.cf.ac.uk>
2019-08-30 14:37:59 +0100

    Update geekbench from 4.4.1 to 4.4.2

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -3,8 +3,8 @@ cask 'geekbench' do
     version '3.4.2'
     sha256 '05e1b977a46648d38cf6c641be7ef34722200d0168a10d4372fca771ffa24e28'
   else
-    version '4.4.1'
-    sha256 'a0f56599a6a94974953500a2b82165dfbf20a624667a190d056f68901763ddf4'
+    version '4.4.2'
+    sha256 '3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc'
   end
 
   url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
