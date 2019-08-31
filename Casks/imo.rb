e1cae6adde57093b40967d8d207480baee19bd45
Vítor Galvão <info@vitorgalvao.com>
2019-08-31 23:43:00 +0100

    Update imo.rb

--- Casks/imo.rb
+++ Casks/imo.rb
@@ -3,9 +3,7 @@ cask 'imo' do
   sha256 :no_check
 
   url 'https://imo.im/macosapp/current/Imo.dmg'
-  name 'Imo'
   name 'Imo Messanger'
-  name 'imo free video calls and chat'
   homepage 'https://imo.im/'
 
   app 'Imo.app'
