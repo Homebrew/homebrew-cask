96c400ff144c88befbac7371d9436ef82c4ee786
Vítor Galvão <info@vitorgalvao.com>
2019-08-31 23:44:25 +0100

    Update okadash.rb

--- Casks/okadash.rb
+++ Casks/okadash.rb
@@ -2,8 +2,8 @@ cask 'okadash' do
   version '1.3.0'
   sha256 'deb1a9a8bb35df7e5cdbe77eb38807cf11012457ceaadf861569faf0cd0e4e61'
 
-  url 'https://github.com/konoyono/okadash/releases/download/1.3.0/okadash-1.3.0-mac.zip'
-  appcast 'https://github.com/konoyono/okadash/releases/latest'
+  url "https://github.com/konoyono/okadash/releases/download/#{version}/okadash-#{version}-mac.zip"
+  appcast 'https://github.com/konoyono/okadash/releases.atom'
   name 'okadash'
   homepage 'https://github.com/konoyono/okadash'
 
