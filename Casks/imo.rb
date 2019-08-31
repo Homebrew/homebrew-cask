4f72a235cd9f9b6102796355f374feefd3c0ebad
Saurov <m.r.saurov@gmail.com>
2019-09-01 01:35:05 +0600

    Add Imo.app v1.1.3
    Added imo messanger cask. Style and Audit check passed.

--- Casks/imo.rb
+++ Casks/imo.rb
@@ -2,7 +2,7 @@ cask 'imo' do
   version :latest
   sha256 :no_check
 
-  url "https://imo.im/macosapp/current/Imo.dmg"
+  url 'https://imo.im/macosapp/current/Imo.dmg'
   name 'Imo'
   name 'Imo Messanger'
   name 'imo free video calls and chat'
