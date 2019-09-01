d595fe46121536c02e345d69670b9f6f21925be1
CoreCode <core-code@users.noreply.github.com>
2019-09-01 20:20:27 +0200

    Update paperspace.rb

--- Casks/paperspace.rb
+++ Casks/paperspace.rb
@@ -4,7 +4,7 @@ cask 'paperspace' do
 
   # ps-receiver.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://ps-receiver.s3.amazonaws.com/darwin/Paperspace-#{version}.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split.cgi?splitters=[]&url=https://www.paperspace.com/download&headers={%22User-Agent%22:%22Mozilla/5.0%20(Macintosh%3B%20Intel%20Mac%20OS%20X%2010_14_5)%22}'
+  appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://www.paperspace.com/download&user_agent=Mozilla/5.0%20(Macintosh%3B%20Intel%20Mac%20OS%20X%2010_14_5)'
   name 'Paperspace'
   homepage 'https://www.paperspace.com/'
 
