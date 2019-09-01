57d1c89a96a2e9ba8120f61f4e20f425c3b1382f
晋先森 <hi@jinxiansen.com>
2019-09-01 23:43:48 +0800

    added Translator

---
+++ Casks/translator.rb
@@ -0,0 +1,13 @@
+cask 'translator' do
+  version '1.0'
+  sha256 :no_check
+
+  url "https://github.com/Jinxiansen/Translator/releases/download/#{version}/Translator.app.zip"
+appcast 'https://github.com/Jinxiansen/Translator/releases.atom'
+  name 'Translator'
+  homepage 'https://github.com/Jinxiansen/Translator/'
+  auto_updates true
+  app 'Translator.app'
+  uninstall pkgutil: 'com.jinxiansen.Translator'
+
+end
