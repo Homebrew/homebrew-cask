960870dd5d76f191438da90d2786a7e9c94ab103
Santiago Maturana Arce <santiago.maturana@unidadeditorial.es>
2019-09-01 21:31:43 +0200

    Update tinymediamanager from 3.0.3 to 3.0.4

--- Casks/tinymediamanager.rb
+++ Casks/tinymediamanager.rb
@@ -1,6 +1,6 @@
 cask 'tinymediamanager' do
-  version '3.0.3'
-  sha256 '7e071c62975af647206288a7e49ea324b0ff639e4f5c282e8c755edfc0091fc8'
+  version '3.0.4'
+  sha256 '3eb0e8027a2ff2fe1e5473b0a4ef85fb3429bded292ab448378696336bf25794'
 
   url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
   appcast 'https://release.tinymediamanager.org/'
