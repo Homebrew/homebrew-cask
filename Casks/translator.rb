ad71082184b1026ed5392cafefdf19ba267c9dfb
晋先森 <hi@jinxiansen.com>
2019-09-02 00:27:36 +0800

    # Fix offenses detected in translator.app Cask

--- Casks/translator.rb
+++ Casks/translator.rb
@@ -3,11 +3,13 @@ cask 'translator' do
   sha256 :no_check
 
   url "https://github.com/Jinxiansen/Translator/releases/download/#{version}/Translator.app.zip"
-appcast 'https://github.com/Jinxiansen/Translator/releases.atom'
+  appcast 'https://github.com/Jinxiansen/Translator/releases.atom'
   name 'Translator'
   homepage 'https://github.com/Jinxiansen/Translator/'
-  auto_updates true
-  app 'Translator.app'
-  uninstall pkgutil: 'com.jinxiansen.Translator'
 
+  auto_updates true
+
+  app 'Translator.app'
+
+  uninstall pkgutil: 'com.jinxiansen.Translator'
 end
