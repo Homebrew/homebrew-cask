63d02b0340c874d242a23281fa46c903ef07590e
CoreCode <core-code@users.noreply.github.com>
2019-09-02 14:16:10 +0200

    Update cookie from 5.10 to 6.0.1 (#68263)
    * Update cookie from 5.10 to 6.0
    
    * Update cookie.rb

--- Casks/cookie.rb
+++ Casks/cookie.rb
@@ -1,13 +1,13 @@
 cask 'cookie' do
-  version '5.10'
-  sha256 '000d74085f6bbb897c45c77afb887e771004bc3452f8104e873c5fd6a2c59927'
+  version '6.0.1'
+  sha256 'a4cabe46d7494f5f0cf177d3aa4e4c781b985d45654ab015d2e5c1bdf595b372'
 
-  url "https://sweetpproductions.com/products/cookie#{version.major}/Cookie#{version.major}.dmg"
-  appcast "https://sweetpproductions.com/products/cookie#{version.major}/appcast.xml"
+  url 'https://sweetpproductions.com/products/cookieapp/Cookie.dmg'
+  appcast 'https://sweetpproductions.com/products/cookieapp/appcast.xml'
   name 'Cookie'
   homepage 'https://sweetpproductions.com/'
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: '>= :mojave'
 
   app 'Cookie.app'
 
@@ -15,5 +15,8 @@ cask 'cookie' do
                '~/Library/Application Scripts/com.sweetpproductions.Cookie5',
                '~/Library/Containers/com.sweetpproductions.Cookie5',
                '~/Library/Preferences/com.sweetpproductions.Cookie5.plist',
+               '~/Library/Application Scripts/com.sweetpproductions.CookieApp',
+               '~/Library/Containers/com.sweetpproductions.CookieApp',
+               '~/Library/Preferences/com.sweetpproductions.CookieApp.plist',
              ]
 end
