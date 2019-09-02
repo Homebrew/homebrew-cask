6de96e4c2de88785160ba2bedf146a41bbb62097
CoreCode <core-code@users.noreply.github.com>
2019-09-02 14:19:53 +0200

    Update gear-player (#68426)

--- Casks/gear-player.rb
+++ Casks/gear-player.rb
@@ -1,6 +1,6 @@
 cask 'gear-player' do
   version '2.2.38'
-  sha256 'fba05445b105728a9647aaae5c7712ad85fbaf0dcd25e1f579220ae0c6f2668d'
+  sha256 'f72a1b15e2a3af42ca1cf8b28af89281d037f46603d00eee52f6bf735139c917'
 
   url 'https://dl.gearmusicplayer.com/gear.zip'
   appcast 'https://dl.gearmusicplayer.com/gearcast.xml'
