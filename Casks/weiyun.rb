57cf89224db1914999293ccd2193d7c0497966e5
yesRabbit <chandler.lu1997@gmail.com>
2019-09-02 21:34:24 +0800

    Update weiyun to 3.0.2.386_52f040

--- Casks/weiyun.rb
+++ Casks/weiyun.rb
@@ -1,9 +1,9 @@
 cask 'weiyun' do
-  version '3.0.1.371_c31767'
-  sha256 'e968dc136aaf9552a382bbb20ad6451e4bfe9e402475957defef36cae86ec917'
+  version '3.0.2.386_52f040'
+  sha256 'bba284e3025879637e92e76f397cbd090524056912008cd22c8fea6f71f1b177'
 
   # dldir1.qq.com/weiyun was verified as official when first introduced to the cask
-  url "https://dldir1.qq.com/weiyun/Weiyun_Mac_#{version}.dmg"
+  url "https://dldir1.qq.com/weiyun/mac/Weiyun_Mac_#{version}.dmg"
   appcast 'https://qzonestyle.gtimg.cn/qzone/qzactStatics/configSystem/data/65/config1.js'
   name 'weiyun'
   homepage 'https://www.weiyun.com/'
