aafaf8a491b7d80a6fea51a7af732e833aef58eb
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-01 23:21:16 -0700

    Update lepton from 1.8.0 to 1.8.1 (#68477)

--- Casks/lepton.rb
+++ Casks/lepton.rb
@@ -1,6 +1,6 @@
 cask 'lepton' do
-  version '1.8.0'
-  sha256 '26790a8bb0f5f34918250ea9c2ac9fe81ec4488cc88244b3cf4a5e586bd3b032'
+  version '1.8.1'
+  sha256 'f73c3633c4bde64e071b8f042baafe1f1ee4acb0faee30c0cbc8592e10600d11'
 
   # github.com/hackjutsu/Lepton was verified as official when first introduced to the cask
   url "https://github.com/hackjutsu/Lepton/releases/download/v#{version}/Lepton-#{version}.dmg"
