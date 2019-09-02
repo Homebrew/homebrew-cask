d57132ef29fd78ffaedb21b9a88f904d8d720a64
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-02 11:36:06 -0700

    Update satellite-eyes from 1.4.2 to 1.4.3 (#68511)

--- Casks/satellite-eyes.rb
+++ Casks/satellite-eyes.rb
@@ -1,6 +1,6 @@
 cask 'satellite-eyes' do
-  version '1.4.2'
-  sha256 'aab5f1d05ff9a96b33407f85075373569b90f425dee948986cb245fcc5d4373f'
+  version '1.4.3'
+  sha256 '512ada3ea57aa99853b86e625e5f6675119c85f6308bdbf52adce49513dc1321'
 
   # satellite-eyes.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://satellite-eyes.s3.amazonaws.com/satellite-eyes-#{version}.zip"
