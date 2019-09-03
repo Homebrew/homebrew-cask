3d18d3259816f46920d169c7ff0589484f86c6a2
Pierre-Luc Paour <github@paour.com>
2019-09-03 12:12:11 +0200

    Add Lelivrescolaire.fr.app v1.0.4

---
+++ Casks/lelivrescolairefr.rb
@@ -0,0 +1,10 @@
+cask 'lelivrescolairefr' do
+  version '1.0.4'
+  sha256 'eaeaab1e63ef3299b0c3b9b219daf5de1c4e14c374011924503c558e6e7ad346'
+
+  url "https://ci.lls.fr/artefacts/apps/desktop/legacy/#{version}/production/mac/x64/Lelivrescolaire.fr.dmg"
+  name 'Lelivrescolaire.fr'
+  homepage 'https://www.lelivrescolaire.fr'
+
+  app 'Lelivrescolaire.fr.app'
+end
