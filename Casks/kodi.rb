b55cc58d13845db08c002461586ab3665f5b4747
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2019-09-03 09:44:19 +0100

    Update kodi

--- Casks/kodi.rb
+++ Casks/kodi.rb
@@ -1,6 +1,6 @@
 cask 'kodi' do
   version '18.4-Leia'
-  sha256 '5d305fb66739a7f733127355fe27d15e9bbcea485ae94e0519ed4e234c78eedf'
+  sha256 'e841d812ee9edc847133e4b474caba7750b3082228eccff386e1f13e7ad7cb0f'
 
   url "https://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
   appcast 'https://github.com/xbmc/xbmc/releases.atom'
