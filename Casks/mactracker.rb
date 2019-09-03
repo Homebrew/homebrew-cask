d0da50849827947951221e4881f1e6e0e57b06ea
ahousseini <contact@anver.ch>
2019-09-03 12:25:49 +0200

    Update mactracker from 7.8.1 to 7.8.2

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask 'mactracker' do
-  version '7.8.1'
-  sha256 'c5ed0018bd1b564804bf22aea65a13bf9e123095841a34b3e2ff3916a238c288'
+  version '7.8.2'
+  sha256 '6da436e8e9ee2fb90be46536029e586b7aa7df763c02c0427e441446c09e17cd'
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast 'https://update.mactracker.ca/appcast-b.xml'
