4a14518464035a8c96642a9ec6d317488733590b
Vítor Galvão <info@vitorgalvao.com>
2019-09-04 15:17:39 +0100

    Update red-eye.rb

--- Casks/red-eye.rb
+++ Casks/red-eye.rb
@@ -3,6 +3,7 @@ cask 'red-eye' do
   sha256 'af58a4b8aa5c08c77f6a5a7b1242a094d1e52f7f74311bfff04ec0ca99bf87ed'
 
   url 'https://www.hexedbits.com/downloads/redeye.zip'
+  appcast 'https://www.hexedbits.com/redeye/'
   name 'Red Eye'
   homepage 'https://www.hexedbits.com/redeye/'
 
