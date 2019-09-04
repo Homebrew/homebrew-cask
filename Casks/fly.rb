e9b567b6e0ef93c3eecc900942bd0e75635b1511
Vítor Galvão <info@vitorgalvao.com>
2019-09-04 16:01:00 +0100

    Update fly.rb

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask 'fly' do
   version '5.4.1'
-  sha256 'eb1724e250fa12477b8d22588eba608a29b704af'
+  sha256 '12479a5830575e6c5b76f7dc3b9d064e1a9234671246a5a998a5dcd5cbad4c92'
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/concourse/concourse/releases.atom'
