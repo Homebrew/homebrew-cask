0dc50f96d85694d27b71223dfba3a805cbb6dfbe
Vítor Galvão <info@vitorgalvao.com>
2019-09-04 15:15:30 +0100

    Update toneprint.rb

--- Casks/toneprint.rb
+++ Casks/toneprint.rb
@@ -1,6 +1,6 @@
 cask 'toneprint' do
   version '4.1.10'
-  sha256 'a0104ea2e55cc44c5c95a7d59e0e8e2b54d15e51d2b7442de425f87879939646'
+  sha256 'ea53e0c1e54d239766f1adf8c0dae5a017d900c7461e3c559799c570e02992cf'
 
   # downloads.music-group.com was verified as official when first introduced to the cask
   url "https://downloads.music-group.com/software/tcelectronic/TonePrintEditor/TonePrint-#{version}.dmg"
