0f234eebe357aa9981d4b8024b735c3a0e909d54
Iain Sproat <isproat@pivotal.io>
2019-09-04 14:34:59 +0100

    Update fly from 5.3.0 to 5.4.1

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask 'fly' do
-  version '5.3.0'
-  sha256 '33416ed108a45fe7462947914e3ac0b3a5372854ef1b621ae1cc19676fb1e375'
+  version '5.4.1'
+  sha256 'eb1724e250fa12477b8d22588eba608a29b704af'
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/concourse/concourse/releases.atom'
