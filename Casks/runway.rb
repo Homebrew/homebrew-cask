c9e03be6012d14a10637887a684509d4efbb8cbc
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-04 08:19:25 -0700

    Update runway from 0.9.3 to 0.9.4 (#68599)

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -1,6 +1,6 @@
 cask 'runway' do
-  version '0.9.3'
-  sha256 '113e86d4cf8c686c8ee136916facdd092ef52eebc2816a5e10ae586cf11b4ca2'
+  version '0.9.4'
+  sha256 '24c4e0ee3b0fe14b81bd47137d6dfcdcdb38f394bfd6e16f3e35c7c4eb2f9a97'
 
   # runway-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg"
