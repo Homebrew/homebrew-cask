6e3c0ca662113caae529ecb38aa45773fee9a5b1
Bill Xiong <bill@billxiong.com>
2019-09-04 07:35:31 -0700

    Update Optimage (and website) to 3.1.1 (#68542)

--- Casks/optimage.rb
+++ Casks/optimage.rb
@@ -2,10 +2,10 @@ cask 'optimage' do
   version '3.1.1'
   sha256 'd84c5e2cd96787892c9556e8bd40bfd5e7754c296c4cd6848809ce3817f1c7ad'
 
-  url 'https://getoptimage.com/download/optimage-mac.zip'
-  appcast 'https://getoptimage.com/appcast.xml'
+  url 'https://optimage.app/download/optimage-mac.zip'
+  appcast 'https://optimage.app/appcast.xml'
   name 'Optimage'
-  homepage 'https://getoptimage.com/'
+  homepage 'https://optimage.app/'
 
   auto_updates true
 
