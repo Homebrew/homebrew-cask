fc4e3193a213450589078a8475d07363aebe2f7c
Garth Mortensen <voldemortensen@users.noreply.github.com>
2019-09-04 12:05:39 -0600

    Update Synergy from 1.10.2,b126:8c010140 to 1.10.3,b139:ca35737a

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,6 +1,6 @@
 cask 'synergy' do
-  version '1.10.2,b126:8c010140'
-  sha256 'e85ab316c7783c3da8caba94f31210d80bb8c68f740a9d14322c3a20c8d2adf0'
+  version '1.10.3,b139:ca35737a'
+  sha256 '10824301d3079abbdcecfb6b2f83dc9371d929331049fd1f405b58d749f22fee'
 
   url "https://binaries.symless.com/synergy/v#{version.before_comma.major}-core-standard/v#{version.before_comma}-stable-#{version.after_colon}/synergy_#{version.before_comma}-stable_#{version.after_comma.before_colon}-#{version.after_colon}_macos.dmg"
   name 'Synergy'
