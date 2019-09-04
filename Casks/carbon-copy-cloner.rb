7d310ec377f908fe9a69b8f544396107df352d2a
suschizu <brigitte@corecode.io>
2019-09-04 22:21:56 +0200

    Update carbon-copy-cloner from 5.1.11.5793 to 5.1.11.5795 (#68626)

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -1,6 +1,6 @@
 cask 'carbon-copy-cloner' do
-  version '5.1.11.5793'
-  sha256 '8bc2c08d2c7b2e9ff5275d32abdfd4624fc2c26d26f3160caed922c441bf3067'
+  version '5.1.11.5795'
+  sha256 '466943df88f9cc37de9ffcd6252451aa6280e81e35895ae355c8d46da88231c0'
 
   # bombich.scdn1.secure.raxcdn.com/software/files was verified as official when first introduced to the cask
   url "https://bombich.scdn1.secure.raxcdn.com/software/files/ccc-#{version}.zip"
