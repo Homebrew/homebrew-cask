adf208738216b230c6813dcad8fcb1a35db2ca90
Vítor Galvão <info@vitorgalvao.com>
2019-09-04 22:50:56 +0100

    Update lelivrescolairefr.rb

--- Casks/lelivrescolairefr.rb
+++ Casks/lelivrescolairefr.rb
@@ -1,9 +1,9 @@
 cask 'lelivrescolairefr' do
-  version '1.0.4'
-  sha256 'eaeaab1e63ef3299b0c3b9b219daf5de1c4e14c374011924503c558e6e7ad346'
+  version :latest
+  sha256 :no:check
 
-  # ci.lls.fr was verified as official when first introduced to the cask
-  url "https://ci.lls.fr/artefacts/apps/desktop/legacy/#{version}/production/mac/x64/Lelivrescolaire.fr.dmg"
+  # ci.lls.fr/build/latest/mac was verified as official when first introduced to the cask
+  url 'https://ci.lls.fr/build/latest/mac/Lelivrescolaire.fr.dmg'
   name 'Lelivrescolaire.fr'
   homepage 'https://www.lelivrescolaire.fr/'
 
