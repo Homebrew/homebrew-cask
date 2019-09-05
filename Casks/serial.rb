e1979850fcd0561f4f7771235da2a20afe42a495
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-05 00:08:11 -0700

    Update serial from 1.3.9 to 1.4 (#68653)

--- Casks/serial.rb
+++ Casks/serial.rb
@@ -1,6 +1,6 @@
 cask 'serial' do
-  version '1.3.9'
-  sha256 'fb029f58a45d59bc6bf99ca2fd6d75a62b017840e7d28f63432fe8379a19421d'
+  version '1.4'
+  sha256 '30d7eae54d6baa44d7c0dd7b59a8e5ea7eac1bfe4d482244ad7179b6823716d6'
 
   url "https://www.decisivetactics.com/products/serial/dl/Serial_#{version}.zip"
   appcast 'https://www.decisivetactics.com/api/checkupdate?x-app_id=com.decisivetactics.serial.direct'
