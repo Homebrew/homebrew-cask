b21c19a09fc2f6a1c0a567a9f2b7b3359c54cb1f
Randall <ran.dall@icloud.com>
2019-09-04 19:50:38 -0700

    Update soulver from 3.1.1-43 to 3.1.1.1-44

--- Casks/soulver.rb
+++ Casks/soulver.rb
@@ -1,6 +1,6 @@
 cask 'soulver' do
-  version '3.1.1-43'
-  sha256 'a5744079a2f0944d5c6a96e31b04bb97c5b5803a7dab4460af2a3f7cab690264'
+  version '3.1.1.1-44'
+  sha256 'efb6b1d45d061feaf8afb399366fd151d5e7e591daa61bd4dfb92c94f937d50a'
 
   url "https://soulver.app/mac/sparkle/soulver-#{version}.zip"
   appcast 'https://soulver.app/mac/sparkle/appcast.xml'
