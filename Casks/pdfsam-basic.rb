6f37e7ba813cdc9b16cc269bdab7b32a7162c6a1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-04 23:21:00 -0700

    Update pdfsam-basic from 4.0.3 to 4.0.4 (#68643)

--- Casks/pdfsam-basic.rb
+++ Casks/pdfsam-basic.rb
@@ -1,6 +1,6 @@
 cask 'pdfsam-basic' do
-  version '4.0.3'
-  sha256 'c96d058f625fc181830adad47e901cb739a71c11a7dc82e759973753511f6212'
+  version '4.0.4'
+  sha256 '4aea1fe056a51dc9137546a4cb2304c033959c3ed5fc7656ba42bd3cd94c3a6a'
 
   # github.com/torakiki/pdfsam was verified as official when first introduced to the cask
   url "https://github.com/torakiki/pdfsam/releases/download/v#{version}/PDFsam-#{version}.dmg"
