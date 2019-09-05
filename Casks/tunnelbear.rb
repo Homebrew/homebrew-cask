84978e2372c0ad943708f4c1184be967a2561cf7
Randall <ran.dall@icloud.com>
2019-09-04 19:49:43 -0700

    Update tunnelbear from 3.8.9 to 3.9.1

--- Casks/tunnelbear.rb
+++ Casks/tunnelbear.rb
@@ -1,6 +1,6 @@
 cask 'tunnelbear' do
-  version '3.8.9'
-  sha256 '3daeebc5c8cd11fd76e417a6f0edb4f4d820e379f16dbb72c8938e3a0aa74dfd'
+  version '3.9.1'
+  sha256 '71c88b08d2103b1e2cc85f27b82ac3aad22130b43dbfffa957c1aae90695f8f5'
 
   # tunnelbear.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://tunnelbear.s3.amazonaws.com/downloads/mac/TunnelBear-#{version}.zip"
