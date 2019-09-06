dd114ca6b155733a636432b4cdb0f97be06a5968
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-06 12:52:28 -0700

    Update kactus from 0.3.21 to 0.3.22 (#68732)

--- Casks/kactus.rb
+++ Casks/kactus.rb
@@ -1,6 +1,6 @@
 cask 'kactus' do
-  version '0.3.21'
-  sha256 '328e39e262d271a9240452a64d002151c4cef5880c42c319eb417b48593499ac'
+  version '0.3.22'
+  sha256 'f4bc4c4678fbb2bc0016848f4fa04b5bb3030ac94c3b834b506dfac3e4f12c48'
 
   # github.com/kactus-io/kactus was verified as official when first introduced to the cask
   url "https://github.com/kactus-io/kactus/releases/download/v#{version}/Kactus-macos.zip"
