64fc1bf3d911a226be138aed59c1a68f432c33a9
Dafydd <daf@daf.lol>
2019-09-06 12:33:47 +0100

    MagicCap 2.0.0 - Finally Done

---
+++ Casks/magiccap.rb
@@ -0,0 +1,13 @@
+cask 'magiccap' do
+  version '2.0.0'
+  sha256 :no_check
+
+  # github.com/magiccap was verified as official when first introduced to the cask
+  url 'https://github.com/magiccap/MagicCap/releases/download/v2.0.0/magiccap-mac.dmg'
+  appcast 'https://github.com/magiccap/MagicCap/releases.atom'
+  name 'MagicCap'
+  homepage 'https://magiccap.me/'
+
+  app 'MagicCap.app'
+end
+
