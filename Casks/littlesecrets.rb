0c70a5aa0f37b8a206d149b2118b62d93362dccb
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-05 21:21:05 -0700

    Update littlesecrets from 1.9.2 to 1.9.4 (#68698)

--- Casks/littlesecrets.rb
+++ Casks/littlesecrets.rb
@@ -1,6 +1,6 @@
 cask 'littlesecrets' do
-  version '1.9.2'
-  sha256 '1cf0ffb294bc7b0d3676dfaa518e659c312fc381d5f28ecf2bb383fa24b44de5'
+  version '1.9.4'
+  sha256 '8281c1f648c038b296a02940126c29032ff387b90a880d63834e303e1b3a5ff7'
 
   url "https://www.mani.de/download/littlesecrets/LittleSecrets#{version}.dmg"
   appcast 'https://www.mani.de/de/littlesecrets/history.html'
