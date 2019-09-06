3378f9f857bd3134664985ebd93645315c4b1823
Randall <ran.dall@icloud.com>
2019-09-05 20:38:05 -0700

    Update metabase from 0.33.0.0 to 0.33.1.0

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,6 +1,6 @@
 cask 'metabase' do
-  version '0.33.0.0'
-  sha256 '0313a15b3d3f28aa5fba49fd17e76ff975a63c57c68707756b604ed3e18162fe'
+  version '0.33.1.0'
+  sha256 'dab0d09ede0cae3459d5905de7ed633d659e4759e6c84800d5ddc70002ac20ba'
 
   # s3.amazonaws.com/downloads.metabase.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/downloads.metabase.com/v#{version.major_minor_patch}/Metabase.zip"
