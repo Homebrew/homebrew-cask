cc176e03720829b953dc0f8e1851dbfc7fc91005
Jérémy CHABERNAUD <djerfy@gmail.com>
2019-09-06 20:39:47 +0200

    Update cutter from 1.8.3 to 1.9.0

--- Casks/cutter.rb
+++ Casks/cutter.rb
@@ -1,6 +1,6 @@
 cask 'cutter' do
-  version '1.8.3'
-  sha256 '02cf1b247767f683fc5179da6070427e37c51727ad89e1893a57e66152fa1ead'
+  version '1.9.0'
+  sha256 'f5024f3eec251656ed5c5983fc04a47d0691d585818fc76abb60ae49ef9d3217'
 
   # github.com/radareorg/cutter was verified as official when first introduced to the cask
   url "https://github.com/radareorg/cutter/releases/download/v#{version}/Cutter-v#{version}-x64.macOS.dmg"
