9550311b89cb673019460d2debae9168d603d067
miccal <miccal.matthews@gmail.com>
2019-09-07 09:37:00 +0800

    Update texshop from 4.31 to 4.35 (#68756)

--- Casks/texshop.rb
+++ Casks/texshop.rb
@@ -1,6 +1,6 @@
 cask 'texshop' do
-  version '4.31'
-  sha256 'e7c356f94cda32f79e31158961042e72970181d6463ed3af24c5b26d93ef7725'
+  version '4.35'
+  sha256 'e22943a73b5c778d3669babd635501ecde4681e4a0956acf647a203a157bda3e'
 
   url "https://pages.uoregon.edu/koch/texshop/texshop-64/texshop#{version.no_dots}.zip"
   appcast 'https://pages.uoregon.edu/koch/texshop/texshop-64/texshopappcast.xml'
