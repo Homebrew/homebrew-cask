8dc04912f967d9bf71fa9a7a1e3d14755301d86e
Randall <ran.dall@icloud.com>
2019-09-08 13:55:40 -0700

    Update power-manager from 5.1.0 to 5.1.1

--- Casks/power-manager.rb
+++ Casks/power-manager.rb
@@ -1,6 +1,6 @@
 cask 'power-manager' do
-  version '5.1.0'
-  sha256 'd58c3fe9c5bcf9f380d1c3c8353ce2ecb6154c4b8860faa6a1416152e7a26659'
+  version '5.1.1'
+  sha256 '3c049a683bcc7d1c1195a3da37a24ce1b21786cc895682c7d1c5613ed1fbd035'
 
   url "https://www.dssw.co.uk/powermanager/dsswpowermanager-#{version.no_dots}.dmg"
   appcast 'https://version.dssw.co.uk/powermanager/'
