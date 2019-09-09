661c9d15db902ff0b5bffa07bc92ce8588ac7982
Julian Mayer <julian@corecode.io>
2019-09-09 23:07:59 +0200

    Update pdf-expert from 2.4.24,617 to 2.4.29,641

--- Casks/pdf-expert.rb
+++ Casks/pdf-expert.rb
@@ -1,6 +1,6 @@
 cask 'pdf-expert' do
-  version '2.4.24,617'
-  sha256 'c3c215e33a75d4eef967441d370f54c66d8fbd042ed2c56c372dce9d9a911f0e'
+  version '2.4.29,641'
+  sha256 'e61de5665f471a8ad8a5a0b9e9775573c358e17e765cffcdc354358ad290a536'
 
   # d1ke680phyeohy.cloudfront.net was verified as official when first introduced to the cask
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
