5cf183224655fcb6560a0d3083d5ef4fce52c136
Vítor Galvão <info@vitorgalvao.com>
2019-09-09 14:54:46 +0100

    Update klokki.rb

--- Casks/klokki.rb
+++ Casks/klokki.rb
@@ -1,9 +1,10 @@
 cask 'klokki' do
-  version :latest
-  sha256 :no_check
+  version '1.1.2'
+  sha256 '8667dd3acac54f35f6f2c1bc489e768a7e007b3a354d9ce49904fc7ef0ce24e6'
 
-  # bit.ly was verified as official when first introduced to the cask
-  url 'https://bit.ly/DownloadKlokki'
+  # storage.yandexcloud.net/klokki was verified as official when first introduced to the cask
+  url 'https://storage.yandexcloud.net/klokki/Klokki.dmg'
+  appcast 'https://storage.yandexcloud.net/klokki/appcast.xml'
   name 'Klokki'
   homepage 'https://klokki.com/'
 
