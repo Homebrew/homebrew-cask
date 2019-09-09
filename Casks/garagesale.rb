221b16ce5c51ec9caa4b5f5353aab4047e1f7962
CoreCode <core-code@users.noreply.github.com>
2019-09-09 12:18:56 +0200

    Update garagesale from 7.0.18 to 7.0.19 (#68814)

--- Casks/garagesale.rb
+++ Casks/garagesale.rb
@@ -1,6 +1,6 @@
 cask 'garagesale' do
-  version '7.0.18'
-  sha256 '52c1bcbf249c3f2b42dd1eaf69e7028169bc9968d4111f43dbd706356ba5b77f'
+  version '7.0.19'
+  sha256 '134ede092a9c72c88db81fac7f49bdb0791dc8318c7b960f7214c08ee0bcb772'
 
   url "https://downloads.iwascoding.com/downloads/GarageSale_#{version}.dmg"
   appcast "https://www.iwascoding.com/cgi-bin/version_check.cgi?APPLICATION=GarageSale&APP_BUNDLE_VERSION=#{version.major}00"
