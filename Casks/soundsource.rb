cdd19ebdc96f5a2d291cdc8f03361f4b48c9564b
CoreCode <core-code@users.noreply.github.com>
2019-09-09 23:11:46 +0200

    Update soundsource.rb

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -3,7 +3,7 @@ cask 'soundsource' do
   sha256 '98b7777c6a24f465cde3e844183364293538e9beae0cb0b53287dbe31518d776'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
-  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&version=4008000'
+  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&system=10146&version=4000000'
   name 'SoundSource'
   homepage 'https://rogueamoeba.com/soundsource/'
 
