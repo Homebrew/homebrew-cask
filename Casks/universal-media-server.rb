e13bddb49d583e161cac1d3eaca874e58ba8a741
suschizu <brigitte@corecode.io>
2019-09-09 16:56:17 +0200

    Update universal-media-server.rb

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -3,7 +3,7 @@ cask 'universal-media-server' do
   sha256 '4d7d0464247b82fb8f91e27a50b26d40112443b093ab4a07d406dbebffec4872'
 
   # github.com/UniversalMediaServer/UniversalMediaServer was verified as official when first introduced to the cask
-  url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg" 
+  url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg"
   appcast 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases.atom'
   name 'Universal Media Server'
   homepage 'https://www.universalmediaserver.com/'
