42b452b4d28c8a5899b3fca86a8b481a6a412253
Randall <ran.dall@icloud.com>
2019-09-09 22:09:50 -0700

    Update visual-paradigm from 16.0,20190902 to 16.0,20190906

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,6 +1,6 @@
 cask 'visual-paradigm' do
-  version '16.0,20190902'
-  sha256 '3a2cd5910c9d3f541ad89c75a22495f425ba111ae7a2069a2498d3b41626a096'
+  version '16.0,20190906'
+  sha256 '793e15325f3ae15a554115823ba6e76023be3f68bf6be7a4201f14972c8e269d'
 
   url "https://usa6.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   appcast 'https://www.visual-paradigm.com/downloads/vp/checksum.html',
