d72abd015607b11378f99c3869450cf40569bd02
Vadim Peretokin <vperetokin@gmail.com>
2019-09-10 18:41:45 +0200

    Use correct URL for Mudlet

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -1,8 +1,8 @@
 cask 'mudlet' do
-  version '4.1.0'
+  version '4.1.1'
   sha256 '0530a26ea7e4f3ecfcee0d2caa5b6184ef2cbb4da367946070c8e5730d816648'
-
-  url "https://www.mudlet.org/download/Mudlet-#{version}.dmg"
+  
+  url "https://www.mudlet.org/wp-content/files/Mudlet-#{version}.dmg"
   appcast 'https://github.com/Mudlet/Mudlet/releases.atom'
   name 'Mudlet'
   homepage 'https://www.mudlet.org/'
