d0c3e3e13fdd1e3bb9b22bbb1c8f1c4b6fd54de1
CoreCode <core-code@users.noreply.github.com>
2019-09-10 15:53:32 +0200

    Update soundsource from 4.1.2 to 4.1.4 (#68714)
    * Update soundsource from 4.1.2 to 4,1,4
    
    * Update soundsource.rb
    
    * Update soundsource.rb
    
    * Update soundsource.rb

--- Casks/soundsource.rb
+++ Casks/soundsource.rb
@@ -1,13 +1,13 @@
 cask 'soundsource' do
-  version '4.1.2'
-  sha256 '24a5e99328ae00f2e565dab706f9eca96c902111d696629783911dfce64cb11f'
+  version '4.1.4'
+  sha256 '98b7777c6a24f465cde3e844183364293538e9beae0cb0b53287dbe31518d776'
 
   url 'https://rogueamoeba.com/soundsource/download/SoundSource.zip'
-  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&version=4008000'
+  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.soundsource&system=10146&version=4000000'
   name 'SoundSource'
   homepage 'https://rogueamoeba.com/soundsource/'
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: '>= :sierra'
 
   app 'SoundSource.app'
 end
