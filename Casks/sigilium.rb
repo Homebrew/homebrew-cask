fab35b93f64ee0a9d5c85fc6a53f60b079e1d094
Vincent Robert <vincent.robert@touch-sell.com>
2019-09-10 15:22:16 +0200

    Add Sigilium 0.9.0 <https://sigilium.com>
    Sigilium is a tool to centralize e-mail signatures for an organization.
    It will then provide tools to integrate with various e-mail clients.
    This application is the client for macOS Mail.app.

---
+++ Casks/sigilium.rb
@@ -0,0 +1,10 @@
+cask 'sigilium' do
+  version '0.9.0'
+  sha256 '3c203219efe3f66f0a00cb34eb79a6acb5dd4fbe723e8b7b84c03db9bfa04d77'
+
+  url "https://sigilium.com/apps/sigilium.zip"
+  name 'Sigilium Mail'
+  homepage 'https://sigilium.com/'
+
+  app 'Sigilium Email Signatures.app'
+end
