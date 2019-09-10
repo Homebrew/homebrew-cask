973c5fbe675310128c31b56e47a092eaee0abe4c
Randall <ran.dall@icloud.com>
2019-09-09 21:55:21 -0700

    Update plant from 1.4.7.3 to 1.4.7.4

--- Casks/plant.rb
+++ Casks/plant.rb
@@ -1,6 +1,6 @@
 cask 'plant' do
-  version '1.4.7.3'
-  sha256 '2d3f0a67e0b3cf2286e37473cb4e8a9efa528367eebb585769a86cddccb72223'
+  version '1.4.7.4'
+  sha256 'd26dcb7fdfadcbb23f52bbcde2b9031717073b230bad7602d77ee71c2f6266de'
 
   url "https://projects.plantapp.io/app/plant_v#{version}.dmg"
   appcast 'https://projects.plantapp.io/app/changelog.xml'
