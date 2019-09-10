c312810223d6bb48c15d32a8fb8c315ebd4bfd76
Randall <ran.dall@icloud.com>
2019-09-10 10:09:07 -0700

    Update camunda-modeler from 3.3.2 to 3.3.4

--- Casks/camunda-modeler.rb
+++ Casks/camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask 'camunda-modeler' do
-  version '3.3.2'
-  sha256 '47f10cadcda492519a92c03992597de4b1af5aeef14701ca7a95441591d1501e'
+  version '3.3.4'
+  sha256 '2e39e5986aa644b238be8da8af1b369279bc0952822c961d9c32e140ef99ec7b'
 
   url "https://camunda.org/release/camunda-modeler/#{version}/camunda-modeler-#{version}-mac.zip"
   appcast 'https://camunda.com/download/modeler/'
