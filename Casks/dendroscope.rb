66868ea93d31576921ab2fe0b4e576eec8306fa3
Romain Studer <romain.studer@benevolent.ai>
2019-09-10 16:09:28 +0100

    Add Dendroscope 3.6.3, Fix "installer script"

--- Casks/dendroscope.rb
+++ Casks/dendroscope.rb
@@ -1,23 +1,22 @@
 cask 'dendroscope' do
-	version '3.6.3'
+  version '3.6.3'
 	sha256 '2b9557232149da9d6d2fa0d07bd98b21836141e2759ca92986e5d7a3e21d0bde'
 
 	url "https://ab.inf.uni-tuebingen.de/data/software/dendroscope3/download/Dendroscope_macos_#{version.dots_to_underscores}.dmg"
-  	name 'Dendroscope'
-  	homepage 'https://ab.inf.uni-tuebingen.de/software/dendroscope/welcome.html'
+	name 'Dendroscope'
+	homepage 'https://ab.inf.uni-tuebingen.de/software/dendroscope/welcome.html'
 
-  	installer script: {
-  		executable: "#{appdir}/Dendroscope Installer.app/Contents/MacOS/JavaApplicationStub"
-  		args: ['-q'],
-  	}
-  	end
+	installer script: {
+		executable: "Dendroscope Installer.app/Contents/MacOS/JavaApplicationStub",
+		args: ['-q'],
+	}
 
-  	uninstaller script: {
-  		executable: "#{appdir}/Dendroscope/Dendroscope Uninstaller.app/Contents/MacOS/JavaApplicationStub"
-  		args: ['-q'],
-  	}
+	uninstall script: {
+		executable: "#{appdir}/Dendroscope/Dendroscope Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+		args: ['-q'],
+	}
 
-  	caveats do
-  		depends_on_java
-	end
+	caveats do
+		depends_on_java
+  end
 end
