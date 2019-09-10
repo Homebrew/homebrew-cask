1e6048d894f41abb8f28363b34df56a501cdb1b2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-10 11:07:23 -0700

    Update activedock from 236,1565611909 to 237,1568037944 (#68873)

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,6 +1,6 @@
 cask 'activedock' do
-  version '236,1565611909'
-  sha256 '58588a1fb6cbfa729ca252319b99ce91b005dd18542698fbf21bbe93fde2660b'
+  version '237,1568037944'
+  sha256 'afaf9a0ce57b27b9795a2a99d6a2a0068e6174f984742182c7f76ed846dc2d33'
 
   # dl.devmate.com/com.sergey-gerasimenko.ActiveDock was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.sergey-gerasimenko.ActiveDock/#{version.before_comma}/#{version.after_comma}/ActiveDock-#{version.before_comma}.zip"
