e7f4e779c0f305452fe469fdaeb56a4cba75d7d1
reboot81 <saurage@gmail.com>
2019-09-10 11:06:55 +0000

    Update unlox.rb (#68834)
    * Update unlox.rb
    
    Changed sha256 to reflect file from https://unlox.it/download/
    
    * Update unlox.rb

--- Casks/unlox.rb
+++ Casks/unlox.rb
@@ -1,6 +1,6 @@
 cask 'unlox' do
-  version '3.0.0.5'
-  sha256 '9e0c64512a11595de12548eb48b9a2b805770d124ac29c8e10798cc213180df0'
+  version '3.0.1.0'
+  sha256 '9f88fcf526b673b7329d9efb8b5731277990d5781bf78cdfd6aa64b082c19bfa'
 
   url 'https://unlox.it/download/'
   appcast 'https://unlox.it/download/update.php'
