24145ef21f961b8eb39d065114611b843d6a09d9
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-10 10:19:21 -0700

    Update `URL` (credit: @vadi2)

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -2,7 +2,7 @@ cask 'mudlet' do
   version '4.1.1'
   sha256 '4abdc2af15530fcc1422b102f91bb006bc82b8e725ec3ae71abc241395e912a2'
 
-  url "https://www.mudlet.org/download/Mudlet-#{version}.dmg"
+  url "https://www.mudlet.org/wp-content/files/Mudlet-#{version}.dmg"
   appcast 'https://github.com/Mudlet/Mudlet/releases.atom'
   name 'Mudlet'
   homepage 'https://www.mudlet.org/'
