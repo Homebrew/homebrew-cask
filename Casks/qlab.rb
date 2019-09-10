dbc786d0bd4f756e3d7fa859edb4cc6b3d5be767
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-10 10:51:20 -0700

    Update qlab from 4.4.5 to 4.5 (#68872)

--- Casks/qlab.rb
+++ Casks/qlab.rb
@@ -1,6 +1,6 @@
 cask 'qlab' do
-  version '4.4.5'
-  sha256 'eab8f96f0e95c46f61555fa76e332cf35952c5f4959aad86552a28c671985410'
+  version '4.5'
+  sha256 '3e5a7fc27992ee050f885eb1092f226ef0797fe418f17a4a5efe82debc280a33'
 
   url "https://figure53.com/qlab/downloads/QLab-#{version}.zip"
   appcast "https://figure53.com/qlab/downloads/appcast-v#{version.major}/"
