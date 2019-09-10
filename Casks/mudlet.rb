22529bde4081bc2db2d920883cec3b666f17f43a
Vadim Peretokin <vperetokin@gmail.com>
2019-09-10 16:50:11 +0000

    Update sha256

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -1,7 +1,7 @@
 cask 'mudlet' do
   version '4.1.1'
-  sha256 '0530a26ea7e4f3ecfcee0d2caa5b6184ef2cbb4da367946070c8e5730d816648'
-  
+  sha256 '4abdc2af15530fcc1422b102f91bb006bc82b8e725ec3ae71abc241395e912a2'
+
   url "https://www.mudlet.org/wp-content/files/Mudlet-#{version}.dmg"
   appcast 'https://github.com/Mudlet/Mudlet/releases.atom'
   name 'Mudlet'
