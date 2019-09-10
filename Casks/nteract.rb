67284855a32e5f54316b8360f4afe3b6f6de72a7
Randall <ran.dall@icloud.com>
2019-09-10 09:10:55 -0700

    Update nteract from 0.14.5 to 0.15.0

--- Casks/nteract.rb
+++ Casks/nteract.rb
@@ -1,6 +1,6 @@
 cask 'nteract' do
-  version '0.14.5'
-  sha256 '92e28683cb8d39c4975eb28109fd5a639cef4d27c0987aff64dd59d9913ec394'
+  version '0.15.0'
+  sha256 '1356996bce666439052cec6322105f80c9da592866384c711040c5a285f1988e'
 
   url "https://github.com/nteract/nteract/releases/download/v#{version}/nteract-#{version}.dmg"
   appcast 'https://github.com/nteract/nteract/releases.atom'
