2d4851db392b62dd6a2a36ea0293f5f073ef0e74
Song Li <boltomli@users.noreply.github.com>
2019-09-11 21:25:56 +0800

    Add Feishu 3.6.3 (#68811)
    * Add Feishu 3.6.3
    
    Feishu (Lark) is a tool to create team collaboration experiences.
    
    * Update feishu.rb
    
    * Add appcast

---
+++ Casks/feishu.rb
@@ -0,0 +1,15 @@
+cask 'feishu' do
+  version '3.6.3'
+  sha256 '8ea622f24938354ec95d79946b21b7f10c7bd36f7198b1290375631f6eaad537'
+
+  # sf3-ttcdn-tos.pstatp.com was verified as official when first introduced to the cask
+  url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/Feishu_#{version}.dmg"
+  appcast 'https://www.feishu.cn/api/downloads'
+  name 'feishu'
+  homepage 'https://www.feishu.cn/'
+
+  depends_on macos: '>= :mavericks'
+
+  # Renamed for consistency: app name is different in the Finder and in a shell.
+  app 'Lark.app', target: 'Feishu.app'
+end
