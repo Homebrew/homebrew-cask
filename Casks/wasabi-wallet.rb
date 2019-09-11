38e28cbfee4f42976014635074f41a770852efec
MitchellCash <mitchell@mitchellcash.com>
2019-09-11 18:28:00 +0100

    Update wasabi-wallet to 1.1.7

--- Casks/wasabi-wallet.rb
+++ Casks/wasabi-wallet.rb
@@ -1,6 +1,6 @@
 cask 'wasabi-wallet' do
-  version '1.1.6'
-  sha256 '6dd5dc9951c72a0c1dcfa68efc3d062248bd638828b470581caaab9e24438fd5'
+  version '1.1.7'
+  sha256 '2ec5131ebc97eace1d5d6e519980536d9467247586e0825388582f455e6203ea'
 
   # github.com/zkSNACKs/WalletWasabi was verified as official when first introduced to the cask
   url "https://github.com/zkSNACKs/WalletWasabi/releases/download/v#{version}/Wasabi-#{version}.dmg"
