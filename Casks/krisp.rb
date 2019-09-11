f93e2e29027282f1b785eb4d0267cb0325c932a3
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-10 21:28:07 -0700

    Update krisp.rb

--- Casks/krisp.rb
+++ Casks/krisp.rb
@@ -3,9 +3,9 @@ cask 'krisp' do
   sha256 '21103f0123982ab608fffe56e7124e80a80f58f718f635ae789981a7fda4ac79'
 
   url "https://cdn.krisp.ai/mac/release/v#{version.major}.#{version.minor}/krisp_#{version}.pkg"
-  appcast 'https://krisp.ai/index.html'
+  appcast 'https://krisp.ai/'
   name 'Krisp'
-  homepage 'https://krisp.ai/index.html'
+  homepage 'https://krisp.ai/'
 
   auto_updates true
   depends_on macos: '>= :sierra'
