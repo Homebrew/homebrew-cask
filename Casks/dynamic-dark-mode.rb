dbc7d5485acbfbc7f9023cd991636b2ee55bcf98
Zhiyu Zhu/朱智语 <public-apollonian@outlook.com>
2019-09-11 08:37:13 -0400

    Use GitHub’s release RSS

--- Casks/dynamic-dark-mode.rb
+++ Casks/dynamic-dark-mode.rb
@@ -3,7 +3,7 @@ cask 'dynamic-dark-mode' do
   sha256 '76fb98e23627ecefc5d426320ff1a433fb06cf4d1e18fa7016e78766a2bd4321'
 
   url "https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases/download/#{version}/Dynamic_Dark_Mode-#{version}.zip"
-  appcast 'https://apollozhu.github.io/Dynamic-Dark-Mode/appcast.xml'
+  appcast 'https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases.atom'
   name 'Dynamic Dark Mode'
   homepage 'https://github.com/ApolloZhu/Dynamic-Dark-Mode'
 
