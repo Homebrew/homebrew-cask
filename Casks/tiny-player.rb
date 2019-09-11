e0f1a74fdc29048a57e1401a96b32a6450159916
Viktor Szakats <commit@vszakats.net>
2019-09-11 15:29:02 +0000

    secure URLs

--- Casks/tiny-player.rb
+++ Casks/tiny-player.rb
@@ -2,10 +2,10 @@ cask 'tiny-player' do
   version '1.2.10'
   sha256 '2ae80b4b6d704d3a97ab212f8374a85cd4e9a8412ae1ea58c2d5539bd99db1d9'
 
-  url "http://download.catnapgames.com/TinyPlayer-#{version}.zip"
-  appcast 'http://download.catnapgames.com/TinyPlayerAppcast.xml'
+  url "https://download.catnapgames.com/TinyPlayer-#{version}.zip"
+  appcast 'https://download.catnapgames.com/TinyPlayerAppcast.xml'
   name 'Tiny Player for Mac'
-  homepage 'http://www.catnapgames.com/tiny-player-for-mac/'
+  homepage 'https://www.catnapgames.com/tiny-player-for-mac/'
 
   auto_updates true
 
