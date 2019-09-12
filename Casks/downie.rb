3dea057e7525137533d12af3f5cf7068e5c6bd6f
Randall <ran.dall@icloud.com>
2019-09-11 22:46:46 -0700

    Update downie from 3.8.3,2055 to 3.8.4,2062

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.8.3,2055'
-  sha256 '22c6e19dce2a609745679bef92c1a90bc1b27b48c2e1a229d01b9df06379b374'
+  version '3.8.4,2062'
+  sha256 'a0b92c2a3b80ff3568acbe7b560a003514da2c29887f1d94fabc95cbb3b1188b'
 
   url "https://trial.charliemonroe.net/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml"
