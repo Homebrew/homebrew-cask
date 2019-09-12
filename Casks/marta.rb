9a6487c44c8cf5d749eb2d7ea292e215e42820fc
Yan Zhulanow <mail@yanex.org>
2019-09-12 18:06:04 +0900

    Update marta from 0.6.2 to 0.7 (#68991)

--- Casks/marta.rb
+++ Casks/marta.rb
@@ -1,6 +1,6 @@
 cask 'marta' do
-  version '0.6.2'
-  sha256 'a50fd738ffbb633b7965427305632e2221ae67b1be54003c5a3d2ebccf461e47'
+  version '0.7'
+  sha256 '0692fabab3f423f740e563a6d5109091d6e2b5c5936b89885b3b6cc101766d5d'
 
   url "https://updates.marta.yanex.org/release/Marta-#{version}.dmg"
   appcast 'https://updates.marta.yanex.org/release/appcast.xml'
