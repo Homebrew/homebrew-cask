b13e2cb05c485617bac96c8f1c274fb836504a62
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-11 23:21:45 -0700

    Update astropad from 3.2.1 to 3.3.0 (#68953)

--- Casks/astropad.rb
+++ Casks/astropad.rb
@@ -1,6 +1,6 @@
 cask 'astropad' do
-  version '3.2.1'
-  sha256 '54132253068fa556a1c002f4a527b2f9fc14ed8dd2d1414117e4760572ff0b30'
+  version '3.3.0'
+  sha256 'c72f14f1969477a28dd7de5380dca5a0510d09bb1e64ff04c7f6d6b3c72a7ea1'
 
   url "https://astropad.com/downloads/Astropad-#{version}.dmg"
   appcast 'https://astropad.com/downloads/sparkle.xml'
