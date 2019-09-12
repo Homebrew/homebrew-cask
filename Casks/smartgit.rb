ff0c05a67a85468d3234dd80648fe245bdaef7ad
ralph <ralph@cs.cf.ac.uk>
2019-09-12 08:56:22 +0100

    Update smartgit from 19.1.2 to 19.1.3

--- Casks/smartgit.rb
+++ Casks/smartgit.rb
@@ -1,6 +1,6 @@
 cask 'smartgit' do
-  version '19.1.2'
-  sha256 '06d03899892be79ab8cc965b775d88752f9676b3f135c2d33ce46cac40d07767'
+  version '19.1.3'
+  sha256 'd57b5ff33e35805aac05221a115ad1ffb89495909dab32cff82176ef59083c39'
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.syntevo.com/smartgit/changelog.txt',
