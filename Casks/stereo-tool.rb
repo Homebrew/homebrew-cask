b29caa322b91da83789496f4cb423cdfd3302a02
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-12 00:21:18 -0700

    Update stereo-tool from 9.34 to 9.35 (#68969)

--- Casks/stereo-tool.rb
+++ Casks/stereo-tool.rb
@@ -1,6 +1,6 @@
 cask 'stereo-tool' do
-  version '9.34'
-  sha256 '8b0c9044b39c29e26ce5c5f1d7d344290908cab107731992daaddd0a8f7c9b24'
+  version '9.35'
+  sha256 'db3e369592f07206d6a28903434787a03fd3195c36ccd2a14c38ed8d2d3396ff'
 
   url 'https://www.stereotool.com/download/ThimeoStereoTool-Installer.dmg'
   appcast 'https://www.stereotool.com/download/'
