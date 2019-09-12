ebb1bc407eb2fe61e90ff6f9314348edbf99950b
Randall <ran.dall@icloud.com>
2019-09-11 23:59:06 -0700

    Update subsurface from 4.9.1 to 4.9.3

--- Casks/subsurface.rb
+++ Casks/subsurface.rb
@@ -1,6 +1,6 @@
 cask 'subsurface' do
-  version '4.9.1'
-  sha256 'c71bab895466d4c7baf7d58a158b60b53cc0488e9eab62ab4d4d74c92ab73db0'
+  version '4.9.3'
+  sha256 '00f6b59d75c6435f64045d0b452be9e61223592761a0d88c4f93c45330308fab'
 
   url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
   appcast 'https://subsurface-divelog.org/download/'
