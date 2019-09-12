c2b32e92b3bba6d7bd1eebda3cec42b5241dd418
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-11 23:22:01 -0700

    Update compositor from 1.12.0 to 1.12.1 (#68952)

--- Casks/compositor.rb
+++ Casks/compositor.rb
@@ -1,6 +1,6 @@
 cask 'compositor' do
-  version '1.12.0'
-  sha256 '5e98cb97b63f3f670b521d537aa4922281c3ddae5d2ee419b00c192f0af7a157'
+  version '1.12.1'
+  sha256 '9ed45b72511cd56aa5a8d92b364d8f56040f9fba96d84a1f9ff08b1b1b124c44'
 
   url "https://compositorapp.com/updates/Compositor_#{version}.zip"
   appcast 'https://compositorapp.com/updates/appcast.xml'
