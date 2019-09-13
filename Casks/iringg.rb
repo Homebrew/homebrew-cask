75f74ac09f18df09710a6a06d92f05e5c4d117b9
Randall <ran.dall@icloud.com>
2019-09-12 22:24:02 -0700

    Update iringg from 1.0.41,1567697104 to 1.0.42,1568243164

--- Casks/iringg.rb
+++ Casks/iringg.rb
@@ -1,6 +1,6 @@
 cask 'iringg' do
-  version '1.0.41,1567697104'
-  sha256 'a0aedb2bb0e874e2d19be10bc76b078cad1ebb78d575ce16154570a85f8b6039'
+  version '1.0.42,1568243164'
+  sha256 '8d826d665b36285adfeec5877f6c0d8bf54fcbb847df62f0b4039ba834a49b43'
 
   # dl.devmate.com/com.softorino.iringg was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.softorino.iringg/#{version.before_comma}/#{version.after_comma}/iRinggforMac-#{version.before_comma}.zip"
