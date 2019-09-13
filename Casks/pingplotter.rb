fcb4f54eb6c91ede6502306d271ae62c1e98c47d
Randall <ran.dall@icloud.com>
2019-09-12 22:32:42 -0700

    Update pingplotter from 5.11.3 to 5.11.5

--- Casks/pingplotter.rb
+++ Casks/pingplotter.rb
@@ -1,5 +1,5 @@
 cask 'pingplotter' do
-  version '5.11.3'
+  version '5.11.5'
   sha256 '0b00c13301faaea9ae71d1fe3ac3c63090e29924eaa0333852337e1c8e0ec59a'
 
   url 'https://www.pingplotter.com/downloads/pingplotter_osx.zip'
