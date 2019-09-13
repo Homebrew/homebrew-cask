9a771d14d281bbc9d1fb632a85166a199c0e9bd4
HeadFox <lucienpeslier@gmail.com>
2019-09-13 11:30:29 +0200

    Add DeepL Latest

---
+++ Casks/deepl.rb
@@ -0,0 +1,22 @@
+cask 'deepl' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.deepl.com/macos/download/DeepL.dmg'
+  name 'DeepL Translator'
+  homepage 'https://www.deepl.com/'
+
+  app 'DeepL.app'
+
+  uninstall quit: [
+                    'com.linguee.DeepLCopyTranslator',
+                    'com.linguee.DeepLLauncher',
+                    'com.linguee.DeepLStatusBar'
+                  ]
+
+  zap trash: [
+               '~/Library/Caches/com.linguee.DeepLCopyTranslator',
+               '~/Library/Group Containers/group.com.linguee.DeepL',
+               '~/Library/Preferences/com.linguee.DeepLCopyTranslator.plist',
+             ]
+end
