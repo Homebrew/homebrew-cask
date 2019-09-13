c34d52a9f34888d99c8e93b73a1bfb48072ba427
JackMacWindows <jackbruienne@yahoo.com>
2019-09-13 00:57:32 -0400

    Add CraftOS-PC v2.0.1
    v2.0.1 was just released, updated recipe

--- Casks/craftos-pc.rb
+++ Casks/craftos-pc.rb
@@ -1,6 +1,6 @@
 cask 'craftos-pc' do
-  version '2.0'
-  sha256 '3fc4d58a1d65a6a93955a2779a6fc11e3bf7a4a17aecd62f0ad1f49f8a0b29d8'
+  version '2.0.1'
+  sha256 'b259fa8f8347221408f1dd16551dd841fa453956a6c2ed516133fd3b0a9f5ed4'
 
   url "https://github.com/MCJack123/craftos2/releases/download/v#{version}/CraftOS-PC.dmg"
   appcast 'https://github.com/MCJack123/craftos2/releases.atom'
