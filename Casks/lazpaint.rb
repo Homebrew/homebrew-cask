9cd81842be2703f0380b9fa853344aa93e1abc5d
suschizu <brigitte@corecode.io>
2019-09-13 16:41:06 +0200

    Update lazpaint from 7.0.5 to 7.0.6 (#69053)

--- Casks/lazpaint.rb
+++ Casks/lazpaint.rb
@@ -1,9 +1,9 @@
 cask 'lazpaint' do
-  version '7.0.5'
-  sha256 'f12d9c977c2e3ea2ec4dab6d914a96f0be3c5de81cffa8a502a2068ac62753b8'
+  version '7.0.6'
+  sha256 'a77881f8d3d3077644a1072e243084d7c95fea8ac7e890384002845f9c52bc06'
 
   # github.com/bgrabitmap/lazpaint was verified as official when first introduced to the cask
-  url "https://github.com/bgrabitmap/lazpaint/releases/download/v#{version}/lazpaint#{version}_macos64.zip"
+  url "https://github.com/bgrabitmap/lazpaint/releases/download/v#{version}/lazpaint#{version}_macos64.dmg"
   appcast 'https://github.com/bgrabitmap/lazpaint/releases.atom'
   name 'LazPaint'
   homepage 'https://bgrabitmap.github.io/lazpaint/'
