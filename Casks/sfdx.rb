77b09c648fc12fca981defbecb3384796e6f82c2
Vítor Galvão <info@vitorgalvao.com>
2019-09-14 13:57:37 +0100

    sfdx.rb: make versioned - 7.8.1-8f830784cc

--- Casks/sfdx.rb
+++ Casks/sfdx.rb
@@ -1,12 +1,13 @@
 cask 'sfdx' do
-  version :latest
-  sha256 :no_check
+  version '7.8.1-8f830784cc'
+  sha256 '91756020f4208041bbe14cf1edbedb7397d747a6fdd3961f7cb1c72a408a5c43'
 
-  url 'https://developer.salesforce.com/media/salesforce-cli/sfdx-osx.pkg'
+  url "https://developer.salesforce.com/media/salesforce-cli/sfdx-cli/channels/stable/sfdx-cli-v#{version}.pkg"
+  appcast 'https://developer.salesforce.com/media/salesforce-cli/manifest.json'
   name 'Salesforce DX CLI'
   homepage 'https://developer.salesforce.com/tools/sfdxcli'
 
-  pkg 'sfdx-osx.pkg'
+  pkg "sfdx-cli-v#{version}.pkg"
 
   uninstall pkgutil: 'com.sfdx.cli',
             delete:  '/usr/local/bin/sfdx'
