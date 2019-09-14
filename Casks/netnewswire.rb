b75007d90a3b7a304feac5c555005e854bd13288
Randall <ran.dall@icloud.com>
2019-09-13 19:26:46 -0700

    Update netnewswire from 5.0 to 5.0.1

--- Casks/netnewswire.rb
+++ Casks/netnewswire.rb
@@ -1,6 +1,6 @@
 cask 'netnewswire' do
-  version '5.0'
-  sha256 'b917714d2d42f8517f17a2a5463dfc0ca6ae4990b57f175eb3bd8152891055ea'
+  version '5.0.1'
+  sha256 '2d52ba4bd191331d55d1b0e46f78c3cdde9e5e51279b05983fe4f1a22bb92e6c'
 
   # github.com/brentsimmons/NetNewsWire was verified as official when first introduced to the cask
   url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip"
