c7547f3c357203a73b669302316bfc7611a420ff
Vítor Galvão <info@vitorgalvao.com>
2019-09-14 15:08:08 +0100

    Update cytoscape.rb

--- Casks/cytoscape.rb
+++ Casks/cytoscape.rb
@@ -2,7 +2,7 @@ cask 'cytoscape' do
   version '3.7.2'
   sha256 '8bb131921f7a030b4f06156d1d6e67d132a41e6febca6e48f2386df8ac028f4d'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/cytoscape/cytoscape was verified as official when first introduced to the cask
   url "https://github.com/cytoscape/cytoscape/releases/download/#{version}/Cytoscape_#{version.dots_to_underscores}_macos.dmg"
   appcast 'https://cytoscape.org/download.html',
           configuration: version.dots_to_underscores
