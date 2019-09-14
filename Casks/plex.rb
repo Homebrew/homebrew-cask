afddfdd9d5b617fb3c2bfe7f2b26f27c4ccdb55c
suschizu <brigitte@corecode.io>
2019-09-14 23:59:09 +0200

    Update plex.rb

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -2,7 +2,7 @@ cask 'plex' do
   version '1.3.1.916-1cb2c34d'
   sha256 'f43f89b758a4531c350c5ca20b8e76512630e0090d0a6754e2c09acf98093416'
 
-  url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.dmg"
+  url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/6.json'
   name 'Plex'
   homepage 'https://www.plex.tv/'
