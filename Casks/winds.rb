6c996e9805433df44694539bc57e471b739f66a9
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-13 20:19:32 -0700

    Update winds from 3.1.11 to 3.1.16 (#69090)

--- Casks/winds.rb
+++ Casks/winds.rb
@@ -1,6 +1,6 @@
 cask 'winds' do
-  version '3.1.11'
-  sha256 'f99d4c0f1d85697078ab20cc88b11a556d0ecaba51966bf9c213ad194ef474b3'
+  version '3.1.16'
+  sha256 '8b69823278849f87a73f7573efe64d2b4fbdf94b8bb5d46d8a8f7be53d85cd74'
 
   # s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/winds-2.0-releases/releases/Winds-#{version}.dmg"
