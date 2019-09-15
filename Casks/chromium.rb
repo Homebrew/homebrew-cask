7362946d45d99cbae6d310ccb7b631d59bba433e
ahousseini <contact@anver.ch>
2019-09-15 22:16:21 +0200

    Update chromium from 692493 to 696687

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask 'chromium' do
-  version '692493'
-  sha256 'e60c5b59e42ed4606c889595682dc7e2f197f72b5919f47a23eabcaca48cf5ee'
+  version '696687'
+  sha256 'd83f8e63edbe6262e8ae0db9e3a1c8a85f11d0043c76d790d335ae4fe6885aa7'
 
   # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac was verified as official when first introduced to the cask
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
