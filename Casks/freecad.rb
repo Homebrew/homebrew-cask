a31230f4f3f5ab9e790d5fcf165f88223e2da833
Carsten Heidmann <carsten.heidmann@disy.net>
2019-09-15 12:49:51 +0200

    corrected hash sum for free cad 0.18.3 (#69135)
    * Update panoply version to 4.9,2
    
    * Adjusted Hash for panoply 4.9.2
    
    * Update panoply.rb
    
    * new version 2.14.0
    
    * Updated rocket-chat to version 2.14.1
    
    * Update pgadmin4.rb
    
    new version 4.4
    
    * update pgadmin4 to version 4.5
    
    * SHA256 for FreeCAD (freecad 0.18.3) has changed. the new Hash has been taken from the website and checked after download

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,6 +1,6 @@
 cask 'freecad' do
   version '0.18.3,16131'
-  sha256 'a9bfeadf2706308de59727d4ecfb9b273623f8dd8d4830af20a62b41ec27a190'
+  sha256 '4d107f5867652ead6fd154ae11749ffa9d4ab473a124ab0673e0f353abf6288d'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg"
