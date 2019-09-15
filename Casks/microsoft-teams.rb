af9dd5bc7bb467dda114e1435c41928dd1afbcc5
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-14 23:14:53 -0700

    Update microsoft-teams from 1.2.00.21868 to 1.2.00.22561 (#68979)

--- Casks/microsoft-teams.rb
+++ Casks/microsoft-teams.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-teams' do
-  version '1.2.00.21868'
-  sha256 '6b537e7f3e1c6cee298a738cb3c567675ad556e0dd8702032aec7fbe51399551'
+  version '1.2.00.22561'
+  sha256 'a64489e0fd96b488901ee811aaf171ff10bc9edaa92aabd517d86d75a95935cf'
 
   url "https://statics.teams.microsoft.com/production-osx/#{version}/Teams_osx.pkg"
   appcast 'https://teams.microsoft.com/downloads/DesktopUrl?env=production&plat=osx'
@@ -11,5 +11,6 @@ cask 'microsoft-teams' do
 
   pkg 'Teams_osx.pkg'
 
-  uninstall pkgutil: 'com.microsoft.teams'
+  uninstall pkgutil:   'com.microsoft.teams',
+            launchctl: 'com.microsoft.teams.TeamsUpdaterDaemon'
 end
