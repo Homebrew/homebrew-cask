77a8c791e08c9db2dd2e34278786fc3a71a380ff
Markus Reiter <me@reitermark.us>
2019-09-15 22:23:40 +0200

    Update onedrive.rb

--- Casks/onedrive.rb
+++ Casks/onedrive.rb
@@ -9,9 +9,7 @@ cask 'onedrive' do
   homepage 'https://onedrive.live.com/'
 
   auto_updates true
-  conflicts_with cask: [
-                         'microsot-office',
-                       ]
+  conflicts_with cask: 'microsot-office'
   depends_on macos: '>= :sierra'
 
   pkg 'OneDrive.pkg'
