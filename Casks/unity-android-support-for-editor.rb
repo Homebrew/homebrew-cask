0a4d036fb8b9ba97baa478ea66db7f231953c4f8
brigitte zmoelnig <brigitte@corecode.io>
2019-09-15 17:01:47 +0200

    Update unity-android-support-for-editor from 2019.2.3f1,8e55c27a4621 to 2019.2.5f1,9dace1eed4cc

--- Casks/unity-android-support-for-editor.rb
+++ Casks/unity-android-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-android-support-for-editor' do
-  version '2019.2.3f1,8e55c27a4621'
-  sha256 '62e9c992f735baa0d3687824164e42635701587d4c74735795d2f0dc86409a19'
+  version '2019.2.5f1,9dace1eed4cc'
+  sha256 'a2c3054cdbb2adb8ee3346fff7b0a6b13f7a00576a3fca411e6e246919777d79'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
