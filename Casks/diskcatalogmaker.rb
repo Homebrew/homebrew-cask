4ba0986e35aa98f9322cac42eed7baf6bec5b518
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-14 23:34:54 -0700

    Update diskcatalogmaker from 7.5.4 to 7.5.5 (#69124)

--- Casks/diskcatalogmaker.rb
+++ Casks/diskcatalogmaker.rb
@@ -1,6 +1,6 @@
 cask 'diskcatalogmaker' do
-  version '7.5.4'
-  sha256 '9e8a48136b7ec8a63bf298c6e1b811f4ab6aba269a5099e40dbffcbe2be47130'
+  version '7.5.5'
+  sha256 '8c29273038682cb67e25217b996510827388aefbbe5d1d4d3420b7b74e326c7d'
 
   url 'https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip'
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.diskcatalogmaker.com/zip/DiskCatalogMaker.zip',
