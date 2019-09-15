fec91d3de80049b6fa3209e034a1f6dfb3a10cd4
Anver Housseini <contact@anver.ch>
2019-09-15 22:36:13 +0200

    Update busycontacts from 1.3.5,130500 to 1.4.1,140101 (#69162)

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask 'busycontacts' do
-  version '1.3.5,130500'
-  sha256 '5300615705f44335d2eb80433a056518adee2fe224a97fedd409a1314c77dd7b'
+  version '1.4.1,140101'
+  sha256 '05ded006794b1b6aaf0b08790f9789e9e94c97e1f18d3fdf873dfa76f8deb814'
 
   url 'https://www.busymac.com/download/BusyContacts.zip'
   appcast 'https://www.busymac.com/busycontacts/news.plist'
