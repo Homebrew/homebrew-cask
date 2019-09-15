1993210534be37835c579095a3c1622858308195
suschizu <brigitte@corecode.io>
2019-09-15 16:49:59 +0200

    Update cctalk.rb

--- Casks/cctalk.rb
+++ Casks/cctalk.rb
@@ -3,7 +3,7 @@ cask 'cctalk' do
   sha256 'a70f9f3e5ef8bb22d5ac3dcb87790f435ef35edbf708a9fb41ab0bb90a719ccd'
 
   # cc.hjfile.cn was verified as official when first introduced to the cask
-  url "http://cc.hjfile.cn/cc/CCtalk.#{version}/8/1/103/CCtalk.#{version}.dmg"
+  url "https://cc.hjfile.cn/cc/CCtalk.#{version}/8/1/103/CCtalk.#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=http://www.cctalk.com/webapi/basic/v1.1/version/down%3Fapptype=1%26terminalType=8%26versionType=103'
   name 'CCtalk'
   homepage 'https://www.cctalk.com/download/'
