d8116599fd4f6abebf9e1e4636dca48274aeea28
Marc Schwede <git@schwedenmut.de>
2019-09-16 21:30:47 +0200

    Add DeepL 1.0.1 (#69102)

---
+++ Casks/deepl.rb
@@ -0,0 +1,23 @@
+cask 'deepl' do
+  version '1.0.1'
+  sha256 'eacb5702a750a0d49a4da8165f3951d4dc519ab6363eb707e28af91164e019e7'
+
+  url "https://www.deepl.com/macos/download/LpsA0EG5frbfX8p5/DeepL#{version.no_dots}.zip"
+  name 'DeepL'
+  homepage 'https://www.deepl.com/'
+
+  app 'DeepL.app'
+
+  uninstall quit: [
+                    'com.linguee.DeepLCopyTranslator',
+                    'com.linguee.DeepLLauncher',
+                    'com.linguee.DeepLStatusBar',
+                  ]
+
+  zap trash: [
+               '~/Library/Caches/com.linguee.DeepLCopyTranslator',
+               '~/Library/Caches/com.linguee.DeepLCopyTranslator.ShipIt',
+               '~/Library/Group Containers/*.com.linguee.DeepL',
+               '~/Library/Preferences/com.linguee.DeepLCopyTranslator.plist',
+             ]
+end
