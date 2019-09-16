e9b64ab68f5a70208a9e7ccca470509f77acc05a
suschizu <brigitte@corecode.io>
2019-09-16 22:51:59 +0200

    Update qiyimedia from 20190818,5.12.8 to 20190912,5.13.11 (#69186)

--- Casks/qiyimedia.rb
+++ Casks/qiyimedia.rb
@@ -1,6 +1,6 @@
 cask 'qiyimedia' do
-  version '20190818,5.12.8'
-  sha256 'd2fc916c7836aa10d36a9cd3469d266105bf14eafe6c5428e858f6c5e6f87f8b'
+  version '20190912,5.13.11'
+  sha256 'ac546972d444c41403f709a11286bde2e32a1a55734ba79e5d16b844b2f8e988'
 
   url 'https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg'
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://mbdapp.iqiyi.com/j/ot/iQIYIMedia_000.dmg'
