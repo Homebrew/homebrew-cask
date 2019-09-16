93c6b3f6b9816ec0fe2315c56ed7a9491817e8f9
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-09-16 03:19:00 -0400

    Update magicavoxel from 0.99.4.1 to 0.99.4.2

--- Casks/magicavoxel.rb
+++ Casks/magicavoxel.rb
@@ -1,6 +1,6 @@
 cask 'magicavoxel' do
-  version '0.99.4.1'
-  sha256 'da875b367cb4a9808fcaf906d3c39321c3715b302c5c2c438571698ed1d66238'
+  version '0.99.4.2'
+  sha256 '24a83440e7f71fa52d548d45195f2fa060d754c624bfff518dbd595ac7868de4'
 
   # github.com/ephtracy/ephtracy.github.io was verified as official when first introduced to the cask
   url "https://github.com/ephtracy/ephtracy.github.io/releases/download/#{version.major_minor_patch}/MagicaVoxel-#{version}-alpha-macos.zip"
