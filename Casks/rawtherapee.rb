8b6a58df21ac6e56afcdce35c57f5158f600518c
Markus Reiter <me@reitermark.us>
2019-09-16 22:59:35 +0200

    Update rawtherapee.rb

--- Casks/rawtherapee.rb
+++ Casks/rawtherapee.rb
@@ -2,7 +2,6 @@ cask 'rawtherapee' do
   version '5.7'
   sha256 '70f7b7fec75f2799d80b2f960cf98815d5a5b199342d993a6019eeb153332c3d'
 
-  # github.com/Beep6581/RawTherapee was verified as official when first introduced to the cask
   url "https://rawtherapee.com/releases_head/mac/RawTherapee_OSX_10.9_64_#{version}.zip"
   appcast 'https://github.com/Beep6581/RawTherapee/releases.atom'
   name 'RawTherapee'
