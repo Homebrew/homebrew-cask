1962cfbfa3c025c8c4fe98144487e5744ae00ffe
Vítor Galvão <info@vitorgalvao.com>
2019-09-16 18:22:19 +0100

    Update devonthink.rb

--- Casks/devonthink.rb
+++ Casks/devonthink.rb
@@ -3,12 +3,12 @@ cask 'devonthink' do
   sha256 'b525e33a3352dae6e9a4e8b1b32588752906c6a915fe528f240cb0b1d1e2d3fa'
 
   # s3.amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_3.dmg.zip"
+  url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_#{version.major}.dmg.zip"
   appcast 'https://api.devontechnologies.com/1/apps/sparkle/sparkle.php?id=217255'
-  name 'DEVONthink 3'
+  name 'DEVONthink'
   homepage 'https://www.devontechnologies.com/apps/devonthink/'
 
   auto_updates true
 
-  app 'DEVONthink 3.app'
+  app "DEVONthink #{version.major}.app"
 end
