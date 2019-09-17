aa23cf48d551a8c69e5a19060be47c708289bc1d
Randall <ran.dall@icloud.com>
2019-09-16 22:42:11 -0700

    Update drivedx from 1.8.2 to 1.8.3

--- Casks/drivedx.rb
+++ Casks/drivedx.rb
@@ -1,6 +1,6 @@
 cask 'drivedx' do
-  version '1.8.2'
-  sha256 'a9d0ac0f1fd9098eb6eb64c53fd4077cece81658e27f8a992b4a3966edb8d993'
+  version '1.8.3'
+  sha256 'fd11eac52d90c121daf17852e0c2604c60a03f3f7c7ee6752e623286b79f1bb0'
 
   url "https://binaryfruit.com/download/drivedx/mac/#{version.major}/bin/DriveDx.#{version}.zip"
   appcast "https://binaryfruit.com/download/drivedx/mac/#{version.major}/updates/?appcast&appName=DriveDxMac"
