e3f7781020091220c8d3af2e3f686476948b147a
Randall <ran.dall@icloud.com>
2019-09-16 22:39:11 -0700

    Update loopback from 2.0.1 to 2.1.0

--- Casks/loopback.rb
+++ Casks/loopback.rb
@@ -1,6 +1,6 @@
 cask 'loopback' do
-  version '2.0.1'
-  sha256 '6b53635c7cd6e06ae280b8b7164196f0147b5d10dc7a622f7d007b7902974d15'
+  version '2.1.0'
+  sha256 'fab2afad94d99e4a0471a38dad0106e339e1f7d7a84f99aa4424a5942870c41f'
 
   # d2oxtzozd38ts8.cloudfront.net/loopback was verified as official when first introduced to the cask
   url 'https://d2oxtzozd38ts8.cloudfront.net/loopback/download/Loopback.zip'
