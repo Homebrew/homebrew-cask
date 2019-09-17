d76c9e48aecba22427cf636a655a1c75761b9d24
suschizu <brigitte@corecode.io>
2019-09-17 15:06:31 +0200

    Update wpsoffice from 1.5.1(2195) to 1.5.2(2273) (#69233)

--- Casks/wpsoffice.rb
+++ Casks/wpsoffice.rb
@@ -1,6 +1,6 @@
 cask 'wpsoffice' do
-  version '1.5.1(2195)'
-  sha256 '98c9c9b95371361dcdecf98c06dc41582ad4694219ee3543fd4f571cd2112b47'
+  version '1.5.2(2273)'
+  sha256 'afe7a31ab656e2ba50c57b3a6ca9f41d5ba6e84301a5bd4eec8f0fb84099788b'
 
   # package.mac.wpscdn.cn was verified as official when first introduced to the cask
   url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.major_minor_patch}/WPS_Office_#{version}.dmg"
