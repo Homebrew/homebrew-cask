104b87ebc8310e734f65fe972dfd253ec9b8ee81
Randall <ran.dall@icloud.com>
2019-09-17 12:19:36 -0700

    Update drama from 1.0.26 to 1.0.27

--- Casks/drama.rb
+++ Casks/drama.rb
@@ -1,6 +1,6 @@
 cask 'drama' do
-  version '1.0.26'
-  sha256 '21239d69d7ac6db38f26ac2e7f94bc4d474101719284c7e326cdc36bf8a5567d'
+  version '1.0.27'
+  sha256 '16ca9929a6e97ac1a1500d1ad2cd6d5d79f7e53bb51916e9d3ac13f2ad446637'
 
   # pixelcut.com/drama was verified as official when first introduced to the cask
   url 'https://www.pixelcut.com/drama/drama.zip'
