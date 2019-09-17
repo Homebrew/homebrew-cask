312beaa29532b67518ad7f4634aed2998f2f2812
Randall <ran.dall@icloud.com>
2019-09-16 22:40:35 -0700

    Update jollysfastvnc from 1.56 to 1.58

--- Casks/jollysfastvnc.rb
+++ Casks/jollysfastvnc.rb
@@ -1,6 +1,6 @@
 cask 'jollysfastvnc' do
-  version '1.56'
-  sha256 '4ff7a8d187316ee36188de89c7198d942fafe6a1d8f88b6452b331d494c6a190'
+  version '1.58'
+  sha256 '6271124c7b127069a5892075fda914c86e6d82030ce6b5db766d9b5c83b70d9d'
 
   url 'https://www.jinx.de/JollysFastVNC_files/JollysFastVNC.current.dmg'
   appcast 'https://www.jinx.de/JollysFastVNC.update.12.x86_64.xml'
