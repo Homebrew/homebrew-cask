031fdf1ef458e64ab748335cf31d0d5a8d91f603
Noah <2554594+nhmchls@users.noreply.github.com>
2019-09-17 13:42:47 -0400

    Update clone-hero from 0.22.4 to 0.22.5 (#69236)

--- Casks/clone-hero.rb
+++ Casks/clone-hero.rb
@@ -1,6 +1,6 @@
 cask 'clone-hero' do
-  version '0.22.4'
-  sha256 'e7aae8ad2b62f47d02ef92fb94d8064980e88e77b9061f58a29acb23b2899438'
+  version '0.22.5'
+  sha256 '2c2c4eaee17ff574e2b0f5349c34465950977fadada39ac378d370da6fb5b754'
 
   url "http://dl.clonehero.net/clonehero-v.#{version.minor_patch}/clonehero-mac.dmg"
   name 'Clone Hero'
