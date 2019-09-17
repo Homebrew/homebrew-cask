6e759df704ec7792e16c0450a617ee8e7a2e0f5d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-17 13:21:26 -0700

    Update runway from 0.9.6 to 0.9.7 (#69258)

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -1,6 +1,6 @@
 cask 'runway' do
-  version '0.9.6'
-  sha256 '8fe3f22e0119f99b991e742148d7b3efdd2e4b0dfd0c0aaf722d6e06544e9970'
+  version '0.9.7'
+  sha256 'c2dd333e0e0189e7f8989736556c76e725eae0521dfc0809af012f35cd8c6560'
 
   # runway-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg"
