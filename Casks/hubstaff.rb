4ccdd3fc5d428ebe388ca4e543a911eece29cffe
Randall <ran.dall@icloud.com>
2019-09-17 17:31:44 -0700

    Correct WIndows/macOS `version`

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,6 +1,6 @@
 cask 'hubstaff' do
-  version '1.4.11,1300'
-  sha256 '354f2fce95040c7397860d387e1ee244e51c246869bc83265992e63ab414394e'
+  version '1.4.11,1268'
+  sha256 '339096d23fe0a55efdf407eec717af8ecc583ef2f751b6761e682bcdbaf9c289'
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
   appcast 'https://app.hubstaff.com/appcast.xml'
