372f04ac26e52995566ce22fd64a159cbc43a9a8
Randall <ran.dall@icloud.com>
2019-09-18 14:41:23 -0700

    Update tableau-reader from 2019.2.3 to 2019.3.0

--- Casks/tableau-reader.rb
+++ Casks/tableau-reader.rb
@@ -1,6 +1,6 @@
 cask 'tableau-reader' do
-  version '2019.2.3'
-  sha256 '8baf88d7438402eecccbe9699b4e161614529a60a5689fd3864a00cad68e09d1'
+  version '2019.3.0'
+  sha256 '23318c1c5463a33c8dab41fe173ed211eb0f3eb3d2feb80bdbc8ce0adfd00b62'
 
   url "https://downloads.tableau.com/tssoftware/TableauReader-#{version.dots_to_hyphens}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/reader/mac',
