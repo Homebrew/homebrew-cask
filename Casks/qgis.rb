b831b1c71583c31109e1d06fec763c763330e7f4
Romain Studer <romain.studer@benevolent.ai>
2019-09-18 22:46:42 +0100

    Add QGIS 3.8.3

---
+++ Casks/qgis.rb
@@ -0,0 +1,10 @@
+cask 'qgis' do
+  version '3.8.3'
+  sha256 'd2d421524233ef86e43981ea67357302f4c55df7acda5d6f6f01a89f99330fb8'
+
+  url 'https://qgis.org/downloads/macos/qgis-macos-pr.dmg'
+  name 'QGIS'
+  homepage 'https://www.qgis.org/'
+
+  app "QGIS#{version.major_minor}.app"
+end
