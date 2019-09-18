dfde126dbc4a90e673dda21882f138acc39d377b
Tomoki Yamashita <tomorrowkey@gmail.com>
2019-09-18 10:14:26 +0900

    Update jasper

--- Casks/jasper.rb
+++ Casks/jasper.rb
@@ -1,6 +1,6 @@
 cask 'jasper' do
-  version '0.7.1'
-  sha256 '917169ba9ac331faa8116ddf2c8ca6323daa7cd5d2c9aec8e4568a83155de679'
+  version '0.8.0'
+  sha256 '4c3c49b4b1b36f59c76fa810400e9f87b783a7d6eefe84cc4f20c9f3e2601110'
 
   # github.com/jasperapp/jasper was verified as official when first introduced to the cask
   url "https://github.com/jasperapp/jasper/releases/download/v#{version}/jasper_v#{version}_mac.zip"
