c8fc292124be9acd6ab3583c318e38c495ccd7dc
Vítor Galvão <info@vitorgalvao.com>
2019-09-18 14:37:35 +0100

    Update nosql-workbench-for-amazon-dynamodb.rb

--- Casks/nosql-workbench-for-amazon-dynamodb.rb
+++ Casks/nosql-workbench-for-amazon-dynamodb.rb
@@ -4,6 +4,7 @@ cask 'nosql-workbench-for-amazon-dynamodb' do
 
   # nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com/NoSQL+Workbench+for+Amazon+DynamoDB+(Preview)-mac-#{version}.dmg"
+  appcast 'https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/workbench.settingup.partial.html'
   name 'NoSQL Workbench for Amazon DynamoDB'
   homepage 'https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/workbench.html'
 
