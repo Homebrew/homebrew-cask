ebc017e3a2794d65313a501fb3f7ee7658f242e1
Randall <ran.dall@icloud.com>
2019-09-18 11:19:06 -0700

    Update rotato from 73.1568717846 to 74.1568818382

--- Casks/rotato.rb
+++ Casks/rotato.rb
@@ -1,6 +1,6 @@
 cask 'rotato' do
-  version '73.1568717846'
-  sha256 'c74b75f7d07fe9da9da7c9d37db5491269d6f50378785cf7773ab1417319c1a8'
+  version '74.1568818382'
+  sha256 'af400b873724f40c5727f768de6c7889c19329b82beaed6e3f8812958ac93090'
 
   # dl.devmate.com/com.mortenjust.Rendermock was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.mortenjust.Rendermock/#{version.major}/#{version.minor}/DesignCamera-#{version.major}.zip"
