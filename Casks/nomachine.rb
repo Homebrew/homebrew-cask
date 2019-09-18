68ccaab11cb3b115586d620e10a01b0d7a921fc2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-18 06:42:30 -0700

    Update nomachine from 6.7.6_6 to 6.8.1_1 (#69259)
    * Update nomachine from 6.7.6_6 to 6.8.1_1
    
    * Update `uninstall lauchctl` stanza in nomachine.rb
    
    * Fix Typos
    
    * Update `uninstall` staza
    
    * Fix Typos 2
    
    * Update nomachine.rb
    
    * Update nomachine.rb

--- Casks/nomachine.rb
+++ Casks/nomachine.rb
@@ -1,6 +1,6 @@
 cask 'nomachine' do
-  version '6.7.6_6'
-  sha256 '908ca2c602afcf17d3c0227bf711b2745946dba4d490be51aa95875529dd991d'
+  version '6.8.1_1'
+  sha256 '82ac992238afa52ec12182298bc3ac8e17cb5e0eddff6ed75d953cf9ea5df02b'
 
   url "https://download.nomachine.com/download/#{version.major_minor}/MacOSX/nomachine_#{version}.dmg"
   appcast 'https://www.nomachine.com/download/download&id=7'
@@ -12,5 +12,11 @@ cask 'nomachine' do
   # A launchctl job ordinarily manages uninstall once the app bundle is removed
   # To ensure it ran, verify if /Library/Application Support/NoMachine/nxuninstall.sh no longer exists
   uninstall delete:    '/Applications/NoMachine.app',
-            launchctl: 'com.nomachine.localnxserver'
+            pkgutil:   'com.nomachine.nomachine.NoMachine-*.pkg',
+            launchctl: [
+                         'com.nomachine.localnxserver',
+                         'com.nomachine.nxserver',
+                         'com.nomachine.server',
+                         'com.nomachine.uninstall',
+                       ]
 end
