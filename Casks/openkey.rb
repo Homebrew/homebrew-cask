f29463d0e02ffefcbaed2b06ff1acd144406e8ee
Tuyen Mai <maivutuyen.91@gmail.com>
2019-09-19 15:34:16 +0700

    Update OpenKey from 1.5 to 1.5.1 (#69351)

--- Casks/openkey.rb
+++ Casks/openkey.rb
@@ -1,6 +1,6 @@
 cask 'openkey' do
-  version '1.5'
-  sha256 '375443d3990fa2bb11f433fb3d13e02c108c4e51398c391b5e505b9306f12a7e'
+  version '1.5.1'
+  sha256 'e9731f72f20af296a6575e390a31cd1292b5bc537af2fc0b14b4b1f9d8c529f1'
 
   url "https://github.com/tuyenvm/OpenKey/releases/download/#{version}/OpenKey#{version}.dmg"
   appcast 'https://github.com/tuyenvm/OpenKey/releases.atom'
