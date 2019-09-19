59e8f70d4b018ab6277bf8101bed41400590006b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-19 11:19:43 -0700

    Update jumpcut from 0.70 to 0.71 (#69372)

--- Casks/jumpcut.rb
+++ Casks/jumpcut.rb
@@ -1,6 +1,6 @@
 cask 'jumpcut' do
-  version '0.70'
-  sha256 '976448c206dfe844b7e444482df6ea977b5456d6f92dc3cb9b30133dc4337432'
+  version '0.71'
+  sha256 '7446e74f195390e74336cd3e3a9ea6b865f1db12e173b7b8237ae75c7568d17a'
 
   # github.com/snark/jumpcut was verified as official when first introduced to the cask
   url "https://github.com/snark/jumpcut/releases/download/v#{version}/Jumpcut-#{version}.tar.bz2"
