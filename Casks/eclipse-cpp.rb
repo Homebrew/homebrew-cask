61604dd681de45c66134d909e704d965cf0b3a94
Florian Floimair <f.floimair@commend.com>
2019-09-19 14:33:50 +0200

    Update eclipse-cpp from 4.12.0,2019-06:R to 4.13.0,2019-09:R

--- Casks/eclipse-cpp.rb
+++ Casks/eclipse-cpp.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-cpp' do
-  version '4.12.0,2019-06:R'
-  sha256 'f37d1a77ceb538f1a3a766b23b19e6e77814c008735b29b354f206ea20baa040'
+  version '4.13.0,2019-09:R'
+  sha256 '1da6f20a2b1b3592db44c7a03399c1c3751e012f92afdfa1419312868c984247'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-cpp-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for C/C++ Developers'
