9fa42a5d7cd4a11a2ff60b67683ab005285149b4
Florian Floimair <ffloimair@users.noreply.github.com>
2019-09-19 15:04:13 +0200

    Update eclipse-javascript from 4.12.0,2019-06:R to 4.13.0,2019-09:R (#69359)

--- Casks/eclipse-javascript.rb
+++ Casks/eclipse-javascript.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-javascript' do
-  version '4.12.0,2019-06:R'
-  sha256 'f33dd60bdb656b77c8e1bc521d85a70c9d61f01a6a1a655d36497e4398a60d2d'
+  version '4.13.0,2019-09:R'
+  sha256 '35d650642a557b740624409e7952ff9a7d39c85a401669dc1119d3b797cdec67'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-javascript-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for JavaScript and Web Developers'
