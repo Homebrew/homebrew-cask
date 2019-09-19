6e79141e44e6251a55a228f7b7c5f88b35d703d7
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-19 14:06:10 -0700

    Update mweb from 3.2.7 to 3.2.8 (#69390)

--- Casks/mweb.rb
+++ Casks/mweb.rb
@@ -1,6 +1,6 @@
 cask 'mweb' do
-  version '3.2.7'
-  sha256 '77f5f76b364a33c1668c5a83586c2d94ca97ac4ad2f29a104c89b51fecc05d4b'
+  version '3.2.8'
+  sha256 '85b68d521d0ec7ac76315acc92027233ec086422f57ee06aa9b78fa7c6ce5af0'
 
   # dl.devmate.com/com.coderforart.MWeb3 was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coderforart.MWeb3/MWeb3-ProMarkdownwriting,notetakingandstaticbloggeneratorApp.dmg'
