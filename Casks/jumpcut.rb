db33351aa72119cd19bc910e0e6aa5bcd857565b
Mike Funk <mike@mikefunk.com>
2019-09-19 18:50:00 -0700

    Update jumpcut (#69411)

--- Casks/jumpcut.rb
+++ Casks/jumpcut.rb
@@ -1,6 +1,6 @@
 cask 'jumpcut' do
   version '0.71'
-  sha256 '7446e74f195390e74336cd3e3a9ea6b865f1db12e173b7b8237ae75c7568d17a'
+  sha256 '8448afa7a4f07163ecf9c0aa3d1eef3670b2bb46253e67adb847e391289fd9e7'
 
   # github.com/snark/jumpcut was verified as official when first introduced to the cask
   url "https://github.com/snark/jumpcut/releases/download/v#{version}/Jumpcut-#{version}.tar.bz2"
