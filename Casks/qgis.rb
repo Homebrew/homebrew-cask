cef91fd70296ceb921f39b26120f1e2002cdd351
Vítor Galvão <info@vitorgalvao.com>
2019-09-20 14:49:50 +0100

    Update qgis.rb

--- Casks/qgis.rb
+++ Casks/qgis.rb
@@ -3,6 +3,7 @@ cask 'qgis' do
   sha256 'd2d421524233ef86e43981ea67357302f4c55df7acda5d6f6f01a89f99330fb8'
 
   url 'https://qgis.org/downloads/macos/qgis-macos-pr.dmg'
+  appcast 'https://www.qgis.org/'
   name 'QGIS'
   homepage 'https://www.qgis.org/'
 
