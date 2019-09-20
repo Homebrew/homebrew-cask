e048381f67c99772bd0c62dc71b968bad5340605
404NetworkError <404NetworkError@users.noreply.github.com>
2019-09-20 09:44:10 -0400

    Update menumeters from 1.9.7bis to 1.9.8

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,6 +1,6 @@
 cask 'menumeters' do
-  version '1.9.7bis'
-  sha256 '6cf2f7b312c1c13f74e9d56082f48b50b7ca5909ee3589df916a65818ac390d9'
+  version '1.9.8'
+  sha256 '7e034975be0d7eac033f0d4fcaf8725d435280411358d50a9828ce62f8bc1cca'
 
   # github.com/yujitach/MenuMeters was verified as official when first introduced to the cask
   url "https://github.com/yujitach/MenuMeters/releases/download/#{version}/MenuMeters_#{version.major_minor_patch}.zip"
