c1ae71a34f8d7c8e2c75859a4e791488ba72e5fe
Randall <ran.dall@icloud.com>
2019-09-20 08:38:45 -0700

    Update keeweb from 1.10.0 to 1.10.1

--- Casks/keeweb.rb
+++ Casks/keeweb.rb
@@ -1,6 +1,6 @@
 cask 'keeweb' do
-  version '1.10.0'
-  sha256 '66fa58c000f8fe5e3167505425da65986b1b8ca8ae280d849d9579012e1edae3'
+  version '1.10.1'
+  sha256 'b500e2269a2a49ae8969c6290796c3e2ddaa2429a08ce738d8fc88659e23c599'
 
   # github.com/keeweb/keeweb was verified as official when first introduced to the cask
   url "https://github.com/keeweb/keeweb/releases/download/v#{version}/KeeWeb-#{version}.mac.dmg"
