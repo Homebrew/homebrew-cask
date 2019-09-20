03548e442793247e9db38347521285b1c4192baa
Randall <ran.dall@icloud.com>
2019-09-20 08:51:52 -0700

    Update 5kplayer from 6.1.0 to 5000

--- Casks/5kplayer.rb
+++ Casks/5kplayer.rb
@@ -1,6 +1,6 @@
 cask '5kplayer' do
-  version '6.1.0'
-  sha256 '75a653e910e019a0000f7df39a1ed8d391e0fc50d9df71496b783d8fa32f05b3'
+  version '5000'
+  sha256 '2cf059635fa604345454efbaad01b1541c3d4b8e3a532ad8e07a6ed7dffa23c0'
 
   url 'https://www.5kplayer.com/download/5kplayer.dmg'
   appcast 'https://www.5kplayer.com/upgrade/mac/updatecast.xml'
