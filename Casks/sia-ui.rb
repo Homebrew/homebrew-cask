c0e273ac554671628a2223b6309cc7ac0f30b240
Ian Levesque <ian@ianlevesque.org>
2019-09-21 15:51:09 -0400

    Update sia-ui to 1.4.1.2

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -3,7 +3,7 @@ cask 'sia-ui' do
   sha256 'ebd6d89a1012098a26f3da4338ae37a5b2194891f8176eaa1cb3cc5b678410e1'
 
   url "https://sia.tech/releases/Sia-UI-v#{version}.dmg"
-  appcast 'https://gitlab.com/NebulousLabs/Sia/-/releases'
+  appcast 'https://gitlab.com/api/v4/projects/7508674/releases'
   name 'Sia-UI'
   homepage 'https://sia.tech/'
 
