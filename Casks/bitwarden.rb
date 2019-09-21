9df6cb7ab0554746223bfd98c6b8ca422b624742
Xo Wang <xo@geekshavefeelings.com>
2019-09-20 19:54:26 -0700

    Update bitwarden from 1.15.2 to 1.16.2

--- Casks/bitwarden.rb
+++ Casks/bitwarden.rb
@@ -1,6 +1,6 @@
 cask 'bitwarden' do
-  version '1.15.2'
-  sha256 '9e14f3d6f5e5f72d963aa8edd7120066d2b51da3d84802bf5fb0191d6139fa51'
+  version '1.16.2'
+  sha256 '519d5f8effe516cfd2c549a2d048865d0074664419d7c91b73277b2e4e47877e'
 
   # github.com/bitwarden/desktop was verified as official when first introduced to the cask
   url "https://github.com/bitwarden/desktop/releases/download/v#{version}/Bitwarden-#{version}-mac.zip"
