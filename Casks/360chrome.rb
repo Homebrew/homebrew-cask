1b2bc744ae721671e2f001605556ae8337ab6ef4
AndyDufrense <42636716+AndyDufrense@users.noreply.github.com>
2019-09-22 02:53:15 +0800

    Add files via upload

---
+++ Casks/360chrome.rb
@@ -0,0 +1,10 @@
+cask '360chrome' do
+  version '1.0.1505.0'
+  sha256 'cbd626aa931d555b54ce469341cb61dc493870982dedcfb397b479ccdcbb934a'
+
+  url 'https://down.360safe.com/cse/360Browser_for_mac_1.0.1505.0.dmg'
+  name '360chrome'
+  homepage 'https://down.360safe.com/'
+
+  app '360chrome.app'
+end
