74ed6a1786b104e39ce60fa249beff908033b4d7
CoreCode <core-code@users.noreply.github.com>
2019-09-22 00:45:07 +0200

    Update keepassxc.rb

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -8,6 +8,8 @@ cask 'keepassxc' do
   name 'KeePassXC'
   homepage 'https://keepassxc.org/'
 
+  depends_on macos: '>= :sierra'
+
   app 'KeePassXC.app'
   binary "#{appdir}/KeePassXC.app/Contents/MacOS/keepassxc-cli"
 
