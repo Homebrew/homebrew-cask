69939817af2f9a1e2ed2c91fbc6f417965332acd
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-22 12:51:13 -0700

    Update idagio from 0.0.100 to 0.0.101 (#69519)

--- Casks/idagio.rb
+++ Casks/idagio.rb
@@ -1,6 +1,6 @@
 cask 'idagio' do
-  version '0.0.100'
-  sha256 'd17195eaa113c675e24645d81d85c874d811d2db13d07178581ceeae811112f1'
+  version '0.0.101'
+  sha256 'a816a3675562eb7a4a6cfa62b393299e8b513c48a39c3250f2598bb3258e38aa'
 
   url "https://dl.idagio.com/IDAGIO-#{version}.dmg"
   name 'IDAGIO'
