b663a0c4fea212527ba8dd4820c517099f09c589
Randall <ran.dall@icloud.com>
2019-09-22 12:44:41 -0700

    Update cacher from 2.19.0 to 2.19.1

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,6 +1,6 @@
 cask 'cacher' do
-  version '2.19.0'
-  sha256 '21e9f452eddd8490e880ba2051f23557928c65a2a0bf1df742bfa8f372422b23'
+  version '2.19.1'
+  sha256 '5cb33f35c26303cf328d770c924e77e84f0797e690685c099548b830bfa2b908'
 
   # cacher-download.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
