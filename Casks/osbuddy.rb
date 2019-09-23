5397ca5abff9aed3a78c2c459d65e91eafd2e9af
Vítor Galvão <info@vitorgalvao.com>
2019-09-23 22:05:16 +0100

    Update osbuddy.rb

--- Casks/osbuddy.rb
+++ Casks/osbuddy.rb
@@ -2,7 +2,7 @@ cask 'osbuddy' do
   version :latest
   sha256 :no_check
 
-  url 'http://cdn.rsbuddy.com/OSBuddy.dmg?x=11'
+  url 'http://cdn.rsbuddy.com/OSBuddy.dmg'
   name 'OSBuddy'
   homepage 'https://rsbuddy.com/osbuddy'
 
