8d149be900d4e3696e6eee02b130ed59cd3bc9ec
Julian Mayer <julian@corecode.io>
2019-09-23 18:27:01 +0200

    Update mylio

--- Casks/mylio.rb
+++ Casks/mylio.rb
@@ -3,7 +3,7 @@ cask 'mylio' do
   sha256 :no_check
 
   # myliodownloads.s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://myliodownloads.s3.amazonaws.com/mylio.dmg'
+  url 'https://myliodownloads.s3.amazonaws.com/Mylio.dmg'
   name 'Mylio'
   homepage 'https://mylio.com/'
 
