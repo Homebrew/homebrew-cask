f405f76c5e2bb062c83c3e5eb208e5f7b03455a9
Vítor Galvão <info@vitorgalvao.com>
2019-09-23 16:29:28 +0100

    Update clix.rb

--- Casks/clix.rb
+++ Casks/clix.rb
@@ -6,13 +6,13 @@ cask 'clix' do
   name 'CLIX'
   homepage 'https://rixstep.com/4/0/clix/'
 
-  app "CLIX/CLIX.app"
+  app 'CLIX/CLIX.app'
 
   preflight do
-    set_permissions "#{staged_path}/CLIX/CLIX.app", '0755'
+    set_permissions '#{staged_path}/CLIX/CLIX.app', '0755'
   end
 
   postflight do
-    set_permissions "#{appdir}/CLIX/CLIX.app", '0700'
+    set_permissions '#{appdir}/CLIX/CLIX.app', '0700'
   end
 end
