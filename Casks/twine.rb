5fce4ddcefd1bcd18d1f4bdafa20aae6e670eaf0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-23 14:07:46 -0700

    Update twine from 2.3.3 to 2.3.4 (#69572)

--- Casks/twine.rb
+++ Casks/twine.rb
@@ -1,6 +1,6 @@
 cask 'twine' do
-  version '2.3.3'
-  sha256 'd8f397a843638bc19347a44f018901b9dca1705e1f6d58e4f484a7c01d38864e'
+  version '2.3.4'
+  sha256 'edf92af402e4ddb88dac835f2342d99174b3c66b4102d59e44452bdfadda0911'
 
   # github.com/klembot/twinejs was verified as official when first introduced to the cask
   url "https://github.com/klembot/twinejs/releases/download/#{version}/twine_#{version}_macos.dmg"
