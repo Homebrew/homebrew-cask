6622572efd370dd7dfe3a9e14cac6d55a9096527
王佳 <wangjia_1919@163.com>
2019-09-23 18:49:40 +0800

    add qqplayer 1.0.4.278

--- Casks/qqplayer.rb
+++ Casks/qqplayer.rb
@@ -1,21 +1,17 @@
 cask 'qqplayer' do
   version '1.0.4.278'
-
   sha256 'a60a9115dcbe197ce62a3e9344408b0d1b4a4494b2c9f65e2c44eef5ae9ef7d2'
 
   url "https://dldir1.qq.com/qqyy/mac/QQPlayer#{version}.dmg"
-
   appcast 'https://player.qq.com/'
-
   name 'QQ影音'
-
   homepage 'https://player.qq.com/'
 
   app 'QQPlayerMac.app'
 
   uninstall quit: 'com.tencent.qqplayermac'
 
-  zap trash [
+  zap trash: [
                '~/Library/Application Support/com.tencent.qqplayermac',
                '~/Library/Caches/com.tencent.qqplayermac',
                '~/Library/Preferences/com.tencent.qqplayermac.plist',
