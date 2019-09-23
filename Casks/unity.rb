a4a949d025773a7b9dbeb44ad5ddc25137a98b72
suschizu <brigitte@corecode.io>
2019-09-23 17:31:50 +0200

    Update unity from 2019.2.5f1,9dace1eed4cc to 2019.2.6f1,fe82a0e88406 (#69531)

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask 'unity' do
-  version '2019.2.5f1,9dace1eed4cc'
-  sha256 '28a791b6778af8708fb6396af52e81606241be05b10aeb285e56bf08e9756ef2'
+  version '2019.2.6f1,fe82a0e88406'
+  sha256 '96ec98f235ea32e73141b56c2dfe6d4f43551cd4657d55b019019c89a89637a9'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/download/archive'
