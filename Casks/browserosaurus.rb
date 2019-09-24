dac4fd4fc8e37394846946471c4051490055f819
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-24 14:22:02 -0700

    Update browserosaurus from 6.3.0 to 6.4.0 (#69622)

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,6 +1,6 @@
 cask 'browserosaurus' do
-  version '6.3.0'
-  sha256 '747211d56c611e4b3abb213c9c522550b3e086a23eab40f7bcb4adc0e3b75c91'
+  version '6.4.0'
+  sha256 '66083e899b0ed93581fb2be3766c7ad85308eebf3cb2e24b889c62db5f5d6247'
 
   # github.com/will-stone/browserosaurus/ was verified as official when first introduced to the cask
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg"
