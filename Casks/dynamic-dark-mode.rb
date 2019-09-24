48beddfcf74ebfb1ebcb736f497cc9475b1ec898
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-24 13:36:05 -0700

    Update dynamic-dark-mode from 1.5.1 to 1.5.2 (#69620)

--- Casks/dynamic-dark-mode.rb
+++ Casks/dynamic-dark-mode.rb
@@ -1,6 +1,6 @@
 cask 'dynamic-dark-mode' do
-  version '1.5.1'
-  sha256 '682da4fad8b3ed527b8efe281e1f7ab4f2c99307feabcfd7e125940a70a0f80a'
+  version '1.5.2'
+  sha256 '696d5d605b3c2b54d2485936eff59cd150116f82f584b76938bf80f252d8f194'
 
   url "https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases/download/#{version}/Dynamic_Dark_Mode-#{version}.zip"
   appcast 'https://github.com/ApolloZhu/Dynamic-Dark-Mode/releases.atom'
