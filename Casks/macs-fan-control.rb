4f5eb6679ffedafaaf6801c4769e1e6c7b8bf58b
suschizu <brigitte@corecode.io>
2019-09-25 00:12:50 +0200

    Update macs-fan-control.rb

--- Casks/macs-fan-control.rb
+++ Casks/macs-fan-control.rb
@@ -1,6 +1,6 @@
 cask 'macs-fan-control' do
-  version '1.5.0.1'
-  sha256 'd86c46e6211cb5739517a9e2333ad37b2e680673a688e332579398e00cbe6999'
+  version '1.5'
+  sha256 '3ce0d04a629fa4ccd2e8a7141f3f7a566d8907d59a197234bd1264b756564465'
 
   # github.com/crystalidea/macs-fan-control was verified as official when first introduced to the cask
   url "https://github.com/crystalidea/macs-fan-control/releases/download/v#{version}/macsfancontrol.zip"
