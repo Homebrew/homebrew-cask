0d9a369e35f57fc4ad3436d89ebbbe439fbfde6d
Qi Zhang <singularitti@outlook.com>
2019-09-24 03:55:07 -0400

    Add new cask 'crystalmaker'

---
+++ Casks/crystalmaker.rb
@@ -0,0 +1,10 @@
+cask 'crystalmaker' do
+  version '10.4.5'
+  sha256 'd51f00f0f0bc4579b90f04098221dbc4b0d0b2e714ef3ee6baefe8baf8873299'
+
+  url 'http://crystalmaker.com/downloads/crystalmaker_mac.zip'
+  name 'CrystalMaker'
+  homepage 'http://crystalmaker.com/index.html'
+
+  app 'CrystalMaker.app'
+end
