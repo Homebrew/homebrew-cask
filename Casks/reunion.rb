389b2cda79a2f152e0edf29e2c16879e7c96eca7
Viktor Szakats <commit@vszakats.net>
2019-09-24 23:55:32 +0000

    reunion 12.0.0.190924

--- Casks/reunion.rb
+++ Casks/reunion.rb
@@ -1,11 +1,11 @@
 cask 'reunion' do
-  version '12.0.0.181218'
-  sha256 '6c4946169eb2f57d3726615265b29a6483ddfbccf19e610f832c5fadba8fd1e6'
+  version '12.0.0.190924'
+  sha256 '729de26b38a0f97990378795a4f372f70880dafa4864da91d7c7e018b88497bf'
 
   url "https://store.leisterpro.com/updates/reunion#{version.major}/Reunion-#{version.dots_to_hyphens}.zip"
   appcast "https://store.leisterpro.com/updates/reunion#{version.major}/appcast.xml"
   name 'Reunion'
-  homepage 'http://www.leisterpro.com/'
+  homepage 'https://www.leisterpro.com/'
 
   app "Reunion #{version.major}.app"
 end
