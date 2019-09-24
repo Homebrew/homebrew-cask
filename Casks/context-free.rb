ad987f51a553658e8f8c5aaecd8a3c0e0e4fb039
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-24 14:37:26 -0700

    Update context-free from 3.1 to 3.2 (#69638)

--- Casks/context-free.rb
+++ Casks/context-free.rb
@@ -1,6 +1,6 @@
 cask 'context-free' do
-  version '3.1'
-  sha256 '57e93777b37f14ad697964fd435a638ef17216503011ebf87b861958cd16ccc3'
+  version '3.2'
+  sha256 '506fc01ac271ee0e4157605201578aedb1ea81e82f1bd9c7fa240ca60cb1bee8'
 
   url "https://www.contextfreeart.org/download/ContextFree#{version}.dmg"
   appcast 'https://github.com/MtnViewJohn/context-free/releases.atom'
