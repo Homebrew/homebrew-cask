116a7da946c2c577e464b2447991657b6af5702f
Vítor Galvão <info@vitorgalvao.com>
2019-09-24 13:16:30 +0100

    Update crystalmaker.rb

--- Casks/crystalmaker.rb
+++ Casks/crystalmaker.rb
@@ -3,6 +3,7 @@ cask 'crystalmaker' do
   sha256 'd51f00f0f0bc4579b90f04098221dbc4b0d0b2e714ef3ee6baefe8baf8873299'
 
   url 'http://crystalmaker.com/downloads/crystalmaker_mac.zip'
+  appcast "http://crystalmaker.com/crystalmaker/release-notes/mac/#{version.major}/index.html"
   name 'CrystalMaker'
   homepage 'http://crystalmaker.com/index.html'
 
