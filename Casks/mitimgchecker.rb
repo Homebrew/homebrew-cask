f7c210e489c91456869625d440930a5716006668
mcmengchen <416922992@qq.com>
2019-09-25 16:53:19 +0800

    Add mitimgchecker v1.0.0

--- Casks/mitimgchecker.rb
+++ Casks/mitimgchecker.rb
@@ -1,9 +1,13 @@
 cask 'mitimgchecker' do
   version '1.0.0'
   sha256 '2b3056ff72aede7f90834f39bd72a0b2325fff0d490fb9e4fd1cfcd69cd0c74e'
-  appcast 'https://github.com/mitchell-dream/MitImgChecker/releases.atom'
-  url "https://github.com/mitchell-dream/MitImgChecker/releases/download/1.0.0/MitImgChecker.dmg"
+
+  url "https://github.com/mitchell-dream/MitImgChecker/releases/download/v#{version}/MitImgChecker.dmg"
+  appcast 'https://github.com/ethereum/mist/releases.atom'
   name 'MitImgChecker'
   homepage 'https://github.com/mitchell-dream/MitImgChecker'
+
+  depends_on macos: '>= :high_sierra'
+
   app 'MitImgChecker.app'
 end
