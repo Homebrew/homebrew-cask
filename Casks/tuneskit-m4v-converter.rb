5ff6d16f98b6e02c2c08a5e51ee5cc93a9100c89
Randall <ran.dall@icloud.com>
2019-09-25 15:50:03 -0700

    Update tuneskit-m4v-converter from 5.0.1 to 5.1.0

--- Casks/tuneskit-m4v-converter.rb
+++ Casks/tuneskit-m4v-converter.rb
@@ -1,6 +1,6 @@
 cask 'tuneskit-m4v-converter' do
-  version '5.0.1'
-  sha256 'f28d153a3b61bf99983765464278d1aea475165c8e40de218506e390a20257d2'
+  version '5.1.0'
+  sha256 'c5034c0b28fe117ca80468ec417b5f25147a6024a676851990b11d7042b9bd38'
 
   url 'https://www.tuneskit.com/TunesKitforMac.dmg'
   appcast 'https://www.tuneskit.com/api/product/version?pid=201'
