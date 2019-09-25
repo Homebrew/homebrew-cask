47d71fa4196e549159e265f7d8dfe8e86f0ca0ae
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-25 16:21:49 -0700

    Update yate from 5.0.0.1 to 5.0.1 (#69702)

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,6 +1,6 @@
 cask 'yate' do
-  version '5.0.0.1'
-  sha256 'f5166aae9a25a206e7748c8136aeae2817c22fc8330703d492f3f4be8ba8d02f'
+  version '5.0.1'
+  sha256 '028e17e2db2fd37059691cd051d05abf063b82937f26a2674034f399eaf19f98'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml'
