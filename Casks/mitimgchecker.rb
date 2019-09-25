9ee08fffff2b89986ce256cfe5c14d67770a1ad0
Mitchell <416922992@qq.com>
2019-09-25 17:51:48 +0800

    Update mitimgchecker.rb

--- Casks/mitimgchecker.rb
+++ Casks/mitimgchecker.rb
@@ -2,7 +2,7 @@ cask 'mitimgchecker' do
   version '1.0.0'
   sha256 '2b3056ff72aede7f90834f39bd72a0b2325fff0d490fb9e4fd1cfcd69cd0c74e'
 
-  url "https://github.com/mitchell-dream/MitImgChecker/releases/download/v#{version}/MitImgChecker.dmg"
+  url "https://github.com/mitchell-dream/MitImgChecker/releases/download/#{version}/MitImgChecker.dmg"
   appcast 'https://github.com/ethereum/mist/releases.atom'
   name 'MitImgChecker'
   homepage 'https://github.com/mitchell-dream/MitImgChecker'
