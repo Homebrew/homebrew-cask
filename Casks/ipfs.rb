341153dc8318c41b3d8f4e2d9150964a444593a3
Henrique Dias <hacdias@gmail.com>
2019-09-25 08:41:32 +0100

    Update ipfs from 0.9.2 to 0.9.3

--- Casks/ipfs.rb
+++ Casks/ipfs.rb
@@ -1,6 +1,6 @@
 cask 'ipfs' do
-  version '0.9.2'
-  sha256 'b079162f5bda4e21a1db43c1394ae1ccb22d06b06201f708e90824eaadf66f3e'
+  version '0.9.3'
+  sha256 '3f009d08ac5412fb6920d04f646708104e903c20152959f13ef3b439d3b2be59'
 
   url "https://github.com/ipfs-shipyard/ipfs-desktop/releases/download/v#{version}/ipfs-desktop-#{version}.dmg"
   appcast 'https://github.com/ipfs-shipyard/ipfs-desktop/releases.atom'
