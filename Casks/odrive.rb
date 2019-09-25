a73af42b179db1a356a24c2d0ee8345cbabf8746
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-24 17:40:30 -0700

    Update odrive from 6529 to 6535 (#69642)

--- Casks/odrive.rb
+++ Casks/odrive.rb
@@ -1,6 +1,6 @@
 cask 'odrive' do
-  version '6529'
-  sha256 'a7154bb7818cd4a4c2d848b0d611616632635587917194761d06d9430df94c14'
+  version '6535'
+  sha256 'ae8fc2a48911cdbbe604e7f2bb757e153b35ad315854edc334a847ea367ef4c7'
 
   # downloads can be found at https://www.odrive.com/downloaddesktop
   # d3huse1s6vwzq6.cloudfront.net was verified as official when first introduced to the cask
@@ -11,5 +11,6 @@ cask 'odrive' do
 
   pkg "odrive.#{version}.pkg"
 
-  uninstall pkgutil: 'com.oxygen.odrive.*'
+  uninstall quit:    'com.oxygencloud.odrive',
+            pkgutil: 'com.oxygen.odrive.*'
 end
