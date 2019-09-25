271e68683b038d1a18fa6321ff502c52298ac879
brigitte zmoelnig <brigitte@corecode.io>
2019-09-25 15:09:36 +0200

    Update flirc from 3.22.4 to 3.24.0

--- Casks/flirc.rb
+++ Casks/flirc.rb
@@ -1,8 +1,8 @@
 cask 'flirc' do
-  version '3.22.4'
+  version '3.24.0'
   sha256 '6fffbb04f84d4441caacf3aa8adefb3d7ef8bd48c922af198cb1d474016c7e20'
 
-  url 'https://flirc.tv/software/release/gui/mac/Flirc.dmg'
+  url "https://flirc.tv/software/flirc-usb/GUI/release/mac/Flirc-#{version}.dmg"
   appcast 'https://flirc.tv/software/release/gui/mac/appcast.xml'
   name 'Flirc'
   homepage 'https://flirc.tv/'
