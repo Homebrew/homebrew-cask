f8780566d72c40c983bf3091e15494e7c3d9cf8f
Dafydd <me@daf.lol>
2019-09-26 08:44:38 +0100

    Update Casks/magiccap.rb
    Co-Authored-By: Vítor Galvão <info@vitorgalvao.com>

--- Casks/magiccap.rb
+++ Casks/magiccap.rb
@@ -2,7 +2,7 @@ cask 'magiccap' do
   version '2.0.0'
   sha256 '8cebb0061062e03e54a71ff120a6c658a41bbf8952aadfd2945fd76808dc2cb6'
 
-  # github.com/magiccap was verified as official when first introduced to the cask
+  # github.com/magiccap/MagicCap was verified as official when first introduced to the cask
   url "https://github.com/magiccap/MagicCap/releases/download/v#{version}/magiccap-mac.dmg"
   appcast 'https://github.com/magiccap/MagicCap/releases.atom'
   name 'MagicCap'
