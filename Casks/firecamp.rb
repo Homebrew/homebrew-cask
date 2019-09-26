f90b3c9d1af7c886fcfa48b3a45744a996109aa9
Nishchit Dhanani <dnishchit@gmail.com>
2019-09-26 17:26:41 +0530

    Firecamp v0.6.7 cask ready

---
+++ Casks/firecamp.rb
@@ -0,0 +1,12 @@
+cask 'firecamp' do
+  version '0.6.7'
+  sha256 :no_check
+
+  # https://firecamp.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask                                
+  url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg"
+  name 'Firecamp'
+  homepage 'https://firecamp.app'
+
+  app 'Firecamp.app'
+end
+
