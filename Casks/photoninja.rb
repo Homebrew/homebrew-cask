39f6a9a03596f50ed63cdbe3d3e1dd994450a8ae
suschizu <brigitte@corecode.io>
2019-09-26 18:44:04 +0200

    Revert "Update photoninja from 1.3.8b to 1.3.8 (#69716)" (#69735)
    This reverts commit 13518133c206e7b4ab0a2c29254a19e230b43887.

--- Casks/photoninja.rb
+++ Casks/photoninja.rb
@@ -1,6 +1,6 @@
 cask 'photoninja' do
-  version '1.3.8'
-  sha256 '8e892330ec4bd9296b97e7a3068de0f6b7e7b0098d0b984d360ff4c836b50431'
+  version '1.3.8b'
+  sha256 '40709d2711ab780054efccc5550941e4f31240311ed62fdf418378eb558a48cb'
 
   # picturecode.cachefly.net was verified as official when first introduced to the cask
   url "https://picturecode.cachefly.net/photoninja/downloads/Install_PhotoNinja_#{version}.dmg"
