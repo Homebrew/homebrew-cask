55b4267bf4f4a7d6c539466fbc0fb925b45a2db2
CoreCode <core-code@users.noreply.github.com>
2019-09-26 21:16:07 +0200

    Update timeular from latest to 2.3.1 (#69611)

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -1,11 +1,14 @@
 cask 'timeular' do
-  version :latest
-  sha256 :no_check
+  version '2.3.1'
+  sha256 'e1c07868b397d1997853058aaecc6b5369b3c49cb33f03eaa06eb7104794093b'
 
   # timeular-desktop-packages.s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular.dmg'
+  url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg"
+  appcast 'https://timeular-desktop-packages.s3.amazonaws.com/mac/production/latest-mac.yml'
   name 'Timeular'
   homepage 'https://timeular.com/'
+
+  auto_updates true
 
   app 'Timeular.app'
 end
