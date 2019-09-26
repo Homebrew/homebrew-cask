ddff93952871d42afd75488482cf3d6a481f10ae
ralph <ralph@cs.cf.ac.uk>
2019-09-26 08:32:13 +0100

    Update geekbench from 5.0.1 to 5.0.2

--- Casks/geekbench.rb
+++ Casks/geekbench.rb
@@ -6,8 +6,8 @@ cask 'geekbench' do
     version '4.4.2'
     sha256 '3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc'
   else
-    version '5.0.1'
-    sha256 'e66e9944f51e2eef136ac83588e9748b31221367e92d745ecc80647f553d63e4'
+    version '5.0.2'
+    sha256 'a2ce68f3dfbffabb2f75b2c4e98437762eab069d8ab5af7b818be15207364505'
   end
 
   url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"
