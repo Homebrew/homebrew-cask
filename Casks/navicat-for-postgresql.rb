bae8c763453294620d9636cf60615a37cef2f29e
Nathaniel Hammond <nathanielhammond@gmail.com>
2019-09-26 03:08:23 +0100

    Update navicat-for-postgresql (#69664)

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-postgresql' do
-  version '12.1.25'
-  sha256 'c1167a806aea76e9b2f4ac4ecd05c00a86e7eaf3ade74dede6c2b2d676c76e1f'
+  version '12.1.24'
+  sha256 'ca1367cea7f1ff53ee90bfc341731b2e3daa9aa3d7248e3ed79c440f90f3662a'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20for%20PostgreSQL&appLang=en'
