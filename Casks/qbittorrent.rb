f3fc45c7b25d3abab1705b2d375621d716a1034e
elsorino <elso@pm.me>
2019-09-26 18:18:53 -0400

    Update qbittorrent from 4.1.7 to 4.1.8 (#69753)

--- Casks/qbittorrent.rb
+++ Casks/qbittorrent.rb
@@ -1,6 +1,6 @@
 cask 'qbittorrent' do
-  version '4.1.7'
-  sha256 '16b3cfa69fae39ef3352e5fd79f4b2306d565a3d37c6d60f25ac01fddf487ed8'
+  version '4.1.8'
+  sha256 '8f12487b2d6be51442778ae196ad3e73c6fe4474feb929b5d4b4e11443d9c108'
 
   # sourceforge.net/qbittorrent was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg"
