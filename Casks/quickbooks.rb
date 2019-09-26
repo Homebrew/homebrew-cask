a7d8c516480aaa47c29de4a73bb55533b284c7e8
suschizu <brigitte@corecode.io>
2019-09-27 00:24:41 +0200

    Update quickbooks.rb

--- Casks/quickbooks.rb
+++ Casks/quickbooks.rb
@@ -3,7 +3,7 @@ cask 'quickbooks' do
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://http-download.intuit.com/http.intuit/Downloads/#{version}/Latest/QuickBooksMac#{version}.dmg"
-  appcast 'https://http-download.intuit.com/http.intuit/Downloads/Mac/QuickBooks/018/9DBF6268-EEBF-431D-9E50-DE3FEAE52FA2/appcast.xml'
+  appcast 'https://http-download.intuit.com/http.intuit/Downloads/Mac/QuickBooks/019/8D4DADF6-42C2-4D64-ABC0-D28253C30D19/appcast.xml'
   name 'QuickBooks Desktop'
   homepage 'https://quickbooks.intuit.com/mac/'
 
