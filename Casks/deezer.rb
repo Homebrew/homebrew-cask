d289c25fd35d3518642cb3d82a2163c1224467c3
brigitte zmoelnig <brigitte@corecode.io>
2019-09-26 17:36:42 +0200

    Update deezer from 4.16.3 to 4.17.0

--- Casks/deezer.rb
+++ Casks/deezer.rb
@@ -1,6 +1,6 @@
 cask 'deezer' do
-  version '4.16.3'
-  sha256 '28ef9bd6baaba664c07fb91b91ea033af25b54dc964d827333425491ae47f882'
+  version '4.17.0'
+  sha256 '07eb8cb670c13917dc5a8057435bd7bd96acc15cd1b6533cd730471929ca3809'
 
   url "https://www.deezer.com/desktop/download/artifact/darwin/x64/#{version}"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.deezer.com/desktop/download%3Fplatform%3Ddarwin%26architecture=x64'
