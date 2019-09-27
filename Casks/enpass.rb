1bf447363847cb4b9e497958df1866e17a3bfd33
Randall <ran.dall@icloud.com>
2019-09-26 22:09:59 -0700

    Update enpass from 6.1.2,495 to 6.2.0,537

--- Casks/enpass.rb
+++ Casks/enpass.rb
@@ -1,6 +1,6 @@
 cask 'enpass' do
-  version '6.1.2,495'
-  sha256 'c02d10b4ba5a2fa38402c1a2af306837e5f55d181b76a894f8763856c0c473d8'
+  version '6.2.0,537'
+  sha256 '91859dafdf4a956a24c6f06632d88e04d344690e8cf340feedc95380763b30e1'
 
   url "https://dl.enpass.io/stable/mac/package/#{version.after_comma}/Enpass.pkg"
   appcast 'https://rest.enpass.io/enpass/alert/?format=json&language=en%7Cen&os=osx%7C10.14&package=in.sinew.Enpass-Desktop.App&version=0.0.0'
