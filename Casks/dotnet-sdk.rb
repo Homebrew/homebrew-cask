271a96f6ee59b05d0cfdc3389cb6af2e56bdff79
lou1306 <lucadistefano90@gmail.com>
2019-09-27 19:34:09 +0200

    Fix dotnet-sdk version for macOs Sierra
    Close issue #69673

--- Casks/dotnet-sdk.rb
+++ Casks/dotnet-sdk.rb
@@ -1,7 +1,12 @@
 cask 'dotnet-sdk' do
-  version '3.0.100,5c281f95-91c4-499d-baa2-31fec919047a:38c6964d72438ac30032bce516b655d9'
-  sha256 '6a5a475d9aa1955470cd2370c9f501bc6a5a05ad5fb74109ddb9da278b55101a'
-
+  if MacOS.version <= :sierra
+    version '2.2.402,7430e32b-092b-4448-add7-2dcf40a7016d:1076952734fbf775062b48344d1a1587'
+    sha256 'e74d816bc034d0fcdfa847286a6cad097227d4864da1c97fe801012af0c26341'
+  else
+    version '3.0.100,5c281f95-91c4-499d-baa2-31fec919047a:38c6964d72438ac30032bce516b655d9'
+    sha256 '6a5a475d9aa1955470cd2370c9f501bc6a5a05ad5fb74109ddb9da278b55101a'
+  end
+  
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
   name '.NET Core SDK'
