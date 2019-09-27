8e1c63c0d2fbd0ff399a6bd610187510f2db21ec
Randall <ran.dall@icloud.com>
2019-09-26 22:03:48 -0700

    Update quicken from 5.12.4,512.29280.100 to 5.12.5,512.29294.100

--- Casks/quicken.rb
+++ Casks/quicken.rb
@@ -1,6 +1,6 @@
 cask 'quicken' do
-  version '5.12.4,512.29280.100'
-  sha256 'eb7531797356bd36617b56ffb06754db5fba403d63637fb46bfda8ec83f69258'
+  version '5.12.5,512.29294.100'
+  sha256 '5ae66aaa2f835f36461250ed47267c984ee70cafd4b792e2fb5d2c7781d4fe98'
 
   url "https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/Quicken-#{version.after_comma}/Quicken-#{version.after_comma}.zip"
   appcast 'https://download.quicken.com/mac/Quicken/001/Release/031A96D9-EFE6-4520-8B6A-7F465DDAA3E4/appcast.xml'
