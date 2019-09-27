4cf22d9afc975720c486e06bbe7a7059ae91f931
chris <mail@chohner.com>
2019-09-27 20:45:00 +0200

    itsycal: update to 0.12.1 & fix 0.11.17 for high_sierra (#69743)

--- Casks/itsycal.rb
+++ Casks/itsycal.rb
@@ -2,24 +2,21 @@ cask 'itsycal' do
   if MacOS.version <= :mavericks
     version '0.8.15'
     sha256 '6470719a1f702c807f98a992880def5f499858231bf35924eaf3e0d5df48b436'
-
-    # itsycal.s3.amazonaws.com was verified as official when first introduced to the cask
-    url "https://itsycal.s3.amazonaws.com/Itsycal-#{version}.zip"
   elsif MacOS.version <= :el_capitan
     version '0.10.16'
     sha256 'dbf1b104c7a3a2ca3ead9879145cb0557955c29d53f35a92b42f48e68122957c'
-
-    # itsycal.s3.amazonaws.com was verified as official when first introduced to the cask
-    url "https://itsycal.s3.amazonaws.com/Itsycal-#{version}.zip"
+  elsif MacOS.version <= :high_sierra
+    version '0.11.17'
+    sha256 'fda1ba5611deaf4d5b834118b3af37ea9c5d08d1f8c813d04e7dd0552a270e11'
+    appcast 'https://itsycal.s3.amazonaws.com/itsycal.xml'
   else
-    version '0.12.0'
-    sha256 '1e3138394c30553a17af5a5b331052eb0af78f94d80d24175571437ebf59510c'
-
-    # itsycal.s3.amazonaws.com was verified as official when first introduced to the cask
-    url "https://itsycal.s3.amazonaws.com/Itsycal-#{version}.zip"
+    version '0.12.1'
+    sha256 'deba5db668d5c488da48e6cfdd3740b61e1b22d2ee808f5f84bc9cdb6ba55a9a'
     appcast 'https://itsycal.s3.amazonaws.com/itsycal.xml'
   end
 
+  # itsycal.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://itsycal.s3.amazonaws.com/Itsycal-#{version}.zip"
   name 'Itsycal'
   homepage 'https://www.mowglii.com/itsycal/'
 
