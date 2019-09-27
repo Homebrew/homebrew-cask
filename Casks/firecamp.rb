2d8d800ddefb0f198572f2203d0b79262ba0197b
Nishchit Dhanani <dnishchit@gmail.com>
2019-09-27 11:32:22 +0530

    Firecamp 0.6.7, sha256 added

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -1,6 +1,6 @@
 cask 'firecamp' do
   version '0.6.7'
-  sha256 :no_check
+  sha256 '9da872bd0de9cee93f7469ace821a1e5b640c326143b1d4e1ab95bfd3268794a'
 
   # firecamp.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg"
