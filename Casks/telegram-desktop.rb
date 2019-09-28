868eedd54dd8f3ff19739c028ad4f14524bb6b5a
Randall <ran.dall@icloud.com>
2019-09-28 13:45:01 -0700

    Update telegram-desktop from 1.8.8 to 1.8.9

--- Casks/telegram-desktop.rb
+++ Casks/telegram-desktop.rb
@@ -1,6 +1,6 @@
 cask 'telegram-desktop' do
-  version '1.8.8'
-  sha256 '864ccc067d6a66824dbbc7eca0e92be97f29838dd04a0818206b49ed7ceff374'
+  version '1.8.9'
+  sha256 'ea23427dec8447420dd0437fcd6e382f7466b69312ed69e49c979b816ba7ce11'
 
   # github.com/telegramdesktop/tdesktop was verified as official when first introduced to the cask
   url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version}/tsetup.#{version}.dmg"
