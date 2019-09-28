0579c1a70331b82cba85fcfd42bf16fc771b10a5
Julian Mayer <julian@corecode.io>
2019-09-28 16:10:49 +0200

    Update fanny from 1.0.3 to 2.0.0

--- Casks/fanny.rb
+++ Casks/fanny.rb
@@ -1,8 +1,9 @@
 cask 'fanny' do
-  version '1.0.3'
-  sha256 '3c7bcc29006650cbcb33a28fa0a97480227ab36f3bb5453243fef1563bb92bc0'
+  version '2.0.0'
+  sha256 'e8dbd45c85528ca3e2ad8bde63a396c74d0c075956310b0ba4d3ae3e8ad5629d'
 
   url 'https://fannywidget.com/FannyWidget.zip'
+  appcast 'https://github.com/DanielStormApps/Fanny/releases.atom'
   name 'FannyWidget'
   homepage 'https://fannywidget.com/'
 
