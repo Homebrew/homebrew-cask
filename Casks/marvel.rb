569e20a15993819f37139e34cb162b4a70c78901
Randall <ran.dall@icloud.com>
2019-09-28 13:48:54 -0700

    Update marvel from 8.4.4 to 8.5

--- Casks/marvel.rb
+++ Casks/marvel.rb
@@ -1,6 +1,6 @@
 cask 'marvel' do
-  version '8.4.4'
-  sha256 '6df46248a55eb4af686593830fc9eae043353189f9c2c6b87e6135d7347e73ef'
+  version '8.5'
+  sha256 'd3391b68b6aadb8938779b718a7409a14d958af9769f548e54dd607cc0e7bdfa'
 
   # storage.googleapis.com/sketch-plugin was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/sketch-plugin/#{version}/Marvel.zip"
