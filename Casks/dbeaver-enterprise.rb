c2dd2f527e14ec3655f2af77efcb58a99e104ec5
CoreCode <core-code@users.noreply.github.com>
2019-09-28 16:42:35 +0200

    Update dbeaver-enterprise (#69873)

--- Casks/dbeaver-enterprise.rb
+++ Casks/dbeaver-enterprise.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-enterprise' do
   version '6.2.0'
-  sha256 '5a8163c19fd6c8890fde316c0ef4c9d3c65303aaa4424e50539c07df32206f3c'
+  sha256 'f723a7d26ee82cbebbdf3bd7fe809f9d1c9f67f71b8266d4ea5712d802856ca8'
 
   url "https://dbeaver.com/files/#{version}/dbeaver-ee-#{version}-macos.dmg"
   appcast 'https://dbeaver.com/product/version.xml'
