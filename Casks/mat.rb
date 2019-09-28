a25a98a7ac9db8613909d9731f245c2c51132341
Randall <ran.dall@icloud.com>
2019-09-28 00:56:04 -0700

    Update mat from 1.9.0.20190605 to S

--- Casks/mat.rb
+++ Casks/mat.rb
@@ -1,6 +1,6 @@
 cask 'mat' do
-  version '1.9.0.20190605'
-  sha256 '62704e6a291403575b1c4ee7e7d0c6bd0bc5b479e4034c513d4ee37b52e665d8'
+  version 'S'
+  sha256 '8fd4409e2b97c2466a64ea46e44bffe1bb53b42c4ef86e9f4d0c084a0968f027'
 
   url "https://www.eclipse.org/downloads/download.php?r=1&file=/mat/#{version.major_minor_patch}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.x86_64.zip"
   appcast 'https://www.eclipse.org/mat/downloads.php'
