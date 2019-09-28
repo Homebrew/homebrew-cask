b190e781a53f79212018261affd4df95c5b55d07
Vítor Galvão <info@vitorgalvao.com>
2019-09-28 13:23:46 +0100

    Update reunion.rb

--- Casks/reunion.rb
+++ Casks/reunion.rb
@@ -3,7 +3,8 @@ cask 'reunion' do
   sha256 '729de26b38a0f97990378795a4f372f70880dafa4864da91d7c7e018b88497bf'
 
   url "https://store.leisterpro.com/updates/reunion#{version.major}/Reunion-#{version.dots_to_hyphens}.zip"
-  appcast "https://store.leisterpro.com/updates/reunion#{version.major}/appcast.xml"
+  appcast "https://store.leisterpro.com/updates/reunion#{version.major}/appcast.xml",
+          configuration: version.dots_to_hyphens
   name 'Reunion'
   homepage 'https://www.leisterpro.com/'
 
