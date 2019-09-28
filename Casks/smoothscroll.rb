9dfec6dec78237827902166c282920b5b3cb886f
Randall <ran.dall@icloud.com>
2019-09-28 13:51:06 -0700

    Update smoothscroll from 1.3.2 to 1.3.5

--- Casks/smoothscroll.rb
+++ Casks/smoothscroll.rb
@@ -1,6 +1,6 @@
 cask 'smoothscroll' do
-  version '1.3.2'
-  sha256 '8cf565f020cf9a5abfb48357d4460eda977a571c26a47c5fe484f3b75ae31801'
+  version '1.3.5'
+  sha256 '9e4564f8923f0b26a89fb0b4693e9432c2d8a283d794e665e8a13ddbecffaa00'
 
   url 'https://www.smoothscroll.net/mac/download/SmoothScroll.app.zip'
   appcast 'https://updater.smoothscroll.net/mac/updater.xml'
