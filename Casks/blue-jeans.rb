bcb25c8d01a305ac589ee155479e674a97d010c3
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-28 14:21:17 -0700

    Update blue-jeans from 2.15.0.237 to 2.16.0.209 (#69893)

--- Casks/blue-jeans.rb
+++ Casks/blue-jeans.rb
@@ -1,6 +1,6 @@
 cask 'blue-jeans' do
-  version '2.15.0.237'
-  sha256 'ed72ee9e64a1601f636f49189e1eaa4dd2d3a1847820e11f1b2aa1d4eb2b6c11'
+  version '2.16.0.209'
+  sha256 '266e1570c2824393c6cc8e693e74108b2481b75293edfeec4ea79f25442108f5'
 
   url "https://swdl.bluejeans.com/desktop-app/mac/#{version.major_minor_patch}/#{version}/BlueJeansInstaller.dmg"
   appcast 'https://www.bluejeans.com/downloads'
