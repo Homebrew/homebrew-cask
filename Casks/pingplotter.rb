688202bd57532012f537fe8c905c0ec2b6839c7e
Logan Fleur <effleurager@users.noreply.github.com>
2019-09-28 02:52:28 +0200

    Update pingplotter (#69818)

--- Casks/pingplotter.rb
+++ Casks/pingplotter.rb
@@ -1,6 +1,6 @@
 cask 'pingplotter' do
   version '5.11.5'
-  sha256 '2458949db124965761b929302d2752f9e3605cf89241fd06a99c7c447ab61b01'
+  sha256 'edacf49d958d9aa033e0b8a2e8a1246ddcdaf4ea9574cc0e1b989937e75bf877'
 
   url 'https://www.pingplotter.com/downloads/pingplotter_osx.zip'
   appcast 'https://www.pingplotter.com/download'
