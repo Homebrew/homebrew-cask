7cadb2c58c888bc8b2455de2f7dd5c596a0d588e
CoreCode <core-code@users.noreply.github.com>
2019-09-28 19:16:57 +0200

    Update launchpad-manager-yosemite from 1.0.8 to 1.0.10 (#69871)

--- Casks/launchpad-manager.rb
+++ Casks/launchpad-manager.rb
@@ -1,23 +1,20 @@
-cask 'launchpad-manager-yosemite' do
+cask 'launchpad-manager' do
   if MacOS.version <= :mavericks
     version '1.3.11'
     sha256 'f686a9a332663a003e9fabd32a1d44fc98debda15225368f1e8aef181955bc72'
 
     url 'http://launchpadmanager.com/download.php/LaunchpadManager.dmg'
-
-    app 'Launchpad Manager.app'
   else
-    version '1.0.9'
-    sha256 '1097199b5be59a3b8967e66d2bd870308c64607c3476e6f1b41c3f51a6e0ce1a'
+    version '1.0.10'
+    sha256 '88b37120c0ae022309573c4d2587a6ec85df7895773bca0fd3b5ba6cd86461a6'
 
     url 'http://launchpadmanager.com/download_yosemite.php/LaunchpadManagerYosemite.dmg'
-    appcast 'http://launchpadmanager.com/appyos/sparkle.rss'
-
-    app 'Launchpad Manager Yosemite.app'
   end
 
   name 'Launchpad Manager'
   homepage 'http://launchpadmanager.com/'
+
+  app 'Launchpad Manager.app'
 
   zap trash: [
                '~/Library/Application Support/Launchpad Manager',
