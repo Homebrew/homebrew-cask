63ce094006361c703af96a775e97159b15051dfa
zhangshaolong <zhangsl@rentongtech.com>
2019-09-28 16:17:32 +0800

    Update youku from 1.7.5.08231 to 1.7.6.09273

--- Casks/youku.rb
+++ Casks/youku.rb
@@ -1,6 +1,6 @@
 cask 'youku' do
-  version '1.7.5.08231'
-  sha256 'f6fb1b219193745c7e9358906710cbabd19f8a8d218ad0319bbfa28a265927d0'
+  version '1.7.6.09273'
+  sha256 'fcb6674922171c8df6d6588715897d9ec46f15bd1397ed5f50cccb9806fd118d'
 
   url "https://pcclient.download.youku.com/ikumac/youkumac_#{version}.dmg"
   appcast 'https://pd.youku.com/pc'
