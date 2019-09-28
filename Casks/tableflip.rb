4b6b85764370ec22a791a8fe655a010e05a333bc
Randall <ran.dall@icloud.com>
2019-09-27 23:51:23 -0700

    Update tableflip from 1.2.1 to 1.2.2

--- Casks/tableflip.rb
+++ Casks/tableflip.rb
@@ -1,6 +1,6 @@
 cask 'tableflip' do
-  version '1.2.1'
-  sha256 '1bda6ec5aa2db25b856527c6770c13e110df06866c0de40f91ed07023bf31625'
+  version '1.2.2'
+  sha256 '009a005c7a1d41d120a9e3d5b819db98e6ca5afdfd065926c8dbf032b20ae6f0'
 
   # update.christiantietze.de/tableflip was verified as official when first introduced to the cask
   url "https://update.christiantietze.de/tableflip/v#{version.major}/TableFlip-v#{version}.zip"
