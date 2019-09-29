85fd604e5391c199dd43db680006a0eb1bfff631
CoreCode <core-code@users.noreply.github.com>
2019-09-29 19:58:53 +0200

    Update phonerescue (#69901)

--- Casks/phonerescue.rb
+++ Casks/phonerescue.rb
@@ -2,7 +2,7 @@ cask 'phonerescue' do
   version :latest
   sha256 :no_check
 
-  url 'https://dl.imobie.com/phonerescue-ios-mac.dmg'
+  url 'https://dl.imobie.com/phonerescue-for-ios.zip'
   name 'PhoneRescue'
   homepage 'https://www.imobie.com/phonerescue/'
 
