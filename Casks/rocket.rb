e124bbcde3e4f7ca4c9e5bd3e42d84399f9e1cc4
suschizu <brigitte@corecode.io>
2019-09-29 20:21:05 +0200

    Update rocket.rb

--- Casks/rocket.rb
+++ Casks/rocket.rb
@@ -1,9 +1,9 @@
 cask 'rocket' do
-  version '1.6,61:db879acd-macrelease'
+  version '1.6,61'
   sha256 '4e6372d2282e51fd315720c87f5754c20be3791c448f407c4dd4e4ec9cf6d0cc'
 
   # ds3.us-east-2.amazonaws.com was verified as official when first introduced to the cask
-  url "https://#{version.after_colon}.s3.us-east-2.amazonaws.com/Rocket-#{version.after_comma.before_colon}.dmg"
+  url "https://macrelease.matthewpalmer.net/distribution/appcasts/Rocket-#{version.after_comma}.dmg
   appcast 'https://updates.devmate.com/net.matthewpalmer.Rocket.xml'
   name 'Rocket'
   homepage 'https://matthewpalmer.net/rocket/'
