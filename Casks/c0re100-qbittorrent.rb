96780ebd9eb4be376fad6b58b1fb7e05a8384a8a
Alexara Wu <alexarawu@outlook.com>
2019-09-29 18:05:58 +0800

    Update c0re100-qbittorrent from 4.1.7.1 to 4.1.8.1 (#69900)

--- Casks/c0re100-qbittorrent.rb
+++ Casks/c0re100-qbittorrent.rb
@@ -1,6 +1,6 @@
 cask 'c0re100-qbittorrent' do
-  version '4.1.7.1'
-  sha256 '487310954af206550cd0eafff1b56d84aaf968e64086e66819aad9b2b0fa0bf3'
+  version '4.1.8.1'
+  sha256 '640624ec1b27d9027e06ffab111c235a4daf9098ba056ca6eb8ff8d0c32b971e'
 
   url "https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-#{version}/qBittorrent-#{version}.dmg"
   appcast 'https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases.atom'
