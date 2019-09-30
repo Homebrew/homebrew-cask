e28a9da7cb9e195c9e660d744da637dab10871bd
Randall <17261190+ran-dall@users.noreply.github.com>
2019-09-29 20:21:27 -0700

    Update youdaonote from 3.5.4 to 3.5.5 (#69920)

--- Casks/youdaonote.rb
+++ Casks/youdaonote.rb
@@ -1,6 +1,6 @@
 cask 'youdaonote' do
-  version '3.5.4'
-  sha256 '7c1d00df2303e0fa605a4c87767e0222d4c62aa7c45d2e4840ed4699573d1924'
+  version '3.5.5'
+  sha256 '5ac5fcd29cc1508caedcc17eb54369f77161cd6dd8c458d58f0123339e1b4804'
 
   # download.ydstatic.com/notewebsite/downloads was verified as official when first introduced to the cask
   url 'https://download.ydstatic.com/notewebsite/downloads/YoudaoNote.dmg'
