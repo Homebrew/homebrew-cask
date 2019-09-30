4e187f4d662a9245688d4ddd2ab33722f0530cd7
CoreCode <core-code@users.noreply.github.com>
2019-09-30 02:17:51 +0200

    Update endlesssky to 0.9.10 (#69904)

--- Casks/endlesssky.rb
+++ Casks/endlesssky.rb
@@ -1,12 +1,12 @@
 cask 'endlesssky' do
-  version '0.9.8'
-  sha256 'b233c7b7aa3c87346abc325ea2958c7d36a17d276760b926184af7a23b454ec2'
+  version '0.9.10'
+  sha256 'f2e7aa2bcd29c6070cac386111362bd353239cbaf6061c22dfa3b440426fd06b'
 
   # github.com/endless-sky/endless-sky was verified as official when first introduced to the cask
-  url "https://github.com/endless-sky/endless-sky/releases/download/v#{version}/endless-sky-macosx-#{version}.dmg"
+  url "https://github.com/endless-sky/endless-sky/releases/download/v#{version}/endless-sky-macos-#{version}.dmg"
   appcast 'https://github.com/endless-sky/endless-sky/releases.atom'
   name 'Endless Sky'
   homepage 'https://endless-sky.github.io/'
 
-  app 'EndlessSky.app'
+  app 'Endless Sky.app'
 end
