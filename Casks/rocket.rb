89435373efc1135d97c751c3b1d6db4ba4393479
suschizu <brigitte@corecode.io>
2019-09-30 17:32:41 +0200

    Update rocket.rb from 59 to 61 + update url (#69895)
    * Update rocket.rb
    
    * Update rocket.rb
    
    * Update rocket.rb
    
    * Update rocket.rb

--- Casks/rocket.rb
+++ Casks/rocket.rb
@@ -1,9 +1,8 @@
 cask 'rocket' do
-  version '1.5.1,59:1565911546'
-  sha256 '964f74383f045c4fba107a9bdf43bdc880c1c1a6f2321c8cf85534baf0f54811'
+  version '1.6,61'
+  sha256 '4e6372d2282e51fd315720c87f5754c20be3791c448f407c4dd4e4ec9cf6d0cc'
 
-  # dl.devmate.com/net.matthewpalmer.Rocket was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/net.matthewpalmer.Rocket/#{version.after_comma.before_colon}/#{version.after_colon}/Rocket-#{version.after_comma.before_colon}.zip"
+  url "https://macrelease.matthewpalmer.net/distribution/appcasts/Rocket-#{version.after_comma}.dmg"
   appcast 'https://updates.devmate.com/net.matthewpalmer.Rocket.xml'
   name 'Rocket'
   homepage 'https://matthewpalmer.net/rocket/'
