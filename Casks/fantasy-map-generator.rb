fffb6bf7d00668ffc040f285e54a4f390fb49518
Vítor Galvão <info@vitorgalvao.com>
2019-10-01 20:05:23 +0100

    Update fantasy-map-generator.rb

--- Casks/fantasy-map-generator.rb
+++ Casks/fantasy-map-generator.rb
@@ -3,10 +3,10 @@ cask 'fantasy-map-generator' do
   sha256 'e15c3d3418c48081c3fe42fe86304c6ad29f9a80980d3fcf7797a7a8b13edbca'
 
   # github.com/Azgaar/Fantasy-Map-Generator was verified as official when first introduced to the cask
-  url 'https://github.com/Azgaar/Fantasy-Map-Generator/releases/download/v1.0/FMG-darwin-x64.zip'
+  url "https://github.com/Azgaar/Fantasy-Map-Generator/releases/download/v#{version}/FMG-darwin-x64.zip"
   appcast 'https://github.com/Azgaar/Fantasy-Map-Generator/releases.atom'
   name "Azgaar's Fantasy Map Generator"
   homepage 'https://azgaar.github.io/Fantasy-Map-Generator'
 
-  app "Azgaar's Fantasy Map Generator-darwin-x64/Azgaar\'s\ Fantasy\ Map\ Generator.app"
+  app "Azgaar's Fantasy Map Generator-darwin-x64/Azgaar's Fantasy Map Generator.app"
 end
