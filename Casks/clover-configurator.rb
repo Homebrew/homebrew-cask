23ccc2fb08f9256cd0b24f11c180ec917d15b867
Yurii Kolesnykov <root@yurikoles.com>
2019-10-01 22:36:17 +0300

    Update clover-configurator from 5.5.0.0 to 5.6.0.0 (#69978)

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,6 +1,6 @@
 cask 'clover-configurator' do
-  version '5.5.0.0'
-  sha256 '4c2059895ef65067269e18b7108781d5a3f20b05aa81d8b87db1d1dd9c84b82b'
+  version '5.6.0.0'
+  sha256 '9df774ce26ef084807d916fc76cb5091891db13f9f0cfe945eec241581622ea0'
 
   url 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/builds-data-ccg/CCG.zip'
   appcast 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/update-data-builds.xml'
