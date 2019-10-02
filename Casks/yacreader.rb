771075461323642cb55ef827ba7d12f814b1c1b2
Dylan Drost <dylan@dynadroid.nl>
2019-10-02 15:04:18 +0200

    Added comment for url and homepage hostname difference

--- Casks/yacreader.rb
+++ Casks/yacreader.rb
@@ -2,6 +2,7 @@ cask 'yacreader' do
   version '9.6.2'
   sha256 '92152f17b4ff072c3463ae045da5d232c2f59cb77267a0eeebb8af5de7d29214'
 
+  # github.com/YACReader/yacreader was verified as official when first introduced to the cask
   url "https://github.com/YACReader/yacreader/releases/download/#{version}/YACReader-#{version}.1909283.MacOSX-Intel.dmg"
   appcast 'https://github.com/YACReader/yacreader/releases.atom'
   name 'YACReader'
