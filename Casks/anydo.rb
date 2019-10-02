1a9e2310710d612d54a6a9f2aec5ba85f21f29e9
Vítor Galvão <info@vitorgalvao.com>
2019-10-02 16:18:34 +0100

    Update anydo.rb

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,8 +1,9 @@
 cask 'anydo' do
-  version ':latest'
+  version '4.2.46'
   sha256 'eeb4eae85b6609c4c3d775b96e0e2c77438bb4be81a9695dabe1bf65e9eb92e6'
 
   url 'https://electron-app.any.do/Any.do.dmg'
+  appcast 'https://electron-app.any.do/latest-mac.yml'
   name 'Any.do'
   homepage 'https://www.any.do/'
 
