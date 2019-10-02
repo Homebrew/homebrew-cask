5101b5a4eed5fdb8dd3decf2d1728cf41a127ef3
Randall <ran.dall@icloud.com>
2019-10-01 23:41:52 -0700

    Update panoply from 4.10.9 to 4.10.10

--- Casks/panoply.rb
+++ Casks/panoply.rb
@@ -1,6 +1,6 @@
 cask 'panoply' do
-  version '4.10.9'
-  sha256 '226d9aaa1194e106876d1304e7dde15ff2981c75330d32493527a55b5ef27fca'
+  version '4.10.10'
+  sha256 '6a347c2755650ce2e284270bc5734f75c4b89caf4d5f9b0b4287717517330d1f'
 
   url "https://www.giss.nasa.gov/tools/panoply/download/PanoplyMacOS-#{version}.dmg"
   appcast 'https://www.giss.nasa.gov/tools/panoply/download/'
