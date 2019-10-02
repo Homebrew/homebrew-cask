0a0dfe31e7cba9783455d57906dec074d78dcc3b
Alessandro Bellanda <a.bellanda@esconsulting.it>
2019-10-02 09:17:46 +0200

    Added Any.do Application Latest
    First commit of Any.do: to-do list, calendar application

---
+++ Casks/anydo.rb
@@ -0,0 +1,10 @@
+cask 'anydo' do
+  version ':latest'
+  sha256 'eeb4eae85b6609c4c3d775b96e0e2c77438bb4be81a9695dabe1bf65e9eb92e6'
+
+  url "https://electron-app.any.do/Any.do.dmg"
+  name 'Any.do'
+  homepage 'https://www.any.do'
+
+  app 'Any.do.app'
+end
