39d638e9828fd91aca72fedcd4a3b5d4f3958bec
brigitte zmoelnig <brigitte@corecode.io>
2019-10-02 16:34:28 +0200

    Update paraview from 5.6.2 to 5.7.0

--- Casks/paraview.rb
+++ Casks/paraview.rb
@@ -1,6 +1,6 @@
 cask 'paraview' do
-  version '5.6.2'
-  sha256 '9cbc9ff9b10625ccd38fd018d3e5b0424c5d394b1b57ff9d1faec182efd6101a'
+  version '5.7.0'
+  sha256 '41cae3522c9f6188aa94b4cfed10624cdce8b426171f03b3246b44c26d036f1d'
 
   url "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v#{version.major_minor}&type=binary&os=macOS&downloadFile=ParaView-#{version}-MPI-OSX10.12-64bit.dmg",
       user_agent: :fake
