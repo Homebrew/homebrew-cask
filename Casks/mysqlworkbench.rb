d1b3478aba3c999921c26e1b5b22c961bf2e3236
jana13 <5565333+jana13@users.noreply.github.com>
2019-10-02 14:37:49 +0200

    mysqlworkbench.rb: updated zap folders
    The previously listed folders didn't match the current files/folders anymore.

--- Casks/mysqlworkbench.rb
+++ Casks/mysqlworkbench.rb
@@ -13,10 +13,8 @@ cask 'mysqlworkbench' do
 
   zap trash: [
                '~/Library/Application Support/MySQL/Workbench',
-               '~/Library/Preferences/com.oracle.mysql.workbench.plist',
-               '~/Library/Preferences/com.oracle.MySQLWorkbench.plist',
-               '~/Library/Saved Application State/com.oracle.mysql.workbench.savedState',
-               '~/Library/Saved Application State/com.oracle.MySQLWorkbench.savedState',
-               '~/Library/Caches/com.oracle.mysql.workbench',
+               '~/Library/Preferences/com.oracle.workbench.MySQLWorkbench.plist',
+               '~/Library/Caches/com.oracle.workbench.MySQLWorkbench',
+               '~/Library/Saved Application State/com.oracle.workbench.MySQLWorkbench.savedState',
              ]
 end
