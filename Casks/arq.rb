1adee1239066fd72cc7d1e7f1023c713dd28db22
yuusharo <36213375+yuusharo@users.noreply.github.com>
2019-10-03 10:34:09 -0700

    Update arq from 5.16.1 to 5.16.2 (#70087)

--- Casks/arq.rb
+++ Casks/arq.rb
@@ -1,6 +1,6 @@
 cask 'arq' do
-  version '5.16.1'
-  sha256 '9517eecae50d7164a868387b383c6a32895496c6db03cceb8d4bd22980a2a8f5'
+  version '5.16.2'
+  sha256 'f953be8d47e937e3dffe23111b838aa6d871ef6b61eb4807dfee558b2d61395a'
 
   url "https://www.arqbackup.com/download/arqbackup/Arq_#{version}.zip"
   appcast "https://www.arqbackup.com/download/arqbackup/arq#{version.major}.xml"
