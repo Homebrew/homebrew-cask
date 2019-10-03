43fb8f8986be46895fc350335eb75a2eefb6ad75
Randall <ran.dall@icloud.com>
2019-10-02 19:53:42 -0700

    Update garagebuy from 3.3 to 3.3.1

--- Casks/garagebuy.rb
+++ Casks/garagebuy.rb
@@ -1,6 +1,6 @@
 cask 'garagebuy' do
-  version '3.3'
-  sha256 '57409fe84cc308fd78fe01101939bd517641e57454bf757a07b47950ce5fdcca'
+  version '3.3.1'
+  sha256 'a1e6f7f105655d1a773755c450a0a062aa5509407a3d5ed02fb5aa2756bdabe9'
 
   # iwascoding.de was verified as official when first introduced to the cask
   url "https://www.iwascoding.de/downloads/GarageBuy_#{version}.dmg"
