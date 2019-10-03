ca10dc3849a57dfbd940d2fb9f8f7ac1820dc352
vikrantrathore <admin@vyomtech.com>
2019-10-03 11:54:25 +0800

    Change SHA hash and download link
    Updated to latest download link and SHA-265 hash as CI was giving error.

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -1,8 +1,8 @@
 cask 'inkscape' do
   version '1.0beta1'
-  sha256 '6939e99761009befa23a85b8f0d117c1df83934bb1d5fcb8084b4040e122aa66'
+  sha256 '68831989b3919e3137d5acfb130a844933706748addc8b4cd7f957348c1c60a3'
 
-  url "https://inkscape.org/gallery/item/14919/Inkscape-#{version}.dmg"
+  url "https://inkscape.org/gallery/item/14919/Inkscape-#{version}_OEMhoXK.dmg"
   name 'Inkscape'
   homepage 'https://inkscape.org/'
 
