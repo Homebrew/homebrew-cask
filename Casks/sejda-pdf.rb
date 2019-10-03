86e1c0feaecaa394681222d4659f0bee7bd98fca
Randall <ran.dall@icloud.com>
2019-10-02 19:37:04 -0700

    Update sejda-pdf from 5.3.6 to 5.3.7

--- Casks/sejda-pdf.rb
+++ Casks/sejda-pdf.rb
@@ -1,6 +1,6 @@
 cask 'sejda-pdf' do
-  version '5.3.6'
-  sha256 '9a480d37fc55f9673bf5e9e204f756bac02c097a087399b47f70cf151a3eb031'
+  version '5.3.7'
+  sha256 '699335cd3119a80afb9fb3e10246b28a43d9cc7ed35ec20158064dbd22406409'
 
   # sejda-cdn.com was verified as official when first introduced to the cask
   url "https://sejda-cdn.com/downloads/sejda-desktop_#{version}.dmg"
