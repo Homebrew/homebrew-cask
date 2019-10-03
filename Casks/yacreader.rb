474d84cdbb34e25b0691dd8a35863e009ab66774
Dylan Drost <dylan@dynadroid.nl>
2019-10-03 09:26:47 +0200

    fixed version number per PR comments

--- Casks/yacreader.rb
+++ Casks/yacreader.rb
@@ -1,9 +1,9 @@
 cask 'yacreader' do
-  version '9.6.2'
+  version '9.6.2.1909283'
   sha256 '92152f17b4ff072c3463ae045da5d232c2f59cb77267a0eeebb8af5de7d29214'
 
   # github.com/YACReader/yacreader was verified as official when first introduced to the cask
-  url "https://github.com/YACReader/yacreader/releases/download/#{version}/YACReader-#{version}.1909283.MacOSX-Intel.dmg"
+  url "https://github.com/YACReader/yacreader/releases/download/#{version.major_minor_patch}/YACReader-#{version}.MacOSX-Intel.dmg"
   appcast 'https://github.com/YACReader/yacreader/releases.atom'
   name 'YACReader'
   homepage 'https://www.yacreader.com/'
