39aaa96daaaccde9c10bf5e20f5bb1f51f80b7c2
CoreCode <core-code@users.noreply.github.com>
2019-10-04 19:35:49 +0200

    Update ipass (#70140)

--- Casks/ipass.rb
+++ Casks/ipass.rb
@@ -2,12 +2,13 @@ cask 'ipass' do
   version :latest
   sha256 :no_check
 
-  # d18uyicgt51j26.cloudfront.net/packages was verified as official when first introduced to the cask
-  url 'https://d18uyicgt51j26.cloudfront.net/packages/iPass_Mac.dmg'
+  # cdn-static-prod.qakr0.net was verified as official when first introduced to the cask
+  url 'https://cdn-static-prod.qakr0.net/packages/iPass_Mac.dmg'
   name 'iPass'
   homepage 'https://www.ipass.com/'
 
   pkg 'iPass.pkg'
 
-  uninstall pkgutil: 'com.ipass.ipass.pkg'
+  uninstall pkgutil: 'com.ipass.ipass.pkg',
+            quit:    'com.iPass.iPass'
 end
