76f7db0cd2fc1d5b649c5b497dfe817adb7c6dac
suschizu <brigitte@corecode.io>
2019-10-04 22:32:01 +0200

    Update preference-manager.rb

--- Casks/preference-manager.rb
+++ Casks/preference-manager.rb
@@ -3,7 +3,8 @@ cask 'preference-manager' do
   sha256 '4920d80e09bf5e492a930896068352c4f08b3764ce723b7c13000e8b1d04ae15'
 
   url "https://www.digitalrebellion.com/download/prefman?version=#{version.no_dots}"
-  appcast 'https://www.digitalrebellion.com/rss/appcasts/pref_man.xml'
+  appcast 'https://www.digitalrebellion.com/prefman/changelog',
+          configuration: version.chomp(".0")
   name 'Preference Manager'
   homepage 'https://www.digitalrebellion.com/prefman/'
 
