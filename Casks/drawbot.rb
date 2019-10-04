59124fb72ae965ec8a00fd8d2197fbc5614a8edf
CoreCode <core-code@users.noreply.github.com>
2019-10-04 16:19:14 +0200

    Update drawbot from 3.120 to 3.121 (#70138)

--- Casks/drawbot.rb
+++ Casks/drawbot.rb
@@ -1,6 +1,6 @@
 cask 'drawbot' do
-  version '3.120'
-  sha256 '3c4367befaac968b47b71320e20132f1b0761e2830c13465004268ea446deed4'
+  version '3.121'
+  sha256 '68b8327fcacf21db7d37374a389fe5609d87eac6e6df01993b0effffef95aa71'
 
   # typemytype.com/drawBot was verified as official when first introduced to the cask
   url 'https://static.typemytype.com/drawBot/DrawBot.dmg'
