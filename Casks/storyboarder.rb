ee750593219cf7aa0a4cba220fdfa0f8290639d1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-03 23:19:14 -0700

    Update storyboarder from 1.13.0 to 1.14.0 (#70112)

--- Casks/storyboarder.rb
+++ Casks/storyboarder.rb
@@ -1,6 +1,6 @@
 cask 'storyboarder' do
-  version '1.13.0'
-  sha256 'ac9daac2c3b22f210367d264f143f9e416398ff157ff543c91b567665ecf6ca4'
+  version '1.14.0'
+  sha256 'b9edf73803bc7c96a92b1591e0728bc78205e665cf7dfb339ef112e0a2811692'
 
   # github.com/wonderunit/storyboarder was verified as official when first introduced to the cask
   url "https://github.com/wonderunit/storyboarder/releases/download/v#{version}/Storyboarder-#{version}.dmg"
