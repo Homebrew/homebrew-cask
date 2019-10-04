a6119c1af867a79485bb7fc3721bd7c0ee3a2333
André David <adavidzh@users.noreply.github.com>
2019-10-04 22:04:55 +0200

    Update freecad.rb (#70163)
    Somehow, the `sha` has changed to [`abf7a7216497b6aa4135ae60038e70234e5e731b9085f4196c63e7e18b2c5af4`](https://github.com/FreeCAD/FreeCAD/releases/download/0.19_pre/FreeCAD_0.19-18353-OSX-x86_64-conda-Qt5-Py3.dmg-SHA256.txt) breaking #70078

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,6 +1,6 @@
 cask 'freecad' do
   version '0.19_pre,18353'
-  sha256 '4ae884e74454d58a50550c7da2caa7339c9344afa651fb7817cfea4dc3761e86'
+  sha256 'abf7a7216497b6aa4135ae60038e70234e5e731b9085f4196c63e7e18b2c5af4'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg"
