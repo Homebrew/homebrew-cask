74b2b4c8966d7090b53af34c4c939e78baccda88
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-04 00:07:17 -0700

    Update jami from 20190902.1120 to 20191002.1514 (#70115)

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -1,6 +1,6 @@
 cask 'jami' do
-  version '20190902.1120'
-  sha256 'a5e0ce5978dce7285a07aa3b2e272f8f647a7f5610db2fbb26f7d8c94b879485'
+  version '20191002.1514'
+  sha256 '6e21be307b182edcdb92fe8053835f34bba129626eb5ff6307956c7a53ecbfe0'
 
   url "https://dl.ring.cx/mac_osx/jami-#{version.no_dots}.dmg"
   appcast 'https://dl.ring.cx/mac_osx/sparkle-ring.xml',
