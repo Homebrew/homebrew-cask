a962244ab0c87752aaa8b0e1beabd43fd6c39edc
Randall <ran.dall@icloud.com>
2019-10-04 00:13:10 -0700

    Update get-backup-pro from 3.4.17 to 3.4.18

--- Casks/get-backup-pro.rb
+++ Casks/get-backup-pro.rb
@@ -1,6 +1,6 @@
 cask 'get-backup-pro' do
-  version '3.4.17'
-  sha256 '221709769a16a06015a4edaa5025cdbc46f7c1703e008042dc1a13133c97d62d'
+  version '3.4.18'
+  sha256 '844ea9110e0763cc6dd8441aead9ab89fcab0ed0f6e0113f2375e9d9dc923da4'
 
   # belightsoft.s3.amazonaws.com/updates was verified as official when first introduced to the cask
   url "https://belightsoft.s3.amazonaws.com/updates/Get+Backup+Pro+#{version.major}.zip"
