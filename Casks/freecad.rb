11d523ac20599031e36dd3263b6866c0decd61ed
Vítor Galvão <info@vitorgalvao.com>
2019-10-05 22:56:25 +0100

    freecad.rb: fix shasum

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,6 +1,6 @@
 cask 'freecad' do
   version '0.19_pre,18353'
-  sha256 'abf7a7216497b6aa4135ae60038e70234e5e731b9085f4196c63e7e18b2c5af4'
+  sha256 'b39385da17c2959448b47ac1d44880b3d800c3786b4703a95ceac9bee12dde72'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg"
