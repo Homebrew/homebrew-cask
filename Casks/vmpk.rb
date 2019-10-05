ed00fbf5e8af391c467eb4de105d223e32d8e81b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-04 20:49:03 -0700

    Update vmpk from 0.7.1 to 0.7.2 (#70195)

--- Casks/vmpk.rb
+++ Casks/vmpk.rb
@@ -1,6 +1,6 @@
 cask 'vmpk' do
-  version '0.7.1'
-  sha256 '1fbf3bdd7917f7f02444e8af3b0c21db350e27e8857bf9f3701b952f904ab8ec'
+  version '0.7.2'
+  sha256 'f032e2efff44f83a9faa60711254fb5b10e4a9465fef00cab86cc7be29ec72e3'
 
   # downloads.sourceforge.net/vmpk was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/vmpk/vmpk-#{version}-mac-x64.dmg"
