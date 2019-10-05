ea9ec512150af24b71f66f6bbbb318f0b1564dae
Randall <ran.dall@icloud.com>
2019-10-04 19:50:11 -0700

    Update orange from 3.23.0 to 3.23.1

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,6 +1,6 @@
 cask 'orange' do
-  version '3.23.0'
-  sha256 'e53f73fe4b0bcb7339430e5a67c5e43136d9a20a6cafe74d4d127746ac3d7789'
+  version '3.23.1'
+  sha256 'c9225f9bd53a7c475fe134602dd270599f3d01cd9c0fbe19ee6f7203dad3464b'
 
   url "https://download.biolab.si/download/files/Orange#{version.major}-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://service.biolab.si/download/orange?platform=mac'
