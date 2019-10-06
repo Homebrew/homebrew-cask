00fc0a9ef6a598ad07640a5053fbc4ce1f79ae93
suschizu <brigitte@corecode.io>
2019-10-06 20:09:13 +0200

    Update navicat-data-modeler.rb

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -3,7 +3,7 @@ cask 'navicat-data-modeler' do
   sha256 'f8ce021fe33a81a72108b2993ca5587e96a748d1bd8e46e5330cc0d1cd6c3159'
 
   url "http://download.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
-  appcast 'https://www.navicat.com/updater/modeler_v2/sysProfileInfo.php?appName=Navicat%20Data%20Modeler'
+  appcast 'https://navicat.com/en/products/navicat-data-modeler-release-note'
   name 'Navicat Data Modeler'
   homepage 'https://www.navicat.com/products/navicat-data-modeler'
 
