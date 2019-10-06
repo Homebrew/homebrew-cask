02e524413c3aa9dca09666395b21d4e2057187bf
Zsolt Prontvai <prozsolt@gmail.com>
2019-10-06 17:32:33 +0200

    Update Eagle links to offical Autodesk ones (#70235)

--- Casks/eagle.rb
+++ Casks/eagle.rb
@@ -2,10 +2,10 @@ cask 'eagle' do
   version '9.5.1'
   sha256 '3a9bb291ea87309a01d414e7eb0d86373f97cf7cb3eaf626c88762777f26bca2'
 
-  url "https://eagle-updates.circuits.io/downloads/#{version.dots_to_underscores}/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
+  url "https://trial2.autodesk.com/NET17SWDLD/2017/EGLPRM/ESD/Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.autodesk.com/eagle-download-mac'
   name 'Autodesk EAGLE'
-  homepage 'https://circuits.io/'
+  homepage 'https://www.autodesk.com/products/eagle/overview'
 
   pkg "Autodesk_EAGLE_#{version}_English_Mac_64bit.pkg"
 
