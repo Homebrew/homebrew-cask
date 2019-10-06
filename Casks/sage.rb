0f5f0501ea96459672b87f57a003094daadaedc4
Randall <ran.dall@icloud.com>
2019-10-05 23:13:14 -0700

    Update sage from 8.8,10.14.5 to 8.9,10.14.6

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -1,6 +1,6 @@
 cask 'sage' do
-  version '8.8,10.14.5'
-  sha256 'c0d7a633cf94231be48f1eb5a5b7de22befb5646eaa8b3e4a13b784f892b6ad6'
+  version '8.9,10.14.6'
+  sha256 '48dfbd697eaadb77913f8a57e01444e45826bdfeabf5e27ba1bc215c4cbd1764'
 
   # mirrors.mit.edu/sage/osx/intel was verified as official when first introduced to the cask
   url "https://mirrors.mit.edu/sage/osx/intel/sage-#{version.before_comma}-OSX_#{version.after_comma}-x86_64.app.dmg"
