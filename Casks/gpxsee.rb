1aaa3dbf7481e8e8f70e60f002602803cfb612a5
Randall <ran.dall@icloud.com>
2019-10-06 17:57:30 -0700

    Update gpxsee from 7.14 to 7.15

--- Casks/gpxsee.rb
+++ Casks/gpxsee.rb
@@ -1,6 +1,6 @@
 cask 'gpxsee' do
-  version '7.14'
-  sha256 '55cfea3b2c63bc6e8de48f77bbd1f71294b64e9b1327ef95871ddba2f2282fc5'
+  version '7.15'
+  sha256 '0abbefd3ec8417f690dfdbd7797809c23c53b1813853a1d07837e44fffc41ad0'
 
   # sourceforge.net/gpxsee/Mac%20OS%20X was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/gpxsee/Mac%20OS%20X/GPXSee-#{version}.dmg"
