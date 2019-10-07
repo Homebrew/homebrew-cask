b008de75979e1b2cdc50fbd51dc414c18da0d504
Jordan Danford <jordandanford@gmail.com>
2019-10-07 13:38:41 -0700

    Update audio-hijack from 3.5.7 to 3.6.0

--- Casks/audio-hijack.rb
+++ Casks/audio-hijack.rb
@@ -1,6 +1,6 @@
 cask 'audio-hijack' do
-  version '3.5.7'
-  sha256 '12bbfb98c7973aca3956f1ed0018ea43e2da3926ad12363dbec9711bfc87a0fb'
+  version '3.6.0'
+  sha256 '107e6cbec991a2a1808cdc7348b5bcb8d40967dc79ccb777fea9aeb9730f26eb'
 
   url 'https://rogueamoeba.com/audiohijack/download/AudioHijack.zip'
   appcast 'https://www.rogueamoeba.com/audiohijack/releasenotes.php'
