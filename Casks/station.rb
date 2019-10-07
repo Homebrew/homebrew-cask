8888b9730bd8d365dddfcc043c68d740d79d09c5
Shoichi Aizawa <shoichiaizawa@gmail.com>
2019-10-08 08:07:03 +0900

    Update station from 1.51.1 to 1.52.1 (#70272)

--- Casks/station.rb
+++ Casks/station.rb
@@ -1,6 +1,6 @@
 cask 'station' do
-  version '1.51.1'
-  sha256 'de2569274cbf7a26d2be8ec11a2f0d0d0fc1ddbe6a11d2a02f932cd8d6bae610'
+  version '1.52.1'
+  sha256 '7d78c53e8c56de578c8fbf22d6fdd8446992e00b326715627d58252ae8c40ae5'
 
   # github.com/getstation/desktop-app-releases was verified as official when first introduced to the cask
   url "https://github.com/getstation/desktop-app-releases/releases/download/#{version}/Station-#{version}.dmg"
