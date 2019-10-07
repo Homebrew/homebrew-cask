97fbf48c3977803238098ae1adb820234e7db77b
blogabe <6888675+blogabe@users.noreply.github.com>
2019-10-07 09:49:28 -0700

    Boxcryptor 2.30 (#70262)

--- Casks/boxcryptor.rb
+++ Casks/boxcryptor.rb
@@ -1,6 +1,6 @@
 cask 'boxcryptor' do
-  version '2.29.1001'
-  sha256 'e37da434c579a74e37727668c0de9f429eb66cfc0a9168a6d8c3f0ff7fdecbc1'
+  version '2.30.1004'
+  sha256 'cccea52e3ac7c1e4957dc0136c4564d224005bd39bfeaf46f6774b541603e4e6'
 
   url "https://downloads.boxcryptor.com/boxcryptor/mac/Boxcryptor_v#{version}_Installer.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.boxcryptor.com/l/download-macosx'
