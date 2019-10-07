2a03bfe699a941611d3dce55a4815ac13700f47d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-06 18:08:06 -0700

    Update virtualhostx from 8.7.12,80_61 to 8.7.13,80_62 (#70246)

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -1,6 +1,6 @@
 cask 'virtualhostx' do
-  version '8.7.12,80_61'
-  sha256 '4db714e733f56d215071eab54b0f2dfbb6b44b9fcda53c37ac698268f0d401be'
+  version '8.7.13,80_62'
+  sha256 'd5dbb0dbbb7e4c5c6d13da6fd538358a717944dbd962273ac47796ae6c963c77'
 
   # downloads-clickonideas.netdna-ssl.com/virtualhostx was verified as official when first introduced to the cask
   url "https://downloads-clickonideas.netdna-ssl.com/virtualhostx/virtualhostx#{version.after_comma}.zip"
