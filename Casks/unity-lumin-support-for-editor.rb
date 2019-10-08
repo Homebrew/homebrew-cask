5f58b96245e73df8cd9b4da2f089ad7d7472a2c1
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2019-10-08 21:58:23 +0200

    update sha256

--- Casks/unity-lumin-support-for-editor.rb
+++ Casks/unity-lumin-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-lumin-support-for-editor' do
   version '2019.2.8f1,ff5b465c8d13'
-  sha256 '4475a6ff876e4384677a21360977ce7af3305a41d72931835545676e14cc6c7b'
+  sha256 '565ac77d4b1398d770276130a32338807be95678bb51e86fcf46c088672e0e06'
   
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Lumin-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
