9bb7c7cf78ff7fe942b14859872031d845f6f484
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2019-10-08 22:22:46 +0200

    remove empty line

--- Casks/unity-lumin-support-for-editor.rb
+++ Casks/unity-lumin-support-for-editor.rb
@@ -3,7 +3,6 @@ cask 'unity-lumin-support-for-editor' do
   sha256 '565ac77d4b1398d770276130a32338807be95678bb51e86fcf46c088672e0e06'
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Lumin-Support-for-Editor-#{version.before_comma}.pkg"
-
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
   name 'Unity Lumin Build Support'
   homepage 'https://unity3d.com/unity/'
