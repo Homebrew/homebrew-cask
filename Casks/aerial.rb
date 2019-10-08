452eaff27c5f256017fffd916aae3c3bc4e09e85
Brian van Burken <b.burken@avisi.nl>
2019-10-08 18:33:36 +0200

    Update aerial from 1.6.1 to 1.6.2 (#70326)

--- Casks/aerial.rb
+++ Casks/aerial.rb
@@ -1,6 +1,6 @@
 cask 'aerial' do
-  version '1.6.1'
-  sha256 '5f506756bfa7a6ded456aa971a784650cedb2012d53e42b529d83b140c66e431'
+  version '1.6.2'
+  sha256 '7e7f938cd1aa4e1d40ed24938f34b88d694b1487424e74775a766c248658eff9'
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip"
   appcast 'https://github.com/JohnCoates/Aerial/releases.atom'
