bc1b56b5ca5e0c26625dc12a02ed4b4a06aa99de
James Pearson <james@needcode.co.uk>
2019-10-08 21:26:21 +0100

    Add Onecast 1.12

---
+++ Casks/onecast.rb
@@ -0,0 +1,12 @@
+cask 'onecast' do
+  version '1.12'
+  sha256 '412799e32f2f92d7ef10916956815282a5fa34c80895dca90c9d90f54a13c65e'
+
+  url 'https://onecast.me/downloads/OneCast.dmg'
+  name 'OneCast'
+  homepage 'https://onecast.me/'
+
+  auto_updates true
+
+  app 'OneCast.app'
+end
