ad07fd8b92f68b31aab519ad43ca51cc78a8c5b9
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2019-10-08 22:00:54 +0200

    remove whitespace

--- Casks/unity-lumin-support-for-editor.rb
+++ Casks/unity-lumin-support-for-editor.rb
@@ -1,15 +1,15 @@
 cask 'unity-lumin-support-for-editor' do
   version '2019.2.8f1,ff5b465c8d13'
   sha256 '565ac77d4b1398d770276130a32338807be95678bb51e86fcf46c088672e0e06'
-  
+
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Lumin-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
   name 'Unity Lumin Build Support'
   homepage 'https://unity3d.com/unity/'
-  
+
   depends_on cask: 'unity'
-  
+
   pkg "UnitySetup-Lumin-Support-for-Editor-#{version.before_comma}.pkg"
-  
+
   uninstall pkgutil: 'com.unity3d.LuminStandaloneSupport'
 end
