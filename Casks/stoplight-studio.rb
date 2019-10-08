a33b19ef4af4100ccc4738a58baaccff2f1b63e1
Vítor Galvão <info@vitorgalvao.com>
2019-10-08 12:21:24 +0100

    Update stoplight-studio.rb

--- Casks/stoplight-studio.rb
+++ Casks/stoplight-studio.rb
@@ -3,7 +3,7 @@ cask 'stoplight-studio' do
   sha256 '00c0e52eb56a29157ed53b1eafd45446b03709be74b2944e50759b1ee3740eba'
 
   # github.com/stoplightio/studio was verified as official when first introduced to the cask
-  url 'https://github.com/stoplightio/studio/releases/download/v1.1.4/Stoplight-Studio-1.1.4.dmg'
+  url "https://github.com/stoplightio/studio/releases/download/v#{version}/Stoplight-Studio-#{version}.dmg"
   appcast 'https://github.com/stoplightio/studio/releases.atom'
   name 'Stoplight Studio'
   homepage 'https://stoplight.io/studio/'
