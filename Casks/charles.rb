dca64e6cf40616f460233411b9202cea650f2b41
daejoon <kkode1911@gmail.com>
2019-10-08 18:52:50 +0900

    Upgrade charles.app to 4.5

--- Casks/charles.rb
+++ Casks/charles.rb
@@ -1,6 +1,6 @@
 cask 'charles' do
-  version '4.2.8'
-  sha256 '091268247ce67ee98eec0fb5fd908f332494234666ad8e81a1be08a144d67b87'
+  version '4.5'
+  sha256 'f3db5284430e336c0217afd2b480075db910d945d4730649fba2e4000dbdf9e4'
 
   url "https://www.charlesproxy.com/assets/release/#{version}/charles-proxy-#{version}.dmg"
   appcast 'https://www.charlesproxy.com/latest.do'
