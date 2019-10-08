6fc37c8af656a873ae56c992b9701b49b7153bc7
沙漠之子 <ymc1216@gmail.com>
2019-10-08 21:25:06 +0800

    Update spek.rb, add the withmorten/spek-alternative forked version
    The original author maybe suspend maintenance, here using the withmorten/spek-alternative forked version.

--- Casks/spek.rb
+++ Casks/spek.rb
@@ -1,14 +1,20 @@
 cask 'spek' do
-  version '0.8.3'
-  sha256 '648ffe37a4605d76b8d63ca677503ba63338b84c5df73961d9a5335ff144cc54'
+  if MacOS.version <= :sierra
+    version '0.8.3'
+    sha256 '648ffe37a4605d76b8d63ca677503ba63338b84c5df73961d9a5335ff144cc54'
+    # github.com/alexkay/spek was verified as official when first introduced to the cask
+    url "https://github.com/alexkay/spek/releases/download/v#{version}/spek-#{version}.dmg"
+    appcast 'https://github.com/alexkay/spek/releases.atom'
+  else
+    version '0.8.2.3'
+    sha256 '6426c4c34ac8bcb2fa1020cb295e3c105ca912c853cbbe84d1d196a32bba361b'
+    # The original author may suspend maintenance, here using the withmorten/spek-alternative forked version
+    url "https://github.com/withmorten/spek-alternative/releases/download/#{version}/spek-alternative-#{version}.dmg"
+    appcast 'https://github.com/withmorten/spek-alternative/releases.atom'
+  end
 
-  # github.com/alexkay/spek was verified as official when first introduced to the cask
-  url "https://github.com/alexkay/spek/releases/download/v#{version}/spek-#{version}.dmg"
-  appcast 'https://github.com/alexkay/spek/releases.atom'
   name 'Spek'
   homepage 'http://spek.cc/'
-
-  depends_on macos: '<= :sierra'
 
   app 'Spek.app'
 end
