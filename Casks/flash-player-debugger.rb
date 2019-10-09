5970cbaf967de0eabbeccb0127ea7c03aa718a4a
Andy Li <andy@onthewings.net>
2019-10-09 19:05:15 +0800

    Update flash-player-debugger from 32.0.0.255 to 32.0.0.270 (#70419)

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,6 +1,6 @@
 cask 'flash-player-debugger' do
-  version '32.0.0.255'
-  sha256 'eb8a0cbf13ac049c422f14a078ca9f97120a8b17dabb74280b1211805bd6bf8c'
+  version '32.0.0.270'
+  sha256 'cff53930d6534c6aff439dfce59fc546cccc53c85d352e72675b73e3b10f289c'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pep.xml',
