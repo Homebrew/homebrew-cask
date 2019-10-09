4aa0c2427b37d993f9f6fe4efcc34a6fc3f2a170
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-09 02:05:33 -0700

    Update youku from 1.7.5.08231 to 1.7.6.10083 (#70405)

--- Casks/youku.rb
+++ Casks/youku.rb
@@ -1,6 +1,6 @@
 cask 'youku' do
-  version '1.7.5.08231'
-  sha256 'f6fb1b219193745c7e9358906710cbabd19f8a8d218ad0319bbfa28a265927d0'
+  version '1.7.6.10083'
+  sha256 'c8eebc1a17a456e50388bdd0902e5801f96304e577493da2ce7b94c321f0b8e9'
 
   url "https://pcclient.download.youku.com/ikumac/youkumac_#{version}.dmg"
   appcast 'https://pd.youku.com/pc'
