9886e380b5c3312d33ba0be9fd5e5d963dae40eb
Randall <ran.dall@icloud.com>
2019-10-09 00:18:53 -0700

    Update thingsmacsandboxhelper from 3.16 to 3.18

--- Casks/thingsmacsandboxhelper.rb
+++ Casks/thingsmacsandboxhelper.rb
@@ -1,6 +1,6 @@
 cask 'thingsmacsandboxhelper' do
-  version '3.16'
-  sha256 '8b9b19ffb406823124bdf60acd0107764d9f5a3e8a98c71f0bd6feb06b38b5d8'
+  version '3.18'
+  sha256 '157c23c55c08dd14b244ee075e3d89684b7359e284198590ba8b091512ed7fb6'
 
   # culturedcode.cachefly.net was verified as official when first introduced to the cask
   url "https://culturedcode.cachefly.net/things/thingssandboxhelper/#{version}/ThingsHelper.zip"
