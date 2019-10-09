3aa96760def78dfd95d1479ca2d134579edf2f07
Randall <ran.dall@icloud.com>
2019-10-09 00:01:45 -0700

    Update readandwrite from 7.1.1 to 7.1.2

--- Casks/readandwrite.rb
+++ Casks/readandwrite.rb
@@ -1,6 +1,6 @@
 cask 'readandwrite' do
-  version '7.1.1'
-  sha256 'e67973d5f50f33deaf8c94033c93d566c7182eb69efcc2f51a585076bc6b49af'
+  version '7.1.2'
+  sha256 '87ee079e8464ec443302d1ad900b5249080b2674dbc9fabc72ce97f40b89cb79'
 
   url "https://fastdownloads2.texthelp.com/readwritemac#{version.major}/updates/live/readwrite_#{version}.zip"
   appcast "https://readwrite.texthelp.com/rw#{version.major}mac/updatesp/appcast.xml"
