b73185cf8ecf51f7c3d4a28712f635fcd604445c
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-09 00:35:00 -0700

    Update mate-translate from 6.0.2,1555331041 to 6.1.0,1570524082 (#70371)

--- Casks/mate-translate.rb
+++ Casks/mate-translate.rb
@@ -1,6 +1,6 @@
 cask 'mate-translate' do
-  version '6.0.2,1555331041'
-  sha256 '71fe9c4285faf55da7ab0456a1c20f6426cb8561bef4b0102d7df281abc4c65e'
+  version '6.1.0,1570524082'
+  sha256 '3a346f7fe69726152863b639ea83f72720ab6dff2d1a14fb0b1549595ac4f02f'
 
   # dl.devmate.com/com.twopeoplesoftware.InstantTranslate-nomas was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.twopeoplesoftware.InstantTranslate-nomas/#{version.before_comma}/#{version.after_comma}/InstantTranslate-#{version.before_comma}.zip"
