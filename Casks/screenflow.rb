4a2593ac92b7b7c076b839e37a96e41aa129385d
Nick Mispoulier <nick@proclame.be>
2019-10-09 19:34:58 +0200

    Update screenflow (#70440)

--- Casks/screenflow.rb
+++ Casks/screenflow.rb
@@ -1,6 +1,6 @@
 cask 'screenflow' do
   version '8.2.4'
-  sha256 'cae68e858b4f5eef3b8dd6d5d56c3f265d1c24080094f8d88a9b36e44769446a'
+  sha256 '81f10b342e7f894802bc8af5e260dbfd690706f129cacf916f2914af081e7719'
 
   url "https://www.telestream.net/download-files/screenflow/#{version.major_minor.dots_to_hyphens}/ScreenFlow-#{version}.dmg"
   appcast 'https://www.telestream.net/updater/screenflow/appcast.xml'
