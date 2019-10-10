0737c4a4d38912489ecc155e993f6d62243b17a9
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-09 19:20:32 -0700

    Update simpholders from 3.0.6,2245 to 3.0.7,2269 (#70487)

--- Casks/simpholders.rb
+++ Casks/simpholders.rb
@@ -1,6 +1,6 @@
 cask 'simpholders' do
-  version '3.0.6,2245'
-  sha256 'bb69e1f185d17edbb24e3b1f1f2981e2344388c478308d75a2b1c129246cddd8'
+  version '3.0.7,2269'
+  sha256 '30a146ab6999a58a44a1e3d910caf9a7afe68c2ce1ac5a4cf8af21ddd0909dcb'
 
   url "https://simpholders.com/site/assets/files/#{version.after_comma}/simpholders_#{version.before_comma.dots_to_underscores}.dmg"
   appcast 'https://simpholders.com/releases/'
