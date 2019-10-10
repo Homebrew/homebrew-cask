9490b86ce89085a1ed55d0d741d02ec41d98b9e7
CoreCode <core-code@users.noreply.github.com>
2019-10-10 15:39:17 +0200

    Update pages-data-merge.rb

--- Casks/pages-data-merge.rb
+++ Casks/pages-data-merge.rb
@@ -2,7 +2,7 @@ cask 'pages-data-merge' do
   version :latest
   sha256 :no_check
 
-  url 'https://iworkautomation.com/pages/PagesDataMergeApp.zip',
+  url 'https://iworkautomation.com/pages/PagesDataMergeAppMojave.zip',
       user_agent: :fake
   name 'Pages Data Merge'
   homepage 'https://iworkautomation.com/pages/script-tags-data-merge.html'
