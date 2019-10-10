6e2836beb3f38b3c229626cce3fcdf52d56ae0b6
Vítor Galvão <info@vitorgalvao.com>
2019-10-10 14:44:33 +0100

    Update onepile.rb

--- Casks/onepile.rb
+++ Casks/onepile.rb
@@ -2,7 +2,7 @@ cask 'onepile' do
   version '2.0-1092'
   sha256 '8deb9355b80cc8ddc14797db98bdb917bae07fb2c92d785e797e19f6a8a30b32'
 
-  url 'https://onepile.app/update/macos/OnePile-2.0-1092.zip'
+  url "https://onepile.app/update/macos/OnePile-{version}.zip"
   appcast 'https://onepile.app/sparklecast.xml'
   name 'OnePile'
   homepage 'https://onepile.app/'
