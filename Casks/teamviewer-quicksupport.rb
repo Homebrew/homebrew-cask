755e78a7e80f40d6d770677baa66abf88f398d99
Daniel Ashley <dashley85@gmail.com>
2019-10-10 12:02:03 -0400

    Update teamviewer-quicksupport from latest to 13.2.75535

--- Casks/teamviewer-quicksupport.rb
+++ Casks/teamviewer-quicksupport.rb
@@ -1,8 +1,8 @@
 cask 'teamviewer-quicksupport' do
-  version :latest
-  sha256 :no_check
+  version '13.2.75535'
+  sha256 'd98eb2563fd727afcf3bead42d9c1750da45a327f1ed259d8bac6de42b259572'
 
-  url 'https://download.teamviewer.com/download/TeamViewerQS.dmg'
+  url 'https://download.teamviewer.com/download/version_13x/TeamViewerQS.dmg'
   name 'TeamViewer QuickSupport'
   name 'TeamViewer QS'
   homepage 'https://www.teamviewer.com/'
