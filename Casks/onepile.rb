f201d90050796e18c8302a5206d4880675ecedc9
Andre Voigtmann <andre.voigtmann@sap.com>
2019-10-10 08:54:59 +0200

    add OnePile.app :latest

---
+++ Casks/onepile.rb
@@ -0,0 +1,10 @@
+cask 'onepile' do
+  version ':latest'
+  sha256 :no_check
+
+  url 'https://onepile.app/download-macos'
+  name 'OnePile'
+  homepage 'https://onepile.app/'
+
+  app 'OnePile.app'
+end
