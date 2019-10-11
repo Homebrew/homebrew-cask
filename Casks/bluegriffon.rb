eb0088c5b6f85f0a13172df1a653a1a1e9516068
suschizu <brigitte@corecode.io>
2019-10-11 13:44:55 +0200

    Revert bluegriffon.rb from 3.1 to 3.0.1 (#70543)

--- Casks/bluegriffon.rb
+++ Casks/bluegriffon.rb
@@ -1,6 +1,6 @@
 cask 'bluegriffon' do
-  version '3.1'
-  sha256 '651f0c089adfbb988870b09f0101ca021de218af4bb138011f196c4630312412'
+  version '3.0.1'
+  sha256 'c62f0af91ee7544c3f83f7a598638dad5e344ef6ce743e18b7d337a37e359580'
 
   url "http://bluegriffon.org/freshmeat/#{version}/bluegriffon-#{version}.mac-x86_64.dmg"
   appcast 'http://bluegriffon.org/freshmeat/?C=M;O=D'
