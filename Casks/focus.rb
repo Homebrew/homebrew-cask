96be42f56f716145ce809de6f9ac840577bd1070
gottabeleGIT <sandesh.manik@icloud.com>
2019-10-11 12:16:39 -0500

    Update focus.rb

--- Casks/focus.rb
+++ Casks/focus.rb
@@ -2,7 +2,7 @@ cask 'focus' do
   version '1.11'
   sha256 '4be805d35fc57d24debc0e4d0fd36b74d08c2f93d26c9daea7c8ac45019cb345'
 
-  url "https://heyfocus.com/uploads/Focus-#{version}.zip"
+  url "https://heyfocus.com/uploads/Focus-v#{version}.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://heyfocus.com/focus.zip'
   name 'Focus'
   homepage 'https://heyfocus.com/'
