a5be3d35a4add68f5c3e9792f61e70344c7b2484
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-10 20:35:24 -0700

    Update ableton-live-lite from 10.1.1 to 10.1.2 (#70583)

--- Casks/ableton-live-lite.rb
+++ Casks/ableton-live-lite.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-lite' do
-  version '10.1.1'
-  sha256 '4fe9ded48ce12b370cc3b4225ab947c3ef7cabf7ff97f07d7a4af6a1e5cf0c5d'
+  version '10.1.2'
+  sha256 '67a4d70d4ce3b3d2de5499c40cddece8d8641b9ca105b11cd4a3734de5f2f1f9'
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_lite_#{version}_64.dmg"
   appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
