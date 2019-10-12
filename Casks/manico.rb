1387b761ea69aa3da417fbed454573c4246b8f99
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-11 18:51:39 -0700

    Update manico from 2.6 to 2.6.1 (#70635)

--- Casks/manico.rb
+++ Casks/manico.rb
@@ -1,6 +1,6 @@
 cask 'manico' do
-  version '2.6'
-  sha256 '9caca1926989bcebe7b768923a69b990ce48b92122bcea6029a6b8adfa63b4d5'
+  version '2.6.1'
+  sha256 '78b6a79357a5a505d564654ccdf43b51332ac0427d61ef5485b0004430ac6f78'
 
   url "https://manico.im/static/Manico_#{version}.dmg"
   appcast 'https://manico.im/static/manico-official-appcast.xml'
