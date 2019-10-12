9e18d3c1ed887d5dfbf2c9b287835978b88b6140
Johan van Eck <johveck@gmail.com>
2019-10-13 00:50:49 +0200

    Add Magic comment to please the linter

--- Casks/free-ruler.rb
+++ Casks/free-ruler.rb
@@ -2,6 +2,7 @@ cask 'free-ruler' do
   version '2.0.1'
   sha256 '0ec0b4d62b7b9be17412d00d3eb045e4f574d6b0e1f725ad569740a983249685'
 
+  # github.com/pascalpp/FreeRuler was verified as official when first introduced to the cask
   url "https://github.com/pascalpp/FreeRuler/releases/download/v#{version}/free-ruler-#{version}.zip"
   appcast 'http://www.pascal.com/software/freeruler/'
   name 'Free Ruler'
