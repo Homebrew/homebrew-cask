4abaf3a053918ff478c271866e1d5b123c314fae
Randall <ran.dall@icloud.com>
2019-10-11 18:22:22 -0700

    Update piezo from 1.6.1 to 1.6.2

--- Casks/piezo.rb
+++ Casks/piezo.rb
@@ -1,6 +1,6 @@
 cask 'piezo' do
-  version '1.6.1'
-  sha256 '36a264ae1b68cf2c73bcaff1fd1f7af52cafc31210ac8a03029faa17e5394db5'
+  version '1.6.2'
+  sha256 'd27f3baa4200576f7405f3832bbdfffbba58b38c25a0288a675bf6a221d6e079'
 
   url 'https://rogueamoeba.com/piezo/download/Piezo.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Piezo&system=10146'
