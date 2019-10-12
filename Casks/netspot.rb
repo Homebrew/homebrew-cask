12eaf3f830bce0fb8d2e7d2d27b0fbf8da7d1c00
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-11 19:09:01 -0700

    Update netspot from 2.11.970 to 2.11.972 (#70638)

--- Casks/netspot.rb
+++ Casks/netspot.rb
@@ -1,6 +1,6 @@
 cask 'netspot' do
-  version '2.11.970'
-  sha256 '280752f1939fc7d43010c270fa289678c95356b2275d0ab767ac32ddf89d08d2'
+  version '2.11.972'
+  sha256 'c0a6b63839001720a4d77b7123f9c9df065adb53bd3c6895933770bec8efa2b2'
 
   url 'https://cdn.netspotapp.com/download/NetSpot.dmg'
   appcast "https://www.netspotapp.com/updates/netspot#{version.major}-appcast.xml"
