bca91a37c1752ca69f8e88cdea131c865b98a2f8
Johan <johman10@users.noreply.github.com>
2019-10-12 22:28:49 +0200

    Update free-ruler.app to version 2.0

--- Casks/free-ruler.rb
+++ Casks/free-ruler.rb
@@ -1,8 +1,8 @@
 cask 'free-ruler' do
-  version '1.7b5'
-  sha256 'f746e49ae012bee5ccef9871592276de47d3b6affbe92ab3f45f2591cb05b915'
+  version '2.0.1'
+  sha256 '0ec0b4d62b7b9be17412d00d3eb045e4f574d6b0e1f725ad569740a983249685'
 
-  url "http://www.pascal.com/software/freeruler/FreeRuler#{version}.zip"
+  url "https://github.com/pascalpp/FreeRuler/releases/download/v#{version}/free-ruler-#{version}.zip"
   appcast 'http://www.pascal.com/software/freeruler/'
   name 'Free Ruler'
   homepage 'http://www.pascal.com/software/freeruler/'
