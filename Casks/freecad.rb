b3dccf68fe1cc402e1b6d8d057a883effd9c0cde
Kuangye Chen <chen.kuangye@gmail.com>
2019-10-12 16:49:12 -0700

    Update freecad.rb (#70685)
    SHA256 changed. Seems it has changed multiple times according to the commits history. Upgrading a cask to pre-release version is a bad idea.

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,6 +1,6 @@
 cask 'freecad' do
   version '0.19_pre,18403'
-  sha256 'fbb3a595b503ec8f1544267e309683f89124b76757159a293762316d963f88dd'
+  sha256 '2e44c55fca7ed794080f9825c7a8a66f7d9c15a24f553a0dda6cef6df916ce2d'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg"
