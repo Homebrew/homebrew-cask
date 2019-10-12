3b7e4020367aee2bbcc88d588455f549b79c4dfa
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-12 15:26:30 -0700

    Update elan-avfx.rb

--- Casks/elan-avfx.rb
+++ Casks/elan-avfx.rb
@@ -1,6 +1,6 @@
 cask 'elan-avfx' do
   version '5.8'
-  sha256 '35e89961de320dc0db5812c4ea1cd65be77a4667cb03bb7bc60603770edf70aa'
+  sha256 '91153961b6ba6ad148bd5021967568e7dcbc2416b709e0c8681163e89f270d7d'
 
   url "https://www.mpi.nl/tools/elan/ELAN_#{version.dots_to_hyphens}_mac.zip"
   appcast 'https://tla.mpi.nl/tools/tla-tools/elan/release-notes/'
