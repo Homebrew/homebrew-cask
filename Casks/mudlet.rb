66739c4503139b53c6419b62b71bba6174838757
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-13 14:07:12 -0700

    Update mudlet from 4.1.2 to 4.2.0 (#70734)

--- Casks/mudlet.rb
+++ Casks/mudlet.rb
@@ -1,6 +1,6 @@
 cask 'mudlet' do
-  version '4.1.2'
-  sha256 '469da0feffb98059f17b20f4fa37401914647d33c0d313d0f4b8d836cf754f5b'
+  version '4.2.0'
+  sha256 '615fab9473cc0752bac0dfae33e29f80ce8ba9d74fc28275aa0c24906a1b67ff'
 
   url "https://www.mudlet.org/wp-content/files/Mudlet-#{version}.dmg"
   appcast 'https://github.com/Mudlet/Mudlet/releases.atom'
