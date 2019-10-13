5fa00dd9b81d097b1e51dd13dd1195ccbab0e6b5
Vítor Galvão <info@vitorgalvao.com>
2019-10-13 21:57:24 +0100

    Update vanilla.rb

--- Casks/vanilla.rb
+++ Casks/vanilla.rb
@@ -2,7 +2,6 @@ cask 'vanilla' do
   version '1.2.2,40'
   sha256 '6ed84c99dde08aa95ddc4cf4e68d5091a70212fc817b216f7793ece767e9c751'
 
-  # devmate.com/net.matthewpalmer.Vanilla was verified as official when first introduced to the cask
   url "https://macrelease.matthewpalmer.net/distribution/appcasts/Vanilla-#{version.after_comma}.dmg"
   appcast 'https://updates.devmate.com/net.matthewpalmer.Vanilla.xml'
   name 'Vanilla'
