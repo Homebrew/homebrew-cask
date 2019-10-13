5ea55d63add1a242a556f41eac813bad4cedf6b2
Micah Lee <micah@micahflee.com>
2019-10-13 12:54:46 -0700

    OnionShare 2.2

--- Casks/onionshare.rb
+++ Casks/onionshare.rb
@@ -1,6 +1,6 @@
 cask 'onionshare' do
-  version '2.1'
-  sha256 '5f1d861eeb1422f592b388b200286704c9d94288a20c146164ebed57fef6b077'
+  version '2.2'
+  sha256 '0a14978214419639e1265754fb9e8f8eaff0e43fed48deadd7ec46e0be54a7ea'
 
   url "https://onionshare.org/dist/#{version}/OnionShare-#{version}.pkg"
   appcast 'https://github.com/micahflee/onionshare/releases.atom'
