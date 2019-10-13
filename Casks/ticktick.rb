b23d4813c904c1dc767c6a19d94a49945d1b5cc2
Randall <ran.dall@icloud.com>
2019-10-12 20:13:27 -0700

    Update ticktick from 3.2.10,110 to 3.2.50,112

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -1,6 +1,6 @@
 cask 'ticktick' do
-  version '3.2.10,110'
-  sha256 '926a362e6b4e2ee9abe0c612c9bfe53e83c52c8d350fedf7a64d85e65d1706f2'
+  version '3.2.50,112'
+  sha256 'ac7154b0fcc53682c17ed392cfbc3f563f45217d7c22975f959df29299299421'
 
   # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
