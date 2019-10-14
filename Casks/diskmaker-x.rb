64703ee647541118fecb3f7236a49ff6788234cb
Patrick Baumgartner <contact@patbaumgartner.com>
2019-10-14 12:34:51 +0200

    Update diskmaker-x.rb

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -1,7 +1,7 @@
 cask 'diskmaker-x' do
   version '9.0.b1'
   sha256 '9946b75cafed3f9e71cbe09c45cac7db66a4a17a2bb61c76f6e006822d077d52'
-  
+
   url "https://diskmakerx.com/downloads/DiskMaker_X_#{version.no_dots}.dmg"
   appcast 'https://diskmakerx.com/feed/'
   name 'DiskMaker X'
