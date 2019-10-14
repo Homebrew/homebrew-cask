a366c3439f2ffd175afe8a3e70b9d3d69cd290ab
kaxing <kaxing@ymail.com>
2019-10-15 00:16:14 +0800

    Fix safe-network.rb cask style

--- Casks/safe-network.rb
+++ Casks/safe-network.rb
@@ -2,9 +2,9 @@ cask 'safe-network' do
   version 'v0.0.3'
   sha256 'be18701cf0670da843ea47f688b3a90167ad0733d1008992029b2fb54c5b95ec'
 
+  # github.com/maidsafe/safe-network-app was verified as official when first introduced to the cask
   url "https://github.com/maidsafe/safe-network-app/releases/download/#{version}/safe-network-app-#{version}-mac-x64.dmg"
   appcast 'https://github.com/maidsafe/safe-network-app/releases.atom'
-
   name 'SAFE Network App'
   homepage 'https://maidsafe.net/'
 
