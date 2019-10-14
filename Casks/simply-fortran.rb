ac29725fc2c78124401be0aa16b553653116be24
suschizu <brigitte@corecode.io>
2019-10-14 11:24:10 +0200

    Update simply-fortran.rb

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -1,9 +1,9 @@
 cask 'simply-fortran' do
-  version '3.5'
+  version '3.5.3103'
   sha256 'a848a36b62667dbb612a668a0e43e0e527baab4dc7340c1455320b4054bc4808'
 
   # download.approximatrix.com/simplyfortran was verified as official when first introduced to the cask
-  url "http://download.approximatrix.com/simplyfortran/#{version}/SimplyFortran-#{version}.dmg"
+  url "http://download.approximatrix.com/simplyfortran/#{version.major_minor}/SimplyFortran-#{version.major_minor}.dmg"
   appcast 'https://simplyfortran.com/download/?platform=macos'
   name 'Simply Fortran'
   homepage 'https://simplyfortran.com/'
