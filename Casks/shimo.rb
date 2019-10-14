c47dff882e7de670507af4c092fad5c954b81532
suschizu <brigitte@corecode.io>
2019-10-14 19:26:42 +0200

    Update shimo.rb (#70797)

--- Casks/shimo.rb
+++ Casks/shimo.rb
@@ -1,14 +1,15 @@
 cask 'shimo' do
-  version '4.1.5.1_8837'
-  sha256 'bd97b294936a84b28daa29df680e0e4e047b19c93f10c69c9d71a295737bf73e'
+  version '5.0_8874'
+  sha256 'de91af81d5ad9982f1dbfaf4b6cdebf98ccf423745e8b54ab216a1448179888f'
 
   # shimo.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://shimo.s3.amazonaws.com/Shimo_#{version}.zip"
-  appcast 'https://www.shimovpn.com/appcast.php'
+  appcast "https://www.shimovpn.com/appcast#{version.major}.php"
   name 'Shimo'
   homepage 'https://www.shimovpn.com/'
 
   auto_updates true
+  depends_on macos: '>= :high_sierra'
 
   app 'Shimo.app'
 end
