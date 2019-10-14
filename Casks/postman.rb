c4d340adcd219e65192f5f47a68cfe1489c7ca8d
Work <brigitte@corecode.io>
2019-10-14 11:06:05 +0200

    Update postman from 7.8.0 to 7.9.0

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,6 +1,6 @@
 cask 'postman' do
-  version '7.8.0'
-  sha256 '1bd4eee94fdbd0f95e2fa523b7359675799bc65583151f4c68ce9edb004f69a8'
+  version '7.9.0'
+  sha256 '51f3592432b7adff4e5b66e64093c4335b835393ca22ddc28e1dee2c9a2f292c'
 
   # dl.pstmn.io/download/version was verified as official when first introduced to the cask
   url "https://dl.pstmn.io/download/version/#{version}/osx64"
