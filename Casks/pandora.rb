8e42607c1fac337cfc0bf6645fee9b709c7704d0
Vítor Galvão <info@vitorgalvao.com>
2019-10-14 17:48:10 +0100

    Update pandora.rb

--- Casks/pandora.rb
+++ Casks/pandora.rb
@@ -1,18 +1,14 @@
 cask 'pandora' do
-  version :latest
-  sha256 :no_check
+  version '15.0.1'
+  sha256 '2342782b5b0effa9cf1a1f8e81f7e1a80d30dcfea6826bb3c50d3eaa22126be6'
 
-  url "https://pdora.co/desktop_mac_download"
-  #pdora.com was verified as official when first introduced to the cask (Pandora Media, Inc)
-  name 'Pandora Desktop'
+  # s3-us-west-2.amazonaws.com/p-desktop-app was verified as official when first introduced to the cask
+  url "https://s3-us-west-2.amazonaws.com/p-desktop-app/releases/Pandora-#{version}.dmg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://pdora.co/desktop_mac_download'
+  name 'Pandora'
   homepage 'https://www.pandora.com/desktop'
 
   app 'Pandora.app'
 
-  zap trash: [
-    '~/Library/Preferences/com.pandora.desktop.plist'
-
-  ]
+  zap trash: '~/Library/Preferences/com.pandora.desktop.plist'
 end
-
-
