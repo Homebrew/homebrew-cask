710b0d124c152d7129f083f125517ee14bc9afa1
Vítor Galvão <info@vitorgalvao.com>
2019-10-15 17:42:12 +0100

    Update bitrix24.rb

--- Casks/bitrix24.rb
+++ Casks/bitrix24.rb
@@ -2,7 +2,7 @@ cask 'bitrix24' do
   # note: "24" is not a version number, but an intrinsic part of the product name
   version '8.1.0.44'
   sha256 '4494265baf3a9927d929131274f7f75d1b05f9bb3c6f1a81ecb7882288a7ca7d'
-  
+
   url "https://dl.bitrix24.com/b24/bitrix24_desktop_#{version.dots_to_underscores}.dmg"
   appcast 'https://www.bitrix24.com/osx_version.php'
   name 'Bitrix24'
