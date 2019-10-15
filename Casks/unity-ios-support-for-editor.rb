9e797752e67123e445b6731a3a03bdd94651e4bc
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2019-10-15 13:28:04 +0200

    roll back homepages on unity packages

--- Casks/unity-ios-support-for-editor.rb
+++ Casks/unity-ios-support-for-editor.rb
@@ -5,7 +5,7 @@ cask 'unity-ios-support-for-editor' do
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
   name 'Unity iOS Build Support'
-  homepage 'https://unity3d.com/unity/whats-new/'
+  homepage 'https://unity3d.com/unity'
 
   depends_on cask: 'unity'
 
