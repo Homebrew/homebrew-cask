cbfbefafa77a24ffbddea8db5650ec514cb3e83b
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2019-10-15 13:30:58 +0200

    add / to homepage

--- Casks/unity-webgl-support-for-editor.rb
+++ Casks/unity-webgl-support-for-editor.rb
@@ -5,7 +5,7 @@ cask 'unity-webgl-support-for-editor' do
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
   name 'Unity WebGL Build Support'
-  homepage 'https://unity3d.com/unity'
+  homepage 'https://unity3d.com/unity/'
 
   depends_on cask: 'unity'
 
