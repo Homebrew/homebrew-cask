677cdb74a22b269f16b0bd6c456d37382f249dfc
Vítor Galvão <info@vitorgalvao.com>
2019-10-15 12:42:11 +0100

    Update flow-e.rb

--- Casks/flow-e.rb
+++ Casks/flow-e.rb
@@ -3,6 +3,7 @@ cask 'flow-e' do
   sha256 '01a041b20cb2e6490e64fea45a336e79c86bfa1b8a65d99448d63077864dbfc8'
 
   url "https://flow-e.com/resources/desktop_files/Flow-e-#{version}-mac.zip"
+  appcast 'https://flow-e.com/download-desktop'
   name 'Flow-e'
   homepage 'https://flow-e.com/'
 
