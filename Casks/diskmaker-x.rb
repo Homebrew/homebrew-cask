e94cb85da6a822ab343edc92c4e9f273f7f7c3df
Patrick Baumgartner <contact@patbaumgartner.com>
2019-10-15 21:03:39 +0200

    Adjusting app name

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -1,10 +1,12 @@
 cask 'diskmaker-x' do
-  if MacOS.version == :catalina
+  if MacOS.version <= :mojave
     sha256 '9946b75cafed3f9e71cbe09c45cac7db66a4a17a2bb61c76f6e006822d077d52'
     version '9.0.b1'
+    app "DiskMaker X #{version.major} for macOS Catalina.app"
   else
     sha256 '79b490dc829775450aafadeddd0afc58bdcef9c60fc82d9db1427c51b57e88a7'
     version '8.0.3'
+    app "DiskMaker X #{version.major} for macOS Mojave.app"
   end
 
   url "https://diskmakerx.com/downloads/DiskMaker_X_#{version.no_dots}.dmg"
@@ -12,5 +14,4 @@ cask 'diskmaker-x' do
   name 'DiskMaker X'
   homepage 'https://diskmakerx.com/'
 
-  app "DiskMaker X #{version.major} for macOS Mojave.app"
 end
