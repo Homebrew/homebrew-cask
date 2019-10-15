88b740abb1e1202df69be0616c818370521a3f5d
Work <brigitte@corecode.io>
2019-10-15 16:28:45 +0200

    Update bitrix24 from 7.0.95 to 8.1.0.44

--- Casks/bitrix24.rb
+++ Casks/bitrix24.rb
@@ -1,6 +1,6 @@
 cask 'bitrix24' do
   # note: "24" is not a version number, but an intrinsic part of the product name
-  version '7.0.95'
+  version '8.1.0.44'
   sha256 '7149942929882b1a00c7b65e652249807e0dc03c4078416778c0fe665584a3e7'
 
   url 'https://dl.bitrix24.com/b24/bitrix24_desktop.dmg'
