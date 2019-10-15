6656b17387c3b2eb4856062913623fc3841606f4
Ralf Weinbrecher <rweinbrecher@agfeo.de>
2019-10-15 16:06:11 +0200

    Add AGFEO Dashboard 1.0.2

---
+++ Casks/agfeo-dashboard.rb
@@ -0,0 +1,13 @@
+cask 'agfeo-dashboard' do
+  version '1.0.2'
+  sha256 '176f393ae2bb11e675ecd054849e0fbb49df89dda62831c8e7515110d2edca51'
+
+  url 'https://www2.agfeo.de/agfeo_web/dokulib.nsf/Anlage_w/E0B7AA5A7141C0B7C1258458002E496B/$FILE/AGFEO-Dashboard_x64_agfeo_1.0.2.dmg'
+  appcast 'https://www.agfeo.de/agfeo_web/hp3.nsf/start.xsp?c=4835&az1=prodsw'
+  name 'AGFEO Dashboard'
+  homepage 'https://www.agfeo.de/'
+
+  depends_on macos: '>= :sierra'
+
+  app 'AGFEO-Dashboard.app'
+end
