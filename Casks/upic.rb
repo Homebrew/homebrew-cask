a71d61a07f7d73aadb8913fc2ed804e10ed7fe2c
Svend <svend.jin@gmail.com>
2019-10-15 23:18:55 +0800

    update upic sha256 (#70866)

--- Casks/upic.rb
+++ Casks/upic.rb
@@ -1,6 +1,6 @@
 cask 'upic' do
   version '0.12.2'
-  sha256 '4f2db1dd778162b84b83bdd4a32e193f4e767efd8bdd7484d6bfb2bdac536d6c'
+  sha256 '8cc7d9c45c7079d4569c34c79ba0578571993f96cc64b0c17eb7b5d20e4d40d4'
 
   url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic.dmg"
   appcast 'https://github.com/gee1k/uPic/releases.atom'
