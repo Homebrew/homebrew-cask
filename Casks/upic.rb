a7f585f605de29b733eb6c0db0331b28af215de6
Svend <svend.jin@gmail.com>
2019-10-16 10:06:31 +0800

    update upic sha256 (#70888)
    sorry!
    
    Tortured by Sparkle

--- Casks/upic.rb
+++ Casks/upic.rb
@@ -1,6 +1,6 @@
 cask 'upic' do
   version '0.12.2'
-  sha256 '8cc7d9c45c7079d4569c34c79ba0578571993f96cc64b0c17eb7b5d20e4d40d4'
+  sha256 '54a23044ebf17a66c629e7900731a36661273f91781c9c4d827a1653e2c36339'
 
   url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic.dmg"
   appcast 'https://github.com/gee1k/uPic/releases.atom'
