b2d925fe0fd586b5c836f00c9637c952719a7209
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-16 14:09:35 -0700

    Update kode54-cog.rb

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,5 +1,5 @@
 cask 'kode54-cog' do
-  version '0.089,0f93b0c7b'
+  version '0.08,0f93b0c7b'
   sha256 'e97b8b4bf60d1706acd683cce5211471916f6fff257b668bb348cd9a4f41ed38'
 
   # losno.co/cog was verified as official when first introduced to the cask
