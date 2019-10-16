2d2e02f2ab83f1eb7b0dfac5c3bcdd0d75fc0a51
Julian Mayer <julian@corecode.io>
2019-10-16 12:18:36 +0200

    Update powerword from 1.1.2,1.0.1 to 1.1.3,1.0.1

--- Casks/powerword.rb
+++ Casks/powerword.rb
@@ -1,6 +1,6 @@
 cask 'powerword' do
-  version '1.1.2,1.0.1'
-  sha256 '708007eeab118925f2d7828c95a0043e260069547b7e98ae7a3f780f5a296711'
+  version '1.1.3,1.0.1'
+  sha256 '2a5b45a6e7d02a80917e2594ee818ff3022c6df7f429af97429c6f98d2befd39'
 
   url "https://download.iciba.com/mac/mac#{version.after_comma}/PowerWord.dmg"
   name 'PowerWord'
