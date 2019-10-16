8e62eadde6f91cf502f1bbf5f57039e4e5cee861
CoreCode <core-code@users.noreply.github.com>
2019-10-16 12:47:19 +0200

    Update securityspy.rb

--- Casks/securityspy.rb
+++ Casks/securityspy.rb
@@ -3,7 +3,8 @@ cask 'securityspy' do
   sha256 '11fdcd72599f7deb49ef1e6d9273a96b313f83e6448418d942287e3d7b895905'
 
   url 'https://www.bensoftware.com/securityspy/SecuritySpy.dmg'
-  appcast 'https://www.bensoftware.com/securityspy/versionhistory.html'
+  appcast 'https://www.bensoftware.com/securityspy/versionhistory.html',
+          configuration: version.chomp('.0')
   name 'SecuritySpy'
   homepage 'https://www.bensoftware.com/securityspy/'
 
