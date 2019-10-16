922d8590d430e3d3ac7f8daf3f2f4b4811b8a6fa
Tuyen Mai <maivutuyen.91@gmail.com>
2019-10-16 22:56:27 +0700

    Update new build for version 2.0.1 (45)

--- Casks/openkey.rb
+++ Casks/openkey.rb
@@ -1,6 +1,6 @@
 cask 'openkey' do
   version '2.0.1'
-  sha256 'f84901f6103b757b0c3eec7ac4a3d1ae96bde521be9fbcf60c7d7d6b9af9e9c9'
+  sha256 'b1521d7782f70dd67810e02f74ec32feaffb43fc71f2b54f5f58d45aec647daf'
 
   url "https://github.com/tuyenvm/OpenKey/releases/download/#{version}/OpenKey#{version}.dmg"
   appcast 'https://github.com/tuyenvm/OpenKey/releases.atom'
