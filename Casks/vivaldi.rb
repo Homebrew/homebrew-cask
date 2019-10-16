48ddce3b8639e02526e417de46d1553dcccb88da
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-15 22:52:57 -0700

    Update vivaldi from 2.8.1664.43 to 2.8.1664.44 (#70905)

--- Casks/vivaldi.rb
+++ Casks/vivaldi.rb
@@ -1,6 +1,6 @@
 cask 'vivaldi' do
-  version '2.8.1664.43'
-  sha256 'd76ce2caf3047d9e86b413a6e6717419ce883ad64002f7fe6b12993eb1fe6072'
+  version '2.8.1664.44'
+  sha256 'e27080800d4844c97c4fe3707766e1b9c2a1f0a8e2dcecf1c5221d2142228124'
 
   url "https://downloads.vivaldi.com/stable/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/public/mac/appcast.xml'
