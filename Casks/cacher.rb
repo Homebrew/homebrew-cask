c394546005e32c8d8d0589f38b0f0fa2b8d1109c
Randall <ran.dall@icloud.com>
2019-10-15 22:47:12 -0700

    Update cacher from 2.21.2 to 2.21.3

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,6 +1,6 @@
 cask 'cacher' do
-  version '2.21.2'
-  sha256 '119271a5100af31341c8cb420c431d546ab1e6b74fea57247862e61c9fe211ff'
+  version '2.21.3'
+  sha256 '609d88fb5f611b8a41e8630692ee40db9c735c52abca4f8d11bcc3cfdfcb9f6d'
 
   # cacher-download.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
