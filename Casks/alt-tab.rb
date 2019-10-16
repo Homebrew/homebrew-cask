1a454d87207f3d3ecf47960579032e763449a49a
OranShuster <oranshuster@gmail.com>
2019-10-16 14:18:47 +0300

    Update alt-tab to 1.0.12 and add depends_on mojave

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,11 +1,13 @@
 cask 'alt-tab' do
-  version '1.0.11'
-  sha256 'ea686d83929bb043b0de9d776d93b07f21a2218f2b9b822342e47954b23b3580'
+  version '1.0.12'
+  sha256 '0785737a0ca5d4e2562754483da11a4ac1220effef38d78617e9aa375da6ffef'
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/alt-tab-macos.zip"
   appcast 'https://github.com/lwouis/alt-tab-macos/releases.atom'
   name 'alt-tab'
   homepage 'https://github.com/lwouis/alt-tab-macos'
+
+  depends_on macos: '>= :mojave'
 
   app 'alt-tab-macos.app'
 end
