981a565df7ba2d98cc9418b02fcc34fadca409f1
Randall <ran.dall@icloud.com>
2019-10-16 13:42:13 -0700

    Update kode54-cog from 0.08,1dceedaa5 to 0.089,0f93b0c7b

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,6 +1,6 @@
 cask 'kode54-cog' do
-  version '0.08,1dceedaa5'
-  sha256 '3054a006462ed797033eb7dfc02f928af8f6e39e93c5a032cee33f8cec301840'
+  version '0.089,0f93b0c7b'
+  sha256 'e97b8b4bf60d1706acd683cce5211471916f6fff257b668bb348cd9a4f41ed38'
 
   # losno.co/cog was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
