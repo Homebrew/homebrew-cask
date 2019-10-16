45916d48162a5f446bfff03aeee0532e88f8ec6e
CoreCode <core-code@users.noreply.github.com>
2019-10-16 12:27:02 +0200

    Update cuteclips.rb

--- Casks/cuteclips.rb
+++ Casks/cuteclips.rb
@@ -2,10 +2,10 @@ cask 'cuteclips' do
   version '3.1.23'
   sha256 'c2d34440fb84ae0ccf82bad8e5630a4d4d3d021202b7d0a6e91486f6c24a5122'
 
-  url "http://cuteclips3.com/builds/CuteClips#{version.major}-#{version}.dmg"
-  appcast 'http://cuteclips3.com/'
+  url "https://cuteclips3.com/builds/CuteClips#{version.major}-#{version}.dmg"
+  appcast 'https://cuteclips3.com/'
   name 'CuteClips'
-  homepage 'http://cuteclips3.com/'
+  homepage 'https://cuteclips3.com/'
 
   app "CuteClips#{version.major}.app"
 end
