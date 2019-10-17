5a0f7cb90d6bcce1459c95c9f15b7b5179850982
Thomas J. Faulkenberry <faulkenberry@tarleton.edu>
2019-10-17 18:19:59 -0500

    Update opensesame cask to version 3.2.8 (#71047)

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -1,9 +1,9 @@
 cask 'opensesame' do
-  version '3.2.6'
-  sha256 '90d07e4013c0f1835dcd944df1ef5ec97669a325713845a30c1da20892c23069'
+  version '3.2.8'
+  sha256 '1f660ce97518c904153ce8a50a57102166c8991f3ed65fb24f87ff69fb2cc0e8'
 
   # github.com/smathot/OpenSesame was verified as official when first introduced to the cask
-  url "https://github.com/smathot/OpenSesame/releases/download/release/#{version}/opensesame_#{version}-py2.7-macos-2.dmg"
+  url "https://github.com/smathot/OpenSesame/releases/download/release/#{version}/opensesame_#{version}-py2.7-macos-1.dmg"
   appcast 'https://github.com/smathot/OpenSesame/releases.atom'
   name 'OpenSesame'
   homepage 'https://osdoc.cogsci.nl/'
