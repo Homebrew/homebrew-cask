78a5436e13ccd4bd058447126eeeedc4ef6fe568
René Schünemann <rene.schuenemann@gmail.com>
2019-10-17 17:36:37 +0200

    Update sapmachine-jdk from 13 to 13.0.1 (#71034)

--- Casks/sapmachine-jdk.rb
+++ Casks/sapmachine-jdk.rb
@@ -1,6 +1,6 @@
 cask 'sapmachine-jdk' do
-  version '13'
-  sha256 'd4f85394cb02db999baa906dedf51897b7b99189cd4c44f298c305529ee0cb2a'
+  version '13.0.1'
+  sha256 '7b77d7eb6dd625fac75b857d6ead53f946fd905e73c463a24f8e3c7e93706a20'
 
   # github.com/SAP/SapMachine was verified as official when first introduced to the cask
   url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg"
