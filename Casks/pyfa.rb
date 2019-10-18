f9f8c9e6eb2664dc40e06f305cfd39ffd6fcac00
Randall <ran.dall@icloud.com>
2019-10-17 20:41:39 -0700

    Update pyfa from 2.12.1 to 2.13.0

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,6 +1,6 @@
 cask 'pyfa' do
-  version '2.12.1'
-  sha256 '1639953736df0f6de5bae779d22eaba906ccbb41eee951c5c25de9955d4a4a8b'
+  version '2.13.0'
+  sha256 'e83cc47b27e5fa0f26b21a50d0dd6161a03ae61dcd49514dfa82cccfea4038d1'
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom'
