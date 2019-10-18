936a2097e727c96e08dec614f76508e71a32b938
Dean Whillier <deanwhillier@users.noreply.github.com>
2019-10-17 20:10:07 -0400

    Update mattermost cask to v4.3.0 (#71050)

--- Casks/mattermost.rb
+++ Casks/mattermost.rb
@@ -1,6 +1,6 @@
 cask 'mattermost' do
-  version '4.2.3'
-  sha256 'bde028f596ac3bc20ecfd04693ffe497b7e9979babd3436c2438500afecfa732'
+  version '4.3.0'
+  sha256 '9dcf760e98edce0c5ac13fc4b2c5a10094ac3311456157b62c4064d1b00933a8'
 
   url "https://releases.mattermost.com/desktop/#{version}/mattermost-desktop-#{version}-mac.zip"
   appcast 'https://github.com/mattermost/desktop/releases.atom'
