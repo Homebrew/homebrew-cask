6e71d9da6c411c5d38983f1d71925417b2c0440b
Work <brigitte@corecode.io>
2019-10-18 18:43:55 +0200

    Update sunsama from 0.9.6 to 0.9.7

--- Casks/sunsama.rb
+++ Casks/sunsama.rb
@@ -1,6 +1,6 @@
 cask 'sunsama' do
-  version '0.9.6'
-  sha256 'c9e5c55dbd14aa533fdfe770f469de2bf448fc726d92826120b67446cdbac3e7'
+  version '0.9.7'
+  sha256 '71c501bfb8eb1557c8b4f620082f3e844aa95f47c2f406d1615a9619fd77573f'
 
   url "http://download.sunsama.com/desktop/Sunsama-#{version}.dmg"
   appcast 'https://app.sunsama.com/b56d1fbd218098348674bde87159e7c18804471c.js'
