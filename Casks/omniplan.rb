7534a00b05413d32c0a0c5b87ec451620e4f71c5
suschizu <brigitte@corecode.io>
2019-10-18 13:36:41 +0200

    Update omniplan.rb

--- Casks/omniplan.rb
+++ Casks/omniplan.rb
@@ -1,10 +1,5 @@
 cask 'omniplan' do
-  if MacOS.version <= :sierra
-    version '3.10.4'
-    sha256 'a30728e72ae970dbf37b2ef9942a6b54267aa3456288dcc1815f20b44667e9e5'
-
-    url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniPlan-#{version}.dmg"
-  elsif MacOS.version <= :high_sierra
+  if MacOS.version <= :high_sierra
     version '3.13'
     sha256 '82e0d7db2626d751f93f97d80dc032e4bc01bba1e05ea52c553e4771c8cfeec5'
 
@@ -12,14 +7,15 @@ cask 'omniplan' do
   else
     version '3.13.1'
     sha256 '631e4ef1a740b5bbc914fb67da007cbabef94c56a1ee21b53af1cc30ac07c72f'
+
+    url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniPlan-#{version}.dmg"
   end
 
-  url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniPlan-#{version}.dmg"
   appcast "https://update.omnigroup.com/appcast/com.omnigroup.OmniPlan#{version.major}"
   name 'OmniPlan'
   homepage 'https://www.omnigroup.com/omniplan/'
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :high_sierra'
 
   app 'OmniPlan.app'
 
