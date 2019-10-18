1dee872fff70a69daec467b31ca8cfba12a48029
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-17 20:21:03 -0700

    Update cd-to from 3.0 to 3.1 (#70896)

--- Casks/cd-to.rb
+++ Casks/cd-to.rb
@@ -1,6 +1,6 @@
 cask 'cd-to' do
-  version '3.0'
-  sha256 'ced82da81b9b3448280b5c465c6649c93a876895de0142a396efca489c7b59b6'
+  version '3.1'
+  sha256 '2871b5324210a04eb13b27aa69ab572215a6ac11a33ca96de8e8526b39f77113'
 
   url "https://github.com/jbtule/cdto/releases/download/v#{version}/cdto_#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/jbtule/cdto/releases.atom'
@@ -9,12 +9,12 @@ cask 'cd-to' do
 
   depends_on macos: '>= :mojave'
 
-  app 'cd to ....app'
+  app 'cd to.app'
 
   caveats <<~EOS
     To complete installation:
     1. Open the the Applications folder in finder.
-    2. Drag "cd to ....app" onto the Finder toolbar while holding down the command(⌘) and option(⌥) keys.
+    2. Drag "cd to.app" onto the Finder toolbar while holding down the command(⌘) and option(⌥) keys.
 
     To use, just click on the new button and instantly opens a new Terminal.app window.
   EOS
