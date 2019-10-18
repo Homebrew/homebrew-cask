27a64c7bfa2172832c9054cfc5827b4674925f9c
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-17 21:52:17 -0700

    Update supersync from 6.2 to 7.0 (#71074)

--- Casks/supersync.rb
+++ Casks/supersync.rb
@@ -1,6 +1,6 @@
 cask 'supersync' do
-  version '6.2'
-  sha256 '007efaad1d8de0703859a3677197f504cfd9faa2183ca01602c7dbb26e02766d'
+  version '7.0'
+  sha256 '9f88bdf6db4fd7f88d7c5796252422f8d5bc93ef4b6bd6a8e8eb441d2a9d9a6d'
 
   url "https://supersync.com/downloads/SuperSync_#{version}.dmg"
   appcast 'https://supersync.com/downloads.php'
