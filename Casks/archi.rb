940559d824ec77ea5adab780f1e61a90d1c59e7b
CoreCode <core-code@users.noreply.github.com>
2019-10-18 05:19:54 +0200

    Update archi (#70944)

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -2,7 +2,7 @@ cask 'archi' do
   version '4.5.1'
   sha256 '32c705fec35a3ff8384bb13da93545fbb1104f78488309c6a4851c0f963d68c3'
 
-  url "https://www.archimatetool.com/downloads/#{version}/Archi-Mac-#{version}.zip",
+  url "https://www.archimatetool.com/dl#{version.no_dots}/#{version}/Archi-Mac-#{version}.zip",
       referer: 'https://www.archimatetool.com/download/'
   appcast 'https://github.com/archimatetool/archi/releases.atom'
   name 'Archi'
