321522ee1b450f294e09ff38f694f63492412a25
suschizu <brigitte@corecode.io>
2019-10-18 12:41:24 +0200

    Update pdfelement.rb

--- Casks/pdfelement.rb
+++ Casks/pdfelement.rb
@@ -1,6 +1,6 @@
 cask 'pdfelement' do
   version '7.3.3,5237'
-  sha256 '863d07ea666278178845a31af31033abb56e47d95f701be660e0bf15c98b406a'
+  sha256 '395f977a674aa50db1e9f417bb163d24ae4769cae00c58427eecbf58d5729d41'
 
   url "http://download.wondershare.com/cbs_down/mac-pdfelement_full#{version.after_comma}.dmg"
   appcast 'https://cbs.wondershare.com/go.php?m=upgrade_info&pid=5237'
