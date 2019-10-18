2dd5b643b161279078f9cd66d38004fab6ffb247
Andrea Bontempelli <andrea.bontempelli@unitn.it>
2019-10-18 14:36:55 +0200

    Update zotero from 5.0.75 to 5.0.76

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask 'zotero' do
-  version '5.0.75'
-  sha256 'de71b6fd42560a13ac67a21f9087cd18caadd5785abf7fd347d84e7591cb7208'
+  version '5.0.76'
+  sha256 '5cfeab499f7e127fd3dccc717e197b82f59e1265ae7dbf1afb551a6961ea5510'
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast 'https://github.com/zotero/zotero/releases.atom'
