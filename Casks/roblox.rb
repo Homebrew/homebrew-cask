866f67797ad3f64df5a10d47ac62605d073eba21
Randall <ran.dall@icloud.com>
2019-10-17 21:03:50 -0700

    Update roblox from 0.405.0.348052,5236228c56b848cc to 0.406.0.351423,c9625b3036514c13

--- Casks/roblox.rb
+++ Casks/roblox.rb
@@ -1,6 +1,6 @@
 cask 'roblox' do
-  version '0.405.0.348052,5236228c56b848cc'
-  sha256 '2df0c41bbfdc08f0fc4ee75d7466588e435e76a9c11fd3c2ff794af2526dfa79'
+  version '0.406.0.351423,c9625b3036514c13'
+  sha256 '3f1d84114d0c819d1587426ad44079682efdecfe91bcec6c2b2dfc083020e090'
 
   # setup.rbxcdn.com was verified as official when first introduced to the cask
   url "https://setup.rbxcdn.com/mac/version-#{version.after_comma}-Roblox.dmg"
