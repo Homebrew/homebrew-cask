de876246aba04b5253fb8bb1e0f5a2f9ad50311e
tnirasawa <nirasawa@gmail.com>
2019-10-19 06:52:55 +0900

    Add ProFind.app v1.7.0

--- Casks/profind.rb
+++ Casks/profind.rb
@@ -3,7 +3,8 @@ cask 'profind' do
   sha256 '21391307ebfa022a90ff9fcd8706d88ef56999e5b5f3e68868e46e514da8fbab'
 
   url "https://www.zeroonetwenty.com/profind/downloads/ProFind#{version.no_dots}.dmg"
-  name ''
+  appcast 'https://www.zeroonetwenty.com/profind/downloads/ProFindVersionInfo.xml'
+  name 'ProFind'
   homepage 'https://www.zeroonetwenty.com/profind/'
 
   app 'ProFind.app'
