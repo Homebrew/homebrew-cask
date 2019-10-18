7d644a470e6fae00893d9e5f93f7c2f260bfc944
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2019-10-18 22:20:48 +0300

    Update fork from 1.0.84 to 1.0.85 (#71102)

--- Casks/fork.rb
+++ Casks/fork.rb
@@ -1,6 +1,6 @@
 cask 'fork' do
-  version '1.0.84'
-  sha256 '2fafa381b5672851a2395af1499e1b9cff86fca1e5dec3ea355193f28f706051'
+  version '1.0.85'
+  sha256 '7bbb3297043d269656e6cab345e201a57dfc2bb0e77e4536b8f0888d4df41d44'
 
   # forkapp.ams3.cdn.digitaloceanspaces.com/mac was verified as official when first introduced to the cask
   url "https://forkapp.ams3.cdn.digitaloceanspaces.com/mac/Fork-#{version}.dmg"
