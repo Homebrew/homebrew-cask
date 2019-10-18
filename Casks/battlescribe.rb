56a786963cb3adbb3f3e5bb35e59029cc1db92de
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-17 21:51:35 -0700

    Update battlescribe from 2.03.02 to 2.03.03 (#71075)

--- Casks/battlescribe.rb
+++ Casks/battlescribe.rb
@@ -1,6 +1,6 @@
 cask 'battlescribe' do
-  version '2.03.02'
-  sha256 'e5e1cebc3592ef8e02f1186292c3a91f58f159929078aabb95cea5695d5470e9'
+  version '2.03.03'
+  sha256 '62a6903aa5a9a45c34c269f5851e36e69a9115f11c869ddd1feb5a48ddc21e3e'
 
   url "https://battlescribe.net/files/BattleScribe_#{version}_Installer.dmg"
   appcast 'https://battlescribe.net/?tab=downloads'
