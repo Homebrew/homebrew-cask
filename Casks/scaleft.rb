1039e32cf976f4c20fd8fb7a98f17ad005e0eda0
Vítor Galvão <info@vitorgalvao.com>
2019-10-18 13:00:51 +0100

    Update scaleft.rb

--- Casks/scaleft.rb
+++ Casks/scaleft.rb
@@ -3,6 +3,7 @@ cask 'scaleft' do
   sha256 '3c8c26d38bcecd3324eedc5d23ca73ffadebdf52942b86b4b85a3b5f6bfde4ff'
 
   url "https://dist.scaleft.com/client-tools/mac/v#{version}/ScaleFT-#{version}.pkg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://dist.scaleft.com/client-tools/mac/latest/ScaleFT.pkg'
   name 'ScaleFT'
   homepage 'https://www.scaleft.com/docs/sft-osx/'
 
@@ -10,5 +11,5 @@ cask 'scaleft' do
 
   uninstall pkgutil: 'com.scaleft.ScaleFT'
 
-  zap trash: ['~/Library/Caches/com.scaleft.ScaleFT']
+  zap trash: '~/Library/Caches/com.scaleft.ScaleFT'
 end
