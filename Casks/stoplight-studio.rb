38dbf5cd0b143721145ad786cc00db1d6799db3b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-17 21:10:10 -0700

    Update stoplight-studio from 1.1.5 to 1.2.0 (#71056)

--- Casks/stoplight-studio.rb
+++ Casks/stoplight-studio.rb
@@ -1,6 +1,6 @@
 cask 'stoplight-studio' do
-  version '1.1.5'
-  sha256 '335c8ee0fdccae679a502d5e69382d957e735079186057f4d49c3473d756df15'
+  version '1.2.0'
+  sha256 'cc22d3f563df1f2c4cb94a8a206dfcd3d686e8fa743fca45512d87d8c1b23dec'
 
   # github.com/stoplightio/studio was verified as official when first introduced to the cask
   url "https://github.com/stoplightio/studio/releases/download/v#{version}/Stoplight-Studio-#{version}.dmg"
