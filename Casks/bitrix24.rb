47ed6fbdd67a73edc290366090eba9e9a9f2145c
suschizu <brigitte@corecode.io>
2019-10-18 20:57:20 +0200

    Update bitrix24.rb (#71101)

--- Casks/bitrix24.rb
+++ Casks/bitrix24.rb
@@ -3,7 +3,7 @@ cask 'bitrix24' do
   version '8.1.0.44'
   sha256 '4494265baf3a9927d929131274f7f75d1b05f9bb3c6f1a81ecb7882288a7ca7d'
 
-  url "https://dl.bitrix24.com/b24/bitrix24_desktop_#{version.dots_to_underscores}.dmg"
+  url 'https://dl.bitrix24.com/b24/bitrix24_desktop.dmg'
   appcast 'https://www.bitrix24.com/osx_version.php'
   name 'Bitrix24'
   homepage 'https://www.bitrix24.com/apps/mobile-and-desktop-apps.php#desktop_app'
