ebeddc947713e929dbf1e0302196e15d47fd5442
Vítor Galvão <info@vitorgalvao.com>
2019-10-18 12:19:07 +0100

    Update teamdrive.rb

--- Casks/teamdrive.rb
+++ Casks/teamdrive.rb
@@ -4,7 +4,8 @@ cask 'teamdrive' do
 
   # s3-eu-west-1.amazonaws.com/s3download.teamdrive.net was verified as official when first introduced to the cask
   url "https://s3-eu-west-1.amazonaws.com/s3download.teamdrive.net/#{version.major_minor}.#{version.split('.').last}/TMDR/mac-10.14.2/Install-TeamDrive-#{version}_TMDR.dmg"
-  appcast 'https://teamdrive.com/en/downloads/?utm_source=startingpage&utm_medium=cta&utm_campaign=download_en'
+  appcast 'https://teamdrive.com/en/downloads/',
+          configuration: version.major_minor_patch
   name 'TeamDrive'
   homepage 'https://www.teamdrive.com/'
 
