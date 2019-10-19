c343218f9477805b1e9ec188421e257b5fa4f059
Randall <ran.dall@icloud.com>
2019-10-18 19:00:31 -0700

    Update horos from 3.3.5 to 4.0.0

--- Casks/horos.rb
+++ Casks/horos.rb
@@ -6,8 +6,8 @@ cask 'horos' do
     version '2.0.2'
     sha256 '5cc1d6c71c8ae643b4df4fecee93dbe3cfacbcffef52001a76a7683a2725ac08'
   else
-    version '3.3.5'
-    sha256 'e99716ee2939fc16cbe06fd04dfc6945fa0dbbf713c9db9bd53189d473ff9544'
+    version '4.0.0'
+    sha256 'fc23124feefc181488a36482a1f63e34a50b02e2c85897c2027f850c5f0242bb'
   end
 
   url "https://horosproject.org/horos-content/Horos#{version}.dmg"
