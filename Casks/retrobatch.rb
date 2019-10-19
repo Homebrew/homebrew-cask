b9007d7deecbdb5dd380e5055664fbb9fbde1aa1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-18 19:07:13 -0700

    Update retrobatch from 1.2 to 1.3 (#71109)

--- Casks/retrobatch.rb
+++ Casks/retrobatch.rb
@@ -1,6 +1,6 @@
 cask 'retrobatch' do
-  version '1.2'
-  sha256 '55565c9c452ce3d96b63543243d12d60d0f55b60b19df9fb6958013f2e5f754b'
+  version '1.3'
+  sha256 '97c326a7c5051c8af59f209f1b53e28025744d1fd851ec1299b23e84c4c5e3b5'
 
   url 'https://flyingmeat.com/download/Retrobatch.zip'
   appcast "https://www.flyingmeat.com/download/retrobatch#{version.major}update.xml"
