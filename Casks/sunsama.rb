da5647295cc8525b5f74389d85f7f6688f9de383
Vítor Galvão <info@vitorgalvao.com>
2019-10-19 16:52:02 +0100

    Update sunsama.rb

--- Casks/sunsama.rb
+++ Casks/sunsama.rb
@@ -3,7 +3,7 @@ cask 'sunsama' do
   sha256 '71c501bfb8eb1557c8b4f620082f3e844aa95f47c2f406d1615a9619fd77573f'
 
   url "http://download.sunsama.com/desktop/Sunsama-#{version}.dmg"
-  appcast 'https://app.sunsama.com/b56d1fbd218098348674bde87159e7c18804471c.js'
+  appcast 'https://app.sunsama.com/e9dae4501ba1e3d28b108aedcd4cc45f3eed9b06.js'
   name 'Sunsama'
   homepage 'https://app.sunsama.com/desktop'
 
