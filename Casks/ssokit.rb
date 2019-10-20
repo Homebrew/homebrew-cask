fef182b83d993eeb58c4e94ca83738c204253326
saka <876038559@qq.com>
2019-10-20 22:44:22 +0800

    add a new cask,ssokit-- a tcp/udp tool for socket version 1.1.0

--- Casks/ssokit.rb
+++ Casks/ssokit.rb
@@ -2,7 +2,7 @@ cask 'ssokit' do
   version '1.1.0'
   sha256 'e3da08bae091e344bb733c822c6b02dfd1aa9c7cca8a05a74838d69ac5b60028'
 
-  url "https://raw.githubusercontent.com/rangaofei/SSokit-qmake/master/SSokit_1.1.0.dmg"
+  url 'https://github.com/rangaofei/SSokit-qmake/releases/download/1.1.0/SSokit_1.1.0.dmg'
   name 'SSoikt'
   homepage 'https://github.com/rangaofei/SSokit-qmake'
 
