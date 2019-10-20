8d4e8d098f77a007be416b01400411018dc62a4e
suschizu <brigitte@corecode.io>
2019-10-20 17:54:33 +0200

    Update flipper from 0.25.0 to 0.26.0 (#71174)

--- Casks/flipper.rb
+++ Casks/flipper.rb
@@ -1,6 +1,6 @@
 cask 'flipper' do
-  version '0.25.0'
-  sha256 '1d5d87ca3628d116970b6fc62f632fd6a32c8fab277052b9e3f98c8cc4989fd9'
+  version '0.26.0'
+  sha256 '4ac339d9c0f9e33a1a812171013afd0ba94c2ed3f733c57b59b272706c938ff6'
 
   # github.com/facebook/flipper was verified as official when first introduced to the cask
   url "https://github.com/facebook/flipper/releases/download/v#{version}/Flipper-mac.zip"
