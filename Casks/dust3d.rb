30e249e028da9d31a06dfbf88848530c8a3ef09e
Randall <ran.dall@icloud.com>
2019-10-20 12:23:19 -0700

    Update dust3d from 1.0.0-beta.24 to 1.0.0-beta.25

--- Casks/dust3d.rb
+++ Casks/dust3d.rb
@@ -1,6 +1,6 @@
 cask 'dust3d' do
-  version '1.0.0-beta.24'
-  sha256 '7d56d34edb3d02af05805317ac4285849d30adc8959a420aa5c88ffe73b22725'
+  version '1.0.0-beta.25'
+  sha256 '7c810b3889769b14a34d342a49d4aac67c2dd7b7b7b2d15cedac7b48635ffe26'
 
   # github.com/huxingyi/dust3d was verified as official when first introduced to the cask
   url "https://github.com/huxingyi/dust3d/releases/download/#{version}/dust3d-#{version}.dmg"
