2c034305337f0585e49ea0a02d6865389f0e11a9
tattali <theoattali@gmail.com>
2019-10-20 19:12:39 +0200

    Update qladdict to 1.2.5

--- Casks/qladdict.rb
+++ Casks/qladdict.rb
@@ -1,5 +1,5 @@
 cask 'qladdict' do
-  version '1.2.4'
+  version '1.2.5'
   sha256 '2af44d8392f75616c7852f37e627b674d561306328351331cb592ac93ea244cd'
 
   url "https://github.com/tattali/QLAddict/releases/download/#{version}/QLAddict.qlgenerator.#{version}.zip"
