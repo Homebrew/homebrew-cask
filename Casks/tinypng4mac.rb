e1540a58dcf41bde71af2919f51d25fde591a098
Licardo <1014660822@qq.com>
2019-10-21 00:28:35 +0800

    Update tinypng4mac.rb

--- Casks/tinypng4mac.rb
+++ Casks/tinypng4mac.rb
@@ -1,12 +1,11 @@
 cask 'tinypng4mac' do
   version '1.0.3'
   sha256 '723a9e0b63b69a5b3c0d20cbe2199ff5dea7ac06ae511ec827b6bb82cf5af112'
-
+  
   url "https://github.com/kyleduo/TinyPNG4Mac/releases/download/v#{version}/TinyPNG4Mac_1_0_3.zip"
   appcast 'https://github.com/kyleduo/TinyPNG4Mac/releases.atom'
   name 'TinyPNG4Mac'
   homepage 'https://github.com/kyleduo/TinyPNG4Mac'
-
+  
   app 'TinyPNG4Mac.app'
-
 end
