d7ac811fac91397d4b7a0d8bb4c538cc0e7bcb78
Kirk Klobe <kirk@habitaware.com>
2019-10-21 11:20:55 -0500

    Update segger-embedded-studio-for-arm from 4.22 to 4.30

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -1,6 +1,6 @@
 cask 'segger-embedded-studio-for-arm' do
-  version '4.22'
-  sha256 '0673272a8f22f8d63f0b37cc4663eda195edc6c02f47757e02a7e4a8e86e6841'
+  version '4.30'
+  sha256 '233903c44d09fa022c74fca1c5b48f4bea42787bb17514410827108116ece8bd'
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.segger.com/downloads/embedded-studio/EmbeddedStudio_ARM_Mac',
