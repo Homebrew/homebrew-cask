c0945d4f8792a24d09cd5a0576e784fb1a7078cd
Randall <ran.dall@icloud.com>
2019-10-21 14:13:33 -0700

    Update ishowu-instant from 1.2.11 to 1.3.0

--- Casks/ishowu-instant.rb
+++ Casks/ishowu-instant.rb
@@ -1,6 +1,6 @@
 cask 'ishowu-instant' do
-  version '1.2.11'
-  sha256 '7429c2d8a9c7ecdf67b2bb00dc1319836f2114281e6906f9432cb522f2b0765b'
+  version '1.3.0'
+  sha256 '6a17a8c2f127397841826b27dfcae9887c454d098ba951e07257635f5bb7ac15'
 
   url "https://www.shinywhitebox.com/downloads/instant/iShowU_Instant_#{version}.dmg"
   appcast 'https://www.shinywhitebox.com/store/appcast.php?p=14'
