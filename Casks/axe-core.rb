599864e93e4fc7375f971aaff579e2c063c33cf6
-k <slowdive@me.com>
2019-10-20 21:52:30 -0700

    Update Axe Core to 1.4.1 (#71206)

--- Casks/axe-core.rb
+++ Casks/axe-core.rb
@@ -1,6 +1,6 @@
 cask 'axe-core' do
-  version '1.4.0.2'
-  sha256 '70fb7ac30214ad6d66ae661c2dc5d8b520413d0aafd916d9065789e974c9f72b'
+  version '1.4.1'
+  sha256 '6d7e8a79f2990e36df9310e196a78711d6c10642d599095633df28309bf0d76c'
 
   url "https://github.com/axerunners/axe/releases/download/v#{version}/axecore-#{version}-osx-unsigned.dmg"
   appcast 'https://github.com/axerunners/axe/releases.atom'
