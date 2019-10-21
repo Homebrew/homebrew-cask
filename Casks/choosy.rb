67a2723fbd0460dc726b71588d87274606c996fd
Nick Wind-McJetters <nick@elearningexperts.net>
2019-10-21 09:57:22 -0400

    Update choosy from 1.3 to 2.0

--- Casks/choosy.rb
+++ Casks/choosy.rb
@@ -3,8 +3,8 @@ cask 'choosy' do
     version '1.1'
     sha256 'c6530d4e0dddbf47c6a8999bda8f3a5ef1857f4481b9325e56cfe00f05b2022c'
   else
-    version '1.3'
-    sha256 'cb1f40df11ac1b52354f4b81367462d2646a6d023c64bafe5022fcec52f796cd'
+    version '2.0'
+    sha256 'dcba0b48d742da6c6860858c81480ff8b08b7cdbaae10a348c3062afddc1cc73'
   end
 
   url "https://downloads.choosyosx.com/choosy_#{version}.zip"
