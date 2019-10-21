f697d3c5d8cb666465f60e26828c13c5323ef0d3
Patrick Baumgartner <contact@patbaumgartner.com>
2019-10-21 12:59:19 +0200

    Changed if statement as suggested

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -1,14 +1,14 @@
 cask 'diskmaker-x' do
   if MacOS.version <= :mojave
-    version '9.0.b1'
-    sha256 '9946b75cafed3f9e71cbe09c45cac7db66a4a17a2bb61c76f6e006822d077d52'
-
-    app "DiskMaker X #{version.major} for macOS Catalina.app"
-  else
     version '8.0.3'
     sha256 '79b490dc829775450aafadeddd0afc58bdcef9c60fc82d9db1427c51b57e88a7'
 
-    app "DiskMaker X #{version.major} for macOS Mojave.app"
+    app "DiskMaker X #{version.major} for macOS Mojave.app"p"
+  else
+    version '9.0.b1'
+    sha256 '9946b75cafed3f9e71cbe09c45cac7db66a4a17a2bb61c76f6e006822d077d52'
+
+    app "DiskMaker X #{version.major} for macOS Catalina.ap
   end
 
   url "https://diskmakerx.com/downloads/DiskMaker_X_#{version.no_dots}.dmg"
