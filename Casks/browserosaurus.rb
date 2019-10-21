e727787872748572e08c6fe96cc9627801c35c78
Andrey Osiyuk <osiyuk@protonmail.com>
2019-10-21 13:05:04 +0300

    Update browserosaurus from 6.5.1 to 6.5.2 (#71216)

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,6 +1,6 @@
 cask 'browserosaurus' do
-  version '6.5.1'
-  sha256 '27bca7220072673014e63d9bb66817bc3fbeb33fc67ba3795a55a771af924f1b'
+  version '6.5.2'
+  sha256 '2721aba87a8d66606ac7b56b8c40dbd81105612c86288eb956fe69670b13d976'
 
   # github.com/will-stone/browserosaurus/ was verified as official when first introduced to the cask
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg"
