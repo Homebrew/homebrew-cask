c201d2b2cf99e4ecdd4ecce1e9a5e353aa35d41d
Work <brigitte@corecode.io>
2019-10-22 13:51:05 +0200

    Update kode54-cog from 0.08,389a70155 to 0.08,242c0b947

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,6 +1,6 @@
 cask 'kode54-cog' do
-  version '0.08,389a70155'
-  sha256 'da0c0e2b071b81519e98a1bbe60b7b78d51c783df55d286a2ab278d87d1465a3'
+  version '0.08,242c0b947'
+  sha256 '62df972b224c295ad3f97da2e8df8f17387c9fdd79b493f3f9c322664239bc78'
 
   # losno.co/cog was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
