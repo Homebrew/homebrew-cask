63678244a404cdd119b5ec9c20e83f0eaf62c146
CoreCode <core-code@users.noreply.github.com>
2019-10-22 13:06:04 +0200

    Update diskmaker-x from 9.0.b1 to 9.0b1 (#71259)

--- Casks/diskmaker-x.rb
+++ Casks/diskmaker-x.rb
@@ -5,7 +5,7 @@ cask 'diskmaker-x' do
 
     app "DiskMaker X #{version.major} for macOS Mojave.app"
   else
-    version '9.0.b1'
+    version '9.0b1'
     sha256 '9946b75cafed3f9e71cbe09c45cac7db66a4a17a2bb61c76f6e006822d077d52'
 
     app "DiskMaker X #{version.major} for macOS Catalina.app"
