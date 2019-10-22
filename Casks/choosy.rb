0226cf7e17988706ced3bcf6f0700c722b665c39
Markus Reiter <me@reitermark.us>
2019-10-22 02:06:14 +0200

    Update choosy.rb

--- Casks/choosy.rb
+++ Casks/choosy.rb
@@ -2,7 +2,7 @@ cask 'choosy' do
   if MacOS.version <= :el_capitan
     version '1.1'
     sha256 'c6530d4e0dddbf47c6a8999bda8f3a5ef1857f4481b9325e56cfe00f05b2022c'
-  else if MacOS.version <= :high_sierra
+  elsif MacOS.version <= :high_sierra
     version '1.3'
     sha256 'cb1f40df11ac1b52354f4b81367462d2646a6d023c64bafe5022fcec52f796cd'
   else
