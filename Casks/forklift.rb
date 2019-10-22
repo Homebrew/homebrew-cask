e7b8e5b39477d8fe9e619864418ace8c1089b1fb
Norman Xie <nonohft@gmail.com>
2019-10-23 07:56:04 +0800

    Update forklift from 3.3.4 to 3.3.5

--- Casks/forklift.rb
+++ Casks/forklift.rb
@@ -1,6 +1,6 @@
 cask 'forklift' do
-  version '3.3.4'
-  sha256 'e69fc7f6436e6f86e1cf0a56c415a6e80bbff792c7bf94786c0d3dd3d13651fc'
+  version '3.3.5'
+  sha256 '5ee2fcb7ca72c2acefd54443e5863329e489b020c4c5adb5f1dc1743afe2a74a'
 
   url "https://download.binarynights.com/ForkLift#{version}.zip"
   appcast "https://updates.binarynights.com/ForkLift#{version.major}/update.xml"
