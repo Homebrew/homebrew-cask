dc69093e07227a05179654d2aff5e33ff9fe393c
suschizu <brigitte@corecode.io>
2019-10-22 16:40:00 +0200

    Update pencil.rb

--- Casks/pencil.rb
+++ Casks/pencil.rb
@@ -3,7 +3,8 @@ cask 'pencil' do
   sha256 '5369787a801a470179a86ba60407b33e647f826b1a8b001b62e4f81a6bb0629d'
 
   url "https://pencil.evolus.vn/dl/V#{version}/Pencil-#{version}.dmg"
-  appcast 'https://github.com/evolus/pencil/releases.atom'
+  appcast 'https://github.com/evolus/pencil/releases.atom',
+          configuration: version.major_minor_patch
   name 'Pencil'
   homepage 'https://pencil.evolus.vn/'
 
