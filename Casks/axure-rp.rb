03d154c857b79ffd516095ad9bf225351e5f2617
Work <brigitte@corecode.io>
2019-10-22 16:50:31 +0200

    Update axure-rp

--- Casks/axure-rp.rb
+++ Casks/axure-rp.rb
@@ -1,6 +1,6 @@
 cask 'axure-rp' do
   version '9.0.0.3665'
-  sha256 '7d0649d39bcf7bd3815f54d63ce71ec597d7c0c3400f04bfa27d51f4d672aa3c'
+  sha256 '005267356c3cae9f9ff79c24f09c804789b92f809e260d014a041867b089fb21'
 
   # axure.cachefly.net was verified as official when first introduced to the cask
   url 'https://axure.cachefly.net/AxureRP-Setup.dmg'
