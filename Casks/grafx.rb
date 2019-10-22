6a9baf93f3431b1a31de242f43ec0cdcb3f794de
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-22 10:18:10 -0700

    Update grafx.rb

--- Casks/grafx.rb
+++ Casks/grafx.rb
@@ -1,6 +1,6 @@
 cask 'grafx' do
   version '2.6.2492,41'
-  sha256 '37c22dcffe51ec6d0ee07369d4b85d489dcc60075eb69b66aceaca8dfe0e5b0a'
+  sha256 '31658ed8983ac3ed7778e2fd0b6dfc5e1abc4c1302c5bc77a4dc584cd7c7efb5'
 
   url "https://pulkomandy.tk/projects/GrafX#{version.major}/downloads/#{version.after_comma}"
   appcast "https://pulkomandy.tk/projects/GrafX#{version.major}/downloads"
