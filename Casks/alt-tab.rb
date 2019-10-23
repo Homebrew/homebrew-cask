da1da31d72e3c45f09ee7d0d96ba6da054379ba0
CoreCode <core-code@users.noreply.github.com>
2019-10-23 15:35:48 +0200

    Update alt-tab from 1.4.5 to 1.4.6 (#71328)

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,6 +1,6 @@
 cask 'alt-tab' do
-  version '1.4.5'
-  sha256 'a10be9f85ed2450b4e38c5f1e8773270f8c523e7fe12b153e81abfa2f431f1ba'
+  version '1.4.6'
+  sha256 '5dbb441bea27ac38ec38f246b31f5c18b63ceef1a4bcced6b08a6a8bef3ec1be'
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
   appcast 'https://github.com/lwouis/alt-tab-macos/releases.atom'
