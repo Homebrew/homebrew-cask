434424e3b30d529fe968643702a052084a3db677
Chris Varenhorst <varenc@gmail.com>
2019-10-22 17:50:44 -0700

    Update steermouse to 5.4.2 (#71322)

--- Casks/steermouse.rb
+++ Casks/steermouse.rb
@@ -1,6 +1,6 @@
 cask 'steermouse' do
-  version '5.4.1'
-  sha256 'f30f8aacb1a6c53056406f9b49ebe038e7b3e35ee22939734bc1d84033fd6d52'
+  version '5.4.2'
+  sha256 'd28f3cf57d5d37771d9e00f1acf229fdff0ac285e3048881f9a6e49488cdcbb4'
 
   url "https://plentycom.jp/ctrl/files_sm/SteerMouse#{version}.dmg"
   appcast 'https://plentycom.jp/en/steermouse/download.php'
