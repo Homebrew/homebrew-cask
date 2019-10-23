16bdd5414d51d102aa0c2a4db3a9e18a7f9191af
Markus Reiter <me@reitermark.us>
2019-10-23 19:35:29 +0200

    Update mockuuups-studio.rb

--- Casks/mockuuups-studio.rb
+++ Casks/mockuuups-studio.rb
@@ -1,6 +1,6 @@
 cask 'mockuuups-studio' do
   version '2.2.1'
-  sha256 '2d542ca9f9e162f20a6df2a39b26d62a8e9570968279a2960da61b68c7c26447'
+  sha256 '1ccdc2cb96b2aea8c320f7df7c2d322b0195a3c11c3d7a0916e24221349b9a55'
 
   # mockuuups.com was verified as official when first introduced to the cask
   url "https://binaries.mockuuups.com/Mockuuups%20Studio-#{version}-mac.zip"
