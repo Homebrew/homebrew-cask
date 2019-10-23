028ca29179e8921ef2caa4866f4c2d4bb23b7e40
suschizu <brigitte@corecode.io>
2019-10-23 16:52:12 +0200

    Update thedesk from 18.11.2 to 18.11.3 (#71333)

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask 'thedesk' do
-  version '18.11.2'
-  sha256 'dbff74ea12958a28f0ad4f1fdf2dddea73d4e903d16462ac20ea8c39df4ee3f6'
+  version '18.11.3'
+  sha256 '21dcf120ea7bd23d406a281e4ca4b45e010229f3f0730558b20151246f8cdc7e'
 
   # github.com/cutls/TheDesk was verified as official when first introduced to the cask
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}.dmg"
