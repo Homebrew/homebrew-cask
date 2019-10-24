e70a38dc15a9c5fb45d050538429babb5260c4a7
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-23 17:36:00 -0700

    Update wordpresscom from 4.4.0 to 4.4.1 (#71365)

--- Casks/wordpresscom.rb
+++ Casks/wordpresscom.rb
@@ -1,6 +1,6 @@
 cask 'wordpresscom' do
-  version '4.4.0'
-  sha256 'ccea3f3701a55c0520ca756cb8537f92400539268a20b5295ec44abed5078638'
+  version '4.4.1'
+  sha256 '6f86aa1301ff725e5d44ad0267d70acfe199c8728e33ebf471bb75de5c707421'
 
   url "https://public-api.wordpress.com/rest/v1.1/desktop/osx/download?type=app&ref=update&version=#{version}"
   appcast 'https://public-api.wordpress.com/rest/v1.1/desktop/osx/version?compare=0.1.0&channel=stable'
