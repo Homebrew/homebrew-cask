c816f32525796b7ceb5f4e4f2a2a78749c4a7caa
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-24 08:11:18 -0700

    Update smart-converter-pro.rb

--- Casks/smart-converter-pro.rb
+++ Casks/smart-converter-pro.rb
@@ -2,7 +2,7 @@ cask 'smart-converter-pro' do
   version '2.4.2'
   sha256 '5dbde9f84974e301d0ee3fbbc61f117a8b75d22b1373c8de265fa626199d2822'
 
-  url "https://download.shedworx.com/scp2/SmartConverterPro-#{version}.dmg"
+  url "https://download.shedworx.com/scp#{version.major}/SmartConverterPro-#{version}.dmg"
   name 'Smart Converter Pro'
   homepage 'https://shedworx.com/smart-converter-pro'
 
