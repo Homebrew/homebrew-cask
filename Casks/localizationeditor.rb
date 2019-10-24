15bfbd190147c1fcfd7a4f473393476f390d8966
Licardo <1014660822@qq.com>
2019-10-24 20:31:52 +0800

    Update localizationeditor.rb

--- Casks/localizationeditor.rb
+++ Casks/localizationeditor.rb
@@ -1,6 +1,6 @@
 cask 'localizationeditor' do
   version '2.3'
-  sha256 :no_check
+  sha256 'a3b6914ec001effd1d74a8de28550dd475d3168f9487ef8d479503cedd60779c'
 
   url "https://github.com/igorkulman/iOSLocalizationEditor/releases/download/v#{version}/LocalizationEditor.app.zip"
   appcast 'https://github.com/igorkulman/iOSLocalizationEditor/releases.atom'
