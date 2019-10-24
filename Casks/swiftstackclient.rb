9b9a5e2c7f1d639e67e0723c462dadb43a3421b3
Ben <BenjaminHCCarr@users.noreply.github.com>
2019-10-24 08:51:11 -0400

    Add SwiftStack GUI Client
    Add new cask for SwiftStack client

---
+++ Casks/swiftstackclient.rb
@@ -0,0 +1,11 @@
+cask 'swiftstackclient' do
+  version '1.21.13'
+  sha256 '5532ffb0b0e0c13e9d9864a1e3ee6cd0d5a63debc3065f0e0c401d8712ade695'
+
+  # storage.googleapis.com/swiftstack was verified as official when first introduced to the cask
+  url "https://storage.googleapis.com/swiftstack/swiftstackclient-#{version}.dmg"
+  name 'Swift Stack GUI Client'
+  homepage 'https://www.swiftstack.com/'
+
+  app 'SwiftStack Client.app'
+end
