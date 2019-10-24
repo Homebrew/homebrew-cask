44780ab21694e25d3c0fb6756923dde7c002cea5
Randall <ran.dall@icloud.com>
2019-10-24 00:15:52 -0700

    Add DueFocus

---
+++ Casks/duefocus.rb
@@ -0,0 +1,16 @@
+cask 'duefocus' do
+  version '1.1.9'
+  sha256 'd0e5238abf2053b4f372bf0242dc34830865d297ca81abc371cdf2bc123864d5'
+
+  url "https://web.duefocus.com/distribution/darwin/v2/DueFocus-#{version}-mac.zip"
+  name 'DueFocus'
+  homepage 'https://duefocus.com/'
+
+  app 'DueFocus.app'
+
+  uninstall delete: [
+                      '~/Library/Application Support/DueFocus',
+                      '~/Library/Preferences/com.qarea.duefocus.plist',
+                      '~/Library/Logs/DueFocus',
+                    ]
+end
