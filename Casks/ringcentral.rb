6495e43269e6d480f47af8c8971913616d1a2663
Randall <ran.dall@icloud.com>
2019-10-24 17:44:01 -0700

    Update ringcentral from 19.3.2 to 19.3.4

--- Casks/ringcentral.rb
+++ Casks/ringcentral.rb
@@ -1,6 +1,6 @@
 cask 'ringcentral' do
-  version '19.3.2'
-  sha256 '4bb84b822082e0a8cbeac4f512fefebf9bcceb6b7e3565aa351eddfc354753a9'
+  version '19.3.4'
+  sha256 'd45d945fc9e6e723a989901ef9e4f7e33c9245b8e3b23d36b3623c2bc6fc1fc3'
 
   url "https://downloads.ringcentral.com/sp/RingCentralPhone-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://downloads.ringcentral.com/sp/RingCentralForMac'
