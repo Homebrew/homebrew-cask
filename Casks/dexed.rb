7f9674720e4d21ecbee3ba6562872e20ae05b52f
Vítor Galvão <info@vitorgalvao.com>
2019-10-25 15:56:34 +0100

    Update dexed.rb

--- Casks/dexed.rb
+++ Casks/dexed.rb
@@ -2,6 +2,7 @@ cask 'dexed' do
   version '0.9.4'
   sha256 '079eac6aa65de3ea19efbb43cadfa050a8c3978ab4ea79d4a660226cd4e0d5e3'
 
+  # github.com/asb2m10/dexed was verified as official when first introduced to the cask
   url "https://github.com/asb2m10/dexed/releases/download/v#{version}hf1/dexed-#{version}-osx.zip"
   appcast 'https://github.com/asb2m10/dexed/releases.atom'
   name 'Dexed'
