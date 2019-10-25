5d6d50032b59c9ef55ed384a6db052661f169dcd
Viktor Szakats <commit@vszakats.net>
2019-10-25 11:19:27 +0000

    secure (and fix) URLs

--- Casks/grisbi.rb
+++ Casks/grisbi.rb
@@ -3,10 +3,10 @@ cask 'grisbi' do
   sha256 '89df854060d0445713e7bd02f99f952bd0fa7286a7102d32b2c40609f433d1cd'
 
   # sourceforge.net/grisbi was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/grisbi/grisbi%20stable/#{version.major_minor}.x/Grisbi-#{version}.dmg"
+  url "https://downloads.sourceforge.net/grisbi/grisbi%20stable/#{version.major_minor}.x/#{version.major_minor_patch}/Grisbi-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/grisbi/rss?path=/grisbi%20stable'
   name 'Grisbi'
-  homepage 'http://www.grisbi.org/'
+  homepage 'https://www.grisbi.org/'
 
   app 'Grisbi.app'
 end
