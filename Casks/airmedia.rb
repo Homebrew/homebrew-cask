0d1c85c56e2113f7e1d2c14ba8e46514dc6f44b1
Markus Reiter <me@reitermark.us>
2019-10-25 15:25:24 +0200

    Update airmedia.rb

--- Casks/airmedia.rb
+++ Casks/airmedia.rb
@@ -2,7 +2,7 @@ cask 'airmedia' do
   version '3.1.29'
   sha256 '809d193a9f71494734be57b3bebc2912f284acd88f4bc408486ef6fb6951522d'
 
-  url "https://www.crestron.com/getmedia/751a3c59-a9a8-49b8-ac28-4989da4dc2e1/airmedia_osx_#{version}_guest"
+  url "https://www.crestron.com/Crestron/media/Crestron/WidenResources/Web%20Miscellaneous/airmedia_osx_#{version.dots_to_underscores}_guest.zip"
   appcast 'https://www.crestron.com/en-US/Products/Featured-Solutions/Airmedia'
   name 'Crestron AirMedia'
   homepage 'https://www.crestron.com/microsites/airmedia-mobile-wireless-hd-presentations'
