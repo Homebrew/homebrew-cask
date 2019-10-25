87cb5d6043dc1ddb56579a49f29155246d9afdc5
Vítor Galvão <info@vitorgalvao.com>
2019-10-25 16:08:43 +0100

    Update freac.rb

--- Casks/freac.rb
+++ Casks/freac.rb
@@ -3,7 +3,7 @@ cask 'freac' do
   sha256 'f6cbe820d0c8c114064b1282ea45e256a95df0c4433483043477cfe77c3627f5'
 
   # github.com/enzo1982/freac was verified as official when first introduced to the cask
-  url "https://github.com/enzo1982/freac/releases/download/v#{version.sub(/a$/, '')}/freac-#{version}-macosx.dmg"
+  url "https://github.com/enzo1982/freac/releases/download/v#{version.chomp('a')}/freac-#{version}-macosx.dmg"
   appcast 'https://github.com/enzo1982/freac/releases.atom'
   name 'fre:ac'
   homepage 'https://www.freac.org/'
