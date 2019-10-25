a0a2054ef7938acf5ea3c026858950af504da167
Juan Ignacio Donoso <jidonoso@gmail.com>
2019-10-24 21:22:32 -0300

    Add Dexed.app v0.9.4

---
+++ Casks/dexed.rb
@@ -0,0 +1,17 @@
+cask 'dexed' do
+  version '0.9.4'
+  sha256 '079eac6aa65de3ea19efbb43cadfa050a8c3978ab4ea79d4a660226cd4e0d5e3'
+
+  url "https://github.com/asb2m10/dexed/releases/download/v#{version}hf1/dexed-#{version}-osx.zip"
+  appcast 'https://github.com/asb2m10/dexed/releases.atom'
+  name 'Dexed'
+  homepage 'https://asb2m10.github.io/dexed/'
+
+  pkg "dexed-#{version}-osx.mpkg"
+
+  uninstall pkgutil: [
+                       'com.digitalsuburban.DexedStandalone',
+                       'com.digitalsuburban.DexedVST',
+                       'com.digitalsuburban.DexedAU',
+                     ]
+end
