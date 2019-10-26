f3d5d5eb60be391378c5f08e7ab0cd9513f62a23
Vítor Galvão <info@vitorgalvao.com>
2019-10-26 14:22:47 +0100

    Update grafx.rb

--- Casks/grafx.rb
+++ Casks/grafx.rb
@@ -3,7 +3,8 @@ cask 'grafx' do
   sha256 '31658ed8983ac3ed7778e2fd0b6dfc5e1abc4c1302c5bc77a4dc584cd7c7efb5'
 
   url "https://pulkomandy.tk/projects/GrafX#{version.major}/downloads/#{version.after_comma}"
-  appcast 'https://gitlab.com/GrafX2/grafX2/-/tags?format=atom'
+  appcast 'https://gitlab.com/GrafX2/grafX2/-/tags?format=atom',
+          configuration: version.major_minor
   name 'GrafX2'
   homepage "https://pulkomandy.tk/projects/GrafX#{version.major}"
 
