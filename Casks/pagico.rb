1be4eda9bb1366d266cee365c31ed0727d4c9be8
suschizu <brigitte@corecode.io>
2019-10-26 16:19:59 +0200

    Update pagico.rb

--- Casks/pagico.rb
+++ Casks/pagico.rb
@@ -1,8 +1,8 @@
 cask 'pagico' do
-  version '9.0,3050'
+  version '9.0.3050'
   sha256 '32c63b76a95464b72bdbeb6dcdeb7c12a47d1bb00eba9f625a844827cd499916'
 
-  url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.after_comma}.dmg"
+  url "https://www.pagico.com/downloads/Pagico_macOS_r#{version.patch}.dmg"
   appcast "https://www.pagico.com/api/pagico#{version.major}.mac.xml",
           configuration: version.patch
   name 'Pagico'
