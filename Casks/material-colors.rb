a4623de4f060fe5df934c9171f36fc7fef238118
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-26 22:53:22 -0700

    Update material-colors from 1.1.0 to 2.0.0 (#71536)

--- Casks/material-colors.rb
+++ Casks/material-colors.rb
@@ -1,6 +1,6 @@
 cask 'material-colors' do
-  version '1.1.0'
-  sha256 '61a23327f723f61b714b1718a7e30cc6f3c5bcee2b2103074354244a76e029d8'
+  version '2.0.0'
+  sha256 'a764f8af33499a96d46e3e79fe2e4594eb0ccff6217cff0920d9b44982a0f8be'
 
   url "https://github.com/romannurik/MaterialColorsApp/releases/download/v#{version}/MaterialColors-#{version}.zip"
   appcast 'https://github.com/romannurik/MaterialColorsApp/releases.atom'
