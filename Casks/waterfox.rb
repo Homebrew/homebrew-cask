0a3ee2281724cc4f3440692116ff296fbc43bbdd
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-27 03:14:52 -0700

    Update waterfox from 56.2.14 to 2019.10 (#71439)

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,14 +1,14 @@
 cask 'waterfox' do
-  version '56.2.14'
-  sha256 'c9df511b7a9474a123f2eba456332744e9bdd8ef0a9fae464441932a6f655bd0'
+  version '2019.10'
+  sha256 'ed30fba5843f041aec9cc5393f5049df05b5c8773423daf7c3806819cb505171'
 
   # storage-waterfox.netdna-ssl.com was verified as official when first introduced to the cask
-  url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20#{version}%20Setup.dmg"
+  url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20Classic%20#{version}%20Setup.dmg"
   appcast 'https://www.waterfox.net/releases/'
   name 'Waterfox'
   homepage 'https://www.waterfox.net/'
 
-  app 'Waterfox.app'
+  app 'Waterfox Classic.app'
 
   zap trash: [
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.mozilla.waterfox.sfl*',
