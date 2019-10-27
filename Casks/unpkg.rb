5110f966cfb025a74a89fe017fb96ef2b900b2d7
Work <brigitte@corecode.io>
2019-10-27 20:36:12 +0100

    Update unpkg from 4.5 to 4.6

--- Casks/unpkg.rb
+++ Casks/unpkg.rb
@@ -1,9 +1,9 @@
 cask 'unpkg' do
-  version '4.5'
-  sha256 'e32754b07073b383320d2bc0ce4ba9bd83a3f352043bd6f500741901f00c8c17'
+  version '4.6'
+  sha256 '6949b648d5cd02138f9e7f505f1fb3541905d6016ed951daa71cb7250c3ace2e'
 
   # github.com/downloads/timdoug/unpkg was verified as official when first introduced to the cask
-  url "https://github.com/downloads/timdoug/unpkg/unpkg-#{version}.zip"
+  url "https://www.timdoug.com/unpkg/unpkg-#{version}.zip"
   appcast 'https://github.com/timdoug/unpkg/releases.atom'
   name 'unpkg'
   homepage 'https://www.timdoug.com/unpkg/'
