830718147c2b1bdb3f073385c8d6bde7c23dd6e1
Randall <ran.dall@icloud.com>
2019-10-26 22:34:42 -0700

    Update veusz from 3.0.1 to 3.1

--- Casks/veusz.rb
+++ Casks/veusz.rb
@@ -1,6 +1,6 @@
 cask 'veusz' do
-  version '3.0.1'
-  sha256 '70fe6d61be9d49628422c7cb5177095fedbad95e04de7c9a398a22f77adaad3a'
+  version '3.1'
+  sha256 '468dd2af4dab24342b9180cdf5ea31d7119ba9db853eba3fabf8143be3f20695'
 
   # github.com/veusz/veusz was verified as official when first introduced to the cask
   url "https://github.com/veusz/veusz/releases/download/veusz-#{version}/veusz-#{version}-AppleOSX.dmg"
