900b28d2b54628ba95b5b6fc5942ed19416c94b6
Randall <ran.dall@icloud.com>
2019-10-27 19:15:07 -0700

    Update hands-off from 4.3.0 to 4.4.0

--- Casks/hands-off.rb
+++ Casks/hands-off.rb
@@ -1,6 +1,6 @@
 cask 'hands-off' do
-  version '4.3.0'
-  sha256 '836da734a43d5434b25fca893ed58363c79af288b2c7d57785cb132e6a534a2f'
+  version '4.4.0'
+  sha256 'a4aa07a33ca903723ecda64c6b28d7dcdee183a94a03fb2f86c38a53b94f40e2'
 
   url "https://www.oneperiodic.com/files/Hands%20Off!%20v#{version}.dmg"
   appcast "https://www.oneperiodic.com/handsoff#{version.major}.xml"
