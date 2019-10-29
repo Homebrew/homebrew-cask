06aaf076678d26d2f23ea39b2d30d7ade1064cf6
Amor <amor90s.zhang@gmail.com>
2019-10-29 09:19:38 +0800

    Update platelet.rb

--- Casks/platelet.rb
+++ Casks/platelet.rb
@@ -3,9 +3,9 @@ cask 'platelet' do
   sha256 'aba7f4239ba3f34cf7bf13926c85c7b41afb192dc6647594cdf55e1fd989e7f6'
 
   url "https://github.com/amorist/platelet/releases/download/v#{version}/platelet-#{version}.dmg"
+  appcast 'https://github.com/amorist/platelet/releases.atom'
   name 'Platelet'
   name 'けっしょうばん'
-  appcast 'https://github.com/amorist/platelet/releases.atom'
   homepage 'https://github.com/amorist/platelet'
 
   app 'platelet.app'
