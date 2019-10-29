11790ad7c516ae4d325c2e46cad6ff7fa167a6fe
Vítor Galvão <info@vitorgalvao.com>
2019-10-29 12:57:45 +0000

    Update luyten.rb

--- Casks/luyten.rb
+++ Casks/luyten.rb
@@ -1,9 +1,9 @@
 cask 'luyten' do
-  version '0.5.4,0.5.4_Rebuilt_with_Latest_depenencies'
+  version '0.5.4'
   sha256 '7f48f8b75991c1c039cfb901440fa05dd3cf18e2219e39cf19f86aaf551a097e'
 
   # github.com/deathmarine/Luyten was verified as official when first introduced to the cask
-  url "https://github.com/deathmarine/Luyten/releases/download/v#{version.after_comma}/luyten-OSX-#{version.before_comma}.zip"
+  url "https://github.com/deathmarine/Luyten/releases/download/v#{version}_Rebuilt_with_Latest_depenencies/luyten-OSX-#{version}.zip"
   appcast 'https://github.com/deathmarine/Luyten/releases.atom'
   name 'Luyten'
   homepage 'https://deathmarine.github.io/Luyten/'
