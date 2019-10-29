324678dc1dd42ef7e706337eb6a14db1ef97d40e
matthiasho <matthias.hortenhuber@ki.se>
2019-10-29 12:04:41 +0100

    Add Tweetbot.app Latest

---
+++ Casks/tweetbot.rb
@@ -0,0 +1,10 @@
+cask 'tweetbot' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://tapbots.net/tweetbot4/Tweetbot.dmg'
+  name 'Tweetbot'
+  homepage 'https://tapbots.net/tweetbot4'
+
+  app 'Tweetbot.app'
+end
