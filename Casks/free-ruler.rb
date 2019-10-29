463b70cec403bf85052823d1cc88b5b6831bcfee
Ruben Opdebeeck <ropdebee@vub.be>
2019-10-29 04:42:31 +0100

    Update free-ruler from 2.0.1 to 2.0.3

--- Casks/free-ruler.rb
+++ Casks/free-ruler.rb
@@ -1,6 +1,6 @@
 cask 'free-ruler' do
-  version '2.0.1'
-  sha256 '0ec0b4d62b7b9be17412d00d3eb045e4f574d6b0e1f725ad569740a983249685'
+  version '2.0.3'
+  sha256 'aa428907c113e249a6baedb329e788b10d6db67d9f9117c3986780d2c592da9d'
 
   # github.com/pascalpp/FreeRuler was verified as official when first introduced to the cask
   url "https://github.com/pascalpp/FreeRuler/releases/download/v#{version}/free-ruler-#{version}.zip"
