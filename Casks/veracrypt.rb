7d7f355c8707c497d8b923b4fdcb813875852741
suschizu <brigitte@corecode.io>
2019-10-29 17:55:44 +0100

    Update veracrypt (#71656)

--- Casks/veracrypt.rb
+++ Casks/veracrypt.rb
@@ -1,9 +1,9 @@
 cask 'veracrypt' do
   version '1.24'
-  sha256 '39feff25b34b75f13a9200d45aa598dc3463129eb6d1684b6403d3fb30a81ac0'
+  sha256 '5515766ebac14dcc3e397f0afd24f31f998a4a3c69571ea40b990772fede679c'
 
   # launchpad.net/veracrypt/trunk was verified as official when first introduced to the cask
-  url "https://launchpad.net/veracrypt/trunk/#{version}/+download/VeraCrypt_#{version}.dmg"
+  url "https://launchpad.net/veracrypt/trunk/#{version}-hotfix1/+download/VeraCrypt_#{version}-Hotfix1.dmg"
   appcast 'https://github.com/veracrypt/VeraCrypt/releases.atom'
   name 'VeraCrypt'
   homepage 'https://www.veracrypt.fr/'
