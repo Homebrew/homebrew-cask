c10fd410497c742479ace90014f105514094bfd2
suschizu <brigitte@corecode.io>
2019-10-29 14:19:16 +0100

    Update miktex-console from 2.9.7050-1 to 2.9.7230-1 (#71651)

--- Casks/miktex-console.rb
+++ Casks/miktex-console.rb
@@ -1,6 +1,6 @@
 cask 'miktex-console' do
-  version '2.9.7050-1'
-  sha256 '46689b9de8bf158d6c74a17ecdcacc418e8303bc47988228fa395c660d41a9fa'
+  version '2.9.7230-1'
+  sha256 'eb55ebe17075516377c45885f2528323deb0176f5e9aaa8092cff77a1f42ff85'
 
   url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-darwin-x86_64.dmg"
   appcast 'https://miktex.org/download'
