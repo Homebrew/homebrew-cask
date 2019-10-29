1395d80c196c1ce75a9ae340861c1d7272674990
chrisfinazzo <chris.finazzo@gmail.com>
2019-10-29 07:50:37 -0400

    Update BBEdit to 13.0.2

--- Casks/bbedit.rb
+++ Casks/bbedit.rb
@@ -6,8 +6,8 @@ cask 'bbedit' do
     version '12.6.7'
     sha256 'd0647c864268b187343bd95bfcf490d6a2388579b1f8fce64a289c65341b1144'
   else
-    version '13.0.1'
-    sha256 'cb330230cbf1795cf79ed7f0ea44a9155007a1a3d753f24ed4a0184897265fd5'
+    version '13.0.2'
+    sha256 '9b0c73bd7f1ab35dd8a1f0efe6ab890f4a8676fa3aa20987800a8d61971ed2ab'
   end
   # s3.amazonaws.com/BBSW-download was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/BBSW-download/BBEdit_#{version}.dmg"
