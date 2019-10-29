faa7317b1895971bc9b149d1ec0baf21f2d7d00c
Amor <amor90s.zhang@gmail.com>
2019-10-29 09:05:08 +0800

    Update Casks/platelet.rb
    Co-Authored-By: Vítor Galvão <info@vitorgalvao.com>

--- Casks/platelet.rb
+++ Casks/platelet.rb
@@ -2,7 +2,7 @@ cask 'platelet' do
   version '0.0.13'
   sha256 'aba7f4239ba3f34cf7bf13926c85c7b41afb192dc6647594cdf55e1fd989e7f6'
 
-  url "https://github.com/amorist/platelet/releases/download/v0.0.13/platelet-0.0.13.dmg"
+  url "https://github.com/amorist/platelet/releases/download/v#{version}/platelet-#{version}.dmg"
   name 'Platelet'
   name 'けっしょうばん'
   appcast 'https://github.com/amorist/platelet/releases.atom'
