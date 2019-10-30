4ffa46390357f993f58594401d777d1ce14cb0f5
Qi Zhang <singularitti@outlook.com>
2019-10-30 17:50:09 -0400

    Update macvim from 8.1.2224,160 to 8.1.2234,161

--- Casks/macvim.rb
+++ Casks/macvim.rb
@@ -1,6 +1,6 @@
 cask 'macvim' do
-  version '8.1.2224,160'
-  sha256 'a9405b7cc8f1199772dd8ec25b096520818572f293fa6d9cbf2a0af1eadc14a2'
+  version '8.1.2234,161'
+  sha256 '623df4432ef0eb663634ad919eee2e782223ec34a62ca90f18a67f28834060d4'
 
   url "https://github.com/macvim-dev/macvim/releases/download/snapshot-#{version.after_comma}/MacVim.dmg"
   appcast 'https://github.com/macvim-dev/macvim/releases.atom'
