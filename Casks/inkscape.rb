6e154369b801eeb71b3ff6925ad387db6e92e1c1
Vítor Galvão <info@vitorgalvao.com>
2019-10-30 14:47:18 +0000

    Update inkscape.rb

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -1,8 +1,18 @@
 cask 'inkscape' do
-  version '1.0beta1'
-  sha256 '68831989b3919e3137d5acfb130a844933706748addc8b4cd7f957348c1c60a3'
+  if MacOS.version <= :mavericks
+    version '0.92.2-1'
+    sha256 'faece7a9a5fa9db7724b0c761f7f2014676d00ef8b90a0ef506fa39d09209fea'
 
-  url "https://inkscape.org/gallery/item/14919/Inkscape-#{version}_OEMhoXK.dmg"
+    url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}-x11-10.7-x86_64.dmg"
+
+    depends_on x11: true
+  else
+    version '1.0beta1'
+    sha256 '68831989b3919e3137d5acfb130a844933706748addc8b4cd7f957348c1c60a3'
+
+    url "https://inkscape.org/gallery/item/14919/Inkscape-#{version}_OEMhoXK.dmg"
+  end
+
   name 'Inkscape'
   homepage 'https://inkscape.org/'
 
