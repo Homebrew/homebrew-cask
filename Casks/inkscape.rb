6fe132498534e75eb44148d811c9c5a19bb9be59
Vítor Galvão <info@vitorgalvao.com>
2019-10-30 15:10:46 +0000

    Update inkscape.rb

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -6,17 +6,21 @@ cask 'inkscape' do
     url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}-x11-10.7-x86_64.dmg"
 
     depends_on x11: true
+    
+    binary "#{appdir}/Inkscape.app/Contents/Resources/bin/inkscape"
+    
+    zap trash: '~/.inkscape-etc'
   else
     version '1.0beta1'
     sha256 '68831989b3919e3137d5acfb130a844933706748addc8b4cd7f957348c1c60a3'
 
     url "https://inkscape.org/gallery/item/14919/Inkscape-#{version}_OEMhoXK.dmg"
+
+    zap trash: '~/Library/Application Support/Inkscape'
   end
 
   name 'Inkscape'
   homepage 'https://inkscape.org/'
 
   app 'Inkscape.app'
-
-  zap trash: '~/Library/Application Support/Inkscape'
 end
