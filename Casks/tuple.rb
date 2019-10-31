a02e6b9d76d9e8ca431f3e809428fa579f7669b7
programmiri <programmiri@gmail.com>
2019-10-31 21:06:59 +0100

    Add Tuple.app Latest

---
+++ Casks/tuple.rb
@@ -0,0 +1,10 @@
+cask 'tuple' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://production.tuple.app/downloads/latest'
+  name 'Tuple'
+  homepage 'https://tuple.app/'
+
+  app 'Tuple.app'
+end
