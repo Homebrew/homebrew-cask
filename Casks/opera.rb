a46665090002f58b95229c754b838c86d7dbfcc1
ashendon <ashendon@users.noreply.github.com>
2019-10-31 10:41:46 -0400

    Update opera from 64.0.3417.73 to 64.0.3417.83

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '64.0.3417.73'
-  sha256 '1ef2812cb6038efe5263a5e65726c83034ae30c2dd2034e34ee6afda966c071e'
+  version '64.0.3417.83'
+  sha256 'f3ce6f23ce26fa63bda385d8fbd634fb82ae08103c5a2ffc6aede278248542eb'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   appcast 'https://ftp.opera.com/pub/opera/desktop/'
