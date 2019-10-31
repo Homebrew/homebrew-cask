39897bb0e2db27c6578aab3877a3a4b76e42de89
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-30 17:06:25 -0700

    Update alt-tab from 1.11.0 to 1.11.3 (#71735)

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,6 +1,6 @@
 cask 'alt-tab' do
-  version '1.11.0'
-  sha256 '8bc397a461193d0006077c58ee5b4b7b0fbd434a1f660e429c7a0daed16f7a2f'
+  version '1.11.3'
+  sha256 'f9968b4b0ba63879b8135d5c23d1a509ffe6edf2ffdc7da1a9724d1a8702dd5f'
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
   appcast 'https://github.com/lwouis/alt-tab-macos/releases.atom'
