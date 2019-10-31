67ff6321c507f8deef1fe74213a5e259b2b9ec54
Vítor Galvão <info@vitorgalvao.com>
2019-10-31 19:50:20 +0000

    moneywiz.rb: fix shasum (#71796)

--- Casks/moneywiz.rb
+++ Casks/moneywiz.rb
@@ -1,6 +1,6 @@
 cask 'moneywiz' do
   version '3.6.3'
-  sha256 'aa14975c3b8f689edabbecaf3a2c7af34c186897db63a5dcb4d76d1deae64c12'
+  sha256 'fd900b217f72db4a7aea2c431dd6d08a34e8e65f9e3ef68bf31037766065a1cb'
 
   url 'https://mac.wiz.money/MoneyWiz_Mac_Free.dmg'
   appcast 'https://macdistribution.wiz.money/version_info_free.xml'
