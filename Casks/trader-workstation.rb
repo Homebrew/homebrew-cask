75a53dee842c7f405312e6599a594732e4e3318a
Matthias Fasching <Matthias.Fasching@zuehlke.com>
2019-10-31 10:28:26 +0100

    Update trader-workstation to 972.1v

--- Casks/trader-workstation.rb
+++ Casks/trader-workstation.rb
@@ -1,19 +1,19 @@
 cask 'trader-workstation' do
-  version '972'
-  sha256 '66fbb43b314c74e97add82a60e4efa0f84a73df3e914adcbe67c6dfa6f677ccc'
+  version '972,972.1v'
+  sha256 '0122e5871c05f6b8116d7495f6eae98ff68fa7759c645ce2e612ff350b61912d'
 
   url 'https://download2.interactivebrokers.com/installers/tws/stable-standalone/tws-stable-standalone-macosx-x64.dmg'
   name 'Trader Workstation'
   homepage 'https://www.interactivebrokers.com/'
 
   installer script: {
-                      executable: "#{staged_path}/Trader Workstation #{version} Installer.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{staged_path}/Trader Workstation #{version.before_comma} Installer.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 
   uninstall quit:   'com.install4j.5889-6375-8446-2021.22',
             script: {
-                      executable: "#{ENV['HOME']}/Applications/Trader Workstation #{version}/Trader Workstation #{version} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{ENV['HOME']}/Applications/Trader Workstation #{version.before_comma}/Trader Workstation #{version.before_comma} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 end
