43b19db83552842a536ee90de27e3f9a4d0d0e42
Vítor Galvão <info@vitorgalvao.com>
2019-10-31 12:39:33 +0000

    Update trader-workstation.rb

--- Casks/trader-workstation.rb
+++ Casks/trader-workstation.rb
@@ -1,5 +1,5 @@
 cask 'trader-workstation' do
-  version '972,972.1v'
+  version '972'
   sha256 '0122e5871c05f6b8116d7495f6eae98ff68fa7759c645ce2e612ff350b61912d'
 
   url 'https://download2.interactivebrokers.com/installers/tws/stable-standalone/tws-stable-standalone-macosx-x64.dmg'
@@ -7,13 +7,13 @@ cask 'trader-workstation' do
   homepage 'https://www.interactivebrokers.com/'
 
   installer script: {
-                      executable: "#{staged_path}/Trader Workstation #{version.before_comma} Installer.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{staged_path}/Trader Workstation #{version} Installer.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 
   uninstall quit:   'com.install4j.5889-6375-8446-2021.22',
             script: {
-                      executable: "#{ENV['HOME']}/Applications/Trader Workstation #{version.before_comma}/Trader Workstation #{version.before_comma} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{ENV['HOME']}/Applications/Trader Workstation #{version}/Trader Workstation #{version} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 end
