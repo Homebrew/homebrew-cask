ce66786700d55628052aea9712220ff5e14f7eb9
suschizu <brigitte@corecode.io>
2019-11-01 19:19:58 +0100

    Update sound-control.rb

--- Casks/sound-control.rb
+++ Casks/sound-control.rb
@@ -3,7 +3,7 @@ cask 'sound-control' do
   sha256 '8cd697bc1879eb36fb2e6dde5e64e06b927adea52f2c664d839864c129a0bffa'
 
   # staticz.net was verified as official when first introduced to the cask
-  url "http://staticz.com/download/#{version.after_comma}/"
+  url "https://staticz.com/download/#{version.after_comma}/"
   appcast 'http://staticz.net/updates/soundcontrol.rss'
   name 'Sound Control'
   homepage 'https://staticz.com/soundcontrol/'
