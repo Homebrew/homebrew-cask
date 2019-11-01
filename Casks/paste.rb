b9de93c78aed89eaab9fcfeba1cb43ae0d93ccb2
Phill Tran <web@philltran.com>
2019-11-01 11:44:15 -0400

    Update paste.rb to 2.5.2.

--- Casks/paste.rb
+++ Casks/paste.rb
@@ -2,7 +2,7 @@ cask 'paste' do
   version '2.5.2,7'
   sha256 '6a95be2b3eacb42930d271bb7676e2b0c2c5b1b2079ef840ea90b7e72030c6b0'
 
-  # pasteapp.me/mac/update-feed provided as official from developer
+  # rink.hockeyapp.net/api/2/apps/d6efdf44318a43ebb2130e89477625c8 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/d6efdf44318a43ebb2130e89477625c8/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://pasteapp.me/mac/update-feed'
   name 'Paste'
