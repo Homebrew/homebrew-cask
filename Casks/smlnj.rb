d7b42e7b4091786cc4b1dc0810891acb344ecbee
Kartik Singhal <kartiksinghal@gmail.com>
2019-11-01 10:00:00 -0500

    Upgrade Standard ML of New Jersey to v110.94 (#71858)
    With Catalina and 64-bit release of smlnj, we switch to amd64 version of
    the package.

--- Casks/smlnj.rb
+++ Casks/smlnj.rb
@@ -1,15 +1,15 @@
 cask 'smlnj' do
-  version '110.91'
-  sha256 '8476fe570b13f722b9b81182590144ccb770495a5534fa77600a7c592028568a'
+  version '110.94'
+  sha256 '8af582d4fc2e1bb425dd4925696d3c56b687baeb50ffc16362a1a46fa495f27b'
 
   # smlnj.cs.uchicago.edu was verified as official when first introduced to the cask
-  url "http://smlnj.cs.uchicago.edu/dist/working/#{version}/smlnj-x86-#{version}.pkg"
+  url "http://smlnj.cs.uchicago.edu/dist/working/#{version}/smlnj-amd64-#{version}.pkg"
   name 'Standard ML of New Jersey'
   homepage 'https://www.smlnj.org/'
 
-  pkg "smlnj-x86-#{version}.pkg"
+  pkg "smlnj-amd64-#{version}.pkg"
 
-  uninstall pkgutil: 'org.smlnj.x86.pkg'
+  uninstall pkgutil: 'org.smlnj.amd64.pkg'
 
   caveats do
     path_environment_variable '/usr/local/smlnj/bin'
