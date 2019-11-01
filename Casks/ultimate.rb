92c31a7725fa54ad81a2812c3d1a8251a4d90130
suschizu <brigitte@corecode.io>
2019-11-01 22:17:29 +0100

    Revert "Update ultimate from 3.0.11.1025 to 3.0.11.1028 (#71836)"
    This reverts commit 9c633ec3603cd63c60c0db9200f1de1c89bf9ccb.

--- Casks/ultimate.rb
+++ Casks/ultimate.rb
@@ -1,5 +1,5 @@
 cask 'ultimate' do
-  version '3.0.11.1028'
+  version '3.0.11.1025'
   sha256 'cffb891540b4b338a1ac32e15a16eb3f44129983657f45bb0872105bd6029700'
 
   url 'https://download.epubor.com/epubor_ultimate.zip'
