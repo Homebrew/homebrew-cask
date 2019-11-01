a741d9b94068bc69b1472e420d89f222baae251d
suschizu <brigitte@corecode.io>
2019-11-01 19:10:31 +0100

    Update izip.rb

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -2,7 +2,7 @@ cask 'izip' do
   version '3.6'
   sha256 '951b631107a532077c99f1bbd1579f6435d3c5715f657068950388af351bd164'
 
-  url 'http://www.izip.com/izip.dmg'
+  url 'https://www.izip.com/izip.dmg'
   appcast 'https://www.izip.com/updates'
   name 'iZip'
   homepage 'https://www.izip.com/'
