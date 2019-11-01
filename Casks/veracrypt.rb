8bbdbd0be1f5c27dfa932630d188312ff397599a
tiiiecherle <tiiiecherle@gmail.com>
2019-11-01 20:37:13 +0100

    Update veracrypt from 1.24 to 1.24,hotfix1

--- Casks/veracrypt.rb
+++ Casks/veracrypt.rb
@@ -1,9 +1,9 @@
 cask 'veracrypt' do
-  version '1.24'
+  version '1.24,hotfix1'
   sha256 '5515766ebac14dcc3e397f0afd24f31f998a4a3c69571ea40b990772fede679c'
 
   # launchpad.net/veracrypt/trunk was verified as official when first introduced to the cask
-  url "https://launchpad.net/veracrypt/trunk/#{version}-hotfix1/+download/VeraCrypt_#{version}-Hotfix1.dmg"
+  url "https://launchpad.net/veracrypt/trunk/#{version.before_comma}-hotfix1/+download/VeraCrypt_#{version.before_comma}-Hotfix1.dmg"
   appcast 'https://github.com/veracrypt/VeraCrypt/releases.atom'
   name 'VeraCrypt'
   homepage 'https://www.veracrypt.fr/'
