b53d6addd2995d6a75659d1b83857aab3a8a6da1
mars <marshalys@gmail.com>
2019-11-01 17:02:32 +0800

    fix liteide download url error
    fix liteide download url error

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask 'liteide' do
   version '36.2'
-  sha256 '9c23c2a4f75f04689aa19189aced081e36148efb7ae3fb1ec9d863198528e012'
+  sha256 '2457801b281062140273c681fd0ee1623a97cba47aa96cb519a0a1fddc793590'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
   url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.12.5-1.zip"
