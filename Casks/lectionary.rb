00fcb344b81a7e6d921119c7aafaad17b1e53e94
Randall <17261190+ran-dall@users.noreply.github.com>
2019-10-31 22:53:52 -0700

    Update lectionary from 1.18.1 to 1.18.1.1 (#71835)

--- Casks/lectionary.rb
+++ Casks/lectionary.rb
@@ -1,6 +1,6 @@
 cask 'lectionary' do
-  version '1.18.1'
-  sha256 '35b22975fbcfa83abdb89be80abf39315e5f60d26fb4200cb2bcd87a2bafa0df'
+  version '1.18.1.1'
+  sha256 '8a19a62c80ccaeb56baaff12cb3e382ebd65185a1400995d8a1a129c086535e9'
 
   url "https://myaccount.hymnsam.co.uk/PublicationRelatedFiles/21/Lectionary%20for%20Mac.#{version}.dmg"
   appcast 'https://www.chpublishing.co.uk/app/lectionary'
