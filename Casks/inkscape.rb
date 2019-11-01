ca41f2b438fba282c9bc1ae2049a8b9b690646db
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-01 12:14:10 +0100

    Update inkscape

--- Casks/inkscape.rb
+++ Casks/inkscape.rb
@@ -14,7 +14,7 @@ cask 'inkscape' do
     version '1.0beta1'
     sha256 '68831989b3919e3137d5acfb130a844933706748addc8b4cd7f957348c1c60a3'
 
-    url "https://inkscape.org/gallery/item/14919/Inkscape-#{version}_OEMhoXK.dmg"
+    url "https://media.inkscape.org/dl/resources/file/Inkscape-#{version}_OEMhoXK.dmg"
 
     zap trash: '~/Library/Application Support/Inkscape'
   end
