36fb7869e4033c99e9beee12bcd41c9dd9f8128f
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-02 14:53:58 -0700

    Update amazon-workspaces from 2.5.9 to 2.5.11 (#71982)

--- Casks/amazon-workspaces.rb
+++ Casks/amazon-workspaces.rb
@@ -1,6 +1,6 @@
 cask 'amazon-workspaces' do
-  version '2.5.9'
-  sha256 '215c762adc7fcc35cb161604a74f25f0b7ac9e94f9c9727c4945349826b1bcfe'
+  version '2.5.11'
+  sha256 'd217cceaecb28cb8d2c4f99f2f84c0aa4696f91dfb779c2cacf0c51e1b93c6a1'
 
   # d2td7dqidlhjx7.cloudfront.net was verified as official when first introduced to the cask
   url 'https://d2td7dqidlhjx7.cloudfront.net/prod/global/osx/WorkSpaces.pkg'
