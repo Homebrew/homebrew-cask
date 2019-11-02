705cea5ee7b6d0766df07fa53d1edcf16abe48c4
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-01 20:37:45 -0700

    Update snapndrag from 4.3.2 to 4.3.3 (#71923)

--- Casks/snapndrag.rb
+++ Casks/snapndrag.rb
@@ -1,6 +1,6 @@
 cask 'snapndrag' do
-  version '4.3.2'
-  sha256 'af579ee0d779416923df403c35da15df4a7720a70a3599fa5c677a5971833a62'
+  version '4.3.3'
+  sha256 'a8a68641fcf766c241a06ecefae549a3851000f471eee48b71fa45672c179452'
 
   url "https://yellowmug.com/download/SnapNDrag_#{version}.dmg"
   appcast 'https://yellowmug.com/snapndrag/appcast-1014.xml'
