7e942b880659792045d591bcbadb78a3f5674418
Randall <ran.dall@icloud.com>
2019-11-01 19:45:09 -0700

    Update qgis from 3.10 to 3.12

--- Casks/qgis.rb
+++ Casks/qgis.rb
@@ -1,5 +1,5 @@
 cask 'qgis' do
-  version '3.10'
+  version '3.12'
   sha256 'c46c05ad0fb175f555401bfc6b5a315ac273ab0d0c355df75958dd909aa8e658'
 
   url 'https://qgis.org/downloads/macos/qgis-macos-pr.dmg'
