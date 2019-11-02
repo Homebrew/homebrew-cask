1b8bc83d2181605d84f2709841efcf55ab2359dd
DocQuantum <shchurgood@gmail.com>
2019-11-02 02:43:42 -0500

    only removed single qoutes on escaped instances

--- Casks/tales-of-majeyal.rb
+++ Casks/tales-of-majeyal.rb
@@ -4,9 +4,9 @@ cask 'tales-of-majeyal' do
 
   url "https://te4.org/dl/t-engine/t-engine4-osx-#{version}.zip"
   appcast 'https://te4.org/download'
-  name 'Tales of Maj\'Eyal'
+  name "Tales of Maj'Eyal"
   homepage 'https://te4.org/'
 
   # Renamed for clarity: app name is inconsistent with its branding.
-  app 'T-Engine.app', target: 'Tales-of-Maj\'Eyal.app'
+  app 'T-Engine.app', target: "Tales-of-Maj'Eyal.app"
 end
