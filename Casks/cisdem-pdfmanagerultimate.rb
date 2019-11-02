0ca302bba62829790f942b7bc2abe64bce35c777
Work <brigitte@corecode.io>
2019-11-02 17:12:57 +0100

    Update cisdem-pdfmanagerultimate from 2.5.0 to 3.2.0

--- Casks/cisdem-pdfmanagerultimate.rb
+++ Casks/cisdem-pdfmanagerultimate.rb
@@ -1,5 +1,5 @@
 cask 'cisdem-pdfmanagerultimate' do
-  version '2.5.0'
+  version '3.2.0'
   sha256 '572ed99e8ea18474a0dfe7fd42e0fe704e40607f12a979ada704054fc81d05de'
 
   url 'https://www.cisdem.com/download/cisdem-pdfmanagerultimate.dmg'
