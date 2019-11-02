34d739303cd67454c46f0810e1680a88688db62b
404NetworkError <404NetworkError@users.noreply.github.com>
2019-11-02 13:30:10 -0400

    Update menumeters from 1.9.8 to 1.9.9

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,16 +1,25 @@
 cask 'menumeters' do
-  version '1.9.8'
-  sha256 '7e034975be0d7eac033f0d4fcaf8725d435280411358d50a9828ce62f8bc1cca'
+  version '1.9.9'
+  sha256 'ee34d38ead45a13d77b81b7c949d6a157d671e4a385b46d207c4a4cac25dfba2'
 
   # github.com/yujitach/MenuMeters was verified as official when first introduced to the cask
   url "https://github.com/yujitach/MenuMeters/releases/download/#{version}/MenuMeters_#{version.major_minor_patch}.zip"
-  appcast 'https://github.com/yujitach/MenuMeters/releases.atom'
+  appcast 'https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/MenuMeters-Update.xml'
   name 'MenuMeters for El Capitan (and later)'
   homepage 'https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/'
 
+  auto_updates true
   depends_on macos: '>= :el_capitan'
 
-  prefpane 'MenuMeters.prefPane'
+  app 'MenuMeters.app'
 
-  zap trash: '~/Library/Preferences/com.ragingmenace.MenuMeters.plist'
+  uninstall login_item: 'MenuMeters',
+            quit:       'com.yujitach.MenuMeters'
+
+  zap trash: [
+               '~/Library/Caches/com.yujitach.MenuMeters',
+               '~/Library/PreferencesPanes/MenuMeters.prefPane',
+               '~/Library/Preferences/com.ragingmenace.MenuMeters.plist',
+               '~/Library/Preferences/com.yujitach.MenuMeters.plist',
+             ]
 end
