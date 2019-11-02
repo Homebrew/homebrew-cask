3948d3ddc35bc4d1a5cce002cab3d5f89a6a8c98
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2019-11-01 23:35:36 -0400

    Update frhelper from 3.9.1 to 3.9.2 (#71949)

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -1,6 +1,6 @@
 cask 'frhelper' do
-  version '3.9.1'
-  sha256 'cb6825c62657f4c8d229294d1239fedc804850d4a5832bd126c8745489b79591'
+  version '3.9.2'
+  sha256 '85c5bb80abdf63b5623ac6561b14e5ed7e439ec9848d36798e66988bf84c793a'
 
   # static.frdic.com was verified as official when first introduced to the cask
   url 'https://static.frdic.com/pkg/fhmac.dmg'
