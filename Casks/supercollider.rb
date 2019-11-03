04cc2b63449c7c7689769b5e3621cd10fd03ccbc
Dmitry Pogodin <lunaticare@users.noreply.github.com>
2019-11-03 05:51:16 +0300

    Update supercollider (#71993)

--- Casks/supercollider.rb
+++ Casks/supercollider.rb
@@ -1,9 +1,9 @@
 cask 'supercollider' do
   version '3.10.3'
-  sha256 'df6c764579d699d54679dbc1badde405566918dac2d3adecc17326712d2b81f4'
+  sha256 'd366015c56ad432a44642b42a5d424a46d416b800e7d2ef369daf2e8875f9ba9'
 
   # github.com/supercollider/supercollider was verified as official when first introduced to the cask
-  url "https://github.com/supercollider/supercollider/releases/download/Version-#{version}/SuperCollider-#{version}-macOS.zip"
+  url "https://github.com/supercollider/supercollider/releases/download/Version-#{version}/SuperCollider-#{version}-macOS-signed.zip"
   appcast 'https://github.com/supercollider/supercollider/releases.atom'
   name 'SuperCollider'
   homepage 'https://supercollider.github.io/'
