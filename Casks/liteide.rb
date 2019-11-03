009c15d102538405c5ef663084c5ab9c6316bb9b
suschizu <brigitte@corecode.io>
2019-11-03 18:32:41 +0100

    Add depends on sierra to liteide.rb (#71966)

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -3,10 +3,12 @@ cask 'liteide' do
   sha256 '2457801b281062140273c681fd0ee1623a97cba47aa96cb519a0a1fddc793590'
 
   # github.com/visualfc/liteide was verified as official when first introduced to the cask
-  url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.12.5-1.zip"
+  url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.12.5.zip"
   appcast 'https://github.com/visualfc/liteide/releases.atom'
   name 'LiteIDE'
   homepage 'http://liteide.org/'
+
+  depends_on macos: '>= :sierra'
 
   app 'liteide/LiteIDE.app'
 end
