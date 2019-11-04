aa301966cfa69ce267f2bbee0cf78a5a946771ab
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-03 18:20:43 -0800

    Update dbvisualizer from 10.0.23 to 10.0.24 (#72027)

--- Casks/dbvisualizer.rb
+++ Casks/dbvisualizer.rb
@@ -1,6 +1,6 @@
 cask 'dbvisualizer' do
-  version '10.0.23'
-  sha256 '7770a0feabb47b9a80f75ca2ad70e5523a72375f52e41fbd458af6067f34816c'
+  version '10.0.24'
+  sha256 'b40f5f6f0d42d33c3265505f3a20358c56a5afaca662a056ee9cf65e13ba1e6a'
 
   url "https://www.dbvis.com/product_download/dbvis-#{version}/media/dbvis_macos_#{version.dots_to_underscores}_jre.dmg"
   appcast "https://www.dbvis.com/download/#{version.major}.0"
