485d738e2c1270b2e9c0e3b35d0669bd02eb68f5
tiemonl <tiemon.liam@gmail.com>
2019-11-04 18:22:59 -0500

    Add Fork.app latest

---
+++ Casks/git-fork.rb
@@ -0,0 +1,10 @@
+cask 'git-fork' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://git-fork.com/update/files/Fork.dmg'
+  name 'Fork'
+  homepage 'https://git-fork.com/'
+
+  app 'Fork.app'
+end
