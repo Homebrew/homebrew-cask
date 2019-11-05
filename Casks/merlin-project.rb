fb4499bcac695ddb49111acfcf8e64f0dc9eba30
Anver Housseini <contact@anver.ch>
2019-11-05 22:34:11 +0100

    Update merlin-project from 5.1.14 to 6.0.0 (#72088)

--- Casks/merlin-project.rb
+++ Casks/merlin-project.rb
@@ -1,6 +1,6 @@
 cask 'merlin-project' do
-  version '5.1.14'
-  sha256 'd5bb512d8d0a7bb0480e2bbc151e2c7d429f79a9382e00e19f8dfd9ca5007971'
+  version '6.0.0'
+  sha256 'be9357a558c451f5f2dc6820d43fd69de44986cb2c3d2e89fb84d14aa51f88ef'
 
   url 'https://www.projectwizards.net/downloads/MerlinProject.zip'
   appcast 'https://www.projectwizards.net/en/support/release-notes/merlin-project-pwstore/xml'
