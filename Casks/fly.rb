b64bfc36481687312c19a24514e7bd280462fb7b
Artur Fafara <afafa@softserveinc.com>
2019-11-05 15:19:21 +0100

    Update fly from 5.6.0 to 5.7.0

--- Casks/fly.rb
+++ Casks/fly.rb
@@ -1,6 +1,6 @@
 cask 'fly' do
-  version '5.6.0'
-  sha256 '307d420acc56556429ea2045cb688b8e222438e6b60497cf272fdcbf36781328'
+  version '5.7.0'
+  sha256 'efa0d9760ac0c6b02088b2c94a0389e62cacb6fddd55775b41661dcfd92ff856'
 
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/concourse/concourse/releases.atom'
