658224da35dccefb73d622d9326f85f4b6fda07f
Work <brigitte@corecode.io>
2019-11-05 19:37:12 +0100

    Update omegat from 4.3.0 to 4.3.1

--- Casks/omegat.rb
+++ Casks/omegat.rb
@@ -1,9 +1,9 @@
 cask 'omegat' do
-  version '4.3.0'
-  sha256 '0beccd1bcc4633e65f4ed272aaf946c95b17e5d343b39a90a7b742f2768b065b'
+  version '4.3.1'
+  sha256 '2e427d9fd8008a88877aeaa0e634f3d4ff8cbd51bb11e48314535940fc43c0a5'
 
   # downloads.sourceforge.net/omegat was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/omegat/OmegaT%20-%20Standard/OmegaT%20#{version.major_minor_patch}/OmegaT_#{version}_Mac_Signed.zip"
+  url "https://downloads.sourceforge.net/omegat/OmegaT%20-%20Standard/OmegaT%20#{version.major_minor_patch}/OmegaT_#{version}_Mac_Notarized.zip"
   appcast 'https://sourceforge.net/projects/omegat/rss?path=/OmegaT%20-%20Standard'
   name 'OmegaT'
   homepage 'https://omegat.org/'
