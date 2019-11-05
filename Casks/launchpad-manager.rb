dbb562c3a00a2c175f72a38a2d5f12e6ada9ebc1
Mark Castle <mark@castle.sx>
2019-11-05 11:54:28 -0600

    Use Latest Vesion for Yosemite Download

--- Casks/launchpad-manager.rb
+++ Casks/launchpad-manager.rb
@@ -1,8 +1,8 @@
 cask 'launchpad-manager' do
 
   if MacOS.version = :yosemite
-    version '1.0.10'
-    sha256 '88b37120c0ae022309573c4d2587a6ec85df7895773bca0fd3b5ba6cd86461a6'
+    version :latest
+    sha256 :no_check
 
     url 'http://launchpadmanager.com/download_yosemite.php/LaunchpadManagerYosemite.dmg'
 
