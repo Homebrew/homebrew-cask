bd62f17e2893671003fc123798e97778d4f116c9
Anver Housseini <contact@anver.ch>
2019-11-05 15:49:08 +0100

    Update opera from 64.0.3417.83 to 64.0.3417.92 (#72073)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '64.0.3417.83'
-  sha256 'f3ce6f23ce26fa63bda385d8fbd634fb82ae08103c5a2ffc6aede278248542eb'
+  version '64.0.3417.92'
+  sha256 '63c0ef3c57128046dfdc933570fcdbb5a82ec3b34bb0d2bf14bb21c3015edecd'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   appcast 'https://ftp.opera.com/pub/opera/desktop/'
