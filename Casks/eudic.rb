f72398a0e1c775ca8ff1b305f2db7fd32cffde20
Qi Zhang <singularitti@outlook.com>
2019-11-04 23:06:16 -0500

    Update eudic

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,6 +1,6 @@
 cask 'eudic' do
   version '3.9.2'
-  sha256 '8b4e9f5e4e0c58649d759be2289cdf9a77e77a3cfd91e4e6375e8b2139cb4f04'
+  sha256 '6a8cf266a1fe0e94f33f93c2c37970057dd3075a1993f1a2161089ca92051803'
 
   # static.frdic.com was verified as official when first introduced to the cask
   url 'https://static.frdic.com/pkg/eudicmac.dmg'
