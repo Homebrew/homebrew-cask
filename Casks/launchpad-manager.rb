02601b03b173a7d7afaf4d4408355d840fe79199
Mark Castle <mark@castle.sx>
2019-11-05 10:11:31 -0600

    Include Yosemite Specific Build of Launchpad Manager

--- Casks/launchpad-manager.rb
+++ Casks/launchpad-manager.rb
@@ -1,8 +1,18 @@
 cask 'launchpad-manager' do
-  version :latest
-  sha256 :no_check
 
-  url 'http://launchpadmanager.com/download.php/LaunchpadManager.dmg'
+  if MacOS.version = :yosemite
+    version '1.0.10'
+    sha256 '88b37120c0ae022309573c4d2587a6ec85df7895773bca0fd3b5ba6cd86461a6'
+
+    url 'http://launchpadmanager.com/download_yosemite.php/LaunchpadManagerYosemite.dmg'
+
+  else
+    version :latest
+    sha256 :no_check
+
+    url 'http://launchpadmanager.com/download.php/LaunchpadManager.dmg'
+  end
+
   name 'Launchpad Manager'
   homepage 'http://launchpadmanager.com/'
 
