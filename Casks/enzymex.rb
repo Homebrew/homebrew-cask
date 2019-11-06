947f45f062e905286ec83d05f00332f4835108cf
Yishen Miao <mys721tx@gmail.com>
2019-11-06 09:55:27 -0800

    Adding EnzymeX latest
    Adds EnzymeX latest from nucleobytes.

---
+++ Casks/enzymex.rb
@@ -0,0 +1,10 @@
+cask 'enzymex' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://downloads.nucleobytes.com/enzymex.dmg'
+  name 'EnzymeX'
+  homepage 'https://nucleobytes.com/enzymex/index.html'
+
+  app 'EnzymeX.app'
+end
