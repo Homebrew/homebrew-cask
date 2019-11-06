8a232894ebc91b51c8ab54f7e171e569f9ebc8aa
suschizu <brigitte@corecode.io>
2019-11-06 16:33:44 +0100

    Update stamp from 4.15.1 to 4.15.2 (#72165)

--- Casks/stamp.rb
+++ Casks/stamp.rb
@@ -1,6 +1,6 @@
 cask 'stamp' do
-  version '4.15.1'
-  sha256 'b3b30a67e14782be5d354bf76d3210081c3c4f14f4b7a322d96893e6ef4b0378'
+  version '4.15.2'
+  sha256 '9fb6b1dfc0551106e21bf2e04a6b3859f3494cdb18f5486813c98027e48fdce4'
 
   # dzqeytqqx888.cloudfront.net was verified as official when first introduced to the cask
   url "https://dzqeytqqx888.cloudfront.net/STAMP#{version.no_dots}.dmg"
