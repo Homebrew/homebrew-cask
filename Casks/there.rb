c36a00e8cd23beb4863a45e0154651d00ab34f71
Julian Mayer <julian@corecode.io>
2019-11-06 23:38:55 +0100

    Update there

--- Casks/there.rb
+++ Casks/there.rb
@@ -1,6 +1,6 @@
 cask 'there' do
   version '1.8.3'
-  sha256 'becf46078c13a744c6c01aff2420113942b7c357eb9a97f277b83212c5377c3f'
+  sha256 '0e9f7ea12a8546964fee9e954ddc494544b348886bc31f409eb57140900ba6dd'
 
   # github.com/therehq/there-desktop was verified as official when first introduced to the cask
   url "https://github.com/therehq/there-desktop/releases/download/v#{version}/There-#{version}-mac.zip"
