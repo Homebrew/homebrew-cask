63d1d0d4aad13ede89a33705ef67f7808018fc21
Zhenfu Shi <szf1234@me.com>
2019-11-06 01:18:39 -0500

    Update browserosaurus from 8.1.0 to 8.1.1 (#72148)

--- Casks/browserosaurus.rb
+++ Casks/browserosaurus.rb
@@ -1,6 +1,6 @@
 cask 'browserosaurus' do
-  version '8.1.0'
-  sha256 'f7594a0735ab66db1a73332afa711701975fb6dab537ba227d3b96c9538a3640'
+  version '8.1.1'
+  sha256 '4ee610ea10bc46e2630acdd1359b5ff5215dca5bbd96f583b89fc213fbd447a6'
 
   # github.com/will-stone/browserosaurus/ was verified as official when first introduced to the cask
   url "https://github.com/will-stone/browserosaurus/releases/download/v#{version}/Browserosaurus-#{version}.dmg"
