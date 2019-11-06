b5d8c7efa2f64c73499c1986134697b78f5b5d86
Randall <ran.dall@icloud.com>
2019-11-05 18:19:20 -0800

    Update fs-uae from 3.0.0 to 3.0.1

--- Casks/fs-uae.rb
+++ Casks/fs-uae.rb
@@ -1,6 +1,6 @@
 cask 'fs-uae' do
-  version '3.0.0'
-  sha256 '606ff910ffc6b50a4675ab4cace5a8a152aed1fd334c98bbac3a725bbace9f52'
+  version '3.0.1'
+  sha256 '3128732e18f479b800f11aa092c7bf0f139a88c06d14a885b15512446a72b3ae'
 
   url "https://fs-uae.net/stable/#{version}/FS-UAE-Suite_#{version}_macOS_x86-64.dmg"
   appcast 'https://fs-uae.net/stable/latest-macosx'
