7a706bc6f4f8d76d62e5c5e9ad07b1eeaf4fef39
Randall <ran.dall@icloud.com>
2019-11-05 18:44:23 -0800

    Update ableton-live from 10.1.3 to 10.1.4

--- Casks/ableton-live.rb
+++ Casks/ableton-live.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live' do
-  version '10.1.3'
-  sha256 '9e0766d403e4381b653c257e22b6ae47e108ddb61f345b57d4d2573efb807efa'
+  version '10.1.4'
+  sha256 'bedd8171a8d71a1d84c7ebcda797d06dbdf49447b46c49f7873c4b38fd035b3d'
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_trial_#{version}_64.dmg"
   appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
