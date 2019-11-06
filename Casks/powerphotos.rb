6730dea423cc8d0e7c7c21cc21d698be4fa8731f
Randall <ran.dall@icloud.com>
2019-11-05 17:45:39 -0800

    Update powerphotos

--- Casks/powerphotos.rb
+++ Casks/powerphotos.rb
@@ -16,8 +16,8 @@ cask 'powerphotos' do
     sha256 'e7c7d5970b734827a5f112029491d2d97f9a6bb318f457893905718bea6b595a'
     url "https://www.fatcatsoftware.com/powerphotos/PowerPhotos_#{version.no_dots}.zip"
   else
-    version '1.7.3'
-    sha256 '9debaa2e292a05767b017953b4fb1e900705d7380d4b462ac6a37d1a8fa73421'
+    version '1.7.4'
+    sha256 'e7c7d5970b734827a5f112029491d2d97f9a6bb318f457893905718bea6b595a'
     url 'https://www.fatcatsoftware.com/powerphotos/PowerPhotos.zip'
   end
 
