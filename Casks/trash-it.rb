d1531de35a783065b3c3eacdba30ea9a3c587e9e
Julian Mayer <julian@corecode.io>
2019-11-06 23:51:53 +0100

    Update trash-it

--- Casks/trash-it.rb
+++ Casks/trash-it.rb
@@ -1,6 +1,6 @@
 cask 'trash-it' do
   version '7.5'
-  sha256 '0a8e6e0c389dd23df321d435d0e63f13899732896c135860ba7e3a4c55e887ea'
+  sha256 '0d57f8f74587c012055b75b671bdfcd5739469ab1b371e317ee6df7e0c855c2b'
 
   url 'https://nonamescriptware.com/wp-content/uploads/Trashit.zip'
   appcast 'https://nonamescriptware.com/downloads/'
