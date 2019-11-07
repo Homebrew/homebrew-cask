97d4aed2383c9c269f07760df0f12ddd13125c82
suschizu <brigitte@corecode.io>
2019-11-07 10:47:06 +0100

    Update syncmate.rb

--- Casks/syncmate.rb
+++ Casks/syncmate.rb
@@ -2,7 +2,7 @@ cask 'syncmate' do
   version '8.0.469'
   sha256 '97eb95fdf49fb8d493a3b9691a9ec40b7df1d88b4c272aa238ad5e95cef5c3a1'
 
-  # cdn.electronic.us/products was verified as official when first introduced to the cask
+  # cdn.electronic.us/products/syncmate/ was verified as official when first introduced to the cask
   url "https://cdn.electronic.us/products/syncmate/mac/update/SyncMate_#{version}.zip"
   appcast 'https://cdn.eltima.com/download/syncmate-update/syncmate6.xml'
   name 'SyncMate'
