e159c3a4255893bc1f9e8fe140472637d95919d6
Vítor Galvão <info@vitorgalvao.com>
2019-11-07 21:40:23 +0000

    Update enzymex.rb

--- Casks/enzymex.rb
+++ Casks/enzymex.rb
@@ -1,8 +1,9 @@
 cask 'enzymex' do
-  version :latest
-  sha256 :no_check
+  version '3.3.3'
+  sha256 'abbb5439e80c2c9d4038d7805d0c29e2d372b83fcd66dba44ce165b555d34772'
 
   url 'http://downloads.nucleobytes.com/enzymex.dmg'
+  appcast 'https://nucleobytes.com/enzymex/index.html'
   name 'EnzymeX'
   homepage 'https://nucleobytes.com/enzymex/index.html'
 
