150b6ea3fc8d85f063272ced0b6983af22651761
Zhenfu Shi <szf1234@me.com>
2019-11-07 17:31:26 -0500

    Update macupdater (#72260)

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -1,8 +1,8 @@
 cask 'macupdater' do
   version '1.4.18'
-  sha256 '7bf7034bb5061b236a00f17efb51558d1468e32101db91b11afbc3b7f2f0482e'
+  sha256 'e73049ab156381849effe405d7974c066deb1b095b7d78b5a2499e198db2744d'
 
-  url "https://www.corecode.io/downloads/macupdater_#{version}.zip"
+  url "https://www.corecode.io/downloads/macupdater_#{version}.dmg"
   appcast 'https://www.corecode.io/macupdater/macupdater.xml'
   name 'MacUpdater'
   homepage 'https://www.corecode.io/macupdater/index.html'
