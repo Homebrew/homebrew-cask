6f3268d94c7f8341837c5f9b25573b53596b57ae
Vítor Galvão <info@vitorgalvao.com>
2019-11-07 21:52:05 +0000

    Update tuple.rb

--- Casks/tuple.rb
+++ Casks/tuple.rb
@@ -2,6 +2,7 @@ cask 'tuple' do
   version '0.53.0,2019-11-04-6350aa0b'
   sha256 '52647664c168eded3f15cd41e801388d8fbb9f140339ba3b6e8f2d14259c8d96'
 
+  # s3.us-east-2.amazonaws.com/tuple-releases was verified as official when first introduced to the cask
   url "https://s3.us-east-2.amazonaws.com/tuple-releases/production/sparkle/tuple-#{version.before_comma}-#{version.after_comma}.zip"
   appcast 'https://s3.us-east-2.amazonaws.com/tuple-releases/production/sparkle/appcast.xml'
   name 'Tuple'
