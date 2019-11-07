4028f9773d59c64809c111ae021e5f5627cc7005
Randall <ran.dall@icloud.com>
2019-11-07 10:56:09 -0800

    Update notable from 1.7.3 to 1.8.0

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -1,6 +1,6 @@
 cask 'notable' do
-  version '1.7.3'
-  sha256 '40740ab18fe83ecaa952b248c18cd6ad790726a54faa8f4860f6236b2757d4f1'
+  version '1.8.0'
+  sha256 '0a9043013a27a44711abeccb58d9bcb5cb09a7db6a617e130f7f5d10c3c8c2bc'
 
   url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg"
   appcast 'https://github.com/notable/notable/releases.atom'
