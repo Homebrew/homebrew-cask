e4f232a875d5b1193d45ed135a93cbfda938ea9b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-06 18:51:47 -0800

    Update marvin from 1.48.0 to 1.48.1 (#72212)

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -1,6 +1,6 @@
 cask 'marvin' do
-  version '1.48.0'
-  sha256 '2364b31d4ad49f95cd773a2d767daf1bcca9fe0ad5f1e25dcebe12e03f6ca866'
+  version '1.48.1'
+  sha256 '4321c51bae3244144196dbc2486a9b7e0c59cc1cdbef36af95d0d7e0eb537089'
 
   # amazingmarvin.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://amazingmarvin.s3.amazonaws.com/Marvin-#{version}.dmg"
