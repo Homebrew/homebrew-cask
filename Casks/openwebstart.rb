ca4ce2e882dd3973e673dcacaccb72a6b5bec557
Vítor Galvão <info@vitorgalvao.com>
2019-11-07 22:05:06 +0000

    Update openwebstart.rb

--- Casks/openwebstart.rb
+++ Casks/openwebstart.rb
@@ -2,8 +2,8 @@ cask 'openwebstart' do
   version '0.5.0'
   sha256 'a6c7ff4c2171a32c0d5bb4fe71743d5fa84f3b8458c16d292191aebdd1476a99'
 
-  # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/karakun/OpenWebStart/releases/download/v0.5.0/OpenWebStart_macos_0_5_0.dmg'
+  # github.com/karakun/OpenWebStart was verified as official when first introduced to the cask
+  url "https://github.com/karakun/OpenWebStart/releases/download/v#{version}/OpenWebStart_macos_#{version.dots_to_underscores}.dmg"
   appcast 'https://github.com/karakun/OpenWebStart/releases.atom'
   name 'OpenWebStart'
   homepage 'https://openwebstart.com/'
