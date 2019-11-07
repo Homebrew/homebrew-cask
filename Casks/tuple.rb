44084546fb59e9fc0b1e1ec9312df3970da5e346
Vítor Galvão <info@vitorgalvao.com>
2019-11-07 21:46:10 +0000

    Update tuple.rb

--- Casks/tuple.rb
+++ Casks/tuple.rb
@@ -1,8 +1,9 @@
 cask 'tuple' do
-  version :latest
-  sha256 :no_check
+  version '0.53.0,2019-11-04-6350aa0b'
+  sha256 '52647664c168eded3f15cd41e801388d8fbb9f140339ba3b6e8f2d14259c8d96'
 
-  url 'https://production.tuple.app/downloads/latest'
+  url "https://s3.us-east-2.amazonaws.com/tuple-releases/production/sparkle/tuple-#{version.before_comma}-#{version.after_comma}.zip"
+  appcast 'https://s3.us-east-2.amazonaws.com/tuple-releases/production/sparkle/appcast.xml'
   name 'Tuple'
   homepage 'https://tuple.app/'
 
