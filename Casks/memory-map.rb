9e4dce1ce7c96f41c76460ec8d924609cdd1bc96
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-06 19:06:12 -0800

    Update memory-map from 1.3 to 1.5 (#72221)

--- Casks/memory-map.rb
+++ Casks/memory-map.rb
@@ -1,6 +1,6 @@
 cask 'memory-map' do
-  version '1.3'
-  sha256 '12a2c800e9030196735e348c2ff80bc7fa6a3b121876c6a1e95c98bc8cc8e0d7'
+  version '1.5'
+  sha256 'dcb3b29c2f38b124dbf85278ded21e429b95e19cde0c856853d6842eaaac2c5f'
 
   url "https://memory-map.com/download/mac/Memory-Map.#{version}.pkg"
   appcast 'https://memory-map.com/Mapping_Software_Downloads.html'
