afe4bd6f10efbed3543ce5aa745345acddedc6dc
Ralph Martin <ralph@cs.cf.ac.uk>
2019-11-07 07:34:58 +0000

    Update integrity (#72226)
    * Update integrity
    
    * Update integrity.rb

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -1,6 +1,6 @@
 cask 'integrity' do
-  version '9.3.3'
-  sha256 '54bc2e62c180da073aa0b9732e407224f027a3874b46b98a3512107484bade89'
+  version '9.3.4'
+  sha256 'e4dd2d51d626d433128e3691717b699ef274631a232c5b3ff16e97db435cc01d'
 
   url 'https://peacockmedia.software/mac/integrity/integrity.dmg'
   appcast 'https://peacockmedia.software/mac/integrity/version_history.html'
