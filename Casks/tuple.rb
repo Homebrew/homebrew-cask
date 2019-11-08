b7946a8e92e3b6eaf9bad60565199f194431e5ef
suschizu <brigitte@corecode.io>
2019-11-08 20:08:18 +0100

    Update tuple from 0.53.0,2019-11-04-6350aa0b to 0.53.1,2019-11-07-110631ac (#72309)

--- Casks/tuple.rb
+++ Casks/tuple.rb
@@ -1,6 +1,6 @@
 cask 'tuple' do
-  version '0.53.0,2019-11-04-6350aa0b'
-  sha256 '52647664c168eded3f15cd41e801388d8fbb9f140339ba3b6e8f2d14259c8d96'
+  version '0.53.1,2019-11-07-110631ac'
+  sha256 'be4bb99757e08bb7e2d5193d89effcf3dfbdc8a5ef075c6d383b0c5032ecf020'
 
   # s3.us-east-2.amazonaws.com/tuple-releases was verified as official when first introduced to the cask
   url "https://s3.us-east-2.amazonaws.com/tuple-releases/production/sparkle/tuple-#{version.before_comma}-#{version.after_comma}.zip"
