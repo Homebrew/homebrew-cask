82ce1e295f4c8da22c902c7eb1736aeef2d7b10e
Randall <ran.dall@icloud.com>
2019-11-08 12:30:27 -0800

    Update superproductivity from 2.13.6 to 2.13.9

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,6 +1,6 @@
 cask 'superproductivity' do
-  version '2.13.6'
-  sha256 'ef56b037bb9b165ad81533dbb11cb1061aa4d1d266c83f5354d005c6c4badee3'
+  version '2.13.9'
+  sha256 'f6f45697215f3ca5eef8feba55c0aeec59f3ccd723d676577b2421d00dd962f2'
 
   # github.com/johannesjo/super-productivity was verified as official when first introduced to the cask
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip"
