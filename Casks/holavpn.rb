7061f4b33fa9b4e6d995121d6e2b158d2f4d6f70
Randall <ran.dall@icloud.com>
2019-11-08 13:02:09 -0800

    Update holavpn from 1.0.24 to 1.0.25

--- Casks/holavpn.rb
+++ Casks/holavpn.rb
@@ -1,6 +1,6 @@
 cask 'holavpn' do
-  version '1.0.24'
-  sha256 '5502448c404cc661897fae0fcca40ee503d7644314bd84db0486c13af057ee77'
+  version '1.0.25'
+  sha256 'a8f2468919174684210773ae01cc375feec8b83ccebfbd547c6bd230f2fd9b5f'
 
   url "https://cdn4.hola.org/static/HolaVPN-#{version}.dmg"
   appcast 'https://hola.org/macos_vpn_update.xml'
