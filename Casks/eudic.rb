01a3405c0f22d2f59b47a8b5e43f51cabba184ac
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2019-11-08 04:51:37 -0500

    Update eudic (#72281)

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,13 +1,13 @@
 cask 'eudic' do
-  version '3.9.2'
-  sha256 '6a8cf266a1fe0e94f33f93c2c37970057dd3075a1993f1a2161089ca92051803'
+  version '3.9.2,2019-10-08'
+  sha256 '8b50efeb0648681a2b38dbd511eca4146586282cfc1bf10b1a14736ddfa3ade3'
 
   # static.frdic.com was verified as official when first introduced to the cask
-  url 'https://static.frdic.com/pkg/eudicmac.dmg'
+  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.after_comma}"
   appcast 'https://www.eudic.net/update/eudic_mac.xml'
   name 'Eudic'
   name '欧路词典'
-  homepage 'https://www.eudic.net/eudic/mac_dictionary.aspx'
+  homepage 'https://www.eudic.net/v4/en/app/eudic'
 
   app 'Eudic.app'
 end
