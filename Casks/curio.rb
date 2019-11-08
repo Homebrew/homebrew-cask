ff2e6654ad7457b7b05e0cdf43847b3c9310f50f
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-08 13:35:50 -0800

    Update curio from 13020.7 to 13020.9 (#72331)

--- Casks/curio.rb
+++ Casks/curio.rb
@@ -1,6 +1,6 @@
 cask 'curio' do
-  version '13020.7'
-  sha256 '329c22433455cc72ed48dc181248ad1e95a3e33ab8fd9e87c572b70da97296a7'
+  version '13020.9'
+  sha256 '09b500dcfa6e2bb1ff93c0ac6eacd83dbd8ad23f611ba1ec93f3fdff8888c003'
 
   url "https://www.zengobi.com/downloads/Curio#{version.no_dots}.zip"
   appcast 'https://www.zengobi.com/appcasts/Curio13HLwLK2C84LaKptcz.xml'
