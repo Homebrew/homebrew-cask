3837256c1f8cb824120d7e1d88708de450a143c2
yesRabbit <chandler.lu1997@gmail.com>
2019-11-08 23:04:57 +0800

    Update adobe-dng-converter.rb (#72299)

--- Casks/adobe-dng-converter.rb
+++ Casks/adobe-dng-converter.rb
@@ -3,8 +3,8 @@ cask 'adobe-dng-converter' do
     version '9.6.1'
     sha256 '087eac5026667e4e6e3c156fd13243c9ea00f6c0238cbbb94d3099ae8772603f'
   else
-    version '11.4.1'
-    sha256 'f23b1576231962e0103107c0cbbbf88982d0d89b932e8400d4fc412c52704490'
+    version '12.0'
+    sha256 '567b84b46af4d4f7a323abe5ce2cdad5bd5e42ec41feb973513d89860f9d7c61'
   end
 
   url "https://download.adobe.com/pub/adobe/dng/mac/DNGConverter_#{version.dots_to_underscores}.dmg"
