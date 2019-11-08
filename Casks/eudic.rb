a4844205e7495cfffd8d885427a18f20803a47aa
Markus Reiter <me@reitermark.us>
2019-11-08 06:25:35 +0100

    Update eudic.rb

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,9 +1,9 @@
 cask 'eudic' do
-  version '3.9.2'
+  version '3.9.2,2019-10-08'
   sha256 '8b50efeb0648681a2b38dbd511eca4146586282cfc1bf10b1a14736ddfa3ade3'
 
   # static.frdic.com was verified as official when first introduced to the cask
-  url 'https://static.frdic.com/pkg/eudicmac.dmg?v=2019-10-08'
+  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.after_comma}"
   appcast 'https://www.eudic.net/update/eudic_mac.xml'
   name 'Eudic'
   name '欧路词典'
