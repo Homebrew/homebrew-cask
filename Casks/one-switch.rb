92a905f03c653be82a3b97a7c00b9a17573ca486
Work <brigitte@corecode.io>
2019-11-08 14:55:04 +0100

    Update one-switch

--- Casks/one-switch.rb
+++ Casks/one-switch.rb
@@ -1,6 +1,6 @@
 cask 'one-switch' do
   version '1.8'
-  sha256 'd75e2c50f947dc735ab52627909d55c951079efa8dde0cc67a80c98fedafab1b'
+  sha256 '03f045aa669f117a97a3f4ddd2d5aa2b0ecaf966d79de08cc13e65a6ae0913fc'
 
   # dl.devmate.com/studio.fireball.OneSwitch was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/studio.fireball.OneSwitch/OneSwitch.zip'
