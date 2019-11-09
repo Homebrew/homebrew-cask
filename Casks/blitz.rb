cfd2cb4fd4b787e10427d7f8d38825c3e633c617
Hannes Braun <hannesbraun@mail.de>
2019-11-09 11:07:58 +0100

    Update blitz from 1.3.19 to 1.4.2

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask 'blitz' do
-  version '1.3.19'
-  sha256 '05fabb20f942a600a2203da4aff990f62c6267a5dcb7d96a76fdb42164c79bfb'
+  version '1.4.2'
+  sha256 'c61475b422965e6148ff309b8a48091fdf948a069ce20b322be164ca49d0c04e'
 
   url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.blitz.gg/download/mac'
