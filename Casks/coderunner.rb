fd6cb9e2e0490a701a11e96b226718a0388d35ff
yesRabbit <chandler.lu1997@gmail.com>
2019-11-09 09:17:34 +0800

    Update coderunner.rb

--- Casks/coderunner.rb
+++ Casks/coderunner.rb
@@ -1,12 +1,15 @@
 cask 'coderunner' do
-  version '3.0.1'
-  sha256 '31976c2551102eb07c3c20b578664a0697a92299c53a4956bc7353c7440a81fa'
+  version '3.1'
+  sha256 'c85ee95a52af5986a0279a8191feff3f0c7d9f86b0f8030779b48b6a33541bee'
 
-  # dktfof1z89xc1.cloudfront.net was verified as official when first introduced to the cask
-  url "https://dktfof1z89xc1.cloudfront.net/CodeRunner-#{version}.zip"
+  # coderunner.nyc3.cdn.digitaloceanspaces.com was verified as official when first introduced to the cask
+  url " https://coderunner.nyc3.cdn.digitaloceanspaces.com/CodeRunner-#{version}.zip"
+ 
   appcast 'https://coderunnerapp.com/appcast.xml'
   name 'CodeRunner'
   homepage 'https://coderunnerapp.com/'
+  
+  auto_updates true
 
   app 'CodeRunner.app'
 end
