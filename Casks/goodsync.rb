37088e21fc6694e15f8938716ccd378a119c23fe
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-09 19:08:29 -0800

    Update goodsync from 10.10.12.2 to 10.10.13.0 (#72379)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.10.12.2'
-  sha256 '5b2a6a0a72e5059f30887d0111a96c98705b640c7d6f9330836365c5b5cd617d'
+  version '10.10.13.0'
+  sha256 'b14510c8318fe358846cb23ad246a5f37e7ae2a8ac1d178240a3ae6b6262167c'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://www.goodsync.com/download?os=macos',
