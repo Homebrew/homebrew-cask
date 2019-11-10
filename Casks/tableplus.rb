b61d0814bf358f07bb8acadc4c6b9d3c96fe1f42
Huy Pham <duchuykun@gmail.com>
2019-11-10 11:21:15 -0400

    Updated TablePlus build to 276.

--- Casks/tableplus.rb
+++ Casks/tableplus.rb
@@ -1,6 +1,6 @@
 cask 'tableplus' do
-  version '2.10.2,272'
-  sha256 'ee4f23b286f5407704d01c9b84eab0084eaa95fc90e234b5f85ca0a46a34edc3'
+  version '2.11,276'
+  sha256 '1704360e2bc97bec96b7606de80b93d415b917894996e71f68b1753ee2f5fc2c'
 
   # tableplus-osx-builds.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://tableplus-osx-builds.s3.amazonaws.com/#{version.after_comma}/TablePlus.dmg"
