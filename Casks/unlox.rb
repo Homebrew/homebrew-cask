099a5a7d74be83bbe9ce40835a208a5b9ee780b6
Sharjeel Junaid <SharjeelJ@users.noreply.github.com>
2019-11-10 14:08:40 -0700

    Update unlox from 3.0.1.0 to 3.0.2.0 (#72406)

--- Casks/unlox.rb
+++ Casks/unlox.rb
@@ -1,6 +1,6 @@
 cask 'unlox' do
-  version '3.0.1.0'
-  sha256 '9f88fcf526b673b7329d9efb8b5731277990d5781bf78cdfd6aa64b082c19bfa'
+  version '3.0.2.0'
+  sha256 '2a19e38a9d10211c905c25cf8c6f270c87a3e942c7888c82820d654f4b956a7a'
 
   url 'https://unlox.it/download/'
   appcast 'https://unlox.it/download/update.php'
