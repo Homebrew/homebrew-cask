8c0744eded2497deb3d432b22dbe3ad65231a40d
Randall <ran.dall@icloud.com>
2019-11-09 19:14:09 -0800

    Update hook from 2888,1572942499 to 2892,1573247948

--- Casks/hook.rb
+++ Casks/hook.rb
@@ -1,6 +1,6 @@
 cask 'hook' do
-  version '2888,1572942499'
-  sha256 'ea7c0a3897e0346a16d759d58800c093349b2161634218adc774438c3f1d3cfc'
+  version '2892,1573247948'
+  sha256 '38ae0af0c08e26f8925060a97b442a7e84b44d050282745587c59162c74f8ff3'
 
   # dl.devmate.com/com.cogsciapps.hook was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.cogsciapps.hook/#{version.before_comma}/#{version.after_comma}/Hook-#{version.before_comma}.dmg"
