3951921914f7dc8530e05129905b8fe2579c94bc
Qi Zhang <singularitti@outlook.com>
2019-11-10 05:05:20 -0500

    Update macupdater from 1.4.18 to 1.4.18,6420

--- Casks/macupdater.rb
+++ Casks/macupdater.rb
@@ -1,8 +1,8 @@
 cask 'macupdater' do
-  version '1.4.18'
+  version '1.4.18,6420'
   sha256 'e73049ab156381849effe405d7974c066deb1b095b7d78b5a2499e198db2744d'
 
-  url "https://www.corecode.io/downloads/macupdater_#{version}.dmg"
+  url "https://www.corecode.io/downloads/macupdater_#{version.before_comma}.dmg"
   appcast 'https://www.corecode.io/macupdater/macupdater.xml'
   name 'MacUpdater'
   homepage 'https://www.corecode.io/macupdater/index.html'
