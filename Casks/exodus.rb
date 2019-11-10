625a2c8ece801bac0d7b33608c5b23a1945773c0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-09 19:09:39 -0800

    Update exodus from 19.10.24 to 19.11.8 (#72377)

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,6 +1,6 @@
 cask 'exodus' do
-  version '19.10.24'
-  sha256 '3635edcd5bee28970baa8478d8c535fc72990928c0053f34034c7046bce02c20'
+  version '19.11.8'
+  sha256 '5d661298d8809af575bb4c168e6e65173a5b17874106ecab4bdcbd86bebc7bed'
 
   # exodusbin.azureedge.net was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-macos-#{version}.dmg"
