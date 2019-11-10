bb563d32d9fd4fc75db64e7810ef80872376cb1c
ahousseini <contact@anver.ch>
2019-11-10 17:51:47 -0600

    Update archi from 4.5.1 to 4.6.0

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -1,6 +1,6 @@
 cask 'archi' do
-  version '4.5.1'
-  sha256 '32c705fec35a3ff8384bb13da93545fbb1104f78488309c6a4851c0f963d68c3'
+  version '4.6.0'
+  sha256 '2cc75122d6e1fb9ce9cf99488aeb041c07cec0df237e9bd496f6f8dc4cf66cbc'
 
   url "https://www.archimatetool.com/downloads/#{version.no_dots}/Archi-Mac-#{version}.zip"
   appcast 'https://github.com/archimatetool/archi/releases.atom'
