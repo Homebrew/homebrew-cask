5b374b4e4c5f617729051367315610ac32b66606
Vítor Galvão <info@vitorgalvao.com>
2019-11-11 16:11:19 +0000

    Update checkra1n.rb

--- Casks/checkra1n.rb
+++ Casks/checkra1n.rb
@@ -3,6 +3,7 @@ cask 'checkra1n' do
   sha256 'dec472b249315a44a7a8caed016eaff2802a8b8d969d2fd299602a3af75ce3a0'
 
   url "https://checkra.in/assets/downloads/macos/#{sha256}/checkra1n%20beta%20#{version}.dmg"
+  appcast 'https://checkra.in/'
   name 'checkra1n'
   homepage 'https://checkra.in/'
 
