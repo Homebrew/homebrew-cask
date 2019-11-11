6634b16ae823f87f1210d11fde7efc1050df96ff
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2019-11-11 18:43:05 +0100

    qt-creator 4.10.2

--- Casks/qt-creator.rb
+++ Casks/qt-creator.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator' do
-  version '4.10.1'
-  sha256 '5567ba20b6389268d347f2b78f25b2471bbebd25af7ed745a2a252d14b2cbbae'
+  version '4.10.2'
+  sha256 '5f650600b7c46a1357cc66959dece896c2de2ee49bfb787ae3e65d736a7dd37e'
 
   url "https://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   appcast 'https://download.qt.io/official_releases/qtcreator/',
