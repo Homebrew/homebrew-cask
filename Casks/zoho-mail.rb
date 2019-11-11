22977253100746cdc1b34cf1aaca7b74d5cd7102
Nick Wind-McJetters <nick@elearningexperts.net>
2019-11-11 15:04:58 -0500

    Update zoho-mail from 1.0.16 to 1.0.17 (#72350)

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -1,10 +1,10 @@
 cask 'zoho-mail' do
-  version '1.0.16'
-  sha256 '70a9408a4e20596ccaa933196c64edd06d1edab8c70a9d01249f3a8ef4984f3e'
+  version '1.0.17'
+  sha256 '7470adcc126cd94a6973be5efc4821ae3f3bc4a073a100f799841cda6be3625d'
 
   # downloads.zohocdn.com/zmail-desktop/mac was verified as official when first introduced to the cask
   url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-installer-v#{version}.dmg"
-  appcast 'https://www.zoho.com/mail/desktop/artifacts.json'
+  appcast 'https://downloads.zohocdn.com/zmail-desktop/artifacts.json'
   name 'Zoho Mail'
   homepage 'https://www.zoho.com/mail/desktop/'
 
