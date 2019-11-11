5e63af6dca9b1d7f4a340d0b624f750cb4ac9ff9
CoreCode <core-code@users.noreply.github.com>
2019-11-11 02:02:45 +0100

    Update wickrme.rb (#72404)

--- Casks/wickrme.rb
+++ Casks/wickrme.rb
@@ -4,7 +4,7 @@ cask 'wickrme' do
 
   # s3.amazonaws.com/static.wickr.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/me/WickrMe-#{version}.dmg"
-  appcast 'https://rink.hockeyapp.net/api/2/apps/5af5d569264b4cc68e1f5156e8f80fb9'
+  appcast 'https://pro-download.wickr.com/api/multiVerify/pro/undefined/'
   name 'Wickr Me'
   homepage 'https://wickr.com/products/personal/'
 
