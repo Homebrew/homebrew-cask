a008ca0dc34d72d359d91305cda5e0cd5dd7eb55
gWOLF3 <wolfe.t.glenn@gmail.com>
2019-11-10 21:56:23 -0500

    update release

--- Casks/moter.rb
+++ Casks/moter.rb
@@ -1,14 +1,16 @@
 cask 'moter' do
-  version 'v0.0.1'
-  sha256 '06c99c133c4f689d496bd1e955f4b1cee91a1adf236acda26b389893c4e22485'
+  version 'v0.0.2'
+  sha256 '6b072440e4bb5ff08cd937d79be34580e8427132637d36ceef05de9484e49ee8'
 
-  url 'https://github.com/gWOLF3/moter/releases/download/v0.0.1/moter'
+  url 'https://github.com/gWOLF3/moter/releases/download/v0.0.2/moter'
   appcast 'https://github.com/gWOLF3/moter/releases.atom'
   name 'Moter'
   homepage 'https://github.com/gWOLF3/moter'
 
   depends_on cask: 'vlc'
   depends_on cask: 'spectacle'
+  depends_on cask: 'iterm2'
+
 
   binary 'moter'
 end
