41b097888b52ee3eff4ad1968b893ad064724e59
suschizu <brigitte@corecode.io>
2019-11-11 16:37:33 +0100

    Update goodsync from 10.10.13.0 to 10.10.13.3 (#72426)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,5 +1,5 @@
 cask 'goodsync' do
-  version '10.10.13.0'
+  version '10.10.13.3'
   sha256 'b14510c8318fe358846cb23ad246a5f37e7ae2a8ac1d178240a3ae6b6262167c'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
