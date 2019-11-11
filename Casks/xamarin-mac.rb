c3a8079e01dab1ba25ceeffc50c1d550b0794572
Vítor Galvão <info@vitorgalvao.com>
2019-11-11 18:06:44 +0000

    Update xamarin-mac.rb

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -3,7 +3,7 @@ cask 'xamarin-mac' do
   sha256 '06b714e24237dd29e9bc61a557fa236f9f6a3885808794aaed211a2bbb97f10e'
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
-  appcast 'https://docs.microsoft.com/en-us/xamarin/mac/release-notes/'
+  appcast 'https://docs.microsoft.com/en-us/xamarin/mac/release-notes/',
           configuration: version.major_minor
   name 'Xamarin Mac'
   homepage 'https://www.xamarin.com/platform'
