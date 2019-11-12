0434c674c29769b0733062dc21d26c8f5bae3ba0
Randall <ran.dall@icloud.com>
2019-11-11 17:23:21 -0800

    Update notable from 1.8.0 to 1.8.1

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -1,6 +1,6 @@
 cask 'notable' do
-  version '1.8.0'
-  sha256 '0a9043013a27a44711abeccb58d9bcb5cb09a7db6a617e130f7f5d10c3c8c2bc'
+  version '1.8.1'
+  sha256 '406a72e4ac9a235c36cafc4724b75e36c91de0c4570d560b088504d28207468d'
 
   url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg"
   appcast 'https://github.com/notable/notable/releases.atom'
