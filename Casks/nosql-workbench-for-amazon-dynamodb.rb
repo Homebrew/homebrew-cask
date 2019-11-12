1f3326ec25bb0df2ee9819e1648efb9b05637283
Kirk Kirkconnell <kirkcon@amazon.com>
2019-11-12 08:41:41 -0800

    Updated NoSQL Workbench for Amazon DynamoDB to 0.3.0

--- Casks/nosql-workbench-for-amazon-dynamodb.rb
+++ Casks/nosql-workbench-for-amazon-dynamodb.rb
@@ -2,7 +2,7 @@ cask 'nosql-workbench-for-amazon-dynamodb' do
   version '0.3.0'
   sha256 '8cf0d7eac9d73a1f5c761dcfe22e617cc384c5d27a49c80d0258070d548201fe'
 
-  # nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com was verified as official when first introduced to the cask
+  # nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com was verified as official local for binary
   url "https://nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com/NoSQL+Workbench+for+Amazon+DynamoDB+(Preview)-mac-#{version}.dmg"
   appcast 'https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/workbench.settingup.partial.html'
   name 'NoSQL Workbench for Amazon DynamoDB'
