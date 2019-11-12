c51a052dd78e9ca103969ba89bb843415439eb6a
ahousseini <contact@anver.ch>
2019-11-12 06:35:39 -0600

    Update plex-media-server from 1.18.1.1973-0f4abfbcc to 1.18.1.2019-c186313fe

--- Casks/plex-media-server.rb
+++ Casks/plex-media-server.rb
@@ -1,6 +1,6 @@
 cask 'plex-media-server' do
-  version '1.18.1.1973-0f4abfbcc'
-  sha256 'b6803a8ffb62182853e30f66414b7078266625497dfbccbcd40fcd7dbe810fe7'
+  version '1.18.1.2019-c186313fe'
+  sha256 'bea00ee9ea1285c4d51749372d0aa09330bd2a0a8e5c31c3d98726ab210eb200'
 
   url "https://downloads.plex.tv/plex-media-server-new/#{version}/macos/PlexMediaServer-#{version}-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/5.json'
