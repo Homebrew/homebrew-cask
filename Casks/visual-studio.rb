270928eb99535eb6dff9f7f85fee3b30f1a20436
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2019-11-11 18:12:18 -0600

    Update visual-studio from 8.3.7.1 to 8.3.8.8 (#72441)

--- Casks/visual-studio.rb
+++ Casks/visual-studio.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio' do
-  version '8.3.7.1'
-  sha256 '3cf438a970507d8da09d3d433c9a527b88b1036b679a278e2432656c394c4f12'
+  version '8.3.8.8'
+  sha256 '0a3cd5a850a387fb115fd51e45c4df6b966da815ec20ad454327ee2e34ed1807'
 
   # dl.xamarin.com/VsMac was verified as official when first introduced to the cask
   url "https://dl.xamarin.com/VsMac/VisualStudioForMac-#{version}.dmg"
