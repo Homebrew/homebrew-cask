3a2bbe3847cd0015671cc44e7a5ad0c4a2c99521
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-12 15:51:50 -0800

    Update augur from 1.16.4 to 1.16.5 (#72501)

--- Casks/augur.rb
+++ Casks/augur.rb
@@ -1,6 +1,6 @@
 cask 'augur' do
-  version '1.16.4'
-  sha256 '0841eede1d41a00465c31b65b74d8ad52bfd640c8ced9c473456b95e0e3a73e7'
+  version '1.16.5'
+  sha256 '0860f41a6c8e558f86fa5430eb0c0a03e09053ce5ebefd5025d30d8e7ea16f40'
 
   url "https://github.com/AugurProject/augur-app/releases/download/v#{version}/mac-Augur-#{version}.dmg"
   appcast 'https://github.com/AugurProject/augur-app/releases.atom'
