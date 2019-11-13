cc51afaeb5f919485063b44b070087822c1e4332
Randall <ran.dall@icloud.com>
2019-11-12 16:10:11 -0800

    Update gyazmail from 1.6.2 to 1.6.3

--- Casks/gyazmail.rb
+++ Casks/gyazmail.rb
@@ -1,6 +1,6 @@
 cask 'gyazmail' do
-  version '1.6.2'
-  sha256 '4d23d0de517aa1558515a7fce4eb6c6ffc1de55538b697c9033a21c1f4218e34'
+  version '1.6.3'
+  sha256 '2a2cfb1b74cc8d6647a5f6fec9759491b3981d524bafe9846ed9ad25744987e1'
 
   url "http://gyazsquare.com/gyazmail/GyazMail-#{version.no_dots}.dmg"
   appcast 'http://gyazsquare.com/gyazmail/'
