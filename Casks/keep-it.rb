adb9d417cc42e7845f980fd2188190121bca708b
Randall <ran.dall@icloud.com>
2019-11-12 16:03:31 -0800

    Update keep-it from 1.7.5 to 1.7.6

--- Casks/keep-it.rb
+++ Casks/keep-it.rb
@@ -1,6 +1,6 @@
 cask 'keep-it' do
-  version '1.7.5'
-  sha256 '83b61919958fa3aabea4f2ceee7a22b52755a3dce2681b3be2691ee5e532d0ba'
+  version '1.7.6'
+  sha256 'c6c7389bd5902e4812d166f309d96250b6d0ec945c064de1b8d8e898eef17a43'
 
   url "https://reinventedsoftware.com/keepit/downloads/KeepIt_#{version}.dmg"
   appcast 'https://reinventedsoftware.com/keepit/downloads/keepit.xml'
