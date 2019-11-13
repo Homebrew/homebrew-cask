dd1c0103d2ab5a681759f4229ed4a32c385b3fb9
Randall <ran.dall@icloud.com>
2019-11-12 18:15:48 -0800

    Update memory

--- Casks/memory.rb
+++ Casks/memory.rb
@@ -1,9 +1,9 @@
 cask 'memory' do
   version '2019.31'
-  sha256 'e4ecaa2ca639b90d07e21fb1960957cc5cbfbc3c37ecbeaeff196cf5d79bd93f'
+  sha256 '5313247b3c80ff9cebfc0cb9411d9a8d8b3d27b1aec046b9514f2698ce1bfa4b'
 
   # timelytimetracking.s3.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://timelytimetracking.s3.amazonaws.com/mac_tracker/Memory%20Tracker%20by%20Timely.zip'
+  url 'https://timelytimetracking.s3.amazonaws.com/mac_tracker/Memory.zip'
   appcast 'https://timelytimetracking.s3.amazonaws.com/mac_tracker/sparkle.xml'
   name 'Memory Tracker by Timely'
   homepage 'https://timelyapp.com/'
