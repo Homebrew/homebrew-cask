e9fb426b5cbc2bfefbe674aef46fa68782fda03c
Zhenfu Shi <szf1234@me.com>
2019-11-13 18:04:50 -0500

    Update caprine from 2.41.0 to 2.41.1 (#72579)

--- Casks/caprine.rb
+++ Casks/caprine.rb
@@ -1,6 +1,6 @@
 cask 'caprine' do
-  version '2.41.0'
-  sha256 'ca8dd863ad1b5f2b624db74bf3fc84762cd36bd54333dea328420ef88721ab37'
+  version '2.41.1'
+  sha256 '9cea7e038d730d047d5621cda40723e9d446695ca4314664ca50bd6227d0b405'
 
   url "https://github.com/sindresorhus/caprine/releases/download/v#{version}/Caprine-#{version}.dmg"
   appcast 'https://github.com/sindresorhus/caprine/releases.atom'
