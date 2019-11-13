66268738560f10d579480653f978ad4872c0adbb
suschizu <brigitte@corecode.io>
2019-11-13 23:32:10 +0100

    Update firecamp.rb

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -1,9 +1,10 @@
 cask 'firecamp' do
-  version '0.7.0-rc-1'
-  sha256 '6a4e68e38e17c6a88972408cbcc9ae321b29d60e784bf5b751a07b5821ce2200'
+  version '0.6.7'
+  sha256 '984322a9f32c0afd0f443df5cf2699ebd6500b05bfa46df7c0f4e01520a58032'
 
   # firecamp.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg"
+  appcast 'https://firecamp.app/docs/installation'
   name 'Firecamp'
   homepage 'https://firecamp.app/'
 
