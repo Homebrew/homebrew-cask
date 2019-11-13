b213288f44fd8b2130a04d248b628160ffd07bee
Giles Winstanley <g.winstanley@snaq.net>
2019-11-13 10:47:59 +0000

    Update arrayfire from 3.6.1 to 3.6.4 (#72545)

--- Casks/arrayfire.rb
+++ Casks/arrayfire.rb
@@ -1,6 +1,6 @@
 cask 'arrayfire' do
-  version '3.6.1'
-  sha256 '4afa07262d4c48a682c13901dd09ad2aadfe988212379c2ed78cc9c45049a5d0'
+  version '3.6.4'
+  sha256 '7ca7f9280fd7bda18fa78275c9d4ce2ea3885307afb29e5b8793711394fd9995'
 
   # arrayfire.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://arrayfire.s3.amazonaws.com/#{version}/ArrayFire-v#{version}_OSX_x86_64.pkg"
