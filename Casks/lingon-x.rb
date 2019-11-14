3e81a27fe7eacb462c702a9c53af55583273d9a8
Work <brigitte@corecode.io>
2019-11-14 15:29:14 +0100

    Update lingon-x from 7.4.4 to 7.4.5

--- Casks/lingon-x.rb
+++ Casks/lingon-x.rb
@@ -3,8 +3,8 @@ cask 'lingon-x' do
     version '6.6.5'
     sha256 'b0231b1a98dcc8f5c4234b419c9f5331407b8cce29b33f0ea2e32b12595adfa8'
   else
-    version '7.4.4'
-    sha256 '8a885e430297f57ae39ad764abf788a96fa1df4574ab2d656a9ebbc2962e7337'
+    version '7.4.5'
+    sha256 'c283192e6c5940b4c463533b092790bcd946d75bf1d4c489e133142177974305'
   end
 
   url "https://www.peterborgapps.com/downloads/LingonX#{version.major}.zip"
