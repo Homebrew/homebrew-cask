fd055eb0e2e39a5c2d4fb132d3689335e81244f8
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2019-11-13 18:10:25 -0600

    Update visual-studio-code from 1.40.0 to 1.40.1 (#72582)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.40.0'
-  sha256 '8142a55ab7662cb7b5d87385d219d5e483e24c90e0e306fffa2d11342fdebdfb'
+  version '1.40.1'
+  sha256 '83d8cc9f5491b8c7bcaa087c61c728b980f42f5f39c0d84b65ffd9990199cc38'
 
   url "https://update.code.visualstudio.com/#{version}/darwin/stable"
   appcast 'https://vscode-update.azurewebsites.net/api/update/darwin/stable/VERSION'
