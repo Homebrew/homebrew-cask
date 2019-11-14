b09839bdfe5fb5ade801c62137367d737c653fd6
tfd0005 <thomas.delozier@unthsc.edu>
2019-11-13 23:58:05 -0600

    Update flash-player from 32.0.0.270 to 32.0.0.293

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -1,6 +1,6 @@
 cask 'flash-player' do
-  version '32.0.0.270'
-  sha256 '9ac063749f23bca4b78bee74136a2f739fae8db463a344f1ca2ff0b6d64ade3a'
+  version '32.0.0.293'
+  sha256 '41fa22891cebafcd2882fe59ed28ef9c790d9211a1c2421ad6c3698e66ad5bf8'
 
   url "https://fpdownload.adobe.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
   appcast 'https://fpdownload.adobe.com/pub/flashplayer/update/current/xml/version_en_mac_pl.xml',
