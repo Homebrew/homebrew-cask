13e879e9237d519df04116e7f0b7ca4195806e2f
tiiiecherle <tiiiecherle@gmail.com>
2019-11-14 14:55:05 +0100

    Update skype from 8.54.0.85 to 8.54.0.91

--- Casks/skype.rb
+++ Casks/skype.rb
@@ -1,6 +1,6 @@
 cask 'skype' do
-  version '8.54.0.85'
-  sha256 '014f94e6c7b057dcc6d0abba730c7e87615dc4d632a44538d5b9fe94dd1df854'
+  version '8.54.0.91'
+  sha256 '820d6769e5988253f358632d1c292db57167715af08ca5fd0a6c005a6de848a2'
 
   # endpoint920510.azureedge.net/s4l/s4l/download/mac was verified as official when first introduced to the cask
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg"
