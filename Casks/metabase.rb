8901cbcad236f767d1402c06751742ef5db3998a
suschizu <brigitte@corecode.io>
2019-11-15 00:18:53 +0100

    Update metabase from 0.33.5.0 to 0.33.5.1 (#72668)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,5 +1,5 @@
 cask 'metabase' do
-  version '0.33.5.0'
+  version '0.33.5.1'
   sha256 '4c53a18b92bb55e88fff1551b9bdc1e083eb8fce3081d6d4af3a88539ac486f0'
 
   # s3.amazonaws.com/downloads.metabase.com was verified as official when first introduced to the cask
