433703b8fa0c7acdd108bd4cff16f4b9cf9df5a7
Tom Brek <me@tombrek.com>
2019-11-14 13:27:02 +0000

    Update lastpass from 4.36.0 to 4.37.0 (#72613)

--- Casks/lastpass.rb
+++ Casks/lastpass.rb
@@ -1,6 +1,6 @@
 cask 'lastpass' do
-  version '4.36.0'
-  sha256 'cf9fa68e6596904732960a67521b79dd8f9bc95f920af9c9425886fbe23741ce'
+  version '4.37.0'
+  sha256 '66d6359fab75c9f04355b64f20bc8437c4c4b8679d57e4627fb0127b50c85e5c'
 
   url 'https://download.cloud.lastpass.com/mac/LastPass.dmg'
   appcast 'https://download.cloud.lastpass.com/mac/AppCast.xml'
