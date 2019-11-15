9fc255b8b26abc98d642dcbf187993d11ffc3b9f
Rick van Hattem <Wolph@wol.ph>
2019-11-15 14:55:01 +0100

    Added Ivideon server 3.8.0

---
+++ Casks/ivideon-server.rb
@@ -0,0 +1,11 @@
+cask 'ivideon-server' do
+  version '3.8.0'
+  sha256 '6e8372627144cef24b8b7425ea123436cd8d1de9277aefb19470cdc822d29109'
+
+  # updates.iv-cdn.com was verified as official when first introduced to the cask
+  url "https://updates.iv-cdn.com/bundles/ivideon_server/#{version}/IvideonServer_#{version}_macosx-x86-64.dmg"
+  name 'Ivideon Client'
+  homepage 'https://www.ivideon.com/'
+
+  app 'IvideonServer.app'
+end
