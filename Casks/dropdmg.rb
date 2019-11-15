45cf4dc2b7a2642dfef75d30b9f4fab4367868de
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-15 14:49:06 -0800

    Update dropdmg from 3.5.8 to 3.5.9 (#72702)

--- Casks/dropdmg.rb
+++ Casks/dropdmg.rb
@@ -1,6 +1,6 @@
 cask 'dropdmg' do
-  version '3.5.8'
-  sha256 '1b137bca48a0af0cc6a58e0ad0e33e84c54afd756c3eab39843cb3f50004bf96'
+  version '3.5.9'
+  sha256 '3e84c0a84f9413ff3dc3a173e007bf7b736c1811a2ce799b0e1ad15903d60b3f'
 
   url "https://c-command.com/downloads/DropDMG-#{version}.dmg"
   appcast 'https://c-command.com/dropdmg/'
