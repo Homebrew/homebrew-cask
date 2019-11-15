7554565dfd4047dcea24e1c2c4a9ae559b9c1099
Rick van Hattem <Wolph@wol.ph>
2019-11-15 14:55:19 +0100

    Added Ivideon client 6.8.1

---
+++ Casks/ivideon-client.rb
@@ -0,0 +1,11 @@
+cask 'ivideon-client' do
+  version '6.8.1'
+  sha256 '970959428ed0d55c61db81617e36c65fce5b3ad0c80dd10dd16d2ed358458bef'
+
+  # updates.iv-cdn.com was verified as official when first introduced to the cask
+  url "https://updates.iv-cdn.com/bundles/ivideon_client/#{version}/IvideonClient_#{version}_macosx-x86-64.dmg"
+  name 'Ivideon Client'
+  homepage 'https://www.ivideon.com/'
+
+  app 'IvideonClient.app'
+end
