cbf920abe6593d1df03e3fcc07f6c020189515b4
Qi Zhang <singularitti@outlook.com>
2019-11-16 09:23:35 -0500

    Update latexit from 2.13.4 to 2.13.5

--- Casks/latexit.rb
+++ Casks/latexit.rb
@@ -1,6 +1,6 @@
 cask 'latexit' do
-  version '2.13.4'
-  sha256 '11acd8c7925607c4177022ad1d459bcf63e52f4f346ee76ea50f2c3975cbc12c'
+  version '2.13.5'
+  sha256 'fa6fb0a6717ede77fc14c7f63260768ab6a9f280d04178c0336f7144acff3b20'
 
   url "https://www.chachatelier.fr/latexit/downloads/LaTeXiT-#{version.dots_to_underscores}.dmg",
       user_agent: :fake
