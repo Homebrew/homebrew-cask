10f96d63ce4d10d6009f40ffebc630aa94c6ff9d
Vítor Galvão <info@vitorgalvao.com>
2019-11-17 21:57:11 +0000

    Update livechat.rb

--- Casks/livechat.rb
+++ Casks/livechat.rb
@@ -3,7 +3,7 @@ cask 'livechat' do
   sha256 '55b8bc5793f7dc0aa220ff6e4df61ebb7d8102b42374b62d7821f032eef10668'
 
   url 'https://www.livechatinc.com/download/Mac/LiveChat.dmg'
-  appcast 'https://rink.hockeyapp.net/api/2/apps/e64c8a48eb174b2fab6afdbd4ff76d31.rss'
+  appcast 'https://s3.amazonaws.com/dal-livechat-main-web/download/Mac/latest-mac.yml'
   name 'LiveChat'
   homepage 'https://www.livechatinc.com/'
 
