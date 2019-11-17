9ccb301a012f209b421c9b00821c76e2df3742da
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2019-11-17 23:12:36 +0100

    Update livechat from 3.6.5 to 9.0.6 (#72725)

--- Casks/livechat.rb
+++ Casks/livechat.rb
@@ -1,9 +1,9 @@
 cask 'livechat' do
-  version '3.6.5'
-  sha256 'a807030bf49acaec1e35c4e74a3e1912c73f03cd6502cac280798a6bee0a446e'
+  version '9.0.6'
+  sha256 '55b8bc5793f7dc0aa220ff6e4df61ebb7d8102b42374b62d7821f032eef10668'
 
   url 'https://www.livechatinc.com/download/Mac/LiveChat.dmg'
-  appcast 'https://rink.hockeyapp.net/api/2/apps/e64c8a48eb174b2fab6afdbd4ff76d31.rss'
+  appcast 'https://s3.amazonaws.com/dal-livechat-main-web/download/Mac/latest-mac.yml'
   name 'LiveChat'
   homepage 'https://www.livechatinc.com/'
 
