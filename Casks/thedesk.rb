e86b095c89542a784f4c578124af62159c589110
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-18 16:38:25 -0800

    Update thedesk from 20.0.3 to 20.0.4 (#72796)

--- Casks/thedesk.rb
+++ Casks/thedesk.rb
@@ -1,6 +1,6 @@
 cask 'thedesk' do
-  version '20.0.3'
-  sha256 '4ebb86c3e98e5b1362969a7d0466b08dea5d7a610b4c29016553dbf9dcd3c787'
+  version '20.0.4'
+  sha256 'e67a5e156422581ae1c7d70f612f97629f6895d1f822c22603a3ac61e5e49620'
 
   # github.com/cutls/TheDesk was verified as official when first introduced to the cask
   url "https://github.com/cutls/TheDesk/releases/download/v#{version}/TheDesk-#{version}.dmg"
