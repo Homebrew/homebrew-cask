cd5e4e13e2084c751b18928c965ae55904f499ac
suschizu <brigitte@corecode.io>
2019-11-19 02:19:06 +0100

    Update proclaim.rb (#72788)

--- Casks/proclaim.rb
+++ Casks/proclaim.rb
@@ -1,9 +1,10 @@
 cask 'proclaim' do
-  version :latest
-  sha256 :no_check
+  version '2.11.0.0084'
+  sha256 'e084a936172d9fb81ef630b95f0e4875cb731b232aa42b859a75bfe1adf453fd'
 
   # logoscdn.com/Proclaim was verified as official when first introduced to the cask
-  url 'https://downloads.logoscdn.com/Proclaim/Installer/Proclaim.dmg'
+  url "https://downloads.logoscdn.com/Proclaim/Installer/#{version}/Proclaim.dmg"
+  appcast 'http://downloads.proclaimonline.com/mac/appcast.xml'
   name 'Faithlife Proclaim'
   homepage 'https://proclaim.faithlife.com/'
 
