a91dc7c2b0497fc61f8e8ef489fda0fffa93ede8
Marcel Dold <16888873+1337MARCEL@users.noreply.github.com>
2019-11-19 11:51:35 +0100

    Update blitz from 1.4.4 to 1.4.5 (#72810)

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask 'blitz' do
-  version '1.4.4'
-  sha256 'd0189b41e23cfa4e5a8e6aff3f140eb1e96671f5eafd2636fea99f7002eff379'
+  version '1.4.5'
+  sha256 'ef99f70337607bc0e61dce9f9bd9ed505806e12e729af0d520d939fb604a004d'
 
   url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.blitz.gg/download/mac'
