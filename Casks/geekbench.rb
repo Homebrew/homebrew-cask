Homebrew/homebrew-cask

commit b66a69b39d235108b18d5062d31c4053d015e000

Update geekbench from 4.4.2 to 5.0.0 (#68537)

Casks/geekbench.rb
@@ -2,9 +2,12 @@ cask 'geekbench' do
   if MacOS.version <= :mavericks
     version '3.4.2'
     sha256 '05e1b977a46648d38cf6c641be7ef34722200d0168a10d4372fca771ffa24e28'
-  else
+  elsif MacOS.version <= :mavericks
     version '4.4.2'
     sha256 '3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc'
+  else
+    version '5.0.0'
+    sha256 '0d3cff078329ba70c43f15028098a8f0be780e571203f26e5252a96ec4f2c568'
   end
 
   url "https://cdn.geekbench.com/Geekbench-#{version}-Mac.zip"

commit 771b49b71de650e9d7a305d7d93a02525255977d

geekbench: version 5 dropped support for macOS <= Sierra
This change is required so that version 4 is installed on machines
where version 5 would not run.

Fixes a regression introduced in b66a69b39d235108b18d5062d31c4053d015e000.

Casks/geekbench.rb
@@ -2,7 +2,7 @@ cask 'geekbench' do
   if MacOS.version <= :mavericks
     version '3.4.2'
     sha256 '05e1b977a46648d38cf6c641be7ef34722200d0168a10d4372fca771ffa24e28'
-  elsif MacOS.version <= :mavericks
+  elsif MacOS.version <= :sierra
     version '4.4.2'
     sha256 '3c46e630a28a0752afd702fc1cd379edd2420001be22302c932e61751284c0cc'
   else