e08e536713d462a525adaa79c1712ad9780d5080
Hasintha Indrajee <hasinthaindrajee@gmail.com>
2019-11-19 01:11:43 +0000

    update cellery to 0.5.0 (#72781)

--- Casks/cellery.rb
+++ Casks/cellery.rb
@@ -1,6 +1,6 @@
 cask 'cellery' do
-  version '0.4.0'
-  sha256 'c0877840a975ead887edac9fb4a602c9a0317fcd51b7a15375cbc32183343c4c'
+  version '0.5.0'
+  sha256 '1a93a344a0e5596ff8b39f16153619740188f21c9feacf3ab476d8f736950b57'
 
   # github.com/wso2-cellery/sdk was verified as official when first introduced to the cask
   url "https://github.com/wso2-cellery/sdk/releases/download/v#{version}/cellery-macos-installer-x64-#{version}.pkg"
@@ -8,8 +8,10 @@ cask 'cellery' do
   name 'Cellery'
   homepage 'https://wso2-cellery.github.io/'
 
+  depends_on cask: 'osxfuse'
   depends_on formula: 'python'
   depends_on formula: 'torsocks'
+  depends_on formula: 'sshfs'
 
   pkg "cellery-macos-installer-x64-#{version}.pkg"
 
