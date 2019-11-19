0beebb1be982822c6886200054028452ac50a074
afsharsafavi <rafsharsafavi@gmail.com>
2019-11-20 02:21:28 +0330

    Update rar.rb (#72836)
    Hi
    This is the latest version of rar

--- Casks/rar.rb
+++ Casks/rar.rb
@@ -1,6 +1,6 @@
 cask 'rar' do
-  version '5.7.1'
-  sha256 'e43d953db5ed0382ae99447d5812b063ed52305652c4fcd36482c27f18b2fde5'
+  version '5.8.b4'
+  sha256 '0386db6ca2c409693e37803722e74527298e5da1fa36afbee3c5186138effaab'
 
   url "https://www.rarlab.com/rar/rarosx-#{version}.tar.gz"
   name 'RAR Archiver'
