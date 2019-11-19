cb9e607a00c03869b5a0f305404b196fcfcbe977
commitay <commitay@users.noreply.github.com>
2019-11-19 11:56:38 +1000

    Update virtualbox install

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -9,7 +9,29 @@ cask 'virtualbox' do
 
   conflicts_with cask: 'virtualbox-beta'
 
-  pkg 'VirtualBox.pkg'
+  pkg 'VirtualBox.pkg',
+      choices: [
+                 {
+                   'choiceIdentifier' => 'choiceVBoxKEXTs',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 1,
+                 },
+                 {
+                   'choiceIdentifier' => 'choiceVBox',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 1,
+                 },
+                 {
+                   'choiceIdentifier' => 'choiceVBoxCLI',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 1,
+                 },
+                 {
+                   'choiceIdentifier' => 'choiceOSXFuseCore',
+                   'choiceAttribute'  => 'selected',
+                   'attributeSetting' => 0,
+                 },
+               ]
 
   postflight do
     # If VirtualBox is installed before `/usr/local/lib/pkgconfig` is created by Homebrew, it creates it itself with incorrect permissions that break other packages
@@ -22,17 +44,18 @@ cask 'virtualbox' do
                        args:       ['--unattended'],
                        sudo:       true,
                      },
-            pkgutil: 'org.virtualbox.pkg.*'
+            pkgutil: 'org.virtualbox.pkg.*',
+            delete:  '/usr/local/bin/vboximg-mount'
 
   zap trash: [
                '/Library/Application Support/VirtualBox',
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualbox.sfl*',
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualboxvm.sfl*',
-               '~/Library/VirtualBox',
                '~/Library/Preferences/org.virtualbox.app.VirtualBox.plist',
                '~/Library/Preferences/org.virtualbox.app.VirtualBoxVM.plist',
                '~/Library/Saved Application State/org.virtualbox.app.VirtualBox.savedState',
                '~/Library/Saved Application State/org.virtualbox.app.VirtualBoxVM.savedState',
+               '~/Library/VirtualBox',
              ],
       rmdir: '~/VirtualBox VMs'
 
