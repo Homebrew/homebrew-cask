6f15738faea1dc1ea5ddeed1e1fd2906ad7d771c
Randall <ran.dall@icloud.com>
2019-11-18 16:19:19 -0800

    Update container-ps from 1.2.0 to 1.2.1

--- Casks/container-ps.rb
+++ Casks/container-ps.rb
@@ -1,6 +1,6 @@
 cask 'container-ps' do
-  version '1.2.0'
-  sha256 '55776d6a0cc3d306bfaae9f831fd516b1b3fc37c8be8c8a521e019a163cfbfeb'
+  version '1.2.1'
+  sha256 '4e3201cf52936c7d92e45fad5f36698780b6b753d9fe03ee28dc20c60cf7953c'
 
   url "https://github.com/Toinane/container-ps/releases/download/#{version}/Container.PS-#{version}.dmg"
   appcast 'https://github.com/Toinane/container-ps/releases.atom'
