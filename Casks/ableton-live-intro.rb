eb79f404359288d5dbaa0ea182939570670714bd
Randall <ran.dall@icloud.com>
2019-11-20 14:31:07 -0800

    Update ableton-live-intro from 10.1.4 to 10.1.5

--- Casks/ableton-live-intro.rb
+++ Casks/ableton-live-intro.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-intro' do
-  version '10.1.4'
-  sha256 '78cb697c8025cdabe08c261d09b63ad468a7f0bc19c864ea076f9fc4259d5db4'
+  version '10.1.5'
+  sha256 '23e84e3acc068d206e5fc93dd2409aed1225f1d3ba7270ae1aea0181891dbadc'
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_64.dmg"
   appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
