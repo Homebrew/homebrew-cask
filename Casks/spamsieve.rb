6294595c07a3ace14d2749a206e23b91d2d5237d
Sebastian Riehm <herrriehm@users.noreply.github.com>
2019-11-20 19:07:20 +0100

    Update spamsieve from 2.9.37 to 2.9.38 (#72897)

--- Casks/spamsieve.rb
+++ Casks/spamsieve.rb
@@ -1,6 +1,6 @@
 cask 'spamsieve' do
-  version '2.9.37'
-  sha256 '86b4ccb2cb4ced10d10c750e6c6d91ef25d53961be138fbc4c4c7f1bade81532'
+  version '2.9.38'
+  sha256 '2769625a211512d09d561d7adbd7bee9dae4b56563165c3958125ebed24b08c2'
 
   url "https://c-command.com/downloads/SpamSieve-#{version}.dmg"
   appcast 'https://c-command.com/spamsieve/'
