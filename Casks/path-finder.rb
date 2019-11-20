ed6d3b7bda78b50ff8beb4843ffa08cfa733fd4c
Randall <ran.dall@icloud.com>
2019-11-20 13:43:14 -0800

    Update path-finder from 9.0.3 to 9.0.4

--- Casks/path-finder.rb
+++ Casks/path-finder.rb
@@ -1,6 +1,6 @@
 cask 'path-finder' do
-  version '9.0.3'
-  sha256 '21a47587872a38bc430d4a68bf8f64133f5d142ba8962192381008e87406257c'
+  version '9.0.4'
+  sha256 '5e0a69fbb0d342efe32d837696a53a14b42f54522923f3510f12158557ef017e'
 
   url "https://get.cocoatech.com/PF#{version.major}.dmg"
   appcast 'https://get.cocoatech.com/releasecast.xml'
