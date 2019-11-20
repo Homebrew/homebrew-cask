1fa8a504beacecafae965ed435270c49818c5873
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-19 18:15:34 -0800

    Update ultimaker-cura from 4.3.0 to 4.4.0 (#72844)

--- Casks/ultimaker-cura.rb
+++ Casks/ultimaker-cura.rb
@@ -1,6 +1,6 @@
 cask 'ultimaker-cura' do
-  version '4.3.0'
-  sha256 '76be61fbf286b434d72355be176988e28d7e5f030ce04782d897c496894877ba'
+  version '4.4.0'
+  sha256 'b9296ee9c87cb47bc64f02fa1da5b976fff360fa0c3bb8c62cd4e61475ee64e8'
 
   url "https://download.ultimaker.com/cura/Ultimaker_Cura-#{version}-Darwin.dmg"
   appcast 'https://github.com/Ultimaker/Cura/releases.atom'
