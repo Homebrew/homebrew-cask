f596daf973c812e53eaa45f56510c07070ce1509
Pavel <36270842+pvlplv@users.noreply.github.com>
2019-11-20 10:27:55 +0300

    Discrepancy of hostnames is commented

--- Casks/hue-sync.rb
+++ Casks/hue-sync.rb
@@ -2,9 +2,9 @@ cask 'hue-sync' do
   version '1.3.3.1'
   sha256 '0de1f39185602ec36e904d8c695772fa50977cea6de628a1feb3f8bcd449c291'
 
-  # URL_SECTION was verified as official when first introduced to the cask
+  # flex1548-esd.flexnetoperations.com was verified as official when first introduced to the cask
   url "https://flex1548-esd.flexnetoperations.com/flexnet/operations/WebContent?fileID=HueSyncMac"
-  name 'Hue Sync desktop app'
+  name 'Hue Sync'
   homepage 'https://www2.meethue.com/en-us/entertainment/sync-with-pc'
 
   pkg "HueSyncInstaller_#{version}.pkg"
