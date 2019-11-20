8836f2a33b1f536db01a9bbacb924ae4624caa25
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-20 15:07:53 -0800

    Update reaper from 5.98.600 to 5.98.700 (#72945)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,6 +1,6 @@
 cask 'reaper' do
-  version '5.98.600'
-  sha256 '15edab8f69707a417680318c58da8cd4024e2b09ea76042c8b0391baaa444229'
+  version '5.98.700'
+  sha256 '49185a83634f030691838d77948b5fafb590fd8d36b53684f66afff34e35455b'
 
   url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots.sub(%r{0*$}, '')}_x86_64.dmg"
   appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64',
