0fd5a022e589fbe23f824e559742f38cd3e302cf
Pavel <36270842+pvlplv@users.noreply.github.com>
2019-11-20 10:41:27 +0300

    Style fixed

--- Casks/hue-sync.rb
+++ Casks/hue-sync.rb
@@ -3,11 +3,11 @@ cask 'hue-sync' do
   sha256 '0de1f39185602ec36e904d8c695772fa50977cea6de628a1feb3f8bcd449c291'
 
   # flex1548-esd.flexnetoperations.com was verified as official when first introduced to the cask
-  url "https://flex1548-esd.flexnetoperations.com/flexnet/operations/WebContent?fileID=HueSyncMac"
+  url 'https://flex1548-esd.flexnetoperations.com/flexnet/operations/WebContent?fileID=HueSyncMac'
   name 'Hue Sync'
   homepage 'https://www2.meethue.com/en-us/entertainment/sync-with-pc'
 
   pkg "HueSyncInstaller_#{version}.pkg"
-  uninstall pkgutil: 'com.lighting.huesync'
 
+  uninstall pkgutil: 'com.lighting.huesync'
 end
