9df4cf509907f6839847aaee518eff8a0bc7fc57
gottabeleGIT <sandesh.manik@icloud.com>
2019-11-20 14:50:45 -0600

    Update flux 40.0 to 40.1 (#72905)

--- Casks/flux.rb
+++ Casks/flux.rb
@@ -1,6 +1,6 @@
 cask 'flux' do
-  version '40.0'
-  sha256 '0035cc1bf8878111028b47de32c76130c4ab5324d0fc244625b27155973c4384'
+  version '40.1'
+  sha256 '1844305519b450cfe3c5475e99f6c06fb2eea51e65c1b0ee25f6671a49fff75d'
 
   url "https://justgetflux.com/mac/Flux#{version}.zip"
   appcast 'https://justgetflux.com/mac/macflux.xml'
