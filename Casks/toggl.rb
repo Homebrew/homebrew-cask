4cc1b8cf529ac6c26035476cb58166ffb828494b
Robert Martin <rmartin@rmart.in>
2019-11-21 12:13:24 -0600

    Fix download URL for toggl 7.4.1034 (#72954)

--- Casks/toggl.rb
+++ Casks/toggl.rb
@@ -2,8 +2,8 @@ cask 'toggl' do
   version '7.4.1034'
   sha256 'eae0804589e73556ffdf45f64b9673e0227440ca9f2de6c009ae2ba6a3789ba3'
 
-  # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
-  url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
+  # github.com/toggl-open-source/toggldesktop was verified as official when first introduced to the cask
+  url "https://github.com/toggl-open-source/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_stable_appcast.xml'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com/toggl-desktop/'
