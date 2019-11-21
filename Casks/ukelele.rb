4d248fd40232c11de1fe9b60aabba27d36af99cf
suschizu <brigitte@corecode.io>
2019-11-21 11:56:23 +0100

    Update ukelele.rb

--- Casks/ukelele.rb
+++ Casks/ukelele.rb
@@ -2,7 +2,7 @@ cask 'ukelele' do
   version '3.4.0'
   sha256 '38b42254b780bbba3e4d3e2fac2a6bb0df8e273b1a1a29e6508b36e2178a019b'
 
-  url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=Ukelele_#{version}&filename=Ukelele_#{version}.dmg"
+  url "https://software.sil.org/downloads/r/ukelele/Ukelele_#{version}.dmg"
   appcast 'https://www.dropbox.com/s/vi51g5jig3etaum/Ukelele_appcast.xml?dl=1'
   name 'Ukelele'
   homepage 'https://software.sil.org/ukelele/'
