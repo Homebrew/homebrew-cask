d370c4794272b4d4727e01c552f4b2c569215e15
Randall <ran.dall@icloud.com>
2019-11-22 10:47:47 -0800

    Update emby-server from 4.2.1.0 to 4.3.0.26

--- Casks/emby-server.rb
+++ Casks/emby-server.rb
@@ -1,6 +1,6 @@
 cask 'emby-server' do
-  version '4.2.1.0'
-  sha256 '2f058b39b5dbce240f30aaaac0e4f57b841b299ff48c84864dfca9ea47bda8be'
+  version '4.3.0.26'
+  sha256 '75ff31830103f53fed923ad6763d5416e4d060810b1be736558394587631d76a'
 
   # github.com/MediaBrowser/Emby.Releases was verified as official when first introduced to the cask
   url "https://github.com/MediaBrowser/Emby.Releases/releases/download/#{version}/embyserver-osx-x64-#{version}.zip"
