1eebe97069da1918b65c598c887b9c2a60756b1a
John Pitchko <john.pitchko@icloud.com>
2019-11-21 22:59:43 -0700

    Add Questrade IQ Edge.app v6.0

---
+++ Casks/questrade-iq-edge.rb
@@ -0,0 +1,10 @@
+cask 'questrade-iq-edge' do
+  version '6.0'
+  sha256 '0526300dcbbafb20bf6038b19e72a8a5b811a87fb525442644b074daf5d9c934'
+
+  url 'http://media.questrade.com/iq_downloads/QuestradeIQEdge.dmg'
+  name 'Questrade IQ Edge'
+  homepage 'https://www.questrade.com/'
+
+  app 'Questrade IQ Edge.app'
+end
