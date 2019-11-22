381242f119f3a73018ffee5f1332173ea4c274a5
Work <brigitte@corecode.io>
2019-11-22 16:29:36 +0100

    Update sourcetrail

--- Casks/sourcetrail.rb
+++ Casks/sourcetrail.rb
@@ -1,9 +1,9 @@
 cask 'sourcetrail' do
   version '2019.4.61'
-  sha256 'c296b7d0ad486eed8a0f252d392fb30325820458b7a88c30cc18dce4b62e5665'
+  sha256 '03ffaa3152121d2131229b5df5599967c5c1b30fbe489b08dafeba07806fe1ac'
 
   url "https://www.sourcetrail.com/downloads/#{version}/osx/64bit"
-  appcast 'https://raw.githubusercontent.com/CoatiSoftware/SourcetrailBugTracker/master/README.md'
+  appcast 'https://github.com/CoatiSoftware/Sourcetrail/releases.atom'
   name 'Sourcetrail'
   homepage 'https://www.sourcetrail.com/'
 
