d781b3340ccf6ddca768741fc5ad8eb1d2502375
Mac MT. Test <mac.test@leshop.ch>
2019-11-22 08:11:20 +0100

    Update yed

--- Casks/yed.rb
+++ Casks/yed.rb
@@ -1,6 +1,6 @@
 cask 'yed' do
   version '3.19.1.1'
-  sha256 '000cd71c65db294a73830b308bcd4b1a8567bcf4395f9355f6394be1f23b7729'
+  sha256 '5cedd5f6c35d1dda239674a52027aa9b427340bcb9ad18eeefa38f85b7e6626c'
 
   url "https://www.yworks.com/resources/yed/demo/yEd-#{version}_with-JRE10.dmg"
   appcast 'https://www.yworks.com/products/yed/download#download'
