622b3e4f39767e7225056b1e0165631f3587cb80
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-22 11:51:57 -0800

    Update cisdem-data-recovery from 5.7.0 to 6.0.0 (#73064)

--- Casks/cisdem-data-recovery.rb
+++ Casks/cisdem-data-recovery.rb
@@ -1,6 +1,6 @@
 cask 'cisdem-data-recovery' do
-  version '5.7.0'
-  sha256 'cf9115653d16889874e393ed8ec3fce08e7b7098549baa2ad78ca77716bf7e5c'
+  version '6.0.0'
+  sha256 '6e43692c0533b14591d60de527b7f6432a8bad0e861318b38f89b9a264232427'
 
   url 'http://download.cisdem.com/cisdem-datarecovery.dmg'
   appcast 'https://www.cisdem.com/data-recovery-mac/release-notes.html'
