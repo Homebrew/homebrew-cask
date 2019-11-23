99d022f964a75727b490d12a6f235315a1c3ff8e
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-22 18:23:40 -0800

    Update Gifox to 020000.07

--- Casks/gifox.rb
+++ Casks/gifox.rb
@@ -1,10 +1,10 @@
 cask 'gifox' do
-  version '010601.03'
-  sha256 'b0dea197c7c0f1533de4ca2d0402247780bc0463eecdfd8dfa3928f81e5399a9'
+  version '020000.07'
+  sha256 '0b4a8984a6f46ae5583675d44144b9741a9f50d8ecd23a76371683c0b9cf5226'
 
   # d1hsvn1xu03mmy.cloudfront.net/gifox was verified as official when first introduced to the cask
-  url "https://d1hsvn1xu03mmy.cloudfront.net/gifox/#{version}.dmg"
-  appcast 'https://d1hsvn1xu03mmy.cloudfront.net/gifox/appcast.xml'
+  url "https://d3si16icyi9iar.cloudfront.net/gifox/#{version}.dmg"
+  appcast 'https://d3si16icyi9iar.cloudfront.net/gifox/appcast.xml'
   name 'gifox'
   homepage 'https://gifox.io/'
 
