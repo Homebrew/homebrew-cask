338e726e1e228140b8931680cf76805f2b798f31
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-23 12:09:03 -0800

    Update qutebrowser from 1.8.1 to 1.8.2 (#73105)

--- Casks/qutebrowser.rb
+++ Casks/qutebrowser.rb
@@ -1,6 +1,6 @@
 cask 'qutebrowser' do
-  version '1.8.1'
-  sha256 'cdc19ecd10ceba3fa7b898b5fa30ff06d4ef263b2532552a2aaa221a3efaaa44'
+  version '1.8.2'
+  sha256 'b13161b7e7aed12add2a2c4fb94d3d7ac73d656f22aef77fce3cd4e88aefc002'
 
   # github.com/qutebrowser/qutebrowser was verified as official when first introduced to the cask
   url "https://github.com/qutebrowser/qutebrowser/releases/download/v#{version}/qutebrowser-#{version}.dmg"
