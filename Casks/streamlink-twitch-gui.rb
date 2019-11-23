812a62493cc1fc8ac60340dd961bde003e09b90f
bastimeyer <mail@bastimeyer.de>
2019-11-23 09:27:00 +0100

    Update streamlink-twitch-gui to 1.9.0

--- Casks/streamlink-twitch-gui.rb
+++ Casks/streamlink-twitch-gui.rb
@@ -1,6 +1,6 @@
 cask 'streamlink-twitch-gui' do
-  version '1.8.1'
-  sha256 'c18b971a6974fbe7ff60210e419265df0443054b73bba5cdae94d1ed648b92c3'
+  version '1.9.0'
+  sha256 '8cef5e200ea115440f20020cb88cc4c40a96ed3f64ef8ee904577c3bebea2181'
 
   url "https://github.com/streamlink/streamlink-twitch-gui/releases/download/v#{version}/streamlink-twitch-gui-v#{version}-macOS.tar.gz"
   appcast 'https://github.com/streamlink/streamlink-twitch-gui/releases.atom'
