da1ad55f97e0b350589a67c0cbd252f1382da4e2
Gerald Combs <gerald@wireshark.org>
2019-11-23 08:07:56 -0800

    Wireshark: Update the download URL (#73062)

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -2,7 +2,7 @@ cask 'wireshark' do
   version '3.0.6'
   sha256 '186970a1e0b610f23cb59314feaa3ce3eeb89b7c39bb5c34b4549910f1782f04'
 
-  url "https://www.wireshark.org/download/osx/Wireshark%20#{version}%20Intel%2064.dmg"
+  url "https://1.na.dl.wireshark.org/osx/all-versions/Wireshark%20#{version}%20Intel%2064.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://www.wireshark.org/download.html&splitter_1=stable&index_1=1&splitter_2=development&index_2=0'
   name 'Wireshark'
   homepage 'https://www.wireshark.org/'
