eb0e966e7e9243262d13ecd846bf1a2146d49a4e
Martin Reinhardt <contact@martinreinhardt-online.de>
2019-11-24 13:32:31 +0100

    Adding BitDefender App

---
+++ Casks/bitdefender.rb
@@ -0,0 +1,11 @@
+cask 'bitdefender' do
+  version ':latest'
+  sha256 '6650cbe63b87ccc342c3f7a953bff4b6fe3f667d09e6cde7ac6281546532ef09'
+
+  url "https://flow.bitdefender.net/connect/release/en_us/bitdefender_5d739df5-ff3b-48ec-9a64-cedf01924755.pkg"
+  name 'BitDefender'
+  homepage 'https://www.bitdefender.com'
+
+  pkg 'bitdefender_5d739df5-ff3b-48ec-9a64-cedf01924755.pkg', allow_untrusted: true
+  uninstall pkgutil: 'com.bitdefender.*'
+end
