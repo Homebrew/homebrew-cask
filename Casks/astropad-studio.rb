34a17226598f35aae4d3efefceea397e0ff722c4
Robert Rasmussen <1918121+thatRobR@users.noreply.github.com>
2019-11-25 15:25:41 +0100

    Update astropad-studio from 3.1 to 3.3

--- Casks/astropad-studio.rb
+++ Casks/astropad-studio.rb
@@ -2,7 +2,7 @@ cask 'astropad-studio' do
   version '3.3'
   sha256 '55d6d08f5b70a1ffd89dd5864312bddc3c3df1784a848d0ba408dff1dec775fd'
 
-  url "https://astropad.com/downloads/AstropadStudio.dmg"
+  url 'https://astropad.com/downloads/AstropadStudio.dmg'
   appcast 'https://astropad.com/downloads/studio-sparkle.xml'
   name 'Astropad Studio'
   homepage 'https://astropad.com/'
