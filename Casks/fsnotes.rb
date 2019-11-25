759f1e96e09dd7d9aaaf63d1871b4bec059fc54d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-25 15:13:06 -0800

    Update fsnotes from 4.0.7 to 4.0.8 (#73166)

--- Casks/fsnotes.rb
+++ Casks/fsnotes.rb
@@ -1,6 +1,6 @@
 cask 'fsnotes' do
-  version '4.0.7'
-  sha256 'a2a9c4d9576401a36694dd01e805d89f14afd9da894ee6441a52049647dafc81'
+  version '4.0.8'
+  sha256 'b35b87ad732a3d76d85cba830b3aacdc4443f15009371dd0f6a9418da566cb12'
 
   # github.com/glushchenko/fsnotes was verified as official when first introduced to the cask
   url "https://github.com/glushchenko/fsnotes/releases/download/#{version}/FSNotes_#{version}.zip"
