62997aba2eeb656871c4e60ecec4d480c5bb65df
George Adams <george.adams@microsoft.com>
2019-11-25 10:21:13 +0100

    Switch adoptopenjdk cask to use pkg
    This PR swaps the `adoptopenjdk` cask to use the notarized `pkg` installer files to avoid macOS signature errors as detailed in https://github.com/AdoptOpenJDK/homebrew-openjdk/issues/267

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -1,15 +1,17 @@
 cask 'adoptopenjdk' do
   version '13.0.1,9'
-  sha256 '9c82de98ce9bc2353bcf314d85366c9a2c572db034e10a71aa47e804e13748c1'
+  sha256 '29f1c87b3ecca38c50ea016657368af0a6a28ade5ba4150bd147ad91c524ea22'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.tar.gz"
+  url 'https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.1%2B9/OpenJDK13U-jdk_x64_mac_hotspot_13.0.1_9.pkg'
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK Java Development Kit'
   homepage 'https://adoptopenjdk.net/'
 
-  artifact "jdk-#{version.before_comma}+#{version.after_comma}", target: "/Library/Java/JavaVirtualMachines/adoptopenjdk-#{version.before_comma}.jdk"
+  pkg 'OpenJDK13U-jdk_x64_mac_hotspot_13.0.1_9.pkg'
 
+  uninstall pkgutil: "net.adoptopenjdk.#{version.major}.jdk"
+  
   caveats <<~EOS
     More versions are available in the AdoptOpenJDK tap:
       #{Formatter.url('https://github.com/AdoptOpenJDK/homebrew-openjdk')}
