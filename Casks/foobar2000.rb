01c6b98f5c04528ca600c76ee21133037f08e95e
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-26 12:01:16 -0500

    Update foobar2000

--- Casks/foobar2000.rb
+++ Casks/foobar2000.rb
@@ -1,6 +1,6 @@
 cask 'foobar2000' do
   version '2.1.55'
-  sha256 'c3841ab4fbdb8ab36c60bde3607387cb74311e70070660401183fee47e0cf6f3'
+  sha256 'e6d5233be0ed74f5900d5e5fc57fd271fcb57813b56b3ee3f9baa6daf7623a59'
 
   url "https://www.foobar2000.org/files/b238d40de415d3afc284eee48712521d/foobar2000-v#{version}.dmg"
   name 'foobar2000'
