49d8691c163e96503d4fd1775a609744b4c5486c
CoreCode <core-code@users.noreply.github.com>
2019-11-26 10:20:55 +0000

    Update macs-fan-control from 1.5.1.7 to 1.5.2.8 (#73182)
    * Update macs-fan-control from 1.5.1.7 to 1.5.2.8
    
    * Update macs-fan-control.rb

--- Casks/macs-fan-control.rb
+++ Casks/macs-fan-control.rb
@@ -1,6 +1,6 @@
 cask 'macs-fan-control' do
-  version '1.5.1.7'
-  sha256 '29c9d84611c7b67bbf134db8e3899f27f515ddd1521424f6a5cddbb34714d4db'
+  version '1.5.2.8'
+  sha256 'f5666cbe2591aa152507ad55a9209664a226b51c7568f59a0f3d3ff72f3f8b32'
 
   # github.com/crystalidea/macs-fan-control was verified as official when first introduced to the cask
   url "https://github.com/crystalidea/macs-fan-control/releases/download/v#{version.major_minor_patch}/macsfancontrol.zip"
