c4ef1aea89ebf9f799e314a87e2d02992b8e8d63
Randall <17261190+ran-dall@users.noreply.github.com>
2019-11-26 15:41:22 -0800

    Update touchosc-bridge.rb

--- Casks/touchosc-bridge.rb
+++ Casks/touchosc-bridge.rb
@@ -2,7 +2,7 @@ cask 'touchosc-bridge' do
   version '1.5.0'
   sha256 'a5a4b5d78733b682d61952959a96fadc74019158166f74d5dfac65e2f76b59fa'
 
-  url "https://hexler.net/pub/touchosc/touchosc-bridge-#{version}-osx.zip"
+  url "https://hexler.net/pub/touchosc/touchosc-bridge-#{version}-macos.zip" 
   appcast 'https://hexler.net/software/touchosc'
   name 'TouchOSC Bridge'
   homepage 'https://hexler.net/software/touchosc'
