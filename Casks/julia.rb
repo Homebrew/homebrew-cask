fe7e98603fdd079721c48e4fa874c2877c7865f7
Qi Zhang <singularitti@outlook.com>
2019-11-26 15:32:17 -0500

    Update julia from 1.2.0 to 1.3.0

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,6 +1,6 @@
 cask 'julia' do
-  version '1.2.0'
-  sha256 'c65f3af899571ba8f100f55339a0251a31342c25f33fc500929fd7404ec8e955'
+  version '1.3.0'
+  sha256 '861505a3407e5406625e1ec0a72a4fc76fa25608fb08bd3d5f327e8f8fda5740'
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
   appcast 'https://github.com/JuliaLang/julia/releases.atom'
