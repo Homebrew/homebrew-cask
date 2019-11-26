4a9d5891886e35fad373d6fcdbd5fccd7a6139a1
suschizu <brigitte@corecode.io>
2019-11-26 23:20:55 +0000

    Update amazon-music from 7.8.6,20191112:0935336bdb to 7.8.7,20191125:084217a638 (#73194)

--- Casks/amazon-music.rb
+++ Casks/amazon-music.rb
@@ -1,6 +1,6 @@
 cask 'amazon-music' do
-  version '7.8.6,20191112:0935336bdb'
-  sha256 'e7848561f2c6f24483f6cd5d3988656c58c3196f26cdfdd4c263a6d91c06589a'
+  version '7.8.7,20191125:084217a638'
+  sha256 '4e6401f63d34639ceda8038e55b4a42a9e265721acf170995b958e12ceef83f7'
 
   # ssl-images-amazon.com/images was verified as official when first introduced to the cask
   url "https://images-na.ssl-images-amazon.com/images/G/01/digital/music/morpho/installers/#{version.after_comma.before_colon}/#{version.after_colon}/AmazonMusicInstaller.dmg"
