6fc6004d89cf4fce9791dab406ad36b7a1b21841
Vítor Galvão <info@vitorgalvao.com>
2019-11-26 17:24:39 +0000

    Update foobar2000.rb

--- Casks/foobar2000.rb
+++ Casks/foobar2000.rb
@@ -1,6 +1,6 @@
 cask 'foobar2000' do
   version '2.1.55'
-  sha256 'e6d5233be0ed74f5900d5e5fc57fd271fcb57813b56b3ee3f9baa6daf7623a59'
+  sha256 '77ed24a9aab8398c326e4f432d7aefc3a6069fa822536e0a64867e18f1180104'
 
   url "https://www.foobar2000.org/files/b238d40de415d3afc284eee48712521d/foobar2000-v#{version}.dmg"
   name 'foobar2000'
