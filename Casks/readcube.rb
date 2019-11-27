cf44184d93bde6a7a74e945cc8021e94da685bf0
Qi Zhang <singularitti@outlook.com>
2019-11-27 00:12:13 -0500

    fix: Find the right homepage

--- Casks/readcube.rb
+++ Casks/readcube.rb
@@ -2,10 +2,9 @@ cask 'readcube' do
   version '4.0.0'
   sha256 '8c63a193ced25c92db168e415b3a537efb828c86bd62b7615bc95508d3532801'
 
-  # download.readcube.com was verified as official when first introduced to the cask
   url 'https://download.readcube.com/app/Install%20Papers.pkg'
   name 'ReadCube Papers'
-  homepage 'https://www.papersapp.com/'
+  homepage 'https://www.readcube.com/home'
 
   pkg 'Install Papers.pkg'
 
