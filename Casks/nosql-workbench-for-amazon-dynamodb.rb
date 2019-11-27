44ecb58f37e18ecc4d8ef12fb8f03b2797d359ae
Ryosuke Sato <rskjtwp@gmail.com>
2019-11-27 23:31:32 +0900

    Update nosql-workbench-for-amazon-dynamodb from 0.3.0 to 0.3.1 (#73244)
    * Update nosql-workbench-for-amazon-dynamodb from 0.3.0 to 0.3.1
    
    * fix AWS document page link

--- Casks/nosql-workbench-for-amazon-dynamodb.rb
+++ Casks/nosql-workbench-for-amazon-dynamodb.rb
@@ -1,12 +1,12 @@
 cask 'nosql-workbench-for-amazon-dynamodb' do
-  version '0.3.0'
-  sha256 '8cf0d7eac9d73a1f5c761dcfe22e617cc384c5d27a49c80d0258070d548201fe'
+  version '0.3.1'
+  sha256 '16b71734e87f3feefbb8955cb7d47a8b24e7bdb1fdcbd7cb8092683888975e55'
 
   # nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com/NoSQL+Workbench+for+Amazon+DynamoDB+(Preview)-mac-#{version}.dmg"
-  appcast 'https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/workbench.settingup.partial.html'
+  appcast 'https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/workbench.settingup.html'
   name 'NoSQL Workbench for Amazon DynamoDB'
-  homepage 'https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/workbench.html'
+  homepage 'https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/workbench.html'
 
   app 'NoSQL Workbench for Amazon DynamoDB (Preview).app'
 end
