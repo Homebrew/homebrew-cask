e6b167d80e3862c6671833f3f518546800cf0e94
Julian Mayer <julian@corecode.io>
2019-11-27 22:10:26 +0100

    Update orange

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -1,6 +1,6 @@
 cask 'orange' do
   version '3.23.1'
-  sha256 '3ab38b74ecc3a4c7a39a96f498630a0518468efbdfe37f1ebc641d7b16e9f226'
+  sha256 '03220412b455df991a101a353b373a50663e2061be916daf88eb6bb4073439a1'
 
   url "https://download.biolab.si/download/files/Orange#{version.major}-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://service.biolab.si/download/orange?platform=mac'
