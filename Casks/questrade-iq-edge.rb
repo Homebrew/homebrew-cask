7dd7ddb704ee1b74ce79be33ca3fe8d661eecb22
John Pitchko <john.pitchko@icloud.com>
2019-11-27 10:08:31 -0700

    Use latest version and disable sha256 check

--- Casks/questrade-iq-edge.rb
+++ Casks/questrade-iq-edge.rb
@@ -1,6 +1,6 @@
 cask 'questrade-iq-edge' do
-  version '6.0'
-  sha256 '0526300dcbbafb20bf6038b19e72a8a5b811a87fb525442644b074daf5d9c934'
+  version :latest
+  sha256 :no_check
 
   url 'http://media.questrade.com/iq_downloads/QuestradeIQEdge.dmg'
   name 'Questrade IQ Edge'
