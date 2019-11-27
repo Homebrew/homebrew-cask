55d367b3e9241f284b884d1c9ac6397ab5fa5d82
Julian Mayer <julian@corecode.io>
2019-11-27 22:16:04 +0100

    Update foobar2000

--- Casks/foobar2000.rb
+++ Casks/foobar2000.rb
@@ -2,7 +2,7 @@ cask 'foobar2000' do
   version '2.1.55'
   sha256 'c3841ab4fbdb8ab36c60bde3607387cb74311e70070660401183fee47e0cf6f3'
 
-  url "https://www.foobar2000.org/files/b238d40de415d3afc284eee48712521d/foobar2000-v#{version}.dmg"
+  url "https://www.foobar2000.org/files/17164cfb089b318ccdbb48f0337ccb6b/foobar2000-v#{version}.dmg"
   name 'foobar2000'
   homepage 'https://www.foobar2000.org/'
 
