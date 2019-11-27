36f7fc26ff92defe9e556e837110fd9113cf2325
ryosan-470 <rskjtwp@gmail.com>
2019-11-27 17:02:11 +0900

    fix AWS document page link

--- Casks/nosql-workbench-for-amazon-dynamodb.rb
+++ Casks/nosql-workbench-for-amazon-dynamodb.rb
@@ -4,9 +4,9 @@ cask 'nosql-workbench-for-amazon-dynamodb' do
 
   # nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com/NoSQL+Workbench+for+Amazon+DynamoDB+(Preview)-mac-#{version}.dmg"
-  appcast 'https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/workbench.settingup.partial.html'
+  appcast 'https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/workbench.settingup.html'
   name 'NoSQL Workbench for Amazon DynamoDB'
-  homepage 'https://docs.aws.amazon.com/en_pv/amazondynamodb/latest/developerguide/workbench.html'
+  homepage 'https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/workbench.html'
 
   app 'NoSQL Workbench for Amazon DynamoDB (Preview).app'
 end
