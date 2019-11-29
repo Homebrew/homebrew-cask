83186750df5abe2c858ec2f30fbe23e6a86c1a94
David Z <geek@lzw.name>
2019-11-29 10:39:17 +0800

    Update sizzy to 0.11

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,10 +1,11 @@
 cask 'sizzy' do
-  version :latest
-  sha256 :no_check
+  version '0.11'
+  sha256 '783242bf6652c7eac79da6d1fef3bbe86239819b8e0b7b74203203ae156ef0c8'
 
   url "https://sizzy.co/get-app"
   name 'Sizzy'
   homepage 'https://sizzy.co'
+  appcast 'https://headway-widget.net/widgets/JAXRr7'
 
   app 'Sizzy.app'
 end
