2440e7b068f43c069a8c0a5e3800f068b7244b9c
Vítor Galvão <info@vitorgalvao.com>
2019-11-29 00:38:21 +0000

    Update flic.rb

--- Casks/flic.rb
+++ Casks/flic.rb
@@ -1,13 +1,11 @@
 cask 'flic' do
-  version '1.1.3'
+  version '1.1.3,2018-09-11 13-14-23'
   sha256 '400b1aed904b527b049af26c61aca234a6812e7f3d366ecb471a5b8fe6646ae1'
 
-  url "https://s3.amazonaws.com/misc-scl-cdn/Flic.#{version}.zip"
-  name 'Flic'
+  # misc-scl-cdn.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://misc-scl-cdn.s3.amazonaws.com/Flic.#{version.before_comma}.zip"
+  appcast 'FlicMac Developer Distribution '
   homepage 'https://flic.io/mac-app'
 
-  # The application is located in an directory with seems to be named after the
-  # build date and is therefor not predictable
-  app "#{Dir.glob('#{appdir}/FlicMac Developer Distribution */Flic.app')[0]}"
-
+  app "FlicMac Developer Distribution #{version.after_comma}/"
 end
