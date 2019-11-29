f77d2da14b1473b2312ba65562d6ce7a9dd5df4d
David Z <geek@lzw.name>
2019-11-29 11:02:19 +0800

    update sizzy allow auto_update

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -3,9 +3,11 @@ cask 'sizzy' do
   sha256 '783242bf6652c7eac79da6d1fef3bbe86239819b8e0b7b74203203ae156ef0c8'
 
   url "https://sizzy.co/get-app"
+  appcast 'https://headway-widget.net/widgets/JAXRr7'
   name 'Sizzy'
   homepage 'https://sizzy.co'
-  appcast 'https://headway-widget.net/widgets/JAXRr7'
+
+  auto_updates true
 
   app 'Sizzy.app'
 end
