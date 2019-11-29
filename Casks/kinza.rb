3e995b16fd0ff81ae765cbf49effa2ca7f4ec54a
Vítor Galvão <info@vitorgalvao.com>
2019-11-29 00:11:30 +0000

    Update kinza.rb

--- Casks/kinza.rb
+++ Casks/kinza.rb
@@ -4,8 +4,9 @@ cask 'kinza' do
 
   # d7s9ygw7nxr46.cloudfront.net was verified as official when first introduced to the cask
   url "https://d7s9ygw7nxr46.cloudfront.net/mac/kinza_#{version}.dmg"
+  appcast 'https://www.kinza.jp/en/download/thankyou/mac/'
   name 'Kinza'
-  homepage 'https://www.kinza.jp/'
+  homepage 'https://www.kinza.jp/en/'
 
   app 'Kinza.app'
 end
