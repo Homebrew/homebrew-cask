f52f5ead735b4165071d21746fc0c445035d59e1
Vítor Galvão <info@vitorgalvao.com>
2019-11-29 00:48:50 +0000

    Update flic.rb

--- Casks/flic.rb
+++ Casks/flic.rb
@@ -4,7 +4,8 @@ cask 'flic' do
 
   # misc-scl-cdn.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://misc-scl-cdn.s3.amazonaws.com/Flic.#{version.before_comma}.zip"
-  appcast 'FlicMac Developer Distribution '
+  appcast 'https://flic.io/mac-app'
+  name 'FlicMac Developer Distribution'
   homepage 'https://flic.io/mac-app'
 
   app "FlicMac Developer Distribution #{version.after_comma}/Flic.app"
