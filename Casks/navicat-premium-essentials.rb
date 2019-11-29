8164f880f674c2d8480fbe338d673606e30516bf
Tomáš Holý <holy@interconnect.cz>
2019-11-29 11:34:47 +0100

    Update navicat-premium-essentials from 12.1.27 to 15.0.3

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,9 +1,9 @@
 cask 'navicat-premium-essentials' do
-  version '12.1.27'
-  sha256 '41f15116c7c186c93a93157436e9e1ed48c7d839bfb7447a7974e718ae498b5d'
+  version '15.0.3'
+  sha256 '97df4bb13f485d8a5cc798935d5b582ae67f282110abbc49d5a149edbf793d26'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
-  appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20Premium%20Essentials&appLang=en'
+  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium%20Essentials&appLang=en'
   name 'Navicat Premium Essentials'
   homepage 'https://navicat.com/products/navicat-essentials'
 
