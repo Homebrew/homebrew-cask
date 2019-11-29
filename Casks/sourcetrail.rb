e4121e16476bc93e75a64a67249f8e1681e817ea
Vítor Galvão <info@vitorgalvao.com>
2019-11-29 00:46:43 +0000

    Update sourcetrail.rb

--- Casks/sourcetrail.rb
+++ Casks/sourcetrail.rb
@@ -2,8 +2,9 @@ cask 'sourcetrail' do
   version '2019.4.61'
   sha256 '03ffaa3152121d2131229b5df5599967c5c1b30fbe489b08dafeba07806fe1ac'
 
-  url "https://www.sourcetrail.com/downloads/#{version}/osx/64bit"
-  appcast 'https://raw.githubusercontent.com/CoatiSoftware/SourcetrailBugTracker/master/README.md'
+  # github.com/CoatiSoftware/Sourcetrail was verified as official when first introduced to the cask
+  url "https://github.com/CoatiSoftware/Sourcetrail/releases/download/#{version}/Sourcetrail_#{version.dots_to_underscores}_macOS_64bit.zip"
+  appcast 'https://github.com/CoatiSoftware/Sourcetrail/releases.atom'
   name 'Sourcetrail'
   homepage 'https://www.sourcetrail.com/'
 
