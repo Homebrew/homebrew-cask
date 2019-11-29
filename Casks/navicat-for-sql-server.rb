e47976a95d4d321a2688018be2737ea4f699187c
Tomáš Holý <holy@interconnect.cz>
2019-11-29 11:30:08 +0100

    Update navicat-for-sql-server from 12.1.27 to 15.0.3

--- Casks/navicat-for-sql-server.rb
+++ Casks/navicat-for-sql-server.rb
@@ -1,9 +1,9 @@
 cask 'navicat-for-sql-server' do
-  version '12.1.27'
-  sha256 'ad85600d2eda1ee0d8cb4b12ad2a9acaded98dcfe4125688400d28651f6deea3'
+  version '15.0.3'
+  sha256 '69aebf183f97010ba1eff6e8564d6871951595f81c958adf3f2e1fc37db638c2'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlserver_en.dmg"
-  appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20for%20SQL%20Server&appLang=en'
+  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20SQL%20Server&appLang=en'
   name 'Navicat for SQL Server'
   homepage 'https://www.navicat.com/products/navicat-for-sqlserver'
 
