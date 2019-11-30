e9a2e34d38c30d13e2a51b8c302089b0ad97492e
Fabian Weisshaar <elnappo@nerdpol.io>
2019-11-30 09:53:23 +0100

    Update kstars from 3.3.6 to 3.3.8

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask 'kstars' do
-  version '3.3.6'
-  sha256 'b998cd6a30e6375871fb0eb5d3c29c1a055bfc172541b0192bec909dc1c1a7b7'
+  version '3.3.8'
+  sha256 '141b0b695399afff0b56b9cc2dc1bdc1c03a323092267003a1205bbe2b6198d9'
 
   # indilib.org/jdownloads/kstars was verified as official when first introduced to the cask
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg"
