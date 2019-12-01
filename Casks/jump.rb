9bc8ecf6d7ae421cb64363c36190f43d38329f45
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-01 10:34:39 -0800

    Update jump from 8.4.7 to 8.4.8 (#73451)

--- Casks/jump.rb
+++ Casks/jump.rb
@@ -1,6 +1,6 @@
 cask 'jump' do
-  version '8.4.7'
-  sha256 '7ba7fdf0c2858fd263f19c5fa3a41adf48a0ab69b7ea058cfc32a1287adb632d'
+  version '8.4.8'
+  sha256 'cc1f0f7b5c3b279bb6b94a2b2d49e5ea73f9ca27b790668cfd3493c6e16b0051'
 
   url "https://mirror.jumpdesktop.com/downloads/JumpDesktopMac-#{version}.zip"
   appcast 'https://jumpdesktop.com/downloads/viewer/jdmac-web-appcast.xml'
