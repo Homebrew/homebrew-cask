b5e88bd7759b1d60dcc0c5dad422e29535e61f2d
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-12-01 21:48:19 +0200

    Update lunar from 2.9.0 to 2.9.1 (#73481)

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,6 +1,6 @@
 cask 'lunar' do
-  version '2.9.0'
-  sha256 'a2f432287e40056456aa96f0b0047163370b7b8470c1c0b3547efe2ba33aa14c'
+  version '2.9.1'
+  sha256 '00976d6f18baffc7b1507be3b5eaddd2a73517d2185d1fe42dd2bc493df98aa5'
 
   # github.com/alin23/Lunar was verified as official when first introduced to the cask
   url "https://github.com/alin23/Lunar/releases/download/v#{version}/Lunar-#{version}.dmg"
