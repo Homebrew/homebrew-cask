27296840aa0a966c2b387192cca8afe69216706e
Randall <ran.dall@icloud.com>
2019-12-01 09:54:33 -0800

    Update magiccap from 2.1.0 to 2.1.1

--- Casks/magiccap.rb
+++ Casks/magiccap.rb
@@ -1,6 +1,6 @@
 cask 'magiccap' do
-  version '2.1.0'
-  sha256 '8b4d81751515f458d2d2044da45bbc9858d09777e194ec76f5ea987f26cbc45b'
+  version '2.1.1'
+  sha256 'f5a1e6ae19fb676556e0ce9994ce4a6e41627f6c7c248b8f298144bf08c0a4dd'
 
   # github.com/magiccap/MagicCap was verified as official when first introduced to the cask
   url "https://github.com/magiccap/MagicCap/releases/download/v#{version}/magiccap-mac.dmg"
