e86339d479eec6dec56923ab845950a2663d54b5
Randall <ran.dall@icloud.com>
2019-12-01 10:51:34 -0800

    Update deepl from 1.3.1 to 1.3.2

--- Casks/deepl.rb
+++ Casks/deepl.rb
@@ -1,6 +1,6 @@
 cask 'deepl' do
-  version '1.3.1'
-  sha256 '581238cccb7ce37355f5571db4558ec28f2505ecc4aca16a6748b40c1be74e02'
+  version '1.3.2'
+  sha256 '0ee97ec7585e735b96001c03d4dca15cdc0fa858d4afc4abd37771ab1e5129c1'
 
   url "https://www.deepl.com/macos/download/LpsA0EG5frbfX8p5/DeepL#{version.no_dots}.zip"
   name 'DeepL'
