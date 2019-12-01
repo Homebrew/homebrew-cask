2325de3d7dc57d47849ec71b79bbe95884f0c352
Vítor Galvão <info@vitorgalvao.com>
2019-12-01 22:37:03 +0000

    Update fontlab.rb

--- Casks/fontlab.rb
+++ Casks/fontlab.rb
@@ -1,11 +1,12 @@
 cask 'fontlab' do
-  version '7.0.0.7264'
-  sha256 '1347268c8e5c28d38a02c6b55f299d93159da24e2d5fa339e24211e8bbce3029'
+  version '7.0.7264'
+  sha256 '761fec570778e5b1aea1eb640a15a6da563a3c80120ca5484b412b726b4cc30b'
 
-  url "https://download.fontlab.com/fontlab-#{version.major}/upd-mac.php"
-  appcast 'https://download.fontlab.com/fontlab-vi/appcast-mac.xml'
+  # fontlab.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://fontlab.s3.amazonaws.com/fontlab#{version.major}/#{version.patch}/FontLab-#{version.major}-Mac-Install-#{version.patch}.dmg"
+  appcast "https://download.fontlab.com/fontlab-#{version.major}/appcast-mac.xml"
   name 'Fontlab'
   homepage 'https://www.fontlab.com/font-editor/fontlab/'
 
-  app 'FontLab VI.app'
+  app "FontLab #{version.major}.app"
 end
