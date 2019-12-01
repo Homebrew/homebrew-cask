5733ac543deca7491e5fc4a5137921c79ea08671
suschizu <brigitte@corecode.io>
2019-12-01 22:47:47 +0000

    Update url + homepeage in  beoplay-software-update.rb (#73420)
    * Update beoplay-software-update.rb
    
    * Update beoplay-software-update.rb

--- Casks/beoplay-software-update.rb
+++ Casks/beoplay-software-update.rb
@@ -1,11 +1,12 @@
 cask 'beoplay-software-update' do
   version '1.0.6'
-  sha256 '2419f18444c711360664492b88d397c06f18298469425833f1e1185c632ecc76'
+  sha256 '7e0500f935557f34d46625e4abe7081cdaa343558a973d3284000ab4c37b21a6'
 
-  url 'https://www.beoplay.com/~/media/81c0752c82974b2794f731f6d4cd5fe9.ashx'
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=#{url}"
+  url "https://www.bang-olufsen.com/~/mediaV3/files/software-downloads/beoplay-software-updater/beoplay-software-updater-#{version.dots_to_hyphens}.dmg"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=#{url}",
+          configuration: version.dots_to_hyphens
   name 'BeoPlay Software Update'
-  homepage 'https://www.beoplay.com/support/product%20support/beoplay%20updater'
+  homepage 'https://www.bang-olufsen.com/en/product-support/accessories/beoplay-software-updater'
 
   app 'BeoPlay Software Update.app'
 end
