26c6ff3f36741d8f30cdc4590f75c991b69d549b
Tomáš Holý <vantomas@vantomas.net>
2019-12-01 23:27:34 +0100

    Update navicat-premium from 12.1.27 to 15.0.3 (#73389)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,9 +1,9 @@
 cask 'navicat-premium' do
-  version '12.1.27'
-  sha256 'a5d4c5695067b38aff458b3545e7b4942dc407cd4926d61710aff5cb78f77ab6'
+  version '15.0.3'
+  sha256 'f450097c790f8c7a12c21cef9c00813d3ce286b9e35da2fbd269f94bc3acdf44'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
-  appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20Premium&appLang=en'
+  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en'
   name 'Navicat Premium'
   homepage 'https://www.navicat.com/products/navicat-premium'
 
