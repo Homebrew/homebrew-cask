5a414209ea706524d378131348919ec14e546513
Randall <ran.dall@icloud.com>
2019-12-02 10:57:51 -0800

    Update josm from 15492 to 15553

--- Casks/josm.rb
+++ Casks/josm.rb
@@ -1,6 +1,6 @@
 cask 'josm' do
-  version '15492'
-  sha256 '712014ce299166a55dd765d431f63c7b61c0e775df9d2c5d318223dd24b89896'
+  version '15553'
+  sha256 '30353e4407e60b959560ba5baac4e2cb8b52fd1d3e030b604e9f444c32399100'
 
   url "https://josm.openstreetmap.de/download/macosx/josm-macosx-#{version}.zip"
   appcast 'https://josm.openstreetmap.de/'
