513057d7c0d3ff2c12a473083f3bc73822fc0b19
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-02 11:20:04 -0800

    Update cacher from 2.22.1 to 2.22.3 (#73542)

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -1,6 +1,6 @@
 cask 'cacher' do
-  version '2.22.1'
-  sha256 '835d760fce8ccc53463769daf184780141a168c317f606db6413a419aad8663a'
+  version '2.22.3'
+  sha256 '3ab0be4b58d30bc25e87bcba00d4531141a9fce6b1674061e1a8d502ea8cda80'
 
   # cacher-download.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
