fb027d8f0c1bf9f4dc502625aae3e5c689d8d5db
jcbot <jcb@leipert.io>
2019-12-02 15:07:49 +0000

    Update mps to 2019.2.4,192.2456

--- Casks/mps.rb
+++ Casks/mps.rb
@@ -1,6 +1,6 @@
 cask 'mps' do
-  version '2019.2.3,192.2321'
-  sha256 '24e5adbd156455c695c26fabbe5dc9b9dfd2790b03a6bb8e9845cb67428b780f'
+  version '2019.2.4,192.2456'
+  sha256 '4095ae7e98480ccf05b6474c6dba5fba736149c74c0aa8c53146b97587dda9af'
 
   url "https://download.jetbrains.com/mps/#{version.before_comma.major_minor}/MPS-#{version.before_comma}-macos.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=MPS&latest=true&type=release'
