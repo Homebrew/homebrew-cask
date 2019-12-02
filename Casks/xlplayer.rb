c4e2d00d9d043e87c3e8e4b778a2c51b2b0590b2
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-01 17:56:35 -0800

    Update xlplayer from 2.0.3.1265 to 2.0.5.1285 (#73467)
    * Update xlplayer from 2.0.3.1265 to 2.0.5.1285
    
    * Update xlplayer.rb
    
    * Update xlplayer.rb

--- Casks/xlplayer.rb
+++ Casks/xlplayer.rb
@@ -1,13 +1,13 @@
 cask 'xlplayer' do
-  version '2.0.3.1265'
-  sha256 'fbdf478d9dcaa7aba0f1cf7c212993edd0af7ba9a520e4ae4dd84ccf843690c0'
+  version '2.0.5.1285'
+  sha256 '80136420cf7e38279af00009ca487b131ae82a7031964488d907287ae2b855a6'
 
   # down.sandai.net was verified as official when first introduced to the cask
   url "http://down.sandai.net/mac/xlplayer_#{version}.dmg"
-  appcast 'https://video.xunlei.com/mac.html'
+  appcast 'http://static-xl.a.88cdn.com/json/xunlei_video_version_mac.json'
   name 'XLPlayer for Mac'
   name '迅雷影音 for Mac'
-  homepage 'https://video.xunlei.com/mac.html'
+  homepage 'http://video.xunlei.com/mac.html'
 
   depends_on macos: '>= :yosemite'
 
