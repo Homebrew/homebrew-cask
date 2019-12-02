c889f5b83eec21e15cd0c6ebcbbc30984f1ad45f
Randall <ran.dall@icloud.com>
2019-12-02 10:30:42 -0800

    Update whatsize from 7.2.1 to 7.3.2

--- Casks/whatsize.rb
+++ Casks/whatsize.rb
@@ -1,6 +1,6 @@
 cask 'whatsize' do
-  version '7.2.1'
-  sha256 '945b6ae5543d568347875c144af7f6e7972e141eb003f5cd0ec54714e3b1521d'
+  version '7.3.2'
+  sha256 '60f62ae68fbc60e71d00496163f71fcb2c3f7bc244658fd43761ce352fd71012'
 
   url "https://www.whatsizemac.com/software/whatsize#{version.major}/whatsize_#{version}.tgz"
   appcast "https://www.whatsizemac.com/software/whatsize#{version.major}/release/notes.xml"
