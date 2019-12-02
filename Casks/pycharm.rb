77c186fc2b0bb51ae7b5ed29b6a43624f874d973
jcbot <jcb@leipert.io>
2019-12-02 19:21:35 +0100

    Update pycharm to 2019.3,193.5233.109 (#73515)

--- Casks/pycharm.rb
+++ Casks/pycharm.rb
@@ -1,6 +1,6 @@
 cask 'pycharm' do
-  version '2019.2.5,192.7142.56'
-  sha256 'ffa96a6f3613e0ce608476cd26263e44bf356a5700013e6b1aae2d1d77592277'
+  version '2019.3,193.5233.109'
+  sha256 '13ba9b0ab45f0a33c359b10cd479b6f6fe318091c91a626ef8584c2998869b79'
 
   url "https://download.jetbrains.com/python/pycharm-professional-#{version.before_comma}.dmg"
   appcast 'https://data.services.jetbrains.com/products/releases?code=PCP&latest=true&type=release'
