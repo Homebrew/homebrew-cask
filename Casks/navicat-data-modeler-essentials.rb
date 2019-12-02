69200d78e02cb9609b35f3d3340d7d30a9772a78
Vítor Galvão <info@vitorgalvao.com>
2019-12-02 12:10:46 +0000

    Update navicat-data-modeler-essentials.rb

--- Casks/navicat-data-modeler-essentials.rb
+++ Casks/navicat-data-modeler-essentials.rb
@@ -3,7 +3,7 @@ cask 'navicat-data-modeler-essentials' do
   sha256 '3628ce3e73711e24c9162bc890a453de39b2883fa5a59dc8c4b394274b4acdd8'
 
   url "http://download3.navicat.com/updater/modeler0#{version.major_minor.no_dots}_ess_mac_en.zip"
-  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Data%20Modeler%20Essentials&appLang=en'
+  appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Data%20Modeler%20Essentials'
   name 'Navicat Data Modeler Essentials'
   homepage 'https://www.navicat.com/en/products/navicat-data-modeler'
 
