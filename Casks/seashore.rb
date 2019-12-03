3af32ffb674c2f7936b7c68e968488915527a1f8
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-03 13:21:48 -0800

    Update seashore from 2.4.13 to 2.4.14 (#73608)

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,6 +1,6 @@
 cask 'seashore' do
-  version '2.4.13'
-  sha256 '5a93f113f77b04bb39ae4ea4a868d95796944819a22b2dd2df54821069bc0fe0'
+  version '2.4.14'
+  sha256 'bde60bb7812233039b9967b42e95f05d73b2a5e8db88dcbe1038a3ce96075c5b'
 
   url "https://github.com/robaho/seashore/releases/download/v#{version}/seashore-bin-#{version}.dmg"
   appcast 'https://github.com/robaho/seashore/releases.atom'
