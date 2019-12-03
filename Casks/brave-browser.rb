e08509eef5de5b4f11258d44e33dc6e15a691ae0
Vítor Galvão <info@vitorgalvao.com>
2019-12-03 21:53:55 +0000

    brave-browser.rb: include updates-cdn. in verified comment

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -2,7 +2,7 @@ cask 'brave-browser' do
   version '78.1.0.1,100.1'
   sha256 'aef49465a7bc9fddfa5743d401301122eb1c3c69c548e9ac09e82f804c8f8666'
 
-  # bravesoftware.com/sparkle/Brave-Browser was verified as official when first introduced to the cask
+  # updates-cdn.bravesoftware.com/sparkle/Brave-Browser was verified as official when first introduced to the cask
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/stable/#{version.after_comma}/Brave-Browser.dmg"
   appcast 'https://updates.bravesoftware.com/sparkle/Brave-Browser/stable/appcast.xml'
   name 'Brave'
