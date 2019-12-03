513efbd9c379849f1fda2706cf039c6b4016fc88
ZhengJin Fang <fangzhengjin@gmail.com>
2019-12-04 00:06:27 +0800

    Add Monitore 1.0.0

---
+++ Casks/monitore.rb
@@ -0,0 +1,9 @@
+cask 'monitore' do
+  version '1.0.0'
+  sha256 '5ef6fcc62a8880f238e9b59d8d7e5a6a4e609fd6305400d750ee1737832a7711'
+
+  url "http://lc-devqyyen.cn-n1.lcfile.com/306d2a604ff2ebb77276.dmg"
+  name 'monitore'
+  homepage 'https://eoikoe.com/monitore'
+  app '即刻地球.app'
+end
