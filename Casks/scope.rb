bba1b1b4a1bc47a13e0afabcff99845caeb69c60
Vítor Galvão <info@vitorgalvao.com>
2019-12-03 00:09:14 +0000

    Update scope.rb

--- Casks/scope.rb
+++ Casks/scope.rb
@@ -1,8 +1,10 @@
 cask 'scope' do
-  version :latest
-  sha256 :no_check
+  version '1.2.2'
+  sha256 '9c5c5bb4ba61c977ebc5a39261c3b3b12150f5bf9f235276e38e925aa00d3fb5'
 
-  url 'https://scope.dev/download'
+  # releases.undefinedlabs.com/scope was verified as official when first introduced to the cask
+  url "https://releases.undefinedlabs.com/scope/local/mac/#{version}/Scope.zip"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://scope.dev/download'
   name 'Scope for Mac'
   homepage 'https://scope.dev/'
 
