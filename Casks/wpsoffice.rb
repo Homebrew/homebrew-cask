aa414cc67c142c026b7e6b453c61231cc2daf183
suschizu <brigitte@corecode.io>
2019-12-03 16:42:21 +0000

    Update wpsoffice.rb from 1.7.0(2619) to 1.8.0,2797 (#73567)
    * Update wpsoffice.rb
    
    * Update wpsoffice.rb

--- Casks/wpsoffice.rb
+++ Casks/wpsoffice.rb
@@ -1,9 +1,9 @@
 cask 'wpsoffice' do
-  version '1.7.0(2619)'
-  sha256 'c8ea97170d7ee6ff76ebd929737313778318b0acbd33245c1e96f831924c09aa'
+  version '1.8.0,2797'
+  sha256 '43995f09cce5663d1cd126706a5bbe53be5240e5e5014689dc6a3f305cf008e0'
 
   # package.mac.wpscdn.cn was verified as official when first introduced to the cask
-  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.major_minor_patch}/WPS_Office_#{version}.dmg"
+  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.before_comma}/WPS_Office_#{version.before_comma}_#{version.after_comma}.dmg"
   appcast 'https://www.wps.cn/product/wpsmac/'
   name 'WPS Office'
   homepage 'https://www.wps.cn/product/wpsmac/'
