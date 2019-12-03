77815254fd2bad5ea1890f6a555f32c6b4dac6fc
ye4241 <ye4241@gmail.com>
2019-12-03 08:03:45 +0800

    Update Clover Configurator to 5.8.0 (#73514)

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,8 +1,8 @@
 cask 'clover-configurator' do
-  version '5.7.0.0'
-  sha256 '7d4cbf65e835541922d0411fa856835a78e642ee05e7573d69e63fe902e299f9'
+  version '5.8.0.0'
+  sha256 '66d4698b48022823cd09d08713d72ac15f3a3528ccf846e975f4632fcbeac771'
 
-  url 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/builds-data-ccg/CCG.zip'
+  url 'https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global'
   appcast 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/update-data-builds.xml'
   name 'Clover Configurator'
   homepage 'https://mackie100projects.altervista.org/'
