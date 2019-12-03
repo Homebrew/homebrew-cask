f1f3f18834703b6ff7f39171ed55421a73bac061
suschizu <brigitte@corecode.io>
2019-12-03 15:25:56 +0100

    Update wpsoffice.rb

--- Casks/wpsoffice.rb
+++ Casks/wpsoffice.rb
@@ -3,7 +3,7 @@ cask 'wpsoffice' do
   sha256 '43995f09cce5663d1cd126706a5bbe53be5240e5e5014689dc6a3f305cf008e0'
 
   # package.mac.wpscdn.cn was verified as official when first introduced to the cask
-  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.before_comma}/WPS_Office_#{version.before_comma}(#{version.after_comma}).dmg"
+  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.before_comma}/WPS_Office_#{version.before_comma}_#{version.after_comma}.dmg"
   appcast 'https://www.wps.cn/product/wpsmac/'
   name 'WPS Office'
   homepage 'https://www.wps.cn/product/wpsmac/'
