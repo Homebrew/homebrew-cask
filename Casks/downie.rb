2152a7f8f7730aafe04c9ec05a3c1bd028738304
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-04 12:51:41 -0800

    Update downie from 3.9,2099 to 3.9.1,3005 (#73663)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.9,2099'
-  sha256 '211651f96f41c6a54fe980f92157dde081458b0e06e5f0c033262bb5a0fc8759'
+  version '3.9.1,3005'
+  sha256 'c6664f67fd8dfe0e116ba016a0992191025f37ca58fd07bb709c2adcd02fd794'
 
   url "https://trial.charliemonroe.net/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://trial.charliemonroe.net/downie/updates_#{version.major}.xml"
