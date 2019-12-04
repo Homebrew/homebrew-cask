1f0effbb2a5dc0e74de7a6f59baa56c240d349e2
sortega306 <ortega@tune-is.de>
2019-12-04 22:36:58 +0100

    Update makemkv from 1.14.6 to 1.14.7

--- Casks/makemkv.rb
+++ Casks/makemkv.rb
@@ -1,6 +1,6 @@
 cask 'makemkv' do
-  version '1.14.6'
-  sha256 '53a18d21b0a70ee3786cf527d53ec792a123a000245269256c403091280bf99e'
+  version '1.14.7'
+  sha256 '4b43eb13a8328b5d637e7574a566b3e0515ca169f00a5eecbbda6ae8648861e0'
 
   url "https://www.makemkv.com/download/makemkv_v#{version}_osx.dmg"
   appcast 'https://www.makemkv.com/download/'
