c6b4e82cbc3025b8b93b16814cece2e7fc1fb70d
Vítor Galvão <info@vitorgalvao.com>
2019-12-04 21:02:09 +0000

    Update get-iplayer-automator.rb

--- Casks/get-iplayer-automator.rb
+++ Casks/get-iplayer-automator.rb
@@ -1,10 +1,10 @@
 cask 'get-iplayer-automator' do
-  version '1.17.b20191130002'
+  version '1.17,b20191130002'
   sha256 '44128ad80e31ed21c0cd1580041f4dec51ecc7b72e7383a9e0ce8f6bbde20be5'
 
-  url "https://github.com/Ascoware/get-iplayer-automator/releases/download/v#{version.major_minor}/Get.iPlayer.Automator.v#{version.major_minor}.#{version.patch}.zip"
+  url "https://github.com/Ascoware/get-iplayer-automator/releases/download/v#{version.before_comma}/Get.iPlayer.Automator.v#{version.before_comma}.#{version.after_comma}.zip"
   appcast 'https://github.com/Ascoware/get-iplayer-automator/releases.atom',
-          configuration: version.major_minor
+          configuration: version.before_comma
   name 'Get iPlayer Automator'
   homepage 'https://github.com/Ascoware/get-iplayer-automator'
 
