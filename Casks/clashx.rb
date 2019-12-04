aa9f347735c0c1743221da98f5a9e3cff44f2a3a
Randall <ran.dall@icloud.com>
2019-12-04 12:23:22 -0800

    Update clashx from 1.16.7 to 1.16.8

--- Casks/clashx.rb
+++ Casks/clashx.rb
@@ -1,6 +1,6 @@
 cask 'clashx' do
-  version '1.16.7'
-  sha256 '4885fe42aba872fa86a9a388072dbf51ad8b9f1c31a442520c410df5faf4792b'
+  version '1.16.8'
+  sha256 '77adfe016574607c1c8704ec8b2fdc10b535563256440a69c860e6bbfa620832'
 
   url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
   appcast 'https://github.com/yichengchen/clashX/releases.atom'
