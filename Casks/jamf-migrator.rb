3823e14aceb8b47c9b608fd56d1c35fcd16f8eb0
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-05 09:20:24 -0800

    Update jamf-migrator from 4.1.2 to 4.1.3 (#73718)

--- Casks/jamf-migrator.rb
+++ Casks/jamf-migrator.rb
@@ -1,6 +1,6 @@
 cask 'jamf-migrator' do
-  version '4.1.2'
-  sha256 '8e210bf88e6e8fcf56b5512f383a05205951054e5184797c1542e952ec3373a9'
+  version '4.1.3'
+  sha256 'd1a081ecf3d9a4153763e74fc346fb9a8f301a4d262df073f208171c219205ff'
 
   url 'https://github.com/jamf/JamfMigrator/releases/download/current/jamf-migrator.zip'
   appcast 'https://github.com/jamf/JamfMigrator/releases.atom'
