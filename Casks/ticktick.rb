8e874c4737c3fe109626ca34d3cb54c656afe82a
Jefferson Bledsoe <me@jeffersonbledsoe.com>
2019-12-05 11:07:06 +0000

    Update ticktick version from 3.3.00,117 to 3.3.00,119 and set auto-updating flag

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -1,12 +1,14 @@
 cask 'ticktick' do
-  version '3.3.00,117'
-  sha256 '366ea616e6d99f64260e6f2a92a74984ad48133416211ee00201763bc5f0da9b'
+  version '3.3.00,119'
+  sha256 '7ae6a1bf2129f242b7d4625e07c6437ec629a0ec289aec0468edeeca0cda95b1'
 
   # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.ticktick.com/static/getApp/download?type=mac'
   name 'TickTick'
   homepage 'https://www.ticktick.com/home'
+
+  auto_updates true
 
   app 'TickTick.app'
 end
