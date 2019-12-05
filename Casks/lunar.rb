3f5c635484df298ce74a835329a1432abee19ab2
Tomasz <tomasz.brek@moogsoft.com>
2019-12-05 09:19:48 +0000

    Update lunar from 2.9.6 to 2.9.7

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,6 +1,6 @@
 cask 'lunar' do
-  version '2.9.6'
-  sha256 '656bbf5dcbae44133c478066dd92a29416a8f0ac795f9a156916cc2f21fbf526'
+  version '2.9.7'
+  sha256 '92abe6d8271a160985f6031b56f361e70a572b725d930009abc52fbc39525ae2'
 
   # github.com/alin23/Lunar was verified as official when first introduced to the cask
   url "https://github.com/alin23/Lunar/releases/download/v#{version}/Lunar-#{version}.dmg"
