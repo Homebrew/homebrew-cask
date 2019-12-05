bb3e0aefb2f45bd89b429f49f63ba2e7eef6bae1
suschizu <brigitte@corecode.io>
2019-12-05 14:50:23 +0100

    Update tastyworks.rb

--- Casks/tastyworks.rb
+++ Casks/tastyworks.rb
@@ -3,7 +3,8 @@ cask 'tastyworks' do
   sha256 '73ce5e43a8455c7e93abcc6bddcea3cfd13ad90ab5893eefd7d0e1fd31512e54'
 
   url "https://download.tastyworks.com/desktop-#{version.major}.x.x/#{version}/tastyworks-#{version}.dmg"
-  appcast 'https://tastyworks.com/technology.html'
+  appcast 'https://tastyworks.freshdesk.com/support/solutions/articles/43000435186-recent-release-notes',
+          configuration: version.major_minor
   name 'tastyworks'
   homepage 'https://tastyworks.com/'
 
