f298dc06052b7c25c01b7db704147758ff810db4
Liam Dawson <liam@ldaws.com>
2019-12-05 16:57:12 +1100

    Add Fitbit OS Simulator v0.8.0

---
+++ Casks/fitbit-os-simulator.rb
@@ -0,0 +1,10 @@
+cask 'fitbit-os-simulator' do
+  version '0.8.0'
+  sha256 'ea378d6bfc1088bea0a33250ba4d0d018200393de67bf80d4474167e6670628b'
+
+  url "https://simulator-updates.fitbit.com/Fitbit%20OS%20Simulator-latest-#{version}.dmg"
+  name 'Fitbit OS Simulator'
+  homepage 'https://dev.fitbit.com/'
+
+  app 'Fitbit OS Simulator.app'
+end
