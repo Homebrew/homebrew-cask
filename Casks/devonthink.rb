eaa3a3f22dc71291e62c9ea359e0cdc11cdc17a7
Randall <ran.dall@icloud.com>
2019-12-05 09:19:10 -0800

    Update devonthink from 3.0.2 to 3.0.3

--- Casks/devonthink.rb
+++ Casks/devonthink.rb
@@ -1,6 +1,6 @@
 cask 'devonthink' do
-  version '3.0.2'
-  sha256 '922ebe623aa7fb66c928a0e316de63ccae36a9bdb508e16077e6227aaca34db5'
+  version '3.0.3'
+  sha256 'e34cba7acf37df73f04a69d21e16c6af81d38c9f3d51d44ad46692ec1747f5dd'
 
   # s3.amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_#{version.major}.dmg.zip"
