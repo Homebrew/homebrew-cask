e69c975c438b1e4cdc5e3161767bd88f7761d949
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-05 09:37:28 -0800

    Update gingko from 2.4.5 to 2.4.6 (#73721)

--- Casks/gingko.rb
+++ Casks/gingko.rb
@@ -1,6 +1,6 @@
 cask 'gingko' do
-  version '2.4.5'
-  sha256 '892bd882da397db1291fe48bbd6746bdd0bf096e451d561a6045a0187a6bcbed'
+  version '2.4.6'
+  sha256 '241f32ffb4fe2be77790e79ffa2e00ba59128a3ecd98bccff60758d29a6beef5'
 
   # github.com/gingko/client was verified as official when first introduced to the cask
   url "https://github.com/gingko/client/releases/download/v#{version}/Gingko-#{version}-mac.zip"
