9999c05d5a924b0bc40c967dc7509a680082b93a
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-05 09:51:59 -0800

    Update kiwi-for-g-suite from 2.0.28v to 2.0.29v (#73729)

--- Casks/kiwi-for-g-suite.rb
+++ Casks/kiwi-for-g-suite.rb
@@ -1,6 +1,6 @@
 cask 'kiwi-for-g-suite' do
-  version '2.0.28v'
-  sha256 '1036f3fd750d8da7177899704fde230391b2a4116077e68216ac0c9ababb429e'
+  version '2.0.29v'
+  sha256 'e5562ca1dca044c3061ff30b53597c1577bbc291a259f9d9e77f224f8448f11d'
 
   # kiwiforgsuite.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://kiwiforgsuite.s3.amazonaws.com/mac/release/Kiwi+for+G+Suite.pkg'
