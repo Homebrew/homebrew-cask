3bc0b3a7f2076dc73da846fb21746386cde2d39b
Svend <svend.jin@gmail.com>
2019-12-06 23:48:43 +0800

    hotfix upic v0.15.0

--- Casks/upic.rb
+++ Casks/upic.rb
@@ -1,6 +1,6 @@
 cask 'upic' do
   version '0.15.0'
-  sha256 '75ef5fb0a80d318c135823ae776ea8df31f03ec3ae989770832b0330696cf8db'
+  sha256 '15bdba4634f027c8f3a32feaf0d5b7aa9a49592a1f3ec8e7ff4d13df12248857'
 
   url "https://github.com/gee1k/uPic/releases/download/v#{version}/uPic.dmg"
   appcast 'https://github.com/gee1k/uPic/releases.atom'
