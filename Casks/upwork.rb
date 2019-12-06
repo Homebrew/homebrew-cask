91ca7980086287ab9ca91ff6a92ba3bcf309ceee
CoreCode <core-code@users.noreply.github.com>
2019-12-06 23:19:52 +0100

    Update upwork.rb

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -3,6 +3,7 @@ cask 'upwork' do
   sha256 '6cba593f4140ddaa55e9a1c38d7f2c3ed87242a25dd9c8b0489654220791719b'
 
   url "https://updates-desktopapp.upwork.com/binaries/v#{version.before_comma.dots_to_underscores}_#{version.after_comma}/Upwork.dmg"
+  appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://updates-desktopapp.upwork.com/binaries/versions-mac.json&splitter_1=Beta&index_1=0'
   name 'Upwork'
   homepage 'https://www.upwork.com/'
 
