118a8b537835f8c026a2d12a8214a8842b4f53d2
Andrei Borisov <schrodinger_cat@me.com>
2019-12-06 23:50:29 +0300

    Update yandex-cloud-cli to 0.45.0 (#73772)

--- Casks/yandex-cloud-cli.rb
+++ Casks/yandex-cloud-cli.rb
@@ -1,6 +1,6 @@
 cask 'yandex-cloud-cli' do
-  version '0.44.0'
-  sha256 'b32069a197dd1724176ff6f71c2ea4c81d4ffefc98b64b2a00531cfeb7f87a5d'
+  version '0.45.0'
+  sha256 'de5783a3bb860cd0bb32877eb909c5735d056fe92a2a09dfaf9a125aeea4787f'
 
   # yandexcloud.net/yandexcloud-yc was verified as official when first introduced to the cask
   url "https://storage.yandexcloud.net/yandexcloud-yc/release/#{version}/darwin/amd64/yc"
