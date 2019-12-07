d68875a8aae723419c8d20437b751b90f04abc76
Vítor Galvão <info@vitorgalvao.com>
2019-12-07 15:49:41 +0000

    Update scummvm.rb

--- Casks/scummvm.rb
+++ Casks/scummvm.rb
@@ -3,7 +3,7 @@ cask 'scummvm' do
   sha256 'c82f8a1c20a555c2dc4fee6854afffe33815a7d1bee58fe13052ff52b41a98f8'
 
   url "https://scummvm.org/frs/scummvm/#{version}/scummvm-#{version}-macosx.dmg"
-  appcast 'https://www.scummvm.org/feeds/rss/'
+  appcast 'https://www.scummvm.org/downloads/'
   name 'ScummVM'
   homepage 'https://www.scummvm.org/'
 
