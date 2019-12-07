d383d100f9205956ee30e1fb6cc10688ae8e7100
Samir Musali <samir.musali@logdna.com>
2019-12-06 22:36:10 -0500

    logdna-agent:v1.6.2

--- Casks/logdna-agent.rb
+++ Casks/logdna-agent.rb
@@ -1,6 +1,7 @@
 cask 'logdna-agent' do
   version '1.6.2'
   sha256 '1335d92a51099685e1773f26016e17d2c39dc67b8437ec107df80abf330fda3c'
+
   # github.com/logdna/logdna-agent was verified as official when first introduced to the cask
   url "https://github.com/logdna/logdna-agent/releases/download/#{version}/logdna-agent-#{version}.pkg"
   appcast 'https://github.com/logdna/logdna-agent/releases.atom'
