c7ad5befb82550a8f2c478f8c97906697e24bc97
Vítor Galvão <info@vitorgalvao.com>
2019-12-07 15:41:36 +0000

    Update scummvm.rb

--- Casks/scummvm.rb
+++ Casks/scummvm.rb
@@ -2,7 +2,7 @@ cask 'scummvm' do
   version '2.5.1'
   sha256 '628add60fdedf2cb5c255b601109f9487e1a789e37145064fc4523ab2bacd52b'
 
-  url "https://scummvm.org/frs/scummvm/#{version.major_minor_patch}/scummvm-#{version}-macosx.dmg"
+  url "https://scummvm.org/frs/scummvm/#{version}/scummvm-#{version}-macosx.dmg"
   appcast 'https://www.scummvm.org/appcasts/macosx/release.xml'
   name 'ScummVM'
   homepage 'https://www.scummvm.org/'
