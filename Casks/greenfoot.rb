4bf3117097cbef5c126018356d70a3450541239d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-06 17:35:21 -0800

    Update greenfoot from 3.6.0 to 3.6.1 (#73809)

--- Casks/greenfoot.rb
+++ Casks/greenfoot.rb
@@ -1,6 +1,6 @@
 cask 'greenfoot' do
-  version '3.6.0'
-  sha256 '575413dd4d1c7a0e4e97c39192478e98ca944cbad9684c161e57191204dd1ebe'
+  version '3.6.1'
+  sha256 '9afbc077f6454d83749224cc62b5192eae96edf4d7d6beaee880712bb17e8646'
 
   url "https://www.greenfoot.org/download/files/Greenfoot-mac-#{version.no_dots}.zip"
   appcast 'https://www.greenfoot.org/download'
