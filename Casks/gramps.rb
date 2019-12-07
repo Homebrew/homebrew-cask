e2a2f5268106f2ed636419eb7986dcce7095da0f
Vítor Galvão <info@vitorgalvao.com>
2019-12-07 21:31:19 +0000

    gramps: updated version to new major_minor_patch split

--- Casks/gramps.rb
+++ Casks/gramps.rb
@@ -1,9 +1,9 @@
 cask 'gramps' do
-  version '5.1.1-1'
+  version '5.1.1,1'
   sha256 '66398f859ef6e4be9fbe7c9970d69f778ba021f0a93ebbd483e26cdd1cb54418'
 
   # github.com/gramps-project/gramps was verified as official when first introduced to the cask
-  url "https://github.com/gramps-project/gramps/releases/download/v#{version.major_minor_patch}/Gramps-Intel-#{version}.dmg"
+  url "https://github.com/gramps-project/gramps/releases/download/v#{version.before_comma}/Gramps-Intel-#{version.before_comma}-#{version.after_comma}.dmg"
   appcast 'https://github.com/gramps-project/gramps/releases.atom'
   name 'Gramps'
   homepage 'https://gramps-project.org/introduction-WP/'
