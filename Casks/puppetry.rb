f00e342ad981a954248736f02b9447d90e45047b
Vítor Galvão <info@vitorgalvao.com>
2019-12-07 15:09:22 +0000

    Update puppetry.rb

--- Casks/puppetry.rb
+++ Casks/puppetry.rb
@@ -2,7 +2,7 @@ cask 'puppetry' do
   version '3.0.0'
   sha256 'a7411c0c6dd5a0bd7d5c51d046a5bd0ae1a06a143606fd49b236b3997aa7fd56'
 
-  # github.com/dsheiko/puppetry/releases was verified as official when first introduced to the cask
+  # github.com/dsheiko/puppetry was verified as official when first introduced to the cask
   url "https://github.com/dsheiko/puppetry/releases/download/v#{version}/puppetry-mac-v#{version}.zip"
   appcast 'https://github.com/dsheiko/puppetry/releases.atom'
   name 'puppetry'
