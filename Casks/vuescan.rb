b022b7415d13976655948b43f5c0c9eeff6b5c07
Raoul Bhatia <raoul@bhatia.at>
2019-12-08 13:20:38 +0100

    vuescan: Update sha256 to no_check for vuescan 9.7.11
    Set sha256 to no_check as upstream packages might be updated in-place.
    
    Partially reverts 3c1f64c5a07f54883ded3a744fdb5f15a5b82b2c

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.7.11'
-  sha256 '2537c70dccd3dc1d6b053e6ea3125460025f1b32c0ebf3920a864b140b7560e6'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
