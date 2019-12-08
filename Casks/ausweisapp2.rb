8975136ae292d519f4e57006f55ba8c4b02c9405
Vítor Galvão <info@vitorgalvao.com>
2019-12-08 02:14:48 +0000

    Update ausweisapp2.rb

--- Casks/ausweisapp2.rb
+++ Casks/ausweisapp2.rb
@@ -2,8 +2,8 @@ cask 'ausweisapp2' do
   version '1.16.2'
   sha256 '630c491fea0897018e2588d98c77dd8cd63dd2126cad8eba9b631bee538f6c85'
 
-  # www.ausweisapp.bund.de was verified as official when first introduced to the cask
   url "https://www.ausweisapp.bund.de/uploads/tx_ausweisdownloads/AusweisApp2-#{version}.dmg"
+  appcast 'https://www.ausweisapp.bund.de/download/windows-und-mac/'
   name 'AusweisApp2'
   homepage 'https://www.ausweisapp.bund.de/'
 
