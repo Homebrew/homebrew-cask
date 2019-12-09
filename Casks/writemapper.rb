60f52ef0dc8177681261c2eaa0ecda6c644fddf3
Tsvetan Ovedenski <dev@tsovedenski.com>
2019-12-09 17:10:15 +0100

    Add WriteMapper v2.9.1

---
+++ Casks/writemapper.rb
@@ -0,0 +1,10 @@
+cask 'writemapper' do
+  version '2.9.1'
+  sha256 '04e46adf4245d61361980cfa6921c505d0c80d6525f474fc10391bb0b0a9c44e'
+
+  url "https://writemapper.com/static/app/mac/WriteMapper-#{version}.dmg"
+  name 'WriteMapper'
+  homepage 'https://writemapper.com/'
+
+  app 'WriteMapper.app'
+end
