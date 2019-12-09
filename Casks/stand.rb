e239aa927067d9ba6ff0ba328b61f7e3f35b4dab
Vítor Galvão <info@vitorgalvao.com>
2019-12-09 14:05:29 +0000

    stand.rb: fix URL and appcast (#73908)

--- Casks/stand.rb
+++ Casks/stand.rb
@@ -3,8 +3,9 @@ cask 'stand' do
   sha256 '8919e43c9c591657d8d6961b25e8dc5f77d706d71eb246839be22522a82bb0ec'
 
   # get-stand-app.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://get-stand-app.s3.amazonaws.com/#{version.major_minor}/Stand.zip"
-  appcast 'https://standapp-sparkle-updater.herokuapp.com/'
+  url "https://get-stand-app.s3.amazonaws.com/#{version.patch}/Stand.zip"
+  appcast 'https://standapp-sparkle-updater.herokuapp.com/',
+          configuration: version.major_minor
   name 'Stand'
   homepage 'https://getstandapp.com/'
 
