825b033be36e7358827e4090f217c5c9d066a212
Vítor Galvão <info@vitorgalvao.com>
2019-12-09 13:26:28 +0000

    komodo-edit: fix extraneous brace in URL

--- Casks/komodo-edit.rb
+++ Casks/komodo-edit.rb
@@ -2,7 +2,7 @@ cask 'komodo-edit' do
   version '11.1.1,18206'
   sha256 '3ee0bfe5494b4eb785cbeb7efff68a39db7cd6e57a43b975a614ed36fd17277a'
 
-  url "https://downloads.activestate.com/Komodo/releases/#{version.before_comma}}/Komodo-Edit-#{version.before_comma}-#{version.after_comma}-macosx-x86_64.dmg"
+  url "https://downloads.activestate.com/Komodo/releases/#{version.before_comma}/Komodo-Edit-#{version.before_comma}-#{version.after_comma}-macosx-x86_64.dmg"
   appcast 'https://www.activestate.com/komodo-ide/downloads/edit'
   name 'Komodo Edit'
   homepage 'https://www.activestate.com/komodo-edit/'
