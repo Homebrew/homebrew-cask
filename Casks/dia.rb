e6ae4d59f1e4921087f46b2f5deedeff4c8cf7fe
Vítor Galvão <info@vitorgalvao.com>
2019-12-09 13:24:57 +0000

    dia.rb: fix extra character in URL

--- Casks/dia.rb
+++ Casks/dia.rb
@@ -3,7 +3,7 @@ cask 'dia' do
   sha256 '9d3038c01347716800688830eaf52204deb78affe74a5f0c6e0a48fd414d44be'
 
   # sourceforge.net/dia-installer/dia was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/dia-installer/dia/#{version.before_comma}}/Dia-#{version.before_comma}-#{version.after_comma}.dmg"
+  url "https://downloads.sourceforge.net/dia-installer/dia/#{version.before_comma}/Dia-#{version.before_comma}-#{version.after_comma}.dmg"
   appcast 'https://sourceforge.net/projects/dia-installer/rss?path=/dia'
   name 'Dia'
   homepage 'http://dia-installer.de/'
