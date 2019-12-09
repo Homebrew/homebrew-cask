77e1fbe48e0f752225befb31c0fcb1bad85d11d8
ZhengJin Fang <fangzhengjin@gmail.com>
2019-12-10 00:11:42 +0800

    Update NeteaseMusic to latest

--- Casks/neteasemusic.rb
+++ Casks/neteasemusic.rb
@@ -1,10 +1,9 @@
 cask 'neteasemusic' do
-  version '2.3.1_824,605533604.4f60.201911523186'
-  sha256 '4860316e46904594ed2ccb96fc451bb4d49ee7de01be7c64069980d50edac989'
+  version :latest
+  sha256 :no_check
 
-  # d1.music.126.net was verified as official when first introduced to the cask
-  url "https://d1.music.126.net/dmusic/obj/w5zCg8OCw6fCn2vDicOl/#{version.after_comma.major}/#{version.after_comma.minor}/#{version.after_comma.patch}/NeteaseMusic_#{version.before_comma}_web.dmg",
-      user_agent: :fake
+  # music.163.com was verified as official when first introduced to the cask
+  url 'https://music.163.com/api/osx/download/latest'
   appcast 'https://music.163.com/api/mac/appcast.xml'
   name 'NetEase cloud music'
   name '网易云音乐'
