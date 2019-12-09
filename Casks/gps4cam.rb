c845ee3cb3f50072cc063c0bd80d647e515428f6
Vítor Galvão <info@vitorgalvao.com>
2019-12-09 14:33:02 +0000

    Update gps4cam.rb

--- Casks/gps4cam.rb
+++ Casks/gps4cam.rb
@@ -1,6 +1,6 @@
 cask 'gps4cam' do
   version '7.2'
-  sha256 :no_check
+  sha256 '19f4b516077f8141471719497b9169cfde48cbd68fec80ea2f7532d256752cb4'
 
   # diguet.fr/gps4cam_EN/gps4cam was verified as official when first introduced to the cask
   url "http://diguet.fr/gps4cam_EN/gps4cam/Download_files/gps4cam_V#{version}.dmg"
