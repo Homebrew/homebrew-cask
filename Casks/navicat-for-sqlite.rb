24afe9716483d998d8cf25e478d4851bc1a14c90
Randall <ran.dall@icloud.com>
2019-12-09 16:12:56 -0800

    Update navicat-for-sqlite from 15.0.3 to 15.0.4

--- Casks/navicat-for-sqlite.rb
+++ Casks/navicat-for-sqlite.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-sqlite' do
-  version '15.0.3'
-  sha256 'a5648641e1a55b9307ac590b8e7635b3913fd19c85e9f403ef585a8bcf46ee60'
+  version '15.0.4'
+  sha256 'ce148a3b182b413dc79d05f8bce6066ef62fae797d22979eea777f786ecd73fd'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_sqlite_en.dmg"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20SQLite&appLang=en'
