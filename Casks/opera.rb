c68d6fb44859799859077a8c1a1992cdfcd592b2
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2019-12-10 15:20:35 -0600

    Update opera from 65.0.3467.62 to 65.0.3467.69 (#74004)

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,6 +1,6 @@
 cask 'opera' do
-  version '65.0.3467.62'
-  sha256 'ea5b0ee21e19f6283e5aaa6aacde475f428861d32f7dd63f02562ab96614e165'
+  version '65.0.3467.69'
+  sha256 'ddc97946ca14f49035dff46b4e47765d698aeaffbc5ec74558917bc7fba558a0'
 
   url "https://get.geo.opera.com/pub/opera/desktop/#{version}/mac/Opera_#{version}_Setup.dmg"
   appcast 'https://ftp.opera.com/pub/opera/desktop/'
