1af75b8ef84dd5b55b3bcfdaf806119d04e10b61
suschizu <brigitte@corecode.io>
2019-12-10 13:59:26 +0100

    Update krisp.rb

--- Casks/krisp.rb
+++ Casks/krisp.rb
@@ -3,7 +3,7 @@ cask 'krisp' do
   sha256 'b6d07e8c2258f53d6a9b35c936dd0293eee8ecc5dad3ab902ece0ebb24c14c2f'
 
   url "https://cdn.krisp.ai/mac/release/v#{version.major}.#{version.minor}/krisp_#{version}.pkg"
-  appcast 'https://krisp.ai/'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://api.krisp.ai/v2/download/mac'
   name 'Krisp'
   homepage 'https://krisp.ai/'
 
