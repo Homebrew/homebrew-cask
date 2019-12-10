a816c4fc1c6dcd3a114ff2936030cb2f141d7c0f
Julian Mayer <julian@corecode.io>
2019-12-10 11:57:40 +0100

    Update beoplay-software-update

--- Casks/beoplay-software-update.rb
+++ Casks/beoplay-software-update.rb
@@ -2,7 +2,7 @@ cask 'beoplay-software-update' do
   version '1.0.6'
   sha256 '7e0500f935557f34d46625e4abe7081cdaa343558a973d3284000ab4c37b21a6'
 
-  url "https://www.bang-olufsen.com/~/mediaV3/files/software-downloads/beoplay-software-updater/beoplay-software-updater-#{version.dots_to_hyphens}.dmg"
+  url "https://www.bang-olufsen.com/~/mediaV3/files/software-downloads/beoplay-software-updater/beoplay-software-updater-#{version.dots_to_hyphens}-mac.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=#{url}",
           configuration: version.dots_to_hyphens
   name 'BeoPlay Software Update'
