9c2d990420680a4f9fa0edbec5bfe35e45b9ab7b
韦洪将 <weihongjiang@ecpark.cn>
2019-12-10 13:35:44 +0800

    Add MacStroke.app v1.0.6

--- Casks/macstroke.rb
+++ Casks/macstroke.rb
@@ -2,7 +2,7 @@ cask 'macstroke' do
   version '1.0.6'
   sha256 '753a017624b6536793a74d3ceec75478b1cf4db2a344dfc041ab110aa0697719'
 
-  url "https://github.com/mtjo/MacStroke/releases/download/1.0.6/MacStroke-1.0.6.zip"
+  url 'https://github.com/mtjo/MacStroke/releases/download/1.0.6/MacStroke-1.0.6.zip'
   name 'MacStroke'
   homepage 'https://github.com/mtjo/MacStroke'
 
