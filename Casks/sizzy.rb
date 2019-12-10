f547626fc706b8918a385b426f552559f44c37e9
CoreCode <core-code@users.noreply.github.com>
2019-12-10 12:34:10 +0100

    Update sizzy.rb

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -3,7 +3,7 @@ cask 'sizzy' do
   sha256 '2bd63371a35acba0e6abd07df9a073a5a4c1e7841656fa5be0dce8ebc315a007'
 
   url 'https://sizzy.co/get-app'
-  appcast 'https://headway-widget.net/widgets/JAXRr7'
+  appcast 'hhttps://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X'
   name 'Sizzy'
   homepage 'https://sizzy.co/'
 
