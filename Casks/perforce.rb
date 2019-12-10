c58acc93e7d5a66357183b52f15136ebc7c1c359
Salvador Jacobi <salvadorj@unity3d.com>
2019-12-10 15:06:44 +0100

    Upgrade perforce to 19.2 (#73991)

--- Casks/perforce.rb
+++ Casks/perforce.rb
@@ -1,6 +1,6 @@
 cask 'perforce' do
-  version '19.1-1876401'
-  sha256 '4eb8a0632792f43e862e80cd8c40be5c22ab8e73555816a19710d9a1466298af'
+  version '19.2'
+  sha256 'b39f2c2c16ba268d4a9e5a2b6be4a063d7cc4da573cdc4accf9bdf803ca45d95'
 
   url "https://cdist2.perforce.com/perforce/r#{version.major_minor}/bin.macosx1010x86_64/helix-core-server.tgz"
   name 'Perforce Helix Versioning Engine'
