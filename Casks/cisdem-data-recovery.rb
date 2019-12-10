20228382f5a1a53048ed12ff2659886e16f51315
CoreCode <core-code@users.noreply.github.com>
2019-12-10 11:47:36 +0100

    Update cisdem-data-recovery (#73960)

--- Casks/cisdem-data-recovery.rb
+++ Casks/cisdem-data-recovery.rb
@@ -1,6 +1,6 @@
 cask 'cisdem-data-recovery' do
   version '6.1.0'
-  sha256 '917c4eceedbce9b757e4504f66c740bd0b52fc4d1295a9878825d0595337f065'
+  sha256 '035207b4082ee08d42c81b71c12ee07a33b925d84d7773558697e3f7ef3151e7'
 
   url 'http://download.cisdem.com/cisdem-datarecovery.dmg'
   appcast 'https://www.cisdem.com/data-recovery-mac/release-notes.html'
