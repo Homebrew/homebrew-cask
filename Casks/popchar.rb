825f19b860398fefc401585f63fa680f92895c20
Randall <ran.dall@icloud.com>
2019-12-11 11:09:58 -0800

    Update popchar from 8.7 to 8.8

--- Casks/popchar.rb
+++ Casks/popchar.rb
@@ -1,6 +1,6 @@
 cask 'popchar' do
-  version '8.7'
-  sha256 '5d21f19329b98cd2faa4e956344a750da2bcff219754c06be890d0caf9025a37'
+  version '8.8'
+  sha256 '298df60d208c635b1573218ad3681114a9d8656dd562929860989f7cb43a5c1a'
 
   url "https://www.ergonis.com/downloads/products/popcharx/PopCharX#{version.no_dots}-Install.dmg",
       user_agent: :fake
