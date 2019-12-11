bcb275e68b14f026d6924ec20c2402e8ee0d5c2c
Kyungwook, Park <kyungw00k@users.noreply.github.com>
2019-12-12 08:11:51 +0900

    Add SideNotes.app (#74049)
    * Add SideNotes.app
    
    * change single quote to double
    
    * Update sidenotes.rb

---
+++ Casks/sidenotes.rb
@@ -0,0 +1,11 @@
+cask 'sidenotes' do
+  version '1.0.2'
+  sha256 '463d9553da81b40a28098b4f2ad5040c3d3b71bfdfb7bc1f8434f8b53325f222'
+
+  url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version}.zip"
+  appcast 'https://www.apptorium.com/updates/sidenotes'
+  name 'SideNotes'
+  homepage 'https://www.apptorium.com/sidenotes'
+
+  app 'SideNotes.app'
+end
