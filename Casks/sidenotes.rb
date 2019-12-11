2835d2343625abd93ece31bf8ddd82579e50f06e
Kyungwook Park <parksama@gmail.com>
2019-12-11 23:26:37 +0900

    change single quote to double

--- Casks/sidenotes.rb
+++ Casks/sidenotes.rb
@@ -2,7 +2,7 @@ cask 'sidenotes' do
   version '1.0.2'
   sha256 '463d9553da81b40a28098b4f2ad5040c3d3b71bfdfb7bc1f8434f8b53325f222'
 
-  url 'https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version}.zip'
+  url "https://www.apptorium.com/public/products/sidenotes/releases/SideNotes-#{version}.zip"
   name 'SideNotes'
   homepage 'https://www.apptorium.com/sidenotes'
 
