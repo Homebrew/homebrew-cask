03c750a1cd8b9f9b14538dc583e3562b031eccfc
Vítor Galvão <info@vitorgalvao.com>
2019-12-11 23:00:37 +0000

    Update vitalsource-bookshelf.rb

--- Casks/vitalsource-bookshelf.rb
+++ Casks/vitalsource-bookshelf.rb
@@ -3,7 +3,7 @@ cask 'vitalsource-bookshelf' do
   sha256 'c3f6e6dbfc63e82078a2198633bb9072e648942c4162c49187efe1e2822395e8'
 
   # downloads.vitalbook.com was verified as official when first introduced to the cask
-  url "https://downloads.vitalbook.com/vsti/bookshelf/#{version.major_minor_patch}/mac/bookshelf/VitalSource-Bookshelf_#{version}.dmg"
+  url "https://downloads.vitalbook.com/vsti/bookshelf/#{version.major_minor_patch}/bookshelf/VitalSource-Bookshelf_#{version}.dmg"
   appcast 'https://support.vitalsource.com/hc/en-us/articles/360014107913-Mac'
   name 'VitalSource Bookshelf'
   homepage 'https://www.vitalsource.com/bookshelf-features'
