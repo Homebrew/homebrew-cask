f8cb3605c9361e6675b9c970e68401e7134add8f
CoreCode <core-code@users.noreply.github.com>
2019-12-12 00:22:48 +0100

    Update visual-paradigm (#73974)
    * Update visual-paradigm
    
    * Update visual-paradigm.rb

--- Casks/visual-paradigm.rb
+++ Casks/visual-paradigm.rb
@@ -1,10 +1,10 @@
 cask 'visual-paradigm' do
-  version '16.1,20191206'
-  sha256 '4508fc74e7ea289868c11ef590641cb77b4d785db7d54f09f5d6f7927b759f6c'
+  version '16.1,20191231'
+  sha256 '6b1a7a050d69217f6c41ef203542d84cbb4402efcda53d96047d3db9b0b58972'
 
   url "https://eu8.dl.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
-  appcast "https://eu8.dl.visual-paradigm.com/visual-paradigm/vp#{version.before_comma}/#{version.after_comma}/checksum.html",
-          configuration: "#{version.before_comma.dots_to_underscores}_#{version.after_comma}"
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.visual-paradigm.com/downloads/vp/checksum.html',
+          configuration: "#{version.before_comma}/#{version.after_comma}"
   name 'Visual Paradigm'
   homepage 'https://www.visual-paradigm.com/'
 
