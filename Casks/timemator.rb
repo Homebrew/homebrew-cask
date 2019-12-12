3152aab1cb21f9a309f275943e75fa9d7c8357c1
Viktor Szakats <vszakats@users.noreply.github.com>
2019-12-12 21:46:58 +0100

    secure URLs (#74124)
    * secure URLs
    
    * Update aurora-hdr.rb

--- Casks/timemator.rb
+++ Casks/timemator.rb
@@ -2,9 +2,9 @@ cask 'timemator' do
   version '2.0.1'
   sha256 '1225a71adb70004f14edb9c10d8e218a3ca38b7c85fbcea3535217b255d2cf0c'
 
-  # s3-eu-west-1.amazonaws.com/catforce-timemator was verified as official when first introduced to the cask
-  url 'https://s3-eu-west-1.amazonaws.com/catforce-timemator/releases/Timemator.dmg'
-  appcast 'https://s3-eu-west-1.amazonaws.com/catforce-timemator/releases/appcast.xml'
+  # catforce-timemator.s3.amazonaws.com was verified as official when first introduced to the cask
+  url 'https://catforce-timemator.s3.amazonaws.com/releases/Timemator.dmg'
+  appcast 'https://catforce-timemator.s3.amazonaws.com/releases/appcast.xml'
   name 'Timemator'
   homepage 'https://timemator.com/'
 
