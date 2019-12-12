276fb1b8d490d31381510d51ec943851bd7ae1ae
Chuyi Zhang <chuyi_zhang@outlook.com>
2019-12-12 12:35:33 -0800

    Update visual-studio-code from 1.40.2 to 1.41.0 (#74129)

--- Casks/visual-studio-code.rb
+++ Casks/visual-studio-code.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code' do
-  version '1.40.2'
-  sha256 'f23cdce5697e6bf40eea52d9903b79cd8853c5e6e9e6c611e58688f50910a974'
+  version '1.41.0'
+  sha256 '15a225fdc285bbf05a4e2ebd77fd6002b0914af5d955039c0ca580119456cd5a'
 
   url "https://update.code.visualstudio.com/#{version}/darwin/stable"
   appcast 'https://vscode-update.azurewebsites.net/api/update/darwin/stable/VERSION'
