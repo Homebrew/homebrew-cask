555c80ae326bc15c0d82433b6bd8557c156ca1be
suschizu <brigitte@corecode.io>
2019-12-13 15:20:18 +0100

    Update runway from 0.10.9 to 0.10.11 (#74179)

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -1,6 +1,6 @@
 cask 'runway' do
-  version '0.10.9'
-  sha256 'c82c0b75935e984b07c7756b7931bc80b865a98f5a2326a4f76cbc3978f3ed0a'
+  version '0.10.11'
+  sha256 'a8e8843c8114479fcca635fd119002bf682ce2ee656cae243d3b3a6c541ecbcd'
 
   # runway-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg"
