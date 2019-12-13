3a5bd6109e3236b4ef996cda060aba1ac5b655e5
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-12 19:05:27 -0800

    Update dcp-o-matic-batch-converter from 2.14.14 to 2.14.17 (#74162)

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-batch-converter' do
-  version '2.14.14'
-  sha256 'fd0cbf5a1f91f2b18b6213ae3b46a76a7c58bbc4f1ce0eb8d1bd5bebab733a6f'
+  version '2.14.17'
+  sha256 '83bbebe4ecbaa684b9f01dcd2917b91944fd6d7c43454ccd4467c674ca049199'
 
   url "https://dcpomatic.com/dl.php?id=osx-batch&version=#{version}"
   appcast 'https://dcpomatic.com/download'
