9a6bf0158a8d63a7912d064880a3ff93a546e311
Vítor Galvão <info@vitorgalvao.com>
2019-12-13 21:56:52 +0000

    Update jami.rb

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -3,11 +3,11 @@ cask 'jami' do
   sha256 '1f1f34cdd69f9c59c54a559364000014695081d2ab912241da82af0df8f4f8fd'
 
   url "https://dl.ring.cx/mac_osx/jami-#{version.no_dots}.dmg"
-  appcast 'https://dl.ring.cx/mac_osx/sparkle-ring.xml',
+  appcast 'https://dl.jami.net/mac_osx/sparkle-ring.xml',
           configuration: version.no_dots
   name 'Jami'
   name 'Savoir-faire Linux Ring'
-  homepage 'https://ring.cx/'
+  homepage 'https://jami.net/'
 
   auto_updates true
 
