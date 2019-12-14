7cddea64d50718f8f00e99196154d344d9009c5b
jabenninghoff <jbenninghoff@mac.com>
2019-12-14 13:13:37 -0600

    Update vuescan

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.7.12'
-  sha256 'ac2ac7afd81776b81417d8457bc5f0d9fd52974fc060cff6fb84ab63eb9ac812'
+  sha256 '94725345a1e9d303e98f33f9dd45c7f2b8747f36a65fc08843378bd23a1153e1'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
