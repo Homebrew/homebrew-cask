80f442c5ee07c225ba65a7a1c32ac59f6e72731f
Robert Blair <robertleeblairjr@gmail.com>
2019-12-14 06:07:17 -0600

    Update balenaetcher from 1.5.69 to 1.5.70

--- Casks/balenaetcher.rb
+++ Casks/balenaetcher.rb
@@ -1,6 +1,6 @@
 cask 'balenaetcher' do
-  version '1.5.69'
-  sha256 '0e6bb080caa2c4a0141f242699d9ed1f25b4752a41c5bad19f5e1bfa01f5ee44'
+  version '1.5.70'
+  sha256 'e70c2f613d70fad444fa2b2115cecbae68a6b958d46967bef700b85e1b377ecc'
 
   # github.com/balena-io/etcher was verified as official when first introduced to the cask
   url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg"
