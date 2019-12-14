345ed89771a2f34372cf57065a3c0e4b0b8c9e89
Randall <ran.dall@icloud.com>
2019-12-14 13:07:38 -0800

    Update displaycal from 3.8.9.2 to 3.8.9.3

--- Casks/displaycal.rb
+++ Casks/displaycal.rb
@@ -1,6 +1,6 @@
 cask 'displaycal' do
-  version '3.8.9.2'
-  sha256 'fa6d59bdcb4b1960295eca15ee4220c3cef4da73a15616359075fae234fa25c2'
+  version '3.8.9.3'
+  sha256 '3e3f4a506c3ffc1e2004d57c6cd521d6cacb1bb6a71f9e3fa4cd81ab6ad4f31a'
 
   # sourceforge.net/dispcalgui was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/dispcalgui/release/#{version}/DisplayCAL-#{version}.pkg"
