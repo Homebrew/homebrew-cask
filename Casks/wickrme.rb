93e27b59f94fd90923256b7171c1e8b950bcaa6f
Randall <ran.dall@icloud.com>
2019-12-13 18:33:15 -0800

    Update wickrme from 5.42.15 to 5.43.8

--- Casks/wickrme.rb
+++ Casks/wickrme.rb
@@ -1,6 +1,6 @@
 cask 'wickrme' do
-  version '5.42.15'
-  sha256 '7cf4349b9f97facd2e9257178ad018d5f359d17bb127a4ea90092a1604ebda4d'
+  version '5.43.8'
+  sha256 'd4664da74f9d0bc1a4e1e82bb44daf1a5ce6343965aa9aec8ecd6bb3062eae6b'
 
   # s3.amazonaws.com/static.wickr.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/me/WickrMe-#{version}.dmg"
