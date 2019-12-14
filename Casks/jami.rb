157f6a5f15782dd009350790dc59d8873e7058db
Vítor Galvão <info@vitorgalvao.com>
2019-12-14 02:04:10 +0000

    Update jami.rb

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -2,7 +2,7 @@ cask 'jami' do
   version '20191209.1141'
   sha256 '1f1f34cdd69f9c59c54a559364000014695081d2ab912241da82af0df8f4f8fd'
 
-  url "https://dl.ring.cx/mac_osx/jami-#{version.no_dots}.dmg"
+  url "https://dl.jami.net/mac_osx/jami-#{version.no_dots}.dmg"
   appcast 'https://dl.jami.net/mac_osx/sparkle-ring.xml',
           configuration: version.no_dots
   name 'Jami'
