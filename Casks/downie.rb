bff944bc7f096c99a2a2da8e341b4bb13b1e1536
yesRabbit <chandler.lu1997@gmail.com>
2019-12-14 14:15:28 +0800

    Update rb style of downie

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,7 +1,7 @@
 cask 'downie' do
   version '3.9.2,3007'
   sha256 'c2e5742af3c20bd12205c9042f2d325f24e89fdcdfbdf763116b1d173d5e72cf'
-  
+
   # charliemonroesoftware.com was verified as official when first introduced to the cask
   url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://trial.charliemonroe.net/downie/v#{version.major}/updates.xml"
