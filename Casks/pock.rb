493b9ecfa1599d6f57c1bb62b076a3f68f418e6d
Sam <sam@keeper.local>
2019-12-15 19:48:57 +0000

    Update pock from 0.7 to 0.7.1

--- Casks/pock.rb
+++ Casks/pock.rb
@@ -1,6 +1,6 @@
 cask 'pock' do
-  version '0.7'
-  sha256 '211037eaa903c20677e6560e74434276b02c0214d4f31b0d834ff47dc66f12b3'
+  version '0.7.1'
+  sha256 '273af80d2bde8a9d9687dad42da018a639b65345e36cefa06120191442f92daa'
 
   url "https://pock.dev/download.php?file=pock_#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/pigigaldi/Pock/releases.atom'
