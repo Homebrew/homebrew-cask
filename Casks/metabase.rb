a560779b941f1a719ccbdd32c07684e576266148
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2019-12-15 18:15:01 +0100

    Update metabase from 0.33.6.0 to 0.33.7.1 (#74292)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,9 +1,9 @@
 cask 'metabase' do
-  version '0.33.6.0'
-  sha256 'cbb66145f7b6d8705983601a519346525305a99275dcc2ea6399bd362752a5e7'
+  version '0.33.7.1'
+  sha256 '67744feb82639db10c6fe0686f710f5bc0df3e953bda09630baaaea264e106db'
 
   # s3.amazonaws.com/downloads.metabase.com was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/downloads.metabase.com/v#{version.major_minor_patch}/Metabase.zip"
+  url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip"
   appcast 'https://s3.amazonaws.com/downloads.metabase.com/appcast.xml'
   name 'Metabase'
   homepage 'https://www.metabase.com/'
