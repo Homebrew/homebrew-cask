d85b8e94ef2760caaf88f66745468196491e058d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-16 10:18:58 -0800

    Update wpsoffice from 1.8.1,2821 to 1.8.2,2861 (#74329)

--- Casks/wpsoffice.rb
+++ Casks/wpsoffice.rb
@@ -1,6 +1,6 @@
 cask 'wpsoffice' do
-  version '1.8.1,2821'
-  sha256 'fb69d6798354138073134f0ba345ad19ea69f25731bcbe60f0c1c5536c718399'
+  version '1.8.2,2861'
+  sha256 '9b2543cb8b611b864ee5826e1ecec9add3c8d5baf249c9f4fb3475bf763fae9d'
 
   # package.mac.wpscdn.cn was verified as official when first introduced to the cask
   url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.before_comma}/WPS_Office_#{version.before_comma}(#{version.after_comma}).dmg"
