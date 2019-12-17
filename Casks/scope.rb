648213372823f48d596dba5951b2a5d9e5a107a3
Fernando Mayo <fernando@undefinedlabs.com>
2019-12-17 12:42:02 +0100

    Update Scope from 1.2.2 to 1.2.3

--- Casks/scope.rb
+++ Casks/scope.rb
@@ -1,6 +1,6 @@
 cask 'scope' do
-  version '1.2.2'
-  sha256 '9c5c5bb4ba61c977ebc5a39261c3b3b12150f5bf9f235276e38e925aa00d3fb5'
+  version '1.2.3'
+  sha256 '45ee3569fc458841c3b44902ec8d1ea6a12d5ac2ffe34fc0835d901250ea6727'
 
   # releases.undefinedlabs.com/scope was verified as official when first introduced to the cask
   url "https://releases.undefinedlabs.com/scope/local/mac/#{version}/Scope.zip"
