e07c5f19e659da10edb75ed0c268f71b2a8952c6
CoreCode <core-code@users.noreply.github.com>
2019-12-17 14:14:48 +0100

    Update reaper.rb

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,17 +1,18 @@
 cask 'reaper' do
-  version '6.20.0,602'
+  version '6.20.0,6.02'
 
   if MacOS.version <= :mojave
     sha256 '05f5eedf73ece7f928c32aa7bdc0bfd799503f3f9fba5a2ed990b6ee4237e91e'
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma}_x86_64.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
   else
     sha256 'b85b2ccd3f95faf5b91fb2fc572114d25ce246a12ef4775dba25090c308ecb2d'
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma}_x86_64_catalina.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
   end
 
-  appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64'
+  appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64',
+          configuration: version.after_comma
   name 'REAPER'
   homepage 'https://www.reaper.fm/'
 
