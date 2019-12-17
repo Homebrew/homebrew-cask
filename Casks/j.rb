02855ed8a8c104dd105dd6a28705296ea4e9c130
Danielle McLean <dani@00dani.me>
2019-12-17 11:25:52 +1100

    Update J formula since the installation procedure changed drastically again in J901

--- Casks/j.rb
+++ Casks/j.rb
@@ -8,34 +8,35 @@ cask 'j' do
 
   apps = ['jbrk', 'jcon', 'jhs', 'jqt']
   apps.each do |a|
-    app "j64-#{version}/#{a}.app"
+    app "j#{version}/#{a}.app"
   end
 
+  installer script: "j#{version}/macos-fix.command"
   installer script: {
-                      executable: "j64-#{version}/bin/jconsole",
-                      args:       ['-js', "exit install'qtide'"],
+                      executable: "j#{version}/bin/jconsole",
+                      args:       ['-js', "load 'pacman'", "'install' jpkg '*'", 'exit 0'],
                     }
 
   # target names according to readme.txt
   ['jcon', 'jconsole'].each do |b|
-    binary "j64-#{version}/bin/jconsole", target: b
+    binary "j#{version}/bin/jconsole", target: b
   end
   commands = ['jbrk', 'jhs', 'jqt']
   commands.each do |b|
-    binary "j64-#{version}/bin/#{b}.command", target: b
+    binary "j#{version}/bin/#{b}.command", target: b
   end
 
   postflight do
     # Use `readlink` to get full path of symlinked commands.
     commands.each do |c|
-      command = "#{staged_path}/j64-#{version}/bin/#{c}.command"
+      command = "#{staged_path}/j#{version}/bin/#{c}.command"
       IO.write command, IO.read(command).gsub('$0', '$(/usr/bin/readlink "$0" || /bin/echo "$0")')
     end
 
     # Fix relative paths inside App bundles.
     apps.each do |a|
       apprun = "#{appdir}/#{a}.app/Contents/MacOS/apprun"
-      IO.write apprun, IO.read(apprun).gsub(%r{`dirname "\$0"`.*?/bin}, "#{staged_path}/j64-#{version}/bin")
+      IO.write apprun, IO.read(apprun).gsub(%r{`dirname "\$0"`.*?/bin}, "#{staged_path}/j#{version}/bin")
     end
   end
 
