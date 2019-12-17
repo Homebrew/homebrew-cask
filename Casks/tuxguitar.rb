14421785931378b64da1d5ff83d3568e7411efc6
Alexey V. Abashkin <aabashkin@mirantis.com>
2019-12-17 10:41:37 +0300

    Update tuxguitar SHA
    Change SHA256 according to version 1.5.3

--- Casks/tuxguitar.rb
+++ Casks/tuxguitar.rb
@@ -1,6 +1,6 @@
 cask 'tuxguitar' do
   version '1.5.3'
-  sha256 '3a25eb11df03904ae5c3bcb21e458c90ec4964cb46f0b1556457ec08800ea1d6'
+  sha256 '806f16487fa6f8329db53698d588662fcb208fc4e8fdf4c7d04e2d724e7b9870'
 
   url "https://downloads.sourceforge.net/tuxguitar/tuxguitar-#{version}-macosx-cocoa-64.app.tar.gz"
   appcast 'https://sourceforge.net/projects/tuxguitar/rss?path=/TuxGuitar'
