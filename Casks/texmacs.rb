751164e9b04710f3e55bf938f482b785fa7d1503
Randall <ran.dall@icloud.com>
2019-12-17 18:37:14 -0800

    Update texmacs from 1.99.11 to 1.99.12

--- Casks/texmacs.rb
+++ Casks/texmacs.rb
@@ -1,6 +1,6 @@
 cask 'texmacs' do
-  version '1.99.11'
-  sha256 '7ebadb569ef05cddb853aa3b70cd354b3a969b30f72914de170133924696cec4'
+  version '1.99.12'
+  sha256 'be1ea063443d2e2ee5b599350276f0ff152d284279fc1a9c91665ac120b377d6'
 
   url "https://www.texmacs.org/Download/ftp/tmftp/macos/TeXmacs-#{version}.dmg"
   appcast 'https://www.texmacs.org/tmweb/download/macosx.en.html'
