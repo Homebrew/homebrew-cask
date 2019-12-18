d3a94c22011ba03247a92481d5e4d5a759a57020
Fernando Miguel <github@FernandoMiguel.net>
2019-12-18 10:48:58 +0000

    escape url

--- Casks/marshallofsound-google-play-music-player.rb
+++ Casks/marshallofsound-google-play-music-player.rb
@@ -5,7 +5,7 @@ cask 'marshallofsound-google-play-music-player' do
   # github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL- was verified as official when first introduced to the cask
   ## original URL before using Circle CI builder URL
   # url "https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v#{version}/Google.Play.Music.Desktop.Player.OSX.zip"	
-  url "https://3744-40008106-gh.circle-artifacts.com/0/Users/distiller/project/dist/installers/darwin/Google Play Music Desktop Player.zip"
+  url "https://3744-40008106-gh.circle-artifacts.com/0/Users/distiller/project/dist/installers/darwin/Google%20Play%20Music%20Desktop%20Player.zip"
   appcast 'https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases.atom'
   name 'Google Play Music Desktop Player'
   homepage 'https://www.googleplaymusicdesktopplayer.com/'
