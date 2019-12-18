e11faadbd494c486b792efe98848c214f3bed69e
Randall <ran.dall@icloud.com>
2019-12-17 18:27:09 -0800

    Update lrtimelapse from 5.3.2 to 5.3.3

--- Casks/lrtimelapse.rb
+++ Casks/lrtimelapse.rb
@@ -1,6 +1,6 @@
 cask 'lrtimelapse' do
-  version '5.3.2'
-  sha256 'dabbad9d6bb1c0bd6e934a10f20bea0212289bef16320a171564465bee43ab10'
+  version '5.3.3'
+  sha256 '9c0550a1ad18a884ace37844509777a0178da7d3911c5cfe989f8c7d29868709'
 
   url "https://lrtimelapse.com/files/lrtimelapse-#{version.major}-mac/"
   appcast 'https://lrtimelapse.com/download/'
