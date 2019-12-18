59a4c0159e7d91d4f085b77e895fc25fb27ba4a1
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-17 18:07:39 -0800

    Update decrediton from 1.4.0 to 1.5.0 (#74360)

--- Casks/decrediton.rb
+++ Casks/decrediton.rb
@@ -1,6 +1,6 @@
 cask 'decrediton' do
-  version '1.4.0'
-  sha256 '990cb6b61999be5d1100581cc84bb95f4e9e5be8a5330c5e6c8940c34fe78b19'
+  version '1.5.0'
+  sha256 '10bf6ecbba2e5c51624c1b4ef1c61dd2465c02e5241d4fc9ce810567fe97c005'
 
   url "https://github.com/decred/decred-binaries/releases/download/v#{version}/decrediton-v#{version}.dmg"
   appcast 'https://github.com/decred/decred-binaries/releases.atom'
