cf637d8086aa0cf7d5c3c9f01078b473e00c5673
Florian Floimair <ffloimair@users.noreply.github.com>
2019-12-18 23:17:55 +0100

    Update clover-configurator from 5.9.0.0 to 5.9.0.1 (#74430)

--- Casks/clover-configurator.rb
+++ Casks/clover-configurator.rb
@@ -1,6 +1,6 @@
 cask 'clover-configurator' do
-  version '5.9.0.0'
-  sha256 'c2ef475bfb15dcca17daa5171b95bf220bfc971c02e3c8e41a694c4596b918fb'
+  version '5.9.0.1'
+  sha256 'b55f56b29baf669ed0284ebf302e73228f29dafad81cd71eb6013c05a97827ad'
 
   url 'https://mackie100projects.altervista.org/apps/cloverconf/download-new-build.php?version=global'
   appcast 'https://mackie100projects.altervista.org/apps/cloverconf/CCG/update-data-builds.xml'
