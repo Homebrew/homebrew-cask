2302d3ceb637a0dcce865428369814d4b5244a7c
Fernando Miguel <github@FernandoMiguel.net>
2019-12-18 10:34:24 +0000

    use CI build to fix issue 3660
    https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/issues/3660

--- Casks/marshallofsound-google-play-music-player.rb
+++ Casks/marshallofsound-google-play-music-player.rb
@@ -1,9 +1,9 @@
 cask 'marshallofsound-google-play-music-player' do
-  version '4.6.1'
-  sha256 '14bbaf4f81c302b787b36c1d0fdc44a990dcb73dd987e8b5fd0f259283a1fa2e'
+  version '4.6.1-ci-_build_3744'
+  sha256 '597a704d7058ad5f3c4f274246d7a1bf1714ce5a78daf88696746778f88f8a93'
 
   # github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL- was verified as official when first introduced to the cask
-  url "https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v#{version}/Google.Play.Music.Desktop.Player.OSX.zip"
+  url "https://3744-40008106-gh.circle-artifacts.com/0/Users/distiller/project/dist/installers/darwin/Google%20Play%20Music%20Desktop%20Player.zip"
   appcast 'https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases.atom'
   name 'Google Play Music Desktop Player'
   homepage 'https://www.googleplaymusicdesktopplayer.com/'
