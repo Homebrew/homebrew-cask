4a8c057b12266ecb8b77763429252bc475fbcb80
Vítor Galvão <info@vitorgalvao.com>
2019-12-19 15:31:35 +0000

    Update monal.rb

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,10 +1,10 @@
 cask 'monal' do
-  version '2.5b3'
+  version '2.5 beta 3,155'
   sha256 '891b9c9c46cc1069970701940a37f9769fb15943ce4ae1aaaca46fc4b35556c0'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
   appcast 'https://monal.im/Monal-OSX/appcast.xml',
-          configuration: version.major_minor
+          configuration: version.after_comma
   name 'Monal'
   homepage 'https://monal.im/'
 
