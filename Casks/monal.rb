0851bf828a4fbb4a68e583ad4a6d6a266971d046
Vítor Galvão <info@vitorgalvao.com>
2019-12-19 16:24:45 +0000

    Update monal.rb

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,5 +1,5 @@
 cask 'monal' do
-  version '2.5 beta 3,155'
+  version '2.5 beta 4,156'
   sha256 'ec4e2afb9620d26bb2b597ab8f6cc6051eabd6453634c13cdaf9afecf6b70a35'
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
