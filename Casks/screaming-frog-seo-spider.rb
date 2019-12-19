8598d8897ec007a641c5424400f17169e0ef988d
Randall <ran.dall@icloud.com>
2019-12-18 19:48:52 -0800

    Update screaming-frog-seo-spider from 12.3 to 12.4

--- Casks/screaming-frog-seo-spider.rb
+++ Casks/screaming-frog-seo-spider.rb
@@ -1,6 +1,6 @@
 cask 'screaming-frog-seo-spider' do
-  version '12.3'
-  sha256 '446e907954a7abfaeb341b57a6aacc57f0b416fe2c4db3ee13abb1ab52a47f50'
+  version '12.4'
+  sha256 '0a00bb6fca07687a5d61d98f8cbbe394378c7cea22515d33a7aab68f3db059d2'
 
   url "https://download.screamingfrog.co.uk/products/seo-spider/ScreamingFrogSEOSpider-#{version}.dmg"
   appcast 'https://www.screamingfrog.co.uk/wp-content/themes/screamingfrog/inc/download-modal.php'
