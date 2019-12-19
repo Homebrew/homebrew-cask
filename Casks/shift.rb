76ceb90551f7014c57d85f86c288ac4da9a9dda2
Randall <ran.dall@icloud.com>
2019-12-18 19:43:28 -0800

    Update shift from 4.0.6 to 4.0.9

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,6 +1,6 @@
 cask 'shift' do
-  version '4.0.6'
-  sha256 '32b6a5875f784826d800b0b93dc5a3024cfa5f105085842aff02eeb8e85d2846'
+  version '4.0.9'
+  sha256 '54cd7b140bc1b3a4522c380a1f177c3c2091019f5ca90ef65be8d483561b6d07'
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
   appcast 'https://tryshift.com/download/?platform=mac'
