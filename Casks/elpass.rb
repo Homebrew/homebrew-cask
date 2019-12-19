30f0b2b99907b4155af27309f8de6f164ba2117f
Vítor Galvão <info@vitorgalvao.com>
2019-12-19 15:25:59 +0000

    Update elpass.rb

--- Casks/elpass.rb
+++ Casks/elpass.rb
@@ -1,8 +1,9 @@
 cask 'elpass' do
-  version :latest
-  sha256 :no_check
+  version '1.0.4,164'
+  sha256 'e33b2ef4a18c2e8c4a285ffaf865d64ca1c8a8f456754481423554799c21b797'
 
-  url 'https://elpass.app/macos/Elpass-latest.zip'
+  url "https://elpass.app/macos/Elpass-#{version.before_comma}-#{version.after_comma}.zip"
+  appcast 'https://elpass.app/macos/appcast.xml'
   name 'Elpass'
   homepage 'https://elpass.app/'
 
