d1abe8c7e7b9b5af0dcf8cf7d70f4f5d10e40e3d
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-18 19:06:25 -0800

    Update obs from 24.0.5 to 24.0.6 (#74455)

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -1,6 +1,6 @@
 cask 'obs' do
-  version '24.0.5'
-  sha256 'bcdfe23ac20ab760456ec74c44f123cf633233cbbf07675e6a16d6a1ddcc9ce1'
+  version '24.0.6'
+  sha256 '7cb521f4eaeebf5807991d22604fd08af146728d1df7a1f4f6c38230f2f32a0d'
 
   url "https://cdn-fastly.obsproject.com/downloads/obs-mac-#{version}.dmg"
   appcast 'https://github.com/obsproject/obs-studio/releases.atom'
