87ef398501d9277894892c72131abd101d8f50cc
tomsihap <sihapanya.thomas@gmail.com>
2019-12-20 16:33:31 +0100

    Update gitkraken from 6.4.0 to 6.4.1 (#74558)

--- Casks/gitkraken.rb
+++ Casks/gitkraken.rb
@@ -1,6 +1,6 @@
 cask 'gitkraken' do
-  version '6.4.0'
-  sha256 '1c89e66b550f16dbedb23f32494b48305ef2666387056b2e5d222f99b210a244'
+  version '6.4.1'
+  sha256 'a669c01467f9fbf01e9fb9e4bbb3e93a06eeae5c9b702ada4d46879acd279476'
 
   # release.axocdn.com/darwin was verified as official when first introduced to the cask
   url 'https://release.axocdn.com/darwin/installGitKraken.dmg'
