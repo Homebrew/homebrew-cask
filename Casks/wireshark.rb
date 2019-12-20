780b491fb20b02f00f604d034ddf1a61a93c2d54
Gerald Combs <gerald@wireshark.org>
2019-12-19 16:15:41 -0800

    Update Wireshark 3.2.0 to use wireshark.org's appcast URL. (#74433)
    * Update Wireshark to use wireshark.org's appcast URL.
    
    Wireshark 3.2.0 adds automatic updates using Sparkle. Switch to the
    macOS appcast URL on www.wireshark.org and set auto_updates to true.
    
    * Update wireshark.rb
    
    * Update wireshark.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -3,10 +3,11 @@ cask 'wireshark' do
   sha256 'a1f5b86d3ae8a2be38db218d07ad1d123315433292adae71a9756bbc881438ee'
 
   url "https://1.na.dl.wireshark.org/osx/all-versions/Wireshark%20#{version}%20Intel%2064.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://www.wireshark.org/download.html&splitter_1=stable&index_1=1&splitter_2=development&index_2=0'
+  appcast 'https://www.wireshark.org/update/0/Wireshark/0.0.0/macOS/x86-64/en-US/stable.xml'
   name 'Wireshark'
   homepage 'https://www.wireshark.org/'
 
+  auto_updates true
   conflicts_with cask: 'wireshark-chmodbpf'
   depends_on macos: '>= :sierra'
 
