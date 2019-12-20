59f72742d611fbb89015421a8ede8def3c6f599d
suschizu <brigitte@corecode.io>
2019-12-20 05:08:02 +0100

    Update hotswitch from 1.19 to 1.20 (#74511)
    * Update hotswitch from 1.19 to 1.20
    
    * Update hotswitch.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/hotswitch.rb
+++ Casks/hotswitch.rb
@@ -1,11 +1,11 @@
 cask 'hotswitch' do
-  version '1.19'
-  sha256 '4e341d025a1951270fcd0ee363f821c5a026b9eb5a1d592e25a955afe497b256'
+  version '1.20'
+  sha256 '55696b292a79f603a4b14c8272232ea30d13b78b4974fd135b9d0a0dd6331be6'
 
   url 'https://oniatsu.github.io/HotSwitch/release/zip/HotSwitch.zip'
   appcast 'https://github.com/oniatsu/HotSwitch/releases.atom'
   name 'HotSwitch'
   homepage 'https://oniatsu.github.io/HotSwitch/'
 
-  app 'Users/ao/Downloads/HotSwitch 2019-11-22 08-52-20/HotSwitch.app'
+  app 'HotSwitch.app'
 end
