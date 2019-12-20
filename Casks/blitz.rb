39907935d1d8e171e078691f914d79e7ddc50b6e
Marcel Dold <marcel.dold@icloud.com>
2019-12-21 00:11:30 +0100

    Update blitz from 1.6.0 to 1.6.1

--- Casks/blitz.rb
+++ Casks/blitz.rb
@@ -1,6 +1,6 @@
 cask 'blitz' do
-  version '1.6.0'
-  sha256 '3628baeb92ec6c524ba82896b46b145fe9501dbf63fd10948094a271cb3c5144'
+  version '1.6.1'
+  sha256 '0d4bd856a5b6e2ddc37892c6cf3305400497cb83ef16f51edd974d199fafd3b1'
 
   url "https://dl.blitz.gg/download/Blitz-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://dl.blitz.gg/download/mac'
