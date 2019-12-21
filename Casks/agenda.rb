7ba8757b8f7a414dac318a812fbb0d2feb90a619
Randall <ran.dall@icloud.com>
2019-12-20 20:43:55 -0800

    Update agenda from 9.1.1 to 9.1.2

--- Casks/agenda.rb
+++ Casks/agenda.rb
@@ -1,6 +1,6 @@
 cask 'agenda' do
-  version '9.1.1'
-  sha256 'f2b78d81746f2a45031bc980be46ca9d7c048b11ae608dc8d76f83f0b01304ba'
+  version '9.1.2'
+  sha256 'b1c636d546b5b5927bed06e381bae273edc21cfb662e5b207378b7576c9845a2'
 
   url "https://downloads.agenda.com/Agenda_#{version}.zip"
   appcast 'https://downloads.agenda.com/AgendaSparkleAppcast.xml'
