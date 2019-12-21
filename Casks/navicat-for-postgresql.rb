caf78a65272ad46da1ecea8942e79f88c7d19f09
Randall <ran.dall@icloud.com>
2019-12-20 20:35:36 -0800

    Update navicat-for-postgresql from 15.0.5 to 15.0.6

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,5 +1,5 @@
 cask 'navicat-for-postgresql' do
-  version '15.0.5'
+  version '15.0.6'
   sha256 'cf7f620f19ea603fc74913a6b28176221df30d534818c8ec4e10f00ec5db9feb'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
