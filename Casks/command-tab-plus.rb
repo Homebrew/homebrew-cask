6db2ea605eb18e482df40e8735531986c11889b4
suschizu <brigitte@corecode.io>
2019-12-21 18:05:05 +0100

    Update command-tab-plus.rb

--- Casks/command-tab-plus.rb
+++ Casks/command-tab-plus.rb
@@ -1,5 +1,5 @@
 cask 'command-tab-plus' do
-  version '359,359:1576772358'
+  version '1.109,359:1576772358'
   sha256 '608458aa55d699f6c1d21a6bc834365d0787174b4f34b266dad8f267acfdc328'
 
   # dl.devmate.com/com.sergey-gerasimenko.Command-Tab was verified as official when first introduced to the cask
