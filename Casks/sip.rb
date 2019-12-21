187ce697f925780298e248f6efdec5445df18e93
Randall <ran.dall@icloud.com>
2019-12-20 20:47:14 -0800

    Update sip from 2.2.2 to 2.2.3

--- Casks/sip.rb
+++ Casks/sip.rb
@@ -3,8 +3,8 @@ cask 'sip' do
     version '1.1.6'
     sha256 'bb170a54090aab5703388a3e7a22e9cf4e4d98e84f5658893e1e6f9677b9a51e'
   else
-    version '2.2.2'
-    sha256 '960c30ebc3a1c8ddc7e1ac5f6b73e41fe4fd3c22906ba5b489bf64b601890d77'
+    version '2.2.3'
+    sha256 'f7b4e5c1c7922fcee4a7e3be874d0e0bc53076bc02aac2b5b3c7e3f5c830c78f'
   end
 
   url "https://sipapp.io/updates/v#{version.major}/sip-#{version}.zip"
