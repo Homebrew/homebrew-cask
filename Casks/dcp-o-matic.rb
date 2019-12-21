21c38586191d09c3e20ae93007d8c2601856dfa8
Randall <ran.dall@icloud.com>
2019-12-20 21:03:38 -0800

    Update dcp-o-matic from 2.14.18 to 2.14.19

--- Casks/dcp-o-matic.rb
+++ Casks/dcp-o-matic.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic' do
-  version '2.14.18'
-  sha256 'e0ec579844f74d0d66f6a02b5bd9dbf7d7788663a460ae4fb777ccdd1ebfc888'
+  version '2.14.19'
+  sha256 'c27bed5ded06c4ea6031f361d167a9073923e68815d4c45bb0671f870f524e1d'
 
   url "https://dcpomatic.com/dl.php?id=osx-main&version=#{version}"
   appcast 'https://dcpomatic.com/download'
