48413b7e71d5aece9f624126e80ba2db195d81e2
suschizu <brigitte@corecode.io>
2019-12-21 01:07:33 +0100

    Update tiger-trade from 5.1.1,20191108:A91865 to 5.3.0,20191218:B9DE81 (#74562)

--- Casks/tiger-trade.rb
+++ Casks/tiger-trade.rb
@@ -1,6 +1,6 @@
 cask 'tiger-trade' do
-  version '5.1.1,20191108:A91865'
-  sha256 '6e110ecff8c9239847c9be6ef13365da1c5b7c7a0dbf6863e952c040d07edaff'
+  version '5.3.0,20191218:B9DE81'
+  sha256 '1d990eb94ddddbf696cb3933f11ec7386646b3f327d4173216275078a71c2d85'
 
   # s.tigerfintech.com was verified as official when first introduced to the cask
   url "https://s.tigerfintech.com/desktop/cdn/f/TigerTrade_#{version.before_comma}_#{version.after_comma.before_colon}_#{version.after_colon}.dmg"
