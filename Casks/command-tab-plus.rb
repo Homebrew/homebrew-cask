3c9ed0e61ea2c8c43f1b40ffc4e00811a97e4f6f
suschizu <brigitte@corecode.io>
2019-12-22 00:40:27 +0100

    Correction of the version notation in command-tab-plus.rb from 359,359… to 1.109,359… (#74631)

--- Casks/command-tab-plus.rb
+++ Casks/command-tab-plus.rb
@@ -1,10 +1,11 @@
 cask 'command-tab-plus' do
-  version '359,359:1576772358'
+  version '1.109,359:1576772358'
   sha256 '608458aa55d699f6c1d21a6bc834365d0787174b4f34b266dad8f267acfdc328'
 
   # dl.devmate.com/com.sergey-gerasimenko.Command-Tab was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.sergey-gerasimenko.Command-Tab/#{version.after_comma.before_colon}/#{version.after_colon}/Command-Tab-#{version.after_comma.before_colon}.zip"
-  appcast 'https://updates.devmate.com/com.sergey-gerasimenko.Command-Tab.xml'
+  appcast 'https://updates.devmate.com/com.sergey-gerasimenko.Command-Tab.xml',
+          configuration: version.after_comma.before_colon
   name 'Command-Tab Plus'
   homepage 'http://commandtab.noteifyapp.com/'
 
