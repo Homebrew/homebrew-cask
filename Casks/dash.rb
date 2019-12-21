3ad4170d20a9dc2b6209ee46c8ed47a24318bc9d
Bogdan Popescu <support@kapeli.com>
2019-12-21 11:19:35 +0200

    Update dash to 5.0.3 (#74615)

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -4,7 +4,7 @@ cask 'dash' do
     sha256 'e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6'
   else
     version '5.0.3'
-    sha256 '7784269c897169752007fe5f8e3fbf27cb4e15255603289eee46ff6b60ce7f66'
+    sha256 '1ae4b0d4c5e750d3b1cfe91f7168ebb1800d3c0a9a169da5098de0d0b92c6238'
   end
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
