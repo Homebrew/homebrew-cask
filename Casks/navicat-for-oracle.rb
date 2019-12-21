c304431ba64ce20ff15d4c9a8db074ab782c4ad0
Randall <ran.dall@icloud.com>
2019-12-20 20:46:46 -0800

    Update navicat-for-oracle from 15.0.5 to 15.0.6

--- Casks/navicat-for-oracle.rb
+++ Casks/navicat-for-oracle.rb
@@ -1,5 +1,5 @@
 cask 'navicat-for-oracle' do
-  version '15.0.5'
+  version '15.0.6'
   sha256 '090daee3d79a6e3162d475ae2b4739359a1d31c85556d7891b524fb9247296f5'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_ora_en.dmg"
