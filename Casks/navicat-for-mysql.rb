ca4a1f8c9a6bcb23f1f58f6f16ce9453b9956f54
Randall <ran.dall@icloud.com>
2019-12-20 20:41:46 -0800

    Update navicat-for-mysql from 15.0.5 to 15.0.6

--- Casks/navicat-for-mysql.rb
+++ Casks/navicat-for-mysql.rb
@@ -1,5 +1,5 @@
 cask 'navicat-for-mysql' do
-  version '15.0.5'
+  version '15.0.6'
   sha256 '51a1295ebdeee3e1d422ecdf5b08f6870841ce78170cf6bcc861bed2fa028c72'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
