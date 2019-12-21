3295330c4dbd89491a4b1d22c4c7cf352617f5be
Randall <ran.dall@icloud.com>
2019-12-20 20:44:48 -0800

    Update navicat-for-mariadb from 15.0.5 to 15.0.6

--- Casks/navicat-for-mariadb.rb
+++ Casks/navicat-for-mariadb.rb
@@ -1,5 +1,5 @@
 cask 'navicat-for-mariadb' do
-  version '15.0.5'
+  version '15.0.6'
   sha256 '99c780b70f088b97e8d9c3f6e3a7fa0519461ba53a73a4237988f3ac20298825'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mariadb_en.dmg"
