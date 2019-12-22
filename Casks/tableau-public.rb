908c02d45f65ef30bfa19903d7de4b9b4197b123
Hannes Braun <hannesbraun@mail.de>
2019-12-22 13:01:11 +0100

    Update tableau-public from 2019.4.0 to 2019.4.1

--- Casks/tableau-public.rb
+++ Casks/tableau-public.rb
@@ -1,6 +1,6 @@
 cask 'tableau-public' do
-  version '2019.4.0'
-  sha256 'a9720cad769e8c5d2d592b7d4cf37f8b8290ebaa180e856cdfa86e93ef6d137b'
+  version '2019.4.1'
+  sha256 '739b6ed5c7ac506c501136a217147edb9ba51d931e1b8c8836c2ed01e2aa8428'
 
   url "https://downloads.tableau.com/public/TableauPublic-#{version.dots_to_hyphens}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.tableau.com/downloads/public/mac',
