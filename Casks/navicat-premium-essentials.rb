7d04428109ff025b5183c2fb86abff68949bd27b
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-21 18:52:59 -0800

    Update navicat-premium-essentials from 15.0.4 to 10.0.5 (#74525)
    * Update navicat-premium-essentials from 15.0.4 to 15.0.6
    
    * Update navicat-premium-essentials.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/navicat-premium-essentials.rb
+++ Casks/navicat-premium-essentials.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium-essentials' do
-  version '15.0.4'
-  sha256 'eaea2d9bf21ecf14be3efcb86fb939051ce3689fa6fb4eed293322c1e7f45ba4'
+  version '15.0.5'
+  sha256 'a8c96d313895af3a95c3e8cc979cf40a2b470608ead149b7aa995df3ef9aac2b'
 
   url "http://download.navicat.com/download/navicatess#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium%20Essentials&appLang=en'
