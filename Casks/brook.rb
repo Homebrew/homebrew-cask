99ee4821889ec7ebdbef3bad2588a6f94da90e32
brigitte zmoelnig <brigitte@corecode.io>
2019-12-22 19:54:01 +0100

    Update brook from 20200101 to 20200102

--- Casks/brook.rb
+++ Casks/brook.rb
@@ -1,5 +1,5 @@
 cask 'brook' do
-  version '20200101'
+  version '20200102'
   sha256 'ee4fbeba88fa57bb0702612ba32e8e5d04036eaad3740f58c38788406b692a47'
 
   url "https://github.com/txthinking/brook/releases/download/v#{version}/Brook.pkg"
