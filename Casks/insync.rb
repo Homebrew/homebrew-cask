ff383d3067ec95128a3f6bef3c7f289f625e7e6f
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-21 20:52:43 -0800

    Update insync from 3.0.25.40643 to 3.0.27.40677 (#74663)

--- Casks/insync.rb
+++ Casks/insync.rb
@@ -1,6 +1,6 @@
 cask 'insync' do
-  version '3.0.25.40643'
-  sha256 'cdf8d3af022e103620fb6e19565bd2c63b3f306031b008fe62230e908ea6d4ce'
+  version '3.0.27.40677'
+  sha256 '0a0b4d8b701330cd51d5f813e5447b09c1fb247b9a1355bcc3aeb34a5ec178b9'
 
   url "http://s.insynchq.com/builds/Insync-#{version}.dmg"
   appcast 'https://www.insynchq.com/downloads?start=true'
