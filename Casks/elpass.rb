c217ef8bf72f44a1f34e172c6881267622a7c212
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2019-12-23 08:35:45 +0100

    Update elpass from 1.0.5,172 to 1.0.5,173 (#74704)

--- Casks/elpass.rb
+++ Casks/elpass.rb
@@ -1,6 +1,6 @@
 cask 'elpass' do
-  version '1.0.5,172'
-  sha256 '42c37a847585ba5d394edf96ba027f019e0d8477dc058985e82a358158e2fcf0'
+  version '1.0.5,173'
+  sha256 '250a1aed16cf07de1394bf51d1cb2ec4c6bd0366106180f467095dc2d98d063d'
 
   url "https://elpass.app/macos/Elpass-#{version.before_comma}-#{version.after_comma}.zip"
   appcast 'https://elpass.app/macos/appcast.xml'
