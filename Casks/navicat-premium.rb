e50839404b3929424cd6bfb71e33dbc2ef89d56b
Tomáš Holý <holy@interconnect.cz>
2019-12-23 15:21:29 +0100

    Update navicat-premium from 15.0.5 to 15.0.6

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium' do
-  version '15.0.5'
-  sha256 '8461c26958c7bb7ca1f9ce4715b4f1fd852b46208b9e988a54b3559796e8362e'
+  version '15.0.6'
+  sha256 '7973d89c14f0ff48ffdc1afdcfd21deb5fe132a76a993181d6a7b4bbebb3f302'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en'
