df06d9c3bf04cd485ce055ecd41c112a08906d42
Randall <ran.dall@icloud.com>
2019-12-22 19:52:18 -0800

    Update sizzy from 0.12 to 0.13.1

--- Casks/sizzy.rb
+++ Casks/sizzy.rb
@@ -1,6 +1,6 @@
 cask 'sizzy' do
-  version '0.12'
-  sha256 '2bd63371a35acba0e6abd07df9a073a5a4c1e7841656fa5be0dce8ebc315a007'
+  version '0.13.1'
+  sha256 'f3a8f0226d22c596cf42e5b312d732744f09936c668af2c8fd1a85bbff50ad4f'
 
   url 'https://sizzy.co/get-app'
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sizzy.co/get-app&user_agent=Intel%20Mac%20OS%20X'
