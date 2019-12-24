8dc2fa6a7c9103a91696afd4825ef379008c2eed
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-23 19:03:42 -0800

    Update bitwig-studio from 3.1 to 3.1.1 (#74758)

--- Casks/bitwig-studio.rb
+++ Casks/bitwig-studio.rb
@@ -1,6 +1,6 @@
 cask 'bitwig-studio' do
-  version '3.1'
-  sha256 'a7a0e00249f2618196bee7c835f817fde68e012d1e6c9d703a705db59fcc94cf'
+  version '3.1.1'
+  sha256 'e6d0703f96b9dafdae8c05cdc2453080821d92a0732795eddcd46fba9beeb776'
 
   url "https://downloads.bitwig.com/stable/#{version}/Bitwig%20Studio%20#{version}.dmg"
   appcast 'https://www.bitwig.com/en/download.html'
