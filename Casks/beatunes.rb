68917ab6ead9ee27d0f848236d03a82067366075
Randall <ran.dall@icloud.com>
2019-12-23 18:18:56 -0800

    Update beatunes from 5.2.3 to 5.2.4

--- Casks/beatunes.rb
+++ Casks/beatunes.rb
@@ -1,6 +1,6 @@
 cask 'beatunes' do
-  version '5.2.3'
-  sha256 '8f9aa40dbfd6d41d930beef2b8b3025a48d93301f01fdfaa766de1a64ffab3d3'
+  version '5.2.4'
+  sha256 '2049db469cfead2fa7f580afa3ef67989dd48b730d9e3ea4b30d4999dafd4576'
 
   url "http://coxy.beatunes.com/download/beaTunes-#{version.dots_to_hyphens}.dmg"
   appcast 'https://www.beatunes.com/en/beatunes-download.html',
