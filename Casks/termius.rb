80988e6c336133f44d60038be2214d9fe13e34a3
Ashlars <yangzhonghao@vip.qq.com>
2019-12-24 18:23:00 +0800

    Update termius from 5.1.1 to 5.2.3

--- Casks/termius.rb
+++ Casks/termius.rb
@@ -1,6 +1,6 @@
 cask 'termius' do
-  version '5.1.1'
-  sha256 '44bcda41ef58318ff58850ef240fdfbcb0327a786220dde1b8c02a1bbb646a26'
+  version '5.2.3'
+  sha256 'b54c4c3169c1276173b05704e5a8a8477fe3787e46987f4eb51720bf97f69f46'
 
   # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/termius.desktop.autoupdate/mac/Termius.dmg'
