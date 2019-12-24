3ef50c5f5d3626f7f57508b238b7e3e7456a0487
owah <4345945+owah@users.noreply.github.com>
2019-12-24 16:05:27 +0100

    Updates ghidra from version 9.1 to 9.1.1 (#74767)
    Changes the version number as well as hash to the new release

--- Casks/ghidra.rb
+++ Casks/ghidra.rb
@@ -1,6 +1,6 @@
 cask 'ghidra' do
-  version '9.1_PUBLIC,20191023'
-  sha256 '29d130dfe85da6ec45dfbf68a344506a8fdcc7cfe7f64a3e7ffb210052d1875e'
+  version '9.1.1_PUBLIC,20191218'
+  sha256 'b0d40a4497c66011084e4a639d61ac76da4b4c5cabd62ab63adadb7293b0e506'
 
   url "https://www.ghidra-sre.org/ghidra_#{version.before_comma}_#{version.after_comma}.zip"
   name 'Ghidra'
