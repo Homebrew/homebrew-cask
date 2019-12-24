02730c541031e17ce8a36cf7c263fcb1ef3641db
brigitte zmoelnig <brigitte@corecode.io>
2019-12-24 16:26:24 +0100

    Update seadrive from 1.0.8 to 1.0.10

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -1,6 +1,6 @@
 cask 'seadrive' do
-  version '1.0.8'
-  sha256 'b1dbcddf02f96b7faa724a516ccf1e9e861dc41f03834f0d503970d37ec67f8c'
+  version '1.0.10'
+  sha256 '8ff20f7f7d79e7afe9ff2ec2dcd413e0a47958f1022387bd2731e8c5903b04a8'
 
   # download.seadrive.org was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seadrive-#{version}.dmg"
