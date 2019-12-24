5d97309f5a2ba3a78b937374c6030c92ac9e2235
Michael Tibben <michael.tibben@99designs.com>
2019-12-24 14:06:06 +1100

    Update aws-vault from 5.0.0 to 5.0.1

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,6 +1,6 @@
 cask 'aws-vault' do
-  version '5.0.0'
-  sha256 'b83182234f524e5aa901316b275fd58bc88463626f60cd71294e6a94ba23440f'
+  version '5.0.1'
+  sha256 '90b45082f1025095d64aaf637b73fedd0292276545df11492da65082ec8f288c'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
   appcast 'https://github.com/99designs/aws-vault/releases.atom'
