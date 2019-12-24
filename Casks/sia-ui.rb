3f1b48ae0f5ed4da23d832fcecc9948c3a557fc4
Randall <ran.dall@icloud.com>
2019-12-24 10:30:08 -0800

    Update sia-ui from 1.4.1.2 to 1.4.2.0

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -1,6 +1,6 @@
 cask 'sia-ui' do
-  version '1.4.1.2'
-  sha256 'ebd6d89a1012098a26f3da4338ae37a5b2194891f8176eaa1cb3cc5b678410e1'
+  version '1.4.2.0'
+  sha256 '955d3fc95e3fe88baf25cf211263444beeea2efe5fa23489261378c84c888b74'
 
   url "https://sia.tech/releases/Sia-UI-v#{version}.dmg"
   appcast 'https://gitlab.com/api/v4/projects/7508674/releases'
