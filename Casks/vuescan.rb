b40a596900e84766c703aa45a1b4e48e26815b32
Lennart Lövstrand <lennart.github@lovstrand.com>
2019-12-23 17:48:33 -0800

    Update vuescan from 9.7.13 to 9.7.14 (#74732)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
-  version '9.7.13'
-  sha256 '77e024e4b5737744b627d3b34f53d619112d493ddf419e9752fcb0c6db51f701'
+  version '9.7.14'
+  sha256 '1a2013fe9d4af8784e7f200c783f85d0b699e8fe0fd74e02025529e94ac1d2e8'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
