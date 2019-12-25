51673452393ee13dd7e9c560e9bdb6b2aadb6f59
weqopy <weqopy@gmail.com>
2019-12-25 18:50:05 +0800

    Update mweb.rb (#74803)

--- Casks/mweb.rb
+++ Casks/mweb.rb
@@ -1,6 +1,6 @@
 cask 'mweb' do
   version '3.3.4'
-  sha256 '6f2a8df5d17b58a95ce40a7484d5bd3c2a0135612af2d848734b661cb72fe681'
+  sha256 '1ffe9c2df906276d4a76894e86133abf9546ed6b8a2bb8e1be34b0b0fdb33aef'
 
   # dl.devmate.com/com.coderforart.MWeb3 was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coderforart.MWeb3/MWeb3-ProMarkdownwriting,notetakingandstaticbloggeneratorApp.dmg'
