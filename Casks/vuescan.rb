e4062093abc92b29a25492a191791a399a88903f
Leon Moonen <leon.moonen@computer.org>
2019-12-25 13:19:32 +0100

    Update vuescan (#74807)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.7.14'
-  sha256 '1a2013fe9d4af8784e7f200c783f85d0b699e8fe0fd74e02025529e94ac1d2e8'
+  sha256 '6bf1224b0dcd983fbbe058206dd2886f55d1f175eb6ae26c593d3306504f834d'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
