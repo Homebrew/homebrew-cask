e93385af83287c1fa9873c866820736a4cb728f9
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-25 11:34:39 -0800

    Update mp3gain-express from 2.3.2 to 2.3.3 (#74814)

--- Casks/mp3gain-express.rb
+++ Casks/mp3gain-express.rb
@@ -1,6 +1,6 @@
 cask 'mp3gain-express' do
-  version '2.3.2'
-  sha256 'ae217c3a6e6106a8bb8dab91b2526b551bd395bc677164b2461f267b36b8ba99'
+  version '2.3.3'
+  sha256 'c96da91165873e4adce960aac50bd78f787c0d8c482fe08829a13f3362e21fd3'
 
   url "https://projects.sappharad.com/mp3gain/mp3gain_mac#{version.no_dots}.zip"
   appcast 'https://projects.sappharad.com/mp3gain/updates.xml'
