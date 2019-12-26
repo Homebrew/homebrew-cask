7af98d79905cac3db4d1fdf7d032068cb215f3cd
brigitte zmoelnig <brigitte@corecode.io>
2019-12-26 18:27:52 +0100

    Update love

--- Casks/love.rb
+++ Casks/love.rb
@@ -1,6 +1,6 @@
 cask 'love' do
   version '11.3'
-  sha256 '65b0cf09e6be1a63251ec212ebf54c748e0115692d8bd1d116297279449a1ae5'
+  sha256 '0e18d81acdbeab0c93fb20760d1fe3334209bf911572c29d9bde0beb3160161a'
 
   # bitbucket.org/rude/love was verified as official when first introduced to the cask
   url "https://bitbucket.org/rude/love/downloads/love-#{version}-macos.zip"
