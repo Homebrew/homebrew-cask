4deb3dcc4525727e5ffe011aa7a124f421ea4d56
suschizu <brigitte@corecode.io>
2019-12-26 20:35:05 +0100

    Update capture-one.rb

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -1,13 +1,13 @@
 cask 'capture-one' do
-  version '13.0.0.186,20'
+  version '13.0.1.19,20.0.1'
   sha256 '776676d221676d0a79e0f7fa5a7a8c7b772e2b84efce8719177a0ae24261e67c'
 
-  url "https://downloads.phaseone.com/374dd05f-e8c7-499d-a703-9a7dca20d5b7/International/CaptureOne#{version.after_comma}.Mac.#{version.after_comma}.0.0.dmg"
+  url "https://downloads.phaseone.com/a9562498-f874-404e-842d-9f5cc8a05efd/International/CaptureOne#{version.after_comma.major}.Mac.#{version.after_comma}.dmg"
   appcast "https://cormws.phaseone.com/corm.asmx/GetNewSoftwareVersion?Platform=Mac&Version=#{version.major}"
   name 'Capture One'
   homepage 'https://www.captureone.com/en/'
 
   depends_on macos: '>= :high_sierra'
 
-  app "Capture One #{version.after_comma}.app"
+  app "Capture One #{version.after_comma.major}.app"
 end
