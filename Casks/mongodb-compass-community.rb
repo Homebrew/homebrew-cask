5b01295df7d0f212118de887867c69d00d69eb0b
Randall <ran.dall@icloud.com>
2019-12-26 11:30:32 -0800

    Update mongodb-compass-community from 1.20.3 to 1.20.4

--- Casks/mongodb-compass-community.rb
+++ Casks/mongodb-compass-community.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass-community' do
-  version '1.20.3'
-  sha256 '59f70f62902ee9632c58d540ee666c29a4a929979dd700a035bb69f98af87759'
+  version '1.20.4'
+  sha256 '0364510e8c7eafb3cec39d9f86a8a14476323beb8dc7050a2d6daa51b01dafee'
 
   url "https://downloads.mongodb.com/compass/mongodb-compass-community-#{version}-darwin-x64.dmg"
   appcast 'https://www.mongodb.com/download-center/compass'
