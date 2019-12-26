de6639d0e81cb083cb7e3a4d0142c2561b7c1d09
Randall <17261190+ran-dall@users.noreply.github.com>
2019-12-26 11:18:15 -0800

    Update downie from 3.9.3,3010 to 3.9.4,3015 (#74846)

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -1,6 +1,6 @@
 cask 'downie' do
-  version '3.9.3,3010'
-  sha256 '9170d76fa0b6a8e1b01b445b0705d68cd2714ca4e03b4ab6dfa865c88fe61172'
+  version '3.9.4,3015'
+  sha256 '35b2b7e6b4ff6d1faed90c0201105a676b58a35708a0d5cef61f5ddafc3d79a3'
 
   # charliemonroesoftware.com was verified as official when first introduced to the cask
   url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
