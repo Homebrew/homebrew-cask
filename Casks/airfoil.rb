9ce23898123776b7835f3bfb651cb2b79dbbdbf1
Jason N <me@sibicle.com>
2019-12-27 15:00:32 -0700

    Update airfoil

--- Casks/airfoil.rb
+++ Casks/airfoil.rb
@@ -2,7 +2,7 @@ cask 'airfoil' do
   version '5.8.8'
   sha256 'fccdcc69b4036fad8aa08c01dfc0446c44476a96b93b999278fafdf0dc07556b'
 
-  url 'https://rogueamoeba.com/airfoil/download/Airfoil.zip'
+  url 'https://rogueamoeba.com/airfoil/mac/download/Airfoil.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Airfoil&platform=osx'
   name 'Airfoil'
   homepage 'https://www.rogueamoeba.com/airfoil/mac/'
