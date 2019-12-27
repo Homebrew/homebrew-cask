a02cbfc43ad43a59472a681ede2fcd8ac9ed5e56
Hannes Braun <hannesbraun@mail.de>
2019-12-27 12:22:41 +0100

    Update futuniuniu from 9.10.564_201910142014 to 9.13.592_201912262039

--- Casks/futuniuniu.rb
+++ Casks/futuniuniu.rb
@@ -1,6 +1,6 @@
 cask 'futuniuniu' do
-  version '9.10.564_201910142014'
-  sha256 'f08e87d9a0efcafb7a4569cee387ee6323be0d03edc2cbe62750e2446856a063'
+  version '9.13.592_201912262039'
+  sha256 '882ec8985ebc8c788d8a1a9c9ab8a43e5d5a8d8021767a1250e02463ee1e6d45'
 
   # software-file-1251001049.file.myqcloud.com was verified as official when first introduced to the cask
   url "https://software-file-1251001049.file.myqcloud.com/FTNNForMac_#{version}_website.dmg"
