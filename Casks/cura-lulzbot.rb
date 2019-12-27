b035a84fd34646a97b5d8bdb9bd63fdbc9646040
brigitte zmoelnig <brigitte@corecode.io>
2019-12-27 17:56:21 +0100

    Update cura-lulzbot from 3.6.20 to 3.6.20,73b212e94766420d7130e8a298c39ce9

--- Casks/cura-lulzbot.rb
+++ Casks/cura-lulzbot.rb
@@ -1,8 +1,9 @@
 cask 'cura-lulzbot' do
-  version '3.6.20'
+  version '3.6.20,73b212e94766420d7130e8a298c39ce9'
   sha256 '454dd66f219b7a85e4fb4802ba5e23584eb95518e016668837fd5f77e3a20651'
 
-  url "https://download.lulzbot.com/Software/cura-lulzbot/mac/cura-lulzbot_#{version}.dmg"
+  # gitlab.com/lulzbot3d/cura-le/cura-lulzbot was verified as official when first introduced to the cask
+  url "https://gitlab.com/lulzbot3d/cura-le/cura-lulzbot/uploads/#{version.after_comma}/cura-lulzbot_#{version.before_comma}.dmg"
   appcast 'https://download.lulzbot.com/Software/cura-lulzbot/mac/'
   name 'Cura LulzBot Edition'
   homepage 'https://www.lulzbot.com/learn/tutorials/cura-lulzbot-edition-installation-osx'
