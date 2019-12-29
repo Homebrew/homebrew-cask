c1bb38bf088aca6baa59257cc87731843588bab9
Randall <ran.dall@icloud.com>
2019-12-29 11:56:03 -0800

    Update sejda-pdf from 6.0.1 to 6.0.2

--- Casks/sejda-pdf.rb
+++ Casks/sejda-pdf.rb
@@ -1,6 +1,6 @@
 cask 'sejda-pdf' do
-  version '6.0.1'
-  sha256 'cb79c0fda1c3afe257f9624f44e2aabfbf3c25022cd284f5dd5f62a24a599852'
+  version '6.0.2'
+  sha256 '11e2a6523396a53cbde3f6e547b3332dea38a79a0404937b825f73ba24855f89'
 
   # sejda-cdn.com was verified as official when first introduced to the cask
   url "https://sejda-cdn.com/downloads/sejda-desktop_#{version}.dmg"
