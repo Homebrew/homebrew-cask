fc741a471d97f60a1e629e3e7bb459085702111c
Vítor Galvão <info@vitorgalvao.com>
2019-12-29 21:35:59 +0000

    Update data-integration.rb

--- Casks/data-integration.rb
+++ Casks/data-integration.rb
@@ -4,7 +4,7 @@ cask 'data-integration' do
 
   # sourceforge.net/pentaho was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/pentaho/pdi-ce-#{version}.zip"
-  appcast 'https://sourceforge.net/projects/pentaho/rss?path=/Data%20Integration'
+  appcast 'https://sourceforge.net/projects/pentaho/rss?path=/'
   name 'Pentaho Data Integration'
   homepage 'https://community.hitachivantara.com/community/products-and-solutions/pentaho/'
 
