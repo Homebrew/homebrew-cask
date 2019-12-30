6c5a7f76ca4a6f592c80120132643ea87073ae41
KorobkaVadim <34455@mail.ua>
2019-12-30 15:21:52 +0200

    Add DueFocus 2.0.8 (#74866)
    * Add DueFocus 2.0.8
    
    * Add appcast field to DueFocus Cask

---
+++ Casks/duefocus.rb
@@ -0,0 +1,17 @@
+cask 'duefocus' do
+  version '2.0.8'
+  sha256 'd14b26054bceb347ac6d94945e5215fb030f1f7447e012d86d35feadb677d8e6'
+
+  url "https://web.duefocus.com/distribution/darwin/v3/DueFocus-#{version}-mac.zip"
+  appcast 'https://web.duefocus.com/distribution/darwin/v3/appcast.html'
+  name 'DueFocus'
+  homepage 'https://duefocus.com/'
+
+  app 'DueFocus.app'
+
+  zap trash: [
+               '~/Library/Application Support/DueFocus',
+               '~/Library/Preferences/com.qarea.duefocus.plist',
+               '~/Library/Logs/DueFocus',
+             ]
+end
