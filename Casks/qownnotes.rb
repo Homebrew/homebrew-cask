8d2d3263a9e72fc63702cd7aa47033610412f1ae
brigitte zmoelnig <brigitte@corecode.io>
2019-12-30 22:05:44 +0100

    Update qownnotes from 19.12.16,b5106-161921 to 19.12.17,b5118-164943

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '19.12.16,b5106-161921'
-  sha256 '1d3772f6b562ce7c268ae86c2cdf3642100bca7038fff8278ca69e7364d24ca2'
+  version '19.12.17,b5118-164943'
+  sha256 'a3ea967577016a2593836eafc91221c62cb38cb4fabb884cb3ad18fe4f02157f'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
