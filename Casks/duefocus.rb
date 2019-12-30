33a04dd65467207db23ac7a728b2049f8acc33cc
KorobkaVadim <34455@mail.ua>
2019-12-30 14:45:00 +0200

    Add appcast field to DueFocus Cask

--- Casks/duefocus.rb
+++ Casks/duefocus.rb
@@ -3,6 +3,7 @@ cask 'duefocus' do
   sha256 'd14b26054bceb347ac6d94945e5215fb030f1f7447e012d86d35feadb677d8e6'
 
   url "https://web.duefocus.com/distribution/darwin/v3/DueFocus-#{version}-mac.zip"
+  appcast 'https://web.duefocus.com/distribution/darwin/v3/appcast.html'
   name 'DueFocus'
   homepage 'https://duefocus.com/'
 
