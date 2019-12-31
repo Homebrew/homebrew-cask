52fd4d5be1516bc2894a0ee0359bdfe2668f073f
Randall <ran.dall@icloud.com>
2019-12-30 16:54:05 -0800

    Update idagio from 0.0.108 to 0.0.109

--- Casks/idagio.rb
+++ Casks/idagio.rb
@@ -1,6 +1,6 @@
 cask 'idagio' do
-  version '0.0.108'
-  sha256 '39d56035164d3d0f4989954d03284daf70f5fd4e9d621d3a46b9b91cb70aa8f8'
+  version '0.0.109'
+  sha256 'f6415dd1fccb449a3d75b86875c80366c781b420d24c96cdeb15a0d1a5c3ea28'
 
   url "https://dl.idagio.com/IDAGIO-#{version}.dmg"
   name 'IDAGIO'
