b64389a0c3cc80afce075adee8dc5464b4a9db7a
Randall <ran.dall@icloud.com>
2019-12-30 16:53:38 -0800

    Update eaglefiler from 1.8.10 to 1.8.11

--- Casks/eaglefiler.rb
+++ Casks/eaglefiler.rb
@@ -1,6 +1,6 @@
 cask 'eaglefiler' do
-  version '1.8.10'
-  sha256 '67990c22dbfca831630ecf2c227280d9ef0219cad0cc6544f6a42960664b03b6'
+  version '1.8.11'
+  sha256 'f423884234ea93ac70e32debb1c53b42bb5d55ef12f90f2df42654ba763b0c47'
 
   url "https://c-command.com/downloads/EagleFiler-#{version}.dmg"
   appcast 'https://c-command.com/eaglefiler/help/version-history'
