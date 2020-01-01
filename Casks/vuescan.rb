5c5c5dfa72886cc2d71516a57f71f6768158b08b
Yoann Aubineau <304520+YoannAubineau@users.noreply.github.com>
2020-01-01 19:32:43 +0100

    Update vuescan (#74990)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.7.18'
-  sha256 'a107d03c2f9df03e2589ec8e1fa7404ecb14710d12a906effe1148ee7aa4c689'
+  sha256 'c61a27141610db8f407467cc3745ed10ff3cac241cc0d2e847a0fde362f6e452'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
