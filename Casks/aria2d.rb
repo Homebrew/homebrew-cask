21f5975b48a13e1ba683076e8407df63fc47b3f3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-01 11:05:16 -0800

    Update aria2d from 1.3.3 to 1.3.4 (#74992)

--- Casks/aria2d.rb
+++ Casks/aria2d.rb
@@ -1,6 +1,6 @@
 cask 'aria2d' do
-  version '1.3.3'
-  sha256 '14e649369bac231edb8b4374c0de142297027a5de0e04c80f4dbfcaf634af216'
+  version '1.3.4'
+  sha256 '0bbfa96007fbae53d42b8a524ec21aa8cf9376d1757436aa33525ae74ddb835d'
 
   # githubusercontent.com/xjbeta was verified as official when first introduced to the cask
   url "https://raw.githubusercontent.com/xjbeta/AppUpdaterAppcasts/master/Aria2D/Aria2D%20#{version}.dmg"
