9bc52930301af1616990a9e693e3bcb4ccde1005
suschizu <brigitte@corecode.io>
2020-01-02 16:09:06 +0100

    Update capture-one.rb

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -1,6 +1,6 @@
 cask 'capture-one' do
   version '13.0.1.19,20.0.1'
-  sha256 '776676d221676d0a79e0f7fa5a7a8c7b772e2b84efce8719177a0ae24261e67c'
+  sha256 'a880db69e7032a5878f95fc4003cce3d94d366bc93cfae91a0fed2deeadfe0c4'
 
   # phaseone.com/a9562498-f874-404e-842d-9f5cc8a05efd/International was verified as official when first introduced to the cask
   url "https://downloads.phaseone.com/a9562498-f874-404e-842d-9f5cc8a05efd/International/CaptureOne#{version.after_comma.major}.Mac.#{version.after_comma}.dmg"
