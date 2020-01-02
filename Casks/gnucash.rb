8f05435cb143f78b38fdd1631c9ac8d3477fcb70
CoreCode <core-code@users.noreply.github.com>
2020-01-02 23:33:39 +0100

    Update gnucash.rb (#75003)

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -3,7 +3,7 @@ cask 'gnucash' do
   sha256 '4a32ba1b770d1c3f72549235c9333d96472f549435fe9d11c2a6b85b325b1a7a'
 
   # github.com/Gnucash/gnucash was verified as official when first introduced to the cask
-  url "https://github.com/Gnucash/gnucash/releases/download/#{version.before_comma}/Gnucash-Intel-#{version.before_comma.delete_suffix('b')}-#{version.after_comma}.dmg"
+  url "https://github.com/Gnucash/gnucash/releases/download/#{version.before_comma}/Gnucash-Intel-#{version.before_comma.chomp('b')}-#{version.after_comma}.dmg"
   appcast 'https://github.com/Gnucash/gnucash/releases.atom'
   name 'GnuCash'
   homepage 'https://www.gnucash.org/'
