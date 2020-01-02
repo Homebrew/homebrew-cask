43c08b72abdbb5e925ef297b52aefdc531ae6561
Nishchit <dnishchit@gmail.com>
2020-01-02 07:52:37 +0530

    Firecamp upgraded to v0.7.10 (#74980)
    * Firecamp upgraded to v0.7.4
    
    * Firecamp upgraded to v0.7.5
    
    * upgraded to v0.7.8
    
    * Firecamp upgraded to v0.7.10

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -4,9 +4,9 @@ cask 'firecamp' do
 
   # firecamp.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg"
-  appcast 'https://firecamp.app/docs/installation'
+  appcast 'https://firecamp.io/docs/installation'
   name 'Firecamp'
-  homepage 'https://firecamp.app/'
+  homepage 'https://firecamp.io/'
 
   app 'Firecamp.app'
 end
