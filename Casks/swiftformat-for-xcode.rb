acc3a9c963143aa88ce98d104aff6b4e2676249a
Nick Lockwood <nicklockwood@users.noreply.github.com>
2020-01-02 02:21:16 +0000

    Add Swiftformat for Xcode 0.43.3 (#74981)

---
+++ Casks/swiftformat-for-xcode.rb
@@ -0,0 +1,20 @@
+cask 'swiftformat-for-xcode' do
+  version '0.43.3'
+  sha256 'b79c9bd9a4e48d29eb8d678fcb9e9d93abfe71cac4b3025e575769181d6f585c'
+
+  url "https://github.com/nicklockwood/SwiftFormat/archive/#{version}.zip"
+  appcast 'https://github.com/nicklockwood/SwiftFormat/releases.atom'
+  name 'SwiftFormat for Xcode'
+  homepage 'https://github.com/nicklockwood/SwiftFormat'
+
+  depends_on macos: '>= :sierra'
+
+  app "SwiftFormat-#{version}/EditorExtension/SwiftFormat for Xcode.app"
+
+  zap trash: [
+               '~/Library/Application Scripts/com.charcoaldesign.SwiftFormat-for-Xcode',
+               '~/Library/Application Scripts/com.charcoaldesign.SwiftFormat-for-Xcode.SourceEditorExtension',
+               '~/Library/Containers/com.charcoaldesign.SwiftFormat-for-Xcode',
+               '~/Library/Group Containers/com.charcoaldesign.SwiftFormat',
+             ]
+end
