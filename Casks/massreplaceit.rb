9abdae9be9fe63048a0ed6e3a519717ddb497f03
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-02 18:48:24 -0800

    Update massreplaceit from 3.1 to 3.1.1 (#75029)

--- Casks/massreplaceit.rb
+++ Casks/massreplaceit.rb
@@ -1,6 +1,6 @@
 cask 'massreplaceit' do
-  version '3.1'
-  sha256 'e9e0c51af2f35543a979519bc2e3cf202c81518977de18e80388d9fdd1bab097'
+  version '3.1.1'
+  sha256 'e44e97787a45b0ae52346f833c0ba3bf1383b407e0fc327d536347eb3eef5509'
 
   url 'http://www.hexmonkeysoftware.com/files/MassReplaceIt.dmg'
   appcast 'http://www.hexmonkeysoftware.com/'
