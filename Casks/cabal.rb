92c23c59b388b624963cc85b28fc4f513148a9ab
Vítor Galvão <info@vitorgalvao.com>
2020-01-03 22:49:40 +0000

    Update cabal.rb

--- Casks/cabal.rb
+++ Casks/cabal.rb
@@ -3,9 +3,9 @@ cask 'cabal' do
   sha256 '019f4c89a372dd5f7e3b5c6e8ccedda8e8ae6fa4b7ffa566f527c70f8d30bf60'
 
   # github.com/cabal-club/cabal-desktop was verified as official when first introduced to the cask
-  url 'https://github.com/cabal-club/cabal-desktop/releases/download/v4.0.0/cabal-desktop-4.0.0-mac.dmg'
+  url "https://github.com/cabal-club/cabal-desktop/releases/download/v#{version}/cabal-desktop-#{version}-mac.dmg"
   appcast 'https://github.com/cabal-club/cabal-desktop/releases.atom'
-  name 'Cabal Desktop'
+  name 'Cabal'
   homepage 'https://cabal.chat/'
 
   app 'Cabal.app'
