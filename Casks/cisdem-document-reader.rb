1791d028eebf38336966fee1cb6d854ebbb12d56
Vítor Galvão <info@vitorgalvao.com>
2020-01-03 16:19:37 +0000

    Update cisdem-document-reader.rb

--- Casks/cisdem-document-reader.rb
+++ Casks/cisdem-document-reader.rb
@@ -1,5 +1,5 @@
 cask 'cisdem-document-reader' do
-  version '5'
+  version '5.0.0'
   sha256 'e621f103edcd8816ca72a1b73cdebaea00e0300f9cf506bcae00178d5fb5965f'
 
   url 'https://www.cisdem.com/downloads/cisdem-documentreader-18.dmg'
