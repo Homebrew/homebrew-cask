d87806dbf5c93a25e6a65ea77d38748e0a278c89
Felipe Turcheti <fturcheti@gmail.com>
2020-01-03 10:20:10 -0300

    Update alt-tab from 2.0.2 to 2.2.0

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,6 +1,6 @@
 cask 'alt-tab' do
-  version '2.0.2'
-  sha256 'ac03e7d64b048935bc5d36d8b31a27aa37931b0e8f62285d1cb227f642b16427'
+  version '2.2.0'
+  sha256 '593d1ebfa50b72c0c39f58355b71ee0e2611de11e0242b55539bc6c143376dfb'
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
   appcast 'https://github.com/lwouis/alt-tab-macos/releases.atom'
