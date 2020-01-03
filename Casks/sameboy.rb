945747e646fb26a695da83bdf659e8cc138572d2
Randall <ran.dall@icloud.com>
2020-01-02 18:06:04 -0800

    Update sameboy from 0.12.2 to 0.12.3

--- Casks/sameboy.rb
+++ Casks/sameboy.rb
@@ -1,6 +1,6 @@
 cask 'sameboy' do
-  version '0.12.2'
-  sha256 'ff366d729fe4c18fefa03edd8936f8146075ac3ee5b5a0a71e5d8e8b753a43b3'
+  version '0.12.3'
+  sha256 '9de462c00fc89b3c7a498aaadfa6c3a3a5beb698bfd307159f4960ad84dc1623'
 
   # github.com/LIJI32/SameBoy was verified as official when first introduced to the cask
   url "https://github.com/LIJI32/SameBoy/releases/download/v#{version}/sameboy_cocoa_v#{version}.zip"
