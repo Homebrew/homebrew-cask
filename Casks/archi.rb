28d8006adfcd22749944bfc7c7e2935c3b870ca2
Jörg Eichhorn <eichhorn@ponton.de>
2020-01-03 19:27:24 +0100

    Re-added archi 4.6.0 with corrected url

---
+++ Casks/archi.rb
@@ -0,0 +1,11 @@
+cask 'archi' do
+  version '4.6.0,74a43a6b4f295fe0d'
+  sha256 '761a1f37af04fc2dc1bb8d9d250f2c4251a08c47ed4893c13fe087800e029030'
+
+  url "https://www.archimatetool.com/downloads/#{version.after_comma}/Archi-Mac-#{version.before_comma}.zip"
+  appcast 'https://github.com/archimatetool/archi/releases.atom'
+  name 'Archi'
+  homepage 'https://www.archimatetool.com/'
+
+  app 'Archi/Archi.app'
+end
