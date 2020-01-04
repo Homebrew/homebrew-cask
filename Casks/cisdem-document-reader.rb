893d9b6b183dd5ed056d441ab91f47c04a2df540
genlisCA <31656849+genlisCA@users.noreply.github.com>
2020-01-04 22:15:09 +0100

    Update cisdem-document-reader.rb

--- Casks/cisdem-document-reader.rb
+++ Casks/cisdem-document-reader.rb
@@ -2,7 +2,7 @@ cask 'cisdem-document-reader' do
   version '5.0.0'
   sha256 'e621f103edcd8816ca72a1b73cdebaea00e0300f9cf506bcae00178d5fb5965f'
 
-  url 'https://www.cisdem.com/downloads/cisdem-documentreader-18.dmg'
+  url 'http://download.cisdem.com/cisdem-documentreader.dmg'
   appcast 'https://www.cisdem.com/document-reader-mac/release-notes.html'
   name 'Cisdem Document Reader'
   homepage 'https://www.cisdem.com/document-reader-mac.html'
