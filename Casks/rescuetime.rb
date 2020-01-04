991a7c121ea3cfff6d738795c34b3f2ddd7e2c74
suschizu <brigitte@corecode.io>
2020-01-05 00:12:13 +0100

    Update rescuetime.rb

--- Casks/rescuetime.rb
+++ Casks/rescuetime.rb
@@ -1,3 +1,4 @@
+cask 'rescuetime' do
   version '2.14.5.1'
   sha256 '68a6335156f2c17376a82c1b8204df00fa5b1ccd92d77c301bc422d41835bc5d'
 
@@ -7,7 +8,9 @@
   homepage 'https://www.rescuetime.com/'
 
   pkg 'RescueTimeInstaller.pkg'
+
   uninstall pkgutil: 'com.rescuetime.RescueTime'
+
   zap trash: [
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.rescuetime.rescuetime.sfl*',
                '~/Library/Caches/com.rescuetime.RescueTime',
