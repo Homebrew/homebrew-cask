9f02aa0edebe45eab2a64c9ab9f87cef87b99a94
Randall <ran.dall@icloud.com>
2020-01-05 14:21:27 -0800

    Update syncovery from 8.58a to 8.59a

--- Casks/syncovery.rb
+++ Casks/syncovery.rb
@@ -1,6 +1,6 @@
 cask 'syncovery' do
-  version '8.58a'
-  sha256 'c7e4a2095c71996793b2ebc52577cff921235c49a60e0ef8c336f317a1efe237'
+  version '8.59a'
+  sha256 '15271fd1a185f9f9d66af1bfc24392b90c0439546a9460d878a6bb3dc3781903'
 
   url "https://www.syncovery.com/release/SyncoveryMac#{version}.dmg"
   appcast 'https://www.syncovery.com/download/mac/'
