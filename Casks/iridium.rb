355b9ea927cb4b22f747ef60d5578848f46a69dd
Vítor Galvão <info@vitorgalvao.com>
2020-01-06 14:45:10 +0000

    Update iridium.rb

--- Casks/iridium.rb
+++ Casks/iridium.rb
@@ -1,9 +1,10 @@
 cask 'iridium' do
-  version '2019.11.79.0,2019.11-0'
+  version '2019.11.79.0'
   sha256 'a53342aa2cad7df3e3792a66efa82e5d9ff61210e6c1ec8e880c65f899942099'
 
-  url "https://downloads.iridiumbrowser.de/macos/#{version.after_comma}/iridium-browser_#{version.after_comma}_macos.dmg"
-  appcast 'https://downloads.iridiumbrowser.de/macos/'
+  url "https://downloads.iridiumbrowser.de/macos/#{version.major_minor}-0/iridium-browser_#{version.major_minor}-0_macos.dmg"
+  appcast 'https://downloads.iridiumbrowser.de/macos/',
+          configuration: version.major_minor
   name 'Iridium Browser'
   homepage 'https://iridiumbrowser.de/'
 
