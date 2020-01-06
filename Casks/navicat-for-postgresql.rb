8d53f4fe9f91d606b19a8eccd2ac44fdaf5342ff
Randall <ran.dall@icloud.com>
2020-01-06 13:18:12 -0800

    Update navicat-for-postgresql from 15.0.6 to 15.0.7

--- Casks/navicat-for-postgresql.rb
+++ Casks/navicat-for-postgresql.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-postgresql' do
-  version '15.0.6'
-  sha256 'f868ada5edb0242dbc8eafbc62b25a27d692c673ff762f2966cb23f8a9313cc1'
+  version '15.0.7'
+  sha256 '2d46c2e8b01530497981e512c76ca2ce6ac3f110e9d219fea4feedb6495c6b15'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_pgsql_en.dmg"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20for%20PostgreSQL&appLang=en'
