c732fb2672893b2ccc371c2e23a3aefa3251333a
Vítor Galvão <info@vitorgalvao.com>
2020-01-06 23:35:52 +0000

    Update doxie.rb

--- Casks/doxie.rb
+++ Casks/doxie.rb
@@ -3,7 +3,7 @@ cask 'doxie' do
   sha256 '6f48066eca465fc17838ed9552eb5421472dcdc40757ee484014d6d9e256446b'
 
   url "https://download.getdoxie.com/Doxie-#{version}.dmg"
-  appcast 'http://r.getdoxie.com/app/sparkle/doxie-macos.xml'
+  appcast 'https://r.getdoxie.com/app/sparkle/doxie-macos.xml'
   name 'Doxie'
   homepage 'https://www.getdoxie.com/'
 
