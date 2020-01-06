2a8d1fce993b43360d99f6cb1042e744aee29db5
Daniel Bayley <daniel.bayley@me.com>
2020-01-06 01:46:04 +0000

    Improve Backblaze Cask (#72782)
    * Improve Backblaze Cask
    
    * Update backblaze.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/backblaze.rb
+++ Casks/backblaze.rb
@@ -1,23 +1,29 @@
 cask 'backblaze' do
-  version :latest
-  sha256 :no_check
+  version '7.0.0.392'
+  sha256 'c5bb26fab34b890c643c8c6ebb23fccc1a7014530e1d6cdcc9010ab722ebe516'
 
   url 'https://secure.backblaze.com/mac/install_backblaze.dmg'
+  appcast 'https://backblaze.com/blog/category/backblaze-bits/release',
+          configuration: version.major_minor
   name 'Backblaze'
-  homepage 'https://www.backblaze.com/'
+  homepage 'https://backblaze.com/'
 
   installer manual: 'Backblaze Installer.app'
 
   uninstall launchctl: [
-                         'com.backblaze.bzserv.plist',
-                         'com.backblaze.bzbmenu.plist',
+                         'com.backblaze.bzserv',
+                         'com.backblaze.bzbmenu',
                        ],
-            delete:    '/Library/PreferencePanes/BackblazeBackup.prefPane'
+            delete:    [
+                         "#{appdir}/Backblaze.app",
+                         '/Library/PreferencePanes/BackblazeBackup.prefPane',
+                       ]
 
   zap trash: [
                '/Library/Backblaze.bzpkg',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.backblaze.backblazebackup.sfl*',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.backblaze.bzdoinstall.sfl*',
+               '~/Library/Preferences/com.backblaze.bzbmenu.plist',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.backblaze.*.sfl*',
+               '/Library/Logs/DiagnosticReports/bzbmenu_*.*_resource.diag',
                '~/Library/Logs/BackblazeGUIInstaller',
              ]
 end
