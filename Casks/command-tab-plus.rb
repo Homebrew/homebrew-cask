e10ec675288a4152dd66948247cb6902e46346c1
brigitte zmoelnig <brigitte@corecode.io>
2020-01-07 13:17:48 +0100

    Update command-tab-plus from 1.109,359:1576772358 to 1.109

--- Casks/command-tab-plus.rb
+++ Casks/command-tab-plus.rb
@@ -1,11 +1,10 @@
 cask 'command-tab-plus' do
-  version '1.109,359:1576772358'
+  version '1.109'
   sha256 '608458aa55d699f6c1d21a6bc834365d0787174b4f34b266dad8f267acfdc328'
 
-  # dl.devmate.com/com.sergey-gerasimenko.Command-Tab was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.sergey-gerasimenko.Command-Tab/#{version.after_comma.before_colon}/#{version.after_colon}/Command-Tab-#{version.after_comma.before_colon}.zip"
-  appcast 'https://updates.devmate.com/com.sergey-gerasimenko.Command-Tab.xml',
-          configuration: version.after_comma.before_colon
+  # macplus-software.com/downloads was verified as official when first introduced to the cask
+  url 'https://macplus-software.com/downloads/Command-Tab%20Plus.zip'
+  appcast 'https://macplus-software.com/downloads/Command-Tab.xml'
   name 'Command-Tab Plus'
   homepage 'http://commandtab.noteifyapp.com/'
 
