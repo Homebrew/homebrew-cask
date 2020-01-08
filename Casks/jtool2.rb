86e58f18da8eee054630316ba4d0fc2e40cbaa27
zcutlip <uid000@gmail.com>
2020-01-08 12:56:46 -0800

    Update jtool2.rb
    Unfortunately we can't check the version or the hash since the author just distributes "jtool2.tgz"

--- Casks/jtool2.rb
+++ Casks/jtool2.rb
@@ -1,6 +1,8 @@
 cask 'jtool2' do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  version '1.0.0.0'
+  # Unfortunately the author doesn't uniquely name the tarball, so there's no way
+  # to download a specific version and no way to check the hash
+  version :latest
   sha256 :no_check
 
   url 'http://newosxbook.com/tools/jtool2.tgz'
