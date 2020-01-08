b273a5a114446848ed68440373ec18ba1d56410c
Randall <ran.dall@icloud.com>
2020-01-08 15:39:33 -0800

    Update seashore from 2.5.0 to 2.5.2

--- Casks/seashore.rb
+++ Casks/seashore.rb
@@ -1,6 +1,6 @@
 cask 'seashore' do
-  version '2.5.0'
-  sha256 '5fbebaa97f6acbc919533e6eff9ef287a3a65fbf85bac2abac818a4485de5932'
+  version '2.5.2'
+  sha256 'c8cb57bbe2a4e4a1810766287e08f0b57908d7bae20743ab17fc2b3706da9186'
 
   url "https://github.com/robaho/seashore/releases/download/v#{version}/seashore-bin-#{version}.dmg"
   appcast 'https://github.com/robaho/seashore/releases.atom'
