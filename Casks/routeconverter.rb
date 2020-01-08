27e556d5f8bd3ac13cb2847f3b45851966325277
suschizu <brigitte@corecode.io>
2020-01-08 01:28:26 +0100

    Update routeconverter from 2.26.69 to 2.27.94 (#75205)
    * Update routeconverter from 2.26.69 to 2.27.94
    
    * Update routeconverter.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/routeconverter.rb
+++ Casks/routeconverter.rb
@@ -1,9 +1,10 @@
 cask 'routeconverter' do
-  version '2.26.69'
-  sha256 'ade167e527527a84e3a51df37b99622f1e3a0a4934656ee4ba088df4e780dda9'
+  version '2.27.94'
+  sha256 'ca6d7be4fbbefd406ef9f87b9f3713d4ab224da43acaa87d32c9ad3b45f1f9e5'
 
   url 'https://static.routeconverter.com/download/RouteConverterMac.app.zip'
-  appcast 'https://static.routeconverter.com/download/previous-releases/'
+  appcast 'https://static.routeconverter.com/download/previous-releases/',
+          configuration: version.major_minor
   name 'RouteConverter'
   homepage 'https://www.routeconverter.com/'
 
