0c6c657796403f8ff8115a7b125e0034d54fbfba
suschizu <brigitte@corecode.io>
2020-01-08 23:32:40 +0100

    Update buttercup (#75259)

--- Casks/buttercup.rb
+++ Casks/buttercup.rb
@@ -1,6 +1,6 @@
 cask 'buttercup' do
   version '1.18.2'
-  sha256 '6128a5e68e1911e6a0ed760ba3ce88d929373e537900247082837d2149c3fb9a'
+  sha256 '0e75d28d54edf7b9fcc0e7d8eb616156c32f074c700ef8a62f15337b05e7f971'
 
   # github.com/buttercup/buttercup-desktop was verified as official when first introduced to the cask
   url "https://github.com/buttercup/buttercup-desktop/releases/download/v#{version}/Buttercup-#{version}.dmg"
