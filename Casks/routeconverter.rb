7d36aa6fbdf99aaa501ea436f962dfee6b62e601
suschizu <brigitte@corecode.io>
2020-01-08 23:02:29 +0100

    Update routeconverter (#75256)

--- Casks/routeconverter.rb
+++ Casks/routeconverter.rb
@@ -1,6 +1,6 @@
 cask 'routeconverter' do
   version '2.27.94'
-  sha256 'ca6d7be4fbbefd406ef9f87b9f3713d4ab224da43acaa87d32c9ad3b45f1f9e5'
+  sha256 'afdda5a0fdcd1a5dd27f2740b069a071199abf78f59dbc2d79846a8890f7314b'
 
   url 'https://static.routeconverter.com/download/RouteConverterMac.app.zip'
   appcast 'https://static.routeconverter.com/download/previous-releases/',
