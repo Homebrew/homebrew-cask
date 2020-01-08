26687e00b963015bf4ff6a617b97ae9292e5e076
Vítor Galvão <info@vitorgalvao.com>
2020-01-08 00:07:06 +0000

    Update routeconverter.rb

--- Casks/routeconverter.rb
+++ Casks/routeconverter.rb
@@ -3,7 +3,8 @@ cask 'routeconverter' do
   sha256 'ca6d7be4fbbefd406ef9f87b9f3713d4ab224da43acaa87d32c9ad3b45f1f9e5'
 
   url 'https://static.routeconverter.com/download/RouteConverterMac.app.zip'
-  appcast 'https://static.routeconverter.com/download/previous-releases/'
+  appcast 'https://static.routeconverter.com/download/previous-releases/',
+          configuration: version.major_minor
   name 'RouteConverter'
   homepage 'https://www.routeconverter.com/'
 
