b5da4c288c05b49b26bef3cfd7be61de4c9cd7be
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-08 16:33:24 -0800

    Update dcp-o-matic-kdm-creator from 2.14.19 to 2.14.21 (#75289)

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-kdm-creator' do
-  version '2.14.19'
-  sha256 '8a8f5f8f07efc4100ed78da41e49fbd4c6e9e3ca348e54e8635c38cca5b13527'
+  version '2.14.21'
+  sha256 'e465b1d49c98c9cfce707a6031c4f7791cfda665e1a11640d52a5c470f7d63fe'
 
   url "https://dcpomatic.com/dl.php?id=osx-kdm&version=#{version}"
   appcast 'https://dcpomatic.com/download'
