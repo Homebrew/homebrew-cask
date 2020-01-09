58bdf4eeee04482afc89f1df0254867228581039
Ji4n1ng <wjnmailg@gmail.com>
2020-01-09 14:01:00 +0800

    Update OpenInTerminal-Lite from 1.0.3 to 1.1.0

--- Casks/openinterminal-lite.rb
+++ Casks/openinterminal-lite.rb
@@ -1,6 +1,6 @@
 cask 'openinterminal-lite' do
-  version '1.0.3'
-  sha256 'db24d1ae5866abfc4a642c1563a9f6d8e38a9d9ad60db44ba74ce10f28c4ff5c'
+  version '1.1.0'
+  sha256 '8efc42a960ee29385b73f2e31759fe207df5f7904a64e422e1d606bc9c18eb3a'
 
   url "https://github.com/Ji4n1ng/OpenInTerminal/releases/download/#{version}/OpenInTerminal-Lite.app.zip"
   appcast 'https://github.com/Ji4n1ng/OpenInTerminal/releases.atom'
