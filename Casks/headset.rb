f329bedc6c7544d42e67f0cee5a41727475636ca
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-09 12:52:01 -0800

    Update headset from 3.1.0 to 3.2.0 (#75321)

--- Casks/headset.rb
+++ Casks/headset.rb
@@ -1,6 +1,6 @@
 cask 'headset' do
-  version '3.1.0'
-  sha256 '95ac91d8cc7fecf363286b1962e99916e8e1cd0f9bcc6a2df163029b0503afa9'
+  version '3.2.0'
+  sha256 '30b9f8ca2afa9dd15554baee2633495b65882fdf8ae15553fa4adbb1d1c0490f'
 
   # github.com/headsetapp/headset-electron was verified as official when first introduced to the cask
   url "https://github.com/headsetapp/headset-electron/releases/download/v#{version}/Headset-#{version}.dmg"
