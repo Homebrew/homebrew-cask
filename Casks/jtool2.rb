b94004cf40d6ff74f900afc876709220a7d2a7b5
Vítor Galvão <info@vitorgalvao.com>
2020-01-10 02:55:43 +0000

    Update jtool2.rb

--- Casks/jtool2.rb
+++ Casks/jtool2.rb
@@ -2,7 +2,7 @@ cask 'jtool2' do
   # note: "2" is not a version number, but an intrinsic part of the product name
   # "version" is based on the release date as noted in WhatsNew.txt
   version '2020.01.01'
-  sha256 '3c94ecffebbd7c051b3ad45922063cc5a5c316b89c7dfbc9fb9071534b2f7332'
+  sha256 '88ab8587a8528238b3c27605d9581fc97b43850c9aa62ad83c63b1674ce7e52b'
 
   url 'http://newosxbook.com/tools/jtool2.tgz'
   name 'jtool2'
