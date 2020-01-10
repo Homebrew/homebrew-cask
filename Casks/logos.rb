1d185be285e65c8373aa7dc1bebeeda0e9cf3ffa
Julian Mayer <julian@corecode.io>
2020-01-10 17:32:39 +0100

    Update logos from 8.9.0.0030 to 8.10.0.0032

--- Casks/logos.rb
+++ Casks/logos.rb
@@ -1,6 +1,6 @@
 cask 'logos' do
-  version '8.9.0.0030'
-  sha256 'cc94fae8556232a444bf52d5b3c98d502637f92b86fdc976399c950fecc654ea'
+  version '8.10.0.0032'
+  sha256 'e742c8b011899c383bbf73357245918fd9c3467d10e8cc4b9d7bd409e58b7366'
 
   # downloads.logoscdn.com was verified as official when first introduced to the cask
   url "https://downloads.logoscdn.com/LBS8/Installer/#{version}/LogosMac.dmg"
