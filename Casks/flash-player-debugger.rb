5ad533857b2868161af544016f0e44e77c55b7bb
Daniel Bayley <daniel.bayley@me.com>
2020-01-10 22:14:39 +0000

    Fix flash-player-debugger uninstall

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -11,8 +11,18 @@ cask 'flash-player-debugger' do
   # Renamed to avoid conflict with flash-player.
   app 'Flash Player.app', target: 'Flash Player Debugger.app'
 
+  uninstall pkgutil:   'com.adobe.pkg.PepperFlashPlayer',
+            launchctl: 'com.adobe.fpsaud',
+            delete:    [
+                         '/Library/Application Support/Adobe/Flash Player Install Manager',
+                         '/Library/Internet Plug-Ins/PepperFlashPlayer',
+                       ]
+
   zap trash: [
+               '/Library/Internet Plug-Ins/flashplayer.xpt',
                '~/Library/Caches/Adobe/Flash Player',
                '~/Library/Logs/FlashPlayerInstallManager.log',
+               '~/Library/Preferences/Macromedia/Flash Player',
+               '~/Library/Saved Application State/com.adobe.flashplayer.installmanager.savedState',
              ]
 end
