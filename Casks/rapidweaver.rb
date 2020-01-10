0bc6198497ef5ccbf17ffe9582b69d6808dc3cc1
suschizu <brigitte@corecode.io>
2020-01-10 13:47:12 +0100

    Update rapidweaver.rb

--- Casks/rapidweaver.rb
+++ Casks/rapidweaver.rb
@@ -3,7 +3,7 @@ cask 'rapidweaver' do
   sha256 'f8d6f6973a5ec070f7bbe12d15ecc2ba8df4979c5f7390babb9be12861420a47'
 
   # github.com/realmacsoftware was verified as official when first introduced to the cask
-  url "https://github.com/realmacsoftware/RapidWeaver#{version.major}-releases/releases/download/8.5-%28#{version.after_comma}%29/RapidWeaver#{version.major}.zip"
+  url "https://github.com/realmacsoftware/RapidWeaver#{version.major}-releases/releases/download/#{version.before_comma}-%28#{version.after_comma}%29/RapidWeaver#{version.major}.zip"
   appcast "https://github.com/realmacsoftware/RapidWeaver#{version.major}-releases/releases.atom"
   name 'RapidWeaver'
   homepage 'https://www.realmacsoftware.com/rapidweaver/'
