e87be991602264de2bf90c483f960bb2d368fa01
Daniel Bayley <daniel.bayley@me.com>
2020-01-10 22:04:07 +0000

    Fix flash-player uninstall

--- Casks/flash-player.rb
+++ Casks/flash-player.rb
@@ -10,8 +10,18 @@ cask 'flash-player' do
 
   app 'Flash Player.app'
 
+  uninstall pkgutil:   'com.adobe.pkg.PepperFlashPlayer',
+            launchctl: 'com.adobe.fpsaud',
+            delete:    [
+                         '/Library/Application Support/Adobe/Flash Player Install Manager',
+                         '/Library/Internet Plug-Ins/PepperFlashPlayer',
+                       ]
+
   zap trash: [
+               '/Library/Internet Plug-Ins/flashplayer.xpt',
                '~/Library/Caches/Adobe/Flash Player',
                '~/Library/Logs/FlashPlayerInstallManager.log',
+               '~/Library/Preferences/Macromedia/Flash Player',
+               '~/Library/Saved Application State/com.adobe.flashplayer.installmanager.savedState',
              ]
 end
