e528f48bce9f039dbba97c6ab6c46839a188e66f
Greg Hamerly <hamerly@cs.baylor.edu>
2020-01-10 14:55:23 -0600

    update gnucash.rb cask from 3.8-2 to 3.8-3
    version 3.8-2 does not exist at https://github.com/Gnucash/gnucash/releases/ as of this writing.

--- Casks/gnucash.rb
+++ Casks/gnucash.rb
@@ -1,6 +1,6 @@
 cask 'gnucash' do
-  version '3.8b,2'
-  sha256 '4a32ba1b770d1c3f72549235c9333d96472f549435fe9d11c2a6b85b325b1a7a'
+  version '3.8b,3'
+  sha256 '05744be1fc8c60609e9315c2bdaf5f7c7ad51c513bea8408df7546f7d1ed30e7'
 
   # github.com/Gnucash/gnucash was verified as official when first introduced to the cask
   url "https://github.com/Gnucash/gnucash/releases/download/#{version.before_comma}/Gnucash-Intel-#{version.before_comma.chomp('b')}-#{version.after_comma}.dmg"
