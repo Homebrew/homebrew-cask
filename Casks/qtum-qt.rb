01ec13d89ec3c869612f88c50ba674ac01f06f6a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-10 17:22:32 -0800

    Update qtum-qt from 0.18.2 to 0.18.3 (#75383)

--- Casks/qtum-qt.rb
+++ Casks/qtum-qt.rb
@@ -1,6 +1,6 @@
 cask 'qtum-qt' do
-  version '0.18.2'
-  sha256 'de21ee839e2ea67ab85e684358deaaecfd6f3c990a9c2f3bfdcbdf92ceb478f7'
+  version '0.18.3'
+  sha256 '80639e7cb0f38a6c2cea72361720629bad3fc99475075ae3771ef6c857637c7e'
 
   # github.com/qtumproject/qtum was verified as official when first introduced to the cask
   url "https://github.com/qtumproject/qtum/releases/download/mainnet-ignition-v#{version}/qtum-#{version}-osx-unsigned.dmg"
