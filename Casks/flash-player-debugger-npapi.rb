07f03fc0ce8b58935375bfe9816d23c0a9545cc7
Daniel Bayley <daniel.bayley@me.com>
2020-01-11 18:20:44 +0000

    Fix flash-player-debugger-npapi (#75371)

--- Casks/flash-player-debugger-npapi.rb
+++ Casks/flash-player-debugger-npapi.rb
@@ -10,11 +10,18 @@ cask 'flash-player-debugger-npapi' do
 
   pkg 'Install Adobe Flash Player Debugger.app/Contents/Resources/Adobe Flash Player Debugger.pkg'
 
-  uninstall pkgutil: 'com.adobe.pkg.FlashPlayer',
-            delete:  '/Library/Internet Plug-Ins/Flash Player.plugin'
+  uninstall pkgutil:   'com.adobe.pkg.FlashPlayer',
+            launchctl: 'com.adobe.fpsaud',
+            delete:    [
+                         '/Library/Application Support/Adobe/Flash Player Install Manager',
+                         '/Library/Internet Plug-Ins/Flash Player.plugin',
+                       ]
 
   zap trash: [
+               '/Library/Internet Plug-Ins/flashplayer.xpt',
                '~/Library/Caches/Adobe/Flash Player',
                '~/Library/Logs/FlashPlayerInstallManager.log',
+               '~/Library/Preferences/Macromedia/Flash Player',
+               '~/Library/Saved Application State/com.adobe.flashplayer.installmanager.savedState',
              ]
 end
