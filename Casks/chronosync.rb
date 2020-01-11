e0a895df9a251ad8236623c997a482eb4fbfc345
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-10 17:36:19 -0800

    Update chronosync from 4.9.7 to 4.9.8 (#75394)

--- Casks/chronosync.rb
+++ Casks/chronosync.rb
@@ -1,6 +1,6 @@
 cask 'chronosync' do
-  version '4.9.7'
-  sha256 'b7e86eda90010050e4855d4f96ee0e97201e8fb8f65a418eec140f303aaa1422'
+  version '4.9.8'
+  sha256 'af52fbc8e762317ed3b38ef7a02d73bb20aacafc05867c587184ba629d295182'
 
   url 'https://downloads.econtechnologies.com/CS4_Download.dmg'
   appcast "https://www.econtechnologies.com/UC/updatecheck.php?prod=ChronoSync&vers=#{version.major_minor}.0&lang=en&plat=mac&os=10.14.1&hw=i64&req=1"
