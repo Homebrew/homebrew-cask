f84a9ed0a0da189a85249efaa79a5f1107ed6b35
Daniel Bayley <daniel.bayley@me.com>
2020-01-11 18:14:18 +0000

    Fix flash-player-debugger-ppapi uninstall (#75373)

--- Casks/flash-player-debugger-ppapi.rb
+++ Casks/flash-player-debugger-ppapi.rb
@@ -10,11 +10,18 @@ cask 'flash-player-debugger-ppapi' do
 
   pkg 'Install Adobe Pepper Flash Player Debugger.app/Contents/Resources/Adobe Flash Player Debugger.pkg'
 
-  uninstall pkgutil: 'com.adobe.pkg.PepperFlashPlayer',
-            delete:  '/Library/Internet Plug-Ins/PepperFlashPlayer'
+  uninstall pkgutil:   'com.adobe.pkg.PepperFlashPlayer',
+            launchctl: 'com.adobe.fpsaud',
+            delete:    [
+                         '/Library/Application Support/Adobe/Flash Player Install Manager',
+                         '/Library/Internet Plug-Ins/PepperFlashPlayer',
+                       ]
 
   zap trash: [
+               '/Library/Internet Plug-Ins/flashplayer.xpt',
                '~/Library/Caches/Adobe/Flash Player',
                '~/Library/Logs/FlashPlayerInstallManager.log',
+               '~/Library/Preferences/Macromedia/Flash Player',
+               '~/Library/Saved Application State/com.adobe.flashplayer.installmanager.savedState',
              ]
 end
