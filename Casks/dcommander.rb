fee5f1f04ffe5d2ed301c2f0a8e2690a54e70f0b
Julian Mayer <julian@corecode.io>
2020-01-11 19:45:03 +0100

    Update dcommander from latest to 3700

--- Casks/dcommander.rb
+++ Casks/dcommander.rb
@@ -1,8 +1,9 @@
 cask 'dcommander' do
-  version :latest
-  sha256 :no_check
+  version '3700'
+  sha256 '41c497e7d80b6667c4303a5e3e55ce3fb15c26d15817abaacbf867930e7ee178'
 
   url 'https://devstorm-apps.com/dc/download.php'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://devstorm-apps.com/dc/download.php'
   name 'DCommander'
   homepage 'https://devstorm-apps.com/dc/'
 
