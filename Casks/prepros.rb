539b302186cc44aef965744691bdc2e651548359
Vítor Galvão <info@vitorgalvao.com>
2020-01-11 20:24:17 +0000

    Update prepros.rb

--- Casks/prepros.rb
+++ Casks/prepros.rb
@@ -3,7 +3,7 @@ cask 'prepros' do
   sha256 '717a0217c3d058190eff46e3c58ace7de67aed5cb8bdef0c813901e68b551b8a'
 
   url "https://downloads.prepros.io/v#{version.major}/Prepros-#{version}.zip"
-  appcast 'https://prepros.io/changelog'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://prepros.io/downloads/stable/mac'
   name 'Prepros'
   homepage 'https://prepros.io/'
 
