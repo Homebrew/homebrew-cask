2a00f0d3f422f40e6103de6a90b8a321603683e5
Vítor Galvão <info@vitorgalvao.com>
2020-01-12 15:32:36 +0000

    Update league-displays.rb

--- Casks/league-displays.rb
+++ Casks/league-displays.rb
@@ -2,17 +2,15 @@ cask 'league-displays' do
   version :latest
   sha256 :no_check
 
-  # riotgames.com was verified as official when first introduced to the cask
+  # screensaver.riotgames.com was verified as official when first introduced to the cask
   url 'https://screensaver.riotgames.com/v2/installer/latest/League%20Displays.dmg'
   name 'League Displays'
-  homepage 'https://na.leagueoflegends.com/en/featured/league-displays'
+  homepage 'https://leagueoflegends.com/en/featured/league-displays'
 
   app 'LeagueDisplays.app'
 
   zap trash: [
                '~/Library/Logs/LeagueDisplays-Configurator.log',
-             ],
-      rmdir: [
                '~/Library/Application Support/LolScreenSaver',
                '~/Library/Screen Savers/League.saver',
              ]
