3e69d03b29dff8c7594ba14c9f3b154c680d7af0
mvllow <mvllow@icloud.com>
2020-01-11 18:30:12 -0600

    Format file via cask style

--- Casks/league-displays.rb
+++ Casks/league-displays.rb
@@ -3,17 +3,17 @@ cask 'league-displays' do
   sha256 :no_check
 
   # riotgames.com was verified as official when first introduced to the cask
-  url "https://screensaver.riotgames.com/v2/installer/latest/League%20Displays.dmg"
+  url 'https://screensaver.riotgames.com/v2/installer/latest/League%20Displays.dmg'
   name 'League Displays'
   homepage 'https://na.leagueoflegends.com/en/featured/league-displays'
 
   app 'LeagueDisplays.app'
 
   zap trash: [
-       '~/Library/Logs/LeagueDisplays-Configurator.log',
-      ],
+               '~/Library/Logs/LeagueDisplays-Configurator.log',
+             ],
       rmdir: [
-        '~/Library/Application Support/LolScreenSaver',
-        '~/Library/Screen Savers/League.saver'
-      ]
+               '~/Library/Application Support/LolScreenSaver',
+               '~/Library/Screen Savers/League.saver',
+             ]
 end
