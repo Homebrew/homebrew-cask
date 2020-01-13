243d4f6560c4b4ea24348630111356224aee7233
Vítor Galvão <info@vitorgalvao.com>
2020-01-13 20:15:46 +0000

    Update beagle-im.rb

--- Casks/beagle-im.rb
+++ Casks/beagle-im.rb
@@ -1,12 +1,12 @@
-cask 'beagle-im' do
+cask 'beagleim' do
   version '3.4'
   sha256 'e00b44abfe5549030c92d4686d10369eb023cf79fae15a132a80606c0fd06583'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/tigase/beagle-im was verified as official when first introduced to the cask
   url "https://github.com/tigase/beagle-im/releases/download/#{version}/BeagleIM.#{version}.zip"
   appcast 'https://github.com/tigase/beagle-im/releases.atom'
   name 'BeagleIM'
   homepage 'https://beagle.im/'
 
-  app 'BeagleIM 3.4.app'
+  app "BeagleIM #{version}.app"
 end
