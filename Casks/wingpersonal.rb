cfbe02469e44dcb411cc847f769454e1f140bd55
Randall <ran.dall@icloud.com>
2020-01-14 13:31:22 -0800

    Update wingpersonal from 7.1.3.0 to 7.2.0.0

--- Casks/wingpersonal.rb
+++ Casks/wingpersonal.rb
@@ -1,6 +1,6 @@
 cask 'wingpersonal' do
-  version '7.1.3.0'
-  sha256 '85fc13b3317f4da76a93937410c4cb65a514af679a530ee451dd5640d3321fd3'
+  version '7.2.0.0'
+  sha256 '2a848e7b0e99bae995bfe5849bfdcdd21702d97949b1d20cbf92dc21b0235ea2'
 
   url "https://wingware.com/pub/wing-personal/#{version}/wing-personal-#{version}.dmg"
   appcast 'https://www.wingware.com/downloads/wing-personal'
