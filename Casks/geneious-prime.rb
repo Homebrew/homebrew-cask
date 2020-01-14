885230cbdb1eca51cbff61800b27d4772f8b12d2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-14 13:33:30 -0800

    Update geneious-prime from 2020.0.4 to 2020.0.5 (#75573)

--- Casks/geneious-prime.rb
+++ Casks/geneious-prime.rb
@@ -1,6 +1,6 @@
 cask 'geneious-prime' do
-  version '2020.0.4'
-  sha256 '34146eb8f9de56c4aceebdc50291254fcf309fd35a2980780dcc9a29ac0c2449'
+  version '2020.0.5'
+  sha256 '5ef6ac1ed4b1cf15f3021c30f590296c65a8458b744047689a66cf905a0dd8fb'
 
   url "https://assets.geneious.com/installers/geneious/release/Geneious_Prime_mac64_#{version.dots_to_underscores}_with_jre.dmg"
   appcast 'https://www.geneious.com/download/'
