98a151cce10bb395916ec69ca6510f8f457f53a3
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-14 13:50:24 -0800

    Update pomello from 0.10.10 to 0.10.11 (#75576)

--- Casks/pomello.rb
+++ Casks/pomello.rb
@@ -1,6 +1,6 @@
 cask 'pomello' do
-  version '0.10.10'
-  sha256 'b9a5e8038f9bba3263fe88af4e79d59ef8713fe8d1b33086c544e813a1862ffc'
+  version '0.10.11'
+  sha256 'e1406108de8301d162159d412a74672d709471b53b9ab039f68f31f6a52ff319'
 
   url 'https://pomelloapp.com/download/mac/latest'
   appcast 'https://pomelloapp.com/download/mac'
