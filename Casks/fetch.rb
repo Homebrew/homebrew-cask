91519dd4daf7f7ecbba6be8e1695fc9710b05eff
Vítor Galvão <info@vitorgalvao.com>
2020-01-14 19:23:04 +0000

    fetch.rb: better appcast

--- Casks/fetch.rb
+++ Casks/fetch.rb
@@ -3,7 +3,7 @@ cask 'fetch' do
   sha256 '88b102ff11c8dd0f932cbc8b1586be3c32a8f171030b6329ebdf7538c3ccf49e'
 
   url "https://fetchsoftworks.com/fetch/download/Fetch_#{version}.zip"
-  appcast 'https://fetchsoftworks.com/fetch/download/'
+  appcast "https://updates.fetchsoftworks.com/list/fetch-#{version.major}K0-release.xml"
   name 'Fetch'
   homepage 'https://fetchsoftworks.com/fetch/'
 
