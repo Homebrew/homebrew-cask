7fcf4ee523b6f4493af2dea137f4573d15a8e546
suschizu <brigitte@corecode.io>
2020-01-14 01:04:21 +0100

    Update frhelper from 3.9.4,2019-12-09 to 3.9.4,2019-12-13 (#75483)
    * Update frhelper from 3.9.4,2019-12-09 to 3.9.4,2019-12-13
    
    * Update frhelper.rb
    
    * Update frhelper.rb

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -1,10 +1,11 @@
 cask 'frhelper' do
-  version '3.9.4,2019-12-09'
+  version '3.9.4,2019-12-13'
   sha256 '30d03c22f3e3fd0533ef8f223f6db546f922722b4dde9eb91d53c1ce7d2f7660'
 
   # static.frdic.com was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/fhmac.dmg?v=#{version.after_comma}"
-  appcast 'https://www.francochinois.com/update/frhelper_mac.xml'
+  appcast 'https://www.eudic.net/v4/fr/app/download',
+          configuration: version.after_comma
   name 'Frhelper'
   name '法语助手'
   homepage 'https://www.eudic.net/v4/fr/app/frhelper'
