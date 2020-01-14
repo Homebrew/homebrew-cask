52f642d5bd68b71b58d25b6633378569cc53a032
suschizu <brigitte@corecode.io>
2020-01-14 23:17:16 +0100

    Update busycontacts (#75583)

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask 'busycontacts' do
   version '1.4.3,140314'
-  sha256 '8769434289d3f8622b2be19dbfeacbd44850d672697a44611e1e7c802f5241b1'
+  sha256 'e30f6540f38fd4ad13664d8f0126b835cda96cadfec48ec7af50bd11823285ba'
 
   url 'https://www.busymac.com/download/BusyContacts.zip'
   appcast 'https://www.busymac.com/busycontacts/news.plist'
