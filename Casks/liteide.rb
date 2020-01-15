b32ef9f8e401813cf71075f58813daceb35cb5ab
Vítor Galvão <info@vitorgalvao.com>
2020-01-15 20:42:41 +0000

    Update liteide.rb

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -2,11 +2,10 @@ cask 'liteide' do
   version '36.3'
   sha256 '61b028c893dcbabf2412d9c76d264208e1a29636b25dafeacb34f9fc73109b55'
 
-  # github.com/visualfc/liteide was verified as official when first introduced to the cask
   url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.12.5.zip"
   appcast 'https://github.com/visualfc/liteide/releases.atom'
   name 'LiteIDE'
-  homepage 'http://liteide.org/'
+  homepage 'https://github.com/visualfc/liteide'
 
   depends_on macos: '>= :sierra'
 
