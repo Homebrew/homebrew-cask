ed60eaf4427a455892676a87b89bbf33588a7023
Lennart Lövstrand <lennart.github@lovstrand.com>
2020-01-14 23:17:32 -0800

    Update vuescan (#75593)

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.7.20'
-  sha256 '7edee0af776000dbd1c99c93e53403d591d30dc4c4104e093d2736b217e4d4e1'
+  sha256 'a2bb4278f51971f91aa535f204fc1050de58488700468d5d14fd3e1cc57d79ee'
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
