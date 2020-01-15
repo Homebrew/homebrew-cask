883d5209d591a2cd8972e7bc5c21993ee3076e13
Ralph Martin <ralph@cs.cf.ac.uk>
2020-01-15 11:33:10 +0000

    Update amadeus-pro from 2.7 to 2.7.1 (#75597)

--- Casks/amadeus-pro.rb
+++ Casks/amadeus-pro.rb
@@ -1,6 +1,6 @@
 cask 'amadeus-pro' do
-  version '2.7'
-  sha256 '5895966e7fd05f3718012c272a895691942652d900bb49ca04bc90162f51cdc2'
+  version '2.7.1'
+  sha256 '4b49c708442f4f2614224449630541ba57c189ff7a2f8966cb33d49c10224ec4'
 
   # s3.amazonaws.com/AmadeusPro2 was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/AmadeusPro2/AmadeusPro.zip'
