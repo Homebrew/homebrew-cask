bad544b2955ee9d80438a29cb72202afd41f749a
zcutlip <uid000@gmail.com>
2020-01-14 16:20:23 -0800

    jtool2: changed version to :latest, hash to :no_check (#75547)
    * Changed version to :latest, hash to :no_check
    
    Can remove hash checking for now until nightly builds stop?
    I'll ask the author if he's willing to cut a "jtool2_1.0.tgz" release and do a
    separate "-nightly" release.
    
    * Update jtool2.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/jtool2.rb
+++ Casks/jtool2.rb
@@ -1,8 +1,7 @@
 cask 'jtool2' do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  # "version" is based on the release date as noted in WhatsNew.txt
-  version '2020.01.01'
-  sha256 '88ab8587a8528238b3c27605d9581fc97b43850c9aa62ad83c63b1674ce7e52b'
+  version :latest
+  sha256 :no_check
 
   url 'http://newosxbook.com/tools/jtool2.tgz'
   name 'jtool2'
