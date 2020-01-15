047afa6b272a4e607da16eb652bd4b66f69c8eac
Stephen <stephengroat@users.noreply.github.com>
2020-01-15 14:14:39 -0800

    encryptme: add auto_updates

--- Casks/encryptme.rb
+++ Casks/encryptme.rb
@@ -1,6 +1,7 @@
 cask 'encryptme' do
   version '4.2.0'
   sha256 '23b9e7f15eeacc7e24f4301585e391371b95fa643150b1b84edaac0ee64adcd9'
+  auto_updates true
 
   url "https://static.encrypt.me/downloads/osx/updates/Release/EncryptMe-#{version}.dmg"
   appcast 'https://www.getcloak.com/updates/osx/public/'
