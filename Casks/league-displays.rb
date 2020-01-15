a62bed07803965b779b23d7cb9a9b747383c4285
suschizu <brigitte@corecode.io>
2020-01-15 01:21:07 +0100

    Update league-displays.rb (#75582)

--- Casks/league-displays.rb
+++ Casks/league-displays.rb
@@ -1,9 +1,10 @@
 cask 'league-displays' do
-  version :latest
-  sha256 :no_check
+  version '1.0.1028-beta'
+  sha256 'cdab7e91f68f8c0e9bad5a84e4f739ed5966e7b7dd5fbe0458612fe2af01ab90'
 
   # screensaver.riotgames.com was verified as official when first introduced to the cask
-  url 'https://screensaver.riotgames.com/v2/installer/latest/League%20Displays.dmg'
+  url "https://screensaver.riotgames.com/v2/installer/latest/LeagueDisplays-#{version}.dmg"
+  appcast 'http://screensavers.riotgames.com/v2/installer/latest/appcast-mac.xml'
   name 'League Displays'
   homepage 'https://leagueoflegends.com/en/featured/league-displays'
 
