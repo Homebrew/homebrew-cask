4645014e062fb7646aad2e4a0237d9cf544fad3e
Simon Meusel <info@simonmeusel.de>
2020-01-15 13:41:58 +0000

    Update mutespotifyads to v1.11.2

--- Casks/mutespotifyads.rb
+++ Casks/mutespotifyads.rb
@@ -1,6 +1,6 @@
 cask 'mutespotifyads' do
-  version '1.11.0'
-  sha256 '56ba27f5ab292fbba8db7ff0e643cd9edf1b60ea24ac3724b6a047c2b73a8db8'
+  version '1.11.2'
+  sha256 '8285a957ee67918f0ca08b50e183717a89ec689b502155b08acb6cd913eb9dff'
 
   url "https://github.com/simonmeusel/MuteSpotifyAds/releases/download/v#{version}/MuteSpotifyAds.app.zip"
   appcast 'https://github.com/simonmeusel/MuteSpotifyAds/releases.atom'
