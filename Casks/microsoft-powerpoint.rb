0fa8f17d0afd6d7cb02d11436a1ef3ec5f3285ef
Qi Zhang <singularitti@outlook.com>
2020-01-14 23:23:48 -0500

    Update microsoft-powerpoint from 16.32.19120802 to 16.33.20011301

--- Casks/microsoft-powerpoint.rb
+++ Casks/microsoft-powerpoint.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-powerpoint' do
-  version '16.32.19120802'
-  sha256 '404a7017cf9b2e92a0d50ca32b33304ed84d83cf20eaf7f882520f4239af1323'
+  version '16.33.20011301'
+  sha256 '0b971687dda98f37eb63c07d06ed4069c3722d0fd0dc9aa35520d8ac8fef9a47'
 
   # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_PowerPoint_#{version}_Installer.pkg"
