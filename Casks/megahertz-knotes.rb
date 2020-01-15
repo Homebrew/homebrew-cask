e98af019e95d89e9919809ee6267dff5bec8c7e3
Randall <ran.dall@icloud.com>
2020-01-15 10:51:16 -0800

    Update megahertz-knotes from 2.1.0 to 2.2.0

--- Casks/megahertz-knotes.rb
+++ Casks/megahertz-knotes.rb
@@ -1,6 +1,6 @@
 cask 'megahertz-knotes' do
-  version '2.1.0'
-  sha256 'd2070cfc96cf168db45e3390866950d1eabb5fdf719bfc85ca558cd68dc151fb'
+  version '2.2.0'
+  sha256 '24cf81de4d27bc7001b06cf86b3933bd25d3a29cab675d907d5bef3ccabca900'
 
   url "http://cdn.knotesapp.cn/download/%e7%b3%af%e8%af%8d%e7%ac%94%e8%ae%b0-#{version}.dmg"
   appcast "https://knotes#{version.major}-release-cn.s3.amazonaws.com/mac/latest-mac.yml"
