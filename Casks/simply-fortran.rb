2880ec3b2d391293a8842df6eb4d156be5e98651
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-15 11:03:41 -0800

    Update simply-fortran from 3.7.3131 to 3.8.3148 (#75635)

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -1,6 +1,6 @@
 cask 'simply-fortran' do
-  version '3.7.3131'
-  sha256 '4c106a3366b9ecdc156beb474aeb3c3be74a1dbc9a6cada7e0e996b20066b5cb'
+  version '3.8.3148'
+  sha256 '9c8ae9916d276632365a52659346fce7b9100aff754d189de6bd4770194f86a7'
 
   # download.approximatrix.com/simplyfortran was verified as official when first introduced to the cask
   url "http://download.approximatrix.com/simplyfortran/#{version.major_minor}/simplyfortran-#{version}.dmg"
