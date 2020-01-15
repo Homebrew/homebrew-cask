a7e9087a609260ccaa38a2f758d1fbedfa1775da
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-14 16:49:04 -0800

    Update cisdem-document-reader from 5.0.0 to 5.1.0 (#75564)
    * Update cisdem-document-reader from 5.0.0 to 5.1.0
    
    * Update cisdem-document-reader.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/cisdem-document-reader.rb
+++ Casks/cisdem-document-reader.rb
@@ -1,5 +1,5 @@
 cask 'cisdem-document-reader' do
-  version '5.0.0'
+  version '5.1.0'
   sha256 'e621f103edcd8816ca72a1b73cdebaea00e0300f9cf506bcae00178d5fb5965f'
 
   url 'http://download.cisdem.com/cisdem-documentreader.dmg'
