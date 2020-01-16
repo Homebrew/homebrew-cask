9362c8c4b24fb76dfc1d6038e25febdecf9c76e1
tiiiecherle <tiiiecherle@gmail.com>
2020-01-16 19:18:54 +0100

    Update signal from 1.29.4 to 1.29.5 (#75678)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,6 +1,6 @@
 cask 'signal' do
-  version '1.29.4'
-  sha256 '2e7c6ccda1d569dfbb8daa266c351f384610cf5cb6bc1f4d5f40a5bb5565bf23'
+  version '1.29.5'
+  sha256 'a9144443c7a70658b8caaee6c3cd9d149209884f650b8bb8a6902526f87c0d83'
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.dmg"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
