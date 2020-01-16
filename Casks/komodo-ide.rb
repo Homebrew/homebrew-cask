1f483cf3e665cd1442b5c6657c394d4492cb0765
suschizu <brigitte@corecode.io>
2020-01-16 15:51:18 +0100

    Update komodo-ide from 11.1.1,91089 to 12.0.0,91824 (#75667)

--- Casks/komodo-ide.rb
+++ Casks/komodo-ide.rb
@@ -1,6 +1,6 @@
 cask 'komodo-ide' do
-  version '11.1.1,91089'
-  sha256 'a522a104da9023311955cfe4ebf3c4e50f7f709e58432a47ea0c4a829d7b2335'
+  version '12.0.0,91824'
+  sha256 '361abef403feaf8f00f504ea8fdbd1d587c1ce720d640493d233868fb58aeb61'
 
   url "https://downloads.activestate.com/Komodo/releases/#{version.before_comma}/Komodo-IDE-#{version.before_comma}-#{version.after_comma}-macosx-x86_64.dmg"
   appcast 'https://www.activestate.com/komodo-ide/downloads/ide'
