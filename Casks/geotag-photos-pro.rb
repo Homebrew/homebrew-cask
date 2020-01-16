5705e0d489f555ba354772a970452bb1d8a643f7
Matthew Gleich <matthewgleich@gmail.com>
2020-01-16 16:49:56 -0500

    Add Geotag Photos Pro 2 v1.5.3 cask

---
+++ Casks/geotag-photos-pro.rb
@@ -0,0 +1,11 @@
+cask 'geotag-photos-pro' do
+  version '1.5.3'
+  sha256 '36093c034a77cf54d9af0b0e886f058b53447b6766d572ae858a3519d73001fd'
+
+  # Check https://github.com/tappytaps/geotag-desktop-app/releases for version num
+  url 'https://www.geotagphotos.net/download?type=mac'
+  name 'Geotag Photos Pro 2'
+  homepage 'https://www.geotagphotos.net/#'
+
+  app 'Geotag Photos Pro 2.app'
+end
