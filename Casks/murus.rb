64c6058e131d96fc2f6843310637c8ec6c861c6c
jonasthiel <41738638+jonasthiel@users.noreply.github.com>
2020-01-16 20:43:14 +0100

    Update murus.rb

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -2,7 +2,7 @@ cask 'murus' do
   version '2.0'
   sha256 'ca8d475f28b3cb4c1a2e5e1e65fb74be2e6575b8f9ab8541b9d1a9c1b6aac16c'
 
-  # https://github.com/TheMurusTeam/ was verified as official when first introduced to the cask
+  # https://github.com/TheMurusTeam was verified as official when first introduced to the cask
   url "https://github.com/TheMurusTeam/Murus/releases/download/v2.0/murus-#{version}.zip"
   name 'Murus Firewall'
   homepage 'https://www.murusfirewall.com/'
