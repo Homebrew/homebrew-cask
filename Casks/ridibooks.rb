d90d1a66dcb814d2383d347dc3b79afba1f704cc
Vítor Galvão <info@vitorgalvao.com>
2020-01-16 15:11:01 +0000

    Update ridibooks.rb

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -2,9 +2,9 @@ cask 'ridibooks' do
   version '0.7.2'
   sha256 '1092abf977976a664ce32fd432e54ed8da1d72f0a6b5843df372a8ec27774395'
 
-  # ridicdn.net/pc_electron was verified as official when first introduced to the cask
+  # viewer-ota.ridicdn.net/pc_electron was verified as official when first introduced to the cask
   url "https://viewer-ota.ridicdn.net/pc_electron/Ridibooks-#{version}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://getapp.ridibooks.com/mac'
+  appcast 'https://s3-ap-northeast-2.amazonaws.com/viewer-ota.ridicdn.net/pc_electron/latest-mac.yml'
   name 'Ridibooks'
   homepage 'https://ridibooks.com/support/app/download'
 
