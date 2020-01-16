2e0b52fb90a7c3fb3dccf54a9bcfb09e843e247d
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2020-01-16 09:18:10 -0600

    Create microsoft-edge (#75644)
    * Create microsoft-edge
    
    * Update microsoft-edge
    
    Thanks to @peterlewis for the suggested revisions.
    
    * Update microsoft-edge
    
    Accidentally removed zap for the trash.
    
    * Update microsoft-edge
    
    Revised for including 'zap' of MAU related files.
    
    * Update and rename microsoft-edge to microsoft-edge.rb
    
    LOL. I'm tired from trying to juggle school and work. Thanks.

---
+++ Casks/microsoft-edge.rb
@@ -0,0 +1,30 @@
+cask 'microsoft-edge' do
+  version '79.0.309.65'
+  sha256 '67b1e8e036c575782b1c9188dd48fa94d9eabcb81947c8632fd4acac7b01644b'
+
+  # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdge-#{version}.pkg"
+  appcast 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDGE01-chk.xml',
+          configuration: "#{version.major}.#{version.patch}"
+  name 'Microsoft Edge'
+  homepage 'https://www.microsoft.com/edge'
+
+  auto_updates true
+
+  pkg "MicrosoftEdge-#{version}.pkg"
+
+  uninstall pkgutil: 'com.microsoft.edgemac',
+            rmdir:   '/Library/Application Support/Microsoft'
+
+  zap launchctl: [
+                   'com.microsoft.autoupdate.helper',
+                   'com.microsoft.update.agent',
+                 ],
+      pkgutil:   'com.microsoft.package.Microsoft_AutoUpdate.app',
+      delete:    '/Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper',
+      trash:     [
+                   '~/Library/Preferences/com.microsoft.edgemac.plist',
+                   '/Library/Application Support/Microsoft',
+                   '~/Library/Application Support/Microsoft Edge',
+                 ]
+end
