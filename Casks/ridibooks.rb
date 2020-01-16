03970ee618b47bec41fe88698f778fc602fd2016
Work <brigitte@corecode.io>
2020-01-16 15:31:59 +0100

    Update ridibooks

--- Casks/ridibooks.rb
+++ Casks/ridibooks.rb
@@ -3,7 +3,7 @@ cask 'ridibooks' do
   sha256 '1aec169de07735513fa37c2329e660db0a9a47f53c40e25d9a8c145d255f5da4'
 
   url "https://viewer-ota.ridibooks.com/mac/ridibooks-#{version}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://getapp.ridibooks.com/mac'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://getapp.ridibooks.com/mac-old'
   name 'Ridibooks'
   homepage 'https://ridibooks.com/support/app/download'
 
