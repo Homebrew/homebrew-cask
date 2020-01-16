45555150838e3f91cdeac96908a60985218e2ec8
Vítor Galvão <info@vitorgalvao.com>
2020-01-16 14:48:24 +0000

    Update sensei.rb

--- Casks/sensei.rb
+++ Casks/sensei.rb
@@ -2,7 +2,7 @@ cask 'sensei' do
   version '1.0.0,9'
   sha256 '2ffd1b2c9788a55d1e0b4e4628c7235b542e75d1b58e973b310860e6668022bd'
 
-  # s3.amazonaws.com/cindori/ was verified as official when first introduced to the cask
+  # s3.amazonaws.com/cindori was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/cindori/Sensei.dmg'
   appcast 'https://api.appcenter.ms/v0.1/public/sparkle/apps/51fc066a-f4b4-49ec-b966-b2f476d2eede'
   name 'Sensei'
