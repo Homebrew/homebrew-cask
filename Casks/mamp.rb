a210a252382254a15bacd29c5eb33dfe6ef134b8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-16 23:50:08 -0800

    Update mamp from 5.5 to 5.6 (#75719)

--- Casks/mamp.rb
+++ Casks/mamp.rb
@@ -1,6 +1,6 @@
 cask 'mamp' do
-  version '5.5'
-  sha256 'eebddf558ba956357fb6e628f13ab7bc296de3202b2382d4cbf5868aadb45ded'
+  version '5.6'
+  sha256 '0b5f8157d2a6f4cef39d3cf89563bab6503d2d1087f436f8ca9ee33731293453'
 
   url "https://downloads.mamp.info/MAMP-PRO/releases/#{version}/MAMP_MAMP_PRO_#{version}.pkg"
   appcast 'https://www.mamp.info/en/downloads/'
