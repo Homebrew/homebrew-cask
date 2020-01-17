7a1c79b279907a86825ab1c113b7cc7ac054c636
Vítor Galvão <info@vitorgalvao.com>
2020-01-17 18:41:57 +0000

    Update murus.rb

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -3,7 +3,7 @@ cask 'murus' do
   sha256 'ca8d475f28b3cb4c1a2e5e1e65fb74be2e6575b8f9ab8541b9d1a9c1b6aac16c'
 
   # github.com/TheMurusTeam was verified as official when first introduced to the cask
-  url "https://github.com/TheMurusTeam/Murus/releases/download/v2.0/murus-#{version}.zip"
+  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip"
   appcast 'https://github.com/TheMurusTeam/Murus/releases.atom'
   name 'Murus Firewall'
   homepage 'https://www.murusfirewall.com/'
