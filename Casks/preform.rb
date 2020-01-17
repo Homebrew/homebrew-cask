b6c9634de8e59d3ecdf5f02d84729a243130784e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-17 00:04:22 -0800

    Update preform from 3.3.0,1705 to 3.3.1,1717 (#75726)

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask 'preform' do
-  version '3.3.0,1705'
-  sha256 'd73392bde825111cd12694c9b8ab39140b98a5d0c957af26d3099fd6a6dec021'
+  version '3.3.1,1717'
+  sha256 'c7e9ddeb6ab15b8ee7e9c1fd9d4f3710d14783b779e186129b7e661f6e9a3d1c'
 
   # s3.amazonaws.com/FormlabsReleases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release_origin_release_#{version.before_comma}_build_#{version.after_comma}.dmg"
