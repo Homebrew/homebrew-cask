f10875f7e72bba2d6c3e427cedee62bc2257d79a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-17 00:06:37 -0800

    Update kite from 0.20200109.0 to 0.20200115.0 (#75698)

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -1,6 +1,6 @@
 cask 'kite' do
-  version '0.20200109.0'
-  sha256 '8f590a05e12bacf0015a3d2b0b5d81873cbd6ba3e3c7d15d498c74b62226f866'
+  version '0.20200115.0'
+  sha256 'a106c3e839767cc61990b45ed0fc5d632dc343227811cbf5640333847c03c73f'
 
   # kite-downloads.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://kite-downloads.s3.amazonaws.com/Kite-#{version}.dmg"
