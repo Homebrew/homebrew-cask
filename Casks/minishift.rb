8f76470369c29d57b201a9d5714d27366991aeab
David Hergert <david@hergert.us>
2020-01-17 17:20:06 -0500

    Update minishift from 1.34.1 to 1.34.2 (#75754)

--- Casks/minishift.rb
+++ Casks/minishift.rb
@@ -1,6 +1,6 @@
 cask 'minishift' do
-  version '1.34.1'
-  sha256 '1d5181b3547f88ef14372cd9c743f3fd3962d7d3ebf2915b8ab8b306af78c97d'
+  version '1.34.2'
+  sha256 '43f624d87d9ba09b5f62b8a5e1fba29f6894125b43341402b0b2a4fd52abc503'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom'
