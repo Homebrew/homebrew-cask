4e91606afa105a16d1963480bb7870b63e093705
Randall <ran.dall@icloud.com>
2020-01-18 11:18:04 -0800

    Update superproductivity from 3.0.7 to 3.1.3

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,6 +1,6 @@
 cask 'superproductivity' do
-  version '3.0.7'
-  sha256 'b718dfac0357bda25b255ed00140cfb5976ab185e1ec300b2da80a939d747ef6'
+  version '3.1.3'
+  sha256 '841721d3639e30d1f057e68a97fd9740e2a4c7c8ae81aedffc5d89c72d17de24'
 
   # github.com/johannesjo/super-productivity was verified as official when first introduced to the cask
   url "https://github.com/johannesjo/super-productivity/releases/download/v#{version}/superProductivity-#{version}-mac.zip"
