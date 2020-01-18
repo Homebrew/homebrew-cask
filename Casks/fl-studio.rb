716f8787a02ebea2dc9c64274cae9f4cfe827a60
suschizu <brigitte@corecode.io>
2020-01-18 15:37:08 +0100

    Update fl-studio from 20.6.0.793 to 20.6.1.848 (#75772)

--- Casks/fl-studio.rb
+++ Casks/fl-studio.rb
@@ -1,6 +1,6 @@
 cask 'fl-studio' do
-  version '20.6.0.793'
-  sha256 'c6756dc5a15eee40d6a490345cb4cb71ded3febeb4113a4cf64d867a277d149d'
+  version '20.6.1.848'
+  sha256 'f8e16c6f3f0ec935d71f6d7fd2d33fe1dde52d883354f4b67aa41f018c0b134d'
 
   url "http://demodownload.image-line.com/flstudio/flstudio_mac_#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://support.image-line.com/redirect/flstudio20_mac_installer'
