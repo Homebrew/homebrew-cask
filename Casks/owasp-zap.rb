330a84f6c6f09de2999328aa23617665d74180e5
kingthorin <kingthorin@users.noreply.github.com>
2020-01-18 23:46:13 -0500

    Update owasp-zap from 2.8.0 to 2.9.0 (#75763)

--- Casks/owasp-zap.rb
+++ Casks/owasp-zap.rb
@@ -1,13 +1,13 @@
 cask 'owasp-zap' do
-  version '2.8.0'
-  sha256 '8fe3c30411524a05d662c2a2d1e2762a43291db7c39b23963b7de0f259ab122c'
+  version '2.9.0'
+  sha256 'b00949bf632b16c29085ebb3eb7b0962afe9ded166f6bade8a8ce8d3dbf1b9bd'
 
   # github.com/zaproxy/zaproxy was verified as official when first introduced to the cask
   url "https://github.com/zaproxy/zaproxy/releases/download/v#{version}/ZAP_#{version}.dmg"
   appcast 'https://github.com/zaproxy/zaproxy/releases.atom'
   name 'OWASP Zed Attack Proxy'
   name 'ZAP'
-  homepage 'https://www.owasp.org/index.php/OWASP_Zed_Attack_Proxy_Project'
+  homepage 'https://www.zaproxy.org/'
 
   app 'OWASP ZAP.app'
 
