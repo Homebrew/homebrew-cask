428c0e09a89d4e99f13332e1ea1a475f65b02ffb
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-19 17:55:39 -0800

    Update qlvideo from 1.93 to 193 (#75840)

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -1,5 +1,5 @@
 cask 'qlvideo' do
-  version '1.93'
+  version '193'
   sha256 'a0bce00cc2073fe065722409d32605cc2c20eaab89981e2d965d571d84fd77e0'
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.no_dots}/QLVideo_#{version.no_dots}.pkg"
