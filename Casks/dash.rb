925c625a64281298d9782312bf34789cc59c5e17
Kapeli <support@kapeli.com>
2020-01-20 13:53:51 +0200

    Update dash to 5.1.1

--- Casks/dash.rb
+++ Casks/dash.rb
@@ -3,8 +3,8 @@ cask 'dash' do
     version '4.6.7'
     sha256 'e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6'
   else
-    version '5.1.0'
-    sha256 'b70a25f3b589e488da86dff6ac2271c22e32d953d1ed53bca713c80d593f979e'
+    version '5.1.1'
+    sha256 '28b9cb8c8e6586626697d2788a574adf605d8e1cf6cae898d7c5de27ca016be1'
   end
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
