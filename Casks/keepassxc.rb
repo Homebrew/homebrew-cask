c27c9f631147ff060cfdc3c0daf41f9edd9f02c1
Daniel Neto <daniel@Daniels-MacBook-Pro.local>
2020-01-20 00:58:51 +0000

    Update keepassxc from 2.5.2 to 2.5.3

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,6 +1,6 @@
 cask 'keepassxc' do
-  version '2.5.2'
-  sha256 '9755d3f16b98db6f8d25e02a445a0b7f4aae822e514105cec02df4c25b53f993'
+  version '2.5.3'
+  sha256 'fb9af9c2eab788e710f93165f063ed26281140266181d3904b4a6b2d62c5c9d8'
 
   # github.com/keepassxreboot/keepassxc was verified as official when first introduced to the cask
   url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg"
