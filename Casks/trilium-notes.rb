8b710197a3951e07738d430bcc33c14c7d58884f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-19 17:35:30 -0800

    Update trilium-notes from 0.39.5 to 0.40.1 (#75838)

--- Casks/trilium-notes.rb
+++ Casks/trilium-notes.rb
@@ -1,6 +1,6 @@
 cask 'trilium-notes' do
-  version '0.39.5'
-  sha256 '02ad1f7211cc322957151016a2fc4886b37c1e600eae0be944cd288de958bc83'
+  version '0.40.1'
+  sha256 '6fcb05df2310a40a41d5ac5310943817e4648468b8b4c45f9ef072acae7dc8bc'
 
   url "https://github.com/zadam/trilium/releases/download/v#{version}/trilium-mac-x64-#{version}.zip"
   appcast 'https://github.com/zadam/trilium/releases.atom'
