a4b4ac9b0b56f168393ce90c2c354dbb8b515108
suschizu <brigitte@corecode.io>
2020-01-20 17:23:34 +0100

    Update abscissa from 3.4.6 to 4.0.0 (#75864)
    * Update abscissa from 3.4.6 to 4.0.0
    
    * Update abscissa.rb

--- Casks/abscissa.rb
+++ Casks/abscissa.rb
@@ -1,11 +1,11 @@
 cask 'abscissa' do
-  version '3.4.6'
-  sha256 '319c6871e4f42eaf6b1925aff66859024241d5c11a542badeb6993d287da3da2'
+  version '4.0.0'
+  sha256 '7e4fb8b04c739342c45dacf998c2f8a020809283ce17e9da04b8bbf582349702'
 
   url "http://rbruehl.macbay.de/Abscissa/Downloads/Abscissa-#{version}.zip"
   appcast 'http://rbruehl.macbay.de/Abscissa/Downloads/'
   name 'Abscissa'
   homepage 'http://rbruehl.macbay.de/Abscissa/'
 
-  app "Abscissa-#{version}/Abscissa.app"
+  app 'Abscissa.app'
 end
