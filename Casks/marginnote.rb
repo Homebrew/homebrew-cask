91f4ffae382cc9480cd581ec348fd9e66a73bd52
suschizu <brigitte@corecode.io>
2020-01-21 12:57:45 +0100

    Update marginnote.rb

--- Casks/marginnote.rb
+++ Casks/marginnote.rb
@@ -1,10 +1,10 @@
 cask 'marginnote' do
-  version '3.5.9007,1576409180'
+  version '3.6.1002,1451f4f692144d46b91cd5898c7e42cb:6'
   sha256 '4132d81bd9b88ea1cbf05385e64431210ea03c8bba152a1a799651264eae4245'
 
-  # dl.devmate.com/QReader.MarginStudyMac was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/QReader.MarginStudyMac/#{version.before_comma}/#{version.after_comma}/MarginNote#{version.major}-#{version.before_comma}.zip"
-  appcast 'https://updates.devmate.com/QReader.MarginStudyMac.xml'
+  # rink.hockeyapp.net/api/2/apps was verified as official when first introduced to the cask
+  url "https://rink.hockeyapp.net/api/2/apps/#{version.after_comma.before_colon}/app_versions/#{version.after_colon}?format=zip"
+  appcast 'https://api.appcenter.ms/v0.1/public/sparkle/apps/1451f4f6-9214-4d46-b91c-d5898c7e42cb'
   name 'MarginNote'
   homepage 'https://www.marginnote.com/'
 
