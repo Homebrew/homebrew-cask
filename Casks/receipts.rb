d96db769f975fc714f45098aafb8731530bca236
suschizu <brigitte@corecode.io>
2020-01-21 12:38:17 +0100

    Update receipts from 1.9.6-289 to 1.9.6-291 (#75904)

--- Casks/receipts.rb
+++ Casks/receipts.rb
@@ -1,6 +1,6 @@
 cask 'receipts' do
-  version '1.9.6-289'
-  sha256 'e7d715a3e21900f090016d86b29390a9ac6ed90483dbd2d9a0e2b8317a6d90c1'
+  version '1.9.6-291'
+  sha256 '5829716ef4fd811d9508de07684b134c06d78e2c4d9b87fbae9cccf8493dde46'
 
   url "https://www.receipts-app.com/update/download/Receipts-#{version}.zip"
   appcast 'https://www.receipts-app.com/updater.php'
