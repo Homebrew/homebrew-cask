fdcb3d572d625dd2d4b472edd878b52d602ce728
Vítor Galvão <info@vitorgalvao.com>
2020-01-21 16:22:08 +0000

    Update noisebuddy.rb

--- Casks/noisebuddy.rb
+++ Casks/noisebuddy.rb
@@ -2,7 +2,7 @@ cask 'noisebuddy' do
   version '1.0'
   sha256 '6d654e208e717193bfe88e2ff7c278fecde96414cbad3d87a3206c1e78452a17'
 
-  url 'https://github.com/insidegui/NoiseBuddy/releases/download/1.0/NoiseBuddy_v1.0.zip'
+  url "https://github.com/insidegui/NoiseBuddy/releases/download/#{version}/NoiseBuddy_v#{version}.zip"
   appcast 'https://github.com/insidegui/NoiseBuddy/releases.atom'
   name 'NoiseBuddy'
   homepage 'https://github.com/insidegui/NoiseBuddy'
