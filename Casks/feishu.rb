6692a4a841d65da477b76fe9a4e04945eff8a26a
suschizu <brigitte@corecode.io>
2020-01-21 11:50:56 +0100

    Update feishu from 3.15.5 to 3.16.3 (#75902)

--- Casks/feishu.rb
+++ Casks/feishu.rb
@@ -1,6 +1,6 @@
 cask 'feishu' do
-  version '3.15.5'
-  sha256 '24092587155ae770fcffd0ccc31e699521cdf77fcd73ab0eafa073b12e47c893'
+  version '3.16.3'
+  sha256 '86417ea4b1ae948e0df9711245a7a8a70c9747a3793fac9d0a4c965e42cce4b1'
 
   # sf3-ttcdn-tos.pstatp.com was verified as official when first introduced to the cask
   url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/Feishu-#{version}.dmg"
