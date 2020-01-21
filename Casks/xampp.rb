980c2f7a65b9a7506843a9d5dadbfbe2da4b7c88
mmoehring <mmoehring@users.noreply.github.com>
2020-01-22 00:36:25 +0100

    Update xampp from 7.3.11,0 to 7.4.1,0 (#75927)

--- Casks/xampp.rb
+++ Casks/xampp.rb
@@ -1,6 +1,6 @@
 cask 'xampp' do
-  version '7.3.11,0'
-  sha256 'cf6a2ddedd64955080e09a6279cbda78569a21ba8d017bd035f73a47b8f9da5b'
+  version '7.4.1,0'
+  sha256 '4b0b6829f2b1152193b02e379e6fac8eeb208afb1de6b80b0387f01b61c98cef'
 
   # downloadsapachefriends.global.ssl.fastly.net/xampp-files was verified as official when first introduced to the cask
   url "https://downloadsapachefriends.global.ssl.fastly.net/xampp-files/#{version.before_comma}/xampp-osx-#{version.before_comma}-#{version.after_comma}-installer.dmg"
