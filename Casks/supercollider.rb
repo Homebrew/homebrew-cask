53a3f90540ccf8bc038959db00d8455eb6a172e2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-21 17:54:05 -0800

    Update supercollider from 3.10.3 to 3.10.4 (#75944)

--- Casks/supercollider.rb
+++ Casks/supercollider.rb
@@ -1,6 +1,6 @@
 cask 'supercollider' do
-  version '3.10.3'
-  sha256 '7adaff8916f9db6f9ee839cb0520e84c29a18244a62e97ef172759cf6cfa7311'
+  version '3.10.4'
+  sha256 'ad4558c8515b4e5fca442d9a889e212060209b8435e61906d0d840d00eef750f'
 
   # github.com/supercollider/supercollider was verified as official when first introduced to the cask
   url "https://github.com/supercollider/supercollider/releases/download/Version-#{version}/SuperCollider-#{version}-macOS-signed.zip"
