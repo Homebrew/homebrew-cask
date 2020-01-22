b0a24094bfc67feb193478442e949978b8af017c
CoreCode <core-code@users.noreply.github.com>
2020-01-22 23:49:29 +0100

    Update goldencheetah.rb (#76000)

--- Casks/goldencheetah.rb
+++ Casks/goldencheetah.rb
@@ -1,6 +1,6 @@
 cask 'goldencheetah' do
   version '3.5'
-  sha256 'b9a4e4575783c0f043260c12ddb5d02272bc566dbb2a31109b4d5166f7025fcd'
+  sha256 '02518bee5427ec126aa2fdb3d3c6d236c03dd25a78623c36e8f3d27080028f2a'
 
   # github.com/GoldenCheetah/GoldenCheetah was verified as official when first introduced to the cask
   url "https://github.com/GoldenCheetah/GoldenCheetah/releases/download/V#{version}/GoldenCheetah_v#{version}_64bit_MacOS.dmg"
