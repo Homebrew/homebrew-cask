eda1ad80de60e07200358825ffaf42e79cbd6a47
CoreCode <core-code@users.noreply.github.com>
2020-01-22 23:14:28 +0100

    Update sococo.rb

--- Casks/sococo.rb
+++ Casks/sococo.rb
@@ -3,7 +3,7 @@ cask 'sococo' do
   sha256 'e235a6a98c8a83eaa3e379c01eee51370f643846f6a8a594ad91f7a182c7eeec'
 
   url "https://s.sococo.com/rs/client/mac/#{version.after_comma}-SococoProd.dmg"
-  appcast 'https://app.sococo.com/a/download',
+  appcast 'https://app.sococo.com/app/app.min.js',
           configuration: version.after_comma
   name 'Sococo'
   homepage 'https://www.sococo.com/'
