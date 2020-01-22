8d30919e1b27d1f0ab85d54c97fe916daea8ca3e
Vítor Galvão <info@vitorgalvao.com>
2020-01-22 00:33:18 +0000

    Update geotag-photos-pro.rb

--- Casks/geotag-photos-pro.rb
+++ Casks/geotag-photos-pro.rb
@@ -2,10 +2,11 @@ cask 'geotag-photos-pro' do
   version '1.5.4'
   sha256 '0c84ee5c1027f9793fd372b517d440f20a534b09ebfdff3452bbb65542f340c0'
 
-  # Check https://github.com/tappytaps/geotag-desktop-app/releases for version num
-  url 'https://www.geotagphotos.net/download?type=mac'
-  name 'Geotag Photos Pro 2'
-  homepage 'https://www.geotagphotos.net/#'
+  # github.com/tappytaps/geotag-desktop-app was verified as official when first introduced to the cask
+  url "https://github.com/tappytaps/geotag-desktop-app/releases/download/v#{version}/Geotag-Photos-Pro-2-#{version}.dmg"
+  appcast 'https://github.com/tappytaps/geotag-desktop-app/releases.atom'
+  name 'Geotag Photos Pro'
+  homepage 'https://www.geotagphotos.net/'
 
   app 'Geotag Photos Pro 2.app'
 end
