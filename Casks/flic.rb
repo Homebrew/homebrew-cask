6d8f025409b0db76dbfc0f0d7fe6d8a28861c7a6
suschizu <brigitte@corecode.io>
2020-01-22 17:31:05 +0100

    Update flic.rb from 1.1.3,2018-09-11 13-14-23 to 2.0.0 + changes (#75966)
    * Update flic.rb
    
    * Update flic.rb

--- Casks/flic.rb
+++ Casks/flic.rb
@@ -1,12 +1,12 @@
 cask 'flic' do
-  version '1.1.3,2018-09-11 13-14-23'
-  sha256 '400b1aed904b527b049af26c61aca234a6812e7f3d366ecb471a5b8fe6646ae1'
+  version '2.0.0'
+  sha256 '5b7a2cac9772723f7ddd9d8a54be21c094c35851ade650384277e6b9770043d6'
 
   # misc-scl-cdn.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://misc-scl-cdn.s3.amazonaws.com/Flic.#{version.before_comma}.zip"
-  appcast 'https://flic.io/mac-app'
-  name 'FlicMac Developer Distribution'
+  url "https://misc-scl-cdn.s3.amazonaws.com/Flic.#{version}.zip"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://flic.io/mac-app/download'
+  name 'Flic'
   homepage 'https://flic.io/mac-app'
 
-  app "FlicMac Developer Distribution #{version.after_comma}/Flic.app"
+  app 'Flic.app'
 end
