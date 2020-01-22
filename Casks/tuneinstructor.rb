5638d14e3e9d6415f9d7022b2b534ea1c1a08132
Vítor Galvão <info@vitorgalvao.com>
2020-01-22 17:16:32 +0000

    Update tuneinstructor.rb

--- Casks/tuneinstructor.rb
+++ Casks/tuneinstructor.rb
@@ -2,7 +2,7 @@ cask 'tuneinstructor' do
   version '3.7v5,17467'
   sha256 '71e03619dfe41e38a1fe34fc320a6ecb89bc68b54febd78c229c86255b0d1726'
 
-  url "https://www.tune-instructor.de/resources/downloads/TuneInstructor#{version.before_comma}.dmg"
+  url "https://www.tune-instructor.de/resources/downloads/TuneInstructor#{version.before_comma}.zip"
   appcast 'https://www.tune-instructor.de/de/download.html'
   name 'Tune•Instructor'
   homepage 'https://www.tune-instructor.de/com/start.html'
