600a1f50d2af17807463fb0e66098a8a8e7b979b
Julian Mayer <julian@corecode.io>
2020-01-22 23:02:12 +0100

    Update jubler

--- Casks/jubler.rb
+++ Casks/jubler.rb
@@ -1,9 +1,9 @@
 cask 'jubler' do
   version '7.0.0-alpha1'
-  sha256 'a3028e4a9a3a345b5f6d8bca6fa6d56527fe5a8721bb9f3ff10417d42096aa85'
+  sha256 '27247aca18009d1f42531b6059d2a7418795b1fdda515a560562cfa08a670379'
 
   # github.com/teras/Jubler was verified as official when first introduced to the cask
-  url "https://github.com/teras/Jubler/releases/download/v#{version}/Jubler-#{version}.dmg"
+  url "https://github.com/teras/Jubler/releases/download/v#{version}/Jubler-#{version}.1.dmg"
   appcast 'https://github.com/teras/Jubler/releases.atom'
   name 'Jubler'
   homepage 'https://www.jubler.org/'
