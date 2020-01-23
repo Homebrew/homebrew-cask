833c041d4237f449a9466d4276500919247587eb
suschizu <brigitte@corecode.io>
2020-01-23 16:10:39 +0100

    Update twitch.rb (#76056)

--- Casks/twitch.rb
+++ Casks/twitch.rb
@@ -2,8 +2,8 @@ cask 'twitch' do
   version :latest
   sha256 :no_check
 
-  # updates.twitchapp.net was verified as official when first introduced to the cask
-  url 'https://updates.twitchapp.net/mac/Twitch.dmg'
+  # desktop.twitchsvc.net/installer was verified as official when first introduced to the cask
+  url 'https://desktop.twitchsvc.net/installer/mac/Twitch.dmg'
   name 'Twitch'
   homepage 'https://app.twitch.tv/'
 
