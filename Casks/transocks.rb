715e3f4ec6233c8724dd3b539ea5ead040e4e096
Zhenfu Shi <szf1234@me.com>
2020-01-23 13:53:10 -0500

    Add transocks v2.4.2 (#76008)
    * Add transocks v2.4.2
    
    * Update transocks.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/transocks.rb
@@ -0,0 +1,24 @@
+cask 'transocks' do
+  version '2.4.2'
+  sha256 'f53cee427d8b48ff124007476a240d78a3a7a4381da10157452d2a5b0d1f0bb1'
+
+  url "https://www.transocks.com/download/mac/Transocks_Mac_#{version}.pkg"
+  appcast 'https://www.transocks.com/js/data.js'
+  name 'Transocks'
+  homepage 'https://www.transocks.com/'
+
+  pkg "Transocks_Mac_#{version}.pkg"
+
+  uninstall pkgutil: 'com.穿梭Transocks'
+
+  zap trash: [
+               '/Library/Logs/DiagnosticReports/穿梭Transocks*',
+               '~/Library/Application Support/CrashReporter/穿梭Transocks*',
+               '/Users/*/.transocks_vendor',
+               '/Users/*/.transocks_store.tmp',
+               '/Users/*/.transocks.log',
+               '~/Library/Logs/穿梭Transocks',
+               '~/Library/Preferences/com.github.Electron.plist',
+               '~/Library/Saved Application State/com.github.Electron.savedState',
+             ]
+end
