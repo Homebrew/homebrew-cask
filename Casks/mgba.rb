4ebea48720b7ffd14610b2b9afc428acdff4f6a6
Randall <ran.dall@icloud.com>
2020-01-22 19:07:16 -0800

    Update mgba from 0.7.3 to 0.8.0

--- Casks/mgba.rb
+++ Casks/mgba.rb
@@ -1,6 +1,6 @@
 cask 'mgba' do
-  version '0.7.3'
-  sha256 '9d36dc2d09afcefde615f3894ecdcc24dfb577ad926912841a4cca83f3728814'
+  version '0.8.0'
+  sha256 'c3322c3592e932b14612f7635f585fbbd72157bfa93d30717d3967f95fdc1aec'
 
   # github.com/mgba-emu/mgba was verified as official when first introduced to the cask
   url "https://github.com/mgba-emu/mgba/releases/download/#{version}/mGBA-#{version}-osx.tar.xz"
