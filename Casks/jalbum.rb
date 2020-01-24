e2c0c8860192ee6c46294d8b1e0ebbc9010f47cc
Randall <ran.dall@icloud.com>
2020-01-23 17:24:52 -0800

    Update jalbum from 19.2 to 19.3

--- Casks/jalbum.rb
+++ Casks/jalbum.rb
@@ -1,6 +1,6 @@
 cask 'jalbum' do
-  version '19.2'
-  sha256 '36f77b67de434d14b3845e13df03a89a4aba27c63dc54e09122322091e6cbcc0'
+  version '19.3'
+  sha256 '30dad701def8b5477d0101668a7dd0b06ce015d43f5ae9fbe1cd40f6baea9f95'
 
   url "https://download.jalbum.net/download/#{version}/MacOSX/jAlbum.dmg"
   appcast 'https://jalbum.net/en/software/download/previous',
