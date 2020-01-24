1aff5eecc9e41a7b235d2ecb7b6de1027316af28
someposer <someposer@users.noreply.github.com>
2020-01-24 10:24:12 -0600

    Updating perforce version to 19.2,1908095. (#75988)
    Updated sha256 and url to match.
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/perforce.rb
+++ Casks/perforce.rb
@@ -1,8 +1,8 @@
 cask 'perforce' do
-  version '19.2'
+  version '19.2,1908095'
   sha256 '28dee91966659dab0a749ba22e38df00c53691e4ce34ecb24a8199a4c816d45b'
 
-  url "https://cdist2.perforce.com/perforce/r#{version}/bin.macosx1010x86_64/helix-core-server.tgz"
+  url "https://cdist2.perforce.com/perforce/r#{version.before_comma}/bin.macosx1010x86_64/helix-core-server.tgz"
   name 'Perforce Helix Versioning Engine'
   homepage 'https://www.perforce.com/'
 
