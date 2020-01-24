dd18480218179dac72070fe3ad0f62388c192764
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-23 17:23:10 -0800

    Update chronicle from 9.5.3 to 9.6.0 (#76076)

--- Casks/chronicle.rb
+++ Casks/chronicle.rb
@@ -1,6 +1,6 @@
 cask 'chronicle' do
-  version '9.5.3'
-  sha256 '7f184305a955ad1806f313b46d380b6b41f607f63f19d92677446a48686a828b'
+  version '9.6.0'
+  sha256 '05ef8c479a3673c03442ab635a69a7f66dbf2ca19826b77bfd15c035d51f0534'
 
   url 'https://www.chronicleapp.com/static/downloads/chroniclepro.zip'
   appcast 'http://www.littlefin.com/downloads/chronicle8.xml'
