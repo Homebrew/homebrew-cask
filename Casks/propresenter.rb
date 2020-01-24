de616c30c0400d1c78f281f6d78acb7b15daff64
Vítor Galvão <info@vitorgalvao.com>
2020-01-24 20:02:58 +0000

    Update propresenter.rb

--- Casks/propresenter.rb
+++ Casks/propresenter.rb
@@ -3,13 +3,13 @@ cask 'propresenter' do
   sha256 '047292587f7e7b558f02e8012d5e7189886e7262336ba1886fed90aa29d93236'
 
   url "https://renewedvision.com/downloads/propresenter/mac/ProPresenter_#{version.before_comma}_#{version.after_comma}.zip"
-  appcast 'https://renewedvision.com/propresenter/'
+  appcast 'https://api.renewedvision.com/v1/pro/upgrade?platform=macos&osVersion=0&appVersion=0&buildNumber=0&includeNotes=0'
   name 'ProPresenter'
   homepage 'https://www.renewedvision.com/propresenter.php'
 
   depends_on macos: '>= :high_sierra'
 
-  app "ProPresenter #{version.major}.app"
+  app 'ProPresenter.app'
 
   zap trash: [
                '~/Library/Application Support/RenewedVision/ProPresenter6',
