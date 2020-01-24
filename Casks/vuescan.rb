eb5bc85d23a3df8b4c97ff3a584ff8efccf61ce8
Yaw Anokwa <yanokwa@users.noreply.github.com>
2020-01-24 12:05:07 -0800

    Suppress checksum verification in Vuescan (#76094)
    * Update vuescan.rb
    
    * Update vuescan.rb
    
    * Update vuescan.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,6 +1,6 @@
 cask 'vuescan' do
   version '9.7.20'
-  sha256 '16660c55efb26ec409f2e6cca1edfe5d4e76e8943474f515bf426a6b8d3f4bec'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.hamrick.com/files/vuex64#{version.major_minor.no_dots}.dmg"
   appcast 'https://www.hamrick.com/alternate-versions.html'
