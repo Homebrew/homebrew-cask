f116e8ddd4fac5359d1a75c5f89e63d73786ce1c
Randall <ran.dall@icloud.com>
2020-01-24 16:39:43 -0800

    Update 4k-video-downloader from 4.10.1.3240 to 4.11.1.3390

--- Casks/4k-video-downloader.rb
+++ Casks/4k-video-downloader.rb
@@ -1,6 +1,6 @@
 cask '4k-video-downloader' do
-  version '4.10.1.3240'
-  sha256 '61e6e7f6626474aeb08aea244f61e792fc1b4fbbc9e440ef89908f51299f10c9'
+  version '4.11.1.3390'
+  sha256 '39f744c3de3ffcbb3a6238cd1d85ffe7f8f970eb61bed87f9d98deac5f5e9d4b'
 
   url "https://dl.4kdownload.com/app/4kvideodownloader_#{version.major_minor_patch}.dmg"
   appcast 'https://www.4kdownload.com/download'
