c5c262c4b3f6da36f01c35ef3b2ce18a2b075480
Paul Brit <paulbrit44@gmail.com>
2020-01-24 22:38:24 -0700

    Add Prezi Next v1.40.0

---
+++ Casks/prezi-next.rb
@@ -0,0 +1,10 @@
+cask 'prezi-next' do
+  version '1.40.0'
+  sha256 '3ad61662a6db86fee8adec634f3f0884650394d5b5169327a90d0dbf31a6b4e3'
+
+  url "https://desktopassets.prezi.com/mac/pitch/releases/Prezi_Next_#{version}.dmg"
+  name 'Prezi Next'
+  homepage 'https://prezi.com/'
+
+  app 'Prezi Next.app'
+end
