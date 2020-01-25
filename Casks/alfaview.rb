97db2b78f55850929dc168c1645e0dba7b28b7a9
Work <brigitte@corecode.io>
2020-01-25 17:56:39 +0100

    Update alfaview from 7.51964 to 8.1.2

--- Casks/alfaview.rb
+++ Casks/alfaview.rb
@@ -1,6 +1,6 @@
 cask 'alfaview' do
-  version '7.51964'
-  sha256 'ce45be5b71abb4f46289d2909cc725aec7d285ec85e41963611c78f49c3b2697'
+  version '8.1.2'
+  sha256 'b0f6e9374414f807c79cd9dadf86c08890a41dfd0d01202667e21574a48d8175'
 
   url "https://assets.alfaview.com/stable/mac/alfaview-mac-production-#{version}.dmg"
   appcast 'https://alfaview.com/downloads'
