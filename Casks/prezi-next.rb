0de8aed1c7049fd8b96379837a47068b021725df
Paul Brit <paulbrit44@gmail.com>
2020-01-25 09:19:52 -0700

    Add Prezi Next v1.40.0 (#76152)
    * Add Prezi Next v1.40.0
    
    * Update prezi-next.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/prezi-next.rb
@@ -0,0 +1,11 @@
+cask 'prezi-next' do
+  version '1.40.0'
+  sha256 '3ad61662a6db86fee8adec634f3f0884650394d5b5169327a90d0dbf31a6b4e3'
+
+  url "https://desktopassets.prezi.com/mac/pitch/releases/Prezi_Next_#{version}.dmg"
+  appcast 'https://s3.amazonaws.com/prezidesktop/assets/mac/pitch/updates/prezi-business.xml'
+  name 'Prezi Next'
+  homepage 'https://prezi.com/'
+
+  app 'Prezi Next.app'
+end
