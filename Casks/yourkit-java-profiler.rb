a4093bd4c1caf24b00006555ef94eada26612b74
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-24 17:18:36 -0800

    Update yourkit-java-profiler from 2019.8,b136 to 2019.8,b137 (#76150)

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,6 +1,6 @@
 cask 'yourkit-java-profiler' do
-  version '2019.8,b136'
-  sha256 '77b76d7588677b49554c964a3faf429c0faf12bca15e56d0899b57284443dd90'
+  version '2019.8,b137'
+  sha256 'efe7e4cefc1e6f73802e412fd6e0e13237c81f313a797d9f1b3ba22f3d210b32'
 
   url "https://www.yourkit.com/download/YourKit-JavaProfiler-#{version.before_comma}-#{version.after_comma}.dmg"
   appcast 'https://www.yourkit.com/download/'
