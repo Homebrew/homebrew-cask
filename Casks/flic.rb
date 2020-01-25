fbe18d91dc6e763561cabe1a3043e7eaff3b59ad
Randall <ran.dall@icloud.com>
2020-01-24 16:44:57 -0800

    Update flic from 2.0.0 to 2.0.2

--- Casks/flic.rb
+++ Casks/flic.rb
@@ -1,6 +1,6 @@
 cask 'flic' do
-  version '2.0.0'
-  sha256 '5b7a2cac9772723f7ddd9d8a54be21c094c35851ade650384277e6b9770043d6'
+  version '2.0.2'
+  sha256 '46fad4ca0e69128b28906e8c2e7b785e39a1dc406a775a6c48184e8bf90869b6'
 
   # misc-scl-cdn.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://misc-scl-cdn.s3.amazonaws.com/Flic.#{version}.zip"
