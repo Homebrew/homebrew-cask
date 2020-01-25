7552930364eb2cf987f5ccaa4341b1a424dc0821
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-24 17:03:41 -0800

    Update kid3 from 3.8.1 to 3.8.2 (#76141)

--- Casks/kid3.rb
+++ Casks/kid3.rb
@@ -1,7 +1,7 @@
 cask 'kid3' do
   # note: "3" is not a version number, but an intrinsic part of the product name (ID3 tags)
-  version '3.8.1'
-  sha256 '1a47c1ae065ee73f6d3e53b6e58dadf88e4a2d3bc9257cbad9c4a5b7d731074b'
+  version '3.8.2'
+  sha256 '0265456af936c96f36c3fb29d60077d5123d670a6bb2cb26e40251daf7e8964d'
 
   # downloads.sourceforge.net/kid3 was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/kid3/kid3-#{version}-Darwin.dmg"
