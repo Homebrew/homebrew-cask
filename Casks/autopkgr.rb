f723792d178fde635adf2ca31f0ab0015dc08d43
Anver Housseini <contact@anver.ch>
2020-01-25 11:05:37 +0100

    Update autopkgr from 1.5.1 to 1.5.2 (#76157)

--- Casks/autopkgr.rb
+++ Casks/autopkgr.rb
@@ -1,6 +1,6 @@
 cask 'autopkgr' do
-  version '1.5.1'
-  sha256 '11c3ce33822f25053731bb084b6cb0d688461306a43b3782f045bdf5745c0904'
+  version '1.5.2'
+  sha256 'db443d0e21d6379a060abb59bced973adbad497a36da1d9e1892bb26ebad0f4e'
 
   # github.com/lindegroup/autopkgr was verified as official when first introduced to the cask
   url "https://github.com/lindegroup/autopkgr/releases/download/v#{version}/AutoPkgr-#{version}.dmg"
