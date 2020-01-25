ea13f9746f8e12ada9ed5293bce961fb06d56ead
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-01-25 17:20:20 +0100

    Add Conferences.digital 0.0.1-alpha21 (#76154)
    * Add Conferences.digital
    
    * Fix Conferences.digital name
    
    * Update and rename conferencesdigital.rb to conferences.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/conferences.rb
@@ -0,0 +1,12 @@
+cask 'conferences' do
+  version '0.0.1-alpha21'
+  sha256 '53348fd30d4c0367014d46cad1e581318ba4915259ac9845c668ebdf6184590c'
+
+  # github.com/zagahr/Conferences.digital was verified as official when first introduced to the cask
+  url "https://github.com/zagahr/Conferences.digital/releases/download/#{version}/Conferences_v#{version}.zip"
+  appcast 'https://zagahr.github.io/Conferences.digital/appcast.xml'
+  name 'Conferences.digital'
+  homepage 'https://conferences.digital/'
+
+  app 'Conferences.app'
+end
