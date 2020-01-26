8f795d21ebc684a38ed64b7b7ef84b70577626ec
Vítor Galvão <info@vitorgalvao.com>
2020-01-26 02:00:50 +0000

    Update postbox.rb

--- Casks/postbox.rb
+++ Casks/postbox.rb
@@ -8,8 +8,8 @@ cask 'postbox' do
   name 'Postbox'
   homepage 'https://www.postbox-inc.com/'
 
-  depends_on macos: '>= :high_sierra'
   auto_updates true
+  depends_on macos: '>= :high_sierra'
 
   app 'Postbox.app'
 
