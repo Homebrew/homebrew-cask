20561e3d0815d9cc6abe31db5161b922ec81a4e2
Randall <ran.dall@icloud.com>
2020-01-25 16:03:09 -0800

    Update jgrasp from 2.0.6 to 2.0.6_01

--- Casks/jgrasp.rb
+++ Casks/jgrasp.rb
@@ -1,6 +1,6 @@
 cask 'jgrasp' do
-  version '2.0.6'
-  sha256 'ce6bbeff0c9f9062db99beeae6faa814b7a3d5e1f85182695c764fb935d7c80c'
+  version '2.0.6_01'
+  sha256 'dc1cf1036ae93bcfae2e713d474c01b3c9d83347a0c88e5e4372b29a1a698675'
 
   url "https://jgrasp.org/dl4g/jgrasp/jgrasp#{version.no_dots}.pkg"
   appcast 'https://jgrasp.org/'
