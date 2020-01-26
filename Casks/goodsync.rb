bc52b247df85e74298eea09d92d7ad772ae9e63a
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-01-26 14:06:28 +0100

    Update goodsync from 10.10.20.0 to 10.10.20.7 (#76193)

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,5 +1,5 @@
 cask 'goodsync' do
-  version '10.10.20.0'
+  version '10.10.20.7'
   sha256 '85459f65c0cd98b65bf6bd25d970d5056d8e849572181566d9681a1154c523bb'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
