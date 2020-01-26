e8a97ca4417369f80923e46efc9007ba35d0a17b
Hannes Braun <hannesbraun@mail.de>
2020-01-26 13:39:37 +0100

    Update prudent from 79.0.3945.88,17 to 79.0.3945.88,19

--- Casks/prudent.rb
+++ Casks/prudent.rb
@@ -1,6 +1,6 @@
 cask 'prudent' do
-  version '79.0.3945.88,17'
-  sha256 '62d5f78b7cbf5bc029813a3eef28f732d70d40586ff258443e7e2696fb28f78c'
+  version '79.0.3945.88,19'
+  sha256 '3ca9fd9cae0f287e171b99cd1a2c5cc03e4ffd81fedfa03a5a9cf2b8983e2cdf'
 
   # github.com/PrudentMe/main was verified as official when first introduced to the cask
   url "https://github.com/PrudentMe/main/releases/download/#{version.after_comma}/Prudent.zip"
