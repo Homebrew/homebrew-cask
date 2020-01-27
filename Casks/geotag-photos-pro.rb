f46fe33db0f50222384b52ddb909474949d9dd84
Matthew Gleich <matthewgleich@gmail.com>
2020-01-27 11:07:04 -0500

    Update geotag-photos-pro from 1.5.4 to 1.5.5

--- Casks/geotag-photos-pro.rb
+++ Casks/geotag-photos-pro.rb
@@ -1,6 +1,6 @@
 cask 'geotag-photos-pro' do
-  version '1.5.4'
-  sha256 '0c84ee5c1027f9793fd372b517d440f20a534b09ebfdff3452bbb65542f340c0'
+  version '1.5.5'
+  sha256 '80779b8316547f00a812d410edede1782bd8a23b18d64ad6eaa87c28e73fab71'
 
   # github.com/tappytaps/geotag-desktop-app was verified as official when first introduced to the cask
   url "https://github.com/tappytaps/geotag-desktop-app/releases/download/v#{version}/Geotag-Photos-Pro-2-#{version}.dmg"
