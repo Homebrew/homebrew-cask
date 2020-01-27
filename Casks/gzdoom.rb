42e24c89f4b56180c09eed5c229b66b1fd295484
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-26 19:03:26 -0800

    Update gzdoom from 4.3.2 to 4.3.3 (#76207)

--- Casks/gzdoom.rb
+++ Casks/gzdoom.rb
@@ -1,6 +1,6 @@
 cask 'gzdoom' do
-  version '4.3.2'
-  sha256 'f655714de0827fa3d0f4b21cbfcd2174e196c2471b614198806bcb2fa580d7cc'
+  version '4.3.3'
+  sha256 '8f474455af2c5cdcf2dd9087d94d5f718ffa1ed50a291b6361f846b30f6d1d16'
 
   # github.com/coelckers/gzdoom was verified as official when first introduced to the cask
   url "https://github.com/coelckers/gzdoom/releases/download/g#{version}/gzdoom-#{version.dots_to_hyphens}-macOS.dmg"
