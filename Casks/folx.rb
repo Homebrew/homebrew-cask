7e9b4f312a20438a0ca13923db432486a8e60cc9
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-27 18:20:56 -0800

    Update folx from 5.14.13922 to 5.15.13938 (#76241)

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -1,6 +1,6 @@
 cask 'folx' do
-  version '5.14.13922'
-  sha256 'a22bdd204132df1213a497aac58cddd7c739f7d3322d642a19f72d488611462e'
+  version '5.15.13938'
+  sha256 'ceb0d3784e57f82ebf4110f2021be1fb9feec45555350cf622f2c27f3cce6650'
 
   url 'https://cdn.eltima.com/download/downloader_mac.dmg'
   appcast 'https://cdn.eltima.com/download/folx-updater/folx.xml',
