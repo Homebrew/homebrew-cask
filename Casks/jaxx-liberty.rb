e0c5f9261eca20b878aa6ed83639d94d39663ab7
Work <brigitte@corecode.io>
2020-01-28 17:40:13 +0100

    Update jaxx-liberty from 2.3.7 to 2.3.8

--- Casks/jaxx-liberty.rb
+++ Casks/jaxx-liberty.rb
@@ -1,6 +1,6 @@
 cask 'jaxx-liberty' do
-  version '2.3.7'
-  sha256 '2611ef7485447fd53c1ad46551cc3e0ef23bcddbfaecd725a283b99bb4a91a95'
+  version '2.3.8'
+  sha256 '6e6901c5c7a9bb796a3d189d092259ccd4107ca6a34b9b299090c3c4310d594b'
 
   url "https://download-liberty.jaxx.io/Jaxx.Liberty-#{version}.dmg"
   appcast 'https://jaxx.io/downloads.html'
