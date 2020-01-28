c79ef2a7ebf5a1668f8cba4d232910ef7e6400cd
Randall <ran.dall@icloud.com>
2020-01-28 15:14:39 -0800

    Update ckan from 1.26.6 to 1.26.8

--- Casks/ckan.rb
+++ Casks/ckan.rb
@@ -1,6 +1,6 @@
 cask 'ckan' do
-  version '1.26.6'
-  sha256 'd0c1204c3e51911c93e76fa7846a771aab99099a340a9d5a4a4f9b556c41f467'
+  version '1.26.8'
+  sha256 'b9b19c05bb92489b3f95bdc938fae4b4adbaac5605e94362382e4015decc699b'
 
   url "https://github.com/KSP-CKAN/CKAN/releases/download/v#{version}/CKAN.dmg"
   appcast 'https://github.com/KSP-CKAN/CKAN/releases.atom'
