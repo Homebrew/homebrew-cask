fb9df716e84923f4f2eeecba12367e9a84c8173c
Jakob Petersson <JakobPetersson@users.noreply.github.com>
2020-01-28 23:02:20 +0100

    Update sha256 of vagrant 2.2.7 (#76289)

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,6 +1,6 @@
 cask 'vagrant' do
   version '2.2.7'
-  sha256 'cebe1d09123346ad9f8b5bfcd9460277e3c86b759dc860c2e7b3e4a1570c0b10'
+  sha256 '45472731bde1df0bf2e0e0cf1ee460e2851c920d8b7b8af939e41156515cf49c'
 
   # hashicorp.com/vagrant was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
