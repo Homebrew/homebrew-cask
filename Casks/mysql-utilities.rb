1fef80adddef2bb31b44a8b8efdb7d5ddeb653e0
Vítor Galvão <info@vitorgalvao.com>
2020-01-28 13:48:46 +0000

    mysql-utilities.rb: update URL

--- Casks/mysql-utilities.rb
+++ Casks/mysql-utilities.rb
@@ -2,7 +2,7 @@ cask 'mysql-utilities' do
   version '1.6.5'
   sha256 '114658256e846b4eeff141065e18ef3f779bcbe59e5d2828ab920d905e693db7'
 
-  url "https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities-#{version}-macos10.12.dmg"
+  url "https://downloads.mysql.com/archives/get/p/30/file/mysql-utilities-#{version}-macos10.12.dmg"
   name 'MySQL Utilities'
   homepage 'https://dev.mysql.com/downloads/utilities/'
 
