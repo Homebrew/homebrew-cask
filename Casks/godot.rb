9d6de5aa1cb66345b555e4a7484d803f1ba11b41
Gadzhi Kharkharov <1460122+kkga@users.noreply.github.com>
2020-01-29 16:03:36 +0300

    Update godot from 3.1.2 to 3.2 (#76321)

--- Casks/godot.rb
+++ Casks/godot.rb
@@ -1,6 +1,6 @@
 cask 'godot' do
-  version '3.1.2'
-  sha256 '23d26e8e6cd6172a75501916fb6142ec50bb7a21ad7c7e26665de99102852ea6'
+  version '3.2'
+  sha256 '3b85748ed69db31e7c024a06df863f228e2f5ed8cf0587551bc64ce72b498c88'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
   url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.64.zip"
