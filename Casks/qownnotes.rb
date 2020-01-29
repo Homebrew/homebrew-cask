7c6c687d37dc7bf1a6ead7c8a388df20f6946dd8
Christian Kraemer <christian.kraemer@proximity.de>
2020-01-29 14:45:58 +0100

    Update qownnotes from 20.1.18,b5297-181335 to 20.1.19,b5304-171959

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '20.1.18,b5297-181335'
-  sha256 '1a8c6d459eeb74f60a7a0d3c24269d96a6b2f124f0fe2af48bc39c3c41863d08'
+  version '20.1.19,b5304-171959'
+  sha256 '22d420841aab62379a3b0cf7acff00038566586e5cfe78434f6ace8f018985b7'
 
   # github.com/pbek/QOwnNotes was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
