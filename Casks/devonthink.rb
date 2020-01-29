5399c389f50d10a54fc9c0e8ec0698a68c16fa48
Anver Housseini <contact@anver.ch>
2020-01-29 14:37:37 +0100

    Update devonthink from 3.0.3 to 3.0.4 (#76319)
    * Update devonthink.rb
    
    * Update devonthink.rb

--- Casks/devonthink.rb
+++ Casks/devonthink.rb
@@ -1,10 +1,9 @@
 cask 'devonthink' do
-  version '3.0.3'
-  sha256 'e34cba7acf37df73f04a69d21e16c6af81d38c9f3d51d44ad46692ec1747f5dd'
+  version '3.0.4'
+  sha256 '58f2dc9877505782dc3529a4c805f4faa29728ea8c4d276670935dd779b21e31'
 
-  # s3.amazonaws.com/DTWebsiteSupport was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/DTWebsiteSupport/download/devonthink/#{version}/DEVONthink_#{version.major}.dmg.zip"
-  appcast "https://www.devontechnologies.com/Sparkle/DEVONthink#{version.major}.xml"
+  url "https://download.devontechnologies.com/download/devonthink/#{version}/DEVONthink_#{version.major}.app.zip"
+  appcast 'https://api.devontechnologies.com/1/apps/sparkle/sparkle.php?id=300900000'
   name 'DEVONthink'
   homepage 'https://www.devontechnologies.com/apps/devonthink/'
 
