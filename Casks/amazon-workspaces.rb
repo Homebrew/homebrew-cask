cccde7108cfcef3ee16a46cbc44ac2f7aa8245c4
Jorn <12266745+jlholtus@users.noreply.github.com>
2020-01-29 10:46:00 +0700

    Update amazon-workspaces from 2.5.11 to 3.0.2

--- Casks/amazon-workspaces.rb
+++ Casks/amazon-workspaces.rb
@@ -1,5 +1,5 @@
 cask 'amazon-workspaces' do
-  version '2.5.11'
+  version '3.0.2'
   sha256 'd217cceaecb28cb8d2c4f99f2f84c0aa4696f91dfb779c2cacf0c51e1b93c6a1'
 
   # d2td7dqidlhjx7.cloudfront.net was verified as official when first introduced to the cask
