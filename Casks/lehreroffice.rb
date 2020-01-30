4b67e1793b376dc71e3e2484eb560ef85e46d6a1
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-29 19:18:51 -0800

    Update lehreroffice from 2019.13.4 to 2019.13.5 (#76374)

--- Casks/lehreroffice.rb
+++ Casks/lehreroffice.rb
@@ -1,6 +1,6 @@
 cask 'lehreroffice' do
-  version '2019.13.4'
-  sha256 'b7d84e03a672f196ec72426d313df8bf9c7d868d350a0ada12e9bed7b1811ebb'
+  version '2019.13.5'
+  sha256 '802e071c552d8550af5167230ea432603f62797bbbb9dac04b26bbd010698184'
 
   url 'https://www.lehreroffice.ch/lo/dateien/easy/lo_desktop_macos.dmg'
   appcast 'https://www.lehreroffice.ch/services/update/getcurrentversion.php?app=Desktop'
