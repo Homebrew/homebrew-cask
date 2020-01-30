5f5e28dfba60277a27d6eb497e5f04d620731cdf
suschizu <brigitte@corecode.io>
2020-01-30 23:41:35 +0100

    Update fantastical from 2.5.15 to 3.0 (#76339)
    * Update fantastical from 2.5.15 to 3.0
    
    * Update fantastical.rb
    
    * Update fantastical.rb

--- Casks/fantastical.rb
+++ Casks/fantastical.rb
@@ -1,15 +1,15 @@
 cask 'fantastical' do
-  version '2.5.15'
-  sha256 '3c2cc82594308dc77fb238eed5907e457cb240871d1ef69b526484bf01679041'
+  version '3.0'
+  sha256 'f19e5f8811eafb39d6ffb26fa1da8d813048362b2faedfb6798df7a70d28373e'
 
   url "http://cdn.flexibits.com/Fantastical_#{version}.zip"
-  appcast "https://flexibits.com/fantastical/appcast#{version.major}.php"
+  appcast 'https://flexibits.com/fantastical/appcast2.php'
   name 'Fantastical'
   homepage 'https://flexibits.com/fantastical'
 
   auto_updates true
 
-  app "Fantastical #{version.major}.app"
+  app 'Fantastical.app'
 
   uninstall launchctl: "com.flexibits.fantastical#{version.major}.mac.launcher",
             quit:      "com.flexibits.fantastical#{version.major}.mac"
