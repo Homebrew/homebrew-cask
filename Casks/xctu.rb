852ac5520507987efc1696f164a1d7858c982537
Vítor Galvão <info@vitorgalvao.com>
2020-01-30 22:23:59 +0000

    Update xctu.rb

--- Casks/xctu.rb
+++ Casks/xctu.rb
@@ -3,6 +3,7 @@ cask 'xctu' do
   sha256 '62e54d4f2760018b1b6b210e34cab26f0986ab117fa25738fedc9f98efffe6a0'
 
   url "https://ftp1.digi.com/support/utilities/#{version}.zip"
+  appcast 'https://www.digi.com/support/includes/utilities.aspx?pid=3352'
   name 'XCTU'
   homepage 'https://www.digi.com/products/embedded-systems/digi-xbee-tools/xctu'
 
