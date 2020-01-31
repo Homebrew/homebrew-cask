9f76e009488d59d2196102cbc02a06b312d3f7fd
Randall <17261190+ran-dall@users.noreply.github.com>
2020-01-30 21:20:50 -0800

    Update netxms-console from 3.1.300 to 3.1.343 (#76432)

--- Casks/netxms-console.rb
+++ Casks/netxms-console.rb
@@ -1,6 +1,6 @@
 cask 'netxms-console' do
-  version '3.1.300'
-  sha256 'fba16e71742d0b0e2dcf3ac98fce5c07e46638ab40156ddfdb408998a68ef51a'
+  version '3.1.343'
+  sha256 '69f520fd221ab51c23a6df5c1ecbe26a3008a47c6e92c73e8b7fe0cc5046f106'
 
   url "https://netxms.org/download/releases/#{version.major_minor}/nxmc-#{version}.dmg"
   appcast "https://netxms.org/download/releases/#{version.major_minor}/"
