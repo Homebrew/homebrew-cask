e43762a0fdce8f3eec02432b5142fd2e967486da
Randall <ran.dall@icloud.com>
2020-01-31 10:21:21 -0800

    Update brook from 20200102 to 20200201

--- Casks/brook.rb
+++ Casks/brook.rb
@@ -1,6 +1,6 @@
 cask 'brook' do
-  version '20200102'
-  sha256 'ee4fbeba88fa57bb0702612ba32e8e5d04036eaad3740f58c38788406b692a47'
+  version '20200201'
+  sha256 '4b964bad957e69d94bf7ef2ec6616d7329f9e5b1a3967296c1fa7968cb99b0c6'
 
   url "https://github.com/txthinking/brook/releases/download/v#{version}/Brook.pkg"
   appcast 'https://github.com/txthinking/brook/releases.atom'
