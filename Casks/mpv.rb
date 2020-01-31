777a44cc1f2f6985843b013c2ca3ddcd77d94634
jnozsc <jnozsc@gmail.com>
2020-01-31 11:14:49 -0800

    bump mpv to 0.32.0 and add a mavericks build 0.29.0

--- Casks/mpv.rb
+++ Casks/mpv.rb
@@ -1,9 +1,13 @@
 cask 'mpv' do
-  version '0.31.0'
-  sha256 '24a8a41ade88b6830218a14b813247bb147637f6ce6a0cbf382259333a69bcd1'
-
-  # laboratory.stolendata.net/~djinn/mpv_osx was verified as official when first introduced to the cask
-  url "https://laboratory.stolendata.net/~djinn/mpv_osx/mpv-#{version}.tar.gz"
+  if MacOS.version <= :mavericks
+    version '0.29.0'
+    sha256 '41f79cc33758888ed75d2163cb1c209d79e6fa7d558b6e8fef3dfe05b4a6fc29'
+    # laboratory.stolendata.net/~djinn/mpv_osx was verified as official when first introduced to the cask
+    url "https://laboratory.stolendata.net/~djinn/mpv_osx/mpv-#{version}-mavericks.tar.gz"
+  else:
+    version '0.32.0'
+    sha256 '5c1b21412a0cfa8fbd06b18aa3cea919c1b9ce86d3aa39672cbc714cb753004c'
+    url "https://laboratory.stolendata.net/~djinn/mpv_osx/mpv-#{version}.tar.gz"
   appcast 'https://laboratory.stolendata.net/~djinn/mpv_osx/'
   name 'mpv'
   homepage 'https://mpv.io/'
