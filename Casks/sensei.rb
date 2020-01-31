eca343461c8ce2e2eda0e24612394b80061a3883
Randall <ran.dall@icloud.com>
2020-01-31 10:25:26 -0800

    Update sensei from 1.0.3,12 to 1.0.4,13

--- Casks/sensei.rb
+++ Casks/sensei.rb
@@ -1,6 +1,6 @@
 cask 'sensei' do
-  version '1.0.3,12'
-  sha256 'bd480c729297190df54c7a3520c16e8a0879719e0ff559c711f9056d4b24f47b'
+  version '1.0.4,13'
+  sha256 '47dbb09a8bcac940c643be2b256890a99e0d1009c6865938b4c5e6ee51f222ed'
 
   # cindori.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://cindori.s3.amazonaws.com/Sensei.dmg'
