3a9b93d3fec22032179f86858be743c2671148a3
Randall <ran.dall@icloud.com>
2020-01-30 20:42:05 -0800

    Update dashlane from 6.1948.0.27265 to 6.2004.0.32116

--- Casks/dashlane.rb
+++ Casks/dashlane.rb
@@ -1,6 +1,6 @@
 cask 'dashlane' do
-  version '6.1948.0.27265'
-  sha256 '42c4fa7884f9f0ef45c61a1693191dc8832644882e877932dba3c60b098ea59c'
+  version '6.2004.0.32116'
+  sha256 '12636fe721c50c1e48d7f7179a573eb2a20127e897a90fcc3ce43b4d8c2f2dd3'
 
   # d3mfqat9ni8wb5.cloudfront.net/releases was verified as official when first introduced to the cask
   url "https://d3mfqat9ni8wb5.cloudfront.net/releases/#{version.major_minor_patch}/#{version}/release/Dashlane.dmg"
