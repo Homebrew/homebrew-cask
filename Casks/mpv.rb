329c140ffdf1e6a972b849aee4dc53e3af9f2d52
jnozsc <jnozsc@gmail.com>
2020-01-31 11:18:10 -0800

    typo

--- Casks/mpv.rb
+++ Casks/mpv.rb
@@ -4,7 +4,7 @@ cask 'mpv' do
     sha256 '41f79cc33758888ed75d2163cb1c209d79e6fa7d558b6e8fef3dfe05b4a6fc29'
     # laboratory.stolendata.net/~djinn/mpv_osx was verified as official when first introduced to the cask
     url "https://laboratory.stolendata.net/~djinn/mpv_osx/mpv-#{version}-mavericks.tar.gz"
-  else:
+  else
     version '0.32.0'
     sha256 '5c1b21412a0cfa8fbd06b18aa3cea919c1b9ce86d3aa39672cbc714cb753004c'
     url "https://laboratory.stolendata.net/~djinn/mpv_osx/mpv-#{version}.tar.gz"
