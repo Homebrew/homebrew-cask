d300480871b8c4813a9bbb02617d82969646e034
Randall <ran.dall@icloud.com>
2020-01-31 10:37:10 -0800

    Update plex from 1.6.2.994-e05b79d6 to 1.6.3.1009-57cf57c8

--- Casks/plex.rb
+++ Casks/plex.rb
@@ -1,6 +1,6 @@
 cask 'plex' do
-  version '1.6.2.994-e05b79d6'
-  sha256 'ab3283b33756cb1d28f605628ad1c247a60c6a6cdf6e5865a7d94031deb865fd'
+  version '1.6.3.1009-57cf57c8'
+  sha256 '35b721677529cd021e99886b6f59641caaa06b2f885f1f0be7092c9c4d000a19'
 
   url "https://downloads.plex.tv/plex-desktop/#{version}/macos/Plex-#{version}-x86_64.zip"
   appcast 'https://plex.tv/api/downloads/6.json'
