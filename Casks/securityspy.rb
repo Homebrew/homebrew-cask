9f962008e9b572c9ae8ff7ee7602ea4a0a5e1e46
Jason Charrier <jcharr1@users.noreply.github.com>
2020-02-01 01:33:52 -0600

    Update securityspy (#76480)

--- Casks/securityspy.rb
+++ Casks/securityspy.rb
@@ -1,6 +1,6 @@
 cask 'securityspy' do
   version '5.2.1'
-  sha256 'c70f1d3d8b187866d96e087257a84063c0837a61c7d0809931edf0268c1b83c7'
+  sha256 'aad491e6fc3d8ab65e8574762529be1788df16ae02e5ed0f5612cea0194a091e'
 
   url 'https://www.bensoftware.com/securityspy/SecuritySpy.dmg'
   appcast 'https://www.bensoftware.com/securityspy/versionhistory.html',
