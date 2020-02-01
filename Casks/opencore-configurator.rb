0400702a5beb422719f314a7dc4084dbe98ed609
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-01 13:05:34 -0800

    Update opencore-configurator from 1.13.2.0 to 1.13.3.0 (#76520)

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,6 +1,6 @@
 cask 'opencore-configurator' do
-  version '1.13.2.0'
-  sha256 '83dd59725091db57c4f265d59cc1098223247d196882393e7250332eaa9a42ec'
+  version '1.13.3.0'
+  sha256 '5882c6cfdf698f72c8b96a86eb7f29eb52f289373ac87e8242f69c0a207634cf'
 
   url 'https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last'
   appcast 'https://mackie100projects.altervista.org/category/opencore-configurator-changelog/'
