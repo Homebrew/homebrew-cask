02a74217b3f124181f052369d48a7ac5c88957fe
Randall <ran.dall@icloud.com>
2020-02-01 12:33:30 -0800

    Update grafx from 2.6.2492,41 to 2.7

--- Casks/grafx.rb
+++ Casks/grafx.rb
@@ -1,6 +1,6 @@
 cask 'grafx' do
-  version '2.6.2492,41'
-  sha256 '31658ed8983ac3ed7778e2fd0b6dfc5e1abc4c1302c5bc77a4dc584cd7c7efb5'
+  version '2.7'
+  sha256 '4b340f5dbe3d0b9223bfd5d13125cef2177fec039ea439d936f2105e08823600'
 
   url "https://pulkomandy.tk/projects/GrafX#{version.major}/downloads/#{version.after_comma}"
   appcast 'https://gitlab.com/GrafX2/grafX2/-/tags?format=atom',
