1d177f510006c991a37d67ccc0be75ae95c31737
Randall <ran.dall@icloud.com>
2020-02-01 12:14:07 -0800

    Update jumpshare from 2.5.6 to 2.5.7

--- Casks/jumpshare.rb
+++ Casks/jumpshare.rb
@@ -1,6 +1,6 @@
 cask 'jumpshare' do
-  version '2.5.6'
-  sha256 'a53797794a1bacb5ca344c6c850a6ce7c81f6148c931a461dee6dab3e35a46e7'
+  version '2.5.7'
+  sha256 '6603d431205592a58344e27af1fc93fc5297357763eae0337c0a53651bd2e921'
 
   url "https://apps.jumpshare.com/desktop/mac/updates/Jumpshare-#{version}.tar.bz2"
   appcast 'https://apps.jumpshare.com/desktop/mac/updates/appcast.xml'
