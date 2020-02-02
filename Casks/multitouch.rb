dd5070cf43c447da981142161016a28a9ecdb1d0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-02 11:19:56 -0800

    Update multitouch from 1.16.11 to 1.16.13 (#76534)

--- Casks/multitouch.rb
+++ Casks/multitouch.rb
@@ -1,6 +1,6 @@
 cask 'multitouch' do
-  version '1.16.11'
-  sha256 '5b644c5ac774e20d1a2fdede340e26ca0af6c7bf45377f8f447f3845edd11d74'
+  version '1.16.13'
+  sha256 '14fe0546947000c127b1391d34e44ad089ef193a975b97ecb03f365a44c3722c'
 
   url "https://multitouch.app/downloads/multitouch#{version}.dmg"
   appcast 'https://www.multitouch.app/downloads/updates.xml'
