c1e9d385fbe98477fd8cdbd0dd5a5991033d22eb
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-02 11:20:10 -0800

    Update biscuit from 1.2.3 to 1.2.4 (#76533)

--- Casks/biscuit.rb
+++ Casks/biscuit.rb
@@ -1,6 +1,6 @@
 cask 'biscuit' do
-  version '1.2.3'
-  sha256 '7a640c7948c21149f163842d6fb8caba0ca040100a07008dcdf3b767c80fc832'
+  version '1.2.4'
+  sha256 '5b11a54fa7fab605500ef5e63e088a9114d643ad4ed67c734e84d02b6bab67d0'
 
   # github.com/agata/dl.biscuit was verified as official when first introduced to the cask
   url "https://github.com/agata/dl.biscuit/releases/download/v#{version}/Biscuit-#{version}.dmg"
