b917cd97ad88e05b23ba89a77bdfca815b666e3d
Randall <ran.dall@icloud.com>
2020-02-02 11:06:02 -0800

    Update fontlab from 7.0.1.7276 to 7.0.2.7334

--- Casks/fontlab.rb
+++ Casks/fontlab.rb
@@ -1,6 +1,6 @@
 cask 'fontlab' do
-  version '7.0.1.7276'
-  sha256 '0ce2655f7238c5b7a29308a124ba2e1790fafd4103a7545d779672f5354ca4f6'
+  version '7.0.2.7334'
+  sha256 '2970b30075325df53db75ed0b8cbc686d3530157bc463d70588f7b4680ad5049'
 
   # fontlab.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://fontlab.s3.amazonaws.com/fontlab-#{version.major}/#{version.split('.').last}/FontLab-#{version.major}-Mac-Install-#{version.split('.').last}.dmg"
