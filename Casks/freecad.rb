e646d636e0577e83e9956eba30bf12838466c3a7
brushmate <brushmate@gmail.com>
2020-02-03 11:05:28 +0100

    Use the correct checksum of FreeCAD 0.18.4 (#76552)

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,6 +1,6 @@
 cask 'freecad' do
   version '0.18.4,16146'
-  sha256 'a3e90a2b946b751082be9b0a0893466c550074cb70ec1e5e88b2d3123d7f5f16'
+  sha256 '94f3eb0c13755ab4a260e550174cdc5d0de547c872c419339a53f8ecc9ef770f'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg"
