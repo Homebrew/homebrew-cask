ec6d424b296814fc1110b2fbf2d71f4cb02cd38a
Randall <ran.dall@icloud.com>
2020-02-04 13:07:26 -0800

    Update pinegrow from 5.92 to 5.93

--- Casks/pinegrow.rb
+++ Casks/pinegrow.rb
@@ -1,6 +1,6 @@
 cask 'pinegrow' do
-  version '5.92'
-  sha256 '2ea546cafa691c0836c6ed7f9ea822d080d291ede96b59fe0440132b632bee86'
+  version '5.93'
+  sha256 '330a5738d8bbc1310b34c50f382637df47357a4505e2d2ae1028e8dbc6f43098'
 
   url "http://download.pinegrow.com/PinegrowMac.#{version}.dmg"
   appcast 'https://pinegrow.com/'
