b2d7b9311af4c8f580c5d7bd15fc730e10d7721b
Paul Walker <paul@pwjw.com>
2020-02-04 13:36:50 -0500

    Move surge-synthesizer to version 1.6.5
    surge-synthesizer.rb upgraded to version 1.6.5, matching the
    release of the synth we did today.

--- Casks/surge-synthesizer.rb
+++ Casks/surge-synthesizer.rb
@@ -1,11 +1,11 @@
 cask 'surge-synthesizer' do
-  version '1.6.4.1'
-  sha256 '50e64edaa6124fec85868fdb6627969d4d6ee9641dce45c606471b0b274906d7'
+  version '1.6.5'
+  sha256 '1357c2534142e6df33a6b5d5f840adc178403c9c97e952e300ce58b074a70602'
 
   # github.com/surge-synthesizer/releases was verified as official when first introduced to the cask
   url "https://github.com/surge-synthesizer/releases/releases/download/#{version}/Surge-#{version}-Setup.dmg"
   appcast 'https://github.com/surge-synthesizer/releases/releases.atom'
-  name 'Surge - a Digital Synthesizer'
+  name 'Surge - A Digital Synthesizer'
   homepage 'https://surge-synthesizer.github.io/'
 
   pkg "Surge-#{version}-Setup.pkg"
