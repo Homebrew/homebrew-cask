c4d22213882e96ece6cf33ff8c49ee39d3cdcadd
Randall <ran.dall@icloud.com>
2020-02-04 12:54:59 -0800

    Update electerm from 1.0.21 to 1.0.23

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask 'electerm' do
-  version '1.0.21'
-  sha256 '48645b956ff7358c9430af4198a7d310dde5eccdcbc98c35558dbb07048580a7'
+  version '1.0.23'
+  sha256 '11b856cf3d3a521a9746125352c0c520b7e5ef196faead55b4440b1ac138d00e'
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   appcast 'https://github.com/electerm/electerm/releases.atom'
