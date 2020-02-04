79236b3672eec8a27d278d8a24265a050b7dd106
suschizu <brigitte@corecode.io>
2020-02-04 11:55:51 +0100

    Update zoomus.rb

--- Casks/zoomus.rb
+++ Casks/zoomus.rb
@@ -2,14 +2,14 @@ cask 'zoomus' do
   version '4.6.17383.0119'
   sha256 '13536a35e8d3813af40e406d340327a4797ff63a3622bd0a8a965f86ac68979b'
 
-  url "https://www.zoom.us/client/#{version}/zoomusInstaller.pkg"
+  url "https://www.zoom.us/client/#{version}/Zoom.pkg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://zoom.us/client/latest/Zoom.pkg'
   name 'Zoom.us'
   homepage 'https://www.zoom.us/'
 
   auto_updates true
 
-  pkg 'zoomusInstaller.pkg'
+  pkg 'Zoom.pkg'
 
   postflight do
     set_ownership '~/Library/Application Support/zoom.us'
