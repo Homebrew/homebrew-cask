2220f51ea00dc3354bf6750640db8d7afd090c03
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-04 13:52:18 -0800

    Update epic-games from 10.7.0 to 10.12.3 (#76620)

--- Casks/epic-games.rb
+++ Casks/epic-games.rb
@@ -1,6 +1,6 @@
 cask 'epic-games' do
-  version '10.7.0'
-  sha256 '0b82a5103df5026de9ef5ed683c04977053c02a40fce41f404e9119889a845fd'
+  version '10.12.3'
+  sha256 '8f7966f974f65d50b38ce6738050aa4f663d9f4628fde4678846127db4392e20'
 
   # epicgames-download1.akamaized.net was verified as official when first introduced to the cask
   url "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-#{version}.dmg"
