1a480b37d7ac231bfb8e5b5aa0e61c519e3434c4
Work <brigitte@corecode.io>
2020-02-05 15:34:07 +0100

    Update razorsql from 9.0.3 to 9.0.4

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,6 +1,6 @@
 cask 'razorsql' do
-  version '9.0.3'
-  sha256 '3955645a29b7ebbcd9c6aec7df575aaa9ac3307a98cd30cd8ef618979d93b6ef'
+  version '9.0.4'
+  sha256 '26e29d633a8cdbb5dddc28467986bc19a096c2f4cf5595cd8a46a579438f4c35'
 
   url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
   appcast 'https://razorsql.com/updates.html'
