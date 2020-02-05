67160a2e84ff30015534a8ec1478789d48e4905b
Vítor Galvão <info@vitorgalvao.com>
2020-02-05 23:30:06 +0000

    Update and rename packetproxy to packetproxy.rb

--- Casks/packetproxy.rb
+++ Casks/packetproxy.rb
@@ -3,6 +3,7 @@ cask 'packetproxy' do
   sha256 'e79dd546f742618e6e93d7f4fc6a129aabdf3266db49a64cbc6bb45cc848a182'
 
   url "https://github.com/DeNA/PacketProxy/releases/download/#{version}/PacketProxy-#{version}-Installer-Mac-Signed.dmg"
+  appcast 'https://github.com/DeNA/PacketProxy/releases.atom'
   name 'PacketProxy'
   homepage 'https://github.com/DeNA/PacketProxy'
 
