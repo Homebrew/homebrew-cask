4956a519ca0c30612c5019fcca3f429e90c47eaa
nrlquaker <ustynov.vy@gmail.com>
2020-02-05 23:50:19 +0200

    Update nrlquaker-winbox to 3.20.5 (#76660)

--- Casks/nrlquaker-winbox.rb
+++ Casks/nrlquaker-winbox.rb
@@ -1,17 +1,13 @@
 cask 'nrlquaker-winbox' do
-  version '3.20.4'
-  sha256 '1aa393ba9bb4db65866305f2e99116956e1b19acf0ea597357978c7c3b9f6d6a'
+  version '3.20.5'
+  sha256 '723e990448282fdd94b58eceb122fa176143a57f572ea79033bfdc5f285af809'
 
   url "https://github.com/nrlquaker/winbox-mac/releases/download/v#{version}/Winbox-mac-#{version}.zip"
   appcast 'https://github.com/nrlquaker/winbox-mac/releases.atom'
   name 'Winbox-mac'
   homepage 'https://github.com/nrlquaker/winbox-mac/'
 
-  depends_on formula: 'freetype',
-             cask:    'xquartz'
-
   app 'Winbox-mac.app'
-  binary "#{appdir}/Winbox-mac.app/Contents/MacOS/winbox-mac-addresses"
 
-  zap trash: '~/Library/Application Support/com.mikrotik.winbox_*'
+  zap trash: '~/Library/Application Support/com.mikrotik.winbox'
 end
