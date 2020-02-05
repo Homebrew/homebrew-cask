30bc6bf9a3ab13439e16982a1ce4c23a685b69df
Vítor Galvão <info@vitorgalvao.com>
2020-02-05 22:12:10 +0000

    Update geph.rb

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -3,6 +3,7 @@ cask 'geph' do
   sha256 '2e082b40b721649fb9c52bb51e3a2f9547333cf74c0e246cb6f6f2528f3ec865'
 
   url "https://dl.geph.io/desktop-builds/geph-macos-#{version}.dmg"
+  appcast 'https://geph.io/'
   name 'Geph'
   homepage 'https://geph.io/'
 
