893b410c92c322397fb39d13fbd56dca82b116b1
CoreCode <core-code@users.noreply.github.com>
2020-02-06 18:18:42 +0100

    Update astah-professional (#76708)

--- Casks/astah-professional.rb
+++ Casks/astah-professional.rb
@@ -1,6 +1,6 @@
 cask 'astah-professional' do
   version '8.2.0,b743f7'
-  sha256 '52ec06d8ed9e9eaa1735c463d6c55ec8ad1de01111f1ba42e9a4ae5dd502bd72'
+  sha256 '5c2a2931bb682e71c422d2b3abd16e53106fcc571f85c3f0a7f3a2f00ed3998e'
 
   # cdn.change-vision.com/files was verified as official when first introduced to the cask
   url "http://cdn.change-vision.com/files/astah-professional-#{version.before_comma.dots_to_underscores}-#{version.after_comma}-MacOs.dmg"
