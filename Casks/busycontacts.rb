7b6cfd8f373fcaba0d82c3b9b379da8afd5398f2
Revisor01 <mail@simoneckhardt.de>
2020-02-06 21:33:36 +0100

    Update busycontacts from 1.4.5,140500 to 1.4.5,140501 (#76719)

--- Casks/busycontacts.rb
+++ Casks/busycontacts.rb
@@ -1,6 +1,6 @@
 cask 'busycontacts' do
-  version '1.4.5,140500'
-  sha256 'c22639434ef633d48c233e99f649d7d6664cedfb91d00304c103b8d3bd580778'
+  version '1.4.5,140501'
+  sha256 'd67724a0dcac6175ef0ddd7d66fd16ccc3bb85cf4e2464133a82c6208cc4ace1'
 
   url 'https://www.busymac.com/download/BusyContacts.zip'
   appcast 'https://www.busymac.com/busycontacts/news.plist'
