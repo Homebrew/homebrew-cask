752f08ad858c42fe9110cff8047d827fd7e23fd2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-07 09:50:54 -0800

    Update seadrive from 1.0.10 to 1.0.11 (#76784)

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -1,6 +1,6 @@
 cask 'seadrive' do
-  version '1.0.10'
-  sha256 '8ff20f7f7d79e7afe9ff2ec2dcd413e0a47958f1022387bd2731e8c5903b04a8'
+  version '1.0.11'
+  sha256 '7b38a0b4870e2ac8221dc737c0b16fa2ab9d41604a20be017069dd71fe60900a'
 
   # download.seadrive.org was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seadrive-#{version}.dmg"
