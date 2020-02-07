24aaff5b8bacf823b77ae063d82294d1205ac0e6
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-07 09:33:34 -0800

    Update idagio from 0.0.112 to 0.0.113 (#76782)

--- Casks/idagio.rb
+++ Casks/idagio.rb
@@ -1,6 +1,6 @@
 cask 'idagio' do
-  version '0.0.112'
-  sha256 'e2910f45195d546883e1e74737042f6413193cdcdee953c1530930b96b4b3765'
+  version '0.0.113'
+  sha256 '13cbd0600a59561d1c6a41a18c41e7e38392382a1d443ee5a5751a77c776909b'
 
   url "https://dl.idagio.com/IDAGIO-#{version}.dmg"
   name 'IDAGIO'
