deb78125565d31bd3570a4bd01886b3c7f37add0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-07 09:19:00 -0800

    Update vscodium from 1.41.1 to 1.42.0 (#76773)

--- Casks/vscodium.rb
+++ Casks/vscodium.rb
@@ -1,6 +1,6 @@
 cask 'vscodium' do
-  version '1.41.1'
-  sha256 '39c9a501ad9d54835e5dd2a3e336f5487edd2050ced6bb05e0dfebeb71827e77'
+  version '1.42.0'
+  sha256 'c4485df4fa994a3d9c5bcf20d155b571f1f7c614bcf0ff416279cfcec582fe39'
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium-darwin-#{version}.zip"
   appcast 'https://github.com/VSCodium/vscodium/releases.atom'
