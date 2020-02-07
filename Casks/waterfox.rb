b1712b0124ec3adf5e636693fc65933cfa93cf5f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-06 17:49:38 -0800

    Update waterfox from 2020.01 to 2020.01.1 (#76745)

--- Casks/waterfox.rb
+++ Casks/waterfox.rb
@@ -1,6 +1,6 @@
 cask 'waterfox' do
-  version '2020.01'
-  sha256 '26388f57b84150fe883cf541fcfcdb186415cfecb2e159a1568bb1d1f66a86db'
+  version '2020.01.1'
+  sha256 'efb8402184d10f90824a4e6d2e7b213581be13415d17bb49f1df2f6bb816e3b7'
 
   # storage-waterfox.netdna-ssl.com was verified as official when first introduced to the cask
   url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20Classic%20#{version}%20Setup.dmg"
