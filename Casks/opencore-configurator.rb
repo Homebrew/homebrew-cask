d399acfb563bc28a672dca777bb6ce7689e127e7
suschizu <brigitte@corecode.io>
2020-02-08 12:34:32 +0100

    Update opencore-configurator from 1.13.3.0 to 1.14.1.1 (#76649)
    * Update opencore-configurator from 1.13.3.0 to 1.14.0.2
    
    * Update opencore-configurator.rb
    
    * Update opencore-configurator.rb
    
    * Update opencore-configurator.rb
    
    * Update opencore-configurator.rb

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,6 +1,6 @@
 cask 'opencore-configurator' do
-  version '1.13.3.0'
-  sha256 '5882c6cfdf698f72c8b96a86eb7f29eb52f289373ac87e8242f69c0a207634cf'
+  version '1.14.1.1'
+  sha256 'c74e2d314ec3d2824cc1166dc4811be1b5e6465276024d41d40ca13a62f00bdc'
 
   url 'https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last'
   appcast 'https://mackie100projects.altervista.org/category/opencore-configurator-changelog/'
