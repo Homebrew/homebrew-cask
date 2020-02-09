56706609a2dbaf668876f9892e1fbd668cff1f1d
Lutz Roeder <lutzroeder@users.noreply.github.com>
2020-02-08 20:04:34 -0800

    Update netron from 3.8.7 to 3.8.8 (#76824)

--- Casks/netron.rb
+++ Casks/netron.rb
@@ -1,6 +1,6 @@
 cask 'netron' do
-  version '3.8.7'
-  sha256 'ec47e9120858785955fd94696e7cdf244d74ff7907f281976c8eaf92134981bb'
+  version '3.8.8'
+  sha256 'c1a20ddaf04d720a65721e9b61e01c81b7a17efc905738438b854fe86c318029'
 
   url "https://github.com/lutzroeder/netron/releases/download/v#{version}/Netron-#{version}-mac.zip"
   appcast 'https://github.com/lutzroeder/netron/releases.atom'
