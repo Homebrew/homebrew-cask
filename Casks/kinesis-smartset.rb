660a289538a0de0be4147e890b0fa222cb330139
Aaron Mathews <aaron.mathews@smartsheet.com>
2020-02-09 14:32:37 -0800

    Adding corrections from brew cask style

--- Casks/kinesis-smartset.rb
+++ Casks/kinesis-smartset.rb
@@ -5,6 +5,8 @@ cask 'kinesis-smartset' do
   url "https://gaming.kinesis-ergo.com/wp-content/uploads/2019/10/SmartSet-App-for-Freestyle-Edge-RGB-for-macOS-Version-#{version}-64-Bit.zip"
   name 'Kinesis SmartSet'
   homepage 'https://gaming.kinesis-ergo.com/fs-edge-rgb-support/#smartset'
-  caveats 'The SmartSet App is currently unsigned, so you will need to approve it in System Preferences>Security.'
+
   app "SmartSet App for Freestyle Edge RGB for macOS (Version #{version}) (64 Bit).app"
+
+  caveats 'The SmartSet App is currently unsigned, so you will need to approve it in System Preferences>Security.'
 end
