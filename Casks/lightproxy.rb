cac8575317f5816a50f7c070da2bbd3b1147d7a6
Mashiro Wang <hyperoot@gmail.com>
2020-02-09 12:08:56 +0800

    fix: fix rb style

--- Casks/lightproxy.rb
+++ Casks/lightproxy.rb
@@ -3,9 +3,9 @@ cask 'lightproxy' do
   sha256 '287cfc258692bf89acb450111b0c7f1ac63ccc66f82f9b13982cf6a1d29cd27c'
 
   # gw.alipayobjects.com/os/LightProxy was verified as official when first introduced to the cask
-  url "https://gw.alipayobjects.com/os/LightProxy/7372dabd-cb4e-43e5-98a2-e7f22785dc76/LightProxy.dmg"
+  url 'https://gw.alipayobjects.com/os/LightProxy/7372dabd-cb4e-43e5-98a2-e7f22785dc76/LightProxy.dmg'
   name 'LightProxy'
-  homepage 'https://github.com/alibaba/lightproxy'
+  homepage 'https://github.com/alibaba/lightproxy/'
 
   app 'LightProxy.app'
 end
