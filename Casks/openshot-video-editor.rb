a59270ee6620fd012d38e4adcba2a868e3711436
Yurii Kolesnykov <root@yurikoles.com>
2020-02-10 13:11:34 +0200

    Update openshot-video-editor from 2.4.4 to 2.5.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/openshot-video-editor.rb
+++ Casks/openshot-video-editor.rb
@@ -1,6 +1,6 @@
 cask 'openshot-video-editor' do
-  version '2.4.4'
-  sha256 '7b9ca5d916f96cb4600c210b9463400299e34c272121224639e33d5671c29613'
+  version '2.5.0'
+  sha256 '8b0efe95f92cae2580b91487211e56a91c95ac1e05676865bff027c590036130'
 
   # github.com/OpenShot/openshot-qt was verified as official when first introduced to the cask
   url "https://github.com/OpenShot/openshot-qt/releases/download/v#{version}/OpenShot-v#{version}-x86_64.dmg"
