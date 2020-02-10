6a39a54919ca5e023dd09d80e8337a859770b878
Randall <ran.dall@icloud.com>
2020-02-10 10:51:55 -0800

    Update duplicacy-web-edition from 1.1.0 to 1.2.0

--- Casks/duplicacy-web-edition.rb
+++ Casks/duplicacy-web-edition.rb
@@ -1,6 +1,6 @@
 cask 'duplicacy-web-edition' do
-  version '1.1.0'
-  sha256 '47297cccc35c64ec29093e1755bc93b89f4d1d940a339fc7903d0b55b67a13d1'
+  version '1.2.0'
+  sha256 '1f5da8d5e4fdf7cb5d184e697578f6fc3cc56c0d7a096ca9b3dbe332ce46dd66'
 
   # acrosync.com/duplicacy-web was verified as official when first introduced to the cask
   url "https://acrosync.com/duplicacy-web/duplicacy_web_osx_x64_#{version}.dmg"
