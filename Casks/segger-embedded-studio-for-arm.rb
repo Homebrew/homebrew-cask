a904e7bbc75a488b14d8b954051f3ceae7f7fa9a
suschizu <brigitte@corecode.io>
2020-02-10 22:04:13 +0100

    Update appcast in  segger-embedded-studio-for-arm.rb (#76810)
    * Update segger-embedded-studio-for-arm.rb
    
    * Update segger-embedded-studio-for-arm.rb
    
    * Update segger-embedded-studio-for-arm.rb

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -3,8 +3,7 @@ cask 'segger-embedded-studio-for-arm' do
   sha256 '471378e6d8b5db8fbbb7d27088df73d8fe6606df42594bc857a25058cde0593c'
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://www.segger.com/downloads/embedded-studio/EmbeddedStudio_ARM_Mac',
-          configuration: version.no_dots
+  appcast 'https://studio.segger.com/arm_segger_studio_release_notes.htm'
   name 'SEGGER Embedded Studio for ARM'
   homepage 'https://www.segger.com/products/development-tools/embedded-studio'
 
