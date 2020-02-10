245404be4065a80fe5b421e136397e2d21c0bc41
Vítor Galvão <info@vitorgalvao.com>
2020-02-10 17:56:13 +0000

    Update sitebulb.rb

--- Casks/sitebulb.rb
+++ Casks/sitebulb.rb
@@ -3,6 +3,7 @@ cask 'sitebulb' do
   sha256 'd651f4648cb71d51aab135f0f6932ca5eea5e995848ce4e7411862e7f5a305bc'
 
   url "https://downloads.sitebulb.com/#{version}/macOS/Sitebulb.dmg"
+  appcast 'https://sitebulb.com/download/'
   name 'Sitebulb'
   homepage 'https://sitebulb.com/'
 
