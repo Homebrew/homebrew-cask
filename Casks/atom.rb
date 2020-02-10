6e0c59c803bd129b82dcdef9d906795523c90fd3
LanikSJ <LanikSJ@users.noreply.github.com>
2020-02-10 13:33:23 -0800

    Update atom from 1.43.0 to 1.44.0 (#76874)

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,6 +1,6 @@
 cask 'atom' do
-  version '1.43.0'
-  sha256 'c7cce11fc9f22a8fdb9a836b321dc4b3d0cdadcb139a9dc947c2473ec0f0c7b1'
+  version '1.44.0'
+  sha256 'e679864702860ce7dbb5d944975dbf0aa9a23a11c9e9c089fb2097615bf8b626'
 
   # github.com/atom/atom was verified as official when first introduced to the cask
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
