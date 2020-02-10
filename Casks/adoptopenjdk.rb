8c98960eb8b75ad8c02d72604378435d6518b44b
George Adams <george.adams@microsoft.com>
2020-02-10 16:49:03 +0000

    Update Casks/adoptopenjdk.rb
    Co-Authored-By: Peter David Faria <zshift@gmail.com>

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -3,7 +3,7 @@ cask 'adoptopenjdk' do
   sha256 '29f1c87b3ecca38c50ea016657368af0a6a28ade5ba4150bd147ad91c524ea22'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
-  url 'https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.1%2B9/OpenJDK13U-jdk_x64_mac_hotspot_13.0.1_9.pkg'
+  url 'https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.pkg'
   appcast "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/latest"
   name 'AdoptOpenJDK Java Development Kit'
   homepage 'https://adoptopenjdk.net/'
