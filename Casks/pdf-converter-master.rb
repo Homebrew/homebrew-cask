234137cafece9f666db0cc326bb0cdb5114e75ba
suschizu <brigitte@corecode.io>
2020-02-11 22:50:45 +0100

    Update appcast in pdf-converter-master (#76912)
    * Update pdf-converter-master
    
    * Update pdf-converter-master.rb

--- Casks/pdf-converter-master.rb
+++ Casks/pdf-converter-master.rb
@@ -3,7 +3,8 @@ cask 'pdf-converter-master' do
   sha256 '11110406915ad5a3c657e876adb000260d939cd3cffa91ef2b4476a84cd128d6'
 
   url "https://www.lightenpdf.com/upload/download/pdf-converter-master-#{version.no_dots}.dmg"
-  appcast 'https://www.lightenpdf.com/xml/PDF-Converter-Mac.xml'
+  appcast 'https://www.lightenpdf.com/pdf-converter-mac.html',
+          configuration: version.no_dots
   name 'PDF Converter Master'
   homepage 'https://www.lightenpdf.com/pdf-converter-mac.html'
 
