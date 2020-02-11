ab2f81c8b0be9157af1c204a1f103559f6dcba3c
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2020-02-11 09:33:32 +0100

    Update brave-browser from 79.1.2.43,102.43 to 80.0.3987.87,103.115 (#76890)
    * Update brave-browser from 79.1.2.43,102.43 to 80.0.3987.87,103.115
    
    * Update brave-browser.rb

--- Casks/brave-browser.rb
+++ Casks/brave-browser.rb
@@ -1,6 +1,6 @@
 cask 'brave-browser' do
-  version '79.1.2.43,102.43'
-  sha256 'b5bc379c6b81cc1ce78fc27bf8b421129d7dab069759e047cf53a69f46710684'
+  version '80.1.3.115,103.115'
+  sha256 'c79cb879119a5ad95f4f2373b100d2e0469158e0b0b696a93b6a604bd1c20462'
 
   # updates-cdn.bravesoftware.com/sparkle/Brave-Browser was verified as official when first introduced to the cask
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/stable/#{version.after_comma}/Brave-Browser.dmg"
