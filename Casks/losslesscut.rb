703cba539cfcef0af66ccb21b3841347d8664b7e
404NetworkError <404NetworkError@users.noreply.github.com>
2020-02-11 17:49:41 -0500

    Update losslesscut from 2.6.3 to 2.7.1

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,6 +1,6 @@
 cask 'losslesscut' do
-  version '2.6.3'
-  sha256 'edfa24a36b4f6d639ff7717dacda8ce85ba7eae840892db80f1b525a0621c408'
+  version '2.7.1'
+  sha256 '87c049823b72b806c86bc0b4b631374c9ce1cc9e6ee08228e0676567d139f474'
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac.dmg"
   appcast 'https://github.com/mifi/lossless-cut/releases.atom'
