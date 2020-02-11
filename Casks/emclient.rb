430f5570a0475581801f244723cee4ead58424ae
Sergio Ortega Sanchez <ortega@tune-is.de>
2020-02-11 09:48:34 +0100

    Update emclient from 7.2.35700 to 7.2.37929 (#76891)

--- Casks/emclient.rb
+++ Casks/emclient.rb
@@ -1,6 +1,6 @@
 cask 'emclient' do
-  version '7.2.35700'
-  sha256 '22d877fd1d1c356e7590e4144ae2ac04013fa437eba2498a73d4e73e62846257'
+  version '7.2.37929'
+  sha256 '623421dcde73b9d1849dbfd23615ea0976e99d7b1c7be2f4f6b9281120e3a282'
 
   url "https://cdn-dist.emclient.com/dist/v#{version}_Mac/setup.pkg"
   appcast 'https://www.emclient.com/release-history?os=mac'
