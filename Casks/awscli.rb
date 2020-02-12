43727464618546ee9b190dc7c17a4ac1c23e9603
Rob Lazzurs <rob.lazzurs@amach.ie>
2020-02-12 13:20:56 +0000

    Adding the AWS CLI v2.
    This is a cask for the AWS CLI v2 as this is now packaged as a pkg
    rather than a python module to be installed with pip.

---
+++ Casks/awscli.rb
@@ -0,0 +1,21 @@
+cask 'awscli' do
+  version '2.0.0'
+  sha256 'a99c26cd2b5655970db91259a247f585aea32cfb7347e1a28a25b6f6484b16dc'
+
+  # awscli.amazonaws.com was verified as official when first introduced to the cask
+  url 'https://awscli.amazonaws.com/AWSCLIV2.pkg'
+  name 'AWS Command Line Interface'
+  homepage 'https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-mac.html'
+
+  auto_updates false
+  conflicts_with formula: 'awscli'
+  depends_on macos: '>= :high_sierra'
+
+  pkg "AWSCLIV#{version.major}.pkg"
+
+  uninstall pkgutil: 'com.amazon.aws.cli.*'
+
+  caveats do
+    files_in_usr_local
+  end
+end
