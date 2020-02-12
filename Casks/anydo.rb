ad355ed1f563583ee480b2ecb88bc3682eb01a49
Mohammed Essehemy <mohammedessehemy@gmail.com>
2020-02-12 15:40:18 +0200

    Update anydo from 4.2.63 to 4.2.64

--- Casks/anydo.rb
+++ Casks/anydo.rb
@@ -1,6 +1,6 @@
 cask 'anydo' do
-  version '4.2.63'
-  sha256 '9c5be13b7e1309e68de69c45786b0faf141cd6d35fd2b811061de33bbab028bb'
+  version '4.2.64'
+  sha256 '850f1377fbcd2e2c70f08b4c1374de05f23460685313bca775b119bbe3b348d9'
 
   url 'https://electron-app.any.do/Any.do.dmg'
   appcast 'https://electron-app.any.do/latest-mac.yml'
