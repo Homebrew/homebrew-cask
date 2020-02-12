428fd4c89b8b9a8841340a3fd5b0769c5f9b2937
Rob Lazzurs <rob.lazzurs@amach.ie>
2020-02-12 14:48:36 +0000

    Pushing correct version.
    Squished the previous commits to fix the commit history, now bringing
    the cask definition up to date with the final changes.

--- Casks/awscli.rb
+++ Casks/awscli.rb
@@ -3,12 +3,10 @@ cask 'awscli' do
   sha256 'a99c26cd2b5655970db91259a247f585aea32cfb7347e1a28a25b6f6484b16dc'
 
   # awscli.amazonaws.com was verified as official when first introduced to the cask
-  url 'https://awscli.amazonaws.com/AWSCLIV2.pkg'
+  url "https://awscli.amazonaws.com/AWSCLIV#{version.major}.pkg"
   name 'AWS Command Line Interface'
   homepage 'https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-mac.html'
 
-  auto_updates false
-  conflicts_with formula: 'awscli'
   depends_on macos: '>= :high_sierra'
 
   pkg "AWSCLIV#{version.major}.pkg"
