910e9fc9d8ebc7d0bba337e31db1b64e27482297
Randall <ran.dall@icloud.com>
2020-02-12 12:58:23 -0800

    Update deepl from 1.5.1 to 1.6.0

--- Casks/deepl.rb
+++ Casks/deepl.rb
@@ -1,6 +1,6 @@
 cask 'deepl' do
-  version '1.5.1'
-  sha256 '8385faf802b88158b25a4e6f65473a4a0cee7e64ffda870a18cfdad7341bc30c'
+  version '1.6.0'
+  sha256 '2982015c3bde03e909a79b14fdd910e0b4281f626bd851e1ab6182e7a9e90e13'
 
   url "https://www.deepl.com/macos/download/LpsA0EG5frbfX8p5/DeepL#{version}.zip"
   name 'DeepL'
