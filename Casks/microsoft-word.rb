652bf5bc938d69d6bbd738dcc927198b1dfe06a5
Qi Zhang <singularitti@outlook.com>
2020-02-11 20:52:33 -0500

    Update microsoft-word from 16.33.20011301 to 16.34.20020900

--- Casks/microsoft-word.rb
+++ Casks/microsoft-word.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-word' do
-  version '16.33.20011301'
-  sha256 'b9c8dbbd1320cce3a59b6a49b01d788a1225bdb0f096ee173dc70c358531d7e7'
+  version '16.34.20020900'
+  sha256 '35feec393e1995b6696ce99fdbaa64f3c5855d3a2a1dde7fec2113cbfbb34356'
 
   # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Word_#{version}_Installer.pkg"
