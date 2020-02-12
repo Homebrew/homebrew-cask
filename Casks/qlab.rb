14acd71d85468ee1fe8c9f7840594c4319f54161
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-12 13:08:15 -0800

    Update qlab from 4.6.1 to 4.6.2 (#77002)

--- Casks/qlab.rb
+++ Casks/qlab.rb
@@ -1,6 +1,6 @@
 cask 'qlab' do
-  version '4.6.1'
-  sha256 'a66a7f3c7142dbd35047ffb73e685f748d965349ef4f42c6fce85c71ef7b2c94'
+  version '4.6.2'
+  sha256 '21feecc79f6f19d3c8ecc8f700d89f8c6b260848c1daa8219eba58fbcec34c62'
 
   url "https://figure53.com/qlab/downloads/QLab-#{version}.zip"
   appcast "https://figure53.com/qlab/downloads/appcast-v#{version.major}/"
