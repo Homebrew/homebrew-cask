54c423fd7b96fa725188a1728943b9b6bcd20616
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-11 16:19:02 -0800

    Update cabal from 4.0.0 to 4.1.0 (#76922)

--- Casks/cabal.rb
+++ Casks/cabal.rb
@@ -1,6 +1,6 @@
 cask 'cabal' do
-  version '4.0.0'
-  sha256 '019f4c89a372dd5f7e3b5c6e8ccedda8e8ae6fa4b7ffa566f527c70f8d30bf60'
+  version '4.1.0'
+  sha256 '189a8e1569fcdbca626e941acd3f0b03548f3586e3bff66d538ff769b6930849'
 
   # github.com/cabal-club/cabal-desktop was verified as official when first introduced to the cask
   url "https://github.com/cabal-club/cabal-desktop/releases/download/v#{version}/cabal-desktop-#{version}-mac.dmg"
