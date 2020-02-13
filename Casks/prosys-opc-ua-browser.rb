4f82881bc566b54768d75139bd1902251817f434
suschizu <brigitte@corecode.io>
2020-02-13 17:41:57 +0100

    Update and rename prosys-opc-ua-client.rb to prosys-opc-ua-browser.rb

--- Casks/prosys-opc-ua-browser.rb
+++ Casks/prosys-opc-ua-browser.rb
@@ -1,11 +1,11 @@
-cask 'prosys-opc-ua-client' do
+cask 'prosys-opc-ua-browser' do
   version '4.0.2-231'
   sha256 '5886f887a282f2bd45066f8c6a85a9431457e26281e598bc35f1f44b9e58056a'
 
   url "https://www.prosysopc.com/opcua/apps/UaBrowser/dist/#{version}/prosys-opc-ua-browser-macos-#{version}.dmg"
-  appcast 'https://downloads.prosysopc.com/opc-ua-client-downloads.php'
-  name 'Prosys OPC UA Client'
+  appcast 'https://downloads.prosysopc.com/opc-ua-browser-downloads.php'
+  name 'Prosys OPC UA Browser'
   homepage 'https://www.prosysopc.com/products/opc-ua-client/'
 
-  app 'prosys-opc-ua-client.app'
+  installer manual: 'Prosys OPC UA Browser Installer.app'
 end
