ea5d12967ca26d766f44df9ce94cdbacefa5780a
Yurii Kolesnykov <root@yurikoles.com>
2020-02-13 09:03:12 +0200

    upwork 5.3.3.862,0gofuuatmjqd0n86
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/upwork.rb
+++ Casks/upwork.rb
@@ -1,6 +1,6 @@
 cask 'upwork' do
-  version '5.3.3.860,wub7hae1mtgzk09u'
-  sha256 '35e30606289ae2a1b02aebd0694cd01df33891cdda7f129174cac241b0b3cc92'
+  version '5.3.3.862,0gofuuatmjqd0n86'
+  sha256 '5ab0d83880dc262061edd642393e5eea346181de34c8498a4d945416555e0b02'
 
   url "https://updates-desktopapp.upwork.com/binaries/v#{version.before_comma.dots_to_underscores}_#{version.after_comma}/Upwork.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?url=https://updates-desktopapp.upwork.com/binaries/versions-mac.json&splitter_1=Beta&index_1=0'
