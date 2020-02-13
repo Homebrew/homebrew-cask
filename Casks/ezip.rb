cb8c51f6a20147137726eb4de19e1ef1f2214c78
Randall <ran.dall@icloud.com>
2020-02-13 13:51:20 -0800

    Update ezip from 1.8.1 to 1.8.2

--- Casks/ezip.rb
+++ Casks/ezip.rb
@@ -1,6 +1,6 @@
 cask 'ezip' do
-  version '1.8.1'
-  sha256 '08b4dd19663fb24d0174f82df686b61839cd8ae2199d5041cb4995151042052c'
+  version '1.8.2'
+  sha256 'ec2eb53e5b02131f2ff9bce5428c9ceaacc6b400fd820bd2b4e634d41932585f'
 
   url "https://cdn.awehunt.com/ezip/release/eZip_V#{version}.dmg"
   appcast 'https://ezip.awehunt.com/'
