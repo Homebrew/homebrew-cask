ea73adfe578d4d5c957b95d4de148e8a7652b2eb
Steven Seiden <53193445+StevenSeiden@users.noreply.github.com>
2020-02-13 19:31:13 -0600

    Add cydownload.app Latest

---
+++ Casks/cydownload.rb
@@ -0,0 +1,11 @@
+cask 'cydownload' do
+  version '1.1.1'
+  sha256 'b35df6bd89949b3321da4411091d5388d35bdf7cae54388ad853861c084e5ce2'
+
+  url "https://github.com/borishonman/cydownload/releases/download/v#{version}/cydownload-macos.zip"
+  appcast 'https://github.com/borishonman/cydownload/releases.atom'
+  name 'cydownload'
+  homepage 'cydownload'
+
+  app 'cydownload.app'
+end
