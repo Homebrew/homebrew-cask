f44e06e5decbc970931298f368bd772c2ce64c32
suschizu <brigitte@corecode.io>
2020-02-14 13:48:29 +0100

    Update bria.rb

--- Casks/bria.rb
+++ Casks/bria.rb
@@ -1,6 +1,6 @@
 cask 'bria' do
   version '6.0.3_102645'
-  sha256 '9730d408fabecc1cfb6e525d64973ea1cb6b7819e7f7f22863740557a50663a6'
+  sha256 'e1acfe6f624f276f318aa77504b3b11192b2c31acf89ed0b16c55618a1710c50'
 
   # s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://counterpath.s3.amazonaws.com/downloads/Bria_#{version}.dmg"
