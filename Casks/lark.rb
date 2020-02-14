3f62c27d635ed862b4139535e0c010e7bb7657eb
Vítor Galvão <info@vitorgalvao.com>
2020-02-14 12:33:41 +0000

    Update lark.rb

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -5,7 +5,7 @@ cask 'lark' do
   # sf3-ttcdn-tos.pstatp.com was verified as official when first introduced to the cask
   url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/Lark-#{version}.dmg"
   appcast 'https://www.larksuite.com/api/downloads'
-  name 'lark'
+  name 'Lark'
   homepage 'https://www.larksuite.com/'
 
   depends_on macos: '>= :mavericks'
