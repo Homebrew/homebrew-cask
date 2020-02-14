3f473a860646bf0caef704787139c427a1b4e2e9
suschizu <brigitte@corecode.io>
2020-02-14 14:56:18 +0100

    Revert "Update clashx from 1.17.1 to 1.17.2 (#76814)"
    This reverts commit 421bb047e6d76542f6739eacd11cad3b7bd743d5.

--- Casks/clashx.rb
+++ Casks/clashx.rb
@@ -1,6 +1,6 @@
 cask 'clashx' do
-  version '1.17.2'
-  sha256 '816f796977457b86b7a9d49865a02bd9f6f542b0f4a8fa3315f3db499b0bb8c1'
+  version '1.17.1'
+  sha256 'b18a5b73b3955ef04aa9cb6650d4adca4ba15c25d9a031db1ed686a4b91c620c'
 
   url "https://github.com/yichengchen/clashX/releases/download/#{version}/ClashX.dmg"
   appcast 'https://github.com/yichengchen/clashX/releases.atom'
