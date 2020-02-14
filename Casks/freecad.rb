ec28b41aac23f9e5078f89899949ab01bf0ef26b
Philip Belemezov <philip@belemezov.net>
2020-02-14 23:36:19 +0100

    Upgrade FreeCad from 0.18.4-16146 to 0.19-19566

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -1,9 +1,9 @@
 cask 'freecad' do
-  version '0.18.4,16146'
-  sha256 :no_check # required as upstream package is updated in-place
+  version '0.19.pre,19566'
+  sha256 'ede7a0e6da913bb6051147d5910c8113bc3ce21a8bfb123a67124099a2f7c3c9'
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
-  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor}-#{version.after_comma}-OSX-x86_64-conda-Qt5-Py3.dmg"
+  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.major_minor}_#{version.patch}/FreeCAD_#{version.major_minor}-#{version.after_comma}-macOS-x86_64-conda-Qt5-Py3.dmg"
   appcast 'https://github.com/FreeCAD/FreeCAD/releases.atom'
   name 'FreeCAD'
   homepage 'https://www.freecadweb.org/'
