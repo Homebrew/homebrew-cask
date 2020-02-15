36b7a72e041dee6e30463c66ee079a16772946f6
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-02-15 11:33:28 +0100

    Update hubstaff from 1.5.7,1893 to 1.5.8,1992 (#77143)

--- Casks/hubstaff.rb
+++ Casks/hubstaff.rb
@@ -1,6 +1,6 @@
 cask 'hubstaff' do
-  version '1.5.7,1893'
-  sha256 'ba062167e9657c0046f5265ffc52a32e998c425534b6b38aa062b0f070b9ba3d'
+  version '1.5.8,1992'
+  sha256 '99a65518c72c66c36983b38e7e17d0a4c920fa9914dd2ed4f0cd33d0f5a21d10'
 
   url "https://app.hubstaff.com/download/#{version.after_comma}-mac-os-x-#{version.before_comma.dots_to_hyphens}-release"
   appcast 'https://app.hubstaff.com/appcast.xml'
