912631a18545b3ecd2b406e0b53be8347e3923d4
Xiao Yu <me@xyu.io>
2020-02-15 21:18:51 +0000

    Upgrade Capture One 12.1.4 -> 12.1.5.8

--- Casks/capture-one.rb
+++ Casks/capture-one.rb
@@ -1,6 +1,6 @@
 cask 'capture-one' do
-  version '12.1.4'
-  sha256 '8bcd5e25336e2766fa11a12dc499393e223f8f17ae3c7f6db413cc89c047265d'
+  version '12.1.5.8'
+  sha256 '2eed05d94ab32b41d788b799e20ce28e80a54bdd5cdda66555aef4b6e33b208b'
 
   url "https://downloads.phaseone.com/d972230a-e941-47ca-a751-35f57a3f2d94/International/CaptureOne12.Mac.#{version}.dmg"
   appcast "https://cormws.phaseone.com/corm.asmx/GetNewSoftwareVersion?Platform=Mac&Version=#{version}"
