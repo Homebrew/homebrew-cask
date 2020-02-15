ad32aa832ef0777366ae1b83396d71bdf8ad75de
Randall <ran.dall@icloud.com>
2020-02-15 11:50:15 -0800

    Update wechatwork from 3.0.7.2053 to 3.0.8.2067

--- Casks/wechatwork.rb
+++ Casks/wechatwork.rb
@@ -1,6 +1,6 @@
 cask 'wechatwork' do
-  version '3.0.7.2053'
-  sha256 '9c15ebc28a8c8f54fb269fcf1dcf383183b3e7c3bc2b5f3acd6099cf336a8822'
+  version '3.0.8.2067'
+  sha256 '855bf829d5db34195ce4761c1eb3e5f21e7b9b2dbe4b1c312779ccc5fbb2c905'
 
   url "https://dldir1.qq.com/wework/work_weixin/WXWork_#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://work.weixin.qq.com/wework_admin/commdownload?platform=mac'
