2f9d100a2d9468239b9800efc033cab8679e2bad
suschizu <brigitte@corecode.io>
2020-02-16 21:37:47 +0100

    Update and rename balsamiq-mockups.rb to balsamiq-wireframes.rb from 3.5.17 to 4.0.19 (#77188)
    * Update and rename balsamiq-mockups.rb to balsamiq-wireframes.rb
    
    I haven't found a proper appcast until now
    
    * Update balsamiq-wireframes.rb
    
    * Update balsamiq-wireframes.rb
    
    * Update balsamiq-wireframes.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -1,13 +1,13 @@
-cask 'balsamiq-mockups' do
-  version '3.5.17'
-  sha256 'a89f2a7959bc7f987710b7f3d6ae665e5765180dd02763d1b5f953415c130df2'
+cask 'balsamiq-wireframes' do
+  version '4.0.19'
+  sha256 '04f850df8dad70927589dadc56385c07a4aa49b4fca7f5ed8c6bfc210bde4899'
 
-  url "https://builds.balsamiq.com/mockups-desktop/Balsamiq_Mockups_#{version}.dmg"
-  appcast 'https://builds.balsamiq.com/mockups-desktop/version.jsonp'
-  name 'Balsamiq Mockups'
+  url "https://builds.balsamiq.com/bwd/Balsamiq%20Wireframes%20#{version}.dmg"
+  appcast 'https://builds.balsamiq.com/bwd/mac.jsonp'
+  name 'Balsamiq Wireframes'
   homepage 'https://balsamiq.com/'
 
-  app "Balsamiq Mockups #{version.major}.app"
+  app 'Balsamiq Wireframes.app'
 
   zap trash: [
                "~/Library/Caches/BalsamiqMockups#{version.major}.*",
