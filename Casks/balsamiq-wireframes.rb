828113a869c2a4865e1f1bff5df9f17bb764e45e
Vítor Galvão <info@vitorgalvao.com>
2020-02-16 20:17:32 +0000

    Update balsamiq-wireframes.rb

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -2,8 +2,7 @@ cask 'balsamiq-wireframes' do
   version '4.0.19'
   sha256 '04f850df8dad70927589dadc56385c07a4aa49b4fca7f5ed8c6bfc210bde4899'
 
-  # build_archives.s3.amazonaws.com/wireframes-desktop was verified as official when first introduced to the cask
-  url "https://build_archives.s3.amazonaws.com/wireframes-desktop/mac/Balsamiq%20Wireframes%20#{version}.dmg"
+  url "https://builds.balsamiq.com/bwd/Balsamiq%20Wireframes%20#{version}.dmg"
   appcast 'https://builds.balsamiq.com/bwd/mac.jsonp'
   name 'Balsamiq Wireframes'
   homepage 'https://balsamiq.com/'
