ff5e3658a2178f291ca643aade78cfda773fcffb
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-02-16 13:52:05 +0100

    Update wpsoffice from 1.9.1,2994 to 1.9.2,3124 (#77182)

--- Casks/wpsoffice.rb
+++ Casks/wpsoffice.rb
@@ -1,9 +1,9 @@
 cask 'wpsoffice' do
-  version '1.9.1,2994'
-  sha256 '0e24e91fba32b03e17c520b1037ca9c1df84042c9b6dc54812fb9da7496156b6'
+  version '1.9.2,3124'
+  sha256 'bc717e99c790483580583e32f8305e00743b3cceb1aac39a4b8a86e2c33a565a'
 
   # package.mac.wpscdn.cn was verified as official when first introduced to the cask
-  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.before_comma}/WPS_Office_#{version.before_comma}(#{version.after_comma}).dmg"
+  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version.before_comma}/WPS_Office_#{version.before_comma}_#{version.after_comma}.dmg"
   appcast 'https://www.wps.cn/product/wpsmac/'
   name 'WPS Office'
   homepage 'https://www.wps.cn/product/wpsmac/'
