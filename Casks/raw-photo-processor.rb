e2c3c048e6a9e8c777e4996c9774c803b5a61b39
Vítor Galvão <info@vitorgalvao.com>
2020-02-16 20:27:16 +0000

    raw-photo-processor.rb: back to stable

--- Casks/raw-photo-processor.rb
+++ Casks/raw-photo-processor.rb
@@ -1,8 +1,8 @@
 cask 'raw-photo-processor' do
-  version '1876Beta'
+  version '4.8.0,1879'
   sha256 '458d9912db08ba1d874bed49242a51e2d01f32220ecc07cc7449fd5352e8bf3d'
 
-  url "https://www.raw-photo-processor.com/RPP/RPP64_#{version}.zip"
+  url 'https://www.raw-photo-processor.com/RPP/RPP_64.zip'
   appcast 'https://www.raw-photo-processor.com/rpp_updates_64.xml'
   name 'Raw Photo Processor'
   homepage 'https://www.raw-photo-processor.com/RPP/Overview.html'
