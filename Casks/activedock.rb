cd4052777f03c16594b4ab3a0723b53b1a89cb79
suschizu <brigitte@corecode.io>
2020-02-16 21:07:50 +0100

    Update activedock from 1.53 to 1.54 (#77160)
    * Update activedock from 1.53 to 1.54
    
    * Update activedock.rb
    
    * Update activedock.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,10 +1,9 @@
 cask 'activedock' do
-  version '1.53'
-  sha256 'd37db887147f9027278ee030406d5a00e72f5cb3acae390ee51f4d695911e265'
+  version '1.54'
+  sha256 '95206e53fa4d62d8ad0b9eb3cd42c1ac7a552bc0e2a2e49d345c631ea19a1890'
 
-  # s3.amazonaws.com/downloads.macplussoftware was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/downloads.macplussoftware/ActiveDock.zip'
-  appcast 'https://s3.amazonaws.com/downloads.macplussoftware/ActiveDock.xml'
+  url 'https://noteifyapp.com/download/ActiveDock.dmg'
+  appcast 'https://macplus-software.com/downloads/ActiveDock.xml'
   name 'ActiveDock'
   homepage 'https://www.noteifyapp.com/activedock/'
 
