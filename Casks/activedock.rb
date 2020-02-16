a78a9652265ff7574d85e8594c142ff89316348b
Vítor Galvão <info@vitorgalvao.com>
2020-02-16 19:10:05 +0000

    Update activedock.rb

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -2,8 +2,7 @@ cask 'activedock' do
   version '1.54'
   sha256 'd37db887147f9027278ee030406d5a00e72f5cb3acae390ee51f4d695911e265'
 
-  # s3.amazonaws.com/downloads.macplussoftware was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/downloads.macplussoftware/ActiveDock.zip'
+  url 'https://noteifyapp.com/download/ActiveDock.dmg'
   appcast 'https://macplus-software.com/downloads/ActiveDock.xml'
   name 'ActiveDock'
   homepage 'https://www.noteifyapp.com/activedock/'
