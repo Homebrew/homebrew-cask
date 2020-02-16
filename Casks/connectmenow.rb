f9fd07e59902c604617df1437456eb6a49b1f8d4
Vítor Galvão <info@vitorgalvao.com>
2020-02-16 19:07:03 +0000

    Update connectmenow.rb

--- Casks/connectmenow.rb
+++ Casks/connectmenow.rb
@@ -3,8 +3,9 @@ cask 'connectmenow' do
   sha256 'c14b84edd99e94d679308593e75dc818170313e650e98ecbc8c918655564a99c'
 
   url "https://www.tweaking4all.com/downloads/network/ConnectMeNow-v#{version}-macOS.dmg"
+  appcast 'https://www.tweaking4all.com/os-tips-and-tricks/macosx-tips-and-tricks/connectmenow-v3'
   name 'ConnectMeNow'
-  homepage 'https://www.tweaking4all.com/os-tips-and-tricks/macosx-tips-and-tricks/connectmenow-v3'
+  homepage 'https://www.tweaking4all.com/os-tips-and-tricks/macosx-tips-and-tricks/connectmenow-v3/'
 
   app 'ConnectMeNow.app'
 end
