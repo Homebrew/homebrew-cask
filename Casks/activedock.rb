be3c7a59c2228d5be171e3f5024d6ef2c380e6c0
Vítor Galvão <info@vitorgalvao.com>
2020-02-16 19:58:46 +0000

    Update activedock.rb

--- Casks/activedock.rb
+++ Casks/activedock.rb
@@ -1,6 +1,6 @@
 cask 'activedock' do
   version '1.54'
-  sha256 'd37db887147f9027278ee030406d5a00e72f5cb3acae390ee51f4d695911e265'
+  sha256 '95206e53fa4d62d8ad0b9eb3cd42c1ac7a552bc0e2a2e49d345c631ea19a1890'
 
   url 'https://noteifyapp.com/download/ActiveDock.dmg'
   appcast 'https://macplus-software.com/downloads/ActiveDock.xml'
