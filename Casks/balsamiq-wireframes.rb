06f7fbc504d662976e435950b77992814dfc22e4
suschizu <brigitte@corecode.io>
2020-02-16 17:48:49 +0100

    Update balsamiq-wireframes.rb

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -2,12 +2,13 @@ cask 'balsamiq-wireframes' do
   version '4.0.19'
   sha256 '04f850df8dad70927589dadc56385c07a4aa49b4fca7f5ed8c6bfc210bde4899'
 
+  # build_archives.s3.amazonaws.com/wireframes-desktop was verified as official when first introduced to the cask
   url "https://build_archives.s3.amazonaws.com/wireframes-desktop/mac/Balsamiq%20Wireframes%20#{version}.dmg"
   appcast 'https://balsamiq.com/wireframes/desktop/'
   name 'Balsamiq Wireframes'
   homepage 'https://balsamiq.com/'
 
-  app "Balsamiq Wireframes.app"
+  app 'Balsamiq Wireframes.app'
 
   zap trash: [
                "~/Library/Caches/BalsamiqMockups#{version.major}.*",
