18bb1a7f4e83d0b0ac962c5c4a0ee72df0a55cd5
Work <brigitte@corecode.io>
2020-02-17 16:30:49 +0100

    Update captin from 1.0.21,111:1577032431 to 1.0.22,116:1581835006

--- Casks/captin.rb
+++ Casks/captin.rb
@@ -1,6 +1,6 @@
 cask 'captin' do
-  version '1.0.21,111:1577032431'
-  sha256 'be20bf61be05b00fef65a9fe28032cd142fad90611c34b239c7f3204c6ece847'
+  version '1.0.22,116:1581835006'
+  sha256 '683f1050a96f1ccaef954ed8c29fe6e7b75bd45baeb930c63a3e3c972efe5836'
 
   # dl.devmate.com/com.100hps.captin was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.100hps.captin/#{version.after_comma.before_colon}/#{version.after_colon}/Captin-#{version.after_comma.before_colon}.dmg"
