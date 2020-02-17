6dc0c19aa89d4957da984c2765d216f728177118
Hannes Braun <hannesbraun@mail.de>
2020-02-17 21:10:08 +0100

    Update segger-embedded-studio-for-arm from 4.42a to 4.50

--- Casks/segger-embedded-studio-for-arm.rb
+++ Casks/segger-embedded-studio-for-arm.rb
@@ -1,6 +1,6 @@
 cask 'segger-embedded-studio-for-arm' do
-  version '4.42a'
-  sha256 '43bd4345de952a957b87e1b6cedc58b019ffd74fa00d599388229f5e02cb5395'
+  version '4.50'
+  sha256 '93435340cf7038a103f88d1c4a17492aa86af228018de9e621ed37a334a7b9e4'
 
   url "https://www.segger.com/downloads/embedded-studio/Setup_EmbeddedStudio_ARM_v#{version.no_dots}_macos_x64.dmg"
   appcast 'https://studio.segger.com/arm_segger_studio_release_notes.htm'
