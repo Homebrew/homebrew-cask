30b2bebeffabd9133f119149f0af83d3fd685f29
i0ntempest <szf1234@me.com>
2020-02-17 15:41:52 -0500

    Update malus

--- Casks/malus.rb
+++ Casks/malus.rb
@@ -1,9 +1,9 @@
 cask 'malus' do
-  version '0.9.0'
-  sha256 '77ceaa6b08bebc169c16cbd3ce179075334949335a9ba606cb5fc1504cf8e58d'
+  version '0.9.0-1'
+  sha256 '393ed66263391028981e89ab67a0ee31d1ee624f3c1b00bb2e876501116751ce'
 
   # malusfile.com was verified as official when first introduced to the cask
-  url "https://malusfile.com/uploads/Malus-#{version}.dmg"
+  url "https://malusfile.com/uploads/Malus.#{version}.dmg"
   appcast 'https://api.getmalus.com/api/checkDesktopUpdate?type=mac'
   name 'Malus'
   homepage 'https://getmalus.com/'
