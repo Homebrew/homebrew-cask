55ec4e54e71923cebc07bdb0fe35f8ca5c7c3e06
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-17 11:23:28 -0800

    Update musictube from 1.11 to 1.11.1 (#77244)

--- Casks/musictube.rb
+++ Casks/musictube.rb
@@ -1,6 +1,6 @@
 cask 'musictube' do
-  version '1.11'
-  sha256 'ff4a2db171b480f3feec212c5aef5eef831af62ae182bf091f465378a685872a'
+  version '1.11.1'
+  sha256 'b2a7501cb07ef2d78a83dd5e1b8bbd8194e8db464b35a9b1a77b2aed5eb08948'
 
   url 'https://flavio.tordini.org/files/musictube/musictube.dmg'
   appcast 'https://flavio.tordini.org/musictube-ws/appcast.xml'
