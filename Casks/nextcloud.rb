7c07bcaf3585a47fb1fda9b95afef20abf19be4c
Sergio Ortega Sanchez <ortega@tune-is.de>
2020-02-17 08:49:07 +0100

    Update nextcloud.rb
    Update to 2.6.3.20200217 / 2.6.3.20200217-legacy

--- Casks/nextcloud.rb
+++ Casks/nextcloud.rb
@@ -1,9 +1,9 @@
 cask 'nextcloud' do
   if MacOS.version <= :el_capitan
-    version '2.6.2.20191224-legacy'
+    version '2.6.3.20200217-legacy'
     sha256 '65a507096658b9128c122f117f6326fc96bb0397cfc9940daf4eb2d4b8b5997c'
   else
-    version '2.6.2.20191224'
+    version '2.6.3.20200217'
     sha256 'dc92a94370963d8ee74693a02b761fac8dd040e4c35c308c3df63eedcaf7ca1b'
   end
 
