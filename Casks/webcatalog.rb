c1da0e1c6ad9d53c0d9a88efc46d154d1f744a7c
Quang Lam <quang.lam2807@gmail.com>
2020-02-16 20:42:48 -0600

    Add WebCatalog.app v19.3.3 (#77195)
    * Add WebCatalog.app v19.3.3
    
    * Update webcatalog.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/webcatalog.rb
@@ -0,0 +1,22 @@
+cask 'webcatalog' do
+  version '19.3.3'
+  sha256 '36212353ff110a3325d259a83b419cfa3d2bcf827236801a02fcbe21d120c151'
+
+  # github.com/quanglam2807/webcatalog was verified as official when first introduced to the cask
+  url "https://github.com/quanglam2807/webcatalog/releases/download/v#{version}/WebCatalog-#{version}-mac.zip"
+  appcast 'https://github.com/quanglam2807/webcatalog/releases.atom'
+  name 'WebCatalog'
+  homepage 'https://getwebcatalog.com/'
+
+  auto_updates true
+
+  app 'WebCatalog.app'
+
+  zap trash: [
+               '~/Library/Application Support/WebCatalog',
+               '~/Library/Caches/com.webcatalog.jordan',
+               '~/Library/Caches/com.webcatalog.jordan.ShipIt',
+               '~/Library/Preferences/com.webcatalog.jordan.plist',
+               '~/Library/Saved Application State/com.webcatalog.jordan.savedState',
+             ]
+end
