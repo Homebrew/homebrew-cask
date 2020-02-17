20444fbe53e55120c468bf760f9cf0064cc8c79c
Vítor Galvão <info@vitorgalvao.com>
2020-02-17 02:36:00 +0000

    Update webcatalog.rb

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -2,7 +2,7 @@ cask 'webcatalog' do
   version '19.3.3'
   sha256 '36212353ff110a3325d259a83b419cfa3d2bcf827236801a02fcbe21d120c151'
 
-  # github.com/quanglam2807/webcatalog/ was verified as official when first introduced to the cask
+  # github.com/quanglam2807/webcatalog was verified as official when first introduced to the cask
   url "https://github.com/quanglam2807/webcatalog/releases/download/v#{version}/WebCatalog-#{version}-mac.zip"
   appcast 'https://github.com/quanglam2807/webcatalog/releases.atom'
   name 'WebCatalog'
