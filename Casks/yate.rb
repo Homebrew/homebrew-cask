4fd4b5b6fa120327fafa1056fa36fe4eebdcabb1
Randall <ran.dall@icloud.com>
2020-02-17 10:46:27 -0800

    Update yate from 5.1.0.1 to 5.1.1

--- Casks/yate.rb
+++ Casks/yate.rb
@@ -1,6 +1,6 @@
 cask 'yate' do
-  version '5.1.0.1'
-  sha256 '22fcecb4eefd22b677a5745e6a3ef56983f1819d6a1aec452333e3919cb06001'
+  version '5.1.1'
+  sha256 '4a6f928c9041d94815ff5e13e6fc69336387ac1f27b2697d2bc2f2c16d6b30c5'
 
   url 'https://2manyrobots.com/Updates/Yate/Yate.zip'
   appcast 'https://2manyrobots.com/Updates/Yate/appcast.xml'
