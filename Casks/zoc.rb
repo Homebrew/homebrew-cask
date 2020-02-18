5d30061d007f69d1ad6bdd5cb6b5ebc8f6d00815
Randall <ran.dall@icloud.com>
2020-02-18 11:43:40 -0800

    Update zoc from 7.25.2 to 7.25.3

--- Casks/zoc.rb
+++ Casks/zoc.rb
@@ -1,6 +1,6 @@
 cask 'zoc' do
-  version '7.25.2'
-  sha256 'c38c761d3602477bc663750599d47a7908e945672b81a759364434de09cee4a3'
+  version '7.25.3'
+  sha256 'b4704781b94c99e27aa8343369d711a0e82bbb904431b9da55f7233def300bef'
 
   url "https://www.emtec.com/downloads/zoc/zoc#{version.no_dots}.dmg"
   appcast 'https://www.emtec.com/downloads/zoc/zoc_changes.txt'
