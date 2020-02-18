b274e4251f979f7d77aca052ac7673f54b6049cf
Julian Mayer <julian@corecode.io>
2020-02-17 18:59:30 -0800

    Update popsql from latest to 0.9.2

--- Casks/popsql.rb
+++ Casks/popsql.rb
@@ -1,8 +1,10 @@
 cask 'popsql' do
-  version :latest
-  sha256 :no_check
+  version '0.9.2'
+  sha256 '9e97c094d3217a4a5f4ab93751615da5793d0a1c545d871f36f77d14e5ac9920'
 
-  url 'https://get.popsql.io/download/mac'
+  # popsql-releases.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://popsql-releases.s3.amazonaws.com/mac/PopSQL-#{version}-mac.zip"
+  appcast 'https://popsql-releases.s3.amazonaws.com/mac/latest-mac.yml'
   name 'PopSQL'
   homepage 'https://popsql.io/'
 
