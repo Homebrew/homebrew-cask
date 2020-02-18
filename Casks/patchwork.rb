373c1c63364d55c89df74e65dfa3914c2c38917e
Miikka Koskinen <miikka@metosin.fi>
2020-02-18 08:04:49 +0200

    Update patchwork from 3.17.2 to 3.17.3 (#77256)

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,6 +1,6 @@
 cask 'patchwork' do
-  version '3.17.2'
-  sha256 '53d08948316fb31dfe23a72cec40747267b4561b0de6f3f241ed8710b160a1b2'
+  version '3.17.3'
+  sha256 '69cbe8e2b09fbbde8918d94974222da869058ab799a41e0082af17466b16011b'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom'
