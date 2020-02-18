fa70f4012f7cb29ef53118d66ca35062ad861dbb
Randall <ran.dall@icloud.com>
2020-02-18 10:58:40 -0800

    Update museeks from 0.11.1 to 0.11.2

--- Casks/museeks.rb
+++ Casks/museeks.rb
@@ -1,6 +1,6 @@
 cask 'museeks' do
-  version '0.11.1'
-  sha256 '78af4edb2b393176aa60577e6e5858bd25f5db0c0bda70a2695c8869644a4dd8'
+  version '0.11.2'
+  sha256 '51b68a6a78ada4c723ce2f3483cbb94e5656950a4e399aae2d5dc4e9cb64aeea'
 
   # github.com/martpie/museeks was verified as official when first introduced to the cask
   url "https://github.com/martpie/museeks/releases/download/#{version}/museeks.dmg"
