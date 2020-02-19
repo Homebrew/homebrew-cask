cf54d0c00a0f9e896109a5cfb9da38bb0fab4c72
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-19 12:05:10 -0800

    Update opencore-configurator from 1.14.1.1 to 1.15.0.0 (#77353)

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,6 +1,6 @@
 cask 'opencore-configurator' do
-  version '1.14.1.1'
-  sha256 'c74e2d314ec3d2824cc1166dc4811be1b5e6465276024d41d40ca13a62f00bdc'
+  version '1.15.0.0'
+  sha256 '48952b3558a5553ec61ae8d39c434293017213517cf77ae3b7b2dcf11124934e'
 
   url 'https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last'
   appcast 'https://mackie100projects.altervista.org/category/opencore-configurator-changelog/'
