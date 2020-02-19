c90f390abf62f4f2a16cd2550c020b2b2bfba1e2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-19 12:49:32 -0800

    Update balsamiq-wireframes from 4.0.19 to 4.0.20 (#77362)

--- Casks/balsamiq-wireframes.rb
+++ Casks/balsamiq-wireframes.rb
@@ -1,6 +1,6 @@
 cask 'balsamiq-wireframes' do
-  version '4.0.19'
-  sha256 '04f850df8dad70927589dadc56385c07a4aa49b4fca7f5ed8c6bfc210bde4899'
+  version '4.0.20'
+  sha256 'c364584ae8cbbcf84df39129c4f2fabdd9c060658ba794e8901677b7e4bfe51b'
 
   url "https://builds.balsamiq.com/bwd/Balsamiq%20Wireframes%20#{version}.dmg"
   appcast 'https://builds.balsamiq.com/bwd/mac.jsonp'
