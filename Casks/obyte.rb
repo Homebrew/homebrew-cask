545a090b90e67e0be134d98df419a0e5dba529f2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-19 12:06:54 -0800

    Update obyte from 2.8.0 to 3.0.0 (#77348)

--- Casks/obyte.rb
+++ Casks/obyte.rb
@@ -1,6 +1,6 @@
 cask 'obyte' do
-  version '2.8.0'
-  sha256 '77b76c63a50aa8bfd62e52be815ff1049fe9810581b8affb1b1f3ab1471f198c'
+  version '3.0.0'
+  sha256 '0aae60f20ef57e958c5d9ce353b9680a73f2e0ec4e71ecf37b71aedf5c378dce'
 
   # github.com/byteball/obyte-gui-wallet was verified as official when first introduced to the cask
   url "https://github.com/byteball/obyte-gui-wallet/releases/download/v#{version}/Obyte-osx64.dmg"
