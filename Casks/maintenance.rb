a3bdc51fdc5b7742a9de000c30d11f85d9a0433a
suschizu <brigitte@corecode.io>
2020-02-20 20:01:41 +0100

    Update maintenance.rb

--- Casks/maintenance.rb
+++ Casks/maintenance.rb
@@ -19,7 +19,7 @@ cask 'maintenance' do
     sha256 'd3b0152ce543b84ed597daba3360f74c3f20b4fb2b41d71005f3a7b311d4d681'
   else
     version '2.6.4'
-    sha256 'd3b0152ce543b84ed597daba3360f74c3f20b4fb2b41d71005f3a7b311d4d681'
+    sha256 'be94c093f030240da2a63207a8c8d2a16e64a7aac1c04e225ac6ffb3d3f0844e'
   end
 
   url "https://www.titanium-software.fr/download/#{MacOS.version.to_s.delete('.')}/Maintenance.dmg"
