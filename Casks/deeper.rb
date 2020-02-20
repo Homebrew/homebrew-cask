9217f031237cebd95220c693e811c633b43156e3
suschizu <brigitte@corecode.io>
2020-02-20 20:04:34 +0100

    Update deeper.rb

--- Casks/deeper.rb
+++ Casks/deeper.rb
@@ -21,7 +21,7 @@ cask 'deeper' do
     sha256 '13dbe7bd680963aca91c40a4fd1e16648b63538f3213692db22dd91a3e3f2c89'
   else
     version '2.5.3'
-    sha256 '13dbe7bd680963aca91c40a4fd1e16648b63538f3213692db22dd91a3e3f2c89'
+    sha256 '8d2474185307123f5ab4747af2cc5731691fb888f4e2db0d7d5072d3bc8e4332'
   end
 
   url "https://www.titanium-software.fr/download/#{macos_release}/Deeper.dmg"
