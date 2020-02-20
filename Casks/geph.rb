e3c246bf53dfd13bef9efd50d2bb4bc96012b598
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@ProtonMail.com>
2020-02-20 13:09:16 +0800

    Update geph from 3.3.0 to 3.4.0

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -1,6 +1,6 @@
 cask 'geph' do
-  version '3.3.0'
-  sha256 '724cd4eda0dc3c656afc44fb08359658c72cfa0173b834ea0cc8cad6dbcd7ce6'
+  version '3.4.0'
+  sha256 'ee8187acac69f62e448fa1515269f29a52c1cc54205a4ccede3d7a409b4c4d7d'
 
   url "https://dl.geph.io/desktop-builds/geph-macos-#{version}.dmg"
   appcast 'https://geph.io/'
