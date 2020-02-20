b9934c720d1c161c0f8a4023a9277ee128f4b53e
Randall <ran.dall@icloud.com>
2020-02-20 15:02:33 -0800

    Update runway from 0.10.27 to 0.10.28

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -1,6 +1,6 @@
 cask 'runway' do
-  version '0.10.27'
-  sha256 '27a463abc167de15ece1fb9dee6a68fb3202749965cdf6b960ca630735620ef0'
+  version '0.10.28'
+  sha256 'b59c4bfa2b00e2f84a6579c486fc37fe0250189f1441f5ad60259012c91aacf5'
 
   # runway-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg"
