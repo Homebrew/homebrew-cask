ff100e216d9a6e109476fa579a7cb74c8301d800
Alexander Moiseenko <WolfMeister@users.noreply.github.com>
2020-02-20 12:32:37 +0300

    Update telegram from 5.9.1.189475 to 5.9.2.191422

--- Casks/telegram.rb
+++ Casks/telegram.rb
@@ -1,6 +1,6 @@
 cask 'telegram' do
-  version '5.9.1.189475'
-  sha256 'a0574871473b9c3db891675b77c5af3323b1323a1f60f3039bcd58c2e0765dc3'
+  version '5.9.2.191422'
+  sha256 '993b9d1e3d7946ad01a93f7ba766d0326725b86cd423b150fd2a4092f490418a'
 
   url "https://osx.telegram.org/updates/Telegram-#{version}.app.zip"
   appcast 'https://osx.telegram.org/updates/versions.xml'
