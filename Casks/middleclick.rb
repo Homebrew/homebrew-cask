39b4901611ab6b0842022ab22f072f2433a3c17b
Arthur Factor <46137336+DaFuqtor@users.noreply.github.com>
2020-02-22 00:12:02 +0300

    Update middleclick from 2.4.8 to 2.4.8.1

--- Casks/middleclick.rb
+++ Casks/middleclick.rb
@@ -1,6 +1,6 @@
 cask 'middleclick' do
-  version '2.4.8'
-  sha256 '764b74654129267b40fb7ff1c733b6acf3b61613b90bbf4bb63e0cfef60e0700'
+  version '2.4.8.1'
+  sha256 'c17d944d9305d6da6c949ab1d5032326668d0e9dbd38ef3061955af4484d3f62'
 
   url "https://github.com/DaFuqtor/MiddleClick/releases/download/#{version}/MiddleClick.zip"
   appcast 'https://github.com/DaFuqtor/MiddleClick/releases.atom'
