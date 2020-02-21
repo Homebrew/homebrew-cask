74d87dbacba9c76222262cbf6bb02f36097d44bf
suschizu <brigitte@corecode.io>
2020-02-21 11:48:33 +0100

    Update opencore-configurator from 1.15.0.0 to 1.15.0.1 (#77480)

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,5 +1,5 @@
 cask 'opencore-configurator' do
-  version '1.15.0.0'
+  version '1.15.0.1'
   sha256 '48952b3558a5553ec61ae8d39c434293017213517cf77ae3b7b2dcf11124934e'
 
   url 'https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last'
