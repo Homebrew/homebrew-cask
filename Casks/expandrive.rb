00b9709b180a35efd246d945c9ef6ea32f053ed0
stevek123 <kissistvan12@gmail.com>
2020-02-21 15:03:50 +0100

    Fix sha256 hash for Expandrive 7.5.0 (#77488)
    The hash does not match the downloaded file's hash.

--- Casks/expandrive.rb
+++ Casks/expandrive.rb
@@ -1,6 +1,6 @@
 cask 'expandrive' do
   version '7.5.0'
-  sha256 '1ca986e98480acdccd433379b349c95dd6d5b0db89fdeb8e2cdb67367699793f'
+  sha256 '8c0ddc599010d8e01433a6593f3cc669ff1b8854ebb1f039adcc4cded2ee7e03'
 
   url "https://updates.expandrive.com/apps/expandrive#{version.major}/v/#{version.dots_to_hyphens}/update_download"
   appcast "https://updates.expandrive.com/appcast/expandrive#{version.major}.json?version=#{version.major}.0.0"
