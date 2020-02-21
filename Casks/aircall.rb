9a60939a15756ec68b0558c927db31745b6b3e7e
Vítor Galvão <info@vitorgalvao.com>
2020-02-21 23:21:45 +0000

    aircall.rb: 2.3.2

--- Casks/aircall.rb
+++ Casks/aircall.rb
@@ -1,10 +1,10 @@
 cask 'aircall' do
-  version '2.3.0'
-  sha256 'f85433a83c0aa28da5f1bd281f44367aa0dbc088d2bd2d4b633bd4f20a70fdc5'
+  version '2.3.2'
+  sha256 '1ef71dd64b671c1583eafee6fb7f415cc239cbd68dc8f41732c7c5b4ac9747f4'
 
   # aircall-electron-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://aircall-electron-releases.s3.amazonaws.com/production/Aircall-#{version}-mac.zip"
-  appcast 'https://electron.aircall.io/update/osx/1.1.0'
+  appcast 'https://electron.aircall.io/update/osx/0.0.0'
   name 'Aircall'
   homepage 'https://aircall.io/'
 
