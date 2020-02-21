b99670cb8c0712b012a596284d0ae4fefe817096
Vítor Galvão <info@vitorgalvao.com>
2020-02-21 23:25:21 +0000

    Update collabshot.rb

--- Casks/collabshot.rb
+++ Casks/collabshot.rb
@@ -3,6 +3,7 @@ cask 'collabshot' do
   sha256 :no_check
 
   url 'https://api.collabshot.com/desktop/latest/mac'
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://api.collabshot.com/desktop/latest/mac'
   name 'Collabshot'
   homepage 'https://www.collabshot.com/'
 
