891b25e5659a11cfd379abc0f1b06507be8f182d
Vítor Galvão <info@vitorgalvao.com>
2020-02-21 23:22:55 +0000

    Update simply-fortran.rb

--- Casks/simply-fortran.rb
+++ Casks/simply-fortran.rb
@@ -2,8 +2,7 @@ cask 'simply-fortran' do
   version '3.9.3177'
   sha256 'a065bcd451ae10556099bb1bd6847f366483448f3c2cd6521bf97c1a9418d0ed'
 
-  # simplyfortran.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
-  url "https://simplyfortran.nyc3.digitaloceanspaces.com/#{version.major_minor}/macos/simplyfortran-#{version}.dmg"
+  url "https://download.simplyfortran.com/#{version.major_minor}/macos/simplyfortran-#{version}.dmg"
   appcast 'https://simplyfortran.com/download/?platform=macos',
           configuration: version.major_minor
   name 'Simply Fortran'
