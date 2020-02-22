21a9a9ad43b92d69a15091f684a5a4abbc17666c
Fabio Spampinato <spampinabio@gmail.com>
2020-02-22 00:47:07 +0000

    Update Notable homepage

--- Casks/notable.rb
+++ Casks/notable.rb
@@ -2,10 +2,11 @@ cask 'notable' do
   version '1.8.4'
   sha256 '5ca00f5135e769165c683dc0174ea95bd9052ca751a0704b1a1a20dd297b4e3d'
 
+  # github.com/notable/notable was verified as official when first introduced to the cask
   url "https://github.com/notable/notable/releases/download/v#{version}/Notable-#{version}.dmg"
   appcast 'https://github.com/notable/notable/releases.atom'
   name 'Notable'
-  homepage 'https://github.com/notable/notable'
+  homepage 'https://notable.md/'
 
   auto_updates true
 
