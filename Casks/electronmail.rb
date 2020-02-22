3c82600ed05c925ef91aa63a2f2a0dc856255855
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-21 18:33:48 -0800

    Update electronmail from 4.2.2 to 4.3.0 (#77493)

--- Casks/electronmail.rb
+++ Casks/electronmail.rb
@@ -1,6 +1,6 @@
 cask 'electronmail' do
-  version '4.2.2'
-  sha256 '5f3d9d3deb327c8812e0283ba5489e24b1e4ce9c6a1c10a94aee111c0c5cfe04'
+  version '4.3.0'
+  sha256 '93ae03db3a4bee735e9247e1dbe77cbc197e0e51d26c6f66a92507ad33b77f0d'
 
   url "https://github.com/vladimiry/ElectronMail/releases/download/v#{version}/electron-mail-#{version}-mac-mojave.dmg"
   appcast 'https://github.com/vladimiry/ElectronMail/releases.atom'
