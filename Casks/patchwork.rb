ef51da032deb6f7bc8826be40db308cd1576d6be
waeltken <waeltken@users.noreply.github.com>
2020-02-22 11:18:47 +0100

    Update patchwork from 3.17.4 to 3.17.5 (#77527)

--- Casks/patchwork.rb
+++ Casks/patchwork.rb
@@ -1,6 +1,6 @@
 cask 'patchwork' do
-  version '3.17.4'
-  sha256 '8ef7ae979763bb5b357e8db30b01b90d21405e847959e5680c150d82a1f80e9e'
+  version '3.17.5'
+  sha256 '3ba1a780a0a218ed91d12dc7a0d5a26d452037b31180498b3d5302cf777da149'
 
   url "https://github.com/ssbc/patchwork/releases/download/v#{version}/Patchwork-#{version}.dmg"
   appcast 'https://github.com/ssbc/patchwork/releases.atom'
