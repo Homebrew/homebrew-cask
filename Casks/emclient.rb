60e8389b2ed524a3d6570fb33a80b91f6d5cc21f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-22 13:19:04 -0800

    Update emclient from 7.2.37943 to 7.2.37961 (#77556)

--- Casks/emclient.rb
+++ Casks/emclient.rb
@@ -1,6 +1,6 @@
 cask 'emclient' do
-  version '7.2.37943'
-  sha256 '3d19538e92a62931582d9cd5c9bca554fa08258ab91134bde1122e46903ea098'
+  version '7.2.37961'
+  sha256 '4b38b07cb6b172e92aeb9b91482b560bda1cb8dbaf728440201ce6e584fcea81'
 
   url "https://cdn-dist.emclient.com/dist/v#{version}_Mac/setup.pkg"
   appcast 'https://www.emclient.com/release-history?os=mac'
