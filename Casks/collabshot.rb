875a4c19e976fd83b4de2ec223407a1f3f19e492
Chris Lorig <cmw@users.noreply.github.com>
2020-02-24 00:15:32 +0100

    Replace sha1 with sha256 sum

--- Casks/collabshot.rb
+++ Casks/collabshot.rb
@@ -1,6 +1,6 @@
 cask 'collabshot' do
   version '1.2.3.985'
-  sha256 '30bffefc6baf6c34966d746bef57929027863451'
+  sha256 'a2d298dc2e6cf813c90c5dc64d9eaedf5ff4ef2e0ad45001d35ecefc511390c3'
 
   url "https://collabshot.s3.amazonaws.com/desktop/builds/mac/Collabshot_#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://api.collabshot.com/desktop/latest/mac'
