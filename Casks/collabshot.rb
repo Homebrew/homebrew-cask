c4aacb780c264bfaa8da77f06a1a43258a9ccbcd
Chris Lorig <cmw@users.noreply.github.com>
2020-02-23 16:08:09 +0100

    Add versioned url, version and shasum

--- Casks/collabshot.rb
+++ Casks/collabshot.rb
@@ -1,8 +1,8 @@
 cask 'collabshot' do
-  version :latest
-  sha256 :no_check
+  version '1.2.3.985'
+  sha256 '30bffefc6baf6c34966d746bef57929027863451'
 
-  url 'https://api.collabshot.com/desktop/latest/mac'
+  url 'https://collabshot.s3.amazonaws.com/desktop/builds/mac/Collabshot_#{version}.dmg'
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://api.collabshot.com/desktop/latest/mac'
   name 'Collabshot'
   homepage 'https://www.collabshot.com/'
