0b7ff29c9e7b695bc662f9906301b70ccce9f44f
Work <brigitte@corecode.io>
2020-02-23 17:14:26 +0100

    Update icab from 5.8.6 to 5.9.1

--- Casks/icab.rb
+++ Casks/icab.rb
@@ -1,6 +1,6 @@
 cask 'icab' do
-  version '5.8.6'
-  sha256 '8ef3f51a54a129b9edf193f8f5d251888981b41bd829d3d457ef6b7930c4a125'
+  version '5.9.1'
+  sha256 '4281500b5f38848c3e376f1acf7dca48e4d13cf61f446dcef289acfa97809e36'
 
   # icab.clauss-net.de was verified as official when first introduced to the cask
   url "https://icab.clauss-net.de/icab/iCab_#{version}_Intel.zip"
