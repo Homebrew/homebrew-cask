188b654c2954d66a3a484e9b0ed9ae515d6a725b
Chris Lorig <cmw@users.noreply.github.com>
2020-02-24 00:04:59 +0100

    Correct quotation marks

--- Casks/collabshot.rb
+++ Casks/collabshot.rb
@@ -2,7 +2,7 @@ cask 'collabshot' do
   version '1.2.3.985'
   sha256 '30bffefc6baf6c34966d746bef57929027863451'
 
-  url 'https://collabshot.s3.amazonaws.com/desktop/builds/mac/Collabshot_#{version}.dmg'
+  url "https://collabshot.s3.amazonaws.com/desktop/builds/mac/Collabshot_#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://api.collabshot.com/desktop/latest/mac'
   name 'Collabshot'
   homepage 'https://www.collabshot.com/'
