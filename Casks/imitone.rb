9171d6ea83dba8fd1aa5517237471eaa50d5d2da
Randall <ran.dall@icloud.com>
2020-02-23 14:00:59 -0800

    Update imitone from 0.10.0b to 0.10.0c

--- Casks/imitone.rb
+++ Casks/imitone.rb
@@ -1,6 +1,6 @@
 cask 'imitone' do
-  version '0.10.0b'
-  sha256 'bd04abd9ab61c23eb0a94d0c1da0158f265d33462b6d5b6c71cd6a3a5ec21265'
+  version '0.10.0c'
+  sha256 '18aedd3b06737cbb71e04260c5e79a0c541d6f8b2745c7679fe4777fca515cf0'
 
   url "https://imitone.com/beta/imitone-#{version}.dmg"
   appcast 'https://imitone.com/beta/'
