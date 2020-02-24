12eecd2918cd84a31e5db3977911ed7364546b51
Dan Sully <dan+github@sully.org>
2020-02-23 18:48:58 -0800

    Update appcast URL (#77596)

--- Casks/querious.rb
+++ Casks/querious.rb
@@ -3,7 +3,7 @@ cask 'querious' do
   sha256 '427e4c32cb2a8539356b38e427c6008617783cefdf0438925ce458f7ef2bb3f2'
 
   url "https://www.araelium.com/querious/downloads/versions/Querious#{version}.zip"
-  appcast 'https://arweb-assets.s3.amazonaws.com/downloads/querious/release-updates.xml'
+  appcast 'https://arweb-assets.s3.amazonaws.com/downloads/querious/updates.json'
   name "Querious #{version.major}"
   homepage 'https://www.araelium.com/querious/'
 
