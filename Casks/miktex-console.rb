10054ba18f2b94cf35dd77e15f6795181d9adeef
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-24 15:58:18 -0800

    Update miktex-console from 2.9.7250-1 to 2.9.7300-1 (#77631)

--- Casks/miktex-console.rb
+++ Casks/miktex-console.rb
@@ -1,6 +1,6 @@
 cask 'miktex-console' do
-  version '2.9.7250-1'
-  sha256 'bae786c68a2c9873392047e8330384db5ce13bc52751c7bcc152b5c2416a3ce5'
+  version '2.9.7300-1'
+  sha256 'da3b5b2ce6189ab7f66aca533c715f91ee0f2dadaede3fbb40e0a070ecf5d9d0'
 
   url "https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-#{version}-darwin-x86_64.dmg"
   appcast 'https://miktex.org/download'
