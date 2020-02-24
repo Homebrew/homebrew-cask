bb498253517bb3b81c537a1c5e25ae4131d01322
Vítor Galvão <info@vitorgalvao.com>
2020-02-24 15:57:15 +0000

    Update openvpn-connect.rb

--- Casks/openvpn-connect.rb
+++ Casks/openvpn-connect.rb
@@ -3,6 +3,7 @@ cask 'openvpn-connect' do
   sha256 'fdd2de126c1080e91f32757ce793fdd1bf7161d8005811031cba88b168e85ade'
 
   url "https://swupdate.openvpn.net/as/clients/openvpn-connect-#{version}_signed.dmg"
+  appcast 'https://openvpn.net/client-connect-vpn-for-mac-os/'
   name 'OpenVPN Connect client'
   homepage 'https://openvpn.net/client-connect-vpn-for-mac-os/'
 
