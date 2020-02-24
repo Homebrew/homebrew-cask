a0ca50ad71d6d2d53b4473a13296277f2f48e44d
Vítor Galvão <info@vitorgalvao.com>
2020-02-24 01:43:52 +0000

    Update kaleidoscope.rb

--- Casks/kaleidoscope.rb
+++ Casks/kaleidoscope.rb
@@ -1,9 +1,9 @@
 cask 'kaleidoscope' do
-  version '2.3,1438:feb-21-2020'
+  version '2.3,1438-feb-21-2020'
   sha256 '347d7fb5e36e0c50ffac56d99b3853b61cf2cf7ed82eb3f55768a2554f90d9a2'
 
   # appcasts.hypergiant.com/ks/prod was verified as official when first introduced to the cask
-  url "https://appcasts.hypergiant.com/ks/prod/Kaleidoscope-#{version.before_comma}-build-#{version.after_comma.before_colon}-#{version.after_colon}.zip"
+  url "https://appcasts.hypergiant.com/ks/prod/Kaleidoscope-#{version.before_comma}-build-#{version.after_comma}.zip"
   appcast 'https://updates.blackpixel.com/updates?app=ks'
   name 'Kaleidoscope'
   homepage 'https://www.kaleidoscopeapp.com/'
