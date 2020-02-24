949bb62a80b08f6afcf766a5ac5b8ff54f93b436
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-24 15:18:29 -0800

    Update boostnote from 0.14.0 to 0.15.0 (#77621)

--- Casks/boostnote.rb
+++ Casks/boostnote.rb
@@ -1,6 +1,6 @@
 cask 'boostnote' do
-  version '0.14.0'
-  sha256 '4939659a2d2195de5457755188d39e1f3428a02b4de0fe8f0ba3428f6c446c45'
+  version '0.15.0'
+  sha256 '4d64b05005eaf1672c450e470615bdaf757b7d9ba7ef787bed14d0d00c4a54a2'
 
   # github.com/BoostIO/boost-releases was verified as official when first introduced to the cask
   url "https://github.com/BoostIO/boost-releases/releases/download/v#{version}/Boostnote-mac.zip"
