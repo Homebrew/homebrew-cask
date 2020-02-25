1a4073ac0bbef6fe81f518b7125f0c10d6f43a3e
CoreCode <core-code@users.noreply.github.com>
2020-02-25 18:41:58 +0100

    Update popsql.rb

--- Casks/popsql.rb
+++ Casks/popsql.rb
@@ -1,6 +1,6 @@
 cask 'popsql' do
-  version '0.9.2'
-  sha256 '9e97c094d3217a4a5f4ab93751615da5793d0a1c545d871f36f77d14e5ac9920'
+  version '0.9.3'
+  sha256 'ecc6e0871bb6299452bdea53a509345d52ac746b91c5e4c9cfbe02bbbd6fc65d'
 
   # popsql-releases.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://popsql-releases.s3.amazonaws.com/mac/PopSQL-#{version}-mac.zip"
