0cd0295f0e0c1be540c3896e78c21bf403255f87
Work <brigitte@corecode.io>
2020-02-25 12:10:27 +0100

    Update integrity from 9.5.1 to 9.5.2

--- Casks/integrity.rb
+++ Casks/integrity.rb
@@ -1,5 +1,5 @@
 cask 'integrity' do
-  version '9.5.1'
+  version '9.5.2'
   sha256 'd89c00c8b4f8a36234d02c45ef9e29f10dead94fddd3983d5778b85af4ffd7a5'
 
   url 'https://peacockmedia.software/mac/integrity/integrity.dmg'
