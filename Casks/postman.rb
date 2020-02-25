9833120396d841fbb9948607bf4c0b1b7786df7e
Work <brigitte@corecode.io>
2020-02-25 11:47:36 +0100

    Update postman from 7.18.0 to 7.19.1

--- Casks/postman.rb
+++ Casks/postman.rb
@@ -1,6 +1,6 @@
 cask 'postman' do
-  version '7.18.0'
-  sha256 'ae6ee66cc6cda77ec2e37c5a8c463815240a8462bb1a0de9facb66e1dd1a76a1'
+  version '7.19.1'
+  sha256 '034c6715bb79b91a797fe669ef193b78d9f1ebafc8a41a59bc489f901445c3a8'
 
   # dl.pstmn.io/download/version was verified as official when first introduced to the cask
   url "https://dl.pstmn.io/download/version/#{version}/osx64"
