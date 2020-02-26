7859715a2eb7ccc0dd6b827bb023e36de1227cf8
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-26 13:06:50 -0800

    Update wavebox from 10.0.86.1 to 10.0.90.1 (#77776)

--- Casks/wavebox.rb
+++ Casks/wavebox.rb
@@ -1,6 +1,6 @@
 cask 'wavebox' do
-  version '10.0.86.1'
-  sha256 'e1f4d9074167ab7b4957f49ba80626ff6f3e12205069ae4d94f4eedee9f74759'
+  version '10.0.90.1'
+  sha256 'af8345d54edd753297388d24282f4bf383e04d9c97ff1bc951f8d76ae4b4b419'
 
   # download.wavebox.app was verified as official when first introduced to the cask
   url "https://download.wavebox.app/core/mac/Install%20Wavebox%20#{version}.dmg"
