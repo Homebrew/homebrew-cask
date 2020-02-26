2d942c5f7794f2d5cde5418ff55fdc31a722f678
suschizu <brigitte@corecode.io>
2020-02-26 18:44:13 +0100

    Update recents.rb

--- Casks/recents.rb
+++ Casks/recents.rb
@@ -4,7 +4,6 @@ cask 'recents' do
 
   # rink.hockeyapp.net/api/2/apps/74f5ee9ebf2d4be3b92a3e8766433b8b was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/74f5ee9ebf2d4be3b92a3e8766433b8b/app_versions/#{version.after_comma}?format=zip"
-  appcast 'https://rink.hockeyapp.net/api/2/apps/74f5ee9ebf2d4be3b92a3e8766433b8b'
   name 'Recents'
   homepage 'https://recentsapp.com/'
 
