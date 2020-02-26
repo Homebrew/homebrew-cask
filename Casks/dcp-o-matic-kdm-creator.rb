44463a98567e81802b6c2323c8af42205cc7ef27
Work <brigitte@corecode.io>
2020-02-26 14:18:48 +0100

    Update dcp-o-matic-kdm-creator from 2.14.26 to 2.14.30

--- Casks/dcp-o-matic-kdm-creator.rb
+++ Casks/dcp-o-matic-kdm-creator.rb
@@ -1,8 +1,8 @@
 cask 'dcp-o-matic-kdm-creator' do
-  version '2.14.26'
-  sha256 'a05ddaca96b27e6e6293243b596b3aec7649ca351c91dce971756d1e67b6453f'
+  version '2.14.30'
+  sha256 'b958c304955b5a12a8572330dbcf8e43a4fcb89687e417e103dea81ed670b92d'
 
-  url "https://dcpomatic.com/dl.php?id=osx-kdm&version=#{version}"
+  url "https://dcpomatic.com/dl.php?id=osx-10.9-kdm&version=#{version}"
   appcast 'https://dcpomatic.com/download'
   name 'DCP-o-matic KDM Creator'
   homepage 'https://dcpomatic.com/'
