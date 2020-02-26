463938aeeeb5dcde29c54bf73dc22f586bf4a86a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-26 13:07:02 -0800

    Update agenda from 9.1.3 to 9.2 (#77775)

--- Casks/agenda.rb
+++ Casks/agenda.rb
@@ -1,6 +1,6 @@
 cask 'agenda' do
-  version '9.1.3'
-  sha256 'a7ffaf98e9a244c450c0f70070ec5706847b7e517311cd871fb25060fa7fe5a5'
+  version '9.2'
+  sha256 '99fb05b0fc79575040134c86e811ba602d060ae22399a86563488bd890f32ac2'
 
   url "https://downloads.agenda.com/Agenda_#{version}.zip"
   appcast 'https://downloads.agenda.com/AgendaSparkleAppcast.xml'
