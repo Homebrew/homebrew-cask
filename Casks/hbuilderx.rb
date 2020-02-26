312c854feec7bdd5d8107f55a9cb33e2434108da
Randall <ran.dall@icloud.com>
2020-02-26 13:06:01 -0800

    Update hbuilderx from 2.6.0.20200223 to 2.6.1.20200226

--- Casks/hbuilderx.rb
+++ Casks/hbuilderx.rb
@@ -1,6 +1,6 @@
 cask 'hbuilderx' do
-  version '2.6.0.20200223'
-  sha256 'b6da09883b339395b277dc75dc3a0f2a549ac7e2a49258ce6f85292703b7913a'
+  version '2.6.1.20200226'
+  sha256 'a228ad446f14b0b3c7cf6124973e1da2f1c9e453ba2ab1a30ea99196102b0742'
 
   # download.dcloud.net.cn was verified as official when first introduced to the cask
   url "https://download.dcloud.net.cn/HBuilderX.#{version}.dmg"
