f15c2676c0f791225db97ecfa2898639b5296399
suschizu <brigitte@corecode.io>
2020-02-27 20:48:51 +0100

    Update navicat-premium from 15.0.10 to 15.0.11 (#77827)

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium' do
-  version '15.0.10'
-  sha256 '4c9946e10cb4163a396af38a2d6d39f23da8d2e989c23ff5cccd4897af32e9a8'
+  version '15.0.11'
+  sha256 'f94612841a8285290e14362d6805bad6f66b842ece3fb32b15cce4bcdf7416f8'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en'
