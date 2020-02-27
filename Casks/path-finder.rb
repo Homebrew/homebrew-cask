ec04d2cfcd81e372b715661737b9e79f25c64cc1
Andware <amuehlemann@andware.dk>
2020-02-27 09:29:52 +0100

    Update path-finder.rb
    How to update checksum. It is coming an error under installation

--- Casks/path-finder.rb
+++ Casks/path-finder.rb
@@ -1,5 +1,5 @@
 cask 'path-finder' do
-  version '9.0.7'
+  version '9.0.8'
   sha256 'c4ecd744dec73f50bf28e3f1d0d816a7f63e6ef4d73d952638010e838066f746'
 
   url "https://get.cocoatech.com/PF#{version.major}.dmg"
