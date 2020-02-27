c2b2ca2277bcfdf950b3c1a5fd5ed8e68929b8fe
Hannes Braun <hannesbraun@mail.de>
2020-02-27 12:08:05 +0100

    Update todometer from 2.0.0 to 2.0.1

--- Casks/todometer.rb
+++ Casks/todometer.rb
@@ -1,9 +1,9 @@
 cask 'todometer' do
-  version '2.0.0'
-  sha256 'e10a9c481ca31235bda964dd25907a389a1085439e3a05ee3c43490874d9cf37'
+  version '2.0.1'
+  sha256 'b8de5c09dc1d5d026130555f3cc85c949ddd7bd0cbf63a45bd1d4c2aba6cbf31'
 
   # github.com/cassidoo/todometer was verified as official when first introduced to the cask
-  url "https://github.com/cassidoo/todometer/releases/download/v#{version}/todometer.for.Mac.zip"
+  url "https://github.com/cassidoo/todometer/releases/download/v#{version}/todometer.for.mac.zip"
   appcast 'https://github.com/cassidoo/todometer/releases.atom'
   name 'todometer'
   homepage 'https://cassidoo.github.io/todometer/'
