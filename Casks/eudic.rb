80813178f8334317ce229349f350673de18082f1
CoreCode <core-code@users.noreply.github.com>
2020-02-27 12:22:33 +0100

    Update eudic.rb

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,9 +1,9 @@
 cask 'eudic' do
-  version '3.9.6,2020-01-27'
+  version '2020-01-27,3.9.6'
   sha256 '5cec29af3ff50a6658c81c33466166aa0de18097ec649ec82557517cf2f73d2e'
 
   # static.frdic.com was verified as official when first introduced to the cask
-  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.after_comma}"
+  url "https://static.frdic.com/pkg/eudicmac.dmg?v=#{version.before_comma}"
   appcast 'https://www.eudic.net/v4/en/app/download'
   name 'Eudic'
   name '欧路词典'
