8c13ce1c662d399eafccec26e0fb0268f1d23a85
Vítor Galvão <info@vitorgalvao.com>
2020-02-28 15:25:41 +0000

    Update jazzup.rb

--- Casks/jazzup.rb
+++ Casks/jazzup.rb
@@ -3,6 +3,7 @@ cask 'jazzup' do
   sha256 :no_check
 
   url 'https://www.irradiatedsoftware.com/download/JazzUp.zip'
+  appcast 'https://www.irradiatedsoftware.com/updates/profiles/jazzup.php'
   name 'JazzUp'
   homepage 'https://www.irradiatedsoftware.com/labs/'
 
