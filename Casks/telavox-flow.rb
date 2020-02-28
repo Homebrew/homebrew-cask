2633a64e809f892f1665b5d2fc822a8c46f5117c
Bo <bo@saurage.se>
2020-02-28 19:46:58 +0100

    Add Telavox Flow v1.93.0

--- Casks/telavox-flow.rb
+++ Casks/telavox-flow.rb
@@ -2,7 +2,7 @@ cask 'telavox-flow' do
   version '1.93.0'
   sha256 'f268679417ddba3c901f2a607381cf93ff3ca4ca9a09e6602c9357f9c8907f8a'
 
-  # https://s3.eu-west-2.amazonaws.com was verified as official when first introduced to the cask
+  # amazonaws.com/flow-desktop/Flow-1.93.0.dmg was verified as official when first introduced to the cask
   url 'https://s3.eu-west-2.amazonaws.com/flow-desktop/Flow-1.93.0.dmg'
   name 'Telavox-Flow'
   homepage 'https://telavox.com/en/apps/'
