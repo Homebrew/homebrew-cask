ed392f5ff312ecb16c783a8abfbc70e9e2aa3e7d
Jordan <whousesversioncontrol@gmail.com>
2020-02-27 19:57:47 -0800

    Update origin from 10.5.63.37653 to 10.5.64.37936

--- Casks/origin.rb
+++ Casks/origin.rb
@@ -1,6 +1,6 @@
 cask 'origin' do
-  version '10.5.63.37653'
-  sha256 '3e9b8f404112b37ab73e78854811e6dcc7ab087517cfc4647cfca3b3a710f6de'
+  version '10.5.64.37936'
+  sha256 '0e7e179ec1fec2cdf7ba92274ddd8e12d964793fde6882ca23f5201b9edb5307'
 
   # origin-a.akamaihd.net was verified as official when first introduced to the cask
   url "https://origin-a.akamaihd.net/Origin-Client-Download/origin/mac/live/OriginUpdate_#{version.dots_to_underscores}.zip"
