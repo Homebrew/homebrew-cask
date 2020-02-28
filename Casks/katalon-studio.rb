db6ffe5c4fb1ef1677a725f51f9a1cc05f7e3786
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-27 22:48:44 -0800

    Update katalon-studio from 7.2.4 to 7.2.5 (#77841)

--- Casks/katalon-studio.rb
+++ Casks/katalon-studio.rb
@@ -1,6 +1,6 @@
 cask 'katalon-studio' do
-  version '7.2.4'
-  sha256 '37a970aff73f06199552bfcbbc2580f44ecf2478c5420600d3ca2988ef75391e'
+  version '7.2.5'
+  sha256 '04d36df59a2e88b96baccf98193af0327376369673704adfae7bd42880a11c47'
 
   url "https://download.katalon.com/#{version}/Katalon%20Studio.dmg"
   appcast 'https://github.com/katalon-studio/katalon-studio/releases.atom'
