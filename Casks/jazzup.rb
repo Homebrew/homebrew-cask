2b33ebf7d23d838562bdfa40c67c8c7125c9648c
Sungbin Jo <pcr910303@icloud.com>
2020-02-29 02:49:11 +0900

    Update version, sha256 and url stanza of Jazzup.app

--- Casks/jazzup.rb
+++ Casks/jazzup.rb
@@ -1,8 +1,8 @@
 cask 'jazzup' do
-  version :latest
-  sha256 :no_check
+  version '1.0b3'
+  sha256 '23139ee856d79b6e78ad3badee82f20b1972f29ffb930a4dd7884d401311a3f8'
 
-  url 'https://www.irradiatedsoftware.com/download/JazzUp.zip'
+  url "https://www.irradiatedsoftware.com/downloads/JazzUp_#{version}.zip"
   appcast 'https://www.irradiatedsoftware.com/updates/profiles/jazzup.php'
   name 'JazzUp'
   homepage 'https://www.irradiatedsoftware.com/labs/'
