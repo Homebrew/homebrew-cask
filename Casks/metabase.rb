a58c558217993faa215495a0e698c774ca763b2a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-27 23:20:41 -0800

    Update metabase from 0.34.2 to 0.34.3 (#77850)

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -1,6 +1,6 @@
 cask 'metabase' do
-  version '0.34.2'
-  sha256 '387b7258741b2360fe69559b2d758fb7a2e429afd95b37876b3d5ec32406ea09'
+  version '0.34.3'
+  sha256 'd191cf4dc21fbe7e34a74dde9b6781b677d9d7a03ea40eb32a85d6c1317deb3b'
 
   # s3.amazonaws.com/downloads.metabase.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip"
