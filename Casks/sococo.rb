5072f605f286e8a4cfb04f2a462c99005d2c8047
suschizu <brigitte@corecode.io>
2020-02-29 22:12:45 +0100

    Update sococo.rb (#77755)

--- Casks/sococo.rb
+++ Casks/sococo.rb
@@ -1,10 +1,8 @@
 cask 'sococo' do
-  version '6.5.8,12-09-2019'
-  sha256 'e235a6a98c8a83eaa3e379c01eee51370f643846f6a8a594ad91f7a182c7eeec'
+  version :latest
+  sha256 :no_check
 
-  url "https://s.sococo.com/rs/client/mac/#{version.after_comma}-SococoProd.dmg"
-  appcast 'https://app.sococo.com/app/app.min.js',
-          configuration: version.after_comma
+  url 'https://s.sococo.com/rs/client/mac/SococoInstaller.dmg'
   name 'Sococo'
   homepage 'https://www.sococo.com/'
 
