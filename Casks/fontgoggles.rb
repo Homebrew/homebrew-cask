9570b77374f8910f982637df3a31e85320bd2f7f
Vítor Galvão <info@vitorgalvao.com>
2020-02-29 17:18:46 +0000

    Update fontgoggles.rb

--- Casks/fontgoggles.rb
+++ Casks/fontgoggles.rb
@@ -2,7 +2,7 @@ cask 'fontgoggles' do
   version '1.0'
   sha256 '6c7632e6276e4195a8f2b0d99dbac61a92fb3dfa99596adff89d4b0bd74ed4cf'
 
-  # github.com/justvanrossum was verified as official when first introduced to the cask
+  # github.com/justvanrossum/fontgoggles was verified as official when first introduced to the cask
   url "https://github.com/justvanrossum/fontgoggles/releases/download/v#{version}/FontGoggles.dmg"
   appcast 'https://github.com/justvanrossum/fontgoggles/releases.atom'
   name 'FontGoggles'
