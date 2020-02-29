07fbc6f49201a617436d52b7c44109dfa736fa61
Dmitriy Motuzinskiy <dmitriy@motuzinskiy.org.ua>
2020-02-29 19:31:52 +0200

    Update Casks/eset-cyber-security-pro.rb
    Co-Authored-By: Vítor Galvão <info@vitorgalvao.com>

--- Casks/eset-cyber-security-pro.rb
+++ Casks/eset-cyber-security-pro.rb
@@ -1,5 +1,5 @@
 cask 'eset-cyber-security-pro' do
-  version :latest
+  version '6.8.300.0'
   sha256 :no_check
 
   url 'https://download.eset.com/com/eset/apps/home/ess/mac/latest/eset_cybersecurity_pro_en.dmg'
