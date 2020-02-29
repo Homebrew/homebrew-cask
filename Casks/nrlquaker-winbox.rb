1a56e7f903fc71233b18bdb518f1ec7272eaee92
nrlquaker <ustynov.vy@gmail.com>
2020-02-29 22:33:27 +0200

    Update nrlquaker-winbox to 3.21.1

--- Casks/nrlquaker-winbox.rb
+++ Casks/nrlquaker-winbox.rb
@@ -1,6 +1,6 @@
 cask 'nrlquaker-winbox' do
-  version '3.21.0'
-  sha256 '0fe8415b7c774a644f2579e86a7c292071fa963cff8710665c18e5b83921ebc4'
+  version '3.21.1'
+  sha256 'd6d2f6bbd42bd2b0e481b3874db94f45db5a99753bfa50465cc82b55f5036863'
 
   url "https://github.com/nrlquaker/winbox-mac/releases/download/v#{version}/Winbox-mac-#{version}.zip"
   appcast 'https://github.com/nrlquaker/winbox-mac/releases.atom'
