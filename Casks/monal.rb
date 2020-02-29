b08014167774bba314f3c32bfb73c4f318546518
suschizu <brigitte@corecode.io>
2020-02-29 22:12:51 +0100

    Update monal from 2.5 beta 5,159 to version :latest (#77743)
    * Update monal from 2.5 beta 5,159 to 4.4,570
    
    * Update monal.rb
    
    This download leads to the beta version download from the website. the stable version is available just inside the App Store - latest version download is 4.4 but I can't find another appcast stanza.

--- Casks/monal.rb
+++ Casks/monal.rb
@@ -1,10 +1,8 @@
 cask 'monal' do
-  version '2.5 beta 5,159'
-  sha256 'e3ffeb9dd8d86359f9eafb877a9cb46938e546c2386af35ba24ebd4fdd375f57'
+  version :latest
+  sha256 :no_check
 
   url 'https://monal.im/Monal-OSX/Monal-OSX.zip'
-  appcast 'https://monal.im/Monal-OSX/appcast.xml',
-          configuration: version.after_comma
   name 'Monal'
   homepage 'https://monal.im/'
 
