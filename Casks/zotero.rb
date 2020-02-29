8b6ec4a773e423d577b2b44c6fc4973fb276a0b5
Wangxiz <wangxizaouu@sina.cn>
2020-02-29 12:54:15 +0800

    Update zotero from 5.0.83 to 5.0.84

--- Casks/zotero.rb
+++ Casks/zotero.rb
@@ -1,6 +1,6 @@
 cask 'zotero' do
-  version '5.0.83'
-  sha256 'adf3e55dbb3a33c8702e0bcee9202f0527f622e749da648e41ba6fc02217a04f'
+  version '5.0.84'
+  sha256 'e8aacde725b36e2d831620fc3d647fbe18abcb3a64c9bd4084e6144df95acf59'
 
   url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
   appcast 'https://github.com/zotero/zotero/releases.atom'
