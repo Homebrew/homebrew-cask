f9cd2e8ab746f6e87f0ef50b4e360890d6a73fdb
Vítor Galvão <info@vitorgalvao.com>
2020-02-29 20:50:08 +0000

    Update retro-virtual-machine.rb

--- Casks/retro-virtual-machine.rb
+++ Casks/retro-virtual-machine.rb
@@ -2,7 +2,7 @@ cask 'retro-virtual-machine' do
   version '2.0,1:7'
   sha256 '75e94f2df589ead3fb1eab529713312a17dc16e6b2ba547594cd9d5975def566'
 
-  # retrovirtualmachine.ams3.digitaloceanspaces.com/release was verified as official when first introduced to the cask
+  # retrovirtualmachine.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://retrovirtualmachine.ams3.digitaloceanspaces.com/release/beta#{version.before_colon.after_comma}/macos/RetroVirtualMachine.#{version.before_comma}.beta-#{version.before_colon.after_comma}.r#{version.after_colon}.macos.dmg"
   appcast 'https://www.retrovirtualmachine.org/en/downloads'
   name 'Retro Virtual Machine'
