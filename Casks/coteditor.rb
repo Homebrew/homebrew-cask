95a1e417b667c008bd41164051ef6b4a8af8360e
Randall <ran.dall@icloud.com>
2020-02-28 17:11:56 -0800

    Update coteditor from 3.8.10 to 3.8.11

--- Casks/coteditor.rb
+++ Casks/coteditor.rb
@@ -6,8 +6,8 @@ cask 'coteditor' do
     version '3.2.8'
     sha256 '73dd20d27b75c7b0c46242a465adb3df5b5f0b901f42c5a9a85777a57c4a17d6'
   else
-    version '3.8.10'
-    sha256 'c3461ec98895f06e647ddd20baf32400a589585d08f305a448aa7bfa60515b2f'
+    version '3.8.11'
+    sha256 'b5a04b98e410f98a72153ec60284ae1bcf39a6bef1222f120fd4f613d1aef2ed'
   end
 
   # github.com/coteditor/CotEditor was verified as official when first introduced to the cask
