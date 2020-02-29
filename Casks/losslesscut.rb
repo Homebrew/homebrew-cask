51ac60b7b575f3b9f855643bcf97f62304b8008f
Randall <ran.dall@icloud.com>
2020-02-29 15:57:38 -0800

    Update losslesscut from 3.12.0 to 3.13.0

--- Casks/losslesscut.rb
+++ Casks/losslesscut.rb
@@ -1,6 +1,6 @@
 cask 'losslesscut' do
-  version '3.12.0'
-  sha256 '95851aea6c37a0f9d6534883966221415bcf9fddd077d2e6de62ce8d9e418c34'
+  version '3.13.0'
+  sha256 'c81a304433df92b1f1a7bc753bca7b98fac9e8091f7af505011bb2cf54301b45'
 
   url "https://github.com/mifi/lossless-cut/releases/download/v#{version}/LosslessCut-mac.dmg"
   appcast 'https://github.com/mifi/lossless-cut/releases.atom'
