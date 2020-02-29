7141ce3b69c0b62f49563c5ab31ec5c7a6cc3871
Randall <ran.dall@icloud.com>
2020-02-28 17:14:35 -0800

    Update journey from 2.14.0 to 2.14.3

--- Casks/journey.rb
+++ Casks/journey.rb
@@ -1,6 +1,6 @@
 cask 'journey' do
-  version '2.14.0'
-  sha256 '1e90ee31f7f8e0cba2693def2448f4dd4838cd8dfc1063d672f242c699387d34'
+  version '2.14.3'
+  sha256 'f1945cd6c19e56b3c1c78943ef5ec18116907a4ca1efc40a57d48ab1db7adfc5'
 
   # github.com/2-App-Studio/journey-releases was verified as official when first introduced to the cask
   url "https://github.com/2-App-Studio/journey-releases/releases/download/v#{version}/Journey-darwin-#{version}.dmg"
