49aa71a1aa99d9ee815fd131d27a55b859c02268
Randall <ran.dall@icloud.com>
2020-02-29 16:22:07 -0800

    Update multimc from 0.6.8 to 0.6.9

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -1,5 +1,5 @@
 cask 'multimc' do
-  version '0.6.8'
+  version '0.6.9'
   sha256 'a015d9c9b93d15fa1ff9c9363a64bcbd3681e7bcce4de04d813baf2c484bfb7c'
 
   url 'https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz'
