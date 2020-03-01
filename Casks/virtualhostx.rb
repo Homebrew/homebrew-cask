70b9a7ce814db017c3dbfae4ee77a5a7ed4e833b
Randall <17261190+ran-dall@users.noreply.github.com>
2020-02-29 16:35:34 -0800

    Update virtualhostx from 2020.02,1007 to 2020.03,1008 (#77929)

--- Casks/virtualhostx.rb
+++ Casks/virtualhostx.rb
@@ -1,6 +1,6 @@
 cask 'virtualhostx' do
-  version '2020.02,1007'
-  sha256 '8ab43b2de01bec33793726af99849a7ec74d5dd3a79e84b13183ce53e578df57'
+  version '2020.03,1008'
+  sha256 '3082b331c232acc3b8a92fcee4ef2f940c9145a2fe49ff86c5e6efa97271cfd9'
 
   url "https://download.clickontyler.com/virtualhostx/virtualhostxpro_#{version.after_comma}.zip"
   appcast 'https://shine.clickontyler.com/appcast.php?id=45'
