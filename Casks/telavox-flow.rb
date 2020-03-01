dce6d41db35112295f4a2f29bfe8aace23095ed1
Bo <bo@saurage.se>
2020-03-02 00:40:54 +0100

    Fixed errors in submission
    Fixed URL
    Added Appcast

--- Casks/telavox-flow.rb
+++ Casks/telavox-flow.rb
@@ -2,10 +2,17 @@ cask 'telavox-flow' do
   version '1.93.0'
   sha256 'f268679417ddba3c901f2a607381cf93ff3ca4ca9a09e6602c9357f9c8907f8a'
 
-  # amazonaws.com/flow-desktop/Flow-1.93.0.dmg was verified as official when first introduced to the cask
+  # s3.eu-west-2.amazonaws.com/flow-desktop was verified as official when first introduced to the cask
   url 'https://s3.eu-west-2.amazonaws.com/flow-desktop/Flow-1.93.0.dmg'
-  name 'Telavox-Flow'
+  appcast: ttps://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://deopappmanager.telavox.com/flow/download/mac/latest  
+  name 'flow'
   homepage 'https://telavox.com/en/apps/'
-
+  auto_updates true
   app 'Flow.app'
+
+  zap trash: [
+               '~/Library/Application Support/Flow',
+               '~/Library/Logs/Flow',
+               '~/Library/Saved Application State/telavox.flow.desktop.savedState',
+             ]
 end
