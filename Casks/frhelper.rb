7ac27e39ffbdf1fdcbbcd0b2842feaa853fad260
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-03-01 17:31:18 +0100

    Update frhelper from 3.9.6,2020-01-26 to 3.9.6,2020-02-01 (#77945)

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -1,5 +1,5 @@
 cask 'frhelper' do
-  version '3.9.6,2020-01-26'
+  version '3.9.6,2020-02-01'
   sha256 'ec638576b929dfd9c2637b5dd152c59fc05812bdfa670c2419d056e329c6e44b'
 
   # static.frdic.com was verified as official when first introduced to the cask
