df2ac05e03e78cbd92f4135452183152080fe59b
suschizu <brigitte@corecode.io>
2020-03-02 12:58:53 +0100

    Revert "Update softu2f from 0.0.19 to 0.0.20 (#77661)"
    This reverts commit 10513471b7edf87f0aed82fa05148148c085a21f.

--- Casks/softu2f.rb
+++ Casks/softu2f.rb
@@ -1,6 +1,6 @@
 cask 'softu2f' do
-  version '0.0.20'
-  sha256 '6bd4ce9e88102660cef01bd7bc49e4a07c08e29d6367ce209a7eccc5bb8fb5c9'
+  version '0.0.19'
+  sha256 'a70e41c69acab59fdac6c03aab70675177f94db31069a5b15166f067e23db97f'
 
   url "https://github.com/github/SoftU2F/releases/download/#{version}/SoftU2F.pkg"
   appcast 'https://github.com/github/SoftU2F/releases.atom'
@@ -13,6 +13,5 @@ cask 'softu2f' do
 
   uninstall launchctl: 'com.github.SoftU2F',
             kext:      'com.github.SoftU2FDriver',
-            pkgutil:   'com.GitHub.SoftU2F',
-            delete:    '/Applications/SoftU2F.app'
+            pkgutil:   'com.GitHub.SoftU2F'
 end
