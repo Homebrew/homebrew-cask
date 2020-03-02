20a9869da878f39a8b8b8a4c4534b286bb7f9735
Randall <ran.dall@icloud.com>
2020-03-02 12:23:21 -0800

    Update catlight from 2.29.2 to 2.30.0

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -1,6 +1,6 @@
 cask 'catlight' do
-  version '2.29.2'
-  sha256 'e1447f0cd18f6df61c099c981f13010969577bb1522a611a2ae86317a034dde0'
+  version '2.30.0'
+  sha256 '7a514b392b2d1e4c12717c5a981eb5e8a10ae53acb29b74a4d5a43f3318b4065'
 
   # de2nac35bcll0.cloudfront.net was verified as official when first introduced to the cask
   url "https://de2nac35bcll0.cloudfront.net/dl/mac/beta/CatLightSetup-#{version}.zip"
