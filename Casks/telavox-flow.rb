832afe08544fc7096641b16fe0ddacbbacaaa17b
Vítor Galvão <info@vitorgalvao.com>
2020-03-02 00:40:26 +0000

    Update telavox-flow.rb

--- Casks/telavox-flow.rb
+++ Casks/telavox-flow.rb
@@ -5,7 +5,7 @@ cask 'telavox-flow' do
   # s3.eu-west-2.amazonaws.com/flow-desktop was verified as official when first introduced to the cask
   url 'https://s3.eu-west-2.amazonaws.com/flow-desktop/Flow-1.93.0.dmg'
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://deopappmanager.telavox.com/flow/download/mac/latest'
-  name 'flow'
+  name 'Telavox Flow'
   homepage 'https://telavox.com/en/apps/'
 
   auto_updates true
