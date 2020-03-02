e09bfc8afd6738cfbfb99ad1354c3b2e7029c84b
suschizu <brigitte@corecode.io>
2020-03-03 00:00:55 +0100

    Update orange (#77954)

--- Casks/orange.rb
+++ Casks/orange.rb
@@ -3,7 +3,7 @@ cask 'orange' do
   sha256 '4f31193260c4f2d9f0a495d664e8fbc7438aebfadc7e83f1d950c1c672873887'
 
   url "https://download.biolab.si/download/files/Orange#{version.major}-#{version}.dmg"
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://service.biolab.si/download/orange?platform=mac'
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://service.biolab.si/download/orange?platform=mac'
   name 'Orange'
   homepage 'https://orange.biolab.si/'
 
