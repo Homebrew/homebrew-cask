b25270898545cf5e664a2f1da542f17a9492524d
Bo <bo@saurage.se>
2020-03-02 01:01:56 +0100

    Fixed 2nd typo

--- Casks/telavox-flow.rb
+++ Casks/telavox-flow.rb
@@ -4,10 +4,12 @@ cask 'telavox-flow' do
 
   # s3.eu-west-2.amazonaws.com/flow-desktop was verified as official when first introduced to the cask
   url 'https://s3.eu-west-2.amazonaws.com/flow-desktop/Flow-1.93.0.dmg'
-  appcast: 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://deopappmanager.telavox.com/flow/download/mac/latest'  
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://deopappmanager.telavox.com/flow/download/mac/latest'
   name 'flow'
   homepage 'https://telavox.com/en/apps/'
+
   auto_updates true
+
   app 'Flow.app'
 
   zap trash: [
