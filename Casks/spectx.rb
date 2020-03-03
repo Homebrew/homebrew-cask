4b17a5722363d8ad0c3c6c7d5d7f3c6ce3863cc1
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-03 13:36:08 -0800

    Update spectx from 1.4.51 to 1.4.52 (#78060)

--- Casks/spectx.rb
+++ Casks/spectx.rb
@@ -1,6 +1,6 @@
 cask 'spectx' do
-  version '1.4.51'
-  sha256 '0872cd241f209b80e93166ac0f547ce5df73fb233e35a7430ab4f8a990b0033d'
+  version '1.4.52'
+  sha256 '91958889c5eeb0699789d81a698427a84b313f529aa7112f8c806321e7b5684f'
 
   url "https://get.spectx.com/03f21b939e022/SpectXDesktop-v#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.spectx.com/get/?desktop-osx64'
