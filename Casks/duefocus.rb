66e8d90bd54cc105936f9f0dd1a59bdbab46f58e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-03 13:36:27 -0800

    Update duefocus from 2.2.0 to 2.3.0 (#78059)

--- Casks/duefocus.rb
+++ Casks/duefocus.rb
@@ -1,6 +1,6 @@
 cask 'duefocus' do
-  version '2.2.0'
-  sha256 'f5ddeaa4c8afd0c4cc75c4f51ea778a6ba629a92cdcb99c864e898e8575d503b'
+  version '2.3.0'
+  sha256 'a30330d89cace86f228c18c04b7087c655a7e99ddde0ee78a19390af8ff54090'
 
   url "https://web.duefocus.com/distribution/darwin/v3/DueFocus-#{version}-mac.zip"
   appcast 'https://web.duefocus.com/distribution/darwin/v3/appcast.html'
