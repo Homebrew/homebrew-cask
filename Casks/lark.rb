63686fcbb6aed13ab90e6a92d8d20276d12daa96
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-03 13:35:31 -0800

    Update lark from 3.18.3 to 3.18.4 (#78061)

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -1,6 +1,6 @@
 cask 'lark' do
-  version '3.18.3'
-  sha256 '6ca13a0041796227d4ae06a4370ea1dca268992c42187b34191ff0a54f60839d'
+  version '3.18.4'
+  sha256 '28d5d19eabc3a85702dedb157dfa03ae0649aa913091f4272278b644d5390e93'
 
   # sf3-ttcdn-tos.pstatp.com was verified as official when first introduced to the cask
   url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/Lark-#{version}.dmg"
