5c5088a19056b90e74ef60c31e86f544e9472ca8
Yurii Kolesnykov <root@yurikoles.com>
2020-03-03 08:33:22 +0200

    electron 8.0.3 (#78020)
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '8.0.2'
-  sha256 'a0ba4ecb236dcc411152b406d6e81d1f209cb34c93e4037a3ee1fd08b713a4df'
+  version '8.0.3'
+  sha256 'e4e64b591de496d16107b524774c85f7bb81efc5c034e1e1abdf3a5fe63f62eb'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
