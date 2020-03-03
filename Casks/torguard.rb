24426bb4156bcf202846c8218bdc0b481061d0bc
Randall <ran.dall@icloud.com>
2020-03-03 13:15:11 -0800

    Update torguard from 3.98.0 to 3.98.2

--- Casks/torguard.rb
+++ Casks/torguard.rb
@@ -1,6 +1,6 @@
 cask 'torguard' do
-  version '3.98.0'
-  sha256 '78d9df847e73a148f21365ca2d1acde3b032cc0a084b06c49e96ec5f113c4661'
+  version '3.98.2'
+  sha256 '86a718f93539410cad509aaf6e3cb8efc88c8ca3d3da2d879aeca396eb7b0553'
 
   # torguard.biz was verified as official when first introduced to the cask
   url "https://updates.torguard.biz/Software/MacOSX/TorGuard-v#{version}.dmg"
