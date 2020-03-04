d66a47d0f5a6a40096ad189a455576855712916c
Vítor Galvão <info@vitorgalvao.com>
2020-03-04 18:00:57 +0000

    Add ElectroCRUD v2.6.0 (#78073)
    * Add ElectroCRUD v2.6.0
    
    * Add ElectroCRUD v2.6.0
    
    * Update electrocrud.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/electrocrud.rb
@@ -0,0 +1,11 @@
+cask 'electrocrud' do
+  version '2.6.0'
+  sha256 '8915ff804c9d9f392d3d0ca0201246a7b3a9a53a5eefbab3602572074dcee12a'
+
+  url "https://github.com/garrylachman/ElectroCRUD/releases/download/#{version}/ElectroCRUD-#{version}.dmg"
+  appcast 'https://github.com/garrylachman/ElectroCRUD/releases.atom'
+  name 'ElectroCRUD'
+  homepage 'https://github.com/garrylachman/ElectroCRUD'
+
+  app 'ElectroCRUD.app'
+end
