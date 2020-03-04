a17aa9649053352771e75d3e3b037a6aaadb8397
Jip J. Dekker <jip@dekker.one>
2020-03-04 11:48:43 +1100

    Update minizincide from 2.4.2 to 2.4.3 (#78072)

--- Casks/minizincide.rb
+++ Casks/minizincide.rb
@@ -1,6 +1,6 @@
 cask 'minizincide' do
-  version '2.4.2'
-  sha256 '0775ddd221fc8ed5e0b9a1393fb86dd6dc95eab23f3d55b3b071476ee00b275b'
+  version '2.4.3'
+  sha256 'ac62403d386fd3b2471f347ce7d78fb2a5eb80fd08964c46881fc91a2603524e'
 
   # github.com/MiniZinc/MiniZincIDE was verified as official when first introduced to the cask
   url "https://github.com/MiniZinc/MiniZincIDE/releases/download/#{version}/MiniZincIDE-#{version}-bundled.dmg"
