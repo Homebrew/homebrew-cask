1062627707aefcccdae1a62a92baaf70f221b273
Garry Lachman <garry@lachman.co>
2020-03-04 02:58:19 +0200

    Add ElectroCRUD v2.6.0

--- Casks/electrocrud.rb
+++ Casks/electrocrud.rb
@@ -3,10 +3,10 @@ cask 'electrocrud' do
   sha256 '8915ff804c9d9f392d3d0ca0201246a7b3a9a53a5eefbab3602572074dcee12a'
 
   url "https://github.com/garrylachman/ElectroCRUD/releases/download/#{version}/ElectroCRUD-#{version}.dmg"
-  name 'ElectroCRUD'
-  homepage 'http://garrylachman.github.io/ElectroCRUD/'
-  desc 'Database CRUD Application Built on Electron'
   appcast 'https://github.com/garrylachman/ElectroCRUD/releases.atom'
+  name 'ElectroCRUD'
+  desc 'Database CRUD Application Built on Electron'
+  homepage 'https://github.com/garrylachman/ElectroCRUD'
 
   app 'ElectroCRUD.app'
 end
