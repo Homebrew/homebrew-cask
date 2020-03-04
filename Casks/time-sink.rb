ca77d89a81d741b969a9f3f1d9e0cf66179fd2d2
suschizu <brigitte@corecode.io>
2020-03-04 14:58:15 +0100

    Update time-sink.rb

--- Casks/time-sink.rb
+++ Casks/time-sink.rb
@@ -3,7 +3,8 @@ cask 'time-sink' do
   sha256 '6a2cec3828d7d3bf244825419dc82e3d5e0a6b640736c401c3b56305ddfef0ba'
 
   url "https://manytricks.com/download/_do_not_hotlink_/timesink#{version.no_dots}.dmg"
-  appcast 'https://manytricks.com/timesink/appcast'
+  appcast 'https://manytricks.com/timesink/appcast',
+          configuration: version.chomp('.0')
   name 'Time Sink'
   homepage 'https://manytricks.com/timesink/'
 
