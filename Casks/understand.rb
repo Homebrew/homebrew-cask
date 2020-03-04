76458cdb327c1b5b51351e495e6bfd864e5fb6cd
Qi <25192197+singularitti@users.noreply.github.com>
2020-03-04 12:34:16 -0500

    Update understand from 5.1.1014 to 5.1.1017 (#78080)
    * Update understand from 5.1.1014 to 5.1.1017
    
    * Update understand.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask 'understand' do
-  version '5.1.1014'
-  sha256 '1270371637aa3fdbec69482c382cee5435378eacf904e682fb0a56c4b89a9abe'
+  version '5.1.1018'
+  sha256 'c90e867a116a800eac3e04a1316cc9e9006a6bc964a87fb6750ba52b37bf96a2'
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
   appcast 'https://scitools.com/download/all-builds/'
