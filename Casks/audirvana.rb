4d01941d4068c0ca39fcb5b840ab6f2fcdca89bf
HASHIMOTO Masayuki <gh@mahori.mail-box.ne.jp>
2020-03-04 19:33:13 +0900

    Update audirvana from 3.5.31 to 3.5.32 (#78081)

--- Casks/audirvana.rb
+++ Casks/audirvana.rb
@@ -1,6 +1,6 @@
 cask 'audirvana' do
-  version '3.5.31'
-  sha256 '0675a031aeb2f22834e3b720c009c04378aa36558a013219d03b294ea6b596ef'
+  version '3.5.32'
+  sha256 '43b581c0e75407b5fb41319fc74080af4c7bb6b2e29a24a7f501e714c9846e59'
 
   url "https://audirvana.com/delivery/Audirvana_#{version}.dmg"
   appcast "https://audirvana.com/delivery/audirvana#{version.major}_#{version.minor}_appcast.xml"
