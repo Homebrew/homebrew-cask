937dc4a85c42493af42e40d5b7d6609324a7dc85
L0 <LorenzoPane@users.noreply.github.com>
2020-03-04 12:04:38 -0500

    Update checkra1n to 0.9.8.1 (#78096)

--- Casks/checkra1n.rb
+++ Casks/checkra1n.rb
@@ -1,6 +1,6 @@
 cask 'checkra1n' do
-  version '0.9.8'
-  sha256 'e7f07934860027fe7c503922498ae86d43fd79dc1fc004a85bad24e0a002a7f8'
+  version '0.9.8.1'
+  sha256 'bf055708225db20f51948690563ec87de1a4d53a4dada993e9f4ff70636873ed'
 
   url "https://checkra.in/assets/downloads/macos/#{sha256}/checkra1n%20beta%20#{version}.dmg"
   appcast 'https://checkra.in/'
