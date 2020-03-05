1d7844fbbabe3f7c74edaf9ae6cb087fd85871a1
BrewTestBot <homebrew-ops@lists.sfconservancy.org>
2020-03-05 12:22:31 +1100

    Update yandexradio from 2.0.1,144 to 2.0.2,145 (#78134)

--- Casks/yandexradio.rb
+++ Casks/yandexradio.rb
@@ -1,6 +1,6 @@
 cask 'yandexradio' do
-  version '2.0.1,144'
-  sha256 'd0a26c75dbc226f837941f309101a0231eea2db945fb0449c45cf771ea960b1c'
+  version '2.0.2,145'
+  sha256 '9711ee8dc61e20133f8c6da45ad8df178e38f33614a95162d3c26fb5c655470f'
 
   url "http://tutra.ru/app/Yandex.Radio.#{version.before_comma}-#{version.after_comma}.dmg"
   appcast 'http://tutra.ru/app/yandex.radio/appcast.xml'
