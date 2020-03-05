d3cf4e0b40025fe2b9938a4231ac3c870952601e
Vítor Galvão <info@vitorgalvao.com>
2020-03-05 19:24:43 +0000

    Update drawbot.rb

--- Casks/drawbot.rb
+++ Casks/drawbot.rb
@@ -2,9 +2,9 @@ cask 'drawbot' do
   version '3.123'
   sha256 'ad395d11706ac4a621fb2f1e88131ad0e0d9bb8595f19d65253b2175fdd43707'
 
-  # github.com/typemytype was verified as official when first introduced to the cask
+  # github.com/typemytype/drawbot was verified as official when first introduced to the cask
   url "https://github.com/typemytype/drawbot/releases/download/#{version}/DrawBot.dmg"
-  appcast 'https://raw.githubusercontent.com/typemytype/drawbot/master/drawBot/drawBotSettings.py'
+  appcast 'https://github.com/typemytype/drawbot/releases.atom'
   name 'DrawBot'
   homepage 'https://www.drawbot.com/'
 
