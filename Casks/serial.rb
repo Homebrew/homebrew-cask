077bb4154e7a0082a6eee7cf6667965cfee5f34c
Vítor Galvão <info@vitorgalvao.com>
2020-03-05 19:26:55 +0000

    Update serial.rb

--- Casks/serial.rb
+++ Casks/serial.rb
@@ -3,7 +3,7 @@ cask 'serial' do
   sha256 '05f3a92f60e8eec7c412557d71785ae67c1ea315d2279cf0163d0f5ceaa5a291'
 
   url "https://download.decisivetactics.com/downloads/serial/Serial_#{version}.zip"
-  appcast 'https://api.decisivetactics.com/api/v1/public/appcast?app=serial&v=2'
+  appcast 'https://api.decisivetactics.com/api/v1/public/appcast?app=serial'
   name 'Serial'
   homepage 'https://www.decisivetactics.com/products/serial/'
 
