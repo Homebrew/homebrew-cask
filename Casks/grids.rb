e8929405e52e2569c075e5313f0b2b69141c36b6
BrewTestBot <homebrew-ops@lists.sfconservancy.org>
2020-03-05 21:34:37 +1100

    Update grids (#78157)

--- Casks/grids.rb
+++ Casks/grids.rb
@@ -1,6 +1,6 @@
 cask 'grids' do
   version '6.0.3'
-  sha256 'aa78b19f4b47db70ce78041734d189454c4b4085fbb5c502094749b9cd4a5751'
+  sha256 '2581d83379f265a41b27ab15a0185c4b1ac12bcf8fb29f0cf10117bc58f058eb'
 
   url "https://gridsapp.net/bin/Grids_#{version}.zip"
   appcast 'https://gridsapp.net/appcast.json'
