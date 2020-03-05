19740b22e913b854a3783ec6e4be3dc7bd885e01
BrewTestBot <homebrew-ops@lists.sfconservancy.org>
2020-03-05 21:47:20 +1100

    Update shotcut (#78163)

--- Casks/shotcut.rb
+++ Casks/shotcut.rb
@@ -1,6 +1,6 @@
 cask 'shotcut' do
   version '20.02.17'
-  sha256 'b427c2e3a3273b649a9beb79d3c2914b6c09f5cea3b70948848f5f3d459aa9d5'
+  sha256 'ff91bc7f6248b8bf3bce7060770613bb3afcdf9394b41fd30869d40a81848499'
 
   # github.com/mltframework/shotcut was verified as official when first introduced to the cask
   url "https://github.com/mltframework/shotcut/releases/download/v#{version}/shotcut-macos-signed-#{version.no_dots}.dmg"
