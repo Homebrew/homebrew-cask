45a4e2e9294e2c1e2a97941872006a3545a3a3e9
Work <brigitte@corecode.io>
2020-03-05 19:17:06 +0100

    Update photosweeper-x from 3.5.0 to 3.6.1

--- Casks/photosweeper-x.rb
+++ Casks/photosweeper-x.rb
@@ -1,9 +1,9 @@
 cask 'photosweeper-x' do
-  version '3.5.0'
+  version '3.6.1'
   sha256 '219449a640233f67c1b111636c0a29018c5525d44929d19605b366c79e128e30'
 
   url 'https://overmacs.com/photosweeper/downloads/PhotoSweeperTrial.dmg'
-  appcast 'https://overmacs.com/photosweeper/updates/photosweeper_update.xml'
+  appcast 'https://overmacs.com/feeds/photosweeper_update.xml'
   name 'PhotoSweeper X'
   homepage 'https://overmacs.com/'
 
