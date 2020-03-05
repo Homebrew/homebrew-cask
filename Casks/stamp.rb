7b8ea3be3c3b882e98cb70f71b1b949945c82015
Randall <ran.dall@icloud.com>
2020-03-04 17:05:19 -0800

    Update stamp from 4.15.4 to 4.15.5

--- Casks/stamp.rb
+++ Casks/stamp.rb
@@ -1,6 +1,6 @@
 cask 'stamp' do
-  version '4.15.4'
-  sha256 'f4a3d411781d54282b3a2cbd8e72ef02b4ba40c5e0b236d4b169fb4abc5d231a'
+  version '4.15.5'
+  sha256 '7712a1485ed09486c3fba921b032869439baa6d6d23fafb07516ff77c94bc87b'
 
   # dzqeytqqx888.cloudfront.net was verified as official when first introduced to the cask
   url "https://dzqeytqqx888.cloudfront.net/STAMP#{version.no_dots}.dmg"
