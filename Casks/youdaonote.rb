a7a4cf829f35ab7ba46781427bea1e1326f4b531
Vítor Galvão <info@vitorgalvao.com>
2020-03-05 19:34:55 +0000

    Update youdaonote (#78179)
    * Update youdaonote
    
    * Update youdaonote.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/youdaonote.rb
+++ Casks/youdaonote.rb
@@ -1,6 +1,6 @@
 cask 'youdaonote' do
-  version '3.5.7'
-  sha256 '57877585085acd84db671cdfb58f794066707f917e0263c2a44232be98cd649c'
+  version '3.5.8'
+  sha256 'd3c10b5a53788ae04f5f6d8620c1d96fdb9b075c5473eac9d58d86f02b75878e'
 
   # download.ydstatic.com/notewebsite/downloads was verified as official when first introduced to the cask
   url 'https://download.ydstatic.com/notewebsite/downloads/YoudaoNote.dmg'
