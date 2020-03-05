3aa5683445996b00e2c7ca2cf5c62a06cd445913
Julian Mayer <julian@corecode.io>
2020-03-05 17:59:15 +0100

    Update boom-3d from 1.3.3,1571300754 to 1.3.5

--- Casks/boom-3d.rb
+++ Casks/boom-3d.rb
@@ -1,10 +1,10 @@
 cask 'boom-3d' do
-  version '1.3.3,1571300754'
-  sha256 'd65bd4bd1c146b8c9bd061f35c4e64b8e1d29c6796f6db0bc42a8b9e2f16741a'
+  version '1.3.5'
+  sha256 '5378befcfc5d1d0e2f2b6eb7fea3f4c435cab0d70d86eb23ceb5103607fccbe5'
 
-  # devmate.com/com.globaldelight.Boom3D was verified as official when first introduced to the cask
-  url "https://dl.devmate.com/com.globaldelight.Boom3D/#{version.before_comma}/#{version.after_comma}/Boom3D-#{version.before_comma}.dmg"
-  appcast 'https://updates.devmate.com/com.globaldelight.Boom3D.xml'
+  # d3jbf8nvvpx3fh.cloudfront.net was verified as official when first introduced to the cask
+  url "https://d3jbf8nvvpx3fh.cloudfront.net/Boom3D/Update/v#{version}/Boom_3D_Update.dmg"
+  appcast 'https://www.macupdater.net/cgi-bin/extract_text/send_post_request_data.cgi?url=https://apiboom3.globaldelight.net/v1/native/queryupdate/&data={%22AppIdentifier%22:%22com.globaldelight.Boom3D%22,%22ApplicationVersion%22:%221.3.3%22,%22AppVersion%22:%22101.3.3018%22,%22ApplicationId%22:%22com.globaldelight.Boom3D%22,%22RequestType%22:0,%22AppName%22:%22Boom%203D%22}'
   name 'Boom 3D'
   homepage 'https://www.globaldelight.com/boom3d'
 
