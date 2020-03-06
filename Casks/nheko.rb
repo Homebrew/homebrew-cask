79a3ec04150a3e768c3d1bee340411ff3f128514
Joseph Donofry <rubberduckie3554@gmail.com>
2020-03-06 13:22:12 -0500

    Update Nheko to Active Fork and v0.6.4
    Updated nheko cask to new fork due to original being archived.
    Nheko is now maintained by Nheko-Reborn org on github.
    
    Also updated the version from v0.6.2 to v0.6.4

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -1,11 +1,11 @@
 cask 'nheko' do
-  version '0.6.2'
-  sha256 'b78a8322f9463a7bbb7b1c04aee2b5318c16cef9b5dafddac828e8fd59f2813e'
+  version '0.6.4'
+  sha256 '1214025539bfbbcb4f0808efe1c6f49f478171d8ce9dceb786dfc200f8755546'
 
-  url "https://github.com/mujx/nheko/releases/download/v#{version}/nheko-v#{version}.dmg"
-  appcast 'https://github.com/mujx/nheko/releases.atom'
+  url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}.dmg"
+  appcast 'https://github.com/Nheko-Reborn/nheko/releases.atom'
   name 'Nheko'
-  homepage 'https://github.com/mujx/nheko'
+  homepage 'https://nheko-reborn.github.io/'
 
   app 'Nheko.app'
 end
