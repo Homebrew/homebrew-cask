aab6ff7ced6aea3870441766fd3d44e7c4dc9ca3
Yurii Kolesnykov <root@yurikoles.com>
2020-03-06 08:40:20 +0200

    Update electron from 8.0.3 to 8.1.0

--- Casks/electron.rb
+++ Casks/electron.rb
@@ -1,6 +1,6 @@
 cask 'electron' do
-  version '8.0.3'
-  sha256 'e4e64b591de496d16107b524774c85f7bb81efc5c034e1e1abdf3a5fe63f62eb'
+  version '8.1.0'
+  sha256 'ed6a5e95aea04f7c892d26fc575d96b451abb852bbd2bb2019cdaebc7f2ff661'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
