054c5869236960f62e30180fdacb3675ada3b71f
sudormroot <sudormroot@163.com>
2020-03-06 14:55:08 +0800

    change thenewplayerfree.rb according to the requirement

--- Casks/thenewplayerfree.rb
+++ Casks/thenewplayerfree.rb
@@ -1,9 +1,13 @@
 cask 'thenewplayerfree' do
   version :latest
   sha256 :no_check
-  # github.com/sudormroot.github.io was verified as official when first introduced to the cask
+
+  # github.com was verified as official when first introduced to the cask
   url 'https://github.com/sudormroot/TheNewPlayer_builds/raw/master/releases/macos/latest.tgz'
+  
   name 'TheNewPlayerFree'
+  
   homepage 'https://sudormroot.github.io/TheNewPlayer_builds/'
+  
   app 'TheNewPlayerFree.app'
 end
