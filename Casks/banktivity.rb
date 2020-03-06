e8c30228c0f206ef00623edf23b5eb42115bf559
Julian Mayer <julian@corecode.io>
2020-03-06 23:10:14 +0100

    Update banktivity from 7 to 7.4.2

--- Casks/banktivity.rb
+++ Casks/banktivity.rb
@@ -1,9 +1,10 @@
 cask 'banktivity' do
-  version '7'
-  sha256 :no_check # required as upstream package is updated in-place
+  version '7.4.2'
+  sha256 '604fbeb5405108b6d37b17fc3055503cd03e3e0276c098d569b1f49ed5a84286'
 
   # iggsoft.com was verified as official when first introduced to the cask
-  url "https://www.iggsoft.com/banktivity/Banktivity#{version}_Web.dmg"
+  url "https://www.iggsoft.com/banktivity/Banktivity#{version.major}_Web.dmg"
+  appcast 'https://www.iggsoftware.com/product-versions.json'
   name 'Banktivity'
   homepage 'https://www.iggsoftware.com/banktivity/'
 
