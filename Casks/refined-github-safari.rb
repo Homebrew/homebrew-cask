d6cc9d6b2f5ab81374e891b36ab5626b7e39993a
Sungbin Jo <pcr910303@icloud.com>
2020-03-07 02:25:14 +0900

    Add Refined GitHub for Safari.app v2.0.27

---
+++ Casks/refined-github-safari.rb
@@ -0,0 +1,18 @@
+cask 'refined-github-safari' do
+  version '2.0.27'
+  sha256 '08e63c7ae360b93e54a44e9309a37e2729d04f20cb3fc28ed01980ed56820135'
+
+  url 'https://github.com/lautis/refined-github-safari/releases/download/v2.0.27/Refined-GitHub-for-Safari.zip'
+  appcast 'https://github.com/lautis/refined-github-safari/releases.atom'
+  name 'refined-github-safari'
+  homepage 'https://github.com/lautis/refined-github-safari'
+
+  app 'Refined GitHub for Safari.app'
+
+  zap delete: [
+                '~/Library/Application Scripts/fi.lautanala.refined-github',
+                '~/Library/Application Scripts/fi.lautanala.refined-github-extension',
+                '~/Library/Containers/fi.lautanala.refined-github',
+                '~/Library/Containers/fi.lautanala.refined-github-extenstion',
+              ]
+end
