b5b4eba48c17e841dc49f44b0ea120e1aca14caa
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-05 18:50:32 -0800

    Update zoho-mail from 1.0.20 to 1.1.0 (#78198)

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -1,6 +1,6 @@
 cask 'zoho-mail' do
-  version '1.0.20'
-  sha256 '96ead615b34f574b9fa591b6abbb469dbdf89c1635bc23aa63ad245722c5b1a7'
+  version '1.1.0'
+  sha256 '17ae1d39d526abdec84decbca3ac24d9a46e9d3394600c3575cb45e7336cb24f'
 
   # downloads.zohocdn.com/zmail-desktop/mac was verified as official when first introduced to the cask
   url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-installer-v#{version}.dmg"
