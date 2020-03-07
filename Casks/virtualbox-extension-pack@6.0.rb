5bc87281f128c419dcc89fb4d5fbdee546bdcdad
Lenny <ldotl@yahoo.com>
2020-03-07 00:04:44 -0800

    Added casks for 6.0.18 virtualbox & extension

---
+++ Casks/virtualbox-extension-pack@6.0.rb
@@ -0,0 +1,40 @@
+cask 'virtualbox-extension-pack@6.0' do
+  version '6.0.18'
+  sha256 'bcc88beba4e920290c914d6627199715b6f5917c57371884078c4bb3e3e12c95'
+
+  url "https://download.virtualbox.org/virtualbox/#{version}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
+  appcast 'https://download.virtualbox.org/virtualbox/LATEST.TXT'
+  name 'Oracle VirtualBox Extension Pack'
+  homepage 'https://www.virtualbox.org/'
+
+  conflicts_with cask: ['virtualbox-extension-pack', 'virtualbox-extension-pack-beta']
+  depends_on cask: 'virtualbox@6.0'
+  container type: :naked
+
+  stage_only true
+
+  postflight do
+    system_command '/usr/local/bin/VBoxManage',
+                   args:  [
+                            'extpack', 'install',
+                            '--replace', "#{staged_path}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
+                          ],
+                   input: 'y',
+                   sudo:  true
+  end
+
+  uninstall_postflight do
+    next unless File.exist?('/usr/local/bin/VBoxManage')
+
+    system_command '/usr/local/bin/VBoxManage',
+                   args: [
+                           'extpack', 'uninstall',
+                           'Oracle VM VirtualBox Extension Pack'
+                         ],
+                   sudo: true
+  end
+
+  caveats do
+    license 'https://www.virtualbox.org/wiki/VirtualBox_PUEL'
+  end
+end
