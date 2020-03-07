9661187a261ab9b54b70805378384874b4a13071
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-07 10:33:57 -0800

    Update freeze from 3.12 to 3.12.1 (#78295)

--- Casks/freeze.rb
+++ Casks/freeze.rb
@@ -1,6 +1,6 @@
 cask 'freeze' do
-  version '3.12'
-  sha256 '59bff1a92d65e2b34852c4660611b85a97f10772bc31ac753342fa80c0b9ded6'
+  version '3.12.1'
+  sha256 '815d667f6dcc5b0fabc657a40afe2fe194ce8411a8f5ca22c68b5547afff4998'
 
   url 'https://www.freezeapp.net/download/Freeze.zip'
   appcast 'https://www.freezeapp.net/appcast.xml'
