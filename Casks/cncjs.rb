a215a10a550aa17e9c1bdfdd03e2683d86a7d1ae
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-07 10:36:26 -0800

    Update cncjs from 1.9.20 to 1.9.21 (#78289)

--- Casks/cncjs.rb
+++ Casks/cncjs.rb
@@ -1,6 +1,6 @@
 cask 'cncjs' do
-  version '1.9.20'
-  sha256 'f66d17573d4f99587dca48c12d24f35b79047c0e9a5fa0b3762f55ffe1dc9160'
+  version '1.9.21'
+  sha256 'e110bf573a4fc2f2c76d0d97100a2e49a517f889e1d34169bad1cf9b1ccfd452'
 
   # github.com/cncjs/cncjs was verified as official when first introduced to the cask
   url "https://github.com/cncjs/cncjs/releases/download/v#{version}/cncjs-app-#{version}-mac-x64.dmg"
