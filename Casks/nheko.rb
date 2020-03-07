a957d2727f66c440bf1e1527496e5cfd8c42897c
Vítor Galvão <info@vitorgalvao.com>
2020-03-07 00:05:06 +0000

    Update nheko.rb

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -2,6 +2,7 @@ cask 'nheko' do
   version '0.6.4'
   sha256 '1214025539bfbbcb4f0808efe1c6f49f478171d8ce9dceb786dfc200f8755546'
 
+  # github.com/Nheko-Reborn/nheko was verified as official when first introduced to the cask
   url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}.dmg"
   appcast 'https://github.com/Nheko-Reborn/nheko/releases.atom'
   name 'Nheko'
