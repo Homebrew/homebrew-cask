92ff96d5d37131e4abe45e6d4c0950397105dec2
suschizu <brigitte@corecode.io>
2020-03-07 12:04:56 +0100

    Update kode54-cog from 1109,9dd4f6804 to 1113,17bb70e72 (#78277)

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,6 +1,6 @@
 cask 'kode54-cog' do
-  version '1109,9dd4f6804'
-  sha256 '9cc4c8553be6384fb1c6d3497e6f3f336ef5fdc9ebca42d0f878ee36f6500dd3'
+  version '1113,17bb70e72'
+  sha256 'a6d5442251f8aeed98eff0af62d6f99d78a53b617265470d9d72a35975b3da84'
 
   # losno.co/cog was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
