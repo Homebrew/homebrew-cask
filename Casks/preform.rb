5c6e280da292e4b5b3709509e7f856359c5394d0
Brigitte Zmoelnig <brigitte@corecode.io>
2020-03-07 11:37:36 +0100

    Update preform from 3.4.1,138 to 3.4.2,156

--- Casks/preform.rb
+++ Casks/preform.rb
@@ -1,6 +1,6 @@
 cask 'preform' do
-  version '3.4.1,138'
-  sha256 '5fa98bca41894c17e375e5efd4b26c49622e808fb5dbb7ea335d815811234810'
+  version '3.4.2,156'
+  sha256 'e533bdf4f3bf4c4bc300651ec54459744aeb8a0c1815f87487690c674a5d8a8e'
 
   # s3.amazonaws.com/FormlabsReleases was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/FormlabsReleases/Release/#{version.before_comma}/PreForm_#{version.before_comma}_release_origin_testing_#{version.before_comma}_build_#{version.after_comma}.dmg"
