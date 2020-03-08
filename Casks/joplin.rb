793503361b74ac2141889b76aa7148599b945c68
Daniel Neto <daniel@Daniels-MacBook-Pro.local>
2020-03-08 11:35:27 +0000

    Update joplin from 1.0.179 to 1.0.193

--- Casks/joplin.rb
+++ Casks/joplin.rb
@@ -1,6 +1,6 @@
 cask 'joplin' do
-  version '1.0.179'
-  sha256 'd0705af2c36c5f0fd4ef00d98da5f9bab67dd0468a36badada41b2c697c3780e'
+  version '1.0.193'
+  sha256 '59b55885064f5e271bc674df9e7d98d1e665dfda4ae7e6b201c69b2b2ac96bde'
 
   # github.com/laurent22/joplin was verified as official when first introduced to the cask
   url "https://github.com/laurent22/joplin/releases/download/v#{version}/Joplin-#{version}.dmg"
