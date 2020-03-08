77ccaa238d858587ace05a6d803b1796e24dc55f
Work <brigitte@corecode.io>
2020-03-08 17:49:57 +0100

    Update psychopy from 2020.1.2 to 2020.1.3

--- Casks/psychopy.rb
+++ Casks/psychopy.rb
@@ -1,8 +1,8 @@
 cask 'psychopy' do
-  version '2020.1.2'
-  sha256 'a61a2d2702a5918c8c9700a642966acbae4f28f1ee63917d2adb69af6d104ff2'
+  version '2020.1.3'
+  sha256 'bccf335b807d8934dfb55f7d6e654e9cd371e01d54268031be8717216221c348'
 
-  url "https://github.com/psychopy/psychopy/releases/download/#{version}/StandalonePsychoPy3-#{version}-MacOS.dmg"
+  url "https://github.com/psychopy/psychopy/releases/download/2020.2.3/StandalonePsychoPy3-#{version}-MacOS.dmg"
   appcast 'https://github.com/psychopy/psychopy/releases.atom'
   name 'PsychoPy'
   homepage 'https://github.com/psychopy/psychopy'
