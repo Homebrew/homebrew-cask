4457b8f4f2e25f198d1fed478c5e8673c2e4e717
Vítor Galvão <info@vitorgalvao.com>
2020-03-09 03:36:16 +0000

    Update sdm.rb

--- Casks/sdm.rb
+++ Casks/sdm.rb
@@ -2,8 +2,8 @@ cask 'sdm' do
   version '1.4.11'
   sha256 '6bc413ae69db4d1ed54ae7393ec8cf2af66c5812b2cc387f7884fe34825bf6ce'
 
-  # sdm-releases-production.s3.amazonaws.com/stable/darwin was verified as official when first introduced to the cask
-  url 'https://sdm-releases-production.s3.amazonaws.com/stable/darwin/1.4.11/6D62E3F917774B61BF648A76AAE5CCA1DDD31E23/SDM-1.4.11.dmg'
+  # sdm-releases-production.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://sdm-releases-production.s3.amazonaws.com/stable/darwin/#{version}/6D62E3F917774B61BF648A76AAE5CCA1DDD31E23/SDM-#{version}.dmg"
   name 'StrongDM'
   homepage 'https://strongdm.com/'
 
