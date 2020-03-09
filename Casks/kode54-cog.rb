f3348d7de2ebcb3cdaa5b33540f542e9d7b65136
Randall <ran.dall@icloud.com>
2020-03-08 23:04:28 -0700

    Update kode54-cog from 1113,17bb70e72 to 1119,18acc7fdd

--- Casks/kode54-cog.rb
+++ Casks/kode54-cog.rb
@@ -1,6 +1,6 @@
 cask 'kode54-cog' do
-  version '1113,17bb70e72'
-  sha256 'a6d5442251f8aeed98eff0af62d6f99d78a53b617265470d9d72a35975b3da84'
+  version '1119,18acc7fdd'
+  sha256 '02a6b468aa9e9d91361a1fe2db59763558a8c4fe88b2bf3f4d1feea05aa3c7fb'
 
   # losno.co/cog was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma}.zip"
