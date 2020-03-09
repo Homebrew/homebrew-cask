b1525709e86320c121365353a96b602067e10fc1
Vítor Galvão <info@vitorgalvao.com>
2020-03-09 14:37:21 +0000

    Update anki.rb

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -3,7 +3,7 @@ cask 'anki' do
   sha256 '37d37199d942fd7cf5ae681097ac33835e991f5a1557908e4cba26482aab8852'
 
   url "https://apps.ankiweb.net/downloads/current/anki-#{version}-mac.dmg"
-  appcast 'https://changes.ankiweb.net/'
+  appcast 'https://changes.ankiweb.net/README.md'
   name 'Anki'
   homepage 'https://apps.ankiweb.net/'
 
