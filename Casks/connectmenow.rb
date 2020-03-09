2225a5b7fb2b4b82fa58d6cc27956c1fb85dfe8f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-09 16:34:39 -0700

    Update connectmenow from 3.0.3 to 3.0.5 (#78393)

--- Casks/connectmenow.rb
+++ Casks/connectmenow.rb
@@ -1,6 +1,6 @@
 cask 'connectmenow' do
-  version '3.0.3'
-  sha256 'aebcb29bb939e29c492ee9b947c543843bba45c03d76bd18a3dce83e88e67924'
+  version '3.0.5'
+  sha256 'b54a1289c0d81f2d873818972f88d68e0def9b438fe7af3278c85a605b4caa82'
 
   url "https://www.tweaking4all.com/downloads/network/ConnectMeNow-v#{version}-macOS-64bit.dmg"
   appcast 'https://www.tweaking4all.com/os-tips-and-tricks/macosx-tips-and-tricks/connectmenow-v3'
