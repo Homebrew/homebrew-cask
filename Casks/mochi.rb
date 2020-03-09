9d802e0549e19ff54bc2f026afeab14611ae017e
Vítor Galvão <info@vitorgalvao.com>
2020-03-09 22:45:13 +0000

    Update mochi.rb

--- Casks/mochi.rb
+++ Casks/mochi.rb
@@ -2,7 +2,8 @@ cask 'mochi' do
   version '1.4.6'
   sha256 'b0c269f974398a6122cc3d1a295cc703bad9549d66f50df92ad53cdfb9a7b228'
 
-  url 'https://mochi.cards/releases/Mochi-1.4.6.dmg'
+  url "https://mochi.cards/releases/Mochi-#{version}.dmg"
+  appcast 'https://mochi.cards/'
   name 'Mochi'
   homepage 'https://mochi.cards/'
 
