209f1aff3db1e8088a480d684414e255c55cfaed
Vítor Galvão <info@vitorgalvao.com>
2020-03-09 03:29:54 +0000

    Update utools.rb

--- Casks/utools.rb
+++ Casks/utools.rb
@@ -2,7 +2,7 @@ cask 'utools' do
   version '0.8.9-beta'
   sha256 'f4c206ee440974d1b3386333f28533d603bbfd0a0878f00c6721b74765a1c967'
 
-  # u-tools.cn was verified as official when first introduced to the cask
+  # resource.u-tools.cn was verified as official when first introduced to the cask
   url "https://resource.u-tools.cn/currentversion/uTools-#{version}.dmg"
   appcast 'https://resource.u-tools.cn/currentversion/latest-mac.yml'
   name 'uTools'
