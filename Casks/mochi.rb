b61b6c65104ee2792d432dfd4ab122a678dea851
Nate Smith <nwjsmith@gmail.com>
2020-03-09 15:01:06 -0400

    Add Mochi.app v1.4.6

---
+++ Casks/mochi.rb
@@ -0,0 +1,10 @@
+cask 'mochi' do
+  version '1.4.6'
+  sha256 'b0c269f974398a6122cc3d1a295cc703bad9549d66f50df92ad53cdfb9a7b228'
+
+  url 'https://mochi.cards/releases/Mochi-1.4.6.dmg'
+  name 'Mochi'
+  homepage 'https://mochi.cards/'
+
+  app 'Mochi.app'
+end
