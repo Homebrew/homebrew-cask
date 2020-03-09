7c524cd58722574c14d8e5e6796500de7c24395b
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-08 23:08:16 -0700

    Update electrocrud from 2.6.0 to 2.6.1 (#78345)

--- Casks/electrocrud.rb
+++ Casks/electrocrud.rb
@@ -1,6 +1,6 @@
 cask 'electrocrud' do
-  version '2.6.0'
-  sha256 '8915ff804c9d9f392d3d0ca0201246a7b3a9a53a5eefbab3602572074dcee12a'
+  version '2.6.1'
+  sha256 '445373333da3810a10a8bfceafcb7a4033e00fadfd203de7df11a31b88f5f404'
 
   url "https://github.com/garrylachman/ElectroCRUD/releases/download/#{version}/ElectroCRUD-#{version}.dmg"
   appcast 'https://github.com/garrylachman/ElectroCRUD/releases.atom'
