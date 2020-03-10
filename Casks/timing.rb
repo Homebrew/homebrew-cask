5590f0fa2d55c08cb140136853c31c11a5ec631b
suschizu <brigitte@corecode.io>
2020-03-10 13:34:36 +0100

    Update timing from 2020.1 to 2020.2 (#78419)

--- Casks/timing.rb
+++ Casks/timing.rb
@@ -1,6 +1,6 @@
 cask 'timing' do
-  version '2020.1'
-  sha256 '7dd2b63ba2b1a7c1a53d3556e6f35763b368dc9f83fba2f0cbc0f2bc5ce5f434'
+  version '2020.2'
+  sha256 '3d1c41fd026327afe3cec412827a789ca00c16ba43af765c31e9991a1d8aa023'
 
   url 'https://timingapp.com/download/Timing.dmg'
   appcast 'https://timingapp.com/updates/timing2.xml'
