2c604c3131c27d3565b388ce94e47cce08afd533
Vítor Galvão <info@vitorgalvao.com>
2020-03-10 16:27:29 +0000

    Update opencore-configurator.rb

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,6 +1,6 @@
 cask 'opencore-configurator' do
   version '1.17.0.0'
-  sha256 '225fef71ffa1338b95f5c08d75fe2dd8a0cc83682431bca0d0c635e4f0826463'
+  sha256 'f2aec36c46623baabce9c46df8812290bdc29687987f871c53091fc6ba1fa987'
 
   url 'https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last'
   appcast 'https://mackie100projects.altervista.org/category/opencore-configurator-changelog/'
