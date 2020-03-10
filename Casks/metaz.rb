42eb8e5445a7280beb545056db2a8207061281e5
Work <brigitte@corecode.io>
2020-03-10 13:28:17 +0100

    Update metaz from 1.0.beta-49 to 1.0.beta-78

--- Casks/metaz.rb
+++ Casks/metaz.rb
@@ -1,6 +1,6 @@
 cask 'metaz' do
-  version '1.0.beta-49'
-  sha256 '4a9a4fb531efa1aef2d62d67ea694b7ac96387ded5b348519958f67619106f74'
+  version '1.0.beta-78'
+  sha256 'b7e9d4235f377f790612f65c7c1e9c35c4d4714b15c0c9bc696537bb282ebc4a'
 
   # github.com/griff/metaz was verified as official when first introduced to the cask
   url "https://github.com/griff/metaz/releases/download/v#{version}/MetaZ-#{version}.zip"
