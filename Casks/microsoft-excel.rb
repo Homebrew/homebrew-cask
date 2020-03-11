20c9fcbbee8913cd3eb23ab6b162cb3547df9e28
Qi <25192197+singularitti@users.noreply.github.com>
2020-03-10 20:34:00 -0400

    Update microsoft-excel from 16.34.20020900 to 16.35.20030802 (#78444)

--- Casks/microsoft-excel.rb
+++ Casks/microsoft-excel.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-excel' do
-  version '16.34.20020900'
-  sha256 '5ba5c8d0a036ad7a53d023521b4656ceefbd0fe7a4a55ba25efeb4e0aae388fb'
+  version '16.35.20030802'
+  sha256 'dfe5f4230dc3e4b85ff226e260580fbd5a85893dd17c6165721f92a03fdfdf91'
 
   # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Excel_#{version}_Installer.pkg"
