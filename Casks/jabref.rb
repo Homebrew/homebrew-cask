2167b4ea7535b8cb09d720df2b1175aa67958fbe
Moritz Falke <m.falke@sap.com>
2020-03-11 07:53:30 +0100

    Update jabref from 4.3.1 to 5.0

--- Casks/jabref.rb
+++ Casks/jabref.rb
@@ -1,15 +1,15 @@
 cask 'jabref' do
-  version '4.3.1'
-  sha256 '6df22704818cee1e32ead6d08f4ee025d85d733581e5b830a0d155481880d49e'
+  version '5.0'
+  sha256 'a02b02c1b54ebcfa5011e571800483d23d1774fb6117abff8e35055079e7d2e3'
 
   # github.com/JabRef/jabref was verified as official when first introduced to the cask
-  url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef_macos_#{version.dots_to_underscores}.dmg"
+  url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef-#{version}.dmg"
   appcast 'https://github.com/JabRef/jabref/releases.atom'
   name 'JabRef'
   homepage 'https://www.jabref.org/'
 
   installer script: {
-                      executable: 'JabRef Installer.app/Contents/MacOS/JavaApplicationStub',
+                      executable: 'JabRef Installer.app/Contents/MacOS/JabRef',
                       args:       [
                                     '-q',
                                     '-VcreateDesktopLinkAction$Boolean=false',
