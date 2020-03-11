c234e48175f8f4b2f9c49dcdf8089cc89488305a
Travis CI <travis@Traviss-Mac.local>
2020-03-11 16:08:34 +0000

    Update alt-tab from 3.1.0 to 3.1.1

--- Casks/alt-tab.rb
+++ Casks/alt-tab.rb
@@ -1,6 +1,6 @@
 cask 'alt-tab' do
-  version '3.1.0'
-  sha256 '1f331144a28305e59fc69435f6d657b09af57d130109f91137c3126f6518e183'
+  version '3.1.1'
+  sha256 'dcaf6048a9eaa12dd5c3bb6c61f7cde891c2aa553b5fd05d94c1e0a216f2b267'
 
   url "https://github.com/lwouis/alt-tab-macos/releases/download/v#{version}/AltTab-#{version}.zip"
   appcast 'https://github.com/lwouis/alt-tab-macos/releases.atom'
