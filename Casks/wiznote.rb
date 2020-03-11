4c26552a19b11bd72a7d3144f56a418a491af92d
Work <brigitte@corecode.io>
2020-03-11 19:10:29 +0100

    Update wiznote

--- Casks/wiznote.rb
+++ Casks/wiznote.rb
@@ -3,7 +3,7 @@ cask 'wiznote' do
   sha256 '14de8ac137eb2e961ce360e464c02985128745ae81992a8d58f28dfeea28d7e3'
 
   url "https://get.wiz.cn/wiznote-macos-#{version.after_comma}.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://url.wiz.cn/u/mac',
+  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://url.wiz.cn/u/mac',
           configuration: version.after_comma
   name 'WizNote'
   homepage 'https://www.wiz.cn/wiznote-mac.html'
