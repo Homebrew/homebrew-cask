29639a319c1f204eb4e48a67d7d4b12b051dbe6a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-11 00:07:53 -0700

    Update stoplight-studio from 1.10.0 to 1.10.1 (#78463)

--- Casks/stoplight-studio.rb
+++ Casks/stoplight-studio.rb
@@ -1,6 +1,6 @@
 cask 'stoplight-studio' do
-  version '1.10.0'
-  sha256 '7910d54d2dca7cc466930ef21fb6a5e4d8dda6a39ad970ee5ac896a4de59fa30'
+  version '1.10.1'
+  sha256 '1a24a7b846e8a1203f468646d717d420a97757a3ae092353ce822285b09fa129'
 
   # github.com/stoplightio/studio was verified as official when first introduced to the cask
   url "https://github.com/stoplightio/studio/releases/download/v#{version}/stoplight-studio-mac.dmg"
