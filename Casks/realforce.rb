1098e5f529acd8269d327d480bd5d7a10955ac12
Yusuke Arakawa <mofneko@users.noreply.github.com>
2020-03-11 21:20:00 +0900

    Create realforce.rb

---
+++ Casks/realforce.rb
@@ -0,0 +1,12 @@
+cask 'realforce' do
+  version '1.0.2'
+  sha256 '9db2e54faee1b3ecb4d727585827edbe0ce07940c2738e296ddaf0ccb28b0d5a'
+
+  url "https://www.realforce.co.jp/support/download/Install%20REALFORCE%20Software_#{version.dots_to_hyphens}.pkg"
+  name 'REALFORCE for Mac'
+  homepage 'https://www.realforce.co.jp/support/download/index.html'
+
+  pkg "Install REALFORCE Software_#{version.dots_to_hyphens}.pkg"
+
+  uninstall pkgutil: 'com.topre.installpkg.realforce'
+end
