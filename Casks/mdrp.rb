8e8bc174078e7362fe68730a60a665ef1cdd81fd
Randall <ran.dall@icloud.com>
2020-03-11 14:55:15 -0700

    Update mdrp from 8.0.4 to 8.0.5

--- Casks/mdrp.rb
+++ Casks/mdrp.rb
@@ -1,6 +1,6 @@
 cask 'mdrp' do
-  version '8.0.4'
-  sha256 'bb4891f44d49a8e9ba4bf313e809422b3168a7055299b0ef5b27c30388573159'
+  version '8.0.5'
+  sha256 'bb76a063f54f06523e17b03ec53f0886d82800dc8fc186e059f489d4285e8960'
 
   url "https://www.macdvdripperpro.com/MDRP_v#{version.no_dots}.zip"
   appcast "https://www.macdvdripperpro.com/mdrp_sparkle#{version.major}.xml"
