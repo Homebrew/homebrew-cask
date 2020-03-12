91da9dd7c2565eeffcf140c7fb7efca84dc17d6b
CoreCode <core-code@users.noreply.github.com>
2020-03-13 00:47:26 +0100

    Update remembear (#78564)

--- Casks/remembear.rb
+++ Casks/remembear.rb
@@ -1,6 +1,6 @@
 cask 'remembear' do
   version '1.4.13'
-  sha256 '4f92f4aa35f0417622d15aed29b626b95b918f803d3cd20e60a05fa451e47f2f'
+  sha256 '2ce98faa5469727ea9a9ea969c816f72b96570ba6023204143fca482055095af'
 
   # remembear.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://remembear.s3.amazonaws.com/app/release/downloads/macOS/RememBear-#{version}.dmg"
