4a8a4a30054bc8c79f1583db520cfe6b2426cb50
Vítor Galvão <info@vitorgalvao.com>
2020-03-12 02:38:21 +0000

    Update realforce.rb

--- Casks/realforce.rb
+++ Casks/realforce.rb
@@ -3,6 +3,7 @@ cask 'realforce' do
   sha256 '9db2e54faee1b3ecb4d727585827edbe0ce07940c2738e296ddaf0ccb28b0d5a'
 
   url "https://www.realforce.co.jp/support/download/Install%20REALFORCE%20Software_#{version.dots_to_hyphens}.pkg"
+  appcast 'https://www.realforce.co.jp/support/download/index.html'
   name 'REALFORCE for Mac'
   homepage 'https://www.realforce.co.jp/support/download/index.html'
 
