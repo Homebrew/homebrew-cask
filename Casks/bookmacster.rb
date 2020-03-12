20b497dc33fa77dffa2eebc128514571d841af77
Randall <ran.dall@icloud.com>
2020-03-12 16:49:14 -0700

    Update bookmacster from 2.10.2 to 2.10.17

--- Casks/bookmacster.rb
+++ Casks/bookmacster.rb
@@ -1,6 +1,6 @@
 cask 'bookmacster' do
-  version '2.10.2'
-  sha256 '5e5092695e3714644e5fbca586e1bf0c55eb2d42f98c944231b2ee85555d72ac'
+  version '2.10.17'
+  sha256 '8de7102d5cc599569f0172b6bb96bfbc93b286713984931fbf0fc0341cdf1ab7'
 
   url 'https://sheepsystems.com/bookmacster/BookMacster.zip'
   appcast 'https://sheepsystems.com/bookmacster/appcast.xml'
