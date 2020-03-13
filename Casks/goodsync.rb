669dff5811d28ff49ef9b07ec2efd26336915072
Julian Mayer <julian@corecode.io>
2020-03-13 12:03:22 +0100

    Update goodsync from 10.10.25.5 to 10.10.27.7

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.10.25.5'
-  sha256 '6f40befde16efc04c9c86f44fa5a0f15c34df2c680c13dbe1872e54edd6edce6'
+  version '10.10.27.7'
+  sha256 '6990a2ef67c49b097497228ca26999a8ae2b8cad6533618d711f95c83b2410ec'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://www.goodsync.com/download?os=macos',
