ef8e4aac9bcfd39a90398396c2b4135dcb187d2b
Maxim Belkin <maxim.belkin@gmail.com>
2020-03-13 14:27:31 -0700

    Thonny XXL: new cask

--- Casks/thonny-xxl.rb
+++ Casks/thonny-xxl.rb
@@ -1,16 +1,16 @@
-cask 'thonny' do
+cask 'thonny-xxl' do
   version '3.2.7'
-  sha256 'f70a3dc2321e2ab0abf433900a5a36c231d1cccdd1e4aba48c400926f34d3c2b'
+  sha256 '748fb7893b3c4ab63291a6ea88ea2aa054f8a4591cbfac56878a5550b663d887'
 
   # github.com/thonny/thonny/releases/download was verified as official when first introduced to the cask
-  url "https://github.com/thonny/thonny/releases/download/v#{version}/thonny-#{version}.pkg"
+  url "https://github.com/thonny/thonny/releases/download/v#{version}/thonny-xxl-#{version}.pkg"
   appcast 'https://github.com/thonny/thonny/releases.atom'
-  name 'Thonny'
+  name 'Thonny (XXL bundle)'
   homepage 'https://thonny.org/'
 
-  conflicts_with cask: 'thonny-xxl'
+  conflicts_with cask: 'thonny'
 
-  pkg "thonny-#{version}.pkg"
+  pkg "thonny-xxl-#{version}.pkg"
 
   uninstall quit:    'Thonny',
             pkgutil: 'org.thonny.Thonny.component',
