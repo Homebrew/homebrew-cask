bd5e12bb0449f0b981c120d5493d4d82f224ed47
Julian Mayer <julian@corecode.io>
2020-03-13 12:26:24 +0100

    Update freeze

--- Casks/freeze.rb
+++ Casks/freeze.rb
@@ -1,6 +1,6 @@
 cask 'freeze' do
   version '3.12.1'
-  sha256 '815d667f6dcc5b0fabc657a40afe2fe194ce8411a8f5ca22c68b5547afff4998'
+  sha256 '12ec213afe5fa0500978a3c454227035e2f53aaceeab1c3bc14ce718fc437103'
 
   url 'https://www.freezeapp.net/download/Freeze.zip'
   appcast 'https://www.freezeapp.net/appcast.xml'
