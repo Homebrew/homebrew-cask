8f511d95ce55517d891bba66bac095967e7f86e1
Randall <ran.dall@icloud.com>
2020-03-13 15:11:06 -0700

    Update roblox from 0.422.0.388145,5a33b28026914549 to 0.423.0.391560,182f34f20e5841a5

--- Casks/roblox.rb
+++ Casks/roblox.rb
@@ -1,6 +1,6 @@
 cask 'roblox' do
-  version '0.422.0.388145,5a33b28026914549'
-  sha256 'aca41c4d937e155596bf4fe3fe4cc400e96767d8be25afee0f7f09066eec58c5'
+  version '0.423.0.391560,182f34f20e5841a5'
+  sha256 '8c96b736b42273f85daa26a46c751421bbd64e1d13c1ec24b877591ceb9e23da'
 
   # setup.rbxcdn.com was verified as official when first introduced to the cask
   url "https://setup.rbxcdn.com/mac/version-#{version.after_comma}-Roblox.dmg"
