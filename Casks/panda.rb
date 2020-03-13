e694c748b1725986ed8d4933e032b023ecde89e2
Vítor Galvão <info@vitorgalvao.com>
2020-03-13 14:54:58 +0000

    Update panda.rb

--- Casks/panda.rb
+++ Casks/panda.rb
@@ -2,7 +2,6 @@ cask 'panda' do
   version '1.4.2'
   sha256 '551a2f4c2195dd1c00e7dfe83836a86485212ebeb6b008f130cecb83e002e3b8'
 
-  # github.com/pablosproject/Panda-Mac-app was verified as official when first introduced to the cask
   url "https://github.com/pablosproject/Panda-Mac-app/releases/download/#{version}/Panda.zip"
   appcast 'https://github.com/pablosproject/Panda-Mac-app/releases.atom'
   name 'Panda'
