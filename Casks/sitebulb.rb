13adde45c62dd186a5b695a79e68e0a434384f81
suschizu <brigitte@corecode.io>
2020-03-13 15:05:03 +0100

    Update sitebulb from 3.3.1 to 3.4 (#78629)

--- Casks/sitebulb.rb
+++ Casks/sitebulb.rb
@@ -1,6 +1,6 @@
 cask 'sitebulb' do
-  version '3.3.1'
-  sha256 '3a6510e9e919951eff36b1c0414c533ddc8f445e7167bc9220ae1efcbec30940'
+  version '3.4'
+  sha256 'c3d2bfcaac54eb6bfba856a20ba857bfc3174b801f5f70b0283f442ef4dab156'
 
   url "https://downloads.sitebulb.com/#{version}/macOS/Sitebulb.dmg"
   appcast 'https://sitebulb.com/download/'
