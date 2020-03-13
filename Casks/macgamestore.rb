62980de3adb2f02ffd09584211878e7c638a21d0
CoreCode <core-code@users.noreply.github.com>
2020-03-13 12:19:45 +0100

    Update macgamestore (#78607)

--- Casks/macgamestore.rb
+++ Casks/macgamestore.rb
@@ -1,6 +1,6 @@
 cask 'macgamestore' do
   version '4.0.3,6068'
-  sha256 'a813bcec63c3c647bf04ec8dbc6d1a7757cb9f0da96e8fdda6bcbadd097db213'
+  sha256 '435a6e31afdd27c014f5d70ab6f290832dfb8aec89cd460f48f1409afc13b8b1'
 
   url "https://www.macgamestore.com/api_clientapp/clientupdates/public/core6/MacGameStore_#{version.before_comma}_#{version.after_comma}.tgz"
   appcast 'https://www.macgamestore.com/api_clientapp/clientupdates/public/update.xml'
