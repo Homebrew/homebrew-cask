520caf46541a1b79445e4b14d945bfd428a9d112
Vítor Galvão <info@vitorgalvao.com>
2020-03-14 02:59:05 +0000

    Update thonny-xxl.rb

--- Casks/thonny-xxl.rb
+++ Casks/thonny-xxl.rb
@@ -2,7 +2,7 @@ cask 'thonny-xxl' do
   version '3.2.7'
   sha256 '748fb7893b3c4ab63291a6ea88ea2aa054f8a4591cbfac56878a5550b663d887'
 
-  # github.com/thonny/thonny/releases/download was verified as official when first introduced to the cask
+  # github.com/thonny/thonny was verified as official when first introduced to the cask
   url "https://github.com/thonny/thonny/releases/download/v#{version}/thonny-xxl-#{version}.pkg"
   appcast 'https://github.com/thonny/thonny/releases.atom'
   name 'Thonny (XXL bundle)'
