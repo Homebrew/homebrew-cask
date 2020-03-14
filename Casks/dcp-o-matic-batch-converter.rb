e2c7bd7f66888bc8686dcc9fba9afef183769d8c
Work <brigitte@corecode.io>
2020-03-14 13:43:46 +0100

    Update dcp-o-matic-batch-converter from 2.14.31 to 2.14.32

--- Casks/dcp-o-matic-batch-converter.rb
+++ Casks/dcp-o-matic-batch-converter.rb
@@ -1,6 +1,6 @@
 cask 'dcp-o-matic-batch-converter' do
-  version '2.14.31'
-  sha256 'd27a088805bbc4772004371169b01521c4c915602dd1c31bbd941fc1ce31c1f9'
+  version '2.14.32'
+  sha256 'f08af591098d5fc8d3e8e2990c9afc97377a4fc1e79f24fabf78585dace22bc0'
 
   url "https://dcpomatic.com/dl.php?id=osx-10.9-batch&version=#{version}"
   appcast 'https://dcpomatic.com/download'
