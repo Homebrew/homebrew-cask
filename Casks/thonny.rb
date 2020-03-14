aef6076d4c6caa11247b5990799b44bed874fd6c
Vítor Galvão <info@vitorgalvao.com>
2020-03-14 02:59:35 +0000

    thonny.rb: shorten comment

--- Casks/thonny.rb
+++ Casks/thonny.rb
@@ -2,7 +2,7 @@ cask 'thonny' do
   version '3.2.7'
   sha256 'f70a3dc2321e2ab0abf433900a5a36c231d1cccdd1e4aba48c400926f34d3c2b'
 
-  # github.com/thonny/thonny/releases/download was verified as official when first introduced to the cask
+  # github.com/thonny/thonny was verified as official when first introduced to the cask
   url "https://github.com/thonny/thonny/releases/download/v#{version}/thonny-#{version}.pkg"
   appcast 'https://github.com/thonny/thonny/releases.atom'
   name 'Thonny'
