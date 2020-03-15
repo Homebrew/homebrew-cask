22c189842ed2c2992a9703d262790be2c611bdee
suschizu <brigitte@corecode.io>
2020-03-15 19:05:27 +0100

    Update unity-download-assistant from 2019.3.4f1,4f139db2fdbd to 2019.3.5f1,d691e07d38ef (#78711)

--- Casks/unity-download-assistant.rb
+++ Casks/unity-download-assistant.rb
@@ -1,6 +1,6 @@
 cask 'unity-download-assistant' do
-  version '2019.3.4f1,4f139db2fdbd'
-  sha256 '5149aecdcf500193d61d6a7a339fb5674aff5c9b326c75a717f2fc7eaab2becd'
+  version '2019.3.5f1,d691e07d38ef'
+  sha256 'f6784145abf05ad68f8f07de748f36c4a5dcd71acc0d2a734054bc8eeab6f2e7'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/UnityDownloadAssistant-#{version.before_comma}.dmg"
   appcast 'https://unity3d.com/get-unity/download/archive'
