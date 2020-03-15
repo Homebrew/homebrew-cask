c48184533d638015994a441506743f79aff3bc67
suschizu <brigitte@corecode.io>
2020-03-15 19:48:47 +0100

    Update corona-tracker from 1.2.1 to 1.3.1 (#78718)

--- Casks/corona-tracker.rb
+++ Casks/corona-tracker.rb
@@ -1,6 +1,6 @@
 cask 'corona-tracker' do
-  version '1.2.1'
-  sha256 '201d5b6103e6c6a7ba28779998c9e3b20077b731e36633be62e340247ada28ea'
+  version '1.3.1'
+  sha256 '94b8c54cf01a359982fa0d972d50aa57f47d0080cfd8be6b7f8ca5001bbb1238'
 
   # github.com/MhdHejazi/CoronaTracker was verified as official when first introduced to the cask
   url "https://github.com/MhdHejazi/CoronaTracker/releases/download/v#{version}/CoronaTracker-macOS.zip"
