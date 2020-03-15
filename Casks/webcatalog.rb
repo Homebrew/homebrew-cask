c50a3d91060eacf4488ae4fc12d30a784ec073f4
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-14 17:10:30 -0700

    Update webcatalog from 19.4.4 to 19.5.0 (#78685)

--- Casks/webcatalog.rb
+++ Casks/webcatalog.rb
@@ -1,6 +1,6 @@
 cask 'webcatalog' do
-  version '19.4.4'
-  sha256 '75cd36f506027388049ceecce1389a11c9ace7c0fb3b11e46849870eeac4b849'
+  version '19.5.0'
+  sha256 'a334a87715faf1c933b9eb5029b7dee09295f01872b34d3cedb7b0c7b40006ef'
 
   # github.com/quanglam2807/webcatalog was verified as official when first introduced to the cask
   url "https://github.com/quanglam2807/webcatalog/releases/download/v#{version}/WebCatalog-#{version}-mac.zip"
