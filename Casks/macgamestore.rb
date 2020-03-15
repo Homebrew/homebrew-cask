22063e695575eb0a1f77217f149b62076d9d9135
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-14 17:09:14 -0700

    Update macgamestore from 4.0.3,6068 to 4.0.4,6069 (#78687)

--- Casks/macgamestore.rb
+++ Casks/macgamestore.rb
@@ -1,6 +1,6 @@
 cask 'macgamestore' do
-  version '4.0.3,6068'
-  sha256 '435a6e31afdd27c014f5d70ab6f290832dfb8aec89cd460f48f1409afc13b8b1'
+  version '4.0.4,6069'
+  sha256 'f405a4e6d57d843432770f0abcd28196c0e7f5c72992b483a0141ba93aa05ba6'
 
   url "https://www.macgamestore.com/api_clientapp/clientupdates/public/core6/MacGameStore_#{version.before_comma}_#{version.after_comma}.tgz"
   appcast 'https://www.macgamestore.com/api_clientapp/clientupdates/public/update.xml'
