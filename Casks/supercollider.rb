d17b3ffcaf0d568175f9a240dd9eb99c54d10165
Vítor Galvão <info@vitorgalvao.com>
2020-03-15 16:11:15 +0000

    supercollider.rb: fix shasum (#78706)

--- Casks/supercollider.rb
+++ Casks/supercollider.rb
@@ -1,6 +1,6 @@
 cask 'supercollider' do
   version '3.11.0'
-  sha256 '89631b3f4685b7b49fb10358feff5e255ee86f1c1c1be478d9d199c4e158cb05'
+  sha256 '549e098f1895d3ae83ce12065b42c34b5cad5b3daebe75bb3002f659d686f04b'
 
   # github.com/supercollider/supercollider was verified as official when first introduced to the cask
   url "https://github.com/supercollider/supercollider/releases/download/Version-#{version}/SuperCollider-#{version}-macOS-signed.zip"
