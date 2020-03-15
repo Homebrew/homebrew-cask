1852da1ba2b3c5a5b149f1c50461e7eb2ebe38f2
m33m33 <m33@tok715.net>
2020-03-15 09:57:39 +0100

    fix indentation

--- Casks/lazarus.rb
+++ Casks/lazarus.rb
@@ -1,9 +1,9 @@
 cask 'lazarus' do
   version '2.0.6'
-   sha256 '9bb74062462775cf325288f2e42361d7287ad8dd9c0150ef79710442a05389f0'
+  sha256 '9bb74062462775cf325288f2e42361d7287ad8dd9c0150ef79710442a05389f0'
 
   # sourceforge.net/lazarus was verified as official when first introduced to the cask
-   url "https://downloads.sourceforge.net/lazarus/LazarusIDE-#{version}-macos-x86_64.pkg"
+  url "https://downloads.sourceforge.net/lazarus/LazarusIDE-#{version}-macos-x86_64.pkg"
 
   appcast 'https://sourceforge.net/projects/lazarus/rss'
   name 'Lazarus'
