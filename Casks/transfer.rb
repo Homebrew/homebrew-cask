2241997508fc1e41a11233674130c4272eba5c2e
Vítor Galvão <info@vitorgalvao.com>
2020-03-16 17:24:14 +0000

    Update transfer.rb

--- Casks/transfer.rb
+++ Casks/transfer.rb
@@ -3,6 +3,7 @@ cask 'transfer' do
   sha256 'e8940345dc4212f6d77cac5b61c1dd0147ad3e9d1d7aeb21e6c344ee0b8de3e5'
 
   url "https://www.elektron.se/wp-content/uploads/2018/03/Elektron_Transfer_#{version}_macOS.zip"
+  appcast 'https://www.elektron.se/support/?connection=transfer'
   name 'Elektron Transfer'
   homepage 'https://www.elektron.se/support/?connection=transfer#resources'
 
