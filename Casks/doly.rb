dd2e8d9ef82e94c4f3dc7924e54eab26269e2da6
Vítor Galvão <info@vitorgalvao.com>
2020-03-16 23:47:00 +0000

    Update doly.rb

--- Casks/doly.rb
+++ Casks/doly.rb
@@ -3,6 +3,7 @@ cask 'doly' do
   sha256 'e33e695dbece9e07851d808e90d44429828d1c2b0832364cb470983b849e6d1d'
 
   url "http://doly.jp/download/doly-mac_#{version}.dmg"
+  appcast 'http://doly.jp/'
   name 'Doly Ebook Reader'
   homepage 'http://doly.jp/'
 
