b14e41af20615a923f5800920b68f3d9e5e6d0e1
suschizu <brigitte@corecode.io>
2020-03-16 17:31:18 +0100

    Update djay-pro from 2.1.1,202002142237 to 2.1.2,202003131654 (#78734)

--- Casks/djay-pro.rb
+++ Casks/djay-pro.rb
@@ -1,6 +1,6 @@
 cask 'djay-pro' do
-  version '2.1.1,202002142237'
-  sha256 'f4b5f9a37fe91c75fe419b3d60ba8cef0d89dbb95024caa02d98103f1b6f97c9'
+  version '2.1.2,202003131654'
+  sha256 '89cbeabe465aa70b4bc3f2a5494a5743154e34860eb27ab6555a592041e89f6a'
 
   url "http://download.algoriddim.com/djay/#{version.after_comma}/djay_Pro_#{version.before_comma}.zip"
   appcast 'https://www.algoriddim.com/djay-pro-mac/releasenotes/appcast'
