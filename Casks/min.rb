070a4df67e2bf5f0043dab9a09ae1949bc525f41
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-16 14:07:42 -0700

    Update min from 1.13.0 to 1.13.2 (#78756)

--- Casks/min.rb
+++ Casks/min.rb
@@ -1,6 +1,6 @@
 cask 'min' do
-  version '1.13.0'
-  sha256 '3e96cda38ff45345d815290a28623895f47627b99a9117a60dec9b999cfa0ed0'
+  version '1.13.2'
+  sha256 'b7173abfb51414162f7a5d924c0857f49cca2ce83afd62bdad649547706af9b4'
 
   # github.com/minbrowser/min was verified as official when first introduced to the cask
   url "https://github.com/minbrowser/min/releases/download/v#{version}/Min-v#{version}-darwin-x64.zip"
