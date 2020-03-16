bcaec1c616464676a21ba1b8b39ccb32c5fae3be
Vítor Galvão <info@vitorgalvao.com>
2020-03-16 16:35:00 +0000

    Update codeexpander.rb

--- Casks/codeexpander.rb
+++ Casks/codeexpander.rb
@@ -2,8 +2,9 @@ cask 'codeexpander' do
   version '3.3.3'
   sha256 'b9175f79547f212580712853e9320c159a4eef9fa61fda42a2208de0a3c11a5c'
 
-  url 'https://github.com/oncework/codeexpander/releases/download/3.3.x/CodeExpander-3.3.3.dmg'
-  appcast 'https://github.com/oncework/codeexpander/releases.atom'
+  url 'https://github.com/oncework/codeexpander/releases/download/#{version.major_minor}.x/CodeExpander-#{version}.dmg'
+  appcast 'https://github.com/oncework/codeexpander/releases.atom',
+          configuration: version.major_minor
   name 'CodeExpander'
   homepage 'https://github.com/oncework/codeexpander'
 
