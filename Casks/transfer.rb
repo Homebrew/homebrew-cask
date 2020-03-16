beb737bb9b202fa96835bbc997f0ff294ff43815
Aleh Suprunovich <br@ahlamon.org>
2020-03-16 19:24:47 +0300

    Add Elektron Transfer 1.2.2.9
    Elektron Transfer is a tool to transfer samples, presets and firmware
    updates to Elektron Music Machines syntesizers, samplers etc.

---
+++ Casks/transfer.rb
@@ -0,0 +1,10 @@
+cask 'transfer' do
+  version '1.2.2.9'
+  sha256 'e8940345dc4212f6d77cac5b61c1dd0147ad3e9d1d7aeb21e6c344ee0b8de3e5'
+
+  url "https://www.elektron.se/wp-content/uploads/2018/03/Elektron_Transfer_#{version}_macOS.zip"
+  name 'Elektron Transfer'
+  homepage 'https://www.elektron.se/support/?connection=transfer#resources'
+
+  app 'Transfer.app'
+end
