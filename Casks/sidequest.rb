e2a0b51585fe7a72d6d92569e511aefc7ee1c3d5
Randall <ran.dall@icloud.com>
2020-03-16 13:51:57 -0700

    Update sidequest from 0.8.7 to 0.10.1

--- Casks/sidequest.rb
+++ Casks/sidequest.rb
@@ -1,6 +1,6 @@
 cask 'sidequest' do
-  version '0.8.7'
-  sha256 '3248d8ca1ad80f3f310df723fe97bdd052b708616c66ccee347da4a98200d646'
+  version '0.10.1'
+  sha256 'dcdbfea5c41aca12218c53bf1c0b1f8c50d0d45966d7feeb29cbe2f2a0557397'
 
   # github.com/the-expanse/SideQuest was verified as official when first introduced to the cask
   url "https://github.com/the-expanse/SideQuest/releases/download/v#{version}/SideQuest-#{version}.dmg"
