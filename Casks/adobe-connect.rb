d3d7c6dd5b959681df01d27a46183470622733c9
Martino Mensio <martinomensio@outlook.it>
2020-03-16 16:07:07 +0000

    Update adobe-connect.rb

--- Casks/adobe-connect.rb
+++ Casks/adobe-connect.rb
@@ -1,6 +1,6 @@
 cask 'adobe-connect' do
   version '10.2,2019.9.2'
-    sha256 'a741ddf5a863bed4a3d4730427844ec93215a472371af4fa8de9c22ac653b224'
+  sha256 'a741ddf5a863bed4a3d4730427844ec93215a472371af4fa8de9c22ac653b224'
 
   url "https://download.adobe.com/pub/connect/updaters/meeting/#{version.before_comma.dots_to_underscores}/AdobeConnect_#{version.after_comma}.dmg"
   name 'Adobe Connect'
