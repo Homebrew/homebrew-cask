f3caf4d77a3aa213a95ca6e5440bce9b97d60a40
tsukker <tsukker@users.noreply.github.com>
2020-03-17 03:07:00 +0900

    Add Doly.app v2.14.0

---
+++ Casks/doly.rb
@@ -0,0 +1,11 @@
+cask 'doly' do
+  version '2.14.0'
+  sha256 'e33e695dbece9e07851d808e90d44429828d1c2b0832364cb470983b849e6d1d'
+
+  url 'http://doly.jp/download/doly-mac_2.14.0.dmg'
+  name 'Doly Ebook Reader'
+  name 'Doly'
+  homepage 'http://doly.jp/'
+
+  app 'Doly.app'
+end
