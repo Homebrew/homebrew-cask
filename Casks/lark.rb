c328c2a6ec3304ac7bf166768e53a7ed909375ba
suschizu <brigitte@corecode.io>
2020-03-17 17:48:41 +0100

    Update lark from 3.19.4 to 3.20.4 (#78801)

--- Casks/lark.rb
+++ Casks/lark.rb
@@ -1,6 +1,6 @@
 cask 'lark' do
-  version '3.19.4'
-  sha256 '1b2f24a993a2f19bde081faad2df6c1c5455eb130fa8bbb9734a21983987a2cb'
+  version '3.20.4'
+  sha256 '7b7641a1e45568eefaf59723b656768acf496a147bee103cbb30f66b575095fd'
 
   # sf3-ttcdn-tos.pstatp.com was verified as official when first introduced to the cask
   url "https://sf3-ttcdn-tos.pstatp.com/obj/ee-appcenter/Lark-#{version}.dmg"
