db7a6a83d286ef0b800c1c192f2950d68365fc91
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2020-03-16 21:56:07 -0400

    Add Glyphfinder 1.0.0 cask

---
+++ Casks/glyphfinder.rb
@@ -0,0 +1,10 @@
+cask 'glyphfinder' do
+  version '1.0.0'
+  sha256 'acd3bf6465629fe742af651e05ef79fc28720b4d6851ca66a52e5c3cb6a2e6db'
+
+  url 'https://ueber.fra1.cdn.digitaloceanspaces.com/glyphfinder/mac/Glyphfinder-1.0.0-mac.dmg'
+  name 'Glyphfinder'
+  homepage 'https://www.glyphfinder.com'
+
+  app "Glyphfinder.app"
+end
