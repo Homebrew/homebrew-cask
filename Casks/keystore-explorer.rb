a03ec828ea23879e43079df1ccb5d793fca386f5
Vítor Galvão <info@vitorgalvao.com>
2020-03-17 00:01:33 +0000

    Update keystore-explorer.rb

--- Casks/keystore-explorer.rb
+++ Casks/keystore-explorer.rb
@@ -1,6 +1,6 @@
 cask 'keystore-explorer' do
   version '5.4.3'
-  sha256 '98c4b78c4ac4c2026b7664af126acfdc49a69d5eb181a2f08248ce091edebde3'
+  sha256 '04bb9a03f56fb279fb9778bd440179b69cd6b2455da02ed1304c73e736afd26d'
 
   # github.com/kaikramer/keystore-explorer was verified as official when first introduced to the cask
   url "https://github.com/kaikramer/keystore-explorer/releases/download/v#{version}/kse-#{version.no_dots}.dmg"
