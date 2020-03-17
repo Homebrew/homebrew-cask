5994e0a28c58b536c2b0b34f1aacd46d90d72a83
Vítor Galvão <info@vitorgalvao.com>
2020-03-17 21:33:07 +0000

    Update juicebar.rb

--- Casks/juicebar.rb
+++ Casks/juicebar.rb
@@ -2,8 +2,8 @@ cask 'juicebar' do
   version '1.0.55'
   sha256 'c790ceb57eb007647278701bb7dfc1ad90e312c0ebc833089b0139da21ce7129'
 
-  url 'https://mango.get-juicebar.com/v1/bundles/macOS/latest'
-  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://mango.get-juicebar.com/v1/bundles/macOS/latest'
+  url "https://mango.get-juicebar.com/v#{version.major}/bundles/macOS/latest"
+  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://mango.get-juicebar.com/v#{version.major}/bundles/macOS/latest"
   name 'JuiceBar'
   homepage 'https://get-juicebar.com/'
 
