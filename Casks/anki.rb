b8ebf5dff5517da05b0889d3bf0cb87511527582
Randall <ran.dall@icloud.com>
2020-03-18 11:55:18 -0700

    Update anki from 2.1.21 to 2.1.22

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,6 +1,6 @@
 cask 'anki' do
-  version '2.1.21'
-  sha256 '1fe04dd0e7f34611a20b2602a63407f64131348950b95e43bbe07302c25aa932'
+  version '2.1.22'
+  sha256 '9cca7358c31025cb24bc20e4d7b91b05df8e4273e2882e89d92e82b6f5a5cde8'
 
   # github.com/ankitects/anki was verified as official when first introduced to the cask
   url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac.dmg"
