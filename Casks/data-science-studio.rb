5f04b0bf7c4072c95d2bfe7b623c9745676d19a3
Randall <ran.dall@icloud.com>
2020-03-18 12:29:33 -0700

    Update data-science-studio from 7.0.0 to 7.0.1

--- Casks/data-science-studio.rb
+++ Casks/data-science-studio.rb
@@ -1,6 +1,6 @@
 cask 'data-science-studio' do
-  version '7.0.0'
-  sha256 'a6d2b77205aaf5a3c572375bd7f6c7ae387107bf64f62d51a8e853e86d06cdb4'
+  version '7.0.1'
+  sha256 '108f530047a44e6f12207ee39207be4499e48e87de4e67de11cbd7cfaf2b4439'
 
   url "https://downloads.dataiku.com/public/studio/#{version}/Data%20Science%20Studio%20#{version}.dmg"
   appcast 'https://www.dataiku.com/dss/trynow/mac/'
