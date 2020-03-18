dfbf0dcd78bf2fb0afe35f21c5582ea9f90c28ae
Work <brigitte@corecode.io>
2020-03-18 22:18:03 +0100

    Update eclipse-javascript from 4.14.0,2019-12:R to 4.15.0,2020-03:R

--- Casks/eclipse-javascript.rb
+++ Casks/eclipse-javascript.rb
@@ -1,6 +1,6 @@
 cask 'eclipse-javascript' do
-  version '4.14.0,2019-12:R'
-  sha256 'e12d99702d59894efd18f7cbc9aeb955f55350d52195227a36f271975ee51ff2'
+  version '4.15.0,2020-03:R'
+  sha256 '25ce89059aa7f008ade1c308c94bb4ca8b047e581b2159b38633aa057d1b4192'
 
   url "https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/#{version.after_comma.before_colon}/#{version.after_colon}/eclipse-javascript-#{version.after_comma.before_colon}-#{version.after_colon}-macosx-cocoa-x86_64.dmg&r=1"
   name 'Eclipse IDE for JavaScript and Web Developers'
