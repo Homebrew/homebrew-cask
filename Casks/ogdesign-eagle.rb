dd970bb445e37a1d26dc23169d0e903d79c908af
Vítor Galvão <info@vitorgalvao.com>
2020-03-18 15:38:35 +0000

    Update ogdesign-eagle.rb

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -1,8 +1,8 @@
 cask 'ogdesign-eagle' do
-  version '1.11.0'
+  version '1.11,build3'
   sha256 'c504887564465c996e2a1ed5dfa5220dcae70bc601aa2c1606c8306cfa7751aa'
 
-  url "https://file.eagle.cool/releases/Eagle-#{version.before_comma}-build3.dmg"
+  url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-#{version.after_comma}.dmg"
   appcast 'https://eagle.cool/changelog'
   name 'Eagle'
   homepage 'https://eagle.cool/macOS'
