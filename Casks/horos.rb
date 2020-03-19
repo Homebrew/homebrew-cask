7819a772bc8ac9aedafadd1126536e66a7f8d086
Hamilton Turner <hamiltont@gmail.com>
2020-03-19 02:42:27 -0400

    Update Horos on catalina from RC1 to RC3
    RC3 specifically has catalina bugfixes

--- Casks/horos.rb
+++ Casks/horos.rb
@@ -6,8 +6,8 @@ cask 'horos' do
     version '2.0.2'
     sha256 '5cc1d6c71c8ae643b4df4fecee93dbe3cfacbcffef52001a76a7683a2725ac08'
   else
-    version '4.0.0'
-    sha256 'b0ea0ac8793ee1e343c815f5e2bfbeba01ea713181c2c66ad69b73fbed69a902'
+    version '4.0.0RC3'
+    sha256 '2baaabccc87f6f84af2af0c6072481d5561d9c918c929143785344fb174eb241'
   end
 
   url "https://horosproject.org/horos-content/Horos#{version}.dmg"
