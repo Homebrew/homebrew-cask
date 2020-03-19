cb5e4cb048d4e0522fd8159299d152d8de680e9b
Timo Arlt <t.arlt@stollvongati.com>
2020-03-19 09:56:25 +0100

    Update activitywatch from 0.8.4 to 0.9.0

--- Casks/activitywatch.rb
+++ Casks/activitywatch.rb
@@ -1,6 +1,6 @@
 cask 'activitywatch' do
-  version '0.8.4'
-  sha256 '1cb7601991f33ed2eaef2d33b91f2e54c1f0024bc57f61a4000877154516c11d'
+  version '0.9.0'
+  sha256 '4f55d3b41abfd29f91fc810e5512b538ab63f97b4807fc66bcf09ea25b7dcc24'
 
   # github.com/ActivityWatch/activitywatch was verified as official when first introduced to the cask
   url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-v#{version}-macos-x86_64.zip"
