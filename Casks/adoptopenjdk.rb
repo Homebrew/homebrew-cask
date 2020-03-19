5894b96fcbdca217ff7f0d0b8f7e1091ba65a1d6
George Adams <george.adams@microsoft.com>
2020-03-19 15:04:02 +0000

    update AdoptOpenJDK.rb to JDK 14+36 (#78922)

--- Casks/adoptopenjdk.rb
+++ Casks/adoptopenjdk.rb
@@ -1,6 +1,6 @@
 cask 'adoptopenjdk' do
-  version '13.0.2,8'
-  sha256 '0ddb24efdf5aab541898d19b7667b149a1a64a8bd039b708fc58ee0284fa7e07'
+  version '14,36'
+  sha256 'aabc3aebb0abf1ba64d9bd5796d0c7eb7239983f6e4c0f015b5b88be5616e4bd'
 
   # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk#{version.major}-binaries/releases/download/jdk-#{version.before_comma}%2B#{version.after_comma}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.before_comma}_#{version.after_comma}.tar.gz"
