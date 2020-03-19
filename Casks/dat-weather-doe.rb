2f030f2d8fab2dc263e6a4d7b7aee77a4acfe56d
Vítor Galvão <info@vitorgalvao.com>
2020-03-19 18:57:38 +0000

    Update dat-weather-doe.rb

--- Casks/dat-weather-doe.rb
+++ Casks/dat-weather-doe.rb
@@ -1,8 +1,8 @@
-cask 'dat-weather-doe' do
+cask 'datweatherdoe' do
   version '1.2.5'
   sha256 'cc3bd6aea7dda073b399fd32e4d0e77d7b453b3ef3fc39bac6cba491af9327d4'
 
-  url 'https://github.com/inderdhir/DatWeatherDoe/releases/download/1.2.5/DatWeatherDoe-1.2.5.dmg'
+  url "https://github.com/inderdhir/DatWeatherDoe/releases/download/#{version}/DatWeatherDoe-#{version}.dmg"
   appcast 'https://github.com/inderdhir/DatWeatherDoe/releases.atom'
   name 'DatWeatherDoe'
   homepage 'https://github.com/inderdhir/DatWeatherDoe'
