c6340cfe3b73f4b5f779088885e939b8f5c0eea8
Vítor Galvão <info@vitorgalvao.com>
2020-03-20 16:26:11 +0000

    Update remoteviewer.rb

--- Casks/remoteviewer.rb
+++ Casks/remoteviewer.rb
@@ -4,6 +4,7 @@ cask 'remoteviewer' do
 
   # people.freedesktop.org/~teuf/spice-gtk-osx was verified as official when first introduced to the cask
   url "https://people.freedesktop.org/~teuf/spice-gtk-osx/dmg/#{version.before_comma}/RemoteViewer-#{version.before_comma}-#{version.after_comma}.dmg"
+  appcast 'https://www.spice-space.org/osx-client.html'
   name 'Remote Viewer'
   homepage 'https://www.spice-space.org/osx-client.html'
 
