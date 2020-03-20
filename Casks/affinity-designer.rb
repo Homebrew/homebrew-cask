0b14d722cacba0dd4ad52b6842d09239c124bc6e
yesRabbit <chandler.lu1997@gmail.com>
2020-03-20 13:48:07 +0800

    Add affinity-designer

---
+++ Casks/affinity-designer.rb
@@ -0,0 +1,15 @@
+cask 'affinity-designer' do
+  version '1.8.2'
+  sha256 'bc9076159db2cb4f17e7239ec43157574a574c670b484ff07835a61732f4b23d'
+
+  # s3-eu-west-1.amazonaws.com was verified as official when first introduced to the cask
+  url "https://s3-eu-west-1.amazonaws.com/affinity-update/mac/retail/Affinity%20Designer-#{version}.app.zip"
+  appcast 'https://s3-eu-west-1.amazonaws.com/affinity-update/mac/retail/designer.xml'
+  name 'affinity-designer'
+  homepage 'https://affinity.serif.com/en-us/designer/'
+
+  auto_updates true
+  depends_on macos: '>= :high_sierra'
+
+  app 'Affinity Designer.app'
+end
