75805f220158e4350bd3b800a9d9c065147dc94d
Work <brigitte@corecode.io>
2020-03-20 18:16:11 +0100

    Update izip

--- Casks/izip.rb
+++ Casks/izip.rb
@@ -3,7 +3,7 @@ cask 'izip' do
   sha256 '7be4dbc10384072058e56164b3a58a5164127e556e4a343a3acc14fd6f8104ba'
 
   url 'https://www.izip.com/izip.dmg'
-  appcast 'https://www.izip.com/updates'
+  appcast 'https://www.izip.com/download'
   name 'iZip'
   homepage 'https://www.izip.com/'
 
