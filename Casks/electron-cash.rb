adbf1ecd56f4d588b8eb0cdc99f9c4cf2b25cdd2
suschizu <brigitte@corecode.io>
2020-03-20 13:34:00 +0100

    Update electron-cash from 4.0.12 to 4.0.13 (#78975)

--- Casks/electron-cash.rb
+++ Casks/electron-cash.rb
@@ -1,6 +1,6 @@
 cask 'electron-cash' do
-  version '4.0.12'
-  sha256 'bafc139fa31a81700abc4d19c86753690cc3b297a78a51bffa34c783cab25de8'
+  version '4.0.13'
+  sha256 '51774cbc85e676227ab5c6c43223f4ddf345e1ab78608a3cd5c24f1c1eb03db9'
 
   url "https://electroncash.org/downloads/#{version}/mac/Electron-Cash-#{version}-macosx.dmg"
   appcast 'https://github.com/Electron-Cash/Electron-Cash/releases.atom'
