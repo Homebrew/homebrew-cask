b6bfc1ef8ba36d80bdf4f7281ea345843e3017d0
bblunt <benjamin_blunt@yahoo.com>
2020-03-21 00:08:24 -0700

    Update chromium from 750417 to 752277

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -1,6 +1,6 @@
 cask 'chromium' do
-  version '750417'
-  sha256 '38145304c25f849920b36a09aa02c3db21ac99fecf283f8aa8b44b42e263f438'
+  version '752277'
+  sha256 'd3a734d6b56c7f176e78b4e22c184c124e91d59b9f1b85f7f4da3c6cd479f72a'
 
   # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac was verified as official when first introduced to the cask
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
