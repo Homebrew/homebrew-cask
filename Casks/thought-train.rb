56a7cc39965238ef54b146dd38cbce4dae61f94b
Vítor Galvão <info@vitorgalvao.com>
2020-03-21 02:22:12 +0000

    Update thought-train.rb

--- Casks/thought-train.rb
+++ Casks/thought-train.rb
@@ -3,6 +3,7 @@ cask 'thought-train' do
   sha256 'cc8ed67924f2a30c54922a525e3f983afd2745f89db45abdb90ae1c9a12a631c'
 
   url "https://thoughttrain.cc/downloads/thought-train-#{version}.pkg"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.thoughttrain.cc/download/2345/'
   name 'Thought Train'
   homepage 'https://www.thoughttrain.cc/'
 
