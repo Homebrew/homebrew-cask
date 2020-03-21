59cf018008b6d21b61061128dc498eb4a87a044b
Parth P Panchal <pqrth@users.noreply.github.com>
2020-03-21 18:57:13 +0000

    Switches activitywatch v0.9.0 from binary to app (#78920)
    Self signed macOS packaged app is released starting from v0.9.0
    see ActivityWatch/activitywatch#334

--- Casks/activitywatch.rb
+++ Casks/activitywatch.rb
@@ -1,14 +1,14 @@
 cask 'activitywatch' do
   version '0.9.0'
-  sha256 '4f55d3b41abfd29f91fc810e5512b538ab63f97b4807fc66bcf09ea25b7dcc24'
+  sha256 '982bc85665e0229c4bace4c5b23d3163792b350d9344010dc672e889d412948d'
 
   # github.com/ActivityWatch/activitywatch was verified as official when first introduced to the cask
-  url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-v#{version}-macos-x86_64.zip"
+  url "https://github.com/ActivityWatch/activitywatch/releases/download/v#{version}/activitywatch-v#{version}-macos-x86_64.dmg"
   appcast 'https://github.com/ActivityWatch/activitywatch/releases.atom'
   name 'ActivityWatch'
   homepage 'https://activitywatch.net/'
 
-  binary 'activitywatch/aw-qt'
+  app 'ActivityWatch.app'
 
   zap trash: '~/Library/Application Support/activitywatch'
 end
