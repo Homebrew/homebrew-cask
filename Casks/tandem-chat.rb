9a6c863be24fda04cc511145ac57a4fcefe4d4e3
Vítor Galvão <info@vitorgalvao.com>
2020-03-22 14:51:23 +0000

    Update tandem-chat.rb

--- Casks/tandem-chat.rb
+++ Casks/tandem-chat.rb
@@ -3,7 +3,8 @@ cask 'tandem-chat' do
   sha256 '02917cbdf5dec79c9bfb84696e606dc2b64667971ce5ff9758a4361fb1eb6055'
 
   # tandem-app.sfo2.cdn.digitaloceanspaces.com was verified as official when first introduced to the cask
-  url 'https://tandem-app.sfo2.cdn.digitaloceanspaces.com/Tandem-1.5.0-mac.zip'
+  url "https://tandem-app.sfo2.cdn.digitaloceanspaces.com/Tandem-#{version}-mac.zip"
+  appcast 'https://tandem-app.sfo2.digitaloceanspaces.com/latest-mac.yml'
   name 'Tandem'
   homepage 'https://tandem.chat/'
 
