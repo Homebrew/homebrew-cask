1897d2dead4f9e854fe6726b5c87cf1da8364d9b
Vítor Galvão <info@vitorgalvao.com>
2020-03-22 21:26:32 +0000

    Update kite.rb

--- Casks/kite.rb
+++ Casks/kite.rb
@@ -2,8 +2,8 @@ cask 'kite' do
   version '0.20200320.0'
   sha256 '4211dca35c9b2cd995e333fa14a957a8d621fbd4318041cc232878fee3b0bc23'
 
-  # azure-cdn-kite-downloads.azureedge.net was verified as official when first introduced to the cask
-  url "https://azure-cdn-kite-downloads.azureedge.net/mac/#{version}/Kite.dmg"
+  # draqv87tt43s0.cloudfront.net was verified as official when first introduced to the cask
+  url "https://draqv87tt43s0.cloudfront.net/mac/#{version}/Kite.dmg"
   appcast 'https://release.kite.com/appcast.xml'
   name 'Kite'
   homepage 'https://kite.com/'
