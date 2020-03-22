9d5668be05e3892dafb44bc37bc1ffe5f72dd259
CoreCode <core-code@users.noreply.github.com>
2020-03-23 00:29:05 +0100

    Update qgis.rb

--- Casks/qgis.rb
+++ Casks/qgis.rb
@@ -3,7 +3,8 @@ cask 'qgis' do
   sha256 '69cc98ac7017f62aa16b14c6e8b5743f5abc7d073643a428e45363300d15c110'
 
   url 'https://qgis.org/downloads/macos/qgis-macos-pr.dmg'
-  appcast 'https://www.qgis.org/'
+  appcast 'https://qgis.org/downloads/macos/qgis-macos-pr.sha256sum',
+          configuration: version.dots_to_underscores
   name 'QGIS'
   homepage 'https://www.qgis.org/'
 
