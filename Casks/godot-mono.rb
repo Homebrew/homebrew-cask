d6f8a35941d3abf37ee5e306237b52d6f974a16e
Reza Kialashaki <rkialashaki@gmail.com>
2020-03-21 18:13:48 -0700

    Updates godot-mono Cask to version 3.2.1

--- Casks/godot-mono.rb
+++ Casks/godot-mono.rb
@@ -1,6 +1,6 @@
 cask 'godot-mono' do
-  version '3.2'
-  sha256 'cedf1ee73d53ba130b2983e3081c759f61714f3508cb1a44dbe02f9715d291b2'
+  version '3.2.1'
+  sha256 'aaede8ad074da8cfe39f179af2eb7d2fe7776391a06d9236c65e426af87a8392'
 
   # downloads.tuxfamily.org/godotengine was verified as official when first introduced to the cask
   url "https://downloads.tuxfamily.org/godotengine/#{version}/mono/Godot_v#{version}-stable_mono_osx.64.zip"
