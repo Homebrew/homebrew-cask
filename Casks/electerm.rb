9db4c26dc57b087f63a6f2bd8e216949072561eb
Randall <ran.dall@icloud.com>
2020-03-23 13:19:32 -0700

    Update electerm from 1.3.1 to 1.3.4

--- Casks/electerm.rb
+++ Casks/electerm.rb
@@ -1,6 +1,6 @@
 cask 'electerm' do
-  version '1.3.1'
-  sha256 '5acb33bbb49534de18b77277aa57a3f1bae31127bf8ebbd3aaa838abad39ced9'
+  version '1.3.4'
+  sha256 'f1c7e0ff75741f83be52a5ed7e2ade1f861190b9c875dfddca38512ca81f09e9'
 
   url "https://github.com/electerm/electerm/releases/download/v#{version}/electerm-#{version}-mac.dmg"
   appcast 'https://github.com/electerm/electerm/releases.atom'
