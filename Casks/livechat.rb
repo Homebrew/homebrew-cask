6e3ff1d608ec22bb58513e7a0d67d5e8b0049c82
Randall <ran.dall@icloud.com>
2020-03-23 14:06:31 -0700

    Update livechat from 9.0.8 to 9.0.9

--- Casks/livechat.rb
+++ Casks/livechat.rb
@@ -1,6 +1,6 @@
 cask 'livechat' do
-  version '9.0.8'
-  sha256 'ad9f350f4313bc2054a1bbc3bf2fe2f18f73c7d4c0e6e74c5c168a620548c75a'
+  version '9.0.9'
+  sha256 'f94d07fe544324eba31e8c25e78fd04f8b37707ff51155ab9972fd7f5910eff2'
 
   url 'https://www.livechatinc.com/download/Mac/LiveChat.dmg'
   appcast 'https://dal-livechat-main-web.s3.amazonaws.com/download/Mac/latest-mac.yml'
