adab6f06cefa43d741c80da20157e33df4143cf0
li-kbotelho <37940059+li-kbotelho@users.noreply.github.com>
2020-03-22 21:47:42 -0700

    Updated screenflick.rb with new checksum (#79117)
    same version as previous 2.7.45

--- Casks/screenflick.rb
+++ Casks/screenflick.rb
@@ -1,6 +1,6 @@
 cask 'screenflick' do
   version '2.7.45'
-  sha256 '94162122ccf75d004240427368b4428e6babe5e10b423987dfcc433872ffcff0'
+  sha256 'da4e78c232804e48e64919a244dd5ddee70365e56bc2646ce78333a7aeff63ce'
 
   url "https://store.araelium.com/screenflick/downloads/versions/Screenflick#{version}.zip"
   appcast "https://arweb-assets.s3.amazonaws.com/downloads/screenflick/screenflick#{version.major}.xml"
