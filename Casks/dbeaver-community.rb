502f3b7eaf10172d286971f0e1fd3c88c5392613
Nick McMahon <npm.mcmahon@gmail.com>
2020-03-23 09:33:38 -0400

    Update dbeaver-community from 7.0.0 to 7.0.1 (#79123)
    Co-authored-by: Nicholas McMahon <nmcmahon@kayak.com>

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
-  version '7.0.0'
-  sha256 '99106db3d800955cefaee63faf884b394b53f85c3f7730151e3e9cdf0ce4c61b'
+  version '7.0.1'
+  sha256 '48dffd85e72523ada380116bcac77253ad9f0d2a57516b452ade2ad8d2dcf123'
 
   # github.com/dbeaver/dbeaver was verified as official when first introduced to the cask
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
