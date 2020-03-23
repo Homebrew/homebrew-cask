f09bd0729bad75b8740ac16df4ca2c1bded3a874
Brice Dutheil <brice.dutheil@gmail.com>
2020-03-23 13:43:53 +0100

    Adds JDK Mission Control 7.0

---
+++ Casks/jdk-mission-control.rb
@@ -0,0 +1,14 @@
+cask 'jdk-mission-control' do
+  version '7.0,14' # JMC 7 build 14
+  sha256 '81a7754c75be718cee852f988cc64d5cb1f2aa90a3a4bbe0f8fdc70d9c719ab3'
+
+  url "https://download.java.net/java/GA/jmc#{version.major}/#{version.after_comma}/jmc-#{version.major}_osx-x64_bin.tar.gz"
+  name 'JDK Mission Control'
+  homepage 'https://jdk.java.net/jmc/'
+
+  app "jmc-#{version.major}+#{version.after_comma}_osx-x64_bin/JDK Mission Control.app"
+
+  caveats do
+    depends_on_java '11'
+  end
+end
