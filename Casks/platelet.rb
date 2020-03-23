f2ef9775f831b4ace88a09e83cdb80a72e46a7e7
Randall <ran.dall@icloud.com>
2020-03-23 13:21:34 -0700

    Update platelet from 0.0.18 to 0.0.19

--- Casks/platelet.rb
+++ Casks/platelet.rb
@@ -1,6 +1,6 @@
 cask 'platelet' do
-  version '0.0.18'
-  sha256 '04050d1a7daf05b20377067253610376b9ba47a560ae2adaa19b619702617f5c'
+  version '0.0.19'
+  sha256 '5a524d75bd5995e182d2ff8a2ff4c45124c8f986c79b2f4d1af5da75c2ff4c28'
 
   url "https://github.com/amorist/platelet/releases/download/v#{version}/platelet-#{version}.dmg"
   appcast 'https://github.com/amorist/platelet/releases.atom'
