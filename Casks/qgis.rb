9b6278198bb6c9792212bd9edb64544d52c04f8c
CoreCode <core-code@users.noreply.github.com>
2020-03-23 17:22:11 +0100

    Update qgis from 3.12.0 to 3.12.1 (#79108)
    * Update qgis from 3.12.0 to 3.12.1
    
    * Update qgis.rb

--- Casks/qgis.rb
+++ Casks/qgis.rb
@@ -1,9 +1,10 @@
 cask 'qgis' do
-  version '3.12.0'
-  sha256 '9eba4fbf92aa7bd5fa9364a1eaf8325ab1db6ec0834acabe9539e830c41167ff'
+  version '3.12.1'
+  sha256 '69cc98ac7017f62aa16b14c6e8b5743f5abc7d073643a428e45363300d15c110'
 
   url 'https://qgis.org/downloads/macos/qgis-macos-pr.dmg'
-  appcast 'https://www.qgis.org/'
+  appcast 'https://qgis.org/downloads/macos/qgis-macos-pr.sha256sum',
+          configuration: version.dots_to_underscores
   name 'QGIS'
   homepage 'https://www.qgis.org/'
 
