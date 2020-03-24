d249faf2269df83e4f54f2c18f0b994de7f87e86
Yusuke Arakawa <mofneko@users.noreply.github.com>
2020-03-24 13:15:05 +0900

    Add DeepL.

---
+++ Casks/deep-l.rb
@@ -0,0 +1,10 @@
+cask 'deep-l' do
+  version '1.7.0'
+  sha256 '63949e44ef2e116aba8ab6d7ce4ccd2f2a7f895c344b1e114a90040e5588d385'
+
+  url 'https://www.deepl.com/macos/download/DeepL.dmg'
+  name 'DeepL for Mac'
+  homepage 'https://deepl.com/'
+
+  app 'DeepL.app'
+end
