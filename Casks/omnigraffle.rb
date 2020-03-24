4e506bc2b02515ccd43e8aaebe048a4cc780b1da
John Benninghoff <jbenninghoff@mac.com>
2020-03-24 10:04:56 -0500

    Update omnigraffle from 7.14 to 7.14.1 (#79207)

--- Casks/omnigraffle.rb
+++ Casks/omnigraffle.rb
@@ -8,8 +8,8 @@ cask 'omnigraffle' do
     sha256 '83ef24af2dbd7977b9922e992f17f23e102562f0589d28bc37d5579b4a4d4938'
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniGraffle-#{version}.dmg"
   else
-    version '7.14'
-    sha256 '11eaedde5945aa797a10a3b9f46a01db29d6af9b87e5a1f4bd71392e1c6ebe05'
+    version '7.14.1'
+    sha256 '0c8aa7f5a7b977461c134cc41c8f4afaf724ab5891e5a48b69912c7a7d1b84d8'
     url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniGraffle-#{version}.dmg"
   end
 
