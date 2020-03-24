86359f05a62c32dd50694b2b042aefa52f44fed1
Peter David Hamilton <pete@peterdavidhamilton.com>
2020-03-24 21:10:25 +0000

    Update GPlates 2.2.0
    Edit url, appcast and add binary

--- Casks/gplates.rb
+++ Casks/gplates.rb
@@ -2,12 +2,14 @@ cask 'gplates' do
   version '2.2.0'
   sha256 'a4658d7dd7e123c53d847c7b98e91d3f9cf222beb0dadaea6a4868eddcf5d85a'
 
-  # sourceforge.net/projects/gplates was verified as official when first introduced to the cask
-  url "https://sourceforge.net/projects/gplates/files/gplates/#{version.major_minor}/GPlates-#{version}-Darwin-x86_64.dmg/download"
+  # downloads.sourceforge.net/gplates was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/gplates/#{version.major_minor}/GPlates-#{version}-Darwin-x86_64.dmg"
+  appcast "https://sourceforge.net/projects/gplates/rss?path=/#{version.major_minor}"
   name 'GPlates'
   homepage 'https://www.gplates.org/'
 
   depends_on macos: '>= :mavericks'
 
   app "GPlates-#{version}/GPlates.app"
+  binary "#{appdir}/GPlates.app/Contents/MacOS/gplates"
 end
