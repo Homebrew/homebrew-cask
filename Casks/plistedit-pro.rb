618cb6afaa2690579959617727cacfb7df760185
Dario Vladovic <d.vladimyr@gmail.com>
2020-03-24 04:12:22 +0100

    Update plistedit-pro

--- Casks/plistedit-pro.rb
+++ Casks/plistedit-pro.rb
@@ -13,7 +13,10 @@ cask 'plistedit-pro' do
   binary "#{appdir}/PlistEdit Pro.app/Contents/MacOS/pledit"
 
   zap trash: [
-               '~/Library/Preferences/com.fatcatsoftware.pledpro.plist',
                '~/Library/Application Support/PlistEdit Pro',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.fatcatsoftware.pledpro.sfl2',
+               '~/Library/Application Support/com.fatcatsoftware.pledpro',
+               '~/Library/Caches/com.fatcatsoftware.pledpro',
+               '~/Library/Preferences/com.fatcatsoftware.pledpro.plist',
              ]
 end
