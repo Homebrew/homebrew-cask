ef20faa2af57cdf929ac2dc4a72325332cec62ee
Randall <ran.dall@icloud.com>
2020-03-24 17:21:34 -0700

    Update juicebar from 1.0.55 to 1.0.57

--- Casks/juicebar.rb
+++ Casks/juicebar.rb
@@ -1,6 +1,6 @@
 cask 'juicebar' do
-  version '1.0.55'
-  sha256 'c790ceb57eb007647278701bb7dfc1ad90e312c0ebc833089b0139da21ce7129'
+  version '1.0.57'
+  sha256 'acbaa1eb19e1658de4d0024b355eb47d889eab87fcd8a28ea727578a94e71568'
 
   url "https://mango.get-juicebar.com/v#{version.major}/bundles/macOS/latest"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_filename.cgi?url=https://mango.get-juicebar.com/v#{version.major}/bundles/macOS/latest"
