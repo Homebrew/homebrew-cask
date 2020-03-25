c01168c83b8475e41c20f15b19908a33b3d788f5
SgtLegend <sgtlegend@teamxlink.dev>
2020-03-25 15:15:03 +1100

    Add XLink Kai.app v7.4.35

---
+++ Casks/xlink-kai.rb
@@ -0,0 +1,11 @@
+cask 'xlink-kai' do
+  version '7.4.35'
+  sha256 'ebc988dd6491439b897f8f6e51f870a919f0d416a7d6096a62441c8e7bf05d05'
+
+  url "https://github.com/Team-XLink/releases/releases/download/v#{version}/XLink.Kai.app.zip"
+  appcast 'https://github.com/Team-XLink/releases/releases.atom'
+  name 'XLink Kai'
+  homepage 'https://www.teamxlink.co.uk/'
+
+  app 'XLink Kai.app'
+end
