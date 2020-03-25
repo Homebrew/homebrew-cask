112a3ff16f063fb6d3c71aa4f6c665af9d223c15
suschizu <brigitte@corecode.io>
2020-03-25 17:54:10 +0100

    Update xamarin-profiler.rb

--- Casks/xamarin-profiler.rb
+++ Casks/xamarin-profiler.rb
@@ -3,7 +3,8 @@ cask 'xamarin-profiler' do
   sha256 'd190fb22d921945a613b2b8b013cfe4cd775401e98d3350373852f3702e28ce2'
 
   url "https://dl.xamarin.com/profiler/profiler-mac-#{version}.pkg"
-  appcast "https://developer.xamarin.com/releases/profiler/#{version.major_minor}/#{version.major_minor_patch}"
+  appcast "https://docs.microsoft.com/en-gb/xamarin/tools/profiler/release-notes/#{version.major_minor}/#{version.major_minor_patch}",
+          configuration: version.major_minor_patch
   name 'Xamarin Profiler'
   homepage 'https://developer.xamarin.com/releases/profiler/'
 
