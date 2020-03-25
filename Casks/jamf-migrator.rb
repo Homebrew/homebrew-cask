bea7c9363da3840565e075b33b99301a18438fe4
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-24 17:19:44 -0700

    Update jamf-migrator from 5.2.0 to 5.2.1 (#79226)

--- Casks/jamf-migrator.rb
+++ Casks/jamf-migrator.rb
@@ -1,6 +1,6 @@
 cask 'jamf-migrator' do
-  version '5.2.0'
-  sha256 '548bc64e1b3d04594289df6ad8db08b3d19d0a80e0ea236fbb1e213c180bb5b0'
+  version '5.2.1'
+  sha256 '868310c4540a91cab3864749243d34a91c54d38be0bc532046d8a9d62686e491'
 
   url 'https://github.com/jamf/JamfMigrator/releases/download/current/jamf-migrator.zip'
   appcast 'https://github.com/jamf/JamfMigrator/releases.atom'
