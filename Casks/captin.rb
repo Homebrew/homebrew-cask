50a60f62d16638f36b74ee92f3cc0454eed0d6e8
Randall <ran.dall@icloud.com>
2020-03-24 17:26:32 -0700

    Update captin from 1.0.23,117:1584800886 to 1.0.24,118:1585049885

--- Casks/captin.rb
+++ Casks/captin.rb
@@ -1,6 +1,6 @@
 cask 'captin' do
-  version '1.0.23,117:1584800886'
-  sha256 'a21c1746f40034a9bcc9db1cc86b81068dea1d364521d16877e659e818dd8d1e'
+  version '1.0.24,118:1585049885'
+  sha256 'cb1290938ec121849511a2a497c91963f092ee2b928195e8b882ae00ee988d1d'
 
   # dl.devmate.com/com.100hps.captin was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.100hps.captin/#{version.after_comma.before_colon}/#{version.after_colon}/Captin-#{version.after_comma.before_colon}.dmg"
