a938443ff10bf1364680fd7fa9fd31c9d0bef827
Maxim Belkin <maxim.belkin@gmail.com>
2020-03-25 21:08:33 -0700

    keepassxc 2.5.3: emergency fix for macOS 10.15.4 (#79282)

--- Casks/keepassxc.rb
+++ Casks/keepassxc.rb
@@ -1,9 +1,9 @@
 cask 'keepassxc' do
   version '2.5.3'
-  sha256 'fb9af9c2eab788e710f93165f063ed26281140266181d3904b4a6b2d62c5c9d8'
+  sha256 'e278f02466e3fb294c5c676d7b0ba3c60302d0b08273b8088ff39ca2e97e79af'
 
   # github.com/keepassxreboot/keepassxc was verified as official when first introduced to the cask
-  url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg"
+  url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-2.dmg"
   appcast 'https://github.com/keepassxreboot/keepassxc/releases.atom'
   name 'KeePassXC'
   homepage 'https://keepassxc.org/'
