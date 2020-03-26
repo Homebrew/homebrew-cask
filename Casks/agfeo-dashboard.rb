6a69d719b266b5a61fd7290fd262546b7c0534ee
suschizu <brigitte@corecode.io>
2020-03-26 05:05:43 +0100

    Update agfeo-dashboard (#79270)

--- Casks/agfeo-dashboard.rb
+++ Casks/agfeo-dashboard.rb
@@ -3,7 +3,7 @@ cask 'agfeo-dashboard' do
   sha256 'c00cef11a7f916b6877a3e5877dcf847d6f338beb1ab3d4d30ea0465f0464b40'
 
   url "https://www2.agfeo.de/agfeo_web/dokulib.nsf/Anlage_w/#{version.after_comma}/$FILE/AGFEO-Dashboard_x64_agfeo_#{version.before_comma}.dmg"
-  appcast 'https://www.agfeo.de/agfeo_web/hp3.nsf/softwareRepN_d'
+  appcast 'https://info.agfeo.de/agfeo_web/DokuLib.nsf/lu_04/5077_2'
   name 'AGFEO Dashboard'
   homepage 'https://www.agfeo.de/'
 
