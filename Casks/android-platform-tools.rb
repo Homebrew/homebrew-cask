623a9290a028a00fe677bf7e386aaa97e5d9376c
Arpit Namdeo <arpitnamdeo@ca.ibm.com>
2020-03-26 18:38:44 -0400

    Update android-platform-tools

--- Casks/android-platform-tools.rb
+++ Casks/android-platform-tools.rb
@@ -1,6 +1,6 @@
 cask 'android-platform-tools' do
   version '29.0.6'
-  sha256 '17cb7642a5b34beda7fc0ef8eeb7dd8664f980c14736fd129c7b2dc0f2407c98'
+  sha256 '7555e8e24958cae4cfd197135950359b9fe8373d4862a03677f089d215119a3a'
 
   # google.com/android/repository/platform-tools was verified as official when first introduced to the cask
   url "https://dl.google.com/android/repository/platform-tools_r#{version}-darwin.zip"
