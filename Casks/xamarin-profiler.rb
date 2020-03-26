d3784609d7fcae44227ba2db827c81c2ed406a8a
Vítor Galvão <info@vitorgalvao.com>
2020-03-26 04:07:15 +0000

    Update xamarin-profiler.rb

--- Casks/xamarin-profiler.rb
+++ Casks/xamarin-profiler.rb
@@ -3,8 +3,7 @@ cask 'xamarin-profiler' do
   sha256 'd190fb22d921945a613b2b8b013cfe4cd775401e98d3350373852f3702e28ce2'
 
   url "https://dl.xamarin.com/profiler/profiler-mac-#{version.before_comma}-#{version.after_comma}.pkg"
-  appcast "https://docs.microsoft.com/en-gb/xamarin/tools/profiler/release-notes/#{version.major_minor}/#{version.before_comma}",
-          configuration: version.split('-').first
+  appcast "https://docs.microsoft.com/en-gb/xamarin/tools/profiler/release-notes/#{version.major_minor}/#{version.before_comma}"
   name 'Xamarin Profiler'
   homepage 'https://developer.xamarin.com/releases/profiler/'
 
