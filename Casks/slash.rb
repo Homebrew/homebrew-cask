4ae18324f93d13155e52e00f82948e413d388d2d
Matthew Gleich <matthewgleich@gmail.com>
2020-03-26 01:25:57 -0400

    Fix URL verification

--- Casks/slash.rb
+++ Casks/slash.rb
@@ -2,7 +2,7 @@ cask 'slash' do
   version '0.8.22'
   sha256 '8221f73a3937dbb46a0b542f4e05b0d1fc0821e0c0bcebad7a28460db3f0e8f0'
 
-  # Releases made from https://github.com/jetlej/slash-releases/releases
+  # github.com/jetlej/slash-releases was verified as official when first introduced to the cask
   url "https://github.com/jetlej/slash-releases/releases/download/v#{version}/Slash-#{version}.dmg"
   appcast 'https://github.com/jetlej/slash-releases/releases.atom'
   name 'Slash'
@@ -10,4 +10,3 @@ cask 'slash' do
 
   app 'Slash.app'
 end
-
