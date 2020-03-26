b568ecf0a35285cec2d8231934d746e6b40f8d94
suschizu <brigitte@corecode.io>
2020-03-26 05:01:30 +0100

    Update comparemerge from 2.11(2)y,104 to 2.11z,105 (#79277)
    * Update comparemerge from 2.11(2)y,104 to 2.11z,105
    
    * Update comparemerge.rb

--- Casks/comparemerge.rb
+++ Casks/comparemerge.rb
@@ -1,11 +1,11 @@
 cask 'comparemerge' do
-  version '2.11(2)y,104'
-  sha256 '4f386114f41a4faf2056db5744489aa011b65bc55949194891b2ee7690e7e098'
+  version '2.11z,105'
+  sha256 '1354834bc0bf02e947289097bd5fe07e8f7f62548890568454500cb6c72bb478'
 
   url "https://downloads.sourceforge.net/comparemergenosandbox/CompareMerge%20#{version.before_comma}.zip"
   appcast 'https://sourceforge.net/projects/comparemergenosandbox/rss'
   name 'CompareMerge'
   homepage 'https://sourceforge.net/projects/comparemergenosandbox/'
 
-  app "CompareMerge #{version.before_comma}.app"
+  app "CompareMerge #{version.before_comma}/CompareMerge #{version.before_comma}.app"
 end
