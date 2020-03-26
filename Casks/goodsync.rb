ff17259481370e9f9b661b42b31a0d046062a9b9
Randall <ran.dall@icloud.com>
2020-03-26 12:05:29 -0700

    Update goodsync from 10.11.2.2 to 10.11.4.4

--- Casks/goodsync.rb
+++ Casks/goodsync.rb
@@ -1,6 +1,6 @@
 cask 'goodsync' do
-  version '10.11.2.2'
-  sha256 '111cbd3f4058d9b5efef6424714cc27afcee797381e88634cbcf40a526e73d80'
+  version '10.11.4.4'
+  sha256 '2269bd11d5fda9cd0d0fb457c6f31c49698899fe3e343f2e0c64699d57cf9fdb'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   appcast 'https://www.goodsync.com/download?os=macos',
