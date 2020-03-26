21e0cf7531b0fc65ae793e3ab1999075e069f0e5
Work <brigitte@corecode.io>
2020-03-26 13:38:56 +0100

    Update sia-ui from 1.4.4 to 1.4.6

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -1,6 +1,6 @@
 cask 'sia-ui' do
-  version '1.4.4'
-  sha256 'd525497ea96e16c032b567cbbd5ed61cd14e20d54942f0759364901f8afb4bd1'
+  version '1.4.6'
+  sha256 '4eda8ae4aa628cbcc87c7ed9bdebbc5a3e3ab604a05a60eb78d608ee88d5582f'
 
   url "https://sia.tech/releases/Sia-UI-v#{version}.dmg"
   appcast 'https://sia.tech/1.f737981720cd8bbc13de.js'
