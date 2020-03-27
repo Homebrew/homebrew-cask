5bf577387b759280057208d0ea9297e39692b6d0
Zhenfu Shi <szf1234@me.com>
2020-03-27 00:12:46 -0400

    Sort entries alphabetically

--- Casks/appcleaner.rb
+++ Casks/appcleaner.rb
@@ -27,7 +27,7 @@ cask 'appcleaner' do
   zap trash: [
                '~/Library/Caches/net.freemacsoft.AppCleaner',
                '~/Library/Preferences/net.freemacsoft.AppCleaner.plist',
-               '~/Library/Saved Application State/net.freemacsoft.AppCleaner.savedState',
                '~/Library/Preferences/net.freemacsoft.AppCleaner-SmartDelete.plist',
+               '~/Library/Saved Application State/net.freemacsoft.AppCleaner.savedState',
              ]
 end
