e5426069cc25d4355f9165d33c59b025f9d05060
Julian Mayer <julian@corecode.io>
2020-03-27 23:45:27 +0100

    Update battlescribe from 2.03.13 to 2.03.14

--- Casks/battlescribe.rb
+++ Casks/battlescribe.rb
@@ -1,6 +1,6 @@
 cask 'battlescribe' do
-  version '2.03.13'
-  sha256 '7717442e007d7a13117d4b8796028a5354f9c36ec6e14df554cb5a907642363f'
+  version '2.03.14'
+  sha256 '91c4bfaf47137608bf71c14aecc2e5e5a7df61797de5d013c7fdaa5b54980650'
 
   url "https://battlescribe.net/files/BattleScribe_#{version}_Installer.dmg"
   appcast 'https://battlescribe.net/?tab=downloads'
