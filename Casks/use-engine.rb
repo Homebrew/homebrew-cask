d43ef812f2ce58dbab897e4f5f959edd2b725f85
Julien Lehuraux <julien.lehuraux@use-together.com>
2020-03-27 10:49:19 +0100

    Update use-engine from 2.2.3 to 2.2.4 (#79396)

--- Casks/use-engine.rb
+++ Casks/use-engine.rb
@@ -1,6 +1,6 @@
 cask 'use-engine' do
-  version '2.2.3'
-  sha256 '679d1c5a1522f2975f29d2e268d1579057401b625b9d3624657205e7ecd1daf7'
+  version '2.2.4'
+  sha256 '39e023ec3c82a79d8d0c1c41eb8a33c51214aba1d427d0b4f12c8f87ab4d5493'
 
   url "https://repository.use-together.com/stable/use-engine/macos/#{version.major}.x/#{version}/use-engine.dmg"
   name 'USE Engine'
