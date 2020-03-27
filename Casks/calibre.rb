358c69a6037a2a0d5b1b917c52ef83072ca34aa5
AppleTechy <applegeek500@gmail.com>
2020-03-27 00:25:00 -0700

    Update Calibre.rb
    Calibre 4.13.0 was released [27 Mar, 2020]. Updating the version number referenced so it downloads the latest version.

--- Casks/calibre.rb
+++ Casks/calibre.rb
@@ -4,7 +4,7 @@ cask 'calibre' do
     sha256 '68829cd902b8e0b2b7d5cf7be132df37bcc274a1e5720b4605d2dd95f3a29168'
     url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
   else
-    version '4.12.0'
+    version '4.13.0'
     sha256 'd4e941735408f72af4870eb95a1c9ae03fccab1e3e7776bd2969b4a3646e7394'
     url "https://download.calibre-ebook.com/#{version}/calibre-#{version}.dmg"
     appcast 'https://github.com/kovidgoyal/calibre/releases.atom'
