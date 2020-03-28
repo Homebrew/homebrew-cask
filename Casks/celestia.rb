8024cacdad090f5adb2938d049aeaa0eb0403457
CoreCode <core-code@users.noreply.github.com>
2020-03-28 11:32:50 +0100

    Update celestia.rb

--- Casks/celestia.rb
+++ Casks/celestia.rb
@@ -1,6 +1,6 @@
 cask 'celestia' do
   version '1.6.1'
-  sha256 'db09842a10b377038bedef87fda8d159549234b2e072fa22e096ade84aa3f52a'
+  sha256 'downloadnotworking'
 
   url "https://celestia.space/files/celestia/#{version}/celestia-osx-#{version}.dmg"
   appcast 'https://github.com/CelestiaProject/Celestia/releases.atom'
