5f9cf211ece5924315d77030c6dc7df3a6e4f965
Niels van Zwieten <niels@nvz.me>
2020-03-28 11:44:19 +0100

    Add Lens.app v3.0.1
    Lens is the only IDE you’ll ever need to take control of your
    Kubernetes clusters. It is a standalone application for MacOS, Windows
    and Linux operating systems. It is open source and free.

---
+++ Casks/lens.rb
@@ -0,0 +1,18 @@
+cask 'lens' do
+  version '3.0.1'
+  sha256 '484f22e626fc99d0f95b6664ebae8cd36b1360dd17c9aa32ccf155cc5b078df0'
+
+  # github.com/lensapp/lens was verified as official when first introduced to the cask
+  url "https://github.com/lensapp/lens/releases/download/v#{version}/Lens-#{version}.dmg"
+  appcast 'https://github.com/lensapp/lens/releases.atom'
+  name 'Lens'
+  homepage 'https://k8slens.dev/'
+
+  app 'Lens.app'
+
+  zap trash: [
+               '~/Library/Preferences/com.electron.kontena-lens.plist',
+               '~/Library/Application Support/Lens',
+               '~/Library/Saved Application State/com.electron.kontena-lens.savedState',
+             ]
+end
