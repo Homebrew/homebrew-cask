01c3cb33acf20f6013e41d4ece1c811234ab793f
Vítor Galvão <info@vitorgalvao.com>
2020-03-28 20:39:59 +0000

    Update dremel-slicer.rb

--- Casks/dremel-slicer.rb
+++ Casks/dremel-slicer.rb
@@ -3,9 +3,10 @@ cask 'dremel-slicer' do
   sha256 'd2d78f1f7d45af36e446ff0ea7f0b5f99b442aca72d354522945d38a90ab5d51'
 
   # dremel3d.blob.core.windows.net was verified as official when first introduced to the cask
-  url 'https://dremel3d.blob.core.windows.net/dremel3dslicer/1.2.3/Dremel3DSlicer-1.2.3-mac.zip'
+  url "https://dremel3d.blob.core.windows.net/dremel3dslicer/#{version}/Dremel3DSlicer-#{version}-mac.zip"
+  appcast 'https://digilab.dremel.com/3D-software'
   name 'Dremel DigiLab 3D Slicer'
   homepage 'https://digilab.dremel.com/3D-software'
 
-  app 'Dremel3DSlicer-1.2.3-mac/Dremel DigiLab 3D Slicer.app'
+  app "Dremel3DSlicer-#{version}-mac/Dremel DigiLab 3D Slicer.app"
 end
