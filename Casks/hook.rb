e3a3a04011ce23105e4444028e2d06752b05ee54
CoreCode <core-code@users.noreply.github.com>
2020-03-28 21:45:17 +0100

    Update hook (#79423)

--- Casks/hook.rb
+++ Casks/hook.rb
@@ -3,7 +3,7 @@ cask 'hook' do
   sha256 '60b25c4c5c160ce1360c55700d5aaf6357fc76a931aeae86a8e2e2c0cfdd9564'
 
   # appcenter-filemanagement-distrib5ede6f06e.azureedge.net was verified as official when first introduced to the cask
-  url 'https://appcenter-filemanagement-distrib5ede6f06e.azureedge.net/9833b7ef-39a1-42cc-ab0e-6def214e04ad/Hook_productivity_app_1.4.dmg?sv=2018-03-28&sr=c&sig=k%2F17I2dBuZJZNFT7FTQHKGodiQ%2Fz2nBJade6WaoGook%3D&se=2020-03-14T10%3A39%3A59Z&sp=r'
+  url 'https://appcenter-filemanagement-distrib5ede6f06e.azureedge.net/9833b7ef-39a1-42cc-ab0e-6def214e04ad/Hook_productivity_app_1.4.dmg?sv=2018-03-28&sr=c&sig=dJjtgIzjzjrRMsS7EaZgMLpS23wlQ2BYTpjnlXBpZOw%3D&se=2020-03-28T22%3A36%3A35Z&sp=r'
   appcast 'https://api.appcenter.ms/v0.1/public/sparkle/apps/a77a1a87-7d69-435d-90ea-7365b2f7bddb'
   name 'Hook'
   homepage 'https://hookproductivity.com/'
