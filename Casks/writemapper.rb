be828c896679ba270243a2afea990e2172696167
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-27 19:52:21 -0700

    Update writemapper from 2.9.1 to 2.10.0 (#79461)

--- Casks/writemapper.rb
+++ Casks/writemapper.rb
@@ -1,6 +1,6 @@
 cask 'writemapper' do
-  version '2.9.1'
-  sha256 '04e46adf4245d61361980cfa6921c505d0c80d6525f474fc10391bb0b0a9c44e'
+  version '2.10.0'
+  sha256 'faa919450d0f92d1f6a93d94f51e3ff45a1c92b70c2a408773f299d6e15624a1'
 
   url "https://writemapper.com/static/app/mac/WriteMapper-#{version}.dmg"
   appcast 'https://writemapper.com/static/app/mac/latest-mac.yml'
