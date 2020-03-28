9fc481a4050756e03cdfa6bcf7df80a3d1aeead5
suschizu <brigitte@corecode.io>
2020-03-28 19:21:33 +0100

    Revert "Update folx from 5.15.13938 to 5.16.13940 (#79155)"
    This reverts commit 517541822c2d4b2b1c6a6e71aa82aa1eb869c481.

--- Casks/folx.rb
+++ Casks/folx.rb
@@ -1,6 +1,6 @@
 cask 'folx' do
-  version '5.16.13940'
-  sha256 '746235de6bf236448fd59a26b0195a40efb5ad7933826d2c611c79eb30c89d6d'
+  version '5.15.13938'
+  sha256 'ceb0d3784e57f82ebf4110f2021be1fb9feec45555350cf622f2c27f3cce6650'
 
   url 'https://cdn.eltima.com/download/downloader_mac.dmg'
   appcast 'https://cdn.eltima.com/download/folx-updater/folx.xml',
