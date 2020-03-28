8a0a862258cc396fc413d5d2f280552700f148d2
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-27 19:51:41 -0700

    Update mat from 1.9.2.20200115 to 1.10.0.20200225 (#79462)

--- Casks/mat.rb
+++ Casks/mat.rb
@@ -1,6 +1,6 @@
 cask 'mat' do
-  version '1.9.2.20200115'
-  sha256 'ab379da580e07659e713996ed0169375246c88b36245a35c9f375a63c3114655'
+  version '1.10.0.20200225'
+  sha256 'dc711052e9ede8b6e9af0443035cee590bb5d2609705440f32d0ef0d6f66cbf3'
 
   url "https://www.eclipse.org/downloads/download.php?r=1&file=/mat/#{version.major_minor_patch}/rcp/MemoryAnalyzer-#{version}-macosx.cocoa.x86_64.zip"
   appcast 'https://www.eclipse.org/mat/downloads.php'
