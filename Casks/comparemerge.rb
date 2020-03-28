414f36fa7b4b64e6c4a76dd111c7399d46960427
suschizu <brigitte@corecode.io>
2020-03-28 21:00:00 +0100

    Update comparemerge (#79412)
    * Update comparemerge
    
    * Update comparemerge.rb

--- Casks/comparemerge.rb
+++ Casks/comparemerge.rb
@@ -1,11 +1,11 @@
 cask 'comparemerge' do
   version '2.11z,105'
-  sha256 '1354834bc0bf02e947289097bd5fe07e8f7f62548890568454500cb6c72bb478'
+  sha256 '5c9cfbed48fee3e913713b2b1e14c5dc2719018803f04d1f67bd3fc470b0b886'
 
-  url "https://downloads.sourceforge.net/comparemergenosandbox/CompareMerge%20#{version.before_comma}.zip"
+  url "https://downloads.sourceforge.net/comparemergenosandbox/CompareMerge%20#{version.before_comma}%28#{version.major}%29.zip"
   appcast 'https://sourceforge.net/projects/comparemergenosandbox/rss'
   name 'CompareMerge'
   homepage 'https://sourceforge.net/projects/comparemergenosandbox/'
 
-  app "CompareMerge #{version.before_comma}/CompareMerge #{version.before_comma}.app"
+  app "CompareMerge#{version.before_comma}/CompareMerge#{version.before_comma}.app"
 end
