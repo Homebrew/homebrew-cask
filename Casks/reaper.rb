1e74662943fb2aaee51dcf4692f845e19a3d84f8
Hannes Braun <hannesbraun@mail.de>
2020-03-30 00:21:06 +0200

    Revert changing the version format

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,17 +1,18 @@
 cask 'reaper' do
-  version '6.06'
+  version '6.60.0,6.06'
 
   if MacOS.version <= :mojave
     sha256 '4629c12f15d98aed55d4a2261a3cb0d58427395b26e700e57fbd6799c946e139'
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots}_x86_64.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
   else
     sha256 '699783865b478f697f337611549213f2679fd34377b546d0dc225f263660ae06'
 
-    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.no_dots}_x86_64_catalina.dmg"
+    url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
   end
 
-  appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64'
+  appcast 'https://www.cockos.com/reaper/latestversion/?p=osx_64',
+          configuration: version.after_comma
   name 'REAPER'
   homepage 'https://www.reaper.fm/'
 
