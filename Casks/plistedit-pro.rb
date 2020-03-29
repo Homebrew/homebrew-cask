9ae1019fbca597237005835cc0222812c0c63a97
Dario Vladovic <d.vladimyr@gmail.com>
2020-03-29 16:23:06 +0200

    Update plistedit-pro
    Update `zap` stanza.

--- Casks/plistedit-pro.rb
+++ Casks/plistedit-pro.rb
@@ -16,6 +16,7 @@ cask 'plistedit-pro' do
                '~/Library/Application Support/PlistEdit Pro',
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.fatcatsoftware.pledpro.sfl2',
                '~/Library/Application Support/com.fatcatsoftware.pledpro',
+               '~/Library/Caches/com.apple.helpd/Generated/com.fatcatsoftware.pledpro.help*',
                '~/Library/Caches/com.fatcatsoftware.pledpro',
                '~/Library/Preferences/com.fatcatsoftware.pledpro.plist',
              ]
