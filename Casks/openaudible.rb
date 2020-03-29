48ccc08f3271f0dfb21d61029d820f158c875404
Vítor Galvão <info@vitorgalvao.com>
2020-03-29 18:02:42 +0100

    Update openaudible.rb

--- Casks/openaudible.rb
+++ Casks/openaudible.rb
@@ -5,7 +5,7 @@ cask 'openaudible' do
   # github.com/openaudible was verified as official when first introduced to the cask
   url "https://github.com/openaudible/openaudible/releases/download/v#{version}/OpenAudible_#{version}_mac.dmg"
   appcast 'https://github.com/openaudible/openaudible/releases.atom'
-  name 'Open Audible'
+  name 'OpenAudible'
   homepage 'https://openaudible.org/'
 
   app 'OpenAudible.app'
