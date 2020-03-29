11ecb9e4aa8d72a27c87d24361e067bbe339422b
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-03-30 00:33:47 +0200

    Update reaper from 6.50.0,6.05 to 6.06 (#79581)
    * Update reaper from 6.50.0,6.05 to 6.06
    
    * Revert changing the version format

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,12 +1,12 @@
 cask 'reaper' do
-  version '6.50.0,6.05'
+  version '6.60.0,6.06'
 
   if MacOS.version <= :mojave
-    sha256 '3cacd09a8af998df06047308a50677014c82599791facebdffb373bdc4fde21a'
+    sha256 '4629c12f15d98aed55d4a2261a3cb0d58427395b26e700e57fbd6799c946e139'
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
   else
-    sha256 'a1ee59d1e91806eb17ebeb7d354a84a8a85f27c093b46bc4b66ea8a41137854a'
+    sha256 '699783865b478f697f337611549213f2679fd34377b546d0dc225f263660ae06'
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
   end
