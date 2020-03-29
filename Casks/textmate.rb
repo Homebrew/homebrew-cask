df2ce66cf82495c98a55498e0e06188adf06cd7f
Dario Vladovic <d.vladimyr@gmail.com>
2020-03-29 16:24:05 +0200

    Update textmate
    Update `zap` stanza.

--- Casks/textmate.rb
+++ Casks/textmate.rb
@@ -20,6 +20,7 @@ cask 'textmate' do
   zap trash: [
                '~/Library/Application Support/TextMate',
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.macromates.textmate.sfl2',
+               "~/Library/Caches/com.apple.helpd/Generated/TextMate #{version.major} Help*",
                '~/Library/Caches/com.macromates.TextMate',
                '~/Library/Preferences/com.macromates.TextMate.plist',
                '~/Library/Preferences/com.macromates.TextMate.preview.plist',
