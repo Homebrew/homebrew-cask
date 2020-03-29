b46e71e64cbb9740fac57a9d708277cc4d0e7b3e
Indexyz <jiduye@gmail.com>
2020-03-29 17:54:33 +0800

    Upload multimc cask version to 0.6.11

--- Casks/multimc.rb
+++ Casks/multimc.rb
@@ -1,6 +1,6 @@
 cask 'multimc' do
-  version '0.6.10'
-  sha256 'b04f2ca50103d3c67391d04bc3b72eef2cb6485321afaf396aba2ab2411f2197'
+  version '0.6.11'
+  sha256 '8b068b7a881a06e2f2c48a1ba696ed208780014dab03b3c2b633f72c15cd7d44'
 
   url 'https://files.multimc.org/downloads/mmc-stable-osx64.tar.gz'
   appcast 'https://github.com/MultiMC/MultiMC5/releases.atom'
