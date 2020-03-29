e1db796cab98ecd885f542ef794866cbf43125cf
Vítor Galvão <info@vitorgalvao.com>
2020-03-29 18:18:54 +0100

    Update uu-booster.rb

--- Casks/uu-booster.rb
+++ Casks/uu-booster.rb
@@ -1,9 +1,9 @@
 cask 'uu-booster' do
-  version '1.4.2(128)'
+  version '1.4.2,128'
   sha256 'e3ce65a19101130f49275bb2bf427b9e7ee0834490816490a4f70c84ddbdc05c'
 
   # uu.gdl.netease.com was verified as official when first introduced to the cask
-  url "https://uu.gdl.netease.com/UU-macOS-#{version}.dmg"
+  url "https://uu.gdl.netease.com/UU-macOS-#{version.before_comma}(#{version.after_comma}).dmg"
   name 'UU Booster'
   homepage 'https://uu.163.com/down/mac/'
 
