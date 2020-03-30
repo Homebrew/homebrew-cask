3238b976cccbabd79daf14d02acf44a8fdc71b2c
suschizu <brigitte@corecode.io>
2020-03-30 16:20:50 +0200

    Update comparemerge.rb

--- Casks/comparemerge.rb
+++ Casks/comparemerge.rb
@@ -7,5 +7,5 @@ cask 'comparemerge' do
   name 'CompareMerge'
   homepage 'https://sourceforge.net/projects/comparemergenosandbox/'
 
-  app "CompareMerge#{version.before_comma}/CompareMerge#{version.before_comma}.app"
+  app "CompareMerge#{version.before_comma}#{version.after_comma}/CompareMerge#{version.before_comma}#{version.after_comma}.app"
 end
