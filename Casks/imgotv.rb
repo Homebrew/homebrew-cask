db92def3c11393c1e0516c9a278be88a1479ef78
Work <brigitte@corecode.io>
2020-03-30 16:02:19 +0200

    Update imgotv from 6.2.1 to 6.2.2,2

--- Casks/imgotv.rb
+++ Casks/imgotv.rb
@@ -1,9 +1,9 @@
 cask 'imgotv' do
-  version '6.2.1'
-  sha256 '836845a9a3aa566d327ee94d648ae995ea76e3559240f216c2e8c43f64bba816'
+  version '6.2.2,2'
+  sha256 'a79c48c642060ec35b721c57f29d3b6ec6ebe701f23b66e811619c0d88b71223'
 
   # download.imgo.tv was verified as official when first introduced to the cask
-  url "https://download.imgo.tv/app/pc/newmac/mgtv-mango2-#{version}.dmg"
+  url "https://download.imgo.tv/app/pc/newmac/#{version.before_comma}-#{version.after_comma}/mgtv-mango2-#{version.before_comma}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Mac%20OS%20X&url=http://pcconf.api.mgtv.com/getPcDownloadUrl?source=mango2'
   name 'hunantv'
   name '芒果视频'
