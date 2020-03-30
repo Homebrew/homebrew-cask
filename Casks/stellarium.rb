523fbb96de36df8270951de7b19dbf714b697fab
Anver Housseini <contact@anver.ch>
2020-03-30 04:16:21 +0200

    Update stellarium.rb (#79617)

--- Casks/stellarium.rb
+++ Casks/stellarium.rb
@@ -1,9 +1,9 @@
 cask 'stellarium' do
-  version '0.19.3'
-  sha256 '58e649ddbefdf13cbb56af6a195a29620e15d2be3e1dc9431da541123b51c853'
+  version '0.20.0'
+  sha256 'f956c1fa05475bc37d86b1c04438912a28ec26736577a9dbb48502f2f9827c10'
 
   # github.com/Stellarium/stellarium was verified as official when first introduced to the cask
-  url "https://github.com/Stellarium/stellarium/releases/download/v#{version}/Stellarium-#{version}.dmg"
+  url "https://github.com/Stellarium/stellarium/releases/download/v#{version}/Stellarium-#{version}.zip"
   appcast 'https://github.com/Stellarium/stellarium/releases.atom'
   name 'Stellarium'
   homepage 'https://stellarium.org/'
