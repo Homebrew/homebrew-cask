6a397c31c5416449c426b80678876735631c1182
韦洪将 <weihongjiang@ecpark.cn>
2020-03-31 11:33:08 +0800

    add macstroke

--- Casks/macstroke.rb
+++ Casks/macstroke.rb
@@ -1,7 +1,7 @@
 cask 'macstroke' do
   version '1.0.8'
   sha256 'c5b805e0a6caca0469f1bdfdc341689a7e5b0786d5652c035e91bf0ed9fdaccf'
-  
+
   url "https://github.com/mtjo/MacStroke/releases/download/#{version}/MacStroke.zip"
   appcast 'https://github.com/mtjo/MacStroke/releases.atom'
   name 'MacStroke'
