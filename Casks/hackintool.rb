a6026e0a36c22eb4615df96598682f7ae931ebe3
Zhenfu Shi <szf1234@me.com>
2020-03-31 08:59:15 -0400

    Add hackintool v3.3.4 (#79644)

---
+++ Casks/hackintool.rb
@@ -0,0 +1,20 @@
+cask 'hackintool' do
+  version '3.3.4'
+  sha256 'f760e61d9f6ce0c3d06189175f21a21dde56568c4a51d5e08f50eb1ee56db9c9'
+
+  url "https://github.com/headkaze/Hackintool/releases/download/#{version}/Hackintool.zip"
+  appcast 'https://github.com/headkaze/Hackintool/releases.atom'
+  name 'Hackintool'
+  homepage 'https://github.com/headkaze/Hackintool'
+
+  auto_updates true
+
+  app 'Hackintool.app'
+
+  zap trash: [
+               '~/Library/Cookies/com.Headsoft.Hackintool.binarycookies',
+               '~/Library/Caches/com.Headsoft.Hackintool',
+               '~/Library/Caches/com.apple.helpd/Generated/com.Headsoft.Hackintool.help*',
+               '~/Library/Preferences/com.Headsoft.Hackintool.plist',
+             ]
+end
