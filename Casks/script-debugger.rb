73c33e69cc18537fc1aebc67788d2606dfd788fe
Julian Mayer <julian@corecode.io>
2020-04-01 00:39:46 +0200

    Update script-debugger from 7.0.10-7A99 to 7.0.11-7A110

--- Casks/script-debugger.rb
+++ Casks/script-debugger.rb
@@ -1,6 +1,6 @@
 cask 'script-debugger' do
-  version '7.0.10-7A99'
-  sha256 '1f2e2ff31750e912e1559928cbd31e7f3f512ff28dfc5618b87fe7dac98785a7'
+  version '7.0.11-7A110'
+  sha256 'eaf7852be5b0595128f484ae9d97b81079feb62349ab7a8fed28cfa09b65ec38'
 
   # s3.amazonaws.com/latenightsw.com was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/latenightsw.com/ScriptDebugger#{version}.dmg"
