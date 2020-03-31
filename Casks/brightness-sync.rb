c330e0f117bd5c7e772d45e63178bd8b98ad7911
Onne van Dijk <onnevandijk@icloud.com>
2020-03-31 13:40:24 +0200

    Add Brightness Sync.app v2.0.1

---
+++ Casks/brightness-sync.rb
@@ -0,0 +1,11 @@
+cask 'brightness-sync' do
+  version '2.0.1'
+  sha256 'f522c7422bb0958a4406e9f2ec431681771dfdfe1e134f0169d630927bc62e98'
+
+  url "https://github.com/OCJvanDijk/Brightness-Sync/releases/download/v#{version}/Brightness.Sync.app.zip"
+  appcast 'https://github.com/OCJvanDijk/Brightness-Sync/releases.atom'
+  name 'Brightness Sync'
+  homepage 'https://github.com/OCJvanDijk/Brightness-Sync'
+
+  app 'Brightness Sync.app'
+end
