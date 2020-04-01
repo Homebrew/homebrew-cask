fabd913e22827da710f6ac288f6ee94da4dfb45f
Randall <17261190+ran-dall@users.noreply.github.com>
2020-03-31 19:38:26 -0700

    Update tableplus from 3.1.2,296 to 3.3.0,300 (#79722)

--- Casks/tableplus.rb
+++ Casks/tableplus.rb
@@ -1,6 +1,6 @@
 cask 'tableplus' do
-  version '3.1.2,296'
-  sha256 'e993c07662c1c0c91fb70c419bf6d7dc700dc39a832a8ce3629a18649cbfc72c'
+  version '3.3.0,300'
+  sha256 'ed8e8add50d60df29ee775d5296c6842b134f3f767984d3afea5ee46555f6b9a'
 
   # tableplus-osx-builds.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://tableplus-osx-builds.s3.amazonaws.com/#{version.after_comma}/TablePlus.dmg"
