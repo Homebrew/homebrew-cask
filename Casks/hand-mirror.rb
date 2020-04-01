215319938980113d85dad3d424b6dfb209fc1dd8
Randall <ran.dall@icloud.com>
2020-03-31 19:45:35 -0700

    Update hand-mirror from 1.1 to 1.1.1

--- Casks/hand-mirror.rb
+++ Casks/hand-mirror.rb
@@ -1,6 +1,6 @@
 cask 'hand-mirror' do
-  version '1.1'
-  sha256 'f9fcc787e09efb8b9a085331dcc6559548bcdb09e29082c0299e42d8221bf585'
+  version '1.1.1'
+  sha256 '83e9cbb5903cd37d40e648f52c0800376e73c6107403a60bf6529a675868520f'
 
   url "https://handmirror.app/Hand%20Mirror%20#{version}.dmg"
   appcast 'https://handmirror.app'
