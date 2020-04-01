08a771d57669cccc0be77e18dd3c3a2f42340a47
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-04-01 17:11:29 +0300

    Update lunar from 3.1.3 to 3.1.4 (#79755)

--- Casks/lunar.rb
+++ Casks/lunar.rb
@@ -1,10 +1,9 @@
 cask 'lunar' do
-  version '3.1.3'
-  sha256 '3d92d6f47370a97c6878a84d62cfed6fd25def5419f8d81b988527e245eaa3d0'
+  version '3.1.4'
+  sha256 'f698589e23fdfb95c818b5369cb436b07e24aa6ab42969d6773b858dcac4fc77'
 
-  # github.com/alin23/Lunar was verified as official when first introduced to the cask
-  url "https://github.com/alin23/Lunar/releases/download/v#{version}/Lunar-#{version}.dmg"
-  appcast 'https://github.com/alin23/Lunar/releases.atom'
+  url "https://lunar.fyi/download/#{version}"
+  appcast 'https://lunar.fyi/appcast.xml'
   name 'Lunar'
   homepage 'https://lunar.fyi/'
 
