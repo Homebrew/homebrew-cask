7b235d91f14bf7c465f2272e757732968c97d94d
Vítor Galvão <info@vitorgalvao.com>
2020-04-01 15:28:12 +0100

    Update ipremoteutility.rb

--- Casks/ipremoteutility.rb
+++ Casks/ipremoteutility.rb
@@ -3,6 +3,7 @@ cask 'ipremoteutility' do
   sha256 '2c6bdbda882d52484891460f0e84cd5c3b77fa8973e6d15cb6b9c4066bc7b465'
 
   url "https://www.flandersscientific.com/ip-remote/release/IPRemoteUtility-#{version}-macOSX.zip"
+  appcast 'https://flandersscientific.com/ip-remote/'
   name 'Flanders IP Remote Utility'
   homepage 'https://www.flandersscientific.com/ip-remote/'
 
