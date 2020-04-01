de75523cfdde18879118f2817cf3449d29b6e023
Andreas Zeidler <az@zitc.de>
2020-04-01 08:48:45 +0200

    Add SilentKnight.app v1.6

---
+++ Casks/silentknight.rb
@@ -0,0 +1,11 @@
+cask 'silentknight' do
+  version '1.6'
+  sha256 'b0584a3adacc0911305e15953ec7a018df08db82606d765fc7acced3d03577a9'
+
+  # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
+  url 'https://eclecticlightdotcom.files.wordpress.com/2020/01/silentknight16.zip'
+  name 'SilentKnight'
+  homepage 'https://eclecticlight.co/lockrattler-systhist/'
+
+  app 'silentknight16/SilentKnight.app'
+end
