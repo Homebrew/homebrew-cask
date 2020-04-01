aeade54aeb7283865a572e408e0bbff0eb252d3e
Randall <ran.dall@icloud.com>
2020-03-31 19:14:39 -0700

    Update keep-it from 1.8.4 to 1.8.5

--- Casks/keep-it.rb
+++ Casks/keep-it.rb
@@ -1,6 +1,6 @@
 cask 'keep-it' do
-  version '1.8.4'
-  sha256 'a82a99eb7f07478b32266578bddee6e7b964eb39c8f58b11e8481e2dc1d7544e'
+  version '1.8.5'
+  sha256 '8d38683b97f4fa982a7954342abe287eee48fa6d0a2dd02073760354a25cb9a1'
 
   url "https://reinventedsoftware.com/keepit/downloads/KeepIt_#{version}.dmg"
   appcast 'https://reinventedsoftware.com/keepit/downloads/keepit.xml'
