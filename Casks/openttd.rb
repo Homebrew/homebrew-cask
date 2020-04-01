2fc164c8c52257cff77c183c4e449540c14391b5
EricFromCanada <enk3@outlook.com>
2020-04-01 09:17:36 -0400

    Update openttd from 1.9.3 to 1.10.0

--- Casks/openttd.rb
+++ Casks/openttd.rb
@@ -1,6 +1,6 @@
 cask 'openttd' do
-  version '1.9.3'
-  sha256 '59dae25ecc8e4b7a7ed46319abe74df391f730cbae91b16f24f14b2f50be2db3'
+  version '1.10.0'
+  sha256 'f4999e8676568ad87f933ba868e97dbe92fd6e47d4d821a3d33775b5d1c8dd39'
 
   url "https://proxy.binaries.openttd.org/openttd-releases/#{version}/openttd-#{version}-macosx.zip"
   appcast 'https://www.openttd.org/downloads/openttd-releases/latest.html'
