c401e5530a2918e4dc3e740db86515e6964823ec
Randall <ran.dall@icloud.com>
2020-03-31 19:43:48 -0700

    Update wacom-inkspace from 2.7.3 to 2.7.4

--- Casks/wacom-inkspace.rb
+++ Casks/wacom-inkspace.rb
@@ -1,6 +1,6 @@
 cask 'wacom-inkspace' do
-  version '2.7.3'
-  sha256 'bff10c04528e30ec9ee4d1e291cac1d4e951bafd7a5ed5320289fe582136110e'
+  version '2.7.4'
+  sha256 'add64f699d26731c19ff563ddaa895c54868c1922c749b8af01724046a86eb97'
 
   url "https://cdn.wacom.com/i/m/mac/WacomInkspaceApp_#{version}.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=http://link.wacom.com/i/m?os=mac'
