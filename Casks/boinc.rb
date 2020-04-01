d59735e218cb6517393c91cdfec6e0814756b15f
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-04-01 02:48:44 +0200

    Update boinc from 7.14.3 to 7.14.4 (#79701)
    * Update boinc from 7.14.3 to 7.14.4
    
    * Add missing launch jobs

--- Casks/boinc.rb
+++ Casks/boinc.rb
@@ -1,6 +1,6 @@
 cask 'boinc' do
-  version '7.14.3'
-  sha256 'c998bc2eba5ccc0e8df3ce1be672704f23bfe6ba361bebcb3009676d0519e225'
+  version '7.14.4'
+  sha256 '106ddd2985ee332164f24827d4decf7b083c5d531a83b2e93fb03f63899fe0a7'
 
   url "https://boinc.berkeley.edu/dl/boinc_#{version}_macOSX_x86_64.zip"
   appcast 'https://boinc.berkeley.edu/download.php'
@@ -10,7 +10,8 @@ cask 'boinc' do
 
   pkg "boinc_#{version}_macOSX_x86_64/BOINC Installer.app/Contents/Resources/BOINC.pkg"
 
-  uninstall pkgutil: 'edu.berkeley.boinc'
+  uninstall pkgutil:   'edu.berkeley.boinc',
+            launchctl: 'edu.berkeley.boinc-sshelper'
 
   zap trash: [
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/edu.berkeley.boinc.sfl*',
