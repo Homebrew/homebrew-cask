36a617b3d2913c6dd7946fe1ca96388accb4018b
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-02 09:35:33 -0700

    Update manico from 2.7.1,385 to 2.7.2,386 (#79866)

--- Casks/manico.rb
+++ Casks/manico.rb
@@ -1,6 +1,6 @@
 cask 'manico' do
-  version '2.7.1,385'
-  sha256 'ac5d3601dfd9d8c8845b7d58bdba9673549e8c37a7582079e9d537adc51c3f24'
+  version '2.7.2,386'
+  sha256 '209fd0cb5e5b72d7b1876ab4c80b045ab34bf069a04dbce52d1432da9aa30e5f'
 
   url "https://manico.im/api/release_manager/downloads/im.manico.Manico/#{version.after_comma}.zip"
   appcast 'https://manico.im/api/release_manager/im.manico.Manico.xml'
