5e8cd6b00710d9c6721728c39b5e231cb0f0b389
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-02 10:19:24 -0700

    Update protopie from 4.3.0 to 4.3.1 (#79884)

--- Casks/protopie.rb
+++ Casks/protopie.rb
@@ -1,6 +1,6 @@
 cask 'protopie' do
-  version '4.3.0'
-  sha256 'c346eab0e98ac7b0d7f1d3a5e5914beea116c9ca4d5ed7b6a4ceb7fb7f1edee1'
+  version '4.3.1'
+  sha256 '894c101b51858ef527c562c2bd666179efa09286be5c72abd8c4dad7dc641b54'
 
   url "https://release.protopie.io/ProtoPie-#{version}.dmg"
   appcast 'https://www.protopie.io/support/updates/'
