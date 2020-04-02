8bb59916e63016ab540635803837953daec66e1e
Kubilay <>
2020-04-02 12:56:16 +0300

    added awsx tool

---
+++ Casks/awsx.rb
@@ -0,0 +1,9 @@
+cask 'awsx' do
+  version '0.1'
+  sha256 '415664d005bdbfd8f96bfbc41ad8d78d16c120ce0d2f26099e83d047298181e9'
+
+  url "https://github.com/mertongngl/awsx/archive/v#{version}.zip"
+  name 'awsx'
+  homepage 'https://github.com/mertongngl/awsx'
+  binary "awsx-#{version}/awsx"
+end
