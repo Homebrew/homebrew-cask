dd1b1a31b89b8021bd4b4730ec9c4831c28c678a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-02 08:51:03 -0700

    Update iglance from 2.0.3 to 2.0.4 (#79843)

--- Casks/iglance.rb
+++ Casks/iglance.rb
@@ -1,6 +1,6 @@
 cask 'iglance' do
-  version '2.0.3'
-  sha256 '4f5c416d99c35884e28ad7b418c28b6f8c1cd75faa33895ae5f85d3ad04b0735'
+  version '2.0.4'
+  sha256 '8c0aa0c67dff70923dc1cab1e43cf8c7bdff51643ad19d1fee57bbcdb1514ad8'
 
   url "https://github.com/iglance/iglance/releases/download/v#{version}/iGlance_v#{version}.zip"
   appcast 'https://github.com/iglance/iglance/releases.atom'
