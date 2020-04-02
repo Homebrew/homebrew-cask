911c694133e436b2085c03df5731a2a530811844
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-01 20:05:51 -0700

    Update mountain-duck from 3.3.5.15470 to 3.3.6.15539 (#79795)

--- Casks/mountain-duck.rb
+++ Casks/mountain-duck.rb
@@ -1,6 +1,6 @@
 cask 'mountain-duck' do
-  version '3.3.5.15470'
-  sha256 '10c4450d360e99a164342d426289dd2f9554c19684fc10f150e6e4f719c52f3e'
+  version '3.3.6.15539'
+  sha256 '56744543ef30538faeebda84120940ad43575e670b4953f470cf23ff875ff039'
 
   url "https://dist.mountainduck.io/Mountain%20Duck-#{version}.zip"
   appcast "https://version.mountainduck.io/#{version.major}/macos/changelog.rss"
