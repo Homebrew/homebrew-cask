331a66cf42362986e33fb3f51689cd5c609b47f4
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-02 09:50:22 -0700

    Update freeyourmusic from 5.0.9 to 5.1.0 (#79874)

--- Casks/freeyourmusic.rb
+++ Casks/freeyourmusic.rb
@@ -1,6 +1,6 @@
 cask 'freeyourmusic' do
-  version '5.0.9'
-  sha256 'f59d7e876821a0bb87b1319fbab68136ac10dfa4cb71f0a5947c86cf601e2acc'
+  version '5.1.0'
+  sha256 '6b0add85aa5f60deda22e9e8bcafaaf71172bb77f8a6ed86ad809da7a46f8e80'
 
   # dzqeytqqx888.cloudfront.net was verified as official when first introduced to the cask
   url "https://dzqeytqqx888.cloudfront.net/FreeYourMusic-#{version}.dmg"
