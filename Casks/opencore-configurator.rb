040c610c4e0244edcc192f8ace0297d3161ac559
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-02 09:19:18 -0700

    Update opencore-configurator from 1.19.0.0 to 1.20.0.0 (#79855)

--- Casks/opencore-configurator.rb
+++ Casks/opencore-configurator.rb
@@ -1,6 +1,6 @@
 cask 'opencore-configurator' do
-  version '1.19.0.0'
-  sha256 'c4586922a6306837e0b12a07d5e66f35e94aac0f034f80a30d96020b58c3d79d'
+  version '1.20.0.0'
+  sha256 'd94974dea51908067beb073618abd8463be9d5c89c05b9d0d1c55fcb45794482'
 
   url 'https://mackie100projects.altervista.org/apps/opencoreconf/download-new-build.php?version=last'
   appcast 'https://mackie100projects.altervista.org/category/opencore-configurator-changelog/'
