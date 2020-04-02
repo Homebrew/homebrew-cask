ad08c8626c7a7cc83d6fdb41e7144086966be09c
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-02 09:48:55 -0700

    Update shift from 4.0.21 to 4.0.22 (#79877)

--- Casks/shift.rb
+++ Casks/shift.rb
@@ -1,6 +1,6 @@
 cask 'shift' do
-  version '4.0.21'
-  sha256 'aa92f2ed2a3eff3e6a356493c70754e7dfcaa3010318e5f004712a7d1374a6ff'
+  version '4.0.22'
+  sha256 '85457183bf5300484fc8f892cd068264b7ec518ec3357ddb6114edacdeb1ae48'
 
   url "https://update.tryshift.com/download/version/#{version}/osx_64"
   appcast 'https://tryshift.com/download/?platform=mac'
