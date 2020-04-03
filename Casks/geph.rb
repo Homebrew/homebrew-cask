da9782836b291e04b1326eb2cf4296b57ecfb7f0
Cap.雪ノ下八幡 <ywq44@iCloud.com>
2020-04-03 10:48:38 +0800

    Update geph from 3.5.2 to 3.5.3 (#79901)

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -1,6 +1,6 @@
 cask 'geph' do
-  version '3.5.2'
-  sha256 'd3ccf59c0aae2793f73b9aefd8fe289e985a7d4e50fa034f62bee0162c21e527'
+  version '3.5.3'
+  sha256 'cabe3521694223152000116e4d9d4b580b7165e5501ce7a3e936ebdd82faac11'
 
   url "https://dl.geph.io/desktop-builds/geph-macos-#{version}.dmg"
   appcast 'https://geph.io/'
