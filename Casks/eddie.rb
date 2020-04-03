3be9e1535776282adc2c29a3b8933c807cb6939a
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-03 09:06:44 -0700

    Update eddie from 2.16.3 to 2.18.9 (#79941)

--- Casks/eddie.rb
+++ Casks/eddie.rb
@@ -1,6 +1,6 @@
 cask 'eddie' do
-  version '2.16.3'
-  sha256 'f4a073d0689bd1218886883bd0a8f1a93412aba2e844e652698cdd56ff2f4fb8'
+  version '2.18.9'
+  sha256 'fd795535ab519e7de25e2ddfef96a5b6313c81af9c950d05dbc0337852573afd'
 
   # eddie.website was verified as official when first introduced to the cask
   url "https://eddie.website/download/?platform=macos&arch=x64&ui=ui&format=disk.dmg&version=#{version}"
