9ebe773403f87f1eadc4ed86dd369a63d0a09e90
Vítor Galvão <info@vitorgalvao.com>
2020-04-03 03:50:27 +0100

    Update zettelkasten.rb

--- Casks/zettelkasten.rb
+++ Casks/zettelkasten.rb
@@ -2,7 +2,7 @@ cask 'zettelkasten' do
   version '3.3.0'
   sha256 '0249695dd3742819624720e457559f707131d6864c76c911fd553e06781cc2f9'
 
-  # github.com/sjPlot/Zettelkasten/ was verified as official when first introduced to the cask
+  # github.com/sjPlot/Zettelkasten was verified as official when first introduced to the cask
   url "https://github.com/sjPlot/Zettelkasten/releases/download/v#{version}/Zettelkasten_#{version}.dmg"
   appcast 'https://github.com/sjPlot/Zettelkasten/releases.atom'
   name 'zettelkasten'
