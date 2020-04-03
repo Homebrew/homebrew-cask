dbaf898ebe11bb65845cadf3c67ec84afd8641c6
Dario Vladović <d.vladimyr@gmail.com>
2020-04-03 17:15:28 +0200

    Update qlvideo (#79926)
    Fix `version`, update `zap` stanza.

--- Casks/qlvideo.rb
+++ Casks/qlvideo.rb
@@ -1,5 +1,5 @@
 cask 'qlvideo' do
-  version '193'
+  version '1.93'
   sha256 'a0bce00cc2073fe065722409d32605cc2c20eaab89981e2d965d571d84fd77e0'
 
   url "https://github.com/Marginal/QLVideo/releases/download/rel-#{version.no_dots}/QLVideo_#{version.no_dots}.pkg"
@@ -11,4 +11,6 @@ cask 'qlvideo' do
 
   uninstall pkgutil:   'uk.org.marginal.qlvideo',
             launchctl: 'uk.org.marginal.qlvideo.mdimporter'
+
+  zap trash: '~/Library/Application Support/uk.org.marginal.qlvideo'
 end
