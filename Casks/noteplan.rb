9973b1a62e6b3035f884bd01e72756917f7f69e5
Beng Eu <beng@bengeu.com>
2020-04-04 09:41:32 -0400

    Update noteplan from 1.6.30,54 to 2.4.1,528

--- Casks/noteplan.rb
+++ Casks/noteplan.rb
@@ -1,5 +1,5 @@
 cask 'noteplan' do
-  version '1.6.30,54'
+  version '2.4.1,528'
   sha256 '51388ea6e157c1540907dd5efa7b2b8e752ef65d223baff34b60daab9da096a1'
 
   # rink.hockeyapp.net/api/2/apps/304b4477155e428780c345bcab69b380 was verified as official when first introduced to the cask
