d117dc46af11c3bd9700eba28e015def8b322de0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-04 11:35:04 -0700

    Update lifesize from 2.210.2637 to 2.210.2639 (#80012)

--- Casks/lifesize.rb
+++ Casks/lifesize.rb
@@ -1,6 +1,6 @@
 cask 'lifesize' do
-  version '2.210.2637'
-  sha256 '28acaaad991223a48cc80ec3804dc63a0ee5bfdc5557e2877b5bfcd3a77e9c0a'
+  version '2.210.2639'
+  sha256 '142514768b655e6600b55b1ee0361733b4cab3ba5c0400aa270ac31b4c4b5497'
 
   # download.lifesizecloud.com/Lifesize- was verified as official when first introduced to the cask
   url "https://download.lifesizecloud.com/Lifesize-#{version}.dmg"
