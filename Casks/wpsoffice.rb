a2e1e18a9ea839c7fc23f9fab3530e41869c75a0
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-04 11:36:07 -0700

    Update wpsoffice from 2.0.2,3356 to 2.1.0,3464 (#80009)

--- Casks/wpsoffice.rb
+++ Casks/wpsoffice.rb
@@ -1,6 +1,6 @@
 cask 'wpsoffice' do
-  version '2.0.2,3356'
-  sha256 'c1eebdeda53a786af2f175b0f1a82b098fe31543b359c96379887f11b2b630e6'
+  version '2.1.0,3464'
+  sha256 'f46ec300e8129c6c1972eee79b0e2208f5b93b7048d6fd43dca8b35752a09c65'
 
   # wdl1.pcfg.cache.wpscdn.com was verified as official when first introduced to the cask
   url "https://wdl1.pcfg.cache.wpscdn.com/wpsdl/macwpsoffice/download/#{version.before_comma}.#{version.after_comma}/WPSOffice_#{version.before_comma}(#{version.after_comma}).dmg"
