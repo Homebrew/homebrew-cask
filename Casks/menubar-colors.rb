cf4376df54179d4fffc741fd066b03071ba4c400
Dario Vladovic <d.vladimyr@gmail.com>
2020-04-05 13:44:01 +0200

    Update menubar-colors
    Update appcast.

--- Casks/menubar-colors.rb
+++ Casks/menubar-colors.rb
@@ -3,7 +3,7 @@ cask 'menubar-colors' do
   sha256 'b12188d45b57ae8614d9fc25d97c8302936916655ac6c8b19b9ca629c9ee7367'
 
   url "https://github.com/nvzqz/menubar-colors/releases/download/v#{version}/Menubar-Colors.zip"
-  appcast 'https://github.com/nvzqz/menubar-colors/releases.atom'
+  appcast 'https://nikolaivazquez.com/Menubar-Colors/releases/appcast.xml'
   name 'Menubar Colors'
   homepage 'https://github.com/nvzqz/Menubar-Colors'
 
