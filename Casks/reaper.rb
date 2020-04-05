48d20b51a65bcd319580fdb8b54aee00837d6368
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-04-05 02:06:45 +0200

    Update reaper from 6.70.0,6.07 to 6.80.0,6.08 (#80026)

--- Casks/reaper.rb
+++ Casks/reaper.rb
@@ -1,12 +1,12 @@
 cask 'reaper' do
-  version '6.70.0,6.07'
+  version '6.80.0,6.08'
 
   if MacOS.version <= :mojave
-    sha256 '4298d8ded384cae26e52c334595dface185ad113d062a0d0abec6d46ae40458e'
+    sha256 '176dd485985b06513d60addc299aeeb23a6301bcd202388b898d72b28b24bab2'
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64.dmg"
   else
-    sha256 '7ffb8117b87cd708a364cbecf8bec3b48e10634d679cc295baa28b75988b87cb'
+    sha256 'd619007f2040fe9440f496ed6db4eb3e88ca5928ff479ccb9c698eeea25b9722'
 
     url "https://www.reaper.fm/files/#{version.major}.x/reaper#{version.after_comma.no_dots}_x86_64_catalina.dmg"
   end
