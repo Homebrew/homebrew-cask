5261183f7e883b72fb919dd92b830c6f90ac6816
Brigitte Zmoelnig <brigitte@corecode.io>
2020-04-05 19:51:04 +0200

    Update aurora-hdr from 1.0.0.5825 to 1.0.0.6432

--- Casks/aurora-hdr.rb
+++ Casks/aurora-hdr.rb
@@ -1,8 +1,8 @@
 cask 'aurora-hdr' do
-  version '1.0.0.5825'
-  sha256 'ebf975075b5ac3dd58277f3ba52b2a0fc70989429ddbacc18a3bbb81cdd014ba'
+  version '1.0.0.6432'
+  sha256 '5e178f7057f7ac66498629358d76cea3cf7760cd27b2ddf2fc426ff36afd4f0f'
 
-  url "https://downloads.skylum.com/aurorahdr2019/mac/AuroraHDR2019_Distribution_v#{version.dots_to_underscores}.zip"
+  url "https://downloads.skylum.com/aurorahdr/mac/AuroraHDR_Distribution_v#{version.dots_to_underscores}.zip"
   appcast 'http://aurorahdr2019mac.update.skylum.com/',
           configuration: version.dots_to_underscores
   name 'Aurora HDR'
