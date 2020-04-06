5bc421ca8044785cb5dda18634f47b1d0a2b58ec
Work <brigitte@corecode.io>
2020-04-06 15:06:25 +0200

    Update pym-player from 7.0.0,329 to 7.0.1,330

--- Casks/pym-player.rb
+++ Casks/pym-player.rb
@@ -1,8 +1,8 @@
 cask 'pym-player' do
-  version '7.0.0,329'
-  sha256 '90ce8010fcfba46a3c8b82ab7097a172df57ac881a8c273d736d71426a62316d'
+  version '7.0.1,330'
+  sha256 'b9a22a37661c0c00591454e5c429ef42b11c8601ac95ad7f50d0e0bb8a7e0f65'
 
-  url "http://pym.uce.pl/download/pliki/PYMPlayer#{version.before_comma.major}_#{version.after_comma}.dmg"
+  url "http://pym.uce.pl/download/pliki/PYMPlayer#{version.before_comma.no_dots}_#{version.after_comma}.dmg"
   appcast 'http://pym.uce.pl/pym-player/'
   name 'PYM Player'
   homepage 'http://pym.uce.pl/pym-player/'
