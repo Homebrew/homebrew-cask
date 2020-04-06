b2a722b3afb03cfe2ab2a025908d95dec43771ee
Vítor Galvão <info@vitorgalvao.com>
2020-04-06 14:22:13 +0100

    Update sia-ui.rb

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -3,7 +3,6 @@ cask 'sia-ui' do
   sha256 '041a6a9bba98db7ecde27a2ce46ddf1f129dd8dba72bc59b732b096e13334330'
 
   url "https://sia.tech/releases/Sia-UI-v#{version}.dmg"
-  appcast 'https://sia.tech/1.f737981720cd8bbc13de.js'
   name 'Sia-UI'
   homepage 'https://sia.tech/'
 
