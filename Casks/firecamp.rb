70fddfe4ece5d3bafa90943d8acab903640e529c
Nishchit <dnishchit@gmail.com>
2020-04-06 16:29:19 +0530

    Firecamp upgraded to v1.1.1

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -1,6 +1,6 @@
 cask 'firecamp' do
-  version '1.0.9'
-  sha256 '92da4d80699dafa97c54ab05490553019d8d8d59af27d1923373eb164f1aa72b'
+  version '1.1.1'
+  sha256 'eb10be146f7aab87716b364e213dbb9b92039f128a8df2549edc6ea1a8835183'
 
   # firecamp.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg"
