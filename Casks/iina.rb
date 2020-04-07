cb9afe722c26dab4358df0a71a9a9088fbdd74c2
miccal <miccal.matthews@gmail.com>
2020-04-08 06:03:28 +0800

    Update iina.rb

--- Casks/iina.rb
+++ Casks/iina.rb
@@ -3,7 +3,7 @@ cask 'iina' do
   sha256 '51cce921ef52f00df9b474e230f33d5a947c5b6e4d33aac4213a0e07b4b4a246'
 
   url "https://dl.iina.io/IINA.v#{version}.dmg"
-  appcast 'https://www.iina.io/appcast.xml'
+  appcast 'https://iina.io/appcast.xml'
   name 'IINA'
   homepage 'https://iina.io/'
 
