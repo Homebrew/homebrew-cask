1baddeec72e0cf21c8ddb4b8a9baad9848e99f8f
Vaios Karampinis <oden@oden.gr>
2020-04-07 11:41:09 +0300

    Update marvin from 1.50.1 to 1.51.0

--- Casks/marvin.rb
+++ Casks/marvin.rb
@@ -1,6 +1,6 @@
 cask 'marvin' do
-  version '1.50.1'
-  sha256 '746f2b9f39c90bcf146cf8bd252089a04f5dff40d6cb48c7884c1365dcdbee0f'
+  version '1.51.0'
+  sha256 'd8422c303c11433eebc639209ce95847f174b26a65c19f67a638c511e583c68d'
 
   # amazingmarvin.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://amazingmarvin.s3.amazonaws.com/Marvin-#{version}.dmg"
