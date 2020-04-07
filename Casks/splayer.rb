5653b5979befb0926b6fe180575a7730f574a0f2
Sin <dev@zouxin.net>
2020-04-07 19:03:55 +0800

    Add SPlayer v4.9.1

---
+++ Casks/splayer.rb
@@ -0,0 +1,10 @@
+cask 'splayer' do
+  version '4.9.1'
+  sha256 'f7755ae224794f221cdd7fbd83cabc1b38fd4e554cd2a74907f75291bccf23bd'
+
+  url "https://cdn.splayer.org/download/SPlayer-#{version}.dmg"
+  name 'SPlayer'
+  homepage 'https://splayer.org/'
+
+  app 'SPlayer.app'
+end
