07ab8824aaa44dcb2fa57400a7cf8f079c0e25f1
Vaios Karampinis <oden@oden.gr>
2020-04-07 16:57:50 +0300

    update appcast url

--- Casks/alfaview.rb
+++ Casks/alfaview.rb
@@ -3,7 +3,7 @@ cask 'alfaview' do
   sha256 'b75d3b5c1656a1128892facbeace44202d3093e8a5d20fe0d76d8fc231090d53'
 
   url "https://assets.alfaview.com/stable/mac/alfaview-mac-production-#{version}.dmg"
-  appcast 'https://alfaview.com/downloads'
+  appcast 'https://production-alfaview-assets.alfaview.com/stable/mac/version.info'
   name 'Alfaview'
   homepage 'https://alfaview.com/'
 
