e8f6ddde1fd04467c4133fbacc86a020a6ec1b9e
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-07 18:25:52 -0700

    Add appcast

--- Casks/splayer.rb
+++ Casks/splayer.rb
@@ -3,6 +3,7 @@ cask 'splayer' do
   sha256 'f7755ae224794f221cdd7fbd83cabc1b38fd4e554cd2a74907f75291bccf23bd'
 
   url "https://cdn.splayer.org/download/SPlayer-#{version}.dmg"
+  appcast 'https://github.com/chiflix/splayerx/releases'
   name 'SPlayer'
   homepage 'https://splayer.org/'
 
