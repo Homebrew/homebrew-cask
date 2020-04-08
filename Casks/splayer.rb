826ceab090e01ea92e620b86f14a843acdcee3e4
Vítor Galvão <info@vitorgalvao.com>
2020-04-08 04:18:31 +0100

    Update splayer.rb

--- Casks/splayer.rb
+++ Casks/splayer.rb
@@ -3,7 +3,7 @@ cask 'splayer' do
   sha256 'f7755ae224794f221cdd7fbd83cabc1b38fd4e554cd2a74907f75291bccf23bd'
 
   url "https://cdn.splayer.org/download/SPlayer-#{version}.dmg"
-  appcast 'https://github.com/chiflix/splayerx/releases'
+  appcast 'https://github.com/chiflix/splayerx/releases.atom'
   name 'SPlayer'
   homepage 'https://splayer.org/'
 
