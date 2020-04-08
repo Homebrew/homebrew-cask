7724dcf4b97f2d347be3fdf6ded4b1484060341e
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2020-04-08 06:18:44 -0500

    Update xamarin-mac from 6.16.0.11 to 6.16.0.13 (#80219)

--- Casks/xamarin-mac.rb
+++ Casks/xamarin-mac.rb
@@ -1,6 +1,6 @@
 cask 'xamarin-mac' do
-  version '6.16.0.11'
-  sha256 'f74df10748efa8169ff852593c422f7ee363f9480a13857853139b42d542540e'
+  version '6.16.0.13'
+  sha256 '43e7ad19bbc4cdc60eb6d7a01bcca27ccf0434c31c9e3ba18f15d99c91736873'
 
   url "https://dl.xamarin.com/XamarinforMac/Mac/xamarin.mac-#{version}.pkg"
   appcast 'https://docs.microsoft.com/en-us/xamarin/mac/release-notes/',
