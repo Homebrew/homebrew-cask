ea14ecc22c8d6c157cb00adc34fcc57ab2ac7d01
Moritz Gunz <moritz.gunz@gmail.com>
2020-04-08 23:04:54 +0200

    Fix style

--- Casks/devolo-cockpit.rb
+++ Casks/devolo-cockpit.rb
@@ -3,8 +3,8 @@ cask 'devolo-cockpit' do
   sha256 'ebec71ac589ac0fd8b9bf7c0209b8fba01203d52b7009c3073441592b2c4bd97'
 
   url "https://www.devolo.com/fileadmin/Web-Content/DE/products/hnw/devolo-cockpit/software/devolo-cockpit-v#{version.dots_to_hyphens}.dmg"
-  name 'Devolo dLAN Cockpit'
   appcast 'https://www.devolo.com/support/downloads/download/devolo-cockpit'
+  name 'Devolo dLAN Cockpit'
   homepage 'https://www.devolo.com/internet-in-any-room/devolo-cockpit'
 
   installer manual: "#{staged_path}/devolo Cockpit Installation.app"
