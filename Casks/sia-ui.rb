b258948996351e9123358a6827c1bf2eb502f492
suschizu <brigitte@corecode.io>
2020-04-08 05:20:34 +0200

    Update sia-ui from 1.4.6 to 1.4.7 (#80101)
    * Update sia-ui from 1.4.6 to 1.4.7
    
    * Update sia-ui.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/sia-ui.rb
+++ Casks/sia-ui.rb
@@ -1,9 +1,8 @@
 cask 'sia-ui' do
-  version '1.4.6'
-  sha256 '4eda8ae4aa628cbcc87c7ed9bdebbc5a3e3ab604a05a60eb78d608ee88d5582f'
+  version '1.4.7'
+  sha256 '041a6a9bba98db7ecde27a2ce46ddf1f129dd8dba72bc59b732b096e13334330'
 
   url "https://sia.tech/releases/Sia-UI-v#{version}.dmg"
-  appcast 'https://sia.tech/1.f737981720cd8bbc13de.js'
   name 'Sia-UI'
   homepage 'https://sia.tech/'
 
