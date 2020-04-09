d7b182fd3e16885c5dfafd4aa604780fca024293
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2020-04-09 06:56:49 -0500

    Create android-clt.rb
    The Android SDK Tools package has been deprecated and replaced by the Command Line Tools. Because of conflicts/removals/additions to each package for specific binaries, then this may be a better alternative to creating a cask for the command line tools. Perhaps, moving the cask 'android-sdk' to the homebrew-cask-versions tap because of deprecation would be better.

---
+++ Casks/android-clt.rb
@@ -0,0 +1,34 @@
+cask 'android-clt' do
+  version '6200805'
+  sha256 '23f0626336a98d70aff7311a73292026af31bc577c6f06b509cd4ad33752313e'
+
+  # dl.google.com/android/repository was verified as official when first introduced to the cask
+  url "https://dl.google.com/android/repository/commandlinetools-mac-#{version}_latest.zip"
+  name 'android-clt'
+  homepage 'https://developer.android.com/'
+  
+  conflicts_with cask: 'android-sdk'
+
+  binary "#{staged_path}/tools/bin/apkanalyzer"
+  binary "#{staged_path}/tools/bin/avdmanager"
+  binary "#{staged_path}/tools/bin/lint"
+  binary "#{staged_path}/tools/bin/screenshot2"
+  binary "#{staged_path}/tools/bin/sdkmanager"
+
+  postflight do
+    FileUtils.ln_sf(staged_path.to_s, "#{HOMEBREW_PREFIX}/share/android-sdk")
+  end
+
+  uninstall_postflight do
+    FileUtils.rm_f("#{HOMEBREW_PREFIX}/share/android-sdk")
+  end
+
+  caveats do
+    depends_on_java '8'
+    <<~EOS
+      You can control android sdk packages via the sdkmanager command.
+      You may want to add to your profile:
+        'export ANDROID_SDK_ROOT="#{HOMEBREW_PREFIX}/share/android-sdk"'
+    EOS
+  end
+end
