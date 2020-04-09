5c47366a3126840b1c41b2fd6f45108083300a90
Sascha Bratton <sascha@rejoiner.com>
2020-04-09 12:35:32 -0400

    Add Zappy.app

---
+++ Casks/zappy.rb
@@ -0,0 +1,10 @@
+cask 'zappy' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://zappy.zapier.com/releases/zappy-latest.zip'
+  name 'Zappy'
+  homepage 'https://zapier.com/zappy'
+
+  app 'Zappy.app'
+end
