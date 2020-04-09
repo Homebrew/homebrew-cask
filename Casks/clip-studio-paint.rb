ae6b7f023dc58b8ff55c81b9bb50f9692e2fd6cb
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-09 12:08:24 -0700

    Update clip-studio-paint.rb

--- Casks/clip-studio-paint.rb
+++ Casks/clip-studio-paint.rb
@@ -3,7 +3,7 @@ cask 'clip-studio-paint' do
   sha256 'ea3cca466ec6b253e84661a6a3f8c8ffdae36d5382a94acf08e1c58316e1261e'
 
   url "https://vd.clipstudio.net/clipcontent/paint/app/#{version.no_dots}/CSP_#{version.no_dots}m_app.pkg"
-  appcast 'https://www.clipstudio.net/en/dl'
+  appcast 'https://www.clipstudio.net/en/dl',
           configuration: version.no_dots
   name 'CLIP STUDIO PAINT'
   homepage 'https://www.clipstudio.net/en'
