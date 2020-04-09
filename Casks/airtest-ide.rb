058e8d715798bbcbbb46d51b1cf146c6c5990bb3
zengxs <xiangsong.zeng@outlook.com>
2020-04-09 13:18:07 +0800

    Add AirtestIDE 1.2.3

---
+++ Casks/airtest-ide.rb
@@ -0,0 +1,10 @@
+cask 'airtest-ide' do
+  version '1.2.3'
+  sha256 '8998233963fb07e9c74185575294a88089ac2349839450722280466b2464b7b7'
+
+  url 'https://airtestproject.s3.netease.com/downloads/AirtestIDE/AirtestIDE_2020-01-20_py3_Mac10-12.dmg'
+  name 'AirtestIDE'
+  homepage 'https://airtest.netease.com'
+
+  app 'AirtestIDE.app'
+end
