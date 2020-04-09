75038beb686b8430f0edfbd2ad9a2086d71af1f2
Hannes Braun <hannesbraun@mail.de>
2020-04-09 21:17:53 +0200

    Update jalbum from 19.3.6 to 20.0.0

--- Casks/jalbum.rb
+++ Casks/jalbum.rb
@@ -1,8 +1,8 @@
 cask 'jalbum' do
-  version '19.3.6'
-  sha256 'd6ed48947e5735f4ab137259ad0f9dd0b38b463999494fd3d9eda5576bf50071'
+  version '20.0.0'
+  sha256 '4c2ee25138bda78479601cd4d1e7ebfd6227967adb4b09876f46d209017a94c3'
 
-  url "https://download.jalbum.net/download/#{version}/MacOSX/jAlbum.dmg"
+  url "https://download.jalbum.net/download/#{version.major}/MacOSX/jAlbum.dmg"
   appcast 'https://jalbum.net/en/software/download/previous',
           configuration: version.major_minor.chomp('.0')
   name 'jAlbum'
