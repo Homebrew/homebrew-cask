e5983bf0609c9ba16d0b9dd00ca68ee01801ba3c
Jan Marcel Ientile <ientij@fastmail.com>
2020-04-10 12:04:03 -0300

    Update day-o from 2.0 to 3.0 (#80384)

--- Casks/day-o.rb
+++ Casks/day-o.rb
@@ -1,6 +1,6 @@
 cask 'day-o' do
-  version '2.0'
-  sha256 '24c64e66499bde5149d43c7b9b33b44b46ac6d136fcc4a988fd7c298aff02071'
+  version '3.0'
+  sha256 'ace5905328106c34e488d353fb52845e807333705076bbf994175c9ffec7f108'
 
   url "https://shauninman.com/assets/downloads/Day-#{version}.zip"
   name 'Day-O'
