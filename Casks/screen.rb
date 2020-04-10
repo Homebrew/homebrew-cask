585c4e0ef8cc6ff4354b23dbca0aa675acd750c5
Randall <ran.dall@icloud.com>
2020-04-10 15:14:36 -0700

    Update screen from 1.0.24 to 1.0.25

--- Casks/screen.rb
+++ Casks/screen.rb
@@ -1,6 +1,6 @@
 cask 'screen' do
-  version '1.0.24'
-  sha256 '6667a58d998080dcc75c2c6ff1c029e7c4154f9bf5a5a45448154fce503b95fd'
+  version '1.0.25'
+  sha256 '5f3120e7d479cb82e3877e42d5c0134f6a12c48b4a42f4fa22b6afb5327efe10'
 
   url "https://download.screen.so/desktop-app/darwin/#{version}/Screen.dmg"
   appcast 'https://download.screen.so/'
