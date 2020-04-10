b0a974ad59f9e967b9bd03752e677b58b2305cb2
Yves Hoppe <info@bytee.net>
2020-04-10 23:40:39 +0200

    Add Empoche 0.2.1

--- Casks/empoche.rb
+++ Casks/empoche.rb
@@ -2,6 +2,7 @@ cask 'empoche' do
   version '0.2.1'
   sha256 '466622f4cb8a062ea4d43b772fbda51917c767f289555cf21b9108b7ea52ef96'
 
+  # empoche-desktop.s3.eu-central-1.amazonaws.com was verified as official when first introduced to the cask
   url "https://empoche-desktop.s3.eu-central-1.amazonaws.com/Empoche-#{version}.dmg"
   name 'Empoche'
   homepage 'https://empoche.com/'
