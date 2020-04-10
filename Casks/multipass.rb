0d753efec4671e2daaf669e24e177b188cc99476
lulor <lu.lorenzo17@gmail.com>
2020-04-10 18:03:56 +0200

    Remove user configuration files multipass v1.1.0

--- Casks/multipass.rb
+++ Casks/multipass.rb
@@ -22,6 +22,9 @@ cask 'multipass' do
                        ]
 
   zap trash: [
+               '~/Library/Application Support/multipass',
+               '~/Library/Application Support/multipass-gui',
+               '~/Library/Preferences/multipass',
                '/var/root/Library/Application Support/multipassd',
                '/var/root/Library/Preferences/multipassd',
                '/Library/Logs/Multipass',
