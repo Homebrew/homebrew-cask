3c9bb17cb3326872b4d4adefb8d2bdf339619489
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-10 15:04:32 -0700

    Update jami from 20200408.1149 to 20200409.1452 (#80411)

--- Casks/jami.rb
+++ Casks/jami.rb
@@ -1,6 +1,6 @@
 cask 'jami' do
-  version '20200408.1149'
-  sha256 '7464edac2c254b6a887655c158b5bf84c1dd13109e84ec0ef9cca892a17c7ca2'
+  version '20200409.1452'
+  sha256 '9ddcc103a78ff9e63a922faa218d276372f602b73a30c0af8eda7342b3df192d'
 
   url "https://dl.jami.net/mac_osx/jami-#{version.no_dots}.dmg"
   appcast 'https://dl.jami.net/mac_osx/sparkle-ring.xml',
