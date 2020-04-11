c469c6544bd91809dac8dd1bb66e826c07e13b5f
Brigitte Zmoelnig <brigitte@corecode.io>
2020-04-11 20:09:33 +0200

    Update unity from 2019.3.7f1,6437fd74d35d to 2019.3.9f1,e6e740a1c473

--- Casks/unity.rb
+++ Casks/unity.rb
@@ -1,6 +1,6 @@
 cask 'unity' do
-  version '2019.3.7f1,6437fd74d35d'
-  sha256 '09fe5b135bc4563750ab20cc7cfd35d66fe30815647eae5c5678f7c2d11acff8'
+  version '2019.3.9f1,e6e740a1c473'
+  sha256 '04cb4d805e7c80985188772c4b7d95f55e5c9ce8ade6a2f19d6c55961cbc846d'
 
   url "https://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity-#{version.before_comma}.pkg"
   appcast 'https://unity3d.com/get-unity/download/archive'
