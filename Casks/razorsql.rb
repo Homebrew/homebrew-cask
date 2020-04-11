0664474c11832f4ad50e183b51d0be8daff355e4
Dario Vladovic <d.vladimyr@gmail.com>
2020-04-11 15:02:21 +0200

    Update razorsql
    Use notarized version & update homepage.

--- Casks/razorsql.rb
+++ Casks/razorsql.rb
@@ -1,11 +1,12 @@
 cask 'razorsql' do
   version '9.0.9'
-  sha256 'b6ae0bd8b23fb70a33bb04921c1cc93239c9233e2b4d2740e79e8f845220cd62'
+  sha256 '95fefa4498725539a6fa9f136cdd3894b6aba622135dd67d49e1df670ba927df'
 
-  url "http://downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}_x64.dmg"
+  # s3.dualstack.us-east-1.amazonaws.com was verified as official when first introduced to the cask
+  url "https://s3.dualstack.us-east-1.amazonaws.com/downloads.razorsql.com/downloads/#{version.dots_to_underscores}/razorsql#{version.dots_to_underscores}.dmg"
   appcast 'https://razorsql.com/updates.html'
   name 'RazorSQL'
-  homepage 'https://razorsql.com/download_mac.html'
+  homepage 'https://razorsql.com/'
 
   depends_on macos: '>= :high_sierra'
 
