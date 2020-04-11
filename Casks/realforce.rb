6bf13dd289088c14288e435e4ba46a65cdb7bd32
Brigitte Zmoelnig <brigitte@corecode.io>
2020-04-11 20:24:03 +0200

    Update realforce from 1.0.2 to 1.0.3

--- Casks/realforce.rb
+++ Casks/realforce.rb
@@ -1,6 +1,6 @@
 cask 'realforce' do
-  version '1.0.2'
-  sha256 '9db2e54faee1b3ecb4d727585827edbe0ce07940c2738e296ddaf0ccb28b0d5a'
+  version '1.0.3'
+  sha256 '0929016237d8992e9248e624761a4fffdebd408a0c061c54395668fa54dfdbf7'
 
   url "https://www.realforce.co.jp/support/download/Install%20REALFORCE%20Software_#{version.dots_to_hyphens}.pkg"
   appcast 'https://www.realforce.co.jp/support/download/index.html'
