d45ab0fd01085109dcda63eeb196caa8a87ddf2e
miccal <miccal.matthews@gmail.com>
2020-04-11 19:34:24 +0800

    Update rectangle.rb

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -2,14 +2,13 @@ cask 'rectangle' do
   version '0.25'
   sha256 '9c5f8009ea024712b88e53e3d75735363b5367f060f6d59475ff97a79a80ba01'
 
-  # github.com/rxhanson/Rectangle/releases/download/v was verified as official when first introduced to the cask
-  url "https://github.com/rxhanson/Rectangle/releases/download/v#{version}/Rectangle#{version}.dmg"
+  url "https://rectangleapp.com/downloads/Rectangle#{version}.dmg"
   appcast 'https://www.rectangleapp.com/downloads/updates.xml'
   name 'Rectangle'
   homepage 'https://rectangleapp.com/'
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :el_capitan'
 
   app 'Rectangle.app'
 
