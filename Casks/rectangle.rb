5813a1f4afd87a62242241796671d8c94d2ef0c3
miccal <miccal.matthews@gmail.com>
2020-04-11 22:07:26 +0800

    Update rectangle `url`, `depends_on` and `zap` (#80443)
    * Update rectangle.rb
    
    * Update rectangle.rb

--- Casks/rectangle.rb
+++ Casks/rectangle.rb
@@ -2,20 +2,21 @@ cask 'rectangle' do
   version '0.25'
   sha256 '9c5f8009ea024712b88e53e3d75735363b5367f060f6d59475ff97a79a80ba01'
 
-  # github.com/rxhanson/Rectangle/releases/download/v was verified as official when first introduced to the cask
-  url "https://github.com/rxhanson/Rectangle/releases/download/v#{version}/Rectangle#{version}.dmg"
+  url "https://rectangleapp.com/downloads/Rectangle#{version}.dmg"
   appcast 'https://www.rectangleapp.com/downloads/updates.xml'
   name 'Rectangle'
   homepage 'https://rectangleapp.com/'
 
   auto_updates true
-  depends_on macos: '>= :sierra'
+  depends_on macos: '>= :el_capitan'
 
   app 'Rectangle.app'
 
   zap trash: [
-               '~/Library/Preferences/com.knollsoft.Rectangle.plist',
+               '~/Library/Application Scripts/com.knollsoft.RectangleLauncher',
                '~/Library/Application Support/Rectangle',
                '~/Library/Caches/com.knollsoft.Rectangle',
+               '~/Library/Containers/com.knollsoft.RectangleLauncher',
+               '~/Library/Preferences/com.knollsoft.Rectangle.plist',
              ]
 end
