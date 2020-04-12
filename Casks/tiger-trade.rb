37dabab4abd88843e2fcc1e5b65e631655713dbc
Brigitte Zmoelnig <brigitte@corecode.io>
2020-04-12 20:30:06 +0200

    Update tiger-trade from 5.3.1,20191224:BACCCE to 5.6.3,20200409:3E708C

--- Casks/tiger-trade.rb
+++ Casks/tiger-trade.rb
@@ -1,6 +1,6 @@
 cask 'tiger-trade' do
-  version '5.3.1,20191224:BACCCE'
-  sha256 '34153f46fc100964934cdadc0a4ea751315d270da0834e4b8324f07d730b0797'
+  version '5.6.3,20200409:3E708C'
+  sha256 'd1056c280ef73996f863e93350a5e76bd0f4aebd5074034ce1073b17382eec83'
 
   # s.tigerfintech.com was verified as official when first introduced to the cask
   url "https://s.tigerfintech.com/desktop/cdn/f/TigerTrade_#{version.before_comma}_#{version.after_comma.before_colon}_#{version.after_colon}.dmg"
