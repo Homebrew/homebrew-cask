44d0acf59f8f36b83d34ed5bdeafb500003c83ae
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-12 10:33:47 -0700

    Update vnote from 2.9 to 2.9.1 (#80536)

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -1,6 +1,6 @@
 cask 'vnote' do
-  version '2.9'
-  sha256 'dabeb8bca928965a3c90f35025ea5b62a8e48470248e5200841ce16b59e9e9d8'
+  version '2.9.1'
+  sha256 '7611fca118e2b5894b1530ae049db9ede91ebbabdd6fc5f135cb61d724f8f12d'
 
   # github.com/tamlok/vnote was verified as official when first introduced to the cask
   url "https://github.com/tamlok/vnote/releases/download/v#{version}/VNote-#{version}-x64.dmg"
