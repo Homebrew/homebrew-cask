bdd19a042f31fb6eb176475a9ad4e68ded2d210d
suschizu <brigitte@corecode.io>
2020-04-12 19:01:48 +0200

    Update firecamp.rb

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -1,6 +1,6 @@
 cask 'firecamp' do
   version '1.2.2'
-  sha256 '1241c4ee06ff845cf4b2dfdeba73b77d54e01854f959f4e55d15b2f9b91caac2'
+  sha256 '3d42d0d2793c283f5b7f86179790b6c77f5fbdab5615a48aab61a04bcec5c572'
 
   # firecamp.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask
   url "https://firecamp.ams3.digitaloceanspaces.com/versions/mac/Firecamp-#{version}.dmg"
