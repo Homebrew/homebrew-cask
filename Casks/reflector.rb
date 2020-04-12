860574d6f00507e51a55596947db391842f3b3a6
Dario Vladovic <d.vladimyr@gmail.com>
2020-04-12 18:17:17 +0200

    Update reflector
    Add `zap` stanza.

--- Casks/reflector.rb
+++ Casks/reflector.rb
@@ -8,4 +8,10 @@ cask 'reflector' do
   homepage 'https://www.airsquirrels.com/reflector/'
 
   app "Reflector #{version.major}.app"
+
+  zap trash: [
+               '~/Library/Application Support/Logs/Reflector3.log*',
+               '~/Library/Caches/com.squirrels.Reflector-3',
+               '~/Library/Preferences/com.squirrels.Reflector-3.plist',
+             ]
 end
