9722ef45115b1e0b5a684af5a683ae8e84fb7fe5
suschizu <brigitte@corecode.io>
2020-04-12 19:00:49 +0200

    Update firecamp.rb

--- Casks/firecamp.rb
+++ Casks/firecamp.rb
@@ -1,5 +1,5 @@
 cask 'firecamp' do
-  version '1.2.1'
+  version '1.2.2'
   sha256 '1241c4ee06ff845cf4b2dfdeba73b77d54e01854f959f4e55d15b2f9b91caac2'
 
   # firecamp.ams3.digitaloceanspaces.com was verified as official when first introduced to the cask
