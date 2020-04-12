ec05bd21a6c8dd1e23eba067e663eebb4dd275fe
suschizu <brigitte@corecode.io>
2020-04-12 14:47:47 +0200

    Update day-o.rb

--- Casks/day-o.rb
+++ Casks/day-o.rb
@@ -1,12 +1,14 @@
 cask 'day-o' do
-  version '3.0'
-  sha256 'ace5905328106c34e488d353fb52845e807333705076bbf994175c9ffec7f108'
+  version '3.0.1'
+  sha256 '4963f503c1e47bfa0f8bdbbbe5694d6a7242d298fb44ff68af80d42f1eaebaf9'
 
-  url "https://shauninman.com/assets/downloads/Day-#{version}.zip"
+  url "https://shauninman.com/assets/downloads/Day-#{version.major_minor}.zip"
   name 'Day-O'
   homepage 'https://shauninman.com/archive/2020/04/08/day_o_mac_menu_bar_clock_for_catalina'
 
-  app "Day-#{version}/Day-O.app"
+  depends_on macos: '>= :catalina'
+
+  app "Day-#{version.major_minor}/Day-O.app"
 
   zap trash: '~/Library/Preferences/com.shauninman.Day-O.plist'
 end
