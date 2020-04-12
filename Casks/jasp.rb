34fb69eeaf19488e06eff8a4c70ce095602b093d
Vítor Galvão <info@vitorgalvao.com>
2020-04-12 18:39:56 +0100

    Update jasp.rb

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -4,7 +4,7 @@ cask 'jasp' do
 
   url "https://static.jasp-stats.org/JASP-#{version}.dmg"
   appcast 'https://jasp-stats.org/download/',
-          configuration: version.chomp('.0').chomp('.0')
+          configuration: version.sub(%r{(.0)+$}, '')
   name 'JASP'
   homepage 'https://jasp-stats.org/'
 
