6328018adb4544ef45d0fa084f582e4c281dd8a7
CoreCode <core-code@users.noreply.github.com>
2020-04-12 21:32:42 +0200

    Update tiger-trade.rb

--- Casks/tiger-trade.rb
+++ Casks/tiger-trade.rb
@@ -4,10 +4,10 @@ cask 'tiger-trade' do
 
   # s.tigerfintech.com was verified as official when first introduced to the cask
   url "https://s.tigerfintech.com/desktop/cdn/f/TigerTrade_#{version.before_comma}_#{version.after_comma.before_colon}_#{version.after_colon}.dmg"
-  appcast 'https://www.itiger.com/download/mac'
+  appcast 'https://up.play-analytics.com/app/upgrade/latest?lang=zh_CN&platform=darwin&appVer=1'
   name 'Tiger Trade'
   name '老虎证券'
-  homepage 'https://www.itiger.com/'
+  homepage 'https://www.itiger.com/download/mac'
 
   depends_on macos: '>= :sierra'
 
