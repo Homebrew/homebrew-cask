12b7b98f62108153280ee9787709d8767e3b4f98
CoreCode <core-code@users.noreply.github.com>
2020-04-12 18:07:32 +0200

    Update screenflick.rb

--- Casks/screenflick.rb
+++ Casks/screenflick.rb
@@ -3,7 +3,8 @@ cask 'screenflick' do
   sha256 '76bceb692c9910c8213b0b5c554e7ca3ec215c7e9860176c56b6ef746ccf1c4a'
 
   url "https://store.araelium.com/screenflick/downloads/versions/Screenflick#{version}.zip"
-  appcast "https://arweb-assets.s3.amazonaws.com/downloads/screenflick/screenflick#{version.major}.xml"
+  appcast 'https://arweb-assets.s3.amazonaws.com/downloads/screenflick/updates.json'
+ 
   name 'Screenflick'
   homepage 'https://www.araelium.com/screenflick/'
 
