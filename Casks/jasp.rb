9fd6d2ef031b01078a97525efc46ebcd4f99482f
CoreCode <core-code@users.noreply.github.com>
2020-04-12 21:35:50 +0200

    Update jasp (#80395)
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -3,7 +3,8 @@ cask 'jasp' do
   sha256 '287ba794734f602a5b9e564356f9d92b09ba0ec1d7f180b71cae2e60d369190a'
 
   url "https://static.jasp-stats.org/JASP-#{version}.dmg"
-  appcast 'https://jasp-stats.org/download/'
+  appcast 'https://jasp-stats.org/download/',
+          configuration: version.sub(%r{(.0)+$}, '')
   name 'JASP'
   homepage 'https://jasp-stats.org/'
 
