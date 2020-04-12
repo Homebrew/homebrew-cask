ccd104fa0d950af81c74b3a77963758acf7ce72c
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-12 12:37:18 +0000

    Create flashprint.rb

---
+++ Casks/flashprint.rb
@@ -0,0 +1,16 @@
+cask 'flashprint' do
+  version '4.2.0'
+  sha256 '8abb4c69f32b669994b7695f981c27aad8f3da5cbf0d02272016f9483c793d33'
+
+  url "http://en.fss.flashforge.com/10000/software/bb8cad0ffed6160ae46a0dcbf032145c.zip"
+  name 'FlashPrint'
+  homepage 'https://www.flashforge.com/software/detail/Flashprint?id=40'
+
+  auto_updates true
+
+  app 'FlashPrint.app'
+
+  zap trash: [
+               '/Users/khushrajrathod/Library/Preferences/FlashForge.FlashPrint.plist',
+             ]
+end
