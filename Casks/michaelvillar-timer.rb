facf42a286bbdb12f07f00666156fc8c6c96f185
Ali Karbassi <ali@karbassi.com>
2020-04-12 22:04:21 -0500

    Update michaelvillar-timer from 1.5.1 to 1.5.2 (#80568)

--- Casks/michaelvillar-timer.rb
+++ Casks/michaelvillar-timer.rb
@@ -1,6 +1,6 @@
 cask 'michaelvillar-timer' do
-  version '1.5.1'
-  sha256 'c5f1bbbad6df63152cc0955a6bdc1c19f6caa570fd517a742a75e5453f01d9a3'
+  version '1.5.2'
+  sha256 'f7adbf0f04ecdfb6cf914774859bc187d2ae944a51b748ddc82de4ecd34e9252'
 
   url "https://github.com/michaelvillar/timer-app/releases/download/#{version}/Timer.app.zip"
   appcast 'https://github.com/michaelvillar/timer-app/releases.atom'
