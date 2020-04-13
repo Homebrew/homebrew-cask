ff8bb48f27f5daa21a6e7d06e48ae6027661a141
Randall <ran.dall@icloud.com>
2020-04-13 15:13:03 -0700

    Update cabal from 5.0.0 to 5.0.1

--- Casks/cabal.rb
+++ Casks/cabal.rb
@@ -1,6 +1,6 @@
 cask 'cabal' do
-  version '5.0.0'
-  sha256 '897ddafb13ca78488490ac572b2da0400a5084d5a4a19efa21e22e82526c02ef'
+  version '5.0.1'
+  sha256 '0ca563c84fb7536f0d0f8eb2011a2175d68183cb89d139573a5004d2c0e60075'
 
   # github.com/cabal-club/cabal-desktop was verified as official when first introduced to the cask
   url "https://github.com/cabal-club/cabal-desktop/releases/download/v#{version}/cabal-desktop-#{version}-mac.dmg"
