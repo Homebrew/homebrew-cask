854c41698c727d818ba45e16cf93e539f2b6dca2
gottabeleGIT <sandesh.manik@icloud.com>
2020-04-13 21:33:30 +0000

    Update fanny to v2.2.1 (#80601)
    * Update sha256 value
    
    * Upgrade to v2.2.1

--- Casks/fanny.rb
+++ Casks/fanny.rb
@@ -1,6 +1,6 @@
 cask 'fanny' do
-  version '2.2.0'
-  sha256 '5fac18409b5f3054446b3d09e93a56658686ebff670bb70afcd1121d3ca6e369'
+  version '2.2.1'
+  sha256 '0fc8c2bce6b80eeb766cfc2fdf503fcb6d1d16e5680a763d040c7059453aea8c'
 
   url 'https://fannywidget.com/FannyWidget.zip'
   appcast 'https://github.com/DanielStormApps/Fanny/releases.atom'
