6fff2637d69f73d2d4d2bd2ce47facb1372ccbf6
Work <brigitte@corecode.io>
2020-04-13 13:22:54 +0200

    Update unity-lumin-support-for-editor from 2019.3.8f1,4ba98e9386ed to 2019.3.9f1,e6e740a1c473

--- Casks/unity-lumin-support-for-editor.rb
+++ Casks/unity-lumin-support-for-editor.rb
@@ -1,6 +1,6 @@
 cask 'unity-lumin-support-for-editor' do
-  version '2019.3.8f1,4ba98e9386ed'
-  sha256 '4b67c1b4eb2b2b9af31f27bacfbf1ce9f5ce8b86211c53baf330212895113551'
+  version '2019.3.9f1,e6e740a1c473'
+  sha256 '04560d0e9d1ef058eeb5cd89fb2302a0166e6ac760ea63e676ae13cd648152e8'
 
   url "https://download.unity3d.com/download_unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Lumin-Support-for-Editor-#{version.before_comma}.pkg"
   appcast 'https://public-cdn.cloud.unity3d.com/hub/prod/releases-darwin.json'
