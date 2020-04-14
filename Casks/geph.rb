6ca2b19133be36a04f6b1b613dd59caf5f71a597
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-13 17:20:02 -0700

    Update geph from 3.5.4 to 3.6.0 (#80614)

--- Casks/geph.rb
+++ Casks/geph.rb
@@ -1,6 +1,6 @@
 cask 'geph' do
-  version '3.5.4'
-  sha256 'f4ef8fe1bc5e194ad34ecd917f7e6f4a7229d89fd181342561135d55fd71e145'
+  version '3.6.0'
+  sha256 '2490f4a82ca7233b9f43a238338dd94a86c7a22a91b1bddb8aaac629fc71d57d'
 
   url "https://dl.geph.io/desktop-builds/geph-macos-#{version}.dmg"
   appcast 'https://geph.io/'
