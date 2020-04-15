0f350177cdbe61c294d67c81bc6ded971c8b3693
Gerrit Beine <mail@gerritbeine.com>
2020-04-15 19:36:18 +0200

    Update netdownloadhelper to v1.5.0

--- Casks/netdownloadhelpercoapp.rb
+++ Casks/netdownloadhelpercoapp.rb
@@ -1,6 +1,6 @@
 cask 'netdownloadhelpercoapp' do
-  version '1.4.0'
-  sha256 'e7e1cf0ece226bc725545b5283c6bc1359f9c2bbc9827f31bdafcfa81d98f3c3'
+  version '1.5.0'
+  sha256 '354b302206da8bda19a006237f3fd609bec79ccca6380ec994f0b9b482571f4b'
 
   # github.com/mi-g/vdhcoapp was verified as official when first introduced to the cask
   url "https://github.com/mi-g/vdhcoapp/releases/download/v#{version}/net.downloadhelper.coapp-#{version}.pkg"
