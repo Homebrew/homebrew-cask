6b5a12b84b29b054422969af5cbe20fe68fb3e30
Nathanaël Deng <36327760+jaxdeu@users.noreply.github.com>
2020-04-15 22:11:46 +0200

    Add Markdown.app v1.3.9

---
+++ Casks/markdown.rb
@@ -0,0 +1,10 @@
+cask 'markdown' do
+  version '1.3.9'
+  sha256 'f17be776385945bc34a3ebdbd0eca51ae862074329842f4b4ea607c01ec155cf'
+
+  url "http://qdownload.quanduan.com/markdown_1.3.9.dmg"
+  name 'Markdown.app'
+  homepage 'https://www.markdown.app/'
+
+  app 'Markdown.app'
+end
