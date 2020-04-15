81e56703ff0187ff0b5721ca0f32931203ed49af
suschizu <brigitte@corecode.io>
2020-04-15 18:18:58 +0200

    Update scrutiny from 9.6.1 to 9.6.2 (#80726)

--- Casks/scrutiny.rb
+++ Casks/scrutiny.rb
@@ -1,6 +1,6 @@
 cask 'scrutiny' do
-  version '9.6.1'
-  sha256 '52381043281abcb2842d7ecefb766918818ba804c9ea0cadf1006fb2335f81ca'
+  version '9.6.2'
+  sha256 '18047fcbf9110e63dd9e5feb0b455b491dbd1fc37246a43cc735d461f8297e6d'
 
   url 'https://peacockmedia.software/mac/scrutiny/scrutiny.dmg'
   appcast 'https://peacockmedia.software/mac/scrutiny/version_history.html'
