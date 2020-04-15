a2b9d2de47b8f3a23d828f6b7aa0da4eaade7855
Loïc PÉRON <44115467+nicerloop@users.noreply.github.com>
2020-04-16 01:04:13 +0200

    Update lastpass from 4.45.0 to 4.46.0 (#80737)

--- Casks/lastpass.rb
+++ Casks/lastpass.rb
@@ -1,6 +1,6 @@
 cask 'lastpass' do
-  version '4.45.0'
-  sha256 'b76b5dabea61d063936193e915c06ba85a24c5a4310bd0756183e4ea584e7ed0'
+  version '4.46.0'
+  sha256 '9a5c512159b50cc3b154522dfadc14faa90b6a50eee1955611d3cf282ab66a32'
 
   url 'https://download.cloud.lastpass.com/mac/LastPass.dmg'
   appcast 'https://download.cloud.lastpass.com/mac/AppCast.xml'
