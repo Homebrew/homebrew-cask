2776bbabbbdf2ea59fe2465b19783c4226e45047
Vítor Galvão <info@vitorgalvao.com>
2020-04-15 17:40:26 +0100

    Update netdownloadhelpercoapp.rb

--- Casks/netdownloadhelpercoapp.rb
+++ Casks/netdownloadhelpercoapp.rb
@@ -2,7 +2,7 @@ cask 'netdownloadhelpercoapp' do
   version '1.4.0'
   sha256 'e7e1cf0ece226bc725545b5283c6bc1359f9c2bbc9827f31bdafcfa81d98f3c3'
 
-  # mi-g/vdhcoapp was verified as official when first introduced to the cask
+  # github.com/mi-g/vdhcoapp was verified as official when first introduced to the cask
   url "https://github.com/mi-g/vdhcoapp/releases/download/v#{version}/net.downloadhelper.coapp-#{version}.pkg"
   appcast 'https://github.com/mi-g/vdhcoapp/releases.atom'
   name 'Video DownloadHelper Companion App'
