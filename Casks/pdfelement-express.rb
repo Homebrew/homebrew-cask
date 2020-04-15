fe0972e9a2b0b5b6e99ecb182b96e605a8a50fcb
suschizu <brigitte@corecode.io>
2020-04-15 17:45:59 +0200

    Update pdfelement-express.rb

--- Casks/pdfelement-express.rb
+++ Casks/pdfelement-express.rb
@@ -4,7 +4,7 @@ cask 'pdfelement-express' do
 
   url 'http://download.wondershare.com/cbs_down/mac-pdfelement-express_full4133.dmg'
   appcast 'https://cbs.wondershare.com/go.php?m=upgrade_info&pid=4133'
-  name 'Wondershare PDFelement Express for Mac'
+  name 'PDFelement Express'
   homepage 'https://pdf.wondershare.com/pdfelement-express-mac.html'
 
   depends_on macos: '>= :sierra'
