20b9c7123a50f59f010b42e14234321c2af56a3e
Reno <25192197+singularitti@users.noreply.github.com>
2020-04-15 05:18:54 -0400

    Update understand from 5.1.1022 to 5.1.1023 (#80706)

--- Casks/understand.rb
+++ Casks/understand.rb
@@ -1,6 +1,6 @@
 cask 'understand' do
-  version '5.1.1022'
-  sha256 'c4ac47d4a8da7cfafc41c6ca65e02b33e63e906b0c7a65376c9c29c5ae928939'
+  version '5.1.1023'
+  sha256 'a550b8d5a5c06794a619e98b6c43b65a1c7c7ad6ece10f24a2979b96457df173'
 
   url "http://builds.scitools.com/all_builds/b#{version.patch}/Understand/Understand-#{version}-MacOSX-x86.dmg"
   appcast 'https://scitools.com/download/all-builds/'
