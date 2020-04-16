cc4c30c19841db8afeb35f1176c2e2e5f5dfa055
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-16 12:06:08 -0700

    Update xact from 2.48 to 2.49 (#80810)

--- Casks/xact.rb
+++ Casks/xact.rb
@@ -1,6 +1,6 @@
 cask 'xact' do
-  version '2.48'
-  sha256 'c42998dfc1c5fcfec5c81cf3a8715e257aa23232324e102c3d6a644b2188ae0c'
+  version '2.49'
+  sha256 'b54d15865424964670720b58d8b3a68a51d536ad9f0b286dae99668b94811c00'
 
   url "http://xact.scottcbrown.org/xACT#{version}.zip"
   appcast 'http://xactupdate.scottcbrown.org/xACT.xml'
