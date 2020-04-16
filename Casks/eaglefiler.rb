33c43553ac5aaf9b3ee801808988f60386caf907
Sebastian Riehm <hallo@herrriehm.de>
2020-04-16 20:40:52 +0200

    Update eaglefiler from 1.8.12 to 1.8.13

--- Casks/eaglefiler.rb
+++ Casks/eaglefiler.rb
@@ -1,6 +1,6 @@
 cask 'eaglefiler' do
-  version '1.8.12'
-  sha256 '350337cc33a77e051ad6bb803eb34ea86681b17dfb968f6a322506b72918c6db'
+  version '1.8.13'
+  sha256 '57bc50bac596a1265afe799daecb698aa8a3c121ccc2965db85e46b839da19fd'
 
   url "https://c-command.com/downloads/EagleFiler-#{version}.dmg"
   appcast 'https://c-command.com/eaglefiler/help/version-history'
