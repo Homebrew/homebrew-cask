f3315cc73f2d62e5af6f3826353aa78efeefcc3e
Vítor Galvão <info@vitorgalvao.com>
2020-04-16 23:10:03 +0100

    Update mimiq.rb

--- Casks/mimiq.rb
+++ Casks/mimiq.rb
@@ -2,7 +2,7 @@ cask 'mimiq' do
   version '0.0.2'
   sha256 'e53b51bf701a33ef4461d89238cdc0bca70ede27086bc57ba57ac41c8a3b05ff'
 
-  url 'https://github.com/wendyliga/mimiq-gui/releases/download/0.0.2/mimiq.zip'
+  url "https://github.com/wendyliga/mimiq-gui/releases/download/#{version}/mimiq.zip"
   appcast 'http://mimiq.wendyliga.com/appcast.xml'
   name 'mimiq'
   homepage 'https://github.com/wendyliga/mimiq-gui'
