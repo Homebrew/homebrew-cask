807f11a1ea6edfc4314da3c58863de0ee7f94c8e
Randall <ran.dall@icloud.com>
2020-04-15 18:57:48 -0700

    Update navicat-premium from 15.0.12 to 15.0.14

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,6 +1,6 @@
 cask 'navicat-premium' do
-  version '15.0.12'
-  sha256 'ffaecaa278ea3cc6ffe6c78cfce4ca610f055dadf8f129c13a8c916a87296dc2'
+  version '15.0.14'
+  sha256 'e801355513419d1075a254d517093ec75a6146ff2a7d80310c40b7efbc203cb1'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
   appcast 'https://updater.navicat.com/mac/navicat_updates.php?appName=Navicat%20Premium&appLang=en'
