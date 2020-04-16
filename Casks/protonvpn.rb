69bf158e440c992571a5548ed3c0b6d1a3bf2275
Yurii Kolesnykov <root@yurikoles.com>
2020-04-16 14:37:58 +0300

    Update protonvpn from 1.5.9 to 1.5.10

--- Casks/protonvpn.rb
+++ Casks/protonvpn.rb
@@ -1,6 +1,6 @@
 cask 'protonvpn' do
-  version '1.5.9'
-  sha256 '14cf6425e7fc5062e5f6fa05049759bdfce8d154d41d56eba2ae9cddebd82493'
+  version '1.5.10'
+  sha256 '466bd3e803e80dae3822ef03e4609a6fff061526818ded406994a0c62bff7d73'
 
   url "https://protonvpn.com/download/ProtonVPN_mac_v#{version}.dmg"
   appcast 'https://protonvpn.com/download/macos-update2.xml'
