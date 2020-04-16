45fc96f89ae3f6f24544272f9c18e0a80ce18cad
Lance Whatley <whatl3y@gmail.com>
2020-04-16 08:51:48 -0400

    Fix documentation gap with adding cask with different URL
    https://github.com/Homebrew/homebrew-cask/blob/master/doc/cask_language_reference/stanzas/url.md#when-url-and-homepage-hostnames-differ-add-a-comment

--- Casks/uberconference.rb
+++ Casks/uberconference.rb
@@ -2,7 +2,8 @@ cask 'uberconference' do
   version :latest
   sha256 :no_check
 
-  url "https://storage.googleapis.com/uc_native/osx/UberConference.dmg"
+  # storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://storage.googleapis.com/uc_native/osx/UberConference.dmg'
   name 'UberConference'
   homepage 'https://www.uberconference.com/'
 
