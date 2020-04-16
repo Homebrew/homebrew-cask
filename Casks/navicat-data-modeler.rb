bd76fb51a871f8a417a033f8218c6906fa0c962b
Randall <ran.dall@icloud.com>
2020-04-15 18:52:18 -0700

    Update navicat-data-modeler from 3.0.7 to 3.0.8

--- Casks/navicat-data-modeler.rb
+++ Casks/navicat-data-modeler.rb
@@ -1,6 +1,6 @@
 cask 'navicat-data-modeler' do
-  version '3.0.7'
-  sha256 '59752dd1352c2100fe20b04a47152047b2c7876f8b20ad9f1dcbe3b2f843b6c2'
+  version '3.0.8'
+  sha256 'b1c5510143dad039bafd7f7024e65575531942cf1b88bef7ed956023aa08f43a'
 
   url "http://download.navicat.com/download/modeler0#{version.major_minor.no_dots}_en.dmg"
   appcast 'https://navicat.com/en/products/navicat-data-modeler-release-note'
