39dc2d9170fdc492ebf1a160f75b0fbed74dc7b3
Austin Kootz <austin.kootz@live.com>
2020-04-16 21:18:55 -0600

    update to 2.20.2 (#80838)

--- Casks/pyfa.rb
+++ Casks/pyfa.rb
@@ -1,6 +1,6 @@
 cask 'pyfa' do
-  version '2.20.1'
-  sha256 '1d503ec0c0f735c522a3e41a1a8c74c4e57548bbff0440678c399a3f696c495f'
+  version '2.20.2'
+  sha256 '2d5931311421751833d129c7552fb0b9667c7f1504f9407180b553e647177951'
 
   url "https://github.com/pyfa-org/Pyfa/releases/download/v#{version}/pyfa-v#{version}-mac.zip"
   appcast 'https://github.com/pyfa-org/Pyfa/releases.atom'
