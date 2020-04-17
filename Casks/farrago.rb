8a149c94f85ad05a6a57b3a2f89c027da4610df6
Chris Finazzo <chris.finazzo@gmail.com>
2020-04-17 12:05:06 -0400

    Update Farrago to 1.5.0 (#80863)

--- Casks/farrago.rb
+++ Casks/farrago.rb
@@ -1,6 +1,6 @@
 cask 'farrago' do
-  version '1.2.7'
-  sha256 'aadf04c3a13c43bbaf3007ef93606eb955d5487a06e3fe9cda30f9b4c8890a98'
+  version '1.5.0'
+  sha256 'f7e6320583e697e1c561c5880658409cbc1e87a24d0e775c0fa180902301ce5d'
 
   url 'https://rogueamoeba.com/farrago/download/Farrago.zip'
   appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.farrago&system=10140'
