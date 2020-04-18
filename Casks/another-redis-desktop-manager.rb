e8aef594f31fe14470c87d8a53c17c1070720552
Randall <ran.dall@icloud.com>
2020-04-18 11:59:24 -0700

    Update another-redis-desktop-manager from 1.3.3 to 1.3.4

--- Casks/another-redis-desktop-manager.rb
+++ Casks/another-redis-desktop-manager.rb
@@ -1,6 +1,6 @@
 cask 'another-redis-desktop-manager' do
-  version '1.3.3'
-  sha256 '19c7d5639611c8e988df1c114453b06c0bd071ef2593a51443fba3c64ed6db07'
+  version '1.3.4'
+  sha256 'b50b0afdabc280c31f5aea81a0b439e9dbea9741a64f3579a44026272566a703'
 
   url "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v#{version}/Another.Redis.Desktop.Manager.#{version}.dmg"
   appcast 'https://github.com/qishibo/AnotherRedisDesktopManager/releases.atom'
