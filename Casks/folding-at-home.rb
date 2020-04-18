383ba4a1493fa04713a989bc2268b129196f9c5b
CoreCode <core-code@users.noreply.github.com>
2020-04-18 15:22:12 +0200

    Update folding-at-home.rb

--- Casks/folding-at-home.rb
+++ Casks/folding-at-home.rb
@@ -2,9 +2,8 @@ cask 'folding-at-home' do
   version '7.6.9'
   sha256 '9475e8aef3614daf92e3cfc8748be5e10e1c5b38a5c5da30ce3fe60f754de16b'
 
-  # download.foldingathome.org/releases/public/release/fah-installer was verified as official when first introduced to the cask
   url "https://download.foldingathome.org/releases/public/release/fah-installer/osx-10.11-64bit/v#{version.major_minor}/fah-installer_#{version}_x86_64.mpkg.zip"
-  appcast 'https://foldingathome.org/alternative-downloads/'
+  appcast 'https://download.foldingathome.org/js/fah-downloads.js'
   name 'Folding@home'
   homepage 'https://foldingathome.org/'
 
