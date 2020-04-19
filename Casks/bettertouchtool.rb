e7829eacd0a82d93bd8875be326959c8e20f81ba
i0ntempest <szf1234@me.com>
2020-04-19 12:17:26 -0400

    Update bettertouchtool from 3.346-1554 to 3.362-1578

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -6,8 +6,8 @@ cask 'bettertouchtool' do
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}_final_10_9.zip"
   else
-    version '3.346-1554'
-    sha256 '244e79c0aa4a0e25bd02f13058be8788c0b117f0c5b0770a17746fa8d580790b'
+    version '3.362-1578'
+    sha256 '3a50e64d2229b2eaf9487f60955a8716fd042a6c5f914f90b2dd0d339e4294be'
 
     # bettertouchtool.net/releases was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}.zip"
