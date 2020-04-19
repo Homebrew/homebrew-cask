6d0371671a566c33bbf6796f74cf686d0a2aab02
Vítor Galvão <info@vitorgalvao.com>
2020-04-19 15:45:45 +0100

    Update redeclipse.rb

--- Casks/redeclipse.rb
+++ Casks/redeclipse.rb
@@ -3,7 +3,7 @@ cask 'redeclipse' do
   sha256 '7eff1f196f9998d7bb7f26b4c79b596c2512b18a8b8ce16335634defbfcb072e'
 
   # github.com was verified as official when first introduced to the cask
-  url 'https://github.com/redeclipse/base/releases/download/v2.0.0/redeclipse_2.0.0_mac.tar.bz2'
+  url "https://github.com/redeclipse/base/releases/download/v#{version}/redeclipse_#{version}_mac.tar.bz2"
   appcast 'https://github.com/redeclipse/base/releases.atom'
   name 'Red Eclipse'
   homepage 'https://www.redeclipse.net/'
