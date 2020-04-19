0f265239920b0e0288da88d24f3c36506e3374c8
Vítor Galvão <info@vitorgalvao.com>
2020-04-19 23:03:29 +0100

    Update stand.rb

--- Casks/stand.rb
+++ Casks/stand.rb
@@ -2,7 +2,7 @@ cask 'stand' do
   version '2.0'
   sha256 'eb0555f6376757105e1faea1fd1f9e4afae21eef73fdd5c7d0fb0c76a8cd9702'
 
-  # f001.backblazeb2.com/file/stand-app was verified as official when first introduced to the cask
+  # f001.backblazeb2.com/file/stand-app/ was verified as official when first introduced to the cask
   url "https://f001.backblazeb2.com/file/stand-app/#{version}/Stand.zip"
   appcast 'https://standapp-sparkle-updater.herokuapp.com/'
   name 'Stand'
