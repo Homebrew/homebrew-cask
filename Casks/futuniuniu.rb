1bcaf5dd0c67543597ebc98b2b89886ae3020a1a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:40:32 +0100

    futuniuniu.rb: Add trailing slash to verified comment

--- Casks/futuniuniu.rb
+++ Casks/futuniuniu.rb
@@ -2,7 +2,7 @@ cask 'futuniuniu' do
   version '10.4.642_202004131013'
   sha256 'f49aa27c4bdd0ae7f395e04e3539c713b97de871a0bd089eafd870d3ac5804a6'
 
-  # software-file-1251001049.file.myqcloud.com was verified as official when first introduced to the cask
+  # software-file-1251001049.file.myqcloud.com/ was verified as official when first introduced to the cask
   url "https://software-file-1251001049.file.myqcloud.com/FTNNForMac_#{version}_website.dmg"
   appcast 'https://www.futu5.com/tools'
   name 'FutuNiuniu'
