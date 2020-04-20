ee749528228c197a817a26775ac67d62299a09c8
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:49:33 +0100

    gitup.rb: Add trailing slash to verified comment

--- Casks/gitup.rb
+++ Casks/gitup.rb
@@ -2,7 +2,7 @@ cask 'gitup' do
   version '1.1.2'
   sha256 '78b10d8e489d7ff0ddd5f7c3bbbdcbecf4c9896619eaf6a3a268e9debef28203'
 
-  # gitup-builds.s3.amazonaws.com was verified as official when first introduced to the cask
+  # gitup-builds.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://gitup-builds.s3.amazonaws.com/stable/GitUp.zip'
   appcast 'https://github.com/git-up/GitUp/releases.atom'
   name 'GitUp'
