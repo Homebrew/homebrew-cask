1e78580e54512534606011ddc0b2153249302e77
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:39 +0100

    bitshares.rb: Add trailing slash to verified comment

--- Casks/bitshares.rb
+++ Casks/bitshares.rb
@@ -2,7 +2,7 @@ cask 'bitshares' do
   version '3.3.191120'
   sha256 '27553d1038f6c301b7bcdc4f8a644e871722024d24585c6b847c868962a967aa'
 
-  # github.com/bitshares/bitshares-ui was verified as official when first introduced to the cask
+  # github.com/bitshares/bitshares-ui/ was verified as official when first introduced to the cask
   url "https://github.com/bitshares/bitshares-ui/releases/download/#{version}/BitShares-#{version}-mac.zip"
   appcast 'https://github.com/bitshares/bitshares-ui/releases.atom'
   name 'BitShares'
