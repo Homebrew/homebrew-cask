00a349002dd75fd7aa49f2c694b7eb0f3a7baef9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:36:25 +0100

    zoho-mail.rb: Add trailing slash to verified comment

--- Casks/zoho-mail.rb
+++ Casks/zoho-mail.rb
@@ -2,7 +2,7 @@ cask 'zoho-mail' do
   version '1.1.6'
   sha256 '966d7d372fc8a4857072fc77079c59fe81d0ab9113c25b5525bf52230968dd17'
 
-  # downloads.zohocdn.com/zmail-desktop/mac was verified as official when first introduced to the cask
+  # downloads.zohocdn.com/zmail-desktop/mac/ was verified as official when first introduced to the cask
   url "https://downloads.zohocdn.com/zmail-desktop/mac/zoho-mail-desktop-installer-v#{version}.dmg"
   appcast 'https://downloads.zohocdn.com/zmail-desktop/artifacts.json'
   name 'Zoho Mail'
