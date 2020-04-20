a5523103a9a7ada328dd9ec962080dab8a26f1cc
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:19:29 +0100

    sigil.rb: Add trailing slash to verified comment

--- Casks/sigil.rb
+++ Casks/sigil.rb
@@ -2,7 +2,7 @@ cask 'sigil' do
   version '1.2.0'
   sha256 '813feec7f8e0b1aace2dacc68e3452ca795b082aa6d11aae0d45e9c20d0ab469'
 
-  # github.com/Sigil-Ebook/Sigil was verified as official when first introduced to the cask
+  # github.com/Sigil-Ebook/Sigil/ was verified as official when first introduced to the cask
   url "https://github.com/Sigil-Ebook/Sigil/releases/download/#{version}/Sigil.app-#{version}-Mac.txz"
   appcast 'https://github.com/Sigil-Ebook/Sigil/releases.atom'
   name 'Sigil'
