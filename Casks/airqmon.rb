f2b801fba88f45922e2812022c77d47e6824c67e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:26:42 +0100

    airqmon.rb: Add trailing slash to verified comment

--- Casks/airqmon.rb
+++ Casks/airqmon.rb
@@ -2,7 +2,7 @@ cask 'airqmon' do
   version '2.0.0'
   sha256 'a516116bb2b2858b435240aa4d946c6aab0aa1066a31c38293402784950c99e5'
 
-  # github.com/jsynowiec/airqmon was verified as official when first introduced to the cask
+  # github.com/jsynowiec/airqmon/ was verified as official when first introduced to the cask
   url "https://github.com/jsynowiec/airqmon/releases/download/v#{version}/airqmon-mac.zip"
   appcast 'https://github.com/jsynowiec/airqmon/releases.atom'
   name 'Airqmon'
