79c24ea6405e493e3ce4c4719cba6af027b5f816
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:57:05 +0100

    listen1.rb: Add trailing slash to verified comment

--- Casks/listen1.rb
+++ Casks/listen1.rb
@@ -3,7 +3,7 @@ cask 'listen1' do
   version '2.5.1'
   sha256 '9d4e32ebfd9f3698d4d10cbdef13a5f6453cffabd7e20bfee47a7b0d49a88337'
 
-  # github.com/listen1/listen1_desktop was verified as official when first introduced to the cask
+  # github.com/listen1/listen1_desktop/ was verified as official when first introduced to the cask
   url "https://github.com/listen1/listen1_desktop/releases/download/v#{version}/Listen1_#{version}_mac.dmg"
   appcast 'https://github.com/listen1/listen1_desktop/releases.atom'
   name 'Listen 1'
