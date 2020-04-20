105f47a8813655c2ffc0bd7a1214fc0634f879b5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:52:04 +0100

    fontgoggles.rb: Add trailing slash to verified comment

--- Casks/fontgoggles.rb
+++ Casks/fontgoggles.rb
@@ -2,7 +2,7 @@ cask 'fontgoggles' do
   version '1.1.13'
   sha256 'b888dd0ee4330ef2b58fa6919202fab66aa26608cdcc95595c029ee6430e224d'
 
-  # github.com/justvanrossum/fontgoggles was verified as official when first introduced to the cask
+  # github.com/justvanrossum/fontgoggles/ was verified as official when first introduced to the cask
   url "https://github.com/justvanrossum/fontgoggles/releases/download/v#{version}/FontGoggles.dmg"
   appcast 'https://github.com/justvanrossum/fontgoggles/releases.atom'
   name 'FontGoggles'
