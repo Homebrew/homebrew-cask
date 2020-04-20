0fcffb437d551962471a1142de18bc939837d0ba
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:18:34 +0100

    drawio.rb: Add trailing slash to verified comment

--- Casks/drawio.rb
+++ Casks/drawio.rb
@@ -2,7 +2,7 @@ cask 'drawio' do
   version '12.9.13'
   sha256 '7b28bb27f91eed6c8c731cee38d006ff5f7d9c604acfba64adc545fa74754e48'
 
-  # github.com/jgraph/drawio-desktop was verified as official when first introduced to the cask
+  # github.com/jgraph/drawio-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{version}.dmg"
   appcast 'https://github.com/jgraph/drawio-desktop/releases.atom'
   name 'draw.io Desktop'
