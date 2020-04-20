18b802025aa72d8c10ca3673a72585eba611d5a9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:48:48 +0100

    visual-studio.rb: Add trailing slash to verified comment

--- Casks/visual-studio.rb
+++ Casks/visual-studio.rb
@@ -2,7 +2,7 @@ cask 'visual-studio' do
   version '8.5.3.16'
   sha256 '497c46f015fbe6e67d4cf88ffd221b801f2bbf2421f077f47c6f76467d331baf'
 
-  # dl.xamarin.com/VsMac was verified as official when first introduced to the cask
+  # dl.xamarin.com/VsMac/ was verified as official when first introduced to the cask
   url "https://dl.xamarin.com/VsMac/VisualStudioForMac-#{version}.dmg"
   appcast 'https://docs.microsoft.com/en-us/visualstudio/releasenotes/vs2019-mac-relnotes',
           configuration: version.major_minor_patch
