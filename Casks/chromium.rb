9bca8ebdeb4b79d7ab427a58cfb55633c57407db
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:37:32 +0100

    chromium.rb: Add trailing slash to verified comment

--- Casks/chromium.rb
+++ Casks/chromium.rb
@@ -2,7 +2,7 @@ cask 'chromium' do
   version '758307'
   sha256 'd53f40ad3faca4b2551213e7f36da9a79260271e07b1c15666dc15be29960bdb'
 
-  # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac was verified as official when first introduced to the cask
+  # commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/ was verified as official when first introduced to the cask
   url "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/#{version}/chrome-mac.zip"
   appcast 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE?alt=media'
   name 'Chromium'
