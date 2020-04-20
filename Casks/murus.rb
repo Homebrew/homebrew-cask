3e0cc6afb7c06880d852bef8d685350fd3347343
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:53 +0100

    murus.rb: Add trailing slash to verified comment

--- Casks/murus.rb
+++ Casks/murus.rb
@@ -2,7 +2,7 @@ cask 'murus' do
   version '2.0.2'
   sha256 '377c5df5e0c77c04e0f52c0d75cc26b44ae3098ff0ac0cb61073b48f68e5f83b'
 
-  # github.com/TheMurusTeam/Murus was verified as official when first introduced to the cask
+  # github.com/TheMurusTeam/Murus/ was verified as official when first introduced to the cask
   url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip"
   appcast 'https://github.com/TheMurusTeam/Murus/releases.atom'
   name 'Murus Firewall'
