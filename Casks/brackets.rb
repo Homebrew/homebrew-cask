115c527937c6c7f9224df26d0fd0bab742c40f8c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:51:39 +0100

    brackets.rb: Add trailing slash to verified comment

--- Casks/brackets.rb
+++ Casks/brackets.rb
@@ -2,7 +2,7 @@ cask 'brackets' do
   version '1.14.2'
   sha256 '716be1a75099079aa1a22dedab403c9839155c4b99aab6677d8a24589f7f9a15'
 
-  # github.com/adobe/brackets was verified as official when first introduced to the cask
+  # github.com/adobe/brackets/ was verified as official when first introduced to the cask
   url "https://github.com/adobe/brackets/releases/download/release-#{version}/Brackets.Release.#{version}.dmg"
   appcast 'https://github.com/adobe/brackets/releases.atom'
   name 'Brackets'
