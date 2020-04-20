851dabb4ffe3bc871f8a2c1eca52e30229f88947
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:35:15 +0100

    corona-tracker.rb: Add trailing slash to verified comment

--- Casks/corona-tracker.rb
+++ Casks/corona-tracker.rb
@@ -2,7 +2,7 @@ cask 'corona-tracker' do
   version '1.6'
   sha256 '25de160574deb71081f37c6573f867d29db2cbfee94b2431f4a684a6ada0a49a'
 
-  # github.com/MhdHejazi/CoronaTracker was verified as official when first introduced to the cask
+  # github.com/MhdHejazi/CoronaTracker/ was verified as official when first introduced to the cask
   url "https://github.com/MhdHejazi/CoronaTracker/releases/download/v#{version}/CoronaTracker-macOS.zip"
   appcast 'https://github.com/MhdHejazi/CoronaTracker/releases.atom'
   name 'Corona Tracker'
