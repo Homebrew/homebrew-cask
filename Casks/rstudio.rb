273449a0840dfcc74441475839755277e5696fee
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:31 +0100

    rstudio.rb: Add trailing slash to verified comment

--- Casks/rstudio.rb
+++ Casks/rstudio.rb
@@ -2,7 +2,7 @@ cask 'rstudio' do
   version '1.2.5042'
   sha256 '74ea68eb92a02f6ced3172b8571a76ed3b1568668d6001d196aaafd826952746'
 
-  # rstudio.org was verified as official when first introduced to the cask
+  # rstudio.org/ was verified as official when first introduced to the cask
   url "https://download1.rstudio.org/desktop/macos/RStudio-#{version}.dmg"
   appcast 'https://www.rstudio.org/links/check_for_update?version=1.0.0&os=mac'
   name 'RStudio'
