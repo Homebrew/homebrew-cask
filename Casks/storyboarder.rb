4f9f54161fe24888fd6b69c58fbb6234a4f8cc38
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:26:35 +0100

    storyboarder.rb: Add trailing slash to verified comment

--- Casks/storyboarder.rb
+++ Casks/storyboarder.rb
@@ -2,7 +2,7 @@ cask 'storyboarder' do
   version '1.18.0'
   sha256 '857c04ba5ed7cc06e816896cfd4f7ef6d4c463b4a481af4568bfbd7337ac9d34'
 
-  # github.com/wonderunit/storyboarder was verified as official when first introduced to the cask
+  # github.com/wonderunit/storyboarder/ was verified as official when first introduced to the cask
   url "https://github.com/wonderunit/storyboarder/releases/download/v#{version}/Storyboarder-#{version}.dmg"
   appcast 'https://github.com/wonderunit/storyboarder/releases.atom'
   name 'Wonder Unit Storyboarder'
