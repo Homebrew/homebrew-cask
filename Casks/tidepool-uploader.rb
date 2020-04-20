afb59fb7142057bed2b6e37b4304454449c5d354
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:14:05 +0100

    tidepool-uploader.rb: Add trailing slash to verified comment

--- Casks/tidepool-uploader.rb
+++ Casks/tidepool-uploader.rb
@@ -2,7 +2,7 @@ cask 'tidepool-uploader' do
   version '2.28.1'
   sha256 '7f7b0642a8b0e72d1931409c63cec0af8e15f88d9e1aabbff240c31fed6b720a'
 
-  # github.com/tidepool-org/chrome-uploader was verified as official when first introduced to the cask
+  # github.com/tidepool-org/chrome-uploader/ was verified as official when first introduced to the cask
   url "https://github.com/tidepool-org/chrome-uploader/releases/download/v#{version}/tidepool-uploader-#{version}.dmg/"
   appcast 'https://github.com/tidepool-org/chrome-uploader/releases.atom'
   name 'Tidepool Uploader'
