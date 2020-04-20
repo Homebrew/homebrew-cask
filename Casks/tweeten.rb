e11a34c8b45a5a10ebbf84e71b50f23198c346bb
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:00:17 +0100

    tweeten.rb: Add trailing slash to verified comment

--- Casks/tweeten.rb
+++ Casks/tweeten.rb
@@ -2,7 +2,7 @@ cask 'tweeten' do
   version '4.7.0'
   sha256 '0feb238d2d1ab57ba3bae2949ffb4b3dc7e51f78257dbdde3d00654acb8e1c0b'
 
-  # github.com/MehediH/Tweeten was verified as official when first introduced to the cask
+  # github.com/MehediH/Tweeten/ was verified as official when first introduced to the cask
   url "https://github.com/MehediH/Tweeten/releases/download/v#{version}/tweeten-darwin-x64.zip"
   appcast 'https://github.com/MehediH/Tweeten/releases.atom'
   name 'Tweeten'
