e6fdfda569bdaa896caf8f5a9e7542c775476178
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:59:26 +0100

    renamer.rb: Add trailing slash to verified comment

--- Casks/renamer.rb
+++ Casks/renamer.rb
@@ -2,7 +2,7 @@ cask 'renamer' do
   version '6.0.5'
   sha256 'fbe82a98acfafa5bd2841a1ffe0c9064edc3504abe07e67363e68c20513b1609'
 
-  # storage.googleapis.com/incrediblebee was verified as official when first introduced to the cask
+  # storage.googleapis.com/incrediblebee/ was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/incrediblebee/apps/Renamer-#{version.major}/Renamer-#{version}.zip"
   appcast "https://api.incrediblebee.com/appcasts/renamer-#{version.major}.xml"
   name 'Renamer'
