0ebfb57313e05d258871637e4ccde7bd04e8b1d4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:33:12 +0100

    opensesame.rb: Add trailing slash to verified comment

--- Casks/opensesame.rb
+++ Casks/opensesame.rb
@@ -2,7 +2,7 @@ cask 'opensesame' do
   version '3.2.8'
   sha256 '1f660ce97518c904153ce8a50a57102166c8991f3ed65fb24f87ff69fb2cc0e8'
 
-  # github.com/smathot/OpenSesame was verified as official when first introduced to the cask
+  # github.com/smathot/OpenSesame/ was verified as official when first introduced to the cask
   url "https://github.com/smathot/OpenSesame/releases/download/release/#{version}/opensesame_#{version}-py2.7-macos-1.dmg"
   appcast 'https://github.com/smathot/OpenSesame/releases.atom'
   name 'OpenSesame'
