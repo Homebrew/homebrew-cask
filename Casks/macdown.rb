29e4bca1b7ee25b7dbe4ff696ba6bb4936e020a4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:13 +0100

    macdown.rb: Add trailing slash to verified comment

--- Casks/macdown.rb
+++ Casks/macdown.rb
@@ -2,7 +2,7 @@ cask 'macdown' do
   version '0.7.2'
   sha256 '271f11eb64c19fccee2615e092067cdecc29adf0c2ed0703dae9acda8fa0a672'
 
-  # github.com/MacDownApp/macdown was verified as official when first introduced to the cask
+  # github.com/MacDownApp/macdown/ was verified as official when first introduced to the cask
   url "https://github.com/MacDownApp/macdown/releases/download/v#{version}/MacDown.app.zip"
   appcast 'https://macdown.uranusjr.com/sparkle/macdown/stable/appcast.xml'
   name 'MacDown'
