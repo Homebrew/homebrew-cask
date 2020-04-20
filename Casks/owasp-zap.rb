28717fc70b3526f13d3228fa26b2166bb5f17a84
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:02:37 +0100

    owasp-zap.rb: Add trailing slash to verified comment

--- Casks/owasp-zap.rb
+++ Casks/owasp-zap.rb
@@ -2,7 +2,7 @@ cask 'owasp-zap' do
   version '2.9.0'
   sha256 'b00949bf632b16c29085ebb3eb7b0962afe9ded166f6bade8a8ce8d3dbf1b9bd'
 
-  # github.com/zaproxy/zaproxy was verified as official when first introduced to the cask
+  # github.com/zaproxy/zaproxy/ was verified as official when first introduced to the cask
   url "https://github.com/zaproxy/zaproxy/releases/download/v#{version}/ZAP_#{version}.dmg"
   appcast 'https://github.com/zaproxy/zaproxy/releases.atom'
   name 'OWASP Zed Attack Proxy'
