d042eed1809d4b488499102cc245b44f703de2df
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:32 +0100

    slicer.rb: Add trailing slash to verified comment

--- Casks/slicer.rb
+++ Casks/slicer.rb
@@ -2,7 +2,7 @@ cask 'slicer' do
   version '4.10.0.27501,896287'
   sha256 '986b771a6d269cab3742a6cc565bb53f4f4221083adfb51cdfcd840016bcca42'
 
-  # slicer.kitware.com/midas3 was verified as official when first introduced to the cask
+  # slicer.kitware.com/midas3/ was verified as official when first introduced to the cask
   url "https://slicer.kitware.com/midas3/download?bitstream=#{version.after_comma}"
   appcast 'https://github.com/Slicer/Slicer/releases.atom'
   name '3D Slicer'
