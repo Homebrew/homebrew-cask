01a65f9faf91411da49ee0399e2b5a138aca0507
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:31:12 +0100

    swish.rb: Add trailing slash to verified comment

--- Casks/swish.rb
+++ Casks/swish.rb
@@ -2,7 +2,7 @@ cask 'swish' do
   version '1.4'
   sha256 '619bf894d805916dfd749c268b14d82a7bb39dd8b5cff9b6bc291e6a234e4591'
 
-  # github.com/chrenn/swish-dl was verified as official when first introduced to the cask
+  # github.com/chrenn/swish-dl/ was verified as official when first introduced to the cask
   url "https://github.com/chrenn/swish-dl/releases/download/#{version}/Swish.zip"
   appcast 'https://highlyopinionated.co/swish/appcast.xml'
   name 'Swish'
