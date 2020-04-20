a32bbda80e83c879d38de7819897ae6b157bf70a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:02:28 +0100

    masterway-note.rb: Add trailing slash to verified comment

--- Casks/masterway-note.rb
+++ Casks/masterway-note.rb
@@ -2,7 +2,7 @@ cask 'masterway-note' do
   version '1.0.0.84528265'
   sha256 'a6e92d580121dc74c96c3392f6e5c4e11fb08fce73a5b4f0368726fa1395be60'
 
-  # prota.oss-cn-beijing.aliyuncs.com was verified as official when first introduced to the cask
+  # prota.oss-cn-beijing.aliyuncs.com/ was verified as official when first introduced to the cask
   url "https://prota.oss-cn-beijing.aliyuncs.com/downloads/#{version.major_minor_patch}/%E5%A4%A7%E5%B8%88%E7%AC%94%E8%AE%B0-#{version}.dmg"
   appcast 'https://masterwaynote.com/mac',
           configuration: version.major_minor
