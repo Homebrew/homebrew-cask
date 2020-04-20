dbd979be40c499d140dfef138e00897e2ad089a4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:44:01 +0100

    datovka.rb: Add trailing slash to verified comment

--- Casks/datovka.rb
+++ Casks/datovka.rb
@@ -2,7 +2,7 @@ cask 'datovka' do
   version '4.15.0'
   sha256 'b7b76c092252a45f66b11b43a049ce062ff58b2a9d0d5b027081ac6e3f5131e8'
 
-  # secure.nic.cz/files/datove_schranky was verified as official when first introduced to the cask
+  # secure.nic.cz/files/datove_schranky/ was verified as official when first introduced to the cask
   url "https://secure.nic.cz/files/datove_schranky/#{version}/datovka-#{version}-64bit-osx10.7.dmg"
   appcast 'https://www.datovka.cz/cs/pages/instalace.html'
   name 'Datovka'
