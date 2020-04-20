863d8fccb4bb085a34c95991a7dfdb40ca49df55
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:02 +0100

    studiolinkstandalone.rb: Add trailing slash to verified comment

--- Casks/studiolinkstandalone.rb
+++ Casks/studiolinkstandalone.rb
@@ -2,7 +2,7 @@ cask 'studiolinkstandalone' do
   version '20.03.8'
   sha256 '599fcba644e4a48561b102be7e6b2391d2002b004d0938c9449b4856ba91bd33'
 
-  # download.studio.link was verified as official when first introduced to the cask
+  # download.studio.link/ was verified as official when first introduced to the cask
   url "https://download.studio.link/releases/v#{version}-stable/osx/signed/studio-link-standalone-v#{version}-stable.zip"
   appcast 'https://gitlab.com/studio.link/app/-/tags?format=atom'
   name 'Studio Link Standalone'
