81a96d241a87888d2683d7992309482a813b6b9a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:05:45 +0100

    cacher.rb: Add trailing slash to verified comment

--- Casks/cacher.rb
+++ Casks/cacher.rb
@@ -2,7 +2,7 @@ cask 'cacher' do
   version '2.25.2'
   sha256 '26fb15724649ac11611d2753c6a720221720be473e2a4a8621c59f5e24ac1a09'
 
-  # cacher-download.nyc3.digitaloceanspaces.com was verified as official when first introduced to the cask
+  # cacher-download.nyc3.digitaloceanspaces.com/ was verified as official when first introduced to the cask
   url "https://cacher-download.nyc3.digitaloceanspaces.com/Cacher-#{version}-mac.zip"
   appcast 'https://cacher-download.nyc3.digitaloceanspaces.com/latest-mac.yml'
   name 'Cacher'
