a34702915d8054ee68a74e93d5464b05ecec0d9a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:39:30 +0100

    borgbackup.rb: Add trailing slash to verified comment

--- Casks/borgbackup.rb
+++ Casks/borgbackup.rb
@@ -2,7 +2,7 @@ cask 'borgbackup' do
   version '1.1.11'
   sha256 '2535effded4cbe6f0b3732af8b8e85447c0da0be4b50921d03bb971ce2b9da44'
 
-  # github.com/borgbackup/borg was verified as official when first introduced to the cask
+  # github.com/borgbackup/borg/ was verified as official when first introduced to the cask
   url "https://github.com/borgbackup/borg/releases/download/#{version}/borg-macosx64"
   appcast 'https://github.com/borgbackup/borg/releases.atom'
   name 'BorgBackup'
