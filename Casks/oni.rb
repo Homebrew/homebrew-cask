218300a1603a47cb9396a50a9cb826c855ab17bd
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:45:36 +0100

    oni.rb: Add trailing slash to verified comment

--- Casks/oni.rb
+++ Casks/oni.rb
@@ -2,7 +2,7 @@ cask 'oni' do
   version '0.3.9'
   sha256 'c1b6e0fcce1a69d6460197158d8e087496199a3d8368901b452ee214eab039c4'
 
-  # github.com/onivim/oni was verified as official when first introduced to the cask
+  # github.com/onivim/oni/ was verified as official when first introduced to the cask
   url "https://github.com/onivim/oni/releases/download/v#{version}/Oni-#{version}-osx.dmg"
   appcast 'https://github.com/onivim/oni/releases.atom'
   name 'Oni'
