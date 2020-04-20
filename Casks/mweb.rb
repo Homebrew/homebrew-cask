c2310b517490ade6f47074736afae92a0c59d729
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:59:59 +0100

    mweb.rb: Add trailing slash to verified comment

--- Casks/mweb.rb
+++ Casks/mweb.rb
@@ -2,7 +2,7 @@ cask 'mweb' do
   version '3.3.8'
   sha256 '921cc4a2795fedde987d14c1faa9130df6b44ecc8b6ca0bf30bd03f96733dcdd'
 
-  # dl.devmate.com/com.coderforart.MWeb3 was verified as official when first introduced to the cask
+  # dl.devmate.com/com.coderforart.MWeb3/ was verified as official when first introduced to the cask
   url 'https://dl.devmate.com/com.coderforart.MWeb3/MWeb3-ProMarkdownwriting,notetakingandstaticbloggeneratorApp.dmg'
   appcast 'https://updates.devmate.com/com.coderforart.MWeb3.xml'
   name 'MWeb'
