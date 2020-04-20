893ab06c07cb8da022a55c6dece6586efac80401
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:01:17 +0100

    apache-couchdb.rb: Add trailing slash to verified comment

--- Casks/apache-couchdb.rb
+++ Casks/apache-couchdb.rb
@@ -2,7 +2,7 @@ cask 'apache-couchdb' do
   version '3.0.0'
   sha256 '09f85a7c93f96db1a9669dd6af9c9fcff2862223419d06b908d54b6f8feb4e68'
 
-  # couchdbneighbourhoodie.fra1.digitaloceanspaces.com was verified as official when first introduced to the cask
+  # couchdbneighbourhoodie.fra1.digitaloceanspaces.com/ was verified as official when first introduced to the cask
   url "https://couchdbneighbourhoodie.fra1.digitaloceanspaces.com/downloads/#{version}/mac/Apache-CouchDB.zip"
   appcast 'https://docs.couchdb.org/en/stable/whatsnew/index.html'
   name 'Apache CouchDB'
