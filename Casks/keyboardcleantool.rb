81af962675bbb5d3d9a75bd443291497615d42c2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:28 +0100

    keyboardcleantool.rb: Add trailing slash to verified comment

--- Casks/keyboardcleantool.rb
+++ Casks/keyboardcleantool.rb
@@ -2,7 +2,7 @@ cask 'keyboardcleantool' do
   version :latest
   sha256 :no_check
 
-  # bettertouchtool.net/releases was verified as official when first introduced to the cask
+  # bettertouchtool.net/releases/ was verified as official when first introduced to the cask
   url 'https://bettertouchtool.net/releases/KeyboardCleanTool.zip'
   name 'KeyboardCleanTool'
   homepage 'https://www.boastr.net/downloads/'
