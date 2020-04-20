f1fdf8fa6e8d5024202dcb0e16c2dc92a09d4a02
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:25:10 +0100

    lynkeos.rb: Add trailing slash to verified comment

--- Casks/lynkeos.rb
+++ Casks/lynkeos.rb
@@ -2,7 +2,7 @@ cask 'lynkeos' do
   version '3.3'
   sha256 '5d5a3eca3aea628ac201f31a59b41377240db62a58f2e9c14ca172de3d0dc25a'
 
-  # downloads.sourceforge.net/lynkeos was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/lynkeos/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/lynkeos/lynkeos/#{version}/Lynkeos-App-#{version.dots_to_hyphens}.zip"
   appcast 'https://sourceforge.net/projects/lynkeos/rss?path=/lynkeos'
   name 'Lynkeos'
