096e450a2af67ed2ec0e3b64d7033756332794b9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:32:01 +0100

    openmw.rb: Add trailing slash to verified comment

--- Casks/openmw.rb
+++ Casks/openmw.rb
@@ -2,7 +2,7 @@ cask 'openmw' do
   version '0.45.0'
   sha256 'dd4e41c40f5beaa6175644a7277651ee28b5f4fecceef49d48db7680abc731a2'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/ was verified as official when first introduced to the cask
   url "https://github.com/OpenMW/openmw/releases/download/openmw-#{version}/OpenMW-#{version}.dmg"
   appcast 'https://github.com/OpenMW/openmw/releases.atom'
   name 'OpenMW'
