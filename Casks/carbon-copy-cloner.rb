42522205c16e517b57d152f2b17e66a1cada6b57
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:18:50 +0100

    carbon-copy-cloner.rb: Add trailing slash to verified comment

--- Casks/carbon-copy-cloner.rb
+++ Casks/carbon-copy-cloner.rb
@@ -2,7 +2,7 @@ cask 'carbon-copy-cloner' do
   version '5.1.16.5965'
   sha256 '9914457ddf7022144e5ffa5edee5dcccbe0d2d7e88e85a17fd78473bb59ee8a6'
 
-  # bombich.scdn1.secure.raxcdn.com/software/files was verified as official when first introduced to the cask
+  # bombich.scdn1.secure.raxcdn.com/software/files/ was verified as official when first introduced to the cask
   url "https://bombich.scdn1.secure.raxcdn.com/software/files/ccc-#{version}.zip"
   appcast "https://bombich.com/software/updates/ccc.php?os_major=10&os_minor=14&os_bugfix=0&ccc=#{version.split('.').last}&beta=0"
   name 'Carbon Copy Cloner'
