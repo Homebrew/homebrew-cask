f51f2f0e28490f272a021fde8c3da29a31259a26
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:55 +0100

    ledger-live.rb: Add trailing slash to verified comment

--- Casks/ledger-live.rb
+++ Casks/ledger-live.rb
@@ -2,7 +2,7 @@ cask 'ledger-live' do
   version '2.2.4'
   sha256 '18178170144c0a25edf06e7901911a7009d51db71ff1b1ff5fc9929dda793ff2'
 
-  # github.com/LedgerHQ/ledger-live-desktop was verified as official when first introduced to the cask
+  # github.com/LedgerHQ/ledger-live-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/LedgerHQ/ledger-live-desktop/releases/download/v#{version}/ledger-live-desktop-#{version}-mac.dmg"
   appcast 'https://github.com/LedgerHQ/ledger-live-desktop/releases.atom'
   name 'Ledger Live'
