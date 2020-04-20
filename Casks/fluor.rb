c5938718cb2c65ea6d1ee89cb14dcfa2b8d93ee1
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:39:39 +0100

    fluor.rb: Add trailing slash to verified comment

--- Casks/fluor.rb
+++ Casks/fluor.rb
@@ -2,7 +2,7 @@ cask 'fluor' do
   version '2.1.0'
   sha256 '92034b630bf5a19eca160054c780a3c1767f21def3c02f28787f79fed5fab757'
 
-  # pyrolyse.it was verified as official when first introduced to the cask
+  # pyrolyse.it/ was verified as official when first introduced to the cask
   url "https://resources.pyrolyse.it/distrib/Fluor/Fluor%20#{version}.dmg"
   appcast 'https://fluorapp.net/'
   name 'Fluor'
