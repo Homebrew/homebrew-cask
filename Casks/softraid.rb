11dbc3d7646520000291c557c5f04bc0402faa25
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:00:29 +0100

    softraid.rb: Add trailing slash to verified comment

--- Casks/softraid.rb
+++ Casks/softraid.rb
@@ -2,7 +2,7 @@ cask 'softraid' do
   version '5.8.3'
   sha256 '3c41be3134c28783cdb61229eea2e87584aff6bf3800c7169ddedfec302c78b4'
 
-  # download.owcdigital.com/softraid was verified as official when first introduced to the cask
+  # download.owcdigital.com/softraid/ was verified as official when first introduced to the cask
   url "https://download.owcdigital.com/softraid/mac/#{version.major}/softraid/SoftRAID%20#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.owcdigital.com/softraid5/mac'
   name 'SoftRAID'
