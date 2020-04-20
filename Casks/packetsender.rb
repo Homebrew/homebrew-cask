794efa382c60adb0d3b179891d9ac6f0a9824bd2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:25 +0100

    packetsender.rb: Add trailing slash to verified comment

--- Casks/packetsender.rb
+++ Casks/packetsender.rb
@@ -2,7 +2,7 @@ cask 'packetsender' do
   version '6.2.3'
   sha256 '4a886b351ef7d30906e20d05ea002a6ae2308c28dd1246957bcaa361e054ee2b'
 
-  # github.com/dannagle/PacketSender was verified as official when first introduced to the cask
+  # github.com/dannagle/PacketSender/ was verified as official when first introduced to the cask
   url "https://github.com/dannagle/PacketSender/releases/download/v#{version}/PacketSender_v#{version}.dmg"
   appcast 'https://github.com/dannagle/PacketSender/releases.atom'
   name 'Packet Sender'
