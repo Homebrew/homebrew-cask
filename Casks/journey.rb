07c66405da9fd85082c749efdd9dc4b9d9a2243d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:32:37 +0100

    journey.rb: Add trailing slash to verified comment

--- Casks/journey.rb
+++ Casks/journey.rb
@@ -2,7 +2,7 @@ cask 'journey' do
   version '2.14.3'
   sha256 'aa3de8061bb169765c44fd256b0885c807ccebb771ba045b59eaf04815231583'
 
-  # github.com/2-App-Studio/journey-releases was verified as official when first introduced to the cask
+  # github.com/2-App-Studio/journey-releases/ was verified as official when first introduced to the cask
   url "https://github.com/2-App-Studio/journey-releases/releases/download/v#{version}/Journey-darwin-#{version}.zip"
   appcast 'https://github.com/2-App-Studio/journey-releases/releases.atom'
   name 'Journey'
