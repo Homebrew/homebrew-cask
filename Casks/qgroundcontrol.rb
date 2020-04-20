dc9e505321de6bec35c2f366c057c4bd92401b08
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:36:37 +0100

    qgroundcontrol.rb: Add trailing slash to verified comment

--- Casks/qgroundcontrol.rb
+++ Casks/qgroundcontrol.rb
@@ -2,7 +2,7 @@ cask 'qgroundcontrol' do
   version :latest
   sha256 :no_check
 
-  # s3-us-west-2.amazonaws.com/qgroundcontrol was verified as official when first introduced to the cask
+  # s3-us-west-2.amazonaws.com/qgroundcontrol/ was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/qgroundcontrol/latest/QGroundControl.dmg'
   name 'QGroundControl'
   homepage 'http://qgroundcontrol.com/'
