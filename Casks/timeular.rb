0a0235b4b2df7b1d707e4dddce83d9a7298042b2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:17:34 +0100

    timeular.rb: Add trailing slash to verified comment

--- Casks/timeular.rb
+++ Casks/timeular.rb
@@ -2,7 +2,7 @@ cask 'timeular' do
   version '3.1.0'
   sha256 '8e9214c1d26ee1d68f46a46eb00698b01d2677affd0beef0258a5a7f1f6f39e8'
 
-  # timeular-desktop-packages.s3.amazonaws.com was verified as official when first introduced to the cask
+  # timeular-desktop-packages.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://timeular-desktop-packages.s3.amazonaws.com/mac/production/Timeular-#{version}.dmg"
   appcast 'https://timeular-desktop-packages.s3.amazonaws.com/mac/production/latest-mac.yml'
   name 'Timeular'
