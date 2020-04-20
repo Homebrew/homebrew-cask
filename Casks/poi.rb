9291f28fe86eb9509c5798479d9e726074217ba9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:29:25 +0100

    poi.rb: Add trailing slash to verified comment

--- Casks/poi.rb
+++ Casks/poi.rb
@@ -2,7 +2,7 @@ cask 'poi' do
   version '10.5.1'
   sha256 '5baf030cf5799ec7ea12da500d9a292bf7d7d52055250a9ffbbbe3385853f8b1'
 
-  # github.com/poooi/poi was verified as official when first introduced to the cask
+  # github.com/poooi/poi/ was verified as official when first introduced to the cask
   url "https://github.com/poooi/poi/releases/download/v#{version}/poi-#{version}.dmg"
   appcast 'https://github.com/poooi/poi/releases.atom'
   name 'poi'
