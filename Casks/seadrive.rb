f112234167e35f377e22464ed550cd692cd4dca9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:12 +0100

    seadrive.rb: Add trailing slash to verified comment

--- Casks/seadrive.rb
+++ Casks/seadrive.rb
@@ -2,7 +2,7 @@ cask 'seadrive' do
   version '1.0.11'
   sha256 '7b38a0b4870e2ac8221dc737c0b16fa2ab9d41604a20be017069dd71fe60900a'
 
-  # download.seadrive.org was verified as official when first introduced to the cask
+  # download.seadrive.org/ was verified as official when first introduced to the cask
   url "https://download.seadrive.org/seadrive-#{version}.dmg"
   appcast 'https://www.seafile.com/en/download/'
   name 'Seadrive'
