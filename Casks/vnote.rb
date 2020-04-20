1982873c18a36c4f9576edbb71ba4c17488fe7b5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:38:09 +0100

    vnote.rb: Add trailing slash to verified comment

--- Casks/vnote.rb
+++ Casks/vnote.rb
@@ -2,7 +2,7 @@ cask 'vnote' do
   version '2.9.1'
   sha256 '7611fca118e2b5894b1530ae049db9ede91ebbabdd6fc5f135cb61d724f8f12d'
 
-  # github.com/tamlok/vnote was verified as official when first introduced to the cask
+  # github.com/tamlok/vnote/ was verified as official when first introduced to the cask
   url "https://github.com/tamlok/vnote/releases/download/v#{version}/VNote-#{version}-x64.dmg"
   appcast 'https://github.com/tamlok/vnote/releases.atom'
   name 'VNote'
