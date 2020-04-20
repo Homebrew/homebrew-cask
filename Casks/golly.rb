f6425027d7e61b7399b29a11470fdad427883858
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:03:01 +0100

    golly.rb: Add trailing slash to verified comment

--- Casks/golly.rb
+++ Casks/golly.rb
@@ -2,7 +2,7 @@ cask 'golly' do
   version '3.3'
   sha256 '3641940d1bd102573a546320c814e847f8fe4f0e6602ab055362d0b85d1e7cd9'
 
-  # downloads.sourceforge.net/golly was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/golly/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/golly/golly/golly-#{version}/Golly-#{version}-Mac.dmg"
   appcast 'https://sourceforge.net/projects/golly/rss?path=/golly'
   name 'Golly'
