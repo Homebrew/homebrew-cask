a67f3bb050167388e12e8c436286bdfb9e635f18
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:22:17 +0100

    prusaslicer.rb: Add trailing slash to verified comment

--- Casks/prusaslicer.rb
+++ Casks/prusaslicer.rb
@@ -2,7 +2,7 @@ cask 'prusaslicer' do
   version '2.2.0,202003211132'
   sha256 '4634361151e0ceb0b75cff6edafa523e3cc92d7979789c32ca50861eb0c3c7c6'
 
-  # github.com/prusa3d/PrusaSlicer was verified as official when first introduced to the cask.
+  # github.com/prusa3d/PrusaSlicer/ was verified as official when first introduced to the cask
   url "https://github.com/prusa3d/PrusaSlicer/releases/download/version_#{version.before_comma}/PrusaSlicer-#{version.before_comma}+-#{version.after_comma}.dmg"
   appcast 'https://github.com/prusa3d/PrusaSlicer/releases.atom'
   name 'PrusaSlicer'
