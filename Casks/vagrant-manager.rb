53b86877b0e1c4079f028a80a6c970032eb169ac
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:02:40 +0100

    vagrant-manager.rb: Add trailing slash to verified comment

--- Casks/vagrant-manager.rb
+++ Casks/vagrant-manager.rb
@@ -2,7 +2,7 @@ cask 'vagrant-manager' do
   version '2.7.1'
   sha256 'ad10795f36a2c0977ec8e278082f1f66624cc3b16f837dc72c0f1c063fefb4de'
 
-  # github.com/lanayotech/vagrant-manager was verified as official when first introduced to the cask
+  # github.com/lanayotech/vagrant-manager/ was verified as official when first introduced to the cask
   url "https://github.com/lanayotech/vagrant-manager/releases/download/#{version}/vagrant-manager-#{version}.dmg"
   appcast 'https://github.com/lanayotech/vagrant-manager/releases.atom'
   name 'Vagrant Manager'
