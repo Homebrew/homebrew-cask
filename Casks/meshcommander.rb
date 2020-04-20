d804bf2db976d2b760ba2ee4296c7792c202efac
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:53:46 +0100

    meshcommander.rb: Add trailing slash to verified comment

--- Casks/meshcommander.rb
+++ Casks/meshcommander.rb
@@ -2,7 +2,7 @@ cask 'meshcommander' do
   version '0.7.7'
   sha256 'ac104f6bd8c79786fdd371064fd6a2a892ba58851e60447a8ce83ad2bed6725c'
 
-  # bintray.com/gomesjj/APPS was verified as official when first introduced to the cask
+  # bintray.com/gomesjj/APPS/ was verified as official when first introduced to the cask
   url "https://bintray.com/gomesjj/APPS/download_file?file_path=#{version.major}.#{version.minor}#{version.patch}%2FMeshCommander.dmg.zip"
   appcast 'https://dl.bintray.com/gomesjj/APPS/',
           configuration: "#{version.major}.#{version.minor}#{version.patch}"
