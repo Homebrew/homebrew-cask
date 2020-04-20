cc3a56680aa61447c2d1d5eb59e4e90a4e3f1c3a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:59:23 +0100

    quip.rb: Add trailing slash to verified comment

--- Casks/quip.rb
+++ Casks/quip.rb
@@ -2,7 +2,7 @@ cask 'quip' do
   version '7.8.1'
   sha256 '2210f1b393ca5be2d6e8dd6754adc1904060ba2a076ec898082450300a76a4a5'
 
-  # quip-clients.com was verified as official when first introduced to the cask
+  # quip-clients.com/ was verified as official when first introduced to the cask
   url "https://quip-clients.com/macosx_#{version}.dmg"
   appcast 'https://api.quip.com/-/sparkle-feed?manual=0'
   name 'Quip'
