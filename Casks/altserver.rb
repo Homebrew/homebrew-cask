9bf1f916a2ffd6570c6204390916e17e1708e73d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:08:02 +0100

    altserver.rb: Add trailing slash to verified comment

--- Casks/altserver.rb
+++ Casks/altserver.rb
@@ -2,7 +2,7 @@ cask 'altserver' do
   version '1.3'
   sha256 'c83d5076435d88260ae99674776274bb3f23aa6dc54e717ad65a0c26c66f88f4'
 
-  # f000.backblazeb2.com/file was verified as official when first introduced to the cask
+  # f000.backblazeb2.com/file/ was verified as official when first introduced to the cask
   url "https://f000.backblazeb2.com/file/altstore/altserver/#{version.dots_to_underscores}.zip"
   appcast 'https://altstore.io/altserver/sparkle-macos.xml'
   name 'AltServer'
