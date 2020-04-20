654f99c471384ad4151ca6937451204fe87b6216
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:20:44 +0100

    ganache.rb: Add trailing slash to verified comment

--- Casks/ganache.rb
+++ Casks/ganache.rb
@@ -2,7 +2,7 @@ cask 'ganache' do
   version '2.3.0'
   sha256 '17740bbe33bb9db80ae3dd2d6b825b0b20836749845b51deb4c70cc1ec8fdb71'
 
-  # github.com/trufflesuite/ganache was verified as official when first introduced to the cask
+  # github.com/trufflesuite/ganache/ was verified as official when first introduced to the cask
   url "https://github.com/trufflesuite/ganache/releases/download/v#{version}/Ganache-#{version}-mac.dmg"
   appcast 'https://github.com/trufflesuite/ganache/releases.atom'
   name 'Ganache'
