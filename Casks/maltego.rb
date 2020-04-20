e12c92ca1ebbcddf06151ff39f3ad9b210e1907b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:54 +0100

    maltego.rb: Add trailing slash to verified comment

--- Casks/maltego.rb
+++ Casks/maltego.rb
@@ -2,7 +2,7 @@ cask 'maltego' do
   version '4.2.9.12898'
   sha256 'a11167cfa3b6ae70bc539dbcc89f70fa44a2aa6690f054b6f62d204c17ec9f5d'
 
-  # maltego-downloads.s3.us-east-2.amazonaws.com was verified as official when first introduced to the cask
+  # maltego-downloads.s3.us-east-2.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://maltego-downloads.s3.us-east-2.amazonaws.com/mac/Maltego.v#{version}.dmg"
   appcast 'https://maltego-downloads.s3.us-east-2.amazonaws.com/info.json'
   name 'Maltego'
