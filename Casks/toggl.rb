84321fff794eed1df688d5e0fa6afe7fe23f2b63
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:40 +0100

    toggl.rb: Add trailing slash to verified comment

--- Casks/toggl.rb
+++ Casks/toggl.rb
@@ -2,7 +2,7 @@ cask 'toggl' do
   version '7.5.85'
   sha256 '8ad2f539abaebe77083e92c1ea0d608d4d174e8c7fd1aa6f2475eda30949fbc3'
 
-  # github.com/toggl-open-source/toggldesktop was verified as official when first introduced to the cask
+  # github.com/toggl-open-source/toggldesktop/ was verified as official when first introduced to the cask
   url "https://github.com/toggl-open-source/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_stable_appcast.xml'
   name 'TogglDesktop'
