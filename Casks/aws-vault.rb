a6cbb3544519ce75d45addac9439caa0afc28ff3
Michael Tibben <michael.tibben@99designs.com>
2020-04-20 22:18:38 +1000

    Update aws-vault from 5.4.0 to 5.4.1 (#80993)

--- Casks/aws-vault.rb
+++ Casks/aws-vault.rb
@@ -1,6 +1,6 @@
 cask 'aws-vault' do
-  version '5.4.0'
-  sha256 '1df607d555cefc7d14c8b508f251e039ab1b849800497746a642844552d4ecc2'
+  version '5.4.1'
+  sha256 '411fd34b28891d9ef95f9e5ec454daf6f5e7f64a08a64d5395e214c89cd9d1be'
 
   url "https://github.com/99designs/aws-vault/releases/download/v#{version}/aws-vault-darwin-amd64.dmg"
   appcast 'https://github.com/99designs/aws-vault/releases.atom'
