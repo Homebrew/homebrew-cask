fab402b7432544302549b2ea65a54238192c5f29
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:03 +0100

    tropy.rb: Add trailing slash to verified comment

--- Casks/tropy.rb
+++ Casks/tropy.rb
@@ -2,7 +2,7 @@ cask 'tropy' do
   version '1.7.0'
   sha256 '97aa6280cfd094c1d0136fcae40874e91bad33975e80df37dde9a1bd29026a37'
 
-  # github.com/tropy/tropy was verified as official when first introduced to the cask
+  # github.com/tropy/tropy/ was verified as official when first introduced to the cask
   url "https://github.com/tropy/tropy/releases/download/#{version}/tropy-#{version}.dmg"
   appcast 'https://github.com/tropy/tropy/releases.atom'
   name 'Tropy'
