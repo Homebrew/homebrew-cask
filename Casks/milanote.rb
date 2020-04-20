1f2d35a3d4344680c65a9d0a2148f6f52c0ab1f6
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:44:35 +0100

    milanote.rb: Add trailing slash to verified comment

--- Casks/milanote.rb
+++ Casks/milanote.rb
@@ -2,7 +2,7 @@ cask 'milanote' do
   version '2.0.8'
   sha256 '986edf96ee527d0fd73af11b7688ad407ecbe30b151472f9a0cfeae6566608bc'
 
-  # milanote-app-releases.s3.amazonaws.com was verified as official when first introduced to the cask
+  # milanote-app-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://milanote-app-releases.s3.amazonaws.com/Milanote-#{version}.dmg"
   appcast 'https://www.milanote.com/download-mac-app'
   name 'Milanote'
