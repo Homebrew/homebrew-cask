b7dd3e91d24af060d2d32767386a3af84dba6fd0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:44:23 +0100

    metaz.rb: Add trailing slash to verified comment

--- Casks/metaz.rb
+++ Casks/metaz.rb
@@ -2,7 +2,7 @@ cask 'metaz' do
   version '1.0.beta-113'
   sha256 '5f0d43794ac76c025ad1c0f6419dfea58c8c696d7e2782060d2aa12554316588'
 
-  # github.com/griff/metaz was verified as official when first introduced to the cask
+  # github.com/griff/metaz/ was verified as official when first introduced to the cask
   url "https://github.com/griff/metaz/releases/download/v#{version}/MetaZ-#{version}.zip"
   appcast 'https://github.com/griff/metaz/releases.atom'
   name 'MetaZ'
