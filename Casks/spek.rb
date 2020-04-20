1ae441a9674d824ead3a8874f80a30b7e0b5d7dd
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:18:18 +0100

    spek.rb: Add trailing slash to verified comment

--- Casks/spek.rb
+++ Casks/spek.rb
@@ -2,7 +2,7 @@ cask 'spek' do
   version '0.8.3'
   sha256 '648ffe37a4605d76b8d63ca677503ba63338b84c5df73961d9a5335ff144cc54'
 
-  # github.com/alexkay/spek was verified as official when first introduced to the cask
+  # github.com/alexkay/spek/ was verified as official when first introduced to the cask
   url "https://github.com/alexkay/spek/releases/download/v#{version}/spek-#{version}.dmg"
   appcast 'https://github.com/alexkay/spek/releases.atom'
   name 'Spek'
