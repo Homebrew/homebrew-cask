7d7672511a2cff3f27f794f3d8885b317d631182
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:16:51 +0100

    hazel.rb: Add trailing slash to verified comment

--- Casks/hazel.rb
+++ Casks/hazel.rb
@@ -2,7 +2,7 @@ cask 'hazel' do
   version '4.4.4'
   sha256 '4419e2a2930d3650778bffa53ef006985cd9de266dfd0b9ac3c686407fdafe01'
 
-  # s3.amazonaws.com/Noodlesoft was verified as official when first introduced to the cask
+  # s3.amazonaws.com/Noodlesoft/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/Noodlesoft/Hazel-#{version}.dmg"
   appcast 'https://www.noodlesoft.com/Products/Hazel/generate-appcast.php'
   name 'Hazel'
