58d0378a17a71c63ed522df2a15ebc77ceb7c23f
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:14:40 +0100

    weiyun.rb: Add trailing slash to verified comment

--- Casks/weiyun.rb
+++ Casks/weiyun.rb
@@ -2,7 +2,7 @@ cask 'weiyun' do
   version '3.0.4.406_2dc389'
   sha256 'be41ee57871992cb4ecfd477ecbb2bb6c264512c531d92c7f4ddf356b48bd64e'
 
-  # dldir1.qq.com/weiyun was verified as official when first introduced to the cask
+  # dldir1.qq.com/weiyun/ was verified as official when first introduced to the cask
   url "https://dldir1.qq.com/weiyun/Weiyun_Mac_#{version}.dmg"
   appcast 'https://qzonestyle.gtimg.cn/qzone/qzactStatics/configSystem/data/65/config1.js'
   name 'weiyun'
