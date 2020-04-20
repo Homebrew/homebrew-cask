e4ba09ea6aa32619788bcb3763ddafcdeb8a8924
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:49:00 +0100

    electron-fiddle.rb: Add trailing slash to verified comment

--- Casks/electron-fiddle.rb
+++ Casks/electron-fiddle.rb
@@ -2,7 +2,7 @@ cask 'electron-fiddle' do
   version '0.14.0'
   sha256 'd32a51e72e8646c6cd21348ad439dad0a7ffb14e842e58182aced42c48c79e2f'
 
-  # github.com/electron/fiddle was verified as official when first introduced to the cask
+  # github.com/electron/fiddle/ was verified as official when first introduced to the cask
   url "https://github.com/electron/fiddle/releases/download/v#{version}/Electron.Fiddle-darwin-x64-#{version}.zip"
   appcast 'https://github.com/electron/fiddle/releases.atom'
   name 'Electron Fiddle'
