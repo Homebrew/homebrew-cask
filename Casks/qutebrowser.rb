88e211c0ac74f8bfe2cb1fc6550a91f96e56bb7b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:41:46 +0100

    qutebrowser.rb: Add trailing slash to verified comment

--- Casks/qutebrowser.rb
+++ Casks/qutebrowser.rb
@@ -2,7 +2,7 @@ cask 'qutebrowser' do
   version '1.10.2'
   sha256 '616c38a96bb24f1fd1d5a332709124bdcac249397cc1370fbaee381a7d55a352'
 
-  # github.com/qutebrowser/qutebrowser was verified as official when first introduced to the cask
+  # github.com/qutebrowser/qutebrowser/ was verified as official when first introduced to the cask
   url "https://github.com/qutebrowser/qutebrowser/releases/download/v#{version}/qutebrowser-#{version}.dmg"
   appcast 'https://github.com/qutebrowser/qutebrowser/releases.atom'
   name 'qutebrowser'
