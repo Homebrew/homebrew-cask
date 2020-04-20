2f81b17955d17c1e9d8dcda20509101c48491220
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:16 +0100

    gretl.rb: Add trailing slash to verified comment

--- Casks/gretl.rb
+++ Casks/gretl.rb
@@ -2,7 +2,7 @@ cask 'gretl' do
   version '2020b'
   sha256 'a6537914d2e221b372c12261d224f14825563fe0bb69c7f980b16b277e09939e'
 
-  # downloads.sourceforge.net/gretl was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/gretl/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/gretl/gretl-#{version}-quartz.pkg"
   appcast 'https://sourceforge.net/projects/gretl/rss?path=/gretl'
   name 'gretl'
