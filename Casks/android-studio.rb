0594b1e7520ce00c7361dfb4b6666ef5746fca9f
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:56:34 +0100

    android-studio.rb: Add trailing slash to verified comment

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -2,7 +2,7 @@ cask 'android-studio' do
   version '3.6.2.0,192.6308749'
   sha256 '94d337b4bb743a9676dae78d60c61bcb52bfdea771edcb4be07c6016da29942f'
 
-  # google.com/dl/android/studio was verified as official when first introduced to the cask
+  # google.com/dl/android/studio/ was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/install/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.dmg"
   appcast 'https://dl.google.com/android/studio/patches/updates.xml',
           configuration: version.major_minor_patch
