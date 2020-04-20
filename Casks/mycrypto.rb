3dd30063d6c97a9804fc3c9c55387a100f813cf6
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:34 +0100

    mycrypto.rb: Add trailing slash to verified comment

--- Casks/mycrypto.rb
+++ Casks/mycrypto.rb
@@ -2,7 +2,7 @@ cask 'mycrypto' do
   version '1.7.9'
   sha256 'b3fdba4dd5a9e17ec468970e30360bd993412b750a68d9580d95d515fff3e49a'
 
-  # github.com/MyCryptoHQ/MyCrypto was verified as official when first introduced to the cask
+  # github.com/MyCryptoHQ/MyCrypto/ was verified as official when first introduced to the cask
   url "https://github.com/MyCryptoHQ/MyCrypto/releases/download/#{version}/mac_#{version}_MyCrypto.dmg"
   appcast 'https://github.com/MyCryptoHQ/MyCrypto/releases.atom'
   name 'MyCrypto'
