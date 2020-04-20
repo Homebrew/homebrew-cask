01faf8d373883386b64f2165b58874b52a02f903
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:42:01 +0100

    minecraft.rb: Add trailing slash to verified comment

--- Casks/minecraft.rb
+++ Casks/minecraft.rb
@@ -2,7 +2,7 @@ cask 'minecraft' do
   version :latest
   sha256 :no_check
 
-  # mojang.com was verified as official when first introduced to the cask
+  # mojang.com/ was verified as official when first introduced to the cask
   url 'https://launcher.mojang.com/download/Minecraft.dmg'
   name 'Minecraft'
   homepage 'https://minecraft.net/'
