f9134522bf6600085dbcdb86a22d0e9ffdf94d2f
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:21:02 +0100

    downie.rb: Add trailing slash to verified comment

--- Casks/downie.rb
+++ Casks/downie.rb
@@ -2,7 +2,7 @@ cask 'downie' do
   version '4.0.8,4092'
   sha256 '00e629d94c4c8d4d9e79754a3584673203eb5c15717eec871e3488747af9cf69'
 
-  # charliemonroesoftware.com was verified as official when first introduced to the cask
+  # charliemonroesoftware.com/ was verified as official when first introduced to the cask
   url "https://charliemonroesoftware.com/trial/downie/v#{version.major}/Downie_#{version.major}_#{version.after_comma}.dmg"
   appcast "https://charliemonroesoftware.com/trial/downie/v#{version.major}/updates.xml"
   name 'Downie'
