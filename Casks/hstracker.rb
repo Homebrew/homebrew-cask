6b42438f19bf71ca2a4d8a44175031c5bb65f4c7
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:21:53 +0100

    hstracker.rb: Add trailing slash to verified comment

--- Casks/hstracker.rb
+++ Casks/hstracker.rb
@@ -2,7 +2,7 @@ cask 'hstracker' do
   version '1.6.25'
   sha256 '15a9bbad8b176a22405cfe929a5728863ae2a8241b82bebe936692d244d945e5'
 
-  # github.com/HearthSim/HSTracker was verified as official when first introduced to the cask
+  # github.com/HearthSim/HSTracker/ was verified as official when first introduced to the cask
   url "https://github.com/HearthSim/HSTracker/releases/download/#{version}/HSTracker.app.zip"
   appcast 'https://github.com/HearthSim/HSTracker/releases.atom'
   name 'Hearthstone Deck Tracker'
