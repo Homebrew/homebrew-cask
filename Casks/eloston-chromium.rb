3637c7f7dde68e5b0a06de7d50fad879a6d85471
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:50:58 +0100

    eloston-chromium.rb: Add trailing slash to verified comment

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -2,7 +2,7 @@ cask 'eloston-chromium' do
   version '81.0.4044.113-1'
   sha256 'ea4642e72e390038e471cd5f055b397077ac677c47e6729d99000e05d0a6dcce'
 
-  # github.com/kramred/ungoogled-chromium-binaries was verified as official when first introduced to the cask
+  # github.com/kramred/ungoogled-chromium-binaries/ was verified as official when first introduced to the cask
   url "https://github.com/kramred/ungoogled-chromium-binaries/releases/download/#{version}/ungoogled-chromium_#{version}.1_macos.dmg"
   appcast 'https://github.com/kramred/ungoogled-chromium-binaries/releases.atom'
   name 'Ungoogled Chromium'
