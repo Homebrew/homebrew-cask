753b05b065d43b04f2ee17741bd6943980f42c59
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:33:38 +0100

    shimo.rb: Add trailing slash to verified comment

--- Casks/shimo.rb
+++ Casks/shimo.rb
@@ -2,7 +2,7 @@ cask 'shimo' do
   version '5.0.2_8880'
   sha256 '09364783b6dcc13b1ef54feaf4c57d1518f25a6c6e3884c9ab6c2a6077942b2a'
 
-  # downloads.mailbutler.io was verified as official when first introduced to the cask
+  # downloads.mailbutler.io/ was verified as official when first introduced to the cask
   url "https://downloads.mailbutler.io/Shimo_#{version}.zip"
   appcast "https://www.shimovpn.com/appcast#{version.major}.php"
   name 'Shimo'
