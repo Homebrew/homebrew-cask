1793b972faf74828345c8f10935bde156aeac1a5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:01:58 +0100

    aviatrix-vpn-client.rb: Add trailing slash to verified comment

--- Casks/aviatrix-vpn-client.rb
+++ Casks/aviatrix-vpn-client.rb
@@ -2,7 +2,7 @@ cask 'aviatrix-vpn-client' do
   version :latest
   sha256 :no_check
 
-  # s3-us-west-2.amazonaws.com/aviatrix-download was verified as official when first introduced to the cask
+  # s3-us-west-2.amazonaws.com/aviatrix-download/ was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/aviatrix-download/AviatrixVPNClient/AVPNC_mac.pkg'
   name 'Aviatrix VPN Client'
   homepage 'https://docs.aviatrix.com/Downloads/samlclient.html'
