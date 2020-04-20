b842b9f0d9935d39111c07708621b5d311b5abd1
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:21:14 +0100

    box-sync.rb: Add trailing slash to verified comment

--- Casks/box-sync.rb
+++ Casks/box-sync.rb
@@ -2,7 +2,7 @@ cask 'box-sync' do
   version :latest
   sha256 :no_check
 
-  # boxcdn.net was verified as official when first introduced to the cask
+  # boxcdn.net/ was verified as official when first introduced to the cask
   url 'https://e3.boxcdn.net/box-installers/sync/Sync+4+External/Box%20Sync%20Installer.dmg'
   name 'Box Sync'
   homepage 'https://www.box.com/'
