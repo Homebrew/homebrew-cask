344ab6e3d6906813b9ca3ef9ea16725c20e047d3
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:36:36 +0100

    Update dwarf-fortress-lmp.rb

--- Casks/dwarf-fortress-lmp.rb
+++ Casks/dwarf-fortress-lmp.rb
@@ -2,7 +2,7 @@ cask 'dwarf-fortress-lmp' do
   version '0.47.04 dfhack-b1'
   sha256 '7023cb5952172bdb74bda215a7856a0dd769188cc277846fd02a70783e86ea36'
 
-  # dffd.bay12games.com was verified as official when first introduced to the cask
+  # dffd.bay12games.com/ was verified as official when first introduced to the cask
   url "http://dffd.bay12games.com/download.php?id=12202&f=Lazy+Mac+Pack.v#{version}.dmg"
   name 'Dwarf Fortress LMP (Lazy Mac Pack)'
   homepage 'http://www.bay12forums.com/smf/index.php?topic=158322.0'
