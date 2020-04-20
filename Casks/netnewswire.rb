fe100c71ac1c89b01ef38c0bd4c8fa191184e75b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:41:43 +0100

    netnewswire.rb: Add trailing slash to verified comment

--- Casks/netnewswire.rb
+++ Casks/netnewswire.rb
@@ -2,7 +2,7 @@ cask 'netnewswire' do
   version '5.0.3'
   sha256 'c78a1015159d0a2154e32ae3e73a2683ec3c684eac81513ebf77478f8770e76a'
 
-  # github.com/brentsimmons/NetNewsWire was verified as official when first introduced to the cask
+  # github.com/brentsimmons/NetNewsWire/ was verified as official when first introduced to the cask
   url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip"
   appcast 'https://github.com/brentsimmons/NetNewsWire/releases.atom'
   name 'NetNetsWire'
