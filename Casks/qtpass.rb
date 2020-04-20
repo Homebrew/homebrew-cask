70184f7b88030409be488566904dd1fa29167484
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:49:48 +0100

    qtpass.rb: Add trailing slash to verified comment

--- Casks/qtpass.rb
+++ Casks/qtpass.rb
@@ -2,7 +2,7 @@ cask 'qtpass' do
   version '1.3.2'
   sha256 '04a091602bfd8da1b19e95fcf9cd48df6a544a44a4a1e97cccfb950df90512ef'
 
-  # github.com/IJHack/qtpass was verified as official when first introduced to the cask
+  # github.com/IJHack/qtpass/ was verified as official when first introduced to the cask
   url "https://github.com/IJHack/qtpass/releases/download/v#{version}/qtpass-#{version}.dmg"
   appcast 'https://github.com/IJHack/qtpass/releases.atom'
   name 'QtPass'
