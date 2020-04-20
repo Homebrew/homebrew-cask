58bcda52d83d889bada23023307c34f7addad927
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:06:12 +0100

    league-of-legends.rb: Add trailing slash to verified comment

--- Casks/league-of-legends.rb
+++ Casks/league-of-legends.rb
@@ -2,7 +2,7 @@ cask 'league-of-legends' do
   version :latest
   sha256 :no_check
 
-  # riotgamespatcher-a.akamaihd.net was verified as official when first introduced to the cask
+  # riotgamespatcher-a.akamaihd.net/ was verified as official when first introduced to the cask
   url 'https://riotgamespatcher-a.akamaihd.net/releases/Maclive/installer/deploy/League%20of%20Legends%20installer%20NA.dmg'
   name 'League of Legends'
   homepage 'https://eune.leagueoflegends.com/en/'
