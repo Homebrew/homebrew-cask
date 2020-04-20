3ccf434ae991abc17b7b53aa65fb61a5783ad567
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:01:02 +0100

    dogecoin.rb: Add trailing slash to verified comment

--- Casks/dogecoin.rb
+++ Casks/dogecoin.rb
@@ -2,7 +2,7 @@ cask 'dogecoin' do
   version '1.14.2'
   sha256 'd8cbb26ceeb5e395417ed5e8984fc3b1d8990f8d5866deb837a4d82a17dba62e'
 
-  # github.com/dogecoin/dogecoin was verified as official when first introduced to the cask
+  # github.com/dogecoin/dogecoin/ was verified as official when first introduced to the cask
   url "https://github.com/dogecoin/dogecoin/releases/download/v#{version}/dogecoin-#{version}-osx.dmg"
   appcast 'https://github.com/dogecoin/dogecoin/releases.atom'
   name 'Dogecoin'
