f0aca88d9d910de1084bc18b7fab59112facfb3f
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:55:13 +0100

    runway.rb: Add trailing slash to verified comment

--- Casks/runway.rb
+++ Casks/runway.rb
@@ -2,7 +2,7 @@ cask 'runway' do
   version '0.11.2'
   sha256 '0262178c4abc80497a858e7962bdb265173c6a21602d7f4615c1f0c34f286510'
 
-  # runway-releases.s3.amazonaws.com was verified as official when first introduced to the cask
+  # runway-releases.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://runway-releases.s3.amazonaws.com/Runway-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://api.runwayml.com/v1/download?platform=mac'
   name 'Runway'
