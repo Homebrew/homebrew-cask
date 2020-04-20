87d1aca682506819115ece4f9365b9c95a6d1575
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:50:08 +0100

    ticktick.rb: Add trailing slash to verified comment

--- Casks/ticktick.rb
+++ Casks/ticktick.rb
@@ -2,7 +2,7 @@ cask 'ticktick' do
   version '3.5.11,139'
   sha256 'aeb3da17db243e38913dde13515ad01ef3628977264e8c783253bdd9e0b35d48'
 
-  # appest-public.s3.amazonaws.com was verified as official when first introduced to the cask
+  # appest-public.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://appest-public.s3.amazonaws.com/download/mac/TickTick_#{version.before_comma}_#{version.after_comma}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.ticktick.com/static/getApp/download?type=mac'
   name 'TickTick'
