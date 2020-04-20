3b088b0b23c2cdfd1a01822136b9b092b76d26ab
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:57:33 +0100

    openshot-video-editor.rb: Add trailing slash to verified comment

--- Casks/openshot-video-editor.rb
+++ Casks/openshot-video-editor.rb
@@ -2,7 +2,7 @@ cask 'openshot-video-editor' do
   version '2.5.1'
   sha256 'cc17586bb7241545659220994c765ae89eb584f6be007069cba519cd4c1793a4'
 
-  # github.com/OpenShot/openshot-qt was verified as official when first introduced to the cask
+  # github.com/OpenShot/openshot-qt/ was verified as official when first introduced to the cask
   url "https://github.com/OpenShot/openshot-qt/releases/download/v#{version}/OpenShot-v#{version}-x86_64.dmg"
   appcast 'https://github.com/OpenShot/openshot-qt/releases.atom'
   name 'OpenShot Video Editor'
