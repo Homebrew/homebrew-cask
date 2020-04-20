abc5f85a6fb1384e3f69b491e497414410a7ee04
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:06:56 +0100

    sqlpro-for-mysql.rb: Add trailing slash to verified comment

--- Casks/sqlpro-for-mysql.rb
+++ Casks/sqlpro-for-mysql.rb
@@ -2,7 +2,7 @@ cask 'sqlpro-for-mysql' do
   version '2020.08'
   sha256 '3ba64e00d07c137a53eb35db6f95e02278130bef7415ca79d3586e31ba2b5c0b'
 
-  # d3fwkemdw8spx3.cloudfront.net/mysql was verified as official when first introduced to the cask
+  # d3fwkemdw8spx3.cloudfront.net/mysql/ was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/mysql/SQLProMySQL.#{version}.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.mysqlui.com/download.php'
   name 'SQLPro for MySQL'
