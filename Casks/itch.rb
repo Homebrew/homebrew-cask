efda9e9e50046c67a40a1b4b86bb29794887e0e5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:19:17 +0100

    itch.rb: Add trailing slash to verified comment

--- Casks/itch.rb
+++ Casks/itch.rb
@@ -2,7 +2,7 @@ cask 'itch' do
   version '1.17.0'
   sha256 '9053778cbae81ce09129364e0b3ec7f2b9abdd9705334011398d2d66d553c31c'
 
-  # broth.itch.ovh was verified as official when first introduced to the cask
+  # broth.itch.ovh/ was verified as official when first introduced to the cask
   url "https://broth.itch.ovh/itch-setup/darwin-amd64/#{version}/archive/default"
   appcast 'https://github.com/itchio/itch/releases.atom'
   name 'itch.io'
