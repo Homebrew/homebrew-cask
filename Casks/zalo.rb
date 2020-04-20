2387056e2b1800098cc7ac5e73af781c3a04fc61
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:03:34 +0100

    zalo.rb: Add trailing slash to verified comment

--- Casks/zalo.rb
+++ Casks/zalo.rb
@@ -2,7 +2,7 @@ cask 'zalo' do
   version '20.3.3'
   sha256 'ae2b694e8eebc2a582a1cbe11d99d8dd18e65aed449271f78bf5c777197d1a26'
 
-  # res-zaloapp-aka.zdn.vn/mac was verified as official when first introduced to the cask
+  # res-zaloapp-aka.zdn.vn/mac/ was verified as official when first introduced to the cask
   url "https://res-zaloapp-aka.zdn.vn/mac/ZaloSetup_#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?user_agent=Macintosh&url=https://zalo.me/download/zalo-pc'
   name 'Zalo'
