902ea2d5349745fe4bc3257dd72df82fab1155d9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:25 +0100

    ring.rb: Add trailing slash to verified comment

--- Casks/ring.rb
+++ Casks/ring.rb
@@ -2,7 +2,7 @@ cask 'ring' do
   version '2.5.0'
   sha256 'eab1d877c8812017e416683c2ceea4e5b8a200e662ea84c5109817d8799c5c47'
 
-  # ring-mac-app-assets.s3.amazonaws.com was verified as official when first introduced to the cask
+  # ring-mac-app-assets.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://ring-mac-app-assets.s3.amazonaws.com/production/Ring_#{version}.zip"
   appcast 'https://ring-mac-app-assets.s3.amazonaws.com/production/ring-appcast.xml'
   name 'Ring'
