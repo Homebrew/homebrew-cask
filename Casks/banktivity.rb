172351cb0483c2ed794cc1df6706be4da090d455
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:11:10 +0100

    banktivity.rb: Add trailing slash to verified comment

--- Casks/banktivity.rb
+++ Casks/banktivity.rb
@@ -2,7 +2,7 @@ cask 'banktivity' do
   version '7.5'
   sha256 '7595e63628dcb7605499cf1c6200d05470d5c676f395413b44c4c0ea29c6f185'
 
-  # iggsoft.com was verified as official when first introduced to the cask
+  # iggsoft.com/ was verified as official when first introduced to the cask
   url "https://www.iggsoft.com/banktivity/Banktivity#{version.major}_Web.dmg"
   appcast 'https://www.iggsoftware.com/product-versions.json'
   name 'Banktivity'
