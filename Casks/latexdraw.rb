2938769a49c2e35b099eb24bdccafa34beff73b2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:28 +0100

    latexdraw.rb: Add trailing slash to verified comment

--- Casks/latexdraw.rb
+++ Casks/latexdraw.rb
@@ -2,7 +2,7 @@ cask 'latexdraw' do
   version '3.3.9'
   sha256 '1197536a5ce3b343e85696fbc5f2821092e885086760d9af4bfe11c073551f6b'
 
-  # downloads.sourceforge.net/latexdraw was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/latexdraw/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/latexdraw/LaTeXDraw-#{version}.app.zip"
   appcast 'https://sourceforge.net/projects/latexdraw/rss?path=/latexdraw'
   name 'LaTexDraw'
