c28c0f4317700c277c44f50b42d4eda757fc0499
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:40:26 +0100

    ximalaya.rb: Add trailing slash to verified comment

--- Casks/ximalaya.rb
+++ Casks/ximalaya.rb
@@ -2,7 +2,7 @@ cask 'ximalaya' do
   version '1.2.16_1587033803'
   sha256 '4f07a12a3ebc3bce405e502931ff1a890efedc808ef6494eebf2b8cdc11e0e52'
 
-  # s1.xmcdn.com was verified as official when first introduced to the cask
+  # s1.xmcdn.com/ was verified as official when first introduced to the cask
   url "https://s1.xmcdn.com/yx/ximalaya-pc-client/last/download/Ximalaya-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.ximalaya.com/down/lite?client=mac'
   name 'ximalaya'
