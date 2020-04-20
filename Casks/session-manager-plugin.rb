c8417d51437b51932e3d95a98a9b9514de4226e8
Luke Hanley <luke.handle@googlemail.com>
2020-04-20 23:31:32 +0100

    Add appcast for AWS session-manager-plugin (1.1.61.0)

--- Casks/session-manager-plugin.rb
+++ Casks/session-manager-plugin.rb
@@ -4,6 +4,7 @@ cask 'session-manager-plugin' do
 
   # session-manager-downloads.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://session-manager-downloads.s3.amazonaws.com/plugin/#{version}/mac/sessionmanager-bundle.zip"
+  appcast 'https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html'
   name 'Session Manager Plugin for the AWS CLI'
   homepage 'https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html'
 
