9be7b9c5393b2ac8d395e7861c7f8dfca578fcb3
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:41:03 +0100

    knockknock.rb: Add trailing slash to verified comment

--- Casks/knockknock.rb
+++ Casks/knockknock.rb
@@ -2,7 +2,7 @@ cask 'knockknock' do
   version '2.1.1'
   sha256 'c3293e6f6524b557246a9c5e06a1fb2eb3f49be2297fe671d0fa8fca7aa142fa'
 
-  # bitbucket.org/objective-see was verified as official when first introduced to the cask
+  # bitbucket.org/objective-see/ was verified as official when first introduced to the cask
   url "https://bitbucket.org/objective-see/deploy/downloads/KnockKnock_#{version}.zip"
   appcast 'https://objective-see.com/products/changelogs/KnockKnock.txt'
   name 'KnockKnock'
