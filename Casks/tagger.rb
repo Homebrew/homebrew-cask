7910a73118d20bba4c336b883fc13e66ac9218d0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:43:06 +0100

    tagger.rb: Add trailing slash to verified comment

--- Casks/tagger.rb
+++ Casks/tagger.rb
@@ -2,7 +2,7 @@ cask 'tagger' do
   version '1.8.0.7'
   sha256 'a4745dcf88f1691d2c681a87e6cfb6326200b6a2d9dfb53c2c62c67905a09e16'
 
-  # github.com/Bilalh/Tagger was verified as official when first introduced to the cask
+  # github.com/Bilalh/Tagger/ was verified as official when first introduced to the cask
   url "https://github.com/Bilalh/Tagger/releases/download/1.8.0/Tagger_#{version}.zip"
   appcast 'https://github.com/Bilalh/Tagger/releases.atom'
   name 'Tagger'
