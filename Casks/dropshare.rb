d0338415e7d6581174e4c3b6fea0561515b78d73
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:57:39 +0100

    dropshare.rb: Add trailing slash to verified comment

--- Casks/dropshare.rb
+++ Casks/dropshare.rb
@@ -2,7 +2,7 @@ cask 'dropshare' do
   version '5.6,5128'
   sha256 '23bbe1e64965bb7cfd07511c0b05e7ed125eef8e9283e3fee9ed634487ac6704'
 
-  # d2wvuuix8c9e48.cloudfront.net was verified as official when first introduced to the cask
+  # d2wvuuix8c9e48.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d2wvuuix8c9e48.cloudfront.net/Dropshare#{version.major}-#{version.after_comma}.app.zip"
   appcast "https://dropshare.app/sparkle/Dropshare#{version.major}.xml"
   name 'Dropshare'
