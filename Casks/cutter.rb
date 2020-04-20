9d19944a98f02ea0b202800cf15101fbfddd4c34
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:47:29 +0100

    cutter.rb: Add trailing slash to verified comment

--- Casks/cutter.rb
+++ Casks/cutter.rb
@@ -2,7 +2,7 @@ cask 'cutter' do
   version '1.10.2'
   sha256 'c999722f7ba4442c3cd9aa619baec2389459526a243c0af48697e18de5893eba'
 
-  # github.com/radareorg/cutter was verified as official when first introduced to the cask
+  # github.com/radareorg/cutter/ was verified as official when first introduced to the cask
   url "https://github.com/radareorg/cutter/releases/download/v#{version}/Cutter-v#{version}-x64.macOS.dmg"
   appcast 'https://github.com/radareorg/cutter/releases.atom'
   name 'Cutter'
