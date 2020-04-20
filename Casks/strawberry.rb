3b8b9392ede62b2de0c7224164a3f882054887e8
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:03:04 +0100

    strawberry.rb: Add trailing slash to verified comment

--- Casks/strawberry.rb
+++ Casks/strawberry.rb
@@ -2,7 +2,7 @@ cask 'strawberry' do
   version '0.6.9'
   sha256 'ce9c6ca46a86b7b7f9456e9351d181ca33400508263f44dde91c3e495d994d20'
 
-  # github.com/strawberrymusicplayer/strawberry was verified as official when first introduced to the cask
+  # github.com/strawberrymusicplayer/strawberry/ was verified as official when first introduced to the cask
   url "https://github.com/strawberrymusicplayer/strawberry/releases/download/#{version}/strawberry-#{version}.dmg"
   appcast 'https://github.com/strawberrymusicplayer/strawberry/releases.atom'
   name 'Strawberry'
