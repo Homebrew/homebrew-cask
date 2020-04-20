f25c53283c92c570412fd0a8919c7cc7b339dfae
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:30:24 +0100

    itubedownloader.rb: Add trailing slash to verified comment

--- Casks/itubedownloader.rb
+++ Casks/itubedownloader.rb
@@ -2,7 +2,7 @@ cask 'itubedownloader' do
   version '6.5.16'
   sha256 '46592a86a834e71b0b23edd6e6e2520c2515c79149f6a213cdf30e8b7166a7c1'
 
-  # itubedownloader.s3.us-east-2.amazonaws.com was verified as official when first introduced to the cask
+  # itubedownloader.s3.us-east-2.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://itubedownloader.s3.us-east-2.amazonaws.com/iTubeDownloader.dmg'
   appcast 'https://itubedownloader.s3.us-east-2.amazonaws.com/appcast.xml'
   name 'iTubeDownloader'
