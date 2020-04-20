a81956f48afea394cc1dcfb7199d8fa62992f02b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:22:47 +0100

    dialpad.rb: Add trailing slash to verified comment

--- Casks/dialpad.rb
+++ Casks/dialpad.rb
@@ -2,7 +2,7 @@ cask 'dialpad' do
   version :latest
   sha256 :no_check
 
-  # storage.googleapis.com/dialpad_native/osx was verified as official when first introduced to the cask
+  # storage.googleapis.com/dialpad_native/osx/ was verified as official when first introduced to the cask
   url 'https://storage.googleapis.com/dialpad_native/osx/Dialpad.dmg'
   name 'Dialpad'
   homepage 'https://dialpad.com/download'
