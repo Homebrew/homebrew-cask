309306323b4ecad2d8dbb6767ec72cec94f7c6de
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:48:27 +0100

    intelliscape-caffeine.rb: Add trailing slash to verified comment

--- Casks/intelliscape-caffeine.rb
+++ Casks/intelliscape-caffeine.rb
@@ -2,7 +2,7 @@ cask 'intelliscape-caffeine' do
   version '1.1.2'
   sha256 '365367f8b3314ffabc1d821f996943755ebc4f90f5ca1433f3123ddb62e1835e'
 
-  # cl.ly/2d0e01d909e4 was verified as official when first introduced to the cask
+  # cl.ly/2d0e01d909e4/ was verified as official when first introduced to the cask
   url 'https://cl.ly/2d0e01d909e4/download/Caffeine.dmg'
   appcast 'https://intelliscapesolutions.com/apps/caffeine/releasenotes'
   name 'Caffeine'
