53c1a96cc5564a01a9476887f5a0ab0e9e909df2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:10:07 +0100

    ngrok.rb: Add trailing slash to verified comment

--- Casks/ngrok.rb
+++ Casks/ngrok.rb
@@ -2,7 +2,7 @@ cask 'ngrok' do
   version :latest
   sha256 :no_check
 
-  # bin.equinox.io was verified as official when first introduced to the cask
+  # bin.equinox.io/ was verified as official when first introduced to the cask
   url 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip'
   name 'ngrok'
   homepage 'https://ngrok.com/'
