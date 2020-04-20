9315c0c0e0a77d9ca3ba80079e15cc144ac7a4e5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:49:45 +0100

    detectx-swift.rb: Add trailing slash to verified comment

--- Casks/detectx-swift.rb
+++ Casks/detectx-swift.rb
@@ -2,7 +2,7 @@ cask 'detectx-swift' do
   version '1.093'
   sha256 '017657d7eab9f423010378262fe00ec4d90a81e37f11166d98dc52b62531e3a0'
 
-  # s3.amazonaws.com/sqwarq.com was verified as official when first introduced to the cask
+  # s3.amazonaws.com/sqwarq.com/ was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/sqwarq.com/PublicZips/DetectX_Swift.dmg'
   appcast 'https://s3.amazonaws.com/sqwarq.com/AppCasts/dtxswift.xml'
   name 'DetectX Swift'
