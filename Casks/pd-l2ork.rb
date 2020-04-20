f7bfba398442f2a23ce534bb008448c066aef5b3
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:51:13 +0100

    pd-l2ork.rb: Add trailing slash to verified comment

--- Casks/pd-l2ork.rb
+++ Casks/pd-l2ork.rb
@@ -2,7 +2,7 @@ cask 'pd-l2ork' do
   version '2.10.1'
   sha256 '3c899164df5bd753bb9fecf0c406af318a611acdb75c9175407aa304ff02e432'
 
-  # github.com/agraef/purr-data was verified as official when first introduced to the cask
+  # github.com/agraef/purr-data/ was verified as official when first introduced to the cask
   url "https://github.com/agraef/purr-data/releases/download/#{version}/pd-l2ork-#{version}-osx_10.11-x86_64-dmg.zip"
   appcast 'https://github.com/agraef/purr-data/releases.atom'
   name 'Pd-l2ork'
