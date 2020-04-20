57e2c8648d6621cb408c5333436fb49dc3fd1f2e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:11 +0100

    gdlauncher.rb: Add trailing slash to verified comment

--- Casks/gdlauncher.rb
+++ Casks/gdlauncher.rb
@@ -2,7 +2,7 @@ cask 'gdlauncher' do
   version '0.12.4'
   sha256 'd73f751659443117b0bf8948e3e74494f96a1c2060cdb1dcaa21854d270209f3'
 
-  # github.com/gorilla-devs/GDLauncher was verified as official when first introduced to the cask
+  # github.com/gorilla-devs/GDLauncher/ was verified as official when first introduced to the cask
   url "https://github.com/gorilla-devs/GDLauncher/releases/download/v#{version}/GDLauncher-mac-setup.zip"
   appcast 'https://github.com/gorilla-devs/GDLauncher/releases.atom'
   name 'GDLauncher'
