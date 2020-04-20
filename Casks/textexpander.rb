e4474e3eaf19e33de08f690ea20daa032b2717d6
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:00:32 +0100

    textexpander.rb: Add trailing slash to verified comment

--- Casks/textexpander.rb
+++ Casks/textexpander.rb
@@ -2,7 +2,7 @@ cask 'textexpander' do
   version '6.5.4'
   sha256 'b4dc8f951902c17e09b549940d7f5f5913b6dbf7b154c807a4f0cc8d9b68ca7e'
 
-  # cdn.textexpander.com/mac was verified as official when first introduced to the cask
+  # cdn.textexpander.com/mac/ was verified as official when first introduced to the cask
   url "https://cdn.textexpander.com/mac/TextExpander_#{version}.zip"
   appcast "https://smilesoftware.com/appcast/TextExpander#{version.major}.xml"
   name 'TextExpander'
