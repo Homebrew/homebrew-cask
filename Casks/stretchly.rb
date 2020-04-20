cb5df91e78a22100649f77a2be2098d3ada9a498
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:54:36 +0100

    stretchly.rb: Add trailing slash to verified comment

--- Casks/stretchly.rb
+++ Casks/stretchly.rb
@@ -2,7 +2,7 @@ cask 'stretchly' do
   version '0.21.1'
   sha256 'b3c36fcabbc33b13f9aca772c674301a3f13b1263318399bbb298565170030dd'
 
-  # github.com/hovancik/stretchly was verified as official when first introduced to the cask
+  # github.com/hovancik/stretchly/ was verified as official when first introduced to the cask
   url "https://github.com/hovancik/stretchly/releases/download/v#{version}/stretchly-#{version}-mac.zip"
   appcast 'https://github.com/hovancik/stretchly/releases.atom'
   name 'stretchly'
