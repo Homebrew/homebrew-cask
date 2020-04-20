57fb5b02f9226231d3f1156debdb156d1e70617f
Luke Hanley <luke.handle@googlemail.com>
2020-04-20 16:01:38 +0100

    Add proper versioning for AWS session-manager-plugin (1.1.61.0)

--- Casks/session-manager-plugin.rb
+++ Casks/session-manager-plugin.rb
@@ -1,9 +1,9 @@
 cask 'session-manager-plugin' do
-  version :latest
-  sha256 :no_check
+  version '1.1.61.0'
+  sha256 '426615b12b2d7728504ee7896c60fa330534314d6cb64ee8cb3ea0488e7d66e4'
 
   # session-manager-downloads.s3.amazonaws.com/ was verified as official when first introduced to the cask
-  url 'https://session-manager-downloads.s3.amazonaws.com/plugin/latest/mac/sessionmanager-bundle.zip'
+  url "https://session-manager-downloads.s3.amazonaws.com/plugin/#{version}/mac/sessionmanager-bundle.zip"
   name 'Session Manager Plugin for the AWS CLI'
   homepage 'https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html'
 
