6e086b933e4546fb224ce0231d3ecb3778b0caa2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:53:52 +0100

    pynsource.rb: Add trailing slash to verified comment

--- Casks/pynsource.rb
+++ Casks/pynsource.rb
@@ -2,7 +2,7 @@ cask 'pynsource' do
   version '1.75'
   sha256 '8382e742d5cfefd429cfbcd00c5e5446a940038cfe8511e5f2a3f6bcbbcccd91'
 
-  # github.com/abulka/pynsource was verified as official when first introduced to the cask
+  # github.com/abulka/pynsource/ was verified as official when first introduced to the cask
   url "https://github.com/abulka/pynsource/releases/download/version-#{version}/pynsource-#{version}-macosx.zip"
   appcast 'https://github.com/abulka/pynsource/releases.atom'
   name 'Pynsource'
