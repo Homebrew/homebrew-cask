cca1a8b1a4d42e99426c7b014d599bed9d1f2ac0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:45:21 +0100

    xdm.rb: Add trailing slash to verified comment

--- Casks/xdm.rb
+++ Casks/xdm.rb
@@ -2,7 +2,7 @@ cask 'xdm' do
   version '7.2.7'
   sha256 'b28d3aac96fe9a2ce161bdd4462fe270eac3dd3ce91828f0e308b9d77aa811b4'
 
-  # downloads.sourceforge.net/xdman was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/xdman/ was verified as official when first introduced to the cask
   url 'https://downloads.sourceforge.net/xdman/xdm-setup.dmg'
   appcast 'https://sourceforge.net/projects/xdman/rss'
   name 'Xtreme Download Manager'
