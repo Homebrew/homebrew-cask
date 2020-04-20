20cf8914a2e361a948ddc7b18b46b11b6618c4ea
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:08:40 +0100

    fuwari.rb: Add trailing slash to verified comment

--- Casks/fuwari.rb
+++ Casks/fuwari.rb
@@ -2,7 +2,7 @@ cask 'fuwari' do
   version '0.5.1'
   sha256 'dd72adc918e9984548a20a513bd3d4319e63ee78ca688ee75546103a16d9f3c2'
 
-  # github.com/kentya6/Fuwari was verified as official when first introduced to the cask
+  # github.com/kentya6/Fuwari/ was verified as official when first introduced to the cask
   url "https://github.com/kentya6/Fuwari/releases/download/v#{version}/Fuwari.zip"
   appcast 'https://github.com/kentya6/Fuwari/releases.atom'
   name 'Fuwari'
