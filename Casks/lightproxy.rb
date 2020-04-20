4820467a262b63d6ee0fbe53eabc06c01f05da0c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:25:00 +0100

    lightproxy.rb: Add trailing slash to verified comment

--- Casks/lightproxy.rb
+++ Casks/lightproxy.rb
@@ -2,7 +2,7 @@ cask 'lightproxy' do
   version '1.1.11'
   sha256 '6c8be69236e3b9f4ba64370666fd30287e84cd7e67911908580aa13af2c0e8e3'
 
-  # gw.alipayobjects.com/os/LightProxy was verified as official when first introduced to the cask
+  # gw.alipayobjects.com/os/LightProxy/ was verified as official when first introduced to the cask
   url 'https://gw.alipayobjects.com/os/LightProxy/LightProxy.dmg'
   appcast 'https://github.com/alibaba/lightproxy/tree/develop/CHANGELOG'
   name 'LightProxy'
