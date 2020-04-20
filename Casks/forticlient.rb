1c3e7be5c0220f8c3ddeb5043959082dbfb2564a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:33:41 +0100

    forticlient.rb: Add trailing slash to verified comment

--- Casks/forticlient.rb
+++ Casks/forticlient.rb
@@ -2,7 +2,7 @@ cask 'forticlient' do
   version '6.2'
   sha256 '13da8e4952797a6a46f5762edc7b378cf53fd31af61d5d27b5fecee2cbf7a14c'
 
-  # filestore.fortinet.com/forticlient was verified as official when first introduced to the cask
+  # filestore.fortinet.com/forticlient/ was verified as official when first introduced to the cask
   url "https://filestore.fortinet.com/forticlient/downloads/FortiClientVPNOnlineInstaller_#{version}.dmg"
   name 'FortiClient'
   homepage 'https://forticlient.com/'
