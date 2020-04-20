7e6a7e09ee2f673282d733baac5eb0f95d2121b8
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:15:07 +0100

    everweb.rb: Add trailing slash to verified comment

--- Casks/everweb.rb
+++ Casks/everweb.rb
@@ -2,7 +2,7 @@ cask 'everweb' do
   version :latest
   sha256 :no_check
 
-  # ragesw.com was verified as official when first introduced to the cask
+  # ragesw.com/ was verified as official when first introduced to the cask
   url 'https://www.ragesw.com/files/everweb/everweb.dmg'
   name 'EverWeb'
   homepage 'https://www.everwebapp.com/'
