164f139283854b6c3282e40fd538be4af62a3049
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:59:06 +0100

    blockstack.rb: Add trailing slash to verified comment

--- Casks/blockstack.rb
+++ Casks/blockstack.rb
@@ -2,7 +2,7 @@ cask 'blockstack' do
   version '0.37.0'
   sha256 'c51a900432b7b7c2885ccd285027ff7c9656f87e934d54bb48dbfbae43ccdf32'
 
-  # github.com/blockstack/blockstack-browser was verified as official when first introduced to the cask
+  # github.com/blockstack/blockstack-browser/ was verified as official when first introduced to the cask
   url "https://github.com/blockstack/blockstack-browser/releases/download/v#{version}/Blockstack-for-macOS-v#{version}.dmg"
   appcast 'https://github.com/blockstack/blockstack-browser/releases.atom'
   name 'Blockstack'
