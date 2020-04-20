7dc817ef002fa167163ab7b3d43f795083efc335
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:18:53 +0100

    frhelper.rb: Add trailing slash to verified comment

--- Casks/frhelper.rb
+++ Casks/frhelper.rb
@@ -2,7 +2,7 @@ cask 'frhelper' do
   version '3.9.6,2020-02-25'
   sha256 'ec638576b929dfd9c2637b5dd152c59fc05812bdfa670c2419d056e329c6e44b'
 
-  # static.frdic.com was verified as official when first introduced to the cask
+  # static.frdic.com/ was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/fhmac.dmg?v=#{version.after_comma}"
   appcast 'https://www.eudic.net/v4/fr/app/download',
           configuration: version.after_comma
