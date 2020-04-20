7c376e13f35c87fe25e15545254c89d9fed9b3b6
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:28 +0100

    mockuuups-studio.rb: Add trailing slash to verified comment

--- Casks/mockuuups-studio.rb
+++ Casks/mockuuups-studio.rb
@@ -2,7 +2,7 @@ cask 'mockuuups-studio' do
   version '2.5.0'
   sha256 'f09358f867efdbd4927a25dcb3805a964e0374ec7e3b38a47ea46cc035a77354'
 
-  # mockuuups.com was verified as official when first introduced to the cask
+  # mockuuups.com/ was verified as official when first introduced to the cask
   url "https://binaries.mockuuups.com/Mockuuups%20Studio-#{version}-mac.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://mockuuups.studio/download/mac/'
   name 'Mockuuups Studio'
