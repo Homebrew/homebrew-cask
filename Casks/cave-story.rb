914581e5ad8586599cc28f4f5ec4ec2c7e11d6ee
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:04:51 +0100

    cave-story.rb: Add trailing slash to verified comment

--- Casks/cave-story.rb
+++ Casks/cave-story.rb
@@ -2,7 +2,7 @@ cask 'cave-story' do
   version '0.1.0'
   sha256 '1d1920e8108ef64616c08524c4a4fad9808329d075428999e48b87eaccc32ce2'
 
-  # nakiwo.com was verified as official when first introduced to the cask
+  # nakiwo.com/ was verified as official when first introduced to the cask
   url "https://www.nakiwo.com/downloads/doukutsu#{version.dots_to_underscores}.dmg"
   appcast 'https://www.nakiwo.com/software.html'
   name 'Pixel Cave Story'
