bc97fe2505675e82a7311993d50bb85f4eba09d4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:55:37 +0100

    ipepresenter.rb: Add trailing slash to verified comment

--- Casks/ipepresenter.rb
+++ Casks/ipepresenter.rb
@@ -2,7 +2,7 @@ cask 'ipepresenter' do
   version '7.2.14'
   sha256 '01425a521034711305f28c43a713d83ab96793f54be5a87a24de89ac8c242fbf'
 
-  # bintray.com/otfried was verified as official when first introduced to the cask
+  # bintray.com/otfried/ was verified as official when first introduced to the cask
   url "https://dl.bintray.com/otfried/generic/ipe/#{version.major_minor}/ipepresenter-#{version}-mac-10.10.dmg"
   appcast 'http://ipepresenter.otfried.org/'
   name 'IpePresenter'
