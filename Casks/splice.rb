28adae84c02207cf2513e25b4d2ad5ddaaca967b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:06 +0100

    splice.rb: Add trailing slash to verified comment

--- Casks/splice.rb
+++ Casks/splice.rb
@@ -2,7 +2,7 @@ cask 'splice' do
   version '3.6.2'
   sha256 '8e7992b1b49057c3f8d31d84383583073dd865cf03fdcdf9dcb6dd4ed51b7f45'
 
-  # splicedesktop.s3.amazonaws.com was verified as official when first introduced to the cask
+  # splicedesktop.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://splicedesktop.s3.amazonaws.com/darwin/stable/Splice.app.zip'
   appcast 'https://api.splice.com/v2/desktop/darwin/stable/latest?v=0.0.0'
   name 'Splice'
