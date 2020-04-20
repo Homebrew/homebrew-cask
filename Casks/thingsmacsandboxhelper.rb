1d233f248a3b6b41c20425fa01a038880dca36d2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:07 +0100

    thingsmacsandboxhelper.rb: Add trailing slash to verified comment

--- Casks/thingsmacsandboxhelper.rb
+++ Casks/thingsmacsandboxhelper.rb
@@ -2,7 +2,7 @@ cask 'thingsmacsandboxhelper' do
   version '3.20'
   sha256 '0f140b820a77afeb33d449691e8a5e5f1e36b52e48447336dbf4c45eed0da497'
 
-  # culturedcode.cachefly.net was verified as official when first introduced to the cask
+  # culturedcode.cachefly.net/ was verified as official when first introduced to the cask
   url "https://culturedcode.cachefly.net/things/thingssandboxhelper/#{version}/ThingsHelper.zip"
   appcast 'https://culturedcode.com/things/mac/help/things-sandboxing-helper-things3/'
   name 'ThingsMacSandboxHelper'
