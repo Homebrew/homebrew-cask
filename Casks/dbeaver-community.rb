5844e6049563126d7db5630ea455f52d3bbe88a7
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:09:40 +0100

    dbeaver-community.rb: Add trailing slash to verified comment

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -2,7 +2,7 @@ cask 'dbeaver-community' do
   version '7.0.2'
   sha256 '9a2d0c0547e6b11468aec067cb33ee4c19b2fa2a5b2d637f11cd7b291a70fa92'
 
-  # github.com/dbeaver/dbeaver was verified as official when first introduced to the cask
+  # github.com/dbeaver/dbeaver/ was verified as official when first introduced to the cask
   url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-macos.dmg"
   appcast 'https://github.com/dbeaver/dbeaver/releases.atom'
   name 'DBeaver Community Edition'
