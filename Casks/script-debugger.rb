39323530d027fa56f57e68797340d4417279230f
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:32:25 +0100

    script-debugger.rb: Add trailing slash to verified comment

--- Casks/script-debugger.rb
+++ Casks/script-debugger.rb
@@ -2,7 +2,7 @@ cask 'script-debugger' do
   version '7.0.11-7A110'
   sha256 'eaf7852be5b0595128f484ae9d97b81079feb62349ab7a8fed28cfa09b65ec38'
 
-  # s3.amazonaws.com/latenightsw.com was verified as official when first introduced to the cask
+  # s3.amazonaws.com/latenightsw.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/latenightsw.com/ScriptDebugger#{version}.dmg"
   appcast "https://www.latenightsw.com/versions/com.latenightsw.ScriptDebugger#{version.major}.php"
   name 'Script Debugger'
