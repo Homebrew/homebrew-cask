767f65c90968ae0de8473d1bc4448f063b730a29
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:35:56 +0100

    captin.rb: Add trailing slash to verified comment

--- Casks/captin.rb
+++ Casks/captin.rb
@@ -2,7 +2,7 @@ cask 'captin' do
   version '1.1.0,119:1585846526'
   sha256 '79e75a8a3b6b6d576b9f06564bca033d7f2fe6b456484d15043a08511b495491'
 
-  # dl.devmate.com/com.100hps.captin was verified as official when first introduced to the cask
+  # dl.devmate.com/com.100hps.captin/ was verified as official when first introduced to the cask
   url "https://dl.devmate.com/com.100hps.captin/#{version.after_comma.before_colon}/#{version.after_colon}/Captin-#{version.after_comma.before_colon}.dmg"
   appcast 'https://updates.devmate.com/com.100hps.captin.xml'
   name 'Captin'
