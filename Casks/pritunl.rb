50a0c50d06ac85c7c80c6e1b0bc55041e14569d8
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:42:07 +0100

    pritunl.rb: Add trailing slash to verified comment

--- Casks/pritunl.rb
+++ Casks/pritunl.rb
@@ -2,7 +2,7 @@ cask 'pritunl' do
   version '1.0.2404.6'
   sha256 '90e83ac8762694204aa9092ccf0ad951800314c98b2e07c1d092c6805e02ba5d'
 
-  # github.com/pritunl/pritunl-client-electron was verified as official when first introduced to the cask
+  # github.com/pritunl/pritunl-client-electron/ was verified as official when first introduced to the cask
   url "https://github.com/pritunl/pritunl-client-electron/releases/download/#{version}/Pritunl.pkg.zip"
   appcast 'https://github.com/pritunl/pritunl-client-electron/releases.atom'
   name 'Pritunl OpenVPN Client'
