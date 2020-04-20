890462d7caba92920c4426b922cfea8c177eee1f
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:45:06 +0100

    bootxchanger.rb: Add trailing slash to verified comment

--- Casks/bootxchanger.rb
+++ Casks/bootxchanger.rb
@@ -2,7 +2,7 @@ cask 'bootxchanger' do
   version '2.0'
   sha256 'cfb05680ab6a7d0b1793a33135dd15562a7b5fd59bb1ebf3ad6067c2c9fad6c1'
 
-  # github.com/zydeco/bootxchanger was verified as official when first introduced to the cask
+  # github.com/zydeco/bootxchanger/ was verified as official when first introduced to the cask
   url "https://github.com/zydeco/bootxchanger/releases/download/v#{version}/bootxchanger_#{version}.dmg"
   appcast 'https://github.com/zydeco/bootxchanger/releases.atom'
   name 'BootXChanger'
