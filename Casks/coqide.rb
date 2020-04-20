41f6241925490afb4871dcda603f4da9504a1b5c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:27:17 +0100

    coqide.rb: Add trailing slash to verified comment

--- Casks/coqide.rb
+++ Casks/coqide.rb
@@ -2,7 +2,7 @@ cask 'coqide' do
   version '8.11.0'
   sha256 '6cce1ec6502a3086d7e02c59a6903fad082e81a3f8a8ee03be21179ab25a08cf'
 
-  # github.com/coq/coq was verified as official when first introduced to the cask
+  # github.com/coq/coq/ was verified as official when first introduced to the cask
   url "https://github.com/coq/coq/releases/download/V#{version.major_minor_patch}/coq-#{version}-installer-macos.dmg"
   appcast 'https://github.com/coq/coq/releases.atom'
   name 'Coq'
