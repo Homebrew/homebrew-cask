1634cb802af51f7f3150539624fa711991f0cdba
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:15:16 +0100

    bettertouchtool.rb: Add trailing slash to verified comment

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -3,13 +3,13 @@ cask 'bettertouchtool' do
     version '2.05'
     sha256 '41013cfeffee286a038363651db3dd315ff3a1e0cf07774d9ce852111be50a5a'
 
-    # bettertouchtool.net/releases was verified as official when first introduced to the cask
+    # bettertouchtool.net/releases/ was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}_final_10_9.zip"
   else
     version '3.362-1578'
     sha256 '3a50e64d2229b2eaf9487f60955a8716fd042a6c5f914f90b2dd0d339e4294be'
 
-    # bettertouchtool.net/releases was verified as official when first introduced to the cask
+    # bettertouchtool.net/releases/ was verified as official when first introduced to the cask
     url "https://bettertouchtool.net/releases/btt#{version}.zip"
     appcast 'https://www.corecode.io/macupdater/appcasts/bettertouchtool.txt'
   end
