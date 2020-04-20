a6982765e5017c7d2957718fdc77b52d982598c9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:38:49 +0100

    digikam.rb: Add trailing slash to verified comment

--- Casks/digikam.rb
+++ Casks/digikam.rb
@@ -2,7 +2,7 @@ cask 'digikam' do
   version '6.4.0'
   sha256 '714ea60db0125bdb38b434aa9d98a3c8b7c3a23869b6f5d397058552a8af6e11'
 
-  # kde.org/stable/digikam was verified as official when first introduced to the cask
+  # kde.org/stable/digikam/ was verified as official when first introduced to the cask
   url "https://download.kde.org/stable/digikam/#{version}/digiKam-#{version}-MacOS-x86-64.pkg"
   appcast 'https://download.kde.org/stable/digikam/'
   name 'digiKam'
