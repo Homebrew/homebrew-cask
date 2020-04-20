be2c0169924b27117c37bb2de4152c3bc89f9472
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:59:17 +0100

    swift-publisher.rb: Add trailing slash to verified comment

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -2,7 +2,7 @@ cask 'swift-publisher' do
   version '5.5.3'
   sha256 '8c9eb3f31d599d04b36c0ff1dae192960c343adeb18a4439c90cabf5f97c0afa'
 
-  # belightsoft.s3.amazonaws.com was verified as official when first introduced to the cask
+  # belightsoft.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://belightsoft.s3.amazonaws.com/SwiftPublisher.dmg'
   appcast "https://www.belightsoft.com/download/updates/appcast_SwiftPublisher#{version.major}.xml"
   name 'Swift Publisher'
