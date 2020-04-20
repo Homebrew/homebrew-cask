249ce947e3492f6e13532941d2552038010d13c9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:06:09 +0100

    catlight.rb: Add trailing slash to verified comment

--- Casks/catlight.rb
+++ Casks/catlight.rb
@@ -2,7 +2,7 @@ cask 'catlight' do
   version '2.30.0'
   sha256 '7a514b392b2d1e4c12717c5a981eb5e8a10ae53acb29b74a4d5a43f3318b4065'
 
-  # de2nac35bcll0.cloudfront.net was verified as official when first introduced to the cask
+  # de2nac35bcll0.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://de2nac35bcll0.cloudfront.net/dl/mac/beta/CatLightSetup-#{version}.zip"
   appcast 'https://catlight.io/downloads'
   name 'catlight'
