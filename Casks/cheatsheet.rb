1b9fcb03fca956072100a46a37426c6702df5ebe
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:22:05 +0100

    cheatsheet.rb: Add trailing slash to verified comment

--- Casks/cheatsheet.rb
+++ Casks/cheatsheet.rb
@@ -2,7 +2,7 @@ cask 'cheatsheet' do
   version '1.3.4'
   sha256 '7b2929ba987204a62c2f94416e41bf0c3ed03f7883cf3a798e20c679ebc88a3b'
 
-  # mediaatelier.com/CheatSheet was verified as official when first introduced to the cask
+  # mediaatelier.com/CheatSheet/ was verified as official when first introduced to the cask
   url "https://mediaatelier.com/CheatSheet/CheatSheet_#{version}.zip"
   appcast 'https://mediaatelier.com/CheatSheet/feed.php'
   name 'CheatSheet'
