8b03e5f49e1396a956bf281fe22ef2eed94aa6dd
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:48:57 +0100

    free-ruler.rb: Add trailing slash to verified comment

--- Casks/free-ruler.rb
+++ Casks/free-ruler.rb
@@ -2,7 +2,7 @@ cask 'free-ruler' do
   version '2.0.3'
   sha256 'aa428907c113e249a6baedb329e788b10d6db67d9f9117c3986780d2c592da9d'
 
-  # github.com/pascalpp/FreeRuler was verified as official when first introduced to the cask
+  # github.com/pascalpp/FreeRuler/ was verified as official when first introduced to the cask
   url "https://github.com/pascalpp/FreeRuler/releases/download/v#{version}/free-ruler-#{version}.zip"
   appcast 'https://github.com/pascalpp/FreeRuler/releases.atom'
   name 'Free Ruler'
