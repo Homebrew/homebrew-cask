9502b139b53fe9467c5c4335240b7a98fd370558
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:49:14 +0100

    yandex.rb: Add trailing slash to verified comment

--- Casks/yandex.rb
+++ Casks/yandex.rb
@@ -2,7 +2,7 @@ cask 'yandex' do
   version :latest
   sha256 :no_check
 
-  # yandex.net was verified as official when first introduced to the cask
+  # yandex.net/ was verified as official when first introduced to the cask
   url 'https://download.cdn.yandex.net/browser/yandex/ru/Yandex.dmg'
   name 'Yandex.Browser'
   homepage 'https://browser.yandex.ru/desktop/'
