52ddc97c58559be398ed90323e143be76b1a9eca
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:42:35 +0100

    loginputmac.rb: Add trailing slash to verified comment

--- Casks/loginputmac.rb
+++ Casks/loginputmac.rb
@@ -2,7 +2,7 @@ cask 'loginputmac' do
   version '2.3.3'
   sha256 '70ad6061bd453292cecd8d90fa3b10a22ae17eeb41a2462323b5e15b3a0c3f36'
 
-  # loginput-mac2.content-delivery.top was verified as official when first introduced to the cask
+  # loginput-mac2.content-delivery.top/ was verified as official when first introduced to the cask
   url "https://loginput-mac2.content-delivery.top/loginputmac#{version.major}_latest.pkg"
   appcast "https://im.logcg.com/appcast#{version.major}.xml"
   name 'LoginputMac'
