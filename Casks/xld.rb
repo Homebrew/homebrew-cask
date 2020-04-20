6fd4d3eb9b862de684e62690ea57baf147595869
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:19:14 +0100

    xld.rb: Add trailing slash to verified comment

--- Casks/xld.rb
+++ Casks/xld.rb
@@ -2,7 +2,7 @@ cask 'xld' do
   version '2019.10.04'
   sha256 '6fedfede1f54d691b69f0d6dc5477062c57fe6c48b7f86624a47eb1b58a1e4ef'
 
-  # sourceforge.net/xld was verified as official when first introduced to the cask
+  # sourceforge.net/xld/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/xld/xld-#{version.no_dots}.dmg"
   appcast 'https://svn.code.sf.net/p/xld/code/appcast/xld-appcast_e.xml',
           configuration: version.no_dots
