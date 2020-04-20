e5fbd97ae0351c501d5257e1e73c148bdd1368a4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:04:30 +0100

    sage.rb: Add trailing slash to verified comment

--- Casks/sage.rb
+++ Casks/sage.rb
@@ -2,7 +2,7 @@ cask 'sage' do
   version '9.0,10.15.2'
   sha256 '79a2a150c1fe10b4b4b4c436fe2eaf27460a902670b0121529f7ccd3500b55d9'
 
-  # mirrors.mit.edu/sage/osx/intel was verified as official when first introduced to the cask
+  # mirrors.mit.edu/sage/osx/intel/ was verified as official when first introduced to the cask
   url "https://mirrors.mit.edu/sage/osx/intel/sage-#{version.before_comma}-OSX_#{version.after_comma}-x86_64.app.dmg"
   appcast 'https://mirrors.mit.edu/sage/osx/intel/index.html'
   name 'Sage'
