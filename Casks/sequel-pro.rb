c0c17747a0766f80572e1fc0a5745d15c2fb22cc
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:59:44 +0100

    sequel-pro.rb: Add trailing slash to verified comment

--- Casks/sequel-pro.rb
+++ Casks/sequel-pro.rb
@@ -2,7 +2,7 @@ cask 'sequel-pro' do
   version '1.1.2'
   sha256 '7b34fd63c13e9e9ca4f87d548241ff9df9a266b554af23549efd7be006f387c6'
 
-  # github.com/sequelpro/sequelpro was verified as official when first introduced to the cask
+  # github.com/sequelpro/sequelpro/ was verified as official when first introduced to the cask
   url "https://github.com/sequelpro/sequelpro/releases/download/release-#{version}/sequel-pro-#{version}.dmg"
   appcast 'https://github.com/sequelpro/sequelpro/releases.atom'
   name 'Sequel Pro'
