e78514f2532cd3945fe32b7575a16a53fc2b1683
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:16:07 +0100

    sqlpro-for-mssql.rb: Add trailing slash to verified comment

--- Casks/sqlpro-for-mssql.rb
+++ Casks/sqlpro-for-mssql.rb
@@ -2,7 +2,7 @@ cask 'sqlpro-for-mssql' do
   version '2020.35'
   sha256 '3388550d81559c91ca941b869acc5f4a4c9a0bf218524e9a9512e0fe36195589'
 
-  # d3fwkemdw8spx3.cloudfront.net/mssql was verified as official when first introduced to the cask
+  # d3fwkemdw8spx3.cloudfront.net/mssql/ was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/mssql/SQLProMSSQL.#{version}.app.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.macsqlclient.com/download.php'
   name 'SQLPro for MSSQL'
