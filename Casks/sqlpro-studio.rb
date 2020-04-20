b7737de5ed1dc650e92b695358df5d38db254f57
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:30:38 +0100

    sqlpro-studio.rb: Add trailing slash to verified comment

--- Casks/sqlpro-studio.rb
+++ Casks/sqlpro-studio.rb
@@ -2,7 +2,7 @@ cask 'sqlpro-studio' do
   version '2020.35'
   sha256 '46ff97867ad6b15794d89802e84d13aba6e61006a038ce1b7cf933daf310ba2a'
 
-  # d3fwkemdw8spx3.cloudfront.net/studio was verified as official when first introduced to the cask
+  # d3fwkemdw8spx3.cloudfront.net/studio/ was verified as official when first introduced to the cask
   url "https://d3fwkemdw8spx3.cloudfront.net/studio/SQLProStudio.#{version}.app.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sqlprostudio.com/download.php'
   name 'SQLPro Studio'
