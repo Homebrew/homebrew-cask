9d4d0da6b474aea117b62379bfbe328a215875be
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:32:31 +0100

    igdm.rb: Add trailing slash to verified comment

--- Casks/igdm.rb
+++ Casks/igdm.rb
@@ -2,7 +2,7 @@ cask 'igdm' do
   version '2.8.1'
   sha256 '252ea2e15861484fd086be49d22fd104b96764dcc4f244cfcce520ecfd281543'
 
-  # github.com/ifedapoolarewaju/igdm was verified as official when first introduced to the cask
+  # github.com/ifedapoolarewaju/igdm/ was verified as official when first introduced to the cask
   url "https://github.com/ifedapoolarewaju/igdm/releases/download/v#{version}/IGdm-#{version}.dmg"
   appcast 'https://github.com/ifedapoolarewaju/igdm/releases.atom'
   name 'IG:dm'
