aaab965ba0b34e0e4c9712c10b55e828efefaa72
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:32:55 +0100

    get-backup-pro.rb: Add trailing slash to verified comment

--- Casks/get-backup-pro.rb
+++ Casks/get-backup-pro.rb
@@ -2,7 +2,7 @@ cask 'get-backup-pro' do
   version '3.5.1'
   sha256 'b9dce46ade0250931117d03268ec32a58a94e5944297c52ecbd0072e13a2d539'
 
-  # belightsoft.s3.amazonaws.com/updates was verified as official when first introduced to the cask
+  # belightsoft.s3.amazonaws.com/updates/ was verified as official when first introduced to the cask
   url "https://belightsoft.s3.amazonaws.com/updates/Get+Backup+Pro+#{version.major}.zip"
   appcast "https://www.belightsoft.com/download/updates/appcast_getbackup_pro#{version.major}.xml"
   name "Get Backup Pro #{version.major}"
