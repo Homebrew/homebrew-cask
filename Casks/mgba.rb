3757eb1f760d1ab44af7638dc2883f812eeef8bd
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:08:11 +0100

    mgba.rb: Add trailing slash to verified comment

--- Casks/mgba.rb
+++ Casks/mgba.rb
@@ -2,7 +2,7 @@ cask 'mgba' do
   version '0.8.1'
   sha256 '867b6d588351505ffae9e90ee75373fc7044a08dbb2c40ada9b91d12e255b295'
 
-  # github.com/mgba-emu/mgba was verified as official when first introduced to the cask
+  # github.com/mgba-emu/mgba/ was verified as official when first introduced to the cask
   url "https://github.com/mgba-emu/mgba/releases/download/#{version}/mGBA-#{version}-osx.tar.xz"
   appcast 'https://github.com/mgba-emu/mgba/releases.atom'
   name 'mGBA'
