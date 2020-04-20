60abaea4b22ce1bec0b9bbb19810afe177213799
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:21:56 +0100

    wickrme.rb: Add trailing slash to verified comment

--- Casks/wickrme.rb
+++ Casks/wickrme.rb
@@ -2,7 +2,7 @@ cask 'wickrme' do
   version '5.52.9'
   sha256 '3b71dd0f38d047980281ea452649f4983759187881e1500d195dd3d9dcac4e9b'
 
-  # s3.amazonaws.com/static.wickr.com was verified as official when first introduced to the cask
+  # s3.amazonaws.com/static.wickr.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/me/WickrMe-#{version}.dmg"
   appcast 'https://pro-download.wickr.com/api/multiVerify/pro/undefined/'
   name 'Wickr Me'
