560046cc592d665986f0debf4d21f5986accd1bd
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:18:03 +0100

    psi-plus.rb: Add trailing slash to verified comment

--- Casks/psi-plus.rb
+++ Casks/psi-plus.rb
@@ -2,7 +2,7 @@ cask 'psi-plus' do
   version '1.4.1072-macOS10.13'
   sha256 '4b7706dbe5a9c32853d7d513399f770f2812f54756f4a25dc959baad8ceb5246'
 
-  # downloads.sourceforge.net/psiplus was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/psiplus/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/psiplus/Psi+-#{version}-x86_64.dmg"
   appcast 'https://sourceforge.net/projects/psiplus/rss?path=/macOS/tehnick'
   name 'Psi+'
