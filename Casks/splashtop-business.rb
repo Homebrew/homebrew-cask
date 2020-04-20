f14f68d07458c334ab92f2398511aa43e478b3d7
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:09 +0100

    splashtop-business.rb: Add trailing slash to verified comment

--- Casks/splashtop-business.rb
+++ Casks/splashtop-business.rb
@@ -2,7 +2,7 @@ cask 'splashtop-business' do
   version '3.3.6.0'
   sha256 '8a8cd410288be799d5c23ad4a4f0c46549be45f269bceeb93a5a6d8bf9450fd5'
 
-  # d17kmd0va0f0mp.cloudfront.net/macclient/STB was verified as official when first introduced to the cask
+  # d17kmd0va0f0mp.cloudfront.net/macclient/STB/ was verified as official when first introduced to the cask
   url "https://d17kmd0va0f0mp.cloudfront.net/macclient/STB/Splashtop_Business_Mac_INSTALLER_v#{version}.dmg"
   appcast 'https://www.splashtop.com/wp-content/themes/responsive/downloadx.php?product=stb&platform=mac-client'
   name 'Splashtop Business'
