f08c4baf616c09d1a2c903309dbd91935eaadb3e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:22:23 +0100

    universal-media-server.rb: Add trailing slash to verified comment

--- Casks/universal-media-server.rb
+++ Casks/universal-media-server.rb
@@ -2,7 +2,7 @@ cask 'universal-media-server' do
   version '9.4.1'
   sha256 '109efe4930f30c9c2c086ad436a77a7ba35fbd57c190358c7ce1d5b223fcddf9'
 
-  # github.com/UniversalMediaServer/UniversalMediaServer was verified as official when first introduced to the cask
+  # github.com/UniversalMediaServer/UniversalMediaServer/ was verified as official when first introduced to the cask
   url "https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/#{version}/UMS-#{version}.dmg"
   appcast 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases.atom'
   name 'Universal Media Server'
