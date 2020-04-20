57878cd1b6ba28876109fbe98b6bf63a3bb4dc9d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:06:44 +0100

    shoes.rb: Add trailing slash to verified comment

--- Casks/shoes.rb
+++ Casks/shoes.rb
@@ -2,7 +2,7 @@ cask 'shoes' do
   version '3.3.7'
   sha256 '9d2d57d8e64a8befc08848939ac0995e2367aadee3aeb24608ebcc8d5c7bb93d'
 
-  # shoes.mvmanila.com/public/shoes was verified as official when first introduced to the cask
+  # shoes.mvmanila.com/public/shoes/ was verified as official when first introduced to the cask
   url "https://shoes.mvmanila.com/public/shoes/shoes-#{version}-osx-10.10.tgz"
   appcast 'http://shoesrb.com/downloads/'
   name 'Shoes'
