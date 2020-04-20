0e20da62304c2d6fe464c49abd860ea4c176e30a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:28:05 +0100

    terminus.rb: Add trailing slash to verified comment

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -2,7 +2,7 @@ cask 'terminus' do
   version '1.0.106'
   sha256 '8952ab29f0cec4f00e894f0669ad2925ef7dac86455df87741c5334c56100c56'
 
-  # github.com/Eugeny/terminus was verified as official when first introduced to the cask
+  # github.com/Eugeny/terminus/ was verified as official when first introduced to the cask
   url "https://github.com/Eugeny/terminus/releases/download/v#{version}/terminus-#{version}-macos.zip"
   appcast 'https://github.com/Eugeny/terminus/releases.atom'
   name 'Terminus'
