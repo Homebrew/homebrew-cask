b5a9ec6fa9989c629ef530cc0cda8b1be27b18b6
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:53:29 +0100

    emailchemy.rb: Add trailing slash to verified comment

--- Casks/emailchemy.rb
+++ Casks/emailchemy.rb
@@ -2,7 +2,7 @@ cask 'emailchemy' do
   version :latest
   sha256 :no_check
 
-  # wksdownload.s3.amazonaws.com was verified as official when first introduced to the cask
+  # wksdownload.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://wksdownload.s3.amazonaws.com/Emailchemy-Mac.dmg'
   name 'Emailchemy'
   homepage 'https://weirdkid.com/emailchemy/'
