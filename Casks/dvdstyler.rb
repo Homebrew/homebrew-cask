68971556b8ee00efb8bcadc03a97dc297d9f3d10
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:18:12 +0100

    dvdstyler.rb: Add trailing slash to verified comment

--- Casks/dvdstyler.rb
+++ Casks/dvdstyler.rb
@@ -2,7 +2,7 @@ cask 'dvdstyler' do
   version '3.1'
   sha256 'c66d291db8b2dbe9af21b8e210983198b930b02fef1b51866c365bb2493376c5'
 
-  # sourceforge.net/dvdstyler was verified as official when first introduced to the cask
+  # sourceforge.net/dvdstyler/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/dvdstyler/DVDStyler-#{version}-MacOSX.dmg"
   appcast 'https://sourceforge.net/projects/dvdstyler/rss'
   name 'DVDStyler'
