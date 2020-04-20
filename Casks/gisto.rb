0f34697cb7cbb5cde224927ea3ec8fc1f1139625
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:50 +0100

    gisto.rb: Add trailing slash to verified comment

--- Casks/gisto.rb
+++ Casks/gisto.rb
@@ -2,7 +2,7 @@ cask 'gisto' do
   version '1.13.2'
   sha256 '1d314ba1f87202d29b25b2d11a2f832baaf8f0b378e24d6d09de8ef89aeaa308'
 
-  # github.com/Gisto/Gisto was verified as official when first introduced to the cask
+  # github.com/Gisto/Gisto/ was verified as official when first introduced to the cask
   url "https://github.com/Gisto/Gisto/releases/download/v#{version}/Gisto-#{version}.dmg"
   appcast 'https://github.com/Gisto/Gisto/releases.atom'
   name 'Gisto'
