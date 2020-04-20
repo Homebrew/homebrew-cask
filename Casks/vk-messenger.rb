69732a592e43735ea92ea06c5bc752a9ec09af73
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:11:57 +0100

    vk-messenger.rb: Add trailing slash to verified comment

--- Casks/vk-messenger.rb
+++ Casks/vk-messenger.rb
@@ -2,7 +2,7 @@ cask 'vk-messenger' do
   version :latest
   sha256 :no_check
 
-  # desktop.userapi.com/mac/master was verified as official when first introduced to the cask
+  # desktop.userapi.com/mac/master/ was verified as official when first introduced to the cask
   url 'https://desktop.userapi.com/mac/master/vk.dmg'
   name 'VK Messenger'
   homepage 'https://vk.com/messenger'
