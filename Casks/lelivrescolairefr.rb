52fa8941fde37772af3bcec9019e1844da5128df
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:06:15 +0100

    lelivrescolairefr.rb: Add trailing slash to verified comment

--- Casks/lelivrescolairefr.rb
+++ Casks/lelivrescolairefr.rb
@@ -2,7 +2,7 @@ cask 'lelivrescolairefr' do
   version :latest
   sha256 :no_check
 
-  # ci.lls.fr/build/latest/mac was verified as official when first introduced to the cask
+  # ci.lls.fr/build/latest/mac/ was verified as official when first introduced to the cask
   url 'https://ci.lls.fr/build/latest/mac/Lelivrescolaire.fr.dmg'
   name 'Lelivrescolaire.fr'
   homepage 'https://www.lelivrescolaire.fr/'
