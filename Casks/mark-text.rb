7014e6f7fc94d4281033ff90bbff1c1561738dd5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:47:33 +0100

    mark-text.rb: Add trailing slash to verified comment

--- Casks/mark-text.rb
+++ Casks/mark-text.rb
@@ -2,7 +2,7 @@ cask 'mark-text' do
   version '0.16.1'
   sha256 '676fbcb7230e736f6b41b3ee21df864131565dee45b212b658990e7760218962'
 
-  # github.com/marktext/marktext was verified as official when first introduced to the cask
+  # github.com/marktext/marktext/ was verified as official when first introduced to the cask
   url "https://github.com/marktext/marktext/releases/download/v#{version}/marktext.dmg"
   appcast 'https://github.com/marktext/marktext/releases.atom'
   name 'Mark Text'
