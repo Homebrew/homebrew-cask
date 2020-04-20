36cee7e44427c3c54cf50fa6c16db3972e1ae389
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:58:30 +0100

    munki.rb: Add trailing slash to verified comment

--- Casks/munki.rb
+++ Casks/munki.rb
@@ -2,7 +2,7 @@ cask 'munki' do
   version '3.6.4.3786'
   sha256 'c20826da7df6dd7c9b3404e1c348523a5f04b760413dfd7a2b632cb861025d0d'
 
-  # github.com/munki/munki was verified as official when first introduced to the cask
+  # github.com/munki/munki/ was verified as official when first introduced to the cask
   url "https://github.com/munki/munki/releases/download/v#{version.major_minor_patch}/munkitools-#{version}.pkg"
   appcast 'https://github.com/munki/munki/releases.atom',
           configuration: version.major_minor_patch
