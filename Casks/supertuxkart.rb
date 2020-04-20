8095929f8464d081e1b2c25564b7ce242941d25e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:39:52 +0100

    supertuxkart.rb: Add trailing slash to verified comment

--- Casks/supertuxkart.rb
+++ Casks/supertuxkart.rb
@@ -2,7 +2,7 @@ cask 'supertuxkart' do
   version '1.1'
   sha256 'ed1783b2e3220ce3b69a88b39528c101154bdfdca10f4ff4a66d065e802334f3'
 
-  # sourceforge.net/supertuxkart was verified as official when first introduced to the cask
+  # sourceforge.net/supertuxkart/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/supertuxkart/SuperTuxKart-#{version}-mac.zip"
   appcast 'https://supertuxkart.net/Download'
   name 'SuperTuxKart'
