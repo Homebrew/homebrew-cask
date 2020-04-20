a426f811f37fb7080e28cb20865a9253a8d9c215
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:48:51 +0100

    session.rb: Add trailing slash to verified comment

--- Casks/session.rb
+++ Casks/session.rb
@@ -2,7 +2,7 @@ cask 'session' do
   version '1.0.6'
   sha256 '04128f0e2ba8568ffde61a68ddf27b8753064823683ab9fa9889857bc27fb98e'
 
-  # github.com/loki-project/session-desktop was verified as official when first introduced to the cask
+  # github.com/loki-project/session-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/loki-project/session-desktop/releases/download/v#{version}/session-messenger-desktop-mac-#{version}.dmg"
   appcast 'https://github.com/loki-project/session-desktop/releases.atom'
   name 'Session'
