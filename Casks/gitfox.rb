8ab9436cefb6756acfcea77646b023e42a893060
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:09:16 +0100

    gitfox.rb: Add trailing slash to verified comment

--- Casks/gitfox.rb
+++ Casks/gitfox.rb
@@ -2,7 +2,7 @@ cask 'gitfox' do
   version :latest
   sha256 :no_check
 
-  # storage.googleapis.com/gitfox was verified as official when first introduced to the cask
+  # storage.googleapis.com/gitfox/ was verified as official when first introduced to the cask
   url 'https://storage.googleapis.com/gitfox/Gitfox.latest.stable.zip'
   name 'Gitfox'
   homepage 'https://www.gitfox.app/'
