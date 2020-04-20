84f7baab9c6192f432eb5481205073889b886125
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:19 +0100

    startninja.rb: Add trailing slash to verified comment

--- Casks/startninja.rb
+++ Casks/startninja.rb
@@ -2,7 +2,7 @@ cask 'startninja' do
   version :latest
   sha256 :no_check
 
-  # dl2.macupdate.com/prod was verified as official when first introduced to the cask
+  # dl2.macupdate.com/prod/ was verified as official when first introduced to the cask
   url 'https://dl2.macupdate.com/prod/StartNinjaInstaller.dmg'
   name 'StartNinja'
   homepage 'https://www.allvu.com/start_ninja'
