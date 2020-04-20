a228a95935c4c540455345bdd73af29794713909
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:39:24 +0100

    xlink-kai.rb: Add trailing slash to verified comment

--- Casks/xlink-kai.rb
+++ Casks/xlink-kai.rb
@@ -2,7 +2,7 @@ cask 'xlink-kai' do
   version '7.4.35'
   sha256 'ebc988dd6491439b897f8f6e51f870a919f0d416a7d6096a62441c8e7bf05d05'
 
-  # github.com/Team-XLink/releases was verified as official when first introduced to the cask
+  # github.com/Team-XLink/releases/ was verified as official when first introduced to the cask
   url "https://github.com/Team-XLink/releases/releases/download/v#{version}/XLink.Kai.app.zip"
   appcast 'https://github.com/Team-XLink/releases/releases.atom'
   name 'XLink Kai'
