a16f09b3ce349d64cf034e17649f26af33214e0b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:38 +0100

    visualvm.rb: Add trailing slash to verified comment

--- Casks/visualvm.rb
+++ Casks/visualvm.rb
@@ -2,7 +2,7 @@ cask 'visualvm' do
   version '2.0.1'
   sha256 '59a72c18c655fb0874930962ea4588857cc55bc458069371f6d76cb41f1d6b0e'
 
-  # github.com/oracle/visualvm was verified as official when first introduced to the cask
+  # github.com/oracle/visualvm/ was verified as official when first introduced to the cask
   url "https://github.com/oracle/visualvm/releases/download/#{version}/VisualVM_#{version.no_dots}.dmg"
   appcast 'https://github.com/oracle/visualvm/releases.atom'
   name 'VisualVM'
