60c6f45044215c2bd643090d7abfef07ef6d540d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:42 +0100

    glyphfinder.rb: Add trailing slash to verified comment

--- Casks/glyphfinder.rb
+++ Casks/glyphfinder.rb
@@ -2,7 +2,7 @@ cask 'glyphfinder' do
   version '1.2.0'
   sha256 '371374cbb85c72530d5953235ec32efc24457c181dcad7fc058121acaf014be2'
 
-  # ueber.fra1.cdn.digitaloceanspaces.com/glyphfinder was verified as official when first introduced to the cask
+  # ueber.fra1.cdn.digitaloceanspaces.com/glyphfinder/ was verified as official when first introduced to the cask
   url "https://ueber.fra1.cdn.digitaloceanspaces.com/glyphfinder/mac/Glyphfinder-#{version}-mac.dmg"
   appcast 'https://www.glyphfinder.com/'
   name 'Glyphfinder'
