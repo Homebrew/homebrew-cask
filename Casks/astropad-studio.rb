9620c1a4b071e2f1322f434ec18572a7c114cac6
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:19:54 +0100

    astropad-studio.rb: Add trailing slash to verified comment

--- Casks/astropad-studio.rb
+++ Casks/astropad-studio.rb
@@ -2,7 +2,7 @@ cask 'astropad-studio' do
   version '3.3.0'
   sha256 '55d6d08f5b70a1ffd89dd5864312bddc3c3df1784a848d0ba408dff1dec775fd'
 
-  # s3.amazonaws.com/astropad.com was verified as official when first introduced to the cask
+  # s3.amazonaws.com/astropad.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/astropad.com/downloads/AstropadStudio-#{version}.dmg"
   appcast 'https://astropad.com/downloads/studio-sparkle.xml'
   name 'Astropad Studio'
