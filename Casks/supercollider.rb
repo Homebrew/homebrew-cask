e99f6f6aefc4d1188c6eb983598a65040b140720
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:07:14 +0100

    supercollider.rb: Add trailing slash to verified comment

--- Casks/supercollider.rb
+++ Casks/supercollider.rb
@@ -2,7 +2,7 @@ cask 'supercollider' do
   version '3.11.0'
   sha256 '549e098f1895d3ae83ce12065b42c34b5cad5b3daebe75bb3002f659d686f04b'
 
-  # github.com/supercollider/supercollider was verified as official when first introduced to the cask
+  # github.com/supercollider/supercollider/ was verified as official when first introduced to the cask
   url "https://github.com/supercollider/supercollider/releases/download/Version-#{version}/SuperCollider-#{version}-macOS-signed.zip"
   appcast 'https://github.com/supercollider/supercollider/releases.atom'
   name 'SuperCollider'
