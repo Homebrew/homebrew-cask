af69befda7620db063afcb19591c56a8850b097f
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:07:44 +0100

    waterfox-current.rb: Add trailing slash to verified comment

--- Casks/waterfox-current.rb
+++ Casks/waterfox-current.rb
@@ -2,7 +2,7 @@ cask 'waterfox-current' do
   version '2020.04,6820.4.7'
   sha256 'fc410c4ed1d1151daffae0b8b77d612adaef1115db738b1049f1271daa9fd69a'
 
-  # storage-waterfox.netdna-ssl.com was verified as official when first introduced to the cask
+  # storage-waterfox.netdna-ssl.com/ was verified as official when first introduced to the cask
   url "https://storage-waterfox.netdna-ssl.com/releases/osx64/installer/Waterfox%20Current%20#{version.before_comma}%20Setup.dmg"
   appcast 'https://www.waterfox.net/download/'
   name 'Waterfox Current'
