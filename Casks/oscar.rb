696b654ddd6f84219dfb8665b31e4f350e2b2ee8
Work <brigitte@corecode.io>
2020-04-20 15:21:44 +0200

    Update oscar from 1.0.1 to 1.1.0

--- Casks/oscar.rb
+++ Casks/oscar.rb
@@ -1,9 +1,9 @@
 cask 'oscar' do
-  version '1.0.1'
-  sha256 'ca1f454cac6c41798604893cb86cede560bce146c1a5900e31aadad3d547af94'
+  version '1.1.0'
+  sha256 '1aa66aba091dd92760ef0e747514fdd35099d71e38e322da132122ebfeb9aa6e'
 
   # apneaboard.com/OSCAR/ was verified as official when first introduced to the cask
-  url "https://www.apneaboard.com/OSCAR/OSCAR-v#{version}-macOS.dmg"
+  url "https://www.apneaboard.com/OSCAR/OSCAR-#{version}.dmg"
   appcast 'https://www.sleepfiles.com/OSCAR/'
   name 'OSCAR'
   homepage 'https://www.sleepfiles.com/OSCAR/'
