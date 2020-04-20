ad815683f9b9402bd25e1bddd8546c9a962b3665
Work <brigitte@corecode.io>
2020-04-20 15:28:01 +0200

    Update videostream from 0.3.6 to 0.4.0

--- Casks/videostream.rb
+++ Casks/videostream.rb
@@ -1,6 +1,6 @@
 cask 'videostream' do
-  version '0.3.6'
-  sha256 'd1f65c33c075709c1890aca871602baffdef22ae810a07c5a4f59d88a0383071'
+  version '0.4.0'
+  sha256 'ac74c9b20fa53ce49ba5b2e36f55456363107d02c58fcb4579d10f8441954771'
 
   url 'https://cdn.getvideostream.com/videostream-native-updates/macOS/Videostream.pkg'
   appcast 'https://videostream-cdn.s3.amazonaws.com/videostream-native-updates/macOS/manifest.json'
