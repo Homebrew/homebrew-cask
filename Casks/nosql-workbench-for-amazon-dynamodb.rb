e9307ff2674c16fabcb84445a09257ce2ba1e083
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:39:18 +0100

    nosql-workbench-for-amazon-dynamodb.rb: Add trailing slash to verified comment

--- Casks/nosql-workbench-for-amazon-dynamodb.rb
+++ Casks/nosql-workbench-for-amazon-dynamodb.rb
@@ -2,7 +2,7 @@ cask 'nosql-workbench-for-amazon-dynamodb' do
   version '1.0.0'
   sha256 'aee4e9daa7925f01385490678e0d852719fa3b845a7522dc0a98fceba61c8043'
 
-  # nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com was verified as official when first introduced to the cask
+  # nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://nosql-workbench-for-amazon-dynamodb.s3.amazonaws.com/NoSQL+Workbench+for+Amazon+DynamoDB-mac-#{version}.dmg"
   appcast 'https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/workbench.settingup.partial.html'
   name 'NoSQL Workbench for Amazon DynamoDB'
