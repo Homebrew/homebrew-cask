a799b7a2382a18dc97e245206bdcd47ee6b7272b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:37:11 +0100

    graphicconverter.rb: Add trailing slash to verified comment

--- Casks/graphicconverter.rb
+++ Casks/graphicconverter.rb
@@ -2,7 +2,7 @@ cask 'graphicconverter' do
   version '11.1.3,4293'
   sha256 'aa8a48d5e7075473499def1774b94270f5eae9ec0aa5765080da791f70d9b652'
 
-  # lemkesoft.info was verified as official when first introduced to the cask
+  # lemkesoft.info/ was verified as official when first introduced to the cask
   url "https://www.lemkesoft.info/files/graphicconverter/gc#{version.major}_build#{version.after_comma}.zip"
   appcast "https://www.lemkesoft.info/sparkle/graphicconverter/graphicconverter#{version.major}.xml"
   name 'GraphicConverter'
