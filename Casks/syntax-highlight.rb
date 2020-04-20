21accb980bbd9b3a44607f499469f2f65eafa2ab
Sean Zhu <interestinglythere@gmail.com>
2020-04-20 03:00:24 -0700

    Add Syntax Highlight 1.0.b19

---
+++ Casks/syntax-highlight.rb
@@ -0,0 +1,11 @@
+cask 'syntax-highlight' do
+  version '1.0.b19'
+  sha256 'a17536242ffa630337441766df7aca3fa953c0be3a303fb212f8fd266e772c74'
+
+  url "https://github.com/sbarex/SourceCodeSyntaxHighlight/releases/download/#{version}/SyntaxHighlight.zip"
+  appcast 'https://github.com/sbarex/SourceCodeSyntaxHighlight/releases.atom'
+  name 'Syntax Highlight'
+  homepage 'https://github.com/sbarex/SourceCodeSyntaxHighlight'
+
+  app 'Syntax Highlight.app'
+end
