9b96176e1321c9a58c03d202d07c2788ca79166b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:02:49 +0100

    neofinder.rb: Add trailing slash to verified comment

--- Casks/neofinder.rb
+++ Casks/neofinder.rb
@@ -2,7 +2,7 @@ cask 'neofinder' do
   version '7.5.1'
   sha256 'be58f3847b629f8bce29d1399970c7baec3d7267f6d8a50065334ac3ded6659f'
 
-  # wfs-apps.de was verified as official when first introduced to the cask
+  # wfs-apps.de/ was verified as official when first introduced to the cask
   url "https://www.wfs-apps.de/updates/neofinder.#{version}.zip"
   appcast 'https://www.wfs-apps.de/updates/neofinder-appcast-64.xml'
   name 'NeoFinder'
