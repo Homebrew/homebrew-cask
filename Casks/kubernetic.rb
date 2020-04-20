8da42b60b575832580225e396c2f7aa7d75dcd4e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:21:41 +0100

    kubernetic.rb: Add trailing slash to verified comment

--- Casks/kubernetic.rb
+++ Casks/kubernetic.rb
@@ -2,7 +2,7 @@ cask 'kubernetic' do
   version '2.6.0'
   sha256 'c1846758c18487090e702a61c909e7d448ac50f4d99f0ac0152de00ff974b3cc'
 
-  # kubernetic.s3.amazonaws.com was verified as official when first introduced to the cask
+  # kubernetic.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://kubernetic.s3.amazonaws.com/Kubernetic-#{version}.dmg"
   appcast 'https://kubernetic.s3.amazonaws.com/latest-mac.yml'
   name 'Kubernetic'
