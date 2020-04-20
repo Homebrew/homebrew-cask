03cb9db3098f6cc607fdebeb06b276a50ffff7e7
CoreCode <core-code@users.noreply.github.com>
2020-04-21 00:33:44 +0200

    Update butt (#81022)

--- Casks/butt.rb
+++ Casks/butt.rb
@@ -1,6 +1,6 @@
 cask 'butt' do
   version '0.1.19'
-  sha256 '893c582f738d84c2c82a3e7ccceac81c622edee1fc6eb28891ee8c0854980bd5'
+  sha256 'a24b369af99e1faa5f30a6f8564c5b5525c5dc9f311775cfd58667864294b282'
 
   # sourceforge.net/butt/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/butt/butt-#{version}.dmg"
