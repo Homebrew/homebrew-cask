2c8d7612a4186f7ce5941f583968f234ba7efe38
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:47:36 +0100

    gifox.rb: Add trailing slash to verified comment

--- Casks/gifox.rb
+++ Casks/gifox.rb
@@ -2,7 +2,7 @@ cask 'gifox' do
   version '2.1.4,020104.00'
   sha256 '846695e5e5e563d9982033c4dafb990a954ec31f29b94ad408c382097a894d37'
 
-  # d3si16icyi9iar.cloudfront.net/gifox was verified as official when first introduced to the cask
+  # d3si16icyi9iar.cloudfront.net/gifox/ was verified as official when first introduced to the cask
   url "https://d3si16icyi9iar.cloudfront.net/gifox/#{version.after_comma}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/extract_text/download_with_useragent.cgi?url=https://api.gifox.io/appcast?prereleases=false'
   name 'gifox'
