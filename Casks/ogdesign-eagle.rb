1409f0711a4f713a04f2dc66849d459ae5a20ebf
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:22 +0100

    ogdesign-eagle.rb: Add trailing slash to verified comment

--- Casks/ogdesign-eagle.rb
+++ Casks/ogdesign-eagle.rb
@@ -2,7 +2,7 @@ cask 'ogdesign-eagle' do
   version '1.11,3'
   sha256 'c504887564465c996e2a1ed5dfa5220dcae70bc601aa2c1606c8306cfa7751aa'
 
-  # eagleapp.s3-accelerate.amazonaws.com was verified as official when first introduced to the cask
+  # eagleapp.s3-accelerate.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://eagleapp.s3-accelerate.amazonaws.com/releases/Eagle-#{version.before_comma}-build#{version.after_comma}.dmg"
   appcast 'https://eagle.cool/changelog'
   name 'Eagle'
