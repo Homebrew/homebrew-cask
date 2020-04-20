1f28af0e96d56c43e4a0f20cfd563fef19b805d4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:35 +0100

    mailmate.rb: Add trailing slash to verified comment

--- Casks/mailmate.rb
+++ Casks/mailmate.rb
@@ -2,7 +2,7 @@ cask 'mailmate' do
   version '5671'
   sha256 '5afed63061f97f255394399c997984eacdd4148dd344013f3ab355d1620688d3'
 
-  # mailmate-app.com was verified as official when first introduced to the cask
+  # mailmate-app.com/ was verified as official when first introduced to the cask
   url "https://updates.mailmate-app.com/archives/MailMate_r#{version}.tbz"
   appcast 'https://updates.mailmate-app.com/10.14/release'
   name 'MailMate'
