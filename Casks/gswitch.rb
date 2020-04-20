bac36bff74ba7d5cdb57deeb08efc0b132d20f01
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:46:02 +0100

    gswitch.rb: Add trailing slash to verified comment

--- Casks/gswitch.rb
+++ Casks/gswitch.rb
@@ -2,7 +2,7 @@ cask 'gswitch' do
   version '1.9.6'
   sha256 '431660ba4f7971f00d5d4648a96b4ac2326dbde366486f6fbbb4727f3d575e48'
 
-  # github.com/CodySchrank/gSwitch was verified as official when first introduced to the cask
+  # github.com/CodySchrank/gSwitch/ was verified as official when first introduced to the cask
   url "https://github.com/CodySchrank/gSwitch/releases/download/#{version}/gSwitch.zip"
   appcast 'https://github.com/CodySchrank/gSwitch/releases.atom'
   name 'gSwitch'
