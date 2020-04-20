e8ccefb0a746cf58afa0550ae335fdd356aea6c5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:16:04 +0100

    xscope.rb: Add trailing slash to verified comment

--- Casks/xscope.rb
+++ Casks/xscope.rb
@@ -2,7 +2,7 @@ cask 'xscope' do
   version '4.4.1'
   sha256 'dbbc939eca01ac8dc9994f5bd81130fbf980bdd6c9e66f23cf51fa585c8c0729'
 
-  # iconfactory.com was verified as official when first introduced to the cask
+  # iconfactory.com/ was verified as official when first introduced to the cask
   url "https://iconfactory.com/assets/software/xscope/xScope-#{version}.zip"
   appcast 'https://iconfactory.com/appcasts/xScope/appcast.xml'
   name 'xScope'
