21e7c1d2594302840f9a1b5c4165f60ae7832306
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:08:20 +0100

    xca.rb: Add trailing slash to verified comment

--- Casks/xca.rb
+++ Casks/xca.rb
@@ -2,7 +2,7 @@ cask 'xca' do
   version '2.2.1'
   sha256 '329b71d8ab203317198cc502a61174db7c56f24caa8688947282bacf24cd3e9b'
 
-  # github.com/chris2511/xca was verified as official when first introduced to the cask
+  # github.com/chris2511/xca/ was verified as official when first introduced to the cask
   url "https://github.com/chris2511/xca/releases/download/RELEASE.#{version}/xca-#{version}.dmg"
   appcast 'https://github.com/chris2511/xca/releases.atom'
   name 'XCA'
