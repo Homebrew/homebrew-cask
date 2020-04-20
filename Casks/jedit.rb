acf601c088ecf9d3ccf60fa268bf24ccd95c3925
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:49:36 +0100

    jedit.rb: Add trailing slash to verified comment

--- Casks/jedit.rb
+++ Casks/jedit.rb
@@ -2,7 +2,7 @@ cask 'jedit' do
   version '5.5.0'
   sha256 '2573720e6b36dca2105d3b16bc2245b3d1dadcd7e84d40f2c41c3c285386d122'
 
-  # sourceforge.net/jedit was verified as official when first introduced to the cask
+  # sourceforge.net/jedit/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/jedit/jedit#{version}install.dmg"
   appcast 'https://sourceforge.net/projects/jedit/rss'
   name 'jEdit'
