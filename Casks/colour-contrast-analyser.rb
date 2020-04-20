1e421812cd3d72d9a385cadc76ae984b717ca8ac
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:33:18 +0100

    colour-contrast-analyser.rb: Add trailing slash to verified comment

--- Casks/colour-contrast-analyser.rb
+++ Casks/colour-contrast-analyser.rb
@@ -2,7 +2,7 @@ cask 'colour-contrast-analyser' do
   version '2.4'
   sha256 'bf8559d329675e776d4b5be382d789b2c94087d8f5f88386112406e6e59f02c9'
 
-  # github.com/ThePacielloGroup/CCA-OSX was verified as official when first introduced to the cask
+  # github.com/ThePacielloGroup/CCA-OSX/ was verified as official when first introduced to the cask
   url "https://github.com/ThePacielloGroup/CCA-OSX/releases/download/#{version}/Colour.Contrast.Analyser.app.zip"
   appcast 'https://github.com/ThePacielloGroup/CCA-OSX/releases.atom'
   name 'Colour Contrast Analyser'
