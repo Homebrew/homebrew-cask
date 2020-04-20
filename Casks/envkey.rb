ce12715e79ee8af1cb79f45a67da3dc9dcf090f0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:58:00 +0100

    envkey.rb: Add trailing slash to verified comment

--- Casks/envkey.rb
+++ Casks/envkey.rb
@@ -2,7 +2,7 @@ cask 'envkey' do
   version '1.4.16'
   sha256 '69ae043e87a05a999d37ef1109300938ff3aaddadfcf5562921cff2dd6d9f8f1'
 
-  # github.com/envkey/envkey-app was verified as official when first introduced to the cask
+  # github.com/envkey/envkey-app/ was verified as official when first introduced to the cask
   url "https://github.com/envkey/envkey-app/releases/download/darwin-x64-prod-v#{version}/EnvKey-#{version}-mac.zip"
   appcast 'https://github.com/envkey/envkey-app/releases.atom'
   name 'EnvKey'
