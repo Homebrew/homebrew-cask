4fef24148626957a5b14dd643fe803fa2a86a792
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:23 +0100

    bricklink-studio.rb: Add trailing slash to verified comment

--- Casks/bricklink-studio.rb
+++ Casks/bricklink-studio.rb
@@ -2,7 +2,7 @@ cask 'bricklink-studio' do
   version :latest
   sha256 :no_check
 
-  # blstudio.s3.amazonaws.com was verified as official when first introduced to the cask
+  # blstudio.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://blstudio.s3.amazonaws.com/Stud.io.pkg'
   name 'Stud.io'
   homepage 'https://studio.bricklink.com/v2/build/studio.page'
