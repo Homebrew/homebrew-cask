9a4feecfb83ea8f84f3ae31a5bb28c0f1ddad33e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:57:02 +0100

    tinkerwell.rb: Add trailing slash to verified comment

--- Casks/tinkerwell.rb
+++ Casks/tinkerwell.rb
@@ -2,7 +2,7 @@ cask 'tinkerwell' do
   version '2.6.0'
   sha256 '390550d9cd65b6ab0c32533e23f0a7638403d4c28327a1e4c79089fca507d5dd'
 
-  # tinkerwell.fra1.digitaloceanspaces.com was verified as official when first introduced to the cask
+  # tinkerwell.fra1.digitaloceanspaces.com/ was verified as official when first introduced to the cask
   url "https://tinkerwell.fra1.digitaloceanspaces.com/tinkerwell/Tinkerwell-#{version}.dmg"
   appcast 'https://tinkerwell.fra1.digitaloceanspaces.com/tinkerwell/latest-mac.yml'
   name 'Tinkerwell'
