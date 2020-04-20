47d6242e183b3ce281c59f5da8c8aeec7bf081e2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:31:26 +0100

    pgadmin4.rb: Add trailing slash to verified comment

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -2,7 +2,7 @@ cask 'pgadmin4' do
   version '4.20'
   sha256 'f744299986fb337c0a8798b0c5b0eba6d391e8ac9ee0a5377371e04b6759803c'
 
-  # ftp.postgresql.org was verified as official when first introduced to the cask
+  # ftp.postgresql.org/ was verified as official when first introduced to the cask
   url "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v#{version}/macos/pgadmin4-#{version}.dmg"
   appcast 'https://ftp.postgresql.org/pub/pgadmin/pgadmin4/'
   name 'pgAdmin4'
