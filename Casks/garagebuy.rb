07e0c48adfbfea2055b73c17a737bde5c0f03453
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:31:51 +0100

    garagebuy.rb: Add trailing slash to verified comment

--- Casks/garagebuy.rb
+++ Casks/garagebuy.rb
@@ -2,7 +2,7 @@ cask 'garagebuy' do
   version '3.3.2'
   sha256 'f3a8d32189e8d6f5f051ad014ede279c10c67cbd1d9ccb4c223f9366b62b72fb'
 
-  # iwascoding.de was verified as official when first introduced to the cask
+  # iwascoding.de/ was verified as official when first introduced to the cask
   url "https://www.iwascoding.de/downloads/GarageBuy_#{version}.dmg"
   appcast 'https://www.iwascoding.com/GarageBuy/Downloads.html'
   name 'GarageBuy'
