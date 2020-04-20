e6bd136bbb1bc64373ed6dbea078e2489af90206
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:00:53 +0100

    send-to-kindle.rb: Add trailing slash to verified comment

--- Casks/send-to-kindle.rb
+++ Casks/send-to-kindle.rb
@@ -2,7 +2,7 @@ cask 'send-to-kindle' do
   version :latest
   sha256 :no_check
 
-  # sendtokindle.s3.amazonaws.com was verified as official when first introduced to the cask
+  # sendtokindle.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://sendtokindle.s3.amazonaws.com/SendToKindleForMac-installer.pkg'
   name 'Send to Kindle'
   homepage 'https://www.amazon.com/gp/sendtokindle/mac'
