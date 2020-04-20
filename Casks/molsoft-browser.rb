4fb3accf513acb9dab5e39ae669bd87007004645
Dana Merrick <dana.merrick@gmail.com>
2020-04-20 12:35:31 -0400

    Adding cask for ICM Browser

---
+++ Casks/molsoft-browser.rb
@@ -0,0 +1,10 @@
+cask 'molsoft-browser' do
+  version '3.8-7d'
+  sha256 '4a831c7525b72f81138f99fc4f96a40a390f3809e7818f93ba43973291bdef28'
+
+  url "http://www.molsoft.com/getbrowser.cgi?product=icm-browser&act=give&mirror=molsoft&platform=mac64&ver=#{version}"
+  name 'Molsoft Browser'
+  homepage 'https://www.molsoft.com/icm_browser.html'
+
+  app 'MolsoftBrowser64.app'
+end
