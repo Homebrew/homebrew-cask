f8c65b170c47e8a570536b4f859fc17d75c32395
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:21 +0100

    flightgear.rb: Add trailing slash to verified comment

--- Casks/flightgear.rb
+++ Casks/flightgear.rb
@@ -2,7 +2,7 @@ cask 'flightgear' do
   version '2019.1.1'
   sha256 'ffff81f9b88a9f96c6563e9bc6d43b6f4f84bcf001cd5d0fa144b4bd0cdca823'
 
-  # sourceforge.net/flightgear was verified as official when first introduced to the cask
+  # sourceforge.net/flightgear/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/flightgear/FlightGear-#{version}.dmg"
   appcast 'https://sourceforge.net/projects/flightgear/rss'
   name 'FlightGear'
