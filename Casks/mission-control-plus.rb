10d89865166b53a28d010ec99f52378c0510b65c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:59:32 +0100

    mission-control-plus.rb: Add trailing slash to verified comment

--- Casks/mission-control-plus.rb
+++ Casks/mission-control-plus.rb
@@ -2,7 +2,7 @@ cask 'mission-control-plus' do
   version '1.14'
   sha256 '8a89794c85dfab931d07bd309058064d6e3cf664057239d4cc75ce6b72276df4'
 
-  # github.com/ronyfadel/MissionControlPlusReleases was verified as official when first introduced to the cask
+  # github.com/ronyfadel/MissionControlPlusReleases/ was verified as official when first introduced to the cask
   url "https://github.com/ronyfadel/MissionControlPlusReleases/releases/download/#{version}/Mission.Control.Plus.tgz"
   appcast 'https://github.com/ronyfadel/MissionControlPlusReleases/releases.atom'
   name 'Mission Control Plus'
