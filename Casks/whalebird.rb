128b6ddeb838973961617e80d1d6f0df9b4d96c9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:56:28 +0100

    whalebird.rb: Add trailing slash to verified comment

--- Casks/whalebird.rb
+++ Casks/whalebird.rb
@@ -2,7 +2,7 @@ cask 'whalebird' do
   version '4.0.2'
   sha256 'd8a3c3ef0fb24505db2fb0f35f979df55bd228bb8182517ca53a62dca99b4ec8'
 
-  # github.com/h3poteto/whalebird-desktop was verified as official when first introduced to the cask
+  # github.com/h3poteto/whalebird-desktop/ was verified as official when first introduced to the cask
   url "https://github.com/h3poteto/whalebird-desktop/releases/download/#{version}/Whalebird-#{version}-darwin-x64.dmg"
   appcast 'https://github.com/h3poteto/whalebird-desktop/releases.atom'
   name 'Whalebird'
