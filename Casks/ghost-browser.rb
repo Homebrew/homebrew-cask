e49382bdaa1368662ae750206fa80acf28d13ea1
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:21:29 +0100

    ghost-browser.rb: Add trailing slash to verified comment

--- Casks/ghost-browser.rb
+++ Casks/ghost-browser.rb
@@ -2,7 +2,7 @@ cask 'ghost-browser' do
   version '2.1.1.11'
   sha256 '39c151fe69e451bd76d7765cc29eb470deb04c836376483c54d3641b91f9fdf5'
 
-  # ghostbrowser.s3.amazonaws.com was verified as official when first introduced to the cask
+  # ghostbrowser.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://ghostbrowser.s3.amazonaws.com/downloads/GhostBrowser-#{version}.dmg"
   appcast 'https://ghostbrowser.s3.amazonaws.com/updates/changelog.html'
   name 'Ghost Browser'
