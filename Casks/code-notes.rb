951aa7eb14772eb6f81a7eef3de70fe88173200e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:27:59 +0100

    code-notes.rb: Add trailing slash to verified comment

--- Casks/code-notes.rb
+++ Casks/code-notes.rb
@@ -2,7 +2,7 @@ cask 'code-notes' do
   version '1.2.3'
   sha256 '6c1677382088bba2b480d04cc07ecc814e39cb1c79050fe5f2fbb73115fa16c3'
 
-  # github.com/lauthieb/code-notes was verified as official when first introduced to the cask
+  # github.com/lauthieb/code-notes/ was verified as official when first introduced to the cask
   url "https://github.com/lauthieb/code-notes/releases/download/#{version}/code-notes-#{version}.dmg"
   appcast 'https://github.com/lauthieb/code-notes/releases.atom'
   name 'Code Notes'
