af2a5ed72d74c7547049d29167e1de3c59394745
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:09:10 +0100

    metabase.rb: Add trailing slash to verified comment

--- Casks/metabase.rb
+++ Casks/metabase.rb
@@ -2,7 +2,7 @@ cask 'metabase' do
   version '0.35.2'
   sha256 '06d56f3e81b9647c6c150c4406d4d340861c4e93ea332fe2e2f45e19110e2cb3'
 
-  # s3.amazonaws.com/downloads.metabase.com was verified as official when first introduced to the cask
+  # s3.amazonaws.com/downloads.metabase.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/downloads.metabase.com/v#{version}/Metabase.zip"
   appcast 'https://s3.amazonaws.com/downloads.metabase.com/appcast.xml'
   name 'Metabase'
