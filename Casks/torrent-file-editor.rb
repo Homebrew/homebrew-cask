84e7d3b11453a5f669a733271ce714359b914473
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:35:53 +0100

    torrent-file-editor.rb: Add trailing slash to verified comment

--- Casks/torrent-file-editor.rb
+++ Casks/torrent-file-editor.rb
@@ -2,7 +2,7 @@ cask 'torrent-file-editor' do
   version '0.3.17'
   sha256 '5ed1303dc25cdb2f6515b75faee574d07a7ec280653b0cc4e6a8b37f6be3744a'
 
-  # github.com/torrent-file-editor/torrent-file-editor was verified as official when first introduced to the cask
+  # github.com/torrent-file-editor/torrent-file-editor/ was verified as official when first introduced to the cask
   url "https://github.com/torrent-file-editor/torrent-file-editor/releases/download/v#{version}/torrent-file-editor-#{version}.dmg"
   appcast 'https://github.com/torrent-file-editor/torrent-file-editor/releases.atom'
   name 'Torrent File Editor'
