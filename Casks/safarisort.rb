e2bde7b48233c28c3f29ccd0dba43ad93b4dea5e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 03:14:43 +0100

    safarisort.rb: Add trailing slash to verified comment

--- Casks/safarisort.rb
+++ Casks/safarisort.rb
@@ -2,7 +2,7 @@ cask 'safarisort' do
   version :latest
   sha256 :no_check
 
-  # macupdate.com was verified as official when first introduced to the cask
+  # macupdate.com/ was verified as official when first introduced to the cask
   url 'https://www.macupdate.com/download/34101/SafariSort.dmg'
   name 'SafariSort'
   homepage 'https://www.safarisort.com/'
