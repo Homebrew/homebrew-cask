c342fe4a8febfd56d65be7f89deac27d81d01e88
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:55:16 +0100

    torguard.rb: Add trailing slash to verified comment

--- Casks/torguard.rb
+++ Casks/torguard.rb
@@ -2,7 +2,7 @@ cask 'torguard' do
   version '3.98.3'
   sha256 '13a6e84034798effcedfb0204275e24803684e73b8dd615bc90d89719ef45bbe'
 
-  # torguard.biz was verified as official when first introduced to the cask
+  # torguard.biz/ was verified as official when first introduced to the cask
   url "https://updates.torguard.biz/Software/MacOSX/TorGuard-v#{version}.dmg"
   appcast 'https://updates.torguard.biz/Software/MacOSX/checksums.sha256'
   name 'TorGuard'
