2d8f163edbc6fb13ac42fa701e7dc5606fbbb82e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:39:21 +0100

    nano.rb: Add trailing slash to verified comment

--- Casks/nano.rb
+++ Casks/nano.rb
@@ -2,7 +2,7 @@ cask 'nano' do
   version '20.0'
   sha256 'b770c78e7bbce29604b9d83edf36498319b3d568f1d1a9132acdb004f3e20ce4'
 
-  # github.com/nanocurrency/nano-node was verified as official when first introduced to the cask
+  # github.com/nanocurrency/nano-node/ was verified as official when first introduced to the cask
   url "https://github.com/nanocurrency/nano-node/releases/download/V#{version}/nano-node-V#{version}-Darwin.dmg"
   appcast 'https://github.com/nanocurrency/nano-node/releases.atom'
   name 'Nano'
