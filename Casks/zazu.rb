14f7f86d9de20c1195d2676f7a697322f944391b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:37 +0100

    zazu.rb: Add trailing slash to verified comment

--- Casks/zazu.rb
+++ Casks/zazu.rb
@@ -2,7 +2,7 @@ cask 'zazu' do
   version '0.6.0'
   sha256 'a726bd20d64d6f03b3251d7841f94fae0f50103533706e9291233aa3adbecf92'
 
-  # github.com was verified as official when first introduced to the cask
+  # github.com/ was verified as official when first introduced to the cask
   url "https://github.com/tinytacoteam/zazu/releases/download/v#{version}/Zazu-#{version}.dmg"
   appcast 'https://github.com/tinytacoteam/zazu/releases.atom'
   name 'Zazu'
