ef8e95cc1b5a9e854279aeb390b99411756e63db
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:26:16 +0100

    yammer.rb: Add trailing slash to verified comment

--- Casks/yammer.rb
+++ Casks/yammer.rb
@@ -2,7 +2,7 @@ cask 'yammer' do
   version '3.4.5'
   sha256 'd26bdb80f1f054494b8fa271df6008d86be526b734cc997264062f9a6ca2165c'
 
-  # yammerdesktopapp.blob.core.windows.net/binaries/dist was verified as official when first introduced to the cask
+  # yammerdesktopapp.blob.core.windows.net/binaries/dist/ was verified as official when first introduced to the cask
   url "https://yammerdesktopapp.blob.core.windows.net/binaries/dist/darwin/x64/#{version}/Yammer-#{version}.dmg"
   appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://aka.ms/yammer_desktop_mac'
   name 'Yammer'
