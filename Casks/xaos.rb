742baf954b561e2ad0e647b34a081fc5d0d4fbdb
Julian Mayer <julian@corecode.io>
2020-04-21 11:01:36 +0200

    Update xaos

--- Casks/xaos.rb
+++ Casks/xaos.rb
@@ -1,6 +1,6 @@
 cask 'xaos' do
   version '4.0'
-  sha256 '228b03d656f82483c3a9cbcae4a66601341be446807bb4fdf425544fbd8fa76a'
+  sha256 'b546012110d508af24113c52449f507d716823f0c25fd29d5a7cd38113b25d58'
 
   # github.com/xaos-project/XaoS/ was verified as official when first introduced to the cask
   url "https://github.com/xaos-project/XaoS/releases/download/release-#{version}/XaoS-#{version}.dmg"
