b43f6ef0fd212e3ebe08c1ac5148aa0fac3f4ed4
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-20 17:19:10 -0700

    Update exodus from 20.4.14 to 20.4.18 (#81043)

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,6 +1,6 @@
 cask 'exodus' do
-  version '20.4.14'
-  sha256 '2d7de4a83acba3aada3a180b8b38e4ec51fac4c3f516ef28aee3606f4fa7584e'
+  version '20.4.18'
+  sha256 '11997f6a9993c75db025caabbbad5837faef225ec9ec39a3ad36006d76541b19'
 
   url "https://downloads.exodus.io/releases/exodus-macos-#{version}.dmg"
   appcast 'https://www.exodus.io/releases/'
