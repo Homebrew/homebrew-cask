621c335b3864c8403c1a12b26b87d1a620ab90b7
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-21 10:49:22 -0700

    Update qownnotes from 20.4.10,b5528-084126 to 20.4.12,b5533-170634 (#81087)

--- Casks/qownnotes.rb
+++ Casks/qownnotes.rb
@@ -1,6 +1,6 @@
 cask 'qownnotes' do
-  version '20.4.10,b5528-084126'
-  sha256 '5c18bc2e8540f7e0dff234b041a41ca7786b6263c8d4ed6a9d088120a51d7d65'
+  version '20.4.12,b5533-170634'
+  sha256 'dfb1dd89f9d8e7f77c1fd3ee0aab82b795d5125f21266fc3d58d55d45a3b12c5'
 
   # github.com/pbek/QOwnNotes/ was verified as official when first introduced to the cask
   url "https://github.com/pbek/QOwnNotes/releases/download/macosx-#{version.after_comma}/QOwnNotes-#{version.before_comma}.dmg"
