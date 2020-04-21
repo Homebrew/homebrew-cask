74b8133d62657c4d3af83c38ddf7a23879aa1a3a
Julian Mayer <julian@corecode.io>
2020-04-21 10:59:41 +0200

    Update macpilot

--- Casks/macpilot.rb
+++ Casks/macpilot.rb
@@ -2,7 +2,7 @@ cask 'macpilot' do
   version '11.1.3'
   sha256 '1990b04414896ef24767e58cd9b56901460375ee8fb805572ca34da019bcda58'
 
-  url 'http://mirror.koingosw.com/products/macpilot/download/macpilot.dmg'
+  url 'https://www.koingosw.com/products/macpilot/download/macpilot.dmg'
   appcast 'https://www.koingosw.com/postback/versioncheck.php?appname=macpilot&type=sparkle'
   name 'MacPilot'
   homepage 'https://www.koingosw.com/products/macpilot/'
