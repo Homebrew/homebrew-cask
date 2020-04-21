20055567c411550e74a746ac1fb09493faaa1497
Dana Merrick <dmerrick@users.noreply.github.com>
2020-04-20 21:31:37 -0400

    Update Casks/molsoftbrowser.rb
    Co-Authored-By: Dario Vladović <d.vladimyr@gmail.com>

--- Casks/molsoftbrowser.rb
+++ Casks/molsoftbrowser.rb
@@ -2,10 +2,10 @@ cask 'molsoftbrowser' do
   version '3.8-7d'
   sha256 '4a831c7525b72f81138f99fc4f96a40a390f3809e7818f93ba43973291bdef28'
 
-  url "http://www.molsoft.com/getbrowser.cgi?product=icm-browser&act=give&mirror=molsoft&platform=mac64&ver=#{version}"
-  appcast 'https://www.molsoft.com/getbrowser.cgi'
+  url "https://molsoft.com/getbrowser.cgi?product=icm-browser&act=give&mirror=molsoft&platform=mac64&ver=#{version}"
+  appcast 'https://molsoft.com/getbrowser.cgi?product=icm-browser&act=list'
   name 'Molsoft Browser'
-  homepage 'https://www.molsoft.com/icm_browser.html'
+  homepage 'https://molsoft.com/icm_browser.html'
 
   app 'MolsoftBrowser64.app'
 
