5b3722b09d6bc25927ed4d3323b8c86773a67739
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-21 14:37:56 -0700

    Update google-earth-pro from 7.3.2.5776 to 7.3.3.7673 (#81109)
    * Update google-earth-pro from 7.3.2.5776 to 7.3.3.7673
    
    * Update google-earth-pro.rb

--- Casks/google-earth-pro.rb
+++ Casks/google-earth-pro.rb
@@ -1,6 +1,6 @@
 cask 'google-earth-pro' do
-  version '7.3.2.5776'
-  sha256 'e84a9e1421f1e1200e7cd7fa1aa811caacab59216f73287293c3ba643c438060'
+  version '7.3.3.7673'
+  sha256 'be3e67eb81e14febad41f173abc7ba514d188a50105606d0e3d30e9b8b66b87a'
 
   url 'https://dl.google.com/earth/client/advanced/current/GoogleEarthProMac-Intel.dmg'
   name 'Google Earth Pro'
@@ -16,6 +16,8 @@ cask 'google-earth-pro' do
                          'com.google.keystone.agent',
                          'com.google.keystone.system.agent',
                          'com.google.keystone.daemon',
+                         'com.google.keystone.xpcservice',
+                         'com.google.keystone.system.xpcservice',
                        ]
 
   zap trash: [
