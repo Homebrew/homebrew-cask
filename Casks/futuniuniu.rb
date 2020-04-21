596746284a7983118eeacf225f00148239b3c9e0
suschizu <brigitte@corecode.io>
2020-04-21 16:48:08 +0200

    Update futuniuniu from 10.4.642_202004131013 to 10.4.644_202004161000 (#81075)

--- Casks/futuniuniu.rb
+++ Casks/futuniuniu.rb
@@ -1,6 +1,6 @@
 cask 'futuniuniu' do
-  version '10.4.642_202004131013'
-  sha256 'f49aa27c4bdd0ae7f395e04e3539c713b97de871a0bd089eafd870d3ac5804a6'
+  version '10.4.644_202004161000'
+  sha256 '20310f99aa53e7763c0a176586c7ace24145f2f49a4f1a2daeee8de8a785f78b'
 
   # software-file-1251001049.file.myqcloud.com/ was verified as official when first introduced to the cask
   url "https://software-file-1251001049.file.myqcloud.com/FTNNForMac_#{version}_website.dmg"
