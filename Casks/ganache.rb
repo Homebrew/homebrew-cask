d174f62d44ec9dca79bb410a4c96884cbb10041f
Randall <ran.dall@icloud.com>
2020-04-21 10:51:07 -0700

    Update ganache from 2.3.0 to 2.3.1

--- Casks/ganache.rb
+++ Casks/ganache.rb
@@ -1,6 +1,6 @@
 cask 'ganache' do
-  version '2.3.0'
-  sha256 '17740bbe33bb9db80ae3dd2d6b825b0b20836749845b51deb4c70cc1ec8fdb71'
+  version '2.3.1'
+  sha256 '548ba0a069beb08a9e15c56c97f51978a01b312b8fd23eaca6eb08f35651502a'
 
   # github.com/trufflesuite/ganache/ was verified as official when first introduced to the cask
   url "https://github.com/trufflesuite/ganache/releases/download/v#{version}/Ganache-#{version}-mac.dmg"
