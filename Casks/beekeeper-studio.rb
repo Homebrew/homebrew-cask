dab1fa46a72f95d2d40d5ef551dc009539a37ac4
Alex <kochalex@users.noreply.github.com>
2020-04-21 22:39:50 -0500

    Update Casks/beekeeper-studio.rb
    Co-Authored-By: Dario Vladović <d.vladimyr@gmail.com>

--- Casks/beekeeper-studio.rb
+++ Casks/beekeeper-studio.rb
@@ -2,7 +2,7 @@ cask 'beekeeper-studio' do
   version '1.1.2'
   sha256 '5850f254c5506ec65d39737200a1409c1e670c2a1644e7b1d72deaacf4d90f06'
 
-  # github.com/beekeeper-studio/beekeeper-studio was verified as official when first introduced to the cask
+  # github.com/beekeeper-studio/beekeeper-studio/ was verified as official when first introduced to the cask
   url "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v#{version}/Beekeeper-Studio-#{version}.dmg"
   appcast 'https://github.com/beekeeper-studio/beekeeper-studio/releases.atom'
   name 'Beekeeper Studio'
