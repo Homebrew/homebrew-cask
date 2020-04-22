43d3b95b452b8f8cbe1ba08e677576fb01af373c
Alex Koch <alex@esparklearning.com>
2020-04-21 22:24:05 -0500

    Add Beekeeper Studio v1.1.2

---
+++ Casks/beekeeper-studio.rb
@@ -0,0 +1,14 @@
+cask 'beekeeper-studio' do
+  version '1.1.2'
+  sha256 '5850f254c5506ec65d39737200a1409c1e670c2a1644e7b1d72deaacf4d90f06'
+
+  # github.com/beekeeper-studio/beekeeper-studio was verified as official when first introduced to the cask
+  url 'https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v1.1.2/Beekeeper-Studio-1.1.2.dmg'
+  appcast 'https://github.com/beekeeper-studio/beekeeper-studio/releases.atom'
+  name 'Beekeeper Studio'
+  homepage 'https://www.beekeeperstudio.io/'
+
+  auto_updates true
+
+  app 'Beekeeper Studio.app'
+end
