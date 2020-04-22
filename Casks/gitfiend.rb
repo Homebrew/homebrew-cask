e42bf852d108bd4094b61e4c57b4f1cfc45ce523
Anton Syuvaev <syuvaev@ubic.tech>
2020-04-22 20:18:30 +0700

    Add GitFiend app v0.22.8
    Add GitFiend git client v0.22.8

---
+++ Casks/gitfiend.rb
@@ -0,0 +1,10 @@
+cask 'gitfiend' do
+  version '0.22.8'
+  sha256 '8d0eaaf47074c28d97f8fca7780e11f868adb8ab97f18c8e196f54817a86237d'
+
+  url "https://gitfiend.com/resources/GitFiend-#{version}.dmg"
+  name 'GitFiend'
+  homepage 'https://gitfiend.com/'
+
+  app 'GitFiend.app'
+end
