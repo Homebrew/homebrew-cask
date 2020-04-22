d30db963eb25beecfa3f1c414be2189ff534f98c
onesmallskipforman <sgonzalez@g.hmc.edu>
2020-04-21 20:34:49 -0400

    Add librepcb.app v0.1.3
    Add librepcb.app, an open-source PCB design and schematic tool.

---
+++ Casks/librepcb.rb
@@ -0,0 +1,14 @@
+cask 'librepcb' do
+  version '0.1.3'
+  sha256 '51fdc65e748cc99c25db90cb9babdc008a66bd4b5c5b6cbc744a7c9049cda3ca'
+
+  url "https://download.librepcb.org/releases/#{version}/librepcb-#{version}-mac-x86_64.dmg"
+  name 'LibrePCB'
+  homepage 'https://librepcb.org/'
+
+  app 'librepcb.app'
+
+  zap delete: [
+                '~/Library/Saved Application State/com.yourcompany.librepcb.savedState',
+              ]
+end
