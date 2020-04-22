a5097709469f0afd0e330ca52828959a5b5dea61
Viktor Szakats <commit@vsz.me>
2020-04-22 09:27:40 +0000

    secure URLs

--- Casks/marginnote.rb
+++ Casks/marginnote.rb
@@ -3,7 +3,7 @@ cask 'marginnote' do
   sha256 '6495892ed0d10aa02fa7dde830aa6a1759158ee4386dd77c3ec486238972e97c'
 
   # marginstudy.com/ was verified as official when first introduced to the cask
-  url "http://marginstudy.com/mac/MarginNote#{version.major}.dmg"
+  url "https://marginstudy.com/mac/MarginNote#{version.major}.dmg"
   appcast 'https://api.appcenter.ms/v0.1/public/sparkle/apps/1451f4f6-9214-4d46-b91c-d5898c7e42cb'
   name 'MarginNote'
   homepage 'https://www.marginnote.com/'
