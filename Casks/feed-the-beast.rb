90f1498363f2ea69f25ea81f8ec91c5137492df2
Andrew Zhou <0az@afzhou.com>
2020-04-22 19:05:04 -0500

    Update feed-the-beast to v1.0.8

--- Casks/feed-the-beast.rb
+++ Casks/feed-the-beast.rb
@@ -1,9 +1,9 @@
 cask 'feed-the-beast' do
-  version :latest
-  sha256 :no_check
+  version '1.0.8'
+  sha256 'b03fe156f41b202b72c6ad554ecdfa7c77f503abf665a12e6acdeaabaef3f88d'
 
-  # dist.creeper.host/FTB2/ was verified as official when first introduced to the cask
-  url 'https://dist.creeper.host/FTB2/launcher/FTB_Launcher.zip'
+  # apps.modpacks.ch/FTBApp was verified as official when first introduced to the cask
+  url "https://apps.modpacks.ch/FTBApp/release/#{version}/FTBA_macos_#{version.dots_to_underscores}.dmg"
   name 'Feed the Beast'
   homepage 'https://www.feed-the-beast.com/'
 
