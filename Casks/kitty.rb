f4ef1c230e079f81f58883c14236eacbbe971790
Ahmed El Gabri <ahmed@gabri.me>
2020-04-23 05:49:41 +0200

    Update kitty from 0.17.2 to 0.17.3

--- Casks/kitty.rb
+++ Casks/kitty.rb
@@ -1,6 +1,6 @@
 cask 'kitty' do
-  version '0.17.2'
-  sha256 'e5fe49584b4327402a49d7465b426b1626d147a474a7a0d8e355697e65df4db2'
+  version '0.17.3'
+  sha256 'cbe298a8521bf5c1a94519f426d419aee17dffcd05a296873dd75998371307cf'
 
   url "https://github.com/kovidgoyal/kitty/releases/download/v#{version}/kitty-#{version}.dmg"
   appcast 'https://github.com/kovidgoyal/kitty/releases.atom'
