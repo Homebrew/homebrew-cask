c48ca000760b59037fdf37f51d14b1d2e329941a
Scott Humphries <sscotth@users.noreply.github.com>
2020-04-23 17:34:07 -0500

    Update plexamp from 3.0.0 to 3.0.1 (#81257)

--- Casks/plexamp.rb
+++ Casks/plexamp.rb
@@ -1,6 +1,6 @@
 cask 'plexamp' do
-  version '3.0.0'
-  sha256 '96781e3924c523a85a9017fdb1a403269d3afcb8576e443f8f29669eb838bbd1'
+  version '3.0.1'
+  sha256 '56ac23d42d7688671399e18556db19f2cba2bed05912a18a7c8b04cea8af918f'
 
   # plexamp.plex.tv/ was verified as official when first introduced to the cask
   url "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-#{version}.dmg"
