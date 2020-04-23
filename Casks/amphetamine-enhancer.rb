2f3c86a159fa9807b6fe83db11a9c1e5d31e642c
revonoc <brooksrichon@Brookss-MacBook-Pro.local>
2020-04-22 22:21:57 -0400

    Add Amphetamine Enhancer.app :latest
    The app requires Amphetamine 5.0 from Mac App Store

---
+++ Casks/amphetamine-enhancer.rb
@@ -0,0 +1,12 @@
+cask 'amphetamine-enhancer' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/x74353/Amphetamine-Enhancer/raw/master/Releases/Current/Amphetamine%20Enhancer.dmg'
+  name 'Amphetamine Enhancer'
+  homepage 'https://github.com/x74353/Amphetamine-Enhancer'
+
+  caveats 'requires Amphetamine 5.0+ from Mac App Store'
+  caveats 'uninstall any scripts within the app before uninstalling'
+  caveats 'uninstall any scripts within the app before uninstalling'
+end
