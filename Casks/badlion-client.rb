8cd715efadc9049c09d205be9acfe6158d5c1c1c
Randall <ran.dall@icloud.com>
2020-04-23 11:57:42 -0700

    Update badlion-client from 2.13.1 to 2.13.2

--- Casks/badlion-client.rb
+++ Casks/badlion-client.rb
@@ -1,6 +1,6 @@
 cask 'badlion-client' do
-  version '2.13.1'
-  sha256 '98ad9066ebf9d5b8010be2a30675600cb5b0063c406e337160dac5a46bda7ee1'
+  version '2.13.2'
+  sha256 '6ba0f70fb07d8559c171b5682ed6a1c9f444909b5c790683da4a8a10b87c2dda'
 
   url "https://client-updates.badlion.net/Badlion%20Client-#{version}.dmg"
   appcast 'https://client-updates.badlion.net/latest-mac.yml'
