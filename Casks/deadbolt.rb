1c63b25e361ac902e90972d13535edec01748dbc
Aaron Lichtman <aaronlichtman@gmail.com>
2020-04-22 19:39:39 -0500

    Add Deadbolt v0.1.0 cask

---
+++ Casks/deadbolt.rb
@@ -0,0 +1,11 @@
+cask 'deadbolt' do
+  version '0.1.0'
+  sha256 'c302f1c532082b200d844d17cd8feadc61821d97591fb5a0dbb1bb97c3a5981b'
+
+  url "https://github.com/alichtman/deadbolt/releases/download/#{version}/Deadbolt-#{version}.dmg"
+  appcast 'https://github.com/alichtman/deadbolt/releases.atom'
+  name 'Deadbolt'
+  homepage 'https://github.com/alichtman/deadbolt'
+
+  app 'Deadbolt.app'
+end
