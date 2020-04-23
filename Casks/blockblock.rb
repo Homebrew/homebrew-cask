525077db7196fda2412276ff9d731f168f2501be
Ben Montour <ben.montour@workiva.com>
2020-04-23 15:55:32 -0500

    updates per vitorgalvao's comments

--- Casks/blockblock.rb
+++ Casks/blockblock.rb
@@ -1,13 +1,14 @@
 cask 'blockblock' do
+  appcast 'https://objective-see.com/products/changelogs/BlockBlock.txt'
+  name 'BlockBlock'
+  homepage 'https://objective-see.com/products/blockblock.html'
+
   if MacOS.version <= :mojave
     version '0.9.9.4'
     sha256 '6ab3a8224e8bc77b9abe8d41492c161454c6b0266e60e61b06931fed4b431282'
 
     # bitbucket.org/objective-see/ was verified as official when first introduced to the cask
     url "https://bitbucket.org/objective-see/deploy/downloads/BlockBlock_#{version}.zip"
-    appcast 'https://objective-see.com/products/changelogs/BlockBlock.txt'
-    name 'BlockBlock'
-    homepage 'https://objective-see.com/products/blockblock.html'
 
     installer script: {
                         executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock",
@@ -26,11 +27,6 @@ cask 'blockblock' do
 
     # bitbucket.org/objective-see/ was verified as official when first introduced to the cask
     url "https://bitbucket.org/objective-see/deploy/downloads/BlockBlock_#{version}.zip"
-    appcast 'https://objective-see.com/products/changelogs/BlockBlock.txt'
-    name 'BlockBlock'
-    homepage 'https://objective-see.com/products/blockblock.html'
-
-    depends_on macos: '>= :catalina'
 
     installer script: {
                         executable: "#{staged_path}/BlockBlock Installer.app/Contents/MacOS/BlockBlock Installer",
@@ -44,5 +40,6 @@ cask 'blockblock' do
                         sudo:       true,
                       }
   end
+
   zap trash: '~/Library/Preferences/com.objectiveSee.BlockBlock.plist'
 end
