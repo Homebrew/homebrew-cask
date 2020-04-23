89a33e1c1f4733db7e7c9d104852769da8c256f8
romainstuder <romainstuder@users.noreply.github.com>
2020-04-23 08:24:43 +0100

    Update Casks/jalview.rb
    Co-Authored-By: Dario Vladović <d.vladimyr@gmail.com>

--- Casks/jalview.rb
+++ Casks/jalview.rb
@@ -3,7 +3,7 @@ cask 'jalview' do
   sha256 'd2bd8b4db3963381ace200c185e0ea4ebd63f61878a3466ecdc0cb1095d198c8'
 
   url "https://www.jalview.org/getdown/release/install4j/1.8/Jalview-#{version.dots_to_underscores}-macos-java_8.dmg"
-  appcast 'https://www.jalview.org/getdown/release/'
+  appcast 'https://www.jalview.org/getdown/release/install4j/1.8/updates.xml'
   name 'Jalview'
   homepage 'https://www.jalview.org/'
 
