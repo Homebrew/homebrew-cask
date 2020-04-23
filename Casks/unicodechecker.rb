2ee37f088d3b6b5cceec7214cb387a9aa5be7957
suschizu <brigitte@corecode.io>
2020-04-23 19:39:36 +0200

    Update unicodechecker.rb

--- Casks/unicodechecker.rb
+++ Casks/unicodechecker.rb
@@ -2,7 +2,7 @@ cask 'unicodechecker' do
   version '1.22,776'
   sha256 '3c173ef1984cc42baa0dd03cfbd4d07cf18c75e3699b2c2a3a24250ac77fbe4f'
 
-  url 'https://earthlingsoft.net/UnicodeChecker/UnicodeChecker.zip'
+  url 'https://earthlingsoft.net/UnicodeChecker/UnicodeChecker%20#{version.before_comma}%20(#{version.after_comma}).zip'
   appcast 'https://earthlingsoft.net/UnicodeChecker/appcast.xml'
   name 'UnicodeChecker'
   homepage 'https://earthlingsoft.net/UnicodeChecker/'
