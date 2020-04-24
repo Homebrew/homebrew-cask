6ef3dee833b3eec86b7905c1cc0ce02dad6b2979
Lucas Chen <2109121+lucaschen@users.noreply.github.com>
2020-04-25 08:50:11 +1000

    Update plan.rb - existing URL returned 403

--- Casks/plan.rb
+++ Casks/plan.rb
@@ -2,7 +2,7 @@ cask 'plan' do
   version :latest
   sha256 :no_check
 
-  url 'https://static.getplan.co/plan-latest.dmg'
+  url 'https://cdn.getplan.co/plan-latest.dmg'
   name 'Plan'
   homepage 'https://getplan.co/'
 
