cb86f6bca23267bce7c3b0a8add7cbea962ee440
Chris Chinchilla <chris@chrischinchilla.com>
2020-04-24 16:58:26 +0200

    Update SF URL

--- Casks/rolisteam.rb
+++ Casks/rolisteam.rb
@@ -2,7 +2,7 @@ cask 'rolisteam' do
   version '1.9.2'
   sha256 'eeb87c5cefc383845303edad44ea8b08ff808cb9e8ab80804a1172bbf1947a2d'
 
-  # sourceforge.net/projects/rolisteam/files/ was verified as official when first introduced to the cask
+  # downloads.sourceforge.net/rolisteam/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/rolisteam/rolisteam_v#{version}_MacOs.dmg"
   appcast 'https://sourceforge.net/projects/rolisteam/rss'
   name 'Rolisteam'
