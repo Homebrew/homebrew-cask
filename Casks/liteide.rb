7ce4f071f202c202389591504980f52ba251c91d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-24 09:05:42 -0700

    Update liteide from 37 to 37.1 (#81294)

--- Casks/liteide.rb
+++ Casks/liteide.rb
@@ -1,6 +1,6 @@
 cask 'liteide' do
-  version '37'
-  sha256 'f012e101e6746725ea564ef55eac1e5d124e2cbe26887e897f8a63e44f0cfb35'
+  version '37.1'
+  sha256 'f739a15d05c227dc53c9501bfa8de917b1b2b7dcc8517c156a49bc55b75e8e67'
 
   url "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.macos-qt5.12.5.zip"
   appcast 'https://github.com/visualfc/liteide/releases.atom'
