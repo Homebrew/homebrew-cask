f93d65228f3556f0165e110a5bc75cbcab38eb7e
Chris Chinchilla <chris@chrischinchilla.com>
2020-04-24 16:48:53 +0200

    Update url to preferred format

--- Casks/rolisteam.rb
+++ Casks/rolisteam.rb
@@ -3,8 +3,7 @@ cask 'rolisteam' do
   sha256 'eeb87c5cefc383845303edad44ea8b08ff808cb9e8ab80804a1172bbf1947a2d'
 
   # sourceforge.net/projects/rolisteam/files/ was verified as official when first introduced to the cask
-
-  url "https://sourceforge.net/projects/rolisteam/files/#{version}/rolisteam_v#{version}_MacOs.dmg/download"
+  url "https://downloads.sourceforge.net/rolisteam/rolisteam_v#{version}_MacOs.dmg"
   appcast 'https://sourceforge.net/projects/rolisteam/rss'
   name 'Rolisteam'
   homepage 'https://rolisteam.org/'
