8091f52a2b1b0bf4ee799eefb687f26504a3012b
Raphael Ling CX <ralcx@outlook.com>
2020-04-24 12:34:14 +0800

    wireshark: Remove config directory on zap (#81264)

--- Casks/wireshark.rb
+++ Casks/wireshark.rb
@@ -37,5 +37,6 @@ cask 'wireshark' do
                '~/Library/Cookies/org.wireshark.Wireshark.binarycookies',
                '~/Library/Preferences/org.wireshark.Wireshark.plist',
                '~/Library/Saved Application State/org.wireshark.Wireshark.savedState',
+               '~/.config/wireshark',
              ]
 end
