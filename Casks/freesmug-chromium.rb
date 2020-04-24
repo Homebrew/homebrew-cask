9831059634f060f51551319a7f13d7bab4bd7fb3
Henrik Siljebråt <h.siljebrat@gold.ac.uk>
2020-04-24 20:49:22 +0100

    Update freesmug-chromium from 80.0.3987.116 to 81.0.4044.122 (#81272)

--- Casks/freesmug-chromium.rb
+++ Casks/freesmug-chromium.rb
@@ -1,12 +1,17 @@
 cask 'freesmug-chromium' do
-  version '80.0.3987.116'
-  sha256 '7257979a6fd92c3e34101de0be34d237557e59d496b41f3f791c7e61e51d29be'
+  version '81.0.4044.122'
+  sha256 'cd7f8da4517b7627411d45bf5057def3bb4a4489172e953e5249eb3003e7d241'
 
   # sourceforge.net/osxportableapps/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/osxportableapps/Chromium_OSX_#{version}.dmg"
   appcast 'https://sourceforge.net/projects/osxportableapps/rss?path=/Chromium'
   name 'Chromium'
   homepage 'http://www.freesmug.org/chromium'
+
+  conflicts_with cask: [
+                         'chromium',
+                         'eloston-chromium',
+                       ]
 
   app 'Chromium.app'
 end
