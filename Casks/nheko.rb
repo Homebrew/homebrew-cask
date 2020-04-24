042c9adba7abcf586e1ec21c145d2a045f08c861
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-24 09:22:01 -0700

    Update nheko from 0.7.0 to 0.7.1 (#81297)

--- Casks/nheko.rb
+++ Casks/nheko.rb
@@ -1,6 +1,6 @@
 cask 'nheko' do
-  version '0.7.0'
-  sha256 '455d4c554eed7830119a6e6480a90d5c09dbab0590a5ea17cd75cb154556e10b'
+  version '0.7.1'
+  sha256 '29e96ae7368744d813ea38c7bee99099859f59cc977f8ba128ec701f72e3aa84'
 
   # github.com/Nheko-Reborn/nheko/ was verified as official when first introduced to the cask
   url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}.dmg"
