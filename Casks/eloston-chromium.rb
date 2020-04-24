9ad53f59634212f85916ccd59e24524ba8ef9637
frodera <219724+frodera@users.noreply.github.com>
2020-04-24 22:48:43 +1000

    Update eloston-chromium from 81.0.4044.113-1 to 81.0.4044.122-1 (#81284)

--- Casks/eloston-chromium.rb
+++ Casks/eloston-chromium.rb
@@ -1,6 +1,6 @@
 cask 'eloston-chromium' do
-  version '81.0.4044.113-1'
-  sha256 'ea4642e72e390038e471cd5f055b397077ac677c47e6729d99000e05d0a6dcce'
+  version '81.0.4044.122-1'
+  sha256 '87d4040e683d78de593e845c12f203ea989786dfabc6361ee71a1c8e4353b948'
 
   # github.com/kramred/ungoogled-chromium-binaries/ was verified as official when first introduced to the cask
   url "https://github.com/kramred/ungoogled-chromium-binaries/releases/download/#{version}/ungoogled-chromium_#{version}.1_macos.dmg"
