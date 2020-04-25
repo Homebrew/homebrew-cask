d4415c1e088b46f79b66f21baa10f258dfa3f7f7
Vítor Galvão <info@vitorgalvao.com>
2020-04-25 22:40:31 +0100

    Update rcse.rb

--- Casks/rcse.rb
+++ Casks/rcse.rb
@@ -5,7 +5,7 @@ cask 'rcse' do
   # downloads.sourceforge.net/rolisteam/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/rolisteam/rcse_v#{version}_MacOs.dmg"
   appcast 'https://sourceforge.net/projects/rolisteam/rss'
-  name 'RCSE'
+  name 'Rolisteam Character Sheet Editor (RCSE)'
   homepage 'https://rolisteam.org/'
 
   app 'rcse.app'
