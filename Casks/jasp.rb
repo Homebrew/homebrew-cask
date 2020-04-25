f762c4d4c01c600da39779b3dcd7d95688d0082d
Thomas J. Faulkenberry <faulkenberry@tarleton.edu>
2020-04-25 05:04:15 -0500

    Update JASP to version 0.12.2.0 (#81339)

--- Casks/jasp.rb
+++ Casks/jasp.rb
@@ -1,6 +1,6 @@
 cask 'jasp' do
-  version '0.12.1.0'
-  sha256 '3a249eb1aef8c0d5f13e6dfffad2b6e5190ac3d203e04f74ed75b59e20e95504'
+  version '0.12.2.0'
+  sha256 '90fad0e14152066b9d93c67abb2894af3b6e89b3917294db4224747138eb0c8f'
 
   url "https://static.jasp-stats.org/JASP-#{version}.dmg"
   appcast 'https://jasp-stats.org/download/',
