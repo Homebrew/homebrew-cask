3d4a578c0167a5fff67a6bfe9441c3221c4a98c8
nodeful <romankisilo@gmail.com>
2020-04-25 21:11:18 +0100

    updated eqMac cask

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -1,26 +1,20 @@
 cask 'eqmac' do
-  version '2.2'
-  sha256 'ff60579197b52571d9686bbfaec695dfa75797b8a9314fbf69a673bbeb12e1fc'
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/bitgapp/eqMac/releases/download/V#{version}/eqMac#{version.major}.dmg"
-  appcast 'https://github.com/bitgapp/eqMac/releases.atom'
+  url 'https://update.eqmac.app/eqMac.dmg'
   name 'eqMac'
-  homepage 'https://github.com/bitgapp/eqMac/releases/'
+  homepage 'https://eqmac.app'
 
-  app "eqMac#{version.major}.app"
+  app 'eqMac.app'
   installer script: {
-                      executable: "#{staged_path}/eqMac#{version.major}.app/Contents/Resources/install_driver.sh",
-                      sudo:       true,
-                    }
+              executable: "#{staged_path}/eqMac.app/Contents/Resources/install_driver.sh",
+              sudo: true,
+            }
 
-  uninstall quit:   "com.bitgapp.eqMac#{version.major}",
+  uninstall quit:   "com.bitgapp.eqmac",
             script: {
-                      executable: "#{appdir}/eqMac#{version.major}.app/Contents/Resources/uninstall_driver.sh",
-                      sudo:       true,
-                    }
-
-  zap trash: [
-               "~/Library/Caches/com.bitgapp.eqMac#{version.major}",
-               "~/Library/Cookies/com.bitgapp.eqMac#{version.major}.binarycookies",
-             ]
+              executable: "#{appdir}/eqMac.app/Contents/Resources/uninstall_driver.sh",
+              sudo: true,
+            }
 end
