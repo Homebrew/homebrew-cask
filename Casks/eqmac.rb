5fa59b6977a459e2f47dc252d0f5eef1f62d3b05
nodeful <romankisilo@gmail.com>
2020-04-25 21:16:52 +0100

    fixed styling for eqMac cask

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -4,17 +4,17 @@ cask 'eqmac' do
 
   url 'https://update.eqmac.app/eqMac.dmg'
   name 'eqMac'
-  homepage 'https://eqmac.app'
+  homepage 'https://eqmac.app/'
 
   app 'eqMac.app'
   installer script: {
-              executable: "#{staged_path}/eqMac.app/Contents/Resources/install_driver.sh",
-              sudo: true,
-            }
+                      executable: "#{staged_path}/eqMac.app/Contents/Resources/install_driver.sh",
+                      sudo:       true,
+                    }
 
-  uninstall quit:   "com.bitgapp.eqmac",
+  uninstall quit:   'com.bitgapp.eqmac',
             script: {
-              executable: "#{appdir}/eqMac.app/Contents/Resources/uninstall_driver.sh",
-              sudo: true,
-            }
+                      executable: "#{appdir}/eqMac.app/Contents/Resources/uninstall_driver.sh",
+                      sudo:       true,
+                    }
 end
