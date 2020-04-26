9b5912e2f7842fef3db3225dac05e67be5dbcc4d
Stefano Cotta Ramusino <whitone@users.noreply.github.com>
2020-04-26 20:04:22 +0200

    Update edfbrowser from 1.74,04a7ca44345f889cc84b1c0d33732a58 to 1.75,17cf821fec8dc9b27e9632c055307117 (#81394)

--- Casks/edfbrowser.rb
+++ Casks/edfbrowser.rb
@@ -1,6 +1,6 @@
 cask 'edfbrowser' do
-  version '1.74,04a7ca44345f889cc84b1c0d33732a58'
-  sha256 '70a5728eacacd45c7d6a5bd956687f3ccef8907c713521f4d341ba951505fd1d'
+  version '1.75,17cf821fec8dc9b27e9632c055307117'
+  sha256 'a0ac2209b2e4372964ba9b4d2e26bdd0b5fad86316fbd982f7b759b2b4305bb4'
 
   # gitlab.com/whitone/EDFbrowser/ was verified as official when first introduced to the cask
   url "https://gitlab.com/whitone/EDFbrowser/uploads/#{version.after_comma}/EDFbrowser-#{version.before_comma}.dmg"
