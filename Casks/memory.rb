6f296f07b5b51207a737efa8d6ec72343b985fa0
suschizu <brigitte@corecode.io>
2020-04-26 18:18:52 +0200

    Update memory.rb

--- Casks/memory.rb
+++ Casks/memory.rb
@@ -2,7 +2,7 @@ cask 'memory' do
   version '2020.08'
   sha256 '5313247b3c80ff9cebfc0cb9411d9a8d8b3d27b1aec046b9514f2698ce1bfa4b'
 
-  # timelytimetracking.s3.amazonaws.com/ was verified as official when first introduced to the cask
+  # memorymacapp.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://memorymacapp.s3.amazonaws.com/Memory.zip'
   appcast 'https://memorymacapp.s3-eu-west-1.amazonaws.com/sparkle.xml'
   name 'Memory Tracker by Timely'
