ef1d1838021e9acabd6996de123ba774f1507a78
Fabian Weisshaar <elnappo@nerdpol.io>
2020-04-27 10:05:25 +0200

    Update kstars from 3.4.1 to 3.4.2

--- Casks/kstars.rb
+++ Casks/kstars.rb
@@ -1,6 +1,6 @@
 cask 'kstars' do
-  version '3.4.1'
-  sha256 '688c7eb9a8ccd3ad41179ca4d935f42551b12969d966e7e157c470a819914855'
+  version '3.4.2'
+  sha256 'fea23b50fb903fc56b048f8ea36470325f32ca967d01b6f410b0f89ac564b215'
 
   # indilib.org/jdownloads/kstars/ was verified as official when first introduced to the cask
   url "https://www.indilib.org/jdownloads/kstars/kstars-#{version}.dmg"
