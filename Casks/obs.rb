1da92d8fd93c6f404713eba470b65e706ee014f5
Erik Minekus <ErikMinekus@users.noreply.github.com>
2020-04-27 16:48:41 +0200

    Update obs from 25.0.7 to 25.0.8 (#81451)

--- Casks/obs.rb
+++ Casks/obs.rb
@@ -1,6 +1,6 @@
 cask 'obs' do
-  version '25.0.7'
-  sha256 '5626ada067653edab3b76e314d2522fc9cdc0a8f44fb98a9b4a2b10ae3f9fdf2'
+  version '25.0.8'
+  sha256 '5674f446c22e34ea2696386a4d04aa201ac692c8217f610ee3c1d002e371d11f'
 
   url "https://cdn-fastly.obsproject.com/downloads/obs-mac-#{version}.dmg"
   appcast 'https://github.com/obsproject/obs-studio/releases.atom'
