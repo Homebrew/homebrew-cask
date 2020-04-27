991afdb22ef327b38fc7918d54c411b8e91487ed
ymshenyu <39402395+ymshenyu@users.noreply.github.com>
2020-04-27 21:10:14 +0800

    Update qv2ray.rb

--- Casks/qv2ray.rb
+++ Casks/qv2ray.rb
@@ -2,7 +2,7 @@ cask 'qv2ray' do
   version '2.5.0'
   sha256 '8c2f7604ac28396420ff042e69ebe2b2f13c6a670df09a4955ece7a855a243e3'
 
-  # github.com/Qv2ray/Qv2ray was verified as official when first introduced to the cask
+  # github.com/Qv2ray/Qv2ray/ was verified as official when first introduced to the cask
   url "https://github.com/Qv2ray/Qv2ray/releases/download/v#{version}/Qv2ray-v#{version}.macOS-x64.dmg"
   appcast 'https://github.com/Qv2ray/Qv2ray/releases.atom'
   name 'Qv2ray'
