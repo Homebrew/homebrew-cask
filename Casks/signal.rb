51a48aee5629a258534acd184314eb309a6b869a
tiiiecherle <tiiiecherle@gmail.com>
2020-04-28 08:04:16 +0200

    Update signal from 1.33.3 to 1.33.4 (#81499)

--- Casks/signal.rb
+++ Casks/signal.rb
@@ -1,6 +1,6 @@
 cask 'signal' do
-  version '1.33.3'
-  sha256 '1f5061ae70fd12af7a81e47b75e0c7c8fc66a0f67edb27e3fc117d8c3f47e56c'
+  version '1.33.4'
+  sha256 '84cbe18f00266594b675b19f1f0edd01ce5aa6fd5e0efc95ab5aa9baa7ea28f0'
 
   url "https://updates.signal.org/desktop/signal-desktop-mac-#{version}.dmg"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
