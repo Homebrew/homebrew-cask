86104462b5cb5a5bb30fbc616483fe192e15f95b
John Benninghoff <jbenninghoff@mac.com>
2020-04-28 16:38:50 -0500

    Update mactracker (#81551)

--- Casks/mactracker.rb
+++ Casks/mactracker.rb
@@ -1,6 +1,6 @@
 cask 'mactracker' do
   version '7.9'
-  sha256 'b65e18344cca843a74b48804808cded0eee9a7ba2116386bcd34cf0469ede81e'
+  sha256 'b73051559db9d5b3a7d1d0e44a70f3b7b36784bfc7c732863017325c10bffca1'
 
   url "https://www.mactracker.ca/downloads/Mactracker_#{version}.zip"
   appcast 'https://update.mactracker.ca/appcast-b.xml'
