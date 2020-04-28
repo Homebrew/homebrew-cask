442915a3b4bcbbf69f71995565dbdba48741b40d
Roman Kisil <8472525+nodeful@users.noreply.github.com>
2020-04-28 12:13:06 +0100

    Update eqmac.rb
    Added trailing slash to repo verification url comment.

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -2,7 +2,7 @@ cask 'eqmac' do
   version '0.0.1'
   sha256 '00538e305f4ad90c688fc4fd9dd07969f713e35051ef98971cda53368996f918'
 
-  # github.com/bitgapp/eqMac was verified as official when first introduced to the cask
+  # github.com/bitgapp/eqMac/ was verified as official when first introduced to the cask
   url "https://github.com/bitgapp/eqMac/releases/download/v#{version}/eqMac.dmg"
   appcast 'https://github.com/bitgapp/eqMac/releases.atom'
   name 'eqMac'
