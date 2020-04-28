aab3d6e1db3c3b054a3ed0a1856939e8aaf14450
John Stader <47798010+jstader@users.noreply.github.com>
2020-04-27 21:10:51 -0400

    Update eqmac.rb

--- Casks/eqmac.rb
+++ Casks/eqmac.rb
@@ -2,7 +2,7 @@ cask 'eqmac' do
   version '0.0.1'
   sha256 '00538e305f4ad90c688fc4fd9dd07969f713e35051ef98971cda53368996f918'
 
-  url "https://github.com/bitgapp/eqMac/releases/download/V#{version}/eqMac.dmg"
+  url "https://github.com/bitgapp/eqMac/releases/download/v#{version}/eqMac.dmg"
   appcast 'https://github.com/bitgapp/eqMac/releases.atom'
   name 'eqMac'
   homepage 'https://github.com/bitgapp/eqMac/releases/'
