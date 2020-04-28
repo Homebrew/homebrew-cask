e60edec8e232c2a6e30ba2654f63ade491c277a5
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-28 11:36:42 -0700

    Update polymail from 2.1.8 to 2.1.9 (#81537)

--- Casks/polymail.rb
+++ Casks/polymail.rb
@@ -1,6 +1,6 @@
 cask 'polymail' do
-  version '2.1.8'
-  sha256 '4f532c6e76869362f9b1334b34ebfbdf211ed03e25a1d6f1ac2bfff077b60f75'
+  version '2.1.9'
+  sha256 '92b91d488ec409887e90ce2a4ad864e3e066ab5cbbc6be496e8c7beffb79df64'
 
   url "https://sparkle-updater.polymail.io/macos/builds/Polymail-v#{version}.zip"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://sparkle-updater.polymail.io/osx/Polymail-Latest.zip'
