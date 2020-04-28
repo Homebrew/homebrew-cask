c2f4d8c4367b61a0e6c3b16466565de95337cb4d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-04-28 11:20:18 -0700

    Update swift-publisher from 5.5.3 to 5.5.4 (#81533)

--- Casks/swift-publisher.rb
+++ Casks/swift-publisher.rb
@@ -1,6 +1,6 @@
 cask 'swift-publisher' do
-  version '5.5.3'
-  sha256 '8c9eb3f31d599d04b36c0ff1dae192960c343adeb18a4439c90cabf5f97c0afa'
+  version '5.5.4'
+  sha256 'c6b6327c4cb5a19d7d6dee2ce56678aa0fc866b2d272178d847965dbd2b41dca'
 
   # belightsoft.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://belightsoft.s3.amazonaws.com/SwiftPublisher.dmg'
